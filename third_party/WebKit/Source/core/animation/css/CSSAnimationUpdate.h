3bef3c2acc5e26e83f05c165e855e899710b8375
samli@chromium.org <samli@chromium.org>
2015-02-16 07:51:13 +0000

    Animations: Allow transitions to be retriggered
    Transitions were required to be cancelled once they finished since they
    had a fill mode of 'both'. Since this has been changed to 'none', this
    is no longer required and prevents the transition from being retriggered.
    
    This change allows finished transitions to remain in that state which
    allows them to be retriggered based on changes to the animation timeline
    current time.
    
    BUG=434576
    
    Review URL: https://codereview.chromium.org/919723003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190229 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/css/CSSAnimationUpdate.h
+++ third_party/WebKit/Source/core/animation/css/CSSAnimationUpdate.h
@@ -120,6 +120,7 @@ public:
     }
     bool isCancelledTransition(CSSPropertyID id) const { return m_cancelledTransitions.contains(id); }
     void cancelTransition(CSSPropertyID id) { m_cancelledTransitions.add(id); }
+    void finishTransition(CSSPropertyID id) { m_finishedTransitions.add(id); }
 
     const WillBeHeapVector<NewAnimation>& newAnimations() const { return m_newAnimations; }
     const Vector<AtomicString>& cancelledAnimationNames() const { return m_cancelledAnimationNames; }
@@ -146,6 +147,7 @@ public:
     using NewTransitionMap = WillBeHeapHashMap<CSSPropertyID, NewTransition>;
     const NewTransitionMap& newTransitions() const { return m_newTransitions; }
     const HashSet<CSSPropertyID>& cancelledTransitions() const { return m_cancelledTransitions; }
+    const HashSet<CSSPropertyID>& finishedTransitions() const { return m_finishedTransitions; }
 
     void adoptActiveInterpolationsForAnimations(WillBeHeapHashMap<CSSPropertyID, RefPtrWillBeMember<Interpolation>>& newMap) { newMap.swap(m_activeInterpolationsForAnimations); }
     void adoptActiveInterpolationsForTransitions(WillBeHeapHashMap<CSSPropertyID, RefPtrWillBeMember<Interpolation>>& newMap) { newMap.swap(m_activeInterpolationsForTransitions); }
@@ -162,6 +164,7 @@ public:
             && m_animationsWithUpdates.isEmpty()
             && m_newTransitions.isEmpty()
             && m_cancelledTransitions.isEmpty()
+            && m_finishedTransitions.isEmpty()
             && m_activeInterpolationsForAnimations.isEmpty()
             && m_activeInterpolationsForTransitions.isEmpty();
     }
@@ -181,6 +184,7 @@ private:
 
     NewTransitionMap m_newTransitions;
     HashSet<CSSPropertyID> m_cancelledTransitions;
+    HashSet<CSSPropertyID> m_finishedTransitions;
 
     WillBeHeapHashMap<CSSPropertyID, RefPtrWillBeMember<Interpolation>> m_activeInterpolationsForAnimations;
     WillBeHeapHashMap<CSSPropertyID, RefPtrWillBeMember<Interpolation>> m_activeInterpolationsForTransitions;
