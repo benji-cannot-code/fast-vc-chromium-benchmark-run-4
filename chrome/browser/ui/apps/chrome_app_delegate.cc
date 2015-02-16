3a89a23578fa9400fce9c65451290fbf2100a46b
jackhou <jackhou@chromium.org>
2015-02-15 16:38:54 -0800

    Hidden windows should not keep Chrome alive.
    For each app window, there is a ChromeAppDelegate which
    holds a ScopedKeepAlive. This CL resets the ScopedKeepAlive
    while the app window is hidden.
    
    BUG=450710
    
    Review URL: https://codereview.chromium.org/875273003
    
    Cr-Commit-Position: refs/heads/master@{#316413}

--- chrome/browser/ui/apps/chrome_app_delegate.cc
+++ chrome/browser/ui/apps/chrome_app_delegate.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/chrome_extension_messages.h"
 #include "components/ui/zoom/zoom_controller.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/host_zoom_map.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/render_view_host.h"
@@ -47,6 +48,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // defined(ENABLE_PRINTING)
 
 namespace {
+
+// Time to wait for an app window to show before allowing Chrome to quit.
+int kAppWindowFirstShowTimeoutSeconds = 10;
 
 bool disable_external_open_for_testing_ = false;
 
@@ -112,6 +116,16 @@ class OpenURLFromTabBasedOnBrowserDefault
 
 }  // namespace
 
+// static
+void ChromeAppDelegate::RelinquishKeepAliveAfterTimeout(
+    const base::WeakPtr<ChromeAppDelegate>& chrome_app_delegate) {
+  // Resetting the ScopedKeepAlive may cause nested destruction of the
+  // ChromeAppDelegate which also resets the ScopedKeepAlive. To avoid this,
+  // move the ScopedKeepAlive out to here and let it fall out of scope.
+  if (chrome_app_delegate.get() && chrome_app_delegate->is_hidden_)
+    scoped_ptr<ScopedKeepAlive>(chrome_app_delegate->keep_alive_.Pass());
+}
+
 class ChromeAppDelegate::NewWindowContentsDelegate
     : public content::WebContentsDelegate {
  public:
@@ -152,8 +166,11 @@ ChromeAppDelegate::NewWindowContentsDelegate::OpenURLFromTab(
 }
 
 ChromeAppDelegate::ChromeAppDelegate(scoped_ptr<ScopedKeepAlive> keep_alive)
-    : keep_alive_(keep_alive.Pass()),
-      new_window_contents_delegate_(new NewWindowContentsDelegate()) {
+    : has_been_shown_(false),
+      is_hidden_(true),
+      keep_alive_(keep_alive.Pass()),
+      new_window_contents_delegate_(new NewWindowContentsDelegate()),
+      weak_factory_(this) {
   registrar_.Add(this,
                  chrome::NOTIFICATION_APP_TERMINATING,
                  content::NotificationService::AllSources());
@@ -303,6 +320,28 @@ bool ChromeAppDelegate::IsWebContentsVisible(
 
 void ChromeAppDelegate::SetTerminatingCallback(const base::Closure& callback) {
   terminating_callback_ = callback;
+}
+
+void ChromeAppDelegate::OnHide() {
+  is_hidden_ = true;
+  if (has_been_shown_) {
+    keep_alive_.reset();
+    return;
+  }
+
+  // Hold on to the keep alive for some time to give the app a chance to show
+  // the window.
+  content::BrowserThread::PostDelayedTask(
+      content::BrowserThread::UI, FROM_HERE,
+      base::Bind(&ChromeAppDelegate::RelinquishKeepAliveAfterTimeout,
+                 weak_factory_.GetWeakPtr()),
+      base::TimeDelta::FromSeconds(kAppWindowFirstShowTimeoutSeconds));
+}
+
+void ChromeAppDelegate::OnShow() {
+  has_been_shown_ = true;
+  is_hidden_ = false;
+  keep_alive_.reset(new ScopedKeepAlive);
 }
 
 void ChromeAppDelegate::Observe(int type,
--- chrome/test/data/extensions/platform_apps/hidden_then_shown/test.js
+++ chrome/test/data/extensions/platform_apps/hidden_then_shown/test.js
@@ -6,7 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('empty.html', {
     hidden: true,
-  }, function () {
-    chrome.test.sendMessage('Launched');
+  }, function (win) {
+    chrome.test.sendMessage('Launched', function () {
+      setTimeout(function () {
+        win.show();
+        chrome.test.sendMessage('Shown');
+      }, 3000);
+    });
   });
 });
