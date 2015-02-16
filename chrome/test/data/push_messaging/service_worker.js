48da4bd2e2aaf4e25c6fda59fac5aa41fd01da45
peter <peter@chromium.org>
2015-02-16 10:32:49 -0800

    Move the PushMessagingBrowserTest to use SWR.showNotification().
    This will allow us to start throwing a TypeError per the specification
    when constructing a new Notification object in a Service Worker.
    
    This is part of a two-sided patch:
      [1] This patch.
      [2] https://codereview.chromium.org/923443005
    
    BUG=459038
    
    Review URL: https://codereview.chromium.org/930963002
    
    Cr-Commit-Position: refs/heads/master@{#316496}

--- chrome/test/data/push_messaging/service_worker.js
+++ chrome/test/data/push_messaging/service_worker.js
@@ -4,18 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 this.onpush = function(event) {
-  // TODO(peter): Remove this check once Blink supports PushMessageData.json().
-  var data = event.data;
-  if (typeof data !== 'string')
-    data = data.text();
-
+  var data = event.data.text();
   if (data !== 'shownotification') {
     sendMessageToClients('push', data);
     return;
   }
 
-  // TODO(peter): Switch to self.registration.showNotification once implemented.
-  event.waitUntil(showLegacyNonPersistentNotification('Push test title', {
+  event.waitUntil(registration.showNotification('Push test title', {
     body: 'Push test body',
     tag: 'push_test_tag'
   }).then(function(notification) {
@@ -36,13 +31,5 @@ function sendMessageToClients(type, data) {
     });
   }, function(error) {
     console.log(error);
-  });
-}
-
-function showLegacyNonPersistentNotification(title, options) {
-  return new Promise(function(resolve, reject) {
-    var notification = new Notification(title, options);
-    notification.onshow = function() { resolve(notification); };
-    notification.onerror = function() { reject(new Error('Failed to show')); };
   });
 }
