110212138ba3c775bdd6c51bca131c92bf95fac3
mazda <mazda@chromium.org>
2015-02-16 04:42:29 -0800

    Prepare for moving nacl_irt_icache definition to the NaCl repository.
    Once nacl_irt_icache has been included in irt.h in the NaCl
    repository, Chrome build would fail due to multiple definition error.
    This is a change for preparing for the NaCl roll that includes
    the NaCl side change.
    
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=4033
    
    Review URL: https://codereview.chromium.org/924783003
    
    Cr-Commit-Position: refs/heads/master@{#316461}

--- ppapi/nacl_irt/public/irt_nonsfi.h
+++ ppapi/nacl_irt/public/irt_nonsfi.h
@@ -4,9 +4,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
+// TODO(mazda): Remove this file once NaCl has been rolled to the revision
+// that includes the definition of nacl_irt_icache.
+// https://code.google.com/p/nativeclient/issues/detail?id=4033
 
 #ifndef PPAPI_NACL_IRT_PUBLIC_IRT_NONSFI_H_
 #define PPAPI_NACL_IRT_PUBLIC_IRT_NONSFI_H_
+
+#if !defined(NACL_IRT_ICACHE_v0_1)
 
 #include <stddef.h>
 
@@ -21,5 +26,7 @@ struct nacl_irt_icache {
    */
   int (*clear_cache)(void* addr, size_t size);
 };
+
+#endif
 
 #endif  // PPAPI_NACL_IRT_PUBLIC_IRT_NONSFI_H_
