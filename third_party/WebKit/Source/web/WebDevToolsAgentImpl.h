93e35d48eb75b2cfe3053fbadb09eb17b66aba08
sergeyv@chromium.org <sergeyv@chromium.org>
2015-02-17 13:29:11 +0000

    DevTools: [Blink-side] Fix debugger command order
    Chromium-side patch: https://codereview.chromium.org/930113002/
    
    BUG=458035
    
    Review URL: https://codereview.chromium.org/932523003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebDevToolsAgentImpl.h
+++ third_party/WebKit/Source/web/WebDevToolsAgentImpl.h
@@ -56,6 +56,7 @@ class WebInputEvent;
 class WebLocalFrameImpl;
 class WebString;
 class WebViewImpl;
+class DebuggerTask;
 
 class WebDevToolsAgentImpl final
     : public WebDevToolsAgent
@@ -140,6 +141,8 @@ private:
     typedef Vector<RefPtr<JSONObject> > NotificationQueue;
     NotificationQueue m_notificationQueue;
     String m_stateCookie;
+
+    friend class DebuggerTask;
 };
 
 } // namespace blink
