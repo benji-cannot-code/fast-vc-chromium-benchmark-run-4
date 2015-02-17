a5c647b9703a4fff5758e2f8f72588b8157da5c0
yawano <yawano@chromium.org>
2015-02-17 02:14:52 -0800

    Add transition animation of thumbnail update in grid view.
    BUG=456016
    TEST=Open a grid view and edit an image. Thumbnail should be updated with animation.
    
    Review URL: https://codereview.chromium.org/931103002
    
    Cr-Commit-Position: refs/heads/master@{#316553}

--- ui/file_manager/file_manager/foreground/js/ui/file_grid.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_grid.js
@@ -16,15 +16,6 @@ function FileGrid() {
 }
 
 /**
- * Thumbnail quality.
- * @enum {number}
- */
-FileGrid.ThumbnailQuality = {
-  LOW: 0,
-  HIGH: 1
-};
-
-/**
  * Inherits from cr.ui.Grid.
  */
 FileGrid.prototype.__proto__ = cr.ui.Grid.prototype;
@@ -114,10 +105,10 @@ FileGrid.prototype.setListThumbnailLoader = function(listThumbnailLoader) {
 FileGrid.prototype.onThumbnailLoaded_ = function(event) {
   var listItem = this.getListItemByIndex(event.index);
   if (listItem) {
-    var thumbnail = listItem.querySelector('.img-container > .thumbnail');
-    if (thumbnail) {
+    var box = listItem.querySelector('.img-container');
+    if (box) {
       FileGrid.setThumbnailImage_(
-          assertInstanceof(thumbnail, HTMLDivElement), event.dataUrl);
+          assertInstanceof(box, HTMLDivElement), event.dataUrl);
     }
   }
 };
@@ -156,7 +147,7 @@ FileGrid.prototype.updateListItemsMetadata = function(type, entries) {
     if (!entry || urls.indexOf(entry.toURL()) === -1)
       continue;
 
-    FileGrid.decorateThumbnailBox_(box,
+    FileGrid.decorateThumbnailBox_(assertInstanceof(box, HTMLDivElement),
                                    entry,
                                    this.fileSystemMetadata_,
                                    this.volumeManager_,
@@ -236,7 +227,7 @@ FileGrid.decorateThumbnail_ = function(
   }
   if (shouldLoadThumbnail) {
     FileGrid.decorateThumbnailBox_(
-        box,
+        assertInstanceof(box, HTMLDivElement),
         entry,
         fileSystemMetadata,
         volumeManager,
@@ -266,7 +257,7 @@ FileGrid.decorateThumbnail_ = function(
 /**
  * Decorates the box containing a centered thumbnail image.
  *
- * @param {Element} box Box to decorate.
+ * @param {!HTMLDivElement} box Box to decorate.
  * @param {Entry} entry Entry which thumbnail is generating for.
  * @param {!FileSystemMetadata} fileSystemMetadata To retrieve metadata.
  * @param {VolumeManagerWrapper} volumeManager Volume manager instance.
@@ -299,16 +290,11 @@ FileGrid.decorateThumbnailBox_ = function(
     return;
   }
 
-  // Create a box for thumbnail here to perform transition correctly.
-  var thumbnail = box.ownerDocument.createElement('div');
-  thumbnail.classList.add('thumbnail');
-  box.appendChild(thumbnail);
-
   // Set thumbnail if it's already in cache.
   if (listThumbnailLoader &&
       listThumbnailLoader.getThumbnailFromCache(entry)) {
     FileGrid.setThumbnailImage_(
-        assertInstanceof(thumbnail, HTMLDivElement),
+        box,
         listThumbnailLoader.getThumbnailFromCache(entry).dataUrl);
   } else {
     var mediaType = FileType.getMediaType(entry);
@@ -318,14 +304,29 @@ FileGrid.decorateThumbnailBox_ = function(
 
 /**
  * Sets thumbnail image to the box.
- * @param {!HTMLDivElement} thumbnail A div element to be filled with thumbnail.
+ * @param {!HTMLDivElement} thumbnail A div element to hold thumbnails.
  * @param {string} dataUrl Data url of thumbnail.
  * @private
  */
-FileGrid.setThumbnailImage_ = function(thumbnail, dataUrl) {
-  // TODO(yawano): Add transition animation for thumbnail update.
+FileGrid.setThumbnailImage_ = function(box, dataUrl) {
+  var oldThumbnails = box.querySelectorAll('.thumbnail');
+
+  var thumbnail = box.ownerDocument.createElement('div');
+  thumbnail.classList.add('thumbnail');
   thumbnail.style.backgroundImage = 'url(' + dataUrl + ')';
-  thumbnail.classList.add('loaded');
+  thumbnail.addEventListener('webkitAnimationEnd', function() {
+    // Remove animation css once animation is completed in order not to animate
+    // agein when an item is attached to the dom again.
+    thumbnail.classList.remove('animate');
+
+    for (var i = 0; i < oldThumbnails.length; i++) {
+      if (box.contains(oldThumbnails[i]))
+        box.removeChild(oldThumbnails[i]);
+    }
+  });
+  thumbnail.classList.add('animate');
+
+  box.appendChild(thumbnail);
 };
 
 /**
