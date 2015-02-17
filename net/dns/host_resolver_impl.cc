ab76571c6bef2006c7d6208eb43ef36b7d3e590f
pauljensen <pauljensen@chromium.org>
2015-02-17 08:59:14 -0800

    Skip IPv6 probes when resolving IPv4 literals.  The probes aren't necessary as they at most restrict to IPv4 results which doesn't change the resolver's result for IPv4 literals.
    BUG=453456
    
    Review URL: https://codereview.chromium.org/926593003
    
    Cr-Commit-Position: refs/heads/master@{#316589}

--- net/dns/host_resolver_impl.cc
+++ net/dns/host_resolver_impl.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/host_resolver_proc.h"
 #include "net/socket/client_socket_factory.h"
 #include "net/udp/datagram_client_socket.h"
+#include "url/url_canon_ip.h"
 
 #if defined(OS_WIN)
 #include "net/base/winsock_init.h"
@@ -2155,7 +2156,13 @@ HostResolverImpl::Key HostResolverImpl::GetEffectiveKeyForRequest(
   AddressFamily effective_address_family = info.address_family();
 
   if (info.address_family() == ADDRESS_FAMILY_UNSPECIFIED) {
-    if (probe_ipv6_support_ && !use_local_ipv6_) {
+    unsigned char ip_number[4];
+    url::Component host_comp(0, info.hostname().size());
+    int num_components;
+    if (probe_ipv6_support_ && !use_local_ipv6_ &&
+        // Don't bother IPv6 probing when resolving IPv4 literals.
+        url::IPv4AddressToNumber(info.hostname().c_str(), host_comp, ip_number,
+                                 &num_components) != url::CanonHostInfo::IPV4) {
       // Google DNS address.
       const uint8 kIPv6Address[] =
           { 0x20, 0x01, 0x48, 0x60, 0x48, 0x60, 0x00, 0x00,
