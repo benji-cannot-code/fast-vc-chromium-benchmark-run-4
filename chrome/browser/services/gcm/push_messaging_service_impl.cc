c9b28148d5348726ab2809d74630c285c2b9a84e
mlamouri <mlamouri@chromium.org>
2015-02-17 06:40:18 -0800

    [Push] Use proper accessors for PushMessagingApplicationId.
    The current design is violating the coding style (public member
    variables) and is breaking some implicit functions used by some
    STL implementations.
    
    BUG=443228
    
    Review URL: https://codereview.chromium.org/931113003
    
    Cr-Commit-Position: refs/heads/master@{#316576}

--- chrome/browser/services/gcm/push_messaging_service_impl.cc
+++ chrome/browser/services/gcm/push_messaging_service_impl.cc
@@ -192,9 +192,9 @@ void PushMessagingServiceImpl::OnMessage(
     return;
   }
   // Drop message and unregister if |origin| has lost push permission.
-  if (!HasPermission(application_id.origin)) {
-    DeliverMessageCallback(app_id, application_id.origin,
-                           application_id.service_worker_registration_id,
+  if (!HasPermission(application_id.origin())) {
+    DeliverMessageCallback(app_id, application_id.origin(),
+                           application_id.service_worker_registration_id(),
                            message,
                            content::PUSH_DELIVERY_STATUS_PERMISSION_DENIED);
     return;
@@ -226,13 +226,13 @@ void PushMessagingServiceImpl::OnMessage(
 
   content::BrowserContext::DeliverPushMessage(
       profile_,
-      application_id.origin,
-      application_id.service_worker_registration_id,
+      application_id.origin(),
+      application_id.service_worker_registration_id(),
       data,
       base::Bind(&PushMessagingServiceImpl::DeliverMessageCallback,
                  weak_factory_.GetWeakPtr(),
-                 application_id.app_id_guid, application_id.origin,
-                 application_id.service_worker_registration_id, message));
+                 application_id.app_id_guid(), application_id.origin(),
+                 application_id.service_worker_registration_id(), message));
 }
 
 void PushMessagingServiceImpl::SetProfileForTesting(Profile* profile) {
@@ -551,7 +551,7 @@ void PushMessagingServiceImpl::RegisterFromWorker(
   IncreasePushRegistrationCount(1, true /* is_pending */);
   std::vector<std::string> sender_ids(1, sender_id);
   gcm_profile_service_->driver()->Register(
-      application_id.app_id_guid, sender_ids,
+      application_id.app_id_guid(), sender_ids,
       base::Bind(&PushMessagingServiceImpl::DidRegister,
                  weak_factory_.GetWeakPtr(),
                  application_id, register_callback));
@@ -608,7 +608,7 @@ void PushMessagingServiceImpl::DidRequestPermission(
   IncreasePushRegistrationCount(1, true /* is_pending */);
   std::vector<std::string> sender_ids(1, sender_id);
   gcm_profile_service_->driver()->Register(
-      application_id.app_id_guid,
+      application_id.app_id_guid(),
       sender_ids,
       base::Bind(&PushMessagingServiceImpl::DidRegister,
                  weak_factory_.GetWeakPtr(),
@@ -630,7 +630,7 @@ void PushMessagingServiceImpl::Unregister(
     return;
   }
 
-  Unregister(application_id.app_id_guid, retry_on_failure, callback);
+  Unregister(application_id.app_id_guid(), retry_on_failure, callback);
 }
 
 void PushMessagingServiceImpl::Unregister(
