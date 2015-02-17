3aee56364e43dcd478e129dd8cd7c549c4c779c6
dcheng <dcheng@chromium.org>
2015-02-17 12:09:44 -0800

    Make logging a scoped_ptr<T> more useful.
    Given the code:
      scoped_ptr<std::string> x;
      LOG(ERROR) << x;
    the logging output will always be 0 or 1. The reason is the
    scoped_ptr<T> is getting turned into a bool before being logged. This
    behavior doesn't seem very useful, so add a logging overload to make it
    print out the pointer value.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/837873002
    
    Cr-Commit-Position: refs/heads/master@{#316640}

--- base/memory/scoped_ptr.h
+++ base/memory/scoped_ptr.h
@@ -86,6 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdlib.h>
 
 #include <algorithm>  // For std::swap().
+#include <iosfwd>
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
@@ -584,6 +585,11 @@ bool operator!=(T* p1, const scoped_ptr<T, D>& p2) {
 template <typename T>
 scoped_ptr<T> make_scoped_ptr(T* ptr) {
   return scoped_ptr<T>(ptr);
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const scoped_ptr<T>& p) {
+  return out << p.get();
 }
 
 #endif  // BASE_MEMORY_SCOPED_PTR_H_
