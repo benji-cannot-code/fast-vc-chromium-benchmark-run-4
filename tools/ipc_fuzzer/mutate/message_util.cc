c54179e14ec6d740a95032c1681afe671637666e
mbarbella <mbarbella@chromium.org>
2015-02-17 11:02:55 -0800

    Typo fix in ipc_message_util help text. R=inferno@chromium.org
    Review URL: https://codereview.chromium.org/937433002
    
    Cr-Commit-Position: refs/heads/master@{#316620}

--- tools/ipc_fuzzer/mutate/message_util.cc
+++ tools/ipc_fuzzer/mutate/message_util.cc
@@ -53,7 +53,7 @@ void usage() {
             << "  ipc_message_util"
             << " [--" << kStartSwitch << "=n]"
             << " [--" << kEndSwitch << "=m]"
-            << " [--" << kInSwitch << "=i,j,...]]"
+            << " [--" << kInSwitch << "=i[,j,...]]"
             << " [--" << kRegexpSwitch << "=x]"
             << " [--" << kInvertSwitch << "]"
             << " [--" << kDumpSwitch << "]"
