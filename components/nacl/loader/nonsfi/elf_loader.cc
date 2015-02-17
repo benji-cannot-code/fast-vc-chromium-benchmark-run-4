2a2b874b5db017d039fd98e896b4ce1913352a48
mseaborn <mseaborn@chromium.org>
2015-02-17 07:17:07 -0800

    NaCl cleanup: Remove unused #include of shared/platform/nacl_host_desc.h
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=2832
    TEST=build
    
    Review URL: https://codereview.chromium.org/933733003
    
    Cr-Commit-Position: refs/heads/master@{#316578}

--- components/nacl/loader/nonsfi/elf_loader.cc
+++ components/nacl/loader/nonsfi/elf_loader.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "native_client/src/include/portability.h"
-#include "native_client/src/shared/platform/nacl_host_desc.h"
 #include "native_client/src/trusted/desc/nacl_desc_base.h"
 #include "native_client/src/trusted/desc/nacl_desc_effector_trusted_mem.h"
 #include "native_client/src/trusted/service_runtime/include/bits/mman.h"
