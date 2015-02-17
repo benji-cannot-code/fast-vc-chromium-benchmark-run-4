2b459c3540282cf202b9cfb7932bf8bb69e655a8
mbarbella <mbarbella@chromium.org>
2015-02-17 12:00:11 -0800

    Minor ipc_message_util cleanup.
    Addresses comments from https://codereview.chromium.org/931153003/
    
    R=tsepez@chromium.org,inferno@chromium.org
    BUG=450268
    
    Review URL: https://codereview.chromium.org/920433003
    
    Cr-Commit-Position: refs/heads/master@{#316637}

--- tools/ipc_fuzzer/mutate/message_util.cc
+++ tools/ipc_fuzzer/mutate/message_util.cc
@@ -31,7 +31,7 @@ const char kHelpSwitchHelp[] =
 
 const char kInSwitch[] = "in";
 const char kInSwitchHelp[] =
-    "output only the messages in the specified list.";
+    "output only the messages at the specified positions in the file.";
 
 const char kInvertSwitch[] = "invert";
 const char kInvertSwitchHelp[] =
@@ -103,9 +103,6 @@ int main(int argc, char** argv) {
       end_index = static_cast<size_t>(temp);
   }
 
-  bool has_indices = cmd->HasSwitch(kInSwitch);
-  std::vector<bool> indices;
-
   bool has_regexp = cmd->HasSwitch(kRegexpSwitch);
   RE2 filter_pattern(cmd->GetSwitchValueASCII(kRegexpSwitch));
 
@@ -134,6 +131,9 @@ int main(int argc, char** argv) {
                                 message_vector.begin(), message_vector.end());
     message_vector.weak_clear();
   }
+
+  bool has_indices = cmd->HasSwitch(kInSwitch);
+  std::vector<bool> indices;
 
   if (has_indices) {
     indices.resize(input_message_vector.size(), false);
