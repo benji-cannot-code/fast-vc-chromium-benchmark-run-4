7a8242b76b522c4d3eb1e126d351a79505cd62ef
spang <spang@chromium.org>
2015-02-17 12:55:29 -0800

    Fix component build for CrOS v4l2
    GetLastEGLErrorString must be exported since is now used from content
    since r310941 ("Reland: V4L2VDA: Generalize EGLImage import and query
    driver for output formats.").
    
    BUG=none
    TEST=build CrOS chrome for link
    
    Review URL: https://codereview.chromium.org/924183002
    
    Cr-Commit-Position: refs/heads/master@{#316650}

--- ui/gl/egl_util.h
+++ ui/gl/egl_util.h
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GL_EGL_UTIL_H_
 #define UI_GL_EGL_UTIL_H_
 
+#include "ui/gl/gl_export.h"
+
 namespace ui {
 
 // Returns the last EGL error as a string.
-const char* GetLastEGLErrorString();
+GL_EXPORT const char* GetLastEGLErrorString();
 
 }  // namespace ui
 
