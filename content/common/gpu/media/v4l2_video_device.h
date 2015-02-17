dfbac3f8f59d90fb88e48aec5abf1a3f9c3aa182
dnicoara <dnicoara@chromium.org>
2015-02-17 13:43:43 -0800

    Fix component build for vide_decode_accelerator_unittest
    BUG=none
    TEST=Compile video_decode_accelerator_unittest with component build
    
    Review URL: https://codereview.chromium.org/934753003
    
    Cr-Commit-Position: refs/heads/master@{#316659}

--- content/common/gpu/media/v4l2_video_device.h
+++ content/common/gpu/media/v4l2_video_device.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_COMMON_GPU_MEDIA_V4L2_VIDEO_DEVICE_H_
 
 #include "base/memory/ref_counted.h"
+#include "content/common/content_export.h"
 #include "media/base/video_decoder_config.h"
 #include "media/base/video_frame.h"
 #include "ui/gfx/geometry/size.h"
@@ -18,7 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-class V4L2Device : public base::RefCountedThreadSafe<V4L2Device> {
+class CONTENT_EXPORT V4L2Device
+    : public base::RefCountedThreadSafe<V4L2Device> {
  public:
   // Utility format conversion functions
   static media::VideoFrame::Format V4L2PixFmtToVideoFrameFormat(uint32 format);
