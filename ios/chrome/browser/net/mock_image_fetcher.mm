9abb84e29845c9590e44a49a43f86846c937eb77
bzanotti <bzanotti@chromium.org>
2015-02-18 07:35:28 -0800

    Add mock class for iOS ImageFetcher.
    BUG=
    
    Review URL: https://codereview.chromium.org/917333006
    
    Cr-Commit-Position: refs/heads/master@{#316830}

---
+++ ios/chrome/browser/net/mock_image_fetcher.mm
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/net/mock_image_fetcher.h"
+
+namespace image_fetcher {
+
+MockImageFetcher::MockImageFetcher(
+    const scoped_refptr<base::TaskRunner>& task_runner)
+    : ImageFetcher(task_runner) {
+}
+
+MockImageFetcher::~MockImageFetcher() {
+}
+
+}  // namespace image_fetcher
