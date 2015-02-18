da4aae916f77cd8d74ea5c880439f483bff6a545
yawano <yawano@chromium.org>
2015-02-17 22:47:40 -0800

    Add contentMimeType and availableOffline to FileSystemMetadataProvider.
    Background of this fix can be found at crrev.com/928043004.
    
    BUG=459140
    TEST=out/Release/browser_test --gtest_filter=FileManagerJsTest.FileSystemMetadataProvider;
    Manually tested as described in the issue.
    
    Review URL: https://codereview.chromium.org/931423002
    
    Cr-Commit-Position: refs/heads/master@{#316776}

--- ui/file_manager/file_manager/foreground/js/metadata/file_system_metadata_provider.js
+++ ui/file_manager/file_manager/foreground/js/metadata/file_system_metadata_provider.js
@@ -4,7 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 /**
- * @typedef {{modificationTime:Date, size:number}}
+ * @typedef {{modificationTime:Date, size:number, contentMimeType:string,
+ *     present:boolean, availableOffline: boolean}}
  */
 var FileSystemMetadataProperties;
 
@@ -24,8 +25,9 @@ function FileSystemMetadataProvider(cache) {
 /**
  * @const {!Array<string>}
  */
-FileSystemMetadataProvider.PROPERTY_NAMES =
-    ['modificationTime', 'size', 'present'];
+FileSystemMetadataProvider.PROPERTY_NAMES = [
+    'modificationTime', 'size', 'present', 'availableOffline', 'contentMimeType'
+];
 
 FileSystemMetadataProvider.prototype.__proto__ = NewMetadataProvider.prototype;
 
@@ -34,14 +36,30 @@ FileSystemMetadataProvider.prototype.__proto__ = NewMetadataProvider.prototype;
  */
 FileSystemMetadataProvider.prototype.getImpl = function(requests) {
   return Promise.all(requests.map(function(request) {
-    return new Promise(function(fulfill, reject) {
-      request.entry.getMetadata(fulfill, reject);
-    }).then(function(properties) {
-      return {
-        modificationTime: properties.modificationTime,
-        size: request.entry.isDirectory ? -1 : properties.size,
-        present: true
+    return Promise.all([
+        new Promise(function(fulfill, reject) {
+          request.entry.getMetadata(fulfill, reject);
+        }),
+        new Promise(function(fulfill) {
+          if (request.names.indexOf('contentMimeType') > -1) {
+            chrome.fileManagerPrivate.getMimeType(
+                request.entry.toURL(), fulfill);
+          } else {
+            fulfill(null);
+          }
+        })
+    ]).then(function(results) {
+      var result = {
+        modificationTime: results[0].modificationTime,
+        size: request.entry.isDirectory ? -1 : results[0].size,
+        present: true,
+        availableOffline: true,
       };
+
+      if (results[1] !== null)
+        result['contentMimeType'] = results[1];
+
+      return result;
     });
   }));
 };
