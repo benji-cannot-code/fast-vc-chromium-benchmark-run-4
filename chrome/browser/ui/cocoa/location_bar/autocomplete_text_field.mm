5230fbbe75fe196e1396799ea960986f44ff9bb4
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-02-17 16:41:54 -0800

    [Toolbar UI Mac] Fix location bar animation
    Pinning the location bar currently uses [[locationBar_ animator] frame], but
    this unfortunately relies on inconsistent (and undocumented) behavior of the
    animator proxy to return the proper value after animation. Remove the uses of
    this, and ensure that the location bar stops animating if it's at the proper
    position.
    
    BUG=457162
    
    Review URL: https://codereview.chromium.org/916993004
    
    Cr-Commit-Position: refs/heads/master@{#316713}

--- chrome/browser/ui/cocoa/location_bar/autocomplete_text_field.mm
+++ chrome/browser/ui/cocoa/location_bar/autocomplete_text_field.mm
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/view_id_util.h"
 #include "ui/gfx/scoped_ns_graphics_context_save_gstate_mac.h"
 
+namespace {
+const CGFloat kAnimationDuration = 0.2;
+}
+
 @implementation AutocompleteTextField
 
 @synthesize observer = observer_;
@@ -33,6 +37,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [[self cell] setTruncatesLastVisibleLine:YES];
   [[self cell] setLineBreakMode:NSLineBreakByTruncatingTail];
   currentToolTips_.reset([[NSMutableArray alloc] init]);
+  resizeAnimation_.reset([[NSViewAnimation alloc] init]);
+  [resizeAnimation_ setDuration:kAnimationDuration];
+  [resizeAnimation_ setAnimationBlockingMode:NSAnimationNonblocking];
 }
 
 - (void)flagsChanged:(NSEvent*)theEvent {
@@ -220,6 +227,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (!undoManager_.get())
     undoManager_.reset([[NSUndoManager alloc] init]);
   return undoManager_.get();
+}
+
+- (void)animateToFrame:(NSRect)frame {
+  [self stopAnimation];
+  NSDictionary* animationDictionary = @{
+    NSViewAnimationTargetKey : self,
+    NSViewAnimationStartFrameKey : [NSValue valueWithRect:[self frame]],
+    NSViewAnimationEndFrameKey : [NSValue valueWithRect:frame]
+  };
+  [resizeAnimation_ setViewAnimations:@[ animationDictionary ]];
+  [resizeAnimation_ startAnimation];
+}
+
+- (void)stopAnimation {
+  if ([resizeAnimation_ isAnimating]) {
+    // [NSViewAnimation stopAnimation] results in advancing the animation to
+    // the end. Since this is almost certainly not the behavior we want, reset
+    // the frame to the current frame.
+    NSRect frame = [self frame];
+    [resizeAnimation_ stopAnimation];
+    [self setFrame:frame];
+  }
 }
 
 - (void)clearUndoChain {
