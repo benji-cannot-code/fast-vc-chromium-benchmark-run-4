5abf5db19962ec7642071765c6708097fcf5259e
rtenneti <rtenneti@chromium.org>
2015-02-17 17:52:46 -0800

    Instrumenting QUIC code to find jank.
    BUG=422516
    
    R=rch@chromium.org, vadimt@chromium.org
    
    Review URL: https://codereview.chromium.org/934093002
    
    Cr-Commit-Position: refs/heads/master@{#316734}

--- net/quic/quic_connection_logger.cc
+++ net/quic/quic_connection_logger.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/metrics/histogram.h"
 #include "base/metrics/sparse_histogram.h"
+#include "base/profiler/scoped_tracker.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
 #include "net/base/net_log.h"
@@ -268,6 +269,11 @@ void UpdatePublicResetAddressMismatchHistogram(
 }
 
 const char* GetConnectionDescriptionString() {
+  // TODO(rtenneti): Remove ScopedTracker below once crbug.com/422516 is fixed.
+  tracked_objects::ScopedTracker tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "422516 QuicConnectionLogger GetConnectionDescriptionString"));
+
   NetworkChangeNotifier::ConnectionType type =
       NetworkChangeNotifier::GetConnectionType();
   const char* description = NetworkChangeNotifier::ConnectionTypeToString(type);
--- net/quic/quic_stream_factory.cc
+++ net/quic/quic_stream_factory.cc
@@ -1164,7 +1164,7 @@ int QuicStreamFactory::CreateSession(
   }
 
   // TODO(vadimt): Remove ScopedTracker below once crbug.com/422516 is fixed.
-  tracked_objects::ScopedTracker tracking_profile6(
+  tracked_objects::ScopedTracker tracking_profile61(
       FROM_HERE_WITH_EXPLICIT_FUNCTION(
           "422516 QuicStreamFactory::CreateSession61"));
 
@@ -1173,6 +1173,12 @@ int QuicStreamFactory::CreateSession(
       server_info.Pass(), config,
       base::MessageLoop::current()->message_loop_proxy().get(),
       net_log.net_log());
+
+  // TODO(rtenneti): Remove ScopedTracker below once crbug.com/422516 is fixed.
+  tracked_objects::ScopedTracker tracking_profile62(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "422516 QuicStreamFactory::CreateSession62"));
+
   all_sessions_[*session] = server_id;  // owning pointer
 
   // TODO(vadimt): Remove ScopedTracker below once crbug.com/422516 is fixed.
