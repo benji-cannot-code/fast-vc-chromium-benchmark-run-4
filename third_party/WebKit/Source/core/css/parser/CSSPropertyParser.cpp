60062e6cc0182146c379cd868b794ac819a1ca1d
rob.buis@samsung.com <rob.buis@samsung.com>
2015-02-18 01:10:54 +0000

    Combine two validUnit calls
    It is less and more efficient code to just combine the
    flags and call validUnit once.
    
    BUG=404023
    
    Review URL: https://codereview.chromium.org/920553002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190374 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -1021,11 +1021,8 @@ bool CSSPropertyParser::parseValue(CSSPropertyID propId, bool important)
         return true;
     }
     case CSSPropertyTabSize:
-        // May be specified as a unit-less non-negative integer indicating number of space characters...
-        validPrimitive = validUnit(value, FInteger | FNonNeg);
-        // ... or as a Length.
-        if (!validPrimitive)
-            validPrimitive = validUnit(value, FLength | FNonNeg);
+        // May be specified as a unit-less non-negative integer or length indicating number of space characters.
+        validPrimitive = validUnit(value, FInteger | FLength | FNonNeg);
         break;
     case CSSPropertyBorderRadius:
     case CSSPropertyWebkitBorderRadius:
