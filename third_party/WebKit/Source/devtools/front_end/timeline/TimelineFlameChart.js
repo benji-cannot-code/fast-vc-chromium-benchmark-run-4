e93f617c0ed8c9b00209689756b47b869b7e28b8
alph@chromium.org <alph@chromium.org>
2015-02-18 16:27:36 +0000

    DevTools: Move events iterator to the TimelineModel
    Review URL: https://codereview.chromium.org/916653005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190432 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineFlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineFlameChart.js
@@ -1015,7 +1015,7 @@ WebInspector.TimelineFlameChartBottomUpDataProvider.prototype = {
             return e.name;
         }
 
-        WebInspector.TimelineJSProfileProcessor.eventsStackedIterator(events, onStartEvent.bind(this), onEndEvent);
+        WebInspector.TimelineModel.forEachEvent(events, onStartEvent.bind(this), onEndEvent);
         root.totalTime -= root.selfTime;
         root.selfTime = 0;
         return root;
