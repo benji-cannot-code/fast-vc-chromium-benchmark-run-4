0b8dfa46ba4c08f34dd442fd4f2e1751ea16e49e
edwardjung <edwardjung@chromium.org>
2015-02-17 17:42:16 -0800

    Update Wrench menu time to action histograms with + Bookmark open + Recent tab open timing metrics
    BUG=432561
    
    Review URL: https://codereview.chromium.org/920873002
    
    Cr-Commit-Position: refs/heads/master@{#316727}

--- chrome/browser/ui/views/toolbar/wrench_menu.cc
+++ chrome/browser/ui/views/toolbar/wrench_menu.cc
@@ -1006,7 +1006,7 @@ bool WrenchMenu::IsCommandEnabled(int command_id) const {
 
 void WrenchMenu::ExecuteCommand(int command_id, int mouse_event_flags) {
   if (IsBookmarkCommand(command_id)) {
-    UMA_HISTOGRAM_TIMES("WrenchMenu.TimeToAction.BookmarkOpen",
+    UMA_HISTOGRAM_MEDIUM_TIMES("WrenchMenu.TimeToAction.OpenBookmark",
                         menu_opened_timer_.Elapsed());
     UMA_HISTOGRAM_ENUMERATION("WrenchMenu.MenuAction",
                               MENU_ACTION_BOOKMARK_OPEN, LIMIT_MENU_ACTION);
