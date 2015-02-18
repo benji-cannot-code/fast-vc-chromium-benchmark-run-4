0010ca1218cb4fed918b8263fb100a8712865957
jl@opera.com <jl@opera.com>
2015-02-18 06:15:40 +0000

    Add [TypeChecking=Unrestricted] to Web Audio interfaces
    This changes behavior when non-finite floating point values are assigned
    to attributes or passed as arguments to methods, by throwing TypeError
    exceptions. The old behavior was to throw InvalidStateError exceptions
    in some cases, as required by the current spec, and to not throw anything
    in other cases.
    
    Since value checks are now done by the bindings layer, some checks can
    be removed from or simplified in the Web Audio implementation.
    
    The new behavior is required by the WebIDL spec and matches Firefox. There
    is an (at the time of writing) open Web Audio bug to align it with WebIDL:
    
      https://github.com/WebAudio/web-audio-api/issues/385
    
    BUG=354298
    
    Review URL: https://codereview.chromium.org/927333004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190397 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webaudio/AudioParamTimeline.cpp
+++ third_party/WebKit/Source/modules/webaudio/AudioParamTimeline.cpp
@@ -39,20 +39,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-static bool isValidAudioParamValue(float value, ExceptionState& exceptionState)
-{
-    if (std::isfinite(value))
-        return true;
-
-    exceptionState.throwDOMException(
-        InvalidAccessError,
-        "Target value must be a finite number: " + String::number(value));
-    return false;
-}
-
 static bool isPositiveAudioParamValue(float value, ExceptionState& exceptionState)
 {
-    if (std::isfinite(value) && (value > 0))
+    if (value > 0)
         return true;
 
     exceptionState.throwDOMException(
@@ -61,9 +50,9 @@ static bool isPositiveAudioParamValue(float value, ExceptionState& exceptionStat
     return false;
 }
 
-static bool isValidAudioParamTime(double time, ExceptionState& exceptionState, String message)
+static bool isNonNegativeAudioParamTime(double time, ExceptionState& exceptionState, String message = "Time")
 {
-    if (std::isfinite(time) && (time >= 0))
+    if (time >= 0)
         return true;
 
     exceptionState.throwDOMException(
@@ -74,7 +63,7 @@ static bool isValidAudioParamTime(double time, ExceptionState& exceptionState, S
 
 static bool isPositiveAudioParamTime(double time, ExceptionState& exceptionState, String message)
 {
-    if (std::isfinite(time) && (time > 0))
+    if (time > 0)
         return true;
 
     exceptionState.throwDOMException(
@@ -83,17 +72,11 @@ static bool isPositiveAudioParamTime(double time, ExceptionState& exceptionState
     return false;
 }
 
-static bool isValidAudioParamTime(double time, ExceptionState& exceptionState)
-{
-    return isValidAudioParamTime(time, exceptionState, "Time");
-}
-
 void AudioParamTimeline::setValueAtTime(float value, double time, ExceptionState& exceptionState)
 {
     ASSERT(isMainThread());
 
-    if (!isValidAudioParamValue(value, exceptionState)
-        || !isValidAudioParamTime(time, exceptionState))
+    if (!isNonNegativeAudioParamTime(time, exceptionState))
         return;
 
     insertEvent(ParamEvent(ParamEvent::SetValue, value, time, 0, 0, nullptr));
@@ -103,8 +86,7 @@ void AudioParamTimeline::linearRampToValueAtTime(float value, double time, Excep
 {
     ASSERT(isMainThread());
 
-    if (!isValidAudioParamValue(value, exceptionState)
-        || !isValidAudioParamTime(time, exceptionState))
+    if (!isNonNegativeAudioParamTime(time, exceptionState))
         return;
 
     insertEvent(ParamEvent(ParamEvent::LinearRampToValue, value, time, 0, 0, nullptr));
@@ -115,7 +97,7 @@ void AudioParamTimeline::exponentialRampToValueAtTime(float value, double time, 
     ASSERT(isMainThread());
 
     if (!isPositiveAudioParamValue(value, exceptionState)
-        || !isValidAudioParamTime(time, exceptionState))
+        || !isNonNegativeAudioParamTime(time, exceptionState))
         return;
 
     insertEvent(ParamEvent(ParamEvent::ExponentialRampToValue, value, time, 0, 0, nullptr));
@@ -125,9 +107,8 @@ void AudioParamTimeline::setTargetAtTime(float target, double time, double timeC
 {
     ASSERT(isMainThread());
 
-    if (!isValidAudioParamValue(target, exceptionState)
-        || !isValidAudioParamTime(time, exceptionState)
-        || !isValidAudioParamTime(timeConstant, exceptionState, "Time constant"))
+    if (!isNonNegativeAudioParamTime(time, exceptionState)
+        || !isNonNegativeAudioParamTime(timeConstant, exceptionState, "Time constant"))
         return;
 
     insertEvent(ParamEvent(ParamEvent::SetTarget, target, time, timeConstant, 0, nullptr));
@@ -137,7 +118,7 @@ void AudioParamTimeline::setValueCurveAtTime(DOMFloat32Array* curve, double time
 {
     ASSERT(isMainThread());
 
-    if (!isValidAudioParamTime(time, exceptionState)
+    if (!isNonNegativeAudioParamTime(time, exceptionState)
         || !isPositiveAudioParamTime(duration, exceptionState, "Duration"))
         return;
 
@@ -180,12 +161,6 @@ void AudioParamTimeline::insertEvent(const ParamEvent& event)
 void AudioParamTimeline::cancelScheduledValues(double startTime, ExceptionState& exceptionState)
 {
     ASSERT(isMainThread());
-
-    if (!std::isfinite(startTime)) {
-        exceptionState.throwDOMException(
-            InvalidStateError,
-            "Time must be a finite number: " + String::number(startTime));
-    }
 
     MutexLocker locker(m_eventsLock);
 
--- third_party/WebKit/Source/modules/webaudio/AudioScheduledSourceNode.cpp
+++ third_party/WebKit/Source/modules/webaudio/AudioScheduledSourceNode.cpp
@@ -146,7 +146,7 @@ void AudioScheduledSourceNode::start(double when, ExceptionState& exceptionState
         return;
     }
 
-    if (!std::isfinite(when) || (when < 0)) {
+    if (when < 0) {
         exceptionState.throwDOMException(
             InvalidAccessError,
             "Start time must be a finite non-negative number: " + String::number(when));
@@ -173,10 +173,10 @@ void AudioScheduledSourceNode::stop(double when, ExceptionState& exceptionState)
         return;
     }
 
-    if (!std::isfinite(when) || (when < 0)) {
+    if (when < 0) {
         exceptionState.throwDOMException(
             InvalidAccessError,
-            "Stop time must be a finite non-negative number: " + String::number(when));
+            "Stop time must be a non-negative number: " + String::number(when));
         return;
     }
 
