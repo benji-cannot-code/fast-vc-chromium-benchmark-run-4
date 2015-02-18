8632e7c45f4dfefa7dfc00f462dc3d9552e720ff
horo <horo@chromium.org>
2015-02-17 21:08:15 -0800

    Introduce AppCacheResponseMetadataWriter to support metadata caching.
    We are implementing V8 code caching for ServiceWorker script.
    
    V8 code caching for the normal script resources which are loaded with <script> tag was implemented. (crbug.com/399580)
    The code cache is stored in HttpCache as a metadata of the original script.
    But ServiceWorker's script is stored not in HttpCache but in ServiceWorkerStorage.
    
    To support V8 code caching for ServiceWorker script, we have to implement metadata caching mechanism in ServiceWorkerStorage.
    ServiceWorkerStorage are using AppCache's storage backend.
    
    So this cl introduces metadata caching mechanism in AppCache's storage backend.
    
    To store the metadata we are using "stream 2" of the DiskCache entry.
    This is the same way HttpCache uses in HttpCache::Transaction::WriteMetadata().
    
    TEST=content_unittestes --gtest_filter=AppCacheResponseTest.*
    BUG=449895
    
    Review URL: https://codereview.chromium.org/932643002
    
    Cr-Commit-Position: refs/heads/master@{#316772}

--- content/browser/appcache/appcache_storage_impl.cc
+++ content/browser/appcache/appcache_storage_impl.cc
@@ -1644,6 +1644,13 @@ AppCacheResponseWriter* AppCacheStorageImpl::CreateResponseWriter(
   return new AppCacheResponseWriter(NewResponseId(), group_id, disk_cache());
 }
 
+AppCacheResponseMetadataWriter*
+AppCacheStorageImpl::CreateResponseMetadataWriter(int64 group_id,
+                                                  int64 response_id) {
+  return new AppCacheResponseMetadataWriter(response_id, group_id,
+                                            disk_cache());
+}
+
 void AppCacheStorageImpl::DoomResponses(
     const GURL& manifest_url, const std::vector<int64>& response_ids) {
   if (response_ids.empty())
