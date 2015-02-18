a31593ea719b1b4a8420aa64aef74e2a8470e074
kalman <kalman@chromium.org>
2015-02-18 10:20:42 -0800

    Pull out ExtensionHost::ProcessCreationQueue into a separate interface, ExtensionHostQueue, and its only implementation, SerialExtensionHostQueue. This will allow easily experimenting with alternative queuing mechanisms.
    BUG=453073
    R=rdevlin.cronin@chromium.org
    
    Review URL: https://codereview.chromium.org/931283003
    
    Cr-Commit-Position: refs/heads/master@{#316862}

---
+++ extensions/browser/extension_host_queue.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef EXTENSIONS_BROWSER_EXTENSION_HOST_QUEUE_H_
+#define EXTENSIONS_BROWSER_EXTENSION_HOST_QUEUE_H_
+
+namespace extensions {
+
+class ExtensionHost;
+
+// An interface for a queue of ExtensionHosts waiting for initialization.
+// This is used to implement different throttling strategies.
+class ExtensionHostQueue {
+ public:
+  virtual ~ExtensionHostQueue() {}
+
+  // Adds a host to the queue for RenderView creation.
+  virtual void Add(ExtensionHost* host) = 0;
+
+  // Removes a host from the queue (for example, it may be deleted before
+  // having a chance to start).
+  virtual void Remove(ExtensionHost* host) = 0;
+};
+
+}  // namespace extensions
+
+#endif  // EXTENSIONS_BROWSER_EXTENSION_HOST_QUEUE_H_
