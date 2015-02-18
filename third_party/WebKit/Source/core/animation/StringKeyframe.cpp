0829ccc982601321a09f1ce0c5f55926b077b01d
brucedawson@chromium.org <brucedawson@chromium.org>
2015-02-18 01:30:16 +0000

    Fix incorrect test -- should be comparing against property.
    In change https://codereview.chromium.org/926483002 a test of
    CSSPropertyWebkitColumnRuleWidth was added but the comparison against
    'property' was omitted which made the test tautologically true. This
    corrects that.
    
    This was found by /analyze which said:
    src\third_party\webkit\source\core\animation\stringkeyframe.cpp(191) : warning
    C6236: (<expression> || <non-zero constant>) is always a non-zero constant.
    
    BUG=427616
    
    Review URL: https://codereview.chromium.org/931313002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190375 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/StringKeyframe.cpp
+++ third_party/WebKit/Source/core/animation/StringKeyframe.cpp
@@ -189,7 +189,7 @@ PassRefPtrWillBeRawPtr<Interpolation> StringKeyframe::PropertySpecificKeyframe::
             fallBackToLegacy = true;
 
         // FIXME: Handle keywords e.g. 'thick'
-        if (property == CSSPropertyOutlineWidth || CSSPropertyWebkitColumnRuleWidth)
+        if (property == CSSPropertyOutlineWidth || property == CSSPropertyWebkitColumnRuleWidth)
             fallBackToLegacy = true;
         break;
     case CSSPropertyOrphans:
