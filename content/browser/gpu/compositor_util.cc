42147a58e997e2fe4e3c432a450ae37ac29ece1a
skyostil <skyostil@chromium.org>
2015-02-18 08:33:11 -0800

    Revert of cc: Default threaded GPU rasterization to ON. (patchset #1 id:1 of https://codereview.chromium.org/927463004/)
    Reason for revert:
    Breaks compositor rendering on Nexus 6: crbug.com/459557
    
    Original issue's description:
    > cc: Default threaded GPU rasterization to ON.
    >
    > BUG=454500
    >
    > Committed: https://crrev.com/96a23833e024783905297b60e7d86ca588ada45c
    > Cr-Commit-Position: refs/heads/master@{#316665}
    
    TBR=sievers@chromium.org,vmiura@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=454500
    
    Review URL: https://codereview.chromium.org/936803002
    
    Cr-Commit-Position: refs/heads/master@{#316840}

--- content/browser/gpu/compositor_util.cc
+++ content/browser/gpu/compositor_util.cc
@@ -302,8 +302,10 @@ bool IsThreadedGpuRasterizationEnabled() {
 
   if (command_line.HasSwitch(switches::kDisableThreadedGpuRasterization))
     return false;
+  if (command_line.HasSwitch(switches::kEnableThreadedGpuRasterization))
+    return true;
 
-  return true;
+  return false;
 }
 
 bool UseSurfacesEnabled() {
