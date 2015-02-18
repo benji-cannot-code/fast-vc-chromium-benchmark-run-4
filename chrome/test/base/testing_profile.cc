e5eb83e2f163cd95d74f57ae3cc79ddfe5bab04c
pneubeck <pneubeck@chromium.org>
2015-02-18 09:37:52 -0800

    ProfilePolicyConnectorFactory: Refactoring from Profile to BrowserContext.
    TBR=sky@chromium.org (since for API usages)
    
    Review URL: https://codereview.chromium.org/936513002
    
    Cr-Commit-Position: refs/heads/master@{#316851}

--- chrome/test/base/testing_profile.cc
+++ chrome/test/base/testing_profile.cc
@@ -817,7 +817,7 @@ if (!policy_service_) {
   policy::ProfilePolicyConnectorFactory::GetInstance()->SetServiceForTesting(
       this, profile_policy_connector_.get());
   CHECK_EQ(profile_policy_connector_.get(),
-           policy::ProfilePolicyConnectorFactory::GetForProfile(this));
+           policy::ProfilePolicyConnectorFactory::GetForBrowserContext(this));
 }
 
 PrefService* TestingProfile::GetPrefs() {
