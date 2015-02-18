adb6a977d05c64c18715e57680be497dc4d8329b
dgozman <dgozman@chromium.org>
2015-02-18 08:02:54 -0800

    Disable elastic overscroll effect in DevTools.
    BUG=456546
    
    Review URL: https://codereview.chromium.org/925903002
    
    Cr-Commit-Position: refs/heads/master@{#316834}

--- content/browser/devtools/devtools_frontend_host_impl.h
+++ content/browser/devtools/devtools_frontend_host_impl.h
@@ -22,6 +22,7 @@ class DevToolsFrontendHostImpl : public DevToolsFrontendHost,
   // WebContentsObserver overrides.
   bool OnMessageReceived(const IPC::Message& message,
                          RenderFrameHost* render_frame_host) override;
+  void RenderFrameCreated(RenderFrameHost* render_frame_host) override;
 
   void OnDispatchOnInspectorBackend(const std::string& message);
   void OnDispatchOnEmbedder(const std::string& message);
