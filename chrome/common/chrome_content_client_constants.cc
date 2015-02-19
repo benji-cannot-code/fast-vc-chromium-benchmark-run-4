b242d4d089db146ecab3a63f63878c4be4c2a199
thestig <thestig@chromium.org>
2015-02-18 16:18:54 -0800

    Fix the histograms whitelist for the OOP PDF plugin. (try 2)
    BUG=405305
    
    Review URL: https://codereview.chromium.org/927483002
    
    Cr-Commit-Position: refs/heads/master@{#316928}

--- chrome/common/chrome_content_client_constants.cc
+++ chrome/common/chrome_content_client_constants.cc
@@ -6,11 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_content_client.h"
 
 #if defined(GOOGLE_CHROME_BUILD)
-const char* const ChromeContentClient::kPDFPluginName = "Chrome PDF Viewer";
+const char ChromeContentClient::kPDFPluginName[] = "Chrome PDF Viewer";
 #else
-const char* const ChromeContentClient::kPDFPluginName = "Chromium PDF Viewer";
+const char ChromeContentClient::kPDFPluginName[] = "Chromium PDF Viewer";
 #endif
-const char* const ChromeContentClient::kPDFPluginPath =
-    "internal-pdf-viewer";
-const char* const ChromeContentClient::kRemotingViewerPluginPath =
+const char ChromeContentClient::kPDFPluginPath[] = "internal-pdf-viewer";
+const char ChromeContentClient::kRemotingViewerPluginPath[] =
     "internal-remoting-viewer";
--- chrome/renderer/pepper/pepper_uma_host.cc
+++ chrome/renderer/pepper/pepper_uma_host.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/sha1.h"
 #include "base/strings/string_number_conversions.h"
+#include "chrome/common/chrome_content_client.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/render_messages.h"
 #include "chrome/renderer/chrome_content_renderer_client.h"
@@ -31,11 +32,11 @@ namespace {
 
 const char* const kPredefinedAllowedUMAOrigins[] = {
     "6EAED1924DB611B6EEF2A664BD077BE7EAD33B8F",  // see http://crbug.com/317833
-    "4EB74897CB187C7633357C2FE832E0AD6A44883A"   // see http://crbug.com/317833
+    "4EB74897CB187C7633357C2FE832E0AD6A44883A",  // see http://crbug.com/317833
 };
 
 const char* const kWhitelistedHistogramPrefixes[] = {
-    "22F67DA2061FFC4DC9A4974036348D9C38C22919"  // see http://crbug.com/390221
+    "22F67DA2061FFC4DC9A4974036348D9C38C22919",  // see http://crbug.com/390221
 };
 
 const char* const kWhitelistedPluginBaseNames[] = {
@@ -43,7 +44,7 @@ const char* const kWhitelistedPluginBaseNames[] = {
     kWidevineCdmAdapterFileName,  // see http://crbug.com/368743
                                   // and http://crbug.com/410630
 #endif
-    "libpdf.so"                   // see http://crbug.com/405305
+    ChromeContentClient::kPDFPluginPath,
 };
 
 std::string HashPrefix(const std::string& histogram) {
@@ -107,13 +108,12 @@ bool PepperUMAHost::IsHistogramAllowed(const std::string& histogram) {
   }
 
   if (IsPluginWhitelisted() &&
-      allowed_histogram_prefixes_.find(HashPrefix(histogram)) !=
-          allowed_histogram_prefixes_.end()) {
+      ContainsKey(allowed_histogram_prefixes_, HashPrefix(histogram))) {
     return true;
   }
 
-  if (allowed_plugin_base_names_.find(plugin_base_name_.MaybeAsASCII()) !=
-      allowed_plugin_base_names_.end()) {
+  if (ContainsKey(allowed_plugin_base_names_,
+                  plugin_base_name_.MaybeAsASCII())) {
     return true;
   }
 
