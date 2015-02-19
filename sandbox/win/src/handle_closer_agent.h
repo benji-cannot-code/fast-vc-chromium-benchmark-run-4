32ede2f663070c76de09292a0175490426650669
wfh <wfh@chromium.org>
2015-02-19 01:01:31 -0800

    Replace handles that the handle closer closes with dummy Events.
    This is because on Windows, we close some handles opened by Windows DLLs to prevent them from leaking into sandboxed processes, and this can cause INVALID_HANDLE_EXCEPTION to be raised if "bad handle detection" gflag is enabled, as it currently is by default on Windows 10.
    
    Only replace File and Event handles at the moment, as File handles are the ones causing the issues on Windows 10.
    
    BUG=452613
    TEST=sbox_integration_tests
    TEST=Run Chrome on Windows 10 with crash reporting enabled, and check there are no crashes listed in chrome://crashes
    
    Review URL: https://codereview.chromium.org/919893002
    
    Cr-Commit-Position: refs/heads/master@{#317016}

--- sandbox/win/src/handle_closer_agent.h
+++ sandbox/win/src/handle_closer_agent.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/strings/string16.h"
+#include "base/win/scoped_handle.h"
 #include "sandbox/win/src/handle_closer.h"
 #include "sandbox/win/src/sandbox_types.h"
 
@@ -16,7 +17,7 @@ namespace sandbox {
 // Target process code to close the handle list copied over from the broker.
 class HandleCloserAgent {
  public:
-  HandleCloserAgent() {}
+  HandleCloserAgent();
 
   // Reads the serialized list from the broker and creates the lookup map.
   void InitializeHandlesToClose();
@@ -24,11 +25,16 @@ class HandleCloserAgent {
   // Closes any handles matching those in the lookup map.
   bool CloseHandles();
 
-  // True if we have handles waiting to be closed
+  // True if we have handles waiting to be closed.
   static bool NeedsHandlesClosed();
 
  private:
+  // Attempt to stuff a closed handle with a dummy Event.
+  bool AttemptToStuffHandleSlot(HANDLE closed_handle,
+                                const base::string16& type);
+
   HandleMap handles_to_close_;
+  base::win::ScopedHandle dummy_handle_;
 
   DISALLOW_COPY_AND_ASSIGN(HandleCloserAgent);
 };
