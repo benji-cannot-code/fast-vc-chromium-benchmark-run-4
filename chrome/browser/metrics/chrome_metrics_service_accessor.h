795ab2db04c2d54dcaf07cc8368bf2c8ef29aad6
anthonyvd <anthonyvd@chromium.org>
2015-02-19 08:11:34 -0800

    Fix ChromeMetricsServiceAccessor::IsMetricsReportingEnabled on CrOs
    Since MetricsServicesManager::IsMetricsReportingEnabled is correct, it is
    called directly.
    
    BUG=457430
    
    Review URL: https://codereview.chromium.org/916133003
    
    Cr-Commit-Position: refs/heads/master@{#317058}

--- chrome/browser/metrics/chrome_metrics_service_accessor.h
+++ chrome/browser/metrics/chrome_metrics_service_accessor.h
@@ -67,6 +67,7 @@ class ChromeMetricsServiceAccessor : public metrics::MetricsServiceAccessor {
   friend class options::BrowserOptionsHandler;
   friend void InitiateMetricsReportingChange(
       bool, const OnMetricsReportingCallbackType&);
+  friend class MetricsServicesManager;
 
   FRIEND_TEST_ALL_PREFIXES(ChromeMetricsServiceAccessorTest,
                            MetricsReportingEnabled);
