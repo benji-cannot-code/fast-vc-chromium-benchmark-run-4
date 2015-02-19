da01654d741577790fc8734181b66563cedcca2c
mlamouri <mlamouri@chromium.org>
2015-02-19 08:02:16 -0800

    Fix Presentation API mocks: do not drop eagerly drop clients.
    BUG=412331
    
    Review URL: https://codereview.chromium.org/938033002
    
    Cr-Commit-Position: refs/heads/master@{#317057}

--- content/shell/renderer/test_runner/mock_presentation_client.cc
+++ content/shell/renderer/test_runner/mock_presentation_client.cc
@@ -21,8 +21,8 @@ MockPresentationClient::MockPresentationClient(MockPresentationService* service)
 
 MockPresentationClient::~MockPresentationClient() {
   DCHECK(!controller_);
-  if (service_)
-    service_->UnregisterPresentationClientMock(this);
+  DCHECK(service_);
+  service_->UnregisterPresentationClientMock(this);
 }
 
 void MockPresentationClient::SetScreenAvailability(bool available) {
@@ -39,7 +39,6 @@ void MockPresentationClient::SetScreenAvailability(bool available) {
 
 void MockPresentationClient::Reset() {
   screen_availability_ = false;
-  service_ = nullptr;
 }
 
 void MockPresentationClient::setController(
