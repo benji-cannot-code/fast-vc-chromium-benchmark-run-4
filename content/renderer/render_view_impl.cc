204250b7204ed24f386e5ee81a907c67a2660505
ccameron <ccameron@chromium.org>
2015-02-18 16:40:40 -0800

    Mac: Add more components of double-tap zoom.
    This was reverted due to high crash rates on 10.10.2 (where we don't
    full crash reports). This was tested on branch and didn't affect the
    crash rate. The next step is to forward the event to the renderer.
    
    BUG=452592
    
    Review URL: https://codereview.chromium.org/940733002
    
    Cr-Commit-Position: refs/heads/master@{#316934}

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -1135,6 +1135,10 @@ void RenderView::ApplyWebPreferences(const WebPreferences& prefs,
 #if defined(OS_WIN)
   settings->setShowContextMenuOnMouseUp(true);
 #endif
+
+#if defined(OS_MACOSX)
+  settings->setDoubleTapToZoomEnabled(true);
+#endif
 }
 
 /*static*/
