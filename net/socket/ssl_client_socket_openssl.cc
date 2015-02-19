6672eb9acb05784de93bb6313e8a3d7a35f0d911
davidben <davidben@chromium.org>
2015-02-19 15:06:51 -0800

    Add a dedicated SSL protocol version metric.
    Remove the old SSL version gathering logic in ConnectionType. Those metrics
    are very difficult to read because the denominator is wrong.
    
    In the meantime, claim the ConnectionType metrics since they're unowned.
    They'll be deprecated once this new one has gathered numbers and the
    SPDY use case for them has also been accounted for.
    
    BUG=459710
    
    Review URL: https://codereview.chromium.org/941743002
    
    Cr-Commit-Position: refs/heads/master@{#317161}

--- net/socket/ssl_client_socket_openssl.cc
+++ net/socket/ssl_client_socket_openssl.cc
@@ -1212,8 +1212,6 @@ int SSLClientSocketOpenSSL::DoVerifyCertComplete(int result) {
   }
 
   if (result == OK) {
-    RecordConnectionTypeMetrics(GetNetSSLVersion(ssl_));
-
     if (SSL_session_reused(ssl_)) {
       // Record whether or not the server tried to resume a session for a
       // different version. See https://crbug.com/441456.
