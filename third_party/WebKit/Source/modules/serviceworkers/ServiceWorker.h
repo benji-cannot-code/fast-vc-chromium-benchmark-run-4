787d9b4d8f8bf208ef2a99ba6882bd05e510c41d
falken@chromium.org <falken@chromium.org>
2015-02-19 12:16:33 +0000

    Service Worker: Remove dead "Service Worker proxy ready" code
    This machinery was required when the register promise resolved to a Service
    Worker. It ensured that state change events for the worker were not dispatched
    until the promise resolved to a worker object in the script context.
    
    Now, register() resolves to a ServiceWorkerRegistration, and there is no
    code using CallbackPromiseAdapter<ServiceWorker>, so the take and dispose
    functions are unused.
    
    TEST=http/tests/serviceworker still pass
    
    Review URL: https://codereview.chromium.org/938633003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190455 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/ServiceWorker.h
+++ third_party/WebKit/Source/modules/serviceworkers/ServiceWorker.h
@@ -50,14 +50,10 @@ class ServiceWorker final : public AbstractWorker, public WebServiceWorkerProxy 
     DEFINE_WRAPPERTYPEINFO();
     WILL_BE_USING_PRE_FINALIZER(ServiceWorker, dispose);
 public:
-    // For CallbackPromiseAdapter
     typedef WebServiceWorker WebType;
-    static PassRefPtrWillBeRawPtr<ServiceWorker> take(ScriptPromiseResolver*, WebType* worker);
-
     static PassRefPtrWillBeRawPtr<ServiceWorker> from(ExecutionContext*, WebType*);
 
     ~ServiceWorker() override;
-    static void dispose(WebType*);
 
     void postMessage(ExecutionContext*, PassRefPtr<SerializedScriptValue> message, const MessagePortArray*, ExceptionState&);
     void terminate(ExceptionState&);
@@ -67,7 +63,6 @@ public:
     DEFINE_ATTRIBUTE_EVENT_LISTENER(statechange);
 
     // WebServiceWorkerProxy overrides.
-    virtual bool isReady() override;
     virtual void dispatchStateChangeEvent() override;
 
     // AbstractWorker overrides.
@@ -77,18 +72,8 @@ public:
 private:
     class ThenFunction;
 
-    enum ProxyState {
-        Initial,
-        RegisterPromisePending,
-        Ready,
-        ContextStopped
-    };
-
     static PassRefPtrWillBeRawPtr<ServiceWorker> getOrCreate(ExecutionContext*, WebType*);
     ServiceWorker(ExecutionContext*, PassOwnPtr<WebServiceWorker>);
-    void setProxyState(ProxyState);
-    void onPromiseResolved();
-    void waitOnPromise(ScriptPromiseResolver*);
 
     // ActiveDOMObject overrides.
     virtual bool hasPendingActivity() const override;
@@ -97,7 +82,7 @@ private:
     void dispose();
 
     OwnPtr<WebServiceWorker> m_outerWorker;
-    ProxyState m_proxyState;
+    bool m_wasStopped;
 };
 
 } // namespace blink
