27dd89f62dc191cd3e7b94e6b6d9731507aa96a3
quiche <quiche@chromium.org>
2015-02-19 14:37:21 -0800

    wifi_sync: don't send duplicate ACTION_ADD requests to sync
    Chrome Sync expects the various SyncableServices to properly distinguish
    between ACTION_ADD and ACTION_UPDATE. As such, it is an error to send
    an ACTION_ADD change for an item that already exists.
    
    Avoid causing errors in Chrome Sync, by tracking which items are already
    known to sync, and not submitting new ACTION_ADD changes for them. Support
    for actually updating an existing item will be provided in a later CL.
    
    BUG=chromium:431435
    BUG=chromium:431438
    TEST=components_unittests  --gtest_filter="Wifi*"
    
    Review URL: https://codereview.chromium.org/860103002
    
    Cr-Commit-Position: refs/heads/master@{#317151}

--- components/wifi_sync/wifi_credential_syncable_service.h
+++ components/wifi_sync/wifi_credential_syncable_service.h
@@ -6,17 +6,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_WIFI_SYNC_WIFI_CREDENTIAL_SYNCABLE_SERVICE_H_
 #define COMPONENTS_WIFI_SYNC_WIFI_CREDENTIAL_SYNCABLE_SERVICE_H_
 
+#include <map>
 #include <string>
+#include <utility>
 
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/wifi_sync/wifi_config_delegate.h"
+#include "components/wifi_sync/wifi_credential.h"
+#include "components/wifi_sync/wifi_security_class.h"
 #include "sync/api/sync_change_processor.h"
 #include "sync/api/syncable_service.h"
 
 namespace wifi_sync {
-class WifiCredential;
 
 // KeyedService that synchronizes WiFi credentials between local settings,
 // and Chrome Sync.
@@ -63,6 +66,11 @@ class WifiCredentialSyncableService
                            const WifiCredential& credential);
 
  private:
+  using SsidAndSecurityClass =
+      std::pair<WifiCredential::SsidBytes, WifiSecurityClass>;
+  using SsidAndSecurityClassToPassphrase =
+      std::map<SsidAndSecurityClass, std::string>;
+
   // The syncer::ModelType that this SyncableService processes and
   // generates updates for.
   static const syncer::ModelType kModelType;
@@ -73,6 +81,11 @@ class WifiCredentialSyncableService
   // Our SyncChangeProcessor instance. Used to push changes into
   // Chrome Sync.
   scoped_ptr<syncer::SyncChangeProcessor> sync_processor_;
+
+  // The networks and passphrases that are already known by Chrome
+  // Sync. All synced networks must be included in this map, even if
+  // they do not use passphrases.
+  SsidAndSecurityClassToPassphrase synced_networks_and_passphrases_;
 
   DISALLOW_COPY_AND_ASSIGN(WifiCredentialSyncableService);
 };
