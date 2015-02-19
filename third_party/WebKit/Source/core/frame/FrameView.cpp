b6f5e28a9b8fafd82f93dda3b1a96aed4bb0d644
bokan@chromium.org <bokan@chromium.org>
2015-02-19 17:48:55 +0000

    Recalculate viewport units directly after changing layout size
    We would previously wait until a layout occurs until we tell the document about
    the changed size. This meant that getComputedStyle before a layout occurred would
    return the wrong value. This call is made from setLayoutSize rather than when the
    frame rect changes since the viewport units are calculated using the layout size.
    
    BUG=444952
    
    Review URL: https://codereview.chromium.org/900823002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190488 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -818,8 +818,6 @@ void FrameView::performPreLayoutTasks()
 
     bool wasResized = wasViewportResized();
     Document* document = m_frame->document();
-    if (wasResized)
-        document->notifyResizeForViewportUnits();
 
     // Viewport-dependent media queries may cause us to need completely different style information.
     if (!document->styleResolver() || (wasResized && document->styleResolver()->mediaQueryAffectedByViewportChange())) {
@@ -3062,6 +3060,12 @@ void FrameView::setLayoutSizeInternal(const IntSize& size)
         return;
 
     m_layoutSize = size;
+
+    if (Document* document = m_frame->document()) {
+        if (document->isActive())
+            document->notifyResizeForViewportUnits();
+    }
+
     contentsResized();
 }
 
