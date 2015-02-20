cbbf7aca79f1907daa106068bb79f52511b8a3be
kaliamoorthi <kaliamoorthi@chromium.org>
2015-02-20 03:15:11 -0800

    Add split tunnel interface to vpnProvider
    This CL adds split tunnel capability to chrome.vpnProvider interface.
    Furthermore, additional sanity checks are added to the input from the
    extensions.
    
    BUG=459609
    
    Review URL: https://codereview.chromium.org/932063003
    
    Cr-Commit-Position: refs/heads/master@{#317289}

--- extensions/extensions_tests.gypi
+++ extensions/extensions_tests.gypi
@@ -39,6 +39,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'shell/test/shell_test_launcher_delegate.h',
       'shell/test/shell_tests_main.cc',
     ],
+    'extensions_browsertests_sources_chromeos': [
+      'browser/api/vpn_provider/vpn_provider_apitest.cc',
+    ],
     'extensions_unittests_sources': [
       'browser/api/alarms/alarms_api_unittest.cc',
       'browser/api/api_resource_manager_unittest.cc',
--- extensions/shell/browser/api/vpn_provider/vpn_service_factory.cc
+++ extensions/shell/browser/api/vpn_provider/vpn_service_factory.cc
@@ -6,8 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/api/vpn_provider/vpn_service_factory.h"
 
 #include "base/memory/singleton.h"
+#include "chromeos/dbus/dbus_thread_manager.h"
+#include "chromeos/network/network_handler.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "extensions/browser/api/vpn_provider/vpn_service.h"
+#include "extensions/browser/event_router.h"
+#include "extensions/browser/extension_registry.h"
 
 namespace chromeos {
 
@@ -44,7 +48,13 @@ bool VpnServiceFactory::ServiceIsNULLWhileTesting() const {
 
 KeyedService* VpnServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
-  return nullptr;
+  return new VpnService(
+      context, "testuser", extensions::ExtensionRegistry::Get(context),
+      extensions::EventRouter::Get(context),
+      DBusThreadManager::Get()->GetShillThirdPartyVpnDriverClient(),
+      NetworkHandler::Get()->network_configuration_handler(),
+      NetworkHandler::Get()->network_profile_handler(),
+      NetworkHandler::Get()->network_state_handler());
 }
 
 }  // namespace chromeos
