405993eba5e05c46b05ef8acb2fbe532a32ad1bc
Adam Rice <ricea@chromium.org>
2015-02-20 14:33:25 +0900

    Apply HSTS to WebSocket connections.
    With this change, ws: connections to hosts which have an existing HSTS
    pin will be automatically changed to use wss:, ie. SSL.
    
    In addition, Strict-Transport-Security headers that are sent from a wss:
    server with a valid SSL certificate will be enforced on subsequent ws:
    and http: connections to the same host.
    
    This CL also modifies HttpNetworkTransaction to treat wss: the same as
    https:.
    
    BUG=455215, 446480
    TEST=net_unittests
    R=rsleevi@chromium.org, tyoshino@chromium.org
    
    Review URL: https://codereview.chromium.org/903553005
    
    Cr-Commit-Position: refs/heads/master@{#317252}

--- net/http/http_network_transaction_unittest.cc
+++ net/http/http_network_transaction_unittest.cc
@@ -12555,7 +12555,7 @@ class FakeWebSocketBasicHandshakeStream : public WebSocketHandshakeStreamBase {
     return false;
   }
 
-  void GetSSLInfo(SSLInfo* ssl_info) override { NOTREACHED(); }
+  void GetSSLInfo(SSLInfo* ssl_info) override {}
 
   void GetSSLCertRequestInfo(SSLCertRequestInfo* cert_request_info) override {
     NOTREACHED();
