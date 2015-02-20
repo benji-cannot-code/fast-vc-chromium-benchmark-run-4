1911e54593d66a887a4ac0d7ed8fdc6910f43365
dfalcantara <dfalcantara@chromium.org>
2015-02-20 15:37:09 -0800

    [App banners] Add server-controlled experiment to disable app banners
    + Dependent on cr/86724062
    
    * Control the banner appearances via a kill-switch via an experiment.
    
    * The app install alert flag in chrome://flags is now used to enable
      or disable app banners for Play Store apps.  Web app banners are
      only affected by the experiment.
    
    BUG=457417
    
    Review URL: https://codereview.chromium.org/941823002
    
    Cr-Commit-Position: refs/heads/master@{#317440}

--- chrome/android/javatests/src/org/chromium/chrome/browser/banners/AppBannerManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/banners/AppBannerManagerTest.java
@@ -12,7 +12,9 @@ import android.view.View;
 import android.widget.TextView;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
+import org.chromium.chrome.ChromeSwitches;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.Tab;
 import org.chromium.chrome.browser.infobar.AnimationHelper;
@@ -132,6 +134,7 @@ public class AppBannerManagerTest extends ChromeShellTestBase {
         });
     }
 
+    @CommandLineFlags.Add(ChromeSwitches.ENABLE_APP_INSTALL_ALERTS)
     @SmallTest
     @Feature({"AppBanners"})
     public void testBannerAppears() throws Exception {
@@ -149,6 +152,7 @@ public class AppBannerManagerTest extends ChromeShellTestBase {
         assertTrue(waitUntilAppBannerInfoBarAppears());
     }
 
+    @CommandLineFlags.Add(ChromeSwitches.ENABLE_APP_INSTALL_ALERTS)
     @MediumTest
     @Feature({"AppBanners"})
     public void testBannerAppearsThenDoesNotAppearAgainForMonths() throws Exception {
@@ -199,6 +203,7 @@ public class AppBannerManagerTest extends ChromeShellTestBase {
         assertTrue(waitUntilAppBannerInfoBarAppears());
     }
 
+    @CommandLineFlags.Add(ChromeSwitches.ENABLE_APP_INSTALL_ALERTS)
     @MediumTest
     @Feature({"AppBanners"})
     public void testBlockedBannerDoesNotAppearAgainForMonths() throws Exception {
@@ -256,6 +261,7 @@ public class AppBannerManagerTest extends ChromeShellTestBase {
         assertTrue(waitUntilAppBannerInfoBarAppears());
     }
 
+    @CommandLineFlags.Add(ChromeSwitches.ENABLE_APP_INSTALL_ALERTS)
     @MediumTest
     @Feature({"AppBanners"})
     public void testBitmapFetchersCanOverlapWithoutCrashing() throws Exception {
--- chrome/browser/android/banners/app_banner_manager.cc
+++ chrome/browser/android/banners/app_banner_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
 #include "base/strings/string_util.h"
 #include "chrome/browser/android/banners/app_banner_infobar_delegate.h"
@@ -175,6 +176,9 @@ void AppBannerManager::OnDidGetManifest(const content::Manifest& manifest) {
       || (manifest.name.is_null() && manifest.short_name.is_null())
       || !DoesManifestContainRequiredIcon(manifest)) {
     // No usable manifest, see if there is a play store meta tag.
+    if (!IsEnabledForNativeApps())
+      return;
+
     Send(new ChromeViewMsg_RetrieveMetaTagContent(routing_id(),
                                                   validated_url_,
                                                   kBannerTag));
@@ -384,6 +388,12 @@ bool AppBannerManager::FetchIcon(const GURL& image_url) {
 }
 
 // static
+bool AppBannerManager::IsEnabledForNativeApps() {
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kEnableAppInstallAlerts);
+}
+
+// static
 base::Time AppBannerManager::GetCurrentTime() {
   return base::Time::Now() + gTimeDeltaForTesting;
 }
@@ -394,8 +404,9 @@ jlong Init(JNIEnv* env, jobject obj, jint icon_size) {
 }
 
 jboolean IsEnabled(JNIEnv* env, jclass clazz) {
-  return base::CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableAppInstallAlerts);
+  const std::string group_name =
+      base::FieldTrialList::FindFullName("AppBanners");
+  return group_name == "Enabled";
 }
 
 void SetTimeDeltaForTesting(JNIEnv* env, jclass clazz, jint days) {
