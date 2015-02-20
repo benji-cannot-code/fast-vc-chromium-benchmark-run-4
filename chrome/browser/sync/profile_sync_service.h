47855f448875a543adcbbf23d204ecbed60c56f5
maxbogue <maxbogue@chromium.org>
2015-02-19 20:08:17 -0800

    Add PSS::RemoveClientFromServer() and call it on STOP_SYNC shutdowns.
    When sync is disabled on a device, we need to notify the server that
    the client is no longer active.
    
    BUG=419552
    
    Review URL: https://codereview.chromium.org/864993002
    
    Cr-Commit-Position: refs/heads/master@{#317236}

--- chrome/browser/sync/profile_sync_service.h
+++ chrome/browser/sync/profile_sync_service.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/protocol_event_observer.h"
 #include "chrome/browser/sync/sessions/sessions_sync_manager.h"
 #include "chrome/browser/sync/startup_controller.h"
+#include "chrome/browser/sync/sync_stopped_reporter.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/signin/core/browser/signin_manager_base.h"
 #include "components/sync_driver/data_type_controller.h"
@@ -981,6 +982,9 @@ class ProfileSyncService : public ProfileSyncServiceBase,
   // Clean up prefs and backup DB when rollback is not needed.
   void CleanUpBackup();
 
+  // Tell the sync server that this client has disabled sync.
+  void RemoveClientFromServer() const;
+
   // Factory used to create various dependent objects.
   scoped_ptr<ProfileSyncComponentsFactory> factory_;
 
@@ -1157,6 +1161,8 @@ class ProfileSyncService : public ProfileSyncServiceBase,
 
   // The full path to the sync data directory.
   base::FilePath directory_path_;
+
+  scoped_ptr<browser_sync::SyncStoppedReporter> sync_stopped_reporter_;
 
   base::WeakPtrFactory<ProfileSyncService> weak_factory_;
 
--- chrome/chrome_tests_unit.gypi
+++ chrome/chrome_tests_unit.gypi
@@ -296,6 +296,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/sync/sessions/tab_node_pool_unittest.cc',
       'browser/sync/startup_controller_unittest.cc',
       'browser/sync/sync_startup_tracker_unittest.cc',
+      'browser/sync/sync_stopped_reporter_unittest.cc',
       'browser/sync/test/test_http_bridge_factory.cc',
       'browser/sync/test/test_http_bridge_factory.h',
       'browser/sync/test_profile_sync_service.cc',
