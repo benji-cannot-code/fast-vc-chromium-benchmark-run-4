c9b7cfc5e78ee4b7bd5fabc1a39349c4b64e3454
reillyg <reillyg@chromium.org>
2015-02-20 12:42:29 -0800

    Pad HID output reports on Windows to the maximum output report size.
    The Windows HID library requires output report buffers to be exactly
    the maximum length specified in the OutputReportByteLength field of
    HIDP_CAPS. This patch automatically pads output reports to this length
    with zeros and rejects reports longer than this length.
    
    BUG=439775
    
    Review URL: https://codereview.chromium.org/943783003
    
    Cr-Commit-Position: refs/heads/master@{#317393}

--- extensions/test/data/api_test/hid/api/background.js
+++ extensions/test/data/api_test/hid/api/background.js
@@ -235,9 +235,20 @@ function testSendWithInvalidConnectionId() {
   });
 }
 
+function testSendOversizeReport() {
+  openDeviceWithReportId(function (connection) {
+    var buffer = stringToArrayBuffer("oversize report");
+    chrome.hid.send(connection, 1, buffer, function () {
+      chrome.test.assertLastError("Transfer failed.");
+      chrome.hid.disconnect(connection);
+      chrome.test.succeed("Caught oversize report.");
+    });
+  });
+}
+
 function testSendWithReportId() {
   openDeviceWithReportId(function (connection) {
-    var buffer = stringToArrayBuffer("This is a HID output report.");
+    var buffer = stringToArrayBuffer("o-report");
     chrome.hid.send(connection, 1, buffer, function () {
       chrome.test.assertNoLastError();
       chrome.hid.disconnect(connection);
@@ -248,7 +259,7 @@ function testSendWithReportId() {
 
 function testSendWithoutReportId() {
   openDeviceWithoutReportId(function (connection) {
-    var buffer = stringToArrayBuffer("This is a HID output report.");
+    var buffer = stringToArrayBuffer("o-report");
     chrome.hid.send(connection, 0, buffer, function () {
       chrome.test.assertNoLastError();
       chrome.hid.disconnect(connection);
@@ -259,7 +270,7 @@ function testSendWithoutReportId() {
 
 function testSendWithInvalidReportId() {
   openDeviceWithReportId(function (connection) {
-    var buffer = stringToArrayBuffer("This is a HID output report.");
+    var buffer = stringToArrayBuffer("o-report");
     chrome.hid.send(connection, 0, buffer, function () {
       chrome.test.assertLastError("Transfer failed.");
       chrome.hid.disconnect(connection);
@@ -270,7 +281,7 @@ function testSendWithInvalidReportId() {
 
 function testSendWithUnexpectedReportId() {
   openDeviceWithoutReportId(function (connection) {
-    var buffer = stringToArrayBuffer("This is a HID output report.");
+    var buffer = stringToArrayBuffer("o-report");
     chrome.hid.send(connection, 1, buffer, function () {
       chrome.test.assertLastError("Transfer failed.");
       chrome.hid.disconnect(connection);
@@ -397,6 +408,7 @@ chrome.test.runTests([
   testReceiveWithReportId,
   testReceiveWithoutReportId,
   testSendWithInvalidConnectionId,
+  testSendOversizeReport,
   testSendWithReportId,
   testSendWithoutReportId,
   testSendWithInvalidReportId,
