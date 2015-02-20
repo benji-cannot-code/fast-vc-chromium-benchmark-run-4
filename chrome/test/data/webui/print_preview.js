41e4272c859bc76302074c500845ba849ff6cdb5
alekseys <alekseys@chromium.org>
2015-02-19 16:53:25 -0800

    Remove overzealous assertion from Print Preview JS code. AppState.selectedDestinationAccount can legitimately be undefined.
    BUG=378112
    
    Review URL: https://codereview.chromium.org/939253004
    
    Cr-Commit-Position: refs/heads/master@{#317199}

--- chrome/test/data/webui/print_preview.js
+++ chrome/test/data/webui/print_preview.js
@@ -114,7 +114,7 @@ PrintPreviewWebUITest.prototype = {
    */
   setLocalDestinations: function() {
     var localDestsSetEvent =
-      new Event(print_preview.NativeLayer.EventType.LOCAL_DESTINATIONS_SET);
+        new Event(print_preview.NativeLayer.EventType.LOCAL_DESTINATIONS_SET);
     localDestsSetEvent.destinationInfos = this.localDestinationInfos_;
     this.nativeLayer_.dispatchEvent(localDestsSetEvent);
   },
@@ -126,7 +126,7 @@ PrintPreviewWebUITest.prototype = {
    */
   setCapabilities: function(device) {
     var capsSetEvent =
-      new Event(print_preview.NativeLayer.EventType.CAPABILITIES_SET);
+        new Event(print_preview.NativeLayer.EventType.CAPABILITIES_SET);
     capsSetEvent.settingsInfo = device;
     this.nativeLayer_.dispatchEvent(capsSetEvent);
   },
@@ -338,6 +338,16 @@ function getCddTemplate(printerId) {
     }
   };
 }
+
+TEST_F('PrintPreviewWebUITest', 'TestPrintPreviewRestoreLocalDestination',
+    function() {
+  this.initialSettings_.serializedAppStateStr_ =
+      '{"version":2,"selectedDestinationId":"ID",' +
+      '"selectedDestinationOrigin":"local"}';
+  this.setInitialSettings();
+
+  testDone();
+});
 
 TEST_F('PrintPreviewWebUITest', 'TestSystemDialogLinkIsHiddenInAppKioskMode',
     function() {
