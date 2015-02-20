e8e55ca4972cfef25397a6ac34ed6e3b106331fb
isherman <isherman@chromium.org>
2015-02-20 03:06:23 -0800

    [Smart Lock] Measure clicks on the lock icon during Easy Unlock's trial run.
    BUG=450508
    TEST=none
    R=nkostylev@chromium.org, tengs@chromium.org
    
    Review URL: https://codereview.chromium.org/935303002
    
    Cr-Commit-Position: refs/heads/master@{#317283}

--- chrome/browser/signin/screenlock_bridge.cc
+++ chrome/browser/signin/screenlock_bridge.cc
@@ -61,7 +61,8 @@ ScreenlockBridge* ScreenlockBridge::Get() {
 
 ScreenlockBridge::UserPodCustomIconOptions::UserPodCustomIconOptions()
     : autoshow_tooltip_(false),
-      hardlock_on_click_(false) {
+      hardlock_on_click_(false),
+      is_trial_run_(false) {
 }
 
 ScreenlockBridge::UserPodCustomIconOptions::~UserPodCustomIconOptions() {}
@@ -85,6 +86,9 @@ ScreenlockBridge::UserPodCustomIconOptions::ToDictionaryValue() const {
   if (hardlock_on_click_)
     result->SetBoolean("hardlockOnClick", true);
 
+  if (is_trial_run_)
+    result->SetBoolean("isTrialRun", true);
+
   return result.Pass();
 }
 
@@ -107,6 +111,10 @@ void ScreenlockBridge::UserPodCustomIconOptions::SetAriaLabel(
 
 void ScreenlockBridge::UserPodCustomIconOptions::SetHardlockOnClick() {
   hardlock_on_click_ = true;
+}
+
+void ScreenlockBridge::UserPodCustomIconOptions::SetTrialRun() {
+  is_trial_run_ = true;
 }
 
 // static
--- ui/login/account_picker/user_pod_row.js
+++ ui/login/account_picker/user_pod_row.js
@@ -2428,6 +2428,7 @@ cr.define('login', function() {
      * @param {string} username Username of pod to add button
      * @param {!{id: !string,
      *           hardlockOnClick: boolean,
+     *           isTrialRun: boolean,
      *           ariaLabel: string | undefined,
      *           tooltip: ({text: string, autoshow: boolean} | undefined)}} icon
      *     The icon parameters.
@@ -2446,7 +2447,10 @@ cr.define('login', function() {
       if (icon.id)
         pod.customIconElement.setIcon(icon.id);
 
-      if (icon.hardlockOnClick) {
+      if (icon.isTrialRun) {
+        pod.customIconElement.setInteractive(
+            this.onDidClickLockIconDuringTrialRun_.bind(this, username));
+      } else if (icon.hardlockOnClick) {
         pod.customIconElement.setInteractive(
             this.hardlockUserPod_.bind(this, username));
       } else {
@@ -2476,6 +2480,16 @@ cr.define('login', function() {
      */
     hardlockUserPod_: function(username) {
       chrome.send('hardlockPod', [username]);
+    },
+
+    /**
+     * Records a metric indicating that the user clicked on the lock icon during
+     * the trial run for Easy Unlock.
+     * @param {!string} username The user's username.
+     * @private
+     */
+    onDidClickLockIconDuringTrialRun_: function(username) {
+      chrome.send('recordClickOnLockIcon', [username]);
     },
 
     /**
