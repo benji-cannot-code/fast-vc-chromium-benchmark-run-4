d87e4cbd9d5ae49f90a7b8485e91a388218fef29
yhirano@chromium.org <yhirano@chromium.org>
2015-02-20 04:46:34 +0000

    ScriptPromiseResolver's assertion fails wrongly when destroyed in stop().
    ScriptPromiseResolver has an assertion in its destructor, in order not to allow
    freeing it without resolving or rejecting. There is one exception: when the
    associated ExecutionContext is stopped, it is OK to not calling resolve /
    reject, because the resolver won't work in such a situation.
    Imagine an ActiveDOMObject x has a ScriptPromiseResolver r and x drops its
    reference to r when it is stopped without calling resolve / reject. If x's stop()
    is called before r's stop(), the assertion fails wrongly.
    
    BUG=456047
    
    Review URL: https://codereview.chromium.org/907803002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190528 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/ScriptPromiseResolver.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ScriptPromiseResolver.cpp
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptPromiseResolver.h"
 
 #include "bindings/core/v8/V8RecursionScope.h"
+#include "platform/LifecycleObserver.h"
 
 namespace blink {
 
@@ -25,6 +26,17 @@ ScriptPromiseResolver::ScriptPromiseResolver(ScriptState* scriptState)
         m_state = ResolvedOrRejected;
         m_resolver.clear();
     }
+#if ENABLE(OILPAN) && ENABLE(ASSERT)
+    // A prefinalizer has already been registered (as a LifecycleObserver);
+    // remove it and register a combined one, as the infrastructure doesn't
+    // support multiple prefinalizers for an object.
+    //
+    // FIXME: Oilpan: remove LifecycleObserver's need for a prefinalizer,
+    // and as a consequence, this unregistration step. If the former is
+    // independently removed, the unregisterPreFinalizer() call will assert.
+    ThreadState::current()->unregisterPreFinalizer(*static_cast<LifecycleObserver*>(this));
+    ThreadState::current()->registerPreFinalizer(*this);
+#endif
 }
 
 void ScriptPromiseResolver::suspend()
