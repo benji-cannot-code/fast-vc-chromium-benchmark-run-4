489600211934d775c46b6f743109553c7ac0e1f1
bengr <bengr@chromium.org>
2015-02-19 21:23:27 -0800

    Add flag to request Data Saver experiment
    Adds a command line flag, --data-saver-experiment, that takes
    a comma-delimited list of experiments to run. These experiments
    are conveyed to the Data Saver proxy via a new Chrome-Proxy
    request header directive, "exp".
    
    BUG=452295
    
    Review URL: https://codereview.chromium.org/934973003
    
    Cr-Commit-Position: refs/heads/master@{#317249}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_request_options.h
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_request_options.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_DATA_REDUCTION_PROXY_CORE_BROWSER_DATA_REDUCTION_PROXY_AUTH_REQUEST_HANDLER_H_
 #define COMPONENTS_DATA_REDUCTION_PROXY_CORE_BROWSER_DATA_REDUCTION_PROXY_AUTH_REQUEST_HANDLER_H_
 
-#include <map>
 #include <string>
+#include <vector>
 
 #include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
@@ -34,6 +34,7 @@ extern const char kBuildNumberHeaderOption[];
 extern const char kPatchNumberHeaderOption[];
 extern const char kClientHeaderOption[];
 extern const char kLoFiHeaderOption[];
+extern const char kExperimentsOption[];
 
 #if defined(OS_ANDROID)
 extern const char kAndroidWebViewProtocolVersion[];
@@ -75,8 +76,8 @@ class DataReductionProxyRequestOptions {
   virtual ~DataReductionProxyRequestOptions();
 
   // Sets |key_| to the default key and initializes the credentials, version,
-  // client, and lo-fi header values in |header_options_|. Generates the
-  // |header_value_| string which is concatenated to the Chrome-proxy header.
+  // client, and lo-fi header values. Generates the |header_value_| string,
+  // which is concatenated to the Chrome-proxy header.
   void Init();
 
   // Adds a 'Chrome-Proxy' header to |request_headers| with the data reduction
@@ -128,17 +129,7 @@ class DataReductionProxyRequestOptions {
 
  private:
   FRIEND_TEST_ALL_PREFIXES(DataReductionProxyRequestOptionsTest,
-                           AuthorizationOnIOThread);
-  FRIEND_TEST_ALL_PREFIXES(DataReductionProxyRequestOptionsTest,
-                           AuthorizationIgnoresEmptyKey);
-  FRIEND_TEST_ALL_PREFIXES(DataReductionProxyRequestOptionsTest,
-                           AuthorizationBogusVersion);
-  FRIEND_TEST_ALL_PREFIXES(DataReductionProxyRequestOptionsTest,
                            AuthHashForSalt);
-  FRIEND_TEST_ALL_PREFIXES(DataReductionProxyRequestOptionsTest,
-                           AuthorizationLoFi);
-  FRIEND_TEST_ALL_PREFIXES(DataReductionProxyRequestOptionsTest,
-                           AuthorizationLoFiOffThenOn);
 
   // Returns the version of Chromium that is being used.
   std::string ChromiumVersion() const;
@@ -149,12 +140,15 @@ class DataReductionProxyRequestOptions {
                                 std::string* build,
                                 std::string* patch) const;
 
-  // Uses |version_| and |client_| to update values in |header_options_|.
+  // Updates client type, build, and patch.
   void UpdateVersion();
 
-  // Updates the value of LoFi in |header_options_| and regenerates the header
-  // if necessary.
+  // Updates the value of LoFi and regenerates the header if necessary.
   void UpdateLoFi();
+
+  // Update the value of the experiments to be run and regenerate the header if
+  // necessary.
+  void UpdateExperiments();
 
   // Generates a session ID and credentials suitable for authenticating with
   // the data reduction proxy.
@@ -162,7 +156,7 @@ class DataReductionProxyRequestOptions {
                           std::string* session,
                           std::string* credentials);
 
-  // Generates and updates the session ID and credentials in |header_options_|.
+  // Generates and updates the session ID and credentials.
   void UpdateCredentials();
 
   // Adds authentication headers only if |expects_ssl| is true and
@@ -177,8 +171,7 @@ class DataReductionProxyRequestOptions {
   // Chrome-proxy header.
   void RegenerateRequestHeaderValue();
 
-  // Map and string of the request options to be added to the header.
-  std::map<std::string, std::string> header_options_;
+  // The Chrome-Proxy header value.
   std::string header_value_;
 
   // Authentication state.
@@ -187,6 +180,12 @@ class DataReductionProxyRequestOptions {
   // Name of the client and version of the data reduction proxy protocol to use.
   std::string client_;
   std::string version_;
+  std::string session_;
+  std::string credentials_;
+  std::string build_;
+  std::string patch_;
+  std::string lofi_;
+  std::vector<std::string> experiments_;
 
   // The last time the session was updated. Used to ensure that a session is
   // never used for more than twenty-four hours.
--- components/data_reduction_proxy/core/common/data_reduction_proxy_switches.h
+++ components/data_reduction_proxy/core/common/data_reduction_proxy_switches.h
@@ -16,6 +16,7 @@ extern const char kDataReductionProxy[];
 extern const char kDataReductionProxyAlt[];
 extern const char kDataReductionProxyAltFallback[];
 extern const char kDataReductionProxyDev[];
+extern const char kDataReductionProxyExperiment[];
 extern const char kDataReductionProxyFallback[];
 extern const char kDataReductionProxyKey[];
 extern const char kDataReductionProxyProbeURL[];
@@ -27,6 +28,7 @@ extern const char kEnableDataReductionProxy[];
 extern const char kEnableDataReductionProxyAlt[];
 extern const char kEnableDataReductionProxyLoFi[];
 extern const char kEnableDataReductionProxyBypassWarning[];
+
 
 }  // namespace switches
 }  // namespace data_reduction_proxy
