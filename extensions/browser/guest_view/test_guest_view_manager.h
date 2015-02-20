b2d466cbcd15032f3afc9c5c0810ca4f2d21e10d
fsamuel <fsamuel@chromium.org>
2015-02-19 17:31:46 -0800

    Allow Signin page to open other chrome:// URLs if login content in <webview>
    BUG=450125
    
    Review URL: https://codereview.chromium.org/890183002
    
    Cr-Commit-Position: refs/heads/master@{#317209}

--- extensions/browser/guest_view/test_guest_view_manager.h
+++ extensions/browser/guest_view/test_guest_view_manager.h
@@ -22,6 +22,8 @@ class TestGuestViewManager : public GuestViewManager {
   void WaitForAllGuestsDeleted();
   content::WebContents* WaitForSingleGuestCreated();
 
+  content::WebContents* GetLastGuestCreated();
+
  private:
   // GuestViewManager override:
   void AddGuest(int guest_instance_id,
@@ -29,7 +31,6 @@ class TestGuestViewManager : public GuestViewManager {
   void RemoveGuest(int guest_instance_id) override;
 
   int GetNumGuests() const;
-  content::WebContents* GetLastGuestCreated();
   void WaitForGuestCreated();
 
   std::vector<linked_ptr<content::WebContentsDestroyedWatcher>>
