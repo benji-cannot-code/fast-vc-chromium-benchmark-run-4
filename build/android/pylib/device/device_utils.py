af5be3e12d8695b90362439d4f67644afc695147
jbudorick <jbudorick@chromium.org>
2015-02-20 07:49:06 -0800

    [Android] Raise NoDevicesError in DeviceUtils.parallel.
    BUG=
    
    Review URL: https://codereview.chromium.org/944673002
    
    Cr-Commit-Position: refs/heads/master@{#317326}

--- build/android/pylib/device/device_utils.py
+++ build/android/pylib/device/device_utils.py
@@ -1429,6 +1429,8 @@ class DeviceUtils(object):
     """
     if not devices:
       devices = adb_wrapper.AdbWrapper.GetDevices()
+      if not devices:
+        raise device_errors.NoDevicesError()
     devices = [d if isinstance(d, cls) else cls(d) for d in devices]
     if async:
       return parallelizer.Parallelizer(devices)
