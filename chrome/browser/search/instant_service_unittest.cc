2a7eb3c233772d551f35c292d0d1e59faee9a8f6
mathp <mathp@chromium.org>
2015-02-19 16:10:05 -0800

    [Instant] Default Search Provider change redirects to local NTP in some cases
    Previously, all instant renderers would reload if a search provider changed. With this patch, some of the renderers are redirected to the local NTP in the case that both the old and new search providers are google.<tld>.
    
    BUG=456681
    TEST=BrowserInstantControllerTest*, InstantService* unit_tests
    
    Review URL: https://codereview.chromium.org/930853005
    
    Cr-Commit-Position: refs/heads/master@{#317180}

--- chrome/browser/search/instant_service_unittest.cc
+++ chrome/browser/search/instant_service_unittest.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class MockInstantServiceObserver : public InstantServiceObserver {
  public:
-  MOCK_METHOD0(DefaultSearchProviderChanged, void());
+  MOCK_METHOD1(DefaultSearchProviderChanged, void(bool));
   MOCK_METHOD1(OmniboxStartMarginChanged, void(int));
 };
 
@@ -68,29 +68,29 @@ class InstantServiceEnabledTest : public InstantServiceTest {
 };
 
 TEST_F(InstantServiceEnabledTest, DispatchDefaultSearchProviderChanged) {
-  EXPECT_CALL(*instant_service_observer_.get(), DefaultSearchProviderChanged())
-      .Times(1);
+  EXPECT_CALL(*instant_service_observer_.get(),
+              DefaultSearchProviderChanged(false)).Times(1);
 
   const std::string new_base_url = "https://bar.com/";
   SetUserSelectedDefaultSearchProvider(new_base_url);
 }
 
 TEST_F(InstantServiceTest, DontDispatchGoogleURLUpdatedForNonGoogleURLs) {
-  EXPECT_CALL(*instant_service_observer_.get(), DefaultSearchProviderChanged())
-      .Times(1);
+  EXPECT_CALL(*instant_service_observer_.get(),
+              DefaultSearchProviderChanged(false)).Times(1);
   const std::string new_dsp_url = "https://bar.com/";
   SetUserSelectedDefaultSearchProvider(new_dsp_url);
   testing::Mock::VerifyAndClearExpectations(instant_service_observer_.get());
 
-  EXPECT_CALL(*instant_service_observer_.get(), DefaultSearchProviderChanged())
-      .Times(0);
+  EXPECT_CALL(*instant_service_observer_.get(),
+              DefaultSearchProviderChanged(false)).Times(0);
   const std::string new_base_url = "https://www.google.es/";
   NotifyGoogleBaseURLUpdate(new_base_url);
 }
 
 TEST_F(InstantServiceTest, DispatchGoogleURLUpdated) {
-  EXPECT_CALL(*instant_service_observer_.get(), DefaultSearchProviderChanged())
-      .Times(1);
+  EXPECT_CALL(*instant_service_observer_.get(),
+              DefaultSearchProviderChanged(true)).Times(1);
 
   const std::string new_base_url = "https://www.google.es/";
   NotifyGoogleBaseURLUpdate(new_base_url);
@@ -124,8 +124,8 @@ TEST_F(InstantServiceTest, InstantSearchEnabled) {
 
 TEST_F(InstantServiceEnabledTest,
        ResetInstantSearchPrerenderer_DefaultProviderChanged) {
-  EXPECT_CALL(*instant_service_observer_.get(), DefaultSearchProviderChanged())
-      .Times(2);
+  EXPECT_CALL(*instant_service_observer_.get(),
+              DefaultSearchProviderChanged(false)).Times(2);
 
   // Set a default search provider that doesn't support Instant.
   TemplateURLData data;
@@ -147,8 +147,8 @@ TEST_F(InstantServiceEnabledTest,
 
 TEST_F(InstantServiceEnabledTest,
        ResetInstantSearchPrerenderer_GoogleBaseURLUpdated) {
-  EXPECT_CALL(*instant_service_observer_.get(), DefaultSearchProviderChanged())
-      .Times(1);
+  EXPECT_CALL(*instant_service_observer_.get(),
+              DefaultSearchProviderChanged(true)).Times(1);
 
   InstantSearchPrerenderer* old_prerenderer = GetInstantSearchPrerenderer();
   EXPECT_TRUE(old_prerenderer != NULL);
