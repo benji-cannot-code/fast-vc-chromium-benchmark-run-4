858b204c08f277283da8981c0454869a82b58773
gunsch <gunsch@chromium.org>
2015-02-19 19:06:45 -0800

    Chromecast: tighten cases for uploading crash dumps to prod server.
    After discussion with gogan@ about Cast on ATV crashes, one thing we
    discovered was the amount of non-public/non-dogfood crashes polluting
    the crash data.
    
    R=kjoswiak@chromium.org,byungchul@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/820883003
    
    Cr-Commit-Position: refs/heads/master@{#317225}

--- chromecast/crash/android/crash_handler.cc
+++ chromecast/crash/android/crash_handler.cc
@@ -6,12 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/crash/android/crash_handler.h"
 
 #include <jni.h>
+#include <stdlib.h>
 #include <string>
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
+#include "base/strings/string_number_conversions.h"
 #include "breakpad/src/client/linux/handler/exception_handler.h"
 #include "breakpad/src/client/linux/handler/minidump_descriptor.h"
 #include "chromecast/common/version.h"
@@ -39,6 +41,19 @@ bool HandleCrash(const void* /* crash_context */,
 
   // Let the exception continue to propagate up to the system.
   return false;
+}
+
+// Debug builds: always to crash-staging
+// Release builds: only to crash-staging for local/invalid build numbers
+bool UploadCrashToStaging() {
+#if CAST_IS_DEBUG_BUILD
+  return true;
+#else
+  int build_number;
+  if (base::StringToInt(CAST_BUILD_REVISION, &build_number))
+    return build_number == 0;
+  return true;
+#endif
 }
 
 }  // namespace
@@ -120,7 +135,7 @@ void CrashHandler::AttemptUploadCrashDump() {
       env,
       crash_dump_path_java.obj(),
       log_file_path_java.obj(),
-      CAST_IS_DEBUG_BUILD);
+      UploadCrashToStaging());
 }
 
 void CrashHandler::UploadCrashDumpsAsync() {
@@ -130,7 +145,7 @@ void CrashHandler::UploadCrashDumpsAsync() {
                                              crash_dump_path_.value());
   Java_CastCrashHandler_uploadCrashDumpsAsync(env,
                                               crash_dump_path_java.obj(),
-                                              CAST_IS_DEBUG_BUILD);
+                                              UploadCrashToStaging());
 }
 
 void CrashHandler::RemoveCrashDumps() {
@@ -140,7 +155,7 @@ void CrashHandler::RemoveCrashDumps() {
       base::android::ConvertUTF8ToJavaString(env,
                                              crash_dump_path_.value());
   Java_CastCrashHandler_removeCrashDumpsSync(
-      env, crash_dump_path_java.obj(), CAST_IS_DEBUG_BUILD);
+      env, crash_dump_path_java.obj(), UploadCrashToStaging());
 }
 
 }  // namespace chromecast
