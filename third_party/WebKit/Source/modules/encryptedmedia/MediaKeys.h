9f08b6dc6251cb36f49797aa97505f24b7e3f2a2
sandersd@chromium.org <sandersd@chromium.org>
2015-02-21 01:25:07 +0000

    Filter supported session types in createSession().
    BUG=384152
    
    Review URL: https://codereview.chromium.org/946503002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190606 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/encryptedmedia/MediaKeys.h
+++ third_party/WebKit/Source/modules/encryptedmedia/MediaKeys.h
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/ContextLifecycleObserver.h"
 #include "core/dom/DOMArrayPiece.h"
 #include "platform/Timer.h"
+#include "public/platform/WebString.h"
+#include "public/platform/WebVector.h"
 #include "wtf/Forward.h"
 #include "wtf/text/WTFString.h"
 
@@ -49,7 +51,7 @@ class MediaKeys : public GarbageCollectedFinalized<MediaKeys>, public ContextLif
     WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(MediaKeys);
     DEFINE_WRAPPERTYPEINFO();
 public:
-    MediaKeys(ExecutionContext*, const String& keySystem, PassOwnPtr<WebContentDecryptionModule>);
+    MediaKeys(ExecutionContext*, const String& keySystem, const blink::WebVector<blink::WebString>& supportedSessionTypes, PassOwnPtr<WebContentDecryptionModule>);
     virtual ~MediaKeys();
 
     // FIXME: This should be removed after crbug.com/425186 is fully
@@ -73,6 +75,7 @@ private:
     void timerFired(Timer<MediaKeys>*);
 
     const String m_keySystem;
+    const blink::WebVector<blink::WebString> m_supportedSessionTypes;
     OwnPtr<blink::WebContentDecryptionModule> m_cdm;
 
     HeapDeque<Member<PendingAction>> m_pendingActions;
