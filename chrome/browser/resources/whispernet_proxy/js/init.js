8802474e1c723e7c85e5a2254355918f89e21f70
ckehoe <ckehoe@chromium.org>
2015-02-20 17:14:40 -0800

    Adding CRC and multi-client support in the Whispernet NaCl wrapper. NaCl changes in cl/86735537.
    BUG=455823
    
    Review URL: https://codereview.chromium.org/943053002
    
    Cr-Commit-Position: refs/heads/master@{#317472}

--- chrome/browser/resources/whispernet_proxy/js/init.js
+++ chrome/browser/resources/whispernet_proxy/js/init.js
@@ -24,7 +24,7 @@ function audioConfig(clientId, audioParams) {
     return;
   }
 
-  console.log('Configuring encoder and decoder!');
+  console.log('Configuring encoder and decoder for client ' + clientId);
   whisperEncoders[clientId] =
       new WhisperEncoder(audioParams.paramData, whispernetNacl, clientId);
   whisperDecoders[clientId] =
@@ -40,7 +40,8 @@ function encodeTokenRequest(clientId, params) {
   if (whisperEncoders[clientId]) {
     whisperEncoders[clientId].encode(params);
   } else {
-    console.error('encodeTokenRequest: Whisper not initialized!');
+    console.error('encodeTokenRequest: Whisper not initialized for client ' +
+        clientId);
   }
 }
 
@@ -53,7 +54,8 @@ function decodeSamplesRequest(clientId, params) {
   if (whisperDecoders[clientId]) {
     whisperDecoders[clientId].processSamples(params);
   } else {
-    console.error('decodeSamplesRequest: Whisper not initialized!');
+    console.error('decodeSamplesRequest: Whisper not initialized for client ' +
+        clientId);
   }
 }
 
