cac4501a693e8b10824b9a54a340bd12041aaa6f
tbarzic <tbarzic@chromium.org>
2015-02-20 16:50:14 -0800

    Add some metrics for printing with extension
    For now just recording user action for printing with extension and
    page count for issued print requests.
    
    Also, add missing histogram for page count when printing with privet.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/938193003
    
    Cr-Commit-Position: refs/heads/master@{#317460}

--- chrome/browser/ui/webui/print_preview/print_preview_handler.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_handler.cc
@@ -108,6 +108,7 @@ enum UserActionBuckets {
   INITIATOR_CLOSED,
   PRINT_WITH_CLOUD_PRINT,
   PRINT_WITH_PRIVET,
+  PRINT_WITH_EXTENSION,
   USERACTION_BUCKET_BOUNDARY
 };
 
@@ -885,7 +886,9 @@ void PrintPreviewHandler::HandlePrint(const base::ListValue* args) {
 #endif
 
   if (print_with_extension) {
-    // TODO(tbarzic): Record UMA stats.
+    UMA_HISTOGRAM_COUNTS("PrintPreview.PageCount.PrintWithExtension",
+                         page_count);
+    ReportUserActionHistogram(PRINT_WITH_EXTENSION);
 
     std::string destination_id;
     std::string print_ticket;
