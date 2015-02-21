eaa1b0c0af56b598f9333834127e4033f4c2c52d
rickyz <rickyz@chromium.org>
2015-02-20 17:52:59 -0800

    Add UMA stats for Linux sandboxing features.
    BUG=
    
    Review URL: https://codereview.chromium.org/938243002
    
    Cr-Commit-Position: refs/heads/master@{#317484}

--- content/browser/zygote_host/zygote_host_impl_linux.cc
+++ content/browser/zygote_host/zygote_host_impl_linux.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/scoped_vector.h"
 #include "base/metrics/histogram.h"
+#include "base/metrics/sparse_histogram.h"
 #include "base/path_service.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/posix/unix_domain_socket_linux.h"
@@ -302,7 +303,9 @@ ssize_t ZygoteHostImpl::ReadReply(void* buf, size_t buf_len) {
         sizeof(sandbox_status_)) {
       return -1;
     }
+
     have_read_sandbox_status_word_ = true;
+    UMA_HISTOGRAM_SPARSE_SLOWLY("Linux.SandboxStatus", sandbox_status_);
   }
 
   return HANDLE_EINTR(read(control_fd_, buf, buf_len));
