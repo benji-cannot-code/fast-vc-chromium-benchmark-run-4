fc4ea5eddd3654f6735bf2469020bee0f04bc743
miletus@chromium.org <miletus@chromium.org>
2015-02-21 00:32:57 +0000

    Reland: Don't setScrollCompensationAdjustment if there is no position-fixed layer
    Original CL was reverted here
    https://codereview.chromium.org/943963003/
    
    setScrollCompensationAdjustment() can cause commit to happen. If there
    is no position-fixed layer, there is no need to set it.
    
    BUG=456162
    
    Review URL: https://codereview.chromium.org/921333005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190601 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/scroll/ScrollableArea.cpp
+++ third_party/WebKit/Source/platform/scroll/ScrollableArea.cpp
@@ -81,6 +81,7 @@ ScrollableArea::ScrollableArea()
     , m_horizontalScrollElasticity(ScrollElasticityNone)
     , m_scrollbarOverlayStyle(ScrollbarOverlayStyleDefault)
     , m_scrollOriginChanged(false)
+    , m_hasViewportConstrainedObjectsForScrollCompensation(false)
 {
 }
 
