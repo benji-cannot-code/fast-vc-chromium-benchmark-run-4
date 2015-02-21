6d5ed49e37528b85372b32c5144394effadb32c8
wez <wez@chromium.org>
2015-02-20 19:51:54 -0800

    Add a setDebugDirtyRegion() feature to the client.
    Calling remoting.clientSession.setDebugDirtyRegion(true) enables
    rendering of each frame's dirty region with an purple, translucent
    overlay.
    
    Currently the dirty region is re-rendered immediately for each frame,
    with no linger nor fade-out behaviour.
    
    BUG=427659
    
    Review URL: https://codereview.chromium.org/932013002
    
    Cr-Commit-Position: refs/heads/master@{#317496}

--- remoting/client/plugin/pepper_video_renderer_3d.h
+++ remoting/client/plugin/pepper_video_renderer_3d.h
@@ -39,6 +39,7 @@ class PepperVideoRenderer3D : public PepperVideoRenderer,
                   const ClientContext& context,
                   EventHandler* event_handler) override;
   void OnViewChanged(const pp::View& view) override;
+  void EnableDebugDirtyRegion(bool enable) override;
 
   // VideoRenderer interface.
   void OnSessionConfig(const protocol::SessionConfig& config) override;
@@ -149,6 +150,9 @@ class PepperVideoRenderer3D : public PepperVideoRenderer,
 
   // Location of the scale value to be passed to the |shader_program_|.
   int shader_texcoord_scale_location_;
+
+  // True if dirty regions are to be sent to |event_handler_| for debugging.
+  bool debug_dirty_region_;
 
   pp::CompletionCallbackFactory<PepperVideoRenderer3D> callback_factory_;
 
