208b6237874e79741ea4d967be9584787a00b639
jam <jam@chromium.org>
2015-02-23 14:19:51 -0800

    Revert of Move base_dir to user_story_set, remove file_path. (patchset #4 id:60001 of https://codereview.chromium.org/946473002/)
    Reason for revert:
    breaks on main waterfall, i.e.
    http://build.chromium.org/p/chromium.win/builders/Win%207%20Tests%20x64%20%281%29/builds/21577/steps/telemetry_unittests/logs/stdio
    
      AssertionError: 'E:\\b\\build\\slave\\Win_7_Tests_x64__1_\\build\\src\\tools\\telemetry\\telemetry\\user_story\\user_story_set_unittest.pyc' != 'E:\\b\\build\\slave\\Win_7_Tests_x64__1_\\build\\src\\tools\\telemetry\\telemetry\\user_story\\user_story_set_unittest.py'
    
    not sure why this fails on main waterfall but not trybots?
    
    Original issue's description:
    > Move base_dir to user_story_set, remove file_path.
    >
    > BUG=454531
    >
    > Committed: https://crrev.com/65ff7d22f8986ab2832045af09160dc753e40c5f
    > Cr-Commit-Position: refs/heads/master@{#317632}
    
    TBR=chrishenry@google.com,dtu@chromium.org,sullivan@chromium.org,nednguyen@google.com,aiolos@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=454531
    
    Review URL: https://codereview.chromium.org/951593003
    
    Cr-Commit-Position: refs/heads/master@{#317665}

--- tools/telemetry/telemetry/page/page_unittest.py
+++ tools/telemetry/telemetry/page/page_unittest.py
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import inspect
 import os
 import unittest
 
@@ -54,11 +53,9 @@ class TestPage(unittest.TestCase):
                       pages)
 
   def testGetUrlBaseDirAndFileForUrlBaseDir(self):
-    base_dir = os.path.dirname(__file__)
-    file_path = os.path.dirname(base_dir) + '/otherdir/file.html'
-    ps = page_set.PageSet(file_path=base_dir, serving_dirs=['../somedir/'])
+    ps = page_set.PageSet(file_path='basedir/', serving_dirs=['../somedir/'])
     ps.AddUserStory(page.Page('file://../otherdir/file.html', ps, ps.base_dir))
-    self.assertPathEqual(ps[0].file_path, file_path)
+    self.assertPathEqual(ps[0].file_path, 'otherdir/file.html')
 
   def testDisplayUrlForHttp(self):
     ps = page_set.PageSet(file_path=os.path.dirname(__file__))
