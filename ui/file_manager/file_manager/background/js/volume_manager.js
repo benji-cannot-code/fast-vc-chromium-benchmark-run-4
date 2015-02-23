6ad0009491fb530c041cef2f209fd21838ae93c1
yawano <yawano@chromium.org>
2015-02-22 19:38:43 -0800

    Update LocationLine without IO.
    This enables to update location line even when IO is busy for other operation,
    e.g. directory rescan.
    
    BUG=460405
    TEST=manually tested. Navigate to a directory and see how the UI changes.
    
    Review URL: https://codereview.chromium.org/946543002
    
    Cr-Commit-Position: refs/heads/master@{#317539}

--- ui/file_manager/file_manager/background/js/volume_manager.js
+++ ui/file_manager/file_manager/background/js/volume_manager.js
@@ -46,7 +46,6 @@ function VolumeInfo(
   this.label_ = label;
   this.displayRoot_ = null;
   this.fakeEntries_ = {};
-  this.displayRoot_ = null;
 
   /** @type {Promise.<DirectoryEntry>} */
   this.displayRootPromise_ = null;
