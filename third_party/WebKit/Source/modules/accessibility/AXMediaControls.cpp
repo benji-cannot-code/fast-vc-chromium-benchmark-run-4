0013650bed21351722b424c81c4191b371a23ca7
philipj@opera.com <philipj@opera.com>
2015-02-23 09:51:42 +0000

    Remove unused MediaControlElementType enum values
    MediaStatusDisplay and MediaTextTrackDisplay, that is.
    
    BUG=448795
    
    Review URL: https://codereview.chromium.org/946243003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190644 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/accessibility/AXMediaControls.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXMediaControls.cpp
@@ -102,8 +102,6 @@ String AccessibilityMediaControl::accessibilityDescription() const
         return queryString(WebLocalizedString::AXMediaUnMuteButton);
     case MediaPauseButton:
         return queryString(WebLocalizedString::AXMediaPauseButton);
-    case MediaStatusDisplay:
-        return queryString(WebLocalizedString::AXMediaStatusDisplay);
     case MediaCurrentTimeDisplay:
         return queryString(WebLocalizedString::AXMediaCurrentTimeDisplay);
     case MediaTimeRemainingDisplay:
@@ -136,8 +134,6 @@ String AccessibilityMediaControl::helpText() const
         return queryString(WebLocalizedString::AXMediaUnMuteButtonHelp);
     case MediaPauseButton:
         return queryString(WebLocalizedString::AXMediaPauseButtonHelp);
-    case MediaStatusDisplay:
-        return queryString(WebLocalizedString::AXMediaStatusDisplayHelp);
     case MediaCurrentTimeDisplay:
         return queryString(WebLocalizedString::AXMediaCurrentTimeDisplayHelp);
     case MediaTimeRemainingDisplay:
@@ -175,9 +171,6 @@ AccessibilityRole AccessibilityMediaControl::roleValue() const
     case MediaShowClosedCaptionsButton:
     case MediaHideClosedCaptionsButton:
         return ButtonRole;
-
-    case MediaStatusDisplay:
-        return StaticTextRole;
 
     case MediaTimelineContainer:
         return GroupRole;
