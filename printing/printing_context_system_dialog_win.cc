6a08a0e60a1ca68dcca17fd885f454d9bc4d7a23
vitalybuka <vitalybuka@chromium.org>
2015-02-23 10:30:49 -0800

    Block nested system print dialogs on Windows.
    Browser needs to allow nested tasks to update backgroud tabs.
    If rendered requests new system dialog, asan build crashes in COMDLG32.dll
    
    NOTRY=true
    BUG=451753
    
    Review URL: https://codereview.chromium.org/934393003
    
    Cr-Commit-Position: refs/heads/master@{#317611}

--- printing/printing_context_system_dialog_win.cc
+++ printing/printing_context_system_dialog_win.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "printing/printing_context_system_dialog_win.h"
 
+#include "base/auto_reset.h"
 #include "base/message_loop/message_loop.h"
 #include "printing/backend/win_helper.h"
 #include "printing/print_settings_initializer_win.h"
@@ -85,6 +86,13 @@ void PrintingContextSytemDialogWin::Cancel() {
 }
 
 HRESULT PrintingContextSytemDialogWin::ShowPrintDialog(PRINTDLGEX* options) {
+  // Runs always on the UI thread.
+  static bool is_dialog_shown = false;
+  if (is_dialog_shown)
+    return E_FAIL;
+  // Block opening dialog from nested task. It crashes PrintDlgEx.
+  base::AutoReset<bool> auto_reset(&is_dialog_shown, true);
+
   // Note that this cannot use ui::BaseShellDialog as the print dialog is
   // system modal: opening it from a background thread can cause Windows to
   // get the wrong Z-order which will make the print dialog appear behind the
