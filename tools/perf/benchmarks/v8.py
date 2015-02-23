037506ba58e90b614407edf3d2f6f792cf65dcc7
ulan <ulan@chromium.org>
2015-02-23 00:06:33 -0800

    [telemetry] Add new measurement that counts number of GCs needed to free V8 context. The lower this number, the more efficient GC.
    Add benchmark that reloads top pages and tracks this measurement.
    
    BUG=chromium:450824
    
    Review URL: https://codereview.chromium.org/930333002
    
    Cr-Commit-Position: refs/heads/master@{#317551}

--- tools/perf/benchmarks/v8.py
+++ tools/perf/benchmarks/v8.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import page_sets
+from measurements import v8_detached_context_age_in_gc
 from measurements import v8_gc_times
 from telemetry import benchmark
 
@@ -43,3 +44,14 @@ class V8KeyMobileSites(benchmark.Benchmark):
   @classmethod
   def Name(cls):
     return 'v8.key_mobile_sites_smooth'
+
+class V8DetachedContextAgeInGC(benchmark.Benchmark):
+  """Measures the number of GCs needed to collect a detached context.
+
+  http://www.chromium.org/developers/design-documents/rendering-benchmarks"""
+  test = v8_detached_context_age_in_gc.V8DetachedContextAgeInGC
+  page_set = page_sets.PageReloadCasesPageSet
+
+  @classmethod
+  def Name(cls):
+    return 'v8.detached_context_age_in_gc'
