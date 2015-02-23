b982bd1178ddbcdf7d67021ef9f40df357707884
sigbjornf@opera.com <sigbjornf@opera.com>
2015-02-23 08:45:26 +0000

    Make Geolocation internal test methods more resilient.
    Have the window.internals Geolocation test methods typecheck its arguments,
    throwing TypeError if passed invalid Document arguments.
    
    Additionally, have the methods gracefully handle detached documents.
    
    R=tkent
    BUG=460550
    
    Review URL: https://codereview.chromium.org/925953003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190642 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/Geolocation/script-tests/invalid-internals-arguments.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Verify that using invalid or detached documents on internal test methods does not crash.");
+
+// The internals object and these test methods aren't available in production
+// builds, but they are exposed to fuzzers.
+
+if (!window.testRunner || !window.internals)
+    debug('This test can not run without testRunner or internals');
+
+shouldThrow("internals.setGeolocationClientMock(null);");
+shouldThrow("internals.setGeolocationPosition(null, 1, 2, 3);");
+shouldThrow("internals.setGeolocationPermission(window.notThere, true);");
+shouldThrow("internals.setGeolocationPositionUnavailableError(null, 'not available');");
+shouldThrow("internals.numberOfPendingGeolocationPermissionRequests(null)");
+
+var ifr = document.getElementById("ifr");
+var iframe = ifr.contentWindow;
+ifr.remove();
+// Verify that detached documents do not crash.
+shouldBeUndefined("iframe.internals.setGeolocationClientMock(iframe.document)");
+shouldBeUndefined("iframe.internals.setGeolocationPosition(iframe.document, 1, 2, 3)");
+shouldBeUndefined("iframe.internals.setGeolocationPermission(iframe.document, true)");
+shouldBeUndefined("iframe.internals.setGeolocationPositionUnavailableError(iframe.document, 'not available')");
+shouldBe("iframe.internals.numberOfPendingGeolocationPermissionRequests(iframe.document)", "-1");
