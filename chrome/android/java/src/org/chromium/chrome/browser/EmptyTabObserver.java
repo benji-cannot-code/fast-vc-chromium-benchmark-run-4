e4bff7cdbec5c4d1e04f1d79225cbb1cb2adf9f7
yfriedman <yfriedman@chromium.org>
2015-02-23 07:59:19 -0800

    Pass LoadUrlParams instead of just url to TabObserver.onLoadUrl
    Some observers will need more information than just the url.
    
    BUG=443773
    
    Review URL: https://codereview.chromium.org/930933006
    
    Cr-Commit-Position: refs/heads/master@{#317588}

--- chrome/android/java/src/org/chromium/chrome/browser/EmptyTabObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/EmptyTabObserver.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser;
 import android.view.ContextMenu;
 
 import org.chromium.content.browser.ContentViewCore;
+import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.WebContents;
 
 /**
@@ -38,6 +39,9 @@ public class EmptyTabObserver implements TabObserver {
 
     @Override
     public void onLoadUrl(Tab tab, String url, int loadType) { }
+
+    @Override
+    public void onLoadUrl(Tab tab, LoadUrlParams params, int loadType) { }
 
     @Override
     public void onPageLoadStarted(Tab tab) { }
