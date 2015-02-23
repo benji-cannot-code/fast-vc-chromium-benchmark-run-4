5b5dcc8e862046afdc3b7b914b82c389688ea7df
jochen <jochen@chromium.org>
2015-02-23 08:55:37 -0800

    When sanitizing serialized navigation entries also take iframes into account
    The state of the iframes is encoded in the page state, so we need to
    decode the page state and recursively check it.
    
    BUG=422871
    R=marja@chromium.org
    
    Review URL: https://codereview.chromium.org/948013002
    
    Cr-Commit-Position: refs/heads/master@{#317595}

--- content/public/common/page_state.cc
+++ content/public/common/page_state.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/strings/utf_string_conversions.h"
 #include "content/common/page_state_serialization.h"
+#include "content/public/common/referrer.h"
 
 namespace content {
 namespace {
@@ -54,6 +55,25 @@ void RecursivelyRemoveReferrer(ExplodedFrameState* state) {
        ++it) {
     RecursivelyRemoveReferrer(&*it);
   }
+}
+
+bool RecursivelyCheckReferrer(ExplodedFrameState* state) {
+  Referrer referrer(GURL(state->referrer.string()), state->referrer_policy);
+  GURL url(state->url_string.string());
+  if (url.SchemeIsHTTPOrHTTPS() &&
+      Referrer::SanitizeForRequest(url, referrer).url != referrer.url) {
+    LOG(ERROR) << "Referrer for request to " << url << " is " << referrer.url
+               << " but should be "
+               << Referrer::SanitizeForRequest(url, referrer).url;
+    return false;
+  }
+  for (std::vector<ExplodedFrameState>::iterator it = state->children.begin();
+       it != state->children.end();
+       ++it) {
+    if (!RecursivelyCheckReferrer(&*it))
+      return false;
+  }
+  return true;
 }
 
 }  // namespace
@@ -109,7 +129,16 @@ PageState::PageState() {
 }
 
 bool PageState::IsValid() const {
-  return !data_.empty();
+  if (data_.empty())
+    return false;
+
+  ExplodedPageState state;
+  // This should return false, but tests create invalid page state.
+  if (!DecodePageState(data_, &state))
+    return true;
+
+  // TODO(jochen): Remove referrer check once http://crbug.com/450589 is fixed.
+  return RecursivelyCheckReferrer(&state.top);
 }
 
 bool PageState::Equals(const PageState& other) const {
