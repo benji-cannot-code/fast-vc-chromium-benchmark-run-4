6ac49265e7484ef8adbef338042cc3bad859f183
bauerb <bauerb@chromium.org>
2015-02-23 03:14:59 -0800

    Use SCOPED_UMA_HISTOGRAM_TIMER in SupervisedUserSiteList.
    Review URL: https://codereview.chromium.org/928253002
    
    Cr-Commit-Position: refs/heads/master@{#317566}

--- chrome/browser/supervised_user/supervised_user_site_list.cc
+++ chrome/browser/supervised_user/supervised_user_site_list.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/json/json_file_value_serializer.h"
 #include "base/logging.h"
-#include "base/metrics/histogram.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/task_runner_util.h"
 #include "base/values.h"
 #include "chrome/browser/safe_json_parser.h"
@@ -29,11 +29,9 @@ namespace {
 bool g_load_in_process = false;
 
 std::string ReadFileOnBlockingThread(const base::FilePath& path) {
-  base::TimeTicks start = base::TimeTicks::Now();
+  SCOPED_UMA_HISTOGRAM_TIMER("ManagedUsers.Whitelist.ReadDuration");
   std::string contents;
   base::ReadFileToString(path, &contents);
-  UMA_HISTOGRAM_TIMES("ManagedUsers.Whitelist.ReadDuration",
-                      base::TimeTicks::Now() - start);
   return contents;
 }
 
