a958d6bd92c57768d484216f8ee1d2eb27c4b766
gunsch <gunsch@chromium.org>
2015-02-23 12:32:17 -0800

    Chromecast buildfix: restores AddKeySystemWithCodecs, fixes namespace.
    This is a partial revert of https://codereview.chromium.org/923283002
    for chromecast/, since we reference AddKeySystemWithCodecs from
    internal code. The functional changes are kept but the API changes
    are reverted.
    
    R=lcwu@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/947233003
    
    Cr-Commit-Position: refs/heads/master@{#317641}

--- chromecast/renderer/key_systems_cast.h
+++ chromecast/renderer/key_systems_cast.h
@@ -13,6 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromecast {
 namespace shell {
 
+// Adds a single key system by name.
+// TODO(gunsch): modify this API to accept specifying different supported
+// features, and/or APIs per key system type.
+void AddKeySystemWithCodecs(
+    const std::string& key_system_name,
+    std::vector<::media::KeySystemInfo>* concrete_key_systems);
+
 void AddChromecastKeySystems(
     std::vector<::media::KeySystemInfo>* key_systems_info);
 
