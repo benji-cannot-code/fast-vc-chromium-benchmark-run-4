e7c034c0199944e6f4d114de67b6790c158eea40
toyoshim@chromium.org <toyoshim@chromium.org>
2015-02-23 00:51:15 +0000

    Web MIDI: blink API update to support MIDIPortState
    The latest editor's draft introduces MIDIPortState state attribute in
    MIDIPort. To support the attribute, update the blink API to use enum
    for representing MIDIPortState instead of bool isActive flag.
    
    Spec: http://webaudio.github.io/web-midi-api/#idl-def-MIDIPort
    
    This is the first step of three to update the MIDIAccessor blink API.
    
    BUG=279097
    TEST=build with chromium
    
    Review URL: https://codereview.chromium.org/942243002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190630 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webmidi/MIDIAccessor.cpp
+++ third_party/WebKit/Source/modules/webmidi/MIDIAccessor.cpp
@@ -66,6 +66,7 @@ void MIDIAccessor::sendMIDIData(unsigned portIndex, const unsigned char* data, s
     m_accessor->sendMIDIData(portIndex, data, length, timeStamp);
 }
 
+// FIXME: Remove obsolete interfaces that use isActive.
 void MIDIAccessor::didAddInputPort(const WebString& id, const WebString& manufacturer, const WebString& name, const WebString& version, bool isActive)
 {
     m_client->didAddInputPort(id, manufacturer, name, version, isActive);
@@ -76,6 +77,17 @@ void MIDIAccessor::didAddOutputPort(const WebString& id, const WebString& manufa
     m_client->didAddOutputPort(id, manufacturer, name, version, isActive);
 }
 
+void MIDIAccessor::didAddInputPort(const WebString& id, const WebString& manufacturer, const WebString& name, const WebString& version, MIDIPortState state)
+{
+    m_client->didAddInputPort(id, manufacturer, name, version, state != MIDIPortStateDisconnected);
+}
+
+void MIDIAccessor::didAddOutputPort(const WebString& id, const WebString& manufacturer, const WebString& name, const WebString& version, MIDIPortState state)
+{
+    m_client->didAddOutputPort(id, manufacturer, name, version, state != MIDIPortStateDisconnected);
+}
+
+// FIXME: Remove obsolete interfaces that use isActive.
 void MIDIAccessor::didSetInputPortState(unsigned portIndex, bool isActive)
 {
     m_client->didSetInputPortState(portIndex, isActive);
@@ -84,6 +96,16 @@ void MIDIAccessor::didSetInputPortState(unsigned portIndex, bool isActive)
 void MIDIAccessor::didSetOutputPortState(unsigned portIndex, bool isActive)
 {
     m_client->didSetOutputPortState(portIndex, isActive);
+}
+
+void MIDIAccessor::didSetInputPortState(unsigned portIndex, MIDIPortState state)
+{
+    m_client->didSetInputPortState(portIndex, state != MIDIPortStateDisconnected);
+}
+
+void MIDIAccessor::didSetOutputPortState(unsigned portIndex, MIDIPortState state)
+{
+    m_client->didSetOutputPortState(portIndex, state != MIDIPortStateDisconnected);
 }
 
 void MIDIAccessor::didStartSession(bool success, const WebString& error, const WebString& message)
