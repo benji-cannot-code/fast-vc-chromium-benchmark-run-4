30ccb608b93f02381648b003774a20571490b057
zhengw@chromium.org <zhengw@chromium.org>
2015-02-23 18:51:22 +0000

    Schedule cursor update when DOM updates layout.
    BUG=26723
    
    Review URL: https://codereview.chromium.org/915543003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190676 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -2107,6 +2107,8 @@ void FrameView::performPostLayoutTasks()
 
     FontFaceSet::didLayout(*m_frame->document());
 
+    frame().eventHandler().scheduleCursorUpdate();
+
     updateWidgetPositions();
 
     // Plugins could have torn down the page inside updateWidgetPositions().
