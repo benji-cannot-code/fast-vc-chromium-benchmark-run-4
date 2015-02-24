a60421c7aafee6674c1adc2f5b968eab778d4675
hans <hans@chromium.org>
2015-02-24 14:14:54 -0800

    Fix some virtual and override specifiers
    The new Clang plugin is more picky.
    
    BUG=444089, 459607
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/943893006
    
    Cr-Commit-Position: refs/heads/master@{#317899}

--- media/blink/run_all_unittests.cc
+++ media/blink/run_all_unittests.cc
@@ -42,10 +42,10 @@ const unsigned char* TestBlinkPlatformSupport::getTraceCategoryEnabledFlag(
 class BlinkMediaTestSuite : public base::TestSuite {
  public:
   BlinkMediaTestSuite(int argc, char** argv);
-  virtual ~BlinkMediaTestSuite();
+  ~BlinkMediaTestSuite() override;
 
  protected:
-  virtual void Initialize() override;
+  void Initialize() override;
 
  private:
   scoped_ptr<TestBlinkPlatformSupport> blink_platform_support_;
--- media/blink/video_frame_compositor_unittest.cc
+++ media/blink/video_frame_compositor_unittest.cc
@@ -27,7 +27,7 @@ class VideoFrameCompositorTest : public testing::Test,
     compositor_->SetVideoFrameProviderClient(this);
   }
 
-  virtual ~VideoFrameCompositorTest() {
+  ~VideoFrameCompositorTest() override {
     compositor_->SetVideoFrameProviderClient(NULL);
   }
 
@@ -41,11 +41,11 @@ class VideoFrameCompositorTest : public testing::Test,
 
  private:
   // cc::VideoFrameProvider::Client implementation.
-  virtual void StopUsingProvider() override {}
-  virtual void DidReceiveFrame() override {
+  void StopUsingProvider() override {}
+  void DidReceiveFrame() override {
     ++did_receive_frame_count_;
   }
-  virtual void DidUpdateMatrix(const float* matrix) override {}
+  void DidUpdateMatrix(const float* matrix) override {}
 
   void NaturalSizeChanged(gfx::Size natural_size) {
     ++natural_size_changed_count_;
