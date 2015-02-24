820b0e7b65b480f9da3a7a3e7987b413cfa01701
kundaji <kundaji@chromium.org>
2015-02-23 17:27:04 -0800

    Context menu to view original image when Data Saver is being used.
    Show menu item to view original image if the request could go over
    the data reduction proxy.
    
    BUG=456247
    
    Review URL: https://codereview.chromium.org/944533002
    
    Cr-Commit-Position: refs/heads/master@{#317720}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_settings.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_settings.cc
@@ -48,6 +48,9 @@ bool IsEnabledOnCommandLine() {
 
 namespace data_reduction_proxy {
 
+const char kDataReductionPassThroughHeader[] =
+    "X-PSA-Client-Options: v=1,m=1\nCache-Control: no-cache";
+
 DataReductionProxySettings::DataReductionProxySettings()
     : unreachable_(false),
       allowed_(false),
@@ -111,8 +114,14 @@ void DataReductionProxySettings::SetOnDataReductionEnabledCallback(
   on_data_reduction_proxy_enabled_.Run(IsDataReductionProxyEnabled());
 }
 
-bool DataReductionProxySettings::IsDataReductionProxyEnabled() {
+bool DataReductionProxySettings::IsDataReductionProxyEnabled() const {
   return spdy_proxy_auth_enabled_.GetValue() || IsEnabledOnCommandLine();
+}
+
+bool DataReductionProxySettings::CanUseDataReductionProxy(
+    const GURL& url) const {
+  return url.is_valid() && url.scheme() == url::kHttpScheme &&
+      IsDataReductionProxyEnabled();
 }
 
 bool
