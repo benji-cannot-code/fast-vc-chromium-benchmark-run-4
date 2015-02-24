87aaf84c0397f76e04393723e30c4dbc4bb5c742
dmazzoni <dmazzoni@chromium.org>
2015-02-24 12:35:17 -0800

    Implement NativeViewAccessibilityWin using AXPlatformNodeWin.
    AXPlatformNodeWin is an implementation of Windows accessibility APIs
    that doesn't know anything about content/ or ui/views/ - it just
    exposes an abstract tree of AXNodeData nodes and uses a delegate
    interface for everything else.
    
    This patch completely removes the Windows-specific views accessibility
    code and implements it using AXPlatformNodeWin instead. A future
    step will be to do the same with BrowserAccessibilityWin in content/.
    
    This patch also includes a new unit test for AXPlatformNodeWin,
    since it's now possible to test it without any views or web
    content code. Hooray!
    
    BUG=457564
    
    Review URL: https://codereview.chromium.org/909143003
    
    Cr-Commit-Position: refs/heads/master@{#317878}

--- ui/views/controls/webview/webview.cc
+++ ui/views/controls/webview/webview.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_view_state.h"
 #include "ui/base/ui_base_switches_util.h"
 #include "ui/events/event.h"
-#include "ui/views/accessibility/native_view_accessibility.h"
 #include "ui/views/controls/native/native_view_host.h"
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/views_delegate.h"
@@ -38,12 +37,10 @@ WebView::WebView(content::BrowserContext* browser_context)
       browser_context_(browser_context),
       allow_accelerators_(false) {
   AddChildView(holder_);  // Takes ownership of |holder_|.
-  NativeViewAccessibility::RegisterWebView(this);
 }
 
 WebView::~WebView() {
   SetWebContents(NULL);  // Make sure all necessary tear-down takes place.
-  NativeViewAccessibility::UnregisterWebView(this);
 }
 
 content::WebContents* WebView::GetWebContents() {
