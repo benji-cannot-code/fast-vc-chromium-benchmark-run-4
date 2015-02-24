9efe6da13ac519802caddedf9d9fd0912ff7e8a3
mazda <mazda@chromium.org>
2015-02-23 17:36:36 -0800

    Remove irt_nonsfi.h, which is no longer necessary.
    The definition of nacl_irt_icache was moved to irt.h in the NaCl repository,
    so irt_nonsfi.h is no longer necessary.
    
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=4033
    
    Review URL: https://codereview.chromium.org/935743004
    
    Cr-Commit-Position: refs/heads/master@{#317728}

--- components/nacl/loader/nonsfi/irt_interfaces.h
+++ components/nacl/loader/nonsfi/irt_interfaces.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "native_client/src/untrusted/irt/irt.h"
-#include "ppapi/nacl_irt/public/irt_nonsfi.h"
 #include "ppapi/nacl_irt/public/irt_ppapi.h"
 
 namespace nacl {
