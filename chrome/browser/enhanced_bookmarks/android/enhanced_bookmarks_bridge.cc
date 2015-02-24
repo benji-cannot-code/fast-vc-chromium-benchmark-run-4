4fe2348eea0aad73fdd6f5647bee73f2e55b029b
ianwen <ianwen@chromium.org>
2015-02-24 09:51:22 -0800

    Fix bug that AddBookmarks returns partner bookmark node
    BookmarkNode::getType() and BookmarkID::Type are not the same things.
    The previous implementation confused them, which caused a bug that
    adding a folder will return a partner bookmark.
    
    BUG=458703
    
    Review URL: https://codereview.chromium.org/950033002
    
    Cr-Commit-Position: refs/heads/master@{#317820}

--- chrome/browser/enhanced_bookmarks/android/enhanced_bookmarks_bridge.cc
+++ chrome/browser/enhanced_bookmarks/android/enhanced_bookmarks_bridge.cc
@@ -205,8 +205,8 @@ ScopedJavaLocalRef<jobject> EnhancedBookmarksBridge::AddFolder(JNIEnv* env,
     NOTREACHED();
     return ScopedJavaLocalRef<jobject>();
   }
-  ScopedJavaLocalRef<jobject> new_java_obj =
-      JavaBookmarkIdCreateBookmarkId(env, new_node->id(), new_node->type());
+  ScopedJavaLocalRef<jobject> new_java_obj = JavaBookmarkIdCreateBookmarkId(
+      env, new_node->id(), BookmarkType::BOOKMARK_TYPE_NORMAL);
   return new_java_obj;
 }
 
@@ -256,8 +256,8 @@ ScopedJavaLocalRef<jobject> EnhancedBookmarksBridge::AddBookmark(
     NOTREACHED();
     return ScopedJavaLocalRef<jobject>();
   }
-  ScopedJavaLocalRef<jobject> new_java_obj =
-      JavaBookmarkIdCreateBookmarkId(env, new_node->id(), new_node->type());
+  ScopedJavaLocalRef<jobject> new_java_obj = JavaBookmarkIdCreateBookmarkId(
+      env, new_node->id(), BookmarkType::BOOKMARK_TYPE_NORMAL);
   return new_java_obj;
 }
 
