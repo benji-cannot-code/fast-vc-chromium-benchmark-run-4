177a88f0f65d66cdf5fc950ef351e8ae105194e6
raphael.kubo.da.costa <raphael.kubo.da.costa@intel.com>
2015-02-24 04:44:26 -0800

    WebAudioMediaCodecBridge: Close file descriptor on error.
    Use a base::ScopedFD instead of a plain integer so we do not have to
    worry about manually closing SaveEncodedAudioToFile()'s temporary file
    description on error cases.
    
    Review URL: https://codereview.chromium.org/942033002
    
    Cr-Commit-Position: refs/heads/master@{#317786}

--- media/base/android/webaudio_media_codec_bridge.cc
+++ media/base/android/webaudio_media_codec_bridge.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "base/basictypes.h"
+#include "base/files/scoped_file.h"
 #include "base/logging.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/stl_util.h"
@@ -65,13 +66,13 @@ int WebAudioMediaCodecBridge::SaveEncodedAudioToFile(
 
   // Open the file and unlink it, so that it will be actually removed
   // when we close the file.
-  int fd = open(temporaryFile.c_str(), O_RDWR);
+  base::ScopedFD fd(open(temporaryFile.c_str(), O_RDWR));
   if (unlink(temporaryFile.c_str())) {
     VLOG(0) << "Couldn't unlink temp file " << temporaryFile
             << ": " << strerror(errno);
   }
 
-  if (fd < 0) {
+  if (!fd.is_valid()) {
     return -1;
   }
 
@@ -84,15 +85,15 @@ int WebAudioMediaCodecBridge::SaveEncodedAudioToFile(
     return -1;
   }
 
-  if (static_cast<uint32_t>(write(fd, encoded_data.memory(), data_size_))
+  if (static_cast<uint32_t>(write(fd.get(), encoded_data.memory(), data_size_))
       != data_size_) {
     VLOG(0) << "Failed to write all audio data to temp file!";
     return -1;
   }
 
-  lseek(fd, 0, SEEK_SET);
+  lseek(fd.get(), 0, SEEK_SET);
 
-  return fd;
+  return fd.release();
 }
 
 bool WebAudioMediaCodecBridge::DecodeInMemoryAudioFile() {
