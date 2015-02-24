6cd8a637a2c669a35f36996fef527ed7cf8dc9f7
sigbjornf@opera.com <sigbjornf@opera.com>
2015-02-24 00:33:50 +0000

    Allow Oilpan heap objects account for their external allocations.
    The GC heuristics take into account the amount of objects allocated since
    the last GC, in terms of the amount of Oilpan heap bytes allocated.
    
    It does not consider the amount of external memory owned by an
    Oilpan object. Allow objects to register such external allocations and
    have the GC heuristics consider it -- if the heap isn't otherwise
    considered worth GCing but it contains references to a large amount of
    external memory, scheduling a GC may lessen the overall memory pressure
    for the process.
    
    Along with this, also add support for letting the outside world notify
    Oilpan that a GC is now really worth considering. Intended used by other
    allocators if they are running into near-OOM conditions.
    
    R=haraken
    BUG=456498
    
    Review URL: https://codereview.chromium.org/875503003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190702 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Text.h
+++ third_party/WebKit/Source/core/dom/Text.h
@@ -62,6 +62,8 @@ public:
     virtual bool canContainRangeEndPoint() const override final { return true; }
     virtual NodeType nodeType() const override;
 
+    virtual void trace(Visitor*) override;
+
 protected:
     Text(TreeScope& treeScope, const String& data, ConstructionType type)
         : CharacterData(treeScope, data, type) { }
