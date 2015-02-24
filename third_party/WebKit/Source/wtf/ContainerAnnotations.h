67b5b0ec458d2f6760bb6bf730aa4a96cdb84aa5
ochang@chromium.org <ochang@chromium.org>
2015-02-24 20:27:39 +0000

    Only enable WTF::Vector container annotations for Linux
    BUG=430162,414136
    R=inferno@chromium.org
    TBR=tkent@chromium.org
    
    Review URL: https://codereview.chromium.org/956683002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190774 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/ContainerAnnotations.h
+++ third_party/WebKit/Source/wtf/ContainerAnnotations.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "wtf/Alignment.h"
 
-#if defined(ADDRESS_SANITIZER)
+#if defined(ADDRESS_SANITIZER) && OS(LINUX)
 #define ANNOTATE_CONTIGUOUS_CONTAINER
 #define ANNOTATE_NEW_BUFFER(buffer, capacity, newSize) \
     if (buffer) { \
