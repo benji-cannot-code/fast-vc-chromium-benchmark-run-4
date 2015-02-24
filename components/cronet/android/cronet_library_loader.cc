e2d49279a37fa0538ac92f7793ffb9cf88afe3a8
xunjieli <xunjieli@chromium.org>
2015-02-24 10:22:59 -0800

    [Cronet] fix NoClassDefFoundError crash
    There have been some recent changes in JNI registration (see:
    crbug.com/442327). These changes affect Cronet as well. Currently
    Cronet builds are crashing with a java.lang.NoClassDefFoundError.
    This CL is to fix the build.
    
    Review URL: https://codereview.chromium.org/953603003
    
    Cr-Commit-Position: refs/heads/master@{#317832}

--- components/cronet/android/cronet_library_loader.cc
+++ components/cronet/android/cronet_library_loader.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/base_jni_registrar.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_registrar.h"
+#include "base/android/jni_utils.h"
 #include "base/at_exit.h"
 #include "base/logging.h"
 #include "base/message_loop/message_loop.h"
@@ -73,6 +74,8 @@ jint CronetOnLoad(JavaVM* vm, void* reserved) {
 
   g_at_exit_manager = new base::AtExitManager();
 
+  base::android::InitReplacementClassLoader(env,
+                                            base::android::GetClassLoader(env));
   url::Initialize();
 
   return JNI_VERSION_1_6;
