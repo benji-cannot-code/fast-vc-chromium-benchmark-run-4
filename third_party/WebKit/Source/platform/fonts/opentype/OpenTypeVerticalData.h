8a7cd1d916e663aa2c2b3861ea8fc032dddf507d
kojii@chromium.org <kojii@chromium.org>
2015-02-25 01:45:02 +0000

    Use complex path for vertical layout
    This patch reverts the temporary switch of vertical layout to the simple code
    path, which was done so that our work-in-progress performance work
    should not affect the branch cut. Since the branch cut was done, we switch
    to the complex path to work on its performance.
    
    We acknowledge that this patch will degrade "vertical-japanese-kokoro-insert"
    perf test by 400% or so. The plan is to switch to what we want to ship, and
    make it faster by the next branch cut. If it does not reach close to the ref
    by the next branch cut, we can discuss.
    
    BUG=445589
    
    Review URL: https://codereview.chromium.org/953733002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190789 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/opentype/OpenTypeVerticalData.h
+++ third_party/WebKit/Source/platform/fonts/opentype/OpenTypeVerticalData.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class FontPlatformData;
-class GlyphPage;
 class SimpleFontData;
 
 class PLATFORM_EXPORT OpenTypeVerticalData : public RefCounted<OpenTypeVerticalData> {
@@ -54,14 +53,12 @@ public:
     void setInFontCache(bool inFontCache) { m_inFontCache = inFontCache; }
 
     void getVerticalTranslationsForGlyphs(const SimpleFontData*, const Glyph*, size_t, float* outXYArray) const;
-    void substituteWithVerticalGlyphs(const SimpleFontData*, GlyphPage*, unsigned offset, unsigned length) const;
 
 private:
 
     explicit OpenTypeVerticalData(const FontPlatformData&);
 
     void loadMetrics(const FontPlatformData&);
-    void loadVerticalGlyphSubstitutions(const FontPlatformData&);
     bool hasVORG() const { return !m_vertOriginY.isEmpty(); }
 
     HashMap<Glyph, Glyph> m_verticalGlyphMap;
