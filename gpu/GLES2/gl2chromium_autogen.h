47309f8e590e6f9874d369c5928bc1d80150af4d
zmo <zmo@chromium.org>
2015-02-24 17:36:17 -0800

    Add glGetSynciv to GPU command buffer.
    BUG=429053
    TEST=gpu_unittests
    R=piman@chromium.org
    
    Review URL: https://codereview.chromium.org/935173002
    
    Cr-Commit-Position: refs/heads/master@{#317943}

--- gpu/GLES2/gl2chromium_autogen.h
+++ gpu/GLES2/gl2chromium_autogen.h
@@ -112,6 +112,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define glGetShaderPrecisionFormat GLES2_GET_FUN(GetShaderPrecisionFormat)
 #define glGetShaderSource GLES2_GET_FUN(GetShaderSource)
 #define glGetString GLES2_GET_FUN(GetString)
+#define glGetSynciv GLES2_GET_FUN(GetSynciv)
 #define glGetTexParameterfv GLES2_GET_FUN(GetTexParameterfv)
 #define glGetTexParameteriv GLES2_GET_FUN(GetTexParameteriv)
 #define glGetTransformFeedbackVarying GLES2_GET_FUN(GetTransformFeedbackVarying)
