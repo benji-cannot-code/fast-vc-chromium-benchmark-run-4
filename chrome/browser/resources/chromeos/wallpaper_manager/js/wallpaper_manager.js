c5f60ed46265618ca140a590ceb034b8461fe968
xdai <xdai@chromium.org>
2015-02-25 13:17:10 -0800

    Unchecking the "Surprise Me" checkbox should go back previous wallpaper before "Surprise Me" was turned on.
    BUG=316692
    TEST=Manually tested
    
    Review URL: https://codereview.chromium.org/948243006
    
    Cr-Commit-Position: refs/heads/master@{#318116}

--- chrome/browser/resources/chromeos/wallpaper_manager/js/wallpaper_manager.js
+++ chrome/browser/resources/chromeos/wallpaper_manager/js/wallpaper_manager.js
@@ -196,6 +196,7 @@ function WallpaperManager(dialogDom) {
    * @private
    */
   WallpaperManager.prototype.toggleSurpriseMe_ = function() {
+    var self = this;
     var checkbox = $('surprise-me').querySelector('#checkbox');
     var shouldEnable = !checkbox.classList.contains('checked');
     WallpaperUtil.saveToStorage(Constants.AccessSurpriseMeEnabledKey,
@@ -204,6 +205,11 @@ function WallpaperManager(dialogDom) {
           if (shouldEnable) {
             checkbox.classList.add('checked');
           } else {
+            // Unchecking the "Surprise me" checkbox falls back to the previous
+            // wallpaper before "Surprise me" was turned on.
+            self.setSelectedWallpaper_(self.wallpaperGrid_.activeItem_);
+            self.onWallpaperChanged_(self.wallpaperGrid_.activeItem_,
+                                     self.currentWallpaper_);
             checkbox.classList.remove('checked');
           }
           $('categories-list').disabled = shouldEnable;
@@ -983,7 +989,7 @@ function WallpaperManager(dialogDom) {
     bar.style.width = selectedListItem.offsetWidth + 'px';
 
     var wallpapersDataModel = new cr.ui.ArrayDataModel([]);
-    var selectedItem;
+    var selectedItem = null;
     if (selectedListItem.custom) {
       this.document_.body.setAttribute('custom', '');
       var errorHandler = this.onFileSystemError_.bind(this);
@@ -1029,8 +1035,10 @@ function WallpaperManager(dialogDom) {
         };
         wallpapersDataModel.push(lastElement);
         self.wallpaperGrid_.dataModel = wallpapersDataModel;
-        self.wallpaperGrid_.selectedItem = selectedItem;
-        self.wallpaperGrid_.activeItem = selectedItem;
+        if (selectedItem) {
+          self.wallpaperGrid_.selectedItem = selectedItem;
+          self.wallpaperGrid_.activeItem = selectedItem;
+        }
       };
 
       var success = function(dirEntry) {
@@ -1083,8 +1091,10 @@ function WallpaperManager(dialogDom) {
         }
       }
       this.wallpaperGrid_.dataModel = wallpapersDataModel;
-      this.wallpaperGrid_.selectedItem = selectedItem;
-      this.wallpaperGrid_.activeItem = selectedItem;
+      if (selectedItem) {
+        this.wallpaperGrid_.selectedItem = selectedItem;
+        this.wallpaperGrid_.activeItem = selectedItem;
+      }
     }
   };
 
