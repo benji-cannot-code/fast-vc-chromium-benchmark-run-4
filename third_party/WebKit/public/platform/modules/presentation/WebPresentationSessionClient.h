87b2cb1dc55df803b8bc702c9939b54d471e7e98
avayvod@chromium.org <avayvod@chromium.org>
2015-02-25 17:27:24 +0000

    [PresentationAPI] Added/changed public/platform interfaces for start/joinSession
    See https://codereview.chromium.org/940503002/ and https://codereview.chromium.org/935083002/
    for uses and implementation.
    
    BUG=459005
    
    Review URL: https://codereview.chromium.org/935103002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190843 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/public/platform/modules/presentation/WebPresentationSessionClient.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WebPresentationSessionClient_h
+#define WebPresentationSessionClient_h
+
+#include "public/platform/WebString.h"
+
+namespace blink {
+
+// The implementation the embedder has to provide for the Presentation API to work.
+class WebPresentationSessionClient {
+public:
+    virtual ~WebPresentationSessionClient() { }
+
+    virtual WebString getId() = 0;
+    virtual WebString getUrl() = 0;
+};
+
+} // namespace blink
+
+#endif // WebPresentationSessionClient_h
