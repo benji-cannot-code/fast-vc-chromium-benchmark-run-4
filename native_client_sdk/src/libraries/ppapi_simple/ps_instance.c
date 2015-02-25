2134985d17441f3b3ecc5e92f2ec5f9c681a2fbc
binji <binji@chromium.org>
2015-02-24 17:55:33 -0800

    [NaCl SDK] Fix bionic build after cd3968da
    BUG=none
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/946213003
    
    Cr-Commit-Position: refs/heads/master@{#317950}

--- native_client_sdk/src/libraries/ppapi_simple/ps_instance.c
+++ native_client_sdk/src/libraries/ppapi_simple/ps_instance.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <termios.h>  /* Needed for struct winsize in bionic */
 #include <unistd.h>
 
 #include <ppapi/c/pp_errors.h>
