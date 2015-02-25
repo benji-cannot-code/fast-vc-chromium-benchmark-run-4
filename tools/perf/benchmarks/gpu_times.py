9513624ece7f2f5c71b763579793906551c818cf
dyen <dyen@chromium.org>
2015-02-24 20:57:59 -0800

    Enabled gpu_times metrics.
    The issue where gpu_times would use too much memory has been fixed:
    https://codereview.chromium.org/920523002
    
    In order to prepare for future changes where additional metrics
    will be added to the TBM module, I have also preemptively added a
    filter function which filters out values relating to gpu_times.
    
    R=nednguyen@chromium.org
    BUG=455292, 453131
    TEST=local runs measuring memory usage shows ~700mb
    
    Review URL: https://codereview.chromium.org/937073003
    
    Cr-Commit-Position: refs/heads/master@{#317974}

--- tools/perf/benchmarks/gpu_times.py
+++ tools/perf/benchmarks/gpu_times.py
@@ -27,8 +27,12 @@ class _GPUTimes(benchmark.Benchmark):
         overhead_level=cat_filter,
         get_metrics_from_flags_callback=_GetGPUTimelineMetric)
 
+  @classmethod
+  def ValueCanBeAddedPredicate(cls, value):
+    return (isinstance(value, gpu_timeline.GPUTimelineListOfValues) or
+            isinstance(value, gpu_timeline.GPUTimelineValue))
 
-@benchmark.Disabled  # http://crbug.com/455292
+
 class GPUTimesKeyMobileSites(_GPUTimes):
   """Measures GPU timeline metric on key mobile sites."""
   page_set = page_sets.KeyMobileSitesSmoothPageSet
@@ -37,7 +41,7 @@ class GPUTimesKeyMobileSites(_GPUTimes):
   def Name(cls):
     return 'gpu_times.key_mobile_sites_smooth'
 
-@benchmark.Disabled  # http://crbug.com/455292
+
 class GPUTimesGpuRasterizationKeyMobileSites(_GPUTimes):
   """Measures GPU timeline metric on key mobile sites with GPU rasterization.
   """
@@ -49,7 +53,7 @@ class GPUTimesGpuRasterizationKeyMobileSites(_GPUTimes):
   def Name(cls):
     return 'gpu_times.gpu_rasterization.key_mobile_sites_smooth'
 
-@benchmark.Disabled  # http://crbug.com/453131, http://crbug.com/455292
+
 class GPUTimesTop25Sites(_GPUTimes):
   """Measures GPU timeline metric for the top 25 sites."""
   page_set = page_sets.Top25SmoothPageSet
@@ -58,7 +62,7 @@ class GPUTimesTop25Sites(_GPUTimes):
   def Name(cls):
     return 'gpu_times.top_25_smooth'
 
-@benchmark.Disabled  # http://crbug.com/455292
+
 class GPUTimesGpuRasterizationTop25Sites(_GPUTimes):
   """Measures GPU timeline metric for the top 25 sites with GPU rasterization.
   """
