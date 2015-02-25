30528cfcd890586dd7d51727daec4e85e13af846
mlerman <mlerman@chromium.org>
2015-02-25 14:47:54 -0800

    Guest mode doesn't show the Bookmarks item in Wrench Menu.
    BUG=436834
    TEST=In guest mode (across browsers), Bookmarks doesn't show in the
    wrench menu.
    
    Review URL: https://codereview.chromium.org/953723003
    
    Cr-Commit-Position: refs/heads/master@{#318133}

--- chrome/browser/profiles/profile_window_browsertest.cc
+++ chrome/browser/profiles/profile_window_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/history/history_service.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/toolbar/wrench_menu_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/history/core/browser/history_db_task.h"
@@ -68,6 +70,15 @@ void WaitForHistoryBackendToRun(Profile* profile) {
   history->HistoryService::ScheduleDBTask(task.Pass(), &task_tracker);
   content::RunMessageLoop();
 }
+
+class EmptyAcceleratorHandler : public ui::AcceleratorProvider {
+ public:
+  // Don't handle accelerators.
+  bool GetAcceleratorForCommandId(int command_id,
+                                  ui::Accelerator* accelerator) override {
+    return false;
+  }
+};
 
 }  // namespace
 
@@ -187,6 +198,18 @@ IN_PROC_BROWSER_TEST_F(ProfileWindowBrowserTest, GuestCannotSignin) {
 
   // Guest profiles can't sign in without a SigninManager.
   ASSERT_FALSE(signin_manager);
+}
+
+IN_PROC_BROWSER_TEST_F(ProfileWindowBrowserTest, GuestWrenchLacksBookmarks) {
+  EmptyAcceleratorHandler accelerator_handler;
+  // Verify the normal browser has a bookmark menu.
+  WrenchMenuModel model_normal_profile(&accelerator_handler, browser());
+  EXPECT_NE(-1, model_normal_profile.GetIndexOfCommandId(IDC_BOOKMARKS_MENU));
+
+  // Guest browser has no bookmark menu.
+  Browser* guest_browser = OpenGuestBrowser();
+  WrenchMenuModel model_guest_profile(&accelerator_handler, guest_browser);
+  EXPECT_EQ(-1, model_guest_profile.GetIndexOfCommandId(IDC_BOOKMARKS_MENU));
 }
 
 #endif  // !defined(OS_CHROMEOS) && !defined(OS_ANDROID) && !defined(OS_IOS)
