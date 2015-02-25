899f33e2e679904b0e1cef4055464c217650c18f
rob.buis@samsung.com <rob.buis@samsung.com>
2015-02-25 01:47:44 +0000

    Remove dead code from unit tests
    This code is not in use anymore. The selectionRootBounds
    code was likely  missed in https://codereview.chromium.org/781743002.
    
    BUG=439559
    
    Review URL: https://codereview.chromium.org/952973003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190790 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/PageSerializerTest.cpp
+++ third_party/WebKit/Source/web/tests/PageSerializerTest.cpp
@@ -76,11 +76,6 @@ protected:
         Platform::current()->unitTestSupport()->unregisterAllMockedURLs();
     }
 
-    void setBaseUrl(const char* url)
-    {
-        m_baseUrl = toKURL(url);
-    }
-
     void setBaseFolder(const char* folder)
     {
         m_folder = WebString::fromUTF8(folder);
