80238c2672d424b576b0688aba62707092dd7bf1
johnme <johnme@chromium.org>
2015-02-25 12:23:40 -0800

    Push API: Workaround for GCM lowercasing subtypes
    Temporary workaround for a bug in GCM where it converts subtypes to
    lowercase.
    
    BUG=461867
    
    Review URL: https://codereview.chromium.org/953423003
    
    Cr-Commit-Position: refs/heads/master@{#318102}

--- chrome/browser/services/gcm/push_messaging_application_id.cc
+++ chrome/browser/services/gcm/push_messaging_application_id.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/prefs/scoped_user_pref_update.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
+#include "base/strings/string_util.h"
 #include "base/values.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
@@ -51,11 +52,20 @@ PushMessagingApplicationId PushMessagingApplicationId::Generate(
 // static
 PushMessagingApplicationId PushMessagingApplicationId::Get(
     Profile* profile, const std::string& app_id_guid) {
+  // Workaround crbug.com/461867 in GCM where it converts subtypes to lowercase.
+  // TODO(johnme): Remove this when obsolete
+  const size_t prefix_len = strlen(kPushMessagingApplicationIdPrefix);
+  if (app_id_guid.size() < prefix_len)
+    return PushMessagingApplicationId();
+  std::string uppercase_app_id =
+      app_id_guid.substr(0, prefix_len) +
+      StringToUpperASCII(app_id_guid.substr(prefix_len, std::string::npos));
+
   const base::DictionaryValue* map =
       profile->GetPrefs()->GetDictionary(prefs::kPushMessagingApplicationIdMap);
 
   std::string origin_and_sw_id;
-  if (!map->GetStringWithoutPathExpansion(app_id_guid, &origin_and_sw_id))
+  if (!map->GetStringWithoutPathExpansion(uppercase_app_id, &origin_and_sw_id))
     return PushMessagingApplicationId();
 
   std::vector<std::string> parts;
@@ -69,7 +79,7 @@ PushMessagingApplicationId PushMessagingApplicationId::Get(
   if (!base::StringToInt64(parts[1], &service_worker_registration_id))
     return PushMessagingApplicationId();
 
-  PushMessagingApplicationId application_id(app_id_guid, origin,
+  PushMessagingApplicationId application_id(uppercase_app_id, origin,
                                             service_worker_registration_id);
   DCHECK(application_id.IsValid());
   return application_id;
