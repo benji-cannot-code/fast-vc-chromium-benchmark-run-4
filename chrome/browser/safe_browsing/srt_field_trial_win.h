a4750c60e45051d59792fb2f148a283e10b45e23
robertshield <robertshield@chromium.org>
2015-02-24 18:33:49 -0800

    Add support for direct download of the SRT.
    Fallsback to the DL page if necessary.
    
    BUG=440407
    
    Review URL: https://codereview.chromium.org/926983002
    
    Cr-Commit-Position: refs/heads/master@{#317954}

---
+++ chrome/browser/safe_browsing/srt_field_trial_win.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SAFE_BROWSING_SRT_FIELD_TRIAL_WIN_H_
+#define CHROME_BROWSER_SAFE_BROWSING_SRT_FIELD_TRIAL_WIN_H_
+
+namespace safe_browsing {
+
+// Returns true if this Chrome is in a field trial group which shows the SRT
+// prompt.
+bool IsInSRTPromptFieldTrialGroups();
+
+// Returns the correct SRT download URL for the current field trial.
+const char* GetSRTDownloadURL();
+
+// Returns the value of the incoming SRT seed.
+std::string GetIncomingSRTSeed();
+
+}  // namespace safe_browsing
+
+#endif  // CHROME_BROWSER_SAFE_BROWSING_SRT_FIELD_TRIAL_WIN_H_
