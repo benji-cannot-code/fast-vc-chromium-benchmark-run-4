d3c8a0c75a9363a14b7db107dec294a7d2162fbe
tapted <tapted@chromium.org>
2015-02-24 19:10:01 -0800

    Mac: Make bubble windows (not their controller) responsible for calling removeChildWindow
    InfoBubbleWindow overrides -[NSWindow close] to schedule a fade
    animation instead of closing. Currently, BaseBubbleController calls
    this, but first removes the bubble from its parent NSWindow. This means
    that if the close is due to a click on a browser window, the
    parent->child relationship is already gone, and the clicked window is
    raised on top of the bubble, making it seem to disappear immediately
    rather than fade out.
    
    This changes the flow so that InfoBubbleWindow only removes itself as a
    child before calling [super close]; once the animation is complete.
    
    BUG=459881
    
    Review URL: https://codereview.chromium.org/944533004
    
    Cr-Commit-Position: refs/heads/master@{#317962}

--- chrome/browser/ui/cocoa/base_bubble_controller.mm
+++ chrome/browser/ui/cocoa/base_bubble_controller.mm
@@ -203,9 +203,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   tabStripObserverBridge_.reset();
-
-  NSWindow* window = [self window];
-  [[window parentWindow] removeChildWindow:window];
 }
 
 - (void)windowWillClose:(NSNotification*)notification {
