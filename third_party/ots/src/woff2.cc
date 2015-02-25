3b1d74b5c653b0547bfc22ab5dba7efc395a12cd
ksakamoto <ksakamoto@chromium.org>
2015-02-24 17:38:15 -0800

    Put the latest Brotli code into third_party/brotli
    Before this patch, Brotli decoder was pulled from the
    font-compression-reference repository [1], which is now obsolete.
    This patch updates Brotli to the latest version at github [2],
    checking the code directly into third_party/brotli.
    
    [1] https://code.google.com/p/font-compression-reference/
    [2] https://github.com/google/brotli
    
    BUG=427852
    TEST=trybots
    
    Review URL: https://codereview.chromium.org/948013003
    
    Cr-Commit-Position: refs/heads/master@{#317944}

--- third_party/ots/src/woff2.cc
+++ third_party/ots/src/woff2.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdlib>
 #include <vector>
 
-#include "third_party/brotli/src/brotli/dec/decode.h"
+#include "third_party/brotli/dec/decode.h"
 
 #include "opentype-sanitiser.h"
 #include "ots-memory-stream.h"
