86bde768a749d1f4972d6cf3310d04e24903b523
aelias <aelias@chromium.org>
2015-02-24 21:02:19 -0800

    Blacklist GPU raster and canvas on all PowerVR SGX.
    Unfortunately, an OEM Lollipop build on an SGX 544 shipped without
    fixing the driver bug causing GPU raster to be incorrect, so
    unconditionally blacklist PowerVR SGX GPUs.
    
    BUG=461456
    
    Review URL: https://codereview.chromium.org/949423002
    
    Cr-Commit-Position: refs/heads/master@{#317980}

--- gpu/config/software_rendering_list_json.cc
+++ gpu/config/software_rendering_list_json.cc
@@ -19,7 +19,7 @@ const char kSoftwareRenderingListJson[] = LONG_STRING_CONST(
 {
   "name": "software rendering list",
   // Please update the version number whenever you change this file.
-  "version": "9.17",
+  "version": "9.18",
   "entries": [
     {
       "id": 1,
@@ -1165,6 +1165,19 @@ LONG_STRING_CONST(
         }
       },
       "gl_renderer": "PowerVR Rogue.*",
+      "features": [
+        "accelerated_2d_canvas",
+        "gpu_rasterization"
+      ]
+    },
+    {
+      "id": 105,
+      "description": "GPU raster broken on PowerVR SGX even on Lollipop",
+      "cr_bugs": [461456],
+      "os": {
+        "type": "android"
+      },
+      "gl_renderer": "PowerVR SGX.*",
       "features": [
         "accelerated_2d_canvas",
         "gpu_rasterization"
