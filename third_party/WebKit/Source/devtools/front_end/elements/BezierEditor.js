3cacf6197d761d69177bc512444adf4cac7eb0f3
lushnikov@chromium.org <lushnikov@chromium.org>
2015-02-25 13:32:22 +0000

    DevTools: [CSSP] fix color swatch to switch between color presentation modes
    The patch fixes color swatch in ComputedStylesSidebarPane to switch
    between color presentation modes (hex, rgb..) on mouse click.
    
    R=dgozman, samli
    BUG=461363
    
    Review URL: https://codereview.chromium.org/950373002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190827 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/elements/BezierEditor.js
+++ third_party/WebKit/Source/devtools/front_end/elements/BezierEditor.js
@@ -346,7 +346,7 @@ WebInspector.BezierIcon.prototype = {
     {
         event.consume(true);
 
-        if (!this._stylesPopoverHelper)
+        if (!this.editable() || !this._stylesPopoverHelper)
             return false;
 
         if (this._stylesPopoverHelper.isShowing()) {
