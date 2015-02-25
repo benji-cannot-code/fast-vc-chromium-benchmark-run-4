4c127268d21c68d59f8c867d618bed97de2cf7f0
dgozman@chromium.org <dgozman@chromium.org>
2015-02-25 20:38:18 +0000

    [DevTools] Teach MediaQueryInspector about disabling models while profiling.
    BUG=461810
    
    Review URL: https://codereview.chromium.org/940373004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190855 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/toolbox/MediaQueryInspector.js
+++ third_party/WebKit/Source/devtools/front_end/toolbox/MediaQueryInspector.js
@@ -68,6 +68,7 @@ WebInspector.MediaQueryInspector.prototype = {
         target.cssModel.removeEventListener(WebInspector.CSSStyleModel.Events.StyleSheetRemoved, this._scheduleMediaQueriesUpdate, this);
         target.cssModel.removeEventListener(WebInspector.CSSStyleModel.Events.StyleSheetChanged, this._scheduleMediaQueriesUpdate, this);
         target.cssModel.removeEventListener(WebInspector.CSSStyleModel.Events.MediaQueryResultChanged, this._scheduleMediaQueriesUpdate, this);
+        delete this._target;
     },
 
     /**
@@ -131,6 +132,9 @@ WebInspector.MediaQueryInspector.prototype = {
      */
     _onContextMenu: function(event)
     {
+        if (!this._target || !this._target.cssModel.isEnabled())
+            return;
+
         var mediaQueryMarker = event.target.enclosingNodeOrSelfWithClass("media-inspector-marker");
         if (!mediaQueryMarker)
             return;
@@ -175,7 +179,7 @@ WebInspector.MediaQueryInspector.prototype = {
      */
     _refetchMediaQueries: function(finishCallback)
     {
-        if (!this._enabled) {
+        if (!this._enabled || !this._target) {
             finishCallback();
             return;
         }
