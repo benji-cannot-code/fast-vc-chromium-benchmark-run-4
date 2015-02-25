c3cd44f0ef86433c2eb3d8475328f0826434f39e
amistry <amistry@chromium.org>
2015-02-24 16:46:40 -0800

    Fix linter errors in UtilityProcessHostImpl.
    Review URL: https://codereview.chromium.org/947503003
    
    Cr-Commit-Position: refs/heads/master@{#317932}

--- content/browser/utility_process_host_impl.h
+++ content/browser/utility_process_host_impl.h
@@ -49,7 +49,7 @@ class CONTENT_EXPORT UtilityProcessHostImpl
   void EnableMDns() override;
   void DisableSandbox() override;
 #if defined(OS_WIN)
-  virtual void ElevatePrivileges() override;
+  void ElevatePrivileges() override;
 #endif
   const ChildProcessData& GetData() override;
 #if defined(OS_POSIX)
