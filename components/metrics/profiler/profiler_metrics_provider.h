9674c8dd0e31c4c2713da8124cab91969494c901
gayane <gayane@chromium.org>
2015-02-25 11:16:24 -0800

    Constructor without callback added to ProfilerMetricsProvider.
    As ProfilerMetricsProvider is used in same cases where network connection type is irrelevant, a constructor without the connection type callback is added to support those cases.
    Constructor with a callback was introduced in CL/937263002.
    
    BUG=455847
    
    Review URL: https://codereview.chromium.org/953403002
    
    Cr-Commit-Position: refs/heads/master@{#318086}

--- components/metrics/profiler/profiler_metrics_provider.h
+++ components/metrics/profiler/profiler_metrics_provider.h
@@ -23,6 +23,8 @@ class ProfilerMetricsProvider : public MetricsProvider {
  public:
   explicit ProfilerMetricsProvider(
       const base::Callback<void(bool*)>& cellular_callback);
+  // Creates profiler metrics provider with a null callback.
+  ProfilerMetricsProvider();
   ~ProfilerMetricsProvider() override;
 
   // MetricsDataProvider:
