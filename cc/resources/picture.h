8f121a6e3a115f43453a795fc6ca8aba191cf776
fmalita <fmalita@chromium.org>
2015-02-26 07:48:42 -0800

    Refactor BenchmarkingCanvas to avoid internal Skia dependencies
    Re-implement BenchmarkingCanvas functionality based on public Skia APIs
    only.
    
    Convert Skia types to base::Value() structured data hierarchies (which
    are then pass through to the viewer).
    
    BUG=457691
    R=reed@google.com,robertphillips@google.com,nduca@chromium.org,pdr@chromium.org
    
    Review URL: https://codereview.chromium.org/942533002
    
    Cr-Commit-Position: refs/heads/master@{#318236}

--- cc/resources/picture.h
+++ cc/resources/picture.h
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkPicture.h"
 #include "ui/gfx/geometry/rect.h"
 
-class SkDrawPictureCallback;
 class SkPixelRef;
 
 namespace base {
@@ -71,13 +70,13 @@ class CC_EXPORT Picture
   // raster operation, i.e., the parts of the canvas which will not get drawn
   // to.
   int Raster(SkCanvas* canvas,
-             SkDrawPictureCallback* callback,
+             SkPicture::AbortCallback* callback,
              const Region& negated_content_region,
              float contents_scale) const;
 
   // Draw the picture directly into the given canvas, without applying any
   // clip/scale/layer transformations.
-  void Replay(SkCanvas* canvas);
+  void Replay(SkCanvas* canvas, SkPicture::AbortCallback* callback = NULL);
 
   scoped_ptr<base::Value> AsValue() const;
 
