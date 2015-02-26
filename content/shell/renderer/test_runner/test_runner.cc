f47efa221207147301f08b63b1536082187b4eed
avayvod <avayvod@chromium.org>
2015-02-26 06:19:32 -0800

    Revert of Retry to add MockPresentationClient to use in layout tests. (patchset #4 id:60001 of https://codereview.chromium.org/907893002/)
    Reason for revert:
    While the LayoutTests only test the logic in the mocks and the plumbing, maintaining the mocks while working on the platform/public interface is a pain and requires landing patches simultaneously in Blink and Chromium repositories. There's no layout test using this anyway yet.
    I'll add layout tests later when the Blink-Chromium interface is stable and maybe Blink is merged into Chromium.
    
    Original issue's description:
    > Retry to add MockPresentationClient to use in layout tests.
    >
    > Now there's a MockPresentationService that's owned by WebTestProxy and
    > accepts private method calls from the tests.
    >
    > MockPresentationClient was moved to WebFrameTestProxy instance since
    > we want one per each frame. Reset logic is added too to make tests more
    > independent.
    >
    > BUG=412331
    >
    > Committed: https://crrev.com/57b3af2412a81a4c0abf54e0146c0f7bf5530249
    > Cr-Commit-Position: refs/heads/master@{#316199}
    
    TBR=mkwst@chromium.org,peter@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=412331
    
    Review URL: https://codereview.chromium.org/963493002
    
    Cr-Commit-Position: refs/heads/master@{#318230}

--- content/shell/renderer/test_runner/test_runner.cc
+++ content/shell/renderer/test_runner/test_runner.cc
@@ -210,7 +210,6 @@ class TestRunnerBindings : public gin::Wrappable<TestRunnerBindings> {
                               double dischargingTime,
                               double level);
   void ResetBatteryStatus();
-  void SetMockScreenAvailability(bool available);
   void DidAcquirePointerLock();
   void DidNotAcquirePointerLock();
   void DidLosePointerLock();
@@ -418,8 +417,6 @@ gin::ObjectTemplateBuilder TestRunnerBindings::GetObjectTemplateBuilder(
       .SetMethod("didChangeBatteryStatus",
                  &TestRunnerBindings::DidChangeBatteryStatus)
       .SetMethod("resetBatteryStatus", &TestRunnerBindings::ResetBatteryStatus)
-      .SetMethod("setMockScreenAvailability",
-                 &TestRunnerBindings::SetMockScreenAvailability)
       .SetMethod("didAcquirePointerLock",
                  &TestRunnerBindings::DidAcquirePointerLock)
       .SetMethod("didNotAcquirePointerLock",
@@ -973,11 +970,6 @@ void TestRunnerBindings::DidChangeBatteryStatus(bool charging,
 void TestRunnerBindings::ResetBatteryStatus() {
   if (runner_)
     runner_->ResetBatteryStatus();
-}
-
-void TestRunnerBindings::SetMockScreenAvailability(bool available) {
-  if (runner_)
-    runner_->SetMockScreenAvailability(available);
 }
 
 void TestRunnerBindings::DidAcquirePointerLock() {
@@ -1650,7 +1642,6 @@ void TestRunner::Reset() {
     delegate_->ResetScreenOrientation();
     delegate_->SetBluetoothMockDataSet("");
     delegate_->ClearGeofencingMockProvider();
-    delegate_->ResetPresentationService();
     ResetBatteryStatus();
     ResetDeviceLight();
   }
@@ -2469,10 +2460,6 @@ void TestRunner::DidChangeBatteryStatus(bool charging,
 void TestRunner::ResetBatteryStatus() {
   blink::WebBatteryStatus status;
   delegate_->DidChangeBatteryStatus(status);
-}
-
-void TestRunner::SetMockScreenAvailability(bool available) {
-  delegate_->SetScreenAvailability(available);
 }
 
 void TestRunner::DidAcquirePointerLock() {
--- content/shell/renderer/test_runner/web_test_proxy.h
+++ content/shell/renderer/test_runner/web_test_proxy.h
@@ -77,7 +77,6 @@ typedef unsigned WebColor;
 namespace content {
 
 class MockCredentialManagerClient;
-class MockPresentationService;
 class MockScreenOrientationClient;
 class MockWebSpeechRecognizer;
 class MockWebUserMediaClient;
@@ -133,7 +132,6 @@ class WebTestProxyBase {
   blink::WebMIDIClientMock* GetMIDIClientMock();
   MockWebSpeechRecognizer* GetSpeechRecognizerMock();
   MockCredentialManagerClient* GetCredentialManagerClientMock();
-  MockPresentationService* GetPresentationServiceMock();
 
   WebTaskList* mutable_task_list() { return &task_list_; }
 
@@ -272,7 +270,6 @@ class WebTestProxyBase {
   scoped_ptr<blink::WebMIDIClientMock> midi_client_;
   scoped_ptr<MockWebSpeechRecognizer> speech_recognizer_;
   scoped_ptr<MockScreenOrientationClient> screen_orientation_client_;
-  scoped_ptr<MockPresentationService> presentation_service_;
 
   std::string accept_languages_;
 
