6ebaf5cad602bfb7ae9ea93df4961dc79de13e8a
peter <peter@chromium.org>
2015-02-26 13:39:40 -0800

    Add two instrumentation tests for Web Notifications on Android.
    Introduce two tests for validating that Web Notifications on Android have
    the expected contents, appearance and behavior, and that notifications
    which have an icon supplied get this icon fetched and included in the
    notification as a Bitmap.
    
    BUG=462251
    
    Review URL: https://codereview.chromium.org/960923004
    
    Cr-Commit-Position: refs/heads/master@{#318319}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationUIManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationUIManager.java
@@ -19,6 +19,7 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import org.chromium.base.CalledByNative;
+import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.preferences.Preferences;
 import org.chromium.chrome.browser.preferences.PreferencesLauncher;
@@ -40,11 +41,12 @@ public class NotificationUIManager {
     private static final int NOTIFICATION_TEXT_SIZE_DP = 28;
 
     private static NotificationUIManager sInstance;
+    private static NotificationManagerProxy sNotificationManagerOverride;
 
     private final long mNativeNotificationManager;
 
     private final Context mAppContext;
-    private final NotificationManager mNotificationManager;
+    private final NotificationManagerProxy mNotificationManager;
 
     private RoundedIconGenerator mIconGenerator;
 
@@ -66,12 +68,29 @@ public class NotificationUIManager {
         return sInstance;
     }
 
+    /**
+     * Overrides the notification manager which is to be used for displaying Notifications on the
+     * Android framework. Should only be used for testing. Tests are expected to clean up after
+     * themselves by setting this to NULL again.
+     *
+     * @param proxy The notification manager instance to use instead of the system's.
+     */
+    @VisibleForTesting
+    public static void overrideNotificationManagerForTesting(
+            NotificationManagerProxy notificationManager) {
+        sNotificationManagerOverride = notificationManager;
+    }
+
     private NotificationUIManager(long nativeNotificationManager, Context context) {
         mNativeNotificationManager = nativeNotificationManager;
         mAppContext = context.getApplicationContext();
 
-        mNotificationManager = (NotificationManager)
-                mAppContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (sNotificationManagerOverride != null) {
+            mNotificationManager = sNotificationManagerOverride;
+        } else {
+            mNotificationManager = new NotificationManagerProxyImpl(
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE));
+        }
 
         mLastNotificationId = 0;
     }
