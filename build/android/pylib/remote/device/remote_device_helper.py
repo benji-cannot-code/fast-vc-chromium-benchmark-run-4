3ffda83fe3cb977342675f36cf11fc22b1bc458e
mikecase <mikecase@chromium.org>
2015-02-26 15:08:22 -0800

    Add exit code for infra failures to testrunner script.
    For recipes to know a failure in the testrunner was caused by
    an infrastructure failure, we are adding a specific exit code.
    
    BUG=451993
    
    Review URL: https://codereview.chromium.org/950893004
    
    Cr-Commit-Position: refs/heads/master@{#318341}

--- build/android/pylib/remote/device/remote_device_helper.py
+++ build/android/pylib/remote/device/remote_device_helper.py
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Common functions and Exceptions for remote_device_*"""
 
-class RemoteDeviceError(Exception):
+from pylib.utils import base_error
+
+
+class RemoteDeviceError(base_error.BaseError):
   """Exception to throw when problems occur with remote device service."""
   pass
 
