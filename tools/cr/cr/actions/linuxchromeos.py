3ca9f76d446cc451e5bf5491ed2014ce512c9f05
tsergeant <tsergeant@chromium.org>
2015-02-26 15:01:31 -0800

    cr: Add support for run to LinuxChromeOS
    This allows LinuxChromeOS to use LinuxRunner and LinuxInstaller to
    launch the ChromeOS binary. Also fixes minor issue with selecting a
    runner from the command line.
    
    BUG=NONE
    
    Review URL: https://codereview.chromium.org/915853003
    
    Cr-Commit-Position: refs/heads/master@{#318337}

---
+++ tools/cr/cr/actions/linuxchromeos.py
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Linux ChromeOS specific implementations."""
+
+import cr
+
+
+class LinuxChromeOSRunner(cr.LinuxRunner):
+  """A version of cr.LinuxRunner for LinuxChromeOS.
+
+  Running ChromeOS in Linux is the same as a normal linux Chrome build -- just
+  executing the output binary.
+  """
+
+  @property
+  def enabled(self):
+    return cr.LinuxChromeOSPlatform.GetInstance().is_active
+
+
+class LinuxChromeOSInstaller(cr.LinuxInstaller):
+  """A version of cr.LinuxInstaller for LinuxChromeOS.
+
+  This does nothing, as there is nothing to be installed.
+  """
+
+  @property
+  def enabled(self):
+    return cr.LinuxChromeOSPlatform.GetInstance().is_active
--- tools/cr/cr/actions/runner.py
+++ tools/cr/cr/actions/runner.py
@@ -31,6 +31,7 @@ class Runner(cr.Action, cr.Plugin.Type):
             Set to 'no' to force it to not be a test.
             """
     )
+    cls.AddSelectorArg(command, parser)
 
   @cr.Plugin.activemethod
   def Kill(self, targets, arguments):
