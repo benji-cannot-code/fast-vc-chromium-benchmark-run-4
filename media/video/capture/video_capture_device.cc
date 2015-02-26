8e08eb443b5b385124d2d08fb338b83d928f8c9b
mcasas <mcasas@chromium.org>
2015-02-25 20:12:29 -0800

    Revert: Issue 545053002: Add still image capture interface for VideoCaptureDevice.
    These parts are not used ATM, this CL removes them until further notice.
    
    > Add still image capture interface for VideoCaptureDevice.
    >
    > BUG=413086
    > TEST=Run the media_unittests on peach_pit.
    >
    > Committed: https://crrev.com/fd5bdd2688768683e30b27941f181597a849656d
    > Cr-Commit-Position: refs/heads/master@{#298009}
    
    TBR=posciak@chromium.org, perkj@chromium.org, tommi@chromium.org
    
    BUG=413086, 440843
    
    Review URL: https://codereview.chromium.org/921683004
    
    Cr-Commit-Position: refs/heads/master@{#318183}

--- media/video/capture/video_capture_device.cc
+++ media/video/capture/video_capture_device.cc
@@ -110,10 +110,4 @@ int VideoCaptureDevice::GetPowerLineFrequencyForLocation() const {
   return kPowerLine60Hz;
 }
 
-bool VideoCaptureDevice::InitializeImageCapture(
-    const ImageCaptureFormat& image_format,
-    scoped_ptr<ImageClient> client) {
-  return false;
-}
-
 }  // namespace media
