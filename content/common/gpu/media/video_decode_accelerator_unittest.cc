ac6f06f5366c6767ab027eb65fdb1dc15a73303f
Steven Bennetts <stevenjb@chromium.org>
2015-02-26 11:43:31 -0800

    Fix the broken include in video_decode_accelerator_unittest
    BUG=62316
    TBR=emircan@chromium.org
    
    Review URL: https://codereview.chromium.org/964573002
    
    Cr-Commit-Position: refs/heads/master@{#318287}

--- content/common/gpu/media/video_decode_accelerator_unittest.cc
+++ content/common/gpu/media/video_decode_accelerator_unittest.cc
@@ -62,9 +62,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/gpu/media/dxva_video_decode_accelerator.h"
 #elif defined(OS_CHROMEOS)
 #if defined(USE_V4L2_CODEC)
+#include "content/common/gpu/media/v4l2_device.h"
 #include "content/common/gpu/media/v4l2_slice_video_decode_accelerator.h"
 #include "content/common/gpu/media/v4l2_video_decode_accelerator.h"
-#include "content/common/gpu/media/v4l2_video_device.h"
 #endif
 #if defined(ARCH_CPU_X86_FAMILY)
 #include "content/common/gpu/media/vaapi_video_decode_accelerator.h"
