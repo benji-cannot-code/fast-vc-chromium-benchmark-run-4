benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit dc5701bdda10922a096669579c7d8f99f3345aa9

Merge several bits of state into Frame::m_isLoading
RemoteFrame::m_isLoading, ProgressTracker::m_inProgress, and FrameLoader::m_state
all track the same basic concept, whether this frame is loading something
according to the embedder's perspective. In the case of FrameLoader::m_state,
it also stores information as to whether a provisional load is in progress, but
that is adequately track in other places. Merge these variables into one.

BUG=426520

Review URL: https://codereview.chromium.org/843573002

git-svn-id: svn://svn.chromium.org/blink/trunk@188017 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
@@ -1048,7 +1048,7 @@ void LocalDOMWindow::print()
     if (!host)
         return;
 
-    if (frame()->loader().state() != FrameStateComplete) {
+    if (!frame()->document()->loadEventFinished()) {
         m_shouldPrintWhenFinishedLoading = true;
         return;
     }

third_party/WebKit/Source/core/dom/Document.cpp
@@ -2347,7 +2347,7 @@ void Document::open(Document* ownerDocument, ExceptionState& exceptionState)
             }
         }
 
-        if (m_frame->loader().state() == FrameStateProvisional)
+        if (m_frame->loader().provisionalDocumentLoader())
             m_frame->loader().stopAllLoaders();
     }
 
@@ -4418,8 +4418,8 @@ KURL Document::openSearchDescriptionURL()
     if (!frame() || frame()->tree().parent())
         return KURL();
 
-    // FIXME: Why do we need to wait for FrameStateComplete?
-    if (frame()->loader().state() != FrameStateComplete)
+    // FIXME: Why do we need to wait for load completion?
+    if (!loadEventFinished())
         return KURL();
 
     if (!head())

third_party/WebKit/Source/core/frame/Frame.cpp
@@ -194,6 +194,7 @@ Frame::Frame(FrameClient* client, FrameHost* host, FrameOwner* owner)
     , m_owner(owner)
     , m_client(client)
     , m_remotePlatformLayer(0)
+    , m_isLoading(false)
 {
     ASSERT(page());
 

third_party/WebKit/Source/core/frame/Frame.h
@@ -105,6 +105,13 @@ public:
 
     virtual bool checkLoadComplete() = 0;
 
+    // isLoading() is true when the embedder should think a load is in progress.
+    // In the case of LocalFrames, it means that the frame has sent a didStartLoading()
+    // callback, but not the matching didStopLoading(). Inside blink, you probably
+    // want Document::loadEventFinished() instead.
+    void setIsLoading(bool isLoading) { m_isLoading = isLoading; }
+    bool isLoading() const { return m_isLoading; }
+
 protected:
     Frame(FrameClient*, FrameHost*, FrameOwner*);
 
@@ -116,6 +123,8 @@ protected:
 private:
     FrameClient* m_client;
     WebLayer* m_remotePlatformLayer;
+
+    bool m_isLoading;
 };
 
 inline FrameClient* Frame::client() const

third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -2383,7 +2383,7 @@ void FrameView::updateScrollableAreaSet()
 
 bool FrameView::shouldSuspendScrollAnimations() const
 {
-    return m_frame->loader().state() != FrameStateComplete;
+    return !m_frame->document()->loadEventFinished();
 }
 
 void FrameView::scrollbarStyleChanged()

third_party/WebKit/Source/core/frame/RemoteFrame.cpp
@@ -19,7 +19,6 @@ inline RemoteFrame::RemoteFrame(RemoteFrameClient* client, FrameHost* host, Fram
     : Frame(client, host, owner)
     , m_securityContext(RemoteSecurityContext::create())
     , m_domWindow(RemoteDOMWindow::create(*this))
-    , m_isLoading(false)
 {
 }
 
@@ -74,7 +73,7 @@ RemoteSecurityContext* RemoteFrame::securityContext() const
 
 bool RemoteFrame::checkLoadComplete()
 {
-    if (m_isLoading)
+    if (isLoading())
         return false;
 
     bool allChildrenAreDoneLoading = true;

third_party/WebKit/Source/core/frame/RemoteFrame.h
@@ -38,7 +38,6 @@ public:
 
     void setView(PassRefPtrWillBeRawPtr<RemoteFrameView>);
     void createView();
-    void setIsLoading(bool isLoading) { m_isLoading = isLoading; }
 
     RemoteFrameView* view() const;
 
@@ -51,8 +50,6 @@ private:
     RefPtrWillBeMember<RemoteFrameView> m_view;
     RefPtr<RemoteSecurityContext> m_securityContext;
     RefPtrWillBeMember<RemoteDOMWindow> m_domWindow;
-
-    bool m_isLoading;
 };
 
 inline RemoteFrameView* RemoteFrame::view() const

third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -115,7 +115,6 @@ FrameLoader::FrameLoader(LocalFrame* frame)
     : m_frame(frame)
     , m_mixedContentChecker(frame)
     , m_progressTracker(ProgressTracker::create(frame))
-    , m_state(FrameStateProvisional)
     , m_loadType(FrameLoadTypeStandard)
     , m_fetchContext(FrameFetchContext::create(frame))
     , m_inStopAllLoaders(false)
@@ -951,7 +950,6 @@ void FrameLoader::notifyIfInitialDocumentAccessed()
 void FrameLoader::commitProvisionalLoad()
 {
     ASSERT(client()->hasWebView());
-    ASSERT(m_state == FrameStateProvisional);
     RefPtr<DocumentLoader> pdl = m_provisionalDocumentLoader;
     RefPtrWillBeRawPtr<LocalFrame> protect(m_frame.get());
 
@@ -975,7 +973,6 @@ void FrameLoader::commitProvisionalLoad()
     if (m_documentLoader)
         m_documentLoader->detachFromFrame();
     m_documentLoader = m_provisionalDocumentLoader.release();
-    m_state = FrameStateCommittedPage;
 
     if (isLoadingMainFrame())
         m_frame->page()->chrome().client().needTouchEvents(false);
@@ -1014,7 +1011,7 @@ bool FrameLoader::checkLoadCompleteForThisFrame()
         allChildrenAreDoneLoading &= child->checkLoadComplete();
     }
 
-    if (m_state == FrameStateProvisional && m_provisionalDocumentLoader) {
+    if (m_provisionalDocumentLoader) {
         const ResourceError& error = m_provisionalDocumentLoader->mainDocumentError();
         if (error.isNull())
             return false;
@@ -1025,7 +1022,6 @@ bool FrameLoader::checkLoadCompleteForThisFrame()
         m_provisionalDocumentLoader->detachFromFrame();
         m_provisionalDocumentLoader = nullptr;
         m_progressTracker->progressCompleted();
-        m_state = FrameStateComplete;
         checkCompleted();
         return true;
     }
@@ -1033,16 +1029,14 @@ bool FrameLoader::checkLoadCompleteForThisFrame()
     if (!allChildrenAreDoneLoading)
         return false;
 
-    if (m_state == FrameStateComplete)
+    if (!m_frame->isLoading())
         return true;
     if (m_provisionalDocumentLoader || !m_documentLoader)
         return false;
     if (!m_frame->document()->loadEventFinished())
         return false;
 
-    m_state = FrameStateComplete;
-
-    // Retry restoring scroll offset since FrameStateComplete disables content
+    // Retry restoring scroll offset since finishing the load event disables content
     // size clamping.
     restoreScrollPositionAndViewState();
 
@@ -1082,7 +1076,7 @@ void FrameLoader::restoreScrollPositionAndViewState()
     // height.
     float mainFrameScale = m_frame->settings()->pinchVirtualViewportEnabled() ? 1 : m_currentItem->pageScaleFactor();
     bool canRestoreWithoutClamping = view->clampOffsetAtScale(m_currentItem->scrollPoint(), mainFrameScale) == m_currentItem->scrollPoint();
-    bool canRestoreWithoutAnnoyingUser = !view->wasScrolledByUser() && (canRestoreWithoutClamping || m_state == FrameStateComplete);
+    bool canRestoreWithoutAnnoyingUser = !view->wasScrolledByUser() && (canRestoreWithoutClamping || m_frame->document()->loadEventFinished());
     if (!canRestoreWithoutAnnoyingUser)
         return;
 
@@ -1293,8 +1287,6 @@ void FrameLoader::startLoad(FrameLoadRequest& frameLoadRequest, FrameLoadType ty
     if ((!m_policyDocumentLoader->shouldContinueForNavigationPolicy(request, frameLoadRequest.shouldCheckMainWorldContentSecurityPolicy(), navigationPolicy, isTransitionNavigation) || !shouldClose()) && m_policyDocumentLoader) {
         m_policyDocumentLoader->detachFromFrame();
         m_policyDocumentLoader = nullptr;
-        if (!m_stateMachine.committedFirstRealDocumentLoad())
-            m_state = FrameStateComplete;
         checkCompleted();
         return;
     }
@@ -1318,7 +1310,6 @@ void FrameLoader::startLoad(FrameLoadRequest& frameLoadRequest, FrameLoadType ty
 
     m_provisionalDocumentLoader = m_policyDocumentLoader.release();
     m_loadType = type;
-    m_state = FrameStateProvisional;
 
     if (FormState* formState = frameLoadRequest.formState())
         client()->dispatchWillSubmitForm(formState->form());

commit a9017cf32a011e74daf3181846da97e24a015683

Fix print preview sometimes not appearing
LocalDOMWindow::print() should use the embedder-definition of load completion
(Frame::isLoading), rather than the JS defintion (Document::loadEventFinished).
They're subtly different during the period immediately after load event
completion.

This was regressed in r188017

BUG=449911

Review URL: https://codereview.chromium.org/928593003

git-svn-id: svn://svn.chromium.org/blink/trunk@190941 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
@@ -961,7 +961,7 @@ void LocalDOMWindow::print()
     if (!host)
         return;
 
-    if (!frame()->document()->loadEventFinished()) {
+    if (frame()->isLoading()) {
         m_shouldPrintWhenFinishedLoading = true;
         return;
     }