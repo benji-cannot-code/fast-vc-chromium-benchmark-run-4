4399a83396878a4c31a7528842d499a5e07faab6
dgozman <dgozman@chromium.org>
2015-02-27 05:13:44 -0800

    Revert of Disable elastic overscroll effect in DevTools. (patchset #1 id:1 of https://codereview.chromium.org/925903002/)
    Reason for revert:
    Turned out this patch is unnecessary.
    
    Original issue's description:
    > Disable elastic overscroll effect in DevTools.
    >
    > BUG=456546
    >
    > Committed: https://crrev.com/5210d618e08fc4a1ce3ab0953d7fa42a98fc8140
    > Cr-Commit-Position: refs/heads/master@{#316834}
    
    TBR=pfeldman@chromium.org,ccameron@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=456546
    
    Review URL: https://codereview.chromium.org/963743003
    
    Cr-Commit-Position: refs/heads/master@{#318440}

--- content/browser/devtools/devtools_frontend_host_impl.cc
+++ content/browser/devtools/devtools_frontend_host_impl.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/devtools/devtools_frontend_host_impl.h"
 
-#include "content/browser/frame_host/render_frame_host_impl.h"
-#include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/common/devtools_messages.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/render_frame_host.h"
@@ -32,11 +30,6 @@ DevToolsFrontendHostImpl::DevToolsFrontendHostImpl(
 }
 
 DevToolsFrontendHostImpl::~DevToolsFrontendHostImpl() {
-}
-
-void DevToolsFrontendHostImpl::RenderFrameCreated(RenderFrameHost* rfh) {
-  static_cast<RenderFrameHostImpl*>(rfh)->GetRenderWidgetHost()->
-      DisableElasticOverscroll();
 }
 
 bool DevToolsFrontendHostImpl::OnMessageReceived(
