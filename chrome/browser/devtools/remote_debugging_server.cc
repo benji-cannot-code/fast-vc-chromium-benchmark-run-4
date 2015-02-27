855964164e122fc76afde0b0fabec5deff34f247
rchlodnicki <rchlodnicki@opera.com>
2015-02-27 03:45:25 -0800

    Remove include for DevToolsUI class which is no longer used in this file
    R=yurys@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/933293002
    
    Cr-Commit-Position: refs/heads/master@{#318433}

--- chrome/browser/devtools/remote_debugging_server.cc
+++ chrome/browser/devtools/remote_debugging_server.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
 #include "chrome/browser/devtools/browser_list_tabcontents_provider.h"
-#include "chrome/browser/ui/webui/devtools_ui.h"
 #include "chrome/common/chrome_paths.h"
 #include "content/public/browser/devtools_http_handler.h"
 #include "net/base/net_errors.h"
