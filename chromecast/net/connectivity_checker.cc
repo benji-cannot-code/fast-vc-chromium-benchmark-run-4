f961c981bd1107317e2dd2b01f8e442b83553a8f
derekjchow <derekjchow@chromium.org>
2015-02-27 12:19:50 -0800

    Perform more aggressive connectivity checks.
    Perform connectivity check even if offline. NetworkChangeNotifier
    does not accuratly report IsOffline on some platforms.
    
    R=gunsch@chromium.org,wzhong@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/963163003
    
    Cr-Commit-Position: refs/heads/master@{#318502}

--- chromecast/net/connectivity_checker.cc
+++ chromecast/net/connectivity_checker.cc
@@ -61,10 +61,8 @@ void ConnectivityChecker::Initialize() {
   url_request_context_.reset(builder.Build());
 
   net::NetworkChangeNotifier::AddConnectionTypeObserver(this);
-  if (!net::NetworkChangeNotifier::IsOffline()) {
-    loop_proxy_->PostTask(FROM_HERE,
-                          base::Bind(&ConnectivityChecker::Check, this));
-  }
+  loop_proxy_->PostTask(FROM_HERE,
+                        base::Bind(&ConnectivityChecker::Check, this));
 }
 
 ConnectivityChecker::~ConnectivityChecker() {
@@ -119,15 +117,11 @@ void ConnectivityChecker::Check() {
 
 void ConnectivityChecker::OnConnectionTypeChanged(
     net::NetworkChangeNotifier::ConnectionType type) {
-  Cancel();
-
-  // If there is no connection, set connectivity to false. Otherwise retest
-  // connectivity
-  if (type == net::NetworkChangeNotifier::CONNECTION_NONE) {
+  if (type == net::NetworkChangeNotifier::CONNECTION_NONE)
     SetConnectivity(false);
-  } else {
-    Check();
-  }
+
+  Cancel();
+  Check();
 }
 
 void ConnectivityChecker::OnResponseStarted(net::URLRequest* request) {
@@ -155,11 +149,9 @@ void ConnectivityChecker::OnResponseStarted(net::URLRequest* request) {
   }
 
   // Check again
-  if (!net::NetworkChangeNotifier::IsOffline()) {
-    loop_proxy_->PostDelayedTask(
-        FROM_HERE, base::Bind(&ConnectivityChecker::Check, this),
-        base::TimeDelta::FromSeconds(kConnectivityPeriodSeconds));
-  }
+  loop_proxy_->PostDelayedTask(
+      FROM_HERE, base::Bind(&ConnectivityChecker::Check, this),
+      base::TimeDelta::FromSeconds(kConnectivityPeriodSeconds));
 }
 
 void ConnectivityChecker::OnReadCompleted(net::URLRequest* request,
