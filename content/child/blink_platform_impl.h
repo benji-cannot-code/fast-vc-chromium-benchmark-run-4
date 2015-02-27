3cb2de57da4e7a2971b2c467e70ca51e0b04a944
Sadrul Habib Chowdhury <sadrul@chromium.org>
2015-02-27 11:13:27 -0500

    chrome: The last step of removing WebWorkerRunLoop.
    Part 1: https://codereview.chromium.org/937313003
    Part 2: https://codereview.chromium.org/960443002
    
    BUG=461664
    R=jochen@chromium.org, kinuko@chromium.org
    
    Review URL: https://codereview.chromium.org/955883002
    
    Cr-Commit-Position: refs/heads/master@{#318459}

--- content/child/blink_platform_impl.h
+++ content/child/blink_platform_impl.h
@@ -32,10 +32,6 @@ namespace base {
 class MessageLoop;
 }
 
-namespace blink {
-class WebWorkerRunLoop;
-}
-
 namespace content {
 class FlingCurveConfiguration;
 class NotificationDispatcher;
@@ -154,10 +150,6 @@ class CONTENT_EXPORT BlinkPlatformImpl
       blink::WebGestureDevice device_source,
       const blink::WebFloatPoint& velocity,
       const blink::WebSize& cumulative_scroll);
-  virtual void didStartWorkerRunLoop(
-      const blink::WebWorkerRunLoop& runLoop);
-  virtual void didStopWorkerRunLoop(
-      const blink::WebWorkerRunLoop& runLoop);
   virtual void didStartWorkerRunLoop();
   virtual void didStopWorkerRunLoop();
   virtual blink::WebCrypto* crypto();
