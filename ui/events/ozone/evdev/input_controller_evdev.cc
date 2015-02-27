84a7e61eecfd1cfca7b5f42525dac295a690c6d8
spang <spang@chromium.org>
2015-02-27 14:43:37 -0800

    ozone: evdev: Release pressed keys when event filtering starts
    This prevents filtering out key release events, resulting in stuck keys.
    
    BUG=449162
    TEST=Fully rotate glimmer while holding spacebar. No spurious
      key repeats.
    
    Review URL: https://codereview.chromium.org/958403003
    
    Cr-Commit-Position: refs/heads/master@{#318535}

--- ui/events/ozone/evdev/input_controller_evdev.cc
+++ ui/events/ozone/evdev/input_controller_evdev.cc
@@ -97,6 +97,9 @@ void InputControllerEvdev::DisableInternalKeyboardExceptKeys(
   if (input_device_factory_) {
     input_device_factory_->DisableInternalKeyboardExceptKeys(
         excepted_keys.Pass());
+
+    // Release keys since we may block key up events.
+    keyboard_->Reset();
   }
 }
 
