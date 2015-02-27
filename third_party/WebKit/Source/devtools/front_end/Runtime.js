9e0083f72e81d85e14fe32f430427d724c299a0b
dgozman@chromium.org <dgozman@chromium.org>
2015-02-27 17:20:32 +0000

    [DevTools] Support remote (non-bundled) modules.
    This patch does not add a single remote module yet.
    To make module remote, just mark it as "type": "remote", and remove
    from the resources list (to not bundle).
    
    Layout tests use locally served modules from /inspector-sources alias.
    
    BUG=459167
    
    Review URL: https://codereview.chromium.org/933693002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191012 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/Runtime.js
+++ third_party/WebKit/Source/devtools/front_end/Runtime.js
@@ -108,9 +108,10 @@ function normalizePath(path)
 
 /**
  * @param {!Array.<string>} scriptNames
+ * @param {string=} base
  * @return {!Promise.<undefined>}
  */
-function loadScriptsPromise(scriptNames)
+function loadScriptsPromise(scriptNames, base)
 {
     /** @type {!Array.<!Promise.<string>>} */
     var promises = [];
@@ -120,7 +121,7 @@ function loadScriptsPromise(scriptNames)
     var scriptToEval = 0;
     for (var i = 0; i < scriptNames.length; ++i) {
         var scriptName = scriptNames[i];
-        var sourceURL = self._importScriptPathPrefix + scriptName;
+        var sourceURL = (base || self._importScriptPathPrefix) + scriptName;
         var schemaIndex = sourceURL.indexOf("://") + 3;
         sourceURL = sourceURL.substring(0, schemaIndex) + normalizePath(sourceURL.substring(schemaIndex));
         if (_loadedScripts[sourceURL])
@@ -366,7 +367,16 @@ Runtime._assert = function(value, message)
     Runtime._originalAssert.call(Runtime._console, value, message);
 }
 
+/**
+ * @type {string|undefined}
+ */
+Runtime._testBase;
+
 Runtime.prototype = {
+    useTestBase: function()
+    {
+        Runtime._testBase = "http://localhost:8000/inspector-sources/";
+    },
 
     /**
      * @param {!Runtime.ModuleDescriptor} descriptor
@@ -580,6 +590,11 @@ Runtime.ModuleDescriptor = function()
      * @type {!Array.<string>}
      */
     this.scripts;
+
+    /**
+     * @type {string|undefined}
+     */
+    this.base;
 }
 
 /**
@@ -722,8 +737,12 @@ Runtime.Module.prototype = {
         if (!this._descriptor.scripts)
             return Promise.resolve();
 
-        if (Runtime.isReleaseMode())
-            return loadScriptsPromise([this._name + "_module.js"]);
+        if (Runtime.isReleaseMode()) {
+            var base = this._descriptor.base;
+            if (base && Runtime._testBase)
+                base = Runtime._testBase;
+            return loadScriptsPromise([this._name + "_module.js"], base);
+        }
 
         return loadScriptsPromise(this._descriptor.scripts.map(this._modularizeURL, this));
     },
