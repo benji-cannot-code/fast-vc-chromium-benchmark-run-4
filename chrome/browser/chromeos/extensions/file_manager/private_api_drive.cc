ed993fbe8173fe44ca7442febd93b7cb83a2e2cc
mtomasz <mtomasz@chromium.org>
2015-02-27 03:05:28 -0800

    Update Styx URLs for thumbnails and downloads in Files app.
    This patch updates the URLs for Drive files to match the recent requirements.
    
    TBR=kinaba@chromium.org
    TEST=unit_tests: *DriveApiUrlGeneratorTest*
    BUG=None
    
    Review URL: https://codereview.chromium.org/831663009
    
    Cr-Commit-Position: refs/heads/master@{#318432}

--- chrome/browser/chromeos/extensions/file_manager/private_api_drive.cc
+++ chrome/browser/chromeos/extensions/file_manager/private_api_drive.cc
@@ -33,8 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/signin_manager.h"
 #include "content/public/browser/browser_thread.h"
 #include "google_apis/drive/auth_service.h"
+#include "google_apis/drive/drive_api_url_generator.h"
 #include "storage/common/fileapi/file_system_info.h"
 #include "storage/common/fileapi/file_system_util.h"
+#include "url/gurl.h"
 
 using content::BrowserThread;
 
@@ -49,6 +51,7 @@ using file_manager::util::EntryDefinitionList;
 using file_manager::util::EntryDefinitionListCallback;
 using file_manager::util::FileDefinition;
 using file_manager::util::FileDefinitionList;
+using google_apis::DriveApiUrlGenerator;
 
 namespace extensions {
 namespace {
@@ -85,9 +88,14 @@ void FillEntryPropertiesValueForDrive(const drive::ResourceEntry& entry_proto,
       entry_proto.file_specific_info();
 
   if (!entry_proto.resource_id().empty()) {
-    properties->thumbnail_url.reset(
-        new std::string("https://www.googledrive.com/thumb/" +
-                        entry_proto.resource_id() + "?width=500&height=500"));
+    DriveApiUrlGenerator url_generator(
+        (GURL(google_apis::DriveApiUrlGenerator::kBaseUrlForProduction)),
+        (GURL(
+            google_apis::DriveApiUrlGenerator::kBaseDownloadUrlForProduction)));
+    properties->thumbnail_url.reset(new std::string(
+        url_generator.GetThumbnailUrl(entry_proto.resource_id(),
+                                      500 /* width */,
+                                      500 /* height */).spec()));
   }
   if (file_specific_info.has_image_width()) {
     properties->image_width.reset(
