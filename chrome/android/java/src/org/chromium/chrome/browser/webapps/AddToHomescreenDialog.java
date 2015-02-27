6680f6571ce14d93a018f6ec5817da19571fb12d
mlamouri <mlamouri@chromium.org>
2015-02-27 10:18:40 -0800

    Don't focus text field in AddToHomescreenDialog.
    It prevents the keyboard to take the entire screen in some situations,
    for example when a phone is held in landscape mode.
    
    BUG=456802
    
    Review URL: https://codereview.chromium.org/960483003
    
    Cr-Commit-Position: refs/heads/master@{#318484}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/AddToHomescreenDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/AddToHomescreenDialog.java
@@ -20,7 +20,6 @@ import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ShortcutHelper;
 import org.chromium.chrome.browser.Tab;
-import org.chromium.ui.UiUtils;
 
 /**
  * Helper class showing the UI regarding Add to Homescreen and delegate the
@@ -76,14 +75,6 @@ public class AddToHomescreenDialog {
 
                 input.setEnabled(true);
                 input.setText(title);
-                input.setSelection(0, title.length());
-                input.requestFocus();
-                input.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        UiUtils.showKeyboard(input);
-                    }
-                });
             }
         });
 
