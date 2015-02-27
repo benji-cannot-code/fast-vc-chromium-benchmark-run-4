8e3cfc34e83a25e98401e787e0a09d0be155bb3c
merkulova <merkulova@chromium.org>
2015-02-27 02:47:09 -0800

    Rename for js pod description to LegacySupervisedUser finished.
    BUG=461621
    
    Review URL: https://codereview.chromium.org/960403003
    
    Cr-Commit-Position: refs/heads/master@{#318430}

--- ui/login/account_picker/user_pod_row.js
+++ ui/login/account_picker/user_pod_row.js
@@ -1027,7 +1027,9 @@ cr.define('login', function() {
           loadTimeData.getStringF('ownerUserPattern', this.user_.displayName) :
           this.user_.displayName;
       this.actionBoxMenuTitleEmailElement.textContent = this.user_.emailAddress;
-      this.actionBoxMenuTitleEmailElement.hidden = this.user_.supervisedUser;
+
+      this.actionBoxMenuTitleEmailElement.hidden =
+          this.user_.legacySupervisedUser;
 
       this.actionBoxMenuCommandElement.textContent =
           loadTimeData.getString('removeUser');
@@ -1266,7 +1268,7 @@ cr.define('login', function() {
     },
 
     showSupervisedUserSigninWarning: function() {
-      // Supervised user token has been invalidated.
+      // Legacy supervised user token has been invalidated.
       // Make sure that pod is focused i.e. "Sign in" button is seen.
       this.parentNode.focusPod(this);
 
@@ -1300,7 +1302,7 @@ cr.define('login', function() {
      * Shows signin UI for this user.
      */
     showSigninUI: function() {
-      if (this.user.supervisedUser && !this.user.isDesktopUser) {
+      if (this.user.legacySupervisedUser && !this.user.isDesktopUser) {
         this.showSupervisedUserSigninWarning();
       } else {
         // Special case for multi-profiles sign in. We show users even if they
