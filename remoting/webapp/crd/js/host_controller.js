79ff1757bff6251ed75a7c5b7f6542d9a6fb2137
garykac <garykac@chromium.org>
2015-02-27 13:17:39 -0800

    [Chromoting] Fix AppRemoting webapp: xhr and identity.
    Both crrev.com/945033002 and crrev.com/937593002 broke the AppRemoting
    webapp. This cl updates the AppRemoting webapp to use the new version
    of these classes.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/966823002
    
    Cr-Commit-Position: refs/heads/master@{#318516}

--- remoting/webapp/crd/js/host_controller.js
+++ remoting/webapp/crd/js/host_controller.js
@@ -279,9 +279,6 @@ remoting.HostController.prototype.start = function(hostPin, consent, onDone,
        publicKey: publicKey
     } };
 
-    var registerHostUrl =
-        remoting.settings.DIRECTORY_API_BASE_URL + '/@me/hosts';
-
     remoting.xhr.start({
       method: 'POST',
       url: remoting.settings.DIRECTORY_API_BASE_URL + '/@me/hosts',
