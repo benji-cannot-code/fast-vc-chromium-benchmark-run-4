8c865f3db3a24add098724d3751578fe047b9912
dfalcantara <dfalcantara@chromium.org>
2015-02-26 18:38:13 -0800

    Remove Incognito notification when closeAllTabs() called
    Because of Android wonkiness, we can end up with a Tab that
    doesn't close itself until the user backgrounds Chrome.  In
    these cases, the notification about incognito tabs existing
    won't go away, and selecting it won't do anything since the
    Tabs are all technically closing.  Just hide it if
    DocumentTabModel.closeAllTabs() is ever called.
    
    BUG=455763
    
    Review URL: https://codereview.chromium.org/946553007
    
    Cr-Commit-Position: refs/heads/master@{#318382}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/document/DocumentTabModelImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/document/DocumentTabModelImpl.java
@@ -25,6 +25,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.browser.Tab;
 import org.chromium.chrome.browser.TabState;
+import org.chromium.chrome.browser.document.IncognitoNotificationManager;
 import org.chromium.chrome.browser.tabmodel.TabList;
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModelJniBridge;
@@ -836,6 +837,7 @@ public class DocumentTabModelImpl extends TabModelJniBridge implements DocumentT
     @Override
     public void closeAllTabs(boolean allowDelegation, boolean uponExit) {
         for (int i = getCount() - 1; i >= 0; i--) closeTabAt(i);
+        if (isIncognito()) IncognitoNotificationManager.dismissIncognitoNotification();
     }
 
     @Override
