85d4b40667c6377c035f1b7e8f0ad639fcf8e241
mtomasz <mtomasz@chromium.org>
2015-02-26 22:17:02 -0800

    Clean up drive::file_system::TouchOperation.
    The local_id doesn't need to be passed as it can be obtained from the resource
    entry.
    
    TEST=unit_tests: *TouchOperation*
    BUG=None
    
    Review URL: https://codereview.chromium.org/958923005
    
    Cr-Commit-Position: refs/heads/master@{#318410}

--- chrome/browser/chromeos/drive/file_system/touch_operation_unittest.cc
+++ chrome/browser/chromeos/drive/file_system/touch_operation_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/drive/file_errors.h"
 #include "chrome/browser/chromeos/drive/file_system/operation_test_base.h"
 #include "chrome/browser/chromeos/drive/resource_metadata.h"
-#include "content/public/test/test_utils.h"
 #include "google_apis/drive/test_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
