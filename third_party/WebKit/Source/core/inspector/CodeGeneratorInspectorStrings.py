38a1641388e221d85e7468f2dc15352bda04b05b
bratell@opera.com <bratell@opera.com>
2015-02-27 14:33:15 +0000

    Inspector: Store array of small numbers in 16 bit instead of 64 bit.
    Save 1.5 KB by storing small numbers in 16 bit instead of 64 bit. Minor change 
    but should be smaller and faster.
    
             +3: blink::InspectorBackendDispatcher::commandName(blink::InspectorBackendDispatcher::MethodNames) type=t, (was 21 bytes, now 24 bytes)
          -1518: blink::InspectorBackendDispatcher::commandNamesIndex type=r, (was 2024 bytes, now 506 bytes)
    
    R=yurys@chromium.org
    
    Review URL: https://codereview.chromium.org/958893002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191000 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/CodeGeneratorInspectorStrings.py
+++ third_party/WebKit/Source/core/inspector/CodeGeneratorInspectorStrings.py
@@ -199,7 +199,7 @@ $methodNamesEnumContent
 
 private:
     static const char commandNames[];
-    static const size_t commandNamesIndex[];
+    static const unsigned short commandNamesIndex[];
 };
 
 } // namespace blink
@@ -226,7 +226,7 @@ const char InspectorBackendDispatcher::commandNames[] = {
 $methodNameDeclarations
 };
 
-const size_t InspectorBackendDispatcher::commandNamesIndex[] = {
+const unsigned short InspectorBackendDispatcher::commandNamesIndex[] = {
 $methodNameDeclarationsIndex
 };
 
