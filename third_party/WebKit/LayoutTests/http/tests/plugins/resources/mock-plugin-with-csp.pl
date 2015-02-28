ef84cb0863fe2c8a19e74779564ee1ea17aa91d9
estark@chromium.org <estark@chromium.org>
2015-02-28 01:16:30 +0000

    Don't inherit parent's CSP in PluginDocuments
    Inheriting the CSP in a PluginDocument causes <iframe src="foo.pdf"> to
    be blocked if the parent's CSP forbids <embed src="foo.pdf">, as the
    iframe turns into a PluginDocument with an <embed> tag inside it.
    
    The only exception is that the plugin-types directive is still inherited from a parent document to a child PluginDocument, which preserves the current behavior and is required by the CSP spec.
    
    BUG=454247
    TEST=visit http://htmlpad.org/object-src-test/ and observe that both
    PDFs load (previously, the PDF that was embedded as an <iframe
    src="..."> was blocked by CSP)
    
    Review URL: https://codereview.chromium.org/928653002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191037 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/plugins/resources/mock-plugin-with-csp.pl
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/perl -wT
+use strict;
+
+
+print "Content-Type: application/x-webkit-test-netscape\n";
+print "Content-Security-Policy: object-src 'none'\n";
+print "\n";
+
+print "This is a mock plugin. It does pretty much nothing.";
