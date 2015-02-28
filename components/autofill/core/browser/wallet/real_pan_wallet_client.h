ef86fc6af5c82841cf95a5a7a288f80078ce6694
estade <estade@chromium.org>
2015-02-27 18:09:27 -0800

    getrealpan - Handle 401 Unauthorized
    On the first 401 Unauthorized, invalidate the oauth token and try again. If there is more than one 401 Unauthorized in a row, we're probably borked, so don't keep retrying.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/966943004
    
    Cr-Commit-Position: refs/heads/master@{#318572}

--- components/autofill/core/browser/wallet/real_pan_wallet_client.h
+++ components/autofill/core/browser/wallet/real_pan_wallet_client.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "components/autofill/core/browser/card_unmask_delegate.h"
+#include "components/autofill/core/browser/credit_card.h"
 #include "google_apis/gaia/oauth2_token_service.h"
 #include "net/url_request/url_fetcher_delegate.h"
 
@@ -20,8 +21,6 @@ class URLRequestContextGetter;
 }
 
 namespace autofill {
-
-class CreditCard;
 
 namespace wallet {
 
@@ -71,8 +70,11 @@ class RealPanWalletClient : public net::URLFetcherDelegate,
   void OnGetTokenFailure(const OAuth2TokenService::Request* request,
                          const GoogleServiceAuthError& error) override;
 
+  // Creates |request_| from |card_| and |response_|.
+  void CreateRequest();
+
   // Initiates a new OAuth2 token request.
-  void StartTokenFetch();
+  void StartTokenFetch(bool invalidate_old);
 
   // Adds the token to |request_| and starts the request.
   void SetOAuth2TokenAndStartRequest();
@@ -84,6 +86,10 @@ class RealPanWalletClient : public net::URLFetcherDelegate,
   // of a request to Online Wallet.
   Delegate* const delegate_;  // must outlive |this|.
 
+  // The card and response for the latest unmask request.
+  CreditCard card_;
+  CardUnmaskDelegate::UnmaskResponse response_;
+
   // The current Wallet request object.
   scoped_ptr<net::URLFetcher> request_;
 
@@ -92,6 +98,9 @@ class RealPanWalletClient : public net::URLFetcherDelegate,
 
   // The OAuth2 token, or empty if not fetched.
   std::string access_token_;
+
+  // True if |this| has already retried due to a 401 response from the server.
+  bool has_retried_authorization_;
 
   base::WeakPtrFactory<RealPanWalletClient> weak_ptr_factory_;
 
