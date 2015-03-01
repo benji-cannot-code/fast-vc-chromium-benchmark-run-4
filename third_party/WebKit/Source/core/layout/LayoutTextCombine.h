67dce7fe3a65c468630aa742a64c4cba7e9dab47
chrishtr@chromium.org <chrishtr@chromium.org>
2015-03-01 05:53:46 +0000

    Fix hack that adjusts vertical alignment after rotation to vertical writing mode.
    This code was originally introduced in this webkit bug:
    
    https://bugs.webkit.org/show_bug.cgi?id=48085
    
    BUG=460198
    
    Review URL: https://codereview.chromium.org/958253002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191046 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutTextCombine.h
+++ third_party/WebKit/Source/core/layout/LayoutTextCombine.h
@@ -52,7 +52,6 @@ private:
 
     float offsetX(const FloatRect& boxRect) const;
     float offsetXNoScale(const FloatRect& boxRect) const;
-    float offsetY() const { return style()->font().fontDescription().computedPixelSize(); }
 
     float m_combinedTextWidth;
     float m_scaleX;
