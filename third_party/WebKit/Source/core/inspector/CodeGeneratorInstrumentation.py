29136d5e23b0bad4275fb6f2f8cc2f4d75611846
kouhei@chromium.org <kouhei@chromium.org>
2015-03-02 08:45:30 +0000

    InlinedVisitor: Migrate CodeGeneratorInstrumentation.py
    This CL manually migrates InstrumentingAgents to inlined tracing,
    which was uncaught by the automated script.
    
    BUG=455524
    
    Review URL: https://codereview.chromium.org/970723002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191087 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/CodeGeneratorInstrumentation.py
+++ third_party/WebKit/Source/core/inspector/CodeGeneratorInstrumentation.py
@@ -129,7 +129,7 @@ public:
         return adoptRefWillBeNoop(new InstrumentingAgents());
     }
     ~InstrumentingAgents() { }
-    void trace(Visitor*);
+    DECLARE_TRACE();
     void reset();
 
 ${accessor_list}
@@ -155,7 +155,7 @@ InstrumentingAgents::InstrumentingAgents()
 {
 }
 
-void InstrumentingAgents::trace(Visitor* visitor)
+DEFINE_TRACE(InstrumentingAgents)
 {
     $trace_list
 }
