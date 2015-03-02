e37fad13a8c2ded3889134dd73485b0221090487
tfarina <tfarina@chromium.org>
2015-03-02 13:01:34 -0800

    app_list: Elevate the verbosity level to 1.
    This elevates it from 4 or 5 depending if it is a normal file or unit
    test file to 1 for everything.
    
    Used the following command line to lint the files:
    
    $ for in $(g ls-files ui/app_list/ | grep -E '.*\.(cc|h)$' | grep -v
    cocoa); do cpplint.py --verbose=1 $f; done
    
    BUG=None
    TEST=see above
    R=tapted@chromium.org
    
    Review URL: https://codereview.chromium.org/972463002
    
    Cr-Commit-Position: refs/heads/master@{#318745}

--- ui/app_list/views/search_result_page_view.cc
+++ ui/app_list/views/search_result_page_view.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/app_list/views/search_result_page_view.h"
 
+#include <algorithm>
+
 #include "ui/app_list/app_list_constants.h"
 #include "ui/app_list/app_list_switches.h"
 #include "ui/app_list/app_list_view_delegate.h"
--- ui/app_list/views/start_page_view.cc
+++ ui/app_list/views/start_page_view.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/app_list/views/start_page_view.h"
 
+#include <string>
+
 #include "base/i18n/rtl.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
