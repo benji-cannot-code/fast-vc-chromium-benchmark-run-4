81349308bf05e84ccf9e78cbb54ee495752ac81e
tfarina <tfarina@chromium.org>
2015-03-02 09:45:32 -0800

    Cleanup: Fix tools/gn header include guards.
    The header include guards should match the file path from root.
    
    These entries were found using a modified version of Eric's
    fix-include-guards.py script found attached in
    https://code.google.com/p/chromium/issues/detail?id=435361#c7.
    
    BUG=435361
    TEST=gn_unittests
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/968193002
    
    Cr-Commit-Position: refs/heads/master@{#318719}

--- tools/gn/commands.h
+++ tools/gn/commands.h
@@ -174,4 +174,4 @@ void FilterAndPrintTargetSet(bool indent,
 
 }  // namespace commands
 
-#endif  // TOOLS_GN_COMMANDS_H
+#endif  // TOOLS_GN_COMMANDS_H_
--- tools/gn/substitution_list.h
+++ tools/gn/substitution_list.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef TOOLS_GN_SUBSTUTITION_LIST_H
-#define TOOLS_GN_SUBSTUTITION_LIST_H
+#ifndef TOOLS_GN_SUBSTITUTION_LIST_H_
+#define TOOLS_GN_SUBSTITUTION_LIST_H_
 
 #include <string>
 #include <vector>
@@ -44,4 +44,4 @@ class SubstitutionList {
   std::vector<SubstitutionType> required_types_;
 };
 
-#endif  // TOOLS_GN_SUBSTUTITION_LIST_H
+#endif  // TOOLS_GN_SUBSTITUTION_LIST_H_
