577436b637e5c0608d2953191e84dbdbf55b563c
wesleylancel <wesleylancel@gmail.com>
2015-03-02 13:44:08 -0800

    Add tooltips to profile name when truncated
    This adds tooltips to profile names in both the old and new profile
    switcher when they are truncated on OSX.
    This lines up with behaviour on Windows.
    
    Also added myself to the AUTHORS file seeing this is my first contribution.
    
    BUG=459572
    
    Review URL: https://codereview.chromium.org/943453003
    
    Cr-Commit-Position: refs/heads/master@{#318769}

--- chrome/browser/ui/cocoa/profiles/avatar_menu_bubble_controller.mm
+++ chrome/browser/ui/cocoa/profiles/avatar_menu_bubble_controller.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/profiles/avatar_menu_bubble_controller.h"
 
 #include "base/mac/bundle_locations.h"
+#include "base/mac/sdk_forward_declarations.h"
 #include "base/strings/sys_string_conversions.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/avatar_menu.h"
@@ -160,6 +161,8 @@ const CGFloat kSupervisedUserSpacing = 26.0;
     NSRect frame = [nameField frame];
     frame.size.width = kMaxItemTextWidth;
     [nameField setFrame:frame];
+    if ([nameField respondsToSelector:@selector(setAllowsExpansionToolTips:)])
+      [nameField setAllowsExpansionToolTips:YES];
   }
   *widthAdjust = std::max(*widthAdjust, delta.width);
 
--- chrome/browser/ui/cocoa/profiles/profile_chooser_controller.mm
+++ chrome/browser/ui/cocoa/profiles/profile_chooser_controller.mm
@@ -702,6 +702,13 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
           IDS_PROFILES_NEW_AVATAR_MENU_EDIT_NAME_ACCESSIBLE_NAME,
           base::SysNSStringToUTF16(profileName))
                                     forAttribute:NSAccessibilityTitleAttribute];
+
+      NSSize textSize = [profileName sizeWithAttributes:@{
+        NSFontAttributeName : [profileNameTextField_ font]
+      }];
+
+      if (textSize.width > frameRect.size.width - [hoverImage size].width * 2)
+        [self setToolTip:profileName];
     }
 
     [[self cell] accessibilitySetOverrideValue:NSAccessibilityButtonRole
@@ -1805,6 +1812,16 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
   [profileButton setTag:itemIndex];
   [profileButton setTarget:self];
   [profileButton setAction:@selector(switchToProfile:)];
+
+  NSSize textSize = [[profileButton title] sizeWithAttributes:@{
+    NSFontAttributeName : [profileButton font]
+  }];
+
+  CGFloat availableWidth = rect.size.width - kSmallImageSide -
+                           kImageTitleSpacing - kHorizontalSpacing;
+
+  if (std::ceil(textSize.width) > availableWidth)
+    [profileButton setToolTip:[profileButton title]];
 
   return profileButton.autorelease();
 }
