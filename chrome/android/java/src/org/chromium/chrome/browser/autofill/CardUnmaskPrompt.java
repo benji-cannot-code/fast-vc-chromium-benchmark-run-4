32c798c0d4d9ccc0dd0cbdd6302f7b261496a5a9
Newton Allen <newt@chromium.org>
2015-03-02 11:02:18 -0800

    Fix crashes on ICS.
    We were using several APIs introduced after API level 14. Fix that.
    
    BUG=462802
    R=estade@chromium.org
    
    Review URL: https://codereview.chromium.org/963993002
    
    Cr-Commit-Position: refs/heads/master@{#318731}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/CardUnmaskPrompt.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/CardUnmaskPrompt.java
@@ -13,6 +13,7 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.os.Build;
 import android.os.Handler;
+import android.support.v4.view.ViewCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -102,8 +103,8 @@ public class CardUnmaskPrompt implements DialogInterface.OnDismissListener, Text
                           .setView(v)
                           .setNegativeButton(R.string.cancel, null)
                           .setPositiveButton(R.string.card_unmask_confirm_button, null)
-                          .setOnDismissListener(this)
                           .create();
+        mDialog.setOnDismissListener(this);
 
         mShouldRequestExpirationDate = shouldRequestExpirationDate;
         mThisYear = Calendar.getInstance().get(Calendar.YEAR);
@@ -169,6 +170,7 @@ public class CardUnmaskPrompt implements DialogInterface.OnDismissListener, Text
             mVerificationView.setText("Your card is verified");
             Handler h = new Handler();
             h.postDelayed(new Runnable() {
+                @Override
                 public void run() {
                     dismiss();
                 }
@@ -224,7 +226,7 @@ public class CardUnmaskPrompt implements DialogInterface.OnDismissListener, Text
         mMonthInput.setEnabled(enabled);
         mYearInput.setEnabled(enabled);
         mMainContents.setAlpha(enabled ? 1.0f : 0.15f);
-        mMainContents.setImportantForAccessibility(
+        ViewCompat.setImportantForAccessibility(mMainContents,
                 enabled ? View.IMPORTANT_FOR_ACCESSIBILITY_AUTO
                         : View.IMPORTANT_FOR_ACCESSIBILITY_NO_HIDE_DESCENDANTS);
         ((ViewGroup) mMainContents).setDescendantFocusability(
