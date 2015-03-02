9def31b9a1317aac76c05a31d0a2ef0942a23340
dcheng@chromium.org <dcheng@chromium.org>
2015-03-02 16:13:09 +0000

    Ignore shadow DOM scope checks for RemoteFrames.
    In the long-term, shadow DOM scopes will need to be replicated across
    renderers when out-of-process iframes are enabled. This is a temporary
    measure to facilitate testing. Otherwise, RemoteFrames Window object
    will not be accessible via window[n] at all.
    
    BUG=425623
    
    Review URL: https://codereview.chromium.org/962903002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191112 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/FrameTree.cpp
+++ third_party/WebKit/Source/core/page/FrameTree.cpp
@@ -181,11 +181,13 @@ Frame* FrameTree::scopedChild(unsigned index) const
 
     unsigned scopedIndex = 0;
     for (Frame* result = firstChild(); result; result = result->tree().nextSibling()) {
-        if (result->isLocalFrame() && toLocalFrame(result)->inScope(scope)) {
-            if (scopedIndex == index)
-                return result;
-            scopedIndex++;
-        }
+        // FIXME: Currently, RemoteFrames are always visible, even through a shadow DOM scope.
+        // Once RemoteFrames have a TreeScope, the scoping check should apply to RemoteFrames too.
+        if (result->isLocalFrame() && !toLocalFrame(result)->inScope(scope))
+            continue;
+        if (scopedIndex == index)
+            return result;
+        scopedIndex++;
     }
 
     return nullptr;
@@ -201,7 +203,9 @@ Frame* FrameTree::scopedChild(const AtomicString& name) const
         return nullptr;
 
     for (Frame* child = firstChild(); child; child = child->tree().nextSibling())
-        if (child->tree().name() == name && child->isLocalFrame() && toLocalFrame(child)->inScope(scope))
+        // FIXME: Currently, RemoteFrames are always visible, even through a shadow DOM scope.
+        // Once RemoteFrames have a TreeScope, the scoping check should apply to RemoteFrames too.
+        if (child->tree().name() == name && (!child->isLocalFrame() || toLocalFrame(child)->inScope(scope)))
             return child;
     return nullptr;
 }
@@ -213,8 +217,11 @@ inline unsigned FrameTree::scopedChildCount(TreeScope* scope) const
 
     unsigned scopedCount = 0;
     for (Frame* result = firstChild(); result; result = result->tree().nextSibling()) {
-        if (result->isLocalFrame() && toLocalFrame(result)->inScope(scope))
-            scopedCount++;
+        // FIXME: Currently, RemoteFrames are always visible, even through a shadow DOM scope.
+        // Once RemoteFrames have a TreeScope, the scoping check should apply to RemoteFrames too.
+        if (result->isLocalFrame() && !toLocalFrame(result)->inScope(scope))
+            continue;
+        scopedCount++;
     }
 
     return scopedCount;
