cefe9ae1e45cd43195745bc70f9c1e8173f13347
sigbjornf@opera.com <sigbjornf@opera.com>
2015-03-02 06:33:52 +0000

    Reduce platform/heap/ wtf includes.
    Rely on wtf/Forward.h where possible, reducing CPP's #include count
    ever so slightly when processing platform/heap/Handle.h
    
    R=
    BUG=420515
    
    Review URL: https://codereview.chromium.org/966243002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191082 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Visitor.h
+++ third_party/WebKit/Source/platform/heap/Visitor.h
@@ -41,19 +41,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Forward.h"
 #include "wtf/HashMap.h"
 #include "wtf/HashTraits.h"
-#include "wtf/InstanceCounter.h"
-#include "wtf/OwnPtr.h"
-#include "wtf/RefPtr.h"
 #include "wtf/TypeTraits.h"
-#include "wtf/WeakPtr.h"
 #if ENABLE(GC_PROFILING)
+#include "wtf/InstanceCounter.h"
 #include "wtf/text/WTFString.h"
-#endif
-
-#if ENABLE(ASSERT)
-#define DEBUG_ONLY(x) x
-#else
-#define DEBUG_ONLY(x)
 #endif
 
 namespace blink {
@@ -108,9 +99,7 @@ struct GCInfo {
 
 #if ENABLE(ASSERT)
 PLATFORM_EXPORT void assertObjectHasGCInfo(const void*, size_t gcInfoIndex);
-
 #endif
-
 
 // The FinalizerTraitImpl specifies how to finalize objects. Object
 // that inherit from GarbageCollectedFinalized are finalized by
@@ -1021,6 +1010,6 @@ struct GCInfoTrait {
     }
 };
 
-}
+} // namespace blink
 
-#endif
+#endif // Visitor_h
