dfd67d661be7a0ef5618c8e02bc342d6ba35455d
calamity <calamity@chromium.org>
2015-03-01 19:08:58 -0800

    Fix sort order of unlaunched apps on app list start page.
    This CL fixes an issue with the app list start page sort order for
    unlaunched apps. Folder items were being weighted too highly, causing
    them to appear first. The items that are not in a folder will now
    appear first in the recommendations, in app list order.
    
    BUG=455338
    
    Review URL: https://codereview.chromium.org/954363002
    
    Cr-Commit-Position: refs/heads/master@{#318642}

--- chrome/browser/ui/app_list/search/app_search_provider_unittest.cc
+++ chrome/browser/ui/app_list/search/app_search_provider_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_set.h"
 #include "sync/api/string_ordinal.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/app_list/app_list_folder_item.h"
 #include "ui/app_list/app_list_item.h"
 #include "ui/app_list/app_list_model.h"
 #include "ui/app_list/search_result.h"
@@ -159,6 +160,7 @@ TEST_F(AppSearchProviderTest, FetchRecommendations) {
 }
 
 TEST_F(AppSearchProviderTest, FetchUnlaunchedRecommendations) {
+  const char kFolderId[] = "folder1";
   extensions::ExtensionPrefs* prefs =
       extensions::ExtensionPrefs::Get(profile_.get());
 
@@ -175,6 +177,18 @@ TEST_F(AppSearchProviderTest, FetchUnlaunchedRecommendations) {
       model()->FindItem(kPackagedApp2Id),
       model()->FindItem(kPackagedApp1Id)->position().CreateBefore());
   EXPECT_EQ("Hosted App,Packaged App 2,Packaged App 1", RunQuery(""));
+
+  // Moving an app into a folder deprioritizes it.
+  model()->AddItem(scoped_ptr<AppListFolderItem>(
+      new AppListFolderItem(kFolderId, AppListFolderItem::FOLDER_TYPE_NORMAL)));
+  model()->MoveItemToFolder(model()->FindItem(kPackagedApp2Id), kFolderId);
+  EXPECT_EQ("Hosted App,Packaged App 1,Packaged App 2", RunQuery(""));
+
+  // The position of the folder shouldn't matter.
+  model()->SetItemPosition(
+      model()->FindItem(kFolderId),
+      model()->FindItem(kPackagedApp1Id)->position().CreateBefore());
+  EXPECT_EQ("Hosted App,Packaged App 1,Packaged App 2", RunQuery(""));
 }
 
 }  // namespace test
