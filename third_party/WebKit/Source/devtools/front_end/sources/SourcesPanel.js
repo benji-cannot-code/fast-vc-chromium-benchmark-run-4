0606ef2b42b91490ed82dc4c98a7ce8a87dc8861
pfeldman@chromium.org <pfeldman@chromium.org>
2015-03-02 11:20:07 +0000

    DevTools: allow dropping urls into timeline to load the trace.
    Review URL: https://codereview.chromium.org/970583002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191094 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sources/SourcesPanel.js
+++ third_party/WebKit/Source/devtools/front_end/sources/SourcesPanel.js
@@ -36,7 +36,7 @@ WebInspector.SourcesPanel = function(workspaceForTest)
 {
     WebInspector.Panel.call(this, "sources");
     this.registerRequiredCSS("sources/sourcesPanel.css");
-    new WebInspector.DropTarget(this.element, [WebInspector.DropTarget.dragAndDropFilesType], WebInspector.UIString("Drop workspace folder here"), this._handleDrop.bind(this));
+    new WebInspector.DropTarget(this.element, [WebInspector.DropTarget.Types.Files], WebInspector.UIString("Drop workspace folder here"), this._handleDrop.bind(this));
 
     this._workspace = workspaceForTest || WebInspector.workspace;
     this._networkMapping = WebInspector.networkMapping;
@@ -1243,10 +1243,11 @@ WebInspector.SourcesPanel.prototype = {
     },
 
     /**
-     * @param {!Array.<!DataTransferItem>} items
+     * @param {!DataTransfer} dataTransfer
      */
-    _handleDrop: function(items)
+    _handleDrop: function(dataTransfer)
     {
+        var items = dataTransfer.items;
         if (!items.length)
             return;
         var entry = items[0].webkitGetAsEntry();
--- third_party/WebKit/Source/devtools/front_end/ui/DropTarget.js
+++ third_party/WebKit/Source/devtools/front_end/ui/DropTarget.js
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @param {!Element} element
  * @param {!Array.<string>} transferTypes
  * @param {string} messageText
- * @param {function(!Array.<!DataTransferItem>)} handleDrop
+ * @param {function(!DataTransfer)} handleDrop
  */
 WebInspector.DropTarget = function(element, transferTypes, messageText, handleDrop)
 {
@@ -18,17 +18,29 @@ WebInspector.DropTarget = function(element, transferTypes, messageText, handleDr
     this._transferTypes = transferTypes;
     this._messageText = messageText;
     this._handleDrop = handleDrop;
+    this._enabled = true;
 }
 
-WebInspector.DropTarget.dragAndDropFilesType = "Files";
+WebInspector.DropTarget.Types = {
+    Files: "Files",
+    URIList: "text/uri-list"
+}
 
 WebInspector.DropTarget.prototype = {
+    /**
+     * @param {boolean} enabled
+     */
+    setEnabled: function(enabled)
+    {
+        this._enabled = enabled;
+    },
+
     /**
      * @param {!Event} event
      */
     _onDragEnter: function(event)
     {
-        if (this._hasMatchingType(event))
+        if (this._enabled && this._hasMatchingType(event))
             event.consume(true);
     },
 
@@ -50,7 +62,7 @@ WebInspector.DropTarget.prototype = {
      */
     _onDragOver: function(event)
     {
-        if (!this._hasMatchingType(event))
+        if (!this._enabled || !this._hasMatchingType(event))
             return;
         event.dataTransfer.dropEffect = "copy";
         event.consume(true);
@@ -64,13 +76,20 @@ WebInspector.DropTarget.prototype = {
         this._dragMaskElement.addEventListener("dragleave", this._onDragLeave.bind(this), true);
     },
 
+    /**
+     * @param {!Event} event
+     */
     _onDrop: function(event)
     {
         event.consume(true);
         this._removeMask();
-        this._handleDrop(/** @type {!Array.<!DataTransferItem>} */ (event.dataTransfer.items));
+        if (this._enabled)
+            this._handleDrop(event.dataTransfer);
     },
 
+    /**
+     * @param {!Event} event
+     */
     _onDragLeave: function(event)
     {
         event.consume(true);
