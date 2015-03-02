e040c3403f44f0f8eb354f48430ebd579d45b9d8
dnicoara <dnicoara@chromium.org>
2015-03-02 10:55:55 -0800

    [Ozone-Gbm] Explicitly crash if trying software rendering on GBM
    Software rendering isn't supported on the GBM platform. Rather than
    allow callers to crash due to nullptr accesses, crash explicitly so we
    get more meaningfull crash reports.
    
    BUG=461396
    
    Review URL: https://codereview.chromium.org/958403004
    
    Cr-Commit-Position: refs/heads/master@{#318728}

--- ui/ozone/platform/dri/gbm_surface_factory.cc
+++ ui/ozone/platform/dri/gbm_surface_factory.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/public/native_pixmap.h"
 #include "ui/ozone/public/overlay_candidates_ozone.h"
 #include "ui/ozone/public/ozone_switches.h"
+#include "ui/ozone/public/surface_ozone_canvas.h"
 #include "ui/ozone/public/surface_ozone_egl.h"
 
 namespace ui {
@@ -118,6 +119,12 @@ bool GbmSurfaceFactory::LoadEGLGLES2Bindings(
       AddGLLibraryCallback add_gl_library,
       SetGLGetProcAddressProcCallback set_gl_get_proc_address) {
   return LoadDefaultEGLGLES2Bindings(add_gl_library, set_gl_get_proc_address);
+}
+
+scoped_ptr<SurfaceOzoneCanvas> GbmSurfaceFactory::CreateCanvasForWidget(
+    gfx::AcceleratedWidget widget) {
+  LOG(FATAL) << "Software rendering mode is not supported with GBM platform";
+  return nullptr;
 }
 
 scoped_ptr<SurfaceOzoneEGL> GbmSurfaceFactory::CreateEGLSurfaceForWidget(
