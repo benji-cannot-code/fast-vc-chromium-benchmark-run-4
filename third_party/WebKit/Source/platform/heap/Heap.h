d1a4ab2c8684073369c13e43f96607f47ab311dd
kouhei@chromium.org <kouhei@chromium.org>
2015-03-03 05:37:34 +0000

    InlinedTracing: Add missing method InlinedGlobalMarkingVisitor::weakTableRegistered
    This CL adds missing definition of the method InlinedGlobalMarkingVisitor::weakTableRegistered,
    which is needed to handle ASSERT in WTF::HashTable when ENABLE_INLINED_TRACING is enabled.
    
    TEST=debug compiles with ENABLE_INLINED_TRACING=1
    BUG=455524
    
    Review URL: https://codereview.chromium.org/970873003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191166 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Heap.h
+++ third_party/WebKit/Source/platform/heap/Heap.h
@@ -1659,7 +1659,8 @@ public:
     }
 
 #if ENABLE(ASSERT)
-    static bool weakTableRegistered(Visitor* visitor, const void* closure)
+    template<typename VisitorDispatcher>
+    static bool weakTableRegistered(VisitorDispatcher visitor, const void* closure)
     {
         return visitor->weakTableRegistered(closure);
     }
--- third_party/WebKit/Source/platform/heap/InlinedGlobalMarkingVisitor.h
+++ third_party/WebKit/Source/platform/heap/InlinedGlobalMarkingVisitor.h
@@ -48,6 +48,10 @@ public:
     using Impl::registerDelayedMarkNoTracing;
     using Impl::registerWeakTable;
 
+#if ENABLE(ASSERT)
+    using Impl::weakTableRegistered;
+#endif
+
     using Helper::registerWeakMembers;
     inline void registerWeakMembers(const void* closure, const void* objectPointer, WeakPointerCallback callback)
     {
