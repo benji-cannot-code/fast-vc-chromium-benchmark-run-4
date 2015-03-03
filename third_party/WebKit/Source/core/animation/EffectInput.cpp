ea7cdd8b85f84016f0c2c96e03b4e752eb97637f
alancutter@chromium.org <alancutter@chromium.org>
2015-03-03 00:02:34 +0000

    Web Animations: Refactor compositor AnimatableValues out of Interpolation creation
    This change simplifies the role of StringKeyframe::maybeCreateInterpolation() to
    not have to worry about creating AnimatableValue caches for compositor animations.
    This task is taken care of by snapshotCompositableProperties() instead.
    
    Review URL: https://codereview.chromium.org/955583003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191151 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/EffectInput.cpp
+++ third_party/WebKit/Source/core/animation/EffectInput.cpp
@@ -117,7 +117,7 @@ PassRefPtrWillBeRawPtr<AnimationEffect> EffectInput::convert(Element* element, c
         exceptionState.throwDOMException(NotSupportedError, "Additive animations are not supported.");
         return nullptr;
     }
-    keyframeEffectModel->forceConversionsToAnimatableValues(element);
+    keyframeEffectModel->forceConversionsToAnimatableValues(*element);
 
     return keyframeEffectModel;
 }
