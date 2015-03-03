05e1919b7e1b0f71a947cf5c3c2925bbc77d9581
yawano <yawano@chromium.org>
2015-03-02 19:39:59 -0800

    Dispatch change events when file changes have happened on Drive.
    BUG=463053
    TEST=manually tested as described in the issue.
    
    Review URL: https://codereview.chromium.org/964393002
    
    Cr-Commit-Position: refs/heads/master@{#318835}

--- ui/file_manager/file_manager/foreground/js/directory_contents.js
+++ ui/file_manager/file_manager/foreground/js/directory_contents.js
@@ -790,6 +790,7 @@ DirectoryContents.prototype.update = function(updatedEntries, removedUrls) {
   }
 
   var updatedList = [];
+  var updatedIndexes = [];
   for (var i = 0; i < this.fileList_.length; i++) {
     var url = this.fileList_.item(i).toURL();
 
@@ -801,9 +802,13 @@ DirectoryContents.prototype.update = function(updatedEntries, removedUrls) {
 
     if (url in updatedMap) {
       updatedList.push(updatedMap[url]);
+      updatedIndexes.push(i);
       delete updatedMap[url];
     }
   }
+
+  if (updatedIndexes.length > 0)
+    this.fileList_.updateIndexes(updatedIndexes);
 
   var addedList = [];
   for (var url in updatedMap) {
