385585900db3fd286558c61f112bda07928334d2
mtomasz <mtomasz@chromium.org>
2015-03-02 16:20:06 -0800

    Allow offsets in blobs larger than 2GB on 32 bit Chromium builds.
    The recent patch crrev.com/895933007 introduced a regression by using size_t
    for offsets, which on 32bit builds is sizeof(size_t) = 4 which was causing
    broken offset values due to assigning a uint64_t variable to such variables.
    
    This CL fixes that by simply converting the types to uint64_t.
    
    TEST=*FileSystemProvider*BigFile* on a 32 bit build Chromium build with
        chromeos=1.
    BUG=375297, 458122
    
    Review URL: https://codereview.chromium.org/959113002
    
    Cr-Commit-Position: refs/heads/master@{#318808}

--- content/browser/fileapi/blob_storage_context_unittest.cc
+++ content/browser/fileapi/blob_storage_context_unittest.cc
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "storage/browser/blob/blob_storage_context.h"
+
+#include <limits>
+#include <string>
+
 #include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
@@ -12,24 +17,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/fileapi/blob_storage_host.h"
 #include "storage/browser/blob/blob_data_builder.h"
 #include "storage/browser/blob/blob_data_handle.h"
+#include "storage/browser/blob/blob_data_item.h"
 #include "storage/browser/blob/blob_data_snapshot.h"
-#include "storage/browser/blob/blob_storage_context.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using storage::BlobDataBuilder;
 using storage::BlobDataHandle;
+using storage::BlobDataItem;
 using storage::BlobDataSnapshot;
 using storage::BlobStorageContext;
 using storage::DataElement;
 
 namespace content {
-
 namespace {
-// This is necessary to clean up the blobs after the handles are released.
-void RunEventLoopTillIdle() {
-  base::RunLoop run_loop;
-  run_loop.RunUntilIdle();
-}
 
 void SetupBasicBlob(BlobStorageHost* host, const std::string& id) {
   EXPECT_TRUE(host->StartBuildingBlob(id));
@@ -39,6 +39,7 @@ void SetupBasicBlob(BlobStorageHost* host, const std::string& id) {
   EXPECT_TRUE(host->FinishBuildingBlob(id, "text/plain"));
   EXPECT_FALSE(host->StartBuildingBlob(id));
 }
+
 }  // namespace
 
 TEST(BlobStorageContextTest, IncrementDecrementRef) {
@@ -55,7 +56,7 @@ TEST(BlobStorageContextTest, IncrementDecrementRef) {
   blob_data_handle = context.GetBlobDataFromUUID(kId);
   EXPECT_TRUE(blob_data_handle);
   blob_data_handle.reset();
-  RunEventLoopTillIdle();
+  base::RunLoop().RunUntilIdle();
 
   // Make sure its still there after inc/dec.
   EXPECT_TRUE(host.IncrementBlobRefCount(kId));
@@ -63,7 +64,7 @@ TEST(BlobStorageContextTest, IncrementDecrementRef) {
   blob_data_handle = context.GetBlobDataFromUUID(kId);
   EXPECT_TRUE(blob_data_handle);
   blob_data_handle.reset();
-  RunEventLoopTillIdle();
+  base::RunLoop().RunUntilIdle();
 
   // Make sure it goes away in the end.
   EXPECT_TRUE(host.DecrementBlobRefCount(kId));
@@ -114,7 +115,7 @@ TEST(BlobStorageContextTest, BlobDataHandle) {
   // Should disappear after dropping both handles.
   blob_data_handle.reset();
   another_handle.reset();
-  RunEventLoopTillIdle();
+  base::RunLoop().RunUntilIdle();
 
   blob_data_handle = context.GetBlobDataFromUUID(kId);
   EXPECT_FALSE(blob_data_handle);
@@ -148,11 +149,11 @@ TEST(BlobStorageContextTest, MemoryUsage) {
   EXPECT_EQ(10lu, context.memory_usage());
 
   blob_data_handle.reset();
-  RunEventLoopTillIdle();
+  base::RunLoop().RunUntilIdle();
 
   EXPECT_EQ(10lu, context.memory_usage());
   blob_data_handle2.reset();
-  RunEventLoopTillIdle();
+  base::RunLoop().RunUntilIdle();
 
   EXPECT_EQ(0lu, context.memory_usage());
 }
@@ -191,7 +192,7 @@ TEST(BlobStorageContextTest, AddFinishedBlob) {
   blob_data_handle.reset();
   data2.reset();
 
-  RunEventLoopTillIdle();
+  base::RunLoop().RunUntilIdle();
 
   blob_data_handle = context.GetBlobDataFromUUID(kId1);
   EXPECT_FALSE(blob_data_handle);
@@ -206,7 +207,7 @@ TEST(BlobStorageContextTest, AddFinishedBlob) {
   scoped_ptr<BlobDataHandle> blob_data_handle3 =
       context.AddFinishedBlob(&builder3);
   blob_data_handle2.reset();
-  RunEventLoopTillIdle();
+  base::RunLoop().RunUntilIdle();
 
   blob_data_handle2 = context.GetBlobDataFromUUID(kId2);
   EXPECT_FALSE(blob_data_handle2);
@@ -223,7 +224,43 @@ TEST(BlobStorageContextTest, AddFinishedBlob) {
   blob_data_handle.reset();
   blob_data_handle2.reset();
   blob_data_handle3.reset();
-  RunEventLoopTillIdle();
+  base::RunLoop().RunUntilIdle();
+}
+
+TEST(BlobStorageContextTest, AddFinishedBlob_LargeOffset) {
+  // A value which does not fit in a 4-byte data type. Used to confirm that
+  // large values are supported on 32-bit Chromium builds. Regression test for:
+  // crbug.com/458122.
+  const uint64_t kLargeSize = std::numeric_limits<uint64_t>::max();
+
+  const uint64_t kBlobLength = 5;
+  const std::string kId1("id1");
+  const std::string kId2("id2");
+  base::MessageLoop fake_io_message_loop;
+
+  BlobDataBuilder builder1(kId1);
+  builder1.AppendFileSystemFile(GURL(), 0, kLargeSize, base::Time::Now());
+
+  BlobDataBuilder builder2(kId2);
+  builder2.AppendBlob(kId1, kLargeSize - kBlobLength, kBlobLength);
+
+  BlobStorageContext context;
+  scoped_ptr<BlobDataHandle> blob_data_handle1 =
+      context.AddFinishedBlob(&builder1);
+  scoped_ptr<BlobDataHandle> blob_data_handle2 =
+      context.AddFinishedBlob(&builder2);
+
+  ASSERT_TRUE(blob_data_handle1);
+  ASSERT_TRUE(blob_data_handle2);
+  scoped_ptr<BlobDataSnapshot> data = blob_data_handle2->CreateSnapshot();
+  ASSERT_EQ(1u, data->items().size());
+  const scoped_refptr<BlobDataItem> item = data->items()[0];
+  EXPECT_EQ(kLargeSize - kBlobLength, item->offset());
+  EXPECT_EQ(kBlobLength, item->length());
+
+  blob_data_handle1.reset();
+  blob_data_handle2.reset();
+  base::RunLoop().RunUntilIdle();
 }
 
 TEST(BlobStorageContextTest, CompoundBlobs) {
@@ -277,7 +314,7 @@ TEST(BlobStorageContextTest, CompoundBlobs) {
   EXPECT_EQ(*data, canonicalized_blob_data2);
 
   blob_data_handle.reset();
-  RunEventLoopTillIdle();
+  base::RunLoop().RunUntilIdle();
 }
 
 TEST(BlobStorageContextTest, PublicBlobUrls) {
@@ -298,7 +335,7 @@ TEST(BlobStorageContextTest, PublicBlobUrls) {
   EXPECT_EQ(kId, blob_data_handle->uuid());
   scoped_ptr<BlobDataSnapshot> data = blob_data_handle->CreateSnapshot();
   blob_data_handle.reset();
-  RunEventLoopTillIdle();
+  base::RunLoop().RunUntilIdle();
 
   // The url registration should keep the blob alive even after
   // explicit references are dropped.
@@ -306,7 +343,7 @@ TEST(BlobStorageContextTest, PublicBlobUrls) {
   blob_data_handle = context.GetBlobDataFromPublicURL(kUrl);
   EXPECT_TRUE(blob_data_handle);
   blob_data_handle.reset();
-  RunEventLoopTillIdle();
+  base::RunLoop().RunUntilIdle();
 
   // Finally get rid of the url registration and the blob.
   EXPECT_TRUE(host.RevokePublicBlobURL(kUrl));
