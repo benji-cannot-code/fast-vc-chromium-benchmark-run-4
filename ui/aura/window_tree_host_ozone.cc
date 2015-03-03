7823135149a0c23ac1132faf1dc0f97125e4d6cf
oshima <oshima@chromium.org>
2015-03-03 14:17:41 -0800

    Add event trace to DispatchEvent on ozone
    BUG=450711
    
    Review URL: https://codereview.chromium.org/967403005
    
    Cr-Commit-Position: refs/heads/master@{#318950}

--- ui/aura/window_tree_host_ozone.cc
+++ ui/aura/window_tree_host_ozone.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/aura/window_tree_host_ozone.h"
 
+#include "base/trace_event/trace_event.h"
 #include "ui/aura/window_event_dispatcher.h"
 #include "ui/ozone/public/ozone_platform.h"
 #include "ui/platform_window/platform_window.h"
@@ -82,6 +83,7 @@ void WindowTreeHostOzone::OnDamageRect(const gfx::Rect& damaged_region) {
 }
 
 void WindowTreeHostOzone::DispatchEvent(ui::Event* event) {
+  TRACE_EVENT0("input", "WindowTreeHostOzone::DispatchEvent");
   SendEventToProcessor(event);
 }
 
