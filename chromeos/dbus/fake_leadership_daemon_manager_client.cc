9b0183977899bde7624dc029fddc15d2b4c5f4d9
dtapuska <dtapuska@chromium.org>
2015-03-03 07:37:25 -0800

    ChromeOS DBus: leaderd API
    We adjusted the leaderd dbus API to pass back an ObjectPath instead of
    a string. Adjust the chromium bindings to match that.
    
    BUG=453872
    
    Review URL: https://codereview.chromium.org/972983003
    
    Cr-Commit-Position: refs/heads/master@{#318876}

--- chromeos/dbus/fake_leadership_daemon_manager_client.cc
+++ chromeos/dbus/fake_leadership_daemon_manager_client.cc
@@ -14,10 +14,15 @@ void StringDBBusMethodCallbackThunk(const StringDBusMethodCallback& callback) {
   callback.Run(DBUS_METHOD_CALL_SUCCESS, std::string());
 }
 
+void ObjectPathDBBusMethodCallbackThunk(
+    const ObjectPathDBusMethodCallback& callback) {
+  callback.Run(DBUS_METHOD_CALL_SUCCESS, dbus::ObjectPath());
+}
+
 void VoidDBBusMethodCallbackThunk(const VoidDBusMethodCallback& callback) {
   callback.Run(DBUS_METHOD_CALL_SUCCESS);
 }
-}
+}  // namespace
 
 FakeLeadershipDaemonManagerClient::FakeLeadershipDaemonManagerClient() {
 }
@@ -37,9 +42,9 @@ void FakeLeadershipDaemonManagerClient::RemoveObserver(Observer* observer) {
 void FakeLeadershipDaemonManagerClient::JoinGroup(
     const std::string& group,
     const base::DictionaryValue& options,
-    const StringDBusMethodCallback& callback) {
+    const ObjectPathDBusMethodCallback& callback) {
   base::MessageLoop::current()->PostTask(
-      FROM_HERE, base::Bind(&StringDBBusMethodCallbackThunk, callback));
+      FROM_HERE, base::Bind(&ObjectPathDBBusMethodCallbackThunk, callback));
 }
 
 void FakeLeadershipDaemonManagerClient::LeaveGroup(
