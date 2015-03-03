041435f8b661e6f0624c54ead42f5a6fd794f63c
tyoshino@chromium.org <tyoshino@chromium.org>
2015-03-03 10:17:36 +0000

    Prepare for removal of fail argument from WebSocketHandleClient::didConnect().
    Chromium side CL: https://codereview.chromium.org/950903004/
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/956563005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191184 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebSocketHandleClient.h
+++ third_party/WebKit/public/platform/WebSocketHandleClient.h
@@ -33,18 +33,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WebSocketHandleClient_h
 
 #include "WebCommon.h"
+#include "WebString.h"
 #include "public/platform/WebSocketHandle.h"
 
 namespace blink {
 
 class WebSocketHandshakeRequestInfo;
 class WebSocketHandshakeResponseInfo;
-class WebString;
 
 class WebSocketHandleClient {
 public:
     // Called when the handle is opened.
-    virtual void didConnect(WebSocketHandle*, bool fail, const WebString& selectedProtocol, const WebString& extensions) = 0;
+    virtual void didConnect(WebSocketHandle*, const WebString& selectedProtocol, const WebString& extensions) = 0;
+    // FIXME: Remove this once content side change is committed.
+    virtual void didConnect(WebSocketHandle* handle, bool fail, const WebString& selectedProtocol, const WebString& extensions)
+    {
+        if (fail) {
+            didFail(handle, WebString());
+        } else {
+            didConnect(handle, selectedProtocol, extensions);
+        }
+    }
 
     // Called when the browser starts the opening handshake.
     // This notification can be omitted when the inspector is not active.
