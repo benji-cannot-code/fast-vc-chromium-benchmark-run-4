40c75a2883240cf5d94d619b91570834d1bd9cfb
tiago.vignatti <tiago.vignatti@intel.com>
2015-03-03 09:31:58 -0800

    [Ozone-Dri] Make GbmDevice destructor private
    It fixes the following clang format message:
    
    In file included from ../../ui/ozone/platform/dri/gbm_buffer.cc:14:
    ../../ui/ozone/platform/dri/gbm_device.h:18:3: error: [chromium-style] Classes that are ref-counted should have destructors that are declared protected or private.
      ~GbmDevice() override;
      ^
    ../../ui/ozone/platform/dri/gbm_device.h:14:19: note: [chromium-style] 'GbmDevice' inherits from 'ui::DrmDevice' here
    class GbmDevice : public DrmDevice {
                      ^
    ../../ui/ozone/platform/dri/drm_device.h:41:32: note: [chromium-style] 'DrmDevice' inherits from 'base::RefCountedThreadSafe<DrmDevice>' here
    class OZONE_EXPORT DrmDevice : public base::RefCountedThreadSafe<DrmDevice> {
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/975773002
    
    Cr-Commit-Position: refs/heads/master@{#318899}

--- ui/ozone/platform/dri/gbm_device.h
+++ ui/ozone/platform/dri/gbm_device.h
@@ -16,7 +16,6 @@ class GbmDevice : public DrmDevice {
  public:
   GbmDevice(const base::FilePath& device_path);
   GbmDevice(const base::FilePath& device_path, base::File file);
-  ~GbmDevice() override;
 
   gbm_device* device() const { return device_; }
 
@@ -24,6 +23,8 @@ class GbmDevice : public DrmDevice {
   bool Initialize() override;
 
  private:
+  ~GbmDevice() override;
+
   gbm_device* device_;
 
   DISALLOW_COPY_AND_ASSIGN(GbmDevice);
