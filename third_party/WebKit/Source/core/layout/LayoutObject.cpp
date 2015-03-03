2be0e51a7e3cd1c36381bbc1a9b4af4ec24d3a53
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2015-03-03 04:28:31 +0000

    [Slimming paint] Separate display item client invalidation from invalidatePaintUsingContainer()
    Previously in slimming paint mode we invalidate display item clients in
    invalidatePaintUsingContainer(). This is not optimal, sometimes even
    incorrect:
    
    - We'll call invalidateDisplayItemClients() multiple times if
      invalidatePaintUsingContainer() is called multiple times;
    
    - invalidatePaintUsingContainer() doesn't always imply that we need to
      invalidate all display item clients of the object. Sometimes it is
      called just to invalidate some small part which may have dedicated
      display item client (e.g. scrollbars).
    
    Seperate display item client invalidation from
    invalidePaintUsingContainer() and invalidate display item clients only
    when needed.
    
    Review URL: https://codereview.chromium.org/970963002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191163 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutObject.cpp
@@ -1111,11 +1111,6 @@ void LayoutObject::invalidatePaintUsingContainer(const LayoutBoxModelObject* pai
     if (r.isEmpty())
         return;
 
-    if (RuntimeEnabledFeatures::slimmingPaintEnabled()) {
-        if (Layer* container = enclosingLayer()->enclosingLayerForPaintInvalidationCrossingFrameBoundaries())
-            invalidateDisplayItemClients(container->graphicsLayerBacking()->displayItemList());
-    }
-
     RELEASE_ASSERT(isRooted());
 
     // FIXME: Unify "devtools.timeline.invalidationTracking" and "blink.invalidation". crbug.com/413527.
@@ -1142,6 +1137,16 @@ void LayoutObject::invalidateDisplayItemClients(DisplayItemList* displayItemList
     displayItemList->invalidate(displayItemClient());
 }
 
+void LayoutObject::invalidateDisplayItemClientsUsingContainer(const LayoutBoxModelObject& paintInvalidationContainer) const
+{
+    ASSERT(RuntimeEnabledFeatures::slimmingPaintEnabled());
+
+    // This is valid because we want to invalidate the client in the display item list of the current graphics layer.
+    DisableCompositingQueryAsserts disabler;
+    if (GraphicsLayer* backing = paintInvalidationContainer.layer()->graphicsLayerBacking())
+        invalidateDisplayItemClients(backing->displayItemList());
+}
+
 LayoutRect LayoutObject::boundsRectForPaintInvalidation(const LayoutBoxModelObject* paintInvalidationContainer, const PaintInvalidationState* paintInvalidationState) const
 {
     if (!paintInvalidationContainer)
@@ -1161,6 +1166,8 @@ void LayoutObject::invalidatePaintRectangle(const LayoutRect& r) const
     const LayoutBoxModelObject* paintInvalidationContainer = containerForPaintInvalidation();
     Layer::mapRectToPaintInvalidationBacking(this, paintInvalidationContainer, dirtyRect);
     invalidatePaintUsingContainer(paintInvalidationContainer, dirtyRect, PaintInvalidationRectangle);
+    if (RuntimeEnabledFeatures::slimmingPaintEnabled() && !r.isEmpty())
+        invalidateDisplayItemClientsUsingContainer(*paintInvalidationContainer);
 }
 
 void LayoutObject::invalidateTreeIfNeeded(const PaintInvalidationState& paintInvalidationState)
@@ -1267,6 +1274,9 @@ PaintInvalidationReason LayoutObject::invalidatePaintIfNeeded(const PaintInvalid
 
     if (invalidationReason == PaintInvalidationNone)
         return invalidationReason;
+
+    if (RuntimeEnabledFeatures::slimmingPaintEnabled())
+        invalidateDisplayItemClientsUsingContainer(paintInvalidationContainer);
 
     if (invalidationReason == PaintInvalidationIncremental) {
         incrementallyInvalidatePaint(paintInvalidationContainer, oldBounds, newBounds, newLocation);
@@ -3149,6 +3159,8 @@ void LayoutObject::invalidatePaintIncludingNonCompositingDescendants()
 void LayoutObject::invalidatePaintIncludingNonCompositingDescendantsInternal(const LayoutBoxModelObject* paintInvalidationContainer)
 {
     invalidatePaintUsingContainer(paintInvalidationContainer, previousPaintInvalidationRect(), PaintInvalidationLayer);
+    if (RuntimeEnabledFeatures::slimmingPaintEnabled())
+        invalidateDisplayItemClientsUsingContainer(*paintInvalidationContainer);
 
     for (LayoutObject* child = slowFirstChild(); child; child = child->nextSibling()) {
         if (!child->isPaintInvalidationContainer())
