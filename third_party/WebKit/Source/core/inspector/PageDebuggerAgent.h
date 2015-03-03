584e5c9e1f73df0fb3444e2e536ee8e24b5bf0d1
yurys@chromium.org <yurys@chromium.org>
2015-03-03 13:40:29 +0000

    DevTools: drop untilReload parameter from Debugger.setSkipAllPauses command
    Instead all pauses are automatically disabled if reload was initiated by Page.reload protocol command.
    
    BUG=462572
    
    Review URL: https://codereview.chromium.org/971753003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191189 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/PageDebuggerAgent.h
+++ third_party/WebKit/Source/core/inspector/PageDebuggerAgent.h
@@ -56,7 +56,6 @@ public:
 
     void didStartProvisionalLoad(LocalFrame*);
     void didClearDocumentOfWindowObject(LocalFrame*);
-    void didCommitLoad(LocalFrame*, DocumentLoader*);
 
 protected:
     void enable() override;
