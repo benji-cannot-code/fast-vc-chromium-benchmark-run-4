c0c7d4e8507191688e0aff1c4cbe50f1811f476b
andresantoso <andresantoso@chromium.org>
2015-03-02 17:36:51 -0800

    MacViews: Unify web contents modal dialog types
    The #if around NativeWebContentsModalDialog is a problem for building
    the MacViews browser. Unify them to be gfx::NativeWindow on all platforms.
    Refactor the cocoa side to move code from ConstrainedWindowMac into
    SingleWebContentsDialogManagerCocoa, while keeping
    ConstrainedWindowMac's interface the same as before.
    The Views side only needs simple changes from NativeView to NativeWindow.
    
    BUG=425229
    
    Review URL: https://codereview.chromium.org/866263008
    
    Cr-Commit-Position: refs/heads/master@{#318813}

--- chrome/browser/ui/cocoa/certificate_viewer_mac.mm
+++ chrome/browser/ui/cocoa/certificate_viewer_mac.mm
@@ -198,6 +198,10 @@ void ShowCertificateViewer(content::WebContents* web_contents,
   // NOOP
 }
 
+- (NSWindow*)sheetWindow {
+  return panel_;
+}
+
 - (void)onConstrainedWindowClosed {
   panel_.reset();
   constrainedWindow_.reset();
