00bd4e66a9df8c73970d06a10f8f8c8db0df7245
brianderson <brianderson@chromium.org>
2015-03-02 17:33:08 -0800

    Change input latency result to depend on start of GPU swap
    Use INPUT_EVENT_GPU_SWAP_BUFFER_COMPONENT instead of
    INPUT_EVENT_LATENCY_TERMINATED_FRAME_SWAP_COMPONENT as the
    final component to calculate input latency.
    
    This will more accurately reflect latency regressions in the
    code we have direct control of in Chrome.
    
    Also make PassThroughImageTransportSurface::PostSubBuffer
    add a INPUT_EVENT_GPU_SWAP_BUFFER_COMPONENT, which it
    was missing before.
    
    Followup patches will snap
    INPUT_EVENT_LATENCY_TERMINATED_FRAME_SWAP_COMPONENT
    to the next vsync to reflect the real display time.
    
    The real display time can then be used for better
    smoothness metrics and also to track latency regressions
    at the OS/driver level for Ozone and potentially other
    platforms where we can extract swap info.
    
    BUG=360844
    
    Review URL: https://codereview.chromium.org/918883002
    
    Cr-Commit-Position: refs/heads/master@{#318812}

--- content/common/gpu/image_transport_surface.cc
+++ content/common/gpu/image_transport_surface.cc
@@ -192,8 +192,12 @@ bool PassThroughImageTransportSurface::SwapBuffers() {
   // GetVsyncValues before SwapBuffers to work around Mali driver bug:
   // crbug.com/223558.
   SendVSyncUpdateIfAvailable();
-  for (auto& latency : latency_info_)
-    latency.AddLatencyNumber(ui::INPUT_EVENT_GPU_SWAP_BUFFER_COMPONENT, 0, 0);
+
+  base::TimeTicks swap_time = base::TimeTicks::Now();
+  for (auto& latency : latency_info_) {
+    latency.AddLatencyNumberWithTimestamp(
+        ui::INPUT_EVENT_GPU_SWAP_BUFFER_COMPONENT, 0, 0, swap_time, 1);
+  }
 
   // We use WeakPtr here to avoid manual management of life time of an instance
   // of this class. Callback will not be called once the instance of this class
@@ -207,6 +211,13 @@ bool PassThroughImageTransportSurface::SwapBuffers() {
 bool PassThroughImageTransportSurface::PostSubBuffer(
     int x, int y, int width, int height) {
   SendVSyncUpdateIfAvailable();
+
+  base::TimeTicks swap_time = base::TimeTicks::Now();
+  for (auto& latency : latency_info_) {
+    latency.AddLatencyNumberWithTimestamp(
+        ui::INPUT_EVENT_GPU_SWAP_BUFFER_COMPONENT, 0, 0, swap_time, 1);
+  }
+
   // We use WeakPtr here to avoid manual management of life time of an instance
   // of this class. Callback will not be called once the instance of this class
   // is destroyed. However, this also means that the callback can be run on
@@ -217,9 +228,11 @@ bool PassThroughImageTransportSurface::PostSubBuffer(
 }
 
 void PassThroughImageTransportSurface::SwapBuffersCallBack() {
-  for (size_t i = 0; i < latency_info_.size(); i++) {
-    latency_info_[i].AddLatencyNumber(
-        ui::INPUT_EVENT_LATENCY_TERMINATED_FRAME_SWAP_COMPONENT, 0, 0);
+  base::TimeTicks swap_ack_time = base::TimeTicks::Now();
+  for (auto& latency : latency_info_) {
+    latency.AddLatencyNumberWithTimestamp(
+        ui::INPUT_EVENT_LATENCY_TERMINATED_FRAME_SWAP_COMPONENT, 0, 0,
+        swap_ack_time, 1);
   }
 
   helper_->stub()->SendSwapBuffersCompleted(latency_info_);
