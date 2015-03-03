49f83be63ce0d5289bbbe2d8164389378551ca21
ginkage <ginkage@chromium.org>
2015-03-03 08:38:16 -0800

    Install default apps if sync is disabled.
    BUG=424075
    TEST=manual
    
    Review URL: https://codereview.chromium.org/957753002
    
    Cr-Commit-Position: refs/heads/master@{#318894}

--- chrome/browser/extensions/external_pref_loader.cc
+++ chrome/browser/extensions/external_pref_loader.cc
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/defaults.h"
 #include "chrome/browser/prefs/pref_service_syncable.h"
+#include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/common/chrome_paths.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -114,12 +116,23 @@ const base::FilePath ExternalPrefLoader::GetBaseCrxFilePath() {
 
 void ExternalPrefLoader::StartLoading() {
   CHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  if (options_ & DELAY_LOAD_UNTIL_PRIORITY_SYNC) {
+  if ((options_ & DELAY_LOAD_UNTIL_PRIORITY_SYNC) &&
+      (profile_ && profile_->IsSyncAccessible())) {
     if (!PostLoadIfPrioritySyncReady()) {
       DCHECK(profile_);
       PrefServiceSyncable* prefs = PrefServiceSyncable::FromProfile(profile_);
       DCHECK(prefs);
       syncable_pref_observer_.Add(prefs);
+      ProfileSyncService* service =
+          ProfileSyncServiceFactory::GetForProfile(profile_);
+      DCHECK(service);
+      if (service->IsSyncEnabledAndLoggedIn() &&
+          (service->HasSyncSetupCompleted() ||
+           browser_defaults::kSyncAutoStarts)) {
+        service->AddObserver(this);
+      } else {
+        PostLoadAndRemoveObservers();
+      }
     }
   } else {
     BrowserThread::PostTask(
@@ -132,6 +145,15 @@ void ExternalPrefLoader::OnIsSyncingChanged() {
   PostLoadIfPrioritySyncReady();
 }
 
+void ExternalPrefLoader::OnStateChanged() {
+  ProfileSyncService* service =
+      ProfileSyncServiceFactory::GetForProfile(profile_);
+  DCHECK(service);
+  if (!service->IsSyncEnabledAndLoggedIn()) {
+    PostLoadAndRemoveObservers();
+  }
+}
+
 bool ExternalPrefLoader::PostLoadIfPrioritySyncReady() {
   DCHECK(options_ & DELAY_LOAD_UNTIL_PRIORITY_SYNC);
   DCHECK(profile_);
@@ -139,14 +161,26 @@ bool ExternalPrefLoader::PostLoadIfPrioritySyncReady() {
   PrefServiceSyncable* prefs = PrefServiceSyncable::FromProfile(profile_);
   DCHECK(prefs);
   if (prefs->IsPrioritySyncing()) {
-    BrowserThread::PostTask(
-        BrowserThread::FILE, FROM_HERE,
-        base::Bind(&ExternalPrefLoader::LoadOnFileThread, this));
-    syncable_pref_observer_.Remove(prefs);
+    PostLoadAndRemoveObservers();
     return true;
   }
 
   return false;
+}
+
+void ExternalPrefLoader::PostLoadAndRemoveObservers() {
+  PrefServiceSyncable* prefs = PrefServiceSyncable::FromProfile(profile_);
+  DCHECK(prefs);
+  syncable_pref_observer_.Remove(prefs);
+
+  ProfileSyncService* service =
+      ProfileSyncServiceFactory::GetForProfile(profile_);
+  DCHECK(service);
+  service->RemoveObserver(this);
+
+  BrowserThread::PostTask(
+      BrowserThread::FILE, FROM_HERE,
+      base::Bind(&ExternalPrefLoader::LoadOnFileThread, this));
 }
 
 void ExternalPrefLoader::LoadOnFileThread() {
--- chrome/browser/extensions/external_pref_loader.h
+++ chrome/browser/extensions/external_pref_loader.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/extensions/external_loader.h"
 #include "chrome/browser/prefs/pref_service_syncable_observer.h"
+#include "chrome/browser/sync/profile_sync_service.h"
+#include "chrome/browser/sync/profile_sync_service_observer.h"
 
 class PrefServiceSyncable;
 class Profile;
@@ -25,7 +27,8 @@ namespace extensions {
 // Instances of this class are expected to be created and destroyed on the UI
 // thread and they are expecting public method calls from the UI thread.
 class ExternalPrefLoader : public ExternalLoader,
-                           public PrefServiceSyncableObserver {
+                           public PrefServiceSyncableObserver,
+                           public ProfileSyncServiceObserver {
  public:
   enum Options {
     NONE = 0,
@@ -68,8 +71,14 @@ class ExternalPrefLoader : public ExternalLoader,
   // PrefServiceSyncableObserver:
   void OnIsSyncingChanged() override;
 
+  // ProfileSyncServiceObserver
+  void OnStateChanged() override;
+
   // If priority sync ready posts LoadOnFileThread and return true.
   bool PostLoadIfPrioritySyncReady();
+
+  // Post LoadOnFileThread and stop observing for sync service states.
+  void PostLoadAndRemoveObservers();
 
   // Actually searches for and loads candidate standalone extension preference
   // files in the path corresponding to |base_path_id|.
