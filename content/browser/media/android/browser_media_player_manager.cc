98f9bfc5be033f792526fc2f4c689320307ee92d
timav <timav@chromium.org>
2015-03-02 19:07:41 -0800

    Propagate audible state from player to the containing tab.
    This is part 1 (chromium),
    part 2 (clank) is https://chrome-internal-review.googlesource.com/#/c/195455/
    
    This CL is only about propagation/plumbing.
    Neither the reporting of audible state my players nor the consumption
    (UI notification) are implemented.
    
    This patch creates a new object AudioMonitorAndroid and
    attaches it to MediaWebContentsObserver. The AudioMonitorAndroid
    maintains the audible state for the tab. It receives notifications
    from MediaPlayerAndroid objects and sends the resulting
    notification to the WebContents.
    
    BUG=414810
    
    Review URL: https://codereview.chromium.org/896673003
    
    Cr-Commit-Position: refs/heads/master@{#318823}

--- content/browser/media/android/browser_media_player_manager.cc
+++ content/browser/media/android/browser_media_player_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "base/command_line.h"
 #include "content/browser/android/content_view_core_impl.h"
+#include "content/browser/android/media_players_observer.h"
 #include "content/browser/media/android/browser_demuxer_android.h"
 #include "content/browser/media/android/media_resource_getter_impl.h"
 #include "content/browser/renderer_host/render_view_host_impl.h"
@@ -58,10 +59,11 @@ void BrowserMediaPlayerManager::RegisterMediaUrlInterceptor(
 
 // static
 BrowserMediaPlayerManager* BrowserMediaPlayerManager::Create(
-    RenderFrameHost* rfh) {
+    RenderFrameHost* rfh,
+    MediaPlayersObserver* audio_monitor) {
   if (g_factory)
-    return g_factory(rfh);
-  return new BrowserMediaPlayerManager(rfh);
+    return g_factory(rfh, audio_monitor);
+  return new BrowserMediaPlayerManager(rfh, audio_monitor);
 }
 
 ContentViewCoreImpl* BrowserMediaPlayerManager::GetContentViewCore() const {
@@ -122,8 +124,10 @@ MediaPlayerAndroid* BrowserMediaPlayerManager::CreateMediaPlayer(
 }
 
 BrowserMediaPlayerManager::BrowserMediaPlayerManager(
-    RenderFrameHost* render_frame_host)
+    RenderFrameHost* render_frame_host,
+    MediaPlayersObserver* audio_monitor)
     : render_frame_host_(render_frame_host),
+      audio_monitor_(audio_monitor),
       fullscreen_player_id_(kInvalidMediaPlayerId),
       fullscreen_player_is_released_(false),
       web_contents_(WebContents::FromRenderFrameHost(render_frame_host)),
@@ -253,6 +257,12 @@ void BrowserMediaPlayerManager::OnVideoSizeChanged(
       width, height));
   if (fullscreen_player_id_ == player_id)
     video_view_->OnVideoSizeChanged(width, height);
+}
+
+void BrowserMediaPlayerManager::OnAudibleStateChanged(
+    int player_id, bool is_audible) {
+  audio_monitor_->OnAudibleStateChanged(
+      render_frame_host_, player_id, is_audible);
 }
 
 media::MediaResourceGetter*
@@ -519,6 +529,7 @@ void BrowserMediaPlayerManager::RemovePlayer(int player_id) {
     if ((*it)->player_id() == player_id) {
       ReleaseMediaResources(player_id);
       players_.erase(it);
+      audio_monitor_->RemovePlayer(render_frame_host_, player_id);
       break;
     }
   }
--- content/content_tests.gypi
+++ content/content_tests.gypi
@@ -1121,6 +1121,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'sources!': [
             'browser/geolocation/network_location_provider_unittest.cc',
             'browser/geolocation/wifi_data_provider_common_unittest.cc',
+            'browser/media/audio_stream_monitor_unittest.cc',
             'browser/webui/url_data_manager_backend_unittest.cc',
           ],
           'dependencies': [
