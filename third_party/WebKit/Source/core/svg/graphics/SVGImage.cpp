079305cf02a763b026164532e27ac245678a9efd
pdr@chromium.org <pdr@chromium.org>
2015-03-03 15:55:34 +0000

    Fix feImage crashes with slimming paint enabled.
    This patch slims feImage by creating a temporary display list for drawing
    feImage into.
    
    This patch fixes most of the remaining SVG crashes with slimming paint
    enabled. When running LayoutTests/svg with --slimming-paint-enabled:
    Before patch: 95/2630 fail (3.6%)
    After patch: 64/2630 (2.4%)
    
    Review URL: https://codereview.chromium.org/973533002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191198 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/graphics/SVGImage.cpp
+++ third_party/WebKit/Source/core/svg/graphics/SVGImage.cpp
@@ -53,7 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/LengthFunctions.h"
 #include "platform/TraceEvent.h"
 #include "platform/geometry/IntRect.h"
-#include "platform/graphics/GraphicsContextStateSaver.h"
+#include "platform/graphics/GraphicsContext.h"
 #include "platform/graphics/ImageBuffer.h"
 #include "platform/graphics/ImageObserver.h"
 #include "platform/graphics/paint/ClipRecorder.h"
