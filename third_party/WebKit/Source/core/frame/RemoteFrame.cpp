ee5bfa13b8e15feec7aeec3ea08e590beff4404f
lfg@chromium.org <lfg@chromium.org>
2015-03-04 22:07:16 +0000

    Fix use-after free when detaching RemoteFrames.
    The RemoteFrame can be destroyed in the middle of detaching it, so protect the a reference to the object while detaching it.
    
    TEST=NavigateRemoteFrame
    BUG=446575
    
    Review URL: https://codereview.chromium.org/921583006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191318 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/RemoteFrame.cpp
+++ third_party/WebKit/Source/core/frame/RemoteFrame.cpp
@@ -76,6 +76,10 @@ void RemoteFrame::reload(ReloadPolicy reloadPolicy, ClientRedirectPolicy clientR
 
 void RemoteFrame::detach()
 {
+    // Frame::detach() requires the caller to keep a reference to this, since
+    // otherwise it may clear the last reference to this, causing it to be
+    // deleted, which can cause a use-after-free.
+    RefPtrWillBeRawPtr<RemoteFrame> protect(this);
     detachChildren();
     if (!client())
         return;
