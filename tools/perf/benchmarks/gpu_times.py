4e54e6a01cc2e9a4c8d369b8390fa113e9db62ef
slamm <slamm@chromium.org>
2015-03-04 14:21:46 -0800

    Make discarding the first result possible through Benchmark.ValueCanBeAddedPredicate (add a "is_first_result" arg to it).
    The main motivation here is to eliminate special cases for PageTest methods/attributes in user_story_runner.Run.
    
    BUG=440101
    
    Review URL: https://codereview.chromium.org/962793004
    
    Cr-Commit-Position: refs/heads/master@{#319140}

--- tools/perf/benchmarks/gpu_times.py
+++ tools/perf/benchmarks/gpu_times.py
@@ -23,7 +23,7 @@ class _GPUTimes(benchmark.Benchmark):
         overhead_level=cat_filter)
 
   @classmethod
-  def ValueCanBeAddedPredicate(cls, value):
+  def ValueCanBeAddedPredicate(cls, value, _):
     return (isinstance(value, gpu_timeline.GPUTimelineListOfValues) or
             isinstance(value, gpu_timeline.GPUTimelineValue))
 
