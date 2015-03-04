0481d6158950f59cfba3d87a998ebe34cd27d3ef
skyostil@chromium.org <skyostil@chromium.org>
2015-03-04 20:38:54 +0000

    Add post(Delayed)Task helper which accepts bound functions
    This patch adds a version of WebThread::postTask and postDelayedTask
    which accept the result of WTF::bind(). This mirrors the interface in
    Chromium's base and makes it more convenient to schedule functions to
    run without having to add a wrapper class.
    
    BUG=450977
    
    Review URL: https://codereview.chromium.org/979813003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191307 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/WebThread.cpp
+++ third_party/WebKit/Source/platform/WebThread.cpp
@@ -8,12 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "public/platform/WebTraceLocation.h"
 #include "wtf/Assertions.h"
+#include "wtf/OwnPtr.h"
 
 #if OS(WIN)
 #include <windows.h>
 #elif OS(POSIX)
 #include <unistd.h>
 #endif
+
+namespace blink {
 
 namespace {
 #if OS(WIN)
@@ -23,5 +26,33 @@ static_assert(sizeof(blink::PlatformThreadId) >= sizeof(pid_t), "size of platfor
 #else
 #error Unexpected platform
 #endif
+
+class FunctionTask: public WebThread::Task {
+    WTF_MAKE_NONCOPYABLE(FunctionTask);
+public:
+    FunctionTask(PassOwnPtr<Function<void()>> function)
+        : m_function(function)
+    {
+    }
+
+    void run() override
+    {
+        (*m_function)();
+    }
+private:
+    OwnPtr<Function<void()>> m_function;
+};
+
+} // namespace
+
+void WebThread::postTask(const WebTraceLocation& location, PassOwnPtr<Function<void()>> function)
+{
+    postTask(location, new FunctionTask(function));
+}
+
+void WebThread::postDelayedTask(const WebTraceLocation& location, PassOwnPtr<Function<void()>> function, long long delayMs)
+{
+    postDelayedTask(location, new FunctionTask(function), delayMs);
+}
 
 } // namespace blink
--- third_party/WebKit/public/platform/WebThread.h
+++ third_party/WebKit/public/platform/WebThread.h
@@ -29,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebCommon.h"
 #include <stdint.h>
 
+#ifdef INSIDE_BLINK
+#include "wtf/Functional.h"
+#endif
+
 namespace blink {
 class WebTraceLocation;
 
@@ -76,6 +80,12 @@ public:
     virtual void exitRunLoop() = 0;
 
     virtual ~WebThread() { }
+
+#ifdef INSIDE_BLINK
+    // Helpers for posting bound functions as tasks.
+    void postTask(const WebTraceLocation&, PassOwnPtr<Function<void()>>);
+    void postDelayedTask(const WebTraceLocation&, PassOwnPtr<Function<void()>>, long long delayMs);
+#endif
 };
 
 } // namespace blink
