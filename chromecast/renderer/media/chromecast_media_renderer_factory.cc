4885baf232b58aa10934411340c33c355fb09aab
servolk <servolk@chromium.org>
2015-03-03 16:21:31 -0800

    Chromecast: Fix build in chromecast_media_renderer_factory.cc
    BUG=none
    
    Review URL: https://codereview.chromium.org/973263002
    
    Cr-Commit-Position: refs/heads/master@{#318975}

--- chromecast/renderer/media/chromecast_media_renderer_factory.cc
+++ chromecast/renderer/media/chromecast_media_renderer_factory.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/audio_hardware_config.h"
 #include "media/base/media_log.h"
 #include "media/renderers/default_renderer_factory.h"
+#include "media/renderers/gpu_video_accelerator_factories.h"
 
 namespace chromecast {
 namespace media {
