a67092645d7c6ed6614a3dc8cde68187981531af
newt <newt@chromium.org>
2015-03-03 21:20:22 -0800

    Fix crash on certain devices when showing week and month picker.
    This skips a method call on devices where it would cause a crash.
    
    BUG=463719
    
    Review URL: https://codereview.chromium.org/969413003
    
    Cr-Commit-Position: refs/heads/master@{#319020}

--- ui/android/java/src/org/chromium/ui/picker/TwoFieldDatePicker.java
+++ ui/android/java/src/org/chromium/ui/picker/TwoFieldDatePicker.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.ui.picker;
 
 import android.content.Context;
+import android.os.Build;
 import android.text.format.DateFormat;
 import android.text.format.DateUtils;
 import android.view.LayoutInflater;
@@ -112,7 +113,12 @@ public abstract class TwoFieldDatePicker extends FrameLayout {
         mYearSpinner.setOnLongPressUpdateInterval(100);
         mYearSpinner.setOnValueChangedListener(onChangeListener);
 
-        reorderSpinners();
+        // TODO(tobiasjs): reorderSpinners causes a crash on Android versions before JB MR2 because
+        // it calls DateFormat.getBestDateTimePattern() which isn't available before then. Fix this
+        // crash and call reorderSpinners on all devices. http://crbug.com/463719
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            reorderSpinners();
+        }
     }
 
     /**
