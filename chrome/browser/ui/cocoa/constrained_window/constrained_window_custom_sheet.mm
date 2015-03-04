8ee930e1ff35a832d09f5d03c171753cb48581a3
Daniel Xie <dxie@google.com>
2015-03-04 13:02:33 -0800

    Revert "Revert "MacViews: Unify web contents modal dialog types""
    This reverts commit d87b783639782496ff2f5826a226df6519a3595f.
    
    Merge branch 'master' of https://chromium.googlesource.com/a/chromium/src
    
    Revert "MacViews: Unify web contents modal dialog types"
    
    speculatively reverting this to solve browser CPM
    
    BUG=463776
    
    This reverts commit 5f1263bb318f072343f59dec4b310a522c80b9a2.
    
    Merge branch 'master' of https://chromium.googlesource.com/a/chromium/src
    
    Revert "Handle unsucessful JsonPrefStore initialization in SupervisedUserSettingsService."
    
    this may be casuing an increase in rendererexit code.
    
    BUG=463646
    
    This reverts commit 0197d5dfe8b8105b0263dbe80f6079020a6ccf5d.
    
    Merge branch 'master' of https://chromium.googlesource.com/a/chromium/src
    
    This is causing a huge amounts of renderer crashes because of RESULT_CODE_KILLED_BAD_MESSAGE.  Reverting this from master.
    
    Revert "<webview>: Removed ResolveURL"
    
    This reverts commit c2fc6144ed227bf2d6bc7791c910a2d4f3c58418.
    
    Cr-Commit-Position: refs/heads/master@{#319124}

--- chrome/browser/ui/cocoa/constrained_window/constrained_window_custom_sheet.mm
+++ chrome/browser/ui/cocoa/constrained_window/constrained_window_custom_sheet.mm
@@ -74,4 +74,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [customWindow_ setFrameOrigin:origin];
 }
 
+- (NSWindow*)sheetWindow {
+  return customWindow_;
+}
+
 @end
