53b20bdea3067e8fae5ad795e886b625fec50eec
keishi@chromium.org <keishi@chromium.org>
2015-03-04 05:09:53 +0000

    Speculative fix for null crash in SelectorChecker::checkScrollbarPseudoClass
    BUG=400997
    
    Review URL: https://codereview.chromium.org/966983005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191244 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/SelectorChecker.cpp
+++ third_party/WebKit/Source/core/css/SelectorChecker.cpp
@@ -1117,7 +1117,7 @@ bool SelectorChecker::checkScrollbarPseudoClass(const SelectorCheckingContext& c
             return false;
         }
     case CSSSelector::PseudoCornerPresent:
-        return scrollbar->scrollableArea()->isScrollCornerVisible();
+        return scrollbar->scrollableArea() && scrollbar->scrollableArea()->isScrollCornerVisible();
     default:
         return false;
     }
