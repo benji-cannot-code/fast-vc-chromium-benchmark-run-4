e76c0325c803540f744e8b0da7da52cf3cce0ceb
avi <avi@chromium.org>
2015-03-04 09:12:21 -0800

    Do not cache NSScreens.
    BUG=463458
    TEST=crashes go away
    
    Review URL: https://codereview.chromium.org/976903003
    
    Cr-Commit-Position: refs/heads/master@{#319076}

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_controller.h
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_controller.h
@@ -137,9 +137,6 @@ class Profile;
   // incomplete animations do not cause valgrind complaints.
   BOOL ignoreAnimations_;
 
-  // The screen to which the menu should be restricted.
-  NSScreen* screen_;
-
   int selectedIndex_;
   NSString* typedPrefix_;
 
