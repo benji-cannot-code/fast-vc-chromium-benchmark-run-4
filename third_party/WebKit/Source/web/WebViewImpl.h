9dfcddd40f77637dbbb7f236a3fe87a1b742e91b
trchen@chromium.org <trchen@chromium.org>
2015-03-04 04:28:13 +0000

    Properly clamp page scale factor to 1 in fullscreen and reset layout size
    This CL fixes a bug that fullscreen media control won't be reachable when
    the viewport tag specifies a fixed width smaller than the screen width.
    The CL does the following two things:
    
    1. Besides settings current page scale factor to 1, also temporarily force
    minimum and maximum scale factor to 1 during fullscreen. This is required
    because with such viewport tag describe above, the computed minimum scale
    factor will be bigger than 1. If we layout fullscreen contents with respect
    to screen size, it will be magnified so some of the contents won't be visible.
    
    2. Force the layout size to the size of the screen during fullscreen. This is
    required because otherwise the main frame RenderView will be small than the
    screen size, and will clip everything below it due to overflow:hidden.
    
    BUG=450524
    
    Review URL: https://codereview.chromium.org/967413002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191236 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebViewImpl.h
+++ third_party/WebKit/Source/web/WebViewImpl.h
@@ -532,6 +532,8 @@ public:
 
     IntSize mainFrameSize();
 
+    PageScaleConstraintsSet& pageScaleConstraintsSet() { return m_pageScaleConstraintsSet; }
+
 private:
     // TODO(bokan): Remains for legacy pinch. Remove once it's gone. Made private to
     // prevent external usage
