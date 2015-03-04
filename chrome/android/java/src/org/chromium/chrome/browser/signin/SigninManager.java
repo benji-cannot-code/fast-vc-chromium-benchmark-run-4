8a46318c8dbabf4c9dc5f30b2f53e5b152113491
maxbogue <maxbogue@chromium.org>
2015-03-04 10:53:39 -0800

    Move ownership of SigninNotificationController upstream.
    Previously owned by GSM downstream. Note that there will be
    two SigninNotificationControllers in existence between this
    landing and the downstream patch landing, but the only call
    its listener makes (cancelNotification) appears to be
    idempotent so it shouldn't matter.
    
    BUG=428882
    
    Review URL: https://codereview.chromium.org/952083002
    
    Cr-Commit-Position: refs/heads/master@{#319091}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManager.java
@@ -26,6 +26,7 @@ import org.chromium.base.ObserverList;
 import org.chromium.base.ThreadUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.invalidation.InvalidationController;
+import org.chromium.chrome.browser.notifications.GoogleServicesNotificationController;
 import org.chromium.chrome.browser.sync.ProfileSyncService;
 import org.chromium.sync.AndroidSyncSettings;
 import org.chromium.sync.internal_api.pub.base.ModelType;
@@ -66,6 +67,8 @@ public class SigninManager {
             new ObserverList<SignInStateObserver>();
     private final ObserverList<SignInAllowedObserver> mSignInAllowedObservers =
             new ObserverList<SignInAllowedObserver>();
+
+    private final SigninNotificationController mSigninNotificationController;
 
     private Activity mSignInActivity;
     private Account mSignInAccount;
@@ -165,6 +168,13 @@ public class SigninManager {
         mContext = context.getApplicationContext();
         mNativeSigninManagerAndroid = nativeInit();
         mSigninAllowedByPolicy = nativeIsSigninAllowedByPolicy(mNativeSigninManagerAndroid);
+
+        // Setup notification system for Google services. This includes both sign-in and sync.
+        GoogleServicesNotificationController controller =
+                GoogleServicesNotificationController.get(mContext);
+        mSigninNotificationController = new SigninNotificationController(
+                mContext, controller, AccountManagementFragment.class);
+        ChromeSigninController.get(mContext).addListener(mSigninNotificationController);
     }
 
     /**
@@ -228,6 +238,13 @@ public class SigninManager {
                 }
             }
         });
+    }
+
+    /**
+     * Return the SigninNotificationController.
+     */
+    public SigninNotificationController getSigninNotificationController() {
+        return mSigninNotificationController;
     }
 
     /**
