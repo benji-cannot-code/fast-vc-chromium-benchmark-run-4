9370b95ed25f66abbfe36e1c937d872019bedc2a
yutak <yutak@chromium.org>
2015-03-03 23:17:31 -0800

    GCPlugin: Recognize templated registerWeakMembers() call.
    After the inlined trace transition, the callee part of registerWeakMembers()
    call can be of type CXXDependentScopeMemberExpr instead of UnresolvedMemberExpr.
    The plugin should be aware of this case.
    
    BUG=462511
    R=kouhei@chromium.org
    CC=zerny@chromium.org, oilpan-reviews@chromium.org
    
    Review URL: https://codereview.chromium.org/975683003
    
    Cr-Commit-Position: refs/heads/master@{#319024}

---
+++ tools/clang/blink_gc_plugin/tests/register_weak_members_template.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REGISTER_WEAK_MEMBERS_TEMPLATE_H_
+#define REGISTER_WEAK_MEMBERS_TEMPLATE_H_
+
+#include "heap/stubs.h"
+
+namespace blink {
+
+class X : public GarbageCollected<X> {
+ public:
+  void trace(Visitor* visitor) { traceImpl(visitor); }
+  void trace(InlinedGlobalMarkingVisitor visitor) { traceImpl(visitor); }
+
+ private:
+  template <typename VisitorDispatcher>
+  void traceImpl(VisitorDispatcher visitor) {}
+};
+
+class HasUntracedWeakMembers : public GarbageCollected<HasUntracedWeakMembers> {
+ public:
+  void trace(Visitor* visitor) { traceImpl(visitor); }
+  void trace(InlinedGlobalMarkingVisitor visitor) { traceImpl(visitor); }
+
+  // Don't have to be defined for the purpose of this test.
+  void clearWeakMembers(Visitor* visitor);
+
+ private:
+  template <typename VisitorDispatcher>
+  void traceImpl(VisitorDispatcher visitor) {
+    visitor->template registerWeakMembers<
+        HasUntracedWeakMembers,
+        &HasUntracedWeakMembers::clearWeakMembers>(this);
+  }
+
+  WeakMember<X> x_;
+};
+
+}
+
+#endif  // REGISTER_WEAK_MEMBERS_TEMPLATE_H_
