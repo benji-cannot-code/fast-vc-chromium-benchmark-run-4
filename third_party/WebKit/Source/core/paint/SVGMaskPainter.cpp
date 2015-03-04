430e4a64a285d86b42010c0e2a2760bc98df31ec
ed@opera.com <ed@opera.com>
2015-03-04 09:20:07 +0000

    [S.P.] Merge getContentPicture into createContentPicture.
    BUG=462077
    
    Review URL: https://codereview.chromium.org/974903003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191268 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/SVGMaskPainter.cpp
+++ third_party/WebKit/Source/core/paint/SVGMaskPainter.cpp
@@ -69,7 +69,7 @@ void SVGMaskPainter::drawMaskForRenderer(GraphicsContext* context, DisplayItemCl
     ASSERT(context);
 
     AffineTransform contentTransformation;
-    RefPtr<const SkPicture> maskContentPicture = m_mask.getContentPicture(contentTransformation, targetBoundingBox);
+    RefPtr<const SkPicture> maskContentPicture = m_mask.createContentPicture(contentTransformation, targetBoundingBox);
 
     TransformRecorder recorder(*context, client, contentTransformation);
 
