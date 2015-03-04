9e4f991550995e1a066ff7f1039d0ff8d92a86c7
rvargas <rvargas@chromium.org>
2015-03-04 14:38:20 -0800

    Remove base::KillProcessById
    BUG=417532
    
    Review URL: https://codereview.chromium.org/970943003
    
    Cr-Commit-Position: refs/heads/master@{#319143}

--- net/tools/stress_cache/stress_cache.cc
+++ net/tools/stress_cache/stress_cache.cc
@@ -26,8 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/message_loop/message_loop.h"
 #include "base/path_service.h"
-#include "base/process/kill.h"
 #include "base/process/launch.h"
+#include "base/process/process.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -204,8 +204,8 @@ void CrashCallback() {
   if (rand() % 100 > 30) {
     printf("sweet death...\n");
 #if defined(OS_WIN)
-    // Windows does more work on _exit() that we would like, so we use Kill.
-    base::KillProcessById(base::GetCurrentProcId(), kExpectedCrash, false);
+    // Windows does more work on _exit() than we would like.
+    base::Process::Current().Terminate(kExpectedCrash);
 #elif defined(OS_POSIX)
     // On POSIX, _exit() will terminate the process with minimal cleanup,
     // and it is cleaner than killing.
--- win8/delegate_execute/delegate_execute_operation.h
+++ win8/delegate_execute/delegate_execute_operation.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/files/file_path.h"
+#include "base/process/process.h"
 #include "base/strings/string16.h"
 
 namespace base {
@@ -60,8 +61,8 @@ class DelegateExecuteOperation {
     return mutex_;
   }
 
-  // Returns the process id of the parent or 0 on failure.
-  DWORD GetParentPid() const;
+  // Returns the parent process.
+  base::Process GetParent() const;
 
   const base::FilePath& shortcut() const {
     return relaunch_shortcut_;
