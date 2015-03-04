bdbb551c0046369f124f3c69914209c817d1bbd8
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2015-03-04 05:14:48 +0000

    [Slimming paint] Check for under-invalidations
    Because of the different model of paint invalidation, the current paint
    invalidation architecture may not generate enough per-object paint
    invalidations for slimming paint, and this might be the main source of
    bugs.
    
    Add RuntimeEnabledFeatures::slimmingPaintUnderInvalidationChecking to
    check under-invalidations.
    
    - If the feature is enabled, we'll always generate new pictures for all
      display items;
    - It's an under-invalidation if non-invalidated client
      - generates a new picture that is not cached;
      - generates a different picture;
        or
      - generates no new picture for a display item (meaning that the client
        no longer has the paint phase, or the client has been deleted)
    
    BUG=450725
    
    Review URL: https://codereview.chromium.org/968523002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191245 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/paint/DisplayItemList.h
+++ third_party/WebKit/Source/platform/graphics/paint/DisplayItemList.h
@@ -57,6 +57,9 @@ private:
 
     void updatePaintList();
 
+    void checkCachedDisplayItemIsUnchangedFromPreviousPaintList(const DisplayItem&);
+    void checkNoRemainingCachedDisplayItems();
+
 #ifndef NDEBUG
     WTF::String paintListAsDebugString(const PaintList&) const;
 #endif
