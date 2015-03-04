965f49a345b7f2e44a175ef4a48a9c627034ba32
finnur <finnur@chromium.org>
2015-03-04 11:19:28 -0800

    Ensure initial state of javascript toggle is correct.
    Fixes crash found when toggling the state.
    
    BUG=463062
    
    Review URL: https://codereview.chromium.org/969033004
    
    Cr-Commit-Position: refs/heads/master@{#319099}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/WebsitePreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/WebsitePreferences.java
@@ -522,6 +522,8 @@ public class WebsitePreferences extends PreferenceFragment
                             LocationSettings.getInstance().isChromeLocationSettingEnabled());
                 } else if (mFilter.showCameraMicSites(mCategoryFilter)) {
                     globalToggle.setChecked(PrefServiceBridge.getInstance().isCameraMicEnabled());
+                } else if (mFilter.showJavaScriptSites(mCategoryFilter)) {
+                    globalToggle.setChecked(PrefServiceBridge.getInstance().javaScriptEnabled());
                 } else if (mFilter.showPopupSites(mCategoryFilter)) {
                     globalToggle.setChecked(PrefServiceBridge.getInstance().popupsEnabled());
                 } else if (mFilter.showPushNotificationsSites(mCategoryFilter)) {
