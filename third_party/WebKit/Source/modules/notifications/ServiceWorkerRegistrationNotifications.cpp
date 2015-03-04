219615a07b59a3c38f11f4d729aaa1f3e06bb825
wesleylancel@gmail.com <wesleylancel@gmail.com>
2015-03-04 04:14:03 +0000

    Pass correct direction to service worker notifications
    Also adds a layout test confirming RTL is passed correctly.
    
    BUG= 442128
    
    Review URL: https://codereview.chromium.org/973183002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191231 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/notifications/ServiceWorkerRegistrationNotifications.cpp
+++ third_party/WebKit/Source/modules/notifications/ServiceWorkerRegistrationNotifications.cpp
@@ -38,7 +38,6 @@ ScriptPromise ServiceWorkerRegistrationNotifications::showNotification(ScriptSta
     ScriptPromise promise = resolver->promise();
 
     // FIXME: Do the appropriate CORS checks on the icon URL.
-    // FIXME: Determine the text direction based on the options dictionary.
 
     KURL iconUrl;
     if (options.hasIcon() && !options.icon().isEmpty()) {
@@ -47,7 +46,8 @@ ScriptPromise ServiceWorkerRegistrationNotifications::showNotification(ScriptSta
             iconUrl = KURL();
     }
 
-    WebNotificationData notification(title, WebNotificationData::DirectionLeftToRight, options.lang(), options.body(), options.tag(), iconUrl);
+    WebNotificationData::Direction dir = options.dir() == "rtl" ? WebNotificationData::DirectionRightToLeft : WebNotificationData::DirectionLeftToRight;
+    WebNotificationData notification(title, dir, options.lang(), options.body(), options.tag(), iconUrl);
     WebNotificationShowCallbacks* callbacks = new CallbackPromiseAdapter<void, void>(resolver);
 
     SecurityOrigin* origin = executionContext->securityOrigin();
