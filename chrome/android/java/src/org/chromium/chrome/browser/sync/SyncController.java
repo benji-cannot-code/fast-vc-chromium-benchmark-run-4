93f0543f76615a77135aee4f8ca67931d5342aa2
maxbogue <maxbogue@chromium.org>
2015-03-04 11:16:36 -0800

    Register sync session ID in SyncController.
    Removing GSM.setupSessionSyncId() part 3/4. Generate the sync
    sessions ID in SyncController and cleanup ChromeShellActivity.
    
    BUG=428882
    
    Review URL: https://codereview.chromium.org/951773003
    
    Cr-Commit-Position: refs/heads/master@{#319097}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/SyncController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/SyncController.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.util.Log;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.chrome.browser.identity.UniqueIdentificationGeneratorFactory;
 import org.chromium.chrome.browser.invalidation.InvalidationController;
 import org.chromium.chrome.browser.signin.SigninManager;
 import org.chromium.chrome.browser.signin.SigninManager.SignInFlowObserver;
@@ -61,6 +62,9 @@ public class SyncController implements ProfileSyncService.SyncStateChangedListen
         mProfileSyncService = ProfileSyncService.get(mContext);
         mProfileSyncService.addSyncStateChangedListener(this);
         mChromeSigninController.ensureGcmIsInitialized();
+        // Set the sessions ID using the generator that was registered for GENERATOR_ID.
+        mProfileSyncService.setSessionsId(
+                UniqueIdentificationGeneratorFactory.getInstance(GENERATOR_ID));
     }
 
     /**
