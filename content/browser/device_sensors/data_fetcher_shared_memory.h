adcfeb3d02ef3182cc8c65a02352d23a328d7e9f
rijubrata.bhaumik <rijubrata.bhaumik@intel.com>
2015-03-04 05:31:11 -0800

    Windows: Ambient Light API
    TEST= http://jsfiddle.net/73v8rmex/3/ (manual)
    
    BUG=336424
    
    Review URL: https://codereview.chromium.org/754963006
    
    Cr-Commit-Position: refs/heads/master@{#319060}

--- content/browser/device_sensors/data_fetcher_shared_memory.h
+++ content/browser/device_sensors/data_fetcher_shared_memory.h
@@ -62,6 +62,7 @@ class CONTENT_EXPORT DataFetcherSharedMemory
   class SensorEventSink;
   class SensorEventSinkMotion;
   class SensorEventSinkOrientation;
+  class SensorEventSinkLight;
 
   virtual FetcherType GetType() const override;
 
@@ -73,6 +74,7 @@ class CONTENT_EXPORT DataFetcherSharedMemory
   base::win::ScopedComPtr<ISensor> sensor_inclinometer_;
   base::win::ScopedComPtr<ISensor> sensor_accelerometer_;
   base::win::ScopedComPtr<ISensor> sensor_gyrometer_;
+  base::win::ScopedComPtr<ISensor> sensor_light_;
 #endif
 
   DISALLOW_COPY_AND_ASSIGN(DataFetcherSharedMemory);
