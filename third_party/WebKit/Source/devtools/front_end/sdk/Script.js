9e75ba5adb70c867b04368855c45ecd92c483473
sergeyv@chromium.org <sergeyv@chromium.org>
2015-03-04 15:29:02 +0000

    Devtools: remove isSnippet function from sdk
    BUG=462924
    
    Review URL: https://codereview.chromium.org/980523003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191289 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/Script.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/Script.js
@@ -60,8 +60,6 @@ WebInspector.Script.Events = {
     SourceMapURLAdded: "SourceMapURLAdded",
 }
 
-WebInspector.Script.snippetSourceURLPrefix = "snippets:///";
-
 WebInspector.Script.sourceURLRegex = /\n[\040\t]*\/\/[@#]\ssourceURL=\s*(\S*?)\s*$/mg;
 
 /**
@@ -255,14 +253,6 @@ WebInspector.Script.prototype = {
     isAnonymousScript: function()
     {
         return !this.sourceURL;
-    },
-
-    /**
-     * @return {boolean}
-     */
-    isSnippet: function()
-    {
-        return !!this.sourceURL && this.sourceURL.startsWith(WebInspector.Script.snippetSourceURLPrefix);
     },
 
     /**
