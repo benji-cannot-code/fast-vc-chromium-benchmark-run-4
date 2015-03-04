3513e27ff15011148f04a630e510b4666b183641
andresantoso <andresantoso@chromium.org>
2015-03-04 14:28:51 -0800

    MacViews: Fix crash when EventMonitorMac receives a system defined event.
    EventFromNative() returns null when given a system defined event, so filter them
    out when monitoring the event stream.
    When running the experimental MacViews browser, I was seeing an event
    with type=SysDefined subtype=7 (apparently it means that a mouse button
    state has changed).
    
    BUG=425229
    
    Review URL: https://codereview.chromium.org/981693002
    
    Cr-Commit-Position: refs/heads/master@{#319142}

--- ui/events/cocoa/events_mac.mm
+++ ui/events/cocoa/events_mac.mm
@@ -53,9 +53,10 @@ EventType EventTypeFromNative(const base::NativeEvent& native_event) {
       return ET_MOUSE_EXITED;
     case NSEventTypeSwipe:
       return ET_SCROLL_FLING_START;
-    case NSFlagsChanged:
     case NSAppKitDefined:
     case NSSystemDefined:
+      return ET_UNKNOWN;
+    case NSFlagsChanged:
     case NSApplicationDefined:
     case NSPeriodic:
     case NSCursorUpdate:
