ac668fdbd7e855f5d212c3080472904bccce758f
rouslan <rouslan@chromium.org>
2015-03-05 14:21:38 -0800

    [a11y] Announce autofill and password generation popups.
    BUG=455006,455602
    
    Review URL: https://codereview.chromium.org/944443002
    
    Cr-Commit-Position: refs/heads/master@{#319341}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/AutofillTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/AutofillTest.java
@@ -39,7 +39,7 @@ public class AutofillTest extends ChromeShellTestBase {
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        ChromeShellActivity activity = launchChromeShellWithBlankPage();
+        final ChromeShellActivity activity = launchChromeShellWithBlankPage();
         assertNotNull(activity);
         waitForActiveShellToBeDoneLoading();
 
@@ -51,9 +51,7 @@ public class AutofillTest extends ChromeShellTestBase {
         ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
-                mAutofillPopup = new AutofillPopup(mWindowAndroid.getActivity().get(),
-                        viewDelegate,
-                        mMockAutofillCallback);
+                mAutofillPopup = new AutofillPopup(activity, viewDelegate, mMockAutofillCallback);
                 mAutofillPopup.filterAndShow(new AutofillSuggestion[0], false);
                 mAutofillPopup.setAnchorRect(50, 500, 500, 50);
             }
