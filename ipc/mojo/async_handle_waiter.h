d19fbc8a3c9b1ca39d8c292b4f2b3c39cc268bea
morrita <morrita@chromium.org>
2015-03-04 19:06:34 -0800

    Make AsyncHandleWaiter reenterancy-safe
    On Windows, message pump can re-enter. AsyncHandleWaiter should
    be prepared for that.
    
    R=viettrungluu@chromium.org
    BUG=none
    TEST=ExtensionApiTest.SharedModuleWhitelist (this hits the DCHECK)
    
    Review URL: https://codereview.chromium.org/973213002
    
    Cr-Commit-Position: refs/heads/master@{#319202}

--- ipc/mojo/async_handle_waiter.h
+++ ipc/mojo/async_handle_waiter.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "base/message_loop/message_loop.h"
 #include "ipc/ipc_export.h"
 #include "third_party/mojo/src/mojo/public/c/system/types.h"
 
@@ -32,6 +33,9 @@ class IPC_MOJO_EXPORT AsyncHandleWaiter {
   ~AsyncHandleWaiter();
 
   MojoResult Wait(MojoHandle handle, MojoHandleSignals signals);
+
+  base::MessageLoopForIO::IOObserver* GetIOObserverForTest();
+  base::Callback<void(MojoResult)> GetWaitCallbackForTest();
 
  private:
   void InvokeCallback(MojoResult result);
