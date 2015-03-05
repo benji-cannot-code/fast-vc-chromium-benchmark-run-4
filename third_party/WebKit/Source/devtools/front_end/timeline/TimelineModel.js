4f4bb57b3a4ad6e9d98bd25f8ea1c37a0f687923
alph@chromium.org <alph@chromium.org>
2015-03-05 12:48:25 +0000

    DevTools: Merge compositor and worker threads in timeline overview
    All compositor threads are merged into a single band. Same for the
    worker threads.
    
    Dithering is moved unchanged to the bottom of file.
    
    BUG=460072
    
    Review URL: https://codereview.chromium.org/977343002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191357 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineModel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineModel.js
@@ -221,6 +221,16 @@ WebInspector.TimelineModel.VirtualThread = function(name)
     this.asyncEvents = [];
 }
 
+WebInspector.TimelineModel.VirtualThread.prototype = {
+    /**
+     * @return {boolean}
+     */
+    isWorker: function()
+    {
+        return this.name === "WebCore: Worker";
+    }
+}
+
 /**
  * @constructor
  * @param {!WebInspector.TimelineModel} model
