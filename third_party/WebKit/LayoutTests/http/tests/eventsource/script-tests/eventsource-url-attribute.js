c770d2613e8e8b2b1dcec91480230bd6dd3ad15e
jsbell@chromium.org <jsbell@chromium.org>
2015-03-05 06:06:14 +0000

    Remove deprecated URL attribute alias from EventSource interface
    Intent to Remove:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/ZS28i6itZEw
    
    R=philipj@opera.com
    BUG=460722
    
    Review URL: https://codereview.chromium.org/973873002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191340 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/eventsource/script-tests/eventsource-url-attribute.js
+++ third_party/WebKit/LayoutTests/http/tests/eventsource/script-tests/eventsource-url-attribute.js
@@ -2,12 +2,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 if (self.importScripts)
     importScripts("/js-test-resources/js-test.js");
 
-description("Both .URL and .url should work (for compatibility reasons).");
+description("Only .url should work, previously supported .URL should not.");
 
 var url = "http://127.0.0.1:8000/eventsource/resources/event-stream.php";
 var source = new EventSource(url);
 
-shouldBeEqualToString("source.URL", url);
 shouldBeEqualToString("source.url", url);
-shouldBeTrue("source.URL === source.url");
+shouldBeUndefined("source.URL");
 finishJSTest();
