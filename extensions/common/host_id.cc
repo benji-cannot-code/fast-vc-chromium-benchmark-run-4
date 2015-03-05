deb5c1f1e7a4abd5ea14b54ce7bcfb00ea009cfb
hanxi <hanxi@chromium.org>
2015-03-05 13:25:53 -0800

    Fix a bug in UserScriptLoader.
    1. Fix a misuse of a released pointer in UserScriptLoader;
    2. Add initilaztions for the variables that will be picked in UserScript.
    
    BUG=461052
    
    Review URL: https://codereview.chromium.org/982113002
    
    Cr-Commit-Position: refs/heads/master@{#319329}

--- extensions/common/host_id.cc
+++ extensions/common/host_id.cc
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/common/host_id.h"
 
-HostID::HostID() {
+HostID::HostID()
+    : type_(HostType::EXTENSIONS) {
 }
 
 HostID::HostID(HostType type, const std::string& id)
