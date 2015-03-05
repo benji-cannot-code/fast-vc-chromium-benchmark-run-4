beac3585e13578df9a932c099e9b0b71db225ccc
sujiths.s <sujiths.s@samsung.com>
2015-03-04 23:03:33 -0800

    Remove Unused setTopControlsContentOffset function
    Removed the unused function setTopControlsContentOffset. since it
    became unused after introducing the setTopControlsShownRatio.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/981483002
    
    Cr-Commit-Position: refs/heads/master@{#319230}

--- content/renderer/gpu/render_widget_compositor.cc
+++ content/renderer/gpu/render_widget_compositor.cc
@@ -773,10 +773,6 @@ void RenderWidgetCompositor::setTopControlsHeight(float height, bool shrink) {
     layer_tree_host_->SetTopControlsHeight(height, shrink);
 }
 
-void RenderWidgetCompositor::setTopControlsContentOffset(float offset) {
-  setTopControlsShownRatio(offset);
-}
-
 void RenderWidgetCompositor::setTopControlsShownRatio(float ratio) {
   layer_tree_host_->SetTopControlsShownRatio(ratio);
 }
