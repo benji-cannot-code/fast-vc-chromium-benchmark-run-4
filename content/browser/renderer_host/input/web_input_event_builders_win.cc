16d537767f9de2e482b4e695436fc24cafcdef72
ananta <ananta@chromium.org>
2015-03-04 17:01:46 -0800

    Ensure that pointer lock works correctly in Windows 7+ with HiDPI scale factors above 1
    We were not scaling the mouse coordinate by the scale factor before invoking the ClientToScreen API. With this
    change the blink::WebMouseEvent.globalX and globalY values have to be converted back to DIP after convering them to screen.
    
    This should fix the mouse locking issue.
    
    BUG=411634
    TEST= The main test case in bug 411634
    
    Review URL: https://codereview.chromium.org/973123003
    
    Cr-Commit-Position: refs/heads/master@{#319181}

--- content/browser/renderer_host/input/web_input_event_builders_win.cc
+++ content/browser/renderer_host/input/web_input_event_builders_win.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "content/browser/renderer_host/input/web_input_event_util.h"
+#include "ui/gfx/win/dpi.h"
 
 using blink::WebInputEvent;
 using blink::WebKeyboardEvent;
@@ -251,11 +252,24 @@ WebMouseEvent WebMouseEventBuilder::Build(HWND hwnd,
   result.windowX = result.x;
   result.windowY = result.y;
 
-  POINT global_point = { result.x, result.y };
+  // The mouse coordinates received here are device independent (DIPs). We need
+  // to convert them to physical coordinates before calling Windows APIs like
+  // ClientToScreen, etc.
+  gfx::Point scaled_screen_point(result.x, result.y);
+  scaled_screen_point = gfx::win::DIPToScreenPoint(scaled_screen_point);
+
+  POINT global_point = { scaled_screen_point.x(), scaled_screen_point.y() };
   ClientToScreen(hwnd, &global_point);
 
-  result.globalX = global_point.x;
-  result.globalY = global_point.y;
+  scaled_screen_point.set_x(global_point.x);
+  scaled_screen_point.set_y(global_point.y);
+
+  // We need to convert the point back to DIP before using it.
+  gfx::Point dip_screen_point = gfx::win::ScreenToDIPPoint(
+      scaled_screen_point);
+
+  result.globalX = dip_screen_point.x();
+  result.globalY = dip_screen_point.y();
 
   // calculate number of clicks:
 
