d37a8e56b3146c12c30ea908a389b070895c3731
dpranke@chromium.org <dpranke@chromium.org>
2015-03-05 02:22:43 +0000

    Revert of Properly clamp page scale factor to 1 in fullscreen and reset layout size (patchset #4 id:60001 of https://codereview.chromium.org/967413002/)
    Reason for revert:
    Unfortunately, it looks like this change is causing some flaky test failures:
    
    http://test-results.appspot.com/dashboards/flakiness_dashboard.html#tests=fast%2Fdom%2FWindow%2Fwindow-scaled-viewport-properties.html%2C%20fast%2Fevents%2Fgesture-pinch-zoom-scroll-bubble.html%2Csvg%2Fas-image%2Fimage-respects-pageScaleFactor.html%2C%C2%A0fast%2Fevents%2Fgesture-pinch-zoom.html%2C%C2%A0fast%2Fevents%2Fgesture-pinch-zoom-scroll-bubble.html%2C%C2%A0fast%2Fevents%2Fpage-scaled-mouse-click.html%2C%C2%A0fast%2Fevents%2Fpage-scaled-mouse-click.html
    
    all started failing on various ports after your change landed.
    
    
    Original issue's description:
    > Properly clamp page scale factor to 1 in fullscreen and reset layout size
    > 
    > This CL fixes a bug that fullscreen media control won't be reachable when
    > the viewport tag specifies a fixed width smaller than the screen width.
    > The CL does the following two things:
    > 
    > 1. Besides settings current page scale factor to 1, also temporarily force
    > minimum and maximum scale factor to 1 during fullscreen. This is required
    > because with such viewport tag describe above, the computed minimum scale
    > factor will be bigger than 1. If we layout fullscreen contents with respect
    > to screen size, it will be magnified so some of the contents won't be visible.
    > 
    > 2. Force the layout size to the size of the screen during fullscreen. This is
    > required because otherwise the main frame RenderView will be small than the
    > screen size, and will clip everything below it due to overflow:hidden.
    > 
    > BUG=450524
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=191236
    
    TBR=aelias@chromium.org,bokan@chromium.org,rbyers@chromium.org,trchen@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=450524
    
    Review URL: https://codereview.chromium.org/977303002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191330 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/PageScaleConstraints.cpp
+++ third_party/WebKit/Source/core/page/PageScaleConstraints.cpp
@@ -53,7 +53,7 @@ void PageScaleConstraints::overrideWith(const PageScaleConstraints& other)
         minimumScale = other.minimumScale;
     if (other.maximumScale != -1)
         maximumScale = other.maximumScale;
-    if (!other.layoutSize.isZero())
+    if (!other.layoutSize.isEmpty())
         layoutSize = other.layoutSize;
     clampAll();
 }
