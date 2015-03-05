42ec1423c8a31bad14a4f4410c39ca2f54a52863
ccameron <ccameron@chromium.org>
2015-03-04 17:35:23 -0800

    Remove fade animation on tab-switch when using the 10.9 SDK
    Add the usual implicit-animation supression system.
    
    BUG=463988
    
    Review URL: https://codereview.chromium.org/983523002
    
    Cr-Commit-Position: refs/heads/master@{#319190}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -830,6 +830,7 @@ RenderWidgetHost* RenderWidgetHostViewMac::GetRenderWidgetHost() const {
 }
 
 void RenderWidgetHostViewMac::Show() {
+  ScopedCAActionDisabler disabler;
   [cocoa_view_ setHidden:NO];
   if (!render_widget_host_->is_hidden())
     return;
@@ -843,6 +844,7 @@ void RenderWidgetHostViewMac::Show() {
 }
 
 void RenderWidgetHostViewMac::Hide() {
+  ScopedCAActionDisabler disabler;
   [cocoa_view_ setHidden:YES];
   WasOccluded();
   DestroySuspendedBrowserCompositorViewIfNeeded();
