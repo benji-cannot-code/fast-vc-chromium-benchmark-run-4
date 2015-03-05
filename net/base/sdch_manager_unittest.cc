31a585db7fd1e1457372b4c95c7c86e2de551117
rdsmith <rdsmith@chromium.org>
2015-03-05 08:49:20 -0800

    Shift SDCH Finch config to a place where iOS will see it, and re-enable SDCH on iOS.
    BUG=None
    
    Review URL: https://codereview.chromium.org/954583002
    
    Cr-Commit-Position: refs/heads/master@{#319261}

--- net/base/sdch_manager_unittest.cc
+++ net/base/sdch_manager_unittest.cc
@@ -19,9 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
-// Workaround for http://crbug.com/437794; remove when fixed.
-#if !defined(OS_IOS)
-
 //------------------------------------------------------------------------------
 // Provide sample data and compression results with a sample VCDIFF dictionary.
 // Note an SDCH dictionary has extra meta-data before the VCDIFF dictionary.
@@ -692,18 +689,5 @@ TEST_F(SdchManagerTest, SdchDictionaryUsed) {
   EXPECT_EQ(1, observer.dictionary_used_notifications());
   EXPECT_EQ("xyzzy", observer.last_server_hash());
 }
-
-#else
-
-TEST(SdchManagerTest, SdchOffByDefault) {
-  GURL google_url("http://www.google.com");
-  scoped_ptr<SdchManager> sdch_manager(new SdchManager);
-
-  EXPECT_EQ(SDCH_DISABLED, sdch_manager->IsInSupportedDomain(google_url));
-  SdchManager::EnableSdchSupport(true);
-  EXPECT_EQ(SDCH_OK, sdch_manager->IsInSupportedDomain(google_url));
-}
-
-#endif  // !defined(OS_IOS)
 
 }  // namespace net
