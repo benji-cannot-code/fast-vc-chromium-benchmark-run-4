1688e997e51dfd7dbeff8496908ce6347358e78d
jrummell@chromium.org <jrummell@chromium.org>
2015-03-05 18:09:11 +0000

    Update WebMediaKeySystemConfiguration to use enums
    initDataTypes and sessionTypes are now WebVector<WebEncryptedMediaInitDataType>
    and WebVector<WebEncryptedMediaSessionType>, respectively.
    
    BUG=418239
    TEST=EME tests still pass
    
    Review URL: https://codereview.chromium.org/978883004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191374 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebMediaKeySystemConfiguration.h
+++ third_party/WebKit/public/platform/WebMediaKeySystemConfiguration.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "public/platform/WebEncryptedMediaTypes.h"
 #include "public/platform/WebMediaKeySystemMediaCapability.h"
-#include "public/platform/WebString.h"
 #include "public/platform/WebVector.h"
 
 namespace blink {
@@ -26,18 +25,17 @@ struct WebMediaKeySystemConfiguration {
     {
     }
 
-    WebVector<WebString> initDataTypes;
+    WebVector<WebEncryptedMediaInitDataType> initDataTypes;
     WebVector<WebMediaKeySystemMediaCapability> audioCapabilities;
     WebVector<WebMediaKeySystemMediaCapability> videoCapabilities;
     Requirement distinctiveIdentifier;
     Requirement persistentState;
-    WebVector<WebString> sessionTypes;
+    WebVector<WebEncryptedMediaSessionType> sessionTypes;
 
     // FIXME: Temporary methods until |initDataTypes| and |sessionTypes|
     // can be converted to be WebVector<enum>.
     BLINK_PLATFORM_EXPORT WebVector<WebEncryptedMediaInitDataType> getInitDataTypes() const;
     BLINK_PLATFORM_EXPORT void setInitDataTypes(const WebVector<WebEncryptedMediaInitDataType>&);
-    BLINK_PLATFORM_EXPORT WebVector<WebEncryptedMediaSessionType> getSessionTypes() const;
     BLINK_PLATFORM_EXPORT void setSessionTypes(const WebVector<WebEncryptedMediaSessionType>&);
 };
 
