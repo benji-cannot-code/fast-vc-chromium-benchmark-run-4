cbdb983f7570c6b20b03aa952d5aa42e4fcfeb64
sdefresne <sdefresne@chromium.org>
2015-03-05 11:07:28 -0800

    Fix a cpplint warnings
    src/chrome/browser/autocomplete/search_provider_unittest.cc:76:
    You don't need a ; after a }  [readability/braces] [4]
    
    src/chrome/browser/ui/omnibox/omnibox_view_browsertest.cc:1410:
    Lines should very rarely be longer than 100 characters  [whitespace/line_length] [4]
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/984543002
    
    Cr-Commit-Position: refs/heads/master@{#319292}

--- chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
+++ chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
@@ -1371,7 +1371,8 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, MAYBE_TabTraverseResultsTest) {
 
 // http://crbug.com/133347
 #if defined(OS_LINUX)
-#define MAYBE_PersistKeywordModeOnTabSwitch DISABLED_PersistKeywordModeOnTabSwitch
+#define MAYBE_PersistKeywordModeOnTabSwitch \
+    DISABLED_PersistKeywordModeOnTabSwitch
 #else
 #define MAYBE_PersistKeywordModeOnTabSwitch PersistKeywordModeOnTabSwitch
 #endif
@@ -1406,9 +1407,11 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest,
 
 // http://crbug.com/133355
 #if defined(OS_LINUX)
-#define MAYBE_CtrlKeyPressedWithInlineAutocompleteTest DISABLED_CtrlKeyPressedWithInlineAutocompleteTest
+#define MAYBE_CtrlKeyPressedWithInlineAutocompleteTest \
+    DISABLED_CtrlKeyPressedWithInlineAutocompleteTest
 #else
-#define MAYBE_CtrlKeyPressedWithInlineAutocompleteTest CtrlKeyPressedWithInlineAutocompleteTest
+#define MAYBE_CtrlKeyPressedWithInlineAutocompleteTest \
+    CtrlKeyPressedWithInlineAutocompleteTest
 #endif
 
 IN_PROC_BROWSER_TEST_F(OmniboxViewTest,
