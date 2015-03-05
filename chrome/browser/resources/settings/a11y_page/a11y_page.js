c288a2b5e8bd6d06ef4825fcac8b4f0abee7c076
jlklein <jlklein@chromium.org>
2015-03-05 15:33:23 -0800

    Add an initial implementation of the a11y settings page and the settings pref model element.
    BUG=464201
    
    Review URL: https://codereview.chromium.org/969253002
    
    Cr-Commit-Position: refs/heads/master@{#319360}

---
+++ chrome/browser/resources/settings/a11y_page/a11y_page.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/* Copyright 2015 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. */
+
+/**
+ * @fileoverview
+ * 'cr-settings-a11y-page' is the settings page containing accessibility
+ * settings.
+ *
+ * Example:
+ *
+ *    <core-animated-pages>
+ *      <cr-settings-a11y-page></cr-settings-a11y-page>
+ *      ... other pages ...
+ *    </core-animated-pages>
+ *
+ * @group Chrome Settings Elements
+ * @element cr-settings-a11y-page
+ */
+Polymer('cr-settings-a11y-page', {
+  publish: {
+    /**
+     * Preferences state.
+     *
+     * @attribute prefs
+     * @type CrSettingsPrefsElement
+     * @default null
+     */
+    prefs: null,
+  },
+});
---
+++ chrome/browser/resources/settings/prefs/prefs_types.js
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Typedefs for CrSettingsPrefsElement.
+ */
+
+var CrSettingsPrefs = {};
+
+/**
+ * @typedef {{
+ *   a11y: CrSettingsPrefs.A11y,
+ *   touchpad: CrSettingsPrefs.Touchpad,
+ *   accessibility: boolean
+ * }}
+ */
+CrSettingsPrefs.Settings;
+
+/**
+ * @typedef {{
+ *   enableMenu: boolean,
+ *   largeCursorEnabled: boolean,
+ *   highContrastEnabled: boolean,
+ *   stickyKeysEnabled: boolean,
+ *   screenMagnifier: boolean,
+ *   autoclick: boolean,
+ *   autoclickDelayMs: number,
+ *   virtualKeyboard: boolean
+ * }}
+ */
+CrSettingsPrefs.A11y;
+
+/**
+ * @typedef {{
+ *   enableTapDragging: boolean
+ * }}
+ */
+CrSettingsPrefs.Touchpad;
