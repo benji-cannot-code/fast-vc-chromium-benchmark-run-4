0865a978d0374e78ccf0bd24c2e05ff7c57f9d81
kouhei@chromium.org <kouhei@chromium.org>
2015-03-05 08:20:46 +0000

    Oilpan: Fix ENABLE(GC_PROFILING) build.
    The profiling infra requires HashTraits<String>::isEmptyValue impl, and it is defined inline at wtf/text/StringHash.h.
    This CL selectively adds an include if ENABLE(GC_PROFILING) build.
    
    BUG=420515
    
    Review URL: https://codereview.chromium.org/981773004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191349 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/ThreadState.cpp
+++ third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/ThreadingPrimitives.h"
 #if ENABLE(GC_PROFILING)
 #include "platform/TracedValue.h"
+#include "wtf/text/StringHash.h"
 #endif
 
 #if OS(WIN)
