8e9c6362af81d0845bad7dff6fb9afdc23069e20
thestig <thestig@chromium.org>
2015-03-05 12:38:07 -0800

    Autofill: Do not match against form field values.
    This fixes a couple bugs where fields with pre-filled data just happen
    to match autofill regexes and gets mistakenly recognized as
    autofillable fields.
    
    Also fix the one known test case that regressed from this change by
    adding more regexes.
    
    BUG=460832,462080
    
    Review URL: https://codereview.chromium.org/980433002
    
    Cr-Commit-Position: refs/heads/master@{#319318}

--- components/autofill/core/browser/form_field.cc
+++ components/autofill/core/browser/form_field.cc
@@ -156,11 +156,6 @@ bool FormField::Match(const AutofillField* field,
     return true;
   }
 
-  if ((match_type & FormField::MATCH_VALUE) &&
-      MatchesPattern(field->value, pattern)) {
-    return true;
-  }
-
   return false;
 }
 
