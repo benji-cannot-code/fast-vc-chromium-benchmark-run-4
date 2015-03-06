ed3fe2cd7e34c12c9f72c3e4b644278158973281
thestig <thestig@chromium.org>
2015-03-06 13:36:04 -0800

    Revert of MSan: check data is initialized when serializing with base::Pickle. (patchset #1 id:1 of https://codereview.chromium.org/988693004/)
    Reason for revert:
    Found errors w.r.t. IPC messages. Reverting for now while people fix the errors.
    
    Original issue's description:
    > MSan: check data is initialized when serializing with base::Pickle.
    >
    > Serialization implies that the data is going to leave the process one way or
    > another, and it's never a good idea to leak uninitialized bytes. In most cases
    > MSan would catch this later on (e.g. on a write()). There are exceptions to
    > that, however, such as writing to shared memory. It doesn't hurt to have an
    > extra check here.
    >
    > BUG=463906
    > R=thakis@chromium.org
    >
    > Committed: https://crrev.com/2c705b038413d30d7aae5ff5d09882575e93c898
    > Cr-Commit-Position: refs/heads/master@{#319459}
    
    TBR=thakis@chromium.org,earthdok@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=463906
    
    Review URL: https://codereview.chromium.org/985893002
    
    Cr-Commit-Position: refs/heads/master@{#319510}

--- base/compiler_specific.h
+++ base/compiler_specific.h
@@ -176,17 +176,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Mark a memory region fully initialized.
 // Use this to annotate code that deliberately reads uninitialized data, for
 // example a GC scavenging root set pointers from the stack.
-#define MSAN_UNPOISON(p, size)  __msan_unpoison(p, size)
-
-// Check a memory region for initializedness, as if it was being used here.
-// If any bits are uninitialized, crash with an MSan report.
-// Use this to sanitize data which MSan won't be able to track, e.g. before
-// passing data to another process via shared memory.
-#define MSAN_CHECK_MEM_IS_INITIALIZED(p, size) \
-    __msan_check_mem_is_initialized(p, size)
+#define MSAN_UNPOISON(p, s)  __msan_unpoison(p, s)
 #else  // MEMORY_SANITIZER
-#define MSAN_UNPOISON(p, size)
-#define MSAN_CHECK_MEM_IS_INITIALIZED(p, size)
+#define MSAN_UNPOISON(p, s)
 #endif  // MEMORY_SANITIZER
 
 // Macro useful for writing cross-platform function pointers.
