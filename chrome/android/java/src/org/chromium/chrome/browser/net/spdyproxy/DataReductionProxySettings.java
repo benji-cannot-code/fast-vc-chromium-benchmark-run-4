4493b41532c9d3c9d5fb316e2089ca41a2b5854e
jeremyim <jeremyim@chromium.org>
2015-03-06 13:13:07 -0800

    Remove unused method (DataReductionProxySettings::PrimaryOrigin) from DataReductionProxySettings.
    BUG=464775
    
    Review URL: https://codereview.chromium.org/983533006
    
    Cr-Commit-Position: refs/heads/master@{#319504}

--- chrome/android/java/src/org/chromium/chrome/browser/net/spdyproxy/DataReductionProxySettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/net/spdyproxy/DataReductionProxySettings.java
@@ -123,13 +123,6 @@ public class DataReductionProxySettings {
     }
 
     /**
-     * Returns the current data reduction proxy origin.
-     */
-    public String getDataReductionProxyOrigin() {
-        return nativeGetDataReductionProxyOrigin(mNativeDataReductionProxySettings);
-    }
-
-    /**
      * Sets the preference on whether to enable/disable the SPDY proxy. This will zero out the
      * data reduction statistics if this is the first time the SPDY proxy has been enabled.
      */
@@ -211,8 +204,6 @@ public class DataReductionProxySettings {
     private native boolean nativeIsDataReductionProxyPromoAllowed(
             long nativeDataReductionProxySettingsAndroid);
     private native boolean nativeIsIncludedInAltFieldTrial(
-            long nativeDataReductionProxySettingsAndroid);
-    private native String nativeGetDataReductionProxyOrigin(
             long nativeDataReductionProxySettingsAndroid);
     private native boolean nativeIsDataReductionProxyEnabled(
             long nativeDataReductionProxySettingsAndroid);
--- chrome/browser/net/spdyproxy/data_reduction_proxy_settings_unittest_android.cc
+++ chrome/browser/net/spdyproxy/data_reduction_proxy_settings_unittest_android.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/base64.h"
-#include "base/command_line.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/prefs/pref_service.h"
@@ -32,8 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using testing::_;
 using testing::AnyNumber;
 using testing::Return;
-
-const char kDataReductionProxyDev[] = "foo-dev.com:80";
 
 using data_reduction_proxy::DataReductionProxySettings;
 
@@ -126,31 +123,6 @@ class DataReductionProxySettingsAndroidTest
   JNIEnv* env_;
 };
 
-TEST_F(DataReductionProxySettingsAndroidTest, TestGetDataReductionProxyOrigin) {
-  // SetUp() adds the origin to the command line, which should be returned here.
-  ScopedJavaLocalRef<jstring> result =
-      SettingsAndroid()->GetDataReductionProxyOrigin(env_, NULL);
-  ASSERT_TRUE(result.obj());
-  const base::android::JavaRef<jstring>& str_ref = result;
-  EXPECT_EQ(test_context_->config()->test_params()->DefaultOrigin(),
-            ConvertJavaStringToUTF8(str_ref));
-}
-
-TEST_F(DataReductionProxySettingsAndroidTest,
-       TestGetDataReductionProxyDevOrigin) {
-  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
-      data_reduction_proxy::switches::kDataReductionProxyDev,
-      kDataReductionProxyDev);
-  ResetSettings(true, true, false, true, false);
-  ResetSettingsAndroid();
-  ScopedJavaLocalRef<jstring> result =
-      SettingsAndroid()->GetDataReductionProxyOrigin(env_, NULL);
-  ASSERT_TRUE(result.obj());
-  const base::android::JavaRef<jstring>& str_ref = result;
-  EXPECT_EQ(kDataReductionProxyDev,
-            ConvertJavaStringToUTF8(str_ref));
-}
-
 TEST_F(DataReductionProxySettingsAndroidTest, TestGetDailyContentLengths) {
   ScopedJavaLocalRef<jlongArray> result =
       SettingsAndroid()->GetDailyContentLengths(
@@ -169,4 +141,3 @@ TEST_F(DataReductionProxySettingsAndroidTest, TestGetDailyContentLengths) {
             (data_reduction_proxy::kNumDaysInHistory - 1 - i) * 2), value);
   }
 }
-
