71b739761ce8868a5568abe7e9d9c9e326c0ab2a
mkwst@chromium.org <mkwst@chromium.org>
2015-03-06 10:26:18 +0000

    Upgrade insecure requests: Pipe navigational hosts down into nested documents.
    After [1], we need to track hosts (including ancestor hosts) that have
    set the 'upgrade-insecure-requests' directive in their respective policies
    in order to correctly upgrade navigational requests to one of those
    hosts.
    
    This patch adds a 'HashSet<unsigned>' to SecurityContext that holds the
    hashes of the hosts which have opted-into such treatment, ensures that
    the set is correctly populated when creating a Document or applying a
    policy, and uses the set to make decisions about navigational upgrades
    inside ResourceFetcher.
    
    [1]: ttps://github.com/w3c/webappsec/commit/f947b75e9b906c53d0bd6e66ca59b60bfe0aa20e
    
    BUG=455674
    
    Review URL: https://codereview.chromium.org/978323002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191421 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/FrameLoader.h
+++ third_party/WebKit/Source/core/loader/FrameLoader.h
@@ -144,6 +144,7 @@ public:
     bool shouldEnforceStrictMixedContentChecking() const;
 
     SecurityContext::InsecureRequestsPolicy insecureRequestsPolicy() const;
+    SecurityContext::InsecureNavigationsSet* insecureNavigationsToUpgrade() const;
 
     Frame* opener();
     void setOpener(LocalFrame*);
