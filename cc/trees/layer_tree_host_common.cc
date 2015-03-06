872cdf5e6a527ec1b469a28913d66389d8f53f12
majidvp <majidvp@chromium.org>
2015-03-06 12:46:39 -0800

    Compositor hit-testing considers scrollable layers that scroll a drawn RSLL
    Compositor should include a scrollable layer in hit testing if it scrolls at
    least one drawn RSSL layer which can be itself, its descendant, or its scroll
    descendant.
    
    BUG=455439
    
    Review URL: https://codereview.chromium.org/868363004
    
    Cr-Commit-Position: refs/heads/master@{#319496}

--- cc/trees/layer_tree_host_common.cc
+++ cc/trees/layer_tree_host_common.cc
@@ -1129,6 +1129,8 @@ static inline void MarkLayerWithRenderSurfaceLayerListId(
     int current_render_surface_layer_list_id) {
   layer->draw_properties().last_drawn_render_surface_layer_list_id =
       current_render_surface_layer_list_id;
+  layer->draw_properties().layer_or_descendant_is_drawn =
+      !!current_render_surface_layer_list_id;
 }
 
 template <typename LayerTypePtr>
@@ -1201,8 +1203,7 @@ struct PreCalculateMetaInformationRecursiveData {
         data.layer_or_descendant_has_copy_request;
     layer_or_descendant_has_input_handler |=
         data.layer_or_descendant_has_input_handler;
-    num_unclipped_descendants +=
-        data.num_unclipped_descendants;
+    num_unclipped_descendants += data.num_unclipped_descendants;
   }
 };
 
@@ -1234,6 +1235,7 @@ static void PreCalculateMetaInformation(
 
   layer->draw_properties().sorted_for_recursion = false;
   layer->draw_properties().has_child_with_a_scroll_parent = false;
+  layer->draw_properties().layer_or_descendant_is_drawn = false;
   layer->draw_properties().visited = false;
 
   if (!HasInvertibleOrAnimatedTransform(layer)) {
@@ -2213,6 +2215,8 @@ static void CalculateDrawPropertiesInternal(
         render_surface_layer_list->size() -
         child->draw_properties()
             .index_of_first_render_surface_layer_list_addition;
+    layer_draw_properties.layer_or_descendant_is_drawn |=
+        child->draw_properties().layer_or_descendant_is_drawn;
   }
 
   // Add the unsorted layer list contributions, if necessary.
