416aff800860b07ba5fb766ab2871af3f5f63acf
skyostil <skyostil@chromium.org>
2015-03-06 11:10:13 -0800

    Re-enable Android profiling helper unittests
    These seem to be passing locally now.
    
    BUG=437081
    
    Review URL: https://codereview.chromium.org/981293002
    
    Cr-Commit-Position: refs/heads/master@{#319471}

--- tools/telemetry/telemetry/core/platform/profiler/android_profiling_helper_unittest.py
+++ tools/telemetry/telemetry/core/platform/profiler/android_profiling_helper_unittest.py
@@ -97,9 +97,7 @@ class TestAndroidProfilingHelperTabTestCase(tab_test_case.TabTestCase):
     browser_backend = self._browser._browser_backend
     self._device = browser_backend._adb.device()
 
-  # Test fails: crbug.com/437081
-  # @decorators.Enabled('android')
-  @decorators.Disabled
+  @decorators.Enabled('android')
   def testCreateSymFs(self):
     # pylint: disable=W0212
     browser_pid = self._browser._browser_backend.pid
@@ -130,9 +128,7 @@ class TestAndroidProfilingHelperTabTestCase(tab_test_case.TabTestCase):
     finally:
       shutil.rmtree(symfs_dir)
 
-  # Test fails: crbug.com/437081
-  # @decorators.Enabled('android')
-  @decorators.Disabled
+  @decorators.Enabled('android')
   def testGetToolchainBinaryPath(self):
     with tempfile.NamedTemporaryFile() as libc:
       self._device.PullFile('/system/lib/libc.so', libc.name)
