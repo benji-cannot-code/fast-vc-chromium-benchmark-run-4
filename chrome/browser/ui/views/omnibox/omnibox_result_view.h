07ecc10af3df69645c4e0e6b437b0ade8facce8a
dschuyler <dschuyler@chromium.org>
2015-03-06 15:13:37 -0800

    Answers in Suggest icon downloading
    Some answers in suggest results have an associated icon, for example the weather may show a sunny or cloudy-skies image.
    
    This change will download the associated icon and display it in the answer suggested.
    
    BUG=456339
    
    Review URL: https://codereview.chromium.org/917333004
    
    Cr-Commit-Position: refs/heads/master@{#319524}

--- chrome/browser/ui/views/omnibox/omnibox_result_view.h
+++ chrome/browser/ui/views/omnibox/omnibox_result_view.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/gtest_prod_util.h"
+#include "chrome/browser/bitmap_fetcher/bitmap_fetcher_service.h"
 #include "components/omnibox/autocomplete_match.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/animation/animation_delegate.h"
@@ -75,6 +76,9 @@ class OmniboxResultView : public views::View,
 
   // Returns the display width required for the match contents.
   int GetMatchContentsWidth() const;
+
+  // Stores the image in a local data member and schedules a repaint.
+  void SetAnswerImage(const gfx::ImageSkia& image);
 
  protected:
   // Paints the given |match| using the RenderText instances |contents| and
@@ -164,6 +168,8 @@ class OmniboxResultView : public views::View,
   size_t model_index_;
 
   LocationBarView* location_bar_view_;
+  // Note: image_service_ may be null in some unit tests.
+  BitmapFetcherService* image_service_;
 
   const gfx::FontList font_list_;
   int font_height_;
@@ -182,6 +188,11 @@ class OmniboxResultView : public views::View,
 
   scoped_ptr<gfx::SlideAnimation> animation_;
 
+  // If the answer has an icon, these control the fetching and updating
+  // of the icon.
+  BitmapFetcherService::RequestId request_id_;
+  gfx::ImageSkia answer_image_;
+
   // We preserve these RenderTexts so that we won't recreate them on every call
   // to GetMatchContentsWidth() or OnPaint().
   mutable scoped_ptr<gfx::RenderText> contents_rendertext_;
@@ -191,6 +202,8 @@ class OmniboxResultView : public views::View,
   mutable scoped_ptr<gfx::RenderText> keyword_description_rendertext_;
 
   mutable int separator_width_;
+
+  base::WeakPtrFactory<OmniboxResultView> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(OmniboxResultView);
 };
