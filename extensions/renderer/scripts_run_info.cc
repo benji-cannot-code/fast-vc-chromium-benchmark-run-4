0c50b3df582c5b577d957a97c7be8f77f8de9cb7
kozyatinskiy <kozyatinskiy@chromium.org>
2015-03-06 04:33:06 -0800

    Extensions: fix ScriptsRunInfo.num_blocking_js initialization
    There is no num_blocking_js initialization in constructor.
    This CL fixes it.
    
    TBR=rdevlin.cronin@chromium.org
    NOTRY=true
    NOTREECHECKS=true
    
    Review URL: https://codereview.chromium.org/983033003
    
    Cr-Commit-Position: refs/heads/master@{#319437}

--- extensions/renderer/scripts_run_info.cc
+++ extensions/renderer/scripts_run_info.cc
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
-ScriptsRunInfo::ScriptsRunInfo() : num_css(0u), num_js(0u) {
+ScriptsRunInfo::ScriptsRunInfo()
+    : num_css(0u), num_js(0u), num_blocking_js(0u) {
 }
 
 ScriptsRunInfo::~ScriptsRunInfo() {
