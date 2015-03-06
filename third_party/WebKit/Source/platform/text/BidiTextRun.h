88f52d3d39e2062c693257ffd39e242433297a73
kojii@chromium.org <kojii@chromium.org>
2015-03-06 02:33:21 +0000

    Optimize BidiTextRun.cpp when hasStrongDirectionality is not needed
    BreakingContext::handleText() calls textWidth() which in turn
    calls directionForRun(), which consumes 5-7% of total layout
    time in a perf test (vertical-japanese-kokoro-insert.html).
    
    When callers do not need hasStrongDirectionality, and if the
    string is 8 bit or of a single character, we could avoid calling
    rather expensive functions such as BidiStatus::BidiStatus() and
    BidiResolver::determineParagraphDirectionality().
    
    This patch optimizes when the conditions meet by allowing callers
    to declare whether hasStrongDirectionality is needed or not.
    
    BUG=446011
    
    Review URL: https://codereview.chromium.org/979673002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191400 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/text/BidiTextRun.h
+++ third_party/WebKit/Source/platform/text/BidiTextRun.h
@@ -38,9 +38,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-PLATFORM_EXPORT TextDirection directionForRun(TextRun&, bool& hasStrongDirectionality);
-PLATFORM_EXPORT TextDirection determineDirectionality(const String& value, bool& hasStrongDirectionality);
-PLATFORM_EXPORT TextRun textRunWithDirectionality(const String& value, bool& hasStrongDirectionality);
+PLATFORM_EXPORT TextDirection directionForRun(TextRun&, bool* hasStrongDirectionality = nullptr);
+PLATFORM_EXPORT TextDirection determineDirectionality(const String& value, bool* hasStrongDirectionality = nullptr);
+PLATFORM_EXPORT TextRun textRunWithDirectionality(const String& value, bool* hasStrongDirectionality = nullptr);
 
 } // namespace blink
 
