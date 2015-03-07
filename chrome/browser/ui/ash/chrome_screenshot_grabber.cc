64d9b0b3c0e1fab74f77fba8b31e654671e4c363
asanka <asanka@chromium.org>
2015-03-06 21:33:41 -0800

    Be explicit about target type in platform_util::OpenItem()
    OpenItem() now takes an OpenItemType parameter that should specify
    expected type of the object to be opened. It verifies the type of the
    object before invoking platform specific logic for opening the item.
    
    Code that assumed that the target of OpenItem() was always a folder
    should now no longer unintentionally open or execute the file at the
    target location when this assumption was found to not be correct.
    
    In addition to the checks performed by OpenItem, the platform specific
    logic used to open folders fail if the target type is not a directory.
    
    BUG=387037
    
    Review URL: https://codereview.chromium.org/352393002
    
    Cr-Commit-Position: refs/heads/master@{#319555}

--- chrome/browser/ui/ash/chrome_screenshot_grabber.cc
+++ chrome/browser/ui/ash/chrome_screenshot_grabber.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/download/download_prefs.h"
 #include "chrome/browser/notifications/notification_ui_manager.h"
+#include "chrome/browser/platform_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/common/pref_names.h"
@@ -115,11 +116,7 @@ class ScreenshotGrabberNotificationDelegate : public NotificationDelegate {
   void Click() override {
     if (!success_)
       return;
-#if defined(OS_CHROMEOS)
-    file_manager::util::ShowItemInFolder(profile_, screenshot_path_);
-#else
-// TODO(sschmitz): perhaps add similar action for Windows.
-#endif
+    platform_util::ShowItemInFolder(profile_, screenshot_path_);
   }
   void ButtonClick(int button_index) override {
     DCHECK(success_ && button_index == 0);
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -522,6 +522,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/platform_util.h',
       'browser/platform_util_android.cc',
       'browser/platform_util_chromeos.cc',
+      'browser/platform_util_internal.h',
       'browser/platform_util_mac.mm',
       'browser/platform_util_win.cc',
       'browser/precache/most_visited_urls_provider.cc',
@@ -778,6 +779,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     ],
     # Everything but Android, iOS, and CrOS.
     'chrome_browser_desktop_sources': [
+      'browser/platform_util.cc',
       'browser/profiles/avatar_menu_actions_desktop.cc',
       'browser/profiles/avatar_menu_actions_desktop.h',
       'browser/profiles/avatar_menu_desktop.cc',
