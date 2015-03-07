ee1eaaf48e0ffb0cd02dd88992773d255bc4e06f
michaelbai <michaelbai@chromium.org>
2015-03-06 21:55:39 -0800

    Refactoring chromoting_jni_onload.cc
    Make it using base::android::OnJNIOnLoadRegister().
    
    BUG=447393
    
    Review URL: https://codereview.chromium.org/981323002
    
    Cr-Commit-Position: refs/heads/master@{#319556}

--- remoting/client/jni/chromoting_jni_runtime.cc
+++ remoting/client/jni/chromoting_jni_runtime.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
+#include "base/android/library_loader/library_loader_hooks.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/basictypes.h"
 #include "base/command_line.h"
@@ -183,8 +184,6 @@ ChromotingJniRuntime* ChromotingJniRuntime::GetInstance() {
 }
 
 ChromotingJniRuntime::ChromotingJniRuntime() {
-  at_exit_manager_.reset(new base::AtExitManager());
-
   // On Android, the UI thread is managed by Java, so we need to attach and
   // start a special type of message loop to allow Chromium code to run tasks.
   ui_loop_.reset(new base::MessageLoopForUI());
@@ -225,6 +224,7 @@ ChromotingJniRuntime::~ChromotingJniRuntime() {
       base::Unretained(this),
       &done_event));
   done_event.Wait();
+  base::android::LibraryLoaderExitHook();
   base::android::DetachFromVM();
 }
 
