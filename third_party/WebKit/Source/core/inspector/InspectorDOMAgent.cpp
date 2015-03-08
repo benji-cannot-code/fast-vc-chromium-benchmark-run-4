6d1b555da18e0060255eea2094ddf143340850ab
dgozman@chromium.org <dgozman@chromium.org>
2015-03-08 22:34:21 +0000

    [DevTools] Only instrument DOM while InspectorDOMAgent is enabled.
    BUG=none
    
    Review URL: https://codereview.chromium.org/988023002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191491 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
@@ -327,7 +327,7 @@ InspectorDOMAgent::InspectorDOMAgent(InspectorPageAgent* pageAgent, InjectedScri
 InspectorDOMAgent::~InspectorDOMAgent()
 {
 #if !ENABLE(OILPAN)
-    reset();
+    setDocument(nullptr);
     ASSERT(m_searchingForNode == NotSearching);
 #endif
 }
@@ -339,7 +339,6 @@ void InspectorDOMAgent::setFrontend(InspectorFrontend* frontend)
     m_domEditor = adoptPtrWillBeNoop(new DOMEditor(m_history.get()));
 
     m_frontend = frontend->dom();
-    m_instrumentingAgents->setInspectorDOMAgent(this);
     m_document = m_pageAgent->inspectedFrame()->document();
 }
 
@@ -355,9 +354,7 @@ void InspectorDOMAgent::clearFrontend()
     hideHighlight(&error);
 
     m_frontend = nullptr;
-    m_instrumentingAgents->setInspectorDOMAgent(0);
     disable(0);
-    reset();
 }
 
 void InspectorDOMAgent::restore()
@@ -381,12 +378,6 @@ WillBeHeapVector<RawPtrWillBeMember<Document> > InspectorDOMAgent::documents()
     return result;
 }
 
-void InspectorDOMAgent::reset()
-{
-    discardFrontendBindings();
-    m_document = nullptr;
-}
-
 void InspectorDOMAgent::setDOMListener(DOMListener* listener)
 {
     m_domListener = listener;
@@ -397,8 +388,7 @@ void InspectorDOMAgent::setDocument(Document* doc)
     if (doc == m_document.get())
         return;
 
-    reset();
-
+    discardFrontendBindings();
     m_document = doc;
 
     if (!enabled())
@@ -589,6 +579,7 @@ void InspectorDOMAgent::innerEnable()
 {
     m_state->setBoolean(DOMAgentState::domAgentEnabled, true);
     m_document = m_pageAgent->inspectedFrame()->document();
+    m_instrumentingAgents->setInspectorDOMAgent(this);
     if (m_listener)
         m_listener->domAgentWasEnabled();
     if (m_backendNodeIdToInspect)
@@ -616,7 +607,8 @@ void InspectorDOMAgent::disable(ErrorString* errorString)
         return;
     }
     m_state->setBoolean(DOMAgentState::domAgentEnabled, false);
-    reset();
+    m_instrumentingAgents->setInspectorDOMAgent(0);
+    setDocument(nullptr);
     if (m_listener)
         m_listener->domAgentWasDisabled();
 }
