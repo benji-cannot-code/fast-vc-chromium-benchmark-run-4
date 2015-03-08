9354c6dda2119b407ed2347b5c4165df6da10e8a
samuong <samuong@chromium.org>
2015-03-07 22:46:26 -0800

    [chromedriver] Drop support for M39 and add support for M42.
    BUG=
    TBR=stgao@chromium.org
    
    Review URL: https://codereview.chromium.org/985253002
    
    Cr-Commit-Position: refs/heads/master@{#319583}

--- chrome/test/chromedriver/chrome/version.cc
+++ chrome/test/chromedriver/chrome/version.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // This variable must be able to be found and parsed by the upload script.
-const int kMinimumSupportedChromeVersion[] = {39, 0, 2171, 0};
+const int kMinimumSupportedChromeVersion[] = {40, 0, 2214, 0};
 
 }  // namespace
 
