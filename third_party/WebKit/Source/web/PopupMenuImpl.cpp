e6ff60c922eccd3fc1ec1e345418f3f2f9f5063b
tkent@chromium.org <tkent@chromium.org>
2015-03-09 11:41:16 +0000

    New SELECT Popup: Popup content should handle padding of the owner SELECT.
    Popup contnet should handle padding of the owner SLECT element. However, this
    doesn't mean we should copy the padding value of the SLECT element to the parent
    of OPTIONs in the popup.
     - The background color of the selected item should occupy the whole width of
       the popup.
     - Separators should not affect the padding.
    So, we need to add the padding value to each of OPTIONs and OPTGROUPs.
    
    Also, this CL corrects RTL scrollbar.
    
    BUG=464337
    
    Review URL: https://codereview.chromium.org/990813003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191536 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/PopupMenuImpl.cpp
+++ third_party/WebKit/Source/web/PopupMenuImpl.cpp
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/HTMLNames.h"
 #include "core/css/CSSFontSelector.h"
+#include "core/dom/NodeLayoutStyle.h"
 #include "core/dom/StyleEngine.h"
 #include "core/frame/FrameView.h"
 #include "core/html/HTMLHRElement.h"
@@ -101,6 +102,10 @@ void PopupMenuImpl::writeDocument(SharedBuffer* data)
     }
     PagePopupClient::addString("],\n", data);
     addProperty("anchorRectInScreen", anchorRectInScreen, data);
+    bool isRTL = !ownerElement().layoutStyle()->isLeftToRightDirection();
+    addProperty("isRTL", isRTL, data);
+    addProperty("paddingStart", isRTL ? m_client->clientPaddingRight().toDouble() : m_client->clientPaddingLeft().toDouble(), data);
+    addProperty("paddingEnd", isRTL ? m_client->clientPaddingLeft().toDouble() : m_client->clientPaddingRight().toDouble(), data);
     PagePopupClient::addString("};\n", data);
     data->append(Platform::current()->loadResource("pickerCommon.js"));
     data->append(Platform::current()->loadResource("listPicker.js"));
