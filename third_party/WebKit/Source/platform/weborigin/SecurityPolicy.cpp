f69f81d978e9f864418f11a2053cd7820d873365
peter@chromium.org <peter@chromium.org>
2015-03-09 16:00:33 +0000

    Remove an unsafe DEFINE_STATIC_LOCAL use from SecurityPolicy.cpp
    The origin access map can be used from multiple threads, and in fact is
    being used by a Service Worker when ServiceWorkerClients.openWindow()
    is being called to check the origin. Change it to use the new
    AtomicallyInitializedStaticReference instead.
    
    BUG=459732
    
    Review URL: https://codereview.chromium.org/938593006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191552 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/weborigin/SecurityPolicy.cpp
+++ third_party/WebKit/Source/platform/weborigin/SecurityPolicy.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/MainThread.h"
 #include "wtf/OwnPtr.h"
 #include "wtf/PassOwnPtr.h"
+#include "wtf/Threading.h"
 #include "wtf/text/StringHash.h"
 
 namespace blink {
@@ -47,7 +48,7 @@ typedef HashMap<String, OwnPtr<OriginAccessWhiteList>> OriginAccessMap;
 
 static OriginAccessMap& originAccessMap()
 {
-    DEFINE_STATIC_LOCAL(OriginAccessMap, originAccessMap, ());
+    AtomicallyInitializedStaticReference(OriginAccessMap, originAccessMap, new OriginAccessMap);
     return originAccessMap;
 }
 
