9488c202dcfe266ed02f715c5946ad1fbf266569
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-03-09 10:16:17 -0700

    [Extensions] Make chrome://extensions use developerPrivate for error calls
    Make the chrome://extensions page use chrome.developerPrivate API for
    extension error-related functionality.
    Also add tests for the api function, and convert it to a
    UIThreadExtensionFunction.
    
    Additionally, fix a bug in extensions.js where calling "showOverlay" on an
    already-visible hid the overlay (temporarily).
    
    BUG=461039
    
    Review URL: https://codereview.chromium.org/973303002
    
    Cr-Commit-Position: refs/heads/master@{#319661}

--- chrome/browser/ui/webui/extensions/extensions_ui.cc
+++ chrome/browser/ui/webui/extensions/extensions_ui.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/extensions/command_handler.h"
-#include "chrome/browser/ui/webui/extensions/extension_error_handler.h"
 #include "chrome/browser/ui/webui/extensions/extension_loader_handler.h"
 #include "chrome/browser/ui/webui/extensions/extension_settings_handler.h"
 #include "chrome/browser/ui/webui/extensions/install_extension_handler.h"
@@ -54,11 +53,6 @@ ExtensionsUI::ExtensionsUI(content::WebUI* web_ui) : WebUIController(web_ui) {
   CommandHandler* commands_handler = new CommandHandler(profile);
   commands_handler->GetLocalizedValues(source);
   web_ui->AddMessageHandler(commands_handler);
-
-  ExtensionErrorHandler* extension_error_handler =
-      new ExtensionErrorHandler(profile);
-  extension_error_handler->GetLocalizedValues(source);
-  web_ui->AddMessageHandler(extension_error_handler);
 
   ExtensionLoaderHandler* extension_loader_handler =
       new ExtensionLoaderHandler(profile);
