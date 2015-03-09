6dc7d320d01d4bcbf3656a2cfdf612f15e8eb21c
eroman <eroman@chromium.org>
2015-03-09 15:12:08 -0700

    Remove some jank tracking instrumentation which is now accounted for.
    BUG=454983
    
    Review URL: https://codereview.chromium.org/993703002
    
    Cr-Commit-Position: refs/heads/master@{#319745}

--- net/proxy/proxy_config_service_win.cc
+++ net/proxy/proxy_config_service_win.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/profiler/scoped_tracker.h"
 #include "base/stl_util.h"
 #include "base/strings/string_tokenizer.h"
 #include "base/strings/string_util.h"
@@ -64,11 +63,6 @@ void ProxyConfigServiceWin::AddObserver(Observer* observer) {
 }
 
 void ProxyConfigServiceWin::StartWatchingRegistryForChanges() {
-  // TODO(eroman): Remove once crbug.com/454983 is solved.
-  tracked_objects::ScopedTracker tracking_profile(
-      FROM_HERE_WITH_EXPLICIT_FUNCTION(
-          "454983 ProxyConfigServiceWin::StartWatchingRegistryForChanges"));
-
   if (!keys_to_watch_.empty())
     return;  // Already initialized.
 
