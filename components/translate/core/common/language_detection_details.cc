e34bf3d6ca31a5236128dcf54a21f41bf0fd48e7
thestig <thestig@chromium.org>
2015-03-09 06:08:12 -0700

    Fix uninit memory access in TranslateManagerRenderViewHostTests.
    BUG=464837
    
    Review URL: https://codereview.chromium.org/983283002
    
    Cr-Commit-Position: refs/heads/master@{#319614}

--- components/translate/core/common/language_detection_details.cc
+++ components/translate/core/common/language_detection_details.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace translate {
 
-LanguageDetectionDetails::LanguageDetectionDetails() {}
+LanguageDetectionDetails::LanguageDetectionDetails()
+    : is_cld_reliable(false) {
+}
 
 LanguageDetectionDetails::~LanguageDetectionDetails() {}
 
