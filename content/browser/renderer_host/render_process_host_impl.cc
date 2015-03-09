6ec0e197d0addc14683f564d35971e8665e28555
bokan <bokan@chromium.org>
2015-03-09 14:51:32 -0700

    (Reland) Always create top controls manager.
    This CL removes the calculate_top_controls_position flag and instead
    always creates the top controls manager. The methods of the manager
    become no-ops if the top controls height is 0 so that it becomes
    effectively enabled when a top controls height gets set.
    
    This fixes the attached bug because some Android Chrome UI pages don't
    have top controls but the manager was still created. These pages set
    the height to 0 to hide the top controls but the methods of the
    manager were still getting called. This caused a crash in Blink since
    ScrollBy would try to calculate the shown ratio with a 0 height, causing
    a NaN to propagate into Blink.
    
    BUG=460007
    
    Committed: https://crrev.com/7610e74eacde3468853c9765bd3f726c14df04c1
    Cr-Commit-Position: refs/heads/master@{#319257}
    
    Review URL: https://codereview.chromium.org/961023002
    
    Cr-Commit-Position: refs/heads/master@{#319738}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1325,7 +1325,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     cc::switches::kDisableThreadedAnimation,
     cc::switches::kEnableGpuBenchmarking,
     cc::switches::kEnableMainFrameBeforeActivation,
-    cc::switches::kEnableTopControlsPositionCalculation,
     cc::switches::kMaxTilesForInterestArea,
     cc::switches::kMaxUnusedResourceMemoryUsagePercentage,
     cc::switches::kShowCompositedLayerBorders,
