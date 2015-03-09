a26edd13bad7ad0e9fadaf30fa4ce35c1498faaf
alancutter@chromium.org <alancutter@chromium.org>
2015-03-09 08:02:27 +0000

    Web Animations: Add a fast path for LengthStyleIntepolation application
    This change bypasses creating a CSSValue when applying length animations
    where only pixels or percentage values are interpolating and the
    LayoutStyle has a Length setter for the animated property.
    
    This is a performance change, there should be no behavioural changes.
    Avoiding creating a CSSValue at application time improves the
    css_value_type_length_simple?api=web_animations benchmark by ~4%
    bringing it to the same level as css_value_type_length_simple?api=css_animations.
    Tested on a Nexus 7 device.
    
    BUG=309981
    
    Review URL: https://codereview.chromium.org/982913003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191521 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/LengthStyleInterpolation.h
+++ third_party/WebKit/Source/core/animation/LengthStyleInterpolation.h
@@ -8,13 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/animation/StyleInterpolation.h"
 #include "core/css/CSSPrimitiveValue.h"
-#include "platform/Length.h"
 
 namespace blink {
 
+class LayoutStyle;
+class Length;
+
 class LengthStyleInterpolation : public StyleInterpolation {
 public:
-
+    typedef void (LayoutStyle::*LengthSetter)(const Length&);
     typedef void NonInterpolableType;
 
     static PassRefPtrWillBeRawPtr<LengthStyleInterpolation> create(const CSSValue& start, const CSSValue& end, CSSPropertyID id, InterpolationRange range)
@@ -32,12 +34,20 @@ public:
     static PassRefPtrWillBeRawPtr<CSSPrimitiveValue> fromInterpolableValue(const InterpolableValue&, InterpolationRange);
 
 private:
-    LengthStyleInterpolation(PassOwnPtrWillBeRawPtr<InterpolableValue> start, PassOwnPtrWillBeRawPtr<InterpolableValue> end, CSSPropertyID id,  InterpolationRange range)
+    LengthStyleInterpolation(PassOwnPtrWillBeRawPtr<InterpolableValue> start, PassOwnPtrWillBeRawPtr<InterpolableValue> end, CSSPropertyID id, InterpolationRange range)
         : StyleInterpolation(start, end, id)
         , m_range(range)
-        { }
+        , m_lengthSetter(nullptr)
+    {
+        if (isPixelsOrPercentOnly(*m_start) && isPixelsOrPercentOnly(*m_end))
+            m_lengthSetter = lengthSetterForProperty(id);
+    }
+
+    static bool isPixelsOrPercentOnly(const InterpolableValue&);
+    static LengthSetter lengthSetterForProperty(CSSPropertyID);
 
     InterpolationRange m_range;
+    LengthSetter m_lengthSetter;
 };
 
 }
