ea821abe3d299adec9afb04ece811502ee3d7327
dtrainor <dtrainor@chromium.org>
2015-03-09 16:35:05 -0700

    Upstream Layout.java and associated files
    Start upstreaming Layout/LayoutManager files.
    
    BUG=428821
    
    Review URL: https://codereview.chromium.org/987883002
    
    Cr-Commit-Position: refs/heads/master@{#319769}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchPanelHost.java
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.compositor.bottombar.contextualsearch;
+
+/**
+ * Interface that allows {@link ContextualSearchPanel} to communicate with its host Layout.
+ */
+public interface ContextualSearchPanelHost {
+    /**
+     * Hides the Contextual Search Supported Layout.
+     * @param immediately Whether it should be hidden immediately.
+     */
+    void hideLayout(boolean immediately);
+}
