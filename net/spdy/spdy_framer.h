3031b151b33cae220cdbca8581175082b44fdb60
bnc <bnc@chromium.org>
2015-03-09 09:09:19 -0700

    Remove some dead code.
    This CL lands server change 87003629 by tgreer and change 87977978 by birenroy.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/980393003
    
    Cr-Commit-Position: refs/heads/master@{#319646}

--- net/spdy/spdy_framer.h
+++ net/spdy/spdy_framer.h
@@ -551,9 +551,6 @@ class NET_EXPORT_PRIVATE SpdyFramer {
   // Returns the maximum size a frame can be (data or control).
   size_t GetFrameMaximumSize() const;
 
-  // Returns the maximum size that a control frame can be.
-  size_t GetControlFrameMaximumSize() const;
-
   // Returns the maximum payload size of a DATA frame.
   size_t GetDataFrameMaximumPayload() const;
 
@@ -569,8 +566,6 @@ class NET_EXPORT_PRIVATE SpdyFramer {
   SpdyMajorVersion protocol_version() const { return protocol_version_; }
 
   bool probable_http_response() const { return probable_http_response_; }
-
-  SpdyStreamId expect_continuation() const { return expect_continuation_; }
 
   SpdyPriority GetLowestPriority() const {
     return protocol_version_ < SPDY3 ? 3 : 7;
