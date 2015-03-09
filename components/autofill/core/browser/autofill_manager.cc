4bd7df40a06b1daaee1dea508a42158061ee078c
rouslan <rouslan@chromium.org>
2015-03-09 15:28:24 -0700

    Credit card scanning field trial.
    Field trial "CreditCardScan" enables scanning credit cards. "Credit card
    scanning" flag can force enable or disable the feature.
    
    BUG=462381
    
    Review URL: https://codereview.chromium.org/991983003
    
    Cr-Commit-Position: refs/heads/master@{#319752}

--- components/autofill/core/browser/autofill_manager.cc
+++ components/autofill/core/browser/autofill_manager.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/guid.h"
 #include "base/logging.h"
 #include "base/message_loop/message_loop.h"
+#include "base/metrics/field_trial.h"
 #include "base/prefs/pref_service.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_util.h"
@@ -297,7 +298,10 @@ int AutofillManager::AccessAddressBookPromptCount() {
 bool AutofillManager::ShouldShowScanCreditCard(const FormData& form,
                                                const FormFieldData& field) {
   if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
-          autofill::switches::kEnableCreditCardScan)) {
+          autofill::switches::kEnableCreditCardScan) &&
+      (base::FieldTrialList::FindFullName("CreditCardScan") != "Enabled" ||
+       base::CommandLine::ForCurrentProcess()->HasSwitch(
+           autofill::switches::kDisableCreditCardScan))) {
     return false;
   }
 
