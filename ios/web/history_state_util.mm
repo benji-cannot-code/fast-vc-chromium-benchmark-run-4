0a35070ae2eb67494d9fb22e1f0f7e9fdeffa6b9
stuartmorgan <stuartmorgan@chromium.org>
2015-03-09 12:19:15 -0700

    Upstream various ios/web utilities and helpers
    Upstreams miscellaneous helper classes and utility files that don't
    have other dependencies, and can thus be built and tested as-is.
    
    BUG=464810
    
    Review URL: https://codereview.chromium.org/988383002
    
    Cr-Commit-Position: refs/heads/master@{#319706}

---
+++ ios/web/history_state_util.mm
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/web/history_state_util.h"
+
+#include "base/logging.h"
+#include "url/gurl.h"
+
+namespace web {
+namespace history_state_util {
+
+bool IsHistoryStateChangeValid(const GURL& currentUrl,
+                               const GURL& toUrl) {
+  // These two checks are very important to the security of the page. We cannot
+  // allow the page to change the state to an invalid URL.
+  CHECK(currentUrl.is_valid());
+  CHECK(toUrl.is_valid());
+
+  return toUrl.GetOrigin() == currentUrl.GetOrigin();
+}
+
+GURL GetHistoryStateChangeUrl(const GURL& currentUrl,
+                              const GURL& baseUrl,
+                              const std::string& destination) {
+  if (!baseUrl.is_valid())
+    return GURL();
+  GURL toUrl = baseUrl.Resolve(destination);
+
+  if (!toUrl.is_valid() || !IsHistoryStateChangeValid(currentUrl, toUrl))
+    return GURL();
+
+  return toUrl;
+}
+
+}  // namespace history_state_util
+}  // namespace web
---
+++ ios/web/web_state/wk_web_view_ssl_error_util.h
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_WEB_STATE_WK_WEB_VIEW_SSL_ERROR_UTIL_H_
+#define IOS_WEB_WEB_STATE_WK_WEB_VIEW_SSL_ERROR_UTIL_H_
+
+#import <Foundation/Foundation.h>
+
+namespace net {
+class SSLInfo;
+}
+
+namespace web {
+
+// NSErrorPeerCertificateChainKey from NSError's userInfo dict.
+extern NSString* const kNSErrorPeerCertificateChainKey;
+
+// Returns YES if geven error is a SSL error.
+BOOL IsWKWebViewSSLError(NSError* error);
+
+// Fills SSLInfo object with information extracted from |error|. Callers are
+// responsible to ensure that given |error| is an SSL error by calling
+// |web::IsSSLError| function.
+void GetSSLInfoFromWKWebViewSSLError(NSError* error, net::SSLInfo* ssl_info);
+
+}  // namespace web
+
+#endif // IOS_WEB_WEB_STATE_WK_WEB_VIEW_SSL_ERROR_UTIL_H_
