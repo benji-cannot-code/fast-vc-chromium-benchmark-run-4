a5c97b9895638409768c7b88885e01f650ed0629
rvargas <rvargas@chromium.org>
2015-03-09 16:53:45 -0700

    Redefine base::Process:Terminate so that it can replace base::KillProcess
    BUG=417532
    
    Review URL: https://codereview.chromium.org/983963002
    
    Cr-Commit-Position: refs/heads/master@{#319776}

--- base/process/process_unittest.cc
+++ base/process/process_unittest.cc
@@ -124,7 +124,7 @@ TEST_F(ProcessTest, Terminate) {
 
   exit_code = kDummyExitCode;
   int kExpectedExitCode = 250;
-  process.Terminate(kExpectedExitCode);
+  process.Terminate(kExpectedExitCode, false);
   process.WaitForExitWithTimeout(TestTimeouts::action_max_timeout(),
                                  &exit_code);
 
@@ -161,7 +161,7 @@ TEST_F(ProcessTest, WaitForExitWithTimeout) {
   EXPECT_FALSE(process.WaitForExitWithTimeout(timeout, &exit_code));
   EXPECT_EQ(kDummyExitCode, exit_code);
 
-  process.Terminate(kDummyExitCode);
+  process.Terminate(kDummyExitCode, false);
 }
 
 // Ensure that the priority of a process is restored correctly after
