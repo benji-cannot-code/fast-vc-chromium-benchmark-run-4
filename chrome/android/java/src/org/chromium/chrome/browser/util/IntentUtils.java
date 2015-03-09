a312b476981bd1879a24895b6ae5837d43b938d2
mariakhomenko <mariakhomenko@chromium.org>
2015-03-09 11:39:43 -0700

    Add IntentUtils to share common intent-related code.
    Adds the ability to extract which applications can handle a particular
    intent.
    
    BUG=464934
    
    Review URL: https://codereview.chromium.org/988963004
    
    Cr-Commit-Position: refs/heads/master@{#319698}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/util/IntentUtils.java
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.util;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utilities dealing with extracting information from intents.
+ */
+public class IntentUtils {
+
+    /**
+     * Retrieves a list of components that would handle the given intent.
+     * @param context The application context.
+     * @param intent The intent which we are interested in.
+     * @return The list of component names.
+     */
+    public static List<ComponentName> getIntentHandlers(Context context, Intent intent) {
+        List<ResolveInfo> list = context.getPackageManager().queryIntentActivities(intent, 0);
+        List<ComponentName> nameList = new ArrayList<ComponentName>();
+        for (ResolveInfo r : list) {
+            nameList.add(new ComponentName(r.activityInfo.packageName, r.activityInfo.name));
+        }
+        return nameList;
+    }
+}
