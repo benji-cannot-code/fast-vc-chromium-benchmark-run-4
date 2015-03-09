321dee423fe45a1b33d67c0d3ff9b550c2615d6c
hbos <hbos@chromium.org>
2015-03-09 09:37:48 -0700

    RTCVideoDecoder: Using I420VideoFrame directly instead of to-be-removed TextureVideoFrame (previously this typeddefed to I420VideoFrame).
    BUG=webrtc:1128
    
    Review URL: https://codereview.chromium.org/990913002
    
    Cr-Commit-Position: refs/heads/master@{#319652}

--- content/renderer/media/rtc_video_decoder.cc
+++ content/renderer/media/rtc_video_decoder.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/bind_to_current_loop.h"
 #include "media/renderers/gpu_video_accelerator_factories.h"
 #include "third_party/skia/include/core/SkBitmap.h"
-#include "third_party/webrtc/common_video/interface/texture_video_frame.h"
+#include "third_party/webrtc/common_video/interface/i420_video_frame.h"
 #include "third_party/webrtc/system_wrappers/interface/ref_count.h"
 
 namespace content {
@@ -391,11 +391,11 @@ void RTCVideoDecoder::PictureReady(const media::Picture& picture) {
   // Create a WebRTC video frame.
   webrtc::RefCountImpl<NativeHandleImpl>* handle =
       new webrtc::RefCountImpl<NativeHandleImpl>(frame);
-  webrtc::TextureVideoFrame decoded_image(handle,
-                                          picture.visible_rect().width(),
-                                          picture.visible_rect().height(),
-                                          timestamp,
-                                          0);
+  webrtc::I420VideoFrame decoded_image(handle,
+                                       picture.visible_rect().width(),
+                                       picture.visible_rect().height(),
+                                       timestamp,
+                                       0);
 
   // Invoke decode callback. WebRTC expects no callback after Reset or Release.
   {
