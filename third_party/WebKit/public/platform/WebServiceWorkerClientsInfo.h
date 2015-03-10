6ccf31e5624ea3cfe8e0f973f562247357d0f53e
kinuko@chromium.org <kinuko@chromium.org>
2015-03-10 00:26:21 +0000

    Use UUID for ServiceWorker Client identifier (1/3, blink)
    I'm not going to surface the new 'id' field on Client surface yet,
    but here're the spec pointers that explains why we do this:
    Spec issue: https://github.com/slightlyoff/ServiceWorker/issues/634
    More context: https://github.com/slightlyoff/ServiceWorker/issues/609
    
    To be landed in 3 patches:
    1/3: THIS
    2/3: chromium-side patch https://crrev.com/988063004/
    3/3: blink-side cleanup
    
    BUG=464668
    TEST=existing tests should pass
    
    Review URL: https://codereview.chromium.org/990583002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebServiceWorkerClientsInfo.h
+++ third_party/WebKit/public/platform/WebServiceWorkerClientsInfo.h
@@ -27,7 +27,8 @@ struct WebServiceWorkerClientInfo {
     {
     }
 
-    int clientID;
+    int clientID; // FIXME: Deprecate this when we switch to UUID.
+    WebString uuid;
 
     WebPageVisibilityState pageVisibilityState;
     bool isFocused;
