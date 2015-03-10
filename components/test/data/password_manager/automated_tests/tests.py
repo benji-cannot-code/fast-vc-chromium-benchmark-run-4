b418327aafded03cc3a3dcc788740a4640538d82
melandory <melandory@chromium.org>
2015-03-10 05:57:25 -0700

    [Password manager tests automation] Tests for alexa.com
    Adding test for Alexa. Test passes.
    
    BUG=369521
    R=vabr@chromium.org
    
    Review URL: https://codereview.chromium.org/992223002
    
    Cr-Commit-Position: refs/heads/master@{#319867}

--- components/test/data/password_manager/automated_tests/tests.py
+++ components/test/data/password_manager/automated_tests/tests.py
@@ -29,6 +29,15 @@ class TypeOfTestedWebsites:
     pass
 
 
+class Alexa(WebsiteTest):
+
+  def Login(self):
+    self.GoTo("https://www.alexa.com/secure/login")
+    self.FillUsernameInto("#email")
+    self.FillPasswordInto("#pwd")
+    self.Submit("#pwd")
+
+
 class Facebook(WebsiteTest):
 
   def Login(self):
@@ -269,6 +278,7 @@ class Vube(WebsiteTest):
 def Tests(environment, tests_to_run=None):
 
   working_tests = {
+    "alexa": Alexa("alexa"),
     "facebook": Facebook("facebook"),
     "google": Google("google"),
     "linkedin": Linkedin("linkedin"),
