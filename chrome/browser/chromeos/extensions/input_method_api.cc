02c535859d087fb8f2d1661b7cdc6c47fad3965c
kalman <kalman@chromium.org>
2015-03-10 14:42:55 -0700

    [Extensions] Make value_builder.h usable outside of tests.
    R=rockot@chromium.org
    
    Review URL: https://codereview.chromium.org/1000443002
    
    Cr-Commit-Position: refs/heads/master@{#319980}

--- chrome/browser/chromeos/extensions/input_method_api.cc
+++ chrome/browser/chromeos/extensions/input_method_api.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/chromeos_switches.h"
 #include "extensions/browser/extension_function_registry.h"
 #include "extensions/browser/extension_system.h"
-#include "extensions/common/value_builder.h"
 #include "ui/base/ime/chromeos/extension_ime_util.h"
 #include "ui/base/ime/chromeos/input_method_descriptor.h"
 #include "ui/base/ime/chromeos/input_method_manager.h"
