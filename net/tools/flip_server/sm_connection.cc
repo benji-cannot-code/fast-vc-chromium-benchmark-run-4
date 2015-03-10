5f0ab7fdf1ae09c84b8d5859b31facbd1b8f523f
dougk <dougk@chromium.org>
2015-03-10 11:24:57 -0700

    make EpollServer with with Posix poll(), take 2.
    This is in lieu of https://codereview.chromium.org/960503005/
    
    BUG=461857
    
    Review URL: https://codereview.chromium.org/987803002
    
    Cr-Commit-Position: refs/heads/master@{#319926}

--- net/tools/flip_server/sm_connection.cc
+++ net/tools/flip_server/sm_connection.cc
@@ -66,8 +66,8 @@ EpollServer* SMConnection::epoll_server() { return epoll_server_; }
 
 void SMConnection::ReadyToSend() {
   VLOG(2) << log_prefix_ << ACCEPTOR_CLIENT_IDENT
-          << "Setting ready to send: EPOLLIN | EPOLLOUT";
-  epoll_server_->SetFDReady(fd_, EPOLLIN | EPOLLOUT);
+          << "Setting ready to send: POLLIN | POLLOUT";
+  epoll_server_->SetFDReady(fd_, NET_POLLIN | NET_POLLOUT);
 }
 
 void SMConnection::EnqueueDataFrame(DataFrame* df) {
@@ -151,7 +151,8 @@ void SMConnection::InitSMConnection(SMConnectionPoolInterface* connection_pool,
 
   read_buffer_.Clear();
 
-  epoll_server_->RegisterFD(fd_, this, EPOLLIN | EPOLLOUT | EPOLLET);
+  epoll_server_->RegisterFD(fd_, this,
+                            PollBits(NET_POLLIN | NET_POLLOUT | NET_POLLET));
 
   if (use_ssl) {
     ssl_ = CreateSSLContext(ssl_state_->ssl_ctx);
@@ -257,14 +258,14 @@ void SMConnection::Cleanup(const char* cleanup) {
 
 void SMConnection::HandleEvents() {
   VLOG(2) << log_prefix_ << ACCEPTOR_CLIENT_IDENT
-          << "Received: " << EpollServer::EventMaskToString(events_).c_str();
+          << "Received: " << PollBits(events_).ToString();
 
-  if (events_ & EPOLLIN) {
+  if (events_ & NET_POLLIN) {
     if (!DoRead())
       goto handle_close_or_error;
   }
 
-  if (events_ & EPOLLOUT) {
+  if (events_ & NET_POLLOUT) {
     // Check if we have connected or not
     if (connection_complete_ == false) {
       int sock_error;
@@ -293,7 +294,7 @@ void SMConnection::HandleEvents() {
       goto handle_close_or_error;
   }
 
-  if (events_ & (EPOLLHUP | EPOLLERR)) {
+  if (events_ & (NET_POLLHUP | NET_POLLERR)) {
     VLOG(1) << log_prefix_ << ACCEPTOR_CLIENT_IDENT << "!!! Got HUP or ERR";
     goto handle_close_or_error;
   }
@@ -430,7 +431,7 @@ bool SMConnection::DoRead() {
           case SSL_ERROR_WANT_WRITE:
           case SSL_ERROR_WANT_ACCEPT:
           case SSL_ERROR_WANT_CONNECT:
-            events_ &= ~EPOLLIN;
+            events_ &= ~NET_POLLIN;
             VLOG(2) << log_prefix_ << ACCEPTOR_CLIENT_IDENT
                     << "DoRead: SSL WANT_XXX: " << err;
             goto done;
@@ -446,7 +447,7 @@ bool SMConnection::DoRead() {
     if (bytes_read == -1) {
       switch (stored_errno) {
         case EAGAIN:
-          events_ &= ~EPOLLIN;
+          events_ &= ~NET_POLLIN;
           VLOG(2) << log_prefix_ << ACCEPTOR_CLIENT_IDENT
                   << "Got EAGAIN while reading";
           goto done;
@@ -510,13 +511,13 @@ bool SMConnection::DoConsumeReadData() {
       VLOG(2) << log_prefix_ << ACCEPTOR_CLIENT_IDENT
               << "HandleRequestFullyRead: Setting EPOLLOUT";
       HandleResponseFullyRead();
-      events_ |= EPOLLOUT;
+      events_ |= NET_POLLOUT;
     } else if (sm_interface_->Error()) {
       LOG(ERROR) << log_prefix_ << ACCEPTOR_CLIENT_IDENT
                  << "Framer error detected: Setting EPOLLOUT: "
                  << sm_interface_->ErrorAsString();
       // this causes everything to be closed/cleaned up.
-      events_ |= EPOLLOUT;
+      events_ |= NET_POLLOUT;
       return false;
     }
     read_buffer_.GetReadablePtr(&bytes, &size);
@@ -540,7 +541,7 @@ bool SMConnection::DoWrite() {
       sm_interface_->GetOutput();
     }
     if (output_list_.empty()) {
-      events_ &= ~EPOLLOUT;
+      events_ &= ~NET_POLLOUT;
     }
   }
   while (!output_list_.empty()) {
@@ -550,7 +551,7 @@ bool SMConnection::DoWrite() {
       VLOG(2) << log_prefix_ << ACCEPTOR_CLIENT_IDENT
               << " byte sent >= max bytes sent per write: Setting EPOLLOUT: "
               << bytes_sent;
-      events_ |= EPOLLOUT;
+      events_ |= NET_POLLOUT;
       break;
     }
     if (sm_interface_ && output_list_.size() < 2) {
@@ -582,7 +583,7 @@ bool SMConnection::DoWrite() {
     if (bytes_written == -1) {
       switch (stored_errno) {
         case EAGAIN:
-          events_ &= ~EPOLLOUT;
+          events_ &= ~NET_POLLOUT;
           VLOG(2) << log_prefix_ << ACCEPTOR_CLIENT_IDENT
                   << "Got EAGAIN while writing";
           goto done;
@@ -604,7 +605,7 @@ bool SMConnection::DoWrite() {
       continue;
     } else if (bytes_written == -2) {
       // -2 handles SSL_ERROR_WANT_* errors
-      events_ &= ~EPOLLOUT;
+      events_ &= ~NET_POLLOUT;
       goto done;
     }
     VLOG(1) << log_prefix_ << ACCEPTOR_CLIENT_IDENT
