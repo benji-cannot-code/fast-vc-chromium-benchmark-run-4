37de1f45210dfda589eca41fd5c8cf77fa5bc4ec
magjed <magjed@chromium.org>
2015-03-10 05:38:06 -0700

    MediaStreamRemoteVideoSource: Wrap cricket::VideoFrame instead of copying.
    We can use cricket::VideoFrame::Copy() to make a shallow reference counted copy.
    
    Review URL: https://codereview.chromium.org/877283004
    
    Cr-Commit-Position: refs/heads/master@{#319866}

--- content/renderer/media/webrtc/media_stream_remote_video_source.cc
+++ content/renderer/media/webrtc/media_stream_remote_video_source.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/webrtc/track_observer.h"
 #include "media/base/bind_to_current_loop.h"
 #include "media/base/video_frame.h"
-#include "media/base/video_frame_pool.h"
 #include "media/base/video_util.h"
 #include "third_party/libjingle/source/talk/media/base/videoframe.h"
 
@@ -49,9 +48,6 @@ class MediaStreamRemoteVideoSource::RemoteVideoSourceDelegate
   base::ThreadChecker thread_checker_;
 
   scoped_refptr<base::MessageLoopProxy> io_message_loop_;
-  // |frame_pool_| is only accessed on whatever
-  // thread webrtc::VideoRendererInterface::RenderFrame is called on.
-  media::VideoFramePool frame_pool_;
 
   // |frame_callback_| is accessed on the IO thread.
   VideoCaptureDeliverFrameCB frame_callback_;
@@ -88,21 +84,23 @@ RemoteVideoSourceDelegate::RenderFrame(
     video_frame->set_timestamp(timestamp);
   } else {
     gfx::Size size(frame->GetWidth(), frame->GetHeight());
-    video_frame = frame_pool_.CreateFrame(
-        media::VideoFrame::YV12, size, gfx::Rect(size), size, timestamp);
 
     // Non-square pixels are unsupported.
     DCHECK_EQ(frame->GetPixelWidth(), 1u);
     DCHECK_EQ(frame->GetPixelHeight(), 1u);
 
-    int y_rows = frame->GetHeight();
-    int uv_rows = frame->GetChromaHeight();
-    CopyYPlane(
-        frame->GetYPlane(), frame->GetYPitch(), y_rows, video_frame.get());
-    CopyUPlane(
-        frame->GetUPlane(), frame->GetUPitch(), uv_rows, video_frame.get());
-    CopyVPlane(
-        frame->GetVPlane(), frame->GetVPitch(), uv_rows, video_frame.get());
+    // Make a shallow copy. Both |frame| and |video_frame| will share a single
+    // reference counted frame buffer. Const cast and hope no one will overwrite
+    // the data.
+    // TODO(magjed): Update media::VideoFrame to support const data so we don't
+    // need to const cast here.
+    video_frame = media::VideoFrame::WrapExternalYuvData(
+        media::VideoFrame::YV12, size, gfx::Rect(size), size,
+        frame->GetYPitch(), frame->GetUPitch(), frame->GetVPitch(),
+        const_cast<uint8_t*>(frame->GetYPlane()),
+        const_cast<uint8_t*>(frame->GetUPlane()),
+        const_cast<uint8_t*>(frame->GetVPlane()), timestamp,
+        base::Bind(&base::DeletePointer<cricket::VideoFrame>, frame->Copy()));
   }
 
   io_message_loop_->PostTask(
