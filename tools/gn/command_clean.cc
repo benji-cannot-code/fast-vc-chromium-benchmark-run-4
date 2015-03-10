aa5c356beef9e5220c7c13a3d2335dc314890fd8
tfarina <tfarina@chromium.org>
2015-03-10 12:13:12 -0700

    tools/gn: Cleanup - remove unused scoped_file.h include.
    This was left behind when cleaning up the 'clean' command after some
    refactoring.
    
    TEST=ninja -C out-gn gn && out-gn/gn gen out-gn
    TBR=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/994153002
    
    Cr-Commit-Position: refs/heads/master@{#319936}

--- tools/gn/command_clean.cc
+++ tools/gn/command_clean.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
-#include "base/files/scoped_file.h"
 #include "base/strings/string_split.h"
 #include "base/strings/stringprintf.h"
 #include "tools/gn/commands.h"
