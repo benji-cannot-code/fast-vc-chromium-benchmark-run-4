b238eac2cdfaf7c4c9d460fa14341ccc5a2f51c3
alexst <alexst@chromium.org>
2015-03-10 14:06:09 -0700

    [ozone] Add an about flag to enable ozone overlay testing.
    BUG=
    
    Committed: https://crrev.com/59e9a996f54bb238beb5a51dd864eb0febb8728c
    Cr-Commit-Position: refs/heads/master@{#318964}
    
    Review URL: https://codereview.chromium.org/967733005
    
    Cr-Commit-Position: refs/heads/master@{#319967}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -67,6 +67,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/switches.h"
 #endif
 
+#if defined(USE_OZONE)
+#include "ui/ozone/public/ozone_switches.h"
+#endif
+
 namespace about_flags {
 
 // Macros to simplify specifying the type.
@@ -2209,6 +2213,15 @@ const Experiment kExperiments[] = {
     ENABLE_DISABLE_VALUE_TYPE(switches::kEnableChildAccountDetection,
                               switches::kDisableChildAccountDetection)
   },
+#if defined(OS_CHROMEOS) && defined(USE_OZONE)
+  {
+    "ozone-test-single-overlay-support",
+    IDS_FLAGS_OZONE_TEST_SINGLE_HARDWARE_OVERLAY,
+    IDS_FLAGS_OZONE_TEST_SINGLE_HARDWARE_OVERLAY_DESCRIPTION,
+    kOsCrOS,
+    SINGLE_VALUE_TYPE(switches::kOzoneTestSingleOverlaySupport)
+  },
+#endif  // defined(OS_CHROMEOS) && defined(USE_OZONE)
 
   // NOTE: Adding new command-line switches requires adding corresponding
   // entries to enum "LoginCustomFlags" in histograms.xml. See note in
