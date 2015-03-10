72c09d1e9714572122192c6f5015b5bc84718e85
hans <hans@chromium.org>
2015-03-10 14:31:59 -0700

    Roll Clang 231191:231690
    This includes fixes for ASan/MSan/UBSan configurations.
    
    BUG=464621
    
    Review URL: https://codereview.chromium.org/993833002
    
    Cr-Commit-Position: refs/heads/master@{#319978}

--- tools/clang/scripts/update.sh
+++ tools/clang/scripts/update.sh
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Do NOT CHANGE this if you don't know what you're doing -- see
 # https://code.google.com/p/chromium/wiki/UpdatingClang
 # Reverting problematic clang rolls is safe, though.
-CLANG_REVISION=231191
+CLANG_REVISION=231690
 
 # This is incremented when pushing a new build of Clang at the same revision.
-CLANG_SUB_REVISION=2
+CLANG_SUB_REVISION=1
 
 PACKAGE_VERSION="${CLANG_REVISION}-${CLANG_SUB_REVISION}"
 
