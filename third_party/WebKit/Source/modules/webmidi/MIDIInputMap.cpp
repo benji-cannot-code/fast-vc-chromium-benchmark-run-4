a66185150cb730ebf6f0ff142f3f3a70943d33a0
yhirano@chromium.org <yhirano@chromium.org>
2015-03-10 09:57:27 +0000

    [MIDI] Keep insertion order in MIDI ports iteration.
    To keep the order in ports iteration, this CL uses a vector to store ports
    instead of a hash map.
    
    BUG=465031
    
    Review URL: https://codereview.chromium.org/986313002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191611 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webmidi/MIDIInputMap.cpp
+++ third_party/WebKit/Source/modules/webmidi/MIDIInputMap.cpp
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-MIDIInputMap::MIDIInputMap(const HeapHashMap<String, Member<MIDIInput>> map)
-    : MIDIPortMap<MIDIInput>(map)
+MIDIInputMap::MIDIInputMap(const HeapVector<Member<MIDIInput>>& entries)
+    : MIDIPortMap<MIDIInput>(entries)
 {
 }
 
--- third_party/WebKit/Source/modules/webmidi/MIDIOutputMap.cpp
+++ third_party/WebKit/Source/modules/webmidi/MIDIOutputMap.cpp
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-MIDIOutputMap::MIDIOutputMap(HeapHashMap<String, Member<MIDIOutput>> map)
-    : MIDIPortMap<MIDIOutput>(map)
+MIDIOutputMap::MIDIOutputMap(HeapVector<Member<MIDIOutput>>& entries)
+    : MIDIPortMap<MIDIOutput>(entries)
 {
 }
 
