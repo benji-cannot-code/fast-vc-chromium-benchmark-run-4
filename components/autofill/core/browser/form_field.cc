332b86d4f7cd9ff01eab18432919030d221f51ef
thestig <thestig@chromium.org>
2015-03-10 14:54:54 -0700

    Autofill: Recognize input tags with type=number.
    BUG=465576
    
    Review URL: https://codereview.chromium.org/987283002
    
    Cr-Commit-Position: refs/heads/master@{#319983}

--- components/autofill/core/browser/form_field.cc
+++ components/autofill/core/browser/form_field.cc
@@ -203,6 +203,9 @@ bool FormField::MatchesFormControlType(const std::string& type,
   if ((match_type & MATCH_PASSWORD) && type == "password")
     return true;
 
+  if ((match_type & MATCH_NUMBER) && type == "number")
+    return true;
+
   return false;
 }
 
