81fb48425b74218b1794334ab53bebc5df2b9574
lizeb <lizeb@chromium.org>
2015-03-10 12:21:17 -0700

    Early preconnection for intent-based startups on Android.
    When Chromium starts with an intent, there is a location to navigate to,
    from the intent. However a lot of initialization defers the first
    network activity by more than 1s on fast devices.
    
    A previous CL (https://codereview.chromium.org/909893003/) has added an
    early DNS request to the destination, and this one adds an early
    preconnection to it. The preconnection is possible as soon as the
    network stack is ready, but currently happens after Tab.loadUrl(),
    several 100s of ms later. This patch moves the preconnection earlier.
    
    However, if the early DNS request is still in flight, then the
    preconnection will itself issue a new DNS request, negating the gains
    from the early first one. Assuming that DNS requests come back in order,
    it is then better to delay the preconnection until the DNS answer has
    been obtained.
    
    Note: the associated bug contains a link to a document explaining the
    motivation, testing methodology and experimental results of this change.
    
    BUG=458216
    
    Review URL: https://codereview.chromium.org/924493002
    
    Cr-Commit-Position: refs/heads/master@{#319939}

---
+++ chrome/browser/android/warmup_manager.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ANDROID_WARMUP_MANAGER_H_
+#define CHROME_BROWSER_ANDROID_WARMUP_MANAGER_H_
+
+#include <jni.h>
+
+bool RegisterWarmupManager(JNIEnv* env);
+#endif  // CHROME_BROWSER_ANDROID_WARMUP_MANAGER_H_
