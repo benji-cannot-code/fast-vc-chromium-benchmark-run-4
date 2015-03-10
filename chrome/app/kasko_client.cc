3ce76722c42951d0f7ae751e688e8e73cdaa4940
erikwright <erikwright@chromium.org>
2015-03-10 14:58:32 -0700

    Make Kasko dump size match breakpad dump size.
    BUG=460512
    
    Review URL: https://codereview.chromium.org/995843002
    
    Cr-Commit-Position: refs/heads/master@{#319984}

--- chrome/app/kasko_client.cc
+++ chrome/app/kasko_client.cc
@@ -25,11 +25,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "syzygy/kasko/api/client.h"
 
 namespace {
+
 ChromeWatcherClient* g_chrome_watcher_client = nullptr;
+kasko::api::MinidumpType g_minidump_type = kasko::api::SMALL_DUMP_TYPE;
+
 }  // namespace
 
-KaskoClient::KaskoClient(ChromeWatcherClient* chrome_watcher_client) {
+KaskoClient::KaskoClient(ChromeWatcherClient* chrome_watcher_client,
+                         kasko::api::MinidumpType minidump_type) {
   DCHECK(!g_chrome_watcher_client);
+  g_minidump_type = minidump_type;
   g_chrome_watcher_client = chrome_watcher_client;
 
   kasko::api::InitializeClient(
@@ -75,8 +80,8 @@ extern "C" void __declspec(dllexport) ReportCrashWithProtobuf(
 
     if (g_chrome_watcher_client &&
         g_chrome_watcher_client->EnsureInitialized()) {
-      kasko::api::SendReport(info, protobuf, protobuf_length, crash_keys,
-                             crash_key_count);
+      kasko::api::SendReport(info, g_minidump_type, protobuf, protobuf_length,
+                             crash_keys, crash_key_count);
     }
   }
 
