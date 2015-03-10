684c7cf39758343645cd434abe14674ce7e2bb50
bnc <bnc@chromium.org>
2015-03-09 17:50:28 -0700

    Add unittest for GetNumberRequiredContinuationFrames.
    Add unittest for Spdy::GetNumberRequiredContinuationFrames, including the test
    case provided in the bug report.
    
    BUG=464748
    
    Review URL: https://codereview.chromium.org/995453003
    
    Cr-Commit-Position: refs/heads/master@{#319797}

--- net/spdy/spdy_framer.h
+++ net/spdy/spdy_framer.h
@@ -599,6 +599,7 @@ class NET_EXPORT_PRIVATE SpdyFramer {
                            UnclosedStreamDataCompressorsOneByteAtATime);
   FRIEND_TEST_ALL_PREFIXES(SpdyFramerTest,
                            UncompressLargerThanFrameBufferInitialSize);
+  FRIEND_TEST_ALL_PREFIXES(SpdyFramerTest, GetNumberRequiredContinuationFrames);
   FRIEND_TEST_ALL_PREFIXES(SpdyFramerTest,
                            CreatePushPromiseThenContinuationUncompressed);
   FRIEND_TEST_ALL_PREFIXES(SpdyFramerTest, ReadLargeSettingsFrame);
