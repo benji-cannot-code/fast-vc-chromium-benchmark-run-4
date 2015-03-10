9a63b6c1cef1ae34ec593c13f637d77867ad5866
sigbjornf@opera.com <sigbjornf@opera.com>
2015-03-10 19:27:00 +0000

    Oilpan: fix build after r191643 (4e6a640c).
    TBR=oilpan-reviews
    BUG=459001
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/995923002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191651 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/presentation/DefaultSessionStartEvent.cpp
+++ third_party/WebKit/Source/modules/presentation/DefaultSessionStartEvent.cpp
@@ -35,4 +35,10 @@ const AtomicString& DefaultSessionStartEvent::interfaceName() const
     return EventNames::DefaultSessionStartEvent;
 }
 
+DEFINE_TRACE(DefaultSessionStartEvent)
+{
+    visitor->trace(m_session);
+    Event::trace(visitor);
+}
+
 } // namespace blink
