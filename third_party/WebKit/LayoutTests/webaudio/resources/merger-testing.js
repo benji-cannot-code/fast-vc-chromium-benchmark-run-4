a3846948e7375e2e325956799144799a06d788a3
hongchan@chromium.org <hongchan@chromium.org>
2015-03-11 18:46:30 +0000

    Fixing input handling behavior of ChannelMergerNode
    The current implementation of ChannelMergerNode has two critical issues:
    1. Impossible to specify input-output mapping due to dynamic channel re-arranging.
      https://github.com/WebAudio/web-audio-api/issues/304, crbug.com/441451
    2. ChannelMergerNode connected with DelayNode in a cyclic audiograph causes a silent failure of audio rendering.  
      https://github.com/WebAudio/web-audio-api/issues/459, crbug.com/442925
    
    To address these issues, rtoy@, cwilso@ and hongchan@ proposed a new behavior of ChannelMergerNode:
    
    1. The following properties are fixed for ChannelMergerNode and InvalidState error should be thrown when user changes them.
      - ChannelMergerNode.channelCount = 1
      - ChannelMergerNode.channelCountMode = ‘explicit’
    2. Each input will be summed to mono based on the specified mixing rule.
    3. The summed mono channel from input will be transferred to the corresponding output channel (input_1 -> output_channel_1). If an input is not connected (inactive or disconnected), it still counts as one silent channel in output.
    
    Note that the up/downmixing rule is only applied to each input when the channel layout is canonical (mono, stereo, quad, 5.0 and 5.1). For undefined channel layout, the mixing rule automatically changes to 'discrete' mode. That means the merger will just take the first channel and drop the rest.
    
    BUG=441451, 442925
    TESTS=4 new tests
    audiochannelmerger-cycle.html: to cover the bug 442925
    audiochannelmerger-disconnect.html: to cover the bug 441451
    audiochannelmerger-input.html: to cover the bug 441451
    audiochannelmerger-input-non-default.html: to test the merger with non-default setting
    
    Note that the following laytout test files were removed (or renamed) and merged into the new tests:
    channelmerger-basic.html => audiochannelmerger-basic.html
    channelmerger-input-handling.html => audiochannelmerger-input.html
    channelmerger-cycle.html => audiochannelmerger-cycle.html
    channelmerger-channel-limit.html => audiochannelmerger-basic.html
    audiochannelmerger-stereo.html => audiochannelmerger-input.html
    
    Review URL: https://codereview.chromium.org/808653010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191723 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/webaudio/resources/merger-testing.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// This file is for the audiochannelmerger-* layout tests.
+// Requires |audio-testing.js| to work properly.
+
+function testMergerInput(config, done) {
+  var context = new OfflineAudioContext(
+    config.numberOfChannels, 128, 44100
+  );
+  var merger = context.createChannelMerger(config.numberOfChannels);
+  var source = context.createBufferSource();
+
+  // Create a test source buffer.
+  source.buffer = createTestingAudioBuffer(
+    context, config.testBufferChannelCount, 128
+  );
+
+  // Connect the output of source into the specified input of merger.
+  if (config.mergerInputIndex)
+    source.connect(merger, 0, config.mergerInputIndex);
+  else
+    source.connect(merger);
+  merger.connect(context.destination);
+  source.start();
+
+  context.startRendering().then(function (buffer) {
+    for (var i = 0; i < config.numberOfChannels; i++)
+      Should.haveValueInChannel(config.expected[i], buffer.getChannelData(i));
+
+    done();
+  });
+}
