ad0ea1d4f44fad3905a7c16e112f6c8e428b5f2a
sigbjornf@opera.com <sigbjornf@opera.com>
2015-03-11 12:51:41 +0000

    Add an explicit dispose step for WebDevToolsAgentImpl.
    In order to provide an orderly shutdown of a WebViewImpl's
    WebDevToolsAgentImpl object, add a dispose() step before clearing out
    its reference. Delaying the dispose steps until WebDevToolsAgentImpl
    is finalized, is incompatible with Oilpan.
    
    R=haraken
    BUG=451004
    
    Review URL: https://codereview.chromium.org/996163002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191698 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -1664,8 +1664,10 @@ void WebViewImpl::close()
     }
 
     // Should happen after m_page.clear().
-    if (m_devToolsAgent)
+    if (m_devToolsAgent) {
+        m_devToolsAgent->dispose();
         m_devToolsAgent.clear();
+    }
 
     // Reset the delegate to prevent notifications being sent as we're being
     // deleted.
