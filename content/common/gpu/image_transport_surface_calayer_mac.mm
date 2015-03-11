f2a64646b2c1bd1388e9fdc0089fb1bc9bfec83e
ccameron <ccameron@chromium.org>
2015-03-10 19:49:18 -0700

    Mac: More quickly transition from asynchronous to synchronous drawing
    CAOpenGLLayers can draw either in synchronous mode, where we explicitly
    issue a draw call, or asynchronous mode, where CA asks us to draw on
    a regular interval. It is only possible to have smooth, vsync-aligned
    animation through the asynchronous mode. The downside to using
    asynchronous mode is that it will wake up the process at every vsync,
    asking if there is new content to be drawn.
    
    Because we do not know if a given frame is intended as a smooth
    animation or a one-time change of content, we must guess if we should
    be in asynchronous mode or synchronous mode. Our guessing scheme is to
    guess that we will need to animate smoothly, and then back off from
    this position if we don't see any new frames from the renderer.
    
    Prior to this patch, we wait 30 frames before deciding to go back to
    synchronous mode. With this patch, we just wait 4. The downside to
    going to synchronous mode is that getting smooth animation will be
    harder (but if we're already skipping 3 out of every 4 frames, then
    we're not smoothly animating to begin with).
    
    BUG=437987
    
    Review URL: https://codereview.chromium.org/994763002
    
    Cr-Commit-Position: refs/heads/master@{#320038}

--- content/common/gpu/image_transport_surface_calayer_mac.mm
+++ content/common/gpu/image_transport_surface_calayer_mac.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 const size_t kFramesToKeepCAContextAfterDiscard = 2;
+const size_t kCanDrawFalsesBeforeSwitchFromAsync = 4;
 }
 
 @interface ImageTransportLayer : CAOpenGLLayer {
@@ -299,10 +300,10 @@ bool CALayerStorageProvider::LayerCanDraw() {
     if ([layer_ isAsynchronous]) {
       DCHECK(!gpu_vsync_disabled_);
       // If we are in asynchronous mode, we will be getting callbacks at every
-      // vsync, asking us if we have anything to draw. If we get 30 of these in
-      // a row, ask that we stop getting these callback for now, so that we
+      // vsync, asking us if we have anything to draw. If we get many of these
+      // in a row, ask that we stop getting these callback for now, so that we
       // don't waste CPU cycles.
-      if (can_draw_returned_false_count_ == 30)
+      if (can_draw_returned_false_count_ >= kCanDrawFalsesBeforeSwitchFromAsync)
         [layer_ setAsynchronous:NO];
       else
         can_draw_returned_false_count_ += 1;
