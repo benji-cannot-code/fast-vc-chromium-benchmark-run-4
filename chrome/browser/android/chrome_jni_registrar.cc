91b4b1f6daa9a7f20429761169465d6bbcbce4a2
mariakhomenko <mariakhomenko@chromium.org>
2015-03-10 19:40:56 -0700

    Upstreaming TransitionPageHelper.
    BUG=464934
    
    Review URL: https://codereview.chromium.org/994813003
    
    Cr-Commit-Position: refs/heads/master@{#320035}

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/signin/signin_manager_android.h"
 #include "chrome/browser/android/tab_android.h"
 #include "chrome/browser/android/tab_state.h"
+#include "chrome/browser/android/transition_page_helper.h"
 #include "chrome/browser/android/url_utilities.h"
 #include "chrome/browser/android/voice_search_tab_helper.h"
 #include "chrome/browser/autofill/android/personal_data_manager_android.h"
@@ -235,6 +236,7 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
     {"TabState", RegisterTabState},
     {"TemplateUrlServiceAndroid", TemplateUrlServiceAndroid::Register},
     {"ToolbarModelAndroid", ToolbarModelAndroid::RegisterToolbarModelAndroid},
+    {"TransitionPageHelper", TransitionPageHelper::Register},
     {"TranslateInfoBarDelegate", RegisterTranslateInfoBarDelegate},
     {"TtsPlatformImpl", TtsPlatformImplAndroid::Register},
     {"UmaBridge", RegisterUmaBridge},
