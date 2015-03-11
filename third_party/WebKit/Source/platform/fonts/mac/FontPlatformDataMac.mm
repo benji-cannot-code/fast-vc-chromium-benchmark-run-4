a7ed7e4e0ea73b0714d7e7316a77790940c2f6e1
fmalita@chromium.org <fmalita@chromium.org>
2015-03-11 22:06:41 +0000

    Remove FontPlatformData::setupPaint()'s GraphicsContext arg
    Exposing a GraphicsContext to setupPaint() and making decisions based
    on its state is undesirable as it iterferes with text blob (and
    slimming paint) caching.
    
    After http://crrev.com/987113002 only FontPlatformDataLinux is still
    peeking at the GC arg, and solely for getting the device scale factor.
    
    Let's lock this down to discourage relying on GraphicsContext state in
    the future: remove the context arg and instead pass an explicit device
    scale factor.
    
    (the change should have no functional side-effects)
    
    R=jbroman@chromium.org,eae@chromium.org,dominik.rottsches@intel.com
    
    Review URL: https://codereview.chromium.org/1002453002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191737 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/mac/FontPlatformDataMac.mm
+++ third_party/WebKit/Source/platform/fonts/mac/FontPlatformDataMac.mm
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void FontPlatformData::setupPaint(SkPaint* paint, GraphicsContext*, const Font* font) const
+void FontPlatformData::setupPaint(SkPaint* paint, float, const Font* font) const
 {
     bool shouldSmoothFonts = true;
     bool shouldAntialias = true;
