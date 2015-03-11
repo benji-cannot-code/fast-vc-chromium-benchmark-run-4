38e7223be12f1c1671262bd2287a590bb3549dbb
bashi@chromium.org <bashi@chromium.org>
2015-03-11 13:25:19 +0000

    bindings: Add a macro for V8 Maybe<T> APIs.
    These APIs require !IsNothing() check before getting values. It's worth adding a macro which does the check.
    
    BUG=462402
    
    Review URL: https://codereview.chromium.org/995203002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191704 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/modules/v8/custom/V8CustomSQLStatementErrorCallback.cpp
+++ third_party/WebKit/Source/bindings/modules/v8/custom/V8CustomSQLStatementErrorCallback.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 
 #include "bindings/core/v8/ScriptController.h"
+#include "bindings/core/v8/V8Binding.h"
 #include "bindings/modules/v8/V8SQLError.h"
 #include "bindings/modules/v8/V8SQLStatementErrorCallback.h"
 #include "bindings/modules/v8/V8SQLTransaction.h"
@@ -77,7 +78,11 @@ bool V8SQLStatementErrorCallback::handleEvent(SQLTransaction* transaction, SQLEr
     // statement, if any, or onto the next overall step otherwise. Otherwise,
     // the error callback did not return false, or there was no error callback.
     // Jump to the last step in the overall steps.
-    return exceptionCatcher.HasCaught() || (!result.IsEmpty() && result->BooleanValue());
+    if (exceptionCatcher.HasCaught())
+        return true;
+    bool value;
+    V8_CALL(value, result, BooleanValue(isolate->GetCurrentContext()), return true);
+    return value;
 }
 
 } // namespace blink
