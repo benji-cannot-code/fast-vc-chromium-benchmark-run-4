202da700445f0759aa1ca6776ec525824931438c
mukai <mukai@chromium.org>
2015-03-11 13:30:05 -0700

    Cache gfx::RenderText instances in views::Label.
    This is a rebase of crrev.com/413823002 by msw@.
    
    Cache RenderText instances, avoid Canvas::DrawString*.
    (avoids repeating itemization, layout, etc. on each paint)
    Recalculate colors and reset the layout as needed.
    Large cleanup; remove obsolete flag tests; update tests.
    
    Update App List's CachedLabel views::Label subclass use.
    (SchedulePaintInRect triggered paint with color changes)
    Ensure SetTitleSubpixelAA is otherwise called as needed.
    (skip early return; which breaks folder reorganization)
    
    This will increase the performance of painting
    significantly. See https://docs.google.com/document/d/1q4RrBjNO52l1pNTkIZPhQ60aPfBZnV4Af0vdQNr2Juc/edit#
    for the detailed analysis.
    
    BUG=240037, 125348, 450791
    TEST=no appearance changes with performance improvement as:
      On daisy with 15 bookmarks, repainting of bookmark is
      decreased as 135msec -> 2.8msec
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/867003002
    
    Cr-Commit-Position: refs/heads/master@{#320138}

--- ui/app_list/views/app_list_item_view.cc
+++ ui/app_list/views/app_list_item_view.cc
@@ -188,6 +188,7 @@ void AppListItemView::SetUIState(UIState state) {
   }
 #endif  // !OS_WIN
 
+  SetTitleSubpixelAA();
   SchedulePaint();
 }
 
@@ -212,10 +213,7 @@ void AppListItemView::SetTitleSubpixelAA() {
                    !is_highlighted_ && !apps_grid_view_->IsSelectedView(this) &&
                    !apps_grid_view_->IsAnimatingView(this);
 
-  bool currently_enabled = title_->background() != NULL;
-  if (currently_enabled == enable_aa)
-    return;
-
+  title_->SetSubpixelRenderingEnabled(enable_aa);
   if (enable_aa) {
     title_->SetBackgroundColor(app_list::kLabelBackgroundColor);
     title_->set_background(views::Background::CreateSolidBackground(
@@ -277,6 +275,7 @@ void AppListItemView::SetItemName(const base::string16& display_name,
 
 void AppListItemView::SetItemIsHighlighted(bool is_highlighted) {
   is_highlighted_ = is_highlighted;
+  SetTitleSubpixelAA();
   SchedulePaint();
 }
 
@@ -286,6 +285,7 @@ void AppListItemView::SetItemIsInstalling(bool is_installing) {
     title_->SetVisible(!is_installing);
     progress_bar_->SetVisible(is_installing);
   }
+  SetTitleSubpixelAA();
   SchedulePaint();
 }
 
@@ -332,17 +332,13 @@ void AppListItemView::Layout() {
                          title_size.height());
   title_bounds.Intersect(rect);
   title_->SetBoundsRect(title_bounds);
+  SetTitleSubpixelAA();
 
   gfx::Rect progress_bar_bounds(progress_bar_->GetPreferredSize());
   progress_bar_bounds.set_x(GetContentsBounds().x() +
                             kProgressBarHorizontalPadding);
   progress_bar_bounds.set_y(title_bounds.y());
   progress_bar_->SetBoundsRect(progress_bar_bounds);
-}
-
-void AppListItemView::SchedulePaintInRect(const gfx::Rect& r) {
-  SetTitleSubpixelAA();
-  views::CustomButton::SchedulePaintInRect(r);
 }
 
 void AppListItemView::OnPaint(gfx::Canvas* canvas) {
@@ -406,7 +402,7 @@ void AppListItemView::StateChanged() {
       item_weak_->set_highlighted(false);
     title_->SetEnabledColor(kGridTitleColor);
   }
-  title_->Invalidate();
+  SetTitleSubpixelAA();
 }
 
 bool AppListItemView::ShouldEnterPushedState(const ui::Event& event) {
