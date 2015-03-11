bb12af7177e0c86a801c5fdc071566278220a6aa
dyen <dyen@chromium.org>
2015-03-10 22:54:38 -0700

    Defer deletion of shaders when shaders are in use instead of compiling.
    R=zmo@chromium.org
    BUG=465921
    TEST=Trybots
    
    Review URL: https://codereview.chromium.org/971983002
    
    Cr-Commit-Position: refs/heads/master@{#320051}

--- gpu/command_buffer/service/shader_manager.h
+++ gpu/command_buffer/service/shader_manager.h
@@ -46,7 +46,7 @@ class GPU_EXPORT Shader : public base::RefCounted<Shader> {
   }
 
   GLuint service_id() const {
-    return service_id_;
+    return marked_for_deletion_ ? 0 : service_id_;
   }
 
   GLenum shader_type() const {
@@ -98,7 +98,7 @@ class GPU_EXPORT Shader : public base::RefCounted<Shader> {
   }
 
   bool IsDeleted() const {
-    return service_id_ == 0;
+    return marked_for_deletion_;
   }
 
   bool InUse() const {
@@ -146,14 +146,22 @@ class GPU_EXPORT Shader : public base::RefCounted<Shader> {
   Shader(GLuint service_id, GLenum shader_type);
   ~Shader();
 
+  // Must be called only if we currently own the context. Forces the deletion
+  // of the underlying shader service id.
+  void Destroy();
+
   void IncUseCount();
   void DecUseCount();
-  void Delete();
+  void MarkForDeletion();
+  void DeleteServiceID();
 
   int use_count_;
 
   // The current state of the shader.
   ShaderState shader_state_;
+
+  // The shader has been marked for deletion.
+  bool marked_for_deletion_;
 
   // The shader this Shader is tracking.
   GLuint service_id_;
