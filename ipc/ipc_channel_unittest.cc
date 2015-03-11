a507aed01af575e83aa837781cf04c0c77b21a94
thestig <thestig@chromium.org>
2015-03-11 13:41:55 -0700

    Cleanup: Remove wstring methods from base::Pickle.
    BUG=23581
    
    Review URL: https://codereview.chromium.org/940783002
    
    Cr-Commit-Position: refs/heads/master@{#320140}

--- ipc/ipc_channel_unittest.cc
+++ ipc/ipc_channel_unittest.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/message_loop/message_loop.h"
 #include "base/pickle.h"
+#include "base/strings/string16.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread.h"
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_test_base.h"
@@ -27,18 +29,18 @@ class IPCChannelTest : public IPCTestBase {
 TEST_F(IPCChannelTest, BasicMessageTest) {
   int v1 = 10;
   std::string v2("foobar");
-  std::wstring v3(L"hello world");
+  base::string16 v3(base::ASCIIToUTF16("hello world"));
 
   IPC::Message m(0, 1, IPC::Message::PRIORITY_NORMAL);
   EXPECT_TRUE(m.WriteInt(v1));
   EXPECT_TRUE(m.WriteString(v2));
-  EXPECT_TRUE(m.WriteWString(v3));
+  EXPECT_TRUE(m.WriteString16(v3));
 
   PickleIterator iter(m);
 
   int vi;
   std::string vs;
-  std::wstring vw;
+  base::string16 vs16;
 
   EXPECT_TRUE(iter.ReadInt(&vi));
   EXPECT_EQ(v1, vi);
@@ -46,13 +48,13 @@ TEST_F(IPCChannelTest, BasicMessageTest) {
   EXPECT_TRUE(iter.ReadString(&vs));
   EXPECT_EQ(v2, vs);
 
-  EXPECT_TRUE(iter.ReadWString(&vw));
-  EXPECT_EQ(v3, vw);
+  EXPECT_TRUE(iter.ReadString16(&vs16));
+  EXPECT_EQ(v3, vs16);
 
   // should fail
   EXPECT_FALSE(iter.ReadInt(&vi));
   EXPECT_FALSE(iter.ReadString(&vs));
-  EXPECT_FALSE(iter.ReadWString(&vw));
+  EXPECT_FALSE(iter.ReadString16(&vs16));
 }
 
 TEST_F(IPCChannelTest, ChannelTest) {
