874ec53a7d58f1a79628273d82e04251b6bdc5c0
brettw <brettw@chromium.org>
2015-03-12 16:20:45 -0700

    Remove check on PersonalDataManager
    The AutofillWalletDataTypeController previously checked for the existance of the autofill component's PersonalDataManagaer.
    
    But the PersonalDataManager is unused. On the backend autofill database is used, and this is checked separately. This patch removes the checks for the PersonalDataManager.
    
    Review URL: https://codereview.chromium.org/1005663003
    
    Cr-Commit-Position: refs/heads/master@{#320406}

--- chrome/browser/sync/glue/autofill_wallet_data_type_controller.h
+++ chrome/browser/sync/glue/autofill_wallet_data_type_controller.h
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Profile;
 class ProfileSyncComponentsFactory;
 
-namespace autofill {
-class PersonalDataManager;
-}
-
 namespace browser_sync {
 
 class AutofillWalletDataTypeController
@@ -42,7 +38,6 @@ class AutofillWalletDataTypeController
   void WebDatabaseLoaded();
 
   Profile* const profile_;
-  autofill::PersonalDataManager* personal_data_;
   bool callback_registered_;
 
   DISALLOW_COPY_AND_ASSIGN(AutofillWalletDataTypeController);
