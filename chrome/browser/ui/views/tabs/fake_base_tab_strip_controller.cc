1ce7b5afd15a67d6b5d42d5c587b7c901bb255a7
tdanderson <tdanderson@chromium.org>
2015-03-12 12:40:48 -0700

    Hide close buttons of inactive stacked tabs by default
    Hide the close buttons of inactive tabs when the tabstrip
    is in stacked mode (i.e., when touch is used to interact
    with the tabstrip and the width of the tabs falls below
    our minimum width threshold for touch). Introduce the flag
    --disable-hide-inactive-stacked-tab-close-buttons
    in order to disable this behaviour if needed.
    
    BUG=455312
    TEST=TabStripTest.TabCloseButtonVisibilityWhenStacked
    
    Review URL: https://codereview.chromium.org/983853002
    
    Cr-Commit-Position: refs/heads/master@{#320346}

--- chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.cc
@@ -66,6 +66,9 @@ bool FakeBaseTabStripController::IsNewTabPage(int index) const {
 }
 
 void FakeBaseTabStripController::SelectTab(int index) {
+  if (!IsValidIndex(index))
+    return;
+  active_index_ = index;
 }
 
 void FakeBaseTabStripController::ExtendSelectionTo(int index) {
