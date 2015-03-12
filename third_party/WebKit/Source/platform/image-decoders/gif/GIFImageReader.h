23a5702fdc083571e2a9ffac0af36117118a6335
hclam@chromium.org <hclam@chromium.org>
2015-03-12 20:41:41 +0000

    Optimize buffer reading for GIF image decoder
    GIF image decoder used to call SharedBuffer::data() to consolidate
    mulitple buffer segments into one consecutive buffer. This had a
    performance consequence for large image file that inflicts a lot of
    memory copy operations.
    
    Decoder should peek into the buffer segment without copying if possible.
    This change introduces a FastSharedBufferReader to avoid copying as
    much as possible.
    
    This was shown to speed up loading of large GIF image files.
    
    BUG=462733
    
    Review URL: https://codereview.chromium.org/976153002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191796 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/image-decoders/gif/GIFImageReader.h
+++ third_party/WebKit/Source/platform/image-decoders/gif/GIFImageReader.h
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Define ourselves as the clientPtr.  Mozilla just hacked their C++ callback class into this old C decoder,
 // so we will too.
 #include "platform/SharedBuffer.h"
+#include "platform/image-decoders/FastSharedBufferReader.h"
 #include "platform/image-decoders/gif/GIFImageDecoder.h"
 #include "wtf/Noncopyable.h"
 #include "wtf/OwnPtr.h"
@@ -168,7 +169,7 @@ public:
     bool isDefined() const { return m_isDefined; }
 
     // Build RGBA table using the data stream.
-    void buildTable(const unsigned char* data, size_t length);
+    void buildTable(blink::FastSharedBufferReader*);
     const Table& table() const { return m_table; }
 
 private:
@@ -210,7 +211,7 @@ public:
         m_lzwBlocks.append(GIFLZWBlock(position, size));
     }
 
-    bool decode(const unsigned char* data, size_t length, blink::GIFImageDecoder* client, bool* frameDecoded);
+    bool decode(blink::FastSharedBufferReader*, blink::GIFImageDecoder* client, bool* frameDecoded);
 
     int frameId() const { return m_frameId; }
     void setRect(unsigned x, unsigned y, unsigned width, unsigned height)
@@ -330,11 +331,6 @@ public:
 private:
     bool parseData(size_t dataPosition, size_t len, blink::GIFImageDecoder::GIFParseQuery);
     void setRemainingBytes(size_t);
-
-    const unsigned char* data(size_t dataPosition) const
-    {
-        return reinterpret_cast<const unsigned char*>(m_data->data()) + dataPosition;
-    }
 
     void addFrameIfNecessary();
     bool currentFrameIsFirstFrame() const
