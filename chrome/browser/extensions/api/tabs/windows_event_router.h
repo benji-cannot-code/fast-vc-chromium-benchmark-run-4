0816438142fc5ec5bed8ac467c54001dac6b1d92
tapted <tapted@chromium.org>
2015-03-12 15:51:58 -0700

    Remove the `focused_before` argument to WidgetFocusChangeListener
    Nothing uses it, and it results in dubious sequences of repeated events
    like
    
    FocusChanged(old=null, new=X)
    FocusChanged(old=X, new=Y)
    FocusChanged(old=X, new=Y)
    
    Instead, this becomes
    
    FocusChanged(new=X)
    FocusChanged(null)  // X losing focus.
    FocusChanged(new=Y)
    
    More tangibly, on Mac, it's not straightforward to pass an argument for
    `old`, since it requires knowing about views in other NSWindows. Also
    switching focus is asynchronous, so we could save "old" when losing
    focus (as a raw/weak/possibly-destroyed pointer), but it will get lost
    if the "new" isn't a Chrome window. This means that, e.g., checks in
    WidgetTestInteractive.WindowModalWindowDestroyedActivationTest would
    require special treatment on Mac. (Instead, just try to make all
    platforms consistent).
    
    This is a step towards resolving some long-standing bugs in the
    chrome.windows extension API for listening to focus changes, and
    ensuring the behaviour is consistent across platforms. It does this by
    returning to behavior documented at
    https://developer.chrome.com/extensions/windows#event-onFocusChanged
    that was once Linux-only. That is, switching between Chrome windows
    would indicate focus lost, followed by focus gained.
    
    In doing so, resolve a bug where focusing a non-browser Chrome window
    wouldn't trigger onFocusChanged via the extensions window API. This is
    because the behavior before this CL would never clear it:
    WindowsEventRouter only triggered WINDOW_ID_NONE when no aura widget
    gained focus, but only triggers non-null onFocusChanged events in
    response to BrowserList::SetLastActive(Browser* browser).
    
    Now de-focusing a browser will always clear focus (i.e. onFocusChanged
    will trigger with WINDOW_ID_NONE). Then when a browser is focused
    (either immediately after the defocus, or after focusing a non-browser),
    an onFocusChanged for the same browser will follow.
    
    BUG=403679, 391471, 466665
    
    Review URL: https://codereview.chromium.org/994333003
    
    Cr-Commit-Position: refs/heads/master@{#320399}

--- chrome/browser/extensions/api/tabs/windows_event_router.h
+++ chrome/browser/extensions/api/tabs/windows_event_router.h
@@ -44,8 +44,7 @@ class WindowsEventRouter : public WindowControllerListObserver,
   void OnWindowControllerRemoved(WindowController* window) override;
 
 #if !defined(OS_MACOSX)
-  void OnNativeFocusChange(gfx::NativeView focused_before,
-                           gfx::NativeView focused_now) override;
+  void OnNativeFocusChanged(gfx::NativeView focused_now) override;
 #endif
 
   // content::NotificationObserver.
