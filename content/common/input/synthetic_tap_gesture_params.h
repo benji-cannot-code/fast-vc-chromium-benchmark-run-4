848e10667373e50612e11baeda06080c46cb1565
ssid <ssid@chromium.org>
2015-03-12 12:46:26 -0700

    Making synthetic gesture API accept float inputs.
    The points on the screen actually float values. So fixing synthetic
    gestures to accept float values.
    
    BUG=461825
    
    Review URL: https://codereview.chromium.org/990903006
    
    Cr-Commit-Position: refs/heads/master@{#320349}

--- content/common/input/synthetic_tap_gesture_params.h
+++ content/common/input/synthetic_tap_gesture_params.h
@@ -21,8 +21,8 @@ struct CONTENT_EXPORT SyntheticTapGestureParams
 
   GestureType GetGestureType() const override;
 
-  gfx::Point position;
-  int duration_ms;
+  gfx::PointF position;
+  float duration_ms;
 
   static const SyntheticTapGestureParams* Cast(
       const SyntheticGestureParams* gesture_params);
