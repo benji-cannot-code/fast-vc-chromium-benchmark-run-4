a8d7869fb834e50a58b3cfbdf5a01bd803fd452d
rfevang <rfevang@chromium.org>
2015-03-12 14:08:19 -0700

    Sort page actions before comparing them to the list of page action views
    This fixes an issue where PageActionDiffer would report differences on
    every call if the external bookmark manager was installed and the user
    had any other extension with a page action and an id preceding that of
    the bookmark manager. This would cause the page action UI elements to be
    constantly destroyed and rebuilt.
    
    BUG=465595,465449
    
    Review URL: https://codereview.chromium.org/993063003
    
    Cr-Commit-Position: refs/heads/master@{#320371}

--- chrome/browser/ui/cocoa/location_bar/location_bar_view_mac.mm
+++ chrome/browser/ui/cocoa/location_bar/location_bar_view_mac.mm
@@ -65,7 +65,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/common/extension.h"
-#include "extensions/common/permissions/permissions_data.h"
 #include "grit/components_scaled_resources.h"
 #include "grit/theme_resources.h"
 #include "net/base/net_util.h"
@@ -81,13 +80,6 @@ namespace {
 // Vertical space between the bottom edge of the location_bar and the first run
 // bubble arrow point.
 const static int kFirstRunBubbleYOffset = 1;
-
-// Functor for moving BookmarkManagerPrivate page actions to the right via
-// stable_partition.
-bool PageActionHasBookmarkManagerPrivate(PageActionDecoration* decoration) {
-  return decoration->GetExtension()->permissions_data()->HasAPIPermission(
-      extensions::APIPermission::kBookmarkManagerPrivate);
-}
 
 }  // namespace
 
@@ -648,11 +640,6 @@ void LocationBarViewMac::RefreshPageActionDecorations() {
       page_action_decorations_.push_back(
           new PageActionDecoration(this, browser_, new_page_actions[i]));
     }
-
-    // Move rightmost extensions to the start.
-    std::stable_partition(page_action_decorations_.begin(),
-                          page_action_decorations_.end(),
-                          PageActionHasBookmarkManagerPrivate);
   }
 
   GURL url = GetToolbarModel()->GetURL();
--- chrome/browser/ui/views/location_bar/location_bar_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -68,7 +68,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/feature_switch.h"
-#include "extensions/common/permissions/permissions_data.h"
 #include "grit/components_scaled_resources.h"
 #include "grit/theme_resources.h"
 #include "ui/accessibility/ax_view_state.h"
@@ -106,28 +105,6 @@ int GetEditLeadingInternalSpace() {
   // The textfield has 1 px of whitespace before the text in the RTL case only.
   return base::i18n::IsRTL() ? 1 : 0;
 }
-
-// Functor for moving BookmarkManagerPrivate page actions to the right via
-// stable_partition.
-class IsPageActionViewRightAligned {
- public:
-  explicit IsPageActionViewRightAligned(
-      extensions::ExtensionRegistry* extension_registry)
-      : extension_registry_(extension_registry) {}
-
-  bool operator()(PageActionWithBadgeView* page_action_view) {
-    return extension_registry_->enabled_extensions().GetByID(
-        page_action_view->image_view()->extension_action()->extension_id())->
-        permissions_data()->
-        HasAPIPermission(extensions::APIPermission::kBookmarkManagerPrivate);
-  }
-
- private:
-  extensions::ExtensionRegistry* extension_registry_;
-
-  // NOTE: Can't DISALLOW_COPY_AND_ASSIGN as we pass this object by value to
-  // std::stable_partition().
-};
 
 }  // namespace
 
@@ -911,13 +888,6 @@ bool LocationBarView::RefreshPageActionViews() {
       page_action_view->SetVisible(false);
       page_action_views_.push_back(page_action_view);
     }
-
-    // Move rightmost extensions to the start.
-    std::stable_partition(
-        page_action_views_.begin(),
-        page_action_views_.end(),
-        IsPageActionViewRightAligned(
-            extensions::ExtensionRegistry::Get(profile())));
 
     View* right_anchor = open_pdf_in_reader_view_;
     if (!right_anchor)
