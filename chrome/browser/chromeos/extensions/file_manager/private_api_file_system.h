70d398830d707a645b578717be99f93a0cb84d3f
mtomasz <mtomasz@chromium.org>
2015-03-12 09:26:47 -0700

    Grant permissions to requested volumes only in Files app.
    Previously, access to all resources on the external file system backend was
    being granted when only one volume was requested. This CL enforces the security
    model by granting permissions only to what's actually requested.
    
    TEST=Tested manually. Also, current browser tests for opening files pass.
    BUG=323422
    
    Review URL: https://codereview.chromium.org/993903002
    
    Cr-Commit-Position: refs/heads/master@{#320298}

--- chrome/browser/chromeos/extensions/file_manager/private_api_file_system.h
+++ chrome/browser/chromeos/extensions/file_manager/private_api_file_system.h
@@ -72,7 +72,9 @@ class FileManagerPrivateRequestFileSystemFunction
       scoped_refptr<storage::FileSystemContext> file_system_context,
       int child_id,
       Profile* profile,
-      scoped_refptr<const extensions::Extension> extension);
+      scoped_refptr<const extensions::Extension> extension,
+      const base::FilePath& mount_path,
+      const base::FilePath& virtual_path);
 
   // Called when the entry definition is computed.
   void OnEntryDefinition(
@@ -83,6 +85,8 @@ class FileManagerPrivateRequestFileSystemFunction
 // from other profiles.
 class FileManagerPrivateGrantAccessFunction : public UIThreadExtensionFunction {
  public:
+  FileManagerPrivateGrantAccessFunction();
+
   DECLARE_EXTENSION_FUNCTION("fileManagerPrivate.grantAccess",
                              FILEMANAGERPRIVATE_GRANTACCESS)
 
@@ -91,6 +95,8 @@ class FileManagerPrivateGrantAccessFunction : public UIThreadExtensionFunction {
 
  private:
   ExtensionFunction::ResponseAction Run() override;
+  const ChromeExtensionFunctionDetails chrome_details_;
+  DISALLOW_COPY_AND_ASSIGN(FileManagerPrivateGrantAccessFunction);
 };
 
 // Base class for FileManagerPrivateAddFileWatchFunction and
