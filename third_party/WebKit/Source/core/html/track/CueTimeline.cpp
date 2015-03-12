58f98ebbd6e4993b7ddba0a2e7cc53a527373ccd
fs@opera.com <fs@opera.com>
2015-03-12 03:35:13 +0000

    Hide call to removeDisplayTree in CueTimeline
    TextTrack::setMode explicitly cleans out the display trees of its cues.
    Move this into a new method - hideCues() - on CueTimeline and call that
    instead. This keeps the manipulation of the display representation in a
    single place.
    
    BUG=321654
    
    Review URL: https://codereview.chromium.org/994353003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191748 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/track/CueTimeline.cpp
+++ third_party/WebKit/Source/core/html/track/CueTimeline.cpp
@@ -84,6 +84,12 @@ void CueTimeline::removeCueInternal(PassRefPtrWillBeRawPtr<TextTrackCue> cue)
     }
 }
 
+void CueTimeline::hideCues(TextTrack*, const TextTrackCueList* cues)
+{
+    for (size_t i = 0; i < cues->length(); ++i)
+        cues->item(i)->removeDisplayTree();
+}
+
 static bool trackIndexCompare(TextTrack* a, TextTrack* b)
 {
     return a->trackIndex() - b->trackIndex() < 0;
