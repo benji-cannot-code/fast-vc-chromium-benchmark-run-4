74ce749747f19007c90f5355ae651f56b90eb45a
kbr <kbr@chromium.org>
2015-03-12 12:19:41 -0700

    Suppress flaky gpu_rasterization on Mac Retina GPU tryservers.
    BUG=466690
    
    Review URL: https://codereview.chromium.org/1007453002
    
    Cr-Commit-Position: refs/heads/master@{#320340}

---
+++ content/test/gpu/gpu_tests/gpu_rasterization_expectations.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from gpu_test_expectations import GpuTestExpectations
+
+# See the GpuTestExpectations class for documentation.
+
+class GpuRasterizationExpectations(GpuTestExpectations):
+  def SetExpectations(self):
+    # Sample Usage:
+    # self.Fail('GpuRasterization.BlueBox',
+    #     ['mac', 'amd', ('nvidia', 0x1234)], bug=123)
+
+    # Flaky on Mac Retina tryservers.
+    self.Fail('GpuRasterization.BlueBox',
+              ['mac', ('nvidia', 0x0fe9)], bug=466690)
