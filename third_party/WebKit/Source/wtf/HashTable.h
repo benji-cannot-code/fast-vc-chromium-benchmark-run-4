a4935df078a8f13819bab5bbecea66220ef361e2
mikhail.pozdnyakov@intel.com <mikhail.pozdnyakov@intel.com>
2015-03-12 02:02:58 +0000

    The class HashTable contains mixed unsigned and bool bitfields
    The class HashTable contains mixed unsigned and bool bitfields, which will pack into separate words on the MSVC compiler
    
    Review URL: https://codereview.chromium.org/978983004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191746 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/HashTable.h
+++ third_party/WebKit/Source/wtf/HashTable.h
@@ -557,7 +557,7 @@ namespace WTF {
         unsigned m_tableSize;
         unsigned m_keyCount;
         unsigned m_deletedCount:31;
-        bool m_queueFlag:1;
+        unsigned m_queueFlag:1;
 #if ENABLE(ASSERT)
         unsigned m_modifications;
 #endif
