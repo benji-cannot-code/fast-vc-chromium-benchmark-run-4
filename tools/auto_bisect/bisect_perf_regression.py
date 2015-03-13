919c6dccb2353536d4f8915781808eae14dfe387
simonhatch <simonhatch@chromium.org>
2015-03-13 12:18:24 -0700

    Make failed telemetry test on perf try job red.
    The bisect RunPerformanceTestAndParseResults function allows flaky runs (since either it's bisecting return codes, or it's only concerned about gathering the metrics). When we run perf tests though, we probably don't want to allow flakes to occur. This patch adds an "allow_flakes" parameter to optionally return a failure code if any run fails.
    
    Without patch: http://build.chromium.org/p/tryserver.chromium.perf/builders/android_nexus7_perf_bisect/builds/1109
    With patch: http://build.chromium.org/p/tryserver.chromium.perf/builders/android_nexus7_perf_bisect/builds/1117
    
    BUG=467058
    
    Review URL: https://codereview.chromium.org/1004793002
    
    Cr-Commit-Position: refs/heads/master@{#320544}

--- tools/auto_bisect/bisect_perf_regression.py
+++ tools/auto_bisect/bisect_perf_regression.py
@@ -1260,7 +1260,8 @@ class BisectPerformanceMetrics(object):
 
   def RunPerformanceTestAndParseResults(
       self, command_to_run, metric, reset_on_first_run=False,
-      upload_on_last_run=False, results_label=None, test_run_multiplier=1):
+      upload_on_last_run=False, results_label=None, test_run_multiplier=1,
+      allow_flakes=True):
     """Runs a performance test on the current revision and parses the results.
 
     Args:
@@ -1275,6 +1276,7 @@ class BisectPerformanceMetrics(object):
           are all ignored if the test is not a Telemetry test.
       test_run_multiplier: Factor by which to multiply the number of test runs
           and the timeout period specified in self.opts.
+      allow_flakes: Report success even if some tests fail to run.
 
     Returns:
       (values dict, 0) if --debug_ignore_perf_test was passed.
@@ -1317,6 +1319,7 @@ class BisectPerformanceMetrics(object):
     metric_values = []
     output_of_all_runs = ''
     repeat_count = self.opts.repeat_test_count * test_run_multiplier
+    return_codes = []
     for i in xrange(repeat_count):
       # Can ignore the return code since if the tests fail, it won't return 0.
       current_args = copy.copy(args)
@@ -1330,6 +1333,7 @@ class BisectPerformanceMetrics(object):
       try:
         output, return_code = bisect_utils.RunProcessAndRetrieveOutput(
             current_args, cwd=self.src_cwd)
+        return_codes.append(return_code)
       except OSError, e:
         if e.errno == errno.ENOENT:
           err_text = ('Something went wrong running the performance test. '
@@ -1407,7 +1411,15 @@ class BisectPerformanceMetrics(object):
       print 'Results of performance test: %12f %12f' % (
           truncated_mean, standard_err)
       print
-    return (values, success_code, output_of_all_runs)
+
+    overall_success = success_code
+    if not allow_flakes and not self._IsBisectModeReturnCode():
+      overall_success = (
+          success_code
+          if (all(current_value == 0 for current_value in return_codes))
+              else failure_code)
+
+    return (values, overall_success, output_of_all_runs)
 
   def PerformPreBuildCleanup(self):
     """Performs cleanup between runs."""
