93b0a00ed14dbf9d369ddd86eb92ce8288e9b308
loyso <loyso@chromium.org>
2015-03-12 19:31:43 -0700

    Animations: Extract LayerTreeHostImpl::ScrollAnimation* methods
    This is a prerequisite to implement compositor animation timelines:
    https://codereview.chromium.org/947033002/
    
    BUG=394777
    R=aelias@chromium.org
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/997823002
    
    Cr-Commit-Position: refs/heads/master@{#320437}

--- cc/trees/layer_tree_host_impl.h
+++ cc/trees/layer_tree_host_impl.h
@@ -40,6 +40,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/geometry/rect.h"
 
+namespace gfx {
+class ScrollOffset;
+}
+
 namespace cc {
 
 class CompletionEvent;
@@ -593,6 +597,12 @@ class CC_EXPORT LayerTreeHostImpl
 
   void NotifySwapPromiseMonitorsOfSetNeedsRedraw();
   void NotifySwapPromiseMonitorsOfForwardingToMainThread();
+
+  void ScrollAnimationCreate(LayerImpl* layer_impl,
+                             const gfx::ScrollOffset& target_offset,
+                             const gfx::ScrollOffset& current_offset);
+  bool ScrollAnimationUpdateTarget(LayerImpl* layer_impl,
+                                   const gfx::Vector2dF& scroll_delta);
 
   typedef base::hash_map<UIResourceId, UIResourceData>
       UIResourceMap;
