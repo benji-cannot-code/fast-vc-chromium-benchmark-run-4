542eb0748c78fbe122ce3ce1e288d2aa0fcf5f99
hirono <hirono@chromium.org>
2015-03-12 22:25:54 -0700

    Files.app: Use content thumbanil if the drive file is present.
    Drive thumbnail can be out of sync even if drive sync completed.
    
    BUG=466037
    TEST=manually
    
    Review URL: https://codereview.chromium.org/997393002
    
    Cr-Commit-Position: refs/heads/master@{#320453}

--- ui/file_manager/file_manager/foreground/js/metadata/thumbnail_model_unittest.js
+++ ui/file_manager/file_manager/foreground/js/metadata/thumbnail_model_unittest.js
@@ -21,7 +21,6 @@ function setUp() {
   metadata = new MetadataItem();
   metadata.modificationTime = new Date(2015, 0, 1);
   metadata.present = true;
-  metadata.dirty = false;
   metadata.thumbnailUrl = 'EXTERNAL_THUMBNAIL_URL';
   metadata.customIconUrl = 'CUSTOM_ICON_URL';
   metadata.contentThumbnailUrl = 'CONTENT_THUMBNAIL_URL';
@@ -47,7 +46,7 @@ function testThumbnailModelGetBasic(callback) {
         results[0].filesystem.modificationTime.toString());
     assertEquals('EXTERNAL_THUMBNAIL_URL', results[0].external.thumbnailUrl);
     assertEquals('CUSTOM_ICON_URL', results[0].external.customIconUrl);
-    assertFalse(results[0].external.dirty);
+    assertTrue(results[0].external.present);
     assertEquals('CONTENT_THUMBNAIL_URL', results[0].thumbnail.url);
     assertEquals('CONTENT_THUMBNAIL_TRANSFORM', results[0].thumbnail.transform);
     assertEquals('CONTENT_IMAGE_TRANSFORM', results[0].media.imageTransform);
@@ -63,7 +62,7 @@ function testThumbnailModelGetNotPresent(callback) {
         results[0].filesystem.modificationTime.toString());
     assertEquals('EXTERNAL_THUMBNAIL_URL', results[0].external.thumbnailUrl);
     assertEquals('CUSTOM_ICON_URL', results[0].external.customIconUrl);
-    assertFalse(results[0].external.dirty);
+    assertFalse(results[0].external.present);
     assertEquals(undefined, results[0].thumbnail.url);
     assertEquals(undefined, results[0].thumbnail.transform);
     assertEquals(undefined, results[0].media.imageTransform);
@@ -78,7 +77,7 @@ function testThumbnailModelGetNonImage(callback) {
         results[0].filesystem.modificationTime.toString());
     assertEquals('EXTERNAL_THUMBNAIL_URL', results[0].external.thumbnailUrl);
     assertEquals('CUSTOM_ICON_URL', results[0].external.customIconUrl);
-    assertFalse(results[0].external.dirty);
+    assertTrue(results[0].external.present);
     assertEquals(undefined, results[0].thumbnail.url);
     assertEquals(undefined, results[0].thumbnail.transform);
     assertEquals(undefined, results[0].media.imageTransform);
