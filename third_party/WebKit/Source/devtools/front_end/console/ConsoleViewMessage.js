77d53d644ccff34a3c4a414df784dc6aabc3e6f5
sergeyv@chromium.org <sergeyv@chromium.org>
2015-03-13 11:16:51 +0000

    Devtools: extract css which styles object to separate file
    BUG=
    
    Review URL: https://codereview.chromium.org/1003733003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191831 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/console/ConsoleViewMessage.js
+++ third_party/WebKit/Source/devtools/front_end/console/ConsoleViewMessage.js
@@ -141,6 +141,7 @@ WebInspector.ConsoleViewMessage.prototype = {
     _formatMessage: function()
     {
         this._formattedMessage = createElement("span");
+        this._formattedMessage.appendChild(WebInspector.View.createStyleElement("components/objectValue.css"));
         this._formattedMessage.className = "console-message-text source-code";
 
         /**
@@ -400,7 +401,7 @@ WebInspector.ConsoleViewMessage.prototype = {
         var type = forceObjectFormat ? "object" : (output.subtype || output.type);
         var formatter = this._customFormatters[type] || this._formatParameterAsValue;
         var span = createElement("span");
-        span.className = "console-formatted-" + type + " source-code";
+        span.className = "object-value-" + type + " source-code";
         formatter.call(this, output, span, includePreview);
         return span;
     },
@@ -593,7 +594,7 @@ WebInspector.ConsoleViewMessage.prototype = {
      */
     _renderPropertyPreview: function(type, subtype, description)
     {
-        var span = createElementWithClass("span", "console-formatted-" + (subtype || type));
+        var span = createElementWithClass("span", "object-value-" + (subtype || type));
         description = description || "";
 
         if (type === "function") {
@@ -602,7 +603,7 @@ WebInspector.ConsoleViewMessage.prototype = {
         }
 
         if (type === "object" && subtype === "node" && description) {
-            span.classList.add("console-formatted-preview-node");
+            span.classList.add("object-value-preview-node");
             WebInspector.DOMPresentationUtils.createSpansForNodeTitle(span, description);
             return span;
         }
@@ -734,11 +735,11 @@ WebInspector.ConsoleViewMessage.prototype = {
     _formatParameterAsString: function(output, elem)
     {
         var span = createElement("span");
-        span.className = "console-formatted-string source-code";
+        span.className = "object-value-string source-code";
         span.appendChild(WebInspector.linkifyStringAsFragment(output.description || ""));
 
         // Make black quotes.
-        elem.classList.remove("console-formatted-string");
+        elem.classList.remove("object-value-string");
         elem.createTextChild("\"");
         elem.appendChild(span);
         elem.createTextChild("\"");
@@ -750,7 +751,7 @@ WebInspector.ConsoleViewMessage.prototype = {
      */
     _formatParameterAsError: function(output, elem)
     {
-        var span = elem.createChild("span", "console-formatted-error source-code");
+        var span = elem.createChild("span", "object-value-error source-code");
         span.appendChild(WebInspector.linkifyStringAsFragment(output.description || ""));
     },
 
@@ -785,7 +786,7 @@ WebInspector.ConsoleViewMessage.prototype = {
         {
             if (index - lastNonEmptyIndex <= 1)
                 return;
-            var span = elem.createChild("span", "console-formatted-undefined");
+            var span = elem.createChild("span", "object-value-undefined");
             span.textContent = WebInspector.UIString("undefined × %d", index - lastNonEmptyIndex - 1);
         }
 
