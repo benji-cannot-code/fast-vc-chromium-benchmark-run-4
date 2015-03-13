e0b3c0bd07de38960be3cba39f030b548a44ee8e
sigbjornf@opera.com <sigbjornf@opera.com>
2015-03-13 12:16:45 +0000

    Remove decl for undefined&unused MediaKeySession::enqueueEvent() method.
    R=haraken
    
    Review URL: https://codereview.chromium.org/1000423002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191838 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/encryptedmedia/MediaKeySession.h
+++ third_party/WebKit/Source/modules/encryptedmedia/MediaKeySession.h
@@ -82,8 +82,6 @@ public:
     ScriptPromise close(ScriptState*);
     ScriptPromise remove(ScriptState*);
 
-    void enqueueEvent(PassRefPtrWillBeRawPtr<Event>);
-
     // EventTarget
     virtual const AtomicString& interfaceName() const override;
     virtual ExecutionContext* executionContext() const override;
