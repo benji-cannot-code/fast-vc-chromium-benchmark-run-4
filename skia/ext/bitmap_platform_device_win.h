a74b5b007e290b4b02af2b8f4a9b8f83ec97024e
reed <reed@google.com>
2015-03-13 15:43:35 -0700

    add new signature so skia can rely on it (subclass of SkDevice)
    in support of landing this in skia
    https://codereview.chromium.org/1006923002
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1003253004
    
    Cr-Commit-Position: refs/heads/master@{#320597}

--- skia/ext/bitmap_platform_device_win.h
+++ skia/ext/bitmap_platform_device_win.h
@@ -66,6 +66,7 @@ class SK_API BitmapPlatformDevice : public SkBitmapDevice, public PlatformDevice
 
   virtual SkBaseDevice* onCreateCompatibleDevice(const CreateInfo& info)
     override;
+  virtual SkBaseDevice* onCreateDevice(const CreateInfo&, const SkPaint*);
 
  private:
   // Private constructor.
