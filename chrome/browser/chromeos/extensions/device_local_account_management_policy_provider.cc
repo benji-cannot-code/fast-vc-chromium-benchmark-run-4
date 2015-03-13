630eae5912005496e6122dbbdc07b77ad2f52a6c
hirono <hirono@chromium.org>
2015-03-12 20:29:03 -0700

    Demo: Add new extension IDs of demo application to the public session white list.
    They are demo applications introducing popular apps/services. The new demos use
    the same template with existing demo apps. Text/images are replaced for each new
    application.
    
    BUG=446429
    TEST=None
    
    Review URL: https://codereview.chromium.org/1001543005
    
    Cr-Commit-Position: refs/heads/master@{#320444}

--- chrome/browser/chromeos/extensions/device_local_account_management_policy_provider.cc
+++ chrome/browser/chromeos/extensions/device_local_account_management_policy_provider.cc
@@ -57,12 +57,14 @@ const char* const kPublicSessionWhitelist[] = {
     "ekgadegabdkcbkodfbgidncffijbghhl",  // Duolingo demo
     "iddohohhpmajlkbejjjcfednjnhlnenk",  // Evernote demo
     "bjdhhokmhgelphffoafoejjmlfblpdha",  // Gmail demo
+    "nldmakcnfaflagmohifhcihkfgcbmhph",  // Gmail offline demo
     "mdhnphfgagkpdhndljccoackjjhghlif",  // Google Drive demo
     "dondgdlndnpianbklfnehgdhkickdjck",  // Google Keep demo
     "amfoiggnkefambnaaphodjdmdooiinna",  // Google Play Movie and TV demo
     "fgjnkhlabjcaajddbaenilcmpcidahll",  // Google+ demo
     "ifpkhncdnjfipfjlhfidljjffdgklanh",  // Google+ Photos demo
     "cgmlfbhkckbedohgdepgbkflommbfkep",  // Hangouts.app demo
+    "ndlgnmfmgpdecjgehbcejboifbbmlkhp",  // Hash demo
     "edhhaiphkklkcfcbnlbpbiepchnkgkpn",  // Helper.extension demo
     "jckncghadoodfbbbmbpldacojkooophh",  // Journal demo
     "diehajhcjifpahdplfdkhiboknagmfii",  // Kindle demo
@@ -81,10 +83,12 @@ const char* const kPublicSessionWhitelist[] = {
     "aleodiobpjillgfjdkblghiiaegggmcm",  // Quickoffice demo
     "nifkmgcdokhkjghdlgflonppnefddien",  // Sheets demo
     "hdmobeajeoanbanmdlabnbnlopepchip",  // Slides demo
+    "ikmidginfdcbojdbmejkeakncgdbmonc",  // Soundtrap demo
     "dgohlccohkojjgkkfholmobjjoledflp",  // Spotify demo
     "dhmdaeekeihmajjnmichlhiffffdbpde",  // Store.app demo
     "onklhlmbpfnmgmelakhgehkfdmkpmekd",  // Todoist demo
     "jeabmjjifhfcejonjjhccaeigpnnjaak",  // TweetDeck demo
+    "gnckahkflocidcgjbeheneogeflpjien",  // Vine demo
     "pdckcbpciaaicoomipamcabpdadhofgh",  // Weatherbug demo
     "biliocemfcghhioihldfdmkkhnofcgmb",  // Webcam Toy demo
     "bhfoghflalnnjfcfkaelngenjgjjhapk",  // Wevideo demo
