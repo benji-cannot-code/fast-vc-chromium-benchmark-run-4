c78a772d3193df0bfb5245cbc97121a218c292be
bnc <bnc@chromium.org>
2015-03-12 18:57:09 -0700

    Add IsAlternativeServiceBroken(), remove is_broken.
    Add HttpServerProperties::IsAlternativeServiceBroken(), remove
    AlternateProtocolInfo.is_broken.  An alternative service can be broken by
    itself, not with respect to an origin.
    
    Patch Set 1 of this CL has been manually landed at https://crrev.com/995013004
    and then reverted at https://crrev.com/999893002.
    
    BUG=392576
    R=rch@chromium.org
    
    Committed: https://crrev.com/9825fc06585af7bf868804a13e9064975d4ec374
    Cr-Commit-Position: refs/heads/master@{#320189}
    
    Review URL: https://codereview.chromium.org/997953003
    
    Cr-Commit-Position: refs/heads/master@{#320428}

--- net/http/http_server_properties.cc
+++ net/http/http_server_properties.cc
@@ -102,10 +102,8 @@ AlternateProtocol AlternateProtocolFromNextProto(NextProto next_proto) {
 }
 
 std::string AlternateProtocolInfo::ToString() const {
-  return base::StringPrintf("%d:%s p=%f%s", port,
-                            AlternateProtocolToString(protocol),
-                            probability,
-                            is_broken ? " (broken)" : "");
+  return base::StringPrintf("%d:%s p=%f", port,
+                            AlternateProtocolToString(protocol), probability);
 }
 
 // static
