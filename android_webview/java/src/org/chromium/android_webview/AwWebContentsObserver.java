65a10b957995e20467d97138122296ad54ef09ae
tobiasjs <tobiasjs@chromium.org>
2015-03-13 07:10:18 -0700

    Reorder imports
    BUG=
    
    Review URL: https://codereview.chromium.org/1005083002
    
    Cr-Commit-Position: refs/heads/master@{#320494}

--- android_webview/java/src/org/chromium/android_webview/AwWebContentsObserver.java
+++ android_webview/java/src/org/chromium/android_webview/AwWebContentsObserver.java
@@ -5,13 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview;
 
-import java.lang.ref.WeakReference;
-
 import org.chromium.android_webview.AwContents.VisualStateCallback;
 import org.chromium.base.ThreadUtils;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.WebContentsObserver;
 import org.chromium.net.NetError;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Routes notifications from WebContents to AwContentsClient and other listeners.
