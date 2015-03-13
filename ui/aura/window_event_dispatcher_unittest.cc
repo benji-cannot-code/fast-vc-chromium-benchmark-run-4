03bec4a620a286b1cb3386d91a86e986a6f81c79
Sadrul Habib Chowdhury <sadrul@chromium.org>
2015-03-12 22:08:33 -0400

    aura: Fix updating |Env::last_mouse_location()|.
    If dispatching a held event triggers a nested message loop (e.g. a context
    menu), then moving the mouse does not update |Env::last_mouse_location()|
    correctly until the nested message loop terminates. To fix this, check the
    dispatched event and make sure that it's not the held event itself when
    setting the value of |Env::last_mouse_location()|.
    
    BUG=400154
    R=oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/1005723002
    
    Cr-Commit-Position: refs/heads/master@{#320432}

--- ui/aura/window_event_dispatcher_unittest.cc
+++ ui/aura/window_event_dispatcher_unittest.cc
@@ -2129,6 +2129,8 @@ TEST_F(WindowEventDispatcherTestInHighDPI,
   // before it reaches the window.
   EXPECT_EQ(gfx::Point(40, 40).ToString(),
             handler.mouse_move_location().ToString());
+  EXPECT_EQ(gfx::Point(40, 40).ToString(),
+            Env::GetInstance()->last_mouse_location().ToString());
   window->RemovePreTargetHandler(&handler);
 }
 
