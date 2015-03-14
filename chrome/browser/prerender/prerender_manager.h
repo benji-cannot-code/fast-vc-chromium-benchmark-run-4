1897d9676c214ca7d53688e646f2fc666847ef99
davidben <davidben@chromium.org>
2015-03-13 17:24:24 -0700

    Remove prerender cookie store, part 2.
    This removes cookie-based prerender histograms which are no longer necessary.
    It reverts:
    
    https://codereview.chromium.org/203783002
    https://codereview.chromium.org/145063002
    https://codereview.chromium.org/133913003
    
    BUG=457344
    
    Review URL: https://codereview.chromium.org/1007943002
    
    Cr-Commit-Position: refs/heads/master@{#320620}

--- chrome/browser/prerender/prerender_manager.h
+++ chrome/browser/prerender/prerender_manager.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/render_process_host_observer.h"
-#include "net/cookies/canonical_cookie.h"
 #include "net/cookies/cookie_monster.h"
 #include "url/gurl.h"
 
@@ -259,16 +258,6 @@ class PrerenderManager : public base::SupportsWeakPtr<PrerenderManager>,
       PrerenderContents::MatchCompleteStatus mc_status,
       FinalStatus final_status) const;
 
-  // Record a cookie status histogram (see prerender_histograms.h).
-  void RecordCookieStatus(Origin origin,
-                          uint8 experiment_id,
-                          int cookie_status) const;
-
-  // Record a cookie send type histogram (see prerender_histograms.h).
-  void RecordCookieSendType(Origin origin,
-                            uint8 experiment_id,
-                            int cookie_send_type) const;
-
   // content::NotificationObserver
   void Observe(int type,
                const content::NotificationSource& source,
@@ -318,17 +307,6 @@ class PrerenderManager : public base::SupportsWeakPtr<PrerenderManager>,
   PrerenderLocalPredictor* local_predictor() {
     return local_predictor_.get();
   }
-
-  // Notification that a cookie event happened on a render frame. Will record a
-  // cookie event for a given render frame, if it is being prerendered.
-  // If cookies were sent, all cookies must be supplied in |cookie_list|.
-  static void RecordCookieEvent(int process_id,
-                                int frame_id,
-                                const GURL& url,
-                                const GURL& frame_url,
-                                bool is_for_blocking_resource,
-                                PrerenderContents::CookieEvent event,
-                                const net::CookieList* cookie_list);
 
   // Notification that a prerender has completed and its bytes should be
   // recorded.
