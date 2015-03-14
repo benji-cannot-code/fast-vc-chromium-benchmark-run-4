18ffd4f496e8cb5ca1b46120cc7f395222bd36a1
bustamante <bustamante@chromium.org>
2015-03-13 17:28:39 -0700

    Workaround for latency tests, and removing perf/metrics dependency.
    This is hacky but will get our tests running again, so this doesn't
    cause a long outage.
    
    BUG=467192
    
    Review URL: https://codereview.chromium.org/1003343002
    
    Cr-Commit-Position: refs/heads/master@{#320621}

--- tools/chrome_proxy/integration_tests/chrome_proxy_measurements.py
+++ tools/chrome_proxy/integration_tests/chrome_proxy_measurements.py
@@ -18,6 +18,7 @@ class ChromeProxyLatency(page_test.PageTest):
 
   def __init__(self, *args, **kwargs):
     super(ChromeProxyLatency, self).__init__(*args, **kwargs)
+    self._metrics = metrics.ChromeProxyMetric()
 
   def CustomizeBrowserOptions(self, options):
     options.AppendExtraBrowserArgs('--enable-spdy-proxy-auth')
@@ -28,7 +29,7 @@ class ChromeProxyLatency(page_test.PageTest):
   def ValidateAndMeasurePage(self, page, tab, results):
     # Wait for the load event.
     tab.WaitForJavaScriptExpression('performance.timing.loadEventStart', 300)
-    loading.LoadingMetric().AddResults(tab, results)
+    self._metrics.AddResultsForLatency(tab, results)
 
 
 class ChromeProxyDataSaving(page_test.PageTest):
