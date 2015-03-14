cf180507571c1b14f4486d50ef8e44d08aca4864
jrummell <jrummell@chromium.org>
2015-03-13 17:19:42 -0700

    Add browser test to play encrypted media twice
    BUG=464504
    TEST=new test runs
    
    Review URL: https://codereview.chromium.org/1005733002
    
    Cr-Commit-Position: refs/heads/master@{#320618}

--- media/test/data/eme_player_js/test_config.js
+++ media/test/data/eme_player_js/test_config.js
@@ -13,6 +13,7 @@ function TestConfig() {
   this.useMSE = false;
   this.usePrefixedEME = false;
   this.runFPS = false;
+  this.playTwice = false;
 }
 
 TestConfig.prototype.loadQueryParams = function() {
@@ -29,6 +30,7 @@ TestConfig.prototype.loadQueryParams = function() {
   this.useMSE = this.useMSE == '1' || this.useMSE == 'true';
   this.usePrefixedEME =
       this.usePrefixedEME == '1' || this.usePrefixedEME == 'true';
+  this.playTwice = this.playTwice == '1' || this.playTwice == 'true';
 
   // Validate that the prefixed/unprefixed EME is available.
   if (this.usePrefixedEME) {
@@ -60,6 +62,7 @@ TestConfig.updateDocument = function() {
   document.getElementById(USE_MSE_ELEMENT_ID).value = this.useMSE;
   if (this.usePrefixedEME)
     document.getElementById(USE_PREFIXED_EME_ID).value = EME_PREFIXED_VERSION;
+  document.getElementById(USE_PLAY_TWICE_ELEMENT_ID).value = this.playTwice;
 };
 
 TestConfig.init = function() {
@@ -70,6 +73,8 @@ TestConfig.init = function() {
   this.useMSE = document.getElementById(USE_MSE_ELEMENT_ID).value == 'true';
   this.usePrefixedEME = document.getElementById(USE_PREFIXED_EME_ID).value ==
       EME_PREFIXED_VERSION;
+  this.playTwice =
+      document.getElementById(USE_PLAY_TWICE_ELEMENT_ID).value == 'true';
   this.licenseServerURL =
       document.getElementById(LICENSE_SERVER_ELEMENT_ID).value;
 };
