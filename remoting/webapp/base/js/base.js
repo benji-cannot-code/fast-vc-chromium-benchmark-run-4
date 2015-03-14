54e25fb3212f94bc47bc7026f79b23e19f8220f8
jrw <jrw@chromium.org>
2015-03-13 17:16:38 -0700

    Added SpyPromise class for testing code that uses promises.
    Review URL: https://codereview.chromium.org/990673003
    
    Cr-Commit-Position: refs/heads/master@{#320614}

--- remoting/webapp/base/js/base.js
+++ remoting/webapp/base/js/base.js
@@ -12,7 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 'use strict';
 
-var base = {};
+/** @suppress {duplicate} */
+var base = base || {};
+
 base.debug = function() {};
 
 /**
--- remoting/webapp/js_proto/qunit_proto.js
+++ remoting/webapp/js_proto/qunit_proto.js
@@ -58,9 +58,9 @@ QUnit.module = function(desc, dict) {};
 /**
  * @param {*} a
  * @param {*} b
- * @param {string} desc
+ * @param {string=} opt_desc
  */
-QUnit.notEqual = function(a, b, desc) {};
+QUnit.notEqual = function(a, b, opt_desc) {};
 
 /**
  * @param {boolean} cond
@@ -80,6 +80,15 @@ QUnit.test = function(desc, f) {};
 /** @param {Function} f */
 QUnit.testStart = function(f) {};
 
+/**
+ * @interface
+ */
+QUnit.Assert = function() {};
+
+/**
+ * @return {function():void}
+ */
+QUnit.Assert.prototype.async = function() {};
 
 var deepEqual = QUnit.deepEqual;
 var equal = QUnit.equal;
