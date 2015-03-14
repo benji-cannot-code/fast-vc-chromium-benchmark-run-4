7e0cebcaaf26110b05cb312ecb72e2c22363126d
sigbjornf@opera.com <sigbjornf@opera.com>
2015-03-14 07:59:20 +0000

    Remove ExecutionContext::lifecycleNotifier.
    No longer used nor created by ExecutionContext, remove entirely.
    
    Notice that this will make ExecutionContext::isIteratingOverObservers()
    now return an accurate status (it was always 'false' before.)
    
    R=haraken
    BUG=462949
    
    Review URL: https://codereview.chromium.org/1009623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191880 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/ExecutionContext.cpp
+++ third_party/WebKit/Source/core/dom/ExecutionContext.cpp
@@ -202,11 +202,6 @@ KURL ExecutionContext::completeURL(const String& url) const
     return virtualCompleteURL(url);
 }
 
-bool ExecutionContext::isIteratingOverObservers() const
-{
-    return m_lifecycleNotifier && m_lifecycleNotifier->isIteratingOverObservers();
-}
-
 void ExecutionContext::allowWindowInteraction()
 {
     ++m_windowInteractionTokens;
