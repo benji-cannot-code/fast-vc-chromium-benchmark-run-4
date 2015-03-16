43d64bc0adb29bba7aa7fa9d097af6ade6f3983d
sigbjornf@opera.com <sigbjornf@opera.com>
2015-03-16 21:34:43 +0000

    Oilpan: fix build after r191942.
    TBR=oilpan-reviews
    BUG=467059
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1014533006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191951 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/InspectorRenderingAgent.cpp
+++ third_party/WebKit/Source/web/InspectorRenderingAgent.cpp
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/frame/FrameView.h"
 #include "core/frame/Settings.h"
+#include "core/inspector/InspectorOverlay.h"
 #include "core/inspector/InspectorState.h"
 #include "core/page/Page.h"
 #include "web/WebLocalFrameImpl.h"
