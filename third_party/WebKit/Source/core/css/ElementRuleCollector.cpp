785215ddac947429fbcefd42a1472cdfa557fa4a
rune@opera.com <rune@opera.com>
2015-03-16 20:56:11 +0000

    Missing collectMatchingShadowHostRules call from hasAnyMatchingRules.
    This caused style to be incorrectly shared between two different hosts
    with different matching for :host([attr]).
    
    Regression from https://codereview.chromium.org/789403005
    
    R=kochi@chromium.org
    BUG=466815
    
    Review URL: https://codereview.chromium.org/1009893004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191947 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/ElementRuleCollector.cpp
+++ third_party/WebKit/Source/core/css/ElementRuleCollector.cpp
@@ -343,7 +343,9 @@ bool ElementRuleCollector::hasAnyMatchingRules(RuleSet* ruleSet)
     // information about "scope".
     int firstRuleIndex = -1, lastRuleIndex = -1;
     RuleRange ruleRange(firstRuleIndex, lastRuleIndex);
-    collectMatchingRules(MatchRequest(ruleSet), ruleRange);
+    MatchRequest matchRequest(ruleSet);
+    collectMatchingRules(matchRequest, ruleRange);
+    collectMatchingShadowHostRules(matchRequest, ruleRange);
 
     return !m_matchedRules.isEmpty();
 }
