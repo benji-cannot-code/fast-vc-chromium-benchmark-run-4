6b6263ac614a63b49db3e5d5d16cb592c15465cc
je_julie.kim@samsung.com <je_julie.kim@samsung.com>
2015-03-16 09:04:50 +0000

    Add handling to respect role attribute in AXObjects.
    Some AXObjects didn't handle role attribute correctly because it returns
    static role for roleValue().
    This patch makes they check role attribute on roleValue().
    It will have a higher priority than implicit role.
    AXObjects handled in this patch:
    - AXSlider
    - AXProgressIndicator
    - AXMenuList
    - AXListBoxOption
    - AXListBox
    
    BUG=467434
    R=dmazzoni
    
    Review URL: https://codereview.chromium.org/1004803004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191899 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/accessibility/AXListBox.h
+++ third_party/WebKit/Source/modules/accessibility/AXListBox.h
@@ -45,7 +45,7 @@ public:
     static PassRefPtr<AXListBox> create(LayoutObject*, AXObjectCacheImpl*);
     virtual ~AXListBox();
 
-    virtual AccessibilityRole roleValue() const override { return ListBoxRole; }
+    virtual AccessibilityRole roleValue() const override;
 };
 
 } // namespace blink
--- third_party/WebKit/Source/modules/accessibility/AXListBoxOption.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXListBoxOption.cpp
@@ -54,6 +54,14 @@ PassRefPtr<AXListBoxOption> AXListBoxOption::create(LayoutObject* layoutObject, 
     return adoptRef(new AXListBoxOption(layoutObject, axObjectCache));
 }
 
+AccessibilityRole AXListBoxOption::roleValue() const
+{
+    AccessibilityRole ariaRole = ariaRoleAttribute();
+    if (ariaRole != UnknownRole)
+        return ariaRole;
+    return ListBoxOptionRole;
+}
+
 bool AXListBoxOption::isEnabled() const
 {
     if (!node())
