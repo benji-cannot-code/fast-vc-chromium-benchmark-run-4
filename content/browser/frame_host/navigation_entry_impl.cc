7f0d864bed5e2d62d37844385e1e5b2229d3ed22
creis <creis@chromium.org>
2015-03-16 08:52:33 -0700

    Add NavigationEntryImpl::TreeNode for tracking FrameNavigationEntries.
    We still only track the main frame and do not share FNEs across
    NavigationEntries, but this new TreeNode class will make it possible
    to track them for subtrees and share them across entries of a tab.
    
    BUG=236848
    TEST=Existing Navigation{Controller,Entry} tests pass.
    
    Review URL: https://codereview.chromium.org/1006693002
    
    Cr-Commit-Position: refs/heads/master@{#320730}

--- content/browser/frame_host/navigation_entry_impl.cc
+++ content/browser/frame_host/navigation_entry_impl.cc
@@ -26,6 +26,22 @@ namespace content {
 
 int NavigationEntryImpl::kInvalidBindings = -1;
 
+NavigationEntryImpl::TreeNode::TreeNode(FrameNavigationEntry* frame_entry)
+    : frame_entry(frame_entry) {
+}
+
+NavigationEntryImpl::TreeNode::~TreeNode() {
+}
+
+NavigationEntryImpl::TreeNode* NavigationEntryImpl::TreeNode::Clone() const {
+  // Clone the tree using a copy of the FrameNavigationEntry, without sharing.
+  NavigationEntryImpl::TreeNode* copy =
+      new NavigationEntryImpl::TreeNode(frame_entry->Clone());
+
+  // TODO(creis): Clone children once we add them.
+  return copy;
+}
+
 NavigationEntry* NavigationEntry::Create() {
   return new NavigationEntryImpl();
 }
@@ -47,7 +63,8 @@ NavigationEntryImpl::NavigationEntryImpl(SiteInstanceImpl* instance,
                                          const base::string16& title,
                                          ui::PageTransition transition_type,
                                          bool is_renderer_initiated)
-    : frame_entry_(instance, url, referrer),
+    : frame_tree_(
+          new TreeNode(new FrameNavigationEntry(instance, url, referrer))),
       unique_id_(GetUniqueIDInConstructor()),
       bindings_(kInvalidBindings),
       page_type_(PAGE_TYPE_NORMAL),
@@ -79,12 +96,12 @@ PageType NavigationEntryImpl::GetPageType() const {
 }
 
 void NavigationEntryImpl::SetURL(const GURL& url) {
-  frame_entry_.set_url(url);
+  frame_tree_->frame_entry->set_url(url);
   cached_display_title_.clear();
 }
 
 const GURL& NavigationEntryImpl::GetURL() const {
-  return frame_entry_.url();
+  return frame_tree_->frame_entry->url();
 }
 
 void NavigationEntryImpl::SetBaseURLForDataURL(const GURL& url) {
@@ -96,11 +113,11 @@ const GURL& NavigationEntryImpl::GetBaseURLForDataURL() const {
 }
 
 void NavigationEntryImpl::SetReferrer(const Referrer& referrer) {
-  frame_entry_.set_referrer(referrer);
+  frame_tree_->frame_entry->set_referrer(referrer);
 }
 
 const Referrer& NavigationEntryImpl::GetReferrer() const {
-  return frame_entry_.referrer();
+  return frame_tree_->frame_entry->referrer();
 }
 
 void NavigationEntryImpl::SetVirtualURL(const GURL& url) {
@@ -138,7 +155,8 @@ int32 NavigationEntryImpl::GetPageID() const {
 }
 
 void NavigationEntryImpl::set_site_instance(SiteInstanceImpl* site_instance) {
-  frame_entry_.set_site_instance(site_instance);
+  // TODO(creis): Update all callers and remove this method.
+  frame_tree_->frame_entry->set_site_instance(site_instance);
 }
 
 void NavigationEntryImpl::set_source_site_instance(
@@ -327,10 +345,9 @@ void NavigationEntryImpl::ClearExtraData(const std::string& key) {
 NavigationEntryImpl* NavigationEntryImpl::Clone() const {
   NavigationEntryImpl* copy = new NavigationEntryImpl();
 
-  // TODO(creis): Once we have a tree of FrameNavigationEntries, make a deep
-  // copy.  Only share the same FrameNavigationEntries if cloning within the
-  // same tab.
-  copy->frame_entry_ = frame_entry_;
+  // TODO(creis): Only share the same FrameNavigationEntries if cloning within
+  // the same tab.
+  copy->frame_tree_.reset(frame_tree_->Clone());
 
   // Copy all state over, unless cleared in ResetForCommit.
   copy->unique_id_ = unique_id_;
