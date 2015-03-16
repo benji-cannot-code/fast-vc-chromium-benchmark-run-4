c14c9a15edd89de6410baaa7960798fc6f21c98d
fs@opera.com <fs@opera.com>
2015-03-16 19:05:20 +0000

    Drop declarations without matching definitions from SVGPathUtilities.h
    appendSVGPathByteStreamFromSVGPathSeg and buildStringFromSVGPathSegList
    lack corresponding definitions.
    Also drop related forward declarations and includes.
    
    Review URL: https://codereview.chromium.org/1014473003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191941 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGPathUtilities.h
+++ third_party/WebKit/Source/core/svg/SVGPathUtilities.h
@@ -23,28 +23,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/svg/SVGPathConsumer.h"
 #include "platform/geometry/FloatPoint.h"
-#include "platform/heap/Handle.h"
-#include "wtf/OwnPtr.h"
 #include "wtf/text/WTFString.h"
 
 namespace blink {
 
 class Path;
 class SVGPathByteStream;
-class SVGPathSeg;
-class SVGPathSegList;
 
 // String/SVGPathByteStream -> Path
 bool buildPathFromString(const String&, Path&);
 bool buildPathFromByteStream(const SVGPathByteStream&, Path&);
 
-// SVGPathSegList/String -> SVGPathByteStream
-bool appendSVGPathByteStreamFromSVGPathSeg(PassRefPtrWillBeRawPtr<SVGPathSeg>, SVGPathByteStream&, PathParsingMode);
+// String -> SVGPathByteStream
 bool buildSVGPathByteStreamFromString(const String&, SVGPathByteStream&, PathParsingMode);
 
-// SVGPathByteStream/SVGPathSegList -> String
+// SVGPathByteStream -> String
 bool buildStringFromByteStream(const SVGPathByteStream&, String&, PathParsingMode);
-bool buildStringFromSVGPathSegList(PassRefPtrWillBeRawPtr<SVGPathSegList>, String&, PathParsingMode);
 
 bool addToSVGPathByteStream(SVGPathByteStream&, const SVGPathByteStream&, unsigned repeatCount = 1);
 
