aed50786c5d9a73ebaa5058f41d77d9481c32e4a
davemoore <davemoore@chromium.org>
2015-03-16 13:38:51 -0700

    Add HTMLViewer command line arg to set user agent
    BUG=
    
    Review URL: https://codereview.chromium.org/1008543005
    
    Cr-Commit-Position: refs/heads/master@{#320787}

--- mojo/services/html_viewer/blink_platform_impl.cc
+++ mojo/services/html_viewer/blink_platform_impl.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cmath>
 
+#include "base/command_line.h"
 #include "base/rand_util.h"
 #include "base/stl_util.h"
 #include "base/synchronization/waitable_event.h"
@@ -22,10 +23,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace html_viewer {
 namespace {
 
+// Allows overriding user agent scring.
+const char kUserAgentSwitch[] = "user-agent";
+
 // TODO(darin): Figure out what our UA should really be.
-const char kUserAgentString[] =
-  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
-  "Chrome/35.0.1916.153 Safari/537.36";
+const char kDefaultUserAgentString[] =
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/35.0.1916.153 Safari/537.36";
 
 class WebWaitableEventImpl : public blink::WebWaitableEvent {
  public:
@@ -173,7 +177,12 @@ blink::WebSocketHandle* BlinkPlatformImpl::createWebSocketHandle() {
 }
 
 blink::WebString BlinkPlatformImpl::userAgent() {
-  return blink::WebString::fromUTF8(kUserAgentString);
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(kUserAgentSwitch)) {
+    return blink::WebString::fromUTF8(
+        command_line->GetSwitchValueASCII(kUserAgentSwitch));
+  }
+  return blink::WebString::fromUTF8(kDefaultUserAgentString);
 }
 
 blink::WebData BlinkPlatformImpl::parseDataURL(
