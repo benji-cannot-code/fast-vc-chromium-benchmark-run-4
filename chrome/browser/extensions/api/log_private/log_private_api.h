879dfe4902a4c73ae8c1dd36cacd22222574b016
mmenke <mmenke@chromium.org>
2015-03-16 10:38:49 -0700

    Add ability for NetLogLogger to gather data from more than just NetLog
    This includes data from other net-internals tabs and from pending
    URLRequests.  This also changes the semantices of NetLogLogger so
    that it takes the input file in StartObserving and closes it in
    StopObserving, to make thread restrictions when using the new
    capability a little simpler.
    
    This CL also fixes a number of cases where StopObserving either wasn't
    being called, or was being called incorrectly by consumers.
    
    BUG=426918
    
    Review URL: https://codereview.chromium.org/976483002
    
    Cr-Commit-Position: refs/heads/master@{#320745}

--- chrome/browser/extensions/api/log_private/log_private_api.h
+++ chrome/browser/extensions/api/log_private/log_private_api.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
+#include "base/files/scoped_file.h"
 #include "base/scoped_observer.h"
 #include "chrome/browser/extensions/api/log_private/filter_handler.h"
 #include "chrome/browser/extensions/api/log_private/log_parser.h"
@@ -89,14 +90,12 @@ class LogPrivateAPI : public BrowserContextKeyedAPI,
   void PostPendingEntries();
   void AddEntriesOnUI(scoped_ptr<base::ListValue> value);
 
-  // Initializes a new instance of net::NetLogLogger and passes it back via
-  // |net_logger| param.
-  void InitializeNetLogger(const std::string& owner_extension_id,
-                           net::NetLogLogger** net_logger);
+  // Creates a file for use with a new instance of net::NetLogLogger.
+  void CreateFileForNetLogger(
+      const std::string& owner_extension_id, base::ScopedFILE* file);
 
   // Starts observing network events with a new |net_logger| instance.
-  void StartObservingNetEvents(IOThread* io_thread,
-                               net::NetLogLogger** net_logger);
+  void StartObservingNetEvents(IOThread* io_thread, base::ScopedFILE* file);
   void MaybeStartNetInternalLogging(const std::string& caller_extension_id,
                                     IOThread* io_thread,
                                     api::log_private::EventSink event_sink);
--- google_apis/gcm/tools/mcs_probe.cc
+++ google_apis/gcm/tools/mcs_probe.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
+#include "base/files/scoped_file.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
@@ -279,6 +280,8 @@ MCSProbe::MCSProbe(
 }
 
 MCSProbe::~MCSProbe() {
+  if (logger_)
+    logger_->StopObserving(nullptr);
   file_thread_.Stop();
 }
 
@@ -346,20 +349,19 @@ void MCSProbe::UpdateCallback(bool success) {
 }
 
 void MCSProbe::InitializeNetworkState() {
-  FILE* log_file = NULL;
+  base::ScopedFILE log_file;
   if (command_line_.HasSwitch(kLogFileSwitch)) {
     base::FilePath log_path = command_line_.GetSwitchValuePath(kLogFileSwitch);
 #if defined(OS_WIN)
-    log_file = _wfopen(log_path.value().c_str(), L"w");
+    log_file.reset(_wfopen(log_path.value().c_str(), L"w"));
 #elif defined(OS_POSIX)
-    log_file = fopen(log_path.value().c_str(), "w");
+    log_file.reset(fopen(log_path.value().c_str(), "w"));
 #endif
   }
-  if (log_file != NULL) {
-    scoped_ptr<base::Value> net_constants(net::NetLogLogger::GetConstants());
-    logger_.reset(new net::NetLogLogger(log_file, *net_constants));
+  if (log_file.get()) {
+    logger_.reset(new net::NetLogLogger());
     logger_->set_log_level(net::NetLog::LOG_ALL_BUT_BYTES);
-    logger_->StartObserving(&net_log_);
+    logger_->StartObserving(&net_log_, log_file.Pass(), nullptr, nullptr);
   }
 
   host_resolver_ = net::HostResolver::CreateDefaultResolver(&net_log_);
