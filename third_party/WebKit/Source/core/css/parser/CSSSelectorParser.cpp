dbbefef8c60e391639cde9714c793f1dfc3105cd
timloh@chromium.org <timloh@chromium.org>
2015-03-17 05:51:25 +0000

    CSS Parser: Remove unneeded String object when parsing classes and ids
    This patch fixes up consumeClass and consumeId, removing an unneeded
    creation of String objects. Previously a parser token's value was a
    String, but crrev.com/990083002 changed this to a CSSParserString
    without updating this code.
    
    This appears to improves the parser's performance (on some arbitrary
    set of CSS files) by about 2%.
    
    BUG=455064
    
    Review URL: https://codereview.chromium.org/1013083002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191979 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/CSSSelectorParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSSelectorParser.cpp
@@ -199,11 +199,11 @@ PassOwnPtr<CSSParserSelector> CSSSelectorParser::consumeId(CSSParserTokenRange& 
         return nullptr;
     OwnPtr<CSSParserSelector> selector = CSSParserSelector::create();
     selector->setMatch(CSSSelector::Id);
-    const String& value = range.consumeIncludingComments().value();
+    const AtomicString& value = range.consumeIncludingComments().value();
     if (isQuirksModeBehavior(m_context.mode()))
-        selector->setValue(AtomicString(value.lower()));
+        selector->setValue(value.lower());
     else
-        selector->setValue(AtomicString(value));
+        selector->setValue(value);
     return selector.release();
 }
 
@@ -216,11 +216,11 @@ PassOwnPtr<CSSParserSelector> CSSSelectorParser::consumeClass(CSSParserTokenRang
         return nullptr;
     OwnPtr<CSSParserSelector> selector = CSSParserSelector::create();
     selector->setMatch(CSSSelector::Class);
-    const String& value = range.consumeIncludingComments().value();
+    const AtomicString& value = range.consumeIncludingComments().value();
     if (isQuirksModeBehavior(m_context.mode()))
-        selector->setValue(AtomicString(value.lower()));
+        selector->setValue(value.lower());
     else
-        selector->setValue(AtomicString(value));
+        selector->setValue(value);
     return selector.release();
 }
 
