92172956612d3f4ae402c0d9c0798a4a05706358
mlerman <mlerman@chromium.org>
2015-03-17 14:41:25 -0700

    IDP IFrame Endpoint is no longer experimental
    BUG=463611
    
    Review URL: https://codereview.chromium.org/1014843003
    
    Cr-Commit-Position: refs/heads/master@{#320981}

--- google_apis/gaia/gaia_auth_fetcher.cc
+++ google_apis/gaia/gaia_auth_fetcher.cc
@@ -446,7 +446,6 @@ std::string GaiaAuthFetcher::MakeListIDPSessionsBody(
   static const char getTokenResponseBodyFormat[] =
     "action=listSessions&"
     "client_id=%s&"
-    "e=3100087&"  // temporarily enable the experiment.
     "origin=%s&"
     "scope=%s";
   std::string encoded_client_id = net::EscapeUrlEncodedData(
@@ -466,7 +465,6 @@ std::string GaiaAuthFetcher::MakeGetTokenResponseBody(
     "client_id=%s&"
     "login_hint=%s&"
     "origin=%s&"
-    "e=3100087&"  // temporarily enable the experiment.
     "response_type=token&"
     "scope=%s";
   std::string encoded_client_id = net::EscapeUrlEncodedData(
