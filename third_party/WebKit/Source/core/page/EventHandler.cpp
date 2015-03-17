05bee4e53e509bb652f10a1ad681a88f330f0bb3
bokan@chromium.org <bokan@chromium.org>
2015-03-17 12:54:08 +0000

    Fix showUnhandledTapUIIfNeeded to account for PinchViewport transformation.
    The passed possition must be converted from the root frame coordinates into
    viewport coordinates.
    
    BUG=455328
    
    Review URL: https://codereview.chromium.org/1004033002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192007 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/EventHandler.cpp
+++ third_party/WebKit/Source/core/page/EventHandler.cpp
@@ -53,8 +53,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/events/WheelEvent.h"
 #include "core/fetch/ImageResource.h"
 #include "core/frame/EventHandlerRegistry.h"
+#include "core/frame/FrameHost.h"
 #include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
+#include "core/frame/PinchViewport.h"
 #include "core/frame/Settings.h"
 #include "core/html/HTMLDialogElement.h"
 #include "core/html/HTMLFrameElementBase.h"
@@ -2352,10 +2354,17 @@ bool EventHandler::handleGestureTap(const GestureEventWithHitTestResults& target
         swallowMouseUpEvent = handleMouseReleaseEvent(MouseEventWithHitTestResults(fakeMouseUp, currentHitTest));
 
     bool swallowed = swallowMouseDownEvent | swallowMouseUpEvent | swallowClickEvent;
-    if (!swallowed && tappedNode) {
+    if (!swallowed && tappedNode && m_frame->page()) {
         bool domTreeChanged = preDispatchDomTreeVersion != m_frame->document()->domTreeVersion();
         bool styleChanged = preDispatchStyleVersion != m_frame->document()->styleVersion();
-        m_frame->chromeClient().showUnhandledTapUIIfNeeded(tappedPosition, tappedNode.get(), domTreeChanged || styleChanged);
+
+        // FIXME: This will be replaced with a standard conversion method once crbug.com/371902 lands.
+        PinchViewport& pinchViewport = m_frame->page()->frameHost().pinchViewport();
+        IntPoint tappedPositionInViewport = tappedPosition;
+        tappedPositionInViewport.moveBy(-flooredIntPoint(pinchViewport.location()));
+        tappedPositionInViewport.scale(pinchViewport.scale(), pinchViewport.scale());
+
+        m_frame->chromeClient().showUnhandledTapUIIfNeeded(tappedPositionInViewport, tappedNode.get(), domTreeChanged || styleChanged);
     }
     return swallowed;
 }
