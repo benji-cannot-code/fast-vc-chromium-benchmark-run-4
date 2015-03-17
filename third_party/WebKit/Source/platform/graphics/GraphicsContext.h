47916b546e8055aa98a4d629f31ae529f05e1666
senorblanco@chromium.org <senorblanco@chromium.org>
2015-03-17 18:42:26 +0000

    Remove some spurious #includes from GraphicsContext.cpp/.h.
    I really just needed to remove SkMatrixImageFilter, since it's going to
    move, but couldn't resist removing a few more.
    
    Review URL: https://codereview.chromium.org/1014833002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192030 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/GraphicsContext.h
+++ third_party/WebKit/Source/platform/graphics/GraphicsContext.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/geometry/FloatRoundedRect.h"
 #include "platform/graphics/DashArray.h"
 #include "platform/graphics/DrawLooperBuilder.h"
-#include "platform/graphics/ImageBufferSurface.h"
 #include "platform/graphics/ImageOrientation.h"
 #include "platform/graphics/GraphicsContextAnnotation.h"
 #include "platform/graphics/GraphicsContextState.h"
