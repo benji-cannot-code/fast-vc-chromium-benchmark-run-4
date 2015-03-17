aa577e05fcc70f677868b1b6593ea036d2e8c358
bokan@chromium.org <bokan@chromium.org>
2015-03-17 15:22:57 +0000

    Anchor viewport during WebViewImpl resizes.
    Since we now have two viewports, shrinking the WebView causes some surprising
    viewport movement. When the WebView shrinks, the shrunken FrameView pushes the
    PinchViewport up to keep it within its bounds. This causes bugs when the
    PinchViewport is zoomed in and fully scrolled and the window is shrunken, either
    by the user on desktop platforms, or by the on screen keyboard on Android.
    
    This CL fixes it so that the visual viewport, from the users perspective,
    remains fixed uring a resize. I've expaned the ViewportAnchor class to support
    two modes of anchoring, the current one we use during rotations, and a new one
    for non-rotation resizes that simply attempts to bring the viewport back to
    where it was prior to the resize. I've also moved as much of this anchoring
    logic as I could from WebViewImpl::resize into RotationViewportAnchor (and added
    the new anchoring logic in ResizeViewportAnchor).
    
    I've changed the semantics of resize slightly by explicitly calling layout() in
    WebViewImpl::resize on desktops. This was previously done on Android only and
    it's an annoying platform difference; many unit tests are written assuming
    resizes cause layout.
    
    BUG=453859
    
    Review URL: https://codereview.chromium.org/1008513003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192015 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/web/ResizeViewportAnchor.cpp
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "web/ResizeViewportAnchor.h"
+
+#include "core/frame/FrameView.h"
+#include "core/frame/PinchViewport.h"
+#include "platform/geometry/DoublePoint.h"
+#include "platform/geometry/DoubleSize.h"
+#include "platform/geometry/FloatSize.h"
+
+namespace blink {
+
+ResizeViewportAnchor::ResizeViewportAnchor(FrameView& rootFrameView, PinchViewport& pinchViewport)
+    : ViewportAnchor(rootFrameView, pinchViewport) { }
+
+
+void ResizeViewportAnchor::setAnchor()
+{
+    m_pinchViewportInDocument = m_pinchViewport.visibleRectInDocument().location();
+}
+
+void ResizeViewportAnchor::restoreToAnchor()
+{
+    FloatSize delta = m_pinchViewportInDocument - m_pinchViewport.visibleRectInDocument().location();
+
+    DoublePoint targetFrameViewPosition = m_rootFrameView.scrollPositionDouble() + DoubleSize(delta);
+    m_rootFrameView.setScrollPosition(targetFrameViewPosition);
+
+    DoubleSize remainder = targetFrameViewPosition - m_rootFrameView.scrollPositionDouble();
+    m_pinchViewport.move(toFloatSize(remainder));
+}
+
+} // namespace blink
