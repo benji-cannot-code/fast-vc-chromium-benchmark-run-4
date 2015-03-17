e35da2733f7a4e8c98518599eade780af85240bb
jdduke <jdduke@chromium.org>
2015-03-17 14:50:23 -0700

    [Android] Add slack to allow refresh after overscrolling
    Allow pull-to-refresh activation when the overscroll glow effect is
    active but mostly transparent. This accomodates accidental refresh
    prevention while easing the frustration of having to wait until
    the overscroll glow has completely faded.
    
    BUG=428429
    
    Review URL: https://codereview.chromium.org/993613007
    
    Cr-Commit-Position: refs/heads/master@{#320986}

--- content/browser/android/edge_effect.h
+++ content/browser/android/edge_effect.h
@@ -38,6 +38,7 @@ class EdgeEffect : public EdgeEffectBase {
 
   void Finish() override;
   bool IsFinished() const override;
+  float GetAlpha() const override;
 
   void ApplyToLayers(const gfx::SizeF& size,
                      const gfx::Transform& transform) override;
--- content/browser/android/edge_effect_base.h
+++ content/browser/android/edge_effect_base.h
@@ -39,6 +39,7 @@ class EdgeEffectBase {
 
   virtual void Finish() = 0;
   virtual bool IsFinished() const = 0;
+  virtual float GetAlpha() const = 0;
 
   virtual void ApplyToLayers(const gfx::SizeF& size,
                              const gfx::Transform& transform) = 0;
