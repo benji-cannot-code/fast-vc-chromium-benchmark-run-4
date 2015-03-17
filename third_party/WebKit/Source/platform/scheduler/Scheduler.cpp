95c21fc7ce6c26ad6fe5e87a779818621f8e8e75
rmcilroy@chromium.org <rmcilroy@chromium.org>
2015-03-17 12:37:40 +0000

    Add canExceedIdleDeadlineIfRequired function to Scheduler and use it in Oilpan.
    Adds a canExceedIdleDeadlineIfRequired function to the Scheduler to enable 
    IdleTasks to estimate the best time for them to exceed their deadline
    if they have a large monolithic task which could not be run otherwise. 
    
    Modifies the Oilpan performIdleTask to perform the GC if
    it would exceed it's deadline
    but the scheduler returns true from canExceedIdleDeadlineIfRequired.
    
    Chromium side of patch: https://codereview.chromium.org/969373002/edit
    
    Long idle task design doc:
    https://docs.google.com/a/chromium.org/document/d/1yBlUdYW8VTIfB-DqhvQqUeP0kf-Ap1W4cao2yQq58Do/edit
    
    BUG=455713
    
    Review URL: https://codereview.chromium.org/971283002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192006 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/scheduler/Scheduler.cpp
+++ third_party/WebKit/Source/platform/scheduler/Scheduler.cpp
@@ -96,4 +96,11 @@ bool Scheduler::shouldYieldForHighPriorityWork() const
     return false;
 }
 
+bool Scheduler::canExceedIdleDeadlineIfRequired() const
+{
+    if (m_webScheduler)
+        return m_webScheduler->canExceedIdleDeadlineIfRequired();
+    return false;
+}
+
 } // namespace blink
