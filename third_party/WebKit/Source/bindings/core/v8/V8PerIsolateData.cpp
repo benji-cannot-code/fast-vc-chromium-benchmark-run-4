0ead6c18d568cc87cb10b627bfda1cc4618ce712
sadrul@chromium.org <sadrul@chromium.org>
2015-03-17 07:29:28 +0000

    unit-tests: Look at Platform::unitTestSupport() to determine when running unit-tests.
    BUG=467277
    
    Review URL: https://codereview.chromium.org/1012643003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191983 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/V8PerIsolateData.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8PerIsolateData.cpp
@@ -48,6 +48,11 @@ static void assertV8RecursionScope()
 {
     ASSERT(V8RecursionScope::properlyUsed(v8::Isolate::GetCurrent()));
 }
+
+static bool runningUnitTest()
+{
+    return blink::Platform::current()->unitTestSupport();
+}
 #endif
 
 static void useCounterCallback(v8::Isolate* isolate, v8::Isolate::UseCounterFeature feature)
@@ -83,8 +88,7 @@ V8PerIsolateData::V8PerIsolateData()
     // FIXME: Remove once all v8::Isolate::GetCurrent() calls are gone.
     isolate()->Enter();
 #if ENABLE(ASSERT)
-    // currentThread will always be non-null in production, but can be null in Chromium unit tests.
-    if (blink::Platform::current()->currentThread())
+    if (!runningUnitTest())
         isolate()->AddCallCompletedCallback(&assertV8RecursionScope);
 #endif
     if (isMainThread())
@@ -137,7 +141,7 @@ void V8PerIsolateData::willBeDestroyed(v8::Isolate* isolate)
 void V8PerIsolateData::destroy(v8::Isolate* isolate)
 {
 #if ENABLE(ASSERT)
-    if (blink::Platform::current()->currentThread())
+    if (!runningUnitTest())
         isolate->RemoveCallCompletedCallback(&assertV8RecursionScope);
 #endif
     V8PerIsolateData* data = from(isolate);
