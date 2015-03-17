2c16e652a44a0c9a4dd1680e5703facbe5a60347
shrikant <shrikant@chromium.org>
2015-03-17 14:47:12 -0700

    Change for building font cache into separate thread inside utility process so that IO thread remains responsive.
    BUG=458751, 436195
    R=cpu,scottmg
    
    Review URL: https://codereview.chromium.org/1011023002
    
    Cr-Commit-Position: refs/heads/master@{#320983}

--- chrome/utility/font_cache_handler_win.h
+++ chrome/utility/font_cache_handler_win.h
@@ -7,7 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_UTILITY_FONT_CACHE_HANDLER_WIN_H_
 
 #include "base/files/file_path.h"
+#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_ptr.h"
 #include "chrome/utility/utility_message_handler.h"
+
+namespace base {
+class TaskRunner;
+class Thread;
+}
 
 // Handles requests to build a static direct write font cache. Must be invoked
 // in a non-sandboxed utility process. We build static font cache in utility
@@ -25,6 +32,14 @@ class FontCacheHandler : public UtilityMessageHandler {
 
  private:
   void OnBuildFontCache(const base::FilePath& full_path);
+  void StartBuildingFontCache(const base::FilePath& full_path);
+  void Cleanup();
+
+  scoped_refptr<base::TaskRunner> utility_task_runner_;
+
+  // Thread that caching process runs on, while FontCacheHandler handles
+  // messages from the browser process.
+  scoped_ptr<base::Thread> cache_thread_;
 
   DISALLOW_COPY_AND_ASSIGN(FontCacheHandler);
 };
