2d0b0f3eee458ec7176cc07778ea8d9c19d6d227
thestig <thestig@chromium.org>
2015-03-16 21:28:36 -0700

    Autofill: Search for standalone label tags while looking through divs.
    Some websites use labels, but do not wrap inputs in labels and/or do not
    add the proper for= attribute.
    
    Also add a failing test case, which this CL improves upon.
    BUG=465587
    
    Review URL: https://codereview.chromium.org/1003163003
    
    Cr-Commit-Position: refs/heads/master@{#320860}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -536,6 +536,9 @@ base::string16 InferLabelFromTableRow(const WebFormControlElement& element) {
 // a surrounding div table,
 // e.g. <div>Some Text<span><input ...></span></div>
 // e.g. <div>Some Text</div><div><input ...></div>
+//
+// Because this is already traversing the <div> structure, if it finds a <label>
+// sibling along the way, infer from that <label>.
 base::string16 InferLabelFromDivTable(const WebFormControlElement& element) {
   WebNode node = element.parentNode();
   bool looking_for_parent = true;
@@ -546,6 +549,7 @@ base::string16 InferLabelFromDivTable(const WebFormControlElement& element) {
   CR_DEFINE_STATIC_LOCAL(WebString, kDiv, ("div"));
   CR_DEFINE_STATIC_LOCAL(WebString, kTable, ("table"));
   CR_DEFINE_STATIC_LOCAL(WebString, kFieldSet, ("fieldset"));
+  CR_DEFINE_STATIC_LOCAL(WebString, kLabel, ("label"));
   while (inferred_label.empty() && !node.isNull()) {
     if (HasTagName(node, kDiv)) {
       if (looking_for_parent)
@@ -566,6 +570,10 @@ base::string16 InferLabelFromDivTable(const WebFormControlElement& element) {
       }
 
       looking_for_parent = false;
+    } else if (!looking_for_parent && HasTagName(node, kLabel)) {
+      WebLabelElement label_element = node.to<WebLabelElement>();
+      if (label_element.correspondingControl().isNull())
+        inferred_label = FindChildText(node);
     } else if (looking_for_parent &&
                (HasTagName(node, kTable) || HasTagName(node, kFieldSet))) {
       // If the element is in a table or fieldset, its label most likely is too.
