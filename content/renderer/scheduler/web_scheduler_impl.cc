bd9692787997ec8d9a14fe4895e1e6f58354be10
rmcilroy <rmcilroy@chromium.org>
2015-03-17 06:09:15 -0700

    [renderer]: Fix RendererScheduler shutdown logic to ensure it is shutdown before the MessageLoop.
    The RendererScheduler needs to be shutdown before the MessageLoop. Previously
    this was the case as blink was shutdown before the message loop, however now
    the message loop is shutdown first. This CL ensures that the renderer
    scheduler is shutdown just before the message loop.
    
    BUG=467369
    
    Review URL: https://codereview.chromium.org/1014643002
    
    Cr-Commit-Position: refs/heads/master@{#320908}

--- content/renderer/scheduler/web_scheduler_impl.cc
+++ content/renderer/scheduler/web_scheduler_impl.cc
@@ -86,9 +86,7 @@ void WebSchedulerImpl::postLoadingTask(
 }
 
 void WebSchedulerImpl::shutdown() {
-  idle_task_runner_ = nullptr;
-  loading_task_runner_ = nullptr;
-  return renderer_scheduler_->Shutdown();
+  // TODO(rmcilroy): Delete this method once the blink side is removed.
 }
 
 }  // namespace content
