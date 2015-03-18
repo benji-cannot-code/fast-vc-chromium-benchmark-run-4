1899a6e17263cbef97c796d1992bf5a565eedf5b
sigbjornf@opera.com <sigbjornf@opera.com>
2015-03-18 07:31:10 +0000

    Allow cross-origin cssRules access to CORS-fetched stylesheet.
    When accessing the cssRules property on CSSStyleSheet an origin check is
    performed for non-inline stylesheets. Should that stylesheet have been
    loaded from another origin following CORS, the access ought to be
    permitted, but wasn't.
    
    Address by having the link element (LinkStyle) record if the stylesheet
    it creates was successfully fetched using CORS. If it was, the stylesheet
    access check will succeed.
    
    R=mkwst
    BUG=467672
    
    Review URL: https://codereview.chromium.org/1011103002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192066 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/security/resources/green-if-cors-anonymous.php
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+if (isset($_SERVER['HTTP_ORIGIN']) && !isset($_COOKIE['cookie'])) {
+    header("Access-Control-Allow-Origin: *");
+    $name = 'green-background.css';
+} else {
+    $name = 'red-background.css';
+}
+
+$fp = fopen($name, 'rb');
+header("Content-Type: text/css");
+header("Content-Length: " . filesize($name));
+
+fpassthru($fp);
+exit;
+?>
--- third_party/WebKit/Source/core/css/CSSStyleSheet.h
+++ third_party/WebKit/Source/core/css/CSSStyleSheet.h
@@ -37,6 +37,7 @@ class CSSStyleSheet;
 class Document;
 class ExceptionState;
 class MediaQuerySet;
+class SecurityOrigin;
 class StyleSheetContents;
 
 enum StyleSheetUpdateType {
@@ -88,6 +89,8 @@ public:
     MediaQuerySet* mediaQueries() const { return m_mediaQueries.get(); }
     void setMediaQueries(PassRefPtrWillBeRawPtr<MediaQuerySet>);
     void setTitle(const String& title) { m_title = title; }
+    // Set by LinkStyle iff CORS-enabled fetch of stylesheet succeeded from this origin.
+    void setAllowRuleAccessFromOrigin(PassRefPtr<SecurityOrigin> allowedOrigin);
 
     class RuleMutationScope {
         WTF_MAKE_NONCOPYABLE(RuleMutationScope);
@@ -136,6 +139,8 @@ private:
     bool m_isDisabled;
     String m_title;
     RefPtrWillBeMember<MediaQuerySet> m_mediaQueries;
+
+    RefPtr<SecurityOrigin> m_allowRuleAccessFromOrigin;
 
     RawPtrWillBeMember<Node> m_ownerNode;
     RawPtrWillBeMember<CSSRule> m_ownerRule;
