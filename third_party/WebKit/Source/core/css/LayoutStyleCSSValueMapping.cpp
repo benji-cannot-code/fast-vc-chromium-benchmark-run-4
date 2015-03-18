e90e75e8bb1a3df0269c72719a2f6e9bc37ead7a
rob.buis@samsung.com <rob.buis@samsung.com>
2015-03-18 23:22:23 +0000

    Do not correct for zoom for number line-height
    When a line-height is specified as a number (for example 1.2)
    StyleBuilderConverter::convertLineHeight makes a Percent type
    Length to store in LayoutStyle. When getting the computed
    style value for line-height, valueForLineHeight uses the
    zoom-independent FontDescription::specifiedSize. Therefore
    there is no need in this case to call zoomAdjustedPixelValue.
    
    This also fixes the value shown in the computed panel in inspector.
    
    BUG=442084
    
    Review URL: https://codereview.chromium.org/1017173002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192124 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/LayoutStyleCSSValueMapping.cpp
+++ third_party/WebKit/Source/core/css/LayoutStyleCSSValueMapping.cpp
@@ -477,7 +477,11 @@ static PassRefPtrWillBeRawPtr<CSSPrimitiveValue> valueForLineHeight(const Layout
     if (length.isNegative())
         return cssValuePool().createIdentifierValue(CSSValueNormal);
 
-    return zoomAdjustedPixelValue(floatValueForLength(length, style.fontDescription().specifiedSize()), style);
+    float floatValue = floatValueForLength(length, style.fontDescription().specifiedSize());
+    if (length.isPercent())
+        return cssValuePool().createValue(floatValue, CSSPrimitiveValue::CSS_PX);
+
+    return zoomAdjustedPixelValue(floatValue, style);
 }
 
 static CSSValueID identifierForFamily(const AtomicString& family)
