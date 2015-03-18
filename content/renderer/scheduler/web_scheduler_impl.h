28f414ab5442ac95980710ab9ab20f275dad7b3e
alexclarke <alexclarke@chromium.org>
2015-03-18 06:00:04 -0700

    Chromium side of adding postTimerTask to blink::WebScheduler
    Initially postTimerTask will post onto the DefaultTaskRunner. In a
    follow up patch a TIMER_TASK_QUEUE will be added to the scheduler and
    the implementations of postTimerTask altered to post to that.
    
    BUG=463143
    
    Review URL: https://codereview.chromium.org/1008743004
    
    Cr-Commit-Position: refs/heads/master@{#321112}

--- content/renderer/scheduler/web_scheduler_impl.h
+++ content/renderer/scheduler/web_scheduler_impl.h
@@ -37,6 +37,9 @@ class CONTENT_EXPORT WebSchedulerImpl : public blink::WebScheduler {
                                        blink::WebScheduler::IdleTask* task);
   virtual void postLoadingTask(const blink::WebTraceLocation& location,
                                blink::WebThread::Task* task);
+  virtual void postTimerTask(const blink::WebTraceLocation& location,
+                             blink::WebThread::Task* task,
+                             long long delayMs);
   virtual void shutdown();
 
  private:
@@ -47,6 +50,7 @@ class CONTENT_EXPORT WebSchedulerImpl : public blink::WebScheduler {
   RendererScheduler* renderer_scheduler_;
   scoped_refptr<SingleThreadIdleTaskRunner> idle_task_runner_;
   scoped_refptr<base::SingleThreadTaskRunner> loading_task_runner_;
+  scoped_refptr<base::SingleThreadTaskRunner> timer_task_runner_;
 };
 
 }  // namespace content
--- mojo/services/html_viewer/webscheduler_impl.h
+++ mojo/services/html_viewer/webscheduler_impl.h
@@ -24,6 +24,9 @@ class WebSchedulerImpl : public blink::WebScheduler {
                     blink::WebScheduler::IdleTask* task);
   virtual void postLoadingTask(const blink::WebTraceLocation& location,
                        blink::WebThread::Task* task);
+  virtual void postTimerTask(const blink::WebTraceLocation& location,
+                             blink::WebThread::Task* task,
+                             long long delayMs);
   virtual void shutdown();
 
   static void RunIdleTask(scoped_ptr<blink::WebScheduler::IdleTask> task);
