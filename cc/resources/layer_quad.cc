22ca7d2302e74770b05ffcdc06e27607d20973c2
awoloszyn <awoloszyn@chromium.org>
2015-03-18 07:27:47 -0700

    Updated layer_quads to deal with degenerate cases more gracefully.
    BUG=467184
    
    Review URL: https://codereview.chromium.org/1018483003
    
    Cr-Commit-Position: refs/heads/master@{#321126}

--- cc/resources/layer_quad.cc
+++ cc/resources/layer_quad.cc
@@ -23,6 +23,14 @@ LayerQuad::Edge::Edge(const gfx::PointF& p, const gfx::PointF& q) {
   scale(1.0f / tangent.Length());
 }
 
+gfx::PointF LayerQuad::Edge::Intersect(const LayerQuad::Edge& e) const {
+  DCHECK(!degenerate());
+  DCHECK(!e.degenerate());
+
+  return gfx::PointF((y() * e.z() - e.y() * z()) / (x() * e.y() - e.x() * y()),
+                     (x() * e.z() - e.x() * z()) / (e.x() * y() - x() * e.y()));
+}
+
 LayerQuad::LayerQuad(const gfx::QuadF& quad) {
   // Create edges.
   left_ = Edge(quad.p4(), quad.p1());
@@ -47,6 +55,12 @@ LayerQuad::LayerQuad(const Edge& left,
       bottom_(bottom) {}
 
 gfx::QuadF LayerQuad::ToQuadF() const {
+  size_t num_degenerate_edges = left_.degenerate() + right_.degenerate() +
+                                top_.degenerate() + bottom_.degenerate();
+  if (num_degenerate_edges > 1) {
+    return gfx::QuadF();
+  }
+
   if (left_.degenerate()) {
     return gfx::QuadF(top_.Intersect(bottom_), top_.Intersect(right_),
                       right_.Intersect(bottom_), bottom_.Intersect(top_));
