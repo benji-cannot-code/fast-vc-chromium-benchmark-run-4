72167515fd7499ce81f610290a6bf2f7aa8180ab
nednguyen <nednguyen@google.com>
2015-03-18 08:21:16 -0700

    [Telemetry] Create shared_page_state.CanRunOnBrowser to replace page.CanRunOnBrowser
    BUG=468085
    
    Review URL: https://codereview.chromium.org/1007253007
    
    Cr-Commit-Position: refs/heads/master@{#321135}

--- tools/perf/page_sets/tough_webgl_cases.py
+++ tools/perf/page_sets/tough_webgl_cases.py
@@ -2,8 +2,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-import logging
+from page_sets import webgl_supported_shared_state
 
+from telemetry.page import shared_page_state
 from telemetry.page import page as page_module
 from telemetry.page import page_set as page_set_module
 
@@ -11,14 +12,12 @@ from telemetry.page import page_set as page_set_module
 class ToughWebglCasesPage(page_module.Page):
 
   def __init__(self, url, page_set):
-    super(ToughWebglCasesPage, self).__init__(url=url, page_set=page_set)
+    super(ToughWebglCasesPage, self).__init__(
+        url=url, page_set=page_set,
+        shared_page_state_class=(
+            webgl_supported_shared_state.WebGLSupportedSharedState))
     self.archive_data_file = 'data/tough_webgl_cases.json'
 
-  def CanRunOnBrowser(self, browser_info):
-    if not browser_info.HasWebGLSupport():
-      logging.warning('Browser does not support webgl, skipping test')
-      return False
-    return True
 
   def RunNavigateSteps(self, action_runner):
     super(ToughWebglCasesPage, self).RunNavigateSteps(action_runner)
