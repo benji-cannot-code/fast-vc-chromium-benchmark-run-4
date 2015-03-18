d4e25b715e99c45910ca13ea105615d67421e758
vmpstr <vmpstr@chromium.org>
2015-03-17 18:11:12 -0700

    cc: Use layer_impls to compute content rect in target space
    This patch changes the way we get damage for a layer to instead use
    the layer itself to compute content bounds in target space.
    
    BUG=463424
    R=danakj, enne
    
    Review URL: https://codereview.chromium.org/1015433002
    
    Cr-Commit-Position: refs/heads/master@{#321041}

--- cc/layers/painted_scrollbar_layer_impl.cc
+++ cc/layers/painted_scrollbar_layer_impl.cc
@@ -132,6 +132,10 @@ void PaintedScrollbarLayerImpl::AppendQuads(
   }
 }
 
+gfx::Rect PaintedScrollbarLayerImpl::GetEnclosingRectInTargetSpace() const {
+  return GetScaledEnclosingRectInTargetSpace(internal_contents_scale_);
+}
+
 void PaintedScrollbarLayerImpl::SetThumbThickness(int thumb_thickness) {
   if (thumb_thickness_ == thumb_thickness)
     return;
