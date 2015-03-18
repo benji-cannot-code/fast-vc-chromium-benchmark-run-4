eb287ed3449d235e887f75cb939fc38591eea89b
bustamante <bustamante@chromium.org>
2015-03-18 11:53:33 -0700

    Disable SafeBrowsingOff test for Android
    This test is designed to verify that safe browsing doesn't interfere on desktop and webview platforms, but shouldn't run for android where safe browsing is expected to work.
    
    BUG=468087
    
    Review URL: https://codereview.chromium.org/1003773003
    
    Cr-Commit-Position: refs/heads/master@{#321179}

--- tools/chrome_proxy/integration_tests/chrome_proxy_benchmark.py
+++ tools/chrome_proxy/integration_tests/chrome_proxy_benchmark.py
@@ -6,7 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from integration_tests import chrome_proxy_measurements as measurements
 from integration_tests import chrome_proxy_pagesets as pagesets
 from telemetry import benchmark
+from telemetry.core.backends.chrome import android_browser_finder
 
+
+ANDROID_CHROME_BROWSERS = [
+  browser for browser in android_browser_finder.CHROME_PACKAGE_NAMES
+    if 'webview' not in browser]
 
 class ChromeProxyLatency(benchmark.Benchmark):
   tag = 'latency'
@@ -150,7 +155,7 @@ class ChromeProxyBlockOnce(benchmark.Benchmark):
     return 'chrome_proxy_benchmark.block_once.block_once'
 
 
-@benchmark.Enabled('android')
+@benchmark.Enabled(*ANDROID_CHROME_BROWSERS)
 # Safebrowsing is enabled for Android and iOS.
 class ChromeProxySafeBrowsingOn(benchmark.Benchmark):
   tag = 'safebrowsing_on'
@@ -162,6 +167,7 @@ class ChromeProxySafeBrowsingOn(benchmark.Benchmark):
     return 'chrome_proxy_benchmark.safebrowsing_on.safebrowsing'
 
 
+@benchmark.Disabled(*ANDROID_CHROME_BROWSERS)
 # Safebrowsing is switched off for Android Webview and all desktop platforms.
 class ChromeProxySafeBrowsingOff(benchmark.Benchmark):
   tag = 'safebrowsing_off'
