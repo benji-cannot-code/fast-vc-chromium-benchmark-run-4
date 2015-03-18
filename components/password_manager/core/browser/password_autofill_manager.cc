abfa0eb078c37a7c039729d9e87109fba39adb91
engedy <engedy@chromium.org>
2015-03-18 08:28:14 -0700

    Prettify Android application realms in autofill suggestions.
    BUG=437865
    
    Review URL: https://codereview.chromium.org/1021453002
    
    Cr-Commit-Position: refs/heads/master@{#321137}

--- components/password_manager/core/browser/password_autofill_manager.cc
+++ components/password_manager/core/browser/password_autofill_manager.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/suggestion.h"
 #include "components/autofill/core/common/autofill_constants.h"
 #include "components/autofill/core/common/autofill_data_validation.h"
+#include "components/password_manager/core/browser/affiliation_utils.h"
 #include "components/password_manager/core/browser/password_manager_driver.h"
 #include "components/strings/grit/components_strings.h"
 #include "grit/components_strings.h"
@@ -46,6 +47,17 @@ base::string16 ReplaceEmptyUsername(const base::string16& username) {
   return username;
 }
 
+// Returns the prettified version of |signon_realm| to be displayed on the UI.
+base::string16 GetHumanReadableRealm(const std::string& signon_realm) {
+  // For Android application realms, remove the hash component. Otherwise, make
+  // no changes.
+  FacetURI maybe_facet_uri(FacetURI::FromPotentiallyInvalidSpec(signon_realm));
+  if (maybe_facet_uri.IsValidAndroidFacetURI())
+    return base::UTF8ToUTF16("android://" +
+                             maybe_facet_uri.android_package_name() + "/");
+  return base::UTF8ToUTF16(signon_realm);
+}
+
 // This function attempts to fill |suggestions| and |realms| form |fill_data|
 // based on |current_username|. Unless |show_all| is true, it only picks
 // suggestions where the username has |current_username| as a prefix.
@@ -57,7 +69,7 @@ void GetSuggestions(const autofill::PasswordFormFillData& fill_data,
       StartsWith(fill_data.username_field.value, current_username, false)) {
     autofill::Suggestion suggestion(
         ReplaceEmptyUsername(fill_data.username_field.value));
-    suggestion.label = base::UTF8ToUTF16(fill_data.preferred_realm);
+    suggestion.label = GetHumanReadableRealm(fill_data.preferred_realm);
     suggestion.frontend_id = autofill::POPUP_ITEM_ID_PASSWORD_ENTRY;
     suggestions->push_back(suggestion);
   }
@@ -65,7 +77,7 @@ void GetSuggestions(const autofill::PasswordFormFillData& fill_data,
   for (const auto& login : fill_data.additional_logins) {
     if (show_all || StartsWith(login.first, current_username, false)) {
       autofill::Suggestion suggestion(ReplaceEmptyUsername(login.first));
-      suggestion.label = base::UTF8ToUTF16(login.second.realm);
+      suggestion.label = GetHumanReadableRealm(login.second.realm);
       suggestion.frontend_id = autofill::POPUP_ITEM_ID_PASSWORD_ENTRY;
       suggestions->push_back(suggestion);
     }
@@ -77,7 +89,7 @@ void GetSuggestions(const autofill::PasswordFormFillData& fill_data,
           StartsWith(usernames.second[i], current_username, false)) {
         autofill::Suggestion suggestion(
             ReplaceEmptyUsername(usernames.second[i]));
-        suggestion.label = base::UTF8ToUTF16(usernames.first.realm);
+        suggestion.label = GetHumanReadableRealm(usernames.first.realm);
         suggestion.frontend_id = autofill::POPUP_ITEM_ID_PASSWORD_ENTRY;
         suggestions->push_back(suggestion);
       }
