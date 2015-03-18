b23503e54ed6cae0b7b455e4226109cde51c6bfc
dsinclair@chromium.org <dsinclair@chromium.org>
2015-03-18 16:07:49 +0000

    Change Renderer to LayoutObject in LayoutTreeBuilder.
    This converts the methods and variables in LayoutTreeBuilder
    over to LayoutObject from Renderer.
    
    BUG=462960
    
    Review URL: https://codereview.chromium.org/1020493002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192101 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/LayoutTreeBuilder.h
+++ third_party/WebKit/Source/core/dom/LayoutTreeBuilder.h
@@ -59,9 +59,9 @@ protected:
         // which does an updateLayoutIgnorePendingStylesheets.
     }
 
-    LayoutObject* parentRenderer() const { return m_layoutObjectParent; }
+    LayoutObject* parentLayoutObject() const { return m_layoutObjectParent; }
 
-    LayoutObject* nextRenderer() const
+    LayoutObject* nextLayoutObject() const
     {
         ASSERT(m_layoutObjectParent);
 
@@ -82,14 +82,14 @@ public:
 
     void createLayoutObjectIfNeeded()
     {
-        if (shouldCreateRenderer())
+        if (shouldCreateLayoutObject())
             createLayoutObject();
     }
 
 private:
-    LayoutObject* parentRenderer() const;
-    LayoutObject* nextRenderer() const;
-    bool shouldCreateRenderer() const;
+    LayoutObject* parentLayoutObject() const;
+    LayoutObject* nextLayoutObject() const;
+    bool shouldCreateLayoutObject() const;
     LayoutStyle& style() const;
     void createLayoutObject();
 
