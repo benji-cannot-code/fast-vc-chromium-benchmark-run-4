a8756eb0c5bc935ddd40627fd1cc609a5d0bcd13
lukasza <lukasza@chromium.org>
2015-03-18 08:27:17 -0700

    Basic Android UI for host-offline-reason.
    BUG=247245
    TEST=Manually verify UI changes in an emulator.
    
    Review URL: https://codereview.chromium.org/1003893003
    
    Cr-Commit-Position: refs/heads/master@{#321136}

--- remoting/android/java/src/org/chromium/chromoting/HostInfo.java
+++ remoting/android/java/src/org/chromium/chromoting/HostInfo.java
@@ -18,6 +18,7 @@ public class HostInfo {
     public final String jabberId;
     public final String publicKey;
     public final boolean isOnline;
+    public final String hostOfflineReason;
     private final ArrayList<String> mTokenUrlPatterns;
 
     public HostInfo(String name,
@@ -25,13 +26,15 @@ public class HostInfo {
                     String jabberId,
                     String publicKey,
                     ArrayList<String> tokenUrlPatterns,
-                    boolean isOnline) {
+                    boolean isOnline,
+                    String hostOfflineReason) {
         this.name = name;
         this.id = id;
         this.jabberId = jabberId;
         this.publicKey = publicKey;
         this.mTokenUrlPatterns = tokenUrlPatterns;
         this.isOnline = isOnline;
+        this.hostOfflineReason = hostOfflineReason;
     }
 
     public ArrayList<String> getTokenUrlPatterns() {
@@ -58,6 +61,7 @@ public class HostInfo {
                 json.optString("jabberId"),
                 json.optString("publicKey"),
                 tokenUrlPatterns,
-                json.optString("status").equals("ONLINE"));
+                json.optString("status").equals("ONLINE"),
+                json.optString("hostOfflineReason"));
     }
 }
