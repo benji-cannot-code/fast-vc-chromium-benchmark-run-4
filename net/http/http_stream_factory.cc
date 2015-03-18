59015d3f0251d86c48b2cb22d176f0fe682312e2
rch <rch@chromium.org>
2015-03-18 11:56:52 -0700

    Clear alternate protocols for an origin when an empty or invalid Alternate-Protocol header is processed.
    Review URL: https://codereview.chromium.org/1014043004
    
    Cr-Commit-Position: refs/heads/master@{#321180}

--- net/http/http_stream_factory.cc
+++ net/http/http_stream_factory.cc
@@ -37,6 +37,7 @@ void HttpStreamFactory::ProcessAlternateProtocol(
   AlternateProtocol protocol = UNINITIALIZED_ALTERNATE_PROTOCOL;
   int port = 0;
   double probability = 1;
+  bool is_valid = true;
   for (size_t i = 0; i < alternate_protocol_values.size(); ++i) {
     const std::string& alternate_protocol_str = alternate_protocol_values[i];
     if (StartsWithASCII(alternate_protocol_str, "p=", true)) {
@@ -46,7 +47,8 @@ void HttpStreamFactory::ProcessAlternateProtocol(
         DVLOG(1) << kAlternateProtocolHeader
                  << " header has unrecognizable probability: "
                  << alternate_protocol_values[i];
-        return;
+        is_valid = false;
+        break;
       }
       continue;
     }
@@ -57,7 +59,8 @@ void HttpStreamFactory::ProcessAlternateProtocol(
       DVLOG(1) << kAlternateProtocolHeader
                << " header has too many tokens: "
                << alternate_protocol_str;
-      return;
+      is_valid = false;
+      break;
     }
 
     if (!base::StringToInt(port_protocol_vector[0], &port) ||
@@ -65,7 +68,8 @@ void HttpStreamFactory::ProcessAlternateProtocol(
       DVLOG(1) << kAlternateProtocolHeader
                << " header has unrecognizable port: "
                << port_protocol_vector[0];
-      return;
+      is_valid = false;
+      break;
     }
 
     protocol = AlternateProtocolFromString(port_protocol_vector[1]);
@@ -75,12 +79,15 @@ void HttpStreamFactory::ProcessAlternateProtocol(
       DVLOG(1) << kAlternateProtocolHeader
                << " header has unrecognized protocol: "
                << port_protocol_vector[1];
-      return;
+      is_valid = false;
+      break;
     }
   }
 
-  if (protocol == UNINITIALIZED_ALTERNATE_PROTOCOL)
+  if (!is_valid || protocol == UNINITIALIZED_ALTERNATE_PROTOCOL) {
+    http_server_properties->ClearAlternateProtocol(http_host_port_pair);
     return;
+  }
 
   HostPortPair host_port(http_host_port_pair);
   const HostMappingRules* mapping_rules = GetHostMappingRules();
