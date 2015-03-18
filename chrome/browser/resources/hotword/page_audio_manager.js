bccc92db20504cf71f0ce3e0684b03ec7d65f6d7
amistry <amistry@chromium.org>
2015-03-17 19:48:52 -0700

    Optionally fill in the |available| field in hotword status result.
    Filling in |available| calls HotwordService::IsServiceAvailable, which
    may cause a blocking operation on the UI thread. This causes jank on
    Chrome startup.
    
    BUG=458397
    
    Review URL: https://codereview.chromium.org/1017443003
    
    Cr-Commit-Position: refs/heads/master@{#321063}

--- chrome/browser/resources/hotword/page_audio_manager.js
+++ chrome/browser/resources/hotword/page_audio_manager.js
@@ -426,6 +426,7 @@ cr.define('hotword', function() {
         case CommandFromPage.PAGE_WAKEUP:
           if (sender.tab && this.isEligibleUrl_(sender.tab.url)) {
             chrome.hotwordPrivate.getStatus(
+                true /* getOptionalFields */,
                 this.statusDone_.bind(
                     this,
                     request.tab || sender.tab || {incognito: true},
--- chrome/test/data/extensions/api_test/hotword_private/isAvailableNoGet/test.js
+++ chrome/test/data/extensions/api_test/hotword_private/isAvailableNoGet/test.js
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 chrome.test.runTests([
   function isAvailable() {
     chrome.hotwordPrivate.getStatus(
-        true,
         chrome.test.callbackPass(function(result) {
           chrome.test.sendMessage("available: " + result.available);
     }));
