cebde8863a0fda6bae253dbdd74ea0cf53cb3d94
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2015-03-19 21:04:48 +0000

    [S.P.] Invalidate the document element for view background when needed
    The document element draws the background of the view even if it has no
    specified background and/or the background is specified on <body>.
    
    Let LayoutObject::skipInvalidationWhenLaidOutChildren() return false
    for document element before checking rendererHasNoBoxEffect().
    
    BUG=468373
    TEST=some virtual/slimmingpaint/fast/repaint tests
    
    Review URL: https://codereview.chromium.org/1011393003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192195 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutObject.cpp
@@ -628,7 +628,10 @@ bool LayoutObject::skipInvalidationWhenLaidOutChildren() const
     if (hasNonCompositedScrollbars())
         return false;
 
-    return rendererHasNoBoxEffect();
+    if (RuntimeEnabledFeatures::slimmingPaintEnabled() && isDocumentElement())
+        return !hasBoxEffect();
+
+    return rendererHasNoBoxEffectObsolete();
 }
 
 LayoutBlock* LayoutObject::firstLineBlock() const
