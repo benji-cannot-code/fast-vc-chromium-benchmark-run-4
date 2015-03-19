b721b96b2378da71e5e9cf9ee870fe8bb396eaff
kjellander <kjellander@chromium.org>
2015-03-19 04:47:07 -0700

    Disable FrameTreeBrowserTest.NavigateWithLeftoverFrames on WinXP
    BUG=468713
    TBR=alexmos@chromium.org
    
    Review URL: https://codereview.chromium.org/1005573003
    
    Cr-Commit-Position: refs/heads/master@{#321336}

--- content/browser/frame_host/frame_tree_browsertest.cc
+++ content/browser/frame_host/frame_tree_browsertest.cc
@@ -22,6 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 
+// For fine-grained suppression on flaky tests.
+#if defined(OS_WIN)
+#include "base/win/windows_version.h"
+#endif
+
 namespace content {
 
 class FrameTreeBrowserTest : public ContentBrowserTest {
@@ -147,6 +152,11 @@ IN_PROC_BROWSER_TEST_F(FrameTreeBrowserTest, FrameTreeAfterCrash) {
 #define MAYBE_NavigateWithLeftoverFrames NavigateWithLeftoverFrames
 #endif
 IN_PROC_BROWSER_TEST_F(FrameTreeBrowserTest, MAYBE_NavigateWithLeftoverFrames) {
+#if defined(OS_WIN)
+  // Flaky on XP bot http://crbug.com/468713
+  if (base::win::GetVersion() <= base::win::VERSION_XP)
+    return;
+#endif
   GURL base_url = embedded_test_server()->GetURL("A.com", "/site_isolation/");
 
   NavigateToURL(shell(),
