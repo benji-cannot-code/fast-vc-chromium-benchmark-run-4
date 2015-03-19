b8fda40f21eb0a2d7c2cca5b08c9fb5934393948
ianwen <ianwen@chromium.org>
2015-03-19 14:03:43 -0700

    Force Enhanced Bookmark to be enabled if command line flags are set true
    kEnhancedBookmarksExperiment is used by android intrumentation tests to
    force enhanced bookmark feature to be enabled.
    https://codereview.chromium.org/1009673002 introduced a regression that
    the even if the flag is set to be true, it does not take effect. This CL
    fixes it.
    
    BUG=468106
    
    Review URL: https://codereview.chromium.org/1017913002
    
    Cr-Commit-Position: refs/heads/master@{#321429}

--- chrome/browser/bookmarks/enhanced_bookmarks_features.h
+++ chrome/browser/bookmarks/enhanced_bookmarks_features.h
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class PrefService;
 
-// Returns true and sets |extension_id| if enhanced bookmarks experiment is
-// enabled. Returns false if no bookmark experiment or extension id is empty.
-bool GetBookmarksExperimentExtensionID(std::string* extension_id);
-
 #if defined(OS_ANDROID)
 // Returns true if enhanced bookmark salient image prefetching is enabled.
 // This can be controlled by field trial.
@@ -23,8 +19,13 @@ bool IsEnhancedBookmarkImageFetchingEnabled(const PrefService* user_prefs);
 
 // Returns true if enhanced bookmarks is enabled.
 bool IsEnhancedBookmarksEnabled();
-
 #endif
+
+// Returns true and sets |extension_id| if enhanced bookmarks experiment is
+// enabled. Returns false if no bookmark experiment or extension id is empty.
+// Besides, this method takes commandline flags into account before trying
+// to get an extension_id.
+bool IsEnhancedBookmarksEnabled(std::string* extension_id);
 
 // Returns true when flag enable-dom-distiller is set or enabled from Finch.
 bool IsEnableDomDistillerSet();
