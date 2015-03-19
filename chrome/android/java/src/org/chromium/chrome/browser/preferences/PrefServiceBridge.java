54084d3dce5b572da5ae67e57d4537fa05d3cb4c
melandory <melandory@chromium.org>
2015-03-19 10:58:41 -0700

    Add auto sign in slider in Clank settings.
    BUG=454815
    
    Review URL: https://codereview.chromium.org/978913004
    
    Cr-Commit-Position: refs/heads/master@{#321390}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
@@ -189,11 +189,19 @@ public final class PrefServiceBridge {
         return nativeGetRememberPasswordsEnabled();
     }
 
+    public boolean isPasswordManagerAutoSigninEnabled() {
+        return nativeGetPasswordManagerAutoSigninEnabled();
+    }
+
     /**
      * @return whether password storage is configured by policy
      */
     public boolean isRememberPasswordsManaged() {
         return nativeGetRememberPasswordsManaged();
+    }
+
+    public boolean isPasswordManagerAutoSigninManaged() {
+        return nativeGetPasswordManagerAutoSigninManaged();
     }
 
     /**
@@ -543,6 +551,10 @@ public final class PrefServiceBridge {
         nativeSetRememberPasswordsEnabled(allow);
     }
 
+    public void setPasswordManagerAutoSigninEnabled(boolean enabled) {
+        nativeSetPasswordManagerAutoSigninEnabled(enabled);
+    }
+
     public void setPushNotificationsEnabled(boolean allow) {
         nativeSetPushNotificationsEnabled(allow);
     }
@@ -774,7 +786,9 @@ public final class PrefServiceBridge {
     private native boolean nativeGetBlockThirdPartyCookiesEnabled();
     private native boolean nativeGetBlockThirdPartyCookiesManaged();
     private native boolean nativeGetRememberPasswordsEnabled();
+    private native boolean nativeGetPasswordManagerAutoSigninEnabled();
     private native boolean nativeGetRememberPasswordsManaged();
+    private native boolean nativeGetPasswordManagerAutoSigninManaged();
     private native boolean nativeGetAllowLocationUserModifiable();
     private native boolean nativeGetAllowLocationManagedByCustodian();
     private native boolean nativeGetDoNotTrackEnabled();
@@ -808,6 +822,7 @@ public final class PrefServiceBridge {
     private native void nativeSetBlockThirdPartyCookiesEnabled(boolean enabled);
     private native void nativeSetDoNotTrackEnabled(boolean enabled);
     private native void nativeSetRememberPasswordsEnabled(boolean allow);
+    private native void nativeSetPasswordManagerAutoSigninEnabled(boolean enabled);
     private native void nativeSetProtectedMediaIdentifierEnabled(boolean enabled);
     private native boolean nativeGetAllowLocationEnabled();
     private native boolean nativeGetPushNotificationsEnabled();
