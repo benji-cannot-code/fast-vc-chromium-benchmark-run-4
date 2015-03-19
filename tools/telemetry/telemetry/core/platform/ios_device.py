c724184a58e7bcec6bb7bdedad451cdbdcada3c6
bustamante <bustamante@chromium.org>
2015-03-19 13:33:27 -0700

    Detect iOS simulator as an iOS test device.
    If on a MacOS machine, iossim and Chromium.app are built than we can
    launch and use them for running tests.
    
    BUG=468565
    
    Review URL: https://codereview.chromium.org/1015313002
    
    Cr-Commit-Position: refs/heads/master@{#321423}

--- tools/telemetry/telemetry/core/platform/ios_device.py
+++ tools/telemetry/telemetry/core/platform/ios_device.py
@@ -3,12 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
 import re
 import subprocess
 
 from telemetry.core import platform
 from telemetry.core.platform import device
+from telemetry.util import path
 
+
+IOSSIM_BUILD_DIRECTORIES = [
+    'Debug-iphonesimulator',
+    'Profile-iphonesimulator',
+    'Release-iphonesimulator'
+]
 
 class IOSDevice(device.Device):
   def __init__(self):
@@ -26,6 +34,25 @@ def _IsIosDeviceAttached():
       return True
   return False
 
+def _IsIosSimulatorAvailable():
+  """Determines whether an iOS simulator is present in the local checkout.
+
+  Assumes the iOS simulator (iossim) and Chromium have already been built.
+
+  Returns:
+    True if at least one simulator is found, otherwise False.
+  """
+  for build_dir in IOSSIM_BUILD_DIRECTORIES:
+    iossim_path = os.path.join(
+        path.GetChromiumSrcDir(), 'out', build_dir, 'iossim')
+    chromium_path = os.path.join(
+        path.GetChromiumSrcDir(), 'out', build_dir, 'Chromium.app')
+
+    # If the iOS simulator and Chromium app are present, return True
+    if os.path.exists(iossim_path) and os.path.exists(chromium_path):
+      return True
+
+  return False
 
 def FindAllAvailableDevices(_):
   """Returns a list of available devices.
@@ -35,7 +62,7 @@ def FindAllAvailableDevices(_):
   if platform.GetHostPlatform().GetOSName() != 'mac':
     return []
 
-  if not _IsIosDeviceAttached():
+  if not _IsIosDeviceAttached() and not _IsIosSimulatorAvailable():
     return []
 
   return [IOSDevice()]
