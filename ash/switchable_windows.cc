0ae339fb956a9887e58e32c24b235cfb119475cb
afakhry <afakhry@chromium.org>
2015-03-18 19:42:56 -0700

    AppList shouldn't be dismissed when an app is uninstalled
    When an app is being uninstalled and the "confirm removal" dialog is
    closed, the focus controller selects the most recently used window to
    return focus to. However, the MruWindowTracker doesn't take into
    consideration the app list container, and hence the app list window is
    not on the MRU window list. Focus is then returned to something else (if
    you have other windows open) which causes the dismissal of the App list.
    
    R=oshima@chromium.org,skuhne@chromium.org
    BUG=467877
    
    Review URL: https://codereview.chromium.org/1011283004
    
    Cr-Commit-Position: refs/heads/master@{#321276}

--- ash/switchable_windows.cc
+++ ash/switchable_windows.cc
@@ -11,8 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 const int kSwitchableWindowContainerIds[] = {
-    kShellWindowId_DefaultContainer, kShellWindowId_AlwaysOnTopContainer,
-    kShellWindowId_PanelContainer};
+    kShellWindowId_DefaultContainer,
+    kShellWindowId_AlwaysOnTopContainer,
+    kShellWindowId_PanelContainer,
+    kShellWindowId_AppListContainer};
 
 const size_t kSwitchableWindowContainerIdsLength =
     arraysize(kSwitchableWindowContainerIds);
