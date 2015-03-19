c6fd198d38458a9539765c4d993eb7b77e2cf764
trchen@chromium.org <trchen@chromium.org>
2015-03-19 04:06:25 +0000

    Invalidate DisplayItemClient when selection state changes
    Selection state affects whether selection highlight should be painted
    during the foreground phase, thus the object should be re-recorded.
    
    BUG=463608
    
    Review URL: https://codereview.chromium.org/1018503004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192135 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutObject.cpp
@@ -1263,6 +1263,9 @@ void LayoutObject::invalidateSelectionIfNeeded(const LayoutBoxModelObject& paint
     LayoutRect newSelectionRect = selectionRectForPaintInvalidation(&paintInvalidationContainer);
     setPreviousSelectionRectForPaintInvalidation(newSelectionRect);
 
+    if (RuntimeEnabledFeatures::slimmingPaintEnabled() && shouldInvalidateSelection())
+        invalidateDisplayItemClients(paintInvalidationContainer);
+
     if (fullInvalidation)
         return;
 
