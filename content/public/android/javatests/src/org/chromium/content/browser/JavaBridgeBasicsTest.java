0864295054ec56bd0fab20b49af31fc616606283
kjellander <kjellander@chromium.org>
2015-03-19 04:00:38 -0700

    Disable JavaBridgeBasicsTest#testRemovalNotReflectedUntilReload in ContentShellTest
    BUG=468679
    TBR=mnaganov@chromium.org
    
    Review URL: https://codereview.chromium.org/1018273002
    
    Cr-Commit-Position: refs/heads/master@{#321332}

--- content/public/android/javatests/src/org/chromium/content/browser/JavaBridgeBasicsTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/JavaBridgeBasicsTest.java
@@ -13,6 +13,7 @@ import junit.framework.Assert;
 
 import org.chromium.base.annotations.SuppressFBWarnings;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content.browser.test.util.TestCallbackHelperContainer;
 import org.chromium.content_public.browser.LoadUrlParams;
@@ -182,6 +183,7 @@ public class JavaBridgeBasicsTest extends JavaBridgeTestBase {
 
     @SmallTest
     @Feature({"AndroidWebView", "Android-JavaBridge"})
+    @DisabledTest //crbug/468679
     public void testRemovalNotReflectedUntilReload() throws Throwable {
         injectObjectAndReload(new Object() {
             public void method() {
