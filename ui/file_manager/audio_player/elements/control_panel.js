ccba0f90edd9134a7f51c1fc0945cb7e3c340379
yoshiki <yoshiki@chromium.org>
2015-03-19 07:49:49 -0700

    [Audio Player] Remove durationChanged hander in 'control-panel' element
    It's no longer necessary. And the handler is fired unexpected timing after unminimized and causes the issue (see the issue for detail). This patch removes it.
    
    BUG=462065
    TEST=browsertest passes
    
    Review URL: https://codereview.chromium.org/1018953002
    
    Cr-Commit-Position: refs/heads/master@{#321357}

--- ui/file_manager/audio_player/elements/control_panel.js
+++ ui/file_manager/audio_player/elements/control_panel.js
@@ -116,16 +116,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     playing: false,
 
     /**
-     * Invoked when the 'duration' property is changed.
-     * @param {number} oldValue old value.
-     * @param {number} newValue new value.
-     */
-    durationChanged: function(oldValue, newValue) {
-      // Reset the current playback position.
-      this.time = 0;
-    },
-
-    /**
      * Invoked when the next button is clicked.
      */
     nextClick: function() {
