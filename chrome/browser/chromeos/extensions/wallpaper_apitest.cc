cf630b21765193d81f397d4ba7f235b13f22df7f
benwells <benwells@chromium.org>
2015-03-18 23:33:02 -0700

    Disable ExtensionApiTest.Wallpaper as it's flaky.
    TBR=bshe@chromium.org
    BUG=468632
    
    Review URL: https://codereview.chromium.org/1004533003
    
    Cr-Commit-Position: refs/heads/master@{#321309}

--- chrome/browser/chromeos/extensions/wallpaper_apitest.cc
+++ chrome/browser/chromeos/extensions/wallpaper_apitest.cc
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "net/dns/mock_host_resolver.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Wallpaper) {
+// Disabled due to flakiness. See http://crbug.com/468632.
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Wallpaper) {
   host_resolver()->AddRule("a.com", "127.0.0.1");
   ASSERT_TRUE(StartEmbeddedTestServer());
   ASSERT_TRUE(RunExtensionTest("wallpaper")) << message_;
