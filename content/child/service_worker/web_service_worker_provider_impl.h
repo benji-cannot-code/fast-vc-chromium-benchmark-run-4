d4b21cb9e5910d0e809b46938340a6da1d5ce1d7
nhiroki <nhiroki@chromium.org>
2015-03-19 01:14:22 -0700

    ServiceWorker: Clean up WebServiceWorkerProviderImpl
    This CL...
    - removes unused getter and header inclusions
    - simplifies if-statement
    
    BUG=n/a
    TEST=compile
    
    Review URL: https://codereview.chromium.org/1023613002
    
    Cr-Commit-Position: refs/heads/master@{#321314}

--- content/child/service_worker/web_service_worker_provider_impl.h
+++ content/child/service_worker/web_service_worker_provider_impl.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
-#include "base/memory/scoped_ptr.h"
 #include "third_party/WebKit/public/platform/WebServiceWorkerProvider.h"
 
 namespace blink {
@@ -46,17 +45,12 @@ class WebServiceWorkerProviderImpl
   virtual void getRegistrationForReady(
       WebServiceWorkerGetRegistrationForReadyCallbacks*);
 
-  ServiceWorkerProviderContext* context() { return context_.get(); }
-
-  int provider_id() const { return provider_id_; }
-
  private:
   void RemoveProviderClient();
   ServiceWorkerDispatcher* GetDispatcher();
 
   scoped_refptr<ThreadSafeSender> thread_safe_sender_;
   scoped_refptr<ServiceWorkerProviderContext> context_;
-  const int provider_id_;
 
   DISALLOW_COPY_AND_ASSIGN(WebServiceWorkerProviderImpl);
 };
