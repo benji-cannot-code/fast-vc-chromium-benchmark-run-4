9ebea4e951eeb7d963088775a46900814b85cbc4
benwells <benwells@chromium.org>
2015-03-19 12:37:45 -0700

    Added test to ensure app banner settings are per-origin.
    The banner engagement persisted data should be per-origin. This test
    ensures that is the case.
    
    BUG=468148
    
    Review URL: https://codereview.chromium.org/1019763002
    
    Cr-Commit-Position: refs/heads/master@{#321409}

--- chrome/browser/banners/app_banner_settings_helper_unittest.cc
+++ chrome/browser/banners/app_banner_settings_helper_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const char kTestURL[] = "https://www.google.com";
+const char kSameOriginTestURL[] = "https://www.google.com/foo.html";
 const char kTestPackageName[] = "test.package";
 
 base::Time GetReferenceTime() {
@@ -289,5 +290,33 @@ TEST_F(AppBannerSettingsHelperTest, ShouldNotShowAfterAdding) {
       AppBannerSettingsHelper::APP_BANNER_EVENT_DID_ADD_TO_HOMESCREEN,
       one_year_ago);
   EXPECT_FALSE(AppBannerSettingsHelper::ShouldShowBanner(
+      web_contents(), url, kTestPackageName, reference_time));
+}
+
+TEST_F(AppBannerSettingsHelperTest, OperatesOnOrigins) {
+  GURL url(kTestURL);
+  NavigateAndCommit(url);
+
+  base::Time reference_time = GetReferenceTime();
+  base::Time one_day_ago = reference_time - base::TimeDelta::FromDays(1);
+
+  // By default the banner should not be shown.
+  EXPECT_FALSE(AppBannerSettingsHelper::ShouldShowBanner(
+      web_contents(), url, kTestPackageName, reference_time));
+
+  // Record events such that the banner should show.
+  AppBannerSettingsHelper::RecordBannerEvent(
+      web_contents(), url, kTestPackageName,
+      AppBannerSettingsHelper::APP_BANNER_EVENT_COULD_SHOW, one_day_ago);
+  AppBannerSettingsHelper::RecordBannerEvent(
+      web_contents(), url, kTestPackageName,
+      AppBannerSettingsHelper::APP_BANNER_EVENT_COULD_SHOW, reference_time);
+
+  // Navigate to another page on the same origin.
+  url = GURL(kSameOriginTestURL);
+  NavigateAndCommit(url);
+
+  // The banner should show as settings are per-origin.
+  EXPECT_TRUE(AppBannerSettingsHelper::ShouldShowBanner(
       web_contents(), url, kTestPackageName, reference_time));
 }
