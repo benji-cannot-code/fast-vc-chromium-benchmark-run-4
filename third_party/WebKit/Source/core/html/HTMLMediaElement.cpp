77c7a3ec505f8fa5f44bbfaed2f14d5bcbd7544b
philipj@opera.com <philipj@opera.com>
2015-03-19 10:09:59 +0000

    Document calls to the "rules for updating the text track rendering"
    Per spec there are only two calls to these rules:
     * The last step of the "time marches on" algorithm.
     * "When the user agent starts exposing a user interface for a video
       element."
    
    We have the first call, and two others that are needed when the text
    track mode changes between hidden and visible. A spec bug was filed
    for this case: https://www.w3.org/Bugs/Public/show_bug.cgi?id=28236
    
    BUG=322434
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1022743002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192148 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -3211,6 +3211,9 @@ void HTMLMediaElement::setClosedCaptionsVisible(bool closedCaptionVisible)
     honorUserPreferencesForAutomaticTextTrackSelection();
     m_processingPreferenceChange = false;
 
+    // As track visibility changed while m_processingPreferenceChange was set,
+    // there was no call to updateTextTrackDisplay(). This call is not in the
+    // spec, see the note in configureTextTrackDisplay().
     updateTextTrackDisplay();
 }
 
@@ -3349,6 +3352,13 @@ void HTMLMediaElement::configureTextTrackDisplay(VisibilityChangeAssumption assu
     mediaControls()->changedClosedCaptionsVisibility();
 
     cueTimeline().updateActiveCues(currentTime());
+
+    // Note: The "time marches on" algorithm (updateActiveCues) runs the "rules
+    // for updating the text track rendering" (updateTextTrackDisplay) only for
+    // "affected tracks", i.e. tracks where the the active cues have changed.
+    // This misses cues in tracks that changed mode between hidden and showing.
+    // This appears to be a spec bug, which we work around here:
+    // https://www.w3.org/Bugs/Public/show_bug.cgi?id=28236
     updateTextTrackDisplay();
 }
 
