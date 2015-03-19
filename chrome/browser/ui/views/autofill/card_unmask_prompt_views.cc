1d85058db5a6e7901b005ae40cd2d928c3bab13e
estade <estade@chromium.org>
2015-03-19 15:34:00 -0700

    Fix crash when user closes card unmasking dialog after verification success
    BUG=none
    
    Review URL: https://codereview.chromium.org/1019143003
    
    Cr-Commit-Position: refs/heads/master@{#321453}

--- chrome/browser/ui/views/autofill/card_unmask_prompt_views.cc
+++ chrome/browser/ui/views/autofill/card_unmask_prompt_views.cc
@@ -59,7 +59,8 @@ class CardUnmaskPromptViews : public CardUnmaskPromptView,
         error_label_(nullptr),
         storage_checkbox_(nullptr),
         progress_overlay_(nullptr),
-        progress_label_(nullptr) {}
+        progress_label_(nullptr),
+        weak_ptr_factory_(this) {}
 
   ~CardUnmaskPromptViews() override {
     if (controller_)
@@ -91,7 +92,7 @@ class CardUnmaskPromptViews : public CardUnmaskPromptView,
           IDS_AUTOFILL_CARD_UNMASK_VERIFICATION_SUCCESS));
       base::MessageLoop::current()->PostDelayedTask(
           FROM_HERE, base::Bind(&CardUnmaskPromptViews::ClosePrompt,
-                                base::Unretained(this)),
+                                weak_ptr_factory_.GetWeakPtr()),
           base::TimeDelta::FromSeconds(1));
     } else {
       // TODO(estade): it's somewhat jarring when the error comes back too
@@ -419,6 +420,8 @@ class CardUnmaskPromptViews : public CardUnmaskPromptView,
 
   views::View* progress_overlay_;
   views::Label* progress_label_;
+
+  base::WeakPtrFactory<CardUnmaskPromptViews> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(CardUnmaskPromptViews);
 };
