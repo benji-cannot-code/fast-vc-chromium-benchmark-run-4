74c70a078b2833458df664613bdb26390e949d62
philipj@opera.com <philipj@opera.com>
2015-03-19 14:27:28 +0000

    Eliminate TextTrackCue::updateDisplayTree()
    updateDisplayTree() cloned and inserted a new tree every time, and was
    called continuously from CueTimeline::updateActiveCues(). The net effect
    of this was to re-layout the cue continuously, causing cues to move into
    areas where a cue was previously. The letter and spirit of the WebVTT
    spec is to have a stable layout, where cues do not move once laid out.
    
    TEST=LayoutTests/media/track/track-webvtt-two-cue-layout-after-first-end.html
    
    As the display tree is now kept intact, it becomes necessary to
    invalidate the style in setIsPastNode.
    
    Test coverage of past and future cues:
    LayoutTests/media/track/cue-style-invalidation.html
    LayoutTests/media/track/track-css-matching-timestamps.html
    LayoutTests/media/track/track-css-matching.html
    
    Note that per spec a part of the cue could match neither :past nor
    :future, but our implementation is mutually exclusive. It's not clear
    what the best behavior is, so a spec bug was filed instead:
    https://www.w3.org/Bugs/Public/show_bug.cgi?id=28237
    
    BUG=322434
    
    Review URL: https://codereview.chromium.org/1013393004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192171 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/track/vtt/VTTElement.h
+++ third_party/WebKit/Source/core/html/track/vtt/VTTElement.h
@@ -55,7 +55,7 @@ public:
     VTTNodeType webVTTNodeType() const { return static_cast<VTTNodeType>(m_webVTTNodeType); }
 
     bool isPastNode() const { return m_isPastNode; }
-    void setIsPastNode(bool value) { m_isPastNode = value; }
+    void setIsPastNode(bool);
 
     virtual bool isVTTElement() const override { return true; }
     AtomicString language() const { return m_language; }
