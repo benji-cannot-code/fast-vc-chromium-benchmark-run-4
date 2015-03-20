e6f48cbba9c628f1943cd5967bd98873a3e4d99a
rouslan <rouslan@chromium.org>
2015-03-19 17:40:59 -0700

    [android] Accessibility for card unmask dialog.
    Announce "Verifying card". Announce the error message and focus the text
    field for recoverable errors.
    
    BUG=466699
    
    Review URL: https://codereview.chromium.org/1015013002
    
    Cr-Commit-Position: refs/heads/master@{#321478}

--- base/android/java/src/org/chromium/base/ApiCompatibilityUtils.java
+++ base/android/java/src/org/chromium/base/ApiCompatibilityUtils.java
@@ -481,4 +481,13 @@ public class ApiCompatibilityUtils {
             window.setStatusBarColor(statusBarColor);
         }
     }
+
+    /**
+     * @see android.view.View#announceForAccessibility(CharSequence text)
+     */
+    public static void announceForAccessibility(View view, CharSequence text) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            view.announceForAccessibility(text);
+        }
+    }
 }
