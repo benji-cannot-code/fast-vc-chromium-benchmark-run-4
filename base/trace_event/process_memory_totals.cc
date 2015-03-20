20d7f0f5805c17444049870f8f07cbd9685ec5d9
primiano <primiano@chromium.org>
2015-03-20 08:20:31 -0700

    [tracing] Improve the memory maps dumper generation format.
    This CL reworks the trace generation format for the memory maps
    dumper according to latest discussion in the design doc
    "Memory Dumping: Mmap and Smap data dumping details"
    (http://goo.gl/e6WRnQ).
    Furthermore this CL changes the output format of the total dumper to
    dump a hex string rather than a float, in order to be consistent with
    the mmaps dumper.
    
    See http://pastebin.com/EHdNDMXN for an example of the generated JSON.
    
    BUG=460884
    
    Review URL: https://codereview.chromium.org/947103003
    
    Cr-Commit-Position: refs/heads/master@{#321570}

--- base/trace_event/process_memory_totals.cc
+++ base/trace_event/process_memory_totals.cc
@@ -5,13 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/trace_event/process_memory_totals.h"
 
+#include "base/format_macros.h"
+#include "base/strings/stringprintf.h"
 #include "base/trace_event/trace_event_argument.h"
 
 namespace base {
 namespace trace_event {
 
 void ProcessMemoryTotals::AsValueInto(TracedValue* value) const {
-  value->SetDouble("resident_set_bytes", resident_set_bytes_);
+  value->SetString("resident_set_bytes",
+                   StringPrintf("%" PRIx64, resident_set_bytes_));
 }
 
 }  // namespace trace_event
