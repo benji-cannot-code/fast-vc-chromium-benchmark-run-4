d7c0873d69247029d7540f7744b91f5d2e35d471
garykac <garykac@chromium.org>
2015-03-19 19:31:39 -0700

    [Chromoting] Move app-specific code out of remoting.js
    After this cl, the last remaining reference to remoting.AppMode in AppRemoting is in app_remoting.js. That will be addressed in a followup cl.
    
    Move the following out of remoting.js and into app-specific locations:
    * remoting.signOut (into crd_event_handlers.js)
    * remoting.showErrorMessage (into desktop_remoting.js)
    * isWindowed_ (into desktop_remoting.js)
    * remoting.promptClose (into desktop_remoting.js)
    * remoting.isMe2MeInstallable (into local_host_section.js)
    
    Rename Delegate.signInFailed -> Delegate.handleAuthError
    Add Application.onAuthError to call Delegate.handleAuthError
    
    Change references to global remoting.showErrorMessage to remoting.app.onAuthError
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1020743002
    
    Cr-Commit-Position: refs/heads/master@{#321502}

--- remoting/webapp/crd/js/host_list.js
+++ remoting/webapp/crd/js/host_list.js
@@ -27,9 +27,11 @@ var remoting = remoting || {};
  * @param {HTMLElement} loadingIndicator The HTML <span> to update while the
  *     host list is being loaded. The first element of this span should be
  *     the reload button.
+ * @param {function(!remoting.Error)} onError Function to call when an error
+ *     occurs.
  */
 remoting.HostList = function(table, noHosts, errorMsg, errorButton,
-                             loadingIndicator) {
+                             loadingIndicator, onError) {
   /** @private {Element} */
   this.table_ = table;
   /**
@@ -44,6 +46,8 @@ remoting.HostList = function(table, noHosts, errorMsg, errorButton,
   this.errorButton_ = errorButton;
   /** @private {HTMLElement} */
   this.loadingIndicator_ = loadingIndicator;
+  this.onError_ = onError;
+
   /** @private {Array<remoting.HostTableEntry>} */
   this.hostTableEntries_ = [];
   /** @private {Array<remoting.Host>} */
@@ -54,7 +58,8 @@ remoting.HostList = function(table, noHosts, errorMsg, errorButton,
   this.localHostSection_ = new remoting.LocalHostSection(
       /** @type {HTMLElement} */ (document.querySelector('.daemon-control')),
       new remoting.LocalHostSection.Controller(
-          this, new remoting.HostSetupDialog(remoting.hostController)));
+          this,
+          new remoting.HostSetupDialog(remoting.hostController, onError)));
 
   /** @private {number} */
   this.webappMajorVersion_ = parseInt(chrome.runtime.getManifest().version, 10);
@@ -296,7 +301,7 @@ remoting.HostList.prototype.deleteHost_ = function(hostTableEntry) {
     this.hostTableEntries_.splice(index, 1);
   }
   remoting.hostListApi.remove(hostTableEntry.host.hostId, base.doNothing,
-                              remoting.showErrorMessage);
+                              this.onError_);
 };
 
 /**
@@ -317,7 +322,7 @@ remoting.HostList.prototype.renameHost = function(hostTableEntry) {
                            hostTableEntry.host.hostName,
                            hostTableEntry.host.publicKey,
                            function() {},
-                           remoting.showErrorMessage);
+                           this.onError_);
 };
 
 /**
@@ -344,7 +349,7 @@ remoting.HostList.prototype.unregisterHostById = function(hostId, opt_onDone) {
       onDone();
     });
   };
-  remoting.hostListApi.remove(hostId, onRemoved, remoting.showErrorMessage);
+  remoting.hostListApi.remove(hostId, onRemoved, this.onError_);
 };
 
 /**
