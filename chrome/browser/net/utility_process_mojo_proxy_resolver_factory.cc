5b9154dc158fadf74ca706c7d9400a14ae6170ec
amistry <amistry@chromium.org>
2015-03-19 19:07:04 -0700

    Add a name to the proxy resolver utility process and display it in the task manager.
    BUG=467832
    
    Review URL: https://codereview.chromium.org/960753003
    
    Cr-Commit-Position: refs/heads/master@{#321495}

--- chrome/browser/net/utility_process_mojo_proxy_resolver_factory.cc
+++ chrome/browser/net/utility_process_mojo_proxy_resolver_factory.cc
@@ -7,11 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/singleton.h"
+#include "chrome/grit/generated_resources.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/utility_process_host.h"
 #include "content/public/browser/utility_process_host_client.h"
 #include "content/public/common/service_registry.h"
 #include "net/proxy/mojo_proxy_resolver_factory.h"
+#include "ui/base/l10n/l10n_util.h"
 
 // static
 UtilityProcessMojoProxyResolverFactory*
@@ -38,6 +40,8 @@ void UtilityProcessMojoProxyResolverFactory::CreateProcessAndConnect() {
       content::UtilityProcessHost::Create(
           scoped_refptr<content::UtilityProcessHostClient>(),
           base::MessageLoopProxy::current().get());
+  utility_process_host->SetName(l10n_util::GetStringUTF8(
+      IDS_UTILITY_PROCESS_PROXY_RESOLVER_NAME));
   bool process_started = utility_process_host->StartMojoMode();
   if (process_started) {
     content::ServiceRegistry* service_registry =
--- content/browser/utility_process_host_impl.cc
+++ content/browser/utility_process_host_impl.cc
@@ -109,7 +109,8 @@ UtilityProcessHostImpl::UtilityProcessHostImpl(
 #else
       child_flags_(ChildProcessHost::CHILD_NORMAL),
 #endif
-      started_(false) {
+      started_(false),
+      name_("utility process") {
 }
 
 UtilityProcessHostImpl::~UtilityProcessHostImpl() {
@@ -192,6 +193,10 @@ ServiceRegistry* UtilityProcessHostImpl::GetServiceRegistry() {
   return mojo_application_host_->service_registry();
 }
 
+void UtilityProcessHostImpl::SetName(const std::string& name) {
+  name_ = name;
+}
+
 bool UtilityProcessHostImpl::StartProcess() {
   if (started_)
     return true;
@@ -203,7 +208,7 @@ bool UtilityProcessHostImpl::StartProcess() {
   // Name must be set or metrics_service will crash in any test which
   // launches a UtilityProcessHost.
   process_.reset(new BrowserChildProcessHostImpl(PROCESS_TYPE_UTILITY, this));
-  process_->SetName(base::ASCIIToUTF16("utility process"));
+  process_->SetName(base::UTF8ToUTF16(name_));
 
   std::string channel_id = process_->GetHost()->CreateChannel();
   if (channel_id.empty())
