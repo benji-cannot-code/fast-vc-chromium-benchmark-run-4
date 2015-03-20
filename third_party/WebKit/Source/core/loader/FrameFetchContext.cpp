b2c924db4e55ee91936db9be083437f371357e1d
mkwst@chromium.org <mkwst@chromium.org>
2015-03-20 16:01:19 +0000

    Upgrade: Drop the 'upgraded' signal, expand the 'https' signal.
    Based on further conversation, we're dropping the 'Upgraded' header
    entirely, and expanding the 'HTTPS' header (nee 'Prefer: tls') to
    cover hosts that haven't opted-into preloadable HSTS[1].
    
    [1]: https://github.com/w3c/webappsec/commit/f8a1183b014697c918e26c80df0523a977dc3a9e
    
    BUG=455674
    
    Review URL: https://codereview.chromium.org/1022093002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192260 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
+++ third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
@@ -636,10 +636,10 @@ void FrameFetchContext::upgradeInsecureRequest(FetchRequest& fetchRequest)
 
     KURL url = fetchRequest.resourceRequest().url();
 
-    // Tack a 'Prefer' header to outgoing navigational requests, as described in
+    // Tack an 'HTTPS' header to outgoing navigational requests, as described in
     // https://w3c.github.io/webappsec/specs/upgrade/#feature-detect
-    if (fetchRequest.resourceRequest().frameType() != WebURLRequest::FrameTypeNone && !SecurityOrigin::isSecure(url))
-        fetchRequest.mutableResourceRequest().addHTTPHeaderField("Prefer", "tls");
+    if (fetchRequest.resourceRequest().frameType() != WebURLRequest::FrameTypeNone)
+        fetchRequest.mutableResourceRequest().addHTTPHeaderField("HTTPS", "1");
 
     if (m_document->insecureRequestsPolicy() == SecurityContext::InsecureRequestsUpgrade && url.protocolIs("http")) {
         ASSERT(m_document->insecureNavigationsToUpgrade());
@@ -659,7 +659,6 @@ void FrameFetchContext::upgradeInsecureRequest(FetchRequest& fetchRequest)
             if (url.port() == 80)
                 url.setPort(443);
             fetchRequest.mutableResourceRequest().setURL(url);
-            fetchRequest.mutableResourceRequest().addHTTPHeaderField("Upgraded", "1");
         }
     }
 }
