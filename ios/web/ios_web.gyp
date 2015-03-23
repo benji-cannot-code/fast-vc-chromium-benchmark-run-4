951b653f38273c2bada86f31500b9046c03c02e9
droger <droger@chromium.org>
2015-03-23 10:51:03 -0700

    [iOS] Upstream files in //ios/chrome/browser/autofill
    BUG=437508
    
    Review URL: https://codereview.chromium.org/1022463002
    
    Cr-Commit-Position: refs/heads/master@{#321805}

--- ios/web/ios_web.gyp
+++ ios/web/ios_web.gyp
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'public/web_state/crw_web_controller_observer.h',
         'public/web_state/crw_web_delegate.h',
         'public/web_state/crw_web_user_interface_delegate.h'
+        'public/web_state/crw_web_view_proxy.h'
         'public/web_state/js/crw_js_base_manager.h',
         'public/web_state/js/crw_js_early_script_manager.h',
         'public/web_state/js/crw_js_injection_evaluator.h',
--- ios/web/public/test/test_web_state.cc
+++ ios/web/public/test/test_web_state.cc
@@ -7,6 +7,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web {
 
+TestWebState::TestWebState() : trust_level_(kAbsolute), content_is_html_(true) {
+}
+
+TestWebState::~TestWebState() = default;
+
+UIView* TestWebState::GetView() {
+  return nullptr;
+}
+
+WebViewType TestWebState::GetWebViewType() const {
+  return web::UI_WEB_VIEW_TYPE;
+}
+
 BrowserState* TestWebState::GetBrowserState() const {
   return nullptr;
 }
@@ -28,7 +41,7 @@ const std::string& TestWebState::GetContentLanguageHeader() const {
 }
 
 bool TestWebState::ContentIsHTML() const {
-  return true;
+  return content_is_html_;
 }
 
 const GURL& TestWebState::GetVisibleURL() const {
@@ -37,6 +50,35 @@ const GURL& TestWebState::GetVisibleURL() const {
 
 const GURL& TestWebState::GetLastCommittedURL() const {
   return url_;
+}
+
+GURL TestWebState::GetCurrentURL(URLVerificationTrustLevel* trust_level) const {
+  *trust_level = trust_level_;
+  return url_;
+}
+
+bool TestWebState::IsShowingWebInterstitial() const {
+  return false;
+}
+
+WebInterstitial* TestWebState::GetWebInterstitial() const {
+  return nullptr;
+}
+
+void TestWebState::SetContentIsHTML(bool content_is_html) {
+  content_is_html_ = content_is_html;
+}
+
+void TestWebState::SetCurrentURL(const GURL& url) {
+  url_ = url;
+}
+
+void TestWebState::SetTrustLevel(URLVerificationTrustLevel trust_level) {
+  trust_level_ = trust_level;
+}
+
+CRWWebViewProxyType TestWebState::GetWebViewProxy() const {
+  return nullptr;
 }
 
 }  // namespace web
