db95fec0f82e334886580e6df1dee0f1d1a1f12f
vasilii <vasilii@chromium.org>
2015-03-23 10:01:30 -0700

    Show the password bubble even if the browser window has no focus.
    The bubble itself steals no focus. It's very important to show it for the new Credential Manager API. The user can be presented with the account chooser or the auto sign-in toast. The auto sign-in toast should stay visible while the browser isn't activated. Then it can be closed by timeout.
    
    BUG=468281
    
    Review URL: https://codereview.chromium.org/1016003002
    
    Cr-Commit-Position: refs/heads/master@{#321795}

--- chrome/browser/ui/views/passwords/manage_passwords_bubble_view.cc
+++ chrome/browser/ui/views/passwords/manage_passwords_bubble_view.cc
@@ -38,12 +38,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/layout_constants.h"
 #include "ui/views/widget/widget.h"
 
+int ManagePasswordsBubbleView::auto_signin_toast_timeout_ = 3;
 
 // Helpers --------------------------------------------------------------------
 
 namespace {
 
-const int kAutoSigninToastTimeout = 3;
 const int kDesiredBubbleWidth = 370;
 
 enum ColumnSetType {
@@ -196,6 +196,8 @@ class ManagePasswordsBubbleView::AccountChooserView
 
   ManagePasswordsBubbleView* parent_;
   views::LabelButton* cancel_button_;
+
+  DISALLOW_COPY_AND_ASSIGN(AccountChooserView);
 };
 
 ManagePasswordsBubbleView::AccountChooserView::AccountChooserView(
@@ -273,7 +275,8 @@ void ManagePasswordsBubbleView::AccountChooserView::ButtonPressed(
 // in.
 class ManagePasswordsBubbleView::AutoSigninView
     : public views::View,
-      public views::ButtonListener {
+      public views::ButtonListener,
+      public views::WidgetObserver {
  public:
   explicit AutoSigninView(ManagePasswordsBubbleView* parent);
 
@@ -281,15 +284,28 @@ class ManagePasswordsBubbleView::AutoSigninView
   // views::ButtonListener:
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
 
+  // views::WidgetObserver:
+  // Tracks the state of the browser window.
+  void OnWidgetActivationChanged(views::Widget* widget, bool active) override;
+  void OnWidgetClosing(views::Widget* widget) override;
+
   void OnTimer();
+  static base::TimeDelta GetTimeout() {
+    return base::TimeDelta::FromSeconds(
+        ManagePasswordsBubbleView::auto_signin_toast_timeout_);
+  }
 
   base::OneShotTimer<AutoSigninView> timer_;
   ManagePasswordsBubbleView* parent_;
+  ScopedObserver<views::Widget, views::WidgetObserver> observed_browser_;
+
+  DISALLOW_COPY_AND_ASSIGN(AutoSigninView);
 };
 
 ManagePasswordsBubbleView::AutoSigninView::AutoSigninView(
     ManagePasswordsBubbleView* parent)
-    : parent_(parent) {
+    : parent_(parent),
+      observed_browser_(this) {
   SetLayoutManager(new views::FillLayout);
   CredentialsItemView* credential = new CredentialsItemView(
       this,
@@ -300,14 +316,31 @@ ManagePasswordsBubbleView::AutoSigninView::AutoSigninView(
   AddChildView(credential);
   parent_->set_initially_focused_view(credential);
 
-  timer_.Start(FROM_HERE, base::TimeDelta::FromSeconds(kAutoSigninToastTimeout),
-               this, &AutoSigninView::OnTimer);
+  Browser* browser =
+      chrome::FindBrowserWithWebContents(parent_->web_contents());
+  DCHECK(browser);
+  BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser);
+  observed_browser_.Add(browser_view->GetWidget());
+
+  if (browser_view->IsActive())
+    timer_.Start(FROM_HERE, GetTimeout(), this, &AutoSigninView::OnTimer);
 }
 
 void ManagePasswordsBubbleView::AutoSigninView::ButtonPressed(
     views::Button* sender, const ui::Event& event) {
   parent_->model()->OnAutoSignInClicked();
   parent_->Close();
+}
+
+void ManagePasswordsBubbleView::AutoSigninView::OnWidgetActivationChanged(
+    views::Widget* widget, bool active) {
+  if (active && !timer_.IsRunning())
+    timer_.Start(FROM_HERE, GetTimeout(), this, &AutoSigninView::OnTimer);
+}
+
+void ManagePasswordsBubbleView::AutoSigninView::OnWidgetClosing(
+    views::Widget* widget) {
+  observed_browser_.RemoveAll();
 }
 
 void ManagePasswordsBubbleView::AutoSigninView::OnTimer() {
@@ -344,6 +377,8 @@ class ManagePasswordsBubbleView::PendingView : public views::View,
   // delete the model _after_ the combobox itself is deleted.
   scoped_ptr<SavePasswordRefusalComboboxModel> combobox_model_;
   scoped_ptr<views::Combobox> refuse_combobox_;
+
+  DISALLOW_COPY_AND_ASSIGN(PendingView);
 };
 
 ManagePasswordsBubbleView::PendingView::PendingView(
@@ -535,6 +570,8 @@ class ManagePasswordsBubbleView::ConfirmNeverView
 
   views::LabelButton* confirm_button_;
   views::LabelButton* undo_button_;
+
+  DISALLOW_COPY_AND_ASSIGN(ConfirmNeverView);
 };
 
 ManagePasswordsBubbleView::ConfirmNeverView::ConfirmNeverView(
@@ -623,6 +660,8 @@ class ManagePasswordsBubbleView::ManageView : public views::View,
 
   views::Link* manage_link_;
   views::LabelButton* done_button_;
+
+  DISALLOW_COPY_AND_ASSIGN(ManageView);
 };
 
 ManagePasswordsBubbleView::ManageView::ManageView(
@@ -724,6 +763,8 @@ class ManagePasswordsBubbleView::ManageAccountsView
 
   views::Link* manage_link_;
   views::LabelButton* done_button_;
+
+  DISALLOW_COPY_AND_ASSIGN(ManageAccountsView);
 };
 
 ManagePasswordsBubbleView::ManageAccountsView::ManageAccountsView(
@@ -815,6 +856,8 @@ class ManagePasswordsBubbleView::BlacklistedView
 
   views::BlueButton* unblacklist_button_;
   views::LabelButton* done_button_;
+
+  DISALLOW_COPY_AND_ASSIGN(BlacklistedView);
 };
 
 ManagePasswordsBubbleView::BlacklistedView::BlacklistedView(
@@ -899,8 +942,9 @@ class ManagePasswordsBubbleView::SaveConfirmationView
                               int event_flags) override;
 
   ManagePasswordsBubbleView* parent_;
-
   views::LabelButton* ok_button_;
+
+  DISALLOW_COPY_AND_ASSIGN(SaveConfirmationView);
 };
 
 ManagePasswordsBubbleView::SaveConfirmationView::SaveConfirmationView(
