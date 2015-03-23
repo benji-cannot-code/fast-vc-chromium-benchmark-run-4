4b3b794f0c75e9de5f7434db78b961f08e0f1ed4
thestig <thestig@chromium.org>
2015-03-23 15:24:48 -0700

    Autofill: Refactor some FormFieldData pickling code.
    Review URL: https://codereview.chromium.org/1023413002
    
    Cr-Commit-Position: refs/heads/master@{#321876}

--- components/autofill/core/common/form_field_data_unittest.cc
+++ components/autofill/core/common/form_field_data_unittest.cc
@@ -12,24 +12,57 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+namespace {
+
+void FillCommonFields(FormFieldData* data) {
+  data->label = base::ASCIIToUTF16("label");
+  data->name = base::ASCIIToUTF16("name");
+  data->value = base::ASCIIToUTF16("value");
+  data->form_control_type = "password";
+  data->autocomplete_attribute = "off";
+  data->max_length = 200;
+  data->is_autofilled = true;
+  data->is_checked = true;
+  data->is_checkable = true;
+  data->is_focusable = true;
+  data->should_autocomplete = false;
+  data->text_direction = base::i18n::RIGHT_TO_LEFT;
+  data->option_values.push_back(base::ASCIIToUTF16("First"));
+  data->option_values.push_back(base::ASCIIToUTF16("Second"));
+  data->option_contents.push_back(base::ASCIIToUTF16("First"));
+  data->option_contents.push_back(base::ASCIIToUTF16("Second"));
+}
+
+void WriteCommonSection1(const FormFieldData& data, Pickle* pickle) {
+  pickle->WriteString16(data.label);
+  pickle->WriteString16(data.name);
+  pickle->WriteString16(data.value);
+  pickle->WriteString(data.form_control_type);
+  pickle->WriteString(data.autocomplete_attribute);
+  pickle->WriteSizeT(data.max_length);
+  pickle->WriteBool(data.is_autofilled);
+  pickle->WriteBool(data.is_checked);
+  pickle->WriteBool(data.is_checkable);
+  pickle->WriteBool(data.is_focusable);
+  pickle->WriteBool(data.should_autocomplete);
+}
+
+void WriteCommonSection2(const FormFieldData& data, Pickle* pickle) {
+  pickle->WriteInt(data.text_direction);
+  pickle->WriteInt(static_cast<int>(data.option_values.size()));
+  for (auto s : data.option_values)
+    pickle->WriteString16(s);
+  pickle->WriteInt(static_cast<int>(data.option_contents.size()));
+  for (auto s : data.option_contents)
+    pickle->WriteString16(s);
+}
+
+}  // namespace
+
 TEST(FormFieldDataTest, SerializeAndDeserialize) {
   FormFieldData data;
-  data.label = base::ASCIIToUTF16("label");
-  data.name = base::ASCIIToUTF16("name");
-  data.value = base::ASCIIToUTF16("value");
-  data.form_control_type = "password";
-  data.autocomplete_attribute = "off";
-  data.max_length = 200;
-  data.is_autofilled = true;
-  data.is_checked = true;
-  data.is_checkable = true;
-  data.is_focusable = true;
-  data.should_autocomplete = false;
-  data.text_direction = base::i18n::RIGHT_TO_LEFT;
-  data.option_values.push_back(base::ASCIIToUTF16("First"));
-  data.option_values.push_back(base::ASCIIToUTF16("Second"));
-  data.option_contents.push_back(base::ASCIIToUTF16("First"));
-  data.option_contents.push_back(base::ASCIIToUTF16("Second"));
+  FillCommonFields(&data);
+  data.role = FormFieldData::ROLE_ATTRIBUTE_PRESENTATION;
 
   Pickle pickle;
   SerializeFormFieldData(data, &pickle);
@@ -43,43 +76,12 @@ TEST(FormFieldDataTest, SerializeAndDeserialize) {
 
 TEST(FormFieldDataTest, DeserializeVersion1) {
   FormFieldData data;
-  data.label = base::ASCIIToUTF16("label");
-  data.name = base::ASCIIToUTF16("name");
-  data.value = base::ASCIIToUTF16("value");
-  data.form_control_type = "password";
-  data.autocomplete_attribute = "off";
-  data.max_length = 200;
-  data.is_autofilled = true;
-  data.is_checked = true;
-  data.is_checkable = true;
-  data.is_focusable = true;
-  data.should_autocomplete = false;
-  data.text_direction = base::i18n::RIGHT_TO_LEFT;
-  data.option_values.push_back(base::ASCIIToUTF16("First"));
-  data.option_values.push_back(base::ASCIIToUTF16("Second"));
-  data.option_contents.push_back(base::ASCIIToUTF16("First"));
-  data.option_contents.push_back(base::ASCIIToUTF16("Second"));
+  FillCommonFields(&data);
 
   Pickle pickle;
   pickle.WriteInt(1);
-  pickle.WriteString16(data.label);
-  pickle.WriteString16(data.name);
-  pickle.WriteString16(data.value);
-  pickle.WriteString(data.form_control_type);
-  pickle.WriteString(data.autocomplete_attribute);
-  pickle.WriteSizeT(data.max_length);
-  pickle.WriteBool(data.is_autofilled);
-  pickle.WriteBool(data.is_checked);
-  pickle.WriteBool(data.is_checkable);
-  pickle.WriteBool(data.is_focusable);
-  pickle.WriteBool(data.should_autocomplete);
-  pickle.WriteInt(data.text_direction);
-  pickle.WriteInt(static_cast<int>(data.option_values.size()));
-  for (auto s: data.option_values)
-    pickle.WriteString16(s);
-  pickle.WriteInt(static_cast<int>(data.option_contents.size()));
-  for (auto s: data.option_contents)
-    pickle.WriteString16(s);
+  WriteCommonSection1(data, &pickle);
+  WriteCommonSection2(data, &pickle);
 
   PickleIterator iter(pickle);
   FormFieldData actual;
