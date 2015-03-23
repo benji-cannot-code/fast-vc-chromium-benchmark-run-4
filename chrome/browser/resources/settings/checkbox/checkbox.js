31bab6b9eb26c0343000a83c51ac239499a692d5
jlklein <jlklein@chromium.org>
2015-03-22 21:41:47 -0700

    Add a cr-settings-checkbox element.
    BUG=469551
    
    Review URL: https://codereview.chromium.org/1025113002
    
    Cr-Commit-Position: refs/heads/master@{#321735}

---
+++ chrome/browser/resources/settings/checkbox/checkbox.js
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * `cr-settings-checkbox` is a checkbox that controls a supplied preference.
+ *
+ * Example:
+ *      <cr-settings-checkbox pref="{{prefs.settings.enableFoo}}"
+ *          label="Enable foo setting." subLabel="(bar also)">
+ *      </cr-settings-checkbox>
+ *
+ * @element cr-settings-checkbox
+ */
+Polymer({
+  publish: {
+    /**
+     * The boolean preference to control.
+     *
+     * @attribute pref
+     * @type {Object}
+     * @default null
+     */
+    pref: null,
+
+    /**
+     * @attribute label
+     * @type {string}
+     * @default ''
+     */
+    label: '',
+
+    /**
+     * @attribute label
+     * @type {string}
+     * @default ''
+     */
+    subLabel: '',
+  },
+
+  ready: function() {
+    this.$.events.forward(this.$.checkbox, ['change']);
+  },
+});
