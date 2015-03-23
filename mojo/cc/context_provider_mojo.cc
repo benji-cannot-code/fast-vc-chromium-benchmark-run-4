6f8703b0078c9d9db65f8e221c4af9a465b6bccc
alhaad <alhaad@chromium.org>
2015-03-23 15:57:58 -0700

    Modify build_gles2_cmd_buffer.py to generate a class that implements gpu::GLES2Interace and for each entry point either defers to Mojo C thunk if we expose an entry point or check fails.
    The code in its current form makes use of MojoGLES2GetGLES2Interface if
    html_viewer makes use but we do not have a Mojo C thunk.
    I'll be adding Mojo C thunks for other functions used by html_viewer and
    eventually remove MojoGLES2GetGLES2Interface.
    
    R=jamesr@chromium.org,davemoore@chromium.org
    BUG=453591
    
    Review URL: https://codereview.chromium.org/993253003
    
    Cr-Commit-Position: refs/heads/master@{#321886}

--- mojo/cc/context_provider_mojo.cc
+++ mojo/cc/context_provider_mojo.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/cc/context_provider_mojo.h"
 
 #include "base/logging.h"
+#include "mojo/gpu/mojo_gles2_impl_autogen.h"
 #include "third_party/mojo/src/mojo/public/cpp/environment/environment.h"
 
 namespace mojo {
@@ -22,14 +23,12 @@ bool ContextProviderMojo::BindToCurrentThread() {
                                     &ContextLostThunk,
                                     this,
                                     Environment::GetDefaultAsyncWaiter());
+  context_gl_.reset(new MojoGLES2Impl(context_));
   return !!context_;
 }
 
 gpu::gles2::GLES2Interface* ContextProviderMojo::ContextGL() {
-  if (!context_)
-    return NULL;
-  return static_cast<gpu::gles2::GLES2Interface*>(
-      MojoGLES2GetGLES2Interface(context_));
+  return context_gl_.get();
 }
 
 gpu::ContextSupport* ContextProviderMojo::ContextSupport() {
@@ -58,6 +57,7 @@ bool ContextProviderMojo::IsContextLost() {
 bool ContextProviderMojo::DestroyedOnMainThread() { return !context_; }
 
 ContextProviderMojo::~ContextProviderMojo() {
+  context_gl_.reset();
   if (context_)
     MojoGLES2DestroyContext(context_);
 }
