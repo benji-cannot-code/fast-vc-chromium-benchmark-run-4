ce6dd73ba867c7409c9f1585f8d9e4871ba6c342
dgozman <dgozman@chromium.org>
2015-03-23 02:52:17 -0700

    [DevTools] Cleanup after moving from Page to Emulation.
    BUG=467059
    
    Review URL: https://codereview.chromium.org/1019533002
    
    Cr-Commit-Position: refs/heads/master@{#321752}

--- content/browser/devtools/protocol/page_handler.h
+++ content/browser/devtools/protocol/page_handler.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/output/compositor_frame_metadata.h"
 #include "content/browser/devtools/protocol/devtools_protocol_handler.h"
 #include "content/public/browser/readback_types.h"
-#include "third_party/WebKit/public/web/WebDeviceEmulationParams.h"
 
 class SkBitmap;
 
@@ -65,30 +64,9 @@ class PageHandler {
 
   Response NavigateToHistoryEntry(int entry_id);
 
-  Response SetGeolocationOverride(double* latitude,
-                                  double* longitude,
-                                  double* accuracy);
-
-  Response ClearGeolocationOverride();
-
-  Response SetTouchEmulationEnabled(bool enabled,
-                                    const std::string* configuration);
-
   Response CaptureScreenshot(DevToolsCommandId command_id);
 
-  Response CanEmulate(bool* result);
-  Response SetDeviceMetricsOverride(int width,
-                                    int height,
-                                    double device_scale_factor,
-                                    bool mobile,
-                                    bool fit_window,
-                                    const double* optional_scale,
-                                    const double* optional_offset_x,
-                                    const double* optional_offset_y);
-  Response ClearDeviceMetricsOverride();
-
   Response CanScreencast(bool* result);
-
   Response StartScreencast(const std::string* format,
                            const int* quality,
                            const int* max_width,
@@ -108,9 +86,6 @@ class PageHandler {
   Response SetColorPickerEnabled(bool enabled);
 
  private:
-  void UpdateTouchEventEmulationState();
-  void UpdateDeviceEmulationState();
-
   void NotifyScreencastVisibility(bool visible);
   void InnerSwapCompositorFrame();
   void ScreencastFrameCaptured(const cc::CompositorFrameMetadata& metadata,
@@ -131,12 +106,6 @@ class PageHandler {
       scoped_refptr<StopRecordingFramesResponse> response_data);
 
   bool enabled_;
-
-  bool touch_emulation_enabled_;
-  std::string touch_emulation_configuration_;
-
-  bool device_emulation_enabled_;
-  blink::WebDeviceEmulationParams device_emulation_params_;
 
   bool screencast_enabled_;
   std::string screencast_format_;
