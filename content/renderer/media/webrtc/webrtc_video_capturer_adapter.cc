4f2cf43f61bf45a332eab841a9e538d3326f4d7d
guoweis <guoweis@chromium.org>
2015-03-23 15:21:29 -0700

    With the introduction of CVO, VideoRendererInterface implementation should handle rotation frame by either rotating in renderer or calling into GetCopyWithRotationApplied.
    BUG=webrtc:4145
    
    Review URL: https://codereview.chromium.org/1025653004
    
    Cr-Commit-Position: refs/heads/master@{#321871}

--- content/renderer/media/webrtc/webrtc_video_capturer_adapter.cc
+++ content/renderer/media/webrtc/webrtc_video_capturer_adapter.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/libyuv/include/libyuv/convert_from.h"
 #include "third_party/libyuv/include/libyuv/scale.h"
 #include "third_party/webrtc/common_video/interface/video_frame_buffer.h"
+#include "third_party/webrtc/common_video/rotation.h"
 
 namespace content {
 namespace {
@@ -97,7 +98,7 @@ class WebRtcVideoCapturerAdapter::MediaVideoFrameFactory
                                  base::Time::kNanosecondsPerMicrosecond;
     captured_frame_.pixel_height = 1;
     captured_frame_.pixel_width = 1;
-    captured_frame_.rotation = 0;
+    captured_frame_.rotation = webrtc::kVideoRotation_0;
     captured_frame_.data = NULL;
     captured_frame_.data_size = cricket::CapturedFrame::kUnknownDataSize;
     captured_frame_.fourcc = static_cast<uint32>(cricket::FOURCC_ANY);
