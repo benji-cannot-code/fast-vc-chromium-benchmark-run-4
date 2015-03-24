5e20ba99b3fd398750db30cd7625af8eb5696a42
grt <grt@chromium.org>
2015-03-24 16:02:40 -0700

    Validate that zip entry filenames are UTF8.
    BUG=462584
    
    Review URL: https://codereview.chromium.org/1028243002
    
    Cr-Commit-Position: refs/heads/master@{#322085}

--- chrome/common/safe_browsing/zip_analyzer.cc
+++ chrome/common/safe_browsing/zip_analyzer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/safe_browsing/zip_analyzer.h"
 
+#include "base/i18n/streaming_utf8_validator.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "chrome/common/safe_browsing/binary_feature_extractor.h"
@@ -59,7 +60,9 @@ void AnalyzeContainedFile(
     zip::ZipReader* reader,
     base::File* temp_file,
     ClientDownloadRequest_ArchivedBinary* archived_binary) {
-  archived_binary->set_file_basename(file_path.BaseName().AsUTF8Unsafe());
+  std::string file_basename(file_path.BaseName().AsUTF8Unsafe());
+  if (base::StreamingUtf8Validator::Validate(file_basename))
+    archived_binary->set_file_basename(file_basename);
   archived_binary->set_download_type(
       download_protection_util::GetDownloadType(file_path));
   archived_binary->set_length(reader->current_entry_info()->original_size());
