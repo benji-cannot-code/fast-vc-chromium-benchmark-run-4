cbdfdb14bba6650ba78ef5694a105c70a15cf58d
cmumford <cmumford@chromium.org>
2015-03-24 13:01:20 -0700

    leveldb: Moved to LevelDBEnv.IOError.BFE histogram entries.
    When Chrome's leveldb Env (ChromiumEnv) switched to using base::File for I/O, in
    crrev.com/710373002, it also (unintentionally) switched some logged histogram
    values from errno to base::File::Error. Prior to r305020 some values were logged
    with errno values, others with PlatformFileError values, and still others with
    Windows Error values (i.e. GetLastError()). The Windows values were introduced
    in r245135 - also uintentionally. Teasing these apart is too difficult now so
    switching to a new set of histograms reporting base::File::Error's.
    
    BUG=431914
    
    Review URL: https://codereview.chromium.org/862723002
    
    Cr-Commit-Position: refs/heads/master@{#322054}

--- third_party/leveldatabase/env_chromium.h
+++ third_party/leveldatabase/env_chromium.h
@@ -60,7 +60,7 @@ leveldb::Status MakeIOError(leveldb::Slice filename,
 
 enum ErrorParsingResult {
   METHOD_ONLY,
-  METHOD_AND_PFE,
+  METHOD_AND_BFE,
   NONE,
 };
 
@@ -130,6 +130,7 @@ class ChromiumEnv : public leveldb::Env,
 
  protected:
   std::string name_;
+  std::string uma_ioerror_base_name_;
   bool make_backup_;
 
  private:
