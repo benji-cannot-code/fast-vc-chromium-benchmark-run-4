0dc4a1ae006d197ac2939435e468961be3361c66
kouhei@chromium.org <kouhei@chromium.org>
2015-03-24 01:21:13 +0000

    Oilpan: Implement marking time estimation
    Oilpan schedules GC as idleTask in the Blink scheduler.
    When idle, the Blink scheduler notifies Oilpan with a "deadline" timestamp,
    and let the idleTask client (Oilpan) decide whether to proceed with
    processing the task, or skip processing the task as it may take beyond
    the "deadline".
    
    Before this CL, Oilpan idleTask handler didn't implement this decision properly,
    and always proceeded with a GC assuming the GC would take 0 ms.
    This CL implements a heuristics so that Oilpan GC would only proceed when
    it is assumed to finish within the given "deadline".
    
    This CL implements a heuristics assuming that GC would take a linear time
    O(N_live_objs). The number of live objects is estimated by taking the ratio of
    heap size increase since last gc.
    
    BUG=420515
    
    Review URL: https://codereview.chromium.org/965283002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192397 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Heap.h
+++ third_party/WebKit/Source/platform/heap/Heap.h
@@ -1076,6 +1076,7 @@ private:
     static size_t s_externallyAllocatedBytes;
     static size_t s_externallyAllocatedBytesAlive;
     static unsigned s_requestedUrgentGC;
+    static double s_markingTimeInLastGC;
 
     friend class ThreadState;
 };
