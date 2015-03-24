f7e10cb46e2cee2c9674e3ab4648c1dbbafac99e
ginkage <ginkage@chromium.org>
2015-03-24 06:30:06 -0700

    Back button for the WebView-based sign-in flow.
    The button state is being updated in the backButton event.
    It remains hidden until the first such event and after each
    click, so the <webview> can always control if the button is
    needed at all.
    
    BUG=462660
    
    Review URL: https://codereview.chromium.org/1029053002
    
    Cr-Commit-Position: refs/heads/master@{#321990}

--- chrome/browser/resources/chromeos/login/screen_gaia_signin.js
+++ chrome/browser/resources/chromeos/login/screen_gaia_signin.js
@@ -120,6 +120,8 @@ login.createScreen('GaiaSigninScreen', 'gaia-signin', function() {
           'dialogShown', this.onDialogShown_.bind(this));
       this.gaiaAuthHost_.addEventListener(
           'dialogHidden', this.onDialogHidden_.bind(this));
+      this.gaiaAuthHost_.addEventListener(
+          'backButton', this.onBackButton_.bind(this));
       this.gaiaAuthHost_.confirmPasswordCallback =
           this.onAuthConfirmPassword_.bind(this);
       this.gaiaAuthHost_.noPasswordCallback =
@@ -140,6 +142,11 @@ login.createScreen('GaiaSigninScreen', 'gaia-signin', function() {
         e.preventDefault();
       });
 
+      $('back-button-item').addEventListener('click', function(e) {
+        $('back-button-item').hidden = true;
+        $('signin-frame').back();
+        e.preventDefault();
+      }.bind(this));
       $('close-button-item').addEventListener('click', function(e) {
         this.cancel();
         e.preventDefault();
@@ -262,6 +269,8 @@ login.createScreen('GaiaSigninScreen', 'gaia-signin', function() {
       window.requestAnimationFrame(function() {
         chrome.send('loginVisible', ['gaia-loading']);
       });
+      $('back-button-item').disabled = false;
+      $('back-button-item').hidden = true;
       $('close-button-item').disabled = false;
       this.classList.toggle('loading', this.loading);
 
@@ -466,6 +475,7 @@ login.createScreen('GaiaSigninScreen', 'gaia-signin', function() {
      * @private
      */
     onDialogShown_: function() {
+      $('back-button-item').disabled = true;
       $('close-button-item').disabled = true;
     },
 
@@ -474,7 +484,16 @@ login.createScreen('GaiaSigninScreen', 'gaia-signin', function() {
      * @private
      */
     onDialogHidden_: function() {
+      $('back-button-item').disabled = false;
       $('close-button-item').disabled = false;
+    },
+
+    /**
+     * Invoked when the auth host emits 'backButton' event.
+     * @private
+     */
+    onBackButton_: function(e) {
+      $('back-button-item').hidden = !e.detail;
     },
 
     /**
