0eccb9bed39f41db86abf85304967af0777f3464
mtomasz <mtomasz@chromium.org>
2015-03-23 23:17:11 -0700

    Add a whitelist for chrome.fileSystem.requestFileSystem.
    Instead of allowing all component apps, this CL whitelists only those apps
    which need access to this API.
    
    TEST=All current tests pass.
    BUG=323422
    
    Review URL: https://codereview.chromium.org/1030533002
    
    Cr-Commit-Position: refs/heads/master@{#321942}

--- chrome/browser/extensions/api/file_system/file_system_api.cc
+++ chrome/browser/extensions/api/file_system/file_system_api.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_CHROMEOS)
 #include "base/thread_task_runner_handle.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_app_manager.h"
+#include "chrome/browser/chromeos/file_manager/app_id.h"
 #include "chrome/browser/chromeos/file_manager/filesystem_api_util.h"
 #include "chrome/browser/chromeos/file_manager/volume_manager.h"
 #include "components/user_manager/user_manager.h"
@@ -93,6 +94,17 @@ const char kNotSupportedOnNonKioskSessionError[] =
     "Operation only supported for kiosk apps running in a kiosk session.";
 const char kVolumeNotFoundError[] = "Volume not found.";
 const char kSecurityError[] = "Security error.";
+
+// List of whitelisted component apps and extensions by their ids for
+// chrome.fileSystem.requestFileSystem.
+const char* const kRequestFileSystemComponentWhitelist[] = {
+    file_manager::kFileManagerAppId,
+    file_manager::kVideoPlayerAppId,
+    file_manager::kGalleryAppId,
+    file_manager::kAudioPlayerAppId,
+    file_manager::kImageLoaderExtensionId,
+    "pkplfbidichfdicaijlchgnapepdginl"  // Testing extensions.
+};
 #endif
 
 namespace file_system = extensions::api::file_system;
@@ -1025,11 +1037,20 @@ ExtensionFunction::ResponseAction FileSystemRequestFileSystemFunction::Run() {
   return RespondNow(Error(kNotSupportedOnCurrentPlatformError));
 
 #else
-  // Only kiosk apps in kiosk sessions can use this API. Additionally component
-  // extensions and apps, which is not documented though.
+  // Only kiosk apps in kiosk sessions can use this API.
+  // Additionally whitelisted component extensions and apps.
+  bool is_whitelisted_component = false;
+  if (extension()->location() == Manifest::COMPONENT) {
+    for (const auto& whitelisted_id : kRequestFileSystemComponentWhitelist) {
+      if (extension_id().compare(whitelisted_id) == 0) {
+        is_whitelisted_component = true;
+        break;
+      }
+    }
+  }
   if ((!user_manager::UserManager::Get()->IsLoggedInAsKioskApp() ||
        !KioskModeInfo::IsKioskEnabled(extension())) &&
-      extension()->location() != Manifest::COMPONENT) {
+      !is_whitelisted_component) {
     return RespondNow(Error(kNotSupportedOnNonKioskSessionError));
   }
 
@@ -1072,8 +1093,7 @@ ExtensionFunction::ResponseAction FileSystemRequestFileSystemFunction::Run() {
   const bool is_auto_launched =
       chromeos::KioskAppManager::Get()->GetApp(extension_id(), &app_info) &&
       app_info.was_auto_launched_with_zero_delay;
-  const bool requires_consent =
-      !is_auto_launched && extension()->location() != Manifest::COMPONENT;
+  const bool requires_consent = !is_auto_launched && !is_whitelisted_component;
 
   if (!requires_consent) {
     // Grant the permission without showing the dialog.
