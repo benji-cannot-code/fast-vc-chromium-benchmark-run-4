fc5e614b6bc21a7e584e5da0f81962765ad14279
eustas@chromium.org <eustas@chromium.org>
2015-03-24 17:25:41 +0000

    DevTools: PromisesPane: use ViewportDataGrid
    BUG=
    
    Review URL: https://codereview.chromium.org/958613002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192453 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui_lazy/ViewportDataGrid.js
+++ third_party/WebKit/Source/devtools/front_end/ui_lazy/ViewportDataGrid.js
@@ -314,6 +314,7 @@ WebInspector.ViewportDataGridNode.prototype = {
      */
     insertChild: function(child, index)
     {
+        child.remove();
         child.parent = this;
         child.dataGrid = this.dataGrid;
         if (!this.children.length)
