294f5a341a39f7ff4ac6af5d9911fb588e8174fe
cylee <cylee@chromium.org>
2015-03-24 08:52:52 -0700

    re-enable unittest related to TracingControllerBackend on CrOS
    BUG=412713
    
    Review URL: https://codereview.chromium.org/1029323002
    
    Cr-Commit-Position: refs/heads/master@{#322006}

--- tools/telemetry/telemetry/core/backends/chrome_inspector/tracing_backend_unittest.py
+++ tools/telemetry/telemetry/core/backends/chrome_inspector/tracing_backend_unittest.py
@@ -78,7 +78,6 @@ class TracingBackendTest(tab_test_case.TabTestCase):
       self.skipTest('Browser does not support tracing, skipping test.')
     self._StartServer()
 
-  @decorators.Disabled('chromeos') # crbug.com/412713.
   def testGotTrace(self):
     options = tracing_options.TracingOptions()
     options.enable_chrome_trace = True
@@ -90,7 +89,6 @@ class TracingBackendTest(tab_test_case.TabTestCase):
     model = model_module.TimelineModel(trace_data)
     assert len(model.processes) > 0
 
-  @decorators.Disabled('chromeos') # crbug.com/412713.
   def testStartAndStopTraceMultipleTimes(self):
     options = tracing_options.TracingOptions()
     options.enable_chrome_trace = True
