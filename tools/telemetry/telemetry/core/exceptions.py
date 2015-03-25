16f3693cfba8c828a86db6ddbe707e438e72fc95
rnephew <rnephew@chromium.org>
2015-03-25 15:50:30 -0700

    [Telemetry][Android] Add support for turning screen off.
    For testing background usage of Chrome on android devices.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1004833003
    
    Cr-Commit-Position: refs/heads/master@{#322254}

--- tools/telemetry/telemetry/core/exceptions.py
+++ tools/telemetry/telemetry/core/exceptions.py
@@ -120,3 +120,6 @@ class UnknownPackageError(Error):
 
 class PackageDetectionError(Error):
   """ Represents an error when parsing an Android APK's package. """
+
+class AndroidDeviceParsingError(Error):
+  """Represents an error when parsing output from an android device"""
