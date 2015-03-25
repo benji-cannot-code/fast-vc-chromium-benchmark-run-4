9ab5342f7adb8f77926bc2d55e32001d1ce702da
ed@opera.com <ed@opera.com>
2015-03-25 10:47:47 +0000

    [S.P.] Use 'const LayoutObject&' in SVGMaskPainter.
    Review URL: https://codereview.chromium.org/1022843004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192515 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/SVGMaskPainter.cpp
+++ third_party/WebKit/Source/core/paint/SVGMaskPainter.cpp
@@ -16,50 +16,48 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-bool SVGMaskPainter::prepareEffect(LayoutObject* object, GraphicsContext* context)
+bool SVGMaskPainter::prepareEffect(const LayoutObject& object, GraphicsContext* context)
 {
-    ASSERT(object);
     ASSERT(context);
     ASSERT(m_mask.style());
     ASSERT_WITH_SECURITY_IMPLICATION(!m_mask.needsLayout());
 
     m_mask.clearInvalidationMask();
 
-    FloatRect paintInvalidationRect = object->paintInvalidationRectInLocalCoordinates();
+    FloatRect paintInvalidationRect = object.paintInvalidationRectInLocalCoordinates();
     if (paintInvalidationRect.isEmpty() || !m_mask.element()->hasChildren())
         return false;
 
     if (RuntimeEnabledFeatures::slimmingPaintEnabled()) {
         ASSERT(context->displayItemList());
-        context->displayItemList()->add(BeginCompositingDisplayItem::create(object->displayItemClient(), WebCoreCompositeToSkiaComposite(context->compositeOperationDeprecated(), WebBlendModeNormal), 1, &paintInvalidationRect));
+        context->displayItemList()->add(BeginCompositingDisplayItem::create(object.displayItemClient(), WebCoreCompositeToSkiaComposite(context->compositeOperationDeprecated(), WebBlendModeNormal), 1, &paintInvalidationRect));
     } else {
-        BeginCompositingDisplayItem beginCompositingContent(object->displayItemClient(), WebCoreCompositeToSkiaComposite(context->compositeOperationDeprecated(), WebBlendModeNormal), 1, &paintInvalidationRect);
+        BeginCompositingDisplayItem beginCompositingContent(object.displayItemClient(), WebCoreCompositeToSkiaComposite(context->compositeOperationDeprecated(), WebBlendModeNormal), 1, &paintInvalidationRect);
         beginCompositingContent.replay(context);
     }
 
     return true;
 }
 
-void SVGMaskPainter::finishEffect(LayoutObject* object, GraphicsContext* context)
+void SVGMaskPainter::finishEffect(const LayoutObject& object, GraphicsContext* context)
 {
-    ASSERT(object);
     ASSERT(context);
     ASSERT(m_mask.style());
     ASSERT_WITH_SECURITY_IMPLICATION(!m_mask.needsLayout());
 
-    FloatRect paintInvalidationRect = object->paintInvalidationRectInLocalCoordinates();
+    FloatRect paintInvalidationRect = object.paintInvalidationRectInLocalCoordinates();
     {
         ColorFilter maskLayerFilter = m_mask.style()->svgStyle().maskType() == MT_LUMINANCE
             ? ColorFilterLuminanceToAlpha : ColorFilterNone;
-        CompositingRecorder maskCompositing(context, object->displayItemClient(), SkXfermode::kDstIn_Mode, 1, &paintInvalidationRect, maskLayerFilter);
-        drawMaskForRenderer(context, object->displayItemClient(), object->objectBoundingBox());
+        CompositingRecorder maskCompositing(context, object.displayItemClient(), SkXfermode::kDstIn_Mode, 1, &paintInvalidationRect, maskLayerFilter);
+        drawMaskForRenderer(context, object.displayItemClient(), object.objectBoundingBox());
     }
 
     if (RuntimeEnabledFeatures::slimmingPaintEnabled()) {
         ASSERT(context->displayItemList());
-        context->displayItemList()->add(EndCompositingDisplayItem::create(object->displayItemClient()));
+        context->displayItemList()->add(EndCompositingDisplayItem::create(object.displayItemClient()));
     } else {
-        EndCompositingDisplayItem endCompositingContent(object->displayItemClient());
+        EndCompositingDisplayItem endCompositingContent(object.displayItemClient());
         endCompositingContent.replay(context);
     }
 }
