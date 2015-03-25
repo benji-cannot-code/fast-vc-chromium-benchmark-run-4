3ff0e060fd0a5e5a3c34adcc73e302f83b0c0f10
kojii@chromium.org <kojii@chromium.org>
2015-03-25 12:40:40 +0000

    Use the composed tree for the dir attributes in Shadow DOM
    To accommodate the spec change, computing the lang attribute was changed
    to traverse tree of trees.
    
    The relevant W3C bug is 27222
    https://www.w3.org/Bugs/Public/show_bug.cgi?id=27222
    
    Support the dir attribute is more complex than other attributes because
    the dir="auto" creates a scope-like semantics as defined in:
    https://html.spec.whatwg.org/multipage/dom.html#the-dir-attribute
    
    To check dir="auto" fast, wkb.ug/50916 added a flag in every node as
    selfOrAncestorHasDirAutoAttribute(), and this flag needs to be updated
    properly in the composed tree.
    
    The fix for issue 296847 makes dir="auto" partially inherit to shadow
    trees so that the UA shadow of textarea can see it. But it's only
    partial fix as esprehn@ pointed out in the comment #36 of the CL:
    https://codereview.chromium.org/302433015#msg36
    
    This patch changes the behavior to set the flag to all of its
    descendants except when they have another scope, so that any nodes can
    know whether they're in dir="auto" or not correctly, and avoids setting
    the flag to all shadow roots. This behavior matches to other similar
    functions such as
    adjustDirectionalityIfNeededAfterChildAttributeChanged().
    
    BUG=450115, 296847
    TEST=fast/dom/shadow/attr-dir-inherit.html
    
    Review URL: https://codereview.chromium.org/884353002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192531 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLElement.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/ExceptionCode.h"
 #include "core/dom/NodeTraversal.h"
 #include "core/dom/Text.h"
+#include "core/dom/shadow/ComposedTreeTraversal.h"
 #include "core/dom/shadow/ElementShadow.h"
 #include "core/dom/shadow/ShadowRoot.h"
 #include "core/editing/markup.h"
@@ -709,12 +710,12 @@ TextDirection HTMLElement::directionality(Node** strongDirectionalityTextNode) c
         return textDirection;
     }
 
-    Node* node = firstChild();
+    Node* node = ComposedTreeTraversal::firstChild(*this);
     while (node) {
         // Skip bdi, script, style and text form controls.
         if (equalIgnoringCase(node->nodeName(), "bdi") || isHTMLScriptElement(*node) || isHTMLStyleElement(*node)
             || (node->isElementNode() && toElement(node)->isTextFormControl())) {
-            node = NodeTraversal::nextSkippingChildren(*node, this);
+            node = ComposedTreeTraversal::nextSkippingChildren(*node, this);
             continue;
         }
 
@@ -722,7 +723,7 @@ TextDirection HTMLElement::directionality(Node** strongDirectionalityTextNode) c
         if (node->isElementNode()) {
             AtomicString dirAttributeValue = toElement(node)->fastGetAttribute(dirAttr);
             if (isValidDirAttribute(dirAttributeValue)) {
-                node = NodeTraversal::nextSkippingChildren(*node, this);
+                node = ComposedTreeTraversal::nextSkippingChildren(*node, this);
                 continue;
             }
         }
@@ -736,7 +737,7 @@ TextDirection HTMLElement::directionality(Node** strongDirectionalityTextNode) c
                 return textDirection;
             }
         }
-        node = NodeTraversal::next(*node, this);
+        node = ComposedTreeTraversal::next(*node, this);
     }
     if (strongDirectionalityTextNode)
         *strongDirectionalityTextNode = 0;
@@ -752,7 +753,8 @@ void HTMLElement::dirAttributeChanged(const AtomicString& value)
 {
     // If an ancestor has dir=auto, and this node has the first character,
     // changes to dir attribute may affect the ancestor.
-    Element* parent = parentElement();
+    updateDistribution();
+    Element* parent = ComposedTreeTraversal::parentElement(*this);
     if (parent && parent->isHTMLElement() && toHTMLElement(parent)->selfOrAncestorHasDirAutoAttribute())
         toHTMLElement(parent)->adjustDirectionalityIfNeededAfterChildAttributeChanged(this);
 
@@ -766,7 +768,7 @@ void HTMLElement::adjustDirectionalityIfNeededAfterChildAttributeChanged(Element
     TextDirection textDirection = directionality();
     if (layoutObject() && layoutObject()->style() && layoutObject()->style()->direction() != textDirection) {
         Element* elementToAdjust = this;
-        for (; elementToAdjust; elementToAdjust = elementToAdjust->parentElement()) {
+        for (; elementToAdjust; elementToAdjust = ComposedTreeTraversal::parentElement(*elementToAdjust)) {
             if (elementAffectsDirectionality(elementToAdjust)) {
                 elementToAdjust->setNeedsStyleRecalc(SubtreeStyleChange, StyleChangeReasonForTracing::create(StyleChangeReason::WritingModeChange));
                 return;
@@ -787,7 +789,9 @@ void HTMLElement::adjustDirectionalityIfNeededAfterChildrenChanged(const Childre
     if (!selfOrAncestorHasDirAutoAttribute())
         return;
 
-    for (Element* elementToAdjust = this; elementToAdjust; elementToAdjust = elementToAdjust->parentElement()) {
+    updateDistribution();
+
+    for (Element* elementToAdjust = this; elementToAdjust; elementToAdjust = ComposedTreeTraversal::parentElement(*elementToAdjust)) {
         if (elementAffectsDirectionality(elementToAdjust)) {
             toHTMLElement(elementToAdjust)->calculateAndAdjustDirectionality();
             return;
