38a27a828395e50334fde2345ac29142811cb785
rob.buis@samsung.com <rob.buis@samsung.com>
2015-03-25 12:32:43 +0000

    Remove duplicated CSSValueNone checks
    In these cases instead of duplicated code to create the None identifier, use
    the validPrimitive machinery instead.
    
    BUG=404023
    
    Review URL: https://codereview.chromium.org/893193005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192529 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -1185,7 +1185,10 @@ bool CSSPropertyParser::parseValue(CSSPropertyID propId, bool important)
         return parseShorthand(propId, parsingShorthandForProperty(CSSPropertyMotion), important);
     case CSSPropertyMotionPath:
         ASSERT(RuntimeEnabledFeatures::cssMotionPathEnabled());
-        parsedValue = parseMotionPath();
+        if (id == CSSValueNone)
+            validPrimitive = true;
+        else
+            parsedValue = parseMotionPath();
         break;
     case CSSPropertyMotionOffset:
         ASSERT(RuntimeEnabledFeatures::cssMotionPathEnabled());
@@ -1519,7 +1522,10 @@ bool CSSPropertyParser::parseValue(CSSPropertyID propId, bool important)
         }
         break;
     case CSSPropertyShapeOutside:
-        parsedValue = parseShapeProperty(propId);
+        if (id == CSSValueNone)
+            validPrimitive = true;
+        else
+            parsedValue = parseShapeProperty(propId);
         break;
     case CSSPropertyShapeMargin:
         validPrimitive = (!id && validUnit(value, FLength | FPercent | FNonNeg));
@@ -1533,7 +1539,10 @@ bool CSSPropertyParser::parseValue(CSSPropertyID propId, bool important)
         break;
 
     case CSSPropertyScrollBlocksOn:
-        parsedValue = parseScrollBlocksOn();
+        if (id == CSSValueNone)
+            validPrimitive = true;
+        else
+            parsedValue = parseScrollBlocksOn();
         break;
 
     case CSSPropertyAlignContent:
@@ -4400,17 +4409,8 @@ static bool isBoxValue(CSSValueID valueId)
 
 PassRefPtrWillBeRawPtr<CSSValue> CSSPropertyParser::parseShapeProperty(CSSPropertyID propId)
 {
-    CSSParserValue* value = m_valueList->current();
-    CSSValueID valueId = value->id;
-
-    if (valueId == CSSValueNone) {
-        RefPtrWillBeRawPtr<CSSPrimitiveValue> keywordValue = parseValidPrimitive(valueId, value);
-        m_valueList->next();
-        return keywordValue.release();
-    }
-
     RefPtrWillBeRawPtr<CSSValue> imageValue = nullptr;
-    if (valueId != CSSValueNone && parseFillImage(m_valueList, imageValue)) {
+    if (parseFillImage(m_valueList, imageValue)) {
         m_valueList->next();
         return imageValue.release();
     }
@@ -7418,13 +7418,8 @@ PassRefPtrWillBeRawPtr<CSSValue> CSSPropertyParser::parseTouchAction()
 
 PassRefPtrWillBeRawPtr<CSSValue> CSSPropertyParser::parseScrollBlocksOn()
 {
-    CSSParserValue* value = m_valueList->current();
-    if (value->id == CSSValueNone) {
-        m_valueList->next();
-        return cssValuePool().createIdentifierValue(CSSValueNone);
-    }
-
     RefPtrWillBeRawPtr<CSSValueList> list = CSSValueList::createSpaceSeparated();
+    CSSParserValue* value = m_valueList->current();
     while (value) {
         switch (value->id) {
         case CSSValueStartTouch:
@@ -8446,10 +8441,6 @@ PassRefPtrWillBeRawPtr<CSSValue> CSSPropertyParser::parseTransformValue(CSSPrope
 PassRefPtrWillBeRawPtr<CSSValue> CSSPropertyParser::parseMotionPath()
 {
     CSSParserValue* value = m_valueList->current();
-    if (value->id == CSSValueNone) {
-        m_valueList->next();
-        return cssValuePool().createIdentifierValue(CSSValueNone);
-    }
 
     // FIXME: Add support for <url>, <basic-shape>, <geometry-box>.
     if (value->unit != CSSParserValue::Function || value->function->id != CSSValuePath)
