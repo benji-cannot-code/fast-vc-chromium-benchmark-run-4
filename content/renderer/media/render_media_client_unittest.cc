f3a52cdef141dec84ffd4a9dbcfb8fb682596a00
sandersd <sandersd@chromium.org>
2015-03-25 12:53:06 -0700

    Implement robustness strings in requestMediaKeySystemAccess().
    BUG=442586,460616
    
    Review URL: https://codereview.chromium.org/1005903003
    
    Cr-Commit-Position: refs/heads/master@{#322219}

--- content/renderer/media/render_media_client_unittest.cc
+++ content/renderer/media/render_media_client_unittest.cc
@@ -28,6 +28,8 @@ class TestContentRendererClient : public ContentRendererClient {
     // TODO(sandersd): Was this supposed to be added to the list?
     media::KeySystemInfo key_system_info;
     key_system_info.key_system = "test.keysystem";
+    key_system_info.max_audio_robustness = media::EmeRobustness::EMPTY;
+    key_system_info.max_video_robustness = media::EmeRobustness::EMPTY;
     key_system_info.persistent_license_support =
         media::EME_SESSION_TYPE_NOT_SUPPORTED;
     key_system_info.persistent_release_message_support =
@@ -41,6 +43,8 @@ class TestContentRendererClient : public ContentRendererClient {
     if (is_extra_key_system_enabled_) {
       media::KeySystemInfo wv_key_system_info;
       wv_key_system_info.key_system = kWidevineKeySystem;
+      wv_key_system_info.max_audio_robustness = media::EmeRobustness::EMPTY;
+      wv_key_system_info.max_video_robustness = media::EmeRobustness::EMPTY;
       wv_key_system_info.persistent_license_support =
           media::EME_SESSION_TYPE_NOT_SUPPORTED;
       wv_key_system_info.persistent_release_message_support =
