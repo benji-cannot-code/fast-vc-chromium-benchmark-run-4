d17c0eb98fc8cc32d2782a96cf3c5b09ff6961a8
hans <hans@chromium.org>
2015-03-25 17:18:21 -0700

    Roll Clang 231690:233105
    BUG=470393
    
    Review URL: https://codereview.chromium.org/1018353003
    
    Cr-Commit-Position: refs/heads/master@{#322271}

--- tools/clang/scripts/update.sh
+++ tools/clang/scripts/update.sh
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Do NOT CHANGE this if you don't know what you're doing -- see
 # https://code.google.com/p/chromium/wiki/UpdatingClang
 # Reverting problematic clang rolls is safe, though.
-CLANG_REVISION=231690
+CLANG_REVISION=233105
 
 # This is incremented when pushing a new build of Clang at the same revision.
 CLANG_SUB_REVISION=1
