fa0597bb32d1f608de3159841ae41155de65ffa4
garykac <garykac@chromium.org>
2015-03-26 11:34:49 -0700

    [Chromoting] Change Application.Delegate to proper subclass of Application.
    App Delegate conversion:
    * Removed Application.Delegate
    * Convert DesktopRemoting and AppRemoting to be subclasses of Application
    * Add ApplicationInterface so that jscompile can verify all required overrides are provided.
    
    Access to SessionConnector was a big motivation for having the apps as a subclass:
    * Create the SessionConnector in the constructor rather than in the getter.
    * Remove some references to remoting.app.getSessionConnector
    
    Test updates:
    * Updated base.inherits unittests to verify calling superclass methods with arguments.
    * Removed desktopDelegateForTesting since we can use remoting.app for that now.
    
    BUG=465878
    
    Review URL: https://codereview.chromium.org/1016373003
    
    Cr-Commit-Position: refs/heads/master@{#322437}

--- remoting/webapp/browser_test/bump_scroll_browser_test.js
+++ remoting/webapp/browser_test/bump_scroll_browser_test.js
@@ -59,7 +59,8 @@ browserTest.FakeDesktopViewport.prototype.raiseEvent =
 
 /** @return {remoting.DesktopViewport} */
 function getViewportForTesting() {
-  var view = remoting.desktopDelegateForTesting.getConnectedViewForTesting();
+  var desktopApp = /** @type {remoting.DesktopRemoting} */ (remoting.app);
+  var view = desktopApp.getConnectedViewForTesting();
   if (view) {
     return view.getViewportForTesting();
   }
