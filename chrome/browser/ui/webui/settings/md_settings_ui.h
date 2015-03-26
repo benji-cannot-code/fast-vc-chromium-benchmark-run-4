23398e693e13892aa13ceeb530df25d4d56b0eb2
khorimoto <khorimoto@chromium.org>
2015-03-25 17:47:05 -0700

    Move MdSettingsUi to its own directory. There will be additional settings-related files in the near future, so this directory will be used to organize them.
    BUG=
    
    Review URL: https://codereview.chromium.org/1015533019
    
    Cr-Commit-Position: refs/heads/master@{#322279}

--- chrome/browser/ui/webui/settings/md_settings_ui.h
+++ chrome/browser/ui/webui/settings/md_settings_ui.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_MD_SETTINGS_UI_H_
-#define CHROME_BROWSER_UI_WEBUI_MD_SETTINGS_UI_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_SETTINGS_MD_SETTINGS_UI_H_
+#define CHROME_BROWSER_UI_WEBUI_SETTINGS_MD_SETTINGS_UI_H_
 
 #include "chrome/browser/ui/webui/options/core_options_handler.h"
 #include "chrome/browser/ui/webui/options/options_ui.h"
@@ -26,4 +26,4 @@ class MdSettingsUI : public content::WebUIController,
   DISALLOW_COPY_AND_ASSIGN(MdSettingsUI);
 };
 
-#endif  // CHROME_BROWSER_UI_WEBUI_MD_SETTINGS_UI_H_
+#endif  // CHROME_BROWSER_UI_WEBUI_SETTINGS_MD_SETTINGS_UI_H_
