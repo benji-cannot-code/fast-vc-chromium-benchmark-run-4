3ac4e4043b30963b235588e24a28d6379839104c
stevenjb <stevenjb@chromium.org>
2015-03-26 01:59:33 -0700

    Format ONC translation files
    BUG=None
    
    Review URL: https://codereview.chromium.org/1031253002
    
    Cr-Commit-Position: refs/heads/master@{#322338}

--- chromeos/network/onc/onc_signature.h
+++ chromeos/network/onc/onc_signature.h
@@ -32,9 +32,8 @@ CHROMEOS_EXPORT const OncFieldSignature* GetFieldSignature(
     const OncValueSignature& signature,
     const std::string& onc_field_name);
 
-CHROMEOS_EXPORT bool FieldIsCredential(
-    const OncValueSignature& signature,
-    const std::string& onc_field_name);
+CHROMEOS_EXPORT bool FieldIsCredential(const OncValueSignature& signature,
+                                       const std::string& onc_field_name);
 
 CHROMEOS_EXPORT extern const OncValueSignature kRecommendedSignature;
 CHROMEOS_EXPORT extern const OncValueSignature kEAPSignature;
--- chromeos/network/onc/onc_translator_onc_to_shill.cc
+++ chromeos/network/onc/onc_translator_onc_to_shill.cc
@@ -127,7 +127,6 @@ void LocalTranslator::TranslateEthernet() {
   CopyFieldsAccordingToSignature();
 }
 
-
 void LocalTranslator::TranslateOpenVPN() {
   // SaveCredentials needs special handling when translating from Shill -> ONC
   // so handle it explicitly here.
@@ -217,8 +216,8 @@ void LocalTranslator::TranslateVPN() {
 
 void LocalTranslator::TranslateWiFi() {
   std::string security;
-  if (onc_object_->GetStringWithoutPathExpansion(
-          ::onc::wifi::kSecurity, &security)) {
+  if (onc_object_->GetStringWithoutPathExpansion(::onc::wifi::kSecurity,
+                                                 &security)) {
     TranslateWithTableAndSet(security, kWiFiSecurityTable,
                              shill::kSecurityClassProperty);
   }
@@ -250,9 +249,9 @@ void LocalTranslator::TranslateEAP() {
     // Shill.
     onc_object_->GetStringWithoutPathExpansion(::onc::eap::kInner, &inner);
     if (inner != ::onc::eap::kAutomatic) {
-      const StringTranslationEntry* table =
-          outer == ::onc::eap::kPEAP ? kEAP_PEAP_InnerTable :
-                                       kEAP_TTLS_InnerTable;
+      const StringTranslationEntry* table = outer == ::onc::eap::kPEAP
+                                                ? kEAP_PEAP_InnerTable
+                                                : kEAP_TTLS_InnerTable;
       TranslateWithTableAndSet(inner, table, shill::kEapPhase2AuthProperty);
     }
   }
@@ -329,8 +328,8 @@ void LocalTranslator::AddValueAccordingToSignature(
   if (!value || !field_translation_table_)
     return;
   std::string shill_property_name;
-  if (!GetShillPropertyName(
-          onc_name, field_translation_table_, &shill_property_name)) {
+  if (!GetShillPropertyName(onc_name, field_translation_table_,
+                            &shill_property_name)) {
     return;
   }
 
@@ -367,8 +366,7 @@ void TranslateONCHierarchy(const OncValueSignature& signature,
       GetPathToNestedShillDictionary(signature);
   for (std::vector<std::string>::const_iterator it =
            path_to_shill_dictionary.begin();
-       it != path_to_shill_dictionary.end();
-       ++it) {
+       it != path_to_shill_dictionary.end(); ++it) {
     base::DictionaryValue* nested_shill_dict = NULL;
     target_shill_dictionary->GetDictionaryWithoutPathExpansion(
         *it, &nested_shill_dict);
