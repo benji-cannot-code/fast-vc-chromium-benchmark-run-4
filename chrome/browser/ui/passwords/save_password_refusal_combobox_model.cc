70e00f90a3ce358f8d987dcec4da8f62fbc0b7f3
vasilii <vasilii@chromium.org>
2015-03-26 02:16:54 -0700

    New smart passsword bubble algorithm.
    The behavior is controlled by Finch. If user clicks "Not save" n times in row then the default value for the button becomes "Never for this site".
    The previous experiment didn't pop up the bubble occasionally. We collected enough statistics and stopped it few months ago.
    
    BUG=431739
    
    Review URL: https://codereview.chromium.org/1022843003
    
    Cr-Commit-Position: refs/heads/master@{#322340}

--- chrome/browser/ui/passwords/save_password_refusal_combobox_model.cc
+++ chrome/browser/ui/passwords/save_password_refusal_combobox_model.cc
@@ -8,12 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 
-SavePasswordRefusalComboboxModel::SavePasswordRefusalComboboxModel() {
+SavePasswordRefusalComboboxModel::SavePasswordRefusalComboboxModel(
+    bool never_is_default)
+    : never_is_default_(never_is_default) {
 #if !defined(OS_ANDROID)
   items_.push_back(
       l10n_util::GetStringUTF16(IDS_PASSWORD_MANAGER_CANCEL_BUTTON));
   items_.push_back(
       l10n_util::GetStringUTF16(IDS_PASSWORD_MANAGER_BLACKLIST_BUTTON));
+  if (never_is_default)
+    swap(items_[0], items_[1]);
 #endif
 }
 
--- chrome/browser/ui/views/passwords/manage_passwords_bubble_view.cc
+++ chrome/browser/ui/views/passwords/manage_passwords_bubble_view.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/exclusive_access/fullscreen_controller.h"
 #include "chrome/browser/ui/passwords/manage_passwords_bubble_model.h"
 #include "chrome/browser/ui/passwords/manage_passwords_ui_controller.h"
+#include "chrome/browser/ui/passwords/password_bubble_experiment.h"
 #include "chrome/browser/ui/passwords/save_password_refusal_combobox_model.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/passwords/credentials_item_view.h"
@@ -398,7 +399,9 @@ ManagePasswordsBubbleView::PendingView::PendingView(
   save_button_->SetFontList(ui::ResourceBundle::GetSharedInstance().GetFontList(
       ui::ResourceBundle::SmallFont));
 
-  combobox_model_.reset(new SavePasswordRefusalComboboxModel());
+  combobox_model_.reset(new SavePasswordRefusalComboboxModel(
+      password_bubble_experiment::ShouldShowNeverForThisSiteDefault(
+          parent_->model()->GetProfile()->GetPrefs())));
   refuse_combobox_.reset(new views::Combobox(combobox_model_.get()));
   refuse_combobox_->set_listener(this);
   refuse_combobox_->SetStyle(views::Combobox::STYLE_ACTION);
@@ -439,14 +442,13 @@ void ManagePasswordsBubbleView::PendingView::ButtonPressed(
 void ManagePasswordsBubbleView::PendingView::OnPerformAction(
     views::Combobox* source) {
   DCHECK_EQ(source, refuse_combobox_);
-  switch (refuse_combobox_->selected_index()) {
-    case SavePasswordRefusalComboboxModel::INDEX_NOPE:
-      parent_->model()->OnNopeClicked();
-      parent_->Close();
-      break;
-    case SavePasswordRefusalComboboxModel::INDEX_NEVER_FOR_THIS_SITE:
-      parent_->NotifyNeverForThisSiteClicked();
-      break;
+  if (source->selected_index() == combobox_model_->index_nope()) {
+    parent_->model()->OnNopeClicked();
+    parent_->Close();
+  } else if (source->selected_index() == combobox_model_->index_never()) {
+    parent_->NotifyNeverForThisSiteClicked();
+  } else {
+    NOTREACHED();
   }
 }
 
