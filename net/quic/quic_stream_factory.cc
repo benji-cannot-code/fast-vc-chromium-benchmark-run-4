f1b1a3e41719f327ec861289f91425330c303890
bnc <bnc@chromium.org>
2015-03-26 13:13:50 -0700

    Confirm QUIC alternative service upon success.
    A non-broken but recently broken QUIC alternative service does not race for 0RTT
    (because it is recently broken) but is still used (because it is not broken).
    Upon successful connection, the service should be confirmed (marked as not
    recently broken so that it can be used next time for 0RTT).
    
    This CL adds this functionality and a test for it.  I locally verified that the
    test fails without the rest of the change.
    
    BUG=392575
    
    Review URL: https://codereview.chromium.org/1035713003
    
    Cr-Commit-Position: refs/heads/master@{#322455}

--- net/quic/quic_stream_factory.cc
+++ net/quic/quic_stream_factory.cc
@@ -1135,7 +1135,10 @@ void QuicStreamFactory::ProcessGoingAwaySession(
     return;
 
   const QuicConnectionStats& stats = session->connection()->GetStats();
+  const AlternativeService alternative_service(QUIC,
+                                               server_id.host_port_pair());
   if (session->IsCryptoHandshakeConfirmed()) {
+    http_server_properties_->ConfirmAlternativeService(alternative_service);
     ServerNetworkStats network_stats;
     network_stats.srtt = base::TimeDelta::FromMicroseconds(stats.srtt_us);
     network_stats.bandwidth_estimate = stats.estimated_bandwidth;
@@ -1162,9 +1165,8 @@ void QuicStreamFactory::ProcessGoingAwaySession(
   // job also fails. So to avoid not using QUIC when we otherwise could, we mark
   // it as recently broken, which means that 0-RTT will be disabled but we'll
   // still race.
-  const HostPortPair& server = server_id.host_port_pair();
   http_server_properties_->MarkAlternativeServiceRecentlyBroken(
-      AlternativeService(QUIC, server.host(), server.port()));
+      alternative_service);
 }
 
 }  // namespace net
