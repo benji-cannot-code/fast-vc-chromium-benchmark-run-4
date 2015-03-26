2b6736647faa5be3e6d81533e73351f7f5ec1def
sadrul <sadrul@chromium.org>
2015-03-25 19:08:08 -0700

    precache: Move the java files into //components/precache/android
    This moves code out of //java/apps/chrome/.../precache into:
      . //components/precache/android/: classes that provide information about
        device-state, network connectivity etc.
      . //chrome/android/.../precache/: precache service and service launchers.
    
    BUG=401552
    
    Review URL: https://codereview.chromium.org/1031003003
    
    Cr-Commit-Position: refs/heads/master@{#322298}

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/precache/MockPrecacheService.java
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.precache;
+
+/**
+ * PrecacheService class with native initialization mocked out.
+ */
+public class MockPrecacheService extends PrecacheService {
+    @Override
+    void prepareNativeLibraries() {}
+}
--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/omnibox/autocomplete_controller_android.h"
 #include "chrome/browser/android/omnibox/omnibox_prerender.h"
 #include "chrome/browser/android/password_ui_view_android.h"
+#include "chrome/browser/android/precache/precache_launcher.h"
 #include "chrome/browser/android/preferences/autofill/autofill_profile_bridge.h"
 #include "chrome/browser/android/preferences/pref_service_bridge.h"
 #include "chrome/browser/android/preferences/website_preference_bridge.h"
@@ -224,6 +225,7 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
      PasswordUIViewAndroid::RegisterPasswordUIViewAndroid},
     {"PersonalDataManagerAndroid",
      autofill::PersonalDataManagerAndroid::Register},
+    {"PrecacheLauncher", RegisterPrecacheLauncher},
     {"PrefServiceBridge", RegisterPrefServiceBridge},
     {"ProfileAndroid", ProfileAndroid::RegisterProfileAndroid},
     {"ProfileDownloader", RegisterProfileDownloader},
