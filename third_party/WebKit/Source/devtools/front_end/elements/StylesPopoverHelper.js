7e6e20d0ec35b6ec298e2836a74d92d414636e2b
lushnikov@chromium.org <lushnikov@chromium.org>
2015-03-26 18:01:55 +0000

    DevTools: [SSP] do not hide color picker when eye-dropper is used
    ATM we hide color picker as it looses focus. However, we should not
    do this if clicks do not happen inside the page.
    
    BUG=470836
    R=dgozman
    
    Review URL: https://codereview.chromium.org/1032993002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192617 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/elements/StylesPopoverHelper.js
+++ third_party/WebKit/Source/devtools/front_end/elements/StylesPopoverHelper.js
@@ -26,7 +26,7 @@ WebInspector.StylesPopoverHelper.prototype = {
      */
     _onFocusOut: function(event)
     {
-        if (event.relatedTarget && event.relatedTarget.isSelfOrDescendant(this._view.contentElement))
+        if (!event.relatedTarget || event.relatedTarget.isSelfOrDescendant(this._view.contentElement))
             return;
         this._hideProxy();
     },
