548be747ffffdc104d86e407f0e7b2140e3950b5
danakj <danakj@chromium.org>
2015-03-26 15:54:09 -0700

    Don't call PaintChildren() from inside OnPaint().
    OnPaint() is not part of the recursion, it won't have access to the
    necessary pieces to do recursion.
    
    If you need to do painting while inside the recursion, this should
    happen in PaintChildren(). So move the contents of OnPaint() to
    PaintChildren().
    
    R=estade@chromium.org
    BUG=466426
    
    Review URL: https://codereview.chromium.org/1029263011
    
    Cr-Commit-Position: refs/heads/master@{#322487}

--- chrome/browser/ui/views/autofill/autofill_dialog_views.h
+++ chrome/browser/ui/views/autofill/autofill_dialog_views.h
@@ -264,7 +264,6 @@ class AutofillDialogViews : public AutofillDialogView,
     const char* GetClassName() const override;
     void PaintChildren(gfx::Canvas* canvas,
                        const views::CullSet& cull_set) override;
-    void OnPaint(gfx::Canvas* canvas) override;
 
     void set_arrow_centering_anchor(
         const base::WeakPtr<views::View>& arrow_centering_anchor) {
@@ -346,8 +345,6 @@ class AutofillDialogViews : public AutofillDialogView,
     // views::MenuButton implementation.
     gfx::Size GetPreferredSize() const override;
     const char* GetClassName() const override;
-    void PaintChildren(gfx::Canvas* canvas,
-                       const views::CullSet& cull_set) override;
     void OnPaint(gfx::Canvas* canvas) override;
 
    private:
