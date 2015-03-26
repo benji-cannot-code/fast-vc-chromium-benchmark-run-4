b6ab945d0fca0b7901c24581caaf5ac0ec79d90d
reveman <reveman@chromium.org>
2015-03-25 19:39:33 -0700

    Restore compositor worker thread names.
    Some of our tests apparently rely on these threads having
    specific names.
    
    BUG=470091
    
    Review URL: https://codereview.chromium.org/1036923002
    
    Cr-Commit-Position: refs/heads/master@{#322303}

--- cc/test/test_task_graph_runner.cc
+++ cc/test/test_task_graph_runner.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cc {
 
 TestTaskGraphRunner::TestTaskGraphRunner()
-    : worker_thread_(this, "CompositorWorker") {
+    : worker_thread_(this, "CompositorTileWorker1") {
   worker_thread_.Start();
 }
 
