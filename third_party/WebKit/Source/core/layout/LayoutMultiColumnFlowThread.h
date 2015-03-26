141e7d7626f4d6e19ec344b5646e6da4ef3ea241
mstensho@opera.com <mstensho@opera.com>
2015-03-26 18:01:17 +0000

    [New Multicolumn] mapAbsoluteToLocalPoint() needs to convert to flow thread coordinates.
    mapAbsoluteToLocalPoint() works on visual coordinates, so pretending that it's
    a flow thread coordinate (and then mapping it to a visual coordinate again, why
    not) on the way up the tree is just going to cause trouble.
    
    Instead we need to convert from visual to flow thread coordinates on our way
    back down the tree, when entering the flow thread on our way to the target
    element. Sadly, we also need to cancel out a flowthread-to-visual conversion
    done in LayoutBox::offsetFromContainer().
    
    This makes fast/events/document-elementFromPoint.html pass in the new multicol
    implementation.
    
    Added a couple of new tests as well (one which actually fails in the old /
    current implementation), since the aforementioned test isn't yet run with the
    new implementation.
    
    R=dsinclair@chromium.org,jchaffraix@chromium.org
    BUG=334335
    
    Review URL: https://codereview.chromium.org/1024023002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192616 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutMultiColumnFlowThread.h
+++ third_party/WebKit/Source/core/layout/LayoutMultiColumnFlowThread.h
@@ -168,6 +168,8 @@ public:
     // Do we need to set a new width and lay out?
     virtual bool needsNewWidth() const;
 
+    virtual LayoutPoint visualPointToFlowThreadPoint(const LayoutPoint& visualPoint) const override;
+
     virtual LayoutMultiColumnSet* columnSetAtBlockOffset(LayoutUnit) const override final;
 
     void layoutColumns(bool relayoutChildren, SubtreeLayoutScope&);
