6d9ec29c9cd788bfcc8b010e7974541603befa76
servolk <servolk@chromium.org>
2015-03-25 18:28:49 -0700

    Cleanup redundant mime_util.h includes
    Should make clearer the remaining dependencies of net/ on mime_util.h
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1028233004
    
    Cr-Commit-Position: refs/heads/master@{#322289}

--- net/base/mime_sniffer.cc
+++ net/base/mime_sniffer.cc
@@ -101,7 +101,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/metrics/histogram.h"
 #include "base/strings/string_util.h"
-#include "net/base/mime_util.h"
 #include "url/gurl.h"
 
 namespace net {
