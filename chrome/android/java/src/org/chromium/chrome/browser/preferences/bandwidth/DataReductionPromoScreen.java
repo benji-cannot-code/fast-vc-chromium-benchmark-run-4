bda30643275dc96d783e0a9dab8c165a7efd985c
megjablon <megjablon@chromium.org>
2015-03-26 15:18:21 -0700

    Data Saver promo changes
    Tweaks to the Data Saver promo currently shown on Chrome
    Android to improve the messaging and the call-to-action.
    Adds a clickable link to the settings page and makes the
    enable button, renamed to "Turn on data saver," directly
    enable the proxy.
    
    BUG=455267
    
    Review URL: https://codereview.chromium.org/1011363006
    
    Cr-Commit-Position: refs/heads/master@{#322480}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/bandwidth/DataReductionPromoScreen.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/bandwidth/DataReductionPromoScreen.java
@@ -12,10 +12,15 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.chromium.base.ApplicationStatus;
 import org.chromium.chrome.R;
@@ -23,6 +28,8 @@ import org.chromium.chrome.browser.ChromiumApplication;
 import org.chromium.chrome.browser.metrics.UmaBridge;
 import org.chromium.chrome.browser.net.spdyproxy.DataReductionProxySettings;
 import org.chromium.chrome.browser.preferences.PreferencesLauncher;
+import org.chromium.ui.text.SpanApplier;
+import org.chromium.ui.text.SpanApplier.SpanInfo;
 
 /**
  * The promo screen encouraging users to enable Data Saver.
@@ -88,6 +95,7 @@ public class DataReductionPromoScreen extends Dialog implements View.OnClickList
         getWindow().setLayout(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
 
         addListenerOnButton();
+        addClickableSpan();
 
         mState = ACTION_DISMISSED_FIRST_SCREEN;
         UmaBridge.dataReductionProxyPromoDisplayed();
@@ -103,6 +111,32 @@ public class DataReductionPromoScreen extends Dialog implements View.OnClickList
         for (int interactiveViewId : interactiveViewIds) {
             findViewById(interactiveViewId).setOnClickListener(this);
         }
+    }
+
+    private void addClickableSpan() {
+        TextView settings_link = (TextView) findViewById(R.id.data_reduction_title_2_link);
+        ClickableSpan clickableSettingsSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                Context context = getContext();
+                if (context == null) return;
+                Intent intent = PreferencesLauncher.createIntentForSettingsPage(
+                        context, BandwidthReductionPreferences.class.getName());
+                intent.putExtra(FROM_PROMO, true);
+                context.startActivity(intent);
+                dismiss();
+            }
+
+            @Override
+            public void updateDrawState(TextPaint textPaint) {
+                textPaint.setColor(textPaint.linkColor);
+                textPaint.setUnderlineText(false);
+            }
+        };
+        settings_link.setMovementMethod(LinkMovementMethod.getInstance());
+        settings_link.setText(SpanApplier.applySpans(
+                getContext().getString(R.string.data_reduction_title_2),
+                new SpanInfo("<link>", "</link>", clickableSettingsSpan)));
     }
 
     @Override
@@ -127,13 +161,12 @@ public class DataReductionPromoScreen extends Dialog implements View.OnClickList
     }
 
     private void handleEnableButtonPressed() {
-        Context context = getContext();
-        if (context == null) return;
-        Intent intent = PreferencesLauncher.createIntentForSettingsPage(context,
-                BandwidthReductionPreferences.class.getName());
-        intent.putExtra(FROM_PROMO, true);
-        context.startActivity(intent);
+        DataReductionProxySettings.getInstance().setDataReductionProxyEnabled(
+                getContext(), true);
+        UmaBridge.dataReductionProxyTurnedOnFromPromo();
         dismiss();
+        Toast.makeText(getContext(), getContext().getString(R.string.data_reduction_enabled_toast),
+                Toast.LENGTH_LONG).show();
     }
 
     private void handleCloseButtonPressed() {
