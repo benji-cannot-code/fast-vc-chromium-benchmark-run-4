388e402cb63eac7e49bc6ce72af0455e20ab0dad
finnur <finnur@chromium.org>
2015-03-27 04:02:59 -0700

    Fix accessibility problem for ExpandablePreferenceGroup.
    BUG=467552
    
    Review URL: https://codereview.chromium.org/1032353004
    
    Cr-Commit-Position: refs/heads/master@{#322549}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/ExpandablePreferenceGroup.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/ExpandablePreferenceGroup.java
@@ -26,8 +26,11 @@ import java.util.Locale;
  * A preference category that accepts clicks for toggling on/off.
  */
 public class ExpandablePreferenceGroup extends PreferenceGroup {
-    private Drawable mDrawable = null;
-    private ImageView mImageView = null;
+    private Drawable mDrawable;
+    private ImageView mImageView;
+
+    // Whether the PreferenceGroup is in an expanded or collapsed state.
+    private boolean mExpanded;
 
     public ExpandablePreferenceGroup(Context context, AttributeSet attrs) {
         super(context, attrs, android.R.attr.preferenceStyle);
@@ -64,6 +67,10 @@ public class ExpandablePreferenceGroup extends PreferenceGroup {
         setTitle(spannable);
     }
 
+    public void setExpanded(boolean expanded) {
+        mExpanded = expanded;
+    }
+
     @Override
     public void setIcon(Drawable drawable) {
         mDrawable = drawable;
@@ -75,5 +82,11 @@ public class ExpandablePreferenceGroup extends PreferenceGroup {
         super.onBindView(view);
         mImageView = (ImageView) view.findViewById(R.id.expando);
         if (mDrawable != null) mImageView.setImageDrawable(mDrawable);
+
+        // For accessibility, read out the whole title and whether the group is collapsed/expanded.
+        String description = getTitle() + getContext().getResources().getString(mExpanded
+                ? R.string.accessibility_expanded_group
+                : R.string.accessibility_collapsed_group);
+        view.setContentDescription(description);
     }
 }
--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/WebsitePreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/WebsitePreferences.java
@@ -244,6 +244,7 @@ public class WebsitePreferences extends PreferenceFragment
         allowedGroup.setGroupTitle(resourceId, numAllowed);
         TintedDrawable icon = TintedDrawable.constructTintedDrawable(getResources(),
                 mAllowListExpanded ? R.drawable.ic_expand : R.drawable.ic_collapse);
+        allowedGroup.setExpanded(mAllowListExpanded);
         allowedGroup.setIcon(icon);
     }
 
@@ -260,6 +261,7 @@ public class WebsitePreferences extends PreferenceFragment
         blockedGroup.setGroupTitle(R.string.website_settings_blocked_group_heading, numBlocked);
         TintedDrawable icon = TintedDrawable.constructTintedDrawable(getResources(),
                 mBlockListExpanded ? R.drawable.ic_expand : R.drawable.ic_collapse);
+        blockedGroup.setExpanded(mBlockListExpanded);
         blockedGroup.setIcon(icon);
     }
 
