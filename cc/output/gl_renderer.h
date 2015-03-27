400d53df054d7ddbbb2c72f8a31929d82b2f5097
ericrk <ericrk@chromium.org>
2015-03-27 11:56:31 -0700

    Remove SetDrawViewport
    This change removes SetDrawViewport, instead tracking the current window
    viewport in DirectRenderer.
    
    Review URL: https://codereview.chromium.org/1035863002
    
    Cr-Commit-Position: refs/heads/master@{#322615}

--- cc/output/gl_renderer.h
+++ cc/output/gl_renderer.h
@@ -109,7 +109,6 @@ class CC_EXPORT GLRenderer : public DirectRenderer {
   bool BindFramebufferToTexture(DrawingFrame* frame,
                                 const ScopedResource* resource,
                                 const gfx::Rect& target_rect) override;
-  void SetDrawViewport(const gfx::Rect& window_space_viewport) override;
   void SetScissorTestRect(const gfx::Rect& scissor_rect) override;
   void PrepareSurfaceForPass(DrawingFrame* frame,
                              SurfaceInitializationMode initialization_mode,
@@ -152,6 +151,7 @@ class CC_EXPORT GLRenderer : public DirectRenderer {
 
   void DiscardPixels();
   void ClearFramebuffer(DrawingFrame* frame);
+  void SetViewport();
 
   void DrawCheckerboardQuad(const DrawingFrame* frame,
                             const CheckerboardDrawQuad* quad,
@@ -488,7 +488,6 @@ class CC_EXPORT GLRenderer : public DirectRenderer {
 
   gfx::Rect swap_buffer_rect_;
   gfx::Rect scissor_rect_;
-  gfx::Rect viewport_;
   bool is_backbuffer_discarded_;
   bool is_using_bind_uniform_;
   bool is_scissor_enabled_;
