8bbd7b0596abb673797f72c8b47181724005b4f7
mkwst@chromium.org <mkwst@chromium.org>
2015-03-27 16:56:29 +0000

    Upgrade: Measure websocket upgrades as well as other subresources.
    I missed WebSockets in https://codereview.chromium.org/1035683003.
    
    BUG=455674
    R=yoav@yoav.ws
    
    Review URL: https://codereview.chromium.org/1033243003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192689 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/websockets/DOMWebSocket.cpp
+++ third_party/WebKit/Source/modules/websockets/DOMWebSocket.cpp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/ConsoleTypes.h"
 #include "core/frame/LocalDOMWindow.h"
 #include "core/frame/LocalFrame.h"
+#include "core/frame/UseCounter.h"
 #include "core/frame/csp/ContentSecurityPolicy.h"
 #include "core/inspector/ConsoleMessage.h"
 #include "core/inspector/ScriptCallStack.h"
@@ -283,6 +284,7 @@ void DOMWebSocket::connect(const String& url, const Vector<String>& protocols, E
     m_url = KURL(KURL(), url);
 
     if (executionContext()->securityContext().insecureRequestsPolicy() == SecurityContext::InsecureRequestsUpgrade && m_url.protocol() == "ws") {
+        UseCounter::count(executionContext(), UseCounter::UpgradeInsecureRequestsUpgradedRequest);
         m_url.setProtocol("wss");
         if (m_url.port() == 80)
             m_url.setPort(443);
