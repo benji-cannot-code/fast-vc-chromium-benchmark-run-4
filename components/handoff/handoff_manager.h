e7f7eacab92be7601c63f548690b341e71447a4a
erikchen <erikchen@chromium.org>
2015-03-27 08:29:38 -0700

    handoff: Fix the origin so that it correctly reflects the sender.
    This broke when I migrated the Handoff Manager from chrome/ into
    components/
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1000723003
    
    Cr-Commit-Position: refs/heads/master@{#322581}

--- components/handoff/handoff_manager.h
+++ components/handoff/handoff_manager.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <Foundation/Foundation.h>
 
 #include "base/mac/objc_property_releaser.h"
+#include "components/handoff/handoff_utility.h"
 #include "url/gurl.h"
 
 @class NSUserActivity;
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   GURL _activeURL;
   NSUserActivity* _userActivity;
+  handoff::Origin _origin;
 }
 
 // The active URL is defined as the URL of the most recently accessed tab. This
