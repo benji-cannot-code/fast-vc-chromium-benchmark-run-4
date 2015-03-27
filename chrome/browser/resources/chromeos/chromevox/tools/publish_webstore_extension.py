a56d4b2849f149de7682e6ff3c084680601db75a
dtseng <dtseng@chromium.org>
2015-03-27 15:56:15 -0700

    Enable chrome.automation.getDesktop on all aura platforms.
    - on Chrome OS, continue to enumerate all Aura windows when building the desktop tree via the Ash shell.
    - on all other Aura platforms, lazily hook up top level windows that have been created in our object cache. This works by only presenting cached windows as children of the desktop node. Since serialization walks up the tree, we are guaranteed windows in the cache whenever a descendant view has an event fired upon it.
    
    - allow ChromeVox to traverse the desktop automation tree on all platforms.
    
    TEST=build ChromeVox webstore extension. Run it on linux chromium. Verify views-based UI reads.
    
    Review URL: https://codereview.chromium.org/1003283002
    
    Cr-Commit-Position: refs/heads/master@{#322666}

--- chrome/browser/resources/chromeos/chromevox/tools/publish_webstore_extension.py
+++ chrome/browser/resources/chromeos/chromevox/tools/publish_webstore_extension.py
@@ -37,7 +37,6 @@ import version
 
 # A list of files (or directories) to exclude from the webstore build.
 EXCLUDE_PATHS = [
-    'cvox2/background/',
     'manifest.json',
     'manifest_guest.json',
     ]
