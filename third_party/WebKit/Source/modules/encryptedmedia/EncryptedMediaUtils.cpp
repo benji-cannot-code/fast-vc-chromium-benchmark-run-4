773c2c91e203184a82bd9b5b471afeaa5074178e
sandersd@chromium.org <sandersd@chromium.org>
2015-03-28 02:00:42 +0000

    Expose sessionTypes in MediaKeySystemConfiguration.
    BUG=470711
    
    Review URL: https://codereview.chromium.org/1034083002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192709 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/encryptedmedia/EncryptedMediaUtils.cpp
+++ third_party/WebKit/Source/modules/encryptedmedia/EncryptedMediaUtils.cpp
@@ -8,6 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+namespace {
+
+const char kTemporary[] = "temporary";
+const char kPersistentLicense[] = "persistent-license";
+const char kPersistentReleaseMessage[] = "persistent-release-message";
+
+} // namespace
+
 WebEncryptedMediaInitDataType EncryptedMediaUtils::convertToInitDataType(const String& initDataType)
 {
     if (initDataType == "cenc")
@@ -31,6 +39,8 @@ String EncryptedMediaUtils::convertFromInitDataType(WebEncryptedMediaInitDataTyp
     case WebEncryptedMediaInitDataType::Webm:
         return "webm";
     case WebEncryptedMediaInitDataType::Unknown:
+        // Chromium should not use Unknown, but we use it in Blink when the
+        // actual value has been blocked for non-same-origin or mixed content.
         return String();
     }
 
@@ -40,15 +50,34 @@ String EncryptedMediaUtils::convertFromInitDataType(WebEncryptedMediaInitDataTyp
 
 WebEncryptedMediaSessionType EncryptedMediaUtils::convertToSessionType(const String& sessionType)
 {
-    if (sessionType == "temporary")
+    if (sessionType == kTemporary)
         return WebEncryptedMediaSessionType::Temporary;
-    if (sessionType == "persistent-license")
+    if (sessionType == kPersistentLicense)
         return WebEncryptedMediaSessionType::PersistentLicense;
-    if (sessionType == "persistent-release-message")
+    if (sessionType == kPersistentReleaseMessage)
         return WebEncryptedMediaSessionType::PersistentReleaseMessage;
 
-    ASSERT_NOT_REACHED();
+    // |sessionType| is not restricted in the idl, so anything is possible.
     return WebEncryptedMediaSessionType::Unknown;
+}
+
+String EncryptedMediaUtils::convertFromSessionType(WebEncryptedMediaSessionType sessionType)
+{
+    switch (sessionType) {
+    case WebEncryptedMediaSessionType::Temporary:
+        return kTemporary;
+    case WebEncryptedMediaSessionType::PersistentLicense:
+        return kPersistentLicense;
+    case WebEncryptedMediaSessionType::PersistentReleaseMessage:
+        return kPersistentReleaseMessage;
+    case WebEncryptedMediaSessionType::Unknown:
+        // Chromium should not use Unknown.
+        ASSERT_NOT_REACHED();
+        return String();
+    }
+
+    ASSERT_NOT_REACHED();
+    return String();
 }
 
 } // namespace blink
