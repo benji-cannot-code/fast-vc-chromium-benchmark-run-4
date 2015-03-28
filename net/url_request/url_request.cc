60ffd1ea7fe6b9bf235642022d546b46a7551e9c
jww <jww@chromium.org>
2015-03-27 17:22:51 -0700

    Set Origin header to "null" for cross origin redirects.
    In order to avoid invaliding CSRF protections on the server, it is
    necessary to set the Origin header to "null" on cross origin redirect.
    This avoids problems where a malicious server redirects a POST request
    to a sender to cause a confused deputy problem where the server believes
    the request originated with the Origin header origin.
    
    This adds a check to url_request.cc where if the redirect is cross
    origin and an Origin header is present, this sets the Origin header to
    "null." If there is not Origin header, it leaves it as-is.
    
    R=rsleevi@chromium.org
    BUG=465517
    
    Review URL: https://codereview.chromium.org/1017583002
    
    Cr-Commit-Position: refs/heads/master@{#322685}

--- net/url_request/url_request.cc
+++ net/url_request/url_request.cc
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_job_manager.h"
 #include "net/url_request/url_request_netlog_params.h"
 #include "net/url_request/url_request_redirect_job.h"
+#include "url/gurl.h"
+#include "url/origin.h"
 
 using base::Time;
 using std::string;
@@ -55,6 +57,8 @@ void StripPostSpecificHeaders(HttpRequestHeaders* headers) {
   // These are headers that may be attached to a POST.
   headers->RemoveHeader(HttpRequestHeaders::kContentLength);
   headers->RemoveHeader(HttpRequestHeaders::kContentType);
+  // TODO(jww): This is Origin header removal is probably layering violation and
+  // should be refactored into //content. See https://crbug.com/471397.
   headers->RemoveHeader(HttpRequestHeaders::kOrigin);
 }
 
@@ -939,6 +943,29 @@ int URLRequest::Redirect(const RedirectInfo& redirect_info) {
     }
     upload_data_stream_.reset();
     method_ = redirect_info.new_method;
+  }
+
+  // Cross-origin redirects should not result in an Origin header value that is
+  // equal to the original request's Origin header. This is necessary to prevent
+  // a reflection of POST requests to bypass CSRF protections. If the header was
+  // not set to "null", a POST request from origin A to a malicious origin M
+  // could be redirected by M back to A.
+  //
+  // In the Section 4.2, Step 4.10 of the Fetch spec
+  // (https://fetch.spec.whatwg.org/#concept-http-fetch), it states that on
+  // cross-origin 301, 302, 303, 307, and 308 redirects, the user agent should
+  // set the request's origin to an "opaque identifier," which serializes to
+  // "null." This matches Firefox and IE behavior, although it supercedes the
+  // suggested behavior in RFC 6454, "The Web Origin Concept."
+  //
+  // See also https://crbug.com/465517.
+  //
+  // TODO(jww): This is probably layering violation and should be refactored
+  // into //content. See https://crbug.com/471397.
+  if (redirect_info.new_url.GetOrigin() != url().GetOrigin() &&
+      extra_request_headers_.HasHeader(HttpRequestHeaders::kOrigin)) {
+    extra_request_headers_.SetHeader(HttpRequestHeaders::kOrigin,
+                                     url::Origin().string());
   }
 
   referrer_ = redirect_info.new_referrer;
