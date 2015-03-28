35acea077245afb1cd9e3ce4d0dc184b9663180b
emircan <emircan@chromium.org>
2015-03-27 17:39:53 -0700

    This CL adds support for MEDIASUBTYPE_RGB32 in Windows platform.
    MEDIASUBTYPE_RGB32 is observed to be equivalent to MEDIASUBTYPE_ARGB32 in terms of color space conversion. This CL adds support for this sub type by directing it to PIXEL_FORMAT_ARGB VideoPixelFormat. It is also observed that output is flipped as it happens on PIXEL_FORMAT_RGB24 [0]. This is solved by adding a Windows specific flip switch.
    
    [0] https://code.google.com/p/chromium/codesearch#chromium/src/content/browser/renderer_host/media/video_capture_device_client.cc&l=156
    
    BUG=464855
    BUG=389542
    TEST=AppRTC loopback was tested with DXTory capturing on Solitaire.
    
    Review URL: https://codereview.chromium.org/1036063002
    
    Cr-Commit-Position: refs/heads/master@{#322687}

--- media/base/video_capture_types.cc
+++ media/base/video_capture_types.cc
@@ -48,6 +48,7 @@ size_t VideoCaptureFormat::ImageAllocationSize() const {
     case PIXEL_FORMAT_RGB24:
       result_frame_size *= 3;
       break;
+    case PIXEL_FORMAT_RGB32:
     case PIXEL_FORMAT_ARGB:
       result_frame_size *= 4;
       break;
@@ -81,6 +82,8 @@ std::string VideoCaptureFormat::PixelFormatToString(VideoPixelFormat format) {
     return "UYVY";
   case PIXEL_FORMAT_RGB24:
     return "RGB24";
+  case PIXEL_FORMAT_RGB32:
+    return "RGB32";
   case PIXEL_FORMAT_ARGB:
     return "ARGB";
   case PIXEL_FORMAT_MJPEG:
