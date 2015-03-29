1a73f7eaf3974126e3e4a0980cba43718270f347
stapelberg <stapelberg@google.com>
2015-03-29 08:12:02 -0700

    hi-dpi: convert _NET_WORKAREA from pixels.
    Just like the gfx::Display member |bounds_| is not stored in pixels, we need to
    convert |work_area_| from pixels.
    
    Both are used in MenuController::UpdateInitialLocation (|work_area_| is
    preferred over |bounds_|), which gets events with already converted
    coordinates.
    
    BUG=469373,143619
    
    Review URL: https://codereview.chromium.org/1036263003
    
    Cr-Commit-Position: refs/heads/master@{#322724}

--- ui/views/widget/desktop_aura/desktop_screen_x11.cc
+++ ui/views/widget/desktop_aura/desktop_screen_x11.cc
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/util/x11/edid_parser_x11.h"
 #include "ui/events/platform/platform_event_source.h"
 #include "ui/gfx/display.h"
+#include "ui/gfx/geometry/point_conversions.h"
+#include "ui/gfx/geometry/size_conversions.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/gfx/screen.h"
 #include "ui/gfx/x/x11_types.h"
@@ -285,11 +287,11 @@ std::vector<gfx::Display> DesktopScreenX11::BuildDisplaysFromXRandRInfo() {
   }
 
   bool has_work_area = false;
-  gfx::Rect work_area;
+  gfx::Rect work_area_in_pixels;
   std::vector<int> value;
   if (ui::GetIntArrayProperty(x_root_window_, "_NET_WORKAREA", &value) &&
       value.size() >= 4) {
-    work_area = gfx::Rect(value[0], value[1], value[2], value[3]);
+    work_area_in_pixels = gfx::Rect(value[0], value[1], value[2], value[3]);
     has_work_area = true;
   }
 
@@ -332,9 +334,17 @@ std::vector<gfx::Display> DesktopScreenX11::BuildDisplaysFromXRandRInfo() {
       }
 
       if (has_work_area) {
-        gfx::Rect intersection = crtc_bounds;
-        intersection.Intersect(work_area);
-        display.set_work_area(intersection);
+        gfx::Rect intersection_in_pixels = crtc_bounds;
+        intersection_in_pixels.Intersect(work_area_in_pixels);
+        // SetScaleAndBounds() above does the conversion from pixels to DIP for
+        // us, but set_work_area does not, so we need to do it here.
+        display.set_work_area(gfx::Rect(
+            gfx::ToFlooredPoint(
+                gfx::ScalePoint(intersection_in_pixels.origin(),
+                                1.0f / display.device_scale_factor())),
+            gfx::ToFlooredSize(
+                gfx::ScaleSize(intersection_in_pixels.size(),
+                               1.0f / display.device_scale_factor()))));
       }
 
       switch (crtc->rotation) {
