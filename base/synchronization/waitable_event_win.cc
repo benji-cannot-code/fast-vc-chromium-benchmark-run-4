2541e4ac8f220c248193d402532d9b7dd0b71435
rvargas <rvargas@chromium.org>
2015-03-30 12:12:44 -0700

    Revert "Make sure that WaitableEvent::TimedWait works fine with large timeouts."
    This reverts commit ac3c7563ee4a116ec66cbec8679afc9ec1a61da4.
    
    BUG=471426, 465948
    TBR=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1048883002
    
    Cr-Commit-Position: refs/heads/master@{#322829}

--- base/synchronization/waitable_event_win.cc
+++ base/synchronization/waitable_event_win.cc
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/synchronization/waitable_event.h"
 
+#include <math.h>
 #include <windows.h>
 
 #include "base/logging.h"
-#include "base/numerics/safe_conversions.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
 
@@ -38,7 +38,7 @@ void WaitableEvent::Signal() {
 }
 
 bool WaitableEvent::IsSignaled() {
-  return TimedWait(TimeDelta());
+  return TimedWait(TimeDelta::FromMilliseconds(0));
 }
 
 void WaitableEvent::Wait() {
@@ -51,13 +51,13 @@ void WaitableEvent::Wait() {
 
 bool WaitableEvent::TimedWait(const TimeDelta& max_time) {
   base::ThreadRestrictions::AssertWaitAllowed();
-  DCHECK_GE(max_time, TimeDelta());
+  DCHECK(max_time >= TimeDelta::FromMicroseconds(0));
   // Be careful here.  TimeDelta has a precision of microseconds, but this API
   // is in milliseconds.  If there are 5.5ms left, should the delay be 5 or 6?
   // It should be 6 to avoid returning too early.
-  DWORD timeout = saturated_cast<DWORD>(max_time.InMillisecondsRoundedUp());
-
-  DWORD result = WaitForSingleObject(handle_.Get(), timeout);
+  double timeout = ceil(max_time.InMillisecondsF());
+  DWORD result = WaitForSingleObject(handle_.Get(),
+                                     static_cast<DWORD>(timeout));
   switch (result) {
     case WAIT_OBJECT_0:
       return true;
