23e10a9e8c947fae8fff7b77dfcd2033583fc92b
xiyuan <xiyuan@chromium.org>
2015-03-30 10:17:51 -0700

    bootstrap: Use Smart Lock keys for existing users.
    - Refactor EasyUnlockService and EasyUnlockAuthAttempt to have
      a callback for finalized unlock/sign-in;
    - Change EasyBootstrapUserContextInitialize to try Smart lock keys
      for existing users;
    - Only do auto pairing and trim "legacy-0" key when a random key is
      used to create cryptohome;
    
    BUG=447006
    
    Review URL: https://codereview.chromium.org/1007923003
    
    Cr-Commit-Position: refs/heads/master@{#322800}

--- chrome/browser/signin/easy_unlock_service_observer.h
+++ chrome/browser/signin/easy_unlock_service_observer.h
@@ -6,10 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SIGNIN_EASY_UNLOCK_SERVICE_OBSERVER_H_
 #define CHROME_BROWSER_SIGNIN_EASY_UNLOCK_SERVICE_OBSERVER_H_
 
+#include "chrome/browser/signin/easy_unlock_screenlock_state_handler.h"
+
 class EasyUnlockServiceObserver {
  public:
   // Invoked when turn-off operation status changes.
-  virtual void OnTurnOffOperationStatusChanged() = 0;
+  virtual void OnTurnOffOperationStatusChanged() {}
+
+  // Invoked when screenlock state changes.
+  virtual void OnScreenlockStateChanged(
+      EasyUnlockScreenlockStateHandler::State state) {}
 
  protected:
   virtual ~EasyUnlockServiceObserver() {}
