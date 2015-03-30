70f969c48eb8d23054788e059f441d450c32de47
rob <rob@robwu.nl>
2015-03-30 14:14:02 -0700

    Improve error message for failing code insertion on about:-URLs
    Currently, extension developers get the following message:
    "Cannot access contents of url "about:blank". Extension manifest must
     request permission to access this host."
    
    Extensions cannot request permission for about:-URLs, so this message is
    useless. The message has been changed to: "Cannot access contents of url
    "about:blank". Extension must have permission to access the frame's
    origin, and matchAboutBlank must be true."
    
    This message will be used in https://codereview.chromium.org/952473002/
    
    R=kalman@chromium.org
    
    Review URL: https://codereview.chromium.org/945743003
    
    Cr-Commit-Position: refs/heads/master@{#322865}

--- extensions/common/manifest_constants.h
+++ extensions/common/manifest_constants.h
@@ -243,6 +243,7 @@ extern const char kAllURLOrActiveTabNeeded[];
 extern const char kAppsNotEnabled[];
 extern const char kBackgroundPermissionNeeded[];
 extern const char kBackgroundRequiredForPlatformApps[];
+extern const char kCannotAccessAboutUrl[];
 extern const char kCannotAccessChromeUrl[];
 extern const char kCannotAccessExtensionUrl[];
 extern const char kCannotAccessPage[];
--- extensions/renderer/programmatic_script_injector.cc
+++ extensions/renderer/programmatic_script_injector.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/values.h"
+#include "content/public/common/url_constants.h"
 #include "content/public/renderer/render_view.h"
 #include "extensions/common/error_utils.h"
 #include "extensions/common/extension_messages.h"
@@ -30,6 +31,8 @@ ProgrammaticScriptInjector::ProgrammaticScriptInjector(
       render_view_(content::RenderView::FromWebView(web_frame->view())),
       results_(new base::ListValue()),
       finished_(false) {
+  effective_url_ = ScriptContext::GetEffectiveDocumentURL(
+      web_frame, url_, params.match_about_blank);
 }
 
 ProgrammaticScriptInjector::~ProgrammaticScriptInjector() {
@@ -128,8 +131,14 @@ void ProgrammaticScriptInjector::OnWillNotInject(InjectFailureReason reason) {
   std::string error;
   switch (reason) {
     case NOT_ALLOWED:
-      error = ErrorUtils::FormatErrorMessage(manifest_errors::kCannotAccessPage,
-                                             url_.spec());
+      if (url_.SchemeIs(url::kAboutScheme)) {
+        error = ErrorUtils::FormatErrorMessage(
+            manifest_errors::kCannotAccessAboutUrl, url_.spec(),
+            effective_url_.GetOrigin().spec());
+      } else {
+        error = ErrorUtils::FormatErrorMessage(
+            manifest_errors::kCannotAccessPage, url_.spec());
+      }
       break;
     case EXTENSION_REMOVED:  // no special error here.
     case WONT_INJECT:
