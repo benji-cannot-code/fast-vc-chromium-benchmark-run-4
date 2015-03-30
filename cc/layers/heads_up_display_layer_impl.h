163b74bd58bc4a8c4147bdbc22cc56b6a1df03fb
jianli <jianli@chromium.org>
2015-03-30 13:43:30 -0700

    Revert of Fix text clipping in CompositorFrameTime Display (patchset #2 id:20001 of https://codereview.chromium.org/1027393004/)
    Reason for revert:
    Speculative revert for Blink layout test failures in WebKit Mac10.8 (retina):
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.8%20%28retina%29/builds/27696
    
    Original issue's description:
    > Fix text clipping in CompositorFrameTime Display
    >
    > Because different OS have different fonts for HUD.
    > Set the width of CompositorFrameTime Display to be maximum
    > of graph width and text width to prevent text clipping.
    >
    > BUG=445877
    >
    > Committed: https://crrev.com/88b4cf8d33b499f972e1c447e741230170beeb8d
    > Cr-Commit-Position: refs/heads/master@{#322807}
    
    TBR=danakj@chromium.org,behara.ms@samsung.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=445877
    
    Review URL: https://codereview.chromium.org/1047013002
    
    Cr-Commit-Position: refs/heads/master@{#322858}

--- cc/layers/heads_up_display_layer_impl.h
+++ cc/layers/heads_up_display_layer_impl.h
@@ -78,7 +78,6 @@ class CC_EXPORT HeadsUpDisplayLayerImpl : public LayerImpl {
   void UpdateHudContents();
   void DrawHudContents(SkCanvas* canvas);
 
-  int MeasureText(SkPaint* paint, const std::string& text, int size) const;
   void DrawText(SkCanvas* canvas,
                 SkPaint* paint,
                 const std::string& text,
