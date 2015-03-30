85f8e13446eeee33d3c222ca2085a3a14590671f
melandory <melandory@chromium.org>
2015-03-30 13:00:41 -0700

    [Smart Lock] Add avatar picture to account chooser infobar.
    After this change account chooser infobar will be able to display avatar
    of the account.
    
    BUG=454815
    
    Review URL: https://codereview.chromium.org/1016393002
    
    Cr-Commit-Position: refs/heads/master@{#322844}

--- chrome/android/java/src/org/chromium/chrome/browser/password_manager/Credential.java
+++ chrome/android/java/src/org/chromium/chrome/browser/password_manager/Credential.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.password_manager;
 
+import android.graphics.Bitmap;
+
 import org.chromium.base.CalledByNative;
 
 /**
@@ -17,6 +19,7 @@ public class Credential {
     private final String mFederation;
     private final int mType;
     private final int mIndex;
+    private Bitmap mAvatar;
 
     /**
      * @param username username which is used to authenticate user.
@@ -34,6 +37,7 @@ public class Credential {
         mFederation = federation;
         mType = type;
         mIndex = index;
+        mAvatar = null;
     }
 
     public String getUsername() {
@@ -54,6 +58,14 @@ public class Credential {
 
     public int getType() {
         return mType;
+    }
+
+    public Bitmap getAvatar() {
+        return mAvatar;
+    }
+
+    public void setBitmap(Bitmap avatar) {
+        mAvatar = avatar;
     }
 
     @CalledByNative
