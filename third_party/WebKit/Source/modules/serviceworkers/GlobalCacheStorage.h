0104a93862c11850299f7e69a0174fd831e58b26
jsbell@chromium.org <jsbell@chromium.org>
2015-03-30 22:10:06 +0000

    Expose Cache Storage API in global window/worker scope
    Exposes self.caches which accesses the origin's CacheStorage instance.
    Requires the --enable-experimental-web-platform-features flag, otherwise
    the API is only available in Service Workers.
    
    https://slightlyoff.github.io/ServiceWorker/spec/service_worker/#cache
    
    BUG=439389
    
    Review URL: https://codereview.chromium.org/1032623008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192778 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/modules/serviceworkers/GlobalCacheStorage.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GlobalCacheStorage_h
+#define GlobalCacheStorage_h
+
+namespace blink {
+
+class CacheStorage;
+class DOMWindow;
+class ExceptionState;
+class ScriptState;
+class WorkerGlobalScope;
+
+class GlobalCacheStorage {
+public:
+    static CacheStorage* caches(ScriptState*, DOMWindow&, ExceptionState&);
+    static CacheStorage* caches(ScriptState*, WorkerGlobalScope&, ExceptionState&);
+};
+
+} // namespace blink
+
+#endif // GlobalCacheStorage_h
