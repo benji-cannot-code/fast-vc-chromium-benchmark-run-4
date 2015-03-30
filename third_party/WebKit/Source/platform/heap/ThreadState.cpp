79c85efdeb802a68435024bbc80714403b95b855
haraken@chromium.org <haraken@chromium.org>
2015-03-30 15:01:36 +0000

    Oilpan: Remove m_hasPendingIdleTask
    What we really want to do is not to schedule a precise GC or an idle GC when a gcState is not NoGCScheduled. By adding the condition, we no longer need the m_hasPendingIdleTask because it's no longer possible that two idle GCs are scheduled in a row. (Note: Even if two idle GCs are scheduled, the second idle GC is just ignored in performIdleGC(), so it wouldn't be a big deal.)
    
    Review URL: https://codereview.chromium.org/1041123002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192759 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/ThreadState.cpp
+++ third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -91,7 +91,6 @@ ThreadState::ThreadState()
     , m_safePointScopeMarker(nullptr)
     , m_atSafePoint(false)
     , m_interruptors()
-    , m_hasPendingIdleTask(false)
     , m_didV8GCAfterLastGC(false)
     , m_sweepForbidden(false)
     , m_noAllocationCount(0)
@@ -507,6 +506,8 @@ Mutex& ThreadState::globalRootsMutex()
 // These heuristics affect performance significantly.
 bool ThreadState::shouldScheduleIdleGC()
 {
+    if (gcState() != NoGCScheduled)
+        return false;
 #if ENABLE(OILPAN)
     // Trigger garbage collection on a 50% increase in size since the last GC,
     // but not for less than 512 KB.
@@ -521,6 +522,8 @@ bool ThreadState::shouldScheduleIdleGC()
 // These heuristics affect performance significantly.
 bool ThreadState::shouldSchedulePreciseGC()
 {
+    if (gcState() != NoGCScheduled)
+        return false;
 #if ENABLE(OILPAN)
     return false;
 #else
@@ -586,8 +589,6 @@ void ThreadState::performIdleGC(double deadlineSeconds)
 {
     ASSERT(isMainThread());
 
-    m_hasPendingIdleTask = false;
-
     if (gcState() != IdleGCScheduled)
         return;
 
@@ -612,10 +613,7 @@ void ThreadState::scheduleIdleGC()
         return;
     }
 
-    if (!m_hasPendingIdleTask) {
-        m_hasPendingIdleTask = true;
-        Scheduler::shared()->postNonNestableIdleTask(FROM_HERE, WTF::bind<double>(&ThreadState::performIdleGC, this));
-    }
+    Scheduler::shared()->postNonNestableIdleTask(FROM_HERE, WTF::bind<double>(&ThreadState::performIdleGC, this));
     setGCState(IdleGCScheduled);
 }
 
