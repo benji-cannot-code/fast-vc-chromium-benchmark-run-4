c9f9e40bf4b52ac8dd6073b78429eb149cfba1f9
gsennton <gsennton@chromium.org>
2015-03-30 12:22:57 -0700

    Add a string to user agent to differentiate webview from clank.
    Add a string representing webview to the user agent string for webview
    to be able to differentiate between webview and clank.
    
    BUG=461383
    
    Review URL: https://codereview.chromium.org/1035973002
    
    Cr-Commit-Position: refs/heads/master@{#322834}

--- android_webview/common/aw_content_client.h
+++ android_webview/common/aw_content_client.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace android_webview {
 
 std::string GetUserAgent();
+// extra text to be put into the OS section of the user agent text
+std::string GetExtraOSUserAgentInfo();
 
 class AwContentClient : public content::ContentClient {
  public:
