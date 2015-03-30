850627ad472dd68d2d671e4c4847a6a742796ed1
gunsch <gunsch@chromium.org>
2015-03-30 16:01:10 -0700

    Chromecast buildfix: permissions manager missing include.
    R=lcwu@chromium.org,gfhuang@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/1042273003
    
    Cr-Commit-Position: refs/heads/master@{#322896}

--- chromecast/browser/cast_permission_manager.cc
+++ chromecast/browser/cast_permission_manager.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/browser/cast_permission_manager.h"
 
 #include "base/callback.h"
+#include "base/logging.h"
 #include "content/public/browser/permission_type.h"
 
 namespace chromecast {
