d6d787fa63002e465b419d7cdac4c89dfdfa3d9b
haraken@chromium.org <haraken@chromium.org>
2015-03-31 03:40:59 +0000

    Oilpan: Count up s_markedObjectSize in markUnmarkedObjectsDead()
    This is a preparation CL for https://codereview.chromium.org/1042853002/.
    The CL will use Heap::markedObjectSize() at the beginning of the marking phase,
    so the size needs to be accurate.
    
    BUG=420515
    
    Review URL: https://codereview.chromium.org/1046873003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192794 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Heap.cpp
+++ third_party/WebKit/Source/platform/heap/Heap.cpp
@@ -1508,6 +1508,7 @@ void NormalPage::sweep()
 
 void NormalPage::markUnmarkedObjectsDead()
 {
+    size_t markedObjectSize = 0;
     for (Address headerAddress = payload(); headerAddress < payloadEnd();) {
         HeapObjectHeader* header = reinterpret_cast<HeapObjectHeader*>(headerAddress);
         ASSERT(header->size() < blinkPagePayloadSize());
@@ -1518,12 +1519,16 @@ void NormalPage::markUnmarkedObjectsDead()
             continue;
         }
         header->checkHeader();
-        if (header->isMarked())
+        if (header->isMarked()) {
             header->unmark();
-        else
+            markedObjectSize += header->size();
+        } else {
             header->markDead();
+        }
         headerAddress += header->size();
     }
+    if (markedObjectSize)
+        Heap::increaseMarkedObjectSize(markedObjectSize);
 }
 
 void NormalPage::populateObjectStartBitMap()
@@ -1766,17 +1771,19 @@ void LargeObjectPage::removeFromHeap()
 
 void LargeObjectPage::sweep()
 {
-    Heap::increaseMarkedObjectSize(size());
     heapObjectHeader()->unmark();
+    Heap::increaseMarkedObjectSize(size());
 }
 
 void LargeObjectPage::markUnmarkedObjectsDead()
 {
     HeapObjectHeader* header = heapObjectHeader();
-    if (header->isMarked())
+    if (header->isMarked()) {
         header->unmark();
-    else
+        Heap::increaseMarkedObjectSize(size());
+    } else {
         header->markDead();
+    }
 }
 
 void LargeObjectPage::checkAndMarkPointer(Visitor* visitor, Address address)
