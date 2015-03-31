c331f811ca38509557bc1c72b96a4f0c376d1a1f
aberent <aberent@chromium.org>
2015-03-31 04:26:24 -0700

    Fix missing null check
    Fixes an occasional crash caused by a missing Null check.
    
    BUG=470969
    
    Review URL: https://codereview.chromium.org/1045723003
    
    Cr-Commit-Position: refs/heads/master@{#323011}

--- chrome/android/java/src/org/chromium/chrome/browser/media/remote/DefaultMediaRouteController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/remote/DefaultMediaRouteController.java
@@ -1062,7 +1062,8 @@ public class DefaultMediaRouteController extends AbstractMediaRouteController {
     }
 
     private void playMedia() {
-        String title = getMediaStateListener().getTitle();
+        String title = null;
+        if (getMediaStateListener() != null) title = getMediaStateListener().getTitle();
         playUri(mLocalVideoUri, title, mStartPositionMillis);
     }
 
