3ca2a2358eeb69d7ad065ad707672c814d5abfce
yawano <yawano@chromium.org>
2015-03-31 02:37:02 -0700

    Implement CreateDirectory.
    BUG=413541
    TEST=manually tested.
    
    Review URL: https://codereview.chromium.org/1030523002
    
    Cr-Commit-Position: refs/heads/master@{#323004}

--- chrome/browser/media_galleries/linux/mtp_device_task_helper.h
+++ chrome/browser/media_galleries/linux/mtp_device_task_helper.h
@@ -32,6 +32,8 @@ class MTPDeviceTaskHelper {
   typedef MTPDeviceAsyncDelegate::GetFileInfoSuccessCallback
       GetFileInfoSuccessCallback;
 
+  typedef base::Closure CreateDirectorySuccessCallback;
+
   // NOTE: The file names in the entry list have their file id appended at the
   // end. e.g. foo.jpg with file id 45 becomes foo.jpg,45.
   typedef base::Callback<void(const storage::AsyncFileUtil::EntryList& entries,
@@ -70,6 +72,12 @@ class MTPDeviceTaskHelper {
   void GetFileInfo(uint32 file_id,
                    const GetFileInfoSuccessCallback& success_callback,
                    const ErrorCallback& error_callback);
+
+  // Forwards CreateDirectory request to the MediaTransferProtocolManager.
+  void CreateDirectory(const uint32 parent_id,
+                       const std::string& directory_name,
+                       const CreateDirectorySuccessCallback& success_callback,
+                       const ErrorCallback& error_callback);
 
   // Dispatches the read directory request to the MediaTransferProtocolManager.
   //
@@ -152,6 +160,11 @@ class MTPDeviceTaskHelper {
                      const ErrorCallback& error_callback,
                      const MtpFileEntry& file_entry,
                      bool error) const;
+
+  // Called when CreateDirectory completes.
+  void OnCreateDirectory(const CreateDirectorySuccessCallback& success_callback,
+                         const ErrorCallback& error_callback,
+                         const bool error) const;
 
   // Query callback for ReadDirectory().
   //
