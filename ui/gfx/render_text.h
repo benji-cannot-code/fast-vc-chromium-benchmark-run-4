5498c0036b32ed95b7948a4db30f866a87ef5419
shuchen <shuchen@chromium.org>
2015-03-30 18:39:24 -0700

    Supports the invisible underline for native input fields.
    Physical keyboard autocorrect feature needs to be launched in M43, which can sets composition with invisible underline.
    For now, views::TextField doesn't respect the config in CompositionText for colors, background, etc.
    
    This cl is to make the RenderText not render the underline if the CompositionText is configured with transparent color.
    
    Note: this cl doesn't try to support background, colors, etc. as some TODOs like https://code.google.com/p/chromium/codesearch#chromium/src/ui/gfx/render_text.h&l=723
    
    BUG=407576
    TEST=Verified on clapper & linux_chromeos.
    
    Review URL: https://codereview.chromium.org/1031533002
    
    Cr-Commit-Position: refs/heads/master@{#322942}

--- ui/gfx/render_text.h
+++ ui/gfx/render_text.h
@@ -339,7 +339,6 @@ class GFX_EXPORT RenderText {
   // boundaries.
   void SelectWord();
 
-  const Range& GetCompositionRange() const;
   void SetCompositionRange(const Range& composition_range);
 
   // Set the text color over the entire text or a logical character range.
