754df4e6989d442eb4cfe21293e75e87630c665e
smckay <smckay@chromium.org>
2015-03-30 19:50:04 -0700

    Disable Cloudbackup for RTL languages in M42, as it is not sufficiently well layed out.
    BUG=471628
    TEST=Manual
    
    Review URL: https://codereview.chromium.org/1043793004
    
    Cr-Commit-Position: refs/heads/master@{#322965}

--- ui/file_manager/file_manager/background/js/device_handler_unittest.js
+++ ui/file_manager/file_manager/background/js/device_handler_unittest.js
@@ -585,6 +585,11 @@ function setupChromeApis() {
         }
       }
     },
+    i18n: {
+      getUILanguage: function() {
+        return 'en-US'
+      }
+    },
     notifications: {
       resolver: new importer.Resolver(),
       create: function(id, params, callback) {
