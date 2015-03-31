e7f49a12dffa170a932c23d28cd4d8c7a5f99bc0
avi <avi@chromium.org>
2015-03-30 17:21:17 -0700

    Add the WebHistoryCommitType to didFailProvisionalLoad, part 1.
    BUG=471292
    TEST=none
    
    Review URL: https://codereview.chromium.org/1033233003
    
    Cr-Commit-Position: refs/heads/master@{#322923}

--- content/renderer/render_frame_impl.h
+++ content/renderer/render_frame_impl.h
@@ -405,6 +405,10 @@ class CONTENT_EXPORT RenderFrameImpl
   virtual void didFailProvisionalLoad(
       blink::WebLocalFrame* frame,
       const blink::WebURLError& error);
+  virtual void didFailProvisionalLoad(
+      blink::WebLocalFrame* frame,
+      const blink::WebURLError& error,
+      blink::WebHistoryCommitType commit_type);
   virtual void didCommitProvisionalLoad(
       blink::WebLocalFrame* frame,
       const blink::WebHistoryItem& item,
@@ -421,6 +425,9 @@ class CONTENT_EXPORT RenderFrameImpl
   virtual void didHandleOnloadEvents(blink::WebLocalFrame* frame);
   virtual void didFailLoad(blink::WebLocalFrame* frame,
                            const blink::WebURLError& error);
+  virtual void didFailLoad(blink::WebLocalFrame* frame,
+                           const blink::WebURLError& error,
+                           blink::WebHistoryCommitType commit_type);
   virtual void didFinishLoad(blink::WebLocalFrame* frame);
   virtual void didNavigateWithinPage(blink::WebLocalFrame* frame,
                                      const blink::WebHistoryItem& item,
