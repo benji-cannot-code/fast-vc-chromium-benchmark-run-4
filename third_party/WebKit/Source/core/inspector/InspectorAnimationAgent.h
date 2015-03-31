dc1ea2fa91fa0962a2ff48cef42d274aee88253e
samli@chromium.org <samli@chromium.org>
2015-03-31 15:58:25 +0000

    Devtools Animations: Store map of animation type and clear on navigate
    This change stores a map of animation types since this information is lost
    and cannot be obtained at a later stage. It also clears stored data when
    the main frame is navigated.
    
    BUG=464247
    
    Review URL: https://codereview.chromium.org/1045233002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192838 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorAnimationAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorAnimationAgent.h
@@ -32,6 +32,7 @@ public:
     // Base agent methods.
     void restore() override;
     void disable(ErrorString*) override;
+    void didCommitLoadForLocalFrame(LocalFrame*) override;
 
     // Protocol method implementations
     virtual void getAnimationPlayersForNode(ErrorString*, int nodeId, bool includeSubtreeAnimations, RefPtr<TypeBuilder::Array<TypeBuilder::Animation::AnimationPlayer> >& animationPlayersArray) override;
@@ -64,6 +65,7 @@ private:
     RawPtrWillBeMember<InspectorPageAgent> m_pageAgent;
     RawPtrWillBeMember<InspectorDOMAgent> m_domAgent;
     WillBeHeapHashMap<String, RefPtrWillBeMember<AnimationPlayer>> m_idToAnimationPlayer;
+    WillBeHeapHashMap<String, AnimationType> m_idToAnimationType;
 };
 
 }
