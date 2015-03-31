5b7495e20aea2fea07be113b7eeab01573ee2a45
pilgrim@chromium.org <pilgrim@chromium.org>
2015-03-31 21:52:37 +0000

    Remove unused #includes from core/dom/CSSSelectorWatch.cpp
    TBR=rbyers@chromium.org
    BUG=472243
    
    Review URL: https://codereview.chromium.org/1039013003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192858 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/CSSSelectorWatch.cpp
+++ third_party/WebKit/Source/core/dom/CSSSelectorWatch.cpp
@@ -32,13 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/dom/CSSSelectorWatch.h"
 
-#include "core/css/CSSSelectorList.h"
-#include "core/css/StylePropertySet.h"
 #include "core/css/parser/CSSParser.h"
-#include "core/dom/Document.h"
-#include "core/dom/ExecutionContext.h"
-#include "core/frame/LocalFrame.h"
-#include "core/layout/style/StyleRareNonInheritedData.h"
 #include "core/loader/FrameLoaderClient.h"
 
 namespace blink {
