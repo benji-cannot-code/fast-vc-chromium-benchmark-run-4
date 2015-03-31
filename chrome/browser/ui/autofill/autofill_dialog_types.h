63ea408367dbd3ca0e35b007e0db1e8c414e45f6
estade <estade@chromium.org>
2015-03-30 19:27:26 -0700

    Clear out some minor TODOs.
    BUG=none
    
    Review URL: https://codereview.chromium.org/1047063002
    
    Cr-Commit-Position: refs/heads/master@{#322959}

--- chrome/browser/ui/autofill/autofill_dialog_types.h
+++ chrome/browser/ui/autofill/autofill_dialog_types.h
@@ -89,6 +89,8 @@ class DialogNotification {
 };
 
 extern SkColor const kWarningColor;
+extern SkColor const kLightShadingColor;
+extern SkColor const kSubtleBorderColor;
 
 struct SuggestionState {
   SuggestionState();
