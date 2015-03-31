afc3f6f08d7863caf63ca789c42353703357c72e
jaekyun <jaekyun@chromium.org>
2015-03-31 07:15:59 -0700

    Add user gesture carryover bit
    When a navigation is requested from XHR request which has user gesture, its
    timestamp is recorded.
    And then all the following navigations within 10s will have user gesture
    carryover bit.
    
    BUG=427357
    
    Review URL: https://codereview.chromium.org/1011813009
    
    Cr-Commit-Position: refs/heads/master@{#323030}

--- components/navigation_interception/intercept_navigation_delegate.cc
+++ components/navigation_interception/intercept_navigation_delegate.cc
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/navigation_interception/intercept_navigation_resource_throttle.h"
 #include "components/navigation_interception/navigation_params_android.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_view_host.h"
+#include "content/public/browser/resource_request_info.h"
 #include "content/public/browser/web_contents.h"
 #include "jni/InterceptNavigationDelegate_jni.h"
 #include "net/url_request/url_request.h"
@@ -28,6 +30,8 @@ namespace navigation_interception {
 
 namespace {
 
+const int kMaxValidityOfUserGestureCarryoverInSeconds = 10;
+
 const void* kInterceptNavigationDelegateUserDataKey =
     &kInterceptNavigationDelegateUserDataKey;
 
@@ -42,6 +46,24 @@ bool CheckIfShouldIgnoreNavigationOnUIThread(WebContents* source,
     return false;
 
   return intercept_navigation_delegate->ShouldIgnoreNavigation(params);
+}
+
+void UpdateUserGestureCarryoverInfoOnUIThread(int render_process_id,
+                                              int render_frame_id) {
+  content::RenderFrameHost* render_frame_host =
+      content::RenderFrameHost::FromID(render_process_id, render_frame_id);
+  if (!render_frame_host)
+    return;
+
+  content::WebContents* web_contents =
+      content::WebContents::FromRenderFrameHost(render_frame_host);
+
+  InterceptNavigationDelegate* intercept_navigation_delegate =
+      InterceptNavigationDelegate::Get(web_contents);
+
+  if (intercept_navigation_delegate) {
+    intercept_navigation_delegate->UpdateLastUserGestureCarryoverTimestamp();
+  }
 }
 
 }  // namespace
@@ -68,6 +90,23 @@ content::ResourceThrottle* InterceptNavigationDelegate::CreateThrottleFor(
       request, base::Bind(&CheckIfShouldIgnoreNavigationOnUIThread));
 }
 
+// static
+void InterceptNavigationDelegate::UpdateUserGestureCarryoverInfo(
+    net::URLRequest* request) {
+  const content::ResourceRequestInfo* info =
+      content::ResourceRequestInfo::ForRequest(request);
+  if (!info || !info->HasUserGesture())
+    return;
+
+  int render_process_id, render_frame_id;
+  if (!info->GetAssociatedRenderFrame(&render_process_id, &render_frame_id))
+    return;
+
+  BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
+                          base::Bind(&UpdateUserGestureCarryoverInfoOnUIThread,
+                                     render_process_id, render_frame_id));
+}
+
 InterceptNavigationDelegate::InterceptNavigationDelegate(
     JNIEnv* env, jobject jdelegate)
     : weak_jdelegate_(env, jdelegate) {
@@ -87,13 +126,23 @@ bool InterceptNavigationDelegate::ShouldIgnoreNavigation(
   if (jdelegate.is_null())
     return false;
 
-  ScopedJavaLocalRef<jobject> jobject_params =
-      CreateJavaNavigationParams(env, navigation_params);
+  bool has_user_gesture_carryover =
+      !navigation_params.has_user_gesture() &&
+      base::TimeTicks::Now() - last_user_gesture_carryover_timestamp_ <=
+          base::TimeDelta::FromSeconds(
+              kMaxValidityOfUserGestureCarryoverInSeconds);
+
+  ScopedJavaLocalRef<jobject> jobject_params = CreateJavaNavigationParams(
+      env, navigation_params, has_user_gesture_carryover);
 
   return Java_InterceptNavigationDelegate_shouldIgnoreNavigation(
       env,
       jdelegate.obj(),
       jobject_params.obj());
+}
+
+void InterceptNavigationDelegate::UpdateLastUserGestureCarryoverTimestamp() {
+  last_user_gesture_carryover_timestamp_ = base::TimeTicks::Now();
 }
 
 // Register native methods.
