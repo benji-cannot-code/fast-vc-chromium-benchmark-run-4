a4e3689620bf4decd197c94937f17f8730c8321a
pilgrim@chromium.org <pilgrim@chromium.org>
2015-03-31 22:00:34 +0000

    Remove unused #includes from core/xmlhttprequest/XMLHttpRequestUpload.cpp
    TBR=yhirano@chromium.org
    BUG=472243
    
    Review URL: https://codereview.chromium.org/1050663002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192861 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xmlhttprequest/XMLHttpRequestUpload.cpp
+++ third_party/WebKit/Source/core/xmlhttprequest/XMLHttpRequestUpload.cpp
@@ -27,10 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/xmlhttprequest/XMLHttpRequestUpload.h"
 
-#include "core/events/Event.h"
 #include "core/xmlhttprequest/XMLHttpRequestProgressEvent.h"
-#include "wtf/Assertions.h"
-#include "wtf/text/AtomicString.h"
 
 namespace blink {
 
