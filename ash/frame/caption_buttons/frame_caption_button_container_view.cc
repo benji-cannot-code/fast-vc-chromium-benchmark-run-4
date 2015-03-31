350ca62f6aeef1c431f0c15e1fa2fa3cb5250037
bruthig <bruthig@chromium.org>
2015-03-31 08:42:11 -0700

    Updated the UMA stat ash::TouchUMA::GESTURE_FRAMEMAXIMIZE_TAP recording.
    Updated the UMA stat ash::TouchUMA::GESTURE_FRAMEMAXIMIZE_TAP (aka
    'Ash.GestureTarget:Window size button tap') to be recorded whenever the
    FrameSizeButton is activated due to a gesture event.
    
    BUG=None
    TEST=Manually verified TouchUMA::GESTURE_FRAMEMAXIMIZE_TAP was recorded for touch only events.
    
    Review URL: https://codereview.chromium.org/1030283002
    
    Cr-Commit-Position: refs/heads/master@{#323040}

--- ash/frame/caption_buttons/frame_caption_button_container_view.cc
+++ ash/frame/caption_buttons/frame_caption_button_container_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/frame/caption_buttons/frame_size_button.h"
 #include "ash/metrics/user_metrics_recorder.h"
 #include "ash/shell.h"
+#include "ash/touch/touch_uma.h"
 #include "ash/wm/maximize_mode/maximize_mode_controller.h"
 #include "ui/base/hit_test.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -335,6 +336,11 @@ void FrameCaptionButtonContainerView::ButtonPressed(views::Button* sender,
     } else {
       frame_->Maximize();
       action = ash::UMA_WINDOW_MAXIMIZE_BUTTON_CLICK_MAXIMIZE;
+    }
+
+    if (event.IsGestureEvent()) {
+      TouchUMA::GetInstance()->RecordGestureAction(
+          TouchUMA::GESTURE_FRAMEMAXIMIZE_TAP);
     }
   } else if (sender == close_button_) {
     frame_->Close();
--- ash/frame/caption_buttons/frame_size_button.cc
+++ ash/frame/caption_buttons/frame_size_button.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/metrics/user_metrics_recorder.h"
 #include "ash/screen_util.h"
 #include "ash/shell.h"
-#include "ash/touch/touch_uma.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
 #include "ash/wm/wm_event.h"
@@ -124,10 +123,6 @@ void FrameSizeButton::OnGestureEvent(ui::GestureEvent* event) {
       event->type() == ui::ET_SCROLL_FLING_START ||
       event->type() == ui::ET_GESTURE_END) {
     if (CommitSnap(*event)) {
-      if (event->type() == ui::ET_GESTURE_TAP) {
-        TouchUMA::GetInstance()->RecordGestureAction(
-            TouchUMA::GESTURE_FRAMEMAXIMIZE_TAP);
-      }
       event->SetHandled();
       return;
     }
