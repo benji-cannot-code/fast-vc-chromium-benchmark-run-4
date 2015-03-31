6d8063d9d4b0bf285b9484a2791b33c825acd12a
dzhioev <dzhioev@chromium.org>
2015-03-31 13:54:46 -0700

    Removed 'gaia-endpoint-chromeos' switch.
    BUG=none
    TEST=browser_tests
    
    Review URL: https://codereview.chromium.org/1049703003
    
    Cr-Commit-Position: refs/heads/master@{#323106}

--- chrome/browser/ui/webui/chromeos/login/gaia_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/gaia_screen_handler.cc
@@ -57,8 +57,6 @@ const char kAuthIframeParentOrigin[] =
     "chrome-extension://mfffpogegjflfpflabcdkioaeobkgjik/";
 
 const char kGaiaSandboxUrlSwitch[] = "gaia-sandbox-url";
-// TODO(rsorokin): Move this to the proper file.
-const char kNewGaiaFlowPath[] = "embedded/setup/chromeos";
 
 void UpdateAuthParams(base::DictionaryValue* params,
                       bool has_users,
@@ -244,10 +242,6 @@ void GaiaScreenHandler::LoadGaia(const GaiaContext& context) {
     params.SetString("chromeType", GetChromeDeviceTypeString());
     params.SetString("clientId",
                      GaiaUrls::GetInstance()->oauth2_chrome_client_id());
-    if (!command_line->HasSwitch(switches::kGaiaEndpointChromeOS)) {
-      command_line->AppendSwitchASCII(switches::kGaiaEndpointChromeOS,
-                                      kNewGaiaFlowPath);
-    }
   } else {
     params.SetBoolean("useNewGaiaFlow", false);
   }
@@ -270,11 +264,6 @@ void GaiaScreenHandler::LoadGaia(const GaiaContext& context) {
             ? GURL(command_line->GetSwitchValueASCII(::switches::kGaiaUrl))
             : GaiaUrls::GetInstance()->gaia_url();
     params.SetString("gaiaUrl", gaia_url.spec());
-  }
-
-  if (command_line->HasSwitch(switches::kGaiaEndpointChromeOS)) {
-    params.SetString("gaiaEndpoint", command_line->GetSwitchValueASCII(
-                                         switches::kGaiaEndpointChromeOS));
   }
 
   if (use_easy_bootstrap_) {
