fe51d32ad2e04f944d203d2c241988a4a6732ba7
jianli@chromium.org <jianli@chromium.org>
2015-03-31 00:34:07 +0000

    Revert of Revert of [DevTools] Enable double tap to zoom support in touch emulation. (patchset #1 id:1 of https://codereview.chromium.org/1044923004/)
    Reason for revert:
    Not a culprit. Bring it back.
    
    Original issue's description:
    > Revert of [DevTools] Enable double tap to zoom support in touch emulation. (patchset #1 id:1 of https://codereview.chromium.org/1044923002/)
    > 
    > Reason for revert:
    > Speculative revert for Blink layout test failures in WebKit Mac10.8 (retina):
    > https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.8%20%28retina%29/builds/27696
    > 
    > I've reverted chromium patch r322807 that is the only one seems to be a bit related, but it did not help. So I had to try some other patches. Will revert the revert if your patch is not a culprit.
    > 
    > Original issue's description:
    > > [DevTools] Enable double tap to zoom support in touch emulation.
    > > 
    > > BUG=469067
    > > 
    > > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=192770
    > 
    > TBR=pfeldman@chromium.org,dgozman@chromium.org
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=469067
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=192779
    
    TBR=pfeldman@chromium.org,dgozman@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=469067
    
    Review URL: https://codereview.chromium.org/1043043002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192783 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/DevToolsEmulator.h
+++ third_party/WebKit/Source/web/DevToolsEmulator.h
@@ -32,6 +32,8 @@ public:
     void setPreferCompositingToLCDTextEnabled(bool);
     void setUseMobileViewportStyle(bool);
     void setScriptEnabled(bool);
+    void setDoubleTapToZoomEnabled(bool);
+    bool doubleTapToZoomEnabled() const;
 
     // Emulation.
     void enableDeviceEmulation(const WebDeviceEmulationParams&);
@@ -60,6 +62,7 @@ private:
     bool m_embedderUseMobileViewport;
 
     bool m_touchEventEmulationEnabled;
+    bool m_doubleTapToZoomEnabled;
     bool m_originalTouchEnabled;
     bool m_originalDeviceSupportsMouse;
     bool m_originalDeviceSupportsTouch;
