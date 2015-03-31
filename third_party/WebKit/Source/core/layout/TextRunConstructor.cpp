fb363d61be90fb76714e9e1ded83ba4436fdd2cc
pilgrim@chromium.org <pilgrim@chromium.org>
2015-03-31 22:16:58 +0000

    Remove unused #includes from core/layout/TextRunConstructor.cpp
    TBR=leviw@chromium.org
    BUG=472243
    
    Review URL: https://codereview.chromium.org/1040423003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192868 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/TextRunConstructor.cpp
+++ third_party/WebKit/Source/core/layout/TextRunConstructor.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/TextRunConstructor.h"
 
 #include "core/layout/LayoutText.h"
-#include "core/layout/style/ComputedStyle.h"
 #include "platform/text/BidiTextRun.h"
 
 namespace blink {
