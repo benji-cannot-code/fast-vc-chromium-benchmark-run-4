fa6cc3e9b21ef8b98b2d319063daf262c03d4fad
robhogan@gmail.com <robhogan@gmail.com>
2015-04-01 23:20:17 +0000

    Fire load event for an image even if it was the last image we loaded successfully.
    When we updated image loading to handle alt content using the shadow dom we made
    most image loads asynchronous, including loads where the src is null. src='' is a
    common idiom for resetting the content of the image but if a user now does "src='';
    src='www.example.com/someimage.gif;" the asynchronous image load doesn't get time
    to reset the current image stored in memory before the subsequent image loads. 
    We have a strange rule in our image loading code that tells us not to fire a load
    event if the new image we've loaded is the same as the old one.
    
    https://html.spec.whatwg.org/multipage/embedded-content.html#the-img-element says
    that we should fire load events when we load an image, even if its cached. This
    means our practice of not firing load events when we already have an image loaded
    is not consistent with the spec or what other browsers do, so always fire a load
    event if we load an image. The only exception is if the load is in response to a
    viewport resize event and we haven't chosen a new image to load from srcset.
    
    
    BUG=472193, 7731
    
    Review URL: https://codereview.chromium.org/1047563002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192974 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/ImageLoader.cpp
+++ third_party/WebKit/Source/core/loader/ImageLoader.cpp
@@ -290,8 +290,11 @@ void ImageLoader::doUpdateFromElement(BypassMainWorldBehavior bypassBehavior, Up
     }
 
     ImageResource* oldImage = m_image.get();
-    if (newImage != oldImage) {
-        sourceImageChanged();
+    if (updateBehavior == UpdateSizeChanged && m_element->layoutObject() && m_element->layoutObject()->isImage() && newImage == oldImage) {
+        toLayoutImage(m_element->layoutObject())->intrinsicSizeChanged();
+    } else {
+        if (newImage != oldImage)
+            sourceImageChanged();
 
         if (m_hasPendingLoadEvent) {
             loadEventSender().cancelEvent(this);
@@ -319,8 +322,6 @@ void ImageLoader::doUpdateFromElement(BypassMainWorldBehavior bypassBehavior, Up
 
         if (oldImage)
             oldImage->removeClient(this);
-    } else if (updateBehavior == UpdateSizeChanged && m_element->layoutObject() && m_element->layoutObject()->isImage()) {
-        toLayoutImage(m_element->layoutObject())->intrinsicSizeChanged();
     }
 
     if (LayoutImageResource* imageResource = layoutImageResource())
