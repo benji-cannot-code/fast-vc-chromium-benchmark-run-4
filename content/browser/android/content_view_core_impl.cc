e29cfb548c5e3a5f12d7a8c1f4445feeb0612ba4
siva.gunturi <siva.gunturi@samsung.com>
2015-03-31 23:42:11 -0700

    Pass format as argument to getContentBitmapAsync. getScaledContentbitmap can process and give output in the requested format. getContentBitmapAsync should have this support. Especially on low-end devices we may need bitmaps with 16-bit formats.
    BUG=472457
    
    Review URL: https://codereview.chromium.org/1021863005
    
    Cr-Commit-Position: refs/heads/master@{#323194}

--- content/browser/android/content_view_core_impl.cc
+++ content/browser/android/content_view_core_impl.cc
@@ -640,7 +640,7 @@ void ContentViewCoreImpl::GetScaledContentBitmap(
     gfx::Rect src_subrect,
     ReadbackRequestCallback& result_callback) {
   RenderWidgetHostViewAndroid* view = GetRenderWidgetHostViewAndroid();
-  if (!view) {
+  if (!view || color_type == kUnknown_SkColorType) {
     result_callback.Run(SkBitmap(), READBACK_FAILED);
     return;
   }
