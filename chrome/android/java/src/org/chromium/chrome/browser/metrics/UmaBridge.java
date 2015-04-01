9e322928731d46cd455c90f8a93a24b4a3be2bf6
megjablon <megjablon@chromium.org>
2015-04-01 11:34:29 -0700

    New UMA for Data Reduction Proxy UI
    Adds new UMA DataReductionProxy.UIAction that reports all user UI
    interactions on the Data Reduction Proxy settings menu and promo.
    Deprecates the following counts and histograms:
    
    DataReductionProxy_TurnedOn
    DataReductionProxy_TurnedOnFromPromo
    DataReductionProxy_TurnedOff
    DataReductionProxy_PromoDisplayed
    DataReductionProxy_PromoLearnMore
    DataReductionProxy.PromoAction
    DataReductionProxy.SettingsConversion
    
    BUG=455267
    
    Review URL: https://codereview.chromium.org/1038253003
    
    Cr-Commit-Position: refs/heads/master@{#323287}

--- chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaBridge.java
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.metrics;
 
-import org.chromium.chrome.browser.preferences.bandwidth.BandwidthReductionPreferences;
-import org.chromium.chrome.browser.preferences.bandwidth.DataReductionPromoScreen;
-
 /**
  * Static methods to record user actions.
  *
@@ -43,57 +40,6 @@ public class UmaBridge {
         nativeRecordBeamInvalidAppState();
     }
 
-    // Data Saver
-
-    /**
-     * Record that Data Saver was turned on.
-     */
-    public static void dataReductionProxyTurnedOn() {
-        nativeRecordDataReductionProxyTurnedOn();
-    }
-
-    /**
-     * Record that Data Saver was turned off.
-     */
-    public static void dataReductionProxyTurnedOff() {
-        nativeRecordDataReductionProxyTurnedOff();
-    }
-
-    /**
-     * Record that Data Saver was turned on immediately after the user viewed the promo screen.
-     */
-    public static void dataReductionProxyTurnedOnFromPromo() {
-        nativeRecordDataReductionProxyTurnedOnFromPromo();
-    }
-
-    /**
-     * Record the DataReductionProxy.PromoAction histogram.
-     * @param action User action at the promo screen
-     */
-    public static void dataReductionProxyPromoAction(int action) {
-        assert action >= 0 && action < DataReductionPromoScreen.ACTION_INDEX_BOUNDARY;
-        nativeRecordDataReductionProxyPromoAction(
-                action, DataReductionPromoScreen.ACTION_INDEX_BOUNDARY);
-    }
-
-    /**
-     * Record that the Data Saver promo was displayed.
-     */
-    public static void dataReductionProxyPromoDisplayed() {
-        nativeRecordDataReductionProxyPromoDisplayed();
-    }
-
-    /**
-     * Record the DataReductionProxy.SettingsConversion histogram.
-     * @param statusChange ON/OFF change at the data saver setting menu
-     */
-    public static void dataReductionProxySettings(int statusChange) {
-        assert statusChange >= 0
-                && statusChange < BandwidthReductionPreferences.DATA_REDUCTION_INDEX_BOUNDARY;
-        nativeRecordDataReductionProxySettings(
-                statusChange, BandwidthReductionPreferences.DATA_REDUCTION_INDEX_BOUNDARY);
-    }
-
     public static void freSignInShown() {
         nativeRecordFreSignInShown();
     }
@@ -102,13 +48,6 @@ public class UmaBridge {
     private static native void nativeRecordUsingMenu(boolean isByHwButton, boolean isDragging);
     private static native void nativeRecordBeamInvalidAppState();
     private static native void nativeRecordBeamCallbackSuccess();
-    private static native void nativeRecordDataReductionProxyTurnedOn();
-    private static native void nativeRecordDataReductionProxyTurnedOff();
-    private static native void nativeRecordDataReductionProxyTurnedOnFromPromo();
-    private static native void nativeRecordDataReductionProxyPromoAction(int action, int boundary);
-    private static native void nativeRecordDataReductionProxyPromoDisplayed();
-    private static native void nativeRecordDataReductionProxySettings(int statusChange,
-            int boundary);
 
     // First Run Experience
     private static native void nativeRecordFreSignInShown();
