bf16d3227585c5d29fb2d9a7349b18415aec21d3
erg <erg@chromium.org>
2015-04-01 15:53:11 -0700

    gn-mode: Add `if' and `else' as keywords and fix file regexp.
    BUG=none
    
    Review URL: https://codereview.chromium.org/1055663002
    
    Cr-Commit-Position: refs/heads/master@{#323352}

--- tools/gn/misc/emacs/gn.el
+++ tools/gn/misc/emacs/gn.el
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 (defvar gn-font-lock-target-declaration-keywords
   '("action" "action_foreach" "copy" "executable" "group"
-    "shared_library" "source_set" "static_library"))
+    "shared_library" "source_set" "static_library" "if" "else"))
 
 (defvar gn-font-lock-buildfile-fun-keywords
   '("assert" "config" "declare_args" "defined" "exec_script" "foreach"
@@ -90,7 +90,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   (modify-syntax-entry ?_ "w" gn-mode-syntax-table))
 
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("^BUILD.gn$" . gn-mode))
-(add-to-list 'auto-mode-alist '("\\.gni$'" . gn-mode))
+(add-to-list 'auto-mode-alist '("\\.gni?\\'" . gn-mode))
 
 (provide 'gn-mode)
