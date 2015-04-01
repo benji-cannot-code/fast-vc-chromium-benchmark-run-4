8a03e1193fae6e18ac34b3588c8859000e56f915
Sam Clegg <sbc@chromium.org>
2015-04-01 15:57:34 -0700

    [NaCl SDK] nacl_io: Fix mac build by including ossignal.h
    R=binji@chromium.org, binji
    BUG=none
    
    Review URL: https://codereview.chromium.org/1051973003
    
    Cr-Commit-Position: refs/heads/master@{#323356}

--- native_client_sdk/src/libraries/nacl_io/kernel_wrap.h
+++ native_client_sdk/src/libraries/nacl_io/kernel_wrap.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/ioctl.h>
 #include <sys/types.h>
 
+#include "nacl_io/ossignal.h"
 #include "nacl_io/ossocket.h"
 #include "nacl_io/ostypes.h"
 #include "nacl_io/osutime.h"
