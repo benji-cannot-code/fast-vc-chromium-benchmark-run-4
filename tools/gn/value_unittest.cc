90780305e7479e3abad30496ee9660da3e956e59
raymes <raymes@chromium.org>
2015-03-31 19:10:14 -0700

    Revert of tools/gn: fix escaping of backslashes (patchset #3 id:40001 of https://codereview.chromium.org/995393005/)
    Reason for revert:
    Speculatively reverting because of: http://build.chromium.org/p/chromium.win/buildstatus?builder=Win8%20GN%20%28dbg%29&number=4637
    
    ERROR at //third_party/WebKit/Source/bindings/scripts/scripts.gni:198:5: Unable to write file.
        write_file(idl_files_list, rebase_path(invoker.sources, root_build_dir))
    
    Original issue's description:
    > tools/gn: fix escaping of backslashes
    >
    > BUG=470217
    >
    > Committed: https://crrev.com/4bf46ca929d17910fcf66cc25e886b986845ca59
    > Cr-Commit-Position: refs/heads/master@{#323161}
    
    TBR=brettw@chromium.org,mdempsky@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=470217
    
    Review URL: https://codereview.chromium.org/1049963002
    
    Cr-Commit-Position: refs/heads/master@{#323167}

--- tools/gn/value_unittest.cc
+++ tools/gn/value_unittest.cc
@@ -12,10 +12,6 @@ TEST(Value, ToString) {
   EXPECT_EQ("hi\" $me\\you\\$\\\"", strval.ToString(false));
   EXPECT_EQ("\"hi\\\" \\$me\\you\\\\\\$\\\\\\\"\"", strval.ToString(true));
 
-  // crbug.com/470217
-  Value strval2(nullptr, "\\foo\\\\bar\\");
-  EXPECT_EQ("\"\\foo\\\\\\bar\\\\\"", strval2.ToString(true));
-
   // Void type.
   EXPECT_EQ("<void>", Value().ToString(false));
 
@@ -40,3 +36,4 @@ TEST(Value, ToString) {
   scope->SetValue("b", Value(nullptr, "hello, world"), nullptr);
   EXPECT_EQ("{\n  a = 42\n  b = \"hello, world\"\n}", scopeval.ToString(false));
 }
+
