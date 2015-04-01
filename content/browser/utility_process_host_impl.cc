f3287496b447a4d38147ad9e6f747ee9c96d48f9
thestig <thestig@chromium.org>
2015-04-01 10:53:39 -0700

    Change UtilityProcessHost::SetName() to take a string16.
    Review URL: https://codereview.chromium.org/1049883002
    
    Cr-Commit-Position: refs/heads/master@{#323282}

--- content/browser/utility_process_host_impl.cc
+++ content/browser/utility_process_host_impl.cc
@@ -110,7 +110,7 @@ UtilityProcessHostImpl::UtilityProcessHostImpl(
       child_flags_(ChildProcessHost::CHILD_NORMAL),
 #endif
       started_(false),
-      name_("utility process") {
+      name_(base::ASCIIToUTF16("utility process")) {
 }
 
 UtilityProcessHostImpl::~UtilityProcessHostImpl() {
@@ -193,7 +193,7 @@ ServiceRegistry* UtilityProcessHostImpl::GetServiceRegistry() {
   return mojo_application_host_->service_registry();
 }
 
-void UtilityProcessHostImpl::SetName(const std::string& name) {
+void UtilityProcessHostImpl::SetName(const base::string16& name) {
   name_ = name;
 }
 
@@ -208,7 +208,7 @@ bool UtilityProcessHostImpl::StartProcess() {
   // Name must be set or metrics_service will crash in any test which
   // launches a UtilityProcessHost.
   process_.reset(new BrowserChildProcessHostImpl(PROCESS_TYPE_UTILITY, this));
-  process_->SetName(base::UTF8ToUTF16(name_));
+  process_->SetName(name_);
 
   std::string channel_id = process_->GetHost()->CreateChannel();
   if (channel_id.empty())
--- content/public/browser/utility_process_host.h
+++ content/public/browser/utility_process_host.h
@@ -86,7 +86,7 @@ class UtilityProcessHost : public IPC::Sender,
   virtual ServiceRegistry* GetServiceRegistry() = 0;
 
   // Set the name of the process to appear in the task manager.
-  virtual void SetName(const std::string& name) = 0;
+  virtual void SetName(const base::string16& name) = 0;
 };
 
 };  // namespace content
