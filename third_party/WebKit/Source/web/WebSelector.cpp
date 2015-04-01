e0522648d2a078bc242fc71610904b13e96ea658
timloh@chromium.org <timloh@chromium.org>
2015-04-01 02:53:21 +0000

    Make the CSSParser interface static-only
    Currently the CSSParser has a BisonCSSParser member which is used for
    parseSelector and parseDeclarationList, but is otherwise static. This
    patch removes the member and makes it static-only, so that code-paths
    like querySelector and parsing inline styles don't need to initialize
    the Bison parser when we use the new CSS Parser.
    
    BUG=330389
    
    Review URL: https://codereview.chromium.org/1051633002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192897 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebSelector.cpp
+++ third_party/WebKit/Source/web/WebSelector.cpp
@@ -40,9 +40,8 @@ namespace blink {
 
 WebString canonicalizeSelector(WebString webSelector, WebSelectorType restriction)
 {
-    CSSParser parser(strictCSSParserContext());
     CSSSelectorList selectorList;
-    parser.parseSelector(webSelector, selectorList);
+    CSSParser::parseSelector(strictCSSParserContext(), webSelector, selectorList);
 
     if (restriction == WebSelectorTypeCompound) {
         for (const CSSSelector* selector = selectorList.first(); selector; selector = selectorList.next(*selector)) {
