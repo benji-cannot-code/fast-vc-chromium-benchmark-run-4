2e9cb87f57d4f91b2d6f24b3039396ca974b9f69
weiliangc <weiliangc@chromium.org>
2015-03-31 17:11:31 -0700

    Implement DisplayList GatherPixelRefs
    Take advantage of DisplayList playing back to generate SkPicture, and
    use existing functions that gathers pixel ref from a single SkPicture.
    
    R=ajuma, enne, vmpstr
    BUG=440468
    
    Review URL: https://codereview.chromium.org/1045953002
    
    Cr-Commit-Position: refs/heads/master@{#323152}

--- cc/resources/display_item_list.h
+++ cc/resources/display_item_list.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/base/cc_export.h"
 #include "cc/base/scoped_ptr_vector.h"
 #include "cc/resources/display_item.h"
+#include "cc/resources/pixel_ref_map.h"
 #include "skia/ext/refptr.h"
 #include "third_party/skia/include/core/SkPicture.h"
 #include "ui/gfx/geometry/rect.h"
@@ -45,6 +46,8 @@ class CC_EXPORT DisplayItemList
 
   void EmitTraceSnapshot() const;
 
+  void GatherPixelRefs(const gfx::Size& grid_cell_size);
+
  private:
   DisplayItemList();
   ~DisplayItemList();
@@ -55,7 +58,10 @@ class CC_EXPORT DisplayItemList
   bool is_suitable_for_gpu_rasterization_;
   int approximate_op_count_;
 
+  scoped_ptr<PixelRefMap> pixel_refs_;
+
   friend class base::RefCountedThreadSafe<DisplayItemList>;
+  friend class PixelRefMap::Iterator;
   DISALLOW_COPY_AND_ASSIGN(DisplayItemList);
 };
 
