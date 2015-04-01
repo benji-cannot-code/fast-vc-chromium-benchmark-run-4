6acbae1407fb8c436da68fcd8b0a2d3f691c9b46
pilgrim@chromium.org <pilgrim@chromium.org>
2015-04-01 00:47:56 +0000

    Remove unused #includes from core/paint/SVGInlineFlowBoxPainter.cpp
    TBR=levin@chromium.org
    BUG=472243
    
    Review URL: https://codereview.chromium.org/1044373002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192888 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/SVGInlineFlowBoxPainter.cpp
+++ third_party/WebKit/Source/core/paint/SVGInlineFlowBoxPainter.cpp
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/paint/SVGInlineFlowBoxPainter.h"
 
-#include "core/layout/PaintInfo.h"
 #include "core/layout/svg/line/SVGInlineFlowBox.h"
 #include "core/layout/svg/line/SVGInlineTextBox.h"
 #include "core/paint/SVGInlineTextBoxPainter.h"
