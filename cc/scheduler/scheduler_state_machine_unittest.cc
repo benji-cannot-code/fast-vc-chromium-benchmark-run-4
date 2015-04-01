eac86b27cf6345abb2da8fc9555efd267623bd48
ccameron <ccameron@chromium.org>
2015-04-01 13:36:28 -0700

    Make cc::Scheduler::BeginFrameNeeded be false when commits deferred
    If this is not the case, then the scheduler will tick its |frame_source_|
    until commits are re-enabled.
    
    This is problematic because (on Mac) ui::Compositors that are not visible
    because they are occluded by other NSWindows have commits deferred
    until they are un-occluded. In the unintended endings department, this
    makes the use of occlusion, intended to save power, end up consuming
    more substantially more power.
    
    BUG=471863
    
    Review URL: https://codereview.chromium.org/1047173003
    
    Cr-Commit-Position: refs/heads/master@{#323320}

--- cc/scheduler/scheduler_state_machine_unittest.cc
+++ cc/scheduler/scheduler_state_machine_unittest.cc
@@ -1819,7 +1819,7 @@ TEST(SchedulerStateMachineTest, TestDeferCommit) {
   state.SetDeferCommits(true);
 
   state.SetNeedsCommit();
-  EXPECT_TRUE(state.BeginFrameNeeded());
+  EXPECT_FALSE(state.BeginFrameNeeded());
   EXPECT_ACTION_UPDATE_STATE(SchedulerStateMachine::ACTION_NONE);
 
   state.OnBeginImplFrame();
