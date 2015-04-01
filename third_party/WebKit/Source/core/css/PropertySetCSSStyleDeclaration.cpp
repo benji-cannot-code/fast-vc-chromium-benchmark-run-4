748325ee9c9f1b75e7663e068c46dc568b33abfb
timloh@chromium.org <timloh@chromium.org>
2015-04-01 00:58:21 +0000

    Rename parseDeclaration to parseDeclarationList
    This patch renames parseDeclaration to parseDeclarationList (..since it
    parses a declaration list, not a single declaration...)
    
    Review URL: https://codereview.chromium.org/1048093003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192890 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/PropertySetCSSStyleDeclaration.cpp
+++ third_party/WebKit/Source/core/css/PropertySetCSSStyleDeclaration.cpp
@@ -161,13 +161,12 @@ String AbstractPropertySetCSSStyleDeclaration::cssText() const
     return propertySet().asText();
 }
 
-void AbstractPropertySetCSSStyleDeclaration::setCSSText(const String& text, ExceptionState& exceptionState)
+void AbstractPropertySetCSSStyleDeclaration::setCSSText(const String& text, ExceptionState&)
 {
     StyleAttributeMutationScope mutationScope(this);
     willMutate();
 
-    // FIXME: Detect syntax errors and set exceptionState.
-    propertySet().parseDeclaration(text, contextStyleSheet());
+    propertySet().parseDeclarationList(text, contextStyleSheet());
 
     didMutate(PropertyChanged);
 
