967c13d3e6ec3cb47bac3bd69f12e62a19b5e57b
pilgrim@chromium.org <pilgrim@chromium.org>
2015-04-01 00:47:08 +0000

    Remove unused #includes from core/html/HTMLViewSourceDocument.cpp
    TBR=philipj@opera.com
    BUG=472243
    
    Review URL: https://codereview.chromium.org/1048203003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192886 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLViewSourceDocument.cpp
+++ third_party/WebKit/Source/core/html/HTMLViewSourceDocument.cpp
@@ -26,8 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/html/HTMLViewSourceDocument.h"
 
-#include "core/HTMLNames.h"
-#include "core/dom/StyleEngine.h"
 #include "core/dom/Text.h"
 #include "core/html/HTMLAnchorElement.h"
 #include "core/html/HTMLBRElement.h"
@@ -41,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLTableElement.h"
 #include "core/html/HTMLTableRowElement.h"
 #include "core/html/HTMLTableSectionElement.h"
-#include "core/html/parser/HTMLToken.h"
 #include "core/html/parser/HTMLViewSourceParser.h"
 
 namespace blink {
