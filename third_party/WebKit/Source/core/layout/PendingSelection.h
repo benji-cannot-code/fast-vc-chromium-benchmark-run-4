03ddcd014c8000c3fad76e1c27362d6d89e9aa84
hajimehoshi@chromium.org <hajimehoshi@chromium.org>
2015-04-01 11:17:42 +0000

    Refactoring: Hide PendingSelection's properties
    This patch moves some logic in LayoutView to PendingSelection and hides
    PendingSelection's properties. This makes it easier to use the new
    composed-tree version of Position on LayoutView and PendingSelection.
    
    This patch is result of collaboration work with yosin@chromium.org
    for selection of web components. After this patch, we are going to
    fix those classes for the composed tree. We've already prepared that at
    crrev.com/963693002.
    
    BUG=275851
    TEST=n/a; No behavior changes.
    
    Review URL: https://codereview.chromium.org/1051743002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192916 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/PendingSelection.h
+++ third_party/WebKit/Source/core/layout/PendingSelection.h
@@ -25,9 +25,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/Position.h"
 #include "core/editing/TextAffinity.h"
+#include "core/editing/VisibleSelection.h"
 
 namespace blink {
 
+class Document;
 class FrameSelection;
 
 class PendingSelection final : public NoBaseWillBeGarbageCollected<PendingSelection> {
@@ -37,14 +39,12 @@ public:
         return adoptPtrWillBeNoop(new PendingSelection);
     }
 
-    Position start() const { return m_start; }
-    Position end() const { return m_end; }
-    Position extent() const { return m_extent; }
-    EAffinity affinity() const { return m_affinity; }
     bool hasPendingSelection() const { return m_hasPendingSelection; }
-    bool shouldShowBlockCursor() const { return m_shouldShowBlockCursor; }
     void setSelection(const FrameSelection&);
     void clear();
+
+    bool isInDocument(const Document&) const;
+    VisibleSelection calcVisibleSelection() const;
 
     DECLARE_TRACE();
 
