9372bfa94b8ef115295a8e99eed9d5cf21162729
hirono <hirono@chromium.org>
2015-04-01 04:28:08 -0700

    Show raw image icons in file list.
    BUG=396702
    TEST=None
    
    Review URL: https://codereview.chromium.org/1052573002
    
    Cr-Commit-Position: refs/heads/master@{#323212}

--- ui/file_manager/file_manager/common/js/file_type.js
+++ ui/file_manager/file_manager/common/js/file_type.js
@@ -324,6 +324,8 @@ FileType.isAudio = function(entry) {
 };
 
 /**
+ * Returns whether the |entry| is image file that can be opened in browser.
+ * Note that it returns false for RAW images.
  * @param {Entry} entry Reference to the file.
  * @return {boolean} True if image file.
  */
@@ -345,15 +347,6 @@ FileType.isVideo = function(entry) {
  */
 FileType.isRaw = function(entry) {
   return FileType.getMediaType(entry) === 'raw';
-};
-
-/**
- * Files with more pixels won't have preview.
- * @param {Entry} entry Reference to the file.
- * @return {boolean} True if image or video.
- */
-FileType.isImageOrVideo = function(entry) {
-  return FileType.isType(entry, ['image', 'video']);
 };
 
 /**
