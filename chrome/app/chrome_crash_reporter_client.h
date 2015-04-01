7b3a4055ee0b0f55928220c907e5c8c2863c88c6
erikwright <erikwright@chromium.org>
2015-03-31 21:13:24 -0700

    Report dump counts as a stability metric.
    Previously these were reported as a regular metric, against the currently running version, even though they were recorded from the previous execution, possibly from a different version.
    
    Stability metrics are attributed to the version of the previous execution.
    
    Also, there was an attempt to mitigate the cross-version reporting by associating each even with the version that recorded it. This is problematic for short-lived versions such as Canary.
    
    I extracted the code, fixed the above deficiencies, wrote tests, and added functionality (also reporting the outcome of the Breakpad invocation).
    
    BUG=460512
    
    Committed: https://crrev.com/a11510d8e5175aa451f11b183a6668f581b4ec60
    Cr-Commit-Position: refs/heads/master@{#323131}
    
    Review URL: https://codereview.chromium.org/1038503002
    
    Cr-Commit-Position: refs/heads/master@{#323182}

--- chrome/app/chrome_crash_reporter_client.h
+++ chrome/app/chrome_crash_reporter_client.h
@@ -8,7 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/memory/scoped_ptr.h"
 #include "components/crash/app/crash_reporter_client.h"
+
+namespace browser_watcher {
+class CrashReportingMetrics;
+}  // namespace browser_watcher
 
 namespace chrome {
 
@@ -40,6 +45,8 @@ class ChromeCrashReporterClient : public crash_reporter::CrashReporterClient {
   virtual int GetResultCodeRespawnFailed() override;
   virtual void InitBrowserCrashDumpsRegKey() override;
   virtual void RecordCrashDumpAttempt(bool is_real_crash) override;
+  virtual void RecordCrashDumpAttemptResult(bool is_real_crash,
+                                            bool succeeded) override;
 #endif
 
 #if defined(OS_POSIX) && !defined(OS_MACOSX) && !defined(OS_IOS)
@@ -67,6 +74,10 @@ class ChromeCrashReporterClient : public crash_reporter::CrashReporterClient {
   bool EnableBreakpadForProcess(const std::string& process_type) override;
 
  private:
+#if defined(OS_WIN)
+  scoped_ptr<browser_watcher::CrashReportingMetrics> crash_reporting_metrics_;
+#endif
+
   DISALLOW_COPY_AND_ASSIGN(ChromeCrashReporterClient);
 };
 
--- components/browser_watcher.gypi
+++ components/browser_watcher.gypi
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'target_name': 'browser_watcher_client',
             'type': 'static_library',
             'sources': [
+              'browser_watcher/crash_reporting_metrics_win.cc',
+              'browser_watcher/crash_reporting_metrics_win.h',
               'browser_watcher/watcher_client_win.cc',
               'browser_watcher/watcher_client_win.h',
               'browser_watcher/watcher_metrics_provider_win.cc',
