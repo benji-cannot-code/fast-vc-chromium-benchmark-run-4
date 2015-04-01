b779fb54ed51cc59c252010a04a0bdb4ae51eee4
pilgrim@chromium.org <pilgrim@chromium.org>
2015-04-01 20:21:52 +0000

    Remove unused #includes from core/paint/BlockPainter.cpp
    TBR=dcheng@chromium.org
    
    Review URL: https://codereview.chromium.org/1051653002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192962 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/BlockPainter.cpp
+++ third_party/WebKit/Source/core/paint/BlockPainter.cpp
@@ -6,15 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/paint/BlockPainter.h"
 
-#include "core/editing/Caret.h"
 #include "core/editing/FrameSelection.h"
-#include "core/frame/LocalFrame.h"
 #include "core/frame/Settings.h"
-#include "core/layout/LayoutBlock.h"
 #include "core/layout/LayoutFlexibleBox.h"
 #include "core/layout/LayoutInline.h"
-#include "core/layout/LayoutView.h"
-#include "core/layout/PaintInfo.h"
 #include "core/page/Page.h"
 #include "core/paint/BoxClipper.h"
 #include "core/paint/BoxPainter.h"
@@ -26,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/paint/ScopeRecorder.h"
 #include "core/paint/ScrollRecorder.h"
 #include "core/paint/ScrollableAreaPainter.h"
-#include "platform/geometry/LayoutPoint.h"
-#include "platform/geometry/LayoutRect.h"
 #include "platform/graphics/paint/ClipRecorder.h"
 
 namespace blink {
