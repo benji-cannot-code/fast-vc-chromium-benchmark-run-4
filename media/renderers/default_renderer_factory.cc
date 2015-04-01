e2c6b1f318145f09de9881166694d43875db2bc8
wolenetz <wolenetz@chromium.org>
2015-04-01 10:14:43 -0700

    Reland: media-internals: Simplify the code path for creating a media log entry
    https://codereview.chromium.org/1047313002/ was reverted due to apparent
    transient build bot failure (see http://crbug.com/471952).
    This CL is to reland the original CL:
    
    Changes MediaLog::CreateLogEvent() to instead be a helper AddLogEvent() that
    both creates and adds the event, and has signature and utility matching LogCB.
    This means various media code pieces no longer must have their own LogCB
    implementations that CreateLogEvent()+AddEvent().
    
    BUG=471902, 260005
    TEST=no media_unittest, layout, or chrome://media-internals log regression observed
    
    Review URL: https://codereview.chromium.org/1048353002
    
    Cr-Commit-Position: refs/heads/master@{#323269}

--- media/renderers/default_renderer_factory.cc
+++ media/renderers/default_renderer_factory.cc
@@ -24,15 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-#if !defined(MEDIA_DISABLE_FFMPEG)
-
-static void AddLogEntry(const scoped_refptr<MediaLog>& media_log,
-                        MediaLog::MediaLogLevel level,
-                        const std::string& message) {
-  media_log->AddEvent(media_log->CreateLogEvent(level, message));
-}
-#endif
-
 DefaultRendererFactory::DefaultRendererFactory(
     const scoped_refptr<MediaLog>& media_log,
     const scoped_refptr<GpuVideoAcceleratorFactories>& gpu_factories,
@@ -56,7 +47,7 @@ scoped_ptr<Renderer> DefaultRendererFactory::CreateRenderer(
 
 #if !defined(MEDIA_DISABLE_FFMPEG)
   audio_decoders.push_back(new FFmpegAudioDecoder(
-      media_task_runner, base::Bind(&AddLogEntry, media_log_)));
+      media_task_runner, base::Bind(&MediaLog::AddLogEvent, media_log_)));
 #endif
 
   audio_decoders.push_back(new OpusAudioDecoder(media_task_runner));
