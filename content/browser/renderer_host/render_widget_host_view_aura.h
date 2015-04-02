ea9296b93096bad32aa62f69fc7bf67e16a03b11
Sadrul Habib Chowdhury <sadrul@chromium.org>
2015-04-02 12:46:52 -0400

    content: Delete some now-dead event related code.
    Some of the removed methods were never used, and the rest had no
    real implementations.
    
    BUG=None
    R=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/1052063002
    
    Cr-Commit-Position: refs/heads/master@{#323512}

--- content/browser/renderer_host/render_widget_host_view_aura.h
+++ content/browser/renderer_host/render_widget_host_view_aura.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/display_observer.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/rect.h"
-#include "ui/wm/public/activation_change_observer.h"
 #include "ui/wm/public/activation_delegate.h"
 
 namespace aura {
@@ -87,7 +86,6 @@ class CONTENT_EXPORT RenderWidgetHostViewAura
       public aura::WindowTreeHostObserver,
       public aura::WindowDelegate,
       public aura::client::ActivationDelegate,
-      public aura::client::ActivationChangeObserver,
       public aura::client::FocusChangeObserver,
       public aura::client::CursorClientObserver {
  public:
@@ -303,10 +301,6 @@ class CONTENT_EXPORT RenderWidgetHostViewAura
 
   // Overridden from aura::client::ActivationDelegate:
   bool ShouldActivate() const override;
-
-  // Overridden from aura::client::ActivationChangeObserver:
-  void OnWindowActivated(aura::Window* gained_activation,
-                         aura::Window* lost_activation) override;
 
   // Overridden from aura::client::CursorClientObserver:
   void OnCursorVisibilityChanged(bool is_visible) override;
