5343f3373f019adc5a9133d5132a17dba7f75d2d
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-04-02 14:25:35 -0700

    [Extensions] Change ProcessManager to use RenderFrameHosts
    RenderViewHosts are going to go away, and things using them are blocking
    OOPIs. Make ProcessManager use RenderFrameHosts.
    Additionally, this takes out ~130 lines of code.
    
    BUG=466373
    
    Review URL: https://codereview.chromium.org/1037263004
    
    Cr-Commit-Position: refs/heads/master@{#323565}

--- extensions/browser/process_manager_observer.h
+++ extensions/browser/process_manager_observer.h
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+namespace content {
+class RenderFrameHost;
+}
+
 namespace extensions {
 class Extension;
 
@@ -22,6 +26,14 @@ class ProcessManagerObserver {
 
   // Called immediately after the extension background host is destroyed.
   virtual void OnBackgroundHostClose(const std::string& extension_id) {}
+
+  virtual void OnExtensionFrameRegistered(
+      const std::string& extension_id,
+      content::RenderFrameHost* render_frame_host) {}
+
+  virtual void OnExtensionFrameUnregistered(
+      const std::string& extension_id,
+      content::RenderFrameHost* render_frame_host) {}
 };
 
 }  // namespace extensions
