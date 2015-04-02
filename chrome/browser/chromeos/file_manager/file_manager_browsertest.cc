247cfdef1122dadabfa22baa95d64d8c4137dc2d
yoshiki <yoshiki@chromium.org>
2015-04-02 03:47:33 -0700

    [Files.app test] Add a test of tabindex in Downloads directory
    BUG=469061
    TEST=newly added patch successes
    
    Review URL: https://codereview.chromium.org/1051883002
    
    Cr-Commit-Position: refs/heads/master@{#323458}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -1122,6 +1122,13 @@ INSTANTIATE_TEST_CASE_P(TabindexFocus,
                                                         "tabindexFocus")));
 
 INSTANTIATE_TEST_CASE_P(
+    TabindexFocusDownloads,
+    FileManagerBrowserTest,
+    ::testing::Values(TestParameter(NOT_IN_GUEST_MODE,
+                                    "tabindexFocusDownloads"),
+                      TestParameter(IN_GUEST_MODE, "tabindexFocusDownloads")));
+
+INSTANTIATE_TEST_CASE_P(
     TabindexFocusDirectorySelected,
     FileManagerBrowserTest,
     ::testing::Values(TestParameter(NOT_IN_GUEST_MODE,
