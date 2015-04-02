f0356c7a7cec295ac32e7802837732a601b626e0
wjmaclean <wjmaclean@chromium.org>
2015-04-02 09:21:24 -0700

    Add default zoom functionality to chrome.tabs Zoom API.
    This CL adds default zoom factor functionality to the tabs zoom api.
    
    Tabs can now be set to the current default zoom via
    
    setZoom(tabId, 0)
    
    and the current default zoom level can be retrieved as a parameter
    default_zoom_factor in the ZoomSettings structure returned by
    getZoomSettings.
    
    BUG=470550
    
    Review URL: https://codereview.chromium.org/1051013002
    
    Cr-Commit-Position: refs/heads/master@{#323505}

--- chrome/browser/extensions/api/tabs/tabs_api.cc
+++ chrome/browser/extensions/api/tabs/tabs_api.cc
@@ -1855,7 +1855,9 @@ bool TabsSetZoomFunction::RunAsync() {
 
   ZoomController* zoom_controller =
       ZoomController::FromWebContents(web_contents);
-  double zoom_level = content::ZoomFactorToZoomLevel(params->zoom_factor);
+  double zoom_level = params->zoom_factor > 0
+                          ? content::ZoomFactorToZoomLevel(params->zoom_factor)
+                          : zoom_controller->GetDefaultZoomLevel();
 
   scoped_refptr<ExtensionZoomRequestClient> client(
       new ExtensionZoomRequestClient(extension()));
@@ -1954,6 +1956,8 @@ bool TabsGetZoomSettingsFunction::RunAsync() {
   ZoomController::ZoomMode zoom_mode = zoom_controller->zoom_mode();
   api::tabs::ZoomSettings zoom_settings;
   ZoomModeToZoomSettings(zoom_mode, &zoom_settings);
+  zoom_settings.default_zoom_factor.reset(new double(
+      content::ZoomLevelToZoomFactor(zoom_controller->GetDefaultZoomLevel())));
 
   results_ = api::tabs::GetZoomSettings::Results::Create(zoom_settings);
   SendResponse(true);
