8113cea9534f7f9b2bbbf0a8b2ff9ca5365df9c9
atwilson <atwilson@chromium.org>
2015-04-02 09:22:51 -0700

    Now parse /proc/stat to extract CPU statistics for kiosk reporting.
    Instead of trying to walk the process tree and accumulate CPU stats,
    we now just pull them directly from /proc/stat, which is more accurate
    and simpler.
    
    BUG=464970
    
    Review URL: https://codereview.chromium.org/1056693002
    
    Cr-Commit-Position: refs/heads/master@{#323506}

--- chrome/browser/chromeos/policy/status_uploader_unittest.cc
+++ chrome/browser/chromeos/policy/status_uploader_unittest.cc
@@ -35,8 +35,8 @@ class MockDeviceStatusCollector : public policy::DeviceStatusCollector {
             local_state,
             nullptr,
             policy::DeviceStatusCollector::LocationUpdateRequester(),
-            policy::DeviceStatusCollector::VolumeInfoFetcher()) {
-  }
+            policy::DeviceStatusCollector::VolumeInfoFetcher(),
+            policy::DeviceStatusCollector::CPUStatisticsFetcher()) {}
 
   MOCK_METHOD1(GetDeviceStatus, bool(em::DeviceStatusReportRequest*));
   MOCK_METHOD1(GetDeviceSessionStatus, bool(em::SessionStatusReportRequest*));
