b9b54c150bb396f3ad9929948a7b95a99ee90daa
garykac <garykac@chromium.org>
2015-04-02 14:10:47 -0700

    [Chromoting] Re-add setDesktopScale.
    setDesktopScale was removed as part of crrev.com/918783002 because it was not
    being used anywhere in the code. However, we sometimes call this function
    manually from the debug console when troubleshooting problems with desktop
    scaling.
    
    This cl re-adds the setDesktopScale function and puts it in
    remoting.experimental so that:
    
    (1) We have a standard place to put these debugging functions (it has moved
    a few times during the refactoring which can make it difficult to find and
    call.
    
    (2) We have a code that indicates how the function is being used so that we
    don't accidentally remove it again.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1052073003
    
    Cr-Commit-Position: refs/heads/master@{#323560}

--- remoting/remoting_webapp_files.gypi
+++ remoting/remoting_webapp_files.gypi
@@ -278,6 +278,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'webapp/crd/js/crd_auth_dialog.js',
       'webapp/crd/js/crd_connect.js',
       'webapp/crd/js/crd_event_handlers.js',
+      'webapp/crd/js/crd_experimental.js',
       'webapp/crd/js/crd_main.js',
       'webapp/crd/js/desktop_remoting.js',
       'webapp/crd/js/it2me_connect_flow.js',
---
+++ remoting/webapp/crd/js/crd_experimental.js
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * Experimental features that can be accessed via the Chrome debugging console.
+ */
+
+'use strict';
+
+/** @suppress {duplicate} */
+var remoting = remoting || {};
+
+remoting.experimental = {};
+
+/**
+ * Sets and stores the scale factor to apply to host sizing requests.
+ * The desktopScale applies to the dimensions reported to the host, not
+ * to the client DPI reported to it.
+ *
+ * @param {number} desktopScale Scale factor to apply.
+ */
+remoting.experimental.setDesktopScale = function(desktopScale) {
+  var mode = remoting.app.getConnectionMode();
+  if (mode == remoting.Application.Mode.IT2ME ||
+      mode == remoting.Application.Mode.ME2ME) {
+    var drApp = /** @type {remoting.DesktopRemoting} */ (remoting.app);
+    var connectedView = drApp.getConnectedViewForTesting();
+    var viewport = connectedView.getViewportForTesting();
+    viewport.setDesktopScale(desktopScale);
+  }
+};
