041c5dc882879342959500aab23786b987fd8f25
dmazzoni <dmazzoni@chromium.org>
2015-04-02 01:47:26 -0700

    Add initial live region support to ChromeVox 2.
    Introduces a concept of mutation observers to the automation API.
    
    BUG=467733
    
    Review URL: https://codereview.chromium.org/1016603003
    
    Cr-Commit-Position: refs/heads/master@{#323447}

--- chrome/renderer/resources/extensions/automation/automation_node.js
+++ chrome/renderer/resources/extensions/automation/automation_node.js
@@ -486,6 +486,13 @@ var TableCellMixinAttributes = {
   tableCellRowSpan: defaultIntAttribute(1)
 };
 
+var LiveRegionMixinAttributes = {
+  containerLiveAtomic: defaultBoolAttribute(),
+  containerLiveBusy: defaultBoolAttribute(),
+  containerLiveRelevant: defaultStringAttribute(),
+  containerLiveStatus: defaultStringAttribute(),
+};
+
 /**
  * AutomationRootNode.
  *
@@ -570,6 +577,28 @@ AutomationRootNodeImpl.prototype = {
                     chrome);
       return false;
     }
+
+    // Notify tree change observers of new nodes.
+    // TODO(dmazzoni): Notify tree change observers of changed nodes,
+    // and handle subtreeCreated and nodeCreated properly.
+    var observers = automationUtil.treeChangeObservers;
+    if (observers.length > 0) {
+      for (var nodeId in updateState.newNodes) {
+        var node = updateState.newNodes[nodeId];
+        var treeChange =
+            {target: node, type: schema.TreeChangeType.nodeCreated};
+        for (var i = 0; i < observers.length; i++) {
+          try {
+            observers[i](treeChange);
+          } catch (e) {
+            console.error('Error in tree change observer for ' +
+                treeChange.type + ': ' + e.message +
+                '\nStack trace: ' + e.stack);
+          }
+        }
+      }
+    }
+
     return true;
   },
 
@@ -619,6 +648,21 @@ AutomationRootNodeImpl.prototype = {
   invalidate_: function(node) {
     if (!node)
       return;
+
+    // Notify tree change observers of the removed node.
+    var observers = automationUtil.treeChangeObservers;
+    if (observers.length > 0) {
+      var treeChange = {target: node, type: schema.TreeChangeType.nodeRemoved};
+      for (var i = 0; i < observers.length; i++) {
+        try {
+          observers[i](treeChange);
+        } catch (e) {
+          console.error('Error in tree change observer for ' + treeChange.type +
+              ': ' + e.message + '\nStack trace: ' + e.stack);
+        }
+      }
+    }
+
     var children = node.children;
 
     for (var i = 0, child; child = children[i]; i++) {
@@ -768,6 +812,13 @@ AutomationRootNodeImpl.prototype = {
         this.mixinAttributes_(nodeImpl, ScrollableMixinAttributes, nodeData);
         break;
       }
+    }
+
+    // If this is inside a live region, set live region mixins.
+    var attr = 'containerLiveStatus';
+    var spec = LiveRegionMixinAttributes[attr];
+    if (this.findAttribute_(attr, spec, nodeData) !== undefined) {
+      this.mixinAttributes_(nodeImpl, LiveRegionMixinAttributes, nodeData);
     }
 
     // If this is a link, set link attributes
@@ -971,8 +1022,10 @@ var AutomationNode = utils.expose('AutomationNode',
                                                 'setSelection',
                                                 'addEventListener',
                                                 'removeEventListener',
+                                                'addTreeChangeObserver',
+                                                'removeTreeChangeObserver',
                                                 'domQuerySelector',
-                                                'toJSON' ],
+                                                'toString' ],
                                     readonly: ['parent',
                                                'firstChild',
                                                'lastChild',
