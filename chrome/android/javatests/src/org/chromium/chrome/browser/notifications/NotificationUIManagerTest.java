7dec7eeed28483eae72b7199bf3b026eaaabb6c4
jam <jam@chromium.org>
2015-04-02 08:51:39 -0700

    Disable flaky NotificationUIManagerTest#testShowAndCloseMultipleNotifications
    BUG=472926
    TBR=peter@chromium.org
    
    Review URL: https://codereview.chromium.org/1057913002
    
    Cr-Commit-Position: refs/heads/master@{#323498}

--- chrome/android/javatests/src/org/chromium/chrome/browser/notifications/NotificationUIManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/notifications/NotificationUIManagerTest.java
@@ -15,6 +15,7 @@ import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.annotations.SuppressFBWarnings;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.chrome.browser.preferences.website.ContentSetting;
@@ -315,8 +316,12 @@ public class NotificationUIManagerTest extends ChromeShellTestBase {
      * Verifies that multiple notifications without a tag can be opened and closed without
      * affecting eachother.
      */
+    /*
     @MediumTest
     @Feature({"Browser", "Notifications"})
+    crbug.com/472926
+    */
+    @DisabledTest
     public void testShowAndCloseMultipleNotifications() throws Exception {
         setNotificationContentSettingForCurrentOrigin(ContentSetting.ALLOW);
 
