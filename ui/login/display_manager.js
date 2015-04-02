9ff2aec26d55e2a7a047a9c215c302568c1ff2c3
merkulova <merkulova@chromium.org>
2015-04-02 03:57:36 -0700

    Prevent unexpected pods sliding on start screen.
    BUG=385627
    
    Review URL: https://codereview.chromium.org/1045803002
    
    Cr-Commit-Position: refs/heads/master@{#323461}

--- ui/login/display_manager.js
+++ ui/login/display_manager.js
@@ -708,7 +708,7 @@ cr.define('cr.ui.login', function() {
       screen.style.width = '';
       screen.style.height = '';
 
-     $('outer-container').classList.toggle(
+      $('outer-container').classList.toggle(
         'fullscreen', screen.classList.contains('fullscreen'));
 
       var width = screen.getPreferredSize().width;
