bce11fe465deea4fc5389dcc1df3f6436c9814bc
horo@chromium.org <horo@chromium.org>
2015-04-02 04:19:51 +0000

    [DevTools] Add "debug on start" check box in ServiceWorkersView of DevTools window [4/4 blink]
    1/4 chromium: https://codereview.chromium.org/1050073002/
    2/4 blink: https://codereview.chromium.org/1054553002/
    3/4 chromium: https://codereview.chromium.org/1056513002/
    4/4 blink: This cl.
    
    Screen shot: https://code.google.com/p/chromium/issues/detail?id=466871#c38
    
    BUG=466871
    
    Review URL: https://codereview.chromium.org/1052623003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192995 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/ServiceWorkerManager.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/ServiceWorkerManager.js
@@ -50,7 +50,8 @@ WebInspector.ServiceWorkerManager = function(target)
 WebInspector.ServiceWorkerManager.Events = {
     WorkersUpdated: "WorkersUpdated",
     RegistrationUpdated: "RegistrationUpdated",
-    RegistrationDeleted: "RegistrationDeleted"
+    RegistrationDeleted: "RegistrationDeleted",
+    DebugOnStartUpdated: "DebugOnStartUpdated"
 }
 
 WebInspector.ServiceWorkerManager.prototype = {
@@ -92,6 +93,22 @@ WebInspector.ServiceWorkerManager.prototype = {
     hasWorkers: function()
     {
         return !!this._workers.size;
+    },
+
+    /**
+     * @return {boolean}
+     */
+    debugOnStart: function()
+    {
+        return !!this._debugOnStart;
+    },
+
+    /**
+     * @param {boolean} flag
+     */
+    setDebugOnStart: function(flag)
+    {
+        this._agent.setDebugOnStart(flag);
     },
 
     /**
@@ -249,6 +266,15 @@ WebInspector.ServiceWorkerManager.prototype = {
     },
 
     /**
+     * @param {boolean} flag
+     */
+    _debugOnStartUpdated: function(flag)
+    {
+        this._debugOnStart = flag;
+        this.dispatchEventToListeners(WebInspector.ServiceWorkerManager.Events.DebugOnStartUpdated, flag);
+    },
+
+    /**
      * @param {!WebInspector.Event} event
      */
     _mainFrameNavigated: function(event)
@@ -402,11 +428,11 @@ WebInspector.ServiceWorkerDispatcher.prototype = {
 
     /**
      * @override
-     * @param {boolean} debugOnStart
+     * @param {boolean} flag
      */
-    debugOnStartUpdated: function(debugOnStart)
+    debugOnStartUpdated: function(flag)
     {
-        // FIXME: implement this.
+        this._manager._debugOnStartUpdated(flag);
     }
 }
 
