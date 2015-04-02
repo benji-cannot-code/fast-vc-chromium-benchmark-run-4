e19ea67eb27f929f7784df200306e03edfd3a3d8
ksakamoto@chromium.org <ksakamoto@chromium.org>
2015-04-02 10:12:27 +0000

    [DevTools] Add test for User-Agent override for ServiceWorkers
    BUG=444820
    
    Review URL: https://codereview.chromium.org/1038693004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193023 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/inspector/service-workers/resources/user-agent-override-worker.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function dumpUserAgent()
+{
+    fetch('http://127.0.0.1:8000/inspector/resources/echo-headers.php')
+        .then(function(response) {
+            return response.text();
+        }).then(function(body) {
+            var userAgentLine = /HTTP_USER_AGENT:.*/.exec(body)[0];
+            console.log(userAgentLine);
+        }).catch(function(err) {
+            console.log('fetch failed: ' + err.message);
+        });
+}
+
+self.oninstall = function(event) {
+    dumpUserAgent();
+};
