2f1fe04f5f0865f618714c777eca513e8cd4ba07
shreeram.k@samsung.com <shreeram.k@samsung.com>
2015-04-02 05:48:10 +0000

    Expose multiline attribute from blink
    This CL adds a method to expose multiline attr to chromium.
    
    This CL is starting patch to Get rid of separate TextField and TextArea AX roles
    and support a single role TextField with multiline attribute. More CLs will follow this.
    
    BUG=471152
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=192814
    
    Review URL: https://codereview.chromium.org/1045693002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192998 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
@@ -603,7 +603,7 @@ bool AXLayoutObject::computeAccessibilityIsIgnored() const
 
         // Don't ignore static text in editable text controls.
         for (AXObject* parent = parentObject(); parent; parent = parent->parentObject()) {
-            if (parent->roleValue() == TextFieldRole || parent->roleValue() == TextAreaRole)
+            if (parent->roleValue() == TextFieldRole)
                 return false;
         }
 
@@ -905,7 +905,6 @@ String AXLayoutObject::actionVerb() const
     case ToggleButtonRole:
         return queryString(WebLocalizedString::AXButtonActionVerb);
     case TextFieldRole:
-    case TextAreaRole:
         return queryString(WebLocalizedString::AXTextFieldActionVerb);
     case RadioButtonRole:
         return queryString(WebLocalizedString::AXRadioButtonActionVerb);
