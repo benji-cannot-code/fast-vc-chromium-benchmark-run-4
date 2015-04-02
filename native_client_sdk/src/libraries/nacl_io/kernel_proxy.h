ccd7e691ca0b04bd40d50baea0da15c1cb2403dc
sbc <sbc@chromium.org>
2015-04-01 18:23:14 -0700

    [NaCl SDK] nacl_io: Add ossignal.h to kernel_proxy.h
    This was removed in erroneously in https://codereview.chromium.org/1051973003
    
    R=binji
    
    Review URL: https://codereview.chromium.org/1057743002
    
    Cr-Commit-Position: refs/heads/master@{#323391}

--- native_client_sdk/src/libraries/nacl_io/kernel_proxy.h
+++ native_client_sdk/src/libraries/nacl_io/kernel_proxy.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "nacl_io/host_resolver.h"
 #include "nacl_io/kernel_object.h"
 #include "nacl_io/nacl_io.h"
+#include "nacl_io/ossignal.h"
 #include "nacl_io/ossocket.h"
 #include "nacl_io/ostypes.h"
 #include "nacl_io/osutime.h"
