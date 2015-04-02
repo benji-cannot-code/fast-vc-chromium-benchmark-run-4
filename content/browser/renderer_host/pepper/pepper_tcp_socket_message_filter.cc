5b18c391043cbd73ad4bf346889f80a6757f362a
rvargas <rvargas@chromium.org>
2015-04-01 18:00:55 -0700

    Add a jank tracker to PepperTCPSocketMessageFilter::OnConnectCompleted.
    BUG=462784
    
    Review URL: https://codereview.chromium.org/1051803003
    
    Cr-Commit-Position: refs/heads/master@{#323383}

--- content/browser/renderer_host/pepper/pepper_tcp_socket_message_filter.cc
+++ content/browser/renderer_host/pepper/pepper_tcp_socket_message_filter.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstring>
 
 #include "base/bind.h"
+#include "base/location.h"
 #include "base/logging.h"
 #include "base/profiler/scoped_tracker.h"
 #include "build/build_config.h"
@@ -791,6 +792,11 @@ void PepperTCPSocketMessageFilter::OnConnectCompleted(
     const ppapi::host::ReplyMessageContext& context,
     int net_result) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
+
+  // TODO(rvargas): Remove ScopedTracker below once crbug.com/462784 is fixed.
+  tracked_objects::ScopedTracker tracking_profile(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "462784 PepperTCPSocketMessageFilter::OnConnectCompleted"));
 
   if (!state_.IsPending(TCPSocketState::CONNECT)) {
     DCHECK(state_.state() == TCPSocketState::CLOSED);
