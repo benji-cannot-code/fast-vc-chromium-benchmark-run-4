a420f6a87e64f5bcdb2cfcff4ce4ec8df8729dfd
John Abd-El-Malek <jam@chromium.org>
2015-04-02 10:59:22 -0700

    Fix compile failures after r323528.
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/1058023002
    
    Cr-Commit-Position: refs/heads/master@{#323538}

--- mojo/services/window_manager/capture_controller_observer.h
+++ mojo/services/window_manager/capture_controller_observer.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_WINDOW_MANAGER_CAPTURE_CONTROLLER_OBSERVER_H_
 #define SERVICES_WINDOW_MANAGER_CAPTURE_CONTROLLER_OBSERVER_H_
 
+namespace mojo {
+class View;
+}
+
 namespace window_manager {
 
 class CaptureControllerObserver {
