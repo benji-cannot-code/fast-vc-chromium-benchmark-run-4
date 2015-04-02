f287677dabca1ba130970d43c906d5450ad63ef9
robhogan@gmail.com <robhogan@gmail.com>
2015-04-02 19:02:22 +0000

    Don't error out if we successfully loaded an image
    In https://codereview.chromium.org/481753002 I inadvertently introduced behaviour
    for img elements where we show fallback content if we got a 404 but still loaded an
    image.
    
    The spec shows that this behaviour is OK for the object element but not for the
    img element, where its silence implies we shoud load and display the image if we
    got one - even if the response was a 404.
    
    BUG=471572
    
    Review URL: https://codereview.chromium.org/1047323002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLImageLoader.cpp
+++ third_party/WebKit/Source/core/html/HTMLImageLoader.cpp
@@ -87,7 +87,7 @@ void HTMLImageLoader::notifyFinished(Resource*)
     RefPtrWillBeRawPtr<Element> element = this->element();
     ImageLoader::notifyFinished(cachedImage);
 
-    bool loadError = cachedImage->errorOccurred() || cachedImage->response().httpStatusCode() >= 400;
+    bool loadError = cachedImage->errorOccurred();
     if (isHTMLImageElement(*element)) {
         if (loadError)
             ensureFallbackContent();
@@ -102,7 +102,7 @@ void HTMLImageLoader::notifyFinished(Resource*)
             toHTMLInputElement(element)->ensurePrimaryContent();
     }
 
-    if (loadError && isHTMLObjectElement(*element))
+    if ((loadError || cachedImage->response().httpStatusCode() >= 400) && isHTMLObjectElement(*element))
         toHTMLObjectElement(element)->renderFallbackContent();
 }
 
