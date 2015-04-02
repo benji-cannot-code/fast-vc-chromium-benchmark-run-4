fe91b85a5b845aada6f65ce2eb77d88f3d0a6dfb
sdefresne <sdefresne@chromium.org>
2015-04-02 06:59:17 -0700

    Move kEnableTouchIcon to chrome/browser/favicon/favicon_tab_helper.cc
    The constant kEnableTouchIcon is only used by FaviconTabHelper so move
    it from chrome/common/chrome_constants.{h,cc} to favicon_tab_helper.cc.
    
    BUG=370878
    
    Review URL: https://codereview.chromium.org/1057863002
    
    Cr-Commit-Position: refs/heads/master@{#323476}

--- chrome/browser/favicon/favicon_tab_helper.cc
+++ chrome/browser/favicon/favicon_tab_helper.cc
@@ -53,8 +53,10 @@ bool IsIconNTPEnabled() {
 
 #if defined(OS_ANDROID) || defined(OS_IOS)
 const bool kDownloadLargestIcon = true;
+const bool kEnableTouchIcon = true;
 #else
 const bool kDownloadLargestIcon = false;
+const bool kEnableTouchIcon = false;
 #endif
 
 }  // namespace
@@ -66,7 +68,7 @@ FaviconTabHelper::FaviconTabHelper(WebContents* web_contents)
       profile_, ServiceAccessType::EXPLICIT_ACCESS);
   favicon_handler_.reset(new favicon::FaviconHandler(
       service, this, favicon::FaviconHandler::FAVICON, kDownloadLargestIcon));
-  if (chrome::kEnableTouchIcon) {
+  if (kEnableTouchIcon) {
     touch_icon_handler_.reset(new favicon::FaviconHandler(
         service, this, favicon::FaviconHandler::TOUCH, kDownloadLargestIcon));
   }
