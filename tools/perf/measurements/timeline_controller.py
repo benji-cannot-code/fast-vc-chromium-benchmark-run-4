ea24e268d1acd19edeea693630376c291b2f355d
aiolos <aiolos@chromium.org>
2015-04-03 12:20:58 -0700

    Move action_runner.py out of actions folder prior to moving actions to internal.
    BUG=473414
    
    Review URL: https://codereview.chromium.org/1058153002
    
    Cr-Commit-Position: refs/heads/master@{#323791}

--- tools/perf/measurements/timeline_controller.py
+++ tools/perf/measurements/timeline_controller.py
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from telemetry.core.platform import tracing_category_filter
 from telemetry.core.platform import tracing_options
-from telemetry.page.actions import action_runner
+from telemetry.page import action_runner
 from telemetry.timeline.model import TimelineModel
 from telemetry.value import trace
 from telemetry.web_perf import timeline_interaction_record as tir_module
