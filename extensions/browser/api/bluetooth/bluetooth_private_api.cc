b62601115100a7adbbf2e3393584dd8b89d2a6e4
jpawlowski <jpawlowski@chromium.org>
2015-04-03 01:48:44 -0700

    Add new method: chrome.bluetoothPrivate.setDiscoveryFilter
    This patch adds new method:
    chrome.bluetoothPrivate.setDiscoveryFilter.
    
    BUG=407773
    R=armansito@chromium.org
    
    Review URL: https://codereview.chromium.org/1054853002
    
    Cr-Commit-Position: refs/heads/master@{#323708}

--- extensions/browser/api/bluetooth/bluetooth_private_api.cc
+++ extensions/browser/api/bluetooth/bluetooth_private_api.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/api/bluetooth_private.h"
 
 namespace bt_private = extensions::core_api::bluetooth_private;
+namespace SetDiscoveryFilter =
+    extensions::core_api::bluetooth_private::SetDiscoveryFilter;
 
 namespace extensions {
 
@@ -84,6 +86,8 @@ const char kAdapterNotPresent[] =
     "Could not find a Bluetooth adapter.";
 
 const char kDisconnectError[] = "Failed to disconnect device";
+
+const char kSetDiscoveryFilterFailed[] = "Failed to set discovery filter";
 
 // Returns true if the pairing response options passed into the
 // setPairingResponse function are valid.
@@ -342,6 +346,19 @@ void BluetoothPrivateDisconnectAllFunction::OnErrorCallback(
     SetError(kDisconnectError);
 
   SendResponse(false);
+}
+
+bool BluetoothPrivateSetDiscoveryFilterFunction::DoWork(
+    scoped_refptr<device::BluetoothAdapter> adapter) {
+  scoped_ptr<SetDiscoveryFilter::Params> params(
+      SetDiscoveryFilter::Params::Create(*args_));
+
+  // TODO(jpawlowski): parse arguments, and call event router when method is
+  // implemented.
+
+  SetError(kSetDiscoveryFilterFailed);
+  SendResponse(false);
+  return true;
 }
 
 }  // namespace core_api
