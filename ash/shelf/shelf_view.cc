5616e33d48e9b53d2fcc01694ed6362f76f17815
bruthig <bruthig@chromium.org>
2015-04-03 13:45:32 -0700

    Added user action metric collection for the shelf.
    Added the following user action metrics to learn about how users are
    interacting with the shelf: Launcher_ButtonPressed_Mouse,
    Launcher_ButtonPressed_Touch, Launcher_LaunchTask.
    
    TBR=asvitkine@chromium.org
    
    TEST=Manually verified user action collection when clicking/tapping
    pinned/unpinned icons.
    
    BUG=471354
    
    Review URL: https://codereview.chromium.org/1059623002
    
    Cr-Commit-Position: refs/heads/master@{#323810}

--- ash/shelf/shelf_view.cc
+++ ash/shelf/shelf_view.cc
@@ -310,6 +310,15 @@ void ReflectItemStatus(const ShelfItem& item, ShelfButton* button) {
   }
 }
 
+void RecordIconActivatedAction(const ui::Event& event) {
+  if (event.IsMouseEvent())
+    Shell::GetInstance()->metrics()->RecordUserMetricsAction(
+        UMA_LAUNCHER_BUTTON_PRESSED_WITH_MOUSE);
+  else if (event.IsGestureEvent())
+    Shell::GetInstance()->metrics()->RecordUserMetricsAction(
+        UMA_LAUNCHER_BUTTON_PRESSED_WITH_TOUCH);
+}
+
 }  // namespace
 
 // AnimationDelegate used when deleting an item. This steadily decreased the
@@ -1684,6 +1693,7 @@ void ShelfView::ButtonPressed(views::Button* sender, const ui::Event& event) {
 
   if (sender == overflow_button_) {
     ToggleOverflowBubble();
+    RecordIconActivatedAction(event);
     return;
   }
 
@@ -1742,10 +1752,16 @@ void ShelfView::ButtonPressed(views::Button* sender, const ui::Event& event) {
         break;
     }
 
+    RecordIconActivatedAction(event);
+
     ShelfItemDelegate* item_delegate =
         item_manager_->GetShelfItemDelegate(model_->items()[view_index].id);
-    if (!item_delegate->ItemSelected(event))
+    if (!item_delegate->ItemSelected(event)) {
       ShowListMenuForView(model_->items()[view_index], sender, event);
+    } else {
+      Shell::GetInstance()->metrics()->RecordUserMetricsAction(
+          UMA_LAUNCHER_LAUNCH_TASK);
+    }
   }
 }
 
