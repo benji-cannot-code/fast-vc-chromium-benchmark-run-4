37931595e5272afe7ad74d1d28c52a9e5c0bbba4
vitalybuka <vitalybuka@chromium.org>
2015-04-03 13:53:19 -0700

    Don't use --check-cloud-print-connector-policy on MAC
    OSX crashes if --check-cloud-print-connector-policy is set.
    
    Policy check on service side is unnecessary as policy
    can be update by browser only. In this case browser will enforce the policy from own side.
    
    This is small fix in case we want merge.
    Subsequent CLs will remove all related code.
    
    BUG=469770
    
    Review URL: https://codereview.chromium.org/1055453004
    
    Cr-Commit-Position: refs/heads/master@{#323812}

--- chrome/service/cloud_print/cloud_print_proxy.cc
+++ chrome/service/cloud_print/cloud_print_proxy.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+#if !defined(OS_MACOSX)
 void LaunchBrowserProcessWithSwitch(const std::string& switch_string) {
   DCHECK(g_service_process->io_thread()->message_loop_proxy()->
       BelongsToCurrentThread());
@@ -61,6 +62,8 @@ void LaunchBrowserProcessWithSwitch(const std::string& switch_string) {
 void CheckCloudPrintProxyPolicyInBrowser() {
   LaunchBrowserProcessWithSwitch(switches::kCheckCloudPrintConnectorPolicy);
 }
+
+#endif  // !OS_MACOSX
 
 }  // namespace
 
@@ -215,8 +218,10 @@ void CloudPrintProxy::GetPrinters(std::vector<std::string>* printers) {
 }
 
 void CloudPrintProxy::CheckCloudPrintProxyPolicy() {
+#if !defined(OS_MACOSX)
   g_service_process->io_thread()->message_loop_proxy()->PostTask(
       FROM_HERE, base::Bind(&CheckCloudPrintProxyPolicyInBrowser));
+#endif
 }
 
 void CloudPrintProxy::OnAuthenticated(
