517eedb31bec7f2aafd23be28c47996ce1c000a2
sadrul <sadrul@chromium.org>
2015-04-02 22:33:48 -0700

    content: Delete some now-dead event related code.
    Some of the removed methods were never used, and the rest had no
    real implementations.
    
    BUG=None
    R=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/1052063002
    
    Cr-Commit-Position: refs/heads/master@{#323637}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -1355,11 +1355,6 @@ void BrowserView::UserChangedTheme() {
   frame_->FrameTypeChanged();
 }
 
-int BrowserView::GetExtraRenderViewHeight() const {
-  // Currently this is only used on linux.
-  return 0;
-}
-
 void BrowserView::WebContentsFocused(WebContents* contents) {
   if (contents_web_view_->GetWebContents() == contents)
     contents_web_view_->OnWebContentsFocused(contents);
--- chrome/test/base/test_browser_window.cc
+++ chrome/test/base/test_browser_window.cc
@@ -206,10 +206,6 @@ DownloadShelf* TestBrowserWindow::GetDownloadShelf() {
   return &download_shelf_;
 }
 
-int TestBrowserWindow::GetExtraRenderViewHeight() const {
-  return 0;
-}
-
 WindowOpenDisposition TestBrowserWindow::GetDispositionForPopupBounds(
     const gfx::Rect& bounds) {
   return NEW_POPUP;
