1bebde768567ffe958c70b707edd0911814ebe2d
bondd <bondd@chromium.org>
2015-04-03 01:01:16 -0700

    Autofill OSX: Draw red outline around incorrect CVC input.
    Change CVC text input field from NSTextField -> AutofillTextField,
    and mark it as invalid (causing red border to be drawn) when a
    retriable error occurs and no expiry date is showing. If an expiry date
    is showing then it's not clear whether the date or CVC caused the error,
    so don't mark either of them invalid.
    
    BUG=448572
    
    Review URL: https://codereview.chromium.org/1052513002
    
    Cr-Commit-Position: refs/heads/master@{#323686}

--- chrome/browser/ui/cocoa/autofill/card_unmask_prompt_view_bridge.mm
+++ chrome/browser/ui/cocoa/autofill/card_unmask_prompt_view_bridge.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/autofill_dialog_types.h"
 #include "chrome/browser/ui/autofill/card_unmask_prompt_controller.h"
 #include "chrome/browser/ui/chrome_style.h"
+#import "chrome/browser/ui/cocoa/autofill/autofill_textfield.h"
 #import "chrome/browser/ui/cocoa/autofill/autofill_tooltip_controller.h"
 #include "chrome/browser/ui/cocoa/autofill/card_unmask_prompt_view_bridge.h"
 #import "chrome/browser/ui/cocoa/constrained_window/constrained_window_button.h"
@@ -140,7 +141,7 @@ void CardUnmaskPromptViewBridge::PerformClose() {
   base::scoped_nsobject<NSTextField> titleLabel_;
   base::scoped_nsobject<NSTextField> permanentErrorLabel_;
   base::scoped_nsobject<NSTextField> instructionsLabel_;
-  base::scoped_nsobject<NSTextField> cvcInput_;
+  base::scoped_nsobject<AutofillTextField> cvcInput_;
   base::scoped_nsobject<NSPopUpButton> monthPopup_;
   base::scoped_nsobject<NSPopUpButton> yearPopup_;
   base::scoped_nsobject<NSButton> cancelButton_;
@@ -250,6 +251,12 @@ void CardUnmaskPromptViewBridge::PerformClose() {
           SysUTF16ToNSString(text), chrome_style::kTextFontStyle,
           NSNaturalTextAlignment, NSLineBreakByWordWrapping);
   [errorLabel_ setAttributedStringValue:attributedString];
+
+  // If there is more than one input showing, don't mark anything as
+  // invalid since we don't know the location of the problem.
+  if (!text.empty() && !bridge_->GetController()->ShouldRequestExpirationDate())
+    [cvcInput_ setValidityMessage:@"invalid"];
+
   [self performLayoutAndDisplay:YES];
 }
 
@@ -334,6 +341,10 @@ void CardUnmaskPromptViewBridge::PerformClose() {
 
 // Called when text in CVC input field changes.
 - (void)controlTextDidChange:(NSNotification*)notification {
+  if (bridge_->GetController()->InputCvcIsValid(
+          base::SysNSStringToUTF16([cvcInput_ stringValue])))
+    [cvcInput_ setValidityMessage:@""];
+
   [self updateVerifyButtonEnabled];
 }
 
@@ -553,7 +564,7 @@ void CardUnmaskPromptViewBridge::PerformClose() {
   }
 
   // Add CVC text input.
-  cvcInput_.reset([[NSTextField alloc] initWithFrame:NSZeroRect]);
+  cvcInput_.reset([[AutofillTextField alloc] initWithFrame:NSZeroRect]);
   [[cvcInput_ cell]
       setPlaceholderString:l10n_util::GetNSString(
                                IDS_AUTOFILL_DIALOG_PLACEHOLDER_CVC)];
