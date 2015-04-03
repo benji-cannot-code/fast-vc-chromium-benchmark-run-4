c7e78c00191d1d7cc2df68a33370fa4491d9da01
xunjieli <xunjieli@chromium.org>
2015-04-02 22:17:12 -0700

    Make TcpListenSocket's CreateAnd* methods private
    This CL makes TcpListenSocket's CreateAnd* methods private, so there won't be
    any new consumers of TcpListenSocket. This is a part of the effort to remove
    TcpListenSocket entirely.
    
    BUG=472766
    
    Review URL: https://codereview.chromium.org/1054033002
    
    Cr-Commit-Position: refs/heads/master@{#323614}

--- net/socket/tcp_listen_socket.h
+++ net/socket/tcp_listen_socket.h
@@ -13,12 +13,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/socket/socket_descriptor.h"
 #include "net/socket/stream_listen_socket.h"
 
+namespace nacl {
+class NaClProcessHost;
+}
+
 namespace net {
 
-// Implements a TCP socket.
+namespace test_server {
+class EmbeddedTestServer;
+}
+
+// Implements a TCP socket. This class is deprecated and will be removed
+// once crbug.com/472766 is fixed. There should not be any new consumer of this
+// class.
 class NET_EXPORT TCPListenSocket : public StreamListenSocket {
  public:
   ~TCPListenSocket() override;
+
+ protected:
+  TCPListenSocket(SocketDescriptor s, StreamListenSocket::Delegate* del);
+
+  // Implements StreamListenSocket::Accept.
+  void Accept() override;
+
+ private:
+  // Note that friend classes are temporary until crbug.com/472766 is fixed.
+  friend class test_server::EmbeddedTestServer;
+  friend class TCPListenSocketTester;
+  friend class TransportClientSocketTest;
+  friend class nacl::NaClProcessHost;
+
   // Listen on port for the specified IP address.  Use 127.0.0.1 to only
   // accept local connections.
   static scoped_ptr<TCPListenSocket> CreateAndListen(
@@ -33,13 +57,6 @@ class NET_EXPORT TCPListenSocket : public StreamListenSocket {
   static SocketDescriptor CreateAndBindAnyPort(const std::string& ip,
                                                uint16* port);
 
- protected:
-  TCPListenSocket(SocketDescriptor s, StreamListenSocket::Delegate* del);
-
-  // Implements StreamListenSocket::Accept.
-  void Accept() override;
-
- private:
   DISALLOW_COPY_AND_ASSIGN(TCPListenSocket);
 };
 
