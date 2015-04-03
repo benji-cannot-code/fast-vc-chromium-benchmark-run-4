ca6ebed6b71203f481163aa09bb04d151f5ece17
fukino <fukino@chromium.org>
2015-04-03 04:53:59 -0700

    Files.app: Use FileListModel for the dummy data model.
    FileGrid assumes that the data model is FileListModel.
    However, the data modal can be cr.ui.ArrayDataModel when the view is list model, which triggers invocation of non-existent method.
    This CL fixes the problem by making dummy data model an instance of FileListModel.
    
    BUG=473611
    TEST=manually
    
    Review URL: https://codereview.chromium.org/1051253005
    
    Cr-Commit-Position: refs/heads/master@{#323728}

--- ui/file_manager/file_manager/foreground/js/file_manager.js
+++ ui/file_manager/file_manager/foreground/js/file_manager.js
@@ -872,6 +872,8 @@ FileManager.prototype = /** @struct */ {
         assert(this.thumbnailModel_),
         this.volumeManager_);
     this.ui_.listContainer.dataModel = this.directoryModel_.getFileList();
+    this.ui_.listContainer.emptyDataModel =
+        this.directoryModel_.getEmptyFileList();
     this.ui_.listContainer.selectionModel =
         this.directoryModel_.getFileListSelection();
 
