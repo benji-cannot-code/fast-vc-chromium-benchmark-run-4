b6129aa89ac24a390e46496706902d93e7b98562
dalecurtis <dalecurtis@chromium.org>
2015-04-03 12:23:37 -0700

    Wire up WebContents titles to audio input controllers.
    Does the same thing for input that http://crrev.com/322963
    did for output.
    
    BUG=336992, 454830
    TEST=chrome://media-internals
    
    Review URL: https://codereview.chromium.org/1049943002
    
    Cr-Commit-Position: refs/heads/master@{#323792}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -757,6 +757,7 @@ void RenderProcessHostImpl::CreateMessageFilters() {
   MediaStreamManager* media_stream_manager =
       BrowserMainLoop::GetInstance()->media_stream_manager();
   AddFilter(new AudioInputRendererHost(
+      GetID(),
       audio_manager,
       media_stream_manager,
       AudioMirroringManager::GetInstance(),
