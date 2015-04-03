fb6a594cf5d95278c3e099ac349fd9943e32ebff
boliu <boliu@chromium.org>
2015-04-03 01:07:10 -0700

    Delete SurfaceTextureListener on right thread
    Java finalizer is called on the gc thread, so is usually not the correct
    thread to destroy SurfaceTextureListener. Post deletion to the browser
    thread instead.
    
    BUG=472713
    
    Review URL: https://codereview.chromium.org/1054773003
    
    Cr-Commit-Position: refs/heads/master@{#323695}

--- ui/gl/android/surface_texture_listener.h
+++ ui/gl/android/surface_texture_listener.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <jni.h>
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
+#include "base/sequenced_task_runner_helpers.h"
 #include "ui/gl/gl_export.h"
 
 namespace base {
@@ -29,6 +30,8 @@ class GL_EXPORT SurfaceTextureListener {
   static bool RegisterSurfaceTextureListener(JNIEnv* env);
 
  private:
+  friend class base::DeleteHelper<SurfaceTextureListener>;
+
   // Native code should not hold any reference to this object, and instead pass
   // it up to Java for being referenced by a SurfaceTexture instance.
   SurfaceTextureListener(const base::Closure& callback);
