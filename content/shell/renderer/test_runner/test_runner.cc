4bbd1da91d5955e9dd25d1b46298be1c991634c4
dcheng <dcheng@chromium.org>
2015-04-02 22:39:40 -0700

    Add testRunner.insertStyleSheet for testing style sheet injection.
    testRunner.injectStyleSheet uses a different code path from the only way
    to inject stylesheets in Chrome (extensions) now that user stylesheets
    are no longer supported. In addition, the Blink implementation is not
    friendly to OOPI, since it expects all frames to be in one process.
    
    Tying it to WebLocalFrame::frameForCurrentContext() is a bit awkward,
    but all the TestRunner plumbing currently expects to work on a
    per-WebView basis, and changing it is rather complicated. Once a
    TestRunner can be associated with a WebFrame, it won't need to be tied
    to the current context and the corresponding layout test can be cleaned
    up.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1056913002
    
    Cr-Commit-Position: refs/heads/master@{#323646}

--- content/shell/renderer/test_runner/test_runner.cc
+++ content/shell/renderer/test_runner/test_runner.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebGraphicsContext.h"
 #include "third_party/WebKit/public/web/WebInputElement.h"
 #include "third_party/WebKit/public/web/WebKit.h"
+#include "third_party/WebKit/public/web/WebLocalFrame.h"
 #include "third_party/WebKit/public/web/WebMIDIClientMock.h"
 #include "third_party/WebKit/public/web/WebPageOverlay.h"
 #include "third_party/WebKit/public/web/WebScriptSource.h"
@@ -188,6 +189,7 @@ class TestRunnerBindings : public gin::Wrappable<TestRunnerBindings> {
   bool HasCustomPageSizeStyle(int page_index);
   void ForceRedSelectionColors();
   void InjectStyleSheet(const std::string& source_code, bool all_frames);
+  void InsertStyleSheet(const std::string& source_code);
   bool FindString(const std::string& search_text,
                   const std::vector<std::string>& options_array);
   std::string SelectionAsMarkup();
@@ -395,6 +397,7 @@ gin::ObjectTemplateBuilder TestRunnerBindings::GetObjectTemplateBuilder(
       .SetMethod("forceRedSelectionColors",
                  &TestRunnerBindings::ForceRedSelectionColors)
       .SetMethod("injectStyleSheet", &TestRunnerBindings::InjectStyleSheet)
+      .SetMethod("insertStyleSheet", &TestRunnerBindings::InsertStyleSheet)
       .SetMethod("findString", &TestRunnerBindings::FindString)
       .SetMethod("selectionAsMarkup", &TestRunnerBindings::SelectionAsMarkup)
       .SetMethod("setTextSubpixelPositioning",
@@ -799,6 +802,11 @@ void TestRunnerBindings::InjectStyleSheet(const std::string& source_code,
                                           bool all_frames) {
   if (runner_)
     runner_->InjectStyleSheet(source_code, all_frames);
+}
+
+void TestRunnerBindings::InsertStyleSheet(const std::string& source_code) {
+  if (runner_)
+    runner_->InsertStyleSheet(source_code);
 }
 
 bool TestRunnerBindings::FindString(
@@ -2273,6 +2281,11 @@ void TestRunner::InjectStyleSheet(const std::string& source_code,
       WebVector<WebString>(),
       all_frames ? WebView::InjectStyleInAllFrames
                  : WebView::InjectStyleInTopFrameOnly);
+}
+
+void TestRunner::InsertStyleSheet(const std::string& source_code) {
+  WebLocalFrame::frameForCurrentContext()->document().insertStyleSheet(
+      WebString::fromUTF8(source_code));
 }
 
 bool TestRunner::FindString(const std::string& search_text,
