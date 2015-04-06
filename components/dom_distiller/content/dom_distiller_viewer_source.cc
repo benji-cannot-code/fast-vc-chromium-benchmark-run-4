89508c89e81bf613f1bb64ad80d5401aea12b212
mdjones <mdjones@chromium.org>
2015-04-06 12:48:47 -0700

    Iframe placeholders, security and resizing
    This change adds javascript to fill in the YouTube placeholders
    created by the distiller. the content security policy now allows
    for iframes from all sources (distiller closely controls which
    iframes are allowed). Some css has also been added to appropriately
    resize these iframes when the page is resized.
    
    This change will not work correctly by itself, it depends on the
    following CL to work correctly:
    
    https://codereview.chromium.org/1015463004
    
    BUG=468862
    
    Review URL: https://codereview.chromium.org/880983007
    
    Cr-Commit-Position: refs/heads/master@{#323934}

--- components/dom_distiller/content/dom_distiller_viewer_source.cc
+++ components/dom_distiller/content/dom_distiller_viewer_source.cc
@@ -376,4 +376,8 @@ std::string DomDistillerViewerSource::GetContentSecurityPolicyObjectSrc()
   return "object-src 'none'; style-src 'self' https://fonts.googleapis.com;";
 }
 
+std::string DomDistillerViewerSource::GetContentSecurityPolicyFrameSrc() const {
+  return "frame-src *;";
+}
+
 }  // namespace dom_distiller
