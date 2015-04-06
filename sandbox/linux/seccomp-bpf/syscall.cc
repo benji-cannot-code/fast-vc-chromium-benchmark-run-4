4c8ea24669f7bc6321b312783bc3f459c3068e65
tfarina <tfarina@chromium.org>
2015-04-06 11:27:26 -0700

    Cleanup: Remove basictypes.h includes from seccomp-bpf.
    Tested on Linux with the following command lines:
    
    $ rm -rf out/
    $ ./build/gyp_chromium
    $ ninja -C out/Debug sandbox_linux_unittests
    $ out/Debug/sandbox_linux_unittests
    
    BUG=138542
    TEST=see above
    R=mdempsky@chromium.org,jln@chromium.org
    
    Review URL: https://codereview.chromium.org/1066523002
    
    Cr-Commit-Position: refs/heads/master@{#323914}

--- sandbox/linux/seccomp-bpf/syscall.cc
+++ sandbox/linux/seccomp-bpf/syscall.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <asm/unistd.h>
 #include <errno.h>
+#include <stdint.h>
 
-#include "base/basictypes.h"
 #include "base/logging.h"
 #include "sandbox/linux/bpf_dsl/seccomp_macros.h"
 
