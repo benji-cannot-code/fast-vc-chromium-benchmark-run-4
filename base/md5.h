a97d3db916cb39899efeaacd7ee97c7eaf176be5
tfarina <tfarina@chromium.org>
2015-04-06 14:04:26 -0700

    Cleanup: Use uint8_t type throughout in the MD5 API.
    This seems to be more consistent with the use of uint32_t type that is
    also used throughout in this API.
    
    BUG=None
    TEST=base_unittests --gtest_filter=MD5*
    R=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1062673003
    
    Cr-Commit-Position: refs/heads/master@{#323949}

--- base/md5.h
+++ base/md5.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_MD5_H_
 #define BASE_MD5_H_
 
+#include <stdint.h>
+
 #include "base/base_export.h"
 #include "base/strings/string_piece.h"
 
@@ -36,7 +38,7 @@ namespace base {
 
 // The output of an MD5 operation.
 struct MD5Digest {
-  unsigned char a[16];
+  uint8_t a[16];
 };
 
 // Used for storing intermediate data during an MD5 computation. Callers
