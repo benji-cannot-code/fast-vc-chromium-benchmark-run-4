1934b12fcb3fd637556059708d520d5bc016ddff
haraken@chromium.org <haraken@chromium.org>
2015-04-06 08:50:32 +0000

    Oilpan: Split GCInfo related classes out of Visitor.h
    This is a part of work to reduce a mess of Visitor.h.
    This CL just moving things around and no change in behavior.
    
    BUG=420515
    
    Review URL: https://codereview.chromium.org/1064603002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193174 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Heap.h
+++ third_party/WebKit/Source/platform/heap/Heap.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/PlatformExport.h"
 #include "platform/heap/AddressSanitizer.h"
+#include "platform/heap/GCInfo.h"
 #include "platform/heap/ThreadState.h"
 #include "platform/heap/Visitor.h"
 #include "public/platform/WebThread.h"
