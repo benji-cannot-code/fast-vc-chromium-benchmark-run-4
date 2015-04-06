f824f0f9fb5d520138fac3fbec7ce8a9b359fdeb
avi <avi@chromium.org>
2015-04-06 16:00:41 -0700

    Don't crash when using IME on an input field with a CSS-specified font.
    BUG=471689
    TEST=repro in bug 473859 no longer crashes
    
    Review URL: https://codereview.chromium.org/1052523006
    
    Cr-Commit-Position: refs/heads/master@{#323971}

--- content/common/mac/attributed_string_coder.mm
+++ content/common/mac/attributed_string_coder.mm
@@ -101,10 +101,12 @@ AttributedStringCoder::FontAttribute::FontAttribute()
 AttributedStringCoder::FontAttribute::~FontAttribute() {
 }
 
-NSDictionary* AttributedStringCoder::FontAttribute::ToAttributesDictionary(
-    void) const {
+NSDictionary*
+AttributedStringCoder::FontAttribute::ToAttributesDictionary() const {
   DCHECK(ShouldEncode());
   NSFont* font = font_descriptor_.ToNSFont();
+  if (!font)
+    return [NSDictionary dictionary];
   return [NSDictionary dictionaryWithObject:font forKey:NSFontAttributeName];
 }
 
