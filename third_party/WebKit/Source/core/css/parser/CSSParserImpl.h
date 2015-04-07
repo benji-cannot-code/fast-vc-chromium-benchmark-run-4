5a3837a1f49481e0beb97f62eb51520dbdaac8b7
timloh@chromium.org <timloh@chromium.org>
2015-04-07 05:42:40 +0000

    CSS Parser: Handle at-rules in declaration lists
    The css-syntax spec allows for at-rules in declaration lists. While we
    don't currently support any instances of this (the only case I know
    where this is used is in @page rules), we should still have the logic
    so we can correctly perform error recovery here.
    
    For example, ".a { @x { } left: 10px }" should still apply the left
    property since the at-rule ends at the }.
    
    http://dev.w3.org/csswg/css-syntax/#consume-list-of-declarations
    
    BUG=330389
    
    Review URL: https://codereview.chromium.org/1053953002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193243 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/CSSParserImpl.h
+++ third_party/WebKit/Source/core/css/parser/CSSParserImpl.h
@@ -49,7 +49,8 @@ public:
         AllowImportRules,
         AllowNamespaceRules,
         RegularRules,
-        KeyframeRules
+        KeyframeRules,
+        NoRules, // For parsing at-rules inside declaration lists
     };
 
     static bool parseValue(MutableStylePropertySet*, CSSPropertyID, const String&, bool important, const CSSParserContext&);
