6c76206fb8c8c4dc1087bf5d30a6c7a8f1157fa4
danakj <danakj@chromium.org>
2015-04-07 16:21:29 -0700

    content/aura: Use a PaintRecorder to access the Canvas for painting.
    Don't use the canvas() from the PaintContext so that we can have the
    PaintContext be backed by a DisplayItemList instead in the future.
    Access to the Canvas should be done through a PaintRecorder.
    
    R=piman@chromium.org, sadrul
    BUG=466426
    
    Review URL: https://codereview.chromium.org/1066183002
    
    Cr-Commit-Position: refs/heads/master@{#324153}

--- content/browser/web_contents/aura/overscroll_navigation_overlay.cc
+++ content/browser/web_contents/aura/overscroll_navigation_overlay.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/layout.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_animation_observer.h"
-#include "ui/compositor/paint_context.h"
+#include "ui/compositor/paint_recorder.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/image/image_png_rep.h"
@@ -60,16 +60,16 @@ class ImageLayerDelegate : public ui::LayerDelegate {
  private:
   // Overridden from ui::LayerDelegate:
   void OnPaintLayer(const ui::PaintContext& context) override {
-    gfx::Canvas* canvas = context.canvas();
+    ui::PaintRecorder recorder(context);
     if (image_.IsEmpty()) {
-      canvas->DrawColor(SK_ColorWHITE);
+      recorder.canvas()->DrawColor(SK_ColorWHITE);
     } else {
-      SkISize size = canvas->sk_canvas()->getDeviceSize();
+      SkISize size = recorder.canvas()->sk_canvas()->getDeviceSize();
       if (size.width() != image_size_.width() ||
           size.height() != image_size_.height()) {
-        canvas->DrawColor(SK_ColorWHITE);
+        recorder.canvas()->DrawColor(SK_ColorWHITE);
       }
-      canvas->DrawImageInt(image_.AsImageSkia(), 0, 0);
+      recorder.canvas()->DrawImageInt(image_.AsImageSkia(), 0, 0);
     }
   }
 
--- content/browser/web_contents/aura/shadow_layer_delegate.cc
+++ content/browser/web_contents/aura/shadow_layer_delegate.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/effects/SkGradientShader.h"
 #include "ui/aura/window.h"
 #include "ui/compositor/layer.h"
-#include "ui/compositor/paint_context.h"
+#include "ui/compositor/paint_recorder.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/skia_util.h"
 
@@ -50,7 +50,9 @@ void ShadowLayerDelegate::OnPaintLayer(const ui::PaintContext& context) {
                                    layer_->bounds().height());
   SkPaint paint;
   paint.setShader(shader.get());
-  context.canvas()->sk_canvas()->drawRect(gfx::RectToSkRect(paint_rect), paint);
+  ui::PaintRecorder recorder(context);
+  recorder.canvas()->sk_canvas()->drawRect(gfx::RectToSkRect(paint_rect),
+                                           paint);
 }
 
 void ShadowLayerDelegate::OnDelegatedFrameDamage(
