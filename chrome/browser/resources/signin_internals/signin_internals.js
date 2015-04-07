aae42b00093bfdbd07df0af297c7acdb61974250
anthonyvd <anthonyvd@chromium.org>
2015-04-07 10:50:48 -0700

    Add an Accounts in Token Service section to about:signin-internals.
    This CL also sorts the "access token details by account" section of the
    page by request time.
    
    BUG=405038
    
    Review URL: https://codereview.chromium.org/1058513002
    
    Cr-Commit-Position: refs/heads/master@{#324066}

--- chrome/browser/resources/signin_internals/signin_internals.js
+++ chrome/browser/resources/signin_internals/signin_internals.js
@@ -169,6 +169,7 @@ function refreshSigninInfo(signinInfo) {
   chrome.signin.internalsInfo = signinInfo;
   jstProcess(new JsEvalContext(signinInfo), $('signin-info'));
   jstProcess(new JsEvalContext(signinInfo), $('token-info'));
+  jstProcess(new JsEvalContext(signinInfo), $('account-info'));
 }
 
 // Replace the cookie information with the fetched values.
