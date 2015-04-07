9111bf9c867e0e7884713f97f158d28540cb2c9c
deepak.m1 <deepak.m1@samsung.com>
2015-04-07 08:19:23 -0700

    Changing Option String Resource to lowerCamelcases in handler_options_handler.cc file.
    BUG=470844
    
    Review URL: https://codereview.chromium.org/1015623007
    
    Cr-Commit-Position: refs/heads/master@{#324043}

--- chrome/browser/resources/options/handler_options_list.js
+++ chrome/browser/resources/options/handler_options_list.js
@@ -134,7 +134,7 @@ cr.define('options', function() {
       var defaultOptionElement = document.createElement('option');
       defaultOptionElement.selected = data.default_handler == -1;
       defaultOptionElement.textContent =
-          loadTimeData.getString('handlers_none_handler');
+          loadTimeData.getString('handlersNoneHandler');
       defaultOptionElement.value = -1;
       selectElement.appendChild(defaultOptionElement);
 
@@ -193,7 +193,7 @@ cr.define('options', function() {
         // Remove link.
         var removeElement = document.createElement('div');
         removeElement.textContent =
-            loadTimeData.getString('handlers_remove_link');
+            loadTimeData.getString('handlersRemoveLink');
         removeElement.addEventListener('click', function(e) {
           var value = selectElement ? selectElement.value : 0;
           delegate.removeHandler(value, data.handlers[value]);
