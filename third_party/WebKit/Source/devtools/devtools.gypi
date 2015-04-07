4a8aaac47e5ad7a847394fd9c8179b00a5673270
dgozman@chromium.org <dgozman@chromium.org>
2015-04-07 17:09:05 +0000

    [DevTools] Make close button a web component.
    BUG=none
    
    Review URL: https://codereview.chromium.org/1061343002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193283 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/devtools.gypi
+++ third_party/WebKit/Source/devtools/devtools.gypi
@@ -217,6 +217,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         ],
         'devtools_ui_js_files': [
             'front_end/ui/checkboxTextLabel.css',
+            'front_end/ui/closeButton.css',
             'front_end/ui/colorSwatch.css',
             'front_end/ui/dropTarget.css',
             'front_end/ui/emptyView.css',
--- third_party/WebKit/Source/devtools/front_end/ui/TabbedPane.js
+++ third_party/WebKit/Source/devtools/front_end/ui/TabbedPane.js
@@ -977,7 +977,7 @@ WebInspector.TabbedPaneTab.prototype = {
             this._titleElement = titleElement;
 
         if (this._closeable)
-            tabElement.createChild("div", "tabbed-pane-close-button-gray");
+            tabElement.createChild("div", "tabbed-pane-close-button", "dt-close-button").gray = true;
 
         if (measuring) {
             tabElement.classList.add("measuring");
@@ -1001,7 +1001,7 @@ WebInspector.TabbedPaneTab.prototype = {
     _tabClicked: function(event)
     {
         var middleButton = event.button === 1;
-        var shouldClose = this._closeable && (middleButton || event.target.classList.contains("tabbed-pane-close-button-gray"));
+        var shouldClose = this._closeable && (middleButton || event.target.classList.contains("tabbed-pane-close-button"));
         if (!shouldClose) {
             this._tabbedPane.focus();
             return;
@@ -1015,7 +1015,7 @@ WebInspector.TabbedPaneTab.prototype = {
      */
     _tabMouseDown: function(event)
     {
-        if (event.target.classList.contains("tabbed-pane-close-button-gray") || event.button === 1)
+        if (event.target.classList.contains("tabbed-pane-close-button") || event.button === 1)
             return;
         this._tabbedPane.selectTab(this.id, true);
     },
@@ -1090,7 +1090,7 @@ WebInspector.TabbedPaneTab.prototype = {
      */
     _startTabDragging: function(event)
     {
-        if (event.target.classList.contains("tabbed-pane-close-button-gray"))
+        if (event.target.classList.contains("tabbed-pane-close-button"))
             return false;
         this._dragStartX = event.pageX;
         return true;
