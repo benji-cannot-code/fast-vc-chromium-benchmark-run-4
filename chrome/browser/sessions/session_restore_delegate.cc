ce6d563eb61c5011d5c360b430acf1fcaea8da73
georgesak <georgesak@chromium.org>
2015-04-07 08:08:24 -0700

    Add experiment to disable loading of background tabs during session restore.
    BUG=465815
    
    Review URL: https://codereview.chromium.org/1059343002
    
    Cr-Commit-Position: refs/heads/master@{#324041}

--- chrome/browser/sessions/session_restore_delegate.cc
+++ chrome/browser/sessions/session_restore_delegate.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/sessions/session_restore_delegate.h"
 
+#include "base/metrics/field_trial.h"
 #include "chrome/browser/sessions/session_restore_stats_collector.h"
 #include "chrome/browser/sessions/tab_loader.h"
 
@@ -12,6 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 void SessionRestoreDelegate::RestoreTabs(
     const std::vector<RestoredTab>& tabs,
     const base::TimeTicks& restore_started) {
-  SessionRestoreStatsCollector::TrackTabs(tabs, restore_started);
-  TabLoader::RestoreTabs(tabs, restore_started);
+  // TODO(georgesak): make tests aware of that behavior so that they succeed if
+  // tab loading is disabled.
+  base::FieldTrial* trial =
+      base::FieldTrialList::Find("SessionRestoreBackgroundLoading");
+  bool active_only = true;
+  if (!trial || trial->group_name() != "Disabled") {
+    TabLoader::RestoreTabs(tabs, restore_started);
+    active_only = false;
+  }
+  SessionRestoreStatsCollector::TrackTabs(tabs, restore_started, active_only);
 }
