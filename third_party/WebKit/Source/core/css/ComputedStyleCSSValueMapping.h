c65b052a2801d650123d42fb4fc0f0b86c2bbef3
haraken@chromium.org <haraken@chromium.org>
2015-04-07 07:58:44 +0000

    Oilpan: Remove HeapIndexTrait and EffectiveGCBaseTrait
    - Removes HeapIndexTrait.
    - Removes EffectiveGCBaseTrait.
    - Removes WILL_HAVE_ALL_INSTANCES_ON_SAME_GC_HEAP.
    - And thus fixes two FIXMEs.
    
    Also this CL replaces Heap::allocate<Node>() in Node.h with operator new. The new code is more correct, because Heap::allocate<Node>() uses GCInfoTrait<Node> whereas it should use GCInfoTrait<EventTarget>. The new code correctly uses GCInfoTrait<EventTarget>. (Note: That being said, this difference won't matter in practice because ~EventTarget() and EventTarget::trace() are empty.)
    
    BUG=420515
    
    Review URL: https://codereview.chromium.org/1052773003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193253 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/ComputedStyleCSSValueMapping.h
+++ third_party/WebKit/Source/core/css/ComputedStyleCSSValueMapping.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ComputedStyleCSSValueMapping_h
 
 #include "core/CSSPropertyNames.h"
+#include "core/css/CSSValue.h"
 
 namespace blink {
 
--- third_party/WebKit/Source/core/css/parser/CSSParser.h
+++ third_party/WebKit/Source/core/css/parser/CSSParser.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CSSParser_h
 
 #include "core/CSSPropertyNames.h"
+#include "core/css/CSSValue.h"
 #include "core/css/parser/CSSParserMode.h"
 #include "platform/graphics/Color.h"
 
