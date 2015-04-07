bed8b90991ad8aac949b3e6390a67c466cae737a
dtseng <dtseng@chromium.org>
2015-04-07 10:01:06 -0700

    Revert of Fire an alert event to notify accessibility when a permissions bubble gets shown. (patchset #6 id:100001 of https://codereview.chromium.org/1055883002/)
    Reason for revert:
    Not lg'ed by OWNER as of latest patchset.
    
    Original issue's description:
    > Add an event to notify accessibility when a permissions bubble gets shown.
    >
    > BUG=381338
    > TEST=ctrl+alt+z; navigate to html5demos.com/geo; observe speech is as expected (reads contents of bubble).
    >
    > Committed: https://crrev.com/36318aa45e02cf0b5cf871bf38bbe47f3431cc8d
    > Cr-Commit-Position: refs/heads/master@{#323982}
    
    TBR=felt@chromium.org,markusheintz@chromium.org,dmazzoni@chromium.org,msw@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=381338
    
    Review URL: https://codereview.chromium.org/1066973002
    
    Cr-Commit-Position: refs/heads/master@{#324060}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/output.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/output.js
@@ -196,7 +196,7 @@ Output.RULES = {
       speak: '@describe_slider($value, $name)'
     },
     staticText: {
-      speak: '$value $name'
+      speak: '$value'
     },
     tab: {
       speak: '@describe_tab($name)'
