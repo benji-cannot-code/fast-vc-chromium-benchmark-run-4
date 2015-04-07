9a4d3cd3f938dd29ded1857e2173c1da1e207e48
bokan@chromium.org <bokan@chromium.org>
2015-04-07 23:16:37 +0000

    Make getClientRects and getBoundingClientRects relative to layout viewport.
    These were made relative to the visual viewport when turning on virtual-viewport
    but other client coordinate based APIs were intentionally made relative to the
    layout viewport. This leaves us in an inconsistent state. We've decided to make
    all client coordinates relative to the layout viewport, matching IE's behaviour.
    
    BUG=474540
    
    Review URL: https://codereview.chromium.org/1065973002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193307 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/testing/Internals.cpp
+++ third_party/WebKit/Source/core/testing/Internals.cpp
@@ -2333,6 +2333,11 @@ String Internals::selectedTextForClipboard()
     return frame()->selection().selectedTextForClipboard();
 }
 
+void Internals::setVisualViewportOffset(int x, int y)
+{
+    frame()->host()->pinchViewport().setLocation(FloatPoint(x, y));
+}
+
 ValueIterable<int>::IterationSource* Internals::startIteration(ScriptState*, ExceptionState&)
 {
     return new InternalsIterationSource();
