d9cc628a13591964e55284c8fd67384d72f1d546
enne <enne@chromium.org>
2015-04-07 12:11:01 -0700

    cc: Remove invalidation history
    Now that Blink handles animated gifs, this code is not needed.
    
    BUG=467197
    
    Review URL: https://codereview.chromium.org/1061203002
    
    Cr-Commit-Position: refs/heads/master@{#324084}

--- cc/resources/display_list_recording_source.cc
+++ cc/resources/display_list_recording_source.cc
@@ -124,10 +124,6 @@ bool DisplayListRecordingSource::UpdateAndExpandInvalidation(
   return true;
 }
 
-void DisplayListRecordingSource::DidMoveToNewCompositor() {
-  // No invalidation history to worry about here.
-}
-
 gfx::Size DisplayListRecordingSource::GetSize() const {
   return size_;
 }
