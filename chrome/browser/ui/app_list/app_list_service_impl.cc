9f365408d7432bb3527b85a3140c23469fa01a1b
calamity <calamity@chromium.org>
2015-04-07 00:01:52 -0700

    Don't warmup the app list if it hasn't been used in the past 28 days. [Win]
    This CL adds a pref that tracks when the app list is last launched and
    doesn't warm up the app list on Windows unless the app list has been
    launched in the last 28 days.
    
    BUG=440224
    
    Review URL: https://codereview.chromium.org/1016503006
    
    Cr-Commit-Position: refs/heads/master@{#324018}

--- chrome/browser/ui/app_list/app_list_service_impl.cc
+++ chrome/browser/ui/app_list/app_list_service_impl.cc
@@ -214,6 +214,7 @@ void AppListServiceImpl::RecordAppListLaunch() {
                             prefs::kAppListLaunchCount,
                             &SendAppListLaunch);
   RecordAppListDiscoverability(local_state_, false);
+  RecordAppListLastLaunch();
 }
 
 // static
@@ -221,6 +222,19 @@ void AppListServiceImpl::RecordAppListAppLaunch() {
   RecordDailyEventFrequency(prefs::kLastAppListAppLaunchPing,
                             prefs::kAppListAppLaunchCount,
                             &SendAppListAppLaunch);
+}
+
+// static
+void AppListServiceImpl::RecordAppListLastLaunch() {
+  if (!g_browser_process)
+    return;  // In a unit test.
+
+  PrefService* local_state = g_browser_process->local_state();
+  if (!local_state)
+    return;  // In a unit test.
+
+  local_state->SetInt64(prefs::kAppListLastLaunchTime,
+                        base::Time::Now().ToInternalValue());
 }
 
 // static
--- chrome/browser/ui/app_list/app_list_service_impl.h
+++ chrome/browser/ui/app_list/app_list_service_impl.h
@@ -46,6 +46,7 @@ class AppListServiceImpl : public AppListService,
 
   void RecordAppListLaunch();
   static void RecordAppListAppLaunch();
+  static void RecordAppListLastLaunch();
 
   // AppListService overrides:
   void SetAppListNextPaintCallback(void (*callback)()) override;
