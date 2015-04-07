1df92a4d494e3c4fc1c45f99ee1b8482e0380450
danakj <danakj@chromium.org>
2015-04-07 14:51:04 -0700

    toolbar: Use a PaintRecorder to access the Canvas for painting.
    Don't use the canvas() from the PaintContext so that we can have the
    PaintContext be backed by a DisplayItemList instead in the future.
    Access to the Canvas should be done through a PaintRecorder.
    
    R=pkasting
    BUG=466426
    
    Review URL: https://codereview.chromium.org/1066133002
    
    Cr-Commit-Position: refs/heads/master@{#324128}

--- chrome/browser/ui/views/toolbar/toolbar_action_view.cc
+++ chrome/browser/ui/views/toolbar/toolbar_action_view.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/theme_resources.h"
 #include "ui/accessibility/ax_view_state.h"
 #include "ui/base/resource/resource_bundle.h"
-#include "ui/compositor/paint_context.h"
+#include "ui/compositor/paint_recorder.h"
 #include "ui/events/event.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/image/image_skia_operations.h"
@@ -111,7 +111,8 @@ void ToolbarActionView::ViewHierarchyChanged(
 
 void ToolbarActionView::PaintChildren(const ui::PaintContext& context) {
   View::PaintChildren(context);
-  view_controller_->PaintExtra(context.canvas(), GetLocalBounds(),
+  ui::PaintRecorder recorder(context);
+  view_controller_->PaintExtra(recorder.canvas(), GetLocalBounds(),
                                GetCurrentWebContents());
 }
 
