5437c1a5c7c88e3766a3cd21672296222d9290a4
wfh <wfh@chromium.org>
2015-04-06 19:21:36 -0700

    Create crash dump if a bad IPC message is received.
    BUG=473370
    
    Review URL: https://codereview.chromium.org/1058423002
    
    Cr-Commit-Position: refs/heads/master@{#324003}

--- content/browser/browser_child_process_host_impl.cc
+++ content/browser/browser_child_process_host_impl.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_switches.h"
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/files/file_path.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
@@ -255,6 +256,11 @@ void BrowserChildProcessHostImpl::OnBadMessageReceived(
   }
   LOG(ERROR) << "Terminating child process for bad IPC message of type "
       << message.type();
+
+  // Create a memory dump. This will contain enough stack frames to work out
+  // what the bad message was.
+  base::debug::DumpWithoutCrashing();
+
   child_process_->GetProcess().Terminate(RESULT_CODE_KILLED_BAD_MESSAGE, false);
 }
 
