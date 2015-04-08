5cf8e8bdea1bc86c58eb59ca30280423a26a6304
megjablon <megjablon@chromium.org>
2015-04-08 12:29:45 -0700

    Integration test for exp Chrome-Proxy request option
    Test the experiment Chrome-Proxy directive by verifying that
    a specific test URL only triggers a bypass when the command
    line flag "--data-reduction-proxy-experiment=test" is set.
    
    BUG=452292
    
    Review URL: https://codereview.chromium.org/1044573002
    
    Cr-Commit-Position: refs/heads/master@{#324270}

---
+++ tools/chrome_proxy/integration_tests/chrome_proxy_pagesets/exp_directive.py
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from telemetry.page import page as page_module
+from telemetry.page import page_set as page_set_module
+
+
+class ExpDirectivePage(page_module.Page):
+  """A test page for the experiment Chrome-Proxy directive tests."""
+
+  def __init__(self, url, page_set):
+    super(ExpDirectivePage, self).__init__(url=url, page_set=page_set)
+
+
+class ExpDirectivePageSet(page_set_module.PageSet):
+  """ Chrome proxy test sites """
+
+  def __init__(self):
+    super(ExpDirectivePageSet, self).__init__()
+
+    urls_list = [
+      'http://aws1.mdw.la/exptest/',
+    ]
+
+    for url in urls_list:
+      self.AddUserStory(ExpDirectivePage(url, self))
