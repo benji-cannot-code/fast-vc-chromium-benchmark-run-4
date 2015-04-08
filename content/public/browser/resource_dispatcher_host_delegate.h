78a448ac9541b7f481278e1ae4f9a776e47ba8d4
deepak.m1 <deepak.m1@samsung.com>
2015-04-07 21:47:16 -0700

    Ensuring interception of stream get determined by plugin path before checking mime type.
    Earlier we are checking whether a extension with url that have mime type
    should be intercepted as stream by checking plugin availablity.
    Now we are passing plugin path and then checking whether their is
    MimehandlerView Plugin available with that path and then we are allowing
    intercepted as stream. So now interception is not depends upon mimetype.
    
    BUG=443466
    
    Review URL: https://codereview.chromium.org/953793003
    
    Cr-Commit-Position: refs/heads/master@{#324187}

--- content/public/browser/resource_dispatcher_host_delegate.h
+++ content/public/browser/resource_dispatcher_host_delegate.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
+#include "base/files/file_path.h"
 #include "base/memory/scoped_ptr.h"
 #include "content/common/content_export.h"
 #include "content/public/common/resource_type.h"
@@ -93,10 +94,12 @@ class CONTENT_EXPORT ResourceDispatcherHostDelegate {
   // If the stream will be rendered in a BrowserPlugin, |payload| will contain
   // the data that should be given to the old ResourceHandler to forward to the
   // renderer process.
-  virtual bool ShouldInterceptResourceAsStream(net::URLRequest* request,
-                                               const std::string& mime_type,
-                                               GURL* origin,
-                                               std::string* payload);
+  virtual bool ShouldInterceptResourceAsStream(
+      net::URLRequest* request,
+      const base::FilePath& plugin_path,
+      const std::string& mime_type,
+      GURL* origin,
+      std::string* payload);
 
   // Informs the delegate that a Stream was created. The Stream can be read from
   // the blob URL of the Stream, but can only be read once.
