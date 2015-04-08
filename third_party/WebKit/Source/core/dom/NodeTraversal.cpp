11c24eecafa399419779efd2128b81b156edcbc5
yosin@chromium.org <yosin@chromium.org>
2015-04-08 07:55:02 +0000

    Introduce commonAncestor() and countChildren() to NodeTraversal class
    This patch introduces |commonAncestor()| and |countChildren()| to
    |NodeTraversal| as |ComposedTreeTraversal| for implementing composed tree
    version of |Position| and |PositionIterator| classes.
    
    This patch is a preparation of templatize |Position| class:
    http://crrev.com/1071473002
    
    This patch is result of collaboration work with hajimehoshi@chromium.org
    for selection of web components. After this patch, we are going to
    implement another strategies for the composed tree. We've already
    prepared that at http://crrev.com/1058203002.
    
    BUG=275851
    TEST=n/a; No behavior changes.
    
    Review URL: https://codereview.chromium.org/1068153002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193333 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/NodeTraversal.cpp
+++ third_party/WebKit/Source/core/dom/NodeTraversal.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/NodeTraversal.h"
 
 #include "core/dom/ContainerNode.h"
+#include "core/dom/Range.h"
 
 namespace blink {
 
@@ -172,6 +173,11 @@ Node* NodeTraversal::previousPostOrder(const Node& current, const Node* stayWith
     if (current.previousSibling())
         return current.previousSibling();
     return previousAncestorSiblingPostOrder(current, stayWithin);
+}
+
+Node* NodeTraversal::commonAncestor(const Node& nodeA, const Node& nodeB)
+{
+    return Range::commonAncestorContainer(&nodeA, &nodeB);
 }
 
 } // namespace blink
