0591f73228de34e72d150701a24e2737a91f440a
alexclarke <alexclarke@chromium.org>
2015-04-08 05:08:57 -0700

    Remove duplicate MoveReadyDelayedTasksToIncomingQueueLocked
    TaskQueue::UpdateWorkQueue had a duplicate call to
    MoveReadyDelayedTasksToIncomingQueueLocked.  That's not intended but
    benign.  This patch removes it.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1057993002
    
    Cr-Commit-Position: refs/heads/master@{#324217}

--- content/child/scheduler/task_queue_manager.cc
+++ content/child/scheduler/task_queue_manager.cc
@@ -304,7 +304,6 @@ bool TaskQueue::UpdateWorkQueue(LazyNow* lazy_now,
 
   {
     base::AutoLock lock(lock_);
-    MoveReadyDelayedTasksToIncomingQueueLocked(lazy_now);
     if (!ShouldAutoPumpQueueLocked(previous_task))
       return false;
     MoveReadyDelayedTasksToIncomingQueueLocked(lazy_now);
