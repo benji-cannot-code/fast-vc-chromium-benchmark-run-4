2ce311db940c72ccbd15ae9dbade06d5e50e252f
wittman <wittman@chromium.org>
2015-04-07 19:20:35 -0700

    Enable startup profiling by Win x64 stack sampling profiler
    Collects stack samples at 10 Hz for 30 seconds at startup. Uploading of
    samples to UMA is controlled by a Finch experiment.
    
    This change moves to a callback-based mechanism for communicating
    completed profiles to the metrics provider to avoid profiles lingering
    in memory when reporting is disabled.
    
    BUG=464929
    
    Review URL: https://codereview.chromium.org/1029653002
    
    Cr-Commit-Position: refs/heads/master@{#324175}

--- chrome/browser/chrome_browser_main.h
+++ chrome/browser/chrome_browser_main.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/metrics/field_trial.h"
+#include "base/profiler/stack_sampling_profiler.h"
 #include "base/tracked_objects.h"
 #include "chrome/browser/chrome_browser_field_trials.h"
 #include "chrome/browser/chrome_process_singleton.h"
@@ -110,10 +111,8 @@ class ChromeBrowserMainParts : public content::BrowserMainParts {
   // Record time from process startup to present time in an UMA histogram.
   void RecordBrowserStartupTime();
 
-  // Records a time value to an UMA histogram in the context of the
-  // PreReadExperiment field-trial. This also reports to the appropriate
-  // sub-histogram (_PreRead(Enabled|Disabled)).
-  void RecordPreReadExperimentTime(const char* name, base::TimeDelta time);
+  // Create parameters for sampling stacks during startup.
+  static base::StackSamplingProfiler::SamplingParams GetStartupSamplingParams();
 
   // Methods for Main Message Loop -------------------------------------------
 
@@ -148,6 +147,10 @@ class ChromeBrowserMainParts : public content::BrowserMainParts {
   // Vector of additional ChromeBrowserMainExtraParts.
   // Parts are deleted in the inverse order they are added.
   std::vector<ChromeBrowserMainExtraParts*> chrome_extra_parts_;
+
+  // A profiler that periodically samples stack traces. Used to sample startup
+  // behavior.
+  base::StackSamplingProfiler sampling_profiler_;
 
   // Members initialized after / released before main_message_loop_ ------------
 
