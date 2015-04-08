031b7c59c7b5841d17ce08807e79df37811760ed
senorblanco <senorblanco@chromium.org>
2015-04-08 09:59:02 -0700

    Add a "Max. MSAA samples" entry to chrome://gpu.
    BUG=474727
    
    Review URL: https://codereview.chromium.org/1063233002
    
    Cr-Commit-Position: refs/heads/master@{#324236}

--- gpu/config/gpu_info.cc
+++ gpu/config/gpu_info.cc
@@ -80,6 +80,7 @@ void GPUInfo::EnumerateFields(Enumerator* enumerator) const {
     std::string driver_date;
     std::string pixel_shader_version;
     std::string vertex_shader_version;
+    std::string max_msaa_samples;
     std::string machine_model_name;
     std::string machine_model_version;
     std::string gl_version_string;
@@ -136,6 +137,7 @@ void GPUInfo::EnumerateFields(Enumerator* enumerator) const {
   enumerator->AddString("driverDate", driver_date);
   enumerator->AddString("pixelShaderVersion", pixel_shader_version);
   enumerator->AddString("vertexShaderVersion", vertex_shader_version);
+  enumerator->AddString("maxMsaaSamples", max_msaa_samples);
   enumerator->AddString("glVersion", gl_version);
   enumerator->AddString("glVendor", gl_vendor);
   enumerator->AddString("glRenderer", gl_renderer);
