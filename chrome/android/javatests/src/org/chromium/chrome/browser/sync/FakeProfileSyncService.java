5d6134b66483ab7d47e27ff00437c66e40665f92
maxbogue <maxbogue@chromium.org>
2015-04-08 17:26:42 -0700

    [Sync] PassphraseActivity: unregister for sync events when paused.
    TEST=For a manual test to exercise this code, see comment #2 in the bug.
    BUG=469890
    
    Review URL: https://codereview.chromium.org/1025403002
    
    Cr-Commit-Position: refs/heads/master@{#324316}

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/FakeProfileSyncService.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.sync;
+
+import android.content.Context;
+
+/**
+ * Fake some ProfileSyncService methods for testing.
+ *
+ * Only what has been needed for tests so far has been faked.
+ */
+public class FakeProfileSyncService extends ProfileSyncService {
+
+    private boolean mBackendInitialized;
+
+    public FakeProfileSyncService(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isSyncInitialized() {
+        return mBackendInitialized;
+    }
+
+    public void setSyncInitialized(boolean syncInitialized) {
+        mBackendInitialized = syncInitialized;
+    }
+
+    @Override
+    public boolean isUsingSecondaryPassphrase() {
+        return true;
+    }
+}
