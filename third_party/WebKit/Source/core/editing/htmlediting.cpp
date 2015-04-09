2b1cb2e006d81d8ad032bcdba95b52df36c40e2f
yosin@chromium.org <yosin@chromium.org>
2015-04-09 07:12:50 +0000

    Re-factor Range::compareBoundaryPoints to comparePositionsAlgorithm<Strategy>
    This patch move implementation of |Range::compareBoundaryPoints()| to
    |comparePositionsAlgorithm<Strategy>| to utilize algorithm to both DOM tree and
    composed tree for avoiding source code duplication.
    
    This patch is a preparation of implementing position in composed tree for
    selection for web components.
    
    This patch also gets rid of redundant include files in "htmlediting.cpp"
    
    This patch is result of collaboration work with hajimehoshi@chromium.org
    for selection of web components. After this patch, we are going to
    implement another strategies for the composed tree. We've already
    prepared that at http://crrev.com/1058203002.
    
    BUG=275851
    TEST=n/a; No behavior changes.
    
    Review URL: https://codereview.chromium.org/1067313002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193422 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/htmlediting.cpp
+++ third_party/WebKit/Source/core/editing/htmlediting.cpp
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/editing/htmlediting.h"
 
-#include "bindings/core/v8/ExceptionState.h"
-#include "bindings/core/v8/ExceptionStatePlaceholder.h"
 #include "core/HTMLElementFactory.h"
 #include "core/HTMLNames.h"
 #include "core/dom/Document.h"
@@ -37,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Range.h"
 #include "core/dom/Text.h"
 #include "core/dom/shadow/ShadowRoot.h"
+#include "core/editing/EditingStrategy.h"
 #include "core/editing/Editor.h"
 #include "core/editing/HTMLInterchange.h"
 #include "core/editing/PlainTextRange.h"
@@ -100,7 +99,7 @@ int comparePositions(const Position& a, const Position& b)
             bias = 1;
     }
 
-    int result = Range::compareBoundaryPoints(nodeA, offsetA, nodeB, offsetB, IGNORE_EXCEPTION);
+    int result = EditingStrategy::comparePositions(nodeA, offsetA, nodeB, offsetB);
     return result ? result : bias;
 }
 
