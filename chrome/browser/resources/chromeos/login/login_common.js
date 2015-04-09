b24742747e5c5ed3f0f227bf4ea4bd49e2837ead
resetswitch <resetswitch@chromium.org>
2015-04-09 13:16:48 -0700

    Enterprise login for test automation.
    This enables ChromeOS enterprise enrollment test automation in the ChromiumOS codebase:
    https://chromium-review.googlesource.com/#/c/264424/
    
    BUG=474707
    
    Review URL: https://codereview.chromium.org/1066173002
    
    Cr-Commit-Position: refs/heads/master@{#324494}

--- chrome/browser/resources/chromeos/login/login_common.js
+++ chrome/browser/resources/chromeos/login/login_common.js
@@ -324,6 +324,13 @@ cr.define('cr.ui', function() {
   };
 
   /**
+   * Begin enterprise enrollment for telemetry.
+   */
+  Oobe.switchToEnterpriseEnrollmentForTesting = function() {
+    chrome.send('toggleEnrollmentScreen');
+  };
+
+  /**
    * Finish enterprise enrollment for telemetry.
    */
   Oobe.enterpriseEnrollmentDone = function() {
