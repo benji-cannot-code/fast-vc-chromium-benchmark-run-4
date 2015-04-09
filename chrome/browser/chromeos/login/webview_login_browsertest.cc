33ed1be547ac66a0cea1fefd60beca72f9c84a87
xiyuan <xiyuan@chromium.org>
2015-04-08 20:42:27 -0700

    cros: Fix WebviewLoginTest.Basic multiple login attempts.
    ShowSigninScreenForCreds is used to automatically fill-in sign-in
    forms and submit it. WebviewLoginTest.Basic does that manually
    thus should not call it. Otherwise, /_/embedded/signin/challenge
    is called multiple times with wrong email (empty) and could trigger
    FakeGaia failures.
    
    BUG=472095
    
    Review URL: https://codereview.chromium.org/1069423004
    
    Cr-Commit-Position: refs/heads/master@{#324353}

--- chrome/browser/chromeos/login/webview_login_browsertest.cc
+++ chrome/browser/chromeos/login/webview_login_browsertest.cc
@@ -47,8 +47,6 @@ class WebviewLoginTest : public OobeBaseTest {
           "});"));
 
     content::DOMMessageQueue message_queue;
-    GetLoginDisplay()->ShowSigninScreenForCreds("", "");
-
     std::string message;
     ASSERT_TRUE(message_queue.WaitForMessage(&message));
     EXPECT_EQ("\"GaiaReady\"", message);
