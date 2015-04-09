5124a0cfdc268963ad78e0d1543e232e7e65203e
nsatragno <nsatragno@chromium.org>
2015-04-09 15:30:51 -0700

    Refactor GestureNavigation to eliminate code redundancy
    Right now, Gesture Navigation has two very distinct phases: the initial
    one, where the web contents window receives events and is moved, and the
    follow up navigation, where the page still hasn't loaded and the user
    scrolls on the overlay. The meat of this patch is to unify as much of
    the logic as possible without sacrificing features. A new class is
    introduced, OverscrollWindowAnimation, and the responsibility of the
    existing OverscrollNavigationOverlay is steered into performing the
    actual navigation.
    
    Reland this patch.
    
    TEST=Overscroll*
    BUG=467692, 464532, 420121
    Committed: https://crrev.com/5384f002f839439ef666ed9688069e42ca5ccdca
    Cr-Commit-Position: refs/heads/master@{#324275}
    
    Review URL: https://codereview.chromium.org/1072123002
    
    Cr-Commit-Position: refs/heads/master@{#324526}

--- content/browser/renderer_host/overscroll_controller_delegate.h
+++ content/browser/renderer_host/overscroll_controller_delegate.h
@@ -9,13 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "content/browser/renderer_host/overscroll_controller.h"
+#include "content/common/content_export.h"
 #include "ui/gfx/geometry/rect.h"
 
 namespace content {
 
 // The delegate receives overscroll gesture updates from the controller and
 // should perform appropriate actions.
-class OverscrollControllerDelegate {
+class CONTENT_EXPORT OverscrollControllerDelegate {
  public:
   OverscrollControllerDelegate() {}
   virtual ~OverscrollControllerDelegate() {}
