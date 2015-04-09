73bcb96347c0ddb0fd2a9f7b8fcd0368f4e64609
erg <erg@chromium.org>
2015-04-09 01:56:56 -0700

    Part 7 of 8: Move blink images to their own grd/pak file.
    This moves the image IDRs used on the chrome side back from IDR2_ to the
    IDR_ versions. (The next and final patch will remove the IDR2_s from
    webkit.)
    
    BUG=471609, 467176
    
    Review URL: https://codereview.chromium.org/1073523004
    
    Cr-Commit-Position: refs/heads/master@{#324401}

--- content/shell/common/shell_content_client.cc
+++ content/shell/common/shell_content_client.cc
@@ -66,7 +66,7 @@ base::StringPiece ShellContentClient::GetDataResource(
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kRunLayoutTest)) {
     switch (resource_id) {
-      case IDR2_BROKENIMAGE:
+      case IDR_BROKENIMAGE:
 #if defined(OS_MACOSX)
         resource_id = IDR_CONTENT_SHELL_MISSING_IMAGE_PNG;
 #else
@@ -74,7 +74,7 @@ base::StringPiece ShellContentClient::GetDataResource(
 #endif
         break;
 
-      case IDR2_TEXTAREA_RESIZER:
+      case IDR_TEXTAREA_RESIZER:
         resource_id = IDR_CONTENT_SHELL_TEXT_AREA_RESIZE_CORNER_PNG;
         break;
     }
