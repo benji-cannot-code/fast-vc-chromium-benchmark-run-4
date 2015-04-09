42bf5de4311950363fcb292f7c45a9f49d3a452f
cblume <cblume@chromium.org>
2015-04-08 19:50:53 -0700

    Adding tests for diagonal scrolling in telemetry.
    Adding tests for diagonal scrolling to telemetry.
    
    BUG=466867
    
    Review URL: https://codereview.chromium.org/999243003
    
    Cr-Commit-Position: refs/heads/master@{#324346}

--- tools/telemetry/telemetry/page/action_runner.py
+++ tools/telemetry/telemetry/page/action_runner.py
@@ -397,7 +397,7 @@ class ActionRunner(object):
           gesture, as a ratio of the visible bounding rectangle for
           document.body.
       direction: The direction of scroll, either 'left', 'right',
-          'up', or 'down'
+          'up', 'down', 'upleft', 'upright', 'downleft', or 'downright'
       distance: The distance to scroll (in pixel).
       distance_expr: A JavaScript expression (as string) that can be
           evaluated to compute scroll distance. Example:
@@ -437,7 +437,7 @@ class ActionRunner(object):
           gesture, as a ratio of the visible bounding rectangle for
           the element.
       direction: The direction of scroll, either 'left', 'right',
-          'up', or 'down'
+          'up', 'down', 'upleft', 'upright', 'downleft', or 'downright'
       distance: The distance to scroll (in pixel).
       distance_expr: A JavaScript expression (as string) that can be
           evaluated to compute scroll distance. Example:
@@ -471,7 +471,7 @@ class ActionRunner(object):
           gesture, as a ratio of the visible bounding rectangle for
           document.body.
       direction: The direction of scroll, either 'left', 'right',
-          'up', or 'down'
+          'up', 'down', 'upleft', 'upright', 'downleft', or 'downright'
       distance: The distance to scroll (in pixel).
       overscroll: The number of additional pixels to scroll back, in
           addition to the givendistance.
@@ -509,7 +509,7 @@ class ActionRunner(object):
           gesture, as a ratio of the visible bounding rectangle for
           document.body.
       direction: The direction of scroll, either 'left', 'right',
-          'up', or 'down'
+          'up', 'down', 'upleft', 'upright', 'downleft', or 'downright'
       distance: The distance to scroll (in pixel).
       overscroll: The number of additional pixels to scroll back, in
           addition to the givendistance.
