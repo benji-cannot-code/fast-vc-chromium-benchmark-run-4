cc3698e253f0c4a8457fde8d34ede1ff3a3dff7c
torne <torne@chromium.org>
2015-04-09 06:25:11 -0700

    android_webview: stop leaking Contexts.
    We were leaking activity Contexts in ResourcesContextWrapperFactory's
    cache of wrapped contexts, because WeakHashMap only references its
    *keys* weakly, and the ContextWrapper objects used as values in the
    hashmap have a strong reference to the Context being used as a key, so
    nothing was ever removed from the map.
    
    Fix this by deleting the cache entirely, as it's not really necessary;
    the number of wrapped contexts we create is already bounded to two per
    WebView (one for the activity context and one for the application
    context), and the objects are very small, so it's not worth the
    complexity of trying to avoid lifetime issues while reusing them.
    
    We do still need to ensure that WebViewChromiumFactoryProvider always
    uses the same wrapper for the application context, though, since in some
    initialisation paths we set the application context more than once and
    the underlying code only permits that if the same context object is used
    each time.
    
    BUG=473146
    
    Review URL: https://codereview.chromium.org/1078693003
    
    Cr-Commit-Position: refs/heads/master@{#324425}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
@@ -80,6 +80,7 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
     private WebStorageAdapter mWebStorage;
     private WebViewDatabaseAdapter mWebViewDatabase;
     private AwDevToolsServer mDevToolsServer;
+    private Context mWrappedAppContext;
 
     private ArrayList<WeakReference<WebViewChromium>> mWebViewsToStart =
             new ArrayList<WeakReference<WebViewChromium>>();
@@ -292,7 +293,11 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
     }
 
     private Context getWrappedCurrentApplicationContext() {
-        return ResourcesContextWrapperFactory.get(mWebViewDelegate.getApplication());
+        if (mWrappedAppContext == null) {
+            mWrappedAppContext = ResourcesContextWrapperFactory.get(
+                    mWebViewDelegate.getApplication());
+        }
+        return mWrappedAppContext;
     }
 
     AwBrowserContext getBrowserContext() {
