130f90c9920effac14c6a945caaf79638b1d2c73
junov@chromium.org <junov@chromium.org>
2015-04-09 00:02:45 +0000

    Fix memory leak in display list canvas overdraw optimization.
    This change simply adds a missing unref() call in the code
    that handles overdraw optimization. The memory leak would
    manifest itself as a race condition in animation loops that
    use setTimeout or setInterval instead of
    requestAnimationFrame: If a new frame starts before the
    previous frame is consumed by the compositor, the previous
    frame is to be droped. But in this case, the frame was
    being leaked instead of dropped.
    
    BUG=474510
    
    Review URL: https://codereview.chromium.org/1068413002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193398 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.cpp
+++ third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.cpp
@@ -148,7 +148,7 @@ void RecordingImageBufferSurface::willOverwriteCanvas()
     m_previousFramePixelCount = 0;
     if (m_didRecordDrawCommandsInCurrentFrame) {
         // Discard previous draw commands
-        m_currentFrame->endRecording();
+        adoptRef(m_currentFrame->endRecording());
         initializeCurrentFrame();
     }
 }
