2704654669f1471caba78213462115cab3ae84d6
fmeawad <fmeawad@chromium.org>
2015-04-08 21:15:43 -0700

    Fix docstring in battery_utils
    BUG=474701
    
    Review URL: https://codereview.chromium.org/1078523002
    
    Cr-Commit-Position: refs/heads/master@{#324367}

--- build/android/pylib/device/battery_utils.py
+++ build/android/pylib/device/battery_utils.py
@@ -201,7 +201,6 @@ class BatteryUtils(object):
     Raises:
       device_errors.CommandFailedError: If method of disabling charging cannot
         be determined.
-      device_errors.DeviceVersionError: If device is not L or higher.
     """
     if 'charging_config' not in self._cache:
       for c in _CONTROL_CHARGING_COMMANDS:
@@ -272,6 +271,9 @@ class BatteryUtils(object):
     Args:
       timeout: timeout in seconds
       retries: number of retries
+
+    Raises:
+      device_errors.DeviceVersionError: If device is not L or higher.
     """
     def battery_updates_enabled():
       return self.GetCharging() is True
