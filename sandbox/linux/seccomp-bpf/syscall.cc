0f24281a2a6dc6800b76a4a17b0f6142ff9d1274
hidehiko <hidehiko@chromium.org>
2015-04-08 21:28:32 -0700

    Remove #include <asm/unistd.h> from linux_syscall_ranges.h and syscall.cc.
    The header is included for __NR_SYSCALL_BASE and __ARM_NR_BASE.
    Some toolchain (in particular, PNaCl toolchain) does not provide
    asm/unistd.h (so that neither __NR_SYSCALL_BASE nor __ARM_NR_BASE
    is provided).
    This CL removes the dependency to asm/unistd.h from linux_syscall_ranges.h,
    instead define the values if necessary.
    
    Also, this CL removes the include from syscall.cc, too, which is
    unused.
    
    TEST=Ran bots.
    BUG=358465
    
    Review URL: https://codereview.chromium.org/1071653002
    
    Cr-Commit-Position: refs/heads/master@{#324371}

--- sandbox/linux/seccomp-bpf/syscall.cc
+++ sandbox/linux/seccomp-bpf/syscall.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "sandbox/linux/seccomp-bpf/syscall.h"
 
-#include <asm/unistd.h>
 #include <errno.h>
 #include <stdint.h>
 
