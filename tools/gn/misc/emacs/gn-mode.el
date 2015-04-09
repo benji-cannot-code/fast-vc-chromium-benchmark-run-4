107cec3e9d8be12a451544decf245970eb518ec2
erg <erg@chromium.org>
2015-04-08 18:08:29 -0700

    gn-mode: Get fill-paragraph working correctly.
    Define a fill-paragraph-function which only fills in comments instead of
    everywhere. Previously, we would fill in other contexts, like lists.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1071733002
    
    Cr-Commit-Position: refs/heads/master@{#324328}

--- tools/gn/misc/emacs/gn-mode.el
+++ tools/gn/misc/emacs/gn-mode.el
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 ;; - We syntax highlight builtin actions, but don't highlight instantiations of
 ;;   templates. Should we?
-;; - `fill-paragraph' works for comments, but when pointed at code, breaks
-;;   spectacularly.
 
 
 
@@ -119,6 +117,14 @@ variable name or the '{{' and '}}' which surround it."
      (and (not (smie-rule-bolp)) (smie-rule-prev-p "else")
           (smie-rule-parent)))))
 
+(defun gn-fill-paragraph (&optional justify)
+  "We only fill inside of comments in GN mode."
+  (interactive "P")
+  (or (fill-comment-paragraph justify)
+      ;; Never return nil; `fill-paragraph' will perform its default behavior
+      ;; if we do.
+      t))
+
 ;;;###autoload
 (define-derived-mode gn-mode prog-mode "GN"
   "Major mode for editing gn (Generate Ninja)."
@@ -127,6 +133,8 @@ variable name or the '{{' and '}}' which surround it."
   (setq-local comment-use-syntax t)
   (setq-local comment-start "#")
   (setq-local comment-end "")
+
+  (setq-local fill-paragraph-function 'gn-fill-paragraph)
 
   (setq-local font-lock-defaults '(gn-font-lock-keywords))
 
