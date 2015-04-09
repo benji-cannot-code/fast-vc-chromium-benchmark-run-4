c5cabee480ffa663479353a4cc3af026af3b3da9
noel@chromium.org <noel@chromium.org>
2015-04-09 19:09:14 +0000

    Order decoder signature matching by rank
    Decoder creation order is by rank (r193361). Order signature matching
    by rank too for code consistency. No behavioral change, no new tests.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1071873002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193467 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/image-decoders/ImageDecoder.cpp
+++ third_party/WebKit/Source/platform/image-decoders/ImageDecoder.cpp
@@ -48,9 +48,9 @@ static unsigned copyFromSharedBuffer(char* buffer, unsigned bufferLength, const 
     return bytesExtracted;
 }
 
-inline bool matchesGIFSignature(char* contents)
+inline bool matchesJPEGSignature(char* contents)
 {
-    return !memcmp(contents, "GIF87a", 6) || !memcmp(contents, "GIF89a", 6);
+    return !memcmp(contents, "\xFF\xD8\xFF", 3);
 }
 
 inline bool matchesPNGSignature(char* contents)
@@ -58,19 +58,14 @@ inline bool matchesPNGSignature(char* contents)
     return !memcmp(contents, "\x89\x50\x4E\x47\x0D\x0A\x1A\x0A", 8);
 }
 
-inline bool matchesJPEGSignature(char* contents)
+inline bool matchesGIFSignature(char* contents)
 {
-    return !memcmp(contents, "\xFF\xD8\xFF", 3);
+    return !memcmp(contents, "GIF87a", 6) || !memcmp(contents, "GIF89a", 6);
 }
 
 inline bool matchesWebPSignature(char* contents)
 {
     return !memcmp(contents, "RIFF", 4) && !memcmp(contents + 8, "WEBPVP", 6);
-}
-
-inline bool matchesBMPSignature(char* contents)
-{
-    return !memcmp(contents, "BM", 2);
 }
 
 inline bool matchesICOSignature(char* contents)
@@ -81,6 +76,11 @@ inline bool matchesICOSignature(char* contents)
 inline bool matchesCURSignature(char* contents)
 {
     return !memcmp(contents, "\x00\x00\x02\x00", 4);
+}
+
+inline bool matchesBMPSignature(char* contents)
+{
+    return !memcmp(contents, "BM", 2);
 }
 
 PassOwnPtr<ImageDecoder> ImageDecoder::create(const SharedBuffer& data, ImageSource::AlphaOption alphaOption, ImageSource::GammaAndColorProfileOption gammaAndColorProfileOption)
