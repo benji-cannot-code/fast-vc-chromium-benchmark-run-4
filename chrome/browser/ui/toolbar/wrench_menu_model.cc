de7ead642634d8128974c967587281f45149be51
wjmaclean <wjmaclean@chromium.org>
2015-04-09 14:52:18 -0700

    Speculative fix for crash in WrenchMenuModel::UpdateZoomControls().
    In WrenchMenuModel::UpdateZoomControls() there is the possibility of
    dereferencing a null TabStripModel* which leads to a crash as described
    in the associated bug. This CL implements a check to avoid this.
    
    BUG=450796
    
    Review URL: https://codereview.chromium.org/1077683002
    
    Cr-Commit-Position: refs/heads/master@{#324513}

--- chrome/browser/ui/toolbar/wrench_menu_model.cc
+++ chrome/browser/ui/toolbar/wrench_menu_model.cc
@@ -1058,7 +1058,8 @@ void WrenchMenuModel::CreateZoomMenu() {
 
 void WrenchMenuModel::UpdateZoomControls() {
   int zoom_percent = 100;
-  if (browser_->tab_strip_model()->GetActiveWebContents()) {
+  if (browser_->tab_strip_model() &&
+      browser_->tab_strip_model()->GetActiveWebContents()) {
     zoom_percent = ui_zoom::ZoomController::FromWebContents(
                        browser_->tab_strip_model()->GetActiveWebContents())
                        ->GetZoomPercent();
