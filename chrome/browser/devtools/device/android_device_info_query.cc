d0949175876c7c40d23435324ad4e70b6f6b4efc
kerz <kerz@chromium.org>
2015-04-09 11:56:24 -0700

    Clean up package names.
    BUG=475197
    
    Review URL: https://codereview.chromium.org/1068163006
    
    Cr-Commit-Position: refs/heads/master@{#324482}

--- chrome/browser/devtools/device/android_device_info_query.cc
+++ chrome/browser/devtools/device/android_device_info_query.cc
@@ -57,7 +57,7 @@ const BrowserDescriptor kBrowserDescriptors[] = {
     "Chrome Canary"
   },
   {
-    "com.google.android.apps.chrome_dev",
+    "com.chrome.dev",
     kChromeDefaultSocket,
     "Chrome Dev"
   },
