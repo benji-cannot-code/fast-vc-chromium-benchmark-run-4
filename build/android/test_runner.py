81f2ac6cb202a0fea4ef34ab264a18ea6bda4894
mikecase <mikecase@chromium.org>
2015-04-08 19:28:57 -0700

    Add support for json-results-file option for Junit tests.
    The recipes need the ability to parse the test results. Adding
    support for the json-results-file option so we can tell what junit
    tests failed in the infra recipes.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1062403004
    
    Cr-Commit-Position: refs/heads/master@{#324343}

--- build/android/test_runner.py
+++ build/android/test_runner.py
@@ -776,6 +776,9 @@ def _RunJUnitTests(args):
       test_type='JUnit',
       test_package=args.test_suite)
 
+  if args.json_results_file:
+    json_results.GenerateJsonResultsFile(results, args.json_results_file)
+
   return exit_code
 
 
