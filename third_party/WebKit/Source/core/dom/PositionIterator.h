d3d84fb3d0a045e77fdef1aee3863ff2911940fa
yosin@chromium.org <yosin@chromium.org>
2015-04-09 09:44:36 +0000

    Re-factor Position class to simplify PositionItertor implementation and templatization
    This patch moves functions into |Position| class for simplify
    implementation of |PositionItertor| class by removing
    |PositionIteratorStrategy| class, which has number of wrapping functions, with
    pairing functions, e.g. beforeNode/afterNode, firstXXX/lastXXX, for ease of
    reading.
    
    This patch is a preparation of |Position| templatization of
    http://crrev.com/1071473002
    
    This patch is result of collaboration work with hajimehoshi@chromium.org
    for selection of web components. After this patch, we are going to
    implement another strategies for the composed tree. We've already
    prepared that at http://crrev.com/1058203002.
    
    BUG=275851
    TEST=n/a; No behavior changes.
    
    Review URL: https://codereview.chromium.org/1063373002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193437 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/PositionIterator.h
+++ third_party/WebKit/Source/core/dom/PositionIterator.h
@@ -35,8 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class Position;
-
 // A Position iterator with constant-time
 // increment, decrement, and several predicates on the Position it is at.
 // Conversion to/from Position is O(n) in the offset.
@@ -64,33 +62,16 @@ public:
     bool isCandidate() const;
 
 private:
-    PositionIteratorAlgorithm(Node*, int);
+    PositionIteratorAlgorithm(Node* anchorNode, int offsetInAnchorNode);
 
     RawPtrWillBeMember<Node> m_anchorNode;
     RawPtrWillBeMember<Node> m_nodeAfterPositionInAnchor; // If this is non-null, Strategy::parent(*m_nodeAfterPositionInAnchor) == m_anchorNode;
     int m_offsetInAnchor;
 };
 
-// Node traversal for |PositionIteratorAlgorithm|
-class PositionIteratorStrategy : public EditingStrategy {
-public:
-    static bool atEditingBoundary(const PositionType&);
-    static PositionType createLegacyEditingPosition(Node*, int);
-    static int editingOffset(const PositionType&);
-    static bool editingIgnoresContent(Node*);
-    static bool inRenderedText(const PositionType&);
-    static int lastOffsetForEditing(Node*);
-    static Position lastPositionInOrAfterNode(Node*);
-    static bool nodeIsUserSelectNone(Node*);
-    static PositionType positionBeforeNode(Node*);
-    static PositionType positionInParentBeforeNode(Node*);
-    static int uncheckedNextOffset(const Node*, int);
-    static int uncheckedPreviousOffset(const Node*, int);
-};
+extern template class PositionIteratorAlgorithm<EditingStrategy>;
 
-extern template class PositionIteratorAlgorithm<PositionIteratorStrategy>;
-
-using PositionIterator = PositionIteratorAlgorithm<PositionIteratorStrategy>;
+using PositionIterator = PositionIteratorAlgorithm<EditingStrategy>;
 
 } // namespace blink
 
