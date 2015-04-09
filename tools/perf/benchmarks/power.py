b879712a28583b26d1ca6e75c0f57c7d4bce9f0e
nednguyen <nednguyen@google.com>
2015-04-09 15:53:42 -0700

    Revert of Disable benchmarks failing due to AdbShellCommandFailedError. (patchset #1 id:1 of https://codereview.chromium.org/1069223002/)
    Reason for revert:
    Revert since fix has been landed in BUG=474696
    
    BUG=475510
    
    Original issue's description:
    > Disable benchmarks failing due to AdbShellCommandFailedError.
    >
    > BUG=474696
    >
    > Committed: https://crrev.com/9228e12295ebabcd45d0a0d103a35a69db0c6919
    > Cr-Commit-Position: refs/heads/master@{#324239}
    
    TBR=dtu@chromium.org,aiolos@chromium.org
    BUG=474696
    
    Review URL: https://codereview.chromium.org/1055083003
    
    Cr-Commit-Position: refs/heads/master@{#324529}

--- tools/perf/benchmarks/power.py
+++ tools/perf/benchmarks/power.py
@@ -9,7 +9,7 @@ from measurements import power
 import page_sets
 
 
-@benchmark.Disabled('android') # crbug.com/474696
+@benchmark.Enabled('android')
 class PowerAndroidAcceptance(benchmark.Benchmark):
   """Android power acceptance test."""
   test = power.Power
@@ -20,7 +20,7 @@ class PowerAndroidAcceptance(benchmark.Benchmark):
 
 
 
-@benchmark.Disabled('android') # crbug.com/474696
+@benchmark.Enabled('android')
 class PowerTypical10Mobile(benchmark.Benchmark):
   """Android typical 10 mobile power test."""
   test = power.Power
