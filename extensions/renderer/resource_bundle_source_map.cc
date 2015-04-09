d443223678d6cd14d1edc2e56277eff2272e736d
kalman <kalman@chromium.org>
2015-04-09 10:57:07 -0700

    Check before trying to convert an empty StringPiece to a v8::String.
    It would be ideal to CHECK this because it shouldn't be possible for a resource
    that exists to be empty. However, given we've seen odd resource corruption
    happening, and given that it's a rare crash, be conservative and only DCHECK.
    
    BUG=474789
    R=adamk@chromium.org
    
    Review URL: https://codereview.chromium.org/1071753002
    
    Cr-Commit-Position: refs/heads/master@{#324471}

--- extensions/renderer/resource_bundle_source_map.cc
+++ extensions/renderer/resource_bundle_source_map.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/renderer/resource_bundle_source_map.h"
 
+#include "base/logging.h"
 #include "ui/base/resource/resource_bundle.h"
 
 namespace extensions {
@@ -25,11 +26,18 @@ void ResourceBundleSourceMap::RegisterSource(const std::string& name,
 v8::Handle<v8::Value> ResourceBundleSourceMap::GetSource(
     v8::Isolate* isolate,
     const std::string& name) {
-  if (!Contains(name))
+  if (!Contains(name)) {
+    NOTREACHED() << "No module is registered with name \"" << name << "\"";
     return v8::Undefined(isolate);
-  int resource_id = resource_id_map_[name];
-  return ConvertString(isolate,
-                       resource_bundle_->GetRawDataResource(resource_id));
+  }
+  base::StringPiece resource =
+      resource_bundle_->GetRawDataResource(resource_id_map_[name]);
+  if (resource.empty()) {
+    NOTREACHED()
+        << "Module resource registered as \"" << name << "\" not found";
+    return v8::Undefined(isolate);
+  }
+  return ConvertString(isolate, resource);
 }
 
 bool ResourceBundleSourceMap::Contains(const std::string& name) {
