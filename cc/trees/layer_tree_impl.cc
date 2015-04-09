27e475e72b638e5eeb63aa53d8be0eca50a95fd1
hush <hush@chromium.org>
2015-04-08 17:59:12 -0700

    Clean up some dead code
    BUG=
    
    Review URL: https://codereview.chromium.org/1076613002
    
    Cr-Commit-Position: refs/heads/master@{#324324}

--- cc/trees/layer_tree_impl.cc
+++ cc/trees/layer_tree_impl.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include <set>
 
-#include "base/auto_reset.h"
 #include "base/trace_event/trace_event.h"
 #include "base/trace_event/trace_event_argument.h"
 #include "cc/animation/keyframed_animation_curve.h"
@@ -501,9 +500,6 @@ void LayerTreeImpl::SetViewportLayersFromIds(
          outer_viewport_scroll_layer_id == Layer::INVALID_ID);
 
   HideInnerViewportScrollbarsIfNearMinimumScale();
-
-  if (!root_layer_scroll_offset_delegate_)
-    return;
 }
 
 void LayerTreeImpl::ClearViewportLayers() {
