92a2c54eb7fccc7244c65e8da4cb7f05850ab821
morrita <morrita@chromium.org>
2015-04-09 12:43:27 -0700

    ChannelMojo: Ensure that it always has ScopedIPCSupport
    ChannelMojo has ScopedIPCSupport, but it is instantiated only in
    in-process mode. This CL lets it always instantiate to make it
    clear that ChannelInfo is protected by the ScopedIPCSupport.
    It simplifies the relationship between the support object and
    the channel, and makes the lifecycle invariant reasonable.
    
    With this change, we no longer need to protect ChannelMojo with
    ScopedIPCSupport on its client side. Now it's built-in.
    
    Note that this is a speculative fix of fuzzer generated crash,
    where Mojo channel related globals are gone before when channel
    mojo is being destroyed.
    
    BUG=473438
    R=viettrungluu@chromium.org, rockot@chromium.org
    
    Committed: https://crrev.com/808706e71c213c916815e967e3156cae64d12c00
    Cr-Commit-Position: refs/heads/master@{#324308}
    
    Review URL: https://codereview.chromium.org/1054253005
    
    Cr-Commit-Position: refs/heads/master@{#324488}

--- ipc/mojo/ipc_channel_mojo.h
+++ ipc/mojo/ipc_channel_mojo.h
@@ -55,7 +55,6 @@ class IPC_MOJO_EXPORT ChannelMojo
    public:
     virtual ~Delegate() {}
     virtual base::WeakPtr<Delegate> ToWeakPtr() = 0;
-    virtual scoped_refptr<base::TaskRunner> GetIOTaskRunner() = 0;
     virtual void OnChannelCreated(base::WeakPtr<ChannelMojo> channel) = 0;
   };
 
@@ -64,20 +63,24 @@ class IPC_MOJO_EXPORT ChannelMojo
 
   // Create ChannelMojo. A bootstrap channel is created as well.
   // |host| must not be null for server channels.
-  static scoped_ptr<ChannelMojo> Create(Delegate* delegate,
-                                        const ChannelHandle& channel_handle,
-                                        Mode mode,
-                                        Listener* listener);
+  static scoped_ptr<ChannelMojo> Create(
+      Delegate* delegate,
+      scoped_refptr<base::TaskRunner> io_runner,
+      const ChannelHandle& channel_handle,
+      Mode mode,
+      Listener* listener);
 
   // Create a factory object for ChannelMojo.
   // The factory is used to create Mojo-based ChannelProxy family.
   // |host| must not be null.
   static scoped_ptr<ChannelFactory> CreateServerFactory(
       Delegate* delegate,
+      scoped_refptr<base::TaskRunner> io_runner,
       const ChannelHandle& channel_handle);
 
   static scoped_ptr<ChannelFactory> CreateClientFactory(
       Delegate* delegate,
+      scoped_refptr<base::TaskRunner> io_runner,
       const ChannelHandle& channel_handle);
 
   ~ChannelMojo() override;
@@ -116,6 +119,7 @@ class IPC_MOJO_EXPORT ChannelMojo
 
  protected:
   ChannelMojo(Delegate* delegate,
+              scoped_refptr<base::TaskRunner> io_runner,
               const ChannelHandle& channel_handle,
               Mode mode,
               Listener* listener);
@@ -137,7 +141,7 @@ class IPC_MOJO_EXPORT ChannelMojo
   // notifications invoked by them.
   typedef internal::MessagePipeReader::DelayedDeleter ReaderDeleter;
 
-  void InitDelegate(ChannelMojo::Delegate* delegate);
+  void InitOnIOThread(ChannelMojo::Delegate* delegate);
 
   scoped_ptr<MojoBootstrap> bootstrap_;
   base::WeakPtr<Delegate> delegate_;
