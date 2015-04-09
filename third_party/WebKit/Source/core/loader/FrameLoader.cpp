1ea8dbcb10bec2e8cdde2ac6195c225f441b986d
sanjoy.pal@samsung.com <sanjoy.pal@samsung.com>
2015-04-09 05:41:34 +0000

    Stop parsing of current document before starting the next load.
    We missed the steps to stop parsing of the current load during https://codereview.chromium.org/181493007
    
    BUG=449498
    
    Review URL: https://codereview.chromium.org/1069743002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193419 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -1240,6 +1240,14 @@ void FrameLoader::startLoad(FrameLoadRequest& frameLoadRequest, FrameLoadType ty
         return;
     }
 
+    // FIXME: This is an odd set of steps to shut down parsing and it's unclear why it works.
+    // It's also unclear why other steps don't work.
+    if (m_frame->document()->parsing()) {
+        finishedParsing();
+        m_frame->document()->setParsingState(Document::FinishedParsing);
+    }
+    m_frame->document()->setReadyState(Document::Complete);
+
     if (m_provisionalDocumentLoader) {
         m_provisionalDocumentLoader->stopLoading();
         if (m_provisionalDocumentLoader)
