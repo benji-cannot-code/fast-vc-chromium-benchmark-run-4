eb6612242dbc916777aa870aa3f2c78257ee7a78
yoav@yoav.ws <yoav@yoav.ws>
2015-04-09 21:07:44 +0000

    Added preloader support for sizes based RW values
    This CL adds support for RW hints that are based on the element's sizes attribute.
    It currently adds this only to the PreloadScanner.
    The same would be added later to the regular parser.
    
    BUG=458983
    
    Review URL: https://codereview.chromium.org/1078633003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193479 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/misc/resources/image-checks-for-rw.php
+++ third_party/WebKit/LayoutTests/http/tests/misc/resources/image-checks-for-rw.php
@@ -1,8 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
     $rw = $_SERVER["HTTP_RW"];
+    $expected_rw = $_GET["rw"];
 
-    if($rw) {
+    if (($rw == $expected_rw) || ($rw && !$expected_rw)) {
         $fn = fopen("compass.jpg", "r");
         fpassthru($fn);
         fclose($fn);
--- third_party/WebKit/Source/core/fetch/FetchRequest.h
+++ third_party/WebKit/Source/core/fetch/FetchRequest.h
@@ -41,6 +41,17 @@ public:
     enum DeferOption { NoDefer, LazyLoad, DeferredByClient };
     enum OriginRestriction { UseDefaultOriginRestrictionForType, RestrictToSameOrigin, NoOriginRestriction };
 
+    struct ResourceWidth {
+        float width;
+        bool isSet;
+
+        ResourceWidth()
+            : width(0)
+            , isSet(false)
+        {
+        }
+    };
+
     explicit FetchRequest(const ResourceRequest&, const AtomicString& initiator, const String& charset = String(), ResourceLoadPriority = ResourceLoadPriorityUnresolved);
     FetchRequest(const ResourceRequest&, const AtomicString& initiator, const ResourceLoaderOptions&);
     FetchRequest(const ResourceRequest&, const FetchInitiatorInfo&);
@@ -57,6 +68,7 @@ public:
     bool forPreload() const { return m_forPreload; }
     void setForPreload(bool forPreload) { m_forPreload = forPreload; }
     DeferOption defer() const { return m_defer; }
+    ResourceWidth resourceWidth() const { return m_resourceWidth; }
     void setDefer(DeferOption defer) { m_defer = defer; }
     void setContentSecurityCheck(ContentSecurityPolicyDisposition contentSecurityPolicyOption) { m_options.contentSecurityPolicyOption = contentSecurityPolicyOption; }
     void setCrossOriginAccessControl(SecurityOrigin*, StoredCredentials, CredentialRequest);
@@ -64,6 +76,7 @@ public:
     void setCrossOriginAccessControl(SecurityOrigin*, const AtomicString& crossOriginMode);
     OriginRestriction originRestriction() const { return m_originRestriction; }
     void setOriginRestriction(OriginRestriction restriction) { m_originRestriction = restriction; }
+    void setResourceWidth(ResourceWidth);
 
 private:
     ResourceRequest m_resourceRequest;
@@ -73,6 +86,7 @@ private:
     bool m_forPreload;
     DeferOption m_defer;
     OriginRestriction m_originRestriction;
+    ResourceWidth m_resourceWidth;
 };
 
 } // namespace blink
