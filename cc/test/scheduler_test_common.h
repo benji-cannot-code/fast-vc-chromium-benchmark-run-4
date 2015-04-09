8fe2bbb3aaa2f2b4c58be0e63192a25d34e0c129
mithro <mithro@mithis.com>
2015-04-09 00:05:01 -0700

    cc: Remove background ticking from LTHI.
    Currently LTHI "background ticks" animations once every second. This background
    ticking doesn't have any js detectable effect, nor user visible effects, which
    means it is just wasting CPU cycles (and thus power).
    
    BUG=371747
    R=jdduke,brianderson,danakj
    
    Review URL: https://codereview.chromium.org/1050833002
    
    Cr-Commit-Position: refs/heads/master@{#324389}

--- cc/test/scheduler_test_common.h
+++ cc/test/scheduler_test_common.h
@@ -142,8 +142,6 @@ class TestSchedulerFrameSourcesConstructor
 
  protected:
   BeginFrameSource* ConstructPrimaryFrameSource(Scheduler* scheduler) override;
-  BeginFrameSource* ConstructBackgroundFrameSource(
-      Scheduler* scheduler) override;
   BeginFrameSource* ConstructUnthrottledFrameSource(
       Scheduler* scheduler) override;
 
