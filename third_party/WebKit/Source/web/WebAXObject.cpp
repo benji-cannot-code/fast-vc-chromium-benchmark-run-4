39c9598035ad90722bbe6fdfb690c892795632b4
dmazzoni@chromium.org <dmazzoni@chromium.org>
2015-04-09 23:43:18 +0000

    Add WebScopedAXContext to allow a one-time snapshot of accessibility.
    This makes it possible to query the accessibility tree without enabling it
    globally.
    
    BUG=472704
    
    Review URL: https://codereview.chromium.org/1053773002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193492 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebAXObject.cpp
+++ third_party/WebKit/Source/web/WebAXObject.cpp
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/HTMLNames.h"
 #include "core/css/CSSPrimitiveValueMappings.h"
-#include "core/dom/AXObjectCache.h"
 #include "core/dom/Document.h"
 #include "core/dom/Node.h"
 #include "core/frame/FrameHost.h"
@@ -45,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/page/EventHandler.h"
 #include "core/page/Page.h"
 #include "modules/accessibility/AXObject.h"
+#include "modules/accessibility/AXObjectCacheImpl.h"
 #include "modules/accessibility/AXTable.h"
 #include "modules/accessibility/AXTableCell.h"
 #include "modules/accessibility/AXTableColumn.h"
@@ -71,6 +71,21 @@ static bool isLayoutClean(Document* document)
         || (document->lifecycle().state() == DocumentLifecycle::StyleClean && !document->view()->needsLayout());
 }
 #endif
+
+WebScopedAXContext::WebScopedAXContext(WebDocument& rootDocument)
+    : m_private(adoptRef(new ScopedAXObjectCache(*rootDocument.unwrap<Document>())))
+{
+}
+
+WebScopedAXContext::~WebScopedAXContext()
+{
+    m_private.reset();
+}
+
+WebAXObject WebScopedAXContext::root() const
+{
+    return WebAXObject(static_cast<AXObjectCacheImpl*>(m_private->get())->root());
+}
 
 void WebAXObject::reset()
 {
