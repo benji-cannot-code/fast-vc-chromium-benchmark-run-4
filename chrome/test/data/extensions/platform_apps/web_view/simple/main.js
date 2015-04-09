f49564c730dbef2ca2a0ed1f85ccb7d1acdc9902
fsamuel <fsamuel@chromium.org>
2015-04-08 18:08:09 -0700

    <webview>: Don't reset allowtransparency and allowscaling between createGuest and attachGuest
    https://codereview.chromium.org/1033373003/ introduced lazily issuing updated GuestView attributes
    between create and attach. The AllowTransparency and AllowScaling attributes were defaulting to false
    if not available in the attach params and so they were getting reset on attach. This CL fixes the issue
    by only updating the attributes if they are passed along on attach.
    
    BUG=471997
    
    Review URL: https://codereview.chromium.org/1065413002
    
    Cr-Commit-Position: refs/heads/master@{#324327}

--- chrome/test/data/extensions/platform_apps/web_view/simple/main.js
+++ chrome/test/data/extensions/platform_apps/web_view/simple/main.js
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 function CreateWebViewAndGuest(callback) {
   var webview = document.createElement('webview');
+  webview.allowtransparency = true;
+  webview.allowscaling = true;
   var onLoadStop = function(e) {
     chrome.test.sendMessage('WebViewTest.LAUNCHED');
     webview.removeEventListener('loadstop', onLoadStop);
