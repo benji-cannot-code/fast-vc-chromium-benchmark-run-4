edf92d7b8b6cb92537360bfd0b35e99a41cdc566
yoav@yoav.ws <yoav@yoav.ws>
2015-04-10 07:52:42 +0000

    Add ImageLoader support for sizes based Client-Hints RW values
    This CL adds support for RW hints that are based on the element's sizes attribute to ImageLoader, to cover dynamically added images.
    
    BUG=458983
    
    Review URL: https://codereview.chromium.org/1060483003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193521 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLImageElement.h
+++ third_party/WebKit/Source/core/html/HTMLImageElement.h
@@ -108,6 +108,8 @@ public:
     void setUseFallbackContent();
     void setIsFallbackImage() { m_isFallbackImage = true; }
 
+    float sourceSize(Element&);
+
 protected:
     explicit HTMLImageElement(Document&, HTMLFormElement* = 0, bool createdByParser = false);
 
