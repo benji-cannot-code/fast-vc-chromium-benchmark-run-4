9a9638e38323fc4dc2f6df892199a28d010df944
jbauman <jbauman@chromium.org>
2015-04-10 14:41:33 -0700

    Don't do SetNeedsDisplay when updating delegated layer on android.
    The layer can mark itself as needing an update if something changed. Not doing this means the compositor doesn't have to redraw when using Surfaces and the browser contents haven't changed.
    
    Review URL: https://codereview.chromium.org/1065423003
    
    Cr-Commit-Position: refs/heads/master@{#324688}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -1182,7 +1182,6 @@ void RenderWidgetHostViewAndroid::SwapDelegatedFrame(
     layer_->SetIsDrawable(true);
     layer_->SetContentsOpaque(true);
     layer_->SetBounds(content_size_in_layer_);
-    layer_->SetNeedsDisplay();
   }
 
   base::Closure ack_callback =
