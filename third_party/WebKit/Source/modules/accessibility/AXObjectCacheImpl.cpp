68b50358d7341c921648336b93afc5ee02bf9a8a
dmazzoni@chromium.org <dmazzoni@chromium.org>
2015-04-10 06:32:41 +0000

    Re-land: Don't keep recreating AXMenuListPopup
    Every time an AXMenuList (corresponding to a
    <select> element) needed to update its children,
    we were recreating the AXMenuListPopup and
    all of the AXMenuListOptions each time, which is
    wasteful. This change avoids deleting the popup
    and stores the options in AXObjectCache by
    element, rather than by ID, so they can be reused
    when the options are updated.
    
    BUG=323462
    
    Review URL: https://codereview.chromium.org/1059503002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193517 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/accessibility/AXObjectCacheImpl.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXObjectCacheImpl.cpp
@@ -204,6 +204,21 @@ AXObject* AXObjectCacheImpl::get(LayoutObject* layoutObject)
     return m_objects.get(axID);
 }
 
+// Returns true if |node| is an <option> element and its parent <select>
+// is a menu list (not a list box).
+static bool isMenuListOption(Node* node)
+{
+    if (!isHTMLOptionElement(node))
+        return false;
+    Element* parent = node->parentElement();
+    if (isHTMLOptGroupElement(parent))
+        parent = parent->parentElement();
+    if (!isHTMLSelectElement(parent))
+        return false;
+    LayoutObject* layoutObject = toHTMLSelectElement(parent)->layoutObject();
+    return layoutObject && layoutObject->isMenuList();
+}
+
 AXObject* AXObjectCacheImpl::get(Node* node)
 {
     if (!node)
@@ -215,7 +230,7 @@ AXObject* AXObjectCacheImpl::get(Node* node)
     AXID nodeID = m_nodeObjectMapping.get(node);
     ASSERT(!HashTraits<AXID>::isDeletedValue(nodeID));
 
-    if (node->layoutObject() && nodeID && !layoutID) {
+    if (node->layoutObject() && nodeID && !layoutID && !isMenuListOption(node)) {
         // This can happen if an AXNodeObject is created for a node that's not
         // laid out, but later something changes and it gets a layoutObject (like if it's
         // reparented).
@@ -313,6 +328,9 @@ PassRefPtr<AXObject> AXObjectCacheImpl::createFromRenderer(LayoutObject* layoutO
 
 PassRefPtr<AXObject> AXObjectCacheImpl::createFromNode(Node* node)
 {
+    if (isMenuListOption(node))
+        return AXMenuListOption::create(toHTMLOptionElement(node), this);
+
     return AXNodeObject::create(node, this);
 }
 
@@ -369,7 +387,7 @@ AXObject* AXObjectCacheImpl::getOrCreate(Node* node)
     // Or if it's a hidden element, but we still want to expose it because of other ARIA attributes.
     bool inCanvasSubtree = node->parentElement()->isInCanvasSubtree();
     bool isHidden = !node->layoutObject() && isNodeAriaVisible(node);
-    if (!inCanvasSubtree && !isHidden)
+    if (!inCanvasSubtree && !isHidden && !isMenuListOption(node))
         return 0;
 
     RefPtr<AXObject> newObj = createFromNode(node);
@@ -461,9 +479,6 @@ AXObject* AXObjectCacheImpl::getOrCreate(AccessibilityRole role)
         break;
     case MenuListPopupRole:
         obj = AXMenuListPopup::create(this);
-        break;
-    case MenuListOptionRole:
-        obj = AXMenuListOption::create(this);
         break;
     case SpinButtonRole:
         obj = AXSpinButton::create(this);
