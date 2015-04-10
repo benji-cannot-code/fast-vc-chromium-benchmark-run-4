411682732c35125b84b03bcb4f60f46d88ee7662
mtomasz <mtomasz@chromium.org>
2015-04-10 07:41:09 -0700

    Go back to undefined in case of an error in callbacks.
    There is no way to attribute nullability of callback arguments in Chromium IDL
    in contrast to optional arguments.
    
    As we're moving towards auto-generation of bindings from IDL, we decided to go
    back to optional for now.
    
    TEST=Compiles.
    BUG=None
    
    Review URL: https://codereview.chromium.org/1077013002
    
    Cr-Commit-Position: refs/heads/master@{#324630}

--- chrome/renderer/resources/extensions/file_system_custom_bindings.js
+++ chrome/renderer/resources/extensions/file_system_custom_bindings.js
@@ -83,7 +83,7 @@ binding.registerCustomHook(function(bindingsAPI) {
 
   apiFunctions.setCustomCallback('requestFileSystem',
       function(name, request, callback, response) {
-    var fileSystem = null;
+    var fileSystem;
     if (response && response.file_system_id) {
       fileSystem = fileSystemNatives.GetIsolatedFileSystem(
           response.file_system_id, response.file_system_path);
@@ -93,16 +93,6 @@ binding.registerCustomHook(function(bindingsAPI) {
         request,
         callback,
         [fileSystem]);
-  });
-
-  apiFunctions.setCustomCallback('getVolumeList',
-      function(name, request, callback, response) {
-    var volumeList = response || null;
-    sendRequest.safeCallbackApply(
-        'fileSystem.getVolumeList',
-        request,
-        callback,
-        [volumeList]);
   });
 
   // TODO(benwells): Remove these deprecated versions of the functions.
