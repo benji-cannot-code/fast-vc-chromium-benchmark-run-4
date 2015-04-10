18c1326e81bd4ba6492e449012e94d964cfb9937
imcheng <imcheng@google.com>
2015-04-10 10:59:18 -0700

    Reland of "MediaRouter interfaces with stub implementations."
    Original patch: crrev.com/1020743003.
    
    The GN file is changed so that the tests are no longer depending on the target that was causing link errors on debug GN builds.
    
    TEST=git cl try + mac_chromium_gn_dbg + win8_chromium_gn_dbg
    
    [Media Router] MediaRouter interfaces with stub implementations.
    
    - Updated Media Router data classes.
    - Added MediaRouter interface with a stub MediaRouterImpl class.
    - MRImpl is a KeyedService and is instantiated via
      its BrowserContextKeyedServiceFactory implementation.
    - Added MediaRoutesObserver / MediaSinksObserver interfaces which are
      used by MediaRouter API.
    
    BUG=461815,464199
    
    Review URL: https://codereview.chromium.org/1076973002
    
    Cr-Commit-Position: refs/heads/master@{#324662}

---
+++ chrome/browser/media/router/media_routes_observer.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/media/router/media_routes_observer.h"
+
+#include "base/logging.h"
+#include "chrome/browser/media/router/media_router.h"
+
+namespace media_router {
+
+MediaRoutesObserver::MediaRoutesObserver(MediaRouter* router)
+    : router_(router) {
+  DCHECK(router_);
+  if (!router_->RegisterMediaRoutesObserver(this))
+    LOG(ERROR) << "RegisterMediaRoutesObserver failed.";
+}
+
+MediaRoutesObserver::~MediaRoutesObserver() {
+  router_->UnregisterMediaRoutesObserver(this);
+}
+
+}  // namespace media_router
--- chrome/browser/media/router/media_source.cc
+++ chrome/browser/media/router/media_source.cc
@@ -9,14 +9,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media_router {
 
-MediaSource::MediaSource(const std::string& source_id)
-    : id_(source_id) {
+MediaSource::MediaSource(const MediaSourceId& source_id) : id_(source_id) {
+}
+
+MediaSource::MediaSource() {
 }
 
 MediaSource::~MediaSource() {}
 
-std::string MediaSource::id() const {
+MediaSourceId MediaSource::id() const {
   return id_;
+}
+
+bool MediaSource::Equals(const MediaSource& other) const {
+  return id_ == other.id();
+}
+
+bool MediaSource::Empty() const {
+  return id_.empty();
+}
+
+std::string MediaSource::ToString() const {
+  return "MediaSource[" + id_ + "]";
 }
 
 }  // namespace media_router
