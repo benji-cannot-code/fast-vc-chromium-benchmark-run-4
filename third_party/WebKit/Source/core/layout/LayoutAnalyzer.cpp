eac13175e9d6edd14a7140bb8ed3c24aab88cab3
benjhayden@chromium.org <benjhayden@chromium.org>
2015-04-10 23:21:51 +0000

    LayoutAnalyzer UMA histograms
    If more than 100 nodes were laid out, record Layout.MicroSecondsPerNode = 1000 * (endMS - startMS) / totalNodes.
    
    If more than half of laid-out nodes were one of {float,TD,positioned,text}, then record a separate similar histogram.
    
    Review URL: https://codereview.chromium.org/1076603002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193576 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutAnalyzer.cpp
+++ third_party/WebKit/Source/core/layout/LayoutAnalyzer.cpp
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/LayoutObject.h"
 #include "core/layout/LayoutText.h"
 #include "platform/TracedValue.h"
+#include "public/platform/Platform.h"
 
 namespace blink {
 
@@ -29,6 +30,7 @@ LayoutAnalyzer::Scope::~Scope()
 
 void LayoutAnalyzer::reset()
 {
+    m_startMs = currentTimeMS();
     m_counters.resize(NumCounters);
     m_counters[LayoutBlockRectangleChanged] = 0;
     m_counters[LayoutBlockRectangleDidNotChange] = 0;
@@ -116,6 +118,30 @@ void LayoutAnalyzer::pop(const LayoutObject& o)
 {
     ASSERT(m_stack.peek() == &o);
     m_stack.pop();
+}
+
+void LayoutAnalyzer::recordCounters()
+{
+    unsigned totalNodes = m_counters[TotalLayoutObjectsThatWereLaidOut];
+    if (totalNodes < 100)
+        return;
+    unsigned usPerNode = (1000.0 * (currentTimeMS() - m_startMs)) / totalNodes;
+    Platform::current()->histogramCustomCounts("Layout.MicroSecondsPerNode", usPerNode, 0, 1000 * 1000, 50);
+    unsigned halfTotalNodes = totalNodes / 2;
+    const char* histogram = nullptr;
+    if (m_counters[LayoutAnalyzer::LayoutObjectsThatAreFloating] > halfTotalNodes) {
+        histogram = "Layout.MicroSecondsPerFloat";
+    } else if (m_counters[LayoutAnalyzer::LayoutObjectsThatAreTableCells] > halfTotalNodes) {
+        histogram = "Layout.MicroSecondsPerTD";
+    } else if (m_counters[LayoutAnalyzer::LayoutObjectsThatAreOutOfFlowPositioned] > halfTotalNodes) {
+        histogram = "Layout.MicroSecondsPerPositioned";
+    } else if (m_counters[LayoutAnalyzer::LayoutObjectsThatAreTextAndCanUseTheSimpleFontCodePath] > halfTotalNodes) {
+        histogram = "Layout.MicroSecondsPerSimpleText";
+    } else if (m_counters[LayoutAnalyzer::LayoutObjectsThatAreTextAndCanNotUseTheSimpleFontCodePath] > halfTotalNodes) {
+        histogram = "Layout.MicroSecondsPerComplexText";
+    }
+    if (histogram)
+        Platform::current()->histogramCustomCounts(histogram, usPerNode, 0, 1000 * 1000, 50);
 }
 
 PassRefPtr<TracedValue> LayoutAnalyzer::toTracedValue()
