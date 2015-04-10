549c3783a844b0b6484d6e7c136f161ed8956402
bashi@chromium.org <bashi@chromium.org>
2015-04-10 08:07:37 +0000

    bindings: Use Visitor to collect union types
    Before this CL, we doesn't collect union types which are used in sequence,
    array or nullable. It causes build errors.
    
    BUG=474856
    
    Review URL: https://codereview.chromium.org/1063253005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193524 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/core/V8TestInterfaceConstructor.cpp
+++ third_party/WebKit/Source/bindings/tests/results/core/V8TestInterfaceConstructor.cpp
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/core/v8/Dictionary.h"
 #include "bindings/core/v8/ExceptionState.h"
+#include "bindings/core/v8/UnionTypesCore.h"
 #include "bindings/core/v8/V8DOMConfiguration.h"
 #include "bindings/core/v8/V8ObjectConstructor.h"
 #include "bindings/core/v8/V8TestInterfaceEmpty.h"
@@ -58,6 +59,7 @@ static void constructor2(const v8::FunctionCallbackInfo<v8::Value>& info)
     Dictionary dictionaryArg;
     Vector<String> sequenceStringArg;
     Vector<Dictionary> sequenceDictionaryArg;
+    Vector<LongOrTestDictionary> sequenceLongOrTestDictionaryArg;
     Dictionary optionalDictionaryArg;
     TestInterfaceEmpty* optionalTestInterfaceEmptyArg;
     {
@@ -82,20 +84,23 @@ static void constructor2(const v8::FunctionCallbackInfo<v8::Value>& info)
         sequenceDictionaryArg = toImplArray<Dictionary>(info[5], 6, info.GetIsolate(), exceptionState);
         if (exceptionState.throwIfNeeded())
             return;
-        if (!isUndefinedOrNull(info[6]) && !info[6]->IsObject()) {
-            exceptionState.throwTypeError("parameter 7 ('optionalDictionaryArg') is not an object.");
+        sequenceLongOrTestDictionaryArg = toImplArray<LongOrTestDictionary>(info[6], 7, info.GetIsolate(), exceptionState);
+        if (exceptionState.throwIfNeeded())
+            return;
+        if (!isUndefinedOrNull(info[7]) && !info[7]->IsObject()) {
+            exceptionState.throwTypeError("parameter 8 ('optionalDictionaryArg') is not an object.");
             exceptionState.throwIfNeeded();
             return;
         }
-        optionalDictionaryArg = Dictionary(info[6], info.GetIsolate(), exceptionState);
+        optionalDictionaryArg = Dictionary(info[7], info.GetIsolate(), exceptionState);
         if (exceptionState.throwIfNeeded())
             return;
-        optionalTestInterfaceEmptyArg = V8TestInterfaceEmpty::toImplWithTypeCheck(info.GetIsolate(), info[7]);
+        optionalTestInterfaceEmptyArg = V8TestInterfaceEmpty::toImplWithTypeCheck(info.GetIsolate(), info[8]);
     }
     ScriptState* scriptState = ScriptState::current(info.GetIsolate());
     ExecutionContext* executionContext = currentExecutionContext(info.GetIsolate());
     Document& document = *toDocument(currentExecutionContext(info.GetIsolate()));
-    RefPtr<TestInterfaceConstructor> impl = TestInterfaceConstructor::create(scriptState, executionContext, document, doubleArg, stringArg, testInterfaceEmptyArg, dictionaryArg, sequenceStringArg, sequenceDictionaryArg, optionalDictionaryArg, optionalTestInterfaceEmptyArg, exceptionState);
+    RefPtr<TestInterfaceConstructor> impl = TestInterfaceConstructor::create(scriptState, executionContext, document, doubleArg, stringArg, testInterfaceEmptyArg, dictionaryArg, sequenceStringArg, sequenceDictionaryArg, sequenceLongOrTestDictionaryArg, optionalDictionaryArg, optionalTestInterfaceEmptyArg, exceptionState);
     if (exceptionState.hadException()) {
         exceptionState.throwIfNeeded();
         return;
@@ -178,7 +183,7 @@ static void constructor4(const v8::FunctionCallbackInfo<v8::Value>& info)
 static void constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     ExceptionState exceptionState(ExceptionState::ConstructionContext, "TestInterfaceConstructor", info.Holder(), info.GetIsolate());
-    switch (std::min(8, info.Length())) {
+    switch (std::min(9, info.Length())) {
     case 0:
         if (true) {
             TestInterfaceConstructorV8Internal::constructor1(info);
@@ -203,12 +208,6 @@ static void constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
             return;
         }
         break;
-    case 6:
-        if (true) {
-            TestInterfaceConstructorV8Internal::constructor2(info);
-            return;
-        }
-        break;
     case 7:
         if (true) {
             TestInterfaceConstructorV8Internal::constructor2(info);
@@ -221,9 +220,15 @@ static void constructor(const v8::FunctionCallbackInfo<v8::Value>& info)
             return;
         }
         break;
+    case 9:
+        if (true) {
+            TestInterfaceConstructorV8Internal::constructor2(info);
+            return;
+        }
+        break;
     default:
         if (info.Length() >= 0) {
-            throwArityTypeError(exceptionState, "[0, 1, 2, 3, 6, 7, 8]", info.Length());
+            throwArityTypeError(exceptionState, "[0, 1, 2, 3, 7, 8, 9]", info.Length());
             return;
         }
         exceptionState.throwTypeError(ExceptionMessages::notEnoughArguments(0, info.Length()));
