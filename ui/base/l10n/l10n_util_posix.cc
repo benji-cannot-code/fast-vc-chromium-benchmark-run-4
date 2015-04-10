9f1c51470bfdfa2315f6cdcd36c178bff3526769
thestig <thestig@chromium.org>
2015-04-10 15:44:39 -0700

    Cleanup misc bits in ui/base/l10n.
    Review URL: https://codereview.chromium.org/1059353004
    
    Cr-Commit-Position: refs/heads/master@{#324702}

--- ui/base/l10n/l10n_util_posix.cc
+++ ui/base/l10n/l10n_util_posix.cc
@@ -12,19 +12,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace l10n_util {
 
-// Return true blindly for now.
 bool IsLocaleSupportedByOS(const std::string& locale) {
-#if !defined(OS_CHROMEOS)
-  return true;
-#else
+#if defined(OS_CHROMEOS)
   // We don't have translations yet for am, and sw.
   // TODO(jungshik): Once the above issues are resolved, change this back
   // to return true.
-  static const char* kUnsupportedLocales[] = {"am", "sw"};
+  static const char kUnsupportedLocales[][3] = {"am", "sw"};
   for (size_t i = 0; i < arraysize(kUnsupportedLocales); ++i) {
     if (LowerCaseEqualsASCII(locale, kUnsupportedLocales[i]))
       return false;
   }
+  return true;
+#else
+  // Return true blindly for now.
   return true;
 #endif
 }
