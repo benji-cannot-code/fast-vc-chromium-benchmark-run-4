01bbd9b9b91c298bc5bcba9eaf67550be0bfd0eb
rdsmith <rdsmith@chromium.org>
2015-04-10 12:03:09 -0700

    Remove SdchDictionary from exposed SdchManager interface.
    BUG=None
    R=ellyjones@chromium.org
    
    Review URL: https://codereview.chromium.org/1053543003
    
    Cr-Commit-Position: refs/heads/master@{#324676}

--- net/base/sdch_dictionary.cc
+++ net/base/sdch_dictionary.cc
@@ -37,8 +37,7 @@ SdchDictionary::SdchDictionary(const std::string& dictionary_text,
       domain_(domain),
       path_(path),
       expiration_(expiration),
-      ports_(ports),
-      clock_(new base::DefaultClock) {
+      ports_(ports) {
 }
 
 SdchDictionary::SdchDictionary(const SdchDictionary& rhs)
@@ -49,8 +48,7 @@ SdchDictionary::SdchDictionary(const SdchDictionary& rhs)
       domain_(rhs.domain_),
       path_(rhs.path_),
       expiration_(rhs.expiration_),
-      ports_(rhs.ports_),
-      clock_(new base::DefaultClock) {
+      ports_(rhs.ports_) {
 }
 
 SdchDictionary::~SdchDictionary() {
@@ -161,11 +159,7 @@ bool SdchDictionary::PathMatch(const std::string& path,
 }
 
 bool SdchDictionary::Expired() const {
-  return clock_->Now() > expiration_;
-}
-
-void SdchDictionary::SetClockForTesting(scoped_ptr<base::Clock> clock) {
-  clock_ = clock.Pass();
+  return base::Time::Now() > expiration_;
 }
 
 }  // namespace net
