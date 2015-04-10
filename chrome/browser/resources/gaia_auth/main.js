4eab10eab1b77a445e2486c6d1c3b4e7878f5b3f
xiyuan <xiyuan@chromium.org>
2015-04-10 06:59:38 -0700

    cros: Replace iframe load event with explicit postMessage.
    - Replace iframe load event with a 'loginUIDOMContentLoaded' message;
    - Wait for NOTIFICATION_SESSION_STARTED before submitting login form
      in GaiaAuthOffline;
    
    BUG=475516
    
    Review URL: https://codereview.chromium.org/1072223002
    
    Cr-Commit-Position: refs/heads/master@{#324626}

--- chrome/browser/resources/gaia_auth/main.js
+++ chrome/browser/resources/gaia_auth/main.js
@@ -106,6 +106,7 @@ Authenticator.prototype = {
 
     document.addEventListener('DOMContentLoaded', function() {
       window.addEventListener('message', handleInitializeMessage);
+      window.parent.postMessage({'method': 'loginUIDOMContentLoaded'}, '*');
     });
   },
 
