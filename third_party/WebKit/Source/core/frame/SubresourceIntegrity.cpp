a2957cbad144e42dd0f0a8805fb954df37f8e385
jww@chromium.org <jww@chromium.org>
2015-04-10 00:29:07 +0000

    Remove ni:/// prefix from Subresource Integrity
    The latest version of the spec has eliminated the ni:/// prefix.
    Additionally, this adds in a suite a new LayoutTests and fixes many old
    ones.
    
    This does not address two important changes that we'll implement in a
    separate, later CL:
    
    (1) Multiple strong hashes from a single  integrity attribute should be
    stored and if any passes, the content should be valid. Currently, we
    assume only one hash per integrity attribute.
    
    (2) MIME type checking should be moved behind an experimental flag.  The
    initial version of the spec does not include support for MIME types,
    although it does contain a generic "options" postfix at the end of
    hashes so we can implement MIME types in the future. Since this is a
    very likely outcome, we will need to disable MIME type checking in
    release, but it makes sense to keep it as experimental in preparation
    for v2.
    
    R=mkwst@chromium.org
    BUG=471429
    
    Review URL: https://codereview.chromium.org/1069883003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193495 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/SubresourceIntegrity.cpp
+++ third_party/WebKit/Source/core/frame/SubresourceIntegrity.cpp
@@ -150,15 +150,15 @@ bool SubresourceIntegrity::CheckSubresourceIntegrity(const Element& element, con
 
 // Before:
 //
-// ni:///[algorithm];[hash]
-//       ^                 ^
-//       position          end
+// [algorithm]-[hash]
+// ^                 ^
+// position          end
 //
 // After (if successful: if the method returns false, we make no promises and the caller should exit early):
 //
-// ni:///[algorithm];[hash]
-//                  ^      ^
-//           position    end
+// [algorithm]-[hash]
+//            ^      ^
+//     position    end
 bool SubresourceIntegrity::parseAlgorithm(const UChar*& position, const UChar* end, HashAlgorithm& algorithm)
 {
     // Any additions or subtractions from this struct should also modify the
@@ -188,15 +188,15 @@ bool SubresourceIntegrity::parseAlgorithm(const UChar*& position, const UChar* e
 
 // Before:
 //
-// ni:///[algorithm];[hash]     OR      ni:///[algorithm];[hash]?[params]
-//                   ^     ^                              ^              ^
-//            position   end                       position            end
+// [algorithm]-[hash]      OR     [algorithm]-[hash]?[options]
+//             ^     ^                        ^               ^
+//      position   end                 position             end
 //
 // After (if successful: if the method returns false, we make no promises and the caller should exit early):
 //
-// ni:///[algorithm];[hash]     OR      ni:///[algorithm];[hash]?[params]
-//                         ^                                    ^        ^
-//                         position/end                  position      end
+// [algorithm]-[hash]      OR     [algorithm]-[hash]?[options]
+//                   ^                              ^         ^
+//        position/end                       position       end
 bool SubresourceIntegrity::parseDigest(const UChar*& position, const UChar* end, String& digest)
 {
     const UChar* begin = position;
@@ -215,18 +215,19 @@ bool SubresourceIntegrity::parseDigest(const UChar*& position, const UChar* end,
 
 // Before:
 //
-// ni:///[algorithm];[hash]     OR      ni:///[algorithm];[hash]?[params]
-//                         ^                                    ^        ^
-//              position/end                             position      end
+// [algorithm]-[hash]     OR      [algorithm]-[hash]?[options]
+//                   ^                              ^         ^
+//        position/end                       position       end
 //
 // After (if successful: if the method returns false, we make no promises and the caller should exit early):
 //
-// ni:///[algorithm];[hash]     OR      ni:///[algorithm];[hash]?[params]
-//                         ^                                             ^
-//              position/end                                  position/end
+// [algorithm]-[hash]     OR      [algorithm]-[hash]?[options]
+//                   ^                                        ^
+//        position/end                             position/end
 bool SubresourceIntegrity::parseMimeType(const UChar*& position, const UChar* end, String& type)
 {
     type = emptyString();
+
     if (position == end)
         return true;
 
@@ -259,11 +260,6 @@ SubresourceIntegrity::IntegrityParseResult SubresourceIntegrity::parseIntegrityA
     const UChar* position = characters.data();
     const UChar* end = characters.end();
 
-    if (!skipToken<UChar>(position, end, "ni:///")) {
-        logErrorToConsole("Error parsing 'integrity' attribute ('" + attribute + "'). The value must begin with 'ni:///'.", document);
-        return IntegrityParseErrorFatal;
-    }
-
     // Algorithm parsing errors are non-fatal (the subresource should
     // still be loaded) because strong hash algorithms should be used
     // without fear of breaking older user agents that don't support
@@ -273,8 +269,8 @@ SubresourceIntegrity::IntegrityParseResult SubresourceIntegrity::parseIntegrityA
         return IntegrityParseErrorNonfatal;
     }
 
-    if (!skipExactly<UChar>(position, end, ';')) {
-        logErrorToConsole("Error parsing 'integrity' attribute ('" + attribute + "'). The hash algorithm must be followed by a ';' character.", document);
+    if (!skipExactly<UChar>(position, end, '-')) {
+        logErrorToConsole("Error parsing 'integrity' attribute ('" + attribute + "'). The hash algorithm must be followed by a '-' character.", document);
         return IntegrityParseErrorFatal;
     }
 
