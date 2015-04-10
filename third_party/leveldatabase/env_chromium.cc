cb7a9398ee6332b147147589bfafac0a49de9831
hashimoto <hashimoto@chromium.org>
2015-04-10 09:58:23 -0700

    AssertIOAllowed() in GetDirectoryEntries()
    Like FileEnumerator::ReadDirectory(), this function should be run on an IO-allowed thread.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1079053002
    
    Cr-Commit-Position: refs/heads/master@{#324651}

--- third_party/leveldatabase/env_chromium.cc
+++ third_party/leveldatabase/env_chromium.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/threading/thread_restrictions.h"
 #include "base/trace_event/trace_event.h"
 #include "third_party/leveldatabase/chromium_logger.h"
 #include "third_party/re2/re2/re2.h"
@@ -49,6 +50,7 @@ static base::File::Error LastFileError() {
 // latter can fail quietly without return an error result.
 static base::File::Error GetDirectoryEntries(const FilePath& dir_param,
                                              std::vector<FilePath>* result) {
+  base::ThreadRestrictions::AssertIOAllowed();
   result->clear();
 #if defined(OS_WIN)
   FilePath dir_filepath = dir_param.Append(FILE_PATH_LITERAL("*"));
