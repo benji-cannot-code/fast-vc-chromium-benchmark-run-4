757c7169f69cee9b2c93fed7346475b326ceccc7
orenb <orenb@chromium.org>
2015-04-10 18:25:19 -0700

    Add a fork of Polymore/more-routing [1] to our settings. This approach will allow us to make local modifications in case things break in the Polymer 0.8 migration. (Main difference currently is I replaced hyphens with underscores in filenames everywhere, in line with our file name convention.)
    Disabling presubmit because every warning is just related to existing JS code from the fork... we don't intend to go through and fix those.
    
    [1] https://github.com/Polymore/more-routing
    
    NOPRESUBMIT=true
    
    Review URL: https://codereview.chromium.org/1079683003
    
    Cr-Commit-Position: refs/heads/master@{#324741}

---
+++ chrome/browser/resources/settings/routing/more_switch/template_switch_extracted.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+    Polymer('template-switch', {
+
+      /** @override */
+      activateChild: function(child, index, external) {
+        if (this._activeContent) {
+          this._activeContent.remove();
+        }
+        var template = this.children[index];
+        if (!template) return;
+
+        var model    = this.modelForChild(template, index);
+        var fragment = template.createInstance(model, this.bindingDelegate);
+
+        this._activeContent = document.createElement('div');
+        this._activeContent.appendChild(fragment);
+        this.appendChild(this._activeContent);
+      },
+
+      /**
+       * @method modelForChild
+       * @protected
+       * @param {!HTMLElement} child The child element.
+       * @param {number}       index The index of the child.
+       * @return {!Object} The object to use as a template model.
+       */
+      modelForChild: function(child, index) {
+        return this.templateInstance && this.templateInstance.model || {};
+      },
+
+    });
+  
