f1775933226356115120ff463a72d1e47c15f792
chrishtr@chromium.org <chrishtr@chromium.org>
2015-04-11 19:45:57 +0000

    Add a test for the WebkitCanvas UseCounter.
    Depends on https://codereview.chromium.org/1074013002.
    
    BUG=475655
    
    Review URL: https://codereview.chromium.org/1079283002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193595 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -728,6 +728,10 @@ public:
     static void countDeprecationIfNotPrivateScript(v8::Isolate*, ExecutionContext*, Feature);
     static String deprecationMessage(Feature);
 
+    // Return whether the Feature was previously counted for this document.
+    // NOTE: only for use in testing.
+    static bool isCounted(Document&, Feature);
+
     void didCommitLoad();
 
     static UseCounter* getFrom(const Document*);
@@ -760,6 +764,13 @@ public:
         }
         void updateMeasurements();
 
+        // NOTE: only for use in testing.
+        bool hasRecordedMeasurement(Feature feature) const
+        {
+            ASSERT(feature >= 0 && feature < NumberOfFeatures);
+            return m_bits->get(feature);
+        }
+
     private:
         OwnPtr<BitVector> m_bits;
     };
@@ -769,6 +780,8 @@ private:
 
     bool recordMeasurement(Feature feature) { return m_countBits.recordMeasurement(feature); }
     void updateMeasurements();
+
+    bool hasRecordedMeasurement(Feature feature) const { return m_countBits.hasRecordedMeasurement(feature); }
 
     CountBits m_countBits;
     BitVector m_CSSFeatureBits;
--- third_party/WebKit/Source/core/testing/Internals.cpp
+++ third_party/WebKit/Source/core/testing/Internals.cpp
@@ -2354,4 +2354,10 @@ ValueIterable<int>::IterationSource* Internals::startIteration(ScriptState*, Exc
     return new InternalsIterationSource();
 }
 
+bool Internals::isUseCounted(Document* document, int useCounterId)
+{
+    return UseCounter::isCounted(*document, static_cast<UseCounter::Feature>(useCounterId));
+}
+
+
 } // namespace blink
