f05e4c06c3f59d3a40ade97d3adac5e460c91fb3
hashimoto <hashimoto@chromium.org>
2015-04-10 19:50:48 -0700

    Stop using FILE thread to run MemoryDetails::CollectProcessData
    Instead, run it on the blocking pool with SKIP_ON_SHUTDOWN behavior.
    
    BUG=474876
    
    Review URL: https://codereview.chromium.org/1066193003
    
    Cr-Commit-Position: refs/heads/master@{#324761}

--- chrome/browser/memory_details_android.cc
+++ chrome/browser/memory_details_android.cc
@@ -107,7 +107,7 @@ ProcessData* MemoryDetails::ChromeBrowser() {
 void MemoryDetails::CollectProcessData(
     CollectionMode mode,
     const std::vector<ProcessMemoryInformation>& chrome_processes) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
+  DCHECK(BrowserThread::GetBlockingPool()->RunsTasksOnCurrentThread());
 
   std::vector<ProcessMemoryInformation> all_processes(chrome_processes);
   AddNonChildChromeProcesses(&all_processes);
