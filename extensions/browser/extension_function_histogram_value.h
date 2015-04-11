22fe6b12c552a9cccc379e2183cbda63614506e8
reillyg <reillyg@chromium.org>
2015-04-10 20:47:21 -0700

    Implement chrome.sockets.udp.setBroadcast.
    Enable Chrome Apps to receive broadcast UDP traffic by setting the
    SO_BROADCAST option on a socket. This feature piggybacks on existing
    permissions as it requires a socket to be bound to the interface where
    the broadcast traffic will be received.
    
    BUG=308461
    
    Review URL: https://codereview.chromium.org/1071813002
    
    Cr-Commit-Position: refs/heads/master@{#324768}

--- extensions/browser/extension_function_histogram_value.h
+++ extensions/browser/extension_function_histogram_value.h
@@ -1061,6 +1061,7 @@ enum HistogramValue {
   NETWORKINGPRIVATE_GETDEVICESTATES,
   DEVELOPERPRIVATE_GETPROFILECONFIGURATION,
   DEVELOPERPRIVATE_UPDATEPROFILECONFIGURATION,
+  SOCKETS_UDP_SETBROADCAST,
   // Last entry: Add new entries above and ensure to update
   // tools/metrics/histograms/histograms.xml.
   ENUM_BOUNDARY
