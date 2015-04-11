eaca81bde77b71314903aade46c800697910ffc2
dmazzoni <dmazzoni@chromium.org>
2015-04-10 18:53:13 -0700

    Explicitly call deprecated AX name calculation API.
    This change explicitly calls deprecated AX name calculation functions
    so that we can delete the ones without the deprecated prefix. It also
    starts to add WebAXObjectProxy support for the new name calculation API
    so that we can start to implement it and write layout tests for it.
    
    BUG=473220
    
    Review URL: https://codereview.chromium.org/1074953003
    
    Cr-Commit-Position: refs/heads/master@{#324751}

--- content/shell/renderer/test_runner/web_ax_object_proxy.h
+++ content/shell/renderer/test_runner/web_ax_object_proxy.h
@@ -59,9 +59,6 @@ class WebAXObjectProxy : public gin::Wrappable<WebAXObjectProxy> {
 
   // Bound properties.
   std::string Role();
-  std::string Title();
-  std::string Description();
-  std::string HelpText();
   std::string StringValue();
   std::string Language();
   int X();
@@ -119,7 +116,6 @@ class WebAXObjectProxy : public gin::Wrappable<WebAXObjectProxy> {
   std::string RowIndexRange();
   std::string ColumnIndexRange();
   v8::Handle<v8::Object> CellForColumnAndRow(int column, int row);
-  v8::Handle<v8::Object> TitleUIElement();
   void SetSelectedTextRange(int selection_start, int length);
   bool IsAttributeSettable(const std::string& attribute);
   bool IsPressActionSupported();
@@ -139,6 +135,18 @@ class WebAXObjectProxy : public gin::Wrappable<WebAXObjectProxy> {
   void ScrollToGlobalPoint(int x, int y);
   int WordStart(int character_index);
   int WordEnd(int character_index);
+
+  // DEPRECATED accessible name and description accessors
+  std::string DeprecatedTitle();
+  std::string DeprecatedDescription();
+  std::string DeprecatedHelpText();
+  v8::Handle<v8::Object> DeprecatedTitleUIElement();
+
+  // NEW accessible name and description accessors
+  std::string Name();
+  std::string NameFrom();
+  int NameElementCount();
+  v8::Handle<v8::Object> NameElementAtIndex(unsigned index);
 
   blink::WebAXObject accessibility_object_;
   Factory* factory_;
