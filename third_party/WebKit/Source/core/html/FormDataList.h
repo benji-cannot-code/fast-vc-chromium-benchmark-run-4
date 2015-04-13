8837c01a1edcbface2a5e081fa7385a7be109862
haraken@chromium.org <haraken@chromium.org>
2015-04-13 06:46:17 +0000

    Revert 193616 "Oilpan: HeapVector's destructor should be correct..."
    This caused crashes in Oilpan builds.
    https://storage.googleapis.com/chromium-layout-test-archives/win_blink_oilpan_dbg/810/layout-test-results/results.html
    
    > Oilpan: HeapVector's destructor should be correctly called
    > 
    > A destructor of HeapVector needs to be called if the HeapVector has an inlined buffer
    > or the items in the HeapVector need destruction. Currently, a destructor of a HeapVector
    > that does not have an inlined buffer but holds items that need destructors is not called.
    > This is wrong. This CL corrects it.
    > 
    > This CL makes the following changes:
    > 
    > - Adds a |noDestructor| parameter to Vector. This is the main change.
    > 
    > - Corrects the condition of static_asserts in VectorTratis.h.
    > 
    > - Add "Finalized" to a couple of GarbageCollected classes. Vector items in these classes were leaking before this CL.
    > 
    > BUG=475801
    > TEST=HeapTest.VectorDestructors
    > 
    > Review URL: https://codereview.chromium.org/1078023002
    
    TBR=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/1086493002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193618 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/FormDataList.h
+++ third_party/WebKit/Source/core/html/FormDataList.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class CORE_EXPORT FormDataList : public RefCountedWillBeGarbageCollectedFinalized<FormDataList> {
+class CORE_EXPORT FormDataList : public RefCountedWillBeGarbageCollected<FormDataList> {
     DECLARE_EMPTY_VIRTUAL_DESTRUCTOR_WILL_BE_REMOVED(FormDataList);
 public:
     class Entry final {
