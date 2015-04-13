187f8dca72216b2bc5e5b7a08500d64ffbeb9f87
fmalita@chromium.org <fmalita@chromium.org>
2015-04-13 23:49:23 +0000

    CTM-independent BackgroundBleedBackgroundOverBorder implementation
    The BackgroundBleedBackgroundOverBorder backdrop bleed avoidance
    strategy draws opaque backgrounds (shrunk to the inner border edge) on
    top of the border. It also attempts to expand the inner border edge by
    one pixel in device space, to mitigate anti-aliasing blending artifacts
    along the common inner-border/outer-background edge.
    
    This approach is fragile and doesn't work reliably with impl-side
    painting and slimming paint, because the final device space transform
    is unknown at recording time.
    
    The CL updates BackgroundBleedBackgroundOverBorder to fill the inner
    border rrect completely. Since this is no longer a device-space
    dependent operation, it now produces correct results under arbitrary
    rasterization-time transformations. It also becomes usable with
    slimming paint.
    
    BUG=445598
    R=junov@chromium.org,chrishtr@chromium.org,robertphillips@google.com
    
    Review URL: https://codereview.chromium.org/1088613002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193660 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/BoxDecorationData.h
+++ third_party/WebKit/Source/core/paint/BoxDecorationData.h
@@ -27,7 +27,6 @@ public:
 
 private:
     BackgroundBleedAvoidance determineBackgroundBleedAvoidance(const LayoutBox&, GraphicsContext*);
-    bool borderObscuresBackgroundEdge(const ComputedStyle&, const FloatSize& contextScale) const;
     unsigned m_bleedAvoidance : 2; // BackgroundBleedAvoidance
 };
 
