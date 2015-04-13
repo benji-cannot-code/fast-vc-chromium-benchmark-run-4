e36a4a177e1beba70b0855d7cf8a8340af8fe7d6
dzhioev <dzhioev@chromium.org>
2015-04-13 13:54:00 -0700

    <gaia-whitelist-error> transformed to a customizable element.
    BUG=none
    TEST=manually
    
    Review URL: https://codereview.chromium.org/1080713002
    
    Cr-Commit-Position: refs/heads/master@{#324913}

--- chrome/browser/resources/chromeos/login/custom_elements.js
+++ chrome/browser/resources/chromeos/login/custom_elements.js
@@ -8,3 +8,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <include src="indeterminate-progress.js">
 <include src="controller-pairing-screen.js">
 <include src="host-pairing-screen.js">
+<include src="notification_card.js">
---
+++ chrome/browser/resources/chromeos/login/notification_card.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+Polymer('notification-card', (function() {
+  return {
+    buttonClicked: function() {
+      this.fire('buttonclick');
+    },
+
+    linkClicked: function(e) {
+      this.fire('linkclick');
+      e.preventDefault();
+    }
+  };
+})());
