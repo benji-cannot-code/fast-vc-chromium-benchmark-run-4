df41fb68c859f87bc77f92a578b2faf59405fbd1
timloh <timloh@chromium.org>
2015-04-12 20:00:56 -0700

    Revert of device scale factor detection: use gtk-xft-dpi consistently (patchset #6 id:100001 of https://codereview.chromium.org/1070433002/)
    Reason for revert:
    This patch makes content_shell segfault during startup.
    
    Program received signal SIGSEGV, Segmentation fault.
    0x00000000049d190a in views::DesktopScreenX11::BuildDisplaysFromXRandRInfo (this=0x35ed099f9200) at ../../ui/views/widget/desktop_aura/desktop_screen_x11.cc:291
    291           views::LinuxUI::instance()->GetDeviceScaleFactor();
    (gdb) bt
    #0  0x00000000049d190a in views::DesktopScreenX11::BuildDisplaysFromXRandRInfo (this=0x35ed099f9200) at ../../ui/views/widget/desktop_aura/desktop_screen_x11.cc:291
    #1  0x00000000049d096f in views::DesktopScreenX11::DesktopScreenX11 (this=0x35ed099f9200) at ../../ui/views/widget/desktop_aura/desktop_screen_x11.cc:107
    #2  0x00000000049d1fab in views::CreateDesktopScreen () at ../../ui/views/widget/desktop_aura/desktop_screen_x11.cc:370
    #3  0x0000000000451f28 in content::Shell::PlatformInitialize (default_window_size=...) at ../../content/shell/browser/shell_views.cc:421
    #4  0x0000000000440158 in content::Shell::Initialize () at ../../content/shell/browser/shell.cc:146
    
    Original issue's description:
    > device scale factor detection: use gtk-xft-dpi consistently
    >
    > Before this CL, we used either gtk-xft-dpi (UI fonts) or the physical DPI of
    > a more or less random display. This CL consistently uses gtk-xft-dpi which
    > is the better alternative: users can directly influence it (by changing the
    > Xft.dpi X resource, or via their gtkrc).
    >
    > See also https://github.com/derat/font-config-info for displaying your
    > current system settings.
    >
    > BUG=473089,143619
    >
    > Committed: https://crrev.com/06cda64742b45e1eb10bbbb66bca4b4da2efb3c9
    > Cr-Commit-Position: refs/heads/master@{#324782}
    
    TBR=sadrul@chromium.org,oshima@chromium.org,erg@google.com,erg@chromium.org,stapelberg@google.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=473089,143619
    
    Review URL: https://codereview.chromium.org/1079883002
    
    Cr-Commit-Position: refs/heads/master@{#324810}

--- ui/views/widget/desktop_aura/desktop_screen_x11.cc
+++ ui/views/widget/desktop_aura/desktop_screen_x11.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/native_widget_types.h"
 #include "ui/gfx/screen.h"
 #include "ui/gfx/x/x11_types.h"
-#include "ui/views/linux_ui/linux_ui.h"
 #include "ui/views/widget/desktop_aura/desktop_screen.h"
 #include "ui/views/widget/desktop_aura/desktop_window_tree_host_x11.h"
 #include "ui/views/widget/desktop_aura/x11_topmost_window_finder.h"
@@ -37,12 +36,21 @@ namespace {
 // in |Dispatch()|.
 const int64 kConfigureDelayMs = 500;
 
-double GetDeviceScaleFactor() {
-  float device_scale_factor = 1.0f;
-  if (views::LinuxUI::instance())
-    device_scale_factor =
-      views::LinuxUI::instance()->GetDeviceScaleFactor();
-  return device_scale_factor;
+// TODO(oshima): Consider using gtk-xft-dpi instead.
+float GetDeviceScaleFactor(int screen_pixels, int screen_mm) {
+  const int kCSSDefaultDPI = 96;
+  const float kInchInMm = 25.4f;
+
+  float screen_inches = screen_mm / kInchInMm;
+  float screen_dpi = screen_pixels / screen_inches;
+  float scale = screen_dpi / kCSSDefaultDPI;
+
+  return ui::GetScaleForScaleFactor(ui::GetSupportedScaleFactor(scale));
+}
+
+float GetDeviceScaleFactor() {
+  gfx::Display display = gfx::Screen::GetNativeScreen()->GetPrimaryDisplay();
+  return display.device_scale_factor();
 }
 
 gfx::Point PixelToDIPPoint(const gfx::Point& pixel_point) {
@@ -64,7 +72,8 @@ std::vector<gfx::Display> GetFallbackDisplayList() {
   gfx::Display gfx_display(0, bounds_in_pixels);
   if (!gfx::Display::HasForceDeviceScaleFactor() &&
       !ui::IsDisplaySizeBlackListed(physical_size)) {
-    const float device_scale_factor = GetDeviceScaleFactor();
+    float device_scale_factor = GetDeviceScaleFactor(
+        width, physical_size.width());
     DCHECK_LE(1.0f, device_scale_factor);
     gfx_display.SetScaleAndBounds(device_scale_factor, bounds_in_pixels);
   }
@@ -286,10 +295,7 @@ std::vector<gfx::Display> DesktopScreenX11::BuildDisplaysFromXRandRInfo() {
     has_work_area = true;
   }
 
-  // As per-display scale factor is not supported right now,
-  // the X11 root window's scale factor is always used.
-  const float device_scale_factor =
-      views::LinuxUI::instance()->GetDeviceScaleFactor();
+  float device_scale_factor = 1.0f;
   for (int i = 0; i < resources->noutput; ++i) {
     RROutput output_id = resources->outputs[i];
     gfx::XScopedPtr<XRROutputInfo,
@@ -316,6 +322,14 @@ std::vector<gfx::Display> DesktopScreenX11::BuildDisplaysFromXRandRInfo() {
       gfx::Display display(display_id, crtc_bounds);
 
       if (!gfx::Display::HasForceDeviceScaleFactor()) {
+        if (i == 0 && !ui::IsDisplaySizeBlackListed(
+            gfx::Size(output_info->mm_width, output_info->mm_height))) {
+          // As per display scale factor is not supported right now,
+          // the primary display's scale factor is always used.
+          device_scale_factor = GetDeviceScaleFactor(crtc->width,
+                                                     output_info->mm_width);
+          DCHECK_LE(1.0f, device_scale_factor);
+        }
         display.SetScaleAndBounds(device_scale_factor, crtc_bounds);
       }
 
