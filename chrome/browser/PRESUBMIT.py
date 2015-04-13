52dd4d67d90e271e73b05c64fc433949b5e56da7
daniimms <daniimms@amazon.com>
2015-04-13 08:59:50 -0700

    Update comment in PRESUBMIT to reflect recent changes
    BUG=NONE
    
    Review URL: https://codereview.chromium.org/1083473002
    
    Cr-Commit-Position: refs/heads/master@{#324855}

--- chrome/browser/PRESUBMIT.py
+++ chrome/browser/PRESUBMIT.py
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Presubmit script for Chromium browser code.
 
-This script currently only checks HTML/CSS/JS files in resources/.
+This script currently checks HTML/CSS/JS files in resources/ and ui/webui/.
 
 See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details about the presubmit API built into depot_tools, and see
