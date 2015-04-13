d3f70b897b3684d30fd3e5440da9e83682f92aac
gangwu <gangwu@chromium.org>
2015-04-13 11:45:52 -0700

    [Sync] Recording sync merge results
    Typed URLs, because they're not on the syncable service API, aren't recording this. So adding sync merge results in this change.
    
    BUG=468084
    
    Review URL: https://codereview.chromium.org/1079263002
    
    Cr-Commit-Position: refs/heads/master@{#324888}

--- chrome/browser/sync/glue/typed_url_model_associator.cc
+++ chrome/browser/sync/glue/typed_url_model_associator.cc
@@ -163,7 +163,8 @@ syncer::SyncError TypedUrlModelAssociator::AssociateModels(
     syncer::SyncMergeResult* local_merge_result,
     syncer::SyncMergeResult* syncer_merge_result) {
   ClearErrorStats();
-  syncer::SyncError error = DoAssociateModels();
+  syncer::SyncError error =
+      DoAssociateModels(local_merge_result, syncer_merge_result);
   UMA_HISTOGRAM_PERCENTAGE("Sync.TypedUrlModelAssociationErrors",
                            GetErrorPercentage());
   ClearErrorStats();
@@ -179,7 +180,9 @@ int TypedUrlModelAssociator::GetErrorPercentage() const {
   return num_db_accesses_ ? (100 * num_db_errors_ / num_db_accesses_) : 0;
 }
 
-syncer::SyncError TypedUrlModelAssociator::DoAssociateModels() {
+syncer::SyncError TypedUrlModelAssociator::DoAssociateModels(
+    syncer::SyncMergeResult* local_merge_result,
+    syncer::SyncMergeResult* syncer_merge_result) {
   DVLOG(1) << "Associating TypedUrl Models";
   DCHECK(expected_loop_ == base::MessageLoop::current());
 
@@ -208,6 +211,7 @@ syncer::SyncError TypedUrlModelAssociator::DoAssociateModels() {
           "Could not get the typed_url entries.",
           model_type());
     }
+    local_merge_result->set_num_items_before_association(typed_urls.size());
 
     // Get all the visits.
     std::map<history::URLID, history::VisitVector> visit_vectors;
@@ -235,6 +239,8 @@ syncer::SyncError TypedUrlModelAssociator::DoAssociateModels() {
           "might be running against an out-of-date server.",
           model_type());
     }
+    syncer_merge_result->set_num_items_before_association(
+        typed_url_root.GetTotalNodeCount());
 
     std::set<std::string> current_urls;
     for (history::URLRows::iterator ix = typed_urls.begin();
@@ -289,6 +295,8 @@ syncer::SyncError TypedUrlModelAssociator::DoAssociateModels() {
           DCHECK_EQ(new_url.last_visit().ToInternalValue(),
                     visits.back().visit_time.ToInternalValue());
           WriteToSyncNode(new_url, visits, &write_node);
+          syncer_merge_result->set_num_items_modified(
+              syncer_merge_result->num_items_modified() + 1);
         }
         if (difference & DIFF_LOCAL_ROW_CHANGED) {
           DCHECK_EQ(ix->id(), new_url.id());
@@ -314,6 +322,8 @@ syncer::SyncError TypedUrlModelAssociator::DoAssociateModels() {
 
         node.SetTitle(tag);
         WriteToSyncNode(*ix, visits, &node);
+        syncer_merge_result->set_num_items_added(
+            syncer_merge_result->num_items_added() + 1);
       }
 
       current_urls.insert(tag);
@@ -374,6 +384,8 @@ syncer::SyncError TypedUrlModelAssociator::DoAssociateModels() {
                          NULL,
                          &updated_urls,
                          &new_urls);
+        local_merge_result->set_num_items_added(
+            local_merge_result->num_items_added() + 1);
       }
     }
 
@@ -393,6 +405,8 @@ syncer::SyncError TypedUrlModelAssociator::DoAssociateModels() {
         sync_node.Tombstone();
       }
     }
+    syncer_merge_result->set_num_items_after_association(
+        typed_url_root.GetTotalNodeCount());
   }
 
   // Since we're on the history thread, we don't have to worry about updating
@@ -401,6 +415,10 @@ syncer::SyncError TypedUrlModelAssociator::DoAssociateModels() {
   // to worry about the sync model getting out of sync, because changes are
   // propagated to the ChangeProcessor on this thread.
   WriteToHistoryBackend(&new_urls, &updated_urls, &new_visits, NULL);
+  local_merge_result->set_num_items_modified(updated_urls.size());
+  local_merge_result->set_num_items_after_association(
+      local_merge_result->num_items_before_association() +
+      local_merge_result->num_items_added());
   return syncer::SyncError();
 }
 
