f15db2c10d2dbe23b45452bd0b365ea0f30ba612
treib <treib@chromium.org>
2015-04-14 08:47:59 -0700

    PermissionRequestCreatorApiary: switch from deprecated "namespace" to "eventType"
    Review URL: https://codereview.chromium.org/1084763004
    
    Cr-Commit-Position: refs/heads/master@{#325058}

--- chrome/browser/supervised_user/child_accounts/permission_request_creator_apiary.h
+++ chrome/browser/supervised_user/child_accounts/permission_request_creator_apiary.h
@@ -65,7 +65,7 @@ class PermissionRequestCreatorApiary : public PermissionRequestCreator,
   GURL GetApiUrl() const;
   std::string GetApiScope() const;
 
-  void CreateRequest(const std::string& request_namespace,
+  void CreateRequest(const std::string& request_type,
                      const std::string& object_ref,
                      const SuccessCallback& callback);
 
