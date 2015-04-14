b1e03071abe0c8cdc0d8699cb2a8510a86ded43d
wfh <wfh@chromium.org>
2015-04-14 10:10:17 -0700

    Dynamically disable Win32k lockdown for future child processes if NPAPI is enabled.
    BUG=343192
    TEST=Start browser with --force-fieldtrials=Win32kLockdown/Enabled/ then log into an account that has a plugin policy to enable NPAPI, then visit http://www.microsoft.com/silverlight/pixel-shader/demo/ - page should render correctly.
    
    Review URL: https://codereview.chromium.org/1083853002
    
    Cr-Commit-Position: refs/heads/master@{#325072}

--- content/browser/plugin_service_impl.cc
+++ content/browser/plugin_service_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/ppapi_plugin_process_host.h"
 #include "content/browser/renderer_host/render_process_host_impl.h"
 #include "content/browser/renderer_host/render_view_host_impl.h"
+#include "content/common/content_switches_internal.h"
 #include "content/common/pepper_plugin_list.h"
 #include "content/common/plugin_list.h"
 #include "content/common/view_messages.h"
@@ -817,6 +818,9 @@ void PluginServiceImpl::DisablePluginsDiscoveryForTesting() {
 }
 
 void PluginServiceImpl::EnableNpapiPlugins() {
+#if defined(OS_WIN)
+  DisableWin32kRendererLockdown();
+#endif
   npapi_plugins_enabled_ = true;
   RefreshPlugins();
   BrowserThread::PostTask(
