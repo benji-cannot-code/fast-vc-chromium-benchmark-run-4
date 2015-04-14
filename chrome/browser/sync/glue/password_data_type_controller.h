44f03aa5d19d5e36c9775e5474b7ad84f105941f
hendrikw <hendrikw@chromium.org>
2015-04-14 07:34:30 -0700

    Revert of Restrict affiliaton-based matching to when sync without a custom passphrase is enabled. (patchset #5 id:100001 of https://codereview.chromium.org/1075073006/)
    Reason for revert:
    GPU FYI build failure, Please see https://code.google.com/p/chromium/issues/detail?id=476942
    
    Original issue's description:
    > Restrict affiliaton-based matching to be enabled only when sync without a custom passphrase is enabled.
    >
    > BUG=437865
    >
    > Committed: https://crrev.com/e60831f0d36f9691256a42d19689ffbcfcb8e13f
    > Cr-Commit-Position: refs/heads/master@{#325016}
    
    TBR=atwilson@chromium.org,zea@chromium.org,mkwst@chromium.org,engedy@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=437865
    
    Review URL: https://codereview.chromium.org/1087833003
    
    Cr-Commit-Position: refs/heads/master@{#325041}

--- chrome/browser/sync/glue/password_data_type_controller.h
+++ chrome/browser/sync/glue/password_data_type_controller.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "components/sync_driver/non_ui_data_type_controller.h"
-#include "components/sync_driver/sync_service_observer.h"
 
 class Profile;
 class ProfileSyncComponentsFactory;
@@ -22,8 +21,7 @@ class PasswordStore;
 namespace browser_sync {
 
 // A class that manages the startup and shutdown of password sync.
-class PasswordDataTypeController : public sync_driver::NonUIDataTypeController,
-                                   public sync_driver::SyncServiceObserver {
+class PasswordDataTypeController : public sync_driver::NonUIDataTypeController {
  public:
   PasswordDataTypeController(
       ProfileSyncComponentsFactory* profile_sync_factory,
@@ -40,10 +38,6 @@ class PasswordDataTypeController : public sync_driver::NonUIDataTypeController,
   bool PostTaskOnBackendThread(const tracked_objects::Location& from_here,
                                const base::Closure& task) override;
   bool StartModels() override;
-  void StopModels() override;
-
-  // sync_driver::SyncServiceObserver:
-  void OnStateChanged() override;
 
  private:
   Profile* const profile_;
