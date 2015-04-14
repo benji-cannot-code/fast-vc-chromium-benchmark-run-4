3b01aae31933bb8f731948d97ce82b6553933e28
falken <falken@chromium.org>
2015-04-13 19:34:53 -0700

    Service Worker: Split StartWorker metrics into new and installed versions.
    This starts track down why the "StartWorker.Status" metric shows more
    failures than expected.
    
    We expect different characteristics for starting a new worker vs. one that
    is already installed. It wouldn't be surprising for a new worker to fail
    to start due to a syntax error or take a longer time due to downloading
    the script, for example. Therefore, split the metrics to distinguish between
    the two cases.
    
    BUG=448003
    
    Review URL: https://codereview.chromium.org/1085553002
    
    Cr-Commit-Position: refs/heads/master@{#324993}

--- content/browser/service_worker/service_worker_metrics.cc
+++ content/browser/service_worker/service_worker_metrics.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/service_worker/service_worker_metrics.h"
 
-#include "base/metrics/histogram.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics_action.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/content_browser_client.h"
@@ -74,6 +74,26 @@ void ServiceWorkerMetrics::CountControlledPageLoad(const GURL& url) {
   RecordAction(base::UserMetricsAction("ServiceWorker.ControlledPageLoad"));
   BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
                           base::Bind(&RecordURLMetricOnUI, url));
+}
+
+void ServiceWorkerMetrics::RecordStartWorkerStatus(
+    ServiceWorkerStatusCode status,
+    bool is_installed) {
+  if (is_installed) {
+    UMA_HISTOGRAM_ENUMERATION("ServiceWorker.StartWorker.Status", status,
+                              SERVICE_WORKER_ERROR_MAX_VALUE);
+  } else {
+    UMA_HISTOGRAM_ENUMERATION("ServiceWorker.StartNewWorker.Status", status,
+                              SERVICE_WORKER_ERROR_MAX_VALUE);
+  }
+}
+
+void ServiceWorkerMetrics::RecordStartWorkerTime(const base::TimeDelta& time,
+                                                 bool is_installed) {
+  if (is_installed)
+    UMA_HISTOGRAM_MEDIUM_TIMES("ServiceWorker.StartWorker.Time", time);
+  else
+    UMA_HISTOGRAM_MEDIUM_TIMES("ServiceWorker.StartNewWorker.Time", time);
 }
 
 }  // namespace content
