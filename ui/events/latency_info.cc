a463379bcce7998d0c519cfdec588eb4a88350aa
miletus <miletus@chromium.org>
2015-04-14 09:42:20 -0700

    Make initialization of benchmark_enabled thread_safe
    BUG=468396
    
    Review URL: https://codereview.chromium.org/1081243002
    
    Cr-Commit-Position: refs/heads/master@{#325069}

--- ui/events/latency_info.cc
+++ ui/events/latency_info.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/json/json_writer.h"
+#include "base/lazy_instance.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/strings/stringprintf.h"
 #include "base/trace_event/trace_event.h"
@@ -137,6 +138,18 @@ scoped_refptr<base::trace_event::ConvertableToTraceFormat> AsTraceableData(
   return LatencyInfoTracedValue::FromValue(record_data.Pass());
 }
 
+struct BenchmarkEnabledInitializer {
+  BenchmarkEnabledInitializer() :
+      benchmark_enabled(TRACE_EVENT_API_GET_CATEGORY_GROUP_ENABLED(
+          "benchmark")) {
+  }
+
+  const unsigned char* benchmark_enabled;
+};
+
+static base::LazyInstance<BenchmarkEnabledInitializer>::Leaky
+  g_benchmark_enabled = LAZY_INSTANCE_INITIALIZER;
+
 }  // namespace
 
 namespace ui {
@@ -214,8 +227,8 @@ void LatencyInfo::AddLatencyNumberWithTimestamp(LatencyComponentType component,
                                                 base::TimeTicks time,
                                                 uint32 event_count) {
 
-  static const unsigned char* benchmark_enabled =
-      TRACE_EVENT_API_GET_CATEGORY_GROUP_ENABLED("benchmark");
+  const unsigned char* benchmark_enabled =
+      g_benchmark_enabled.Get().benchmark_enabled;
 
   if (IsBeginComponent(component)) {
     // Should only ever add begin component once.
