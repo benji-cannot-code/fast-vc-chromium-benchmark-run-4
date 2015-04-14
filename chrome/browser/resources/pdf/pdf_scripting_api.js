41b637e5e726751c40ac62fed1ce1905877ed424
raymes <raymes@chromium.org>
2015-04-13 17:44:44 -0700

    Revert of Use custom scrollbars for print preview on non-retina mac displays. (patchset #8 id:140001 of https://codereview.chromium.org/1036943004/)
    Reason for revert:
    The bug this hack is working around was fixed in   http://src.chromium.org/viewvc/blink?view=rev&rev=193593. This hack can now be reverted.
    
    Original issue's description:
    > Use custom scrollbars for print preview on non-retina mac displays.
    >
    > This is a hack to workaround a blink bug where overlay scrollbars aren't
    > rendered correctly in print preview. This bug is being looked at but
    > until then we use custom scrollbars that imitate mac scrollbars only
    > in this specific case.
    >
    > BUG=466039
    >
    > Committed: https://crrev.com/6826023abd24c8aa8981f4f0fadcf98f0f962bba
    > Cr-Commit-Position: refs/heads/master@{#322331}
    
    BUG=466039
    TBR=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1085483002
    
    Cr-Commit-Position: refs/heads/master@{#324965}

--- chrome/browser/resources/pdf/pdf_scripting_api.js
+++ chrome/browser/resources/pdf/pdf_scripting_api.js
@@ -251,8 +251,6 @@ PDFScriptingAPI.prototype = {
   },
 };
 
-var IS_MAC_PARAM = 'isMac&';
-
 /**
  * Creates a PDF viewer with a scripting interface. This is basically 1) an
  * iframe which is navigated to the PDF viewer extension and 2) a scripting
@@ -263,11 +261,9 @@ var IS_MAC_PARAM = 'isMac&';
  */
 function PDFCreateOutOfProcessPlugin(src) {
   var iframe = window.document.createElement('iframe');
-  var isMac = cr.isMac ? IS_MAC_PARAM : '';
   iframe.setAttribute(
       'src',
-      'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/index.html?' +
-      isMac + src);
+      'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/index.html?' + src);
   // Prevent the frame from being tab-focusable.
   iframe.setAttribute('tabindex', '-1');
   var client = new PDFScriptingAPI(window);
