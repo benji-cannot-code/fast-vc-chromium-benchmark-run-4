b9714b43a41de82a2b1059f115fec7d894694c8e
hidehiko <hidehiko@chromium.org>
2015-04-13 21:59:21 -0700

    Non-SFI mode: Use syscall() for sandbox sanity check.
    Currently, ptrace() is used for sandbox sanity check, but
    the syscall wrapper is not provided by PNaCl toolchain for
    Non-SFI build. This CL replaces it with syscall().
    
    TEST=ran bots.
    BUG=358465
    
    Review URL: https://codereview.chromium.org/1051723004
    
    Cr-Commit-Position: refs/heads/master@{#325001}

--- components/nacl/loader/nonsfi/nonsfi_sandbox.cc
+++ components/nacl/loader/nonsfi/nonsfi_sandbox.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <linux/net.h>
 #include <sys/mman.h>
 #include <sys/prctl.h>
-#include <sys/ptrace.h>
 #include <sys/socket.h>
 #include <sys/syscall.h>
 #include <sys/time.h>
@@ -186,7 +185,8 @@ bool IsGracefullyDenied(int sysno) {
 void RunSandboxSanityChecks() {
   errno = 0;
   // Make a ptrace request with an invalid PID.
-  long ptrace_ret = ptrace(PTRACE_PEEKUSER, -1 /* pid */, NULL, NULL);
+  long ptrace_ret = syscall(
+      __NR_ptrace, 3 /* = PTRACE_PEEKUSER */, -1 /* pid */, NULL, NULL);
   CHECK_EQ(-1, ptrace_ret);
   // Without the sandbox on, this ptrace call would ESRCH instead.
   CHECK_EQ(EPERM, errno);
