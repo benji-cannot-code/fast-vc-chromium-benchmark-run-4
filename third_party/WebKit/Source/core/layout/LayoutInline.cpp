64e7916bae7490604d8d7e7dcd1175eaa853efae
myid.o.shin@gmail.com <myid.o.shin@gmail.com>
2015-04-14 19:49:11 +0000

    Reland "We need to account for culled inline parents of hit-tested nodes"
    There are two kind of problem.
    One is that we get the wrong hit-test result from culled inlines
    because we try to overrun checking them until top of parent.
    The other is that mouse events aren't fired when there is the
    element culled inline like label element that doesn't has any
    background color or border or self-layer.
    
    This patch makes hit-test include not only area-based hit-tests
    but also point-based ones at the place handling culled inline.
    
    More information: https://docs.google.com/presentation/d/1KlT_FQxCTGeH-lPWjzXnPdZ3Bb1p7GL-IwoP-SfGvEo/edit#slide=id.g535467cad_40
    
    BUG=312199, 435483
    R=pdr@chromium.org
    TEST=fast/events/hit-test-culled_inline.html
        =fast/dom/nodesFromRect/nodesFromRect-culled-inlines-between-silblings.html
    
    Review URL: https://codereview.chromium.org/947793002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutInline.cpp
+++ third_party/WebKit/Source/core/layout/LayoutInline.cpp
@@ -813,7 +813,7 @@ private:
 
 bool LayoutInline::hitTestCulledInline(HitTestResult& result, const HitTestLocation& locationInContainer, const LayoutPoint& accumulatedOffset)
 {
-    ASSERT(result.isRectBasedTest() && !alwaysCreateLineBoxes());
+    ASSERT(!alwaysCreateLineBoxes());
     if (!visibleToHitTestRequest(result.hitTestRequest()))
         return false;
 
