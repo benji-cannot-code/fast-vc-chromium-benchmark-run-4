2d7481c2d46887c0c757c99ffbe9adec915702ca
sadrul@chromium.org <sadrul@chromium.org>
2015-04-14 21:09:12 +0000

    layout: Move static LayoutBoxModelObject::s_wasFloating into the cpp
    Move LayoutBoxModelObject::s_wasFloating into the cpp file, and add a sanity
    check that the same LayoutBoxModelObject is used when setting/getting
    s_wasFloating.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1058033003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193733 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutBoxModelObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBoxModelObject.cpp
@@ -51,6 +51,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class FloatStateForStyleChange {
+public:
+    static void setWasFloating(LayoutBoxModelObject* boxModelObject, bool wasFloating)
+    {
+        s_wasFloating = wasFloating;
+        s_boxModelObject = boxModelObject;
+    }
+
+    static bool wasFloating(LayoutBoxModelObject* boxModelObject)
+    {
+        ASSERT(boxModelObject == s_boxModelObject);
+        return s_wasFloating;
+    }
+
+private:
+    // Used to store state between styleWillChange and styleDidChange
+    static bool s_wasFloating;
+    static LayoutBoxModelObject* s_boxModelObject;
+};
+
+bool FloatStateForStyleChange::s_wasFloating = false;
+LayoutBoxModelObject* FloatStateForStyleChange::s_boxModelObject = nullptr;
+
 // The HashMap for storing continuation pointers.
 // An inline can be split with blocks occuring in between the inline content.
 // When this occurs we need a pointer to the next object. We can basically be
@@ -60,8 +83,6 @@ namespace blink {
 // its continuation but the <b> will just have an inline as its continuation.
 typedef HashMap<const LayoutBoxModelObject*, LayoutBoxModelObject*> ContinuationMap;
 static ContinuationMap* continuationMap = nullptr;
-
-bool LayoutBoxModelObject::s_wasFloating = false;
 
 void LayoutBoxModelObject::setSelectionState(SelectionState state)
 {
@@ -145,7 +166,7 @@ void LayoutBoxModelObject::styleWillChange(StyleDifference diff, const ComputedS
         invalidatePaintIncludingNonCompositingDescendants();
     }
 
-    s_wasFloating = isFloating();
+    FloatStateForStyleChange::setWasFloating(this, isFloating());
 
     if (const ComputedStyle* oldStyle = style()) {
         if (parent() && diff.needsPaintInvalidationLayer()) {
@@ -163,6 +184,7 @@ void LayoutBoxModelObject::styleDidChange(StyleDifference diff, const ComputedSt
     bool hadTransform = hasTransformRelatedProperty();
     bool hadLayer = hasLayer();
     bool layerWasSelfPainting = hadLayer && layer()->isSelfPaintingLayer();
+    bool wasFloatingBeforeStyleChanged = FloatStateForStyleChange::wasFloating(this);
 
     LayoutObject::styleDidChange(diff, oldStyle);
     updateFromStyle();
@@ -170,7 +192,7 @@ void LayoutBoxModelObject::styleDidChange(StyleDifference diff, const ComputedSt
     DeprecatedPaintLayerType type = layerTypeRequired();
     if (type != NoDeprecatedPaintLayer) {
         if (!layer() && layerCreationAllowedForSubtree()) {
-            if (s_wasFloating && isFloating())
+            if (wasFloatingBeforeStyleChanged && isFloating())
                 setChildNeedsLayout();
             createLayer(type);
             if (parent() && !needsLayout()) {
@@ -183,7 +205,7 @@ void LayoutBoxModelObject::styleDidChange(StyleDifference diff, const ComputedSt
         setHasTransformRelatedProperty(false); // Either a transform wasn't specified or the object doesn't support transforms, so just null out the bit.
         setHasReflection(false);
         layer()->removeOnlyThisLayer(); // calls destroyLayer() which clears m_layer
-        if (s_wasFloating && isFloating())
+        if (wasFloatingBeforeStyleChanged && isFloating())
             setChildNeedsLayout();
         if (hadTransform)
             setNeedsLayoutAndPrefWidthsRecalcAndFullPaintInvalidation(LayoutInvalidationReason::StyleChange);
