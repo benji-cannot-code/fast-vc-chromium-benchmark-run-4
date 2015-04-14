2e85b229c9472c716eccf6cfdee8d7c400e79216
bajones@chromium.org <bajones@chromium.org>
2015-04-14 20:39:19 +0000

    Renamed WebVR runtime feature from "VRDevice" to "WebVR"
    Made in response to suggestions on review of the Chrome-side patch
    (https://codereview.chromium.org/829803003/)
    
    BUG=389343
    
    Review URL: https://codereview.chromium.org/1079943007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193732 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
+++ third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
@@ -135,7 +135,7 @@ void FrameLoaderClientImpl::dispatchDidClearWindowObjectInMainWorld()
             if (RuntimeEnabledFeatures::serviceWorkerEnabled())
                 NavigatorServiceWorker::from(*document);
             DOMWindowStorageController::from(*document);
-            if (RuntimeEnabledFeatures::vRDeviceEnabled())
+            if (RuntimeEnabledFeatures::webVREnabled())
                 NavigatorVRDevice::from(*document);
         }
     }
