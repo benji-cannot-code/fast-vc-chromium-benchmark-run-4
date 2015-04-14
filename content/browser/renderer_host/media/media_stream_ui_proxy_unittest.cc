a8a26c5ef45b6694a5ae1513e8837d0d3a0f4bcf
wjmaclean <wjmaclean@chromium.org>
2015-04-14 10:10:38 -0700

    Remove unused include in media_stream_ui_proxy_unittest.cc
    BUG=
    
    Review URL: https://codereview.chromium.org/1077373004
    
    Cr-Commit-Position: refs/heads/master@{#325073}

--- content/browser/renderer_host/media/media_stream_ui_proxy_unittest.cc
+++ content/browser/renderer_host/media/media_stream_ui_proxy_unittest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/message_loop/message_loop.h"
 #include "content/browser/frame_host/render_frame_host_delegate.h"
-#include "content/public/common/renderer_preferences.h"
 #include "content/public/test/test_browser_thread.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
