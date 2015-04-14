a68030520e9874d721a38d83897d227e1de9d49e
bratell <bratell@opera.com>
2015-04-14 14:44:34 -0700

    Move an include guard to guard all code in base/move.h
    gcc -H warned about base/move.h missing an include guard and it was
    because the existing include guard didn't encompass all the code in the
    file.
    
    This fixes that warning and maybe makes compilations one nanosecond
    faster.
    
    The code outside the include guard was added Oct 2014 when fixing bug
    418297.
    
    BUG=418297
    
    Review URL: https://codereview.chromium.org/1082263002
    
    Cr-Commit-Position: refs/heads/master@{#325132}

--- base/move.h
+++ base/move.h
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/compiler_specific.h"
-
 #ifndef BASE_MOVE_H_
 #define BASE_MOVE_H_
+
+#include "base/compiler_specific.h"
 
 // Macro with the boilerplate that makes a type move-only in C++03.
 //
