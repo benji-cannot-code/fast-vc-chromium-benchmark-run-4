4d3507a8051bd18b1e566875c16746edf1bb9f88
bnc <bnc@chromium.org>
2015-04-14 10:42:08 -0700

    * Add brokenness info to NetLog (this fixes a regression due to refactoring). * Change alternative service formatting from "(proto, host, port)" to "proto host:port". * Mark HttpServerProperties::IsAlternativeServiceBroken a const method, so that the handle net_log_util.cc::GetNetInfo uses can stay const.
    BUG=476068
    
    Review URL: https://codereview.chromium.org/1075383003
    
    Cr-Commit-Position: refs/heads/master@{#325084}

--- net/http/http_server_properties.cc
+++ net/http/http_server_properties.cc
@@ -102,7 +102,7 @@ AlternateProtocol AlternateProtocolFromNextProto(NextProto next_proto) {
 }
 
 std::string AlternativeService::ToString() const {
-  return base::StringPrintf("(%s, %s, %d)", AlternateProtocolToString(protocol),
+  return base::StringPrintf("%s %s:%d", AlternateProtocolToString(protocol),
                             host.c_str(), port);
 }
 
--- net/http/http_server_properties_manager.cc
+++ net/http/http_server_properties_manager.cc
@@ -216,7 +216,7 @@ void HttpServerPropertiesManager::MarkAlternativeServiceRecentlyBroken(
 }
 
 bool HttpServerPropertiesManager::IsAlternativeServiceBroken(
-    const AlternativeService& alternative_service) {
+    const AlternativeService& alternative_service) const {
   DCHECK(network_task_runner_->RunsTasksOnCurrentThread());
   return http_server_properties_impl_->IsAlternativeServiceBroken(
       alternative_service);
@@ -247,6 +247,12 @@ const AlternativeServiceMap&
 HttpServerPropertiesManager::alternative_service_map() const {
   DCHECK(network_task_runner_->RunsTasksOnCurrentThread());
   return http_server_properties_impl_->alternative_service_map();
+}
+
+base::Value* HttpServerPropertiesManager::GetAlternativeServiceInfoAsValue()
+    const {
+  DCHECK(network_task_runner_->RunsTasksOnCurrentThread());
+  return http_server_properties_impl_->GetAlternativeServiceInfoAsValue();
 }
 
 void HttpServerPropertiesManager::SetAlternateProtocolProbabilityThreshold(
