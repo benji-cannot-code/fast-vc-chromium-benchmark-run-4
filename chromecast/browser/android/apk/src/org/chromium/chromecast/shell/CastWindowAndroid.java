6ed21dff9ca83ec4456cf6f38ee8595d4c741de7
derekjchow <derekjchow@chromium.org>
2015-04-14 14:41:48 -0700

    [Chromecast] Expose WebContents in CastWindowAndroid.
    Add getWebContents() to CastWindowAndroid. This allows adding other
    WebContentObservers.
    
    R=gunsch@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1068573005
    
    Cr-Commit-Position: refs/heads/master@{#325130}

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWindowAndroid.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWindowAndroid.java
@@ -168,4 +168,11 @@ public class CastWindowAndroid extends LinearLayout {
     public ContentViewCore getContentViewCore() {
         return mContentViewCore;
     }
+
+    /**
+     * @return The {@link WebContents} managed by this class.
+     */
+    public WebContents getWebContents() {
+        return mWebContents;
+    }
 }
