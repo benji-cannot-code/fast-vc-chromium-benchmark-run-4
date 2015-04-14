6e974ed6077590807a89fe42e81f417e3f9d0afa
mlamouri@chromium.org <mlamouri@chromium.org>
2015-04-14 09:47:03 +0000

    Revert of [DevTools] Move to Network.{getCookies, deleteCookie}. (patchset #2 id:20001 of https://codereview.chromium.org/1079863002/)
    Reason for revert:
    This is breaking telemetry_unittests, see:
    https://build.chromium.org/p/chromium.webkit/builders/Mac10.6%20Tests
    https://build.chromium.org/p/chromium.webkit/builders/Win7%20(dbg)
    https://build.chromium.org/p/chromium.webkit/builders/Win7%20Tests
    https://build.chromium.org/p/chromium.webkit/builders/Mac10.8%20Tests
    
    Original issue's description:
    > [DevTools] Move to Network.{getCookies, deleteCookie}.
    > 
    > Also cleanup old implementation in page domain.
    > 
    > BUG=476389
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=193652
    
    TBR=pfeldman@chromium.org,dgozman@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=476389
    
    Review URL: https://codereview.chromium.org/1077873005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193679 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/CookieParser.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/CookieParser.js
@@ -371,7 +371,7 @@ WebInspector.Cookie.prototype = {
      */
     remove: function(callback)
     {
-        this._target.networkAgent().deleteCookie(this.name(), (this.secure() ? "https://" : "http://") + this.domain() + this.path(), callback);
+        this._target.pageAgent().deleteCookie(this.name(), (this.secure() ? "https://" : "http://") + this.domain() + this.path(), callback);
     }
 }
 
@@ -394,7 +394,7 @@ WebInspector.Cookies.getCookiesAsync = function(callback)
     /**
      * @param {!WebInspector.Target} target
      * @param {?Protocol.Error} error
-     * @param {!Array.<!NetworkAgent.Cookie>} cookies
+     * @param {!Array.<!PageAgent.Cookie>} cookies
      */
     function mycallback(target, error, cookies)
     {
@@ -408,13 +408,13 @@ WebInspector.Cookies.getCookiesAsync = function(callback)
 
     var barrier = new CallbackBarrier();
     for (var target of WebInspector.targetManager.targets())
-        target.networkAgent().getCookies(barrier.createCallback(mycallback.bind(null, target)));
+        target.pageAgent().getCookies(barrier.createCallback(mycallback.bind(null, target)));
     barrier.callWhenDone(callback.bind(null, allCookies));
 }
 
 /**
  * @param {!WebInspector.Target} target
- * @param {!NetworkAgent.Cookie} protocolCookie
+ * @param {!PageAgent.Cookie} protocolCookie
  * @return {!WebInspector.Cookie}
  */
 WebInspector.Cookies._parseProtocolCookie = function(target, protocolCookie)
