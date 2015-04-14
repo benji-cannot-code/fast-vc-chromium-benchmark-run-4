ce8047ca3792e1ca114ec58ede4657d17dc6021d
ianwen <ianwen@chromium.org>
2015-04-14 13:53:02 -0700

    Make it possible to set porterduff mode to TintedImageButton
    The 3-dot button't tinting requires SRC_MULTIPLY mode when tinting the
    image button, in order to keep the original shadow. However the default
    mode is SRC_IN. We should open up API to set different color filter
    modes.
    
    BUG=476729
    
    Review URL: https://codereview.chromium.org/1087903002
    
    Cr-Commit-Position: refs/heads/master@{#325115}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/TintedImageButton.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/TintedImageButton.java
@@ -20,6 +20,7 @@ import org.chromium.chrome.R;
  */
 public class TintedImageButton extends ImageButton {
     private ColorStateList mTint;
+    private PorterDuff.Mode mColorFilterMode = PorterDuff.Mode.SRC_IN;
 
     public TintedImageButton(Context context) {
         super(context);
@@ -58,12 +59,22 @@ public class TintedImageButton extends ImageButton {
         updateTintColor();
     }
 
+    /**
+     * Sets the Porter-Duff mode to use when tinting the image.
+     * @param mode The porterduff mode to use to set color filter.
+     */
+    public void setColorFilterMode(PorterDuff.Mode mode) {
+        if (mColorFilterMode == mode) return;
+        mColorFilterMode = mode;
+        updateTintColor();
+    }
+
     private void setTintInternal(ColorStateList tint) {
         mTint = tint;
     }
 
     private void updateTintColor() {
         if (mTint == null) return;
-        setColorFilter(mTint.getColorForState(getDrawableState(), 0), PorterDuff.Mode.SRC_IN);
+        setColorFilter(mTint.getColorForState(getDrawableState(), 0), mColorFilterMode);
     }
 }
