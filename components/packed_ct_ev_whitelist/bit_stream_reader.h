4873be37660565066c1deb4c4ab44529f32dc65d
ian <ian@chromium.org>
2015-04-15 12:14:57 -0700

    Optimize BitStreamReader, speed up EV Whitelist loading
    The EV Certificate Whitelist loads ~1MB of data using BitStreamReader.
    Currently this is read in one bit at a time. Changing BitStreamReader to
    support byte reads sped up loading by about 65% on my computer.
    
    BUG=457949
    
    TEST=bit_stream_reader_unittest.cc
    
    Review URL: https://codereview.chromium.org/1070903003
    
    Cr-Commit-Position: refs/heads/master@{#325286}

--- components/packed_ct_ev_whitelist/bit_stream_reader.h
+++ components/packed_ct_ev_whitelist/bit_stream_reader.h
@@ -42,6 +42,11 @@ class BitStreamReader {
   // LSB.
   uint8_t ReadBit();
 
+  // Reads a single byte.
+  // Precondition: The stream must be byte-aligned (current_bit_ == 7) before
+  // calling this function.
+  uint8_t ReadByte();
+
   const base::StringPiece source_;
 
   // Index of the byte currently being read from.
