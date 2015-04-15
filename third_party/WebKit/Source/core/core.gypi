e993dc6a41f5d1ee66cd36394c3fbbe7567be035
mlamouri@chromium.org <mlamouri@chromium.org>
2015-04-15 09:25:34 +0000

    Revert of Don't keep kicking the "image animating" timer if the size is not changing. (patchset #3 id:40001 of https://codereview.chromium.org/1085943002/)
    Reason for revert:
    This is breaking Webkit Android bots, see:
    https://build.chromium.org/p/chromium.webkit/builders/Android%20Tests%20%28dbg%29/builds/25468
    
    Original issue's description:
    > Don't keep kicking the "image animating" timer if the size is not changing.
    > 
    > Added a test for this and a bunch of other functionality in ImageQualityController.
    > 
    > BUG=447293
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=193738
    
    TBR=pdr@chromium.org,chrishtr@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=447293
    
    Review URL: https://codereview.chromium.org/1088483004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193763 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/core.gypi
+++ third_party/WebKit/Source/core/core.gypi
@@ -3801,7 +3801,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'html/parser/HTMLSrcsetParserTest.cpp',
             'html/track/vtt/BufferedLineReaderTest.cpp',
             'html/track/vtt/VTTScannerTest.cpp',
-            'layout/ImageQualityControllerTest.cpp',
             'layout/LayoutBlockTest.cpp',
             'layout/LayoutInlineTest.cpp',
             'layout/LayoutMenuListTest.cpp',
--- third_party/WebKit/Source/core/layout/ImageQualityController.cpp
+++ third_party/WebKit/Source/core/layout/ImageQualityController.cpp
@@ -91,15 +91,10 @@ ImageQualityController::~ImageQualityController()
 }
 
 ImageQualityController::ImageQualityController()
-    : m_timer(adoptPtr(new Timer<ImageQualityController>(this, &ImageQualityController::highQualityRepaintTimerFired)))
+    : m_timer(this, &ImageQualityController::highQualityRepaintTimerFired)
     , m_animatedResizeIsActive(false)
     , m_liveResizeOptimizationIsActive(false)
 {
-}
-
-void ImageQualityController::setTimer(Timer<ImageQualityController>* newTimer)
-{
-    m_timer = adoptPtr(newTimer);
 }
 
 void ImageQualityController::removeLayer(LayoutObject* object, LayerSizeMap* innerMap, const void* layer)
@@ -127,7 +122,7 @@ void ImageQualityController::objectDestroyed(LayoutObject* object)
     m_objectLayerSizeMap.remove(object);
     if (m_objectLayerSizeMap.isEmpty()) {
         m_animatedResizeIsActive = false;
-        m_timer->stop();
+        m_timer.stop();
     }
 }
 
@@ -153,7 +148,7 @@ void ImageQualityController::highQualityRepaintTimerFired(Timer<ImageQualityCont
 
 void ImageQualityController::restartTimer()
 {
-    m_timer->startOneShot(cLowQualityTimeThreshold, FROM_HERE);
+    m_timer.startOneShot(cLowQualityTimeThreshold, FROM_HERE);
 }
 
 bool ImageQualityController::shouldPaintAtLowQuality(GraphicsContext* context, LayoutObject* object, Image* image, const void *layer, const LayoutSize& layoutSize)
@@ -221,8 +216,7 @@ bool ImageQualityController::shouldPaintAtLowQuality(GraphicsContext* context, L
     // If an animated resize is active, paint in low quality and kick the timer ahead.
     if (m_animatedResizeIsActive) {
         set(object, innerMap, layer, scaledLayoutSize);
-        if (oldSize != scaledLayoutSize)
-            restartTimer();
+        restartTimer();
         return true;
     }
     // If this is the first time resizing this image, or its size is the
@@ -235,11 +229,10 @@ bool ImageQualityController::shouldPaintAtLowQuality(GraphicsContext* context, L
     }
     // If the timer is no longer active, draw at high quality and don't
     // set the timer.
-    if (!m_timer->isActive()) {
+    if (!m_timer.isActive()) {
         removeLayer(object, innerMap, layer);
         return false;
     }
-
     // This object has been resized to two different sizes while the timer
     // is active, so draw at low quality, set the flag for animated resizes and
     // the object to the list for high quality redraw.
