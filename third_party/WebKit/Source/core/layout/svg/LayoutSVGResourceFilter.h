f11ed24cf4a45c6ec8b5b4fbfc6cb94b09edee58
pdr@chromium.org <pdr@chromium.org>
2015-04-15 05:49:16 +0000

    Fix feImage-self-and-other-referencing cycle with more state
    This patch adds a new filter state, RecordingContentCycleDetected, for
    when a filter tries to record when it is already recording. Similar to
    painting cycles, we do not need a "cycle counter" because cycles are
    stopped as soon as they are detected.
    
    This fixes an assert on the following test:
    virtual/slimmingpaint/svg/filters/feImage-self-and-other-referencing.html
    
    BUG=451606
    
    Review URL: https://codereview.chromium.org/1058013007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193750 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGResourceFilter.h
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGResourceFilter.h
@@ -40,13 +40,14 @@ public:
     /*
      * The state transitions should follow the following:
      * Initial -> RecordingContent -> ReadyToPaint -> PaintingFilter -> ReadyToPaint
-     *                                                   |     ^
-     *                                                   v     |
-     *                                              PaintingFilterCycle
+     *               |     ^                              |     ^
+     *               v     |                              v     |
+     *     RecordingContentCycleDetected            PaintingFilterCycle
      */
     enum FilterDataState {
         Initial,
         RecordingContent,
+        RecordingContentCycleDetected,
         ReadyToPaint,
         PaintingFilter,
         PaintingFilterCycleDetected
