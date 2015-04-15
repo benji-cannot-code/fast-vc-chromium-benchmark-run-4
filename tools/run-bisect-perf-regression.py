9fd3a928152881e7c0f1c71f63dfd26aefef572d
prasadv <prasadv@chromium.org>
2015-04-15 13:46:14 -0700

    Add tool/perf/measurements path to check for CQ jobs.
    BUG=462581
    
    Review URL: https://codereview.chromium.org/1061803004
    
    Cr-Commit-Position: refs/heads/master@{#325296}

--- tools/run-bisect-perf-regression.py
+++ tools/run-bisect-perf-regression.py
@@ -38,6 +38,7 @@ WEBKIT_RUN_TEST_CONFIG_PATH = os.path.join(
 BISECT_SCRIPT_DIR = os.path.join(SCRIPT_DIR, 'auto_bisect')
 
 PERF_BENCHMARKS_PATH = 'tools/perf/benchmarks'
+PERF_MEASUREMENTS_PATH = 'tools/perf/measurements'
 BUILDBOT_BUILDERNAME = 'BUILDBOT_BUILDERNAME'
 BENCHMARKS_JSON_FILE = 'benchmarks.json'
 
@@ -635,7 +636,8 @@ def _GetAffectedBenchmarkModuleNames():
   all_affected_files = _GetModifiedFilesFromPatch()
   modified_benchmarks = []
   for affected_file in all_affected_files:
-    if affected_file.startswith(PERF_BENCHMARKS_PATH):
+    if (affected_file.startswith(PERF_BENCHMARKS_PATH) or
+        affected_file.startswith(PERF_MEASUREMENTS_PATH)):
       benchmark = os.path.basename(os.path.splitext(affected_file)[0])
       modified_benchmarks.append(benchmark)
   return modified_benchmarks
