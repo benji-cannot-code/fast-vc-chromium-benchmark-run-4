b6455d2a50822368782daed534f3cdcac6d5f662
schenney@chromium.org <schenney@chromium.org>
2015-04-15 17:35:12 +0000

    Fix for layout during paint in WebPluginContainerImpl.
    After https://codereview.chromium.org/1076283002/, crashes
    were occuring and perf tests and in debug builds due to
    setNeedsLayout being called during paint. This patch prevents
    the paint invalidation that occurs during layout inside paint
    in WebViewPlugin.
    
    TBR=chrishtr@chromium.org
    BUG=477156,476990,457054
    
    Review URL: https://codereview.chromium.org/1084233002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193807 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebPluginContainerImpl.h
+++ third_party/WebKit/Source/web/WebPluginContainerImpl.h
@@ -208,6 +208,9 @@ private:
 
     IntRect m_pendingInvalidationRect;
 
+    // TODO(schenney) Needed while layout is still called during paint in some plugins
+    bool m_isInPaint;
+
     // The associated scrollbar group object, created lazily. Used for Pepper
     // scrollbars.
     OwnPtr<ScrollbarGroup> m_scrollbarGroup;
