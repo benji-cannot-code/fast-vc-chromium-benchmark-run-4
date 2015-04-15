9df400d66aba39e42422b90233231f4d20f09249
benjhayden@chromium.org <benjhayden@chromium.org>
2015-04-15 00:24:36 +0000

    Always use LayoutAnalyzer.
    Review URL: https://codereview.chromium.org/1080603003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193736 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutBlock.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlock.cpp
@@ -1622,9 +1622,7 @@ bool LayoutBlock::simplifiedLayout()
 
     clearNeedsLayout();
 
-    LayoutAnalyzer* analyzer = frameView()->layoutAnalyzer();
-    if (UNLIKELY(analyzer != nullptr))
-        analyzer->increment(LayoutAnalyzer::LayoutObjectsThatNeedSimplifiedLayout);
+    frameView()->layoutAnalyzer()->increment(LayoutAnalyzer::LayoutObjectsThatNeedSimplifiedLayout);
 
     return true;
 }
