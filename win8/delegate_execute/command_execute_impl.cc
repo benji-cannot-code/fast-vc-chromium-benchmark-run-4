295de721e0dddf58a7356a35c83084e2cce47663
erikwright <erikwright@chromium.org>
2015-04-15 12:13:25 -0700

    Remove a dead include.
    BUG=
    
    Review URL: https://codereview.chromium.org/1088743003
    
    Cr-Commit-Position: refs/heads/master@{#325285}

--- win8/delegate_execute/command_execute_impl.cc
+++ win8/delegate_execute/command_execute_impl.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/process/launch.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/win/message_window.h"
 #include "base/win/registry.h"
 #include "base/win/scoped_co_mem.h"
 #include "base/win/scoped_handle.h"
