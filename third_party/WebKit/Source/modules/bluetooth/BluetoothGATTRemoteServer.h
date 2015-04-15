98f68ac247fbcd4b0fba5b1ce91c09ac6c234db1
ortuno@chromium.org <ortuno@chromium.org>
2015-04-15 22:01:06 +0000

    bluetooth: Stub out BluetoothGattRemoteServer.
    BUG=476735
    
    Review URL: https://codereview.chromium.org/1052023004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193828 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/modules/bluetooth/BluetoothGATTRemoteServer.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BluetoothGATTRemoteServer_h
+#define BluetoothGATTRemoteServer_h
+
+#include "bindings/core/v8/ScriptWrappable.h"
+#include "platform/heap/Heap.h"
+
+namespace blink {
+
+// BluetoothGATTRemoteServer provides a way to interact with a connected bluetooth peripheral.
+class BluetoothGATTRemoteServer final
+    : public GarbageCollected<BluetoothGATTRemoteServer>
+    , public ScriptWrappable {
+    DEFINE_WRAPPERTYPEINFO();
+public:
+
+    // Interface required by Garbage Collectoin:
+    DEFINE_INLINE_TRACE() { }
+};
+
+} // namespace blink
+
+#endif // BluetoothDevice_h
