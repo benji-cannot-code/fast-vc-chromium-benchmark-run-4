abf121f7836b33422134f57308027fcc973c61b1
erikchen <erikchen@chromium.org>
2015-04-15 14:35:21 -0700

    Disable large profile tests for reference builds.
    The large profiles are typically too new to be used with the reference builds.
    
    BUG=476882
    
    Review URL: https://codereview.chromium.org/1090453002
    
    Cr-Commit-Position: refs/heads/master@{#325311}

--- tools/perf/benchmarks/startup.py
+++ tools/perf/benchmarks/startup.py
@@ -61,6 +61,7 @@ class StartupWarmBlankPage(_StartupWarm):
 
 @benchmark.Enabled('has tabs')
 @benchmark.Enabled('win', 'linux', 'mac')
+@benchmark.Disabled('reference')
 class StartupLargeProfileColdBlankPage(_StartupCold):
   """Measures cold startup time with a large profile."""
   tag = 'cold'
@@ -80,6 +81,7 @@ class StartupLargeProfileColdBlankPage(_StartupCold):
 
 @benchmark.Enabled('has tabs')
 @benchmark.Enabled('win', 'linux', 'mac')
+@benchmark.Disabled('reference')
 class StartupLargeProfileWarmBlankPage(_StartupWarm):
   """Measures warm startup time with a large profile."""
   tag = 'warm'
