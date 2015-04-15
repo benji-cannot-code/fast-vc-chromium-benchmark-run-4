c3f22d9cd7eb07f71efac50a8f86955b1efccd9d
jaekyun@chromium.org <jaekyun@chromium.org>
2015-04-15 21:10:38 +0000

    Record HasUserGesture before the value is invalidated
    When window.open is used to open a new window, we should record a user
    gesture bit inside createWindow(const String& urlString, ...) before
    the value is invalidated.
    
    BUG=477260
    
    Review URL: https://codereview.chromium.org/1072303004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193827 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/CreateWindow.cpp
+++ third_party/WebKit/Source/core/page/CreateWindow.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/page/FocusController.h"
 #include "core/page/Page.h"
 #include "core/page/WindowFeatures.h"
+#include "platform/UserGestureIndicator.h"
 #include "platform/network/ResourceRequest.h"
 #include "platform/weborigin/KURL.h"
 #include "platform/weborigin/SecurityOrigin.h"
@@ -147,6 +148,8 @@ LocalFrame* createWindow(const String& urlString, const AtomicString& frameName,
     // so we need to ensure the proper referrer is set now.
     frameRequest.resourceRequest().setHTTPReferrer(SecurityPolicy::generateReferrer(activeFrame->document()->referrerPolicy(), completedURL, activeFrame->document()->outgoingReferrer()));
 
+    bool hasUserGesture = UserGestureIndicator::processingUserGesture();
+
     // We pass the opener frame for the lookupFrame in case the active frame is different from
     // the opener frame, and the name references a frame relative to the opener frame.
     bool created;
@@ -159,10 +162,13 @@ LocalFrame* createWindow(const String& urlString, const AtomicString& frameName,
     if (newFrame->localDOMWindow()->isInsecureScriptAccess(callingWindow, completedURL))
         return newFrame;
 
-    if (created)
-        newFrame->loader().load(FrameLoadRequest(callingWindow.document(), completedURL));
-    else if (!urlString.isEmpty())
+    if (created) {
+        FrameLoadRequest request(callingWindow.document(), completedURL);
+        request.resourceRequest().setHasUserGesture(hasUserGesture);
+        newFrame->loader().load(request);
+    } else if (!urlString.isEmpty()) {
         newFrame->navigate(*callingWindow.document(), completedURL, false);
+    }
     return newFrame;
 }
 
