6a4e76e155e5726c4afaa49f92b69506bb94a446
limasdf <limasdf@gmail.com>
2015-04-16 09:38:28 -0700

    cleanup/ Remove unused forward declaration from app_window.h
    BUG=
    
    Review URL: https://codereview.chromium.org/1051253006
    
    Cr-Commit-Position: refs/heads/master@{#325455}

--- extensions/browser/app_window/app_window.h
+++ extensions/browser/app_window/app_window.h
@@ -34,19 +34,13 @@ class BrowserContext;
 class WebContents;
 }
 
-namespace ui {
-class BaseWindow;
-}
-
 namespace extensions {
 
 class AppDelegate;
 class AppWebContentsHelper;
 class Extension;
-class ExtensionRegistry;
 class NativeAppWindow;
 class PlatformAppBrowserTest;
-class WindowController;
 
 struct DraggableRegion;
 
