03757061c7be30a997cbf4d22dcfddd0f40e41dc
Daniel Cheng <dcheng@chromium.org>
2015-04-15 18:47:00 -0700

    Build the Clang plugin on Windows.
    This also includes several fixes for the plugin logic on Windows:
    - The banned directory check normalizes path separators.
    - The banned directory check now returns true for files that are
      considered system headers.
    - Diagnostics from the plugin are always reported as warnings.
    
    BUG=467287
    R=thakis@chromium.org
    
    Committed: https://chromium.googlesource.com/chromium/src/+/26ba172e8543b9029f320600f3aeced40028ff20
    
    Committed: https://chromium.googlesource.com/chromium/src/+/5b5d1269ade9eeb50875b20570ef3e0d8c5d95fb
    
    Review URL: https://codereview.chromium.org/1072203002
    
    Cr-Commit-Position: refs/heads/master@{#325373}

--- tools/clang/plugins/ChromeClassTester.cpp
+++ tools/clang/plugins/ChromeClassTester.cpp
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ChromeClassTester.h"
 
+#include <algorithm>
+
 #include "clang/AST/AST.h"
 #include "clang/Basic/FileManager.h"
 #include "clang/Basic/SourceManager.h"
@@ -233,6 +235,9 @@ std::string ChromeClassTester::GetNamespaceImpl(const DeclContext* context,
 }
 
 bool ChromeClassTester::InBannedDirectory(SourceLocation loc) {
+  if (instance().getSourceManager().isInSystemHeader(loc))
+    return true;
+
   std::string filename;
   if (!GetFilename(loc, &filename)) {
     // If the filename cannot be determined, simply treat this as a banned
@@ -251,7 +256,7 @@ bool ChromeClassTester::InBannedDirectory(SourceLocation loc) {
     return true;
   }
 
-#ifdef LLVM_ON_UNIX
+#if defined(LLVM_ON_UNIX)
   // We need to munge the paths so that they are relative to the repository
   // srcroot. We first resolve the symlinktastic relative path and then
   // remove our known srcroot from it if needed.
@@ -259,6 +264,10 @@ bool ChromeClassTester::InBannedDirectory(SourceLocation loc) {
   if (realpath(filename.c_str(), resolvedPath)) {
     filename = resolvedPath;
   }
+#endif
+
+#if defined(LLVM_ON_WIN32)
+  std::replace(filename.begin(), filename.end(), '\\', '/');
 #endif
 
   for (const std::string& banned_dir : banned_directories_) {
