728f0a707092cd66327dd65a7200b476e9659b2d
stevenjb <stevenjb@chromium.org>
2015-04-16 12:03:14 -0700

    Use networkingPrivate.startConnect (Take 2)
    This adds checks for unconfigured or non-activated networks to internet_details.js since it no longer uses the checks in network_connect.cc.
    It also moves some notification handling from network_connect.cc to network_state_notifier.cc so that it does not rely on network_connect.cc.
    
    Original CL (reverted for unrelated failure):
    https://codereview.chromium.org/1043343002/
    
    BUG=430115
    TBR=michaelpg, pneubeck, mpearson@chromium.org
    
    Review URL: https://codereview.chromium.org/1091043002
    
    Cr-Commit-Position: refs/heads/master@{#325484}

--- chrome/browser/chromeos/options/network_config_view.cc
+++ chrome/browser/chromeos/options/network_config_view.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/login/login_state.h"
 #include "chromeos/network/network_state.h"
 #include "chromeos/network/network_state_handler.h"
+#include "components/device_event_log/device_event_log.h"
 #include "components/user_manager/user.h"
 #include "ui/accessibility/ax_view_state.h"
 #include "ui/aura/window_event_dispatcher.h"
@@ -137,6 +138,7 @@ void NetworkConfigView::Show(const std::string& service_path,
     delete view;
     return;
   }
+  NET_LOG(USER) << "NetworkConfigView::Show: " << service_path;
   view->ShowDialog(parent);
 }
 
@@ -152,6 +154,7 @@ void NetworkConfigView::ShowForType(const std::string& type,
     delete view;
     return;
   }
+  NET_LOG(USER) << "NetworkConfigView::ShowForType: " << type;
   view->ShowDialog(parent);
 }
 
