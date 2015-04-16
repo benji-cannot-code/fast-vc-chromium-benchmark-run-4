d5aed00a812637b02724b688540b73c27b36665f
rouslan@chromium.org <rouslan@chromium.org>
2015-04-16 21:37:33 +0000

    IME composition is a user gesture.
    Treat IME composition as a user gesture.
    
    BUG=353001
    
    Review URL: https://codereview.chromium.org/1091573003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193906 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/WebViewTest.cpp
+++ third_party/WebKit/Source/web/tests/WebViewTest.cpp
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/paint/DeprecatedPaintLayer.h"
 #include "core/paint/DeprecatedPaintLayerPainter.h"
 #include "platform/KeyboardCodes.h"
+#include "platform/UserGestureIndicator.h"
 #include "platform/geometry/IntSize.h"
 #include "platform/graphics/Color.h"
 #include "platform/testing/URLTestHelpers.h"
@@ -1560,6 +1561,7 @@ class MockAutofillClient : public WebAutofillClient {
 public:
     MockAutofillClient()
         : m_ignoreTextChanges(false)
+        , m_textChangesFromUserGesture(0)
         , m_textChangesWhileIgnored(0)
         , m_textChangesWhileNotIgnored(0)
         , m_userGestureNotificationsCount(0) { }
@@ -1573,6 +1575,9 @@ public:
             ++m_textChangesWhileIgnored;
         else
             ++m_textChangesWhileNotIgnored;
+
+        if (UserGestureIndicator::processingUserGesture())
+            ++m_textChangesFromUserGesture;
     }
     virtual void firstUserGestureObserved() override { ++m_userGestureNotificationsCount; }
 
@@ -1582,12 +1587,14 @@ public:
         m_textChangesWhileNotIgnored = 0;
     }
 
+    int textChangesFromUserGesture() { return m_textChangesFromUserGesture; }
     int textChangesWhileIgnored() { return m_textChangesWhileIgnored; }
     int textChangesWhileNotIgnored() { return m_textChangesWhileNotIgnored; }
     int getUserGestureNotificationsCount() { return m_userGestureNotificationsCount; }
 
 private:
     bool m_ignoreTextChanges;
+    int m_textChangesFromUserGesture;
     int m_textChangesWhileIgnored;
     int m_textChangesWhileNotIgnored;
     int m_userGestureNotificationsCount;
@@ -2500,6 +2507,23 @@ TEST_F(WebViewTest, FirstUserGestureObservedGestureTap)
     EXPECT_TRUE(tapElementById(webView, WebInputEvent::GestureTap, WebString::fromUTF8("target")));
 
     EXPECT_EQ(1, client.getUserGestureNotificationsCount());
+    frame->setAutofillClient(0);
+}
+
+TEST_F(WebViewTest, CompositionIsUserGesture)
+{
+    URLTestHelpers::registerMockedURLFromBaseURL(WebString::fromUTF8(m_baseURL.c_str()), WebString::fromUTF8("input_field_populated.html"));
+    WebView* webView = m_webViewHelper.initializeAndLoad(m_baseURL + "input_field_populated.html");
+    WebLocalFrameImpl* frame = toWebLocalFrameImpl(webView->mainFrame());
+    MockAutofillClient client;
+    frame->setAutofillClient(&client);
+    webView->setInitialFocus(false);
+
+    EXPECT_TRUE(webView->setComposition(WebString::fromUTF8(std::string("hello").c_str()), WebVector<WebCompositionUnderline>(), 3, 3));
+    EXPECT_EQ(1, client.textChangesFromUserGesture());
+    EXPECT_FALSE(UserGestureIndicator::processingUserGesture());
+    EXPECT_TRUE(frame->hasMarkedText());
+
     frame->setAutofillClient(0);
 }
 
