4592d2365feb93d2857cad3a1425041d59420f2a
johnme <johnme@chromium.org>
2015-04-16 04:12:44 -0700

    Disable flaky ImeTest#testFinishComposingText
    BUG=477632
    TBR=aurimas@chromium.org,tdresser@chromium.org
    NOTRY=true
    NOTREECHECKS=true
    
    Review URL: https://codereview.chromium.org/1090893002
    
    Cr-Commit-Position: refs/heads/master@{#325414}

--- content/public/android/javatests/src/org/chromium/content/browser/input/ImeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/input/ImeTest.java
@@ -325,8 +325,12 @@ public class ImeTest extends ContentShellTestBase {
         assertWaitForKeyboardStatus(true);
     }
 
+    /*
     @SmallTest
     @Feature({"TextInput", "Main"})
+    http://crbug.com/477632
+    */
+    @DisabledTest
     public void testFinishComposingText() throws Throwable {
         DOMUtils.focusNode(mWebContents, "input_radio");
         assertWaitForKeyboardStatus(false);
