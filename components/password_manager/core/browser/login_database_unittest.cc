5f1f63d5fc45414b449f14c69ab3c0475dc72548
msramek <msramek@chromium.org>
2015-04-16 09:10:38 -0700

    Report the number of empty usernames without a nonempty pair.
    In the process of investigation of empty username occurences in the password manager, we suspect re-authentication forms to be a frequent cause. This metric will show us in how many cases this is not the cause.
    
    We interpret occurences of an empty and nonempty username pair on the same signon realm as a presence of both a first-time authentication and re-authentication form.
    
    We report the number of password forms with an empty username in the login database, for which there isn't another password form with a nonempty username on the same signon realm.
    
    Out of several possible approaches:
    a) SELECT ... WHERE NOT EXISTS
    b) SELECT DISTINCT ... FROM ... JOIN ... ON a.signon_realm=b.signon_realm
    c) SELECT DISTINCT ... FROM ... WHERE ... AND a.signon_realm=b.signon_realm
    d) SELECT, put into std::set<>, SELECT, iterate and search the set
    
    we implemented a), as manual testing on 1000 random samples seemed to be the fastest.
    
    BUG=456728
    
    Review URL: https://codereview.chromium.org/1087233002
    
    Cr-Commit-Position: refs/heads/master@{#325451}

--- components/password_manager/core/browser/login_database_unittest.cc
+++ components/password_manager/core/browser/login_database_unittest.cc
@@ -1056,12 +1056,25 @@ TEST_F(LoginDatabaseTest, ReportMetricsTest) {
   password_form.blacklisted_by_user = true;
   EXPECT_EQ(AddChangeForForm(password_form), db().AddLogin(password_form));
 
+  password_form.origin = GURL("http://sixth.example.com/");
+  password_form.signon_realm = "http://sixth.example.com/";
+  password_form.username_value = ASCIIToUTF16("");
+  password_form.password_value = ASCIIToUTF16("my_password");
+  password_form.blacklisted_by_user = false;
+  EXPECT_EQ(AddChangeForForm(password_form), db().AddLogin(password_form));
+
+  password_form.username_element = ASCIIToUTF16("some_other_input");
+  EXPECT_EQ(AddChangeForForm(password_form), db().AddLogin(password_form));
+
+  password_form.username_value = ASCIIToUTF16("my_username");
+  EXPECT_EQ(AddChangeForForm(password_form), db().AddLogin(password_form));
+
   base::HistogramTester histogram_tester;
   db().ReportMetrics("", false);
 
   histogram_tester.ExpectUniqueSample(
       "PasswordManager.TotalAccounts.UserCreated.WithoutCustomPassphrase",
-      4,
+      6,
       1);
   histogram_tester.ExpectBucketCount(
       "PasswordManager.AccountsPerSite.UserCreated.WithoutCustomPassphrase",
@@ -1070,7 +1083,7 @@ TEST_F(LoginDatabaseTest, ReportMetricsTest) {
   histogram_tester.ExpectBucketCount(
       "PasswordManager.AccountsPerSite.UserCreated.WithoutCustomPassphrase",
       2,
-      1);
+      2);
   histogram_tester.ExpectBucketCount(
       "PasswordManager.TimesPasswordUsed.UserCreated.WithoutCustomPassphrase",
       0,
@@ -1101,6 +1114,10 @@ TEST_F(LoginDatabaseTest, ReportMetricsTest) {
       1);
   histogram_tester.ExpectUniqueSample(
       "PasswordManager.EmptyUsernames.CountInDatabase",
+      3,
+      1);
+  histogram_tester.ExpectUniqueSample(
+      "PasswordManager.EmptyUsernames.WithoutCorrespondingNonempty",
       1,
       1);
 }
