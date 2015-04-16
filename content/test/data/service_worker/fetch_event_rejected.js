4aa63d8a313251e03c36b4cb6639b40d83dfda5c
ksakamoto <ksakamoto@chromium.org>
2015-04-15 19:53:38 -0700

    Add tests for console messages of waitUntil() and respondWith()
    This adds browser tests that verify console message logged when a
    promise passed to waitUntil() or respondWith() is rejected.
    
    These tests are here because console outputs from SWs aren't testable
    with layout tests (crbug.com/456708).
    
    BUG=359423
    
    Review URL: https://codereview.chromium.org/1081953004
    
    Cr-Commit-Position: refs/heads/master@{#325383}

--- content/test/data/service_worker/fetch_event_rejected.js
+++ content/test/data/service_worker/fetch_event_rejected.js
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-this.oninstall = function(event) {
-    event.waitUntil(new Promise(function(resolve, reject) {
-        setTimeout(function() { reject("Rejecting oninstall event"); }, 5);
+this.onfetch = function(event) {
+    event.respondWith(new Promise(function(resolve, reject) {
+        setTimeout(function() { reject("Rejecting respondWith promise"); }, 5);
     }));
 };
