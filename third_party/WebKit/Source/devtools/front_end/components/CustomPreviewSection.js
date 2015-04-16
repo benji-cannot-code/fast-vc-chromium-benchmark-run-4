dee2b282e74d8d0249f6119bdbc224c022c3c04e
sergeyv@chromium.org <sergeyv@chromium.org>
2015-04-16 11:27:09 +0000

    DevTools:[CustomFormatters] Ignore nested jsonML in object tag
    As a drive by:
    1. forbid object tag in header
    2. generate proper header for objects without custom formatters
    
    Review URL: https://codereview.chromium.org/1080033003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193856 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/components/CustomPreviewSection.js
+++ third_party/WebKit/Source/devtools/front_end/components/CustomPreviewSection.js
@@ -6,21 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @constructor
  * @param {!WebInspector.RemoteObject} object
- * @param {!Array.<*>=} prefixML
  */
-WebInspector.CustomPreviewSection = function(object, prefixML)
+WebInspector.CustomPreviewSection = function(object)
 {
-    this._sectionElement = createElement("span");
+    this._sectionElement = createElementWithClass("span", "custom-expandable-section");
     this._object = object;
     this._expanded = false;
     this._cachedContent = null;
     var customPreview = object.customPreview();
-    if (customPreview.hasBody) {
-        this._sectionElement.classList.add("custom-expandable-section");
-        this._sectionElement.addEventListener("click", this._onClick.bind(this), false);
-    }
-    if (prefixML)
-        this._appendJsonMLTags(this._sectionElement, prefixML);
 
     try {
         var headerJSON = JSON.parse(customPreview.header);
@@ -28,8 +21,13 @@ WebInspector.CustomPreviewSection = function(object, prefixML)
         WebInspector.console.error("Broken formatter: header is invalid json " + e);
         return;
     }
-    var header = this._renderJSONMLTag(headerJSON);
-    this._sectionElement.appendChild(header);
+    this._header = this._renderJSONMLTag(headerJSON);
+    if (customPreview.hasBody) {
+        this._header.classList.add("custom-expandable-section-header");
+        this._header.addEventListener("click", this._onClick.bind(this), false);
+    }
+
+    this._sectionElement.appendChild(this._header);
 }
 
 /**
@@ -40,10 +38,21 @@ WebInspector.CustomPreviewSection = function(object, prefixML)
 WebInspector.CustomPreviewSection.createInShadow = function(object)
 {
     var customPreviewSection = new WebInspector.CustomPreviewSection(object);
-    var element = createElement("span");
+    var element = WebInspector.CustomPreviewSection._createComponentRoot();
     var shadowRoot = element.createShadowRoot();
     shadowRoot.appendChild(WebInspector.View.createStyleElement("components/customPreviewSection.css"));
     shadowRoot.appendChild(customPreviewSection.element());
+    return element;
+}
+
+/**
+ * @return {!Element}
+ */
+WebInspector.CustomPreviewSection._createComponentRoot = function()
+{
+    var element = createElement("span");
+    WebInspector.installComponentRootStyles(element);
+    element.classList.add("source-code");
     return element;
 }
 
@@ -157,15 +166,22 @@ WebInspector.CustomPreviewSection.prototype = {
         objectTag.shift();
         var attributes = objectTag.shift();
         var remoteObject = this._object.target().runtimeModel.createRemoteObject(/** @type {!RuntimeAgent.RemoteObject} */ (attributes));
-        if (!remoteObject.customPreview()) {
-            var header = createElement("span");
-            this._appendJsonMLTags(header, objectTag);
-            var objectPropertiesSection = new WebInspector.ObjectPropertiesSection(remoteObject, header);
-            return objectPropertiesSection.element;
-        }
+        if (remoteObject.customPreview())
+            return (new WebInspector.CustomPreviewSection(remoteObject)).element();
 
-        var customSection = new WebInspector.CustomPreviewSection(remoteObject, objectTag);
-        return customSection.element();
+        var header = createElement("span");
+        var componentRoot = WebInspector.CustomPreviewSection._createComponentRoot();
+        header.appendChild(componentRoot);
+        var shadowRoot = componentRoot.createShadowRoot();
+        shadowRoot.appendChild(WebInspector.View.createStyleElement("components/objectValue.css"));
+        shadowRoot.appendChild(WebInspector.ObjectPropertiesSection.createValueElement(remoteObject, false));
+        if (!remoteObject.hasChildren)
+            return header;
+
+        var objectPropertiesSection = new WebInspector.ObjectPropertiesSection(remoteObject, header);
+        var sectionElement = objectPropertiesSection.element;
+        sectionElement.classList.add("custom-expandable-section-standard-section");
+        return sectionElement;
     },
 
     /**
@@ -189,12 +205,8 @@ WebInspector.CustomPreviewSection.prototype = {
     _toggleExpand: function()
     {
         this._expanded = !this._expanded;
-        this._sectionElement.classList.toggle("expanded", this._expanded);
-        var parent = this._sectionElement.parentNode;
-        if (this._expanded)
-            parent.insertBefore(this._cachedContent, this._sectionElement.nextSibling);
-        else
-            parent.removeChild(this._cachedContent);
+        this._header.classList.toggle("expanded", this._expanded);
+        this._cachedContent.classList.toggle("hidden", !this._expanded);
     },
 
     _loadBody: function()
@@ -258,6 +270,7 @@ WebInspector.CustomPreviewSection.prototype = {
                 return;
 
             this._cachedContent = this._renderJSONMLTag(bodyJsonML);
+            this._sectionElement.appendChild(this._cachedContent);
             this._toggleExpand();
         }
     }
