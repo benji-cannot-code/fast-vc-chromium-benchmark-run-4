4d3b78cf578f1731b84de50811a25acc1f357997
skobes@chromium.org <skobes@chromium.org>
2015-04-16 20:05:54 +0000

    Clear FrameView::m_viewportRenderer when it is destroyed.
    BUG=464792
    TEST=verified with clusterfuzz test case
    
    Review URL: https://codereview.chromium.org/1070233006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193898 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.h
+++ third_party/WebKit/Source/core/frame/FrameView.h
@@ -275,7 +275,7 @@ public:
     bool isScrollable();
 
     enum ScrollbarModesCalculationStrategy { RulesFromWebContentOnly, AnyRule };
-    void calculateScrollbarModesForLayoutAndSetViewportRenderer(ScrollbarMode& hMode, ScrollbarMode& vMode, ScrollbarModesCalculationStrategy = AnyRule);
+    void calculateScrollbarModesForLayoutAndSetViewportLayoutObject(ScrollbarMode& hMode, ScrollbarMode& vMode, ScrollbarModesCalculationStrategy = AnyRule);
 
     virtual IntPoint lastKnownMousePosition() const override;
     bool shouldSetCursor() const;
@@ -577,6 +577,10 @@ public:
 
     LayoutAnalyzer& layoutAnalyzer() { return m_analyzer; }
 
+    // LayoutObject for the viewport-defining element (see Document::viewportDefiningElement).
+    LayoutObject* viewportLayoutObject() { return m_viewportLayoutObject; }
+    void clearViewportLayoutObject() { m_viewportLayoutObject = nullptr; }
+
 protected:
     // Scroll the content via the compositor.
     bool scrollContentsFastPath(const IntSize& scrollDelta);
@@ -638,7 +642,7 @@ private:
 
     bool contentsInCompositedLayer() const;
 
-    void applyOverflowToViewportAndSetRenderer(LayoutObject*, ScrollbarMode& hMode, ScrollbarMode& vMode);
+    void applyOverflowToViewportAndSetLayoutObject(LayoutObject*, ScrollbarMode& hMode, ScrollbarMode& vMode);
     void updateOverflowStatus(bool horizontalOverflow, bool verticalOverflow);
 
     void updateCounters();
@@ -769,7 +773,7 @@ private:
     bool m_overflowStatusDirty;
     bool m_horizontalOverflow;
     bool m_verticalOverflow;
-    LayoutObject* m_viewportRenderer;
+    LayoutObject* m_viewportLayoutObject;
 
     bool m_wasScrolledByUser;
     bool m_inProgrammaticScroll;
