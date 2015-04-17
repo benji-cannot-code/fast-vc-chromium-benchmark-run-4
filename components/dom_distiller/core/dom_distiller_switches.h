98c1ea27759a21749eb1c655535dd4a6c6393796
cjhopman <cjhopman@chromium.org>
2015-04-17 15:55:19 -0700

    Change reader mode heuristics to a single switch
    Currently, we have a switch for each different type of heuristic. This
    allows us to have a ForcedXxxxx experiment group for each different
    heuristic. But, we don't really care if a user is in ForcedAdaBoost vs
    ForcedOGArticle vs ForcedOtherThing.
    
    Instead, switch to using a single switch with different values to select
    the heuristic to use. This will also need a change to the finch config
    to have just a single "Forced" group that anything with an explicit
    heuristic set can be added to.
    
    BUG=471439
    
    Review URL: https://codereview.chromium.org/1084613004
    
    Cr-Commit-Position: refs/heads/master@{#325729}

--- components/dom_distiller/core/dom_distiller_switches.h
+++ components/dom_distiller/core/dom_distiller_switches.h
@@ -11,10 +11,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace switches {
 
-extern const char kEnableReaderModeOGArticleHeuristics[];
-extern const char kEnableReaderModeAdaBoostHeuristics[];
-extern const char kEnableReaderModeAlwaysTrueHeuristics[];
+// Switch to enable specific heuristics for detecting if a page is distillable
+// or not.
+extern const char kReaderModeHeuristics[];
 
+namespace reader_mode_heuristics {
+extern const char kAdaBoost[];
+extern const char kOGArticle[];
+extern const char kAlwaysTrue[];
+extern const char kNone[];
 };
+
+}  // namespace switches
 
 #endif  // COMPONENTS_DOM_DISTILLER_CORE_DOM_DISTILLER_SWITCHES_H_
