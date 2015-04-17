15dd00aa03b2b9e67bc80384197bbad3afea8631
melandory <melandory@chromium.org>
2015-04-17 05:38:45 -0700

    [Password manager tests automation] Adds an ability to specify test_case which should be run.
    This CL in a step in a direction of making it possible to run one test-case for all websites, then another one for all websites.
    
    BUG=369521
    R=vabr@chromium.org
    
    Review URL: https://codereview.chromium.org/1084553003
    
    Cr-Commit-Position: refs/heads/master@{#325621}

--- components/test/data/password_manager/automated_tests/environment.py
+++ components/test/data/password_manager/automated_tests/environment.py
@@ -262,7 +262,7 @@ class Environment:
 
     self._ClearDataForCheckbox("#delete-cookies-checkbox")
 
-  def RunTestsOnSites(self, test_type):
+  def RunTestsOnSites(self, test_case_name):
     """Runs the specified test on the known websites.
 
     Also saves the test results in the environment. Note that test types
@@ -272,7 +272,7 @@ class Environment:
     on construction.
 
     Args:
-      test_type: A test identifier understood by WebsiteTest.run_test().
+      test_case_name: A test name which is a method of WebsiteTest.
     """
 
     self.DeleteCookies()
@@ -283,12 +283,15 @@ class Environment:
       successful = True
       error = ""
       try:
-        websitetest.RunTest(test_type)
+        # TODO(melandory): Implement a decorator for WesiteTest methods
+        # which allows to mark them as test cases. And then add a check if
+        # test_case_name is a valid test case.
+        getattr(websitetest, test_case_name)()
       except Exception as e:
         successful = False
         error = e.message
       self.tests_results.append(
-          (websitetest.name, test_type, successful, error))
+          (websitetest.name, test_case_name, successful, error))
 
   def Quit(self):
     """Shuts down the driver."""
