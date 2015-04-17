b9ef649c9e16ff5d460345ea296da082dd71a28a
nednguyen <nednguyen@google.com>
2015-04-17 10:51:43 -0700

    [Telemetry] Switch discover.DiscoverClasses's index_by_class_name to True by default
    BUG=466804
    
    Review URL: https://codereview.chromium.org/1094723002
    
    Cr-Commit-Position: refs/heads/master@{#325669}

--- tools/perf/measurements/measurement_smoke_test.py
+++ tools/perf/measurements/measurement_smoke_test.py
@@ -2,8 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Measurement smoke test to make sure that no new action_name_to_run is
-defined."""
 
 import logging
 import optparse
@@ -11,6 +9,7 @@ import os
 import unittest
 
 from telemetry import benchmark as benchmark_module
+from telemetry.core import browser_options
 from telemetry.core import discover
 from telemetry.page import page_test
 from telemetry.unittest_util import options_for_unittests
@@ -43,9 +42,15 @@ def _GetAllPossiblePageTestInstances():
   for benchmark_class in all_benchmarks_classes:
     options = options_for_unittests.GetCopy()
     parser = optparse.OptionParser()
-    benchmark_class.AddCommandLineArgs(parser)
-    benchmark_module.AddCommandLineArgs(parser)
-    benchmark_class.SetArgumentDefaults(parser)
+    browser_options.BrowserOptions.AddCommandLineArgs(parser)
+    try:
+      benchmark_class.AddCommandLineArgs(parser)
+      benchmark_module.AddCommandLineArgs(parser)
+      benchmark_class.SetArgumentDefaults(parser)
+    except Exception:
+      logging.error('Exception raised when processing benchmark %s'
+          % benchmark_class)
+      raise
     options.MergeDefaultValues(parser.get_default_values())
     pt = benchmark_class().CreatePageTest(options)
     if not isinstance(pt, timeline_based_measurement.TimelineBasedMeasurement):
