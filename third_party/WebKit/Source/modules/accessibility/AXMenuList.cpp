d960c894bb622027edf238263776a51b886c8bb0
dmazzoni@chromium.org <dmazzoni@chromium.org>
2015-04-17 03:07:28 +0000

    Add missing null checks to AXMenuList.cpp
    Caught by a fuzz test
    
    BUG=476468
    
    Review URL: https://codereview.chromium.org/1091833002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193917 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/accessibility/AXMenuList.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXMenuList.cpp
@@ -53,6 +53,9 @@ AccessibilityRole AXMenuList::determineAccessibilityRole()
 
 bool AXMenuList::press() const
 {
+    if (!m_layoutObject)
+        return false;
+
     LayoutMenuList* menuList = toLayoutMenuList(m_layoutObject);
     if (menuList->popupIsVisible())
         menuList->hidePopup();
@@ -96,6 +99,11 @@ void AXMenuList::addChildren()
 
 bool AXMenuList::isCollapsed() const
 {
+    // Collapsed is the "default" state, so if the LayoutObject doesn't exist
+    // this makes slightly more sense than returning false.
+    if (!m_layoutObject)
+        return true;
+
     return !toLayoutMenuList(m_layoutObject)->popupIsVisible();
 }
 
