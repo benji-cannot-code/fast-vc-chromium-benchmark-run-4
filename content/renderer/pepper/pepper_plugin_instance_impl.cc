23f8cb5dadb0be40d6bd55d7129de8fe3a677d6b
bbudge <bbudge@chromium.org>
2015-04-16 17:12:05 -0700

    Changes PepperPluginInstanceImpl::IsRectTopmost to not use container_ if we are destroying the instance.
    BUG=476590
    
    Review URL: https://codereview.chromium.org/1085423004
    
    Cr-Commit-Position: refs/heads/master@{#325561}

--- content/renderer/pepper/pepper_plugin_instance_impl.cc
+++ content/renderer/pepper/pepper_plugin_instance_impl.cc
@@ -3136,6 +3136,9 @@ bool PepperPluginInstanceImpl::FlashSetFullscreen(bool fullscreen,
 }
 
 bool PepperPluginInstanceImpl::IsRectTopmost(const gfx::Rect& rect) {
+  if (is_deleted_)
+    return false;
+
   if (flash_fullscreen_)
     return true;
 
