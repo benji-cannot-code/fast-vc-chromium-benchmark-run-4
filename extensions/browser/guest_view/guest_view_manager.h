77b4d8794a994cadbc334f6108fa01c4fd9d8c1b
raymes <raymes@chromium.org>
2015-04-16 19:01:30 -0700

    Make Edit>Speech>Speak Selection work for PDFs on Mac
    This makes Speak Selection from the menu work for PDFs on Mac. It also
    simplifies the code slightly by creating a function which gets the "full page"
    guest if there is one.
    
    BUG=476787
    
    Review URL: https://codereview.chromium.org/1042743002
    
    Cr-Commit-Position: refs/heads/master@{#325584}

--- extensions/browser/guest_view/guest_view_manager.h
+++ extensions/browser/guest_view/guest_view_manager.h
@@ -93,6 +93,8 @@ class GuestViewManager : public content::BrowserPluginGuestManager,
       int element_instance_id) override;
   bool ForEachGuest(content::WebContents* owner_web_contents,
                     const GuestCallback& callback) override;
+  content::WebContents* GetFullPageGuest(
+      content::WebContents* embedder_web_contents) override;
 
  protected:
   friend class GuestViewBase;
@@ -124,6 +126,9 @@ class GuestViewManager : public content::BrowserPluginGuestManager,
   // We disallow adding new guest with instance IDs that were previously removed
   // from this manager using RemoveGuest.
   bool CanUseGuestInstanceID(int guest_instance_id);
+
+  static bool GetFullPageGuestHelper(content::WebContents** result,
+                                     content::WebContents* guest_web_contents);
 
   // Static factory instance (always NULL for non-test).
   static GuestViewManagerFactory* factory_;
