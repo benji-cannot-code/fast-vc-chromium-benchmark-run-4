9b8136aa51f899d869f9f8d50e3dac314a1ae793
bartfab <bartfab@chromium.org>
2015-04-17 02:53:31 -0700

    Disable flaky SelectFileDialogExtensionBrowserTest tests
    This CL disables 5 flaky SelectFileDialogExtensionBrowserTest test
    cases.
    
    BUG=477360
    TEST=Disabled flaky tests
    
    TBR=hirono
    
    Review URL: https://codereview.chromium.org/1083743004
    
    Cr-Commit-Position: refs/heads/master@{#325615}

--- chrome/browser/ui/views/select_file_dialog_extension_browsertest.cc
+++ chrome/browser/ui/views/select_file_dialog_extension_browsertest.cc
@@ -251,8 +251,14 @@ IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest, DestroyListener) {
 // page element, as that uses different memory management pathways.
 // crbug.com/98791
 
+// Flaky on Chrome OS, see: http://crbug.com/477360
+#if defined(OS_CHROMEOS)
+#define MAYBE_SelectFileAndCancel DISABLED_SelectFileAndCancel
+#else
+#define MAYBE_SelectFileAndCancel SelectFileAndCancel
+#endif
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
-                       SelectFileAndCancel) {
+                       MAYBE_SelectFileAndCancel) {
   AddMountPoint(downloads_dir_);
 
   gfx::NativeWindow owning_window = browser()->window()->GetNativeWindow();
@@ -270,8 +276,14 @@ IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
   ASSERT_EQ(this, listener_->params());
 }
 
+// Flaky on Chrome OS, see: http://crbug.com/477360
+#if defined(OS_CHROMEOS)
+#define MAYBE_SelectFileAndOpen DISABLED_SelectFileAndOpen
+#else
+#define MAYBE_SelectFileAndOpen SelectFileAndOpen
+#endif
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
-                       SelectFileAndOpen) {
+                       MAYBE_SelectFileAndOpen) {
   AddMountPoint(downloads_dir_);
 
   base::FilePath test_file =
@@ -301,8 +313,14 @@ IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
   ASSERT_EQ(this, listener_->params());
 }
 
+// Flaky on Chrome OS, see: http://crbug.com/477360
+#if defined(OS_CHROMEOS)
+#define MAYBE_SelectFileAndSave DISABLED_SelectFileAndSave
+#else
+#define MAYBE_SelectFileAndSave SelectFileAndSave
+#endif
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
-                       SelectFileAndSave) {
+                       MAYBE_SelectFileAndSave) {
   AddMountPoint(downloads_dir_);
 
   base::FilePath test_file =
@@ -327,8 +345,14 @@ IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
   ASSERT_EQ(this, listener_->params());
 }
 
+// Flaky on Chrome OS, see: http://crbug.com/477360
+#if defined(OS_CHROMEOS)
+#define MAYBE_OpenSingletonTabAndCancel DISABLED_OpenSingletonTabAndCancel
+#else
+#define MAYBE_OpenSingletonTabAndCancel OpenSingletonTabAndCancel
+#endif
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
-                       OpenSingletonTabAndCancel) {
+                       MAYBE_OpenSingletonTabAndCancel) {
   AddMountPoint(downloads_dir_);
 
   gfx::NativeWindow owning_window = browser()->window()->GetNativeWindow();
@@ -352,8 +376,14 @@ IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
   ASSERT_EQ(this, listener_->params());
 }
 
+// Flaky on Chrome OS, see: http://crbug.com/477360
+#if defined(OS_CHROMEOS)
+#define MAYBE_OpenTwoDialogs DISABLED_OpenTwoDialogs
+#else
+#define MAYBE_OpenTwoDialogs OpenTwoDialogs
+#endif
 IN_PROC_BROWSER_TEST_F(SelectFileDialogExtensionBrowserTest,
-                       OpenTwoDialogs) {
+                       MAYBE_OpenTwoDialogs) {
   AddMountPoint(downloads_dir_);
 
   gfx::NativeWindow owning_window = browser()->window()->GetNativeWindow();
