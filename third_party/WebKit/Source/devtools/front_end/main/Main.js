69876dcf51314aa3052b4dc100f1810c7b78a995
dgozman@chromium.org <dgozman@chromium.org>
2015-04-17 13:48:18 +0000

    [DevTools] Make OverridesSupport operate on a single target.
    BUG=none
    
    Review URL: https://codereview.chromium.org/1089933002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193945 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/main/Main.js
+++ third_party/WebKit/Source/devtools/front_end/main/Main.js
@@ -288,7 +288,6 @@ WebInspector.Main.prototype = {
 
         new WebInspector.WorkspaceController(WebInspector.workspace);
         new WebInspector.RenderingOptions();
-        new WebInspector.DisableJavaScriptObserver();
         new WebInspector.Main.PauseListener();
         new WebInspector.Main.InspectedNodeRevealer();
         WebInspector.domBreakpointsSidebarPane = new WebInspector.DOMBreakpointsSidebarPane();
@@ -396,9 +395,9 @@ WebInspector.Main.prototype = {
         if (this._mainTarget.isServiceWorker())
             this._mainTarget.runtimeAgent().run();
 
-        WebInspector.overridesSupport.applyInitialOverrides(overridesApplied);
+        WebInspector.overridesSupport.init(this._mainTarget, overridesReady);
 
-        function overridesApplied()
+        function overridesReady()
         {
             if (!WebInspector.overridesSupport.responsiveDesignAvailable() && WebInspector.overridesSupport.emulationEnabled())
                 WebInspector.inspectorView.showViewInDrawer("emulation", true);
@@ -902,47 +901,6 @@ WebInspector.WorkerTerminatedScreen = function()
 WebInspector.WorkerTerminatedScreen.prototype = {
 
     __proto__: WebInspector.HelpScreen.prototype
-}
-
-/**
- * @constructor
- * @implements {WebInspector.TargetManager.Observer}
- */
-WebInspector.DisableJavaScriptObserver = function()
-{
-    this._setting = WebInspector.moduleSetting("javaScriptDisabled");
-    this._setting.addChangeListener(this._settingChanged, this);
-    WebInspector.targetManager.observeTargets(this, WebInspector.Target.Type.Page);
-}
-
-WebInspector.DisableJavaScriptObserver.prototype = {
-    /**
-     * @override
-     * @param {!WebInspector.Target} target
-     */
-    targetAdded: function(target)
-    {
-        target.emulationAgent().setScriptExecutionDisabled(this._setting.get());
-    },
-
-    /**
-     * @override
-     * @param {!WebInspector.Target} target
-     */
-    targetRemoved: function(target)
-    {
-    },
-
-    /**
-     * @param {!WebInspector.Event} event
-     */
-    _settingChanged: function(event)
-    {
-        var value = this._setting.get();
-        var targets = WebInspector.OverridesSupport.targetsSupportingEmulation();
-        for (var i = 0; i < targets.length; ++i)
-            targets[i].emulationAgent().setScriptExecutionDisabled(value);
-    }
 }
 
 new WebInspector.Main();
