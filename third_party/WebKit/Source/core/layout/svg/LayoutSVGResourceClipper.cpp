e8d7ab0463c6068516a574cc8bf9364dbcb18e9c
pdr@chromium.org <pdr@chromium.org>
2015-04-17 05:39:58 +0000

    Add PictureRecorder for sp/non-sp recording without an existing context
    This patch adds a PictureRecorder for recording when there is no
    existing graphics context. For slimming paint, this patch does not
    change behavior. For non-slimming paint this patch now creates a
    graphics context using GC::deprecatedCreateWithCanvas which prevents
    crashes seen in https://codereview.chromium.org/1087633002.
    
    BUG=470553
    
    Review URL: https://codereview.chromium.org/1089293002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193923 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGResourceClipper.cpp
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGResourceClipper.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/paint/CompositingDisplayItem.h"
 #include "platform/graphics/paint/DisplayItemList.h"
 #include "platform/graphics/paint/DrawingDisplayItem.h"
+#include "platform/graphics/paint/SkPictureBuilder.h"
 #include "third_party/skia/include/core/SkPicture.h"
 
 namespace blink {
@@ -155,11 +156,7 @@ PassRefPtr<const SkPicture> LayoutSVGResourceClipper::createContentPicture(Affin
     // userSpaceOnUse units (http://crbug.com/294900).
     FloatRect bounds = strokeBoundingBox();
 
-    OwnPtr<DisplayItemList> displayItemList;
-    if (RuntimeEnabledFeatures::slimmingPaintEnabled())
-        displayItemList = DisplayItemList::create();
-    GraphicsContext context(displayItemList.get());
-    context.beginRecording(bounds);
+    SkPictureBuilder pictureBuilder(bounds);
 
     for (SVGElement* childElement = Traversal<SVGElement>::firstChild(*element()); childElement; childElement = Traversal<SVGElement>::nextSibling(*childElement)) {
         LayoutObject* layoutObject = childElement->layoutObject();
@@ -189,13 +186,11 @@ PassRefPtr<const SkPicture> LayoutSVGResourceClipper::createContentPicture(Affin
         // - masker/filter not applied when laying out the children
         // - fill is set to the initial fill paint server (solid, black)
         // - stroke is set to the initial stroke paint server (none)
-        PaintInfo info(&context, LayoutRect::infiniteIntRect(), PaintPhaseForeground, PaintBehaviorRenderingClipPathAsMask);
+        PaintInfo info(&pictureBuilder.context(), LayoutRect::infiniteIntRect(), PaintPhaseForeground, PaintBehaviorRenderingClipPathAsMask);
         layoutObject->paint(info, IntPoint());
     }
 
-    if (displayItemList)
-        displayItemList->commitNewDisplayItemsAndReplay(context);
-    m_clipContentPicture = context.endRecording();
+    m_clipContentPicture = pictureBuilder.endRecording();
     return m_clipContentPicture;
 }
 
--- third_party/WebKit/Source/core/layout/svg/LayoutSVGResourceMasker.cpp
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGResourceMasker.cpp
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/svg/SVGLayoutSupport.h"
 #include "core/paint/SVGPaintContext.h"
 #include "core/svg/SVGElement.h"
-#include "platform/graphics/paint/DisplayItemList.h"
+#include "platform/graphics/paint/SkPictureBuilder.h"
 #include "platform/transforms/AffineTransform.h"
 #include "third_party/skia/include/core/SkPicture.h"
 
@@ -71,15 +71,11 @@ PassRefPtr<const SkPicture> LayoutSVGResourceMasker::createContentPicture(Affine
     // userSpaceOnUse units (http://crbug.com/294900).
     FloatRect bounds = strokeBoundingBox();
 
-    OwnPtr<DisplayItemList> displayItemList;
-    if (RuntimeEnabledFeatures::slimmingPaintEnabled())
-        displayItemList = DisplayItemList::create();
-    GraphicsContext context(displayItemList.get());
-    context.beginRecording(bounds);
+    SkPictureBuilder pictureBuilder(bounds);
 
     ColorFilter maskContentFilter = style()->svgStyle().colorInterpolation() == CI_LINEARRGB
         ? ColorFilterSRGBToLinearRGB : ColorFilterNone;
-    context.setColorFilter(maskContentFilter);
+    pictureBuilder.context().setColorFilter(maskContentFilter);
 
     for (SVGElement* childElement = Traversal<SVGElement>::firstChild(*element()); childElement; childElement = Traversal<SVGElement>::nextSibling(*childElement)) {
         LayoutObject* layoutObject = childElement->layoutObject();
@@ -89,12 +85,10 @@ PassRefPtr<const SkPicture> LayoutSVGResourceMasker::createContentPicture(Affine
         if (!style || style->display() == NONE || style->visibility() != VISIBLE)
             continue;
 
-        SVGPaintContext::paintSubtree(&context, layoutObject);
+        SVGPaintContext::paintSubtree(&pictureBuilder.context(), layoutObject);
     }
 
-    if (displayItemList)
-        displayItemList->commitNewDisplayItemsAndReplay(context);
-    m_maskContentPicture = context.endRecording();
+    m_maskContentPicture = pictureBuilder.endRecording();
     return m_maskContentPicture;
 }
 
