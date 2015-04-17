4aca04fc3eb29d95ac974cf7807bfe428c282044
caseq@chromium.org <caseq@chromium.org>
2015-04-17 17:52:11 +0000

    DevTools: make DOMNodeIds a trivial wrapper around WeakIdentifierMap
    WeakIdentifierMap is intended for mapping between objects and their identifiers, and
    there's currently no reason to generate ids externally, so:
    
    - move id allocation/assignment to WeakIdentifierMap;
    - implement Oilpan version of WeakIdentifierMap;
    - move DOMNodeIds a trivial wrapper around WeakIdentifierMap;
    - get rid of WeakNodeMap header, move the definition to DOMNodeIds.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1089933004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193960 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/core.gypi
+++ third_party/WebKit/Source/core/core.gypi
@@ -2352,7 +2352,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'dom/VisitedLinkState.cpp',
             'dom/VisitedLinkState.h',
             'dom/WeakIdentifierMap.h',
-            'dom/WeakNodeMap.h',
             'dom/XMLDocument.cpp',
             'dom/XMLDocument.h',
             'dom/custom/CustomElement.cpp',
--- third_party/WebKit/Source/core/dom/DOMNodeIds.h
+++ third_party/WebKit/Source/core/dom/DOMNodeIds.h
@@ -7,10 +7,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DOMNodeIds_h
 
 #include "core/CoreExport.h"
+#include "core/dom/WeakIdentifierMap.h"
 
 namespace blink {
 
 class Node;
+
+#if !ENABLE(OILPAN)
+template<> struct WeakIdentifierMapTraits<Node> {
+    static void removedFromIdentifierMap(Node*);
+    static void addedToIdentifierMap(Node*);
+};
+#endif
+
+typedef WeakIdentifierMap<Node> WeakNodeMap;
 
 class CORE_EXPORT DOMNodeIds {
 public:
