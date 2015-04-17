cf4373bbd99e8f40b34490046c01312436cdcc36
johnme <johnme@chromium.org>
2015-04-17 09:08:28 -0700

    Revert of Introduce sys_sigprocmask and sys_sigaction. (patchset #5 id:160001 of https://codereview.chromium.org/1077143002/)
    Reason for revert:
    Tentative revert to try to fix all the failing tests on https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20MSAN/builds/2548 with stderrs like:
    
    ==22726== WARNING: MemorySanitizer: use-of-uninitialized-value
        #0 0x7fb677cf3d50 in sandbox::Trap::SigSys(int, siginfo*, void*) sandbox/linux/seccomp-bpf/trap.cc:140:58
        #1 0x7fb6744e3fcf in sandbox::sys_sigaction(int, sigaction const*, sigaction*) ???:0
    
      Uninitialized value was created by an allocation of 'temp.lvalue49' in the stack frame of function '_ZN7sandbox7CodeGen6AppendEtjmm'
        #0 0x7fb677cf82b0 in sandbox::CodeGen::Append(unsigned short, unsigned int, unsigned long, unsigned long) sandbox/linux/bpf_dsl/codegen.cc:121:0
    
    SUMMARY: MemorySanitizer: use-of-uninitialized-value (/b/build/slave/WebKit_Linux_MSAN/build/src/out/Release/content_shell+0xef94d50)
    Exiting
    [22595:22595:0417/081926:1015885843:ERROR:command_buffer_proxy_impl.cc(166)] Could not send GpuCommandBufferMsg_Initialize.
    [22595:22595:0417/081926:1015885969:ERROR:webgraphicscontext3d_command_buffer_impl.cc(209)] CommandBufferProxy::Initialize failed.
    [4:4:0417/081926:1015885973:ERROR:command_buffer_proxy_impl.cc(166)] Could not send GpuCommandBufferMsg_Initialize.
    [22595:22595:0417/081926:1015886031:ERROR:webgraphicscontext3d_command_buffer_impl.cc(226)] Failed to initialize command buffer.
    [4:4:0417/081926:1015886209:ERROR:webgraphicscontext3d_command_buffer_impl.cc(209)] CommandBufferProxy::Initialize failed.
    [4:4:0417/081926:1015886330:ERROR:webgraphicscontext3d_command_buffer_impl.cc(226)] Failed to initialize command buffer.
    
    Original issue's description:
    > Introduce sys_sigprocmask and sys_sigaction.
    >
    > This is preparation of nacl_helper_nonsfi's secompbpf sandbox
    > implementation.
    > PNaCl toolchain's signal ABI is incompatible with Linux's.
    > For example, the length of sigset_t is shoter than Linux's.
    > (Android has also same problem).
    > siginfo_t has different memory layout.
    > Some signal numbers, including SIGBUS, SIGCHLD and SIGSYS,
    > or some signal flags are different.
    > This CL fills the gap, by introducing linux_signal.h and
    > two syscalls, sys_sigprocmask and sys_sigaction.
    >
    > Also, as signal.h provides ucontext_t, but PNaCl toolchain
    > does not provides ucontext_t, this CL re-use the
    > android_ucontext.h (by renaming it to linux_ucontext.h).
    >
    > TEST=Ran bots.
    > BUG=358465
    > CQ_EXTRA_TRYBOTS=tryserver.chromium.linux:linux_chromium_trusty32_rel,linux_arm
    >
    > Committed: https://crrev.com/3a315e3246a5191cee27216c09e669ea2d6c33d3
    > Cr-Commit-Position: refs/heads/master@{#325634}
    
    TBR=mdempsky@chromium.org,hidehiko@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=358465
    
    Review URL: https://codereview.chromium.org/1093843002
    
    Cr-Commit-Position: refs/heads/master@{#325644}

--- sandbox/linux/system_headers/android_x86_64_ucontext.h
+++ sandbox/linux/system_headers/android_x86_64_ucontext.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef SANDBOX_LINUX_SYSTEM_HEADERS_X86_64_LINUX_UCONTEXT_H_
-#define SANDBOX_LINUX_SYSTEM_HEADERS_X86_64_LINUX_UCONTEXT_H_
+#ifndef SANDBOX_LINUX_SYSTEM_HEADERS_ANDROID_X86_64_UCONTEXT_H_
+#define SANDBOX_LINUX_SYSTEM_HEADERS_ANDROID_X86_64_UCONTEXT_H_
 
 // We do something compatible with glibc. Hopefully, at some point Android will
 // provide that for us, and __BIONIC_HAVE_UCONTEXT_T should be defined.
@@ -86,4 +86,4 @@ typedef struct ucontext {
 #include <sys/ucontext.h>
 #endif  // __BIONIC_HAVE_UCONTEXT_T
 
-#endif  // SANDBOX_LINUX_SYSTEM_HEADERS_X86_64_LINUX_UCONTEXT_H_
+#endif  // SANDBOX_LINUX_SYSTEM_HEADERS_ANDROID_X86_64_UCONTEXT_H_
