74637d5584e80055d1e2888838dddff794f18541
reillyg <reillyg@chromium.org>
2015-04-17 19:11:43 -0700

    Remove knowledge of USB devices from permission prompt implementations.
    This change hides all the details of USB device objects inside of the
    DevicePermissionsPrompt::Prompt object so that the Views and OS X
    implementations do not need to be modified when this prompting logic is
    extended to support HID devices as well.
    
    BUG=457899
    
    Review URL: https://codereview.chromium.org/1097603003
    
    Cr-Commit-Position: refs/heads/master@{#325761}

--- chrome/browser/ui/views/extensions/device_permissions_dialog_view.h
+++ chrome/browser/ui/views/extensions/device_permissions_dialog_view.h
@@ -20,7 +20,6 @@ class DevicePermissionsTableModel;
 class DevicePermissionsDialogView : public views::DialogDelegateView {
  public:
   DevicePermissionsDialogView(
-      extensions::DevicePermissionsPrompt::Delegate* delegate,
       scoped_refptr<extensions::DevicePermissionsPrompt::Prompt> prompt);
   ~DevicePermissionsDialogView() override;
 
@@ -39,7 +38,6 @@ class DevicePermissionsDialogView : public views::DialogDelegateView {
   gfx::Size GetPreferredSize() const override;
 
  private:
-  extensions::DevicePermissionsPrompt::Delegate* delegate_;
   scoped_refptr<extensions::DevicePermissionsPrompt::Prompt> prompt_;
 
   // Displays the list of devices.
