a2f24da7fd5ad78c10659ef11a0195889d457c8b
mad <mad@chromium.org>
2015-04-20 13:25:30 -0700

    Support the Canary group of the srt prompt experiment
    BUG=478079
    
    Review URL: https://codereview.chromium.org/1096713002
    
    Cr-Commit-Position: refs/heads/master@{#325897}

--- chrome/browser/safe_browsing/srt_field_trial_win.h
+++ chrome/browser/safe_browsing/srt_field_trial_win.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SAFE_BROWSING_SRT_FIELD_TRIAL_WIN_H_
 #define CHROME_BROWSER_SAFE_BROWSING_SRT_FIELD_TRIAL_WIN_H_
 
+#include <string>
+
 namespace safe_browsing {
 
 // Returns true if this Chrome is in a field trial group which shows the SRT
