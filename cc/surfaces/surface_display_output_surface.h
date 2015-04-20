dbe00aadf3148a1ca3f791eaea8bbd0ab8993d01
brianderson <brianderson@chromium.org>
2015-04-20 16:09:43 -0700

    Delete unused SurfaceDisplayOutputSurface::surface_manager_
    BUG=none
    
    Review URL: https://codereview.chromium.org/1068833004
    
    Cr-Commit-Position: refs/heads/master@{#325932}

--- cc/surfaces/surface_display_output_surface.h
+++ cc/surfaces/surface_display_output_surface.h
@@ -50,7 +50,6 @@ class CC_SURFACES_EXPORT SurfaceDisplayOutputSurface
   void SwapBuffersComplete(SurfaceDrawStatus drawn);
 
   OnscreenDisplayClient* display_client_;
-  SurfaceManager* surface_manager_;
   SurfaceFactory factory_;
   gfx::Size display_size_;
   SurfaceId surface_id_;
