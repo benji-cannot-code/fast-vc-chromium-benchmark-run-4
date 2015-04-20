671a08f3c1499b2544778cbbf2e9419bdf18c0c6
benwells <benwells@chromium.org>
2015-04-20 02:27:23 -0700

    Add related_applications field to manifest parser.
    The related_applications field will be used for sites to specify which apps the banners should show for.
    
    This change also adds the prefer_related_applications field.
    
    BUG=457403
    
    Review URL: https://codereview.chromium.org/919293002
    
    Cr-Commit-Position: refs/heads/master@{#325819}

--- content/public/common/manifest.cc
+++ content/public/common/manifest.cc
@@ -17,9 +17,16 @@ Manifest::Icon::Icon()
 Manifest::Icon::~Icon() {
 }
 
+Manifest::RelatedApplication::RelatedApplication() {
+}
+
+Manifest::RelatedApplication::~RelatedApplication() {
+}
+
 Manifest::Manifest()
     : display(DISPLAY_MODE_UNSPECIFIED),
       orientation(blink::WebScreenOrientationLockDefault),
+      prefer_related_applications(false),
       gcm_user_visible_only(false) {
 }
 
@@ -33,6 +40,8 @@ bool Manifest::IsEmpty() const {
          display == DISPLAY_MODE_UNSPECIFIED &&
          orientation == blink::WebScreenOrientationLockDefault &&
          icons.empty() &&
+         related_applications.empty() &&
+         !prefer_related_applications &&
          gcm_sender_id.is_null() &&
          !gcm_user_visible_only;
 }
