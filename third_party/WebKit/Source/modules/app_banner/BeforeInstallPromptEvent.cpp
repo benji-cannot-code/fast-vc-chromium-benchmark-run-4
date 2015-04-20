143e0bbaeae2fea0c1d4b6bdc2118ae7908990c6
benwells@chromium.org <benwells@chromium.org>
2015-04-20 08:44:23 +0000

    Change beforeinstallprompt event to take an array of platforms.
    Currently it just takes a single platform.
    
    BUG=460945
    
    Review URL: https://codereview.chromium.org/1089813002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/app_banner/BeforeInstallPromptEvent.cpp
+++ third_party/WebKit/Source/modules/app_banner/BeforeInstallPromptEvent.cpp
@@ -17,15 +17,15 @@ BeforeInstallPromptEvent::BeforeInstallPromptEvent()
 {
 }
 
-BeforeInstallPromptEvent::BeforeInstallPromptEvent(const AtomicString& name, const String& platform)
+BeforeInstallPromptEvent::BeforeInstallPromptEvent(const AtomicString& name, const Vector<String>& platforms)
     : Event(name, false, true)
-    , m_platform(platform)
+    , m_platforms(platforms)
 {
 }
 
 BeforeInstallPromptEvent::BeforeInstallPromptEvent(const AtomicString& name, const BeforeInstallPromptEventInit& init)
     : Event(name, false, true)
-    , m_platform(init.platform())
+    , m_platforms(init.platforms())
 {
 }
 
@@ -33,9 +33,9 @@ BeforeInstallPromptEvent::~BeforeInstallPromptEvent()
 {
 }
 
-String BeforeInstallPromptEvent::platform() const
+Vector<String> BeforeInstallPromptEvent::platforms() const
 {
-    return m_platform;
+    return m_platforms;
 }
 
 ScriptPromise BeforeInstallPromptEvent::userChoice(ScriptState* scriptState) const
