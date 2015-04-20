fc3552e426cba4781e8eb4778dda7985a2c11099
rkc <rkc@chromium.org>
2015-04-20 14:36:32 -0700

    Show existing feedback window when feedback is invoked.
    When a user selects to send feedback, if an existing feedback window exists,
    show it.
    
    R=xiyuan@chromium.org
    BUG=423677
    
    Review URL: https://codereview.chromium.org/1094153003
    
    Cr-Commit-Position: refs/heads/master@{#325914}

--- chrome/browser/resources/feedback/js/event_handler.js
+++ chrome/browser/resources/feedback/js/event_handler.js
@@ -124,6 +124,11 @@ function requestFeedbackHandler(request, sender, sendResponse) {
  */
 function startFeedbackUI(feedbackInfo) {
   initialFeedbackInfo = feedbackInfo;
+  var win = chrome.app.window.get('default_window');
+  if (win) {
+    win.show();
+    return;
+  }
   chrome.app.window.create('html/default.html', {
       frame: 'none',
       id: 'default_window',
