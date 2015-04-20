eeca8dc12a42b82d8bc56777342991a246ba40b3
payal.pandey <payal.pandey@samsung.com>
2015-04-20 10:10:19 -0700

    Apply automatic range checks to content enum types across IPC.
    This is part of a long-running background task to remove the remaining
    uses of the unchecked IPC_ENUM_TRAITS() macro.
    
    BUG=246708
    
    Review URL: https://codereview.chromium.org/1091773002
    
    Cr-Commit-Position: refs/heads/master@{#325867}

--- ui/gfx/font_render_params.h
+++ ui/gfx/font_render_params.h
@@ -25,6 +25,8 @@ struct GFX_EXPORT FontRenderParams {
     HINTING_SLIGHT,
     HINTING_MEDIUM,
     HINTING_FULL,
+
+    HINTING_MAX = HINTING_FULL,
   };
 
   // Different subpixel orders to be used for subpixel rendering.
@@ -34,6 +36,8 @@ struct GFX_EXPORT FontRenderParams {
     SUBPIXEL_RENDERING_BGR,
     SUBPIXEL_RENDERING_VRGB,
     SUBPIXEL_RENDERING_VBGR,
+
+    SUBPIXEL_RENDERING_MAX = SUBPIXEL_RENDERING_VBGR,
   };
 
   // Antialiasing (grayscale if |subpixel_rendering| is SUBPIXEL_RENDERING_NONE
