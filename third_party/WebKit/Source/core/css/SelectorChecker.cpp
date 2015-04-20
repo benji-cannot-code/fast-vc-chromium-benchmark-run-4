565fa336eebfe3e0f91051d7f949242e3b0a8b5a
kochi@chromium.org <kochi@chromium.org>
2015-04-20 13:24:03 +0000

    Only match :window-inactive on certain pseudo elements
    :window-inactive CSS pseudo class is only supported on ::selection
    and various scrollbar pseudo elements, not any elements in general,
    and it is not yet standardized either.
    
    The original implementation dates back to WebKit around 2009:
    http://trac.webkit.org/changeset/41094
    
    If :window-inactive is specified on anything other than ::selection
    or scrollbar pseudo elements, it causes an assertion in
    SelectorChecker::checkPseudoClass()
    (the case is not covered in switch-case).
    
    BUG=478275
    
    Review URL: https://codereview.chromium.org/1054583004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194025 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/SelectorChecker.cpp
+++ third_party/WebKit/Source/core/css/SelectorChecker.cpp
@@ -929,6 +929,7 @@ bool SelectorChecker::checkPseudoClass(const SelectorCheckingContext& context, c
     case CSSSelector::PseudoSingleButton:
     case CSSSelector::PseudoNoButton:
     case CSSSelector::PseudoCornerPresent:
+    case CSSSelector::PseudoWindowInactive:
         return false;
     case CSSSelector::PseudoUnknown:
     case CSSSelector::PseudoNotParsed:
