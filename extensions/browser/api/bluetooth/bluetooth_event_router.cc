0caad09747fb0441662dbc883a02a37a55a12c5d
anujk.sharma <anujk.sharma@samsung.com>
2015-04-20 20:10:21 -0700

    favor DCHECK_CURRENTLY_ON for better logs in extensions/
    BUG=466848
    
    Review URL: https://codereview.chromium.org/1059973004
    
    Cr-Commit-Position: refs/heads/master@{#325974}

--- extensions/browser/api/bluetooth/bluetooth_event_router.cc
+++ extensions/browser/api/bluetooth/bluetooth_event_router.cc
@@ -43,7 +43,7 @@ BluetoothEventRouter::BluetoothEventRouter(content::BrowserContext* context)
       num_event_listeners_(0),
       extension_registry_observer_(this),
       weak_ptr_factory_(this) {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   DCHECK(browser_context_);
   registrar_.Add(this,
                  extensions::NOTIFICATION_EXTENSION_HOST_DESTROYED,
@@ -52,7 +52,7 @@ BluetoothEventRouter::BluetoothEventRouter(content::BrowserContext* context)
 }
 
 BluetoothEventRouter::~BluetoothEventRouter() {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   if (adapter_.get()) {
     adapter_->RemoveObserver(this);
     adapter_ = NULL;
@@ -226,7 +226,7 @@ void BluetoothEventRouter::RemovePairingDelegate(
 void BluetoothEventRouter::AdapterPresentChanged(
     device::BluetoothAdapter* adapter,
     bool present) {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   if (adapter != adapter_.get()) {
     DVLOG(1) << "Ignoring event for adapter " << adapter->GetAddress();
     return;
--- extensions/browser/api/sockets_tcp/sockets_tcp_api.cc
+++ extensions/browser/api/sockets_tcp/sockets_tcp_api.cc
@@ -458,7 +458,7 @@ SocketsTcpSecureFunction::~SocketsTcpSecureFunction() {
 }
 
 bool SocketsTcpSecureFunction::Prepare() {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   params_ = core_api::sockets_tcp::Secure::Params::Create(*args_);
   EXTENSION_FUNCTION_VALIDATE(params_.get());
   url_request_getter_ = browser_context()->GetRequestContext();
@@ -468,7 +468,7 @@ bool SocketsTcpSecureFunction::Prepare() {
 // Override the regular implementation, which would call AsyncWorkCompleted
 // immediately after Work().
 void SocketsTcpSecureFunction::AsyncWorkStart() {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
 
   ResumableTCPSocket* socket = GetTcpSocket(params_->socket_id);
   if (!socket) {
