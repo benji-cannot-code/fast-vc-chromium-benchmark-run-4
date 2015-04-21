98161f581a73e780c47fb13bd1480bed23adbbf9
dgozman@chromium.org <dgozman@chromium.org>
2015-04-21 11:43:31 +0000

    [DevTools] Rename toolbox->emulation, move emulation-related code there.
    BUG=462924
    
    Review URL: https://codereview.chromium.org/1084563005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194112 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/emulation/OverridesView.js
+++ third_party/WebKit/Source/devtools/front_end/emulation/OverridesView.js
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.OverridesView = function()
 {
     WebInspector.VBox.call(this);
-    this.registerRequiredCSS("main/overrides.css");
+    this.registerRequiredCSS("emulation/overrides.css");
     this.element.classList.add("overrides-view");
 
     this._tabbedPane = new WebInspector.TabbedPane();
@@ -374,7 +374,7 @@ WebInspector.OverridesView.SensorsTab = function()
     ]);
 
     this.element.classList.add("overrides-sensors");
-    this.registerRequiredCSS("main/accelerometer.css");
+    this.registerRequiredCSS("emulation/accelerometer.css");
     this.element.appendChild(this._createSettingCheckbox(WebInspector.UIString("Emulate touch screen"), WebInspector.overridesSupport.settings.emulateTouch, undefined));
     this._appendGeolocationOverrideControl();
     this._apendDeviceOrientationOverrideControl();
