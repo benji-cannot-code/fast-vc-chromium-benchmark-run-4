4371aca13754e3e5443364ac32bb6bc6874fbec3
hashimoto <hashimoto@chromium.org>
2015-04-21 09:02:48 -0700

    Stop including chromeos_switches.h when !defined(OS_CHROMEOS)
    BUG=None
    TBR=bauerb@chromium.org for include fix under chrome/browser/ui/webui
    
    Review URL: https://codereview.chromium.org/1094183005
    
    Cr-Commit-Position: refs/heads/master@{#326043}

--- chrome/browser/ui/webui/local_discovery/local_discovery_ui_browsertest.cc
+++ chrome/browser/ui/webui/local_discovery/local_discovery_ui_browsertest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "chrome/test/base/web_ui_browser_test.h"
-#include "chromeos/chromeos_switches.h"
 #include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "components/signin/core/browser/signin_manager.h"
 #include "components/signin/core/browser/signin_manager_base.h"
@@ -34,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_CHROMEOS)
 #include "base/prefs/pref_service.h"
 #include "chrome/common/pref_names.h"
+#include "chromeos/chromeos_switches.h"
 #endif
 
 using testing::InvokeWithoutArgs;
