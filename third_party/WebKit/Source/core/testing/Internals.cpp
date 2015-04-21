0d6e1a92fc8d7513e03a810477efae0488711daa
jl@opera.com <jl@opera.com>
2015-04-21 07:51:59 +0000

    IDL: Add support for [Unscopeable] on attributes and methods
    Adding [Unscopeable] to an attribute or method means it won't be found
    during name lookup with an object that has the attribute or method in
    the scope (i.e. using the "with" statement.)
    
    The mechanism that makes this happen is that the interface prototype
    object has a property named @@unscopables that is an object with a
    property for every unscopeable member named as the member.
    
    BUG=462916
    
    Review URL: https://codereview.chromium.org/1085453003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194091 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/testing/Internals.cpp
+++ third_party/WebKit/Source/core/testing/Internals.cpp
@@ -2359,5 +2359,14 @@ bool Internals::isUseCounted(Document* document, int useCounterId)
     return UseCounter::isCounted(*document, static_cast<UseCounter::Feature>(useCounterId));
 }
 
+String Internals::unscopeableAttribute()
+{
+    return "unscopeableAttribute";
+}
+
+String Internals::unscopeableMethod()
+{
+    return "unscopeableMethod";
+}
 
 } // namespace blink
