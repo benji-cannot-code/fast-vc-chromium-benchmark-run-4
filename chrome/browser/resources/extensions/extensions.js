6c97f6320e9b501c41988c3a43615233437caa16
andyzg <andyzg@google.com>
2015-04-21 13:56:12 -0700

    Added missing user actions to extension settings and profile settings.
    R=jwd
    
    Review URL: https://codereview.chromium.org/1088673002
    
    Cr-Commit-Position: refs/heads/master@{#326108}

--- chrome/browser/resources/extensions/extensions.js
+++ chrome/browser/resources/extensions/extensions.js
@@ -151,6 +151,9 @@ cr.define('extensions', function() {
         extensionList.updateFocusableElements();
         chrome.developerPrivate.updateProfileConfiguration(
             {inDeveloperMode: e.target.checked});
+        var suffix = $('toggle-dev-on').checked ? 'Enabled' : 'Disabled';
+        chrome.send('metricsHandler:recordAction',
+                    ['Options_ToggleDeveloperMode_' + suffix]);
       }.bind(this));
 
       window.addEventListener('resize', function() {
@@ -159,6 +162,8 @@ cr.define('extensions', function() {
 
       // Set up the three dev mode buttons (load unpacked, pack and update).
       $('load-unpacked').addEventListener('click', function(e) {
+        chrome.send('metricsHandler:recordAction',
+                    ['Options_LoadUnpackedExtension']);
         extensionLoader.loadUnpacked();
       });
       $('pack-extension').addEventListener('click',
@@ -187,6 +192,16 @@ cr.define('extensions', function() {
 
       extensions.ExtensionOptionsOverlay.getInstance().initializePage(
           extensions.ExtensionSettings.showOverlay);
+
+      // Add user action logging for bottom links.
+      var moreExtensionLink =
+          document.getElementsByClassName('more-extensions-link');
+      for (var i = 0; i < moreExtensionLink.length; i++) {
+        moreExtensionLink[i].addEventListener('click', function(e) {
+          chrome.send('metricsHandler:recordAction',
+                      ['Options_GetMoreExtensions']);
+        });
+      }
 
       // Initialize the kiosk overlay.
       if (cr.isChromeOS) {
@@ -305,6 +320,8 @@ cr.define('extensions', function() {
      */
     handleUpdateExtensionNow_: function(e) {
       chrome.developerPrivate.autoUpdate();
+      chrome.send('metricsHandler:recordAction',
+                  ['Options_UpdateExtensions']);
     },
 
     /**
