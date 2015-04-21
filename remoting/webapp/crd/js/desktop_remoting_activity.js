9463f11a74074820c9127cf8ff66e5502fbb90b3
kelvinp <kelvinp@chromium.org>
2015-04-20 18:45:54 -0700

    [Chromoting] Expose LogToServer for logging.
    This CL exposes LogToServer from DesktopRemoting so that
    Me2MeActivity can access it for logging.
    
    It also properly named the private variable logToServer_
    with the underscore.
    
    Review URL: https://codereview.chromium.org/1091373003
    
    Cr-Commit-Position: refs/heads/master@{#325963}

--- remoting/webapp/crd/js/desktop_remoting_activity.js
+++ remoting/webapp/crd/js/desktop_remoting_activity.js
@@ -131,4 +131,11 @@ remoting.DesktopRemotingActivity.prototype.getConnectedView = function() {
   return this.connectedView_;
 };
 
+/**
+ * @return {remoting.ClientSession}.
+ */
+remoting.DesktopRemotingActivity.prototype.getSession = function() {
+  return this.session_;
+};
+
 })();
