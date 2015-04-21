164e7f4552a75a1cfe1966861c641dc75c4c7078
estark@chromium.org <estark@chromium.org>
2015-04-21 16:21:00 +0000

    Use the served CSP header for dedicated workers
    This CL makes workers obey the CSP that was served as a header along
    with the worker script, instead of inheriting the CSP from the document
    that spawned the worker. (Blob and file URLs still inherit the CSP from
    the parent document.)
    
    BUG=474872
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=194094
    
    Review URL: https://codereview.chromium.org/1100583003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194143 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/resources/worker-importscripts.js
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-try {
-    importScripts("http://localhost:8000/security/contentSecurityPolicy/resources/post-message.js");
-    postMessage("importScripts allowed");
-} catch(e) {
-    postMessage("importScripts blocked: " + e);
-}
-
--- third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/resources/worker-make-xhr.js
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-try {
-    var xhr = new XMLHttpRequest;
-    xhr.open("GET", "http://127.0.0.1:8000/xmlhttprequest/resources/get.txt", true); 
-    postMessage("xhr allowed");
-} catch(e) {
-    postMessage("xhr blocked");
-}
-
