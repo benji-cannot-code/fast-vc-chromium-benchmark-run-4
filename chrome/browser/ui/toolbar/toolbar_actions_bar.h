59111315e9df1a41eb6944ba9e238efdcdf0380b
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-04-21 14:23:53 -0700

    [Extensions Toolbar] Remove the TabOrderHelper
    This was added when we weren't sure if we were going to have extensions pop
    out if they wanted to act. Discussions since and the gap of time between then
    and now (with no change towards this) have implied that, while we may still
    pop extensions out under very exact circumstances (e.g., when showing a popup),
    it won't be like this, so it's safe to kill this code.
    
    And, of course, it's always in git history if need be.
    
    Review URL: https://codereview.chromium.org/1079793004
    
    Cr-Commit-Position: refs/heads/master@{#326123}

--- chrome/browser/ui/toolbar/toolbar_actions_bar.h
+++ chrome/browser/ui/toolbar/toolbar_actions_bar.h
@@ -138,11 +138,6 @@ class ToolbarActionsBar : public extensions::ExtensionToolbarModel::Observer {
 
   ToolbarActionsBarDelegate* delegate_for_test() { return delegate_; }
 
-  static void set_pop_out_actions_to_run_for_testing(
-      bool pop_out_actions_to_run) {
-    pop_out_actions_to_run_ = pop_out_actions_to_run;
-  }
-
   static void set_send_overflowed_action_changes_for_testing(
       bool send_overflowed_action_changes) {
     send_overflowed_action_changes_ = send_overflowed_action_changes;
@@ -154,8 +149,6 @@ class ToolbarActionsBar : public extensions::ExtensionToolbarModel::Observer {
   static bool disable_animations_for_testing_;
 
  private:
-  class TabOrderHelper;
-
   using ToolbarActions = ScopedVector<ToolbarActionViewController>;
 
   // ExtensionToolbarModel::Observer:
@@ -215,10 +208,6 @@ class ToolbarActionsBar : public extensions::ExtensionToolbarModel::Observer {
 
   // The toolbar actions.
   ToolbarActions toolbar_actions_;
-
-  // The TabOrderHelper that manages popping out actions that want to act.
-  // This is only non-null if |pop_out_actions_to_run| is true.
-  scoped_ptr<TabOrderHelper> tab_order_helper_;
 
   ScopedObserver<extensions::ExtensionToolbarModel,
                  extensions::ExtensionToolbarModel::Observer> model_observer_;
