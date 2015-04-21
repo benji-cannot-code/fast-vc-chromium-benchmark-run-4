1c6736ae5bb7170e4c6b6799706b9571b4daeaad
shuchen <shuchen@chromium.org>
2015-04-21 09:55:35 -0700

    Improves perf by avoid small memory allocation for key event handling if IME extension doesn't want to handle the key event.
    Currently there are 2 places allocate small memory for each key event:
    1) In InputMethodChromeOS::DispatchKeyEvent(): https://code.google.com/p/chromium/codesearch#chromium/src/ui/base/ime/input_method_chromeos.cc&l=143
    2) In InputMethodEngine::ProcessKeyEvent(): https://code.google.com/p/chromium/codesearch#chromium/src/chrome/browser/chromeos/input_method/input_method_engine.cc&l=603
    
    Those allocations can be avoided for key events what IMEs are not interested in.
    
    BUG=478373
    TEST=Verified on linux_chromeos.
    
    Review URL: https://codereview.chromium.org/1094273002
    
    Cr-Commit-Position: refs/heads/master@{#326056}

--- chrome/browser/chromeos/input_method/mock_input_method_engine.cc
+++ chrome/browser/chromeos/input_method/mock_input_method_engine.cc
@@ -117,6 +117,10 @@ void MockInputMethodEngine::PropertyActivate(const std::string& property_name) {
 void MockInputMethodEngine::Reset() {
 }
 
+bool MockInputMethodEngine::IsInterestedInKeyEvent() const {
+  return true;
+}
+
 void MockInputMethodEngine::ProcessKeyEvent(
     const ui::KeyEvent& key_event,
     const KeyEventDoneCallback& callback) {
