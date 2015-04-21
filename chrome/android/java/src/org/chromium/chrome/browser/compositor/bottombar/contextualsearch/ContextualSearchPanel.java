69391012d34ab34c9d9e765816bb4f8bf22440ea
pedrosimonetti <pedrosimonetti@chromium.org>
2015-04-20 17:12:30 -0700

    [Contextual Search] Prevent Panel from becoming disembodied.
    BUG=478970
    
    Review URL: https://codereview.chromium.org/1090373005
    
    Cr-Commit-Position: refs/heads/master@{#325948}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchPanel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchPanel.java
@@ -165,7 +165,7 @@ public class ContextualSearchPanel extends ContextualSearchPanelAnimation
      */
     public void handleSwipeStart() {
         if (animationIsRunning()) {
-            cancelAnimation(this, Property.PANEL_HEIGHT);
+            cancelHeightAnimation();
         }
 
         mHasDetectedTouchGesture = false;
