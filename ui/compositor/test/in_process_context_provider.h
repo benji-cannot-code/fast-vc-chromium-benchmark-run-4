50f24104f7189470490843a2200817ef9b279256
tfarina <tfarina@chromium.org>
2015-04-21 04:11:06 -0700

    compositor: Fix header guard of in_process_context_provider.h.
    I found this while doing
    https://codereview.chromium.org/1083843002/#ps20001.
    
    Tested on Linux with the following command lines:
    $ ./build/gyp_chromium
    $ ninja -C out/Debug compositor_unittests
    $ out/Debug/compositor_unittests
    
    BUG=None
    TEST=see above
    R=danakj@chromium.org
    
    Review URL: https://codereview.chromium.org/1095963002
    
    Cr-Commit-Position: refs/heads/master@{#326009}

--- ui/compositor/test/in_process_context_provider.h
+++ ui/compositor/test/in_process_context_provider.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef WEBKIT_COMMON_GPU_CONTEXT_PROVIDER_IN_PROCESS_H_
-#define WEBKIT_COMMON_GPU_CONTEXT_PROVIDER_IN_PROCESS_H_
+#ifndef UI_COMPOSITOR_TEST_IN_PROCESS_CONTEXT_PROVIDER_H_
+#define UI_COMPOSITOR_TEST_IN_PROCESS_CONTEXT_PROVIDER_H_
 
 #include <string>
 
@@ -87,4 +87,4 @@ class InProcessContextProvider : public cc::ContextProvider {
 
 }  // namespace ui
 
-#endif  // WEBKIT_COMMON_GPU_CONTEXT_PROVIDER_IN_PROCESS_H_
+#endif  // UI_COMPOSITOR_TEST_IN_PROCESS_CONTEXT_PROVIDER_H_
