ba2746f63300a3f7f6279934e11f3319bf9b04de
sky <sky@chromium.org>
2015-04-21 07:16:33 -0700

    Fixes possible crash when dragging tabs and new tab inserted
    The crash happens because ending the drag queries back to the TabStrip
    for tabs before tabs_ is in sync with the model. If the newly inserted
    tab is at the beginning of the model and the tab being dragged is at
    the end of the model then GetTabsMatchingDraggedContents tries to
    use an index that is outside of tabs_ and we crash.
    
    The fix is to add the tab before ending the drag. By adding the tab
    first we ensure the model and tabstrip are in sync.
    
    BUG=474082
    TEST=covered by test
    R=pkotwicz@chromium.org
    
    Review URL: https://codereview.chromium.org/1086893003
    
    Cr-Commit-Position: refs/heads/master@{#326027}

--- chrome/browser/ui/views/tabs/tab_drag_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/tabs/tab_drag_controller_interactive_uitest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_iterator.h"
 #include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/host_desktop.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
@@ -1326,8 +1327,14 @@ void CancelOnNewTabWhenDraggingStep2(
   ASSERT_TRUE(TabDragController::IsActive());
   ASSERT_EQ(2u, browser_list->size());
 
-  // Add another tab. This should trigger exiting the nested loop.
-  test->AddBlankTabAndShow(browser_list->GetLastActive());
+  // Add another tab. This should trigger exiting the nested loop. Add at the
+  // to exercise past crash when model/tabstrip got out of sync (474082).
+  content::WindowedNotificationObserver observer(
+      content::NOTIFICATION_LOAD_STOP,
+      content::NotificationService::AllSources());
+  chrome::AddTabAt(browser_list->GetLastActive(), GURL(url::kAboutBlankURL),
+                   0, false);
+  observer.Wait();
 }
 
 }  // namespace
