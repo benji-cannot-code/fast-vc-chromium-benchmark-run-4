2e33049f5864946b1810fe5d22efd701a1c25500
danakj <danakj@chromium.org>
2015-04-20 18:03:37 -0700

    ui: DCHECK that PaintRecorders are not nested.
    It's not valid to nest a PaintRecorder in another, that would mean the
    DisplayItems are nested which is not possible, as they just appear in
    a list.
    
    R=piman@chromium.org
    BUG=466426
    
    Review URL: https://codereview.chromium.org/1081393002
    
    Cr-Commit-Position: refs/heads/master@{#325960}

--- ui/compositor/paint_context.cc
+++ ui/compositor/paint_context.cc
@@ -18,6 +18,7 @@ PaintContext::PaintContext(gfx::Canvas* canvas, const gfx::Rect& invalidation)
       invalidation_(invalidation) {
 #if DCHECK_IS_ON()
   root_visited_ = nullptr;
+  inside_paint_recorder_ = false;
 #endif
 }
 
@@ -33,6 +34,7 @@ PaintContext::PaintContext(cc::DisplayItemList* list,
       invalidation_(invalidation) {
 #if DCHECK_IS_ON()
   root_visited_ = nullptr;
+  inside_paint_recorder_ = false;
 #endif
 }
 
@@ -53,6 +55,7 @@ PaintContext::PaintContext(const PaintContext& other)
       offset_(other.offset_) {
 #if DCHECK_IS_ON()
   root_visited_ = other.root_visited_;
+  inside_paint_recorder_ = other.inside_paint_recorder_;
 #endif
 }
 
