12e2bc3b31750f736493c2f9a2dad6b4fa66afac
aurimas <aurimas@chromium.org>
2015-04-22 14:57:16 -0700

    Revert of Don't construct TtsPlatformImpl prior to ICS_MR1 (patchset #1 id:1 of https://codereview.chromium.org/1048983002/)
    Reason for revert:
    We no longer need to support ICS as minsdk is now 16, so this reverts these added checks.
    
    Original issue's description:
    > Don't construct TtsPlatformImpl prior to ICS_MR1
    >
    > BUG=462764
    >
    > Committed: https://crrev.com/ede977e8e5bd4415655e5c81a142276417b4250e
    > Cr-Commit-Position: refs/heads/master@{#322892}
    
    TBR=newt@chromium.org,dmazzoni@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=462764
    
    Review URL: https://codereview.chromium.org/1087033007
    
    Cr-Commit-Position: refs/heads/master@{#326376}

--- chrome/browser/speech/tts_android.cc
+++ chrome/browser/speech/tts_android.cc
@@ -31,8 +31,7 @@ TtsPlatformImplAndroid::TtsPlatformImplAndroid()
 
 TtsPlatformImplAndroid::~TtsPlatformImplAndroid() {
   JNIEnv* env = AttachCurrentThread();
-  if (java_ref_.obj())
-    Java_TtsPlatformImpl_destroy(env, java_ref_.obj());
+  Java_TtsPlatformImpl_destroy(env, java_ref_.obj());
 }
 
 bool TtsPlatformImplAndroid::PlatformImplAvailable() {
@@ -45,9 +44,6 @@ bool TtsPlatformImplAndroid::Speak(
     const std::string& lang,
     const VoiceData& voice,
     const UtteranceContinuousParameters& params) {
-  if (!java_ref_.obj())
-    return false;
-
   JNIEnv* env = AttachCurrentThread();
   jboolean success = Java_TtsPlatformImpl_speak(
       env, java_ref_.obj(),
@@ -64,9 +60,6 @@ bool TtsPlatformImplAndroid::Speak(
 }
 
 bool TtsPlatformImplAndroid::StopSpeaking() {
-  if (!java_ref_.obj())
-    return false;
-
   JNIEnv* env = AttachCurrentThread();
   Java_TtsPlatformImpl_stop(env, java_ref_.obj());
   utterance_id_ = 0;
@@ -87,9 +80,6 @@ bool TtsPlatformImplAndroid::IsSpeaking() {
 
 void TtsPlatformImplAndroid::GetVoices(
     std::vector<VoiceData>* out_voices) {
-  if (!java_ref_.obj())
-    return;
-
   JNIEnv* env = AttachCurrentThread();
   if (!Java_TtsPlatformImpl_isInitialized(env, java_ref_.obj()))
     return;
