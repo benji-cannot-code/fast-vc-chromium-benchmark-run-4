404699c83bd5d610ef9ae2d1f261eada13752641
scheib <scheib@chromium.org>
2015-04-21 17:35:35 -0700

    bluetooth: Refactor (Add|Remove)Observer impls into BluetoothAdapter.
    In preparation before BluetoothAdapterAndroid impl, move duplicate
    implementation of AddObserver, RemoveObserver into the BluetoothAdapter
    base class.
    
    BUG=471536
    
    Review URL: https://codereview.chromium.org/1057603004
    
    Cr-Commit-Position: refs/heads/master@{#326186}

--- device/bluetooth/bluetooth_adapter_unittest.cc
+++ device/bluetooth/bluetooth_adapter_unittest.cc
@@ -20,10 +20,6 @@ class TestBluetoothAdapter : public BluetoothAdapter {
   TestBluetoothAdapter() {
   }
 
-  void AddObserver(BluetoothAdapter::Observer* observer) override {}
-
-  void RemoveObserver(BluetoothAdapter::Observer* observer) override {}
-
   std::string GetAddress() const override { return ""; }
 
   std::string GetName() const override { return ""; }
