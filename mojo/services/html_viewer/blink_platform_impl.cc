bcc4c2a1741e209161d639097de181bd8c0792ec
jam <jam@chromium.org>
2015-04-21 17:27:59 -0700

    Implement message ports in html_viewer.
    Since they are very similar to message pipes, I used the latter to impelement them.
    
    BUG=476111
    
    Review URL: https://codereview.chromium.org/1086893004
    
    Cr-Commit-Position: refs/heads/master@{#326185}

--- mojo/services/html_viewer/blink_platform_impl.cc
+++ mojo/services/html_viewer/blink_platform_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/services/html_viewer/blink_resource_constants.h"
 #include "mojo/services/html_viewer/web_clipboard_impl.h"
 #include "mojo/services/html_viewer/web_cookie_jar_impl.h"
+#include "mojo/services/html_viewer/web_message_port_channel_impl.h"
 #include "mojo/services/html_viewer/web_socket_handle_impl.h"
 #include "mojo/services/html_viewer/web_thread_impl.h"
 #include "mojo/services/html_viewer/web_url_loader_impl.h"
@@ -168,6 +169,12 @@ bool BlinkPlatformImpl::isThreadedCompositingEnabled() {
 
 blink::WebCompositorSupport* BlinkPlatformImpl::compositorSupport() {
   return &compositor_support_;
+}
+
+void BlinkPlatformImpl::createMessageChannel(
+    blink::WebMessagePortChannel** channel1,
+    blink::WebMessagePortChannel** channel2) {
+  WebMessagePortChannelImpl::CreatePair(channel1, channel2);
 }
 
 blink::WebScrollbarBehavior* BlinkPlatformImpl::scrollbarBehavior() {
