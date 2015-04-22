6053e82ac730c4ead0cea757709b51d282c221ef
haraken@chromium.org <haraken@chromium.org>
2015-04-22 13:53:34 +0000

    Oilpan: Build fix after r194208
    TBR=keishi@chromium.org
    
    Review URL: https://codereview.chromium.org/1066003004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194213 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/FontFaceSet.cpp
+++ third_party/WebKit/Source/core/css/FontFaceSet.cpp
@@ -585,7 +585,7 @@ DEFINE_TRACE(FontFaceSet)
     visitor->trace(m_loadedFonts);
     visitor->trace(m_failedFonts);
     visitor->trace(m_nonCSSConnectedFaces);
-    DocumentSupplement::trace(visitor);
+    WillBeHeapSupplement<Document>::trace(visitor);
 #endif
     EventTargetWithInlineData::trace(visitor);
     ActiveDOMObject::trace(visitor);
