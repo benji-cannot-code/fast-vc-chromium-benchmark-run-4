f41301fc72a2948d769ea29e9c771af5bde09456
wfh <wfh@chromium.org>
2015-04-21 17:04:20 -0700

    Fix comparison method for ClassInfo and also load icon images as shared.
    This fixes a GDI handle leak.
    
    BUG=469465
    
    Review URL: https://codereview.chromium.org/1096243002
    
    Cr-Commit-Position: refs/heads/master@{#326182}

--- ui/gfx/win/window_impl.cc
+++ ui/gfx/win/window_impl.cc
@@ -42,7 +42,8 @@ struct ClassInfo {
 
   // Compares two ClassInfos. Returns true if all members match.
   bool Equals(const ClassInfo& other) const {
-    return (other.style == style && other.icon == icon);
+    return (other.style == style && other.icon == icon &&
+            other.small_icon == small_icon);
   }
 };
 
