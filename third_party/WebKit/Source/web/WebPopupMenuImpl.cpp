f87b815306d9a7f4c2225750dcf43fda65aad85c
schenney@chromium.org <schenney@chromium.org>
2015-04-22 23:06:46 +0000

    Add support for painting disabled mode for Slimming Paint
    Plumb the "painting disabled" mode through to disable the GraphicsContext
    in Slimming Paint perf testing. This mode causes Graphics Context to do
    no work, producing empty SkPictures for all drawing display items. This
    seems the best interpretation of "painting disabled".
    
    To get meaningful results we must disable caching, otherwise the results
    only include painting calls on the first iteration, while cached empty
    context early outs on subsequent iterations.
    
    The perf results line for record_time_painting_disabled and
    record_time_null_canvas will change for the worse with this patch, because
    previously we were reporting cached painting in this slot. Now both report
    the non-cached path with no GC actions.
    
    R=pdr@chromium.org,chrishtr@chromium.org
    BUG=471348
    
    Review URL: https://codereview.chromium.org/1101753002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194243 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebPopupMenuImpl.cpp
+++ third_party/WebKit/Source/web/WebPopupMenuImpl.cpp
@@ -219,11 +219,10 @@ void WebPopupMenuImpl::paintContents(WebCanvas* canvas, const WebRect& rect, Web
         return;
 
     OwnPtr<GraphicsContext> context;
-    GraphicsContext::DisabledMode disabledMode;
-    if (paintingControl == PaintingControlSetting::DisplayListConstructionDisabled)
+    GraphicsContext::DisabledMode disabledMode = GraphicsContext::NothingDisabled;
+    if (paintingControl == PaintingControlSetting::DisplayListPaintingDisabled
+        || paintingControl == PaintingControlSetting::DisplayListConstructionDisabled)
         disabledMode = GraphicsContext::FullyDisabled;
-    else
-        disabledMode = GraphicsContext::NothingDisabled;
 
     if (displayItemList())
         context = adoptPtr(new GraphicsContext(displayItemList(), disabledMode));
@@ -240,7 +239,7 @@ void WebPopupMenuImpl::paintContents(WebDisplayItemList* webDisplayItemList, con
     if (!m_widget)
         return;
 
-    if (paintingControl == WebContentLayerClient::DisplayListCachingDisabled && m_displayItemList)
+    if (paintingControl != WebContentLayerClient::PaintDefaultBehavior && m_displayItemList)
         m_displayItemList->invalidateAll();
 
     paintContents(static_cast<WebCanvas*>(nullptr), clip, paintingControl);
