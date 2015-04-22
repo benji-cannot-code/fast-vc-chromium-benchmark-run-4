a11d61591bfcc511aa1625ffc5bbffa5a2d4767d
paritosh.in@samsung.com <paritosh.in@samsung.com>
2015-04-22 10:16:51 +0000

    Introducing FetchEventInit Dictionary.
    Spec: https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#fetch-event-section
    
    This cl is progress on https://codereview.chromium.org/1079073003/
    
    BUG=411193,  479536
    
    Review URL: https://codereview.chromium.org/1099073002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194206 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/FetchEvent.h
+++ third_party/WebKit/Source/modules/serviceworkers/FetchEvent.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "modules/EventModules.h"
 #include "modules/fetch/Request.h"
+#include "modules/serviceworkers/ExtendableEvent.h"
+#include "modules/serviceworkers/FetchEventInit.h"
 #include "modules/serviceworkers/RespondWithObserver.h"
 #include "platform/heap/Handle.h"
 
@@ -20,11 +22,12 @@ class RespondWithObserver;
 // A fetch event is dispatched by the client to a service worker's script
 // context. RespondWithObserver can be used to notify the client about the
 // service worker's response.
-class FetchEvent final : public Event {
+class FetchEvent final : public ExtendableEvent {
     DEFINE_WRAPPERTYPEINFO();
 public:
     static PassRefPtrWillBeRawPtr<FetchEvent> create();
-    static PassRefPtrWillBeRawPtr<FetchEvent> create(RespondWithObserver*, Request*);
+    static PassRefPtrWillBeRawPtr<FetchEvent> create(const AtomicString& type, FetchEventInit&);
+    static PassRefPtrWillBeRawPtr<FetchEvent> create(const AtomicString& type, FetchEventInit&, RespondWithObserver*);
 
     Request* request() const;
     bool isReload() const;
@@ -33,13 +36,11 @@ public:
 
     virtual const AtomicString& interfaceName() const override;
 
-    void setIsReload(bool);
-
     DECLARE_VIRTUAL_TRACE();
 
 protected:
     FetchEvent();
-    FetchEvent(RespondWithObserver*, Request*);
+    FetchEvent(const AtomicString& type, const FetchEventInit&, RespondWithObserver*);
 
 private:
     PersistentWillBeMember<RespondWithObserver> m_observer;
