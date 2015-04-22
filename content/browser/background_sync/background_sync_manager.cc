7861972bad31b8fa777289336beccebb06cd4727
jkarlin <jkarlin@chromium.org>
2015-04-22 10:55:07 -0700

    [BackgroundSync] Store origin with registration data
    The origin GURL needs to be stored along with the registration for
    looking up service worker registrations (to start the necessary service worker and
    fire its events).
    
    BUG=479665
    
    Review URL: https://codereview.chromium.org/1084043003
    
    Cr-Commit-Position: refs/heads/master@{#326339}

--- content/browser/background_sync/background_sync_manager.cc
+++ content/browser/background_sync/background_sync_manager.cc
@@ -29,10 +29,7 @@ BackgroundSyncManager::BackgroundSyncRegistrations::
     BackgroundSyncRegistrations()
     : next_id(BackgroundSyncRegistration::kInitialId) {
 }
-BackgroundSyncManager::BackgroundSyncRegistrations::BackgroundSyncRegistrations(
-    BackgroundSyncRegistration::RegistrationId next_id)
-    : next_id(next_id) {
-}
+
 BackgroundSyncManager::BackgroundSyncRegistrations::
     ~BackgroundSyncRegistrations() {
 }
@@ -207,10 +204,10 @@ void BackgroundSyncManager::InitDidGetDataFromBackend(
   for (const std::pair<int64, std::string>& data : user_data) {
     BackgroundSyncRegistrationsProto registrations_proto;
     if (registrations_proto.ParseFromString(data.second)) {
-      sw_to_registrations_map_[data.first] = BackgroundSyncRegistrations(
-          registrations_proto.next_registration_id());
       BackgroundSyncRegistrations* registrations =
           &sw_to_registrations_map_[data.first];
+      registrations->next_id = registrations_proto.next_registration_id();
+      registrations->origin = GURL(registrations_proto.origin());
 
       for (int i = 0, max = registrations_proto.registration_size(); i < max;
            ++i) {
@@ -277,10 +274,10 @@ void BackgroundSyncManager::RegisterImpl(
       &sw_to_registrations_map_[sw_registration_id];
   new_registration.id = registrations->next_id++;
 
-  AddRegistrationToMap(sw_registration_id, new_registration);
+  AddRegistrationToMap(sw_registration_id, origin, new_registration);
 
   StoreRegistrations(
-      origin, sw_registration_id,
+      sw_registration_id,
       base::Bind(&BackgroundSyncManager::RegisterDidStore,
                  weak_ptr_factory_.GetWeakPtr(), sw_registration_id,
                  new_registration, callback));
@@ -343,6 +340,9 @@ BackgroundSyncManager::LookupRegistration(
     return nullptr;
 
   BackgroundSyncRegistrations& registrations = it->second;
+  DCHECK_LE(BackgroundSyncRegistration::kInitialId, registrations.next_id);
+  DCHECK(!registrations.origin.is_empty());
+
   auto key_and_registration_iter =
       registrations.registration_map.find(registration_key);
   if (key_and_registration_iter == registrations.registration_map.end())
@@ -352,7 +352,6 @@ BackgroundSyncManager::LookupRegistration(
 }
 
 void BackgroundSyncManager::StoreRegistrations(
-    const GURL& origin,
     int64 sw_registration_id,
     const ServiceWorkerStorage::StatusCallback& callback) {
   // Serialize the data.
@@ -360,6 +359,7 @@ void BackgroundSyncManager::StoreRegistrations(
       sw_to_registrations_map_[sw_registration_id];
   BackgroundSyncRegistrationsProto registrations_proto;
   registrations_proto.set_next_registration_id(registrations.next_id);
+  registrations_proto.set_origin(registrations.origin.spec());
 
   for (const auto& key_and_registration : registrations.registration_map) {
     const BackgroundSyncRegistration& registration =
@@ -377,8 +377,8 @@ void BackgroundSyncManager::StoreRegistrations(
   bool success = registrations_proto.SerializeToString(&serialized);
   DCHECK(success);
 
-  StoreDataInBackend(sw_registration_id, origin, kBackgroundSyncUserDataKey,
-                     serialized, callback);
+  StoreDataInBackend(sw_registration_id, registrations.origin,
+                     kBackgroundSyncUserDataKey, serialized, callback);
 }
 
 void BackgroundSyncManager::RegisterDidStore(
@@ -421,12 +421,14 @@ void BackgroundSyncManager::RemoveRegistrationFromMap(
 
 void BackgroundSyncManager::AddRegistrationToMap(
     int64 sw_registration_id,
+    const GURL& origin,
     const BackgroundSyncRegistration& sync_registration) {
   DCHECK_NE(BackgroundSyncRegistration::kInvalidRegistrationId,
             sw_registration_id);
 
   BackgroundSyncRegistrations* registrations =
       &sw_to_registrations_map_[sw_registration_id];
+  registrations->origin = origin;
 
   RegistrationKey registration_key(sync_registration);
   registrations->registration_map[registration_key] = sync_registration;
@@ -476,7 +478,7 @@ void BackgroundSyncManager::UnregisterImpl(
   RemoveRegistrationFromMap(sw_registration_id, registration_key);
 
   StoreRegistrations(
-      origin, sw_registration_id,
+      sw_registration_id,
       base::Bind(&BackgroundSyncManager::UnregisterDidStore,
                  weak_ptr_factory_.GetWeakPtr(), sw_registration_id, callback));
 }
--- content/browser/background_sync/background_sync_manager.h
+++ content/browser/background_sync/background_sync_manager.h
@@ -158,12 +158,11 @@ class CONTENT_EXPORT BackgroundSyncManager
         std::map<RegistrationKey, BackgroundSyncRegistration>;
 
     BackgroundSyncRegistrations();
-    explicit BackgroundSyncRegistrations(
-        BackgroundSyncRegistration::RegistrationId next_id);
     ~BackgroundSyncRegistrations();
 
     RegistrationMap registration_map;
     BackgroundSyncRegistration::RegistrationId next_id;
+    GURL origin;
   };
 
   using PermissionStatusCallback = base::Callback<void(bool)>;
@@ -189,8 +188,7 @@ class CONTENT_EXPORT BackgroundSyncManager
       const RegistrationKey& registration_key);
 
   // Store all registrations for a given |sw_registration_id|.
-  void StoreRegistrations(const GURL& origin,
-                          int64 sw_registration_id,
+  void StoreRegistrations(int64 sw_registration_id,
                           const ServiceWorkerStorage::StatusCallback& callback);
 
   // Removes the registration if it is in the map.
@@ -199,6 +197,7 @@ class CONTENT_EXPORT BackgroundSyncManager
 
   void AddRegistrationToMap(
       int64 sw_registration_id,
+      const GURL& origin,
       const BackgroundSyncRegistration& sync_registration);
 
   void InitImpl(const base::Closure& callback);
