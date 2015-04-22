b903c23e8006dd9da569b9b9539ccda76d79adc9
horo@chromium.org <horo@chromium.org>
2015-04-22 09:54:29 +0000

    Fix test failure of http/tests/inspector/service-workers/service-workers-redundant.html
    This cl sets the "testPath" correctly when re-opening the inspector.
    
    BUG=472488
    
    Review URL: https://codereview.chromium.org/1094273006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194205 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/inspector-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/inspector-test.js
@@ -883,7 +883,7 @@ function closeFrontend(callback)
 function openFrontendAndIncrement()
 {
     frontendReopeningCount++;
-    testRunner.showWebInspector();
+    testRunner.showWebInspector(JSON.stringify({testPath: '"' + location.href + '"'}).replace(/\"/g,"%22"));
     setTimeout(runTest, 1);
 }
 
