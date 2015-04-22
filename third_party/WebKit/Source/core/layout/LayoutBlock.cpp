69df3b3bf0adf3851ecc73880464ef6af8a73316
shivamidow@gmail.com <shivamidow@gmail.com>
2015-04-22 13:16:13 +0000

    Anonymous block is not removed even though it is no more required.
    When force re-layout happens after first layout, some floating elements take wrong positions.
    This is because anonymous block which wraps static positioned element is not removed
    after it's unnecessary (e,g restoring origical content size).
    In the first for loop of LayoutBlock::removeAnonymousWrappersIfRequired, we check
    if floating or outOfFlowPositioned elements are also childrenInline or continuation.
    But this doesn't make sense since they are placed out of the flow so that we don't need to
    care about them when deciding to remove or not anonymous block.
    
    Actually this bug doesn't happen on WebKit since it doesn't stop the procedure deciding to remove
    anonymous block for children which are floating or outOfFlowPositioned.
    http://trac.webkit.org/browser/trunk/Source/WebCore/rendering/RenderElement.cpp#L948
    
    BUG=472122
    
    Review URL: https://codereview.chromium.org/1052233004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194212 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutBlock.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlock.cpp
@@ -1097,8 +1097,11 @@ void LayoutBlock::removeAnonymousWrappersIfRequired()
     ASSERT(isLayoutBlockFlow());
     Vector<LayoutBox*, 16> blocksToRemove;
     for (LayoutBox* child = firstChildBox(); child; child = child->nextSiblingBox()) {
+        if (child->isFloatingOrOutOfFlowPositioned())
+            continue;
+
         // There are still block children in the container, so any anonymous wrappers are still needed.
-        if (!child->isAnonymousBlock() && !child->isFloatingOrOutOfFlowPositioned())
+        if (!child->isAnonymousBlock())
             return;
         // We can't remove anonymous wrappers if they contain continuations as this means there are block children present.
         if (child->isLayoutBlock() && toLayoutBlock(child)->continuation())
@@ -1106,6 +1109,7 @@ void LayoutBlock::removeAnonymousWrappersIfRequired()
         // We are only interested in removing anonymous wrappers if there are inline siblings underneath them.
         if (!child->childrenInline())
             return;
+
         if (child->isAnonymousBlock())
             blocksToRemove.append(child);
     }
