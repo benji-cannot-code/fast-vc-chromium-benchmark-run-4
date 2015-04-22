491f7df238cf0ae80bd813ecffa0efefd4bc964a
pkasting <pkasting@chromium.org>
2015-04-21 17:14:41 -0700

    Misc. minor cleanups/simplifications.
    No functional change.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1097813002
    
    Cr-Commit-Position: refs/heads/master@{#326184}

--- chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
+++ chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
@@ -884,7 +884,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, AcceptKeywordBySpace) {
   ASSERT_TRUE(omnibox_view->GetText().empty());
 
   // Revert to keyword hint mode.
-  omnibox_view->model()->ClearKeyword(base::string16());
+  omnibox_view->model()->ClearKeyword();
   ASSERT_TRUE(omnibox_view->model()->is_keyword_hint());
   ASSERT_EQ(search_keyword, omnibox_view->model()->keyword());
   ASSERT_EQ(search_keyword, omnibox_view->GetText());
@@ -899,7 +899,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, AcceptKeywordBySpace) {
   ASSERT_TRUE(omnibox_view->GetText().empty());
 
   // Revert to keyword hint mode.
-  omnibox_view->model()->ClearKeyword(base::string16());
+  omnibox_view->model()->ClearKeyword();
   ASSERT_TRUE(omnibox_view->model()->is_keyword_hint());
   ASSERT_EQ(search_keyword, omnibox_view->model()->keyword());
   ASSERT_EQ(search_keyword, omnibox_view->GetText());
--- chrome/browser/ui/views/omnibox/omnibox_view_views.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -417,10 +417,8 @@ bool OmniboxViewViews::HandleEarlyTabActions(const ui::KeyEvent& event) {
   if (!views::FocusManager::IsTabTraversalKeyEvent(event))
     return false;
 
-  if (model()->is_keyword_hint() && !event.IsShiftDown()) {
-    model()->AcceptKeyword(ENTERED_KEYWORD_MODE_VIA_TAB);
-    return true;
-  }
+  if (model()->is_keyword_hint() && !event.IsShiftDown())
+    return model()->AcceptKeyword(ENTERED_KEYWORD_MODE_VIA_TAB);
 
   if (!model()->popup_model()->IsOpen())
     return false;
@@ -428,7 +426,7 @@ bool OmniboxViewViews::HandleEarlyTabActions(const ui::KeyEvent& event) {
   if (event.IsShiftDown() &&
       (model()->popup_model()->selected_line_state() ==
           OmniboxPopupModel::KEYWORD))
-    model()->ClearKeyword(text());
+    model()->ClearKeyword();
   else
     model()->OnUpOrDownKeyPressed(event.IsShiftDown() ? -1 : 1);
 
@@ -931,7 +929,7 @@ bool OmniboxViewViews::HandleKeyEvent(views::Textfield* textfield,
     if (model()->is_keyword_hint() || model()->keyword().empty() ||
         HasSelection() || GetCursorPosition() != 0)
       return false;
-    model()->ClearKeyword(text());
+    model()->ClearKeyword();
     return true;
   }
 
