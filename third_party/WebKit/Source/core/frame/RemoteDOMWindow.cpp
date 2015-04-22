6c3c5996aac13ac317b02e60910c47c016069b5f
yukishiino@chromium.org <yukishiino@chromium.org>
2015-04-22 04:16:35 +0000

    bindings: Do not use ScriptWrappable::wrap for RemoteDOMWindow.
    Custom toV8(EventTarget*) has been handling LocalDOMWindow in a special
    manner so that custom toV8(DOMWindow*) is always used.  However, when
    RemoteDOMWindow, and DOMWindow as their superclass, were introduced, we
    didn't add the same support for RemoteDOMWindow nor DOMWindow.  It was
    wrong.
    
    When toV8(EventTarget*) is called with a RemoteDOMWindow before
    installDOMWindow is called, toV8(EventTarget*) calls
    RemoteDOMWindow::wrap() and it creates a wrapper.  After that, when
    installDOMWindow is called, the wrapper already exists for the
    RemoteDOMWindow and it causes crash.
    
    I think this is the cause of mysterious crashes around installDOMWindow.
    
    This CL fixes:
    - Makes sure that DOMWindow::wrap is never called.
    - Calls toV8(DOMWindow*) for not only LocalDOMWindow but also
      RemoteDOMWindow.
    
    I've confirmed that crash occurs at www.nhl.com with --site-per-process
    specified, and fix with this CL.
    
    BUG=478890,477410,475880
    
    Review URL: https://codereview.chromium.org/1100693002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194193 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/RemoteDOMWindow.cpp
+++ third_party/WebKit/Source/core/frame/RemoteDOMWindow.cpp
@@ -13,11 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-const AtomicString& RemoteDOMWindow::interfaceName() const
-{
-    return emptyAtom;
-}
-
 ExecutionContext* RemoteDOMWindow::executionContext() const
 {
     return nullptr;
