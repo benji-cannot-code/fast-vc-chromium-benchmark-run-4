3a4368de1b36be4307c7f4837af57f7ac1e7eb46
jackhou <jackhou@chromium.org>
2015-04-21 18:20:21 -0700

    [Mac] Don't call AppWindow::OSFullscreen if entering fullscreen via another mode.
    NativeAppWindowCocoa::WindowDidEnterFullscreen is triggered whenever
    the window goes into fullscreen. It should only call
    AppWindow::OSFullscreen when it was actually an OS-level fullscreen,
    i.e. was not fullscreened via the HTML or chrome.app.window API.
    
    TEST=Steps in bug comment #0.
    
    BUG=477649
    
    Review URL: https://codereview.chromium.org/1093333002
    
    Cr-Commit-Position: refs/heads/master@{#326208}

--- chrome/browser/ui/cocoa/apps/native_app_window_cocoa.mm
+++ chrome/browser/ui/cocoa/apps/native_app_window_cocoa.mm
@@ -856,8 +856,10 @@ void NativeAppWindowCocoa::WindowDidDeminiaturize() {
 }
 
 void NativeAppWindowCocoa::WindowDidEnterFullscreen() {
-  is_fullscreen_ = true;
-  app_window_->OSFullscreen();
+  if (!is_fullscreen_) {
+    is_fullscreen_ = true;
+    app_window_->OSFullscreen();
+  }
   app_window_->OnNativeWindowChanged();
 }
 
