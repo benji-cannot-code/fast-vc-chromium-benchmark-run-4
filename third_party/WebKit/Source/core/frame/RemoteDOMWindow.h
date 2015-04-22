fc12a07fecefc04a835491df1d715171f7359e36
alancutter@chromium.org <alancutter@chromium.org>
2015-04-22 08:29:45 +0000

    Revert of bindings: Do not use ScriptWrappable::wrap for RemoteDOMWindow. (patchset #4 id:60001 of https://codereview.chromium.org/1100693002/)
    Reason for revert:
    This causes the following test to fail:
    out/Release/browser_tests --gtest_filter=ActivityLogApiTest.TriggerEvent
    This is preventing Blink from rolling:
    https://codereview.chromium.org/1102483002
    
    Original issue's description:
    > bindings: Do not use ScriptWrappable::wrap for RemoteDOMWindow.
    > 
    > Custom toV8(EventTarget*) has been handling LocalDOMWindow in a special
    > manner so that custom toV8(DOMWindow*) is always used.  However, when
    > RemoteDOMWindow, and DOMWindow as their superclass, were introduced, we
    > didn't add the same support for RemoteDOMWindow nor DOMWindow.  It was
    > wrong.
    > 
    > When toV8(EventTarget*) is called with a RemoteDOMWindow before
    > installDOMWindow is called, toV8(EventTarget*) calls
    > RemoteDOMWindow::wrap() and it creates a wrapper.  After that, when
    > installDOMWindow is called, the wrapper already exists for the
    > RemoteDOMWindow and it causes crash.
    > 
    > I think this is the cause of mysterious crashes around installDOMWindow.
    > 
    > This CL fixes:
    > - Makes sure that DOMWindow::wrap is never called.
    > - Calls toV8(DOMWindow*) for not only LocalDOMWindow but also
    >   RemoteDOMWindow.
    > 
    > I've confirmed that crash occurs at www.nhl.com with --site-per-process
    > specified, and fix with this CL.
    > 
    > BUG=478890,477410,475880
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=194193
    
    TBR=dcheng@chromium.org,haraken@chromium.org,yukishiino@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=478890,477410,475880
    
    Review URL: https://codereview.chromium.org/1100893002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/RemoteDOMWindow.h
+++ third_party/WebKit/Source/core/frame/RemoteDOMWindow.h
@@ -19,6 +19,7 @@ public:
     }
 
     // EventTarget overrides:
+    const AtomicString& interfaceName() const override;
     ExecutionContext* executionContext() const override;
 
     // DOMWindow overrides:
--- third_party/WebKit/Source/web/tests/ActivityLoggerTest.cpp
+++ third_party/WebKit/Source/web/tests/ActivityLoggerTest.cpp
@@ -116,7 +116,7 @@ TEST_F(ActivityLoggerTest, EventHandler)
         "blinkAddEventListener | A | click\n"
         "blinkAddElement | a | \n"
         "blinkAddEventListener | BODY | change\n"
-        "blinkAddEventListener | DOMWindow | focus\n"
+        "blinkAddEventListener | LocalDOMWindow | focus\n"
         "blinkAddEventListener | BODY | onload";
     executeScriptInMainWorld(code);
     ASSERT_TRUE(verifyActivities(""));
