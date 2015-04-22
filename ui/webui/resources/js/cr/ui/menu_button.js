69c9c651d1b6a3f2967063f188de6906294179b8
deepak.m1 <deepak.m1@samsung.com>
2015-04-21 22:04:27 -0700

    Web content context menu shows over chrome://bookmarks menu.
    Earlier 'contextmenu' event is getting passed to the background page and
    due to this WebPage context menu is coming on top of menu button context menu.
    Changes done so that 'contextmenu' event will get handled and not passed to background
    page on similar lines as in context_menuu_handler.js file.
    
    BUG=442200
    
    Review URL: https://codereview.chromium.org/1088023003
    
    Cr-Commit-Position: refs/heads/master@{#326230}

--- ui/webui/resources/js/cr/ui/menu_button.js
+++ ui/webui/resources/js/cr/ui/menu_button.js
@@ -155,6 +155,14 @@ cr.define('cr.ui', function() {
         case 'resize':
           this.hideMenu();
           break;
+        case 'contextmenu':
+          if ((!this.menu || !this.menu.contains(e.target)) &&
+              (!this.hideTimestamp_ || Date.now() - this.hideTimestamp_ > 50))
+            this.showMenu(true);
+          e.preventDefault();
+          // Don't allow elements further up in the DOM to show their menus.
+          e.stopPropagation();
+          break;
       }
     },
 
@@ -187,6 +195,7 @@ cr.define('cr.ui', function() {
       this.showingEvents_.add(doc, 'scroll', this, true);
       this.showingEvents_.add(win, 'popstate', this);
       this.showingEvents_.add(win, 'resize', this);
+      this.showingEvents_.add(this.menu, 'contextmenu', this);
       this.showingEvents_.add(this.menu, 'activate', this);
       this.positionMenu_();
 
@@ -213,6 +222,11 @@ cr.define('cr.ui', function() {
 
       this.showingEvents_.removeAll();
       this.focus();
+
+      // On windows we might hide the menu in a right mouse button up and if
+      // that is the case we wait some short period before we allow the menu
+      // to be shown again.
+      this.hideTimestamp_ = cr.isWindows ? Date.now() : 0;
     },
 
     /**
