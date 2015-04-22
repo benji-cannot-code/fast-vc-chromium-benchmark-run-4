9cbd5b82d7922c64e58d718b30d05a188e6e082b
japhet <japhet@chromium.org>
2015-04-22 16:52:59 -0700

    Ensure we properly set PageTransition for iframes.
    We currently don't set subframe navigations as manual when it isn't the first
    navigation of the iframe. Also, we don't propagate the state correctly in the
    case of a cross-process transition.
    
    BUG=464014
    
    Review URL: https://codereview.chromium.org/1002953004
    
    Cr-Commit-Position: refs/heads/master@{#326404}

--- content/test/test_frame_navigation_observer.h
+++ content/test/test_frame_navigation_observer.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
+#include "content/public/browser/navigation_details.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/test/test_utils.h"
 
@@ -20,7 +21,6 @@ namespace content {
 class FrameTreeNode;
 class RenderFrameHostImpl;
 class WebContents;
-struct LoadCommittedDetails;
 
 // For content_browsertests, which run on the UI thread, run a second
 // MessageLoop and quit when the navigation in a specific frame completes
@@ -38,6 +38,11 @@ class TestFrameNavigationObserver : public WebContentsObserver {
   // Runs a nested message loop and blocks until the expected number of
   // navigations are complete.
   void Wait();
+
+  // Returns the LoadCommittedDetails for the last navigation to commit.
+  const LoadCommittedDetails& load_committed_details() const {
+    return load_committed_details_;
+  }
 
  private:
   // WebContentsObserver
@@ -60,6 +65,8 @@ class TestFrameNavigationObserver : public WebContentsObserver {
 
   // The number of navigations to wait for.
   int number_of_navigations_;
+
+  LoadCommittedDetails load_committed_details_;
 
   // The MessageLoopRunner used to spin the message loop.
   scoped_refptr<MessageLoopRunner> message_loop_runner_;
