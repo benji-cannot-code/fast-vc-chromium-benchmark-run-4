50ece0a869d81263fffac69cd6d0a5a5c45625af
msw <msw@chromium.org>
2015-04-23 16:31:08 -0700

    Update run_mojo_media_renderer.py for shell and service changes.
    Updates the script to handle recent changes:
      html_viewer has moved to components.
      kiosk_wm is run by default.
      services built locally, don't need origin mapping?
      avoid html_document.cc:150 Check failed: !is_headless_
      --args-for is deprecated (pass app args as shell args)
    
    Also update mojo_media_application.cc arg init.
    
    Tested that this works with:
      gn gen out/Debug --args='is_debug=true use_goma=true proprietary_codecs=true ffmpeg_branding="ChromeOS" enable_media_mojo_renderer=true'
      ninja -C out/Debug/ -j 1500 -l 10 mojo media/mojo/services components/html_viewer mojo/services/network
      media/mojo/scripts/run_mojo_media_renderer.py --build-dir out/Debug --verbose http://wxh.kir/ensemble/kqed.html
    
    BUG=NONE
    TEST=Script works (or closer to working...)
    R=sky@chromium.org,xhwang@chromium.org
    
    Review URL: https://codereview.chromium.org/1088993004
    
    Cr-Commit-Position: refs/heads/master@{#326690}

--- media/mojo/services/mojo_media_application.cc
+++ media/mojo/services/mojo_media_application.cc
@@ -3,9 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/command_line.h"
 #include "base/logging.h"
-#include "base/strings/utf_string_conversions.h"
 #include "media/mojo/services/mojo_renderer_service.h"
 #include "mojo/application/application_runner_chromium.h"
 #include "third_party/mojo/src/mojo/public/c/system/main.h"
@@ -22,17 +20,6 @@ class MojoMediaApplication
  public:
   // mojo::ApplicationDelegate implementation.
   void Initialize(mojo::ApplicationImpl* app) override {
-    base::CommandLine::StringVector command_line_args;
-#if defined(OS_WIN)
-    for (const auto& arg : app->args())
-      command_line_args.push_back(base::UTF8ToUTF16(arg));
-#elif defined(OS_POSIX)
-    command_line_args = app->args();
-#endif
-
-    base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-    command_line->InitFromArgv(command_line_args);
-
     logging::LoggingSettings settings;
     settings.logging_dest = logging::LOG_TO_SYSTEM_DEBUG_LOG;
     logging::InitLogging(settings);
