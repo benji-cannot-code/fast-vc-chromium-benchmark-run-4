a803860602752aa3c824a37e7a665fdc597b2ccf
dfalcantara <dfalcantara@chromium.org>
2015-04-23 14:07:55 -0700

    Properly attach InfoBarContainer when it is swapped to a new WebContents
    * Moves the swapping of the InfoBarService from TabAndroid::SwapWebContents() to after TabAndroid::InitWebContents() is called from Tab.setContentViewCore().  This is necessary because InitWebContents attaches the InfoBarService to the WebContents; trying to swap before this point means that the InfoBarContainer will be observing nothing.
    
    * Fixes the ChromeShellActivity/Tab to properly update the ContentViewCore when it is swapped.  This fixes the "Distill page" menu item.
    
    * Adds a test to try to prevent this from happening in the future.
    
    BUG=469026
    TEST=InfoBarTest.testInfoBarContainerSwapsWebContents
    
    Review URL: https://codereview.chromium.org/1049383008
    
    Cr-Commit-Position: refs/heads/master@{#326642}

--- chrome/android/shell/java/src/org/chromium/chrome/shell/TabManager.java
+++ chrome/android/shell/java/src/org/chromium/chrome/shell/TabManager.java
@@ -176,6 +176,11 @@ public class TabManager extends LinearLayout {
             public void onToggleFullscreenMode(Tab tab, boolean enable) {
                 mToolbar.setVisibility(enable ? GONE : VISIBLE);
             }
+
+            @Override
+            public void onContentChanged(Tab tab) {
+                setupContent();
+            }
         });
         return tab;
     }
