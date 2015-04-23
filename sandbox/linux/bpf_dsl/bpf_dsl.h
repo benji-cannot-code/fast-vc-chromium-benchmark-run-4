f15de4bdb230203e3f6082a8e9837915a16e9225
nick <nick@chromium.org>
2015-04-23 12:54:59 -0700

    Update {virtual,override} to follow C++11 style in sandbox.
    The Google style guide states that only one of {virtual,override,final} should be used for each declaration, since override implies virtual and final implies both virtual and override.
    
    This patch was manually generated using a regex and a text editor.
    
    BUG=417463
    
    Review URL: https://codereview.chromium.org/1058123003
    
    Cr-Commit-Position: refs/heads/master@{#326616}

--- sandbox/linux/bpf_dsl/bpf_dsl.h
+++ sandbox/linux/bpf_dsl/bpf_dsl.h
@@ -34,8 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //      class SillyPolicy : public Policy {
 //       public:
 //        SillyPolicy() {}
-//        virtual ~SillyPolicy() {}
-//        virtual ResultExpr EvaluateSyscall(int sysno) const override {
+//        ~SillyPolicy() override {}
+//        ResultExpr EvaluateSyscall(int sysno) const override {
 //          if (sysno == __NR_fcntl) {
 //            Arg<int> fd(0), cmd(1);
 //            Arg<unsigned long> flags(2);
