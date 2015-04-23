2df8116d1dfe1bd24ac80bf7ccd515c343437051
rsorokin <rsorokin@chromium.org>
2015-04-23 08:52:34 -0700

    New GAIA UI for password change flow
    -Added element gaia-raised-on-focus-button.
    -gaia-* buttons are tabbed by default.
    -Added style for the class 'link-button' so paper-button will look like link.
    
    BUG=464238
    TBR=grt@chromium.org
    
    Review URL: https://codereview.chromium.org/1083333002
    
    Cr-Commit-Position: refs/heads/master@{#326528}

--- ui/login/display_manager.js
+++ ui/login/display_manager.js
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   SUPERVISED_USER_CREATION_FLOW: 4,
   SAML_PASSWORD_CONFIRM: 5,
   CONSUMER_MANAGEMENT_ENROLLMENT: 6,
+  PASSWORD_CHANGED: 7,
 };
 
 /* Possible UI states of the error screen. */
@@ -1007,9 +1008,11 @@ cr.define('cr.ui.login', function() {
   /**
    * Shows password changed screen that offers migration.
    * @param {boolean} showError Whether to show the incorrect password error.
+   * @param {string} email What user does reauth. Being used for display in the
+   * new UI.
    */
-  DisplayManager.showPasswordChangedScreen = function(showError) {
-    login.PasswordChangedScreen.show(showError);
+  DisplayManager.showPasswordChangedScreen = function(showError, email) {
+    login.PasswordChangedScreen.show(showError, email);
   };
 
   /**
