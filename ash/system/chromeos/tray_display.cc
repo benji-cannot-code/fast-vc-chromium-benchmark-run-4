4f2622cf41269bf4c58e8891ab0aa438010cf405
mukai <mukai@chromium.org>
2015-04-23 16:39:57 -0700

    Put display messages as 'additional' message.
    Notification title has 2-line limit, therefore sometimes long
    messages are cut off. The 'body' field has much more space for
    longer text.
    
    BUG=454827
    R=oshima@chromium.org
    TEST=manually
    
    Review URL: https://codereview.chromium.org/1094863006
    
    Cr-Commit-Position: refs/heads/master@{#326692}

--- ash/system/chromeos/tray_display.cc
+++ ash/system/chromeos/tray_display.cc
@@ -335,10 +335,9 @@ bool TrayDisplay::GetDisplayMessageForNotification(
 
     if (iter->second.configured_ui_scale() !=
         old_iter->second.configured_ui_scale()) {
-      *message_out = l10n_util::GetStringFUTF16(
+      *additional_message_out = l10n_util::GetStringFUTF16(
           IDS_ASH_STATUS_TRAY_DISPLAY_RESOLUTION_CHANGED,
-          GetDisplayName(iter->first),
-          GetDisplaySize(iter->first));
+          GetDisplayName(iter->first), GetDisplaySize(iter->first));
       return true;
     }
     if (iter->second.GetActiveRotation() !=
@@ -358,9 +357,8 @@ bool TrayDisplay::GetDisplayMessageForNotification(
           rotation_text_id = IDS_ASH_STATUS_TRAY_DISPLAY_ORIENTATION_270;
           break;
       }
-      *message_out = l10n_util::GetStringFUTF16(
-          IDS_ASH_STATUS_TRAY_DISPLAY_ROTATED,
-          GetDisplayName(iter->first),
+      *additional_message_out = l10n_util::GetStringFUTF16(
+          IDS_ASH_STATUS_TRAY_DISPLAY_ROTATED, GetDisplayName(iter->first),
           l10n_util::GetStringUTF16(rotation_text_id));
       return true;
     }
@@ -378,7 +376,7 @@ void TrayDisplay::CreateOrUpdateNotification(
   message_center::MessageCenter::Get()->RemoveNotification(
       kNotificationId, false /* by_user */);
 
-  if (message.empty())
+  if (message.empty() && additional_message.empty())
     return;
 
   // Don't display notifications for accelerometer triggered screen rotations.
