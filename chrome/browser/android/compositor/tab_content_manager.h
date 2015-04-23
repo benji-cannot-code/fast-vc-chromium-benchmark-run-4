03edda1739b89cd9ed775f0dad39e66e88579d76
wnwen <wnwen@chromium.org>
2015-04-22 17:24:38 -0700

    Rename ThumbnailStore back to ThumbnailCache.
    Review URL: https://codereview.chromium.org/1102573007
    
    Cr-Commit-Position: refs/heads/master@{#326414}

--- chrome/browser/android/compositor/tab_content_manager.h
+++ chrome/browser/android/compositor/tab_content_manager.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/scoped_ptr_hash_map.h"
 #include "base/memory/weak_ptr.h"
 #include "cc/layers/ui_resource_layer.h"
-#include "chrome/browser/android/thumbnail/thumbnail_store.h"
+#include "chrome/browser/android/thumbnail/thumbnail_cache.h"
 
 using base::android::ScopedJavaLocalRef;
 
@@ -33,7 +33,7 @@ namespace android {
 class ThumbnailLayer;
 
 // A native component of the Java TabContentManager class.
-class TabContentManager : public ThumbnailStoreObserver {
+class TabContentManager : public ThumbnailCacheObserver {
  public:
   static TabContentManager* FromJavaObject(jobject jobj);
 
@@ -93,7 +93,7 @@ class TabContentManager : public ThumbnailStoreObserver {
                                               jint min_forbidden_id);
   void GetDecompressedThumbnail(JNIEnv* env, jobject obj, jint tab_id);
 
-  // ThumbnailStoreObserver implementation;
+  // ThumbnailCacheObserver implementation;
   void OnFinishedThumbnailRead(TabId tab_id) override;
 
  private:
@@ -101,11 +101,6 @@ class TabContentManager : public ThumbnailStoreObserver {
   typedef base::hash_map<int, scoped_refptr<cc::Layer>> LayerMap;
   typedef base::hash_map<int, scoped_refptr<ThumbnailLayer>> ThumbnailLayerMap;
   typedef base::ScopedPtrHashMap<int, TabReadbackRequest> TabReadbackRequestMap;
-
-  // TODO(): The upstream ThumbnailCache class was temporarily renamed to
-  // ThumbnailStore to avoid conflict with downstream.  Please rename the
-  // upstream to ThumbnailCache once the downstream is in a good state.
-  typedef ThumbnailStore ThumbnailCache;
 
   void PutThumbnailIntoCache(int tab_id,
                              float thumbnail_scale,
