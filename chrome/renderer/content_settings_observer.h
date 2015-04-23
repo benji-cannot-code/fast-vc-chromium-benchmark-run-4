881634d9584c8b9f5874d40d69d54b2fc58f1ea8
mlamouri <mlamouri@chromium.org>
2015-04-23 04:24:54 -0700

    Remove references to WebPermissionCallbacks in Chromium.
    This is part of a suite of five CLs:
    1. https://codereview.chromium.org/1007083002
    2. https://codereview.chromium.org/1005313002
    3. https://codereview.chromium.org/1094343002
    4. <this>
    5. https://codereview.chromium.org/1006153002
    
    BUG=462255
    
    Review URL: https://codereview.chromium.org/1089243006
    
    Cr-Commit-Position: refs/heads/master@{#326492}

--- chrome/renderer/content_settings_observer.h
+++ chrome/renderer/content_settings_observer.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "content/public/renderer/render_frame_observer.h"
 #include "content/public/renderer/render_frame_observer_tracker.h"
-#include "third_party/WebKit/public/platform/WebPermissionCallbacks.h"
 #include "third_party/WebKit/public/web/WebContentSettingsClient.h"
 
 class GURL;
@@ -64,8 +63,6 @@ class ContentSettingsObserver
                              unsigned long estimated_size);
   virtual void requestFileSystemAccessAsync(
       const blink::WebContentSettingCallbacks& callbacks);
-  virtual void requestFileSystemAccessAsync(
-      const blink::WebPermissionCallbacks& callbacks);
   virtual bool allowImage(bool enabled_per_settings,
                           const blink::WebURL& image_url);
   virtual bool allowIndexedDB(const blink::WebString& name,
