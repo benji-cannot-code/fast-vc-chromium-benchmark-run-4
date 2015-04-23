393fb85326805378f68691db297f7a1b3cff79bd
oshima <oshima@chromium.org>
2015-04-23 01:07:43 -0700

    Move HasInternalDisplay to gfx::Display Remove IsInternalDisplay as it's not useful.
    BUG=None
    
    Review URL: https://codereview.chromium.org/1102713002
    
    Cr-Commit-Position: refs/heads/master@{#326475}

--- ash/system/chromeos/brightness/tray_brightness.cc
+++ ash/system/chromeos/brightness/tray_brightness.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/accelerators/accelerator_controller.h"
 #include "ash/ash_constants.h"
-#include "ash/display/display_manager.h"
 #include "ash/metrics/user_metrics_recorder.h"
 #include "ash/shell.h"
 #include "ash/shell_observer.h"
@@ -27,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/ash_resources.h"
 #include "grit/ash_strings.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/display.h"
 #include "ui/gfx/image/image.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/image_view.h"
@@ -274,7 +274,7 @@ void TrayBrightness::HandleBrightnessChanged(double percent,
   // Never show the bubble on systems that lack internal displays: if an
   // external display's brightness is changed, it may already display the new
   // level via an on-screen display.
-  if (!Shell::GetInstance()->display_manager()->HasInternalDisplay())
+  if (!gfx::Display::HasInternalDisplay())
     return;
 
   if (brightness_view_)
