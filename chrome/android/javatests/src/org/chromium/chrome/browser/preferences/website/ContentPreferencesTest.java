e06fc82e8fc3ddff5cfff22b477db8f284605531
mckev <mckev@amazon.com>
2015-04-23 14:30:10 -0700

    Use fake media stream in ContentPreferencesTest#testCameraMicNotBlocked
    This adds the --use-fake-device-for-media-stream flag to the
    ChromeShell test ContentPreferencesTest#testCameraMicNotBlocked, so that
    it can succeed when running on a device that does not have a
    microphone.
    
    BUG=475675
    TEST=ContentPreferencesTest#testCameraMicNotBlocked
    
    Review URL: https://codereview.chromium.org/1078923002
    
    Cr-Commit-Position: refs/heads/master@{#326647}

--- chrome/android/javatests/src/org/chromium/chrome/browser/preferences/website/ContentPreferencesTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/preferences/website/ContentPreferencesTest.java
@@ -10,7 +10,9 @@ import android.os.Bundle;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
+import org.chromium.chrome.ChromeSwitches;
 import org.chromium.chrome.browser.infobar.InfoBar;
 import org.chromium.chrome.browser.infobar.InfoBarContainer;
 import org.chromium.chrome.browser.location.LocationSettingsTestUtil;
@@ -365,6 +367,7 @@ public class ContentPreferencesTest extends ChromeShellTestBase {
      */
     @SmallTest
     @Feature({"Preferences"})
+    @CommandLineFlags.Add(ChromeSwitches.USE_FAKE_DEVICE_FOR_MEDIA_STREAM)
     public void testCameraMicNotBlocked() throws Exception {
         setEnableCameraMic(true);
 
