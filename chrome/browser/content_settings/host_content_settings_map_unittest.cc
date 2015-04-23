07e16f57629107f432992a9b947610091493c492
msramek <msramek@chromium.org>
2015-04-23 02:48:03 -0700

    Disable the ALLOW value for camera and mic content settings.
    We used to use
      MEDIASTREAM content setting for media default ( ASK / BLOCK )
      CAMERA / MIC content settings for media exceptions ( ALLOW / ASK / BLOCK )
    
    When we stopped using MEDIASTREAM and replaced it with CAMERA / MIC for defaults as well, we didn't ensure that the ALLOW value is still unsupported as default.
    
    This CL fixes that. Although during the short period when the default settings supported ALLOW values we had no UI to set it, we include a migration code just to be on the safe side.
    
    BUG=464382
    
    Review URL: https://codereview.chromium.org/1092743005
    
    Cr-Commit-Position: refs/heads/master@{#326486}

--- chrome/browser/content_settings/host_content_settings_map_unittest.cc
+++ chrome/browser/content_settings/host_content_settings_map_unittest.cc
@@ -966,6 +966,7 @@ TEST_F(HostContentSettingsMapTest, IsSettingAllowedForType) {
                   prefs, CONTENT_SETTING_ASK,
                   CONTENT_SETTINGS_TYPE_FULLSCREEN));
 
+  // The mediastream setting is deprecated.
   EXPECT_FALSE(HostContentSettingsMap::IsSettingAllowedForType(
                    prefs, CONTENT_SETTING_ALLOW,
                    CONTENT_SETTINGS_TYPE_MEDIASTREAM));
@@ -975,6 +976,21 @@ TEST_F(HostContentSettingsMapTest, IsSettingAllowedForType) {
   EXPECT_FALSE(HostContentSettingsMap::IsSettingAllowedForType(
                    prefs, CONTENT_SETTING_BLOCK,
                    CONTENT_SETTINGS_TYPE_MEDIASTREAM));
+
+  // We support the ALLOW value for media permission exceptions,
+  // but not as the default setting.
+  EXPECT_TRUE(HostContentSettingsMap::IsSettingAllowedForType(
+                  prefs, CONTENT_SETTING_ALLOW,
+                  CONTENT_SETTINGS_TYPE_MEDIASTREAM_MIC));
+  EXPECT_TRUE(HostContentSettingsMap::IsSettingAllowedForType(
+                  prefs, CONTENT_SETTING_ALLOW,
+                  CONTENT_SETTINGS_TYPE_MEDIASTREAM_CAMERA));
+  EXPECT_FALSE(HostContentSettingsMap::IsDefaultSettingAllowedForType(
+                   prefs, CONTENT_SETTING_ALLOW,
+                   CONTENT_SETTINGS_TYPE_MEDIASTREAM_MIC));
+  EXPECT_FALSE(HostContentSettingsMap::IsDefaultSettingAllowedForType(
+                   prefs, CONTENT_SETTING_ALLOW,
+                   CONTENT_SETTINGS_TYPE_MEDIASTREAM_CAMERA));
 
   // TODO(msramek): Add more checks for setting type - setting pairs where
   // it is not obvious whether or not they are allowed.
