79449e922392817cbdb77760fb9765dd30e64dcb
hashimoto <hashimoto@chromium.org>
2015-04-22 22:31:59 -0700

    Use SKIP_ON_SHUTDOWN shutdown behavior to seek device
    SequencedWorkerPool's default shutdown behavior is BLOCK_SHUTDOWN.
    Use SKIP_ON_SHUTDOWN instead to avoid blocking shutdown.
    
    BUG=418627
    
    Review URL: https://codereview.chromium.org/1055943003
    
    Cr-Commit-Position: refs/heads/master@{#326459}

--- chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_api.cc
+++ chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_api.cc
@@ -462,7 +462,9 @@ bool EasyUnlockPrivateSeekBluetoothDeviceByAddressFunction::RunAsync() {
       base::Bind(
           &EasyUnlockPrivateSeekBluetoothDeviceByAddressFunction::OnSeekFailure,
           this),
-      content::BrowserThread::GetBlockingPool());
+      content::BrowserThread::GetBlockingPool()->
+          GetTaskRunnerWithShutdownBehavior(
+              base::SequencedWorkerPool::CONTINUE_ON_SHUTDOWN).get());
   return true;
 }
 
