df374478290ff3d86f7abd637348b41b660d15a0
hiroshige@chromium.org <hiroshige@chromium.org>
2015-04-23 15:05:20 +0000

    Use threadSafeBind() in WorkerThread
    This CL replaces bind() used for cross-thread task posting with threadSafeBind().
    No behavior changes.
    
    BUG=479068
    
    Review URL: https://codereview.chromium.org/1106623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194300 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/workers/WorkerThread.cpp
+++ third_party/WebKit/Source/core/workers/WorkerThread.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/workers/WorkerThreadStartupData.h"
 #include "platform/PlatformThreadData.h"
 #include "platform/Task.h"
+#include "platform/ThreadSafeFunctional.h"
 #include "platform/ThreadTimers.h"
 #include "platform/heap/SafePoint.h"
 #include "platform/heap/ThreadState.h"
@@ -283,7 +284,7 @@ void WorkerThread::start()
         return;
 
     m_thread = createWebThreadSupportingGC();
-    m_thread->postTask(FROM_HERE, new Task(WTF::bind(&WorkerThread::initialize, this)));
+    m_thread->postTask(FROM_HERE, new Task(threadSafeBind(&WorkerThread::initialize, AllowCrossThreadAccess(this))));
 }
 
 void WorkerThread::interruptAndDispatchInspectorCommands()
