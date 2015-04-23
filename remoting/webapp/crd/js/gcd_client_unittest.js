8c7c5906173d546d93a0658e1690f0f18168faba
jrw <jrw@chromium.org>
2015-04-23 13:58:54 -0700

    Fixed implementation of patchRegistrationTicket.
    The previous implementation was wrong because it used the POST method rather than PATCH, and because it used OAuth where the spec calls for an unauthenticated call using a Google API key.
    
    BUG=471928
    
    Review URL: https://codereview.chromium.org/1098223005
    
    Cr-Commit-Position: refs/heads/master@{#326641}

--- remoting/webapp/crd/js/gcd_client_unittest.js
+++ remoting/webapp/crd/js/gcd_client_unittest.js
@@ -60,8 +60,7 @@ QUnit.module('gcd_client', {
     chromeMocks.identity.mock$setToken('fake_token');
     client = new remoting.gcd.Client({
       apiBaseUrl: 'https://fake.api',
-      apiKey: 'fake_key',
-      oauthClientId: 'fake_client_id'
+      apiKey: 'fake_key'
     });
   },
   teardown: function() {
@@ -111,12 +110,10 @@ QUnit.test('patchRegistrationTicket', function(assert) {
       200, {'Content-type': 'application/json'},
       JSON.stringify(FAKE_REGISTRATION_TICKET),
       function() {
-        assert.equal(fakeXhr.method, 'POST');
+        assert.equal(fakeXhr.method, 'PATCH');
         assert.equal(
             fakeXhr.url,
-            'https://fake.api/registrationTickets/fake_ticket_id');
-        assert.equal(fakeXhr.requestHeaders['Authorization'],
-                     'Bearer fake_token');
+            'https://fake.api/registrationTickets/fake_ticket_id?key=fake_key');
         assert.deepEqual(
             JSON.parse(fakeXhr.requestBody || ''), {
               deviceDraft: { 'fake_device_draft': true },
@@ -125,7 +122,7 @@ QUnit.test('patchRegistrationTicket', function(assert) {
       });
   return client.patchRegistrationTicket('fake_ticket_id', {
     'fake_device_draft': true
-  }).then(function(ticket) {
+  }, 'fake_client_id').then(function(ticket) {
     assert.deepEqual(ticket, FAKE_REGISTRATION_TICKET);
   });
 });
