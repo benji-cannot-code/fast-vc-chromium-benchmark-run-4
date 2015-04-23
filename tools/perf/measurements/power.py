99dd2e52ab65f70ba8eee6a52e0919b85815f5ec
rnephew <rnephew@chromium.org>
2015-04-23 14:45:37 -0700

    [Telemetry][Android] Add network data to android power tests.
    BUG=476043
    
    Review URL: https://codereview.chromium.org/1080463003
    
    Cr-Commit-Position: refs/heads/master@{#326657}

--- tools/perf/measurements/power.py
+++ tools/perf/measurements/power.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from telemetry.page import page_test
 
+from metrics import network
 from metrics import power
 
 
@@ -12,13 +13,20 @@ class Power(page_test.PageTest):
   def __init__(self):
     super(Power, self).__init__()
     self._power_metric = None
+    self._network_metric = None
 
   def WillStartBrowser(self, platform):
     self._power_metric = power.PowerMetric(platform)
+    self._network_metric = network.NetworkMetric(platform)
+
+  def WillNavigateToPage(self, page, tab):
+    self._network_metric.Start(page, tab)
 
   def DidNavigateToPage(self, page, tab):
     self._power_metric.Start(page, tab)
 
   def ValidateAndMeasurePage(self, page, tab, results):
+    self._network_metric.Stop(page, tab)
     self._power_metric.Stop(page, tab)
+    self._network_metric.AddResults(tab, results)
     self._power_metric.AddResults(tab, results)
