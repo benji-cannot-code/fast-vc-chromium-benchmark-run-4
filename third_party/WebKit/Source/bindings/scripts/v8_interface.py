67b02ac7f7bd245b746550eeed8a713f9575b5d7
jl@opera.com <jl@opera.com>
2015-04-23 09:47:34 +0000

    IDL: Drop [PerContextEnabled] extended attribute
    It is no longer used, and adds complexity to the code generator.
    
    BUG=465624
    
    Review URL: https://codereview.chromium.org/1107523002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194275 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/v8_interface.py
+++ third_party/WebKit/Source/bindings/scripts/v8_interface.py
@@ -296,7 +296,7 @@ def interface_context(interface):
     attributes = [v8_attributes.attribute_context(interface, attribute)
                   for attribute in interface.attributes]
 
-    has_conditional_attributes = any(attribute['per_context_enabled_function'] or attribute['exposed_test'] for attribute in attributes)
+    has_conditional_attributes = any(attribute['exposed_test'] for attribute in attributes)
     if has_conditional_attributes and interface.is_partial:
         raise Exception('Conditional attributes between partial interfaces in modules and the original interfaces(%s) in core are not allowed.' % interface.name)
 
@@ -305,15 +305,13 @@ def interface_context(interface):
         'has_accessor_configuration': any(
             attribute['is_expose_js_accessors'] and
             not (attribute['is_static'] or
-                 attribute['runtime_enabled_function'] or
-                 attribute['per_context_enabled_function']) and
+                 attribute['runtime_enabled_function']) and
             attribute['should_be_exposed_to_script']
             for attribute in attributes),
         'has_attribute_configuration': any(
              not (attribute['is_expose_js_accessors'] or
                   attribute['is_static'] or
-                  attribute['runtime_enabled_function'] or
-                  attribute['per_context_enabled_function'])
+                  attribute['runtime_enabled_function'])
              and attribute['should_be_exposed_to_script']
              for attribute in attributes),
         'has_conditional_attributes': has_conditional_attributes,
@@ -527,7 +525,6 @@ def interface_context(interface):
             # original interface will register instead of partial interface.
             if overloads['has_partial_overloads'] and interface.is_partial:
                 continue
-            per_context_enabled_function = overloads['per_context_enabled_function_all']
             conditionally_exposed_function = overloads['exposed_test_all']
             runtime_enabled_function = overloads['runtime_enabled_function_all']
             has_custom_registration = (overloads['has_custom_registration_all'] or
@@ -535,7 +532,6 @@ def interface_context(interface):
         else:
             if not method['visible']:
                 continue
-            per_context_enabled_function = method['per_context_enabled_function']
             conditionally_exposed_function = method['exposed_test']
             runtime_enabled_function = method['runtime_enabled_function']
             has_custom_registration = method['has_custom_registration']
@@ -543,7 +539,7 @@ def interface_context(interface):
         if has_custom_registration:
             custom_registration_methods.append(method)
             continue
-        if per_context_enabled_function or conditionally_exposed_function:
+        if conditionally_exposed_function:
             conditionally_enabled_methods.append(method)
             continue
         if runtime_enabled_function:
@@ -741,7 +737,6 @@ def overloads_context(interface, overloads):
         'maxarg': lengths[-1],
         'measure_all_as': common_value(overloads, 'measure_as'),  # [MeasureAs]
         'minarg': lengths[0],
-        'per_context_enabled_function_all': common_value(overloads, 'per_context_enabled_function'),  # [PerContextEnabled]
         'returns_promise_all': promise_overload_count > 0,
         'runtime_determined_lengths': runtime_determined_lengths,
         'runtime_enabled_function_all': common_value(overloads, 'runtime_enabled_function'),  # [RuntimeEnabled]
