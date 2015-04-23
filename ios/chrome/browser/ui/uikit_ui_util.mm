e089b211389ae3366fea8229dde89146bc3e61d1
jif <jif@chromium.org>
2015-04-23 09:53:46 -0700

    Add centering, cropping, and constraint setup methods to uikit_ui_util.
    BUG=None.
    
    Review URL: https://codereview.chromium.org/1079933002
    
    Cr-Commit-Position: refs/heads/master@{#326550}

--- ios/chrome/browser/ui/uikit_ui_util.mm
+++ ios/chrome/browser/ui/uikit_ui_util.mm
@@ -354,6 +354,13 @@ UIImage* BlurImage(UIImage* image,
   return outputImage;
 }
 
+UIImage* CropImage(UIImage* image, const CGRect& cropRect) {
+  CGImageRef cgImage = CGImageCreateWithImageInRect([image CGImage], cropRect);
+  UIImage* result = [UIImage imageWithCGImage:cgImage];
+  CGImageRelease(cgImage);
+  return result;
+}
+
 UIInterfaceOrientation GetInterfaceOrientation() {
   return [[UIApplication sharedApplication] statusBarOrientation];
 }
@@ -420,13 +427,21 @@ UIColor* InterpolateFromColorToColor(UIColor* firstColor,
 void ApplyVisualConstraints(NSArray* constraints,
                             NSDictionary* subviewsDictionary,
                             UIView* parentView) {
+  ApplyVisualConstraintsWithMetrics(constraints, subviewsDictionary, nil,
+                                    parentView);
+}
+
+void ApplyVisualConstraintsWithMetrics(NSArray* constraints,
+                                       NSDictionary* subviewsDictionary,
+                                       NSDictionary* metrics,
+                                       UIView* parentView) {
   for (NSString* constraint in constraints) {
     DCHECK([constraint isKindOfClass:[NSString class]]);
     [parentView
         addConstraints:[NSLayoutConstraint
                            constraintsWithVisualFormat:constraint
                                                options:0
-                                               metrics:nil
+                                               metrics:metrics
                                                  views:subviewsDictionary]];
   }
 }
@@ -439,6 +454,18 @@ void AddSameCenterXConstraint(UIView* parentView, UIView* subview) {
                                          relatedBy:NSLayoutRelationEqual
                                             toItem:parentView
                                          attribute:NSLayoutAttributeCenterX
+                                        multiplier:1
+                                          constant:0]];
+}
+
+void AddSameCenterYConstraint(UIView* parentView, UIView* subview) {
+  DCHECK_EQ(parentView, [subview superview]);
+  [parentView addConstraint:[NSLayoutConstraint
+                                constraintWithItem:subview
+                                         attribute:NSLayoutAttributeCenterY
+                                         relatedBy:NSLayoutRelationEqual
+                                            toItem:parentView
+                                         attribute:NSLayoutAttributeCenterY
                                         multiplier:1
                                           constant:0]];
 }
