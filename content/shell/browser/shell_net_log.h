205d4580f1ce2a4d439de1b00a006ce98f2b3f69
vishal.b <vishal.b@samsung.com>
2015-04-23 06:10:41 -0700

    net_log_logger_ replaced with write_to_file_observer_ considering https://codereview.chromium.org/1084533002 refactor
    BUG=473252
    
    TBR=jam
    
    Review URL: https://codereview.chromium.org/1079163007
    
    Cr-Commit-Position: refs/heads/master@{#326499}

--- content/shell/browser/shell_net_log.h
+++ content/shell/browser/shell_net_log.h
@@ -19,7 +19,7 @@ class ShellNetLog : public net::NetLog {
   ~ShellNetLog() override;
 
  private:
-  scoped_ptr<net::WriteToFileNetLogObserver> net_log_logger_;
+  scoped_ptr<net::WriteToFileNetLogObserver> write_to_file_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(ShellNetLog);
 };
