c4cf0935cfadc26a93e87ff64e98b911253e2e49
alancutter@chromium.org <alancutter@chromium.org>
2015-04-23 05:18:36 +0000

    Revert of DevTools: [console] Logged promise rejections do not change state once handled (patchset #13 id:240001 of https://codereview.chromium.org/1095943002/)
    Reason for revert:
    The following tests were made leaky due to this change:
    virtual/slimmingpaint/fast/css/fontfaceset-add-remove-while-loading.html
    inspector/console/console-revoke-error.html
    inspector/console/console-uncaught-promise.html
    inspector/sources/debugger/debugger-uncaught-promise-on-pause.html
    inspector/sources/debugger/debugger-pause-on-promise-rejection.html
    fast/css/fontfaceset-add-remove-while-loading.html
    http/tests/serviceworker/fetch-request-resources.html
    http/tests/fetch/window/fetch-base-https-other-https.html
    http/tests/fetch/window/fetch.html
    inspector-enabled/console/console-uncaught-promise-no-inspector.html
    web-animations-api/player-finished-promise.html
    web-animations-api/player-ready-promise.html
    
    http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Leak/builds/10612
    
    Confirmed with manual bisect.
    
    Original issue's description:
    > DevTools: [console] Logged promise rejections do not change state once handled
    > 
    > BUG=477557
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=194136
    
    TBR=dgozman@chromium.org,sergeyv@chromium.org,kozyatinskiy@chromium.org,pfeldman@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=477557
    
    Review URL: https://codereview.chromium.org/1090583005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194264 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/ConsoleMessage.h
+++ third_party/WebKit/Source/core/inspector/ConsoleMessage.h
@@ -52,10 +52,6 @@ public:
     void setTimestamp(double);
     WorkerGlobalScopeProxy* workerGlobalScopeProxy() { return m_workerProxy; }
     void setWorkerGlobalScopeProxy(WorkerGlobalScopeProxy* proxy) { m_workerProxy = proxy; }
-    unsigned assignMessageId();
-    unsigned messageId() const { return m_messageId; }
-    unsigned relatedMessageId() const { return m_relatedMessageId; }
-    void setRelatedMessageId(unsigned relatedMessageId) { m_relatedMessageId = relatedMessageId; }
 
     MessageSource source() const;
     MessageLevel level() const;
@@ -86,8 +82,6 @@ private:
     unsigned long m_requestIdentifier;
     double m_timestamp;
     WorkerGlobalScopeProxy* m_workerProxy;
-    unsigned m_messageId;
-    unsigned m_relatedMessageId;
 };
 
 } // namespace blink
