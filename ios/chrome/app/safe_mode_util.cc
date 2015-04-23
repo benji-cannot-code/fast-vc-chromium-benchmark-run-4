3901106816aea9f85c55707138ab3c29c0554225
sdefresne <sdefresne@chromium.org>
2015-04-23 09:35:13 -0700

    [iOS] Upstream safe mode utils
    SafeMode is a feature of Chrome on iOS that attempts to recover from crash
    loop on startup and provide additional information in crash reports.
    
    BUG=429756
    
    Review URL: https://codereview.chromium.org/1098273003
    
    Cr-Commit-Position: refs/heads/master@{#326539}

---
+++ ios/chrome/app/safe_mode_util.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/app/safe_mode_util.h"
+
+#include <mach-o/dyld.h>
+
+namespace safe_mode_util {
+
+std::vector<std::string> GetLoadedImages(const char* path_filter) {
+  std::vector<std::string> images;
+  uint32_t image_count = _dyld_image_count();
+  for (uint32_t i = 0; i < image_count; ++i) {
+    const char* path = _dyld_get_image_name(i);
+    if (path_filter && strncmp(path, path_filter, strlen(path_filter)) != 0)
+      continue;
+    images.push_back(path);
+  }
+  return images;
+}
+
+}  // namespace safe_mode_util
--- ios/chrome/ios_chrome.gyp
+++ ios/chrome/ios_chrome.gyp
@@ -9,6 +9,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
    },
   'targets': [
     {
+      'target_name': 'ios_chrome_app',
+      'type': 'static_library',
+      'include_dirs': [
+        '../..',
+      ],
+      'dependencies': [
+        'ios_chrome_browser',
+      ],
+      'link_settings': {
+        'libraries': [
+          '$(SDKROOT)/System/Library/Frameworks/Foundation.framework',
+        ],
+      },
+      'sources': [
+        'app/safe_mode_crashing_modules_config.h',
+        'app/safe_mode_crashing_modules_config.mm',
+        'app/safe_mode_util.h',
+        'app/safe_mode_util.cc',
+      ],
+    },
+    {
       'target_name': 'ios_chrome_browser',
       'type': 'static_library',
       'include_dirs': [
