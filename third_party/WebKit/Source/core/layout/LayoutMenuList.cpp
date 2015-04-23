a3bb6eeb6e8ee34672dca0497b6e662045eaac65
tkent@chromium.org <tkent@chromium.org>
2015-04-23 00:37:11 +0000

    Revert of New SELECT popup: LayoutMenuList may not exist after changing value when canceling the popup (patchset #3 id:40001 of https://codereview.chromium.org/1089943004/)
    Reason for revert:
    Caused another crash. crbug.com/479825
    
    
    
    Original issue's description:
    > New SELECT popup: LayoutMenuList may not exist after changing value when canceling the popup
    > 
    > LayoutMenuList::valueChanged clears out the provisional selection so we didn't need to clear the provisional selection after changing the value.
    > 
    > BUG=477932
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=194134
    
    TBR=keishi@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=477932
    
    Review URL: https://codereview.chromium.org/1063863006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194252 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutMenuList.cpp
+++ third_party/WebKit/Source/core/layout/LayoutMenuList.cpp
@@ -600,8 +600,10 @@ void LayoutMenuList::popupDidHide()
 
 void LayoutMenuList::popupDidCancel()
 {
-    if (m_indexToSelectOnCancel >= 0)
+    if (m_indexToSelectOnCancel >= 0) {
         valueChanged(m_indexToSelectOnCancel);
+        setIndexToSelectOnCancel(-1);
+    }
 }
 
 bool LayoutMenuList::itemIsSeparator(unsigned listIndex) const
