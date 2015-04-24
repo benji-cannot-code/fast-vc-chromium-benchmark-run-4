c6e85715967f52637eb7cfa318b6fb2516e15fac
Hans Wennborg <hans@chromium.org>
2015-04-24 14:38:04 -0700

    dbus/values_util_unittest.cc: use sqrt from cmath
    This should fix the following error on an ASan Linux builder:
    
    ../../dbus/values_util_unittest.cc:360:15: error: use of undeclared identifier 'sqrt'
        keys[i] = sqrt(values[i]);
                  ^
    
    BUG=none
    R=thakis@chromium.org
    TBR=satorux
    
    Review URL: https://codereview.chromium.org/1083323005
    
    Cr-Commit-Position: refs/heads/master@{#326882}

--- dbus/values_util_unittest.cc
+++ dbus/values_util_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "dbus/values_util.h"
 
+#include <cmath>
 #include <vector>
 
 #include "base/json/json_writer.h"
@@ -358,7 +359,7 @@ TEST(ValuesUtilTest, PopDoubleToIntDictionary) {
   const std::vector<int32> values(kValues, kValues + arraysize(kValues));
   std::vector<double> keys(values.size());
   for (size_t i = 0; i != values.size(); ++i)
-    keys[i] = sqrt(values[i]);
+    keys[i] = std::sqrt(values[i]);
 
   // Append a dictionary.
   scoped_ptr<Response> response(Response::CreateEmpty());
