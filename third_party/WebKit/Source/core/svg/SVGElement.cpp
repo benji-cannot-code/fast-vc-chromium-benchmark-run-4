d6f7a25aa9f6e40a9a49c94a0c1352fd0da92bef
ed@opera.com <ed@opera.com>
2015-04-24 09:17:43 +0000

    [svg] Update the <use> shadowtree if a referenced element is removed.
    BUG=477601
    
    Review URL: https://codereview.chromium.org/1087653004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194363 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGElement.cpp
@@ -967,9 +967,6 @@ void SVGElement::markForLayoutAndParentResourceInvalidation(LayoutObject* render
 
 void SVGElement::invalidateInstances()
 {
-    if (!inDocument())
-        return;
-
     if (instanceUpdatesBlocked())
         return;
 
@@ -982,14 +979,15 @@ void SVGElement::invalidateInstances()
         instance->setCorrespondingElement(0);
 
         if (SVGUseElement* element = instance->correspondingUseElement()) {
-            ASSERT(element->inDocument());
-            element->invalidateShadowTree();
+            if (element->inDocument())
+                element->invalidateShadowTree();
         }
     }
 
     svgRareData()->elementInstances().clear();
 
-    document().updateRenderTreeIfNeeded();
+    if (inDocument())
+        document().updateRenderTreeIfNeeded();
 }
 
 SVGElement::InstanceUpdateBlocker::InstanceUpdateBlocker(SVGElement* targetElement)
