38d2625ed63b520e9bdb79cd73400d3929a7a25c
alph@chromium.org <alph@chromium.org>
2015-04-24 11:56:27 +0000

    DevTools: Turn flamechart box selection into range selection
    It did not actually select anything along y axis, but the time range.
    
    Review URL: https://codereview.chromium.org/1099413002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194382 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/profiler/CPUProfileFlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/profiler/CPUProfileFlameChart.js
@@ -622,7 +622,7 @@ WebInspector.CPUProfileFlameChart.OverviewPane.prototype = {
      * @param {number} startTime
      * @param {number} endTime
      */
-    updateBoxSelection: function(startTime, endTime)
+    updateRangeSelection: function(startTime, endTime)
     {
     },
 
--- third_party/WebKit/Source/devtools/front_end/timeline/TimelinePanel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelinePanel.js
@@ -1419,7 +1419,7 @@ WebInspector.TimelineDetailsView.BottomUpChartView.prototype = {
      * @param {number} startTime
      * @param {number} endTime
      */
-    updateBoxSelection: function(startTime, endTime)
+    updateRangeSelection: function(startTime, endTime)
     {
     },
 
