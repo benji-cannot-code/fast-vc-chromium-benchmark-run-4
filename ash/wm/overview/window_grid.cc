1898c64a71ef26c6725c65b144585c28f6e82f38
flackr <flackr@chromium.org>
2015-04-24 09:14:22 -0700

    Draw a 2pt white stroke around selection rectangle in overview.
    This draws a light stroke around the selection rectangle so that it will be
    visible on dark or solid black backgrounds. Additionally the border painting
    code was fixed to not draw the corners twice, which with a transparent draw
    color was visible as more opaque borders in the corners.
    
    BUG=294860
    TEST=visual, enter overview mode (F5) and press arrow key to show selection.
    
    Review URL: https://codereview.chromium.org/1105563003
    
    Cr-Commit-Position: refs/heads/master@{#326812}

--- ash/wm/overview/window_grid.cc
+++ ash/wm/overview/window_grid.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/animation/tween.h"
 #include "ui/gfx/geometry/vector2d.h"
 #include "ui/views/background.h"
+#include "ui/views/border.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
 #include "ui/wm/core/window_animations.h"
@@ -93,8 +94,9 @@ const int kMinCardsMajor = 3;
 const int kOverviewSelectorTransitionMilliseconds = 100;
 
 // The color and opacity of the overview selector.
-const SkColor kWindowOverviewSelectionColor = SK_ColorBLACK;
-const unsigned char kWindowOverviewSelectorOpacity = 128;
+const SkColor kWindowSelectionColor = SkColorSetARGB(128, 0, 0, 0);
+const SkColor kWindowSelectionBorderColor = SkColorSetARGB(38, 255, 255, 255);
+const int kWindowSelectionBorderThickness = 2;
 
 // The minimum amount of spacing between the bottom of the text filtering
 // text field and the top of the selection widget on the first row of items.
@@ -369,7 +371,9 @@ void WindowGrid::InitSelectionWidget(WindowSelector::Direction direction) {
 
   views::View* content_view = new views::View;
   content_view->set_background(
-      views::Background::CreateSolidBackground(kWindowOverviewSelectionColor));
+      views::Background::CreateSolidBackground(kWindowSelectionColor));
+  content_view->SetBorder(views::Border::CreateSolidBorder(
+      kWindowSelectionBorderThickness, kWindowSelectionBorderColor));
   selection_widget_->SetContentsView(content_view);
   selection_widget_->GetNativeWindow()->parent()->StackChildAtBottom(
       selection_widget_->GetNativeWindow());
@@ -439,11 +443,11 @@ void WindowGrid::MoveSelectionWidgetToTarget(bool animate) {
     animation_settings.SetPreemptionStrategy(
         ui::LayerAnimator::IMMEDIATELY_ANIMATE_TO_NEW_TARGET);
     selection_widget_->SetBounds(SelectedWindow()->target_bounds());
-    selection_widget_->SetOpacity(kWindowOverviewSelectorOpacity);
+    selection_widget_->SetOpacity(255);
     return;
   }
   selection_widget_->SetBounds(SelectedWindow()->target_bounds());
-  selection_widget_->SetOpacity(kWindowOverviewSelectorOpacity);
+  selection_widget_->SetOpacity(255);
 }
 
 }  // namespace ash
