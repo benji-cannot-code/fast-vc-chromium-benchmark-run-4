d0573ea7345da8170b2b6f43e53f95d6658c1619
haraken@chromium.org <haraken@chromium.org>
2015-04-24 05:03:38 +0000

    Oilpan: Remove ScopeNesting
    AllowNesting is now used only by ThreadState::addInterrupter/removeInterrupter. These methods are called at the beginning and the end of program executions and thus shouldn't be called in a nested way. So we can remove AllowNesting from these methods. Then we can remove ScopeNesting.
    
    BUG=437144
    
    Review URL: https://codereview.chromium.org/1062783003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194346 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/ThreadState.cpp
+++ third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -1107,7 +1107,7 @@ void ThreadState::preSweep()
 void ThreadState::addInterruptor(Interruptor* interruptor)
 {
     checkThread();
-    SafePointScope scope(HeapPointersOnStack, SafePointScope::AllowNesting);
+    SafePointScope scope(HeapPointersOnStack);
     {
         MutexLocker locker(threadAttachMutex());
         m_interruptors.append(interruptor);
@@ -1117,7 +1117,7 @@ void ThreadState::addInterruptor(Interruptor* interruptor)
 void ThreadState::removeInterruptor(Interruptor* interruptor)
 {
     checkThread();
-    SafePointScope scope(HeapPointersOnStack, SafePointScope::AllowNesting);
+    SafePointScope scope(HeapPointersOnStack);
     {
         MutexLocker locker(threadAttachMutex());
         size_t index = m_interruptors.find(interruptor);
