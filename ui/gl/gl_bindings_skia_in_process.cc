e703935529e30c72af1be9d3e11ad1fb477d21bf
bsalomon <bsalomon@google.com>
2015-04-23 20:13:15 -0700

    Add glBlendBarrierKHR to Skia's GL bindings.
    BUG=480534
    
    TBR=sievers@chromium.org
    
    Review URL: https://codereview.chromium.org/1097153003
    
    Cr-Commit-Position: refs/heads/master@{#326731}

--- ui/gl/gl_bindings_skia_in_process.cc
+++ ui/gl/gl_bindings_skia_in_process.cc
@@ -65,6 +65,10 @@ GLvoid StubGLBindVertexArray(GLuint array) {
   glBindVertexArrayOES(array);
 }
 
+GLvoid StubGLBlendBarrier() {
+  glBlendBarrierKHR();
+}
+
 GLvoid StubGLBlendColor(GLclampf red, GLclampf green, GLclampf blue,
                         GLclampf alpha) {
   glBlendColor(red, green, blue, alpha);
@@ -674,6 +678,7 @@ GrGLInterface* CreateInProcessSkiaGLBinding() {
   functions->fBindFragDataLocation = StubGLBindFragDataLocation;
   functions->fBindTexture = StubGLBindTexture;
   functions->fBindVertexArray = StubGLBindVertexArray;
+  functions->fBlendBarrier = StubGLBlendBarrier;
   functions->fBlendColor = StubGLBlendColor;
   functions->fBlendEquation = StubGLBlendEquation;
   functions->fBlendFunc = StubGLBlendFunc;
