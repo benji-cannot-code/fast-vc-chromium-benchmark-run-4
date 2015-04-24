bb177ec2b5dcfd29c38e3ee5ad680bacc0e28791
enne@chromium.org <enne@chromium.org>
2015-04-24 19:53:16 +0000

    Remove WebViewImpl::setIsAcceleratedCompositingActive
    This function no longer makes sense now that WebViewImpl is either
    always or never composited.  The setup that is done when a new
    root layer is set is now done whenever those settings are changed,
    as the WebLayerTreeView is always there to store those values.
    
    There's nothing pressing here to change this; it's just previous
    cruft that never got removed when force compositing mode got turned
    on.  The new code seems a lot cleaner.
    
    BUG=474818
    
    Review URL: https://codereview.chromium.org/1078473002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194415 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/PinchViewport.cpp
+++ third_party/WebKit/Source/core/frame/PinchViewport.cpp
@@ -329,7 +329,8 @@ void PinchViewport::attachToLayerTree(GraphicsLayer* currentLayerTreeRoot, Graph
 {
     TRACE_EVENT1("blink", "PinchViewport::attachToLayerTree", "currentLayerTreeRoot", (bool)currentLayerTreeRoot);
     if (!currentLayerTreeRoot) {
-        m_innerViewportScrollLayer->removeAllChildren();
+        if (m_innerViewportScrollLayer)
+            m_innerViewportScrollLayer->removeAllChildren();
         return;
     }
 
