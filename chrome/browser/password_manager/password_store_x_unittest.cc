7cc7e18025ccf2e2c260c4822af573ac9f8a98af
xtreit <xtreit@gmail.com>
2015-04-24 08:06:21 -0700

    Substitued pattern push_back(ptr.release()) with push_back(ptr.Pass()) in the following directories: chrome/browser/password_manager components/password_manager
    BUG=457697
    
    Review URL: https://codereview.chromium.org/1083083007
    
    Cr-Commit-Position: refs/heads/master@{#326798}

--- chrome/browser/password_manager/password_store_x_unittest.cc
+++ chrome/browser/password_manager/password_store_x_unittest.cc
@@ -257,7 +257,7 @@ void InitExpectedForms(bool autofillable,
         autofillable,
         false,
         static_cast<double>(i + 1)};
-    forms->push_back(CreatePasswordFormFromDataForTesting(data).release());
+    forms->push_back(CreatePasswordFormFromDataForTesting(data).Pass());
   }
 }
 
--- components/password_manager/core/browser/password_form_manager_unittest.cc
+++ components/password_manager/core/browser/password_form_manager_unittest.cc
@@ -971,7 +971,7 @@ TEST_F(PasswordFormManagerTest, TestUpdateIncompleteCredentials) {
 
   // Feed the incomplete credentials to the manager.
   ScopedVector<PasswordForm> simulated_results;
-  simulated_results.push_back(incomplete_form.release());
+  simulated_results.push_back(incomplete_form.Pass());
   form_manager.OnGetPasswordStoreResults(simulated_results.Pass());
 
   form_manager.ProvisionallySave(
@@ -1373,7 +1373,7 @@ TEST_F(PasswordFormManagerTest, DriverDeletedBeforeStoreDone) {
   client_with_store.KillDriver();
 
   ScopedVector<PasswordForm> simulated_results;
-  simulated_results.push_back(form.release());
+  simulated_results.push_back(form.Pass());
   form_manager.OnGetPasswordStoreResults(simulated_results.Pass());
 }
 
