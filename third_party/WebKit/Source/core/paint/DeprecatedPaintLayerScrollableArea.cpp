a17befec8366d6fd5c8a920a68b19863acbcc522
wkorman@chromium.org <wkorman@chromium.org>
2015-04-25 01:28:48 +0000

    Fix typo -- isIntristicallyScrollable -> isIntrinsicallyScrollable.
    Review URL: https://codereview.chromium.org/1107733005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194427 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/DeprecatedPaintLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/paint/DeprecatedPaintLayerScrollableArea.cpp
@@ -547,7 +547,7 @@ void DeprecatedPaintLayerScrollableArea::deregisterForAnimation()
 
 bool DeprecatedPaintLayerScrollableArea::userInputScrollable(ScrollbarOrientation orientation) const
 {
-    if (box().isIntristicallyScrollable(orientation))
+    if (box().isIntrinsicallyScrollable(orientation))
         return true;
 
     EOverflow overflowStyle = (orientation == HorizontalScrollbar) ?
@@ -1357,7 +1357,7 @@ void DeprecatedPaintLayerScrollableArea::updateCompositingLayersAfterScroll()
 bool DeprecatedPaintLayerScrollableArea::usesCompositedScrolling() const
 {
     // Scroll form controls on the main thread so they exhibit correct touch scroll event bubbling
-    if (box().isIntristicallyScrollable(VerticalScrollbar) || box().isIntristicallyScrollable(HorizontalScrollbar))
+    if (box().isIntrinsicallyScrollable(VerticalScrollbar) || box().isIntrinsicallyScrollable(HorizontalScrollbar))
         return false;
 
     // See https://codereview.chromium.org/176633003/ for the tests that fail without this disabler.
