b31e327520829b2df7133afabb592f30b1a850b7
pilgrim@chromium.org <pilgrim@chromium.org>
2015-04-25 01:12:26 +0000

    Make LayoutObject::setNeedsOverflowRecalcAfterStyleChange private
    BUG=436473
    
    Review URL: https://codereview.chromium.org/1097073004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutObject.h
+++ third_party/WebKit/Source/core/layout/LayoutObject.h
@@ -1060,8 +1060,6 @@ public:
 
     virtual bool supportsPaintInvalidationStateCachedOffsets() const { return !hasColumns() && !hasTransformRelatedProperty() && !hasReflection() && !style()->isFlippedBlocksWritingMode(); }
 
-    void setNeedsOverflowRecalcAfterStyleChange();
-
     virtual LayoutRect viewRect() const;
 
     DisplayItemClient displayItemClient() const { return toDisplayItemClient(this); }
@@ -1202,6 +1200,8 @@ protected:
     virtual void invalidateDisplayItemClients(const LayoutBoxModelObject& paintInvalidationContainer) const;
 
 private:
+    void setNeedsOverflowRecalcAfterStyleChange();
+
     inline bool mustInvalidateFillLayersPaintOnWidthChange(const FillLayer&) const;
 
     // FIXME: This should be 'markContaingBoxChainForOverflowRecalc when we make LayoutBox
