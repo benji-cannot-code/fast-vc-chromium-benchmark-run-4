fbd028e633d77c4ce5e89338f4b371481c8fb9b1
spang <spang@chromium.org>
2015-04-27 12:45:43 -0700

    Revert of Move ScopedFileDescriptor to dbus/file_descriptor.h (patchset #1 id:1 of https://codereview.chromium.org/1090243003/)
    Reason for revert:
    Breaks shared_library build with broken exports:
    
    ./../dbus/dbus_export.h:20:65: error: ignoring attributes applied to class type 'scoped_ptr<dbus::FileDescriptor, dbus::FileDescriptor::Deleter>' outside of definition [-Werror=attributes]
     #define CHROME_DBUS_EXPORT __attribute__((visibility("default")))
                                                                     ^
    ../../dbus/file_descriptor.h:81:57: note: in expansion of macro 'CHROME_DBUS_EXPORT'
         scoped_ptr<FileDescriptor, FileDescriptor::Deleter> CHROME_DBUS_EXPORT;
    
    Original issue's description:
    > Move ScopedFileDescriptor to dbus/file_descriptor.h
    >
    > ScopedFileDescriptor was initially introduced to facilitate the
    > management of a 'lifeline' FD used when asking permission_broker to
    > open a port in the device's firewall on CrOS.  It's actually a scoped
    > wrapper around DBus::FileDescriptor, not a platform file descriptor. I
    > would like to use it to facilitate some other similar functionality in
    > which I create a pipe and pass one end over DBus, so it seems like a
    > good idea to move ScopedFileDescriptor into the dbus library directly.
    >
    > BUG=chromium:481340
    > TEST=FirewallHole unit tests still pass
    > STATUS=Fixed
    > R=keybuk, reillyg, stevenjb
    >
    > Committed: https://crrev.com/b44fbaabbe9f91e63ad8aa164609a6b31e0de57e
    > Cr-Commit-Position: refs/heads/master@{#327089}
    
    TBR=keybuk@chromium.org,reillyg@chromium.org,stevenjb@chromium.org,cmasone@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=chromium:481340
    
    Review URL: https://codereview.chromium.org/1108893003
    
    Cr-Commit-Position: refs/heads/master@{#327103}

--- chromeos/network/firewall_hole.cc
+++ chromeos/network/firewall_hole.cc
@@ -51,7 +51,7 @@ const char* PortTypeToString(FirewallHole::PortType type) {
 void PortReleased(FirewallHole::PortType type,
                   uint16_t port,
                   const std::string& interface,
-                  dbus::ScopedFileDescriptor lifeline_fd,
+                  FirewallHole::ScopedFileDescriptor lifeline_fd,
                   bool success) {
   if (!success) {
     LOG(WARNING) << "Failed to release firewall hole for "
@@ -62,13 +62,20 @@ void PortReleased(FirewallHole::PortType type,
 
 }  // namespace
 
+void CHROMEOS_EXPORT FirewallHole::FileDescriptorDeleter::operator()(
+    dbus::FileDescriptor* fd) {
+  base::WorkerPool::PostTask(
+      FROM_HERE, base::Bind(&base::DeletePointer<dbus::FileDescriptor>, fd),
+      false);
+}
+
 // static
 void FirewallHole::Open(PortType type,
                         uint16_t port,
                         const std::string& interface,
                         const OpenCallback& callback) {
-  dbus::ScopedFileDescriptor lifeline_local(new dbus::FileDescriptor());
-  dbus::ScopedFileDescriptor lifeline_remote(new dbus::FileDescriptor());
+  ScopedFileDescriptor lifeline_local(new dbus::FileDescriptor());
+  ScopedFileDescriptor lifeline_remote(new dbus::FileDescriptor());
 
   // This closure shares pointers with the one below. PostTaskAndReply
   // guarantees that it will always be deleted first.
@@ -103,8 +110,8 @@ FirewallHole::~FirewallHole() {
 void FirewallHole::RequestPortAccess(PortType type,
                                      uint16_t port,
                                      const std::string& interface,
-                                     dbus::ScopedFileDescriptor lifeline_local,
-                                     dbus::ScopedFileDescriptor lifeline_remote,
+                                     ScopedFileDescriptor lifeline_local,
+                                     ScopedFileDescriptor lifeline_remote,
                                      const OpenCallback& callback) {
   if (!lifeline_local->is_valid() || !lifeline_remote->is_valid()) {
     callback.Run(nullptr);
@@ -134,7 +141,7 @@ void FirewallHole::RequestPortAccess(PortType type,
 void FirewallHole::PortAccessGranted(PortType type,
                                      uint16_t port,
                                      const std::string& interface,
-                                     dbus::ScopedFileDescriptor lifeline_fd,
+                                     ScopedFileDescriptor lifeline_fd,
                                      const FirewallHole::OpenCallback& callback,
                                      bool success) {
   if (success) {
@@ -148,7 +155,7 @@ void FirewallHole::PortAccessGranted(PortType type,
 FirewallHole::FirewallHole(PortType type,
                            uint16_t port,
                            const std::string& interface,
-                           dbus::ScopedFileDescriptor lifeline_fd)
+                           ScopedFileDescriptor lifeline_fd)
     : type_(type),
       port_(port),
       interface_(interface),
