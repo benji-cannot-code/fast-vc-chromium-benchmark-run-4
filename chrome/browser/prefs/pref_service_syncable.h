72f2bdf5ce09b512571f9676b081ad8218d7a997
raymes <raymes@chromium.org>
2015-04-26 18:23:10 -0700

    Move pref registration functions into PrefRegistrySimple
    This moves all pref registration functions from PrefRegistrySyncable
    into PrefRegistrySimple and changes PrefRegistrySyncable to derive
    from PrefRegistrySyncable. This is done to consolidate the registration
    functions in a single place that can be used by syncable and unsyncable
    prefs.
    
    BUG=476800
    
    Review URL: https://codereview.chromium.org/1095673003
    
    Cr-Commit-Position: refs/heads/master@{#326991}

--- chrome/browser/prefs/pref_service_syncable.h
+++ chrome/browser/prefs/pref_service_syncable.h
@@ -90,7 +90,7 @@ class PrefServiceSyncable : public PrefService {
  private:
   friend class PrefModelAssociator;
 
-  void AddRegisteredSyncablePreference(const char* path, uint32 flags);
+  void AddRegisteredSyncablePreference(const std::string& path, uint32 flags);
 
   // Invoked internally when the IsSyncing() state changes.
   void OnIsSyncingChanged();
