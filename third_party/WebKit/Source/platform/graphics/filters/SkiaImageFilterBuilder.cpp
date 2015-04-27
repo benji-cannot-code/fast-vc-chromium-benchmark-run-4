5ff70d6d3bc1d6dc37bc2e39c9b3ff012727dd50
fs@opera.com <fs@opera.com>
2015-04-27 16:56:04 +0000

    Remove SourceGraphic reference from SkiaImageFilterBuilder
    Only set, not read. (Last user was SourceAlpha.)
    
    BUG=109224
    
    Review URL: https://codereview.chromium.org/1108823002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194527 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/filters/SkiaImageFilterBuilder.cpp
+++ third_party/WebKit/Source/platform/graphics/filters/SkiaImageFilterBuilder.cpp
@@ -39,15 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-SkiaImageFilterBuilder::SkiaImageFilterBuilder()
-    : m_context(nullptr)
-    , m_sourceGraphic(nullptr)
-{
-}
-
 SkiaImageFilterBuilder::SkiaImageFilterBuilder(GraphicsContext* context)
     : m_context(context)
-    , m_sourceGraphic(nullptr)
 {
 }
 
