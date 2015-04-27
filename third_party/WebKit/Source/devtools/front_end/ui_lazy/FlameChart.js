44715380bf5cf7e5fa476cf3c89f27feb9e147cc
alph@chromium.org <alph@chromium.org>
2015-04-27 19:06:34 +0000

    DevTools: Speedup flamechart range selection
    ... by eliminating layout thrashing. Don't do anything if selection is not changed.
    
    Review URL: https://codereview.chromium.org/1105163003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194538 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui_lazy/FlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/ui_lazy/FlameChart.js
@@ -1195,6 +1195,8 @@ WebInspector.FlameChart.prototype = {
      */
     setSelectedEntry: function(entryIndex)
     {
+        if (this._selectedEntryIndex === entryIndex)
+            return;
         this._selectedEntryIndex = entryIndex;
         this._revealEntry(entryIndex);
         this._updateElementPosition(this._selectedElement, this._selectedEntryIndex);
