f00c9317b737491bc857c83501d13cb831adba2d
yoshiki <yoshiki@chromium.org>
2015-04-26 21:59:06 -0700

    Fix cast from video player by using DriveApiUrlGenerator to generate URL
    This patch fixes the regression which is caused by the following CL.
    https://chromium.googlesource.com/chromium/src/+/3315eb5fe7bcb9300995dae3326de2d2c5f3c44a
    
    Since the Styx URLs are updated in that CL, we need to use DriveApiUrlGenerator::GenerateDownloadFileURL to create a download URL to pass a cast device.
    
    BUG=477733
    TEST=can cast a video
    
    Review URL: https://codereview.chromium.org/1055403004
    
    Cr-Commit-Position: refs/heads/master@{#326999}

--- chrome/browser/chromeos/extensions/file_manager/private_api_drive.h
+++ chrome/browser/chromeos/extensions/file_manager/private_api_drive.h
@@ -241,7 +241,7 @@ class FileManagerPrivateGetDownloadUrlFunction
                       const std::string& access_token);
 
  private:
-  std::string download_url_;
+  GURL download_url_;
   scoped_ptr<google_apis::AuthService> auth_service_;
 };
 
