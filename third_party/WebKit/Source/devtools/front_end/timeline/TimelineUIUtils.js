c64b59e534be1f92c75543c44b22c7bfcfa188a6
pfeldman@chromium.org <pfeldman@chromium.org>
2015-04-27 13:35:57 +0000

    DevTools: render ParseHTML details in timeline: url, start and end lines.
    Review URL: https://codereview.chromium.org/1106623003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194513 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineUIUtils.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineUIUtils.js
@@ -258,6 +258,10 @@ WebInspector.TimelineUIUtils.buildDetailsTextForTraceEvent = function(event, tar
         detailsText = linkifyTopCallFrameAsText() || eventData["id"];
         break;
     case recordType.ParseHTML:
+        var endLine = event.args["endData"]["endLine"];
+        var url = event.args["beginData"]["url"];
+        detailsText = endLine ? WebInspector.UIString("%s [%d:%d]", url, event.args["beginData"]["startLine"] + 1, endLine + 1) : url;
+        break;
     case recordType.RecalculateStyles:
         detailsText = linkifyTopCallFrameAsText();
         break;
@@ -374,6 +378,7 @@ WebInspector.TimelineUIUtils.buildDetailsNodeForTraceEvent = function(event, tar
     case recordType.ResourceReceiveResponse:
     case recordType.ResourceFinish:
     case recordType.EmbedderCallback:
+    case recordType.ParseHTML:
         detailsText = WebInspector.TimelineUIUtils.buildDetailsTextForTraceEvent(event, target);
         break;
     case recordType.FunctionCall:
@@ -398,7 +403,6 @@ WebInspector.TimelineUIUtils.buildDetailsNodeForTraceEvent = function(event, tar
         details = linkifyTopCallFrame();
         detailsText = eventData["id"];
         break;
-    case recordType.ParseHTML:
     case recordType.RecalculateStyles:
         details = linkifyTopCallFrame();
         break;
@@ -633,6 +637,16 @@ WebInspector.TimelineUIUtils._buildTraceEventDetailsSynchronously = function(eve
     case recordTypes.Animation:
         if (event.phase === WebInspector.TracingModel.Phase.NestableAsyncInstant)
             contentHelper.appendTextRow(WebInspector.UIString("State"), eventData["state"]);
+        break;
+    case recordTypes.ParseHTML:
+        var beginData = event.args["beginData"];
+        var url = beginData["url"];
+        if (url)
+            contentHelper.appendTextRow(WebInspector.UIString("URL"), url);
+        var startLine = beginData["startLine"] + 1;
+        var endLine = event.args["endData"]["endLine"] + 1;
+        if (endLine)
+            contentHelper.appendTextRow(WebInspector.UIString("Range"), WebInspector.UIString("%d \u2014 %d", startLine, endLine));
         break;
     default:
         var detailsNode = WebInspector.TimelineUIUtils.buildDetailsNodeForTraceEvent(event, model.target(), linkifier);
