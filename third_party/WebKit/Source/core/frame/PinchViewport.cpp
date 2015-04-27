b68edca83fa176f0bd9432f944ddc30081ce0a71
bokan@chromium.org <bokan@chromium.org>
2015-04-27 15:44:05 +0000

    Fix fractional scrolling in PinchViewport.
    The shouldUseIntegerScrollOffset method has a logic-inverting typo from
    https://chromium.googlesource.com/chromium/blink/+/43b93db8758c8f068695bb186a7ad04222948038
    
    Additionally, PinchViewport is missing the double-precision version of
    setScrollOffset which meant it uses the default which truncates to ints.
    
    These fixes unmasked a bug in TopControls where some scroll deltas would result
    in TopControls::scrollBy returning unused scroll delta even though it consumes
    the whole thing. I've fixed this issue here as well.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1091513006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194523 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/PinchViewport.cpp
+++ third_party/WebKit/Source/core/frame/PinchViewport.cpp
@@ -497,7 +497,7 @@ ScrollResult PinchViewport::wheelEvent(const PlatformWheelEvent& event)
 bool PinchViewport::shouldUseIntegerScrollOffset() const
 {
     LocalFrame* frame = mainFrame();
-    if (frame && frame->settings() && frame->settings()->preferCompositingToLCDTextEnabled())
+    if (frame && frame->settings() && !frame->settings()->preferCompositingToLCDTextEnabled())
         return true;
     return false;
 }
@@ -601,7 +601,12 @@ void PinchViewport::invalidateScrollbarRect(Scrollbar*, const IntRect&)
 
 void PinchViewport::setScrollOffset(const IntPoint& offset)
 {
-    setLocation(offset);
+    setScrollOffset(DoublePoint(offset));
+}
+
+void PinchViewport::setScrollOffset(const DoublePoint& offset)
+{
+    setLocation(toFloatPoint(offset));
 }
 
 GraphicsLayer* PinchViewport::layerForContainer() const
