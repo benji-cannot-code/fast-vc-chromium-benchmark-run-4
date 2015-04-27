28a216c55640043a34a968808971b002177cc6c4
phoglund <phoglund@chromium.org>
2015-04-27 01:24:55 -0700

    Implementing screenshare bypass flag on Mac.
    The --auto-select-desktop-capture-source flag is quite useful for
    testing since tools like Selenium generally can't interact with dialogs
    we pop up in the browser. It is implemented for aura-using platforms
    like Linux, CrOS and Windows and this patch adds it for Mac too.
    
    BUG=475372
    
    Review URL: https://codereview.chromium.org/1084913002
    
    Cr-Commit-Position: refs/heads/master@{#327013}

--- chrome/browser/ui/cocoa/media_picker/desktop_media_picker_controller.mm
+++ chrome/browser/ui/cocoa/media_picker/desktop_media_picker_controller.mm
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/media_picker/desktop_media_picker_controller.h"
 
 #include "base/bind.h"
+#include "base/command_line.h"
 #import "base/mac/bundle_locations.h"
 #include "base/strings/sys_string_conversions.h"
 #import "chrome/browser/ui/cocoa/media_picker/desktop_media_picker_item.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/grit/generated_resources.h"
 #include "content/public/browser/browser_thread.h"
 #import "third_party/google_toolbox_for_mac/src/AppKit/GTMUILocalizerAndLayoutTweaker.h"
@@ -284,6 +286,15 @@ const int kExcessButtonPadding = 6;
                                             imageTitle:imageTitle]);
   [items_ insertObject:item atIndex:index];
   [sourceBrowser_ reloadData];
+
+  NSString* autoselectSource = base::SysUTF8ToNSString(
+      base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+          switches::kAutoSelectDesktopCaptureSource));
+
+  if ([autoselectSource isEqualToString:imageTitle]) {
+    [self reportResult:[item sourceID]];
+    [self close];
+  }
 }
 
 - (void)sourceRemovedAtIndex:(int)index {
