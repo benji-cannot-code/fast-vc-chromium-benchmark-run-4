8bf40fe743cdc31ea251dcab18ea6dbc72c79af8
cmasone <cmasone@chromium.org>
2015-04-27 11:50:55 -0700

    Move ScopedFileDescriptor to dbus/file_descriptor.h
    ScopedFileDescriptor was initially introduced to facilitate the
    management of a 'lifeline' FD used when asking permission_broker to
    open a port in the device's firewall on CrOS.  It's actually a scoped
    wrapper around DBus::FileDescriptor, not a platform file descriptor. I
    would like to use it to facilitate some other similar functionality in
    which I create a pipe and pass one end over DBus, so it seems like a
    good idea to move ScopedFileDescriptor into the dbus library directly.
    
    BUG=chromium:481340
    TEST=FirewallHole unit tests still pass
    STATUS=Fixed
    R=keybuk, reillyg, stevenjb
    
    Review URL: https://codereview.chromium.org/1090243003
    
    Cr-Commit-Position: refs/heads/master@{#327089}

--- dbus/file_descriptor.cc
+++ dbus/file_descriptor.cc
@@ -3,11 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/bind.h"
 #include "base/files/file.h"
+#include "base/location.h"
 #include "base/logging.h"
+#include "base/threading/worker_pool.h"
 #include "dbus/file_descriptor.h"
 
 namespace dbus {
+
+void CHROME_DBUS_EXPORT FileDescriptor::Deleter::operator()(
+    FileDescriptor* fd) {
+  base::WorkerPool::PostTask(
+      FROM_HERE, base::Bind(&base::DeletePointer<FileDescriptor>, fd), false);
+}
 
 FileDescriptor::~FileDescriptor() {
   if (owner_)
