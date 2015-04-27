eba3c85c0b6f8bad9137d3ec0106d9c76bc7f2e5
qiankun.miao@intel.com <qiankun.miao@intel.com>
2015-04-27 15:49:04 +0000

    Remove LocalStorage runtime flag (status=stable) for blink
    The feature is in stable status since 2013. It should be fine to remove
    this runtime flag.
    
    The chromium side patch is at: https://codereview.chromium.org/1094103007/
    
    BUG=402536
    
    Review URL: https://codereview.chromium.org/1074943004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194524 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebRuntimeFeatures.h
+++ third_party/WebKit/public/web/WebRuntimeFeatures.h
@@ -80,8 +80,6 @@ public:
 
     BLINK_EXPORT static void enableImageColorProfiles(bool);
 
-    BLINK_EXPORT static void enableLocalStorage(bool);
-
     BLINK_EXPORT static void enableMediaPlayer(bool);
 
     BLINK_EXPORT static void enableSubpixelFontScaling(bool);
