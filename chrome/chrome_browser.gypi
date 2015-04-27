bb582297b92ae8f4317f969aa7ab1446118686cd
sdefresne <sdefresne@chromium.org>
2015-04-27 04:08:11 -0700

    Move top_sites_impl.{cc,h} into history component
    All problematic dependencies of TopSitesImpl have been removed, thus
    move the file into //components/history/core/browser.
    
    BUG=479174
    
    Review URL: https://codereview.chromium.org/1099303003
    
    Cr-Commit-Position: refs/heads/master@{#327026}

--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -1610,8 +1610,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/history/history_utils.h',
       'browser/history/top_sites_factory.cc',
       'browser/history/top_sites_factory.h',
-      'browser/history/top_sites_impl.cc',
-      'browser/history/top_sites_impl.h',
       'browser/history/web_history_service_factory.cc',
       'browser/history/web_history_service_factory.h',
     ],
--- components/history/core/browser/top_sites_impl.h
+++ components/history/core/browser/top_sites_impl.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_HISTORY_TOP_SITES_IMPL_H_
-#define CHROME_BROWSER_HISTORY_TOP_SITES_IMPL_H_
+#ifndef COMPONENTS_HISTORY_CORE_BROWSER_TOP_SITES_IMPL_H_
+#define COMPONENTS_HISTORY_CORE_BROWSER_TOP_SITES_IMPL_H_
 
 #include <list>
 #include <set>
@@ -318,4 +318,4 @@ class TopSitesImpl : public TopSites, public HistoryServiceObserver {
 
 }  // namespace history
 
-#endif  // CHROME_BROWSER_HISTORY_TOP_SITES_IMPL_H_
+#endif  // COMPONENTS_HISTORY_CORE_BROWSER_TOP_SITES_IMPL_H_
