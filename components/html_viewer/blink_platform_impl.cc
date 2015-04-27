b7ca5af0565e30d9c5aa7e549008ca4718dddcd4
erg <erg@chromium.org>
2015-04-27 11:27:09 -0700

    Start building core_services.mojo.
    Instead of having an application for clipboard.mojo, a
    view_manager.mojo, et all, we instead will have one core_services
    application which provides implementations of all the services compiled
    into one physical file so we don't have to pay the cost of //base/ in
    each application.
    
    For now, this just moves clipboard.mojo into core_services.
    
    BUG=477435
    R=sky@chromium.org
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/1084463003
    
    Cr-Commit-Position: refs/heads/master@{#327077}

--- components/html_viewer/blink_platform_impl.cc
+++ components/html_viewer/blink_platform_impl.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_util.h"
 #include "third_party/WebKit/public/platform/WebWaitableEvent.h"
 #include "third_party/mojo/src/mojo/public/cpp/application/application_impl.h"
+#include "third_party/mojo/src/mojo/public/cpp/application/connect.h"
 #include "ui/events/gestures/blink/web_gesture_curve_impl.h"
 
 namespace html_viewer {
@@ -73,8 +74,10 @@ BlinkPlatformImpl::BlinkPlatformImpl(mojo::ApplicationImpl* app)
     network_service_->GetCookieStore(GetProxy(&cookie_store));
     cookie_jar_.reset(new WebCookieJarImpl(cookie_store.Pass()));
 
+    mojo::ServiceProviderPtr service_provider;
+    app->ConnectToService("mojo:core_services", &service_provider);
     mojo::ClipboardPtr clipboard;
-    app->ConnectToService("mojo:clipboard", &clipboard);
+    mojo::ConnectToService(service_provider.get(), &clipboard);
     clipboard_.reset(new WebClipboardImpl(clipboard.Pass()));
   }
 }
