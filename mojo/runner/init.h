5a108ce4285e933694829431f8181ac3b7ed8007
ben <ben@chromium.org>
2015-04-27 14:52:57 -0700

    Separate mojo/shell into a runner and the application manager (shell)
    R=sky@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1091513005
    
    Cr-Commit-Position: refs/heads/master@{#327148}

---
+++ mojo/runner/init.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MOJO_RUNNER_INIT_H_
+#define MOJO_RUNNER_INIT_H_
+
+namespace mojo {
+namespace shell {
+
+// Initialization routines shared by desktop and Android main functions.
+
+void InitializeLogging();
+
+}  // namespace shell
+}  // namespace mojo
+
+#endif  // MOJO_RUNNER_INIT_H_
