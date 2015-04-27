a99cd8d77496aa6562da699caa66790658d7a8a5
jam <jam@chromium.org>
2015-04-27 09:48:43 -0700

    Pass mojo_shell args to child processes.
    This allows URL(s) sent as args to be passed to the window manager. Also send --override-use-gl-with-osmesa-for-tests since it's listed on the sites page.
    
    Review URL: https://codereview.chromium.org/1108023002
    
    Cr-Commit-Position: refs/heads/master@{#327056}

--- mojo/shell/child_process_host.cc
+++ mojo/shell/child_process_host.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/shell/context.h"
 #include "mojo/shell/switches.h"
 #include "mojo/shell/task_runners.h"
+#include "ui/gl/gl_switches.h"
 
 namespace mojo {
 namespace shell {
@@ -98,7 +99,10 @@ void ChildProcessHost::DidStart(bool success) {
 
 bool ChildProcessHost::DoLaunch() {
   static const char* kForwardSwitches[] = {
-      switches::kTraceToConsole, switches::kV, switches::kVModule,
+      switches::kOverrideUseGLWithOSMesaForTests,
+      switches::kTraceToConsole,
+      switches::kV,
+      switches::kVModule,
   };
 
   const base::CommandLine* parent_command_line =
@@ -116,6 +120,10 @@ bool ChildProcessHost::DoLaunch() {
     if (apps_to_debug.empty() || ContainsValue(apps_to_debug, name_))
       child_command_line.AppendSwitch(switches::kWaitForDebugger);
   }
+
+  auto args = parent_command_line->GetArgs();
+  for (const auto& arg : args)
+    child_command_line.AppendArgNative(arg);
 
   embedder::HandlePassingInformation handle_passing_info;
   platform_channel_pair_.PrepareToPassClientHandleToChildProcess(
