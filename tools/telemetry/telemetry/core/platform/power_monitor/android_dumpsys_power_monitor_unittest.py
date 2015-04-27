c453a144211c13de5f57766b0b06deae6fa66416
rnephew <rnephew@chromium.org>
2015-04-27 16:46:55 -0700

    [Telemetry][Android] Get voltage from device for dumpsys power monitor
    Currently the dumpsys power monitor uses a static value of 4.0 for the voltage
    when determining the power usage. This changes it to get the voltage from the
    device to make the power measurements more accurate.
    
    BUG=476043
    
    Review URL: https://codereview.chromium.org/1107173002
    
    Cr-Commit-Position: refs/heads/master@{#327177}

--- tools/telemetry/telemetry/core/platform/power_monitor/android_dumpsys_power_monitor_unittest.py
+++ tools/telemetry/telemetry/core/platform/power_monitor/android_dumpsys_power_monitor_unittest.py
@@ -17,7 +17,7 @@ class DumpsysPowerMonitorMonitorTest(unittest.TestCase):
     power_data = {'data': [23.9], 'uid': '12345'}
     results = (
         android_dumpsys_power_monitor.DumpsysPowerMonitor.ProcessPowerData(
-            power_data, package))
+            power_data, 4.0, package))
     self.assertEqual(results['identifier'], 'dumpsys')
     self.assertAlmostEqual(results['energy_consumption_mwh'], 95.6)
 
@@ -27,7 +27,7 @@ class DumpsysPowerMonitorMonitorTest(unittest.TestCase):
     power_data = None
     results = (
         android_dumpsys_power_monitor.DumpsysPowerMonitor.ProcessPowerData(
-            power_data, package))
+            power_data, 4.0, package))
     self.assertEqual(results['identifier'], 'dumpsys')
     self.assertEqual(results['energy_consumption_mwh'], 0)
 
