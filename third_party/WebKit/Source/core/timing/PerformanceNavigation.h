9482172f9021eccb20e3a9d0184af165efe27b4f
sigbjornf@opera.com <sigbjornf@opera.com>
2015-04-27 05:18:43 +0000

    Oilpan: keep navigation timing objects on the heap by default.
    R=haraken
    BUG=479467
    
    Review URL: https://codereview.chromium.org/1101123004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194481 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/timing/PerformanceNavigation.h
+++ third_party/WebKit/Source/core/timing/PerformanceNavigation.h
@@ -36,20 +36,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CoreExport.h"
 #include "core/frame/DOMWindowProperty.h"
 #include "platform/heap/Handle.h"
-#include "wtf/PassRefPtr.h"
-#include "wtf/RefCounted.h"
 
 namespace blink {
 
 class LocalFrame;
 
-class CORE_EXPORT PerformanceNavigation final : public RefCountedWillBeGarbageCollected<PerformanceNavigation>, public ScriptWrappable, public DOMWindowProperty {
+class CORE_EXPORT PerformanceNavigation final : public GarbageCollectedFinalized<PerformanceNavigation>, public ScriptWrappable, public DOMWindowProperty {
     DEFINE_WRAPPERTYPEINFO();
     WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(PerformanceNavigation);
 public:
-    static PassRefPtrWillBeRawPtr<PerformanceNavigation> create(LocalFrame* frame)
+    static PerformanceNavigation* create(LocalFrame* frame)
     {
-        return adoptRefWillBeNoop(new PerformanceNavigation(frame));
+        return new PerformanceNavigation(frame);
     }
 
     enum PerformanceNavigationType {
--- third_party/WebKit/Source/core/timing/PerformanceTiming.h
+++ third_party/WebKit/Source/core/timing/PerformanceTiming.h
@@ -36,8 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CoreExport.h"
 #include "core/frame/DOMWindowProperty.h"
 #include "platform/heap/Handle.h"
-#include "wtf/PassRefPtr.h"
-#include "wtf/RefCounted.h"
 
 namespace blink {
 
@@ -47,13 +45,13 @@ class DocumentTiming;
 class LocalFrame;
 class ResourceLoadTiming;
 
-class CORE_EXPORT PerformanceTiming final : public RefCountedWillBeGarbageCollected<PerformanceTiming>, public ScriptWrappable, public DOMWindowProperty {
+class CORE_EXPORT PerformanceTiming final : public GarbageCollectedFinalized<PerformanceTiming>, public ScriptWrappable, public DOMWindowProperty {
     DEFINE_WRAPPERTYPEINFO();
     WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(PerformanceTiming);
 public:
-    static PassRefPtrWillBeRawPtr<PerformanceTiming> create(LocalFrame* frame)
+    static PerformanceTiming* create(LocalFrame* frame)
     {
-        return adoptRefWillBeNoop(new PerformanceTiming(frame));
+        return new PerformanceTiming(frame);
     }
 
     unsigned long long navigationStart() const;
