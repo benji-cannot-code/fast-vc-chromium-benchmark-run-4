f1b1d6bf7d633465fb502e5149da515ebb7514ca
erikchen <erikchen@chromium.org>
2015-04-27 12:49:36 -0700

    Delete session cookies immediately after loading the cookie DB. (attempt #2)
    The original CL had non-determinism in the unit test.
    
    > Delete session cookies immediately after loading the cookie DB.
    >
    > Previously, the session cookies were deleted after all cookies were loaded into
    > memory. This allows for a race condition where new session cookies are inserted
    > into the database while the cookies are being loaded into memory, which causes
    > the new session cookies to also be deleted.
    >
    > BUG=
    >
    > Committed: https://crrev.com/ca924dcfb4f3c6b89e0c40898e42aec349b6b843
    > Cr-Commit-Position: refs/heads/master@{#326672}
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1103003002
    
    Cr-Commit-Position: refs/heads/master@{#327104}

--- content/browser/net/sqlite_persistent_cookie_store.cc
+++ content/browser/net/sqlite_persistent_cookie_store.cc
@@ -702,6 +702,9 @@ bool SQLitePersistentCookieStore::Backend::InitializeDatabase() {
       50);
 
   initialized_ = true;
+
+  if (!restore_old_session_cookies_)
+    DeleteSessionCookiesOnStartup();
   return true;
 }
 
@@ -1297,7 +1300,7 @@ void SQLitePersistentCookieStore::Backend::SetForceKeepSessionState() {
 
 void SQLitePersistentCookieStore::Backend::DeleteSessionCookiesOnStartup() {
   DCHECK(background_task_runner_->RunsTasksOnCurrentThread());
-  if (!db_->Execute("DELETE FROM cookies WHERE persistent == 0"))
+  if (!db_->Execute("DELETE FROM cookies WHERE persistent != 1"))
     LOG(WARNING) << "Unable to delete session cookies.";
 }
 
@@ -1322,8 +1325,6 @@ void SQLitePersistentCookieStore::Backend::FinishedLoadingCookies(
     bool success) {
   PostClientTask(FROM_HERE, base::Bind(&Backend::CompleteLoadInForeground, this,
                                        loaded_callback, success));
-  if (success && !restore_old_session_cookies_)
-    DeleteSessionCookiesOnStartup();
 }
 
 SQLitePersistentCookieStore::SQLitePersistentCookieStore(
