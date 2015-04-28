c3aef619e4813f09cf07ad66eb62dea5d9542225
horo@chromium.org <horo@chromium.org>
2015-04-28 04:53:49 +0000

    Add skipWaiting checkbox to ServiceWorkersView in DevTools [2/2 blink]
    1/2 chromium: https://codereview.chromium.org/1102263002/
    2/2 blink: this cl
    
    screen shot: https://code.google.com/p/chromium/issues/detail?id=466871#c61
    
    BUG=466871
    
    Review URL: https://codereview.chromium.org/1100403004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194570 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/ServiceWorkerManager.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/ServiceWorkerManager.js
@@ -195,6 +195,14 @@ WebInspector.ServiceWorkerManager.prototype = {
     },
 
     /**
+     * @param {string} versionId
+     */
+    skipWaiting: function(versionId)
+    {
+        this._agent.skipWaiting(versionId);
+    },
+
+    /**
      * @param {string} workerId
      * @param {string} url
      */
