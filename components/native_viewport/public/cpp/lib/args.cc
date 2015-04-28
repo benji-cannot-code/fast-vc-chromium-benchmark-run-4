6de4fd8518417d803abc740e5a651c285e8f8fd4
msw <msw@chromium.org>
2015-04-27 17:06:34 -0700

    Skip gl surface init when headless; enable wm apptests.
    Skip native_viewport's init of gfx::GLSurface when headless.
    Remove the unused --use-osmesa flag (--use-test-config suffices?).
    Re-enable the window_manager_apptests again; fingers crossed.
    
    BUG=480040
    TEST=Bot passes window_manager_apptests.
    R=sky@chromium.org
    TBR=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/1103413002
    
    Cr-Commit-Position: refs/heads/master@{#327184}

--- components/native_viewport/public/cpp/lib/args.cc
+++ components/native_viewport/public/cpp/lib/args.cc
@@ -7,12 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace mojo {
 
-// Instead of using the system GL implementation, use OSMesa.
-const char kUseOSMesa[] = "use-osmesa";
-// Loads an app from the specified directory and launches it.
-// Force gl to be initialized in test mode.
-const char kUseTestConfig[] = "use-test-config";
 // Create native viewport in headless mode.
 const char kUseHeadlessConfig[] = "use-headless-config";
+// Force gl to be initialized in test mode.
+const char kUseTestConfig[] = "use-test-config";
 
 }  // namespace mojo
