b9f64633c062b4900d06b5b3cdb0aafed704c042
rkc <rkc@chromium.org>
2015-04-28 10:28:52 -0700

    Revert of Revert of Add CPP API for BLE advertisments. (patchset #1 id:1 of https://codereview.chromium.org/1111563002/)
    Reason for revert:
    Can't seem to find any bots this was failing on. Trybots on the original CL that had component=shared_library are working fine and locally it builds fine too.
    
    Original issue's description:
    > Revert of Add CPP API for BLE advertisments. (patchset #12 id:220001 of https://codereview.chromium.org/1054743003/)
    >
    > Reason for revert:
    > Breaks component=shared_library build (missing export?)
    >
    > ../../device/bluetooth/bluetooth_adapter_chromeos.cc:321: error: undefined reference to 'chromeos::BluetoothAdvertisementChromeOS::BluetoothAdvertisementChromeOS(scoped_ptr<device::BluetoothAdvertisement::Data, base::DefaultDeleter<device::BluetoothAdvertisement::Data> >, scoped_refptr<chromeos::BluetoothAdapterChromeOS>)'
    > ../../base/memory/scoped_ptr.h:128: error: undefined reference to 'device::BluetoothAdvertisement::Data::~Data()'
    > ../../device/bluetooth/bluetooth_adapter_chromeos.cc:322: error: undefined reference to 'chromeos::BluetoothAdvertisementChromeOS::Register(base::Callback<void ()> const&, base::Callback<void (device::BluetoothAdvertisement::ErrorCode)> const&)'
    > obj/device/bluetooth/device_bluetooth.bluetooth_adapter_chromeos.o(.debug_addr+0x6f20): error: undefined reference to 'chromeos::BluetoothAdvertisementChromeOS::Register(base::Callback<void ()> const&, base::Callback<void (device::BluetoothAdvertisement::ErrorCode)> const&)'
    >
    > Original issue's description:
    > > Add CPP API for BLE advertisments.
    > >
    > > This CL adds the new classes, changes to existing classes and tests for adding
    > > the CPP API for LE advertisements. The design for this is available at
    > > http://go/chrome-ble-advertising.
    > >
    > > R=armansito@chromium.org, jamuraa@chromium.org
    > > BUG=466375
    > >
    > > Committed: https://crrev.com/c96da18077ef4b5ab28cb8b2684cd84386075e5a
    > > Cr-Commit-Position: refs/heads/master@{#327128}
    >
    > TBR=armansito@chromium.org,jamuraa@chromium.org,scheib@chromium.org,rkc@chromium.org
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=466375
    >
    > Committed: https://crrev.com/28bc6bec94e67f23a6a9a16490e6bdf721cf6b27
    > Cr-Commit-Position: refs/heads/master@{#327155}
    
    TBR=armansito@chromium.org,jamuraa@chromium.org,scheib@chromium.org,spang@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=466375
    
    Review URL: https://codereview.chromium.org/1108403002
    
    Cr-Commit-Position: refs/heads/master@{#327316}

--- chromeos/dbus/fake_bluetooth_le_advertising_manager_client.h
+++ chromeos/dbus/fake_bluetooth_le_advertising_manager_client.h
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/observer_list.h"
-#include "bluetooth_le_advertising_manager_client.h"
 #include "chromeos/chromeos_export.h"
+#include "chromeos/dbus/bluetooth_le_advertising_manager_client.h"
 #include "dbus/object_path.h"
 #include "dbus/property.h"
 
