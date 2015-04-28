30df47c3e5861eec87d15e5a00de9f48a4e45f72
jlklein <jlklein@chromium.org>
2015-04-28 12:39:28 -0700

    Add a 0.8 version of cr-checkbox with a demo
    BUG=480762
    
    Review URL: https://codereview.chromium.org/1105743003
    
    Cr-Commit-Position: refs/heads/master@{#327349}

---
+++ ui/webui/resources/cr_elements/v0_8/cr_checkbox/cr_checkbox.js
@@ -0,0 +1,54 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * `cr-checkbox` is a button that can be either checked or unchecked. User
+ * can tap the checkbox to check or uncheck it. Usually you use checkboxes
+ * to allow user to select multiple options from a set. If you have a single
+ * ON/OFF option, avoid using a single checkbox and use `cr-toggle-button`
+ * instead.
+ *
+ * Example:
+ *     <cr-checkbox></cr-checkbox>
+ *     <cr-checkbox checked></cr-checkbox>
+ *
+ * @element cr-checkbox
+ */
+Polymer({
+  is: 'cr-checkbox',
+
+  properties: {
+    /**
+     * Gets or sets the state. `true` is checked and `false` is unchecked.
+     *
+     * @attribute checked
+     * @type {boolean}
+     * @default false
+     */
+    checked: {
+      type: Boolean,
+      value: false,
+      reflectToAttribute: true
+    },
+
+    /**
+     * If true, the user cannot interact with this element.
+     *
+     * @attribute disabled
+     * @type {boolean}
+     * @default false
+     */
+    disabled: {
+      type: Boolean,
+      value: false,
+      reflectToAttribute: true
+    },
+  },
+
+  toggle: function() {
+    this.$.checkbox.toggle();
+  },
+});
