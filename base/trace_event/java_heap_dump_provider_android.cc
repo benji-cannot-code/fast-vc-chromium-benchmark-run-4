baf3f4c489ed693176d9fa46d497ff69c906d888
primiano <primiano@chromium.org>
2015-04-28 10:10:39 -0700

    [tracing] Simplify the MemoryAllocatorDump API
    Up until now the API surface of the MemoryAllocatorDump has been
    strongly typed, enforcing three basic counters (physical_size,
    allocated_objects_count and allocated_objects_size) + arbitrary
    extra attributes.
    This CL simplifies the MAD interface as follows:
    - Remove the two argument (allocator_name, heap_name) in favor of
      a simpler and single argument absolute_name (e.g.,
      CreateAllocatorDump("v8/isolate1/heap2") instead of
      CreateAllocatorDump("v8", "isolate1/heap2"))
    - Remove the storngly typed properties in favor of a generic
      Add(attribute_name, type, units, value) method.
    - Add helper methods MAD.AddScalar() and AddString().
    - Rewrite the existing dumpers to use the new simpler API.
    - Fix the tests.
    
    BUG=466141
    
    Review URL: https://codereview.chromium.org/1105143003
    
    Cr-Commit-Position: refs/heads/master@{#327310}

--- base/trace_event/java_heap_dump_provider_android.cc
+++ base/trace_event/java_heap_dump_provider_android.cc
@@ -11,12 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace trace_event {
 
-namespace {
-
-const char kDumperName[] = "java_heap";
-
-}  // namespace
-
 // static
 JavaHeapDumpProvider* JavaHeapDumpProvider::GetInstance() {
   return Singleton<JavaHeapDumpProvider,
@@ -32,16 +26,17 @@ JavaHeapDumpProvider::~JavaHeapDumpProvider() {
 // Called at trace dump point time. Creates a snapshot with the memory counters
 // for the current process.
 bool JavaHeapDumpProvider::OnMemoryDump(ProcessMemoryDump* pmd) {
-  MemoryAllocatorDump* dump =
-      pmd->CreateAllocatorDump(kDumperName, MemoryAllocatorDump::kRootHeap);
+  MemoryAllocatorDump* dump = pmd->CreateAllocatorDump("java_heap");
 
   // These numbers come from java.lang.Runtime stats.
   long total_heap_size = 0;
   long free_heap_size = 0;
   android::JavaRuntime::GetMemoryUsage(&total_heap_size, &free_heap_size);
-  dump->set_physical_size_in_bytes(total_heap_size);
-  dump->set_allocated_objects_count(0);
-  dump->set_allocated_objects_size_in_bytes(total_heap_size - free_heap_size);
+  dump->AddScalar(MemoryAllocatorDump::kNameOuterSize,
+                  MemoryAllocatorDump::kUnitsBytes, total_heap_size);
+  dump->AddScalar(MemoryAllocatorDump::kNameInnerSize,
+                  MemoryAllocatorDump::kUnitsBytes,
+                  total_heap_size - free_heap_size);
   return true;
 }
 
