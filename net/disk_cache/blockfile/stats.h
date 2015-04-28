c681407a6702792839fae6fe4078e45ec9c08404
payal.pandey <payal.pandey@samsung.com>
2015-04-27 23:37:48 -0700

    Use of base::StringPairs appropriately in stats.h
    Bescause base/strings/string_split.h defines:
    typedef std::vector<std::pair<std::string, std::string> > StringPairs;
    
    BUG=412250
    
    Review URL: https://codereview.chromium.org/1062803009
    
    Cr-Commit-Position: refs/heads/master@{#327240}

--- net/disk_cache/blockfile/stats.h
+++ net/disk_cache/blockfile/stats.h
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NET_DISK_CACHE_BLOCKFILE_STATS_H_
 
 #include <string>
-#include <vector>
 
 #include "base/basictypes.h"
+#include "base/strings/string_split.h"
 #include "net/base/net_export.h"
 #include "net/disk_cache/blockfile/addr.h"
 
@@ -19,7 +19,7 @@ class HistogramSamples;
 
 namespace disk_cache {
 
-typedef std::vector<std::pair<std::string, std::string> > StatsItems;
+using StatsItems = base::StringPairs;
 
 // This class stores cache-specific usage information, for tunning purposes.
 class NET_EXPORT_PRIVATE Stats {
