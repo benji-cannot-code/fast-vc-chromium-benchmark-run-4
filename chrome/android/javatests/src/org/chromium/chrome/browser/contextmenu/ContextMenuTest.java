5b1918a2031b0d9afe50292431b21d82bb48aaa2
aurimas <aurimas@chromium.org>
2015-04-27 17:29:05 -0700

    Remove ICS support code in chrome/ and components/precache/
    BUG=473837,431717
    TBR=bengr@chromium.org
    
    Review URL: https://codereview.chromium.org/1103293003
    
    Cr-Commit-Position: refs/heads/master@{#327192}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
@@ -8,7 +8,6 @@ package org.chromium.chrome.browser.contextmenu;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
-import android.os.Build;
 import android.test.FlakyTest;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.view.ContextMenu;
@@ -97,8 +96,6 @@ public class ContextMenuTest extends ChromeShellTestBase {
     // @MediumTest
     @Feature({"Browser"})
     public void testCopyImageToClipboard() throws InterruptedException, TimeoutException {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) return;
-
         Tab tab = getActivity().getActiveTab();
         ContextMenuUtils.selectContextMenuItem(this, tab, "testImage",
                 R.id.contextmenu_copy_image);
--- chrome/android/javatests/src/org/chromium/chrome/browser/preferences/website/WebsiteAddressTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/preferences/website/WebsiteAddressTest.java
@@ -5,21 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.preferences.website;
 
-import android.os.Build;
 import android.test.suitebuilder.annotation.SmallTest;
 import android.test.suitebuilder.annotation.Smoke;
 
 import org.chromium.base.CommandLine;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.content.browser.test.NativeLibraryTestBase;
 
 /**
  * Tests for WebsiteAddress.
- * loadNativeLibraryAndInitBrowserProcess seems to be flaky on ICS.
- * http://crbug.com/431717
  */
-@MinAndroidSdkLevel(Build.VERSION_CODES.JELLY_BEAN)
 public class WebsiteAddressTest extends NativeLibraryTestBase {
 
     @Smoke
