8d6dec3d505df7c56003fa6668e82fc3334e4917
pauljensen <pauljensen@chromium.org>
2015-04-28 11:26:39 -0700

    [cronet] Pass total bytes received to CronetUrlRequest.onSucceeded()
    Previously onSucceeded() had to synchronize and turn around and
    request total bytes received.  By passing it we avoid the
    synchronization.
    
    Review URL: https://codereview.chromium.org/1100923002
    
    Cr-Commit-Position: refs/heads/master@{#327334}

--- components/cronet/android/cronet_url_request_adapter.cc
+++ components/cronet/android/cronet_url_request_adapter.cc
@@ -233,12 +233,6 @@ jboolean CronetURLRequestAdapter::GetWasCached(JNIEnv* env,
   return url_request_->response_info().was_cached;
 }
 
-int64 CronetURLRequestAdapter::GetTotalReceivedBytes(JNIEnv* env,
-                                                     jobject jcaller) const {
-  DCHECK(context_->IsOnNetworkThread());
-  return url_request_->GetTotalReceivedBytes();
-}
-
 // net::URLRequest::Delegate overrides (called on network thread).
 
 void CronetURLRequestAdapter::OnReceivedRedirect(
@@ -279,7 +273,8 @@ void CronetURLRequestAdapter::OnReadCompleted(net::URLRequest* request,
     read_buffer_ = nullptr;
   } else {
     JNIEnv* env = base::android::AttachCurrentThread();
-    cronet::Java_CronetUrlRequest_onSucceeded(env, owner_.obj());
+    cronet::Java_CronetUrlRequest_onSucceeded(
+        env, owner_.obj(), url_request_->GetTotalReceivedBytes());
   }
 }
 
--- components/cronet/android/java/src/org/chromium/net/CronetUrlRequest.java
+++ components/cronet/android/java/src/org/chromium/net/CronetUrlRequest.java
@@ -617,16 +617,7 @@ final class CronetUrlRequest implements UrlRequest {
      */
     @SuppressWarnings("unused")
     @CalledByNative
-    private void onSucceeded() {
-        long totalReceivedBytes;
-        synchronized (mUrlRequestAdapterLock) {
-            if (mUrlRequestAdapter == 0) {
-                return;
-            }
-            totalReceivedBytes =
-                    nativeGetTotalReceivedBytes(mUrlRequestAdapter);
-        }
-
+    private void onSucceeded(long totalReceivedBytes) {
         final NativeExtendedResponseInfo extendedResponseInfo =
                 new NativeExtendedResponseInfo(mResponseInfo,
                         totalReceivedBytes);
@@ -715,7 +706,4 @@ final class CronetUrlRequest implements UrlRequest {
 
     @NativeClassQualifiedName("CronetURLRequestAdapter")
     private native boolean nativeGetWasCached(long nativePtr);
-
-    @NativeClassQualifiedName("CronetURLRequestAdapter")
-    private native long nativeGetTotalReceivedBytes(long nativePtr);
 }
