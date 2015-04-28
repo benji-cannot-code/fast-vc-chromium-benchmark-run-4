75b9a7b8e8bcb65962d97773d56cae4a79f3265e
sigbjornf@opera.com <sigbjornf@opera.com>
2015-04-28 13:16:32 +0000

    Oilpan: fix build after r194592.
    TBR=oilpan-reviews
    BUG=480341
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1108823004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194598 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorTracingAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorTracingAgent.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/inspector/InspectorTracingAgent.h"
 
+#include "core/frame/LocalFrame.h"
 #include "core/inspector/IdentifiersFactory.h"
 #include "core/inspector/InspectorState.h"
 #include "core/inspector/InspectorTraceEvents.h"
