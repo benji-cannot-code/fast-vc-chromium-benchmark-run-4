d583f6c614fdc6904c241702189151d5a1aa3ee4
mnaganov <mnaganov@chromium.org>
2015-04-28 08:58:05 -0700

    [Android] Refactor Java Bridge tests
    This is to bring in some sanity, and to facilitate extracting common
    test code to be used by both ContentShellTests and AwShellTests.
    
    BUG=476880
    
    Review URL: https://codereview.chromium.org/1102753002
    
    Cr-Commit-Position: refs/heads/master@{#327294}

--- content/public/android/javatests/src/org/chromium/content/browser/JavaBridgeChildFrameTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/JavaBridgeChildFrameTest.java
@@ -10,6 +10,8 @@ import android.test.suitebuilder.annotation.SmallTest;
 import org.chromium.base.annotations.SuppressFBWarnings;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content_public.browser.JavaScriptCallback;
+import org.chromium.content_public.browser.LoadUrlParams;
+import org.chromium.content_public.browser.NavigationController;
 import org.chromium.content_public.browser.WebContents;
 
 /**
@@ -41,7 +43,7 @@ public class JavaBridgeChildFrameTest extends JavaBridgeTestBase {
     protected void setUp() throws Exception {
         super.setUp();
         mTestController = new TestController();
-        setUpContentView(mTestController, "testController");
+        injectObjectAndReload(mTestController, "testController");
     }
 
     @SmallTest
@@ -124,5 +126,14 @@ public class JavaBridgeChildFrameTest extends JavaBridgeTestBase {
         });
         resultCallback.waitForResult();
         return result[0];
+    }
+
+    /**
+     * Loads data on the UI thread and blocks until onPageFinished is called.
+     */
+    private void loadDataSync(final NavigationController navigationController, final String data,
+            final String mimeType, final boolean isBase64Encoded) throws Throwable {
+        loadUrl(navigationController, mTestCallbackHelperContainer,
+                LoadUrlParams.createLoadDataParams(data, mimeType, isBase64Encoded));
     }
 }
--- content/public/android/javatests/src/org/chromium/content/browser/JavaBridgeReturnValuesTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/JavaBridgeReturnValuesTest.java
@@ -109,7 +109,7 @@ public class JavaBridgeReturnValuesTest extends JavaBridgeTestBase {
     protected void setUp() throws Exception {
         super.setUp();
         mTestObject = new TestObject();
-        setUpContentView(mTestObject, "testObject");
+        injectObjectAndReload(mTestObject, "testObject");
     }
 
     // Note that this requires that we can pass a JavaScript string to Java.
