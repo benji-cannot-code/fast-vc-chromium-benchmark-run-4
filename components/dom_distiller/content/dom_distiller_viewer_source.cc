7a30c53c8c27df2c25bbca3313e4a4405074d180
mdjones <mdjones@chromium.org>
2015-04-27 17:54:37 -0700

    Fix hanging get requests
    Run the GotDataCallback for distiller feedback responses.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1106293004
    
    Cr-Commit-Position: refs/heads/master@{#327203}

--- components/dom_distiller/content/dom_distiller_viewer_source.cc
+++ components/dom_distiller/content/dom_distiller_viewer_source.cc
@@ -233,6 +233,7 @@ void DomDistillerViewerSource::StartDataRequest(
     return;
   } else if (kFeedbackBad == path) {
     FeedbackReporter::ReportQuality(false);
+    callback.Run(NULL);
     if (!external_feedback_reporter_)
       return;
     content::WebContents* contents =
@@ -242,6 +243,7 @@ void DomDistillerViewerSource::StartDataRequest(
     return;
   } else if (kFeedbackGood == path) {
     FeedbackReporter::ReportQuality(true);
+    callback.Run(NULL);
     return;
   }
   content::WebContents* web_contents =
