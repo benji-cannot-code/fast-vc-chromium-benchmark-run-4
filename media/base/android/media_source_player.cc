872c63d26160f65f95918dfd5acd046cc1fd2a67
timav <timav@chromium.org>
2015-04-28 11:35:58 -0700

    Set audible status to false if audio is stopped after prefetch.
    We discovered an audio playing site that often triggers
    the starvation condition during the playback. This introduces
    a race between Pause , starvation detecton and the MediaDecoderCallback.
    If starvation detection happens between Pause and the callback,
    we will do Prefetch and then stop the flow of packets,
    and won't update the audible status properly.
    
    This fix sets the audible status to false in OnPrefetchDone()
    if we realize that the flow will stop.
    
    BUG=474761
    
    Review URL: https://codereview.chromium.org/1067153002
    
    Cr-Commit-Position: refs/heads/master@{#327336}

--- media/base/android/media_source_player.cc
+++ media/base/android/media_source_player.cc
@@ -135,6 +135,8 @@ void MediaSourcePlayer::Pause(bool is_media_related_action) {
   // MediaDecoderCallback() is called.
   playing_ = false;
   start_time_ticks_ = base::TimeTicks();
+
+  SetAudible(false);
 }
 
 bool MediaSourcePlayer::IsPlaying() {
