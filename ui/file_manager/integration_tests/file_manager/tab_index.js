36cd5d2b5c465f0bd8d59a079431045af14fae18
yoshiki <yoshiki@chromium.org>
2015-04-28 02:58:20 -0700

    Fix potential flakiness in Files.app test
    This patch makes the banner & focus code robust with promise. And make sure the banner existences before test runs.
    
    BUG=478640
    TEST=run TabindexFocusDownloads/FileManagerBrowserTest.Test/0 100 times
    
    Review URL: https://codereview.chromium.org/1105333003
    
    Cr-Commit-Position: refs/heads/master@{#327255}

--- ui/file_manager/integration_tests/file_manager/tab_index.js
+++ ui/file_manager/integration_tests/file_manager/tab_index.js
@@ -67,9 +67,12 @@ testcase.tabindexFocus = function() {
       appId = inAppId;
       remoteCall.waitForElement(appId, ['#file-list:focus']).then(this.next);
     },
-    // Press the Tab key.
+    function(element) {
+      remoteCall.waitForElement(appId, ['#drive-welcome-link']).then(this.next);
+    },
     function(element) {
       remoteCall.callRemoteTestUtil('getActiveElement', appId, [], this.next);
+    // Press the Tab key.
     }, function(element) {
       chrome.test.assertEq('list', element.attributes['class']);
       remoteCall.checkNextTabFocus(appId, 'search-button').then(this.next);
@@ -151,7 +154,9 @@ testcase.tabindexFocusDirectorySelected = function() {
       appId = inAppId;
       remoteCall.waitForElement(appId, ['#file-list:focus']).then(this.next);
     },
-    // Press the Tab key.
+    function(element) {
+      remoteCall.waitForElement(appId, ['#drive-welcome-link']).then(this.next);
+    },
     function(element) {
       remoteCall.callRemoteTestUtil('getActiveElement', appId, [], this.next);
     }, function(element) {
@@ -159,6 +164,7 @@ testcase.tabindexFocusDirectorySelected = function() {
       // Select the directory named 'photos'.
       remoteCall.callRemoteTestUtil(
           'selectFile', appId, ['photos']).then(this.next);
+    // Press the Tab key.
     }, function(result) {
       chrome.test.assertTrue(result);
       remoteCall.checkNextTabFocus(appId, 'share-button').then(this.next);
