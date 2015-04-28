4c8c6bc4da82c7c73f16d709baef7f706642d634
zea <zea@chromium.org>
2015-04-27 18:08:20 -0700

    [Sync] Fix passphrase prompt for legacy accounts
    Make sure that we always include the dashboard reset link in any passphrase
    prompt. Also ensure we use the term "sync passphrase" rather than "google
    password". Lastly, use the passphrase time if its available.
    
    BUG=481224
    
    Review URL: https://codereview.chromium.org/1101373002
    
    Cr-Commit-Position: refs/heads/master@{#327209}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/ui/SyncCustomizationFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/ui/SyncCustomizationFragment.java
@@ -293,13 +293,8 @@ public class SyncCustomizationFragment extends PreferenceFragment implements
             return;
         }
         if (mProfileSyncService.isPassphraseRequiredForDecryption() && isAdded()) {
-            if (mProfileSyncService.isUsingSecondaryPassphrase()) {
-                mSyncEncryption.setSummary(
-                        errorSummary(getString(R.string.sync_need_passphrase)));
-            } else {
-                mSyncEncryption.setSummary(
-                        errorSummary(getString(R.string.sync_need_password)));
-            }
+            mSyncEncryption.setSummary(
+                    errorSummary(getString(R.string.sync_need_passphrase)));
         }
     }
 
@@ -435,11 +430,7 @@ public class SyncCustomizationFragment extends PreferenceFragment implements
     @Override
     public void onPassphraseEntered(String passphrase, boolean isGaia, boolean isUpdate) {
         if (isUpdate) {
-            if (isGaia) {
-                handleEncryptWithGaia(passphrase);
-            } else {
-                handleEncryptWithCustomPassphrase(passphrase);
-            }
+            handleEncryptWithCustomPassphrase(passphrase);
         } else {
             handleDecryption(passphrase);
         }
@@ -468,25 +459,13 @@ public class SyncCustomizationFragment extends PreferenceFragment implements
     @Override
     public void onPassphraseTypeSelected(PassphraseType type) {
         boolean isAllDataEncrypted = mProfileSyncService.isEncryptEverythingEnabled();
-        boolean isPassphraseGaia = !mProfileSyncService.isUsingSecondaryPassphrase();
+        boolean isUsingSecondaryPassphrase = mProfileSyncService.isUsingSecondaryPassphrase();
 
-        if (type == PassphraseType.IMPLICIT_PASSPHRASE) {
-            // custom passphrase -> gaia is not allowed
-            assert (isPassphraseGaia);
-            boolean isGaia = true;
-            boolean isUpdate = !isAllDataEncrypted;
-            displayPasswordDialog(isGaia, isUpdate);
-        } else if (type == PassphraseType.CUSTOM_PASSPHRASE) {
-            if (isPassphraseGaia) {
-                displayCustomPasswordDialog();
-            } else {
-                // Now using the existing custom passphrase to encrypt
-                // everything.
-                boolean isGaia = false;
-                boolean isUpdate = false;
-                displayPasswordDialog(isGaia, isUpdate);
-            }
-        }
+        // The passphrase type should only ever be selected if the account doesn't have
+        // full encryption enabled. Otherwise both options should be disabled.
+        assert !isAllDataEncrypted;
+        assert !isUsingSecondaryPassphrase;
+        displayCustomPasswordDialog();
     }
 
     /**
@@ -501,7 +480,7 @@ public class SyncCustomizationFragment extends PreferenceFragment implements
         }
         if (preference == mSyncEncryption && mProfileSyncService.isSyncInitialized()) {
             if (mProfileSyncService.isPassphraseRequiredForDecryption()) {
-                displayPasswordDialog(!mProfileSyncService.isUsingSecondaryPassphrase(), false);
+                displayPasswordDialog(false, false);
             } else {
                 displayPasswordTypeDialog();
                 return true;
