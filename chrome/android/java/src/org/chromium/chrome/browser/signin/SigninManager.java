49b5b37069a9a8580c0292a27f33ba41c8dca5fc
maxbogue <maxbogue@chromium.org>
2015-04-27 17:03:15 -0700

    Fix a crash in SigninManager.finishSignIn()
    No test because SigninManager is low on my list of things to write tests
    for. I've filed http://crbug.com/481099 to track it.
    
    BUG=478597
    
    Review URL: https://codereview.chromium.org/1100983005
    
    Cr-Commit-Position: refs/heads/master@{#327182}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManager.java
@@ -396,6 +396,11 @@ public class SigninManager {
     }
 
     private void finishSignIn(AccountIdsAndNames accountIdsAndNames) {
+        if (mSignInAccount == null) {
+            Log.w(TAG, "Sign in request was canceled; aborting finishSignIn().");
+            return;
+        }
+
         // Cache the signed-in account name.
         ChromeSigninController.get(mContext).setSignedInAccountName(mSignInAccount.name);
 
