dd2b2b27a105de4ebd21fd90ebb0cde1c2496bbe
yurys@chromium.org <yurys@chromium.org>
2015-04-28 09:43:47 +0000

    Fix crash in inspector/audits/audits-empty-stylesheet.html
    ScriptDebugServer::setPauseOnExceptionsState must not be called if ScriptDebugServer is not enabled as the call would call ensureDebuggerScriptCompiled when debugger is off and will lead to an incosistent state of ScriptDebugServer when it has not listener but initialized debugger script.
    
    BUG=473619
    
    Review URL: https://codereview.chromium.org/1060273005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194586 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/ScriptDebugServer.h
+++ third_party/WebKit/Source/bindings/core/v8/ScriptDebugServer.h
@@ -58,6 +58,7 @@ public:
 
     void enable();
     void disable();
+    bool enabled() const;
 
     static void setContextDebugData(v8::Local<v8::Context>, const String& contextDebugData);
     // Each script inherits debug data from v8::Context where it has been compiled.
@@ -131,7 +132,6 @@ protected:
     virtual void quitMessageLoopOnPause() = 0;
 
 private:
-    bool enabled() const;
     void ensureDebuggerScriptCompiled();
     v8::MaybeLocal<v8::Value> callDebuggerMethod(const char* functionName, int argc, v8::Local<v8::Value> argv[]);
     v8::Local<v8::Object> debuggerScriptLocal() const;
