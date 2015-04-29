713835d01b175b7f55f774ab3f783fabd4d7c9ac
trchen@chromium.org <trchen@chromium.org>
2015-04-29 04:55:53 +0000

    Correct fixed-position recording for Slimming Paint
    Before this CL, the location of display items of the descendants of a
    fixed-position container is specified in absolute coordinate. Scrolling the
    viewport with non-composited fixed position container will need a full subtree
    invalidation.
    
    This CL paints fixed position layers by adding a full-layer transform then
    change the painting root to the current layer for painting. Identical to
    how we paint non-composited transformed layer. This way scrolling won't need
    to invalidate any display items in the subtree.
    
    As a drive-by change, LayerFixedPositionRecorder, FixedPositionDisplayItem,
    and FixedPositionContainerDisplayItem are added for layerization.
    Currently they are no-op.
    
    BUG=482170
    
    Review URL: https://codereview.chromium.org/1082333005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194643 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutObject.h
+++ third_party/WebKit/Source/core/layout/LayoutObject.h
@@ -1062,6 +1062,7 @@ public:
     DisplayItemClient displayItemClient() const { return toDisplayItemClient(this); }
 
     void invalidateDisplayItemClient(const DisplayItemClientWrapper&) const;
+    void invalidateDisplayItemClientForNonCompositingDescendants() const;
 
 protected:
     enum LayoutObjectType {
