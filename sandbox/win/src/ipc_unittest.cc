19d39d165161cd1ad25ab79328fc8a167fe02d6f
dcheng <dcheng@chromium.org>
2015-04-29 14:28:44 -0700

    Apply automated fixits for Chrome clang plugin to sandbox.
    Working on trimming down plugin warnings on the Windows clang build.
    
    BUG=467287
    TBR=cpu@chromium.org
    
    Review URL: https://codereview.chromium.org/1118473005
    
    Cr-Commit-Position: refs/heads/master@{#327558}

--- sandbox/win/src/ipc_unittest.cc
+++ sandbox/win/src/ipc_unittest.cc
@@ -566,9 +566,9 @@ class UnitTestIPCDispatcher : public Dispatcher {
   };
 
   UnitTestIPCDispatcher();
-  ~UnitTestIPCDispatcher() {};
+  ~UnitTestIPCDispatcher() override{};
 
-  virtual bool SetupService(InterceptionManager* manager, int service) {
+  bool SetupService(InterceptionManager* manager, int service) override {
     return true;
   }
 
