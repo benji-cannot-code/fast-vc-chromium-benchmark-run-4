750cd5ee1e044a0f99e8f2f5ccb51f288dbf43a1
erikchen <erikchen@chromium.org>
2015-04-29 12:11:06 -0700

    mac: Suppress a -Wpartial-availability warning in bluetooth_socket_mac.mm.
    BUG=471823
    
    Review URL: https://codereview.chromium.org/1117513002
    
    Cr-Commit-Position: refs/heads/master@{#327538}

--- device/bluetooth/bluetooth_socket_mac.mm
+++ device/bluetooth/bluetooth_socket_mac.mm
@@ -31,9 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/io_buffer.h"
 #include "net/base/net_errors.h"
 
-// Replicate specific 10.7 SDK declarations for building with prior SDKs.
 #if !defined(MAC_OS_X_VERSION_10_7) || \
-    MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_7
+    MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_7
 
 @interface IOBluetoothDevice (LionSDKDeclarations)
 - (IOReturn)performSDPQuery:(id)target uuids:(NSArray*)uuids;
