c6cc9bc06501261bdcb3bd37ffee08987b308484
achuith <achuith@chromium.org>
2015-04-29 16:21:55 -0700

    Revert of Revert "Enable BlockingLoginTest, ForceMaximizeOnFirstRunTest, UserCloudPolicyManagerTest, for webv… (patchset #1 id:1 of https://codereview.chromium.org/1109393005/)
    Reason for revert:
    This was a speculative revert, but I don't believe the browser tests reverted here were the cause of the blink failures.
    
    Original issue's description:
    > Revert "Enable BlockingLoginTest, ForceMaximizeOnFirstRunTest, UserCloudPolicyManagerTest, for webview."
    >
    > This reverts commit 8c52ec023857ec7d082d54518864edd84d25b116.
    >
    > This seems to be breaking "Linux ChromiumOS Tests (dbg)" on the Blink waterfall, and blocking the Blink roll:
    > http://build.chromium.org/p/chromium.webkit/builders/Linux%20ChromiumOS%20Tests%20%28dbg%29%281%29/builds/1485
    >
    > TBR=achuith,nkostylev@chromium.org
    > BUG=
    > NOTREECHECKS=true
    >
    > Committed: https://crrev.com/a5080f268e879b41a34a3f912fb5dabfc6b7a2dd
    > Cr-Commit-Position: refs/heads/master@{#327552}
    
    TBR=nkostylev@chromium.org,macourteau@chromium.org,senorblanco@chromium.org
    
    BUG=475567,477402
    
    Review URL: https://codereview.chromium.org/1111593006
    
    Cr-Commit-Position: refs/heads/master@{#327611}

--- chrome/browser/chromeos/policy/force_maximize_on_first_run_chromeos_browsertest.cc
+++ chrome/browser/chromeos/policy/force_maximize_on_first_run_chromeos_browsertest.cc
@@ -30,9 +30,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace policy {
 
-class ForceMaximizeOnFirstRunTest : public LoginPolicyTestBase {
+// Boolean parameter is used to run this test for webview (true) and for
+// iframe (false) GAIA sign in.
+class ForceMaximizeOnFirstRunTest : public LoginPolicyTestBase,
+                                    public testing::WithParamInterface<bool> {
  protected:
-  ForceMaximizeOnFirstRunTest() : LoginPolicyTestBase() {}
+  ForceMaximizeOnFirstRunTest() : LoginPolicyTestBase() {
+    set_use_webview(GetParam());
+  }
 
   scoped_ptr<base::DictionaryValue> GetMandatoryPoliciesValue() const override {
     scoped_ptr<base::DictionaryValue> dict(new base::DictionaryValue);
@@ -65,7 +70,7 @@ class ForceMaximizeOnFirstRunTest : public LoginPolicyTestBase {
   DISALLOW_COPY_AND_ASSIGN(ForceMaximizeOnFirstRunTest);
 };
 
-IN_PROC_BROWSER_TEST_F(ForceMaximizeOnFirstRunTest, PRE_TwoRuns) {
+IN_PROC_BROWSER_TEST_P(ForceMaximizeOnFirstRunTest, PRE_TwoRuns) {
   SetUpResolution();
   SkipToLoginScreen();
   LogIn(kAccountId, kAccountPassword);
@@ -89,7 +94,7 @@ IN_PROC_BROWSER_TEST_F(ForceMaximizeOnFirstRunTest, PRE_TwoRuns) {
   EXPECT_FALSE(browser1->window()->IsMaximized());
 }
 
-IN_PROC_BROWSER_TEST_F(ForceMaximizeOnFirstRunTest, TwoRuns) {
+IN_PROC_BROWSER_TEST_P(ForceMaximizeOnFirstRunTest, TwoRuns) {
   SetUpResolution();
   content::WindowedNotificationObserver(
       chrome::NOTIFICATION_LOGIN_OR_LOCK_WEBUI_VISIBLE,
@@ -101,9 +106,9 @@ IN_PROC_BROWSER_TEST_F(ForceMaximizeOnFirstRunTest, TwoRuns) {
   EXPECT_FALSE(browser->window()->IsMaximized());
 }
 
-class ForceMaximizetPolicyFalseTest : public ForceMaximizeOnFirstRunTest {
+class ForceMaximizePolicyFalseTest : public ForceMaximizeOnFirstRunTest {
  protected:
-  ForceMaximizetPolicyFalseTest() : ForceMaximizeOnFirstRunTest() {}
+  ForceMaximizePolicyFalseTest() : ForceMaximizeOnFirstRunTest() {}
 
   scoped_ptr<base::DictionaryValue> GetMandatoryPoliciesValue() const override {
     scoped_ptr<base::DictionaryValue> dict(new base::DictionaryValue);
@@ -112,10 +117,10 @@ class ForceMaximizetPolicyFalseTest : public ForceMaximizeOnFirstRunTest {
   }
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(ForceMaximizetPolicyFalseTest);
+  DISALLOW_COPY_AND_ASSIGN(ForceMaximizePolicyFalseTest);
 };
 
-IN_PROC_BROWSER_TEST_F(ForceMaximizetPolicyFalseTest, GeneralFirstRun) {
+IN_PROC_BROWSER_TEST_P(ForceMaximizePolicyFalseTest, GeneralFirstRun) {
   SetUpResolution();
   SkipToLoginScreen();
   LogIn(kAccountId, kAccountPassword);
@@ -128,4 +133,11 @@ IN_PROC_BROWSER_TEST_F(ForceMaximizetPolicyFalseTest, GeneralFirstRun) {
   EXPECT_FALSE(browser->window()->IsMaximized());
 }
 
+INSTANTIATE_TEST_CASE_P(ForceMaximizeOnFirstRunTestSuite,
+                        ForceMaximizeOnFirstRunTest,
+                        testing::Bool());
+
+INSTANTIATE_TEST_CASE_P(ForceMaximizePolicyFalseTestSuite,
+                        ForceMaximizePolicyFalseTest,
+                        testing::Bool());
 }  // namespace policy
