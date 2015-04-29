9754b50bfa8e2c2bb6e147f8da2d4a02891d2a76
pranay.kumar <pranay.kumar@samsung.com>
2015-04-29 05:53:17 -0700

    Replace MessageLoopProxy usage with ThreadTaskRunnerHandle in chrome/browser/browsing_data module.
    MessageLoopProxy is deprecated.
    This basically does a search and replace of:
    
    MessageLoopProxy::current() -> ThreadTaskRunnerHandle::Get().
    
    MessageLoopProxy -> SingleThreadTaskRunner
    
    BUG=391045
    
    Review URL: https://codereview.chromium.org/1117583003
    
    Cr-Commit-Position: refs/heads/master@{#327479}

--- chrome/browser/browsing_data/browsing_data_indexed_db_helper_unittest.cc
+++ chrome/browser/browsing_data/browsing_data_indexed_db_helper_unittest.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browsing_data/browsing_data_indexed_db_helper.h"
 
-#include "base/message_loop/message_loop_proxy.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/thread_task_runner_handle.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/storage_partition.h"
@@ -19,7 +19,7 @@ class CannedBrowsingDataIndexedDBHelperTest : public testing::Test {
  public:
   void SetUp() override {
     IndexedDBContext()->SetTaskRunnerForTesting(
-        base::MessageLoopProxy::current().get());
+        base::ThreadTaskRunnerHandle::Get().get());
   }
 
   content::IndexedDBContext* IndexedDBContext() {
--- chrome/browser/browsing_data/browsing_data_quota_helper.cc
+++ chrome/browser/browsing_data/browsing_data_quota_helper.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browsing_data/browsing_data_quota_helper.h"
 
 #include "base/location.h"
+#include "base/single_thread_task_runner.h"
 
 BrowsingDataQuotaHelper::QuotaInfo::QuotaInfo()
     : temporary_usage(0),
@@ -36,7 +37,7 @@ void BrowsingDataQuotaHelperDeleter::Destruct(
 }
 
 BrowsingDataQuotaHelper::BrowsingDataQuotaHelper(
-    base::MessageLoopProxy* io_thread)
+    base::SingleThreadTaskRunner* io_thread)
     : io_thread_(io_thread) {
 }
 
