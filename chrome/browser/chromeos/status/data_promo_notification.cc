b98991c60cef8a67f164f1bd902831291e764144
glevin <glevin@chromium.org>
2015-04-29 08:17:44 -0700

    Uma stats for Data Saver prompt
    BUG=475194
    
    Collects UMA stats for
    (i) how  many times the Data Saver prompt (notification) is shown, and
    (ii) how many times the Data Saver prompt is clicked on.
    
    Review URL: https://codereview.chromium.org/1106173003
    
    Cr-Commit-Position: refs/heads/master@{#327490}

--- chrome/browser/chromeos/status/data_promo_notification.cc
+++ chrome/browser/chromeos/status/data_promo_notification.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/network/network_event_log.h"
 #include "chromeos/network/network_state.h"
 #include "chromeos/network/network_state_handler.h"
+#include "content/public/browser/user_metrics.h"
 #include "extensions/browser/extension_registry.h"
 #include "grit/ui_chromeos_resources.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
@@ -172,6 +173,8 @@ void NotificationClicked(const std::string& service_path,
     chrome::ScopedTabbedBrowserDisplayer displayer(
         ProfileManager::GetPrimaryUserProfile(), chrome::HOST_DESKTOP_TYPE_ASH);
     chrome::ShowSingletonTab(displayer.browser(), GURL(info_url));
+    if (info_url == kDataSaverExtensionUrl)
+      content::RecordAction(base::UserMetricsAction("DataSaverPrompt_Clicked"));
   } else {
     ui::NetworkConnect::Get()->ShowNetworkSettingsForPath(service_path);
   }
@@ -309,6 +312,7 @@ bool DataPromoNotification::ShowDataSaverNotification() {
           kDataSaverNotificationId, title, message, icon,
           ui::NetworkStateNotifier::kNotifierNetwork,
           base::Bind(&NotificationClicked, "", kDataSaverExtensionUrl)));
+  content::RecordAction(base::UserMetricsAction("DataSaverPrompt_Shown"));
 
   if (DataSaverSwitchDemoMode()) {
     SetDataSaverPromptsShown(0);  // demo mode resets times shown counts.
