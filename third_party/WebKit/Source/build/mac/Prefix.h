d052cc2ae1d0aac055bbd91fe4444c31a2afc2fe
a.berwal@samsung.com <a.berwal@samsung.com>
2015-04-29 12:51:48 +0000

    Removing blink::prefix
    Removing blink::prefix in Source/build/* whereever it is not needed.
    
    BUG=477256
    
    Review URL: https://codereview.chromium.org/1115623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194651 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/build/mac/Prefix.h
+++ third_party/WebKit/Source/build/mac/Prefix.h
@@ -65,7 +65,7 @@ class TimerHeapReference;
 _LIBCPP_BEGIN_NAMESPACE_STD
 
 inline _LIBCPP_INLINE_VISIBILITY
-const blink::TimerHeapReference& move(const blink::TimerHeapReference& t)
+const TimerHeapReference& move(const TimerHeapReference& t)
 {
     return t;
 }
