8e0bf205717b2117595e38760f596fa7c89c5145
avi <avi@chromium.org>
2015-04-28 21:18:49 -0700

    Use the new ICU plural syntax to fix the strings in the hung pages dialog.
    BUG=43122
    TEST=hang page(s) and verify strings
    
    Review URL: https://codereview.chromium.org/1107273002
    
    Cr-Commit-Position: refs/heads/master@{#327434}

--- chrome/browser/ui/views/hung_renderer_view.h
+++ chrome/browser/ui/views/hung_renderer_view.h
@@ -20,6 +20,7 @@ class WebContents;
 }
 
 namespace views {
+class Label;
 class LabelButton;
 }
 
@@ -150,6 +151,12 @@ class HungRendererDialogView : public views::DialogDelegateView,
 
   static void InitClass();
 
+  // An amusing icon image.
+  static gfx::ImageSkia* frozen_icon_;
+
+  // The label describing the list.
+  views::Label* info_label_;
+
   // Controls within the dialog box.
   views::TableView* hung_pages_table_;
 
@@ -162,9 +169,6 @@ class HungRendererDialogView : public views::DialogDelegateView,
 
   // Whether or not we've created controls for ourself.
   bool initialized_;
-
-  // An amusing icon image.
-  static gfx::ImageSkia* frozen_icon_;
 
   DISALLOW_COPY_AND_ASSIGN(HungRendererDialogView);
 };
