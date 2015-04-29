0000912a1b98af8635b2b65be86ebe7d78671eb6
jianli <jianli@chromium.org>
2015-04-29 14:55:34 -0700

    Add InstanceIDDriver and InstanceID.
    They are used for Instance ID service. Not implemented yet.
    
    BUG=477084
    TEST=none
    
    Review URL: https://codereview.chromium.org/1102963004
    
    Cr-Commit-Position: refs/heads/master@{#327589}

---
+++ components/gcm_driver/instance_id/instance_id.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/gcm_driver/instance_id/instance_id.h"
+
+namespace instance_id {
+
+InstanceID::InstanceID(const std::string& app_id)
+    : app_id_(app_id) {
+}
+
+InstanceID::~InstanceID() {
+}
+
+void InstanceID::SetTokenRefreshCallback(const TokenRefreshCallback& callback) {
+  token_refresh_callback_ = callback;
+}
+
+void InstanceID::NotifyTokenRefresh(bool update_id) {
+  if (!token_refresh_callback_.is_null())
+    token_refresh_callback_.Run(this, update_id);
+}
+
+}  // namespace instance_id
