650e1a0d2c125203d5ab49cc434b5aaa7185d0b6
payal.pandey <payal.pandey@samsung.com>
2015-04-29 14:30:33 -0700

    Use of base::StringPairs appropriately in backend_unittest
    Because base/strings/string_split.h defines:
    typedef std::vector<std::pair<std::string, std::string> > StringPairs;
    
    BUG=412250
    
    Review URL: https://codereview.chromium.org/1105623002
    
    Cr-Commit-Position: refs/heads/master@{#327560}

--- net/disk_cache/backend_unittest.cc
+++ net/disk_cache/backend_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial.h"
 #include "base/port.h"
 #include "base/run_loop.h"
+#include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/mock_entropy_provider.h"
@@ -2751,7 +2752,7 @@ void DiskCacheBackendTest::BackendDisabledAPI() {
   iter = CreateIterator();
   EXPECT_NE(net::OK, iter->OpenNextEntry(&entry2));
 
-  std::vector<std::pair<std::string, std::string>> stats;
+  base::StringPairs stats;
   cache_->GetStats(&stats);
   EXPECT_TRUE(stats.empty());
   cache_->OnExternalCacheHit("First");
