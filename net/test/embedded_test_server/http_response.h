88d297296127d19e6e614297d2a42b0ff4305407
payal.pandey <payal.pandey@samsung.com>
2015-04-29 14:40:10 -0700

    Use of base::StringPairs appropriately in net/test/embedded_test_server/http_response.h
    Bescause base/strings/string_split.h defines:
    typedef std::vector<std::pair<std::string, std::string> > StringPairs;
    
    BUG=412250
    
    Review URL: https://codereview.chromium.org/1114843004
    
    Cr-Commit-Position: refs/heads/master@{#327570}

--- net/test/embedded_test_server/http_response.h
+++ net/test/embedded_test_server/http_response.h
@@ -8,10 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <string>
-#include <vector>
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/strings/string_split.h"
 #include "net/http/http_status_code.h"
 
 namespace net {
@@ -61,7 +61,7 @@ class BasicHttpResponse : public HttpResponse {
   HttpStatusCode code_;
   std::string content_;
   std::string content_type_;
-  std::vector<std::pair<std::string, std::string> > custom_headers_;
+  base::StringPairs custom_headers_;
 
   DISALLOW_COPY_AND_ASSIGN(BasicHttpResponse);
 };
