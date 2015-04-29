63cb2620d62c0fff078b65703f20515df430c1a7
anujk.sharma <anujk.sharma@samsung.com>
2015-04-29 03:07:45 -0700

    favor DCHECK_CURRENTLY_ON for better logs in chrome/browser/plugins
    BUG=466848
    
    Review URL: https://codereview.chromium.org/1110333002
    
    Cr-Commit-Position: refs/heads/master@{#327464}

--- chrome/browser/plugins/chrome_plugin_service_filter.cc
+++ chrome/browser/plugins/chrome_plugin_service_filter.cc
@@ -42,7 +42,7 @@ ChromePluginServiceFilter* ChromePluginServiceFilter::GetInstance() {
 void ChromePluginServiceFilter::RegisterResourceContext(
     PluginPrefs* plugin_prefs,
     const void* context) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   base::AutoLock lock(lock_);
   resource_context_map_[context] = plugin_prefs;
 }
@@ -167,7 +167,7 @@ void ChromePluginServiceFilter::AuthorizeAllPlugins(
     content::WebContents* web_contents,
     bool load_blocked,
     const std::string& identifier) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   web_contents->ForEachFrame(base::Bind(&AuthorizeRenderer));
   if (load_blocked) {
     web_contents->SendToAllFrames(new ChromeViewMsg_LoadBlockedPlugins(
@@ -176,7 +176,7 @@ void ChromePluginServiceFilter::AuthorizeAllPlugins(
 }
 
 ChromePluginServiceFilter::ChromePluginServiceFilter() {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   registrar_.Add(this, content::NOTIFICATION_RENDERER_PROCESS_CLOSED,
                  content::NotificationService::AllSources());
   registrar_.Add(this, chrome::NOTIFICATION_PLUGIN_ENABLE_STATUS_CHANGED,
@@ -190,7 +190,7 @@ void ChromePluginServiceFilter::Observe(
     int type,
     const content::NotificationSource& source,
     const content::NotificationDetails& details) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   switch (type) {
     case content::NOTIFICATION_RENDERER_PROCESS_CLOSED: {
       int render_process_id =
