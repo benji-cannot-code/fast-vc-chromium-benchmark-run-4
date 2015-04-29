08465f8b40a6f2893e7ac386d796245f5d3b6c90
jdduke <jdduke@chromium.org>
2015-04-28 19:16:08 -0700

    Prevent selection handles from showing after a tap
    Tap-triggered selections should not trigger the selection handles.
    If a longpress occurs before a tap-triggered selection, suppress
    activation of the selection handles.
    
    BUG=471280
    
    Review URL: https://codereview.chromium.org/1113653002
    
    Cr-Commit-Position: refs/heads/master@{#327419}

--- ui/touch_selection/touch_selection_controller.cc
+++ ui/touch_selection/touch_selection_controller.cc
@@ -174,6 +174,7 @@ void TouchSelectionController::AllowShowingFromCurrentSelection() {
 
 void TouchSelectionController::OnTapEvent() {
   response_pending_input_event_ = TAP;
+  activate_selection_automatically_ = false;
   ShowInsertionHandleAutomatically();
   if (selection_empty_ && !show_on_tap_for_empty_editable_)
     DeactivateInsertion();
