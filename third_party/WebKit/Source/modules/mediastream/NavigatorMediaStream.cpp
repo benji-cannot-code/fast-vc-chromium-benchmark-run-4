0148c69815a082f1674053a83f58808c4fa25079
jww@chromium.org <jww@chromium.org>
2015-04-30 19:33:36 +0000

    Add deprecation warnings to old powerful features on insecure origins.
    As part of the effort to move old powerful features to secure origins
    only (see https://goo.gl/rStTGz for more details), this CL adds warning
    messages for the APIs in question. In particular, this adds warnings
    for:
      - Geolocation
      - Fullscreen
      - Device motion
      - getUserMedia()
    
    R=mkwst@chromium.org
    BUG=481604
    
    Review URL: https://codereview.chromium.org/1115913002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194768 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/mediastream/NavigatorMediaStream.cpp
+++ third_party/WebKit/Source/modules/mediastream/NavigatorMediaStream.cpp
@@ -66,7 +66,7 @@ void NavigatorMediaStream::webkitGetUserMedia(Navigator& navigator, const Dictio
     if (navigator.frame()->document()->isPrivilegedContext(errorMessage)) {
         UseCounter::count(navigator.frame(), UseCounter::GetUserMediaSecureOrigin);
     } else {
-        UseCounter::count(navigator.frame(), UseCounter::GetUserMediaInsecureOrigin);
+        UseCounter::countDeprecation(navigator.frame(), UseCounter::GetUserMediaInsecureOrigin);
         if (navigator.frame()->settings()->strictPowerfulFeatureRestrictions()) {
             exceptionState.throwSecurityError(ExceptionMessages::failedToExecute("webkitGetUserMedia", "Navigator", errorMessage));
             return;
