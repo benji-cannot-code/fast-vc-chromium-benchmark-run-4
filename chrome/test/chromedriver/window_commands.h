38de4e7950a0760b2f1bae60b03f086d78ee3622
samuong <samuong@chromium.org>
2015-04-30 16:10:54 -0700

    [DevTools] Implement synthetic gesture commands.
    This is the third of three CLs to add synthetic gesture commands to the DevTools Input domain:
    
    1. crrev.com/846113004
    2. crrev.com/841013002
    3. crrev.com/792883007
    
    BUG=448182
    
    Review URL: https://codereview.chromium.org/792883007
    
    Cr-Commit-Position: refs/heads/master@{#327823}

--- chrome/test/chromedriver/window_commands.h
+++ chrome/test/chromedriver/window_commands.h
@@ -182,6 +182,19 @@ Status ExecuteTouchMove(
     const base::DictionaryValue& params,
     scoped_ptr<base::Value>* value);
 
+// Do a swipe (scroll) gesture beginning at the element.
+Status ExecuteTouchScroll(
+    Session *session,
+    WebView* web_view,
+    const base::DictionaryValue& params,
+    scoped_ptr<base::Value>* value);
+
+Status ExecuteTouchPinch(
+    Session* session,
+    WebView* web_view,
+    const base::DictionaryValue& params,
+    scoped_ptr<base::Value>* value);
+
 Status ExecuteGetActiveElement(
     Session* session,
     WebView* web_view,
