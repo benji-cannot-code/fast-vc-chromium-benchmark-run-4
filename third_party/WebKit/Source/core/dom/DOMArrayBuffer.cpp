14b4fac911fbbbe8ce6db5fe819e67acea22150e
ashish1.a@samsung.com <ashish1.a@samsung.com>
2015-04-30 05:33:36 +0000

    Replacing v8::Handle<T> with v8::Local<T>
    v8::Handle<T> is just an alias of v8::Local<T> so, replacing v8::Handle
    with v8::Local in Source/core/*
    
    BUG=424445
    
    Review URL: https://codereview.chromium.org/1108833003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194700 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/DOMArrayBuffer.cpp
+++ third_party/WebKit/Source/core/dom/DOMArrayBuffer.cpp
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-v8::Handle<v8::Object> DOMArrayBuffer::wrap(v8::Handle<v8::Object> creationContext, v8::Isolate* isolate)
+v8::Local<v8::Object> DOMArrayBuffer::wrap(v8::Local<v8::Object> creationContext, v8::Isolate* isolate)
 {
     // It's possible that no one except for the new wrapper owns this object at
     // this moment, so we have to prevent GC to collect this object until the
@@ -22,7 +22,7 @@ v8::Handle<v8::Object> DOMArrayBuffer::wrap(v8::Handle<v8::Object> creationConte
     ASSERT(!DOMDataStore::containsWrapper(this, isolate));
 
     const WrapperTypeInfo* wrapperTypeInfo = this->wrapperTypeInfo();
-    v8::Handle<v8::Object> wrapper = v8::ArrayBuffer::New(isolate, data(), byteLength());
+    v8::Local<v8::Object> wrapper = v8::ArrayBuffer::New(isolate, data(), byteLength());
 
     // Only when we create a new wrapper, let V8 know that we allocated and
     // associated a new memory block with the wrapper. Note that
@@ -33,7 +33,7 @@ v8::Handle<v8::Object> DOMArrayBuffer::wrap(v8::Handle<v8::Object> creationConte
     return associateWithWrapper(isolate, wrapperTypeInfo, wrapper);
 }
 
-v8::Handle<v8::Object> DOMArrayBuffer::associateWithWrapper(v8::Isolate* isolate, const WrapperTypeInfo* wrapperTypeInfo, v8::Handle<v8::Object> wrapper)
+v8::Local<v8::Object> DOMArrayBuffer::associateWithWrapper(v8::Isolate* isolate, const WrapperTypeInfo* wrapperTypeInfo, v8::Local<v8::Object> wrapper)
 {
     // This function does not set a deallocation observer to the underlying
     // array buffer.  It's a caller's duty.
