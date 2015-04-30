d9f7ee060f314d93bf47298a691f0ca4c7fd2b1c
pfeldman@chromium.org <pfeldman@chromium.org>
2015-04-30 15:13:27 +0000

    Revert of Remove callback in TracingManager.start(), let it use client interface (patchset #1 id:1 of https://codereview.chromium.org/1118473003/)
    Reason for revert:
    This clearly needs to be the other way around. Imagine that those start / stop actions are async actions, ala promises. You expect to get the result at the caller instead of storing plumbing the success / error in the the call site from the client post-factum.
    
    Original issue's description:
    > Remove callback in TracingManager.start(), let it use client interface
    > 
    > BUG=
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=194737
    
    TBR=yurys@chromium.org,caseq@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review URL: https://codereview.chromium.org/1117973002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194745 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/network/NetworkPanel.js
+++ third_party/WebKit/Source/devtools/front_end/network/NetworkPanel.js
@@ -602,16 +602,6 @@ WebInspector.NetworkPanel.FilmStripRecorder.prototype = {
 
     /**
      * @override
-     * @param {string} error
-     */
-    tracingFailed: function(error)
-    {
-        WebInspector.console.error("Failed to start tracing: " + error);
-        this._target = null;
-    },
-
-    /**
-     * @override
      * @param {!Array.<!WebInspector.TracingManager.EventPayload>} events
      */
     traceEventsCollected: function(events)
