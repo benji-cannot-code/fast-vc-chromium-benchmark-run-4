a59b629412674077d073a08412016758dce8709a
rbyers@chromium.org <rbyers@chromium.org>
2015-04-30 14:51:25 +0000

    Revert of Schedule cursor update when DOM updates layout. (patchset #4 id:60001 of https://codereview.chromium.org/915543003/)
    Reason for revert:
    Exposed mouse cursor flicker regression: http://crbug.com/468641 (due to a pre-existing bug - http://crbug.com/482971).
    
    Original issue's description:
    > Schedule cursor update when DOM updates layout.
    > 
    > BUG=26723
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=190676
    
    TBR=zhengw@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=26723
    
    Review URL: https://codereview.chromium.org/1120653002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194743 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -2045,8 +2045,6 @@ void FrameView::performPostLayoutTasks()
 
     FontFaceSet::didLayout(*m_frame->document());
 
-    frame().eventHandler().scheduleCursorUpdate();
-
     updateWidgetPositions();
 
     // Plugins could have torn down the page inside updateWidgetPositions().
