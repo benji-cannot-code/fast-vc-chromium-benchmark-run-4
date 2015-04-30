91539138d06a485000df68d549ca6345182120d7
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2015-04-30 18:27:37 +0000

    Remove shrinkStandaloneImagesToFit setting
    Blink is now always shrinking image documents to fit, having different
    shrink-to-fit mode for viewport and desktop.
    The shrinkStandaloneImagesToFit setting is no longer used.
    
    Paired with chromium-side https://codereview.chromium.org/1116813002/.
    
    BUG=464295
    
    Review URL: https://codereview.chromium.org/1119593002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194763 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebSettingsImpl.h
+++ third_party/WebKit/Source/web/WebSettingsImpl.h
@@ -147,7 +147,6 @@ public:
     virtual void setShowContextMenuOnMouseUp(bool) override;
     virtual void setShowFPSCounter(bool) override;
     virtual void setShowPaintRects(bool) override;
-    virtual void setShrinksStandaloneImagesToFit(bool) override;
     virtual void setShrinksViewportContentToFit(bool) override;
     virtual void setSmartInsertDeleteEnabled(bool) override;
     virtual void setSpatialNavigationEnabled(bool) override;
