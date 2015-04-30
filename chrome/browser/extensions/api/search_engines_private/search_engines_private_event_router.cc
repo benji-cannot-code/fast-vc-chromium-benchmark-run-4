cfc5f441615e903090477947e7c8cea9b9403cc1
orenb <orenb@chromium.org>
2015-04-29 18:28:30 -0700

    Implement remaining chrome.searchEnginesPrivate methods.
    There may be more nuances to the implementation we'll have to fix later, as it's hard to know until I try it with the UI in place. There is also a chance we'll have to make further IDL changes.. But hopefully this is the last major interface change for searchEnginesPrivate.
    
    (Disabling presubmit because it requires a manual confirmation that the histograms changes were OK since I renamed a method. I confirmed this.)
    
    BUG=480043
    
    NOPRESUBMIT=true
    
    Review URL: https://codereview.chromium.org/1109563003
    
    Cr-Commit-Position: refs/heads/master@{#327636}

--- chrome/browser/extensions/api/search_engines_private/search_engines_private_event_router.cc
+++ chrome/browser/extensions/api/search_engines_private/search_engines_private_event_router.cc
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
+namespace search_engines_private = api::search_engines_private;
+
 SearchEnginesPrivateEventRouter::SearchEnginesPrivateEventRouter(
     content::BrowserContext* context)
     : template_url_service_(nullptr),
@@ -32,8 +34,7 @@ SearchEnginesPrivateEventRouter::SearchEnginesPrivateEventRouter(
     return;
 
   event_router->RegisterObserver(
-      this,
-      api::search_engines_private::OnDefaultSearchEnginesChanged::kEventName);
+      this, search_engines_private::OnSearchEnginesChanged::kEventName);
   template_url_service_ = TemplateURLServiceFactory::GetForProfile(
       Profile::FromBrowserContext(context_));
   StartOrStopListeningForSearchEnginesChanges();
@@ -77,26 +78,30 @@ void SearchEnginesPrivateEventRouter::OnTemplateURLServiceChanged() {
       template_url_service_->GetDefaultSearchProvider();
   std::vector<TemplateURL*> urls = template_url_service_->GetTemplateURLs();
 
-  std::vector<linked_ptr<api::search_engines_private::SearchEngine>> engines;
+  std::vector<linked_ptr<search_engines_private::SearchEngine>> engines;
+
   for (TemplateURL* url : urls) {
-    api::search_engines_private::SearchEngine* engine =
-        new api::search_engines_private::SearchEngine();
+    search_engines_private::SearchEngine* engine =
+        new search_engines_private::SearchEngine();
     engine->guid = url->sync_guid();
     engine->name = base::UTF16ToASCII(url->short_name());
+    engine->keyword = base::UTF16ToASCII(url->keyword());
+    engine->url = url->url();
+    engine->type = url->show_in_default_list()
+        ? search_engines_private::SearchEngineType::SEARCH_ENGINE_TYPE_DEFAULT
+        : search_engines_private::SearchEngineType::SEARCH_ENGINE_TYPE_OTHER;
+
     if (url == default_url)
       engine->is_selected = scoped_ptr<bool>(new bool(true));
 
     engines.push_back(
-        linked_ptr<api::search_engines_private::SearchEngine>(engine));
+        linked_ptr<search_engines_private::SearchEngine>(engine));
   }
 
   scoped_ptr<base::ListValue> args(
-      api::search_engines_private::OnDefaultSearchEnginesChanged::Create(
-          engines));
-
+      search_engines_private::OnSearchEnginesChanged::Create(engines));
   scoped_ptr<Event> extension_event(new Event(
-      api::search_engines_private::OnDefaultSearchEnginesChanged::kEventName,
-      args.Pass()));
+      search_engines_private::OnSearchEnginesChanged::kEventName, args.Pass()));
   EventRouter::Get(context_)->BroadcastEvent(extension_event.Pass());
 }
 
@@ -104,7 +109,7 @@ void SearchEnginesPrivateEventRouter::
     StartOrStopListeningForSearchEnginesChanges() {
   EventRouter* event_router = EventRouter::Get(context_);
   bool should_listen = event_router->HasEventListener(
-      api::search_engines_private::OnDefaultSearchEnginesChanged::kEventName);
+      search_engines_private::OnSearchEnginesChanged::kEventName);
 
   if (should_listen && !listening_) {
     template_url_service_->Load();
@@ -112,6 +117,7 @@ void SearchEnginesPrivateEventRouter::
   } else if (!should_listen && listening_) {
     template_url_service_->RemoveObserver(this);
   }
+
   listening_ = should_listen;
 }
 
