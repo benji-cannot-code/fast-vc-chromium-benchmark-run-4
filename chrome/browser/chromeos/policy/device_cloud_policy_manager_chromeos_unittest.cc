16b454e884a51cca621023ef159098430755cd63
binjin <binjin@chromium.org>
2015-04-30 12:14:39 -0700

    add DeviceCommandsFactoryChromeOS
    DeviceCommandsFactoryChromeOS build commands specified for ChromeOS, and is attached to DeviceCloudPolicyManagerChromeOS.
    
    Only remote reboot command is integrated at this moment.
    
    BUG=480982
    
    Review URL: https://codereview.chromium.org/928393002
    
    Cr-Commit-Position: refs/heads/master@{#327759}

--- chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos_unittest.cc
+++ chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos_unittest.cc
@@ -142,6 +142,8 @@ class DeviceCloudPolicyManagerChromeOSTest
     url_fetcher_response_string_ = "{\"access_token\":\"accessToken4Test\","
                                    "\"expires_in\":1234,"
                                    "\"refresh_token\":\"refreshToken4Test\"}";
+
+    AllowUninterestingRemoteCommandFetches();
   }
 
   void TearDown() override {
@@ -192,6 +194,19 @@ class DeviceCloudPolicyManagerChromeOSTest
              new base::FundamentalValue(false),
              NULL);
     EXPECT_TRUE(manager_->policies().Equals(bundle));
+  }
+
+  void AllowUninterestingRemoteCommandFetches() {
+    // We are not interested in remote command fetches that the client initiates
+    // automatically. Make them fail and ignore them otherwise.
+    EXPECT_CALL(device_management_service_,
+                CreateJob(DeviceManagementRequestJob::TYPE_REMOTE_COMMANDS, _))
+        .Times(AnyNumber())
+        .WillRepeatedly(device_management_service_.FailJob(
+            DM_STATUS_TEMPORARY_UNAVAILABLE));
+    EXPECT_CALL(device_management_service_,
+                StartJob(dm_protocol::kValueRequestRemoteCommands, _, _, _, _,
+                         _, _)).Times(AnyNumber());
   }
 
   MOCK_METHOD0(OnDeviceCloudPolicyManagerConnected, void());
@@ -246,7 +261,8 @@ TEST_F(DeviceCloudPolicyManagerChromeOSTest, EnrolledDevice) {
               CreateJob(DeviceManagementRequestJob::TYPE_POLICY_FETCH, _))
       .Times(AtMost(1))
       .WillOnce(device_management_service_.CreateAsyncJob(&policy_fetch_job));
-  EXPECT_CALL(device_management_service_, StartJob(_, _, _, _, _, _, _))
+  EXPECT_CALL(device_management_service_,
+              StartJob(dm_protocol::kValueRequestPolicy, _, _, _, _, _, _))
       .Times(AtMost(1));
   ConnectManager();
   base::RunLoop().RunUntilIdle();
@@ -283,7 +299,8 @@ TEST_F(DeviceCloudPolicyManagerChromeOSTest, UnmanagedDevice) {
               CreateJob(DeviceManagementRequestJob::TYPE_POLICY_FETCH, _))
       .Times(AtMost(1))
       .WillOnce(device_management_service_.CreateAsyncJob(&policy_fetch_job));
-  EXPECT_CALL(device_management_service_, StartJob(_, _, _, _, _, _, _))
+  EXPECT_CALL(device_management_service_,
+              StartJob(dm_protocol::kValueRequestPolicy, _, _, _, _, _, _))
       .Times(AtMost(1));
   ConnectManager();
   base::RunLoop().RunUntilIdle();
@@ -335,7 +352,8 @@ TEST_F(DeviceCloudPolicyManagerChromeOSTest, ConnectAndDisconnect) {
   EXPECT_CALL(device_management_service_,
               CreateJob(DeviceManagementRequestJob::TYPE_POLICY_FETCH, _))
       .WillOnce(device_management_service_.CreateAsyncJob(&policy_fetch_job));
-  EXPECT_CALL(device_management_service_, StartJob(_, _, _, _, _, _, _));
+  EXPECT_CALL(device_management_service_,
+              StartJob(dm_protocol::kValueRequestPolicy, _, _, _, _, _, _));
   EXPECT_CALL(*this, OnDeviceCloudPolicyManagerConnected());
   ConnectManager();
   base::RunLoop().RunUntilIdle();
@@ -425,10 +443,11 @@ class DeviceCloudPolicyManagerChromeOSEnrollmentTest
                 CreateJob(DeviceManagementRequestJob::TYPE_REGISTRATION, _))
         .Times(AtMost(1))
         .WillOnce(device_management_service_.CreateAsyncJob(&register_job));
-    EXPECT_CALL(device_management_service_, StartJob(_, _, _, _, _, _, _))
+    EXPECT_CALL(device_management_service_,
+                StartJob(dm_protocol::kValueRequestRegister, _, _, _, _, _, _))
         .Times(AtMost(1))
-        .WillOnce(DoAll(SaveArg<5>(&client_id_),
-                        SaveArg<6>(&register_request_)));
+        .WillOnce(
+            DoAll(SaveArg<5>(&client_id_), SaveArg<6>(&register_request_)));
     DeviceCloudPolicyInitializer::AllowedDeviceModes modes;
     modes[DEVICE_MODE_ENTERPRISE] = true;
 
@@ -446,6 +465,7 @@ class DeviceCloudPolicyManagerChromeOSEnrollmentTest
                    base::Unretained(this)));
     base::RunLoop().RunUntilIdle();
     Mock::VerifyAndClearExpectations(&device_management_service_);
+    AllowUninterestingRemoteCommandFetches();
 
     if (done_)
       return;
@@ -457,10 +477,12 @@ class DeviceCloudPolicyManagerChromeOSEnrollmentTest
                 CreateJob(DeviceManagementRequestJob::TYPE_POLICY_FETCH, _))
         .Times(AtMost(1))
         .WillOnce(device_management_service_.CreateAsyncJob(&policy_fetch_job));
-    EXPECT_CALL(device_management_service_, StartJob(_, _, _, _, _, _, _))
+    EXPECT_CALL(device_management_service_,
+                StartJob(dm_protocol::kValueRequestPolicy, _, _, _, _, _, _))
         .Times(AtMost(1));
     register_job->SendResponse(register_status_, register_response_);
     Mock::VerifyAndClearExpectations(&device_management_service_);
+    AllowUninterestingRemoteCommandFetches();
 
     if (done_)
       return;
@@ -480,10 +502,12 @@ class DeviceCloudPolicyManagerChromeOSEnrollmentTest
         .Times(AtMost(1))
         .WillOnce(device_management_service_.CreateAsyncJob(
             &robot_auth_fetch_job));
-    EXPECT_CALL(device_management_service_, StartJob(_, _, _, _, _, _, _))
-        .Times(AtMost(1));
+    EXPECT_CALL(device_management_service_,
+                StartJob(dm_protocol::kValueRequestApiAuthorization, _, _, _, _,
+                         _, _)).Times(AtMost(1));
     base::RunLoop().RunUntilIdle();
     Mock::VerifyAndClearExpectations(&device_management_service_);
+    AllowUninterestingRemoteCommandFetches();
 
     if (done_)
       return;
@@ -493,6 +517,7 @@ class DeviceCloudPolicyManagerChromeOSEnrollmentTest
     robot_auth_fetch_job->SendResponse(robot_auth_fetch_status_,
                                        robot_auth_fetch_response_);
     Mock::VerifyAndClearExpectations(&device_management_service_);
+    AllowUninterestingRemoteCommandFetches();
 
     if (done_)
       return;
---
+++ chrome/browser/chromeos/policy/remote_commands/device_commands_factory_chromeos.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/policy/remote_commands/device_commands_factory_chromeos.h"
+
+#include "base/memory/scoped_ptr.h"
+#include "chrome/browser/chromeos/policy/remote_commands/device_command_reboot_job.h"
+#include "chromeos/dbus/dbus_thread_manager.h"
+#include "components/policy/core/common/remote_commands/remote_command_job.h"
+#include "policy/proto/device_management_backend.pb.h"
+
+namespace em = enterprise_management;
+
+namespace policy {
+
+DeviceCommandsFactoryChromeOS::DeviceCommandsFactoryChromeOS() {
+}
+
+DeviceCommandsFactoryChromeOS::~DeviceCommandsFactoryChromeOS() {
+}
+
+scoped_ptr<RemoteCommandJob> DeviceCommandsFactoryChromeOS::BuildJobForType(
+    em::RemoteCommand_Type type) {
+  switch (type) {
+    case em::RemoteCommand_Type_DEVICE_REBOOT:
+      return make_scoped_ptr<RemoteCommandJob>(new DeviceCommandRebootJob(
+          chromeos::DBusThreadManager::Get()->GetPowerManagerClient()));
+    default:
+      return nullptr;
+  }
+}
+
+}  // namespace policy
