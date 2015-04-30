7b615b1826e75e8396398c0e671bf1203527ed95
jam <jam@chromium.org>
2015-04-30 12:29:40 -0700

    Fix html_viewer Windows build after r327559
    Review URL: https://codereview.chromium.org/1119763002
    
    Cr-Commit-Position: refs/heads/master@{#327763}

--- mojo/runner/native_application_support.h
+++ mojo/runner/native_application_support.h
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/interface_request.h"
 #include "mojo/shell/native_runner.h"
 
-#if defined(OS_WIN)
-#undef DELETE
-#endif
-
 namespace base {
 class FilePath;
 }
