19418b0728d563987d12cbb72eab1a5de7037863
danduong <danduong@chromium.org>
2015-04-29 17:23:17 -0700

    Add a mechanism to force parameters of a group to a specific value.
    When using this, the ENTIRE parameter list of the group will be
    overriden. E.g. if you override some parameters of a group, but
    not others, they will be wiped out.
    
    This is to be used in conjunction with --force-fieldtrials which
    forces the group into the trial.
    
    BUG=482720
    
    Review URL: https://codereview.chromium.org/1090813005
    
    Cr-Commit-Position: refs/heads/master@{#327627}

--- chrome/chrome_tests_unit.gypi
+++ chrome/chrome_tests_unit.gypi
@@ -564,6 +564,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'common/search_urls_unittest.cc',
       'common/switch_utils_unittest.cc',
       'common/variations/experiment_labels_unittest.cc',
+      'common/variations/variations_util_unittest.cc',
       'common/worker_thread_ticker_unittest.cc',
       'renderer/chrome_content_renderer_client_unittest.cc',
       'renderer/content_settings_observer_unittest.cc',
--- chrome/common/variations/variations_util.h
+++ chrome/common/variations/variations_util.h
@@ -6,11 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_COMMON_VARIATIONS_VARIATIONS_UTIL_H_
 #define CHROME_COMMON_VARIATIONS_VARIATIONS_UTIL_H_
 
+#include <string>
+
 namespace chrome_variations {
 
 // Get the current set of chosen FieldTrial groups (aka variations) and send
 // them to the child process logging module so it can save it for crash dumps.
 void SetChildProcessLoggingVariationList();
+
+// Provides a mechanism to associate multiple set of params to multiple groups
+// with a formatted string specified from commandline. See
+// kForceFieldTrialParams in chrome/common/chrome_switches.cc for more details
+// on the formatting.
+bool AssociateParamsFromString(const std::string& variations_string);
 
 }  // namespace chrome_variations
 
