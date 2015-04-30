bd951e0d3b6f7ed296e34f8eec6ba5ee048c714b
scottmg <scottmg@chromium.org>
2015-04-30 12:14:51 -0700

    vs2015: remove unused and deprecated hash_set #include
    R=rpaquay@chromium.org
    BUG=440500
    
    Review URL: https://codereview.chromium.org/1118003002
    
    Cr-Commit-Position: refs/heads/master@{#327760}

--- device/bluetooth/bluetooth_adapter_win.h
+++ device/bluetooth/bluetooth_adapter_win.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DEVICE_BLUETOOTH_BLUETOOTH_ADAPTER_WIN_H_
 #define DEVICE_BLUETOOTH_BLUETOOTH_ADAPTER_WIN_H_
 
-#include <hash_set>
 #include <set>
 #include <string>
 #include <utility>
