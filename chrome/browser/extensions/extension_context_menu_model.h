b96e80ab0fbf4d8458553d4ba7e67642f42d2bf0
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-04-30 13:58:31 -0700

    [Extensions Toolbar] Tweak context menu show/hide strings
    Instead of just saying "show" and "hide" (the latter of which implies an action
    *can* be truly hidden - which it can't anymore), be slightly more specific with
    "Show in toolbar" and "Hide in Chrome menu", with a special case for popped-out
    actions of "Keep in toolbar".
    
    BUG=482242
    
    Review URL: https://codereview.chromium.org/1113533005
    
    Cr-Commit-Position: refs/heads/master@{#327785}

--- chrome/browser/extensions/extension_context_menu_model.h
+++ chrome/browser/extensions/extension_context_menu_model.h
@@ -46,6 +46,17 @@ class ExtensionContextMenuModel
   // Type of action the extension icon represents.
   enum ActionType { NO_ACTION = 0, BROWSER_ACTION, PAGE_ACTION };
 
+  // The current visibility of the button; this can affect the "hide"/"show"
+  // strings in the menu.
+  enum ButtonVisibility {
+    // The button is visible on the toolbar.
+    VISIBLE,
+    // The button is temporarily visible on the toolbar, as for showign a popup.
+    TRANSITIVELY_VISIBLE,
+    // The button is showed in the overflow menu.
+    OVERFLOWED
+  };
+
   // Delegate to handle showing an ExtensionAction popup.
   class PopupDelegate {
    public:
@@ -64,6 +75,7 @@ class ExtensionContextMenuModel
   // ShowPopupForDevToolsWindow() to be called on |delegate|.
   ExtensionContextMenuModel(const extensions::Extension* extension,
                             Browser* browser,
+                            ButtonVisibility visibility,
                             PopupDelegate* delegate);
 
   // Create a menu model for the given extension, without support
@@ -88,7 +100,8 @@ class ExtensionContextMenuModel
 
   ~ExtensionContextMenuModel() override;
 
-  void InitMenu(const extensions::Extension* extension);
+  void InitMenu(const extensions::Extension* extension,
+                ButtonVisibility button_visibility_);
 
   // Gets the extension we are displaying the menu for. Returns NULL if the
   // extension has been uninstalled and no longer exists.
