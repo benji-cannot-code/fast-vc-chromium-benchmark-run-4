4bb98eb919525009964df68c0620e0f50eea20d3
amistry <amistry@chromium.org>
2015-04-30 16:50:30 -0700

    Fail all started requests if the utility process crashes or fails to start.
    Review URL: https://codereview.chromium.org/1071453002
    
    Cr-Commit-Position: refs/heads/master@{#327837}

--- chrome/browser/image_decoder_browsertest.cc
+++ chrome/browser/image_decoder_browsertest.cc
@@ -5,8 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/image_decoder.h"
 
+#if defined(OS_POSIX)
+#include <sys/types.h>
+#include <signal.h>
+#endif
+
 #include "chrome/test/base/in_process_browser_test.h"
+#include "content/public/browser/browser_child_process_observer.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/child_process_data.h"
 #include "content/public/test/test_utils.h"
 
 using content::BrowserThread;
@@ -47,6 +54,59 @@ class TestImageRequest : public ImageDecoder::ImageRequest {
   DISALLOW_COPY_AND_ASSIGN(TestImageRequest);
 };
 
+class KillProcessObserver : public content::BrowserChildProcessObserver {
+ public:
+  KillProcessObserver() {
+    Add(this);
+  }
+
+  ~KillProcessObserver() override {
+    Remove(this);
+  }
+
+ private:
+  void BrowserChildProcessHostConnected(
+      const content::ChildProcessData& data) override {
+    base::ProcessHandle handle = data.handle;
+
+    if (handle == base::kNullProcessHandle)
+      return;
+
+#if defined(OS_WIN)
+    // On windows, duplicate the process handle since base::Process closes it on
+    // destruction.
+    base::ProcessHandle out_handle;
+    if (!::DuplicateHandle(GetCurrentProcess(), handle,
+                           GetCurrentProcess(), &out_handle,
+                           0, FALSE, DUPLICATE_SAME_ACCESS))
+      return;
+    handle = out_handle;
+#endif
+
+    EXPECT_TRUE(base::Process(handle).Terminate(0, true));
+  }
+};
+
+#if defined(OS_POSIX)
+class FreezeProcessObserver : public content::BrowserChildProcessObserver {
+ public:
+  FreezeProcessObserver() {
+    Add(this);
+  }
+
+  ~FreezeProcessObserver() override {
+    Remove(this);
+  }
+
+ private:
+  void BrowserChildProcessHostConnected(
+      const content::ChildProcessData& data) override {
+    if (data.handle != base::kNullProcessHandle)
+      EXPECT_EQ(0, kill(data.handle, SIGSTOP));
+  }
+};
+#endif  // defined(OS_POSIX)
+
 }  // namespace
 
 class ImageDecoderBrowserTest : public InProcessBrowserTest {
@@ -69,3 +129,25 @@ IN_PROC_BROWSER_TEST_F(ImageDecoderBrowserTest, StartAndDestroy) {
   test_request.reset();
   runner->Run();
 }
+
+IN_PROC_BROWSER_TEST_F(ImageDecoderBrowserTest, StartAndKillProcess) {
+  KillProcessObserver observer;
+  scoped_refptr<content::MessageLoopRunner> runner =
+      new content::MessageLoopRunner;
+  scoped_ptr<TestImageRequest> test_request(
+      new TestImageRequest(runner->QuitClosure()));
+  ImageDecoder::Start(test_request.get(), std::string());
+  runner->Run();
+}
+
+#if defined(OS_POSIX)
+IN_PROC_BROWSER_TEST_F(ImageDecoderBrowserTest, StartAndFreezeProcess) {
+  FreezeProcessObserver observer;
+  scoped_refptr<content::MessageLoopRunner> runner =
+      new content::MessageLoopRunner;
+  scoped_ptr<TestImageRequest> test_request(
+      new TestImageRequest(runner->QuitClosure()));
+  ImageDecoder::Start(test_request.get(), std::string());
+  runner->Run();
+}
+#endif  // defined(OS_POSIX)
