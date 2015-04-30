7863957d3baa55e2cb0681edf12920abeb4f765a
yurys@chromium.org <yurys@chromium.org>
2015-04-30 11:45:30 +0000

    DevTools: make Debugger.PromiseDetails.status field optional
    PromiseDetails is generic payload sent along with new, update and gc events. There is no information about promise's status during gc so the field should be optional.
    
    BUG=478507
    
    Review URL: https://codereview.chromium.org/1109923003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194731 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/PromiseTracker.cpp
+++ third_party/WebKit/Source/core/inspector/PromiseTracker.cpp
@@ -33,9 +33,7 @@ public:
     {
         if (!m_tracker)
             return;
-        RefPtr<PromiseDetails> promiseDetails = PromiseDetails::create()
-            .setId(m_id)
-            .setStatus(PromiseDetails::Status::Pending); // FIXME: drop this parameter
+        RefPtr<PromiseDetails> promiseDetails = PromiseDetails::create().setId(m_id);
         m_tracker->m_listener->didUpdatePromise(InspectorFrontend::Debugger::EventType::Gc, promiseDetails.release());
     }
 
@@ -154,9 +152,8 @@ void PromiseTracker::didReceiveV8PromiseEvent(ScriptState* scriptState, v8::Loca
     default:
         promiseStatus = PromiseDetails::Status::Rejected;
     };
-    RefPtr<PromiseDetails> promiseDetails = PromiseDetails::create()
-        .setId(id)
-        .setStatus(promiseStatus);
+    RefPtr<PromiseDetails> promiseDetails = PromiseDetails::create().setId(id);
+    promiseDetails->setStatus(promiseStatus);
 
     if (!parentPromise.IsEmpty() && parentPromise->IsObject()) {
         v8::Local<v8::Object> handle = parentPromise->ToObject(scriptState->isolate());
