804c1568d96f88e2373953c489d4bd6343bff3de
bauerb <bauerb@chromium.org>
2015-04-30 10:05:30 -0700

    Plugin placeholder cleanup.
    * Use an accurate message for plugins blocked by policy (which can't be loaded even via right-click).
    * Bind the `plugin` object in the placeholder Web UI in LoadablePluginPlaceholder, because that is where the callbacks are registered (ChromePluginPlaceholder now only overrides GetObjectTemplateBuilder to register additional callbacks).
    
    Review URL: https://codereview.chromium.org/1109363002
    
    Cr-Commit-Position: refs/heads/master@{#327727}

--- chrome/renderer/plugins/chrome_plugin_placeholder.h
+++ chrome/renderer/plugins/chrome_plugin_placeholder.h
@@ -70,9 +70,6 @@ class ChromePluginPlaceholder : public plugins::LoadablePluginPlaceholder,
                           const base::string16& title);
   ~ChromePluginPlaceholder() override;
 
-  // WebViewPlugin::Delegate (via PluginPlaceholder) method
-  void BindWebFrame(blink::WebFrame* frame) override;
-
   // gin::Wrappable (via PluginPlaceholder) method
   gin::ObjectTemplateBuilder GetObjectTemplateBuilder(
       v8::Isolate* isolate) override;
@@ -90,9 +87,11 @@ class ChromePluginPlaceholder : public plugins::LoadablePluginPlaceholder,
   void OnMenuAction(int request_id, unsigned action) override;
   void OnMenuClosed(int request_id) override;
 
-  // Javascript callback opens chrome://plugins in a new tab.
+  // Javascript callbacks:
+  // Open chrome://plugins in a new tab.
   void OpenAboutPluginsCallback();
 
+  // IPC message handlers:
 #if defined(ENABLE_PLUGIN_INSTALLATION)
   void OnDidNotFindMissingPlugin();
   void OnFoundMissingPlugin(const base::string16& plugin_name);
