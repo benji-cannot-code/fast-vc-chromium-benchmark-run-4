356ea34d56d021b5ebff78703b08659aedfd92c0
thakis <thakis@chromium.org>
2015-04-30 13:07:48 -0700

    win: Fix remaining plugin warnings in ash, device, printing, sandbox.
    https://www.chromium.org/developers/coding-style/chromium-style-checker-errors
    
    BUG=467287
    TBR=cpu
    
    Review URL: https://codereview.chromium.org/1115293002
    
    Cr-Commit-Position: refs/heads/master@{#327773}

--- ash/test/test_suite.cc
+++ ash/test/test_suite.cc
@@ -26,7 +26,11 @@ namespace ash {
 namespace test {
 
 AuraShellTestSuite::AuraShellTestSuite(int argc, char** argv)
-    : TestSuite(argc, argv) {}
+    : TestSuite(argc, argv) {
+}
+
+AuraShellTestSuite::~AuraShellTestSuite() {
+}
 
 void AuraShellTestSuite::Initialize() {
   base::TestSuite::Initialize();
--- ash/test/test_suite.h
+++ ash/test/test_suite.h
@@ -21,6 +21,7 @@ namespace test {
 class AuraShellTestSuite : public base::TestSuite {
  public:
   AuraShellTestSuite(int argc, char** argv);
+  ~AuraShellTestSuite() override;
 
  protected:
   // base::TestSuite:
