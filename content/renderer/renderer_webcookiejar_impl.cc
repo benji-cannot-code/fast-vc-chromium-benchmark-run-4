adf7e05b95fe1b2be62ed6e285ded9eb0131b1d0
nick <nick@chromium.org>
2015-05-01 16:15:37 -0700

    Move cookie IPC messages from View to Frame.
    They were actually already frame-oriented, so this is just a matter of
    renaming them.
    
    BUG=304341
    
    Review URL: https://codereview.chromium.org/1064863005
    
    Cr-Commit-Position: refs/heads/master@{#328011}

--- content/renderer/renderer_webcookiejar_impl.cc
+++ content/renderer/renderer_webcookiejar_impl.cc
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/renderer_webcookiejar_impl.h"
 
 #include "base/strings/utf_string_conversions.h"
-#include "content/common/view_messages.h"
+#include "content/common/frame_messages.h"
 #include "content/public/renderer/content_renderer_client.h"
-#include "content/renderer/render_view_impl.h"
+#include "content/renderer/render_frame_impl.h"
 
 using blink::WebString;
 using blink::WebURL;
@@ -19,14 +19,14 @@ void RendererWebCookieJarImpl::setCookie(
     const WebURL& url, const WebURL& first_party_for_cookies,
     const WebString& value) {
   std::string value_utf8 = base::UTF16ToUTF8(value);
-  sender_->Send(new ViewHostMsg_SetCookie(
+  sender_->Send(new FrameHostMsg_SetCookie(
       sender_->GetRoutingID(), url, first_party_for_cookies, value_utf8));
 }
 
 WebString RendererWebCookieJarImpl::cookies(
     const WebURL& url, const WebURL& first_party_for_cookies) {
   std::string value_utf8;
-  sender_->Send(new ViewHostMsg_GetCookies(
+  sender_->Send(new FrameHostMsg_GetCookies(
       sender_->GetRoutingID(), url, first_party_for_cookies, &value_utf8));
   return WebString::fromUTF8(value_utf8);
 }
@@ -39,7 +39,7 @@ WebString RendererWebCookieJarImpl::cookieRequestHeaderFieldValue(
 bool RendererWebCookieJarImpl::cookiesEnabled(
     const WebURL& url, const WebURL& first_party_for_cookies) {
   bool cookies_enabled = false;
-  sender_->Send(new ViewHostMsg_CookiesEnabled(
+  sender_->Send(new FrameHostMsg_CookiesEnabled(
       sender_->GetRoutingID(), url, first_party_for_cookies, &cookies_enabled));
   return cookies_enabled;
 }
