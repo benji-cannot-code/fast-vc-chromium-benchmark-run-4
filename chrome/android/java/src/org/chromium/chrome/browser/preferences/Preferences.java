be0eaf4b2227f42f3c4c2f3d0914fde93528d6cb
yfriedman <yfriedman@chromium.org>
2015-05-01 11:12:38 -0700

    More fixes for NFC being revoked.
    I missed a place that was accessing NFC.  In addition, check for
    permission rather than catching SecurityException. The exception fires
    too late, after NFC internally registers for ActivityLifecyleCallbacks
    which means we'd just end up crashing later in NFC code when activity
    state changes.
    
    BUG=481979
    
    Review URL: https://codereview.chromium.org/1115303003
    
    Cr-Commit-Position: refs/heads/master@{#327943}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/Preferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/Preferences.java
@@ -5,16 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.preferences;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.graphics.BitmapFactory;
 import android.nfc.NfcAdapter;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Process;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceFragment.OnPreferenceStartFragmentCallback;
@@ -153,10 +156,14 @@ public abstract class Preferences extends AppCompatActivity implements
                     .commit();
         }
 
-        // Disable Android Beam on JB and later devices.
-        // In ICS it does nothing - i.e. we will send a Play Store link if NFC is used.
-        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        if (nfcAdapter != null) nfcAdapter.setNdefPushMessage(null, this);
+        if (checkPermission(Manifest.permission.NFC, Process.myPid(), Process.myUid())
+                == PackageManager.PERMISSION_GRANTED) {
+            // Disable Android Beam on JB and later devices.
+            // In ICS it does nothing - i.e. we will send a Play Store link if NFC is used.
+            NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+            if (nfcAdapter != null) nfcAdapter.setNdefPushMessage(null, this);
+        }
+
 
         Resources res = getResources();
         ApiCompatibilityUtils.setTaskDescription(this, res.getString(R.string.app_name),
