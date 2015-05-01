bab74f465b6e674e3b31aced43d6e12c7cbe79c2
fgorski <fgorski@chromium.org>
2015-05-01 12:46:19 -0700

    [GCM] Adding custom heartbeat interval to MCS Client
    In this patch:
    * Heartbeat manager is extended to expose max and min custom heartbeat,
      based on the current network connection, as well as calculate HB to
      use including client request
    * MCS Client adds methods to specify a heartbeat as well as revoke it,
      calculates the current minimal interval and if necessary, resets the
      connection to use it.
    
    BUG=481174
    R=zea@chromium.org
    
    Review URL: https://codereview.chromium.org/1101333002
    
    Cr-Commit-Position: refs/heads/master@{#327962}

--- google_apis/gcm/engine/fake_connection_factory.cc
+++ google_apis/gcm/engine/fake_connection_factory.cc
@@ -13,7 +13,8 @@ namespace gcm {
 
 FakeConnectionFactory::FakeConnectionFactory()
     : reconnect_pending_(false),
-      delay_reconnect_(false) {
+      delay_reconnect_(false),
+      connection_listener_(nullptr) {
 }
 
 FakeConnectionFactory::~FakeConnectionFactory() {
@@ -56,10 +57,13 @@ void FakeConnectionFactory::SignalConnectionReset(
     Connect();
   else
     reconnect_pending_ = true;
+  if (connection_listener_)
+    connection_listener_->OnDisconnected();
 }
 
 void FakeConnectionFactory::SetConnectionListener(
     ConnectionListener* listener) {
+  connection_listener_ = listener;
 }
 
 }  // namespace gcm
