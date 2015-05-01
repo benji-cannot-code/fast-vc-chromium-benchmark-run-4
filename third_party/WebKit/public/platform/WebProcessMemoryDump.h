c9eb4b932478736720e6748a5dd6684afb987e58
primiano@chromium.org <primiano@chromium.org>
2015-05-01 18:45:07 +0000

    [tracing] Expose memory-infra tracing infrastructure to Blink.
    Exposes the core memory-infra classes to Blink. These classes are, in
    the very essence, a reproposition of base::trace_event ones and keep
    the same semantic.
    The Chromium-side changes are in crrev.com/1108403006
    For more documentation see the design doc: http://goo.gl/Bdyql6
    
    BUG=480500
    
    Review URL: https://codereview.chromium.org/1111383002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194822 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/public/platform/WebProcessMemoryDump.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WebProcessMemoryDump_h
+#define WebProcessMemoryDump_h
+
+#include "WebNonCopyable.h"
+#include "WebString.h"
+
+namespace blink {
+
+class WebMemoryAllocatorDump;
+
+// A container which holds all the dumps for the various allocators for a given
+// process. Embedders of WebMemoryDumpProvider are expected to populate a
+// WebProcessMemoryDump instance with the stats of their allocators.
+class BLINK_PLATFORM_EXPORT WebProcessMemoryDump : public WebNonCopyable {
+public:
+    // Creates a new MemoryAllocatorDump with the given name and returns the
+    // empty object back to the caller. |absoluteName| uniquely identifies the
+    // dump within the scope of a ProcessMemoryDump. It is possible to express
+    // nesting by means of a slash-separated path naming (e.g.,
+    // "allocator_name/arena_1/subheap_X").
+    virtual WebMemoryAllocatorDump* createMemoryAllocatorDump(const WebString& absoluteName)
+    {
+        return nullptr;
+    }
+};
+
+} // namespace blink
+
+#endif // WebProcessMemoryDump_h
