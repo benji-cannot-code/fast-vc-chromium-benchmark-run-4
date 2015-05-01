43f320aa2e1eee98f094daff69978bf95ad3a31d
peter <peter@chromium.org>
2015-05-01 09:24:30 -0700

    Prepare Push Messaging browser tests for the endpoint value change.
    The subscriptionId property is being deprecated and its value will
    be appended to the actual push endpoint. This CL prepares the browser
    tests for this change, to make sure they don't break.
    
    This CL is part of a three-sided patch:
      [1] This CL.
      [2] https://codereview.chromium.org/1084683003/
      [3] https://codereview.chromium.org/1078873004/
    
    BUG=477401
    
    Review URL: https://codereview.chromium.org/1099673002
    
    Cr-Commit-Position: refs/heads/master@{#327918}

--- chrome/browser/push_messaging/push_messaging_browsertest.cc
+++ chrome/browser/push_messaging/push_messaging_browsertest.cc
@@ -224,8 +224,9 @@ void PushMessagingBrowserTest::TryToSubscribeSuccessfully(
   EXPECT_EQ("permission status - granted", script_result);
 
   EXPECT_TRUE(RunScript("subscribePush()", &script_result));
-  EXPECT_EQ(std::string(kPushMessagingEndpoint) + " - "
-            + expected_push_subscription_id, script_result);
+
+  // TODO(peter): Add an EXPECT for the given endpoint after the value change
+  // has gone through.
 }
 
 PushMessagingApplicationId PushMessagingBrowserTest::GetServiceWorkerAppId(
@@ -264,7 +265,9 @@ IN_PROC_BROWSER_TEST_F(PushMessagingBrowserTest,
 
   InfoBarResponder accepting_responder(GetInfoBarService(), true);
   ASSERT_TRUE(RunScript("subscribePush()", &script_result));
-  EXPECT_EQ(std::string(kPushMessagingEndpoint) + " - 1-0", script_result);
+
+  // TODO(peter): Add an EXPECT for the given endpoint after the value change
+  // has gone through.
 
   PushMessagingApplicationId app_id = GetServiceWorkerAppId(0LL);
   EXPECT_EQ(app_id.app_id_guid(), gcm_service()->last_registered_app_id());
@@ -588,7 +591,9 @@ IN_PROC_BROWSER_TEST_F(PushMessagingBrowserTest, PermissionStateSaysGranted) {
   EXPECT_EQ("permission status - granted", script_result);
 
   ASSERT_TRUE(RunScript("subscribePush()", &script_result));
-  EXPECT_EQ(std::string(kPushMessagingEndpoint) + " - 1-0", script_result);
+
+  // TODO(peter): Add an EXPECT for the given endpoint after the value change
+  // has gone through.
 
   ASSERT_TRUE(RunScript("permissionState()", &script_result));
   EXPECT_EQ("permission status - granted", script_result);
