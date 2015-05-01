9c894ab9582133b15510a1e24b4182fc68321c56
tasak@google.com <tasak@google.com>
2015-05-01 06:39:05 +0000

    Componentization: add toV8(void*,...) to check function overload mismatch.
    Added toV8(void*,...) without any definition to find function overload
    mismatch.
    The toV8(void*,...) should be never used. So if we see any unresolved
    symbols: toV8(void*,...), we will know that we have bugs.
    If we don't have toV8(void*,...), such buggy code will use toV8(bool,...).
    This makes it a little difficult for us to find the bugs.
    
    BUG=358074
    
    Review URL: https://codereview.chromium.org/1118653002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194789 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/background_sync/SyncError.h
+++ third_party/WebKit/Source/modules/background_sync/SyncError.h
@@ -6,12 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SyncError_h
 #define SyncError_h
 
+#include "core/dom/DOMException.h"
 #include "platform/heap/Handle.h"
 #include "public/platform/modules/background_sync/WebSyncError.h"
 
 namespace blink {
 
-class DOMException;
 class ScriptPromiseResolver;
 
 class SyncError {
--- third_party/WebKit/Source/modules/cachestorage/Cache.cpp
+++ third_party/WebKit/Source/modules/cachestorage/Cache.cpp
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptState.h"
 #include "bindings/core/v8/V8ThrowException.h"
 #include "bindings/modules/v8/V8Response.h"
-#include "core/dom/DOMException.h"
 #include "modules/fetch/BodyStreamBuffer.h"
 #include "modules/fetch/GlobalFetch.h"
 #include "modules/fetch/Request.h"
