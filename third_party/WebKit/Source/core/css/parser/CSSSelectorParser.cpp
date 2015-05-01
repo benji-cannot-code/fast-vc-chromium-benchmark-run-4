f7a1c99ef6534890aad29f96add026f4be8c924a
timloh@chromium.org <timloh@chromium.org>
2015-05-01 07:46:43 +0000

    Allow whitespace at end of :not() pseudo-class, i.e. :not(div )
    This patch fixes a bug in the (new) CSS selector parser where we would
    reject :not() pseudo-classes with trailing whitespace. Note that our
    current implementation or :not() is partway between the level 3 and 4
    specs, where we allow compound selectors instead of simple selectors
    and selector lists respectively.
    
    BUG=482221
    
    Review URL: https://codereview.chromium.org/1119983002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/CSSSelectorParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSSelectorParser.cpp
@@ -310,6 +310,7 @@ PassOwnPtr<CSSParserSelector> CSSSelectorParser::consumePseudo(CSSParserTokenRan
 
     if (colons == 1 && token.valueEqualsIgnoringCase("not")) {
         OwnPtr<CSSParserSelector> innerSelector = consumeCompoundSelector(block);
+        block.consumeWhitespace();
         if (!innerSelector || !innerSelector->isSimple() || !block.atEnd())
             return nullptr;
         Vector<OwnPtr<CSSParserSelector>> selectorVector;
