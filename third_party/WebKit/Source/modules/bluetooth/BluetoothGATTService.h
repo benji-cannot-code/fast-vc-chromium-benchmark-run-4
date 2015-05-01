59d29bedbfbd4eb13ab093844bbedb47d500a14f
ortuno@chromium.org <ortuno@chromium.org>
2015-05-01 03:50:05 +0000

    bluetooth: Stub out BluetoothGATTService.
    https://webbluetoothcg.github.io/web-bluetooth/#idl-def-bluetoothgattservice
    
    BUG=483342
    
    Review URL: https://codereview.chromium.org/1114093005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194783 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/modules/bluetooth/BluetoothGATTService.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BluetoothGATTService_h
+#define BluetoothGATTService_h
+
+#include "bindings/core/v8/ScriptWrappable.h"
+#include "platform/heap/Handle.h"
+
+namespace blink {
+
+// Represents a GATT Service within a Bluetooth Peripheral, a collection of
+// characteristics and relationships to other services that encapsulate the
+// behavior of part of a device.
+class BluetoothGATTService final
+    : public GarbageCollected<BluetoothGATTService>
+    , public ScriptWrappable {
+    DEFINE_WRAPPERTYPEINFO();
+public:
+    DEFINE_INLINE_TRACE() { }
+};
+
+} // namespace blink
+
+#endif // BluetoothGATTService_h
