b043c09f54545d85b2c25e602291120e17e1a164
prabhur <prabhur@chromium.org>
2015-05-01 18:51:23 -0700

    Enable CNS tests on all OSes except android, mac. These tests time out in mac (hence disabled). I will be fixing the mac runs in a separate run, but in the mean while enabling them in other OSes.
    BUG=
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;
    
    Review URL: https://codereview.chromium.org/1118383003
    
    Cr-Commit-Position: refs/heads/master@{#328042}

--- tools/perf/benchmarks/media.py
+++ tools/perf/benchmarks/media.py
@@ -46,7 +46,7 @@ class Media(benchmark.Benchmark):
     return 'media.tough_video_cases'
 
 
-@benchmark.Disabled
+@benchmark.Disabled('android', 'mac')
 class MediaNetworkSimulation(benchmark.Benchmark):
   """Obtains media metrics under different network simulations."""
   test = media.Media
@@ -123,3 +123,4 @@ class MediaSourceExtensions(benchmark.Benchmark):
     options.AppendExtraBrowserArgs(
         ['--enable-experimental-web-platform-features',
          '--disable-gesture-requirement-for-media-playback'])
+
