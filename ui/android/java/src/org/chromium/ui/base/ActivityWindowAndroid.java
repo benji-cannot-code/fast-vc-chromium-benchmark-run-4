885fef7d1b689f5dc6c6b5785054f6c80bca15be
estade <estade@chromium.org>
2015-05-03 13:21:57 -0700

    Make ActivityWindowAndroid listen for layout changes on the content view so that WindowAndroid can send keyboard visibility notifications.
    BUG=428087
    
    Review URL: https://codereview.chromium.org/1113303002
    
    Cr-Commit-Position: refs/heads/master@{#328080}

--- ui/android/java/src/org/chromium/ui/base/ActivityWindowAndroid.java
+++ ui/android/java/src/org/chromium/ui/base/ActivityWindowAndroid.java
@@ -10,6 +10,9 @@ import android.app.PendingIntent;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.IntentSender.SendIntentException;
+import android.view.View;
+
+import org.chromium.ui.UiUtils;
 
 import java.lang.ref.WeakReference;
 
@@ -18,7 +21,7 @@ import java.lang.ref.WeakReference;
  * Activity Instance.
  * Only instantiate this class when you need the implemented features.
  */
-public class ActivityWindowAndroid extends WindowAndroid {
+public class ActivityWindowAndroid extends WindowAndroid implements View.OnLayoutChangeListener {
     // Constants used for intent request code bounding.
     private static final int REQUEST_CODE_PREFIX = 1000;
     private static final int REQUEST_CODE_RANGE_SIZE = 100;
@@ -30,6 +33,7 @@ public class ActivityWindowAndroid extends WindowAndroid {
     public ActivityWindowAndroid(Activity activity) {
         super(activity.getApplicationContext());
         mActivityRef = new WeakReference<Activity>(activity);
+        activity.findViewById(android.R.id.content).addOnLayoutChangeListener(this);
     }
 
     @Override
@@ -98,6 +102,12 @@ public class ActivityWindowAndroid extends WindowAndroid {
     public WeakReference<Activity> getActivity() {
         // Return a new WeakReference to prevent clients from releasing our internal WeakReference.
         return new WeakReference<Activity>(mActivityRef.get());
+    }
+
+    @Override
+    public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft,
+            int oldTop, int oldRight, int oldBottom) {
+        keyboardVisibilityPossiblyChanged(UiUtils.isKeyboardShowing(mActivityRef.get(), v));
     }
 
     private int generateNextRequestCode() {
