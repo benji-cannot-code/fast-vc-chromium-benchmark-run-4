d658dd56e90ebda1aaf4227c41755b2284b39b6f
msarda <msarda@chromium.org>
2015-05-04 08:22:13 -0700

    Allow the sign-in client to update the account info.
    This CL allows the sign-in client to update the account info before
    attempting to fetch it from GAIA. This avoids fetching the account
    info from GAIA when the data it already available on the client.
    
    BUG=478770
    
    Review URL: https://codereview.chromium.org/1120363002
    
    Cr-Commit-Position: refs/heads/master@{#328128}

--- ios/public/provider/components/signin/browser/profile_oauth2_token_service_ios_provider.h
+++ ios/public/provider/components/signin/browser/profile_oauth2_token_service_ios_provider.h
@@ -24,6 +24,14 @@ class NSString;
 
 namespace ios {
 
+// Account information.
+struct AccountInfo {
+  AccountInfo() {}
+  ~AccountInfo() {}
+  std::string gaia;
+  std::string email;
+};
+
 enum AuthenticationErrorCategory {
   // Unknown errors.
   kAuthenticationErrorCategoryUnknownErrors,
@@ -69,6 +77,10 @@ class ProfileOAuth2TokenServiceIOSProvider {
   // Returns the authentication error category of |error|.
   virtual AuthenticationErrorCategory GetAuthenticationErrorCategory(
       NSError* error) const = 0;
+
+  // Returns the account info composed of a GAIA id and email corresponding to
+  // |account_id|.
+  virtual AccountInfo GetAccountInfo(const std::string& account_id) const = 0;
 };
 
 }  // namespace ios
--- ios/public/test/fake_profile_oauth2_token_service_ios_provider.mm
+++ ios/public/test/fake_profile_oauth2_token_service_ios_provider.mm
@@ -82,4 +82,9 @@ FakeProfileOAuth2TokenServiceIOSProvider::GetAuthenticationErrorCategory(
   return ios::kAuthenticationErrorCategoryAuthorizationErrors;
 }
 
+ios::AccountInfo FakeProfileOAuth2TokenServiceIOSProvider::GetAccountInfo(
+    const std::string& account_id) const {
+  return ios::AccountInfo();
+}
+
 }  // namespace ios
