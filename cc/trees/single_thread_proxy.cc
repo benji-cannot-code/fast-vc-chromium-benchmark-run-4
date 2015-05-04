e99501e69f7ca26ef4c170d499ab1dbfcabd2c88
mithro <mithro@mithis.com>
2015-05-04 16:51:13 -0700

    cc: Make SingleThreadProxy::CompositeImmediately match scheduler path.
    Only send the BeginMainFrame inside a BeginImplFrame period.
    
    As this path is used without the scheduler it was missed in
    https://codereview.chromium.org/798323003/
    
    BUG=346230
    
    Review URL: https://codereview.chromium.org/1099703004
    
    Cr-Commit-Position: refs/heads/master@{#328220}

--- cc/trees/single_thread_proxy.cc
+++ cc/trees/single_thread_proxy.cc
@@ -588,6 +588,7 @@ void SingleThreadProxy::OnDrawForOutputSurface() {
 void SingleThreadProxy::CompositeImmediately(base::TimeTicks frame_begin_time) {
   TRACE_EVENT0("cc,benchmark", "SingleThreadProxy::CompositeImmediately");
   DCHECK(Proxy::IsMainThread());
+  DCHECK(!inside_impl_frame_);
   base::AutoReset<bool> inside_composite(&inside_synchronous_composite_, true);
 
   if (layer_tree_host_->output_surface_lost()) {
@@ -598,10 +599,19 @@ void SingleThreadProxy::CompositeImmediately(base::TimeTicks frame_begin_time) {
       return;
   }
 
+  BeginFrameArgs begin_frame_args(BeginFrameArgs::Create(
+      BEGINFRAME_FROM_HERE, frame_begin_time, base::TimeTicks(),
+      BeginFrameArgs::DefaultInterval(), BeginFrameArgs::NORMAL));
+
+  // Start the impl frame.
   {
-    BeginFrameArgs begin_frame_args(BeginFrameArgs::Create(
-        BEGINFRAME_FROM_HERE, frame_begin_time, base::TimeTicks(),
-        BeginFrameArgs::DefaultInterval(), BeginFrameArgs::NORMAL));
+    DebugScopedSetImplThread impl(this);
+    WillBeginImplFrame(begin_frame_args);
+  }
+
+  // Run the "main thread" and get it to commit.
+  {
+    DCHECK(inside_impl_frame_);
     DoBeginMainFrame(begin_frame_args);
     DoCommit();
 
@@ -609,8 +619,9 @@ void SingleThreadProxy::CompositeImmediately(base::TimeTicks frame_begin_time) {
         << "Commit should always succeed and transfer promises.";
   }
 
+  // Finish the impl frame.
   {
-    DebugScopedSetImplThread impl(const_cast<SingleThreadProxy*>(this));
+    DebugScopedSetImplThread impl(this);
     if (layer_tree_host_impl_->settings().impl_side_painting) {
       layer_tree_host_impl_->ActivateSyncTree();
       DCHECK(!layer_tree_host_impl_->active_tree()
@@ -628,6 +639,8 @@ void SingleThreadProxy::CompositeImmediately(base::TimeTicks frame_begin_time) {
     // another draw will never be scheduled, so break remaining promises.
     layer_tree_host_impl_->active_tree()->BreakSwapPromises(
         SwapPromise::SWAP_FAILS);
+
+    DidBeginImplFrameDeadline();
   }
 }
 
@@ -718,12 +731,6 @@ DrawResult SingleThreadProxy::DoComposite(base::TimeTicks frame_begin_time,
         FROM_HERE_WITH_EXPLICIT_FUNCTION(
             "461509 SingleThreadProxy::DoComposite5"));
     layer_tree_host_impl_->UpdateAnimationState(start_ready_animations);
-    // TODO(robliao): Remove ScopedTracker below once https://crbug.com/461509
-    // is fixed.
-    tracked_objects::ScopedTracker tracking_profile6(
-        FROM_HERE_WITH_EXPLICIT_FUNCTION(
-            "461509 SingleThreadProxy::DoComposite6"));
-    layer_tree_host_impl_->ResetCurrentBeginFrameArgsForNextFrame();
 
     // TODO(robliao): Remove ScopedTracker below once https://crbug.com/461509
     // is fixed.
