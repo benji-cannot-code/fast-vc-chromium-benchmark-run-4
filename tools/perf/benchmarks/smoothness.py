6752778d17ec747eea783d62013456867c6f56a0
alancutter <alancutter@chromium.org>
2015-05-04 04:14:09 -0700

    Restart the browser before each smoothness.tough_animation_cases test page
    The smoothness.tough_animation_cases tests occasionally fail to execute
    the first frame within the 60s timeout period on Android devices causing
    the test to fail.
    This change resets the browser state before each test to ensure
    things like garbage collection of the previous test don't cause
    tests to time out before they're able to initialise their state.
    
    BUG=469684
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:android_nexus6_perf_bisect
    
    Review URL: https://codereview.chromium.org/1112213006
    
    Cr-Commit-Position: refs/heads/master@{#328112}

--- tools/perf/benchmarks/smoothness.py
+++ tools/perf/benchmarks/smoothness.py
@@ -133,7 +133,7 @@ class SmoothnessKeyMobileSitesWithSlimmingPaint(benchmark.Benchmark):
 
 
 class SmoothnessToughAnimationCases(benchmark.Benchmark):
-  test = smoothness.Smoothness
+  test = smoothness.SmoothnessWithRestart
   page_set = page_sets.ToughAnimationCasesPageSet
 
   @classmethod
--- tools/perf/measurements/smoothness.py
+++ tools/perf/measurements/smoothness.py
@@ -9,8 +9,8 @@ from measurements import smoothness_controller
 
 
 class Smoothness(page_test.PageTest):
-  def __init__(self):
-    super(Smoothness, self).__init__()
+  def __init__(self, **kwargs):
+    super(Smoothness, self).__init__(**kwargs)
     self._smoothness_controller = None
 
   @classmethod
@@ -32,3 +32,8 @@ class Smoothness(page_test.PageTest):
   def CleanUpAfterPage(self, page, tab):
     if self._smoothness_controller:
       self._smoothness_controller.CleanUp(tab)
+
+class SmoothnessWithRestart(Smoothness):
+  def __init__(self):
+    super(SmoothnessWithRestart, self).__init__(
+        needs_browser_restart_after_each_page=True)
