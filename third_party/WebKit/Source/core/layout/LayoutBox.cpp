25e23b6fd33d02fb36c5e843b97bbaf9985fd4ca
rego@igalia.com <rego@igalia.com>
2015-05-04 14:38:57 +0000

    [CSS Grid Layout] Fix grid items overflow
    This is a regression introduced in r194408:
    https://codereview.chromium.org/1098013002
    
    Per spec, min-width|height auto should only apply if overflow is
    visible:
    http://dev.w3.org/csswg/css-flexbox-1/#valdef-min-width-auto
    
    This patch adds the missing condition to the if statements.
    
    BUG=426898,483473
    TEST=fast/css-grid-layout/min-width-height-auto-overflow.html
    
    Review URL: https://codereview.chromium.org/1122763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194863 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -2007,7 +2007,7 @@ void LayoutBox::computeLogicalWidth(LogicalExtentComputedValues& computedValues)
     LayoutUnit containerLogicalWidth = std::max(LayoutUnit(), containingBlockLogicalWidthForContent());
     bool hasPerpendicularContainingBlock = cb->isHorizontalWritingMode() != isHorizontalWritingMode();
 
-    if (parent()->isLayoutGrid() && style()->logicalWidth().isAuto() && style()->minWidth().isAuto()) {
+    if (parent()->isLayoutGrid() && style()->logicalWidth().isAuto() && style()->minWidth().isAuto() && style()->overflowX() == OVISIBLE) {
         LayoutUnit minLogicalWidth = minPreferredLogicalWidth();
         if (containerLogicalWidth < minLogicalWidth) {
             computedValues.m_extent = constrainLogicalWidthByMinMax(minLogicalWidth, containerLogicalWidth, cb);
@@ -2341,7 +2341,7 @@ void LayoutBox::computeLogicalHeight(LayoutUnit logicalHeight, LayoutUnit logica
         // https://bugs.webkit.org/show_bug.cgi?id=46418
         if (hasOverrideLogicalContentHeight() && (parent()->isFlexibleBoxIncludingDeprecated() || parent()->isLayoutGrid())) {
             LayoutUnit contentHeight = overrideLogicalContentHeight();
-            if (parent()->isLayoutGrid() && style()->minHeight().isAuto()) {
+            if (parent()->isLayoutGrid() && style()->minHeight().isAuto() && style()->overflowY() == OVISIBLE) {
                 ASSERT(style()->logicalHeight().isAuto());
                 LayoutUnit minContentHeight = computeContentLogicalHeight(MinSize, Length(MinContent), computedValues.m_extent - borderAndPaddingLogicalHeight());
                 contentHeight = std::max(contentHeight, constrainLogicalHeightByMinMax(minContentHeight, computedValues.m_extent - borderAndPaddingLogicalHeight()));
