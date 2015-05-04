086efe912631a6991aea99b2b320fec9997f0e98
thestig <thestig@chromium.org>
2015-05-04 14:12:30 -0700

    When printing to PDF in kiosk mode, do not prompt for the file name.
    BUG=466441
    
    Review URL: https://codereview.chromium.org/1107083004
    
    Cr-Commit-Position: refs/heads/master@{#328184}

--- chrome/browser/ui/webui/print_preview/print_preview_handler.h
+++ chrome/browser/ui/webui/print_preview/print_preview_handler.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // ENABLE_SERVICE_DISCOVERY
 
 class PrinterHandler;
+class PrintPreviewUI;
 class PrintSystemTaskProxy;
 
 namespace base {
@@ -68,9 +69,6 @@ class PrintPreviewHandler
   void OnAddAccountToCookieCompleted(
       const std::string& account_id,
       const GoogleServiceAuthError& error) override;
-
-  // Displays a modal dialog, prompting the user to select a file.
-  void SelectFile(const base::FilePath& default_path);
 
   // Called when the print preview dialog is destroyed. This is the last time
   // this object has access to the PrintViewManager in order to disconnect the
@@ -119,6 +117,8 @@ class PrintPreviewHandler
   static bool PrivetPrintingEnabled();
 
   content::WebContents* preview_web_contents() const;
+
+  PrintPreviewUI* print_preview_ui() const;
 
   // Gets the list of printers. |args| is unused.
   void HandleGetPrinters(const base::ListValue* args);
@@ -251,6 +251,14 @@ class PrintPreviewHandler
 
   bool GetPreviewDataAndTitle(scoped_refptr<base::RefCountedBytes>* data,
                               base::string16* title) const;
+
+  // If |prompt_user| is true, displays a modal dialog, prompting the user to
+  // select a file. Otherwise, just accept |default_path| and uniquify it.
+  void SelectFile(const base::FilePath& default_path, bool prompt_user);
+
+  // Helper for getting a unique file name for SelectFile() without prompting
+  // the user. Just an adaptor for FileSelected().
+  void OnGotUniqueFileName(const base::FilePath& path);
 
 #if defined(USE_CUPS)
   void SaveCUPSColorSetting(const base::DictionaryValue* settings);
