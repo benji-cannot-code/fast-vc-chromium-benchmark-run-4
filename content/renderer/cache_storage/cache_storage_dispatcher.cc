975f5d66b6fe255132c17a8e76c390513a76b7eb
nhiroki <nhiroki@chromium.org>
2015-05-05 16:59:27 -0700

    CacheStorage: Remove unused responses param from callbacks for batch operations (3)
    (1) Chromium: https://codereview.chromium.org/1112803006/
    (2) Blink: https://codereview.chromium.org/1112963002/
    (3) Chromium: THIS PATCH
    (4) Blink: https://codereview.chromium.org/1107973005
    
    BUG=482827
    TEST=compile
    
    Review URL: https://codereview.chromium.org/1111183007
    
    Cr-Commit-Position: refs/heads/master@{#328441}

--- content/renderer/cache_storage/cache_storage_dispatcher.cc
+++ content/renderer/cache_storage/cache_storage_dispatcher.cc
@@ -177,11 +177,7 @@ class CacheStorageDispatcher::WebCache : public blink::WebServiceWorkerCache {
                                       query_params);
   }
   virtual void dispatchBatch(
-#ifdef CRBUG_482827
       CacheBatchCallbacks* callbacks,
-#else
-      CacheWithResponsesCallbacks* callbacks,
-#endif
       const blink::WebVector<BatchOperation>& batch_operations) {
     if (!dispatcher_)
       return;
@@ -474,15 +470,9 @@ void CacheStorageDispatcher::OnCacheBatchSuccess(
 
   UMA_HISTOGRAM_TIMES("ServiceWorkerCache.Cache.Batch",
                       TimeTicks::Now() - cache_batch_times_[request_id]);
-#ifdef CRBUG_482827
   blink::WebServiceWorkerCache::CacheBatchCallbacks* callbacks =
       cache_batch_callbacks_.Lookup(request_id);
   callbacks->onSuccess();
-#else
-  blink::WebServiceWorkerCache::CacheWithResponsesCallbacks* callbacks =
-      cache_batch_callbacks_.Lookup(request_id);
-  callbacks->onSuccess(&web_responses);
-#endif
   cache_batch_callbacks_.Remove(request_id);
   cache_batch_times_.erase(request_id);
 }
@@ -528,13 +518,8 @@ void CacheStorageDispatcher::OnCacheBatchError(
     int request_id,
     blink::WebServiceWorkerCacheError reason) {
   DCHECK_EQ(thread_id, CurrentWorkerId());
-#ifdef CRBUG_482827
   blink::WebServiceWorkerCache::CacheBatchCallbacks* callbacks =
       cache_batch_callbacks_.Lookup(request_id);
-#else
-  blink::WebServiceWorkerCache::CacheWithResponsesCallbacks* callbacks =
-      cache_batch_callbacks_.Lookup(request_id);
-#endif
   callbacks->onError(&reason);
   cache_batch_callbacks_.Remove(request_id);
   cache_batch_times_.erase(request_id);
@@ -637,11 +622,7 @@ void CacheStorageDispatcher::dispatchKeysForCache(
 
 void CacheStorageDispatcher::dispatchBatchForCache(
     int cache_id,
-#ifdef CRBUG_482827
     blink::WebServiceWorkerCache::CacheBatchCallbacks* callbacks,
-#else
-    blink::WebServiceWorkerCache::CacheWithResponsesCallbacks* callbacks,
-#endif
     const blink::WebVector<blink::WebServiceWorkerCache::BatchOperation>&
         web_operations) {
   int request_id = cache_batch_callbacks_.Add(callbacks);
