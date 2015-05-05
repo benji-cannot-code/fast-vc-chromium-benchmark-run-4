44b82f8029562e3ab2b368baee787390e9320baf
derekjchow <derekjchow@chromium.org>
2015-05-05 12:28:09 -0700

    (Reland) Enable ignoring network interfaces in NetworkChangeNotifierLinux.
    NetworkChangeNotifier should not report network changes from interfaces
    that are not used to connect to the internet (ie. wifi APs). Adjust constructor
    for NetworkChangeNotifierLinux to accept a list of interfaces that are known to
    not be used to connect to internet.
    
    R=pauljensen@chromium.org,akuegel@chromium.org,jochen@chromium.org
    BUG=469863
    
    Review URL: https://codereview.chromium.org/1128533003
    
    Cr-Commit-Position: refs/heads/master@{#328378}

--- net/base/network_change_notifier.cc
+++ net/base/network_change_notifier.cc
@@ -520,7 +520,7 @@ NetworkChangeNotifier* NetworkChangeNotifier::Create() {
 #endif
   return NULL;
 #elif defined(OS_LINUX)
-  return NetworkChangeNotifierLinux::Create();
+  return new NetworkChangeNotifierLinux(base::hash_set<std::string>());
 #elif defined(OS_MACOSX)
   return new NetworkChangeNotifierMac();
 #else
