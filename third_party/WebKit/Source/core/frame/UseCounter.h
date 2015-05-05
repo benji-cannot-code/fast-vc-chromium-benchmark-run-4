028e5024f73c80b860aa30bcc94d02d6f8a646be
rtoy@chromium.org <rtoy@chromium.org>
2015-05-05 17:18:47 +0000

    Throw error on setting AudioBufferSource.buffer more than once
    The deprecation warning was scheduled to be removed by M43, which is
    now beta.  Remove the warning and throw an error if the buffer is set
    more than once.
    
    https://www.chromestatus.com/metrics/feature/timeline/popularity/678 shows no usage at all.
    
    BUG=455373,448960
    TEST=dom-exception.html updated
    
    Review URL: https://codereview.chromium.org/1094433006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194931 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -607,7 +607,6 @@ public:
         Fetch = 675,
         FetchBodyStream = 676,
         XMLHttpRequestAsynchronous = 677,
-        AudioBufferSourceBufferOnce = 678,
         WhiteSpacePreFromXMLSpace = 679,
         WhiteSpaceNowrapFromXMLSpace = 680,
         SVGElementXmlbase = 681,
