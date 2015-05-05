521d722906bcd7a164f5ed3101329d315bc39d87
eranm <eranm@chromium.org>
2015-05-05 09:57:03 -0700

    Certificate Transparency: Add Log URL to log data. The log URL will be used for fetching signed tree heads from CT logs. Having it reviewed now will make future reviews easier as this change touches quite a few files but is functionally straightforward.
    BUG=480867
    
    Review URL: https://codereview.chromium.org/1102413002
    
    Cr-Commit-Position: refs/heads/master@{#328344}

--- net/cert/ct_log_verifier_unittest.cc
+++ net/cert/ct_log_verifier_unittest.cc
@@ -20,7 +20,8 @@ class CTLogVerifierTest : public ::testing::Test {
   CTLogVerifierTest() {}
 
   void SetUp() override {
-    log_ = CTLogVerifier::Create(ct::GetTestPublicKey(), "testlog").Pass();
+    log_ = CTLogVerifier::Create(ct::GetTestPublicKey(), "testlog",
+                                 "https://ct.example.com").Pass();
 
     ASSERT_TRUE(log_);
     ASSERT_EQ(log_->key_id(), ct::GetTestPublicKeyId());
@@ -95,7 +96,8 @@ TEST_F(CTLogVerifierTest, ExcessDataInPublicKey) {
   std::string key = ct::GetTestPublicKey();
   key += "extra";
 
-  scoped_ptr<CTLogVerifier> log = CTLogVerifier::Create(key, "testlog");
+  scoped_ptr<CTLogVerifier> log =
+      CTLogVerifier::Create(key, "testlog", "https://ct.example.com");
   EXPECT_FALSE(log);
 }
 
