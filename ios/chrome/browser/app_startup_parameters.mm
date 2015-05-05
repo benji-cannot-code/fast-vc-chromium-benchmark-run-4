f29a4751644fae8759dc65a0457d36f760633409
sdefresne <sdefresne@chromium.org>
2015-05-05 03:19:50 -0700

    [iOS] Upstream AppStartupParameters
    AppStartupParameters stores all the parameters relevant to the app startup
    in case of launch from another app.
    
    BUG=429756
    
    Review URL: https://codereview.chromium.org/1124703004
    
    Cr-Commit-Position: refs/heads/master@{#328306}

---
+++ ios/chrome/browser/app_startup_parameters.mm
@@ -0,0 +1,66 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/app_startup_parameters.h"
+
+#include "base/logging.h"
+#import "base/mac/scoped_nsobject.h"
+#import "ios/chrome/browser/xcallback_parameters.h"
+#include "url/gurl.h"
+
+@implementation AppStartupParameters {
+  GURL _externalURL;
+  base::scoped_nsobject<XCallbackParameters> _xCallbackParameters;
+  BOOL _launchVoiceSearch;
+}
+
+@synthesize launchVoiceSearch = _launchVoiceSearch;
+
+- (const GURL&)externalURL {
+  return _externalURL;
+}
+
+- (XCallbackParameters*)xCallbackParameters {
+  return _xCallbackParameters.get();
+}
+
+- (instancetype)init {
+  NOTREACHED();
+  return
+      [self initWithExternalURL:GURL() xCallbackParameters:nil voiceSearch:NO];
+}
+
+- (instancetype)initWithExternalURL:(const GURL&)externalURL {
+  return [self initWithExternalURL:externalURL
+               xCallbackParameters:nil
+                       voiceSearch:NO];
+}
+
+- (instancetype)initWithExternalURL:(const GURL&)externalURL
+                xCallbackParameters:(XCallbackParameters*)xCallbackParameters {
+  return [self initWithExternalURL:externalURL
+               xCallbackParameters:xCallbackParameters
+                       voiceSearch:NO];
+}
+
+- (instancetype)initWithExternalURL:(const GURL&)externalURL
+                xCallbackParameters:(XCallbackParameters*)xCallbackParameters
+                        voiceSearch:(BOOL)voicesearch {
+  self = [super init];
+  if (self) {
+    _externalURL = GURL(externalURL);
+    _xCallbackParameters.reset([xCallbackParameters retain]);
+    _launchVoiceSearch = voicesearch;
+  }
+  return self;
+}
+
+- (NSString*)description {
+  return [NSString stringWithFormat:@"ExternalURL: %s \nXCallbackParams: %@",
+                                    _externalURL.spec().c_str(),
+                                    _xCallbackParameters.get()];
+}
+
+@end
--- ios/chrome/ios_chrome.gyp
+++ ios/chrome/ios_chrome.gyp
@@ -83,6 +83,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         ],
       },
       'sources': [
+        'browser/app_startup_parameters.h',
+        'browser/app_startup_parameters.mm',
         'browser/application_context.cc',
         'browser/application_context.h',
         'browser/application_context_impl.cc',
