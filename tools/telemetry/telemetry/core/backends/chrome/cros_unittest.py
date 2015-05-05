326a2bab1d87b1d8bc972d538d417c6b0c4398d3
achuith <achuith@chromium.org>
2015-05-05 09:40:57 -0700

    Enable login_GaiaLogin for powerloadtest acct.
    Similar to the treatment here: https://chromium-review.googlesource.com/#/c/265250/
    BUG=483212
    
    Review URL: https://codereview.chromium.org/1121323002
    
    Cr-Commit-Position: refs/heads/master@{#328343}

--- tools/telemetry/telemetry/core/backends/chrome/cros_unittest.py
+++ tools/telemetry/telemetry/core/backends/chrome/cros_unittest.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import logging
+import urllib2
 
 from telemetry.core.backends.chrome import cros_test_case
 from telemetry.core import exceptions
@@ -70,12 +71,14 @@ class CrOSLoginTest(cros_test_case.CrOSTestCase):
     credentials.txt file, with a single line of format username:password."""
     if self._is_guest:
       return
-    (username, password) = self._Credentials('credentials.txt')
-    if username and password:
-      with self._CreateBrowser(gaia_login=True,
-                               username=username,
-                               password=password):
-        self.assertTrue(util.WaitFor(self._IsCryptohomeMounted, 10))
+    username = 'powerloadtest@gmail.com'
+    password = urllib2.urlopen(
+        'https://sites.google.com/a/chromium.org/dev/chromium-os/testing/'
+        'power-testing/pltp/pltp').read().rstrip()
+    with self._CreateBrowser(gaia_login=True,
+                             username=username,
+                             password=password):
+      self.assertTrue(util.WaitFor(self._IsCryptohomeMounted, 10))
 
 
 class CrOSScreenLockerTest(cros_test_case.CrOSTestCase):
