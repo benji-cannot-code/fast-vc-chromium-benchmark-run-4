d47db85aea34f46bfc7fe3d1b6295962c3bb3124
ccameron <ccameron@chromium.org>
2015-05-05 16:07:42 -0700

    Mac: Ensure that web contents background matches theme
    Have the TabContentsContainerView send the theme's background color down
    to the WebContentsViewCocoa. Make the RenderWidgetHostViewCocoa be transparent,
    so that the WebContentsViewCocoa which is always behind it shows.
    
    Simplify the computation of the blended background color for the
    TabContentsContainerView.
    
    Also fix a bug in TabContentsController where calls to [self view] in dealloc
    would cause the TabContentsContainerView to be lazily allocated (only to be
    immediately destroyed). This was causing as this allocate-while-being-destroyed
    arrangement was creating poorly defined states, which would now be tickled
    more because of the calls to SetBackgroundColor that were inserted.
    
    BUG=470669
    
    Review URL: https://codereview.chromium.org/1124813002
    
    Cr-Commit-Position: refs/heads/master@{#328427}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -4273,6 +4273,13 @@ bool WebContentsImpl::GetAllowOtherViews() {
   return view_->GetAllowOtherViews();
 }
 
+void WebContentsImpl::SetBackgroundColor(SkColor background_color) {
+  if (is_being_destroyed_)
+    return;
+  if (view_)
+    view_->SetBackgroundColor(background_color);
+}
+
 #endif
 
 void WebContentsImpl::OnDialogClosed(int render_process_id,
