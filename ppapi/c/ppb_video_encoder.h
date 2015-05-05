bf01f1ddec4c2fb077a433cf481135187b7ec605
lionel.g.landwerlin <lionel.g.landwerlin@intel.com>
2015-05-05 11:51:42 -0700

    ppapi: VideoEncoder: add more details about supported codecs/platforms
    BUG=455409
    
    Review URL: https://codereview.chromium.org/1127653004
    
    Cr-Commit-Position: refs/heads/master@{#328368}

--- ppapi/c/ppb_video_encoder.h
+++ ppapi/c/ppb_video_encoder.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From ppb_video_encoder.idl modified Thu Feb  5 10:33:32 2015. */
+/* From ppb_video_encoder.idl modified Tue May  5 18:07:09 2015. */
 
 #ifndef PPAPI_C_PPB_VIDEO_ENCODER_H_
 #define PPAPI_C_PPB_VIDEO_ENCODER_H_
@@ -51,9 +51,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *   it. Any pending callbacks will abort before the encoder is destroyed.
  *
  * Available video codecs vary by platform.
- * All: theora, vorbis, vp8.
- * Chrome and ChromeOS: h264.
- * ChromeOS: mpeg4.
+ * All: vp8 (software).
+ * ChromeOS, depending on your device: h264 (hardware), vp8 (hardware)
  */
 struct PPB_VideoEncoder_0_1 { /* dev */
   /**
