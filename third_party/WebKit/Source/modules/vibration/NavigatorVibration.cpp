919a234cb1d02cb4da1859739d06a72e946ed42b
peter@chromium.org <peter@chromium.org>
2015-05-05 18:25:46 +0000

    Change kVibrationDurationMax to be file-local to NavigatorVibration.
    There's only one use of this constant, which is in a file in Blink, so
    there's no need to expose it as part of the Blink API.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1127713005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194933 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/vibration/NavigatorVibration.cpp
+++ third_party/WebKit/Source/modules/vibration/NavigatorVibration.cpp
@@ -26,10 +26,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/Navigator.h"
 #include "core/page/PageVisibilityState.h"
 #include "public/platform/Platform.h"
-#include "public/platform/WebVibration.h"
 
 // Maximum number of entries in a vibration pattern.
 const unsigned kVibrationPatternLengthMax = 99;
+
+// Maximum duration of a vibration is 10 seconds.
+const unsigned kVibrationDurationMsMax = 10000;
 
 blink::NavigatorVibration::VibrationPattern sanitizeVibrationPatternInternal(const blink::NavigatorVibration::VibrationPattern& pattern)
 {
@@ -44,8 +46,8 @@ blink::NavigatorVibration::VibrationPattern sanitizeVibrationPatternInternal(con
 
     // If any pattern entry is too long then truncate it.
     for (size_t i = 0; i < length; ++i) {
-        if (sanitized[i] > blink::kVibrationDurationMax)
-            sanitized[i] = blink::kVibrationDurationMax;
+        if (sanitized[i] > kVibrationDurationMsMax)
+            sanitized[i] = kVibrationDurationMsMax;
     }
 
     // If the last item in the pattern is a pause then discard it.
