c68aa90526230e867419564f91ee9acf18e3c4e4
skyostil <skyostil@chromium.org>
2015-05-05 11:14:44 -0700

    Add a TestBrowserThreadBundle to FileManagerPathUtilTest
    Using a TestingProfile requires a task runner on the current thread.
    
    BUG=465354
    TBR=kinaba@chromium.org
    
    Review URL: https://codereview.chromium.org/1124803004
    
    Cr-Commit-Position: refs/heads/master@{#328359}

--- chrome/browser/chromeos/file_manager/path_util_unittest.cc
+++ chrome/browser/chromeos/file_manager/path_util_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "chrome/browser/download/download_prefs.h"
 #include "chrome/test/base/testing_profile.h"
+#include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace file_manager {
@@ -15,6 +16,7 @@ namespace util {
 namespace {
 
 TEST(FileManagerPathUtilTest, MultiProfileDownloadsFolderMigration) {
+  content::TestBrowserThreadBundle thread_bundle;
   TestingProfile profile;
 
   // This looks like "/home/chronos/u-hash/Downloads" in the production
