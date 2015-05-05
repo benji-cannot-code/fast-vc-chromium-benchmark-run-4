645eff543e0b522af6979910adcab984dacc98fa
thakis <thakis@chromium.org>
2015-05-04 19:51:04 -0700

    android: Fix reliance on implementation-specific behavior in md5sum.
    md5sum calls readsome().
    http://en.cppreference.com/w/cpp/io/basic_istream/readsome says
    
    """The behavior of this function is highly implementation-specific. For
    example, when used with std::ifstream, some library implementations fill the
    underlying filebuf with data as soon as the file is opened (and readsome() on
    such implementations reads data, potentially, but not necessarily, the entire
    file), while other implementations only read from file when an actual input
    operation is requested (and readsome() issued after file opening never extracts
    any characters). Likewise, a call to std::cin.readsome() may return all pending
    unprocessed console input, or may always return zero and extract no
    characters."""
    
    It looks like libc++ chooses to return zero from this function if there's no
    data buffered.
    
    Instead of trying to coax the C++ stream API into blockwise reading, just use
    the C API.
    
    With this patch, running content_unittests_apk becomes 20s faster on first run
    and over twice as fast on subsequent runs with libc++ (matching the speed with
    stlport).
    
    While here, also increase read buffer size from 1kB to 64kB.
    
    BUG=456396
    
    Review URL: https://codereview.chromium.org/1125673009
    
    Cr-Commit-Position: refs/heads/master@{#328257}

--- tools/android/md5sum/md5sum.cc
+++ tools/android/md5sum/md5sum.cc
@@ -19,25 +19,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const int kBufferSize = 1024;
-
 // Returns whether |path|'s MD5 was successfully written to |digest_string|.
 bool MD5Sum(const char* path, std::string* digest_string) {
-  std::ifstream stream(path);
-  if (!stream.good()) {
+  base::ScopedFILE file(fopen(path, "rb"));
+  if (!file) {
     LOG(ERROR) << "Could not open file " << path;
     return false;
   }
   base::MD5Context ctx;
   base::MD5Init(&ctx);
-  char buf[kBufferSize];
-  while (stream.good()) {
-    std::streamsize bytes_read = stream.readsome(buf, sizeof(buf));
-    if (bytes_read == 0)
-      break;
-    base::MD5Update(&ctx, base::StringPiece(buf, bytes_read));
-  }
-  if (stream.fail()) {
+  const size_t kBufferSize = 1 << 16;
+  scoped_ptr<char[]> buf(new char[kBufferSize]);
+  size_t len;
+  while ((len = fread(buf.get(), 1, kBufferSize, file.get())) > 0)
+    base::MD5Update(&ctx, base::StringPiece(buf.get(), len));
+  if (ferror(file.get())) {
     LOG(ERROR) << "Error reading file " << path;
     return false;
   }
