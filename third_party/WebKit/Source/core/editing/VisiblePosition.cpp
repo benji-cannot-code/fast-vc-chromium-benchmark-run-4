6647aec23d1929fee24ee6a4839db414e6237c44
dsinclair@chromium.org <dsinclair@chromium.org>
2015-05-05 20:16:25 +0000

    Rename renderer to layoutObject in core/editing.
    This Cl updates the core/editing code to use layoutObject instead
    of renderer.
    
    BUG=462960
    
    Review URL: https://codereview.chromium.org/1122723005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194938 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/VisiblePosition.cpp
+++ third_party/WebKit/Source/core/editing/VisiblePosition.cpp
@@ -136,22 +136,22 @@ Position VisiblePosition::leftVisuallyDistinctCandidate() const
         if (!box)
             return primaryDirection == LTR ? previousVisuallyDistinctCandidate(m_deepPosition) : nextVisuallyDistinctCandidate(m_deepPosition);
 
-        LayoutObject* renderer = &box->layoutObject();
+        LayoutObject* layoutObject = &box->layoutObject();
 
         while (true) {
-            if ((renderer->isReplaced() || renderer->isBR()) && offset == box->caretRightmostOffset())
+            if ((layoutObject->isReplaced() || layoutObject->isBR()) && offset == box->caretRightmostOffset())
                 return box->isLeftToRightDirection() ? previousVisuallyDistinctCandidate(m_deepPosition) : nextVisuallyDistinctCandidate(m_deepPosition);
 
-            if (!renderer->node()) {
+            if (!layoutObject->node()) {
                 box = box->prevLeafChild();
                 if (!box)
                     return primaryDirection == LTR ? previousVisuallyDistinctCandidate(m_deepPosition) : nextVisuallyDistinctCandidate(m_deepPosition);
-                renderer = &box->layoutObject();
+                layoutObject = &box->layoutObject();
                 offset = box->caretRightmostOffset();
                 continue;
             }
 
-            offset = box->isLeftToRightDirection() ? renderer->previousOffset(offset) : renderer->nextOffset(offset);
+            offset = box->isLeftToRightDirection() ? layoutObject->previousOffset(offset) : layoutObject->nextOffset(offset);
 
             int caretMinOffset = box->caretMinOffset();
             int caretMaxOffset = box->caretMaxOffset();
@@ -177,7 +177,7 @@ Position VisiblePosition::leftVisuallyDistinctCandidate() const
 
                 // Reposition at the other logical position corresponding to our edge's visual position and go for another round.
                 box = prevBox;
-                renderer = &box->layoutObject();
+                layoutObject = &box->layoutObject();
                 offset = prevBox->caretRightmostOffset();
                 continue;
             }
@@ -192,7 +192,7 @@ Position VisiblePosition::leftVisuallyDistinctCandidate() const
                     InlineBox* logicalStart = 0;
                     if (primaryDirection == LTR ? box->root().getLogicalStartBoxWithNode(logicalStart) : box->root().getLogicalEndBoxWithNode(logicalStart)) {
                         box = logicalStart;
-                        renderer = &box->layoutObject();
+                        layoutObject = &box->layoutObject();
                         offset = primaryDirection == LTR ? box->caretMinOffset() : box->caretMaxOffset();
                     }
                     break;
@@ -211,7 +211,7 @@ Position VisiblePosition::leftVisuallyDistinctCandidate() const
                     break;
 
                 box = prevBox;
-                renderer = &box->layoutObject();
+                layoutObject = &box->layoutObject();
                 offset = box->caretRightmostOffset();
                 if (box->direction() == primaryDirection)
                     break;
@@ -223,7 +223,7 @@ Position VisiblePosition::leftVisuallyDistinctCandidate() const
 
             if (prevBox) {
                 box = prevBox;
-                renderer = &box->layoutObject();
+                layoutObject = &box->layoutObject();
                 offset = box->caretRightmostOffset();
                 if (box->bidiLevel() > level) {
                     do {
@@ -253,13 +253,13 @@ Position VisiblePosition::leftVisuallyDistinctCandidate() const
                         break;
                     level = box->bidiLevel();
                 }
-                renderer = &box->layoutObject();
+                layoutObject = &box->layoutObject();
                 offset = primaryDirection == LTR ? box->caretMinOffset() : box->caretMaxOffset();
             }
             break;
         }
 
-        p = createLegacyEditingPosition(renderer->node(), offset);
+        p = createLegacyEditingPosition(layoutObject->node(), offset);
 
         if ((p.isCandidate() && p.downstream() != downstreamStart) || p.atStartOfTree() || p.atEndOfTree())
             return p;
@@ -300,22 +300,22 @@ Position VisiblePosition::rightVisuallyDistinctCandidate() const
         if (!box)
             return primaryDirection == LTR ? nextVisuallyDistinctCandidate(m_deepPosition) : previousVisuallyDistinctCandidate(m_deepPosition);
 
-        LayoutObject* renderer = &box->layoutObject();
+        LayoutObject* layoutObject = &box->layoutObject();
 
         while (true) {
-            if ((renderer->isReplaced() || renderer->isBR()) && offset == box->caretLeftmostOffset())
+            if ((layoutObject->isReplaced() || layoutObject->isBR()) && offset == box->caretLeftmostOffset())
                 return box->isLeftToRightDirection() ? nextVisuallyDistinctCandidate(m_deepPosition) : previousVisuallyDistinctCandidate(m_deepPosition);
 
-            if (!renderer->node()) {
+            if (!layoutObject->node()) {
                 box = box->nextLeafChild();
                 if (!box)
                     return primaryDirection == LTR ? nextVisuallyDistinctCandidate(m_deepPosition) : previousVisuallyDistinctCandidate(m_deepPosition);
-                renderer = &box->layoutObject();
+                layoutObject = &box->layoutObject();
                 offset = box->caretLeftmostOffset();
                 continue;
             }
 
-            offset = box->isLeftToRightDirection() ? renderer->nextOffset(offset) : renderer->previousOffset(offset);
+            offset = box->isLeftToRightDirection() ? layoutObject->nextOffset(offset) : layoutObject->previousOffset(offset);
 
             int caretMinOffset = box->caretMinOffset();
             int caretMaxOffset = box->caretMaxOffset();
@@ -341,7 +341,7 @@ Position VisiblePosition::rightVisuallyDistinctCandidate() const
 
                 // Reposition at the other logical position corresponding to our edge's visual position and go for another round.
                 box = nextBox;
-                renderer = &box->layoutObject();
+                layoutObject = &box->layoutObject();
                 offset = nextBox->caretLeftmostOffset();
                 continue;
             }
@@ -356,7 +356,7 @@ Position VisiblePosition::rightVisuallyDistinctCandidate() const
                     InlineBox* logicalEnd = 0;
                     if (primaryDirection == LTR ? box->root().getLogicalEndBoxWithNode(logicalEnd) : box->root().getLogicalStartBoxWithNode(logicalEnd)) {
                         box = logicalEnd;
-                        renderer = &box->layoutObject();
+                        layoutObject = &box->layoutObject();
                         offset = primaryDirection == LTR ? box->caretMaxOffset() : box->caretMinOffset();
                     }
                     break;
@@ -377,7 +377,7 @@ Position VisiblePosition::rightVisuallyDistinctCandidate() const
 
                 // For example, abc 123 ^ CBA or 123 ^ CBA abc
                 box = nextBox;
-                renderer = &box->layoutObject();
+                layoutObject = &box->layoutObject();
                 offset = box->caretLeftmostOffset();
                 if (box->direction() == primaryDirection)
                     break;
@@ -389,7 +389,7 @@ Position VisiblePosition::rightVisuallyDistinctCandidate() const
 
             if (nextBox) {
                 box = nextBox;
-                renderer = &box->layoutObject();
+                layoutObject = &box->layoutObject();
                 offset = box->caretLeftmostOffset();
 
                 if (box->bidiLevel() > level) {
@@ -420,13 +420,13 @@ Position VisiblePosition::rightVisuallyDistinctCandidate() const
                         break;
                     level = box->bidiLevel();
                 }
-                renderer = &box->layoutObject();
+                layoutObject = &box->layoutObject();
                 offset = primaryDirection == LTR ? box->caretMaxOffset() : box->caretMinOffset();
             }
             break;
         }
 
-        p = createLegacyEditingPosition(renderer->node(), offset);
+        p = createLegacyEditingPosition(layoutObject->node(), offset);
 
         if ((p.isCandidate() && p.downstream() != downstreamStart) || p.atStartOfTree() || p.atEndOfTree())
             return p;
@@ -567,9 +567,9 @@ Position VisiblePosition::canonicalPosition(const Position& passedPosition)
     Position position = passedPosition;
 
     // FIXME (9535):  Canonicalizing to the leftmost candidate means that if we're at a line wrap, we will
-    // ask renderers to paint downstream carets for other renderers.
+    // ask layoutObjects to paint downstream carets for other layoutObjects.
     // To fix this, we need to either a) add code to all paintCarets to pass the responsibility off to
-    // the appropriate renderer for VisiblePosition's like these, or b) canonicalize to the rightmost candidate
+    // the appropriate layoutObject for VisiblePosition's like these, or b) canonicalize to the rightmost candidate
     // unless the affinity is upstream.
     if (position.isNull())
         return Position();
@@ -651,36 +651,36 @@ UChar32 VisiblePosition::characterAfter() const
     return textNode->data().characterStartingAt(offset);
 }
 
-LayoutRect VisiblePosition::localCaretRect(LayoutObject*& renderer) const
+LayoutRect VisiblePosition::localCaretRect(LayoutObject*& layoutObject) const
 {
     PositionWithAffinity positionWithAffinity(m_deepPosition, m_affinity);
-    return localCaretRectOfPosition(positionWithAffinity, renderer);
+    return localCaretRectOfPosition(positionWithAffinity, layoutObject);
 }
 
 IntRect VisiblePosition::absoluteCaretBounds() const
 {
-    LayoutObject* renderer;
-    LayoutRect localRect = localCaretRect(renderer);
-    if (localRect.isEmpty() || !renderer)
+    LayoutObject* layoutObject;
+    LayoutRect localRect = localCaretRect(layoutObject);
+    if (localRect.isEmpty() || !layoutObject)
         return IntRect();
 
-    return renderer->localToAbsoluteQuad(FloatRect(localRect)).enclosingBoundingBox();
+    return layoutObject->localToAbsoluteQuad(FloatRect(localRect)).enclosingBoundingBox();
 }
 
 int VisiblePosition::lineDirectionPointForBlockDirectionNavigation() const
 {
-    LayoutObject* renderer;
-    LayoutRect localRect = localCaretRect(renderer);
-    if (localRect.isEmpty() || !renderer)
+    LayoutObject* layoutObject;
+    LayoutRect localRect = localCaretRect(layoutObject);
+    if (localRect.isEmpty() || !layoutObject)
         return 0;
 
     // This ignores transforms on purpose, for now. Vertical navigation is done
     // without consulting transforms, so that 'up' in transformed text is 'up'
     // relative to the text, not absolute 'up'.
-    FloatPoint caretPoint = renderer->localToAbsolute(FloatPoint(localRect.location()));
-    LayoutObject* containingBlock = renderer->containingBlock();
+    FloatPoint caretPoint = layoutObject->localToAbsolute(FloatPoint(localRect.location()));
+    LayoutObject* containingBlock = layoutObject->containingBlock();
     if (!containingBlock)
-        containingBlock = renderer; // Just use ourselves to determine the writing mode if we have no containing block.
+        containingBlock = layoutObject; // Just use ourselves to determine the writing mode if we have no containing block.
     return containingBlock->isHorizontalWritingMode() ? caretPoint.x() : caretPoint.y();
 }
 
--- third_party/WebKit/Source/core/editing/htmlediting.cpp
+++ third_party/WebKit/Source/core/editing/htmlediting.cpp
@@ -355,8 +355,8 @@ TextDirection directionOfEnclosingBlock(const Position& position)
     Element* enclosingBlockElement = enclosingBlock(position.containerNode());
     if (!enclosingBlockElement)
         return LTR;
-    LayoutObject* renderer = enclosingBlockElement->layoutObject();
-    return renderer ? renderer->style()->direction() : LTR;
+    LayoutObject* layoutObject = enclosingBlockElement->layoutObject();
+    return layoutObject ? layoutObject->style()->direction() : LTR;
 }
 
 // This method is used to create positions in the DOM. It returns the maximum valid offset
@@ -400,8 +400,8 @@ String stringWithRebalancedWhitespace(const String& string, bool startIsStartOfP
 
 bool isTableStructureNode(const Node *node)
 {
-    LayoutObject* renderer = node->layoutObject();
-    return (renderer && (renderer->isTableCell() || renderer->isTableRow() || renderer->isTableSection() || renderer->isLayoutTableCol()));
+    LayoutObject* layoutObject = node->layoutObject();
+    return (layoutObject && (layoutObject->isTableCell() || layoutObject->isTableRow() || layoutObject->isTableSection() || layoutObject->isLayoutTableCol()));
 }
 
 const String& nonBreakingSpaceString()
@@ -419,14 +419,14 @@ static bool isSpecialHTMLElement(const Node& n)
     if (n.isLink())
         return true;
 
-    LayoutObject* renderer = n.layoutObject();
-    if (!renderer)
+    LayoutObject* layoutObject = n.layoutObject();
+    if (!layoutObject)
         return false;
 
-    if (renderer->style()->display() == TABLE || renderer->style()->display() == INLINE_TABLE)
+    if (layoutObject->style()->display() == TABLE || layoutObject->style()->display() == INLINE_TABLE)
         return true;
 
-    if (renderer->style()->isFloating())
+    if (layoutObject->style()->isFloating())
         return true;
 
     return false;
@@ -616,8 +616,8 @@ Node* highestNodeToRemoveInPruning(Node* node, Node* excludeNode)
     Node* previousNode = nullptr;
     Element* rootEditableElement = node ? node->rootEditableElement() : nullptr;
     for (; node; node = node->parentNode()) {
-        if (LayoutObject* renderer = node->layoutObject()) {
-            if (!renderer->canHaveChildren() || hasARenderedDescendant(node, previousNode) || rootEditableElement == node || excludeNode == node)
+        if (LayoutObject* layoutObject = node->layoutObject()) {
+            if (!layoutObject->canHaveChildren() || hasARenderedDescendant(node, previousNode) || rootEditableElement == node || excludeNode == node)
                 return previousNode;
         }
         previousNode = node;
@@ -739,8 +739,8 @@ bool isRenderedTableElement(const Node* node)
     if (!node || !node->isElementNode())
         return false;
 
-    LayoutObject* renderer = node->layoutObject();
-    return (renderer && renderer->isTable());
+    LayoutObject* layoutObject = node->layoutObject();
+    return (layoutObject && layoutObject->isTable());
 }
 
 bool isTableCell(const Node* node)
@@ -754,7 +754,7 @@ bool isEmptyTableCell(const Node* node)
 {
     // Returns true IFF the passed in node is one of:
     //   .) a table cell with no children,
-    //   .) a table cell with a single BR child, and which has no other child renderers, including :before and :after renderers
+    //   .) a table cell with a single BR child, and which has no other child layoutObject, including :before and :after layoutObject
     //   .) the BR child of such a table cell
 
     // Find rendered node
@@ -765,17 +765,17 @@ bool isEmptyTableCell(const Node* node)
 
     // Make sure the rendered node is a table cell or <br>.
     // If it's a <br>, then the parent node has to be a table cell.
-    LayoutObject* renderer = node->layoutObject();
-    if (renderer->isBR()) {
-        renderer = renderer->parent();
-        if (!renderer)
+    LayoutObject* layoutObject = node->layoutObject();
+    if (layoutObject->isBR()) {
+        layoutObject = layoutObject->parent();
+        if (!layoutObject)
             return false;
     }
-    if (!renderer->isTableCell())
+    if (!layoutObject->isTableCell())
         return false;
 
-    // Check that the table cell contains no child renderers except for perhaps a single <br>.
-    LayoutObject* childLayoutObject = toLayoutTableCell(renderer)->firstChild();
+    // Check that the table cell contains no child layoutObjects except for perhaps a single <br>.
+    LayoutObject* childLayoutObject = toLayoutTableCell(layoutObject)->firstChild();
     if (!childLayoutObject)
         return true;
     if (!childLayoutObject->isBR())
@@ -869,11 +869,11 @@ PassRefPtrWillBeRawPtr<HTMLBRElement> createBlockPlaceholderElement(Document& do
 
 bool isNodeRendered(const Node& node)
 {
-    LayoutObject* renderer = node.layoutObject();
-    if (!renderer)
+    LayoutObject* layoutObject = node.layoutObject();
+    if (!layoutObject)
         return false;
 
-    return renderer->style()->visibility() == VISIBLE;
+    return layoutObject->style()->visibility() == VISIBLE;
 }
 
 // return first preceding DOM position rendered at a different location, or "this"
@@ -1122,8 +1122,8 @@ bool isRenderedAsNonInlineTableImageOrHR(const Node* node)
 {
     if (!node)
         return false;
-    LayoutObject* renderer = node->layoutObject();
-    return renderer && ((renderer->isTable() && !renderer->isInline()) || (renderer->isImage() && !renderer->isInline()) || renderer->isHR());
+    LayoutObject* layoutObject = node->layoutObject();
+    return layoutObject && ((layoutObject->isTable() && !layoutObject->isInline()) || (layoutObject->isImage() && !layoutObject->isInline()) || layoutObject->isHR());
 }
 
 bool areIdenticalElements(const Node* first, const Node* second)
@@ -1160,8 +1160,8 @@ bool isNonTableCellHTMLBlockElement(const Node* node)
 
 bool isBlockFlowElement(const Node& node)
 {
-    LayoutObject* renderer = node.layoutObject();
-    return node.isElementNode() && renderer && renderer->isLayoutBlockFlow();
+    LayoutObject* layoutObject = node.layoutObject();
+    return node.isElementNode() && layoutObject && layoutObject->isLayoutBlockFlow();
 }
 
 Position adjustedSelectionStartForStyleComputation(const VisibleSelection& selection)
