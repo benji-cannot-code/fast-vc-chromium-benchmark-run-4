817fa62172eb8652ce7c0da4d8c5723621901103
jochen <jochen@chromium.org>
2015-05-05 02:53:18 -0700

    Revert of Add functions to ignore network interfaces to NetworkChangeNotifier. (patchset #15 id:280001 of https://codereview.chromium.org/1023023002/)
    Reason for revert:
    Breaks Chrome OS compile
    
    Original issue's description:
    > Enable ignoring network interfaces in NetworkChangeNotifierLinux.
    >
    > NetworkChangeNotifier should not report network changes from interfaces
    > that are not used to connect to the internet (ie. wifi APs). Adjust constructor
    > for NetworkChangeNotifierLinux to accept a list of interfaces that are known to
    > not be used to connect to internet.
    >
    > BUG=469863
    >
    > Committed: https://crrev.com/6f45b03b29456bcdf088a06356facc48fd337e63
    > Cr-Commit-Position: refs/heads/master@{#328173}
    
    TBR=pauljensen@chromium.org,wzhong@chromium.org,akuegel@chromium.org,derekjchow@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=469863
    
    Review URL: https://codereview.chromium.org/1115373005
    
    Cr-Commit-Position: refs/heads/master@{#328303}

--- net/tools/net_watcher/net_watcher.cc
+++ net/tools/net_watcher/net_watcher.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop/message_loop.h"
-#include "base/strings/string_split.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "net/base/network_change_notifier.h"
@@ -27,21 +26,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <glib-object.h>
 #endif
 
-#if defined(OS_LINUX)
-#include "net/base/network_change_notifier_linux.h"
-#endif
-
 #if defined(OS_MACOSX)
 #include "base/mac/scoped_nsautorelease_pool.h"
 #endif
 
 namespace {
-
-#if defined(OS_LINUX)
-// Flag to specifies which network interfaces to ignore. Interfaces should
-// follow as a comma seperated list.
-const char kIgnoreNetifFlag[] = "ignore-netif";
-#endif
 
 // Conversions from various network-related types to string.
 
@@ -165,25 +154,8 @@ int main(int argc, char* argv[]) {
 
   NetWatcher net_watcher;
 
-#if defined(OS_LINUX)
-  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-  std::string ignored_netifs_str =
-      command_line->GetSwitchValueASCII(kIgnoreNetifFlag);
-  base::hash_set<std::string> ignored_interfaces;
-  if (!ignored_netifs_str.empty()) {
-    std::vector<std::string> ignored_netifs;
-    base::SplitString(ignored_netifs_str, ',', &ignored_netifs);
-    for (const std::string& ignored_netif : ignored_netifs) {
-      LOG(INFO) << "Ignoring: " << ignored_netif;
-      ignored_interfaces.insert(ignored_netif);
-    }
-  }
-  scoped_ptr<net::NetworkChangeNotifier> network_change_notifier(
-      new net::NetworkChangeNotifierLinux(ignored_interfaces));
-#else
   scoped_ptr<net::NetworkChangeNotifier> network_change_notifier(
       net::NetworkChangeNotifier::Create());
-#endif
 
   // Use the network loop as the file loop also.
   scoped_ptr<net::ProxyConfigService> proxy_config_service(
