1b18777c62aa8c4a0617f3c21bdfbf2bfca4426e
pbond <pbond@chromium.org>
2015-05-05 10:24:19 -0700

    Edited asset id presentation in version string.
    "Asset ID: <asset id>" is shown only if it is specified.
    Removed 'None Specified' localized string.
    
    BUG=464184
    
    Review URL: https://codereview.chromium.org/1124543007
    
    Cr-Commit-Position: refs/heads/master@{#328350}

--- chrome/browser/ui/webui/chromeos/login/core_oobe_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/core_oobe_handler.cc
@@ -99,7 +99,6 @@ void CoreOobeHandler::DeclareLocalizedValues(
 
   // Strings for Asset Identifier shown in version string.
   builder->Add("assetIdLabel", IDS_OOBE_ASSET_ID_LABEL);
-  builder->Add("noneSpecified", IDS_OOBE_NONE_SPECIFIED);
 }
 
 void CoreOobeHandler::Initialize() {
