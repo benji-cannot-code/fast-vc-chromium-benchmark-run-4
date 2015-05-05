765beb385d53a66043b3689b5411667d388b93be
danakj <danakj@chromium.org>
2015-05-05 16:15:58 -0700

    ui: Disable the use_cached_picture option in DisplayListRecordingSource
    The ui compositor does not need the use_cached_picture option and it
    slows down recording a fair bit. Add a flag to turn it off in
    LayerTreeSettings and disable it in ui::Compositor.
    
    R=ajuma
    BUG=466426
    
    Review URL: https://codereview.chromium.org/1125983002
    
    Cr-Commit-Position: refs/heads/master@{#328431}

--- cc/resources/display_list_recording_source.h
+++ cc/resources/display_list_recording_source.h
@@ -15,7 +15,8 @@ class DisplayListRasterSource;
 
 class CC_EXPORT DisplayListRecordingSource : public RecordingSource {
  public:
-  explicit DisplayListRecordingSource(const gfx::Size& grid_cell_size);
+  DisplayListRecordingSource(const gfx::Size& grid_cell_size,
+                             bool use_cached_picture);
   ~DisplayListRecordingSource() override;
 
   // RecordingSource overrides.
@@ -39,6 +40,8 @@ class CC_EXPORT DisplayListRecordingSource : public RecordingSource {
 
  protected:
   void Clear();
+
+  const bool use_cached_picture_;
 
   gfx::Rect recorded_viewport_;
   gfx::Size size_;
