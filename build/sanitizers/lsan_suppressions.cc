8c4e0cd473e33e44e1ea4cfb07f30a73fbb7737e
dcheng <dcheng@chromium.org>
2015-05-04 17:11:31 -0700

    Add a leak sanitizer suppression for RemoteDOMWindow.
    In browser tests, LSan seems to think RemoteDOMWindow leaks even though
    the v8 DOM wrapper is still holding a reference to it.
    
    BUG=480676
    
    Review URL: https://codereview.chromium.org/1123583002
    
    Cr-Commit-Position: refs/heads/master@{#328226}

--- build/sanitizers/lsan_suppressions.cc
+++ build/sanitizers/lsan_suppressions.cc
@@ -67,6 +67,7 @@ char kLSanDefaultSuppressions[] =
 "leak:gin/object_template_builder.h\n"
 "leak:gin::internal::Dispatcher\n"
 "leak:blink::LocalDOMWindow::getComputedStyle\n"
+"leak:blink::RemoteDOMWindow::create\n"
 
 // http://crbug.com/356785
 "leak:content::RenderViewImplTest_DecideNavigationPolicyForWebUI_Test::TestBody\n"
