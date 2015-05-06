1851221183f2ee2e4bec82c72edf55480e313a84
peter <peter@chromium.org>
2015-05-06 06:41:27 -0700

    Address the TODOs in the PushMessagingBrowserTest for the endpoint merge.
    The endpoint value checks have been put back in place, now following
    the new format and only checking the |endpoint| value. Inclusion of the
    subscription Id in the endpoint is covered by Blink's layout tests.
    
    This CL is part of a three-sided patch:
      [1] https://codereview.chromium.org/1099673002/
      [2] https://codereview.chromium.org/1084683003/
      [3] This CL.
    
    BUG=477401
    
    Review URL: https://codereview.chromium.org/1078873004
    
    Cr-Commit-Position: refs/heads/master@{#328523}

--- chrome/test/data/push_messaging/push_test.js
+++ chrome/test/data/push_messaging/push_test.js
@@ -98,8 +98,7 @@ function subscribePush() {
     return swRegistration.pushManager.subscribe()
         .then(function(subscription) {
           pushSubscription = subscription;
-          sendResultToTest(
-              subscription.endpoint + ' - ' + subscription.subscriptionId);
+          sendResultToTest(subscription.endpoint);
         });
   }).catch(sendErrorToTest);
 }
