dd39b075ff5bab682abe17d7001b3c2bbd65d8d3
jdduke <jdduke@chromium.org>
2015-05-06 10:59:56 -0700

    [Android] Cherry-pick upstream appcompat fixes for SwipeRefreshLayout
    Cherry-pick several recent changes to SwipeRefreshLayout and its
    dependencies. This includes both several stylistic tweaks as well as a
    fix for animation transitions on several devices.
    
    BUG=485145
    
    Review URL: https://codereview.chromium.org/1127193002
    
    Cr-Commit-Position: refs/heads/master@{#328559}

--- third_party/android_swipe_refresh/android_swipe_refresh.gyp
+++ third_party/android_swipe_refresh/android_swipe_refresh.gyp
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     {
       'target_name': 'android_swipe_refresh_java',
       'type': 'none',
+      'dependencies': [
+        '../android_tools/android_tools.gyp:android_support_v13_javalib',
+      ],
       'variables': {
         'java_in_dir': 'java',
       },
--- third_party/android_swipe_refresh/java/src/org/chromium/third_party/android/swiperefresh/SwipeRefreshLayout.java
+++ third_party/android_swipe_refresh/java/src/org/chromium/third_party/android/swiperefresh/SwipeRefreshLayout.java
@@ -442,13 +442,30 @@ public class SwipeRefreshLayout extends ViewGroup {
     }
 
     /**
+     * @deprecated Use {@link #setProgressBackgroundColorSchemeResource(int)}
+     */
+    @Deprecated
+    public void setProgressBackgroundColor(int colorRes) {
+        setProgressBackgroundColorSchemeResource(colorRes);
+    }
+
+    /**
      * Set the background color of the progress spinner disc.
      *
      * @param colorRes Resource id of the color.
      */
-    public void setProgressBackgroundColor(int colorRes) {
-        mCircleView.setBackgroundColor(colorRes);
-        mProgress.setBackgroundColor(getResources().getColor(colorRes));
+    public void setProgressBackgroundColorSchemeResource(int colorRes) {
+        setProgressBackgroundColorSchemeColor(getResources().getColor(colorRes));
+    }
+
+    /**
+     * Set the background color of the progress spinner disc.
+     *
+     * @param color
+     */
+    public void setProgressBackgroundColorSchemeColor(int color) {
+        mCircleView.setBackgroundColor(color);
+        mProgress.setBackgroundColor(color);
     }
 
     /**
@@ -597,7 +614,7 @@ public class SwipeRefreshLayout extends ViewGroup {
                 setAnimationProgress(overscrollTop / mTotalDragDistance);
             }
         }
-        float strokeStart = (float) (adjustedPercent * .8f);
+        float strokeStart = adjustedPercent * .8f;
         mProgress.setStartEndTrim(0f, Math.min(MAX_PROGRESS_ANGLE, strokeStart));
         mProgress.setArrowScale(Math.min(1f, adjustedPercent));
 
@@ -720,6 +737,7 @@ public class SwipeRefreshLayout extends ViewGroup {
             targetTop = (mFrom + (int) ((endTarget - mFrom) * interpolatedTime));
             int offset = targetTop - mCircleView.getTop();
             setTargetOffsetTopAndBottom(offset, false /* requires update */);
+            mProgress.setArrowScale(1 - interpolatedTime);
         }
     };
 
