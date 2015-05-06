8b63dca0d4a0ef525dcc2cb6a069bc403339e1d6
cjhopman <cjhopman@chromium.org>
2015-05-06 14:37:48 -0700

    Create minidumps for Java-only crashes
    BUG=477479
    
    Review URL: https://codereview.chromium.org/1109243002
    
    Cr-Commit-Position: refs/heads/master@{#328620}

--- chrome/app/chrome_main_delegate.cc
+++ chrome/app/chrome_main_delegate.cc
@@ -84,6 +84,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_ANDROID)
+#include "chrome/browser/android/java_exception_reporter.h"
 #include "chrome/common/descriptors_android.h"
 #else
 // Diagnostics is only available on non-android platforms.
@@ -785,10 +786,12 @@ void ChromeMainDelegate::PreSandboxStartup() {
   // Zygote needs to call InitCrashReporter() in RunZygote().
   if (process_type != switches::kZygoteProcess) {
 #if defined(OS_ANDROID)
-    if (process_type.empty())
+    if (process_type.empty()) {
       breakpad::InitCrashReporter(process_type);
-    else
+      chrome::android::InitJavaExceptionReporter();
+    } else {
       breakpad::InitNonBrowserCrashReporterForAndroid(process_type);
+    }
 #else  // !defined(OS_ANDROID)
     breakpad::InitCrashReporter(process_type);
 #endif  // defined(OS_ANDROID)
--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/foreign_session_helper.h"
 #include "chrome/browser/android/fullscreen/fullscreen_infobar_delegate.h"
 #include "chrome/browser/android/intent_helper.h"
+#include "chrome/browser/android/java_exception_reporter.h"
 #include "chrome/browser/android/large_icon_bridge.h"
 #include "chrome/browser/android/location_settings_impl.h"
 #include "chrome/browser/android/logo_bridge.h"
@@ -213,6 +214,7 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
      invalidation::InvalidationServiceFactoryAndroid::Register},
     {"ShortcutHelper", ShortcutHelper::RegisterShortcutHelper},
     {"IntentHelper", RegisterIntentHelper},
+    {"JavaExceptionReporter", RegisterJavaExceptionReporterJni},
     {"JavascriptAppModalDialog",
      JavascriptAppModalDialogAndroid::RegisterJavascriptAppModalDialog},
     {"LargeIconBridge", LargeIconBridge::RegisterLargeIconBridge},
