3178487e7f7733262691fe64eba01537d4fdee30
eroman <eroman@chromium.org>
2015-05-06 15:24:35 -0700

    Remove NetLogCaptureMode::ToInternalValueForTesting()
    BUG=484762
    
    Review URL: https://codereview.chromium.org/1123333002
    
    Cr-Commit-Position: refs/heads/master@{#328629}

--- net/log/net_log_capture_mode_unittest.cc
+++ net/log/net_log_capture_mode_unittest.cc
@@ -24,8 +24,6 @@ TEST(NetLogCaptureMode, Default) {
   EXPECT_EQ(mode, NetLogCaptureMode::Default());
   EXPECT_NE(mode, NetLogCaptureMode::IncludeCookiesAndCredentials());
   EXPECT_NE(mode, NetLogCaptureMode::IncludeSocketBytes());
-  EXPECT_EQ(mode.ToInternalValueForTesting(),
-            NetLogCaptureMode::Default().ToInternalValueForTesting());
 }
 
 TEST(NetLogCaptureMode, IncludeCookiesAndCredentials) {
@@ -37,9 +35,6 @@ TEST(NetLogCaptureMode, IncludeCookiesAndCredentials) {
   EXPECT_NE(mode, NetLogCaptureMode::Default());
   EXPECT_EQ(mode, NetLogCaptureMode::IncludeCookiesAndCredentials());
   EXPECT_NE(mode, NetLogCaptureMode::IncludeSocketBytes());
-  EXPECT_EQ(mode.ToInternalValueForTesting(),
-            NetLogCaptureMode::IncludeCookiesAndCredentials()
-                .ToInternalValueForTesting());
 }
 
 TEST(NetLogCaptureMode, IncludeSocketBytes) {
@@ -51,9 +46,6 @@ TEST(NetLogCaptureMode, IncludeSocketBytes) {
   EXPECT_NE(mode, NetLogCaptureMode::Default());
   EXPECT_NE(mode, NetLogCaptureMode::IncludeCookiesAndCredentials());
   EXPECT_EQ(mode, NetLogCaptureMode::IncludeSocketBytes());
-  EXPECT_EQ(
-      mode.ToInternalValueForTesting(),
-      NetLogCaptureMode::IncludeSocketBytes().ToInternalValueForTesting());
 }
 
 }  // namespace
