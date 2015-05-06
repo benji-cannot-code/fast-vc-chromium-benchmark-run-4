7f58527fe69307c4fec306a300b9cb17b9a8f062
hendrikw <hendrikw@chromium.org>
2015-05-05 17:08:42 -0700

    cc: Remove pairs from the raster and eviction queues
    Replace the queue of layer pairs in the raster and eviction
    queues with two separate queues.
    
    BUG=483292
    
    Review URL: https://codereview.chromium.org/1121383002
    
    Cr-Commit-Position: refs/heads/master@{#328444}

--- cc/resources/raster_tile_priority_queue_required.cc
+++ cc/resources/raster_tile_priority_queue_required.cc
@@ -9,6 +9,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
+namespace {
+
+void AppendTilingSetRequiredQueues(
+    const std::vector<PictureLayerImpl*>& layers,
+    ScopedPtrVector<TilingSetRasterQueueRequired>* queues) {
+  for (auto* layer : layers) {
+    scoped_ptr<TilingSetRasterQueueRequired> tiling_set_queue(
+        new TilingSetRasterQueueRequired(
+            layer->picture_layer_tiling_set(),
+            RasterTilePriorityQueueRequired::Type::REQUIRED_FOR_ACTIVATION));
+    if (!tiling_set_queue->IsEmpty())
+      queues->push_back(tiling_set_queue.Pass());
+  }
+}
+
+}  // namespace
+
 RasterTilePriorityQueueRequired::RasterTilePriorityQueueRequired() {
 }
 
@@ -16,49 +33,32 @@ RasterTilePriorityQueueRequired::~RasterTilePriorityQueueRequired() {
 }
 
 void RasterTilePriorityQueueRequired::Build(
-    const std::vector<PictureLayerImpl::Pair>& paired_layers,
+    const std::vector<PictureLayerImpl*>& active_layers,
+    const std::vector<PictureLayerImpl*>& pending_layers,
     Type type) {
   DCHECK_NE(static_cast<int>(type), static_cast<int>(Type::ALL));
   if (type == Type::REQUIRED_FOR_DRAW)
-    BuildRequiredForDraw(paired_layers);
+    BuildRequiredForDraw(active_layers);
   else
-    BuildRequiredForActivation(paired_layers);
+    BuildRequiredForActivation(active_layers, pending_layers);
 }
 
 void RasterTilePriorityQueueRequired::BuildRequiredForDraw(
-    const std::vector<PictureLayerImpl::Pair>& paired_layers) {
-  for (const auto& pair : paired_layers) {
-    if (!pair.active)
-      continue;
+    const std::vector<PictureLayerImpl*>& active_layers) {
+  for (const auto& layer : active_layers) {
     scoped_ptr<TilingSetRasterQueueRequired> tiling_set_queue(
-        new TilingSetRasterQueueRequired(
-            pair.active->picture_layer_tiling_set(), Type::REQUIRED_FOR_DRAW));
-    if (tiling_set_queue->IsEmpty())
-      continue;
-    tiling_set_queues_.push_back(tiling_set_queue.Pass());
+        new TilingSetRasterQueueRequired(layer->picture_layer_tiling_set(),
+                                         Type::REQUIRED_FOR_DRAW));
+    if (!tiling_set_queue->IsEmpty())
+      tiling_set_queues_.push_back(tiling_set_queue.Pass());
   }
 }
 
 void RasterTilePriorityQueueRequired::BuildRequiredForActivation(
-    const std::vector<PictureLayerImpl::Pair>& paired_layers) {
-  for (const auto& pair : paired_layers) {
-    if (pair.active) {
-      scoped_ptr<TilingSetRasterQueueRequired> tiling_set_queue(
-          new TilingSetRasterQueueRequired(
-              pair.active->picture_layer_tiling_set(),
-              Type::REQUIRED_FOR_ACTIVATION));
-      if (!tiling_set_queue->IsEmpty())
-        tiling_set_queues_.push_back(tiling_set_queue.Pass());
-    }
-    if (pair.pending) {
-      scoped_ptr<TilingSetRasterQueueRequired> tiling_set_queue(
-          new TilingSetRasterQueueRequired(
-              pair.pending->picture_layer_tiling_set(),
-              Type::REQUIRED_FOR_ACTIVATION));
-      if (!tiling_set_queue->IsEmpty())
-        tiling_set_queues_.push_back(tiling_set_queue.Pass());
-    }
-  }
+    const std::vector<PictureLayerImpl*>& active_layers,
+    const std::vector<PictureLayerImpl*>& pending_layers) {
+  AppendTilingSetRequiredQueues(active_layers, &tiling_set_queues_);
+  AppendTilingSetRequiredQueues(pending_layers, &tiling_set_queues_);
 }
 
 bool RasterTilePriorityQueueRequired::IsEmpty() const {
