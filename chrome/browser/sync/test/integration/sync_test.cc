867cee38dec168d403b3a460b07fd9df4e3eac44
shadi <shadi@chromium.org>
2015-05-06 11:37:10 -0700

    Fix observer hanging after shudown because the sign in popup was not dismissed.
    This only happens in real sync server integration tests. The fix adds a call to SyncConfirmationUIClosed() after sync setup completion. The call deletes any observer owned by OneClickSigninSyncStarter object.
    
    BUG=484388
    
    Review URL: https://codereview.chromium.org/1120393002
    
    Cr-Commit-Position: refs/heads/master@{#328566}

--- chrome/browser/sync/test/integration/sync_test.cc
+++ chrome/browser/sync/test/integration/sync_test.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/host_desktop.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/webui/signin/login_ui_service.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service_factory.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
@@ -549,6 +550,16 @@ bool SyncTest::SetupSync() {
   if (server_type_ == EXTERNAL_LIVE_SERVER) {
     for (int i = 0; i < num_clients_; ++i) {
       sync_refreshers_[i] = new P2PSyncRefresher(clients_[i]->service());
+    }
+
+    // OneClickSigninSyncStarter observer is created with a real user sign in.
+    // It is deleted on certain conditions which are not satisfied by our tests,
+    // and this causes the SigninTracker observer to stay hanging at shutdown.
+    // Calling LoginUIService::SyncConfirmationUIClosed forces the observer to
+    // be removed. http://crbug.com/484388
+    for (int i = 0; i < num_clients_; ++i) {
+      LoginUIServiceFactory::GetForProfile(GetProfile(i))->
+          SyncConfirmationUIClosed(false /* configure_sync_first */);
     }
   }
 
