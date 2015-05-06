benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit c9dec75e110516ac1cd6c780acba89f9718ffb50

Consolidate root frame scrolling behavior in RootFrameViewport class.
Introduces RootFrameViewport which is a ScrollableArea that combines the root
frame's FrameView and the PinchViewport. This class encapsulates the unique
behavior of the root frame (the fact that it has two viewports, visual and
layout, due to pinch-zoom semantics). Replaced most callers of FrameView
scrolling to use the ScrollableArea returned by FrameView::scrollableArea
which returns either the SA used by FrameView or RootFrameViewport.

This required a fair number of small moves and changes which I've summarized
below:

ScrollableArea

- Added scrollIntoRect method (moved from FrameView)
- Added virtual scrollIntoView method, implemented in DPLScrollableArea,
  FrameView and RootFrameViewport. ASSERTs if base impl is called as all callers
  are currently implemented. The implementation should probably be in the base
  class but that would require a fairly significant shuffle since ScrollAlignment
  and the expose method are in Source/core
- Added Double version of visibleContentRect to replace PinchViewport's
  locationInDocument and since offsets are doubles
- Added Double version of clampScrollPosition
- Moved a number of methods up from subclasses to allow using ScrollableArea as the
  scrolling interface:
    adjustScrollPositionWithinRect
    invalidateScrollbarRect/Corner
    scrollBehaviorStyle
    isScrollable

FrameView:

- Removed scrollElementToRect and replaced with ScrollableArea::scrollIntoRect
- scroll() method had same signature as ScrollableArea::scroll but was hiding
  it, since ScrollableArea::scroll is now virtual, this method overrides it.
- Moved |adjustScrollPositionWithinRange| to ScrollableArea.
- Made isScrollable a base method in ScrollableArea with FrameView as a special
  implementation.
- Implementation of ScrollableArea::scrollIntoView (used for iframes)
- Lazily create and return a RootFrameViewport in FrameView::scrollableArea

LocalDOMWindow:

third_party/WebKit/Source/core/frame/PinchViewport.cpp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/page/Page.h"
 #include "core/page/scrolling/ScrollingCoordinator.h"
 #include "platform/TraceEvent.h"
+#include "platform/geometry/DoubleRect.h"
 #include "platform/geometry/FloatSize.h"
 #include "platform/graphics/GraphicsLayer.h"
 #include "platform/graphics/GraphicsLayerFactory.h"
@@ -173,29 +174,6 @@ FloatPoint PinchViewport::viewportCSSPixelsToRootFrame(const FloatPoint& point) 
     return pointInRootFrame;
 }
 
-void PinchViewport::scrollIntoView(const LayoutRect& rect)
-{
-    if (!mainFrame() || !mainFrame()->view())
-        return;
-
-    FrameView* view = mainFrame()->view();
-
-    // Snap the visible rect to layout units to match the input rect.
-    FloatRect visible = LayoutRect(visibleRect());
-
-    float centeringOffsetX = (visible.width() - rect.width()) / 2;
-    float centeringOffsetY = (visible.height() - rect.height()) / 2;
-
-    DoublePoint targetOffset(
-        rect.x() - centeringOffsetX - visible.x(),
-        rect.y() - centeringOffsetY - visible.y());
-
-    view->setScrollPosition(targetOffset);
-
-    DoublePoint remainder = DoublePoint(targetOffset - view->scrollPositionDouble());
-    move(toFloatPoint(remainder));
-}
-
 void PinchViewport::setLocation(const FloatPoint& newLocation)
 {
     setScaleAndLocation(m_scale, newLocation);
@@ -209,20 +187,6 @@ void PinchViewport::move(const FloatPoint& delta)
 void PinchViewport::move(const FloatSize& delta)
 {
     setLocation(m_offset + delta);
-}
-
-void PinchViewport::setLocationInDocument(const DoublePoint& location)
-{
-    if (!mainFrame() || !mainFrame()->view())
-        return;
-
-    ScrollableArea* layoutViewport = mainFrame()->view()->scrollableArea();
-
-    DoubleSize delta = location - visibleRectInDocument().location();
-    layoutViewport->setScrollPosition(layoutViewport->scrollPositionDouble() + delta);
-
-    delta = location - visibleRectInDocument().location();
-    move(toFloatSize(delta));
 }
 
 void PinchViewport::setScale(float scale)
@@ -464,34 +428,14 @@ void PinchViewport::clearLayersForTreeView(WebLayerTreeView* layerTreeView) cons
     layerTreeView->clearViewportLayers();
 }
 
-ScrollResult PinchViewport::wheelEvent(const PlatformWheelEvent& event)
+DoubleRect PinchViewport::visibleContentRectDouble(IncludeScrollbarsInRect) const
 {
-    FrameView* view = mainFrame()->view();
-    ScrollResult viewScrollResult = view->wheelEvent(event);
+    return visibleRect();
+}
 
-    // The virtual viewport will only accept pixel scrolls.
-    if (!event.canScroll() || event.granularity() == ScrollByPageWheelEvent)
-        return viewScrollResult;
-
-    // Move the location by the negative of the remaining scroll delta.
-    FloatPoint oldOffset = m_offset;
-    FloatPoint locationDelta;
-    if (viewScrollResult.didScroll) {
-        locationDelta = -FloatPoint(viewScrollResult.unusedScrollDeltaX, viewScrollResult.unusedScrollDeltaY);
-    } else {
-        if (event.railsMode() != PlatformEvent::RailsModeVertical)
-            locationDelta.setX(-event.deltaX());
-        if (event.railsMode() != PlatformEvent::RailsModeHorizontal)
-            locationDelta.setY(-event.deltaY());
-    }
-    move(locationDelta);
-
-    FloatPoint usedLocationDelta(m_offset - oldOffset);
-    if (!viewScrollResult.didScroll && usedLocationDelta == FloatPoint::zero())
-        return ScrollResult(false);
-
-    FloatPoint unusedLocationDelta(locationDelta - usedLocationDelta);
-    return ScrollResult(true, -unusedLocationDelta.x(), -unusedLocationDelta.y());
+IntRect PinchViewport::visibleContentRect(IncludeScrollbarsInRect scrollbarInclusion) const
+{
+    return enclosingIntRect(visibleContentRectDouble(scrollbarInclusion));
 }
 
 bool PinchViewport::shouldUseIntegerScrollOffset() const
@@ -499,7 +443,8 @@ bool PinchViewport::shouldUseIntegerScrollOffset() const
     LocalFrame* frame = mainFrame();
     if (frame && frame->settings() && !frame->settings()->preferCompositingToLCDTextEnabled())
         return true;
-    return false;
+
+    return ScrollableArea::shouldUseIntegerScrollOffset();
 }
 
 int PinchViewport::scrollSize(ScrollbarOrientation orientation) const

third_party/WebKit/Source/core/frame/PinchViewport.h
@@ -94,12 +94,6 @@ public:
     void move(const FloatSize&);
     FloatPoint location() const { return m_offset; }
 
-    // Sets the location of the pinch viewport relative to the document. This
-    // will attempt to scroll the root FrameView such that the pinch viewport
-    // is at the given location. It will then scroll the pinch viewport if
-    // scrolling only the root FrameView couldn't reach the location.
-    void setLocationInDocument(const DoublePoint&);
-
     // Sets the size of the inner viewport when unscaled in CSS pixels.
     void setSize(const IntSize&);
     IntSize size() const { return m_size; }
@@ -127,8 +121,6 @@ public:
     void registerLayersWithTreeView(WebLayerTreeView*) const;
     void clearLayersForTreeView(WebLayerTreeView*) const;
 
-    ScrollResult wheelEvent(const PlatformWheelEvent&);
-
     // The portion of the unzoomed frame visible in the inner "pinch" viewport,
     // in partial CSS pixels. Relative to the main frame.
     FloatRect visibleRect() const;
@@ -142,11 +134,6 @@ public:
     // scale isn't applied.
     FloatRect mainViewToViewportCSSPixels(const FloatRect&) const;
     FloatPoint viewportCSSPixelsToRootFrame(const FloatPoint&) const;
-
-    // Scroll the main frame and pinch viewport so that the given rect in the
-    // top-level document is centered in the viewport. This method will avoid
-    // scrolling the pinch viewport unless necessary.
-    void scrollIntoView(const LayoutRect&);
 
     // Clamp the given point, in document coordinates, to the maximum/minimum
     // scroll extents of the viewport within the document.
@@ -176,6 +163,8 @@ public:
     IntPoint rootFrameToViewport(const IntPoint&) const;
 
     // ScrollableArea implementation
+    virtual DoubleRect visibleContentRectDouble(IncludeScrollbarsInRect = ExcludeScrollbars) const override;
+    virtual IntRect visibleContentRect(IncludeScrollbarsInRect = ExcludeScrollbars) const override;
     virtual bool shouldUseIntegerScrollOffset() const override;
     virtual bool isActive() const override { return false; }
     virtual int scrollSize(ScrollbarOrientation) const override;

third_party/WebKit/Source/core/frame/RootFrameViewport.cpp
@@ -1,0 +1,309 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "core/frame/RootFrameViewport.h"
+
+#include "core/frame/FrameView.h"
+#include "core/layout/ScrollAlignment.h"
+#include "platform/geometry/DoubleRect.h"
+#include "platform/geometry/FloatRect.h"
+#include "platform/geometry/LayoutRect.h"
+
+namespace blink {
+
+RootFrameViewport::RootFrameViewport(ScrollableArea& visualViewport, ScrollableArea& layoutViewport)
+    : m_visualViewport(visualViewport)
+    , m_layoutViewport(layoutViewport)
+{
+}
+
+DEFINE_TRACE(RootFrameViewport)
+{
+    visitor->trace(m_visualViewport);
+    visitor->trace(m_layoutViewport);
+}
+
+void RootFrameViewport::updateScrollAnimator()
+{
+    scrollAnimator()->setCurrentPosition(toFloatPoint(scrollOffsetFromScrollAnimators()));
+}
+
+DoublePoint RootFrameViewport::scrollOffsetFromScrollAnimators() const
+{
+    return visualViewport().scrollAnimator()->currentPosition() + layoutViewport().scrollAnimator()->currentPosition();
+}
+
+DoubleRect RootFrameViewport::visibleContentRectDouble(IncludeScrollbarsInRect scrollbarInclusion) const
+{
+    return DoubleRect(scrollPositionDouble(), visualViewport().visibleContentRectDouble(scrollbarInclusion).size());
+}
+
+IntRect RootFrameViewport::visibleContentRect(IncludeScrollbarsInRect scrollbarInclusion) const
+{
+    return enclosingIntRect(visibleContentRectDouble(scrollbarInclusion));
+}
+
+bool RootFrameViewport::shouldUseIntegerScrollOffset() const
+{
+    // Fractionals are floored in the ScrollAnimator but it's important that the ScrollAnimators of the
+    // visual and layout viewports get the precise fractional number so never use integer scrolling for
+    // RootFrameViewport, we'll let the truncation happen in the subviewports.
+    return false;
+}
+
+bool RootFrameViewport::isActive() const
+{
+    return layoutViewport().isActive();
+}
+
+int RootFrameViewport::scrollSize(ScrollbarOrientation orientation) const
+{
+    IntSize scrollDimensions = maximumScrollPosition() - minimumScrollPosition();
+    return (orientation == HorizontalScrollbar) ? scrollDimensions.width() : scrollDimensions.height();
+}
+
+bool RootFrameViewport::isScrollCornerVisible() const
+{
+    return layoutViewport().isScrollCornerVisible();
+}
+
+IntRect RootFrameViewport::scrollCornerRect() const
+{
+    return layoutViewport().scrollCornerRect();
+}
+
+void RootFrameViewport::setScrollPosition(const DoublePoint& position, ScrollBehavior scrollBehavior)
+{
+    updateScrollAnimator();
+
+    // TODO(bokan): Support smooth scrolling the visual viewport.
+    if (scrollBehavior == ScrollBehaviorAuto)
+        scrollBehavior = layoutViewport().scrollBehaviorStyle();
+    if (scrollBehavior == ScrollBehaviorSmooth) {
+        layoutViewport().setScrollPosition(position, scrollBehavior);
+        return;
+    }
+
+    if (!layoutViewport().isProgrammaticallyScrollable())
+        return;
+
+    DoublePoint clampedPosition = clampScrollPosition(position);
+    scrollToOffsetWithoutAnimation(toFloatPoint(clampedPosition));
+}
+
+ScrollResult RootFrameViewport::handleWheel(const PlatformWheelEvent& event)
+{
+    updateScrollAnimator();
+
+    ScrollResult viewScrollResult(false);
+    if (layoutViewport().isScrollable())
+        viewScrollResult = layoutViewport().handleWheel(event);
+
+    // The visual viewport will only accept pixel scrolls.
+    if (!event.canScroll() || event.granularity() == ScrollByPageWheelEvent)
+        return viewScrollResult;
+
+    // Move the location by the negative of the remaining scroll delta.
+    DoublePoint oldOffset = visualViewport().scrollPositionDouble();
+    DoublePoint locationDelta;
+    if (viewScrollResult.didScroll) {
+        locationDelta = -DoublePoint(viewScrollResult.unusedScrollDeltaX, viewScrollResult.unusedScrollDeltaY);
+    } else {
+        if (event.railsMode() != PlatformEvent::RailsModeVertical)
+            locationDelta.setX(-event.deltaX());
+        if (event.railsMode() != PlatformEvent::RailsModeHorizontal)
+            locationDelta.setY(-event.deltaY());
+    }
+
+    DoublePoint targetPosition = visualViewport().adjustScrollPositionWithinRange(
+        visualViewport().scrollPositionDouble() + toDoubleSize(locationDelta));
+    visualViewport().scrollToOffsetWithoutAnimation(FloatPoint(targetPosition));
+
+    DoublePoint usedLocationDelta(visualViewport().scrollPositionDouble() - oldOffset);
+    if (!viewScrollResult.didScroll && usedLocationDelta == DoublePoint::zero())
+        return ScrollResult(false);
+
+    DoubleSize unusedLocationDelta(locationDelta - usedLocationDelta);
+    return ScrollResult(true, -unusedLocationDelta.width(), -unusedLocationDelta.height());
+}
+
+LayoutRect RootFrameViewport::scrollIntoView(const LayoutRect& rectInContent, const ScrollAlignment& alignX, const ScrollAlignment& alignY)
+{
+    // We want to move the rect into the viewport that excludes the scrollbars so we intersect
+    // the pinch viewport with the scrollbar-excluded frameView content rect. However, we don't
+    // use visibleContentRect directly since it floors the scroll position. Instead, we use
+    // FrameView::scrollPositionDouble and construct a LayoutRect from that (the FrameView size
+    // is always integer sized.
+
+    LayoutRect frameRectInContent = LayoutRect(
+        layoutViewport().scrollPositionDouble(),
+        layoutViewport().visibleContentRect().size());
+    LayoutRect pinchRectInContent = LayoutRect(
+        layoutViewport().scrollPositionDouble() + toDoubleSize(visualViewport().scrollPositionDouble()),
+        visualViewport().visibleContentRect().size());
+
+    LayoutRect viewRectInContent = intersection(pinchRectInContent, frameRectInContent);
+    LayoutRect targetViewport =
+        ScrollAlignment::getRectToExpose(viewRectInContent, rectInContent, alignX, alignY);
+
+    // pinchViewport.scrollIntoView will attempt to center the given rect within the viewport
+    // so to prevent it from adjusting r's coordinates the rect must match the viewport's size
+    // i.e. add the subtracted scrollbars from above back in.
+    // FIXME: This is hacky and required because getRectToExpose doesn't naturally account
+    // for the two viewports. crbug.com/449340.
+    targetViewport.setSize(LayoutSize(visualViewport().visibleContentRect().size()));
+
+    // Snap the visible rect to layout units to match the calculated target viewport rect.
+    FloatRect visible =
+        LayoutRect(visualViewport().scrollPositionDouble(), visualViewport().visibleContentRect().size());
+
+    float centeringOffsetX = (visible.width() - targetViewport.width()) / 2;
+    float centeringOffsetY = (visible.height() - targetViewport.height()) / 2;
+
+    DoublePoint targetOffset(
+        targetViewport.x() - centeringOffsetX,
+        targetViewport.y() - centeringOffsetY);
+
+    setScrollPosition(targetOffset);
+
+    // RootFrameViewport only changes the viewport relative to the document so we can't change the input
+    // rect's location relative to the document origin.
+    return rectInContent;
+}
+
+void RootFrameViewport::setScrollOffset(const IntPoint& offset)
+{
+    setScrollOffset(DoublePoint(offset));
+}
+
+void RootFrameViewport::setScrollOffset(const DoublePoint& offset)
+{
+    // Make sure we use the scroll positions as reported by each viewport's ScrollAnimator, since its
+    // ScrollableArea's position may have the fractional part truncated off.
+    DoublePoint oldPosition = scrollOffsetFromScrollAnimators();
+
+    DoubleSize delta = offset - oldPosition;
+
+    if (delta.isZero())
+        return;
+
+    DoublePoint targetPosition = layoutViewport().adjustScrollPositionWithinRange(layoutViewport().scrollAnimator()->currentPosition() + delta);
+    layoutViewport().scrollToOffsetWithoutAnimation(toFloatPoint(targetPosition));
+
+    DoubleSize applied = scrollOffsetFromScrollAnimators() - oldPosition;
+    delta -= applied;
+
+    if (delta.isZero())
+        return;
+
+    targetPosition = visualViewport().adjustScrollPositionWithinRange(visualViewport().scrollAnimator()->currentPosition() + delta);
+    visualViewport().scrollToOffsetWithoutAnimation(toFloatPoint(targetPosition));
+}
+
+IntPoint RootFrameViewport::scrollPosition() const
+{
+    return flooredIntPoint(scrollPositionDouble());
+}
+
+DoublePoint RootFrameViewport::scrollPositionDouble() const
+{
+    return layoutViewport().scrollPositionDouble() + toDoubleSize(visualViewport().scrollPositionDouble());
+}
+
+IntPoint RootFrameViewport::minimumScrollPosition() const
+{
+    return IntPoint(layoutViewport().minimumScrollPosition() - visualViewport().minimumScrollPosition());
+}
+
+IntPoint RootFrameViewport::maximumScrollPosition() const
+{
+    return layoutViewport().maximumScrollPosition() + visualViewport().maximumScrollPosition();
+}
+
+DoublePoint RootFrameViewport::maximumScrollPositionDouble() const
+{
+    return layoutViewport().maximumScrollPositionDouble() + toDoubleSize(visualViewport().maximumScrollPositionDouble());
+}
+
+IntSize RootFrameViewport::contentsSize() const
+{
+    return layoutViewport().contentsSize();
+}
+
+bool RootFrameViewport::scrollbarsCanBeActive() const
+{
+    return layoutViewport().scrollbarsCanBeActive();
+}
+
+IntRect RootFrameViewport::scrollableAreaBoundingBox() const
+{
+    return layoutViewport().scrollableAreaBoundingBox();
+}
+
+bool RootFrameViewport::userInputScrollable(ScrollbarOrientation orientation) const
+{
+    return visualViewport().userInputScrollable(orientation) || layoutViewport().userInputScrollable(orientation);
+}
+
+bool RootFrameViewport::shouldPlaceVerticalScrollbarOnLeft() const
+{
+    return layoutViewport().shouldPlaceVerticalScrollbarOnLeft();
+}
+
+void RootFrameViewport::invalidateScrollbarRect(Scrollbar* scrollbar, const IntRect& rect)
+{
+    layoutViewport().invalidateScrollbarRect(scrollbar, rect);
+}
+
+void RootFrameViewport::invalidateScrollCornerRect(const IntRect& rect)
+{
+    layoutViewport().invalidateScrollCornerRect(rect);
+}
+
+GraphicsLayer* RootFrameViewport::layerForContainer() const
+{
+    return layoutViewport().layerForContainer();
+}
+
+GraphicsLayer* RootFrameViewport::layerForScrolling() const
+{
+    return layoutViewport().layerForScrolling();
+}
+
+GraphicsLayer* RootFrameViewport::layerForHorizontalScrollbar() const
+{
+    return layoutViewport().layerForHorizontalScrollbar();
+}
+
+GraphicsLayer* RootFrameViewport::layerForVerticalScrollbar() const
+{
+    return layoutViewport().layerForVerticalScrollbar();
+}
+
+bool RootFrameViewport::scroll(ScrollDirection direction, ScrollGranularity granularity, float delta)
+{
+    updateScrollAnimator();
+
+    ScrollbarOrientation orientation;
+
+    if (direction == ScrollUp || direction == ScrollDown)
+        orientation = VerticalScrollbar;
+    else
+        orientation = HorizontalScrollbar;
+
+    if (layoutViewport().userInputScrollable(orientation) && visualViewport().userInputScrollable(orientation))
+        return ScrollableArea::scroll(direction, granularity, delta);
+
+    if (visualViewport().userInputScrollable(orientation))
+        return visualViewport().scroll(direction, granularity, delta);
+
+    if (layoutViewport().userInputScrollable(orientation))
+        return layoutViewport().scroll(direction, granularity, delta);
+
+    return false;
+}
+
+} // namespace blink

commit 016db83a194c5c45b1c078911b590218ffa3b0cb

Oilpan: address ScrollableArea-induced build breakage from r194957.
ScrollableArea is not an Oilpan allocated mixin currently, so in order
to support the assumption of RootFrameViewport that all ScrollableAreas
are allocated the same way, pull PinchViewport off the Oilpan heap
(PinchViewport being the only ScrollableArea that was Oilpan controlled.)

This is not ideal, but a shorter term measure to unbreak Oilpan builds.
Making ScrollableArea a GC mixin would be the longer term solution, but
that's not a straightforward task.

TBR=oilpan-reviews
BUG=443724
NOTRY=true

Review URL: https://codereview.chromium.org/1127023002

git-svn-id: svn://svn.chromium.org/blink/trunk@194964 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/frame/FrameHost.cpp
@@ -105,7 +105,6 @@ DEFINE_TRACE(FrameHost)
 {
     visitor->trace(m_page);
     visitor->trace(m_topControls);
-    visitor->trace(m_pinchViewport);
     visitor->trace(m_eventHandlerRegistry);
     visitor->trace(m_consoleMessageStorage);
 }

third_party/WebKit/Source/core/frame/FrameHost.h
@@ -103,7 +103,7 @@ private:
 
     RawPtrWillBeMember<Page> m_page;
     const OwnPtrWillBeMember<TopControls> m_topControls;
-    const OwnPtrWillBeMember<PinchViewport> m_pinchViewport;
+    const OwnPtr<PinchViewport> m_pinchViewport;
     const OwnPtrWillBeMember<EventHandlerRegistry> m_eventHandlerRegistry;
     const OwnPtrWillBeMember<ConsoleMessageStorage> m_consoleMessageStorage;
 

third_party/WebKit/Source/core/frame/PinchViewport.cpp
@@ -81,11 +81,6 @@ PinchViewport::~PinchViewport()
 {
 }
 
-DEFINE_TRACE(PinchViewport)
-{
-    visitor->trace(m_frameHost);
-}
-
 void PinchViewport::setSize(const IntSize& size)
 {
     // When the main frame is remote, we won't have an associated frame.

third_party/WebKit/Source/core/frame/PinchViewport.h
@@ -63,14 +63,13 @@ class LocalFrame;
 // offset is set through the GraphicsLayer <-> CC sync mechanisms. Its contents is the page's
 // main FrameView, which corresponds to the outer viewport. The inner viewport is always contained
 // in the outer viewport and can pan within it.
-class CORE_EXPORT PinchViewport final : public NoBaseWillBeGarbageCollectedFinalized<PinchViewport>, public GraphicsLayerClient, public ScrollableArea {
+class CORE_EXPORT PinchViewport final : public GraphicsLayerClient, public ScrollableArea {
 public:
-    static PassOwnPtrWillBeRawPtr<PinchViewport> create(FrameHost& host)
+    static PassOwnPtr<PinchViewport> create(FrameHost& host)
     {
-        return adoptPtrWillBeNoop(new PinchViewport(host));
+        return adoptPtr(new PinchViewport(host));
     }
     virtual ~PinchViewport();
-    DECLARE_VIRTUAL_TRACE();
 
     void attachToLayerTree(GraphicsLayer*, GraphicsLayerFactory*);
     GraphicsLayer* rootGraphicsLayer()
@@ -205,11 +204,13 @@ private:
 
     FrameHost& frameHost() const
     {
-        ASSERT(m_frameHost);
-        return *m_frameHost;
+        return m_frameHost;
     }
 
-    RawPtrWillBeMember<FrameHost> m_frameHost;
+    // TODO(Oilpan): this back reference is safe, but not ideal.
+    // Turning it into a traced Member<> would require moving
+    // ScrollableArea to the heap.
+    FrameHost& m_frameHost;
     OwnPtr<GraphicsLayer> m_rootTransformLayer;
     OwnPtr<GraphicsLayer> m_innerViewportContainerLayer;
     OwnPtr<GraphicsLayer> m_overscrollElasticityLayer;

third_party/WebKit/Source/core/frame/RootFrameViewport.cpp
@@ -20,12 +20,6 @@ RootFrameViewport::RootFrameViewport(ScrollableArea& visualViewport, ScrollableA
 {
 }
 
-DEFINE_TRACE(RootFrameViewport)
-{
-    visitor->trace(m_visualViewport);
-    visitor->trace(m_layoutViewport);
-}
-
 void RootFrameViewport::updateScrollAnimator()
 {
     scrollAnimator()->setCurrentPosition(toFloatPoint(scrollOffsetFromScrollAnimators()));

third_party/WebKit/Source/core/frame/RootFrameViewport.h
@@ -21,12 +21,11 @@ class LayoutRect;
 // APIs that don't make sense on the combined viewport, the call is delegated to
 // the layout viewport. Thus, we could say this class is a decorator on the
 // FrameView scrollable area that adds pinch-zoom semantics to scrolling.
-class RootFrameViewport final : public NoBaseWillBeGarbageCollectedFinalized<RootFrameViewport>, public ScrollableArea {
+class RootFrameViewport final : public ScrollableArea {
 public:
-    DECLARE_VIRTUAL_TRACE();
-    static PassOwnPtrWillBeRawPtr<RootFrameViewport> create(ScrollableArea& visualViewport, ScrollableArea& layoutViewport)
+    static PassOwnPtr<RootFrameViewport> create(ScrollableArea& visualViewport, ScrollableArea& layoutViewport)
     {
-        return adoptPtrWillBeNoop(new RootFrameViewport(visualViewport, layoutViewport));
+        return adoptPtr(new RootFrameViewport(visualViewport, layoutViewport));
     }
 
     // ScrollableArea Implementation
@@ -66,7 +65,6 @@ public:
     // call to this class' scroll method.
     ScrollResult handleWheel(const PlatformWheelEvent&) override;
 
-
 private:
     RootFrameViewport(ScrollableArea& visualViewport, ScrollableArea& layoutViewport);
 
@@ -77,11 +75,11 @@ private:
     // animator so use this method to pull updated values when necessary.
     void updateScrollAnimator();
 
-    ScrollableArea& visualViewport() const { return *m_visualViewport; }
-    ScrollableArea& layoutViewport() const { return *m_layoutViewport; }
+    ScrollableArea& visualViewport() const { return m_visualViewport; }
+    ScrollableArea& layoutViewport() const { return m_layoutViewport; }
 
-    RawPtrWillBeMember<ScrollableArea> m_visualViewport;
-    RawPtrWillBeMember<ScrollableArea> m_layoutViewport;
+    ScrollableArea& m_visualViewport;
+    ScrollableArea& m_layoutViewport;
 };
 
 } // namespace blink