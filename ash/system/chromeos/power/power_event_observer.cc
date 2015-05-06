337edc540e5f860bceb6acf23edfda5368d29850
dnicoara <dnicoara@chromium.org>
2015-05-06 14:06:51 -0700

    Wait for display configuration to finish before suspending
    If the suspend doesn't wait, the display configuration query will race with the
    suspend call. It may be possible to suspend before the configuration query
    finishes. That would mean that we'd be reading display configuration at
    resume.
    
    BUG=477433
    
    Review URL: https://codereview.chromium.org/1130823003
    
    Cr-Commit-Position: refs/heads/master@{#328612}

--- ash/system/chromeos/power/power_event_observer.cc
+++ ash/system/chromeos/power/power_event_observer.cc
@@ -38,6 +38,11 @@ void ResumeRenderingRequests() {
     window->GetHost()->compositor()->SetVisible(true);
 }
 
+void OnSuspendDisplaysCompleted(const base::Closure& suspend_callback,
+                                bool status) {
+  suspend_callback.Run();
+}
+
 }  // namespace
 
 PowerEventObserver::PowerEventObserver()
@@ -114,7 +119,10 @@ void PowerEventObserver::SuspendImminent() {
   }
 
   ui::UserActivityDetector::Get()->OnDisplayPowerChanging();
-  shell->display_configurator()->SuspendDisplays();
+  shell->display_configurator()->SuspendDisplays(base::Bind(
+      &OnSuspendDisplaysCompleted, chromeos::DBusThreadManager::Get()
+                                       ->GetPowerManagerClient()
+                                       ->GetSuspendReadinessCallback()));
 }
 
 void PowerEventObserver::SuspendDone(const base::TimeDelta& sleep_duration) {
