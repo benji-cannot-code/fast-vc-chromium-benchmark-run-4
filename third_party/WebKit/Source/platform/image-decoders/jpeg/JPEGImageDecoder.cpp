bfcecc10d1c50357c61be6380fd8e1830da885f6
noel@chromium.org <noel@chromium.org>
2015-05-06 03:19:43 +0000

    [JPEGImageDecoder] Remove Torch Mobile Copyright notice
    Added in https://bugs.webkit.org/show_bug.cgi?id=28051 to support WINCE.
    The code was removed at the Blink / WebKit fork but the Copyright notice
    was not changed. Remove the notice: the associated code is gone.
    
    TBR=urvang@chromium.org
    TEST=None (comment only change)
    NOTRY=true
    BUG=None
    
    Review URL: https://codereview.chromium.org/1122403003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194959 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/image-decoders/jpeg/JPEGImageDecoder.cpp
+++ third_party/WebKit/Source/platform/image-decoders/jpeg/JPEGImageDecoder.cpp
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Other contributors:
  *   Stuart Parmenter <stuart@mozilla.com>
  *
- * Copyright (C) 2007-2009 Torch Mobile, Inc.
- *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
