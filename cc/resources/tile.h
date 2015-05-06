bdcedc6a75ffde00ff15bafb69a30a192e2fb095
bruening <bruening@chromium.org>
2015-05-06 14:00:13 -0700

    Revert "cc: Remove Tile refcounting and RefCountedManaged."
    This reverts commit 6d440ed4fc458fae27397b868acb4ab2866119b1.
    
    Reason for revert:
    Breaks the build_for_tool=drmemory Windows build:
    http://build.chromium.org/p/chromium.memory.fyi/builders/Chromium%20Windows%20Builder%20%28DrMemory%29/builds/12393
    
    TBR=vmpstr@chromium.org
    NOTRY=true
    BUG=none
    
    Review URL: https://codereview.chromium.org/1127253002
    
    Cr-Commit-Position: refs/heads/master@{#328607}

--- cc/resources/tile.h
+++ cc/resources/tile.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CC_RESOURCES_TILE_H_
 
 #include "base/memory/ref_counted.h"
+#include "cc/base/ref_counted_managed.h"
 #include "cc/resources/raster_source.h"
 #include "cc/resources/tile_draw_info.h"
 #include "cc/resources/tile_priority.h"
@@ -17,13 +18,8 @@ namespace cc {
 
 class TileManager;
 
-class CC_EXPORT Tile {
+class CC_EXPORT Tile : public RefCountedManaged<Tile> {
  public:
-  class Deleter {
-   public:
-    void operator()(Tile* tile) const;
-  };
-
   enum TileRasterFlags { USE_PICTURE_ANALYSIS = 1 << 0 };
 
   typedef uint64 Id;
@@ -101,7 +97,9 @@ class CC_EXPORT Tile {
 
  private:
   friend class TileManager;
+  friend class PrioritizedTileSet;
   friend class FakeTileManager;
+  friend class BinComparator;
   friend class FakePictureLayerImpl;
 
   // Methods called by by tile manager.
@@ -117,7 +115,6 @@ class CC_EXPORT Tile {
 
   bool HasRasterTask() const { return !!raster_task_.get(); }
 
-  TileManager* tile_manager_;
   scoped_refptr<RasterSource> raster_source_;
   gfx::Size desired_texture_size_;
   gfx::Rect content_rect_;
@@ -144,8 +141,6 @@ class CC_EXPORT Tile {
 
   DISALLOW_COPY_AND_ASSIGN(Tile);
 };
-
-using ScopedTilePtr = scoped_ptr<Tile, Tile::Deleter>;
 
 }  // namespace cc
 
--- cc/resources/tile_manager.h
+++ cc/resources/tile_manager.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/hash_tables.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/values.h"
+#include "cc/base/ref_counted_managed.h"
 #include "cc/base/unique_notifier.h"
 #include "cc/resources/eviction_tile_priority_queue.h"
 #include "cc/resources/memory_history.h"
@@ -86,7 +87,8 @@ RasterTaskCompletionStatsAsValue(const RasterTaskCompletionStats& stats);
 // should no longer have any memory assigned to them. Tile objects are "owned"
 // by layers; they automatically register with the manager when they are
 // created, and unregister from the manager when they are deleted.
-class CC_EXPORT TileManager : public TileTaskRunnerClient {
+class CC_EXPORT TileManager : public TileTaskRunnerClient,
+                              public RefCountedManager<Tile> {
  public:
   enum NamedTaskSet {
     REQUIRED_FOR_ACTIVATION,
@@ -117,13 +119,13 @@ class CC_EXPORT TileManager : public TileTaskRunnerClient {
 
   void UpdateVisibleTiles(const GlobalStateThatImpactsTilePriority& state);
 
-  ScopedTilePtr CreateTile(RasterSource* raster_source,
-                           const gfx::Size& desired_texture_size,
-                           const gfx::Rect& content_rect,
-                           float contents_scale,
-                           int layer_id,
-                           int source_frame_number,
-                           int flags);
+  scoped_refptr<Tile> CreateTile(RasterSource* raster_source,
+                                 const gfx::Size& desired_texture_size,
+                                 const gfx::Rect& content_rect,
+                                 float contents_scale,
+                                 int layer_id,
+                                 int source_frame_number,
+                                 int flags);
 
   bool IsReadyToActivate() const;
   bool IsReadyToDraw() const;
@@ -191,9 +193,9 @@ class CC_EXPORT TileManager : public TileTaskRunnerClient {
   void FreeResourcesForReleasedTiles();
   void CleanUpReleasedTiles();
 
+  // Overriden from RefCountedManager<Tile>:
   friend class Tile;
-  // Virtual for testing.
-  virtual void Release(Tile* tile);
+  void Release(Tile* tile) override;
 
   // Overriden from TileTaskRunnerClient:
   void DidFinishRunningTileTasks(TaskSet task_set) override;
