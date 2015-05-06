5b382605b93deb581680da8ec2f6fb7a7edcb86c
dalecurtis <dalecurtis@chromium.org>
2015-05-05 23:32:18 -0700

    Add tracking metrics for the new video rendering pipeline.
    Adds a metric for tracking the the number of cadence
    switches, from 0 to a max of 10.
    
    BUG=439548
    TEST=playback, verify histograms are present.
    
    Review URL: https://codereview.chromium.org/1121313002
    
    Cr-Commit-Position: refs/heads/master@{#328493}

--- media/filters/video_cadence_estimator.cc
+++ media/filters/video_cadence_estimator.cc
@@ -8,11 +8,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <limits>
 
+#include "base/metrics/histogram_macros.h"
+
 namespace media {
 
 // To prevent oscillation in and out of cadence or between cadence values, we
 // require some time to elapse before a cadence switch is accepted.
 const int kMinimumCadenceDurationMs = 100;
+
+// Records the number of cadence changes to UMA.
+static void HistogramCadenceChangeCount(int cadence_changes) {
+  const int kCadenceChangeMax = 10;
+  UMA_HISTOGRAM_CUSTOM_COUNTS("Media.VideoRenderer.CadenceChanges",
+                              cadence_changes, 0, kCadenceChangeMax,
+                              kCadenceChangeMax);
+}
 
 VideoCadenceEstimator::VideoCadenceEstimator(
     base::TimeDelta minimum_time_until_glitch)
@@ -28,7 +38,8 @@ VideoCadenceEstimator::~VideoCadenceEstimator() {
 void VideoCadenceEstimator::Reset() {
   cadence_ = fractional_cadence_ = 0;
   pending_cadence_ = pending_fractional_cadence_ = 0;
-  render_intervals_cadence_held_ = 0;
+  cadence_changes_ = render_intervals_cadence_held_ = 0;
+  first_update_call_ = true;
 }
 
 bool VideoCadenceEstimator::UpdateCadenceEstimate(
@@ -54,6 +65,15 @@ bool VideoCadenceEstimator::UpdateCadenceEstimate(
     DCHECK(new_fractional_cadence);
   }
 
+  // If this is the first time UpdateCadenceEstimate() has been called,
+  // initialize the histogram with a zero count for cadence changes; this
+  // allows us to track the number of playbacks which have cadence at all.
+  if (first_update_call_) {
+    DCHECK_EQ(cadence_changes_, 0);
+    first_update_call_ = false;
+    HistogramCadenceChangeCount(0);
+  }
+
   // Nothing changed, so do nothing.
   if (new_cadence == cadence_ &&
       new_fractional_cadence == fractional_cadence_) {
@@ -77,6 +97,10 @@ bool VideoCadenceEstimator::UpdateCadenceEstimate(
 
       cadence_ = new_cadence;
       fractional_cadence_ = new_fractional_cadence;
+
+      // Note: Because this class is transitively owned by a garbage collected
+      // object, WebMediaPlayer, we log cadence changes as they are encountered.
+      HistogramCadenceChangeCount(++cadence_changes_);
       return true;
     }
 
