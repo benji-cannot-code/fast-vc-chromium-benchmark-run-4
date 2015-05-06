40d49ea086a5e400b100c5c627fffd822fdd660c
dalecurtis <dalecurtis@chromium.org>
2015-05-06 16:13:53 -0700

    Avoid repeated unnecessary task posting where possible.
    Adds a delay before MaybeStopSinkAfterFirstPaint() is called to
    allow autoplaying videos time to actually play. Avoids reposting
    that task if multiple Render() calls come in succession.
    
    Avoids calling AttemptRead() if we know it's going to fail. In
    total this seems to reduce CPU% by 10% (120% -> 110%) and
    energy_consumption_mwh is reduced by 1.6mwh for a 24fps 4K video.
    
    Also cleans up ended handling, it was previously possible for
    ended to fire during |render_first_frame_and_stop_| if time
    moved forward too much. The delayed stop now exposes this in
    the unit tests.
    
    BUG=439548
    TEST=tests pass
    
    Review URL: https://codereview.chromium.org/1129603003
    
    Cr-Commit-Position: refs/heads/master@{#328649}

--- media/renderers/video_renderer_impl_unittest.cc
+++ media/renderers/video_renderer_impl_unittest.cc
@@ -55,10 +55,8 @@ class VideoRendererImplTest : public testing::TestWithParam<bool> {
     ScopedVector<VideoDecoder> decoders;
     decoders.push_back(decoder_);
 
-    // Since the Underflow test needs a render interval shorter than the frame
-    // duration, use 120Hz (which makes each interval is < 10ms; ~9.9ms).
     null_video_sink_.reset(new NullVideoSink(
-        false, base::TimeDelta::FromSecondsD(1.0 / 120),
+        false, base::TimeDelta::FromSecondsD(1.0 / 60),
         base::Bind(&MockCB::FrameReceived, base::Unretained(&mock_cb_)),
         message_loop_.task_runner()));
 
@@ -526,7 +524,7 @@ TEST_P(VideoRendererImplTest, VideoDecoder_InitFailure) {
 
 TEST_P(VideoRendererImplTest, Underflow) {
   Initialize();
-  QueueFrames("0 10 20 30");
+  QueueFrames("0 30 60 90");
 
   {
     WaitableMessageLoopEvent event;
@@ -545,13 +543,15 @@ TEST_P(VideoRendererImplTest, Underflow) {
   {
     SCOPED_TRACE("Waiting for frame drops");
     WaitableMessageLoopEvent event;
-    EXPECT_CALL(mock_cb_, FrameReceived(HasTimestamp(10)))
-        .Times(0);
-    EXPECT_CALL(mock_cb_, FrameReceived(HasTimestamp(20)))
-        .Times(0);
     EXPECT_CALL(mock_cb_, FrameReceived(HasTimestamp(30)))
+        .Times(0);
+    EXPECT_CALL(mock_cb_, FrameReceived(HasTimestamp(60)))
+        .Times(0);
+    EXPECT_CALL(mock_cb_, FrameReceived(HasTimestamp(90)))
         .WillOnce(RunClosure(event.GetClosure()));
-    AdvanceTimeInMs(31);
+    AdvanceWallclockTimeInMs(91);
+    AdvanceTimeInMs(91);
+
     event.RunAndWait();
     Mock::VerifyAndClearExpectations(&mock_cb_);
   }
@@ -563,7 +563,7 @@ TEST_P(VideoRendererImplTest, Underflow) {
     WaitableMessageLoopEvent event;
     EXPECT_CALL(mock_cb_, BufferingStateChange(BUFFERING_HAVE_NOTHING))
         .WillOnce(RunClosure(event.GetClosure()));
-    AdvanceWallclockTimeInMs(9);
+    AdvanceWallclockTimeInMs(29);
     event.RunAndWait();
     Mock::VerifyAndClearExpectations(&mock_cb_);
   }
@@ -636,11 +636,12 @@ TEST_P(VideoRendererImplTest, RenderingStopsAfterOneFrameWithEOS) {
 
     EXPECT_TRUE(IsReadPending());
     SatisfyPendingReadWithEndOfStream();
+    WaitForEnded();
 
+    renderer_->OnTimeStateChanged(false);
     event.RunAndWait();
   }
 
-  WaitForEnded();
   Destroy();
 }
 
@@ -652,7 +653,7 @@ TEST_P(VideoRendererImplTest, RenderingStartedThenStopped) {
     return;
 
   Initialize();
-  QueueFrames("0 10 20 30");
+  QueueFrames("0 30 60 90");
 
   // Start the sink and wait for the first callback.  Set statistics to a non
   // zero value, once we have some decoded frames they should be overwritten.
@@ -674,8 +675,9 @@ TEST_P(VideoRendererImplTest, RenderingStartedThenStopped) {
   // because this is a background render, we won't underflow by waiting until
   // a pending read is ready.
   null_video_sink_->set_background_render(true);
-  AdvanceTimeInMs(41);
-  EXPECT_CALL(mock_cb_, FrameReceived(HasTimestamp(30)));
+  AdvanceWallclockTimeInMs(91);
+  AdvanceTimeInMs(91);
+  EXPECT_CALL(mock_cb_, FrameReceived(HasTimestamp(90)));
   WaitForPendingRead();
   SatisfyPendingReadWithEndOfStream();
 
