40a56c1bb04582158c83abbd2d1e54bbd5d3052d
bratell <bratell@opera.com>
2015-05-06 02:30:35 -0700

    Adding missing profile.h include to extensions code.
    external_pref_loader.cc uses profile.h so it needs to include it and
    not rely on some other random code to include it. Fixes compilation
    problems in certain configurations.
    
    Review URL: https://codereview.chromium.org/1126543003
    
    Cr-Commit-Position: refs/heads/master@{#328511}

--- chrome/browser/extensions/external_pref_loader.cc
+++ chrome/browser/extensions/external_pref_loader.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/defaults.h"
 #include "chrome/browser/prefs/pref_service_syncable.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/common/chrome_paths.h"
 #include "content/public/browser/browser_thread.h"
