48d267d8dc50d05d9a3407d4adce8c5f8a511b98
raymes <raymes@chromium.org>
2015-05-06 00:07:33 -0700

    Remove the in-process PDF viewer from pdf/
    BUG=303491
    
    Review URL: https://codereview.chromium.org/1125103002
    
    Cr-Commit-Position: refs/heads/master@{#328498}

--- pdf/pdf.cc
+++ pdf/pdf.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/logging.h"
-#include "pdf/instance.h"
 #include "pdf/out_of_process_instance.h"
 #include "ppapi/c/ppp.h"
 #include "ppapi/cpp/private/internal_module.h"
@@ -52,9 +51,7 @@ pp::Instance* PDFModule::CreateInstance(PP_Instance instance) {
     g_sdk_initialized_via_pepper = true;
   }
 
-  if (pp::PDF::IsOutOfProcess(pp::InstanceHandle(instance)))
-    return new OutOfProcessInstance(instance);
-  return new Instance(instance);
+  return new OutOfProcessInstance(instance);
 }
 
 
--- pdf/preview_mode_client.cc
+++ pdf/preview_mode_client.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "pdf/preview_mode_client.h"
 
 #include "base/logging.h"
-#include "pdf/instance.h"
 
 namespace chrome_pdf {
 
