c990110d7a73e9a60f752b1ec7319fd84bfdce73
twellington <twellington@chromium.org>
2015-05-06 13:55:12 -0700

    Only enable autofill credit card save button if name or number is present
    BUG=485033
    
    Review URL: https://codereview.chromium.org/1126253004
    
    Cr-Commit-Position: refs/heads/master@{#328605}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AutofillCreditCardEditor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AutofillCreditCardEditor.java
@@ -47,7 +47,6 @@ public class AutofillCreditCardEditor extends Fragment implements OnItemSelected
 
     private int mInitialExpirationMonthPos;
     private int mInitialExpirationYearPos;
-    private boolean mYearOrMonthSelected;
 
     @Override
     public void onCreate(Bundle savedState) {
@@ -96,7 +95,6 @@ public class AutofillCreditCardEditor extends Fragment implements OnItemSelected
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         if ((parent == mExpirationYear && position != mInitialExpirationYearPos)
                 || (parent == mExpirationMonth && position != mInitialExpirationMonthPos)) {
-            mYearOrMonthSelected = true;
             updateSaveButtonEnabled();
         }
     }
@@ -248,7 +246,7 @@ public class AutofillCreditCardEditor extends Fragment implements OnItemSelected
     }
 
     private void updateSaveButtonEnabled() {
-        boolean enabled = mYearOrMonthSelected || !TextUtils.isEmpty(mNameText.getText())
+        boolean enabled = !TextUtils.isEmpty(mNameText.getText())
                 || !TextUtils.isEmpty(mNumberText.getText());
         Button button = (Button) getView().findViewById(R.id.autofill_credit_card_save);
         button.setEnabled(enabled);
