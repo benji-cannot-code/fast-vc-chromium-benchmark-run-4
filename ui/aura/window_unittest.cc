d18107ad32efa0828bf4592f5c949f32e92f118f
thestig <thestig@chromium.org>
2015-05-06 14:22:59 -0700

    Cleanup: Change {ASSERT,EXPECT}_FALSE(!x) to {ASSERT,EXPECT}_TRUE(x).
    Review URL: https://codereview.chromium.org/1121383003
    
    Cr-Commit-Position: refs/heads/master@{#328617}

--- ui/aura/window_unittest.cc
+++ ui/aura/window_unittest.cc
@@ -1704,7 +1704,7 @@ TEST_F(WindowTest, SetBoundsInternalShouldCheckTargetBounds) {
   scoped_ptr<Window> w1(
       CreateTestWindowWithBounds(gfx::Rect(0, 0, 100, 100), root_window()));
 
-  EXPECT_FALSE(!w1->layer());
+  EXPECT_TRUE(w1->layer());
   w1->layer()->GetAnimator()->set_disable_timer_for_test(true);
   ui::LayerAnimator* animator = w1->layer()->GetAnimator();
 
@@ -1882,8 +1882,8 @@ TEST_F(WindowObserverTest, WindowVisibility) {
   // Hide should make the window invisible and the passed visible
   // parameter is false.
   w2->Hide();
-  EXPECT_FALSE(!GetVisibilityInfo());
-  EXPECT_FALSE(!GetVisibilityInfo());
+  EXPECT_TRUE(GetVisibilityInfo());
+  EXPECT_TRUE(GetVisibilityInfo());
   if (!GetVisibilityInfo())
     return;
   EXPECT_FALSE(GetVisibilityInfo()->window_visible);
@@ -1896,7 +1896,7 @@ TEST_F(WindowObserverTest, WindowVisibility) {
   ResetVisibilityInfo();
   EXPECT_TRUE(!GetVisibilityInfo());
   w2->Show();
-  EXPECT_FALSE(!GetVisibilityInfo());
+  EXPECT_TRUE(GetVisibilityInfo());
   if (!GetVisibilityInfo())
     return;
   EXPECT_FALSE(GetVisibilityInfo()->window_visible);
@@ -1909,7 +1909,7 @@ TEST_F(WindowObserverTest, WindowVisibility) {
   w2->Hide();
   ResetVisibilityInfo();
   w2->Show();
-  EXPECT_FALSE(!GetVisibilityInfo());
+  EXPECT_TRUE(GetVisibilityInfo());
   if (!GetVisibilityInfo())
     return;
   EXPECT_TRUE(GetVisibilityInfo()->window_visible);
@@ -2644,7 +2644,7 @@ TEST_F(WindowTest, DelegateDestroysSelfOnWindowDestroy) {
 
 class HierarchyObserver : public WindowObserver {
  public:
-  HierarchyObserver(Window* target) : target_(target) {
+  explicit HierarchyObserver(Window* target) : target_(target) {
     target_->AddObserver(this);
   }
   ~HierarchyObserver() override { target_->RemoveObserver(this); }
@@ -2836,7 +2836,6 @@ TEST_F(WindowTest, OnWindowHierarchyChange) {
     w1.reset();
     w2.reset();
   }
-
 }
 
 namespace {
@@ -2875,7 +2874,7 @@ class TestLayerAnimationObserver : public ui::LayerAnimationObserver {
   DISALLOW_COPY_AND_ASSIGN(TestLayerAnimationObserver);
 };
 
-}
+}  // namespace
 
 TEST_F(WindowTest, WindowDestroyCompletesAnimations) {
   ui::ScopedAnimationDurationScaleMode test_duration_mode(
