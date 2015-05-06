3e9e5b1abf3eebe218f67694d7805a0bd193d720
esprehn@chromium.org <esprehn@chromium.org>
2015-05-06 17:48:46 +0000

    Use an imperative distribution filter for <details>
    Instead of using summary:first-of-type we introduce a callback filter that
    takes a list of nodes and the index of the current one. This allows
    implementing custom distribution behavior removing the one internal user
    of the pseudo support in <content select>.
    
    BUG=484953
    
    Review URL: https://codereview.chromium.org/1126033002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194987 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLContentElement.h
+++ third_party/WebKit/Source/core/html/HTMLContentElement.h
@@ -35,13 +35,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CoreExport.h"
 #include "core/css/CSSSelectorList.h"
 #include "core/dom/shadow/InsertionPoint.h"
+#include "platform/heap/Handle.h"
 
 namespace blink {
+
+class HTMLContentSelectFilter : public NoBaseWillBeGarbageCollected<HTMLContentSelectFilter> {
+public:
+    virtual ~HTMLContentSelectFilter() { }
+    virtual bool canSelectNode(const WillBeHeapVector<RawPtrWillBeMember<Node>, 32>& siblings, int nth) const = 0;
+
+    DEFINE_INLINE_VIRTUAL_TRACE() { }
+};
 
 class CORE_EXPORT HTMLContentElement final : public InsertionPoint {
     DEFINE_WRAPPERTYPEINFO();
 public:
-    DECLARE_NODE_FACTORY(HTMLContentElement);
+    static PassRefPtrWillBeRawPtr<HTMLContentElement> create(Document&, PassOwnPtrWillBeRawPtr<HTMLContentSelectFilter> = nullptr);
     virtual ~HTMLContentElement();
 
     virtual bool canAffectSelector() const override { return true; }
@@ -52,7 +61,7 @@ public:
     bool isSelectValid() const;
 
 private:
-    explicit HTMLContentElement(Document&);
+    HTMLContentElement(Document&, PassOwnPtrWillBeRawPtr<HTMLContentSelectFilter>);
 
     virtual void parseAttribute(const QualifiedName&, const AtomicString&) override;
 
@@ -65,6 +74,7 @@ private:
     bool m_isValidSelector;
     AtomicString m_select;
     CSSSelectorList m_selectorList;
+    OwnPtrWillBeMember<HTMLContentSelectFilter> m_filter;
 };
 
 inline const CSSSelectorList& HTMLContentElement::selectorList() const
@@ -83,6 +93,8 @@ inline bool HTMLContentElement::isSelectValid() const
 
 inline bool HTMLContentElement::canSelectNode(const WillBeHeapVector<RawPtrWillBeMember<Node>, 32>& siblings, int nth) const
 {
+    if (m_filter)
+        return m_filter->canSelectNode(siblings, nth);
     if (m_select.isNull() || m_select.isEmpty())
         return true;
     if (!isSelectValid())
