187f1c7c0aa08c7ea5d614405206e7a06c5aa338
eroman <eroman@chromium.org>
2015-05-07 14:39:12 -0700

    Address some minor nits in net/der.
    * ifdef name to match filepath
    * Add some newlines
    * Delete a duplicated comment in .cc from header
    * update a variable name in comment block
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1132613002
    
    Cr-Commit-Position: refs/heads/master@{#328840}

--- net/der/parse_values.h
+++ net/der/parse_values.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_DER_PARSE_TYPES_H_
-#define NET_DER_PARSE_TYPES_H_
+#ifndef NET_DER_PARSE_VALUES_H_
+#define NET_DER_PARSE_VALUES_H_
 
 #include "base/compiler_specific.h"
 #include "net/base/net_export.h"
@@ -67,4 +67,4 @@ NET_EXPORT bool ParseGeneralizedTime(const Input& in,
 
 }  // namespace net
 
-#endif  // NET_DER_PARSE_TYPES_H_
+#endif  // NET_DER_PARSE_VALUES_H_
