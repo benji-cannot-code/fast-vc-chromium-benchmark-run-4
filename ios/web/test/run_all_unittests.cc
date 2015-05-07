000441e8dd7b9728f66f8230ce76ec2c8a72fac5
stuartmorgan <stuartmorgan@chromium.org>
2015-05-07 07:57:25 -0700

    Upstream the remainder of ios/web/test/
    Synchronizes ios/web/test/ with the downstream version.
    
    BUG=464810
    
    Review URL: https://codereview.chromium.org/1121783002
    
    Cr-Commit-Position: refs/heads/master@{#328765}

---
+++ ios/web/test/run_all_unittests.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/bind.h"
+#include "base/test/launcher/unit_test_launcher.h"
+#include "ios/web/test/web_test_suite.h"
+
+int main(int argc, char** argv) {
+  web::WebTestSuite test_suite(argc, argv);
+
+  return base::LaunchUnitTests(
+      argc, argv, base::Bind(&web::WebTestSuite::Run,
+                             base::Unretained(&test_suite)));
+}
