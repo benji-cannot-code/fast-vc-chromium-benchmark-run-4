d36043a2d1cb7ce6496925b4d4b4433a3155b45b
haraken@chromium.org <haraken@chromium.org>
2015-05-07 04:36:32 +0000

    Oilpan: Enable lazy sweeping on non-oilpan builds
    Currently lazy sweeping is enabled only on oilpan builds. Since the lazy sweeping has been stable on the oilpan builds for >3 months, I think it's now safe to enable it on non-oilpan builds as well.
    
    BUG=480837
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=194808
    
    Review URL: https://codereview.chromium.org/1055193004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195022 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/HeapTest.cpp
+++ third_party/WebKit/Source/platform/heap/HeapTest.cpp
@@ -1801,8 +1801,6 @@ TEST(HeapTest, SimpleFinalization)
     EXPECT_EQ(1, SimpleFinalizedObject::s_destructorCalls);
 }
 
-// FIXME: Lazy sweeping is disabled in non-oilpan builds.
-#if ENABLE(OILPAN)
 TEST(HeapTest, LazySweepingPages)
 {
     clearOutOldGarbage();
@@ -1841,7 +1839,6 @@ TEST(HeapTest, LazySweepingLargeObjectPages)
     Heap::collectGarbage(ThreadState::NoHeapPointersOnStack, ThreadState::GCWithSweep, Heap::ForcedGC);
     EXPECT_EQ(22, LargeHeapObject::s_destructorCalls);
 }
-#endif
 
 TEST(HeapTest, Finalization)
 {
