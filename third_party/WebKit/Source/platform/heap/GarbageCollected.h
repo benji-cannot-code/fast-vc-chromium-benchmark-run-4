d6dc3fa437f2c8722e68adf3d897d4726b9e3797
haraken@chromium.org <haraken@chromium.org>
2015-05-07 10:28:38 +0000

    Oilpan: Add static_asserts about sizeof(T) and IsGarbageCollectedType(T)
    Add some more static_asserts to strengthen our static verification.
    This CL adds HeapVectorBacking and HeapHashTableBacking to IsGarbageCollectedType(T) since they are garbage collected types.
    
    Also this CL adds isHeapObjectAlive(const WeakMember<T>&), which had been missing.
    
    BUG=437144
    
    Review URL: https://codereview.chromium.org/1114323002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195043 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/GarbageCollected.h
+++ third_party/WebKit/Source/platform/heap/GarbageCollected.h
@@ -13,11 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-#define STATIC_ASSERT_IS_GARBAGE_COLLECTED(T, ErrorMessage) \
-    static_assert(IsGarbageCollectedType<T>::value, ErrorMessage)
-#define STATIC_ASSERT_IS_NOT_GARBAGE_COLLECTED(T, ErrorMessage) \
-    static_assert(!IsGarbageCollectedType<T>::value, ErrorMessage)
-
 template<typename T> class GarbageCollected;
 template<typename T, typename U, typename V, typename W, typename X> class HeapHashMap;
 template<typename T, typename U, typename V> class HeapHashSet;
@@ -27,6 +22,8 @@ template<typename T, size_t inlineCapacity> class HeapVector;
 template<typename T, size_t inlineCapacity> class HeapDeque;
 template<typename T, typename U, typename V> class HeapHashCountedSet;
 template<typename T> class HeapTerminatedArray;
+template<typename T, typename Traits> class HeapVectorBacking;
+template<typename Table> class HeapHashTableBacking;
 template<typename ValueArg, size_t inlineCapacity> class HeapListHashSetAllocator;
 class InlinedGlobalMarkingVisitor;
 template<ThreadAffinity affinity> class ThreadLocalPersistents;
@@ -67,12 +64,14 @@ struct IsGarbageCollectedType {
     using HeapDequeSubclass = WTF::IsSubclassOfTemplateTypenameSize<NonConstType, HeapDeque>;
     using HeapHashCountedSetSubclass = WTF::IsSubclassOfTemplate<NonConstType, HeapHashCountedSet>;
     using HeapTerminatedArraySubclass = WTF::IsSubclassOfTemplate<NonConstType, HeapTerminatedArray>;
+    using HeapVectorBackingSubclass = WTF::IsSubclassOfTemplate<NonConstType, HeapVectorBacking>;
+    using HeapHashTableBackingSubclass = WTF::IsSubclassOfTemplate<NonConstType, HeapHashTableBacking>;
 
     template<typename U, size_t inlineCapacity> static TrueType listHashSetNodeIsHeapAllocated(WTF::ListHashSetNode<U, HeapListHashSetAllocator<U, inlineCapacity>>*);
     static FalseType listHashSetNodeIsHeapAllocated(...);
     static const bool isHeapAllocatedListHashSetNode = sizeof(TrueType) == sizeof(listHashSetNodeIsHeapAllocated(reinterpret_cast<NonConstType*>(0)));
 
-    static_assert(sizeof(T), "type must be complete");
+    static_assert(sizeof(T), "T must be fully defined");
 
     static const bool value =
         GarbageCollectedSubclass::value
@@ -85,6 +84,8 @@ struct IsGarbageCollectedType {
         || HeapDequeSubclass::value
         || HeapHashCountedSetSubclass::value
         || HeapTerminatedArraySubclass::value
+        || HeapVectorBackingSubclass::value
+        || HeapHashTableBackingSubclass::value
         || isHeapAllocatedListHashSetNode;
 };
 
