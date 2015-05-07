c7b84f23b8633fd1ceb250df2474b26788ef8e06
dsinclair@chromium.org <dsinclair@chromium.org>
2015-05-07 16:51:51 +0000

    Update list markers in notify change.
    This CL updates the list item code to place the LayoutListMaker into
    the tree during the NotifyChange phase. This moves the layout tree
    modification outside of the actual layout walk.
    
    BUG=370461
    
    Review URL: https://codereview.chromium.org/1110233003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195060 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/TextAutosizer.h
+++ third_party/WebKit/Source/core/layout/TextAutosizer.h
@@ -65,7 +65,6 @@ public:
     void updatePageInfo();
     void record(const LayoutBlock*);
     void destroy(const LayoutBlock*);
-    void inflateListItem(LayoutListItem*, LayoutListMarker*);
 
     DECLARE_TRACE();
 
