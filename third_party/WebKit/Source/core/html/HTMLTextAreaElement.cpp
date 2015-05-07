6224d0f8ba9ca87ca1afa18f81818ad73cc1f3ac
rouslan@chromium.org <rouslan@chromium.org>
2015-05-07 04:36:53 +0000

    Textarea placeholder should disappear when inserting text.
    BUG=485041
    
    Review URL: https://codereview.chromium.org/1127213003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195023 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLTextAreaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLTextAreaElement.cpp
@@ -274,6 +274,7 @@ void HTMLTextAreaElement::subtreeHasChanged()
     m_valueIsUpToDate = false;
     setNeedsValidityCheck();
     setAutofilled(false);
+    updatePlaceholderVisibility(false);
 
     if (!focused())
         return;
