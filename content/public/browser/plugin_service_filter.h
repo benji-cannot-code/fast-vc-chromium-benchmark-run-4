16f6eeadbf76d9f478c2af9040f7fea388c61b98
wfh <wfh@chromium.org>
2015-05-07 16:44:54 -0700

    Add Infobar when NPAPI plugin is not found.
    When a plugin cannot be found, and NPAPI plugins are disabled - try and determine if it's probably an NPAPI plugin and if so display an infobar about NPAPI deprecation to the user. This aims to catch the case of plugins suddenly not working after M42.
    
    This is the second part of two CLs to better inform users about the NPAPI deprecation.
    
    See mocks - https://docs.google.com/document/d/1hwSFxMKL1Q2FVNAqmy1GxiZZb0Df8PO_oCkXnUk2QpQ/edit
    
    BUG=380227
    
    Review URL: https://codereview.chromium.org/1122683002
    
    Cr-Commit-Position: refs/heads/master@{#328881}

--- content/public/browser/plugin_service_filter.h
+++ content/public/browser/plugin_service_filter.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_BROWSER_PLUGIN_SERVICE_FILTER_H_
 #define CONTENT_PUBLIC_BROWSER_PLUGIN_SERVICE_FILTER_H_
 
+#include <string>
+
 class GURL;
 
 namespace base {
@@ -35,10 +37,11 @@ class PluginServiceFilter {
   virtual bool CanLoadPlugin(int render_process_id,
                              const base::FilePath& path) = 0;
 
-  // Called when a renderer loads an NPAPI |plugin|.
+  // Called when a renderer loads an NPAPI |plugin| that matched |mime_type|.
   // TODO(wfh): Remove when NPAPI is gone.
   virtual void NPAPIPluginLoaded(int render_process_id,
                                  int render_frame_id,
+                                 const std::string& mime_type,
                                  const WebPluginInfo& plugin) {}
 };
 
