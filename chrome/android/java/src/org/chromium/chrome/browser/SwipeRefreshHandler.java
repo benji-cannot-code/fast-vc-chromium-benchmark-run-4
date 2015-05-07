3c47c900b211cdb024be132eb5499be594f6b553
jdduke <jdduke@chromium.org>
2015-05-07 14:00:38 -0700

    [Android] Dismiss refresh spinner when showing the repost warn dialog
    If the user cancels a refresh via the report form warning dialog, we
    never get a callback that the page has been reloaded. Consequently,
    for gesture-triggered reloads the refresh spinner would continue showing
    after the reload cancellation.
    
    Instead, simply cancel the refresh animation when the repost warning
    dialog is shown. This avoids any associated callback guarantee issues
    while also preventing animation jank as the dialog is shown.
    
    Also set the MaterialProgressDrawable animation's repeat count back to
    infinite, instead relying on the SwipeRefreshHandler container to
    terminate the refresh animation after a reasonable upper limit.
    
    BUG=485609
    
    Review URL: https://codereview.chromium.org/1131903003
    
    Cr-Commit-Position: refs/heads/master@{#328825}

--- chrome/android/java/src/org/chromium/chrome/browser/SwipeRefreshHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/SwipeRefreshHandler.java
@@ -25,6 +25,10 @@ public class SwipeRefreshHandler implements OverscrollRefreshHandler {
     // call to stop the refresh animation.
     private static final int STOP_REFRESH_ANIMATION_DELAY_MS = 500;
 
+    // Max allowed duration of the refresh animation after a refresh signal,
+    // guarding against cases where the page reload fails or takes too long.
+    private static final int MAX_REFRESH_ANIMATION_DURATION_MS = 7500;
+
     // The modified AppCompat version of the refresh effect, handling all core
     // logic, rendering and animation.
     private final SwipeRefreshLayout mSwipeRefreshLayout;
@@ -66,6 +70,7 @@ public class SwipeRefreshHandler implements OverscrollRefreshHandler {
 
         if (mContentViewCore != null) {
             setEnabled(false);
+            cancelStopRefreshingRunnable();
             mSwipeRefreshLayout.setOnRefreshListener(null);
             mContentViewCore.setOverscrollRefreshHandler(null);
         }
@@ -78,9 +83,9 @@ public class SwipeRefreshHandler implements OverscrollRefreshHandler {
         mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                if (mStopRefreshingRunnable != null) {
-                    mSwipeRefreshLayout.removeCallbacks(mStopRefreshingRunnable);
-                }
+                cancelStopRefreshingRunnable();
+                mSwipeRefreshLayout.postDelayed(
+                        getStopRefreshingRunnable(), MAX_REFRESH_ANIMATION_DURATION_MS);
                 if (mAccessibilityRefreshString == null) {
                     int resId = R.string.accessibility_swipe_refresh;
                     mAccessibilityRefreshString =
@@ -100,16 +105,9 @@ public class SwipeRefreshHandler implements OverscrollRefreshHandler {
      */
     public void didStopRefreshing() {
         if (!mSwipeRefreshLayout.isRefreshing()) return;
-        if (mStopRefreshingRunnable == null) {
-            mStopRefreshingRunnable = new Runnable() {
-                @Override
-                public void run() {
-                    mSwipeRefreshLayout.setRefreshing(false);
-                }
-            };
-        }
-        mSwipeRefreshLayout.removeCallbacks(mStopRefreshingRunnable);
-        mSwipeRefreshLayout.postDelayed(mStopRefreshingRunnable, STOP_REFRESH_ANIMATION_DELAY_MS);
+        cancelStopRefreshingRunnable();
+        mSwipeRefreshLayout.postDelayed(
+                getStopRefreshingRunnable(), STOP_REFRESH_ANIMATION_DELAY_MS);
     }
 
     @Override
@@ -134,6 +132,7 @@ public class SwipeRefreshHandler implements OverscrollRefreshHandler {
 
     @Override
     public void reset() {
+        cancelStopRefreshingRunnable();
         mSwipeRefreshLayout.reset();
         detachSwipeRefreshLayoutIfNecessary();
     }
@@ -142,6 +141,24 @@ public class SwipeRefreshHandler implements OverscrollRefreshHandler {
     public void setEnabled(boolean enabled) {
         mSwipeRefreshLayout.setEnabled(enabled);
         if (!enabled) reset();
+    }
+
+    private void cancelStopRefreshingRunnable() {
+        if (mStopRefreshingRunnable != null) {
+            mSwipeRefreshLayout.removeCallbacks(mStopRefreshingRunnable);
+        }
+    }
+
+    private Runnable getStopRefreshingRunnable() {
+        if (mStopRefreshingRunnable == null) {
+            mStopRefreshingRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    mSwipeRefreshLayout.setRefreshing(false);
+                }
+            };
+        }
+        return mStopRefreshingRunnable;
     }
 
     // The animation view is attached/detached on-demand to minimize overlap
--- third_party/android_swipe_refresh/java/src/org/chromium/third_party/android/swiperefresh/MaterialProgressDrawable.java
+++ third_party/android_swipe_refresh/java/src/org/chromium/third_party/android/swiperefresh/MaterialProgressDrawable.java
@@ -423,7 +423,7 @@ class MaterialProgressDrawable extends Drawable implements Animatable {
                 }
             }
         };
-        animation.setRepeatCount(10);
+        animation.setRepeatCount(Animation.INFINITE);
         animation.setRepeatMode(Animation.RESTART);
         animation.setInterpolator(LINEAR_INTERPOLATOR);
         animation.setAnimationListener(new Animation.AnimationListener() {
