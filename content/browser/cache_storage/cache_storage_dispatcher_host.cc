cc550c79f382b660c75b951cf7dca5a8b0831d5d
nhiroki <nhiroki@chromium.org>
2015-05-06 22:09:44 -0700

    CacheStorage: Consolidate callbacks for put and delete operations
    This CL consolidates OnCachePutCallback and OnCacheDeleteCallback into
    OnCacheBatchCallback for cleanup.
    
    BUG=426928
    TEST=run-webkit-tests http/tests/cachestorage/
    
    Review URL: https://codereview.chromium.org/1133473002
    
    Cr-Commit-Position: refs/heads/master@{#328712}

--- content/browser/cache_storage/cache_storage_dispatcher_host.cc
+++ content/browser/cache_storage/cache_storage_dispatcher_host.cc
@@ -284,7 +284,7 @@ void CacheStorageDispatcherHost::OnCacheBatch(
 
   if (operation.operation_type == CACHE_STORAGE_CACHE_OPERATION_TYPE_DELETE) {
     cache->Delete(scoped_request.Pass(),
-                  base::Bind(&CacheStorageDispatcherHost::OnCacheDeleteCallback,
+                  base::Bind(&CacheStorageDispatcherHost::OnCacheBatchCallback,
                              this, thread_id, request_id, cache));
     return;
   }
@@ -298,8 +298,8 @@ void CacheStorageDispatcherHost::OnCacheBatch(
         operation.response.headers, operation.response.blob_uuid,
         operation.response.blob_size, operation.response.stream_url));
     cache->Put(scoped_request.Pass(), scoped_response.Pass(),
-               base::Bind(&CacheStorageDispatcherHost::OnCachePutCallback, this,
-                          thread_id, request_id, cache));
+               base::Bind(&CacheStorageDispatcherHost::OnCacheBatchCallback,
+                          this, thread_id, request_id, cache));
 
     return;
   }
@@ -443,21 +443,7 @@ void CacheStorageDispatcherHost::OnCacheKeysCallback(
   Send(new CacheStorageMsg_CacheKeysSuccess(thread_id, request_id, out));
 }
 
-void CacheStorageDispatcherHost::OnCacheDeleteCallback(
-    int thread_id,
-    int request_id,
-    const scoped_refptr<CacheStorageCache>& cache,
-    CacheStorageCache::ErrorType error) {
-  if (error != CacheStorageCache::ERROR_TYPE_OK) {
-    Send(new CacheStorageMsg_CacheBatchError(
-        thread_id, request_id, CacheErrorToWebServiceWorkerCacheError(error)));
-    return;
-  }
-
-  Send(new CacheStorageMsg_CacheBatchSuccess(thread_id, request_id));
-}
-
-void CacheStorageDispatcherHost::OnCachePutCallback(
+void CacheStorageDispatcherHost::OnCacheBatchCallback(
     int thread_id,
     int request_id,
     const scoped_refptr<CacheStorageCache>& cache,
