763150a9b66feb4c36b2be6538235918eec2a168
yoshiki <yoshiki@chromium.org>
2015-05-07 01:23:15 -0700

    Close the notification after user clicks cancel button in message center
    When the message center opens, NotificationUIManager::CancelById method doesn't close the notification. We need to call MessageCenter::RemoveNotification in addition.
    
    BUG=481078
    TEST=manually tested, added test succees
    
    Review URL: https://codereview.chromium.org/1099053003
    
    Cr-Commit-Position: refs/heads/master@{#328728}

--- chrome/browser/download/notification/download_notification_item.h
+++ chrome/browser/download/notification/download_notification_item.h
@@ -43,6 +43,11 @@ class DownloadNotificationItem : public content::DownloadItem::Observer {
  private:
   friend class test::DownloadNotificationItemTest;
 
+  enum NotificationUpdateType {
+    ADD_NEW,
+    UPDATE_EXISTING
+  };
+
   class NotificationWatcher : public NotificationDelegate {
    public:
     explicit NotificationWatcher(DownloadNotificationItem* item);
@@ -79,7 +84,8 @@ class DownloadNotificationItem : public content::DownloadItem::Observer {
   void OnDownloadRemoved(content::DownloadItem* item) override;
   void OnDownloadDestroyed(content::DownloadItem* item) override;
 
-  void UpdateNotificationData();
+  void CloseNotificationByUser();
+  void UpdateNotificationData(NotificationUpdateType type);
   void SetNotificationImage(int resource_id);
 
   NotificationUIManager* notification_ui_manager() const;
