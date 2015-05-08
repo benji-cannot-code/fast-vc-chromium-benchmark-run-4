5565a301bf1e887b83fdba6141502b50dbc55e7c
georgesak <georgesak@chromium.org>
2015-05-08 15:58:53 -0700

    [Session restore] Restore favicons of background tabs.
    BUG=
    
    Review URL: https://codereview.chromium.org/1134723002
    
    Cr-Commit-Position: refs/heads/master@{#329027}

--- chrome/browser/sessions/session_restore_delegate.cc
+++ chrome/browser/sessions/session_restore_delegate.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sessions/session_restore_stats_collector.h"
 #include "chrome/browser/sessions/tab_loader.h"
 #include "chrome/common/url_constants.h"
-#include "components/favicon/content/content_favicon_driver.h"
+#include "content/public/browser/web_contents.h"
 
 namespace {
 
