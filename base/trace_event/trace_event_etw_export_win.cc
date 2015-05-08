4bc2e9c7f8b40274c5c490b43725d9d07d81f0fb
georgesak <georgesak@chromium.org>
2015-05-08 10:24:59 -0700

    [ETW Export] Fix possible NULL ptr dereference. This was happening in tests only and was not caught before.
    BUG=486035
    
    Review URL: https://codereview.chromium.org/1137653003
    
    Cr-Commit-Position: refs/heads/master@{#328971}

--- base/trace_event/trace_event_etw_export_win.cc
+++ base/trace_event/trace_event_etw_export_win.cc
@@ -107,12 +107,14 @@ TraceEventETWExport* TraceEventETWExport::GetInstance() {
 
 // static
 void TraceEventETWExport::EnableETWExport() {
-  GetInstance()->ETWExportEnabled_ = true;
+  if (GetInstance())
+    GetInstance()->ETWExportEnabled_ = true;
 }
 
 // static
 void TraceEventETWExport::DisableETWExport() {
-  GetInstance()->ETWExportEnabled_ = false;
+  if (GetInstance())
+    GetInstance()->ETWExportEnabled_ = false;
 }
 
 // static
@@ -127,7 +129,8 @@ void TraceEventETWExport::AddEvent(
     const unsigned long long* arg_values,
     const scoped_refptr<ConvertableToTraceFormat>* convertable_values) {
   // We bail early in case exporting is disabled or no consumer is listening.
-  if (!GetInstance()->ETWExportEnabled_ || !EventEnabledChromeEvent())
+  if (!GetInstance() || !GetInstance()->ETWExportEnabled_ ||
+      !EventEnabledChromeEvent())
     return;
 
   std::string phase_string;
@@ -225,7 +228,8 @@ void TraceEventETWExport::AddCustomEvent(const char* name,
                                          const char* arg_value_2,
                                          const char* arg_name_3,
                                          const char* arg_value_3) {
-  if (!GetInstance()->ETWExportEnabled_ || !EventEnabledChromeEvent())
+  if (!GetInstance() || !GetInstance()->ETWExportEnabled_ ||
+      !EventEnabledChromeEvent())
     return;
 
   EventWriteChromeEvent(name, phase, arg_name_1, arg_value_1, arg_name_2,
