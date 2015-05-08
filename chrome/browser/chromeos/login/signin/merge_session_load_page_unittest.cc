861054f1daba15f2aa0c894a2ad1c3ed4c083ce4
ksakamoto <ksakamoto@chromium.org>
2015-05-07 21:16:23 -0700

    Revert of Classify navigations without page id in parallel to the existing classifier. (patchset #40 id:780001 of https://codereview.chromium.org/1002803002/)
    Reason for revert:
    This still breaks Instrumentation test AndroidWebViewTest.
    failures:
    org.chromium.android_webview.test.LoadDataWithBaseUrlTest#testOnPageFinishedUrlIsBaseUrl
    
    http://build.chromium.org/p/chromium.linux/builders/Android%20Tests%20%28dbg%29/builds/27757/steps/Instrumentation%20test%20AndroidWebViewTest/logs/stdio
    
    Original issue's description:
    > Classify navigations without page id in parallel to the existing classifier.
    >
    > For now, this only happens in debug builds.
    >
    > BUG=369661
    > TEST=NavigationControllerBrowserTest.NavigationTypeClassification_*
    > TEST=Every other test on the planet.
    >
    > Committed: https://crrev.com/d8d93348bbd8c646c337bdaa40fc0c64204fc5ff
    > Cr-Commit-Position: refs/heads/master@{#327122}
    >
    > Reverted: https://crrev.com/5348e920f4119aff9a4eb76c0965725dc85a66cc
    > Cr-Revert-Position: refs/heads/master@{#327152}
    >
    > Committed: https://crrev.com/5671403d44971669e4d81aecf3f002188ce0e95f
    > Cr-Commit-Position: refs/heads/master@{#327214}
    >
    > Reverted: https://crrev.com/49180eb13549e440bbd4f66390e32e84699dcdfd
    > Cr-Revert-Position: refs/heads/master@{#327269}
    >
    > Committed: https://crrev.com/a038c6670f450313a8e224ccc5d05dd59e3488c4
    > Cr-Commit-Position: refs/heads/master@{#328131}
    >
    > Reverted: https://crrev.com/1f5d6196b8d53e70e7a7a2276cdf3481365e7e04
    > Cr-Revert-Position: refs/heads/master@{#328282}
    >
    > Committed: https://crrev.com/61cae85448cfeb793270e804b5ad1023993279c5
    > Cr-Commit-Position: refs/heads/master@{#328829}
    
    TBR=phajdan.jr@chromium.org,clamy@chromium.org,creis@chromium.org,hajimehoshi@chromium.org,isherman@chromium.org,jeremyim@chromium.org,mattm@chromium.org,mnaganov@chromium.org,mvanouwerkerk@chromium.org,nasko@chromium.org,stevenjb@chromium.org,bengr@chromium.org,cpu@chromium.org,nkostylev@chromium.org,avi@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=369661
    
    Review URL: https://codereview.chromium.org/1136553005
    
    Cr-Commit-Position: refs/heads/master@{#328924}

--- chrome/browser/chromeos/login/signin/merge_session_load_page_unittest.cc
+++ chrome/browser/chromeos/login/signin/merge_session_load_page_unittest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "content/public/browser/interstitial_page.h"
 #include "content/public/browser/navigation_controller.h"
-#include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/web_contents_tester.h"
 
@@ -66,14 +65,10 @@ class MergeSessionLoadPageTest : public ChromeRenderViewHostTestHarness {
     ChromeRenderViewHostTestHarness::TearDown();
   }
 
-  void Navigate(const char* url,
-                int page_id,
-                int nav_entry_id,
-                bool did_create_new_entry) {
-    WebContentsTester::For(web_contents())
-        ->TestDidNavigate(web_contents()->GetMainFrame(), page_id, nav_entry_id,
-                          did_create_new_entry, GURL(url),
-                          ui::PAGE_TRANSITION_TYPED);
+  void Navigate(const char* url, int page_id) {
+    WebContentsTester::For(web_contents())->TestDidNavigate(
+        web_contents()->GetMainFrame(), page_id, GURL(url),
+        ui::PAGE_TRANSITION_TYPED);
   }
 
   void ShowInterstitial(const char* url) {
@@ -123,7 +118,7 @@ class MergeSessionLoadPageTest : public ChromeRenderViewHostTestHarness {
 TEST_F(MergeSessionLoadPageTest, MergeSessionPageNotShown) {
   SetMergeSessionState(OAuth2LoginManager::SESSION_RESTORE_DONE);
   // Start a load.
-  Navigate(kURL1, 1, 0, true);
+  Navigate(kURL1, 1);
   // Load next page.
   controller().LoadURL(GURL(kURL2), content::Referrer(),
                        ui::PAGE_TRANSITION_TYPED, std::string());
@@ -141,7 +136,7 @@ TEST_F(MergeSessionLoadPageTest, MergeSessionPageNotShownOnTimeout) {
       base::TimeDelta::FromSeconds(kSessionMergeTimeout + 1));
 
   // Start a load.
-  Navigate(kURL1, 1, 0, true);
+  Navigate(kURL1, 1);
   // Load next page.
   controller().LoadURL(GURL(kURL2), content::Referrer(),
                        ui::PAGE_TRANSITION_TYPED, std::string());
@@ -156,11 +151,10 @@ TEST_F(MergeSessionLoadPageTest, MergeSessionPageShown) {
   SetMergeSessionState(OAuth2LoginManager::SESSION_RESTORE_IN_PROGRESS);
 
   // Start a load.
-  Navigate(kURL1, 1, 0, true);
+  Navigate(kURL1, 1);
   // Load next page.
   controller().LoadURL(GURL(kURL2), content::Referrer(),
                        ui::PAGE_TRANSITION_TYPED, std::string());
-  int pending_id = controller().GetPendingEntry()->GetUniqueID();
 
   // Simulate the load causing an merge session interstitial page
   // to be shown.
@@ -177,7 +171,7 @@ TEST_F(MergeSessionLoadPageTest, MergeSessionPageShown) {
   EXPECT_EQ(kURL2, web_contents()->GetVisibleURL().spec());
 
   // Commit navigation and the interstitial page is gone.
-  Navigate(kURL2, 2, pending_id, true);
+  Navigate(kURL2, 2);
   EXPECT_FALSE(GetMergeSessionLoadPage());
 }
 
