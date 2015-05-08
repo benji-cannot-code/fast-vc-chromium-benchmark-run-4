2610dc15ab98af127fe10b2ea76e0a545b6b9eb9
nednguyen <nednguyen@google.com>
2015-05-08 11:35:57 -0700

    Modify smoothness' category filter to include less categories.
    The refactoring that modifies smoothness to use TimelineBasedMeasurement's
    control code (https://codereview.chromium.org/1117953004)
    has change the tracing categories used for smoothness slightly, which
    may cause a small regression to queuing_durations metric.
    This patch explictly specifies the tracing categories to be used for smoothness
    instead of using the default no-overhead-level
    (https://code.google.com/p/chromium/codesearch#chromium/src/tools/telemetry/telemetry/core/platform/tracing_category_filter.py&l=18).
    
    BUG=484802, 444701
    
    Review URL: https://codereview.chromium.org/1131883002
    
    Cr-Commit-Position: refs/heads/master@{#328994}

--- tools/perf/measurements/smoothness.py
+++ tools/perf/measurements/smoothness.py
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from telemetry.core.platform import tracing_category_filter
 from telemetry.page import page_test
 from telemetry.web_perf import timeline_based_measurement
 
@@ -35,8 +36,14 @@ class Smoothness(page_test.PageTest):
 
   def WillNavigateToPage(self, page, tab):
     tracing_controller = tab.browser.platform.tracing_controller
+    # FIXME: Remove webkit.console when blink.console lands in chromium and
+    # the ref builds are updated. crbug.com/386847
+    custom_categories = [
+        'webkit.console', 'blink.console', 'benchmark', 'trace_event_overhead']
+    category_filter = tracing_category_filter.TracingCategoryFilter(
+        ','.join(custom_categories))
     self._tbm = timeline_based_measurement.TimelineBasedMeasurement(
-        timeline_based_measurement.Options(),
+        timeline_based_measurement.Options(category_filter),
         _CustomResultsWrapper)
     self._tbm.WillRunUserStory(
         tracing_controller, page.GetSyntheticDelayCategories())
