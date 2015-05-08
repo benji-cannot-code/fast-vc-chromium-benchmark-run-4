533c12e9d766058055b7ee87a9fe9bb652a93554
georgesak <georgesak@chromium.org>
2015-05-07 20:53:30 -0700

    [Session restore] Add prioritization criteria when loading background tabs.
    BUG=
    
    Review URL: https://codereview.chromium.org/1124673005
    
    Cr-Commit-Position: refs/heads/master@{#328920}

--- chrome/browser/sessions/session_restore.cc
+++ chrome/browser/sessions/session_restore.cc
@@ -180,7 +180,7 @@ class SessionRestoreImpl : public content::NotificationObserver {
     }
 
     // Always create in a new window.
-    FinishedTabCreation(true, true, created_contents);
+    FinishedTabCreation(true, true, &created_contents);
 
     on_session_restored_callbacks_->Notify(
         static_cast<int>(created_contents.size()));
@@ -281,10 +281,9 @@ class SessionRestoreImpl : public content::NotificationObserver {
   // have been loaded.
   //
   // Returns the Browser that was created, if any.
-  Browser* FinishedTabCreation(
-      bool succeeded,
-      bool created_tabbed_browser,
-      const std::vector<RestoredTab>& contents_created) {
+  Browser* FinishedTabCreation(bool succeeded,
+                               bool created_tabbed_browser,
+                               std::vector<RestoredTab>* contents_created) {
     Browser* browser = nullptr;
     if (!created_tabbed_browser && always_create_tabbed_browser_) {
       browser =
@@ -300,7 +299,9 @@ class SessionRestoreImpl : public content::NotificationObserver {
 
     if (succeeded) {
       // Start Loading tabs.
-      SessionRestoreDelegate::RestoreTabs(contents_created, restore_started_);
+      if (SessionRestore::SmartLoadingEnabled())
+        std::stable_sort(contents_created->begin(), contents_created->end());
+      SessionRestoreDelegate::RestoreTabs(*contents_created, restore_started_);
     }
 
     if (!synchronous_) {
@@ -363,7 +364,7 @@ class SessionRestoreImpl : public content::NotificationObserver {
       content::BrowserContext::GetDefaultStoragePartition(profile_)
           ->GetDOMStorageContext()
           ->StartScavengingUnusedSessionStorage();
-      return FinishedTabCreation(false, false, *created_contents);
+      return FinishedTabCreation(false, false, created_contents);
     }
 
 #if defined(OS_CHROMEOS)
@@ -463,7 +464,7 @@ class SessionRestoreImpl : public content::NotificationObserver {
     // FinishedTabCreation will create a new TabbedBrowser and add the urls to
     // it.
     Browser* finished_browser =
-        FinishedTabCreation(true, has_tabbed_browser, *created_contents);
+        FinishedTabCreation(true, has_tabbed_browser, created_contents);
     if (finished_browser)
       last_browser = finished_browser;
 
@@ -519,9 +520,8 @@ class SessionRestoreImpl : public content::NotificationObserver {
         if (!contents)
           continue;
 
-        RestoredTab restored_tab;
-        restored_tab.contents = contents;
-        restored_tab.is_active = is_selected_tab;
+        RestoredTab restored_tab(contents, is_selected_tab,
+                                 tab.extension_app_id.empty(), tab.pinned);
         created_contents->push_back(restored_tab);
 
         // If this isn't the selected tab, there's nothing else to do.
@@ -544,9 +544,8 @@ class SessionRestoreImpl : public content::NotificationObserver {
         WebContents* contents =
             RestoreTab(tab, tab_index_offset + i, browser, false);
         if (contents) {
-          RestoredTab restored_tab;
-          restored_tab.contents = contents;
-          restored_tab.is_active = false;
+          RestoredTab restored_tab(contents, false,
+                                   tab.extension_app_id.empty(), tab.pinned);
           created_contents->push_back(restored_tab);
         }
       }
@@ -817,6 +816,15 @@ SessionRestore::CallbackSubscription
     SessionRestore::RegisterOnSessionRestoredCallback(
         const base::Callback<void(int)>& callback) {
   return on_session_restored_callbacks()->Add(callback);
+}
+
+// static
+bool SessionRestore::SmartLoadingEnabled() {
+  base::FieldTrial* trial =
+      base::FieldTrialList::Find("SessionRestoreBackgroundLoading");
+  if (!trial || trial->group_name() != "Smart")
+    return false;
+  return true;
 }
 
 // static
