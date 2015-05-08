f9524dd56041369bd8b655b40907e9ffaf10ce88
davve@opera.com <davve@opera.com>
2015-05-08 08:00:50 +0000

    Don't conflate intrinsic width and height with logical width and height
    This is wrong in vertical writing mode and shouldn't be necessary. The
    intrinsic dimensions are propagated from attributes to properties in
    SVGSVGElement::collectStyleForPresentationAttribute().
    
    BUG=484425
    
    Review URL: https://codereview.chromium.org/1128893002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195104 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGRoot.cpp
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGRoot.cpp
@@ -116,16 +116,8 @@ bool LayoutSVGRoot::isEmbeddedThroughFrameContainingSVGDocument() const
     return frame->document()->isSVGDocument();
 }
 
-static inline LayoutUnit resolveLengthAttributeForSVG(const Length& length, float scale, float maxSize)
-{
-    return static_cast<LayoutUnit>(valueForLength(length, maxSize) * (length.isFixed() ? scale : 1));
-}
-
 LayoutUnit LayoutSVGRoot::computeReplacedLogicalWidth(ShouldComputePreferred shouldComputePreferred) const
 {
-    SVGSVGElement* svg = toSVGSVGElement(node());
-    ASSERT(svg);
-
     // When we're embedded through SVGImage (border-image/background-image/<html:img>/...) we're forced to resize to a specific size.
     if (!m_containerSize.isEmpty())
         return m_containerSize.width();
@@ -136,18 +128,12 @@ LayoutUnit LayoutSVGRoot::computeReplacedLogicalWidth(ShouldComputePreferred sho
     if (style()->logicalWidth().isSpecified() || style()->logicalMaxWidth().isSpecified())
         return LayoutReplaced::computeReplacedLogicalWidth(shouldComputePreferred);
 
-    if (svg->hasIntrinsicWidth())
-        return resolveLengthAttributeForSVG(svg->intrinsicWidth(), style()->effectiveZoom(), containingBlock()->availableLogicalWidth().toFloat());
-
     // SVG embedded via SVGImage (background-image/border-image/etc) / Inline SVG.
     return LayoutReplaced::computeReplacedLogicalWidth(shouldComputePreferred);
 }
 
 LayoutUnit LayoutSVGRoot::computeReplacedLogicalHeight() const
 {
-    SVGSVGElement* svg = toSVGSVGElement(node());
-    ASSERT(svg);
-
     // When we're embedded through SVGImage (border-image/background-image/<html:img>/...) we're forced to resize to a specific size.
     if (!m_containerSize.isEmpty())
         return m_containerSize.height();
@@ -157,9 +143,6 @@ LayoutUnit LayoutSVGRoot::computeReplacedLogicalHeight() const
 
     if (style()->logicalHeight().isSpecified() || style()->logicalMaxHeight().isSpecified())
         return LayoutReplaced::computeReplacedLogicalHeight();
-
-    if (svg->hasIntrinsicHeight())
-        return resolveLengthAttributeForSVG(svg->intrinsicHeight(), style()->effectiveZoom(), containingBlock()->availableLogicalHeight(IncludeMarginBorderPadding).toFloat());
 
     // SVG embedded via SVGImage (background-image/border-image/etc) / Inline SVG.
     return LayoutReplaced::computeReplacedLogicalHeight();
