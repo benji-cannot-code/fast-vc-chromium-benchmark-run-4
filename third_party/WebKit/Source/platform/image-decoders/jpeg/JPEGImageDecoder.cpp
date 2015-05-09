246744f67bb578de312f836aabc2c58e50a1fe79
noel@chromium.org <noel@chromium.org>
2015-05-09 08:52:22 +0000

    [JPEGImageDecoder] Remove superfluous brackets and add FIXME
    The PNG decoder can live without the brackets (so consistency). Add
    FIXME notes for future clean-up.
    
    TBR=urvang@chromium.org
    BUG=439655
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1129403004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195143 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/image-decoders/jpeg/JPEGImageDecoder.cpp
+++ third_party/WebKit/Source/platform/image-decoders/jpeg/JPEGImageDecoder.cpp
@@ -321,6 +321,7 @@ public:
         jpeg_create_decompress(&m_info);
 
         decoder_source_mgr* src = 0;
+        // FIXME: why would m_info.src be anything but null at decompressor creation time?
         if (!m_info.src) {
             src = (decoder_source_mgr*)fastZeroedMalloc(sizeof(decoder_source_mgr));
             if (!src) {
@@ -991,9 +992,8 @@ void JPEGImageDecoder::decode(bool onlySize)
     if (failed())
         return;
 
-    if (!m_reader) {
+    if (!m_reader)
         m_reader = adoptPtr(new JPEGImageReader(this));
-    }
 
     // If we couldn't decode the image but we've received all the data, decoding
     // has failed.
@@ -1001,6 +1001,7 @@ void JPEGImageDecoder::decode(bool onlySize)
         setFailed();
     // If we're done decoding the image, we don't need the JPEGImageReader
     // anymore.  (If we failed, |m_reader| has already been cleared.)
+    // FIXME: factor these cases into an isComplete() routine, refer to the PNG encoder.
     else if ((!m_frameBufferCache.isEmpty() && (m_frameBufferCache[0].status() == ImageFrame::FrameComplete)) || (hasImagePlanes() && !onlySize))
         m_reader.clear();
 }
