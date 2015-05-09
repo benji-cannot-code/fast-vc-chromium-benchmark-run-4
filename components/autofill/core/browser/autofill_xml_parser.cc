584857ef79208a3085030e1db05c33e5c6ea5682
estade <estade@chromium.org>
2015-05-08 19:48:24 -0700

    Correctly parse doubles in Autofill XML parser.
    Can be tested with:
    LANG="fr_FR.utf8" ./out/Release/components_unittests --gtest_filter=AutofillDownload*
    
    BUG=485566
    
    Review URL: https://codereview.chromium.org/1131193003
    
    Cr-Commit-Position: refs/heads/master@{#329029}

--- components/autofill/core/browser/autofill_xml_parser.cc
+++ components/autofill/core/browser/autofill_xml_parser.cc
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/autofill_xml_parser.h"
 
-#include <stdlib.h>
 #include <string.h>
 
 #include "base/logging.h"
+#include "base/strings/string_number_conversions.h"
 #include "components/autofill/core/browser/autofill_server_field_info.h"
 #include "third_party/webrtc/libjingle/xmllite/qname.h"
 
@@ -131,9 +131,8 @@ void AutofillQueryXmlParser::ParseElementDescriptor(
 
 int AutofillQueryXmlParser::GetIntValue(buzz::XmlParseContext* context,
                                         const char* attribute) {
-  char* attr_end = NULL;
-  int value = strtol(attribute, &attr_end, 10);
-  if (attr_end != NULL && attr_end == attribute) {
+  int value = 0;
+  if (!base::StringToInt(attribute, &value)) {
     context->RaiseError(XML_ERROR_SYNTAX);
     return 0;
   }
@@ -171,9 +170,8 @@ void AutofillUploadXmlParser::StartElement(buzz::XmlParseContext* context,
 
 double AutofillUploadXmlParser::GetDoubleValue(buzz::XmlParseContext* context,
                                                const char* attribute) {
-  char* attr_end = NULL;
-  double value = strtod(attribute, &attr_end);
-  if (attr_end != NULL && attr_end == attribute) {
+  double value = 0;
+  if (!base::StringToDouble(attribute, &value)) {
     context->RaiseError(XML_ERROR_SYNTAX);
     return 0.0;
   }
