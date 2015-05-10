5010e24af6c11f303d4a2b4cb7b491deb4562726
cmumford <cmumford@chromium.org>
2015-05-10 09:44:13 -0700

    localStorage: Aggressively flush to disk for Android WebView.
    Android WebView does not save data during shutdown like Chrome does.
    Adding an --enable-aggressive-domstorage-flushing flag to return the
    DOM Storage implementation to the prior implementation which had a 1
    sec. timer to flush the commit queue.
    
    BUG=479767
    
    Review URL: https://codereview.chromium.org/1129233003
    
    Cr-Commit-Position: refs/heads/master@{#329070}

--- content/browser/dom_storage/dom_storage_area.cc
+++ content/browser/dom_storage/dom_storage_area.cc
@@ -43,6 +43,8 @@ const int kMaxCommitsPerHour = 6;
 
 }  // namespace
 
+bool DOMStorageArea::s_aggressive_flushing_enabled_ = false;
+
 DOMStorageArea::RateLimiter::RateLimiter(size_t desired_rate,
                                          base::TimeDelta time_quantum)
     : rate_(desired_rate), samples_(0), time_quantum_(time_quantum) {
@@ -89,6 +91,10 @@ GURL DOMStorageArea::OriginFromDatabaseFileName(const base::FilePath& name) {
   std::string origin_id =
       name.BaseName().RemoveExtension().MaybeAsASCII();
   return storage::GetOriginFromIdentifier(origin_id);
+}
+
+void DOMStorageArea::EnableAggressiveCommitDelay() {
+  s_aggressive_flushing_enabled_ = true;
 }
 
 DOMStorageArea::DOMStorageArea(const GURL& origin,
@@ -389,6 +395,9 @@ void DOMStorageArea::StartCommitTimer() {
 }
 
 base::TimeDelta DOMStorageArea::ComputeCommitDelay() const {
+  if (s_aggressive_flushing_enabled_)
+    return base::TimeDelta::FromSeconds(1);
+
   base::TimeDelta elapsed_time = base::TimeTicks::Now() - start_time_;
   base::TimeDelta delay = std::max(
       base::TimeDelta::FromSeconds(kCommitDefaultDelaySecs),
