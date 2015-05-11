22d7155512a1176b2b4c7508c2289f62fc74215a
scheib <scheib@chromium.org>
2015-05-11 11:52:49 -0700

    bluetooth: Android adapter can be created with and without Bluetooth permission.
    This enables unit tests to run with Bluetooth permission,
    by adding the Bluetooth permission to native_test.
    
    Non-test applications will not have the Bluetooth permission,
    and unit tests should also verify behavior
    when the permission is not given. To enable this
    createWithoutPermissionForTesting is added and
    results in a state equivalent to when the permission
    is not available.
    
    Unit tests will be built in parallel for both when Bluetooth
    permission exists and doesn't.
    
    BUG=471536
    
    Review URL: https://codereview.chromium.org/1129683002
    
    Cr-Commit-Position: refs/heads/master@{#329212}

--- device/bluetooth/android/java/src/org/chromium/device/bluetooth/BluetoothAdapter.java
+++ device/bluetooth/android/java/src/org/chromium/device/bluetooth/BluetoothAdapter.java
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.device.bluetooth;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.pm.PackageManager;
 
 import org.chromium.base.CalledByNative;
@@ -21,6 +23,7 @@ final class BluetoothAdapter {
     private static final String TAG = Log.makeTag("Bluetooth");
 
     private final boolean mHasBluetoothPermission;
+    private android.bluetooth.BluetoothAdapter mAdapter;
 
     @CalledByNative
     private static BluetoothAdapter create(Context context) {
@@ -28,16 +31,78 @@ final class BluetoothAdapter {
     }
 
     @CalledByNative
+    private static BluetoothAdapter createWithoutPermissionForTesting(Context context) {
+        Context contextWithoutPermission = new ContextWrapper(context) {
+            @Override
+            public int checkCallingOrSelfPermission(String permission) {
+                return PackageManager.PERMISSION_DENIED;
+            }
+        };
+        return new BluetoothAdapter(contextWithoutPermission);
+    }
+
+    // Constructs a BluetoothAdapter.
+    private BluetoothAdapter(Context context) {
+        mHasBluetoothPermission =
+                context.checkCallingOrSelfPermission(Manifest.permission.BLUETOOTH)
+                        == PackageManager.PERMISSION_GRANTED
+                && context.checkCallingOrSelfPermission(Manifest.permission.BLUETOOTH_ADMIN)
+                        == PackageManager.PERMISSION_GRANTED;
+        if (!mHasBluetoothPermission) {
+            Log.w(TAG,
+                    "Bluetooth API disabled; BLUETOOTH and BLUETOOTH_ADMIN permissions required.");
+            return;
+        }
+
+        mAdapter = android.bluetooth.BluetoothAdapter.getDefaultAdapter();
+        if (mAdapter == null) Log.i(TAG, "No adapter found.");
+    }
+
+    @CalledByNative
     private boolean hasBluetoothPermission() {
         return mHasBluetoothPermission;
     }
 
-    private BluetoothAdapter(Context context) {
-        mHasBluetoothPermission =
-                context.checkCallingOrSelfPermission(android.Manifest.permission.BLUETOOTH)
-                == PackageManager.PERMISSION_GRANTED;
-        if (!mHasBluetoothPermission) {
-            Log.w(TAG, "Can not use bluetooth API, requires BLUETOOTH permission.");
+    // ---------------------------------------------------------------------------------------------
+    // BluetoothAdapterAndroid.h interface:
+
+    @CalledByNative
+    private String getAddress() {
+        if (isPresent()) {
+            return mAdapter.getAddress();
+        } else {
+            return "";
         }
+    }
+
+    @CalledByNative
+    private String getName() {
+        if (isPresent()) {
+            return mAdapter.getName();
+        } else {
+            return "";
+        }
+    }
+
+    @CalledByNative
+    private boolean isPresent() {
+        return mAdapter != null;
+    }
+
+    @CalledByNative
+    private boolean isPowered() {
+        return isPresent() && mAdapter.isEnabled();
+    }
+
+    @CalledByNative
+    private boolean isDiscoverable() {
+        return isPresent()
+                && mAdapter.getScanMode()
+                == android.bluetooth.BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE;
+    }
+
+    @CalledByNative
+    private boolean isDiscovering() {
+        return isPresent() && mAdapter.isDiscovering();
     }
 }
