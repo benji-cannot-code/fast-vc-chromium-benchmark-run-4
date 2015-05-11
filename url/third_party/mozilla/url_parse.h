7de1ddd1ecc423a208c9a141387d70514e71c734
tfarina <tfarina@chromium.org>
2015-05-11 01:22:41 -0700

    url: Remove basictypes.h includes.
    Use the standard integer types from stdint.h instead.
    
    We don't need basictypes.h for other things otherwise.
    
    BUG=138542
    TEST=url_unittests
    R=brettw@chromium.org
    
    Review URL: https://codereview.chromium.org/1135713005
    
    Cr-Commit-Position: refs/heads/master@{#329117}

--- url/third_party/mozilla/url_parse.h
+++ url/third_party/mozilla/url_parse.h
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef URL_THIRD_PARTY_MOZILLA_URL_PARSE_H_
 #define URL_THIRD_PARTY_MOZILLA_URL_PARSE_H_
 
-#include <string>
-
-#include "base/basictypes.h"
 #include "base/strings/string16.h"
 #include "url/url_export.h"
 
