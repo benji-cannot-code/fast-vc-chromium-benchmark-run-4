1e6adcb2bcde9fec2ecbef07f8048b94561d4c4b
jam <jam@chromium.org>
2015-05-11 11:11:38 -0700

    Make --wait-for-debugger work for mandoline launcher process.
    If specified without any switch values, it'll make the launcher process wait. Otherwise one can use an app name of "launcher".
    
    Review URL: https://codereview.chromium.org/1140553002
    
    Cr-Commit-Position: refs/heads/master@{#329186}

--- mojo/runner/child_process_host.cc
+++ mojo/runner/child_process_host.cc
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/process/kill.h"
 #include "base/process/launch.h"
-#include "base/stl_util.h"
-#include "base/strings/string_split.h"
 #include "base/task_runner.h"
 #include "base/task_runner_util.h"
 #include "mojo/edk/embedder/embedder.h"
@@ -103,6 +101,7 @@ bool ChildProcessHost::DoLaunch() {
       switches::kTraceToConsole,
       switches::kV,
       switches::kVModule,
+      switches::kWaitForDebugger,
   };
 
   const base::CommandLine* parent_command_line =
@@ -112,14 +111,6 @@ bool ChildProcessHost::DoLaunch() {
                                       arraysize(kForwardSwitches));
   child_command_line.AppendSwitchASCII(switches::kApp, name_);
   child_command_line.AppendSwitch(switches::kChildProcess);
-  if (parent_command_line->HasSwitch(switches::kWaitForDebugger)) {
-    std::vector<std::string> apps_to_debug;
-    base::SplitString(
-        parent_command_line->GetSwitchValueASCII(switches::kWaitForDebugger),
-        ',', &apps_to_debug);
-    if (apps_to_debug.empty() || ContainsValue(apps_to_debug, name_))
-      child_command_line.AppendSwitch(switches::kWaitForDebugger);
-  }
 
   auto args = parent_command_line->GetArgs();
   for (const auto& arg : args)
