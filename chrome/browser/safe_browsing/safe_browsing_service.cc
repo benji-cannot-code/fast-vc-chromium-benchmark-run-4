b2d99a7b023e7916a2d1b9f5ac7544aa76639bbd
asvitkine <asvitkine@chromium.org>
2015-05-11 09:46:29 -0700

    Remove SafeBrowsing LightSpeed experiment code.
    The experiment is now concluded.
    
    BUG=450037
    
    Review URL: https://codereview.chromium.org/1126233005
    
    Cr-Commit-Position: refs/heads/master@{#329159}

--- chrome/browser/safe_browsing/safe_browsing_service.cc
+++ chrome/browser/safe_browsing/safe_browsing_service.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/debug/leak_tracker.h"
 #include "base/lazy_instance.h"
+#include "base/metrics/field_trial.h"
 #include "base/path_service.h"
 #include "base/prefs/pref_change_registrar.h"
 #include "base/prefs/pref_service.h"
@@ -39,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "components/startup_metric_utils/startup_metric_utils.h"
-#include "components/variations/variations_associated_data.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/cookie_store_factory.h"
 #include "content/public/browser/notification_service.h"
@@ -583,11 +583,6 @@ void SafeBrowsingService::RefreshState() {
       break;
     }
   }
-
-  // TODO(asvitkine): Experimental code for measuring start up impact of SB.
-  // Remove when experimentation is complete. http://crbug.com/450037
-  if (!variations::GetVariationParamValue("LightSpeed", "DisableSB").empty())
-    enable = false;
 
   if (enable)
     Start();
