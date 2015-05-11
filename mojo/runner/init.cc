e828dab776b3ab50497a225de06678a2d436c909
John Abd-El-Malek <jam@chromium.org>
2015-05-11 11:33:17 -0700

    Fix Android build failure.
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1125283006
    
    Cr-Commit-Position: refs/heads/master@{#329195}

--- mojo/runner/init.cc
+++ mojo/runner/init.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include <windows.h>
+#elif (OS_POSIX)
+#include <unistd.h>
 #endif
 
 namespace mojo {
