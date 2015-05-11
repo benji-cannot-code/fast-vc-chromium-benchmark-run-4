4537caa3c17bc0d6877ca5bb3280d88e4107a52b
tfarina <tfarina@chromium.org>
2015-05-11 00:40:26 -0700

    Stop pulling in hunspell through DEPS (gclient sync).
    Hunspell is now checked-in the Chromium tree (under
    third_party/hunspell_new) and the dependencies are using it from there.
    Thus we don't need to be pulling it from the external repo anymore.
    
    After this lands, we are going to wait a week or so, to rename
    third_party/hunspell_new back to third_party/hunspell to smooth the
    transition and not get into the foot of the other devs.
    
    BUG=472897
    R=brettw@chromium.org
    TBR=phajdan.jr@chromium.org # for checklicenses
    
    Review URL: https://codereview.chromium.org/1130203003
    
    Cr-Commit-Position: refs/heads/master@{#329108}

--- tools/checklicenses/checklicenses.py
+++ tools/checklicenses/checklicenses.py
@@ -205,9 +205,6 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
     'third_party/freetype2': [ # http://crbug.com/177319
         'UNKNOWN',
     ],
-    'third_party/hunspell': [  # http://crbug.com/98134
-        'UNKNOWN',
-    ],
     'third_party/hunspell_new': [  # http://crbug.com/98134
         'UNKNOWN',
     ],
