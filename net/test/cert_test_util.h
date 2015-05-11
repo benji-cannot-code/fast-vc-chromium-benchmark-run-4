f7fb299ada83a78a6314fe3c7b39db7664b854bd
davidben <davidben@chromium.org>
2015-05-11 13:20:10 -0700

    Reland "Don't use RSAPrivateKey in NSS integration code."
    This is a reland of https://codereview.chromium.org/1106103003/ with fixes to
    ensure callers never pass in a null slot.
    
    Currently some NSS platform integration logic transits private keys through
    RSAPrivateKey on CrOS. This prevents incrementally switching RSAPrivateKey to
    BoringSSL while keeping platform integrations on NSS.
    
    The intent of this change is to clarify RSAPrivateKey as a BoringSSL vs NSS
    internal crypto library (use_openssl=0 vs use_openssl=1) abstraction. It's
    primarily to be used with SignatureCreator. Code which uses NSS based on
    use_nss_certs rather than use_openssl because the underlying platform is NSS
    should call NSS routines directly, or introduce different abstractions.
    
    Remove the problematic RSAPrivateKey methods and instead add
    crypto/nss_key_util.h which contains some helper functions for manipulating NSS
    keys. This is sufficient to allow consumers of the removed methods to use NSS
    directly with about as much code. (This should not set back migrating that
    logic to NSS as that code was already very NSS-specific; those APIs assumed
    PK11SlotInfo.)
    
    nss_key_util.h, like nss_util.h, is built whenever NSS is used either
    internally or for platform integrations. This is so rsa_private_key_nss.cc can
    continue to use the helper functions to implement the NSS-agnostic interface.
    
    With this, the chimera CrOS configuration should build. The RSAPrivateKey logic
    is functional with the exception of some logic in components/ownership. That
    will be resolved in a future CL.
    
    BUG=478777,483606
    
    Review URL: https://codereview.chromium.org/1128153003
    
    Cr-Commit-Position: refs/heads/master@{#329227}

--- net/test/cert_test_util.h
+++ net/test/cert_test_util.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/x509_certificate.h"
 
 #if defined(USE_NSS_CERTS)
-#include "base/memory/scoped_ptr.h"
-
 // From <pk11pub.h>
 typedef struct PK11SlotInfoStr PK11SlotInfo;
 #endif
@@ -32,13 +30,12 @@ namespace net {
 class EVRootCAMetadata;
 
 #if defined(USE_NSS_CERTS)
-// Imports a private key from file |key_filename| in |dir|. The file must
-// contain a PKCS#8 PrivateKeyInfo in DER encoding. The key is imported to
-// |slot|.
-scoped_ptr<crypto::RSAPrivateKey> ImportSensitiveKeyFromFile(
-    const base::FilePath& dir,
-    const std::string& key_filename,
-    PK11SlotInfo* slot);
+// Imports a private key from file |key_filename| in |dir| into |slot|. The file
+// must contain a PKCS#8 PrivateKeyInfo in DER encoding. Returns true on success
+// and false on failure.
+bool ImportSensitiveKeyFromFile(const base::FilePath& dir,
+                                const std::string& key_filename,
+                                PK11SlotInfo* slot);
 
 bool ImportClientCertToSlot(const scoped_refptr<X509Certificate>& cert,
                             PK11SlotInfo* slot);
