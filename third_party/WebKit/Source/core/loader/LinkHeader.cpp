591436e627dd9e0f42aea4875a56a4bd1ee250ed
yoav@yoav.ws <yoav@yoav.ws>
2015-05-11 16:45:08 +0000

    Added <link rel=preconnect> crossorigin attribute
    In order to support preconnect for both anonymous and non-anonymous connection pools we need to add support for the crossorigin attribute in <link rel=preconnect>.
    This CL implements the core functionality of this change on the Blink side.
    
    BUG=468005
    
    Review URL: https://codereview.chromium.org/1138743002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195184 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/LinkHeader.cpp
+++ third_party/WebKit/Source/core/loader/LinkHeader.cpp
@@ -41,6 +41,11 @@ static bool isValidParameterValueChar(CharType chr)
     return !isWhitespace(chr) && !isValidParameterValueEnd(chr);
 }
 
+static bool isExtensionParameter(LinkHeader::LinkParameterName name)
+{
+    return name > LinkHeader::LinkParameterAnchor;
+}
+
 // Before:
 //
 // <cat.jpg>; rel=preload
@@ -117,6 +122,8 @@ static LinkHeader::LinkParameterName paramterNameFromString(String name)
         return LinkHeader::LinkParameterRel;
     else if (equalIgnoringCase(name, "anchor"))
         return LinkHeader::LinkParameterAnchor;
+    else if (equalIgnoringCase(name, "crossorigin"))
+        return LinkHeader::LinkParameterCrossOrigin;
     return LinkHeader::LinkParameterUnknown;
 }
 
@@ -138,11 +145,10 @@ static bool parseParameterName(CharType*& position, CharType* end, LinkHeader::L
     skipWhile<CharType, isValidParameterNameChar>(position, end);
     CharType* nameEnd = position;
     skipWhile<CharType, isWhitespace>(position, end);
-    if (!skipExactly<CharType>(position, end, '='))
-        return false;
+    bool hasEqual = skipExactly<CharType>(position, end, '=');
     skipWhile<CharType, isWhitespace>(position, end);
     name = paramterNameFromString(String(nameStart, nameEnd - nameStart));
-    return true;
+    return hasEqual || isExtensionParameter(name);
 }
 
 // Before:
@@ -201,8 +207,10 @@ static bool parseParameterValue(CharType*& position, CharType* end, String& valu
     }
     valueEnd = position;
     skipWhile<CharType, isWhitespace>(position, end);
-    if ((!completeQuotes && valueStart == valueEnd) || (position != end && !isValidParameterValueEnd(*position)))
+    if ((!completeQuotes && valueStart == valueEnd) || (position != end && !isValidParameterValueEnd(*position))) {
+        value = String("");
         return false;
+    }
     if (hasQuotes)
         ++valueStart;
     if (completeQuotes)
@@ -218,6 +226,8 @@ void LinkHeader::setValue(LinkParameterName name, String value)
         m_rel = value.lower();
     else if (name == LinkParameterAnchor)
         m_isValid = false;
+    else if (name == LinkParameterCrossOrigin)
+        m_crossOrigin = crossOriginAttributeValue(value);
 }
 
 template <typename CharType>
@@ -229,7 +239,8 @@ static void findNextHeader(CharType*& position, CharType* end)
 
 template <typename CharType>
 LinkHeader::LinkHeader(CharType*& position, CharType* end)
-    : m_isValid(true)
+    : m_crossOrigin(CrossOriginAttributeNotSet)
+    , m_isValid(true)
 {
     if (!parseURL(position, end, m_url)) {
         m_isValid = false;
@@ -251,7 +262,7 @@ LinkHeader::LinkHeader(CharType*& position, CharType* end)
         }
 
         String parameterValue;
-        if (!parseParameterValue(position, end, parameterValue)) {
+        if (!parseParameterValue(position, end, parameterValue) && !isExtensionParameter(parameterName)) {
             findNextHeader(position, end);
             m_isValid = false;
             return;
--- third_party/WebKit/Source/platform/network/NetworkHints.h
+++ third_party/WebKit/Source/platform/network/NetworkHints.h
@@ -30,12 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/PlatformExport.h"
 #include "platform/weborigin/KURL.h"
+#include "public/platform/WebCrossOriginAttribute.h"
 #include "wtf/Forward.h"
 
 namespace blink {
 
 PLATFORM_EXPORT void prefetchDNS(const String& hostname);
-PLATFORM_EXPORT void preconnect(const KURL&);
+PLATFORM_EXPORT void preconnect(const KURL&, const CrossOriginAttributeValue);
 
 } // namespace blink
 
