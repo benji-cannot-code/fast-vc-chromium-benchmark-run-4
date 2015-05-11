0e7a4ed311f2c7bab1f2f42dec436685d26e10a7
jpawlowski <jpawlowski@chromium.org>
2015-05-11 11:17:00 -0700

    Expose RSSI for bluetooth devices during discovery.
    This patch adds wiring to expose RSSI field during device discovery.
    
    BUG=407773
    R=armansito@chromium.org
    
    Review URL: https://codereview.chromium.org/1096433010
    
    Cr-Commit-Position: refs/heads/master@{#329190}

--- device/bluetooth/bluetooth_device_mac.h
+++ device/bluetooth/bluetooth_device_mac.h
@@ -36,6 +36,7 @@ class BluetoothDeviceMac : public BluetoothDevice {
   bool IsConnectable() const override;
   bool IsConnecting() const override;
   UUIDList GetUUIDs() const override;
+  int16 GetInquiryRSSI() const override;
   bool ExpectingPinCode() const override;
   bool ExpectingPasskey() const override;
   bool ExpectingConfirmation() const override;
