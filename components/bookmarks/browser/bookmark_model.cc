7d886365b11fc51fa344391d8d49adbd8a34ac01
tfarina <tfarina@chromium.org>
2015-05-12 14:50:10 -0700

    bookmarks: Do not handle some failures.
    In those cases |node| should not be NULL.
    
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1124103005
    
    Cr-Commit-Position: refs/heads/master@{#329502}

--- components/bookmarks/browser/bookmark_model.cc
+++ components/bookmarks/browser/bookmark_model.cc
@@ -314,10 +314,8 @@ favicon_base::IconType BookmarkModel::GetFaviconType(const BookmarkNode* node) {
 
 void BookmarkModel::SetTitle(const BookmarkNode* node,
                              const base::string16& title) {
-  if (!node) {
-    NOTREACHED();
-    return;
-  }
+  DCHECK(node);
+
   if (node->GetTitle() == title)
     return;
 
@@ -343,16 +341,7 @@ void BookmarkModel::SetTitle(const BookmarkNode* node,
 }
 
 void BookmarkModel::SetURL(const BookmarkNode* node, const GURL& url) {
-  if (!node) {
-    NOTREACHED();
-    return;
-  }
-
-  // We cannot change the URL of a folder.
-  if (node->is_folder()) {
-    NOTREACHED();
-    return;
-  }
+  DCHECK(node && !node->is_folder());
 
   if (node->url() == url)
     return;
@@ -468,20 +457,11 @@ void BookmarkModel::OnFaviconChanged(const std::set<GURL>& urls) {
   }
 }
 
-void BookmarkModel::SetDateAdded(const BookmarkNode* node,
-                                 Time date_added) {
-  if (!node) {
-    NOTREACHED();
-    return;
-  }
+void BookmarkModel::SetDateAdded(const BookmarkNode* node, Time date_added) {
+  DCHECK(node && !is_permanent_node(node));
 
   if (node->date_added() == date_added)
     return;
-
-  if (is_permanent_node(node)) {
-    NOTREACHED();
-    return;
-  }
 
   AsMutable(node)->set_date_added(date_added);
 
