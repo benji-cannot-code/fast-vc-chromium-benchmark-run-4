08438de9a6b2b7e6da3fcee7914545b502eb3f6c
gunsch <gunsch@chromium.org>
2015-05-11 21:22:25 -0700

    Revert of [Chromecast] Ignore interfaces not used to connect to internet. (patchset #1 id:1 of https://codereview.chromium.org/1137483004/)
    Reason for revert:
    broke the Android builds. Not sure how "cast_shell_android" passed, actually.
    
    Original issue's description:
    > [Chromecast] Ignore interfaces not used to connect to internet.
    >
    > Add NetworkChangeNotifierFactoryCast to create instance of
    > NetworkChangeNotifierLinux that ignores interfaces that will not be
    > used to connect to internet.
    >
    > BUG=469863
    > R=gunsch@chromium.org,byungchul@chromium.org,wzhong@chromium.org
    >
    > Committed: https://crrev.com/561d7adb5ec842c5e56ece4524d0e486691ca614
    > Cr-Commit-Position: refs/heads/master@{#329319}
    
    TBR=byungchul@chromium.org,wzhong@chromium.org,derekjchow@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=469863
    
    Review URL: https://codereview.chromium.org/1141503002
    
    Cr-Commit-Position: refs/heads/master@{#329348}

--- chromecast/browser/cast_browser_main_parts.cc
+++ chromecast/browser/cast_browser_main_parts.cc
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/android/network_change_notifier_factory_android.h"
 #else
 #include "chromecast/browser/media/cast_browser_cdm_factory.h"
-#include "chromecast/net/network_change_notifier_factory_cast.h"
 #endif
 
 #if defined(USE_AURA)
@@ -229,9 +228,6 @@ void CastBrowserMainParts::PreMainMessageLoopStart() {
 #if defined(OS_ANDROID)
   net::NetworkChangeNotifier::SetFactory(
       new net::NetworkChangeNotifierFactoryAndroid());
-#else
-  net::NetworkChangeNotifier::SetFactory(
-      new NetworkChangeNotifierFactoryCast());
 #endif  // defined(OS_ANDROID)
 }
 
--- chromecast/chromecast.gyp
+++ chromecast/chromecast.gyp
@@ -102,12 +102,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'sources': [
         'net/connectivity_checker.cc',
         'net/connectivity_checker.h',
-        'net/network_change_notifier_factory_cast.cc',
-        'net/network_change_notifier_factory_cast.h',
         'net/net_switches.cc',
         'net/net_switches.h',
-        'net/net_util_cast.cc',
-        'net/net_util_cast.h',
       ],
     },
     {
