c83286d07dffea8dfc7c12a357e132427427da8c
davidben <davidben@chromium.org>
2015-05-12 15:30:18 -0700

    Split DoHandshake into DoHandshake and DoHandshakeComplete.
    The rv == 1 case was getting a little unwieldy. This should hopefully be
    simpler. DoHandshake converts the error to a net error code and decides which
    state to transition to. DoHandshakeComplete handles the result of the
    handshake.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1126113004
    
    Cr-Commit-Position: refs/heads/master@{#329518}

--- net/socket/ssl_client_socket_openssl.h
+++ net/socket/ssl_client_socket_openssl.h
@@ -114,6 +114,7 @@ class SSLClientSocketOpenSSL : public SSLClientSocket {
 
   bool DoTransportIO();
   int DoHandshake();
+  int DoHandshakeComplete(int result);
   int DoChannelIDLookup();
   int DoChannelIDLookupComplete(int result);
   int DoVerifyCert(int result);
@@ -280,6 +281,7 @@ class SSLClientSocketOpenSSL : public SSLClientSocket {
   enum State {
     STATE_NONE,
     STATE_HANDSHAKE,
+    STATE_HANDSHAKE_COMPLETE,
     STATE_CHANNEL_ID_LOOKUP,
     STATE_CHANNEL_ID_LOOKUP_COMPLETE,
     STATE_VERIFY_CERT,
