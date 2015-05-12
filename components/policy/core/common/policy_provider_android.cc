946328e5d19523a728f9539702e6467700642e7e
bauerb <bauerb@chromium.org>
2015-05-12 01:55:16 -0700

    Check that only a single delegate for the Android PolicyProvider exists.
    BUG=484997
    
    Review URL: https://codereview.chromium.org/1129323005
    
    Cr-Commit-Position: refs/heads/master@{#329378}

--- components/policy/core/common/policy_provider_android.cc
+++ components/policy/core/common/policy_provider_android.cc
@@ -33,6 +33,7 @@ void PolicyProviderAndroid::SetShouldWaitForPolicy(
 
 void PolicyProviderAndroid::SetDelegate(
     PolicyProviderAndroidDelegate* delegate) {
+  DCHECK(!delegate || !delegate_);
   delegate_ = delegate;
 }
 
