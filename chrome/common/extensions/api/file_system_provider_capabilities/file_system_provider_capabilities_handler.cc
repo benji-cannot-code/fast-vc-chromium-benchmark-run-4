642d1a797ba643a755c1807c9fed60e3f2c43f17
mtomasz <mtomasz@chromium.org>
2015-05-12 01:21:25 -0700

    Improve parsing manifest for FSP API.
    This CL makes the FSP section a prerequisite for the permission. It also adds
    a warning if a section is used without a permission.
    
    TEST=unit_tests: *FileSystemProviderHandlerTest*"
    BUG=474146
    
    Review URL: https://codereview.chromium.org/1133013002
    
    Cr-Commit-Position: refs/heads/master@{#329374}

--- chrome/common/extensions/api/file_system_provider_capabilities/file_system_provider_capabilities_handler.cc
+++ chrome/common/extensions/api/file_system_provider_capabilities/file_system_provider_capabilities_handler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/api/manifest_types.h"
 #include "extensions/common/error_utils.h"
 #include "extensions/common/manifest_constants.h"
+#include "extensions/common/manifest_handlers/permissions_parser.h"
 #include "url/gurl.h"
 
 namespace extensions {
@@ -48,10 +49,30 @@ const FileSystemProviderCapabilities* FileSystemProviderCapabilities::Get(
 
 bool FileSystemProviderCapabilitiesHandler::Parse(Extension* extension,
                                                   base::string16* error) {
-  const base::DictionaryValue* section = NULL;
+  const bool has_permission = extensions::PermissionsParser::HasAPIPermission(
+      extension, extensions::APIPermission::ID::kFileSystemProvider);
+  const base::DictionaryValue* section = nullptr;
   extension->manifest()->GetDictionary(
       manifest_keys::kFileSystemProviderCapabilities, &section);
-  DCHECK(section);
+
+  if (has_permission && !section) {
+    *error = base::ASCIIToUTF16(
+        manifest_errors::kInvalidFileSystemProviderMissingCapabilities);
+    return false;
+  }
+
+  if (!has_permission && section) {
+    // If the permission is not specified, then the section has simply no
+    // effect, hence just a warning.
+    extension->AddInstallWarning(extensions::InstallWarning(
+        manifest_errors::kInvalidFileSystemProviderMissingPermission));
+    return true;
+  }
+
+  if (!has_permission && !section) {
+    // No permission and no capabilities, so ignore.
+    return true;
+  }
 
   api::manifest_types::FileSystemProviderCapabilities idl_capabilities;
   if (!api::manifest_types::FileSystemProviderCapabilities::Populate(
@@ -87,6 +108,11 @@ bool FileSystemProviderCapabilitiesHandler::Parse(Extension* extension,
 const std::vector<std::string> FileSystemProviderCapabilitiesHandler::Keys()
     const {
   return SingleKey(manifest_keys::kFileSystemProviderCapabilities);
+}
+
+bool FileSystemProviderCapabilitiesHandler::AlwaysParseForType(
+    Manifest::Type type) const {
+  return true;
 }
 
 }  // namespace extensions
