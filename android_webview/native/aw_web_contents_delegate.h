d651fc9c902040d3f24caa28a0a201ea5b9b5747
mnaganov <mnaganov@chromium.org>
2015-05-12 08:07:17 -0700

    [Android WebView] Send WebChromeClient.onReceivedTitle when navigating back
    The usual channel for delivering onReceivedTitle (via updating title for
    a NavigationEntry) doesn't work in the case of history navigations, because
    WebContentsImpl silences no-op updates of entries titles.
    
    Chrome Browser uses another event for updating the title in this case --
    via WebContentsDelegate::LoadingStateChanged. This patch enables WebView to
    use it as well.
    
    Note that WebChromeClient.onReceivedIcon doesn't need to be re-sent, as
    nobody seems to bother checking whether the icon update is a "no-op", so
    updates on history navigations just work.
    
    BUG=481570
    
    Review URL: https://codereview.chromium.org/1126383005
    
    Cr-Commit-Position: refs/heads/master@{#329408}

--- android_webview/native/aw_web_contents_delegate.h
+++ android_webview/native/aw_web_contents_delegate.h
@@ -51,6 +51,8 @@ class AwWebContentsDelegate
 
   void CloseContents(content::WebContents* source) override;
   void ActivateContents(content::WebContents* contents) override;
+  void LoadingStateChanged(content::WebContents* source,
+                           bool to_different_document) override;
   void RequestMediaAccessPermission(
       content::WebContents* web_contents,
       const content::MediaStreamRequest& request,
