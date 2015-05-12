20b0a008aa423350afbfa7f387af04523b6ab12e
penghuang <penghuang@chromium.org>
2015-05-12 07:43:30 -0700

    cc: Render TextureLayer which texture target is not GL_TEXTURE_2D correctly.
    BUG=483203
    
    Review URL: https://codereview.chromium.org/1124203006
    
    Cr-Commit-Position: refs/heads/master@{#329403}

--- cc/output/gl_renderer_unittest.cc
+++ cc/output/gl_renderer_unittest.cc
@@ -124,12 +124,6 @@ class GLRendererShaderPixelTest : public GLRendererPixelTest {
   }
 
   void TestShadersWithPrecision(TexCoordPrecision precision) {
-    EXPECT_PROGRAM_VALID(renderer()->GetTextureProgram(precision));
-    EXPECT_PROGRAM_VALID(
-        renderer()->GetNonPremultipliedTextureProgram(precision));
-    EXPECT_PROGRAM_VALID(renderer()->GetTextureBackgroundProgram(precision));
-    EXPECT_PROGRAM_VALID(
-        renderer()->GetNonPremultipliedTextureBackgroundProgram(precision));
     EXPECT_PROGRAM_VALID(renderer()->GetTextureIOSurfaceProgram(precision));
     EXPECT_PROGRAM_VALID(renderer()->GetVideoYUVProgram(precision));
     EXPECT_PROGRAM_VALID(renderer()->GetVideoYUVAProgram(precision));
@@ -154,6 +148,15 @@ class GLRendererShaderPixelTest : public GLRendererPixelTest {
       // This will likely be hit in tests due to usage of osmesa.
       return;
     }
+
+    EXPECT_PROGRAM_VALID(renderer()->GetTextureProgram(precision, sampler));
+    EXPECT_PROGRAM_VALID(
+        renderer()->GetNonPremultipliedTextureProgram(precision, sampler));
+    EXPECT_PROGRAM_VALID(
+        renderer()->GetTextureBackgroundProgram(precision, sampler));
+    EXPECT_PROGRAM_VALID(
+        renderer()->GetNonPremultipliedTextureBackgroundProgram(precision,
+                                                                sampler));
 
     EXPECT_PROGRAM_VALID(renderer()->GetTileProgram(precision, sampler));
     EXPECT_PROGRAM_VALID(renderer()->GetTileProgramOpaque(precision, sampler));
