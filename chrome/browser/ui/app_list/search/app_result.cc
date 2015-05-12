9d52039b353b46491dacfe34b4a8ac86945c633a
mgiuca <mgiuca@chromium.org>
2015-05-11 22:36:06 -0700

    App Launcher: Webstore results are now highlighted.
    We now highlight (in bold) the part of the query that you typed, just as
    we do with other types of queries.
    
    Moved AppResult::UpdateFromMatch into SearchResult so it can be used by
    any provider.
    
    BUG=481837
    
    Review URL: https://codereview.chromium.org/1110883003
    
    Cr-Commit-Position: refs/heads/master@{#329356}

--- chrome/browser/ui/app_list/search/app_result.cc
+++ chrome/browser/ui/app_list/search/app_result.cc
@@ -24,8 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_icon_set.h"
 #include "extensions/common/manifest_handlers/icons_handler.h"
 #include "ui/app_list/app_list_switches.h"
-#include "ui/app_list/search/tokenized_string.h"
-#include "ui/app_list/search/tokenized_string_match.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/image/image_skia_operations.h"
 
@@ -64,20 +62,6 @@ AppResult::AppResult(Profile* profile,
 
 AppResult::~AppResult() {
   StopObservingExtensionRegistry();
-}
-
-void AppResult::UpdateFromMatch(const TokenizedString& title,
-                                const TokenizedStringMatch& match) {
-  const TokenizedStringMatch::Hits& hits = match.hits();
-
-  Tags tags;
-  tags.reserve(hits.size());
-  for (size_t i = 0; i < hits.size(); ++i)
-    tags.push_back(Tag(Tag::MATCH, hits[i].start(), hits[i].end()));
-
-  set_title(title.text());
-  set_title_tags(tags);
-  set_relevance(match.relevance());
 }
 
 void AppResult::UpdateFromLastLaunched(const base::Time& current_time,
@@ -130,6 +114,7 @@ scoped_ptr<SearchResult> AppResult::Duplicate() const {
                     display_type() == DISPLAY_RECOMMENDATION));
   copy->set_title(title());
   copy->set_title_tags(title_tags());
+  copy->set_relevance(relevance());
 
   return copy.Pass();
 }
