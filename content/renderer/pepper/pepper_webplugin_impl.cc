b068799bac3a5a9e83b9193ff5d15129be6974dc
trchen <trchen@chromium.org>
2015-05-11 19:01:31 -0700

    Fix WebViewPlugin::scheduleAnimation crash
    The crash was probably due to accessing a dangling pointer to the plugin
    container during a small time frame between PepperWebPluginImpl::destroy()
    and the destructor being called. (Speculated from source since no reliable
    repro is found.)
    
    This CL clears eveything in the destroy() function as if the destructor has
    been called, only delaying memory release.
    
    R=tommycli
    BUG=483068
    
    Review URL: https://codereview.chromium.org/1137663006
    
    Cr-Commit-Position: refs/heads/master@{#329309}

--- content/renderer/pepper/pepper_webplugin_impl.cc
+++ content/renderer/pepper/pepper_webplugin_impl.cc
@@ -125,6 +125,7 @@ void PepperWebPluginImpl::destroy() {
   // Tell |container_| to clear references to this plugin's script objects.
   if (container_)
     container_->clearScriptObjects();
+  container_ = nullptr;
 
   if (instance_.get()) {
     ppapi::PpapiGlobals::Get()->GetVarTracker()->ReleaseVar(instance_object_);
@@ -132,6 +133,7 @@ void PepperWebPluginImpl::destroy() {
     instance_->Delete();
     instance_ = NULL;
   }
+  throttler_.reset();
 
   base::MessageLoop::current()->DeleteSoon(FROM_HERE, this);
 }
