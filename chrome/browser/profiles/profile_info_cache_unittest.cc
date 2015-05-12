be4fcf89398c8bfa0ceb037f246ac979c87cfa7d
emaxx <emaxx@chromium.org>
2015-05-12 10:19:11 -0700

    Override user data directory to the temp for ProfileInfoCacheTest.
    Currently the tests of this suite (or, at least,
    DownloadHighResAvatarTest) read and write to the default system-wide
    location, which is not good and can cause tests flakiness.
    
    Now we override the DIR_USER_DATA to a temporary directory instead.
    
    BUG=486785
    
    Review URL: https://codereview.chromium.org/1138003004
    
    Cr-Commit-Position: refs/heads/master@{#329429}

--- chrome/browser/profiles/profile_info_cache_unittest.cc
+++ chrome/browser/profiles/profile_info_cache_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_avatar_icon_util.h"
 #include "chrome/browser/profiles/profile_info_cache.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/testing_browser_process.h"
@@ -89,7 +90,8 @@ ProfileInfoCache* ProfileNameVerifierObserver::GetCache() {
 
 ProfileInfoCacheTest::ProfileInfoCacheTest()
     : testing_profile_manager_(TestingBrowserProcess::GetGlobal()),
-      name_observer_(&testing_profile_manager_) {
+      name_observer_(&testing_profile_manager_),
+      user_data_dir_override_(chrome::DIR_USER_DATA) {
 }
 
 ProfileInfoCacheTest::~ProfileInfoCacheTest() {
@@ -557,7 +559,7 @@ TEST_F(ProfileInfoCacheTest, DownloadHighResAvatarTest) {
   ProfileInfoCache profile_info_cache(g_browser_process->local_state(),
       testing_profile_manager_.profile_manager()->user_data_dir());
 
-  // // Make sure there are no avatars already on disk.
+  // Make sure there are no avatars already on disk.
   const size_t kIconIndex = 0;
   base::FilePath icon_path =
       profiles::GetPathOfHighResAvatarAtIndex(kIconIndex);
