5f131b25c902eb8064d0fabc3a6790b376dac605
yukishiino@chromium.org <yukishiino@chromium.org>
2015-05-12 10:18:41 +0000

    Revert "bindings: Attempt to detect recursive call to installDOMWindow."
    This reverts commit 02b86af1477e381482dd40df38dadf112464452f.
    
    Assuming http://crrev.com/1098233003 fixed the crash issue,
    we no longer need this trap, I think.
    
    BUG=475880
    
    Review URL: https://codereview.chromium.org/1100413005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195234 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/WindowProxy.cpp
+++ third_party/WebKit/Source/bindings/core/v8/WindowProxy.cpp
@@ -63,7 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Assertions.h"
 #include "wtf/OwnPtr.h"
 #include "wtf/StringExtras.h"
-#include "wtf/TemporaryChange.h"
 #include "wtf/text/CString.h"
 #include <algorithm>
 #include <utility>
@@ -87,7 +86,6 @@ WindowProxy::WindowProxy(Frame* frame, PassRefPtr<DOMWrapperWorld> world, v8::Is
     : m_frame(frame)
     , m_isolate(isolate)
     , m_world(world)
-    , m_installingDOMWindow(false)
 {
 }
 
@@ -317,9 +315,6 @@ static v8::Local<v8::Object> toInnerGlobalObject(v8::Local<v8::Context> context)
 
 bool WindowProxy::installDOMWindow()
 {
-    RELEASE_ASSERT(!m_installingDOMWindow);
-    TemporaryChange<bool> installing(m_installingDOMWindow, true);
-
     DOMWindow* window = m_frame->domWindow();
     const WrapperTypeInfo* wrapperTypeInfo = window->wrapperTypeInfo();
     v8::Local<v8::Object> windowWrapper;
