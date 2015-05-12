4c4042c9485b1f6b01d738e4269e915c4171d59c
lushnikov@chromium.org <lushnikov@chromium.org>
2015-05-12 11:04:16 +0000

    DevTools: append uiSourceCode-applicable items to context menu of editor tab
    The patch extends TabbedPaneTabDelegate with onContextMenu
    method and fullfills the context menu from inside TabbedEditorContainer.
    
    BUG=481003
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/1128333004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195238 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/TabbedPane.js
+++ third_party/WebKit/Source/devtools/front_end/ui/TabbedPane.js
@@ -1003,10 +1003,9 @@ WebInspector.TabbedPaneTab.prototype = {
             tabElement.addEventListener("mousedown", this._tabMouseDown.bind(this), false);
             tabElement.addEventListener("mouseup", this._tabMouseUp.bind(this), false);
 
-            if (this._closeable) {
-                tabElement.addEventListener("contextmenu", this._tabContextMenu.bind(this), false);
+            tabElement.addEventListener("contextmenu", this._tabContextMenu.bind(this), false);
+            if (this._closeable)
                 WebInspector.installDragHandle(tabElement, this._startTabDragging.bind(this), this._tabDragging.bind(this), this._endTabDragging.bind(this), "pointer");
-            }
         }
 
         return tabElement;
@@ -1094,10 +1093,14 @@ WebInspector.TabbedPaneTab.prototype = {
         }
 
         var contextMenu = new WebInspector.ContextMenu(event);
-        contextMenu.appendItem(WebInspector.UIString.capitalize("Close"), close.bind(this));
-        contextMenu.appendItem(WebInspector.UIString.capitalize("Close ^others"), closeOthers.bind(this));
-        contextMenu.appendItem(WebInspector.UIString.capitalize("Close ^tabs to the ^right"), closeToTheRight.bind(this));
-        contextMenu.appendItem(WebInspector.UIString.capitalize("Close ^all"), closeAll.bind(this));
+        if (this._closeable) {
+            contextMenu.appendItem(WebInspector.UIString.capitalize("Close"), close.bind(this));
+            contextMenu.appendItem(WebInspector.UIString.capitalize("Close ^others"), closeOthers.bind(this));
+            contextMenu.appendItem(WebInspector.UIString.capitalize("Close ^tabs to the ^right"), closeToTheRight.bind(this));
+            contextMenu.appendItem(WebInspector.UIString.capitalize("Close ^all"), closeAll.bind(this));
+        }
+        if (this._delegate)
+            this._delegate.onContextMenu(this.id, contextMenu);
         contextMenu.show();
     },
 
@@ -1179,7 +1182,13 @@ WebInspector.TabbedPaneTabDelegate.prototype = {
      * @param {!WebInspector.TabbedPane} tabbedPane
      * @param {!Array.<string>} ids
      */
-    closeTabs: function(tabbedPane, ids) { }
+    closeTabs: function(tabbedPane, ids) { },
+
+    /**
+     * @param {string} tabId
+     * @param {!WebInspector.ContextMenu} contextMenu
+     */
+    onContextMenu: function(tabId, contextMenu) { }
 }
 
 /**
