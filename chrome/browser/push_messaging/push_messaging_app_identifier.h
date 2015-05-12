e55110a5dfd184d7c0d5144f220cbbfc2e8fa255
johnme <johnme@chromium.org>
2015-05-12 07:55:42 -0700

    Cleanup PushMessagingAppIdentifier
    In particular, the IsValid method had a confusing dual purpose - it was
    used both to sanity check the inputs, and to check whether the instances
    returned by the Get methods were null (i.e. Get failed). It has been
    split into is_null() and DCheckValid(). Note that having DCheckValid as
    a method also gives better stacktraces than the old DCHECK(IsValid())
    style, since stacktraces will indicate which check failed.
    
    BUG=458592
    
    Review URL: https://codereview.chromium.org/1131303002
    
    Cr-Commit-Position: refs/heads/master@{#329406}

--- chrome/browser/push_messaging/push_messaging_app_identifier.h
+++ chrome/browser/push_messaging/push_messaging_app_identifier.h
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PUSH_MESSAGING_PUSH_MESSAGING_APP_IDENTIFIER_H_
 #define CHROME_BROWSER_PUSH_MESSAGING_PUSH_MESSAGING_APP_IDENTIFIER_H_
 
+#include <stdint.h>
 #include <string>
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/logging.h"
 #include "url/gurl.h"
 
 class Profile;
@@ -21,9 +23,9 @@ class PrefRegistrySyncable;
 // The prefix used for all push messaging application ids.
 extern const char kPushMessagingAppIdentifierPrefix[];
 
-// Type used to identify a web app from a Push API perspective.
-// These can be persisted to disk, in a 1:1 mapping between app_id and
-// pair<origin, service_worker_registration_id>.
+// Type used to identify a Service Worker registration from a Push API
+// perspective. These can be persisted to prefs, in a 1:1 mapping between
+// app_id and pair<origin, service_worker_registration_id>.
 class PushMessagingAppIdentifier {
  public:
   // Register profile-specific prefs.
@@ -32,17 +34,17 @@ class PushMessagingAppIdentifier {
   // Generates a new app identifier with random app_id.
   static PushMessagingAppIdentifier Generate(
       const GURL& origin,
-      int64 service_worker_registration_id);
+      int64_t service_worker_registration_id);
 
-  // Looks up an app identifier by app_id. Will be invalid if not found.
+  // Looks up an app identifier by app_id. If not found, is_null() will be true.
   static PushMessagingAppIdentifier Get(Profile* profile,
                                         const std::string& app_id);
 
   // Looks up an app identifier by origin & service worker registration id.
-  // Will be invalid if not found.
+  // If not found, is_null() will be true.
   static PushMessagingAppIdentifier Get(Profile* profile,
                                         const GURL& origin,
-                                        int64 service_worker_registration_id);
+                                        int64_t service_worker_registration_id);
 
   // Returns all the PushMessagingAppIdentifiers currently registered for the
   // given |profile|.
@@ -50,17 +52,31 @@ class PushMessagingAppIdentifier {
 
   ~PushMessagingAppIdentifier();
 
-  // Persist this app identifier to disk.
-  void PersistToDisk(Profile* profile) const;
+  // Persist this app identifier to prefs.
+  void PersistToPrefs(Profile* profile) const;
 
-  // Delete this app identifier from disk.
-  void DeleteFromDisk(Profile* profile) const; // TODO: Does const make sense?
+  // Delete this app identifier from prefs.
+  void DeleteFromPrefs(Profile* profile) const;
 
-  bool IsValid() const;
+  // Returns true if this identifier does not represent an app (i.e. this was
+  // returned by a failed call to Get).
+  bool is_null() const { return service_worker_registration_id_ < 0; }
 
-  const std::string& app_id() const { return app_id_; }
-  const GURL& origin() const { return origin_; }
-  int64 service_worker_registration_id() const {
+  // String that should be passed to push services like GCM to identify a
+  // particular Service Worker (so we can route incoming messages). Example:
+  // wp:9CC55CCE-B8F9-4092-A364-3B0F73A3AB5F
+  const std::string& app_id() const {
+    DCHECK(!is_null());
+    return app_id_;
+  }
+
+  const GURL& origin() const {
+    DCHECK(!is_null());
+    return origin_;
+  }
+
+  int64_t service_worker_registration_id() const {
+    DCHECK(!is_null());
     return service_worker_registration_id_;
   }
 
@@ -72,11 +88,14 @@ class PushMessagingAppIdentifier {
   // Constructs a valid app identifier.
   PushMessagingAppIdentifier(const std::string& app_id,
                              const GURL& origin,
-                             int64 service_worker_registration_id);
+                             int64_t service_worker_registration_id);
+
+  // Validates that all the fields contain valid values.
+  void DCheckValid() const;
 
   std::string app_id_;
   GURL origin_;
-  int64 service_worker_registration_id_;
+  int64_t service_worker_registration_id_;
 };
 
 #endif  // CHROME_BROWSER_PUSH_MESSAGING_PUSH_MESSAGING_APP_IDENTIFIER_H_
--- chrome/browser/push_messaging/push_messaging_browsertest.cc
+++ chrome/browser/push_messaging/push_messaging_browsertest.cc
@@ -249,7 +249,7 @@ PushMessagingBrowserTest::GetAppIdentifierForServiceWorkerRegistration(
   GURL origin = https_server()->GetURL(std::string()).GetOrigin();
   PushMessagingAppIdentifier app_identifier = PushMessagingAppIdentifier::Get(
       GetBrowser()->profile(), origin, service_worker_registration_id);
-  EXPECT_TRUE(app_identifier.IsValid());
+  EXPECT_FALSE(app_identifier.is_null());
   return app_identifier;
 }
 
@@ -1039,7 +1039,7 @@ IN_PROC_BROWSER_TEST_F(PushMessagingBrowserTest,
   PushMessagingAppIdentifier stored_app_identifier =
       PushMessagingAppIdentifier::Get(GetBrowser()->profile(),
                                       app_identifier.app_id());
-  EXPECT_TRUE(stored_app_identifier.IsValid());
+  EXPECT_FALSE(stored_app_identifier.is_null());
 
   // Simulate a user clearing site data (including Service Workers, crucially).
   BrowsingDataRemover* remover =
@@ -1066,7 +1066,7 @@ IN_PROC_BROWSER_TEST_F(PushMessagingBrowserTest,
   PushMessagingAppIdentifier stored_app_identifier2 =
       PushMessagingAppIdentifier::Get(GetBrowser()->profile(),
                                       app_identifier.app_id());
-  EXPECT_FALSE(stored_app_identifier2.IsValid());
+  EXPECT_TRUE(stored_app_identifier2.is_null());
 }
 
 class PushMessagingIncognitoBrowserTest : public PushMessagingBrowserTest {
