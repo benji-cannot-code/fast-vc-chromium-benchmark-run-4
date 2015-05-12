5f5cb74edb5a706f2463997bb49ceea193f49180
dnicoara <dnicoara@chromium.org>
2015-05-12 12:45:23 -0700

    [Ozone-Drm] Refactor GPU display management
    This change decouples the DisplaySnapshot from the DrmDisplay since the
    snapshot is only needed to IPC the display information to the browser
    process. Also, move all the display configuration calls into the
    DrmDisplay object to simplify state and clarify the intent of the
    operations.
    
    This change also moves the logic to parse the DRM display state into
    DisplaySnapshot objects into a common location intended to be used by
    both GPU and Browser processes. The parsing logic will be used in a
    subsequent change to allow proper parsing of display configuration on
    the browser side during startup.
    
    BUG=484294
    
    Review URL: https://codereview.chromium.org/1129923004
    
    Cr-Commit-Position: refs/heads/master@{#329462}

--- ui/ozone/platform/drm/common/drm_util.h
+++ ui/ozone/platform/drm/common/drm_util.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/macros.h"
 #include "base/memory/scoped_vector.h"
+#include "ui/ozone/common/gpu/ozone_gpu_message_params.h"
 #include "ui/ozone/platform/drm/common/scoped_drm_types.h"
 
 typedef struct _drmModeModeInfo drmModeModeInfo;
@@ -39,6 +40,17 @@ ScopedVector<HardwareDisplayControllerInfo> GetAvailableDisplayControllerInfos(
     int fd);
 
 bool SameMode(const drmModeModeInfo& lhs, const drmModeModeInfo& rhs);
+
+DisplayMode_Params CreateDisplayModeParams(const drmModeModeInfo& mode);
+
+// |info| provides the DRM information related to the display, |fd| is the
+// connection to the DRM device and |index| provides a unique identifier for the
+// display. |index| will be used to generate the display id (it may be the id if
+// the monitor's EDID lacks the necessary identifiers).
+DisplaySnapshot_Params CreateDisplaySnapshotParams(
+    HardwareDisplayControllerInfo* info,
+    int fd,
+    size_t display_index);
 
 }  // namespace ui
 
