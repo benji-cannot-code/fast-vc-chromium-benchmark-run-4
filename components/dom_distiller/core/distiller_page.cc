1ba5228777744da8ccdef4a3b88beb9edcfd64d7
wychen <wychen@chromium.org>
2015-05-12 13:14:41 -0700

    Add UMA histogram DomDistiller.Time.DistillPage
    This measures time spent in DistillPageImpl(), which is mostly
    ExecuteJavaScript(). This includes JavaScript compilation and execution.
    
    We already have DomDistiller.Time.DistillationTotal, but it is measured
    within JavaScript.
    
    Review URL: https://codereview.chromium.org/1131493005
    
    Cr-Commit-Position: refs/heads/master@{#329472}

--- components/dom_distiller/core/distiller_page.cc
+++ components/dom_distiller/core/distiller_page.cc
@@ -89,6 +89,7 @@ void DistillerPage::DistillPage(
   // the callback to OnDistillationDone happens.
   ready_ = false;
   distiller_page_callback_ = callback;
+  distillation_start_ = base::TimeTicks::Now();
   DistillPageImpl(gurl, GetDistillerScriptWithOptions(options,
                                                       StringifyOutput(),
                                                       CreateNewContext()));
@@ -111,6 +112,11 @@ void DistillerPage::OnDistillationDone(const GURL& page_url,
     if (!found_content) {
       DVLOG(1) << "Unable to parse DomDistillerResult.";
     } else {
+      base::TimeDelta distillation_time =
+          base::TimeTicks::Now() - distillation_start_;
+      UMA_HISTOGRAM_TIMES("DomDistiller.Time.DistillPage", distillation_time);
+      VLOG(1) << "DomDistiller.Time.DistillPage = " << distillation_time;
+
       if (distiller_result->has_timing_info()) {
         const dom_distiller::proto::TimingInfo& timing =
             distiller_result->timing_info();
@@ -140,6 +146,8 @@ void DistillerPage::OnDistillationDone(const GURL& page_url,
           UMA_HISTOGRAM_TIMES(
               "DomDistiller.Time.DistillationTotal",
               base::TimeDelta::FromMillisecondsD(timing.total_time()));
+          VLOG(1) << "DomDistiller.Time.DistillationTotal = " <<
+              base::TimeDelta::FromMillisecondsD(timing.total_time());
         }
       }
       if (distiller_result->has_statistics_info()) {
