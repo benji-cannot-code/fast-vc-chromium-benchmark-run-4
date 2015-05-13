2c86dc9f247b97e05d28d083e9bb3ae157cf7867
tobiasjs <tobiasjs@chromium.org>
2015-05-13 09:13:45 -0700

    Use the wrapped context to inflate the selection menu.
    BUG=486734
    
    Review URL: https://codereview.chromium.org/1139743003
    
    Cr-Commit-Position: refs/heads/master@{#329648}

--- content/public/android/java/src/org/chromium/content/browser/SelectActionModeCallback.java
+++ content/public/android/java/src/org/chromium/content/browser/SelectActionModeCallback.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.graphics.Rect;
 import android.view.ActionMode;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -142,7 +143,7 @@ public class SelectActionModeCallback implements ActionMode.Callback {
     }
 
     private void createActionMenu(ActionMode mode, Menu menu) {
-        mode.getMenuInflater().inflate(R.menu.select_action_menu, menu);
+        new MenuInflater(getContext()).inflate(R.menu.select_action_menu, menu);
 
         if (mIsInsertion) {
             menu.removeItem(R.id.select_action_menu_select_all);
