66ef50eb963a0c81f4533de3464670c137980ffa
siva.gunturi <siva.gunturi@samsung.com>
2015-05-13 10:50:23 -0700

    Allow retrieval of updated bitmaps in thumbnails.
    When the thumbnails are updated using setBitmap
    and setCompressedBitmap, getbitmap should return
    newly updated bitmap instead of the placeholder.
    
    BUG=410658
    
    Review URL: https://codereview.chromium.org/1128113010
    
    Cr-Commit-Position: refs/heads/master@{#329672}

--- chrome/browser/android/thumbnail/thumbnail.cc
+++ chrome/browser/android/thumbnail/thumbnail.cc
@@ -55,6 +55,7 @@ Thumbnail::~Thumbnail() {
 
 void Thumbnail::SetBitmap(const SkBitmap& bitmap) {
   DCHECK(!bitmap.empty());
+  retrieved_ = false;
   ClearUIResourceId();
   scaled_content_size_ =
       gfx::ScaleSize(gfx::Size(bitmap.width(), bitmap.height()), 1.f / scale_);
@@ -66,6 +67,7 @@ void Thumbnail::SetCompressedBitmap(skia::RefPtr<SkPixelRef> compressed_bitmap,
                                     const gfx::Size& content_size) {
   DCHECK(compressed_bitmap);
   DCHECK(!content_size.IsEmpty());
+  retrieved_ = false;
   ClearUIResourceId();
   gfx::Size data_size(compressed_bitmap->info().width(),
                       compressed_bitmap->info().height());
