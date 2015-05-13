10c67bf347bd39c582f4072fec09b3d4840f95cc
melandory <melandory@chromium.org>
2015-05-13 07:28:56 -0700

    [Smart Lock] Prettify android:// URIs shown on "Passwords page" for Clank.
    Password setting page should show android URLs (e.g.
    android://hash@com.example.android/path) in human readable form (android://com.example.android/path) for both saved passwords and exceptions.
    
    BUG=486736
    
    Review URL: https://codereview.chromium.org/1140943002
    
    Cr-Commit-Position: refs/heads/master@{#329633}

--- chrome/browser/android/password_ui_view_android.cc
+++ chrome/browser/android/password_ui_view_android.cc
@@ -9,7 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_weak_ref.h"
 #include "base/command_line.h"
 #include "base/metrics/field_trial.h"
+#include "base/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/passwords/manage_passwords_view_utils.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "components/autofill/core/common/password_form.h"
 #include "components/password_manager/core/common/experiments.h"
@@ -74,9 +77,10 @@ PasswordUIViewAndroid::GetSavedPasswordEntry(JNIEnv* env, jobject, int index) {
         ConvertUTF8ToJavaString(env, std::string()).obj(),
         ConvertUTF16ToJavaString(env, base::string16()).obj());
   }
+  std::string human_readable_origin = GetHumanReadableOrigin(
+      *form, GetProfile()->GetPrefs()->GetString(prefs::kAcceptLanguages));
   return Java_PasswordUIView_createSavedPasswordEntry(
-      env,
-      ConvertUTF8ToJavaString(env, form->origin.spec()).obj(),
+      env, ConvertUTF8ToJavaString(env, human_readable_origin).obj(),
       ConvertUTF16ToJavaString(env, form->username_value).obj());
 }
 
@@ -86,7 +90,9 @@ ScopedJavaLocalRef<jstring> PasswordUIViewAndroid::GetSavedPasswordException(
       password_manager_presenter_.GetPasswordException(index);
   if (!form)
     return ConvertUTF8ToJavaString(env, std::string());
-  return ConvertUTF8ToJavaString(env, form->origin.spec());
+  std::string human_readable_origin = GetHumanReadableOrigin(
+      *form, GetProfile()->GetPrefs()->GetString(prefs::kAcceptLanguages));
+  return ConvertUTF8ToJavaString(env, human_readable_origin);
 }
 
 void PasswordUIViewAndroid::HandleRemoveSavedPasswordEntry(
