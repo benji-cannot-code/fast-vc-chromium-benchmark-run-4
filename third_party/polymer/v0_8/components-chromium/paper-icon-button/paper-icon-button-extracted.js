ad369dadd47dd1d98b34fe7aeea2fd14dd87887e
jlklein <jlklein@chromium.org>
2015-05-12 23:39:50 -0700

    Rerun reproduce.sh and pick up some missing elements with git add -f.
    BUG=
    
    Review URL: https://codereview.chromium.org/1134013004
    
    Cr-Commit-Position: refs/heads/master@{#329596}

---
+++ third_party/polymer/v0_8/components-chromium/paper-icon-button/paper-icon-button-extracted.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
+  Polymer({
+    is: 'paper-icon-button',
+
+    behaviors: [
+      Polymer.PaperButtonBehavior
+    ],
+
+    enableCustomStyleProperties: true,
+
+    properties: {
+      /**
+       * The URL of an image for the icon. If the src property is specified,
+       * the icon property should not be.
+       */
+      src: {
+        type: String
+      },
+
+      /**
+       * Specifies the icon name or index in the set of icons available in
+       * the icon's icon set. If the icon property is specified,
+       * the src property should not be.
+       */
+      icon: {
+        type: String
+      }
+    }
+  });
