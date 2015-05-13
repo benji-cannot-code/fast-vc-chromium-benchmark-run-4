0be1d398903e3b8640504e5d2f7c85194dab5e64
xunjieli <xunjieli@chromium.org>
2015-05-13 11:20:41 -0700

    [Cronet] Added a unit test in QuicTest to test async API.
    This CL removed the incorrect comment in QuicTest, and
    added a second unit test to make sure addQuicHints
    works for the new async API.
    
    It made QuicTestServer install test files during start up
    to match the new behavior of NativeTestServer, and adds an
    assertion to make sure the server has test data.
    
    The CL caches the result returned by
    CronetTestActivity#getContextConfig, so we don't
    need to go through that path for initializing
    mUrlRequestContext, mHttpUrlRequestFactory and
    mStreamHandlerFactory, and cleaned up log statements.
    
    Review URL: https://codereview.chromium.org/1138493002
    
    Cr-Commit-Position: refs/heads/master@{#329678}

--- components/cronet/android/test/quic_test_server.cc
+++ components/cronet/android/test/quic_test_server.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/bind.h"
 #include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "base/threading/thread.h"
 #include "jni/QuicTestServer_jni.h"
 #include "net/base/ip_endpoint.h"
@@ -31,6 +32,7 @@ void ServeFilesFromDirectory(
   DCHECK(g_quic_server_thread->task_runner()->BelongsToCurrentThread());
   DCHECK(!g_quic_server);
   base::FilePath file_dir = directory.Append("quic_data");
+  CHECK(base::PathExists(file_dir)) << "Quic data does not exist";
   net::tools::QuicInMemoryCache::GetInstance()->InitializeFromDirectory(
       file_dir.value());
   net::IPAddressNumber ip;
--- components/cronet/android/test/src/org/chromium/net/QuicTestServer.java
+++ components/cronet/android/test/src/org/chromium/net/QuicTestServer.java
@@ -10,6 +10,7 @@ import android.os.ConditionVariable;
 
 import org.chromium.base.CalledByNative;
 import org.chromium.base.JNINamespace;
+import org.chromium.base.Log;
 
 /**
  * Wrapper class to start a Quic test server.
@@ -17,8 +18,10 @@ import org.chromium.base.JNINamespace;
 @JNINamespace("cronet")
 public final class QuicTestServer {
     private static final ConditionVariable sBlock = new ConditionVariable();
+    private static final String TAG = Log.makeTag("QuicTestServer");
 
     public static void startQuicTestServer(Context context) {
+        TestFilesInstaller.installIfNeeded(context);
         nativeStartQuicTestServer(TestFilesInstaller.getInstalledPath(context));
         sBlock.block();
     }
@@ -42,6 +45,7 @@ public final class QuicTestServer {
 
     @CalledByNative
     private static void onServerStarted() {
+        Log.i(TAG, "Quic server started.");
         sBlock.open();
     }
 
