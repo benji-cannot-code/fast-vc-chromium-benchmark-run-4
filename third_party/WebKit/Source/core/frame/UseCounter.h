990daa5fa410ce3faf4d7c7f115bb2a2cd8baf2f
rtoy@chromium.org <rtoy@chromium.org>
2015-05-14 22:27:17 +0000

    Use correct use counter for StereoPanner.
    Fix the typo where the use counter for StereoPannerNode was using the
    counter for PannerNode.
    
    BUG=487651
    TEST=none
    
    Review URL: https://codereview.chromium.org/1140953002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195380 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -564,6 +564,7 @@ public:
         AudioContextCreatePanner = 644,
         AudioContextCreatePeriodicWave = 645,
         AudioContextCreateScriptProcessor = 646,
+        AudioContextCreateStereoPanner = 647,
         AudioContextCreateWaveShaper = 648,
         AudioContextDecodeAudioData = 649,
         AudioContextResume = 650,
