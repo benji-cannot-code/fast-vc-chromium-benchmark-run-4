f11d367ba0e4acbbeb8ee9e59b6146bf750de9d4
fgorski <fgorski@chromium.org>
2015-05-13 17:05:22 -0700

    [GCM] Wiring heartbeat interval calls to GCMDriver
    Wires the call to Add/RemoveHeartbeatInterval through:
    * GCMDriver/Desktop/Android/Fake
    * GCMDriverDesktop::IOWorker
    * GCMClient/Impl to MCSClient
    * Adding loading of persisted interval in MCSClient
    
    BUG=481174
    
    Review URL: https://codereview.chromium.org/1124783002
    
    Cr-Commit-Position: refs/heads/master@{#329750}

--- google_apis/gcm/engine/mcs_client.h
+++ google_apis/gcm/engine/mcs_client.h
@@ -161,8 +161,8 @@ class GCM_EXPORT MCSClient {
   void AddHeartbeatInterval(const std::string& scope, int interval_ms);
   void RemoveHeartbeatInterval(const std::string& scope);
 
-  HeartbeatManager& GetHeartbeatManagerForTesting() {
-    return heartbeat_manager_;
+  HeartbeatManager* GetHeartbeatManagerForTesting() {
+    return &heartbeat_manager_;
   }
 
  private:
@@ -226,8 +226,9 @@ class GCM_EXPORT MCSClient {
   // any associated state).
   MCSPacketInternal PopMessageForSend();
 
-  // Gets the minimum interval from the map of scopes to intervals.
-  int GetMinCustomHeartbeatInterval();
+  // Gets the minimum interval from the map of scopes to intervals in
+  // milliseconds.
+  int GetMinHeartbeatIntervalMs();
 
   // Local version string. Sent on login.
   const std::string version_string_;
