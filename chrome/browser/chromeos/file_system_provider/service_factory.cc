9176a7a69f27d430725396c36e9d17303b6cacf2
mtomasz <mtomasz@chromium.org>
2015-05-13 20:33:49 -0700

    Enable file_system_provider::Service in incognito.
    There is no need to limit the FSP API to non-incognito only. Especially, such
    limitation was causing crashes in Files app running in incognito, which
    assumed the service exists, but it didn't.
    
    With this CL the service is created for both non-incognito and incognito pages.
    Both instances are *same*, so mounted providers will be shared between incognito
    and non-incognito windows.
    
    TEST=Tested manually by launching with --incognito
    BUG=487901
    
    Review URL: https://codereview.chromium.org/1138423002
    
    Cr-Commit-Position: refs/heads/master@{#329792}

--- chrome/browser/chromeos/file_system_provider/service_factory.cc
+++ chrome/browser/chromeos/file_system_provider/service_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/file_system_provider/service.h"
 #include "chrome/browser/extensions/extension_system_factory.h"
+#include "chrome/browser/profiles/incognito_helpers.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_registry_factory.h"
@@ -48,6 +49,11 @@ KeyedService* ServiceFactory::BuildServiceInstanceFor(
 }
 
 bool ServiceFactory::ServiceIsCreatedWithBrowserContext() const { return true; }
+
+content::BrowserContext* ServiceFactory::GetBrowserContextToUse(
+    content::BrowserContext* context) const {
+  return chrome::GetBrowserContextRedirectedInIncognito(context);
+}
 
 }  // namespace file_system_provider
 }  // namespace chromeos
