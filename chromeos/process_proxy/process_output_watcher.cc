63b9ef1d131c112ae756776fbd85ccdfe0c74e10
tbarzic <tbarzic@chromium.org>
2015-05-14 12:40:37 -0700

    Prevent ProcessProxy from passing bad fds to ProcessOutputWatcher
    This makes crosh process creation (used by terminalPrivate API) fail
    gracefully when file descriptors used to observe process output
    cannot be used by select (as opposed to crashing).
    
    BUG=480616
    TEST=Open huge number of crosh terminals in short time, until terminal
    launching starts failing.
    
    Review URL: https://codereview.chromium.org/1135823007
    
    Cr-Commit-Position: refs/heads/master@{#329901}

--- chromeos/process_proxy/process_output_watcher.cc
+++ chromeos/process_proxy/process_output_watcher.cc
@@ -56,6 +56,11 @@ size_t UTF8SizeFromLeadingByte(uint8 leading_byte) {
 
 namespace chromeos {
 
+// static
+bool ProcessOutputWatcher::VerifyFileDescriptor(int fd) {
+  return (fd >= 0) && (fd < FD_SETSIZE);
+}
+
 ProcessOutputWatcher::ProcessOutputWatcher(
     int out_fd,
     int stop_fd,
@@ -64,8 +69,8 @@ ProcessOutputWatcher::ProcessOutputWatcher(
       out_fd_(out_fd),
       stop_fd_(stop_fd),
       on_read_callback_(callback) {
-  VerifyFileDescriptor(out_fd_);
-  VerifyFileDescriptor(stop_fd_);
+  CHECK(VerifyFileDescriptor(out_fd_));
+  CHECK(VerifyFileDescriptor(stop_fd_));
   max_fd_ = std::max(out_fd_, stop_fd_);
   // We want to be sure we will be able to add 0 at the end of the input, so -1.
   read_buffer_capacity_ = arraysize(read_buffer_) - 1;
@@ -105,11 +110,6 @@ void ProcessOutputWatcher::WatchProcessOutput() {
       ReadFromFd(PROCESS_OUTPUT_TYPE_OUT, &out_fd_);
     }
   }
-}
-
-void ProcessOutputWatcher::VerifyFileDescriptor(int fd) {
-  CHECK_LE(0, fd);
-  CHECK_GT(FD_SETSIZE, fd);
 }
 
 void ProcessOutputWatcher::ReadFromFd(ProcessOutputType type, int* fd) {
