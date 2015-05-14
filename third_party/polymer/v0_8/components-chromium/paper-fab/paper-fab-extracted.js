05ec47178c00e209fcb02471e408e8f1980b4205
tsergeant <tsergeant@chromium.org>
2015-05-13 19:48:37 -0700

    Material PDF: Add required Polymer 0.8 elements.
    Adds paper-fab, paper-toolbar and polymer/layout, which are required by
    the material PDF viewer. Also adds required files from existing
    components to polymer_resources.
    
    BUG=482793
    
    Review URL: https://codereview.chromium.org/1140583003
    
    Cr-Commit-Position: refs/heads/master@{#329787}

---
+++ third_party/polymer/v0_8/components-chromium/paper-fab/paper-fab-extracted.js
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
+  Polymer({
+    is: 'paper-fab',
+
+    enableCustomStyleProperties: true,
+
+    behaviors: [
+      Polymer.PaperButtonBehavior
+    ],
+
+    properties: {
+      /**
+       * The URL of an image for the icon. If the src property is specified,
+       * the icon property should not be.
+       *
+       * @attribute src
+       * @type string
+       * @default ''
+       */
+      src: {
+        type: String,
+        value: ''
+      },
+
+      /**
+       * Specifies the icon name or index in the set of icons available in
+       * the icon's icon set. If the icon property is specified,
+       * the src property should not be.
+       *
+       * @attribute icon
+       * @type string
+       * @default ''
+       */
+      icon: {
+        type: String,
+        value: ''
+      },
+
+      /**
+       * Set this to true to style this is a "mini" FAB.
+       *
+       * @attribute mini
+       * @type boolean
+       * @default false
+       */
+      mini: {
+        type: Boolean,
+        value: false
+      }
+    }
+  });
