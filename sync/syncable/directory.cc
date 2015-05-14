cd7d644a0867f436d8455d5afd15faafa40ee684
stanisc <stanisc@chromium.org>
2015-05-14 15:16:01 -0700

    Sync: Change Local IDs to GUID based to avoid ID collision
    This changes implementation of Directory::NextId() to no longer depend on
    "next_id" counter stored in Sync DB. Even though there was a special precaution
    to avoid potential collision of local IDs by fast forwarding next_id by a large
    number on every save there are still a couple scenarios leading the the collision
    between a local ID generated on a client and originator_client_item_id coming
    from the server (which is basically a local ID of an item previously committed
    to the server.
    
    The new implementation of Directory::NextId() is based on base::GenerateGUID.
    The following changes accompany the transition to the new ID format:
    1) The next_id field is no longer needed in Directory::PersistedKernelInfo
    2) The code that persists next_id is no longer needed.
    3) Forcing the directory to save by marking it with KERNEL_SHARE_INFO_DIRTY
        flag right after loading it is no longer necessary. That was done primarily
        to increment next_id by a large value as a way to attempt this problem.
    4) Fixing #3 revealed a bug in Directory::IncrementTransactionVersion that
        doesn't mark the directory with KERNEL_SHARE_INFO_DIRTY when incrementing
        a datatype's transaction version. That caused a number of bookmark tests
        to fail due to the transaction version check. Fixed it.
    5) Changing local IDs to GUIDs had a subtle effect on some tests. For the
        types that don't support ordering, sorting of entries in ParentChildIndex
        defaults to ID comparison. Since generation of local IDs base on next_id
        followed a predictable pattern, some tests relied on order of items returned
        by ParentChildIndex. Switching to GUIDs disrupted the expected order
        (of entries that are unordered in their nature). To preserve the existing
        ordering pattern for unordered entries, I changed ParentChildIndex to
        default to META_HANDLE comparison.
    6) Another test that failed due to the GUID transition was
        SyncableDirectoryTest.BookmarkTagTest. The test relied on a predefined,
        repeatable local ID which was no longer the case with the GUID generation.
        I found the test to be redundant. There is another tests that verifies
        the same functionality - unique bookmark tag generation algorithm.
    
    For now the change leaves next_id in the database even though the field is
    no longer used. I should probably file another bug to track its removal.
    
    BUG=362467
    
    Review URL: https://codereview.chromium.org/1136953013
    
    Cr-Commit-Position: refs/heads/master@{#329946}

--- sync/syncable/directory.cc
+++ sync/syncable/directory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iterator>
 
 #include "base/base64.h"
+#include "base/guid.h"
 #include "base/metrics/histogram.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
@@ -38,8 +39,7 @@ namespace syncable {
 const base::FilePath::CharType Directory::kSyncDatabaseFilename[] =
     FILE_PATH_LITERAL("SyncData.sqlite3");
 
-Directory::PersistedKernelInfo::PersistedKernelInfo()
-    : next_id(0) {
+Directory::PersistedKernelInfo::PersistedKernelInfo() {
   ModelTypeSet protocol_types = ProtocolTypes();
   for (ModelTypeSet::Iterator iter = protocol_types.First(); iter.Good();
        iter.Inc()) {
@@ -198,15 +198,11 @@ DirOpenResult Directory::OpenImpl(
 
   DCHECK(!kernel_);
   kernel_ = new Kernel(name, info, delegate, transaction_observer);
+  kernel_->metahandles_to_purge.swap(metahandles_to_purge);
   delete_journal_.reset(new DeleteJournal(&delete_journals));
   InitializeIndices(&tmp_handles_map);
 
-  // Write back the share info to reserve some space in 'next_id'.  This will
-  // prevent local ID reuse in the case of an early crash.  See the comments in
-  // TakeSnapshotForSaveChanges() or crbug.com/142987 for more information.
-  kernel_->info_status = KERNEL_SHARE_INFO_DIRTY;
-
-  kernel_->metahandles_to_purge.swap(metahandles_to_purge);
+  // Save changes back in case there are any metahandles to purge.
   if (!SaveChanges())
     return FAILED_INITIAL_WRITE;
 
@@ -576,11 +572,6 @@ void Directory::TakeSnapshotForSaveChanges(SaveChangesSnapshot* snapshot) {
 
   // Fill kernel_info_status and kernel_info.
   snapshot->kernel_info = kernel_->persisted_info;
-  // To avoid duplicates when the process crashes, we record the next_id to be
-  // greater magnitude than could possibly be reached before the next save
-  // changes.  In other words, it's effectively impossible for the user to
-  // generate 65536 new bookmarks in 3 seconds.
-  snapshot->kernel_info.next_id -= 65536;
   snapshot->kernel_info_status = kernel_->info_status;
   // This one we reset on failure.
   kernel_->info_status = KERNEL_SHARE_INFO_VALID;
@@ -956,6 +947,7 @@ int64 Directory::GetTransactionVersion(ModelType type) const {
 void Directory::IncrementTransactionVersion(ModelType type) {
   kernel_->transaction_mutex.AssertAcquired();
   kernel_->persisted_info.transaction_version[type]++;
+  kernel_->info_status = KERNEL_SHARE_INFO_DIRTY;
 }
 
 void Directory::GetDataTypeContext(BaseTransaction* trans,
@@ -1340,17 +1332,9 @@ int64 Directory::NextMetahandle() {
   return metahandle;
 }
 
-// Always returns a client ID that is the string representation of a negative
-// number.
+// Generates next client ID based on a randomly generated GUID.
 Id Directory::NextId() {
-  int64 result;
-  {
-    ScopedKernelLock lock(this);
-    result = (kernel_->persisted_info.next_id)--;
-    kernel_->info_status = KERNEL_SHARE_INFO_DIRTY;
-  }
-  DCHECK_LT(result, 0);
-  return Id::CreateFromClientString(base::Int64ToString(result));
+  return Id::CreateFromClientString(base::GenerateGUID());
 }
 
 bool Directory::HasChildren(BaseTransaction* trans, const Id& id) {
