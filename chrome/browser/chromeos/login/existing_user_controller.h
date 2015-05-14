bac07535c8d8c8038d2a719119fba563f8ecfebb
antrim <antrim@chromium.org>
2015-05-14 03:59:55 -0700

    Disable token handle fetching on some devices
    BUG=484690
    R=nkostylev@chromium.org
    
    Review URL: https://codereview.chromium.org/1138883003
    
    Cr-Commit-Position: refs/heads/master@{#329828}

--- chrome/browser/chromeos/login/existing_user_controller.h
+++ chrome/browser/chromeos/login/existing_user_controller.h
@@ -248,6 +248,9 @@ class ExistingUserController : public LoginDisplay::Delegate,
   void OnTokenHandleObtained(const user_manager::UserID& id,
                              TokenHandleUtil::TokenHandleStatus status);
 
+  // Returns |true| if token handles should be used on this device.
+  bool TokenHandlesEnabled();
+
   // Public session auto-login timer.
   scoped_ptr<base::OneShotTimer<ExistingUserController> > auto_login_timer_;
 
