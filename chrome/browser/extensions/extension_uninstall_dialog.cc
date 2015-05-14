500faa5d01200af1938541c7321c56074ef084b0
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-05-14 12:48:34 -0700

    [Extensions] Enable the "Report Abuse" checkbox in extension uninstall
    After talking it over more, it's been decided that this should really just be
    enabled by default, rather than Finch'd.
    
    BUG=441377
    
    Review URL: https://codereview.chromium.org/1131113003
    
    Cr-Commit-Position: refs/heads/master@{#329908}

--- chrome/browser/extensions/extension_uninstall_dialog.cc
+++ chrome/browser/extensions/extension_uninstall_dialog.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/logging.h"
 #include "base/message_loop/message_loop.h"
-#include "base/metrics/field_trial.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/extensions/extension_util.h"
@@ -132,9 +131,7 @@ std::string ExtensionUninstallDialog::GetHeadingText() {
 }
 
 bool ExtensionUninstallDialog::ShouldShowReportAbuseCheckbox() const {
-  return ManifestURL::UpdatesFromGallery(extension_) &&
-      base::FieldTrialList::FindFullName("ExtensionUninstall.ReportAbuse") ==
-          "ShowCheckbox";
+  return ManifestURL::UpdatesFromGallery(extension_);
 }
 
 void ExtensionUninstallDialog::OnDialogClosed(CloseAction action) {
