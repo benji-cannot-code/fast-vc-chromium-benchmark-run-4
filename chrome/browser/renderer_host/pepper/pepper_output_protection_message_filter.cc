16cf9b9cdda5c08933c7c3211c3e03f42be81ec3
anujk.sharma <anujk.sharma@samsung.com>
2015-05-14 13:25:18 -0700

    favor DCHECK_CURRENTLY_ON for better logs in chrome/browser/renderer_host
    BUG=466848
    
    Review URL: https://codereview.chromium.org/1100293004
    
    Cr-Commit-Position: refs/heads/master@{#329918}

--- chrome/browser/renderer_host/pepper/pepper_output_protection_message_filter.cc
+++ chrome/browser/renderer_host/pepper/pepper_output_protection_message_filter.cc
@@ -67,7 +67,7 @@ static_assert(static_cast<int>(PP_OUTPUT_PROTECTION_METHOD_PRIVATE_HDCP) ==
                "PP_OUTPUT_PROTECTION_METHOD_PRIVATE_HDCP value mismatch");
 
 bool GetCurrentDisplayId(content::RenderFrameHost* rfh, int64* display_id) {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   gfx::NativeView native_view = rfh->GetNativeView();
   gfx::Screen* screen = gfx::Screen::GetScreenFor(native_view);
   if (!screen)
@@ -145,11 +145,11 @@ PepperOutputProtectionMessageFilter::Delegate::Delegate(int render_process_id,
       client_id_(ui::DisplayConfigurator::kInvalidClientId),
       display_id_(0),
       weak_ptr_factory_(this) {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
 }
 
 PepperOutputProtectionMessageFilter::Delegate::~Delegate() {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   ui::DisplayConfigurator* configurator =
       ash::Shell::GetInstance()->display_configurator();
@@ -161,7 +161,7 @@ PepperOutputProtectionMessageFilter::Delegate::~Delegate() {
 
 ui::DisplayConfigurator::ContentProtectionClientId
 PepperOutputProtectionMessageFilter::Delegate::GetClientId() {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   if (client_id_ == ui::DisplayConfigurator::kInvalidClientId) {
     content::RenderFrameHost* rfh =
         content::RenderFrameHost::FromID(render_process_id_, render_frame_id_);
@@ -184,7 +184,7 @@ PepperOutputProtectionMessageFilter::Delegate::GetClientId() {
 
 void PepperOutputProtectionMessageFilter::Delegate::QueryStatus(
     const QueryStatusCallback& callback) {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   content::RenderFrameHost* rfh =
       content::RenderFrameHost::FromID(render_process_id_, render_frame_id_);
@@ -206,7 +206,7 @@ void PepperOutputProtectionMessageFilter::Delegate::QueryStatus(
 void PepperOutputProtectionMessageFilter::Delegate::EnableProtection(
     uint32_t desired_method_mask,
     const EnableProtectionCallback& callback) {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   ui::DisplayConfigurator* configurator =
       ash::Shell::GetInstance()->display_configurator();
@@ -295,7 +295,7 @@ PepperOutputProtectionMessageFilter::PepperOutputProtectionMessageFilter(
     PP_Instance instance)
     : weak_ptr_factory_(this) {
 #if defined(OS_CHROMEOS)
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
   int render_process_id = 0;
   int render_frame_id = 0;
   host->GetRenderFrameIDsForInstance(
--- chrome/browser/renderer_host/pepper/pepper_platform_verification_message_filter.cc
+++ chrome/browser/renderer_host/pepper/pepper_platform_verification_message_filter.cc
@@ -41,7 +41,7 @@ PepperPlatformVerificationMessageFilter::OverrideTaskRunnerForMessage(
 int32_t PepperPlatformVerificationMessageFilter::OnResourceMessageReceived(
     const IPC::Message& msg,
     ppapi::host::HostMessageContext* context) {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   PPAPI_BEGIN_MESSAGE_MAP(PepperPlatformVerificationMessageFilter, msg)
     PPAPI_DISPATCH_HOST_RESOURCE_CALL(
@@ -56,7 +56,7 @@ int32_t PepperPlatformVerificationMessageFilter::OnChallengePlatform(
     ppapi::host::HostMessageContext* context,
     const std::string& service_id,
     const std::vector<uint8_t>& challenge) {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   // Ensure the RenderFrameHost is still alive.
   content::RenderFrameHost* rfh =
@@ -93,7 +93,7 @@ void PepperPlatformVerificationMessageFilter::ChallengePlatformCallback(
     const std::string& signed_data,
     const std::string& signature,
     const std::string& platform_key_certificate) {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   if (challenge_result == PlatformVerificationFlow::SUCCESS) {
     reply_context.params.set_result(PP_OK);
