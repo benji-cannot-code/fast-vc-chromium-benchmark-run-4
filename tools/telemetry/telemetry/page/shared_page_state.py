a22bb8917ba9b5dc29c1040786fd948bc13b2692
nednguyen <nednguyen@google.com>
2015-05-14 11:56:32 -0700

    [Telemetry] Remove results param in shared_state.TearDownState().
    The results param was needed in shared_state.TearDownState() because of
    page_test.DidRunTest hook. Now that hook is removed, we no longer need to
    provide access to the results object in this method.
    
    BUG=435757
    
    Review URL: https://codereview.chromium.org/1140143002
    
    Cr-Commit-Position: refs/heads/master@{#329887}

--- tools/telemetry/telemetry/page/shared_page_state.py
+++ tools/telemetry/telemetry/page/shared_page_state.py
@@ -268,7 +268,7 @@ class SharedPageState(shared_state.SharedState):
         raise page_test.MultiTabTestAppCrashError
       raise
 
-  def TearDownState(self, results):
+  def TearDownState(self):
     self._StopBrowser()
 
   def _StopBrowser(self):
