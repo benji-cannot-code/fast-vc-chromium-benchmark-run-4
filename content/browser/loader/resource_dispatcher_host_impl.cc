cc3fc3b11a45124e3c4e9a6227da52ccc900ae3f
rdsmith <rdsmith@chromium.org>
2015-05-14 09:36:42 -0700

    Create histograms to track time-to-error values for different errors.
    This is only done for main frame loads (so that it matches the behavior
    of Net.ErrorCodesForMainFrame3), and is done for the dominant errors
    seen in the wild.  It also includes successful loads (for comparison) and all
    non-dominant errors in a final bucket (for completeness).
    
    BUG= 487663
    R=mmenke@chromium.org
    R=jkarlin@chromium.org
    
    Review URL: https://codereview.chromium.org/1127383009
    
    Cr-Commit-Position: refs/heads/master@{#329858}

--- content/browser/loader/resource_dispatcher_host_impl.cc
+++ content/browser/loader/resource_dispatcher_host_impl.cc
@@ -20,11 +20,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/shared_memory.h"
 #include "base/message_loop/message_loop.h"
-#include "base/metrics/histogram.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/metrics/sparse_histogram.h"
 #include "base/profiler/scoped_tracker.h"
 #include "base/stl_util.h"
 #include "base/third_party/dynamic_annotations/dynamic_annotations.h"
+#include "base/time/time.h"
 #include "content/browser/appcache/appcache_interceptor.h"
 #include "content/browser/appcache/chrome_appcache_service.h"
 #include "content/browser/cert_store_impl.h"
@@ -862,6 +863,45 @@ void ResourceDispatcherHostImpl::DidFinishLoading(ResourceLoader* loader) {
     UMA_HISTOGRAM_SPARSE_SLOWLY(
         "Net.ErrorCodesForMainFrame3",
         -loader->request()->status().error());
+
+    // Record time to success and error for the most common errors, and for
+    // the aggregate remainder errors.
+    base::TimeDelta request_loading_time(
+        base::Time::Now() - loader->request()->request_time());
+    switch (loader->request()->status().error()) {
+      case net::OK:
+        UMA_HISTOGRAM_LONG_TIMES(
+            "Net.RequestTime.Success", request_loading_time);
+        break;
+      case net::ERR_ABORTED:
+        UMA_HISTOGRAM_LONG_TIMES(
+            "Net.RequestTime.ErrAborted", request_loading_time);
+        break;
+      case net::ERR_CONNECTION_RESET:
+        UMA_HISTOGRAM_LONG_TIMES(
+            "Net.RequestTime.ErrConnectionReset", request_loading_time);
+        break;
+      case net::ERR_CONNECTION_TIMED_OUT:
+        UMA_HISTOGRAM_LONG_TIMES(
+            "Net.RequestTime.ErrConnectionTimedOut", request_loading_time);
+        break;
+      case net::ERR_INTERNET_DISCONNECTED:
+        UMA_HISTOGRAM_LONG_TIMES(
+            "Net.RequestTime.ErrInternetDisconnected", request_loading_time);
+        break;
+      case net::ERR_NAME_NOT_RESOLVED:
+        UMA_HISTOGRAM_LONG_TIMES(
+            "Net.RequestTime.ErrNameNotResolved", request_loading_time);
+        break;
+      case net::ERR_TIMED_OUT:
+        UMA_HISTOGRAM_LONG_TIMES(
+            "Net.RequestTime.ErrTimedOut", request_loading_time);
+        break;
+      default:
+        UMA_HISTOGRAM_LONG_TIMES(
+            "Net.RequestTime.MiscError", request_loading_time);
+        break;
+    }
 
     if (loader->request()->url().SchemeIsCryptographic()) {
       if (loader->request()->url().host() == "www.google.com") {
