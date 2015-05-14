b9e2d25e7c5d0de2be866df4ac385c8ffbe58786
sergeyv <sergeyv@chromium.org>
2015-05-14 04:59:11 -0700

    Revert of Revert of Move SecurityLevel into a class of its own (patchset #1 id:1 of https://codereview.chromium.org/1135373003/)
    Reason for revert:
    Reland initial patch. There is a fix for SecurityLevelTest:
    
    https://codereview.chromium.org/1135073003
    
    Original issue's description:
    > Revert of Move SecurityLevel into a class of its own (patchset #19 id:420001 of https://codereview.chromium.org/1123943002/)
    >
    > Reason for revert:
    > Android Tests dbg bot is failing:
    > http://build.chromium.org/p/chromium.linux/builders/Android%20Tests%20(dbg)
    >
    > Failure example: http://build.chromium.org/p/chromium.linux/builders/Android%20Tests%20%28dbg%29/builds/27901
    >
    > Original issue's description:
    > > Move SecurityLevel into a class of its own
    > >
    > > This CL moves |GetSecurityLevelForWebContents| and the |SecurityLevel|
    > > enum into a class of their own: |ConnectionSecurityHelper| in
    > > //chrome/browser/ssl (since it is applying Chrome-specific security
    > > policy to determine the security level of a page).
    > >
    > > Later, we will use the new |ConnectionSecurityHelper| class to compute a
    > > security level for a page to send to the DevTools Security
    > > panel. Specifically, we need to be able to calculate the security level
    > > for a given page, not just the WebContents for the active tab.
    > >
    > > BUG=484392
    > >
    > > Committed: https://crrev.com/ca7c0a3374c21ab10c821f4f3a890afc79182acf
    > > Cr-Commit-Position: refs/heads/master@{#329591}
    > >
    > > Committed: https://crrev.com/f50c9b4834e151aa6b5538545a503614cb74f5f9
    > > Cr-Commit-Position: refs/heads/master@{#329724}
    >
    > TBR=felt@chromium.org,pkasting@chromium.org,rsleevi@chromium.org,bauerb@chromium.org,sky@chromium.org,tedchoc@chromium.org,estark@chromium.org
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=484392
    >
    > Committed: https://crrev.com/39722bb693112be3d91c729246c0ade351e7e900
    > Cr-Commit-Position: refs/heads/master@{#329827}
    
    TBR=felt@chromium.org,pkasting@chromium.org,rsleevi@chromium.org,bauerb@chromium.org,sky@chromium.org,tedchoc@chromium.org,estark@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=484392
    
    Review URL: https://codereview.chromium.org/1124383007
    
    Cr-Commit-Position: refs/heads/master@{#329832}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/ssl/ConnectionSecurityHelper.java
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ssl;
+
+import org.chromium.content_public.browser.WebContents;
+
+/**
+ * Provides a way of accessing helpers for connection security levels.
+ */
+public class ConnectionSecurityHelper {
+    /**
+     * Fetch the security level for a given web contents.
+     *
+     * @param webContents The web contents to get the security level for.
+     * @return The ConnectionSecurityHelperSecurityLevel for the specified web contents.
+     *
+     * @see ConnectionSecurityHelperSecurityLevel
+     */
+    public static int getSecurityLevelForWebContents(WebContents webContents) {
+        if (webContents == null) return ConnectionSecurityHelperSecurityLevel.NONE;
+        return nativeGetSecurityLevelForWebContents(webContents);
+    }
+
+    private ConnectionSecurityHelper() {}
+
+    private static native int nativeGetSecurityLevelForWebContents(WebContents webContents);
+}
