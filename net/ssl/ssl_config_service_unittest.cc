9bef188cfaf6a3a081a6cced96a2de7eee148927
davidben <davidben@chromium.org>
2015-05-13 21:53:42 -0700

    Completely remove SSLv3 support.
    It's been off by default since Chrome 40 with a policy opt-in. The policy was
    documented to stop working after Chrome 43, so remove all logic for SSL 3.0.
    Chrome will no longer honor a --ssl-version-min=ssl3 flag and the option is
    removed from about:flags.
    
    This CL also removes the SSL 3.0 support from the extensions' TLS socket API.
    
    BUG=487730
    
    Review URL: https://codereview.chromium.org/1139013002
    
    Cr-Commit-Position: refs/heads/master@{#329805}

--- net/ssl/ssl_config_service_unittest.cc
+++ net/ssl/ssl_config_service_unittest.cc
@@ -50,8 +50,8 @@ TEST(SSLConfigServiceTest, NoChangesWontNotifyObservers) {
   SSLConfig initial_config;
   initial_config.rev_checking_enabled = true;
   initial_config.false_start_enabled = false;
-  initial_config.version_min = SSL_PROTOCOL_VERSION_SSL3;
-  initial_config.version_max = SSL_PROTOCOL_VERSION_TLS1_1;
+  initial_config.version_min = SSL_PROTOCOL_VERSION_TLS1;
+  initial_config.version_max = SSL_PROTOCOL_VERSION_TLS1_2;
 
   scoped_refptr<MockSSLConfigService> mock_service(
       new MockSSLConfigService(initial_config));
@@ -68,8 +68,8 @@ TEST(SSLConfigServiceTest, ConfigUpdatesNotifyObservers) {
   SSLConfig initial_config;
   initial_config.rev_checking_enabled = true;
   initial_config.false_start_enabled = false;
-  initial_config.version_min = SSL_PROTOCOL_VERSION_SSL3;
-  initial_config.version_max = SSL_PROTOCOL_VERSION_TLS1_1;
+  initial_config.version_min = SSL_PROTOCOL_VERSION_TLS1;
+  initial_config.version_max = SSL_PROTOCOL_VERSION_TLS1_2;
 
   scoped_refptr<MockSSLConfigService> mock_service(
       new MockSSLConfigService(initial_config));
@@ -86,11 +86,11 @@ TEST(SSLConfigServiceTest, ConfigUpdatesNotifyObservers) {
   mock_service->SetSSLConfig(initial_config);
 
   // Test that changing the SSL version range triggers updates.
-  initial_config.version_min = SSL_PROTOCOL_VERSION_TLS1;
+  initial_config.version_min = SSL_PROTOCOL_VERSION_TLS1_1;
   EXPECT_CALL(observer, OnSSLConfigChanged()).Times(1);
   mock_service->SetSSLConfig(initial_config);
 
-  initial_config.version_max = SSL_PROTOCOL_VERSION_SSL3;
+  initial_config.version_max = SSL_PROTOCOL_VERSION_TLS1_1;
   EXPECT_CALL(observer, OnSSLConfigChanged()).Times(1);
   mock_service->SetSSLConfig(initial_config);
 
