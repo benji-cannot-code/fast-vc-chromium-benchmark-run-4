f4303df97757963cd10f3ad6d0ea89aad1089535
achaulk <achaulk@chromium.org>
2015-05-14 13:26:35 -0700

    ozone: Add sync flag for atomic commit and page flip event handling
    Committed: https://crrev.com/f7553583492a8ea9a54d75cd6b2d3dd83c94512c
    Cr-Commit-Position: refs/heads/master@{#326863}
    
    Review URL: https://codereview.chromium.org/1099713005
    
    Cr-Commit-Position: refs/heads/master@{#329919}

--- ui/ozone/platform/drm/test/mock_drm_device.cc
+++ ui/ozone/platform/drm/test/mock_drm_device.cc
@@ -237,6 +237,7 @@ bool MockDrmDevice::CloseBufferHandle(uint32_t handle) {
 
 bool MockDrmDevice::CommitProperties(drmModePropertySet* properties,
                                      uint32_t flags,
+                                     bool is_sync,
                                      const PageFlipCallback& callback) {
   return false;
 }
