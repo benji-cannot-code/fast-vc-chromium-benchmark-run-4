b56ea09701d0cd68b1dd3d563683c629dff88881
sohan.jyoti <sohan.jyoti@samsung.com>
2015-05-14 10:37:18 -0700

    cc: Make tiling interest rect calc based on viewport.
    Current interest/eventually/live rect is calculated based
    on  max_tiles_for_interest_area setting which is not the
    best choice for determining the size. As part of this patch
    tiling interest rect is calculated based on viewport area.
    Which is multiplied by a constant factor. The factor is
    currently 9, viewport dist in all directions.
    Also, max_tiles_for_interest_area setting is removed
    and its associated switch and entries in chrome://flags.
    
    BUG=483520
    
    Review URL: https://codereview.chromium.org/1126793002
    
    Cr-Commit-Position: refs/heads/master@{#329870}

--- cc/resources/tile_manager_unittest.cc
+++ cc/resources/tile_manager_unittest.cc
@@ -31,7 +31,10 @@ namespace {
 
 class LowResTilingsSettings : public ImplSidePaintingSettings {
  public:
-  LowResTilingsSettings() { create_low_res_tiling = true; }
+  LowResTilingsSettings() {
+    tiling_interest_area_viewport_multiplier = 10000;
+    create_low_res_tiling = true;
+  }
 };
 
 class TileManagerTilePriorityQueueTest : public testing::Test {
@@ -1178,10 +1181,10 @@ TEST_F(TileManagerTilePriorityQueueTest,
 
   client.SetTileSize(gfx::Size(30, 30));
   LayerTreeSettings settings;
-  settings.max_tiles_for_interest_area = 10000;
+  settings.tiling_interest_area_viewport_multiplier = 10000;
 
   scoped_ptr<PictureLayerTilingSet> tiling_set = PictureLayerTilingSet::Create(
-      ACTIVE_TREE, &client, settings.max_tiles_for_interest_area,
+      ACTIVE_TREE, &client, settings.tiling_interest_area_viewport_multiplier,
       settings.skewport_target_time_in_seconds,
       settings.skewport_extrapolation_limit_in_content_pixels);
 
@@ -1288,10 +1291,9 @@ TEST_F(TileManagerTilePriorityQueueTest,
 
   client.SetTileSize(gfx::Size(30, 30));
   LayerTreeSettings settings;
-  settings.max_tiles_for_interest_area = 10000;
 
   scoped_ptr<PictureLayerTilingSet> tiling_set = PictureLayerTilingSet::Create(
-      ACTIVE_TREE, &client, settings.max_tiles_for_interest_area,
+      ACTIVE_TREE, &client, settings.tiling_interest_area_viewport_multiplier,
       settings.skewport_target_time_in_seconds,
       settings.skewport_extrapolation_limit_in_content_pixels);
 
