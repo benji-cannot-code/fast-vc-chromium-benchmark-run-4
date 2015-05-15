cf26f789bff2deea0b2aea58810b6dc7aa355236
yzshen <yzshen@chromium.org>
2015-05-15 16:54:02 -0700

    Introduce Http{Request,Response} mojom structs.
    The reason to stop using URL{Request,Response} for HTTP server interfaces: some
    fields of those structs belong to a higher level, e.g., |auto_follow_redirects|.
    It seems cleaner to define Http{Request,Response} which match the HTTP
    request/response definitions.
    
    BUG=478249
    TEST=None
    
    Review URL: https://codereview.chromium.org/1129143005
    
    Cr-Commit-Position: refs/heads/master@{#330238}

--- mojo/services/network/http_connection_impl.h
+++ mojo/services/network/http_connection_impl.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
 #include "mojo/services/network/public/interfaces/http_connection.mojom.h"
-#include "mojo/services/network/public/interfaces/url_loader.mojom.h"
+#include "mojo/services/network/public/interfaces/http_message.mojom.h"
 #include "third_party/mojo/src/mojo/public/cpp/bindings/binding.h"
 #include "third_party/mojo/src/mojo/public/cpp/bindings/error_handler.h"
 
@@ -52,7 +52,7 @@ class HttpConnectionImpl : public HttpConnection,
   // ErrorHandler implementation.
   void OnConnectionError() override;
 
-  void OnFinishedReadingResponseBody(URLResponsePtr response_ptr,
+  void OnFinishedReadingResponseBody(HttpResponsePtr response_ptr,
                                      SimpleDataPipeReader* reader,
                                      scoped_ptr<std::string> body);
 
