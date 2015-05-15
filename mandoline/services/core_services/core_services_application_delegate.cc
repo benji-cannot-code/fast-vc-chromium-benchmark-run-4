4d55fd184ff34045b6dc4bdb71385f8d1231a36d
erg <erg@chromium.org>
2015-05-15 13:12:01 -0700

    core_services: Use network_service in core_services on android.
    There was a mismatch between core_services_initialization.cc, the
    BUILD.gn file, and the package listing.
    
    BUG=484234
    
    Review URL: https://codereview.chromium.org/1141593004
    
    Cr-Commit-Position: refs/heads/master@{#330181}

--- mandoline/services/core_services/core_services_application_delegate.cc
+++ mandoline/services/core_services/core_services_application_delegate.cc
@@ -14,12 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/application/public/cpp/application_connection.h"
 #include "mojo/application/public/cpp/application_impl.h"
 #include "mojo/common/message_pump_mojo.h"
+#include "mojo/services/network/network_service_delegate.h"
 #include "mojo/services/tracing/tracing_app.h"
 #include "url/gurl.h"
 
 #if !defined(OS_ANDROID)
 #include "mandoline/ui/omnibox/omnibox_impl.h"
-#include "mojo/services/network/network_service_delegate.h"
 #endif
 
 namespace core_services {
@@ -129,9 +129,9 @@ void CoreServicesApplicationDelegate::StartApplication(
   scoped_ptr<mojo::ApplicationDelegate> delegate;
   if (url == "mojo://clipboard/")
     delegate.reset(new clipboard::ClipboardApplicationDelegate);
-#if !defined(OS_ANDROID)
   else if (url == "mojo://network_service/")
     delegate.reset(new NetworkServiceDelegate);
+#if !defined(OS_ANDROID)
   else if (url == "mojo://omnibox/")
     delegate.reset(new mandoline::OmniboxImpl);
 #endif
