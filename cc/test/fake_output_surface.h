bd0f1eb93cb81465dde466ab9bca45bff5fc208a
vmiura <vmiura@chromium.org>
2015-05-15 09:16:20 -0700

    cc: Add null checks for GrContext created by ContextProviderCommandBuffer.
    GrContext::Create may return NULL if the base GL context was lost.
    
     - Check for the NULL case in GlRenderer.
     - For the GPU rasterizer, don't allow GPU rasterization to be initialized
       with a NULL GrContext.
    
    BUG=464892
    
    Review URL: https://codereview.chromium.org/1135743004
    
    Cr-Commit-Position: refs/heads/master@{#330104}

--- cc/test/fake_output_surface.h
+++ cc/test/fake_output_surface.h
@@ -30,7 +30,8 @@ class FakeOutputSurface : public OutputSurface {
 
   static scoped_ptr<FakeOutputSurface> Create3d(
       scoped_refptr<ContextProvider> context_provider) {
-    return make_scoped_ptr(new FakeOutputSurface(context_provider, false));
+    return make_scoped_ptr(new FakeOutputSurface(
+        context_provider, TestContextProvider::Create(), false));
   }
 
   static scoped_ptr<FakeOutputSurface> Create3d(
@@ -42,8 +43,9 @@ class FakeOutputSurface : public OutputSurface {
 
   static scoped_ptr<FakeOutputSurface> Create3d(
       scoped_ptr<TestWebGraphicsContext3D> context) {
-    return make_scoped_ptr(new FakeOutputSurface(
-        TestContextProvider::Create(context.Pass()), false));
+    return make_scoped_ptr(
+        new FakeOutputSurface(TestContextProvider::Create(context.Pass()),
+                              TestContextProvider::Create(), false));
   }
 
   static scoped_ptr<FakeOutputSurface> CreateSoftware(
