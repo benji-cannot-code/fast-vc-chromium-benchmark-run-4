6c810fc8313f7c6d27ca458e6d1af54b612c461f
dmazzoni <dmazzoni@chromium.org>
2015-05-15 13:59:07 -0700

    Allow whitelisted content scripts to be injected in WebViews.
    The code that tried to inject ChromeVox into WebViews in
    chrome_web_view_guest_delegate.cc wasn't working. Instead, this allows
    whitelisted content scripts to be injected into WebViews.
    
    BUG=484904
    
    Review URL: https://codereview.chromium.org/1140173003
    
    Cr-Commit-Position: refs/heads/master@{#330193}

--- extensions/browser/guest_view/web_view/web_view_guest.cc
+++ extensions/browser/guest_view/web_view/web_view_guest.cc
@@ -331,8 +331,6 @@ void WebViewGuest::DidInitialize(const base::DictionaryValue& create_params) {
                               content::NOTIFICATION_RESOURCE_RECEIVED_REDIRECT,
                               content::Source<WebContents>(web_contents()));
 
-  if (web_view_guest_delegate_)
-    web_view_guest_delegate_->OnDidInitialize();
   AttachWebViewHelpers(web_contents());
 
   rules_registry_id_ = GetOrGenerateRulesRegistryID(
@@ -787,11 +785,6 @@ void WebViewGuest::DidCommitProvisionalLoadForFrame(
       new GuestViewEvent(webview::kEventLoadCommit, args.Pass()));
 
   find_helper_.CancelAllFindSessions();
-
-  if (web_view_guest_delegate_) {
-    web_view_guest_delegate_->OnDidCommitProvisionalLoadForFrame(
-        !render_frame_host->GetParent());
-  }
 }
 
 void WebViewGuest::DidFailProvisionalLoad(
@@ -813,12 +806,6 @@ void WebViewGuest::DidStartProvisionalLoadForFrame(
   args->SetBoolean(guest_view::kIsTopLevel, !render_frame_host->GetParent());
   DispatchEventToView(
       new GuestViewEvent(webview::kEventLoadStart, args.Pass()));
-}
-
-void WebViewGuest::DocumentLoadedInFrame(
-    content::RenderFrameHost* render_frame_host) {
-  if (web_view_guest_delegate_)
-    web_view_guest_delegate_->OnDocumentLoadedInFrame(render_frame_host);
 }
 
 void WebViewGuest::RenderProcessGone(base::TerminationStatus status) {
