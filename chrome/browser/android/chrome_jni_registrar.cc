00a1a54d159381d3d637e13221ffb4928f6d6bec
nyquist <nyquist@chromium.org>
2015-05-14 17:06:24 -0700

    Add connectivity check for Android feedback.
    This CL adds functionality to check whether
    http://clients4.google.com/generate_204 URL can be reached and returns
    status code HTTP 204. The cache is bypassed and no redirects are allowed.
    
    The functionality this provides will be used by the feedback system to
    provide additional data in the report akin to a "Is this thing on?" type
    test.
    
    BUG=386395
    
    Review URL: https://codereview.chromium.org/1127983002
    
    Cr-Commit-Position: refs/heads/master@{#329981}

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/download/chrome_download_delegate.h"
 #include "chrome/browser/android/favicon_helper.h"
 #include "chrome/browser/android/feature_utilities.h"
+#include "chrome/browser/android/feedback/connectivity_checker.h"
 #include "chrome/browser/android/find_in_page/find_in_page_bridge.h"
 #include "chrome/browser/android/foreign_session_helper.h"
 #include "chrome/browser/android/fullscreen/fullscreen_infobar_delegate.h"
@@ -182,6 +183,7 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
     {"ConnectionInfoPopupAndroid",
      ConnectionInfoPopupAndroid::RegisterConnectionInfoPopupAndroid},
     {"ConnectionSecurityHelper", RegisterConnectionSecurityHelperAndroid},
+    {"ConnectivityChecker", RegisterConnectivityChecker},
     {"ContentViewUtil", RegisterContentViewUtil},
     {"ContextMenuHelper", RegisterContextMenuHelper},
     {"CookiesFetcher", RegisterCookiesFetcher},
