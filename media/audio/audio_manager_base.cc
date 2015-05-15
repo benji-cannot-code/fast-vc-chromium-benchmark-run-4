54af23cc8abdc4ecff4a4dfa09031797faec8b5d
ckehoe <ckehoe@chromium.org>
2015-05-15 13:58:31 -0700

    Allowing a custom audio buffer size in WebRtcAudioCapturer.
    Review URL: https://codereview.chromium.org/1130063002
    
    Cr-Commit-Position: refs/heads/master@{#330192}

--- media/audio/audio_manager_base.cc
+++ media/audio/audio_manager_base.cc
@@ -201,6 +201,9 @@ AudioInputStream* AudioManagerBase::MakeAudioInputStream(
     return NULL;
   }
 
+  DVLOG(2) << "Creating a new AudioInputStream with buffer size = "
+           << params.frames_per_buffer();
+
   AudioInputStream* stream;
   switch (params.format()) {
     case AudioParameters::AUDIO_PCM_LINEAR:
