daa066c9ca3f10479ee148f45960fd5124aa7cfa
georgesak <georgesak@chromium.org>
2015-05-14 20:01:50 -0700

    Change WebContents::last_active_time_ to Time instead of Timeticks.
    For context, last_active_time_ is going to be used by session restore to order the loading of background tabs using MRU. In order for this to be robust, last_active_time_ must be saved and restore between sessions. Timeticks cannot be reliably restored as it's dependent on the current OS session. MRU code for session restore is being implemented in https://codereview.chromium.org/1131373003
    
    Notes:
    - In dev tools, replaced "activity" with "active" for consistency
    - In OomPriorityManagerTest.Comparator, initialized last_active_time for all tabs, as this is necessary with Time (a default TimeTicks can go back in time, not the case with Time).
    
    BUG=472772
    
    Review URL: https://codereview.chromium.org/1140083004
    
    Cr-Commit-Position: refs/heads/master@{#330029}

--- components/devtools_http_handler/devtools_http_handler.cc
+++ components/devtools_http_handler/devtools_http_handler.cc
@@ -321,7 +321,7 @@ class DevToolsAgentHostClientImpl : public DevToolsAgentHostClient {
 
 static bool TimeComparator(const DevToolsTargetDescriptor* desc1,
                            const DevToolsTargetDescriptor* desc2) {
-  return desc1->GetLastActivityTime() > desc2->GetLastActivityTime();
+  return desc1->GetLastActiveTime() > desc2->GetLastActiveTime();
 }
 
 // DevToolsHttpHandler::ServerSocketFactory ----------------------------------
