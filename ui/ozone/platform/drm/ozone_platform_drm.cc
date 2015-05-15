29fb96a3cb1f4a50e0c63be8f103874103b826e6
dnicoara <dnicoara@chromium.org>
2015-05-15 11:39:50 -0700

    [Ozone-Drm] Remove GPU lock
    Workarounds to support grabbing the DRM devices as master after GPU crashes
    made the GPU lock unnecessary.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1141243002
    
    Cr-Commit-Position: refs/heads/master@{#330146}

--- ui/ozone/platform/drm/ozone_platform_drm.cc
+++ ui/ozone/platform/drm/ozone_platform_drm.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/drm/gpu/drm_gpu_display_manager.h"
 #include "ui/ozone/platform/drm/gpu/drm_gpu_platform_support.h"
 #include "ui/ozone/platform/drm/gpu/drm_window.h"
-#include "ui/ozone/platform/drm/gpu/gpu_lock.h"
 #include "ui/ozone/platform/drm/gpu/screen_manager.h"
 #include "ui/ozone/platform/drm/host/drm_cursor.h"
 #include "ui/ozone/platform/drm/host/drm_display_host_manager.h"
@@ -89,9 +88,6 @@ class OzonePlatformDrm : public OzonePlatform {
         new DrmNativeDisplayDelegate(display_manager_.get()));
   }
   void InitializeUI() override {
-#if defined(OS_CHROMEOS)
-    gpu_lock_.reset(new GpuLock());
-#endif
     drm_device_manager_.reset(new DrmDeviceManager(
         scoped_ptr<DrmDeviceGenerator>(new DrmDeviceGenerator())));
     window_manager_.reset(new DrmWindowHostManager());
@@ -126,7 +122,6 @@ class OzonePlatformDrm : public OzonePlatform {
 
  private:
   // Objects in the "GPU" process.
-  scoped_ptr<GpuLock> gpu_lock_;
   scoped_ptr<DrmDeviceManager> drm_device_manager_;
   scoped_ptr<DrmBufferGenerator> buffer_generator_;
   scoped_ptr<ScreenManager> screen_manager_;
