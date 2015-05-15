7798dc6f51e3c515d058f88c3915714f75ce73c3
amistry <amistry@chromium.org>
2015-05-15 12:53:31 -0700

    Add a field trial to enable out-of-process V8 pac.
    At the same time, add a "disable" flag as an escape hatch for when
    things go bad.
    
    BUG=467832
    
    Review URL: https://codereview.chromium.org/1124083004
    
    Cr-Commit-Position: refs/heads/master@{#330175}

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -87,6 +87,7 @@ extern const char kDisableNewOfflineErrorPage[];
 extern const char kDisableNTPOtherSessionsMenu[];
 extern const char kDisableOfflineAutoReload[];
 extern const char kDisableOfflineAutoReloadVisibleOnly[];
+extern const char kDisableOutOfProcessPac[];
 extern const char kDisablePasswordManagerReauthentication[];
 extern const char kDisablePdfMaterialUI[];
 extern const char kDisablePermissionsBubbles[];
