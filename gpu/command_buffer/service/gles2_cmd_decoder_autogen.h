a3100c50567b6308cb993d38411feaf0c31c40fd
zmo <zmo@chromium.org>
2015-05-14 20:04:07 -0700

    Fine tune vertex attrib commands handling.
    1) Correctly cache vertex attrib values with the right data type
    2) client and service side caching of |integer|
    
    BUG=429053
    TEST=gpu_unittests
    R=piman@chromium.org
    
    Review URL: https://codereview.chromium.org/1131273005
    
    Cr-Commit-Position: refs/heads/master@{#330030}

--- gpu/command_buffer/service/gles2_cmd_decoder_autogen.h
+++ gpu/command_buffer/service/gles2_cmd_decoder_autogen.h
@@ -3949,7 +3949,7 @@ error::Error GLES2DecoderImpl::HandleVertexAttribI4i(
   GLint y = static_cast<GLint>(c.y);
   GLint z = static_cast<GLint>(c.z);
   GLint w = static_cast<GLint>(c.w);
-  glVertexAttribI4i(indx, x, y, z, w);
+  DoVertexAttribI4i(indx, x, y, z, w);
   return error::kNoError;
 }
 
@@ -3974,7 +3974,7 @@ error::Error GLES2DecoderImpl::HandleVertexAttribI4ivImmediate(
   if (values == NULL) {
     return error::kOutOfBounds;
   }
-  glVertexAttribI4iv(indx, values);
+  DoVertexAttribI4iv(indx, values);
   return error::kNoError;
 }
 
@@ -3991,7 +3991,7 @@ error::Error GLES2DecoderImpl::HandleVertexAttribI4ui(
   GLuint y = static_cast<GLuint>(c.y);
   GLuint z = static_cast<GLuint>(c.z);
   GLuint w = static_cast<GLuint>(c.w);
-  glVertexAttribI4ui(indx, x, y, z, w);
+  DoVertexAttribI4ui(indx, x, y, z, w);
   return error::kNoError;
 }
 
@@ -4016,7 +4016,7 @@ error::Error GLES2DecoderImpl::HandleVertexAttribI4uivImmediate(
   if (values == NULL) {
     return error::kOutOfBounds;
   }
-  glVertexAttribI4uiv(indx, values);
+  DoVertexAttribI4uiv(indx, values);
   return error::kNoError;
 }
 
--- gpu/command_buffer/service/vertex_attrib_manager.h
+++ gpu/command_buffer/service/vertex_attrib_manager.h
@@ -117,14 +117,11 @@ class GPU_EXPORT VertexAttrib {
       GLboolean normalized,
       GLsizei gl_stride,
       GLsizei real_stride,
-      GLsizei offset);
+      GLsizei offset,
+      GLboolean integer);
 
   void SetDivisor(GLsizei divisor) {
     divisor_ = divisor;
-  }
-
-  void SetInteger(GLboolean integer) {
-    integer_ = integer;
   }
 
   void Unbind(Buffer* buffer);
@@ -208,7 +205,8 @@ class GPU_EXPORT VertexAttribManager :
       GLboolean normalized,
       GLsizei gl_stride,
       GLsizei real_stride,
-      GLsizei offset) {
+      GLsizei offset,
+      GLboolean integer) {
     VertexAttrib* attrib = GetVertexAttrib(index);
     if (attrib) {
       if (attrib->type() == GL_FIXED) {
@@ -217,8 +215,8 @@ class GPU_EXPORT VertexAttribManager :
       if (type == GL_FIXED) {
         ++num_fixed_attribs_;
       }
-      attrib->SetInfo(
-          buffer, size, type, normalized, gl_stride, real_stride, offset);
+      attrib->SetInfo(buffer, size, type, normalized, gl_stride, real_stride,
+                      offset, integer);
     }
   }
 
@@ -226,13 +224,6 @@ class GPU_EXPORT VertexAttribManager :
     VertexAttrib* attrib = GetVertexAttrib(index);
     if (attrib) {
       attrib->SetDivisor(divisor);
-    }
-  }
-
-  void SetInteger(GLuint index, GLboolean integer) {
-    VertexAttrib* attrib = GetVertexAttrib(index);
-    if (attrib) {
-      attrib->SetInteger(integer);
     }
   }
 
