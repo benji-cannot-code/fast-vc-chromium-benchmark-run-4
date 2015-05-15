24bbdc31699f4ce5015fc9b6bfea55c606f0a129
yzshen <yzshen@chromium.org>
2015-05-15 12:37:34 -0700

    Mojo service implementation for HTTP server - part 2
    This CL supports HTTPConnectionDelegate.OnReceivedRequest() and adds corresponding tests.
    
    The following work will be in future CLs:
    - support WebSocket.
    - more tests.
    
    BUG=478249
    TEST=Newly-added tests.
    
    Review URL: https://codereview.chromium.org/1128863004
    
    Cr-Commit-Position: refs/heads/master@{#330168}

--- mojo/services/network/http_connection_impl.h
+++ mojo/services/network/http_connection_impl.h
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MOJO_SERVICES_NETWORK_HTTP_CONNECTION_IMPL_H_
 #define MOJO_SERVICES_NETWORK_HTTP_CONNECTION_IMPL_H_
 
+#include <set>
 #include <string>
 
 #include "base/macros.h"
+#include "base/memory/scoped_ptr.h"
 #include "mojo/services/network/public/interfaces/http_connection.mojom.h"
+#include "mojo/services/network/public/interfaces/url_loader.mojom.h"
 #include "third_party/mojo/src/mojo/public/cpp/bindings/binding.h"
 #include "third_party/mojo/src/mojo/public/cpp/bindings/error_handler.h"
 
@@ -37,6 +40,8 @@ class HttpConnectionImpl : public HttpConnection,
   void OnReceivedWebSocketMessage(const std::string& data);
 
  private:
+  class SimpleDataPipeReader;
+
   // HttpConnection implementation.
   void SetSendBufferSize(uint32_t size,
                          const SetSendBufferSizeCallback& callback) override;
@@ -47,10 +52,20 @@ class HttpConnectionImpl : public HttpConnection,
   // ErrorHandler implementation.
   void OnConnectionError() override;
 
+  void OnFinishedReadingResponseBody(URLResponsePtr response_ptr,
+                                     SimpleDataPipeReader* reader,
+                                     scoped_ptr<std::string> body);
+
+  bool EncounteredConnectionError() const {
+    return !binding_.is_bound() || !delegate_;
+  }
+
   const int connection_id_;
   HttpServerImpl* const owner_;
   HttpConnectionDelegatePtr delegate_;
   Binding<HttpConnection> binding_;
+  // Owns its elements.
+  std::set<SimpleDataPipeReader*> response_body_readers_;
 
   DISALLOW_COPY_AND_ASSIGN(HttpConnectionImpl);
 };
