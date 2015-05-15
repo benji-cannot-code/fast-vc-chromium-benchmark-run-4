dee456ed3dffa2894f6ce69ca5c0f602c1d76b37
achuith <achuith@chromium.org>
2015-05-15 16:10:34 -0700

    Disable testUnregisterDomainWithUnregisteredDomain and testUserAgent on chromeos.
    testUnregisterDomainWithUnregisteredDomain fails consistently, testUserAgent is flaky.
    
    BUG=483212
    
    Review URL: https://codereview.chromium.org/1143673002
    
    Cr-Commit-Position: refs/heads/master@{#330226}

--- tools/telemetry/telemetry/core/user_agent_unittest.py
+++ tools/telemetry/telemetry/core/user_agent_unittest.py
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from telemetry import decorators
 from telemetry.core import user_agent
 from telemetry.unittest_util import tab_test_case
 
@@ -12,6 +13,7 @@ class MobileUserAgentTest(tab_test_case.TabTestCase):
   def CustomizeBrowserOptions(cls, options):
     options.browser_user_agent_type = 'mobile'
 
+  @decorators.Disabled('chromeos')  # crbug.com/483212
   def testUserAgent(self):
     ua = self._tab.EvaluateJavaScript('window.navigator.userAgent')
     self.assertEquals(ua, user_agent.UA_TYPE_MAPPING['mobile'])
@@ -21,6 +23,7 @@ class TabletUserAgentTest(tab_test_case.TabTestCase):
   def CustomizeBrowserOptions(cls, options):
     options.browser_user_agent_type = 'tablet'
 
+  @decorators.Disabled('chromeos')  # crbug.com/483212
   def testUserAgent(self):
     ua = self._tab.EvaluateJavaScript('window.navigator.userAgent')
     self.assertEquals(ua, user_agent.UA_TYPE_MAPPING['tablet'])
@@ -30,6 +33,7 @@ class DesktopUserAgentTest(tab_test_case.TabTestCase):
   def CustomizeBrowserOptions(cls, options):
     options.browser_user_agent_type = 'desktop'
 
+  @decorators.Disabled('chromeos')  # crbug.com/483212
   def testUserAgent(self):
     ua = self._tab.EvaluateJavaScript('window.navigator.userAgent')
     self.assertEquals(ua, user_agent.UA_TYPE_MAPPING['desktop'])
