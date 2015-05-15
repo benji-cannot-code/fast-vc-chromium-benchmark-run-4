86c179b1cf3af4eeb47be8d367395f6532868ab6
iclelland <iclelland@chromium.org>
2015-05-15 07:40:15 -0700

    [BackgroundSync] Fix lifetime issues with callbacks pointer in CallbackThreadAdapter
    This correctly assigns ownership of the callbacks_ object to the closure created
    by base::Bind when posting the success/error handling task to the correct
    thread.
    
    BUG=488452
    
    Review URL: https://codereview.chromium.org/1139643008
    
    Cr-Commit-Position: refs/heads/master@{#330094}

--- content/child/background_sync/background_sync_provider_thread_proxy.cc
+++ content/child/background_sync/background_sync_provider_thread_proxy.cc
@@ -41,7 +41,7 @@ class CallbackThreadAdapter : public blink::WebCallbacks<S, T> {
     WorkerTaskRunner::Instance()->PostTask(
         worker_thread_id_,
         base::Bind(&blink::WebCallbacks<S, T>::onSuccess,
-                   base::Unretained(callbacks_.get()), results));
+                   base::Owned(callbacks_.release()), results));
   }
 
   virtual void onError(T* error) {
@@ -50,7 +50,7 @@ class CallbackThreadAdapter : public blink::WebCallbacks<S, T> {
     WorkerTaskRunner::Instance()->PostTask(
         worker_thread_id_,
         base::Bind(&blink::WebCallbacks<S, T>::onError,
-                   base::Unretained(callbacks_.get()), error));
+                   base::Owned(callbacks_.release()), error));
   }
 
  private:
