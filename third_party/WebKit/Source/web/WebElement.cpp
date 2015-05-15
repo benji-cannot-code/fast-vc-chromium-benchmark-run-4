c891f191da4db51e815725deb1884f3cee3f92a0
kbr@chromium.org <kbr@chromium.org>
2015-05-15 22:48:52 +0000

    Revert of Remove WebElement::innerText. (patchset #1 id:1 of https://codereview.chromium.org/1137623005/)
    Reason for revert:
    Broke Android WebView compile: http://build.chromium.org/p/chromium.webkit/builders/Android%20Builder%20%28dbg%29/builds/44894
    
    
    Original issue's description:
    > Remove WebElement::innerText.
    > 
    > This is the final part 3.
    > 
    > BUG=488088
    > R=esprehn
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=195421
    
    TBR=esprehn@chromium.org,dglazkov@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=488088
    
    Review URL: https://codereview.chromium.org/1139603004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195423 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebElement.cpp
+++ third_party/WebKit/Source/web/WebElement.cpp
@@ -134,6 +134,11 @@ WebString WebElement::textContent()
     return unwrap<Element>()->textContent();
 }
 
+WebString WebElement::innerText()
+{
+    return unwrap<Element>()->innerText();
+}
+
 WebString WebElement::computeInheritedLanguage() const
 {
     return WebString(constUnwrap<Element>()->computeInheritedLanguage());
