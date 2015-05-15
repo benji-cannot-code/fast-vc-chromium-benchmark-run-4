8fe86bd177ad29ff0e58573b2365e866f0fc9580
skyostil <skyostil@chromium.org>
2015-05-15 08:25:47 -0700

    Re-enable blink_perf.canvas on Windows
    Did 50 runs locally to confirm that this is working.
    
    BUG=455796
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1129203004
    
    Cr-Commit-Position: refs/heads/master@{#330097}

--- tools/perf/benchmarks/blink_perf.py
+++ tools/perf/benchmarks/blink_perf.py
@@ -168,7 +168,6 @@ class BlinkPerfCSS(benchmark.Benchmark):
     return CreatePageSetFromPath(path, SKIPPED_FILE)
 
 
-@benchmark.Disabled('win')  # crbug.com/455796
 class BlinkPerfCanvas(benchmark.Benchmark):
   tag = 'canvas'
   test = _BlinkPerfMeasurement
