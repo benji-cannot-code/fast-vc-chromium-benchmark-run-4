a7620744e995052299dbb4b7de2c1f6025c2f7b3
ben <ben@chromium.org>
2015-05-15 17:15:55 -0700

    Get the omnibox to show up again. Sky's changes from yesterday removed the window manager interface. This is a temporary interface to allow the omnibox app to ask the browser app to place its window.
    R=sadrul@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1135933003
    
    Cr-Commit-Position: refs/heads/master@{#330241}

--- mandoline/ui/browser/browser.cc
+++ mandoline/ui/browser/browser.cc
@@ -37,7 +37,7 @@ Browser::Browser()
       omnibox_(nullptr),
       navigator_host_(this),
       ui_(nullptr) {
-  exposed_services_impl_.AddService(this);
+  exposed_services_impl_.AddService<mojo::NavigatorHost>(this);
 }
 
 Browser::~Browser() {
@@ -73,6 +73,7 @@ bool Browser::ConfigureIncomingConnection(
 
 bool Browser::ConfigureOutgoingConnection(
     mojo::ApplicationConnection* connection) {
+  connection->AddService<ViewEmbedder>(this);
   return true;
 }
 
@@ -123,24 +124,6 @@ void Browser::OpenURL(const mojo::String& url) {
   ReplaceContentWithURL(url);
 }
 
-void Browser::Create(mojo::ApplicationConnection* connection,
-                     mojo::InterfaceRequest<mojo::NavigatorHost> request) {
-  navigator_host_.Bind(request.Pass());
-}
-
-void Browser::ShowOmnibox(
-    const mojo::String& url,
-    mojo::InterfaceRequest<mojo::ServiceProvider> services,
-    mojo::ServiceProviderPtr exposed_services) {
-  if (!omnibox_) {
-    omnibox_ = root_->view_manager()->CreateView();
-    root_->AddChild(omnibox_);
-    omnibox_->SetVisible(true);
-    omnibox_->SetBounds(root_->bounds());
-  }
-  omnibox_->Embed(url, services.Pass(), exposed_services.Pass());
-}
-
 void Browser::Embed(const mojo::String& url,
                     mojo::InterfaceRequest<mojo::ServiceProvider> services,
                     mojo::ServiceProviderPtr exposed_services) {
@@ -169,6 +152,29 @@ void Browser::Embed(const mojo::String& url,
                   merged_service_provider_->GetServiceProviderPtr().Pass());
 
   navigator_host_.RecordNavigation(url);
+}
+
+void Browser::Create(mojo::ApplicationConnection* connection,
+                     mojo::InterfaceRequest<mojo::NavigatorHost> request) {
+  navigator_host_.Bind(request.Pass());
+}
+
+void Browser::Create(mojo::ApplicationConnection* connection,
+                     mojo::InterfaceRequest<ViewEmbedder> request) {
+  view_embedder_bindings_.AddBinding(this, request.Pass());
+}
+
+void Browser::ShowOmnibox(
+    const mojo::String& url,
+    mojo::InterfaceRequest<mojo::ServiceProvider> services,
+    mojo::ServiceProviderPtr exposed_services) {
+  if (!omnibox_) {
+    omnibox_ = root_->view_manager()->CreateView();
+    root_->AddChild(omnibox_);
+    omnibox_->SetVisible(true);
+    omnibox_->SetBounds(root_->bounds());
+  }
+  omnibox_->Embed(url, services.Pass(), exposed_services.Pass());
 }
 
 }  // namespace mandoline
--- mandoline/ui/omnibox/omnibox_impl.h
+++ mandoline/ui/omnibox/omnibox_impl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/view_manager/public/cpp/view_manager_delegate.h"
 #include "mandoline/ui/browser/omnibox.mojom.h"
+#include "mandoline/ui/browser/view_embedder.mojom.h"
 #include "mojo/application/public/cpp/application_delegate.h"
 #include "mojo/application/public/cpp/interface_factory.h"
 #include "mojo/common/weak_binding_set.h"
@@ -69,6 +70,7 @@ class OmniboxImpl : public mojo::ApplicationDelegate,
   views::Textfield* edit_;
   mojo::WeakBindingSet<Omnibox> bindings_;
   scoped_ptr<mojo::ViewManagerClientFactory> view_manager_client_factory_;
+  ViewEmbedderPtr view_embedder_;
 
   DISALLOW_COPY_AND_ASSIGN(OmniboxImpl);
 };
