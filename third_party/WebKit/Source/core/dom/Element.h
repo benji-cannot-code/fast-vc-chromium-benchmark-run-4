6a7905a924a62f1d484e41e0e227cb28667bacc9
philipj@opera.com <philipj@opera.com>
2015-05-17 23:05:46 +0000

    Remove xml:base support
    Intent to Deprecate and Remove:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/ZvArHAXyAHM/ftybb0waCKYJ
    
    BUG=341854
    
    Review URL: https://codereview.chromium.org/1137753004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195443 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.h
+++ third_party/WebKit/Source/core/dom/Element.h
@@ -248,8 +248,6 @@ public:
 
     const AtomicString& locateNamespacePrefix(const AtomicString& namespaceURI) const;
 
-    virtual KURL baseURI() const override final;
-
     virtual String nodeName() const override;
 
     PassRefPtrWillBeRawPtr<Element> cloneElementWithChildren();
