041aa87aa008bd46d87a7dc50c70b4de06c449a0
stevenjb <stevenjb@chromium.org>
2015-05-16 19:17:53 -0700

    Add demo_page.html to cr_elements/v0_8
    This CL should facilitate conversion of cr- elements and help debugging
    them in isolation.
    
    To view elements in demo_elements.html, navigate to:
    chrome://resources/cr_elements/v0_8/demo_page.html
    
    BUG=485381
    
    Review URL: https://codereview.chromium.org/1124073007
    
    Cr-Commit-Position: refs/heads/master@{#330297}

---
+++ ui/webui/resources/cr_elements/v0_8/demo_element.js
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Polymer element for UI wrapping a list of cr- elements.
+ */
+Polymer({
+  is: 'cr-demo-element',
+
+  properties: {
+    checkboxChecked: {
+      type: Boolean,
+      value: true,
+      observer: 'checkboxCheckedChanged_'
+    },
+
+    collapseOpened: {
+      type: Boolean,
+      value: true,
+      observer: 'collapseOpenedChanged_'
+    },
+
+    inputValue: {
+      type: String,
+      value: '',
+      observer: 'inputValueChanged_'
+    },
+  },
+
+  checkboxCheckedChanged_: function() {
+    console.log('checkboxCheckedChanged=' + this.checkboxChecked);
+  },
+
+  collapseOpenedChanged_: function() {
+    console.log('collapseOpened=' + this.collapseOpened);
+  },
+
+  inputValueChanged_: function() {
+    console.log('inputValue=' + this.inputValue);
+  }
+});
