50430b71d673e3243b7bc1041e385561147b0ee1
samahto@cisco.com <samahto@cisco.com>
2015-05-18 01:16:48 +0000

    Set hovered state of current Hovered node even if it is also common ancestor of oldHovered node and newHovered node.
    Before this patch if the commonAncestor of newHovered and oldHovered
    node is newHovered node then setHover gets ignored for newHovered node.
        
    BUG=95995
    TEST=LayoutTests/fast/forms/hover-on-moving-mouse-checkbox-to-parent-label.html
    
    Review URL: https://codereview.chromium.org/1064543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195446 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -5360,7 +5360,7 @@ void Document::updateHoverActiveState(const HitTestRequest& request, Element* in
             sawCommonAncestor = true;
         if (allowActiveChanges)
             nodesToAddToChain[i]->setActive(true);
-        if (!sawCommonAncestor) {
+        if (!sawCommonAncestor || nodesToAddToChain[i] == m_hoverNode) {
             nodesToAddToChain[i]->setHovered(true);
         }
     }
