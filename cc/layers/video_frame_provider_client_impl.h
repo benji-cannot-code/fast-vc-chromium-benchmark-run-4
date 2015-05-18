00af1b6f8b0976a8f313b4d49b5d7c10bbe86216
hendrikw <hendrikw@chromium.org>
2015-05-18 16:14:14 -0700

    cc: Video layers without a frame should not occlude
    Added VisibleContentOpaqueRegion() to the VideoLayerImpl, if
    a frame hasn't been set yet, then we don't have an opaque region.
    
    + test
    
    BUG=460612
    
    Review URL: https://codereview.chromium.org/1134663005
    
    Cr-Commit-Position: refs/heads/master@{#330449}

--- cc/layers/video_frame_provider_client_impl.h
+++ cc/layers/video_frame_provider_client_impl.h
@@ -43,6 +43,7 @@ class CC_EXPORT VideoFrameProviderClientImpl
   scoped_refptr<media::VideoFrame> AcquireLockAndCurrentFrame();
   void PutCurrentFrame();
   void ReleaseLock();
+  bool HasCurrentFrame();
 
   const gfx::Transform& StreamTextureMatrix() const;
 
