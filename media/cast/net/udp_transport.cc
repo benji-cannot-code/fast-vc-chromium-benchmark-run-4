b44c73ea063d449dba870793c36b10028713a2a6
hubbe <hubbe@chromium.org>
2015-05-18 15:29:09 -0700

    Cast: Fix crash in udp_transport
    When connecting to a port on localhost which is not yet open, connect fails and udp_socket crashes when trying to send packets. This fixes that.
    
    Review URL: https://codereview.chromium.org/967723002
    
    Cr-Commit-Position: refs/heads/master@{#330442}

--- media/cast/net/udp_transport.cc
+++ media/cast/net/udp_transport.cc
@@ -67,6 +67,11 @@ void UdpTransport::StartReceiving(
     const PacketReceiverCallbackWithStatus& packet_receiver) {
   DCHECK(io_thread_proxy_->RunsTasksOnCurrentThread());
 
+  if (!udp_socket_) {
+    status_callback_.Run(TRANSPORT_SOCKET_ERROR);
+    return;
+  }
+
   packet_receiver_ = packet_receiver;
   udp_socket_->SetMulticastLoopbackMode(true);
   if (!IsEmpty(local_addr_)) {
@@ -74,6 +79,7 @@ void UdpTransport::StartReceiving(
         udp_socket_->AllowAddressReuse() < 0 ||
         udp_socket_->Bind(local_addr_) < 0) {
       udp_socket_->Close();
+      udp_socket_.reset();
       status_callback_.Run(TRANSPORT_SOCKET_ERROR);
       LOG(ERROR) << "Failed to bind local address.";
       return;
@@ -83,6 +89,7 @@ void UdpTransport::StartReceiving(
         udp_socket_->AllowAddressReuse() < 0 ||
         udp_socket_->Connect(remote_addr_) < 0) {
       udp_socket_->Close();
+      udp_socket_.reset();
       status_callback_.Run(TRANSPORT_SOCKET_ERROR);
       LOG(ERROR) << "Failed to connect to remote address.";
       return;
@@ -112,6 +119,8 @@ void UdpTransport::SetDscp(net::DiffServCodePoint dscp) {
 #if defined(OS_WIN)
 void UdpTransport::UseNonBlockingIO() {
   DCHECK(io_thread_proxy_->RunsTasksOnCurrentThread());
+  if (!udp_socket_)
+    return;
   udp_socket_->UseNonBlockingIO();
 }
 #endif
@@ -131,6 +140,8 @@ void UdpTransport::ReceiveNextPacket(int length_or_status) {
   DCHECK(io_thread_proxy_->RunsTasksOnCurrentThread());
 
   if (packet_receiver_.is_null())
+    return;
+  if (!udp_socket_)
     return;
 
   // Loop while UdpSocket is delivering data synchronously.  When it responds
@@ -188,6 +199,8 @@ void UdpTransport::ReceiveNextPacket(int length_or_status) {
 
 bool UdpTransport::SendPacket(PacketRef packet, const base::Closure& cb) {
   DCHECK(io_thread_proxy_->RunsTasksOnCurrentThread());
+  if (!udp_socket_)
+    return true;
 
   // Increase byte count no matter the packet was sent or dropped.
   bytes_sent_ += packet->data.size();
