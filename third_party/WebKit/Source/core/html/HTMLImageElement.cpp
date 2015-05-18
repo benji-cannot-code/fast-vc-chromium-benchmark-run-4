a5b354e22955de0815bafdef2694624e6bb6fa78
robhogan@gmail.com <robhogan@gmail.com>
2015-05-18 00:13:51 +0000

    content: shouldn't prevent fallback content on an image getting displayed
    If an image has fallback content then we need to display it, don't let a content:
    directive prevent that. For what it's worth this fix makes our rendering of the
    test match FF.
    
    BUG=485414
    
    Review URL: https://codereview.chromium.org/1134903002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195445 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLImageElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLImageElement.cpp
@@ -331,11 +331,11 @@ ImageCandidate HTMLImageElement::findBestFitImageFromPictureParent()
 
 LayoutObject* HTMLImageElement::createLayoutObject(const ComputedStyle& style)
 {
-    if (style.hasContent())
-        return LayoutObject::createObject(this, style);
-
     if (m_useFallbackContent)
         return new LayoutBlockFlow(this);
+
+    if (style.hasContent())
+        return LayoutObject::createObject(this, style);
 
     LayoutImage* image = new LayoutImage(this);
     image->setImageResource(LayoutImageResource::create());
