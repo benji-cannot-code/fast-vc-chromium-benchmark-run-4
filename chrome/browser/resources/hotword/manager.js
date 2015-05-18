71e80c7378eee9238a3759fa7f170bee4229560f
amistry <amistry@chromium.org>
2015-05-17 19:19:57 -0700

    Don't start the hotword extension on Chrome startup if the user doesn't have hotwording enabled.
    BUG=488357
    
    Review URL: https://codereview.chromium.org/1130763005
    
    Cr-Commit-Position: refs/heads/master@{#330322}

--- chrome/browser/resources/hotword/manager.js
+++ chrome/browser/resources/hotword/manager.js
@@ -25,11 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var launcherManager = new hotword.LauncherManager(stateManager);
   var trainingManager = new hotword.TrainingManager(stateManager);
 
-  // Detect Chrome startup and make sure we get a chance to run.
-  chrome.runtime.onStartup.addListener(function() {
-    stateManager.updateStatus();
-  });
-
   // Detect when hotword settings have changed.
   chrome.hotwordPrivate.onEnabledChanged.addListener(function() {
     stateManager.updateStatus();
