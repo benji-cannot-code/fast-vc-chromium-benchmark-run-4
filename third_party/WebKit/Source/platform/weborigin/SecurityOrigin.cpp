137501f754f72b213aa6d9df9f81af13b8da2acd
jannhorn@googlemail.com <jannhorn@googlemail.com>
2015-05-18 12:46:05 +0000

    make file: an effectively unique origin
    This, if Chrome is started without flags that reduce security,
    prevents local files from being same-origin with other files
    at the same path. This can especially be an issue with
    pseudofilesystems in which the meaning of a path often changes.
    
    BUG=429542
    
    Review URL: https://codereview.chromium.org/1140203002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195464 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/weborigin/SecurityOrigin.cpp
+++ third_party/WebKit/Source/platform/weborigin/SecurityOrigin.cpp
@@ -135,9 +135,6 @@ SecurityOrigin::SecurityOrigin(const KURL& url)
 
     // By default, only local SecurityOrigins can load local resources.
     m_canLoadLocalResources = isLocal();
-
-    if (m_canLoadLocalResources)
-        m_filePath = url.path(); // In case enforceFilePathSeparation() is called.
 }
 
 SecurityOrigin::SecurityOrigin()
@@ -158,7 +155,6 @@ SecurityOrigin::SecurityOrigin(const SecurityOrigin* other)
     : m_protocol(other->m_protocol.isolatedCopy())
     , m_host(other->m_host.isolatedCopy())
     , m_domain(other->m_domain.isolatedCopy())
-    , m_filePath(other->m_filePath.isolatedCopy())
     , m_port(other->m_port)
     , m_isUnique(other->m_isUnique)
     , m_universalAccess(other->m_universalAccess)
@@ -276,10 +272,7 @@ bool SecurityOrigin::passesFileCheck(const SecurityOrigin* other) const
 {
     ASSERT(isLocal() && other->isLocal());
 
-    if (!m_enforceFilePathSeparation && !other->m_enforceFilePathSeparation)
-        return true;
-
-    return (m_filePath == other->m_filePath);
+    return !m_enforceFilePathSeparation && !other->m_enforceFilePathSeparation;
 }
 
 bool SecurityOrigin::canRequest(const KURL& url) const
