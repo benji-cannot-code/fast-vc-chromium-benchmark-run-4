8d84a29e667a3d41b9312522c07c46812502c5ed
huayong.xu@samsung.com <huayong.xu@samsung.com>
2015-05-18 09:52:15 +0000

    Oilpan: remove incorrect LayoutPart transition type usage.
    This is cleanup of https://codereview.chromium.org/869323003.
    
    BUG=398342
    
    Review URL: https://codereview.chromium.org/1142033002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195459 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutPart.cpp
+++ third_party/WebKit/Source/core/layout/LayoutPart.cpp
@@ -311,7 +311,7 @@ bool LayoutPart::setWidgetGeometry(const LayoutRect& frame)
     if (widget->frameRect() == newFrame)
         return false;
 
-    RefPtrWillBeRawPtr<LayoutPart> protector(this);
+    RefPtr<LayoutPart> protector(this);
     RefPtrWillBeRawPtr<Node> protectedNode(node());
     widget->setFrameRect(newFrame);
     return widget->frameRect().size() != newFrame.size();
