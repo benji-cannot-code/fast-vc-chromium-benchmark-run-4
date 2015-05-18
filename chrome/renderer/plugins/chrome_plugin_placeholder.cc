ea5fcbd9994f3e1d6637eaa06e1ea72baa72769e
tommycli <tommycli@chromium.org>
2015-05-18 15:58:10 -0700

    Plugin Placeholders: Refactor for platforms that don't support plugins
    This patch removes a lot of plugin placeholder code from builds that don't support plugins (enable_plugins==0).
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1126073003
    
    Cr-Commit-Position: refs/heads/master@{#330444}

--- chrome/renderer/plugins/chrome_plugin_placeholder.cc
+++ chrome/renderer/plugins/chrome_plugin_placeholder.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/plugins/plugin_preroller.h"
 #include "chrome/renderer/plugins/plugin_uma.h"
 #include "components/content_settings/content/common/content_settings_messages.h"
-#include "content/app/strings/grit/content_strings.h"
 #include "content/public/common/context_menu_params.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_thread.h"
@@ -49,11 +48,6 @@ namespace {
 const plugins::PluginPlaceholder* g_last_active_menu = NULL;
 }  // namespace
 
-// The placeholder is loaded in normal web renderer processes, so it should not
-// have a chrome:// scheme that might let it be confused with a WebUI page.
-const char ChromePluginPlaceholder::kPluginPlaceholderDataURL[] =
-    "data:text/html,pluginplaceholderdata";
-
 ChromePluginPlaceholder::ChromePluginPlaceholder(
     content::RenderFrame* render_frame,
     blink::WebLocalFrame* frame,
@@ -63,8 +57,7 @@ ChromePluginPlaceholder::ChromePluginPlaceholder(
     : plugins::LoadablePluginPlaceholder(render_frame,
                                          frame,
                                          params,
-                                         html_data,
-                                         GURL(kPluginPlaceholderDataURL)),
+                                         html_data),
       status_(ChromeViewHostMsg_GetPluginInfo_Status::kAllowed),
       title_(title),
 #if defined(ENABLE_PLUGIN_INSTALLATION)
@@ -92,7 +85,7 @@ ChromePluginPlaceholder::~ChromePluginPlaceholder() {
 }
 
 // static
-ChromePluginPlaceholder* ChromePluginPlaceholder::CreateMissingPlugin(
+ChromePluginPlaceholder* ChromePluginPlaceholder::CreateLoadableMissingPlugin(
     content::RenderFrame* render_frame,
     WebLocalFrame* frame,
     const WebPluginParams& params) {
@@ -106,34 +99,9 @@ ChromePluginPlaceholder* ChromePluginPlaceholder::CreateMissingPlugin(
 
   std::string html_data = webui::GetI18nTemplateHtml(template_html, &values);
 
-  // |missing_plugin| will destroy itself when its WebViewPlugin is going away.
-  ChromePluginPlaceholder* missing_plugin = new ChromePluginPlaceholder(
-      render_frame, frame, params, html_data, params.mimeType);
-  missing_plugin->set_allow_loading(true);
-  return missing_plugin;
-}
-
-// static
-ChromePluginPlaceholder* ChromePluginPlaceholder::CreateErrorPlugin(
-    content::RenderFrame* render_frame,
-    const base::FilePath& file_path) {
-  base::DictionaryValue values;
-  values.SetString("message",
-                   l10n_util::GetStringUTF8(IDS_PLUGIN_INITIALIZATION_ERROR));
-
-  const base::StringPiece template_html(
-      ResourceBundle::GetSharedInstance().GetRawDataResource(
-          IDR_BLOCKED_PLUGIN_HTML));
-  std::string html_data = webui::GetI18nTemplateHtml(template_html, &values);
-
-  WebPluginParams params;
-  // |missing_plugin| will destroy itself when its WebViewPlugin is going away.
-  ChromePluginPlaceholder* plugin = new ChromePluginPlaceholder(
-      render_frame, NULL, params, html_data, params.mimeType);
-
-  RenderThread::Get()->Send(new ChromeViewHostMsg_CouldNotLoadPlugin(
-      plugin->routing_id(), file_path));
-  return plugin;
+  // Will destroy itself when its WebViewPlugin is going away.
+  return new ChromePluginPlaceholder(render_frame, frame, params, html_data,
+                                     params.mimeType);
 }
 
 // static
@@ -177,10 +145,8 @@ ChromePluginPlaceholder* ChromePluginPlaceholder::CreateBlockedPlugin(
   ChromePluginPlaceholder* blocked_plugin = new ChromePluginPlaceholder(
       render_frame, frame, params, html_data, name);
 
-#if defined(ENABLE_PLUGINS)
   if (!poster_info.poster_attribute.empty())
     blocked_plugin->BlockForPowerSaverPoster();
-#endif
   blocked_plugin->SetPluginInfo(info);
   blocked_plugin->SetIdentifier(identifier);
   return blocked_plugin;
@@ -306,10 +272,8 @@ void ChromePluginPlaceholder::OnMenuAction(int request_id, unsigned action) {
   switch (action) {
     case chrome::MENU_COMMAND_PLUGIN_RUN: {
       RenderThread::Get()->RecordAction(UserMetricsAction("Plugin_Load_Menu"));
-#if defined(ENABLE_PLUGINS)
       MarkPluginEssential(
           content::PluginInstanceThrottler::UNTHROTTLE_METHOD_BY_CLICK);
-#endif
       LoadPlugin();
       break;
     }
@@ -329,7 +293,6 @@ void ChromePluginPlaceholder::OnMenuClosed(int request_id) {
 }
 
 void ChromePluginPlaceholder::ShowContextMenu(const WebMouseEvent& event) {
-#if !defined(OS_ANDROID)  // The context menu is not applicable on Android.
   if (context_menu_request_id_)
     return;  // Don't allow nested context menu requests.
 
@@ -365,12 +328,10 @@ void ChromePluginPlaceholder::ShowContextMenu(const WebMouseEvent& event) {
 
   context_menu_request_id_ = render_frame()->ShowContextMenu(this, params);
   g_last_active_menu = this;
-#endif  // OS_ANDROID
 }
 
 blink::WebPlugin* ChromePluginPlaceholder::CreatePlugin() {
   scoped_ptr<content::PluginInstanceThrottler> throttler;
-#if defined(ENABLE_PLUGINS)
   // If the plugin has already been marked essential in its placeholder form,
   // we shouldn't create a new throttler and start the process all over again.
   if (power_saver_enabled()) {
@@ -381,7 +342,6 @@ blink::WebPlugin* ChromePluginPlaceholder::CreatePlugin() {
                         l10n_util::GetStringFUTF16(IDS_PLUGIN_BLOCKED, title_),
                         throttler.get());
   }
-#endif
   return render_frame()->CreatePlugin(GetFrame(), GetPluginInfo(),
                                       GetPluginParams(), throttler.Pass());
 }
--- chrome/renderer/plugins/chrome_plugin_placeholder.h
+++ chrome/renderer/plugins/chrome_plugin_placeholder.h
@@ -33,8 +33,6 @@ class ChromePluginPlaceholder : public plugins::LoadablePluginPlaceholder,
                                 public content::RenderProcessObserver,
                                 public content::ContextMenuClient {
  public:
-  static const char kPluginPlaceholderDataURL[];
-
   static ChromePluginPlaceholder* CreateBlockedPlugin(
       content::RenderFrame* render_frame,
       blink::WebLocalFrame* frame,
@@ -47,14 +45,10 @@ class ChromePluginPlaceholder : public plugins::LoadablePluginPlaceholder,
       const PlaceholderPosterInfo& poster_info);
 
   // Creates a new WebViewPlugin with a MissingPlugin as a delegate.
-  static ChromePluginPlaceholder* CreateMissingPlugin(
+  static ChromePluginPlaceholder* CreateLoadableMissingPlugin(
       content::RenderFrame* render_frame,
       blink::WebLocalFrame* frame,
       const blink::WebPluginParams& params);
-
-  static ChromePluginPlaceholder* CreateErrorPlugin(
-      content::RenderFrame* render_frame,
-      const base::FilePath& plugin_path);
 
   void SetStatus(ChromeViewHostMsg_GetPluginInfo_Status status);
 
