4b8a9f78176ee27357791b6c8d9ab91b193cd572
bustamante <bustamante@google.com>
2015-05-19 15:39:42 -0700

    Update experiment test to only verify /exptest/... are bypassed
    The exp_directive test verifies the experiment flag is passed to the
    page, by bypassing any request to http://aws1.mdw.la/exptest/... when
    the flag --data-reduction-proxy-experiment=test is set.  Which works
    as expected.
    
    However the favicon request from http://aws1.mdw.la/favicon.ico won't
    be bypassed, causing the test to fail.  To fix the test, I'm adding an
    optional url_pattern parameter to the bypass verification, that will
    verify only /exptest/ urls are checked.
    
    BUG=488232
    
    Review URL: https://codereview.chromium.org/1141923004
    
    Cr-Commit-Position: refs/heads/master@{#330644}

--- tools/chrome_proxy/integration_tests/chrome_proxy_measurements.py
+++ tools/chrome_proxy/integration_tests/chrome_proxy_measurements.py
@@ -289,7 +289,7 @@ class ChromeProxyExpDirective(ChromeProxyValidation):
     options.AppendExtraBrowserArgs('--data-reduction-proxy-experiment=test')
 
   def AddResults(self, tab, results):
-    self._metrics.AddResultsForBypass(tab, results)
+    self._metrics.AddResultsForBypass(tab, results, url_pattern='/exptest/')
 
 
 class ChromeProxyHTTPToDirectFallback(ChromeProxyValidation):
