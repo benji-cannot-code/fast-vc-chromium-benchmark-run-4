6664e807e0fa02dd777ff9e70fe801aebb5ef537
spang <spang@chromium.org>
2015-05-19 10:20:41 -0700

    Remove unused blitting functions from ui/gfx/blit.cc
    There are no remaining call sites for either BlitContextToCanvas or
    BlitCanvasToContext. Also remove some leftover #includes.
    
    TEST=Linux built compiles & links.
    
    Review URL: https://codereview.chromium.org/1138753003
    
    Cr-Commit-Position: refs/heads/master@{#330543}

--- content/plugin/webplugin_proxy.cc
+++ content/plugin/webplugin_proxy.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/ext/platform_canvas.h"
 #include "skia/ext/platform_device.h"
 #include "third_party/WebKit/public/web/WebBindings.h"
-#include "ui/gfx/blit.h"
 #include "ui/gfx/canvas.h"
 #include "url/url_constants.h"
 
