79719e6ba71dd69837db2abcaed6e32a6ee582c9
mkwst@chromium.org <mkwst@chromium.org>
2015-05-19 06:15:08 +0000

    Drop the hideous IPAddressAsDomain behavior from OriginAccessEntry.
    We have better ways of writing 'document.domain' tests these days; which
    means we can get rid of some really ugly behavior in OriginAccessEntry.
    
    BUG=477578
    R=rsleevi@chromium.org
    
    Review URL: https://codereview.chromium.org/1134253003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195518 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/weborigin/OriginAccessEntry.cpp
+++ third_party/WebKit/Source/platform/weborigin/OriginAccessEntry.cpp
@@ -65,11 +65,10 @@ bool HostIsIPAddress(const String& host)
 
 }
 
-OriginAccessEntry::OriginAccessEntry(const String& protocol, const String& host, SubdomainSetting subdomainSetting, IPAddressSetting ipAddressSetting)
+OriginAccessEntry::OriginAccessEntry(const String& protocol, const String& host, SubdomainSetting subdomainSetting)
     : m_protocol(protocol.lower())
     , m_host(host.lower())
     , m_subdomainSettings(subdomainSetting)
-    , m_ipAddressSettings(ipAddressSetting)
     , m_hostIsPublicSuffix(false)
 {
     ASSERT(subdomainSetting == AllowSubdomains || subdomainSetting == DisallowSubdomains);
@@ -104,8 +103,8 @@ OriginAccessEntry::MatchResult OriginAccessEntry::matchesOrigin(const SecurityOr
     if (m_subdomainSettings == DisallowSubdomains)
         return DoesNotMatchOrigin;
 
-    // Don't try to do subdomain matching on IP addresses (except for testing).
-    if (m_hostIsIPAddress && m_ipAddressSettings == TreatIPAddressAsIPAddress)
+    // Don't try to do subdomain matching on IP addresses.
+    if (m_hostIsIPAddress)
         return DoesNotMatchOrigin;
 
     // Match subdomains.
--- third_party/WebKit/Source/platform/weborigin/SecurityPolicy.cpp
+++ third_party/WebKit/Source/platform/weborigin/SecurityPolicy.cpp
@@ -177,7 +177,7 @@ void SecurityPolicy::addOriginAccessWhitelistEntry(const SecurityOrigin& sourceO
         result.storedValue->value = adoptPtr(new OriginAccessWhiteList);
 
     OriginAccessWhiteList* list = result.storedValue->value.get();
-    list->append(OriginAccessEntry(destinationProtocol, destinationDomain, allowDestinationSubdomains ? OriginAccessEntry::AllowSubdomains : OriginAccessEntry::DisallowSubdomains, OriginAccessEntry::TreatIPAddressAsIPAddress));
+    list->append(OriginAccessEntry(destinationProtocol, destinationDomain, allowDestinationSubdomains ? OriginAccessEntry::AllowSubdomains : OriginAccessEntry::DisallowSubdomains));
 }
 
 void SecurityPolicy::removeOriginAccessWhitelistEntry(const SecurityOrigin& sourceOrigin, const String& destinationProtocol, const String& destinationDomain, bool allowDestinationSubdomains)
@@ -194,7 +194,8 @@ void SecurityPolicy::removeOriginAccessWhitelistEntry(const SecurityOrigin& sour
         return;
 
     OriginAccessWhiteList* list = it->value.get();
-    size_t index = list->find(OriginAccessEntry(destinationProtocol, destinationDomain, allowDestinationSubdomains ? OriginAccessEntry::AllowSubdomains : OriginAccessEntry::DisallowSubdomains, OriginAccessEntry::TreatIPAddressAsIPAddress));
+    size_t index = list->find(OriginAccessEntry(destinationProtocol, destinationDomain, allowDestinationSubdomains ? OriginAccessEntry::AllowSubdomains : OriginAccessEntry::DisallowSubdomains));
+
     if (index == kNotFound)
         return;
 
