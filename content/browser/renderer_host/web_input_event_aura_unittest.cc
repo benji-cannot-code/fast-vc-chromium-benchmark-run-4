cd2d8ca1a6195f5dc576bc25464e4efb93ab9cfa
kpschoedel <kpschoedel@chromium.org>
2015-05-19 12:45:41 -0700

    Set WebKeyboardEvent.domCode on Windows.
    BUG=489446
    
    Review URL: https://codereview.chromium.org/1147843003
    
    Cr-Commit-Position: refs/heads/master@{#330586}

--- content/browser/renderer_host/web_input_event_aura_unittest.cc
+++ content/browser/renderer_host/web_input_event_aura_unittest.cc
@@ -33,6 +33,8 @@ TEST(WebInputEventAuraTest, TestMakeWebKeyboardEvent) {
     // However, modifier bit for Control in |webkit_event| should be set.
     EXPECT_EQ(blink::WebInputEvent::ControlKey | blink::WebInputEvent::IsLeft,
               webkit_event.modifiers);
+    EXPECT_EQ(static_cast<int>(ui::DomCode::CONTROL_LEFT),
+              webkit_event.domCode);
   }
   {
     // Release Ctrl.
@@ -41,6 +43,8 @@ TEST(WebInputEventAuraTest, TestMakeWebKeyboardEvent) {
     blink::WebKeyboardEvent webkit_event = MakeWebKeyboardEvent(event);
     // However, modifier bit for Control in |webkit_event| shouldn't be set.
     EXPECT_EQ(blink::WebInputEvent::IsLeft, webkit_event.modifiers);
+    EXPECT_EQ(static_cast<int>(ui::DomCode::CONTROL_LEFT),
+              webkit_event.domCode);
   }
 #if defined(USE_X11)
   const int kLocationModifiers =
