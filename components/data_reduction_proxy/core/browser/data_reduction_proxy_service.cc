e39de10f46e20f79a2337418df3eb25014d229c6
bengr <bengr@chromium.org>
2015-05-19 12:00:38 -0700

    Fix Data Saver test in Cronet
    Task posts in Data Saver logic were delaying initialization
    unnecessarily in Cronet, and consequently, the first
    request after initialization wasn't necessarily proxied.
    This CL makes the initialization logic synchronous when
    Data Saver's UI and IO task runners are on the same thread.
    
    BUG=461910
    
    Review URL: https://codereview.chromium.org/1143503005
    
    Cr-Commit-Position: refs/heads/master@{#330579}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_service.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_service.cc
@@ -116,6 +116,10 @@ void DataReductionProxyService::SetProxyPrefs(bool enabled,
                                               bool alternative_enabled,
                                               bool at_startup) {
   DCHECK(CalledOnValidThread());
+  if (io_task_runner_->BelongsToCurrentThread()) {
+    io_data_->SetProxyPrefs(enabled, alternative_enabled, at_startup);
+    return;
+  }
   io_task_runner_->PostTask(
       FROM_HERE,
       base::Bind(&DataReductionProxyIOData::SetProxyPrefs,
@@ -124,6 +128,10 @@ void DataReductionProxyService::SetProxyPrefs(bool enabled,
 
 void DataReductionProxyService::RetrieveConfig() {
   DCHECK(CalledOnValidThread());
+  if (io_task_runner_->BelongsToCurrentThread()) {
+    io_data_->RetrieveConfig();
+    return;
+  }
   io_task_runner_->PostTask(
       FROM_HERE,
       base::Bind(&DataReductionProxyIOData::RetrieveConfig, io_data_));
