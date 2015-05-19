c403aea88fbbf5cee1b46a498da189a819bc272a
enne <enne@chromium.org>
2015-05-19 14:52:51 -0700

    cc: Remove LTHCommon temporary property trees
    This was a missing part of putting property trees on the compositor
    thread. Because these temporary property trees are being used instead of
    the ones on LayerTreeImpl, it makes future calls that touch property
    trees through LayerImpls have bogus indices because the trees on the
    LayerTreeImpl are not the same as the temporary property trees.
    
    R=ajuma@chromium.org,vollick@chromium.org
    BUG=481585
    
    Review URL: https://codereview.chromium.org/1146523005
    
    Cr-Commit-Position: refs/heads/master@{#330621}

--- cc/trees/layer_tree_host_common.h
+++ cc/trees/layer_tree_host_common.h
@@ -103,9 +103,6 @@ class CC_EXPORT LayerTreeHostCommon {
         LayerType* root_layer,
         const gfx::Size& device_viewport_size,
         RenderSurfaceLayerListType* render_surface_layer_list);
-
-   private:
-    PropertyTrees temporary_property_trees;
   };
 
   typedef CalcDrawPropsInputs<Layer, RenderSurfaceLayerList>
@@ -237,10 +234,8 @@ void LayerTreeHostCommon::CallFunctionForSubtree(LayerType* layer,
   }
 }
 
-CC_EXPORT PropertyTrees* GetPropertyTrees(Layer* layer,
-                                          PropertyTrees* trees_from_inputs);
-CC_EXPORT PropertyTrees* GetPropertyTrees(LayerImpl* layer,
-                                          PropertyTrees* trees_from_inputs);
+CC_EXPORT PropertyTrees* GetPropertyTrees(Layer* layer);
+CC_EXPORT PropertyTrees* GetPropertyTrees(LayerImpl* layer);
 
 template <typename LayerType, typename RenderSurfaceLayerListType>
 LayerTreeHostCommon::CalcDrawPropsInputsForTesting<LayerType,
@@ -267,7 +262,7 @@ LayerTreeHostCommon::CalcDrawPropsInputsForTesting<LayerType,
           false,
           render_surface_layer_list,
           0,
-          GetPropertyTrees(root_layer, &temporary_property_trees)) {
+          GetPropertyTrees(root_layer)) {
   DCHECK(root_layer);
   DCHECK(render_surface_layer_list);
 }
@@ -296,7 +291,7 @@ LayerTreeHostCommon::CalcDrawPropsInputsForTesting<LayerType,
           false,
           render_surface_layer_list,
           0,
-          GetPropertyTrees(root_layer, &temporary_property_trees)) {
+          GetPropertyTrees(root_layer)) {
   DCHECK(root_layer);
   DCHECK(render_surface_layer_list);
 }
