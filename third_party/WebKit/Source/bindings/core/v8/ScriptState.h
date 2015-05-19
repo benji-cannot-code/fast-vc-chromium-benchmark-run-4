c94f7034ae13bbbcae014f307573495a109d1d7e
yurys@chromium.org <yurys@chromium.org>
2015-05-19 03:50:37 +0000

    Don't cast debugger v8::Context to ScriptState
    Otherwise we will end up with null pointer crash.
    
    BUG=482290
    
    Review URL: https://codereview.chromium.org/1116893002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195509 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/ScriptState.h
+++ third_party/WebKit/Source/bindings/core/v8/ScriptState.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CoreExport.h"
 #include "wtf/RefCounted.h"
 #include "wtf/Vector.h"
+#include <v8-debug.h>
 #include <v8.h>
 
 namespace blink {
@@ -54,6 +55,17 @@ public:
     static ScriptState* current(v8::Isolate* isolate)
     {
         return from(isolate->GetCurrentContext());
+    }
+
+    // Debugger context doesn't have associated ScriptState and when current
+    // context is debugger it should be treated as if context stack was empty.
+    static bool hasCurrentScriptState(v8::Isolate* isolate)
+    {
+        v8::HandleScope scope(isolate);
+        v8::Local<v8::Context> context = isolate->GetCurrentContext();
+        if (context.IsEmpty())
+            return false;
+        return context != v8::Debug::GetDebugContext();
     }
 
     static ScriptState* from(v8::Local<v8::Context> context)
