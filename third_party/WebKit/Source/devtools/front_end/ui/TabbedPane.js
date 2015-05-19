fdb5fcfb0bfbd2e437bedba78afbfe00368425ff
samli@chromium.org <samli@chromium.org>
2015-05-19 01:36:57 +0000

    Devtools Animations: Move animation timeline to elements panel
    With the experiment enabled, this change removes the animations sidebar
    pane. The timeline appears when the animations button is toggled, revealing
    the timeline above the Styles pane and taking space from the DOM view.
    
    BUG=447083
    
    Review URL: https://codereview.chromium.org/1041043003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195500 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/TabbedPane.js
+++ third_party/WebKit/Source/devtools/front_end/ui/TabbedPane.js
@@ -755,6 +755,8 @@ WebInspector.TabbedPane.prototype = {
 
     _updateTabSlider: function()
     {
+        if (!this._currentTab)
+            return;
         if (!this._sliderEnabled)
             return;
         var left = 0;
