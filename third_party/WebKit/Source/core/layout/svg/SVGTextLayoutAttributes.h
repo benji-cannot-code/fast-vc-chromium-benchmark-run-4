7b0acfb1666e5956b14ab4169d8eb676453dd1b5
fs@opera.com <fs@opera.com>
2015-05-19 21:54:51 +0000

    Inline SVGTextLayoutAttributes::emptyValue
    This function just returns a simple float - inlining it allows the
    compiler to generate more efficient code (and smaller - ~.5k) code
    since the relatively high overhead call can be eliminated.
    Take the opportunity to change the "empty value" into NaN (instead of
    "max() - 1"), with corresponding semantic changes required by that.
    
    BUG=486669
    
    Review URL: https://codereview.chromium.org/1129333004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195555 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/svg/SVGTextLayoutAttributes.h
+++ third_party/WebKit/Source/core/layout/svg/SVGTextLayoutAttributes.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/layout/svg/SVGTextMetrics.h"
 #include "wtf/HashMap.h"
+#include "wtf/MathExtras.h"
 #include "wtf/Noncopyable.h"
 #include "wtf/Vector.h"
 
@@ -48,7 +49,8 @@ public:
     SVGTextLayoutAttributes(LayoutSVGInlineText*);
 
     void clear();
-    static float emptyValue();
+    static float emptyValue() { return std::numeric_limits<float>::quiet_NaN(); }
+    static bool isEmptyValue(float value) { return std::isnan(value); }
 
     LayoutSVGInlineText* context() const { return m_context; }
 
