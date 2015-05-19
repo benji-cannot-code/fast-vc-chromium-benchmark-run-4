6559c64687d40e3f853b46d56a815f571e92a339
cmumford@chromium.org <cmumford@chromium.org>
2015-05-19 16:58:40 +0000

    IndexedDB: Throwing TypeError if IDBObjectStore.getAll(..., maxCount=0)
    Modification to prior IDBObjectStore.getAll implementation which treated
    maxCount=0 as maxCount=Infinity. This change will throw a TypeError exception
    if maxCount=0.
    
    BUG=457450
    
    Review URL: https://codereview.chromium.org/1142663002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195533 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/indexeddb/IDBDatabase.h
+++ third_party/WebKit/Source/modules/indexeddb/IDBDatabase.h
@@ -132,6 +132,7 @@ public:
     static const char transactionInactiveErrorMessage[];
     static const char transactionReadOnlyErrorMessage[];
     static const char databaseClosedErrorMessage[];
+    static const char notValidMaxCountErrorMessage[];
 
 private:
     IDBDatabase(ExecutionContext*, PassOwnPtr<WebIDBDatabase>, IDBDatabaseCallbacks*);
