0ecc428e42943264f09c592e5d71821e997cf255
sigbjornf@opera.com <sigbjornf@opera.com>
2015-05-20 10:28:47 +0000

    Various ASan exemptions to allow Oilpan pre-sweep poisoning of unmarkeds.
    Heap objects must not access other dead objects while finalizing.
    To help catch out those that do, the Oilpan GC can now poison 
    unmarked, to-be-swept objects. To be able to make use of that
    with ASan enabled, a couple of benign accesses that run afoul
    of that strict check are still needed & must be allowed to
    make this feasible.
    
    Here provided together -- the ones related to Timer stopping &
    removal from the timer heap aren't something we'd typically want
    to impose one the Blink codebase in general, but beside the
    goal of better ASan coverage, the timer heap will soon be removed
    and the need for these should fall away.
    
    R=haraken,tkent,yutak
    BUG=420515
    
    Review URL: https://codereview.chromium.org/1120943002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195609 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/Timer.h
+++ third_party/WebKit/Source/platform/Timer.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define Timer_h
 
 #include "platform/PlatformExport.h"
+#include "platform/heap/AddressSanitizer.h"
 #include "platform/heap/Handle.h"
 #include "public/platform/WebTraceLocation.h"
 #include "wtf/Noncopyable.h"
@@ -73,6 +74,7 @@ public:
 private:
     virtual void fired() = 0;
 
+    NO_LAZY_SWEEP_SANITIZE_ADDRESS
     virtual double alignedFireTime(double fireTime) const { return fireTime; }
 
     void checkConsistency() const;
@@ -80,6 +82,7 @@ private:
 
     void setNextFireTime(double);
 
+    NO_LAZY_SWEEP_SANITIZE_ADDRESS
     bool inHeap() const { return m_heapIndex != -1; }
 
     bool hasValidHeapPosition() const;
@@ -93,6 +96,7 @@ private:
     void heapPop();
     void heapPopMin();
 
+    NO_LAZY_SWEEP_SANITIZE_ADDRESS
     Vector<TimerBase*>& timerHeap() const { ASSERT(m_cachedThreadGlobalTimerHeap); return *m_cachedThreadGlobalTimerHeap; }
 
     double m_nextFireTime; // 0 if inactive
@@ -155,6 +159,7 @@ private:
     TimerFiredFunction m_function;
 };
 
+NO_LAZY_SWEEP_SANITIZE_ADDRESS
 inline bool TimerBase::isActive() const
 {
     ASSERT(m_thread == currentThread());
