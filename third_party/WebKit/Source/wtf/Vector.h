fe8c24987baaa87961c086b9997e25b6fc0958d8
mikhail.pozdnyakov@intel.com <mikhail.pozdnyakov@intel.com>
2015-05-20 04:15:42 +0000

    WTF::Vector: null pointer check for mem functions parameters
    Intel Inspector XE memory profile detected plenty of memory functions (memcpy, memmove) calls with null arguments in Blink. These calls were caused by valid using of WTF:Vector API (copy constructor, assignment operator, others). This brought risk of undefined behavior
    (pls. see https://groups.google.com/forum/#!topic/comp.lang.c/aJN8dfH0p6k and a more concrete example with GCC 4.9: https://gcc.gnu.org/gcc-4.9/porting_to.html).
    
    The problem is fixed now by introducing null-checks to the corresponding VectorCopier and VectorMover methods.
    
    Review URL: https://codereview.chromium.org/1123803005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195581 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/Vector.h
+++ third_party/WebKit/Source/wtf/Vector.h
@@ -168,11 +168,13 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
     {
         static void move(const T* src, const T* srcEnd, T* dst)
         {
-            memcpy(dst, src, reinterpret_cast<const char*>(srcEnd) - reinterpret_cast<const char*>(src));
+            if (LIKELY(dst && src))
+                memcpy(dst, src, reinterpret_cast<const char*>(srcEnd) - reinterpret_cast<const char*>(src));
         }
         static void moveOverlapping(const T* src, const T* srcEnd, T* dst)
         {
-            memmove(dst, src, reinterpret_cast<const char*>(srcEnd) - reinterpret_cast<const char*>(src));
+            if (LIKELY(dst && src))
+                memmove(dst, src, reinterpret_cast<const char*>(srcEnd) - reinterpret_cast<const char*>(src));
         }
         static void swap(T* src, T* srcEnd, T* dst)
         {
@@ -202,7 +204,8 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
     {
         static void uninitializedCopy(const T* src, const T* srcEnd, T* dst)
         {
-            memcpy(dst, src, reinterpret_cast<const char*>(srcEnd) - reinterpret_cast<const char*>(src));
+            if (LIKELY(dst && src))
+                memcpy(dst, src, reinterpret_cast<const char*>(srcEnd) - reinterpret_cast<const char*>(src));
         }
         template<typename U>
         static void uninitializedCopy(const U* src, const U* srcEnd, T* dst)
