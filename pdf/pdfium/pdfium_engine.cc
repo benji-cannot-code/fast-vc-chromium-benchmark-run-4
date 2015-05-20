cc9f7403a857b96be8b7ea356d3bb380a353eb3a
thestig <thestig@chromium.org>
2015-05-20 14:04:33 -0700

    PDF: On Linux, convert font names to UTF-8 if needed.
    BUG=419410
    
    Review URL: https://codereview.chromium.org/1142313002
    
    Cr-Commit-Position: refs/heads/master@{#330801}

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <math.h>
 
+#include "base/i18n/icu_encoding_detection.h"
+#include "base/i18n/icu_string_conversions.h"
 #include "base/json/json_writer.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
@@ -224,10 +226,22 @@ void* MapFont(struct _FPDF_SYSFONTINFO*, int weight, int italic,
   }
 
   if (i == arraysize(PDFFontSubstitutions)) {
-    // TODO(kochi): Pass the face in UTF-8. If face is not encoded in UTF-8,
-    // convert to UTF-8 before passing.
-    description.set_face(face);
+    // Convert to UTF-8 before calling set_face().
+    std::string face_utf8;
+    if (base::IsStringUTF8(face)) {
+      face_utf8 = face;
+    } else {
+      std::string encoding;
+      if (base::DetectEncoding(face, &encoding)) {
+        // ConvertToUtf8AndNormalize() clears |face_utf8| on failure.
+        base::ConvertToUtf8AndNormalize(face, encoding, &face_utf8);
+      }
+    }
 
+    if (face_utf8.empty())
+      return nullptr;
+
+    description.set_face(face_utf8);
     description.set_weight(WeightToBrowserFontTrustedWeight(weight));
     description.set_italic(italic > 0);
   }
