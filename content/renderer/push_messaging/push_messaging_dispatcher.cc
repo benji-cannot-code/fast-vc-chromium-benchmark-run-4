fd933fbdb1959f0080ffd8db998cc6f4a328d99c
peter <peter@chromium.org>
2015-05-20 08:12:34 -0700

    Include a deprecation warning when people only use "gcm_user_visible_only".
    Usage of the "gcm_user_visible_only" Manifest key has been deprecated in favor
    of using PushSubscriptionOptions when subscribing. Show a deprecation
    warning when only the former is used.
    
    BUG=471534
    
    Review URL: https://codereview.chromium.org/1145163002
    
    Cr-Commit-Position: refs/heads/master@{#330735}

--- content/renderer/push_messaging/push_messaging_dispatcher.cc
+++ content/renderer/push_messaging/push_messaging_dispatcher.cc
@@ -16,11 +16,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/modules/push_messaging/WebPushError.h"
 #include "third_party/WebKit/public/platform/modules/push_messaging/WebPushSubscription.h"
 #include "third_party/WebKit/public/platform/modules/push_messaging/WebPushSubscriptionOptions.h"
+#include "third_party/WebKit/public/web/WebConsoleMessage.h"
+#include "third_party/WebKit/public/web/WebLocalFrame.h"
 #include "url/gurl.h"
 
 using blink::WebString;
 
 namespace content {
+
+const char kManifestDeprecationWarning[] =
+    "The 'gcm_user_visible_only' manifest key is deprecated and will be "
+    "removed in Chrome 45, around August 2015. Use pushManager.subscribe({"
+    "userVisibleOnly: true}) instead. See https://goo.gl/RHFwWx for more "
+    "details.";
 
 PushMessagingDispatcher::PushMessagingDispatcher(RenderFrame* render_frame)
     : RenderFrameObserver(render_frame) {
@@ -75,8 +83,17 @@ void PushMessagingDispatcher::DoSubscribe(
     return;
   }
 
-  // TODO(peter): Display a deprecation warning if gcm_user_visible_only is
-  // set to true. See https://crbug.com/471534
+  // Support for the "gcm_user_visible_only" Manifest key has been deprecated
+  // in favor of the userVisibleOnly subscription option, and will be removed
+  // in a future Chrome release. Inform developers of this deprecation.
+  if (manifest.gcm_user_visible_only && !options.userVisibleOnly) {
+    blink::WebConsoleMessage message(
+        blink::WebConsoleMessage::LevelWarning,
+        blink::WebString::fromUTF8(kManifestDeprecationWarning));
+
+    render_frame()->GetWebFrame()->addMessageToConsole(message);
+  }
+
   const bool user_visible = manifest.gcm_user_visible_only ||
                             options.userVisibleOnly;
 
