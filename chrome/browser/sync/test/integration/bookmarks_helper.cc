72e31f196b29b5cb67ccd95bc0421d4f0eb35f49
pkotwicz <pkotwicz@chromium.org>
2015-05-20 14:21:59 -0700

    Allow favicons for synced bookmarks to expire
    This CL prevents sync from updating the "last updated time" for bookmark
    favicons each time that Chrome is started. The "last updated time" determines
    when the favicon is redownloaded from the web (in case it has been updated).
    
    Sync calls HistoryBackend::MergeFavicon() for each bookmark favicon on startup.
    This CL stops HistoryBackend::MergeFavicon() from updating the "last updated
    time" when MergeFavicon() is called with the same bitmap data as already stored
    in the database (no new bitmap data).
    
    BUG=481414
    TEST=TwoClientBookmarksSyncTest.SC_UpdatingTitleDoesNotUpdateFaviconLastUpdatedTime
    
    Review URL: https://codereview.chromium.org/1128343004
    
    Cr-Commit-Position: refs/heads/master@{#330802}

--- chrome/browser/sync/test/integration/bookmarks_helper.cc
+++ chrome/browser/sync/test/integration/bookmarks_helper.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/path_service.h"
 #include "base/rand_util.h"
+#include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
@@ -268,6 +269,25 @@ void SetFaviconImpl(Profile* profile,
     // Wait for the BookmarkModel to fetch the updated favicon and for the new
     // favicon to be sent to BookmarkChangeProcessor.
     GetFaviconData(model, node);
+}
+
+// Expires the favicon for |profile| and |node|. |profile| may be
+// |test()->verifier()|.
+void ExpireFaviconImpl(Profile* profile, const BookmarkNode* node) {
+  favicon::FaviconService* favicon_service =
+      FaviconServiceFactory::GetForProfile(profile,
+                                           ServiceAccessType::EXPLICIT_ACCESS);
+  favicon_service->SetFaviconOutOfDateForPage(node->url());
+}
+
+// Called asynchronously from CheckFaviconExpired() with the favicon data from
+// the database.
+void OnGotFaviconForExpiryCheck(
+    const base::Closure& callback,
+    const favicon_base::FaviconRawBitmapResult& bitmap_result) {
+  ASSERT_TRUE(bitmap_result.is_valid());
+  ASSERT_TRUE(bitmap_result.expired);
+  callback.Run();
 }
 
 // Wait for all currently scheduled tasks on the history thread for all
@@ -584,6 +604,41 @@ void SetFavicon(int profile,
                  icon_url,
                  image,
                  favicon_source);
+}
+
+void ExpireFavicon(int profile, const BookmarkNode* node) {
+  BookmarkModel* model = GetBookmarkModel(profile);
+  ASSERT_EQ(bookmarks::GetBookmarkNodeByID(model, node->id()), node)
+      << "Node " << node->GetTitle() << " does not belong to "
+      << "Profile " << profile;
+  ASSERT_EQ(BookmarkNode::URL, node->type()) << "Node " << node->GetTitle()
+                                             << " must be a url.";
+  ASSERT_EQ(1u, urls_with_favicons_->count(node->url()));
+
+  if (sync_datatype_helper::test()->use_verifier()) {
+    const BookmarkNode* v_node = nullptr;
+    FindNodeInVerifier(model, node, &v_node);
+    ExpireFaviconImpl(sync_datatype_helper::test()->verifier(), node);
+  }
+  ExpireFaviconImpl(sync_datatype_helper::test()->GetProfile(profile), node);
+
+  WaitForHistoryToProcessPendingTasks();
+}
+
+void CheckFaviconExpired(int profile, const GURL& icon_url) {
+  base::RunLoop run_loop;
+
+  favicon::FaviconService* favicon_service =
+      FaviconServiceFactory::GetForProfile(
+          sync_datatype_helper::test()->GetProfile(profile),
+          ServiceAccessType::EXPLICIT_ACCESS);
+  base::CancelableTaskTracker task_tracker;
+  favicon_service->GetRawFavicon(
+      icon_url, favicon_base::FAVICON, 0,
+      base::Bind(&OnGotFaviconForExpiryCheck, run_loop.QuitClosure()),
+      &task_tracker);
+
+  run_loop.Run();
 }
 
 const BookmarkNode* SetURL(int profile,
