e0127416927da6a156ef0128307722a28eb945d8
ortuno <ortuno@chromium.org>
2015-05-19 21:42:13 -0700

    bluetooth: Move testing IPC from BluetoothDispatcher to BlinkTestRunner
    This patch removes the testing IPC from BluetoothDispatcher and
    BluetoothDispatcherHost.
    
    This patch also changes the way the mock BluetoothAdapter was set in
    BluetoothDispatcherHost. Instead of receiving an IPC to set the adapter,
    BluetoothDispatcherHost exposes a function to directly set the adapter.
    This function is used by LayoutTestSupport to set the adapter.
    
    Mock adapter flow before:
    BlinkTestRunner -> LayoutTestSupport -> BluetoothDispatcher --IPC-->
    BluetoothDispatcherHost (Mock constructed here)
    
    After:
    BlinkTestRunner --IPC--> LayoutTestMessages(Mock constructed here) ->
    LayoutTestSupport -> BluetoothDispatcherHost
    
    This is the first of two patches to remove testing from BluetoothDispatcher
    and BluetoothDispatcherHost:
    
     [1] This patch.
     [2] http://crrev.com/1132943002
    
    BUG=436284
    
    Committed: https://crrev.com/3f7142d0acf5e930743cbe5d754084c464ac3c85
    Cr-Commit-Position: refs/heads/master@{#330647}
    
    Review URL: https://codereview.chromium.org/1125133005
    
    Cr-Commit-Position: refs/heads/master@{#330696}

--- content/child/bluetooth/web_bluetooth_impl.h
+++ content/child/bluetooth/web_bluetooth_impl.h
@@ -33,9 +33,6 @@ class CONTENT_EXPORT WebBluetoothImpl
   void connectGATT(const blink::WebString& device_instance_id,
       blink::WebBluetoothConnectGATTCallbacks* callbacks) override;
 
-  // Testing interface:
-  void SetBluetoothMockDataSetForTesting(const std::string& name);
-
  private:
   BluetoothDispatcher* GetDispatcher();
 
