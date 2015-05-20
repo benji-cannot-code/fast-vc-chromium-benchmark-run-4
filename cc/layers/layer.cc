1240aa91d49b3dfce106e361a86fb69ee0539f6b
ajuma <ajuma@chromium.org>
2015-05-20 15:29:06 -0700

    cc: Use property tree index accessors in Layer::On<Property>Animated
    This fixes two places where we directly access property tree indices
    rather than using their accessors.
    
    BUG=489725
    
    Review URL: https://codereview.chromium.org/1144343002
    
    Cr-Commit-Position: refs/heads/master@{#330818}

--- cc/layers/layer.cc
+++ cc/layers/layer.cc
@@ -1409,7 +1409,7 @@ void Layer::OnOpacityAnimated(float opacity) {
   if (layer_tree_host_) {
     if (OpacityNode* node =
             layer_tree_host_->property_trees()->opacity_tree.Node(
-                opacity_tree_index_)) {
+                opacity_tree_index())) {
       if (node->owner_id == id())
         node->data = opacity;
     }
@@ -1424,7 +1424,7 @@ void Layer::OnTransformAnimated(const gfx::Transform& transform) {
   if (layer_tree_host_) {
     if (TransformNode* node =
             layer_tree_host_->property_trees()->transform_tree.Node(
-                transform_tree_index_)) {
+                transform_tree_index())) {
       if (node->owner_id == id()) {
         node->data.local = transform;
         node->data.needs_local_transform_update = true;
