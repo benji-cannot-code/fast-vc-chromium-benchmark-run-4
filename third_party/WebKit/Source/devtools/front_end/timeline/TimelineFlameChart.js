f4d0b542a34db7207703f5530ce1f2aeefbfc116
alph@chromium.org <alph@chromium.org>
2015-05-20 00:24:47 +0000

    DevTools: Make selection on flamechart persistent.
    Make selection do not reset on zoom and pan. The selection is reset
    (and changes to range selection) when clicked anywhere on the whitespace.
    
    Review URL: https://codereview.chromium.org/1067513003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195563 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineFlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineFlameChart.js
@@ -1554,9 +1554,7 @@ WebInspector.TimelineFlameChartView.prototype = {
     _onEntrySelected: function(dataProvider, event)
     {
         var entryIndex = /** @type{number} */ (event.data);
-        var timelineSelection = dataProvider.createSelection(entryIndex);
-        if (timelineSelection)
-            this._delegate.select(timelineSelection);
+        this._delegate.select(dataProvider.createSelection(entryIndex));
     },
 
     /**
