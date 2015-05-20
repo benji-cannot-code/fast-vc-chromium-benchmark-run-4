2fee0091f1ab6a3ae00b0ec406a746f7afe54a87
philipj@opera.com <philipj@opera.com>
2015-05-20 07:43:27 +0000

    Drop the document.charset part of fast/encoding/external-script-charset.xhtml
    Simplify to only test the inheritance, but use the koi8-r for the XHTML
    document to ensure that it's not just the default encoding being used to
    decode the script.
    
    The bots cannot apply this patch because of the encoding of
    LayoutTests/fast/encoding/external-script-charset-koi-8.js.
    
    BUG=438392
    R=pdr@chromium.org
    
    Review URL: https://codereview.chromium.org/1144913002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195595 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/encoding/external-script-charset-koi-8.js
+++ third_party/WebKit/LayoutTests/fast/encoding/external-script-charset-koi-8.js
@@ -1,2 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-document.getElementById("result2").innerHTML += ("ó" == "\u0421") ? "PASS" : "FAIL";
+if (window.testRunner)
+    testRunner.dumpAsText();
+
+document.getElementById("result").textContent = ("ó" == "\u0421") ? "PASS" : "FAIL";
