bfb39e52d2500d667d43a54bf676a98c87c4e873
avi <avi@chromium.org>
2015-05-20 14:01:11 -0700

    Eliminate redundancy in the parameters to NotifyEntryChanged.
    BUG=369661
    TEST=none
    
    Review URL: https://codereview.chromium.org/1143333003
    
    Cr-Commit-Position: refs/heads/master@{#330800}

--- content/browser/frame_host/navigation_controller_impl.cc
+++ content/browser/frame_host/navigation_controller_impl.cc
@@ -1911,11 +1911,12 @@ void NavigationControllerImpl::LoadIfNecessary() {
   NavigateToPendingEntry(NO_RELOAD);
 }
 
-void NavigationControllerImpl::NotifyEntryChanged(const NavigationEntry* entry,
-                                                  int index) {
+void NavigationControllerImpl::NotifyEntryChanged(
+    const NavigationEntry* entry) {
   EntryChangedDetails det;
   det.changed_entry = entry;
-  det.index = index;
+  det.index = GetIndexOfEntry(
+      NavigationEntryImpl::FromNavigationEntry(entry));
   NotificationService::current()->Notify(
       NOTIFICATION_NAV_ENTRY_CHANGED,
       Source<NavigationController>(this),
--- content/browser/frame_host/navigation_entry_impl.h
+++ content/browser/frame_host/navigation_entry_impl.h
@@ -51,6 +51,8 @@ class CONTENT_EXPORT NavigationEntryImpl
   };
 
   static NavigationEntryImpl* FromNavigationEntry(NavigationEntry* entry);
+  static const NavigationEntryImpl* FromNavigationEntry(
+      const NavigationEntry* entry);
 
   // The value of bindings() before it is set during commit.
   static int kInvalidBindings;
