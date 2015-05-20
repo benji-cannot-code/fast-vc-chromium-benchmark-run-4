409c57a75698b3d8049e8c0eac3dc2eed8bafb6b
dsinclair@chromium.org <dsinclair@chromium.org>
2015-05-20 23:38:01 +0000

    Move LayoutImage cleanup to willBeDestroyed.
    This CL switches LayoutImage from overriding ::destroy to using
    ::willBeDestroyed which is called as the first step of ::destroy.
    
    BUG=490314
    
    Review URL: https://codereview.chromium.org/1148293002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195649 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutImage.h
+++ third_party/WebKit/Source/core/layout/LayoutImage.h
@@ -43,7 +43,6 @@ public:
 
     LayoutImage(Element*);
     virtual ~LayoutImage();
-    virtual void destroy() override;
 
     static LayoutImage* createAnonymous(Document*);
 
@@ -84,6 +83,8 @@ protected:
     virtual bool updateImageLoadingPriorities() override final;
 
     virtual bool isOfType(LayoutObjectType type) const override { return type == LayoutObjectLayoutImage || LayoutReplaced::isOfType(type); }
+
+    virtual void willBeDestroyed() override;
 
 private:
     virtual bool isImage() const override { return true; }
