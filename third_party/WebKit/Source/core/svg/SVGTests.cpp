79e7003ccd8b4e83d987aa142e5b400724433d0f
fs@opera.com <fs@opera.com>
2015-05-21 12:40:33 +0000

    Cleanup includes after DOMImplementation::hasFeature neutering
    Unused after https://codereview.chromium.org/1108293002
    
    R=philipj@opera.com
    BUG=429536
    
    Review URL: https://codereview.chromium.org/1148633004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195690 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGTests.cpp
+++ third_party/WebKit/Source/core/svg/SVGTests.cpp
@@ -20,12 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-
 #include "core/svg/SVGTests.h"
 
 #include "core/SVGNames.h"
-#include "core/dom/DOMImplementation.h"
-#include "core/frame/UseCounter.h"
 #include "core/svg/SVGElement.h"
 #include "platform/Language.h"
 
