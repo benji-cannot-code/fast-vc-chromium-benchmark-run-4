efbdb875d24350bd2699f89fd5dde0222544c397
jchaffraix@chromium.org <jchaffraix@chromium.org>
2015-05-21 20:01:06 +0000

    Revert of Implement timers by posting delayed tasks (patchset #18 id:340001 of https://codereview.chromium.org/1134523002/)
    Reason for revert:
    The patch made some content_browsertests fail on Linux:
    DomSerializerTests.SerializeHTMLDOMWithDocType
    DomSerializerTests.SerializeHTMLDOMWithEmptyHead
    DomSerializerTests.SerializeHTMLDOMWithEntitiesInText
    DomSerializerTests.SerializeHTMLDOMWithMultipleMetaCharsetInOriginalDoc
    DomSerializerTests.SerializeHTMLDOMWithNoMetaCharsetInOriginalDoc
    DomSerializerTests.SerializeHTMLDOMWithoutDocType
    RenderFrameHostManagerTest.DisownOpener
    
    Original issue's description:
    > Implement timers by posting delayed tasks
    > 
    > This patch refactors TimerBase to post tasks delayed tasks and
    > deletes the now-obsolete timer heap and shared timer mechanism.
    > 
    > ATTN Sheriffs: If there are weird layout test flakes all of a
    > sudden, this patch may be the cause since the interleaving of
    > timers with other posted tasks will change.
    > 
    > Original patch by Alex Clarke <alexclarke@chromium.org>.
    > 
    > BUG=463143,416362,480522
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=195706
    
    TBR=jochen@chromium.org,alexclarke@chromium.org,erikcorry@chromium.org,kinuko@chromium.org,rmcilroy@chromium.org,skyostil@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=463143,416362,480522
    
    Review URL: https://codereview.chromium.org/1151633004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195725 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/CachingCorrectnessTest.cpp
+++ third_party/WebKit/Source/core/fetch/CachingCorrectnessTest.cpp
@@ -125,7 +125,7 @@ private:
     // A simple platform that mocks out the clock, for cache freshness testing.
     class ProxyPlatform : public blink::Platform {
     public:
-        ProxyPlatform() : m_oldPlatform(blink::Platform::current()), m_elapsedSeconds(0.) { }
+        ProxyPlatform() : m_elapsedSeconds(0.) { }
 
         void advanceClock(double seconds)
         {
@@ -146,12 +146,6 @@ private:
             return &kAConstUnsignedCharZero;
         }
 
-        WebThread* currentThread() override
-        {
-            return m_oldPlatform->currentThread();
-        }
-
-        blink::Platform* m_oldPlatform; // Not owned.
         double m_elapsedSeconds;
     };
 
