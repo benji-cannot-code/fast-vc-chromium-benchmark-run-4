e64674b673f73eabff5832738a3491978a97aecf
kochi@chromium.org <kochi@chromium.org>
2015-05-21 10:04:24 +0000

    Call updateDistribution() in Element::matches()
    Element::matches() can be called with selectors such as
    "::content" to match distributed nodes, from JavaScript.
    Assertion in InsertionPoint.cpp detected the code path
    that can be called without updating distribution.
    
    Recalculate distribution before selector matching is done
    in Element::matches() fixes the issue.
    
    BUG=489911
    TEST=fast/dom/shadow/update-distribution-for-matches-expected.html
    
    Review URL: https://codereview.chromium.org/1150533002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195684 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -2783,6 +2783,7 @@ LayoutObject* Element::pseudoElementLayoutObject(PseudoId pseudoId) const
 
 bool Element::matches(const String& selectors, ExceptionState& exceptionState)
 {
+    updateDistribution();
     SelectorQuery* selectorQuery = document().selectorQueryCache().add(AtomicString(selectors), document(), exceptionState);
     if (!selectorQuery)
         return false;
