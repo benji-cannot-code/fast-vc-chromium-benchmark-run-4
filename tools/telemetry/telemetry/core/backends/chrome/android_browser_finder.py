6f2899466f682313f1ecd93db6b00df04a191360
fdegans <fdegans@chromium.org>
2015-05-21 14:12:19 -0700

    Add ChromePublic to available browsers in telemetry.
    BUG=467069
    
    Review URL: https://codereview.chromium.org/1154513002
    
    Cr-Commit-Position: refs/heads/master@{#330986}

--- tools/telemetry/telemetry/core/backends/chrome/android_browser_finder.py
+++ tools/telemetry/telemetry/core/backends/chrome/android_browser_finder.py
@@ -37,6 +37,10 @@ CHROME_PACKAGE_NAMES = {
       ['org.chromium.android_webview.shell',
        android_browser_backend_settings.WebviewShellBackendSettings,
        'AndroidWebView.apk'],
+  'android-chromium':
+      ['org.chromium.chrome',
+       android_browser_backend_settings.ChromeBackendSettings,
+       'ChromePublic.apk'],
   'android-chrome':
       ['com.google.android.apps.chrome',
        android_browser_backend_settings.ChromeBackendSettings,
