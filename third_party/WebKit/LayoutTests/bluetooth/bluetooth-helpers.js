63d5e32cce9762e3c5266c31525d767beb00a6fa
ortuno@chromium.org <ortuno@chromium.org>
2015-05-21 18:15:34 +0000

    bluetooth: connectGATT Layout Tests
    This layout tests correspond to the partial browser side implementation
    of connectGATT on:
    
    http://crrev.com/1120373004
    
    BUG=421668
    
    Review URL: https://codereview.chromium.org/1150523004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195715 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/bluetooth/bluetooth-helpers.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var promise_tests = Promise.resolve();
+// Helper function to run promise tests one after the other.
+// TODO(ortuno): Remove once https://github.com/w3c/testharness.js/pull/115/files
+// gets through.
+function sequential_promise_test(func, name) {
+  var test = async_test(name);
+  promise_tests = promise_tests.then(function() {
+    return Promise.resolve(test.step(func, test, test))
+      .then(function() {
+        test.done();
+      })
+      .catch(test.step_func(
+        function(value) {
+          throw value;
+        }));
+  });
+}
