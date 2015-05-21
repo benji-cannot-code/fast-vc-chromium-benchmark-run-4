aa1474a821e299c2ad7bc32bd1b2f0aa9907859f
tombergan <tombergan@chromium.org>
2015-05-20 17:07:59 -0700

    Add data reduction proxy integration tests for video compression.
    BUG=474397
    
    Review URL: https://codereview.chromium.org/1065763002
    
    Cr-Commit-Position: refs/heads/master@{#330856}

--- tools/chrome_proxy/integration_tests/chrome_proxy_pagesets/__init__.py
+++ tools/chrome_proxy/integration_tests/chrome_proxy_pagesets/__init__.py
@@ -9,6 +9,7 @@ import sys
 
 from telemetry.core import discover
 from telemetry.page import page_set
+import video
 
 
 # Import all submodules' PageSet classes.
@@ -17,4 +18,11 @@ top_level_dir = os.path.abspath(os.path.join(start_dir, os.pardir, os.pardir))
 base_class = page_set.PageSet
 for cls in discover.DiscoverClasses(
     start_dir, top_level_dir, base_class).values():
+  setattr(sys.modules[__name__], cls.__name__, cls)
+
+# DiscoverClasses makes the assumption that there is exactly one matching
+# class per file, however the following are declared in the same file.
+for cls in (video.VideoDirectPageSet,
+            video.VideoProxiedPageSet,
+            video.VideoComparePageSet):
   setattr(sys.modules[__name__], cls.__name__, cls)
