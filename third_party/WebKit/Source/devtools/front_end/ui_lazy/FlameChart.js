a368400c9039118fc4f8bf5659a943589680763e
alph@chromium.org <alph@chromium.org>
2015-05-21 19:40:26 +0000

    DevTools: Reset scroll position on flamechart reset.
    Review URL: https://codereview.chromium.org/1147273003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195719 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui_lazy/FlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/ui_lazy/FlameChart.js
@@ -1446,6 +1446,7 @@ WebInspector.FlameChart.prototype = {
 
     reset: function()
     {
+        this._vScrollElement.scrollTop = 0;
         this._highlightedMarkerIndex = -1;
         this._highlightedEntryIndex = -1;
         this._selectedEntryIndex = -1;
