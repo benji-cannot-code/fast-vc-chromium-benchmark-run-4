876c706f72c078faf72426a15fdcfd5a5d89a3fc
sky <sky@chromium.org>
2015-05-21 13:28:43 -0700

    Moves OmniboxUI off InterfaceImpl
    It now uses StrongBinding, which gives old semantics.
    
    BUG=489752
    TEST=none
    R=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/1148993002
    
    Cr-Commit-Position: refs/heads/master@{#330963}

--- chrome/browser/ui/webui/omnibox/omnibox_ui_handler.h
+++ chrome/browser/ui/webui/omnibox/omnibox_ui_handler.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/omnibox/omnibox.mojom.h"
 #include "components/omnibox/autocomplete_input.h"
 #include "components/omnibox/autocomplete_match.h"
+#include "third_party/mojo/src/mojo/public/cpp/bindings/strong_binding.h"
 
 class AutocompleteController;
 class Profile;
@@ -24,19 +25,16 @@ class Profile;
 // AutocompleteController to OnResultChanged() and passes those results to
 // the OmniboxPage.
 class OmniboxUIHandler : public AutocompleteControllerDelegate,
-                         public mojo::InterfaceImpl<OmniboxUIHandlerMojo>,
+                         public OmniboxUIHandlerMojo,
                          public MojoWebUIHandler {
  public:
-  explicit OmniboxUIHandler(Profile* profile);
+  // OmniboxUIHandler is deleted when the supplied pipe is destroyed.
+  OmniboxUIHandler(Profile* profile,
+                   mojo::InterfaceRequest<OmniboxUIHandlerMojo> request);
   ~OmniboxUIHandler() override;
 
   // AutocompleteControllerDelegate overrides:
   void OnResultChanged(bool default_match_changed) override;
-
-  // ErrorHandler overrides:
-  void OnConnectionError() override {
-    // TODO(darin): How should we handle connection error?
-  }
 
   // OmniboxUIHandlerMojo overrides:
   void StartOmniboxQuery(const mojo::String& input_string,
@@ -73,6 +71,8 @@ class OmniboxUIHandler : public AutocompleteControllerDelegate,
 
   // The Profile* handed to us in our constructor.
   Profile* profile_;
+
+  mojo::StrongBinding<OmniboxUIHandlerMojo> binding_;
 
   DISALLOW_COPY_AND_ASSIGN(OmniboxUIHandler);
 };
