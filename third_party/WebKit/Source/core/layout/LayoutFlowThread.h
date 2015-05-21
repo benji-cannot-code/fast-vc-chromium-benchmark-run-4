8a2a38e5a18be3b907d045a1e3e2c80f7e1b96dd
mstensho@opera.com <mstensho@opera.com>
2015-05-21 15:25:11 +0000

    Changing out-of-flow-ness on a multicol child may affect the need for column sets.
    If a sole out-of-flow child becomes in-flow, we have to insert a column set,
    since we suddenly have column content.
    
    Conversely, if a sole in-flow child becomes out-of-flow, we have to remove the
    column set, since there's no column content anymore.
    
    BUG=486993
    R=dsinclair@chromium.org,jchaffraix@chromium.org
    
    Review URL: https://codereview.chromium.org/1148173003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195693 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutFlowThread.h
+++ third_party/WebKit/Source/core/layout/LayoutFlowThread.h
@@ -72,6 +72,8 @@ public:
 
     virtual void flowThreadDescendantWasInserted(LayoutObject*) { }
     virtual void flowThreadDescendantWillBeRemoved(LayoutObject*) { }
+    virtual void flowThreadDescendantStyleWillChange(LayoutObject*, StyleDifference, const ComputedStyle& newStyle) { }
+    virtual void flowThreadDescendantStyleDidChange(LayoutObject*, StyleDifference, const ComputedStyle& oldStyle) { }
 
     virtual bool nodeAtPoint(HitTestResult&, const HitTestLocation& locationInContainer, const LayoutPoint& accumulatedOffset, HitTestAction) override final;
 
--- third_party/WebKit/Source/core/layout/LayoutObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutObject.cpp
@@ -1755,6 +1755,10 @@ void LayoutObject::setStyle(PassRefPtr<ComputedStyle> style)
 
     diff = adjustStyleDifference(diff);
 
+    if (m_style) {
+        if (LayoutFlowThread* flowThread = flowThreadContainingBlock())
+            flowThread->flowThreadDescendantStyleWillChange(this, diff, *style);
+    }
     styleWillChange(diff, *style);
 
     RefPtr<ComputedStyle> oldStyle = m_style.release();
@@ -1771,6 +1775,10 @@ void LayoutObject::setStyle(PassRefPtr<ComputedStyle> style)
     bool doesNotNeedLayoutOrPaintInvalidation = !m_parent;
 
     styleDidChange(diff, oldStyle.get());
+    if (oldStyle.get()) {
+        if (LayoutFlowThread* flowThread = flowThreadContainingBlock())
+            flowThread->flowThreadDescendantStyleDidChange(this, diff, *oldStyle.get());
+    }
 
     // FIXME: |this| might be destroyed here. This can currently happen for a LayoutTextFragment when
     // its first-letter block gets an update in LayoutTextFragment::styleDidChange. For LayoutTextFragment(s),
