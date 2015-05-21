1e497baafed5a0df5eb4b50f44a8e014eb746ee1
pilgrim@chromium.org <pilgrim@chromium.org>
2015-05-21 17:03:28 +0000

    Restore not-unused LayoutObject::getImageAnimationPolicy
    reverts https://codereview.chromium.org/1144143003
    
    BUG=436473
    TBR=leviw
    
    Review URL: https://codereview.chromium.org/1147183003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195704 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutObject.cpp
@@ -2926,6 +2926,14 @@ bool LayoutObject::willRenderImage(ImageResource*)
     return document().view()->isVisible();
 }
 
+bool LayoutObject::getImageAnimationPolicy(ImageResource*, ImageAnimationPolicy& policy)
+{
+    if (!document().settings())
+        return false;
+    policy = document().settings()->imageAnimationPolicy();
+    return true;
+}
+
 int LayoutObject::caretMinOffset() const
 {
     return 0;
