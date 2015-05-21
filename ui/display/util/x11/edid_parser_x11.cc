79b7e2bd97202c21690f6857acb8a4939dc495fe
robert.bradford <robert.bradford@intel.com>
2015-05-21 03:54:47 -0700

    Generate display product id with EDID manufacturer product code
    Formerly the product id for the DisplaySnapshot was generated with a
    combination of the manufacturer code combined with a hash of the product
    display name. However as the product display name can vary based on
    display connector it is more appropriate to use the product code.
    
    TEST=display_unittests passes.
    BUG=471749
    
    Review URL: https://codereview.chromium.org/1151583002
    
    Cr-Commit-Position: refs/heads/master@{#330929}

--- ui/display/util/x11/edid_parser_x11.cc
+++ ui/display/util/x11/edid_parser_x11.cc
@@ -87,8 +87,8 @@ bool GetOutputDeviceData(XID output,
   if (!GetEDIDProperty(output, &edid))
     return false;
 
-  return ParseOutputDeviceData(edid, manufacturer_id, human_readable_name,
-                               nullptr, nullptr);
+  return ParseOutputDeviceData(edid, manufacturer_id, nullptr,
+                               human_readable_name, nullptr, nullptr);
 }
 
 }  // namespace
