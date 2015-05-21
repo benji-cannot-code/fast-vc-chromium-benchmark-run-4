eddc026a145ce3d82fa46ee8b84c14d9fae206cb
ulan <ulan@chromium.org>
2015-05-21 04:17:05 -0700

    Add benchmark that tests garbage collector and idle time handler.
    High memory usage is forced by duplicate tabs in the same process.
    
    BUG=
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1145173002
    
    Cr-Commit-Position: refs/heads/master@{#330930}

---
+++ tools/perf/page_sets/idle_multi_tab_cases.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+__telemetry_spawned_tabs = [];
+
+function __telemetry_spawn_tab() {
+  var tab = window.open(window.location.href);
+  if (typeof tab === "undefined") {
+    throw "Cannot open tab. Forgot --disable-popup-blocking flag?";
+  }
+  __telemetry_spawned_tabs.push(tab);
+  return __telemetry_spawned_tabs.length - 1;
+}
+
+function __telemetry_close_tab(tab) {
+  __telemetry_spawned_tabs[tab].close();
+  __telemetry_spawned_tabs[tab] = null;
+}
