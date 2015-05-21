bab6524ae64ee5a4d0cffd741ea8aaf5f30b523d
timvolodine@chromium.org <timvolodine@chromium.org>
2015-05-21 15:06:45 +0000

    Add WebRuntimeFeatures::enablePermissionsAPI.
    Add the ability to control whether we want to expose the Permissions API
    in blink at runtime. This is e.g. needed for WebView where the
    Permissions API is currently not supported.
    
    BUG=489926
    
    Review URL: https://codereview.chromium.org/1150653002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195691 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
@@ -189,6 +189,11 @@ void WebRuntimeFeatures::enablePagePopup(bool enable)
     RuntimeEnabledFeatures::setPagePopupEnabled(enable);
 }
 
+void WebRuntimeFeatures::enablePermissionsAPI(bool enable)
+{
+    RuntimeEnabledFeatures::setPermissionsEnabled(enable);
+}
+
 void WebRuntimeFeatures::enableRequestAutocomplete(bool enable)
 {
     RuntimeEnabledFeatures::setRequestAutocompleteEnabled(enable);
