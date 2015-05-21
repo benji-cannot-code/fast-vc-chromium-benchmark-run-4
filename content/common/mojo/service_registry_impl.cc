c6e1be0be701ec50cd6207cfccec800aa6383f6f
xhwang <xhwang@chromium.org>
2015-05-21 10:32:26 -0700

    Add connection error handing in ServiceRegistryImpl.
    This will be useful when hosting mojo services in the utility process where the process can go away after being idle for a certain period of time. When that happens, the ServiceRegistryImpl will not be bound and needs to be reconnected before it can be used again. For background, please see http://goo.gl/sMpZ0g.
    
    BUG=479935
    TEST=Tested by manually killing the process hosting the other side of the ServiceRegistryImpl.
    
    Review URL: https://codereview.chromium.org/1128493002
    
    Cr-Commit-Position: refs/heads/master@{#330946}

--- content/common/mojo/service_registry_impl.cc
+++ content/common/mojo/service_registry_impl.cc
@@ -11,6 +11,7 @@ namespace content {
 
 ServiceRegistryImpl::ServiceRegistryImpl()
     : binding_(this), weak_factory_(this) {
+  binding_.set_error_handler(this);
 }
 
 ServiceRegistryImpl::~ServiceRegistryImpl() {
@@ -59,6 +60,10 @@ void ServiceRegistryImpl::ConnectToRemoteService(
                                      handle.Pass());
 }
 
+bool ServiceRegistryImpl::IsBound() const {
+  return binding_.is_bound();
+}
+
 base::WeakPtr<ServiceRegistry> ServiceRegistryImpl::GetWeakPtr() {
   return weak_factory_.GetWeakPtr();
 }
@@ -73,6 +78,10 @@ void ServiceRegistryImpl::ConnectToService(
     return;
 
   it->second.Run(client_handle.Pass());
+}
+
+void ServiceRegistryImpl::OnConnectionError() {
+  binding_.Close();
 }
 
 }  // namespace content
--- content/common/mojo/service_registry_impl.h
+++ content/common/mojo/service_registry_impl.h
@@ -17,13 +17,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/service_registry.h"
 #include "mojo/application/public/interfaces/service_provider.mojom.h"
 #include "third_party/mojo/src/mojo/public/cpp/bindings/binding.h"
+#include "third_party/mojo/src/mojo/public/cpp/bindings/error_handler.h"
 #include "third_party/mojo/src/mojo/public/cpp/system/core.h"
 
 namespace content {
 
 class CONTENT_EXPORT ServiceRegistryImpl
     : public ServiceRegistry,
-      public NON_EXPORTED_BASE(mojo::ServiceProvider) {
+      public NON_EXPORTED_BASE(mojo::ServiceProvider),
+      public NON_EXPORTED_BASE(mojo::ErrorHandler) {
  public:
   ServiceRegistryImpl();
   ~ServiceRegistryImpl() override;
@@ -45,12 +47,17 @@ class CONTENT_EXPORT ServiceRegistryImpl
   void ConnectToRemoteService(const base::StringPiece& service_name,
                               mojo::ScopedMessagePipeHandle handle) override;
 
+  bool IsBound() const;
+
   base::WeakPtr<ServiceRegistry> GetWeakPtr();
 
  private:
   // mojo::ServiceProvider overrides.
   void ConnectToService(const mojo::String& name,
                         mojo::ScopedMessagePipeHandle client_handle) override;
+
+  // mojo::ErrorHandler overrides.
+  void OnConnectionError() override;
 
   mojo::Binding<mojo::ServiceProvider> binding_;
   mojo::ServiceProviderPtr remote_provider_;
