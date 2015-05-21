a6ef4bfd40c93d31116da8a5b8170180d5b9797e
varkha <varkha@chromium.org>
2015-05-21 13:30:08 -0700

    Skips disabled menu items when selection is changed
    BUG=297561
    TEST=views_unittests --gtest_filter=*MenuControllerTest*SelectedItem*
    
    Review URL: https://codereview.chromium.org/1129203003
    
    Cr-Commit-Position: refs/heads/master@{#330965}

--- ui/views/controls/menu/menu_controller.h
+++ ui/views/controls/menu/menu_controller.h
@@ -436,9 +436,13 @@ class VIEWS_EXPORT MenuController : public WidgetObserver {
   // Selects the next/previous menu item.
   void IncrementSelection(int delta);
 
+  // Returns the first selectable child menu item of |parent|. If there are no
+  // selectable menu items NULL is returned.
+  MenuItemView* FindFirstSelectableMenuItem(MenuItemView* parent);
+
   // Returns the next selectable child menu item of |parent| starting at |index|
-  // and incrementing index by |delta|. If there are no more selected menu items
-  // NULL is returned.
+  // and incrementing index by |delta|. If there are no more selectable menu
+  // items NULL is returned.
   MenuItemView* FindNextSelectableMenuItem(MenuItemView* parent,
                                            int index,
                                            int delta);
