9431b7c197d32f336156fa64f76391bc6b3e6906
petrcermak <petrcermak@chromium.org>
2015-05-22 04:51:32 -0700

    Remove no map executable support probe and fallback
    This patch removes the check for support for memory mapping files inside
    the Chrome APK with executable permissions and the associated fallback.
    
    Background: We were checking the APK executable mmap support to
    determine when we could start loading directly from APK on 32-bit
    devices. However, we decided to do this only on L+ devices (where the
    support is mandatory).
    
    BUG=489267
    
    Review URL: https://codereview.chromium.org/1152493002
    
    Cr-Commit-Position: refs/heads/master@{#331094}

--- third_party/android_crazy_linker/src/src/crazy_linker_wrappers.cpp
+++ third_party/android_crazy_linker/src/src/crazy_linker_wrappers.cpp
@@ -83,7 +83,6 @@ void* WrapDlopen(const char* path, int mode) {
                                               0U /* file_offset */,
                                               Globals::GetSearchPaths(),
                                               false,
-                                              false,
                                               &error);
     if (wrap)
       return wrap;
