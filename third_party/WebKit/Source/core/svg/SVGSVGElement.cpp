6bab6953d253cf32e1a49069f4f22a8ae5cc5f86
pdr@chromium.org <pdr@chromium.org>
2015-05-22 03:08:55 +0000

    Invalidate relative length clients on SVG viewBox changes
    Before this patch we would fail to invalidate elements that depend
    on an SVG element's viewbox. The added invalidateRelativeLengthClients()
    call follows the pattern used a few lines above when the x, y, width,
    or height attributes change. With this patch we correctly invalidate
    relative length clients which fixes an under-invalidation bug with
    slimming paint.
    
    TEST=LayoutTests/svg/repaint/inner-svg-change-viewBox.svg
    BUG=451606
    
    Review URL: https://codereview.chromium.org/1152513004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195744 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGSVGElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGSVGElement.cpp
@@ -316,6 +316,7 @@ void SVGSVGElement::svgAttributeChanged(const QualifiedName& attrName)
 
     if (SVGFitToViewBox::isKnownAttribute(attrName)) {
         updateRelativeLengthsOrViewBox = true;
+        invalidateRelativeLengthClients();
         if (LayoutObject* object = layoutObject())
             object->setNeedsTransformUpdate();
     }
