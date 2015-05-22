255e265e398bd6858bf0ee4b1b65318f22087dc0
tkent@chromium.org <tkent@chromium.org>
2015-05-22 12:07:50 +0000

    Can't open <select> popup with an <option> text including "</script>".
    We should escape "</script>" when we generate an internal HTML document.
    
    BUG=490607
    
    Review URL: https://codereview.chromium.org/1151153004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195781 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/core.gypi
+++ third_party/WebKit/Source/core/core.gypi
@@ -3899,6 +3899,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'loader/MixedContentCheckerTest.cpp',
             'page/ContextMenuControllerTest.cpp',
             'page/NetworkStateNotifierTest.cpp',
+            'page/PagePopupClientTest.cpp',
             'page/PrintContextTest.cpp',
             'page/scrolling/ScrollStateTest.cpp',
             'paint/DisplayItemListPaintTest.cpp',
---
+++ third_party/WebKit/Source/core/page/PagePopupClientTest.cpp
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "core/page/PagePopupClient.h"
+
+#include <gtest/gtest.h>
+#include <string>
+
+namespace blink {
+
+TEST(PagePopupClientTest, AddJavaScriptString)
+{
+    RefPtr<SharedBuffer> buffer = SharedBuffer::create();
+    PagePopupClient::addJavaScriptString("abc\r\n'\"</script>", buffer.get());
+    EXPECT_EQ("\"abc\\r\\n'\\\"\\x3C/script>\"", std::string(buffer->data(), buffer->size()));
+}
+
+} // namespace blink
+
