6104a0db6a8cc40666c901b77187c094c5452847
bashi@chromium.org <bashi@chromium.org>
2015-05-22 03:29:21 +0000

    bindings: Use ErrorEventInit dictionary for ErrorEvent constructor
    To improve spec conformance. Move ErrorEvent specific handling from
    the code generator to custom getter function.
    
    BUG=433179
    
    Review URL: https://codereview.chromium.org/1142363003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195748 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/events/ErrorEvent.h
+++ third_party/WebKit/Source/core/events/ErrorEvent.h
@@ -33,20 +33,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ErrorEvent_h
 
 #include "bindings/core/v8/DOMWrapperWorld.h"
+#include "core/events/ErrorEventInit.h"
 #include "core/events/Event.h"
 #include "wtf/RefPtr.h"
 #include "wtf/text/WTFString.h"
 
 namespace blink {
-
-struct ErrorEventInit : public EventInit {
-    ErrorEventInit();
-
-    String message;
-    String filename;
-    unsigned lineno;
-    unsigned colno;
-};
 
 class ErrorEvent final : public Event {
     DEFINE_WRAPPERTYPEINFO();
@@ -74,6 +66,7 @@ public:
     const String& filename() const { return m_fileName; }
     unsigned lineno() const { return m_lineNumber; }
     unsigned colno() const { return m_columnNumber; }
+    ScriptValue error(ScriptState*) const;
 
     // 'messageForConsole' is not exposed to JavaScript, and prefers 'm_unsanitizedMessage'.
     const String& messageForConsole() const { return !m_unsanitizedMessage.isEmpty() ? m_unsanitizedMessage : m_sanitizedMessage; }
@@ -96,6 +89,7 @@ private:
     String m_fileName;
     unsigned m_lineNumber;
     unsigned m_columnNumber;
+    ScriptValue m_error;
 
     RefPtr<DOMWrapperWorld> m_world;
 };
