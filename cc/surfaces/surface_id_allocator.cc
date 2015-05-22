b2b5213a38d2a9d350612806b2d10e7e0fcda77b
kenrb <kenrb@chromium.org>
2015-05-22 09:57:44 -0700

    Ignore Surface destruction dependencies on crashed renderers.
    If a renderer process crashes without satisfying all of its destruction
    dependencies, the SurfaceManager needs a way to know that it should not
    wait for Satisfy sequences before releasing dependent surfaces.
    
    This patch does this by having SurfaceManagers register the surface ID
    namespaces that they use, so that Sequences associated with invalidated
    namespaces can be ignored.
    
    BUG=478802
    
    Review URL: https://codereview.chromium.org/1135693004
    
    Cr-Commit-Position: refs/heads/master@{#331131}

--- cc/surfaces/surface_id_allocator.cc
+++ cc/surfaces/surface_id_allocator.cc
@@ -5,10 +5,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/surfaces/surface_id_allocator.h"
 
+#include "cc/surfaces/surface_manager.h"
+
 namespace cc {
 
 SurfaceIdAllocator::SurfaceIdAllocator(uint32_t id_namespace)
-    : id_namespace_(id_namespace), next_id_(1u) {
+    : id_namespace_(id_namespace), next_id_(1u), manager_(nullptr) {
+}
+
+void SurfaceIdAllocator::RegisterSurfaceIdNamespace(SurfaceManager* manager) {
+  DCHECK(!manager_);
+  manager_ = manager;
+  manager_->RegisterSurfaceIdNamespace(id_namespace_);
+}
+
+SurfaceIdAllocator::~SurfaceIdAllocator() {
+  if (manager_)
+    manager_->InvalidateSurfaceIdNamespace(id_namespace_);
 }
 
 SurfaceId SurfaceIdAllocator::GenerateId() {
