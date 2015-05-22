8495e304b8ac4bf0b82b4e33f1275000493fedd5
yurys@google.com <yurys@google.com>
2015-05-22 04:36:01 +0000

    DevTools: change debugger state to enabled earlier in Debugger.enable command handler
    Debugger.enable command results in reporing all parsed scripts. When scripts are reported debugger agent will try to find breakpoint matching those scripts and should already be enabled.
    
    BUG=490849
    
    Review URL: https://codereview.chromium.org/1151813003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195756 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/V8Debugger.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8Debugger.cpp
@@ -491,8 +491,6 @@ void V8Debugger::interruptAndRun(PassOwnPtr<Task> task)
 
 void V8Debugger::runPendingTasks()
 {
-    if (!enabled())
-        return;
     while (true) {
         OwnPtr<Task> task = m_taskQueue->tryTake();
         if (!task)
@@ -563,7 +561,8 @@ void V8Debugger::handleProgramBreak(ScriptState* pausedScriptState, v8::Local<v8
 void V8Debugger::v8InterruptCallback(v8::Isolate*, void* data)
 {
     V8Debugger* server = static_cast<V8Debugger*>(data);
-    server->runPendingTasks();
+    if (server->enabled())
+        server->runPendingTasks();
 }
 
 void V8Debugger::v8DebugEventCallback(const v8::Debug::EventDetails& eventDetails)
