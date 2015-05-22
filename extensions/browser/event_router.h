76044b1d8613bd9f2e15114b616269221913032f
amistry <amistry@chromium.org>
2015-05-22 00:40:25 -0700

    Use a RenderProcessHostObserver to observe render process shutdown.
    BUG=268984
    
    Review URL: https://codereview.chromium.org/1157583003
    
    Cr-Commit-Position: refs/heads/master@{#331077}

--- extensions/browser/event_router.h
+++ extensions/browser/event_router.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "content/public/browser/render_process_host_observer.h"
 #include "extensions/browser/event_listener_map.h"
 #include "extensions/browser/extension_registry_observer.h"
 #include "extensions/common/event_filtering_info.h"
@@ -46,7 +47,8 @@ struct EventListenerInfo;
 
 class EventRouter : public content::NotificationObserver,
                     public ExtensionRegistryObserver,
-                    public EventListenerMap::Delegate {
+                    public EventListenerMap::Delegate,
+                    public content::RenderProcessHostObserver {
  public:
   // These constants convey the state of our knowledge of whether we're in
   // a user-caused gesture as part of DispatchEvent.
@@ -299,6 +301,12 @@ class EventRouter : public content::NotificationObserver,
   void OnListenerAdded(const EventListener* listener) override;
   void OnListenerRemoved(const EventListener* listener) override;
 
+  // RenderProcessHostObserver implementation.
+  void RenderProcessExited(content::RenderProcessHost* host,
+                           base::TerminationStatus status,
+                           int exit_code) override;
+  void RenderProcessHostDestroyed(content::RenderProcessHost* host) override;
+
   content::BrowserContext* browser_context_;
 
   // The ExtensionPrefs associated with |browser_context_|. May be NULL in
@@ -315,6 +323,8 @@ class EventRouter : public content::NotificationObserver,
   // Map from base event name to observer.
   typedef base::hash_map<std::string, Observer*> ObserverMap;
   ObserverMap observers_;
+
+  std::set<content::RenderProcessHost*> observed_process_set_;
 
   DISALLOW_COPY_AND_ASSIGN(EventRouter);
 };
