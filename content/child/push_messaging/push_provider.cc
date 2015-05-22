bd4083c473ae656cb2d918a1458d7ad22e55a4ef
pranay.kumar <pranay.kumar@samsung.com>
2015-05-21 21:00:29 -0700

    Push API: use of (un)subscription instead of (un)registration
    BUG=446883
    
    This is in response to this spec issue:
    https://github.com/w3c/push-api/issues/98
    
    Review URL: https://codereview.chromium.org/1134733006
    
    Cr-Commit-Position: refs/heads/master@{#331062}

--- content/child/push_messaging/push_provider.cc
+++ content/child/push_messaging/push_provider.cc
@@ -76,7 +76,7 @@ void PushProvider::subscribe(
   subscription_callbacks_.AddWithID(callbacks, request_id);
   int64 service_worker_registration_id =
       GetServiceWorkerRegistrationId(service_worker_registration);
-  thread_safe_sender_->Send(new PushMessagingHostMsg_RegisterFromWorker(
+  thread_safe_sender_->Send(new PushMessagingHostMsg_SubscribeFromWorker(
       request_id, service_worker_registration_id, options.userVisibleOnly));
 }
 
@@ -91,7 +91,7 @@ void PushProvider::unsubscribe(
 
   int64 service_worker_registration_id =
       GetServiceWorkerRegistrationId(service_worker_registration);
-  thread_safe_sender_->Send(new PushMessagingHostMsg_Unregister(
+  thread_safe_sender_->Send(new PushMessagingHostMsg_Unsubscribe(
       request_id, service_worker_registration_id));
 }
 
@@ -125,14 +125,14 @@ void PushProvider::getPermissionStatus(
 bool PushProvider::OnMessageReceived(const IPC::Message& message) {
   bool handled = true;
   IPC_BEGIN_MESSAGE_MAP(PushProvider, message)
-    IPC_MESSAGE_HANDLER(PushMessagingMsg_RegisterFromWorkerSuccess,
-                        OnRegisterFromWorkerSuccess);
-    IPC_MESSAGE_HANDLER(PushMessagingMsg_RegisterFromWorkerError,
-                        OnRegisterFromWorkerError);
-    IPC_MESSAGE_HANDLER(PushMessagingMsg_UnregisterSuccess,
-                        OnUnregisterSuccess);
-    IPC_MESSAGE_HANDLER(PushMessagingMsg_UnregisterError,
-                        OnUnregisterError);
+    IPC_MESSAGE_HANDLER(PushMessagingMsg_SubscribeFromWorkerSuccess,
+                        OnSubscribeFromWorkerSuccess);
+    IPC_MESSAGE_HANDLER(PushMessagingMsg_SubscribeFromWorkerError,
+                        OnSubscribeFromWorkerError);
+    IPC_MESSAGE_HANDLER(PushMessagingMsg_UnsubscribeSuccess,
+                        OnUnsubscribeSuccess);
+    IPC_MESSAGE_HANDLER(PushMessagingMsg_UnsubscribeError,
+                        OnUnsubscribeError);
     IPC_MESSAGE_HANDLER(PushMessagingMsg_GetRegistrationSuccess,
                         OnGetRegistrationSuccess);
     IPC_MESSAGE_HANDLER(PushMessagingMsg_GetRegistrationError,
@@ -147,10 +147,10 @@ bool PushProvider::OnMessageReceived(const IPC::Message& message) {
   return handled;
 }
 
-void PushProvider::OnRegisterFromWorkerSuccess(
+void PushProvider::OnSubscribeFromWorkerSuccess(
     int request_id,
     const GURL& endpoint,
-    const std::string& registration_id) {
+    const std::string& subscription_id) {
   blink::WebPushSubscriptionCallbacks* callbacks =
       subscription_callbacks_.Lookup(request_id);
   if (!callbacks)
@@ -159,14 +159,14 @@ void PushProvider::OnRegisterFromWorkerSuccess(
   scoped_ptr<blink::WebPushSubscription> subscription(
       new blink::WebPushSubscription(
           blink::WebString::fromUTF8(endpoint.spec()),
-          blink::WebString::fromUTF8(registration_id)));
+          blink::WebString::fromUTF8(subscription_id)));
   callbacks->onSuccess(subscription.release());
 
   subscription_callbacks_.Remove(request_id);
 }
 
-void PushProvider::OnRegisterFromWorkerError(int request_id,
-                                             PushRegistrationStatus status) {
+void PushProvider::OnSubscribeFromWorkerError(int request_id,
+                                              PushRegistrationStatus status) {
   blink::WebPushSubscriptionCallbacks* callbacks =
       subscription_callbacks_.Lookup(request_id);
   if (!callbacks)
@@ -180,18 +180,18 @@ void PushProvider::OnRegisterFromWorkerError(int request_id,
   subscription_callbacks_.Remove(request_id);
 }
 
-void PushProvider::OnUnregisterSuccess(int request_id, bool did_unregister) {
+void PushProvider::OnUnsubscribeSuccess(int request_id, bool did_unsubscribe) {
   blink::WebPushUnsubscribeCallbacks* callbacks =
       unsubscribe_callbacks_.Lookup(request_id);
   if (!callbacks)
     return;
 
-  callbacks->onSuccess(&did_unregister);
+  callbacks->onSuccess(&did_unsubscribe);
 
   unsubscribe_callbacks_.Remove(request_id);
 }
 
-void PushProvider::OnUnregisterError(
+void PushProvider::OnUnsubscribeError(
     int request_id,
     blink::WebPushError::ErrorType error_type,
     const std::string& error_message) {
