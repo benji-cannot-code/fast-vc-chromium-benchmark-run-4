8d65894ebe3e9fe46971ac40ef453ec20d39e2e4
miu <miu@chromium.org>
2015-05-22 16:22:59 -0700

    RELAND: [Cast] Compute utilization metrics and add performance overlay.
    This change relands https://codereview.chromium.org/1148233002/ with a
    compile fix for Mac that somehow got through the CQ unnoticed.  :-/
    
    Adds computation of two frame-level utilization metrics to the software
    VP8 encoder in Cast: deadline utilization and lossy utilization.  The
    first is a measure of how long the encoding of each frame takes compared
    to the frame duration.  The second is a measure of the complexity of a
    frame, in terms of the quality versus encoded size trade-off.
    
    In a future change, these utilization metrics will be sent as feedback
    signals to the producer of the video frames, allowing the producer to
    adjust data volumes based on the consumer's capability throughout a
    session.  See bug for more details.
    
    Also, this change adds an overlay display, where frame-level performance
    metrics are rendered in the lower-right corner of each video frame just
    before it is sent.  This provides an "on screen display" of end-to-end
    system performance.  This is turned on with a command line argument:
    --vmodule=performance_metrics_overlay=3
    
    BUG=156767
    TBR=hubbe@chromium.org
    
    Review URL: https://codereview.chromium.org/1155943002
    
    Cr-Commit-Position: refs/heads/master@{#331202}

--- media/cast/receiver/video_decoder_unittest.cc
+++ media/cast/receiver/video_decoder_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "media/cast/cast_config.h"
 #include "media/cast/receiver/video_decoder.h"
+#include "media/cast/sender/sender_encoded_frame.h"
 #include "media/cast/sender/vp8_encoder.h"
 #include "media/cast/test/utility/default_config.h"
 #include "media/cast/test/utility/standalone_cast_environment.h"
@@ -85,7 +86,7 @@ class VideoDecoderTest : public ::testing::TestWithParam<Codec> {
     PopulateVideoFrame(video_frame.get(), 0);
 
     // Encode |frame| into |encoded_frame->data|.
-    scoped_ptr<EncodedFrame> encoded_frame(new EncodedFrame());
+    scoped_ptr<SenderEncodedFrame> encoded_frame(new SenderEncodedFrame());
     // Test only supports VP8, currently.
     CHECK_EQ(CODEC_VIDEO_VP8, GetParam());
     vp8_encoder_.Encode(video_frame, reference_time, encoded_frame.get());
