71493548d2dbb96b505a25c24dde8527ae01c8ab
sigbjornf@opera.com <sigbjornf@opera.com>
2015-05-22 07:30:05 +0000

    Oilpan: insist on eager sweeping for some objects.
    To prepare for enabling lazy sweeping, non-Oilpan - add EAGERLY_SWEEP()
    annotations for LifecycleObserver and DOMWindowProperty.
    
    The former is a template, so extend range of EAGERLY_SWEEP() macros
    to handle that also, along with ways to handle ambiguity.
    
    R=haraken
    BUG=420515
    
    Review URL: https://codereview.chromium.org/1146353002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195772 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/DOMWindowProperty.h
+++ third_party/WebKit/Source/core/frame/DOMWindowProperty.h
@@ -44,6 +44,9 @@ public:
 
     LocalFrame* frame() const { return m_frame; }
 
+    // TODO(Oilpan): remove eager sweeping once DOMWindowProperty is
+    // always on the heap.
+    EAGERLY_SWEEP();
     DECLARE_VIRTUAL_TRACE();
 
 protected:
@@ -63,4 +66,4 @@ private:
 
 } // namespace blink
 
-#endif
+#endif // DOMWindowProperty_h
