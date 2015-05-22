f5c2e9cc99c7a099d2723e1ef16832f80ecad215
yzshen <yzshen@chromium.org>
2015-05-22 15:55:04 -0700

    Add the basic structure of a Mojo DevTools service.
    The implementation will be done in follow-up CLs.
    
    This service is used by Mandoline only and won't have any effect on existing Chrome DevTools code.
    
    BUG=478249
    TEST=None
    
    Review URL: https://codereview.chromium.org/1144393002
    
    Cr-Commit-Position: refs/heads/master@{#331194}

---
+++ components/devtools_service/main.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/devtools_service/devtools_service_delegate.h"
+#include "mojo/application/public/cpp/application_runner.h"
+#include "third_party/mojo/src/mojo/public/c/system/main.h"
+
+MojoResult MojoMain(MojoHandle shell_handle) {
+  mojo::ApplicationRunner runner(new devtools_service::DevToolsServiceDelegate);
+  return runner.Run(shell_handle);
+}
