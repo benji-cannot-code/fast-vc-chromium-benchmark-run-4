26fc6dc0d2185fbb5665df6a7d9926d847865b97
megjablon <megjablon@chromium.org>
2015-05-22 18:25:29 -0700

    Use Chrome-Proxy header instead of X-PSA-Client-Options.
    Replace X-PSA-Client-Options: v=1,m=1 with Chrome-Proxy: pass-through
    for loads without the Data Reduction Proxy.
    
    BUG=334329
    
    Review URL: https://codereview.chromium.org/1143423002
    
    Cr-Commit-Position: refs/heads/master@{#331217}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_settings.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_settings.cc
@@ -35,7 +35,7 @@ bool IsLoFiEnabledOnCommandLine() {
 namespace data_reduction_proxy {
 
 const char kDataReductionPassThroughHeader[] =
-    "X-PSA-Client-Options: v=1,m=1\nCache-Control: no-cache";
+    "Chrome-Proxy: pass-through\nCache-Control: no-cache";
 
 DataReductionProxySettings::DataReductionProxySettings()
     : unreachable_(false),
