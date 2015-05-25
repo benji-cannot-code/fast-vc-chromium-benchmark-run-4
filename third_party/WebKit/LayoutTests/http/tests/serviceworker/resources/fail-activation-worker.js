a753706be12b7a1bea2d80a626309ef11994a87d
falken@chromium.org <falken@chromium.org>
2015-05-25 06:25:59 +0000

    Service Worker: Remove the concept of "fail to activate".
    As per spec: https://github.com/slightlyoff/ServiceWorker/issues/659
    
    Three-sided patch:
    [1] Disable layout tests. https://codereview.chromium.org/1139163003/
    [2] Chromium change. https://codereview.chromium.org/1126283005
    [3] Fix layout tests. (this patch)
    
    BUG=480050
    
    Review URL: https://codereview.chromium.org/1133493004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195850 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/fail-activation-worker.js
+++
@@ -1,4 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-onactivate = function(e) {
-  e.waitUntil(clients.claim().then(function() { return Promise.reject(); }));
-};
