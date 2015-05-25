3a2f4b274a98a85d3477e20bd93e1fbfb12fc7dc
mikhail.pozdnyakov@intel.com <mikhail.pozdnyakov@intel.com>
2015-05-25 10:22:12 +0000

    Fix comparison between two vectors with different inline capacity
    There was an error when comparing two empty vectors one of which had inline capacity. A vector with inline capacity always returns non-zero value from its data() method (even when it is empty) so VectorComparer erroneously returned 'false' after checking two data pointers for equality to null.
    
    Besides fixing above, this CL adds null-check assertions for the VectorComparer arguments.
    
    Review URL: https://codereview.chromium.org/1121743004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195855 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/Vector.h
+++ third_party/WebKit/Source/wtf/Vector.h
@@ -250,9 +250,9 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
     {
         static bool compare(const T* a, const T* b, size_t size)
         {
-            if (LIKELY(a && b))
-                return std::equal(a, a + size, b);
-            return !a && !b;
+            ASSERT(a);
+            ASSERT(b);
+            return std::equal(a, a + size, b);
         }
     };
 
@@ -261,6 +261,8 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
     {
         static bool compare(const T* a, const T* b, size_t size)
         {
+            ASSERT(a);
+            ASSERT(b);
             return memcmp(a, b, sizeof(T) * size) == 0;
         }
     };
@@ -1294,7 +1296,8 @@ static const size_t kInitialVectorSize = WTF_VECTOR_INITIAL_SIZE;
     {
         if (a.size() != b.size())
             return false;
-
+        if (a.isEmpty())
+            return true;
         return VectorTypeOperations<T>::compare(a.data(), b.data(), a.size());
     }
 
