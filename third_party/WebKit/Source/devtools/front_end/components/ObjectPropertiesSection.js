46455ba739fc7fb0a791592289694d3d82c34221
pfeldman@chromium.org <pfeldman@chromium.org>
2015-05-25 15:48:41 +0000

    DevTools: render part of the function body as its preview.
    BUG=490202
    
    Review URL: https://codereview.chromium.org/1152993003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195867 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/components/ObjectPropertiesSection.js
+++ third_party/WebKit/Source/devtools/front_end/components/ObjectPropertiesSection.js
@@ -1171,8 +1171,9 @@ WebInspector.ObjectPropertiesSection.createValueElement = function(value, wasThr
  * @param {!WebInspector.RemoteObject} func
  * @param {!Element} element
  * @param {boolean} linkify
+ * @param {boolean=} includePreview
  */
-WebInspector.ObjectPropertiesSection.formatObjectAsFunction = function(func, element, linkify)
+WebInspector.ObjectPropertiesSection.formatObjectAsFunction = function(func, element, linkify, includePreview)
 {
     func.functionDetails(didGetDetails);
 
@@ -1194,6 +1195,12 @@ WebInspector.ObjectPropertiesSection.formatObjectAsFunction = function(func, ele
             element = anchor;
         }
 
+        var text = func.description.substring(0, 200);
+        if (includePreview) {
+            element.textContent = text.replace(/^function /, "") + (func.description.length > 200 ? "\u2026" : "");
+            return;
+        }
+
         // Now parse description and get the real params and title.
         self.runtime.instancePromise(WebInspector.TokenizerFactory).then(processTokens);
 
@@ -1205,7 +1212,6 @@ WebInspector.ObjectPropertiesSection.formatObjectAsFunction = function(func, ele
          */
         function processTokens(tokenizerFactory)
         {
-            var text = func.description.substring(0, 200);
             var tokenize = tokenizerFactory.createTokenizer("text/javascript");
             tokenize(text, processToken);
             element.textContent = (functionName || "anonymous") + "(" + (params || []).join(", ") + ")";
