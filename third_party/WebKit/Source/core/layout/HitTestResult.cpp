c87a33b1f7f7052f0010e1702b15e29243caec52
robhogan@gmail.com <robhogan@gmail.com>
2015-05-26 19:42:08 +0000

    Image elements can have inline layout objects
    If it has a content: directive and no source then an image element
    will get an inline renderer, so we can't assume that an image is always
    a box. I'd resisted introducing this check as I felt that it wasn't possible
    but clearly didn't experiment enough.
    
    BUG=491582
    
    Review URL: https://codereview.chromium.org/1146073003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195920 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/HitTestResult.cpp
+++ third_party/WebKit/Source/core/layout/HitTestResult.cpp
@@ -167,7 +167,7 @@ HTMLAreaElement* HitTestResult::imageAreaForImage() const
         }
     }
 
-    if (!imageElement || !imageElement->layoutObject())
+    if (!imageElement || !imageElement->layoutObject() || !imageElement->layoutObject()->isBox())
         return nullptr;
 
     HTMLMapElement* map = imageElement->treeScope().getImageMap(imageElement->fastGetAttribute(usemapAttr));
