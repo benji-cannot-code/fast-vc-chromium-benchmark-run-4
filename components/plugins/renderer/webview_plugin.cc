38cd386a45895a04cc78de3b72e1be4a5e97f466
chrishtr <chrishtr@chromium.org>
2015-05-26 12:21:33 -0700

    Call setNeedsLayout instead of invalidate when scheduling plugin webview animation.
    scheduleAnimation could be called for a variety of reasons, but we need to assume that layout and not just paint invalidation is needed, in order to avoid stale layout state during paint of the plugin's WebView.
    
    BUG=436562
    
    Review URL: https://codereview.chromium.org/1158693002
    
    Cr-Commit-Position: refs/heads/master@{#331402}

--- components/plugins/renderer/webview_plugin.cc
+++ components/plugins/renderer/webview_plugin.cc
@@ -258,7 +258,7 @@ void WebViewPlugin::didChangeCursor(const WebCursorInfo& cursor) {
 
 void WebViewPlugin::scheduleAnimation() {
   if (container_)
-    container_->invalidate();
+    container_->setNeedsLayout();
 }
 
 void WebViewPlugin::didClearWindowObject(WebLocalFrame* frame) {
