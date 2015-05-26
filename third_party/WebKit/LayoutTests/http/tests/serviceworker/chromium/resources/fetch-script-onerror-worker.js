ee0f219475bab03632769a177c9b2cdd8a938b07
horo@chromium.org <horo@chromium.org>
2015-05-26 09:08:48 +0000

    Add LayoutTest for the script error sanitization for ServiceWorker fetched scripts.
    BUG=483488
    
    Review URL: https://codereview.chromium.org/1151433002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/chromium/resources/fetch-script-onerror-worker.js
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function get_query_params(url) {
+  var search = (new URL(url)).search;
+  if (!search) {
+    return {};
+  }
+  var ret = {};
+  var params = search.substring(1).split('&');
+  params.forEach(function(param) {
+      var element = param.split('=');
+      ret[decodeURIComponent(element[0])] = decodeURIComponent(element[1]);
+    });
+  return ret;
+}
+
+function get_request_init(params) {
+  var init = {};
+  if (params['mode']) {
+    init['mode'] = params['mode'];
+  }
+  return init;
+}
+
+self.addEventListener('fetch', function(event) {
+    var params = get_query_params(event.request.url);
+    var init = get_request_init(params);
+    var url = params['url'];
+    event.respondWith(new Promise(function(resolve, reject) {
+        var request = event.request;
+        if (url) {
+          request = new Request(url, init);
+        }
+        fetch(request).then(resolve, reject);
+      }));
+  });
