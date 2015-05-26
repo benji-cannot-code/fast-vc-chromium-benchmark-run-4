396f64922ebd6c31d28e072ccaac63675309c368
japhet <japhet@chromium.org>
2015-05-26 12:25:56 -0700

    Fix a use-after-free in content_shell
    If a test completes inside a DecidePolicyForNavigation call, and the navigation
    in question is a redirect, we might delete the provisional data source, then
    try to access its now-deleted ExtraData. If the test ends inside
    WebTestProxyBase::DecidePolicyForNavigation, abort the navigation.
    
    BUG=452638
    
    Review URL: https://codereview.chromium.org/1140733007
    
    Cr-Commit-Position: refs/heads/master@{#331406}

--- content/shell/renderer/test_runner/web_test_proxy.cc
+++ content/shell/renderer/test_runner/web_test_proxy.cc
@@ -1361,8 +1361,11 @@ blink::WebNavigationPolicy WebTestProxyBase::DecidePolicyForNavigation(
   else
     result = blink::WebNavigationPolicyIgnore;
 
-  if (test_interfaces_->GetTestRunner()->policyDelegateShouldNotifyDone())
+  if (test_interfaces_->GetTestRunner()->policyDelegateShouldNotifyDone()) {
     test_interfaces_->GetTestRunner()->policyDelegateDone();
+    result = blink::WebNavigationPolicyIgnore;
+  }
+
   return result;
 }
 
