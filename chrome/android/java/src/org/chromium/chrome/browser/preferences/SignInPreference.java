74119e1da396848a9936fb3ec11b6196edf31374
aruslan <aruslan@chromium.org>
2015-05-26 16:54:53 -0700

    Allow sign-in in Preferences if FRE sign-in is done.
    BUG=487379
    
    Review URL: https://codereview.chromium.org/1151773002
    
    Cr-Commit-Position: refs/heads/master@{#331479}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/SignInPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/SignInPreference.java
@@ -16,6 +16,7 @@ import android.util.AttributeSet;
 import android.view.View;
 
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.firstrun.FirstRunSignInProcessor;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.profiles.ProfileDownloader;
 import org.chromium.chrome.browser.signin.AccountManagementFragment;
@@ -51,6 +52,7 @@ public class SignInPreference extends Preference implements SignInAllowedObserve
         SigninManager manager = SigninManager.get(getContext());
         manager.addSignInAllowedObserver(this);
         ProfileDownloader.addObserver(this);
+        FirstRunSignInProcessor.updateSigninManagerFirstRunCheckDone(getContext());
     }
 
     /**
