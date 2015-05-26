6d717b0df2db321f50ee5358d50ecc198e3ed035
primiano <primiano@chromium.org>
2015-05-26 02:44:33 -0700

    android-webview: Add WebView version to microdumps.
    Add the plumbing to return the right product name and version
    to breakpad, which will be propagated into the microdump.
    
    The new microdump output looks as follows:
    --------- beginning of main
    W/google-breakpad(25214): -----BEGIN BREAKPAD MICRODUMP-----
    W/google-breakpad(25214): V WebView:44.0.2403.0
    W/google-breakpad(25214): O A arm 04 google/occam/mako:5.1.1/LMY48D/1906941:userdebug/dev-keys
    [the rest is unchanged]
    
    BUG=456494,457700
    TBR=torne@chromium.org
    
    Review URL: https://codereview.chromium.org/1159773002
    
    Cr-Commit-Position: refs/heads/master@{#331341}

--- android_webview/crash_reporter/aw_microdump_crash_reporter.cc
+++ android_webview/crash_reporter/aw_microdump_crash_reporter.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/crash_reporter/aw_microdump_crash_reporter.h"
 
+#include "android_webview/common/aw_version_info_values.h"
 #include "base/lazy_instance.h"
 #include "build/build_config.h"
 #include "components/crash/app/breakpad_linux.h"
@@ -23,6 +24,11 @@ class AwCrashReporterClient : public ::crash_reporter::CrashReporterClient {
   bool IsRunningUnattended() override { return false; }
   bool GetCollectStatsConsent() override { return false; }
 
+  void GetProductNameAndVersion(const char** product_name,
+                                const char** version) override {
+    *product_name = "WebView";
+    *version = PRODUCT_VERSION;
+  }
   // Microdumps are always enabled in WebView builds, conversely to what happens
   // in the case of the other Chrome for Android builds (where they are enabled
   // only when NO_UNWIND_TABLES == 1).
