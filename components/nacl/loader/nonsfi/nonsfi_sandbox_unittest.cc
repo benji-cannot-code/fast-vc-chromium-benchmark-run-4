2e942276e7bc1c86b7854158964019d13b255259
hidehiko <hidehiko@chromium.org>
2015-05-26 03:08:09 -0700

    Non-SFI mode: Disallow socketpair in nacl_helper_nonsfi's sandbox.
    All socketpair() usage in nacl_helper_nonsfi was removed.
    This CL restricts it in the sandbox.
    Note that it is still allowed on nacl_helper in Non-SFI mode.
    It is used by host-build libevent, which is used by nacl_helper.
    
    TEST=Ran trybots. Ran nacl_helper_nonsfi_unittests locally. Ran browser_tests --gtest_filter=*NonSfi* locally.
    BUG=358417
    
    Review URL: https://codereview.chromium.org/1159633003
    
    Cr-Commit-Position: refs/heads/master@{#331342}

--- components/nacl/loader/nonsfi/nonsfi_sandbox_unittest.cc
+++ components/nacl/loader/nonsfi/nonsfi_sandbox_unittest.cc
@@ -201,6 +201,15 @@ BPF_DEATH_TEST_C(NaClNonSfiSandboxTest,
   syscall(__NR_prctl, PR_SET_DUMPABLE, 1UL);
 }
 
+#if defined(OS_NACL_NONSFI)
+BPF_DEATH_TEST_C(NaClNonsfiSandboxTest,
+                 socketpair,
+                 DEATH_SEGV_MESSAGE(sandbox::GetErrorMessageContentForTests()),
+                 nacl::nonsfi::NaClNonSfiBPFSandboxPolicy) {
+  int tmp_fds[2];
+  socketpair(AF_UNIX, SOCK_STREAM, 0, tmp_fds);
+}
+#else
 BPF_TEST_C(NaClNonSfiSandboxTest,
            socketcall_allowed,
            nacl::nonsfi::NaClNonSfiBPFSandboxPolicy) {
@@ -219,6 +228,7 @@ BPF_TEST_C(NaClNonSfiSandboxTest,
                 HANDLE_EINTR(recvmsg(fds[0].get(), &msg, 0)));
   BPF_ASSERT_EQ(0, shutdown(fds[0].get(), SHUT_RDWR));
 }
+#endif
 
 BPF_DEATH_TEST_C(NaClNonSfiSandboxTest,
                  accept,
@@ -398,7 +408,7 @@ BPF_TEST_C(NaClNonSfiSandboxTest,
            fcntl_SETFD_allowed,
            nacl::nonsfi::NaClNonSfiBPFSandboxPolicy) {
   base::ScopedFD fds[2];
-  DoSocketpair(fds);
+  DoPipe(fds);
   BPF_ASSERT_EQ(0, fcntl(fds[0].get(), F_SETFD, FD_CLOEXEC));
 }
 
@@ -407,7 +417,7 @@ BPF_DEATH_TEST_C(NaClNonSfiSandboxTest,
                  DEATH_SEGV_MESSAGE(sandbox::GetErrorMessageContentForTests()),
                  nacl::nonsfi::NaClNonSfiBPFSandboxPolicy) {
   base::ScopedFD fds[2];
-  DoSocketpair(fds);
+  DoPipe(fds);
   fcntl(fds[0].get(), F_SETFD, 99);
 }
 
@@ -427,7 +437,7 @@ BPF_DEATH_TEST_C(NaClNonSfiSandboxTest,
                  DEATH_SEGV_MESSAGE(sandbox::GetErrorMessageContentForTests()),
                  nacl::nonsfi::NaClNonSfiBPFSandboxPolicy) {
   base::ScopedFD fds[2];
-  DoSocketpair(fds);
+  DoPipe(fds);
   fcntl(fds[0].get(), F_SETFL, O_APPEND);
 }
 
