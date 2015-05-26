0db30f6e7ec73b52c178349a1d7a54ea0f8b8447
danakj <danakj@chromium.org>
2015-05-26 13:08:38 -0700

    Add PERSISTENT_MAP usage for GpuMemoryBuffers.
    A GpuMemoryBuffer with this usage flag will always point at the
    same memory contents each time it is mapped. This will enable
    partial tile updates by avoiding rastering content from the
    previous frame again in the compositor.
    
    R=reveman,piman
    BUG=489447
    
    Committed: https://crrev.com/77180b4434d93f45022b8c65110344cdced4b19d
    Cr-Commit-Position: refs/heads/master@{#330987}
    
    Review URL: https://codereview.chromium.org/1139903005
    
    Cr-Commit-Position: refs/heads/master@{#331416}

--- ui/ozone/platform/drm/gpu/gbm_surface_factory.cc
+++ ui/ozone/platform/drm/gpu/gbm_surface_factory.cc
@@ -156,6 +156,8 @@ bool GbmSurfaceFactory::CanCreateNativePixmap(BufferUsage usage) {
   switch (usage) {
     case MAP:
       return false;
+    case PERSISTENT_MAP:
+      return false;
     case SCANOUT:
       return true;
   }
