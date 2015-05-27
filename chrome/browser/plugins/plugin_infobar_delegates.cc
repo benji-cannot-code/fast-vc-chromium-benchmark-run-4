fa3842aeb08030d106e5eb31d91d3677d8ea61d7
gburanov <gburanov@gmail.com>
2015-05-27 07:01:33 -0700

    remove brand specific resources from Chromium resources
    See Remove product specific strings from generated_resources - similar review
    
    https://codereview.chromium.org/1071633002/
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1116673002
    
    Cr-Commit-Position: refs/heads/master@{#331568}

--- chrome/browser/plugins/plugin_infobar_delegates.cc
+++ chrome/browser/plugins/plugin_infobar_delegates.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/shell_integration.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/common/url_constants.h"
+#include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/locale_settings.h"
 #include "components/google/core/browser/google_util.h"
