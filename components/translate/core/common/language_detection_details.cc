f855e08fb76efe074a5712c268c74650dabe6794
toyoshim <toyoshim@chromium.org>
2015-05-26 21:35:10 -0700

    Translate: fix msan errors on unit_tests
    Three translate tests on unit_tests fail with msan errors.
    This change fixes an error of use-of-uninitialized-value.
    
    BUG=488311
    
    Review URL: https://codereview.chromium.org/1158143002
    
    Cr-Commit-Position: refs/heads/master@{#331530}

--- components/translate/core/common/language_detection_details.cc
+++ components/translate/core/common/language_detection_details.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace translate {
 
 LanguageDetectionDetails::LanguageDetectionDetails()
-    : is_cld_reliable(false) {
+    : is_cld_reliable(false), has_notranslate(false) {
 }
 
 LanguageDetectionDetails::~LanguageDetectionDetails() {}
