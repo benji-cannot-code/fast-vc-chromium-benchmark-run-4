d67dfb3d659e46595f2c0d30241044ad2dff51b9
nya <nya@chromium.org>
2015-05-26 23:34:23 -0700

    NaCl: Increase validation cache size.
    Extend NaCl validation cache since ARC is going to have ~200
    DSOs soon. Memory footprint increase is very small
    (32bytes/entry).
    
    TEST=Verified validation cache miss does not occur by setting
         kValidationCacheCacheSize = 300 and launching ARC apps
    TEST=Verified validation cache miss occurs by setting
         kValidationCacheCacheSize = 10 and launching ARC apps
    BUG=486269
    
    Review URL: https://codereview.chromium.org/1145983002
    
    Cr-Commit-Position: refs/heads/master@{#331541}

--- components/nacl/browser/nacl_validation_cache.cc
+++ components/nacl/browser/nacl_validation_cache.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace nacl {
 
 // For the moment, choose an arbitrary cache size.
-const size_t kValidationCacheCacheSize = 200;
+const size_t kValidationCacheCacheSize = 500;
 // Key size is equal to the block size (not the digest size) of SHA256.
 const size_t kValidationCacheKeySize = 64;
 // Entry size is equal to the digest size of SHA256.
