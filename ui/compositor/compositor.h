6f224ca195fd7a2c2d635293f914e8b5eae8a1cc
danakj <danakj@chromium.org>
2015-05-26 19:09:43 -0700

    Move decisions about RGBA vs BGRA textures to ui::Compositor.
    Pass that decision to the ContextFactory instead of making the decision
    in the GpuProcessTransportFactory. This is a decision that the
    compositor should be making for itself.
    
    R=piman@chromium.org, reveman
    TBR=sky
    BUG=487387
    
    Review URL: https://codereview.chromium.org/1158783004
    
    Cr-Commit-Position: refs/heads/master@{#331511}

--- ui/compositor/compositor.h
+++ ui/compositor/compositor.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer_animator_collection.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/geometry/vector2d.h"
+#include "ui/gfx/gpu_memory_buffer.h"
 #include "ui/gfx/native_widget_types.h"
 
 namespace base {
@@ -96,7 +97,8 @@ class COMPOSITOR_EXPORT ContextFactory {
   virtual bool DoesCreateTestContexts() = 0;
 
   // Returns the OpenGL target to use for image textures.
-  virtual uint32 GetImageTextureTarget() = 0;
+  virtual uint32 GetImageTextureTarget(gfx::GpuMemoryBuffer::Format format,
+                                       gfx::GpuMemoryBuffer::Usage usage) = 0;
 
   // Gets the shared bitmap manager for software mode.
   virtual cc::SharedBitmapManager* GetSharedBitmapManager() = 0;
