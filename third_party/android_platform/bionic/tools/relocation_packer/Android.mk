b66ea4bac90ed735a89345634d090e538ae429ce
caitkp <caitkp@chromium.org>
2015-05-28 11:39:45 -0700

    Revert of Refresh android relocation packer from AOSP bionic. (patchset #1 id:1 of https://codereview.chromium.org/1164453002/)
    Reason for revert:
    This CL is causing check_perms failures on the linux builds: http://build.chromium.org/p/chromium/builders/Linux/builds/62718/steps/check_perms/logs/stdio
    
    Logs:
    
    /b/build/slave/Linux/build/src/third_party/android_platform/bionic/tools/relocation_packer/test_data/elf_file_unittest_relocs_ia32.so: Has ELF header but not executable bit
    /b/build/slave/Linux/build/src/third_party/android_platform/bionic/tools/relocation_packer/test_data/elf_file_unittest_relocs_ia32_packed.so: Has ELF header but not executable bit
    /b/build/slave/Linux/build/src/third_party/android_platform/bionic/tools/relocation_packer/test_data/elf_file_unittest_relocs_mips32.so: Has ELF header but not executable bit
    /b/build/slave/Linux/build/src/third_party/android_platform/bionic/tools/relocation_packer/test_data/elf_file_unittest_relocs_mips32_packed.so: Has ELF header but not executable bit
    /b/build/slave/Linux/build/src/third_party/android_platform/bionic/tools/relocation_packer/test_data/elf_file_unittest_relocs_x64.so: Has ELF header but not executable bit
    /b/build/slave/Linux/build/src/third_party/android_platform/bionic/tools/relocation_packer/test_data/elf_file_unittest_relocs_x64_packed.so: Has ELF header but not executable bit
    
    Original issue's description:
    > Refresh android relocation packer from AOSP bionic.
    >
    > Imports:
    >   Fix unit tests, and extend for other architectures
    >     https://android-review.googlesource.com/#/c/151901/
    >
    >   Do not adjust PT_GNU_STACK segment
    >     https://android-review.googlesource.com/#/c/149300/
    >
    >   Adjust DT_MIPS_RLD_MAP2 value
    >     https://android-review.googlesource.com/#/c/148822/
    >
    >   Reduce p_align for program header to page size.
    >     https://android-review.googlesource.com/#/c/148492/
    >
    >   Exit normally when relocations are already packed.
    >     https://android-review.googlesource.com/#/c/148175/
    >
    > NOPRESUBMIT=true
    > BUG=385553
    >
    > Committed: https://crrev.com/9cb10e88d01e71c5786a463f7c39409e7a41ddcd
    > Cr-Commit-Position: refs/heads/master@{#331808}
    
    TBR=rmcilroy@chromium.org,simonb@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=385553
    
    Review URL: https://codereview.chromium.org/1143303007
    
    Cr-Commit-Position: refs/heads/master@{#331827}

--- third_party/android_platform/bionic/tools/relocation_packer/Android.mk
+++ third_party/android_platform/bionic/tools/relocation_packer/Android.mk
@@ -27,6 +27,7 @@ LOCAL_SRC_FILES := \
   src/debug.cc \
   src/delta_encoder.cc \
   src/elf_file.cc \
+  src/leb128.cc \
   src/packer.cc \
   src/sleb128.cc \
 
@@ -46,9 +47,6 @@ LOCAL_CPP_EXTENSION := .cc
 
 LOCAL_SRC_FILES := src/main.cc
 LOCAL_STATIC_LIBRARIES := lib_relocation_packer libelf
-
-# Statically linking libc++ to make it work from prebuilts
-LOCAL_CXX_STL := libc++_static
 LOCAL_C_INCLUDES := external/elfutils/src/libelf libnativehelper/include
 
 LOCAL_MODULE := relocation_packer
@@ -67,6 +65,7 @@ LOCAL_SRC_FILES := \
   src/debug_unittest.cc \
   src/delta_encoder_unittest.cc \
   src/elf_file_unittest.cc \
+  src/leb128_unittest.cc \
   src/sleb128_unittest.cc \
   src/packer_unittest.cc \
 
@@ -96,9 +95,3 @@ $(eval $(call copy-test-library,elf_file_unittest_relocs_arm32.so))
 $(eval $(call copy-test-library,elf_file_unittest_relocs_arm32_packed.so))
 $(eval $(call copy-test-library,elf_file_unittest_relocs_arm64.so))
 $(eval $(call copy-test-library,elf_file_unittest_relocs_arm64_packed.so))
-$(eval $(call copy-test-library,elf_file_unittest_relocs_ia32.so))
-$(eval $(call copy-test-library,elf_file_unittest_relocs_ia32_packed.so))
-$(eval $(call copy-test-library,elf_file_unittest_relocs_x64.so))
-$(eval $(call copy-test-library,elf_file_unittest_relocs_x64_packed.so))
-$(eval $(call copy-test-library,elf_file_unittest_relocs_mips32.so))
-$(eval $(call copy-test-library,elf_file_unittest_relocs_mips32_packed.so))
