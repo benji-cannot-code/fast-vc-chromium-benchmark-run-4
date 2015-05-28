caad7ba21ee278b0cb41943a9a734855c87aadff
dstockwell@chromium.org <dstockwell@chromium.org>
2015-05-28 09:48:49 +0000

    Export StyleResolverStats via tracing
    Also adds new counters for rules matched, rejected and fast-rejected.
    
    This is a reland of https://src.chromium.org/viewvc/blink?view=rev&revision=195952
    The previous version did not test the blink_style category when enabling stats.
    
    BUG=475370
    
    Review URL: https://codereview.chromium.org/1152193004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/SharedStyleFinder.cpp
+++ third_party/WebKit/Source/core/css/resolver/SharedStyleFinder.cpp
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLOptionElement.h"
 #include "core/style/ComputedStyle.h"
 #include "core/svg/SVGElement.h"
+#include "platform/TraceEvent.h"
 #include "wtf/HashSet.h"
 #include "wtf/text/AtomicString.h"
 
@@ -311,7 +312,7 @@ bool SharedStyleFinder::matchesRuleSet(RuleSet* ruleSet)
 
 ComputedStyle* SharedStyleFinder::findSharedStyle()
 {
-    INCREMENT_STYLE_STATS_COUNTER(m_styleResolver, sharedStyleLookups);
+    INCREMENT_STYLE_STATS_COUNTER(m_styleResolver, sharedStyleLookups, 1);
 
     if (!element().supportsStyleSharing())
         return 0;
@@ -322,26 +323,26 @@ ComputedStyle* SharedStyleFinder::findSharedStyle()
     Element* shareElement = findElementForStyleSharing();
 
     if (!shareElement) {
-        if (m_styleResolver.stats() && m_styleResolver.stats()->printMissedCandidateCount && documentContainsValidCandidate())
-            INCREMENT_STYLE_STATS_COUNTER(m_styleResolver, sharedStyleMissed);
+        if (m_styleResolver.stats() && m_styleResolver.stats()->allCountersEnabled() && documentContainsValidCandidate())
+            INCREMENT_STYLE_STATS_COUNTER(m_styleResolver, sharedStyleMissed, 1);
         return 0;
     }
 
-    INCREMENT_STYLE_STATS_COUNTER(m_styleResolver, sharedStyleFound);
+    INCREMENT_STYLE_STATS_COUNTER(m_styleResolver, sharedStyleFound, 1);
 
     if (matchesRuleSet(m_siblingRuleSet)) {
-        INCREMENT_STYLE_STATS_COUNTER(m_styleResolver, sharedStyleRejectedBySiblingRules);
+        INCREMENT_STYLE_STATS_COUNTER(m_styleResolver, sharedStyleRejectedBySiblingRules, 1);
         return 0;
     }
 
     if (matchesRuleSet(m_uncommonAttributeRuleSet)) {
-        INCREMENT_STYLE_STATS_COUNTER(m_styleResolver, sharedStyleRejectedByUncommonAttributeRules);
+        INCREMENT_STYLE_STATS_COUNTER(m_styleResolver, sharedStyleRejectedByUncommonAttributeRules, 1);
         return 0;
     }
 
     // Tracking child index requires unique style for each node. This may get set by the sibling rule match above.
     if (!element().parentElementOrShadowRoot()->childrenSupportStyleSharing()) {
-        INCREMENT_STYLE_STATS_COUNTER(m_styleResolver, sharedStyleRejectedByParent);
+        INCREMENT_STYLE_STATS_COUNTER(m_styleResolver, sharedStyleRejectedByParent, 1);
         return 0;
     }
 
