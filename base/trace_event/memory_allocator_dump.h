29a449e44f0808f9593c05f1f519cbf323712f02
primiano <primiano@chromium.org>
2015-05-28 05:34:49 -0700

    [tracing] Extend memory dump API with graphs
    This CL allows to express ownership relationships between
    MemoryAllocatorDump(s). Ownership edges are the building block
    to express suballocation, sharing (co-ownership) and
    cross-process-sharing (the latter might require an extra CL to
    introduce global nodes).
    See the related bug for the design doc and concrete use cases.
    
    BUG=492102
    
    Review URL: https://codereview.chromium.org/1161813004
    
    Cr-Commit-Position: refs/heads/master@{#331768}

--- base/trace_event/memory_allocator_dump.h
+++ base/trace_event/memory_allocator_dump.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_export.h"
 #include "base/basictypes.h"
 #include "base/logging.h"
+#include "base/trace_event/memory_allocator_dump_guid.h"
 #include "base/values.h"
 
 namespace base {
@@ -22,6 +23,9 @@ class TracedValue;
 class BASE_EXPORT MemoryAllocatorDump {
  public:
   // MemoryAllocatorDump is owned by ProcessMemoryDump.
+  MemoryAllocatorDump(const std::string& absolute_name,
+                      ProcessMemoryDump* process_memory_dump,
+                      const MemoryAllocatorDumpGuid& guid);
   MemoryAllocatorDump(const std::string& absolute_name,
                       ProcessMemoryDump* process_memory_dump);
   ~MemoryAllocatorDump();
@@ -69,10 +73,19 @@ class BASE_EXPORT MemoryAllocatorDump {
     return process_memory_dump_;
   }
 
+  // |guid| is an optional global dump identifier, unique across all processes
+  // within the scope of a global dump. It is only required when using the
+  // graph APIs (see TODO_method_name) to express retention / suballocation or
+  // cross process sharing. See crbug.com/492102 for design docs.
+  // Subsequent MemoryAllocatorDump(s) with the same |absolute_name| are
+  // expected to have the same guid.
+  const MemoryAllocatorDumpGuid& guid() const { return guid_; }
+
  private:
   const std::string absolute_name_;
   ProcessMemoryDump* const process_memory_dump_;  // Not owned (PMD owns this).
   DictionaryValue attributes_;
+  MemoryAllocatorDumpGuid guid_;
 
   DISALLOW_COPY_AND_ASSIGN(MemoryAllocatorDump);
 };
