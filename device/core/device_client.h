f4d6712e131fb354e7ac0b1bb0b82f07b1d1c306
rockot <rockot@chromium.org>
2015-05-27 17:42:53 -0700

    Build a basic Mojo service framework for device/usb
    BUG=492805
    R=reillyg@chromium.org
    
    Review URL: https://codereview.chromium.org/1155163008
    
    Cr-Commit-Position: refs/heads/master@{#331707}

--- device/core/device_client.h
+++ device/core/device_client.h
@@ -7,11 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DEVICE_CORE_DEVICE_CLIENT_H_
 
 #include "base/macros.h"
+#include "third_party/mojo/src/mojo/public/cpp/bindings/interface_request.h"
 
 namespace device {
 
 class HidService;
 class UsbService;
+
+namespace usb {
+class DeviceManager;
+}
 
 // Interface used by consumers of //device APIs to get pointers to the service
 // singletons appropriate for a given embedding application. For an example see
@@ -22,13 +27,18 @@ class DeviceClient {
   DeviceClient();
 
   // Destruction clears the single instance.
-  ~DeviceClient();
+  virtual ~DeviceClient();
 
   // Returns the single instance of |this|.
   static DeviceClient* Get();
 
   // Returns the UsbService instance for this embedder.
   virtual UsbService* GetUsbService();
+
+  // Connects a USB DeviceManager client to a concrete implementation. If
+  // no such implementation is available the request is dropped.
+  virtual void ConnectToUSBDeviceManager(
+      mojo::InterfaceRequest<usb::DeviceManager> request);
 
   // Returns the HidService instance for this embedder.
   virtual HidService* GetHidService();
