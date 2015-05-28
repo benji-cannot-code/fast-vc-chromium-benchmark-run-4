54e819a66e04d1f8c751689b5273c79d5e5dcf52
pcc@chromium.org <pcc@chromium.org>
2015-05-28 19:25:33 +0000

    Use new no_sanitize attribute to blacklist GC operator new function.
    Introduce a NO_SANITIZE_UNRELATED_CAST macro which disables
    runtime checks relating to casts between unrelated objects. When
    compiling with Clang it expands to the new no_sanitize attribute (see
    http://clang.llvm.org/docs/AttributeReference.html#no-sanitize-clang-no-sanitize)
    which we use to disable -fsanitize=vptr and -fsanitize=cfi-unrelated-cast.
    
    Apply this attribute to the operator new function expanded from the
    DEFINE_GARBAGE_COLLECTED_MIXIN_CONSTRUCTOR_MARKER macro, and remove the
    forbidGCDuringConstruction member function, which was introduced as a
    workaround for the lack of such an attribute.
    
    Mostly reverts https://codereview.chromium.org/1064693005.
    
    BUG=457523,476063
    R=thakis@chromium.org
    CC=inferno@chromium.org,kcc@chromium.org
    
    Review URL: https://codereview.chromium.org/1147183002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196079 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/Compiler.h
+++ third_party/WebKit/Source/wtf/Compiler.h
@@ -159,4 +159,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_PRETTY_FUNCTION __FUNCTION__
 #endif
 
+
+/* NO_SANITIZE_UNRELATED_CAST - Disable runtime checks related to casts between
+ * unrelated objects (-fsanitize=cfi-unrelated-cast or -fsanitize=vptr). */
+
+#if COMPILER(CLANG)
+#define NO_SANITIZE_UNRELATED_CAST __attribute__((no_sanitize("cfi-unrelated-cast", "vptr")))
+#else
+#define NO_SANITIZE_UNRELATED_CAST
+#endif
+
 #endif /* WTF_Compiler_h */
