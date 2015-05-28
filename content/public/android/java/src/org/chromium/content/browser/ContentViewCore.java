2bf44c4a55c8f1f255d98bd0d02d7f095a3479a4
jdduke <jdduke@chromium.org>
2015-05-27 17:21:59 -0700

    aw: Ensure synchronization of PopupWindow handle visibility/position
    Setting the visibility and position of a PopupWindow does not guarantee
    that the new visibility/position will take effect in the same frame.
    This leads to cases where the PopupWindow touch handles can appear out
    of position for a single frame.
    
    Prevent this by delaying visibility updates by one frame when the handles
    transition from hidden to visible.
    
    Also avoid issues where Android rejects one-dimensional selection
    rect regions, as well as issues where selection bounds queried from
    inside the selection event callback were stale.
    
    BUG=490800
    
    Review URL: https://codereview.chromium.org/1129193007
    
    Cr-Commit-Position: refs/heads/master@{#331703}

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -2177,6 +2177,10 @@ public class ContentViewCore implements
     @CalledByNative
     private void onSelectionEvent(
             int eventType, int xAnchor, int yAnchor, int left, int top, int right, int bottom) {
+        // Ensure the provided selection coordinates form a non-empty rect, as required by
+        // the selection action mode.
+        if (left == right) ++right;
+        if (top == bottom) ++bottom;
         switch (eventType) {
             case SelectionEventType.SELECTION_SHOWN:
                 mSelectionRect.set(left, top, right, bottom);
@@ -2185,11 +2189,7 @@ public class ContentViewCore implements
                 // TODO(cjhopman): Remove this when there is a better signal that long press caused
                 // a selection. See http://crbug.com/150151.
                 mContainerView.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-                // There may already be an active selection, e.g., if the user
-                // longpresses different text while an existing selection is
-                // active. In such case, we need to update the selection rect.
                 showSelectActionMode(true);
-                invalidateActionModeContentRect();
                 break;
 
             case SelectionEventType.SELECTION_MOVED:
