69ea9833a7164c2865b726f1dc4121d8edf1a2a6
fs@opera.com <fs@opera.com>
2015-05-28 11:21:19 +0000

    Avoid resetting the metrics-list/character offset for each text box
    SVGTextLayoutEngine::layoutTextOnLineOrPath needs to keep track of the
    SVGTextMetrics corresponding to each glyph it layouts. To do this, the
    members m_visual{Character,MetricsList}Offset are used. However, this
    state is reset for each invocation, which means that finding the
    starting metrics entry can yield O(N^2) behavior. For text nodes with
    many (logically consecutive) text boxes, this can become noticeable.
    Introduce a helper SVGTextMetricsIterator - replacing the m_visual*
    members - and let it persist across calls to layoutTextOnLineOrPath.
    This speeds up the case of many logically consecutive text boxes.
    
    Using the PerfTestRunner.measurePageLoadTime harness with
    separate-x.svg from the bug give the following results locally:
    
           Before:     After:
    avg    101.5 ms -> 93.1 ms
    median 101.1 ms -> 93.1 ms
    stdev    2.8 ms ->  2.3 ms
    
    BUG=486669
    
    Review URL: https://codereview.chromium.org/1160623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196054 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/svg/SVGTextLayoutEngine.h
+++ third_party/WebKit/Source/core/layout/svg/SVGTextLayoutEngine.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SVGTextLayoutEngine_h
 #define SVGTextLayoutEngine_h
 
+#include "core/layout/svg/LayoutSVGInlineText.h"
 #include "core/layout/svg/SVGTextFragment.h"
 #include "core/layout/svg/SVGTextLayoutAttributes.h"
 #include "core/layout/svg/SVGTextMetrics.h"
@@ -31,7 +32,6 @@ namespace blink {
 
 class LayoutObject;
 class ComputedStyle;
-class LayoutSVGInlineText;
 class SVGInlineTextBox;
 
 // SVGTextLayoutEngine performs the second layout phase for SVG text.
@@ -60,17 +60,14 @@ private:
     void updateCurrentTextPosition(float x, float y, float glyphAdvance);
     void updateRelativePositionAdjustmentsIfNeeded(float dx, float dy);
 
-    void recordTextFragment(SVGInlineTextBox*, const Vector<SVGTextMetrics>&);
+    void recordTextFragment(SVGInlineTextBox*);
     bool parentDefinesTextLength(LayoutObject*) const;
 
     void layoutTextOnLineOrPath(SVGInlineTextBox*, const LayoutSVGInlineText&, const ComputedStyle&);
 
     bool currentLogicalCharacterAttributes(SVGTextLayoutAttributes*&);
     bool currentLogicalCharacterMetrics(SVGTextLayoutAttributes*&, SVGTextMetrics&);
-    bool currentVisualCharacterMetrics(SVGInlineTextBox*, const Vector<SVGTextMetrics>&, SVGTextMetrics&);
-
     void advanceToNextLogicalCharacter(const SVGTextMetrics&);
-    void advanceToNextVisualCharacter(const SVGTextMetrics&);
 
 private:
     Vector<SVGTextLayoutAttributes*>& m_layoutAttributes;
@@ -81,8 +78,7 @@ private:
     unsigned m_layoutAttributesPosition;
     unsigned m_logicalCharacterOffset;
     unsigned m_logicalMetricsListOffset;
-    unsigned m_visualCharacterOffset;
-    unsigned m_visualMetricsListOffset;
+    SVGInlineTextMetricsIterator m_visualMetricsIterator;
     float m_x;
     float m_y;
     float m_dx;
