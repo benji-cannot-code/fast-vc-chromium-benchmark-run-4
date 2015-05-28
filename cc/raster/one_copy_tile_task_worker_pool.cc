0a30faf8c2a1886bc23e10c3a273223488026245
reveman <reveman@chromium.org>
2015-05-27 18:59:43 -0700

    cc: Fix support for compressed texture formats and partial copy operations.
    It's not enought for copy operations to be at least 4 rows. They
    need to be a multiple of 4 rows.
    
    BUG=460288
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1158763002
    
    Cr-Commit-Position: refs/heads/master@{#331716}

--- cc/raster/one_copy_tile_task_worker_pool.cc
+++ cc/raster/one_copy_tile_task_worker_pool.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/trace_event/trace_event.h"
 #include "base/trace_event/trace_event_argument.h"
+#include "cc/base/util.h"
 #include "cc/debug/traced_value.h"
 #include "cc/raster/raster_buffer.h"
 #include "cc/resources/resource_pool.h"
@@ -80,10 +81,6 @@ const int kCopyFlushPeriod = 4;
 
 // Number of in-flight copy operations to allow.
 const int kMaxCopyOperations = 32;
-
-// Minimum number of rows per copy operation. 4 or greater is required to
-// support compressed texture formats.
-const size_t kMinRowsPerCopyOperation = 4;
 
 // Delay been checking for copy operations to complete.
 const int kCheckForCompletedCopyOperationsTickRateMs = 1;
@@ -309,7 +306,9 @@ OneCopyTileTaskWorkerPool::PlaybackAndScheduleCopyOnWorkerThread(
   size_t bytes_per_row =
       (BitsPerPixel(src->format()) * src->size().width()) / 8;
   size_t chunk_size_in_rows = std::max(
-      kMinRowsPerCopyOperation, max_bytes_per_copy_operation_ / bytes_per_row);
+      static_cast<size_t>(1), max_bytes_per_copy_operation_ / bytes_per_row);
+  // Align chunk size to 4. Required to support compressed texture formats.
+  chunk_size_in_rows = RoundUp(chunk_size_in_rows, static_cast<size_t>(4));
   size_t y = 0;
   size_t height = src->size().height();
   while (y < height) {
