4e7796a5a10418e173fa5e186deb069368762295
jochen@chromium.org <jochen@chromium.org>
2015-05-28 16:08:11 +0000

    Reland "Correctly set ScriptState in the image loader microtask"
    Original review: https://codereview.chromium.org/1153233002/
    
    BUG=487155
    R=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/1163543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196071 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/ImageLoader.cpp
+++ third_party/WebKit/Source/core/loader/ImageLoader.cpp
@@ -24,6 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/loader/ImageLoader.h"
 
 #include "bindings/core/v8/ScriptController.h"
+#include "bindings/core/v8/ScriptState.h"
+#include "bindings/core/v8/V8Binding.h"
+#include "bindings/core/v8/V8PerIsolateData.h"
 #include "core/dom/Document.h"
 #include "core/dom/Element.h"
 #include "core/dom/IncrementLoadEventDelayCount.h"
@@ -87,9 +90,22 @@ public:
         , m_updateBehavior(updateBehavior)
         , m_weakFactory(this)
     {
+        v8::Isolate* isolate = V8PerIsolateData::mainThreadIsolate();
+        v8::HandleScope scope(isolate);
+        v8::Local<v8::Context> context = isolate->GetCurrentContext();
+        // If we're invoked from C++ without a V8 context on the stack, we should
+        // run the microtask in the context of the element's document's main world.
+        if (context.IsEmpty())
+            m_scriptState = ScriptState::from(toV8Context(&loader->element()->document(), DOMWrapperWorld::mainWorld()));
+        else
+            m_scriptState = ScriptState::from(context);
     }
 
-    virtual void run() override
+    ~Task() override
+    {
+    }
+
+    void run() override
     {
         if (m_loader) {
 #if ENABLE(OILPAN)
@@ -104,13 +120,19 @@ public:
             if (Heap::willObjectBeLazilySwept(m_loader))
                 return;
 #endif
-            m_loader->doUpdateFromElement(m_shouldBypassMainWorldCSP, m_updateBehavior);
+            if (m_scriptState->contextIsValid()) {
+                ScriptState::Scope scope(m_scriptState.get());
+                m_loader->doUpdateFromElement(m_shouldBypassMainWorldCSP, m_updateBehavior);
+            } else {
+                m_loader->doUpdateFromElement(m_shouldBypassMainWorldCSP, m_updateBehavior);
+            }
         }
     }
 
     void clearLoader()
     {
         m_loader = 0;
+        m_scriptState.clear();
     }
 
     WeakPtr<Task> createWeakPtr()
@@ -122,6 +144,7 @@ private:
     ImageLoader* m_loader;
     BypassMainWorldBehavior m_shouldBypassMainWorldCSP;
     UpdateFromElementBehavior m_updateBehavior;
+    RefPtr<ScriptState> m_scriptState;
     WeakPtrFactory<Task> m_weakFactory;
 };
 
@@ -377,7 +400,12 @@ void ImageLoader::updateFromElement(UpdateFromElementBehavior updateBehavior)
             image->removeClient(this);
         m_image = nullptr;
     }
-    enqueueImageLoadingMicroTask(updateBehavior);
+
+    // Don't load images for inactive documents. We don't want to slow down the
+    // raw HTML parsing case by loading images we don't intend to display.
+    Document& document = m_element->document();
+    if (document.isActive())
+        enqueueImageLoadingMicroTask(updateBehavior);
 }
 
 KURL ImageLoader::imageSourceToKURL(AtomicString imageSourceURL) const
