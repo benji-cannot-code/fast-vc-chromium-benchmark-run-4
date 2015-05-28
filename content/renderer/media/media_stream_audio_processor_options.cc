be8bcd436a2bf2d1557b6701835f326296d58d8c
aluebs <aluebs@chromium.org>
2015-05-28 14:43:27 -0700

    Add a Media Constraint to specify the array geometry for Beamforming
    Beamforming gets enabled if:
    * googBeamforming is true
    AND
    {
    * It is supported device (peach_pi, swanky), in which case it uses the hardcoded geometry for each device
    OR
    * A valid googArrayGeometry is provided using space delimited coordinates. For example, googArrayGeometry=-0.026 0 0 0.026 0 0
    }
    
    Review URL: https://codereview.chromium.org/1156073005
    
    Cr-Commit-Position: refs/heads/master@{#331859}

--- content/renderer/media/media_stream_audio_processor_options.cc
+++ content/renderer/media/media_stream_audio_processor_options.cc
@@ -36,6 +36,7 @@ const char MediaAudioConstraints::kGoogNoiseSuppression[] =
 const char MediaAudioConstraints::kGoogExperimentalNoiseSuppression[] =
     "googNoiseSuppression2";
 const char MediaAudioConstraints::kGoogBeamforming[] = "googBeamforming";
+const char MediaAudioConstraints::kGoogArrayGeometry[] = "googArrayGeometry";
 const char MediaAudioConstraints::kGoogHighpassFilter[] = "googHighpassFilter";
 const char MediaAudioConstraints::kGoogTypingNoiseDetection[] =
     "googTypingNoiseDetection";
@@ -158,6 +159,13 @@ bool MediaAudioConstraints::GetProperty(const std::string& key) const {
   if (!GetConstraintValueAsBoolean(constraints_, key, &value))
     value = GetDefaultValueForConstraint(constraints_, key);
 
+  return value;
+}
+
+std::string MediaAudioConstraints::GetPropertyAsString(
+    const std::string& key) const {
+  std::string value;
+  GetConstraintValueAsString(constraints_, key, &value);
   return value;
 }
 
