99e02104a5fccd8e2207dc7d83a938c6b95717bf
tanay.c@samsung.com <tanay.c@samsung.com>
2015-05-28 22:27:41 +0000

    Replacing the 'return 0;' statements with 'return nullptr;'
    BUG=492312
    
    Review URL: https://codereview.chromium.org/1158913002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196086 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebUnitTestSupport.h
+++ third_party/WebKit/public/platform/WebUnitTestSupport.h
@@ -60,7 +60,7 @@ public:
 
     // Constructs a WebLayerTreeView set up with reasonable defaults for
     // testing.
-    virtual WebLayerTreeView* createLayerTreeViewForTesting() { return 0; }
+    virtual WebLayerTreeView* createLayerTreeViewForTesting() { return nullptr; }
 
     virtual WebData readFromFile(const WebString& path) { return WebData(); }
 
