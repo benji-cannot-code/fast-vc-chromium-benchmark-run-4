e0e8982ba008a11fea49aaf1f78b59305485eb6e
haraken@chromium.org <haraken@chromium.org>
2015-05-28 13:09:49 +0000

    Oilpan: Implement a GC to take a heap snapshot
    This CL implements Heap::collectGarbage(gcType = TakeSnapshot), which is intended
    to be used to take a heap snapshot when onMemoryDump is requested etc.
    
    The GC is expected to be as side-effect-free as possible. Thus the GC behaves as follows:
    
    1) Run a marking task.
    2) Take a heap snapshot (The actual implementation will come in a follow-up CL).
    3) Clear all marks on marked objects. No sweeping task runs.
    
    More contexts:
    https://codereview.chromium.org/1149673002/
    https://groups.google.com/a/chromium.org/d/topic/oilpan-reviews/6LhByzmVt6Q/discussion
    
    BUG=490087
    TEST=HeapTest.ThreadedWeakness
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=195982
    
    Review URL: https://codereview.chromium.org/1159773004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196059 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Heap.h
+++ third_party/WebKit/Source/platform/heap/Heap.h
@@ -387,6 +387,7 @@ public:
     virtual void removeFromHeap() = 0;
     virtual void sweep() = 0;
     virtual void makeConsistentForGC() = 0;
+    virtual void makeConsistentForMutator() = 0;
 #if defined(ADDRESS_SANITIZER)
     virtual void poisonUnmarkedObjects() = 0;
 #endif
@@ -474,6 +475,7 @@ public:
     virtual void removeFromHeap() override;
     virtual void sweep() override;
     virtual void makeConsistentForGC() override;
+    virtual void makeConsistentForMutator() override;
 #if defined(ADDRESS_SANITIZER)
     virtual void poisonUnmarkedObjects() override;
 #endif
@@ -536,6 +538,7 @@ public:
     virtual void removeFromHeap() override;
     virtual void sweep() override;
     virtual void makeConsistentForGC() override;
+    virtual void makeConsistentForMutator() override;
 #if defined(ADDRESS_SANITIZER)
     virtual void poisonUnmarkedObjects() override;
 #endif
@@ -694,6 +697,7 @@ public:
 
     virtual void clearFreeLists() { }
     void makeConsistentForGC();
+    void makeConsistentForMutator();
 #if ENABLE(ASSERT)
     virtual bool isConsistentForGC() = 0;
 #endif
