0faea7d0877bbbf60025ff38f33ae9f78bec0bb0
fsamuel <fsamuel@chromium.org>
2015-05-28 10:05:30 -0700

    <webview>: Remove Undocumented Resize DOM event
    <webview> had an undocumented resize event that we added as an experiment
    but it seems to be causing more harm than good, so let's just remove it.
    
    BUG=492648
    TBR=nasko@chromium.org for trivial browser_plugin_delegate.h change.
    
    Review URL: https://codereview.chromium.org/1155163009
    
    Cr-Commit-Position: refs/heads/master@{#331803}

--- chrome/test/data/extensions/platform_apps/web_view/shim/main.js
+++ chrome/test/data/extensions/platform_apps/web_view/shim/main.js
@@ -2538,14 +2538,6 @@ function testResizeEvents() {
     embedder.test.assertEq(e.newHeight, 400)
   }
 
-  var resizeListener = function(e) {
-    webview.onresize = null;
-    webview.oncontentresize = contentResizeListener;
-
-    console.log('onresize');
-    checkSizes(e);
-  };
-
   var contentResizeListener = function(e) {
     webview.oncontentresize = null;
 
@@ -2556,7 +2548,7 @@ function testResizeEvents() {
 
   var loadstopListener = function(e) {
     webview.removeEventListener('loadstop', loadstopListener);
-    webview.onresize = resizeListener;
+    webview.oncontentresize = contentResizeListener;
 
     console.log('Resizing <webview> width from 600px to 500px.');
     webview.style.width = '500px';
