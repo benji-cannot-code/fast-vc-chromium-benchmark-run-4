42baa0afc42e25e03ca655c8b87f64da28be7bfa
thakis <thakis@chromium.org>
2015-05-29 15:06:04 -0700

    Make testing/buildbot presubmit output less mysterious.
    BUG=98637
    
    Review URL: https://codereview.chromium.org/1162063002
    
    Cr-Commit-Position: refs/heads/master@{#332050}

--- testing/buildbot/manage.py
+++ testing/buildbot/manage.py
@@ -146,6 +146,7 @@ def main():
           print('Updated %s' % filename)
       else:
         print('%s is not in canonical format' % filename)
+        print('run `testing/buildbot/manage.py -w` to fix')
 
   if args.remaining:
     if args.test_name:
