1d746cd0a275ea78179553c6410a8a0eeb83bf8e
jsbell@chromium.org <jsbell@chromium.org>
2015-05-29 01:04:04 +0000

    IndexedDB: Rethrow exceptions generated during key conversion
    Convering script values to Indexed DB keys may throw e.g. due to
    getters on the value or a prototype. These should be rethrown when
    encountered. For arguments that take multiple keys such as
    indexedDB.cmp(a,b) the first argument should be fully processed before
    the second is processed at all.
    
    This was underspecified in the first edition of the Indexed DB spec,
    and we lacked tests so the implementation could lead to renderer
    crashes. The "second edition" draft[2] tightens this up, and this
    CL ensures we behave correctly.
    
    [2] https://w3c.github.io/IndexedDB/
    
    BUG=492909
    R=cmumford@chromium.org,haraken@chromium.org
    TEST=storage/indexeddb/key_conversion_exceptions.html
    
    Review URL: https://codereview.chromium.org/1146813007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196104 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/indexeddb/IDBKeyRange.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IDBKeyRange.cpp
@@ -38,16 +38,18 @@ namespace blink {
 IDBKeyRange* IDBKeyRange::fromScriptValue(ExecutionContext* context, const ScriptValue& value, ExceptionState& exceptionState)
 {
     if (value.isUndefined() || value.isNull())
-        return 0;
+        return nullptr;
 
     IDBKeyRange* range = ScriptValue::to<IDBKeyRange*>(toIsolate(context), value, exceptionState);
     if (range)
         return range;
 
     IDBKey* key = ScriptValue::to<IDBKey*>(toIsolate(context), value, exceptionState);
+    if (exceptionState.hadException())
+        return nullptr;
     if (!key || !key->isValid()) {
         exceptionState.throwDOMException(DataError, IDBDatabase::notValidKeyErrorMessage);
-        return 0;
+        return nullptr;
     }
 
     return new IDBKeyRange(key, key, LowerBoundClosed, UpperBoundClosed);
@@ -81,7 +83,7 @@ IDBKeyRange* IDBKeyRange::only(IDBKey* key, ExceptionState& exceptionState)
 {
     if (!key || !key->isValid()) {
         exceptionState.throwDOMException(DataError, IDBDatabase::notValidKeyErrorMessage);
-        return 0;
+        return nullptr;
     }
 
     return IDBKeyRange::create(key, key, LowerBoundClosed, UpperBoundClosed);
@@ -90,9 +92,11 @@ IDBKeyRange* IDBKeyRange::only(IDBKey* key, ExceptionState& exceptionState)
 IDBKeyRange* IDBKeyRange::only(ExecutionContext* context, const ScriptValue& keyValue, ExceptionState& exceptionState)
 {
     IDBKey* key = ScriptValue::to<IDBKey*>(toIsolate(context), keyValue, exceptionState);
+    if (exceptionState.hadException())
+        return nullptr;
     if (!key || !key->isValid()) {
         exceptionState.throwDOMException(DataError, IDBDatabase::notValidKeyErrorMessage);
-        return 0;
+        return nullptr;
     }
 
     return IDBKeyRange::create(key, key, LowerBoundClosed, UpperBoundClosed);
@@ -101,9 +105,11 @@ IDBKeyRange* IDBKeyRange::only(ExecutionContext* context, const ScriptValue& key
 IDBKeyRange* IDBKeyRange::lowerBound(ExecutionContext* context, const ScriptValue& boundValue, bool open, ExceptionState& exceptionState)
 {
     IDBKey* bound = ScriptValue::to<IDBKey*>(toIsolate(context), boundValue, exceptionState);
+    if (exceptionState.hadException())
+        return nullptr;
     if (!bound || !bound->isValid()) {
         exceptionState.throwDOMException(DataError, IDBDatabase::notValidKeyErrorMessage);
-        return 0;
+        return nullptr;
     }
 
     return IDBKeyRange::create(bound, 0, open ? LowerBoundOpen : LowerBoundClosed, UpperBoundOpen);
@@ -112,9 +118,11 @@ IDBKeyRange* IDBKeyRange::lowerBound(ExecutionContext* context, const ScriptValu
 IDBKeyRange* IDBKeyRange::upperBound(ExecutionContext* context, const ScriptValue& boundValue, bool open, ExceptionState& exceptionState)
 {
     IDBKey* bound = ScriptValue::to<IDBKey*>(toIsolate(context), boundValue, exceptionState);
+    if (exceptionState.hadException())
+        return nullptr;
     if (!bound || !bound->isValid()) {
         exceptionState.throwDOMException(DataError, IDBDatabase::notValidKeyErrorMessage);
-        return 0;
+        return nullptr;
     }
 
     return IDBKeyRange::create(0, bound, LowerBoundOpen, open ? UpperBoundOpen : UpperBoundClosed);
@@ -123,19 +131,28 @@ IDBKeyRange* IDBKeyRange::upperBound(ExecutionContext* context, const ScriptValu
 IDBKeyRange* IDBKeyRange::bound(ExecutionContext* context, const ScriptValue& lowerValue, const ScriptValue& upperValue, bool lowerOpen, bool upperOpen, ExceptionState& exceptionState)
 {
     IDBKey* lower = ScriptValue::to<IDBKey*>(toIsolate(context), lowerValue, exceptionState);
-    IDBKey* upper = ScriptValue::to<IDBKey*>(toIsolate(context), upperValue, exceptionState);
-
-    if (!lower || !lower->isValid() || !upper || !upper->isValid()) {
+    if (exceptionState.hadException())
+        return nullptr;
+    if (!lower || !lower->isValid()) {
         exceptionState.throwDOMException(DataError, IDBDatabase::notValidKeyErrorMessage);
-        return 0;
+        return nullptr;
     }
+
+    IDBKey* upper = ScriptValue::to<IDBKey*>(toIsolate(context), upperValue, exceptionState);
+    if (exceptionState.hadException())
+        return nullptr;
+    if (!upper || !upper->isValid()) {
+        exceptionState.throwDOMException(DataError, IDBDatabase::notValidKeyErrorMessage);
+        return nullptr;
+    }
+
     if (upper->isLessThan(lower)) {
         exceptionState.throwDOMException(DataError, "The lower key is greater than the upper key.");
-        return 0;
+        return nullptr;
     }
     if (upper->isEqual(lower) && (lowerOpen || upperOpen)) {
         exceptionState.throwDOMException(DataError, "The lower key and upper key are equal and one of the bounds is open.");
-        return 0;
+        return nullptr;
     }
 
     return IDBKeyRange::create(lower, upper, lowerOpen ? LowerBoundOpen : LowerBoundClosed, upperOpen ? UpperBoundOpen : UpperBoundClosed);
--- third_party/WebKit/Source/modules/indexeddb/IDBObjectStore.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IDBObjectStore.cpp
@@ -195,6 +195,8 @@ IDBRequest* IDBObjectStore::put(ScriptState* scriptState, const ScriptValue& val
 IDBRequest* IDBObjectStore::put(ScriptState* scriptState, WebIDBPutMode putMode, IDBAny* source, const ScriptValue& value, const ScriptValue& keyValue, ExceptionState& exceptionState)
 {
     IDBKey* key = keyValue.isUndefined() ? nullptr : ScriptValue::to<IDBKey*>(scriptState->isolate(), keyValue, exceptionState);
+    if (exceptionState.hadException())
+        return nullptr;
     return put(scriptState, putMode, source, value, key, exceptionState);
 }
 
@@ -242,6 +244,8 @@ IDBRequest* IDBObjectStore::put(ScriptState* scriptState, WebIDBPutMode putMode,
         if (clone.isEmpty())
             clone = deserializeScriptValue(scriptState, serializedValue.get(), &blobInfo);
         IDBKey* keyPathKey = ScriptValue::to<IDBKey*>(scriptState->isolate(), clone, exceptionState, keyPath);
+        if (exceptionState.hadException())
+            return nullptr;
         if (!keyPathKey || !keyPathKey->isEqual(key)) {
             exceptionState.throwDOMException(DataError, "The effective object store of this cursor uses in-line keys and evaluating the key path of the value parameter results in a different value than the cursor's effective key.");
             return nullptr;
@@ -256,6 +260,8 @@ IDBRequest* IDBObjectStore::put(ScriptState* scriptState, WebIDBPutMode putMode,
         if (clone.isEmpty())
             clone = deserializeScriptValue(scriptState, serializedValue.get(), &blobInfo);
         IDBKey* keyPathKey = ScriptValue::to<IDBKey*>(scriptState->isolate(), clone, exceptionState, keyPath);
+        if (exceptionState.hadException())
+            return nullptr;
         if (keyPathKey && !keyPathKey->isValid()) {
             exceptionState.throwDOMException(DataError, "Evaluating the object store's key path yielded a value that is not a valid key.");
             return 0;
