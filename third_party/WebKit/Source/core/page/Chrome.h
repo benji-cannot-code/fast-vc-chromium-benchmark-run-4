be36108546a12eba546b50ff8989d8ea77b84c18
tkent@chromium.org <tkent@chromium.org>
2015-05-29 09:38:48 +0000

    Remove blink::Chrome, part 2.
    Remove Page dependency from Chrome.
    
    BUG=493549
    
    Review URL: https://codereview.chromium.org/1156003005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196130 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/Chrome.h
+++ third_party/WebKit/Source/core/page/Chrome.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/CoreExport.h"
 #include "core/loader/NavigationPolicy.h"
+#include "core/page/ChromeClient.h"
 #include "platform/Cursor.h"
 #include "platform/heap/Handle.h"
 #include "public/platform/WebFocusType.h"
@@ -32,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class ChromeClient;
 class ColorChooser;
 class ColorChooserClient;
 class DateTimeChooser;
@@ -43,7 +43,6 @@ class HTMLInputElement;
 class HitTestResult;
 class IntRect;
 class Node;
-class Page;
 class PopupMenu;
 class PopupMenuClient;
 class PopupOpeningObserver;
@@ -56,7 +55,7 @@ class CORE_EXPORT Chrome final {
 public:
     virtual ~Chrome();
 
-    static PassOwnPtr<Chrome> create(Page*, ChromeClient*);
+    static PassOwnPtr<Chrome> create(ChromeClient*);
 
     ChromeClient& client() { return *m_client; }
 
@@ -93,10 +92,10 @@ public:
     void unregisterPopupOpeningObserver(PopupOpeningObserver*);
 
 private:
-    Chrome(Page*, ChromeClient*);
+    Chrome(ChromeClient*);
+    bool canRunModalIfDuringPageDismissal(Frame* mainFrame, ChromeClient::DialogType, const String& message);
     void notifyPopupOpeningObservers() const;
 
-    Page* m_page;
     ChromeClient* m_client;
     Vector<PopupOpeningObserver*> m_popupOpeningObservers;
     Cursor m_lastSetMouseCursorForTesting;
