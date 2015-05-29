715ea90020d7ccd576881c05db01715e7198a555
rtenneti <rtenneti@chromium.org>
2015-05-29 11:22:04 -0700

    QUIC - Track how often a user hits 1, 2, 3, 4, and 5 bad packet loss events.
    Added Net.QuicStreamFactory.BadPacketLossEvents histogram
    to track bad packet loss events.
    
    BUG=492468
    R=rch@chromium.org, asvitkine@chromium.org
    
    Review URL: https://codereview.chromium.org/1156193003
    
    Cr-Commit-Position: refs/heads/master@{#332013}

--- net/quic/quic_stream_factory.cc
+++ net/quic/quic_stream_factory.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/quic/quic_stream_factory.h"
 
+#include <algorithm>
 #include <set>
 
 #include "base/cpu.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/rand_util.h"
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
 #include "base/values.h"
 #include "net/base/net_errors.h"
 #include "net/cert/cert_verifier.h"
@@ -837,6 +839,14 @@ bool QuicStreamFactory::OnHandshakeConfirmed(QuicClientSession* session,
     UMA_HISTOGRAM_SPARSE_SLOWLY("Net.QuicStreamFactory.QuicIsDisabled", port);
   }
 
+  // Collect data for port 443 for packet loss events.
+  if (port == 443 && max_number_of_lossy_connections_ > 0) {
+    UMA_HISTOGRAM_SPARSE_SLOWLY(
+        base::StringPrintf("Net.QuicStreamFactory.BadPacketLossEvents%d",
+                           max_number_of_lossy_connections_),
+        std::min(number_of_lossy_connections_[port],
+                 max_number_of_lossy_connections_));
+  }
   return true;
 }
 
