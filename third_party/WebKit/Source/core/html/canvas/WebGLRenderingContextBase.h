29ae29228394264c83fad38ce4317efd4ef9712f
zmo@chromium.org <zmo@chromium.org>
2015-05-29 23:27:00 +0000

    Fix WebGL 2's renderbufferStorageMultisample function.
    We incorrectly called into a wrong function.
    
    BUG=485745
    TEST=webgl 2 conformance
    R=kbr@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1134753006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196176 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/WebGLRenderingContextBase.h
+++ third_party/WebKit/Source/core/html/canvas/WebGLRenderingContextBase.h
@@ -1000,6 +1000,8 @@ protected:
 
     void findNewMaxNonDefaultTextureUnit();
 
+    virtual void renderbufferStorageImpl(GLenum target, GLsizei samples, GLenum internalformat, GLsizei width, GLsizei height, const char* functionName);
+
     friend class WebGLStateRestorer;
     friend class WebGLRenderingContextEvictionManager;
 
--- third_party/WebKit/Source/platform/graphics/GraphicsTypes3D.h
+++ third_party/WebKit/Source/platform/graphics/GraphicsTypes3D.h
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 typedef unsigned Platform3DObject;
 
 // WebGL-specific enums
-const unsigned GC3D_DEPTH_STENCIL_ATTACHMENT_WEBGL = 0x821A;
 const unsigned GC3D_UNPACK_FLIP_Y_WEBGL = 0x9240;
 const unsigned GC3D_UNPACK_PREMULTIPLY_ALPHA_WEBGL = 0x9241;
 const unsigned GC3D_CONTEXT_LOST_WEBGL = 0x9242;
