2fdbcdebba89655058ea041941014abf4456f9df
msramek <msramek@chromium.org>
2015-05-29 07:56:37 -0700

    Prepare the infrastructure for password overrides.
    Currently use the autofill field categories "USERNAME" and "USERNAME_AND_EMAIL" in our password form parsing heuristics. Use the same pipeline to pass the predictions of "PASSWORD" and "ACCOUNT_CREATION_PASSWORD" as well.
    
    This is mostly refactoring. The new hints on password fields are not yet used in this CL.
    
    BUG=492604
    
    Review URL: https://codereview.chromium.org/1153023004
    
    Cr-Commit-Position: refs/heads/master@{#331968}

--- components/autofill/content/renderer/password_autofill_agent.h
+++ components/autofill/content/renderer/password_autofill_agent.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "components/autofill/core/common/form_data_predictions.h"
+#include "components/autofill/core/common/password_form_field_prediction_map.h"
 #include "components/autofill/core/common/password_form_fill_data.h"
 #include "content/public/renderer/render_frame_observer.h"
 #include "content/public/renderer/render_view_observer.h"
@@ -108,8 +109,8 @@ class PasswordAutofillAgent : public content::RenderFrameObserver {
   typedef std::map<blink::WebElement, int> LoginToPasswordInfoKeyMap;
   typedef std::map<blink::WebInputElement, blink::WebInputElement>
       PasswordToLoginMap;
-  using FormDataFieldDataMap =
-      std::map<autofill::FormData, autofill::FormFieldData>;
+  using FormsPredictionsMap =
+      std::map<autofill::FormData, autofill::PasswordFormFieldPredictionMap>;
 
   // This class keeps track of autofilled password input elements and makes sure
   // the autofilled password value is not accessible to JavaScript code until
@@ -181,7 +182,8 @@ class PasswordAutofillAgent : public content::RenderFrameObserver {
   // RenderView IPC handlers:
   void OnFillPasswordForm(int key, const PasswordFormFillData& form_data);
   void OnSetLoggingState(bool active);
-  void OnAutofillUsernameDataReceived(const FormDataFieldDataMap& predictions);
+  void OnAutofillUsernameAndPasswordDataReceived(
+      const FormsPredictionsMap& predictions);
 
   // Scans the given frame for password forms and sends them up to the browser.
   // If |only_visible| is true, only forms visible in the layout are sent.
@@ -283,9 +285,9 @@ class PasswordAutofillAgent : public content::RenderFrameObserver {
   // save password prompt on in-page navigations.
   bool save_password_on_in_page_navigation_;
 
-  // Contains server predictions for which field is the username field for forms
-  // on the page.
-  FormDataFieldDataMap form_predictions_;
+  // Contains server predictions for username, password and/or new password
+  // fields for individual forms.
+  FormsPredictionsMap form_predictions_;
 
   base::WeakPtrFactory<PasswordAutofillAgent> weak_ptr_factory_;
 
--- components/password_manager/content/browser/content_password_manager_driver.h
+++ components/password_manager/content/browser/content_password_manager_driver.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "components/autofill/core/common/password_form_field_prediction_map.h"
 #include "components/password_manager/core/browser/password_autofill_manager.h"
 #include "components/password_manager/core/browser/password_generation_manager.h"
 #include "components/password_manager/core/browser/password_manager.h"
@@ -52,7 +53,8 @@ class ContentPasswordManagerDriver : public PasswordManagerDriver {
   void AccountCreationFormsFound(
       const std::vector<autofill::FormData>& forms) override;
   void AutofillDataReceived(
-      const std::map<autofill::FormData, autofill::FormFieldData>& predictions)
+      const std::map<autofill::FormData,
+                     autofill::PasswordFormFieldPredictionMap>& predictions)
       override;
   void GeneratedPasswordAccepted(const base::string16& password) override;
   void FillSuggestion(const base::string16& username,
