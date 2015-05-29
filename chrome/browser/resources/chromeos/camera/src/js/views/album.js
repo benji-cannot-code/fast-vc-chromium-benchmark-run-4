2434981826f1ea80110e0400c1a91adb5f427f98
Tomasz Mikolajewski <mtomasz@google.com>
2015-05-29 16:15:04 +0900

    Close the gallery when the last picture is removed.
    TEST=Tested manually by removing the last picture in both views: album and
         browser.
    BUG=chromium:493112
    
    Change-Id: I2dc294ffea66e14945da43185224907e2808ca06
    Reviewed-on: https://chromium-review.googlesource.com/273991
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Tested-by: Tomasz Mikolajewski <mtomasz@chromium.org>

--- chrome/browser/resources/chromeos/camera/src/js/views/album.js
+++ chrome/browser/resources/chromeos/camera/src/js/views/album.js
@@ -171,6 +171,20 @@ camera.views.Album.prototype.updateButtons_ = function() {
 };
 
 /**
+ * Opens the current picture in the browser view.
+ * @private
+ */
+camera.views.Album.prototype.openCurrentInBrowser_ = function() {
+  this.router.navigate(
+      camera.Router.ViewIdentifier.BROWSER,
+      undefined /* opt_arguments */,
+      function() {
+        if (this.entered && !this.currentPicture())
+          this.router.back();
+      }.bind(this));
+};
+
+/**
  * @override
  */
 camera.views.Album.prototype.onCurrentIndexChanged = function(
@@ -239,7 +253,7 @@ camera.views.Album.prototype.onKeyPressed = function(event) {
         return;
       }
       if (currentPicture)
-        this.router.navigate(camera.Router.ViewIdentifier.BROWSER);
+        this.openCurrentInBrowser_();
       event.preventDefault();
       return;
     case 'Ctrl-U+0053':  // Ctrl+S for saving.
@@ -291,7 +305,7 @@ camera.views.Album.prototype.addPictureToDOM = function(picture) {
   }.bind(this));
 
   img.addEventListener('dblclick', function() {
-    this.router.navigate(camera.Router.ViewIdentifier.BROWSER);
+    this.openCurrentInBrowser_();
   }.bind(this));
 };
 
