618d7a3fa5a40c856ac40bff4b83654494eaa7fb
jww <jww@chromium.org>
2015-05-29 11:23:19 -0700

    Make one week certificate memory the default.
    This CL changes the default setting for certificate memory so that
    instead of remembering a user's decision to proceed through a bad
    certificate until session restart, it will be remembered for one week
    (including across session restarts). Notably, this CL does not remove
    the flags or current experiment; it just makes one week memory the
    default.
    
    BUG=487270
    
    Review URL: https://codereview.chromium.org/1150083004
    
    Cr-Commit-Position: refs/heads/master@{#332015}

--- chrome/browser/ssl/chrome_ssl_host_state_delegate.cc
+++ chrome/browser/ssl/chrome_ssl_host_state_delegate.cc
@@ -37,6 +37,10 @@ namespace {
 // the end of the current session.
 const int64 kForgetAtSessionEndSwitchValue = -1;
 
+// The default expiration is one week, unless overidden by an experiment group
+// or a flag. See https://crbug.com/487270.
+const uint64_t kDeltaDefaultExpirationInSeconds = UINT64_C(604800);
+
 // Experiment information
 const char kRememberCertificateErrorDecisionsFieldTrialName[] =
     "RememberCertificateErrorDecisions";
@@ -92,7 +96,7 @@ int64 GetExpirationDelta() {
         expiration_delta < kForgetAtSessionEndSwitchValue) {
       LOG(ERROR) << "Failed to parse the certificate error decision "
                  << "memory length: " << switch_value;
-      return kForgetAtSessionEndSwitchValue;
+      return kDeltaDefaultExpirationInSeconds;
     }
 
     return expiration_delta;
@@ -116,7 +120,7 @@ int64 GetExpirationDelta() {
     }
   }
 
-  return kForgetAtSessionEndSwitchValue;
+  return kDeltaDefaultExpirationInSeconds;
 }
 
 std::string GetKey(const net::X509Certificate& cert, net::CertStatus error) {
