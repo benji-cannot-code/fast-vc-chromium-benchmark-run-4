c5d6864fcd145a096c5100060167014d25afd9e4
haraken@chromium.org <haraken@chromium.org>
2015-05-29 03:35:44 +0000

    Oilpan: Add isInGC checks to pushXXXCallback methods
    They shouldn't be called outside a GC.
    This is a preparation for landing https://codereview.chromium.org/1159773004/.
    
    BUG=490087
    
    Review URL: https://codereview.chromium.org/1151883003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196116 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Heap.cpp
+++ third_party/WebKit/Source/platform/heap/Heap.cpp
@@ -1761,6 +1761,8 @@ String Heap::createBacktraceString()
 
 void Heap::pushTraceCallback(void* object, TraceCallback callback)
 {
+    ASSERT(ThreadState::current()->isInGC());
+
     // Trace should never reach an orphaned page.
     ASSERT(!Heap::orphanedPagePool()->contains(object));
     CallbackStack::Item* slot = s_markingStack->allocateEntry();
@@ -1782,6 +1784,8 @@ bool Heap::popAndInvokeTraceCallback(Visitor* visitor)
 
 void Heap::pushPostMarkingCallback(void* object, TraceCallback callback)
 {
+    ASSERT(ThreadState::current()->isInGC());
+
     // Trace should never reach an orphaned page.
     ASSERT(!Heap::orphanedPagePool()->contains(object));
     CallbackStack::Item* slot = s_postMarkingCallbackStack->allocateEntry();
@@ -1799,6 +1803,8 @@ bool Heap::popAndInvokePostMarkingCallback(Visitor* visitor)
 
 void Heap::pushGlobalWeakCallback(void** cell, WeakCallback callback)
 {
+    ASSERT(ThreadState::current()->isInGC());
+
     // Trace should never reach an orphaned page.
     ASSERT(!Heap::orphanedPagePool()->contains(cell));
     CallbackStack::Item* slot = s_globalWeakCallbackStack->allocateEntry();
@@ -1807,6 +1813,8 @@ void Heap::pushGlobalWeakCallback(void** cell, WeakCallback callback)
 
 void Heap::pushThreadLocalWeakCallback(void* closure, void* object, WeakCallback callback)
 {
+    ASSERT(ThreadState::current()->isInGC());
+
     // Trace should never reach an orphaned page.
     ASSERT(!Heap::orphanedPagePool()->contains(object));
     ThreadState* state = pageFromObject(object)->heap()->threadState();
@@ -1824,6 +1832,8 @@ bool Heap::popAndInvokeGlobalWeakCallback(Visitor* visitor)
 
 void Heap::registerWeakTable(void* table, EphemeronCallback iterationCallback, EphemeronCallback iterationDoneCallback)
 {
+    ASSERT(ThreadState::current()->isInGC());
+
     // Trace should never reach an orphaned page.
     ASSERT(!Heap::orphanedPagePool()->contains(table));
     CallbackStack::Item* slot = s_ephemeronStack->allocateEntry();
