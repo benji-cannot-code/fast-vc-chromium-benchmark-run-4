9b4e0816cd92243315f06e0c28d21fa9e5c51356
aruslan <aruslan@chromium.org>
2015-05-29 15:42:56 -0700

    Revert of Ignore account management actions after onSaveInstanceState (patchset #1 id:1 of https://codereview.chromium.org/1144113005/)
    Reason for revert:
    Nopes, didn't help, but made things worse: running another activity renders the dialog fragment unusable.
    
    Original issue's description:
    > Ignore account management actions after onSaveInstanceState
    >
    > BUG=490629
    >
    > Committed: https://crrev.com/01ef47a29ff30d2edf7e46fe70000cf5a54a4f2b
    > Cr-Commit-Position: refs/heads/master@{#331909}
    
    TBR=newt@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=490629
    
    Review URL: https://codereview.chromium.org/1143933005
    
    Cr-Commit-Position: refs/heads/master@{#332067}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/AccountManagementFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/AccountManagementFragment.java
@@ -119,8 +119,6 @@ public class AccountManagementFragment extends PreferenceFragment
     private ArrayList<Preference> mAccountsListPreferences = new ArrayList<Preference>();
     private Preference mPrimaryAccountPreference;
 
-    private boolean mSaveInstanceStateWasCalled;
-
     private AccountManagementFragmentDelegate getDelegate() {
         return AccountManagementScreenHelper.getDelegate();
     }
@@ -158,12 +156,6 @@ public class AccountManagementFragment extends PreferenceFragment
         SigninManager.get(getActivity()).removeSignInStateObserver(this);
         ProfileDownloader.removeObserver(this);
         ProfileSyncService.get(getActivity()).removeSyncStateChangedListener(this);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mSaveInstanceStateWasCalled = true;
     }
 
     /**
@@ -228,7 +220,6 @@ public class AccountManagementFragment extends PreferenceFragment
                 @Override
                 public boolean onPreferenceChange(Preference preference, Object newValue) {
                     if ((boolean) newValue) return true;
-                    if (mSaveInstanceStateWasCalled) return false;
 
                     if (ChromeSigninController.get(getActivity()).isSignedIn()
                             && getSignOutAllowedPreferenceValue(getActivity())) {
@@ -262,8 +253,6 @@ public class AccountManagementFragment extends PreferenceFragment
             addAccount.setOnPreferenceClickListener(new OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    if (mSaveInstanceStateWasCalled) return false;
-
                     AccountManagementScreenHelper.logEvent(
                             ProfileAccountManagementMetrics.ADD_ACCOUNT,
                             mGaiaServiceType);
@@ -296,7 +285,6 @@ public class AccountManagementFragment extends PreferenceFragment
             goIncognito.setOnPreferenceClickListener(new OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    if (mSaveInstanceStateWasCalled) return false;
                     if (!PrefServiceBridge.getInstance().isIncognitoModeEnabled()) return false;
 
                     AccountManagementFragmentDelegate delegate = getDelegate();
@@ -402,8 +390,6 @@ public class AccountManagementFragment extends PreferenceFragment
                 pref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
                     @Override
                     public boolean onPreferenceClick(Preference preference) {
-                        if (mSaveInstanceStateWasCalled) return false;
-
                         AccountManagementScreenHelper.logEvent(
                                 ProfileAccountManagementMetrics.CLICK_PRIMARY_ACCOUNT,
                                 mGaiaServiceType);
