0afb0f004dcbf9c2d9253b203e342ae3b4b4d5fa
dewittj <dewittj@chromium.org>
2015-05-29 11:22:21 -0700

    Notifications: Correctly mask the app icon on OSX.
    Formerly the masking code was implemented in Views, meaning that the
    icon was passed directly to cocoa as-is.  Now the extension API code
    does the masking itself.
    
    BUG=481448
    
    Review URL: https://codereview.chromium.org/1162643003
    
    Cr-Commit-Position: refs/heads/master@{#332014}

--- chrome/browser/extensions/api/notifications/notifications_api.cc
+++ chrome/browser/extensions/api/notifications/notifications_api.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/layout.h"
 #include "ui/gfx/image/image.h"
 #include "ui/gfx/image/image_skia.h"
+#include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/image/image_skia_rep.h"
 #include "ui/message_center/message_center_style.h"
 #include "ui/message_center/notifier_settings.h"
@@ -72,6 +73,33 @@ std::string StripScopeFromIdentifier(const std::string& extension_id,
   DCHECK_LT(index_of_separator, id.length());
 
   return id.substr(index_of_separator);
+}
+
+const gfx::ImageSkia CreateSolidColorImage(int width,
+                                           int height,
+                                           SkColor color) {
+  SkBitmap bitmap;
+  bitmap.allocN32Pixels(width, height);
+  bitmap.eraseColor(color);
+  return gfx::ImageSkia::CreateFrom1xBitmap(bitmap);
+}
+
+// Take the alpha channel of small_image, mask it with the foreground,
+// then add the masked foreground on top of the background
+const gfx::Image GetMaskedSmallImage(const gfx::ImageSkia& small_image) {
+  int width = small_image.width();
+  int height = small_image.height();
+
+  // Background color grey
+  const gfx::ImageSkia background = CreateSolidColorImage(
+      width, height, message_center::kSmallImageMaskBackgroundColor);
+  // Foreground color white
+  const gfx::ImageSkia foreground = CreateSolidColorImage(
+      width, height, message_center::kSmallImageMaskForegroundColor);
+  const gfx::ImageSkia masked_small_image =
+      gfx::ImageSkiaOperations::CreateMaskedImage(foreground, small_image);
+  return gfx::Image(gfx::ImageSkiaOperations::CreateSuperimposedImage(
+      background, masked_small_image));
 }
 
 class NotificationsApiDelegate : public NotificationDelegate,
@@ -222,14 +250,15 @@ bool NotificationsApiFunction::CreateNotification(
   // Then, handle any optional data that's been provided.
   message_center::RichNotificationData optional_fields;
   if (options->app_icon_mask_url.get()) {
+    gfx::Image small_icon_mask;
     if (!NotificationConversionHelper::NotificationBitmapToGfxImage(
-            image_scale,
-            bitmap_sizes.app_icon_mask_size,
-            options->app_icon_mask_bitmap.get(),
-            &optional_fields.small_image)) {
+            image_scale, bitmap_sizes.app_icon_mask_size,
+            options->app_icon_mask_bitmap.get(), &small_icon_mask)) {
       SetError(kUnableToDecodeIconError);
       return false;
     }
+    optional_fields.small_image =
+        GetMaskedSmallImage(small_icon_mask.AsImageSkia());
   }
 
   if (options->priority.get())
@@ -356,7 +385,8 @@ bool NotificationsApiFunction::UpdateNotification(
           bitmap_sizes.app_icon_mask_size,
           options->app_icon_mask_bitmap.get(),
           &app_icon_mask)) {
-    notification->set_small_image(app_icon_mask);
+    notification->set_small_image(
+        GetMaskedSmallImage(app_icon_mask.AsImageSkia()));
   }
 
   if (options->priority)
