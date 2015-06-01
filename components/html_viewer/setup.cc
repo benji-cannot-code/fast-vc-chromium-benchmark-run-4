db1e807356beaf7c5d6a8c2d2036db03c3766396
skyostil <skyostil@chromium.org>
2015-06-01 11:16:45 -0700

    mojo: Fix Blink's shutdown sequence in AxProviderImplTest
    This patch ensures the sequence used to tear down Blink in
    AxProviderImplTest matches the real implementation in RenderThreadImpl.
    Specifically, we need to shut down the scheduler before terminating
    Blink, because otherwise the scheduler might keep pointers to tasks
    that live on the Blink heap and access freed memory in its destructor.
    
    We also add an assertion to the renderer scheduler that it is shut down
    explicitly.
    
    Note that Blink cannot shut down the scheduler internally because of
    crbug.com/467369. We expect that this depedency can be cleaned up once
    the message loop is better integrated with the scheduler
    (crbug.com/465354) and when the Blink repository has been merged into
    Chromium.
    
    BUG=463143
    
    Review URL: https://codereview.chromium.org/1145973011
    
    Cr-Commit-Position: refs/heads/master@{#332221}

--- components/html_viewer/setup.cc
+++ components/html_viewer/setup.cc
@@ -74,6 +74,10 @@ Setup::Setup(mojo::ApplicationImpl* app)
 }
 
 Setup::~Setup() {
+  if (blink_platform_) {
+    renderer_scheduler_->Shutdown();
+    blink::shutdown();
+  }
 }
 
 void Setup::InitHeadless() {
