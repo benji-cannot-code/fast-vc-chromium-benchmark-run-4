05bc29d0d38908d21e49a96f7906361c946bfb9f
jennyz <jennyz@chromium.org>
2015-06-01 13:12:18 -0700

    Center the focus of the text input in magnifier screen when this feature is enabled in settings for magnifier mode.
    BUG=446270
    
    Review URL: https://codereview.chromium.org/1157553003
    
    Cr-Commit-Position: refs/heads/master@{#332254}

--- chrome/browser/resources/options/browser_options.js
+++ chrome/browser/resources/options/browser_options.js
@@ -668,6 +668,14 @@ cr.define('options', function() {
             updateAccessibilitySettingsButton;
         updateAccessibilitySettingsButton();
 
+        var updateScreenMagnifierCenterFocus = function() {
+          $('accessibility-screen-magnifier-center-focus-check').disabled =
+              !$('accessibility-screen-magnifier-check').checked;
+        };
+        Preferences.getInstance().addEventListener(
+            $('accessibility-screen-magnifier-check').getAttribute('pref'),
+            updateScreenMagnifierCenterFocus);
+
         var updateDelayDropdown = function() {
           $('accessibility-autoclick-dropdown').disabled =
               !$('accessibility-autoclick-check').checked;
