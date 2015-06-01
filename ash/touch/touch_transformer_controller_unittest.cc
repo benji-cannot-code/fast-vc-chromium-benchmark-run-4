a791665adee8b4fe23afe1cc4ae99eb94d7d9c20
kpschoedel <kpschoedel@chromium.org>
2015-06-01 10:43:07 -0700

    Remove unnecessary #include.
    Review URL: https://codereview.chromium.org/1157053003
    
    Cr-Commit-Position: refs/heads/master@{#332211}

--- ash/touch/touch_transformer_controller_unittest.cc
+++ ash/touch/touch_transformer_controller_unittest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/ash_test_base.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/events/devices/device_data_manager.h"
-#include "ui/events/test/events_test_utils_x11.h"
 #include "ui/gfx/display.h"
 
 namespace ash {
