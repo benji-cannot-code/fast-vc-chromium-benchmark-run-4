6cd1c6ff3ba33fda896695e35cfdeab0ef60e405
tbansal <tbansal@chromium.org>
2015-06-01 13:24:18 -0700

    Fix two NetworkQualityEstimator tests that were failing on Windows.
    Current guess is that timer on Windows is low resolution
    (~16 msec).
    This can cause request.creation_time to be equal to
     last_connection_change_. This was causing following
    2 tests to fail on Windows:
    (1) NetworkQualityEstimatorTest.TestPeakKbpsFastestRTTUpdates
    (2) URLRequestTestHTTP.NetworkQualityEstimator
    
    If the guess is correct, these tests could potentially fail
    on other platforms too.
    
    Trying a fix which makes this check less stringent. Now,
     if request creation time is exactly equal to time of last
    connection change, the request is still used to
    estimate the network quality (unlike before, when the
    former was required to be strictly larger than the latter).
    
    BUG=492410
    
    Review URL: https://codereview.chromium.org/1153023003
    
    Cr-Commit-Position: refs/heads/master@{#332259}

--- net/base/network_quality_estimator_unittest.cc
+++ net/base/network_quality_estimator_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/network_quality_estimator.h"
 
 #include "base/basictypes.h"
+#include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/run_loop.h"
 #include "base/test/histogram_tester.h"
@@ -23,9 +24,7 @@ namespace net {
 
 // SpawnedTestServer not supported on iOS (see http://crbug.com/148666).
 #if !defined(OS_IOS)
-// TODO(tbansal): Check why this local server test is failing on Windows
-// (see http://crbug.com/492410).
-TEST(NetworkQualityEstimatorTest, DISABLED_TestPeakKbpsFastestRTTUpdates) {
+TEST(NetworkQualityEstimatorTest, TestPeakKbpsFastestRTTUpdates) {
   SpawnedTestServer test_server_(
       SpawnedTestServer::TYPE_HTTP, SpawnedTestServer::kLocalhost,
       base::FilePath(FILE_PATH_LITERAL("net/data/url_request_unittest")));
