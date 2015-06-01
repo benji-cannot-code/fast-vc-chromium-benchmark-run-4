257d36cc62512cdb780250e9c6a4ce3abf1157e7
asvitkine <asvitkine@chromium.org>
2015-06-01 14:23:16 -0700

    Delete code corresponding to old UMA prefs.
    Sufficient time has passed that the vast majority
    of users have migrated.
    
    BUG=357704
    
    Review URL: https://codereview.chromium.org/1161133004
    
    Cr-Commit-Position: refs/heads/master@{#332268}

--- chromecast/browser/metrics/cast_metrics_service_client.cc
+++ chromecast/browser/metrics/cast_metrics_service_client.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/common/pref_names.h"
 #include "components/metrics/client_info.h"
 #include "components/metrics/gpu/gpu_metrics_provider.h"
-#include "components/metrics/metrics_pref_names.h"
 #include "components/metrics/metrics_provider.h"
 #include "components/metrics/metrics_service.h"
 #include "components/metrics/metrics_state_manager.h"
@@ -34,7 +33,11 @@ namespace chromecast {
 namespace metrics {
 
 namespace {
+
 const int kStandardUploadIntervalMinutes = 5;
+
+const char kMetricsOldClientID[] = "user_experience_metrics.client_id";
+
 }  // namespace
 
 // static
@@ -73,8 +76,7 @@ scoped_ptr< ::metrics::ClientInfo> CastMetricsServiceClient::LoadClientInfo() {
   if (!pref_service_->GetBoolean(prefs::kMetricsIsNewClientID)) {
     // If the old client id exists, the device must be on pre-v1.2 build,
     // instead of just being FDR'ed.
-    if (!pref_service_->GetString(::metrics::prefs::kMetricsOldClientID)
-        .empty()) {
+    if (!pref_service_->GetString(kMetricsOldClientID).empty()) {
       // Force old client id to be regenerated. See b/9487011.
       client_info->client_id = base::GenerateGUID();
       pref_service_->SetBoolean(prefs::kMetricsIsNewClientID, true);
