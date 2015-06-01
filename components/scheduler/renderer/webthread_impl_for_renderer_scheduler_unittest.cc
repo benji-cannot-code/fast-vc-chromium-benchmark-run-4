ed573517780df7401b3a079ec7bdea73ba5bc360
skyostil <skyostil@chromium.org>
2015-06-01 04:53:38 -0700

    scheduler: Refactor unit tests and shut down the scheduler in tests
    This patch refactors some of the scheduler unit tests to remove
    duplicated code and ensures that the renderer scheduler is shut down
    after each test. A future patch will add a DCHECK that requires the
    scheduler to be shut down prior to deletion to guard against stale
    pointers to the Blink heap.
    
    BUG=463143
    
    Review URL: https://codereview.chromium.org/1165703003
    
    Cr-Commit-Position: refs/heads/master@{#332179}

--- components/scheduler/renderer/webthread_impl_for_renderer_scheduler_unittest.cc
+++ components/scheduler/renderer/webthread_impl_for_renderer_scheduler_unittest.cc
@@ -43,6 +43,8 @@ class WebThreadImplForRendererSchedulerTest : public testing::Test {
         work_batch_size);
   }
 
+  void TearDown() override { scheduler_.Shutdown(); }
+
  protected:
   void EatDefaultTask(MockTaskObserver* observer) {
     // The scheduler posts one extra DoWork() task automatically.
