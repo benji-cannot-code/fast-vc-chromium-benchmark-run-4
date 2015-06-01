18e9a49352a7f770b75808d39316e30df3002f90
iclelland <iclelland@chromium.org>
2015-06-01 06:06:25 -0700

    [BackgroundSync] Implement GetPermissionStatus in background sync provider
    The request is passed down to the BackgroundSyncService, which implements a stub
    policy (grant for one-shot; prompt for periodic) which will have to be fleshed
    out more fully before shipping.
    
    This CL depends on the corresponding Blink CL in
    https://codereview.chromium.org/1160663003/
    
    BUG=479708
    
    Review URL: https://codereview.chromium.org/1149833005
    
    Cr-Commit-Position: refs/heads/master@{#332183}

--- content/child/background_sync/background_sync_provider.h
+++ content/child/background_sync/background_sync_provider.h
@@ -48,6 +48,10 @@ class BackgroundSyncProvider : public blink::WebSyncProvider {
       blink::WebSyncRegistration::Periodicity periodicity,
       blink::WebServiceWorkerRegistration* service_worker_registration,
       blink::WebSyncGetRegistrationsCallbacks* callbacks);
+  void getPermissionStatus(
+      blink::WebSyncRegistration::Periodicity periodicity,
+      blink::WebServiceWorkerRegistration* service_worker_registration,
+      blink::WebSyncGetPermissionStatusCallbacks* callbacks);
 
  private:
   // Callback handlers
@@ -66,6 +70,10 @@ class BackgroundSyncProvider : public blink::WebSyncProvider {
       scoped_ptr<blink::WebSyncGetRegistrationsCallbacks> callbacks,
       BackgroundSyncError error,
       const mojo::Array<SyncRegistrationPtr>& registrations);
+  void GetPermissionStatusCallback(
+      scoped_ptr<blink::WebSyncGetPermissionStatusCallbacks> callbacks,
+      BackgroundSyncError error,
+      PermissionStatus status);
 
   // Helper method that returns an initialized BackgroundSyncServicePtr.
   BackgroundSyncServicePtr& GetBackgroundSyncServicePtr();
