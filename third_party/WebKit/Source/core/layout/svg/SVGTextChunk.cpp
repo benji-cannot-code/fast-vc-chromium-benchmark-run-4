1d80daa3b4e8df416af4e4c8b84f7bb2039d900d
fs@opera.com <fs@opera.com>
2015-06-01 14:59:06 +0000

    Drop SVGTextChunk::m_boxes
    Since text chunks are now processed as they are formed, the m_boxes in
    SVGTextChunk is of little use, and just means an additional copy is
    needed. Drop it and use the range passed into handleTextChunk directly
    instead.
    This requires moving SVGTextChunk::calculateLength to
    SVGTextChunkBuilder.cpp, so do that and put it in a helper class.
    Also changing from passing <vector,startIndex,endIndex> to just passing
    two (vector) iterators describing the start and end in the vector.
    In SVGTextChunkBuilder::handleTextChunk, drop a redundant test
    (!boxCount), and move the code that recalculates the chunk extent after
    applying spacing adjustments for 'textLength' up into the relevant
    branch.
    
    BUG=486669
    
    Review URL: https://codereview.chromium.org/1162653002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196229 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/svg/SVGTextChunk.cpp
+++ third_party/WebKit/Source/core/layout/svg/SVGTextChunk.cpp
@@ -19,57 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-
 #include "core/layout/svg/SVGTextChunk.h"
 
-#include "core/layout/svg/SVGTextFragment.h"
-#include "core/layout/svg/line/SVGInlineTextBox.h"
-
 namespace blink {
-
-SVGTextChunk::SVGTextChunk(unsigned chunkStyle, float desiredTextLength)
-    : m_chunkStyle(chunkStyle)
-    , m_desiredTextLength(desiredTextLength)
-{
-}
-
-void SVGTextChunk::calculateLength(float& length, unsigned& characters) const
-{
-    SVGTextFragment* lastFragment = 0;
-
-    unsigned boxCount = m_boxes.size();
-    for (unsigned boxPosition = 0; boxPosition < boxCount; ++boxPosition) {
-        SVGInlineTextBox* textBox = m_boxes.at(boxPosition);
-        Vector<SVGTextFragment>& fragments = textBox->textFragments();
-
-        unsigned size = fragments.size();
-        if (!size)
-            continue;
-
-        for (unsigned i = 0; i < size; ++i) {
-            SVGTextFragment& fragment = fragments.at(i);
-            characters += fragment.length;
-
-            if (m_chunkStyle & VerticalText)
-                length += fragment.height;
-            else
-                length += fragment.width;
-
-            if (!lastFragment) {
-                lastFragment = &fragment;
-                continue;
-            }
-
-            // Resepect gap between chunks.
-            if (m_chunkStyle & VerticalText)
-                length += fragment.y - (lastFragment->y + lastFragment->height);
-            else
-                length += fragment.x - (lastFragment->x + lastFragment->width);
-
-            lastFragment = &fragment;
-        }
-    }
-}
 
 float SVGTextChunk::calculateTextAnchorShift(float length) const
 {
