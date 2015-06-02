723be11cdabc6d1d6c31bc616594d24e9d85d88b
justincohen <justincohen@chromium.org>
2015-06-02 16:11:35 -0700

    Reland "Roll DEPS to class-dump 3.5"
    Removes some build warnings.
    
    Moves the class-dump repo to a github auto-mirror instead of a mirror
    that needs to be manually updated.
    
    This also requires that the class-dump.gyp file move from the
    third-party repo into the main source tree. Note that the file
    structure of the class-dump source changed so the .gyp had to be
    updated. For comparison, the old .class-dump.gyp can be found at
    https://chromium.googlesource.com/chromium/deps/class-dump/+/master/class-dump.gyp
    
    BUG=318160, 417264
    
    Review URL: https://codereview.chromium.org/1161813003
    
    Cr-Commit-Position: refs/heads/master@{#332485}

--- testing/iossim/iossim.mm
+++ testing/iossim/iossim.mm
@@ -29,15 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "DVTFoundation.h"
 #endif  // IOSSIM_USE_XCODE_6
 
-@protocol OS_dispatch_queue
-@end
-@protocol OS_dispatch_source
-@end
 // TODO(lliabraa): Once all builders are on Xcode 6 this ifdef can be removed
 // (crbug.com/385030).
 #if defined(IOSSIM_USE_XCODE_6)
-@protocol OS_xpc_object
-@end
 @protocol SimBridge;
 @class SimDeviceSet;
 @class SimDeviceType;
