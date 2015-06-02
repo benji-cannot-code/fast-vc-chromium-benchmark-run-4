22464660dcc14d5633f0229f88776bc0915003c2
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2015-06-02 01:59:28 +0000

    Don't check under invalidation for delayed invalidated object
    We delay invalidation for out-of-view animated images etc, but we
    may paint them before they are really invalidated.
    
    BUG=450725
    
    Review URL: https://codereview.chromium.org/1152703005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196274 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/LayoutObjectDrawingRecorder.h
+++ third_party/WebKit/Source/core/paint/LayoutObjectDrawingRecorder.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef LayoutObjectDrawingRecorder_h
 #define LayoutObjectDrawingRecorder_h
 
+#include "core/layout/LayoutObject.h"
 #include "core/paint/PaintPhase.h"
 #include "platform/geometry/LayoutRect.h"
 #include "platform/graphics/paint/DrawingRecorder.h"
@@ -13,13 +14,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class GraphicsContext;
-class LayoutObject;
 
 // Convenience constructors for creating DrawingRecorders.
 class LayoutObjectDrawingRecorder final : public DrawingRecorder {
 public:
     LayoutObjectDrawingRecorder(GraphicsContext& context, const LayoutObject& layoutObject, DisplayItem::Type displayItemType, const LayoutRect& clip)
-        : DrawingRecorder(context, layoutObject, displayItemType, pixelSnappedIntRect(clip)) { }
+        : DrawingRecorder(context, layoutObject, displayItemType, pixelSnappedIntRect(clip))
+    {
+#if ENABLE(ASSERT)
+        if (layoutObject.fullPaintInvalidationReason() == PaintInvalidationDelayedFull)
+            setUnderInvalidationCheckingMode(DrawingDisplayItem::DontCheck);
+#endif
+    }
 
     LayoutObjectDrawingRecorder(GraphicsContext& context, const LayoutObject& layoutObject, PaintPhase phase, const FloatRect& clip)
         : DrawingRecorder(context, layoutObject, DisplayItem::paintPhaseToDrawingType(phase), clip) { }
