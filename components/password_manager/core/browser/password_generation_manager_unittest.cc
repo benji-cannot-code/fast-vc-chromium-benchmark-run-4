2f1774c195010dcc37c1e96855e22da569fded6d
skyostil <skyostil@chromium.org>
2015-06-02 02:43:38 -0700

    Add message loops to password manager tests
    These tests are using functionality that will soon start to require an
    active task runner on the main thread.
    
    BUG=465354
    
    Review URL: https://codereview.chromium.org/1162253003
    
    Cr-Commit-Position: refs/heads/master@{#332362}

--- components/password_manager/core/browser/password_generation_manager_unittest.cc
+++ components/password_manager/core/browser/password_generation_manager_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/message_loop/message_loop.h"
 #include "base/prefs/pref_registry_simple.h"
 #include "base/prefs/pref_service.h"
 #include "base/prefs/testing_pref_service.h"
@@ -118,6 +119,7 @@ class PasswordGenerationManagerTest : public testing::Test {
     GetGenerationManager()->DetectAccountCreationForms(forms);
   }
 
+  base::MessageLoop message_loop_;
   scoped_ptr<MockPasswordManagerClient> client_;
 };
 
--- components/password_manager/core/browser/password_manager_unittest.cc
+++ components/password_manager/core/browser/password_manager_unittest.cc
@@ -227,6 +227,7 @@ class PasswordManagerTest : public testing::Test {
     submitted_form_ = form;
   }
 
+  base::MessageLoop message_loop_;
   scoped_refptr<MockPasswordStore> store_;
   MockPasswordManagerClient client_;
   MockPasswordManagerDriver driver_;
