a7b1aae3189ed726d927e812236060ab489d2f60
fs@opera.com <fs@opera.com>
2015-06-02 19:56:36 +0000

    Drop fillRule state from GraphicsContext
    There was only a few trivial (local) uses of this state field left. Since
    the same state is carried in the Path (SkPath) object itself, make use of
    that instead.
    
    BUG=424655
    
    Review URL: https://codereview.chromium.org/1147343003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196340 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/BoxBorderPainter.cpp
+++ third_party/WebKit/Source/core/paint/BoxBorderPainter.cpp
@@ -419,6 +419,7 @@ bool BoxBorderPainter::paintBorderFastPath(GraphicsContext* context, const Layou
         ASSERT(m_visibleEdgeSet != AllBorderEdges);
         // solid, rectangular border => one drawPath()
         Path path;
+        path.setWindRule(RULE_NONZERO);
 
         for (int i = BSTop; i <= BSLeft; ++i) {
             const BorderEdge& currEdge = m_edges[i];
@@ -426,10 +427,8 @@ bool BoxBorderPainter::paintBorderFastPath(GraphicsContext* context, const Layou
                 path.addRect(calculateSideRect(m_outer, currEdge, i));
         }
 
-        context->setFillRule(RULE_NONZERO);
         context->setFillColor(firstEdge().color);
         context->fillPath(path);
-
         return true;
     }
 
