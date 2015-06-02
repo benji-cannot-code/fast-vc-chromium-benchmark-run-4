958b18c6355dd44ca02b64b547ee4f115fd2793a
kenrb <kenrb@chromium.org>
2015-06-02 06:41:44 -0700

    Cleanup ImageTransportFactory properly during unit tests.
    Unit test memory leak introduced by
    https://codereview.chromium.org/1094113003/. Adding the correct cleanup
    line and removing the suppressions.
    
    BUG=494808
    
    Review URL: https://codereview.chromium.org/1156223006
    
    Cr-Commit-Position: refs/heads/master@{#332379}

--- content/browser/frame_host/render_frame_host_manager_unittest.cc
+++ content/browser/frame_host/render_frame_host_manager_unittest.cc
@@ -280,6 +280,9 @@ class RenderFrameHostManagerTest : public RenderViewHostImplTestHarness {
   }
 
   void TearDown() override {
+#if !defined(OS_ANDROID)
+    ImageTransportFactory::Terminate();
+#endif
     RenderViewHostImplTestHarness::TearDown();
     WebUIControllerFactory::UnregisterFactoryForTesting(&factory_);
   }
