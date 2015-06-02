014e2cde96e415d9d14575a63e34a1a145053176
nick <nick@chromium.org>
2015-06-01 18:02:12 -0700

    Run python update_extension_permission.py
    extensions\common\permissions\api_permission.h had a value added (on
    May 11 for bug 485227) that threw off the numbering. Move it to the
    bottom. We should probably also merge this to the branch.
    
    BUG=485227
    NOPRESUBMIT=true
    
    Review URL: https://codereview.chromium.org/1153503003
    
    Cr-Commit-Position: refs/heads/master@{#332286}

--- extensions/common/permissions/api_permission.h
+++ extensions/common/permissions/api_permission.h
@@ -159,7 +159,6 @@ class APIPermission {
     kNotifications,
     kOverrideEscFullscreen,
     kPageCapture,
-    kPasswordsPrivate,
     kPointerLock,
     kPlatformKeys,
     kPlugin,
@@ -242,6 +241,7 @@ class APIPermission {
     kUsbDeviceUnknownProduct,
     kUsbDeviceUnknownVendor,
     kUsersPrivate,
+    kPasswordsPrivate,
     // Last entry: Add new entries above and ensure to update the
     // "ExtensionPermission3" enum in tools/metrics/histograms/histograms.xml
     // (by running update_extension_permission.py).
