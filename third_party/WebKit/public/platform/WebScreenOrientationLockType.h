12d9618e126a0938935758b1aabb6dde6c1ea4cc
mlamouri@chromium.org <mlamouri@chromium.org>
2015-06-02 17:03:53 +0000

    Move screen_orientation public headers to module directory.
    This is part of a suite of three CLs:
    1. <this>
    2. https://codereview.chromium.org/1151373005
    3. https://codereview.chromium.org/1165753006
    
    BUG=453032
    
    Review URL: https://codereview.chromium.org/1164753002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196331 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebScreenOrientationLockType.h
+++ third_party/WebKit/public/platform/WebScreenOrientationLockType.h
@@ -3,23 +3,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef WebScreenOrientationLockType_h
-#define WebScreenOrientationLockType_h
-
-namespace blink {
-
-enum WebScreenOrientationLockType {
-    WebScreenOrientationLockDefault = 0, // Equivalent to unlock.
-    WebScreenOrientationLockPortraitPrimary,
-    WebScreenOrientationLockPortraitSecondary,
-    WebScreenOrientationLockLandscapePrimary,
-    WebScreenOrientationLockLandscapeSecondary,
-    WebScreenOrientationLockAny,
-    WebScreenOrientationLockLandscape,
-    WebScreenOrientationLockPortrait,
-    WebScreenOrientationLockNatural,
-};
-
-} // namespace blink
-
-#endif // WebScreenOrientationLockType_h
+#include "public/platform/modules/screen_orientation/WebScreenOrientationLockType.h"
