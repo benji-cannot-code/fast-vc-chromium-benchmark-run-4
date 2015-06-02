c4d068eba6d895f605bb980f7a2d7db16bb84326
raymes <raymes@chromium.org>
2015-06-02 00:57:10 -0700

    Fix graphics corruption that can occur in the PDF viewer when scrolling
    In a PDF, if we scroll in a reverse direction to the direction we originally scrolled
    (within the same paint) and there were invalidations that happened in-between we
    may end up incorrectly clipping the invalidated rects (see crbug.com/488390). This bug
    doesn't exist in the original implementation
    (ppapi/utility/graphics/paint_aggregator.cc) which uses a different method
    of handling invalidations that occur after a scroll. The problem is that
    when we scroll the invalidated region, we clip it to the scroll rect. This
    can cause us to lose information about what the invalidated region was if
    it gets scrolled back into view. We either need to not do this clipping or
    disallow combining scrolls that occur in different directions with
    invalidations that happen in-between.
    
    BUG=488390
    
    Review URL: https://codereview.chromium.org/1160513004
    
    Cr-Commit-Position: refs/heads/master@{#332354}

--- pdf/paint_aggregator.cc
+++ pdf/paint_aggregator.cc
@@ -9,6 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 
+namespace {
+
+bool IsNegative(int32_t num) {
+  return num < 0;
+}
+
+}  // namespace
+
 // ----------------------------------------------------------------------------
 // ALGORITHM NOTES
 //
@@ -144,6 +152,25 @@ void PaintAggregator::ScrollRect(const pp::Rect& clip_rect,
       (amount.y() && update_.scroll_delta.x())) {
     InvalidateRect(clip_rect);
     return;
+  }
+
+  // If we scroll in a reverse direction to the direction we originally scrolled
+  // and there were invalidations that happened in-between we may end up
+  // incorrectly clipping the invalidated rects (see crbug.com/488390). This bug
+  // doesn't exist in the original implementation
+  // (ppapi/utility/graphics/paint_aggregator.cc) which uses a different method
+  // of handling invalidations that occur after a scroll. The problem is that
+  // when we scroll the invalidated region, we clip it to the scroll rect. This
+  // can cause us to lose information about what the invalidated region was if
+  // it gets scrolled back into view. We either need to not do this clipping or
+  // disallow combining scrolls that occur in different directions with
+  // invalidations that happen in-between. This code really needs some tests...
+  if (!update_.paint_rects.empty()) {
+    if (IsNegative(amount.x()) != IsNegative(update_.scroll_delta.x()) ||
+        IsNegative(amount.y()) != IsNegative(update_.scroll_delta.y())) {
+      InvalidateRect(clip_rect);
+      return;
+    }
   }
 
   // The scroll rect is new or isn't changing (though the scroll amount may
