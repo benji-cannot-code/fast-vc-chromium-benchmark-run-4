2bc47e312d697f03b157a88072c3591b5e4fa475
tkent@chromium.org <tkent@chromium.org>
2015-06-02 08:14:58 +0000

    Cleanup: Rename ChromeClient::setWindowRect and setWindowRectInternal.
    - Rename ChromeClient::setWindowRect to setWindowRectWithAdjustment
    - Rename ChromeClient::setWindowRectInternal to setWindowRect
    
    This CL has no behavior changes.
    
    BUG=495378
    
    Review URL: https://codereview.chromium.org/1167653007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
+++ third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
@@ -1237,7 +1237,7 @@ void LocalDOMWindow::moveBy(int x, int y, bool hasX, bool hasY) const
     IntRect windowRect = host->chromeClient().windowRect();
     windowRect.move(x, y);
     // Security check (the spec talks about UniversalBrowserWrite to disable this check...)
-    host->chromeClient().setWindowRect(windowRect);
+    host->chromeClient().setWindowRectWithAdjustment(windowRect);
 }
 
 void LocalDOMWindow::moveTo(int x, int y, bool hasX, bool hasY) const
@@ -1255,7 +1255,7 @@ void LocalDOMWindow::moveTo(int x, int y, bool hasX, bool hasY) const
     IntRect windowRect = host->chromeClient().windowRect();
     windowRect.setLocation(IntPoint(hasX ? x : windowRect.x(), hasY ? y : windowRect.y()));
     // Security check (the spec talks about UniversalBrowserWrite to disable this check...)
-    host->chromeClient().setWindowRect(windowRect);
+    host->chromeClient().setWindowRectWithAdjustment(windowRect);
 }
 
 void LocalDOMWindow::resizeBy(int x, int y, bool hasX, bool hasY) const
@@ -1273,7 +1273,7 @@ void LocalDOMWindow::resizeBy(int x, int y, bool hasX, bool hasY) const
     IntRect fr = host->chromeClient().windowRect();
     IntSize dest = fr.size() + IntSize(x, y);
     IntRect update(fr.location(), dest);
-    host->chromeClient().setWindowRect(update);
+    host->chromeClient().setWindowRectWithAdjustment(update);
 }
 
 void LocalDOMWindow::resizeTo(int width, int height, bool hasWidth, bool hasHeight) const
@@ -1291,7 +1291,7 @@ void LocalDOMWindow::resizeTo(int width, int height, bool hasWidth, bool hasHeig
     IntRect fr = host->chromeClient().windowRect();
     IntSize dest = IntSize(hasWidth ? width : fr.width(), hasHeight ? height : fr.height());
     IntRect update(fr.location(), dest);
-    host->chromeClient().setWindowRect(update);
+    host->chromeClient().setWindowRectWithAdjustment(update);
 }
 
 int LocalDOMWindow::requestAnimationFrame(FrameRequestCallback* callback)
--- third_party/WebKit/Source/core/page/ChromeClient.cpp
+++ third_party/WebKit/Source/core/page/ChromeClient.cpp
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void ChromeClient::setWindowRect(const IntRect& pendingRect)
+void ChromeClient::setWindowRectWithAdjustment(const IntRect& pendingRect)
 {
     IntRect screen = screenInfo().availableRect;
     IntRect window = pendingRect;
@@ -59,7 +59,7 @@ void ChromeClient::setWindowRect(const IntRect& pendingRect)
     // Constrain the window position within the valid screen area.
     window.setX(std::max(screen.x(), std::min(window.x(), screen.maxX() - window.width())));
     window.setY(std::max(screen.y(), std::min(window.y(), screen.maxY() - window.height())));
-    setWindowRectInternal(window);
+    setWindowRect(window);
 }
 
 bool ChromeClient::canRunModalIfDuringPageDismissal(Frame* mainFrame, ChromeClient::DialogType dialog, const String& message)
