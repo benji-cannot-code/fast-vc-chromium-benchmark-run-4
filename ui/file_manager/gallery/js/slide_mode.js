598063e5923d69ccb3f3dd911ceb2844a231997f
yawano <yawano@chromium.org>
2015-06-01 22:21:25 -0700

    Gallery: Add toolbar at the top of window.
    BUG=488227
    TEST=Manually tested.
    
    Review URL: https://codereview.chromium.org/1145893010
    
    Cr-Commit-Position: refs/heads/master@{#332346}

--- ui/file_manager/gallery/js/slide_mode.js
+++ ui/file_manager/gallery/js/slide_mode.js
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  * @param {!HTMLElement} container Main container element.
  * @param {!HTMLElement} content Content container element.
- * @param {!HTMLElement} toolbar Toolbar element.
+ * @param {!HTMLElement} topToolbar Top toolbar element.
+ * @param {!HTMLElement} bottomToolbar Toolbar element.
  * @param {!ImageEditor.Prompt} prompt Prompt.
  * @param {!ErrorBanner} errorBanner Error banner.
  * @param {!cr.ui.ArrayDataModel} dataModel Data model.
@@ -26,9 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @suppress {checkStructDictInheritance}
  * @extends {cr.EventTarget}
  */
-function SlideMode(container, content, toolbar, prompt, errorBanner, dataModel,
-    selectionModel, metadataModel, thumbnailModel, context, volumeManager,
-    toggleMode, displayStringFunction) {
+function SlideMode(container, content, topToolbar, bottomToolbar, prompt,
+    errorBanner, dataModel, selectionModel, metadataModel, thumbnailModel,
+    context, volumeManager, toggleMode, displayStringFunction) {
   /**
    * @type {!HTMLElement}
    * @private
@@ -54,7 +55,14 @@ function SlideMode(container, content, toolbar, prompt, errorBanner, dataModel,
    * @private
    * @const
    */
-  this.toolbar_ = toolbar;
+  this.topToolbar_ = topToolbar;
+
+  /**
+   * @type {!HTMLElement}
+   * @private
+   * @const
+   */
+  this.bottomToolbar_ = bottomToolbar;
 
   /**
    * @type {!ImageEditor.Prompt}
@@ -220,7 +228,7 @@ function SlideMode(container, content, toolbar, prompt, errorBanner, dataModel,
    * @const
    */
   this.options_ = util.createChild(queryRequiredElement(
-      this.toolbar_, '.filename-spacer'), 'options');
+      this.topToolbar_, '.filename-spacer'), 'options');
 
   /**
    * @type {!HTMLElement}
@@ -271,7 +279,7 @@ function SlideMode(container, content, toolbar, prompt, errorBanner, dataModel,
    * @private
    * @const
    */
-  this.bubble_ = util.createChild(this.toolbar_, 'bubble');
+  this.bubble_ = util.createChild(this.bottomToolbar_, 'bubble');
   this.bubble_.hidden = true;
 
   /**
@@ -328,7 +336,8 @@ function SlideMode(container, content, toolbar, prompt, errorBanner, dataModel,
    * @private
    * @const
    */
-  this.ribbonSpacer_ = queryRequiredElement(this.toolbar_, '.ribbon-spacer');
+  this.ribbonSpacer_ = queryRequiredElement(this.bottomToolbar_,
+      '.ribbon-spacer');
 
   /**
    * @type {!Ribbon}
@@ -345,7 +354,8 @@ function SlideMode(container, content, toolbar, prompt, errorBanner, dataModel,
    * @type {!HTMLElement}
    * @const
    */
-  var slideShowButton = queryRequiredElement(this.toolbar_, 'button.slideshow');
+  var slideShowButton = queryRequiredElement(this.topToolbar_,
+      'button.slideshow');
   slideShowButton.title = this.displayStringFunction_('GALLERY_SLIDESHOW');
   slideShowButton.addEventListener('click',
       this.startSlideshow.bind(this, SlideMode.SLIDESHOW_INTERVAL_FIRST));
@@ -367,7 +377,7 @@ function SlideMode(container, content, toolbar, prompt, errorBanner, dataModel,
    * @private
    * @const
    */
-  this.editButton_ = queryRequiredElement(this.toolbar_, 'button.edit');
+  this.editButton_ = queryRequiredElement(this.topToolbar_, 'button.edit');
   this.editButton_.title = this.displayStringFunction_('GALLERY_EDIT');
   this.editButton_.disabled = true;  // Disabled by default.
   this.editButton_.addEventListener('click', this.toggleEditor.bind(this));
@@ -377,7 +387,7 @@ function SlideMode(container, content, toolbar, prompt, errorBanner, dataModel,
    * @private
    * @const
    */
-  this.printButton_ = queryRequiredElement(this.toolbar_, 'button.print');
+  this.printButton_ = queryRequiredElement(this.topToolbar_, 'button.print');
   this.printButton_.title = this.displayStringFunction_('GALLERY_PRINT');
   this.printButton_.disabled = true;  // Disabled by default.
   this.printButton_.addEventListener('click', this.print_.bind(this));
@@ -387,7 +397,8 @@ function SlideMode(container, content, toolbar, prompt, errorBanner, dataModel,
    * @private
    * @const
    */
-  this.editBarSpacer_ = queryRequiredElement(this.toolbar_, '.edit-bar-spacer');
+  this.editBarSpacer_ = queryRequiredElement(this.bottomToolbar_,
+      '.edit-bar-spacer');
 
   /**
    * @type {!HTMLElement}
