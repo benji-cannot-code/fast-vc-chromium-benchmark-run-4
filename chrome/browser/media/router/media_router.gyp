b0f422fa1df5c296d09e254fb106d44bc6c5c2a8
imcheng <imcheng@google.com>
2015-06-01 18:00:06 -0700

    [Media Router] Reland of MediaRouterUI + WebUI handler implementation.
    Tests failed due to event_page_tracker_ nullptr in MediaRouterMojoImpl.
    There is actually no reason we should delay instantiation of that field
    until BindToMojoRequest. So the change that would also fix the test is
    to initialize event_page_tracker_ on construction. It also eliminates
    a ForTest method since now tests can inject mock/fake version of
    EventPageTracker via the common constructor.
    
    TEST=*_rel trybots,asan trybots. Also run the tests locally.
    
    Original patch description: (crrev.com/1139203003)
    
    This patch has two parts:
    - MediaRouterWebUIMessageHandler handles calls coming
    from WebUI/JS. Also sends up-to-date data (e.g. MediaSinks)
    to WebUI.
    - MediaRouterUI interfaces with the MediaRouter to handle calls
    from the WebUI (such as CreateRoute) and listen for sink /
    route updates.
    
    Implemented handler logic for the following from Media Router WebUI:
    
    OnGetInitialSettings
    OnCreateRoute
    OnActOnIssue - skeleton impl
    OnCloseRoute
    OnCloseDialog
    
    Implemented WebUI logic for the following (to update the UI):
    UpdateSinks
    UpdateRoutes
    UpdateCastModes
    AddRoute
    UpdateIssue - skeleton impl
    
    Some Issues related APIs are left unimplemented until more resources
    have been upstreamed.
    
    BUG=464216, 464208
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1161483003
    
    Cr-Commit-Position: refs/heads/master@{#332283}

--- chrome/browser/media/router/media_router.gyp
+++ chrome/browser/media/router/media_router.gyp
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'dependencies': [
         'media_router',
         'media_router_mojo',
+        'media_router_mojo_gen',
         '<(DEPTH)/base/base.gyp:base',
         '<(DEPTH)/testing/gmock.gyp:gmock',
       ],
--- chrome/browser/media/router/media_router_mojo_impl_factory.cc
+++ chrome/browser/media/router/media_router_mojo_impl_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/media/router/media_router_mojo_impl.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "extensions/browser/process_manager.h"
 #include "extensions/browser/process_manager_factory.h"
 
 using content::BrowserContext;
@@ -42,7 +43,7 @@ MediaRouterMojoImplFactory::~MediaRouterMojoImplFactory() {
 
 KeyedService* MediaRouterMojoImplFactory::BuildServiceInstanceFor(
     BrowserContext* context) const {
-  return new MediaRouterMojoImpl();
+  return new MediaRouterMojoImpl(extensions::ProcessManager::Get(context));
 }
 
 BrowserContext* MediaRouterMojoImplFactory::GetBrowserContextToUse(
