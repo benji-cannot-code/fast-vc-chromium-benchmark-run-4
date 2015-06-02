e4675847a2dd9379cbfb1210184077f69c50fd34
sunnyps <sunnyps@chromium.org>
2015-06-01 20:09:22 -0700

    webview: Initialize synchronous compositor after first activation.
    Drawing before the first activation produces no results but can cause
    cc DCHECKs to fail. To prevent this SynchronousCompositorImpl doesn't
    register with it's client until the first activation.
    
    BUG=492815
    
    Review URL: https://codereview.chromium.org/1150423003
    
    Cr-Commit-Position: refs/heads/master@{#332334}

--- android_webview/browser/browser_view_renderer.cc
+++ android_webview/browser/browser_view_renderer.cc
@@ -451,6 +451,7 @@ void BrowserViewRenderer::DidDestroyCompositor(
     content::SynchronousCompositor* compositor) {
   TRACE_EVENT0("android_webview", "BrowserViewRenderer::DidDestroyCompositor");
   DCHECK(compositor_);
+  compositor_->SetIsActive(false);
   compositor_ = NULL;
 }
 
--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsRenderTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsRenderTest.java
@@ -12,7 +12,6 @@ import android.test.suitebuilder.annotation.SmallTest;
 import org.chromium.android_webview.AwContents;
 import org.chromium.android_webview.test.util.GraphicsTestUtils;
 import org.chromium.base.ThreadUtils;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 
@@ -44,12 +43,9 @@ public class AwContentsRenderTest extends AwTestBase {
         });
     }
 
-    /*
+
     @SmallTest
     @Feature({"AndroidWebView"})
-    crbug.com/492815
-    */
-    @DisabledTest
     public void testSetGetBackgroundColor() throws Throwable {
         setBackgroundColorOnUiThread(Color.MAGENTA);
         GraphicsTestUtils.pollForBackgroundColor(mAwContents, Color.MAGENTA);
