e03fe91da684d915486a96e7c3ecc7258ee88d06
shreyasv <shreyasv@chromium.org>
2015-06-02 20:26:01 -0700

    Consult the delegate for makeActive/Inactive operations
    5 notable changes:
    1) Removed the weak ref dance in makeActive/makeInactive. The retaining of self
    here is intended and is the correct behavior.
    The previous code had a subtle bug. Due to the fact that |weakSelf
    andCallCompletionHandler:completionHandler| was being called from a
    completionHandler a makeActive/Inactive completionHandler would never get
    called if the CRWBrowsingDataStore was deallocated -- this is not good from an
    API design perspective since the completionHandler should always be called even
    if the underlying object went away e.g this is the WKWebView.
    
    2) Perform no operation when the mode is already in the intended state. The
    |completionHandler| is however deferred since calling the completionHandler
    before the client caller has even finished is awkward. We still need to call
    |finalizeChangeToMode:| in this case because another operation could have been
    enqueued before the completionHandler is actually called.
    
    3) Consolidated the logic for makeActive/makeInactive into a common
    function (changeModeTo:comptionHandler)
    
    4) Added a NONE OperationType to indicate a NOP
    
    5) Moved the comments for the operations into the enum because this is a more appropriate place.
    
    BUG=480654
    
    Review URL: https://codereview.chromium.org/1152083010
    
    Cr-Commit-Position: refs/heads/master@{#332543}

--- ios/web/crw_browsing_data_store_unittest.mm
+++ ios/web/crw_browsing_data_store_unittest.mm
@@ -151,6 +151,7 @@ TEST_F(BrowsingDataStoreTest, MakeActiveAndInactiveOperations) {
     return;
   }
 
+  MakeInactive();
   base::scoped_nsobject<CRWTestBrowsingDataStoreObserver> observer(
       [[CRWTestBrowsingDataStoreObserver alloc]
           initWithBrowsingDataStore:browsing_data_store_]);
