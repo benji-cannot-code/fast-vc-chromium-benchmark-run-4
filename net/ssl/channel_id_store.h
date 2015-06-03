c6c07663be8cf460d7ba6aa5a6fee118fde903c5
davidben <davidben@chromium.org>
2015-06-03 14:37:39 -0700

    Update some comments for origin-bounds certs -> channel ID
    BUG=488555
    
    Review URL: https://codereview.chromium.org/1152273006
    
    Cr-Commit-Position: refs/heads/master@{#332692}

--- net/ssl/channel_id_store.h
+++ net/ssl/channel_id_store.h
@@ -19,10 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 // An interface for storing and retrieving channel ID keypairs.
-// There isn't a domain bound certs spec yet, but the old origin bound
-// certificates are specified in
-// http://balfanz.github.com/tls-obc-spec/draft-balfanz-tls-obc-01.html.
-// TODO(wtc): Update this comment.
+// See https://tools.ietf.org/html/draft-balfanz-tls-channelid-01
 
 // Owned only by a single ChannelIDService object, which is responsible
 // for deleting it.
