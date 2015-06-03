72c31118897716f105d757cd20a07658f7481e3f
derekjchow <derekjchow@chromium.org>
2015-06-03 12:00:56 -0700

    [Chromecast] ToT buildfix.
    Include "base/location.h" in cast_content_renderer_client.
    
    R=gunsch@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1150863006
    
    Cr-Commit-Position: refs/heads/master@{#332657}

--- chromecast/renderer/cast_content_renderer_client.cc
+++ chromecast/renderer/cast_content_renderer_client.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/sysinfo.h>
 
 #include "base/command_line.h"
+#include "base/location.h"
 #include "base/memory/memory_pressure_listener.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/thread_task_runner_handle.h"
