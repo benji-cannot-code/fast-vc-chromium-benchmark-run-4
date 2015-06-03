b26c2fa376ffbb41065413f11a0c0821ddf24249
alexclarke <alexclarke@chromium.org>
2015-06-03 04:17:21 -0700

    Simplify the renderer scheduler's input event handling.
    Rather than trying to infer when input events are handled,
    actually observe when it happens.  This lets us simplify the
    handling of input events in the renderer scheduler.
    
    This is a pre-requisite for later work to limit dom timers to
    only run in idle time.
    
    BUG=463143
    
    Review URL: https://codereview.chromium.org/1148333005
    
    Cr-Commit-Position: refs/heads/master@{#332590}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/debug/benchmark_instrumentation.h"
 #include "cc/output/output_surface.h"
 #include "cc/trees/layer_tree_host.h"
+#include "components/scheduler/renderer/renderer_scheduler.h"
 #include "content/child/npapi/webplugin.h"
 #include "content/common/gpu/client/context_provider_command_buffer.h"
 #include "content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.h"
@@ -1267,6 +1268,11 @@ void RenderWidget::OnHandleInputEvent(const blink::WebInputEvent* input_event,
     } else {
       Send(response.release());
     }
+  }
+  if (!no_ack && RenderThreadImpl::current()) {
+    RenderThreadImpl::current()
+        ->GetRendererScheduler()
+        ->DidHandleInputEventOnMainThread(*input_event);
   }
   if (input_event->type == WebInputEvent::MouseMove)
     ignore_ack_for_mouse_move_from_debugger_ = false;
