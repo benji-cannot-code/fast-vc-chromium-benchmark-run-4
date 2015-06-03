ef85baa160d8bb133ea5f7c328fdea80435d66df
ekaramad <ekaramad@chromium.org>
2015-06-03 15:15:18 -0700

    Webview browser test for cookie isolation.
    Moved the previously disabled |WebViewTest.CookieIsolation| test from C++ to javascript.
    
    BUG=294196
    
    Review URL: https://codereview.chromium.org/1153723004
    
    Cr-Commit-Position: refs/heads/master@{#332707}

---
+++ chrome/test/data/extensions/platform_apps/web_view/cookie_isolation/message_types.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Different message types for the host app and the guest webviews.
+var START_TEST = 'Start the test.';
+var TEST_ENDED = 'Test ended.';
+var SET_COOKIES = 'Set the given cookie.';
+var SET_COOKIES_COMPLETE = 'Cookie was set to the given value.';
+var GET_COOKIES = 'Get cookies.';
+var GET_COOKIES_COMPLETE = 'Cookies are ready.';
+var CLEAR_COOKIES = 'Clear all cookies.'
+var CLEAR_COOKIES_COMPLETE = 'Cookies cleard.';
