3dce467d71b5b864a02255e9340c6c932978bd94
erg <erg@chromium.org>
2015-06-02 17:19:54 -0700

    mandoline filesystem: Compile filesystem into core_services.
    This packages the filesystem application physically with the rest of
    core_services.
    
    BUG=490237
    
    Review URL: https://codereview.chromium.org/1167693009
    
    Cr-Commit-Position: refs/heads/master@{#332504}

---
+++ components/filesystem/file_system_app.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/filesystem/file_system_app.h"
+
+#include "mojo/application/public/cpp/application_connection.h"
+
+namespace filesystem {
+
+FileSystemApp::FileSystemApp() {}
+
+FileSystemApp::~FileSystemApp() {}
+
+bool FileSystemApp::ConfigureIncomingConnection(
+    mojo::ApplicationConnection* connection) {
+  connection->AddService<FileSystem>(this);
+  return true;
+}
+
+// |InterfaceFactory<Files>| implementation:
+void FileSystemApp::Create(mojo::ApplicationConnection* connection,
+                           mojo::InterfaceRequest<FileSystem> request) {
+  new FileSystemImpl(connection, request.Pass());
+}
+
+}  // namespace filesystem
