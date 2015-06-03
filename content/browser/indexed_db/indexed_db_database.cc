7b6d245acdc8abcff79768d5cd34782ab41d44ee
jsbell <jsbell@chromium.org>
2015-06-03 11:46:03 -0700

    Indexed DB: Fix tracing of abort operations
    Abort operations always have a null transaction, so don't dereference
    when tracing.
    
    R=cmumford@chromium.org
    BUG=495834
    
    Review URL: https://codereview.chromium.org/1157313004
    
    Cr-Commit-Position: refs/heads/master@{#332655}

--- content/browser/indexed_db/indexed_db_database.cc
+++ content/browser/indexed_db/indexed_db_database.cc
@@ -425,10 +425,8 @@ void IndexedDBDatabase::CreateIndexAbortOperation(
     int64 object_store_id,
     int64 index_id,
     IndexedDBTransaction* transaction) {
-  IDB_TRACE1("IndexedDBDatabase::CreateIndexAbortOperation",
-             "txn.id",
-             transaction->id());
   DCHECK(!transaction);
+  IDB_TRACE("IndexedDBDatabase::CreateIndexAbortOperation");
   RemoveIndex(object_store_id, index_id);
 }
 
@@ -492,9 +490,7 @@ void IndexedDBDatabase::DeleteIndexAbortOperation(
     const IndexedDBIndexMetadata& index_metadata,
     IndexedDBTransaction* transaction) {
   DCHECK(!transaction);
-  IDB_TRACE1("IndexedDBDatabase::DeleteIndexAbortOperation",
-             "txn.id",
-             transaction->id());
+  IDB_TRACE("IndexedDBDatabase::DeleteIndexAbortOperation");
   AddIndex(object_store_id, index_metadata, IndexedDBIndexMetadata::kInvalidId);
 }
 
@@ -1950,9 +1946,7 @@ void IndexedDBDatabase::CreateObjectStoreAbortOperation(
     int64 object_store_id,
     IndexedDBTransaction* transaction) {
   DCHECK(!transaction);
-  IDB_TRACE1("IndexedDBDatabase::CreateObjectStoreAbortOperation",
-             "txn.id",
-             transaction->id());
+  IDB_TRACE("IndexedDBDatabase::CreateObjectStoreAbortOperation");
   RemoveObjectStore(object_store_id);
 }
 
@@ -1960,9 +1954,7 @@ void IndexedDBDatabase::DeleteObjectStoreAbortOperation(
     const IndexedDBObjectStoreMetadata& object_store_metadata,
     IndexedDBTransaction* transaction) {
   DCHECK(!transaction);
-  IDB_TRACE1("IndexedDBDatabase::DeleteObjectStoreAbortOperation",
-             "txn.id",
-             transaction->id());
+  IDB_TRACE("IndexedDBDatabase::DeleteObjectStoreAbortOperation");
   AddObjectStore(object_store_metadata,
                  IndexedDBObjectStoreMetadata::kInvalidId);
 }
@@ -1972,9 +1964,7 @@ void IndexedDBDatabase::VersionChangeAbortOperation(
     int64 previous_int_version,
     IndexedDBTransaction* transaction) {
   DCHECK(!transaction);
-  IDB_TRACE1("IndexedDBDatabase::VersionChangeAbortOperation",
-             "txn.id",
-             transaction->id());
+  IDB_TRACE("IndexedDBDatabase::VersionChangeAbortOperation");
   metadata_.version = previous_version;
   metadata_.int_version = previous_int_version;
 }
