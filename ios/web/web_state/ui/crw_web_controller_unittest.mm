792735e2fb36500496bfe71086b2d069fe0bbe3f
sdefresne <sdefresne@chromium.org>
2015-06-03 00:51:37 -0700

    Revert of Added CGFloat comparison functions. (patchset #7 id:120001 of https://codereview.chromium.org/1155623002/)
    Reason for revert:
    Reverting because it cause the downstream waterfall to be flaky.
    
    Original issue's description:
    > Added CGFloat comparison functions.
    >
    > Comparing CGFloat values using direct equality was causing flakes in
    > CRWWebController's PageScrollState unittests, so compare using CGFloat's
    > epsilon.
    >
    > BUG=474290
    >
    > Committed: https://crrev.com/53ce056bd3c312723adca2d3245d75a0599d904c
    > Cr-Commit-Position: refs/heads/master@{#332223}
    
    TBR=eugenebut@chromium.org,baxley@chromium.org,stuartmorgan@chromium.org,danakj@chromium.org,stevenjb@chromium.org,kkhorimoto@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=474290
    
    Review URL: https://codereview.chromium.org/1168523004
    
    Cr-Commit-Position: refs/heads/master@{#332562}

--- ios/web/web_state/ui/crw_web_controller_unittest.mm
+++ ios/web/web_state/ui/crw_web_controller_unittest.mm
@@ -915,11 +915,6 @@ class CRWWKWebControllerPageScrollStateTest : public web::WKWebViewWebTest {
   }
 };
 
-// Helper function for comparing two CGFloats.
-bool CGFloatsAreEqual(CGFloat val1, CGFloat val2) {
-  return std::fabs(val1 - val2) < std::numeric_limits<CGFloat>::epsilon();
-}
-
 WEB_TEST_F(CRWUIWebControllerPageScrollStateTest,
            CRWWKWebControllerPageScrollStateTest,
            SetPageStateWithUserScalableDisabled) {
@@ -951,12 +946,12 @@ WEB_TEST_F(CRWUIWebControllerPageScrollStateTest,
   // setPageState: is async; wait for its completion.
   scrollView = [[[this->webController_ view] subviews][0] scrollView];
   base::test::ios::WaitUntilCondition(^bool() {
-    return CGFloatsAreEqual(scrollView.contentOffset.x, 1.0f);
+    return [scrollView contentOffset].x == 1.0f;
   });
 
-  ASSERT_CGFLOAT_EQ(originZoomScale, scrollView.zoomScale);
-  ASSERT_CGFLOAT_EQ(originMinimumZoomScale, scrollView.minimumZoomScale);
-  ASSERT_CGFLOAT_EQ(originMaximumZoomScale, scrollView.maximumZoomScale);
+  ASSERT_EQ(originZoomScale, scrollView.zoomScale);
+  ASSERT_EQ(originMinimumZoomScale, scrollView.minimumZoomScale);
+  ASSERT_EQ(originMaximumZoomScale, scrollView.maximumZoomScale);
 };
 
 WEB_TEST_F(CRWUIWebControllerPageScrollStateTest,
@@ -980,10 +975,10 @@ WEB_TEST_F(CRWUIWebControllerPageScrollStateTest,
   id webView = [[this->webController_ view] subviews][0];
   UIScrollView* scrollView = [webView scrollView];
   base::test::ios::WaitUntilCondition(^bool() {
-    return CGFloatsAreEqual(scrollView.contentOffset.x, 1.0f);
+    return [scrollView contentOffset].x == 1.0f;
   });
 
-  ASSERT_FLOAT_EQ(3, scrollView.zoomScale / scrollView.minimumZoomScale);
+  EXPECT_FLOAT_EQ(3, scrollView.zoomScale / scrollView.minimumZoomScale);
 };
 
 WEB_TEST_F(CRWUIWebControllerPageScrollStateTest,
@@ -1012,7 +1007,7 @@ WEB_TEST_F(CRWUIWebControllerPageScrollStateTest,
   // setPageState: is async; wait for its completion.
   id webView = [[this->webController_ view] subviews][0];
   base::test::ios::WaitUntilCondition(^bool() {
-    return CGFloatsAreEqual([webView scrollView].contentOffset.y, 30.0f);
+    return [[webView scrollView] contentOffset].y == 30.0f;
   });
 
   ASSERT_FALSE([this->webController_ atTop]);
--- ui/message_center/cocoa/tray_view_controller_unittest.mm
+++ ui/message_center/cocoa/tray_view_controller_unittest.mm
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
-#import "testing/gtest_mac.h"
 #import "ui/gfx/test/ui_cocoa_test_helper.h"
 #include "ui/message_center/fake_notifier_settings_provider.h"
 #include "ui/message_center/message_center.h"
