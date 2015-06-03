b42c5540e3f1b9dd0475fa786e1674e9f6b1caa3
hashimoto <hashimoto@chromium.org>
2015-06-02 20:17:45 -0700

    Compress crash reports with gzip
    BUG=493717
    TEST=Opening chrome://inducebrowsercrashforrealz results in a new crash report in chrome://crashes
    
    Review URL: https://codereview.chromium.org/1162873002
    
    Cr-Commit-Position: refs/heads/master@{#332531}

--- chrome/android/java_staging/src/org/chromium/chrome/browser/crash/MinidumpUploadCallable.java
+++ chrome/android/java_staging/src/org/chromium/chrome/browser/crash/MinidumpUploadCallable.java
@@ -30,6 +30,7 @@ import java.net.HttpURLConnection;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.concurrent.Callable;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * This class tries to upload a minidump to the crash server.
@@ -99,7 +100,7 @@ public class MinidumpUploadCallable implements Callable<Boolean> {
                 return false;
             }
             minidumpInputStream = new FileInputStream(mFileToUpload);
-            streamCopy(minidumpInputStream, connection.getOutputStream());
+            streamCopy(minidumpInputStream, new GZIPOutputStream(connection.getOutputStream()));
             boolean status = handleExecutionResponse(connection);
 
             if (isLimited) updateUploadPrefs();
@@ -136,6 +137,7 @@ public class MinidumpUploadCallable implements Callable<Boolean> {
 
         connection.setDoOutput(true);
         connection.setRequestProperty("Connection", "Keep-Alive");
+        connection.setRequestProperty("Content-Encoding", "gzip");
         connection.setRequestProperty("Content-Type", String.format(CONTENT_TYPE_TMPL, boundary));
         return true;
     }
