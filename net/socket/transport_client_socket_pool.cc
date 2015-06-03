4760ad3cc7e0ac1bf158b166a821a6651e51f70e
ketan.goyal <ketan.goyal@samsung.com>
2015-06-03 03:53:36 -0700

    Converted bare pointer to scoped_ptr<> in net/socket and net/http
    BUG=479898
    R=eroman@chromium.org
    
    Review URL: https://codereview.chromium.org/1158193006
    
    Cr-Commit-Position: refs/heads/master@{#332587}

--- net/socket/transport_client_socket_pool.cc
+++ net/socket/transport_client_socket_pool.cc
@@ -610,7 +610,7 @@ LoadState TransportClientSocketPool::GetLoadState(
   return base_.GetLoadState(group_name, handle);
 }
 
-base::DictionaryValue* TransportClientSocketPool::GetInfoAsValue(
+scoped_ptr<base::DictionaryValue> TransportClientSocketPool::GetInfoAsValue(
     const std::string& name,
     const std::string& type,
     bool include_nested_pools) const {
