95e2485925491e4a65caa0f85059ceaec1f60e52
shail.s <shail.s@samsung.com>
2015-06-02 20:47:45 -0700

    Subsituting pattern ScopedVector push_back.(ptr.release()) with push_back(ptr.Pass()) in device/bluetooth
    BUG=457697
    R=rpaquay@chromium.org
    
    Review URL: https://codereview.chromium.org/1160263002
    
    Cr-Commit-Position: refs/heads/master@{#332546}

--- device/bluetooth/bluetooth_low_energy_win.cc
+++ device/bluetooth/bluetooth_low_energy_win.cc
@@ -650,7 +650,7 @@ bool EnumerateKnownBluetoothLowEnergyDevices(
       case kError:
         return false;
       case kOk:
-        devices->push_back(device_info.release());
+        devices->push_back(device_info.Pass());
     }
   }
 }
