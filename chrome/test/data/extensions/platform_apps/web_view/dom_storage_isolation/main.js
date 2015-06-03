abf5782bc2655d39fb88152ff55934265da41b79
ekaramad <ekaramad@chromium.org>
2015-06-03 11:06:59 -0700

    Moved the "WebViewTest.DOMStorageIsolation" browser test from C++ to mostly in Javascript.
    The part still remaining in C++ is the test to verify isolation of
    browser tab's DOM storage from that of the webviews.
    
    BUG=248873
    
    Review URL: https://codereview.chromium.org/1161843006
    
    Cr-Commit-Position: refs/heads/master@{#332642}

---
+++ chrome/test/data/extensions/platform_apps/web_view/dom_storage_isolation/main.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function createWindow() {
+  chrome.app.window.create('window.html', {
+    'bounds': {
+      'width': 400,
+      'height': 400
+    }
+  });
+}
+chrome.app.runtime.onLaunched.addListener(createWindow);
---
+++ chrome/test/data/extensions/platform_apps/web_view/dom_storage_isolation/message_titles.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var INIT_DOM_STORAGE = 'Initialize DOM Storage';
+var INIT_DOM_STORAGE_COMPLETE = 'Initialization Complete';
+var GET_DOM_STORAGE_INFO = 'Get DOM Storage Info.';
+var GET_DOM_STORAGE_INFO_COMPLETE = 'DOM Storage Info Ready.';
