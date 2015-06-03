4a16280ef2e74903213a818ecbfaa2dafcef8fcc
shess <shess@chromium.org>
2015-06-02 17:19:32 -0700

    [sql] Stats gathering for sql/ APIs.
    Generate stats for how many SQL statements are executed, how many
    results they return, and how many changes they make.  Generate timing
    values for how long is spent doing all queries, doing updating
    operations, doing autocommit updates, and commiting transactions.
    
    The goal of these metrics is to quantify results of decisions like
    enabling write-ahead log or memory-mapped I/O.
    
    BUG=489788,489444
    
    Review URL: https://codereview.chromium.org/1145833002
    
    Cr-Commit-Position: refs/heads/master@{#332503}

--- storage/browser/database/database_tracker.cc
+++ storage/browser/database/database_tracker.cc
@@ -460,6 +460,8 @@ bool DatabaseTracker::LazyInit() {
       }
     }
 
+    db_->set_histogram_tag("DatabaseTracker");
+
     // If the tracker database exists, but it's corrupt or doesn't
     // have a meta table, delete the database directory.
     const base::FilePath kTrackerDatabaseFullPath =
@@ -472,8 +474,6 @@ bool DatabaseTracker::LazyInit() {
       if (!base::DeleteFile(db_dir_, true))
         return false;
     }
-
-    db_->set_histogram_tag("DatabaseTracker");
 
     databases_table_.reset(new DatabasesTable(db_.get()));
     meta_table_.reset(new sql::MetaTable());
