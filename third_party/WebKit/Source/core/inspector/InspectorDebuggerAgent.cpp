1c3445a0b34e957f8149757f1adaf4b8042d8826
sigbjornf@opera.com <sigbjornf@opera.com>
2015-06-03 20:23:16 +0000

    Oilpan: fix build after r196422.
    TBR=oilpan-reviews
    BUG=466631
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1156463003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196426 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorDebuggerAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorDebuggerAgent.cpp
@@ -134,7 +134,7 @@ static PassRefPtrWillBeRawPtr<ScriptCallStack> toScriptCallStack(JavaScriptCallF
 
 static PassRefPtrWillBeRawPtr<ScriptCallStack> toScriptCallStack(const ScriptValue& callFrames)
 {
-    RefPtrWillBeRawPtr<JavaScriptCallFrame> jsCallFrame = V8Debugger::toJavaScriptCallFrameUnsafe(callFrames);
+    RefPtr<JavaScriptCallFrame> jsCallFrame = V8Debugger::toJavaScriptCallFrameUnsafe(callFrames);
     return jsCallFrame ? toScriptCallStack(jsCallFrame.get()) : nullptr;
 }
 
@@ -489,7 +489,7 @@ bool InspectorDebuggerAgent::isCallStackEmptyOrBlackboxed()
 {
     ASSERT(enabled());
     for (int index = 0; ; ++index) {
-        RefPtrWillBeRawPtr<JavaScriptCallFrame> frame = debugger().callFrameNoScopes(index);
+        RefPtr<JavaScriptCallFrame> frame = debugger().callFrameNoScopes(index);
         if (!frame)
             break;
         if (!isCallFrameWithUnknownScriptOrBlackboxed(frame.release()))
@@ -504,9 +504,9 @@ bool InspectorDebuggerAgent::isTopCallFrameBlackboxed()
     return isCallFrameWithUnknownScriptOrBlackboxed(debugger().callFrameNoScopes(0));
 }
 
-bool InspectorDebuggerAgent::isCallFrameWithUnknownScriptOrBlackboxed(PassRefPtrWillBeRawPtr<JavaScriptCallFrame> pFrame)
+bool InspectorDebuggerAgent::isCallFrameWithUnknownScriptOrBlackboxed(PassRefPtr<JavaScriptCallFrame> pFrame)
 {
-    RefPtrWillBeRawPtr<JavaScriptCallFrame> frame = pFrame;
+    RefPtr<JavaScriptCallFrame> frame = pFrame;
     if (!frame)
         return true;
     ScriptsMap::iterator it = m_scripts.find(String::number(frame->sourceID()));
@@ -798,7 +798,7 @@ void InspectorDebuggerAgent::stepOver(ErrorString* errorString)
     if (!assertPaused(errorString))
         return;
     // StepOver at function return point should fallback to StepInto.
-    RefPtrWillBeRawPtr<JavaScriptCallFrame> frame = debugger().callFrameNoScopes(0);
+    RefPtr<JavaScriptCallFrame> frame = debugger().callFrameNoScopes(0);
     if (frame && frame->isAtReturn()) {
         stepInto(errorString);
         return;
@@ -1457,7 +1457,7 @@ PassRefPtrWillBeRawPtr<ScriptAsyncCallStack> InspectorDebuggerAgent::currentAsyn
         return nullptr;
     RefPtrWillBeRawPtr<ScriptAsyncCallStack> result = nullptr;
     for (AsyncCallStackVector::const_reverse_iterator it = callStacks.rbegin(); it != callStacks.rend(); ++it) {
-        RefPtrWillBeRawPtr<JavaScriptCallFrame> callFrame = V8Debugger::toJavaScriptCallFrameUnsafe((*it)->callFrames());
+        RefPtr<JavaScriptCallFrame> callFrame = V8Debugger::toJavaScriptCallFrameUnsafe((*it)->callFrames());
         if (!callFrame)
             break;
         result = ScriptAsyncCallStack::create((*it)->description(), toScriptCallStack(callFrame.get()), result.release());
