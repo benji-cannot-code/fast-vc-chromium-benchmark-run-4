b9ef58a2db6b7f5cbee2a2731f75ce796826da84
simonhatch <simonhatch@chromium.org>
2015-06-03 07:04:00 -0700

    Add high cpu usage triggers to performance_monitor.
    This adds triggers for tracing when we hit a sustained high cpu situation on either the browser, gpu, or plugin processes.
    
    BUG=495239
    
    Review URL: https://codereview.chromium.org/1157243005
    
    Cr-Commit-Position: refs/heads/master@{#332609}

--- chrome/browser/performance_monitor/process_metrics_history.h
+++ chrome/browser/performance_monitor/process_metrics_history.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/linked_ptr.h"
 #include "base/process/process_handle.h"
+#include "content/public/browser/background_tracing_manager.h"
 #include "content/public/browser/child_process_data.h"
 #include "content/public/common/process_type.h"
 
@@ -65,6 +66,8 @@ class ProcessMetricsHistory {
   int last_update_sequence_;
 
   double cpu_usage_;
+
+  content::BackgroundTracingManager::TriggerHandle trace_trigger_handle_;
 
   DISALLOW_ASSIGN(ProcessMetricsHistory);
 };
