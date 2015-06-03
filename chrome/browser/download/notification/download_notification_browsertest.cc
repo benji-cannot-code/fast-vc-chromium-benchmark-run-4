07817f01064a10d85babd4b27d76342d883f0cac
yoshiki <yoshiki@chromium.org>
2015-06-03 01:11:58 -0700

    [Download Notification] Cleanup: Remove Unused Constructors
    They has been added accidentally...
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1154043002
    
    Cr-Commit-Position: refs/heads/master@{#332564}

--- chrome/browser/download/notification/download_notification_browsertest.cc
+++ chrome/browser/download/notification/download_notification_browsertest.cc
@@ -100,9 +100,7 @@ class MessageCenterChangeObserver
 // Class observing of "ADD" notification events.
 class NotificationAddObserver : public MessageCenterChangeObserver {
  public:
-  NotificationAddObserver() : count_(1) {
-    MessageCenterChangeObserver();
-  }
+  NotificationAddObserver() : count_(1) {}
   explicit NotificationAddObserver(int count) : count_(count) {
     MessageCenterChangeObserver();
   }
@@ -142,9 +140,7 @@ class NotificationAddObserver : public MessageCenterChangeObserver {
 // Class observing of "UPDATE" notification events.
 class NotificationUpdateObserver : public MessageCenterChangeObserver {
  public:
-  NotificationUpdateObserver() {
-    MessageCenterChangeObserver();
-  }
+  NotificationUpdateObserver() {}
   ~NotificationUpdateObserver() override {}
 
   std::string Wait() {
@@ -176,9 +172,7 @@ class NotificationUpdateObserver : public MessageCenterChangeObserver {
 // Class observing of "REMOVE" notification events.
 class NotificationRemoveObserver : public MessageCenterChangeObserver {
  public:
-  NotificationRemoveObserver() {
-    MessageCenterChangeObserver();
-  }
+  NotificationRemoveObserver() {}
   ~NotificationRemoveObserver() override {}
 
   std::string Wait() {
