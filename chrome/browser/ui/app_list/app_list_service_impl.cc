fde22787258c50ad1813efb9963c6d9c7761f7f8
anthonyvd <anthonyvd@chromium.org>
2015-06-03 11:25:58 -0700

    Prevent the System Profile from being used as the last used profile.
    BUG=486914
    
    Review URL: https://codereview.chromium.org/1150803003
    
    Cr-Commit-Position: refs/heads/master@{#332648}

--- chrome/browser/ui/app_list/app_list_service_impl.cc
+++ chrome/browser/ui/app_list/app_list_service_impl.cc
@@ -217,8 +217,9 @@ bool IsValidProfileName(const std::string& profile_name) {
   if (profile_name.empty())
     return false;
 
-  return profile_name !=
-         base::FilePath(chrome::kGuestProfileDir).AsUTF8Unsafe();
+  return
+      profile_name != base::FilePath(chrome::kGuestProfileDir).AsUTF8Unsafe() &&
+      profile_name != base::FilePath(chrome::kSystemProfileDir).AsUTF8Unsafe();
 }
 
 }  // namespace
