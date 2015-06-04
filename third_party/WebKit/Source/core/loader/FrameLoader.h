7cb919829dd67ed2ab0620772d5676a395bf776d
japhet@chromium.org <japhet@chromium.org>
2015-06-04 21:30:38 +0000

    Don't reuse HistoryItems for multiple navigations
    Explicitly propagate shared state instead of implicitly doing so via HistoryItem reuse in FrameLoader::setHistoryItemStateForCommit().
    
    If, for example, a page calls replaceState() while a back/forward navigation is in progress, it would co-op the provisional history item for the back/forward navigation and set its scroll state on it. This state would then get reused when the back/forward navigation actually committed.
    
    BUG=481393
    TEST=fast/history/resources/replaceState-during-beforeunload.html
    
    Review URL: https://codereview.chromium.org/1159723003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196532 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/FrameLoader.h
+++ third_party/WebKit/Source/core/loader/FrameLoader.h
@@ -172,7 +172,6 @@ public:
 
     HistoryItem* currentItem() const { return m_currentItem.get(); }
     void saveScrollState();
-    void clearScrollPositionAndViewState();
 
     void restoreScrollPositionAndViewState();
 
@@ -193,8 +192,12 @@ private:
 
     void startLoad(FrameLoadRequest&, FrameLoadType, NavigationPolicy);
 
-
-    void setHistoryItemStateForCommit(HistoryCommitType, bool isPushOrReplaceState = false, HistoryScrollRestorationType = ScrollRestorationAuto, PassRefPtr<SerializedScriptValue> = nullptr);
+    enum class HistoryNavigationType {
+        DifferentDocument,
+        Fragment,
+        HistoryApi
+    };
+    void setHistoryItemStateForCommit(HistoryCommitType, HistoryNavigationType);
 
     void loadInSameDocument(const KURL&, PassRefPtr<SerializedScriptValue> stateObject, FrameLoadType, ClientRedirectPolicy);
 
--- third_party/WebKit/Source/core/loader/HistoryItem.h
+++ third_party/WebKit/Source/core/loader/HistoryItem.h
@@ -54,11 +54,6 @@ public:
     }
     ~HistoryItem();
 
-    // Used when the frame this item represents was navigated to a different
-    // url but a new item wasn't created.
-    void generateNewItemSequenceNumber();
-    void generateNewDocumentSequenceNumber();
-
     const String& urlString() const;
     KURL url() const;
 
@@ -72,7 +67,6 @@ public:
     void setPinchViewportScrollPoint(const FloatPoint&);
     const IntPoint& scrollPoint() const;
     void setScrollPoint(const IntPoint&);
-    void clearScrollPoint();
 
     float pageScaleFactor() const;
     void setPageScaleFactor(float);
