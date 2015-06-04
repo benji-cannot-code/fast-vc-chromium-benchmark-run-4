af2dcf04e1233390c17088861844d60d733e3f69
sigbjornf@opera.com <sigbjornf@opera.com>
2015-06-04 19:14:44 +0000

    Oilpan: fix build after r196513.
    TBR=oilpan-reviews
    BUG=466631
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1171463003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196524 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/inspector/V8InjectedScriptHost.h
+++ third_party/WebKit/Source/bindings/core/v8/inspector/V8InjectedScriptHost.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef V8InjectedScriptHost_h
 #define V8InjectedScriptHost_h
 
+#include "platform/heap/Handle.h"
 #include "wtf/Forward.h"
 #include <v8.h>
 
@@ -15,7 +16,7 @@ class InjectedScriptHost;
 
 class V8InjectedScriptHost {
 public:
-    static v8::Local<v8::Object> wrap(v8::Isolate*, v8::Local<v8::FunctionTemplate> constructorTemplate, PassRefPtr<InjectedScriptHost>);
+    static v8::Local<v8::Object> wrap(v8::Isolate*, v8::Local<v8::FunctionTemplate> constructorTemplate, PassRefPtrWillBeRawPtr<InjectedScriptHost>);
     static InjectedScriptHost* toImpl(v8::Local<v8::Object>);
     static v8::Local<v8::FunctionTemplate> createWrapperTemplate(v8::Isolate*);
 
