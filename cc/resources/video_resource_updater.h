02cb7548bc55ef3c606ff646a3e1fb0a49965cc8
vmpstr <vmpstr@chromium.org>
2015-06-03 18:38:51 -0700

    cc: Fix size_t to int truncations in resources/ scheduler/
    This patch fixes size_t to int truncations in resources/ and scheduler/
    in cc.
    
    R=danakj
    BUG=167187
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1160433005
    
    Cr-Commit-Position: refs/heads/master@{#332761}

--- cc/resources/video_resource_updater.h
+++ cc/resources/video_resource_updater.h
@@ -89,7 +89,7 @@ class CC_EXPORT VideoResourceUpdater
     // frame pointer will only be used for pointer comparison, i.e. the
     // underlying data will not be accessed.
     const void* frame_ptr;
-    int plane_index;
+    size_t plane_index;
     base::TimeDelta timestamp;
 
     PlaneResource(unsigned resource_id,
@@ -100,10 +100,10 @@ class CC_EXPORT VideoResourceUpdater
 
   static bool PlaneResourceMatchesUniqueID(const PlaneResource& plane_resource,
                                            const media::VideoFrame* video_frame,
-                                           int plane_index);
+                                           size_t plane_index);
 
   static void SetPlaneResourceUniqueId(const media::VideoFrame* video_frame,
-                                       int plane_index,
+                                       size_t plane_index,
                                        PlaneResource* plane_resource);
 
   // This needs to be a container where iterators can be erased without
