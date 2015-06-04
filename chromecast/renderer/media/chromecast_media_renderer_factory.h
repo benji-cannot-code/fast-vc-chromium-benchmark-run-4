8aba3f9de63895c62657a5d17821a1ad0fbcf115
servolk <servolk@chromium.org>
2015-06-03 17:29:21 -0700

    Chromecast: fix accidental dangling reference in CC renderer factory
    The last parameter of media::DefaultRendererFactory is audio_config,
    which is passed by reference and is assumed to be owned by the caller.
    Currently we are passing a reference to local object in there, which
    creates dangling reference after we leave local scope. We need to
    own and keep AudioHardwareConfig object alive as long as
    DefaultRendererFactory is alive.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1167873002
    
    Cr-Commit-Position: refs/heads/master@{#332750}

--- chromecast/renderer/media/chromecast_media_renderer_factory.h
+++ chromecast/renderer/media/chromecast_media_renderer_factory.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/renderer_factory.h"
 
 namespace media {
+class AudioHardwareConfig;
 class MediaLog;
 class DefaultRendererFactory;
 }
@@ -35,6 +36,9 @@ class ChromecastMediaRendererFactory : public ::media::RendererFactory {
   int render_frame_id_;
   scoped_refptr<::media::MediaLog> media_log_;
   scoped_ptr<::media::DefaultRendererFactory> default_render_factory_;
+
+  // Audio config for the default media renderer.
+  scoped_ptr<::media::AudioHardwareConfig> audio_config_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromecastMediaRendererFactory);
 };
