02e7f405f77db2f20a5e54ca3dfc64f11a644fe0
jlklein <jlklein@chromium.org>
2015-06-03 19:07:30 -0700

    Fix a couple bugs in the settings_page_header wrt breadcrumbs.
    1) Do not clear the pagestack when selectedPage is undefined.
    2) Use the last item in the stack for currentPageTitle instead of the first.
    3) Update to MoreRouting.urlFor to adjust for more-routing changes for 0.8.
    
    BUG=495817
    
    Review URL: https://codereview.chromium.org/1169473002
    
    Cr-Commit-Position: refs/heads/master@{#332765}

--- chrome/browser/resources/settings/settings_page/settings_page_header.js
+++ chrome/browser/resources/settings/settings_page/settings_page_header.js
@@ -82,7 +82,7 @@ Polymer({
    * @private
    */
   urlFor_: function(page) {
-    return page.route ? urlFor(page.route) : '';
+    return page.route ? MoreRouting.urlFor(page.route) : '';
   },
 
   /**
@@ -93,7 +93,7 @@ Polymer({
   getCurrentPageTitle_: function(pageStack) {
     if (pageStack.length == 0)
       return '';
-    return pageStack[0].pageTitle;
+    return pageStack[pageStack.length - 1].pageTitle;
   },
 
   /** @private */
@@ -104,8 +104,6 @@ Polymer({
       this.pageStack = this.pageStack.concat(this.selectedPage);
     } else if (this.selectedPage) {
       this.pageStack = [this.selectedPage];
-    } else {
-      this.pageStack = [];
     }
   },
 
