c897bc6ffd015d353503f4aec83c2270a21e3139
bokan@chromium.org <bokan@chromium.org>
2015-06-04 14:04:01 +0000

    Remove setMainFrameScrollOffset method from WebViewImpl.
    Whoops, forgot to remove the declaration in r196421
    
    BUG=
    TBR=skobes@chromium.org
    
    Review URL: https://codereview.chromium.org/1148263006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196503 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebViewImpl.h
+++ third_party/WebKit/Source/web/WebViewImpl.h
@@ -274,8 +274,6 @@ public:
     void enableViewport();
     void disableViewport();
 
-    void setMainFrameScrollOffset(const WebPoint&);
-
     float defaultMinimumPageScaleFactor() const;
     float defaultMaximumPageScaleFactor() const;
     float minimumPageScaleFactor() const;
