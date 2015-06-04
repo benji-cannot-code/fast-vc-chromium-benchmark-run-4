d71d3a0cfc08297d7b1e43e7ee063e2b22dc3ba9
oetuaho <oetuaho@nvidia.com>
2015-06-04 10:46:27 -0700

    Enable ANGLE workaround for NVIDIA GLSL compiler pow bug
    The bug is known to affect the compiler in at least NVIDIA 331.x drivers,
    which are widespread on Linux. Newest NVIDIA drivers should have a fix
    for this issue.
    
    TEST=WebGL conformance tests
    BUG=477306
    
    Review URL: https://codereview.chromium.org/1157293005
    
    Cr-Commit-Position: refs/heads/master@{#332854}

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -95,6 +95,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          needs_offscreen_buffer_workaround)                  \
   GPU_OP(REGENERATE_STRUCT_NAMES,                            \
          regenerate_struct_names)                            \
+  GPU_OP(REMOVE_POW_WITH_CONSTANT_EXPONENT,                  \
+         remove_pow_with_constant_exponent)                  \
   GPU_OP(RESTORE_SCISSOR_ON_FBO_CHANGE,                      \
          restore_scissor_on_fbo_change)                      \
   GPU_OP(REVERSE_POINT_SPRITE_COORD_ORIGIN,                  \
