c05584fd2c87ed168871f348d8eaa92e9bddf0fa
mikhail.pozdnyakov@intel.com <mikhail.pozdnyakov@intel.com>
2015-06-04 08:06:03 +0000

    Speed up case folding for 8-bit strings
    From http://trac.webkit.org/changeset/160954 by Anders Carlsson  <andersca@apple.com>
    
    "Add a case folding lookup table for 8-bit strings and use it instead of calling down to u_foldCase.
    On a simple microbenchmark using a lookup table is about 15x faster."
    
    The Intel Vtune Amplifier profiler showed 'ucase_fold()' calls among the top hotspots by CPU usage when profile the 'modify-element-classname.html' performance test and these calls were from 'WTF::equalIgnoringCase(const LChar* a, const LChar* b, unsigned length)'.
    
    This patch optimizes 'WTF::equalIgnoringCase()' so that the test's median time value falls approximately by 12% and the profiler shows the following delta of the total CPU time for the whole test execution: 3.550s - 3.145s = 0.405s
    
    Considering that 'WTF::equalIgnoringCase()' is a generic function and it is used very intensively in many places we can anticipate significant performance improvements for other scenarios as well.
    
    Review URL: https://codereview.chromium.org/1152163005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196478 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/text/StringHash.h
+++ third_party/WebKit/Source/wtf/text/StringHash.h
@@ -127,6 +127,8 @@ namespace WTF {
         // correctly-folded code point in all cases (see comment below).
         template<typename T> static inline UChar foldCase(T ch)
         {
+            if (IsSameType<T, LChar>::value)
+                return StringImpl::latin1CaseFoldTable[ch];
             // It's possible for WTF::Unicode::foldCase() to return a 32-bit
             // value that's not representable as a UChar.  However, since this
             // is rare and deterministic, and the result of this is merely used
