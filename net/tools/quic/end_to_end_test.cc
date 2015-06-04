c1f31251670c1bbf1f1260e362eda95fa34b67cc
rtenneti <rtenneti@chromium.org>
2015-06-03 18:53:51 -0700

    QUIC - enable LargePostSmallBandwidthLargeBuffer unittest.
    We had commented out "EXPECT_EQ(0u, client_stats.packets_lost);"
    in the internal source tree and that change is in chromium.
    
    BUG=374871
    R=rch@chromium.org
    TBR=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1155623003
    
    Cr-Commit-Position: refs/heads/master@{#332763}

--- net/tools/quic/end_to_end_test.cc
+++ net/tools/quic/end_to_end_test.cc
@@ -714,9 +714,7 @@ TEST_P(EndToEndTest, CorrectlyConfiguredFec) {
             client_->GetOrCreateStream()->fec_policy());
 }
 
-// TODO(shess): This is flaky on ChromiumOS bots.
-// http://crbug.com/374871
-TEST_P(EndToEndTest, DISABLED_LargePostSmallBandwidthLargeBuffer) {
+TEST_P(EndToEndTest, LargePostSmallBandwidthLargeBuffer) {
   ASSERT_TRUE(Initialize());
   SetPacketSendDelay(QuicTime::Delta::FromMicroseconds(1));
   // 256KB per second with a 256KB buffer from server to client.  Wireless
@@ -730,8 +728,7 @@ TEST_P(EndToEndTest, DISABLED_LargePostSmallBandwidthLargeBuffer) {
   string body;
   GenerateBody(&body, 1024 * 1024);
 
-  HTTPMessage request(HttpConstants::HTTP_1_1,
-                      HttpConstants::POST, "/foo");
+  HTTPMessage request(HttpConstants::HTTP_1_1, HttpConstants::POST, "/foo");
   request.AddBody(body, true);
 
   EXPECT_EQ(kFooResponseBody, client_->SendCustomSynchronousRequest(request));
@@ -787,8 +784,7 @@ TEST_P(EndToEndTest, InvalidStream) {
   string body;
   GenerateBody(&body, kMaxPacketSize);
 
-  HTTPMessage request(HttpConstants::HTTP_1_1,
-                      HttpConstants::POST, "/foo");
+  HTTPMessage request(HttpConstants::HTTP_1_1, HttpConstants::POST, "/foo");
   request.AddBody(body, true);
   // Force the client to write with a stream ID belonging to a nonexistent
   // server-side stream.
