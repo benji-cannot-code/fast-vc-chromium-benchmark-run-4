42c8c1c1c0207279d89d5e653b2403ae2adac825
dongseong.hwang <dongseong.hwang@intel.com>
2015-06-04 05:39:06 -0700

    drm: Make DrmDisplayHostManager handle VGEM fd by itself.
    GPU process doesn't need VGEM fd, so DrmDisplayHostManager doesn't send it to
    GPU process.
    
    This CL fixes a subtle bug which is that VGEM device is corrupted because of
    invalid ioctl call to VGEM device. After this CL, following error log disappears;
    [ERROR:drm_device_manager.cc(54)] Could not initialize DRM device for /dev/dri/card1
    
    It's part of "native GpuMemoryBuffer on ChromeOS Freon" implementation.
     * Design doc: https://docs.google.com/document/d/1qpLLo4mBkzHBh5cuBtBjJZAzXK2X9BgBJtpESh-mNn8
    
    TEST=Run chromeos using amd64-generic_freon image
    BUG=475633
    
    Review URL: https://codereview.chromium.org/1151533002
    
    Cr-Commit-Position: refs/heads/master@{#332822}

--- ui/ozone/platform/drm/host/drm_display_host_manager.h
+++ ui/ozone/platform/drm/host/drm_display_host_manager.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <queue>
 #include <set>
 
-#include "base/files/file_path.h"
+#include "base/files/scoped_file.h"
 #include "base/memory/scoped_vector.h"
 #include "base/memory/weak_ptr.h"
 #include "ui/display/types/native_display_delegate.h"
@@ -99,6 +99,9 @@ class DrmDisplayHostManager : public DeviceEventObserver,
   // with the GPU process trying to open it and aquire DRM master.
   base::FilePath primary_graphics_card_path_;
 
+  // File path for virtual gem (VGEM) device.
+  base::FilePath vgem_card_path_;
+
   // Keeps track if there is a dummy display. This happens on initialization
   // when there is no connection to the GPU to update the displays.
   bool has_dummy_display_;
@@ -126,6 +129,9 @@ class DrmDisplayHostManager : public DeviceEventObserver,
   // This is used to cache the primary DRM device until the channel is
   // established.
   scoped_ptr<DrmDeviceHandle> primary_drm_device_handle_;
+
+  // Manages the VGEM device by itself and doesn't send it to GPU process.
+  base::ScopedFD vgem_card_device_file_;
 
   base::WeakPtrFactory<DrmDisplayHostManager> weak_ptr_factory_;
 
