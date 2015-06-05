013864bcf5bd5b799187735a5b4efdd567965df2
enne <enne@chromium.org>
2015-06-04 20:43:15 -0700

    Fix SurfaceLayerImpl scale under impl-side painting
    With impl-side painting turned on (which is really property trees),
    Layer::CalculateContentsScale isn't run and Layer::PushPropertiesTo
    clobbers contents scale to 1.f, and content bounds to bounds.
    
    The problem was that the renderer would submit quads at scale=2 and the
    browser SurfaceLayerImpl would also see that it had a DSF would then
    scale its surface quad.  To fix this, the SurfaceLayerImpl needs to
    apply the inverse scale of its surface when generating quads.
    
    Ideally, SurfaceAggregator could be changed in the future to just always
    unapply the DSF of the frame and reduce plumbing, but in the short term,
    this is a minimal change to ship impl-side painting.
    
    This change is similar to https://codereview.chromium.org/913243003.
    
    BUG=495142
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1166983007
    
    Cr-Commit-Position: refs/heads/master@{#333004}

--- cc/layers/surface_layer_impl.h
+++ cc/layers/surface_layer_impl.h
@@ -21,6 +21,8 @@ class CC_EXPORT SurfaceLayerImpl : public LayerImpl {
   ~SurfaceLayerImpl() override;
 
   void SetSurfaceId(SurfaceId surface_id);
+  void SetSurfaceScale(float scale);
+  void SetSurfaceSize(const gfx::Size& size);
 
   // LayerImpl overrides.
   scoped_ptr<LayerImpl> CreateLayerImpl(LayerTreeImpl* tree_impl) override;
@@ -37,6 +39,8 @@ class CC_EXPORT SurfaceLayerImpl : public LayerImpl {
   const char* LayerTypeAsString() const override;
 
   SurfaceId surface_id_;
+  gfx::Size surface_size_;
+  float surface_scale_;
 
   DISALLOW_COPY_AND_ASSIGN(SurfaceLayerImpl);
 };
