3c76b6838157b1a1319b692d752087f1ba30f988
sbc <sbc@chromium.org>
2015-06-04 17:07:04 -0700

    Add mips support to install-debian.wheezy.sysroot.py
    Also, fix remaining issues with sysroot-creator.sh
    WRT mips.  Not sure I missed those the first time.
    
    TEST=GYP_DEFINES=target_arch=mipsel gclient runhooks
    
    Review URL: https://codereview.chromium.org/1164073004
    
    Cr-Commit-Position: refs/heads/master@{#332972}

--- chrome/installer/linux/sysroot_scripts/sysroot-creator.sh
+++ chrome/installer/linux/sysroot_scripts/sysroot-creator.sh
@@ -256,7 +256,7 @@ HacksAndPatchesAmd64() {
   lscripts="${INSTALL_ROOT}/usr/lib/x86_64-linux-gnu/libpthread.so \
             ${INSTALL_ROOT}/usr/lib/x86_64-linux-gnu/libc.so"
 
-  #SubBanner "Rewriting Linker Scripts"
+  # Rewrite linker scripts
   sed -i -e 's|/usr/lib/x86_64-linux-gnu/||g'  ${lscripts}
   sed -i -e 's|/lib/x86_64-linux-gnu/||g' ${lscripts}
 
@@ -280,7 +280,7 @@ HacksAndPatchesI386() {
   lscripts="${INSTALL_ROOT}/usr/lib/i386-linux-gnu/libpthread.so \
             ${INSTALL_ROOT}/usr/lib/i386-linux-gnu/libc.so"
 
-  #SubBanner "Rewriting Linker Scripts"
+  # Rewrite linker scripts
   sed -i -e 's|/usr/lib/i386-linux-gnu/||g'  ${lscripts}
   sed -i -e 's|/lib/i386-linux-gnu/||g' ${lscripts}
 
@@ -304,7 +304,7 @@ HacksAndPatchesARM() {
   lscripts="${INSTALL_ROOT}/usr/lib/arm-linux-gnueabihf/libpthread.so \
             ${INSTALL_ROOT}/usr/lib/arm-linux-gnueabihf/libc.so"
 
-  #SubBanner "Rewriting Linker Scripts"
+  # Rewrite linker scripts
   sed -i -e 's|/usr/lib/arm-linux-gnueabihf/||g' ${lscripts}
   sed -i -e 's|/lib/arm-linux-gnueabihf/||g' ${lscripts}
 
@@ -313,6 +313,25 @@ HacksAndPatchesARM() {
   SubBanner "Package Configs Symlink"
   mkdir -p ${INSTALL_ROOT}/usr/share
   ln -s ../lib/arm-linux-gnueabihf/pkgconfig ${INSTALL_ROOT}/usr/share/pkgconfig
+}
+
+
+HacksAndPatchesMips() {
+  Banner "Misc Hacks & Patches"
+  # these are linker scripts with absolute pathnames in them
+  # which we rewrite here
+  lscripts="${INSTALL_ROOT}/usr/lib/mipsel-linux-gnu/libpthread.so \
+            ${INSTALL_ROOT}/usr/lib/mipsel-linux-gnu/libc.so"
+
+  # Rewrite linker scripts
+  sed -i -e 's|/usr/lib/mipsel-linux-gnu/||g' ${lscripts}
+  sed -i -e 's|/lib/mipsel-linux-gnu/||g' ${lscripts}
+
+  # This is for chrome's ./build/linux/pkg-config-wrapper
+  # which overwrites PKG_CONFIG_PATH internally
+  SubBanner "Package Configs Symlink"
+  mkdir -p ${INSTALL_ROOT}/usr/share
+  ln -s ../lib/mipsel-linux-gnu/pkgconfig ${INSTALL_ROOT}/usr/share/pkgconfig
 }
 
 
@@ -364,7 +383,7 @@ CleanupJailSymlinks() {
     case "${link}" in
       usr/lib/gcc/x86_64-linux-gnu/4.*/* | usr/lib/gcc/i486-linux-gnu/4.*/* | \
       usr/lib/gcc/arm-linux-gnueabihf/4.*/* | \
-      usr/lib/gcc/misel-linux-gnu/4.*/*)
+      usr/lib/gcc/mipsel-linux-gnu/4.*/*)
         # Relativize the symlink.
         ln -snfv "../../../../..${target}" "${link}"
         ;;
