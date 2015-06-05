65d784f9c523532163c3054001d274a0c80c79be
sky <sky@chromium.org>
2015-06-05 08:36:28 -0700

    Changes html_viewer to get services from appropriate application
    There are actually two changes here:
    . Convert HTMLViewerApplication to use an ApplicationImpl rather than
      be an Application.
    . Make HTMLDocument use services from the application it was created
      for rather than HTMLViewer's application.
    
    BUG=none
    TEST=none
    R=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/1165013005
    
    Cr-Commit-Position: refs/heads/master@{#333050}

--- mojo/application/public/cpp/application_impl.h
+++ mojo/application/public/cpp/application_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback.h"
+#include "base/memory/weak_ptr.h"
 #include "mojo/application/public/cpp/app_lifetime_helper.h"
 #include "mojo/application/public/cpp/application_connection.h"
 #include "mojo/application/public/cpp/application_delegate.h"
@@ -134,6 +135,7 @@ class ApplicationImpl : public Application,
   base::Closure termination_closure_;
   AppLifetimeHelper app_lifetime_helper_;
   bool quit_requested_;
+  base::WeakPtrFactory<ApplicationImpl> weak_factory_;
 
   MOJO_DISALLOW_COPY_AND_ASSIGN(ApplicationImpl);
 };
