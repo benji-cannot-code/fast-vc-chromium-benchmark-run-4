48e488777ad5798e78930031046de94ec6087a22
reed@google.com <reed@google.com>
2015-06-05 12:26:39 +0000

    remove unused android-specific includes
    BUG=
    
    Review URL: https://codereview.chromium.org/1167003003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196570 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebMediaPlayerClientImpl.cpp
+++ third_party/WebKit/Source/web/WebMediaPlayerClientImpl.cpp
@@ -35,14 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "web/WebLocalFrameImpl.h"
 #include "web/WebViewImpl.h"
 
-#if OS(ANDROID)
-#include "GrContext.h"
-#include "GrTypes.h"
-#include "SkCanvas.h"
-#include "SkGrPixelRef.h"
-#endif
-
-
 #include "wtf/Assertions.h"
 #include "wtf/text/CString.h"
 
