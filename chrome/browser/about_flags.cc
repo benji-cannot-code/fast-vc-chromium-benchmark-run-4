63d09f17b14905d31d4169db86805d098abdce69
mukai <mukai@chromium.org>
2015-06-05 15:37:05 -0700

    Reveals --ash-enable-fullscreen-app-list flag to chrome://flags.
    BUG=434113
    R=oshima@chromium.org
    TBR=jwd@chromium.org
    TEST=manually
    
    Review URL: https://codereview.chromium.org/1146303008
    
    Cr-Commit-Position: refs/heads/master@{#333156}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1433,6 +1433,13 @@ const Experiment kExperiments[] = {
     kOsCrOS,
     MULTI_VALUE_TYPE(kGestureEditingChoices)
   },
+  {
+    "enable-fullscreen-app-list",
+    IDS_FLAGS_FULLSCREEN_APP_LIST_NAME,
+    IDS_FLAGS_FULLSCREEN_APP_LIST_DESCRIPTION,
+    kOsCrOS,
+    SINGLE_VALUE_TYPE(ash::switches::kAshEnableFullscreenAppList)
+  },
 #endif
   {
     "enable-simple-cache-backend",
