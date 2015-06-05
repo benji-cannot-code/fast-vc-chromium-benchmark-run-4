c0c11fa396719fa0e0fdced8125990ec79a71653
zea <zea@chromium.org>
2015-06-05 10:32:39 -0700

    Revert of ios: Remove use of MessageLoopProxy and deprecated MessageLoop APIs (patchset #3 id:40001 of https://codereview.chromium.org/1160403005/)
    Reason for revert:
    Broke IOS_Device compile.
    https://build.chromium.org/p/chromium.mac/builders/iOS_Device/builds/8802
    
    Original issue's description:
    > ios: Remove use of MessageLoopProxy and deprecated MessageLoop APIs
    >
    > BUG=465354
    >
    > Committed: https://crrev.com/9c8e5d7259661cda32b698ee88254491e6984900
    > Cr-Commit-Position: refs/heads/master@{#333080}
    
    TBR=stuartmorgan@chromium.org,skyostil@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=465354
    
    Review URL: https://codereview.chromium.org/1167533009
    
    Cr-Commit-Position: refs/heads/master@{#333083}

--- ios/web/web_thread_adapter.cc
+++ ios/web/web_thread_adapter.cc
@@ -165,9 +165,9 @@ bool WebThread::GetCurrentThreadIdentifier(ID* identifier) {
 }
 
 // static
-scoped_refptr<base::SingleThreadTaskRunner> WebThread::GetTaskRunnerForThread(
+scoped_refptr<base::MessageLoopProxy> WebThread::GetMessageLoopProxyForThread(
     ID identifier) {
-  return content::BrowserThread::GetTaskRunnerForThread(
+  return content::BrowserThread::GetMessageLoopProxyForThread(
       BrowserThreadIDFromWebThreadID(identifier));
 }
 
