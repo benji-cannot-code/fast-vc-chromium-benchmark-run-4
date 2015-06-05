16fd56e928e2b55d4bc0834939b0bcfcd23fca98
fs@opera.com <fs@opera.com>
2015-06-05 17:38:58 +0000

    Include 'outline' in LayoutSVGBlock::visualOverflowRect
    LayoutSVGBlock doesn't maintain overflow via LayoutBox::m_overflow, but
    rather overrides LayoutBox::visualOverflowRect.
    Instead of doing that, add visual effect overflow during layout of
    LayoutSVGText and stop overriding said method.
    This will include outsets for border-image (which doesn't apply
    to any of <text> or <foreignObject>) too in the unlikely event it's
    specified.
    
    TEST=svg/text/text-outline-2.html (S.P. strict cull rect)
    BUG=495368
    
    Review URL: https://codereview.chromium.org/1158323005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGText.cpp
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGText.cpp
@@ -369,7 +369,6 @@ void LayoutSVGText::layout()
     ASSERT(!scrollsOverflow());
     ASSERT(!hasControlClip());
     ASSERT(!positionedObjects());
-    ASSERT(!m_overflow);
     ASSERT(!isAnonymousBlock());
 
     if (!firstChild())
@@ -396,6 +395,9 @@ void LayoutSVGText::layout()
     // If we don't have any line boxes, then make sure the frame rect is still cleared.
     if (!firstLineBox())
         setFrameRect(LayoutRect());
+
+    m_overflow.clear();
+    addVisualEffectOverflow();
 
     if (!updateCachedBoundariesInParents)
         updateCachedBoundariesInParents = oldBoundaries != objectBoundingBox();
