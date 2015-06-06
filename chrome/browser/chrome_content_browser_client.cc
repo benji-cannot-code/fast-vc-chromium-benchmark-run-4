fc2afebb487395810ff43004432c7e5102612e34
haibinlu <haibinlu@chromium.org>
2015-06-05 20:45:09 -0700

    [MediaRouter] This patch connects PresentationServiceDelegateImpl to the presentation service for Chrome.
    BUG=461815
    
    Review URL: https://codereview.chromium.org/1149293008
    
    Cr-Commit-Position: refs/heads/master@{#333211}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -241,6 +241,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/webrtc_logging_handler_host.h"
 #endif
 
+#if defined(ENABLE_MEDIA_ROUTER)
+#include "chrome/browser/media/router/presentation_service_delegate_impl.h"
+#endif
+
 using base::FileDescriptor;
 using blink::WebWindowFeatures;
 using content::AccessTokenStore;
@@ -2360,6 +2364,18 @@ void ChromeContentBrowserClient::OpenURL(
 #else
   NOTIMPLEMENTED();
 #endif
+}
+
+content::PresentationServiceDelegate*
+ChromeContentBrowserClient::GetPresentationServiceDelegate(
+      content::WebContents* web_contents) {
+#if defined(ENABLE_MEDIA_ROUTER)
+  if (switches::MediaRouterEnabled()) {
+    return media_router::PresentationServiceDelegateImpl::
+        GetOrCreateForWebContents(web_contents);
+  }
+#endif
+  return nullptr;
 }
 
 void ChromeContentBrowserClient::RecordURLMetric(const std::string& metric,
