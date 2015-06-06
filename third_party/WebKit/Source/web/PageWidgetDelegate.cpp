ad5a3ab00c9726bf714ea61f8b26ad4873809c5c
chrishtr@chromium.org <chrishtr@chromium.org>
2015-06-06 00:02:11 +0000

    Use SkPictureBuilder in PageWidgetDelegate
    Also remove an unneeded include in SVGImage
    
    BUG=486830
    
    Review URL: https://codereview.chromium.org/1154783007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196611 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/PageWidgetDelegate.cpp
+++ third_party/WebKit/Source/web/PageWidgetDelegate.cpp
@@ -43,8 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/Logging.h"
 #include "platform/graphics/GraphicsContext.h"
 #include "platform/graphics/paint/ClipRecorder.h"
-#include "platform/graphics/paint/DisplayItemListContextRecorder.h"
 #include "platform/graphics/paint/DrawingRecorder.h"
+#include "platform/graphics/paint/SkPictureBuilder.h"
 #include "platform/transforms/AffineTransform.h"
 #include "public/web/WebInputEvent.h"
 #include "web/PageOverlayList.h"
@@ -73,10 +73,10 @@ void PageWidgetDelegate::paint(Page& page, PageOverlayList* overlays, WebCanvas*
     if (rect.isEmpty())
         return;
 
-    OwnPtr<GraphicsContext> context = GraphicsContext::deprecatedCreateWithCanvas(canvas);
+    IntRect intRect(rect);
+    SkPictureBuilder pictureBuilder(intRect);
     {
-        DisplayItemListContextRecorder contextRecorder(*context);
-        GraphicsContext& paintContext = contextRecorder.context();
+        GraphicsContext& paintContext = pictureBuilder.context();
 
         // FIXME: device scale factor settings are layering violations and should not
         // be used within Blink paint code.
@@ -101,6 +101,7 @@ void PageWidgetDelegate::paint(Page& page, PageOverlayList* overlays, WebCanvas*
                 paintContext.fillRect(dirtyRect, Color::white);
         }
     }
+    pictureBuilder.endRecording()->playback(canvas);
 }
 
 bool PageWidgetDelegate::handleInputEvent(PageWidgetEventHandler& handler, const WebInputEvent& event, LocalFrame* root)
