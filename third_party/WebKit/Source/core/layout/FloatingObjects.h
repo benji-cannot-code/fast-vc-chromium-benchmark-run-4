27389eddeb7c77167d3a74a97c5d05f177cbf064
robhogan@gmail.com <robhogan@gmail.com>
2015-06-07 18:25:58 +0000

    Formatting contexts should always expand to enclose floats
    If a block that creates a formatting context contains floats in its descendants that
    overhang, it should expand to enclose them. We were failing to do this when the float
    overhung the formatting context but not its direct ancestor.
    
    BUG=477076
    
    Review URL: https://codereview.chromium.org/1106533002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/FloatingObjects.h
+++ third_party/WebKit/Source/core/layout/FloatingObjects.h
@@ -88,6 +88,8 @@ public:
     void setShouldPaint(bool shouldPaint) { m_shouldPaint = shouldPaint; }
     bool isDescendant() const { return m_isDescendant; }
     void setIsDescendant(bool isDescendant) { m_isDescendant = isDescendant; }
+    bool isLowestNonOverhangingFloatInChild() const { return m_isLowestNonOverhangingFloatInChild; }
+    void setIsLowestNonOverhangingFloatInChild(bool isLowestNonOverhangingFloatInChild) { m_isLowestNonOverhangingFloatInChild = isLowestNonOverhangingFloatInChild; }
 
     // FIXME: Callers of these methods are dangerous and should be whitelisted explicitly or removed.
     RootInlineBox* originatingLine() const { return m_originatingLine; }
@@ -95,7 +97,7 @@ public:
 
 private:
     explicit FloatingObject(LayoutBox*);
-    FloatingObject(LayoutBox*, Type, const LayoutRect&, bool shouldPaint, bool isDescendant);
+    FloatingObject(LayoutBox*, Type, const LayoutRect&, bool shouldPaint, bool isDescendant, bool isLowestNonOverhangingFloatInChild);
 
     LayoutBox* m_layoutObject;
     RootInlineBox* m_originatingLine;
@@ -106,6 +108,7 @@ private:
     unsigned m_shouldPaint : 1;
     unsigned m_isDescendant : 1;
     unsigned m_isPlaced : 1;
+    unsigned m_isLowestNonOverhangingFloatInChild : 1;
 #if ENABLE(ASSERT)
     unsigned m_isInPlacedTree : 1;
 #endif
@@ -159,11 +162,12 @@ public:
     LayoutUnit logicalRightOffsetForPositioningFloat(LayoutUnit fixedOffset, LayoutUnit logicalTop, LayoutUnit* heightRemaining);
 
     LayoutUnit lowestFloatLogicalBottom(FloatingObject::Type);
+    FloatingObject* lowestFloatingObject() const;
 
 private:
     bool hasLowestFloatLogicalBottomCached(bool isHorizontal, FloatingObject::Type floatType) const;
     LayoutUnit getCachedlowestFloatLogicalBottom(FloatingObject::Type floatType) const;
-    void setCachedLowestFloatLogicalBottom(bool isHorizontal, FloatingObject::Type floatType, LayoutUnit value);
+    void setCachedLowestFloatLogicalBottom(bool isHorizontal, FloatingObject::Type floatType, FloatingObject*);
     void markLowestFloatLogicalBottomCacheAsDirty();
 
     void computePlacedFloatsTree();
@@ -186,7 +190,7 @@ private:
 
     struct FloatBottomCachedValue {
         FloatBottomCachedValue();
-        LayoutUnit value;
+        FloatingObject* floatingObject;
         bool dirty;
     };
     FloatBottomCachedValue m_lowestFloatBottomCache[2];
