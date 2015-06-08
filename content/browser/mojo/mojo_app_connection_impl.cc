328be71c851154ce41056cf39bd2bfa837072722
rockot <rockot@chromium.org>
2015-06-08 14:26:05 -0700

    Revert of Add a mojo shell service to render frames (patchset #3 id:160001 of https://codereview.chromium.org/1164163002/)
    Reason for revert:
    Broke MSan bot for confusing reasons.
    
    Original issue's description:
    > Add a mojo shell service to render frames
    >
    > This adds a Shell service to each render frame's service
    > registry, allowing frames to connect to Mojo applications.
    > When connecting to an application, the frame identifies
    > itself using its SiteInstance's URL.
    >
    > BUG=497474
    >
    > Committed: https://crrev.com/89b0cc457ff83154f80709d9b58575047845730e
    > Cr-Commit-Position: refs/heads/master@{#333312}
    
    TBR=jam@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=497474
    
    Review URL: https://codereview.chromium.org/1169933002
    
    Cr-Commit-Position: refs/heads/master@{#333342}

--- content/browser/mojo/mojo_app_connection_impl.cc
+++ content/browser/mojo/mojo_app_connection_impl.cc
@@ -9,20 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-const char kBrowserMojoAppUrl[] = "system:content_browser";
-
-// static
-scoped_ptr<MojoAppConnection> MojoAppConnection::Create(
-    const GURL& url,
-    const GURL& requestor_url) {
-  return scoped_ptr<MojoAppConnection>(
-      new MojoAppConnectionImpl(url, requestor_url));
+scoped_ptr<MojoAppConnection> MojoAppConnection::Create(const GURL& url) {
+  return scoped_ptr<MojoAppConnection>(new MojoAppConnectionImpl(url));
 }
 
-MojoAppConnectionImpl::MojoAppConnectionImpl(const GURL& url,
-                                             const GURL& requestor_url) {
-  MojoShellContext::ConnectToApplication(url, requestor_url,
-                                         mojo::GetProxy(&services_));
+MojoAppConnectionImpl::MojoAppConnectionImpl(const GURL& url) {
+  MojoShellContext::ConnectToApplication(url, mojo::GetProxy(&services_));
 }
 
 MojoAppConnectionImpl::~MojoAppConnectionImpl() {
--- content/content_browser.gypi
+++ content/content_browser.gypi
@@ -702,8 +702,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/frame_host/debug_urls.h',
       'browser/frame_host/frame_accessibility.cc',
       'browser/frame_host/frame_accessibility.h',
-      'browser/frame_host/frame_mojo_shell.cc',
-      'browser/frame_host/frame_mojo_shell.h',
       'browser/frame_host/frame_navigation_entry.cc',
       'browser/frame_host/frame_navigation_entry.h',
       'browser/frame_host/frame_tree.cc',
