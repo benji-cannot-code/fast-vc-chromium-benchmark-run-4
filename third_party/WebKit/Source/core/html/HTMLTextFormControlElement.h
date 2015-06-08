a2e34dea65fc32fe3c028d234b70dce6ef8e7668
samahto@cisco.com <samahto@cisco.com>
2015-06-08 15:27:04 +0000

    Reapply the patch r191685 with fixing the regression issue. This also fixes the issue where on cloneNode(true) doesn't copy dynamic updated text instead default text is copied.
        Orignial bug description and casue of revert.
        
        > Revert r191685 "Implement copyNonAttributePropertiesFromElement for
        HTMLTextAreaElement"
        >It caused a regression about cloneNode() for <textarea>.
        
            > Implement copyNonAttributePropertiesFromElement for
            > HTMLTextAreaElement
            >
            > Before this patch on cloning textarea element checkValidity() gives
            > incorrect result becasue value is not copied in cloned node.
            > Now we copy value by overriding copyNonAttributePropertiesFromElement
            >
            > BUG=461414, 465365. 
            > TESTS=Exisitng LayoutTests/fast/forms/checkValidity-on-cloned-node.html    
            >BUG=487352
    
    BUG=487352
    TEST=LayoutTests/fast/forms/textarea-cloned-text-match.html
    
    Review URL: https://codereview.chromium.org/1134593004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196677 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLTextFormControlElement.h
+++ third_party/WebKit/Source/core/html/HTMLTextFormControlElement.h
@@ -135,6 +135,7 @@ protected:
     String valueWithHardLineBreaks() const;
 
     virtual bool shouldDispatchFormControlChangeEvent(String&, String&);
+    virtual void copyNonAttributePropertiesFromElement(const Element&) override;
 
 private:
     int computeSelectionStart() const;
