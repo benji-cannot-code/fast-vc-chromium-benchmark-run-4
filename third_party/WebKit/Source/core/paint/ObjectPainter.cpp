dcfd47ee6ecdd6829f4bfac8680892b82dcea305
fs@opera.com <fs@opera.com>
2015-06-08 23:05:34 +0000

    Fix cull rect computation for SVG{Container,Image,Shape}Painter outlines
    TEST=svg/custom/transformed-outlines.svg (S.P. strict cull rect)
    BUG=495368
    
    Review URL: https://codereview.chromium.org/1160643004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196704 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/ObjectPainter.cpp
+++ third_party/WebKit/Source/core/paint/ObjectPainter.cpp
@@ -16,6 +16,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+LayoutRect ObjectPainter::outlineBounds(const LayoutRect& objectBounds, const ComputedStyle& style)
+{
+    int outlineOutset;
+    if (style.outlineStyleIsAuto())
+        outlineOutset = GraphicsContext::focusRingOutsetExtent(style.outlineOffset(), style.outlineWidth());
+    else
+        outlineOutset = style.outlineSize();
+    LayoutRect outlineBounds(objectBounds);
+    outlineBounds.inflate(outlineOutset);
+    return outlineBounds;
+}
+
 void ObjectPainter::paintFocusRing(const PaintInfo& paintInfo, const ComputedStyle& style, const Vector<LayoutRect>& focusRingRects)
 {
     ASSERT(style.outlineStyleIsAuto());
