072d3e1d3299c81bd8524ade07cb5b00342143d5
spang <spang@chromium.org>
2015-06-08 14:40:32 -0700

    ozone: evdev: Add enable_repeat flag to key events
    This adds an enable_repeat flag to key events that enables local key
    repeat. The flag is true for keyboard devices and false for keys
    injected by the remoting host.
    
    This also changes the semantics of consecutive keypresses for the same
    key. Previously they were completely ignored, now they are considered
    repeats. This is used by the remoting host to implement remote repeat.
    
    The remoting host no longer uses the input controller to change the
    global key repeat setting; it's now set exclusively from prefs.
    
    BUG=496420
    TEST=visit rbyers.github.io/eventTest.html
         via it2me from chromebook to chromebook
         key repeat works normally
    
    Review URL: https://codereview.chromium.org/1166113003
    
    Cr-Commit-Position: refs/heads/master@{#333349}

--- ui/events/ozone/evdev/libgestures_glue/gesture_interpreter_libevdev_cros.cc
+++ ui/events/ozone/evdev/libgestures_glue/gesture_interpreter_libevdev_cros.cc
@@ -453,7 +453,8 @@ void GestureInterpreterLibevdevCros::DispatchChangedKeys(
 
       // Dispatch key press or release to keyboard.
       dispatcher_->DispatchKeyEvent(
-          KeyEventParams(id_, key, value, StimeToTimedelta(timestamp)));
+          KeyEventParams(id_, key, value, true /* enable_repeat */,
+                         StimeToTimedelta(timestamp)));
     }
   }
 
