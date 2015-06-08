d2d9ef2de9d2762a894209ca12aa8a3362f31a9f
caseq@chromium.org <caseq@chromium.org>
2015-06-08 10:26:07 +0000

    DevTools: better error handling in paint profiler UI
    Avoid exception when displaying an empty paint command list.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1163223003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196664 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelinePaintProfilerView.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelinePaintProfilerView.js
@@ -93,8 +93,10 @@ WebInspector.TimelinePaintProfilerView.prototype = {
             this._disposeSnapshot();
             this._lastLoadedSnapshot = snapshot;
             this._imageView.setMask(tileRect);
-            if (!snapshot)
+            if (!snapshot) {
+                this._imageView.showImage();
                 return;
+            }
             snapshot.commandLog(onCommandLogDone.bind(this, snapshot, tileRect));
         }
 
@@ -106,7 +108,7 @@ WebInspector.TimelinePaintProfilerView.prototype = {
          */
         function onCommandLogDone(snapshot, clipRect, log)
         {
-            this._logTreeView.setCommandLog(snapshot.target(), log);
+            this._logTreeView.setCommandLog(snapshot.target(), log || []);
             this._paintProfilerView.setSnapshotAndLog(snapshot, log || [], clipRect);
         }
     },
@@ -194,7 +196,8 @@ WebInspector.TimelinePaintImageView.prototype = {
     showImage: function(imageURL)
     {
         this._imageContainer.classList.toggle("hidden", !imageURL);
-        this._imageElement.src = imageURL;
+        if (imageURL)
+            this._imageElement.src = imageURL;
     },
 
     /**
