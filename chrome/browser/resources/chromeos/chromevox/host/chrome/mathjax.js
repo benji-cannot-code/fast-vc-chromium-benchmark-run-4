f5e4d8475950d0c828a7ebae71557f0e8b95ba44
plundblad <plundblad@chromium.org>
2015-06-08 09:31:37 -0700

    Kill a confusing error message.
    Chromevox doesn't install scripts on some blackilsted pages.  In that case,
    the mathjax code was trying to use a null port object and that lead
    to an uncaught exception.
    
    R=dtseng@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/1165453006
    
    Cr-Commit-Position: refs/heads/master@{#333275}

--- chrome/browser/resources/chromeos/chromevox/host/chrome/mathjax.js
+++ chrome/browser/resources/chromeos/chromevox/host/chrome/mathjax.js
@@ -25,6 +25,13 @@ cvox.ChromeMathJax = function() {
   goog.base(this);
 
   /**
+   * Set to when the bridge is initialized.
+   * @type {boolean}
+   * @private
+   */
+  this.initialized_ = false;
+
+  /**
    * The port to communicate with the content script.
    * @type {Port}
    */
@@ -96,7 +103,7 @@ cvox.ChromeMathJax.prototype.init = function() {
       'chromevox/injected/mathjax_external_util.js'));
   scripts.push(cvox.ChromeVox.host.getFileSrc('chromevox/injected/mathjax.js'));
   scripts.push(cvox.ApiImplementation.siteSpecificScriptLoader);
-  cvox.ScriptInstaller.installScript(
+  this.initialized_ = cvox.ScriptInstaller.installScript(
       scripts, 'mathjax', undefined,
       cvox.ApiImplementation.siteSpecificScriptBase);
 };
@@ -182,6 +189,11 @@ cvox.ChromeMathJax.prototype.applyBoolean = function(
  * @override
  */
 cvox.ChromeMathJax.prototype.isMathjaxActive = function(callback) {
+  if (!this.initialized_) {
+    callback(false);
+    return;
+  }
+
   var retries = 0;
 
   var fetch = goog.bind(function() {
