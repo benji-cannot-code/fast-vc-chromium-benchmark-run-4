f9cb32cc0d954757de152723e74af7f0f1bb6c41
jbudorick <jbudorick@chromium.org>
2015-06-08 10:58:50 -0700

    [Android] Speculative fix: use WaitUntilFullyBooted in telemetry.
    adb wait-for-device (and thus AdbWrapper.WaitForDevice) can return before
    the device is completely ready for use (see e.g. crbug/493900). Switching
    to DeviceUtils.WaitUntilFullyBooted ensures that the device is either
    ready or is irrecoverably gone.
    
    BUG=489317
    
    Review URL: https://codereview.chromium.org/1152293008
    
    Cr-Commit-Position: refs/heads/master@{#333290}

--- tools/telemetry/telemetry/core/forwarders/android_forwarder.py
+++ tools/telemetry/telemetry/core/forwarders/android_forwarder.py
@@ -280,7 +280,7 @@ class AndroidRndisConfigurator(object):
     except device_errors.AdbCommandFailedError:
       # Ignore exception due to USB connection being reset.
       pass
-    self._device.adb.WaitForDevice()
+    self._device.WaitUntilFullyBooted()
 
   def _EnableRndis(self):
     """Enables the RNDIS network interface."""
@@ -322,7 +322,7 @@ doit &
     # TODO(szym): run via su -c if necessary.
     self._device.RunShellCommand('rm %s.log' % script_prefix)
     self._device.RunShellCommand('. %s.sh' % script_prefix)
-    self._device.adb.WaitForDevice()
+    self._device.WaitUntilFullyBooted()
     result = self._device.ReadFile('%s.log' % script_prefix).splitlines()
     assert any('DONE' in line for line in result), 'RNDIS script did not run!'
 
@@ -490,7 +490,7 @@ doit &
     self._device.RunShellCommand(
         'ifconfig %s %s netmask %s up' % (device_iface, device_ip, netmask))
     # Enabling the interface sometimes breaks adb.
-    self._device.adb.WaitForDevice()
+    self._device.WaitUntilFullyBooted()
     self._host_iface = host_iface
     self._host_ip = host_ip
     self.device_iface = device_iface
