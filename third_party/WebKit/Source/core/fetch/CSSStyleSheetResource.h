3094fe2685ae7c1c8ba7fc22175c47f22eea1947
mkwst@chromium.org <mkwst@chromium.org>
2015-06-08 15:48:58 +0000

    Allow lax MIME type parsing for same-origin CSS in quirks mode.
    The change in [1] is too strict for the web, and doesn't match Firefox
    or IE's behavior. In particular, it does not respect the same-origin
    carveout that the HTML spec specifies[2].
    
    This patch corrects that oversight.
    
    [1]: https://chromium.googlesource.com/chromium/blink/+/80f95a77030f72e9502f52873eeb4490c4b3f458
    [2]: https://html.spec.whatwg.org/multipage/semantics.html#link-type-stylesheet
    
    BUG=495711
    
    Review URL: https://codereview.chromium.org/1169713004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196678 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/CSSStyleSheetResource.h
+++ third_party/WebKit/Source/core/fetch/CSSStyleSheetResource.h
@@ -39,11 +39,13 @@ class StyleSheetContents;
 
 class CSSStyleSheetResource final : public StyleSheetResource {
 public:
+    enum class MIMETypeCheck { Strict, Lax };
+
     CSSStyleSheetResource(const ResourceRequest&, const String& charset);
     virtual ~CSSStyleSheetResource();
     DECLARE_VIRTUAL_TRACE();
 
-    const String sheetText(bool* hasValidMIMEType = 0) const;
+    const String sheetText(MIMETypeCheck = MIMETypeCheck::Strict) const;
 
     const AtomicString mimeType() const;
 
@@ -57,7 +59,7 @@ protected:
     virtual void destroyDecodedDataIfPossible() override;
 
 private:
-    bool canUseSheet(bool* hasValidMIMEType) const;
+    bool canUseSheet(MIMETypeCheck) const;
     virtual void dispose() override;
     virtual void checkNotify() override;
 
