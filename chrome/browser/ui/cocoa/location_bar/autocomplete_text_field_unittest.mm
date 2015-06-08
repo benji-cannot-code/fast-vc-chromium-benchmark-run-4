3b19f5caf55446ad64287f0fb10c3a50b933d97b
asvitkine <asvitkine@chromium.org>
2015-06-08 14:26:24 -0700

    Implement Omnibox.CharTypedToRepaintLatency metric on Mac.
    This is similar to https://codereview.chromium.org/969813004,
    which implemented this metric for Views.
    
    BUG=463202
    
    Review URL: https://codereview.chromium.org/1163383003
    
    Cr-Commit-Position: refs/heads/master@{#333343}

--- chrome/browser/ui/cocoa/location_bar/autocomplete_text_field_unittest.mm
+++ chrome/browser/ui/cocoa/location_bar/autocomplete_text_field_unittest.mm
@@ -829,6 +829,7 @@ TEST_F(AutocompleteTextFieldObserverTest,
   EXPECT_CALL(field_observer_, OnSetFocus(false)).Times(testing::AnyNumber());
   EXPECT_CALL(field_observer_, OnKillFocus()).Times(testing::AnyNumber());
   EXPECT_CALL(field_observer_, OnDidEndEditing()).Times(testing::AnyNumber());
+  EXPECT_CALL(field_observer_, OnDidDrawRect()).Times(testing::AnyNumber());
 
   // Ensure the field is currently not first responder.
   [test_window() makePretendKeyWindowAndSetFirstResponder:nil];
