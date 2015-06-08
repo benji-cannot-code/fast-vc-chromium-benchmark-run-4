408ddb3236caf2bcf162ec8456cf466fd87df741
lizeb <lizeb@chromium.org>
2015-06-08 08:18:47 -0700

    hosted: Add prerendering to the Warmup service.
    This CL adds prerendering to the warmup service used by the hosted
    chrome implementation. This reuses the logic from the PrerenderService.
    
    It is incompatible with ChromePrerenderService, so disables prerendering
    through WarmupManager when the hosted service is used.
    
    Note that this only handles one prerender at a time, and will be improved in
    forthcoming CLs.
    
    BUG=484639
    
    Review URL: https://codereview.chromium.org/1151023002
    
    Cr-Commit-Position: refs/heads/master@{#333268}

--- chrome/android/java/src/org/chromium/chrome/browser/WarmupManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/WarmupManager.java
@@ -40,6 +40,7 @@ public final class WarmupManager {
     private final Set<String> mDnsRequestsInFlight;
     private final Map<String, Profile> mPendingPreconnectWithProfile;
 
+    private boolean mPrerenderIsAllowed;
     private WebContents mPrerenderedWebContents;
     private boolean mPrerendered;
     private ViewGroup mMainView;
@@ -55,8 +56,19 @@ public final class WarmupManager {
     }
 
     private WarmupManager() {
+        mPrerenderIsAllowed = true;
         mDnsRequestsInFlight = new HashSet<String>();
         mPendingPreconnectWithProfile = new HashMap<String, Profile>();
+    }
+
+    /**
+     * Disallow prerendering from now until the browser process death.
+     */
+    public void disallowPrerendering() {
+        ThreadUtils.assertOnUiThread();
+        mPrerenderIsAllowed = false;
+        cancelCurrentPrerender();
+        mExternalPrerenderHandler = null;
     }
 
     /**
@@ -70,6 +82,7 @@ public final class WarmupManager {
      */
     public boolean hasPrerenderedUrl(String url) {
         ThreadUtils.assertOnUiThread();
+        if (!mPrerenderIsAllowed) return false;
         return hasAnyPrerenderedUrl() && ExternalPrerenderHandler.hasPrerenderedUrl(
                 Profile.getLastUsedProfile(), url, mPrerenderedWebContents);
     }
@@ -79,6 +92,7 @@ public final class WarmupManager {
      */
     public boolean hasAnyPrerenderedUrl() {
         ThreadUtils.assertOnUiThread();
+        if (!mPrerenderIsAllowed) return false;
         return mPrerendered;
     }
 
@@ -87,6 +101,7 @@ public final class WarmupManager {
      */
     public WebContents takePrerenderedWebContents() {
         ThreadUtils.assertOnUiThread();
+        if (!mPrerenderIsAllowed) return null;
         WebContents prerenderedWebContents = mPrerenderedWebContents;
         assert (mPrerenderedWebContents != null);
         mPrerenderedWebContents = null;
@@ -106,6 +121,7 @@ public final class WarmupManager {
     public void prerenderUrl(final String url, final String referrer,
             final int widthPix, final int heightPix) {
         ThreadUtils.assertOnUiThread();
+        if (!mPrerenderIsAllowed) return;
         clearWebContentsIfNecessary();
         if (mExternalPrerenderHandler == null) {
             mExternalPrerenderHandler = new ExternalPrerenderHandler();
