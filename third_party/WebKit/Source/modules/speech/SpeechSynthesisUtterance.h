340b95372d7f88a20e959396497a43cc165df336
haraken@chromium.org <haraken@chromium.org>
2015-06-08 10:24:09 +0000

    Clean up macros around DEFINE_EVENT_TARGET_REFCOUNTING
    The current macros are messy. This CL changes the macros as follows (which is more consistent and easy to understand):
    
    - If the EventTarget class is RefCounted on non-oilpan builds, use REFCOUNTED_EVENT_TARGET.
    - If the EventTarget class is RefCountedGarbageCollected on non-oilpan builds, use REFCOUNTED_GARBAGE_COLLECTED_EVENT_TARGET.
    
    BUG=497595
    
    Review URL: https://codereview.chromium.org/1163253003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196663 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/speech/SpeechSynthesisUtterance.h
+++ third_party/WebKit/Source/modules/speech/SpeechSynthesisUtterance.h
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class SpeechSynthesisUtterance final : public RefCountedGarbageCollectedEventTargetWithInlineData<SpeechSynthesisUtterance>, public PlatformSpeechSynthesisUtteranceClient, public ContextLifecycleObserver {
-    DEFINE_EVENT_TARGET_REFCOUNTING_WILL_BE_REMOVED(RefCountedGarbageCollected<SpeechSynthesisUtterance>);
+    REFCOUNTED_GARBAGE_COLLECTED_EVENT_TARGET(SpeechSynthesisUtterance);
     DEFINE_WRAPPERTYPEINFO();
     USING_GARBAGE_COLLECTED_MIXIN(SpeechSynthesisUtterance);
 public:
