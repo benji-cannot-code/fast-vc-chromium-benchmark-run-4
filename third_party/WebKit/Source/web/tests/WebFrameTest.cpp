007aa5c1314251ab2e262b83b6b5092379958dbb
kouhei@chromium.org <kouhei@chromium.org>
2015-06-08 07:33:17 +0000

    Throw DOMException when invoked Document::execCommand on non-{X,}HTML documents
    Throw DOMException when invoked Document::execCommand on non-{X,}HTML documents as it may
    lead to unexpected results. (e.g. InsertHTML execCommand running
    HTML parser on top of XML document)
    
    This change also aligns Blink better to other browsers and the
    HTML Editing spec, which doesn't mention execCommand being applied
    to non-HTML document:
    https://dvcs.w3.org/hg/editing/raw-file/tip/editing.html
    
    BUG=484974
    
    Review URL: https://codereview.chromium.org/1155353002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196652 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/web/tests/WebFrameTest.cpp
@@ -4978,7 +4978,9 @@ TEST_P(ParameterizedWebFrameTest, ReplaceMisspelledRange)
     webViewHelper.webView()->settings()->setEditingBehavior(WebSettings::EditingBehaviorWin);
 
     element->focus();
-    document->execCommand("InsertText", false, "_wellcome_.");
+    NonThrowableExceptionState exceptionState;
+    document->execCommand("InsertText", false, "_wellcome_.", exceptionState);
+    EXPECT_FALSE(exceptionState.hadException());
 
     const int allTextBeginOffset = 0;
     const int allTextLength = 11;
@@ -5009,7 +5011,9 @@ TEST_P(ParameterizedWebFrameTest, RemoveSpellingMarkers)
     webViewHelper.webView()->settings()->setEditingBehavior(WebSettings::EditingBehaviorWin);
 
     element->focus();
-    document->execCommand("InsertText", false, "_wellcome_.");
+    NonThrowableExceptionState exceptionState;
+    document->execCommand("InsertText", false, "_wellcome_.", exceptionState);
+    EXPECT_FALSE(exceptionState.hadException());
 
     frame->removeSpellingMarkers();
 
@@ -5038,7 +5042,9 @@ TEST_P(ParameterizedWebFrameTest, RemoveSpellingMarkersUnderWords)
     webViewHelper.webView()->settings()->setEditingBehavior(WebSettings::EditingBehaviorWin);
 
     element->focus();
-    document->execCommand("InsertText", false, " wellcome ");
+    NonThrowableExceptionState exceptionState;
+    document->execCommand("InsertText", false, " wellcome ", exceptionState);
+    EXPECT_FALSE(exceptionState.hadException());
 
     WebVector<uint32_t> documentMarkers1;
     webViewHelper.webView()->spellingMarkers(&documentMarkers1);
@@ -5072,7 +5078,9 @@ TEST_P(ParameterizedWebFrameTest, MarkerHashIdentifiers)
     webViewHelper.webView()->settings()->setEditingBehavior(WebSettings::EditingBehaviorWin);
 
     element->focus();
-    document->execCommand("InsertText", false, "wellcome.");
+    NonThrowableExceptionState exceptionState;
+    document->execCommand("InsertText", false, "wellcome.", exceptionState);
+    EXPECT_FALSE(exceptionState.hadException());
 
     WebVector<uint32_t> documentMarkers;
     webViewHelper.webView()->spellingMarkers(&documentMarkers);
@@ -5148,9 +5156,12 @@ TEST_P(ParameterizedWebFrameTest, SlowSpellcheckMarkerPosition)
     webViewHelper.webView()->settings()->setEditingBehavior(WebSettings::EditingBehaviorWin);
 
     element->focus();
-    document->execCommand("InsertText", false, "wellcome ");
+    NonThrowableExceptionState exceptionState;
+    document->execCommand("InsertText", false, "wellcome ", exceptionState);
+    EXPECT_FALSE(exceptionState.hadException());
     webInputElement.setSelectionRange(0, 0);
-    document->execCommand("InsertText", false, "he");
+    document->execCommand("InsertText", false, "he", exceptionState);
+    EXPECT_FALSE(exceptionState.hadException());
 
     spellcheck.kick();
 
@@ -5200,7 +5211,9 @@ TEST_P(ParameterizedWebFrameTest, SpellcheckResultErasesMarkers)
     webViewHelper.webView()->settings()->setEditingBehavior(WebSettings::EditingBehaviorWin);
 
     element->focus();
-    document->execCommand("InsertText", false, "welcome ");
+    NonThrowableExceptionState exceptionState;
+    document->execCommand("InsertText", false, "welcome ", exceptionState);
+    EXPECT_FALSE(exceptionState.hadException());
     document->markers().addMarker(rangeOfContents(element->toNode()).get(), DocumentMarker::Spelling);
     document->markers().addMarker(rangeOfContents(element->toNode()).get(), DocumentMarker::Grammar);
     document->markers().addMarker(rangeOfContents(element->toNode()).get(), DocumentMarker::InvisibleSpellcheck);
@@ -5229,21 +5242,25 @@ TEST_P(ParameterizedWebFrameTest, SpellcheckResultsSavedInDocument)
     webViewHelper.webView()->settings()->setEditingBehavior(WebSettings::EditingBehaviorWin);
 
     element->focus();
-    document->execCommand("InsertText", false, "wellcome ");
+    NonThrowableExceptionState exceptionState;
+    document->execCommand("InsertText", false, "wellcome ", exceptionState);
+    EXPECT_FALSE(exceptionState.hadException());
 
     spellcheck.kick();
     ASSERT_EQ(1U, document->markers().markers().size());
     ASSERT_NE(static_cast<DocumentMarker*>(0), document->markers().markers()[0]);
     EXPECT_EQ(DocumentMarker::Spelling, document->markers().markers()[0]->type());
 
-    document->execCommand("InsertText", false, "wellcome ");
+    document->execCommand("InsertText", false, "wellcome ", exceptionState);
+    EXPECT_FALSE(exceptionState.hadException());
 
     spellcheck.kickGrammar();
     ASSERT_EQ(1U, document->markers().markers().size());
     ASSERT_NE(static_cast<DocumentMarker*>(0), document->markers().markers()[0]);
     EXPECT_EQ(DocumentMarker::Grammar, document->markers().markers()[0]->type());
 
-    document->execCommand("InsertText", false, "wellcome ");
+    document->execCommand("InsertText", false, "wellcome ", exceptionState);
+    EXPECT_FALSE(exceptionState.hadException());
 
     spellcheck.kickInvisibleSpellcheck();
     ASSERT_EQ(1U, document->markers().markers().size());
