555759e395fa21d97f9b4c6dbfe95a7f842401a2
scottmg <scottmg@chromium.org>
2015-06-09 09:46:15 -0700

    use va_start, not [_]_crt_va_start
    R=rnk@chromium.org
    TBR=jschuh@chromium.org
    BUG=440500
    
    Review URL: https://codereview.chromium.org/1168113007
    
    Cr-Commit-Position: refs/heads/master@{#333507}

--- sandbox/win/sandbox_poc/main_ui_window.cc
+++ sandbox/win/sandbox_poc/main_ui_window.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #include <CommCtrl.h>
 #include <commdlg.h>
+#include <stdarg.h>
 #include <time.h>
 #include <windowsx.h>
 #include <atlbase.h>
@@ -625,11 +626,7 @@ void MainUIWindow::AddDebugMessage(const wchar_t* format, ...) {
   const int kMaxDebugBuffSize = 1024;
 
   va_list arg_list;
-#if _MSC_VER >= 1900
-  __crt_va_start(arg_list, format);
-#else
-  _crt_va_start(arg_list, format);
-#endif
+  va_start(arg_list, format);
 
   wchar_t text[kMaxDebugBuffSize + 1];
   vswprintf_s(text, kMaxDebugBuffSize, format, arg_list);
