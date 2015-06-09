1d25c81219af584c5a554ecd80c1b2a9c9e57024
ksakamoto@chromium.org <ksakamoto@chromium.org>
2015-06-09 00:58:17 +0000

    Add workerReady timing for ServiceWorker controlled requests [3/3]
    To make ServiceWorker startup time measureable, in addition to
    workerStart, we need another timing "workerReady" which is the point
    ServiceWorker finished starting and is ready to handle events.
    
    This patch makes ResourceTiming's redirectStart (for redirected fetches)
    or fetchStart (for non-redirected fetches) return workerReady time,
    instead of workerStart.
    
    [1/3] https://codereview.chromium.org/1149273003/
    [2/3] https://codereview.chromium.org/1146253004/
    [3/3] THIS
    
    BUG=465640
    TEST=http/tests/serviceworker/resource-timing.html
    
    Review URL: https://codereview.chromium.org/1166953007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196720 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/timing/PerformanceResourceTiming.cpp
+++ third_party/WebKit/Source/core/timing/PerformanceResourceTiming.cpp
@@ -78,13 +78,21 @@ double PerformanceResourceTiming::workerStart() const
     return monotonicTimeToDocumentMilliseconds(m_requestingDocument.get(), m_timing->workerStart());
 }
 
+double PerformanceResourceTiming::workerReady() const
+{
+    if (!m_timing || m_timing->workerReady() == 0.0)
+        return 0.0;
+
+    return monotonicTimeToDocumentMilliseconds(m_requestingDocument.get(), m_timing->workerReady());
+}
+
 double PerformanceResourceTiming::redirectStart() const
 {
     if (!m_lastRedirectEndTime || !m_allowRedirectDetails)
         return 0.0;
 
-    if (double workerStartTime = workerStart())
-        return workerStartTime;
+    if (double workerReadyTime = workerReady())
+        return workerReadyTime;
 
     return PerformanceEntry::startTime();
 }
@@ -106,8 +114,8 @@ double PerformanceResourceTiming::fetchStart() const
         return monotonicTimeToDocumentMilliseconds(m_requestingDocument.get(), m_timing->requestTime());
     }
 
-    if (double workerStartTime = workerStart())
-        return workerStartTime;
+    if (double workerReadyTime = workerReady())
+        return workerReadyTime;
 
     return PerformanceEntry::startTime();
 }
