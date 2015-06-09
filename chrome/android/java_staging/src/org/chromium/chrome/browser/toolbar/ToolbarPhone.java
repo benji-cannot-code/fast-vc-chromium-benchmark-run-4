52a99a7d932a00e694aa696ca732be688ce5ade5
dtrainor <dtrainor@chromium.org>
2015-06-09 14:20:12 -0700

    No toolbar updates while animating tab stack
    - When leaving the tab switcher, don't update the toolbar button states.  This can cause
    the home button to be displayed early.
    
    BUG=497602
    
    Review URL: https://codereview.chromium.org/1169233002
    
    Cr-Commit-Position: refs/heads/master@{#333575}

--- chrome/android/java_staging/src/org/chromium/chrome/browser/toolbar/ToolbarPhone.java
+++ chrome/android/java_staging/src/org/chromium/chrome/browser/toolbar/ToolbarPhone.java
@@ -674,7 +674,7 @@ public class ToolbarPhone extends ToolbarLayout
     }
 
     private void updateUrlExpansionAnimation() {
-        if (mInTabSwitcherMode) return;
+        if (mInTabSwitcherMode || isTabSwitcherAnimationRunning()) return;
 
         mLocationBarBackgroundOffset.setEmpty();
 
