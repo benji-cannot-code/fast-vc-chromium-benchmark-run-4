e7faa3ab0be2770ff6e8097ea5a5806c138f5c0d
eugenebut <eugenebut@chromium.org>
2015-06-09 11:19:24 -0700

    Moved crw_native_content/crw_native_content_provider to ui subdirectory.
    BUG=389405
    
    Review URL: https://codereview.chromium.org/1164653005
    
    Cr-Commit-Position: refs/heads/master@{#333530}

--- ios/web/public/web_state/crw_web_delegate.h
+++ ios/web/public/web_state/crw_web_delegate.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ios/block_types.h"
 #include "ios/web/public/favicon_url.h"
 #include "ios/web/public/ssl_status.h"
-#import "ios/web/public/web_state/crw_native_content.h"
+#import "ios/web/public/web_state/ui/crw_native_content.h"
 #include "ios/web/public/web_state/web_state.h"
 #include "ui/base/page_transition_types.h"
 
