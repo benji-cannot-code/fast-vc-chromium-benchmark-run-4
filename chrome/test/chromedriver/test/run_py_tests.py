086b892066c374deb06e94bda1fb3abd7d4ce2b8
samuong <samuong@chromium.org>
2015-06-09 13:05:20 -0700

    [chromedriver] Disable ChromeDriverTest.testCanSwitchToPrintPreviewDialog on Android.
    BUG=chromedriver:1087
    
    Review URL: https://codereview.chromium.org/1174663002
    
    Cr-Commit-Position: refs/heads/master@{#333557}

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -143,6 +143,8 @@ _ANDROID_NEGATIVE_FILTER['chrome'] = (
         'ChromeDriverTest.testShouldHandleNewWindowLoadingProperly',
         # Android doesn't support multiple sessions on one device.
         'SessionHandlingTest.testGetSessions',
+        # Android doesn't use the chrome://print dialog.
+        'ChromeDriverTest.testCanSwitchToPrintPreviewDialog',
     ]
 )
 _ANDROID_NEGATIVE_FILTER['chrome_stable'] = (
