ddce2434c15cefcfcbb77acb4614900ab051aca2
mstensho@opera.com <mstensho@opera.com>
2015-06-09 20:02:03 +0000

    Need deeper layout passes inside flexboxes when fragmenting (pagination, multicol).
    When the fragmentainer (page / column) boundaries have changed, we need to go
    deep when laying out. Objects otherwise not marked for layout may still need
    layout.
    
    We already do this for regular blocks and when inside tables, but it was
    missing for flexbox. Adding this both improves rendering of multicol with
    flexbox and gets rid of an assertion failure.
    
    BUG=497981
    R=cbiesinger@chromium.org,dsinclair@chromium.org
    
    Review URL: https://codereview.chromium.org/1167323003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutFlexibleBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutFlexibleBox.cpp
@@ -234,6 +234,7 @@ void LayoutFlexibleBox::layoutBlock(bool relayoutChildren)
     if (updateLogicalWidthAndColumnWidth())
         relayoutChildren = true;
 
+    SubtreeLayoutScope layoutScope(*this);
     LayoutUnit previousHeight = logicalHeight();
     setLogicalHeight(borderAndPaddingLogicalHeight() + scrollbarLogicalHeight());
 
@@ -250,7 +251,7 @@ void LayoutFlexibleBox::layoutBlock(bool relayoutChildren)
         ChildFrameRects oldChildRects;
         appendChildFrameRects(oldChildRects);
 
-        layoutFlexItems(relayoutChildren);
+        layoutFlexItems(relayoutChildren, layoutScope);
 
         LayoutBlock::finishDelayUpdateScrollInfo();
 
@@ -646,7 +647,7 @@ LayoutUnit LayoutFlexibleBox::computeInnerFlexBaseSizeForChild(LayoutBox& child,
     return std::max(LayoutUnit(), computeMainAxisExtentForChild(child, MainOrPreferredSize, flexBasis));
 }
 
-void LayoutFlexibleBox::layoutFlexItems(bool relayoutChildren)
+void LayoutFlexibleBox::layoutFlexItems(bool relayoutChildren, SubtreeLayoutScope& layoutScope)
 {
     Vector<LineContext> lineContexts;
     OrderedFlexItemList orderedChildren;
@@ -670,7 +671,7 @@ void LayoutFlexibleBox::layoutFlexItems(bool relayoutChildren)
             ASSERT(inflexibleItems.size() > 0);
         }
 
-        layoutAndPlaceChildren(crossAxisOffset, orderedChildren, childSizes, availableFreeSpace, relayoutChildren, lineContexts);
+        layoutAndPlaceChildren(crossAxisOffset, orderedChildren, childSizes, availableFreeSpace, relayoutChildren, layoutScope, lineContexts);
     }
     if (hasLineIfEmpty()) {
         // Even if computeNextFlexLine returns true, the flexbox might not have
@@ -1111,7 +1112,7 @@ EOverflow LayoutFlexibleBox::mainAxisOverflowForChild(LayoutBox& child) const
     return child.styleRef().overflowY();
 }
 
-void LayoutFlexibleBox::layoutAndPlaceChildren(LayoutUnit& crossAxisOffset, const OrderedFlexItemList& children, const Vector<LayoutUnit, 16>& childSizes, LayoutUnit availableFreeSpace, bool relayoutChildren, Vector<LineContext>& lineContexts)
+void LayoutFlexibleBox::layoutAndPlaceChildren(LayoutUnit& crossAxisOffset, const OrderedFlexItemList& children, const Vector<LayoutUnit, 16>& childSizes, LayoutUnit availableFreeSpace, bool relayoutChildren, SubtreeLayoutScope& layoutScope, Vector<LineContext>& lineContexts)
 {
     ASSERT(childSizes.size() == children.size());
 
@@ -1149,6 +1150,8 @@ void LayoutFlexibleBox::layoutAndPlaceChildren(LayoutUnit& crossAxisOffset, cons
         // We may have already forced relayout for orthogonal flowing children in computeInnerFlexBaseSizeForChild.
         bool forceChildRelayout = relayoutChildren && !childFlexBaseSizeRequiresLayout(*child);
         updateBlockChildDirtyBitsBeforeLayout(forceChildRelayout, *child);
+        if (!child->needsLayout())
+            child->markForPaginationRelayoutIfNeeded(layoutScope);
         child->layoutIfNeeded();
 
         updateAutoMarginsInMainAxis(*child, autoMarginOffset);
