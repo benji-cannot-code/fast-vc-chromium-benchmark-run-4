262be7c9cd1042a3f602f0632248b3755cd0e7ef
jbroman@chromium.org <jbroman@chromium.org>
2015-06-09 23:55:04 +0000

    wtf: Add missing operator==(const char*, const AtomicString&).
    Because of this, the following (natural) statement would not compile in tests:
    
      ASSERT_EQ("expected", AtomicString("actual"));
    
    AtomicStringTest didn't catch this because it had the actual and expected values
    reversed in each case that used equality.
    
    Review URL: https://codereview.chromium.org/1175713002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196807 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/text/AtomicString.h
+++ third_party/WebKit/Source/wtf/text/AtomicString.h
@@ -189,6 +189,7 @@ inline bool operator==(const AtomicString& a, const char* b) { return WTF::equal
 inline bool operator==(const AtomicString& a, const Vector<UChar>& b) { return a.impl() && equal(a.impl(), b.data(), b.size()); }
 inline bool operator==(const AtomicString& a, const String& b) { return equal(a.impl(), b.impl()); }
 inline bool operator==(const LChar* a, const AtomicString& b) { return b == a; }
+inline bool operator==(const char* a, const AtomicString& b) { return b == a; }
 inline bool operator==(const String& a, const AtomicString& b) { return equal(a.impl(), b.impl()); }
 inline bool operator==(const Vector<UChar>& a, const AtomicString& b) { return b == a; }
 
@@ -198,6 +199,7 @@ inline bool operator!=(const AtomicString& a, const char* b) { return !(a == b);
 inline bool operator!=(const AtomicString& a, const String& b) { return !equal(a.impl(), b.impl()); }
 inline bool operator!=(const AtomicString& a, const Vector<UChar>& b) { return !(a == b); }
 inline bool operator!=(const LChar* a, const AtomicString& b) { return !(b == a); }
+inline bool operator!=(const char* a, const AtomicString& b) { return !(b == a); }
 inline bool operator!=(const String& a, const AtomicString& b) { return !equal(a.impl(), b.impl()); }
 inline bool operator!=(const Vector<UChar>& a, const AtomicString& b) { return !(a == b); }
 
