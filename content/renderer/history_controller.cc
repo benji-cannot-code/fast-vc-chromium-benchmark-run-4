ed74de5df003ad2fcba9491f0cb9dd82ae85ac5a
clamy <clamy@chromium.org>
2015-06-09 07:44:29 -0700

    Use WebFrame::loadRequest for reloads and history navigations
    This CL follows the blink interface changes of
    https://codereview.chromium.org/1156473002/. It removes the uses of
    WebFrame::reload, WebFrame::reloadWithOverrideURL and WebFrame::loadHistoryItem
    in favor of using WebFrame::loadRequest for all kinds of navigations.
    
    PlzNavigate: reload and history navigations are properly recognized as such at
    commit time when browser-side navigation is enabled.
    
    BUG=490713
    
    Review URL: https://codereview.chromium.org/1157863005
    
    Cr-Commit-Position: refs/heads/master@{#333485}

--- content/renderer/history_controller.cc
+++ content/renderer/history_controller.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/navigation_params.h"
 #include "content/renderer/render_frame_impl.h"
 #include "content/renderer/render_view_impl.h"
+#include "third_party/WebKit/public/web/WebFrameLoadType.h"
 #include "third_party/WebKit/public/web/WebLocalFrame.h"
 
 using blink::WebFrame;
@@ -90,9 +91,11 @@ void HistoryController::GoToEntry(
       continue;
     render_frame->SetPendingNavigationParams(make_scoped_ptr(
         new NavigationParams(*navigation_params_.get())));
-    frame->loadHistoryItem(item.second,
-                           blink::WebHistorySameDocumentLoad,
-                           cache_policy);
+    WebURLRequest request = frame->toWebLocalFrame()->requestFromHistoryItem(
+        item.second, cache_policy);
+    frame->toWebLocalFrame()->load(
+        request, blink::WebFrameLoadType::BackForward, item.second,
+        blink::WebHistorySameDocumentLoad);
   }
   for (const auto& item : different_document_loads) {
     WebFrame* frame = item.first;
@@ -101,9 +104,11 @@ void HistoryController::GoToEntry(
       continue;
     render_frame->SetPendingNavigationParams(make_scoped_ptr(
         new NavigationParams(*navigation_params_.get())));
-    frame->loadHistoryItem(item.second,
-                           blink::WebHistoryDifferentDocumentLoad,
-                           cache_policy);
+    WebURLRequest request = frame->toWebLocalFrame()->requestFromHistoryItem(
+        item.second, cache_policy);
+    frame->toWebLocalFrame()->load(
+        request, blink::WebFrameLoadType::BackForward, item.second,
+        blink::WebHistoryDifferentDocumentLoad);
   }
 }
 
