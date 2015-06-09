8fd723adfe5dfc0b77b895a808ccfdceb7c853d4
yzshen <yzshen@chromium.org>
2015-06-09 10:35:01 -0700

    html_viewer: Add DevToolsAgentImpl.
    DevToolsAgentImpl connects the DevTools mojo service with the DevTools agent support in blink.
    
    BUG=478249
    
    Review URL: https://codereview.chromium.org/1163953002
    
    Cr-Commit-Position: refs/heads/master@{#333516}

--- components/html_viewer/html_document.h
+++ components/html_viewer/html_document.h
@@ -43,6 +43,7 @@ class View;
 namespace html_viewer {
 
 class AxProviderImpl;
+class DevToolsAgentImpl;
 class Setup;
 class WebLayerTreeViewImpl;
 
@@ -171,6 +172,8 @@ class HTMLDocument : public blink::WebViewClient,
 
   FrameTreeManager frame_tree_manager_;
   mojo::Binding<mandoline::FrameTreeClient> frame_tree_manager_binding_;
+
+  scoped_ptr<DevToolsAgentImpl> devtools_agent_;
 
   DISALLOW_COPY_AND_ASSIGN(HTMLDocument);
 };
