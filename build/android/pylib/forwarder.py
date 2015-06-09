c97ca14119aa96386fbbcccb9626b1ac84d0c19e
jbudorick <jbudorick@chromium.org>
2015-06-09 15:20:30 -0700

    [Android] Add check_return=True to Forwarder init & kill.
    BUG=489317
    
    Review URL: https://codereview.chromium.org/1174683002
    
    Cr-Commit-Position: refs/heads/master@{#333594}

--- build/android/pylib/forwarder.py
+++ build/android/pylib/forwarder.py
@@ -290,7 +290,8 @@ class Forwarder(object):
         Forwarder._DEVICE_FORWARDER_FOLDER)])
     cmd = '%s %s' % (tool.GetUtilWrapper(), Forwarder._DEVICE_FORWARDER_PATH)
     device.RunShellCommand(
-        cmd, env={'LD_LIBRARY_PATH': Forwarder._DEVICE_FORWARDER_FOLDER})
+        cmd, env={'LD_LIBRARY_PATH': Forwarder._DEVICE_FORWARDER_FOLDER},
+        check_return=True)
     self._initialized_devices.add(device_serial)
 
   def _KillHostLocked(self):
@@ -327,4 +328,5 @@ class Forwarder(object):
     cmd = '%s %s --kill-server' % (tool.GetUtilWrapper(),
                                    Forwarder._DEVICE_FORWARDER_PATH)
     device.RunShellCommand(
-        cmd, env={'LD_LIBRARY_PATH': Forwarder._DEVICE_FORWARDER_FOLDER})
+        cmd, env={'LD_LIBRARY_PATH': Forwarder._DEVICE_FORWARDER_FOLDER},
+        check_return=True)
