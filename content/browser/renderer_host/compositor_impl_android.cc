cdfa64e46b4171c5652bb8d57082195be7397b97
sunnyps <sunnyps@chromium.org>
2015-06-09 12:26:22 -0700

    Remove unnecessary task runner / PostTask in OutputSurfaceWithoutParent.
    Both OutputSurfaceWithoutParent and CompositorImpl live on the same
    thread. It's unlikely that this will change in the near future. In the
    meantime using a task runner to communicate between the output surface
    and the compositor is confusing.
    
    R=aelias@chromium.org,sievers@chromium.org
    
    Review URL: https://codereview.chromium.org/1171603002
    
    Cr-Commit-Position: refs/heads/master@{#333549}

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -83,7 +83,6 @@ class OutputSurfaceWithoutParent : public cc::OutputSurface {
     capabilities_.adjust_deadline_for_parent = false;
     capabilities_.max_frames_pending = 2;
     compositor_impl_ = compositor_impl;
-    main_thread_ = base::ThreadTaskRunnerHandle::Get();
   }
 
   void SwapBuffers(cc::CompositorFrame* frame) override {
@@ -101,11 +100,8 @@ class OutputSurfaceWithoutParent : public cc::OutputSurface {
     GetCommandBufferProxy()->SetSwapBuffersCompletionCallback(
         swap_buffers_completion_callback_.callback());
 
-    main_thread_->PostTask(
-        FROM_HERE,
-        base::Bind(&CompositorImpl::PopulateGpuCapabilities,
-                   compositor_impl_,
-                   context_provider_->ContextCapabilities().gpu));
+    compositor_impl_->PopulateGpuCapabilities(
+        context_provider_->ContextCapabilities().gpu);
 
     return true;
   }
@@ -131,7 +127,6 @@ class OutputSurfaceWithoutParent : public cc::OutputSurface {
                                 gfx::SwapResult)>
       swap_buffers_completion_callback_;
 
-  scoped_refptr<base::SingleThreadTaskRunner> main_thread_;
   base::WeakPtr<CompositorImpl> compositor_impl_;
 };
 
