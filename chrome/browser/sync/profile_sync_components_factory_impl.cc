7ab37f3c6bce40f9107172a30e721412ef502631
dominicc <dominicc@chromium.org>
2015-06-08 22:10:43 -0700

    Revert of [autofill] Sync server card and address metadata. (patchset #8 id:820001 of https://codereview.chromium.org/1110833002/)
    Reason for revert:
    Chrome Stability sheriff here--I suspect that this is causing crashes on Win, Mac and Android (maybe everything); see https://code.google.com/p/chromium/issues/detail?id=497659#c6
    
    Original issue's description:
    > [autofill] Sync server card and address metadata.
    >
    > BUG=481595
    >
    > Committed: https://crrev.com/7c5be3bcf846d4de1d552342f46abb793127f5c5
    > Cr-Commit-Position: refs/heads/master@{#333202}
    
    TBR=estade@chromium.org,zea@chromium.org,pkasting@chromium.org,rouslan@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=481595
    
    Review URL: https://codereview.chromium.org/1170123002
    
    Cr-Commit-Position: refs/heads/master@{#333448}

--- chrome/browser/sync/profile_sync_components_factory_impl.cc
+++ chrome/browser/sync/profile_sync_components_factory_impl.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "components/autofill/core/browser/webdata/autocomplete_syncable_service.h"
 #include "components/autofill/core/browser/webdata/autofill_profile_syncable_service.h"
-#include "components/autofill/core/browser/webdata/autofill_wallet_metadata_syncable_service.h"
 #include "components/autofill/core/browser/webdata/autofill_wallet_syncable_service.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
 #include "components/autofill/core/common/autofill_pref_names.h"
@@ -521,8 +520,7 @@ base::WeakPtr<syncer::SyncableService> ProfileSyncComponentsFactoryImpl::
         return autofill::AutofillProfileSyncableService::FromWebDataService(
             web_data_service_.get())->AsWeakPtr();
       } else if (type == syncer::AUTOFILL_WALLET_METADATA) {
-        return autofill::AutofillWalletMetadataSyncableService::
-            FromWebDataService(web_data_service_.get())->AsWeakPtr();
+        return base::WeakPtr<syncer::SyncableService>();
       }
       return autofill::AutofillWalletSyncableService::FromWebDataService(
           web_data_service_.get())->AsWeakPtr();
--- components/components_tests.gyp
+++ components/components_tests.gyp
@@ -59,7 +59,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'autofill/core/browser/ui/card_unmask_prompt_controller_impl_unittest.cc',
       'autofill/core/browser/validation_unittest.cc',
       'autofill/core/browser/webdata/autofill_profile_syncable_service_unittest.cc',
-      'autofill/core/browser/webdata/autofill_wallet_metadata_syncable_service_unittest.cc',
       'autofill/core/browser/webdata/autofill_table_unittest.cc',
       'autofill/core/browser/webdata/web_data_service_unittest.cc',
       'autofill/core/common/autofill_regexes_unittest.cc',
