d68c7e3765dcbb734d452f86a3eca16f6d6a3900
caseq@chromium.org <caseq@chromium.org>
2015-06-09 13:48:38 +0000

    DevTools: fix paint profiler in slimming paint mode
    Let Timeline extract recorded picture from cc::DisplayItemList in the same
    way as it used to do with cc::Picture.
    
    BUG=496779
    
    Review URL: https://codereview.chromium.org/1149383009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196764 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineModel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineModel.js
@@ -147,6 +147,7 @@ WebInspector.TimelineModel.RecordType = {
     LazyPixelRef: "LazyPixelRef",
     LayerTreeHostImplSnapshot: "cc::LayerTreeHostImpl",
     PictureSnapshot: "cc::Picture",
+    DisplayItemListSnapshot: "cc::DisplayItemList",
 
     // CpuProfile is a virtual event created on frontend to support
     // serialization of CPU Profiles within tracing timeline data.
@@ -1133,6 +1134,7 @@ WebInspector.TimelineModel.prototype = {
             this._lastPaintForLayer[layerUpdateEvent.args["layerId"]] = event;
             break;
 
+        case recordTypes.DisplayItemListSnapshot:
         case recordTypes.PictureSnapshot:
             var layerUpdateEvent = this._findAncestorEvent(recordTypes.UpdateLayer);
             if (!layerUpdateEvent || layerUpdateEvent.args["layerTreeId"] !== this._inspectedTargetLayerTreeId)
