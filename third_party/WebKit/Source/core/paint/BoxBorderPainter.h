09708a713b9d1a7fffea5f32aed4ce9235a08c94
fmalita@chromium.org <fmalita@chromium.org>
2015-06-09 16:35:03 +0000

    [SP] Always anti-alias borders
    When slimming paint is enabled, skip the CTM dependent heuristic and
    always anti-alias borders.
    
    This relies on heavy use of overdraw, which is now applicable in most
    cases thanks to the recent BoxBorderPainter changes.
    
    BUG=474265
    R=chrishtr@chromium.org,fs@opera.com
    
    Review URL: https://codereview.chromium.org/1167293004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196779 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/BoxBorderPainter.h
+++ third_party/WebKit/Source/core/paint/BoxBorderPainter.h
@@ -29,10 +29,10 @@ public:
 
 private:
     struct ComplexBorderInfo;
-    enum MitreType {
-        NoMitre,
-        SoftMitre, // Anti-aliased
-        HardMitre, // Not anti-aliased
+    enum MiterType {
+        NoMiter,
+        SoftMiter, // Anti-aliased
+        HardMiter, // Not anti-aliased
     };
 
     BorderEdgeFlags paintOpacityGroup(GraphicsContext*, const ComplexBorderInfo&, unsigned index,
@@ -45,11 +45,11 @@ private:
 
     void drawBoxSideFromPath(GraphicsContext*, const LayoutRect&, const Path&, float thickness,
         float drawThickness, BoxSide, Color, EBorderStyle) const;
-    void clipBorderSidePolygon(GraphicsContext*, BoxSide, MitreType mitre1, MitreType mitre2) const;
+    void clipBorderSidePolygon(GraphicsContext*, BoxSide, MiterType miter1, MiterType miter2) const;
     void clipBorderSideForComplexInnerPath(GraphicsContext*, BoxSide) const;
 
-    MitreType computeMitre(BoxSide, BoxSide adjacentSide, BorderEdgeFlags, bool antialias) const;
-    static bool mitresRequireClipping(MitreType mitre1, MitreType mitre2, EBorderStyle, bool antialias);
+    MiterType computeMiter(BoxSide, BoxSide adjacentSide, BorderEdgeFlags, bool antialias) const;
+    static bool mitersRequireClipping(MiterType miter1, MiterType miter2, EBorderStyle, bool antialias);
 
     const BorderEdge& firstEdge() const
     {
