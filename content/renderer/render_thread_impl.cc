e5ae2aefbd034607bad5f5e972f9518568c973c6
kinuko <kinuko@chromium.org>
2015-06-08 20:38:46 -0700

    Reland: Cleanup: Remove extra EmbeddedWorker layering in the renderer SW code
    Original review: https://codereview.chromium.org/1136323003/
    
    Threading fixes from the original patch (PS1):
    - Don't bind WebString (as it's not thread-safe)
    - Introduce internal WorkerContextData struct for holding data
      (specifically callback maps) that need to be bound/destructed on the
      worker thread.
    
    BUG=491039, 492625, 492570
    TEST=existing tests, layout tests
    
    Review URL: https://codereview.chromium.org/1157273009
    
    Cr-Commit-Position: refs/heads/master@{#333437}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -110,8 +110,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/render_view_impl.h"
 #include "content/renderer/renderer_blink_platform_impl.h"
 #include "content/renderer/scheduler/resource_dispatch_throttler.h"
-#include "content/renderer/service_worker/embedded_worker_context_message_filter.h"
 #include "content/renderer/service_worker/embedded_worker_dispatcher.h"
+#include "content/renderer/service_worker/service_worker_context_message_filter.h"
 #include "content/renderer/shared_worker/embedded_shared_worker_stub.h"
 #include "gin/public/debug.h"
 #include "gpu/GLES2/gl2extchromium.h"
@@ -566,7 +566,7 @@ void RenderThreadImpl::Init() {
 
   AddFilter((new CacheStorageMessageFilter(thread_safe_sender()))->GetFilter());
 
-  AddFilter((new EmbeddedWorkerContextMessageFilter())->GetFilter());
+  AddFilter((new ServiceWorkerContextMessageFilter())->GetFilter());
 
   GetContentClient()->renderer()->RenderThreadStarted();
 
