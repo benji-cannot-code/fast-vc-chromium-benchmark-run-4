e9d11553e5b504ff75221f5f9586c98b3c0108d0
yurys@chromium.org <yurys@chromium.org>
2015-06-09 08:03:37 +0000

    Reland "DevTools: extract common inspector bindigns code into InspectorWrapper"
    This CL relands f35316229ae02ef512dee9c8aab9578c6ece2f13 (Oilpan: fix build after r196583.)and
    08a9fc8c50d1df0e5364356ba513b75de50f8475 (DevTools: extract common inspector bindigns code into InspectorWrapper
    ) which were reverted in 14ccb0425b9deda0b575a12aa7e6592d4a6866d7 due to browser test failure which should be fixed by https://src.chromium.org/viewvc/blink?view=rev&revision=196742.
    
    BUG=466631
    
    Review URL: https://codereview.chromium.org/1165453007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196743 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/inspector/V8JavaScriptCallFrame.h
+++ third_party/WebKit/Source/bindings/core/v8/inspector/V8JavaScriptCallFrame.h
@@ -6,18 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef V8JavaScriptCallFrame_h
 #define V8JavaScriptCallFrame_h
 
-#include "core/inspector/JavaScriptCallFrame.h"
+#include "wtf/Forward.h"
+#include <v8.h>
 
 namespace blink {
+
+class JavaScriptCallFrame;
 
 class V8JavaScriptCallFrame {
 public:
     static v8::Local<v8::FunctionTemplate> createWrapperTemplate(v8::Isolate*);
     static v8::Local<v8::Object> wrap(v8::Local<v8::FunctionTemplate> constructorTemplate, v8::Local<v8::Context>, PassRefPtr<JavaScriptCallFrame>);
     static JavaScriptCallFrame* unwrap(v8::Local<v8::Object>);
-
-private:
-    static v8::Local<v8::String> hiddenPropertyName(v8::Isolate*);
 };
 
 } // namespace blink
