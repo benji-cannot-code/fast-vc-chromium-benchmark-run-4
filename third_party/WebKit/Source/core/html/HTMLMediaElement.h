52617f6dc3cb77bd3451b4a20eedbc250872eaeb
mathiash@opera.com <mathiash@opera.com>
2015-06-09 07:20:10 +0000

    Don't allow text selection to start on a HTMLMediaElement
    BUG=449146
    
    Review URL: https://codereview.chromium.org/838003005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196741 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLMediaElement.h
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.h
@@ -318,6 +318,8 @@ private:
     virtual void removedFrom(ContainerNode*) override final;
     virtual void didRecalcStyle(StyleRecalcChange) override final;
 
+    virtual bool canStartSelection() const override { return false; }
+
     virtual void didBecomeFullscreenElement() override final;
     virtual void willStopBeingFullscreenElement() override final;
     virtual bool isInteractiveContent() const override final;
