e7211e0e5c329374f4afb38c9d0bf9066a874982
yukishiino@chromium.org <yukishiino@chromium.org>
2015-06-09 11:34:24 +0000

    bindings: Stop using the given creationContext in public APIs.
    The creation context passed through the public APIs may be unsafe
    and faked by user script.  The callers of the APIs often do not
    check if the context is safe and valid or not.  Plus, there shouldn't
    be a case that a caller needs to handle cross-origin cases, so let us
    always use the current context instead of the creation context given
    through the callers, which are not reliable.
    
    BUG=497507
    
    Review URL: https://codereview.chromium.org/1166793006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196755 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebArrayBufferConverter.cpp
+++ third_party/WebKit/Source/web/WebArrayBufferConverter.cpp
@@ -36,11 +36,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-v8::Local<v8::Value> WebArrayBufferConverter::toV8Value(WebArrayBuffer* buffer, v8::Local<v8::Object> creationContext, v8::Isolate* isolate)
+v8::Local<v8::Value> WebArrayBufferConverter::toV8Value(WebArrayBuffer* buffer, v8::Local<v8::Object> /* creationContext */, v8::Isolate* isolate)
 {
+    // We no longer use |creationContext| because it's often misused and points
+    // to a context faked by user script.
     if (!buffer)
         return v8::Local<v8::Value>();
-    return toV8(PassRefPtr<DOMArrayBuffer>(*buffer), creationContext, isolate);
+    return toV8(PassRefPtr<DOMArrayBuffer>(*buffer), isolate->GetCurrentContext()->Global(), isolate);
 }
 
 WebArrayBuffer* WebArrayBufferConverter::createFromV8Value(v8::Local<v8::Value> value, v8::Isolate* isolate)
