4604415e3cb15167e325dddb8645d4ed34bc2521
magjed <magjed@chromium.org>
2015-06-10 05:12:21 -0700

    FakeVideoCaptureDevice: Regulate framerate without drifting
    We currently rely on PostDelayedTask with a constant frame interval to get a specified frame rate.
    This CL dynamically adapts the delay to PostDelayedTask to keep the frame rate in sync.
    
    This CL is similar to https://codereview.chromium.org/1162903004/.
    
    BUG=492189
    
    Review URL: https://codereview.chromium.org/1153713006
    
    Cr-Commit-Position: refs/heads/master@{#333723}

--- media/video/capture/fake_video_capture_device.h
+++ media/video/capture/fake_video_capture_device.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread.h"
 #include "base/threading/thread_checker.h"
+#include "base/time/time.h"
 #include "media/video/capture/video_capture_device.h"
 
 namespace media {
@@ -42,9 +43,12 @@ class MEDIA_EXPORT FakeVideoCaptureDevice : public VideoCaptureDevice {
  private:
   static const int kFakeCapturePeriodMs = 50;
 
-  void CaptureUsingOwnBuffers();
-  void CaptureUsingClientBuffers(VideoPixelFormat pixel_format);
-  void BeepAndScheduleNextCapture(const base::Closure& next_capture);
+  void CaptureUsingOwnBuffers(base::TimeTicks expected_execution_time);
+  void CaptureUsingClientBuffers(VideoPixelFormat pixel_format,
+                                 base::TimeTicks expected_execution_time);
+  void BeepAndScheduleNextCapture(
+      base::TimeTicks expected_execution_time,
+      const base::Callback<void(base::TimeTicks)>& next_capture);
 
   // |thread_checker_| is used to check that all methods are called in the
   // correct thread that owns the object.
