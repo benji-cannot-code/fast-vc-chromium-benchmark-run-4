daa8b41ce3c9a0a0fc6ea9ae154ffab84749aff5
kelvinp <kelvinp@chromium.org>
2015-06-09 18:57:34 -0700

    [AppRemoting] Implements LicenseManager.
    This CL defines the LicenseManager interface and implements a dummy
    GaiaLicenseManager that returns the user's Gaia token.
    
    BUG=b/19799333
    
    Review URL: https://codereview.chromium.org/1163023002
    
    Cr-Commit-Position: refs/heads/master@{#333645}

--- remoting/webapp/app_remoting/js/app_remoting_activity.js
+++ remoting/webapp/app_remoting/js/app_remoting_activity.js
@@ -29,11 +29,13 @@ remoting.AppHostResponse;
  * @param {Array<string>} appCapabilities Array of application capabilities.
  * @param {remoting.Application} app
  * @param {remoting.WindowShape} windowShape
+ * @param {string} subscriptionToken
  *
  * @constructor
  * @implements {remoting.Activity}
  */
-remoting.AppRemotingActivity = function(appCapabilities, app, windowShape) {
+remoting.AppRemotingActivity = function(appCapabilities, app, windowShape,
+                                        subscriptionToken) {
   /** @private */
   this.sessionFactory_ = new remoting.ClientSessionFactory(
       document.querySelector('#client-container .client-plugin-container'),
@@ -50,6 +52,9 @@ remoting.AppRemotingActivity = function(appCapabilities, app, windowShape) {
 
   /** @private */
   this.windowShape_ = windowShape;
+
+  /** @private */
+  this.subscriptionToken_ = subscriptionToken;
 };
 
 remoting.AppRemotingActivity.prototype.dispose = function() {
@@ -91,6 +96,7 @@ remoting.AppRemotingActivity.prototype.cleanup_ = function() {
 remoting.AppRemotingActivity.prototype.getAppHostInfo_ = function(token) {
   var url = remoting.settings.APP_REMOTING_API_BASE_URL + '/applications/' +
             remoting.settings.getAppRemotingApplicationId() + '/run';
+  // TODO(kelvinp): Passes |this.subscriptionToken_| to the XHR.
   return new remoting.AutoRetryXhr({
     method: 'POST',
     url: url,
