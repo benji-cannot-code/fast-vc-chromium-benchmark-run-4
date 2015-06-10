9fa3678c3d8a8e70ba2e36363f5643d7b4d06266
kkhorimoto <kkhorimoto@chromium.org>
2015-06-10 15:36:33 -0700

    Fixed PageScrollState application.
    This fixes the bug when the NavigationItem that owns the
    PageScrollState being applied is deallocated before application
    can occur.
    
    BUG= 498361
    
    Review URL: https://codereview.chromium.org/1163403005
    
    Cr-Commit-Position: refs/heads/master@{#333821}

--- ios/web/public/web_state/page_scroll_state.mm
+++ ios/web/public/web_state/page_scroll_state.mm
@@ -70,4 +70,8 @@ bool PageScrollState::operator==(const PageScrollState& other) const {
          StateValuesAreEqual(zoom_scale_, other.zoom_scale_);
 }
 
+bool PageScrollState::operator!=(const PageScrollState& other) const {
+  return !(*this == other);
+}
+
 }  // namespace web
