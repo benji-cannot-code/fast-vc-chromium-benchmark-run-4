a313e203b3ea12c835f34cd965206eef3a3a52bf
mohsen <mohsen@chromium.org>
2015-06-10 09:58:40 -0700

    Add DCHECKs for the touch selection menu runner singleton
    This is to ensure that there exists at most one instance of the menu
    runner simultaneously. This fix is now possible because of the
    ViewsDelegate refactoring.
    
    BUG=399721,492991
    
    Review URL: https://codereview.chromium.org/1173083002
    
    Cr-Commit-Position: refs/heads/master@{#333747}

--- ui/touch_selection/touch_selection_menu_runner.cc
+++ ui/touch_selection/touch_selection_menu_runner.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/touch_selection/touch_selection_menu_runner.h"
 
+#include "base/logging.h"
+
 namespace ui {
 namespace {
 
@@ -13,6 +15,7 @@ TouchSelectionMenuRunner* g_touch_selection_menu_runner = nullptr;
 }  // namespace
 
 TouchSelectionMenuRunner::~TouchSelectionMenuRunner() {
+  DCHECK_EQ(this, g_touch_selection_menu_runner);
   g_touch_selection_menu_runner = nullptr;
 }
 
@@ -21,11 +24,7 @@ TouchSelectionMenuRunner* TouchSelectionMenuRunner::GetInstance() {
 }
 
 TouchSelectionMenuRunner::TouchSelectionMenuRunner() {
-  // TODO(mohsen): Ideally we should DCHECK that |g_touch_selection_menu_runner|
-  // is not set here, in order to make sure we don't create multiple menu
-  // runners accidentally. Currently, this is not possible because we can have
-  // multiple ViewsDelegate's at the same time which should not happen. See
-  // crbug.com/492991.
+  DCHECK(!g_touch_selection_menu_runner);
   g_touch_selection_menu_runner = this;
 }
 
