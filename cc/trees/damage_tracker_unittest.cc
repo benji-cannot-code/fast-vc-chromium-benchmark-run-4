53871abc503f87f9e8e993be274cada96387a5b0
miletus <miletus@chromium.org>
2015-06-09 18:27:47 -0700

    Force property_trees rebuild for DamageTrackerTest
    DamageTrackerTest does a lot of impl side operations that could not
    happen in practice, e.g. LayerImpl::SetPosition(),
    LayerImpl::Add/RemoveChild(). These operations won't trigger property
    trees rebuild/update so the verification on draw_transform would fail.
    
    The simple fix in this case seems to be forcing a property trees
    rebuild.
    
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1170383003
    
    Cr-Commit-Position: refs/heads/master@{#333638}

--- cc/trees/damage_tracker_unittest.cc
+++ cc/trees/damage_tracker_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/test/test_shared_bitmap_manager.h"
 #include "cc/test/test_task_graph_runner.h"
 #include "cc/trees/layer_tree_host_common.h"
+#include "cc/trees/layer_tree_impl.h"
 #include "cc/trees/single_thread_proxy.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/effects/SkBlurImageFilter.h"
@@ -48,6 +49,7 @@ void ClearDamageForAllSurfaces(LayerImpl* layer) {
 }
 
 void EmulateDrawingOneFrame(LayerImpl* root) {
+  root->layer_tree_impl()->property_trees()->needs_rebuild = true;
   // This emulates only steps that are relevant to testing the damage tracker:
   //   1. computing the render passes and layerlists
   //   2. updating all damage trackers in the correct order
