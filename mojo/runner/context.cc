e2ccf556f5934ba442c72074dba4e20bef852d85
yzshen <yzshen@chromium.org>
2015-06-10 16:11:03 -0700

    html_viewer: Add DevToolsAgentImpl.
    DevToolsAgentImpl connects the DevTools mojo service with the DevTools agent support in blink.
    
    BUG=478249
    
    Committed: https://crrev.com/32166090d27c6450957f59eedaa6ea5fa12762e4
    Cr-Commit-Position: refs/heads/master@{#333516}
    
    Review URL: https://codereview.chromium.org/1163953002
    
    Cr-Commit-Position: refs/heads/master@{#333834}

--- mojo/runner/context.cc
+++ mojo/runner/context.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
+#include "components/devtools_service/public/cpp/switches.h"
 #include "components/devtools_service/public/interfaces/devtools_service.mojom.h"
 #include "mojo/application/public/cpp/application_connection.h"
 #include "mojo/application/public/cpp/application_delegate.h"
@@ -175,15 +176,16 @@ void InitNativeOptions(shell::ApplicationManager* manager,
 
 void InitDevToolsServiceIfNeeded(shell::ApplicationManager* manager,
                                  const base::CommandLine& command_line) {
-  if (!command_line.HasSwitch(switches::kRemoteDebuggingPort))
+  if (!command_line.HasSwitch(devtools_service::kRemoteDebuggingPort))
     return;
 
   std::string port_str =
-      command_line.GetSwitchValueASCII(switches::kRemoteDebuggingPort);
+      command_line.GetSwitchValueASCII(devtools_service::kRemoteDebuggingPort);
   unsigned port;
   if (!base::StringToUint(port_str, &port) || port > 65535) {
-    LOG(ERROR) << "Invalid value for switch " << switches::kRemoteDebuggingPort
-               << ": '" << port_str << "' is not a valid port number.";
+    LOG(ERROR) << "Invalid value for switch "
+               << devtools_service::kRemoteDebuggingPort << ": '" << port_str
+               << "' is not a valid port number.";
     return;
   }
 
