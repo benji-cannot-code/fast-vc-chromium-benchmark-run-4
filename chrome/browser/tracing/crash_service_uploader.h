024094de80416024662d33f98829a6550c19af1e
simonhatch <simonhatch@chromium.org>
2015-06-10 08:19:37 -0700

    Slow Reports - Allow URL to be set on TraceCrashServiceUploader.
    We can use this for both Deep Reports (since those get sent to a different server), and for Slow Reports will need this to route to another endpoint.
    
    BUG=498419
    
    Review URL: https://codereview.chromium.org/1164313006
    
    Cr-Commit-Position: refs/heads/master@{#333736}

--- chrome/browser/tracing/crash_service_uploader.h
+++ chrome/browser/tracing/crash_service_uploader.h
@@ -39,6 +39,8 @@ class TraceCrashServiceUploader : public content::TraceUploader,
       net::URLRequestContextGetter* request_context);
   ~TraceCrashServiceUploader() override;
 
+  void SetUploadURL(const std::string& url);
+
   // net::URLFetcherDelegate implementation.
   void OnURLFetchComplete(const net::URLFetcher* source) override;
   void OnURLFetchUploadProgress(const net::URLFetcher* source,
@@ -52,6 +54,7 @@ class TraceCrashServiceUploader : public content::TraceUploader,
 
  private:
   void DoUploadOnFileThread(const std::string& file_contents,
+                            const std::string& upload_url,
                             const UploadProgressCallback& progress_callback,
                             const UploadDoneCallback& done_callback);
   // Sets up a multipart body to be uploaded. The body is produced according
@@ -78,6 +81,8 @@ class TraceCrashServiceUploader : public content::TraceUploader,
   UploadDoneCallback done_callback_;
 
   net::URLRequestContextGetter* request_context_;
+
+  std::string upload_url_;
 
   DISALLOW_COPY_AND_ASSIGN(TraceCrashServiceUploader);
 };
