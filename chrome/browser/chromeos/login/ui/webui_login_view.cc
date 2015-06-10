357a9feeb9a69570feeb40882b5f7bfd2a081cb5
achuith <achuith@chromium.org>
2015-06-10 12:43:06 -0700

    Disable ctrl-alt-shift-w on beta/stable channels.
    BUG=497898
    
    Review URL: https://codereview.chromium.org/1167353002
    
    Cr-Commit-Position: refs/heads/master@{#333777}

--- chrome/browser/chromeos/login/ui/webui_login_view.cc
+++ chrome/browser/chromeos/login/ui/webui_login_view.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sessions/session_tab_helper.h"
 #include "chrome/browser/ui/autofill/chrome_autofill_client.h"
 #include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
+#include "chrome/common/chrome_version_info.h"
 #include "chrome/common/render_messages.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/session_manager_client.h"
@@ -132,9 +133,6 @@ WebUILoginView::WebUILoginView()
   accel_map_[ui::Accelerator(ui::VKEY_X,
       ui::EF_CONTROL_DOWN | ui::EF_ALT_DOWN | ui::EF_SHIFT_DOWN)] =
       kAccelNameEnableDebugging;
-  accel_map_[ui::Accelerator(ui::VKEY_W,
-      ui::EF_CONTROL_DOWN | ui::EF_ALT_DOWN | ui::EF_SHIFT_DOWN)] =
-      kAccelNameToggleWebviewSignin;
   accel_map_[ui::Accelerator(
       ui::VKEY_L, ui::EF_CONTROL_DOWN | ui::EF_ALT_DOWN | ui::EF_SHIFT_DOWN)] =
       kAccelNameToggleNewLoginUI;
@@ -146,6 +144,16 @@ WebUILoginView::WebUILoginView()
       kAccelFocusPrev;
   accel_map_[ui::Accelerator(ui::VKEY_RIGHT, ui::EF_NONE)] =
       kAccelFocusNext;
+
+  // Ctrl-Alt-Shift-W for canary/dev builds only.
+  const chrome::VersionInfo::Channel channel =
+      chrome::VersionInfo::GetChannel();
+  if (channel != chrome::VersionInfo::CHANNEL_STABLE &&
+      channel != chrome::VersionInfo::CHANNEL_BETA) {
+    accel_map_[ui::Accelerator(
+        ui::VKEY_W, ui::EF_CONTROL_DOWN | ui::EF_ALT_DOWN |
+                        ui::EF_SHIFT_DOWN)] = kAccelNameToggleWebviewSignin;
+  }
 
   // Use KEY_RELEASED because Gaia consumes KEY_PRESSED for up/down key.
   ui::Accelerator key_up(ui::VKEY_UP, ui::EF_NONE);
