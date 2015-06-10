a53eaedff0b42687999a266cb8dca5b5cd5c50cb
alexclarke <alexclarke@chromium.org>
2015-06-10 08:52:39 -0700

    Pipe impl_latency_takes_priority_ to the RenderScheduler.
    The scheduler will use this as a signal to help determine when blink
    timers can be limited to running only during idle time.
    
    BUG=463143
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1165853002
    
    Cr-Commit-Position: refs/heads/master@{#333740}

--- cc/output/begin_frame_args.h
+++ cc/output/begin_frame_args.h
@@ -84,6 +84,7 @@ struct CC_EXPORT BeginFrameArgs {
   base::TimeTicks deadline;
   base::TimeDelta interval;
   BeginFrameArgsType type;
+  bool on_critical_path;
 
  private:
   BeginFrameArgs(base::TimeTicks frame_time,
