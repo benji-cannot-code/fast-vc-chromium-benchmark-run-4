1a186af415f59e78c4d51c003cbd544a8fc755c0
reveman <reveman@chromium.org>
2015-06-10 15:41:55 -0700

    Re-land: content: Single process support for native GpuMemoryBuffers.
    This adds single process support for IOSurface and
    SurfaceTexture backed GpuMemoryBuffers.
    
    Also makes the SurfaceTexture code more consistent
    with IOSurface code by using a singleton instead of
    leaking a raw pointer.
    
    BUG=497559
    TEST=content_browsertests --gtest_also_run_disabled_tests --gtest_filter=ChildThreadImplGpuMemoryBufferBrowserTests/*
    
    Review URL: https://codereview.chromium.org/1120873002
    
    Cr-Commit-Position: refs/heads/master@{#333822}

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -87,6 +87,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "content/browser/android/browser_startup_controller.h"
 #include "content/browser/android/browser_surface_texture_manager.h"
+#include "content/browser/android/in_process_surface_texture_manager.h"
 #include "content/browser/android/tracing_controller_android.h"
 #include "content/browser/screen_orientation/screen_orientation_delegate_android.h"
 #include "content/public/browser/screen_orientation_provider.h"
@@ -103,6 +104,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/browser_io_surface_manager_mac.h"
 #include "content/browser/cocoa/system_hotkey_helper_mac.h"
 #include "content/browser/compositor/browser_compositor_view_mac.h"
+#include "content/browser/in_process_io_surface_manager_mac.h"
 #include "content/browser/theme_helper_mac.h"
 #endif
 
@@ -590,7 +592,13 @@ void BrowserMainLoop::PostMainMessageLoopStart() {
 #if defined(OS_ANDROID)
   {
     TRACE_EVENT0("startup", "BrowserMainLoop::Subsystem:SurfaceTextureManager");
-    SurfaceTextureManager::SetInstance(new BrowserSurfaceTextureManager);
+    if (parsed_command_line_.HasSwitch(switches::kSingleProcess)) {
+      SurfaceTextureManager::SetInstance(
+          InProcessSurfaceTextureManager::GetInstance());
+    } else {
+      SurfaceTextureManager::SetInstance(
+          BrowserSurfaceTextureManager::GetInstance());
+    }
   }
 
   if (!parsed_command_line_.HasSwitch(
@@ -606,7 +614,11 @@ void BrowserMainLoop::PostMainMessageLoopStart() {
 #if defined(OS_MACOSX) && !defined(OS_IOS)
   {
     TRACE_EVENT0("startup", "BrowserMainLoop::Subsystem:IOSurfaceManager");
-    IOSurfaceManager::SetInstance(BrowserIOSurfaceManager::GetInstance());
+    if (parsed_command_line_.HasSwitch(switches::kSingleProcess)) {
+      IOSurfaceManager::SetInstance(InProcessIOSurfaceManager::GetInstance());
+    } else {
+      IOSurfaceManager::SetInstance(BrowserIOSurfaceManager::GetInstance());
+    }
   }
 #endif
 
