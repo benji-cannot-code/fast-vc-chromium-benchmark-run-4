af3af57c9b8805cd5fe0f2ae1436ed0977c36d78
rch <rch@chromium.org>
2015-06-10 14:27:23 -0700

    Add a new prefer_aes finch options for QUIC.
    Review URL: https://codereview.chromium.org/1165283004
    
    Cr-Commit-Position: refs/heads/master@{#333805}

--- net/quic/quic_stream_factory.cc
+++ net/quic/quic_stream_factory.cc
@@ -554,6 +554,7 @@ QuicStreamFactory::QuicStreamFactory(
     bool enable_connection_racing,
     bool enable_non_blocking_io,
     bool disable_disk_cache,
+    bool prefer_aes,
     int max_number_of_lossy_connections,
     float packet_loss_threshold,
     int socket_receive_buffer_size,
@@ -579,6 +580,7 @@ QuicStreamFactory::QuicStreamFactory(
       enable_connection_racing_(enable_connection_racing),
       enable_non_blocking_io_(enable_non_blocking_io),
       disable_disk_cache_(disable_disk_cache),
+      prefer_aes_(prefer_aes),
       max_number_of_lossy_connections_(max_number_of_lossy_connections),
       packet_loss_threshold_(packet_loss_threshold),
       socket_receive_buffer_size_(socket_receive_buffer_size),
@@ -603,7 +605,7 @@ QuicStreamFactory::QuicStreamFactory(
   bool has_aes_hardware_support = cpu.has_aesni() && cpu.has_avx();
   UMA_HISTOGRAM_BOOLEAN("Net.QuicSession.PreferAesGcm",
                         has_aes_hardware_support);
-  if (has_aes_hardware_support)
+  if (has_aes_hardware_support || prefer_aes_)
     crypto_config_.PreferAesGcm();
   if (!IsEcdsaSupported())
     crypto_config_.DisableEcdsa();
