a364517d56e8efcccd50b3a505128ba44479ed75
brianderson <brianderson@chromium.org>
2015-06-09 20:08:53 -0700

    cc: DCHECK if SDOS::SwapBuffersComplete is called prematurely
    This is for debugging purposes only and will be reverted
    soon.
    
    BUG=495650
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1175703004
    
    Cr-Commit-Position: refs/heads/master@{#333667}

--- cc/scheduler/scheduler.cc
+++ cc/scheduler/scheduler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/auto_reset.h"
+#include "base/debug/stack_trace.h"
 #include "base/logging.h"
 #include "base/profiler/scoped_tracker.h"
 #include "base/single_thread_task_runner.h"
@@ -227,7 +228,8 @@ void Scheduler::DidSwapBuffers() {
 }
 
 void Scheduler::DidSwapBuffersComplete() {
-  DCHECK_GT(state_machine_.pending_swaps(), 0) << AsValue()->ToString();
+  DCHECK_GT(state_machine_.pending_swaps(), 0)
+      << AsValue()->ToString() << base::debug::StackTrace().ToString();
   state_machine_.DidSwapBuffersComplete();
   ProcessScheduledActions();
 }
