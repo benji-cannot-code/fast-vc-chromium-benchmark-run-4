2df1b44442f237950a09c40431114ee06d1ac104
dalecurtis <dalecurtis@chromium.org>
2015-06-09 20:17:55 -0700

    Improve audio/video sync during underflow, reduce underflow frequency.
    The current approach doesn't account for expired frames when resuming
    after underflow, which means we may resume and then immediately fail
    again; these frames are also out of sync with the audio playing out.
    Fixing this requires several changes to the pipeline:
    
    - TimeSource::ConvertMediaTimestamps() will now always convert timestamps,
    even when time is stopped.  When time is stopped the converted values are
    based on the last known media wall clock time.
    
    - TimeSource::ConvertMediaTimestamps() will now return the current media
    wall clock time when an empty timestamps vector is given; this value is
    used to figure out which video frames have been played out already.
    
    - Introduce AudioClock::CompensateForSuspendedWrites() to ensure the audio
    delay value is correctly used when return the current wall clock time.
    
    - Introduce |was_time_moving_| to VideoRendererAlgorithm to ensure the
    |last_deadline_max_| value, used by RemoveExpiredFrames, is only changed
    when time is ticking; otherwise the value given to RemoveExpiredFrames is
    overwritten by Render() calls and EffectiveFramesQueued() will vary.
    
    - Modifies VideoRendererImpl::FrameReady() to remove expired frames based
    on the last known media wall clock time during underflow states.
    
    Combined these changes lead to a noticeable improvement in audio/video sync
    during underflow as well as a greatly reduced number of underflow events.
    
    BUG=498525
    TEST=lots of new tests.
    
    Review URL: https://codereview.chromium.org/1160853006
    
    Cr-Commit-Position: refs/heads/master@{#333672}

--- media/renderers/audio_renderer_impl.h
+++ media/renderers/audio_renderer_impl.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 class SingleThreadTaskRunner;
+class TickClock;
 }
 
 namespace media {
@@ -218,6 +219,9 @@ class MEDIA_EXPORT AudioRendererImpl
   // Callback provided to Flush().
   base::Closure flush_cb_;
 
+  // Overridable tick clock for testing.
+  scoped_ptr<base::TickClock> tick_clock_;
+
   // After Initialize() has completed, all variables below must be accessed
   // under |lock_|. ------------------------------------------------------------
   base::Lock lock_;
@@ -258,7 +262,8 @@ class MEDIA_EXPORT AudioRendererImpl
   base::TimeTicks last_render_time_;
 
   // Set to the value of |last_render_time_| when StopRendering_Locked() is
-  // called for any reason.  Cleared by the next successful Render() call.
+  // called for any reason.  Cleared by the next successful Render() call after
+  // being used to adjust for lost time between the last call.
   base::TimeTicks stop_rendering_time_;
 
   // Set upon receipt of the first decoded buffer after a StartPlayingFrom().
