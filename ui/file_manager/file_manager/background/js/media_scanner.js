2be050b5a95428a3d4e499b99d6d42915e441038
smckay <smckay@chromium.org>
2015-06-10 13:03:48 -0700

    WIP: Show progress bar during scanning.
    BUG=475182
    
    Review URL: https://codereview.chromium.org/1174653002
    
    Cr-Commit-Position: refs/heads/master@{#333779}

--- ui/file_manager/file_manager/background/js/media_scanner.js
+++ ui/file_manager/file_manager/background/js/media_scanner.js
@@ -148,6 +148,7 @@ importer.DefaultMediaScanner.prototype.scanFiles = function(entries) {
         this.notify_(importer.ScanEvent.INVALIDATED, scan);
       }.bind(this));
 
+  scan.setCandidateCount(entries.length);
   var scanPromises = entries.map(this.onUniqueFileFound_.bind(this, scan));
 
   Promise.all(scanPromises)
@@ -179,6 +180,7 @@ importer.DefaultMediaScanner.SCAN_BATCH_SIZE = 1;
  */
 importer.DefaultMediaScanner.prototype.scanMediaFiles_ =
     function(scan, entries) {
+  scan.setCandidateCount(entries.length);
   var handleFileEntry = this.onFileEntryFound_.bind(this, scan);
 
   /**
@@ -275,6 +277,7 @@ importer.DefaultMediaScanner.prototype.crawlDirectory_ =
  */
 importer.DefaultMediaScanner.prototype.onFileEntryFound_ =
     function(scan, entry) {
+
   return this.getDisposition_(entry, importer.Destination.GOOGLE_DRIVE)
       .then(
           /**
@@ -301,7 +304,9 @@ importer.DefaultMediaScanner.prototype.onFileEntryFound_ =
 importer.DefaultMediaScanner.prototype.onUniqueFileFound_ =
     function(scan, entry) {
 
+  scan.onCandidatesProcessed(1);
   if (!importer.isEligibleType(entry)) {
+    this.notify_(importer.ScanEvent.UPDATED, scan);
     return Promise.resolve();
   }
 
@@ -330,7 +335,9 @@ importer.DefaultMediaScanner.prototype.onUniqueFileFound_ =
  */
 importer.DefaultMediaScanner.prototype.onDuplicateFileFound_ =
     function(scan, entry, disposition) {
+  scan.onCandidatesProcessed(1);
   scan.addDuplicateEntry(entry, disposition);
+  this.notify_(importer.ScanEvent.UPDATED, scan);
   return Promise.resolve();
 };
 
@@ -357,6 +364,19 @@ importer.ScanResult.prototype.cancel;
  * work started prior to cancelation may still be ongoing.
  */
 importer.ScanResult.prototype.canceled;
+
+/**
+ * @param {number} count Sets the total number of candidate entries
+ *     that were checked while scanning. Used when determining
+ *     total progress.
+ */
+importer.ScanResult.prototype.setCandidateCount;
+
+/**
+ * Event method called when a candidate has been processed.
+ * @param {number} count
+ */
+importer.ScanResult.prototype.onCandidatesProcessed;
 
 /**
  * Returns all files entries discovered so far. The list will be
@@ -400,7 +420,13 @@ importer.ScanResult.prototype.getStatistics;
  *   scanDuration: number,
  *   newFileCount: number,
  *   duplicates: !Object<!importer.Disposition, number>,
- *   sizeBytes: number
+ *   sizeBytes: number,
+ *   candidates: {
+ *     total: number,
+ *     processed: number,
+ *   }
+ *   scannedFiles: number,
+ *   progress: number
  * }}
  */
 importer.ScanResult.Statistics;
@@ -428,6 +454,12 @@ importer.DefaultScanResult = function(hashGenerator) {
 
   /** @private {function(!FileEntry): !Promise.<string>} */
   this.createHashcode_ = hashGenerator;
+
+  /** @private {number} */
+  this.candidateCount_ = 0;
+
+  /** @private {number} */
+  this.candidatesProcessed_ = 0;
 
   /**
    * List of file entries found while scanning.
@@ -497,6 +529,16 @@ importer.DefaultScanResult.prototype = {
 /** @override */
 importer.DefaultScanResult.prototype.isFinal = function() {
   return this.resolver_.settled;
+};
+
+/** @override */
+importer.DefaultScanResult.prototype.setCandidateCount = function(count) {
+  this.candidateCount_ = count;
+};
+
+/** @override */
+importer.DefaultScanResult.prototype.onCandidatesProcessed = function(count) {
+  this.candidatesProcessed_ = this.candidatesProcessed_ + count;
 };
 
 /** @override */
@@ -596,8 +638,32 @@ importer.DefaultScanResult.prototype.getStatistics = function() {
         this.lastScanActivity_.getTime() - this.scanStarted_.getTime(),
     newFileCount: this.fileEntries_.length,
     duplicates: this.duplicateStats_,
-    sizeBytes: this.totalBytes_
+    sizeBytes: this.totalBytes_,
+    candidates: {
+      total: this.candidateCount_,
+      processed: this.candidatesProcessed_
+    },
+    progress: this.calculateProgress_()
   };
+};
+
+/**
+ * @return {number} Progress as an integer from 0-100.
+ * @private
+ */
+importer.DefaultScanResult.prototype.calculateProgress_ = function() {
+  var progress = (this.candidateCount_ > 0) ?
+      Math.floor(this.candidatesProcessed_ / this.candidateCount_ * 100) :
+      0;
+
+  // In case candate count was off, or any other mischief has happened,
+  // we want to ensure progress never exceeds 100.
+  if (progress > 100) {
+    console.warn('Progress exceeded 100.');
+    progress = 100;
+  }
+
+  return progress;
 };
 
 /**
--- ui/file_manager/file_manager/foreground/js/import_controller.js
+++ ui/file_manager/file_manager/foreground/js/import_controller.js
@@ -570,6 +570,13 @@ importer.RuntimeCommandWidget = function() {
   /** @private {Element} */
   this.detailsBanner_ = document.querySelector('#cloud-import-details .banner');
 
+  /** @private {Element} */
+  this.progressContainer_ =
+      document.querySelector('#cloud-import-details .progress');
+  /** @private {Element} */
+  this.progressBar_ =
+      document.querySelector('#cloud-import-details .progress .value');
+
   /** @private {function(!importer.ClickSource)} */
   this.clickListener_;
 
@@ -734,6 +741,7 @@ importer.RuntimeCommandWidget.prototype.update =
       this.sideButton_.hidden = false;
       this.importButton_.hidden = true;
       this.cancelButton_.hidden = false;
+      this.progressContainer_.hidden = true;
 
       this.toolbarIcon_.setAttribute('icon', 'autorenew');
       this.statusIcon_.setAttribute('icon', 'autorenew');
@@ -753,6 +761,7 @@ importer.RuntimeCommandWidget.prototype.update =
       this.sideButton_.hidden = false;
       this.importButton_.hidden = true;
       this.cancelButton_.hidden = true;
+      this.progressContainer_.hidden = true;
 
       this.toolbarIcon_.setAttribute('icon', 'cloud-off');
       this.statusIcon_.setAttribute('icon', 'image:photo');
@@ -768,6 +777,7 @@ importer.RuntimeCommandWidget.prototype.update =
       this.sideButton_.hidden = false;
       this.importButton_.hidden = true;
       this.cancelButton_.hidden = true;
+      this.progressContainer_.hidden = true;
 
       this.toolbarIcon_.setAttribute('icon', 'cloud-done');
       this.statusIcon_.setAttribute('icon', 'cloud-done');
@@ -787,6 +797,7 @@ importer.RuntimeCommandWidget.prototype.update =
       this.sideButton_.hidden = false;
       this.importButton_.hidden = false;
       this.cancelButton_.hidden = true;
+      this.progressContainer_.hidden = true;
 
       this.toolbarIcon_.setAttribute('icon', 'cloud-upload');
       this.statusIcon_.setAttribute('icon', 'image:photo');
@@ -806,6 +817,10 @@ importer.RuntimeCommandWidget.prototype.update =
       this.importButton_.hidden = true;
       // TODO(smckay): implement cancellation for scanning.
       this.cancelButton_.hidden = true;
+      this.progressContainer_.hidden = false;
+
+      var stats = opt_scan.getStatistics();
+      this.progressBar_.style.width = stats.progress + '%';
 
       this.toolbarIcon_.setAttribute('icon', 'autorenew');
       this.statusIcon_.setAttribute('icon', 'autorenew');
