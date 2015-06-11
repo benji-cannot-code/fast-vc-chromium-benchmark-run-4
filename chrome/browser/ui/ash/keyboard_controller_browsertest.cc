d69cefa30b8c0857093d9d08709ee6d2ef64500a
bshe <bshe@chromium.org>
2015-06-11 08:22:53 -0700

    Fix centered vk when switch to a new extension while in floating mode
    BUG=489366
    
    Review URL: https://codereview.chromium.org/1167733002
    
    Cr-Commit-Position: refs/heads/master@{#333954}

--- chrome/browser/ui/ash/keyboard_controller_browsertest.cc
+++ chrome/browser/ui/ash/keyboard_controller_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ash/shell.h"
 #include "base/command_line.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/browser/web_contents.h"
@@ -21,8 +22,8 @@ const int kKeyboardHeightForTest = 100;
 
 class VirtualKeyboardWebContentTest : public InProcessBrowserTest {
  public:
-  VirtualKeyboardWebContentTest() {};
-  ~VirtualKeyboardWebContentTest() override{};
+  VirtualKeyboardWebContentTest() {}
+  ~VirtualKeyboardWebContentTest() override {}
 
   void SetUp() override {
     ui::SetUpInputMethodFactoryForTesting();
@@ -40,12 +41,14 @@ class VirtualKeyboardWebContentTest : public InProcessBrowserTest {
   }
 
  protected:
-  void FocusEditableNodeAndShowKeyboard() {
+  void FocusEditableNodeAndShowKeyboard(const gfx::Rect& init_bounds) {
     client.reset(new ui::DummyTextInputClient(ui::TEXT_INPUT_TYPE_TEXT));
     ui::InputMethod* input_method = proxy()->GetInputMethod();
     input_method->SetFocusedTextInputClient(client.get());
     input_method->ShowImeIfNeeded();
-    ResizeKeyboardWindow();
+    // Mock window.resizeTo that is expected to be called after navigate to a
+    // new virtual keyboard.
+    proxy()->GetKeyboardWindow()->SetBounds(init_bounds);
   }
 
   void FocusNonEditableNode() {
@@ -54,10 +57,13 @@ class VirtualKeyboardWebContentTest : public InProcessBrowserTest {
     input_method->SetFocusedTextInputClient(client.get());
   }
 
-  void MockEnableIMEInDifferentExtension(const std::string& url) {
+  void MockEnableIMEInDifferentExtension(const std::string& url,
+                                         const gfx::Rect& init_bounds) {
     keyboard::SetOverrideContentUrl(GURL(url));
     keyboard::KeyboardController::GetInstance()->Reload();
-    ResizeKeyboardWindow();
+    // Mock window.resizeTo that is expected to be called after navigate to a
+    // new virtual keyboard.
+    proxy()->GetKeyboardWindow()->SetBounds(init_bounds);
   }
 
   bool IsKeyboardVisible() const {
@@ -65,17 +71,6 @@ class VirtualKeyboardWebContentTest : public InProcessBrowserTest {
   }
 
  private:
-  // Mock window.resizeTo that is expected to be called after navigate to a new
-  // virtual keyboard.
-  void ResizeKeyboardWindow() {
-    gfx::Rect bounds = proxy()->GetKeyboardWindow()->bounds();
-    proxy()->GetKeyboardWindow()->SetBounds(gfx::Rect(
-        bounds.x(),
-        bounds.bottom() - kKeyboardHeightForTest,
-        bounds.width(),
-        kKeyboardHeightForTest));
-  }
-
   scoped_ptr<ui::DummyTextInputClient> client;
 
   DISALLOW_COPY_AND_ASSIGN(VirtualKeyboardWebContentTest);
@@ -85,16 +80,17 @@ class VirtualKeyboardWebContentTest : public InProcessBrowserTest {
 // its virtual keyboard should not become visible if previous one is not.
 IN_PROC_BROWSER_TEST_F(VirtualKeyboardWebContentTest,
                        EnableIMEInDifferentExtension) {
-  FocusEditableNodeAndShowKeyboard();
+  gfx::Rect test_bounds(0, 0, 0, kKeyboardHeightForTest);
+  FocusEditableNodeAndShowKeyboard(test_bounds);
   EXPECT_TRUE(IsKeyboardVisible());
   FocusNonEditableNode();
   EXPECT_FALSE(IsKeyboardVisible());
 
-  MockEnableIMEInDifferentExtension("chrome-extension://domain-1");
+  MockEnableIMEInDifferentExtension("chrome-extension://domain-1", test_bounds);
   // Keyboard should not become visible if previous keyboard is not.
   EXPECT_FALSE(IsKeyboardVisible());
 
-  FocusEditableNodeAndShowKeyboard();
+  FocusEditableNodeAndShowKeyboard(test_bounds);
   // Keyboard should become visible after focus on an editable node.
   EXPECT_TRUE(IsKeyboardVisible());
 
@@ -103,8 +99,35 @@ IN_PROC_BROWSER_TEST_F(VirtualKeyboardWebContentTest,
       keyboard::KeyboardController::HIDE_REASON_MANUAL);
   EXPECT_FALSE(IsKeyboardVisible());
 
-  MockEnableIMEInDifferentExtension("chrome-extension://domain-2");
+  MockEnableIMEInDifferentExtension("chrome-extension://domain-2", test_bounds);
   // Keyboard should not become visible if previous keyboard is not, even if it
   // is currently focused on an editable node.
   EXPECT_FALSE(IsKeyboardVisible());
+}
+
+// Test for crbug.com/489366. In FLOATING mode, switch to a new IME in a
+// different extension should exist FLOATIN mode and position the new IME in
+// FULL_WIDTH mode.
+IN_PROC_BROWSER_TEST_F(VirtualKeyboardWebContentTest,
+                       IMEInDifferentExtensionNotCentered) {
+  gfx::Rect test_bounds(0, 0, 0, kKeyboardHeightForTest);
+  FocusEditableNodeAndShowKeyboard(test_bounds);
+  keyboard::KeyboardController* controller =
+      keyboard::KeyboardController::GetInstance();
+  const gfx::Rect& screen_bounds = ash::Shell::GetPrimaryRootWindow()->bounds();
+  gfx::Rect keyboard_bounds = controller->GetContainerWindow()->bounds();
+  EXPECT_EQ(kKeyboardHeightForTest, keyboard_bounds.height());
+  EXPECT_EQ(screen_bounds.height(),
+            keyboard_bounds.height() + keyboard_bounds.y());
+  controller->SetKeyboardMode(keyboard::FLOATING);
+  // Move keyboard to a random place.
+  proxy()->GetKeyboardWindow()->SetBounds(gfx::Rect(50, 50, 50, 50));
+  EXPECT_EQ(gfx::Rect(50, 50, 50, 50),
+            controller->GetContainerWindow()->bounds());
+
+  MockEnableIMEInDifferentExtension("chrome-extension://domain-1", test_bounds);
+  keyboard_bounds = controller->GetContainerWindow()->bounds();
+  EXPECT_EQ(kKeyboardHeightForTest, keyboard_bounds.height());
+  EXPECT_EQ(screen_bounds.height(),
+            keyboard_bounds.height() + keyboard_bounds.y());
 }
--- ui/keyboard/keyboard_controller_proxy.cc
+++ ui/keyboard/keyboard_controller_proxy.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/input_method.h"
 #include "ui/base/ime/text_input_client.h"
 #include "ui/keyboard/keyboard_constants.h"
+#include "ui/keyboard/keyboard_controller.h"
 #include "ui/keyboard/keyboard_switches.h"
 #include "ui/keyboard/keyboard_util.h"
 #include "ui/wm/core/shadow.h"
@@ -102,7 +103,9 @@ namespace keyboard {
 
 KeyboardControllerProxy::KeyboardControllerProxy(
     content::BrowserContext* context)
-    : browser_context_(context), default_url_(kKeyboardURL) {
+    : browser_context_(context),
+      default_url_(kKeyboardURL),
+      keyboard_controller_(nullptr) {
 }
 
 KeyboardControllerProxy::~KeyboardControllerProxy() {
@@ -188,15 +191,20 @@ void KeyboardControllerProxy::ReloadKeyboardIfNeeded() {
   if (keyboard_contents_->GetURL() != GetVirtualKeyboardUrl()) {
     if (keyboard_contents_->GetURL().GetOrigin() !=
         GetVirtualKeyboardUrl().GetOrigin()) {
-      // Sets keyboard window height to 0 before navigate to a keyboard in a
-      // different extension. This keeps the UX the same as Android.
-      gfx::Rect bounds = GetKeyboardWindow()->bounds();
-      bounds.set_y(bounds.y() + bounds.height());
-      bounds.set_height(0);
-      GetKeyboardWindow()->SetBounds(bounds);
+      // Sets keyboard window rectangle to 0 and close current page before
+      // navigate to a keyboard in a different extension. This keeps the UX the
+      // same as Android. Note we need to explicitly close current page as it
+      // might try to resize keyboard window in javascript on a resize event.
+      GetKeyboardWindow()->SetBounds(gfx::Rect());
+      keyboard_contents_->ClosePage();
+      keyboard_controller()->SetKeyboardMode(FULL_WIDTH);
     }
     LoadContents(GetVirtualKeyboardUrl());
   }
+}
+
+void KeyboardControllerProxy::SetController(KeyboardController* controller) {
+  keyboard_controller_ = controller;
 }
 
 void KeyboardControllerProxy::SetupWebContents(content::WebContents* contents) {
