cf6960c6edd40e2cf5c3a8e09474bccd67cb88f9
hajimehoshi@chromium.org <hajimehoshi@chromium.org>
2015-06-11 05:24:09 +0000

    Refactoring: Remove StyledMarkupAccumulator::RangeFullySelectsNodes
    This CL aims to remove an enum StyledMarkupAccumulator::
    RangeFullySelectsNodes, which is used like a boolean value.
    
    This value is used to create an inline style for appendElement, and this
    CL exposes a function to create an inline style (createInlineStyle) and
    has StyledMarkupSerializer call this.
    
    There was only one case when the value DoesNotFullySelectNode was used,
    and this CL moves the logic for this case to the caller of
    createInlineStyle.
    
    I plan to remove another boolean parameter |addDisplayInline| later.
    
    This patch is result of collaboration work with yosin@chromium.org
    for selection of web components. We've already prepared the new
    node-traversal usages at crrev.com/1134473004.
    
    BUG=n/a
    TEST=n/a; no behavior change
    
    Review URL: https://codereview.chromium.org/1163673005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196911 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/StyledMarkupAccumulator.h
+++ third_party/WebKit/Source/core/editing/StyledMarkupAccumulator.h
@@ -45,13 +45,13 @@ class StyledMarkupAccumulator final {
     WTF_MAKE_NONCOPYABLE(StyledMarkupAccumulator);
     STACK_ALLOCATED();
 public:
-    enum RangeFullySelectsNode { DoesFullySelectNode, DoesNotFullySelectNode };
-
     StyledMarkupAccumulator(EAbsoluteURLs, const TextOffset& start, const TextOffset& end, const PassRefPtrWillBeRawPtr<Document>, EAnnotateForInterchange, Node*);
 
     void appendStartTag(Node&);
     void appendEndTag(const Element&);
     void appendInterchangeNewline();
+
+    void appendText(Text&);
 
     // TODO(hajimehoshi): These functions are called from the serializer, but
     // should not.
@@ -63,7 +63,9 @@ public:
     String takeResults();
 
     void pushMarkup(const String&);
-    void appendElement(StringBuilder&, Element&, bool addDisplayInline, RangeFullySelectsNode);
+    RefPtrWillBeMember<EditingStyle> createInlineStyle(Element&, bool addDisplayInline);
+
+    void appendElement(StringBuilder&, Element&, bool addDisplayInline, PassRefPtrWillBeRawPtr<EditingStyle>);
     void appendStartMarkup(StringBuilder&, Node&);
 
 private:
@@ -71,15 +73,11 @@ private:
 
     String renderedText(Text&);
     String stringValueForRange(const Text&);
-
     bool shouldApplyWrappingStyle(const Node&) const;
-
     bool shouldAnnotate() const;
 
-    void appendElement(StringBuilder&, Element&);
+    void appendElement(Element&, PassRefPtrWillBeRawPtr<EditingStyle>);
     void appendEndMarkup(StringBuilder&, const Element&);
-
-    RefPtrWillBeRawPtr<EditingStyle> createInlineStyle(Element&, bool addDisplayInline, RangeFullySelectsNode);
 
     MarkupFormatter m_formatter;
     const TextOffset m_start;
