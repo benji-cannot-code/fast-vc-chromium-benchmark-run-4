d9934e88560d22902866b3fbf41a18e0b200131d
nhiroki@chromium.org <nhiroki@chromium.org>
2015-06-11 04:20:24 +0000

    Revert of ServiceWorker: Implement ServiceWorkerRegistration.update() (1) (patchset #1 id:20001 of https://codereview.chromium.org/1166283006/)
    Reason for revert:
    I'll remake this CL as per the discussion in the CL(2).
    
    Original issue's description:
    > ServiceWorker: Implement ServiceWorkerRegistration.update() (1)
    > 
    > Spec: https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-registration-update
    > Intent-to-Ship: https://groups.google.com/a/chromium.org/forum/#!topic/Blink-dev/bvi8fXqvNhs
    > 
    > (1) Blink: THIS PATCH
    > (2) Chromium: https://codereview.chromium.org/1175823002
    > (3) Blink: https://codereview.chromium.org/1177563005
    > 
    > BUG=450507
    > TEST=will be added by (3)
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=196838
    
    TBR=falken@chromium.org,tkent@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=450507
    
    Review URL: https://codereview.chromium.org/1178753003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196909 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/serviceworkers/ServiceWorkerRegistration.h
+++ third_party/WebKit/Source/modules/serviceworkers/ServiceWorkerRegistration.h
@@ -58,7 +58,6 @@ public:
 
     WebServiceWorkerRegistration* webRegistration() { return m_outerRegistration.get(); }
 
-    void update(ScriptState*, ExceptionState&);
     ScriptPromise unregister(ScriptState*);
 
     DEFINE_ATTRIBUTE_EVENT_LISTENER(updatefound);
