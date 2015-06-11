7a5238d64dd9039c130979596edde4c0b86364ba
mcasas <mcasas@chromium.org>
2015-06-10 17:11:39 -0700

    VideoFrame cleanup: change |allow_overlay_| to metadata and |has_alpha| to Format
    VideoFrame::WrapNativeTexture() has a parameter
    |allow_overlay_| that is overwhelmingly |false|
    around the codebase. This CL moves this parameter
    as a metadata entry, false by default except
    when explicitly set.
    
    VideoFrame::WrapNativeTexture() loses
    the parameter |has_alpha| and instead gets a
    VideoFrame::Format; all cases I found say ARGB,
    added DCHECK to see if bots agree and to make
    this situation evident. This is in preparation
    for Texture frames that are not ARGB, e.g. any
    4:2:2.
    
    BUG=440843, 489744
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    TBR=
    bbudge@chromium.org for content/renderer/pepper/video_decoder_shim.cc
    reveman@chromium.org for cc/resources/video_resource_updater.cc and
    a few associated unittests.
    
    In both cases it's just an automatic change derived
    from VideoFrame method smart-renaming:
    
    video_frame->allow_overlay()
     turns into
    video_frame->metadata()->IsTrue(media::VideoFrameMetadata::ALLOW_OVERLAY)
    
    Review URL: https://codereview.chromium.org/1150863008
    
    Cr-Commit-Position: refs/heads/master@{#333855}

--- media/renderers/video_renderer_impl.cc
+++ media/renderers/video_renderer_impl.cc
@@ -487,7 +487,7 @@ void VideoRendererImpl::FrameReady(VideoFrameStream::Status status,
       return;
     }
 
-    if (frame->IsEndOfStream()) {
+    if (frame->metadata()->IsTrue(VideoFrameMetadata::END_OF_STREAM)) {
       DCHECK(!received_end_of_stream_);
       received_end_of_stream_ = true;
 
@@ -619,7 +619,7 @@ void VideoRendererImpl::AddReadyFrame_Locked(
     const scoped_refptr<VideoFrame>& frame) {
   DCHECK(task_runner_->BelongsToCurrentThread());
   lock_.AssertAcquired();
-  DCHECK(!frame->IsEndOfStream());
+  DCHECK(!frame->metadata()->IsTrue(VideoFrameMetadata::END_OF_STREAM));
 
   frames_decoded_++;
 
