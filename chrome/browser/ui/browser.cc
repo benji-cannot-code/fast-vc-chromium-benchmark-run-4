e81a213db416ddeef98579dd16d25be857c1a803
lgarron <lgarron@chromium.org>
2015-06-11 13:06:07 -0700

    Change WebContentsDelegate::GetSecurityStyle to take non-const WebContents.
    Based on the discussion in https://codereview.chromium.org/1162663004 .
    
    BUG=484395
    
    Review URL: https://codereview.chromium.org/1173213002
    
    Cr-Commit-Position: refs/heads/master@{#334023}

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1302,8 +1302,7 @@ bool Browser::CanDragEnter(content::WebContents* source,
   return true;
 }
 
-content::SecurityStyle Browser::GetSecurityStyle(
-    const WebContents* web_contents) {
+content::SecurityStyle Browser::GetSecurityStyle(WebContents* web_contents) {
   return ConnectionSecurityHelper::GetSecurityStyleForWebContents(web_contents);
 }
 
