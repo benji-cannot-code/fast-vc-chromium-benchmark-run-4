93c3ce39e295187e19784c9e740d1358a9c17984
adamk@chromium.org <adamk@chromium.org>
2015-06-11 15:54:08 +0000

    Remove unused code from WTF TypedArrays
    These look to be leftovers from before V8 implemented TypedArrays.
    
    Review URL: https://codereview.chromium.org/1180713004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196958 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/Float32Array.h
+++ third_party/WebKit/Source/wtf/Float32Array.h
@@ -41,10 +41,6 @@ public:
 
     static inline PassRefPtr<Float32Array> createOrNull(unsigned length);
 
-    // Should only be used when it is known the entire array will be filled. Do
-    // not return these results directly to JavaScript without filling first.
-    static inline PassRefPtr<Float32Array> createUninitialized(unsigned length);
-
     using TypedArrayBase<float>::set;
 
     void set(unsigned index, double value)
@@ -53,9 +49,6 @@ public:
             return;
         TypedArrayBase<float>::data()[index] = static_cast<float>(value);
     }
-
-    inline PassRefPtr<Float32Array> subarray(int start) const;
-    inline PassRefPtr<Float32Array> subarray(int start, int end) const;
 
     virtual ViewType type() const override
     {
@@ -90,24 +83,9 @@ PassRefPtr<Float32Array> Float32Array::createOrNull(unsigned length)
     return TypedArrayBase<float>::createOrNull<Float32Array>(length);
 }
 
-PassRefPtr<Float32Array> Float32Array::createUninitialized(unsigned length)
-{
-    return TypedArrayBase<float>::createUninitialized<Float32Array>(length);
-}
-
 Float32Array::Float32Array(PassRefPtr<ArrayBuffer> buffer, unsigned byteOffset, unsigned length)
     : TypedArrayBase<float>(buffer, byteOffset, length)
 {
-}
-
-PassRefPtr<Float32Array> Float32Array::subarray(int start) const
-{
-    return subarray(start, length());
-}
-
-PassRefPtr<Float32Array> Float32Array::subarray(int start, int end) const
-{
-    return subarrayImpl<Float32Array>(start, end);
 }
 
 } // namespace WTF
