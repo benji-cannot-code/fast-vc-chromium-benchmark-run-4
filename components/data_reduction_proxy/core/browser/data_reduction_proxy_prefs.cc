d730d28eae75bcc6849ea42f1584b91bd5ed7324
megjablon <megjablon@chromium.org>
2015-06-11 16:39:14 -0700

    UMA for Data Reduction Proxy Lo-Fi implicit opt out
    Counts how often:
    
    1. Lo-Fi is temporarily disabled for the remainder of the
    session (This occurs if a user clicks 'show images' for k
    pages in a session)
    
    2. Lo-Fi is disabled for the user (This occurs if
    LoFi is temporarily disabled for j consecutive sessions)
    
    3. Lo-Fi is turned back on using a new implicit opt out
    version number.
    
    BUG=495689
    
    Review URL: https://codereview.chromium.org/1161773013
    
    Cr-Commit-Position: refs/heads/master@{#334081}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_prefs.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_prefs.cc
@@ -60,7 +60,7 @@ void RegisterSyncableProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
       prefs::kDailyOriginalContentLengthViaDataReductionProxy);
   registry->RegisterListPref(prefs::kDailyContentLengthViaDataReductionProxy);
   registry->RegisterInt64Pref(prefs::kDailyHttpContentLengthLastUpdateDate, 0L);
-  registry->RegisterIntegerPref(prefs::kLoFiImplicitOptOutVersion, 0);
+  registry->RegisterIntegerPref(prefs::kLoFiImplicitOptOutEpoch, 0);
   registry->RegisterIntegerPref(prefs::kLoFiLoadImagesPerSession, 0);
   registry->RegisterIntegerPref(prefs::kLoFiConsecutiveSessionDisables, 0);
   registry->RegisterInt64Pref(prefs::kSimulatedConfigRetrieveTime, 0L);
@@ -111,7 +111,7 @@ void RegisterPrefs(PrefRegistrySimple* registry) {
       prefs::kDailyContentLengthViaDataReductionProxy);
   registry->RegisterInt64Pref(
       prefs::kDailyHttpContentLengthLastUpdateDate, 0L);
-  registry->RegisterIntegerPref(prefs::kLoFiImplicitOptOutVersion, 0);
+  registry->RegisterIntegerPref(prefs::kLoFiImplicitOptOutEpoch, 0);
   registry->RegisterIntegerPref(prefs::kLoFiLoadImagesPerSession, 0);
   registry->RegisterIntegerPref(prefs::kLoFiConsecutiveSessionDisables, 0);
   registry->RegisterInt64Pref(prefs::kSimulatedConfigRetrieveTime, 0L);
