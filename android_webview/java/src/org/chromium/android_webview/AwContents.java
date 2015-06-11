a251d9d73a2c23d3f12bae414c072a4a8b9f9ed0
torne <torne@chromium.org>
2015-06-11 04:34:51 -0700

    Don't use android.util.FloatMath.
    This was deprecated in L and java.lang.Math is recommended instead.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1180753004
    
    Cr-Commit-Position: refs/heads/master@{#333926}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -26,7 +26,6 @@ import android.os.Message;
 import android.os.SystemClock;
 import android.text.TextUtils;
 import android.util.Base64;
-import android.util.FloatMath;
 import android.util.Log;
 import android.util.Pair;
 import android.view.KeyEvent;
@@ -2581,7 +2580,6 @@ public class AwContents implements SmartClipProvider,
         client.init(mContentViewCore);
     }
 
-    @SuppressLint("NewApi") // FloatMath#hypot requires API level 17.
     @CalledByNative
     private void didOverscroll(int deltaX, int deltaY, float velocityX, float velocityY) {
         mScrollOffsetManager.overScrollBy(deltaX, deltaY);
@@ -2596,7 +2594,7 @@ public class AwContents implements SmartClipProvider,
         final int scrollRangeX = mScrollOffsetManager.computeMaximumHorizontalScrollOffset();
         final int scrollRangeY = mScrollOffsetManager.computeMaximumVerticalScrollOffset();
         mOverScrollGlow.absorbGlow(x, y, oldX, oldY, scrollRangeX, scrollRangeY,
-                FloatMath.hypot(velocityX, velocityY));
+                (float) Math.hypot(velocityX, velocityY));
 
         if (mOverScrollGlow.isAnimating()) {
             postInvalidateOnAnimation();
