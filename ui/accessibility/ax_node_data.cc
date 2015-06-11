799396c62ed10d96936ba83116fc22dc8db3b396
je_julie.kim <je_julie.kim@samsung.com>
2015-06-11 10:25:11 -0700

    Exposes datetime attribute.
    According to ARIA spec,
    http://rawgit.com/w3c/aria/master/html-aam/html-aam.html#att-datetime-time,
    datetime attribute should be exposed as a object attribute.
    
    This patch changes how to expose HTML attributes such as type for
    text-input, aria-dropeffect and aria-grabbed.
    The variable, html_attributes, from ui::AXNodeData is used instead of adding
    additional attributes.
    
    BUG=480861
    TBR=aboxhall
    
    Review URL: https://codereview.chromium.org/1162263008
    
    Cr-Commit-Position: refs/heads/master@{#333974}

--- ui/accessibility/ax_node_data.cc
+++ ui/accessibility/ax_node_data.cc
@@ -500,9 +500,6 @@ std::string AXNodeData::ToString() const {
       case AX_ATTR_DISPLAY:
         result += " display=" + value;
         break;
-      case AX_ATTR_DROPEFFECT:
-        result += " dropeffect=" + value;
-        break;
       case AX_ATTR_HELP:
         result += " help=" + value;
         break;
@@ -532,9 +529,6 @@ std::string AXNodeData::ToString() const {
         break;
       case AX_ATTR_SHORTCUT:
         result += " shortcut=" + value;
-        break;
-     case AX_ATTR_TEXT_INPUT_TYPE:
-        result += " text_input_type=" + value;
         break;
       case AX_ATTR_URL:
         result += " url=" + value;
@@ -608,9 +602,6 @@ std::string AXNodeData::ToString() const {
         break;
       case AX_ATTR_IS_AX_TREE_HOST:
         result += " is_ax_tree_host=" + value;
-        break;
-      case AX_ATTR_GRABBED:
-        result += " grabbed=" + value;
         break;
       case AX_BOOL_ATTRIBUTE_NONE:
         break;
