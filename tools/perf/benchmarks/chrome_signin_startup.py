4e7513a079c9830a61eb2654b011c1f46ec855a5
shadi <shadi@chromium.org>
2015-06-10 19:18:04 -0700

    Add support for chrome sign-in in Telemetry using login helpers.
    This approach eliminates the need for browser_credentials.
    
    BUG=496836
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1143513002
    
    Cr-Commit-Position: refs/heads/master@{#333874}

---
+++ tools/perf/benchmarks/chrome_signin_startup.py
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from core import perf_benchmark
+from measurements import startup
+import page_sets
+from telemetry import benchmark
+
+
+class _StartupWarm(perf_benchmark.PerfBenchmark):
+  """Measures warm startup time with a clean profile."""
+  options = {'pageset_repeat': 5}
+
+  @classmethod
+  def Name(cls):
+    return 'chrome_signin_starup'
+
+  def CreatePageTest(self, options):
+    return startup.Startup(cold=False)
+
+
+@benchmark.Enabled('has tabs')
+class SigninStartup(_StartupWarm):
+  """Measures warm startup time of signing a profile into Chrome."""
+  page_set = page_sets.ChromeSigninPageSet
+
+  @classmethod
+  def Name(cls):
+    return 'startup.warm.chrome_signin'
