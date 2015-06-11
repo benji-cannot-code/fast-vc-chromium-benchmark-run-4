27cfdbcab84954065ec6b0b2e54101723a86fbdb
lanwei@chromium.org <lanwei@chromium.org>
2015-06-11 19:18:54 +0000

    Populates sourceDevice attribute into TouchEvent
    Since now we have InputDevice information, in this CL we populates sourceDevice
    attribute into TouchEvent, which has firesTouchEvents set to be true.
    
    The Intent-to-Implement: UIEvent.sourceDevice & InputDevice.firesTouchEvents link:
    https://groups.google.com/a/chromium.org/forum/#!searchin/blink-dev/inputdevice/blink-dev/wjtZl9jpCpI/xC0JTRKgc9kJ 
    
    This CL is depended on Issue 1157173003, which creates the InputDevice class
    (https://codereview.chromium.org/1157173003/)
    and Issue 1160753005, which Implement UIEvent.sourceDevice
    (https://codereview.chromium.org/1160753005/)
    
    BUG=476530
    
    Review URL: https://codereview.chromium.org/1161783006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196967 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/input/InputDevice.cpp
+++ third_party/WebKit/Source/core/input/InputDevice.cpp
@@ -22,4 +22,10 @@ InputDevice::~InputDevice()
 {
 }
 
+InputDevice* InputDevice::touchEventInstance()
+{
+    DEFINE_STATIC_LOCAL(Persistent<InputDevice>, instance, (InputDevice::create(true)));
+    return instance;
+}
+
 } // namespace blink
