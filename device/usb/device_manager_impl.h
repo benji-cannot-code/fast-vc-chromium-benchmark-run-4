638a92bd508015ce9ff6240c3efa050444070c9c
kjellander <kjellander@chromium.org>
2015-06-11 06:41:21 -0700

    Revert of Introduce the devices Mojo app (patchset #5 id:160001 of https://codereview.chromium.org/1165223004/)
    Reason for revert:
    This broke the Win 8 GN (dbg) and Win x64 GN (dbg) bots:
    
    https://build.chromium.org/p/chromium.win/builders/Win8%20GN%20%28dbg%29/builds/7453
    https://build.chromium.org/p/chromium.win/builders/Win%20x64%20GN%20%28dbg%29/builds/3219
    
    Original issue's description:
    > Introduce the devices Mojo app
    >
    > It's a small Mojo app that currently only hosts USB device
    > services. This CL adds the app along with an apptest suite
    > (run manually with mojo_runner mojo:devices_apptests).
    >
    > The app is hooked up to Chrome to run in the browser process,
    > though there are currently no production code paths which
    > cause the app to launch.
    >
    > BUG=498557
    >
    > Committed: https://crrev.com/ed800dc138edfa261aa766281b96f736ff9824f6
    > Cr-Commit-Position: refs/heads/master@{#333899}
    
    TBR=reillyg@chromium.org,sky@chromium.org,rockot@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=498557
    
    Review URL: https://codereview.chromium.org/1176383002
    
    Cr-Commit-Position: refs/heads/master@{#333942}

--- device/usb/device_manager_impl.h
+++ device/usb/device_manager_impl.h
@@ -16,14 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/mojo/src/mojo/public/cpp/bindings/interface_request.h"
 #include "third_party/mojo/src/mojo/public/cpp/bindings/strong_binding.h"
 
-namespace base {
-class SequencedTaskRunner;
-}
-
-namespace mojo {
-class ErrorHandler;
-}
-
 namespace device {
 
 class UsbDevice;
@@ -34,17 +26,13 @@ namespace usb {
 
 class DeviceManagerDelegate;
 
-// Implementation of the public DeviceManager interface. This interface can be
-// requested from the devices app located at "system:devices", if available.
+// Implementation of the public DeviceManager interface. Clients in the browser
+// can connect to this service via DeviceClient::ConnectToUSBDeviceManager.
 class DeviceManagerImpl : public DeviceManager {
  public:
-  DeviceManagerImpl(
-      mojo::InterfaceRequest<DeviceManager> request,
-      scoped_ptr<DeviceManagerDelegate> delegate,
-      scoped_refptr<base::SequencedTaskRunner> service_task_runner);
+  DeviceManagerImpl(mojo::InterfaceRequest<DeviceManager> request,
+                    scoped_ptr<DeviceManagerDelegate> delegate);
   ~DeviceManagerImpl() override;
-
-  void set_error_handler(mojo::ErrorHandler* error_handler);
 
  private:
   // DeviceManager implementation:
@@ -55,14 +43,18 @@ class DeviceManagerImpl : public DeviceManager {
                   const OpenDeviceCallback& callback) override;
 
   // Callback to handle the async response from the underlying UsbService.
-  void OnGetDevices(EnumerationOptionsPtr options,
-                    const GetDevicesCallback& callback,
+  void OnGetDevices(const GetDevicesCallback& callback,
+                    const std::vector<UsbDeviceFilter>& filters,
                     const std::vector<scoped_refptr<UsbDevice>>& devices);
+
+  // Callback to handle the async Open response from a UsbDevice.
+  void OnOpenDevice(const OpenDeviceCallback& callback,
+                    mojo::InterfaceRequest<Device> device_request,
+                    scoped_refptr<UsbDeviceHandle> device_handle);
 
   mojo::StrongBinding<DeviceManager> binding_;
 
   scoped_ptr<DeviceManagerDelegate> delegate_;
-  scoped_refptr<base::SequencedTaskRunner> service_task_runner_;
 
   base::WeakPtrFactory<DeviceManagerImpl> weak_factory_;
 
