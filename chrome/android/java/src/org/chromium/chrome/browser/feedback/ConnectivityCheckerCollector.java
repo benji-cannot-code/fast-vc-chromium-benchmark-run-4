18f6f5c8491eef00ad109a0b596939ee7b8a4e83
nyquist <nyquist@chromium.org>
2015-06-11 16:45:31 -0700

    Add elapsed time and timeout value to feedback data.
    To help identifying issues with the connectivity feedback data, this CL
    adds data about timeouts and the elapsed time before the data was
    gathered.
    
    Some of the flows where this data is used, there is a very short or no
    delay between starting the connectivity checks and gathering the results
    so this should help identify such cases.
    
    BUG=386395
    
    Review URL: https://codereview.chromium.org/1178703010
    
    Cr-Commit-Position: refs/heads/master@{#334084}

--- chrome/android/java/src/org/chromium/chrome/browser/feedback/ConnectivityCheckerCollector.java
+++ chrome/android/java/src/org/chromium/chrome/browser/feedback/ConnectivityCheckerCollector.java
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.feedback;
 
+import android.os.SystemClock;
+
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.browser.profiles.Profile;
 
 import java.util.Collections;
@@ -27,18 +30,55 @@ public class ConnectivityCheckerCollector {
      * FeedbackData contains the set of information that is to be included in a feedback report.
      */
     public static final class FeedbackData {
-        private final Map<Type, Result> mConnections;
+        /**
+         * The key for the data describing the timeout that was set as a maximum for collecting
+         * the connection data. This is to better understand the connection data.
+         * This string is user visible.
+         */
+        @VisibleForTesting
+        static final String CONNECTION_CHECK_TIMEOUT_MS = "Connection check timeout (ms)";
 
-        FeedbackData(Map<Type, Result> connections) {
+        /**
+         * The key for the data describing how long time from the connection check was started,
+         * until the data was collected. This is to better understand the connection data.
+         * This string is user visible.
+         */
+        @VisibleForTesting
+        static final String CONNECTION_CHECK_ELAPSED_MS = "Connection check elapsed (ms)";
+
+        private final Map<Type, Result> mConnections;
+        private final int mTimeoutMs;
+        private final long mElapsedTimeMs;
+
+        FeedbackData(Map<Type, Result> connections, int timeoutMs, long elapsedTimeMs) {
             mConnections = connections;
+            mTimeoutMs = timeoutMs;
+            mElapsedTimeMs = elapsedTimeMs;
         }
 
         /**
          * @return a {@link Map} with information about connection status for different connection
          * types.
          */
-        public Map<Type, Result> getConnections() {
+        @VisibleForTesting
+        Map<Type, Result> getConnections() {
             return Collections.unmodifiableMap(mConnections);
+        }
+
+        /**
+         * @return the timeout that was used for data collection.
+         */
+        @VisibleForTesting
+        int getTimeoutMs() {
+            return mTimeoutMs;
+        }
+
+        /**
+         * @return the time that was used from starting the check until data was gathered.
+         */
+        @VisibleForTesting
+        long getElapsedTimeMs() {
+            return mElapsedTimeMs;
         }
 
         /**
@@ -49,6 +89,8 @@ public class ConnectivityCheckerCollector {
             for (Map.Entry<Type, Result> entry : mConnections.entrySet()) {
                 map.put(entry.getKey().name(), entry.getValue().name());
             }
+            map.put(CONNECTION_CHECK_TIMEOUT_MS, String.valueOf(mTimeoutMs));
+            map.put(CONNECTION_CHECK_ELAPSED_MS, String.valueOf(mElapsedTimeMs));
             return map;
         }
     }
@@ -66,6 +108,10 @@ public class ConnectivityCheckerCollector {
 
     private static class ConnectivityCheckerFuture implements Future<FeedbackData> {
         private final Map<Type, Result> mResult = new EnumMap<Type, Result>(Type.class);
+        // These timing values are set to -1 to signify an error until they are correctly set
+        // when starting the checks and gathering the data.
+        private int mTimeoutMs = -1;
+        private long mStartCheckTimeMs = -1;
 
         private class SingleTypeTask implements ConnectivityChecker.ConnectivityCheckerCallback {
             private final Type mType;
@@ -141,7 +187,8 @@ public class ConnectivityCheckerCollector {
                     result.put(type, Result.UNKNOWN);
                 }
             }
-            return new FeedbackData(result);
+            long elapsedTimeMs = SystemClock.elapsedRealtime() - mStartCheckTimeMs;
+            return new FeedbackData(result, mTimeoutMs, elapsedTimeMs);
         }
 
         @Override
@@ -153,6 +200,8 @@ public class ConnectivityCheckerCollector {
          * Starts a separate connectivity check for each {@link Type}.
          */
         public void startChecks(Profile profile, int timeoutMs) {
+            mTimeoutMs = timeoutMs;
+            mStartCheckTimeMs = SystemClock.elapsedRealtime();
             for (Type t : Type.values()) {
                 SingleTypeTask task = new SingleTypeTask(t);
                 task.start(profile, timeoutMs);
