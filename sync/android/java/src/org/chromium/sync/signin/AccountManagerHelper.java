3ea8fc242a022448a68e865bfe755fceedf14dee
khushalsagar <khushalsagar@google.com>
2015-06-11 11:59:58 -0700

    AccountManagerHelper returns null token when USE_CREDENTIALS permission denied
    BUG=499399
    
    Review URL: https://codereview.chromium.org/1178643009
    
    Cr-Commit-Position: refs/heads/master@{#333998}

--- sync/android/java/src/org/chromium/sync/signin/AccountManagerHelper.java
+++ sync/android/java/src/org/chromium/sync/signin/AccountManagerHelper.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.sync.signin;
 
 
+import android.Manifest;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.accounts.AccountManagerFuture;
@@ -14,8 +15,10 @@ import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Process;
 import android.util.Log;
 
 import org.chromium.base.ThreadUtils;
@@ -237,6 +240,11 @@ public class AccountManagerHelper {
         }
     }
 
+    private boolean hasUseCredentialsPermission() {
+        return mApplicationContext.checkPermission(Manifest.permission.USE_CREDENTIALS,
+                Process.myPid(), Process.myUid()) == PackageManager.PERMISSION_GRANTED;
+    }
+
     // Gets the auth token synchronously
     private String getAuthTokenInner(AccountManagerFuture<Bundle> future,
             AtomicBoolean errorEncountered) {
@@ -262,6 +270,11 @@ public class AccountManagerHelper {
             final String authTokenType, final GetAuthTokenCallback callback,
             final AtomicInteger numTries, final AtomicBoolean errorEncountered,
             final ConnectionRetry retry) {
+        // Return null token for no USE_CREDENTIALS permission.
+        if (!hasUseCredentialsPermission()) {
+            callback.tokenAvailable(null);
+            return;
+        }
         final AccountManagerFuture<Bundle> future = mAccountManager.getAuthToken(
                 account, authTokenType, true, null, null);
         errorEncountered.set(false);
@@ -350,6 +363,10 @@ public class AccountManagerHelper {
      * Removes an auth token from the AccountManager's cache.
      */
     public void invalidateAuthToken(String authToken) {
+        // Cancel operation for no USE_CREDENTIALS permission.
+        if (!hasUseCredentialsPermission()) {
+            return;
+        }
         if (authToken != null && !authToken.isEmpty()) {
             mAccountManager.invalidateAuthToken(GOOGLE_ACCOUNT_TYPE, authToken);
         }
