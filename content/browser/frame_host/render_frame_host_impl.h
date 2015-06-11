1990510d3672283714a9ef78c1c479575f027af8
avi <avi@chromium.org>
2015-06-11 13:21:23 -0700

    Revert of Ensure the new navigation classifier works in the real world. (patchset #2 id:20001 of https://codereview.chromium.org/1171973004/)
    Reason for revert:
    We have an explanation.
    
    Original issue's description:
    > Ensure the new navigation classifier works in the real world.
    >
    > BUG=369661
    > TEST=No whammies!
    >
    > Committed: https://crrev.com/39825ed5795d26cc69167c1fe774f23117cc8f77
    > Cr-Commit-Position: refs/heads/master@{#333755}
    
    TBR=creis@chromium.org,rsesek@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=369661
    
    Review URL: https://codereview.chromium.org/1180933002
    
    Cr-Commit-Position: refs/heads/master@{#334027}

--- content/browser/frame_host/render_frame_host_impl.h
+++ content/browser/frame_host/render_frame_host_impl.h
@@ -11,11 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/compiler_specific.h"
-#include "base/debug/crash_logging.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
-#include "base/strings/string_number_conversions.h"  // Temporary
 #include "base/time/time.h"
 #include "content/browser/accessibility/browser_accessibility_manager.h"
 #include "content/browser/site_instance_impl.h"
@@ -442,9 +440,6 @@ class CONTENT_EXPORT RenderFrameHostImpl
   // addition, its associated RenderWidgetHost has to be focused.
   bool IsFocused();
 
-  // Temporary for http://crbug.com/369661.
-  std::string CommitCountString();
-
  protected:
   friend class RenderFrameHostFactory;
 
@@ -733,9 +728,6 @@ class CONTENT_EXPORT RenderFrameHostImpl
 
   // The frame's Mojo Shell service.
   scoped_ptr<FrameMojoShell> frame_mojo_shell_;
-
-  // Temporary for http://crbug.com/369661
-  int commit_count_ = 0;
 
   // NOTE: This must be the last member.
   base::WeakPtrFactory<RenderFrameHostImpl> weak_ptr_factory_;
