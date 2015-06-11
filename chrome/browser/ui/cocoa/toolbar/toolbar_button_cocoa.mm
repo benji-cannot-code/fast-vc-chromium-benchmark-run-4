41ba13faa91e5df681098a8bcf5baf10271c0ce5
andresantoso <andresantoso@chromium.org>
2015-06-11 14:57:29 -0700

    Mac: Fix missing focus rings on toolbar, profile, and tab buttons.
    In 10.7, AppKit introduced new API for drawing focus rings.
    NSControl/NSCell subclasses can opt-in by overriding -focusRingMaskBounds and
    -drawFocusRingMask. Implement these methods on the toolbar, profile, new tab and
    tab close buttons so that they get focus rings when Full Keyboard Access is
    enabled.
    
    But it turns out that this new focus ring does not work on buttons on 10.7, only on
    editable text fields. They do work starting with 10.8.
    
    10.6 before: manually drawn, tab close and avatar buttons missing focus rings.
    10.6 after: no change.
    
    10.7 before: broken, no focus rings on toolbar, avatar, tab close, new tab buttons.
    10.7 after: manually drawn, same as 10.6.
    
    10.8+ before: broken, no focus rings on toolbar, avatar, tab close, new tab buttons.
    10.8+ after: drawn by AppKit.
    
    Toolbar.xib changes:
    Change the toolbar buttons (back, forward, reload, home, wrench) Focus Ring from
    None to Default, to allow the new style focus ring to draw.
    
    BUG=459860
    
    Review URL: https://codereview.chromium.org/1160143010
    
    Cr-Commit-Position: refs/heads/master@{#334045}

--- chrome/browser/ui/cocoa/toolbar/toolbar_button_cocoa.mm
+++ chrome/browser/ui/cocoa/toolbar/toolbar_button_cocoa.mm
@@ -49,4 +49,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return handleMiddleClick_ && [theEvent buttonNumber] == 2;
 }
 
+- (NSRect)focusRingMaskBounds {
+  // This override won't be needed once we link with 10.8+ SDK.
+  return [self bounds];
+}
+
+- (void)drawFocusRingMask {
+  // Match the hover image's bezel.
+  [[NSBezierPath bezierPathWithRoundedRect:NSInsetRect([self bounds], 2, 2)
+                                   xRadius:2
+                                   yRadius:2] fill];
+}
+
 @end
