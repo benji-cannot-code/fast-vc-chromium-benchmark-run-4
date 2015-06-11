a98ae5efae62f2b0283e024a2a42bda0119aea28
simonhatch <simonhatch@chromium.org>
2015-06-11 10:47:23 -0700

    Slow Reports - Grab upload url from config.
    BUG=498419
    
    Review URL: https://codereview.chromium.org/1171823004
    
    Cr-Commit-Position: refs/heads/master@{#333980}

--- chrome/browser/tracing/background_tracing_field_trial.cc
+++ chrome/browser/tracing/background_tracing_field_trial.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/background_tracing_config.h"
 #include "content/public/browser/background_tracing_manager.h"
 #include "content/public/browser/browser_thread.h"
+#include "url/gurl.h"
 
 namespace tracing {
 
@@ -21,6 +22,7 @@ namespace {
 
 const char kBackgroundTracingFieldTrial[] = "BackgroundTracing";
 const char kBackgroundTracingConfig[] = "config";
+const char kBackgroundTracingUploadUrl[] = "upload_url";
 
 void OnUploadProgress(int64, int64) {
   // We don't actually care about the progress, but TraceUploader::DoUpload
@@ -35,11 +37,13 @@ void OnUploadComplete(TraceCrashServiceUploader* uploader,
   done_callback.Run();
 }
 
-void UploadCallback(const scoped_refptr<base::RefCountedString>& file_contents,
+void UploadCallback(const std::string& upload_url,
+                    const scoped_refptr<base::RefCountedString>& file_contents,
                     base::Closure callback) {
   TraceCrashServiceUploader* uploader = new TraceCrashServiceUploader(
       g_browser_process->system_request_context());
 
+  uploader->SetUploadURL(upload_url);
   uploader->DoUpload(
       file_contents->data(), base::Bind(&OnUploadProgress),
       base::Bind(&OnUploadComplete, base::Owned(uploader), callback));
@@ -50,6 +54,12 @@ void UploadCallback(const scoped_refptr<base::RefCountedString>& file_contents,
 void SetupBackgroundTracingFieldTrial() {
   std::string config_text = variations::GetVariationParamValue(
       kBackgroundTracingFieldTrial, kBackgroundTracingConfig);
+  std::string upload_url = variations::GetVariationParamValue(
+      kBackgroundTracingFieldTrial, kBackgroundTracingUploadUrl);
+
+  if (!GURL(upload_url).is_valid())
+    return;
+
   if (config_text.empty())
     return;
 
@@ -67,7 +77,7 @@ void SetupBackgroundTracingFieldTrial() {
     return;
 
   content::BackgroundTracingManager::GetInstance()->SetActiveScenario(
-      config.Pass(), base::Bind(&UploadCallback),
+      config.Pass(), base::Bind(&UploadCallback, upload_url),
       content::BackgroundTracingManager::ANONYMIZE_DATA);
 }
 
