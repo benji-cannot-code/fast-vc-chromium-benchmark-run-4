38da10f8bb83fe83b806a03ce7def4937ef6bca8
oysteine <oysteine@chromium.org>
2015-06-10 20:53:53 -0700

    Slow Reports: Instrumented the BackgroundTracingManagerImpl class with UMA histograms.
    R=dsinclair,asvitkine,shatch
    BUG=497964
    
    Review URL: https://codereview.chromium.org/1169923003
    
    Cr-Commit-Position: refs/heads/master@{#333889}

--- content/browser/tracing/background_tracing_manager_impl.cc
+++ content/browser/tracing/background_tracing_manager_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/tracing/background_tracing_manager_impl.h"
 
 #include "base/macros.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/time/time.h"
 #include "content/public/browser/background_tracing_preemptive_config.h"
 #include "content/public/browser/background_tracing_reactive_config.h"
@@ -20,6 +21,25 @@ namespace {
 
 base::LazyInstance<BackgroundTracingManagerImpl>::Leaky g_controller =
     LAZY_INSTANCE_INITIALIZER;
+
+// These values are used for a histogram. Do not reorder.
+enum BackgroundTracingMetrics {
+  SCENARIO_ACTIVATION_REQUESTED = 0,
+  SCENARIO_ACTIVATED_SUCCESSFULLY = 1,
+  RECORDING_ENABLED = 2,
+  PREEMPTIVE_TRIGGERED = 3,
+  REACTIVE_TRIGGERED = 4,
+  FINALIZATION_ALLOWED = 5,
+  FINALIZATION_DISALLOWED = 6,
+  FINALIZATION_STARTED = 7,
+  FINALIZATION_COMPLETE = 8,
+  NUMBER_OF_BACKGROUND_TRACING_METRICS,
+};
+
+void RecordBackgroundTracingMetric(BackgroundTracingMetrics metric) {
+  UMA_HISTOGRAM_ENUMERATION("Tracing.Background.ScenarioState", metric,
+                            NUMBER_OF_BACKGROUND_TRACING_METRICS);
+}
 
 }  // namespace
 
@@ -137,6 +157,8 @@ bool BackgroundTracingManagerImpl::SetActiveScenario(
     const BackgroundTracingManager::ReceiveCallback& receive_callback,
     DataFiltering data_filtering) {
   CHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  RecordBackgroundTracingMetric(SCENARIO_ACTIVATION_REQUESTED);
+
   if (is_tracing_)
     return false;
 
@@ -162,6 +184,7 @@ bool BackgroundTracingManagerImpl::SetActiveScenario(
 
   EnableRecordingIfConfigNeedsIt();
 
+  RecordBackgroundTracingMetric(SCENARIO_ACTIVATED_SUCCESSFULLY);
   return true;
 }
 
@@ -254,8 +277,10 @@ void BackgroundTracingManagerImpl::TriggerNamedEvent(
   }
 
   if (config_->mode == BackgroundTracingConfig::PREEMPTIVE_TRACING_MODE) {
+    RecordBackgroundTracingMetric(PREEMPTIVE_TRIGGERED);
     BeginFinalizing(callback);
   } else {
+    RecordBackgroundTracingMetric(REACTIVE_TRIGGERED);
     if (is_tracing_) {
       tracing_timer_->CancelTimer();
       BeginFinalizing(callback);
@@ -335,11 +360,16 @@ void BackgroundTracingManagerImpl::EnableRecording(
 
   is_tracing_ = TracingController::GetInstance()->EnableRecording(
       trace_config, tracing_enabled_callback_for_testing_);
+  RecordBackgroundTracingMetric(RECORDING_ENABLED);
 }
 
 void BackgroundTracingManagerImpl::OnFinalizeStarted(
     scoped_refptr<base::RefCountedString> file_contents) {
   CHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+
+  RecordBackgroundTracingMetric(FINALIZATION_STARTED);
+  UMA_HISTOGRAM_MEMORY_KB("Tracing.Background.FinalizingTraceSizeInKB",
+                          file_contents->size() / 1024);
 
   if (!receive_callback_.is_null())
     receive_callback_.Run(
@@ -366,6 +396,7 @@ void BackgroundTracingManagerImpl::OnFinalizeComplete() {
 
   // Now that a trace has completed, we may need to enable recording again.
   EnableRecordingIfConfigNeedsIt();
+  RecordBackgroundTracingMetric(FINALIZATION_COMPLETE);
 }
 
 void BackgroundTracingManagerImpl::BeginFinalizing(
@@ -382,6 +413,9 @@ void BackgroundTracingManagerImpl::BeginFinalizing(
   if (is_allowed_finalization) {
     trace_data_sink = content::TracingController::CreateCompressedStringSink(
         data_endpoint_wrapper_);
+    RecordBackgroundTracingMetric(FINALIZATION_ALLOWED);
+  } else {
+    RecordBackgroundTracingMetric(FINALIZATION_DISALLOWED);
   }
 
   content::TracingController::GetInstance()->DisableRecording(trace_data_sink);
