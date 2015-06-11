eb5f3e231281295d4dffed11edc49eb22da849b9
danakj <danakj@chromium.org>
2015-06-11 14:39:03 -0700

    content: Remove call to setDeferredImageDecodingEnabled.
    It's now doing nothing on the blink side, so we can remove it here
    to enable removing it in blink.
    
    R=sievers@chromium.org
    BUG=413479
    
    Review URL: https://codereview.chromium.org/1183503004
    
    Cr-Commit-Position: refs/heads/master@{#334039}

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -1042,8 +1042,6 @@ void RenderView::ApplyWebPreferences(const WebPreferences& prefs,
   settings->setMultiTargetTapNotificationEnabled(
       switches::IsLinkDisambiguationPopupEnabled());
 
-  // TODO(danakj): Remove this it's always true.
-  settings->setDeferredImageDecodingEnabled(true);
   WebRuntimeFeatures::enableImageColorProfiles(
       prefs.image_color_profiles_enabled);
   settings->setShouldRespectImageOrientation(
