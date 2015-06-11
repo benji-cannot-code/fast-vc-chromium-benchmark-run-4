7a965d317777582e1b4c7474a86eeb261912ba56
ianwen <ianwen@chromium.org>
2015-06-11 13:21:09 -0700

    [Android] Add tests for toolbar of Chrome Custom Tabs
    Two more instrumentation tests are introduced in this CL, targeting the
    color of toolbar and the action button behavior.
    
    BUG=491015
    
    Review URL: https://codereview.chromium.org/1147243003
    
    Cr-Commit-Position: refs/heads/master@{#334026}

--- chrome/android/java_staging/src/org/chromium/chrome/browser/customtabs/CustomTabIntentDataProvider.java
+++ chrome/android/java_staging/src/org/chromium/chrome/browser/customtabs/CustomTabIntentDataProvider.java
@@ -115,6 +115,7 @@ public class CustomTabIntentDataProvider {
     private PendingIntent mActionButtonPendingIntent;
     private List<Pair<String, PendingIntent>> mMenuEntries = new ArrayList<>();
     private Bundle mAnimationBundle;
+    // OnFinished listener for PendingIntents. Used for testing only.
     private PendingIntent.OnFinished mOnFinished;
 
     /**
@@ -214,6 +215,15 @@ public class CustomTabIntentDataProvider {
     }
 
     /**
+     * @return The {@link PendingIntent} that will be sent when the user clicks the action button.
+     *         For testing only.
+     */
+    @VisibleForTesting
+    public PendingIntent getActionButtonPendingIntentForTest() {
+        return mActionButtonPendingIntent;
+    }
+
+    /**
      * @return Titles of menu items that were passed from client app via intent.
      */
     public List<String> getMenuTitles() {
@@ -285,7 +295,7 @@ public class CustomTabIntentDataProvider {
         Intent addedIntent = new Intent();
         addedIntent.setData(Uri.parse(url));
         try {
-            mActionButtonPendingIntent.send(context, 0, addedIntent);
+            mActionButtonPendingIntent.send(context, 0, addedIntent, mOnFinished, null);
         } catch (CanceledException e) {
             Log.e(TAG, "CanceledException while sending pending intent in custom tab");
         }
@@ -299,8 +309,12 @@ public class CustomTabIntentDataProvider {
         return true;
     }
 
+    /**
+     * Set the callback object for {@link PendingIntent}s that are sent in this class. For testing
+     * purpose only.
+     */
     @VisibleForTesting
-    void setMenuSelectionOnFinishedForTesting(PendingIntent.OnFinished onFinished) {
+    void setPendingIntentOnFinishedForTesting(PendingIntent.OnFinished onFinished) {
         mOnFinished = onFinished;
     }
 }
