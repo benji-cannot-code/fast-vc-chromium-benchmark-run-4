7de9ff07124711cbb10beaf7345e1155decf3cc1
skobes@chromium.org <skobes@chromium.org>
2015-06-12 19:32:49 +0000

    Absolute-position the initial containing block.
    LayoutView needs to clip its contents when root layer scrolling is enabled. It
    already does, but only as a side effect of being composited, and some things
    hit-test in front of the scrollbars because DeprecatedPaintLayerClipper thinks
    absolute-positioned content can only be clipped by positioned containers.
    
    Generally a static-positioned scroller will not clip absolute-positioned
    descendants, so this seems like the cleanest fix (versus special-casing
    LayoutView in DPLClipper).
    
    I think there are other cases where the assumption in DPLClipper may be wrong
    (in particular, when the scroller has a transform), but that can be addressed
    separately; see http://crbug.com/499083.
    
    Two test cases in WebFrameTest no longer pass with root layer scrolling, but
    they only worked by accident. Fix tracked in http://crbug.com/499053.
    
    BUG=495856
    
    Review URL: https://codereview.chromium.org/1172153004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197054 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
@@ -468,6 +468,7 @@ PassRefPtr<ComputedStyle> StyleResolver::styleForDocument(Document& document)
     // Document::inheritHtmlAndBodyElementStyles.
     documentStyle->setDisplay(BLOCK);
     documentStyle->setScrollBlocksOn(WebScrollBlocksOnStartTouch | WebScrollBlocksOnWheelEvent);
+    documentStyle->setPosition(AbsolutePosition);
 
     document.setupFontBuilder(*documentStyle);
 
