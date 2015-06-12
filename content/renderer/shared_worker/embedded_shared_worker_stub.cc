ef9775fe2855cfdbaa462877cbbffcfcea1809cf
kinuko <kinuko@chromium.org>
2015-06-12 03:18:53 -0700

    Minor cleanups for EmbeddedSharedWorkerStub
    - typo fix: runing_ -> running_
    - inline initialization
    - slightly better documentation for shutdown sequence
    
    (Blink-side cleanup: https://codereview.chromium.org/1175463004/)
    
    BUG=n/a
    
    Review URL: https://codereview.chromium.org/1181963003
    
    Cr-Commit-Position: refs/heads/master@{#334139}

--- content/renderer/shared_worker/embedded_shared_worker_stub.cc
+++ content/renderer/shared_worker/embedded_shared_worker_stub.cc
@@ -123,9 +123,7 @@ EmbeddedSharedWorkerStub::EmbeddedSharedWorkerStub(
     int route_id)
     : route_id_(route_id),
       name_(name),
-      runing_(false),
-      url_(url),
-      app_cache_host_(nullptr) {
+      url_(url) {
   RenderThreadImpl::current()->AddEmbeddedWorkerRoute(route_id_, this);
   impl_ = blink::WebSharedWorker::create(this);
   if (pause_on_start) {
@@ -141,6 +139,7 @@ EmbeddedSharedWorkerStub::EmbeddedSharedWorkerStub(
 
 EmbeddedSharedWorkerStub::~EmbeddedSharedWorkerStub() {
   RenderThreadImpl::current()->RemoveEmbeddedWorkerRoute(route_id_);
+  DCHECK(!impl_);
 }
 
 bool EmbeddedSharedWorkerStub::OnMessageReceived(
@@ -167,7 +166,7 @@ void EmbeddedSharedWorkerStub::workerReadyForInspection() {
 
 void EmbeddedSharedWorkerStub::workerScriptLoaded() {
   Send(new WorkerHostMsg_WorkerScriptLoaded(route_id_));
-  runing_ = true;
+  running_ = true;
   // Process any pending connections.
   for (PendingChannelList::const_iterator iter = pending_channels_.begin();
        iter != pending_channels_.end();
@@ -259,6 +258,9 @@ void EmbeddedSharedWorkerStub::sendDevToolsMessage(
 }
 
 void EmbeddedSharedWorkerStub::Shutdown() {
+  // WebSharedWorker must be already deleted in the blink side
+  // when this is called.
+  impl_ = nullptr;
   delete this;
 }
 
@@ -279,7 +281,7 @@ void EmbeddedSharedWorkerStub::OnConnect(int sent_message_port_id,
   port.id = sent_message_port_id;
   WebMessagePortChannelImpl* channel = new WebMessagePortChannelImpl(
       routing_id, port, base::ThreadTaskRunnerHandle::Get().get());
-  if (runing_) {
+  if (running_) {
     ConnectToChannel(channel);
   } else {
     // If two documents try to load a SharedWorker at the same time, the
@@ -291,7 +293,8 @@ void EmbeddedSharedWorkerStub::OnConnect(int sent_message_port_id,
 }
 
 void EmbeddedSharedWorkerStub::OnTerminateWorkerContext() {
-  runing_ = false;
+  // After this we wouldn't get any IPC for this stub.
+  running_ = false;
   impl_->terminateWorkerContext();
 }
 
