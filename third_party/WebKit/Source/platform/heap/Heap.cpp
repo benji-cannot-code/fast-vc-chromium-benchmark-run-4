974d8b9d19f84576c91f2aaced7de5eafdd52260
haraken@chromium.org <haraken@chromium.org>
2015-06-12 06:57:34 +0000

    Build fix: Add NO_SANITIZE_ADDRESS to addToFreeList
    This fixes crashes in ASan builds.
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20ASAN/builds/18946
    
    BUG=
    TBR=keishi@chromium.org
    
    Review URL: https://codereview.chromium.org/1184633005.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197005 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Heap.cpp
+++ third_party/WebKit/Source/platform/heap/Heap.cpp
@@ -1045,6 +1045,7 @@ FreeList::FreeList()
 {
 }
 
+NO_SANITIZE_ADDRESS
 void FreeList::addToFreeList(Address address, size_t size)
 {
     ASSERT(size < blinkPagePayloadSize());
@@ -1113,6 +1114,7 @@ void FreeList::addToFreeList(Address address, size_t size)
 }
 
 #if ENABLE(ASSERT) || defined(LEAK_SANITIZER) || defined(ADDRESS_SANITIZER)
+NO_SANITIZE_ADDRESS
 void FreeList::zapFreedMemory(Address address, size_t size)
 {
     for (size_t i = 0; i < size; i++) {
