8d83d6f0e17025893d5238a5154942181ad9bd6e
thestig <thestig@chromium.org>
2015-06-11 17:45:08 -0700

    Fix some nits in ios::ChromeBrowserProvider().
    Review URL: https://codereview.chromium.org/1181003002
    
    Cr-Commit-Position: refs/heads/master@{#334095}

--- ios/public/provider/chrome/browser/chrome_browser_provider.cc
+++ ios/public/provider/chrome/browser/chrome_browser_provider.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ios {
 
 namespace {
-ChromeBrowserProvider* g_chrome_browser_provider;
+ChromeBrowserProvider* g_chrome_browser_provider = nullptr;
 }  // namespace
 
 void SetChromeBrowserProvider(ChromeBrowserProvider* provider) {
--- ios/public/provider/chrome/browser/chrome_browser_provider.h
+++ ios/public/provider/chrome/browser/chrome_browser_provider.h
@@ -22,10 +22,6 @@ namespace net {
 class URLRequestContextGetter;
 }
 
-namespace web {
-class WebState;
-}
-
 // TODO(ios): Determine the best way to interface with Obj-C code through
 // the ChromeBrowserProvider. crbug/298181
 #ifdef __OBJC__
@@ -84,9 +80,8 @@ class ChromeBrowserProvider {
   virtual void SetUIViewAlphaWithAnimation(UIView* view, float alpha);
   // Returns an instance of a CardUnmaskPromptView used to unmask Wallet cards.
   // The view is responsible for its own lifetime.
-  virtual autofill::CardUnmaskPromptView*
-      CreateCardUnmaskPromptView(
-          autofill::CardUnmaskPromptController* controller);
+  virtual autofill::CardUnmaskPromptView* CreateCardUnmaskPromptView(
+      autofill::CardUnmaskPromptController* controller);
   // Returns risk data used in Wallet requests.
   virtual std::string GetRiskData();
   // Returns product version with prefix.
