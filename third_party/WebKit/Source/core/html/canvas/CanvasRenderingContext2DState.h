135e07244ee6482efe479028c57c97fe6c08b94e
sigbjornf@opera.com <sigbjornf@opera.com>
2015-06-12 16:10:30 +0000

    Oilpan: fix build after r197039.
    CSSFontSelectorClient now needs to a GC mixin.
    
    TBR=oilpan-reviews
    BUG=481515
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1186543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197044 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2DState.h
+++ third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2DState.h
@@ -16,9 +16,14 @@ namespace blink {
 
 class CanvasStyle;
 
-class CanvasRenderingContext2DState final : public CSSFontSelectorClient {
+class CanvasRenderingContext2DState final : public NoBaseWillBeGarbageCollectedFinalized<CanvasRenderingContext2DState>, public CSSFontSelectorClient {
+    WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(CanvasRenderingContext2DState);
 public:
-    CanvasRenderingContext2DState();
+    static PassOwnPtrWillBeRawPtr<CanvasRenderingContext2DState> create()
+    {
+        return adoptPtrWillBeNoop(new CanvasRenderingContext2DState);
+    }
+
     virtual ~CanvasRenderingContext2DState();
 
     DECLARE_VIRTUAL_TRACE();
@@ -34,7 +39,10 @@ public:
         ImagePaintType,
     };
 
-    CanvasRenderingContext2DState(const CanvasRenderingContext2DState&, ClipListCopyMode = CopyClipList);
+    static PassOwnPtrWillBeRawPtr<CanvasRenderingContext2DState> create(const CanvasRenderingContext2DState& other, ClipListCopyMode mode)
+    {
+        return adoptPtrWillBeNoop(new CanvasRenderingContext2DState(other, mode));
+    }
     CanvasRenderingContext2DState& operator=(const CanvasRenderingContext2DState&);
 
     // CSSFontSelectorClient implementation
@@ -143,6 +151,9 @@ public:
     const SkPaint* getPaint(PaintType, ShadowMode, ImageType = NoImage) const;
 
 private:
+    CanvasRenderingContext2DState();
+    CanvasRenderingContext2DState(const CanvasRenderingContext2DState&, ClipListCopyMode = CopyClipList);
+
     void updateLineDash() const;
     void updateStrokeStyle() const;
     void updateFillStyle() const;
--- third_party/WebKit/Source/web/PopupMenuImpl.cpp
+++ third_party/WebKit/Source/web/PopupMenuImpl.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class PopupMenuCSSFontSelector : public CSSFontSelector, private CSSFontSelectorClient {
+    WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(PopupMenuCSSFontSelector);
 public:
     static PassRefPtrWillBeRawPtr<PopupMenuCSSFontSelector> create(Document* document, CSSFontSelector* ownerFontSelector)
     {
