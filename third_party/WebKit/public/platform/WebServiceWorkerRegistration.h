2b0897b7b41be35df270641248b8adf7d8d5ae89
nhiroki@chromium.org <nhiroki@chromium.org>
2015-06-12 08:45:54 +0000

    [Retry] ServiceWorker: Implement ServiceWorkerRegistration.update() (1)
    The difference from the original patch is to route an update request through
    WebServiceWorkerRegistration instead of WebServiceWorkerProvider. See (2) for
    details.
    
    Original patch: https://codereview.chromium.org/1166283006/
    
    Spec: https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-registration-update
    Intent-to-Ship: https://groups.google.com/a/chromium.org/forum/#!topic/Blink-dev/bvi8fXqvNhs
    
    (1) Blink: THIS PATCH
    (2) Chromium: https://codereview.chromium.org/1175823002
    (3) Blink: https://codereview.chromium.org/1177563005
    
    BUG=450507
    TEST=will be added by (3)
    
    Review URL: https://codereview.chromium.org/1177913002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197017 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebServiceWorkerRegistration.h
+++ third_party/WebKit/public/platform/WebServiceWorkerRegistration.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class WebServiceWorkerProvider;
 class WebServiceWorkerRegistrationProxy;
 
 class WebServiceWorkerRegistration {
@@ -21,6 +22,7 @@ public:
     virtual void proxyStopped() { }
 
     virtual WebURL scope() const { return WebURL(); }
+    virtual void update(WebServiceWorkerProvider*) { }
 };
 
 } // namespace blink
