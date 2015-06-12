248f8d54e7d7ea39e4572380b0a0849c545ac47c
rsadam <rsadam@chromium.org>
2015-06-12 09:04:53 -0700

    [Code cleanup] Delete obsolete flag.
    BUG=
    
    Review URL: https://codereview.chromium.org/1178173002
    
    Cr-Commit-Position: refs/heads/master@{#334170}

--- ui/keyboard/keyboard_switches.cc
+++ ui/keyboard/keyboard_switches.cc
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace keyboard {
 namespace switches {
 
-const char kEnableSwipeSelection[] = "enable-swipe-selection";
-
 const char kDisableInputView[] = "disable-input-view";
 const char kEnableInputView[] = "enable-input-view";
 const char kEnableExperimentalInputViewFeatures[] =
--- ui/keyboard/keyboard_switches.h
+++ ui/keyboard/keyboard_switches.h
@@ -11,9 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace keyboard {
 namespace switches {
 
-// Enables the swipe selection feature on the virtual keyboard.
-KEYBOARD_EXPORT extern const char kEnableSwipeSelection[];
-
 // Disables IME extension APIs from overriding the URL for specifying the
 // contents of the virtual keyboard container.
 KEYBOARD_EXPORT extern const char kDisableInputView[];
