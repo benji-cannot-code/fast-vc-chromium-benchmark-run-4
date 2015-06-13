ae2fbda564ca8269c89e576b5dadeaff21412c59
philipj@opera.com <philipj@opera.com>
2015-06-13 16:00:49 +0000

    Add <stdarg.h> include required for va_start/va_end in GCC
    https://codereview.chromium.org/1181713004 removed some base/port.h
    includes, which in turn includes <stdarg.h>. This include is needed
    to compile with GCC, at least on some systems.
    
    BUG=138542
    
    Review URL: https://codereview.chromium.org/1185683002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197088 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/web/tests/WebFrameTest.cpp
@@ -122,6 +122,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <map>
+#include <stdarg.h>
 #include <v8.h>
 
 using blink::URLTestHelpers::toKURL;
