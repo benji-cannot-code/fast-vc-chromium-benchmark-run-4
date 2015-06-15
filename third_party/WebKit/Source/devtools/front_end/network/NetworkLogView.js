844a726b36301979afc12637571b1d12c7620341
pfeldman@chromium.org <pfeldman@chromium.org>
2015-06-15 15:57:34 +0000

    DevTools: introduce network log view min size.
    BUG=500457
    TBR=alph
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1188523003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197121 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/network/NetworkLogView.js
+++ third_party/WebKit/Source/devtools/front_end/network/NetworkLogView.js
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.NetworkLogView = function(filterBar, progressBarContainer, networkLogLargeRowsSetting)
 {
     WebInspector.VBox.call(this);
+    this.setMinimumSize(50, 64);
     this.registerRequiredCSS("network/networkLogView.css");
     this.registerRequiredCSS("ui/filter.css");
 
