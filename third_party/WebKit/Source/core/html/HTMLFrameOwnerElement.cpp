2401cf44d30e5632094b411fa83b5c9e7c3510e4
japhet@chromium.org <japhet@chromium.org>
2015-06-15 21:26:44 +0000

    Fix the logic that limits the number of frames in a page.
    This check apparently doesn't run soon enough, and we can create more than the
    intended limit of 1000 frames. Once we hit 1024,
    NodeRareData::m_connecetedFrameCount can overflow and we no longer fully detach
    Frames from their owners at teardown.
    
    BUG=493243
    TEST=WebFrameTest.MaxFramesDetach
    
    Review URL: https://codereview.chromium.org/1180603002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLFrameOwnerElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLFrameOwnerElement.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/AXObjectCache.h"
 #include "core/dom/ExceptionCode.h"
 #include "core/events/Event.h"
+#include "core/frame/FrameHost.h"
 #include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
 #include "core/layout/LayoutPart.h"
@@ -256,6 +257,9 @@ bool HTMLFrameOwnerElement::loadOrRedirectSubframe(const KURL& url, const Atomic
     }
 
     if (!SubframeLoadingDisabler::canLoadFrame(*this))
+        return false;
+
+    if (document().frame()->host()->subframeCount() >= FrameHost::maxNumberOfFrames)
         return false;
 
     return parentFrame->loader().client()->createFrame(FrameLoadRequest(&document(), url, "_self", CheckContentSecurityPolicy), frameName, this);
--- third_party/WebKit/Source/web/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/web/tests/WebFrameTest.cpp
@@ -7734,4 +7734,12 @@ TEST_F(WebFrameTest, OrientationFrameDetach)
     webViewImpl->mainFrameImpl()->sendOrientationChangeEvent();
 }
 
+TEST_F(WebFrameTest, MaxFramesDetach)
+{
+    registerMockedHttpURLLoad("max-frames-detach.html");
+    FrameTestHelpers::WebViewHelper webViewHelper;
+    WebViewImpl* webViewImpl = webViewHelper.initializeAndLoad(m_baseURL + "max-frames-detach.html", true);
+    webViewImpl->mainFrameImpl()->collectGarbage();
+}
+
 } // namespace blink
