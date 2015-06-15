4465ecc6df521291fcf965d0461488cc4ef13e38
msw <msw@chromium.org>
2015-06-15 15:27:06 -0700

    Revert of Build Mojo apps in subdirectories, like application packages. (patchset #8 id:160001 of https://codereview.chromium.org/1180653002/)
    Reason for revert:
    This makes mojo_runner_unittests fail:
    http://build.chromium.org/p/chromium.mojo/builders/Chromium%20Mojo%20Android/builds/3099/steps/mojo_runner_unittests/logs/stdio
    
    First failure repro steps and output:
    build/android/test_runner.py gtest -s mojo_runner_unittests --output-directory=/work/chrome-git/src/out/android_Debug --gtest_filter=ShellTestBaseTest.ConnectBasic
    
    [==========] Running 20 tests from 3 test cases.
    [----------] Global test environment set-up.
    [----------] 7 tests from ShellTestBaseTest
    [ RUN      ] ShellTestBaseTest.ConnectBasic
    [ERROR:application_manager.cc(409)] Library not started because library path '/data/app/org.chromium.native_test-1/lib/arm/tracing/tracing.mojo' does not exist.
    [ERROR:application_manager.cc(409)] Library not started because library path '/data/data/org.chromium.native_test/app_test_apps/test_app/test_app.mojo' does not exist.
    ../../mojo/runner/shell_test_base_unittest.cc:96: Failure
    Value of: was_run
      Actual: false
    Expected: true
    ../../mojo/runner/shell_test_base_unittest.cc:97: Failure
    Value of: service.encountered_error()
      Actual: true
    Expected: false
    E  254.854s 6d40  Test terminated after 120 second timeout.
    
    Original issue's description:
    > Build Mojo apps in subdirectories, like application packages.
    >
    > Build all <foo>.mojo targets in a <foo> sub-directory.
    > Support resources bundled in mojo_native_application.
    > Remove mojo_application_package (now redundant).
    > Update hard-coded paths and simplify URL resolution code.
    >
    > Remove apps bundled in core_services from Mandoline.apk
    > (network service, surfaces service, view manager)
    >
    > This would help NetworkFetcher resolve app locations.
    > (Android apptests host the build dir on a local server)
    > (but apptests should instead bundle apps in an apk)
    >
    > BUG=498020
    > TEST=Mojo runner and Mandoline still work (as much as they currently do :-/)
    > R=scottmg@chromium.org,sky@chromium.org
    >
    > Committed: https://crrev.com/e6439a2c956a2f09f40f10e8394a53437ad269ed
    > Cr-Commit-Position: refs/heads/master@{#334455}
    
    TBR=scottmg@chromium.org,sky@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=498020
    
    Review URL: https://codereview.chromium.org/1186683008
    
    Cr-Commit-Position: refs/heads/master@{#334478}

--- mojo/runner/url_resolver.cc
+++ mojo/runner/url_resolver.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/runner/url_resolver.h"
 
 #include "base/base_paths.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/strings/utf_string_conversions.h"
 #include "mojo/runner/switches.h"
@@ -110,8 +112,20 @@ GURL URLResolver::ResolveMojoURL(const GURL& mojo_url) const {
   // It's still a mojo: URL, use the default mapping scheme.
   std::string query;
   GURL base_url = shell::GetBaseURLAndQuery(mojo_url, &query);
-  const std::string host = base_url.host();
-  return mojo_base_url_.Resolve(host + "/" + host + ".mojo" + query);
+  if (mojo_base_url_.SchemeIsFile()) {
+    const GURL url_with_directory(
+        mojo_base_url_.Resolve(base_url.host() + "/"));
+    const base::FilePath dir(util::UrlToFilePath(url_with_directory));
+    if (base::DirectoryExists(dir)) {
+      const std::string mojo_file_name(base_url.host() + ".mojo");
+      const base::FilePath mojo_path =
+          dir.Append(base::FilePath::FromUTF8Unsafe(mojo_file_name));
+      // Only use the directory if the .mojo exists in the directory.
+      if (base::PathExists(mojo_path))
+        return url_with_directory.Resolve(base_url.host() + ".mojo" + query);
+    }
+  }
+  return mojo_base_url_.Resolve(base_url.host() + ".mojo" + query);
 }
 
 }  // namespace runner
