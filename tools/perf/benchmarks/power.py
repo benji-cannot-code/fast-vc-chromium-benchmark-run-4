1aa8d885d654f92c3651e572975219d4329ca4db
dyen <dyen@chromium.org>
2015-06-15 11:37:50 -0700

    Turned off full performance mode for the power benchmark.
    Added an option to turn off full performance mode in benchmarks.
    Normally this is necessary to ensure consistent results, but in the
    case of measuring energy consumption turning on full performance
    mode skews the results.
    
    R=jbudorick@chromium.org
    BUG=490304
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1186613004
    
    Cr-Commit-Position: refs/heads/master@{#334420}

--- tools/perf/benchmarks/power.py
+++ tools/perf/benchmarks/power.py
@@ -16,6 +16,10 @@ class PowerAndroidAcceptance(perf_benchmark.PerfBenchmark):
   """Android power acceptance test."""
   test = power.Power
   page_set = page_sets.AndroidAcceptancePageSet
+
+  def SetExtraBrowserOptions(self, options):
+    options.full_performance_mode = False
+
   @classmethod
   def Name(cls):
     return 'power.android_acceptance'
@@ -26,6 +30,10 @@ class PowerTypical10Mobile(perf_benchmark.PerfBenchmark):
   """Android typical 10 mobile power test."""
   test = power.Power
   page_set = page_sets.Typical10MobilePageSet
+
+  def SetExtraBrowserOptions(self, options):
+    options.full_performance_mode = False
+
   @classmethod
   def Name(cls):
     return 'power.typical_10_mobile'
@@ -37,6 +45,10 @@ class PowerTypical10MobileReload(perf_benchmark.PerfBenchmark):
   """Android typical 10 mobile power reload test."""
   test = power.LoadPower
   page_set = page_sets.Typical10MobileReloadPageSet
+
+  def SetExtraBrowserOptions(self, options):
+    options.full_performance_mode = False
+
   @classmethod
   def Name(cls):
     return 'power.typical_10_mobile_reload'
@@ -51,6 +63,7 @@ class PowerGpuRasterizationTypical10Mobile(perf_benchmark.PerfBenchmark):
 
   def SetExtraBrowserOptions(self, options):
     silk_flags.CustomizeBrowserOptionsForGpuRasterization(options)
+    options.full_performance_mode = False
 
   @classmethod
   def Name(cls):
@@ -61,6 +74,10 @@ class PowerTop10(perf_benchmark.PerfBenchmark):
   """Top 10 quiescent power test."""
   test = power.QuiescentPower
   page_set = page_sets.Top10PageSet
+
+  def SetExtraBrowserOptions(self, options):
+    options.full_performance_mode = False
+
   @classmethod
   def Name(cls):
     return 'power.top_10'
@@ -71,6 +88,10 @@ class PowerTop25(perf_benchmark.PerfBenchmark):
   """Top 25 quiescent power test."""
   test = power.QuiescentPower
   page_set = page_sets.Top25PageSet
+
+  def SetExtraBrowserOptions(self, options):
+    options.full_performance_mode = False
+
   @classmethod
   def Name(cls):
     return 'power.top_25'
@@ -96,6 +117,7 @@ class PowerPPSControlDisabled(perf_benchmark.PerfBenchmark):
 
   def SetExtraBrowserOptions(self, options):
     options.AppendExtraBrowserArgs(['--disable-plugin-power-saver'])
+    options.full_performance_mode = False
 
   @classmethod
   def Name(cls):
@@ -113,6 +135,7 @@ class PowerPPSControlEnabled(perf_benchmark.PerfBenchmark):
 
   def SetExtraBrowserOptions(self, options):
     options.AppendExtraBrowserArgs(['--enable-plugin-power-saver'])
+    options.full_performance_mode = False
 
   @classmethod
   def Name(cls):
@@ -129,6 +152,7 @@ class PowerThrottledPlugins(perf_benchmark.PerfBenchmark):
 
   def SetExtraBrowserOptions(self, options):
     options.AppendExtraBrowserArgs(['--disable-plugin-power-saver'])
+    options.full_performance_mode = False
 
   @classmethod
   def Name(cls):
@@ -145,6 +169,7 @@ class PowerThrottledPluginsPPS(perf_benchmark.PerfBenchmark):
 
   def SetExtraBrowserOptions(self, options):
     options.AppendExtraBrowserArgs(['--enable-plugin-power-saver'])
+    options.full_performance_mode = False
 
   @classmethod
   def Name(cls):
