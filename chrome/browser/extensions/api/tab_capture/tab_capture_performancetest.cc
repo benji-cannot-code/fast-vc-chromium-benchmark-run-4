98bfa9fc9c55314d5a66f0b7eef42b15c5ceddc3
jdduke <jdduke@chromium.org>
2015-06-15 16:30:26 -0700

    Cleanup of unnecessary WebWidgetClient compositor methods
    There are several compositor-specific WebWidgetClient methods that are
    unused by Blink and implemented by RenderWidget. These methods are only
    used by RenderWidgetCompositor. Decouple the Blink dependency, making
    these methods local to content/.
    
    The WebWidgetClient methods will be removed in
    https://codereview.chromium.org/1174273005.
    
    BUG=438646
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1181983005
    
    Cr-Commit-Position: refs/heads/master@{#334494}

--- chrome/browser/extensions/api/tab_capture/tab_capture_performancetest.cc
+++ chrome/browser/extensions/api/tab_capture/tab_capture_performancetest.cc
@@ -199,7 +199,7 @@ class TabCapturePerformanceTest
     bool gpu_frames = PrintResults(
         analyzer.get(),
         test_name,
-        "RenderWidget::didCommitAndDrawCompositorFrame",
+        "RenderWidget::DidCommitAndDrawCompositorFrame",
         "ms");
     EXPECT_TRUE(gpu_frames);
 
