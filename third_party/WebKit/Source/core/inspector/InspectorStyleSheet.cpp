a3bbc607863a92d12f227ce0f6d0f8f84069b221
sigbjornf@opera.com <sigbjornf@opera.com>
2015-06-15 20:19:22 +0000

    Oilpan: fix build after r197129.
    TBR=oilpan-reviews
    BUG=none
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1186823005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197136 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorStyleSheet.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorStyleSheet.cpp
@@ -338,7 +338,10 @@ void StyleSheetHandler::endMediaQuery()
 
 class ParsedStyleSheet : public NoBaseWillBeGarbageCollectedFinalized<ParsedStyleSheet> {
 public:
-    ParsedStyleSheet(CSSStyleSheet* pageStyleSheet);
+    static PassOwnPtrWillBeRawPtr<ParsedStyleSheet> create(CSSStyleSheet* pageStyleSheet)
+    {
+        return adoptPtrWillBeNoop(new ParsedStyleSheet(pageStyleSheet));
+    }
 
     const String& text() const { ASSERT(m_hasText); return m_text; }
     void setText(const String&);
@@ -351,6 +354,8 @@ public:
     DECLARE_TRACE();
 
 private:
+    explicit ParsedStyleSheet(CSSStyleSheet* pageStyleSheet);
+
     void flattenSourceData(RuleSourceDataList*);
     void setSourceData(PassOwnPtrWillBeRawPtr<RuleSourceDataList>);
 
@@ -949,7 +954,7 @@ InspectorStyleSheet::InspectorStyleSheet(InspectorResourceAgent* resourceAgent, 
     , m_origin(origin)
     , m_documentURL(documentURL)
 {
-    m_parsedStyleSheet = adoptPtr(new ParsedStyleSheet(m_pageStyleSheet.get()));
+    m_parsedStyleSheet = ParsedStyleSheet::create(m_pageStyleSheet.get());
 }
 
 InspectorStyleSheet::~InspectorStyleSheet()
