9ebbd8022bac48ba30dc500b950199d57314d274
fmalita@chromium.org <fmalita@chromium.org>
2015-06-16 21:45:57 +0000

    [SVG] Re-enable experimental PathOpsSVGClipping support
    All known PathOps issues have been addressed, re-enabling.
    
    Also refactored to use an SkOpBuilder for more effcient path unioning.
    
    BUG=263911
    R=caryclark@google.com,fs@opera.com
    
    Review URL: https://codereview.chromium.org/1185383002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGResourceClipper.cpp
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGResourceClipper.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/paint/DrawingDisplayItem.h"
 #include "platform/graphics/paint/SkPictureBuilder.h"
 #include "third_party/skia/include/core/SkPicture.h"
+#include "third_party/skia/include/pathops/SkPathOps.h"
 
 namespace blink {
 
@@ -74,6 +75,8 @@ bool LayoutSVGResourceClipper::tryPathOnlyClipping(const LayoutObject& layoutObj
         return false;
 
     Path clipPath;
+    bool usingBuilder = false;
+    SkOpBuilder clipPathBuilder;
 
     for (SVGElement* childElement = Traversal<SVGElement>::firstChild(*element()); childElement; childElement = Traversal<SVGElement>::nextSibling(*childElement)) {
         LayoutObject* childLayoutObject = childElement->layoutObject();
@@ -93,21 +96,32 @@ bool LayoutSVGResourceClipper::tryPathOnlyClipping(const LayoutObject& layoutObj
         if (!svgStyle.clipperResource().isEmpty())
             return false;
 
+        // First clip shape.
         if (clipPath.isEmpty()) {
-            // First clip shape.
             styled->toClipPath(clipPath);
+
             continue;
         }
 
-        if (RuntimeEnabledFeatures::pathOpsSVGClippingEnabled()) {
-            // Attempt to generate a combined clip path, fall back to masking if not possible.
-            Path subPath;
-            styled->toClipPath(subPath);
-            if (!clipPath.unionPath(subPath))
-                return false;
-        } else {
+        // Multiple shapes require PathOps.
+        if (!RuntimeEnabledFeatures::pathOpsSVGClippingEnabled())
             return false;
+
+        // Second clip shape => start using the builder.
+        if (!usingBuilder) {
+            clipPathBuilder.add(clipPath.skPath(), kUnion_SkPathOp);
+            usingBuilder = true;
         }
+
+        Path subPath;
+        styled->toClipPath(subPath);
+        clipPathBuilder.add(subPath.skPath(), kUnion_SkPathOp);
+    }
+
+    if (usingBuilder) {
+        SkPath resolvedPath;
+        clipPathBuilder.resolve(&resolvedPath);
+        clipPath = resolvedPath;
     }
 
     // We are able to represent the clip as a path. Continue with direct clipping,
