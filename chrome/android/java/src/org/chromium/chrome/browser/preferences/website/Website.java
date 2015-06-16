5d4b4150bb308d7da59b13aecbf366f6a519c319
newt <newt@chromium.org>
2015-06-16 03:57:36 -0700

    Fix crash when sorting WebsitePreferences.
    Website.compareByStorageTo() had an error where it would return -1
    (instead of 0) when comparing two websites with the same amount of
    storage. As a result, Collections.sort() would sometimes throw an
    exception: "Comparison method violates its general contract!".
    
    This fixes that bug and introduces MathUtils.compareLongs() to prevent
    similar errors in other parts of the codebase. A follow-up CL will use
    this method in more places.
    
    Note: it's possible there are other bugs in the compareTo() method. I'll
    monitor the crash reports after landing this to see if more bugs need
    fixing.
    
    BUG=486884
    
    Review URL: https://codereview.chromium.org/1184163006
    
    Cr-Commit-Position: refs/heads/master@{#334583}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/Website.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/Website.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.preferences.website;
 
+import org.chromium.chrome.browser.util.MathUtils;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -67,7 +69,7 @@ public class Website implements Serializable {
      */
     public int compareByStorageTo(Website to) {
         if (this == to) return 0;
-        return getTotalUsage() < to.getTotalUsage() ? 1 : -1;
+        return MathUtils.compareLongs(to.getTotalUsage(), getTotalUsage());
     }
 
     /**
