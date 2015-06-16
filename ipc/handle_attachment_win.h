e2f232d918a10fccdeeff30d78193e5139f7ee30
erikchen <erikchen@chromium.org>
2015-06-16 13:20:51 -0700

    IPC: Add new classes required for attachment brokering.
    The implementations of these classes are just stubs.
    
    BUG=493414
    
    Review URL: https://codereview.chromium.org/1180313007
    
    Cr-Commit-Position: refs/heads/master@{#334676}

---
+++ ipc/handle_attachment_win.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IPC_HANDLE_ATTACHMENT_WIN_H_
+#define IPC_HANDLE_ATTACHMENT_WIN_H_
+
+#include "ipc/brokerable_attachment.h"
+#include "ipc/ipc_export.h"
+
+namespace IPC {
+namespace internal {
+
+// This class represents a Windows HANDLE attached to a Chrome IPC message.
+class IPC_EXPORT HandleAttachmentWin : public BrokerableAttachment {
+ private:
+  Type GetType() const override { return TYPE_WIN_HANDLE; }
+  HANDLE handle_;
+};
+
+}  // namespace internal
+}  // namespace IPC
+
+#endif  // IPC_HANDLE_ATTACHMENT_WIN_H_
--- ipc/ipc.gypi
+++ ipc/ipc.gypi
@@ -12,6 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       # This part is shared between the targets defined below.
       ['ipc_target==1', {
         'sources': [
+          'attachment_broker.h',
+          'attachment_broker_win.cc',
+          'attachment_broker_win.h',
+          'brokerable_attachment.cc',
+          'brokerable_attachment.h',
+          'handle_attachment_win.h',
           'ipc_channel.cc',
           'ipc_channel.h',
           'ipc_channel_factory.cc',
