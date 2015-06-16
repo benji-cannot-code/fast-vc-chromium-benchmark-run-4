2d5b1bba21c63fca5819adf6e6f38c92dd934d7e
kmarshall <kmarshall@chromium.org>
2015-06-16 15:28:10 -0700

    Modify Cast Channel API to convert socket library connection timeouts into Cast Channel timeouts. The change is made to the TCP and SSL portions of the connection logic. Add unit tests to verify the new functionality. BUG=500726 R=mfoltz@chromium.org CC=amp@chromium.org
    Review URL: https://codereview.chromium.org/1183453003
    
    Cr-Commit-Position: refs/heads/master@{#334716}

--- extensions/browser/api/cast_channel/cast_socket.cc
+++ extensions/browser/api/cast_channel/cast_socket.cc
@@ -385,6 +385,8 @@ int CastSocketImpl::DoTcpConnectComplete(int connect_result) {
                                 connect_result);
   if (connect_result == net::OK) {
     SetConnectState(proto::CONN_STATE_SSL_CONNECT);
+  } else if (connect_result == net::ERR_CONNECTION_TIMED_OUT) {
+    SetErrorState(CHANNEL_ERROR_CONNECT_TIMEOUT);
   } else {
     SetErrorState(CHANNEL_ERROR_CONNECT_ERROR);
   }
@@ -428,6 +430,8 @@ int CastSocketImpl::DoSslConnectComplete(int result) {
     } else {
       transport_->Start();
     }
+  } else if (result == net::ERR_CONNECTION_TIMED_OUT) {
+    SetErrorState(CHANNEL_ERROR_CONNECT_TIMEOUT);
   } else {
     SetErrorState(CHANNEL_ERROR_AUTHENTICATION_ERROR);
   }
