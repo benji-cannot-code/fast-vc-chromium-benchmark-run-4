b571a6c7eac63f360c57ad5bdbaaa783b17ebbe5
hush <hush@chromium.org>
2015-06-15 18:31:03 -0700

    Test WebView without a WebViewClient
    This is the supposed behavior when a WebView is created with no
    WebViewClient:
    case 1. loadUrl will load the url directly in the webview without
    creating an intent and query the OS to handle the intent. If the loaded
    url is redirected by the server, then a browser intent with the
    redirected url will be sent.
    case 2. clicking on the links should create an intent instead of loading
    the link in WebView
    
    BUG=499331
    
    Review URL: https://codereview.chromium.org/1181623005
    
    Cr-Commit-Position: refs/heads/master@{#334526}

--- android_webview/test/shell/src/org/chromium/android_webview/test/AwTestRunnerActivity.java
+++ android_webview/test/shell/src/org/chromium/android_webview/test/AwTestRunnerActivity.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.android_webview.test;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
@@ -17,6 +18,8 @@ import android.widget.LinearLayout;
 public class AwTestRunnerActivity extends Activity {
 
     private LinearLayout mLinearLayout;
+    private Intent mLastSentIntent;
+    private boolean mIgnoreStartActivity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,5 +52,19 @@ public class AwTestRunnerActivity extends Activity {
      */
     public void removeAllViews() {
         mLinearLayout.removeAllViews();
+    }
+
+    @Override
+    public void startActivity(Intent i) {
+        mLastSentIntent = i;
+        if (!mIgnoreStartActivity) super.startActivity(i);
+    }
+
+    public Intent getLastSentIntent() {
+        return mLastSentIntent;
+    }
+
+    public void setIgnoreStartActivity(boolean ignore) {
+        mIgnoreStartActivity = ignore;
     }
 }
