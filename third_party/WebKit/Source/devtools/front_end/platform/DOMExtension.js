ade3960604731a9761588be977af63c155e3a014
alph@chromium.org <alph@chromium.org>
2015-06-16 10:04:17 +0000

    DevTools: Fix Element.positionAt
    Review URL: https://codereview.chromium.org/1188083002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197168 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/platform/DOMExtension.js
+++ third_party/WebKit/Source/devtools/front_end/platform/DOMExtension.js
@@ -161,6 +161,11 @@ Element.prototype.positionAt = function(x, y, relativeTo)
         this.style.setProperty("top", (shift.y + y) + "px");
     else
         this.style.removeProperty("top");
+
+    if (typeof x === "number" || typeof y === "number")
+        this.style.setProperty("position", "absolute");
+    else
+        this.style.removeProperty("position");
 }
 
 /**
