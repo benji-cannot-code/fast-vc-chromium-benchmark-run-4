319b72fbfc8c942b7457c42e7293b20fe885da7f
cevans@chromium.org <cevans@chromium.org>
2015-06-16 17:19:57 +0000

    Improve seeding of AddressSpaceRandomization randomness.
    - Mix in a bit of time as well as pid. (On Linux, pids can be identical
    between processes because of namespacing.)
    - We use raw calls to fetch the time, to keep PartitionAlloc standalone and
    not depend on a platform callback. Furthermore, we're deep inside an allocator
    at this point and the platform callbacks are not simple and might re-enter a
    memory allocator which is a no-no. For consistency, bring the process id
    related calls inline.
    - Also add a TODO to improve the randomization range on Win8.1+.
    
    BUG=499465
    
    Review URL: https://codereview.chromium.org/1185303003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197182 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/AddressSpaceRandomization.cpp
+++ third_party/WebKit/Source/wtf/AddressSpaceRandomization.cpp
@@ -7,8 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/AddressSpaceRandomization.h"
 
 #include "wtf/PageAllocator.h"
-#include "wtf/ProcessID.h"
 #include "wtf/SpinLock.h"
+
+#if OS(WIN)
+#include <windows.h>
+#else
+#include <sys/time.h>
+#include <unistd.h>
+#endif
 
 namespace WTF {
 
@@ -46,7 +52,21 @@ uint32_t ranval(ranctx* x)
         x->initialized = true;
         char c;
         uint32_t seed = static_cast<uint32_t>(reinterpret_cast<uintptr_t>(&c));
-        seed ^= static_cast<uint32_t>(getCurrentProcessID());
+        uint32_t pid;
+        uint32_t usec;
+#if OS(WIN)
+        pid = GetCurrentProcessId();
+        SYSTEMTIME st;
+        GetSystemTime(&st);
+        usec = static_cast<uint32_t>(st.wMilliseconds * 1000);
+#else
+        pid = static_cast<uint32_t>(getpid());
+        struct timeval tv;
+        gettimeofday(&tv, 0);
+        usec = static_cast<uint32_t>(tv.tv_usec);
+#endif
+        seed ^= pid;
+        seed ^= usec;
         x->a = 0xf1ea5eed;
         x->b = x->c = x->d = seed;
         for (int i = 0; i < 20; ++i) {
@@ -77,6 +97,7 @@ void* getRandomPageBase()
 #if OS(WIN)
     // 64-bit Windows has a bizarrely small 8TB user address space.
     // Allocates in the 1-5TB region.
+    // TODO(cevans): I think Win 8.1 has 47-bits like Linux.
     random &= 0x3ffffffffffUL;
     random += 0x10000000000UL;
 #else
