2a467d49a793528514855fe8c267f7e0e20e8e71
tedchoc <tedchoc@chromium.org>
2015-06-16 16:47:50 -0700

    Fix lint error introduced by NTP doodle change for document mode.
    https://codereview.chromium.org/1187953003
    
    BUG=428091
    
    Review URL: https://codereview.chromium.org/1183433005
    
    Cr-Commit-Position: refs/heads/master@{#334751}

--- chrome/android/java_staging/src/org/chromium/chrome/browser/ntp/NewTabPage.java
+++ chrome/android/java_staging/src/org/chromium/chrome/browser/ntp/NewTabPage.java
@@ -19,6 +19,7 @@ import android.view.View;
 
 import com.google.android.apps.chrome.R;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
@@ -256,7 +257,7 @@ public class NewTabPage
                 case ID_OPEN_IN_INCOGNITO_TAB:
                     recordOpenedMostVisitedItem(item);
                     if (FeatureUtilities.isDocumentMode(mActivity)) {
-                        mActivity.finishAndRemoveTask();
+                        ApiCompatibilityUtils.finishAndRemoveTask(mActivity);
                     }
                     mTabModelSelector.openNewTab(new LoadUrlParams(item.getUrl()),
                             TabLaunchType.FROM_LONGPRESS_FOREGROUND, mTab, true);
