c8b4c1b499f2bf1e29950ce5e1f206641ee740c9
afakhry <afakhry@chromium.org>
2015-06-15 19:54:14 -0700

    Enable options to remap CapsLock whenever an external keyboard is connected.
    As per comments #16 and #17 on issue 167237, we will assume that most
    external keyboards have a CapsLock key (chromeos external keyboards are
    exceptions. However, there's no way to distinguish them at the moment).
    
    This CL enables/disables the CapsLock remapping options in the keyboard
    settings whenever there's an external keyboard is connected/disconnected
    respectively.
    
    BUG=167237
    
    Review URL: https://codereview.chromium.org/1188693002
    
    Cr-Commit-Position: refs/heads/master@{#334538}

--- chrome/browser/ui/webui/options/chromeos/keyboard_handler.cc
+++ chrome/browser/ui/webui/options/chromeos/keyboard_handler.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui.h"
 #include "ui/base/ime/chromeos/ime_keyboard.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/events/devices/device_data_manager.h"
+#include "ui/events/devices/keyboard_device.h"
 
 namespace {
 const struct ModifierKeysSelectItem {
@@ -43,15 +45,27 @@ const char* kDataValuesNames[] = {
   "remapCapsLockKeyToValue",
   "remapDiamondKeyToValue",
 };
+
+bool HasExternalKeyboard() {
+  for (const ui::KeyboardDevice& keyboard :
+       ui::DeviceDataManager::GetInstance()->keyboard_devices()) {
+    if (keyboard.type == ui::InputDeviceType::INPUT_DEVICE_EXTERNAL)
+      return true;
+  }
+
+  return false;
+}
 }  // namespace
 
 namespace chromeos {
 namespace options {
 
 KeyboardHandler::KeyboardHandler() {
+  ui::DeviceDataManager::GetInstance()->AddObserver(this);
 }
 
 KeyboardHandler::~KeyboardHandler() {
+  ui::DeviceDataManager::GetInstance()->RemoveObserver(this);
 }
 
 void KeyboardHandler::GetLocalizedValues(
@@ -132,20 +146,15 @@ void KeyboardHandler::GetLocalizedValues(
 }
 
 void KeyboardHandler::InitializePage() {
-  bool chromeos_keyboard = base::CommandLine::ForCurrentProcess()->HasSwitch(
-      chromeos::switches::kHasChromeOSKeyboard);
-  const base::FundamentalValue show_caps_lock_options(!chromeos_keyboard);
-
   bool has_diamond_key = base::CommandLine::ForCurrentProcess()->HasSwitch(
       chromeos::switches::kHasChromeOSDiamondKey);
   const base::FundamentalValue show_diamond_key_options(has_diamond_key);
 
   web_ui()->CallJavascriptFunction(
-      "options.KeyboardOverlay.showCapsLockOptions",
-      show_caps_lock_options);
-  web_ui()->CallJavascriptFunction(
       "options.KeyboardOverlay.showDiamondKeyOptions",
       show_diamond_key_options);
+
+  UpdateCapsLockOptions();
 }
 
 void KeyboardHandler::RegisterMessages() {
@@ -156,8 +165,19 @@ void KeyboardHandler::RegisterMessages() {
                  base::Unretained(this)));
 }
 
+void KeyboardHandler::OnKeyboardDeviceConfigurationChanged() {
+  UpdateCapsLockOptions();
+}
+
 void KeyboardHandler::HandleShowKeyboardShortcuts(const base::ListValue* args) {
   ash::Shell::GetInstance()->new_window_delegate()->ShowKeyboardOverlay();
+}
+
+void KeyboardHandler::UpdateCapsLockOptions() const {
+  const base::FundamentalValue show_caps_lock_options(HasExternalKeyboard());
+  web_ui()->CallJavascriptFunction(
+      "options.KeyboardOverlay.showCapsLockOptions",
+      show_caps_lock_options);
 }
 
 }  // namespace options
--- chrome/browser/ui/webui/options/chromeos/keyboard_handler.h
+++ chrome/browser/ui/webui/options/chromeos/keyboard_handler.h
@@ -9,12 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/prefs/pref_member.h"
 #include "chrome/browser/ui/webui/options/options_ui.h"
+#include "ui/events/devices/input_device_event_observer.h"
 
 namespace chromeos {
 namespace options {
 
 // Customize modifier keys overlay page UI handler.
-class KeyboardHandler : public ::options::OptionsPageUIHandler {
+class KeyboardHandler
+    : public ::options::OptionsPageUIHandler,
+      public ui::InputDeviceEventObserver {
  public:
   KeyboardHandler();
   ~KeyboardHandler() override;
@@ -24,9 +27,16 @@ class KeyboardHandler : public ::options::OptionsPageUIHandler {
   void InitializePage() override;
   void RegisterMessages() override;
 
+  // ui::InputDeviceEventObserver:
+  void OnKeyboardDeviceConfigurationChanged() override;
+
  private:
   // Show the keyboard shortcuts overlay from the options page.
   void HandleShowKeyboardShortcuts(const base::ListValue* args);
+
+  // Shows or hides the CapsLock options based on whether or not there is an
+  // external keyboard connected.
+  void UpdateCapsLockOptions() const;
 
   DISALLOW_COPY_AND_ASSIGN(KeyboardHandler);
 };
