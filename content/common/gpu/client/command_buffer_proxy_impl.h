f39f070775c4bde93734ad2f4592e65bedf0f459
reveman <reveman@chromium.org>
2015-06-15 19:51:58 -0700

    content: Remove unused member in CommandBufferProxyImpl.
    This removes CommandBufferProxyImpl::gpu_memory_buffers_.
    
    BUG=
    TBR=piman@chromium.org
    
    Review URL: https://codereview.chromium.org/1189743002
    
    Cr-Commit-Position: refs/heads/master@{#334535}

--- content/common/gpu/client/command_buffer_proxy_impl.h
+++ content/common/gpu/client/command_buffer_proxy_impl.h
@@ -164,8 +164,6 @@ class CommandBufferProxyImpl
  private:
   typedef std::map<int32, scoped_refptr<gpu::Buffer> > TransferBufferMap;
   typedef base::hash_map<uint32, base::Closure> SignalTaskMap;
-  typedef base::ScopedPtrHashMap<int32, scoped_ptr<gfx::GpuMemoryBuffer>>
-      GpuMemoryBufferMap;
 
   void CheckLock() {
     if (lock_)
@@ -223,9 +221,6 @@ class CommandBufferProxyImpl
   // Tasks to be invoked in SignalSyncPoint responses.
   uint32 next_signal_id_;
   SignalTaskMap signal_tasks_;
-
-  // Local cache of id to gpu memory buffer mapping.
-  GpuMemoryBufferMap gpu_memory_buffers_;
 
   gpu::Capabilities capabilities_;
 
