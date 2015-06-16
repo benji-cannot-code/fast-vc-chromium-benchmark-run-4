6779b9ce9fbafb3f57060b865c1cfe819fdd7311
gmanikpure <gmanikpure@chromium.org>
2015-06-16 10:31:39 -0700

    Revert of [chromedriver] silence chrome error message on windows (patchset #5 id:80001 of https://codereview.chromium.org/699273002/)
    Reason for revert:
    The code seems to have caused chromedriver Issue1080.
    Chromedriver version 2.12 works fine, and the issue started with the version 2.13 which contains this code.
    Hence reverting.
    
    Original issue's description:
    > [chromedriver] silence chrome error message on windows
    >
    > BUG=chromedriver:626
    >
    > Committed: https://crrev.com/fb2145ccc9bf28832d63f16e5deeb727aa18b7e9
    > Cr-Commit-Position: refs/heads/master@{#302943}
    
    TBR=samuong@google.com,samuong@chromium.org,andrewcheng@chromium.org
    
    Review URL: https://codereview.chromium.org/1181803006
    
    Cr-Commit-Position: refs/heads/master@{#334628}

--- chrome/test/chromedriver/chrome_launcher.cc
+++ chrome/test/chromedriver/chrome_launcher.cc
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/stat.h>
 #include <sys/types.h>
 #elif defined(OS_WIN)
-#include "base/win/scoped_handle.h"
 #include "chrome/test/chromedriver/keycode_text_conversion.h"
 #endif
 
@@ -329,25 +328,6 @@ Status LaunchDesktopChrome(
     options.fds_to_remap = &no_stderr;
   }
 #elif defined(OS_WIN)
-  // Silence chrome error message.
-  HANDLE out_read;
-  HANDLE out_write;
-  SECURITY_ATTRIBUTES sa_attr;
-
-  sa_attr.nLength = sizeof(SECURITY_ATTRIBUTES);
-  sa_attr.bInheritHandle = TRUE;
-  sa_attr.lpSecurityDescriptor = NULL;
-  if (!CreatePipe(&out_read, &out_write, &sa_attr, 0))
-      return Status(kUnknownError, "CreatePipe() - Pipe creation failed");
-  // Prevent handle leak.
-  base::win::ScopedHandle scoped_out_read(out_read);
-  base::win::ScopedHandle scoped_out_write(out_write);
-
-  options.stdout_handle = out_write;
-  options.stderr_handle = out_write;
-  options.stdin_handle = GetStdHandle(STD_INPUT_HANDLE);
-  options.inherit_handles = true;
-
   if (!SwitchToUSKeyboardLayout())
     VLOG(0) << "Can not set to US keyboard layout - Some keycodes may be"
         "interpreted incorrectly";
