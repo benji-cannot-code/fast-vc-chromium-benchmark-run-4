76bd94f4c5fe8b6d784078be1583d33051e13f5c
msw <msw@chromium.org>
2015-06-15 20:09:32 -0700

    Reland: Build Mojo apps in sub-dirs, like application packages.
    Reland https://codereview.chromium.org/1189703004/
    Also fix ShellTestBase for mojo_runner_unittests, etc.
    Share and copy asset caching code used by ShellMain.java.
    
    Original description (still correct):
    Build Mojo apps in subdirectories, like application packages.
    
    Build all <foo>.mojo targets in a <foo> sub-directory.
    Support resources bundled in mojo_native_application.
    Remove mojo_application_package (now redundant).
    Update hard-coded paths and simplify URL resolution code.
    
    Remove apps bundled in core_services from Mandoline.apk
    (network service, surfaces service, view manager)
    
    This would help NetworkFetcher resolve app locations.
    (Android apptests host the build dir on a local server)
    (but apptests should instead bundle apps in an apk)
    
    BUG=498020
    TEST=Mojo runner and Mandoline still work (as much as they currently do :-/)
    R=scottmg@chromium.org,sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1189703004
    
    Cr-Commit-Position: refs/heads/master@{#334547}

--- mojo/runner/android/apk/src/org/chromium/mojo/shell/AndroidHandler.java
+++ mojo/runner/android/apk/src/org/chromium/mojo/shell/AndroidHandler.java
@@ -244,8 +244,8 @@ public class AndroidHandler {
     }
 
     @CalledByNative
-    static String getLocalAppsDir(Context context) {
-        return ShellMain.getLocalAppsDir(context).getAbsolutePath();
+    static String getCachedAppsDir(Context context) {
+        return ShellMain.getCachedAppsDir(context).getAbsolutePath();
     }
 
     private static File getDexOutputDir(Context context) {
