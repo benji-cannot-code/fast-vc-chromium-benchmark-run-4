1a942b1960df8f43b233ca0050441a1c47736ed2
ssid@chromium.org <ssid@chromium.org>
2015-06-16 10:10:21 +0000

    Adding unittest for blink gc dump provider.
    This CL adds the unittest for the dump provider, which was added
    earlier in crrev.com/1148823002
    
    BUG=490087
    
    Review URL: https://codereview.chromium.org/1179613004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197169 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/platform/heap/BlinkGCMemoryDumpProviderTest.cpp
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "platform/heap/BlinkGCMemoryDumpProvider.h"
+
+#include "public/platform/Platform.h"
+#include "public/platform/WebProcessMemoryDump.h"
+
+#include <gtest/gtest.h>
+
+namespace blink {
+
+TEST(BlinkGCDumpProviderTest, MemoryDump)
+{
+    WebProcessMemoryDump* dump  = Platform::current()->createProcessMemoryDump();
+    ASSERT(dump);
+    BlinkGCMemoryDumpProvider::instance()->onMemoryDump(dump);
+    ASSERT(dump->getMemoryAllocatorDump("blink_gc"));
+    ASSERT(dump->getMemoryAllocatorDump("blink_gc/allocated_objects"));
+}
+
+} // namespace blink
--- third_party/WebKit/Source/platform/heap/blink_heap.gypi
+++ third_party/WebKit/Source/platform/heap/blink_heap.gypi
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     ],
     'platform_heap_test_files': [
       'HeapTest.cpp',
+      'BlinkGCMemoryDumpProviderTest.cpp',
     ],
     'conditions': [
       ['target_arch == "arm"', {
