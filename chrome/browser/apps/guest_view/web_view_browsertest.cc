3a671b789b44dffe1094923380ecfa5d3a306961
paulmeyer <paulmeyer@chromium.org>
2015-06-16 11:25:10 -0700

    Renamed RenderProcessHost::IsIsolatedGuest() to RenderProcessHost::IsForGuestsOnly().
    BUG=499133
    
    TBR=kalman@chromium.org for user_script_loader.cc
    
    Review URL: https://codereview.chromium.org/1191573002
    
    Cr-Commit-Position: refs/heads/master@{#334642}

--- chrome/browser/apps/guest_view/web_view_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_browsertest.cc
@@ -523,7 +523,7 @@ class WebViewTest : public extensions::PlatformAppBrowserTest {
     content::Source<content::NavigationController> source =
         guest_observer.source();
     EXPECT_TRUE(source->GetWebContents()->GetRenderProcessHost()->
-        IsIsolatedGuest());
+        IsForGuestsOnly());
 
     content::WebContents* guest_web_contents = source->GetWebContents();
     return guest_web_contents;
@@ -557,7 +557,7 @@ class WebViewTest : public extensions::PlatformAppBrowserTest {
     // Wait for interstitial page to be shown in guest.
     content::WebContents* guest_web_contents =
         GetGuestViewManager()->WaitForSingleGuestCreated();
-    ASSERT_TRUE(guest_web_contents->GetRenderProcessHost()->IsIsolatedGuest());
+    ASSERT_TRUE(guest_web_contents->GetRenderProcessHost()->IsForGuestsOnly());
     content::WaitForInterstitialAttach(guest_web_contents);
   }
 
@@ -1206,7 +1206,7 @@ IN_PROC_BROWSER_TEST_F(WebViewTest, Shim_TestRemoveWebviewOnExit) {
   content::Source<content::NavigationController> source =
       guest_observer.source();
   EXPECT_TRUE(source->GetWebContents()->GetRenderProcessHost()->
-      IsIsolatedGuest());
+      IsForGuestsOnly());
 
   ASSERT_TRUE(guest_loaded_listener.WaitUntilSatisfied());
 
