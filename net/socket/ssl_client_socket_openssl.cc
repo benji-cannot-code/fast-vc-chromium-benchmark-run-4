675912cbe4d349c378732a873a3c8f6d6be4de8c
davidben <davidben@chromium.org>
2015-06-16 15:21:27 -0700

    Don't put IP addresses in SNI in SSLClientSocketOpenSSL.
    The server_name extension should only be used for hostnames.
    
    BUG=500981
    
    Review URL: https://codereview.chromium.org/1191483004
    
    Cr-Commit-Position: refs/heads/master@{#334714}

--- net/socket/ssl_client_socket_openssl.cc
+++ net/socket/ssl_client_socket_openssl.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/ec_private_key.h"
 #include "crypto/openssl_util.h"
 #include "crypto/scoped_openssl_types.h"
+#include "net/base/ip_address_number.h"
 #include "net/base/net_errors.h"
 #include "net/cert/cert_policy_enforcer.h"
 #include "net/cert/cert_verifier.h"
@@ -697,8 +698,16 @@ int SSLClientSocketOpenSSL::Init() {
   if (!ssl_ || !context->SetClientSocketForSSL(ssl_, this))
     return ERR_UNEXPECTED;
 
-  if (!SSL_set_tlsext_host_name(ssl_, host_and_port_.host().c_str()))
+  // SNI should only contain valid DNS hostnames, not IP addresses (see RFC
+  // 6066, Section 3).
+  //
+  // TODO(rsleevi): Should this code allow hostnames that violate the LDH rule?
+  // See https://crbug.com/496472 and https://crbug.com/496468 for discussion.
+  IPAddressNumber unused;
+  if (!ParseIPLiteralToNumber(host_and_port_.host(), &unused) &&
+      !SSL_set_tlsext_host_name(ssl_, host_and_port_.host().c_str())) {
     return ERR_UNEXPECTED;
+  }
 
   SSL_SESSION* session = context->session_cache()->Lookup(GetSessionCacheKey());
   if (session != nullptr)
