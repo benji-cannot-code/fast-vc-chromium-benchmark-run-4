d407fcf139f71e6c6e611e5e49ef64ec03adabbe
yzshen <yzshen@chromium.org>
2015-06-17 11:23:12 -0700

    Mandoline DevTools service: make blink_perf.dom Telemetry tests work.
    - Close HTTP connection if the request is to list available agents while
      no agents are registered.
    - Always handle Page.navigate command within the same html_viewer
      instance. This is a temporary approach that should be removed once
      the browser is able to navigate an existing html_viewer instance and
      about:blank page support is ready.
    
    BUG=478249
    
    Review URL: https://codereview.chromium.org/1191953003
    
    Cr-Commit-Position: refs/heads/master@{#334878}

--- components/html_viewer/devtools_agent_impl.h
+++ components/html_viewer/devtools_agent_impl.h
@@ -31,6 +31,11 @@ class DevToolsAgentImpl : public devtools_service::DevToolsAgent,
 
   blink::WebLocalFrame* frame() const { return frame_; }
 
+  // Returns whether a "Page.navigate" command is being handled.
+  bool handling_page_navigate_request() const {
+    return handling_page_navigate_request_;
+  }
+
  private:
   // devtools_service::DevToolsAgent implementation.
   void SetClient(devtools_service::DevToolsAgentClientPtr client,
@@ -48,6 +53,8 @@ class DevToolsAgentImpl : public devtools_service::DevToolsAgent,
   blink::WebLocalFrame* const frame_;
   mojo::Binding<DevToolsAgent> binding_;
   devtools_service::DevToolsAgentClientPtr client_;
+
+  bool handling_page_navigate_request_;
 
   DISALLOW_COPY_AND_ASSIGN(DevToolsAgentImpl);
 };
