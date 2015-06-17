76ec710a3d3da300f43d3f01fe0c4b7c5986d51a
timloh@chromium.org <timloh@chromium.org>
2015-06-17 01:53:03 +0000

    Remove constant default-arg from DeprecatedPaintLayer::currentTransform
    Review URL: https://codereview.chromium.org/1192463006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197224 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/DeprecatedPaintLayer.h
+++ third_party/WebKit/Source/core/paint/DeprecatedPaintLayer.h
@@ -272,7 +272,7 @@ public:
     // currentTransform computes a transform which takes accelerated animations into account. The
     // resulting transform has transform-origin baked in. If the layer does not have a transform,
     // returns the identity matrix.
-    TransformationMatrix currentTransform(ComputedStyle::ApplyTransformOrigin = ComputedStyle::IncludeTransformOrigin) const;
+    TransformationMatrix currentTransform() const;
     TransformationMatrix renderableTransform(PaintBehavior) const;
 
     // Get the perspective transform, which is applied to transformed sublayers.
