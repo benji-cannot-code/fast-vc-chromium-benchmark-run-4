e6ce9324ee6ce0c5c2df9cf229188cbbebff959f
paulmeyer <paulmeyer@chromium.org>
2015-06-17 11:04:22 -0700

    Revert of Revert of Abstract //content-level dependencies out of InMemoryURLIndex (patchset #1 id:1 of https://codereview.chromium.org/1184923006/)
    Reason for revert:
    This revert broke other things. Reverting the revert.
    
    Original issue's description:
    > Revert of Abstract //content-level dependencies out of InMemoryURLIndex (patchset #5 id:100001 of https://codereview.chromium.org/1186103002/)
    >
    > Reason for revert:
    > Speculative revert to fix the Windows unit_test failures.
    >
    > Original issue's description:
    > > Abstract //content-level dependencies out of InMemoryURLIndex
    > >
    > > It now takes a base::SequencedWorkerPool and a set of client schemes to
    > > whitelist in its constructor.
    > >
    > > BUG=371538
    > >
    > > TBR=phajdan.jr
    > >
    > > Committed: https://crrev.com/a6da40f8abbf44f8dab8db59a425b85c1db44376
    > > Cr-Commit-Position: refs/heads/master@{#334799}
    >
    > TBR=pkasting@chromium.org,phajdan.jr@chromium.org,blundell@chromium.org
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=371538
    >
    > Committed: https://crrev.com/9d9c3c5655eff730578dc1e9524139ccd60fdb98
    > Cr-Commit-Position: refs/heads/master@{#334866}
    
    TBR=pkasting@chromium.org,phajdan.jr@chromium.org,blundell@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=371538
    
    Review URL: https://codereview.chromium.org/1181353003
    
    Cr-Commit-Position: refs/heads/master@{#334872}

--- chrome/browser/autocomplete/in_memory_url_index.h
+++ chrome/browser/autocomplete/in_memory_url_index.h
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
 #include "base/task/cancelable_task_tracker.h"
+#include "base/threading/sequenced_worker_pool.h"
+#include "base/threading/thread_checker.h"
 #include "components/history/core/browser/history_db_task.h"
 #include "components/history/core/browser/history_service_observer.h"
 #include "components/history/core/browser/history_types.h"
@@ -46,6 +48,8 @@ class HistoryService;
 }
 
 class URLIndexPrivateData;
+
+typedef std::set<std::string> SchemeSet;
 
 // The URL history source.
 // Holds portions of the URL database in memory in an indexed form.  Used to
@@ -102,8 +106,10 @@ class InMemoryURLIndex : public KeyedService,
   // characters.
   InMemoryURLIndex(bookmarks::BookmarkModel* bookmark_model,
                    history::HistoryService* history_service,
+                   base::SequencedWorkerPool* worker_pool,
                    const base::FilePath& history_dir,
-                   const std::string& languages);
+                   const std::string& languages,
+                   const SchemeSet& client_schemes_to_whitelist);
   ~InMemoryURLIndex() override;
 
   // Opens and prepares the index of historical URL visits. If the index private
@@ -153,7 +159,7 @@ class InMemoryURLIndex : public KeyedService,
     explicit RebuildPrivateDataFromHistoryDBTask(
         InMemoryURLIndex* index,
         const std::string& languages,
-        const std::set<std::string>& scheme_whitelist);
+        const SchemeSet& scheme_whitelist);
 
     bool RunOnDBThread(history::HistoryBackend* backend,
                        history::HistoryDatabase* db) override;
@@ -164,7 +170,7 @@ class InMemoryURLIndex : public KeyedService,
 
     InMemoryURLIndex* index_;  // Call back to this index at completion.
     std::string languages_;    // Languages for word-breaking.
-    std::set<std::string> scheme_whitelist_;  // Schemes to be indexed.
+    SchemeSet scheme_whitelist_;  // Schemes to be indexed.
     bool succeeded_;  // Indicates if the rebuild was successful.
     scoped_refptr<URLIndexPrivateData> data_;  // The rebuilt private data.
 
@@ -263,7 +269,7 @@ class InMemoryURLIndex : public KeyedService,
   }
 
   // Returns the set of whitelisted schemes. For unit testing only.
-  const std::set<std::string>& scheme_whitelist() { return scheme_whitelist_; }
+  const SchemeSet& scheme_whitelist() { return scheme_whitelist_; }
 
   // The BookmarkModel; may be null when testing.
   bookmarks::BookmarkModel* bookmark_model_;
@@ -280,7 +286,7 @@ class InMemoryURLIndex : public KeyedService,
   std::string languages_;
 
   // Only URLs with a whitelisted scheme are indexed.
-  std::set<std::string> scheme_whitelist_;
+  SchemeSet scheme_whitelist_;
 
   // The index's durable private data.
   scoped_refptr<URLIndexPrivateData> private_data_;
@@ -311,6 +317,8 @@ class InMemoryURLIndex : public KeyedService,
   // This flag is set to true if we want to listen to the
   // HistoryServiceLoaded Notification.
   bool listen_to_history_service_loaded_;
+
+  base::ThreadChecker thread_checker_;
 
   DISALLOW_COPY_AND_ASSIGN(InMemoryURLIndex);
 };
--- chrome/browser/autocomplete/in_memory_url_index_factory.cc
+++ chrome/browser/autocomplete/in_memory_url_index_factory.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/keyed_service/core/service_access_type.h"
+#include "content/public/browser/browser_thread.h"
+#include "content/public/common/url_constants.h"
 
 // static
 InMemoryURLIndex* InMemoryURLIndexFactory::GetForProfile(Profile* profile) {
@@ -42,12 +44,15 @@ KeyedService* InMemoryURLIndexFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
   // Do not force creation of the HistoryService if saving history is disabled.
   Profile* profile = Profile::FromBrowserContext(context);
+  SchemeSet chrome_schemes_to_whitelist;
+  chrome_schemes_to_whitelist.insert(content::kChromeUIScheme);
   InMemoryURLIndex* in_memory_url_index = new InMemoryURLIndex(
       BookmarkModelFactory::GetForProfile(profile),
       HistoryServiceFactory::GetForProfile(profile,
                                            ServiceAccessType::IMPLICIT_ACCESS),
-      profile->GetPath(),
-      profile->GetPrefs()->GetString(prefs::kAcceptLanguages));
+      content::BrowserThread::GetBlockingPool(), profile->GetPath(),
+      profile->GetPrefs()->GetString(prefs::kAcceptLanguages),
+      chrome_schemes_to_whitelist);
   in_memory_url_index->Init();
   return in_memory_url_index;
 }
