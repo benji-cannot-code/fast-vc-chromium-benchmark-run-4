7dc5d5e46ca736d31d83bda74c73fc60f5dceffe
yoichio@chromium.org <yoichio@chromium.org>
2015-06-17 13:50:30 +0000

    HTML Imports: Teach preloader about HTML Imports.
    This change tells HTMLPreloadScanner about <link rel=import> so that
    it can preload the resource for the import.
    This patch let import load not blocked by previous scripts.
    
    This is a revised patch. The original was written by morrita@ but reverted:
    https://codereview.chromium.org/243793006
    
    LayoutTests/http/tests/htmlimports/import-async.html,
    LayoutTests/http/tests/resources/redirect.php:
    - This test confirms <link rel=import async> doesn't block next script but this 
    patch
     let import load not blocked by previous script. To clarify such two cases, delayed 
    loading.
    
    LayoutTests/http/tests/htmlimports/import-cors-credentials.html,
    LayoutTests/http/tests/htmlimports/resources/import-cors-credentials-body.html:
    - Similar to previous case. Load the cookie surely before loading.
    
    LayoutTests/http/tests/htmlimports/import-preload.html,
    LayoutTests/http/tests/htmlimports/resources/hello.css,
    LayoutTests/http/tests/htmlimports/resources/hello.js,
    LayoutTests/http/tests/htmlimports/resources/preload.html:
    - This is a main test. While there is slow script before import, we prefetch 
    resources.
    
    Source/core/fetch/ResourceFetcher.cpp:
    - Let ResourceFetcher::preload load import resource.
    
    Source/core/html/parser/HTMLPreloadScanner.cpp:
    - Let HTMLPreloadScanner::shouldPreload return true if import.
    
    Source/core/html/parser/PreloadRequest.cpp:
    - Set CORS enabled.
    
    Source/core/testing/Internals*:
    - Add the helper isPreloadedby(url, document) function generalized of
     isPreloaded(url).
    
    BUG=323096
    
    Review URL: https://codereview.chromium.org/976263002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197260 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/resources/redirect.php
+++ third_party/WebKit/LayoutTests/http/tests/resources/redirect.php
@@ -1,5 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
+    $sleep = $_GET['delay'];
+    if (isset($sleep))
+        usleep($sleep*1000);
     $url = $_GET['url'];
     $refresh = $_GET['refresh'];
 
