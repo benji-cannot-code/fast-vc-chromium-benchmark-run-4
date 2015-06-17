330db29159abca7aaf5c61161a1cbcfbfcebd8e0
achaulk <achaulk@chromium.org>
2015-06-17 14:41:37 -0700

    Process the recently added SWAP_NAK_RECREATE_BUFFERS swap result
    We discard any buffers that are not in use and replace old buffers with
    new ones and copy the data. This should happen very infrequently (currently
    only when moving a window to/from a UDL display) so the performance cost
    shouldn't be a problem
    
    BUG=476966
    
    Review URL: https://codereview.chromium.org/1177203003
    
    Cr-Commit-Position: refs/heads/master@{#334930}

--- content/browser/compositor/buffer_queue.h
+++ content/browser/compositor/buffer_queue.h
@@ -44,6 +44,8 @@ class CONTENT_EXPORT BufferQueue {
   void PageFlipComplete();
   void Reshape(const gfx::Size& size, float scale_factor);
 
+  void RecreateBuffers();
+
   unsigned int current_texture_id() const { return current_surface_.texture; }
   unsigned int fbo() const { return fbo_; }
 
@@ -76,6 +78,8 @@ class CONTENT_EXPORT BufferQueue {
 
   // Return a surface, available to be drawn into.
   AllocatedSurface GetNextSurface();
+
+  AllocatedSurface RecreateBuffer(AllocatedSurface* surface);
 
   gfx::Size size_;
   scoped_refptr<cc::ContextProvider> context_provider_;
