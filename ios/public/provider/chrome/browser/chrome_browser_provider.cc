beea63ee824042eb5df4c61662b4ff03a08d6c9c
sdefresne <sdefresne@chromium.org>
2015-06-17 08:08:13 -0700

    Remove access to KeyedServiceProvider via ChromeBrowserProvider
    Since both objects have different lifetime and the KeyedServiceProvider is
    accessible directly, remove the access via the ChromeBrowserProvider.
    
    BUG=478763
    
    Review URL: https://codereview.chromium.org/1190973002
    
    Cr-Commit-Position: refs/heads/master@{#334829}

--- ios/public/provider/chrome/browser/chrome_browser_provider.cc
+++ ios/public/provider/chrome/browser/chrome_browser_provider.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 
 #include "base/logging.h"
-#include "ios/public/provider/chrome/browser/keyed_service_provider.h"
 
 namespace ios {
 
@@ -88,10 +87,6 @@ std::string ChromeBrowserProvider::GetProductVersionWithPrefix(
     const std::string& prefix) {
   NOTREACHED();
   return std::string();
-}
-
-KeyedServiceProvider* ChromeBrowserProvider::GetKeyedServiceProvider() {
-  return ::ios::GetKeyedServiceProvider();
 }
 
 }  // namespace ios
--- ios/public/provider/chrome/browser/chrome_browser_provider.h
+++ ios/public/provider/chrome/browser/chrome_browser_provider.h
@@ -38,7 +38,6 @@ namespace ios {
 
 class ChromeBrowserProvider;
 class GeolocationUpdaterProvider;
-class KeyedServiceProvider;
 class StringProvider;
 class UpdatableResourceProvider;
 
@@ -86,8 +85,6 @@ class ChromeBrowserProvider {
   virtual std::string GetRiskData();
   // Returns product version with prefix.
   virtual std::string GetProductVersionWithPrefix(const std::string& prefix);
-  // Returns an instance of KeyedServiceProvider.
-  virtual KeyedServiceProvider* GetKeyedServiceProvider();
 };
 
 }  // namespace ios
