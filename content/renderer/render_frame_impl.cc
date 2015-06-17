848989bce5a1aca429d746515b7f312e33c399c2
alexmos <alexmos@chromium.org>
2015-06-16 17:04:49 -0700

    OOPIF: use replicated origins on frames that may be remote.
    A few places in the code access the top frame's origin via the
    document().  This will crash in --site-per-process mode if the top
    frame is remote.  This CL switches these places to use
    WebFrame::securityOrigin, which keeps the current behavior for local
    frames and use replicated origins for remote frames.
    
    BUG=426512
    
    Review URL: https://codereview.chromium.org/1188883002
    
    Cr-Commit-Position: refs/heads/master@{#334756}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -3641,7 +3641,7 @@ bool RenderFrameImpl::allowWebGL(blink::WebLocalFrame* frame,
   bool blocked = true;
   render_view_->Send(new ViewHostMsg_Are3DAPIsBlocked(
       render_view_->GetRoutingID(),
-      GURL(frame->top()->document().securityOrigin().toString()),
+      GURL(frame->top()->securityOrigin().toString()),
       THREE_D_API_TYPE_WEBGL,
       &blocked));
   return !blocked;
@@ -3651,7 +3651,7 @@ void RenderFrameImpl::didLoseWebGLContext(blink::WebLocalFrame* frame,
                                           int arb_robustness_status_code) {
   DCHECK(!frame_ || frame_ == frame);
   render_view_->Send(new ViewHostMsg_DidLose3DContext(
-      GURL(frame->top()->document().securityOrigin().toString()),
+      GURL(frame->top()->securityOrigin().toString()),
       THREE_D_API_TYPE_WEBGL,
       arb_robustness_status_code));
 }
