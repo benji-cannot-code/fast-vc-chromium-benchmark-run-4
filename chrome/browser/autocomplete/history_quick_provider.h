e28ec131dba92849b6979508f8b13449a63bdc70
blundell <blundell@chromium.org>
2015-06-17 04:21:11 -0700

    Prepare HistoryQuickProvider for componentization
    - //content- and //chrome-level dependencies are abstracted via the client.
    
    - The crash key for crbug.com/464926 is changed to be defined internally at the
      two usage points to break the dependency of history_quick_provider.cc on
      //chrome/common/crash_keys.h. Note that the proper way to do this would be
      to create an omnibox_crash_keys.{h,cc} file defining this constant and have
      //chrome/common/crash_keys.cc depend on that. However, that would require
      dividing //components/omnibox into browser/ and common/ subdirectories so
      that //chrome/common could depend on //components/omnibox/common.  As (a) at
      some point crbug.com/464926 will be closed and this code will be removed, and
      (b) there is no other omnibox-related code that will need to be referenced by
      //chrome/common in the future, doing a big restructuring of
      //components/omnibox would just be churn for very little gain.
    
    BUG=371538
    
    Review URL: https://codereview.chromium.org/1183773006
    
    Cr-Commit-Position: refs/heads/master@{#334809}

--- chrome/browser/autocomplete/history_quick_provider.h
+++ chrome/browser/autocomplete/history_quick_provider.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/autocomplete_match.h"
 #include "components/omnibox/history_provider.h"
 
-class Profile;
 struct ScoredHistoryMatch;
 
 // This class is an autocomplete provider (a pseudo-internal component of
@@ -26,7 +25,6 @@ struct ScoredHistoryMatch;
 class HistoryQuickProvider : public HistoryProvider {
  public:
   HistoryQuickProvider(AutocompleteProviderClient* client,
-                       Profile* profile,
                        InMemoryURLIndex* in_memory_url_index);
 
   // AutocompleteProvider. |minimal_changes| is ignored since there is no asynch
@@ -55,7 +53,6 @@ class HistoryQuickProvider : public HistoryProvider {
   AutocompleteMatch QuickMatchToACMatch(const ScoredHistoryMatch& history_match,
                                         int score);
 
-  Profile* profile_;
   AutocompleteInput autocomplete_input_;
   std::string languages_;
   InMemoryURLIndex* in_memory_url_index_;  // Not owned by this class.
