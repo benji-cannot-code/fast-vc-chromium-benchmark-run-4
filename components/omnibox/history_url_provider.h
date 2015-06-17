625e589225e913e28657f2a47c2a333c2a5833cd
blundell <blundell@chromium.org>
2015-06-16 23:49:03 -0700

    Componentize HistoryURLProvider/ScoredHistoryMatch.
    No changes are needed.
    
    BUG=371538
    
    Review URL: https://codereview.chromium.org/1181113005
    
    Cr-Commit-Position: refs/heads/master@{#334780}

--- components/omnibox/history_url_provider.h
+++ components/omnibox/history_url_provider.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_AUTOCOMPLETE_HISTORY_URL_PROVIDER_H_
-#define CHROME_BROWSER_AUTOCOMPLETE_HISTORY_URL_PROVIDER_H_
+#ifndef COMPONENTS_OMNIBOX_HISTORY_URL_PROVIDER_H_
+#define COMPONENTS_OMNIBOX_HISTORY_URL_PROVIDER_H_
 
 #include <string>
 #include <vector>
@@ -342,4 +342,4 @@ class HistoryURLProvider : public HistoryProvider {
   DISALLOW_COPY_AND_ASSIGN(HistoryURLProvider);
 };
 
-#endif  // CHROME_BROWSER_AUTOCOMPLETE_HISTORY_URL_PROVIDER_H_
+#endif  // COMPONENTS_OMNIBOX_HISTORY_URL_PROVIDER_H_
