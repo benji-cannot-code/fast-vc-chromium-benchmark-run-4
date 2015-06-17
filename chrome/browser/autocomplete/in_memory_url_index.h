b3a06ab13c0a19c8cd0ee46860a38ed5c43c00c0
paulmeyer <paulmeyer@chromium.org>
2015-06-17 10:59:59 -0700

    Revert of Revert of Componentize HistoryURLProvider/ScoredHistoryMatch. (patchset #1 id:1 of https://codereview.chromium.org/1190783006/)
    Reason for revert:
    This revert broke other things. Reverting the revert.
    
    Original issue's description:
    > Revert of Componentize HistoryURLProvider/ScoredHistoryMatch. (patchset #3 id:60001 of https://codereview.chromium.org/1181113005/)
    >
    > Reason for revert:
    > Speculative revert to fix the Windows unit_test failures.
    >
    > Original issue's description:
    > > Componentize HistoryURLProvider/ScoredHistoryMatch.
    > >
    > > No changes are needed.
    > >
    > > BUG=371538
    > >
    > > Committed: https://crrev.com/66c202106ea7ab3f3c078a5edfb4abf0d23cee94
    > > Cr-Commit-Position: refs/heads/master@{#334780}
    >
    > TBR=pkasting@chromium.org,blundell@chromium.org
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=371538
    >
    > Committed: https://crrev.com/1b7219bbfc7470b6e48d008fa5e55e29e7f7898e
    > Cr-Commit-Position: refs/heads/master@{#334868}
    
    TBR=pkasting@chromium.org,blundell@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=371538
    
    Review URL: https://codereview.chromium.org/1191953004
    
    Cr-Commit-Position: refs/heads/master@{#334871}

--- chrome/browser/autocomplete/in_memory_url_index.h
+++ chrome/browser/autocomplete/in_memory_url_index.h
@@ -19,11 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
 #include "base/task/cancelable_task_tracker.h"
-#include "chrome/browser/autocomplete/scored_history_match.h"
 #include "components/history/core/browser/history_db_task.h"
 #include "components/history/core/browser/history_service_observer.h"
 #include "components/history/core/browser/history_types.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/omnibox/scored_history_match.h"
 
 class HistoryQuickProviderTest;
 
