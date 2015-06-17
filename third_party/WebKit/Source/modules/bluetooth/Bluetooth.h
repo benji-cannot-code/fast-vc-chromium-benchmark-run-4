17c6456d4cc9c111a1e4c242b43e9f9865c66a32
jyasskin@chromium.org <jyasskin@chromium.org>
2015-06-17 01:09:39 +0000

    Implement the Blink side of RequestDeviceOptions.
    Except for the tests that require an update to the fake bluetooth adapters.
    
    This is the first of three patches to implement RequestDeviceOptions:
    
      [1] This patch.
      [2] https://crrev.com/1172853004 (Chromium side)
      [3] https://crrev.com/1174843002 (Blink tests)
    
    BUG=440594
    
    Review URL: https://codereview.chromium.org/1182973002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197221 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/bluetooth/Bluetooth.h
+++ third_party/WebKit/Source/modules/bluetooth/Bluetooth.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class BluetoothUUIDs;
+class RequestDeviceOptions;
 class ScriptPromise;
 class ScriptState;
 
@@ -27,7 +28,7 @@ public:
     }
 
     // BluetoothDiscovery interface
-    ScriptPromise requestDevice(ScriptState*);
+    ScriptPromise requestDevice(ScriptState*, const RequestDeviceOptions&);
 
     DEFINE_INLINE_TRACE() { }
 
