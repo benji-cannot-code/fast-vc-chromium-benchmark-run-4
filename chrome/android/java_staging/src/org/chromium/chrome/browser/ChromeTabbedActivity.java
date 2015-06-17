1fcf39f58c331b3e778ea390f30d7f1640c66a40
yusufo <yusufo@chromium.org>
2015-06-17 14:34:27 -0700

    Move functionality from ChromeActivity to AsyncInitializationActivity
    Move bits that are not Chrome specific and might be useful in a general context.
    - Deferred startup logic
    - Orientation change logic
    - Last user interaction
    - isTablet
    
    Review URL: https://codereview.chromium.org/1186033003
    
    Cr-Commit-Position: refs/heads/master@{#334927}

--- chrome/android/java_staging/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java_staging/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -462,7 +462,7 @@ public class ChromeTabbedActivity extends CompositorChromeActivity implements Ac
 
             removeWindowBackground();
 
-            if (mIsTablet) {
+            if (isTablet()) {
                 EmptyBackgroundViewWrapper bgViewWrapper = new EmptyBackgroundViewWrapper(
                         getTabModelSelector(), getTabCreator(false), ChromeTabbedActivity.this,
                         mAppMenuHandler, mLayoutManager);
@@ -601,7 +601,7 @@ public class ChromeTabbedActivity extends CompositorChromeActivity implements Ac
             mLayoutManager.setEnableAnimations(
                     DeviceClassManager.enableAnimations(getApplicationContext()));
         }
-        if (mIsTablet) {
+        if (isTablet()) {
             if (getCompositorViewHolder() != null) {
                 getCompositorViewHolder().onAccessibilityStatusChanged(enabled);
             }
@@ -920,7 +920,7 @@ public class ChromeTabbedActivity extends CompositorChromeActivity implements Ac
                     fromMenu ? TabLaunchType.FROM_MENU_OR_OVERVIEW : TabLaunchType.FROM_KEYBOARD);
             RecordUserAction.record("MobileMenuNewTab");
             RecordUserAction.record("MobileNewTabOpened");
-            if (mIsTablet && !fromMenu && !launchedTab.isHidden()) {
+            if (isTablet() && !fromMenu && !launchedTab.isHidden()) {
                 mToolbarHelper.setUrlBarFocus(true);
             }
         } else if (id == R.id.new_incognito_tab_menu_id) {
@@ -933,7 +933,7 @@ public class ChromeTabbedActivity extends CompositorChromeActivity implements Ac
                         UrlConstants.NTP_URL,
                         fromMenu ? TabLaunchType.FROM_MENU_OR_OVERVIEW
                                 : TabLaunchType.FROM_KEYBOARD);
-                if (mIsTablet && !fromMenu && !launchedTab.isHidden()) {
+                if (isTablet() && !fromMenu && !launchedTab.isHidden()) {
                     mToolbarHelper.setUrlBarFocus(true);
                 }
             }
@@ -979,7 +979,7 @@ public class ChromeTabbedActivity extends CompositorChromeActivity implements Ac
             showMenu();
         } else if (id == R.id.focus_url_bar) {
             boolean isUrlBarVisible = !mLayoutManager.overviewVisible()
-                    && (!mIsTablet || getCurrentTabModel().getCount() != 0);
+                    && (!isTablet() || getCurrentTabModel().getCount() != 0);
             if (isUrlBarVisible) {
                 mToolbarHelper.setUrlBarFocus(true);
             }
@@ -1006,7 +1006,7 @@ public class ChromeTabbedActivity extends CompositorChromeActivity implements Ac
         }
 
         // If we are in overview mode and not a tablet, then leave overview mode on back.
-        if (mLayoutManager.overviewVisible() && !mIsTablet) {
+        if (mLayoutManager.overviewVisible() && !isTablet()) {
             mLayoutManager.hideOverview(true);
             // TODO(benm): Record any user metrics in this case?
             return true;
@@ -1181,7 +1181,7 @@ public class ChromeTabbedActivity extends CompositorChromeActivity implements Ac
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (!mUIInitialized) return false;
         boolean isCurrentTabVisible = !mLayoutManager.overviewVisible()
-                && (!mIsTablet || getCurrentTabModel().getCount() != 0);
+                && (!isTablet() || getCurrentTabModel().getCount() != 0);
         return KeyboardShortcuts.onKeyDown(event, this, isCurrentTabVisible, true)
                 || super.onKeyDown(keyCode, event);
     }
@@ -1266,7 +1266,7 @@ public class ChromeTabbedActivity extends CompositorChromeActivity implements Ac
         if (!mUIInitialized) return false;
 
         // Do not show the menu if we are in find in page view.
-        if (mFindToolbarManager != null && mFindToolbarManager.isShowing() && !mIsTablet) {
+        if (mFindToolbarManager != null && mFindToolbarManager.isShowing() && !isTablet()) {
             return false;
         }
 
