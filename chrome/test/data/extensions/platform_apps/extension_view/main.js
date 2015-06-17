1fba4a041e8fa48f2b2e84c28d1895c13b772717
apacible <apacible@chromium.org>
2015-06-17 05:35:35 -0700

    extensionview: Replace connect API function with load.
    This makes a change to the <extensionview>'s API. The connect call will be replaced with load, which takes in a full URL including the extension scheme, id, and page.
    
    A load call will return a Promise object, which will indicate whether or not loading that extension url into the extensionview was successful.
    
    These changes also include:
    - Queue load calls to perform them in order.
    - Add two loadSrc and parseSrc ExtensionViewInternal calls; remove navigate.
    - Make both extension and src attributes read only.
    - Updated browser tests. Per offline discussion, the embedder is declared in each test directory.
    
    BUG=500616
    
    Review URL: https://codereview.chromium.org/1184543003
    
    Cr-Commit-Position: refs/heads/master@{#334816}

--- chrome/test/data/extensions/platform_apps/extension_view/main.js
+++ chrome/test/data/extensions/platform_apps/extension_view/main.js
@@ -3,30 +3,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var embedder = {};
+var LOG = function(msg) {
+  window.console.log(msg);
+};
 
+var embedder = {};
 embedder.setUp_ = function(config) {
-  if (!config || !config.testServer) {
+  if (!config || !config.testServer)
     return;
-  }
-  embedder.baseGuestURL = 'http://localhost:' + config.testServer.port;
-  embedder.emptyGuestURL = embedder.baseGuestURL +
-      '/extensions/platform_apps/web_view/shim/empty_guest.html';
 };
 
 window.runTest = function(testName, appToEmbed) {
   if (!embedder.test.testList[testName]) {
-    window.console.log('Incorrect testName: ' + testName);
+    LOG('Incorrect testName: ' + testName);
     embedder.test.fail();
     return;
   }
 
   // Run the test.
   embedder.test.testList[testName](appToEmbed);
-};
-
-var LOG = function(msg) {
-  window.console.log(msg);
 };
 
 embedder.test = {};
@@ -38,30 +33,23 @@ embedder.test.fail = function() {
   chrome.test.sendMessage('TEST_FAILED');
 };
 
-embedder.test.assertTrue = function(condition) {
-  if (!condition) {
-    console.log('Assertion failed: true != ' + condition);
-    embedder.test.fail();
-  }
-};
-
 embedder.test.assertFalse = function(condition) {
   if (condition) {
-    console.log('Assertion failed: false != ' + condition);
+    LOG('Assertion failed: false != ' + condition);
     embedder.test.fail();
   }
 };
 
 // Tests begin.
 function testExtensionViewCreationShouldSucceed(appToEmbed) {
-  LOG('Checking that there are no instances of <extensionview>.');
+  // Checking that there are no instances of <extensionview>.
   embedder.test.assertFalse(document.querySelector('extensionview'));
   var extensionview = new ExtensionView();
 
-  LOG('Appending new <extensionview> to DOM.');
+  // Appending new <extensionview> to DOM.
   document.body.appendChild(extensionview);
 
-  LOG('Checking that <extensionview> exists.');
+  // Checking that <extensionview> exists.
   if (document.querySelector('extensionview')) {
     embedder.test.succeed();
     return;
@@ -71,7 +59,7 @@ function testExtensionViewCreationShouldSucceed(appToEmbed) {
 
 embedder.test.testList = {
   'testExtensionViewCreationShouldSucceed':
-      testExtensionViewCreationShouldSucceed
+      testExtensionViewCreationShouldSucceed,
 };
 
 onload = function() {
