b612c28a624c5ed03ca0132c862f79ffb765cb63
sadrul <sadrul@chromium.org>
2015-06-17 12:18:04 -0700

    webui: Do not set 'toolkit' property for webui.
    The 'toolkit' property is no longer used by webui pages. So do not set it
    anymore for views. Also, remove associated functions for detecting gtk.
    
    BUG=500004
    
    Review URL: https://codereview.chromium.org/1185363002
    
    Cr-Commit-Position: refs/heads/master@{#334888}

--- chrome/common/extensions/docs/examples/extensions/plugin_settings/domui/js/cr.js
+++ chrome/common/extensions/docs/examples/extensions/plugin_settings/domui/js/cr.js
@@ -30,20 +30,6 @@ const cr = (function() {
   const isLinux = /Linux/.test(navigator.userAgent);
 
   /**
-   * Whether this uses GTK or not.
-   * @type {boolean}
-   */
-  const isGTK = typeof chrome.getVariableValue == 'function' &&
-        /GTK/.test(chrome.getVariableValue('toolkit'));
-
-  /**
-   * Whether this uses the views toolkit or not.
-   * @type {boolean}
-   */
-  const isViews = typeof chrome.getVariableValue == 'function' &&
-        /views/.test(chrome.getVariableValue('toolkit'));
-
-  /**
    * Sets the os and toolkit attributes in the <html> element so that platform
    * specific css rules can be applied.
    */
@@ -56,10 +42,6 @@ const cr = (function() {
       doc.documentElement.setAttribute('os', 'chromeos');
     if (isLinux)
       doc.documentElement.setAttribute('os', 'linux');
-    if (isGTK)
-      doc.documentElement.setAttribute('toolkit', 'gtk');
-    if (isViews)
-      doc.documentElement.setAttribute('toolkit', 'views');
   }
 
   /**
@@ -332,7 +314,6 @@ const cr = (function() {
     isMac: isMac,
     isWindows: isWindows,
     isLinux: isLinux,
-    isViews: isViews,
     enablePlatformSpecificCSSRules: enablePlatformSpecificCSSRules,
     define: define,
     defineProperty: defineProperty,
