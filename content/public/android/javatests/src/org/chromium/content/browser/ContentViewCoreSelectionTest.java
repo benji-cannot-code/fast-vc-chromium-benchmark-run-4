9af53cfc1bb5d25cbbe92686cd42047dbc4978f3
hush <hush@chromium.org>
2015-06-17 12:52:00 -0700

    Test text handles appear in Android WebView popups
    Also fix some possible flakiness for similar tests in content shell.
    BUG=437069
    
    Review URL: https://codereview.chromium.org/759363003
    
    Cr-Commit-Position: refs/heads/master@{#334894}

--- content/public/android/javatests/src/org/chromium/content/browser/ContentViewCoreSelectionTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ContentViewCoreSelectionTest.java
@@ -20,8 +20,6 @@ import org.chromium.content.browser.test.util.CriteriaHelper;
 import org.chromium.content.browser.test.util.DOMUtils;
 import org.chromium.content_shell_apk.ContentShellTestBase;
 
-import java.util.concurrent.Callable;
-
 /**
  * Integration tests for text selection-related behavior.
  */
@@ -549,27 +547,21 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
 
     private void assertClipboardContents(final Context context, final String expectedContents)
             throws InterruptedException {
-        assertTrue(CriteriaHelper.pollForCriteria(new Criteria() {
+        assertTrue(CriteriaHelper.pollForUIThreadCriteria(new Criteria() {
             @Override
             public boolean isSatisfied() {
-                return ThreadUtils.runOnUiThreadBlockingNoException(new Callable<Boolean>() {
-                    @Override
-                    public Boolean call() throws Exception {
-                        ClipboardManager clipboardManager =
-                                (ClipboardManager) context.getSystemService(
-                                        Context.CLIPBOARD_SERVICE);
-                        ClipData clip = clipboardManager.getPrimaryClip();
-                        return clip != null && clip.getItemCount() == 1
-                                && TextUtils.equals(clip.getItemAt(0).getText(), expectedContents);
-                    }
-                });
+                ClipboardManager clipboardManager =
+                        (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = clipboardManager.getPrimaryClip();
+                return clip != null && clip.getItemCount() == 1
+                        && TextUtils.equals(clip.getItemAt(0).getText(), expectedContents);
             }
         }));
     }
 
     private void assertWaitForSelectActionBarVisible(
             final boolean visible) throws InterruptedException {
-        assertTrue(CriteriaHelper.pollForCriteria(new Criteria() {
+        assertTrue(CriteriaHelper.pollForUIThreadCriteria(new Criteria() {
             @Override
             public boolean isSatisfied() {
                 return visible == mContentViewCore.isSelectActionBarShowing();
@@ -624,7 +616,7 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
     }
 
     private void assertWaitForPastePopupStatus(final boolean show) throws InterruptedException {
-        assertTrue(CriteriaHelper.pollForCriteria(new Criteria() {
+        assertTrue(CriteriaHelper.pollForUIThreadCriteria(new Criteria() {
             @Override
             public boolean isSatisfied() {
                 return show == mContentViewCore.getPastePopupForTest().isShowing();
