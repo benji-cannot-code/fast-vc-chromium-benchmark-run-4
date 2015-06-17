47ae19495b788587526eefd36701a6f6ce8361ec
kozyatinskiy@chromium.org <kozyatinskiy@chromium.org>
2015-06-17 19:25:43 +0000

    [DevTools] Added missing EventListenersView addEmptyHolderIfNeeded
    R=lushnikov@chromium.org
    
    Review URL: https://codereview.chromium.org/1187303002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197271 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/components/EventListenersView.js
+++ third_party/WebKit/Source/devtools/front_end/components/EventListenersView.js
@@ -42,7 +42,7 @@ WebInspector.EventListenersView.prototype = {
         {
             this.reset();
             for (var i = 0; i < listeners.length; ++i)
-                this.addObjectEventListeners(objects[i], listeners[i]);
+                this._addObjectEventListeners(objects[i], listeners[i]);
             this.addEmptyHolderIfNeeded();
             this._eventListenersArrivedForTest();
         }
@@ -52,7 +52,7 @@ WebInspector.EventListenersView.prototype = {
      * @param {!WebInspector.RemoteObject} object
      * @param {?Array<!WebInspector.EventListener>} eventListeners
      */
-    addObjectEventListeners: function(object, eventListeners)
+    _addObjectEventListeners: function(object, eventListeners)
     {
         if (!eventListeners)
             return;
--- third_party/WebKit/Source/devtools/front_end/sources/ObjectEventListenersSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/sources/ObjectEventListenersSidebarPane.js
@@ -32,6 +32,7 @@ WebInspector.ObjectEventListenersSidebarPane.prototype = {
         var executionContext = WebInspector.context.flavor(WebInspector.ExecutionContext);
         if (!executionContext) {
             this._eventListenersView.reset();
+            this._eventListenersView.addEmptyHolderIfNeeded();
             return;
         }
         this._lastRequestedContext = executionContext;
