be4a612986790a4e9e7bf0259624adfecd384c71
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2015-06-17 22:42:16 +0000

    Move overflow in InlineTextBox::adjustPosition() if it has glyph overflows
    If the InlineTextBox has glyph overflows, without overflow adjustment,
    the overflow will be inconsistent with the changed position of the
    inline box. Now also move the overflow.
    
    BTW, renamed adjustPosition() to move() to make the meaning clearer. 
    
    BUG=499690
    TEST=fast/repaint/line-flow-with-floats-2.html --enable-slimming-paint
    
    Review URL: https://codereview.chromium.org/1183003003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197290 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutListItem.cpp
+++ third_party/WebKit/Source/core/layout/LayoutListItem.cpp
@@ -341,7 +341,7 @@ void LayoutListItem::positionListMarker()
         if (style()->isLeftToRightDirection()) {
             LayoutUnit leftLineOffset = logicalLeftOffsetForLine(blockOffset, logicalLeftOffsetForLine(blockOffset, false), false);
             markerLogicalLeft = leftLineOffset - lineOffset - paddingStart() - borderStart() + m_marker->marginStart();
-            m_marker->inlineBoxWrapper()->adjustLineDirectionPosition((markerLogicalLeft - markerOldLogicalLeft).toFloat());
+            m_marker->inlineBoxWrapper()->moveInInlineDirection((markerLogicalLeft - markerOldLogicalLeft).toFloat());
             for (InlineFlowBox* box = m_marker->inlineBoxWrapper()->parent(); box; box = box->parent()) {
                 LayoutRect newLogicalVisualOverflowRect = box->logicalVisualOverflowRect(lineTop, lineBottom);
                 LayoutRect newLogicalLayoutOverflowRect = box->logicalLayoutOverflowRect(lineTop, lineBottom);
@@ -364,7 +364,7 @@ void LayoutListItem::positionListMarker()
         } else {
             LayoutUnit rightLineOffset = logicalRightOffsetForLine(blockOffset, logicalRightOffsetForLine(blockOffset, false), false);
             markerLogicalLeft = rightLineOffset - lineOffset + paddingStart() + borderStart() + m_marker->marginEnd();
-            m_marker->inlineBoxWrapper()->adjustLineDirectionPosition((markerLogicalLeft - markerOldLogicalLeft).toFloat());
+            m_marker->inlineBoxWrapper()->moveInInlineDirection((markerLogicalLeft - markerOldLogicalLeft).toFloat());
             for (InlineFlowBox* box = m_marker->inlineBoxWrapper()->parent(); box; box = box->parent()) {
                 LayoutRect newLogicalVisualOverflowRect = box->logicalVisualOverflowRect(lineTop, lineBottom);
                 LayoutRect newLogicalLayoutOverflowRect = box->logicalLayoutOverflowRect(lineTop, lineBottom);
--- third_party/WebKit/Source/core/layout/line/InlineFlowBox.h
+++ third_party/WebKit/Source/core/layout/line/InlineFlowBox.h
@@ -107,7 +107,7 @@ public:
     virtual void deleteLine() override final;
     virtual void extractLine() override final;
     virtual void attachLine() override final;
-    virtual void adjustPosition(LayoutUnit dx, LayoutUnit dy) override;
+    virtual void move(const LayoutSize&) override;
 
     virtual void extractLineBoxFromLayoutObject();
     virtual void attachLineBoxToLayoutObject();
