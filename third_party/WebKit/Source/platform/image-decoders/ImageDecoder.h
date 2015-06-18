1fb7239897c07a431eff682ee1d735c4f4dd9afd
noel@chromium.org <noel@chromium.org>
2015-06-18 06:20:41 +0000

    Remove deferred image decoding code from ImageDecodeBench
    ImageDecodeBench tool does not need or depend on Blink deferred decoding.
    Remove code references to deferred decoding. Minor: fix spelling.
    
    BUG=492961,413479
    
    Review URL: https://codereview.chromium.org/1185933002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197321 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/image-decoders/ImageDecoder.h
+++ third_party/WebKit/Source/platform/image-decoders/ImageDecoder.h
@@ -36,7 +36,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/image-decoders/ImageFrame.h"
 #include "public/platform/Platform.h"
 #include "wtf/Assertions.h"
+#include "wtf/PassOwnPtr.h"
 #include "wtf/RefPtr.h"
+#include "wtf/Threading.h"
 #include "wtf/Vector.h"
 #include "wtf/text/WTFString.h"
 
