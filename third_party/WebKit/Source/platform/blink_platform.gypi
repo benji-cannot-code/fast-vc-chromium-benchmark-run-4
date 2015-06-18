e86f3585668e4adf4c3ff3b5c8cce0ba48467e7f
caseq@chromium.org <caseq@chromium.org>
2015-06-18 11:11:51 +0000

    Paint Profiler: unroll SkPictures
    Perform SkPicure::playback() instead of SkCanvas::drawPicture() in
    all paint profiler intercepting canvas, so that we actually expose
    a step-by-step picture painting instead of one drawPicture() call.
    Otherwise, paint profiler is unusable in slimming paint mode.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1183863005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197340 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/blink_platform.gypi
+++ third_party/WebKit/Source/platform/blink_platform.gypi
@@ -542,6 +542,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'graphics/ImageOrientation.h',
       'graphics/ImageSource.cpp',
       'graphics/ImageSource.h',
+      'graphics/InterceptingCanvas.cpp',
+      'graphics/InterceptingCanvas.h',
       'graphics/LoggingCanvas.cpp',
       'graphics/LoggingCanvas.h',
       'graphics/PaintInvalidationReason.cpp',
--- third_party/WebKit/Source/platform/graphics/InterceptingCanvas.h
+++ third_party/WebKit/Source/platform/graphics/InterceptingCanvas.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define InterceptingCanvas_h
 
 #include "third_party/skia/include/core/SkCanvas.h"
+#include "third_party/skia/include/core/SkPicture.h"
 #include "wtf/Assertions.h"
 #include "wtf/Noncopyable.h"
 
@@ -67,6 +68,8 @@ public:
 protected:
     explicit InterceptingCanvasBase(SkBitmap bitmap) : SkCanvas(bitmap), m_callNestingDepth(0), m_callCount(0) { };
     InterceptingCanvasBase(int width, int height) : SkCanvas(width, height), m_callNestingDepth(0), m_callCount(0) { };
+
+    void unrollDrawPicture(const SkPicture*, const SkMatrix*, const SkPaint*, SkPicture::AbortCallback*);
 
     void onDrawPaint(const SkPaint&) override = 0;
     void onDrawPoints(PointMode, size_t count, const SkPoint pts[], const SkPaint&) override = 0;
@@ -257,8 +260,7 @@ protected:
 
     void onDrawPicture(const SkPicture* picture, const SkMatrix* matrix, const SkPaint* paint) override
     {
-        Interceptor interceptor(this);
-        this->SkCanvas::onDrawPicture(picture, matrix, paint);
+        this->unrollDrawPicture(picture, matrix, paint, nullptr);
     }
 
     void didSetMatrix(const SkMatrix& matrix) override
