8d5a1c296846140663726b5e30455d551a0cbd40
bungeman <bungeman@chromium.org>
2015-06-18 07:37:33 -0700

    Use new Skia header file in test.
    The content of the Skia header SkTypeface_android.h has moved into
    SkFontMgr_android.h since the old name was somewhat misleading, as
    it affected the font management, not the typefaces. The old header
    is now deprecated and will be removed soon, so move to the new one.
    
    Review URL: https://codereview.chromium.org/1181083005
    
    Cr-Commit-Position: refs/heads/master@{#335037}

--- content/shell/app/blink_test_platform_support_android.cc
+++ content/shell/app/blink_test_platform_support_android.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/shell/app/blink_test_platform_support.h"
 
-#include "third_party/skia/include/ports/SkTypeface_android.h"
+#include "third_party/skia/include/ports/SkFontMgr_android.h"
 
 namespace {
 
