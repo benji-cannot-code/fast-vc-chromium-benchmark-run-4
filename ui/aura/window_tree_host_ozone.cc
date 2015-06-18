5c2685377e9a6ab2ed52178105f4682b3398b9d5
sky <sky@chromium.org>
2015-06-18 09:24:25 -0700

    Plumbs through device scale factor when widget is available
    BUG=none
    TEST=none
    R=sadrul@chromium.org
    TBR=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/1184123008
    
    Cr-Commit-Position: refs/heads/master@{#335059}

--- ui/aura/window_tree_host_ozone.cc
+++ ui/aura/window_tree_host_ozone.cc
@@ -102,7 +102,8 @@ void WindowTreeHostOzone::OnLostCapture() {
 }
 
 void WindowTreeHostOzone::OnAcceleratedWidgetAvailable(
-    gfx::AcceleratedWidget widget) {
+    gfx::AcceleratedWidget widget,
+    float device_pixel_ratio) {
   widget_ = widget;
   CreateCompositor(widget_);
 }
