891c1f7b2e2fb356ef0c664559c30432c86b23a5
sigbjornf@opera.com <sigbjornf@opera.com>
2015-06-18 10:56:36 +0000

    Oilpan: add prefinalizer for HTMLMediaElement.
    A prefinalizer is needed for the media element for the case where it is
    finalized in the same GC as its execution context (Document.) The
    interaction between the media element and the Document to synchronize
    load event handling can only safely be done prior to either is swept
    out and finalized.
    
    R=
    BUG=500352
    
    Review URL: https://codereview.chromium.org/1184373006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197339 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLMediaElement.h
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.h
@@ -73,6 +73,7 @@ class VideoTrackList;
 class CORE_EXPORT HTMLMediaElement : public HTMLElement, public WillBeHeapSupplementable<HTMLMediaElement>, public MediaPlayerClient, public ActiveDOMObject {
     DEFINE_WRAPPERTYPEINFO();
     WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(HTMLMediaElement);
+    WILL_BE_USING_PRE_FINALIZER(HTMLMediaElement, dispose);
 public:
     static WebMimeRegistry::SupportsType supportsType(const ContentType&, const String& keySystem = String());
 
@@ -289,6 +290,9 @@ public:
 protected:
     HTMLMediaElement(const QualifiedName&, Document&);
     virtual ~HTMLMediaElement();
+#if ENABLE(OILPAN)
+    void dispose();
+#endif
 
     virtual void parseAttribute(const QualifiedName&, const AtomicString&) override;
     virtual void finishParsingChildren() override final;
