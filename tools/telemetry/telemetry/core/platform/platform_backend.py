9c7f260c22e0a0ecdac03838cb1dd88ad7b8b2fb
aiolos <aiolos@chromium.org>
2015-06-18 16:31:54 -0700

    Add device type name in platform.
    BUG=501924
    
    Review URL: https://codereview.chromium.org/1181873005
    
    Cr-Commit-Position: refs/heads/master@{#335160}

--- tools/telemetry/telemetry/core/platform/platform_backend.py
+++ tools/telemetry/telemetry/core/platform/platform_backend.py
@@ -203,6 +203,9 @@ class PlatformBackend(object):
   def GetCommandLine(self, pid):
     raise NotImplementedError()
 
+  def GetDeviceTypeName(self):
+    raise NotImplementedError()
+
   def GetArchName(self):
     raise NotImplementedError()
 
