9a69c2e6253e4df86daeb4959eaf03438e194795
ricea <ricea@chromium.org>
2015-06-17 22:47:57 -0700

    Include-what-you-use cleanup for http_cache_transaction.*
    Add missing includes to http_cache_transaction.h and
    http_cache_transaction.cc.
    
    Remove includes from http_cache_transaction.cc that duplicate those in
    http_cache_transaction.h.
    
    Remove includes from http_cache_transaction.cc that are no longer used.
    
    BUG=none
    TEST=net_unittests
    
    Review URL: https://codereview.chromium.org/1178013004
    
    Cr-Commit-Position: refs/heads/master@{#334998}

--- net/http/http_cache_transaction.h
+++ net/http/http_cache_transaction.h
@@ -9,18 +9,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_HTTP_HTTP_CACHE_TRANSACTION_H_
 #define NET_HTTP_HTTP_CACHE_TRANSACTION_H_
 
+#include <stddef.h>
+
 #include <string>
 
+#include "base/basictypes.h"
+#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "net/base/completion_callback.h"
+#include "net/base/io_buffer.h"
+#include "net/base/load_states.h"
 #include "net/base/request_priority.h"
+#include "net/base/upload_progress.h"
 #include "net/http/http_cache.h"
 #include "net/http/http_request_headers.h"
 #include "net/http/http_response_headers.h"
 #include "net/http/http_response_info.h"
 #include "net/http/http_transaction.h"
+#include "net/http/partial_data.h"
 #include "net/log/net_log.h"
 #include "net/socket/connection_attempts.h"
+#include "net/websockets/websocket_handshake_stream_base.h"
 
 namespace net {
 
