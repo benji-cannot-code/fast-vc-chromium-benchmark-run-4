2c36df4b9f02f9b343a5e3656e04aa2c8a3c7ae1
xdai <xdai@chromium.org>
2015-06-17 19:36:25 -0700

    Use ApiResourceManager to manage the life time of web cameras.
    Also revert back to the original prototypes of WallpaperPrivateAPI.
    
    BUG=486150,498028
    
    Review URL: https://codereview.chromium.org/1180323002
    
    Cr-Commit-Position: refs/heads/master@{#334983}

--- extensions/browser/api/webcam_private/webcam.h
+++ extensions/browser/api/webcam_private/webcam.h
@@ -10,10 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
+#include "base/memory/ref_counted.h"
+#include "content/public/browser/browser_thread.h"
+#include "extensions/browser/api/api_resource.h"
 
 namespace extensions {
 
-class Webcam {
+class Webcam : public base::RefCounted<Webcam> {
  public:
   enum PanDirection {
     PAN_LEFT,
@@ -28,7 +31,6 @@ class Webcam {
   };
 
   Webcam();
-  virtual ~Webcam();
 
   virtual void Reset(bool pan, bool tilt, bool zoom) = 0;
   virtual bool GetPan(int* value) = 0;
@@ -40,22 +42,35 @@ class Webcam {
   virtual bool SetPanDirection(PanDirection direction) = 0;
   virtual bool SetTiltDirection(TiltDirection direction) = 0;
 
-  void AddExtensionRef(const std::string& extension_id) {
-    extension_refs_.insert(extension_id);
-  }
-
-  void RemoveExtensionRef(const std::string& extension_id) {
-    extension_refs_.erase(extension_id);
-  }
-
-  bool ShouldDelete() {
-    return extension_refs_.empty();
-  }
+ protected:
+  friend class base::RefCounted<Webcam>;
+  virtual ~Webcam();
 
  private:
-  std::set<std::string> extension_refs_;
-
   DISALLOW_COPY_AND_ASSIGN(Webcam);
+};
+
+class WebcamResource : public ApiResource {
+ public:
+  WebcamResource(const std::string& owner_extension_id,
+                 Webcam* webcam,
+                 const std::string& webcam_id);
+  ~WebcamResource() override;
+
+  static const content::BrowserThread::ID kThreadId =
+      content::BrowserThread::UI;
+
+  Webcam* GetWebcam() const;
+  const std::string GetWebcamId() const;
+
+  // ApiResource overrides.
+  bool IsPersistent() const override;
+
+ private:
+  scoped_refptr<Webcam> webcam_;
+  std::string webcam_id_;
+
+  DISALLOW_COPY_AND_ASSIGN(WebcamResource);
 };
 
 }  // namespace extensions
