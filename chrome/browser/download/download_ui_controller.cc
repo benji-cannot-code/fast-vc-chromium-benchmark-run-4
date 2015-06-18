8c09c0841ed758018dd86774a0d7e3756978b852
yoshiki <yoshiki@chromium.org>
2015-06-18 13:06:44 -0700

    Make enable-download-notification flag available on all desktop platform
    This feature is developped only for ChromeOS, but users on other desktop platform may try it.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1182913006
    
    Cr-Commit-Position: refs/heads/master@{#335108}

--- chrome/browser/download/download_ui_controller.cc
+++ chrome/browser/download/download_ui_controller.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/stl_util.h"
 #include "chrome/browser/download/download_item_model.h"
-#include "chrome/browser/download/notification/download_notification_manager.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/common/chrome_switches.h"
@@ -20,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_ANDROID)
 #include "content/public/browser/android/download_controller_android.h"
 #else
+#include "chrome/browser/download/notification/download_notification_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/host_desktop.h"
