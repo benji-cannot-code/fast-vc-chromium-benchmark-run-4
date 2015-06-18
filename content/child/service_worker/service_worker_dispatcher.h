7ac5d90071360dac0a954b20f1705f82f58257b3
nhiroki <nhiroki@chromium.org>
2015-06-18 04:00:20 -0700

    ServiceWorker: Route unregister() through WebServiceWorkerRegistration for refactoring (2)
    This is a part of refactoring CLs for unregister(). See the CL[1] for details.
    
    This CL renames existing Unregister* for "pattern" to DeprecatedUnregister*,
    and adds new Unregister* for "registration_id".
    
    [1] Blink: https://codereview.chromium.org/1185483003
    [2] Chromium: THIS PATCH
    [3] Blink: https://codereview.chromium.org/1190503002
    [4] Chromium: https://codereview.chromium.org/1186803002
    
    BUG=500404
    TEST=compile
    
    Review URL: https://codereview.chromium.org/1181973004
    
    Cr-Commit-Position: refs/heads/master@{#335009}

--- content/child/service_worker/service_worker_dispatcher.h
+++ content/child/service_worker/service_worker_dispatcher.h
@@ -47,8 +47,7 @@ class CONTENT_EXPORT ServiceWorkerDispatcher
  public:
   typedef blink::WebServiceWorkerProvider::WebServiceWorkerRegistrationCallbacks
       WebServiceWorkerRegistrationCallbacks;
-  typedef
-      blink::WebServiceWorkerProvider::WebServiceWorkerUnregistrationCallbacks
+  typedef blink::WebCallbacks<bool, blink::WebServiceWorkerError>
       WebServiceWorkerUnregistrationCallbacks;
   typedef
       blink::WebServiceWorkerProvider::WebServiceWorkerGetRegistrationCallbacks
@@ -75,9 +74,15 @@ class CONTENT_EXPORT ServiceWorkerDispatcher
   // Corresponds to ServiceWorkerRegistration.update().
   void UpdateServiceWorker(int provider_id, int64 registration_id);
   // Corresponds to ServiceWorkerRegistration.unregister().
-  void UnregisterServiceWorker(
+  // TODO(nhiroki): Remove a deprecated version after http://crbug.com/500404 is
+  // fixed.
+  void DeprecatedUnregisterServiceWorker(
       int provider_id,
       const GURL& pattern,
+      WebServiceWorkerUnregistrationCallbacks* callbacks);
+  void UnregisterServiceWorker(
+      int provider_id,
+      int64 registration_id,
       WebServiceWorkerUnregistrationCallbacks* callbacks);
   // Corresponds to navigator.serviceWorker.getRegistration().
   void GetRegistration(
