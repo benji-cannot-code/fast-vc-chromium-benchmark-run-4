ce37f94c6bb289d2078e9bbb2838ff389ad780bc
dgozman@chromium.org <dgozman@chromium.org>
2015-06-18 11:13:03 +0000

    [DevTools] Log failed XHR requests.
    BUG=501058
    
    Review URL: https://codereview.chromium.org/1179903003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197341 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xmlhttprequest/XMLHttpRequest.cpp
+++ third_party/WebKit/Source/core/xmlhttprequest/XMLHttpRequest.cpp
@@ -343,7 +343,6 @@ PassRefPtrWillBeRawPtr<XMLHttpRequest> XMLHttpRequest::create(ExecutionContext* 
 XMLHttpRequest::XMLHttpRequest(ExecutionContext* context, PassRefPtr<SecurityOrigin> securityOrigin)
     : ActiveDOMObject(context)
     , m_timeoutMilliseconds(0)
-    , m_loaderIdentifier(0)
     , m_state(UNSENT)
     , m_lengthDownloadedToFile(0)
     , m_receivedLength(0)
@@ -1149,8 +1148,6 @@ bool XMLHttpRequest::internalAbort()
 
     clearVariablesForLoading();
 
-    InspectorInstrumentation::didFailXHRLoading(executionContext(), this, this);
-
     if (m_responseLegacyStream && m_state != DONE)
         m_responseLegacyStream->abort();
 
@@ -1268,6 +1265,8 @@ void XMLHttpRequest::handleDidCancel()
 void XMLHttpRequest::handleRequestError(ExceptionCode exceptionCode, const AtomicString& type, long long receivedLength, long long expectedLength)
 {
     WTF_LOG(Network, "XMLHttpRequest %p handleRequestError()", this);
+
+    InspectorInstrumentation::didFailXHRLoading(executionContext(), this, this, m_method, m_url);
 
     // The request error steps for event 'type' and exception 'exceptionCode'.
 
@@ -1504,8 +1503,6 @@ void XMLHttpRequest::didFinishLoading(unsigned long identifier, double)
     if (m_state < HEADERS_RECEIVED)
         changeState(HEADERS_RECEIVED);
 
-    m_loaderIdentifier = identifier;
-
     if (m_downloadingToFile && m_responseTypeCode != ResponseTypeBlob && m_lengthDownloadedToFile) {
         ASSERT(m_state == LOADING);
         // In this case, we have sent the request with DownloadToFile true,
@@ -1609,11 +1606,10 @@ void XMLHttpRequest::notifyParserStopped()
 
 void XMLHttpRequest::endLoading()
 {
-    InspectorInstrumentation::didFinishXHRLoading(executionContext(), this, this, m_loaderIdentifier, m_responseText, m_method, m_url);
+    InspectorInstrumentation::didFinishXHRLoading(executionContext(), this, this, m_method, m_url);
 
     if (m_loader)
         m_loader = nullptr;
-    m_loaderIdentifier = 0;
 
     changeState(DONE);
 
@@ -1822,6 +1818,7 @@ void XMLHttpRequest::resume()
 
 void XMLHttpRequest::stop()
 {
+    InspectorInstrumentation::didFailXHRLoading(executionContext(), this, this, m_method, m_url);
     internalAbort();
 }
 
