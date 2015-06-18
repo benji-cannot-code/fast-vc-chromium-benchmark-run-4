ef484bdc96cd71d0954a0e12b56b367b7f671c69
yawano <yawano@chromium.org>
2015-06-17 23:59:40 -0700

    Fix a bug that delete button in toolbar is not available.
    BUG=501661
    TEST=manually tested; Select a file and press delete button in toolbar. Confirm that selected files can be deleted.
    
    Review URL: https://codereview.chromium.org/1177153008
    
    Cr-Commit-Position: refs/heads/master@{#335001}

--- ui/file_manager/file_manager/foreground/js/toolbar_controller.js
+++ ui/file_manager/file_manager/foreground/js/toolbar_controller.js
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @param {!HTMLElement} toolbar Toolbar element which contains controls.
  * @param {!HTMLElement} navigationList Navigation list on the left pane. The
  *     position of silesSelectedLabel depends on the navitaion list's width.
+ * @param {!cr.ui.List} listContainer List container.
  * @param {!LocationLine} locationLine Location line shown on the left side of
  *     the toolbar.
  * @param {!FileSelectionHandler} selectionHandler
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 function ToolbarController(toolbar,
                            navigationList,
+                           listContainer,
                            locationLine,
                            selectionHandler,
                            directoryModel) {
@@ -68,6 +70,12 @@ function ToolbarController(toolbar,
    * @const
    */
   this.navigationList_ = navigationList;
+
+  /**
+   * @private {!cr.ui.List}
+   * @const
+   */
+  this.listContainer_ = listContainer;
 
   /**
    * @private {!LocationLine}
@@ -163,7 +171,7 @@ ToolbarController.prototype.onCancelSelectionButtonClicked_ = function() {
  */
 ToolbarController.prototype.onDeleteButtonClicked_ = function() {
   this.deleteButton_.blur();
-  this.deleteCommand_.execute();
+  this.deleteCommand_.execute(this.listContainer_.currentList);
 }
 
 /**
