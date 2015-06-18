972c2603b3a87b838c5b95d70c17f53e2882e2b0
maxbogue <maxbogue@chromium.org>
2015-06-18 15:42:07 -0700

    [Sync] Add tests for disabled data types on Android.
    BUG=480604
    
    Review URL: https://codereview.chromium.org/1181253004
    
    Cr-Commit-Position: refs/heads/master@{#335148}

--- chrome/android/sync_shell/javatests/src/org/chromium/chrome/browser/sync/AutofillTest.java
+++ chrome/android/sync_shell/javatests/src/org/chromium/chrome/browser/sync/AutofillTest.java
@@ -65,7 +65,7 @@ public class AutofillTest extends SyncTestBase {
     @LargeTest
     @Feature({"Sync"})
     public void testDownloadAutofill() throws Exception {
-        addServerAutofillProfile(STREET, CITY, STATE, ZIP);
+        addServerAutofillData(STREET, CITY, STATE, ZIP);
         assertServerAutofillCountWithName(1, STREET);
         SyncTestUtil.triggerSyncAndWaitForCompletion(mContext);
 
@@ -85,7 +85,7 @@ public class AutofillTest extends SyncTestBase {
     @Feature({"Sync"})
     public void testDownloadDeletedAutofill() throws Exception {
         // Add the entity to test deleting.
-        addServerAutofillProfile(STREET, CITY, STATE, ZIP);
+        addServerAutofillData(STREET, CITY, STATE, ZIP);
         SyncTestUtil.triggerSyncAndWaitForCompletion(mContext);
         assertServerAutofillCountWithName(1, STREET);
         assertClientAutofillCount(1);
@@ -98,7 +98,19 @@ public class AutofillTest extends SyncTestBase {
         waitForClientAutofillCount(0);
     }
 
-    private void addServerAutofillProfile(String street, String city, String state, String zip) {
+    // Test that autofill entries don't get synced if the data type is disabled.
+    @LargeTest
+    @Feature({"Sync"})
+    public void testDisabledNoDownloadAutofill() throws Exception {
+        disableDataType(ModelType.AUTOFILL);
+        addServerAutofillData(STREET, CITY, STATE, ZIP);
+        assertServerAutofillCountWithName(1, STREET);
+        SyncTestUtil.triggerSyncAndWaitForCompletion(mContext);
+        assertClientAutofillCount(0);
+    }
+
+    // TODO(maxbogue): Switch to using specifics.autofill_profile instead.
+    private void addServerAutofillData(String street, String city, String state, String zip) {
         EntitySpecifics specifics = new EntitySpecifics();
         specifics.autofill = new AutofillSpecifics();
         AutofillProfileSpecifics profile = new AutofillProfileSpecifics();
