fdd58d22759e6258dd562dd4f144ecffcf1fd79e
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2015-06-18 01:35:56 +0000

    Change DontCheck under-invalidation checking mode to CacheSkipper
    We have some situation that under-invalidation is difficult to resolve.
    Previously we just set DontCheck under-invalidation checking mode when
    ENABLE(ASSERT) and ignore unmatched CacheDisplayItems. Now we have
    strict checking for unmatched CachedDisplayItems so we need to skip
    cache in all modes.
    
    Might be one of the reasons of bug 497614.
    
    BUG=497614
    
    Review URL: https://codereview.chromium.org/1195513004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197304 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/paint/DisplayItemList.cpp
+++ third_party/WebKit/Source/platform/graphics/paint/DisplayItemList.cpp
@@ -357,11 +357,7 @@ void DisplayItemList::checkCachedDisplayItemIsUnchanged(const DisplayItems::Item
 {
     ASSERT(RuntimeEnabledFeatures::slimmingPaintUnderInvalidationCheckingEnabled());
 
-    if (!DisplayItem::isDrawingType(displayItem.type()) || !clientCacheIsValid(displayItem.client()))
-        return;
-
-    DrawingDisplayItem::UnderInvalidationCheckingMode mode = displayItem.underInvalidationCheckingMode();
-    if (mode == DrawingDisplayItem::DontCheck)
+    if (!DisplayItem::isDrawingType(displayItem.type()) || displayItem.skippedCache() || !clientCacheIsValid(displayItem.client()))
         return;
 
     // If checking under-invalidation, we always generate new display item even if the client is not invalidated.
@@ -383,7 +379,7 @@ void DisplayItemList::checkCachedDisplayItemIsUnchanged(const DisplayItems::Item
     if (!newPicture && !oldPicture)
         return;
     if (newPicture && oldPicture) {
-        switch (mode) {
+        switch (displayItem.underInvalidationCheckingMode()) {
         case DrawingDisplayItem::CheckPicture:
             if (newPicture->approximateOpCount() == oldPicture->approximateOpCount()) {
                 SkDynamicMemoryWStream newPictureSerialized;
