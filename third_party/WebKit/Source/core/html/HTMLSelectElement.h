47b4b10f18bf14080677b88f177d04fcc4fdf339
esprehn@chromium.org <esprehn@chromium.org>
2015-06-19 02:17:00 +0000

    Recalc list items for <select> before updating style for the select.
    listItems() might get called inside recalcStyle for a <select> which can then
    schedule style invalidation sets or leave dirty bits after style recalc.
    
    Instead we should make sure to call recalcListItems() at the start of
    recalcStyle for <select> (in willRecalcStyle) so that all selected
    <option>'s are updated before we try to update their style.
    
    We also add an early out in Element::pseudoStateChanged so that we don't
    schedule any invalidation sets inside recalcStyle which is bad since we'd
    never process them. A future patch will add an ASSERT in the style invalidation
    system to make sure we never do that another way as well.
    
    BUG=497953
    R=ojan@chromium.org
    
    Review URL: https://codereview.chromium.org/1181183004.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197420 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLSelectElement.h
+++ third_party/WebKit/Source/core/html/HTMLSelectElement.h
@@ -135,6 +135,8 @@ protected:
     HTMLSelectElement(Document&, HTMLFormElement*);
 
 private:
+    virtual void willRecalcStyle(StyleRecalcChange) override;
+
     virtual const AtomicString& formControlType() const override;
 
     virtual bool shouldShowFocusRingOnMouseFocus() const override;
