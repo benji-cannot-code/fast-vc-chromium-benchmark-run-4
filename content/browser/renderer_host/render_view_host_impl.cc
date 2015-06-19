78c9f70d06e58f0b4784002563e177bcb4fe4710
wkorman <wkorman@chromium.org>
2015-06-19 16:11:30 -0700

    [SP] Turn off slimming paint by default.
    Partial revert of http://crrev.com/1169193003 while we explore performance
    regressions.
    
    Leave the --disable-slimming-paint switch in existence, but restore
    prior logic around switches.
    
    BUG=497950,501251
    TBR=jochen
    
    Review URL: https://codereview.chromium.org/1186623003
    
    Cr-Commit-Position: refs/heads/master@{#335386}

--- content/browser/renderer_host/render_view_host_impl.cc
+++ content/browser/renderer_host/render_view_host_impl.cc
@@ -469,8 +469,7 @@ WebPreferences RenderViewHostImpl::ComputeWebkitPrefs() {
       !command_line.HasSwitch(switches::kDisableTouchAdjustment);
 
   prefs.slimming_paint_enabled = command_line.HasSwitch(
-      switches::kEnableSlimmingPaint) ||
-      !command_line.HasSwitch(switches::kDisableSlimmingPaint);
+      switches::kEnableSlimmingPaint);
 
 #if defined(OS_MACOSX) || defined(OS_CHROMEOS)
   bool default_enable_scroll_animator = true;
