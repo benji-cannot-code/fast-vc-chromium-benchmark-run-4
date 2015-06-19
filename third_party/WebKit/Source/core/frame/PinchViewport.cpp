1be4e6245f01f84de22f627a40a9516bab9dc042
bokan@chromium.org <bokan@chromium.org>
2015-06-19 14:34:35 +0000

    Fix PinchViewport::visibleRectInDocument
    This method needs to return the visual viewport rect relative to the document
    origin. Introducing the RootFrameViewport in r194957 meant that
    frameView->scrollableArea()->scrollPositionDouble() went from being the offset
    of just the layout viewport to being the visual viewport offset. This method
    wasn't updated at the time so it erroneously moves the returned offset by the
    visual viewport offset.
    
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1178273008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197473 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/PinchViewport.cpp
+++ third_party/WebKit/Source/core/frame/PinchViewport.cpp
@@ -147,9 +147,7 @@ FloatRect PinchViewport::visibleRectInDocument() const
         return FloatRect();
 
     FloatPoint viewLocation = FloatPoint(mainFrame()->view()->scrollableArea()->scrollPositionDouble());
-    FloatRect pinchRect = visibleRect();
-    pinchRect.moveBy(viewLocation);
-    return pinchRect;
+    return FloatRect(viewLocation, visibleSize());
 }
 
 FloatRect PinchViewport::mainViewToViewportCSSPixels(const FloatRect& rect) const
