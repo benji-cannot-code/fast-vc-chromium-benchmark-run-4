fdd58987302f0997db39c47991098db4c957f909
jochen@chromium.org <jochen@chromium.org>
2015-06-19 15:03:48 +0000

    Return null when no promise/reason is given in PromiseRejectionEvent
    BUG=495801
    R=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/1181353005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197475 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/custom/custom.gypi
+++ third_party/WebKit/Source/bindings/core/v8/custom/custom.gypi
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'V8MessageEventCustom.cpp',
             'V8MutationObserverCustom.cpp',
             'V8PopStateEventCustom.cpp',
+            'V8PromiseRejectionEventCustom.cpp',
             'V8WindowCustom.cpp',
             'V8XMLHttpRequestCustom.cpp',
         ],
--- third_party/WebKit/Source/core/events/PromiseRejectionEvent.h
+++ third_party/WebKit/Source/core/events/PromiseRejectionEvent.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/core/v8/ScopedPersistent.h"
 #include "bindings/core/v8/ScriptPromise.h"
+#include "bindings/core/v8/ScriptState.h"
 #include "bindings/core/v8/ScriptValue.h"
 #include "core/CoreExport.h"
 #include "core/events/Event.h"
@@ -22,9 +23,9 @@ public:
     {
         return adoptRefWillBeNoop(new PromiseRejectionEvent);
     }
-    static PassRefPtrWillBeRawPtr<PromiseRejectionEvent> create(const AtomicString& type, const PromiseRejectionEventInit& initializer)
+    static PassRefPtrWillBeRawPtr<PromiseRejectionEvent> create(ScriptState* state, const AtomicString& type, const PromiseRejectionEventInit& initializer)
     {
-        return adoptRefWillBeNoop(new PromiseRejectionEvent(type, initializer));
+        return adoptRefWillBeNoop(new PromiseRejectionEvent(state, type, initializer));
     }
 
     ScriptValue reason(ScriptState*) const;
@@ -36,13 +37,13 @@ public:
 
 private:
     PromiseRejectionEvent();
-    PromiseRejectionEvent(const AtomicString& type, ScriptPromise, ScriptValue reason);
-    PromiseRejectionEvent(const AtomicString&, const PromiseRejectionEventInit&);
+    PromiseRejectionEvent(ScriptState*, const AtomicString&, const PromiseRejectionEventInit&);
     ~PromiseRejectionEvent() override;
 
     static void didCollectPromise(const v8::WeakCallbackInfo<PromiseRejectionEvent>&);
     static void didCollectReason(const v8::WeakCallbackInfo<PromiseRejectionEvent>&);
 
+    RefPtr<ScriptState> m_scriptState;
     ScopedPersistent<v8::Value> m_promise;
     ScopedPersistent<v8::Value> m_reason;
 };
