c50766b981d2a8299afcb08ea931665f8e2b911a
mpearson <mpearson@chromium.org>
2015-06-19 11:28:02 -0700

    Omnibox: Make URL-What-You-Typed Does Not Appear First for non-URL Inputs (unless it's in the user's history)
    This change implements the new behavior and gates it by a field trial.
    
    In the process, it removes the previous change that forced "c#whatever"
    inputs to be parsed as QUERY instead of UNKNOWN.  This is safe to
    remove because the new code should as a consequence have the same
    end result (UWYT does not appear first).
    
    BUG=421514
    
    Review URL: https://codereview.chromium.org/707583004
    
    Cr-Commit-Position: refs/heads/master@{#335301}

--- components/omnibox/autocomplete_input.cc
+++ components/omnibox/autocomplete_input.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "components/metrics/proto/omnibox_event.pb.h"
 #include "components/omnibox/autocomplete_scheme_classifier.h"
+#include "components/omnibox/omnibox_field_trial.h"
 #include "components/url_fixer/url_fixer.h"
 #include "net/base/net_util.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
@@ -402,7 +403,8 @@ metrics::OmniboxInputType::Type AutocompleteInput::Parse(
   // intranet host, and by placing this check late enough that other tests
   // (e.g., for a non-empty TLD or a non-empty scheme) will have already
   // returned URL.
-  if (!parts->path.is_valid() && !canonicalized_url->has_query() &&
+  if (!OmniboxFieldTrial::PreventUWYTDefaultForNonURLInputs() &&
+      !parts->path.is_valid() && !canonicalized_url->has_query() &&
       canonicalized_url->has_ref())
     return metrics::OmniboxInputType::QUERY;
 
