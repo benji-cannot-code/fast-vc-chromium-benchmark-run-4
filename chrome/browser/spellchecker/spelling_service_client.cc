f96b5fcb849bf81968e3aaebeb07a36414c2bf06
rouslan <rouslan@chromium.org>
2015-06-18 19:30:14 -0700

    Handle typographical apostrophe with spelling service client
    Do not mark typographical apostrophe as misspelled, if the server wants
    to replace it with a typewriter apostrophe.
    
    TEST=SpellCheckTest.CreateTextCheckingResults
    TEST=SpellCheckTest.IsValidContraction
    TEST=SpellcheckWordIteratorTest.TypographicalApostropheIsPartOfWord
    TEST=SpellingServiceClientTest.RequestTextCheck
    BUG=165079
    
    Review URL: https://codereview.chromium.org/1092243002
    
    Cr-Commit-Position: refs/heads/master@{#335193}

--- chrome/browser/spellchecker/spelling_service_client.cc
+++ chrome/browser/spellchecker/spelling_service_client.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/spellchecker/spelling_service_client.h"
 
+#include <algorithm>
+
 #include "base/json/json_reader.h"
 #include "base/json/string_escape.h"
 #include "base/logging.h"
@@ -67,8 +69,16 @@ bool SpellingServiceClient::RequestTextCheck(
       &language_code,
       &country_code);
 
+  // Replace typographical apostrophes with typewriter apostrophes, so that
+  // server word breaker behaves correctly.
+  const base::char16 kApostrophe = 0x27;
+  const base::char16 kRightSingleQuotationMark = 0x2019;
+  base::string16 text_copy = text;
+  std::replace(text_copy.begin(), text_copy.end(), kRightSingleQuotationMark,
+               kApostrophe);
+
   // Format the JSON request to be sent to the Spelling service.
-  std::string encoded_text = base::GetQuotedJSONString(text);
+  std::string encoded_text = base::GetQuotedJSONString(text_copy);
 
   static const char kSpellingRequest[] =
       "{"
