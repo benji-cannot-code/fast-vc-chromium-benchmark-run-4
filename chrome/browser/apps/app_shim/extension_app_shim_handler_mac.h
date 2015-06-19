38030ed4e81857c44b7fa3df29c9e09e493effc3
bcwhite <bcwhite@chromium.org>
2015-06-19 07:14:00 -0700

    Show the user manager on Mac when launching an app shortcut for a locked profile.
    Currently, if the profile is locked then the app fails but does not report the status back to the "shim" which hangs around forever until a force-quit is issued.
    
    BUG=490615
    
    Review URL: https://codereview.chromium.org/1182113006
    
    Cr-Commit-Position: refs/heads/master@{#335253}

--- chrome/browser/apps/app_shim/extension_app_shim_handler_mac.h
+++ chrome/browser/apps/app_shim/extension_app_shim_handler_mac.h
@@ -53,6 +53,7 @@ class ExtensionAppShimHandler : public AppShimHandler,
     virtual Profile* ProfileForPath(const base::FilePath& path);
     virtual void LoadProfileAsync(const base::FilePath& path,
                                   base::Callback<void(Profile*)> callback);
+    virtual bool IsProfileLockedForPath(const base::FilePath& path);
 
     virtual extensions::AppWindowRegistry::AppWindowList GetWindows(
         Profile* profile,
@@ -69,6 +70,7 @@ class ExtensionAppShimHandler : public AppShimHandler,
                            const std::vector<base::FilePath>& files);
     virtual void LaunchShim(Profile* profile,
                             const extensions::Extension* extension);
+    virtual void LaunchUserManager();
 
     virtual void MaybeTerminate();
   };
--- chrome/browser/ui/views/profiles/user_manager_view.h
+++ chrome/browser/ui/views/profiles/user_manager_view.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_PROFILES_USER_MANAGER_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_PROFILES_USER_MANAGER_VIEW_H_
 
+#include "base/auto_reset.h"
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_window.h"
@@ -26,6 +27,7 @@ class UserManagerView : public views::DialogDelegateView {
   // Creates a new UserManagerView instance for the |system_profile| and shows
   // the |url|.
   static void OnSystemProfileCreated(scoped_ptr<UserManagerView> instance,
+                                     base::AutoReset<bool>* pending,
                                      Profile* system_profile,
                                      const std::string& url);
 
