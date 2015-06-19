3e172c1beed00f13a602eda3403a1a40c8c99d87
mikecase <mikecase@chromium.org>
2015-06-19 13:43:39 -0700

    Add InstallSplitApk function to device utils.
    Adding a InstallSplitApk function to device utils so that our
    test runner will be able to support install/testing split
    apks.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1166113002
    
    Cr-Commit-Position: refs/heads/master@{#335336}

--- build/android/gyp/util/build_device.py
+++ build/android/gyp/util/build_device.py
@@ -43,6 +43,9 @@ class BuildDevice(object):
   def Install(self, *args, **kwargs):
     return self.device.Install(*args, **kwargs)
 
+  def InstallSplitApk(self, *args, **kwargs):
+    return self.device.InstallSplitApk(*args, **kwargs)
+
   def GetInstallMetadata(self, apk_package):
     """Gets the metadata on the device for the apk_package apk."""
     # Matches lines like:
