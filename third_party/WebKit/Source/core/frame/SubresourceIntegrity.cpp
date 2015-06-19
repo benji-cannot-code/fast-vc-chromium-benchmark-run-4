d3a7abb279ef00c0b9fd4119e941dde0d72be1f3
jww@chromium.org <jww@chromium.org>
2015-06-19 19:01:27 +0000

    Ship Subresource Integrity
    Removes the runtime flag for subresource integrity so it is enabled by
    default. Enables the 'integrity' attribute for HTMLLinkElement and
    HTMLScriptElement where a hash can be specified for the expected
    content. If hash values do not match, fetch will be blocked.
    
    Intent to Ship (and approvals):
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/G3HY0qqTvw8
    
    Spec: https://w3c.github.io/webappsec/specs/subresourceintegrity/
    
    R=mkwst@chromium.org
    BUG=355467
    
    Review URL: https://codereview.chromium.org/1186883003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197494 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/SubresourceIntegrity.cpp
+++ third_party/WebKit/Source/core/frame/SubresourceIntegrity.cpp
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/ConsoleMessage.h"
 #include "platform/Crypto.h"
 #include "platform/ParsingUtilities.h"
-#include "platform/RuntimeEnabledFeatures.h"
 #include "platform/weborigin/KURL.h"
 #include "platform/weborigin/SecurityOrigin.h"
 #include "public/platform/WebCrypto.h"
@@ -106,9 +105,6 @@ HashAlgorithm SubresourceIntegrity::getPrioritizedHashFunction(HashAlgorithm alg
 
 bool SubresourceIntegrity::CheckSubresourceIntegrity(const Element& element, const String& source, const KURL& resourceUrl, const Resource& resource)
 {
-    if (!RuntimeEnabledFeatures::subresourceIntegrityEnabled())
-        return true;
-
     Document& document = element.document();
     String attribute = element.fastGetAttribute(HTMLNames::integrityAttr);
     if (attribute.isEmpty())
