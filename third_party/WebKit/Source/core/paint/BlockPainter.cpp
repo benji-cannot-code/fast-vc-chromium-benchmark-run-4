3040cc38ff3009e76c5e7d7dd5e7060e0a66ed4a
chrishtr@chromium.org <chrishtr@chromium.org>
2015-06-20 01:44:01 +0000

    Allocate a drawing recorder locally to BoxPainter code in the mask phase.
    Previously, call sites were expected to allocate one. This is a buggy pattern,
    since some might forget. In particular, PartPainter did forget, causing the
    referenced crash.
    
    BUG=500426,501485
    
    Review URL: https://codereview.chromium.org/1197843004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197518 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/BlockPainter.cpp
+++ third_party/WebKit/Source/core/paint/BlockPainter.cpp
@@ -161,7 +161,7 @@ void BlockPainter::paintAsInlineBlock(LayoutObject& layoutObject, const PaintInf
 
 void BlockPainter::paintObject(const PaintInfo& paintInfo, const LayoutPoint& paintOffset)
 {
-    PaintPhase paintPhase = paintInfo.phase;
+    const PaintPhase paintPhase = paintInfo.phase;
 
     LayoutRect bounds;
     if (RuntimeEnabledFeatures::slimmingPaintEnabled()) {
@@ -175,9 +175,7 @@ void BlockPainter::paintObject(const PaintInfo& paintInfo, const LayoutPoint& pa
         m_layoutBlock.paintBoxDecorationBackground(paintInfo, paintOffset);
 
     if (paintPhase == PaintPhaseMask && m_layoutBlock.style()->visibility() == VISIBLE) {
-        LayoutObjectDrawingRecorder recorder(*paintInfo.context, m_layoutBlock, paintPhase, bounds);
-        if (!recorder.canUseCachedDrawing())
-            m_layoutBlock.paintMask(paintInfo, paintOffset);
+        m_layoutBlock.paintMask(paintInfo, paintOffset);
         return;
     }
 
