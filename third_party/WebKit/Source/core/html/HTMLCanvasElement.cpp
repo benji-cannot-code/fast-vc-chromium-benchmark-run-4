0c4c25da0493cd882f297331fdc8ff15fd6bf590
chrishtr@chromium.org <chrishtr@chromium.org>
2015-06-21 07:49:56 +0000

    Remove GraphicsContext::drawImageBuffer
    Also assert that getCTM and getTotalMatrix are never called in slimming paint mode.
    
    BUG=382491
    
    Review URL: https://codereview.chromium.org/1180693003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197541 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
@@ -420,7 +420,6 @@ bool HTMLCanvasElement::paintsIntoCanvasBuffer() const
     return true;
 }
 
-
 void HTMLCanvasElement::paint(GraphicsContext* context, const LayoutRect& r)
 {
     // FIXME: crbug.com/438240; there is a bug with the new CSS blending and compositing feature.
@@ -431,8 +430,10 @@ void HTMLCanvasElement::paint(GraphicsContext* context, const LayoutRect& r)
 
     m_context->paintRenderingResultsToCanvas(FrontBuffer);
     if (hasImageBuffer()) {
-        SkXfermode::Mode compositeOperator = !m_context || m_context->hasAlpha() ? SkXfermode::kSrcOver_Mode : SkXfermode::kSrc_Mode;
-        context->drawImageBuffer(buffer(), pixelSnappedIntRect(r), 0, compositeOperator);
+        if (!context->contextDisabled()) {
+            SkXfermode::Mode compositeOperator = !m_context || m_context->hasAlpha() ? SkXfermode::kSrcOver_Mode : SkXfermode::kSrc_Mode;
+            buffer()->draw(context, pixelSnappedIntRect(r), 0, compositeOperator);
+        }
     } else {
         // When alpha is false, we should draw to opaque black.
         if (!m_context->hasAlpha())
