302450596b78230399e8f4c2581af947bcf5d846
alexclarke <alexclarke@chromium.org>
2015-06-22 12:19:08 -0700

    In compositor priority, run loading tasks only when idle
    In compositor priority, if the main thread is on the critical path, only
    run loading tasks in idle periods, assuming there has been an idle task
    in the last 10s.  In addition when loading tasks are enabled, run them
    at normal priority instead of best effort.
    
    This appears to substantially reduce queueing durations for
    smoothness.sync_scroll.pathological_mobile_sites see
    https://codereview.chromium.org/1199843003
    
    BUG=463143
    
    Review URL: https://codereview.chromium.org/1201913002
    
    Cr-Commit-Position: refs/heads/master@{#335541}

--- components/scheduler/renderer/renderer_scheduler_impl_unittest.cc
+++ components/scheduler/renderer/renderer_scheduler_impl_unittest.cc
@@ -592,8 +592,8 @@ TEST_F(RendererSchedulerImplTest, TestCompositorPolicy_CompositorHandlesInput) {
   RunUntilIdle();
   EXPECT_THAT(run_order,
               testing::ElementsAre(std::string("C1"), std::string("C2"),
-                                   std::string("D1"), std::string("D2"),
-                                   std::string("L1"), std::string("I1")));
+                                   std::string("L1"), std::string("D1"),
+                                   std::string("D2"), std::string("I1")));
 }
 
 TEST_F(RendererSchedulerImplTest, TestCompositorPolicy_MainThreadHandlesInput) {
@@ -607,8 +607,8 @@ TEST_F(RendererSchedulerImplTest, TestCompositorPolicy_MainThreadHandlesInput) {
   RunUntilIdle();
   EXPECT_THAT(run_order,
               testing::ElementsAre(std::string("C1"), std::string("C2"),
-                                   std::string("D1"), std::string("D2"),
-                                   std::string("L1"), std::string("I1")));
+                                   std::string("L1"), std::string("D1"),
+                                   std::string("D2"), std::string("I1")));
   scheduler_->DidHandleInputEventOnMainThread(
       FakeInputEvent(blink::WebInputEvent::GestureFlingStart));
 }
@@ -743,8 +743,8 @@ TEST_F(RendererSchedulerImplTest, TestTouchstartPolicy_Compositor) {
   RunUntilIdle();
 
   EXPECT_THAT(run_order,
-              testing::ElementsAre(std::string("T1"), std::string("T2"),
-                                   std::string("L1")));
+              testing::ElementsAre(std::string("L1"), std::string("T1"),
+                                   std::string("T2")));
 }
 
 TEST_F(RendererSchedulerImplTest, TestTouchstartPolicy_MainThread) {
@@ -790,8 +790,8 @@ TEST_F(RendererSchedulerImplTest, TestTouchstartPolicy_MainThread) {
   RunUntilIdle();
 
   EXPECT_THAT(run_order,
-              testing::ElementsAre(std::string("T1"), std::string("T2"),
-                                   std::string("L1")));
+              testing::ElementsAre(std::string("L1"), std::string("T1"),
+                                   std::string("T2")));
 }
 
 TEST_F(RendererSchedulerImplTest, LoadingPriorityPolicy) {
