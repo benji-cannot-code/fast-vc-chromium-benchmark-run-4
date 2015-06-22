10d4fbd788e4c0abfc7e67bcac96d51a5e166431
sacomoto <sacomoto@chromium.org>
2015-06-22 04:14:34 -0700

    Fetching remote devices public keys in ProximityAuthBleSystem.
    This CL modifies the proximity_auth::ProximityAuthBleSystem class to
    fetch the public keys of the devices registered in CryptAuth. It then
    caches the relevant ones.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1198453002
    
    Cr-Commit-Position: refs/heads/master@{#335487}

--- components/proximity_auth/ble/proximity_auth_ble_system.h
+++ components/proximity_auth/ble/proximity_auth_ble_system.h
@@ -6,10 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PROXIMITY_AUTH_BLE_PROXIMITY_AUTH_BLE_SYSTEM_H_
 #define COMPONENTS_PROXIMITY_AUTH_BLE_PROXIMITY_AUTH_BLE_SYSTEM_H_
 
+#include <map>
+#include <string>
+
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/time/time.h"
 #include "components/proximity_auth/connection_observer.h"
+#include "components/proximity_auth/cryptauth/cryptauth_client.h"
 #include "components/proximity_auth/screenlock_bridge.h"
 
 namespace content {
@@ -34,8 +38,10 @@ class ConnectionFinder;
 class ProximityAuthBleSystem : public ScreenlockBridge::Observer,
                                public ConnectionObserver {
  public:
-  ProximityAuthBleSystem(ScreenlockBridge* screenlock_bridge,
-                         content::BrowserContext* browser_context);
+  ProximityAuthBleSystem(
+      ScreenlockBridge* screenlock_bridge,
+      content::BrowserContext* browser_context,
+      scoped_ptr<CryptAuthClientFactory> cryptauth_client_factory);
   ~ProximityAuthBleSystem() override;
 
   // ScreenlockBridge::Observer:
@@ -75,6 +81,13 @@ class ProximityAuthBleSystem : public ScreenlockBridge::Observer,
   virtual ConnectionFinder* CreateConnectionFinder();
 
  private:
+  // Fetches the the public keys of devices that can be used as unlock keys.
+  void GetUnlockKeys();
+
+  // Callbacks for cryptauth::CryptAuthClient::GetMyDevices.
+  void OnGetMyDevices(const cryptauth::GetMyDevicesResponse& response);
+  void OnGetMyDevicesError(const std::string& error);
+
   // Handler for a new connection found event.
   void OnConnectionFound(scoped_ptr<Connection> connection);
 
@@ -89,6 +102,15 @@ class ProximityAuthBleSystem : public ScreenlockBridge::Observer,
 
   content::BrowserContext*
       browser_context_;  // Not owned. Must outlive this object.
+
+  // Creates CryptAuth client instances to make API calls.
+  scoped_ptr<CryptAuthClientFactory> cryptauth_client_factory_;
+
+  // We only support one concurrent API call.
+  scoped_ptr<CryptAuthClient> cryptauth_client_;
+
+  // Maps devices public keys to the device friendly name.
+  std::map<std::string, std::string> unlock_keys_;
 
   scoped_ptr<ConnectionFinder> connection_finder_;
 
