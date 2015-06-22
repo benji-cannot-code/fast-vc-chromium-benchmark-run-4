a2a7bf3305b917ee2f95b0f48f292e7924576405
philipj@opera.com <philipj@opera.com>
2015-06-22 02:58:02 +0000

    Measure the impact of a propsed change to createAttribute()
    There is a proposal to lowercase name in HTML documents:
    https://bugzilla.mozilla.org/show_bug.cgi?id=1165851#c34
    
    The counter in Document::createAttribute will catch any case where the
    change would be observable, providing an upper limit to the risk.
    
    The counter in Element::setAttributeNode is a reasonable proxy for the
    case which is most likely problematic, namely trying to set a camelCase
    Attr created by an HTML document on a non-HTML element, likely SVG, as
    suggested by Boris Zbarsky:
    https://bugzilla.mozilla.org/show_bug.cgi?id=1175031#c5
    
    (The source of the Attr object need not be createAttribute, but extra
    bookkeeping to determine that would either increase sizeof(Attr) or
    require a new NodeFlags entry, so don't do that for now.)
    
    BUG=502301
    
    Review URL: https://codereview.chromium.org/1195583002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197549 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -4482,6 +4482,8 @@ WeakPtrWillBeRawPtr<Document> Document::contextDocument()
 
 PassRefPtrWillBeRawPtr<Attr> Document::createAttribute(const AtomicString& name, ExceptionState& exceptionState)
 {
+    if (isHTMLDocument() && name != name.lower())
+        UseCounter::count(*this, UseCounter::HTMLDocumentCreateAttributeNameNotLowercase);
     return createAttributeNS(nullAtom, name, exceptionState, true);
 }
 
