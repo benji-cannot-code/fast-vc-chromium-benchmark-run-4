db5b44334f793cbfc9291808588afd27b8e86840
bauerb <bauerb@chromium.org>
2015-06-22 16:47:17 -0700

    Don't check for USE_CREDENTIALS permission on Android M.
    BUG=501435
    
    Review URL: https://codereview.chromium.org/1194423002
    
    Cr-Commit-Position: refs/heads/master@{#335585}

--- base/android/java/src/org/chromium/base/BuildInfo.java
+++ base/android/java/src/org/chromium/base/BuildInfo.java
@@ -11,6 +11,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Build;
+import android.text.TextUtils;
 import android.util.Log;
 
 /**
@@ -122,5 +123,14 @@ public class BuildInfo {
     @CalledByNative
     public static int getSdkInt() {
         return Build.VERSION.SDK_INT;
+    }
+
+    /**
+     * @return Whether the Android build is M or later.
+     */
+    public static boolean isMncOrLater() {
+        // TODO(bauerb): Update this once the SDK is updated.
+        return Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1
+                 || TextUtils.equals("MNC", Build.VERSION.CODENAME);
     }
 }
