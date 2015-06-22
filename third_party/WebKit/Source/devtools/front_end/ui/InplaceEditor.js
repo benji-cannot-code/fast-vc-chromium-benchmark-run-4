65b672666fcea6ef9d15dd007620b232180fa2e7
lushnikov@chromium.org <lushnikov@chromium.org>
2015-06-22 18:29:47 +0000

    Revert of DevTools: [Elements] F2 should commit changes in edit-as-html (patchset #1 id:1 of https://codereview.chromium.org/1200833006/)
    Reason for revert:
    Revert as I forgot to address comments
    
    Original issue's description:
    > DevTools: [Elements] F2 should commit changes in edit-as-html
    > 
    > BUG=493091
    > R=pfeldman
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=197588
    
    TBR=pfeldman@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=493091
    
    Review URL: https://codereview.chromium.org/1196343002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197590 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/InplaceEditor.js
+++ third_party/WebKit/Source/devtools/front_end/ui/InplaceEditor.js
@@ -170,10 +170,6 @@ WebInspector.InplaceEditor.prototype = {
             committedCallback(this, self.editorContent(editingContext), editingContext.oldText, context, moveDirection);
         }
 
-        /**
-         * @param {!Event} event
-         * @return {string}
-         */
         function defaultFinishHandler(event)
         {
             var isMetaOrCtrl = WebInspector.isMac() ?
@@ -185,7 +181,6 @@ WebInspector.InplaceEditor.prototype = {
                 return "cancel";
             else if (!isMultiline && event.keyIdentifier === "U+0009") // Tab key
                 return "move-" + (event.shiftKey ? "backward" : "forward");
-            return "";
         }
 
         function handleEditingResult(result, event)
@@ -203,23 +198,16 @@ WebInspector.InplaceEditor.prototype = {
             }
         }
 
-        /**
-         * @param {!Event} event
-         */
         function pasteEventListener(event)
         {
             var result = pasteCallback(event);
             handleEditingResult(result, event);
         }
 
-        /**
-         * @param {!Event} event
-         */
         function keyDownEventListener(event)
         {
-            var result = defaultFinishHandler(event);
-            if (!result && config.postKeydownFinishHandler)
-                result = config.postKeydownFinishHandler(event);
+            var handler = config.customFinishHandler || defaultFinishHandler;
+            var result = handler(event);
             handleEditingResult(result, event);
         }
 
@@ -254,19 +242,22 @@ WebInspector.InplaceEditor.Config = function(commitHandler, cancelHandler, conte
     this.blurHandler = blurHandler;
 
     /**
-     * @type {function(!Event):string|undefined}
+     * Handles the "paste" event, return values are the same as those for customFinishHandler
+     * @type {function(!Element)|undefined}
      */
     this.pasteHandler;
 
     /**
+     * Whether the edited element is multiline
      * @type {boolean|undefined}
      */
     this.multiline;
 
     /**
-     * @type {function(!Event):string|undefined}
+     * Custom finish handler for the editing session (invoked on keydown)
+     * @type {function(!Element,*)|undefined}
      */
-    this.postKeydownFinishHandler;
+    this.customFinishHandler;
 }
 
 WebInspector.InplaceEditor.Config.prototype = {
@@ -292,11 +283,8 @@ WebInspector.InplaceEditor.Config.prototype = {
         this.smartIndent = smartIndent;
     },
 
-    /**
-     * @param {function(!Event):string} postKeydownFinishHandler
-     */
-    setPostKeydownFinishHandler: function(postKeydownFinishHandler)
+    setCustomFinishHandler: function(customFinishHandler)
     {
-        this.postKeydownFinishHandler = postKeydownFinishHandler;
+        this.customFinishHandler = customFinishHandler;
     }
 }
