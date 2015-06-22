105b56efef9932b4d0d07f36002afaeebf923eae
jianli <jianli@chromium.org>
2015-06-22 14:06:22 -0700

    Fix a couple problems related to getting and deleting Instance ID
    Problems fixed:
    1) Add/Remove/Get InstanceID data should be delayed until GCMClient
       is ready
    2) Unregistration request should not be triggered if no token has
       been retrieved
    
    BUG=502385
    TEST=new tests
    NOTRY=TRUE
    
    Review URL: https://codereview.chromium.org/1192753006
    
    Cr-Commit-Position: refs/heads/master@{#335558}

--- components/gcm_driver/fake_gcm_client.h
+++ components/gcm_driver/fake_gcm_client.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_GCM_DRIVER_FAKE_GCM_CLIENT_H_
 #define COMPONENTS_GCM_DRIVER_FAKE_GCM_CLIENT_H_
 
+#include <map>
+
 #include "base/compiler_specific.h"
 #include "base/memory/weak_ptr.h"
 #include "components/gcm_driver/gcm_client.h"
@@ -112,6 +114,7 @@ class FakeGCMClient : public GCMClient {
   StartModeOverridding start_mode_overridding_;
   scoped_refptr<base::SequencedTaskRunner> ui_thread_;
   scoped_refptr<base::SequencedTaskRunner> io_thread_;
+  std::map<std::string, std::pair<std::string, std::string>> instance_id_data_;
   base::WeakPtrFactory<FakeGCMClient> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(FakeGCMClient);
