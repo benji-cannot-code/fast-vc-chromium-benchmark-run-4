ccbbf7bd19e49517bda54d97910120e69beaac59
yosin@chromium.org <yosin@chromium.org>
2015-06-22 11:23:13 +0000

    Templatize  honorEditingBoundaryAtOrBefore in VisiblePosition.cpp
    This patch makes | honorEditingBoundaryAtOrBefore()| to a template function to
    work with both DOM tree positions and composed tree positions for a preparation
    of making |VisbilePosition| to work on both DOM tree and composed tree position.
    
    This patch is result of collaboration work with hajimehoshi@chromium.org
    for selection of web components. After this patch, we are going to
    implement another strategies for the composed tree. We've already
    prepared that at http://crrev.com/1106433002.
    
    BUG=275851
    TEST=n/a; No behavior changes.
    
    Review URL: https://codereview.chromium.org/1198243002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197565 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/VisiblePosition.cpp
+++ third_party/WebKit/Source/core/editing/VisiblePosition.cpp
@@ -445,7 +445,8 @@ VisiblePosition VisiblePosition::right(bool stayInEditableContent) const
     return directionOfEnclosingBlock(right.deepEquivalent()) == LTR ? honorEditingBoundaryAtOrAfter(right) : honorEditingBoundaryAtOrBefore(right);
 }
 
-PositionWithAffinity honorEditingBoundaryAtOrBeforeOf(const PositionWithAffinity& pos, const Position& anchor)
+template <typename PositionWithAffinityType>
+PositionWithAffinityType honorEditingBoundaryAtOrBeforeAlgorithm(const PositionWithAffinityType& pos, const Position& anchor)
 {
     if (pos.isNull())
         return pos;
@@ -454,7 +455,7 @@ PositionWithAffinity honorEditingBoundaryAtOrBeforeOf(const PositionWithAffinity
 
     // Return empty position if pos is not somewhere inside the editable region containing this position
     if (highestRoot && !pos.position().deprecatedNode()->isDescendantOf(highestRoot))
-        return PositionWithAffinity();
+        return PositionWithAffinityType();
 
     // Return pos itself if the two are from the very same editable region, or both are non-editable
     // FIXME: In the non-editable case, just because the new position is non-editable doesn't mean movement
@@ -465,10 +466,15 @@ PositionWithAffinity honorEditingBoundaryAtOrBeforeOf(const PositionWithAffinity
     // Return empty position if this position is non-editable, but pos is editable
     // FIXME: Move to the previous non-editable region.
     if (!highestRoot)
-        return PositionWithAffinity();
+        return PositionWithAffinityType();
 
     // Return the last position before pos that is in the same editable region as this position
     return lastEditablePositionBeforePositionInRoot(pos.position(), highestRoot);
+}
+
+PositionWithAffinity honorEditingBoundaryAtOrBeforeOf(const PositionWithAffinity& pos, const Position& anchor)
+{
+    return honorEditingBoundaryAtOrBeforeAlgorithm(pos, anchor);
 }
 
 VisiblePosition VisiblePosition::honorEditingBoundaryAtOrBefore(const VisiblePosition &pos) const
