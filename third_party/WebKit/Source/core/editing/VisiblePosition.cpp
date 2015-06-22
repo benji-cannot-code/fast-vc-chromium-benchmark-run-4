89c68df02aa5297d7cb89e478c26e6cbea127225
hajimehoshi@chromium.org <hajimehoshi@chromium.org>
2015-06-22 13:10:16 +0000

    Templatize VisiblePosition::init
    As preparation for VisiblePosition for the composed tree, this CL
    templatizes VisiblePosition::init.
    
    This patch is result of collaboration work with yosin@chromium.org
    for selection of web components. We are also commiting other CLs
    to templatize other functions of VisiblePosition (e.g.
    crrev.com/1198243002).
    
    BUG=275851
    TEST=n/a; no behavior change
    
    Review URL: https://codereview.chromium.org/1201573003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197573 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/VisiblePosition.cpp
+++ third_party/WebKit/Source/core/editing/VisiblePosition.cpp
@@ -638,7 +638,8 @@ Position canonicalPositionOf(const Position& position)
     return canonicalPosition(position);
 }
 
-void VisiblePosition::init(const Position& position, EAffinity affinity)
+template<typename PositionType>
+void VisiblePosition::init(const PositionType& position, EAffinity affinity)
 {
     m_affinity = affinity;
 
