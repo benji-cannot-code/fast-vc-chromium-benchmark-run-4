b253699a0c940d3ff520432df7d45d250fd56cc4
robertshield <robertshield@chromium.org>
2015-06-22 06:49:21 -0700

    Add TestBrowserThreadBundle to ChromeBlacklistTrialTest.
    These tests used to post tasks, seemingly without anything to post to.
    
    Tests, how do they work?
    
    BUG=502406
    TEST=unit_tests.exe
    
    Review URL: https://codereview.chromium.org/1199723002
    
    Cr-Commit-Position: refs/heads/master@{#335496}

--- chrome/browser/chrome_elf_init_unittest_win.cc
+++ chrome/browser/chrome_elf_init_unittest_win.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome_elf/chrome_elf_constants.h"
 #include "components/variations/entropy_provider.h"
 #include "components/variations/variations_associated_data.h"
+#include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "version.h"  // NOLINT
 
@@ -56,6 +57,7 @@ class ChromeBlacklistTrialTest : public testing::Test {
 
   scoped_ptr<base::win::RegKey> blacklist_registry_key_;
   registry_util::RegistryOverrideManager override_manager_;
+  content::TestBrowserThreadBundle test_browser_thread_bundle_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ChromeBlacklistTrialTest);
