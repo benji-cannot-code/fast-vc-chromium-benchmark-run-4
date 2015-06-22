2b8e91984ba63033be3e98485da063118ff24172
rob.buis@samsung.com <rob.buis@samsung.com>
2015-06-22 11:42:05 +0000

    Clean up some CSSPropertyID helpers
    Move unresolvedCSSPropertyID/cssPropertyID declarations
    to CSSPropertyNames generated files, and remove the
    CSSParserString overload for unresolvedCSSPropertyID.
    
    BUG=489144
    
    Review URL: https://codereview.chromium.org/1184413005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/build/scripts/make_css_property_names.py
+++ third_party/WebKit/Source/build/scripts/make_css_property_names.py
@@ -56,6 +56,10 @@ inline CSSPropertyID resolveCSSPropertyID(CSSPropertyID id)
 
 inline bool isPropertyAlias(CSSPropertyID id) { return id & 512; }
 
+CSSPropertyID unresolvedCSSPropertyID(const WTF::String&);
+
+CSSPropertyID cssPropertyID(const WTF::String&);
+
 } // namespace blink
 
 namespace WTF {
@@ -160,6 +164,11 @@ String getJSPropertyName(CSSPropertyID id)
     }
     *resultPointer = '\\0';
     return String(result);
+}
+
+CSSPropertyID cssPropertyID(const String& string)
+{
+    return resolveCSSPropertyID(unresolvedCSSPropertyID(string));
 }
 
 } // namespace blink
