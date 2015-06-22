8e531ad46d6ce5dbda9d88f641a2176080dd16dc
primiano@chromium.org <primiano@chromium.org>
2015-06-22 10:18:34 +0000

    Fix an include in OpenTypeSanitizer.cpp
    crrev.com/983973004 introduced a change in OpenTypeSanitizer.cpp
    which uses va_start/end forgetting to includie stdarg.
    The patch landed in Blink but is preventing the roll into chromium
    failing on the following bots:
    
    http://build.chromium.org/p/tryserver.chromium.linux/builders/chromeos_amd64-generic_chromium_compile_only_ng/builds/3422/steps/compile%20%28with%20patch%29/logs/stdio
    http://build.chromium.org/p/tryserver.chromium.linux/builders/chromeos_daisy_chromium_compile_only_ng/builds/3423/steps/compile%20%28with%20patch%29/logs/stdio
    http://build.chromium.org/p/tryserver.chromium.linux/builders/chromeos_amd64-generic_chromium_compile_only_ng/builds/3422/steps/compile%20%28with%20patch%29/logs/stdio
    
    BUG=97467
    
    Review URL: https://codereview.chromium.org/1200803002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197562 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/opentype/OpenTypeSanitizer.cpp
+++ third_party/WebKit/Source/platform/fonts/opentype/OpenTypeSanitizer.cpp
@@ -37,6 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/Platform.h"
 #include "wtf/CurrentTime.h"
 
+#include <stdarg.h>
+
 namespace blink {
 
 static void recordDecodeSpeedHistogram(SharedBuffer* buffer, double decodeTime, size_t decodedSize)
