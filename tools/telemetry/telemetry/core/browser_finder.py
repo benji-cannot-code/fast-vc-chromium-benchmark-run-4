bb906b5cc955123ac6396a5ccc7c47cf1f416eb1
yzshen <yzshen@chromium.org>
2015-06-22 10:56:04 -0700

    Telemetry: add support for Mandoline.
    BUG=478249
    
    Review URL: https://codereview.chromium.org/1173303005
    
    Cr-Commit-Position: refs/heads/master@{#335520}

--- tools/telemetry/telemetry/core/browser_finder.py
+++ tools/telemetry/telemetry/core/browser_finder.py
@@ -12,6 +12,7 @@ from telemetry.core.backends.chrome import android_browser_finder
 from telemetry.core.backends.chrome import cros_browser_finder
 from telemetry.core.backends.chrome import desktop_browser_finder
 from telemetry.core.backends.chrome import ios_browser_finder
+from telemetry.core.backends.mandoline import desktop_mandoline_finder
 from telemetry.core.backends.remote import trybot_browser_finder
 from telemetry.core import browser_finder_exceptions
 from telemetry.core import device_finder
@@ -23,6 +24,7 @@ BROWSER_FINDERS = [
   cros_browser_finder,
   ios_browser_finder,
   trybot_browser_finder,
+  desktop_mandoline_finder,
   ]
 
 
