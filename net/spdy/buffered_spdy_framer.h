d5bbdb99f3b5d929d99f0782704036682fb59235
bnc <bnc@chromium.org>
2015-06-23 14:38:26 -0700

    Parse stream dependency values carried in HTTP/2 headers frame.
    Modify SpdyFramer to parse stream dependency values present in HTTP/2 headers
    frames, and pass them to SpdyFramerVisitorInterface::OnHeaders() via two newly
    added parameters, |parent_stream_id| and |exclusive|.  Modify existing
    implementations of SpdyFramerVisitorInterface::OnHeaders() to accept and ignore
    the new arguments.
    
    This CL lands server change 95217891 by mpw.
    
    BUG=500673
    
    Review URL: https://codereview.chromium.org/1196473003
    
    Cr-Commit-Position: refs/heads/master@{#335754}

--- net/spdy/buffered_spdy_framer.h
+++ net/spdy/buffered_spdy_framer.h
@@ -52,6 +52,8 @@ class NET_EXPORT_PRIVATE BufferedSpdyFramerVisitorInterface {
   virtual void OnHeaders(SpdyStreamId stream_id,
                          bool has_priority,
                          SpdyPriority priority,
+                         SpdyStreamId parent_stream_id,
+                         bool exclusive,
                          bool fin,
                          const SpdyHeaderBlock& headers) = 0;
 
@@ -153,6 +155,8 @@ class NET_EXPORT_PRIVATE BufferedSpdyFramer
   void OnHeaders(SpdyStreamId stream_id,
                  bool has_priority,
                  SpdyPriority priority,
+                 SpdyStreamId parent_stream_id,
+                 bool exclusive,
                  bool fin,
                  bool end) override;
   bool OnControlFrameHeaderData(SpdyStreamId stream_id,
@@ -278,6 +282,8 @@ class NET_EXPORT_PRIVATE BufferedSpdyFramer
     SpdyStreamId promised_stream_id;
     bool has_priority;
     SpdyPriority priority;
+    SpdyStreamId parent_stream_id;
+    bool exclusive;
     uint8 credential_slot;
     bool fin;
     bool unidirectional;
