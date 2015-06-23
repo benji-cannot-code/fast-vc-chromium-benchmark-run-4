9e198f08d6cb9901e46d3da00fbf7356f0710334
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-06-23 11:27:40 -0700

    [Extensions] Add a test that for scripting iframes on tab load complete
    Add a test that confirms you can script iframes of a tab upon receiving the
    'complete' tabs.onUpdated event. It was debated whether or not script injection
    into iframes should work even on tabs.onUpdated 'loading' (which is roughly
    'document_start'), but decided that tabs.executeScript will inject only into
    those frames that are visible at the time executeScript is called, not the time
    the script is executed. This is partially for simplicity, and partially for
    security with activeTab.
    
    Review URL: https://codereview.chromium.org/1192363008
    
    Cr-Commit-Position: refs/heads/master@{#335710}

---
+++ chrome/test/data/extensions/api_test/executescript/subframes_on_load/test.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  // If we want to inject into subframes using execute script, we need to wait
+  // for the tab to finish loading. Otherwise, the frames that would be injected
+  // into don't exist.
+  if (changeInfo.status != 'complete')
+    return;
+
+  chrome.test.runTests([
+    function() {
+      var injectFrameCode = 'chrome.runtime.sendMessage("ping");';
+      var pings = 0;
+      chrome.runtime.onMessage.addListener(
+           function(request, sender, sendResponse) {
+        chrome.test.assertEq(request, "ping");
+        // Wait for two pings - the main frame and the iframe.
+        if (++pings == 2)
+          chrome.test.succeed();
+      });
+      chrome.tabs.executeScript(
+          tabId,
+          {code: injectFrameCode, allFrames: true, runAt: 'document_idle'});
+    }
+  ]);
+});
+
+chrome.test.getConfig(function(config) {
+  var url = ('http://a.com:PORT/extensions/api_test/executescript/' +
+             'subframes_on_load/outer.html').replace(/PORT/,
+                                                     config.testServer.port);
+  chrome.tabs.create({url: url});
+});
