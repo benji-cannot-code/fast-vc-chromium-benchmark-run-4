4a977f9f5cd013306c8303f3209073f92dd68ac9
sigbjornf@opera.com <sigbjornf@opera.com>
2015-06-23 09:46:23 +0000

    Oilpan: eagerly finalize SVGFEImageElement as an ImageResourceClient.
    R=haraken
    BUG=491488
    
    Review URL: https://codereview.chromium.org/1197283006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197638 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGFEImageElement.h
+++ third_party/WebKit/Source/core/svg/SVGFEImageElement.h
@@ -46,6 +46,8 @@ public:
     virtual ~SVGFEImageElement();
     SVGAnimatedPreserveAspectRatio* preserveAspectRatio() { return m_preserveAspectRatio.get(); }
 
+    // Promptly remove as a ImageResource client.
+    EAGERLY_FINALIZE();
     DECLARE_VIRTUAL_TRACE();
 
 private:
