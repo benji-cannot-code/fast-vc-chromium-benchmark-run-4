84ff4d844c0f5be1d358bb8c8a4c8672d2dc6984
tobiasjs <tobiasjs@chromium.org>
2015-06-22 19:56:53 -0700

    Implement self-contained gpu data collection on Android
    By removing the dependency of gpu data collection on
    gfx::GLSurface::InitializeOneOff() (and hence complete iniitalization of
    gl bindings and extensions), the initialization flow can be simplified
    and potentially delayed.
    
    This also fixes the incorrect behaviour of extension blacklisting in
    Android WebViews, where the decision regarding what to blacklist is only
    made after extensions are loaded and made available.
    
    BUG=462553,32629,499928
    
    Review URL: https://codereview.chromium.org/1172003002
    
    Cr-Commit-Position: refs/heads/master@{#335617}

--- ui/gl/egl_util.h
+++ ui/gl/egl_util.h
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GL_EGL_UTIL_H_
 #define UI_GL_EGL_UTIL_H_
 
+#include "base/basictypes.h"
 #include "ui/gl/gl_export.h"
 
 namespace ui {
+
+GL_EXPORT const char* GetEGLErrorString(uint32_t error);
 
 // Returns the last EGL error as a string.
 GL_EXPORT const char* GetLastEGLErrorString();
