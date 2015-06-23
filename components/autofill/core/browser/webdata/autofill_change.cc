e8c747c7fc22defae98677cd3c703a46ef82b864
rouslan <rouslan@chromium.org>
2015-06-23 15:10:15 -0700

    Reland: [autofill] Sync server card and address metadata.
    First landed in: http://crrev.com/7c5be3b
    
    Reverted due to crashing in: http://crrev.com/df8814b
    
    This patch has the original patch and the fix for the crash. The fix is
    tested in AutofillProfileSyncableServiceTest.IgnoreServerProfileUpdate.
    
    BUG=481595
    
    Review URL: https://codereview.chromium.org/1172133002
    
    Cr-Commit-Position: refs/heads/master@{#335766}

--- components/autofill/core/browser/webdata/autofill_change.cc
+++ components/autofill/core/browser/webdata/autofill_change.cc
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/webdata/autofill_change.h"
 
 #include "base/logging.h"
-#include "components/autofill/core/browser/autofill_profile.h"
-#include "components/autofill/core/browser/credit_card.h"
 
 namespace autofill {
 
@@ -16,25 +14,6 @@ AutofillChange::AutofillChange(Type type, const AutofillKey& key)
 }
 
 AutofillChange::~AutofillChange() {
-}
-
-AutofillProfileChange::AutofillProfileChange(
-  Type type, const std::string& key, const AutofillProfile* profile)
-    : GenericAutofillChange<std::string>(type, key),
-      profile_(profile) {
-  DCHECK(type == ADD ? (profile && profile->guid() == key) : true);
-  DCHECK(type == UPDATE ? (profile && profile->guid() == key) : true);
-  DCHECK(type == REMOVE ? !profile : true);
-}
-
-AutofillProfileChange::~AutofillProfileChange() {
-}
-
-bool AutofillProfileChange::operator==(
-    const AutofillProfileChange& change) const {
-  return type() == change.type() &&
-         key() == change.key() &&
-         (type() != REMOVE) ? *profile() == *change.profile() : true;
 }
 
 }  // namespace autofill
