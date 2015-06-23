b8333196922f8559a65376d44691c13e6d944e23
vogelheim@chromium.org <vogelheim@chromium.org>
2015-06-23 12:15:01 +0000

    Cleanup: Remove experimental V8 cache strategies that we discarded.
    [The Chrome-side part of this cleanup are in crrev.com/1192323002.]
    
    Remaining are only:
    - none (all off),
    - parse (formerly parse-memory, the pre-M43 default)
    - code (formerly heuristics, the new default)
    - default (whatever we think is best. We think "code" is best.)
    
    BUG=455187, 399580
    
    Review URL: https://codereview.chromium.org/1195763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197643 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/V8ScriptRunnerTest.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8ScriptRunnerTest.cpp
@@ -102,7 +102,6 @@ int V8ScriptRunnerTest::counter = 0;
 TEST_F(V8ScriptRunnerTest, resourcelessShouldPass)
 {
     EXPECT_TRUE(compileScript(V8CacheOptionsNone));
-    EXPECT_TRUE(compileScript(V8CacheOptionsParseMemory));
     EXPECT_TRUE(compileScript(V8CacheOptionsParse));
     EXPECT_TRUE(compileScript(V8CacheOptionsCode));
 }
@@ -111,18 +110,6 @@ TEST_F(V8ScriptRunnerTest, emptyResourceDoesNotHaveCacheHandler)
 {
     setEmptyResource();
     EXPECT_FALSE(cacheHandler());
-}
-
-TEST_F(V8ScriptRunnerTest, parseMemoryOption)
-{
-    setResource();
-    EXPECT_TRUE(compileScript(V8CacheOptionsParseMemory));
-    EXPECT_TRUE(cacheHandler()->cachedMetadata(tagForParserCache(cacheHandler())));
-    EXPECT_FALSE(cacheHandler()->cachedMetadata(tagForCodeCache(cacheHandler())));
-    // The cached data is associated with the encoding.
-    ResourceRequest request(url());
-    ResourcePtr<ScriptResource> anotherResource = new ScriptResource(request, "UTF-16");
-    EXPECT_FALSE(cacheHandler()->cachedMetadata(tagForParserCache(anotherResource->cacheHandler())));
 }
 
 TEST_F(V8ScriptRunnerTest, parseOption)
@@ -140,21 +127,17 @@ TEST_F(V8ScriptRunnerTest, parseOption)
 TEST_F(V8ScriptRunnerTest, codeOption)
 {
     setResource();
+
+    // Compile twice, since 'code' has a probation period before it caches.
     EXPECT_TRUE(compileScript(V8CacheOptionsCode));
+    EXPECT_TRUE(compileScript(V8CacheOptionsCode));
+
     EXPECT_FALSE(cacheHandler()->cachedMetadata(tagForParserCache(cacheHandler())));
     EXPECT_TRUE(cacheHandler()->cachedMetadata(tagForCodeCache(cacheHandler())));
     // The cached data is associated with the encoding.
     ResourceRequest request(url());
     ResourcePtr<ScriptResource> anotherResource = new ScriptResource(request, "UTF-16");
     EXPECT_FALSE(cacheHandler()->cachedMetadata(tagForCodeCache(anotherResource->cacheHandler())));
-}
-
-TEST_F(V8ScriptRunnerTest, codeCompressedOptions)
-{
-    setResource();
-    EXPECT_TRUE(compileScript(V8CacheOptionsCodeCompressed));
-    EXPECT_FALSE(cacheHandler()->cachedMetadata(tagForParserCache(cacheHandler())));
-    EXPECT_FALSE(cacheHandler()->cachedMetadata(tagForCodeCache(cacheHandler())));
 }
 
 } // namespace
