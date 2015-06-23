346d9af24dc99ff7964ca8ba4799195f5cd4f5a2
miu <miu@chromium.org>
2015-06-23 16:26:24 -0700

    Tab Mute Experiment: Disable click-to-mute for small/pinned tabs
    When tabs become small (e.g., pinned tabs), it was difficult to click on
    the tab to select it while audio was playing.  This change disables the
    click-to-mute/unmute functionality while a tab is small.  The user can
    still right-click to toggle mute from the tab's context menu.
    
    BUG=420410
    
    Review URL: https://codereview.chromium.org/1170223003
    
    Cr-Commit-Position: refs/heads/master@{#335804}

--- chrome/browser/ui/views/tabs/media_indicator_button.h
+++ chrome/browser/ui/views/tabs/media_indicator_button.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/view_targeter_delegate.h"
 
+class Tab;
+
 namespace gfx {
 class Animation;
 class AnimationDelegate;
@@ -30,7 +32,7 @@ class MediaIndicatorButton : public views::ImageButton,
   // The MediaIndicatorButton's class name.
   static const char kViewClassName[];
 
-  MediaIndicatorButton();
+  explicit MediaIndicatorButton(Tab* parent_tab);
   ~MediaIndicatorButton() override;
 
   // Returns the current TabMediaState except, while the indicator image is
@@ -43,12 +45,19 @@ class MediaIndicatorButton : public views::ImageButton,
   // activates/deactivates button functionality as appropriate.
   void TransitionToMediaState(TabMediaState next_state);
 
+  // Determines whether the MediaIndicatorButton will be clickable for toggling
+  // muting.  This should be called whenever the active/inactive state of a tab
+  // has changed.  Internally, TransitionToMediaState() and OnBoundsChanged()
+  // calls this when the TabMediaState or the bounds have changed.
+  void UpdateEnabledForMuteToggle();
+
  protected:
   // views::View:
   const char* GetClassName() const override;
   View* GetTooltipHandlerForPoint(const gfx::Point& point) override;
   bool OnMousePressed(const ui::MouseEvent& event) override;
   bool OnMouseDragged(const ui::MouseEvent& event) override;
+  void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
   void OnPaint(gfx::Canvas* canvas) override;
 
   // views::ViewTargeterDelegate
@@ -58,8 +67,16 @@ class MediaIndicatorButton : public views::ImageButton,
   // views::Button:
   void NotifyClick(const ui::Event& event) override;
 
+  // views::CustomButton:
+  bool IsTriggerableEvent(const ui::Event& event) override;
+
  private:
   class FadeAnimationDelegate;
+
+  // Returns the tab (parent view) of this MediaIndicatorButton.
+  Tab* GetTab() const;
+
+  Tab* const parent_tab_;
 
   TabMediaState media_state_;
 
