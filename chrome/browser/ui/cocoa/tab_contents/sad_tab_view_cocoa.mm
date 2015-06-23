168700160d1ba7a35ea35319753f5e936b0e7054
shahriar.rostami <shahriar.rostami@gmail.com>
2015-06-23 13:41:27 -0700

    [Mac] Add url link to help link shown in sad_tab_view_cocoa.mm titled with "suggestions".
    We were rendering HyperlinkTextView and using clickedOnLink delegate to open a page containing <h1>"Aw, Snap!"</h1>. However in Mac when we right click on such links, we can see "copy link" button in context menu, but it was not working because it was lacking url. Now it is set to the proper URL.
    
    BUG=484130
    
    TEST=Follow these steps (taken from bug report)
    
    1.Launch chrome,navigate to any webpage and enter chrome://kill inside omnibox to crash the page manually.
    2.Now right click on suggestions link button and select 'copy link' option from context menu.
    3.Open Ntp, right click and select paste option, observe.
    
    Review URL: https://codereview.chromium.org/1204673002
    
    Cr-Commit-Position: refs/heads/master@{#335741}

--- chrome/browser/ui/cocoa/tab_contents/sad_tab_view_cocoa.mm
+++ chrome/browser/ui/cocoa/tab_contents/sad_tab_view_cocoa.mm
@@ -182,7 +182,7 @@ static const CGFloat kMaxTopMargin = 130;
            withFont:helpFont
        messageColor:[message_ textColor]];
   [help_ addLinkRange:NSMakeRange(linkOffset, helpLink.length())
-             withName:@""
+             withName:@(chrome::kCrashReasonURL)
             linkColor:[message_ textColor]];
   [help_ setAlignment:NSLeftTextAlignment];
   [help_ sizeToFit];
