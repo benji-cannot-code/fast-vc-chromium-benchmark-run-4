f840739871fa74c8ba1f0979b088387cbc695da9
vogelheim <vogelheim@chromium.org>
2015-06-23 02:55:07 -0700

    Cleanup: Remove experimental V8 cache strategies that we discarded.
    Remaining are only:
    - none (all off),
    - parse (formerly parse-memory, the pre-M43 default)
    - code (formerly heuristics, the new default)
    - default (whatever we think is best. We think "code" is best.)
    
    blink companion CL is forthcoming, and will be submitted right afterwards.
    
    BUG=455187,399580
    
    Review URL: https://codereview.chromium.org/1192323002
    
    Cr-Commit-Position: refs/heads/master@{#335652}

--- content/test/data/service_worker/worker.js
+++ content/test/data/service_worker/worker.js
@@ -4,3 +4,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 this.oninstall = function(event) {};
+
+// So sorry about this waste of bytes:
+// Filler comment, to trigger code caching heuristic (script > 1K.)
+// Filler comment, to trigger code caching heuristic (script > 1K.)
+// Filler comment, to trigger code caching heuristic (script > 1K.)
+// Filler comment, to trigger code caching heuristic (script > 1K.)
+// Filler comment, to trigger code caching heuristic (script > 1K.)
+// Filler comment, to trigger code caching heuristic (script > 1K.)
+// Filler comment, to trigger code caching heuristic (script > 1K.)
+// Filler comment, to trigger code caching heuristic (script > 1K.)
+// Filler comment, to trigger code caching heuristic (script > 1K.)
+// Filler comment, to trigger code caching heuristic (script > 1K.)
+// Filler comment, to trigger code caching heuristic (script > 1K.)
+// Filler comment, to trigger code caching heuristic (script > 1K.)
+
