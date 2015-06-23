3d7cdc90d3e3b87bcea386a6e120fc4c034ccfd3
wjmaclean <wjmaclean@chromium.org>
2015-06-23 15:26:21 -0700

    Disable WebViewFocusTest on Site Isolation FYI bots.
    The WebViewFocusTest class is based on WebViewTest, which is currently
    disabled on the Site Isolation FYI bots. Therefore there's no real
    chance of it working until WebViewTest is fixed for those bots.
    
    This CL adds TODOs to both test classes, as well as disabling
    WebViewFocusTest on the Site Isolation FYI bots as well.
    
    BUG=none
    TBR=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/1201213002
    
    Cr-Commit-Position: refs/heads/master@{#335776}

--- chrome/browser/apps/guest_view/web_view_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_browsertest.cc
@@ -354,6 +354,10 @@ class MockDownloadWebContentsDelegate : public content::WebContentsDelegate {
   DISALLOW_COPY_AND_ASSIGN(MockDownloadWebContentsDelegate);
 };
 
+// TODO(wjmaclean): Fix this test class at some point so it can be re-enabled on
+// the site isolation bots, and then look at re-enabling WebViewFocusTest when
+// that happens.
+// https://crbug.com/503751
 class WebViewTest : public extensions::PlatformAppBrowserTest {
  protected:
   void SetUp() override {
@@ -2405,6 +2409,9 @@ IN_PROC_BROWSER_TEST_F(WebViewTest, Shim_TestGarbageCollect) {
 }
 
 #if defined(USE_AURA)
+// TODO(wjmaclean): when WebViewTest is re-enabled on the site-isolation
+// bots, then re-enable this test class as well.
+// https://crbug.com/503751
 class WebViewFocusTest : public WebViewTest {
  public:
   ~WebViewFocusTest() override {}
