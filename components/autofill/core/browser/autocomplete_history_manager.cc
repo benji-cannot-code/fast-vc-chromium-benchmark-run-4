71617f31706c04f1ffc37681840bf38fb02289cb
brettw <brettw@chromium.org>
2015-06-23 15:26:48 -0700

    Fix the "autofill disabled" experiment.
    This experiment attempts to measure form submissions with no autofill. But the previous version disabled autofill completely which disabled metrics also, rendering the experiment useless.
    
    This version just suppresses autofill suggestions when the experiment is running (i.e. autofill disabled) so the same metrics will be collected in either case.
    
    Clarifies the meaning of the "non-fillable form" metric. I didn't realize this also included cases of non matching data and found this surprising.
    
    BUG=503253
    
    Review URL: https://codereview.chromium.org/1193843005
    
    Cr-Commit-Position: refs/heads/master@{#335777}

--- components/autofill/core/browser/autocomplete_history_manager.cc
+++ components/autofill/core/browser/autocomplete_history_manager.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "components/autofill/core/browser/autofill_client.h"
 #include "components/autofill/core/browser/autofill_driver.h"
+#include "components/autofill/core/browser/autofill_experiments.h"
 #include "components/autofill/core/browser/autofill_external_delegate.h"
 #include "components/autofill/core/browser/validation.h"
 #include "components/autofill/core/common/autofill_pref_names.h"
@@ -96,7 +97,8 @@ void AutocompleteHistoryManager::OnGetAutocompleteSuggestions(
   query_id_ = query_id;
   autofill_suggestions_ = suggestions;
   if (!autofill_client_->IsAutocompleteEnabled() ||
-      form_control_type == "textarea") {
+      form_control_type == "textarea" ||
+      IsInAutofillSuggestionsDisabledExperiment()) {
     SendSuggestions(NULL);
     return;
   }
