82ec90c65f7ca08543d2ed1f3f11a9b50775d146
ianwen <ianwen@chromium.org>
2015-06-23 01:05:08 -0700

    [Android] Hide keyboard when edit activity finishes
    Dismiss the software keyboard when the edit activity finishes. onPause()
    is preferred here because when the activity is pushed to
    recents/background, destroy() isn't going to be called right away.
    
    BUG=499415
    
    Review URL: https://codereview.chromium.org/1196393003
    
    Cr-Commit-Position: refs/heads/master@{#335644}

--- chrome/android/java_staging/src/org/chromium/chrome/browser/enhancedbookmarks/EnhancedBookmarkEditActivity.java
+++ chrome/android/java_staging/src/org/chromium/chrome/browser/enhancedbookmarks/EnhancedBookmarkEditActivity.java
@@ -20,6 +20,7 @@ import org.chromium.chrome.browser.UrlUtilities;
 import org.chromium.chrome.browser.enhanced_bookmarks.EnhancedBookmarksModel;
 import org.chromium.chrome.browser.widget.EmptyAlertEditText;
 import org.chromium.components.bookmarks.BookmarkId;
+import org.chromium.ui.UiUtils;
 
 /**
  * The activity that enables the user to modify the title, url and parent folder of a bookmark.
@@ -139,6 +140,12 @@ public class EnhancedBookmarkEditActivity extends EnhancedBookmarkActivityBase {
         mEnhancedBookmarksModel.setBookmarkTitle(mBookmarkId, newTitle);
         mEnhancedBookmarksModel.setBookmarkUrl(mBookmarkId, newUrl);
         super.onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        UiUtils.hideKeyboard(mTitleEditText);
     }
 
     @Override
