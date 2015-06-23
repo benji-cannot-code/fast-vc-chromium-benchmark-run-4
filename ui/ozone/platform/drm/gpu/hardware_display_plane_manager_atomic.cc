e44aa32ceeb51d1cac525915e168fe57dc41c110
achaulk <achaulk@chromium.org>
2015-06-23 12:05:55 -0700

    ozone: Reallocate atomic property set
    The library doesn't clear the existing set for us, so we start fresh each frame
    
    BUG=485571
    
    Committed: https://crrev.com/23171db9366c6e9b3a926ccc558c3bd001f0852e
    Cr-Commit-Position: refs/heads/master@{#335685}
    
    Review URL: https://codereview.chromium.org/1206513005
    
    Cr-Commit-Position: refs/heads/master@{#335723}

--- ui/ozone/platform/drm/gpu/hardware_display_plane_manager_atomic.cc
+++ ui/ozone/platform/drm/gpu/hardware_display_plane_manager_atomic.cc
@@ -71,6 +71,7 @@ bool HardwareDisplayPlaneManagerAtomic::Commit(
     PLOG(ERROR) << "Failed to commit properties";
     return false;
   }
+  plane_list->atomic_property_set.reset(drmModePropertySetAlloc());
   return true;
 }
 
