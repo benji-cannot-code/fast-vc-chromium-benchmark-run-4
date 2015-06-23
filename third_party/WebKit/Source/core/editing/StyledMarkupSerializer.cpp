b5703b26764f3b0977168c7f68621c47744bfc26
hajimehoshi@chromium.org <hajimehoshi@chromium.org>
2015-06-23 05:10:30 +0000

    Bug fix: StyledMarkupSerializer's m_lastClosed can be null
    crrev.com/1188693005 introduced StyledMarkupTraverser and added a new
    assertion that |m_lastClosed| is not null iff |m_accumulator| is null.
    This was wrong and this has led crashes. This CL fixes this and adds a
    test case to reproduce.
    
    BUG=502404
    TEST=webkit_unit_tests --gtest_filter=StyledMarkupSerializerTest.SytleDisplayNoneAndNewLines
    
    Review URL: https://codereview.chromium.org/1196173003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197627 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/StyledMarkupSerializer.cpp
+++ third_party/WebKit/Source/core/editing/StyledMarkupSerializer.cpp
@@ -274,7 +274,8 @@ StyledMarkupTraverser<Strategy>::StyledMarkupTraverser(StyledMarkupAccumulator* 
         ASSERT(!m_lastClosed);
         return;
     }
-    ASSERT(m_lastClosed);
+    if (!m_lastClosed)
+        return;
     ContainerNode* parent = Strategy::parent(*m_lastClosed);
     if (!parent)
         return;
