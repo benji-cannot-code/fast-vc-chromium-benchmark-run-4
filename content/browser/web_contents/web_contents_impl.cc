af6b295024e3d88226d7fa38a944ad0baed121c9
hugo.holgersson <hugo.holgersson@sonymobile.com>
2015-06-23 11:48:40 -0700

    Remove unused include from WebContentsImpl
    WebContentsImpl used BrowserMediaPlayerManager from
    https://codereview.chromium.org/262823006, but after
    https://codereview.chromium.org/278353003 it isn't used.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1205583002
    
    Cr-Commit-Position: refs/heads/master@{#335718}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -116,7 +116,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/android/content_video_view.h"
 #include "content/browser/android/date_time_chooser_android.h"
 #include "content/browser/android/media_players_observer.h"
-#include "content/browser/media/android/browser_media_player_manager.h"
 #include "content/browser/web_contents/web_contents_android.h"
 #endif
 
