874b94208515b0585976a2deb6feab81db89ecd0
groby <groby@chromium.org>
2015-06-23 16:07:14 -0700

    Handle failed fetches/decodes from BitmapFetcher.
    While the comments clearly stated that BitmapFetcher can never deliver
    a nullptr image pointer, it can. Add code to handle this case.
    
    BitmapFetcherService will treat nullptr images the same as it treats
    empty images - no caching or notification for them.
    
    BUG=503573
    R=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1202363002
    
    Cr-Commit-Position: refs/heads/master@{#335797}

--- chrome/browser/bitmap_fetcher/bitmap_fetcher_service.cc
+++ chrome/browser/bitmap_fetcher/bitmap_fetcher_service.cc
@@ -24,7 +24,7 @@ class BitmapFetcherRequest {
                        BitmapFetcherService::Observer* observer);
   ~BitmapFetcherRequest();
 
-  void NotifyImageChanged(const SkBitmap& bitmap);
+  void NotifyImageChanged(const SkBitmap* bitmap);
   BitmapFetcherService::RequestId request_id() const { return request_id_; }
 
   // Weak ptr |fetcher| is used to identify associated fetchers.
@@ -48,9 +48,9 @@ BitmapFetcherRequest::BitmapFetcherRequest(
 BitmapFetcherRequest::~BitmapFetcherRequest() {
 }
 
-void BitmapFetcherRequest::NotifyImageChanged(const SkBitmap& bitmap) {
-  if (!bitmap.empty())
-    observer_->OnImageChanged(request_id_, bitmap);
+void BitmapFetcherRequest::NotifyImageChanged(const SkBitmap* bitmap) {
+  if (bitmap && !bitmap->empty())
+    observer_->OnImageChanged(request_id_, *bitmap);
 }
 
 BitmapFetcherService::CacheEntry::CacheEntry() {
@@ -96,7 +96,7 @@ BitmapFetcherService::RequestId BitmapFetcherService::RequestImage(
   base::OwningMRUCache<GURL, CacheEntry*>::iterator iter = cache_.Get(url);
   if (iter != cache_.end()) {
     BitmapFetcherService::CacheEntry* entry = iter->second;
-    request->NotifyImageChanged(*(entry->bitmap.get()));
+    request->NotifyImageChanged(entry->bitmap.get());
 
     // There is no request ID associated with this - data is already delivered.
     return REQUEST_ID_INVALID;
@@ -167,8 +167,6 @@ void BitmapFetcherService::RemoveFetcher(const chrome::BitmapFetcher* fetcher) {
 
 void BitmapFetcherService::OnFetchComplete(const GURL& url,
                                            const SkBitmap* bitmap) {
-  DCHECK(bitmap);  // can never be NULL, guaranteed by BitmapFetcher.
-
   const chrome::BitmapFetcher* fetcher = FindFetcherForUrl(url);
   DCHECK(fetcher);
 
@@ -176,14 +174,14 @@ void BitmapFetcherService::OnFetchComplete(const GURL& url,
   ScopedVector<BitmapFetcherRequest>::iterator iter = requests_.begin();
   while (iter != requests_.end()) {
     if ((*iter)->get_fetcher() == fetcher) {
-      (*iter)->NotifyImageChanged(*bitmap);
+      (*iter)->NotifyImageChanged(bitmap);
       iter = requests_.erase(iter);
     } else {
       ++iter;
     }
   }
 
-  if (!bitmap->isNull()) {
+  if (bitmap && !bitmap->isNull()) {
     CacheEntry* entry = new CacheEntry;
     entry->bitmap.reset(new SkBitmap(*bitmap));
     cache_.Put(fetcher->url(), entry);
