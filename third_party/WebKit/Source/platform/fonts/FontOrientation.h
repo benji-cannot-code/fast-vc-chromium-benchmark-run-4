2d38af04a761f89dfa9e2dd3800a7baaafcbe6df
kojii@chromium.org <kojii@chromium.org>
2015-06-24 23:53:53 +0000

    Fix default text orientation that do not conform Unicode Technical Report #50
    This patch fixes the default orientation of glyphs in vertical flow.
    
    In vertical flow, when the author does not specify the orientation of
    glyphs using the text-orientation property[1], the spec defines[2] that
    UA must typeset by following UTR#50[3].
    
    Currently, Blink honors UTR#50 only for non-CJK code points, and some of
    the non-CJK have incorrect orientation, resulting 462 failures in total.
    
    This patch fixes both some CJK characters that must rotate, and
    incorrect orientation of non-CJK characters.
    
    Mongolian/Phags-Pa specific orientation test was removed as these
    scripts are now covered by imported CSS WG tests.
    
    One code point in the imported tests still fail:
    text-orientation-script-001e.html
    This needs further analysis, which is deferred from this patch.
    
    [1] http://dev.w3.org/csswg/css-writing-modes-3/#text-orientation
    [2] http://dev.w3.org/csswg/css-writing-modes-3/#vertical-orientations
    [3] http://www.unicode.org/reports/tr50/
    
    BUG=503979
    TEST=imported/csswg-test/css-writing-modes-3/text-orientation-script-001[a-o].html
    
    Review URL: https://codereview.chromium.org/1206883002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197786 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/FontOrientation.h
+++ third_party/WebKit/Source/platform/fonts/FontOrientation.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef FontOrientation_h
 #define FontOrientation_h
 
+#include "platform/fonts/Character.h"
+
 namespace blink {
 
 enum class FontOrientation {
@@ -47,6 +49,11 @@ enum class FontOrientation {
 inline bool operator&(FontOrientation value, FontOrientation mask) { return static_cast<unsigned>(value) & static_cast<unsigned>(mask); }
 inline bool isVerticalAnyUpright(FontOrientation orientation) { return orientation & FontOrientation::AnyUprightMask; }
 inline bool isVerticalNonCJKUpright(FontOrientation orientation) { return orientation == FontOrientation::VerticalUpright; }
+inline bool isVerticalUpright(FontOrientation orientation, UChar32 character)
+{
+    return orientation == FontOrientation::VerticalUpright
+        || (orientation == FontOrientation::VerticalMixed && Character::isUprightInMixedVertical(character));
+}
 inline bool isVerticalBaseline(FontOrientation orientation) { return orientation != FontOrientation::Horizontal; }
 
 } // namespace blink
