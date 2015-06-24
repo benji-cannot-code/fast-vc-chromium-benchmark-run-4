2a63c3a7eac1ea9eb0fe39e711f58d37ff8a0384
mek@chromium.org <mek@chromium.org>
2015-06-24 04:11:51 +0000

    Start implementing new navigator.connect API.
    As it turns out, we changed our mind about the desired API yet again, and this
    time rather completely. This is the start of implementing the currently specced
    API, which hopefully will actually stick.
    
    This CL adds stubs for the new ServicePortCollection API.
    
    BUG=426458
    
    Review URL: https://codereview.chromium.org/1192913003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197697 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/modules/navigatorconnect/ServicePortCollection.h
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ServicePortCollection_h
+#define ServicePortCollection_h
+
+#include "bindings/core/v8/ScriptPromise.h"
+#include "core/dom/ContextLifecycleObserver.h"
+#include "core/events/EventTarget.h"
+#include "modules/ModulesExport.h"
+#include "wtf/RefCounted.h"
+
+namespace blink {
+
+class ServicePortConnectOptions;
+class ServicePortMatchOptions;
+
+class MODULES_EXPORT ServicePortCollection final
+    : public EventTargetWithInlineData
+    , public RefCountedWillBeNoBase<ServicePortCollection>
+    , public ContextLifecycleObserver {
+    DEFINE_WRAPPERTYPEINFO();
+    REFCOUNTED_EVENT_TARGET(ServicePortCollection);
+    WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(ServicePortCollection);
+    WTF_MAKE_NONCOPYABLE(ServicePortCollection);
+public:
+    static PassRefPtrWillBeRawPtr<ServicePortCollection> create(ExecutionContext*);
+    ~ServicePortCollection() override;
+
+    // ServicePortCollection.idl
+    ScriptPromise connect(ScriptState*, const String& url, const ServicePortConnectOptions&);
+    ScriptPromise match(ScriptState*, const ServicePortMatchOptions&);
+    ScriptPromise matchAll(ScriptState*, const ServicePortMatchOptions&);
+    DEFINE_ATTRIBUTE_EVENT_LISTENER(connect);
+    DEFINE_ATTRIBUTE_EVENT_LISTENER(message);
+    DEFINE_ATTRIBUTE_EVENT_LISTENER(close);
+
+    // EventTarget overrides.
+    const AtomicString& interfaceName() const override;
+    ExecutionContext* executionContext() const override;
+
+    DECLARE_VIRTUAL_TRACE();
+
+private:
+    explicit ServicePortCollection(ExecutionContext*);
+};
+
+} // namespace blink
+
+#endif // ServicePortCollection_h
