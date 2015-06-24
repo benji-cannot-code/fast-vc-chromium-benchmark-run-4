782545952ab3dc716611a009d9cd5ad22d9aa7de
caseq <caseq@chromium.org>
2015-06-24 01:23:45 -0700

    DevTools: fix concurrency problems in DevToolsFrameTraceRecorder
    DevToolsFrameTraceRecorder used to mutate the class being traced
    after the moment when it was traced, which created potential races
    against conversion of that class to trace format, potentially performed
    on a different thread after trace completion. Let's create a
    retrospective trace event when frame capture is done instead. Also,
    convert the trace event from instant to object snapshot, which is
    more appropriate for the data being logged.
    
    Tracing: add TRACE_EVENT_OBJECT_SNAPSHOT_WITH_ID_AND_TIMESTAMP()
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1188773005
    
    Cr-Commit-Position: refs/heads/master@{#335886}

--- base/trace_event/trace_event.h
+++ base/trace_event/trace_event.h
@@ -889,6 +889,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         category_group, name, TRACE_ID_DONT_MANGLE(id), TRACE_EVENT_FLAG_NONE,\
         "snapshot", snapshot)
 
+#define TRACE_EVENT_OBJECT_SNAPSHOT_WITH_ID_AND_TIMESTAMP(            \
+    category_group, name, id, timestamp, snapshot)                    \
+  INTERNAL_TRACE_EVENT_ADD_WITH_ID_TID_AND_TIMESTAMP(                 \
+      TRACE_EVENT_PHASE_SNAPSHOT_OBJECT, category_group, name,        \
+      TRACE_ID_DONT_MANGLE(id),                                       \
+      static_cast<int>(base::PlatformThread::CurrentId()), timestamp, \
+      TRACE_EVENT_FLAG_NONE, "snapshot", snapshot)
+
 #define TRACE_EVENT_OBJECT_DELETED_WITH_ID(category_group, name, id) \
     INTERNAL_TRACE_EVENT_ADD_WITH_ID(TRACE_EVENT_PHASE_DELETE_OBJECT, \
         category_group, name, TRACE_ID_DONT_MANGLE(id), TRACE_EVENT_FLAG_NONE)
