5c0eb02d4e90b5bdb0178f77d2e75914fd8c4d06
bajones@chromium.org <bajones@chromium.org>
2015-06-24 15:06:25 +0000

    Some name casing corrections suggested by esprehn@
    BUG=389343
    
    Review URL: https://codereview.chromium.org/1207513003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197741 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/vr/HMDVRDevice.cpp
+++ third_party/WebKit/Source/modules/vr/HMDVRDevice.cpp
@@ -12,20 +12,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-HMDVRDevice::HMDVRDevice(VRHardwareUnit* hardwareUnit, unsigned deviceId)
-    : VRDevice(hardwareUnit, deviceId)
-{
-    m_eyeParametersLeft = VREyeParameters::create();
-    m_eyeParametersRight = VREyeParameters::create();
-}
+namespace {
 
-VREye HMDVRDevice::StringToVREye(const String& whichEye)
+VREye stringToVREye(const String& whichEye)
 {
     if (whichEye == "left")
         return VREyeLeft;
     if (whichEye == "right")
         return VREyeRight;
     return VREyeNone;
+}
+
+} // namepspace
+
+HMDVRDevice::HMDVRDevice(VRHardwareUnit* hardwareUnit, unsigned deviceId)
+    : VRDevice(hardwareUnit, deviceId)
+{
+    m_eyeParametersLeft = VREyeParameters::create();
+    m_eyeParametersRight = VREyeParameters::create();
 }
 
 void HMDVRDevice::updateFromWebVRDevice(const WebVRDevice& device)
@@ -39,7 +43,7 @@ void HMDVRDevice::updateFromWebVRDevice(const WebVRDevice& device)
 
 VREyeParameters* HMDVRDevice::getEyeParameters(const String& whichEye)
 {
-    switch (StringToVREye(whichEye)) {
+    switch (stringToVREye(whichEye)) {
     case VREyeLeft:
         return m_eyeParametersLeft;
     case VREyeRight:
