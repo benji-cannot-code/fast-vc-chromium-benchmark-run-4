cf5114fbd6b3cc7a7536eb8116568c15672b8e1d
h.joshi@samsung.com <h.joshi@samsung.com>
2015-06-24 01:13:28 +0000

    Remove duplicate measure hyphen string method
    Remove duplicate measure hyphen string method. We are having two methods
    to calculate Hyphen string width. One in LayoutText and another in
    BreakingContextInlineHeaders. This patch removed duplicated method
    from BreakingContextInlineHeaders.
    
    TEST=none
    
    Review URL: https://codereview.chromium.org/1196213002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197692 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutText.h
+++ third_party/WebKit/Source/core/layout/LayoutText.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CoreExport.h"
 #include "core/dom/Text.h"
 #include "core/layout/LayoutObject.h"
+#include "core/layout/TextRunConstructor.h"
 #include "platform/LengthFunctions.h"
 #include "platform/text/TextPath.h"
 #include "wtf/Forward.h"
@@ -142,6 +143,8 @@ public:
 
     PassRefPtr<AbstractInlineTextBox> firstAbstractInlineTextBox();
 
+    float hyphenWidth(const Font&, TextDirection);
+
 protected:
     virtual void willBeDestroyed() override;
 
@@ -223,6 +226,12 @@ inline UChar LayoutText::characterAt(unsigned i) const
         return 0;
 
     return uncheckedCharacterAt(i);
+}
+
+inline float LayoutText::hyphenWidth(const Font& font, TextDirection direction)
+{
+    const ComputedStyle& style = styleRef();
+    return font.width(constructTextRun(this, font, style.hyphenString().string(), style, direction));
 }
 
 DEFINE_LAYOUT_OBJECT_TYPE_CASTS(LayoutText, isText());
