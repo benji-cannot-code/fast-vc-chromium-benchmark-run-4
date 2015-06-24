c8def637b5e64bb9d7ceea7d2aadeb69cf77feba
yawano <yawano@chromium.org>
2015-06-24 00:14:04 -0700

    Add toggle ripple effect to cloud import button.
    BUG=480719
    TEST=manually tested
    
    Review URL: https://codereview.chromium.org/1191893005
    
    Cr-Commit-Position: refs/heads/master@{#335882}

---
+++ ui/file_manager/externs/paper_elements.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @constructor
+ * @struct
+ * @extends {HTMLElement}
+ *
+ * TODO(yawano): This extern file is a temporary fix. Create or migrate to
+ *     proper extern definitions of paper elements.
+ */
+function PaperRipple() {}
+
+PaperRipple.prototype.simulatedRipple = function() {};
