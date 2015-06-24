02dabe263c5ac878563f72eb8f25f70158e9e905
alexclarke@chromium.org <alexclarke@chromium.org>
2015-06-24 16:09:42 +0000

    Lazily constructed bespoke cancellable timer task. This is a bit cheaper that using the CancellableTaskFactory and goes part way towards fixing the performance regression in dromaeo.jslibmodifyprototype.
    For perf results with this patch, see:
    http://storage.googleapis.com/chromium-telemetry/html-results/results-2015-06-23_10-45-33 and
    http://storage.googleapis.com/chromium-telemetry/html-results/results-2015-06-23_10-42-47
    
    BUG=498229,463143
    
    Review URL: https://codereview.chromium.org/1185643002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197752 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/Timer.h
+++ third_party/WebKit/Source/platform/Timer.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/PlatformExport.h"
 #include "platform/heap/Handle.h"
-#include "platform/scheduler/CancellableTaskFactory.h"
 #include "public/platform/WebTraceLocation.h"
 #include "wtf/AddressSanitizer.h"
 #include "wtf/Noncopyable.h"
@@ -88,13 +87,37 @@ private:
 
     void setNextFireTime(double now, double delay);
 
-    void run();
+    void runInternal();
+
+    class CancellableTimerTask final : public WebThread::Task {
+        WTF_MAKE_NONCOPYABLE(CancellableTimerTask);
+    public:
+        explicit CancellableTimerTask(TimerBase* timer) : m_timer(timer) { }
+
+        ~CancellableTimerTask() override { }
+
+        void run() override
+        {
+            if (m_timer) {
+                m_timer->m_cancellableTimerTask = nullptr;
+                m_timer->runInternal();
+            }
+        }
+
+        void cancel()
+        {
+            m_timer = nullptr;
+        }
+
+    private:
+        TimerBase* m_timer; // NOT OWNED
+    };
 
     double m_nextFireTime; // 0 if inactive
     double m_unalignedNextFireTime; // m_nextFireTime not considering alignment interval
     double m_repeatInterval; // 0 if not repeating
     WebTraceLocation m_location;
-    CancellableTaskFactory m_cancellableTaskFactory;
+    CancellableTimerTask* m_cancellableTimerTask; // NOT OWNED
     WebScheduler* m_webScheduler; // Not owned.
 
 #if ENABLE(ASSERT)
@@ -163,7 +186,7 @@ NO_LAZY_SWEEP_SANITIZE_ADDRESS
 inline bool TimerBase::isActive() const
 {
     ASSERT(m_thread == currentThread());
-    return m_cancellableTaskFactory.isPending();
+    return m_cancellableTimerTask;
 }
 
 } // namespace blink
