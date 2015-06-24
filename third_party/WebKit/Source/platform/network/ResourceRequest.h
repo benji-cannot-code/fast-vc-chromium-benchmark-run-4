e9b6c51ccbea98ac4336e27722aeaef1995fd56b
shiva.jm@samsung.com <shiva.jm@samsung.com>
2015-06-24 10:02:36 +0000

    Removing unused functions from ResourceRequest class
    This patch removes the following unused function: ResourceRequest::clearHTTPAuthorization(),
    ResourceRequest::defaultTimeoutInterval(),
    ResourceRequest::setDefaultTimeoutInterval(),
    initializeMaximumHTTPConnectionCountPerHost()
    
    BUG=439559
    
    Review URL: https://codereview.chromium.org/1207593006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197727 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/network/ResourceRequest.h
+++ third_party/WebKit/Source/platform/network/ResourceRequest.h
@@ -117,8 +117,6 @@ public:
     void addHTTPHeaderFields(const HTTPHeaderMap& headerFields);
     void clearHTTPHeaderField(const AtomicString& name);
 
-    void clearHTTPAuthorization();
-
     const AtomicString& httpContentType() const { return httpHeaderField("Content-Type");  }
     void setHTTPContentType(const AtomicString& httpContentType) { setHTTPHeaderField("Content-Type", httpContentType); }
 
@@ -215,9 +213,6 @@ public:
     bool cacheControlContainsNoCache() const;
     bool cacheControlContainsNoStore() const;
     bool hasCacheValidatorFields() const;
-
-    static double defaultTimeoutInterval(); // May return 0 when using platform default.
-    static void setDefaultTimeoutInterval(double);
 
     static bool compare(const ResourceRequest&, const ResourceRequest&);
 
@@ -324,8 +319,6 @@ public:
     InputToLoadPerfMetricReportPolicy m_inputPerfMetricReportPolicy;
     bool m_followedRedirect;
 };
-
-unsigned initializeMaximumHTTPConnectionCountPerHost();
 
 } // namespace blink
 
