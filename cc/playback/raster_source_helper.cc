8883bfcfed7375301533b86890be6a0b3bc22f18
varkha <varkha@chromium.org>
2015-06-24 13:09:02 -0700

    Fixes docked background painting
    Docked background widget is using an aura::Window backed layer which
    explains discrepancy with the launcher shelf background (which uses a
    view-backed layer). This discrepancy will be gone with
    https://codereview.chromium.org/1202663002/.
    Compositor clear that should be happening for the case of partial update
    was not happening because canvas->drawColor is using SkXfermode::kSrcOver_Mode
    by default.
    
    BUG=501931
    TEST=Make sure that background is painted properly in cases where partial
    background is drawn, e.g. docked background for a window that is not
    covering the whole docked area AND no maximized window in the workspace
    as well as launcher shelf background.
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1194383004
    
    Cr-Commit-Position: refs/heads/master@{#335986}

--- cc/playback/raster_source_helper.cc
+++ cc/playback/raster_source_helper.cc
@@ -52,7 +52,7 @@ void RasterSourceHelper::PrepareForPlaybackToCanvas(
       canvas->save();
       canvas->clipRect(gfx::RectToSkRect(
           canvas_playback_rect - canvas_bitmap_rect.OffsetFromOrigin()));
-      canvas->drawColor(SK_ColorTRANSPARENT);
+      canvas->drawColor(SK_ColorTRANSPARENT, SkXfermode::kClear_Mode);
       canvas->restore();
     }
   } else {
