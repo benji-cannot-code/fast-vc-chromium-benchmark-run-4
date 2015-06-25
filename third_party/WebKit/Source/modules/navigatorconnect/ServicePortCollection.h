1229c8dd241ffb73419e0fc7ab1d2f85285aee4c
sigbjornf@opera.com <sigbjornf@opera.com>
2015-06-25 09:15:30 +0000

    Oilpan: fix build after r197794.
    TBR=oilpan-reviews
    BUG=426458
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1209873002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197806 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/navigatorconnect/ServicePortCollection.h
+++ third_party/WebKit/Source/modules/navigatorconnect/ServicePortCollection.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/ContextLifecycleObserver.h"
 #include "core/events/EventTarget.h"
 #include "modules/ModulesExport.h"
+#include "platform/heap/Handle.h"
 #include "public/platform/modules/navigator_services/WebServicePortProvider.h"
 #include "public/platform/modules/navigator_services/WebServicePortProviderClient.h"
 #include "wtf/RefCounted.h"
@@ -27,6 +28,7 @@ class MODULES_EXPORT ServicePortCollection final
     DEFINE_WRAPPERTYPEINFO();
     REFCOUNTED_GARBAGE_COLLECTED_EVENT_TARGET(ServicePortCollection);
     WTF_MAKE_NONCOPYABLE(ServicePortCollection);
+    WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(ServicePortCollection);
 public:
     static ServicePortCollection* create(ExecutionContext*);
     ~ServicePortCollection() override;
