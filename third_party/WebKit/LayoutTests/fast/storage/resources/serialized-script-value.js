035efe75080c528e825ca59812df8b343591da3b
adamk@chromium.org <adamk@chromium.org>
2015-06-25 01:42:01 +0000

    Support structured cloning of ES'15 Map and Set
    BUG=478263
    
    Review URL: https://codereview.chromium.org/1205973002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197791 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/storage/resources/serialized-script-value.js
+++ third_party/WebKit/LayoutTests/fast/storage/resources/serialized-script-value.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-var kSerializedScriptValueVersion = 8;
+var kSerializedScriptValueVersion = 9;
 
 function forVersion(version, values) {
     var versionTag = 0xff;
