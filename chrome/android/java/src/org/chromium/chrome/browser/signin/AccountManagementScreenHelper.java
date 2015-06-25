c8d61472bfab0c70d5cbd5107add9a46c9e4d1cb
aruslan <aruslan@chromium.org>
2015-06-24 20:37:51 -0700

    Dissolve AccountManagementFragmentDelegate
    Everything is now upstreamed, the split is no longer necessary.
    
    BUG=504197
    
    Review URL: https://codereview.chromium.org/1203163003
    
    Cr-Commit-Position: refs/heads/master@{#336087}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/AccountManagementScreenHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/AccountManagementScreenHelper.java
@@ -5,15 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.signin;
 
-import android.accounts.Account;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
 
 import org.chromium.base.CalledByNative;
 import org.chromium.base.ThreadUtils;
-import org.chromium.chrome.browser.preferences.Preferences;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.profiles.ProfileAccountManagementMetrics;
 
@@ -21,7 +18,6 @@ import org.chromium.chrome.browser.profiles.ProfileAccountManagementMetrics;
  * Stub entry points and implementation interface for the account management fragment delegate.
  */
 public class AccountManagementScreenHelper {
-    private static AccountManagementFragmentDelegate sDelegate;
 
     /*
      * TODO(guohui): add all Gaia service types.
@@ -33,46 +29,6 @@ public class AccountManagementScreenHelper {
     private static final String EXTRA_ACCOUNT_TYPES = "account_types";
     private static final String EXTRA_VALUE_GOOGLE_ACCOUNTS = "com.google";
 
-    /**
-     * An account management screen delegate interface.
-     */
-    public interface AccountManagementFragmentDelegate {
-        /**
-         * Opens an incognito tab.
-         * @param activity Fragment activity
-         */
-        void openIncognitoTab(Activity activity);
-
-        /**
-         * Opens a sign out dialog.
-         * @param activity Fragment activity.
-         */
-        void openSignOutDialog(Activity activity);
-
-        /**
-         * Opens the sync customization preferences fragment.
-         * @param activity A preferences activity
-         * @param account An account to customize
-         */
-        void openSyncCustomizationFragment(Preferences activity, Account account);
-    }
-
-    /**
-     * Sets the account management fragment delegate.
-     * @param delegate An implementation of AccountManagementFragmentDelegate interface
-     */
-    public static void setDelegate(AccountManagementFragmentDelegate delegate) {
-        ThreadUtils.assertOnUiThread();
-        sDelegate = delegate;
-    }
-
-    /**
-     * @return An instance of the account management fragment delegate, or null.
-     */
-    public static AccountManagementFragmentDelegate getDelegate() {
-        return sDelegate;
-    }
-
     @CalledByNative
     private static void openAccountManagementScreen(
             Context applicationContext, Profile profile, int gaiaServiceType) {
@@ -83,7 +39,6 @@ public class AccountManagementScreenHelper {
             return;
         }
 
-        if (sDelegate == null) return;
         AccountManagementFragment.openAccountManagementScreen(
                 applicationContext, profile, gaiaServiceType);
     }
