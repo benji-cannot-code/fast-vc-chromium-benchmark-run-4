229942aeb11450790d8c0879d74b9c549df344ee
wittman <wittman@chromium.org>
2015-06-25 12:42:18 -0700

    Revert of [tracing] Send unique tracing process id for cross-process memory dumps identification. (patchset #15 id:320001 of https://codereview.chromium.org/1173263004/)
    Reason for revert:
    Appears to regress sizes
    
    PERF_REGRESS: chrome-si/initializers (14.29%), chrome-si/initializers (14.29%)
    
    https://build.chromium.org/p/chromium/builders/Linux%20x64/builds/5461
    
    Original issue's description:
    > [tracing] Send unique tracing process id for cross-process memory dumps identification.
    >
    > The memory shared between browser and renderer must be identified with
    > the same unique id for memory dumps in tracing. Currently the child
    > process is not aware of its unique child process id. But, the child
    > process id cannot be exposed in the child process (see discussion in
    > crrev.com/1155683009).
    >
    > So, a hash of the child process id allocated by the browser process is
    > sent to child process along with begin tracing message and will be
    > exposed in the child process MemoryDumpManager. The allocator that
    > needs to create a global guid will use the id to identify memory dumps.
    >
    > See the design doc https://goo.gl/ncMfUV for more details.
    >
    > BUG=497726
    >
    > Committed: https://crrev.com/a3e7c30a16e711b59226da21f993522ee47ac674
    > Cr-Commit-Position: refs/heads/master@{#336210}
    
    TBR=dcheng@chromium.org,jam@chromium.org,picksi@google.com,primiano@chromium.org,ssid@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=497726
    
    Review URL: https://codereview.chromium.org/1211883003
    
    Cr-Commit-Position: refs/heads/master@{#336232}

--- components/tracing/child_trace_message_filter.h
+++ components/tracing/child_trace_message_filter.h
@@ -42,8 +42,7 @@ class ChildTraceMessageFilter : public IPC::MessageFilter {
  private:
   // Message handlers.
   void OnBeginTracing(const std::string& trace_config_str,
-                      base::TraceTicks browser_time,
-                      int tracing_process_id);
+                      base::TraceTicks browser_time);
   void OnEndTracing();
   void OnCancelTracing();
   void OnEnableMonitoring(const std::string& trace_config_str,
--- content/browser/tracing/trace_message_filter.cc
+++ content/browser/tracing/trace_message_filter.cc
@@ -10,12 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-TraceMessageFilter::TraceMessageFilter(int child_process_id)
+TraceMessageFilter::TraceMessageFilter()
     : BrowserMessageFilter(TracingMsgStart),
       has_child_(false),
-      tracing_process_id_(
-          base::trace_event::MemoryDumpManager::
-              ChildProcessIdToTracingProcessId(child_process_id)),
       is_awaiting_end_ack_(false),
       is_awaiting_capture_monitoring_snapshot_ack_(false),
       is_awaiting_buffer_percent_full_ack_(false) {
@@ -67,8 +64,8 @@ bool TraceMessageFilter::OnMessageReceived(const IPC::Message& message) {
 void TraceMessageFilter::SendBeginTracing(
       const base::trace_event::TraceConfig& trace_config) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  Send(new TracingMsg_BeginTracing(
-      trace_config.ToString(), base::TraceTicks::Now(), tracing_process_id_));
+  Send(new TracingMsg_BeginTracing(trace_config.ToString(),
+                                   base::TraceTicks::Now()));
 }
 
 void TraceMessageFilter::SendEndTracing() {
