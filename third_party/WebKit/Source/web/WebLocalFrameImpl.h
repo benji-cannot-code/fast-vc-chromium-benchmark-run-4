9149af6fdfabfcdf430a7029226670a9949aa6cb
skobes@chromium.org <skobes@chromium.org>
2015-06-25 13:24:16 +0000

    Update WebLocalFrameImpl and RotationViewportAnchor for root layer scrolling.
    These classes now use the layout viewport ScrollableArea instead of FrameView.
    
    BUG=490942
    
    Review URL: https://codereview.chromium.org/1207103002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197820 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebLocalFrameImpl.h
+++ third_party/WebKit/Source/web/WebLocalFrameImpl.h
@@ -358,6 +358,7 @@ private:
     void loadJavaScriptURL(const KURL&);
 
     WebPlugin* focusedPluginIfInputMethodSupported();
+    ScrollableArea* layoutViewportScrollableArea() const;
 
     FrameLoaderClientImpl m_frameLoaderClientImpl;
 
