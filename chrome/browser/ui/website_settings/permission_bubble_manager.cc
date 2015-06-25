fc39e29ac8a1e2aaa576f2b719fb86e31138cc35
felt <felt@chromium.org>
2015-06-24 17:36:43 -0700

    Permanently switch from infobars to permission bubbles
    * Remove chrome://flags to control permission request UI
    * Remove Finch check from PermissionBubbleManager::Enabled()
    * Remove command line enable check from PermissionBubbleManager::Enabled()
    * Leave command line disable check in PermissionBubbleManager::Enabled()
    
    BUG=364686
    TEST=
    1. Set the command line as described
    2. Visit https://adrifelt.github.io/bubble-demo.html
    You should get the following combinations:
    * Bubble:   --force-fieldtrials=PermissionBubbleRollout/Disabled/
    * Bubble:   --force-fieldtrials=PermissionBubbleRollout/Enabled/
    * Bubble:   --enable-permissions-bubbles
    * Infobar:  --disable-permissions-bubbles
    
    Review URL: https://codereview.chromium.org/1178703005
    
    Cr-Commit-Position: refs/heads/master@{#336054}

--- chrome/browser/ui/website_settings/permission_bubble_manager.cc
+++ chrome/browser/ui/website_settings/permission_bubble_manager.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/website_settings/permission_bubble_manager.h"
 
 #include "base/command_line.h"
-#include "base/metrics/field_trial.h"
 #include "base/metrics/user_metrics_action.h"
 #include "chrome/browser/ui/website_settings/permission_bubble_request.h"
 #include "chrome/common/chrome_switches.h"
@@ -15,11 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/user_metrics.h"
 
 namespace {
-
-// String constants to control whether bubbles are enabled by default.
-const char kTrialName[] = "PermissionBubbleRollout";
-const char kEnabled[] = "Enabled";
-const char kDisabled[] = "Disabled";
 
 class CancelledRequest : public PermissionBubbleRequest {
  public:
@@ -70,21 +64,13 @@ DEFINE_WEB_CONTENTS_USER_DATA_KEY(PermissionBubbleManager);
 
 // static
 bool PermissionBubbleManager::Enabled() {
-  // Command line flags take precedence.
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kEnablePermissionsBubbles))
-    return true;
+#if defined(OS_ANDROID) || defined(OS_IOS)
+  return false;
+#endif
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kDisablePermissionsBubbles))
     return false;
-
-  std::string group(base::FieldTrialList::FindFullName(kTrialName));
-  if (group == kEnabled)
-    return true;
-  if (group == kDisabled)
-    return false;
-
-  return false;
+  return true;
 }
 
 PermissionBubbleManager::PermissionBubbleManager(
--- chrome/browser/ui/website_settings/permission_bubble_manager_unittest.cc
+++ chrome/browser/ui/website_settings/permission_bubble_manager_unittest.cc
@@ -117,10 +117,10 @@ class PermissionBubbleManagerTest : public ChromeRenderViewHostTestHarness {
 };
 
 TEST_F(PermissionBubbleManagerTest, TestFlag) {
-  EXPECT_FALSE(PermissionBubbleManager::Enabled());
-  base::CommandLine::ForCurrentProcess()->AppendSwitch(
-      switches::kEnablePermissionsBubbles);
   EXPECT_TRUE(PermissionBubbleManager::Enabled());
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kDisablePermissionsBubbles);
+  EXPECT_FALSE(PermissionBubbleManager::Enabled());
 }
 
 TEST_F(PermissionBubbleManagerTest, SingleRequest) {
@@ -560,4 +560,3 @@ TEST_F(PermissionBubbleManagerTest, RequestsDontNeedUserGesture) {
 
   EXPECT_TRUE(view_.shown_);
 }
-
