0aa9cbf3e531e9eb64104c90e8287dc96d43e7e7
tapted <tapted@chromium.org>
2015-06-24 18:07:08 -0700

    MacViewsBrowser: Fix linking for CreateSavePasswordInfoBar
    Mac Views browser link regressed in r334182, with
    
    Undefined symbols for architecture x86_64:
      "CreateSavePasswordInfoBar(..)", referenced from:
          SavePasswordInfoBarDelegate::Create(..)
    
    This moves the #ifdefs into platform-specific files so that Mac can link
    in either views or Cocoa code.
    
    BUG=425229
    
    Review URL: https://codereview.chromium.org/1193913007
    
    Cr-Commit-Position: refs/heads/master@{#336059}

--- chrome/browser/ui/cocoa/infobars/save_password_infobar_controller.mm
+++ chrome/browser/ui/cocoa/infobars/save_password_infobar_controller.mm
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/cocoa/infobars/save_password_infobar_controller.h"
-
 #include "chrome/browser/password_manager/save_password_infobar_delegate.h"
 #include "chrome/browser/ui/chrome_style.h"
 #include "chrome/browser/ui/cocoa/infobars/confirm_infobar_controller.h"
---
+++ chrome/browser/ui/views/infobars/save_password_infobar.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/password_manager/save_password_infobar_delegate.h"
+#include "chrome/browser/ui/views/infobars/confirm_infobar.h"
+
+scoped_ptr<infobars::InfoBar> CreateSavePasswordInfoBar(
+    scoped_ptr<SavePasswordInfoBarDelegate> delegate) {
+  return make_scoped_ptr(new ConfirmInfoBar(delegate.Pass()));
+}
