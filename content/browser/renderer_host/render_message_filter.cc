834b17f6888a3bd5d3aee6173732037a97056665
mcasas <mcasas@chromium.org>
2015-06-25 11:31:54 -0700

    Reland: Video Capture: extract storage info from pixel format in VideoCaptureFormat.
    Relanding https://codereview.chromium.org/1179323002/ with dcheng@
    comments addressed.
    
    Original description ---------------------------------------------------
    Video Capture: extract storage info from pixel format in VideoCaptureFormat.
    
    Video Capture Devices treat both Texture and GpuMemoryBuffer
    as a VideoPixelFormat, but they are storage types. Moreover,
    this merging prevents capture devices from indicating a
    combination of Storage and Capture formats, i.e.
    Texture + ARGB, or GpuMemoryBuffer+YUY2.
    
    This CL separates both concepts and updates necessary
    call points. It also extends the translation
    VideoCaptureFormat --> VideoFrame in
    VideoCaptureDeviceClient and in VideoCaptureBufferPool.
    VideoCaptureBufferPool::ReserveOutputBuffer()
    also gets a param to specify the StorageType.
    
    This separation also allows for VideoCaptureBufferPool
    to operate using VideoPixel{Format, Storage} ISO
    VideoFrame types, which spares the constant conversion
    between ones and others.
    
    Test: All video captures working exactly as before.
    BUG=440843
    --------------------------------------------------------------------------
    
    TBR=dalecurtis@chromium.org, miu@chromium.org, hubbe@chromium.org
    Rationale: they already LGTM PS1 and the changes
    from there onwards are either cosmetic (git cl format,
    DCHECK_EQ(expected, actual)) or discussed with dcheng@.
    
    Review URL: https://codereview.chromium.org/1204063005
    
    Cr-Commit-Position: refs/heads/master@{#336208}

--- content/browser/renderer_host/render_message_filter.cc
+++ content/browser/renderer_host/render_message_filter.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/frame_messages.h"
 #include "content/common/gpu/client/gpu_memory_buffer_impl.h"
 #include "content/common/host_shared_bitmap_manager.h"
-#include "content/common/media/media_param_traits.h"
 #include "content/common/view_messages.h"
 #include "content/public/browser/browser_child_process_host.h"
 #include "content/public/browser/browser_context.h"
