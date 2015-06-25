a979645a8e8ca30bb3ea4edf68a3aeed13cb18cd
sdefresne <sdefresne@chromium.org>
2015-06-25 06:25:30 -0700

    Expand KeyedServiceProvider interface.
    Add method GetHistoryServiceForBrowserStateIfExists that do not create the
    HistoryService instance if it does not exists.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1206303002
    
    Cr-Commit-Position: refs/heads/master@{#336136}

--- ios/public/test/test_keyed_service_provider.cc
+++ ios/public/test/test_keyed_service_provider.cc
@@ -146,4 +146,11 @@ TestKeyedServiceProvider::GetHistoryServiceForBrowserState(
   return nullptr;
 }
 
+history::HistoryService*
+TestKeyedServiceProvider::GetHistoryServiceForBrowserStateIfExists(
+    ChromeBrowserState* browser_state,
+    ServiceAccessType access_type) {
+  return nullptr;
+}
+
 }  // namespace ios
