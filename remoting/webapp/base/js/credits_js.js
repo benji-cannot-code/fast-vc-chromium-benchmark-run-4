f5e0fe9cef4e8d4cfa656646f419282f9bb0d49d
jamiewalch <jamiewalch@chromium.org>
2015-06-24 19:35:23 -0700

    Hide collapsed licenses when one is expanded.
    This works around a bug in Chrome on Android where the scroll position is
    lost when a license is expanded. The UX is fine on desktop too, so this
    applies cross-platform.
    
    BUG=499478
    
    Review URL: https://codereview.chromium.org/1207853002
    
    Cr-Commit-Position: refs/heads/master@{#336078}

--- remoting/webapp/base/js/credits_js.js
+++ remoting/webapp/base/js/credits_js.js
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function onClick(event) {
   var element = /** @type {HTMLElement} */ (event.target);
   if (element.classList.contains('toggle-licence')) {
-    element.classList.toggle('hide-licence');
+    element.parentElement.parentElement.classList.toggle('selected');
+    document.body.classList.toggle('show-only-selected');
   }
 }
 
