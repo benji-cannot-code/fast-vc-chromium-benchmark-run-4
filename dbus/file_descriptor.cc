e4b91a0c546b7fa06e1ed51373aa4f422c725e69
reillyg <reillyg@chromium.org>
2015-06-25 15:27:57 -0700

    Don't attempt to validate invalid DBus-passed file descriptors.
    If an invalid file descriptor (i.e. -1) is passed over DBus then it is
    impossible to determine if it is valid because the value() accessor
    requires CheckValidity() to be called first and this will trigger a
    DCHECK in base::File::GetInfo() because the file descriptor is invalid.
    The solution is to abort the validity check if file.IsValid() is false.
    This way after calling CheckValidity() is_valid() will indicate either
    that the file descriptor was unsafe (i.e. a directory) or invalid (i.e.
    -1).
    
    BUG=496469
    
    Review URL: https://codereview.chromium.org/1211093002
    
    Cr-Commit-Position: refs/heads/master@{#336276}

--- dbus/file_descriptor.cc
+++ dbus/file_descriptor.cc
@@ -50,6 +50,11 @@ int FileDescriptor::TakeValue() {
 
 void FileDescriptor::CheckValidity() {
   base::File file(value_);
+  if (!file.IsValid()) {
+    valid_ = false;
+    return;
+  }
+
   base::File::Info info;
   bool ok = file.GetInfo(&info);
   file.TakePlatformFile();  // Prevent |value_| from being closed by |file|.
