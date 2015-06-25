54998648ff18d7ad9183cf57971b4474764e80c7
mstensho@opera.com <mstensho@opera.com>
2015-06-25 15:50:24 +0000

    Add isMultiColumnContainer() for convenience and readability.
    R=dsinclair@chromium.org
    
    Review URL: https://codereview.chromium.org/1210803002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197828 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutMultiColumnFlowThread.cpp
+++ third_party/WebKit/Source/core/layout/LayoutMultiColumnFlowThread.cpp
@@ -74,6 +74,13 @@ LayoutMultiColumnSet* LayoutMultiColumnFlowThread::lastMultiColumnSet() const
     return nullptr;
 }
 
+static inline bool isMultiColumnContainer(const LayoutObject& object)
+{
+    if (!object.isLayoutBlockFlow())
+        return false;
+    return toLayoutBlockFlow(object).multiColumnFlowThread();
+}
+
 // Find the next layout object that has the multicol container in its containing block chain, skipping nested multicol containers.
 static LayoutObject* nextInPreOrderAfterChildrenSkippingOutOfFlow(LayoutMultiColumnFlowThread* flowThread, LayoutObject* descendant)
 {
@@ -96,7 +103,7 @@ static LayoutObject* nextInPreOrderAfterChildrenSkippingOutOfFlow(LayoutMultiCol
 #if ENABLE(ASSERT)
     // Make sure that we didn't stumble into an inner multicol container.
     for (LayoutObject* walker = object->parent(); walker && walker != flowThread; walker = walker->parent())
-        ASSERT(!walker->isLayoutBlockFlow() || !toLayoutBlockFlow(walker)->multiColumnFlowThread());
+        ASSERT(!isMultiColumnContainer(*walker));
 #endif
     return object;
 }
@@ -121,7 +128,7 @@ static LayoutObject* previousInPreOrderSkippingOutOfFlow(LayoutMultiColumnFlowTh
             for (ancestor = object->parent(); ; ancestor = ancestor->parent()) {
                 if (ancestor == flowThread)
                     return object;
-                if (ancestor->isLayoutBlockFlow() && toLayoutBlockFlow(ancestor)->multiColumnFlowThread()) {
+                if (isMultiColumnContainer(*ancestor)) {
                     // We're inside an inner multicol container. We have no business there.
                     break;
                 }
@@ -138,7 +145,7 @@ static LayoutObject* previousInPreOrderSkippingOutOfFlow(LayoutMultiColumnFlowTh
 #if ENABLE(ASSERT)
     // Make sure that we didn't stumble into an inner multicol container.
     for (LayoutObject* walker = object->parent(); walker && walker != flowThread; walker = walker->parent())
-        ASSERT(!walker->isLayoutBlockFlow() || !toLayoutBlockFlow(walker)->multiColumnFlowThread());
+        ASSERT(!isMultiColumnContainer(*walker));
 #endif
     return object;
 }
