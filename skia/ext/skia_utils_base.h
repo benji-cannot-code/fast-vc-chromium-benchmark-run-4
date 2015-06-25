94eef59ac1fa2d2e982a512130ca8cda4d86124e
robertphillips <robertphillips@google.com>
2015-06-25 09:47:41 -0700

    Add ComputeDefaultPixelGeometry helper to skia/ext
    BUG=skia:3934
    
    Review URL: https://codereview.chromium.org/1199543002
    
    Cr-Commit-Position: refs/heads/master@{#336174}

--- skia/ext/skia_utils_base.h
+++ skia/ext/skia_utils_base.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/pickle.h"
 #include "third_party/skia/include/ports/SkFontConfigInterface.h"
+#include "third_party/skia/include/core/SkFontHost.h"
+#include "third_party/skia/include/core/SkSurfaceProps.h"
 
 namespace skia {
 
@@ -27,6 +29,9 @@ SK_API bool WriteSkString(base::Pickle* pickle, const SkString& str);
 SK_API bool WriteSkFontIdentity(
     base::Pickle* pickle,
     const SkFontConfigInterface::FontIdentity& identity);
+
+// Determine the default pixel geometry (for LCD) by querying the font host
+SK_API SkPixelGeometry ComputeDefaultPixelGeometry();
 
 }  // namespace skia
 
