ae025759d4951681f7ccf012fddc830147826022
benchan <benchan@chromium.org>
2015-06-26 11:46:30 -0700

    Remove the obsolete Metronome DBus client binding.
    The Metronome DBus service is obsolete and no longer available on the
    Chrome OS side. This CL removes the Metronome DBus client binding.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1213693002
    
    Cr-Commit-Position: refs/heads/master@{#336423}

--- chromeos/dbus/dbus_client_bundle.cc
+++ chromeos/dbus/dbus_client_bundle.cc
@@ -77,7 +77,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/introspectable_client.h"
 #include "chromeos/dbus/leadership_daemon_manager_client.h"
 #include "chromeos/dbus/lorgnette_manager_client.h"
-#include "chromeos/dbus/metronome_client.h"
 #include "chromeos/dbus/modem_messaging_client.h"
 #include "chromeos/dbus/nfc_adapter_client.h"
 #include "chromeos/dbus/nfc_device_client.h"
@@ -120,7 +119,6 @@ const struct {
     { "easy_unlock",  DBusClientBundle::EASY_UNLOCK },
     { "leadership_daemon",  DBusClientBundle::LEADERSHIP_DAEMON },
     { "lorgnette_manager",  DBusClientBundle::LORGNETTE_MANAGER },
-    { "metronome",  DBusClientBundle::METRONOME },
     { "shill",  DBusClientBundle::SHILL },
     { "gsm_sms",  DBusClientBundle::GSM_SMS },
     { "image_burner",  DBusClientBundle::IMAGE_BURNER },
@@ -232,10 +230,6 @@ DBusClientBundle::DBusClientBundle(DBusClientTypeMask unstub_client_mask)
     lorgnette_manager_client_.reset(LorgnetteManagerClient::Create());
   else
     lorgnette_manager_client_.reset(new FakeLorgnetteManagerClient);
-
-  metronome_client_.reset(MetronomeClient::Create(
-      IsUsingStub(METRONOME) ? STUB_DBUS_CLIENT_IMPLEMENTATION
-                             : REAL_DBUS_CLIENT_IMPLEMENTATION));
 
   if (!IsUsingStub(SHILL)) {
     shill_manager_client_.reset(ShillManagerClient::Create());
