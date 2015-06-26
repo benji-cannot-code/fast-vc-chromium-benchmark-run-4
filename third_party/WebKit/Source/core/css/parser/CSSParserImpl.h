fd75d4abfb6c8c53402e0938e81c6369dafd9248
pfeldman@chromium.org <pfeldman@chromium.org>
2015-06-26 13:50:48 +0000

    DevTools: allow injecting CSS rules without breaking styles sidebar.
    R=lushnikov
    BUG=178410
    
    Review URL: https://codereview.chromium.org/1211813002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197913 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/CSSParserImpl.h
+++ third_party/WebKit/Source/core/css/parser/CSSParserImpl.h
@@ -66,7 +66,7 @@ public:
     bool supportsDeclaration(CSSParserTokenRange&);
 
     static void parseDeclarationListForInspector(const String&, const CSSParserContext&, CSSParserObserver&);
-    static void parseStyleSheetForInspector(const String&, const CSSParserContext&, CSSParserObserver&);
+    static void parseStyleSheetForInspector(const String&, const CSSParserContext&, StyleSheetContents*, CSSParserObserver&);
 
 private:
     enum RuleListType {
