ad5712408bd2a7a43c9b8583ff33097406e5b8b8
jkarlin <jkarlin@chromium.org>
2015-06-26 05:39:14 -0700

    Make CacheStorageBlobToDiskCache cancel requests at RequestContextGetter deletion
    CacheStorageBlobToDiskCache is owned by the CacheStorageCache which is
    ref counted and whose lifetime is potentially beyond that of the
    RequestContextGetter. As such, listen for RequestContextGetter
    deletion and cancel any requests upon deletion.
    
    BUG=493291
    
    Review URL: https://codereview.chromium.org/1204303002
    
    Cr-Commit-Position: refs/heads/master@{#336359}

--- content/browser/cache_storage/cache_storage_blob_to_disk_cache.cc
+++ content/browser/cache_storage/cache_storage_blob_to_disk_cache.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cache_storage_blob_to_disk_cache.h"
 
+#include "base/logging.h"
 #include "net/base/io_buffer.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_getter.h"
@@ -23,6 +24,8 @@ CacheStorageBlobToDiskCache::CacheStorageBlobToDiskCache()
 }
 
 CacheStorageBlobToDiskCache::~CacheStorageBlobToDiskCache() {
+  if (blob_request_)
+    request_context_getter_->RemoveObserver(this);
 }
 
 void CacheStorageBlobToDiskCache::StreamBlobToCache(
@@ -41,16 +44,18 @@ void CacheStorageBlobToDiskCache::StreamBlobToCache(
 
   entry_ = entry.Pass();
   callback_ = callback;
+  request_context_getter_ = request_context_getter;
 
   blob_request_ = storage::BlobProtocolHandler::CreateBlobRequest(
       blob_data_handle.Pass(), request_context_getter->GetURLRequestContext(),
       this);
+  request_context_getter_->AddObserver(this);
   blob_request_->Start();
 }
 
 void CacheStorageBlobToDiskCache::OnResponseStarted(net::URLRequest* request) {
   if (!request->status().is_success()) {
-    callback_.Run(entry_.Pass(), false);
+    RunCallbackAndRemoveObserver(false);
     return;
   }
 
@@ -60,12 +65,12 @@ void CacheStorageBlobToDiskCache::OnResponseStarted(net::URLRequest* request) {
 void CacheStorageBlobToDiskCache::OnReadCompleted(net::URLRequest* request,
                                                   int bytes_read) {
   if (!request->status().is_success()) {
-    callback_.Run(entry_.Pass(), false);
+    RunCallbackAndRemoveObserver(false);
     return;
   }
 
   if (bytes_read == 0) {
-    callback_.Run(entry_.Pass(), true);
+    RunCallbackAndRemoveObserver(true);
     return;
   }
 
@@ -108,6 +113,11 @@ void CacheStorageBlobToDiskCache::OnBeforeNetworkStart(net::URLRequest* request,
   NOTREACHED();
 }
 
+void CacheStorageBlobToDiskCache::OnContextShuttingDown() {
+  DCHECK(blob_request_);
+  RunCallbackAndRemoveObserver(false);
+}
+
 void CacheStorageBlobToDiskCache::ReadFromBlob() {
   int bytes_read = 0;
   bool done = blob_request_->Read(buffer_.get(), buffer_->size(), &bytes_read);
@@ -118,12 +128,20 @@ void CacheStorageBlobToDiskCache::ReadFromBlob() {
 void CacheStorageBlobToDiskCache::DidWriteDataToEntry(int expected_bytes,
                                                       int rv) {
   if (rv != expected_bytes) {
-    callback_.Run(entry_.Pass(), false);
+    RunCallbackAndRemoveObserver(false);
     return;
   }
 
   cache_entry_offset_ += rv;
   ReadFromBlob();
+}
+
+void CacheStorageBlobToDiskCache::RunCallbackAndRemoveObserver(bool success) {
+  DCHECK(request_context_getter_);
+
+  request_context_getter_->RemoveObserver(this);
+  blob_request_.reset();
+  callback_.Run(entry_.Pass(), success);
 }
 
 }  // namespace content
--- content/browser/cache_storage/cache_storage_blob_to_disk_cache.h
+++ content/browser/cache_storage/cache_storage_blob_to_disk_cache.h
@@ -7,11 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_CACHE_STORAGE_CACHE_STORAGE_BLOB_TO_DISK_CACHE_H_
 
 #include "base/callback.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "content/common/content_export.h"
 #include "net/disk_cache/disk_cache.h"
 #include "net/url_request/url_request.h"
+#include "net/url_request/url_request_context_getter_observer.h"
 
 namespace net {
 class IOBufferWithSize;
@@ -26,7 +28,8 @@ namespace content {
 
 // Streams data from a blob and writes it to a given disk_cache::Entry.
 class CONTENT_EXPORT CacheStorageBlobToDiskCache
-    : public net::URLRequest::Delegate {
+    : public net::URLRequest::Delegate,
+      public net::URLRequestContextGetterObserver {
  public:
   using EntryAndBoolCallback =
       base::Callback<void(disk_cache::ScopedEntryPtr, bool)>;
@@ -63,12 +66,21 @@ class CONTENT_EXPORT CacheStorageBlobToDiskCache
                              bool fatal) override;
   void OnBeforeNetworkStart(net::URLRequest* request, bool* defer) override;
 
+  // URLRequestContextGetterObserver override for canceling requests just
+  // before the URLRequestContext is destroyed.
+  void OnContextShuttingDown() override;
+
+ protected:
+  // Virtual for testing.
+  virtual void ReadFromBlob();
+
  private:
-  void ReadFromBlob();
   void DidWriteDataToEntry(int expected_bytes, int rv);
+  void RunCallbackAndRemoveObserver(bool success);
 
   int cache_entry_offset_;
   disk_cache::ScopedEntryPtr entry_;
+  scoped_refptr<net::URLRequestContextGetter> request_context_getter_;
   int disk_cache_body_index_;
   scoped_ptr<net::URLRequest> blob_request_;
   EntryAndBoolCallback callback_;
