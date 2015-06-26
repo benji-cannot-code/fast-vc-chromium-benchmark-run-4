2ff9eedcf62c052368a944a6bd5ed9c5a6beb229
horo@chromium.org <horo@chromium.org>
2015-06-26 06:00:33 +0000

    Add LayoutTest for ServiceWorker's sandbox iframe handling.
    This is a test for https://codereview.chromium.org/1191293002/
    
    BUG=486308
    
    Review URL: https://codereview.chromium.org/1208693003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197892 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/chromium/resources/sandboxed-iframe-fetch-event-worker.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var requests = [];
+
+self.addEventListener('message', function(event) {
+    self.clients.matchAll()
+      .then(function(clients) {
+          var client_urls = [];
+          for(var client of clients){
+            client_urls.push(client.url);
+          }
+          client_urls = client_urls.sort();
+          event.data.port.postMessage(
+              {clients: client_urls, requests: requests});
+        });
+  });
+
+self.addEventListener('fetch', function(event) {
+    requests.push(event.request.url);
+    event.respondWith(fetch(event.request));
+  });
