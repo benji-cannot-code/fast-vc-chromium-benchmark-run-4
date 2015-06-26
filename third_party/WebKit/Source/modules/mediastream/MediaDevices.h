1327055684ae4d4751ffdba68cb55fb373eb0023
hta@chromium.org <hta@chromium.org>
2015-06-26 14:31:46 +0000

    Implement navigator.mediaDevices.getUserMedia()
    This makes a promise-based API for acquiring media available.
    
    The implementation is still hidden behind the flag that guards MediaDevices (EnumerateDevices), so is not immediately available to users.
    
    R=tommi
    BUG=503227
    
    Review URL: https://codereview.chromium.org/1202553002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197920 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/mediastream/MediaDevices.h
+++ third_party/WebKit/Source/modules/mediastream/MediaDevices.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class Dictionary;
 class ScriptState;
 
 class MediaDevices final : public GarbageCollected<MediaDevices>, public ScriptWrappable {
@@ -22,7 +23,7 @@ public:
     }
 
     ScriptPromise enumerateDevices(ScriptState*);
-
+    ScriptPromise getUserMedia(ScriptState*, const Dictionary&, ExceptionState&);
     DEFINE_INLINE_TRACE() { }
 
 private:
