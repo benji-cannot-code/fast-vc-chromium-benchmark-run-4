5f6c526c5e844bc8522995be34bb08549088338b
felt <felt@chromium.org>
2015-06-26 09:10:31 -0700

    Remove infobars from notifications interative_ui_tests
    Now that bubbles are the permanent permission request UI for desktop, the
    tests no longer need to test infobars. This removes the infobar test code from
    the notification interactive_ui_tests (NotificationsTest.* and
    PlatformNotificationService.*).
    
    BUG=502515
    
    Review URL: https://codereview.chromium.org/1214623002
    
    Cr-Commit-Position: refs/heads/master@{#336379}

--- chrome/browser/notifications/platform_notification_service_browsertest.cc
+++ chrome/browser/notifications/platform_notification_service_browsertest.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/path_service.h"
 #include "base/strings/utf_string_conversions.h"
-#include "chrome/browser/infobars/infobar_responder.h"
-#include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/notifications/desktop_notification_profile_util.h"
 #include "chrome/browser/notifications/desktop_notification_service.h"
 #include "chrome/browser/notifications/desktop_notification_service_factory.h"
@@ -75,8 +73,7 @@ class PlatformNotificationServiceBrowserTest : public InProcessBrowserTest {
 
  private:
   std::string RequestAndRespondToPermission(
-      PermissionBubbleManager::AutoResponseType bubble_response,
-      InfoBarResponder::AutoResponseType infobar_response);
+      PermissionBubbleManager::AutoResponseType bubble_response);
 
   content::WebContents* GetActiveWebContents(Browser* browser) {
     return browser->tab_strip_model()->GetActiveWebContents();
@@ -158,31 +155,24 @@ GURL PlatformNotificationServiceBrowserTest::TestPageUrl() const {
 
 std::string
 PlatformNotificationServiceBrowserTest::RequestAndRespondToPermission(
-    PermissionBubbleManager::AutoResponseType bubble_response,
-    InfoBarResponder::AutoResponseType infobar_response) {
+    PermissionBubbleManager::AutoResponseType bubble_response) {
   std::string result;
   content::WebContents* web_contents = GetActiveWebContents(browser());
-  scoped_ptr<InfoBarResponder> infobar_responder;
-  if (PermissionBubbleManager::Enabled()) {
-    PermissionBubbleManager::FromWebContents(web_contents)
-        ->set_auto_response_for_test(bubble_response);
-  } else {
-    infobar_responder.reset(new InfoBarResponder(
-        InfoBarService::FromWebContents(web_contents), infobar_response));
-  }
+  PermissionBubbleManager::FromWebContents(web_contents)
+      ->set_auto_response_for_test(bubble_response);
   EXPECT_TRUE(RunScript("RequestPermission();", &result));
   return result;
 }
 
 bool PlatformNotificationServiceBrowserTest::RequestAndAcceptPermission() {
-  std::string result = RequestAndRespondToPermission(
-      PermissionBubbleManager::ACCEPT_ALL, InfoBarResponder::ACCEPT);
+  std::string result =
+      RequestAndRespondToPermission(PermissionBubbleManager::ACCEPT_ALL);
   return "granted" == result;
 }
 
 bool PlatformNotificationServiceBrowserTest::RequestAndDenyPermission() {
-  std::string result = RequestAndRespondToPermission(
-      PermissionBubbleManager::DENY_ALL, InfoBarResponder::DENY);
+  std::string result =
+      RequestAndRespondToPermission(PermissionBubbleManager::DENY_ALL);
   return "denied" == result;
 }
 
