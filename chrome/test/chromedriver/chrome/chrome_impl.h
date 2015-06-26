810665e272d9cb1f5623693cbc88e1245a6878c7
samuong <samuong@chromium.org>
2015-06-26 12:39:13 -0700

    [chromedriver] When launching a Chrome App, automatically switch to the new window.
    BUG=chromedriver:1130
    
    Review URL: https://codereview.chromium.org/1196663005
    
    Cr-Commit-Position: refs/heads/master@{#336444}

--- chrome/test/chromedriver/chrome/chrome_impl.h
+++ chrome/test/chromedriver/chrome/chrome_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <list>
 #include <string>
 
+#include "base/callback.h"
 #include "base/compiler_specific.h"
 #include "base/memory/linked_ptr.h"
 #include "base/memory/scoped_ptr.h"
@@ -25,6 +26,9 @@ class PortReservation;
 class Status;
 class WebView;
 class WebViewImpl;
+struct WebViewInfo;
+
+typedef base::Callback<void(const WebViewInfo&)> WebViewCallback;
 
 class ChromeImpl : public Chrome {
  public:
@@ -48,6 +52,9 @@ class ChromeImpl : public Chrome {
       scoped_ptr<DevToolsClient> websocket_client,
       ScopedVector<DevToolsEventListener>& devtools_event_listeners,
       scoped_ptr<PortReservation> port_reservation);
+
+  Status UpdateWebViewIds(std::list<std::string>* web_view_ids,
+                          const WebViewCallback& callback);
 
   virtual Status QuitImpl() = 0;
 
