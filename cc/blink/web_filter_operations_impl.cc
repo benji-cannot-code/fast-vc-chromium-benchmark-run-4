786a5a0b9a6dc5dc95e35fd6adf6af6e883ee024
ajuma <ajuma@chromium.org>
2015-06-26 14:33:35 -0700

    Add an isEmpty() method to WebFilterOperationsImpl
    This is a prerequiste for a Blink-side change
    (http://crrev.com/1214673005) that involves checking whether
    a WebFilterOperations instance is empty.
    
    BUG=502026
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1218543002
    
    Cr-Commit-Position: refs/heads/master@{#336458}

--- cc/blink/web_filter_operations_impl.cc
+++ cc/blink/web_filter_operations_impl.cc
@@ -93,4 +93,8 @@ void WebFilterOperationsImpl::clear() {
   filter_operations_.Clear();
 }
 
+bool WebFilterOperationsImpl::isEmpty() const {
+  return filter_operations_.IsEmpty();
+}
+
 }  // namespace cc_blink
