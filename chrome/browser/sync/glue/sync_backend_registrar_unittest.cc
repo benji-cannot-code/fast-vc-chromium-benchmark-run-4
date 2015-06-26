ecab5d24ca3e28c4ff77fdfd4a1b92c757018272
kinuko <kinuko@chromium.org>
2015-06-25 18:28:49 -0700

    Fix DCHECK crash in SyncBackendRegistrar test
    Make sure the thread's started before we call RunUntilIdle()
    in SyncBackendRegistrarTest::TearDown().
    
    Also re-enable the test so that we can easily catch if it's
    still broken.
    
    BUG=498238
    TEST=SyncBackendRegistrarTest.*
    
    Review URL: https://codereview.chromium.org/1202383008
    
    Cr-Commit-Position: refs/heads/master@{#336315}

--- chrome/browser/sync/glue/sync_backend_registrar_unittest.cc
+++ chrome/browser/sync/glue/sync_backend_registrar_unittest.cc
@@ -46,7 +46,7 @@ void TriggerChanges(SyncBackendRegistrar* registrar, ModelType type) {
 }
 
 // Flaky: https://crbug.com/498238
-class DISABLED_SyncBackendRegistrarTest : public testing::Test {
+class SyncBackendRegistrarTest : public testing::Test {
  public:
   void TestNonUIDataTypeActivationAsync(sync_driver::ChangeProcessor* processor,
                                         base::WaitableEvent* done) {
@@ -63,13 +63,13 @@ class DISABLED_SyncBackendRegistrarTest : public testing::Test {
   }
 
  protected:
-  DISABLED_SyncBackendRegistrarTest()
+  SyncBackendRegistrarTest()
       : thread_bundle_(content::TestBrowserThreadBundle::REAL_DB_THREAD |
                        content::TestBrowserThreadBundle::REAL_FILE_THREAD |
                        content::TestBrowserThreadBundle::REAL_IO_THREAD),
         sync_thread_(NULL) {}
 
-  ~DISABLED_SyncBackendRegistrarTest() override {}
+  ~SyncBackendRegistrarTest() override {}
 
   void SetUp() override {
     test_user_share_.SetUp();
@@ -84,6 +84,7 @@ class DISABLED_SyncBackendRegistrarTest : public testing::Test {
     sync_thread_->task_runner()->PostTask(
         FROM_HERE, base::Bind(&SyncBackendRegistrar::Shutdown,
                               base::Unretained(registrar_.release())));
+    sync_thread_->WaitUntilThreadStarted();
     sync_thread_->message_loop()->RunUntilIdle();
   }
 
@@ -112,7 +113,7 @@ class DISABLED_SyncBackendRegistrarTest : public testing::Test {
   base::Thread* sync_thread_;
 };
 
-TEST_F(DISABLED_SyncBackendRegistrarTest, ConstructorEmpty) {
+TEST_F(SyncBackendRegistrarTest, ConstructorEmpty) {
   registrar_->SetInitialTypes(ModelTypeSet());
   EXPECT_FALSE(registrar_->IsNigoriEnabled());
   {
@@ -124,7 +125,7 @@ TEST_F(DISABLED_SyncBackendRegistrarTest, ConstructorEmpty) {
   ExpectHasProcessorsForTypes(*registrar_, ModelTypeSet());
 }
 
-TEST_F(DISABLED_SyncBackendRegistrarTest, ConstructorNonEmpty) {
+TEST_F(SyncBackendRegistrarTest, ConstructorNonEmpty) {
   const ModelTypeSet initial_types(BOOKMARKS, NIGORI, PASSWORDS);
   registrar_->SetInitialTypes(initial_types);
   EXPECT_TRUE(registrar_->IsNigoriEnabled());
@@ -143,7 +144,7 @@ TEST_F(DISABLED_SyncBackendRegistrarTest, ConstructorNonEmpty) {
   ExpectHasProcessorsForTypes(*registrar_, ModelTypeSet());
 }
 
-TEST_F(DISABLED_SyncBackendRegistrarTest, ConfigureDataTypes) {
+TEST_F(SyncBackendRegistrarTest, ConfigureDataTypes) {
   registrar_->SetInitialTypes(ModelTypeSet());
 
   // Add.
@@ -179,7 +180,7 @@ TEST_F(DISABLED_SyncBackendRegistrarTest, ConfigureDataTypes) {
   EXPECT_TRUE(ModelTypeSet().Equals(registrar_->GetLastConfiguredTypes()));
 }
 
-TEST_F(DISABLED_SyncBackendRegistrarTest, ActivateDeactivateUIDataType) {
+TEST_F(SyncBackendRegistrarTest, ActivateDeactivateUIDataType) {
   InSequence in_sequence;
   registrar_->SetInitialTypes(ModelTypeSet());
 
@@ -220,7 +221,7 @@ TEST_F(DISABLED_SyncBackendRegistrarTest, ActivateDeactivateUIDataType) {
   TriggerChanges(registrar_.get(), BOOKMARKS);
 }
 
-TEST_F(DISABLED_SyncBackendRegistrarTest, ActivateDeactivateNonUIDataType) {
+TEST_F(SyncBackendRegistrarTest, ActivateDeactivateNonUIDataType) {
   InSequence in_sequence;
   registrar_->SetInitialTypes(ModelTypeSet());
 
@@ -246,8 +247,7 @@ TEST_F(DISABLED_SyncBackendRegistrarTest, ActivateDeactivateNonUIDataType) {
   BrowserThread::PostTask(
       BrowserThread::DB,
       FROM_HERE,
-      base::Bind(
-          &DISABLED_SyncBackendRegistrarTest::TestNonUIDataTypeActivationAsync,
+      base::Bind(&SyncBackendRegistrarTest::TestNonUIDataTypeActivationAsync,
           base::Unretained(this),
           &change_processor_mock,
           &done));
