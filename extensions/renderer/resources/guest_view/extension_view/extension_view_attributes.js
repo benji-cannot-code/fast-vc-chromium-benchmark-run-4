e7d58bacf5af6132874c2f3b6bfe0dedf926aedc
apacible <apacible@chromium.org>
2015-06-26 18:22:43 -0700

    extensionview: Show message in console when setting src is attempted.
    Shows a message that the src attribute is read only and to use the load() function.
    
    Review URL: https://codereview.chromium.org/1215873002
    
    Cr-Commit-Position: refs/heads/master@{#336485}

--- extensions/renderer/resources/guest_view/extension_view/extension_view_attributes.js
+++ extensions/renderer/resources/guest_view/extension_view/extension_view_attributes.js
@@ -39,6 +39,12 @@ function SrcAttribute(view) {
 SrcAttribute.prototype.__proto__ =
     GuestViewAttributes.ReadOnlyAttribute.prototype;
 
+SrcAttribute.prototype.handleMutation = function(oldValue, newValue) {
+  console.log('src is read only. Use .load(url) to navigate to a new ' +
+      'extension page.');
+  this.setValueIgnoreMutation(oldValue);
+}
+
 // -----------------------------------------------------------------------------
 
 // Sets up all of the extensionview attributes.
