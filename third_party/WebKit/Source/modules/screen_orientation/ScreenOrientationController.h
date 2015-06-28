66b169b2e724fa033abfe3ee321844ae7be868f4
amineer@google.com <amineer@google.com>
2015-06-28 21:58:29 +0000

    Revert of [DevTools] Implement screen orientation override. (patchset #3 id:40001 of https://codereview.chromium.org/1195793008/)
    Reason for revert:
    Causing crashes
    BUG=505293
    
    Original issue's description:
    > [DevTools] Implement screen orientation override.
    > 
    > BUG=427199
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=197908
    
    TBR=mlamouri@chromium.org,pfeldman@chromium.org,dgozman@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=427199
    
    Review URL: https://codereview.chromium.org/1220643002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197951 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/screen_orientation/ScreenOrientationController.h
+++ third_party/WebKit/Source/modules/screen_orientation/ScreenOrientationController.h
@@ -37,9 +37,6 @@ public:
     void lock(WebScreenOrientationLockType, WebLockOrientationCallback*);
     void unlock();
 
-    void setOverride(WebScreenOrientationType, unsigned short angle);
-    void clearOverride();
-
     static void provideTo(LocalFrame&, WebScreenOrientationClient*);
     static ScreenOrientationController* from(LocalFrame&);
     static const char* supplementName();
@@ -49,7 +46,7 @@ public:
 private:
     ScreenOrientationController(LocalFrame&, WebScreenOrientationClient*);
 
-    static WebScreenOrientationType computeOrientation(const IntRect&, uint16_t);
+    static WebScreenOrientationType computeOrientation(ChromeClient&);
 
     // Inherited from PlatformEventController.
     virtual void didUpdateData() override;
@@ -60,9 +57,6 @@ private:
 
     // Inherited from LocalFrameLifecycleObserver.
     virtual void willDetachFrameHost() override;
-
-    unsigned short effectiveAngle(ChromeClient&);
-    WebScreenOrientationType effectiveType(ChromeClient&);
 
     void notifyDispatcher();
 
@@ -75,9 +69,6 @@ private:
     PersistentWillBeMember<ScreenOrientation> m_orientation;
     WebScreenOrientationClient* m_client;
     Timer<ScreenOrientationController> m_dispatchEventTimer;
-    bool m_override;
-    WebScreenOrientationType m_overrideType;
-    unsigned short m_overrideAngle;
 };
 
 } // namespace blink
