cb3a785a4db123226ff0291e1c8258f2f4dd6233
grt <grt@chromium.org>
2015-06-29 06:56:55 -0700

    Revert of Pretty print AlternativeService in test macros. (patchset #1 id:1 of https://codereview.chromium.org/1212813006/)
    Reason for revert:
    Introduced new static initializers:
    
    # http_server_properties.cc _GLOBAL__sub_I_http_server_properties.cc+0x10
    # http_server_properties.cc std::ios_base::Init::Init()@plt
    # http_server_properties.cc __cxa_atexit@plt [registers a dtor to run at exit]
    
    Original issue's description:
    > Pretty print AlternativeService in test macros.
    >
    > Implement operator<<(std::ostream&, const AlternativeService&) so that when
    > EXPECT_EQ or ASSERT_EQ fails between two AlternativeService structs, the test
    > output contains protocol names and hostnames pretty printed, instead of a binary
    > blob including useless things like the memory address of where std::string
    > stores the hostname.
    >
    > Currently used in AlternateProtocolServerPropertiesTest.BrokenShadowsCanonical
    > only, but more tests involving EXPECT_EQ between AlternativeService structs are
    > coming.
    >
    > BUG=392575
    >
    > Committed: https://crrev.com/a3542c2eb9d3760800d718c51eb9f573a42c6b42
    > Cr-Commit-Position: refs/heads/master@{#336551}
    
    TBR=rch@chromium.org,bnc@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=392575
    
    Review URL: https://codereview.chromium.org/1218813005
    
    Cr-Commit-Position: refs/heads/master@{#336556}

--- net/http/http_server_properties.cc
+++ net/http/http_server_properties.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/http/http_server_properties.h"
 
-#include <iostream>
-
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/stringprintf.h"
@@ -111,11 +109,6 @@ std::string AlternativeService::ToString() const {
 std::string AlternativeServiceInfo::ToString() const {
   return base::StringPrintf("%s, p=%f", alternative_service.ToString().c_str(),
                             probability);
-}
-
-std::ostream& operator<<(std::ostream& os,
-                         const AlternativeService& alternative_service) {
-  return (os << alternative_service.ToString());
 }
 
 // static
