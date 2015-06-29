b59ed1850fd2527b798c66eccf4c78607fa579e6
grt <grt@chromium.org>
2015-06-29 11:17:17 -0700

    Revert of Revert of content: Fix single process support for native GpuMemoryBuffers. (patchset #1 id:1 of https://codereview.chromium.org/1210073003/)
    Reason for revert:
    Reverting the revert since this wasn't the problem after all.
    
    Original issue's description:
    > Revert of content: Fix single process support for native GpuMemoryBuffers. (patchset #1 id:1 of https://codereview.chromium.org/1221473003/)
    >
    > Reason for revert:
    > XP_tests started failing with "missing shard" exceptions. Suspect this cl.
    >
    > https://build.chromium.org/p/chromium.win/builders/XP%20Tests%20%281%29/builds/38438
    >
    > Original issue's description:
    > > content: Fix single process support for native GpuMemoryBuffers.
    > >
    > > Also add the startup message filter for CreateGpuMemoryBuffer
    > > messages in single process mode.
    > >
    > > BUG=497559,504121
    > >
    > > Committed: https://crrev.com/29ec87df6cef29b9f28dd21d385f16ce99df9ce3
    > > Cr-Commit-Position: refs/heads/master@{#336495}
    >
    > TBR=piman@chromium.org,reveman@chromium.org
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=497559,504121
    >
    > Committed: https://crrev.com/1cf221f6cebc8850220100c24d00a18ad54bfe8d
    > Cr-Commit-Position: refs/heads/master@{#336531}
    
    TBR=piman@chromium.org,reveman@chromium.org,kcarattini@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=497559,504121
    
    Review URL: https://codereview.chromium.org/1218783003
    
    Cr-Commit-Position: refs/heads/master@{#336593}

--- content/gpu/in_process_gpu_thread.cc
+++ content/gpu/in_process_gpu_thread.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/gpu/in_process_gpu_thread.h"
 
+#include "content/common/gpu/gpu_memory_buffer_factory.h"
 #include "content/gpu/gpu_child_thread.h"
 #include "content/gpu/gpu_process.h"
 
@@ -13,7 +14,9 @@ namespace content {
 InProcessGpuThread::InProcessGpuThread(const InProcessChildThreadParams& params)
     : base::Thread("Chrome_InProcGpuThread"),
       params_(params),
-      gpu_process_(NULL) {
+      gpu_process_(NULL),
+      gpu_memory_buffer_factory_(GpuMemoryBufferFactory::Create(
+          GpuChildThread::GetGpuMemoryBufferFactoryType())) {
 }
 
 InProcessGpuThread::~InProcessGpuThread() {
@@ -24,7 +27,8 @@ void InProcessGpuThread::Init() {
   gpu_process_ = new GpuProcess();
   // The process object takes ownership of the thread object, so do not
   // save and delete the pointer.
-  gpu_process_->set_main_thread(new GpuChildThread(params_));
+  gpu_process_->set_main_thread(
+      new GpuChildThread(params_, gpu_memory_buffer_factory_.get()));
 }
 
 void InProcessGpuThread::CleanUp() {
