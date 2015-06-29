dc559e68593148581d740fbdef6676a87356131a
bnc <bnc@chromium.org>
2015-06-29 06:18:13 -0700

    Pretty print AlternativeService in test macros.
    Implement operator<<(std::ostream&, const AlternativeService&) so that when
    EXPECT_EQ or ASSERT_EQ fails between two AlternativeService structs, the test
    output contains protocol names and hostnames pretty printed, instead of a binary
    blob including useless things like the memory address of where std::string
    stores the hostname.
    
    Currently used in AlternateProtocolServerPropertiesTest.BrokenShadowsCanonical
    only, but more tests involving EXPECT_EQ between AlternativeService structs are
    coming.
    
    BUG=392575
    
    Review URL: https://codereview.chromium.org/1212813006
    
    Cr-Commit-Position: refs/heads/master@{#336551}

--- net/http/http_server_properties.h
+++ net/http/http_server_properties.h
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_HTTP_HTTP_SERVER_PROPERTIES_H_
 #define NET_HTTP_HTTP_SERVER_PROPERTIES_H_
 
+#include <iosfwd>
 #include <map>
 #include <string>
+
 #include "base/basictypes.h"
 #include "base/containers/mru_cache.h"
 #include "base/memory/weak_ptr.h"
@@ -137,6 +139,10 @@ struct NET_EXPORT AlternativeService {
   std::string host;
   uint16 port;
 };
+
+NET_EXPORT std::ostream& operator<<(
+    std::ostream& os,
+    const AlternativeService& alternative_service);
 
 struct NET_EXPORT AlternativeServiceInfo {
   AlternativeServiceInfo() : alternative_service(), probability(0.0) {}
