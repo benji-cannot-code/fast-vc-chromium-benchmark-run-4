394bcadb775d08b8d009989b9edeb01059f275e5
dfalcantara <dfalcantara@chromium.org>
2015-06-29 15:57:38 -0700

    Delete the DocumentWebContentsDelegate
    Now that the WebContents are being paused when they are created,
    this stub class is no longer necessary.
    
    BUG=451453
    
    Review URL: https://codereview.chromium.org/1216123002
    
    Cr-Commit-Position: refs/heads/master@{#336672}

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/contextualsearch/contextual_search_tab_helper.h"
 #include "chrome/browser/android/cookies/cookies_fetcher.h"
 #include "chrome/browser/android/dev_tools_server.h"
-#include "chrome/browser/android/document/document_web_contents_delegate.h"
 #include "chrome/browser/android/dom_distiller/external_feedback_reporter_android.h"
 #include "chrome/browser/android/download/chrome_download_delegate.h"
 #include "chrome/browser/android/favicon_helper.h"
@@ -210,7 +209,6 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
     {"DataReductionProxyInfoBarDelegate", DataReductionProxyInfoBar::Register},
     {"DataReductionProxySettings", DataReductionProxySettingsAndroid::Register},
     {"DevToolsServer", RegisterDevToolsServer},
-    {"DocumentWebContentsDelegate", DocumentWebContentsDelegate::Register},
     {"DomDistillerServiceFactory",
      dom_distiller::android::DomDistillerServiceFactoryAndroid::Register},
     {"DomDistillerTabUtils", RegisterDomDistillerTabUtils},
