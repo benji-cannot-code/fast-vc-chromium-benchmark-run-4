0d79599caae89021c702086b2ee7601957c5a523
jochen <jochen@chromium.org>
2015-06-28 21:12:32 -0700

    Make html_viewer's WebViewClient/WebFrameClient impls take only one ctor arg
    This is required to be able to inject the test_runner's
    WebTestProxy/WebFrameProxy for running layout tests
    
    BUG=478250
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1212273002
    
    Cr-Commit-Position: refs/heads/master@{#336528}

--- components/html_viewer/html_viewer.cc
+++ components/html_viewer/html_viewer.cc
@@ -151,10 +151,11 @@ class HTMLDocumentApplicationDelegate : public mojo::ApplicationDelegate {
                      base::Unretained(this)));
       documents2_.insert(document);
     } else {
-      HTMLDocument* document = new HTMLDocument(
+      HTMLDocument::CreateParams params(
           &app_, connection, response.Pass(), setup_,
           base::Bind(&HTMLDocumentApplicationDelegate::OnHTMLDocumentDeleted,
                      base::Unretained(this)));
+      HTMLDocument* document = new HTMLDocument(&params);
       documents_.insert(document);
     }
   }
