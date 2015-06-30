4e20e145df36c597b355ef3ef1174558c663f01b
brettw <brettw@chromium.org>
2015-06-30 12:32:03 -0700

    GN: Throw an error for duplicate object files.
    Detect duplicate object files in the same target and throw an error. This provides a stronger barrier and a more actionable message than the Ninja "multiple rules" error that will result.
    
    Review URL: https://codereview.chromium.org/1210143003
    
    Cr-Commit-Position: refs/heads/master@{#336853}

--- tools/gn/ninja_binary_target_writer.cc
+++ tools/gn/ninja_binary_target_writer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <sstream>
 
+#include "base/containers/hash_tables.h"
 #include "base/strings/string_util.h"
 #include "tools/gn/config_values_extractors.h"
 #include "tools/gn/deps_iterator.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "tools/gn/escape.h"
 #include "tools/gn/filesystem_utils.h"
 #include "tools/gn/ninja_utils.h"
+#include "tools/gn/scheduler.h"
 #include "tools/gn/settings.h"
 #include "tools/gn/source_file_type.h"
 #include "tools/gn/string_utils.h"
@@ -294,6 +296,9 @@ void NinjaBinaryTargetWriter::Run() {
 
   // Also link all pch object files.
   obj_files.insert(obj_files.end(), pch_obj_files.begin(), pch_obj_files.end());
+
+  if (!CheckForDuplicateObjectFiles(obj_files))
+    return;
 
   if (target_->output_type() == Target::SOURCE_SET) {
     WriteSourceSetStamp(obj_files);
@@ -791,4 +796,31 @@ OutputFile NinjaBinaryTargetWriter::GetWindowsPCHFile(
   ret.value().append(".pch");
 
   return ret;
+}
+
+bool NinjaBinaryTargetWriter::CheckForDuplicateObjectFiles(
+    const std::vector<OutputFile>& files) const {
+  base::hash_set<std::string> set;
+  for (const auto& file : files) {
+    if (!set.insert(file.value()).second) {
+      Err err(
+          target_->defined_from(),
+          "Duplicate object file",
+          "The target " + target_->label().GetUserVisibleName(false) +
+          "\ngenerates two object files with the same name:\n  " +
+          file.value() + "\n"
+          "\n"
+          "It could be you accidentally have a file listed twice in the\n"
+          "sources. Or, depending on how your toolchain maps sources to\n"
+          "object files, two source files with the same name in different\n"
+          "directories could map to the same object file.\n"
+          "\n"
+          "In the latter case, either rename one of the files or move one of\n"
+          "the sources to a separate source_set to avoid them both being in\n"
+          "the same target.");
+      g_scheduler->FailWithError(err);
+      return false;
+    }
+  }
+  return true;
 }
