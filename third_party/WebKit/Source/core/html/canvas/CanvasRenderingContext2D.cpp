f0417ffbfb1e0c06efc631b49048743da22f38bd
junov@chromium.org <junov@chromium.org>
2015-06-30 15:22:15 +0000

    Adding early exit in CanvasRenderingContext2D::setFont if value is unchanged.
    BUG=497864
    TEST=telemetry blink_perf.canvas setFontAndMeasureTextSingleFont.html
    
    Review URL: https://codereview.chromium.org/1216713002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198067 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.cpp
@@ -1709,6 +1709,9 @@ String CanvasRenderingContext2D::font() const
 
 void CanvasRenderingContext2D::setFont(const String& newFont)
 {
+    if (newFont == state().unparsedFont() && state().hasRealizedFont())
+        return;
+
     // The style resolution required for rendering text is not available in frame-less documents.
     if (!canvas()->document().frame())
         return;
