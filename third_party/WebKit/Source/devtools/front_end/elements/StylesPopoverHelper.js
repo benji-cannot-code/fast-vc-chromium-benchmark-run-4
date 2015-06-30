cf828758e1f2626d255e3c59dfc9eb8a595fe85d
lushnikov@chromium.org <lushnikov@chromium.org>
2015-06-30 14:11:26 +0000

    DevTools: employ HTML tooltips intstead of "title" svg element
    BUG=505715
    R=pfeldman, samli
    
    Review URL: https://codereview.chromium.org/1216203002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198057 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/elements/StylesPopoverHelper.js
+++ third_party/WebKit/Source/devtools/front_end/elements/StylesPopoverHelper.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
- // Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -164,14 +164,13 @@ WebInspector.BezierPopoverIcon.prototype = {
     _createDOM: function(text)
     {
         this._element = createElement("nobr");
+        this._element.title = WebInspector.UIString("Click to open a cubic bezier editor.");
 
         this._iconElement = this._element.createSVGChild("svg", "popover-icon bezier-icon");
         this._iconElement.setAttribute("height", 10);
         this._iconElement.setAttribute("width", 10);
         this._iconElement.addEventListener("click", this._iconClick.bind(this), false);
         var g = this._iconElement.createSVGChild("g");
-        var title = g.createSVGChild("title");
-        title.textContent = WebInspector.UIString("Click to open a cubic bezier editor.");
         var path = g.createSVGChild("path");
         path.setAttribute("d", "M2,8 C2,3 8,7 8,2");
 
