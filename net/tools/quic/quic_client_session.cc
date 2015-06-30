4175f6f306f068ed6349a66266cbadce732ba989
rch <rch@chromium.org>
2015-06-30 12:15:34 -0700

    Add a ProofVerifyContext to tools::QuicClientSession.
    Review URL: https://codereview.chromium.org/1216553004
    
    Cr-Commit-Position: refs/heads/master@{#336845}

--- net/tools/quic/quic_client_session.cc
+++ net/tools/quic/quic_client_session.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "net/quic/crypto/crypto_protocol.h"
+#include "net/quic/crypto/proof_verifier_chromium.h"
 #include "net/quic/quic_server_id.h"
 #include "net/tools/quic/quic_spdy_client_stream.h"
 
@@ -20,8 +21,11 @@ QuicClientSession::QuicClientSession(const QuicConfig& config,
                                      const QuicServerId& server_id,
                                      QuicCryptoClientConfig* crypto_config)
     : QuicClientSessionBase(connection, config),
-      crypto_stream_(
-          new QuicCryptoClientStream(server_id, this, nullptr, crypto_config)),
+      crypto_stream_(new QuicCryptoClientStream(
+          server_id,
+          this,
+          new ProofVerifyContextChromium(0, BoundNetLog()),
+          crypto_config)),
       respect_goaway_(true) {
 }
 
