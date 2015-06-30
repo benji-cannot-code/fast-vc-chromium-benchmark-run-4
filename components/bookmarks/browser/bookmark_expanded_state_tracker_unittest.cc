0b7c3e7e6776f17574cd850415c65d02307e58eb
tfarina <tfarina@chromium.org>
2015-06-30 15:07:20 -0700

    bookmarks: Simplify prefs utilization in BookmarkExpandedStateTrackerTest.
    We can simply use TestingPrefServiceSimple in this test, simplifying how
    things are set up.
    
    BUG=None
    TEST=python testing/xvfb.py out/Debug/ out/Debug/components_unittests
    --gtest_filter=BookmarkExpandedStateTrackerTest.*
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1214863002
    
    Cr-Commit-Position: refs/heads/master@{#336887}

--- components/bookmarks/browser/bookmark_expanded_state_tracker_unittest.cc
+++ components/bookmarks/browser/bookmark_expanded_state_tracker_unittest.cc
@@ -6,31 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/bookmarks/browser/bookmark_expanded_state_tracker.h"
 
 #include "base/files/file_path.h"
-#include "base/prefs/pref_service.h"
-#include "base/prefs/pref_service_factory.h"
-#include "base/prefs/testing_pref_store.h"
-#include "base/run_loop.h"
+#include "base/message_loop/message_loop.h"
+#include "base/prefs/pref_registry_simple.h"
+#include "base/prefs/testing_pref_service.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/thread_task_runner_handle.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/common/bookmark_pref_names.h"
 #include "components/bookmarks/test/bookmark_test_helpers.h"
 #include "components/bookmarks/test/test_bookmark_client.h"
-#include "components/pref_registry/pref_registry_syncable.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace bookmarks {
-
-scoped_ptr<PrefService> PrefServiceForTesting() {
-  scoped_refptr<user_prefs::PrefRegistrySyncable> registry(
-      new user_prefs::PrefRegistrySyncable());
-  registry->RegisterListPref(prefs::kBookmarkEditorExpandedNodes,
-                             new base::ListValue);
-  registry->RegisterListPref(prefs::kManagedBookmarks);
-  base::PrefServiceFactory factory;
-  factory.set_user_prefs(make_scoped_refptr(new TestingPrefStore()));
-  return factory.Create(registry.get());
-}
 
 class BookmarkExpandedStateTrackerTest : public testing::Test {
  public:
@@ -43,8 +30,8 @@ class BookmarkExpandedStateTrackerTest : public testing::Test {
   void TearDown() override;
 
   base::MessageLoop message_loop_;
+  TestingPrefServiceSimple prefs_;
   TestBookmarkClient client_;
-  scoped_ptr<PrefService> prefs_;
   scoped_ptr<BookmarkModel> model_;
 
   DISALLOW_COPY_AND_ASSIGN(BookmarkExpandedStateTrackerTest);
@@ -55,9 +42,11 @@ BookmarkExpandedStateTrackerTest::BookmarkExpandedStateTrackerTest() {}
 BookmarkExpandedStateTrackerTest::~BookmarkExpandedStateTrackerTest() {}
 
 void BookmarkExpandedStateTrackerTest::SetUp() {
-  prefs_ = PrefServiceForTesting();
+  prefs_.registry()->RegisterListPref(prefs::kBookmarkEditorExpandedNodes,
+                                      new base::ListValue);
+  prefs_.registry()->RegisterListPref(prefs::kManagedBookmarks);
   model_.reset(new BookmarkModel(&client_));
-  model_->Load(prefs_.get(), std::string(), base::FilePath(),
+  model_->Load(&prefs_, std::string(), base::FilePath(),
                base::ThreadTaskRunnerHandle::Get(),
                base::ThreadTaskRunnerHandle::Get());
   test::WaitForBookmarkModelToLoad(model_.get());
