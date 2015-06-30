bc40d77cae99c439db6983882eb034ea7175f12b
jochen@chromium.org <jochen@chromium.org>
2015-06-30 09:08:46 +0000

    Correctly track lifetime of promise rejection events
    BUG=495801
    R=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/1215813003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198042 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/custom/V8PromiseRejectionEventCustom.cpp
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8PromiseRejectionEventCustom.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptPromise.h"
 #include "bindings/core/v8/ScriptState.h"
 #include "bindings/core/v8/ScriptValue.h"
+#include "bindings/core/v8/ScriptWrappable.h"
 #include "bindings/core/v8/V8Binding.h"
 
 namespace blink {
@@ -24,6 +25,12 @@ void V8PromiseRejectionEvent::promiseAttributeGetterCustom(const v8::FunctionCal
     }
 
     v8SetReturnValue(info, promise.v8Value());
+}
+
+void V8PromiseRejectionEvent::visitDOMWrapper(v8::Isolate* isolate, ScriptWrappable* scriptWrappable, const v8::Persistent<v8::Object>& wrapper)
+{
+    PromiseRejectionEvent* event = scriptWrappable->toImpl<PromiseRejectionEvent>();
+    event->setWrapperReference(isolate, wrapper);
 }
 
 } // namespace blink
