e000adf6d6ee28d05fb317df7be825dc0c00ce52
xiang.long <xiang.long@intel.com>
2015-06-29 20:53:00 -0700

    ServiceWorker: Introduce ServiceWorkerMessageEvent to replace MessageEvent (2/3).
    The message sent from Service Worker to its client will land at
    navigator.serviceWorker.onmessage. The event type will be a new
    ServiceWorkerMessageEvent which 'source' attribute could be
    ServiceWorker or MessagePort.
    
    This's the Chrome side CL to include ServiceWorker info when
    dispatching message to renderer.
    
    spec: https://slightlyoff.github.io/ServiceWorker/spec/service_worker/#serviceworkermessage-event-section
    
    BUG=498596
    
    Review URL: https://codereview.chromium.org/1141813002
    
    Cr-Commit-Position: refs/heads/master@{#336725}

--- content/child/service_worker/service_worker_dispatcher.h
+++ content/child/service_worker/service_worker_dispatcher.h
@@ -27,6 +27,8 @@ namespace IPC {
 class Message;
 }
 
+struct ServiceWorkerMsg_MessageToDocument_Params;
+
 namespace content {
 
 class ServiceWorkerMessageFilter;
@@ -37,7 +39,6 @@ class WebServiceWorkerRegistrationImpl;
 struct ServiceWorkerObjectInfo;
 struct ServiceWorkerRegistrationObjectInfo;
 struct ServiceWorkerVersionAttributes;
-struct TransferredMessagePort;
 
 // This class manages communication with the browser process about
 // registration of the service worker, exposed to renderer and worker
@@ -230,12 +231,7 @@ class CONTENT_EXPORT ServiceWorkerDispatcher
                                     int provider_id,
                                     const ServiceWorkerObjectInfo& info,
                                     bool should_notify_controllerchange);
-  void OnPostMessage(
-      int thread_id,
-      int provider_id,
-      const base::string16& message,
-      const std::vector<TransferredMessagePort>& sent_message_ports,
-      const std::vector<int>& new_routing_ids);
+  void OnPostMessage(const ServiceWorkerMsg_MessageToDocument_Params& params);
 
   // Keeps map from handle_id to ServiceWorker object.
   void AddServiceWorker(int handle_id, WebServiceWorkerImpl* worker);
