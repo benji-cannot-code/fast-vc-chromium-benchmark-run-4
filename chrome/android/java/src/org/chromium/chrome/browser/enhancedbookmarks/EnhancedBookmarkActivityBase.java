b04aa4c263d0785a039fb89fd58125d91d5c21de
kkimlabs <kkimlabs@chromium.org>
2015-06-30 15:32:43 -0700

    [Android] Hide keyboard on EnhancedBookmarkEditActivity.
    EnhancedBookmarkEditActivity showed software keyboard on start, but it's not ideal:
    - The only way to check URL is EnhancedBookmarkEditActivity, other than launching,
      so the actual intent might not be editing.
    - Users might just want to move or edit URL, but focusing on title and showing
      software keyboard makes it distracting.
    
    Also, move keyboard hiding logic to xml, which is cleaner.
    
    BUG=504762
    
    Review URL: https://codereview.chromium.org/1218173002
    
    Cr-Commit-Position: refs/heads/master@{#336896}

--- chrome/android/java/src/org/chromium/chrome/browser/enhancedbookmarks/EnhancedBookmarkActivityBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/enhancedbookmarks/EnhancedBookmarkActivityBase.java
@@ -11,7 +11,6 @@ import android.util.Log;
 
 import org.chromium.base.library_loader.ProcessInitException;
 import org.chromium.chrome.browser.ChromeApplication;
-import org.chromium.ui.UiUtils;
 
 /**
  * Activity base class that all the EnhnacedBookmark activities inherit. Currently it's responsible
@@ -31,11 +30,5 @@ abstract class EnhancedBookmarkActivityBase extends AppCompatActivity {
             Log.e(TAG, "Failed to start browser process.", e);
             ChromeApplication.reportStartupErrorAndExit(e);
         }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        UiUtils.hideKeyboard(getWindow().getDecorView());
     }
 }
