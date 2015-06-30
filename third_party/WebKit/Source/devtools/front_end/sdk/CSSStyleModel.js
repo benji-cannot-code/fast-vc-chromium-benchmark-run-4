3380e5ce927f1e889fd5b071c08dc1298866bee6
pfeldman@chromium.org <pfeldman@chromium.org>
2015-06-30 14:19:29 +0000

    DevTools: [Regression] setting property text throws exceptions
    R=lushnikov
    BUG=505771
    
    Review URL: https://codereview.chromium.org/1211323008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198060 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/CSSStyleModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/CSSStyleModel.js
@@ -1330,6 +1330,7 @@ WebInspector.CSSProperty.prototype = {
         }
 
         var lastWasSemicolon = true;
+        var lastWasMeta = false;
         var insideProperty = false;
         /**
          * @param {string} token
@@ -1362,11 +1363,13 @@ WebInspector.CSSProperty.prototype = {
             if (isSemicolon)
                 insideProperty = false;
 
-            if (tokenType === "css-tag") {
+            if (tokenType === "css-meta" || (tokenType === "css-tag" && !lastWasMeta)) {
                 result += "\n" + indentation;
                 insideProperty = true;
             }
             result += token;
+
+            lastWasMeta = tokenType === "css-meta";
         }
     },
 
