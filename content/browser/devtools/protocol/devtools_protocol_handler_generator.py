5a87e44b9fd0e59dbb37542c2574f7e9f673e56f
dgozman <dgozman@chromium.org>
2015-06-30 09:29:07 -0700

    Revert of [DevTools] Capabilities on browser side. (patchset #3 id:40001 of https://codereview.chromium.org/1208993002/)
    Reason for revert:
    Decided against this path.
    
    Original issue's description:
    > [DevTools] Capabilities on browser side.
    >
    > BUG=504412
    >
    > Committed: https://crrev.com/ec5a24bd9aff8645121d06c72ca93576e4a30128
    > Cr-Commit-Position: refs/heads/master@{#336772}
    
    TBR=pfeldman@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=504412
    
    Review URL: https://codereview.chromium.org/1217943005
    
    Cr-Commit-Position: refs/heads/master@{#336794}

--- content/browser/devtools/protocol/devtools_protocol_handler_generator.py
+++ content/browser/devtools/protocol/devtools_protocol_handler_generator.py
@@ -69,8 +69,6 @@ base::Value* CreateValue(const std::vector<T> param) {
 template<>
 base::Value* CreateValue(const std::string& param);
 
-${capabilities}\
-
 ${types}\
 
 }  // namespace devtools
@@ -103,30 +101,6 @@ ${fields}\
 }  // namespace content
 
 #endif  // CONTENT_BROWSER_DEVTOOLS_PROTOCOL_DEVTOOLS_PROTOCOL_DISPATCHER_H_
-""")
-
-tmpl_domain_capability = string.Template("""\
-  static const char ${capability}[];
-""")
-
-tmpl_domain_capabilities = string.Template("""\
-namespace ${domain} {
-struct Capabilities {
-  static const char ${Domain}[];
-${capabilities}\
-};
-}  // namespace ${domain}
-""")
-
-tmpl_domain_capability_impl = string.Template("""\
-  const char Capabilities::${capability}[] = "${Domain}.${capability_lower}";
-""")
-
-tmpl_domain_capabilities_impl = string.Template("""\
-namespace ${domain} {
-  const char Capabilities::${Domain}[] = "${Domain}";
-${capabilities}\
-}  // namespace ${domain}
 """)
 
 tmpl_typedef = string.Template("""\
@@ -310,8 +284,6 @@ template<>
 base::Value* CreateValue(const std::string& param) {
   return new base::StringValue(param);
 }
-
-${capabilities}\
 
 ${types}\
 
@@ -658,8 +630,6 @@ def ResolveType(json, mapping):
     raise Exception("Unknown type at %s.%s %s" %
         (mapping["Domain"], mapping["command"], mapping["proto_param"]))
 
-capabilities = []
-capabilities_impl = []
 setters = []
 fields = []
 
@@ -676,25 +646,6 @@ for json_domain in all_domains:
   client_method_impls = []
   domain_empty = True
   domain_needs_client = False
-
-  domain_capabilities = []
-  domain_capabilities_impl = []
-  if "capabilities" in json_domain:
-    for json_capability in json_domain["capabilities"]:
-      domain_capabilities.append(tmpl_domain_capability.substitute(
-          domain_map,
-          capability=Capitalize(json_capability["name"])))
-      domain_capabilities_impl.append(tmpl_domain_capability_impl.substitute(
-          domain_map,
-          capability=Capitalize(json_capability["name"]),
-          capability_lower=json_capability["name"]))
-
-  capabilities.append(tmpl_domain_capabilities.substitute(
-      domain_map,
-      capabilities="\n".join(domain_capabilities)))
-  capabilities_impl.append(tmpl_domain_capabilities_impl.substitute(
-      domain_map,
-      capabilities="\n".join(domain_capabilities_impl)))
 
   if "commands" in json_domain:
     for json_command in json_domain["commands"]:
@@ -834,7 +785,6 @@ output_h_file = open(output_h_path, "w")
 output_cc_file = open(output_cc_path, "w")
 
 output_h_file.write(template_h.substitute({},
-    capabilities = "\n".join(capabilities),
     types = "\n".join(type_decls),
     setters = "".join(setters),
     methods = "".join(handler_methods),
@@ -847,6 +797,5 @@ output_cc_file.write(template_cc.substitute({},
     includes = "".join(sorted(includes)),
     fields_init = ",\n      ".join(fields_init),
     methods = "\n".join(handler_method_impls),
-    capabilities = "\n".join(capabilities_impl),
     types = "\n".join(type_impls)))
 output_cc_file.close()
