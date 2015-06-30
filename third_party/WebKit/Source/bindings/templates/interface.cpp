dada9cbb3b4732750d6817a5115e67511b1fce6a
haraken@chromium.org <haraken@chromium.org>
2015-06-30 09:10:02 +0000

    Binding: Remove unnecessary setObjectGroupId from visitDOMWrapper
    I believe the setObjectGroupId is unnecessary.
    
    - Regarding [SetWrapperReferenceTo], it shouldn't be necessary because [SetWrapperReferenceTo] doesn't create any object group id.
    
    - Regarding [SetWrapperReferenceFrom], setObjectGroupId is called only when the C++ reachable function returns null (e.g., NodeList::owner() returns null). In this case, it shouldn't be necessary to create an object id for the NodeList.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1214903006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198043 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/templates/interface.cpp
+++ third_party/WebKit/Source/bindings/templates/interface.cpp
@@ -578,7 +578,6 @@ void {{v8_class}}::visitDOMWrapper(v8::Isolate* isolate, ScriptWrappable* script
         return;
     }
     {% endif %}
-    setObjectGroup(isolate, scriptWrappable, wrapper);
 }
 
 {% endif %}
