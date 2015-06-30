acb27825628b0592a44883e0884489b299209e05
kmarshall <kmarshall@chromium.org>
2015-06-29 18:23:42 -0700

    Wire up the onDefaultSessionStarted handler to receive and process session events from the presentation API Mojo service.
    The OnSessionStateChange implementation is currently incomplete
    so the handler is left unconnected.
    
    R=vadimgo@chromium.org,mfoltz@chromium.org
    
    Review URL: https://codereview.chromium.org/1216933005
    
    Cr-Commit-Position: refs/heads/master@{#336708}

--- content/renderer/presentation/presentation_dispatcher.cc
+++ content/renderer/presentation/presentation_dispatcher.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/renderer/presentation/presentation_dispatcher.h"
 
+#include <algorithm>
+#include <vector>
+
 #include "base/logging.h"
 #include "content/common/presentation/presentation_service.mojom.h"
 #include "content/public/common/presentation_constants.h"
@@ -399,16 +402,15 @@ void PresentationDispatcher::ConnectToPresentationServiceIfNeeded() {
   binding_.Bind(GetProxy(&client_ptr));
   presentation_service_->SetClient(client_ptr.Pass());
 
-  // TODO(imcheng): Uncomment these once they are implemented on the browser
-  // side. (crbug.com/459006)
-  /*
   presentation_service_->ListenForDefaultSessionStart(base::Bind(
       &PresentationDispatcher::OnDefaultSessionStarted,
       base::Unretained(this)));
-  presentation_service_->ListenForSessionStateChange(base::Bind(
-      &PresentationDispatcher::OnSessionStateChange,
-      base::Unretained(this)));
-  */
+
+  // TODO(imcheng): Uncomment this once it is implemented on the browser
+  // side. (crbug.com/459006)
+  // presentation_service_->ListenForSessionStateChange(base::Bind(
+  //     &PresentationDispatcher::OnSessionStateChange,
+  //     base::Unretained(this)));
 }
 
 }  // namespace content
