674d65fe60995ee714415caddd935ec282f31593
reed@google.com <reed@google.com>
2015-06-30 01:23:21 +0000

    remove unused cachedBitmap from ImageBufferSurface
    BUG=
    
    Review URL: https://codereview.chromium.org/1217893004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198029 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.h
+++ third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.h
@@ -55,10 +55,6 @@ public:
     bool restore() override;
     WebLayer* layer() const override;
     bool isAccelerated() const override;
-    bool cachedBitmapEnabled() const override;
-    const SkBitmap& cachedBitmap() const override;
-    void invalidateCachedBitmap() override;
-    void updateCachedBitmapIfNeeded() override;
     void setIsHidden(bool) override;
 
 private:
