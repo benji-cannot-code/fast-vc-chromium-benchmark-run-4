39360a04e3f32e7372af6484aaa80b2d60f411bf
justincohen <justincohen@chromium.org>
2015-06-30 02:21:22 -0700

    Correct iOS build for latest Xcode beta.
    NSObject's NS_DESIGNATED_INITIALIZER -init method require an -init
    NS_UNAVAILABLE implementation.
    
    BUG=498581
    
    Review URL: https://codereview.chromium.org/1221733003
    
    Cr-Commit-Position: refs/heads/master@{#336750}

--- ios/chrome/browser/ui/native_content_controller.mm
+++ ios/chrome/browser/ui/native_content_controller.mm
@@ -35,6 +35,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return self;
 }
 
+- (instancetype)init {
+  NOTREACHED();
+  return nil;
+}
+
 - (instancetype)initWithURL:(const GURL&)url {
   return [self initWithNibName:nil url:url];
 }
