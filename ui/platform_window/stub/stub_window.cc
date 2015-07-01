17f132be3ff203b7bb9ea03a92368e79dfcdbb4e
sky <sky@chromium.org>
2015-07-01 14:32:24 -0700

    Makes stub window callback to delete with null widget and scale
    This matches what we were doing before.
    
    BUG=none
    TEST=none
    R=sadrul@chromium.org
    TBR=sadrul@chromium.org
    
    Review URL: https://codereview.chromium.org/1214083010
    
    Cr-Commit-Position: refs/heads/master@{#337115}

--- ui/platform_window/stub/stub_window.cc
+++ ui/platform_window/stub/stub_window.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 StubWindow::StubWindow(PlatformWindowDelegate* delegate) : delegate_(delegate) {
+  delegate_->OnAcceleratedWidgetAvailable(gfx::kNullAcceleratedWidget, 1.f);
 }
 
 StubWindow::~StubWindow() {
