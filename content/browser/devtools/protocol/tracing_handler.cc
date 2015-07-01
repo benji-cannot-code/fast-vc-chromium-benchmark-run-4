acae31fc178ae39a56edf8f5e224a9840954298f
pfeldman <pfeldman@chromium.org>
2015-07-01 11:23:59 -0700

    DevTools: only record filmstrip frames of the inspected page. (Landing this as a workaround, proper solution needs to generically plumb inspected target / routing id and filter by it).
    BUG=504062
    
    Review URL: https://codereview.chromium.org/1221993002
    
    Cr-Commit-Position: refs/heads/master@{#337072}

--- content/browser/devtools/protocol/tracing_handler.cc
+++ content/browser/devtools/protocol/tracing_handler.cc
@@ -48,6 +48,7 @@ class DevToolsTraceSinkProxy : public TracingController::TraceDataSink {
 
 TracingHandler::TracingHandler(TracingHandler::Target target)
     : target_(target),
+      did_initiate_recording_(false),
       weak_factory_(this) {
 }
 
@@ -85,6 +86,8 @@ Response TracingHandler::Start(DevToolsCommandId command_id,
                                const double* buffer_usage_reporting_interval) {
   if (IsRecording())
     return Response::InternalError("Tracing is already started");
+
+  did_initiate_recording_ = true;
 
   base::trace_event::TraceConfig trace_config(
       categories ? *categories : std::string(),
@@ -173,6 +176,7 @@ void TracingHandler::DisableRecording(bool abort) {
   buffer_usage_poll_timer_.reset();
   TracingController::GetInstance()->DisableRecording(
       abort ? nullptr : new DevToolsTraceSinkProxy(weak_factory_.GetWeakPtr()));
+  did_initiate_recording_ = false;
 }
 
 bool TracingHandler::IsRecording() const {
--- content/browser/devtools/protocol/tracing_handler.h
+++ content/browser/devtools/protocol/tracing_handler.h
@@ -43,6 +43,7 @@ class TracingHandler {
                  const double* buffer_usage_reporting_interval);
   Response End(DevToolsCommandId command_id);
   Response GetCategories(DevToolsCommandId command);
+  bool did_initiate_recording() { return did_initiate_recording_; }
 
  private:
   void OnRecordingEnabled(DevToolsCommandId command_id);
@@ -61,6 +62,7 @@ class TracingHandler {
   Target target_;
 
   scoped_ptr<Client> client_;
+  bool did_initiate_recording_;
   base::WeakPtrFactory<TracingHandler> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(TracingHandler);
