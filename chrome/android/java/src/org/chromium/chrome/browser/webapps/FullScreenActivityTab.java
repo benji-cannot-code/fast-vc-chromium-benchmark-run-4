cddc55502b700211a9e37836b3d81c7d319de907
bauerb <bauerb@chromium.org>
2015-07-01 00:51:33 -0700

    Move StreamUtil.java to base/.
    This allows using it in e.g. components.
    
    BUG=501333
    
    Review URL: https://codereview.chromium.org/1216813003
    
    Cr-Commit-Position: refs/heads/master@{#336992}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/FullScreenActivityTab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/FullScreenActivityTab.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
 
+import org.chromium.base.StreamUtil;
 import org.chromium.base.annotations.SuppressFBWarnings;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
@@ -30,7 +31,6 @@ import org.chromium.chrome.browser.contextmenu.ContextMenuPopulator;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
 import org.chromium.chrome.browser.tab.ChromeTab;
 import org.chromium.chrome.browser.tabmodel.TabModel.TabLaunchType;
-import org.chromium.chrome.browser.util.StreamUtil;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.WebContentsObserver;
