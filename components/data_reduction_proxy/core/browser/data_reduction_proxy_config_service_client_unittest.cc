fda7fc1f1163552ad094679b07caabc4ff5b6d7d
jeremyim <jeremyim@chromium.org>
2015-07-01 11:11:28 -0700

    Set the production URL for the Data Reduction Proxy config service.
    BUG=466753
    
    Review URL: https://codereview.chromium.org/1213863003
    
    Cr-Commit-Position: refs/heads/master@{#337063}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client_unittest.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/base64.h"
-#include "base/command_line.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/metrics/field_trial.h"
 #include "base/time/tick_clock.h"
@@ -21,9 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_params.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_params_test_utils.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_pref_names.h"
-#include "components/data_reduction_proxy/core/common/data_reduction_proxy_switches.h"
 #include "components/data_reduction_proxy/proto/client_config.pb.h"
-#include "components/variations/variations_associated_data.h"
 #include "net/http/http_response_headers.h"
 #include "net/proxy/proxy_server.h"
 #include "net/socket/socket_test_util.h"
@@ -50,9 +47,6 @@ const char kOldSuccessSessionKey[] = "OldSecretSessionKey";
 const char kPersistedOrigin[] = "https://persisted.net:443";
 const char kPersistedFallback[] = "persisted.net:80";
 const char kPersistedSessionKey[] = "PersistedSessionKey";
-
-const char kConfigServiceFieldTrial[] = "DataReductionProxyConfigService";
-const char kConfigServiceURLParam[] = "url";
 
 }  // namespace
 
@@ -386,85 +380,6 @@ TEST_F(DataReductionProxyConfigServiceClientTest, ConfigDisabled) {
   EXPECT_TRUE(configurator()->proxies_for_http().empty());
   EXPECT_TRUE(configurator()->proxies_for_https().empty());
   EXPECT_EQ(base::TimeDelta::FromDays(1), config_client()->GetDelay());
-}
-
-TEST_F(DataReductionProxyConfigServiceClientTest, GetConfigServiceURL) {
-  const struct {
-    std::string trial_group_value;
-    std::string trial_url_param;
-  } variations[] = {
-      {
-       "Enabled", "http://enabled.config-service/",
-      },
-      {
-       "Disabled", "http://disabled.config-service/",
-      },
-      {
-       "EnabledOther", "http://other.config-service/",
-      },
-  };
-
-  variations::testing::ClearAllVariationParams();
-  for (const auto& variation : variations) {
-    std::map<std::string, std::string> variation_params;
-    variation_params[kConfigServiceURLParam] = variation.trial_url_param;
-    ASSERT_TRUE(variations::AssociateVariationParams(
-        kConfigServiceFieldTrial, variation.trial_group_value,
-        variation_params));
-  }
-
-  const struct {
-    std::string test_case;
-    std::string flag_value;
-    std::string trial_group_value;
-    GURL expected;
-  } tests[] = {
-      {
-       "Nothing set", "", "", GURL(),
-      },
-      {
-       "Only command line set",
-       "http://commandline.config-service/",
-       "",
-       GURL("http://commandline.config-service/"),
-      },
-      {
-       "Enabled group", "", "Enabled", GURL("http://enabled.config-service/"),
-      },
-      {
-       "Disabled group",
-       "",
-       "Disabled",
-       GURL("http://disabled.config-service/"),
-      },
-      {
-       "Alternate enabled group",
-       "",
-       "EnabledOther",
-       GURL("http://other.config-service/"),
-      },
-      {
-       "Command line precedence",
-       "http://commandline.config-service/",
-       "Enabled",
-       GURL("http://commandline.config-service/"),
-      },
-  };
-
-  for (const auto& test : tests) {
-    // Reset all flags.
-    base::CommandLine::ForCurrentProcess()->InitFromArgv(0, NULL);
-    if (!test.flag_value.empty()) {
-      base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
-          switches::kDataReductionProxyConfigURL, test.flag_value);
-    }
-    base::FieldTrialList field_trial_list(nullptr);
-    if (!test.trial_group_value.empty()) {
-      base::FieldTrialList::CreateFieldTrial(kConfigServiceFieldTrial,
-                                             test.trial_group_value);
-    }
-    EXPECT_EQ(test.expected, params::GetConfigServiceURL()) << test.test_case;
-  }
 }
 
 TEST_F(DataReductionProxyConfigServiceClientTest, RemoteConfigSuccess) {
