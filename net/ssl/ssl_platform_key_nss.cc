2882a332570e006fa4b6c78813037fbcf883b693
davidben <davidben@chromium.org>
2015-07-01 11:48:46 -0700

    Sign CertificateVerify messages on a background thread.
    In the general case, client certificates may involve accessing a smart
    card (slow). Now that BoringSSL supports asynchronous certificate signing
    operations in the SSL layer, use it on Mac and Windows ports. This will
    avoid janking the IO thread in this (extremely uncommon) use case. Moreover,
    some Windows smartcard drivers will show UI which, as IPC traffic between
    UI thread and GPU process increases, causes deadlocks.
    
    This introduces an SSLPrivateKey interface on the Chromium side which
    implements an asynchronous signing operation. We then add a
    ThreadedSSLPrivateKey implementation which pushes otherwise synchronous
    signatures to a base::TaskRunner and implement Windows and Mac bridges to it.
    
    These SSLPrivateKey implementations are still vended from an X509Certificate
    for now, but https://crbug.com/394131 will track moving it up the stack for
    testability (at which point it'll also gain a Copy method).
    
    Android will be switched to use it in a follow-up. (The Android codepath
    goes through this OpenSSLClientKeyStore hack so doing it separately
    is easier.)
    
    BUG=493575,347404
    
    Review URL: https://codereview.chromium.org/1178193002
    
    Cr-Commit-Position: refs/heads/master@{#337079}

--- net/ssl/ssl_platform_key_nss.cc
+++ net/ssl/ssl_platform_key_nss.cc
@@ -3,13 +3,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "net/ssl/ssl_platform_key.h"
+
 #include "base/logging.h"
-#include "net/ssl/openssl_platform_key.h"
+#include "base/sequenced_task_runner.h"
+#include "net/ssl/ssl_private_key.h"
 
 namespace net {
 
-crypto::ScopedEVP_PKEY FetchClientCertPrivateKey(
-    const X509Certificate* certificate) {
+scoped_ptr<SSLPrivateKey> FetchClientCertPrivateKey(
+    X509Certificate* certificate,
+    scoped_refptr<base::SequencedTaskRunner> task_runner) {
   // TODO(davidben): Implement client auth for NSS. https://crbug.com/479036
   NOTIMPLEMENTED();
   return nullptr;
