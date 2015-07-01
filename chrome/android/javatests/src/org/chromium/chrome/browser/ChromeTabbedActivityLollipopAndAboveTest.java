98b5dade8fdeb6f9ffd3ebef5f4b67e3d4b1bc4c
jbudorick <jbudorick@chromium.org>
2015-07-01 11:22:40 -0700

    [Android] Move ChromeTabbedActivityLollipopAndAboveTest to the right directory.
    Its previous location was part of chrome_java_test_support, which is built into
    both ChromePublicTest and ChromeShellTest.
    
    BUG=506225
    
    Review URL: https://codereview.chromium.org/1218233002
    
    Cr-Commit-Position: refs/heads/master@{#337070}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ChromeTabbedActivityLollipopAndAboveTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ChromeTabbedActivityLollipopAndAboveTest.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.test;
+package org.chromium.chrome.browser;
 
 import android.app.Activity;
 import android.content.Context;
@@ -14,11 +14,11 @@ import org.chromium.base.ApplicationStatus;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.Restriction;
-import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
 import org.chromium.chrome.browser.document.DocumentActivity;
 import org.chromium.chrome.browser.document.DocumentMetricIds;
 import org.chromium.chrome.browser.util.FeatureUtilities;
+import org.chromium.chrome.test.ChromeTabbedActivityTestBase;
 import org.chromium.chrome.test.util.ActivityUtils;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
