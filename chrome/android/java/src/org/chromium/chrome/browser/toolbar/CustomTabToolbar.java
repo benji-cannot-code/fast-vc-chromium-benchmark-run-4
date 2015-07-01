00e97f82dcca386883a76831aa53887aa292a644
yusufo <yusufo@chromium.org>
2015-07-01 14:14:09 -0700

    Add a slide animation to security lock appearing on a custom tab
    Slides the url_info_container that contains both url and potentially title.
    Then makes the security lock visible and fades it in.
    
    BUG=505620
    
    Review URL: https://codereview.chromium.org/1218923002
    
    Cr-Commit-Position: refs/heads/master@{#337112}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/CustomTabToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/CustomTabToolbar.java
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.toolbar;
 
 import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -52,11 +54,14 @@ import org.chromium.chrome.browser.widget.TintedImageButton;
 import org.chromium.components.dom_distiller.core.DomDistillerService;
 import org.chromium.components.dom_distiller.core.DomDistillerUrlUtils;
 import org.chromium.ui.base.WindowAndroid;
+import org.chromium.ui.interpolators.BakedBezierInterpolator;
 
 /**
  * The Toolbar layout to be used for a custom tab. This is used for both phone and tablet UIs.
  */
 public class CustomTabToolbar extends ToolbarLayout implements LocationBar {
+    private static final int CUSTOM_TAB_TOOLBAR_SLIDE_DURATION_MS = 200;
+    private static final int CUSTOM_TAB_TOOLBAR_FADE_DURATION_MS = 150;
     private View mUrlInfoContainer;
     private UrlBar mUrlBar;
     private TextView mTitleBar;
@@ -65,7 +70,8 @@ public class CustomTabToolbar extends ToolbarLayout implements LocationBar {
     private int mSecurityIconType;
     private boolean mUseDarkColors;
     private TintedImageButton mCloseButton;
-    private Animator mSecurityButtonShowAnimator;
+
+    private AnimatorSet mSecurityButtonShowAnimator;
     private boolean mBackgroundColorSet;
 
     /**
@@ -90,9 +96,31 @@ public class CustomTabToolbar extends ToolbarLayout implements LocationBar {
         mSecurityIconType = ConnectionSecurityLevel.NONE;
         mCustomActionButton = (ImageButton) findViewById(R.id.action_button);
         mCloseButton = (TintedImageButton) findViewById(R.id.close_button);
-        mSecurityButtonShowAnimator = ObjectAnimator.ofFloat(mSecurityButton, ALPHA, 1);
-        mSecurityButtonShowAnimator
-                .setDuration(ToolbarPhone.URL_FOCUS_CHANGE_ANIMATION_DURATION_MS);
+        populateToolbarAnimations();
+    }
+
+    private void populateToolbarAnimations() {
+        mSecurityButtonShowAnimator = new AnimatorSet();
+        int securityIconButtonWidth =
+                getResources().getDimensionPixelSize(R.dimen.location_bar_icon_width);
+        Animator urlInfoContainerTranslateAnimator =
+                ObjectAnimator.ofFloat(mUrlInfoContainer, TRANSLATION_X, securityIconButtonWidth);
+        urlInfoContainerTranslateAnimator.setInterpolator(BakedBezierInterpolator.TRANSFORM_CURVE);
+        urlInfoContainerTranslateAnimator.setDuration(CUSTOM_TAB_TOOLBAR_SLIDE_DURATION_MS);
+
+        Animator securityButtonAlphaAnimator = ObjectAnimator.ofFloat(mSecurityButton, ALPHA, 1);
+        securityButtonAlphaAnimator.setInterpolator(BakedBezierInterpolator.FADE_IN_CURVE);
+        securityButtonAlphaAnimator.setDuration(CUSTOM_TAB_TOOLBAR_FADE_DURATION_MS);
+        securityButtonAlphaAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                mSecurityButton.setVisibility(VISIBLE);
+                mUrlInfoContainer.setTranslationX(0);
+            }
+        });
+
+        mSecurityButtonShowAnimator.playSequentially(
+                urlInfoContainerTranslateAnimator, securityButtonAlphaAnimator);
     }
 
     @Override
@@ -345,7 +373,6 @@ public class CustomTabToolbar extends ToolbarLayout implements LocationBar {
             mSecurityButton.setVisibility(GONE);
         } else {
             if (mSecurityButtonShowAnimator.isRunning()) mSecurityButtonShowAnimator.cancel();
-            mSecurityButton.setVisibility(VISIBLE);
             mSecurityButtonShowAnimator.start();
             mUrlBar.deEmphasizeUrl();
         }
