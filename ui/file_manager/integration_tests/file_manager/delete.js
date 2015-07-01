6758b82fbb1a4e0065b86adce316dc95fe8937e5
yawano <yawano@chromium.org>
2015-06-30 22:18:58 -0700

    Disable delete command when no item is selected.
    BUG=504592
    TEST=browser_tests:Delete/FileManagerBrowserTest.Test/0
    
    Review URL: https://codereview.chromium.org/1214663002
    
    Cr-Commit-Position: refs/heads/master@{#336981}

---
+++ ui/file_manager/integration_tests/file_manager/delete.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Delete menu item should be disabled when no item is selected.
+testcase.deleteMenuItemIsDisabledWhenNoItemIsSelected = function() {
+  testPromise(setupAndWaitUntilReady(null, RootPath.DOWNALOD).then(
+      function(windowId) {
+        // Right click the list without selecting an item.
+        return remoteCall.callRemoteTestUtil(
+            'fakeMouseRightClick', windowId, ['list.list']
+            ).then(function(result) {
+          chrome.test.assertTrue(result);
+
+          // Wait until the context menu is shown.
+          return remoteCall.waitForElement(
+              windowId,
+              '#file-context-menu:not([hidden])');
+        }).then(function() {
+          // Assert that delete command is disabled.
+          return remoteCall.waitForElement(
+              windowId,
+              'cr-menu-item[command="#delete"][disabled="disabled"]');
+        });
+      }));
+};
