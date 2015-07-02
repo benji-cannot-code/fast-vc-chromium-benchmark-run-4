488f5a1027528f88e7516f06592a476c894e0ad4
mustaq@chromium.org <mustaq@chromium.org>
2015-07-02 22:11:42 +0000

    Prevent sending PEs to dummy targets.
    There are some dummy targets in the touch lists that are kept for TE
    targetTouches list only. For PE, these targets are meaningless.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1221503002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198242 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/input/EventHandler.cpp
+++ third_party/WebKit/Source/core/input/EventHandler.cpp
@@ -3439,8 +3439,9 @@ void EventHandler::dispatchPointerEventsForTouchEvent(const PlatformTouchEvent& 
         const unsigned& pointerId = point.id();
         const PlatformTouchPoint::State pointState = point.state();
 
-        if (pointState == PlatformTouchPoint::TouchStationary)
+        if (pointState == PlatformTouchPoint::TouchStationary || !touchInfo.knownTarget)
             continue;
+
         bool pointerReleasedOrCancelled =
             pointState == PlatformTouchPoint::TouchReleased || pointState == PlatformTouchPoint::TouchCancelled;
         const AtomicString& eventName(pointerEventNameForTouchPointState(pointState));
