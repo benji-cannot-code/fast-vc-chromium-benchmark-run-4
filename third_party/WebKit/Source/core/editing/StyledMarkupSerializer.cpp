07902258f8273f313f0de868750537fecdc504d4
yoichio@chromium.org <yoichio@chromium.org>
2015-07-02 04:05:58 +0000

    [CodeHealth] Use PositionIteratorAlgorithm<Strategy> instead of StyledMarkupSerializer::PositionType. Thus we can use only Strategy template in cpp.
    BUG=504739
    
    Review URL: https://codereview.chromium.org/1221043002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198182 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/StyledMarkupSerializer.cpp
+++ third_party/WebKit/Source/core/editing/StyledMarkupSerializer.cpp
@@ -49,8 +49,8 @@ namespace blink {
 
 namespace {
 
-template<typename PositionType>
-TextOffset toTextOffset(const PositionType& position)
+template<typename Strategy>
+TextOffset toTextOffset(const PositionAlgorithm<Strategy>& position)
 {
     if (position.isNull())
         return TextOffset();
@@ -124,7 +124,7 @@ bool StyledMarkupTraverser<Strategy>::convertBlocksToInlines() const
 }
 
 template<typename Strategy>
-StyledMarkupSerializer<Strategy>::StyledMarkupSerializer(EAbsoluteURLs shouldResolveURLs, EAnnotateForInterchange shouldAnnotate, const PositionType& start, const PositionType& end, Node* highestNodeToBeSerialized, ConvertBlocksToInlines convertBlocksToInlines)
+StyledMarkupSerializer<Strategy>::StyledMarkupSerializer(EAbsoluteURLs shouldResolveURLs, EAnnotateForInterchange shouldAnnotate, const PositionAlgorithm<Strategy>& start, const PositionAlgorithm<Strategy>& end, Node* highestNodeToBeSerialized, ConvertBlocksToInlines convertBlocksToInlines)
     : m_start(start)
     , m_end(end)
     , m_shouldResolveURLs(shouldResolveURLs)
@@ -151,8 +151,8 @@ static bool needInterchangeNewlineAt(const VisiblePosition& v)
     return needInterchangeNewlineAfter(v.previous());
 }
 
-template<typename PositionType>
-static bool areSameRanges(Node* node, const PositionType& startPosition, const PositionType& endPosition)
+template<typename Strategy>
+static bool areSameRanges(Node* node, const PositionAlgorithm<Strategy>& startPosition, const PositionAlgorithm<Strategy>& endPosition)
 {
     ASSERT(node);
     Position otherStartPosition;
@@ -187,7 +187,7 @@ String StyledMarkupSerializer<Strategy>::createMarkup()
 
         firstNode = visibleStart.next().deepEquivalent().deprecatedNode();
 
-        if (pastEnd && Strategy::PositionType::beforeNode(firstNode).compareTo(Strategy::PositionType::beforeNode(pastEnd)) >= 0) {
+        if (pastEnd && PositionAlgorithm<Strategy>::beforeNode(firstNode).compareTo(PositionAlgorithm<Strategy>::beforeNode(pastEnd)) >= 0) {
             // This condition hits in editing/pasteboard/copy-display-none.html.
             return markupAccumulator.takeResults();
         }
