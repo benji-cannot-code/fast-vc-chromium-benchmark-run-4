78a815745f587a3ea8f83d9d996e6ddee290e363
fs@opera.com <fs@opera.com>
2015-07-02 08:43:17 +0000

    Unify handling of <color> for SVG/non-SVG properties
    Stop resolving system colors and "basic" named colors at parse time, and
    instead resolve them when resolving style.
    Implementation-wise this means getting rid of
    CSSPropertyParser::parseSVGColor in favor of parseColor, and similarly
    get rid of convertSVGColor in favor of convertColor on the style
    resolver side.
    
    Changes behavior for "basic" color names and system colors (as well as
    some proprietary/internal keywords). New behavior matches Gecko.
    
    BUG=370830,505410
    
    Review URL: https://codereview.chromium.org/1219153003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198199 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -2371,20 +2371,6 @@ PassRefPtrWillBeRawPtr<CSSPrimitiveValue> CSSPropertyParser::parseDeprecatedGrad
     return parseGradientStopColor(value);
 }
 
-// Used to parse <color> for SVG properties.
-PassRefPtrWillBeRawPtr<CSSPrimitiveValue> CSSPropertyParser::parseSVGColor(const CSSParserValue* value)
-{
-    CSSValueID id = value->id;
-    if (id == CSSValueCurrentcolor)
-        return cssValuePool().createIdentifierValue(id);
-    if ((id >= CSSValueActiveborder && id <= CSSValueWindowtext) || id == CSSValueMenu)
-        return cssValuePool().createColorValue(LayoutTheme::theme().systemColor(id).rgb());
-    RGBA32 c = Color::transparent;
-    if (!parseColorFromValue(value, c))
-        return nullptr;
-    return cssValuePool().createColorValue(c);
-}
-
 bool CSSPropertyParser::parseFillImage(CSSParserValueList* valueList, RefPtrWillBeRawPtr<CSSValue>& value)
 {
     if (valueList->current()->id == CSSValueNone) {
@@ -7851,7 +7837,7 @@ bool CSSPropertyParser::parseSVGValue(CSSPropertyID propId, bool important)
                     if (m_valueList->current()->id == CSSValueNone)
                         parsedValue = cssValuePool().createIdentifierValue(m_valueList->current()->id);
                     else
-                        parsedValue = parseSVGColor(m_valueList->current());
+                        parsedValue = parseColor(m_valueList->current());
                     if (parsedValue) {
                         values->append(parsedValue);
                         parsedValue = values;
@@ -7860,7 +7846,7 @@ bool CSSPropertyParser::parseSVGValue(CSSPropertyID propId, bool important)
                 if (!parsedValue)
                     parsedValue = CSSPrimitiveValue::create(value->string, CSSPrimitiveValue::CSS_URI);
             } else {
-                parsedValue = parseSVGColor(m_valueList->current());
+                parsedValue = parseColor(m_valueList->current());
             }
 
             if (parsedValue)
@@ -7871,7 +7857,7 @@ bool CSSPropertyParser::parseSVGValue(CSSPropertyID propId, bool important)
     case CSSPropertyStopColor: // TODO : icccolor
     case CSSPropertyFloodColor:
     case CSSPropertyLightingColor:
-        parsedValue = parseSVGColor(m_valueList->current());
+        parsedValue = parseColor(m_valueList->current());
         if (parsedValue)
             m_valueList->next();
 
