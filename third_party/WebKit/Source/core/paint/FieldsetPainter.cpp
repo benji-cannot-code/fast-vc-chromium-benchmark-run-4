064554a7113be23f8cd693bca7621c9a479c74b6
chrishtr@chromium.org <chrishtr@chromium.org>
2015-07-02 00:36:55 +0000

    SP: add drawing recorders for painting table cell and fielsset masks.
    BUG=504701
    
    Review URL: https://codereview.chromium.org/1220713009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198174 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/FieldsetPainter.cpp
+++ third_party/WebKit/Source/core/paint/FieldsetPainter.cpp
@@ -94,6 +94,10 @@ void FieldsetPainter::paintMask(const PaintInfo& paintInfo, const LayoutPoint& p
         paintRect.move(xOff, 0);
     }
 
+    LayoutObjectDrawingRecorder recorder(*paintInfo.context, m_layoutFieldset, paintInfo.phase, paintRect);
+    if (recorder.canUseCachedDrawing())
+        return;
+
     BoxPainter(m_layoutFieldset).paintMaskImages(paintInfo, paintRect);
 }
 
