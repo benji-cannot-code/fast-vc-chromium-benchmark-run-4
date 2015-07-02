055acec73144af688484741065e6a8b893f08354
bshe <bshe@chromium.org>
2015-07-02 09:32:04 -0700

    Fix user pod not always scroll to the center after VK shows up
    User pod at login/lock screen sometimes doesn't scroll to the center after
    VK shows up. The problem was we need to set keyboard state before we invoke
    the resize handler at the login/lock page. But this is not always the case
    due to the observer for resize is added before observer for keyboard state.
    This CL simply remove the dependency to keyboard state from resize logic. The dependency was unnecessary anymore.
    
    BUG=491214
    
    Review URL: https://codereview.chromium.org/1205063002
    
    Cr-Commit-Position: refs/heads/master@{#337230}

--- ui/login/account_picker/user_pod_row.js
+++ ui/login/account_picker/user_pod_row.js
@@ -2585,8 +2585,7 @@ cr.define('login', function() {
       if (layout.columns != this.columns || layout.rows != this.rows)
         this.placePods_();
 
-      if (Oobe.getInstance().virtualKeyboardShown)
-        this.scrollFocusedPodIntoView();
+      this.scrollFocusedPodIntoView();
     },
 
     /**
@@ -2784,9 +2783,7 @@ cr.define('login', function() {
           chrome.send('focusPod', [podToFocus.user.username]);
         this.firstShown_ = false;
         this.lastFocusedPod_ = podToFocus;
-
-        if (Oobe.getInstance().virtualKeyboardShown)
-          this.scrollFocusedPodIntoView();
+        this.scrollFocusedPodIntoView();
       }
       this.insideFocusPod_ = false;
       this.keyboardActivated_ = false;
