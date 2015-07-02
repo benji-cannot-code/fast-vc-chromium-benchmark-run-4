0e784d75f79b87010d786793f8e5d985db0fc081
jochen@chromium.org <jochen@chromium.org>
2015-07-02 17:13:51 +0000

    Remove unused WebViewClient::postAccessibilityEvent API
    BUG=506546
    R=nasko@chromium.org
    
    Review URL: https://codereview.chromium.org/1222653004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198228 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebViewClient.h
+++ third_party/WebKit/public/web/WebViewClient.h
@@ -218,12 +218,6 @@ public:
     virtual int historyForwardListCount() { return 0; }
 
 
-    // Accessibility -------------------------------------------------------
-
-    // Notifies embedder about an accessibility event.
-    virtual void postAccessibilityEvent(const WebAXObject&, WebAXEvent) { }
-
-
     // Developer tools -----------------------------------------------------
 
     // Called to notify the client that the inspector's settings were
