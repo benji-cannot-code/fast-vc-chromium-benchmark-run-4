69e34bdddd32154326a7225a13825d7810b73985
jkarlin <jkarlin@chromium.org>
2015-07-03 05:54:51 -0700

    Make BackgroundSyncRegistration a proper class
    This CL refactors content::BackgroundSyncManager::BackgroundSyncRegistration into content::BackgroundSyncRegistration. It also moves it from a struct into a class for better access control. In a followup CL the BackgroundSyncRegistration will become refcounted (so that the clients can own the registrations after the BackgroundSyncManager no longer cares about them). For more information see the bug.
    
    BUG=506271
    
    Review URL: https://codereview.chromium.org/1221883004
    
    Cr-Commit-Position: refs/heads/master@{#337306}

---
+++ content/browser/background_sync/background_sync_registration.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/background_sync/background_sync_registration.h"
+
+namespace content {
+
+const BackgroundSyncRegistration::RegistrationId
+    BackgroundSyncRegistration::kInvalidRegistrationId = -1;
+
+const BackgroundSyncRegistration::RegistrationId
+    BackgroundSyncRegistration::kInitialId = 0;
+
+bool BackgroundSyncRegistration::Equals(
+    const BackgroundSyncRegistration& other) const {
+  return options_.Equals(other.options_);
+}
+
+bool BackgroundSyncRegistration::IsValid() const {
+  return id_ != kInvalidRegistrationId;
+}
+
+}  // namespace content
