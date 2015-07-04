7edb7540dae62f03541c279c1b68859544e0c59c
Nico Weber <thakis@chromium.org>
2015-07-03 17:21:23 -0700

    Try to work around that clang/win bug in another file.
    Looks like https://codereview.chromium.org/1215613006 did help after all,
    but the same problem appears in another file too.
    
    If this helps, I'll revert https://codereview.chromium.org/1208103006/
    again.
    
    BUG=488634
    TBR=jkarlin@chromium.org
    
    Review URL: https://codereview.chromium.org/1213093005.
    
    Cr-Commit-Position: refs/heads/master@{#337333}

--- content/browser/background_sync/background_sync_registration.cc
+++ content/browser/background_sync/background_sync_registration.cc
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+// TODO(thakis): Remove this once http://crbug.com/488634 is fixed.
+BackgroundSyncRegistration::BackgroundSyncRegistration() = default;
+
 const BackgroundSyncRegistration::RegistrationId
     BackgroundSyncRegistration::kInvalidRegistrationId = -1;
 
