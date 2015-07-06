bf53cf4e54dde5a6b0ec6a428b99d8842a65fdf8
yhirano@chromium.org <yhirano@chromium.org>
2015-07-06 05:42:00 +0000

    Remove unused code from ScriptPromise.
    This is a refactoring and does not change the behavior.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1215543004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198300 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/ScriptPromise.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ScriptPromise.cpp
@@ -41,18 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-namespace {
-
-struct WithScriptState {
-    // Used by ToV8Value<WithScriptState, ScriptState*>.
-    static v8::Local<v8::Object> getCreationContext(ScriptState* scriptState)
-    {
-        return scriptState->context()->Global();
-    }
-};
-
-} // namespace
-
 unsigned ScriptPromise::s_instanceCount = 0;
 
 ScriptPromise::InternalResolver::InternalResolver(ScriptState* scriptState)
