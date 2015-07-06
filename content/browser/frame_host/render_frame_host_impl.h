9c2b4c229aa7ae901a6c43735ce93a1fda22aa9e
leon.han <leon.han@intel.com>
2015-07-06 05:27:27 -0700

    Remove improper comment statement.
    This CL is to address the outstanding issues of
    https://codereview.chromium.org/1085783002/
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1221333003
    
    Cr-Commit-Position: refs/heads/master@{#337386}

--- content/browser/frame_host/render_frame_host_impl.h
+++ content/browser/frame_host/render_frame_host_impl.h
@@ -447,7 +447,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
   // addition, its associated RenderWidgetHost has to be focused.
   bool IsFocused();
 
-  // Returns the Mojo ImageDownloader service pointer.
+  // Returns the Mojo ImageDownloader service.
   const image_downloader::ImageDownloaderPtr& GetMojoImageDownloader();
 
  protected:
