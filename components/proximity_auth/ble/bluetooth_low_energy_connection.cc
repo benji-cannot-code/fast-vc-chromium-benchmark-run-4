0e4dfa18977e2e3cbc6d843f1c1bc44df05dbf72
sacomoto <sacomoto@chromium.org>
2015-07-06 08:21:51 -0700

    Fixing a reconnection bug in ProximityAuthBleSystem
    BluetoothLowEnergyConnectionFinder was unable to reconnect to a BLE
    device immediatelly after (< 1s) after successfully disconnecting from
    it.
    
    This was caused by a race condition with |BluetoothDevice::Disconnect()|.
    Basically, the last call to |BluetoothDevice::Disconnect()| was not
    completed before the next |BluetoothDevice::CreateGattConnection()|
    was done. So, |BluetoothDevice::Disconnect()| would complete after, and
    interrupt the later call to |BluetoothDevice::CreateGattConnection()|.
    
    The use of |BluetoothDevice::Disconnect()| was incorrect in this case,
    as it effectively kills all connections to the given device. We should
    use |BluetoothGattConnection::Disconnect()| instead, as it only kills
    a given |BluetoothGattConnection| instance. This change solves the
    problem.
    
    BUG=506633
    
    Review URL: https://codereview.chromium.org/1216413005
    
    Cr-Commit-Position: refs/heads/master@{#337397}

--- components/proximity_auth/ble/bluetooth_low_energy_connection.cc
+++ components/proximity_auth/ble/bluetooth_low_energy_connection.cc
@@ -54,7 +54,7 @@ BluetoothLowEnergyConnection::BluetoothLowEnergyConnection(
       remote_service_({remote_service_uuid, ""}),
       to_peripheral_char_({to_peripheral_char_uuid, ""}),
       from_peripheral_char_({from_peripheral_char_uuid, ""}),
-      connection_(gatt_connection.Pass()),
+      gatt_connection_(gatt_connection.Pass()),
       sub_status_(SubStatus::DISCONNECTED),
       receiving_bytes_(false),
       write_remote_characteristic_pending_(false),
@@ -77,8 +77,8 @@ BluetoothLowEnergyConnection::~BluetoothLowEnergyConnection() {
 }
 
 void BluetoothLowEnergyConnection::Connect() {
-  if (connection_ && connection_->IsConnected()) {
-    OnGattConnectionCreated(connection_.Pass());
+  if (gatt_connection_ && gatt_connection_->IsConnected()) {
+    OnGattConnectionCreated(gatt_connection_.Pass());
     return;
   }
 
@@ -98,17 +98,12 @@ void BluetoothLowEnergyConnection::Disconnect() {
     ClearWriteRequestsQueue();
     StopNotifySession();
     SetSubStatus(SubStatus::DISCONNECTED);
-    if (connection_) {
-      connection_.reset();
-      BluetoothDevice* device = GetRemoteDevice();
-      if (device) {
-        PA_LOG(INFO) << "Disconnect from device " << device->GetAddress();
-        device->Disconnect(
-            base::Bind(&BluetoothLowEnergyConnection::OnDisconnected,
-                       weak_ptr_factory_.GetWeakPtr()),
-            base::Bind(&BluetoothLowEnergyConnection::OnDisconnectError,
-                       weak_ptr_factory_.GetWeakPtr()));
-      }
+    if (gatt_connection_) {
+      PA_LOG(INFO) << "Disconnect from device "
+                   << gatt_connection_->GetDeviceAddress();
+
+      // Destroying BluetoothGattConnection also disconnects it.
+      gatt_connection_.reset();
     }
   }
 }
@@ -274,7 +269,7 @@ void BluetoothLowEnergyConnection::OnCreateGattConnectionError(
 
 void BluetoothLowEnergyConnection::OnGattConnectionCreated(
     scoped_ptr<device::BluetoothGattConnection> gatt_connection) {
-  connection_ = gatt_connection.Pass();
+  gatt_connection_ = gatt_connection.Pass();
   SetSubStatus(SubStatus::WAITING_CHARACTERISTICS);
   characteristic_finder_.reset(CreateCharacteristicsFinder(
       base::Bind(&BluetoothLowEnergyConnection::OnCharacteristicsFound,
