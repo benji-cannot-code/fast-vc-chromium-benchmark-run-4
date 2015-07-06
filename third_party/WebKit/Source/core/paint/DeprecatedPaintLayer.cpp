eff6ee46d08aebb5bff77380d0b5a6c2a0d59232
schenney@chromium.org <schenney@chromium.org>
2015-07-06 18:57:01 +0000

    Clear the groupedMapping parameter in layers when removed from the group.
    Also clean up the code somewhat: avoid magic numbers, return as soon as possible, clarify parameter meaning.
    
    R=vollick
    BUG=504688
    
    Review URL: https://codereview.chromium.org/1209033009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198327 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/DeprecatedPaintLayer.cpp
+++ third_party/WebKit/Source/core/paint/DeprecatedPaintLayer.cpp
@@ -162,7 +162,7 @@ DeprecatedPaintLayer::~DeprecatedPaintLayer()
     if (groupedMapping()) {
         DisableCompositingQueryAsserts disabler;
         groupedMapping()->removeLayerFromSquashingGraphicsLayer(this);
-        setGroupedMapping(0);
+        setGroupedMapping(0, InvalidateLayerAndRemoveFromMapping);
     }
 
     // Child layers will be deleted by their corresponding layout objects, so
@@ -2353,17 +2353,17 @@ void DeprecatedPaintLayer::clearCompositedDeprecatedPaintLayerMapping(bool layer
         updateOrRemoveFilterEffectBuilder();
 }
 
-void DeprecatedPaintLayer::setGroupedMapping(CompositedDeprecatedPaintLayerMapping* groupedMapping, bool layerBeingDestroyed)
+void DeprecatedPaintLayer::setGroupedMapping(CompositedDeprecatedPaintLayerMapping* groupedMapping, SetGroupMappingOptions options)
 {
     if (groupedMapping == m_groupedMapping)
         return;
 
-    if (!layerBeingDestroyed && m_groupedMapping) {
+    if (options == InvalidateLayerAndRemoveFromMapping && m_groupedMapping) {
         m_groupedMapping->setNeedsGraphicsLayerUpdate(GraphicsLayerUpdateSubtree);
         m_groupedMapping->removeLayerFromSquashingGraphicsLayer(this);
     }
     m_groupedMapping = groupedMapping;
-    if (!layerBeingDestroyed && m_groupedMapping)
+    if (options == InvalidateLayerAndRemoveFromMapping && m_groupedMapping)
         m_groupedMapping->setNeedsGraphicsLayerUpdate(GraphicsLayerUpdateSubtree);
 }
 
