53d468373219eeadce768005572203d56a79920b
haraken@chromium.org <haraken@chromium.org>
2015-07-06 23:39:07 +0000

    Oilpan: Build fix after r198323
    BUG=
    TBR=keishi@chromium.org
    
    Review URL: https://codereview.chromium.org/1210173014.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198347 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/presentation/PresentationAvailabilityCallback.cpp
+++ third_party/WebKit/Source/modules/presentation/PresentationAvailabilityCallback.cpp
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-PresentationAvailabilityCallback::PresentationAvailabilityCallback(PassRefPtr<ScriptPromiseResolver> resolver)
+PresentationAvailabilityCallback::PresentationAvailabilityCallback(PassRefPtrWillBeRawPtr<ScriptPromiseResolver> resolver)
     : m_resolver(resolver)
 {
     ASSERT(m_resolver);
