33adb2a4131059ce6f73a56889d2c6f8d51fc2be
yiyaoliu <yiyaoliu@chromium.org>
2015-07-06 12:35:23 -0700

    Add capabilities to have multiple enable & disable groups for bubble study.
    Preparing to launch the bubble, before which some analysis  needs to be done but group name is not known yet.
    
    Only check against group name's prefix.
    
    BUG=364670
    
    Review URL: https://codereview.chromium.org/1204833002
    
    Cr-Commit-Position: refs/heads/master@{#337441}

--- chrome/browser/ui/views/session_crashed_bubble_view.cc
+++ chrome/browser/ui/views/session_crashed_bubble_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/session_crashed_bubble_view.h"
 
+#include <string>
 #include <vector>
 
 #include "base/bind.h"
@@ -13,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
 #include "base/prefs/pref_service.h"
+#include "base/strings/string_util.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/metrics/metrics_reporting_state.h"
@@ -63,7 +65,7 @@ const SkColor kTextColor = SkColorSetRGB(102, 102, 102);
 #if !defined(OS_CHROMEOS)
 // The Finch study name and group name that enables session crashed bubble UI.
 const char kEnableBubbleUIFinchName[] = "EnableSessionCrashedBubbleUI";
-const char kEnableBubbleUIGroupDisabled[] = "Disabled";
+const char kDisableBubbleUIGroupPrefix[] = "Disabled";
 #endif
 
 enum SessionCrashedBubbleHistogramValue {
@@ -99,7 +101,13 @@ bool IsBubbleUIEnabled() {
     return true;
   const std::string group_name = base::FieldTrialList::FindFullName(
       kEnableBubbleUIFinchName);
-  return group_name != kEnableBubbleUIGroupDisabled;
+
+  // When |group_name| starts with |kDisableBubbleUIGroupPrefix|, disable the
+  // bubble UI. I.e. the default behavior is bubble enabled unless overridden.
+  // This is to accommodate potential new group names without needing to change
+  // the code here.
+  return !base::StartsWith(group_name, kDisableBubbleUIGroupPrefix,
+                           base::CompareCase::SENSITIVE);
 #endif
 }
 
