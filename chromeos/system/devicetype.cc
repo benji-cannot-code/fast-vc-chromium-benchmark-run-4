ba2ac84cb01aed562f014df46baaee503f4a2d91
jdufault <jdufault@chromium.org>
2015-07-06 12:52:10 -0700

    Use the correct lsb key when fetching the CrOS device type.
    This fixes a typo where the wrong key was used. The data is exposed under
    a different key.
    
    BUG=341356
    
    Review URL: https://codereview.chromium.org/1220253007
    
    Cr-Commit-Position: refs/heads/master@{#337452}

--- chromeos/system/devicetype.cc
+++ chromeos/system/devicetype.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 namespace {
-const char kDeviceType[] = "CHROMEOS_DEVICE_TYPE";
+const char kDeviceType[] = "DEVICETYPE";
 }
 
 DeviceType GetDeviceType() {
