51fa8b65c77e28fd609c5cbfe8a2273f9e7fc77c
joedow <joedow@chromium.org>
2015-07-06 11:44:18 -0700

    Updating App Remoting Test Driver logging behavior.
    I am changing the log statements in the app remoting test driver to use
    VLOG instead of DVLOG.  The reason for this is that we build and run the
    release version of the tool on the chromoting waterfall and there isn't
    enough information in the normal output to debug failures when they
    occur.  The other option is to run the debug version of the tool on the
    waterfall but I think this will be better in the log term (helps
    debugging both versions of the tool).
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1215863004
    
    Cr-Commit-Position: refs/heads/master@{#337426}

--- remoting/test/access_token_fetcher.cc
+++ remoting/test/access_token_fetcher.cc
@@ -45,7 +45,7 @@ void AccessTokenFetcher::GetAccessTokenFromAuthCode(
   DCHECK(!callback.is_null());
   DCHECK(access_token_callback_.is_null());
 
-  DVLOG(2) << "Calling GetTokensFromAuthCode to exchange auth_code for token";
+  VLOG(2) << "Calling GetTokensFromAuthCode to exchange auth_code for token";
 
   access_token_.clear();
   refresh_token_.clear();
@@ -65,7 +65,7 @@ void AccessTokenFetcher::GetAccessTokenFromRefreshToken(
   DCHECK(!callback.is_null());
   DCHECK(access_token_callback_.is_null());
 
-  DVLOG(2) << "Calling RefreshToken to generate a new access token";
+  VLOG(2) << "Calling RefreshToken to generate a new access token";
 
   access_token_.clear();
   refresh_token_ = refresh_token;
@@ -91,10 +91,10 @@ void AccessTokenFetcher::CreateNewGaiaOAuthClientInstance() {
 void AccessTokenFetcher::OnGetTokensResponse(const std::string& refresh_token,
                                              const std::string& access_token,
                                              int expires_in_seconds) {
-  DVLOG(1) << "AccessTokenFetcher::OnGetTokensResponse() Called";
-  DVLOG(1) << "--refresh_token: " << refresh_token;
-  DVLOG(1) << "--access_token: " << access_token;
-  DVLOG(1) << "--expires_in_seconds: " << expires_in_seconds;
+  VLOG(1) << "AccessTokenFetcher::OnGetTokensResponse() Called";
+  VLOG(1) << "--refresh_token: " << refresh_token;
+  VLOG(1) << "--access_token: " << access_token;
+  VLOG(1) << "--expires_in_seconds: " << expires_in_seconds;
 
   refresh_token_ = refresh_token;
   access_token_ = access_token;
@@ -104,9 +104,9 @@ void AccessTokenFetcher::OnGetTokensResponse(const std::string& refresh_token,
 
 void AccessTokenFetcher::OnRefreshTokenResponse(const std::string& access_token,
                                                 int expires_in_seconds) {
-  DVLOG(1) << "AccessTokenFetcher::OnRefreshTokenResponse() Called";
-  DVLOG(1) << "--access_token: " << access_token;
-  DVLOG(1) << "--expires_in_seconds: " << expires_in_seconds;
+  VLOG(1) << "AccessTokenFetcher::OnRefreshTokenResponse() Called";
+  VLOG(1) << "--access_token: " << access_token;
+  VLOG(1) << "--expires_in_seconds: " << expires_in_seconds;
 
   access_token_ = access_token;
 
@@ -131,7 +131,7 @@ void AccessTokenFetcher::OnGetUserInfoResponse(
 
 void AccessTokenFetcher::OnGetTokenInfoResponse(
     scoped_ptr<base::DictionaryValue> token_info) {
-  DVLOG(1) << "AccessTokenFetcher::OnGetTokenInfoResponse() Called";
+  VLOG(1) << "AccessTokenFetcher::OnGetTokenInfoResponse() Called";
 
   std::string error_string;
   std::string error_description;
@@ -148,7 +148,7 @@ void AccessTokenFetcher::OnGetTokenInfoResponse(
     access_token_.clear();
     refresh_token_.clear();
   } else {
-    DVLOG(1) << "Access Token has been validated";
+    VLOG(1) << "Access Token has been validated";
   }
 
   base::ResetAndReturn(&access_token_callback_)
@@ -177,7 +177,7 @@ void AccessTokenFetcher::OnNetworkError(int response_code) {
 }
 
 void AccessTokenFetcher::ValidateAccessToken() {
-  DVLOG(2) << "Calling GetTokenInfo to validate access token";
+  VLOG(2) << "Calling GetTokenInfo to validate access token";
 
   // Create a new GaiaOAuthClient for each request to GAIA.
   CreateNewGaiaOAuthClientInstance();
