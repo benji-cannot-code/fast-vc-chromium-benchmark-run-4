64d58f304714e6375ccc1e96927ab253802c299b
vasilii <vasilii@chromium.org>
2015-07-06 03:25:19 -0700

    Revert of Reset PYTHONPATH in runtest_wrapper.py (patchset #5 id:80001 of https://codereview.chromium.org/1224593002/)
    Reason for revert:
    Failed telemetry_unittests on Mac10.8 Tests (http://build.chromium.org/p/chromium.mac/builders/Mac10.8%20Tests/builds/4576):
    
    [1/1] telemetry.telemetry_dependencies_unittest.TelemetryDependenciesTest.testNoNewTelemetryDependencies failed unexpectedly 10.7608s:
      Traceback (most recent call last):
        File "/Volumes/data/b/build/slave/Mac10_8_Tests/build/src/tools/telemetry/telemetry/telemetry_dependencies_unittest.py", line 57, in testNoNewTelemetryDependencies
          'Extra dependencies:\n%s' % '\n'.join(extra_dep_paths))
      AssertionError: Your patch adds new dependencies to telemetry. Please contact aiolos@,dtu@, or nednguyen@ on how to proceed with this change. Extra dependencies:
      /Volumes/data/b/build/slave/Mac10_8_Tests/build/src/third_party/ply/README.chromium
      /Volumes/data/b/build/slave/Mac10_8_Tests/build/src/third_party/ply/license.patch
      /Volumes/data/b/build/slave/Mac10_8_Tests/build/src/third_party/ply/__init__.py
      /Volumes/data/b/build/slave/Mac10_8_Tests/build/src/third_party/ply/LICENSE
      /Volumes/data/b/build/slave/Mac10_8_Tests/build/src/third_party/ply/lex.py
      /Volumes/data/b/build/slave/Mac10_8_Tests/build/src/third_party/ply/README
      /Volumes/data/b/build/slave/Mac10_8_Tests/build/src/third_party/ply/yacc.py
    
    Original issue's description:
    > Reset PYTHONPATH in runtest_wrapper.py
    >
    > This ensures we won't use build-side python modules,
    > and that any changes in src-side modules do take effect.
    >
    > BUG=506498
    >
    > Committed: https://crrev.com/2bafb027d8916c84ea4e70f933b9df7eed4a8a87
    > Cr-Commit-Position: refs/heads/master@{#337372}
    
    TBR=akuegel@chromium.org,sergiyb@chromium.org,phajdan.jr@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=506498
    
    Review URL: https://codereview.chromium.org/1223883002
    
    Cr-Commit-Position: refs/heads/master@{#337377}

--- infra/scripts/legacy/site_config/config.py
+++ infra/scripts/legacy/site_config/config.py
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """
 
 import os
+from twisted.spread import banana
 
 # DatabaseSetup was moved. Import it for backward compatibility
 from common.chromium_utils import DatabaseSetup  # pylint: disable=W0611
@@ -15,5 +16,18 @@ from config_bootstrap import Master # pylint: disable=W0403,W0611
 
 SITE_CONFIG_PATH = os.path.abspath(os.path.dirname(__file__))
 
+# By default, the banana's string size limit is 640kb, which is unsufficient
+# when passing diff's around. Raise it to 100megs. Do this here since the limit
+# is enforced on both the server and the client so both need to raise the
+# limit.
+banana.SIZE_LIMIT = 100 * 1024 * 1024
+
+
 def SiteConfigPath():
   return SITE_CONFIG_PATH
+
+
+
+
+
+
