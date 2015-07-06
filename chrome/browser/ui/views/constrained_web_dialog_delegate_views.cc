af081b49b2a805a4499c322784acb79cebca9a4c
hcarmona <hcarmona@chromium.org>
2015-07-06 12:56:48 -0700

    No more PopupManager::ShowModalDialog calls
    Making sure no one is using the PopupManager as a step in stealing the
    PopupManager for bubbles.
    
    BUG=496955
    
    Review URL: https://codereview.chromium.org/1211233002
    
    Cr-Commit-Position: refs/heads/master@{#337455}

--- chrome/browser/ui/views/constrained_web_dialog_delegate_views.cc
+++ chrome/browser/ui/views/constrained_web_dialog_delegate_views.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/webui/chrome_web_contents_handler.h"
 #include "components/constrained_window/constrained_window_views.h"
-#include "components/web_modal/popup_manager.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
 #include "content/public/browser/native_web_keyboard_event.h"
 #include "content/public/browser/render_view_host.h"
@@ -240,11 +239,9 @@ class ConstrainedWebDialogDelegateViewViews
     if (!max_size_.IsEmpty()) {
       EnableAutoResize();
       if (initiator_observer_.web_contents()) {
-        web_modal::PopupManager* popup_manager =
-            web_modal::PopupManager::FromWebContents(
-                initiator_observer_.web_contents());
-        popup_manager->ShowModalDialog(GetWidget()->GetNativeWindow(),
-                                       initiator_observer_.web_contents());
+        web_modal::WebContentsModalDialogManager::FromWebContents(
+            initiator_observer_.web_contents())
+            ->ShowModalDialog(GetWidget()->GetNativeWindow());
       }
     }
   }
