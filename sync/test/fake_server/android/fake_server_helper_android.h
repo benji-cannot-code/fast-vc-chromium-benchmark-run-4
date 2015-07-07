c617740ecf09e2558cd3f0c6fbaf624a1d119d7f
maxbogue <maxbogue@chromium.org>
2015-07-07 11:05:47 -0700

    [Sync] Test downloading a bookmark modification on Android.
    BUG=480604
    
    Review URL: https://codereview.chromium.org/1224813002
    
    Cr-Commit-Position: refs/heads/master@{#337641}

--- sync/test/fake_server/android/fake_server_helper_android.h
+++ sync/test/fake_server/android/fake_server_helper_android.h
@@ -64,6 +64,15 @@ class FakeServerHelperAndroid {
                             jstring url,
                             jstring parent_id);
 
+  // Modify the BookmarkEntity with |entity_id| on |fake_server|.
+  void ModifyBookmarkEntity(JNIEnv* env,
+                            jobject obj,
+                            jlong fake_server,
+                            jstring entity_id,
+                            jstring title,
+                            jstring url,
+                            jstring parent_id);
+
   // Returns the bookmark bar folder ID.
   base::android::ScopedJavaLocalRef<jstring> GetBookmarkBarFolderId(
       JNIEnv* env,
@@ -79,6 +88,13 @@ class FakeServerHelperAndroid {
 
  private:
   virtual ~FakeServerHelperAndroid();
+
+  // Creates a bookmark entity.
+  scoped_ptr<fake_server::FakeServerEntity> CreateBookmarkEntity(
+      JNIEnv* env,
+      jstring title,
+      jstring url,
+      jstring parent_id);
 };
 
 #endif  // SYNC_TEST_FAKE_SERVER_ANDROID_FAKE_SERVER_HELPER_ANDROID
