a633767a3a6a488aa939289534ac036e42225d14
maxbogue <maxbogue@chromium.org>
2015-07-07 15:18:33 -0700

    [Sync] Test downloading a modified autofill entry on Android.
    BUG=480604
    
    Review URL: https://codereview.chromium.org/1221333009
    
    Cr-Commit-Position: refs/heads/master@{#337697}

--- chrome/android/sync_shell/javatests/src/org/chromium/chrome/browser/sync/AutofillTest.java
+++ chrome/android/sync_shell/javatests/src/org/chromium/chrome/browser/sync/AutofillTest.java
@@ -34,6 +34,7 @@ public class AutofillTest extends SyncTestBase {
 
     private static final String STREET = "1600 Amphitheatre Pkwy";
     private static final String CITY = "Mountain View";
+    private static final String MODIFIED_CITY = "Sunnyvale";
     private static final String STATE = "CA";
     private static final String ZIP = "94043";
 
@@ -82,6 +83,25 @@ public class AutofillTest extends SyncTestBase {
         assertEquals("The wrong zip was found for the autofill.", ZIP, autofill.zip);
     }
 
+    // Test syncing an autofill profile modification from server to client.
+    @LargeTest
+    @Feature({"Sync"})
+    public void testDownloadAutofillModification() throws Exception {
+        // Add the entity to test modifying.
+        EntitySpecifics specifics = addServerAutofillProfile(STREET, CITY, STATE, ZIP);
+        SyncTestUtil.triggerSyncAndWaitForCompletion(mContext);
+        assertServerAutofillProfileCountWithName(1, STREET);
+        assertClientAutofillProfileCount(1);
+
+        // Modify on server, sync, and verify modification locally.
+        Autofill autofill = getClientAutofillProfiles().get(0);
+        specifics.autofillProfile.addressHomeCity = MODIFIED_CITY;
+        mFakeServerHelper.modifyEntitySpecifics(autofill.id, specifics);
+        SyncTestUtil.triggerSyncAndWaitForCompletion(mContext);
+        Autofill modifiedAutofill = getClientAutofillProfiles().get(0);
+        assertEquals("The city was not modified.", MODIFIED_CITY, modifiedAutofill.city);
+    }
+
     // Test syncing an autofill profile deletion from server to client.
     @LargeTest
     @Feature({"Sync"})
@@ -112,7 +132,8 @@ public class AutofillTest extends SyncTestBase {
         assertClientAutofillProfileCount(0);
     }
 
-    private void addServerAutofillProfile(String street, String city, String state, String zip) {
+    private EntitySpecifics addServerAutofillProfile(
+            String street, String city, String state, String zip) {
         EntitySpecifics specifics = new EntitySpecifics();
         AutofillProfileSpecifics profile = new AutofillProfileSpecifics();
         profile.guid = GUID;
@@ -123,6 +144,7 @@ public class AutofillTest extends SyncTestBase {
         profile.addressHomeZip = zip;
         specifics.autofillProfile = profile;
         mFakeServerHelper.injectUniqueClientEntity(street /* name */, specifics);
+        return specifics;
     }
 
     private List<Autofill> getClientAutofillProfiles() throws JSONException {
