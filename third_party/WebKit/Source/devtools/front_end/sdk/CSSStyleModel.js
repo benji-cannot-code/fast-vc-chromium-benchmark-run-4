48ecc8de5864863a643f518ab62a703da7560e00
lushnikov@chromium.org <lushnikov@chromium.org>
2015-07-07 17:41:31 +0000

    DevTools: fix style formatter
    The style formatter behaved badly whenever it was hit with a complex
    property values.
    
    R=pfeldman
    
    Review URL: https://codereview.chromium.org/1217473013
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198414 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/CSSStyleModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/CSSStyleModel.js
@@ -1321,7 +1321,8 @@ WebInspector.CSSProperty.prototype = {
         function processTokens(tokenizerFactory)
         {
             var tokenize = tokenizerFactory.createTokenizer("text/css");
-            tokenize(styleText, processToken);
+            tokenize("*{" + styleText + "}", processToken);
+            result = result.slice(0, result.length - 1);
             callback(result + (indentation ? "\n" + endIndentation : ""));
         }
 
@@ -1336,10 +1337,12 @@ WebInspector.CSSProperty.prototype = {
          */
         function processToken(token, tokenType, column, newColumn)
         {
+            if (newColumn <= 2)
+                return;
             var isSemicolon = token === ";";
             if (isSemicolon && lastWasSemicolon)
                 return;
-            lastWasSemicolon = isSemicolon || (lastWasSemicolon && tokenType === "css-comment") || (lastWasSemicolon && !token.trim());
+            lastWasSemicolon = isSemicolon || (lastWasSemicolon && tokenType && tokenType.includes("css-comment")) || (lastWasSemicolon && !token.trim());
 
             // No formatting, only remove dupe ;
             if (!indentation) {
@@ -1350,7 +1353,7 @@ WebInspector.CSSProperty.prototype = {
             // Format line breaks.
             if (!insideProperty && !token.trim())
                 return;
-            if (tokenType === "css-comment" && token.includes(":") && token.includes(";")) {
+            if (tokenType && tokenType.includes("css-comment") && token.includes(":") && token.includes(";")) {
                 result += "\n" + indentation + token;
                 insideProperty = false;
                 return;
@@ -1359,13 +1362,13 @@ WebInspector.CSSProperty.prototype = {
             if (isSemicolon)
                 insideProperty = false;
 
-            if (tokenType === "css-meta" || (tokenType === "css-tag" && !lastWasMeta)) {
+            if (tokenType && (tokenType.includes("css-meta") || (tokenType.includes("css-property") && !lastWasMeta))) {
                 result += "\n" + indentation;
                 insideProperty = true;
             }
             result += token;
 
-            lastWasMeta = tokenType === "css-meta";
+            lastWasMeta = tokenType && tokenType.includes("css-meta");
         }
     },
 
