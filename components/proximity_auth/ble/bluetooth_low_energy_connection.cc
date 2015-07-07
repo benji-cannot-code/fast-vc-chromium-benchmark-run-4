d6060553491ce827f4cd6006e8bd9c92c97ffedc
sacomoto <sacomoto@chromium.org>
2015-07-07 04:25:07 -0700

    Fixing a crash in ProximityAuthBleSystem.
    This CL fixes a bug causing crashes in
    proximity_auth::ProximityAuthBleSystem:
    
    - BluetoothLowEnergyConnection::Disconnect() should only change its
    status to DISCONNECTED after performing all operations. Otherwise, it
    will trigger a notification to its observers, which may then destroy the
    instance, causing a crash when |BluetoothLowEnergyConnection::Disconnect()|
    resume executing.
    
    BUG=506196
    
    Review URL: https://codereview.chromium.org/1222563002
    
    Cr-Commit-Position: refs/heads/master@{#337586}

--- components/proximity_auth/ble/bluetooth_low_energy_connection.cc
+++ components/proximity_auth/ble/bluetooth_low_energy_connection.cc
@@ -97,7 +97,6 @@ void BluetoothLowEnergyConnection::Disconnect() {
   if (sub_status_ != SubStatus::DISCONNECTED) {
     ClearWriteRequestsQueue();
     StopNotifySession();
-    SetSubStatus(SubStatus::DISCONNECTED);
     if (gatt_connection_) {
       PA_LOG(INFO) << "Disconnect from device "
                    << gatt_connection_->GetDeviceAddress();
@@ -105,6 +104,10 @@ void BluetoothLowEnergyConnection::Disconnect() {
       // Destroying BluetoothGattConnection also disconnects it.
       gatt_connection_.reset();
     }
+    // Only transition to the DISCONNECTED state after perfoming all necessary
+    // operations. Otherwise, it'll trigger observers that can pontentially
+    // destroy the current instance (causing a crash).
+    SetSubStatus(SubStatus::DISCONNECTED);
   }
 }
 
