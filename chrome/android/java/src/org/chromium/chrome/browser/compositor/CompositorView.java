ea52f947c2a44ccb67891f8bb5e13c9fa2449ec3
zbowling <zbowling@chromium.org>
2015-07-06 20:54:05 -0700

    Don't preload rarely seen large images
    The layer cardback image uses 180k memory decompressed but only seen on
    merged tab switcher on non-low memory devices.
    
    Also tweak the preloading logic to not preload tablet only resources on
    phones.
    
    BUG=503437
    
    Review URL: https://codereview.chromium.org/1217893013
    
    Cr-Commit-Position: refs/heads/master@{#337557}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorView.java
@@ -383,7 +383,7 @@ public class CompositorView
             // Attempt to prefetch any necessary resources
             mResourceManager.preloadResources(AndroidResourceType.STATIC,
                     StaticResourcePreloads.getSynchronousResources(getContext()),
-                    StaticResourcePreloads.getAsynchronousResources());
+                    StaticResourcePreloads.getAsynchronousResources(getContext()));
             mPreloadedResources = true;
         }
 
