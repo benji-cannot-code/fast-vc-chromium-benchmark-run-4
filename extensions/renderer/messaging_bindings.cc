336f8c9dd8d58007b5d2a62905012202257e9b68
bashi <bashi@chromium.org>
2015-07-07 00:33:25 -0700

    extensions: Add undefined check in DispatchOnConnectToScriptContext()
    CallModuleMethod() could return undefined so call site should check it before casting a specific type.
    
    BUG=505904
    
    Review URL: https://codereview.chromium.org/1221923002
    
    Cr-Commit-Position: refs/heads/master@{#337566}

--- extensions/renderer/messaging_bindings.cc
+++ extensions/renderer/messaging_bindings.cc
@@ -55,6 +55,7 @@ namespace extensions {
 
 using v8_helpers::ToV8String;
 using v8_helpers::ToV8StringUnsafe;
+using v8_helpers::IsEmptyOrUndefied;
 
 namespace {
 
@@ -430,7 +431,7 @@ void DispatchOnConnectToScriptContext(
       script_context->module_system()->CallModuleMethod(
           "messaging", "dispatchOnConnect", arraysize(arguments), arguments);
 
-  if (!retval.IsEmpty()) {
+  if (!IsEmptyOrUndefied(retval)) {
     CHECK(retval->IsBoolean());
     *port_created |= retval.As<v8::Boolean>()->Value();
   } else {
