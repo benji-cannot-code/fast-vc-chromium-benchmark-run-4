c9665b2e623cffaef2b58701cfd5b4414cc084be
eugenebut <eugenebut@chromium.org>
2015-07-07 10:15:32 -0700

    Updated predeclarations to match changes in Chrome for iOS.
    BUG=404741
    
    Review URL: https://codereview.chromium.org/1221303006
    
    Cr-Commit-Position: refs/heads/master@{#337628}

--- components/autofill/core/browser/autofill_manager.h
+++ components/autofill/core/browser/autofill_manager.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/wallet/real_pan_wallet_client.h"
 #include "components/autofill/core/common/form_data.h"
 
-class ChromeWebTest;
+class ChromeUIWebViewWebTest;
 class ChromeWKWebViewWebTest;
 
 namespace gfx {
@@ -452,7 +452,7 @@ class AutofillManager : public AutofillDownloadManager::Observer,
 
   friend class AutofillManagerTest;
   friend class FormStructureBrowserTest;
-  friend class FormStructureBrowserTestIos<ChromeWebTest>;
+  friend class FormStructureBrowserTestIos<ChromeUIWebViewWebTest>;
   friend class FormStructureBrowserTestIos<ChromeWKWebViewWebTest>;
   FRIEND_TEST_ALL_PREFIXES(AutofillManagerTest,
                            DeterminePossibleFieldTypesForUpload);
