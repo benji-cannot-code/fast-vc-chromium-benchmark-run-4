768ea866493a4d31be343111d8ed4253bfd51405
yosin@chromium.org <yosin@chromium.org>
2015-07-07 10:08:35 +0000

    Get rid of SimplifiedBackwardsTextIterator constructor with Range parameter
    This patch gets rid of |SimplifiedBackwardsTextIterator| constructor with
    |Range| parameter for a preparation of making |SimplifiedBackwardsTextIterator|
    to work both DOM tree and composed tree with templatization like |TextIterator|.
    
    This patch is a preparation of making Find-In-Page to work on composed tree,
    http://crrev.com/1215903005
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1225623005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198385 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/iterators/SimplifiedBackwardsTextIterator.h
+++ third_party/WebKit/Source/core/editing/iterators/SimplifiedBackwardsTextIterator.h
@@ -43,7 +43,6 @@ class LayoutTextFragment;
 class SimplifiedBackwardsTextIterator {
     STACK_ALLOCATED();
 public:
-    explicit SimplifiedBackwardsTextIterator(const Range*, TextIteratorBehaviorFlags = TextIteratorDefaultBehavior);
     SimplifiedBackwardsTextIterator(const Position& start, const Position& end, TextIteratorBehaviorFlags = TextIteratorDefaultBehavior);
 
     bool atEnd() const { return !m_positionNode || m_shouldStop; }
