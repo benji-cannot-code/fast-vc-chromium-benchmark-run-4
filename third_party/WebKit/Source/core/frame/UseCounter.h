31837cce62a57393bcb436c7d48e9c5038340ade
falken@chromium.org <falken@chromium.org>
2015-07-08 03:12:07 +0000

    Service Worker: Undeprecate Client.postMessage
    This was deprecated in r190286 in anticipation of API changes.
    We're now shipping the new API, so undeprecate.
    
    BUG=498596,457094
    
    Review URL: https://codereview.chromium.org/1215643005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198451 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -590,7 +590,6 @@ public:
         SVGSVGElementUnsuspendRedraw = 687,
         SVGSVGElementUnsuspendRedrawAll = 688,
         AudioContextClose = 689,
-        ServiceWorkerClientPostMessage = 690,
         CSSZoomNotEqualToOne = 691,
         SVGGraphicsElementGetTransformToElement = 692,
         // The above items are available in M42 branch.
