12d717dbf0ad23a73714e20819002e4ac8b5de76
Antony Sargent <asargent@chromium.org>
2015-07-08 11:28:44 -0700

    Revert "Start the migration of passwords from the Keychain."
    This reverts commit b312da5608ac316b7e49459c906b5a5d72332776.
    
    This was causing failures on the Mac Asan 64 bot. See crbug.com/508227
    for details.
    
    BUG=508227,466638
    
    Review URL: https://codereview.chromium.org/1211253015.
    
    Cr-Commit-Position: refs/heads/master@{#337864}

--- components/password_manager/core/browser/password_manager.cc
+++ components/password_manager/core/browser/password_manager.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/password_form_field_prediction_map.h"
 #include "components/password_manager/core/browser/affiliation_utils.h"
 #include "components/password_manager/core/browser/browser_save_password_progress_logger.h"
-#include "components/password_manager/core/browser/keychain_migration_status_mac.h"
 #include "components/password_manager/core/browser/password_autofill_manager.h"
 #include "components/password_manager/core/browser/password_form_manager.h"
 #include "components/password_manager/core/browser/password_manager_client.h"
@@ -144,10 +143,6 @@ void PasswordManager::RegisterProfilePrefs(
   registry->RegisterBooleanPref(prefs::kPasswordManagerAllowShowPasswords,
                                 true);
   registry->RegisterListPref(prefs::kPasswordManagerGroupsForDomains);
-#if defined(OS_MACOSX)
-  registry->RegisterIntegerPref(prefs::kKeychainMigrationStatus,
-                                static_cast<int>(MigrationStatus::NOT_STARTED));
-#endif
 }
 
 #if defined(OS_WIN)
