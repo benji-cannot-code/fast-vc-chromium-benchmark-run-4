c18a8addb3384df41ae8ce4531913aa839b8609c
jww@chromium.org <jww@chromium.org>
2015-07-08 19:18:33 +0000

    Add Suborigin CORS style tests
    Adds a few LayoutTests for Suborigin loads of CORS styles. Also moves
    the XHR tests into the same test file.
    
    TBR=jochen@chromium.org
    BUG=336894
    
    Review URL: https://codereview.chromium.org/1226433004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198528 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/security/resources/cors-style.php
+++ third_party/WebKit/LayoutTests/http/tests/security/resources/cors-style.php
@@ -1,7 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-if (strtolower($_GET["cors"]) != "false") {
-    header("Access-Control-Allow-Origin: http://127.0.0.1:8000");
+$cors_arg = strtolower($_GET["cors"]);
+if ($cors_arg != "false") {
+    if ($cors_arg == "" || $cors_arg == "true") {
+        header("Access-Control-Allow-Origin: http://127.0.0.1:8000");
+    } else {
+        header("Access-Control-Allow-Origin: " . $cors_arg . "");
+    }
 }
 if (strtolower($_GET["credentials"]) == "true") {
     header("Access-Control-Allow-Credentials: true");
