2496d99601b818c45378b18138530415354d90ec
yosin@chromium.org <yosin@chromium.org>
2015-07-08 09:10:26 +0000

    Templatize BackwardsCharacterIterator class
    Now |BackwardsCharacterIterator| works only on DOM tree. This patch
    templatizes |BackwardsCharacterIterator| class and end enables it to allow
    other node-traversing strategies, such as composed tree.
    
    This patch is a preparation of making Find-In-Page to work on composed tree,
    http://crrev.com/1215903005
    
    This patch is similar to http://crrev.com/1208993014, which templatize
    |SimplifiedBackwardsTextIterator|.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1211873011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198483 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/iterators/BackwardsCharacterIterator.cpp
+++ third_party/WebKit/Source/core/editing/iterators/BackwardsCharacterIterator.cpp
@@ -30,7 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-BackwardsCharacterIterator::BackwardsCharacterIterator(const Position& start, const Position& end, TextIteratorBehaviorFlags behavior)
+template <typename Strategy>
+BackwardsCharacterIteratorAlgorithm<Strategy>::BackwardsCharacterIteratorAlgorithm(const PositionAlgorithm<Strategy>& start, const PositionAlgorithm<Strategy>& end, TextIteratorBehaviorFlags behavior)
     : m_offset(0)
     , m_runOffset(0)
     , m_atBreak(true)
@@ -40,19 +41,21 @@ BackwardsCharacterIterator::BackwardsCharacterIterator(const Position& start, co
         m_textIterator.advance();
 }
 
-Position BackwardsCharacterIterator::endPosition() const
+template <typename Strategy>
+PositionAlgorithm<Strategy> BackwardsCharacterIteratorAlgorithm<Strategy>::endPosition() const
 {
     if (!m_textIterator.atEnd()) {
         if (m_textIterator.length() > 1) {
             Node* n = m_textIterator.startContainer();
-            return createLegacyEditingPosition(n, m_textIterator.endOffset() - m_runOffset);
+            return PositionAlgorithm<Strategy>::createLegacyEditingPosition(n, m_textIterator.endOffset() - m_runOffset);
         }
         ASSERT(!m_runOffset);
     }
     return m_textIterator.endPosition();
 }
 
-void BackwardsCharacterIterator::advance(int count)
+template <typename Strategy>
+void BackwardsCharacterIteratorAlgorithm<Strategy>::advance(int count)
 {
     if (count <= 0) {
         ASSERT(!count);
@@ -90,5 +93,7 @@ void BackwardsCharacterIterator::advance(int count)
     m_atBreak = true;
     m_runOffset = 0;
 }
+
+template class CORE_TEMPLATE_EXPORT BackwardsCharacterIteratorAlgorithm<EditingStrategy>;
 
 } // namespace blink
--- third_party/WebKit/Source/core/editing/iterators/BackwardsCharacterIterator.h
+++ third_party/WebKit/Source/core/editing/iterators/BackwardsCharacterIterator.h
@@ -32,24 +32,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class BackwardsCharacterIterator {
+template <typename Strategy>
+class BackwardsCharacterIteratorAlgorithm {
     STACK_ALLOCATED();
 public:
-    BackwardsCharacterIterator(const Position&, const Position&, TextIteratorBehaviorFlags = TextIteratorDefaultBehavior);
+    BackwardsCharacterIteratorAlgorithm(const PositionAlgorithm<Strategy>&, const PositionAlgorithm<Strategy>&, TextIteratorBehaviorFlags = TextIteratorDefaultBehavior);
 
     void advance(int);
 
     bool atEnd() const { return m_textIterator.atEnd(); }
 
-    Position endPosition() const;
+    PositionAlgorithm<Strategy> endPosition() const;
 
 private:
     int m_offset;
     int m_runOffset;
     bool m_atBreak;
 
-    SimplifiedBackwardsTextIterator m_textIterator;
+    SimplifiedBackwardsTextIteratorAlgorithm<Strategy> m_textIterator;
 };
+
+extern template class CORE_EXTERN_TEMPLATE_EXPORT BackwardsCharacterIteratorAlgorithm<EditingStrategy>;
+using BackwardsCharacterIterator = BackwardsCharacterIteratorAlgorithm<EditingStrategy>;
 
 } // namespace blink
 
