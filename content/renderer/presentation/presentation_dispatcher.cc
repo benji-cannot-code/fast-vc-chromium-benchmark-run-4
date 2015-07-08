cd5b5c6791c2337a19f2b3943bbe59e62c245c35
mlamouri <mlamouri@chromium.org>
2015-07-08 11:40:56 -0700

    Presentation API: ignore presentation id passed to startSession().
    The presentation id will soon no longer be passed by Blink.
    
    This is part of a suite of three CLs:
    1. <this>
    2. https://codereview.chromium.org/1225523002
    3. https://codereview.chromium.org/1221093004
    
    BUG=502047
    
    Review URL: https://codereview.chromium.org/1208083004
    
    Cr-Commit-Position: refs/heads/master@{#337868}

--- content/renderer/presentation/presentation_dispatcher.cc
+++ content/renderer/presentation/presentation_dispatcher.cc
@@ -115,6 +115,12 @@ void PresentationDispatcher::startSession(
     const blink::WebString& presentationUrl,
     const blink::WebString& presentationId,
     blink::WebPresentationSessionClientCallbacks* callback) {
+  startSession(presentationUrl, callback);
+}
+
+void PresentationDispatcher::startSession(
+    const blink::WebString& presentationUrl,
+    blink::WebPresentationSessionClientCallbacks* callback) {
   DCHECK(callback);
   ConnectToPresentationServiceIfNeeded();
 
@@ -123,7 +129,6 @@ void PresentationDispatcher::startSession(
   // to be destroyed so we transfer its ownership to the mojo callback.
   presentation_service_->StartSession(
       presentationUrl.utf8(),
-      presentationId.utf8(),
       base::Bind(&PresentationDispatcher::OnSessionCreated,
           base::Unretained(this),
           base::Owned(callback)));
