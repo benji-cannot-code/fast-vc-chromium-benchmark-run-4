a726594d1b2505857eef18d2d48fedb9bf42a5e8
zqiu <zqiu@chromium.org>
2015-07-07 19:08:30 -0700

    dbus: expose bus connection unique name
    Add an API to Bus class to expose the unique name of the bus connection.
    The function will return an empty string if bus connection is not
    established.
    
    BUG=chromium:507718
    TEST=unittest
    
    Review URL: https://codereview.chromium.org/1222073003
    
    Cr-Commit-Position: refs/heads/master@{#337732}

--- dbus/bus.cc
+++ dbus/bus.cc
@@ -1021,6 +1021,12 @@ void Bus::UnlistenForServiceOwnerChangeInternal(
     RemoveFilterFunction(Bus::OnServiceOwnerChangedFilter, this);
 }
 
+std::string Bus::GetConnectionName() {
+  if (!connection_)
+    return "";
+  return dbus_bus_get_unique_name(connection_);
+}
+
 dbus_bool_t Bus::OnAddWatch(DBusWatch* raw_watch) {
   AssertOnDBusThread();
 
