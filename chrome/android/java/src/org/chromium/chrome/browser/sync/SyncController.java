7a458dfee20a165b5cf9510b4ba504d5df1e9699
maxbogue <maxbogue@chromium.org>
2015-07-08 13:47:29 -0700

    [Sync] Clean up the Android PSS start and stop methods.
    - Rename enableSync to requestStart and remove its short circuit.
    - Rename disableSync to requestStop and remove its short circuit.
    - Remove syncSignIn and replace calls to it with requestStart.
    
    The only technical result of these changes should be
    PSS::RequestStart and PSS::RequestStop being called sometimes
    when they were previously assumed to be unnecessary. I do not
    believe this will have any real functional effect.
    
    BUG=495192,497518
    
    Review URL: https://codereview.chromium.org/1191733002
    
    Cr-Commit-Position: refs/heads/master@{#337903}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/SyncController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/SyncController.java
@@ -126,7 +126,6 @@ public class SyncController implements ApplicationStateListener,
             public void onSigninComplete() {
                 SigninManager.get(mContext).logInSignedInUser();
                 mProfileSyncService.setSetupInProgress(false);
-                mProfileSyncService.syncSignIn();
                 start();
             }
 
@@ -158,7 +157,7 @@ public class SyncController implements ApplicationStateListener,
         if (AndroidSyncSettings.isMasterSyncEnabled(mContext)) {
             Log.d(TAG, "Enabling sync");
             InvalidationController.get(mContext).ensureStartedAndUpdateRegisteredTypes();
-            mProfileSyncService.enableSync();
+            mProfileSyncService.requestStart();
             AndroidSyncSettings.enableChromeSync(mContext);
         }
     }
@@ -172,7 +171,7 @@ public class SyncController implements ApplicationStateListener,
         ThreadUtils.assertOnUiThread();
         Log.d(TAG, "Disabling sync");
         InvalidationController.get(mContext).stop();
-        mProfileSyncService.disableSync();
+        mProfileSyncService.requestStop();
         if (AndroidSyncSettings.isMasterSyncEnabled(mContext)) {
             // Only disable Android's Chrome sync setting if we weren't disabled
             // by the master sync setting. This way, when master sync is enabled
