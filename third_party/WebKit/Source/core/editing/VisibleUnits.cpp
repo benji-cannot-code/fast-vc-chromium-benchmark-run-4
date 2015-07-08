6f3d1135f61ef80daef8fe14b34ef5bd2d51cd45
esprehn@chromium.org <esprehn@chromium.org>
2015-07-08 21:48:07 +0000

    Move editing specific nextLeafNode and previousLeafNode to htmlediting.h
    This fixes an ancient fixme and makes the interface to Node simpler
    and easier to understand since the leaf concept for these methods is
    very specific to editing.
    
    Renamed the methods to nextAtomicLeafNode and previousAtomicLeafNode
    to reflect their purpose.
    
    Review URL: https://codereview.chromium.org/1210283004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198537 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/VisibleUnits.cpp
+++ third_party/WebKit/Source/core/editing/VisibleUnits.cpp
@@ -63,11 +63,11 @@ using namespace WTF::Unicode;
 static Node* previousLeafWithSameEditability(Node* node, EditableType editableType)
 {
     bool editable = node->hasEditableStyle(editableType);
-    node = node->previousLeafNode();
+    node = previousAtomicLeafNode(*node);
     while (node) {
         if (editable == node->hasEditableStyle(editableType))
             return node;
-        node = node->previousLeafNode();
+        node = previousAtomicLeafNode(*node);
     }
     return 0;
 }
@@ -78,11 +78,11 @@ static Node* nextLeafWithSameEditability(Node* node, EditableType editableType =
         return 0;
 
     bool editable = node->hasEditableStyle(editableType);
-    node = node->nextLeafNode();
+    node = nextAtomicLeafNode(*node);
     while (node) {
         if (editable == node->hasEditableStyle(editableType))
             return node;
-        node = node->nextLeafNode();
+        node = nextAtomicLeafNode(*node);
     }
     return 0;
 }
