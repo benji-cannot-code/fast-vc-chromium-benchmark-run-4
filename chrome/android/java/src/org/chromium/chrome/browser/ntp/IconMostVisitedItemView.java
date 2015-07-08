6910b538924a172a39ee686fd4ddb196a113741f
kkimlabs <kkimlabs@chromium.org>
2015-07-08 12:53:30 -0700

    [Android] Use standard selector for NTP most visited item.
    This will give a ripple effect on L and above.
    Also it changes the selection color from blue to gray.
    
    BUG=498914,436301
    
    Review URL: https://codereview.chromium.org/1176033002
    
    Cr-Commit-Position: refs/heads/master@{#337887}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/IconMostVisitedItemView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/IconMostVisitedItemView.java
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ntp;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
@@ -22,10 +19,6 @@ import org.chromium.chrome.R;
  * icon isn't available, displays a rounded rectangle with a single letter in its place.
  */
 public class IconMostVisitedItemView extends FrameLayout {
-
-    private static final int HIGHLIGHT_COLOR = 0x550099cc;
-
-    private boolean mLastDrawnPressed;
 
     /**
      * Constructor for inflating from XML.
@@ -47,28 +40,5 @@ public class IconMostVisitedItemView extends FrameLayout {
     public void setIcon(Drawable icon) {
         ImageView iconView = (ImageView) findViewById(R.id.most_visited_icon);
         iconView.setImageDrawable(icon);
-    }
-
-    @Override
-    public void setPressed(boolean pressed) {
-        super.setPressed(pressed);
-        if (isPressed() != mLastDrawnPressed) invalidate();
-    }
-
-    @Override
-    protected void dispatchDraw(Canvas canvas) {
-        super.dispatchDraw(canvas);
-
-        // Draw highlight overlay over the child views when this view is pressed.
-        if (isPressed()) {
-            Paint highlightPaint = new Paint();
-            highlightPaint.setColor(HIGHLIGHT_COLOR);
-            highlightPaint.setAntiAlias(true);
-            RectF highlightRect = new RectF(0, 0, getWidth(), getHeight());
-            int cornerRadius = getResources().getDimensionPixelOffset(
-                    R.dimen.most_visited_bg_corner_radius);
-            canvas.drawRoundRect(highlightRect, cornerRadius, cornerRadius, highlightPaint);
-        }
-        mLastDrawnPressed = isPressed();
     }
 }
