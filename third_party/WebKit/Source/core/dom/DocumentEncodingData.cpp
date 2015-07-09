a1e8fbed87b3b1bb3ae22d63eaf9f0570f5f018f
habib.virji@samsung.com <habib.virji@samsung.com>
2015-07-09 05:22:27 +0000

    Document attribute charset and its aliases to return UTF-8
    Document::charset returns value of the document::encodingName.
    The encodingName value is of type DocumentEncodingData, it has
    m_encoding of type TextEncoding which holds the encoding type. 
    This type was not initialized and now has been updated to always
    initialize to UTF-8 as per 
    <https://dom.spec.whatwg.org/#concept-document-encoding>.
    
    ::characterSet replicates the functionality of the charset.
    ::inputEncoding uses characterSet. It has been updated to not 
    return nullable value. 
    
    document-attribute-js-null has been updated to reflect the 
    changes. Also updated documentgetinputencoding02.js as it is
    based on old DOM3 and behavior does not match with DOM4 
    specification. 
    
    In Firefox only characterSet is supported and it returns an 
    "utf-8". It does not implement charset.
    
    In IE all characterSet and charset are supported. It returns 
    for characterSet and charset UTF-8.
    
    BUG=497982
    R=philipj, haraken
    
    Review URL: https://codereview.chromium.org/1180793002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198563 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/DocumentEncodingData.cpp
+++ third_party/WebKit/Source/core/dom/DocumentEncodingData.cpp
@@ -37,7 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 DocumentEncodingData::DocumentEncodingData()
-    : m_wasDetectedHeuristically(false)
+    : m_encoding(UTF8Encoding())
+    , m_wasDetectedHeuristically(false)
     , m_sawDecodingError(false)
 {
 }
