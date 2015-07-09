6476bfe3675c38270e779a4e66403f0011922ed3
mnaganov <mnaganov@chromium.org>
2015-07-09 13:45:21 -0700

    Check user gesture before firing an intent in WebView
    Only fire intents when navigation is initiated by the user
    (via a gesture), or by the server (a redirect). This curbs
    down attempts to launch intents purely by JS code, which
    lead to a poor UX -- tasks appearing from nowhere.
    
    BUG=501633
    
    Review URL: https://codereview.chromium.org/1180223004
    
    Cr-Commit-Position: refs/heads/master@{#338133}

--- android_webview/java/src/org/chromium/android_webview/AwContentViewClient.java
+++ android_webview/java/src/org/chromium/android_webview/AwContentViewClient.java
@@ -48,7 +48,9 @@ public class AwContentViewClient extends ContentViewClient implements ContentVid
             return;
         }
 
-        AwContentsClient.sendBrowsingIntent(context, contentUrl);
+        // Comes from WebViewImpl::detectContentOnTouch in Blink, so must be user-initiated, and
+        // isn't a redirect.
+        AwContentsClient.sendBrowsingIntent(context, contentUrl, true, false);
     }
 
     @Override
