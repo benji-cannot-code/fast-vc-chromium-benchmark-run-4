829e0cc38d9335ac1127deb6c1e127c1f0c13e43
haraken@chromium.org <haraken@chromium.org>
2015-07-09 00:40:15 +0000

    Oilpan: Build fix after r198541
    BUG=
    TBR=keishi@chromium.org
    
    Review URL: https://codereview.chromium.org/1228853002 .
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198545 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/navigatorconnect/AcceptConnectionObserver.cpp
+++ third_party/WebKit/Source/modules/navigatorconnect/AcceptConnectionObserver.cpp
@@ -201,6 +201,7 @@ AcceptConnectionObserver::AcceptConnectionObserver(ServicePortCollection* collec
 DEFINE_TRACE(AcceptConnectionObserver)
 {
     visitor->trace(m_collection);
+    visitor->trace(m_resolver);
     ContextLifecycleObserver::trace(visitor);
 }
 
