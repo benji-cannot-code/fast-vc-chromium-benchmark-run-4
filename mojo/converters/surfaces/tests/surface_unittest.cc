ddad7f0a4891f3baba4f98440768ad0324de25ee
penghuang <penghuang@chromium.org>
2015-07-09 12:38:36 -0700

    Support cc::DebugBorderDrawQuad for mojo surface.
    Currently cc::DebugBorderDrawQuad struct can not be serialized to
    or deserialized from  mojo::Quad correctly. So the mandoline will
    crash if the cc issue any DebugBorderDrawQuad. This CL fix this issue.
    
    BUG=None
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1229913004
    
    Cr-Commit-Position: refs/heads/master@{#338116}

--- mojo/converters/surfaces/tests/surface_unittest.cc
+++ mojo/converters/surfaces/tests/surface_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "cc/quads/checkerboard_draw_quad.h"
+#include "cc/quads/debug_border_draw_quad.h"
 #include "cc/quads/render_pass.h"
 #include "cc/quads/solid_color_draw_quad.h"
 #include "cc/quads/surface_draw_quad.h"
@@ -446,6 +447,33 @@ TEST(SurfaceLibTest, ReturnedResource) {
   EXPECT_EQ(lost, round_trip_resource.lost);
 }
 
+TEST_F(SurfaceLibQuadTest, DebugBorderQuad) {
+  cc::DebugBorderDrawQuad* debug_border_quad =
+      pass->CreateAndAppendDrawQuad<cc::DebugBorderDrawQuad>();
+  const SkColor arbitrary_color = SK_ColorGREEN;
+  const int width = 3;
+  debug_border_quad->SetAll(sqs,
+                            rect,
+                            opaque_rect,
+                            visible_rect,
+                            needs_blending,
+                            arbitrary_color,
+                            width);
+
+  QuadPtr mojo_quad = Quad::From<cc::DrawQuad>(*debug_border_quad);
+  ASSERT_FALSE(mojo_quad.is_null());
+  EXPECT_EQ(MATERIAL_DEBUG_BORDER, mojo_quad->material);
+  EXPECT_EQ(Rect::From(rect), mojo_quad->rect);
+  EXPECT_EQ(Rect::From(opaque_rect), mojo_quad->opaque_rect);
+  EXPECT_EQ(Rect::From(visible_rect), mojo_quad->visible_rect);
+  EXPECT_EQ(needs_blending, mojo_quad->needs_blending);
+  ASSERT_TRUE(mojo_quad->debug_border_quad_state);
+  DebugBorderQuadStatePtr& mojo_debug_border_state =
+      mojo_quad->debug_border_quad_state;
+  EXPECT_EQ(Color::From(arbitrary_color), mojo_debug_border_state->color);
+  EXPECT_EQ(width, mojo_debug_border_state->width);
+}
+
 TEST_F(SurfaceLibQuadTest, CheckerboardQuad) {
   cc::CheckerboardDrawQuad* checkerboard_quad =
       pass->CreateAndAppendDrawQuad<cc::CheckerboardDrawQuad>();
@@ -472,7 +500,6 @@ TEST_F(SurfaceLibQuadTest, CheckerboardQuad) {
   EXPECT_EQ(Color::From(arbitrary_color), mojo_checkerboard_state->color);
   EXPECT_EQ(scale, mojo_checkerboard_state->scale);
 }
-
 
 }  // namespace
 }  // namespace mojo
