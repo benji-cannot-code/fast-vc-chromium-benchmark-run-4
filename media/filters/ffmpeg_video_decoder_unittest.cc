aab6adbbaf9f5c9fe4aab2a33abedb050c805057
amineer <amineer@google.com>
2015-07-09 13:20:34 -0700

    Revert of Change the video color space default. (patchset #4 id:60001 of https://codereview.chromium.org/1221903003/)
    Reason for revert:
    Causing official continuous builds to fail with the following:
    
    ../../media/cast/sender/h264_vt_encoder_unittest.cc:308:22: error: no matching constructor for initialization of 'media::VideoDecoderConfig'
      VideoDecoderConfig config(kCodecH264, H264PROFILE_MAIN, frame_->format(),
                         ^      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ../../media/base/video_decoder_config.h:78:3: note: candidate constructor not viable: requires 10 arguments, but 9 were provided
      VideoDecoderConfig(VideoCodec codec,
      ^
    ../../media/base/video_decoder_config.h:70:20: note: candidate constructor (the implicit copy constructor) not viable: requires 1 argument, but 9 were provided
    class MEDIA_EXPORT VideoDecoderConfig {
                       ^
    ../../media/base/video_decoder_config.h:74:3: note: candidate constructor not viable: requires 0 arguments, but 9 were provided
      VideoDecoderConfig();
      ^
    1 error generated.
    
    Original issue's description:
    > Change the video color space default.
    >
    > Previously video without color space metadata was assumed to be in
    > Rec601. Now the default depends on the kind of playback. Normal src=
    > defaults to Rec601 for SD sized video (<720 pixels high), and Rec709 for
    > HD. MSE will always default to Rec709. Using a size based heuristic
    > doesn't make sense for MSE where it is common for the resolution to
    > change mid playback.
    >
    > This CL doesn't change the meaning of COLOR_SPACE_UNSPECIFIED. Instead,
    > it adds a color space field to VideoDecoderConfig, and updates the video
    > decoders to use this as the default if they don't find a more
    > authoritative value in the bitstream.
    >
    > This also fixes a (year old!) bug causing the blackwhite
    > tests to always succeed, renames the rec709 blackwhite test
    > file to match the name in blackwhite.html, and re-encodes it
    > to contain the color space metadata (previously it had none).
    >
    > BUG=333619
    >
    > Committed: https://crrev.com/4dc6c2ad0e595a5e0b543e8e1b8961ee0d742a32
    > Cr-Commit-Position: refs/heads/master@{#338110}
    
    TBR=dalecurtis@chromium.org,bbudge@chromium.org,codebythepound@gmail.com,gunsch@chromium.org,lcwu@chromium.org,watk@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=333619
    
    Review URL: https://codereview.chromium.org/1228843003
    
    Cr-Commit-Position: refs/heads/master@{#338124}

--- media/filters/ffmpeg_video_decoder_unittest.cc
+++ media/filters/ffmpeg_video_decoder_unittest.cc
@@ -225,15 +225,15 @@ TEST_F(FFmpegVideoDecoderTest, Initialize_UnsupportedPixelFormat) {
   // Ensure decoder handles unsupported pixel formats without crashing.
   VideoDecoderConfig config(kCodecVP8, VIDEO_CODEC_PROFILE_UNKNOWN,
                             VideoFrame::UNKNOWN,
-                            VideoFrame::COLOR_SPACE_UNSPECIFIED, kCodedSize,
-                            kVisibleRect, kNaturalSize, NULL, 0, false);
+                            kCodedSize, kVisibleRect, kNaturalSize,
+                            NULL, 0, false);
   InitializeWithConfigWithResult(config, false);
 }
 
 TEST_F(FFmpegVideoDecoderTest, Initialize_OpenDecoderFails) {
   // Specify Theora w/o extra data so that avcodec_open2() fails.
   VideoDecoderConfig config(kCodecTheora, VIDEO_CODEC_PROFILE_UNKNOWN,
-                            kVideoFormat, VideoFrame::COLOR_SPACE_UNSPECIFIED,
+                            kVideoFormat,
                             kCodedSize, kVisibleRect, kNaturalSize,
                             NULL, 0, false);
   InitializeWithConfigWithResult(config, false);
@@ -244,7 +244,6 @@ TEST_F(FFmpegVideoDecoderTest, Initialize_AspectRatioNumeratorZero) {
   VideoDecoderConfig config(kCodecVP8,
                             VP8PROFILE_ANY,
                             kVideoFormat,
-                            VideoFrame::COLOR_SPACE_UNSPECIFIED,
                             kCodedSize,
                             kVisibleRect,
                             natural_size,
@@ -259,7 +258,6 @@ TEST_F(FFmpegVideoDecoderTest, Initialize_AspectRatioDenominatorZero) {
   VideoDecoderConfig config(kCodecVP8,
                             VP8PROFILE_ANY,
                             kVideoFormat,
-                            VideoFrame::COLOR_SPACE_UNSPECIFIED,
                             kCodedSize,
                             kVisibleRect,
                             natural_size,
@@ -274,7 +272,6 @@ TEST_F(FFmpegVideoDecoderTest, Initialize_AspectRatioNumeratorNegative) {
   VideoDecoderConfig config(kCodecVP8,
                             VP8PROFILE_ANY,
                             kVideoFormat,
-                            VideoFrame::COLOR_SPACE_UNSPECIFIED,
                             kCodedSize,
                             kVisibleRect,
                             natural_size,
@@ -289,7 +286,6 @@ TEST_F(FFmpegVideoDecoderTest, Initialize_AspectRatioDenominatorNegative) {
   VideoDecoderConfig config(kCodecVP8,
                             VP8PROFILE_ANY,
                             kVideoFormat,
-                            VideoFrame::COLOR_SPACE_UNSPECIFIED,
                             kCodedSize,
                             kVisibleRect,
                             natural_size,
@@ -306,7 +302,6 @@ TEST_F(FFmpegVideoDecoderTest, Initialize_AspectRatioNumeratorTooLarge) {
   VideoDecoderConfig config(kCodecVP8,
                             VP8PROFILE_ANY,
                             kVideoFormat,
-                            VideoFrame::COLOR_SPACE_UNSPECIFIED,
                             kCodedSize,
                             kVisibleRect,
                             natural_size,
@@ -322,7 +317,6 @@ TEST_F(FFmpegVideoDecoderTest, Initialize_AspectRatioDenominatorTooLarge) {
   VideoDecoderConfig config(kCodecVP8,
                             VP8PROFILE_ANY,
                             kVideoFormat,
-                            VideoFrame::COLOR_SPACE_UNSPECIFIED,
                             kCodedSize,
                             kVisibleRect,
                             natural_size,
