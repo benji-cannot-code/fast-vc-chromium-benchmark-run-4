4ba628a1a98bbbad3e80524e6f463c9544189c92
xunlu <xunlu@chromium.org>
2015-07-09 11:55:43 -0700

    [Password Manager] Add UMA statistics for PasswordForm::ssl_valid usage
    We are planning to drop the PasswordForm::ssl_valid field as it's no longer used in new password forms and we believe it was set very rarely. This change will give us the statistics to confirm our belief.
    
    BUG=413020
    
    Review URL: https://codereview.chromium.org/1217273007
    
    Cr-Commit-Position: refs/heads/master@{#338103}

--- components/password_manager/core/browser/login_database.cc
+++ components/password_manager/core/browser/login_database.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sql/connection.h"
 #include "sql/statement.h"
 #include "sql/transaction.h"
+#include "url/url_constants.h"
 
 using autofill::PasswordForm;
 
@@ -527,6 +528,22 @@ void LoginDatabase::ReportMetrics(const std::string& sync_username,
     UMA_HISTOGRAM_COUNTS_100(
         "PasswordManager.EmptyUsernames.WithoutCorrespondingNonempty",
         num_entries);
+  }
+
+  sql::Statement invalid_ssl_cert_statement(db_.GetCachedStatement(
+      SQL_FROM_HERE, "SELECT origin_url, ssl_valid FROM logins;"));
+
+  if (!invalid_ssl_cert_statement.is_valid())
+    return;
+
+  while (invalid_ssl_cert_statement.Step()) {
+    GURL url = GURL(invalid_ssl_cert_statement.ColumnString(0));
+
+    if (url.SchemeIs(url::kHttpsScheme)) {
+      metrics_util::LogUMAHistogramBoolean(
+          "PasswordManager.UserStoredPasswordWithInvalidSSLCert",
+          invalid_ssl_cert_statement.ColumnInt(1) == 0);
+    }
   }
 }
 
