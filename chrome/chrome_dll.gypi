3b5ca72e34604a5a52d95e3e9d775ebb850acf01
caitkp <caitkp@chromium.org>
2015-07-09 13:07:08 -0700

    Pull Chrome_Elf out of the renderer process
    (since we neither redirect nor blacklist anything in there
    anyways, and we're currently causing some crashes).
    
    BUG=477137
    
    Review URL: https://codereview.chromium.org/1225613003
    
    Cr-Commit-Position: refs/heads/master@{#338123}

--- chrome/chrome_dll.gypi
+++ chrome/chrome_dll.gypi
@@ -353,9 +353,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           ],
           'conditions': [
             ['OS=="win"', {
-              'dependencies': [
-                '<(DEPTH)/chrome_elf/chrome_elf.gyp:chrome_elf',
-              ],
               'conditions': [
                 ['chrome_pgo_phase==1', {
                   'msvs_settings': {
--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -155,10 +155,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/media/webrtc_logging_message_filter.h"
 #endif
 
-#if defined(OS_WIN)
-#include "chrome_elf/blacklist/blacklist.h"
-#endif
-
 using autofill::AutofillAgent;
 using autofill::PasswordAutofillAgent;
 using autofill::PasswordGenerationAgent;
@@ -502,13 +498,6 @@ void ChromeContentRendererClient::RenderThreadStarted() {
   WebSecurityPolicy::registerURLSchemeAsBypassingContentSecurityPolicy(
       extension_resource_scheme);
 
-#if defined(OS_WIN)
-  // Report if the renderer process has been patched by chrome_elf.
-  // TODO(csharp): Remove once the renderer is no longer getting
-  // patched this way.
-  if (blacklist::IsBlacklistInitialized())
-    UMA_HISTOGRAM_BOOLEAN("Blacklist.PatchedInRenderer", true);
-#endif
 #if defined(ENABLE_PRINT_PREVIEW)
   pdf_print_client_.reset(new ChromePDFPrintClient());
   pdf::PepperPDFHost::SetPrintClient(pdf_print_client_.get());
