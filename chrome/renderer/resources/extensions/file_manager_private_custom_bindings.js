f28d43c94f22f3c80225e2331bcfb554bccc94d4
mtomasz <mtomasz@chromium.org>
2015-07-09 01:11:35 -0700

    Make chrome.fileManagerPrivate.getEntryProperties accept entries, not urls.
    The plan is to migrate completely to isolated entries in Files app, which will
    allow to get rid of the costly resolving of root in volume manager to an
    external entry.
    
    NOPRESUBMIT=true
    TEST=All current Files app tests pass.
    BUG=507210
    
    Review URL: https://codereview.chromium.org/1225753002
    
    Cr-Commit-Position: refs/heads/master@{#337997}

--- chrome/renderer/resources/extensions/file_manager_private_custom_bindings.js
+++ chrome/renderer/resources/extensions/file_manager_private_custom_bindings.js
@@ -69,6 +69,14 @@ binding.registerCustomHook(function(bindingsAPI) {
       }));
     });
   });
+
+  apiFunctions.setHandleRequest('getEntryProperties',
+                                function(entries, names, callback) {
+    var urls = entries.map(function(entry) {
+      return fileBrowserHandlerNatives.GetEntryURL(entry);
+    });
+    fileManagerPrivateInternal.getEntryProperties(urls, names, callback);
+  });
 });
 
 eventBindings.registerArgumentMassager(
