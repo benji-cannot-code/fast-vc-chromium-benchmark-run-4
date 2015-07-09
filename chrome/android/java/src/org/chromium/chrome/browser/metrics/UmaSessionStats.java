3f38a119af3986cea4cacc5234f126256e28fb3e
mariakhomenko <mariakhomenko@chromium.org>
2015-07-09 10:41:27 -0700

    Revert of Revert of Remove use of tab selector. (patchset #1 id:1 of https://codereview.chromium.org/1225133002/)
    Reason for revert:
    Revert did not fix the test problem, resubmitting.
    
    Original issue's description:
    > Revert of Remove use of tab selector. (patchset #1 id:20001 of https://codereview.chromium.org/1229543002/)
    >
    > Reason for revert:
    > Downstream test started breaking at this change with [FATAL:uma_session_stats.cc(57)] Check failed: active_session_count_ >= 0. Doing a speculative revert to see if it fixes the test.
    >
    > Original issue's description:
    > > Remove use of tab selector.
    > >
    > > Seems like when activity onpause is called the mTabModelSelector is reset to null.
    > > It is possible that after that onPageLoadFinished event
    > > will fire which results in  use of mTabModelSelector in
    > > recordPageLoadStats and therefore will throw a
    > > NullPointerException.
    > >
    > > The guess is that removing the use of tab selector will fix the crashes.
    > >
    > > BUG=507452
    > >
    > > Committed: https://crrev.com/b8a6ad03629f475781d54baafe748aae6991852e
    > > Cr-Commit-Position: refs/heads/master@{#337666}
    >
    > TBR=nyquist@chromium.org,asvitkine@chromium.org,gayane@chromium.org
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=507452
    >
    > Committed: https://crrev.com/877dc3114a1a55c82859c9f56e8013a703fb7d8e
    > Cr-Commit-Position: refs/heads/master@{#337929}
    
    TBR=nyquist@chromium.org,asvitkine@chromium.org,gayane@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=507452
    
    Review URL: https://codereview.chromium.org/1229723006
    
    Cr-Commit-Position: refs/heads/master@{#338078}

--- chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaSessionStats.java
+++ chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaSessionStats.java
@@ -53,9 +53,7 @@ public class UmaSessionStats implements NetworkChangeNotifier.ConnectionTypeObse
         mReportingPermissionManager = PrivacyPreferencesManager.getInstance(context);
     }
 
-    private void recordPageLoadStats(int tabId) {
-        Tab tab = mTabModelSelector.getTabById(tabId);
-        if (tab == null) return;
+    private void recordPageLoadStats(Tab tab) {
         WebContents webContents = tab.getWebContents();
         boolean isDesktopUserAgent = webContents != null
                 && webContents.getNavigationController().getUseDesktopUserAgent();
@@ -99,7 +97,7 @@ public class UmaSessionStats implements NetworkChangeNotifier.ConnectionTypeObse
             mTabModelSelectorTabObserver = new TabModelSelectorTabObserver(mTabModelSelector) {
                 @Override
                 public void onPageLoadFinished(Tab tab) {
-                    recordPageLoadStats(tab.getId());
+                    recordPageLoadStats(tab);
                 }
             };
         }
