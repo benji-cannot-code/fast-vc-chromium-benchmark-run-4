1a03a195ae01865bfc40fa9ecbc3d6a3e5af5264
sherouk <sherouk@google.com>
2015-07-09 06:07:12 -0700

    Cleanup SnapshotOverlay.
    BUG=none
    
    Review URL: https://codereview.chromium.org/1225733003
    
    Cr-Commit-Position: refs/heads/master@{#338024}

--- ios/chrome/browser/snapshots/snapshot_overlay.mm
+++ ios/chrome/browser/snapshots/snapshot_overlay.mm
@@ -6,25 +6,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/snapshots/snapshot_overlay.h"
 
 #include "base/logging.h"
+#import "base/mac/scoped_nsobject.h"
 
-@implementation SnapshotOverlay
+@implementation SnapshotOverlay {
+  base::scoped_nsobject<UIView> _view;
+}
 
-- (id)initWithView:(UIView*)view yOffset:(CGFloat)yOffset {
+@synthesize yOffset = _yOffset;
+
+- (instancetype)initWithView:(UIView*)view yOffset:(CGFloat)yOffset {
   self = [super init];
   if (self) {
     DCHECK(view);
-    view_.reset([view retain]);
-    yOffset_ = yOffset;
+    _view.reset([view retain]);
+    _yOffset = yOffset;
   }
   return self;
 }
 
-- (UIView*)view {
-  return view_;
+- (instancetype)init {
+  NOTREACHED();
+  return nil;
 }
 
-- (CGFloat)yOffset {
-  return yOffset_;
+- (UIView*)view {
+  return _view;
 }
 
 @end
