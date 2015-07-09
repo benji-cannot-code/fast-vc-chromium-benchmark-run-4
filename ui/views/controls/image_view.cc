1534c01ce1e2f02c512b2f370ced606c385c69b1
estade <estade@chromium.org>
2015-07-08 19:55:58 -0700

    Split vector icon commands into separate files aggregated at build time.
    depends on https://codereview.chromium.org/1214693005/
    
    BUG=505953
    
    Review URL: https://codereview.chromium.org/1219293004
    
    Cr-Commit-Position: refs/heads/master@{#337973}

--- ui/views/controls/image_view.cc
+++ ui/views/controls/image_view.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_view_state.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/insets.h"
+#include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/painter.h"
 
 namespace views {
--- ui/views/controls/image_view.h
+++ ui/views/controls/image_view.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/image/image_skia.h"
-#include "ui/gfx/vector_icons.h"
+#include "ui/gfx/vector_icons_public.h"
 #include "ui/views/view.h"
 
 namespace gfx {
