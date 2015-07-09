32b3f751cd8d84cc4e1eed763e66b07bc0d2ea01
mlamouri <mlamouri@chromium.org>
2015-07-09 13:37:37 -0700

    Media Notifications: mark notification as private for incognito tabs.
    Starting from Android L, users can say whether they want private
    notifications to be visible in their lockscreen or replaced by some
    placeholder content.
    
    When replaced by placeholder content, the user will no longer be able to
    play/pause from the notification it their lockscreen but they probably
    don't want anyone to play a media that was paused and if a media is
    playing, nothing is really private anymore anyway.
    
    BUG=470600
    
    Review URL: https://codereview.chromium.org/1224273005
    
    Cr-Commit-Position: refs/heads/master@{#338129}

--- chrome/android/java/src/org/chromium/chrome/browser/media/ui/MediaSessionTabHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/ui/MediaSessionTabHelper.java
@@ -70,11 +70,12 @@ public class MediaSessionTabHelper {
                 }
                 NotificationMediaPlaybackControls.show(
                         ApplicationStatus.getApplicationContext(),
-                        new MediaInfo(
+                        new MediaNotificationInfo(
                                 mTab.getTitle(),
                                 isPaused,
                                 origin,
                                 mTab.getId(),
+                                mTab.isIncognito(),
                                 mControlsListener));
             }
         };
