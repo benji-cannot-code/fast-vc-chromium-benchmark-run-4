9f3da7a36a5b1e825cc0b6a497063ed8f6070054
hubbe <hubbe@chromium.org>
2015-07-09 12:01:02 -0700

    minor cleanup for PartialData
    Review URL: https://codereview.chromium.org/1213603005
    
    Cr-Commit-Position: refs/heads/master@{#338107}

--- net/http/partial_data.h
+++ net/http/partial_data.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NET_HTTP_PARTIAL_DATA_H_
 
 #include "base/basictypes.h"
+#include "base/memory/weak_ptr.h"
 #include "net/base/completion_callback.h"
 #include "net/http/http_byte_range.h"
 #include "net/http/http_request_headers.h"
@@ -120,12 +121,11 @@ class PartialData {
   bool initial_validation() const { return initial_validation_; }
 
  private:
-  class Core;
   // Returns the length to use when scanning the cache.
   int GetNextRangeLen();
 
   // Completion routine for our callback.
-  void GetAvailableRangeCompleted(int result, int64 start);
+  void GetAvailableRangeCompleted(int64* start, int result);
 
   int64 current_range_start_;
   int64 current_range_end_;
@@ -140,8 +140,8 @@ class PartialData {
   bool sparse_entry_;
   bool truncated_;  // We have an incomplete 200 stored.
   bool initial_validation_;  // Only used for truncated entries.
-  Core* core_;
   CompletionCallback callback_;
+  base::WeakPtrFactory<PartialData> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(PartialData);
 };
