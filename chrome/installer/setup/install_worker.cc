9df821feaae4b3aabc805144e4a72d68d834a8d7
gab <gab@chromium.org>
2015-07-09 07:08:57 -0700

    Cleanup unused param in AddActiveSetupWorkItems
    BUG=None (not required for Win10 CLs/merges)
    
    Review URL: https://codereview.chromium.org/1221333011
    
    Cr-Commit-Position: refs/heads/master@{#338034}

--- chrome/installer/setup/install_worker.cc
+++ chrome/installer/setup/install_worker.cc
@@ -1242,7 +1242,7 @@ void AddInstallWorkItems(const InstallationState& original_state,
     AddDelegateExecuteWorkItems(installer_state, target_path, new_version,
                                 product, install_list);
 
-    AddActiveSetupWorkItems(installer_state, setup_path, new_version, product,
+    AddActiveSetupWorkItems(installer_state, new_version, product,
                             install_list);
   }
 
@@ -1400,7 +1400,6 @@ void AddDelegateExecuteWorkItems(const InstallerState& installer_state,
 }
 
 void AddActiveSetupWorkItems(const InstallerState& installer_state,
-                             const base::FilePath& setup_path,
                              const Version& new_version,
                              const Product& product,
                              WorkItemList* list) {
