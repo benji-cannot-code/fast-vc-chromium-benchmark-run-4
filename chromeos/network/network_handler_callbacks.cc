fb7e9062f771c4434e60a07f6a6a099e179ef3f5
stevenjb <stevenjb@chromium.org>
2015-07-09 12:22:49 -0700

    Allow suppression of expected wake-on-wifi error message
    This includes a bit of code cleanup too:
    * Transition to device_event_log.h
    * Clang formatting
    
    BUG=467232
    
    Review URL: https://codereview.chromium.org/1227193002
    
    Cr-Commit-Position: refs/heads/master@{#338111}

--- chromeos/network/network_handler_callbacks.cc
+++ chromeos/network/network_handler_callbacks.cc
@@ -7,7 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/values.h"
-#include "chromeos/network/network_event_log.h"
+#include "components/device_event_log/device_event_log.h"
+
+namespace {
+
+bool SuppressError(const std::string& dbus_error_message) {
+  if (dbus_error_message == "Wake on WiFi not supported")
+    return true;
+  return false;
+}
+
+}  // namespace
 
 namespace chromeos {
 namespace network_handler {
@@ -61,19 +71,21 @@ void ShillErrorCallbackFunction(const std::string& error_name,
                                 const ErrorCallback& error_callback,
                                 const std::string& dbus_error_name,
                                 const std::string& dbus_error_message) {
-  std::string detail;
+  std::string detail = error_name + ": ";
   if (!path.empty())
     detail += path + ": ";
   detail += dbus_error_name;
   if (!dbus_error_message.empty())
     detail += ": " + dbus_error_message;
-  NET_LOG_ERROR(error_name, detail);
+  device_event_log::LogLevel log_level =
+      SuppressError(dbus_error_message) ? device_event_log::LOG_LEVEL_DEBUG
+                                        : device_event_log::LOG_LEVEL_ERROR;
+  DEVICE_LOG(::device_event_log::LOG_TYPE_NETWORK, log_level) << detail;
 
   if (error_callback.is_null())
     return;
-  scoped_ptr<base::DictionaryValue> error_data(
-      CreateDBusErrorData(path, error_name, detail,
-                          dbus_error_name, dbus_error_message));
+  scoped_ptr<base::DictionaryValue> error_data(CreateDBusErrorData(
+      path, error_name, detail, dbus_error_name, dbus_error_message));
   error_callback.Run(error_name, error_data.Pass());
 }
 
@@ -83,9 +95,8 @@ void GetPropertiesCallback(const DictionaryResultCallback& callback,
                            DBusMethodCallStatus call_status,
                            const base::DictionaryValue& value) {
   if (call_status != DBUS_METHOD_CALL_SUCCESS) {
-    NET_LOG_ERROR(
-        base::StringPrintf("GetProperties failed. Status: %d", call_status),
-        path);
+    NET_LOG(ERROR) << "GetProperties failed: " << path
+                   << " Status: " << call_status;
     RunErrorCallback(
         error_callback, path, kDBusFailedError, kDBusFailedErrorMessage);
   } else if (!callback.is_null()) {
