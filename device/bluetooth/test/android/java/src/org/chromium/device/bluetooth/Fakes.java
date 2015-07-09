684f29d692c8569aaef3e39103127b2fe619a70c
scheib <scheib@chromium.org>
2015-07-08 17:46:16 -0700

    Revert of bluetooth: android: Initial Low Energy Discovery Sessions. (patchset #9 id:320001 of https://codereview.chromium.org/1150833002/)
    Reason for revert:
    Downstream Chrome build encountered runtime errors:
    
    java.lang.NoClassDefFoundError: org/chromium/device/bluetooth/ChromeBluetoothAdapter 6a6b6: 07-08 23:04:48.277 3034 3054 W System.err: at org.chromium.base.library_loader.Linker.nativeLoadLibrary(Native Method) 6a6b6: 07-08 23:04:48.277 3034 3054 W System.err: at org.chromium.base.library_loader.Linker.loadLibrary(Linker.java:747) 6a6b6: 07-08 23:04:48.277 3034 3054 W System.err: at org.chromium.base.library_loader.LibraryLoader.loadLibrary(LibraryLoader.java:304) 6a6b6: 07-08 23:04:48.277 3034 3054 W System.err: at org.chromium.base.library_loader.LibraryLoader.loadAlreadyLocked(LibraryLoader.java:241) 6a6b6: 07-08 23:04:48.277 3034 3054 W System.err: at org.chromium.base.library_loader.LibraryLoader.ensureInitialized(LibraryLoader.java:123) 6a6b6: 07-08 23:04:48.277 3034 3054 W System.err: at org.chromium.chrome.browser.init.NativeInitializationController$1.run(NativeInitializationController.java:83) 6a6b6: 07-08 23:04:48.287 3034 3054 W System.err: Caused by: java.lang.ClassNotFoundException: Didn't find class "org.chromium.device.bluetooth.ChromeBluetoothAdapter" on path: DexPathList[[zip file "/system/framework/android.test.runner.jar", zip file "/data/app/com.google.android.apps.chrome.tests-1.apk", zip file "/data/app/com.google.android.apps.chrome-1.apk"],nativeLibraryDirectories=[/data/app-lib/com.google.android.apps.chrome.tests-1, /data/app-lib/com.google.android.apps.chrome-1, /vendor/lib, /system/lib]] 6a6b6: 07-08 23:04:48.287 3034 3054 W System.err: at dalvik.system.BaseDexClassLoader.findClass(BaseDexClassLoader.java:56) 6a6b6: 07-08 23:04:48.287 3034 3054 W System.err: at java.lang.ClassLoader.loadClass(ClassLoader.java:497) 6a6b6: 07-08 23:04:48.287 3034 3054 W System.err: at java.lang.ClassLoader.loadClass(ClassLoader.java:457)
    
    Original issue's description:
    > bluetooth: android: Initial Low Energy Discovery Sessions.
    >
    > Add ability to start a low energy scan, though do nothing with the
    > results for now besides log that they are found.
    >
    > Rename hasBluetoothPermission -> hasBluetoothCapability because the
    > permissions, SDK version, and hardware feature must all be present.
    >
    > BUG=488575
    >
    > Committed: https://crrev.com/649ce43ef16cd8c4a3315f2ebfa5823dcc06585e
    > Cr-Commit-Position: refs/heads/master@{#337924}
    
    TBR=ortuno@chromium.org,tedchoc@chromium.org,armansito@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=488575
    
    Review URL: https://codereview.chromium.org/1226103005
    
    Cr-Commit-Position: refs/heads/master@{#337955}

--- device/bluetooth/test/android/java/src/org/chromium/device/bluetooth/Fakes.java
+++ device/bluetooth/test/android/java/src/org/chromium/device/bluetooth/Fakes.java
@@ -5,19 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.device.bluetooth;
 
-import android.annotation.TargetApi;
-import android.bluetooth.le.ScanFilter;
-import android.os.Build;
-
 import org.chromium.base.CalledByNative;
 import org.chromium.base.Log;
-
-import java.util.List;
 
 /**
  * Fake implementations of android.bluetooth.* classes for testing.
  */
-@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 class Fakes {
     private static final String TAG = "cr.Bluetooth";
 
@@ -25,8 +18,6 @@ class Fakes {
      * Fakes android.bluetooth.BluetoothAdapter.
      */
     static class FakeBluetoothAdapter extends Wrappers.BluetoothAdapterWrapper {
-        private final FakeBluetoothLeScanner mFakeScanner;
-
         /**
          * Creates a FakeBluetoothAdapter.
          */
@@ -37,8 +28,7 @@ class Fakes {
         }
 
         private FakeBluetoothAdapter() {
-            super(null, new FakeBluetoothLeScanner());
-            mFakeScanner = (FakeBluetoothLeScanner) mScanner;
+            super(null);
         }
 
         // -----------------------------------------------------------------------------------------
@@ -67,74 +57,6 @@ class Fakes {
         @Override
         public boolean isDiscovering() {
             return false;
-        }
-    }
-
-    /**
-     * Fakes android.bluetooth.le.BluetoothLeScanner.
-     */
-    static class FakeBluetoothLeScanner extends Wrappers.BluetoothLeScannerWrapper {
-        public Wrappers.ScanCallbackWrapper mCallback;
-
-        private FakeBluetoothLeScanner() {
-            super(null);
-        }
-
-        @Override
-        public void startScan(List<ScanFilter> filters, int scanSettingsScanMode,
-                Wrappers.ScanCallbackWrapper callback) {
-            if (mCallback != null) {
-                throw new IllegalArgumentException(
-                        "FakeBluetoothLeScanner does not support multiple scans.");
-            }
-            mCallback = callback;
-        }
-
-        @Override
-        public void stopScan(Wrappers.ScanCallbackWrapper callback) {
-            if (mCallback != callback) {
-                throw new IllegalArgumentException("No scan in progress.");
-            }
-            mCallback = null;
-        }
-    }
-
-    /**
-     * Fakes android.bluetooth.le.ScanResult
-     */
-    static class FakeScanResult extends Wrappers.ScanResultWrapper {
-        private final FakeBluetoothDevice mDevice;
-
-        FakeScanResult(FakeBluetoothDevice device) {
-            super(null);
-            mDevice = device;
-        }
-
-        @Override
-        public Wrappers.BluetoothDeviceWrapper getDevice() {
-            return mDevice;
-        }
-    }
-
-    /**
-     * Fakes android.bluetooth.BluetoothDevice.
-     */
-    static class FakeBluetoothDevice extends Wrappers.BluetoothDeviceWrapper {
-        private static final String ADDRESS = "A1:B2:C3:DD:DD:DD";
-        private static final String NAME = "FakeBluetoothDevice";
-
-        public FakeBluetoothDevice() {
-            super(null);
-        }
-
-        @Override
-        public String getAddress() {
-            return ADDRESS;
-        }
-
-        @Override
-        public String getName() {
-            return NAME;
         }
     }
 }
