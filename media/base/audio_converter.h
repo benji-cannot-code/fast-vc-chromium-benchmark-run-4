4ffc99c62d94edeabee09312919c29def62518e8
henrika <henrika@chromium.org>
2015-07-09 04:09:38 -0700

    Fixes issue where Web Speech API drops a frame every 5.1 seconds
    TBR=tommi
    
    BUG=506051
    TEST=content_unittests --gtest_filter=Speech* and manual testing using https://www.google.com/intl/en/chrome/demos/speech.html
    
    Verified 8000, 11025, 16000, 22050, 32000, 44100, 48000, 96000 and 192000 as native input sample rate.
    
    Review URL: https://codereview.chromium.org/1211203006
    
    Cr-Commit-Position: refs/heads/master@{#338013}

--- media/base/audio_converter.h
+++ media/base/audio_converter.h
@@ -91,6 +91,9 @@ class MEDIA_EXPORT AudioConverter {
   // to each input's ProvideInput for more data.
   int ChunkSize() const;
 
+  // See SincResampler::PrimeWithSilence.
+  void PrimeWithSilence();
+
   bool empty() const { return transform_inputs_.empty(); }
 
  private:
