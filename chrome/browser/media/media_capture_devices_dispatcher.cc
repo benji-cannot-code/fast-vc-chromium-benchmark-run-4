7b617ac2bf10b7a96d90e4a1516846ee1cdc9f25
ckulakowski <ckulakowski@opera.com>
2015-07-09 00:08:29 -0700

    Added missing include in media_capture_devices_dispatcher.cc
    Introduced by https://codereview.chromium.org/1095393004. Profile is still used in this file and missing include causes compilation error in our product.
    
    Review URL: https://codereview.chromium.org/1221283005
    
    Cr-Commit-Position: refs/heads/master@{#337992}

--- chrome/browser/media/media_capture_devices_dispatcher.cc
+++ chrome/browser/media/media_capture_devices_dispatcher.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/media_access_handler.h"
 #include "chrome/browser/media/media_stream_capture_indicator.h"
 #include "chrome/browser/media/permission_bubble_media_access_handler.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/common/chrome_switches.h"
