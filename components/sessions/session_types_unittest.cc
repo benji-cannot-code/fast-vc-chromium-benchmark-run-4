909d2fa425d302079bcd9f2ca99529f0852ad5aa
sdefresne <sdefresne@chromium.org>
2015-07-10 09:50:12 -0700

    Remove commented out #include in components/sessions/session_types_unittest.cc
    BUG=None
    
    Review URL: https://codereview.chromium.org/1225213007
    
    Cr-Commit-Position: refs/heads/master@{#338305}

--- components/sessions/session_types_unittest.cc
+++ components/sessions/session_types_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
-//#include "chrome/browser/search/search.h"
 #include "components/sessions/serialized_navigation_entry_test_helper.h"
 #include "sync/protocol/session_specifics.pb.h"
 #include "sync/util/time.h"
