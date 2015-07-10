80f9127ae8048f5d4aaf38a69a7b5234cf4e5fdd
jrummell <jrummell@chromium.org>
2015-07-10 13:40:15 -0700

    Clear Key CDM should resolve the promise if session not found
    Rather than rejecting the promise with an error, Clear Key should
    resolve the promise with an empty string (which will get passed to
    Blink as false). Also updates the browser test to verify that false
    is actually returned.
    
    For prefixed EME an error still needs to be returned.
    
    BUG=507736
    TEST=updated test passes
    
    Review URL: https://codereview.chromium.org/1224053003
    
    Cr-Commit-Position: refs/heads/master@{#338347}

--- chrome/browser/media/encrypted_media_browsertest.cc
+++ chrome/browser/media/encrypted_media_browsertest.cc
@@ -68,6 +68,7 @@ const char kUnknownSession[] = "UnknownSession";
 const char kFileIOTestSuccess[] = "FILE_IO_TEST_SUCCESS";
 const char kEmeNotSupportedError[] = "NOTSUPPORTEDERROR";
 const char kEmeGenerateRequestFailed[] = "EME_GENERATEREQUEST_FAILED";
+const char kEmeSessionNotFound[] = "EME_SESSION_NOT_FOUND";
 const char kEmeLoadFailed[] = "EME_LOAD_FAILED";
 const char kEmeUpdateFailed[] = "EME_UPDATE_FAILED";
 const char kEmeErrorEvent[] = "EME_ERROR_EVENT";
@@ -244,6 +245,7 @@ class EncryptedMediaTestBase : public MediaBrowserTest {
     title_watcher->AlsoWaitForTitle(base::ASCIIToUTF16(kEmeNotSupportedError));
     title_watcher->AlsoWaitForTitle(
         base::ASCIIToUTF16(kEmeGenerateRequestFailed));
+    title_watcher->AlsoWaitForTitle(base::ASCIIToUTF16(kEmeSessionNotFound));
     title_watcher->AlsoWaitForTitle(base::ASCIIToUTF16(kEmeLoadFailed));
     title_watcher->AlsoWaitForTitle(base::ASCIIToUTF16(kEmeUpdateFailed));
     title_watcher->AlsoWaitForTitle(base::ASCIIToUTF16(kEmeErrorEvent));
@@ -707,9 +709,7 @@ IN_PROC_BROWSER_TEST_F(ECKEncryptedMediaTest, LoadLoadableSession) {
 }
 
 IN_PROC_BROWSER_TEST_F(ECKEncryptedMediaTest, LoadUnknownSession) {
-  // TODO(jrummell): Load should not fail -- it should return false instead.
-  // http://crbug.com/507736
-  TestPlaybackCase(kUnknownSession, kEmeLoadFailed);
+  TestPlaybackCase(kUnknownSession, kEmeSessionNotFound);
 }
 
 IN_PROC_BROWSER_TEST_F(ECKPrefixedEncryptedMediaTest, InitializeCDMFail) {
