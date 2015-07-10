9bebd5731aade46fc7a23779993af32fe5099a1e
mlamouri <mlamouri@chromium.org>
2015-07-10 04:00:34 -0700

    Presentation API: remove stall PresentationDispatcher::startSession().
    This is part of a suite of three CLs:
    1. https://codereview.chromium.org/1208083004
    2. https://codereview.chromium.org/1225523002
    3. <this>
    
    BUG=502047
    
    Review URL: https://codereview.chromium.org/1221093004
    
    Cr-Commit-Position: refs/heads/master@{#338271}

--- content/renderer/presentation/presentation_dispatcher.cc
+++ content/renderer/presentation/presentation_dispatcher.cc
@@ -114,13 +114,6 @@ void PresentationDispatcher::updateAvailableChangeWatched(bool watched) {
 
 void PresentationDispatcher::startSession(
     const blink::WebString& presentationUrl,
-    const blink::WebString& presentationId,
-    blink::WebPresentationSessionClientCallbacks* callback) {
-  startSession(presentationUrl, callback);
-}
-
-void PresentationDispatcher::startSession(
-    const blink::WebString& presentationUrl,
     blink::WebPresentationSessionClientCallbacks* callback) {
   DCHECK(callback);
   ConnectToPresentationServiceIfNeeded();
@@ -353,6 +346,9 @@ void PresentationDispatcher::OnScreenAvailabilityUpdated(bool available) {
 
   if (listening_state_ == ListeningState::Waiting)
     listening_state_ = ListeningState::Active;
+
+  for (auto observer : availability_observers_)
+    observer->availabilityChanged(available);
 
   for (auto observer : availability_observers_)
     observer->availabilityChanged(available);
