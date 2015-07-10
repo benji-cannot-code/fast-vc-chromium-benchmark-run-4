914fc58fc197a96295252618d8f800825c548e08
dfalcantara <dfalcantara@chromium.org>
2015-07-10 11:14:16 -0700

    Track whether a created WebContents has a resume pending.
    On Android, it's possible that a WebContents is created and
    cannot be loaded immediately, which occurs when Chrome needs
    to start an Activity asynchronously.  There's currently no
    good way to know when the WebContents is in this state, though.
    
    * Start storing whether the WebContents has needs to resume loading
      in the WebContentsImpl, which can then be checked when the
      WebContentsImpl may begin accepting requests from the renderer.
    
    * Clean up Android code that heuristically tracks whether the
      WebContents could be in this state.
    
    * Existing Java and Chromium tests catch regressions.
    
    BUG=508186
    
    Review URL: https://codereview.chromium.org/1214723012
    
    Cr-Commit-Position: refs/heads/master@{#338323}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -361,6 +361,7 @@ WebContentsImpl::WebContentsImpl(BrowserContext* browser_context)
       load_state_(net::LOAD_STATE_IDLE, base::string16()),
       upload_size_(0),
       upload_position_(0),
+      is_resume_pending_(false),
       displayed_insecure_content_(false),
       has_accessed_initial_document_(false),
       theme_color_(SK_ColorTRANSPARENT),
@@ -1818,6 +1819,7 @@ void WebContentsImpl::ShowCreatedWindow(int route_id,
   WebContentsImpl* contents = GetCreatedWindow(route_id);
   if (contents) {
     WebContentsDelegate* delegate = GetDelegate();
+    contents->is_resume_pending_ = true;
     if (!delegate || delegate->ShouldResumeRequestsForCreatedWindow())
       contents->ResumeLoadingCreatedWebContents();
 
@@ -2668,8 +2670,11 @@ void WebContentsImpl::ExitFullscreen() {
 void WebContentsImpl::ResumeLoadingCreatedWebContents() {
   // Resume blocked requests for both the RenderViewHost and RenderFrameHost.
   // TODO(brettw): It seems bogus to reach into here and initialize the host.
-  GetRenderViewHost()->Init();
-  GetMainFrame()->Init();
+  if (is_resume_pending_) {
+    is_resume_pending_ = false;
+    GetRenderViewHost()->Init();
+    GetMainFrame()->Init();
+  }
 }
 
 bool WebContentsImpl::FocusLocationBarByDefault() {
