467d42ec2da0b819b69c0790233f8c4226d8cd59
yosin@chromium.org <yosin@chromium.org>
2015-07-10 08:35:39 +0000

    Make findPlaintText() to take EphemeralRange instead of two Position parameters
    This patch changes a function signature of |findPlainText()| to take
    |EphemeralRange| instead of two |Position| parameters for preparation of
    making |findPlainText()| to work on both DOM tree and composed tree, with
    introducing |EphemeralRange| version of |CharacterItertor| for shorter source
    code.
    
    This patch is a preparation of making Find-In-Page to work on composed tree,
    http://crrev.com/1215903005
    
    Note: http://crrev.com/1226303002, change return value of |findPlainText()| to
    |EphemeralRange|.
    
    BUGS=n/a
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1229213005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198674 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/Editor.cpp
+++ third_party/WebKit/Source/core/editing/Editor.cpp
@@ -1184,7 +1184,7 @@ static PassRefPtrWillBeRawPtr<Range> findStringBetweenPositions(const String& ta
     bool forward = !(options & Backwards);
 
     while (true) {
-        EphemeralRange resultRange = findPlainText(searchStart, searchEnd, target, options);
+        EphemeralRange resultRange = findPlainText(EphemeralRange(searchStart, searchEnd), target, options);
         if (resultRange.isCollapsed())
             return nullptr;
 
--- third_party/WebKit/Source/core/editing/iterators/CharacterIterator.h
+++ third_party/WebKit/Source/core/editing/iterators/CharacterIterator.h
@@ -43,6 +43,7 @@ class CORE_EXPORT CharacterIteratorAlgorithm {
     STACK_ALLOCATED();
 public:
     CharacterIteratorAlgorithm(const PositionAlgorithm<Strategy>& start, const PositionAlgorithm<Strategy>& end, TextIteratorBehaviorFlags = TextIteratorDefaultBehavior);
+    explicit CharacterIteratorAlgorithm(const EphemeralRangeTemplate<Strategy>&, TextIteratorBehaviorFlags = TextIteratorDefaultBehavior);
 
     void advance(int numCharacters);
 
@@ -80,7 +81,7 @@ private:
 extern template class CORE_EXTERN_TEMPLATE_EXPORT CharacterIteratorAlgorithm<EditingStrategy>;
 using CharacterIterator = CharacterIteratorAlgorithm<EditingStrategy>;
 
-CORE_EXPORT EphemeralRange findPlainText(const Position& inputStart, const Position& inputEnd, const String&, FindOptions);
+CORE_EXPORT EphemeralRange findPlainText(const EphemeralRange& inputRange, const String&, FindOptions);
 
 } // namespace blink
 
