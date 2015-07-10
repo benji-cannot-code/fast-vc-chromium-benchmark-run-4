95b95af2e8d489cbf25b877e40c64df46778ddcd
peria@chromium.org <peria@chromium.org>
2015-07-10 08:10:59 +0000

    Migrate XMLHttpRequest on Oilpan heap.
    BUG=497595
    
    Review URL: https://codereview.chromium.org/1220673002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198673 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xmlhttprequest/XMLHttpRequestUpload.h
+++ third_party/WebKit/Source/core/xmlhttprequest/XMLHttpRequestUpload.h
@@ -43,15 +43,10 @@ class ExecutionContext;
 class XMLHttpRequestUpload final : public XMLHttpRequestEventTarget {
     DEFINE_WRAPPERTYPEINFO();
 public:
-    static PassOwnPtrWillBeRawPtr<XMLHttpRequestUpload> create(XMLHttpRequest* xmlHttpRequest)
+    static XMLHttpRequestUpload* create(XMLHttpRequest* xmlHttpRequest)
     {
-        return adoptPtrWillBeNoop(new XMLHttpRequestUpload(xmlHttpRequest));
+        return new XMLHttpRequestUpload(xmlHttpRequest);
     }
-
-#if !ENABLE(OILPAN)
-    void ref() { m_xmlHttpRequest->ref(); }
-    void deref() { m_xmlHttpRequest->deref(); }
-#endif
 
     XMLHttpRequest* xmlHttpRequest() const { return m_xmlHttpRequest; }
 
@@ -68,12 +63,7 @@ public:
 private:
     explicit XMLHttpRequestUpload(XMLHttpRequest*);
 
-#if !ENABLE(OILPAN)
-    void refEventTarget() override { ref(); }
-    void derefEventTarget() override { deref(); }
-#endif
-
-    RawPtrWillBeMember<XMLHttpRequest> m_xmlHttpRequest;
+    Member<XMLHttpRequest> m_xmlHttpRequest;
 
     // Last progress event values; used when issuing the
     // required 'progress' event on a request error or abort.
