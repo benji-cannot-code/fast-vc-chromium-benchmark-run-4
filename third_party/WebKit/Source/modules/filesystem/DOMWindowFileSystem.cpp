b5412ec760fdb6f0c8cbd991c0ad5ec5f1388a50
mkwst@chromium.org <mkwst@chromium.org>
2015-07-10 09:21:13 +0000

    Measure 'requestFileSystem' from non-webby URLs.
    Let's find out how often FileSystem is accessed on non-webby (e.g.
    'chrome-extension:') URLs, shall we? Seems like a good way of
    quantifying the impact to the actual web of removing the feature.
    
    R=philipj@opera.com
    
    Review URL: https://codereview.chromium.org/1227893003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198675 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/filesystem/DOMWindowFileSystem.cpp
+++ third_party/WebKit/Source/modules/filesystem/DOMWindowFileSystem.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Document.h"
 #include "core/fileapi/FileError.h"
 #include "core/frame/LocalDOMWindow.h"
+#include "core/frame/UseCounter.h"
 #include "modules/filesystem/DOMFileSystem.h"
 #include "modules/filesystem/EntryCallback.h"
 #include "modules/filesystem/ErrorCallback.h"
@@ -37,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/filesystem/FileSystemCallbacks.h"
 #include "modules/filesystem/LocalFileSystem.h"
 #include "platform/FileSystemType.h"
+#include "platform/weborigin/SchemeRegistry.h"
 #include "platform/weborigin/SecurityOrigin.h"
 
 namespace blink {
@@ -58,6 +60,9 @@ void DOMWindowFileSystem::webkitRequestFileSystem(DOMWindow& windowArg, int type
     Document* document = window.document();
     if (!document)
         return;
+
+    if (SchemeRegistry::schemeShouldBypassContentSecurityPolicy(document->securityOrigin()->protocol()))
+        UseCounter::count(document, UseCounter::RequestFileSystemNonWebbyOrigin);
 
     if (!document->securityOrigin()->canAccessFileSystem()) {
         DOMFileSystem::scheduleCallback(document, errorCallback, FileError::create(FileError::SECURITY_ERR));
