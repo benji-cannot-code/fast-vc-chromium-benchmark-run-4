218f81ad637c598c95004f71573df1d744d4e60f
pilgrim@chromium.org <pilgrim@chromium.org>
2015-07-10 16:00:12 +0000

    Code cleanup #1 in core/editing
    Spacing, header sorting, braces, other minor nits suggested by check-webkit-style
    
    BUG=
    TBR=eae@chromium.org
    
    Review URL: https://codereview.chromium.org/1228393002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198692 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/SpellChecker.cpp
+++ third_party/WebKit/Source/core/editing/SpellChecker.cpp
@@ -43,9 +43,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/LocalFrame.h"
 #include "core/frame/Settings.h"
 #include "core/html/HTMLInputElement.h"
+#include "core/layout/LayoutTextControl.h"
 #include "core/loader/EmptyClients.h"
 #include "core/page/Page.h"
-#include "core/layout/LayoutTextControl.h"
 #include "core/page/SpellCheckerClient.h"
 #include "platform/text/TextCheckerClient.h"
 
--- third_party/WebKit/Source/core/editing/VisibleSelection.h
+++ third_party/WebKit/Source/core/editing/VisibleSelection.h
@@ -142,7 +142,7 @@ public:
 
     void appendTrailingWhitespace();
 
-    bool expandUsingGranularity(TextGranularity granularity);
+    bool expandUsingGranularity(TextGranularity);
     bool expandUsingGranularityInComposedTree(TextGranularity);
 
     // We don't yet support multi-range selections, so we only ever have one range to return.
