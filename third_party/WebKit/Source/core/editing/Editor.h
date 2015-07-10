f9cd750c859c206ca9bbdedf800351726a8a64ef
yosin@chromium.org <yosin@chromium.org>
2015-07-10 05:45:31 +0000

    Introduce Editor::shouldDeleteRange() with EphemeralRange
    This patch introduces |Editor::shouldDeleteRange()| to take |EphemeralRange|
    instead of |Range| for a preparation of replacing redundant |Range| object
    with |EphemeralRange|.
    
    This patch is similar to http://crrev.com/1230703004, which changes
    |Editor::canDeleteRange()| to take |EphemeralRange|.
    
    This patch is a part of reducing redundant |Range| object
    http://crrev.com/1224693016
    
    Review URL: https://codereview.chromium.org/1229863003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198663 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/Editor.h
+++ third_party/WebKit/Source/core/editing/Editor.h
@@ -96,8 +96,6 @@ public:
 
     void transpose();
 
-    bool shouldDeleteRange(Range*) const;
-
     void respondToChangedContents(const VisibleSelection& endingSelection);
 
     bool selectionStartHasStyle(CSSPropertyID, const String& value) const;
@@ -261,6 +259,7 @@ private:
     }
 
     bool canDeleteRange(const EphemeralRange&) const;
+    bool shouldDeleteRange(const EphemeralRange&) const;
 
     UndoStack* undoStack() const;
 
