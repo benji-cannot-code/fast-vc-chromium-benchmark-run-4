25c69604094e54f2bf832f63a9dc2c05d8ec87db
s.singapati@samsung.com <s.singapati@samsung.com>
2015-07-10 00:56:51 +0000

    Remove typedefs for stdint types on WIN32.
    stdint.h should be available now on WIN32 / Visual Studio.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1223843017
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198642 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebCommon.h
+++ third_party/WebKit/public/platform/WebCommon.h
@@ -85,18 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Basic types
 
 #include <stddef.h> // For size_t
-
-#if defined(WIN32)
-// Visual Studio doesn't have stdint.h.
-typedef short int16_t;
-typedef unsigned short uint16_t;
-typedef int int32_t;
-typedef __int64 int64_t;
-typedef unsigned int uint32_t;
-typedef unsigned __int64 uint64_t;
-#else
 #include <stdint.h> // For int32_t
-#endif
 
 namespace blink {
 
