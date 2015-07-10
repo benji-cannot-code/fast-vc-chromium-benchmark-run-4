2cad1ba7d59d6d41ab701865a89f599d875ea10a
tonychun <tonychun@google.com>
2015-07-10 11:45:30 -0700

    Adding the ChromotingHostListFetcher and its respective unittests.
    The ChromotingHostListFetcher requests a host list from the Directory service (currently a REST call). The rest call is parsed and creates a ChromotingHostInfo struct instance, which encapsulates important host information. Once the host list of ChromotingHostInfos is created, it is returned to the callback.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1212333011
    
    Cr-Commit-Position: refs/heads/master@{#338328}

---
+++ remoting/test/host_info.h
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_TEST_HOST_INFO_H_
+#define REMOTING_TEST_HOST_INFO_H_
+
+#include <string>
+#include <vector>
+
+#include "base/values.h"
+
+namespace remoting {
+namespace test {
+
+enum HostStatus {
+  kHostStatusOnline,
+  kHostStatusOffline
+};
+
+struct HostInfo {
+  HostInfo();
+  ~HostInfo();
+
+  bool ParseHostInfo(const base::DictionaryValue& host_info);
+
+  std::string host_id;
+  std::string host_jid;
+  std::string host_name;
+  HostStatus status = kHostStatusOffline;
+  std::string offline_reason;
+  std::string public_key;
+  std::vector<std::string> token_url_patterns;
+};
+
+}  // namespace test
+}  // namespace remoting
+
+#endif  // REMOTING_TEST_HOST_INFO_H_
