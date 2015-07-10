26b6868d1deacebe6f7f7ac82af882ab7b9cd913
imcheng <imcheng@google.com>
2015-07-10 15:09:04 -0700

    [Media Router] Remove unnecessary CONTENT_EXPORT.
    BUG=504938
    NOTRY=true
    TBR=mfoltz
    
    Review URL: https://codereview.chromium.org/1232063005
    
    Cr-Commit-Position: refs/heads/master@{#338377}

--- content/public/browser/presentation_session.h
+++ content/public/browser/presentation_session.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-enum CONTENT_EXPORT PresentationSessionState {
+enum PresentationSessionState {
   PRESENTATION_SESSION_STATE_CONNECTED,
   PRESENTATION_SESSION_STATE_DISCONNECTED
 };
