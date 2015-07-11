4bc76254ff4f5cde9c95fef9eff91818ae80fe4a
mkwst@chromium.org <mkwst@chromium.org>
2015-07-11 12:51:01 +0000

    Revert of Remove 'blink::WebSerializedOrigin' (1/3) (patchset #2 id:20001 of https://codereview.chromium.org/1232563003/)
    Reason for revert:
    Blocking the roll; fails on http://build.chromium.org/p/tryserver.chromium.linux/builders/linux_chromium_gn_rel/builds/108650 for some reason. :/
    
    Original issue's description:
    > Remove 'blink::WebSerializedOrigin' (1/3)
    > 
    > As part of the plan to rationalize Origins across Blink and its embedder,
    > we're dropping 'blink::WebSerializedOrigin' in favor of
    > 'blink::WebSecurityOrigin' (which can serialize itself just fine).
    > 
    > Patch 1 (Blink): This patch.
    > Patch 2 (Chromium): https://codereview.chromium.org/1229783005
    > Patch 3 (Blink): https://codereview.chromium.org/1231643010
    > 
    > BUG=508896
    > R=jochen@chromium.org
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=198731
    
    TBR=jochen@chromium.org,peter@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=508896
    
    Review URL: https://codereview.chromium.org/1235673003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198733 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebSocketHandle.h
+++ third_party/WebKit/public/platform/WebSocketHandle.h
@@ -33,12 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WebSocketHandle_h
 
 #include "public/platform/WebCommon.h"
-#include "public/platform/WebSerializedOrigin.h"
 #include "public/platform/WebVector.h"
 
 namespace blink {
 
-class WebSecurityOrigin;
+class WebSerializedOrigin;
 class WebSocketHandleClient;
 class WebString;
 class WebURL;
@@ -59,12 +58,7 @@ public:
 
     virtual ~WebSocketHandle() { }
 
-    // TODO(mkwst): Drop the WebSerializedOrigin version once Chromium is updated: https://crbug.com/508896
-    virtual void connect(const WebURL& url, const WebVector<WebString>& protocols, const WebSecurityOrigin& origin, WebSocketHandleClient* client)
-    {
-        connect(url, protocols, WebSerializedOrigin(origin), client);
-    }
-    virtual void connect(const WebURL&, const WebVector<WebString>& protocols, const WebSerializedOrigin&, WebSocketHandleClient*) {}
+    virtual void connect(const WebURL& /* url */, const WebVector<WebString>& protocols, const WebSerializedOrigin& /* origin */, WebSocketHandleClient*) = 0;
     virtual void send(bool fin, MessageType, const char* data, size_t /* size */) = 0;
     virtual void flowControl(int64_t quota) = 0;
     virtual void close(unsigned short code, const WebString& reason) = 0;
