23d3cbede2932e6cfc9c9d884fec820e77c05ca3
erikchen <erikchen@chromium.org>
2015-07-13 11:23:51 -0700

    mac: Fix a bug in ui_gfx_render_text_mac.
    Both CTFontCreateCopyWithAttributes and CTFontCreateCopyWithSymbolicTraits are
    broken on OSX 10.10. Use CTFontCreateWithFontDescriptor instead.
    
    BUG=392920
    
    Review URL: https://codereview.chromium.org/1229323002
    
    Cr-Commit-Position: refs/heads/master@{#338533}

--- ui/gfx/render_text_mac.cc
+++ ui/gfx/render_text_mac.cc
@@ -16,6 +16,41 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "skia/ext/skia_utils_mac.h"
 
+namespace {
+
+// This function makes a copy of |font| with the given symbolic traits. This
+// function is similar to CTFontCreateCopyWithSymbolicTraits, but this function
+// works on OSX 10.10, unlike CTFontCreateCopyWithSymbolicTraits.
+base::ScopedCFTypeRef<CTFontRef> CopyFontWithSymbolicTraits(CTFontRef font,
+                                                            int sym_traits) {
+  base::ScopedCFTypeRef<CTFontDescriptorRef> orig_desc(
+      CTFontCopyFontDescriptor(font));
+  base::ScopedCFTypeRef<CFDictionaryRef> orig_attributes(
+      CTFontDescriptorCopyAttributes(orig_desc));
+  // Make a mutable copy of orig_attributes.
+  base::ScopedCFTypeRef<CFMutableDictionaryRef> attributes(
+      CFDictionaryCreateMutableCopy(kCFAllocatorDefault, 0, orig_attributes));
+
+  base::ScopedCFTypeRef<CFMutableDictionaryRef> traits(
+      CFDictionaryCreateMutable(kCFAllocatorDefault, 0,
+                                &kCFTypeDictionaryKeyCallBacks,
+                                &kCFTypeDictionaryValueCallBacks));
+  base::ScopedCFTypeRef<CFNumberRef> n(
+      CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &sym_traits));
+  CFDictionarySetValue(traits, kCTFontSymbolicTrait, n.release());
+  CFDictionarySetValue(attributes, kCTFontTraitsAttribute, traits.release());
+
+  base::ScopedCFTypeRef<CFStringRef> family_name(CTFontCopyFamilyName(font));
+  CFDictionarySetValue(attributes, kCTFontNameAttribute, family_name.release());
+
+  base::ScopedCFTypeRef<CTFontDescriptorRef> desc(
+      CTFontDescriptorCreateWithAttributes(attributes));
+  return base::ScopedCFTypeRef<CTFontRef>(
+      CTFontCreateWithFontDescriptor(desc, 0.0, nullptr));
+}
+
+}  // namespace
+
 namespace gfx {
 
 RenderTextMac::RenderTextMac()
@@ -280,8 +315,8 @@ base::ScopedCFTypeRef<CFMutableArrayRef> RenderTextMac::ApplyStyles(
     const int traits = (style.style(BOLD) ? kCTFontBoldTrait : 0) |
                        (style.style(ITALIC) ? kCTFontItalicTrait : 0);
     if (traits != 0) {
-      base::ScopedCFTypeRef<CTFontRef> styled_font(
-          CTFontCreateCopyWithSymbolicTraits(font, 0.0, NULL, traits, traits));
+      base::ScopedCFTypeRef<CTFontRef> styled_font =
+          CopyFontWithSymbolicTraits(font, traits);
       // TODO(asvitkine): Handle |styled_font| == NULL case better.
       if (styled_font) {
         CFAttributedStringSetAttribute(attr_string, range, kCTFontAttributeName,
