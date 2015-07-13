9a16b3db02c29d25110a7e0a50140cd270928798
erikchen <erikchen@chromium.org>
2015-07-13 16:41:20 -0700

    mac: Flush the autorelease pool after making a browser window in browser tests.
    Failure to do so causes some Chrome NSObjects to not be destroyed until after
    the rest of the test infrastructure has been torn down. This causes failures
    when running browser tests on Yosemite.
    
    This CL moves the method OpenURLOffTheRecord from the ui_test_utils namespace
    into an instance method on InProcessBrowserTest. This allows the method to flush
    the autoreleasepool on Macs.
    
    This CL adds the method OpenDevToolsWindow to InProcessBrowserTest. This is a
    wrapper around DevToolsWindow::OpenDevToolsWindow that flushes the
    autoreleasepool on Macs.
    
    BUG=445495
    
    Review URL: https://codereview.chromium.org/1225093003
    
    Cr-Commit-Position: refs/heads/master@{#338593}

--- chrome/browser/prerender/prerender_browsertest.cc
+++ chrome/browser/prerender/prerender_browsertest.cc
@@ -3930,8 +3930,7 @@ class PrerenderIncognitoBrowserTest : public PrerenderBrowserTest {
  public:
   void SetUpOnMainThread() override {
     Profile* normal_profile = current_browser()->profile();
-    set_browser(ui_test_utils::OpenURLOffTheRecord(
-        normal_profile, GURL("about:blank")));
+    set_browser(OpenURLOffTheRecord(normal_profile, GURL("about:blank")));
     PrerenderBrowserTest::SetUpOnMainThread();
   }
 };
--- chrome/browser/profiles/profile_browsertest.cc
+++ chrome/browser/profiles/profile_browsertest.cc
@@ -538,8 +538,7 @@ IN_PROC_BROWSER_TEST_F(ProfileBrowserTest,
 IN_PROC_BROWSER_TEST_F(ProfileBrowserTest,
                        URLFetcherUsingMainContextDuringIncognitoTeardown) {
   Browser* incognito_browser =
-      ui_test_utils::OpenURLOffTheRecord(browser()->profile(),
-                                         GURL("about:blank"));
+      OpenURLOffTheRecord(browser()->profile(), GURL("about:blank"));
   RunURLFetcherActiveDuringIncognitoTeardownTest(
       incognito_browser, incognito_browser->profile()->GetRequestContext());
 }
@@ -547,8 +546,8 @@ IN_PROC_BROWSER_TEST_F(ProfileBrowserTest,
 IN_PROC_BROWSER_TEST_F(ProfileBrowserTest,
                        URLFetcherUsingExtensionContextDuringIncognitoTeardown) {
   Browser* incognito_browser =
-      ui_test_utils::OpenURLOffTheRecord(browser()->profile(),
-                                         GURL("about:blank"));
+      OpenURLOffTheRecord(browser()->profile(), GURL("about:blank"));
+
   RunURLFetcherActiveDuringIncognitoTeardownTest(
       incognito_browser,
       incognito_browser->profile()->GetRequestContextForExtensions());
