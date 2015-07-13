9c49d63ea4294f2032aa904780f83f9f0627723e
reillyg <reillyg@chromium.org>
2015-07-13 00:24:55 -0700

    Use permission_broker's OpenPath method to open serial ports.
    This new method allows Chrome to open device nodes without having to
    chmod them to a group of which Chrome is a member and allows the
    permission broker to set opens on the file descriptor before passing it
    to Chrome. This is a win for security and future extensibility.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1235683003
    
    Cr-Commit-Position: refs/heads/master@{#338478}

--- device/serial/serial_io_handler.h
+++ device/serial/serial_io_handler.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/serial/buffer.h"
 #include "device/serial/serial.mojom.h"
 
+namespace dbus {
+class FileDescriptor;
+}
+
 namespace device {
 
 // Provides a simplified interface for performing asynchronous I/O on serial
@@ -36,8 +40,18 @@ class SerialIoHandler : public base::NonThreadSafe,
                     const serial::ConnectionOptions& options,
                     const OpenCompleteCallback& callback);
 
-  // Signals that the access request for |port| is complete.
-  void OnRequestAccessComplete(const std::string& port, bool success);
+#if defined(OS_CHROMEOS)
+  // Signals that the port has been opened.
+  void OnPathOpened(
+      scoped_refptr<base::SingleThreadTaskRunner> file_thread_task_runner,
+      scoped_refptr<base::SingleThreadTaskRunner> io_thread_task_runner,
+      dbus::FileDescriptor fd);
+
+  // Validates the file descriptor provided by the permission broker.
+  void ValidateOpenPort(
+      scoped_refptr<base::SingleThreadTaskRunner> io_thread_task_runner,
+      dbus::FileDescriptor fd);
+#endif  // defined(OS_CHROMEOS)
 
   // Performs an async Read operation. Behavior is undefined if this is called
   // while a Read is already pending. Otherwise, the Done or DoneWithError
@@ -120,12 +134,6 @@ class SerialIoHandler : public base::NonThreadSafe,
 
   // Platform-specific port configuration applies options_ to the device.
   virtual bool ConfigurePortImpl() = 0;
-
-  // Requests access to the underlying serial device, if needed.
-  virtual void RequestAccess(
-      const std::string& port,
-      scoped_refptr<base::SingleThreadTaskRunner> file_task_runner,
-      scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner);
 
   // Performs platform-specific, one-time port configuration on open.
   virtual bool PostOpen();
