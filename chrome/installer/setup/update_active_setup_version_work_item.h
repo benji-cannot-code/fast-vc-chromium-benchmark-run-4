fa784361ccb451c089cb095273e09fec33e59757
gab <gab@chromium.org>
2015-07-13 14:58:36 -0700

    Force restoration of Chrome's shortcuts when Active Setup kicks in in response to a major OS upgrade.
    BUG=502363
    TEST=setup_unittests.exe --gtest_filter=SetupUtilTest.UpdateLastOSUpgradeHandledByActiveSetup
    
    Review URL: https://codereview.chromium.org/1231973002
    
    Cr-Commit-Position: refs/heads/master@{#338572}

--- chrome/installer/setup/update_active_setup_version_work_item.h
+++ chrome/installer/setup/update_active_setup_version_work_item.h
@@ -16,6 +16,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // on demand. This WorkItem is only viable on machine-wide installs.
 class UpdateActiveSetupVersionWorkItem : public WorkItem {
  public:
+  // The components of the Active Setup Version entry, in order.
+  enum VersionComponent {
+    // The major version.
+    MAJOR,
+    // Unused component, always 0 for now.
+    UNUSED1,
+    // Number of OS upgrades handled since original install.
+    OS_UPGRADES,
+    // Unused component, always 0 for now.
+    UNUSED2,
+  };
+
   // The operation to be performed by this UpdateActiveSetupVersionWorkItem.
   enum Operation {
     // Update (or install if not present) the Active Setup "Version" in the
@@ -37,18 +49,6 @@ class UpdateActiveSetupVersionWorkItem : public WorkItem {
   void Rollback() override;
 
  private:
-  // The components of the Active Setup Version entry, in order.
-  enum ActiveSetupVersionComponent {
-    // The major version.
-    MAJOR,
-    // Unused component, always 0 for now.
-    UNUSED1,
-    // Number of OS upgrades handled since original install.
-    OS_UPGRADES,
-    // Unused component, always 0 for now.
-    UNUSED2,
-  };
-
   // Returns the updated Active Setup version to be used based on the
   // |existing_version|.
   base::string16 GetUpdatedActiveSetupVersion(
