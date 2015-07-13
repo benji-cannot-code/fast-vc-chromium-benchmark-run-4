fd68f58a3f53aaf2933dd409d5f2406cd3db9044
haraken@chromium.org <haraken@chromium.org>
2015-07-13 12:32:53 +0000

    Revert 198747 "Oilpan: Use SafePointAwareMutexLocker in add/remo..."
    It doesn't make sense to use SafePointAwareMutexLocker while we're in a safe point.
    This CL is harmless but doesn't make any sense. So revert it.
    
    > Oilpan: Use SafePointAwareMutexLocker in add/removeInterrupter and Heap::findPageFromAddress
    > 
    > MutexLocker has a risk of causing dead lock (although I'm not sure
    > if it will happen in practice).
    > 
    > BUG=420515
    > 
    > Review URL: https://codereview.chromium.org/1226353002
    
    TBR=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/1229833003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198772 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/ThreadState.cpp
+++ third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -1245,7 +1245,7 @@ void ThreadState::addInterruptor(Interruptor* interruptor)
     ASSERT(checkThread());
     SafePointScope scope(HeapPointersOnStack);
     {
-        SafePointAwareMutexLocker locker(threadAttachMutex(), HeapPointersOnStack);
+        MutexLocker locker(threadAttachMutex());
         m_interruptors.append(interruptor);
     }
 }
@@ -1255,7 +1255,7 @@ void ThreadState::removeInterruptor(Interruptor* interruptor)
     ASSERT(checkThread());
     SafePointScope scope(HeapPointersOnStack);
     {
-        SafePointAwareMutexLocker locker(threadAttachMutex(), HeapPointersOnStack);
+        MutexLocker locker(threadAttachMutex());
         size_t index = m_interruptors.find(interruptor);
         RELEASE_ASSERT(index != kNotFound);
         m_interruptors.remove(index);
