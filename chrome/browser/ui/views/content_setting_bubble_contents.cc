c5a94af5c1a736fad576be024dd1c4b097668517
estade <estade@chromium.org>
2015-07-13 16:53:11 -0700

    Remove unused class views::Menu
    BUG=none
    TBR=msw@chromium.org
    
    Review URL: https://codereview.chromium.org/1230053002
    
    Cr-Commit-Position: refs/heads/master@{#338597}

--- chrome/browser/ui/views/content_setting_bubble_contents.cc
+++ chrome/browser/ui/views/content_setting_bubble_contents.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/link.h"
-#include "ui/views/controls/menu/menu.h"
 #include "ui/views/controls/menu/menu_config.h"
 #include "ui/views/controls/menu/menu_runner.h"
 #include "ui/views/controls/separator.h"
--- ui/views/views.gyp
+++ ui/views/views.gyp
@@ -85,8 +85,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'controls/link.h',
       'controls/link_listener.h',
       'controls/menu/display_change_listener_mac.cc',
-      'controls/menu/menu.cc',
-      'controls/menu/menu.h',
       'controls/menu/menu_config.cc',
       'controls/menu/menu_config.h',
       'controls/menu/menu_config_mac.cc',
