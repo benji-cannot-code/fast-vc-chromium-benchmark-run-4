7c36836e4301b1f05f2e639e4a4b364161cd232e
sigbjornf@opera.com <sigbjornf@opera.com>
2015-07-13 06:50:37 +0000

    Oilpan: switch ImageLoader to be prefinalized.
    If an ImageLoader for an SVGImage ends up being swept out along with a
    FrameView (a ScrollableArea), the former may circuitously end up accessing
    the latter via its destructor. Having both be eagerly finalized, or
    prefinalized, doesn't guarantee a correct destruction order.
    
    Hence, to address, arrange for the ImageLoader to be finalized/disposed of
    first by way of a prefinalizing action.
    
    R=haraken
    BUG=491488
    
    Review URL: https://codereview.chromium.org/1226373008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198753 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/ImageLoader.h
+++ third_party/WebKit/Source/core/loader/ImageLoader.h
@@ -60,16 +60,11 @@ template<typename T> class EventSender;
 typedef EventSender<ImageLoader> ImageEventSender;
 
 class CORE_EXPORT ImageLoader : public NoBaseWillBeGarbageCollectedFinalized<ImageLoader>, public ImageResourceClient {
+    WILL_BE_USING_PRE_FINALIZER(ImageLoader, dispose);
 public:
     explicit ImageLoader(Element*);
     ~ImageLoader() override;
 
-    // We must run the destructor in the eager sweeping phase and call
-    // m_image->removeClient(this). Otherwise, the ImageResource can invoke
-    // didAddClient() for the ImageLoader that is about to die in the current
-    // lazy sweeping, and the didAddClient() can access on-heap objects that
-    // have already been finalized in the current lazy sweeping.
-    EAGERLY_FINALIZE();
     DECLARE_TRACE();
 
     enum UpdateFromElementBehavior {
@@ -157,12 +152,20 @@ private:
 
     void willRemoveClient(ImageLoaderClient&);
 
+    // For Oilpan, we must run dispose() as a prefinalizer and call
+    // m_image->removeClient(this) (and more.) Otherwise, the ImageResource can invoke
+    // didAddClient() for the ImageLoader that is about to die in the current
+    // lazy sweeping, and the didAddClient() can access on-heap objects that
+    // have already been finalized in the current lazy sweeping.
+    void dispose();
+
     RawPtrWillBeMember<Element> m_element;
     ResourcePtr<ImageResource> m_image;
     // FIXME: Oilpan: We might be able to remove this Persistent hack when
     // ImageResourceClient is traceable.
     GC_PLUGIN_IGNORE("http://crbug.com/383741")
     RefPtrWillBePersistent<Element> m_keepAlive;
+
 #if ENABLE(OILPAN)
     class ImageLoaderClientRemover {
     public:
@@ -174,6 +177,7 @@ private:
         ImageLoaderClient& m_client;
     };
     friend class ImageLoaderClientRemover;
+
     // Oilpan: This ImageLoader object must outlive its clients because they
     // need to call ImageLoader::willRemoveClient before they
     // die. Non-Persistent HeapHashMap doesn't work well because weak processing
