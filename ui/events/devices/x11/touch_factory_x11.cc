b7dc717cfc127ae2db7cfe391579b01702d1b00a
lanwei <lanwei@chromium.org>
2015-07-13 12:54:15 -0700

    Small fix for touch screen on Linux with or without flag --touch-devices
    This is a small fix, missing a pair of brackets of "if" statement for
    issue 1186833005: Fix touch screen on Linux with or without flag --touch-devices
    (https://codereview.chromium.org/1186833005/)
    
    BUG=486492
    
    Review URL: https://codereview.chromium.org/1230353002
    
    Cr-Commit-Position: refs/heads/master@{#338553}

--- ui/events/devices/x11/touch_factory_x11.cc
+++ ui/events/devices/x11/touch_factory_x11.cc
@@ -129,7 +129,7 @@ void TouchFactory::UpdateDeviceList(Display* display) {
           XITouchClassInfo* tci =
               reinterpret_cast<XITouchClassInfo*>(xiclassinfo);
           // Only care direct touch device (such as touch screen) right now
-          if (tci->mode == XIDirectTouch)
+          if (tci->mode == XIDirectTouch) {
             CacheTouchscreenIds(devinfo.deviceid);
             if (devinfo.use == XISlavePointer) {
               device_master_id_list_[devinfo.deviceid] = devinfo.attachment;
@@ -138,6 +138,7 @@ void TouchFactory::UpdateDeviceList(Display* display) {
               touch_device_lookup_[devinfo.attachment] = true;
               touch_device_list_[devinfo.attachment] = true;
             }
+          }
         }
       }
     }
