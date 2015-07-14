5f33fe08c4d1ff2b83ce937d09bf6a4659c4d4b2
Jaekyun Seok <jaekyun@chromium.org>
2015-07-14 10:43:40 +0900

    Use ChromeTabUtils#waitForTabPageLoaded() for a backgroud tab
    After https://codereview.chromium.org/1214243006 is landed, contents of
    background tab is hidden initially.
    And so we should use ChromeTabUtils#waitForTabPageLoaded() to wait for
    its loading.
    
    BUG=414719
    TBR=dfalcantara@chromium.org
    
    Review URL: https://codereview.chromium.org/1232603004 .
    
    Cr-Commit-Position: refs/heads/master@{#338617}

--- chrome/android/javatests/src/org/chromium/chrome/browser/document/DocumentModeTestBase.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/document/DocumentModeTestBase.java
@@ -286,6 +286,10 @@ public class DocumentModeTestBase extends MultiActivityTestBase {
                     }
                 }
         );
-        waitForFullLoad(newActivity, "Page 4");
+        if (selectIncognitoItem) {
+            waitForFullLoad(newActivity, "Page 4");
+        } else {
+            ChromeTabUtils.waitForTabPageLoaded(newActivity.getActivityTab(), (String) null);
+        }
     }
 }
