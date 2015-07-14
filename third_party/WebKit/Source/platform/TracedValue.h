561778083c472275ee73fbc11c65b793003ddc61
bashi@chromium.org <bashi@chromium.org>
2015-07-14 08:40:38 +0000

    Stop using JSONValues to implement TracedValue
    We allocate TracedValue using PartitionAlloc but we don't count amount of
    memory TracedValue consumes. This means that the memory dump for PartitionAlloc
    is overestimated. To keep track of TracedValue's memory usage, we need to
    treat all members of TracedValue as tracing overhead. The current TracedValue
    has JSONValues as its members, but JSONValues are not just for tracing so
    it would be difficult to treat them as tracing overhead. Instead, this CL
    re-implement TracedValue without using JSONValues. This implementation depends
    on Vectors and HashMap but we can easily treat them as tracing overhead by
    passing specialized allocators. Once we have an allocator for tracing overhead,
    we could use them to count amount of memory TracedValue used.
    
    Note that TracedValue also uses String so we also take Strings into account.
    
    BUG=508826
    
    Review URL: https://codereview.chromium.org/1226123006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198849 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/TracedValue.h
+++ third_party/WebKit/Source/platform/TracedValue.h
@@ -12,9 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/text/WTFString.h"
 
 namespace blink {
-class JSONArray;
-class JSONObject;
-class JSONValue;
+class TracedArrayValue;
+class TracedDictionaryValue;
+class InternalValue;
+
+// TODO(bashi): Use Allocator which counts allocation size as tracing overhead.
+typedef Vector<RefPtr<InternalValue>> TracedValueVector;
+typedef HashMap<String, RefPtr<InternalValue>> TracedValueHashMap;
 
 class PLATFORM_EXPORT TracedValue : public TraceEvent::ConvertableToTraceFormat {
     WTF_MAKE_NONCOPYABLE(TracedValue);
@@ -45,10 +49,10 @@ private:
     TracedValue();
     ~TracedValue() override;
 
-    JSONObject* currentDictionary() const;
-    JSONArray* currentArray() const;
+    TracedDictionaryValue* currentDictionary() const;
+    TracedArrayValue* currentArray() const;
 
-    Vector<RefPtr<JSONValue>> m_stack;
+    TracedValueVector m_stack;
 };
 
 } // namespace blink
