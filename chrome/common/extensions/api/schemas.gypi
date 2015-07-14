0bbd0056bb43e88f7a4241c9223ee504dfa5d008
michaelpg <michaelpg@chromium.org>
2015-07-13 21:00:42 -0700

    languageSettingsPrivate API definition and stub implementation
    The IDL was approved by API review and got l-g-t-m from kalman@ at https://codereview.chromium.org/1102693002/
    
    This IDL is the same, modulo stevenjb's comments on that CL and some bug fixes.
    
    BUG=479043
    
    R=stevenjb@chromium.org,kalman@chromium.org,asvitkine@chromium.org
    TBR=dbeam@chromium.org
    
    kalman:
      chrome/common/extensions/api/_api_features.json
      chrome/common/extensions/api/language_settings_private.idl (rubberstamped in 1102693002)
      chrome/common/extensions/api/schemas.gypi
    
    asvitkine:
      extensions/browser/extension_function_histogram_value.h
      tools/metrics/histograms/histograms.xml
    
    dbeam:
      (generated file: third_party/closure_compiler/externs/language_settings_private.js)
    
    Review URL: https://codereview.chromium.org/1155093006
    
    Cr-Commit-Position: refs/heads/master@{#338632}

--- chrome/common/extensions/api/schemas.gypi
+++ chrome/common/extensions/api/schemas.gypi
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'image_writer_private.idl',
       'inline_install_private.idl',
       'instance_id.json',
+      'language_settings_private.idl',
       'launcher_page.idl',
       'location.idl',
       'manifest_types.json',
--- chrome/common/extensions/permissions/permission_set_unittest.cc
+++ chrome/common/extensions/permissions/permission_set_unittest.cc
@@ -812,6 +812,7 @@ TEST(PermissionsTest, PermissionMessages) {
   skip.insert(APIPermission::kHotwordPrivate);
   skip.insert(APIPermission::kIdentityPrivate);
   skip.insert(APIPermission::kInputMethodPrivate);
+  skip.insert(APIPermission::kLanguageSettingsPrivate);
   skip.insert(APIPermission::kMediaPlayerPrivate);
   skip.insert(APIPermission::kMediaRouterPrivate);
   skip.insert(APIPermission::kMetricsPrivate);
