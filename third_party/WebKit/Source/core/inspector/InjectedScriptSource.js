c1127e03d30ae523cb1d9d3d7cfc06c5c84df31e
jonathan.garbee@chromium.org <jonathan.garbee@chromium.org>
2015-07-14 18:04:27 +0000

    Return an array from bling-bling
    BUG=507518
    
    Review URL: https://codereview.chromium.org/1232393003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198879 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InjectedScriptSource.js
+++ third_party/WebKit/Source/core/inspector/InjectedScriptSource.js
@@ -1771,8 +1771,8 @@ CommandLineAPIImpl.prototype = {
     $$: function (selector, opt_startNode)
     {
         if (this._canQuerySelectorOnNode(opt_startNode))
-            return opt_startNode.querySelectorAll(selector);
-        return inspectedGlobalObject.document.querySelectorAll(selector);
+            return slice(opt_startNode.querySelectorAll(selector));
+        return slice(inspectedGlobalObject.document.querySelectorAll(selector));
     },
 
     /**
