9879670c3d14302ccb53f14457f4b73e0775a4a6
vasilii <vasilii@chromium.org>
2015-07-14 07:55:03 -0700

    Show "via <IDP>" instead of a password in the password bubble.
    The string appears for federated credentials saved through the Credential Manager API on Aura platforms.
    
    BUG=400674
    
    Review URL: https://codereview.chromium.org/1237833002
    
    Cr-Commit-Position: refs/heads/master@{#338685}

--- chrome/browser/ui/views/passwords/manage_password_items_view.cc
+++ chrome/browser/ui/views/passwords/manage_password_items_view.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <numeric>
 
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ui/passwords/manage_passwords_bubble_model.h"
 #include "chrome/grit/generated_resources.h"
 #include "grit/components_strings.h"
@@ -80,11 +81,17 @@ scoped_ptr<views::Label> GenerateUsernameLabel(
 
 scoped_ptr<views::Label> GeneratePasswordLabel(
     const autofill::PasswordForm& form) {
-  scoped_ptr<views::Label> label(new views::Label(form.password_value));
+  base::string16 text = form.federation_url.is_empty()
+      ? form.password_value
+      : l10n_util::GetStringFUTF16(
+            IDS_MANAGE_PASSWORDS_IDENTITY_PROVIDER,
+            base::UTF8ToUTF16(form.federation_url.host()));
+  scoped_ptr<views::Label> label(new views::Label(text));
   label->SetFontList(ui::ResourceBundle::GetSharedInstance().GetFontList(
       ui::ResourceBundle::SmallFont));
   label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-  label->SetObscured(true);
+  if (form.federation_url.is_empty())
+    label->SetObscured(true);
   return label.Pass();
 }
 
