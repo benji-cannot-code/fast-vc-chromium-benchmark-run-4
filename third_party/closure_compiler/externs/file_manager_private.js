0109384a83fc9c84b249dd04ae8c67639fa4473a
mtomasz <mtomasz@chromium.org>
2015-07-13 19:15:21 -0700

    Update computeChecksum to accept entries instead of urls.
    NOPRESUBMIT=true
    TBR=isherman@chromium.org
    TEST=browser_tests: *FileManager*Checksum*
    BUG=507210
    
    Review URL: https://codereview.chromium.org/1211413005
    
    Cr-Commit-Position: refs/heads/master@{#338622}

--- third_party/closure_compiler/externs/file_manager_private.js
+++ third_party/closure_compiler/externs/file_manager_private.js
@@ -572,10 +572,10 @@ chrome.fileManagerPrivate.openInspector = function(type) {};
 
 /**
  * Computes an MD5 checksum for the given file.
- * @param {string} fileUrl
+ * @param {!Entry} entry
  * @param {function(string)} callback
  */
-chrome.fileManagerPrivate.computeChecksum = function(fileUrl, callback) {};
+chrome.fileManagerPrivate.computeChecksum = function(entry, callback) {};
 
 /**
  * Gets the MIME type of a file.
