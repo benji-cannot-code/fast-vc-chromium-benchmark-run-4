4a6b98b09b547b3320833c9c6a632bd03cd618b6
shuchen <shuchen@chromium.org>
2015-07-14 01:26:04 -0700

    InputMethod should stop TextInputClient::InsertChar/InsertText calls when the event is stopped propagation after DispatchKeyEventPostIME().
    BUG=502113,476893
    TEST=Verified on pixel device.
    
    Review URL: https://codereview.chromium.org/1209913002
    
    Cr-Commit-Position: refs/heads/master@{#338657}

--- ash/host/ash_remote_window_tree_host_win.cc
+++ ash/host/ash_remote_window_tree_host_win.cc
@@ -64,7 +64,7 @@ bool AshRemoteWindowTreeHostWin::DispatchKeyEventPostIME(
   input_method_handler()->SetPostIME(true);
   ui::EventSource::DeliverEventToProcessor(&event_copy);
   input_method_handler()->SetPostIME(false);
-  return event_copy.handled();
+  return event_copy.stopped_propagation();
 }
 
 ui::EventDispatchDetails AshRemoteWindowTreeHostWin::DeliverEventToProcessor(
