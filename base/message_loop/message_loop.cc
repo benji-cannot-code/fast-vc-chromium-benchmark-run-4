06c181388a71f0d6098947c6700452604a4937ef
caseq <caseq@chromium.org>
2015-07-14 06:31:18 -0700

    Revert of Trace MessageLoop::RunTask execution (patchset #2 id:20001 of https://codereview.chromium.org/1225333002/)
    Reason for revert:
    Regressed perf on Nexus 7
    BUG=509982,508005
    
    Original issue's description:
    > Trace MessageLoop::RunTask execution
    >
    > Previously, we were only issuing trace events around the task execution
    > within TaskAnnotator::RunTask. However, this does not include time spent
    > in observers (see bug for details on how it bites).
    >
    > BUG=508005
    >
    > Committed: https://crrev.com/87a2c639ff073ed3194a788ee1eaf82aabe77bab
    > Cr-Commit-Position: refs/heads/master@{#338492}
    
    TBR=danakj@chromium.org,dsinclair@chromium.org,nduca@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=508005
    
    Review URL: https://codereview.chromium.org/1234573005
    
    Cr-Commit-Position: refs/heads/master@{#338676}

--- base/message_loop/message_loop.cc
+++ base/message_loop/message_loop.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/thread_task_runner_handle.h"
 #include "base/threading/thread_local.h"
 #include "base/time/time.h"
-#include "base/trace_event/trace_event.h"
 #include "base/tracked_objects.h"
 
 #if defined(OS_MACOSX)
@@ -462,7 +461,6 @@ bool MessageLoop::ProcessNextDelayedNonNestableTask() {
 
 void MessageLoop::RunTask(const PendingTask& pending_task) {
   DCHECK(nestable_tasks_allowed_);
-  TRACE_EVENT0("toplevel", "MessageLoop::RunTask.WithObservers");
 
 #if defined(OS_WIN)
   if (pending_task.is_high_res) {
