2de3edb100839418df2278a1092cbe86b6280a93
toyoshim <toyoshim@chromium.org>
2015-07-14 02:42:03 -0700

    base/threading: restrict to set only current thread priority
    On some platforms, e.g., Linux, changing other thread's priority is not
    permitted by sandbox mechanisms for a security reason.
    Since there are no real use cases that really need to change another thread's
    priority for now, I'll replace the base API with the one that change the
    current thread priority to avoid confusion.
    
    BUG=468793,505474
    TEST=base_unittests
    TEST=sudo base_unittests --gtest_filter='Thread*'
    TEST=git cl try
    
    Review URL: https://codereview.chromium.org/1193303002
    
    Cr-Commit-Position: refs/heads/master@{#338661}

--- base/threading/platform_thread_linux.cc
+++ base/threading/platform_thread_linux.cc
@@ -39,12 +39,11 @@ const ThreadPriorityToNiceValuePair kThreadPriorityToNiceValueMap[4] = {
     {ThreadPriority::REALTIME_AUDIO, -10},
 };
 
-bool SetThreadPriorityForPlatform(PlatformThreadHandle handle,
-                                  ThreadPriority priority) {
+bool SetCurrentThreadPriorityForPlatform(ThreadPriority priority) {
 #if !defined(OS_NACL)
   ThreadPriority current_priority;
   if (priority != ThreadPriority::REALTIME_AUDIO &&
-      GetThreadPriorityForPlatform(handle, &current_priority) &&
+      GetCurrentThreadPriorityForPlatform(&current_priority) &&
       current_priority == ThreadPriority::REALTIME_AUDIO) {
     // If the pthread's round-robin scheduler is already enabled, and the new
     // priority will use setpriority() instead, the pthread scheduler should be
@@ -52,8 +51,6 @@ bool SetThreadPriorityForPlatform(PlatformThreadHandle handle,
     pthread_setschedparam(pthread_self(), SCHED_OTHER, &kResetPrio);
     return false;
   }
-  // TODO(gab): Assess the correctness of using |pthread_self()| below instead
-  // of |handle|. http://crbug.com/468793.
   return priority == ThreadPriority::REALTIME_AUDIO  &&
          pthread_setschedparam(pthread_self(), SCHED_RR, &kRealTimePrio) == 0;
 #else
@@ -61,13 +58,10 @@ bool SetThreadPriorityForPlatform(PlatformThreadHandle handle,
 #endif
 }
 
-bool GetThreadPriorityForPlatform(PlatformThreadHandle handle,
-                                  ThreadPriority* priority) {
+bool GetCurrentThreadPriorityForPlatform(ThreadPriority* priority) {
 #if !defined(OS_NACL)
   int maybe_sched_rr = 0;
   struct sched_param maybe_realtime_prio = {0};
-  // TODO(gab): Assess the correctness of using |pthread_self()| below instead
-  // of |handle|. http://crbug.com/468793.
   if (pthread_getschedparam(pthread_self(), &maybe_sched_rr,
                             &maybe_realtime_prio) == 0 &&
       maybe_sched_rr == SCHED_RR &&
