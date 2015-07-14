94031ed33145e77d137a24e241d3ff76f15bb66d
yhirano@chromium.org <yhirano@chromium.org>
2015-07-14 08:36:29 +0000

    Add implicit nullptr constructor to WebPrivateOwnPtr.
    Make WebPrivateOwnPtr constructible from nullptr implicitly.
    This is a preparation for WebCallbacks refactoring.
    
    BUG=493531
    
    Review URL: https://codereview.chromium.org/1237773002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198847 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebPrivateOwnPtr.h
+++ third_party/WebKit/public/platform/WebPrivateOwnPtr.h
@@ -46,7 +46,8 @@ namespace blink {
 template <typename T>
 class WebPrivateOwnPtr : public WebNonCopyable {
 public:
-    WebPrivateOwnPtr() : m_ptr(0) {}
+    WebPrivateOwnPtr() : m_ptr(nullptr) {}
+    WebPrivateOwnPtr(decltype(nullptr)) : m_ptr(nullptr) {}
     ~WebPrivateOwnPtr() { BLINK_ASSERT(!m_ptr); }
 
     explicit WebPrivateOwnPtr(T* ptr)
@@ -73,7 +74,7 @@ public:
     PassOwnPtr<T> release()
     {
         T* ptr = m_ptr;
-        m_ptr = 0;
+        m_ptr = nullptr;
         return adoptPtr(ptr);
     }
 
