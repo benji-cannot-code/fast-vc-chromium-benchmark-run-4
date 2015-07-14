d34ee4671033035de8114495c599f98fceb86c4f
Daniel Sievers <sievers@chromium.org>
2015-07-14 14:47:42 -0700

    gpu: Disable program caching on Adreno 4xx 103.0 drivers
    R=dyen@chromium.org
    TBR=zmo@chromium.org
    BUG=486117
    
    Review URL: https://codereview.chromium.org/1238753003 .
    
    Cr-Commit-Position: refs/heads/master@{#338737}

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -47,6 +47,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          disable_ns_cgl_surface_api)                         \
   GPU_OP(DISABLE_POST_SUB_BUFFERS_FOR_ONSCREEN_SURFACES,     \
          disable_post_sub_buffers_for_onscreen_surfaces)     \
+  GPU_OP(DISABLE_PROGRAM_CACHE,                              \
+         disable_program_cache)                              \
   GPU_OP(DISABLE_TIMESTAMP_QUERIES,                          \
          disable_timestamp_queries)                          \
   GPU_OP(ETC1_POWER_OF_TWO_ONLY,                             \
