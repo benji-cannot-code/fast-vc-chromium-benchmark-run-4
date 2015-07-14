d3158bc8591eae2fd1663e6be4884f9357275aea
eae@chromium.org <eae@chromium.org>
2015-07-14 02:42:13 +0000

    Removed unused variable in ShapeResult destructor
    Removed unused & unnecessary destory variable in ShapeResult destructor.
    
    TBR=pdr@chromium.org
    
    Review URL: https://codereview.chromium.org/1234763003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198829 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/shaping/HarfBuzzShaper.cpp
+++ third_party/WebKit/Source/platform/fonts/shaping/HarfBuzzShaper.cpp
@@ -167,12 +167,9 @@ void ShapeResult::RunInfo::setGlyphAndPositions(unsigned index,
 
 ShapeResult::~ShapeResult()
 {
-    unsigned destroyed = 0;
     for (unsigned i = 0; i < m_runs.size(); i++) {
-        if (m_runs[i]) {
+        if (m_runs[i])
             delete m_runs[i];
-            destroyed++;
-        }
     }
 }
 
