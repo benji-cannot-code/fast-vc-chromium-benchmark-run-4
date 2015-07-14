055facc7bcfc803c974deb0d5479cb05941b7b40
kotenkov@yandex-team.ru <kotenkov@yandex-team.ru>
2015-07-14 14:18:14 +0000

    Unify include style.
    Remove "Source/" part from includes in
    Source/platform/PartitionAllocMemoryDumpProvider.cpp and
    Source/platform/heap/BlinkGCMemoryDumpProvider.cpp.
    
    Review URL: https://codereview.chromium.org/1234923005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198864 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/BlinkGCMemoryDumpProvider.cpp
+++ third_party/WebKit/Source/platform/heap/BlinkGCMemoryDumpProvider.cpp
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "config.h"
-#include "Source/platform/heap/BlinkGCMemoryDumpProvider.h"
+#include "platform/heap/BlinkGCMemoryDumpProvider.h"
 
 #include "platform/heap/Handle.h"
 #include "public/platform/Platform.h"
