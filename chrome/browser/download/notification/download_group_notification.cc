9b6feca0d79bdeab4ef014d6c0df953e714a5e48
yoshiki <yoshiki@chromium.org>
2015-07-13 23:38:07 -0700

    [Download Notification] Truncate long filename in group notification
    Truncate and add an ellipsis if the filename has more than 20 characters.
    
    BUG=468558
    TEST=none
    
    Review URL: https://codereview.chromium.org/1184483006
    
    Cr-Commit-Position: refs/heads/master@{#338647}

--- chrome/browser/download/notification/download_group_notification.cc
+++ chrome/browser/download/notification/download_group_notification.cc
@@ -20,11 +20,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/l10n/time_format.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/text_elider.h"
+#include "ui/views/controls/label.h"
 
 namespace {
 
 const char kDownloadNotificationNotifierId[] =
     "chrome://downloads/notification/id-notifier";
+
+const size_t kMaxFilenameWidth = 160;  // in px
+
+base::string16 TruncateFilename(const base::FilePath& filename) {
+  return gfx::ElideFilename(filename,
+      views::Label().font_list(),
+      kMaxFilenameWidth);
+}
 
 }  // anonymous namespace
 
@@ -169,10 +179,9 @@ void DownloadGroupNotification::UpdateNotificationData() {
   std::vector<message_center::NotificationItem> subitems;
   for (auto download : items_) {
     DownloadItemModel model(download);
-    // TODO(yoshiki): Truncate long filename.
     // TODO(yoshiki): Use emplace_back when C++11 becomes allowed.
     subitems.push_back(message_center::NotificationItem(
-        download->GetFileNameToReportUser().LossyDisplayName(),
+        TruncateFilename(download->GetFileNameToReportUser()),
         model.GetStatusText()));
 
     if (!download->IsDone())
