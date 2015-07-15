f440104c8886f8c228d5baa7c8e388a5c3e3a082
yosin@chromium.org <yosin@chromium.org>
2015-07-15 22:21:51 +0000

    Make DocumentMarkerController::markersInRange() to take EphemeralRange instead of Range
    This patch makes |DocumentMarkerController::markersInRange()| to take
    |EphemeralRange| instead of |Range| to avoid using temporary |Range|
    for Oilpan.
    
    This patch is a preparation of getting rid of
    |FrameSelection::toNormalizedRange()| which returns |Range| object.
    
    BUG=388681
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1235163008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198984 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/DocumentMarkerController.h
+++ third_party/WebKit/Source/core/dom/DocumentMarkerController.h
@@ -86,7 +86,7 @@ public:
 
     DocumentMarker* markerContainingPoint(const LayoutPoint&, DocumentMarker::MarkerType);
     DocumentMarkerVector markersFor(Node*, DocumentMarker::MarkerTypes = DocumentMarker::AllMarkers());
-    DocumentMarkerVector markersInRange(Range*, DocumentMarker::MarkerTypes);
+    DocumentMarkerVector markersInRange(const EphemeralRange&, DocumentMarker::MarkerTypes);
     DocumentMarkerVector markers();
     Vector<IntRect> renderedRectsForMarkers(DocumentMarker::MarkerType);
     void updateRenderedRectsForMarkers();
