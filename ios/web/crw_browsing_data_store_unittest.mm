0552db0070d28682a9a29f7348059063b47afa38
justincohen <justincohen@chromium.org>
2015-07-15 13:38:05 -0700

    Correct iOS build for latest Xcode beta.
    NSObject's NS_DESIGNATED_INITIALIZER -init method require an -init
    NS_UNAVAILABLE implementation.
    
    BUG=498581
    
    Review URL: https://codereview.chromium.org/1237233005
    
    Cr-Commit-Position: refs/heads/master@{#338906}

--- ios/web/crw_browsing_data_store_unittest.mm
+++ ios/web/crw_browsing_data_store_unittest.mm
@@ -51,6 +51,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return self;
 }
 
+- (instancetype)init {
+  NOTREACHED();
+  return nil;
+}
+
 - (void)dealloc {
   [_browsingDataStore removeObserver:self forKeyPath:@"mode"];
   [super dealloc];
