bdcb9402bdd1b6634a3df506796adca59fa04004
yosin@chromium.org <yosin@chromium.org>
2015-07-15 22:22:34 +0000

    Get rid of redundant member function PositionIterator::isCandidate()
    This patch gets rid of redundant member function |isCandidate()| from
    |PositionIteratorAlgorithm<Strategy>| template class by replacing call sites
    with |PositionAlgorithm<Strategy>::isCandidate()| for ease of working these
    code, since call sites, |nextCandidate()| and |previousCandidate()|, assume
    returned |Position| satisfies |isCandidate()| |true| in
    |VisiblePosition::canonicalizeCandidate()|
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1238853002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198986 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/htmlediting.cpp
+++ third_party/WebKit/Source/core/editing/htmlediting.cpp
@@ -337,8 +337,9 @@ PositionAlgorithm<Strategy> nextCandidateAlgorithm(const PositionAlgorithm<Strat
     PositionIteratorAlgorithm<Strategy> p(position);
     while (!p.atEnd()) {
         p.increment();
-        if (p.isCandidate())
-            return p;
+        PositionAlgorithm<Strategy> candidate = p;
+        if (candidate.isCandidate())
+            return candidate;
     }
     return PositionAlgorithm<Strategy>();
 }
@@ -371,8 +372,9 @@ PositionAlgorithm<Strategy> previousCandidateAlgorithm(const PositionAlgorithm<S
     PositionIteratorAlgorithm<Strategy> p(position);
     while (!p.atStart()) {
         p.decrement();
-        if (p.isCandidate())
-            return p;
+        PositionAlgorithm<Strategy> candidate = p;
+        if (candidate.isCandidate())
+            return candidate;
     }
     return PositionAlgorithm<Strategy>();
 }
