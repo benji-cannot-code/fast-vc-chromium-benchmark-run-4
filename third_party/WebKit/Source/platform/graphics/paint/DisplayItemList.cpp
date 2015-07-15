321332eae46e4c8a90e2df319d2f70f84af7db85
jbroman@chromium.org <jbroman@chromium.org>
2015-07-15 13:40:42 +0000

    Expose approximate unshared memory usage through a WebContentLayerClient getter.
    BUG=481083
    
    Review URL: https://codereview.chromium.org/1235983003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198953 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/paint/DisplayItemList.cpp
+++ third_party/WebKit/Source/platform/graphics/paint/DisplayItemList.cpp
@@ -329,7 +329,7 @@ void DisplayItemList::commitNewDisplayItems()
     m_numCachedItems = 0;
 }
 
-size_t DisplayItemList::approximateMemoryUsage() const
+size_t DisplayItemList::approximateUnsharedMemoryUsage() const
 {
     size_t memoryUsage = sizeof(*this);
 
