929afdef6374ce1327ce8f2905183798c984681d
msw <msw@chromium.org>
2015-07-15 16:10:59 -0700

    Get output from hung mojo apptests.
    Depends on https://codereview.chromium.org/1239563003
    
    Start Xvfb and Openbox separately from the test process.
    This lets us get output from killed hung test processes.
    (wrapping the commandline with xvfb.py prevents this)
    
    Remove xvfb from Paths objects. Use more single quotes.
    
    TODO: Use test_env.run_executable() or its env vars?
    
    BUG=493535,504917
    TEST=Apptests killed for timeouts yield output.
    R=maruel@chromium.org
    TBR=ben@chromium.org,sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1230683002
    
    Cr-Commit-Position: refs/heads/master@{#338943}

--- mojo/tools/generate_mojo_shell_assets_list.py
+++ mojo/tools/generate_mojo_shell_assets_list.py
@@ -5,14 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Generates the assets_list file from a directory."""
+'''Generates the assets_list file from a directory.'''
 
 import argparse
 import os
 import sys
 
 def main():
-  parser = argparse.ArgumentParser(usage="--dir <directory>")
+  parser = argparse.ArgumentParser(usage='--dir <directory>')
 
   parser.add_argument('--dir', help='Directory read files from.', required=True)
 
