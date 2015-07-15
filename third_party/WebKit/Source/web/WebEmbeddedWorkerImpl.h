e36d054b7d8e5d39d1c705d04a723efe01536048
falken@chromium.org <falken@chromium.org>
2015-07-15 05:20:21 +0000

    Remove defunct pause after download code in WebEmbeddedWorkerImpl.
    This is no longer needed after the Chromium change
    http://crrev.com/441ec9912e6b
    
    BUG=457013
    
    Review URL: https://codereview.chromium.org/1238633002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198927 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebEmbeddedWorkerImpl.h
+++ third_party/WebKit/Source/web/WebEmbeddedWorkerImpl.h
@@ -62,7 +62,6 @@ public:
 
     // WebEmbeddedWorker overrides.
     void startWorkerContext(const WebEmbeddedWorkerStartData&) override;
-    void resumeAfterDownload() override;
     void terminateWorkerContext() override;
     void attachDevTools(const WebString& hostId) override;
     void reattachDevTools(const WebString& hostId, const WebString& savedState) override;
@@ -127,12 +126,6 @@ private:
         WaitingForDebuggerAfterScriptLoaded,
         NotWaitingForDebugger
     };
-
-    enum {
-        DontPauseAfterDownload,
-        DoPauseAfterDownload,
-        IsPausedAfterDownload
-    } m_pauseAfterDownloadState;
 
     WaitingForDebuggerState m_waitingForDebuggerState;
 };
