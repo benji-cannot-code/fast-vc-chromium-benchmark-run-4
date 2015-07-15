275a98004c0c251191c7cbb14fb304b4c7d195a1
dominicc@chromium.org <dominicc@chromium.org>
2015-07-15 00:04:45 +0000

    Simplify AbstractAudioContext's stopping mechanism.
    AbstractAudioContext has comments about the ExecutionContext calling
    ActiveDOMObjcet::stop multiple times, however that should not
    happen. In fact, AudioContext::closeContext is calling stop()
    itself. So don't do that.
    
    Further, there's ominous comments about dereferencing other
    ActiveDOMObjects when stopping. ActiveDOMObject notification should
    handle that (if an ActiveDOMObject is deleted during iteration, for
    example) so make uninitialization happen synchronously.
    
    The goal here is to make it so after AudioContext's get the
    ActiveDOMObject::stop notification, they are really stopped.
    
    BUG=503845
    
    Review URL: https://codereview.chromium.org/1236983003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198897 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webaudio/AbstractAudioContext.h
+++ third_party/WebKit/Source/modules/webaudio/AbstractAudioContext.h
@@ -229,10 +229,9 @@ public:
     void notifyStateChange();
 
     // A context is considered closed if:
-    //  - closeContext() has been called, even if the audio HW has not yet been
-    //    stopped.  It will be stopped eventually.
-    //  - it has been stopped (or is stopping) by its execution context.
-    virtual bool isContextClosed() const { return m_isStopScheduled || m_isCleared; }
+    //  - closeContext() has been called.
+    //  - it has been stopped by its execution context.
+    virtual bool isContextClosed() const { return m_isCleared; }
 
     // Get the security origin for this audio context.
     SecurityOrigin* securityOrigin() const;
@@ -255,9 +254,6 @@ protected:
 private:
     void initialize();
 
-    // ExecutionContext calls stop twice.
-    // We'd like to schedule only one stop action for them.
-    bool m_isStopScheduled;
     bool m_isCleared;
     void clear();
 
