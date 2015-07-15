735d74fcf9e75095fc13704494c95c959da85228
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-07-15 16:51:39 -0700

    [Extensions] Add a referrer to the url for reporting extension abuse
    Add a referrer to the url that we use to report abuse from the extension
    uninstall dialog so that we can track its usage separately.
    
    BUG=510618
    
    Review URL: https://codereview.chromium.org/1237313003
    
    Cr-Commit-Position: refs/heads/master@{#338950}

--- extensions/common/extension_urls.cc
+++ extensions/common/extension_urls.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_urls.h"
 
 #include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extensions_client.h"
@@ -72,8 +73,11 @@ GURL GetWebstoreUpdateUrl() {
   return GURL(kChromeWebstoreUpdateURL);
 }
 
-GURL GetWebstoreReportAbuseUrl(const std::string& extension_id) {
-  return GURL(GetWebstoreLaunchURL() + "/report/" + extension_id);
+GURL GetWebstoreReportAbuseUrl(const std::string& extension_id,
+                               const std::string& referrer_id) {
+  return GURL(base::StringPrintf("%s/report/%s?utm_source=%s",
+                                 GetWebstoreLaunchURL().c_str(),
+                                 extension_id.c_str(), referrer_id.c_str()));
 }
 
 bool IsWebstoreUpdateUrl(const GURL& update_url) {
