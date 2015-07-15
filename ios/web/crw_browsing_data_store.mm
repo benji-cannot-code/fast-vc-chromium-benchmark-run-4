6dc645e95799744dd74bd0dce0a19f65ebb6dc33
justincohen <justincohen@chromium.org>
2015-07-15 06:12:40 -0700

    Correct iOS build for latest Xcode beta.
    NSObject's NS_DESIGNATED_INITIALIZER -init method require an -init
    NS_UNAVAILABLE implementation.
    
    BUG=498581
    
    Review URL: https://codereview.chromium.org/1225203005
    
    Cr-Commit-Position: refs/heads/master@{#338846}

--- ios/web/crw_browsing_data_store.mm
+++ ios/web/crw_browsing_data_store.mm
@@ -170,6 +170,11 @@ NSString* const kStashOperationName = @"CRWBrowsingDataStore.STASH";
   return self;
 }
 
+- (instancetype)init {
+  NOTREACHED();
+  return nil;
+}
+
 - (NSString*)description {
   NSString* format = @"<%@: %p; hasPendingOperations = { %@ }; mode = { %@ }>";
   NSString* hasPendingOperationsString =
