c83031e716740246134f21fbf7e11060127e7c9e
dtseng <dtseng@chromium.org>
2015-07-15 09:17:35 -0700

    Support braille commands in ChromeVox Next.
    This includes commands inside of views and web contents. The distinction no longer matters because of the automation tree. It also works in the various modes utilizing the automation tree (Next, force next, and compat). This enables panning inside of the shelf for the first time in ChromeVox since compat mode is now enabled by default there.
    
    TEST=command inside of OOBE, shelf.
    BUG=488693
    
    Review URL: https://codereview.chromium.org/1218103003
    
    Cr-Commit-Position: refs/heads/master@{#338867}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
@@ -17,6 +17,7 @@ goog.require('ClassicCompatibility');
 goog.require('Output');
 goog.require('Output.EventType');
 goog.require('cursors.Cursor');
+goog.require('cvox.BrailleKeyCommand');
 goog.require('cvox.ChromeVoxEditableTextBase');
 
 goog.scope(function() {
@@ -316,8 +317,10 @@ Background.prototype = {
       if (node) {
         current = cursors.Range.fromNode(node);
       } else {
+        if (predErrorMsg) {
           cvox.ChromeVox.tts.speak(cvox.ChromeVox.msgs.getMsg(predErrorMsg),
-                                  cvox.QueueMode.FLUSH);
+                                   cvox.QueueMode.FLUSH);
+        }
         return;
       }
     }
@@ -336,6 +339,40 @@ Background.prototype = {
               this.currentRange_, prevRange, Output.EventType.NAVIGATE)
           .go();
     }
+  },
+
+  /**
+   * Handles a braille command.
+   * @param {!cvox.BrailleKeyEvent} evt
+   * @return {boolean} True if evt was processed.
+   */
+  onGotBrailleCommand: function(evt) {
+    if (this.mode_ === ChromeVoxMode.CLASSIC)
+      return false;
+
+    switch (evt.command) {
+      case cvox.BrailleKeyCommand.PAN_LEFT:
+        this.onGotCommand('previousElement', true);
+        break;
+      case cvox.BrailleKeyCommand.PAN_RIGHT:
+        this.onGotCommand('nextElement', true);
+        break;
+      case cvox.BrailleKeyCommand.LINE_UP:
+        this.onGotCommand('previousLine', true);
+        break;
+      case cvox.BrailleKeyCommand.LINE_DOWN:
+        this.onGotCommand('nextLine', true);
+        break;
+      case cvox.BrailleKeyCommand.TOP:
+        this.onGotCommand('goToBeginning', true);
+        break;
+      case cvox.BrailleKeyCommand.BOTTOM:
+        this.onGotCommand('goToEnd', true);
+        break;
+      default:
+        return false;
+    }
+    return true;
   },
 
   /**
