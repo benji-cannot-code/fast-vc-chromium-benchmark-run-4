d7558a07bda95424d56805f9e9a5255f461d2b4c
tapted <tapted@chromium.org>
2015-07-14 18:20:25 -0700

    Just set borders once when creating a views::LabelButton
    When creating a LabelButton, OnNativeThemeChanged(ui::NativeTheme*) is
    currently called 2 or 3 times:
    
    - via a SetStyle(STYLE_TEXTBUTTON) call in the LabelButton constructor
    (passes null)
    - for STYLE_BUTTON buttons, via a second SetStyle(STYLE_BUTTON) call
    shortly after construction (passes null since the button is not yet in a
    Widget)
    - by an ancestor View when added to a Widget's view hierarchy for the
    first time.
    
    Each call creates and sets the button border painters, which are not
    simple. The first call also calls virtual functions from a constructor,
    which may not even provide a correct border for a subclass. Also,
    subclasses that make their own SetBorder() call do not even want borders
    provided by LabelButton::UpdateThemedBorder(), but they currently get
    them created anyway (which they then replace).
    
    Since the first 1-2 borders that are created will always be replaced
    when the view is added to a Widget, don't create borders then.
    
    Instead, SetStyle() now just sets things up for an anticipated call to
    OnNativeThemeChanged() to finish creating the borders.
    
    After this, LabelButton no longer calls virtual methods from its
    constructor.
    
    BUG=506729,508410
    
    Review URL: https://codereview.chromium.org/1228213003
    
    Cr-Commit-Position: refs/heads/master@{#338796}

--- chrome/test/base/view_event_test_base.cc
+++ chrome/test/base/view_event_test_base.cc
@@ -24,17 +24,10 @@ namespace {
 // View subclass that allows you to specify the preferred size.
 class TestView : public views::View {
  public:
-  TestView() {}
-
-  void SetPreferredSize(const gfx::Size& size) {
-    preferred_size_ = size;
-    PreferredSizeChanged();
-  }
+  explicit TestView(ViewEventTestBase* harness) : harness_(harness) {}
 
   gfx::Size GetPreferredSize() const override {
-    if (!preferred_size_.IsEmpty())
-      return preferred_size_;
-    return View::GetPreferredSize();
+    return harness_->GetPreferredSize();
   }
 
   void Layout() override {
@@ -43,7 +36,7 @@ class TestView : public views::View {
   }
 
  private:
-  gfx::Size preferred_size_;
+  ViewEventTestBase* harness_;
 
   DISALLOW_COPY_AND_ASSIGN(TestView);
 };
@@ -101,6 +94,10 @@ void ViewEventTestBase::TearDown() {
   ui::ShutdownInputMethodForTesting();
 }
 
+gfx::Size ViewEventTestBase::GetPreferredSize() const {
+  return gfx::Size();
+}
+
 bool ViewEventTestBase::CanResize() const {
   return true;
 }
@@ -109,8 +106,7 @@ views::View* ViewEventTestBase::GetContentsView() {
   if (!content_view_) {
     // Wrap the real view (as returned by CreateContentsView) in a View so
     // that we can customize the preferred size.
-    TestView* test_view = new TestView();
-    test_view->SetPreferredSize(GetPreferredSize());
+    TestView* test_view = new TestView(this);
     test_view->AddChildView(CreateContentsView());
     content_view_ = test_view;
   }
@@ -142,10 +138,6 @@ void ViewEventTestBase::StartMessageLoopAndRunTest() {
       FROM_HERE, base::Bind(&ViewEventTestBase::DoTestOnMessageLoop, this));
 
   content::RunThisRunLoop(&run_loop_);
-}
-
-gfx::Size ViewEventTestBase::GetPreferredSize() const {
-  return gfx::Size();
 }
 
 void ViewEventTestBase::ScheduleMouseMoveInBackground(int x, int y) {
