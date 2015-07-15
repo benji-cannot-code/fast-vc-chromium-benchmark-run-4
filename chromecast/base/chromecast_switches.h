c52adb8c2cb6089ef250891a7bcb76d42a9afc63
halliwell <halliwell@chromium.org>
2015-07-15 13:40:22 -0700

    Change Cast window transparency option from switch to API
    The main use case for this feature involves starting a new Cast app
    that requires transparency (not starting a new cast_shell process).
    Also folded in the code to set the background to black for default
    case - this is to prevent white flashes between apps.  Previously
    was internal and it doesn't make sense to set this from two places.
    
    BUG=internal b/22121206
    
    Review URL: https://codereview.chromium.org/1239623004
    
    Cr-Commit-Position: refs/heads/master@{#338907}

--- chromecast/base/chromecast_switches.h
+++ chromecast/base/chromecast_switches.h
@@ -28,9 +28,6 @@ extern const char kNoWifi[];
 extern const char kLastLaunchedApp[];
 extern const char kPreviousApp[];
 
-// Graphics switches
-extern const char kEnableTransparentBackground[];
-
 }  // namespace switches
 
 #endif  // CHROMECAST_BASE_CHROMECAST_SWITCHES_H_
