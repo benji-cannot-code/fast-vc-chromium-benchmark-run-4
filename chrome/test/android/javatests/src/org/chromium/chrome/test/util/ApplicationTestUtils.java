4c9687500c0fff6ec9afbce014878f6234c65f5b
dfalcantara <dfalcantara@chromium.org>
2015-07-16 14:20:11 -0700

    Don't cause tearDown() failures in ApplicationTestUtils
    If the test is just trying to clean up, don't count it as a failure.
    
    BUG=510659
    
    Review URL: https://codereview.chromium.org/1244453002
    
    Cr-Commit-Position: refs/heads/master@{#339123}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/ApplicationTestUtils.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/ApplicationTestUtils.java
@@ -13,6 +13,7 @@ import android.os.Build;
 import android.os.PowerManager;
 
 import junit.framework.Assert;
+import junit.framework.AssertionFailedError;
 
 import org.chromium.base.ApplicationState;
 import org.chromium.base.ApplicationStatus;
@@ -56,7 +57,12 @@ public class ApplicationTestUtils {
     public static void tearDown(Context context) throws Exception {
         Assert.assertNotNull("Uninitialized wake lock", sWakeLock);
         sWakeLock.release();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) finishAllChromeTasks(context);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            try {
+                finishAllChromeTasks(context);
+            } catch (AssertionFailedError exception) {
+            }
+        }
     }
 
     /**
@@ -114,9 +120,6 @@ public class ApplicationTestUtils {
     /** Finishes all tasks Chrome has listed in Android's Overview. */
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public static void finishAllChromeTasks(final Context context) throws Exception {
-        // Go to the Home screen so that Android has no good reason to keep Chrome Activities alive.
-        fireHomeScreenIntent(context);
-
         // Close all of the tasks one by one.
         ActivityManager activityManager =
                 (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
