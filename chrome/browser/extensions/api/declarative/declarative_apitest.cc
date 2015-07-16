77b995486a3f2320d5e3287e5e62d115c0deeca3
wittman <wittman@chromium.org>
2015-07-16 15:53:01 -0700

    Remove RulesRegistry::Rule typedef
    The actual type is only three more characters, and makes it easier to
    distinguish the API rule representation from the registry rule
    representation. No functional change is intended.
    
    BUG=492946
    
    Review URL: https://codereview.chromium.org/1242503002
    
    Cr-Commit-Position: refs/heads/master@{#339144}

--- chrome/browser/extensions/api/declarative/declarative_apitest.cc
+++ chrome/browser/extensions/api/declarative/declarative_apitest.cc
@@ -24,12 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/test/extension_test_message_listener.h"
 
 using content::BrowserThread;
-using extensions::Extension;
-using extensions::ExtensionPrefs;
-using extensions::RulesRegistry;
-using extensions::RulesRegistryService;
-using extensions::TestExtensionDir;
-using extensions::WebRequestRulesRegistry;
+
+namespace extensions {
 
 namespace {
 
@@ -117,7 +113,8 @@ class DeclarativeApiTest : public ExtensionApiTest {
         rules_registry_service->GetRulesRegistry(
             RulesRegistryService::kDefaultRulesRegistryID,
             extensions::declarative_webrequest_constants::kOnRequest);
-    std::vector<linked_ptr<RulesRegistry::Rule> > rules;
+
+    std::vector<linked_ptr<core_api::events::Rule>> rules;
     BrowserThread::PostTask(
         BrowserThread::IO,
         FROM_HERE,
@@ -255,3 +252,5 @@ IN_PROC_BROWSER_TEST_F(DeclarativeApiTest, NoTracesAfterUninstalling) {
   EXPECT_EQ(0u, NumberOfRegisteredRules(extension_id));
   EXPECT_FALSE(extension_prefs->HasPrefForExtension(extension_id));
 }
+
+}  // namespace extensions
