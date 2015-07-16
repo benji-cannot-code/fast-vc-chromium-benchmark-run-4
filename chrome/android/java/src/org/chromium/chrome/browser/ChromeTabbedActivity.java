a62434d14b88a83ff269badfe726a70f58575302
newt <newt@chromium.org>
2015-07-16 14:54:33 -0700

    Standardize naming of "data reduction" in Android UI code.
    DataReduction is the standard name used in code for the "Data Saver"
    feature. Parts of the codebase, however, use the terms
    BandwidthReduction and ReduceDataUsage instead, and inconsistently so.
    This changes everything to use DataReduction.
    
    This also moves BandwidthType.java to the privacy package, since that's
    the only place it's used.
    
    Review URL: https://codereview.chromium.org/1242513003
    
    Cr-Commit-Position: refs/heads/master@{#339134}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -65,8 +65,8 @@ import org.chromium.chrome.browser.partnercustomizations.PartnerBrowserCustomiza
 import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
 import org.chromium.chrome.browser.preferences.ConnectionChangeReceiver;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
-import org.chromium.chrome.browser.preferences.bandwidth.BandwidthReductionPreferences;
-import org.chromium.chrome.browser.preferences.bandwidth.DataReductionPromoScreen;
+import org.chromium.chrome.browser.preferences.datareduction.DataReductionPreferences;
+import org.chromium.chrome.browser.preferences.datareduction.DataReductionPromoScreen;
 import org.chromium.chrome.browser.signin.SigninPromoScreen;
 import org.chromium.chrome.browser.snackbar.undo.UndoBarPopupController;
 import org.chromium.chrome.browser.sync.SyncController;
@@ -271,7 +271,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements ActionBarDel
             super.finishNativeInitialization();
 
             if (getActivityTab() != null) {
-                BandwidthReductionPreferences.launchDataReductionSSLInfoBar(
+                DataReductionPreferences.launchDataReductionSSLInfoBar(
                         this, getActivityTab().getWebContents());
             }
         } finally {
