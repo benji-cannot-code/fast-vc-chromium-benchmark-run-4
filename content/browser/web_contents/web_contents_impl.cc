4e5454eecdcfc383fb5cbd4b03457ee2630adbe0
dtseng <dtseng@chromium.org>
2015-07-16 09:41:59 -0700

    Reset RendererAccessibility state when enabling tree only accessibility mode.
    When a caller of the automation extension API requests a tree (desktop or tabs) for a previously requested tree, a load complete  does not fire as a result of the request. This is problematic when the caller was not the one to make any of the previous requests.
    
    To reproduce:
    - turn on ChromeVox in OOBE
    - notice all elements are read
    - toggle ChromeVox
    - notice some elements are no longer read.
    
    This is because ChromeVox attempts to call EnableFrame() on a subtree of the desktop tree. The request on the browser side attempts to enable tree only accessibility on a rfh that already has tree only accessibility enabled. The call returns early as a result. Events fired on this subtree never get handled inside of the extension because the tree data never got sent.
    
    BUG=488693
    
    Review URL: https://codereview.chromium.org/1215063002
    
    Cr-Commit-Position: refs/heads/master@{#339051}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -864,6 +864,7 @@ const std::string& WebContentsImpl::GetUserAgentOverride() const {
 }
 
 void WebContentsImpl::EnableTreeOnlyAccessibilityMode() {
+  RemoveAccessibilityMode(AccessibilityModeTreeOnly);
   AddAccessibilityMode(AccessibilityModeTreeOnly);
 }
 
