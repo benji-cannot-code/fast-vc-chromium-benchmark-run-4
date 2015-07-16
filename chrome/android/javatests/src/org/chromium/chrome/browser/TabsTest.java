5992538a6e86a432412927dffa1d53924a4ad583
pkotwicz <pkotwicz@chromium.org>
2015-07-16 08:49:30 -0700

    Actually disabling flaky TabsTest#testAlertDialogDoesNotChangeActiveModel()
    BUG=510659
    TBR=yusufo
    
    Review URL: https://codereview.chromium.org/1226123011
    
    Cr-Commit-Position: refs/heads/master@{#339039}

--- chrome/android/javatests/src/org/chromium/chrome/browser/TabsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/TabsTest.java
@@ -155,7 +155,7 @@ public class TabsTest extends ChromeTabbedActivityTestBase {
      * crbug.com/510659
      * @MediumTest
      */
-    @FlakyTest
+    @DisabledTest
     public void testAlertDialogDoesNotChangeActiveModel() throws InterruptedException {
         newIncognitoTabFromMenu();
         loadUrl(UrlUtils.getIsolatedTestFileUrl(TEST_FILE_PATH));
