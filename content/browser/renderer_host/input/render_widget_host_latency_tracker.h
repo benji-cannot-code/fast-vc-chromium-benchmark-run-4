de6cd9d054f30f15b906ba8df51fc0351b73d928
brianderson <brianderson@chromium.org>
2015-07-16 13:46:40 -0700

    android: Use a hard coded BeginFrame deadline
    The previous estimation logic measures the time from when the
    Browser receives the Renderer swap to when the Browser swaps,
    which may include idle time when the Renderer is in a high
    latency mode.
    
    This often prevented us from detecting that we can recover
    that latency.
    
    The correct estimation logic will look completely different for
    Surfaces vs. Android's current path; so the plan is to use a
    hard coded for now and use estimation logic again once
    Surfaces come to Android.
    
    BUG=510623
    
    Review URL: https://codereview.chromium.org/1244443003
    
    Cr-Commit-Position: refs/heads/master@{#339114}

--- content/browser/renderer_host/input/render_widget_host_latency_tracker.h
+++ content/browser/renderer_host/input/render_widget_host_latency_tracker.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
-#include "cc/base/rolling_time_delta_history.h"
 #include "content/browser/renderer_host/event_with_latency_info.h"
 #include "content/common/content_export.h"
 #include "ui/events/latency_info.h"
@@ -50,10 +49,6 @@ class CONTENT_EXPORT RenderWidgetHostLatencyTracker {
   // Called when the RenderWidgetHost receives a swap update from the GPU.
   void OnFrameSwapped(const ui::LatencyInfo& latency);
 
-  // Produces an estimate of the time between browser composite and GPU swap,
-  // as informed by historical latency values.
-  base::TimeDelta GetEstimatedBrowserCompositeTime() const;
-
   // WebInputEvent coordinates are in DPIs, while LatencyInfo expects
   // coordinates in device pixels.
   void set_device_scale_factor(float device_scale_factor) {
@@ -69,7 +64,6 @@ class CONTENT_EXPORT RenderWidgetHostLatencyTracker {
   int64 latency_component_id_;
   float device_scale_factor_;
   bool has_seent_first_gesture_scroll_update_;
-  cc::RollingTimeDeltaHistory browser_composite_latency_history_;
 
   DISALLOW_COPY_AND_ASSIGN(RenderWidgetHostLatencyTracker);
 };
