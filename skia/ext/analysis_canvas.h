9e0a0bd1a183d4192114f918cb100fbb0c0847ab
reed <reed@google.com>
2015-07-15 21:03:12 -0700

    transition to new Constraints for drawBitmap/ImageRect
    will need https://codereview.chromium.org/1233333002/# to land first
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1241923004
    
    Cr-Commit-Position: refs/heads/master@{#338984}

--- skia/ext/analysis_canvas.h
+++ skia/ext/analysis_canvas.h
@@ -48,7 +48,7 @@ class SK_API AnalysisCanvas : public SkCanvas, public SkPicture::AbortCallback {
                         const SkRect* src,
                         const SkRect& dst,
                         const SkPaint* paint,
-                        DrawBitmapRectFlags flags) override;
+                        SrcRectConstraint) override;
   void onDrawBitmapNine(const SkBitmap& bitmap,
                         const SkIRect& center,
                         const SkRect& dst,
@@ -58,9 +58,10 @@ class SK_API AnalysisCanvas : public SkCanvas, public SkPicture::AbortCallback {
                     SkScalar top,
                     const SkPaint* paint = NULL) override;
   void onDrawImageRect(const SkImage*,
-                        const SkRect* src,
-                        const SkRect& dst,
-                        const SkPaint* paint) override;
+                       const SkRect* src,
+                       const SkRect& dst,
+                       const SkPaint* paint,
+                       SrcRectConstraint) override;
   void onDrawSprite(const SkBitmap&,
                     int left,
                     int top,
