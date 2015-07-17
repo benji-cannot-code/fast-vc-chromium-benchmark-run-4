77f4e86dccc33edb593ccf8b038542e36202c282
hongchan@chromium.org <hongchan@chromium.org>
2015-07-17 19:24:57 +0000

    Adds suspend() and resume() feature in OfflineAudioContext to support the synchronous graph manipulation with the render block precision (k-rate) in the non-realtime audio rendering.
    The benefit of being able to suspend/resume the context with the render block
    precision is:
    
    1) The audio graph can be modified in a time-accurate way, independent of the
       hardware. Without this, setTimeout, completion events, or state change events
       are needed to manipulate the graph, and the results depend on when the events
       are fired and on how fast the hardware is.
    
    2) Makes an OfflineAudioContext more symmetrical to the AudioContext, which
       already supports suspend/resume. (There are minor difference required by the
       difference between offline and online contexts.)
    
    This feature also can be used in Blink layout tests to verify the behavior of
    audio rendering. With this feature in the implementation, several flaky web
    audio layout tests can be fixed.
    
    # Editor's draft spec PR:
    http://webaudio.github.io/web-audio-api/#the-offlineaudiocontext-interface
    
    # Web Audio API issue tracker entry:
    https://github.com/WebAudio/web-audio-api/issues/302#issuecomment-106101885
    
    
    BUG=497933
    TEST=
    webaudio/offlineaudiocontext-suspend-resume-basic.html
    webaudio/offlineaudiocontext-suspend-resume-eventhandler.html
    webaudio/offlineaudiocontext-suspend-resume-graph-manipulation.html
    webaudio/offlineaudiocontext-suspend-resume-promise.html
    webaudio/offlineaudiocontext-suspend-resume-sequence.html
    
    Review URL: https://codereview.chromium.org/1140723003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199129 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webaudio/DeferredTaskHandler.cpp
+++ third_party/WebKit/Source/modules/webaudio/DeferredTaskHandler.cpp
@@ -42,6 +42,12 @@ void DeferredTaskHandler::lock()
     m_contextGraphMutex.lock();
 }
 
+void DeferredTaskHandler::offlineContextLock()
+{
+    ASSERT(!isMainThread());
+    m_contextGraphMutex.lock();
+}
+
 bool DeferredTaskHandler::tryLock()
 {
     // Try to catch cases of using try lock on main thread
