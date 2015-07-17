21e887ff5bf6ee4a9fe9c839bc37dd44b6ccbf59
maniscalco <maniscalco@chromium.org>
2015-07-17 13:21:38 -0700

    [Sync] Don't call HasUnsyncedItems unless IsSyncActive
    Change UpdatedProgressMarkerChecker's exit condition criteria to include
    ProfileSyncService::IsSyncActive to avoid a NOTREACHED in
    ProfileSyncService::HasUnsyncedItems
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1236363006
    
    Cr-Commit-Position: refs/heads/master@{#339317}

--- chrome/browser/sync/test/integration/updated_progress_marker_checker.cc
+++ chrome/browser/sync/test/integration/updated_progress_marker_checker.cc
@@ -39,7 +39,7 @@ bool UpdatedProgressMarkerChecker::IsExitConditionSatisfied() {
   const syncer::sessions::SyncSessionSnapshot& snap =
       service()->GetLastSessionSnapshot();
   return snap.model_neutral_state().num_successful_commits == 0 &&
-         !service()->HasUnsyncedItems();
+         service()->IsSyncActive() && !service()->HasUnsyncedItems();
 }
 
 std::string UpdatedProgressMarkerChecker::GetDebugMessage() const {
