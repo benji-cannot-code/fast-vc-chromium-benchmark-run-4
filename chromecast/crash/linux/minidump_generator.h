9c52730342f4a137cb198e9017a49263570d5051
dcaiafa <dcaiafa@chromium.org>
2015-07-16 17:09:55 -0700

    Revert of Replace system IO calls in chromecast/crash with Chrome utilities. (patchset #9 id:160001 of https://codereview.chromium.org/1227963002/)
    Reason for revert:
    Breaks CastCrashReporterClientTest.EndToEndTestOnIORestrictedThread:
    
    https://build.chromium.org/p/chromium.linux/builders/Cast%20Linux/builds/2709
    
    Original issue's description:
    > Replace system IO calls in chromecast/crash with Chrome utilities.
    >
    > System IO calls had been used to avoid thread IO restrictions in the
    > crash handler. This change replaces each of these calls with the more
    > platform-independent, stable Chrome IO utilities from ::base. Thread
    > restrictions are disabled in crash_util.cc.
    >
    > BUG= b/22329428
    >
    > Committed: https://crrev.com/4927df40d0342522fc268f884835b43f9004549e
    > Cr-Commit-Position: refs/heads/master@{#339161}
    
    TBR=wzhong@chromium.org,bcf@google.com,gunsch@chromium.org,gfhuang@chromium.org,slan@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG= b/22329428
    
    Review URL: https://codereview.chromium.org/1239083005
    
    Cr-Commit-Position: refs/heads/master@{#339183}

--- chromecast/crash/linux/minidump_generator.h
+++ chromecast/crash/linux/minidump_generator.h
@@ -10,15 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromecast {
 
-// An interface to generate a minidump at a given filepath.
 class MinidumpGenerator {
  public:
   virtual ~MinidumpGenerator() {}
 
-  // Generates a minidump file at |minidump_path|. This method should only be
-  // called on a thread without IO restrictions, as non-trivial implementations
-  // will almost certainly require IO permissions. Returns true if minidump was
-  // successfully generated.
+  // Interface to generate a minidump file in given path.
+  // This is called inside MinidumpWriter::DoWorkLocked().
   virtual bool Generate(const std::string& minidump_path) = 0;
 };
 
