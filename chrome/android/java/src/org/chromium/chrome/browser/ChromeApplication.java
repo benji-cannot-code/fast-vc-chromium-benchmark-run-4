c91527ade0a1052c82dc14d16187b096ba0ed59f
nyquist <nyquist@chromium.org>
2015-07-17 09:59:52 -0700

    Add support for overriding AccountManagerDelegate
    Currently the SystemAccountManagerDelegate is always created by the
    AccountManagerHelper. This changes it so that it is created by the
    application context instead.
    
    BUG=509826
    
    Review URL: https://codereview.chromium.org/1242043006
    
    Cr-Commit-Position: refs/heads/master@{#339273}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
@@ -94,6 +94,9 @@ import org.chromium.content.browser.ChildProcessLauncher;
 import org.chromium.content.browser.ContentViewStatics;
 import org.chromium.content.browser.DownloadController;
 import org.chromium.printing.PrintingController;
+import org.chromium.sync.signin.AccountManagerDelegate;
+import org.chromium.sync.signin.AccountManagerHelper;
+import org.chromium.sync.signin.SystemAccountManagerDelegate;
 import org.chromium.ui.UiUtils;
 import org.chromium.ui.base.ActivityWindowAndroid;
 import org.chromium.ui.base.ResourceBundle;
@@ -206,6 +209,11 @@ public class ChromeApplication extends ContentApplication {
                 return activity != null && MultiWindowUtils.getInstance().isMultiWindow(activity);
             }
         });
+
+        // Set the default AccountManagerDelegate to ensure it is always used when the instance
+        // of the AccountManagerHelper is created. Must be done before AccountMangerHelper.get(...)
+        // is called.
+        AccountManagerHelper.setDefaultAccountManagerDelegate(createAccountManagerDelegate());
 
         // Set the unique identification generator for invalidations.  The
         // invalidations system can start and attempt to fetch the client ID
@@ -782,6 +790,14 @@ public class ChromeApplication extends ContentApplication {
      */
     public RevenueStats createRevenueStatsInstance() {
         return new RevenueStats();
+    }
+
+    /**
+     * Creates a new {@link AccountManagerDelegate}.
+     * @return the created {@link AccountManagerDelegate}.
+     */
+    public AccountManagerDelegate createAccountManagerDelegate() {
+        return new SystemAccountManagerDelegate(this);
     }
 
     /**
