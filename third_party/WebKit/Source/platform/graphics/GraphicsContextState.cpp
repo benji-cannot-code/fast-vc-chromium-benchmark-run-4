0cde5ac2378315b8b8a3064a087bf02cf9a12f56
fs@opera.com <fs@opera.com>
2015-07-17 08:40:25 +0000

    Drop WebCoreInterpolationQualityToSkFilterQuality
    Except for GraphicsContextState, this function is only used in one
    location - SVGPaintContext. In that one instance it's hard-wired to 'low',
    so make that even more obvious.
    Move the function from SkiaUtils.h to GraphicsContextState.cpp and rename.
    
    Review URL: https://codereview.chromium.org/1236363003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199092 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/GraphicsContextState.cpp
+++ third_party/WebKit/Source/platform/graphics/GraphicsContextState.cpp
@@ -8,6 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+static inline SkFilterQuality filterQualityForPaint(InterpolationQuality quality)
+{
+    // The filter quality "selected" here will primarily be used when painting a
+    // primitive using one of the SkPaints below. For the most part this will
+    // not affect things that are part of the Image class hierarchy (which use
+    // the unmodified m_interpolationQuality.)
+    return quality != InterpolationNone ? kLow_SkFilterQuality : kNone_SkFilterQuality;
+}
+
 GraphicsContextState::GraphicsContextState()
     : m_strokeColor(Color::black)
     , m_fillColor(Color::black)
@@ -22,10 +31,10 @@ GraphicsContextState::GraphicsContextState()
     m_strokePaint.setStrokeCap(SkPaint::kDefault_Cap);
     m_strokePaint.setStrokeJoin(SkPaint::kDefault_Join);
     m_strokePaint.setStrokeMiter(SkFloatToScalar(m_strokeData.miterLimit()));
-    m_strokePaint.setFilterQuality(WebCoreInterpolationQualityToSkFilterQuality(m_interpolationQuality));
+    m_strokePaint.setFilterQuality(filterQualityForPaint(m_interpolationQuality));
     m_strokePaint.setAntiAlias(m_shouldAntialias);
     m_fillPaint.setColor(m_fillColor.rgb());
-    m_fillPaint.setFilterQuality(WebCoreInterpolationQualityToSkFilterQuality(m_interpolationQuality));
+    m_fillPaint.setFilterQuality(filterQualityForPaint(m_interpolationQuality));
     m_fillPaint.setAntiAlias(m_shouldAntialias);
 }
 
@@ -156,8 +165,8 @@ void GraphicsContextState::setColorFilter(PassRefPtr<SkColorFilter> colorFilter)
 void GraphicsContextState::setInterpolationQuality(InterpolationQuality quality)
 {
     m_interpolationQuality = quality;
-    m_strokePaint.setFilterQuality(WebCoreInterpolationQualityToSkFilterQuality(quality));
-    m_fillPaint.setFilterQuality(WebCoreInterpolationQualityToSkFilterQuality(quality));
+    m_strokePaint.setFilterQuality(filterQualityForPaint(quality));
+    m_fillPaint.setFilterQuality(filterQualityForPaint(quality));
 }
 
 void GraphicsContextState::setShouldAntialias(bool shouldAntialias)
