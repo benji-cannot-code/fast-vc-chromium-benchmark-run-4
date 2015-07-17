2a6db9c3eed1b2cee4ad65c8a9288069134dfb02
gangwu <gangwu@chromium.org>
2015-07-17 09:20:08 -0700

    Remove using finch experiment to control database size, and now permanently increase all the sync client database size to 32k.
    Increase corruption_testing::kNumEntriesRequiredForCorruption
    to 12000 since after database page size become 32k, 2000 trials
    cannot find corrupt the database.
    
    BUG=468363,510660
    
    Review URL: https://codereview.chromium.org/1241683004
    
    Cr-Commit-Position: refs/heads/master@{#339265}

--- sync/syncable/directory_backing_store.cc
+++ sync/syncable/directory_backing_store.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base64.h"
 #include "base/logging.h"
-#include "base/metrics/field_trial.h"
 #include "base/rand_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/thread_task_runner_handle.h"
@@ -178,12 +177,6 @@ namespace {
 // modifies all the columns in the entry table.
 static const string::size_type kUpdateStatementBufferSize = 2048;
 
-bool IsSyncBackingDatabase32KEnabled() {
-  const std::string group_name =
-      base::FieldTrialList::FindFullName("SyncBackingDatabase32K");
-  return group_name == "Enabled";
-}
-
 void OnSqliteError(const base::Closure& catastrophic_error_handler,
                    int err,
                    sql::Statement* statement) {
@@ -237,7 +230,7 @@ bool SaveEntryToDB(sql::Statement* save_statement, const EntryKernel& entry) {
 
 DirectoryBackingStore::DirectoryBackingStore(const string& dir_name)
     : dir_name_(dir_name),
-      database_page_size_(IsSyncBackingDatabase32KEnabled() ? 32768 : 4096),
+      database_page_size_(32768),
       needs_column_refresh_(false) {
   DCHECK(base::ThreadTaskRunnerHandle::IsSet());
   ResetAndCreateConnection();
@@ -246,7 +239,7 @@ DirectoryBackingStore::DirectoryBackingStore(const string& dir_name)
 DirectoryBackingStore::DirectoryBackingStore(const string& dir_name,
                                              sql::Connection* db)
     : dir_name_(dir_name),
-      database_page_size_(IsSyncBackingDatabase32KEnabled() ? 32768 : 4096),
+      database_page_size_(32768),
       db_(db),
       needs_column_refresh_(false) {
   DCHECK(base::ThreadTaskRunnerHandle::IsSet());
@@ -389,8 +382,7 @@ bool DirectoryBackingStore::OpenInMemory() {
 
 bool DirectoryBackingStore::InitializeTables() {
   int page_size = 0;
-  if (IsSyncBackingDatabase32KEnabled() && GetDatabasePageSize(&page_size) &&
-      page_size == 4096) {
+  if (GetDatabasePageSize(&page_size) && page_size == 4096) {
     IncreasePageSizeTo32K();
   }
   sql::Transaction transaction(db_.get());
