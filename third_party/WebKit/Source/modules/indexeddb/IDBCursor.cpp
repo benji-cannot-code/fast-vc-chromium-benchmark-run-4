9cc6877ff7a5303f76a948ef54ffdf684b200b3b
jsbell@chromium.org <jsbell@chromium.org>
2015-07-17 23:47:08 +0000

    IndexedDB: Make custom IDBCursor->IDBRequest wrapper linking more robust
    An IDBCursor's associated IDBRequest should be re-used each time the
    cursor advances. To keep the request's wrapper alive a hidden
    reference is set on the cursor's wrapper, with custom bindings code.
    
    Move that code to a custom toV8() overload rather than repeating it in
    all the places that might call toV8(), to simplify the code and make
    it less fragile.
    
    R=haraken@chromium.org
    TEST=storage/indexeddb/cursor-request-cycle.html
    TEST=storage/indexeddb/key-cursor-request-cycle.html
    
    Review URL: https://codereview.chromium.org/1240703004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199141 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/indexeddb/IDBCursor.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IDBCursor.cpp
@@ -29,8 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/core/v8/ExceptionState.h"
 #include "bindings/core/v8/ScriptState.h"
+#include "bindings/core/v8/V8HiddenValue.h"
 #include "bindings/modules/v8/ToV8ForModules.h"
 #include "bindings/modules/v8/V8BindingForModules.h"
+#include "bindings/modules/v8/V8IDBRequest.h"
 #include "core/dom/ExceptionCode.h"
 #include "core/inspector/ScriptCallStack.h"
 #include "modules/IndexedDBNames.h"
@@ -82,6 +84,16 @@ DEFINE_TRACE(IDBCursor)
     visitor->trace(m_transaction);
     visitor->trace(m_key);
     visitor->trace(m_primaryKey);
+}
+
+// Keep the request's wrapper alive as long as the cursor's wrapper is alive,
+// so that the same script object is seen each time the cursor is used.
+v8::Local<v8::Object> IDBCursor::associateWithWrapper(v8::Isolate* isolate, const WrapperTypeInfo* wrapperType, v8::Local<v8::Object> wrapper)
+{
+    wrapper = ScriptWrappable::associateWithWrapper(isolate, wrapperType, wrapper);
+    if (!wrapper.IsEmpty())
+        V8HiddenValue::setHiddenValue(isolate, wrapper, V8HiddenValue::idbCursorRequest(isolate), toV8(m_request.get(), wrapper, isolate));
+    return wrapper;
 }
 
 IDBRequest* IDBCursor::update(ScriptState* scriptState, const ScriptValue& value, ExceptionState& exceptionState)
