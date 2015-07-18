3cb713b0d6e40446c58608112503e0ecbc73aa17
wfh <wfh@chromium.org>
2015-07-17 18:44:22 -0700

    Disable DEP checking tests on 64-bit since it's always enabled.
    Windows returns ERROR_INVALID_PARAMETER if you call this on 64-bit
    on Windows 8.1 and above.
    
    BUG=508742
    
    Review URL: https://codereview.chromium.org/1235183003
    
    Cr-Commit-Position: refs/heads/master@{#339377}

--- sandbox/win/src/process_mitigations_test.cc
+++ sandbox/win/src/process_mitigations_test.cc
@@ -32,7 +32,7 @@ typedef BOOL (WINAPI *GetProcessMitigationPolicyFunction)(
 GetProcessMitigationPolicyFunction get_process_mitigation_policy;
 
 bool CheckWin8DepPolicy() {
-  PROCESS_MITIGATION_DEP_POLICY policy;
+  PROCESS_MITIGATION_DEP_POLICY policy = {};
   if (!get_process_mitigation_policy(::GetCurrentProcess(), ProcessDEPPolicy,
                                      &policy, sizeof(policy))) {
     return false;
@@ -41,7 +41,7 @@ bool CheckWin8DepPolicy() {
 }
 
 bool CheckWin8AslrPolicy() {
-  PROCESS_MITIGATION_ASLR_POLICY policy;
+  PROCESS_MITIGATION_ASLR_POLICY policy = {};
   if (!get_process_mitigation_policy(::GetCurrentProcess(), ProcessASLRPolicy,
                                      &policy, sizeof(policy))) {
      return false;
@@ -50,7 +50,7 @@ bool CheckWin8AslrPolicy() {
 }
 
 bool CheckWin8StrictHandlePolicy() {
-  PROCESS_MITIGATION_STRICT_HANDLE_CHECK_POLICY policy;
+  PROCESS_MITIGATION_STRICT_HANDLE_CHECK_POLICY policy = {};
   if (!get_process_mitigation_policy(::GetCurrentProcess(),
                                      ProcessStrictHandleCheckPolicy,
                                      &policy, sizeof(policy))) {
@@ -61,7 +61,7 @@ bool CheckWin8StrictHandlePolicy() {
 }
 
 bool CheckWin8Win32CallPolicy() {
-  PROCESS_MITIGATION_SYSTEM_CALL_DISABLE_POLICY policy;
+  PROCESS_MITIGATION_SYSTEM_CALL_DISABLE_POLICY policy = {};
   if (!get_process_mitigation_policy(::GetCurrentProcess(),
                                      ProcessSystemCallDisablePolicy,
                                      &policy, sizeof(policy))) {
@@ -71,7 +71,7 @@ bool CheckWin8Win32CallPolicy() {
 }
 
 bool CheckWin8DllExtensionPolicy() {
-  PROCESS_MITIGATION_EXTENSION_POINT_DISABLE_POLICY policy;
+  PROCESS_MITIGATION_EXTENSION_POINT_DISABLE_POLICY policy = {};
   if (!get_process_mitigation_policy(::GetCurrentProcess(),
                                      ProcessExtensionPointDisablePolicy,
                                      &policy, sizeof(policy))) {
@@ -92,8 +92,10 @@ SBOX_TESTS_COMMAND int CheckWin8(int argc, wchar_t **argv) {
   if (!get_process_mitigation_policy)
     return SBOX_TEST_NOT_FOUND;
 
+#if !defined(_WIN64)  // DEP is always enabled on 64-bit.
   if (!CheckWin8DepPolicy())
     return SBOX_TEST_FIRST_ERROR;
+#endif
 
 #if defined(NDEBUG)  // ASLR cannot be forced in debug builds.
   if (!CheckWin8AslrPolicy())
