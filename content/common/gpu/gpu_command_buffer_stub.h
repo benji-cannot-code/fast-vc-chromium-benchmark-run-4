10c8d72f801ffe676d20890f567de869870c39c2
boliu <boliu@chromium.org>
2015-07-17 19:26:53 -0700

    Add Push/PullTextureUpdates to CommandBufferStub
    Only call them if UsesSync is true to avoid perf impact on the
    MakeCurrent when they are not needed.
    
    BUG=509702
    
    Review URL: https://codereview.chromium.org/1228703005
    
    Cr-Commit-Position: refs/heads/master@{#339382}

--- content/common/gpu/gpu_command_buffer_stub.h
+++ content/common/gpu/gpu_command_buffer_stub.h
@@ -202,7 +202,7 @@ class GpuCommandBufferStub
 
   void OnRetireSyncPoint(uint32 sync_point);
   bool OnWaitSyncPoint(uint32 sync_point);
-  void OnSyncPointRetired();
+  void OnWaitSyncPointCompleted(uint32 sync_point);
   void OnSignalSyncPoint(uint32 sync_point, uint32 id);
   void OnSignalSyncPointAck(uint32 id);
   void OnSignalQuery(uint32 query, uint32 id);
@@ -236,6 +236,7 @@ class GpuCommandBufferStub
 
   bool CheckContextLost();
   void CheckCompleteWaits();
+  void PullTextureUpdates(uint32 sync_point);
 
   // The lifetime of objects of this class is managed by a GpuChannel. The
   // GpuChannels destroy all the GpuCommandBufferStubs that they own when they
