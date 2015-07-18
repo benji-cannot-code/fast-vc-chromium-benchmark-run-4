88a86d067ae2569c60fa11a1135577c9ad4487ec
simonhatch <simonhatch@chromium.org>
2015-07-17 20:51:08 -0700

    Slow Reports - Add support for UMA triggers in BackgroundTracingManager.
    This CL adds support for triggering when new samples are recorded on histograms. At the moment, this only works if the histogram is in the same process, but we'll follow up in a subsequent CL with cross-process support.
    
    Needs to land first: https://codereview.chromium.org/1227163006/
    
    BUG=504889
    
    Review URL: https://codereview.chromium.org/1232453002
    
    Cr-Commit-Position: refs/heads/master@{#339386}

--- content/public/browser/background_tracing_preemptive_config.h
+++ content/public/browser/background_tracing_preemptive_config.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_BROWSER_BACKGROUND_TRACING_PREEMPTIVE_CONFIG_H_
 #define CONTENT_PUBLIC_BROWSER_BACKGROUND_TRACING_PREEMPTIVE_CONFIG_H_
 
+#include "base/metrics/histogram_base.h"
 #include "content/public/browser/background_tracing_config.h"
 
 namespace content {
@@ -25,8 +26,8 @@ struct CONTENT_EXPORT BackgroundTracingPreemptiveConfig
     MONITOR_AND_DUMP_WHEN_BROWSER_STARTUP_COMPLETE,
   };
   struct HistogramTriggerInfo {
-    std::string histogram_name_to_trigger_on;
-    int histogram_bin_to_trigger_on;
+    std::string histogram_name;
+    base::HistogramBase::Sample histogram_value;
   };
   struct NamedTriggerInfo {
     std::string trigger_name;
