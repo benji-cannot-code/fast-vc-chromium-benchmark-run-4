72fcc2b5feed0d0baf6606440085f8a5d17e42ff
sievers <sievers@chromium.org>
2015-07-17 18:14:25 -0700

    gpu: Use shader cache on ES3
    The ES3 core specs supports gl(Get)ProgramBinary().
    BUG=510172
    
    Review URL: https://codereview.chromium.org/1223393003
    
    Cr-Commit-Position: refs/heads/master@{#339369}

--- content/common/gpu/gpu_channel_manager.cc
+++ content/common/gpu/gpu_channel_manager.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/sync_point_manager.h"
 #include "ipc/message_filter.h"
 #include "ui/gl/gl_bindings.h"
+#include "ui/gl/gl_gl_api_implementation.h"
 #include "ui/gl/gl_share_group.h"
 #if defined(USE_OZONE)
 #include "ui/ozone/public/gpu_platform_support.h"
@@ -69,7 +70,8 @@ GpuChannelManager::~GpuChannelManager() {
 gpu::gles2::ProgramCache* GpuChannelManager::program_cache() {
   if (!program_cache_.get() &&
       (gfx::g_driver_gl.ext.b_GL_ARB_get_program_binary ||
-       gfx::g_driver_gl.ext.b_GL_OES_get_program_binary) &&
+       gfx::g_driver_gl.ext.b_GL_OES_get_program_binary ||
+       gfx::GetGLVersionInfo()->is_es3) &&
       !base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kDisableGpuProgramCache)) {
     program_cache_.reset(new gpu::gles2::MemoryProgramCache());
