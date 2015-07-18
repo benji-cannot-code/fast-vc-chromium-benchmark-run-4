5d758fa608a010ffff961dba320d321ab30eccfd
dominickn <dominickn@chromium.org>
2015-07-17 17:37:51 -0700

    Convert ShortcutHelper.SOURCE_UNKNOWN to the new ShortcutSource generated enum.
    The new enum was added in https://codereview.chromium.org/1241823002/
    
    BUG=486177
    
    Review URL: https://codereview.chromium.org/1225333003
    
    Cr-Commit-Position: refs/heads/master@{#339366}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappInfoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappInfoTest.java
@@ -11,6 +11,7 @@ import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.ShortcutHelper;
+import org.chromium.chrome.browser.ShortcutSource;
 import org.chromium.content_public.common.ScreenOrientationValues;
 
 /**
@@ -25,7 +26,7 @@ public class WebappInfoTest extends InstrumentationTestCase {
         String url = "about:blank";
 
         WebappInfo info = WebappInfo.create(id, url,
-                null, title, ScreenOrientationValues.DEFAULT, ShortcutHelper.SOURCE_UNKNOWN);
+                null, title, ScreenOrientationValues.DEFAULT, ShortcutSource.UNKNOWN);
         assertNotNull(info);
     }
 
@@ -37,7 +38,7 @@ public class WebappInfoTest extends InstrumentationTestCase {
         String url = "http://google.com";
 
         WebappInfo info = WebappInfo.create(id, url,
-                null, title, ScreenOrientationValues.DEFAULT, ShortcutHelper.SOURCE_UNKNOWN);
+                null, title, ScreenOrientationValues.DEFAULT, ShortcutSource.UNKNOWN);
         assertNotNull(info);
     }
 
@@ -65,9 +66,9 @@ public class WebappInfoTest extends InstrumentationTestCase {
         String url = "http://money.cnn.com";
 
         WebappInfo info = WebappInfo.create(id, url,
-                null, title, ScreenOrientationValues.DEFAULT, ShortcutHelper.SOURCE_UNKNOWN);
+                null, title, ScreenOrientationValues.DEFAULT, ShortcutSource.UNKNOWN);
         assertNotNull(info);
         assertEquals(info.orientation(), ScreenOrientationValues.DEFAULT);
-        assertEquals(info.source(), ShortcutHelper.SOURCE_UNKNOWN);
+        assertEquals(info.source(), ShortcutSource.UNKNOWN);
     }
 }
