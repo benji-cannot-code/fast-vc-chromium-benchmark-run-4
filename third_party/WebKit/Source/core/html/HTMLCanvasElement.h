738b1620d68efe8e17a4e331306d5912f5886396
sigbjornf@opera.com <sigbjornf@opera.com>
2015-07-18 06:46:28 +0000

    Revert of Canvas.toDataURL to use SkBitmap::readPixels to avoid uninitialized memory (patchset #4 id:60001 of https://codereview.chromium.org/1234083003/)
    Reason for revert:
    ASan bots are failing on toDataURL() tests,
    
     http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20ASAN/builds/19488
     http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Oilpan%20ASAN/builds/2978
    
    Original issue's description:
    > Canvas.toDataURL to use SkBitmap::readPixels to avoid uninitialized memory
    > 
    > This change refactors toDataURL to make it read the canvas data into
    > a pre-initialized buffer to avoid accessing unitialized memory in cases
    > where a GPU readback fails silently.
    > 
    > BUG=504690
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=199131
    
    TBR=senorblanco@chromium.org,junov@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=504690
    
    Review URL: https://codereview.chromium.org/1236173005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199154 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.h
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.h
@@ -56,7 +56,6 @@ class HTMLCanvasElement;
 class Image;
 class ImageBuffer;
 class ImageBufferSurface;
-class ImageData;
 class IntSize;
 
 class CORE_EXPORT CanvasObserver : public WillBeGarbageCollectedMixin {
@@ -201,7 +200,6 @@ private:
 
     bool paintsIntoCanvasBuffer() const;
 
-    ImageData* toImageData(SourceDrawingBuffer) const;
     String toDataURLInternal(const String& mimeType, const double* quality, SourceDrawingBuffer) const;
 
     WillBeHeapHashSet<RawPtrWillBeWeakMember<CanvasObserver>> m_observers;
