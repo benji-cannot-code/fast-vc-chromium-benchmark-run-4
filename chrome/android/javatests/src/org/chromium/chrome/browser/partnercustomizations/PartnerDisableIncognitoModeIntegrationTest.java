d012d4f2225794dc0e3987698a241cf19cdd08d0
pkotwicz <pkotwicz@chromium.org>
2015-07-18 21:34:43 -0700

    Disable failing PartnerDisableIncognitoModeIntegrationTest#testEnabledParentalControlsClosesIncognitoTabs
    BUG=511688
    NOTRY=true
    TBR=boliu
    
    Review URL: https://codereview.chromium.org/1230873006
    
    Cr-Commit-Position: refs/heads/master@{#339405}

--- chrome/android/javatests/src/org/chromium/chrome/browser/partnercustomizations/PartnerDisableIncognitoModeIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/partnercustomizations/PartnerDisableIncognitoModeIntegrationTest.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import android.widget.PopupMenu;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
@@ -147,6 +148,7 @@ public class PartnerDisableIncognitoModeIntegrationTest extends
         assertIncognitoMenuItemEnabled(true);
     }
 
+    @DisabledTest
     @MediumTest
     @Feature({"DisableIncognitoMode"})
     public void testEnabledParentalControlsClosesIncognitoTabs() throws InterruptedException {
