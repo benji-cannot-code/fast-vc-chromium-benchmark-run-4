0ac8de2148cebead492d509905821892e2a69e5b
pkotwicz <pkotwicz@chromium.org>
2015-07-19 13:40:53 -0700

    Properly disable failing AwContentsClientFullScreenTest and PartnerDisableIncognitoModeIntegrationTest
    BUG=511688
    TBR=pkotwicz
    
    Review URL: https://codereview.chromium.org/1233823006
    
    Cr-Commit-Position: refs/heads/master@{#339411}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientFullScreenTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientFullScreenTest.java
@@ -91,16 +91,22 @@ public class AwContentsClientFullScreenTest extends AwTestBase {
         doTestOnShowAndHideCustomViewWithCallback(VIDEO_INSIDE_DIV_TEST_URL);
     }
 
-    @DisabledTest
+    /*
     @MediumTest
     @Feature({"AndroidWebView"})
+    crbug.com/511688
+    */
+    @DisabledTest
     public void testOnShowAndHideCustomViewWithCallback_video() throws Throwable {
         doTestOnShowAndHideCustomViewWithCallback(VIDEO_TEST_URL);
     }
 
-    @DisabledTest
+    /*
     @MediumTest
     @Feature({"AndroidWebView"})
+    crbug.com/511688
+    */
+    @DisabledTest
     public void testOnShowAndHideCustomViewWithCallback_videoInsideDiv() throws Throwable {
         doTestOnShowAndHideCustomViewWithCallback(VIDEO_INSIDE_DIV_TEST_URL);
     }
@@ -120,9 +126,12 @@ public class AwContentsClientFullScreenTest extends AwTestBase {
         doTestOnShowAndHideCustomViewWithJavascript(VIDEO_TEST_URL);
     }
 
-    @DisabledTest
+    /*
     @MediumTest
     @Feature({"AndroidWebView"})
+    crbug.com/511688
+    */
+    @DisabledTest
     public void testOnShowAndHideCustomViewWithJavascript_videoInsideDiv()
             throws Throwable {
         doTestOnShowAndHideCustomViewWithJavascript(VIDEO_INSIDE_DIV_TEST_URL);
@@ -137,16 +146,22 @@ public class AwContentsClientFullScreenTest extends AwTestBase {
         });
     }
 
-    @DisabledTest
+    /*
     @MediumTest
     @Feature({"AndroidWebView"})
+    crbug.com/511688
+    */
+    @DisabledTest
     public void testOnShowAndHideCustomViewWithBackKey_video() throws Throwable {
         doTestOnShowAndHideCustomViewWithBackKey(VIDEO_TEST_URL);
     }
 
-    @DisabledTest
+    /*
     @MediumTest
     @Feature({"AndroidWebView"})
+    crbug.com/511688
+    */
+    @DisabledTest
     public void testOnShowAndHideCustomViewWithBackKey_videoInsideDiv()
             throws Throwable {
         doTestOnShowAndHideCustomViewWithBackKey(VIDEO_INSIDE_DIV_TEST_URL);
@@ -393,9 +408,12 @@ public class AwContentsClientFullScreenTest extends AwTestBase {
         assertKeepScreenOnActive(mTestContainerView, false);
     }
 
-    @DisabledTest
+    /*
     @MediumTest
     @Feature({"AndroidWebView"})
+    crbug.com/511688
+    */
+    @DisabledTest
     public void testPowerSaveBlockerIsTransferredToEmbedded()
             throws Throwable {
         // Enter fullscreen.
