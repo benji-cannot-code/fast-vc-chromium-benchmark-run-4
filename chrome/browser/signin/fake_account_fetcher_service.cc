d2aa4045492faa3f9b9e6170c972ac101fd814f0
antrim <antrim@chromium.org>
2015-07-20 06:33:00 -0700

    Revert of Split AccountInfoFetcher into a ChildAccountInfoFetcher. (patchset #9 id:160001 of https://codereview.chromium.org/1224643007/)
    Reason for revert:
    It seems that this CL reliably breaks AccountTrackerServiceTest.Persistence on Win 7 Tests x64.
    
    See
    
    https://build.chromium.org/p/chromium.win/builders/Win%207%20Tests%20x64%20%281%29/builds/3647
    
    Original issue's description:
    > Split AccountInfoFetcher into a ChildAccountInfoFetcher.
    >
    > This neither has the Android piece nor supports invalidations which will come
    > in a follow up cl for making it easier to track.
    >
    > The reason for having a single fetcher is invalidations will only be provided
    > for the signed in account and we only support a single account for children anyway.
    >
    > Based on http://crrev.com/1223813011/
    >
    > BUG=508054
    >
    > Committed: https://crrev.com/9aed089d3d94463605b1a20a6f857fe158922263
    > Cr-Commit-Position: refs/heads/master@{#339434}
    
    TBR=rogerta@chromium.org,anthonyvd@chromium.org,bauerb@chromium.org,knn@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=508054
    
    Review URL: https://codereview.chromium.org/1246633003
    
    Cr-Commit-Position: refs/heads/master@{#339443}

--- chrome/browser/signin/fake_account_fetcher_service.cc
+++ chrome/browser/signin/fake_account_fetcher_service.cc
@@ -33,11 +33,6 @@ void FakeAccountFetcherService::StartFetchingUserInfo(
   // In tests, don't do actual network fetch.
 }
 
-void FakeAccountFetcherService::StartFetchingChildInfo(
-    const std::string& account_id) {
-  // In tests, don't do actual network fetch.
-}
-
 void FakeAccountFetcherService::FakeUserInfoFetchSuccess(
     const std::string& email,
     const std::string& gaia,
@@ -54,9 +49,11 @@ void FakeAccountFetcherService::FakeUserInfoFetchSuccess(
   user_info.SetString("given_name", given_name);
   user_info.SetString("locale", locale);
   user_info.SetString("picture", picture_url);
+  std::vector<std::string> service_flags;
   account_tracker_service()->SetAccountStateFromUserInfo(
       account_tracker_service()->PickAccountIdForAccount(gaia, email),
-      &user_info);
+      &user_info,
+      &service_flags);
 }
 
 void FakeAccountFetcherService::SendRefreshTokenAnnotationRequest(
--- components/signin.gypi
+++ components/signin.gypi
@@ -58,8 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'signin/core/browser/account_reconcilor.h',
         'signin/core/browser/account_tracker_service.cc',
         'signin/core/browser/account_tracker_service.h',
-        'signin/core/browser/child_account_info_fetcher.cc',
-        'signin/core/browser/child_account_info_fetcher.h',
         'signin/core/browser/device_activity_fetcher.cc',
         'signin/core/browser/device_activity_fetcher.h',
         'signin/core/browser/gaia_cookie_manager_service.cc',
