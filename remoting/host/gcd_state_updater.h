9529de10b4deb90e9254033cac6de7a11fbf96b0
jrw <jrw@chromium.org>
2015-07-20 16:58:53 -0700

    Simplified GcdRestClient.
    Now only one outstanding request is allowed at any given time.
    
    BUG=503804
    
    Review URL: https://codereview.chromium.org/1230113016
    
    Cr-Commit-Position: refs/heads/master@{#339567}

--- remoting/host/gcd_state_updater.h
+++ remoting/host/gcd_state_updater.h
@@ -51,7 +51,7 @@ class GcdStateUpdater : public SignalStrategy::Listener {
   void OnSignalStrategyStateChange(SignalStrategy::State state) override;
   bool OnSignalStrategyIncomingStanza(const buzz::XmlElement* stanza) override;
 
-  void OnPatchStateStatus(GcdRestClient::Status status);
+  void OnPatchStateResult(GcdRestClient::Result result);
   void MaybeSendStateUpdate();
 
   base::Closure on_update_successful_callback_;
@@ -60,7 +60,6 @@ class GcdStateUpdater : public SignalStrategy::Listener {
   scoped_ptr<GcdRestClient> gcd_rest_client_;
   BackoffTimer timer_;
   base::ThreadChecker thread_checker_;
-  bool has_pending_state_request_ = false;
   std::string pending_request_jid_;
 
   DISALLOW_COPY_AND_ASSIGN(GcdStateUpdater);
