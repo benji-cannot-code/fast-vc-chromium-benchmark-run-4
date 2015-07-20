668656fb10074d13a0c122cb841488946311be76
simonb <simonb@chromium.org>
2015-07-20 10:22:09 -0700

    Use ConvertJavaStringToUTF8() instead of GetStringUTFChars()
    BUG=508876
    
    Review URL: https://codereview.chromium.org/1242733002
    
    Cr-Commit-Position: refs/heads/master@{#339463}

--- media/capture/video/android/video_capture_device_android.cc
+++ media/capture/video/android/video_capture_device_android.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/capture/video/android/video_capture_device_android.h"
 
 #include "base/android/jni_android.h"
+#include "base/android/jni_string.h"
 #include "base/strings/string_number_conversions.h"
 #include "jni/VideoCapture_jni.h"
 #include "media/capture/video/android/video_capture_device_factory_android.h"
@@ -172,9 +173,7 @@ void VideoCaptureDeviceAndroid::OnFrameAvailable(JNIEnv* env,
 void VideoCaptureDeviceAndroid::OnError(JNIEnv* env,
                                         jobject obj,
                                         jstring message) {
-  const char* native_string = env->GetStringUTFChars(message, JNI_FALSE);
-  SetErrorState(native_string);
-  env->ReleaseStringUTFChars(message, native_string);
+  SetErrorState(base::android::ConvertJavaStringToUTF8(env, message));
 }
 
 VideoCapturePixelFormat VideoCaptureDeviceAndroid::GetColorspace() {
