88055534ebc937cc4f04eb9352cadb95ab5a0b01
maruel <maruel@chromium.org>
2015-07-20 12:45:45 -0700

    Remove 'read_only': 1 from most .isolate files.
    These were mostly due to blind copy-paste. read_only:1 is the default so there's
    no point in specifying it.
    
    As per crbug.com/342913, only unit_tests has issue with running from read only
    files.
    
    R=jam@chromium.org
    BUG=116251
    
    Review URL: https://codereview.chromium.org/1240303002
    
    Cr-Commit-Position: refs/heads/master@{#339490}

--- tools/mb/mb.py
+++ tools/mb/mb.py
@@ -374,7 +374,6 @@ class MetaBuildWrapper(object):
           'variables': {
             'command': command,
             'files': sorted(runtime_deps + extra_files),
-            'read_only': 1,
           }
         }) + '\n')
 
