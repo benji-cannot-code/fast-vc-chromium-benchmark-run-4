394164d9dedcfdcb30d09b89d878f8b28b72a977
thakis <thakis@chromium.org>
2015-07-20 15:37:53 -0700

    clang/win: Fix another -Wunused-local-typedef warning.
    Fixes
    
    ..\..\chrome/installer/mini_installer/mini_string.h(77,36) :  error: unused typedef 'invalid_buffer_size' [-Werror,-Wunused-local-typedef]
        COMPILE_ASSERT(kCapacity != 0, invalid_buffer_size);
                                       ^
    ..\..\chrome/installer/mini_installer/mini_string.h(13,41) :  note: expanded from macro 'COMPILE_ASSERT'
        typedef CompileAssert<(bool(expr))> msg[bool(expr) ? 1 : -1]
                                            ^
    
    BUG=411648
    TBR=robertshield
    
    Review URL: https://codereview.chromium.org/1248483002
    
    Cr-Commit-Position: refs/heads/master@{#339541}

--- chrome/installer/mini_installer/mini_string.h
+++ chrome/installer/mini_installer/mini_string.h
@@ -7,11 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_INSTALLER_MINI_INSTALLER_MINI_STRING_H_
 
 #ifndef COMPILE_ASSERT
-// COMPILE_ASSERT macro borrowed from basictypes.h
-template <bool>
-struct CompileAssert {};
-#define COMPILE_ASSERT(expr, msg) \
-    typedef CompileAssert<(bool(expr))> msg[bool(expr) ? 1 : -1]
+// COMPILE_ASSERT macro borrowed from macros.h
+#define COMPILE_ASSERT(expr, msg) static_assert(expr, #msg)
 #endif
 
 namespace mini_installer {
