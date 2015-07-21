23460e811378106d5f72c6bba234f18e9968cbe4
dtseng <dtseng@chromium.org>
2015-07-21 10:25:41 -0700

    Potential fix for browser crash while ChromeVox is on.
    BUG=512000
    TEST=navigate to the launcher without and then with ChromeVox on. No crashes.
    
    Review URL: https://codereview.chromium.org/1245003002
    
    Cr-Commit-Position: refs/heads/master@{#339681}

--- chrome/browser/ui/aura/accessibility/automation_manager_aura.h
+++ chrome/browser/ui/aura/accessibility/automation_manager_aura.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/automation_internal/automation_action_adapter.h"
 #include "chrome/browser/ui/aura/accessibility/ax_tree_source_aura.h"
 #include "ui/accessibility/ax_tree_serializer.h"
+#include "ui/aura/window_observer.h"
 #include "ui/views/focus/widget_focus_manager.h"
 
 template <typename T>
@@ -27,6 +28,7 @@ class View;
 
 // Manages a tree of automation nodes.
 class AutomationManagerAura : public extensions::AutomationActionAdapter,
+                              public aura::WindowObserver,
                               public views::WidgetFocusChangeListener {
  public:
   // Get the single instance of this class.
@@ -66,6 +68,9 @@ class AutomationManagerAura : public extensions::AutomationActionAdapter,
   void SendEvent(content::BrowserContext* context,
                  views::AXAuraObjWrapper* aura_obj,
                  ui::AXEvent event_type);
+
+  // aura::WindowObserver:
+  void OnWindowDestroying(aura::Window* window) override;
 
   // views::WidgetFocusChangeListener:
   void OnNativeFocusChanged(aura::Window* focused_now) override;
