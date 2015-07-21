c78795a92403b15a2509089a4cd9b5bd1a4ebc56
schenney@chromium.org <schenney@chromium.org>
2015-07-21 16:53:26 +0000

    Fix the clearing of pointers in CompositedDeprecatedPaintLayerMapping
    A previous patch addressing the clearing of pointers on layers removed
    from a grouped mapping was nulling too many pointers, resulting in crashes
    in some cases and missing content in others. This patch makes sure we
    do not clear the pointer on somethign that appears multiple times in
    the list.
    
    R=vollick
    BUG=508062,508264
    
    Review URL: https://codereview.chromium.org/1245973002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199229 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/DeprecatedPaintLayer.cpp
+++ third_party/WebKit/Source/core/paint/DeprecatedPaintLayer.cpp
@@ -161,7 +161,6 @@ DeprecatedPaintLayer::~DeprecatedPaintLayer()
 
     if (groupedMapping()) {
         DisableCompositingQueryAsserts disabler;
-        groupedMapping()->removeLayerFromSquashingGraphicsLayer(this);
         setGroupedMapping(0, InvalidateLayerAndRemoveFromMapping);
     }
 
@@ -2322,6 +2321,7 @@ void DeprecatedPaintLayer::setGroupedMapping(CompositedDeprecatedPaintLayerMappi
         m_groupedMapping->removeLayerFromSquashingGraphicsLayer(this);
     }
     m_groupedMapping = groupedMapping;
+    ASSERT(!m_groupedMapping || m_groupedMapping->verifyLayerInSquashingVector(this));
     if (options == InvalidateLayerAndRemoveFromMapping && m_groupedMapping)
         m_groupedMapping->setNeedsGraphicsLayerUpdate(GraphicsLayerUpdateSubtree);
 }
