0c4636347c1fccd8cd98b4be3acd4ec4c99531c3
edwardjung <edwardjung@chromium.org>
2015-07-21 12:23:16 -0700

    Add UMA histograms for Google cached copy experiment on network error interstitial
    Tracks usage:
    + of promoted 'Google cached copy' link doctor button.
    + on each of the two button label variants.
    
    BUG=474848
    
    Review URL: https://codereview.chromium.org/1214303003
    
    Cr-Commit-Position: refs/heads/master@{#339714}

--- components/error_page/renderer/net_error_helper_core.cc
+++ components/error_page/renderer/net_error_helper_core.cc
@@ -392,6 +392,8 @@ struct NetErrorHelperCore::ErrorPageInfo {
         needs_load_navigation_corrections(false),
         reload_button_in_page(false),
         show_saved_copy_button_in_page(false),
+        show_cached_page_button_in_page(false),
+        show_cached_copy_button_in_page(false),
         is_finished_loading(false),
         auto_reload_triggered(false) {
   }
@@ -425,6 +427,8 @@ struct NetErrorHelperCore::ErrorPageInfo {
   // Track if specific buttons are included in an error page, for statistics.
   bool reload_button_in_page;
   bool show_saved_copy_button_in_page;
+  bool show_cached_page_button_in_page;
+  bool show_cached_copy_button_in_page;
 
   // True if a page has completed loading, at which point it can receive
   // updates.
@@ -611,6 +615,13 @@ void NetErrorHelperCore::OnFinishLoad(FrameType frame_type) {
     chrome_common_net::RecordEvent(
         chrome_common_net::NETWORK_ERROR_PAGE_BOTH_BUTTONS_SHOWN);
   }
+  if (committed_error_page_info_->show_cached_copy_button_in_page) {
+    chrome_common_net::RecordEvent(
+        chrome_common_net::NETWORK_ERROR_PAGE_CACHED_COPY_BUTTON_SHOWN);
+  } else if (committed_error_page_info_->show_cached_page_button_in_page) {
+    chrome_common_net::RecordEvent(
+        chrome_common_net::NETWORK_ERROR_PAGE_CACHED_PAGE_BUTTON_SHOWN);
+  }
 
   delegate_->EnablePageHelperFunctions();
 
@@ -656,10 +667,13 @@ void NetErrorHelperCore::GetErrorHTML(
     // These values do not matter, as error pages in iframes hide the buttons.
     bool reload_button_in_page;
     bool show_saved_copy_button_in_page;
+    bool show_cached_copy_button_in_page;
+    bool show_cached_page_button_in_page;
 
     delegate_->GenerateLocalizedErrorPage(
         error, is_failed_post, scoped_ptr<ErrorPageParams>(),
         &reload_button_in_page, &show_saved_copy_button_in_page,
+        &show_cached_copy_button_in_page, &show_cached_page_button_in_page,
         error_html);
   }
 }
@@ -721,6 +735,8 @@ void NetErrorHelperCore::GetErrorHtmlForMainFrame(
       scoped_ptr<ErrorPageParams>(),
       &pending_error_page_info->reload_button_in_page,
       &pending_error_page_info->show_saved_copy_button_in_page,
+      &pending_error_page_info->show_cached_copy_button_in_page,
+      &pending_error_page_info->show_cached_page_button_in_page,
       error_html);
 }
 
@@ -782,6 +798,8 @@ void NetErrorHelperCore::OnNavigationCorrectionsFetched(
         params.Pass(),
         &pending_error_page_info_->reload_button_in_page,
         &pending_error_page_info_->show_saved_copy_button_in_page,
+        &pending_error_page_info_->show_cached_copy_button_in_page,
+        &pending_error_page_info_->show_cached_page_button_in_page,
         &error_html);
   } else {
     // Since |navigation_correction_params| in |pending_error_page_info_| is
@@ -964,6 +982,14 @@ void NetErrorHelperCore::ExecuteButtonPress(bool is_error_page, Button button) {
     case EASTER_EGG:
       chrome_common_net::RecordEvent(
           chrome_common_net::NETWORK_ERROR_EASTER_EGG_ACTIVATED);
+      return;
+    case SHOW_CACHED_COPY_BUTTON:
+      chrome_common_net::RecordEvent(
+          chrome_common_net::NETWORK_ERROR_PAGE_CACHED_COPY_BUTTON_CLICKED);
+      return;
+    case SHOW_CACHED_PAGE_BUTTON:
+      chrome_common_net::RecordEvent(
+          chrome_common_net::NETWORK_ERROR_PAGE_CACHED_PAGE_BUTTON_CLICKED);
       return;
     case BUTTON_MAX:
     case NO_BUTTON:
