bcc33b4759ef80ee06cf750952ef9a2d47e279e9
benwells <benwells@chromium.org>
2015-07-20 17:53:39 -0700

    Remove gap in ChromeOS hosted app frames which are showing the location.
    For security reasons, hosted apps which have navigated away from their starting origin show the origin. On ChromeOS this caused a bug where a 2 pixel transparent gap was created between the frame and the location bar.
    
    This was because the for this type of frame, the toolbar was positioned to be calculated using the HeaderHeightForPainting. However the HeaderHeightForPainting was adjusted prior to the toolbar being positioned to account for the toolbar not painting the top two rows of pixels. The fix is to position the toolbar relative to the base height of the frame.
    
    BUG=508043
    
    Review URL: https://codereview.chromium.org/1240733003
    
    Cr-Commit-Position: refs/heads/master@{#339581}

--- chrome/browser/ui/views/frame/browser_header_painter_ash.h
+++ chrome/browser/ui/views/frame/browser_header_painter_ash.h
@@ -47,6 +47,7 @@ class BrowserHeaderPainterAsh : public ash::HeaderPainter,
   int GetMinimumHeaderWidth() const override;
   void PaintHeader(gfx::Canvas* canvas, Mode mode) override;
   void LayoutHeader() override;
+  int GetHeaderHeight() const override;
   int GetHeaderHeightForPainting() const override;
   void SetHeaderHeightForPainting(int height) override;
   void SchedulePaintForTitle() override;
