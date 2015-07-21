6b42c7a4d09e11c8f9e9509d730434e031cde6fd
sdefresne <sdefresne@chromium.org>
2015-07-21 05:14:32 -0700

    Implements N:M forwarding of OmniboxEditModel events on iOS.
    RLZTrackerDelegate implementation on iOS need to listen to events from
    any OmniboxEditModel (all other clients of the same events on iOS also
    do not care about which OmniboxEditModel sends them).
    
    Introduce OmniboxEditModelObserverRelay to allow N:M forwarding. This
    class will be informed of events sent by all OmniboxEditModel and will
    forward them to all interested observers.
    
    BUG=511570,371536
    
    Review URL: https://codereview.chromium.org/1245683002
    
    Cr-Commit-Position: refs/heads/master@{#339640}

---
+++ ios/chrome/browser/omnibox/omnibox_event_global_tracker.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/omnibox/omnibox_event_global_tracker.h"
+
+#include "base/memory/singleton.h"
+
+OmniboxEventGlobalTracker* OmniboxEventGlobalTracker::GetInstance() {
+  return Singleton<OmniboxEventGlobalTracker>::get();
+}
+
+scoped_ptr<base::CallbackList<void(OmniboxLog*)>::Subscription>
+OmniboxEventGlobalTracker::RegisterCallback(const OnURLOpenedCallback& cb) {
+  return on_url_opened_callback_list_.Add(cb);
+}
+
+void OmniboxEventGlobalTracker::OnURLOpened(OmniboxLog* log) {
+  on_url_opened_callback_list_.Notify(log);
+}
+
+OmniboxEventGlobalTracker::OmniboxEventGlobalTracker() {}
+
+OmniboxEventGlobalTracker::~OmniboxEventGlobalTracker() {}
