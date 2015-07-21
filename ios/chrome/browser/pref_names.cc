45ecb7480e26a541bcbcee7b91b6a4ab1899438a
sdefresne <sdefresne@chromium.org>
2015-07-21 11:10:03 -0700

    Implements RLZTrackerDelegate on iOS.
    Reland of https://codereview.chromium.org/1242373002/ but correctly rebased
    on ToT.
    
    BUG=511570
    
    Review URL: https://codereview.chromium.org/1242743003
    
    Cr-Commit-Position: refs/heads/master@{#339696}

--- ios/chrome/browser/pref_names.cc
+++ ios/chrome/browser/pref_names.cc
@@ -15,6 +15,7 @@ namespace prefs {
 // preference.
 
 const char kAcceptLanguages[] = "intl.accept_languages";
+const char kHomePage[] = "homepage";
 const char kSavingBrowserHistoryDisabled[] = "history.saving_disabled";
 
 }  // namespace prefs
