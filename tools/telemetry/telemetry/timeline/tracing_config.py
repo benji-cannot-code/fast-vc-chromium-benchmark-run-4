31e1df32211dc92ea2bbf31ddea9c5daf3e6579c
zhenw <zhenw@chromium.org>
2015-07-20 22:34:15 -0700

    [Startup Tracing][Telemetry] Add tracing_config
    This CL adds tracing_config to Telemetry. It is used to produce the trace config JSON string, which will be used by startup tracing.
    
    Gradually, Telemetry will switch to use the trace config JSON format instead of the trace options and category filters in the future.
    
    Design doc:
    https://docs.google.com/document/d/1yRCXhrQ-0rsfUgNHt9T4YdnmJYrXKN6aK56Ozk3kPVc/edit?usp=sharing
    
    BUG=317481, 482098
    
    Review URL: https://codereview.chromium.org/1234783006
    
    Cr-Commit-Position: refs/heads/master@{#339614}

---
+++ tools/telemetry/telemetry/timeline/tracing_config.py
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import json
+
+
+class TracingConfig(object):
+  """Tracing config is the configuration for Chrome tracing.
+
+  This produces the trace config JSON string for Chrome tracing. For the details
+  about the JSON string format, see base/trace_event/trace_config.h.
+  """
+  def __init__(self, tracing_options, tracing_category_filter):
+    self._tracing_options = tracing_options
+    self._tracing_category_filter = tracing_category_filter
+
+  def GetTraceConfigJsonString(self):
+    result = {}
+    result.update(self._tracing_options.GetDictForChromeTracing())
+    result.update(self._tracing_category_filter.GetDictForChromeTracing())
+    return json.dumps(result, sort_keys=True)
