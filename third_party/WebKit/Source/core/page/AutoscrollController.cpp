e773bb2fe4ecd2aafdd0ac0d6e076fd7da00400f
thakis@chromium.org <thakis@chromium.org>
2015-07-21 05:59:52 +0000

    clang/win: Fix a -Wswitch warning.
    No intended behavior change. Fixes:
    
    ..\..\third_party\WebKit\Source\core\page\AutoscrollController.cpp(178,13) :
            warning(clang): enumeration values 'NoAutoscroll',
                            'AutoscrollForDragAndDrop', and 'AutoscrollForSelection'
                            not handled in switch [-Wswitch]
        switch (m_autoscrollType) {
                ^
    BUG=505308
    
    Review URL: https://codereview.chromium.org/1241403002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199204 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/AutoscrollController.cpp
+++ third_party/WebKit/Source/core/page/AutoscrollController.cpp
@@ -184,6 +184,11 @@ void AutoscrollController::handleMouseReleaseForPanScrolling(LocalFrame* frame, 
     case AutoscrollForPanCanStop:
         stopAutoscroll();
         break;
+    case AutoscrollForDragAndDrop:
+    case AutoscrollForSelection:
+    case NoAutoscroll:
+        // Nothing to do.
+        break;
     }
 }
 
