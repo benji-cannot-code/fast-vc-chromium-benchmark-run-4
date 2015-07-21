84c681a1ea7202b0efa067180a5a8dd05e0303e3
trchen@chromium.org <trchen@chromium.org>
2015-07-21 05:50:38 +0000

    Initialize every bitfields in FillLayer
    LLVM/Clang generates add/lea instructions on x86 for bitfield manipulation,
    which can confuse Valgrind's valid bit tracking.
    
    As a drive-by change, reset m_cachedPropertiesComputed to false on assignment.
    No code is affected for now. Just preventive care.
    
    No test added because no behavioral change.
    
    BUG=509513
    
    Review URL: https://codereview.chromium.org/1244993003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199203 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/style/FillLayer.cpp
+++ third_party/WebKit/Source/core/style/FillLayer.cpp
@@ -74,6 +74,9 @@ FillLayer::FillLayer(EFillLayerType type, bool useInitialValues)
     , m_blendModeSet(useInitialValues)
     , m_maskSourceTypeSet(useInitialValues)
     , m_type(type)
+    , m_thisOrNextLayersClipMax(0)
+    , m_thisOrNextLayersUseContentBox(0)
+    , m_thisOrNextLayersHaveLocalAttachment(0)
     , m_cachedPropertiesComputed(false)
 {
 }
@@ -109,6 +112,9 @@ FillLayer::FillLayer(const FillLayer& o)
     , m_blendModeSet(o.m_blendModeSet)
     , m_maskSourceTypeSet(o.m_maskSourceTypeSet)
     , m_type(o.m_type)
+    , m_thisOrNextLayersClipMax(0)
+    , m_thisOrNextLayersUseContentBox(0)
+    , m_thisOrNextLayersHaveLocalAttachment(0)
     , m_cachedPropertiesComputed(false)
 {
 }
@@ -156,6 +162,8 @@ FillLayer& FillLayer::operator=(const FillLayer& o)
     m_maskSourceTypeSet = o.m_maskSourceTypeSet;
 
     m_type = o.m_type;
+
+    m_cachedPropertiesComputed = false;
 
     return *this;
 }
