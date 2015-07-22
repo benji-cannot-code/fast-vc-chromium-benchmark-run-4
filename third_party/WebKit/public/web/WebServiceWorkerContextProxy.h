b2561e4971df8f8c3a6dfda5d6d0c20e869a8800
mek@chromium.org <mek@chromium.org>
2015-07-22 20:20:56 +0000

    Delete last bit of stashed message port implementation from blink.
    It's not entirely clear what the future of navigator.connect is, but stashed
    ports is definitely not it.
    
    [1/3] https://codereview.chromium.org/1248003003, deleting most of the blink side of the code
    [2/3] https://codereview.chromium.org/1248763005, deletes the content side
    [3/3] This patch, deletes the rest of the blink code
    
    BUG=483108
    
    Review URL: https://codereview.chromium.org/1248893003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199330 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebServiceWorkerContextProxy.h
+++ third_party/WebKit/public/web/WebServiceWorkerContextProxy.h
@@ -79,8 +79,6 @@ public:
     // have been updated.
     virtual void dispatchSyncEvent(int syncEventID) = 0;
     virtual void dispatchSyncEvent(int syncEventID, const WebSyncRegistration&) = 0;
-
-    virtual void addStashedMessagePorts(const WebMessagePortChannelArray& channels, const WebVector<WebString>& channelNames) {}
 };
 
 } // namespace blink
