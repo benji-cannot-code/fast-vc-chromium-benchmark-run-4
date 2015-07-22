181bb5001d05f9eb28eacbf1e7c294610f978e94
sievers <sievers@chromium.org>
2015-07-21 17:50:28 -0700

    Revert of gpu: Use shader cache on ES3 (patchset #2 id:20001 of https://codereview.chromium.org/1223393003/)
    Reason for revert:
    This codepath is actually flaky.
    
    OnLoadedShader() races with dynamic GL binding initialization. Previously it meant that we just might not end up loading this shader (as long as b_GL_OES_get_program_binary etc. default to false), but the GetGLVersionInfo() might actually crash.
    
    Original issue's description:
    > gpu: Use shader cache on ES3
    >
    > The ES3 core specs supports gl(Get)ProgramBinary().
    > BUG=510172
    >
    > Committed: https://crrev.com/809d9b979c0edd3976132eaaf77fc4740fcee019
    > Cr-Commit-Position: refs/heads/master@{#339369}
    
    TBR=dyen@chromium.org,zmo@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=510172,512464
    
    Review URL: https://codereview.chromium.org/1247233002
    
    Cr-Commit-Position: refs/heads/master@{#339802}

--- ui/gl/gl_gl_api_implementation.h
+++ ui/gl/gl_gl_api_implementation.h
@@ -37,7 +37,7 @@ void ClearGLBindingsGL();
 void SetGLToRealGLApi();
 void SetGLApi(GLApi* api);
 void SetGLApiToNoContext();
-const GL_EXPORT GLVersionInfo* GetGLVersionInfo();
+const GLVersionInfo* GetGLVersionInfo();
 
 class GL_EXPORT GLApiBase : public GLApi {
  public:
