d7bb2db014a895bf9411c7634eaaf093c1248283
shreyasv <shreyasv@chromium.org>
2015-07-22 10:42:16 -0700

    Removing kChromeForTestingHost constant
    This is not in use anymore.
    
    BUG=511241
    
    Review URL: https://codereview.chromium.org/1246773002
    
    Cr-Commit-Position: refs/heads/master@{#339906}

--- ios/chrome/browser/chrome_url_constants.cc
+++ ios/chrome/browser/chrome_url_constants.cc
@@ -7,4 +7,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 const char kChromeUIExternalFileHost[] = "external-file";
 const char kChromeUIOmahaHost[] = "omaha";
-const char kChromeUISetUpForTestingHost[] = "setupfortesting";
