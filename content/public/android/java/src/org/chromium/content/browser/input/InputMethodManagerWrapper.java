f28e2f7e883b96a1529f59292f0fde33603ee636
changwan <changwan@chromium.org>
2015-07-22 11:10:32 -0700

    Add debug logging for InputMethodManagerWrapper
    This will help debug soft keyboard show / hide issues.
    
    BUG=502151
    
    Review URL: https://codereview.chromium.org/1246273002
    
    Cr-Commit-Position: refs/heads/master@{#339913}

--- content/public/android/java/src/org/chromium/content/browser/input/InputMethodManagerWrapper.java
+++ content/public/android/java/src/org/chromium/content/browser/input/InputMethodManagerWrapper.java
@@ -14,13 +14,19 @@ import android.view.View;
 import android.view.inputmethod.CursorAnchorInfo;
 import android.view.inputmethod.InputMethodManager;
 
+import org.chromium.base.Log;
+
 /**
  * Wrapper around Android's InputMethodManager
  */
 public class InputMethodManagerWrapper {
+    private static final String TAG = "cr.InputMethodManager";
+    private static final boolean DEBUG = false;
+
     private final Context mContext;
 
     public InputMethodManagerWrapper(Context context) {
+        if (DEBUG) Log.w(TAG, "Constructor");
         mContext = context;
     }
 
@@ -32,6 +38,7 @@ public class InputMethodManagerWrapper {
      * @see android.view.inputmethod.InputMethodManager#restartInput(View)
      */
     public void restartInput(View view) {
+        if (DEBUG) Log.w(TAG, "restartInput");
         getInputMethodManager().restartInput(view);
     }
 
@@ -39,6 +46,7 @@ public class InputMethodManagerWrapper {
      * @see android.view.inputmethod.InputMethodManager#showSoftInput(View, int, ResultReceiver)
      */
     public void showSoftInput(View view, int flags, ResultReceiver resultReceiver) {
+        if (DEBUG) Log.w(TAG, "showSoftInput");
         getInputMethodManager().showSoftInput(view, flags, resultReceiver);
     }
 
@@ -46,7 +54,9 @@ public class InputMethodManagerWrapper {
      * @see android.view.inputmethod.InputMethodManager#isActive(View)
      */
     public boolean isActive(View view) {
-        return getInputMethodManager().isActive(view);
+        final boolean active = getInputMethodManager().isActive(view);
+        if (DEBUG) Log.w(TAG, "isActive: " + active);
+        return active;
     }
 
     /**
@@ -54,6 +64,7 @@ public class InputMethodManagerWrapper {
      */
     public boolean hideSoftInputFromWindow(IBinder windowToken, int flags,
             ResultReceiver resultReceiver) {
+        if (DEBUG) Log.w(TAG, "hideSoftInputFromWindow");
         return getInputMethodManager().hideSoftInputFromWindow(windowToken, flags, resultReceiver);
     }
 
@@ -62,6 +73,10 @@ public class InputMethodManagerWrapper {
      */
     public void updateSelection(View view, int selStart, int selEnd,
             int candidatesStart, int candidatesEnd) {
+        if (DEBUG) {
+            Log.w(TAG, "updateSelection: SEL [%d, %d], COM [%d, %d]", selStart, selEnd,
+                    candidatesStart, candidatesEnd);
+        }
         getInputMethodManager().updateSelection(view, selStart, selEnd, candidatesStart,
                 candidatesEnd);
     }
@@ -72,6 +87,7 @@ public class InputMethodManagerWrapper {
      */
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public void updateCursorAnchorInfo(View view, CursorAnchorInfo cursorAnchorInfo) {
+        if (DEBUG) Log.w(TAG, "updateCursorAnchorInfo");
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getInputMethodManager().updateCursorAnchorInfo(view, cursorAnchorInfo);
         }
