53249450d0742efb38c8f76700e2e97b9fddef88
lionel.g.landwerlin <lionel.g.landwerlin@intel.com>
2015-07-22 16:10:04 -0700

    extensions: tabs: add checks on tab id
    BUG=394341
    TEST=browser_tests --gtest_filter=ExtensionApiTest.TabQuery && browser_tests --gtest_filter=ExtensionTabs*
    
    Review URL: https://codereview.chromium.org/1242483003
    
    Cr-Commit-Position: refs/heads/master@{#339976}

--- chrome/test/data/extensions/api_test/tabs/basics/query.js
+++ chrome/test/data/extensions/api_test/tabs/basics/query.js
@@ -181,12 +181,16 @@ chrome.test.runTests([
         }));
         chrome.tabs.query({windowType: 'popup'}, pass(function(tabs) {
           assertEq(2, tabs.length);
+          for (var i = 0; i < tabs.length; i++)
+            assertFalse(tabs[i].id == chrome.tabs.TAB_ID_NONE);
         }));
         chrome.tabs.query({
           windowType: 'popup',
           url: 'about:*'
         }, pass(function(tabs) {
           assertEq(2, tabs.length);
+          for (var i = 0; i < tabs.length; i++)
+            assertFalse(tabs[i].id == chrome.tabs.TAB_ID_NONE);
         }));
       }));
     }));
@@ -212,4 +216,3 @@ chrome.test.runTests([
     }));
   }
 ]);
-
