3a8b38209a2e08018ba90589d28d8034d33acb3b
gangwu <gangwu@chromium.org>
2015-07-22 09:58:32 -0700

    Enable DeleteDirectoryWhenCorrupted test
    The test was disabled since flaky on server. The test failed
    due to timeout because the number of entries was increased in
    bug 468363.
    So in this check in, I reduce the number of entries, and
    increase the size of each entry to let sync service detect
    database corrupt faster.
    
    BUG=511388
    
    Review URL: https://codereview.chromium.org/1238593007
    
    Cr-Commit-Position: refs/heads/master@{#339891}

--- sync/syncable/directory_backing_store_unittest.cc
+++ sync/syncable/directory_backing_store_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/stringprintf.h"
 #include "sql/connection.h"
 #include "sql/statement.h"
 #include "sql/test/scoped_error_ignorer.h"
@@ -40,10 +41,11 @@ void CatastrophicErrorHandler(bool* catastrophic_error_handler_was_called) {
   *catastrophic_error_handler_was_called = true;
 }
 
-// Create a dirty EntryKernel with an ID derived from |id|.
-scoped_ptr<EntryKernel> CreateEntry(int id) {
+// Create a dirty EntryKernel with an ID derived from |id| + |id_suffix|.
+scoped_ptr<EntryKernel> CreateEntry(int id, const std::string &id_suffix) {
   scoped_ptr<EntryKernel> entry(new EntryKernel());
-  entry->put(ID, Id::CreateFromClientString(base::Int64ToString(id)));
+  std::string id_string = base::Int64ToString(id) + id_suffix;
+  entry->put(ID, Id::CreateFromClientString(id_string));
   entry->put(META_HANDLE, id);
   entry->mark_dirty(NULL);
   return entry;
@@ -4035,7 +4037,7 @@ TEST_F(DirectoryBackingStoreTest,
     ASSERT_TRUE(LoadAndIgnoreReturnedData(dbs.get()));
     ASSERT_FALSE(dbs->DidFailFirstOpenAttempt());
     Directory::SaveChangesSnapshot snapshot;
-    snapshot.dirty_metas.insert(CreateEntry(2).release());
+    snapshot.dirty_metas.insert(CreateEntry(2, "").release());
     ASSERT_TRUE(dbs->SaveChanges(snapshot));
   }
 
@@ -4087,9 +4089,11 @@ TEST_F(DirectoryBackingStoreTest,
   ASSERT_TRUE(LoadAndIgnoreReturnedData(dbs.get()));
   ASSERT_FALSE(dbs->DidFailFirstOpenAttempt());
   Directory::SaveChangesSnapshot snapshot;
+  const std::string suffix(400, 'o');
   for (int i = 0; i < corruption_testing::kNumEntriesRequiredForCorruption;
        ++i) {
-    snapshot.dirty_metas.insert(CreateEntry(i).release());
+    scoped_ptr<EntryKernel> large_entry = CreateEntry(i, suffix);
+    snapshot.dirty_metas.insert(large_entry.release());
   }
   ASSERT_TRUE(dbs->SaveChanges(snapshot));
   // Corrupt it.
