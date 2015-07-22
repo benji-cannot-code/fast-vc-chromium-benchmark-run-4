2849b9e98fe719cc9fc6eea619ecff5dd8fa2467
yutak@chromium.org <yutak@chromium.org>
2015-07-22 10:52:17 +0000

    Revert of Oilpan: Remove raw pointer to WorkerThreadDebugger from WorkerDebuggerAgent (patchset #1 id:1 of https://codereview.chromium.org/1234223002/)
    Reason for revert:
    Broke Oilpan compilation.
    
    This patch is not correct, because WorkerThreadDebugger
    is not a garbage-collected class.
    
    Original issue's description:
    > Oilpan: Remove raw pointer to WorkerThreadDebugger from WorkerDebuggerAgent
    > 
    > BUG=509911
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=199297
    
    TBR=haraken@chromium.org,oilpan-reviews@chromium.org,keishi@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=509911
    
    Review URL: https://codereview.chromium.org/1242773004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199305 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/WorkerDebuggerAgent.cpp
+++ third_party/WebKit/Source/core/inspector/WorkerDebuggerAgent.cpp
@@ -57,7 +57,6 @@ WorkerDebuggerAgent::~WorkerDebuggerAgent()
 
 DEFINE_TRACE(WorkerDebuggerAgent)
 {
-    visitor->trace(m_workerThreadDebugger);
     visitor->trace(m_inspectedWorkerGlobalScope);
     InspectorDebuggerAgent::trace(visitor);
 }
--- third_party/WebKit/Source/core/inspector/WorkerDebuggerAgent.h
+++ third_party/WebKit/Source/core/inspector/WorkerDebuggerAgent.h
@@ -57,7 +57,7 @@ private:
     void muteConsole() override;
     void unmuteConsole() override;
 
-    RawPtrWillBeMember<WorkerThreadDebugger> m_workerThreadDebugger;
+    WorkerThreadDebugger* m_workerThreadDebugger;
     RawPtrWillBeMember<WorkerGlobalScope> m_inspectedWorkerGlobalScope;
 };
 
