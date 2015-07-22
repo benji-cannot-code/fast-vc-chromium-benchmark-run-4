18d82bc09bd7e6d428d0a8fba0517043ad8e8f4d
tsergeant <tsergeant@chromium.org>
2015-07-22 00:40:08 -0700

    Material PDF: Hide toolbars offscreen rather than making them invisible
    This improves animation jank caused by expensive layer updates when
    hiding the toolbars.
    
    BUG=439114
    
    Review URL: https://codereview.chromium.org/1244793003
    
    Cr-Commit-Position: refs/heads/master@{#339844}

--- chrome/browser/resources/pdf/elements/viewer-pdf-toolbar/viewer-pdf-toolbar.js
+++ chrome/browser/resources/pdf/elements/viewer-pdf-toolbar/viewer-pdf-toolbar.js
@@ -79,8 +79,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     },
 
     _onAnimationFinished: function() {
-      if (!this.opened)
-        this.style.visibility = 'hidden';
+        this.style.transform = this.opened ? 'none' : 'translateY(-100%)';
     },
 
     loadProgressChanged: function() {
@@ -99,7 +98,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     show: function() {
       if (!this.opened) {
         this.toggleVisibility();
-        this.style.visibility = 'initial';
       }
     },
 
