03de0c5d6a254bc25e745117df80cf6c637ca205
lalitm@google.com <lalitm@google.com>
2015-07-22 10:30:12 +0000

    blink: permissions: cleanup and add tests for revoke
    NOTE: this is a part of a three sided patch:
    https://codereview.chromium.org/1234023002
    https://codereview.chromium.org/1238673002
    https://codereview.chromium.org/1241133002 (this)
    
    BUG=510405
    
    Review URL: https://codereview.chromium.org/1241133002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199302 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/modules/permissions/WebPermissionClient.h
+++ third_party/WebKit/public/platform/modules/permissions/WebPermissionClient.h
@@ -12,9 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// WebPermissionQueryCallback is kept for backward compatibility
-using WebPermissionQueryCallback = WebCallbacks<WebPermissionStatus, void>;
-using WebPermissionCallback = WebPermissionQueryCallback;
+using WebPermissionCallback = WebCallbacks<WebPermissionStatus, void>;
 
 class WebPermissionObserver;
 class WebURL;
