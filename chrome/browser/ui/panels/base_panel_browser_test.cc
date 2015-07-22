e718acf0f166762125fe0e99e328947ddd52d774
avi <avi@chromium.org>
2015-07-22 15:25:22 -0700

    Remove NOTIFICATION_PANEL_WINDOW_SIZE_KNOWN.
    It hasn't been signaled since r263101 (the deletion of the GTK+ port), so remove it and all code depending on it.
    
    BUG=268984
    TEST=no change
    
    Review URL: https://codereview.chromium.org/1251133002
    
    Cr-Commit-Position: refs/heads/master@{#339960}

--- chrome/browser/ui/panels/base_panel_browser_test.cc
+++ chrome/browser/ui/panels/base_panel_browser_test.cc
@@ -289,18 +289,6 @@ void BasePanelBrowserTest::WaitForPanelActiveState(
   signal.Wait();
 }
 
-void BasePanelBrowserTest::WaitForWindowSizeAvailable(Panel* panel) {
-  scoped_ptr<NativePanelTesting> panel_testing(
-      CreateNativePanelTesting(panel));
-  content::WindowedNotificationObserver signal(
-      chrome::NOTIFICATION_PANEL_WINDOW_SIZE_KNOWN,
-      content::Source<Panel>(panel));
-  if (panel_testing->IsWindowSizeKnown())
-    return;
-  signal.Wait();
-  EXPECT_TRUE(panel_testing->IsWindowSizeKnown());
-}
-
 void BasePanelBrowserTest::WaitForBoundsAnimationFinished(Panel* panel) {
   scoped_ptr<NativePanelTesting> panel_testing(
       CreateNativePanelTesting(panel));
@@ -389,10 +377,6 @@ Panel* BasePanelBrowserTest::CreatePanelWithParams(
     // asynchronous communication, and it is not enough to just run the local
     // message loop to make sure this activity has completed.
     WaitForPanelActiveState(panel, params.expected_active_state);
-
-    // On Linux, window size is not available right away and we should wait
-    // before moving forward with the test.
-    WaitForWindowSizeAvailable(panel);
 
     // Wait for the bounds animations on creation to finish.
     WaitForBoundsAnimationFinished(panel);
