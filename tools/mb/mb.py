315c21c0a8deed83b47bbd855125eb6dc27de6ac
dpranke <dpranke@chromium.org>
2015-07-22 15:53:12 -0700

    Clean up code nits in //tools/mb, //testing/buildbot.
    Minor code edits from a codereview comment for #339804.
    
    TBR=maruel@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1256443002
    
    Cr-Commit-Position: refs/heads/master@{#339965}

--- tools/mb/mb.py
+++ tools/mb/mb.py
@@ -478,7 +478,6 @@ class MetaBuildWrapper(object):
           'variables': {
             'command': command,
             'files': sorted(runtime_deps + extra_files),
-            'read_only': 1,
           }
         }) + '\n')
 
