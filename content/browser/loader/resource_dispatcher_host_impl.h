52a7ded6b51f048a323f6feef2068ef411b1f15d
raymes <raymes@chromium.org>
2015-07-21 19:03:41 -0700

    Make the logic for stream interception by MimeHandlerViews consistent with starting the plugin
    Currently there is logic in the renderer which checks whether a plugin can
    handle a mime type and if it can the plugin is started. This is also how
    MimeHandlerView plugins are started in the renderer. MimeHandlerViews also
    rely on the original http request being intercepted as a stream which the
    plugin can read when it chooses. This happens in the browser. Currently
    the logic for determining whether the stream should be intercepted is different
    to the logic for determining whether to start the plugin which can lead to
    problems in some cases. This CL makes sure the logic is the same: on both
    sides we check whether the plugin can handle the mime type.
    
    MimeHandlerView plugins load a chrome extension inside a BrowserPlugin.
    Once it has been determined that the plugin can handle the mime type inside
    BufferedResourceHandler, the extension ID of the plugin is passed down through into
    ChromeResourceDispatcherHostDelegate::ShouldInterceptResourceAsStream.
    It is then checked that the extension is enabled and if so the stream is intercepted.
    There is some complexity because we also need to support the legacy case
    where the stream is intercepted for the streamsPrivate api in which case there is no
    MimeHandlerView plugin.
    
    This CL was originally written by deepak.m1@samsung.com (Deepak Mittal)
    and landed here: https://codereview.chromium.org/953793003/. It was reverted
    because of a bug it triggered with not downloading PDFs when the PDF plugin
    was disabled. This bug has been addressed and a test added for that case.
    
    BUG=443466
    
    Review URL: https://codereview.chromium.org/1164073006
    
    Cr-Commit-Position: refs/heads/master@{#339816}

--- content/browser/loader/resource_dispatcher_host_impl.h
+++ content/browser/loader/resource_dispatcher_host_impl.h
@@ -46,6 +46,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class ResourceHandler;
 struct ResourceHostMsg_Request;
 
+namespace base {
+class FilePath;
+}
+
 namespace net {
 class URLRequestJobFactory;
 }
@@ -234,12 +238,20 @@ class CONTENT_EXPORT ResourceDispatcherHostImpl
       scoped_ptr<DownloadSaveInfo> save_info,
       const DownloadUrlParameters::OnStartedCallback& started_cb);
 
-  // Must be called after the ResourceRequestInfo has been created
-  // and associated with the request.  If |payload| is set to a non-empty value,
-  // the value will be sent to the old resource handler instead of canceling
-  // it, except on HTTP errors. This is marked virtual so it can be overriden in
-  // testing.
+  // Called to determine whether the response to |request| should be intercepted
+  // and handled as a stream. Streams are used to pass direct access to a
+  // resource response to another application (e.g. a web page) without being
+  // handled by the browser itself. If the request should be intercepted as a
+  // stream, a StreamResourceHandler is returned which provides access to the
+  // response. |plugin_path| is the path to the plugin which is handling the
+  // URL request. This may be empty if there is no plugin handling the request.
+  //
+  // This function must be called after the ResourceRequestInfo has been created
+  // and associated with the request. If |payload| is set to a non-empty value,
+  // the caller must send it to the old resource handler instead of cancelling
+  // it.
   virtual scoped_ptr<ResourceHandler> MaybeInterceptAsStream(
+      const base::FilePath& plugin_path,
       net::URLRequest* request,
       ResourceResponse* response,
       std::string* payload);
