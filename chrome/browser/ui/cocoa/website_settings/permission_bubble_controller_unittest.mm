1225e53d5d07d357d09618bf9de69b8de4c366a4
hcarmona <hcarmona@chromium.org>
2015-07-22 10:24:26 -0700

    Don't dismiss permission bubbles on fullscreen toggle + add test.
    BUG=512630
    
    Review URL: https://codereview.chromium.org/1243043003
    
    Cr-Commit-Position: refs/heads/master@{#339899}

--- chrome/browser/ui/cocoa/website_settings/permission_bubble_controller_unittest.mm
+++ chrome/browser/ui/cocoa/website_settings/permission_bubble_controller_unittest.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <Carbon/Carbon.h>
 
 #include "base/mac/foundation_util.h"
+#include "base/mac/sdk_forward_declarations.h"
 #include "base/stl_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
@@ -306,4 +307,36 @@ TEST_F(PermissionBubbleControllerTest, EscapeCloses) {
       performKeyEquivalent:cocoa_test_event_utils::KeyEventWithKeyCode(
                                kVK_Escape, '\e', NSKeyDown, 0)];
   EXPECT_FALSE([[controller_ window] isVisible]);
+}
+
+TEST_F(PermissionBubbleControllerTest, EnterFullscreen) {
+  [controller_ showAtAnchor:NSZeroPoint
+               withDelegate:this
+                forRequests:requests_
+               acceptStates:accept_states_];
+
+  EXPECT_TRUE([[controller_ window] isVisible]);
+
+  // Post the "enter fullscreen" notification.
+  NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
+  [center postNotificationName:NSWindowWillEnterFullScreenNotification
+                        object:test_window()];
+
+  EXPECT_TRUE([[controller_ window] isVisible]);
+}
+
+TEST_F(PermissionBubbleControllerTest, ExitFullscreen) {
+  [controller_ showAtAnchor:NSZeroPoint
+               withDelegate:this
+                forRequests:requests_
+               acceptStates:accept_states_];
+
+  EXPECT_TRUE([[controller_ window] isVisible]);
+
+  // Post the "enter fullscreen" notification.
+  NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
+  [center postNotificationName:NSWindowWillExitFullScreenNotification
+                        object:test_window()];
+
+  EXPECT_TRUE([[controller_ window] isVisible]);
 }
