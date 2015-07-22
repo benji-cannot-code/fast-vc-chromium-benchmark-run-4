b271acabb8dc067136f773d5b7d282d4e1d80776
dfalcantara <dfalcantara@chromium.org>
2015-07-21 17:42:51 -0700

    [Document mode] Allow multiple popup windows to appear.
    Introduce a queue in TabDelegate for creating multiple Tabs in a row.
    This prevents triggering some issue in Android's ActivityManager that
    causes it to throw away the tasks for Activities after it launches them,
    which in turn causes Android to destroy our Activities.
    
    BUG=498920
    
    Review URL: https://codereview.chromium.org/1229853006
    
    Cr-Commit-Position: refs/heads/master@{#339799}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/document/TabDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/document/TabDelegate.java
@@ -12,7 +12,6 @@ import android.net.Uri;
 import android.text.TextUtils;
 
 import org.chromium.base.ApplicationStatus;
-import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.Tab;
 import org.chromium.chrome.browser.TabState;
@@ -29,13 +28,12 @@ import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.PageTransition;
 
-import java.lang.ref.WeakReference;
-
 /**
  * Asynchronously creates Tabs by creating/starting up Activities.
  */
 public class TabDelegate extends TabCreator {
-    private boolean mIsIncognito;
+    private final boolean mIsIncognito;
+    private final AsyncDocumentLauncher mLaunchSerializer;
 
     /**
      * Creates a TabDelegate.
@@ -43,6 +41,7 @@ public class TabDelegate extends TabCreator {
      */
     public TabDelegate(boolean incognito) {
         mIsIncognito = incognito;
+        mLaunchSerializer = new AsyncDocumentLauncher(mIsIncognito);
     }
 
     @Override
@@ -144,11 +143,10 @@ public class TabDelegate extends TabCreator {
                 && asyncParams.getWebContents() != null);
 
         Context context = ApplicationStatus.getApplicationContext();
-        Activity parentActivity = getActivityForTabId(parentId);
+        Activity parentActivity = ActivityDelegate.getActivityForTabId(parentId);
 
         if (FeatureUtilities.isDocumentMode(context)) {
-            ChromeLauncherActivity.launchDocumentInstance(
-                    parentActivity, mIsIncognito, asyncParams);
+            mLaunchSerializer.enqueueLaunch(parentId, asyncParams);
         } else {
             // TODO(dfalcantara): Is it possible to get rid of this conditional?
             int assignedTabId = TabIdManager.getInstance().generateValidId(Tab.INVALID_TAB_ID);
@@ -173,20 +171,5 @@ public class TabDelegate extends TabCreator {
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             IntentHandler.startActivityForTrustedIntent(intent, context);
         }
-    }
-
-    /** @return Running Activity that owns the given Tab, null if the Activity couldn't be found. */
-    private Activity getActivityForTabId(int id) {
-        if (id == Tab.INVALID_TAB_ID) return null;
-
-        for (WeakReference<Activity> ref : ApplicationStatus.getRunningActivities()) {
-            if (!(ref.get() instanceof ChromeActivity)) continue;
-
-            ChromeActivity activity = (ChromeActivity) ref.get();
-            if (activity == null) continue;
-
-            if (activity.getTabModelSelector().getModelForTabId(id) != null) return activity;
-        }
-        return null;
     }
 }
