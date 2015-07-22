58b304bd2895d9ce44db0d24f43d72169c0d1f46
keishi@chromium.org <keishi@chromium.org>
2015-07-22 04:45:10 +0000

    Oilpan: Remove raw pointer to SVGTextPositioningElement fromTextPosition
    BUG= 509911
    
    Review URL: https://codereview.chromium.org/1230763011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199276 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/svg/SVGTextLayoutAttributesBuilder.h
+++ third_party/WebKit/Source/core/layout/svg/SVGTextLayoutAttributesBuilder.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SVGTextLayoutAttributesBuilder_h
 
 #include "core/layout/svg/SVGTextLayoutAttributes.h"
+#include "platform/heap/Handle.h"
 #include "wtf/Vector.h"
 #include "wtf/text/Unicode.h"
 
@@ -53,8 +54,9 @@ public:
     void clearTextPositioningElements() { m_textPositions.clear(); }
     unsigned numberOfTextPositioningElements() const { return m_textPositions.size(); }
 
-private:
     struct TextPosition {
+        ALLOW_ONLY_INLINE_ALLOCATION();
+    public:
         TextPosition(SVGTextPositioningElement* newElement = nullptr, unsigned newStart = 0, unsigned newLength = 0)
             : element(newElement)
             , start(newStart)
@@ -62,21 +64,26 @@ private:
         {
         }
 
-        SVGTextPositioningElement* element;
+        DECLARE_TRACE();
+
+        RawPtrWillBeMember<SVGTextPositioningElement> element;
         unsigned start;
         unsigned length;
     };
 
+private:
     void buildCharacterDataMap(LayoutSVGText&);
     void collectTextPositioningElements(LayoutBoxModelObject&, UChar& lastCharacter);
     void fillCharacterDataMap(const TextPosition&);
 
 private:
     unsigned m_textLength;
-    Vector<TextPosition> m_textPositions;
+    WillBePersistentHeapVector<TextPosition> m_textPositions;
     SVGCharacterDataMap m_characterDataMap;
 };
 
 } // namespace blink
+
+WTF_ALLOW_MOVE_AND_INIT_WITH_MEM_FUNCTIONS(blink::SVGTextLayoutAttributesBuilder::TextPosition);
 
 #endif
