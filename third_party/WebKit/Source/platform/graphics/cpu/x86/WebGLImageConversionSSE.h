7286341c99f602234c45454e6da45081b6243a40
zmo@chromium.org <zmo@chromium.org>
2015-07-22 03:30:23 +0000

    Fix a bug in SSE image packing code for textures.
    This bug caused R8 texture uploading fails.
    
    BUG=511416
    TEST=webgl 2 conformance tests
    R=kbr@chromium.org
    
    Review URL: https://codereview.chromium.org/1242413003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199264 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/cpu/x86/WebGLImageConversionSSE.h
+++ third_party/WebKit/Source/platform/graphics/cpu/x86/WebGLImageConversionSSE.h
@@ -40,7 +40,7 @@ ALWAYS_INLINE void packOneRowOfRGBA8LittleToR8(const uint8_t*& source, uint8_t*&
     float tmp[4];
     unsigned pixelsPerRowTrunc = (pixelsPerRow / 4) * 4;
 
-    for (unsigned i = 0; i < pixelsPerRow; i += 4) {
+    for (unsigned i = 0; i < pixelsPerRowTrunc; i += 4) {
         __m128 scale = _mm_set_ps(source[15] ? source[15] : 255,
             source[11] ? source[11] : 255,
             source[7] ? source[7] : 255,
@@ -62,10 +62,11 @@ ALWAYS_INLINE void packOneRowOfRGBA8LittleToR8(const uint8_t*& source, uint8_t*&
     pixelsPerRow -= pixelsPerRowTrunc;
 }
 
-ALWAYS_INLINE void packOneRowOfRGBA8LittleToRGBA8(const uint8_t*& source, uint8_t*& destination, unsigned& pixelsPerRow)
+// This function deliberately doesn't mutate the incoming source, destination,
+// or pixelsPerRow arguments, since it always handles the full row.
+ALWAYS_INLINE void packOneRowOfRGBA8LittleToRGBA8(const uint8_t* source, uint8_t* destination, unsigned pixelsPerRow)
 {
     float tmp[4];
-    unsigned pixelsPerRowTrunc = (pixelsPerRow / 4) * 4;
     float scale;
 
     for (unsigned i = 0; i < pixelsPerRow; i++) {
@@ -86,8 +87,6 @@ ALWAYS_INLINE void packOneRowOfRGBA8LittleToRGBA8(const uint8_t*& source, uint8_
         source += 4;
         destination += 4;
     }
-
-    pixelsPerRow -= pixelsPerRowTrunc;
 }
 
 } // namespace SIMD
