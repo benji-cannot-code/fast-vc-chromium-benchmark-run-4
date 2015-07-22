b3415cba23a07691a140461d4d9cc02898e218d8
pkotwicz <pkotwicz@chromium.org>
2015-07-22 10:22:58 -0700

    Move more tests from ChromeShellTest to ChromePublicTest
    BUG=505040
    TEST=None
    
    Review URL: https://codereview.chromium.org/1237303003
    
    Cr-Commit-Position: refs/heads/master@{#339897}

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -1140,6 +1140,7 @@ public class ContentViewCore implements
     /**
      * @return The selected text (empty if no text selected).
      */
+    @VisibleForTesting
     public String getSelectedText() {
         return mHasSelection ? mLastSelectedText : "";
     }
@@ -2467,6 +2468,7 @@ public class ContentViewCore implements
     /**
      * @return The visible select popup being shown.
      */
+    @VisibleForTesting
     public SelectPopup getSelectPopupForTest() {
         return mSelectPopup;
     }
