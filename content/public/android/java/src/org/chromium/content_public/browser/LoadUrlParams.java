35147c6c036594cd605da6631dfcbfd61c63e27b
pkotwicz <pkotwicz@chromium.org>
2015-07-22 12:25:02 -0700

    Move RepostFormWarningTest from ChromeShellTest to ChromePublicTest
    BUG=505040
    TEST=None
    
    Review URL: https://codereview.chromium.org/1245393002
    
    Cr-Commit-Position: refs/heads/master@{#339935}

--- content/public/android/java/src/org/chromium/content_public/browser/LoadUrlParams.java
+++ content/public/android/java/src/org/chromium/content_public/browser/LoadUrlParams.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.content_public.browser;
 
 import org.chromium.base.JNINamespace;
+import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.SuppressFBWarnings;
 import org.chromium.content_public.browser.navigation_controller.LoadURLType;
 import org.chromium.content_public.browser.navigation_controller.UserAgentOverrideOption;
@@ -160,6 +161,7 @@ public class LoadUrlParams {
      * @param url URL of the load.
      * @param postData Post data of the load. Can be null.
      */
+    @VisibleForTesting
     public static LoadUrlParams createLoadHttpPostParams(
             String url, byte[] postData) {
         LoadUrlParams params = new LoadUrlParams(url);
