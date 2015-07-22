f0e55c37e3afa9b7e613c365edc3271e31785f46
yurys@chromium.org <yurys@chromium.org>
2015-07-22 16:43:20 +0000

    DevTools: simplify setScriptSource and restartFrame return values
    For the live edit it is always boolean instead of object. For restart frame we always need to do step in after restart, so there is no need to pass additional parameter in the result.
    
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1249013002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199319 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/DebuggerScript.js
+++ third_party/WebKit/Source/core/inspector/DebuggerScript.js
@@ -296,7 +296,7 @@ DebuggerScript.liveEditScriptSource = function(scriptId, newSource, preview)
     var changeLog = [];
     try {
         var result = Debug.LiveEdit.SetScriptSource(scriptToEdit, newSource, preview, changeLog);
-        return [0, result];
+        return [0, result.stack_modified];
     } catch (e) {
         if (e instanceof Debug.LiveEdit.Failure && "details" in e) {
             var details = e.details;
@@ -491,7 +491,7 @@ DebuggerScript._frameMirrorToJSCallFrame = function(frameMirror, callerFrame, sc
 
     function restart()
     {
-        return Debug.LiveEdit.RestartFrame(frameMirror);
+        return frameMirror.restart();
     }
 
     function setVariableValue(scopeNumber, variableName, newValue)
