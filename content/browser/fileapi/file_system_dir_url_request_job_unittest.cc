833356a80ad7fc4673c30f60c42995bbb04841db
tmoniuszko <tmoniuszko@opera.com>
2015-07-23 00:19:28 -0700

    Make FileSystemDirURLRequestJobTest locale-invulnerable
    The format of the date in directory listing entry depends on the current
    locale. In some countries (for example Poland) DD.mm.YYYY format is used.
    base::Time::FromString() internally uses PR_ParseTimeString() from NSPR
    library which doesn't support this format (date with dots as separators).
    Use icu::DateFormat instead which is able to parse such kind of date
    format.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1246463002
    
    Cr-Commit-Position: refs/heads/master@{#340050}

--- content/browser/fileapi/file_system_dir_url_request_job_unittest.cc
+++ content/browser/fileapi/file_system_dir_url_request_job_unittest.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/fileapi/file_system_operation_context.h"
 #include "storage/browser/fileapi/file_system_url.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/icu/source/i18n/unicode/datefmt.h"
 #include "third_party/icu/source/i18n/unicode/regex.h"
 
 using storage::FileSystemContext;
@@ -271,11 +272,13 @@ class FileSystemDirURLRequestJobTest : public testing::Test {
       EXPECT_EQ(size_string, match.group(4, status));
     }
 
-    base::Time date;
     icu::UnicodeString date_ustr(match.group(5, status));
-    std::string date_str;
-    base::UTF16ToUTF8(date_ustr.getBuffer(), date_ustr.length(), &date_str);
-    EXPECT_TRUE(base::Time::FromString(date_str.c_str(), &date));
+    scoped_ptr<icu::DateFormat> formatter(
+        icu::DateFormat::createDateTimeInstance(icu::DateFormat::kShort));
+    UErrorCode parse_status = U_ZERO_ERROR;
+    UDate udate = formatter->parse(date_ustr, parse_status);
+    EXPECT_TRUE(U_SUCCESS(parse_status));
+    base::Time date = base::Time::FromJsTime(udate);
     EXPECT_FALSE(date.is_null());
   }
 
