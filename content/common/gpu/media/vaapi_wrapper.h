f270093c1bf74d64f6d6193de1377b323d0ef04f
henryhsu <henryhsu@chromium.org>
2015-07-23 01:34:37 -0700

    Add Jpeg decoder profile to GpuInfo
    Browser process uses GpuInfo to decide whether jpeg hardware decode is
    supported.
    
    BUG=503568
    TEST=run apprtc on exynos and intel platform.
    
    Review URL: https://codereview.chromium.org/1239643003
    
    Cr-Commit-Position: refs/heads/master@{#340058}

--- content/common/gpu/media/vaapi_wrapper.h
+++ content/common/gpu/media/vaapi_wrapper.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/gpu/media/va_surface.h"
 #include "media/base/video_decoder_config.h"
 #include "media/base/video_frame.h"
+#include "media/video/jpeg_decode_accelerator.h"
 #include "media/video/video_decode_accelerator.h"
 #include "media/video/video_encode_accelerator.h"
 #include "third_party/libva/va/va.h"
@@ -68,13 +69,16 @@ class CONTENT_EXPORT VaapiWrapper {
       media::VideoCodecProfile profile,
       const base::Closure& report_error_to_uma_cb);
 
-  // Return the supported encode profiles.
+  // Return the supported video encode profiles.
   static media::VideoEncodeAccelerator::SupportedProfiles
       GetSupportedEncodeProfiles();
 
-  // Return the supported decode profiles.
+  // Return the supported video decode profiles.
   static media::VideoDecodeAccelerator::SupportedProfiles
       GetSupportedDecodeProfiles();
+
+  // Return true when JPEG decode is supported.
+  static bool IsJpegDecodeSupported();
 
   ~VaapiWrapper();
 
--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/gpu/gpu_config.h"
 #include "content/common/gpu/gpu_memory_buffer_factory.h"
 #include "content/common/gpu/gpu_messages.h"
+#include "content/common/gpu/media/gpu_jpeg_decode_accelerator.h"
 #include "content/common/gpu/media/gpu_video_decode_accelerator.h"
 #include "content/common/gpu/media/gpu_video_encode_accelerator.h"
 #include "content/common/sandbox_linux/sandbox_linux.h"
@@ -348,6 +349,8 @@ int GpuMain(const MainFunctionParams& parameters) {
         content::GpuVideoDecodeAccelerator::GetSupportedProfiles();
     gpu_info.video_encode_accelerator_supported_profiles =
         content::GpuVideoEncodeAccelerator::GetSupportedProfiles();
+    gpu_info.jpeg_decode_accelerator_supported =
+        content::GpuJpegDecodeAccelerator::IsSupported();
   } else {
     dead_on_arrival = true;
   }
