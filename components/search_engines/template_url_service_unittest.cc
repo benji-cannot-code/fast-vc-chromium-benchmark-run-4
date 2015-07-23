36a70c2762e8fbfafc0a5ba244d685b692d33b8d
brettw <brettw@chromium.org>
2015-07-23 16:23:31 -0700

    Update SplitString calls in components
    This converts calls from the old form to the new form. Some calls that iterated over the results were changed to a range-based for loop with an inline call to SplitString. Some places were changed to use StringPieces when it was safe to do so.
    
    Review URL: https://codereview.chromium.org/1234973004
    
    Cr-Commit-Position: refs/heads/master@{#340209}

--- components/search_engines/template_url_service_unittest.cc
+++ components/search_engines/template_url_service_unittest.cc
@@ -80,7 +80,8 @@ TemplateURL* CreateKeywordWithDate(
   data.favicon_url = GURL(favicon_url);
   data.safe_for_autoreplace = safe_for_autoreplace;
   data.show_in_default_list = show_in_default_list;
-  base::SplitString(encodings, ';', &data.input_encodings);
+  data.input_encodings = base::SplitString(
+      encodings, ";", base::TRIM_WHITESPACE, base::SPLIT_WANT_ALL);
   data.date_created = date_created;
   data.last_modified = last_modified;
   return new TemplateURL(data);
@@ -856,7 +857,8 @@ TEST_F(TemplateURLServiceTest, RepairSearchEnginesWithManagedDefault) {
   data.SetURL(kSearchURL);
   data.favicon_url = GURL(kIconURL);
   data.show_in_default_list = true;
-  base::SplitString(kEncodings, ';', &data.input_encodings);
+  data.input_encodings = base::SplitString(
+      kEncodings, ";", base::TRIM_WHITESPACE, base::SPLIT_WANT_ALL);
   data.alternate_urls.push_back(kAlternateURL);
   data.search_terms_replacement_key = kSearchTermsReplacementKey;
   scoped_ptr<TemplateURL> expected_managed_default(new TemplateURL(data));
@@ -1235,7 +1237,8 @@ TEST_F(TemplateURLServiceTest, TestManagedDefaultSearch) {
   data.SetURL(kSearchURL);
   data.favicon_url = GURL(kIconURL);
   data.show_in_default_list = true;
-  base::SplitString(kEncodings, ';', &data.input_encodings);
+  data.input_encodings = base::SplitString(
+      kEncodings, ";", base::TRIM_WHITESPACE, base::SPLIT_WANT_ALL);
   data.alternate_urls.push_back(kAlternateURL);
   data.search_terms_replacement_key = kSearchTermsReplacementKey;
   scoped_ptr<TemplateURL> expected_managed_default1(new TemplateURL(data));
@@ -1478,7 +1481,8 @@ TEST_F(TemplateURLServiceTest, ExtensionEngineVsPolicy) {
   data.SetURL(kSearchURL);
   data.favicon_url = GURL(kIconURL);
   data.show_in_default_list = true;
-  base::SplitString(kEncodings, ';', &data.input_encodings);
+  data.input_encodings = base::SplitString(
+      kEncodings, ";", base::TRIM_WHITESPACE, base::SPLIT_WANT_ALL);
   data.alternate_urls.push_back(kAlternateURL);
   data.search_terms_replacement_key = kSearchTermsReplacementKey;
   scoped_ptr<TemplateURL> expected_managed_default(new TemplateURL(data));
