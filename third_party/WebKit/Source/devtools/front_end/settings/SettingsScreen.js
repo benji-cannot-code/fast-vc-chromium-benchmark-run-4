cfe71b37c456db83f24119378aa46bd3b4242178
pfeldman@chromium.org <pfeldman@chromium.org>
2015-07-23 21:48:39 +0000

    DevTools: place dock and help items into the new menu.
    BUG=512672
    
    Review URL: https://codereview.chromium.org/1252033002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199402 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/settings/SettingsScreen.js
+++ third_party/WebKit/Source/devtools/front_end/settings/SettingsScreen.js
@@ -586,7 +586,10 @@ WebInspector.SettingsController.ActionDelegate.prototype = {
      */
     handleAction: function(context, actionId)
     {
-        WebInspector._settingsController.showSettingsScreen();
+        if (actionId === "settings.show")
+            WebInspector._settingsController.showSettingsScreen();
+        else if (actionId === "help.show")
+            InspectorFrontendHost.openInNewTab("https://developer.chrome.com/devtools");
     }
 }
 
