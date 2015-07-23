5eff5f1227090814fc0adadeba2d098dafa7a0d7
knn <knn@chromium.org>
2015-07-23 12:54:25 -0700

    Do not fetch child account status for supervised users.
    Supervised users have a specially scoped token that when used for
    general purposes throw spurious errors.
    
    Also  additional minor change: use child_request_account_id_ instead of
    child_info_request_ everywhere for consistency.
    
    TEST=manually verified from bug
    
    BUG=513213
    
    Review URL: https://codereview.chromium.org/1254533004
    
    Cr-Commit-Position: refs/heads/master@{#340148}

--- components/signin/core/browser/account_fetcher_service.cc
+++ components/signin/core/browser/account_fetcher_service.cc
@@ -23,6 +23,14 @@ namespace {
 const base::TimeDelta kRefreshFromTokenServiceDelay =
     base::TimeDelta::FromHours(24);
 
+bool AccountSupportsUserInfo(const std::string& account_id) {
+  // Supervised users use a specially scoped token which when used for general
+  // purposes causes the token service to raise spurious auth errors.
+  // TODO(treib): this string is also used in supervised_user_constants.cc.
+  // Should put in a common place.
+  return account_id != "managed_user@localhost";
+}
+
 #if !defined(OS_ANDROID) && !defined(OS_IOS)
 // IsRefreshTokenDeviceIdExperimentEnabled is called from
 // SendRefreshTokenAnnotationRequest only on desktop platforms.
@@ -122,7 +130,7 @@ void AccountFetcherService::UpdateChildInfo() {
   if (accounts.size() == 1) {
     if (accounts[0] == child_request_account_id_)
       return;
-    if (child_info_request_)
+    if (!child_request_account_id_.empty())
       ResetChildInfo();
     StartFetchingChildInfo(accounts[0]);
   } else {
@@ -175,6 +183,8 @@ void AccountFetcherService::StartFetchingUserInfo(
 // Starts fetching whether this is a child account. Handles refresh internally.
 void AccountFetcherService::StartFetchingChildInfo(
     const std::string& account_id) {
+  if (!AccountSupportsUserInfo(account_id))
+    return;
   child_request_account_id_ = account_id;
   child_info_request_.reset(new ChildAccountInfoFetcher(
       token_service_, signin_client_->GetURLRequestContext(), this,
@@ -205,12 +215,7 @@ void AccountFetcherService::RefreshAccountInfo(const std::string& account_id,
   account_tracker_service_->StartTrackingAccount(account_id);
   const AccountTrackerService::AccountInfo& info =
       account_tracker_service_->GetAccountInfo(account_id);
-
-  // Don't bother fetching for supervised users since this causes the token
-  // service to raise spurious auth errors.
-  // TODO(treib): this string is also used in supervised_user_constants.cc.
-  // Should put in a common place.
-  if (account_id == "managed_user@localhost")
+  if (!AccountSupportsUserInfo(account_id))
     return;
 
 // |only_fetch_if_invalid| is false when the service is due for a timed update.
