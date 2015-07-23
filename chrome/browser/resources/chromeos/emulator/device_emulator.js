2e5baa181124a215bc40726a11b2ba24f272d6e5
mozartalouis <mozartalouis@google.com>
2015-07-23 12:13:00 -0700

    Connecting the Ui to the fake battery implementation BUG=510991 R=oshima@chromium.org
    Review URL: https://codereview.chromium.org/1246553002
    
    Cr-Commit-Position: refs/heads/master@{#340134}

--- chrome/browser/resources/chromeos/emulator/device_emulator.js
+++ chrome/browser/resources/chromeos/emulator/device_emulator.js
@@ -10,8 +10,7 @@ cr.define('device_emulator', function() {
 
   function initialize() {
     batterySettings = $('battery-settings');
-    chrome.send('requestBatteryInfo');
-    chrome.send('requestExternalPowerOptions');
+    chrome.send('requestPowerInfo');
 
     document.querySelector('.menu-item-toggle')
         .addEventListener('click', function(e) {
