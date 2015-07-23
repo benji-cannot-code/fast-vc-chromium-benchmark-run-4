4a0f772ac1a20f90a813bf03184d77ac10f24fc5
dbeam <dbeam@chromium.org>
2015-07-23 12:00:43 -0700

    Fix some issues with MD downloads toolbar.
    - Change search input placeholder
    - Fix markup error that was hiding clear search (X)
    - Tweak the margins and alignment method
    - Remove use of <paper-toolbar> (wasn't buying us anything)
    
    R=michaelpg@chromium.org
    BUG=425626
    
    Review URL: https://codereview.chromium.org/1250523002
    
    Cr-Commit-Position: refs/heads/master@{#340128}

--- chrome/browser/ui/webui/downloads_ui.cc
+++ chrome/browser/ui/webui/downloads_ui.cc
@@ -42,7 +42,6 @@ content::WebUIDataSource* CreateDownloadsUIHTMLSource(Profile* profile) {
       content::WebUIDataSource::Create(chrome::kChromeUIDownloadsHost);
 
   source->AddLocalizedString("title", IDS_DOWNLOAD_TITLE);
-  source->AddLocalizedString("searchButton", IDS_DOWNLOAD_SEARCH_BUTTON);
   source->AddLocalizedString("searchResultsFor", IDS_DOWNLOAD_SEARCHRESULTSFOR);
   source->AddLocalizedString("downloads", IDS_DOWNLOAD_TITLE);
   source->AddLocalizedString("clearAll", IDS_DOWNLOAD_LINK_CLEAR_ALL);
@@ -98,6 +97,8 @@ content::WebUIDataSource* CreateDownloadsUIHTMLSource(Profile* profile) {
                           IDR_DOWNLOADS_THROTTLED_ICON_LOADER_JS);
 
   if (switches::MdDownloadsEnabled()) {
+    source->AddLocalizedString("search", IDS_MD_DOWNLOAD_SEARCH);
+
     source->AddResourcePath("action_service.html",
                             IDR_MD_DOWNLOADS_ACTION_SERVICE_HTML);
     source->AddResourcePath("action_service.js",
@@ -116,6 +117,8 @@ content::WebUIDataSource* CreateDownloadsUIHTMLSource(Profile* profile) {
     source->AddResourcePath("toolbar.js", IDR_MD_DOWNLOADS_TOOLBAR_JS);
     source->SetDefaultResource(IDR_MD_DOWNLOADS_DOWNLOADS_HTML);
   } else {
+    source->AddLocalizedString("searchButton", IDS_DOWNLOAD_SEARCH_BUTTON);
+
     source->AddResourcePath("item_view.js", IDR_DOWNLOADS_ITEM_VIEW_JS);
     source->AddResourcePath("focus_row.js", IDR_DOWNLOADS_FOCUS_ROW_JS);
     source->AddResourcePath("manager.js", IDR_DOWNLOADS_MANAGER_JS);
