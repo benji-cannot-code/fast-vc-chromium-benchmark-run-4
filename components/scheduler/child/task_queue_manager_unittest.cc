7f5bb6b63718c42375ced577590c27c4e09fb9b1
skyostil <skyostil@chromium.org>
2015-07-23 12:08:39 -0700

    scheduler: Don't run multiple tasks per batch in a nested run loop
    If the scheduler is in a nested run loop, it mustn't run multiple tasks
    per batch. This is because one of those tasks could be a call to
    RunLoop::Quit(), which means we have to yield control back to the
    message loop. Since we have no direct way of observing when Quit is
    called, just limit the number of tasks per batch to 1 in nested run
    loops.
    
    BUG=444764
    
    Review URL: https://codereview.chromium.org/1252143002
    
    Cr-Commit-Position: refs/heads/master@{#340133}

--- components/scheduler/child/task_queue_manager_unittest.cc
+++ components/scheduler/child/task_queue_manager_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/scheduler/child/task_queue_manager.h"
 
 #include "base/location.h"
+#include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/test/simple_test_tick_clock.h"
 #include "base/threading/thread.h"
@@ -996,6 +997,37 @@ TEST_F(TaskQueueManagerTest, DelayedTaskWithAbsoluteRunTime) {
   test_task_runner_->RunUntilIdle();
 
   EXPECT_THAT(run_order, ElementsAre(1, 2, 3, 4));
+}
+
+void CheckIsNested(bool* is_nested) {
+  *is_nested = base::MessageLoop::current()->IsNested();
+}
+
+void PostAndQuitFromNestedRunloop(base::RunLoop* run_loop,
+                                  base::SingleThreadTaskRunner* runner,
+                                  bool* was_nested) {
+  base::MessageLoop::ScopedNestableTaskAllower allow(
+      base::MessageLoop::current());
+  runner->PostTask(FROM_HERE, run_loop->QuitClosure());
+  runner->PostTask(FROM_HERE, base::Bind(&CheckIsNested, was_nested));
+  run_loop->Run();
+}
+
+TEST_F(TaskQueueManagerTest, QuitWhileNested) {
+  // This test makes sure we don't continue running a work batch after a nested
+  // run loop has been exited in the middle of the batch.
+  InitializeWithRealMessageLoop(1u);
+  manager_->SetWorkBatchSize(2);
+
+  bool was_nested = true;
+  base::RunLoop run_loop;
+  runners_[0]->PostTask(
+      FROM_HERE,
+      base::Bind(&PostAndQuitFromNestedRunloop, base::Unretained(&run_loop),
+                 runners_[0], base::Unretained(&was_nested)));
+
+  message_loop_->RunUntilIdle();
+  EXPECT_FALSE(was_nested);
 }
 
 }  // namespace scheduler
