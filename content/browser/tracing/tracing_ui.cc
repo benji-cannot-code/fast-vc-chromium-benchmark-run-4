f5c391973c8d1418e7fe4c11a522fdc91633b510
simonhatch <simonhatch@chromium.org>
2015-07-23 06:30:44 -0700

    Include Chrome Version + Command Line info in traces from about:tracing.
    This was previously exported with traces, but lost at some point. This CL adds the chrome product version and the command line as part of the metadata when traces are saved manually via about:tracing.
    
    This does not affect traces saved other ways (ie. via devtools, startup tracing)
    
    Example output:
    
    metadata
    
    {command_line: "./out/Release/chrome --flag-switches-begin --flag-switches-end",
     version: "Chrome/46.0.2462.0"}
    
    BUG=360897
    
    Review URL: https://codereview.chromium.org/1249663003
    
    Cr-Commit-Position: refs/heads/master@{#340083}

--- content/browser/tracing/tracing_ui.cc
+++ content/browser/tracing/tracing_ui.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/command_line.h"
 #include "base/format_macros.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
@@ -200,6 +201,20 @@ void TracingCallbackWrapperBase64(
   callback.Run(data_base64);
 }
 
+std::string GenerateMetadataJSON() {
+  // Serialize metadata to json.
+  scoped_ptr<base::DictionaryValue> metadata_dict(new base::DictionaryValue());
+  metadata_dict->SetString("version", GetContentClient()->GetProduct());
+  metadata_dict->SetString(
+      "command_line",
+      base::CommandLine::ForCurrentProcess()->GetCommandLineString());
+
+  std::string results;
+  if (base::JSONWriter::Write(*metadata_dict.get(), &results))
+    return results;
+  return std::string();
+}
+
 bool OnBeginJSONRequest(const std::string& path,
                         const WebUIDataSource::GotDataCallback& callback) {
   if (path == "json/categories") {
@@ -222,10 +237,12 @@ bool OnBeginJSONRequest(const std::string& path,
         base::Bind(OnTraceBufferStatusResult, callback));
   }
   if (path == "json/end_recording_compressed") {
-    return TracingController::GetInstance()->DisableRecording(
+    scoped_refptr<TracingControllerImpl::TraceDataSink> data_sink =
         TracingController::CreateCompressedStringSink(
             TracingController::CreateCallbackEndpoint(
-                base::Bind(TracingCallbackWrapperBase64, callback))));
+                base::Bind(TracingCallbackWrapperBase64, callback)));
+    data_sink->SetMetadata(GenerateMetadataJSON());
+    return TracingController::GetInstance()->DisableRecording(data_sink);
   }
 
   const char* enableMonitoringPath = "json/begin_monitoring?";
@@ -238,10 +255,12 @@ bool OnBeginJSONRequest(const std::string& path,
         base::Bind(OnMonitoringDisabled, callback));
   }
   if (path == "json/capture_monitoring_compressed") {
-    TracingController::GetInstance()->CaptureMonitoringSnapshot(
+    scoped_refptr<TracingControllerImpl::TraceDataSink> data_sink =
         TracingController::CreateCompressedStringSink(
             TracingController::CreateCallbackEndpoint(
-                base::Bind(TracingCallbackWrapperBase64, callback))));
+                base::Bind(TracingCallbackWrapperBase64, callback)));
+    data_sink->SetMetadata(GenerateMetadataJSON());
+    TracingController::GetInstance()->CaptureMonitoringSnapshot(data_sink);
     return true;
   }
   if (path == "json/get_monitoring_status") {
