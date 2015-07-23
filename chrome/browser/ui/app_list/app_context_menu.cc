639e1ed5a7f6b07fccd5c160aac4b815a695afee
apacible <apacible@chromium.org>
2015-07-22 19:35:24 -0700

    Fix misspelling in app_context_menu.cc/h.
    Review URL: https://codereview.chromium.org/1248613002
    
    Cr-Commit-Position: refs/heads/master@{#340034}

--- chrome/browser/ui/app_list/app_context_menu.cc
+++ chrome/browser/ui/app_list/app_context_menu.cc
@@ -249,7 +249,7 @@ bool AppContextMenu::IsCommandIdEnabled(int command_id) const {
 
 bool AppContextMenu::GetAcceleratorForCommandId(
     int command_id,
-    ui::Accelerator* acclelrator) {
+    ui::Accelerator* accelerator) {
   return false;
 }
 
