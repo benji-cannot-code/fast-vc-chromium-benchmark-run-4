a19b784f4ca905e1006323a3e3468a77ed4a1ba0
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-07-23 12:17:15 -0700

    [Views] Fix crash in running nested menus from the keyboard
    Return false from MenuController::OnKeyDown() in the case of running a nested
    message loop menu. This is in-line with our behavior for other "execution"
    key down handling (like running the item with enter, or hitting the escape key),
    and prevents the MenuEventDispatcher from continuing to try and process the
    key.
    
    BUG=512415
    
    Review URL: https://codereview.chromium.org/1249853002
    
    Cr-Commit-Position: refs/heads/master@{#340137}

--- ui/views/controls/menu/menu_controller.cc
+++ ui/views/controls/menu/menu_controller.cc
@@ -1105,6 +1105,7 @@ bool MenuController::OnKeyDown(ui::KeyboardCode key_code) {
       if (hot_view) {
         hot_view->ShowContextMenu(hot_view->GetKeyboardContextMenuLocation(),
                                   ui::MENU_SOURCE_KEYBOARD);
+        return false;
       } else if (pending_state_.item->enabled() &&
                  pending_state_.item->GetRootMenuItem() !=
                      pending_state_.item) {
@@ -1116,6 +1117,7 @@ bool MenuController::OnKeyDown(ui::KeyboardCode key_code) {
         ShowContextMenu(pending_state_.item,
                         pending_state_.item->GetKeyboardContextMenuLocation(),
                         ui::MENU_SOURCE_KEYBOARD);
+        return false;
       }
       break;
     }
