89081b0ea194a750c05973635fd94449fbe0b624
ccameron@chromium.org <ccameron@chromium.org>
2015-07-23 04:49:57 +0000

    Mac: Remove event latching for momentum events
    This behavior is not consistent with how flings are done on other
    platforms, so remove it. There is some debate as to whether or not
    latching should be done on all event types.
    
    BUG=312427
    
    Review URL: https://codereview.chromium.org/1230253002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199353 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/input/EventHandler.h
+++ third_party/WebKit/Source/core/input/EventHandler.h
@@ -401,9 +401,6 @@ private:
     PlatformMouseEvent m_mouseDown;
     RefPtr<UserGestureToken> m_lastMouseDownUserGestureToken;
 
-    RefPtrWillBeMember<Node> m_latchedWheelEventNode;
-    bool m_widgetIsLatched;
-
     RefPtrWillBeMember<Node> m_previousWheelScrolledNode;
 
     // The target of each active touch point indexed by the touch ID.
