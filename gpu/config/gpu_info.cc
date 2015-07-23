f667389100f5dd33d9b13e411fbff11e13364872
bajones <bajones@chromium.org>
2015-07-23 15:57:35 -0700

    Allow WebGL on Android if the GPU has it's own process
    BUG=512992
    
    Review URL: https://codereview.chromium.org/1247093006
    
    Cr-Commit-Position: refs/heads/master@{#340198}

--- gpu/config/gpu_info.cc
+++ gpu/config/gpu_info.cc
@@ -66,6 +66,7 @@ GPUInfo::GPUInfo()
       direct_rendering(true),
       sandboxed(false),
       process_crash_count(0),
+      in_process_gpu(true),
       basic_info_state(kCollectInfoNone),
       context_info_state(kCollectInfoNone),
 #if defined(OS_WIN)
@@ -107,6 +108,7 @@ void GPUInfo::EnumerateFields(Enumerator* enumerator) const {
     bool direct_rendering;
     bool sandboxed;
     int process_crash_count;
+    bool in_process_gpu;
     CollectInfoResult basic_info_state;
     CollectInfoResult context_info_state;
 #if defined(OS_WIN)
@@ -167,6 +169,7 @@ void GPUInfo::EnumerateFields(Enumerator* enumerator) const {
   enumerator->AddBool("directRendering", direct_rendering);
   enumerator->AddBool("sandboxed", sandboxed);
   enumerator->AddInt("processCrashCount", process_crash_count);
+  enumerator->AddBool("inProcessGpu", in_process_gpu);
   enumerator->AddInt("basicInfoState", basic_info_state);
   enumerator->AddInt("contextInfoState", context_info_state);
 #if defined(OS_WIN)
