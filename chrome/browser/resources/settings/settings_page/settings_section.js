9d34daa3458cdf812ccddd9b24a8297f9324d04d
tommycli <tommycli@chromium.org>
2015-07-23 13:44:50 -0700

    Settings Rewrite: Make a common settings-section component.
    Adds a common settings-section component for sections within the new settings.
    
    This component displays the correct icon and header for each section.
    
    BUG=425627
    NOPRESUBMIT=true
    (PRESUBMIT has a false positive here)
    
    Review URL: https://codereview.chromium.org/1241363002
    
    Cr-Commit-Position: refs/heads/master@{#340159}

---
+++ chrome/browser/resources/settings/settings_page/settings_section.js
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * 'cr-settings-section' shows a paper material themed section with a header
+ * which shows its page title and icon.
+ *
+ * Example:
+ *
+ *    <cr-settings-section page-title="[[pageTitle]]" icon="[[icon]]">
+ *      <!-- Insert your section controls here -->
+ *    </cr-settings-section>
+ *
+ * @group Chrome Settings Elements
+ * @element cr-settings-section
+ */
+Polymer({
+  is: 'cr-settings-section',
+
+  properties: {
+    /**
+     * Title for the page header and navigation menu.
+     */
+    pageTitle: String,
+
+    /**
+     * Name of the 'iron-icon' to show.
+     */
+    icon: String,
+  },
+});
