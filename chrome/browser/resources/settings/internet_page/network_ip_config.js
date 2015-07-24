31859749fc1c6d96e180c8e1546355d9febd3c68
stevenjb <stevenjb@chromium.org>
2015-07-24 12:30:05 -0700

    Add network-apnlist to md-settings/internet
    This adds support for selecting an existing Cellular APN
    or specifying a custom one.
    
    BUG=481563
    
    Review URL: https://codereview.chromium.org/1246993004
    
    Cr-Commit-Position: refs/heads/master@{#340312}

--- chrome/browser/resources/settings/internet_page/network_ip_config.js
+++ chrome/browser/resources/settings/internet_page/network_ip_config.js
@@ -56,6 +56,7 @@ Polymer({
 
     /**
      * Array of properties to pass to the property list.
+     * @type {!Array<string>}
      */
     ipConfigFields_: {
       type: Array,
@@ -192,8 +193,6 @@ Polymer({
    * @private
    */
   onIPChange_: function(event) {
-    event.stopPropagation();
-
     var field = event.detail.field;
     var value = event.detail.value;
     // Note: |field| includes the 'ipv4.' prefix.
