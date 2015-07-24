99da9afd3837ebb1f82aef46089d717d5d415638
brianderson <brianderson@chromium.org>
2015-07-24 14:13:10 -0700

    cc: Don't record first two frames in CompositorTimingHistory
    Ignore the first two frames after becoming visible,
    creating a new output surface, or coming out of idle,
    since they are likely to be anomalies.
    
    BUG=406158, 347366
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1203203003
    
    Cr-Commit-Position: refs/heads/master@{#340337}

--- cc/scheduler/scheduler.cc
+++ cc/scheduler/scheduler.cc
@@ -277,6 +277,7 @@ void Scheduler::SetupNextBeginFrameIfNeeded() {
       frame_source_->SetNeedsBeginFrames(true);
       devtools_instrumentation::NeedsBeginFrameChanged(layer_tree_host_id_,
                                                        true);
+      UpdateCompositorTimingHistoryRecordingEnabled();
     } else if (state_machine_.begin_impl_frame_state() ==
                SchedulerStateMachine::BEGIN_IMPL_FRAME_STATE_IDLE) {
       // Call SetNeedsBeginFrames(false) in between frames only.
@@ -284,6 +285,7 @@ void Scheduler::SetupNextBeginFrameIfNeeded() {
       client_->SendBeginMainFrameNotExpectedSoon();
       devtools_instrumentation::NeedsBeginFrameChanged(layer_tree_host_id_,
                                                        false);
+      UpdateCompositorTimingHistoryRecordingEnabled();
     }
   }
 
@@ -722,8 +724,6 @@ void Scheduler::AsValueInto(base::trace_event::TracedValue* state) const {
   state->BeginDictionary("scheduler_state");
   state->SetDouble("estimated_parent_draw_time_ms",
                    estimated_parent_draw_time_.InMillisecondsF());
-  state->SetBoolean("last_set_needs_begin_frame_",
-                    frame_source_->NeedsBeginFrames());
   state->SetInteger("begin_retro_frame_args",
                     static_cast<int>(begin_retro_frame_args_.size()));
   state->SetBoolean("begin_retro_frame_task",
@@ -749,7 +749,8 @@ void Scheduler::AsValueInto(base::trace_event::TracedValue* state) const {
 
 void Scheduler::UpdateCompositorTimingHistoryRecordingEnabled() {
   compositor_timing_history_->SetRecordingEnabled(
-      state_machine_.HasInitializedOutputSurface() && state_machine_.visible());
+      state_machine_.HasInitializedOutputSurface() &&
+      state_machine_.visible() && frame_source_->NeedsBeginFrames());
 }
 
 bool Scheduler::ShouldRecoverMainLatency(const BeginFrameArgs& args) const {
