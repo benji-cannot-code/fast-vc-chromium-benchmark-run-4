45f3e4ea05215ad41542f1d8c65d5011f61b18f5
zmo@chromium.org <zmo@chromium.org>
2015-07-24 23:39:13 +0000

    Upgrade readPixels to work in WebGL 2 (reland)
    This has to be done before we can test INTEGER textures.
    
    BUG=295792
    TEST=webgl conformance 1/2
    R=bajones@chromium.org,kbr@chromium.org
    
    Review URL: https://codereview.chromium.org/1258683005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199469 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webgl/WebGLFramebuffer.cpp
+++ third_party/WebKit/Source/modules/webgl/WebGLFramebuffer.cpp
@@ -141,7 +141,6 @@ namespace {
 
     GLenum WebGLRenderbufferAttachment::type() const
     {
-        notImplemented();
         return 0;
     }
 
@@ -647,6 +646,20 @@ GLenum WebGLFramebuffer::getDrawBuffer(GLenum drawBuffer)
     if (drawBuffer == GL_DRAW_BUFFER0_EXT)
         return GL_COLOR_ATTACHMENT0;
     return GL_NONE;
+}
+
+bool WebGLFramebuffer::getReadBufferFormatAndType(GLenum* format, GLenum* type) const
+{
+    if (m_readBuffer == GL_NONE)
+        return false;
+    WebGLAttachment* image = getAttachment(m_readBuffer);
+    if (!image)
+        return false;
+    if (format)
+        *format = image->format();
+    if (type)
+        *type = image->type();
+    return true;
 }
 
 DEFINE_TRACE(WebGLFramebuffer)
