61de1753e8000a8b756fb8ba29b88c7da3045293
Daniel Cheng <dcheng@chromium.org>
2015-07-24 15:40:48 -0700

    Cleanup some calls to WebWidget methods with an empty implementation.
    BUG=none
    
    Review URL: https://codereview.chromium.org/1255983003 .
    
    Cr-Commit-Position: refs/heads/master@{#340360}

--- content/renderer/render_widget.h
+++ content/renderer/render_widget.h
@@ -507,7 +507,6 @@ class CONTENT_EXPORT RenderWidget
   // state.
   void SetHidden(bool hidden);
 
-  void WillToggleFullscreen();
   void DidToggleFullscreen();
 
   bool next_paint_is_resize_ack() const;
