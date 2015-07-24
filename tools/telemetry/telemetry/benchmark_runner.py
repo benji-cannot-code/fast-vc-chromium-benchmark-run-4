45744246204f67c781bbf90a6b91159c01df97ff
deanliao <deanliao@chromium.org>
2015-07-24 01:26:08 -0700

    record_wpr: better handling target not found error.
    In the past, when target (either benchmark or story set) is not found,
    record_wpr just response a usage. User doesn't know what's wrong without
    tracing the code. Now just like run_benchmark, if a target is not found,
    it tries to match benchmarks/story sets with similar names and give
    user a hint if found.
    
    For --list-benchmarks and --list-stories, pretty print the list with
    description.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1226093003
    
    Cr-Commit-Position: refs/heads/master@{#340239}

--- tools/telemetry/telemetry/benchmark_runner.py
+++ tools/telemetry/telemetry/benchmark_runner.py
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Handles benchmark configuration, but all the logic for
 actually running the benchmark is in Benchmark and PageRunner."""
 
-import difflib
 import hashlib
 import inspect
 import json
@@ -66,32 +65,6 @@ def PrintBenchmarkList(benchmarks, possible_browser, output_pipe=sys.stdout):
   print >> output_pipe, (
       'Pass --browser to list benchmarks for another browser.')
   print >> output_pipe
-
-def GetMostLikelyMatchedBenchmarks(all_benchmarks, input_benchmark_name):
-  """ Returns the list of benchmarks whose name most likely matched with
-    |input_benchmark_name|.
-
-  Args:
-    all_benchmarks: the list of benchmark classes.
-    input_benchmark_name: a string to be matched against the names of benchmarks
-      in |all_benchmarks|.
-
-  Returns:
-    A list of benchmark classes whose name likely matched
-    |input_benchmark_name|. Benchmark classes are arranged in descending order
-    of similarity between their names to |input_benchmark_name|.
-  """
-  def MatchedWithBenchmarkInputNameScore(benchmark_class):
-    return difflib.SequenceMatcher(
-        isjunk=None,
-        a=benchmark_class.Name(), b=input_benchmark_name).ratio()
-  benchmarks_with_similar_names = [
-      b for b in all_benchmarks if
-      MatchedWithBenchmarkInputNameScore(b) > 0.4]
-  ordered_list = sorted(benchmarks_with_similar_names,
-                        key=MatchedWithBenchmarkInputNameScore,
-                        reverse=True)
-  return ordered_list
 
 
 class ProjectConfig(object):
@@ -240,8 +213,8 @@ class Run(command_line.OptparseCommand):
     if not matching_benchmarks:
       print >> sys.stderr, 'No benchmark named "%s".' % input_benchmark_name
       print >> sys.stderr
-      most_likely_matched_benchmarks = GetMostLikelyMatchedBenchmarks(
-          all_benchmarks, input_benchmark_name)
+      most_likely_matched_benchmarks = command_line.GetMostLikelyMatchedObject(
+          all_benchmarks, input_benchmark_name, lambda x: x.Name())
       if most_likely_matched_benchmarks:
         print >> sys.stderr, 'Do you mean any of those benchmarks below?'
         PrintBenchmarkList(most_likely_matched_benchmarks, None, sys.stderr)
