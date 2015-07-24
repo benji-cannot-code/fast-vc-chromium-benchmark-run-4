79884ffafc360b918765cd40d66b500b6bc1e36e
mlamouri <mlamouri@chromium.org>
2015-07-24 16:24:12 -0700

    Presentation API: availability callback no longer owns boolean.
    With https://codereview.chromium.org/1249983003, the callback will
    no longer owns the boolean it takes in case of success.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1251923002
    
    Cr-Commit-Position: refs/heads/master@{#340374}

--- content/renderer/presentation/presentation_dispatcher.cc
+++ content/renderer/presentation/presentation_dispatcher.cc
@@ -289,7 +289,7 @@ void PresentationDispatcher::getAvailability(
     const blink::WebString& presentationUrl,
     blink::WebPresentationAvailabilityCallbacks* callbacks) {
   if (listening_state_ == ListeningState::Active) {
-    callbacks->onSuccess(new bool(last_known_availability_));
+    callbacks->onSuccess(&last_known_availability_);
     delete callbacks;
     return;
   }
@@ -343,7 +343,7 @@ void PresentationDispatcher::OnScreenAvailabilityUpdated(bool available) {
 
   for (AvailabilityCallbacksMap::iterator iter(&availability_callbacks_);
        !iter.IsAtEnd(); iter.Advance()) {
-    iter.GetCurrentValue()->onSuccess(new bool(available));
+    iter.GetCurrentValue()->onSuccess(&available);
   }
   availability_callbacks_.Clear();
 
