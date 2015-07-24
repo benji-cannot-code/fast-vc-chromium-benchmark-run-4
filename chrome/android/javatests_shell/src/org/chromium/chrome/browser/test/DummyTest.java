e71f3ee8f6db96515492341a095e0fd8d4334229
alancutter <alancutter@chromium.org>
2015-07-24 03:44:33 -0700

    Revert of Move DummyTest from ChromeShellTest to ChromePublicTest (patchset #1 id:1 of https://codereview.chromium.org/1243773005/)
    Reason for revert:
    Revert patch suspected for causing test failure
    
    Test: HostDriven_DummyTest.testPass
    Builder: Android Tests (dbg)
    Build: https://build.chromium.org/p/chromium.webkit/builders/Android%20Tests%20%28dbg%29/builds/27205
    
    Original issue's description:
    > Move DummyTest from ChromeShellTest to ChromePublicTest
    >
    > BUG=505040
    > TEST=None
    >
    > Committed: https://crrev.com/e51c61f0fe65248104069ba5d9c2e489383cd530
    > Cr-Commit-Position: refs/heads/master@{#340121}
    
    TBR=jbudorick@chromium.org,tedchoc@chromium.org,pkotwicz@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=505040
    
    Review URL: https://codereview.chromium.org/1256813002
    
    Cr-Commit-Position: refs/heads/master@{#340252}

--- chrome/android/javatests_shell/src/org/chromium/chrome/browser/test/DummyTest.java
+++ chrome/android/javatests_shell/src/org/chromium/chrome/browser/test/DummyTest.java
@@ -6,20 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.test;
 
 import org.chromium.base.test.util.HostDrivenTest;
-import org.chromium.chrome.browser.ChromeActivity;
-import org.chromium.chrome.test.ChromeActivityTestCaseBase;
+import org.chromium.chrome.shell.ChromeShellTestBase;
 
 /**
  * Dummy test suite for verifying the host-driven test framework.
  */
-public class DummyTest extends ChromeActivityTestCaseBase<ChromeActivity> {
-    public DummyTest() {
-        super(ChromeActivity.class);
-    }
-
-    @Override
-    public void startMainActivity() {}
-
+public class DummyTest extends ChromeShellTestBase {
     @HostDrivenTest
     public void testPass() {}
 }
