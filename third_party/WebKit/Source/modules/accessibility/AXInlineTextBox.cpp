d238f9411b8a87b5a941968789cba7fb9b66aa99
nektar@chromium.org <nektar@chromium.org>
2015-07-24 04:23:37 +0000

    Implements the ability to get and set the caret position and the current selection from anywhere in the accessibility tree.
    This is required in order to support contenteditables on some platforms.
    BUG=491027
    
    Review URL: https://codereview.chromium.org/1185343003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199415 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/accessibility/AXInlineTextBox.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXInlineTextBox.cpp
@@ -102,7 +102,7 @@ void AXInlineTextBox::textCharacterOffsets(Vector<int>& offsets) const
     }
 }
 
-void AXInlineTextBox::wordBoundaries(Vector<PlainTextRange>& words) const
+void AXInlineTextBox::wordBoundaries(Vector<AXRange>& words) const
 {
     if (!m_inlineTextBox)
         return;
@@ -111,7 +111,7 @@ void AXInlineTextBox::wordBoundaries(Vector<PlainTextRange>& words) const
     m_inlineTextBox->wordBoundaries(wordBoundaries);
     words.resize(wordBoundaries.size());
     for (unsigned i = 0; i < wordBoundaries.size(); i++)
-        words[i] = PlainTextRange(wordBoundaries[i].startIndex, wordBoundaries[i].endIndex - wordBoundaries[i].startIndex);
+        words[i] = AXRange(wordBoundaries[i].startIndex, wordBoundaries[i].endIndex);
 }
 
 String AXInlineTextBox::stringValue() const
--- third_party/WebKit/Source/modules/accessibility/AXLayoutObject.h
+++ third_party/WebKit/Source/modules/accessibility/AXLayoutObject.h
@@ -44,7 +44,6 @@ class FrameView;
 class HTMLAreaElement;
 class IntPoint;
 class Node;
-class VisibleSelection;
 class Widget;
 
 class MODULES_EXPORT AXLayoutObject : public AXNodeObject {
@@ -152,6 +151,12 @@ protected:
     // Accessibility Text - (To be deprecated).
     String deprecatedHelpText() const override;
 
+    // Methods that retrieve or manipulate the current selection.
+
+    AXRange selection() const override;
+    AXRange selectionUnderObject() const override;
+    void setSelection(const AXRange&) override;
+
     // Location and click point in frame-relative coordinates.
     void markCachedElementRectDirty() const override;
     IntPoint clickPoint() override;
@@ -184,11 +189,6 @@ protected:
     Element* anchorElement() const override;
     Widget* widgetForAttachmentView() const override;
 
-    // Selected text.
-    PlainTextRange selectedTextRange() const override;
-
-    // Modify or take an action on an object.
-    void setSelectedTextRange(const PlainTextRange&) override;
     void setValue(const String&) override;
 
     // Notifications that this object may have changed.
@@ -204,9 +204,9 @@ protected:
 private:
     AXObject* treeAncestorDisallowingChild() const;
     void ariaListboxSelectedChildren(AccessibilityChildrenVector&);
-    PlainTextRange visibleSelectionUnderObject() const;
     bool nodeIsTextControl(const Node*) const;
     bool isTabItemSelected() const;
+    bool isValidSelectionBound(const AXObject*) const;
     AXObject* accessibilityImageMapHitTest(HTMLAreaElement*, const IntPoint&) const;
     LayoutObject* layoutParentObject() const;
     bool isSVGImage() const;
@@ -226,7 +226,7 @@ private:
     void ariaSelectedRows(AccessibilityChildrenVector&);
     bool elementAttributeValue(const QualifiedName&) const;
     LayoutRect computeElementRect() const;
-    VisibleSelection selection() const;
+    AXRange textControlSelection() const;
     int indexForVisiblePosition(const VisiblePosition&) const;
 };
 
