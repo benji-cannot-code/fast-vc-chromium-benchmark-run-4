c5130a843c27ad2389928ee37a8847324a63a273
tonychun <tonychun@google.com>
2015-07-24 09:22:06 -0700

    Added chromoting test environment for the chromoting tests to share data.
    I've refactored out the fake_refresh_token_store to a shared class so that both chromoting test environment and app remoting test environment can use it. I've also added a new class called fake_host_list_fetcher to fake retrieving a host list.
    
    Added a new test and made some cleanups.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1237883002
    
    Cr-Commit-Position: refs/heads/master@{#340283}

---
+++ remoting/test/fake_host_list_fetcher.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/test/fake_host_list_fetcher.h"
+
+namespace remoting {
+namespace test {
+
+FakeHostListFetcher::FakeHostListFetcher() {
+}
+
+FakeHostListFetcher::~FakeHostListFetcher() {
+}
+
+void FakeHostListFetcher::RetrieveHostlist(const std::string& access_token,
+                                           const HostlistCallback& callback) {
+  callback.Run(host_list_);
+}
+
+}  // namespace test
+}  // namespace remoting
