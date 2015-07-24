1ab4a566ce69e9251f98b6cdc5bfa51ed1212945
nednguyen <nednguyen@google.com>
2015-07-23 22:09:37 -0700

    [Telemetry] Add deprecation tag for page_test.CleanUpAfterPage hook & add page_test.DidRunPage hook
    BUG=513487
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:win_perf_bisect
    
    Review URL: https://codereview.chromium.org/1242263009
    
    Cr-Commit-Position: refs/heads/master@{#340222}

--- tools/telemetry/telemetry/page/page_test.py
+++ tools/telemetry/telemetry/page/page_test.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import logging
 
 from telemetry.core import exceptions
+from telemetry import decorators
 from telemetry.page import action_runner as action_runner_module
 
 
@@ -145,7 +146,12 @@ class PageTest(object):
     """Override to do operations right after the page is navigated and after
     all waiting for completion has occurred."""
 
+  @decorators.Deprecated(
+    2015, 8, 10, 'This hook is deprecated. Please use DidRunPage hook instead')
   def CleanUpAfterPage(self, page, tab):
+    """Called after the test run method was run, even if it failed."""
+
+  def DidRunPage(self, platform):
     """Called after the test run method was run, even if it failed."""
 
   def TabForPage(self, page, browser):   # pylint: disable=W0613
