3c769e453ef2d0e9f1c54272b49f997da2bacf20
mlamouri@chromium.org <mlamouri@chromium.org>
2015-07-24 18:25:16 +0000

    Presentation API: getAvailability() shouldn't return a boolean.
    This is fixing a regression from
    https://codereview.chromium.org/1249983003
    
    BUG=510483
    
    Review URL: https://codereview.chromium.org/1252383002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199454 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/presentation/PresentationRequest.cpp
+++ third_party/WebKit/Source/modules/presentation/PresentationRequest.cpp
@@ -107,7 +107,7 @@ ScriptPromise PresentationRequest::getAvailability(ScriptState* scriptState)
         resolver->reject(DOMException::create(InvalidStateError, "The object is no longer associated to a frame."));
         return promise;
     }
-    client->getAvailability(m_url.string(), new CallbackPromiseAdapter<bool, PresentationError>(resolver));
+    client->getAvailability(m_url.string(), new CallbackPromiseAdapter<PresentationAvailability, PresentationError>(resolver));
 
     return promise;
 }
