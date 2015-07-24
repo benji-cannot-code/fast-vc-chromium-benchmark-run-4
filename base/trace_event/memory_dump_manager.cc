470102d133f1916607e5fbb710a4840dc4445354
dcastagna <dcastagna@chromium.org>
2015-07-24 02:45:31 -0700

    Expose the current kAllocatedObjects in MemoryDumpManager.
    In order to create a suballocator that accounts for memory allocated
    using the system allocator, the current system allocator name must
    be known.
    The system allocator name varies by platform (e.g:
    "malloc/allocated_objects", "winheap/allocated_objects").
    
    This CL adds a method GetSystemAllocatorPoolName to MemoryDumpManager
    to get the current name.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1242263006
    
    Cr-Commit-Position: refs/heads/master@{#340245}

--- base/trace_event/memory_dump_manager.cc
+++ base/trace_event/memory_dump_manager.cc
@@ -97,6 +97,7 @@ MemoryDumpManager::MemoryDumpManager()
       delegate_(nullptr),
       memory_tracing_enabled_(0),
       tracing_process_id_(kInvalidTracingProcessId),
+      system_allocator_pool_name_(nullptr),
       skip_core_dumpers_auto_registration_for_testing_(false) {
   g_next_guid.GetNext();  // Make sure that first guid is not zero.
 }
@@ -121,6 +122,7 @@ void MemoryDumpManager::Initialize() {
   g_mmaps_dump_provider = ProcessMemoryMapsDumpProvider::GetInstance();
   RegisterDumpProvider(g_mmaps_dump_provider);
   RegisterDumpProvider(MallocDumpProvider::GetInstance());
+  system_allocator_pool_name_ = MallocDumpProvider::kAllocatedObjects;
 #endif
 
 #if defined(OS_ANDROID)
@@ -129,6 +131,7 @@ void MemoryDumpManager::Initialize() {
 
 #if defined(OS_WIN)
   RegisterDumpProvider(WinHeapDumpProvider::GetInstance());
+  system_allocator_pool_name_ = WinHeapDumpProvider::kAllocatedObjects;
 #endif
 }
 
