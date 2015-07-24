395b0cd4bf984ff868336bf061b8fb923177757d
glevin <glevin@chromium.org>
2015-07-24 12:56:25 -0700

    Set GAIA focus when changing pages
    BUG=499008
    TEST=Navigate forward and back through GAIA login pages.  After
    switching pages, the first input element should always have focus.
    
    Review URL: https://codereview.chromium.org/1250013002
    
    Cr-Commit-Position: refs/heads/master@{#340317}

--- chrome/browser/resources/chromeos/login/screen_gaia_signin.js
+++ chrome/browser/resources/chromeos/login/screen_gaia_signin.js
@@ -689,6 +689,7 @@ login.createScreen('GaiaSigninScreen', 'gaia-signin', function() {
     onBackButton_: function(e) {
       $('back-button-item').hidden = !e.detail;
       $('login-header-bar').updateUI_();
+      $('signin-frame').focus();
     },
 
     /**
