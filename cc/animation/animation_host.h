9f997512d1fd341378d677d719cc59cb0aab230b
ajuma <ajuma@chromium.org>
2015-07-24 14:16:34 -0700

    cc: Clean up <Property>IsAnimating and related logic
    Layers currently have two notions of having an animated property, such
    as an animated transform. HasPotentiallyRunningTransformAnimation means
    there's a transform animation that hasn't finished. TransformIsAnimating
    is a stronger condition: it means there's a transform animation that
    hasn't finished, and is currently "in effect". For animations without
    start delays, these notions are equivalent, since unfinished animations
    without a start delay are always "in effect". However, an animation
    with a start delay can be unfinished without yet being "in effect"; this
    is the case during the start delay, for animations that don't have a
    "backwards" fill-mode. Animations that aren't yet in effect don't get
    ticked.
    
    CDP and property trees use <Property>IsAnimating when deciding whether a
    property's value might change on the compositor thread. For example,
    when a layer has opacity 0, its subtree is skipped unless
    OpacityIsAnimating, and similar logic exists for uninvertible transforms
    and TransformIsAnimating. However, this doesn't correctly account for
    animations with start delays -- once the delay has finished, such
    animations will cause changes on the compositor thread. This CL replaces
    usage of <Property>IsAnimating with
    HasPotentiallyRunning<Property>Animation wherever we're really trying to
    check "will this value change on the compositor thread". In particular,
    this includes the computation of screen_space_transform_is_animating.
    
    A second problem with <Property>IsAnimating and
    HasPotentiallyRunning<Property>Animation is that on the compositor
    thread, they don't distinguish between animations that affect the
    pending tree and those that affect the active tree. This means, for
    example, that right after a commit that adds a transform animation to
    a layer, the corresponding active layer's
    screen_space_transform_is_animating value is true, even before
    activation, and even though there isn't yet any animation that affects
    it. This is unintuitive, and also awkward behavior to match when using
    property trees, since property trees propagate through commit and
    activation. This CL makes <Property>IsAnimating and
    HasPotentiallyRunning<Property>Animation take into account whether
    particular animations affect the active tree, the pending tree, or
    both.
    
    BUG=509673
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1239983006
    
    Cr-Commit-Position: refs/heads/master@{#340338}

--- cc/animation/animation_host.h
+++ cc/animation/animation_host.h
@@ -94,12 +94,17 @@ class CC_EXPORT AnimationHost {
 
   bool ScrollOffsetAnimationWasInterrupted(int layer_id) const;
 
-  bool IsAnimatingFilterProperty(int layer_id) const;
-  bool IsAnimatingOpacityProperty(int layer_id) const;
-  bool IsAnimatingTransformProperty(int layer_id) const;
+  bool IsAnimatingFilterProperty(int layer_id, LayerTreeType tree_type) const;
+  bool IsAnimatingOpacityProperty(int layer_id, LayerTreeType tree_type) const;
+  bool IsAnimatingTransformProperty(int layer_id,
+                                    LayerTreeType tree_type) const;
 
-  bool HasPotentiallyRunningOpacityAnimation(int layer_id) const;
-  bool HasPotentiallyRunningTransformAnimation(int layer_id) const;
+  bool HasPotentiallyRunningFilterAnimation(int layer_id,
+                                            LayerTreeType tree_type) const;
+  bool HasPotentiallyRunningOpacityAnimation(int layer_id,
+                                             LayerTreeType tree_type) const;
+  bool HasPotentiallyRunningTransformAnimation(int layer_id,
+                                               LayerTreeType tree_type) const;
 
   bool HasAnyAnimationTargetingProperty(
       int layer_id,
--- cc/trees/layer_tree_host.cc
+++ cc/trees/layer_tree_host.cc
@@ -1151,19 +1151,30 @@ bool LayerTreeHost::ScrollOffsetAnimationWasInterrupted(
 
 bool LayerTreeHost::IsAnimatingFilterProperty(const Layer* layer) const {
   return animation_host_
-             ? animation_host_->IsAnimatingFilterProperty(layer->id())
+             ? animation_host_->IsAnimatingFilterProperty(layer->id(),
+                                                          LayerTreeType::ACTIVE)
              : false;
 }
 
 bool LayerTreeHost::IsAnimatingOpacityProperty(const Layer* layer) const {
   return animation_host_
-             ? animation_host_->IsAnimatingOpacityProperty(layer->id())
+             ? animation_host_->IsAnimatingOpacityProperty(
+                   layer->id(), LayerTreeType::ACTIVE)
              : false;
 }
 
 bool LayerTreeHost::IsAnimatingTransformProperty(const Layer* layer) const {
   return animation_host_
-             ? animation_host_->IsAnimatingTransformProperty(layer->id())
+             ? animation_host_->IsAnimatingTransformProperty(
+                   layer->id(), LayerTreeType::ACTIVE)
+             : false;
+}
+
+bool LayerTreeHost::HasPotentiallyRunningFilterAnimation(
+    const Layer* layer) const {
+  return animation_host_
+             ? animation_host_->HasPotentiallyRunningFilterAnimation(
+                   layer->id(), LayerTreeType::ACTIVE)
              : false;
 }
 
@@ -1171,7 +1182,7 @@ bool LayerTreeHost::HasPotentiallyRunningOpacityAnimation(
     const Layer* layer) const {
   return animation_host_
              ? animation_host_->HasPotentiallyRunningOpacityAnimation(
-                   layer->id())
+                   layer->id(), LayerTreeType::ACTIVE)
              : false;
 }
 
@@ -1179,7 +1190,7 @@ bool LayerTreeHost::HasPotentiallyRunningTransformAnimation(
     const Layer* layer) const {
   return animation_host_
              ? animation_host_->HasPotentiallyRunningTransformAnimation(
-                   layer->id())
+                   layer->id(), LayerTreeType::ACTIVE)
              : false;
 }
 
