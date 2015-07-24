58d6e874c0547880d22ad27f574858c3b99bce18
tyoshino <tyoshino@chromium.org>
2015-07-24 00:21:34 -0700

    Remove try-catch from CrossOriginXHRBackgroundPage test file
    It's no longer necessary as Blink side patch
    https://codereview.chromium.org/1060113004/ has been landed.
    
    BUG=479484
    R=yhirano
    
    Review URL: https://codereview.chromium.org/1251043003
    
    Cr-Commit-Position: refs/heads/master@{#340234}

--- chrome/test/data/extensions/api_test/cross_origin_xhr/no_file_access/test.js
+++ chrome/test/data/extensions/api_test/cross_origin_xhr/no_file_access/test.js
@@ -20,10 +20,7 @@ chrome.test.getConfig(function(config) {
         chrome.test.succeed();
       }
 
-      try {
-        req.send(null);
-      } catch (e) {
-      }
+      req.send(null);
     }
   ]);
 });
