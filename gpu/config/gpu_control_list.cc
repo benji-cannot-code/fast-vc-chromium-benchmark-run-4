379346f6fedf67da37172453be8ee7e54fd68b28
bajones <bajones@chromium.org>
2015-07-24 17:41:56 -0700

    Allow WebGL on Android if the GPU has it's own process (Take 2)
    First attempt (https://codereview.chromium.org/1247093006/) was reverted due to
    failures on the Android bots. This is because the Android bots use content shell,
    not Chromium, to run the tests and that uses in-process GPU. As such the test
    were running without WebGL support.
    
    This patch adds back in the EXT_robusness exceptions, which allowed the bots to
    run WebGL content previously and won't appear to users as a regression.
    
    BUG=512992
    TBR=mkwst@chromium.org
    
    Review URL: https://codereview.chromium.org/1252943008
    
    Cr-Commit-Position: refs/heads/master@{#340385}

--- gpu/config/gpu_control_list.cc
+++ gpu/config/gpu_control_list.cc
@@ -724,6 +724,12 @@ GpuControlList::GpuControlListEntry::GetEntryFromValue(
     dictionary_entry_count++;
   }
 
+  bool in_process_gpu;
+  if (value->GetBoolean("in_process_gpu", &in_process_gpu)) {
+    entry->SetInProcessGPUInfo(in_process_gpu);
+    dictionary_entry_count++;
+  }
+
   if (top_level) {
     const base::ListValue* feature_value = NULL;
     if (value->GetList("features", &feature_value)) {
@@ -979,6 +985,10 @@ bool GpuControlList::GpuControlListEntry::SetGpuCountInfo(
 
 void GpuControlList::GpuControlListEntry::SetDirectRenderingInfo(bool value) {
   direct_rendering_info_.reset(new BoolInfo(value));
+}
+
+void GpuControlList::GpuControlListEntry::SetInProcessGPUInfo(bool value) {
+  in_process_gpu_info_.reset(new BoolInfo(value));
 }
 
 bool GpuControlList::GpuControlListEntry::SetFeatures(
@@ -1243,6 +1253,9 @@ bool GpuControlList::GpuControlListEntry::Contains(
     return false;
   if (direct_rendering_info_.get() != NULL &&
       !direct_rendering_info_->Contains(gpu_info.direct_rendering))
+    return false;
+  if (in_process_gpu_info_.get() != NULL &&
+      !in_process_gpu_info_->Contains(gpu_info.in_process_gpu))
     return false;
   if (!cpu_brand_.empty()) {
     base::CPU cpu_info;
