cb676d8d24fdadf5991b7b11ba3587cf142619c1
plundblad <plundblad@chromium.org>
2015-07-25 01:54:41 -0700

    Add 9 more languages to the built-in network text to speech engine.
    BUG=513181
    TESTED=Updated relevant browser test, manually tried all the voices from an extension. The can speak the voice name and a few digits.
    
    Review URL: https://codereview.chromium.org/1253453002
    
    Cr-Commit-Position: refs/heads/master@{#340406}

--- chrome/test/data/extensions/api_test/tts_engine/network_speech_engine/test.js
+++ chrome/test/data/extensions/api_test/tts_engine/network_speech_engine/test.js
@@ -5,14 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // browser_tests.exe --gtest_filter="TtsApiTest.*"
 
+var pass = chrome.test.callbackPass;
+
 chrome.test.runTests([
   function testNetworkSpeechVoices() {
-    chrome.tts.getVoices(function(voices) {
-      chrome.test.assertTrue(voices.length >= 6);
+    chrome.tts.getVoices(pass(function(voices) {
+      chrome.test.assertTrue(voices.length >= 19);
       for (var i = 0; i < voices.length; i++) {
         chrome.test.assertEq(true, voices[i].remote);
       }
-      chrome.test.succeed();
-    });
+    }));
   }
 ]);
