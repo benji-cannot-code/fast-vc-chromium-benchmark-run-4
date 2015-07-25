a7b54687e2e55e318ae4f4cbb0a72f04b2ae7f11
timav <timav@chromium.org>
2015-07-24 18:05:13 -0700

    MediaCodecPlayer implementation (stage 2 - seek)
    Implements seek operation for MediaCodecPlayer.
    
    BUG=407577
    
    Review URL: https://codereview.chromium.org/1213583002
    
    Cr-Commit-Position: refs/heads/master@{#340388}

--- media/base/android/demuxer_stream_player_params.cc
+++ media/base/android/demuxer_stream_player_params.cc
@@ -72,6 +72,19 @@ const char* AsString(VideoCodec codec) {
   return nullptr;  // crash early
 }
 
+const char* AsString(DemuxerStream::Status status) {
+  switch (status) {
+    case DemuxerStream::kOk:
+      return "kOk";
+    case DemuxerStream::kAborted:
+      return "kAborted";
+    case DemuxerStream::kConfigChanged:
+      return "kConfigChanged";
+  }
+  NOTREACHED();
+  return nullptr;  // crash early
+}
+
 #undef RETURN_STRING
 
 }  // namespace (anonymous)
@@ -79,7 +92,8 @@ const char* AsString(VideoCodec codec) {
 }  // namespace media
 
 std::ostream& operator<<(std::ostream& os, const media::AccessUnit& au) {
-  os << "status:" << au.status << (au.is_end_of_stream ? " EOS" : "")
+  os << "status:" << media::AsString(au.status)
+     << (au.is_end_of_stream ? " EOS" : "")
      << (au.is_key_frame ? " KEY_FRAME" : "") << " pts:" << au.timestamp
      << " size:" << au.data.size();
   return os;
