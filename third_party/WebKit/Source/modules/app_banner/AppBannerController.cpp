c283aad4560af75f00fe29a34abbe3a9ff5bef82
dominickn@chromium.org <dominickn@chromium.org>
2015-07-25 00:55:49 +0000

    Refactor BeforeInstallPromptEvent to use ScriptPromiseProperty
    This stops the event holding a ScriptPromise as a member, which may 
    lead to a memory leak. Instead, ScriptPromiseProperty members are held
    as members in the event. A WebCallbacks subclass is used to store a
    pointer to the necessary ScriptPromiseProperty in the associated event,
    with the interface exposed to Chromium. When a banner event is resolved,
    the WebCallbacks implementation in modules/app_banner looks up the 
    ScriptPromiseProperty and resolves its promise. This also makes
    resolving the prompt promise simpler as well, as it occurs entirely in the
    event.
    
    This CL also removes the app-banner-event-prompt.html test from 
    LeakExpectations, and fixes some bugs in the test relating to promise
    resolution.
    
    BUG=504675
    
    Review URL: https://codereview.chromium.org/1247283004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199472 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/app_banner/AppBannerController.cpp
+++ third_party/WebKit/Source/modules/app_banner/AppBannerController.cpp
@@ -7,11 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/app_banner/AppBannerController.h"
 
 #include "core/EventTypeNames.h"
+#include "core/dom/Document.h"
 #include "core/frame/DOMWindow.h"
 #include "core/frame/LocalFrame.h"
+#include "modules/app_banner/AppBannerCallbacks.h"
 #include "modules/app_banner/BeforeInstallPromptEvent.h"
 #include "platform/RuntimeEnabledFeatures.h"
 #include "public/platform/WebVector.h"
+#include "public/platform/modules/app_banner/WebAppBannerClient.h"
 #include "public/platform/modules/app_banner/WebAppBannerPromptReply.h"
 
 namespace blink {
@@ -25,9 +28,13 @@ void AppBannerController::willShowInstallBannerPrompt(int requestId, WebAppBanne
     for (const WebString& platform : platforms)
         wtfPlatforms.append(platform);
 
+    RefPtrWillBeRawPtr<BeforeInstallPromptEvent> event = BeforeInstallPromptEvent::create(EventTypeNames::beforeinstallprompt, frame->document(), wtfPlatforms, requestId, client);
+    WebAppBannerCallbacks* callbacks = new AppBannerCallbacks(event->userChoiceProperty());
+    client->registerBannerCallbacks(requestId, callbacks);
+
     // dispatchEvent() returns whether the default behavior can happen. In other
     // words, it returns false if preventDefault() was called.
-    *reply = frame->domWindow()->dispatchEvent(BeforeInstallPromptEvent::create(EventTypeNames::beforeinstallprompt, wtfPlatforms, requestId, client))
+    *reply = frame->domWindow()->dispatchEvent(event)
         ? WebAppBannerPromptReply::None : WebAppBannerPromptReply::Cancel;
 }
 
