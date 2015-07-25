bef49b4b937562c5b1d24f17bb3ebc253162f440
qyearsley <qyearsley@chromium.org>
2015-07-24 17:33:31 -0700

    Proof-reading comments in base/message_loop/.
    This CL contains a few minor changes to comments that I made while
    skimming through the code. Mostly spelling changes.
    
    Review URL: https://codereview.chromium.org/1255673004
    
    Cr-Commit-Position: refs/heads/master@{#340383}

--- base/message_loop/message_pump_mac.h
+++ base/message_loop/message_pump_mac.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // Typically, MessagePumpNSApplication only makes sense on a Cocoa
 // application's main thread.  If a CFRunLoop-based message pump is needed on
-// any other thread, one of the other concrete subclasses is preferrable.
+// any other thread, one of the other concrete subclasses is preferable.
 // MessagePumpMac::Create is defined, which returns a new NSApplication-based
 // or NSRunLoop-based MessagePump subclass depending on which thread it is
 // called on.
