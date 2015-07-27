e72aed85944b01a575514212e9d3c68821746f58
dskiba <dskiba@google.com>
2015-07-27 15:58:52 -0700

    Fix flaky low-end document mode test.
    Review URL: https://codereview.chromium.org/1257153002
    
    Cr-Commit-Position: refs/heads/master@{#340593}

--- chrome/android/javatests/src/org/chromium/chrome/browser/document/DocumentModeLowEndTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/document/DocumentModeLowEndTest.java
@@ -19,13 +19,13 @@ import org.chromium.chrome.browser.ChromeApplication;
 import org.chromium.chrome.browser.EmptyTabObserver;
 import org.chromium.chrome.browser.Tab;
 import org.chromium.chrome.browser.tabmodel.EmptyTabModelSelectorObserver;
-import org.chromium.chrome.browser.tabmodel.TabModelUtils;
 import org.chromium.chrome.browser.tabmodel.document.DocumentTabModelSelector;
 import org.chromium.chrome.test.util.DisableInTabbedMode;
 import org.chromium.content.app.SandboxedProcessService;
 import org.chromium.content.browser.test.util.CallbackHelper;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Tests the how Document mode works on low end devices.
@@ -39,6 +39,7 @@ public class DocumentModeLowEndTest extends DocumentModeTestBase {
     public void testNewTabLoadLowEnd() throws Exception {
         launchViaLaunchDocumentInstance(false, HREF_LINK, "href link page");
 
+        final AtomicReference<Tab> backgroundTab = new AtomicReference<Tab>();
         final CallbackHelper tabCreatedCallback = new CallbackHelper();
         final CallbackHelper tabLoadStartedCallback = new CallbackHelper();
 
@@ -48,6 +49,7 @@ public class DocumentModeLowEndTest extends DocumentModeTestBase {
             @Override
             public void onNewTabCreated(final Tab newTab) {
                 selector.removeObserver(this);
+                backgroundTab.set(newTab);
                 tabCreatedCallback.notifyCalled();
 
                 assertFalse(newTab.getWebContents().isLoadingToDifferentDocument());
@@ -68,7 +70,8 @@ public class DocumentModeLowEndTest extends DocumentModeTestBase {
         assertEquals(1, tabCreatedCallback.getCallCount());
         assertEquals(0, tabLoadStartedCallback.getCallCount());
 
-        TabModelUtils.setIndex(selector.getCurrentModel(), 1);
+        switchToTab((DocumentTab) backgroundTab.get());
+
         tabLoadStartedCallback.waitForCallback(0);
     }
 
