e38760ebf643f2409c57f3576ffb750f03abb715
lfg <lfg@chromium.org>
2015-07-27 08:41:40 -0700

    Prevent GuestView from swapping process on cross-site navigations in --site-per-process.
    BUG=330264
    
    Review URL: https://codereview.chromium.org/1259573006
    
    Cr-Commit-Position: refs/heads/master@{#340470}

--- content/browser/loader/cross_site_resource_handler.cc
+++ content/browser/loader/cross_site_resource_handler.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/loader/resource_dispatcher_host_impl.h"
 #include "content/browser/loader/resource_request_info_impl.h"
 #include "content/browser/site_instance_impl.h"
+#include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/global_request_id.h"
@@ -98,6 +99,12 @@ bool CheckNavigationPolicyOnUI(GURL url, int process_id, int render_frame_id) {
   // A transfer is not needed if the current SiteInstance doesn't yet have a
   // site.  This is the case for tests that use NavigateToURL.
   if (!rfh->GetSiteInstance()->HasSite())
+    return false;
+
+  // For now, GuestViews never transfer on cross-site navigations.
+  WebContentsImpl* web_contents =
+      static_cast<WebContentsImpl*>(WebContents::FromRenderFrameHost(rfh));
+  if (web_contents->GetBrowserPluginGuest())
     return false;
 
   // TODO(nasko): This check is very simplistic and is used temporarily only
