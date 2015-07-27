bbb8d7fb325bb5e95060833314c74776cfa8edb9
bhnascar <bhnascar@chromium.org>
2015-07-27 16:51:20 -0700

    Adding base class SupportsUserData to NavigationItem
    We will need to associate a WKBackForwardListItem with each
    NavigationItem as part of a later CL for fixing form-resubmission.
    Other developers have also previously expressed interest in adding
    SupportsUserData as a base class for NavigationItem.
    
    BUG=462406
    
    Review URL: https://codereview.chromium.org/1257103004
    
    Cr-Commit-Position: refs/heads/master@{#340604}

--- ios/web/public/navigation_item.h
+++ ios/web/public/navigation_item.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_ptr.h"
 #include "base/strings/string16.h"
+#include "base/supports_user_data.h"
 #include "base/time/time.h"
 #include "ios/web/public/web_state/page_display_state.h"
 #include "ui/base/page_transition_types.h"
@@ -28,10 +29,8 @@ struct SSLStatus;
 // A NavigationItem is a data structure that captures all the information
 // required to recreate a browsing state. It represents one point in the
 // chain of navigation managed by a NavigationManager.
-class NavigationItem {
+class NavigationItem : public base::SupportsUserData {
  public:
-  virtual ~NavigationItem() {}
-
   // Creates a new NavigationItem.
   static scoped_ptr<NavigationItem> Create();
 
