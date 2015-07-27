8650cd4acb498e1ac5e8e3eaf85147fd6f505c59
mkwst@chromium.org <mkwst@chromium.org>
2015-07-27 14:21:49 +0000

    Initialize ResourceRequest::m_requestorOrigin.
    We're not using this on the Chromium side yet, which is good, because
    as soon as we start converting the exposed  'nullptr' WebSecurityOrigin
    into a url::Origin, it'll crash. :) This patch fixes that.
    
    BUG=459154
    
    Review URL: https://codereview.chromium.org/1255843003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199514 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/network/ResourceRequest.cpp
+++ third_party/WebKit/Source/platform/network/ResourceRequest.cpp
@@ -452,6 +452,7 @@ void ResourceRequest::initialize(const KURL& url)
     m_originatesFromReservedIPRange = false;
     m_inputPerfMetricReportPolicy = InputToLoadPerfMetricReportPolicy::NoReport;
     m_followedRedirect = false;
+    m_requestorOrigin = SecurityOrigin::createUnique();
 }
 
 } // namespace blink
--- third_party/WebKit/Source/platform/network/ResourceRequestTest.cpp
+++ third_party/WebKit/Source/platform/network/ResourceRequestTest.cpp
@@ -15,6 +15,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+TEST(ResourceRequestTest, RequestorOriginNonNull)
+{
+    ResourceRequest req;
+    EXPECT_NE(nullptr, req.requestorOrigin().get());
+    EXPECT_TRUE(req.requestorOrigin()->isUnique());
+}
+
 TEST(ResourceRequestTest, CrossThreadResourceRequestData)
 {
     ResourceRequest original;
