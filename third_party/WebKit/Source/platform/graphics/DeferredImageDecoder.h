1973b72f41afb0b754d789d35da3db7cc88c19b0
fmalita@chromium.org <fmalita@chromium.org>
2015-07-27 19:44:14 +0000

    Revert of Avoid intermediate SkBitmap frames in DeferredImageDecoder (patchset #4 id:60001 of https://codereview.chromium.org/1257453002/)
    Reason for revert:
    Revert to unblock dev while we figure out https://code.google.com/p/chromium/issues/detail?id=514143
    
    Original issue's description:
    > Avoid intermediate SkBitmap frames in DeferredImageDecoder
    > 
    > We're currently building SkDiscardablePixelRef-backed SkBitmap frames
    > only to immediately wrap them in SkImages.
    > 
    > Instead, we can allocate SkDiscardablePixelRef-backed SkImage frames
    > directly.  This avoids early decoding/pixel copies when the bitmap is
    > not immutable, and removes all SkBitmap use DeferredImageDecoder.
    > 
    > BUG=449197,512182
    > R=junov@chromium.org,noel@chromium.org,scroggo@chromium.org
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=199445
    
    TBR=junov@chromium.org,noel@chromium.org,scroggo@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=449197,512182
    
    Review URL: https://codereview.chromium.org/1255253003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199529 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/DeferredImageDecoder.h
+++ third_party/WebKit/Source/platform/graphics/DeferredImageDecoder.h
@@ -83,7 +83,7 @@ public:
 private:
     explicit DeferredImageDecoder(PassOwnPtr<ImageDecoder> actualDecoder);
     void prepareLazyDecodedFrames();
-    PassRefPtr<SkImage> createImage(size_t index, bool knownToBeOpaque) const;
+    SkBitmap createBitmap(size_t index);
     void activateLazyDecoding();
 
     RefPtr<SharedBuffer> m_data;
