ed30a75cae465a639abae8f66fc2eaf2836b4c3f
avi <avi@chromium.org>
2015-07-27 11:25:46 -0700

    Clean up around NavigationEntryScreenshotManager a bit.
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1254993003
    
    Cr-Commit-Position: refs/heads/master@{#340508}

--- content/browser/frame_host/navigation_entry_screenshot_manager.cc
+++ content/browser/frame_host/navigation_entry_screenshot_manager.cc
@@ -86,8 +86,7 @@ void NavigationEntryScreenshotManager::TakeScreenshot() {
   if (!owner_->delegate()->CanOverscrollContent())
     return;
 
-  RenderViewHost* render_view_host =
-      owner_->delegate()->GetRenderViewHost();
+  RenderViewHost* render_view_host = owner_->delegate()->GetRenderViewHost();
   content::RenderWidgetHostView* view = render_view_host->GetView();
   if (!view)
     return;
@@ -138,16 +137,7 @@ void NavigationEntryScreenshotManager::OnScreenshotTaken(
     int unique_id,
     const SkBitmap& bitmap,
     ReadbackResponse response) {
-  NavigationEntryImpl* entry = NULL;
-  int entry_count = owner_->GetEntryCount();
-  for (int i = 0; i < entry_count; ++i) {
-    NavigationEntryImpl* iter = owner_->GetEntryAtIndex(i);
-    if (iter->GetUniqueID() == unique_id) {
-      entry = iter;
-      break;
-    }
-  }
-
+  NavigationEntryImpl* entry = owner_->GetEntryWithUniqueID(unique_id);
   if (!entry) {
     LOG(ERROR) << "Invalid entry with unique id: " << unique_id;
     return;
@@ -182,15 +172,7 @@ int NavigationEntryScreenshotManager::GetScreenshotCount() const {
 void NavigationEntryScreenshotManager::OnScreenshotEncodeComplete(
     int unique_id,
     scoped_refptr<ScreenshotData> screenshot) {
-  NavigationEntryImpl* entry = NULL;
-  int entry_count = owner_->GetEntryCount();
-  for (int i = 0; i < entry_count; ++i) {
-    NavigationEntryImpl* iter = owner_->GetEntryAtIndex(i);
-    if (iter->GetUniqueID() == unique_id) {
-      entry = iter;
-      break;
-    }
-  }
+  NavigationEntryImpl* entry = owner_->GetEntryWithUniqueID(unique_id);
   if (!entry)
     return;
   entry->SetScreenshotPNGData(screenshot->data());
