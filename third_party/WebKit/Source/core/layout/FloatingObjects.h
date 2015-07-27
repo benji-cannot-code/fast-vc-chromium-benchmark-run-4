e95e3f842385c28da4e43e5c5133d9c549d11cbc
robhogan@gmail.com <robhogan@gmail.com>
2015-07-27 17:55:15 +0000

    Refactor the treatment of shape-outside objects in float overlapping checks
    Integrate shape-outside into the red-black tree used for checking whether an
    object overlaps with a float.
    
    This merges WebKit CL http://trac.webkit.org/changeset/158954
    
    BUG=510872
    
    Review URL: https://codereview.chromium.org/1242253002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199519 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/FloatingObjects.h
+++ third_party/WebKit/Source/core/layout/FloatingObjects.h
@@ -36,9 +36,6 @@ namespace blink {
 class LayoutBlockFlow;
 class LayoutBox;
 
-// FIXME this should be removed once LayoutBlockFlow::nextFloatLogicalBottomBelow doesn't need it anymore. (Bug 123931)
-enum ShapeOutsideFloatOffsetMode { ShapeOutsideFloatShapeOffset, ShapeOutsideFloatMarginBoxOffset };
-
 class FloatingObject {
     WTF_MAKE_NONCOPYABLE(FloatingObject); WTF_MAKE_FAST_ALLOCATED(FloatingObject);
 public:
@@ -160,6 +157,8 @@ public:
 
     LayoutUnit logicalLeftOffsetForPositioningFloat(LayoutUnit fixedOffset, LayoutUnit logicalTop, LayoutUnit* heightRemaining);
     LayoutUnit logicalRightOffsetForPositioningFloat(LayoutUnit fixedOffset, LayoutUnit logicalTop, LayoutUnit* heightRemaining);
+    LayoutUnit findNextFloatLogicalBottomBelow(LayoutUnit logicalHeight);
+    LayoutUnit findNextFloatLogicalBottomBelowForBlock(LayoutUnit logicalHeight);
 
     LayoutUnit lowestFloatLogicalBottom(FloatingObject::Type);
     FloatingObject* lowestFloatingObject() const;
