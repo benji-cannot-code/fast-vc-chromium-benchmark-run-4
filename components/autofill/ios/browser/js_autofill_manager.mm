208e4c8735b80d77da060568c58e40b596390fdd
bondd <bondd@chromium.org>
2015-07-27 11:53:54 -0700

    iOS full-form Autofill: Fire JS 'change' events for autofilled fields.
    Existing single-field Autofill sends JS 'change' events when each field
    is autofilled. Do the same for full-form Autofill.
    
    Previous to this CL fillForm() was used for both full-form Autofill and InstantBuy, with parameters to specify different behaviors depending on which code path called it. This CL splits out fillFormForInstantBuy(), so that fillForm() is used only for full-form Autofill and fillFormForInstantBuy() is used only for InstantBuy. This removes the need for extra parameters and makes the code easier to understand.
    
    fillFormForInstantBuy() is exactly the same code as fillForm() was before I started modifying it to handle full-form Autofill.
    
    Downstream part is here: https://chromereviews.googleplex.com/228217013
    
    BUG=504644
    
    Review URL: https://codereview.chromium.org/1248023004
    
    Cr-Commit-Position: refs/heads/master@{#340516}

--- components/autofill/ios/browser/js_autofill_manager.mm
+++ components/autofill/ios/browser/js_autofill_manager.mm
@@ -58,19 +58,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)fillForm:(NSString*)dataString
-         onlyFillEmpty:(BOOL)onlyFillEmpty
     forceFillFieldName:(NSString*)forceFillFieldName
-         styleElements:(BOOL)styleElements
      completionHandler:(ProceduralBlock)completionHandler {
   DCHECK(completionHandler);
   std::string fieldName =
       forceFillFieldName
           ? base::GetQuotedJSONString([forceFillFieldName UTF8String])
           : "null";
+  NSString* fillFormJS =
+      [NSString stringWithFormat:@"__gCrWeb.autofill.fillForm(%@, %s);",
+                                 dataString, fieldName.c_str()];
+  id stringResultHandler = ^(NSString*, NSError*) {
+    completionHandler();
+  };
+  return [self evaluate:fillFormJS stringResultHandler:stringResultHandler];
+}
+
+- (void)fillFormForInstantBuy:(NSString*)dataString
+            completionHandler:(ProceduralBlock)completionHandler {
+  DCHECK(completionHandler);
   NSString* fillFormJS = [NSString
-      stringWithFormat:@"__gCrWeb.autofill.fillForm(%@, %s, %s, %s);",
-                       dataString, onlyFillEmpty ? "true" : "false",
-                       fieldName.c_str(), styleElements ? "true" : "false"];
+      stringWithFormat:@"__gCrWeb.autofill.fillFormForInstantBuy(%@);",
+                       dataString];
   id stringResultHandler = ^(NSString*, NSError*) {
     completionHandler();
   };
