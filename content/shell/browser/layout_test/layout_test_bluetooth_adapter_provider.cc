fa23ab9b43cbeddbdae1f3d1ab9e36e59b9b59d1
ortuno <ortuno@chromium.org>
2015-07-27 12:17:00 -0700

    bluetooth: Remove old UnconnectableDevice and UnconnectableDeviceAdapter
    Both have been replaced with a more flexible UnconnectableDevice and
    FailingConnectionsAdapter.
    
    Three-sided patch.
     [1] Implement FailingConnectionAdapter in browser-side (http://crrev.com/1247553002)
     [2] Add unconnetable device tests and remove old tests. (http://crrev.com/1244973002)
     [3] This patch.
    
    BUG=499552
    
    Review URL: https://codereview.chromium.org/1243803002
    
    Cr-Commit-Position: refs/heads/master@{#340525}

--- content/shell/browser/layout_test/layout_test_bluetooth_adapter_provider.cc
+++ content/shell/browser/layout_test/layout_test_bluetooth_adapter_provider.cc
@@ -99,8 +99,6 @@ LayoutTestBluetoothAdapterProvider::GetBluetoothAdapter(
     return GetSingleEmptyDeviceAdapter();
   else if (fake_adapter_name == "ConnectableDeviceAdapter")
     return GetConnectableDeviceAdapter();
-  else if (fake_adapter_name == "UnconnectableDeviceAdapter")
-    return GetUnconnectableDeviceAdapter();
   // New adapters
   else if (fake_adapter_name == "BaseAdapter")
     return GetBaseAdapter();
@@ -437,16 +435,6 @@ LayoutTestBluetoothAdapterProvider::GetConnectableDeviceAdapter() {
 }
 
 // static
-scoped_refptr<NiceMock<MockBluetoothAdapter>>
-LayoutTestBluetoothAdapterProvider::GetUnconnectableDeviceAdapter() {
-  scoped_refptr<NiceMock<MockBluetoothAdapter>> adapter(GetEmptyAdapter());
-
-  adapter->AddMockDevice(GetUnconnectableDevice(adapter.get()));
-
-  return adapter.Pass();
-}
-
-// static
 scoped_ptr<NiceMock<MockBluetoothDevice>>
 LayoutTestBluetoothAdapterProvider::GetEmptyDevice(
     MockBluetoothAdapter* adapter,
@@ -532,19 +520,6 @@ LayoutTestBluetoothAdapterProvider::GetConnectableDevice(
             return make_scoped_ptr(new NiceMock<MockBluetoothGattConnection>(
                 device_ptr->GetAddress()));
           }));
-
-  return device.Pass();
-}
-
-// static
-scoped_ptr<NiceMock<MockBluetoothDevice>>
-LayoutTestBluetoothAdapterProvider::GetUnconnectableDevice(
-    MockBluetoothAdapter* adapter) {
-  scoped_ptr<NiceMock<MockBluetoothDevice>> device(GetEmptyDevice(adapter));
-
-  ON_CALL(*device, CreateGattConnection(_, _))
-      .WillByDefault(
-          RunCallback<1 /* error_callback */>(BluetoothDevice::ERROR_FAILED));
 
   return device.Pass();
 }
