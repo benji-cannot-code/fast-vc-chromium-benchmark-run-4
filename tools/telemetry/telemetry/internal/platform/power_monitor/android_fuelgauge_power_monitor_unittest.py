8837577a8889ceaee1e8b528492fc05ca8a7958d
rnephew <rnephew@chromium.org>
2015-07-27 07:46:02 -0700

    [Android][Telemetry] Seperate fuel gauge from AndroidDumpsysPowerMonitor
    BUG=513004
    
    Review URL: https://codereview.chromium.org/1251673002
    
    Cr-Commit-Position: refs/heads/master@{#340461}

---
+++ tools/telemetry/telemetry/internal/platform/power_monitor/android_fuelgauge_power_monitor_unittest.py
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from telemetry.internal.platform.power_monitor import (
+    android_fuelgauge_power_monitor)
+
+
+class FuelGaugePowerMonitorMonitorTest(unittest.TestCase):
+
+  def testEnergyComsumption(self):
+    fuel_gauge_delta = 100
+    results = (
+        android_fuelgauge_power_monitor.FuelGaugePowerMonitor.ProcessPowerData(
+            4.0, fuel_gauge_delta))
+    self.assertEqual(results['identifier'], 'fuel_gauge')
+    self.assertEqual(
+        results.get('fuel_gauge_energy_consumption_mwh'), 400)
+
+
+if __name__ == '__main__':
+  unittest.main()
