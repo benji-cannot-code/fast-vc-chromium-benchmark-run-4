0240c26128daf9c228c73eb3680ae544d381d309
alexclarke <alexclarke@chromium.org>
2015-07-27 07:26:53 -0700

    TaskQueueManager::UpdateWorkQueues only visits non empty queues
    The TQM now keeps track of which queues have a non-empty incomming_queue_
    and it visits them in TaskQueueManager::UpdateWorkQueues.
    
    On average this significantly reduces the number of locks
    taken in TaskQueueManager::UpdateWorkQueues.
    
    BUG=510398
    
    Review URL: https://codereview.chromium.org/1254453003
    
    Cr-Commit-Position: refs/heads/master@{#340455}

--- components/scheduler/child/task_queue_impl.h
+++ components/scheduler/child/task_queue_impl.h
@@ -49,7 +49,7 @@ class SCHEDULER_EXPORT TaskQueueImpl final : public TaskQueue {
   bool NextPendingDelayedTaskRunTime(
       base::TimeTicks* next_pending_delayed_task);
 
-  bool UpdateWorkQueue(LazyNow* lazy_now,
+  void UpdateWorkQueue(LazyNow* lazy_now,
                        bool should_trigger_wakeup,
                        const base::PendingTask* previous_task);
   base::PendingTask TakeTaskFromWorkQueue();
