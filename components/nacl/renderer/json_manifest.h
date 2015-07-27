b8d3a2bc21a6b7d75d1251251f89d74035bdf5eb
jvoung <jvoung@chromium.org>
2015-07-27 14:18:50 -0700

    Remove a use of third_party/jsoncpp in components/nacl/renderer.
    One remaining use is for NaCl's json_manifest.  Use
    base/values and base/json/ instead.
    
    Did not remove references to jsoncpp in DEPS,
    build/linux/unbundle/, or other gyp files (remoting) etc.
    
    BUG=476671
    
    Review URL: https://codereview.chromium.org/1259803002
    
    Cr-Commit-Position: refs/heads/master@{#340565}

--- components/nacl/renderer/json_manifest.h
+++ components/nacl/renderer/json_manifest.h
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/memory/scoped_ptr.h"
+#include "base/values.h"
 #include "components/nacl/renderer/ppb_nacl_private.h"
-#include "ppapi/c/pp_array_output.h"
-#include "third_party/jsoncpp/source/include/json/value.h"
 
 namespace nacl {
 class JsonManifest;
@@ -30,6 +30,7 @@ class JsonManifest {
                const std::string& sandbox_isa,
                bool nonsfi_enabled,
                bool pnacl_debug);
+  ~JsonManifest();
 
   // Initialize the manifest object for use by later lookups. Returns
   // true if the manifest parses correctly and matches the schema.
@@ -57,11 +58,11 @@ class JsonManifest {
 
  private:
   bool MatchesSchema(ErrorInfo* error_info);
-  bool GetKeyUrl(const Json::Value& dictionary,
+  bool GetKeyUrl(const base::DictionaryValue& dictionary,
                  const std::string& key,
                  std::string* full_url,
                  PP_PNaClOptions* pnacl_options) const;
-  bool GetURLFromISADictionary(const Json::Value& dictionary,
+  bool GetURLFromISADictionary(const base::DictionaryValue& parent_dictionary,
                                const std::string& parent_key,
                                std::string* url,
                                PP_PNaClOptions* pnacl_options,
@@ -74,7 +75,7 @@ class JsonManifest {
   bool pnacl_debug_;
 
   // The dictionary of manifest information parsed in Init().
-  Json::Value dictionary_;
+  scoped_ptr<base::DictionaryValue> dictionary_;
 };
 
 }  // namespace nacl
