c9fc32f066d8b00c4811185f58c86021614bce2d
tonychun <tonychun@google.com>
2015-07-28 10:38:24 -0700

    Removed all temporary functionality of main() to use the chromoting test driver environment.
    This check-in removes all functions that belong in the environment from main() into the environment. Also, the global pointer has been re-added in for shared usage between tests. The shared environment is instantiated in this way because of the comments given here: https://code.google.com/p/chromium/codesearch#chromium/src/testing/gtest/include/gtest/gtest.h&sq=package:chromium&rcl=1438011313&l=1350&type=cs.
    
    In a different CL, I will be using a test fixture and a test to connect to the host and show performance metrics.
    
    Remove all environment functionality in main() and use environment.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1256323002
    
    Cr-Commit-Position: refs/heads/master@{#340712}

--- remoting/test/chromoting_test_driver_environment.cc
+++ remoting/test/chromoting_test_driver_environment.cc
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace remoting {
 namespace test {
 
+ChromotingTestDriverEnvironment* g_chromoting_shared_data = nullptr;
+
 ChromotingTestDriverEnvironment::EnvironmentOptions::EnvironmentOptions() {
 }
 
@@ -31,6 +33,7 @@ ChromotingTestDriverEnvironment::ChromotingTestDriverEnvironment(
     const EnvironmentOptions& options)
     : host_name_(options.host_name),
       user_name_(options.user_name),
+      pin_(options.pin),
       refresh_token_file_path_(options.refresh_token_file_path),
       test_access_token_fetcher_(nullptr),
       test_refresh_token_store_(nullptr),
