017a3a90c5d40e927f60318f608ff74bc159a7d1
maxbogue <maxbogue@chromium.org>
2015-07-28 11:46:34 -0700

    [Sync] Add a null check for getFragmentManager().
    BUG=480544
    
    Review URL: https://codereview.chromium.org/1255753002
    
    Cr-Commit-Position: refs/heads/master@{#340735}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/ui/SyncCustomizationFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/ui/SyncCustomizationFragment.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.sync.ui;
 
 import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.graphics.Color;
@@ -349,7 +350,12 @@ public class SyncCustomizationFragment extends PreferenceFragment
     }
 
     private void closeDialogIfOpen(String tag) {
-        DialogFragment df = (DialogFragment) getFragmentManager().findFragmentByTag(tag);
+        FragmentManager manager = getFragmentManager();
+        if (manager == null) {
+            // Do nothing if the manager doesn't exist yet; see http://crbug.com/480544.
+            return;
+        }
+        DialogFragment df = (DialogFragment) manager.findFragmentByTag(tag);
         if (df != null) {
             df.dismiss();
         }
