a8f9797f8723c318476ab259cd1b82f89c975ac2
jrummell <jrummell@chromium.org>
2015-07-28 11:01:48 -0700

    Update MediaKeyStatus to replace OutputNotAllowed with OutputRestricted
    Latest EME spec replaced OutputNotAllowed with OutputRestricted, so
    update the code to rename the status. We are not changing the CDM
    interface at this time, so CDMs will still return OutputNotAllowed but
    it will get passed to blink as OutputRestricted.
    
    BUG=507791
    TEST=EME layout tests pass
    
    Review URL: https://codereview.chromium.org/1242753006
    
    Cr-Commit-Position: refs/heads/master@{#340721}

--- ppapi/c/private/pp_content_decryptor.h
+++ ppapi/c/private/pp_content_decryptor.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From private/pp_content_decryptor.idl modified Thu Mar 19 16:04:42 2015. */
+/* From private/pp_content_decryptor.idl modified Mon Jul 20 16:41:39 2015. */
 
 #ifndef PPAPI_C_PRIVATE_PP_CONTENT_DECRYPTOR_H_
 #define PPAPI_C_PRIVATE_PP_CONTENT_DECRYPTOR_H_
@@ -502,7 +502,7 @@ typedef enum {
   PP_CDMKEYSTATUS_USABLE = 0,
   PP_CDMKEYSTATUS_INVALID = 1,
   PP_CDMKEYSTATUS_EXPIRED = 2,
-  PP_CDMKEYSTATUS_OUTPUTNOTALLOWED = 3,
+  PP_CDMKEYSTATUS_OUTPUTRESTRICTED = 3,
   PP_CDMKEYSTATUS_OUTPUTDOWNSCALED = 4,
   PP_CDMKEYSTATUS_STATUSPENDING = 5
 } PP_CdmKeyStatus;
