71523ef5f494487fd4c6167cfd093f2a6c98d6a6
estark <estark@chromium.org>
2015-07-27 22:16:47 -0700

    Add net::CertificateReportSender for handling cert report sending
    net::CertificateReportSender contains code factored out of
    CertificateErrorReporter in //chrome. net::CertificateReportSender will
    be used for both HPKP violation reports and the Safe Browsing Extended
    Reporting cert reports that CertificateErrorReporter handles.
    
    CL #1: crrev.com/1211363005 (parse report-uri)
    This is CL #2.
    CL #3: crrev.com/1212613004 (build and send HPKP reports)
    
    BUG=445793
    
    Review URL: https://codereview.chromium.org/1212973002
    
    Cr-Commit-Position: refs/heads/master@{#340641}

--- chrome/browser/safe_browsing/ping_manager.cc
+++ chrome/browser/safe_browsing/ping_manager.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/escape.h"
 #include "net/base/load_flags.h"
 #include "net/ssl/ssl_info.h"
+#include "net/url_request/certificate_report_sender.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "net/url_request/url_request_status.h"
@@ -59,13 +60,13 @@ SafeBrowsingPingManager::SafeBrowsingPingManager(
     bool use_insecure_certificate_upload_url =
         CertificateErrorReporter::IsHttpUploadUrlSupported();
 
-    CertificateErrorReporter::CookiesPreference cookies_preference;
+    net::CertificateReportSender::CookiesPreference cookies_preference;
     GURL certificate_upload_url;
     if (use_insecure_certificate_upload_url) {
-      cookies_preference = CertificateErrorReporter::DO_NOT_SEND_COOKIES;
+      cookies_preference = net::CertificateReportSender::DO_NOT_SEND_COOKIES;
       certificate_upload_url = GURL(kExtendedReportingUploadUrlInsecure);
     } else {
-      cookies_preference = CertificateErrorReporter::SEND_COOKIES;
+      cookies_preference = net::CertificateReportSender::SEND_COOKIES;
       certificate_upload_url = GURL(kExtendedReportingUploadUrlSecure);
     }
 
--- chrome/browser/ssl/chrome_fraudulent_certificate_reporter.cc
+++ chrome/browser/ssl/chrome_fraudulent_certificate_reporter.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/certificate_error_reporter.h"
 #include "chrome/browser/ssl/certificate_error_report.h"
 #include "net/ssl/ssl_info.h"
+#include "net/url_request/certificate_report_sender.h"
 #include "net/url_request/url_request_context.h"
 #include "url/gurl.h"
 
@@ -26,8 +27,7 @@ ChromeFraudulentCertificateReporter::ChromeFraudulentCertificateReporter(
     : certificate_reporter_(new chrome_browser_net::CertificateErrorReporter(
           request_context,
           GURL(kFraudulentCertificateUploadEndpoint),
-          chrome_browser_net::CertificateErrorReporter::DO_NOT_SEND_COOKIES)) {
-}
+          net::CertificateReportSender::DO_NOT_SEND_COOKIES)) {}
 
 ChromeFraudulentCertificateReporter::ChromeFraudulentCertificateReporter(
     scoped_ptr<chrome_browser_net::CertificateErrorReporter>
