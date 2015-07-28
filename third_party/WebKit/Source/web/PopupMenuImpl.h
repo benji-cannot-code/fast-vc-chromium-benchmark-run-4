bf49a334c036ff77e37dbbbff67b914ee3eda040
tkent@chromium.org <tkent@chromium.org>
2015-07-28 07:39:50 +0000

    New SELECT Popup: Remove the limitation of the number of styled OPTION elements.
    We introduced a limitation of the number of styled OPTION elements in M44
    release in order to avoid a performance issue.
    This CL removes the limitation by making any style properties optional.
    
    Implementation:
     - Alwyas pass style properties of the owner SELECT elemnet.
     - Serialize an OPTION style property only if its value is different from the
       corresponding style value of the owner SELECT element.
    
    BUG=513339
    
    Review URL: https://codereview.chromium.org/1262563002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/PopupMenuImpl.h
+++ third_party/WebKit/Source/web/PopupMenuImpl.h
@@ -32,7 +32,6 @@ private:
     PopupMenuImpl(ChromeClientImpl*, PopupMenuClient*);
 
     class ItemIterationContext;
-    bool hasTooManyItemsForStyling();
     void addOption(ItemIterationContext&, HTMLOptionElement&);
     void addOptGroup(ItemIterationContext&, HTMLOptGroupElement&);
     void addSeparator(ItemIterationContext&, HTMLHRElement&);
