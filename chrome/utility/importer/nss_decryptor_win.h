29feace2ca7308c2c23cdd7696e266310af0d8b3
xunlu <xunlu@chromium.org>
2015-07-28 11:27:03 -0700

    Fix silent failure of password import from Firefox
    Firefox switched storage of password from signons.sqlite to logins.json since
    version 32. This cl modified FirefoxImporter::ImportPassword() to adapt to
    that change.
    
    BUG=510865
    
    Review URL: https://codereview.chromium.org/1244473006
    
    Cr-Commit-Position: refs/heads/master@{#340729}

--- chrome/utility/importer/nss_decryptor_win.h
+++ chrome/utility/importer/nss_decryptor_win.h
@@ -105,6 +105,8 @@ typedef void (*SECITEMFreeItemFunc)(SECItem *item, PRBool free_it);
 typedef void (*PLArenaFinishFunc)(void);
 typedef PRStatus (*PRCleanupFunc)(void);
 
+struct FirefoxRawPasswordInfo;
+
 namespace autofill {
 struct PasswordForm;
 }
@@ -130,7 +132,7 @@ class NSSDecryptor {
   // Parses the Firefox password file content, decrypts the
   // username/password and reads other related information.
   // The result will be stored in |forms|.
-  void ParseSignons(const std::string& content,
+  void ParseSignons(const base::FilePath& signon_file,
                     std::vector<autofill::PasswordForm>* forms);
 
   // Reads and parses the Firefox password sqlite db, decrypts the
@@ -139,6 +141,12 @@ class NSSDecryptor {
   bool ReadAndParseSignons(const base::FilePath& sqlite_file,
                            std::vector<autofill::PasswordForm>* forms);
 
+  // Reads and parses the Firefox password file logins.json, decrypts the
+  // username/password and reads other related information.
+  // The result will be stored in |forms|.
+  bool ReadAndParseLogins(const base::FilePath& json_file,
+                          std::vector<autofill::PasswordForm>* forms);
+
  private:
   // Call NSS initialization funcs.
   bool InitNSS(const base::FilePath& db_path,
@@ -146,7 +154,14 @@ class NSSDecryptor {
                base::NativeLibrary nspr4_dll);
 
   PK11SlotInfo* GetKeySlotForDB() const { return PK11_GetInternalKeySlot(); }
+
   void FreeSlot(PK11SlotInfo* slot) const { PK11_FreeSlot(slot); }
+
+  // Turns unprocessed information extracted from Firefox's password file
+  // into PasswordForm.
+  bool CreatePasswordFormFromRawInfo(
+      const FirefoxRawPasswordInfo& raw_password_info,
+      autofill::PasswordForm* form);
 
   // Methods in Firefox security components.
   NSSInitFunc NSS_Init;
