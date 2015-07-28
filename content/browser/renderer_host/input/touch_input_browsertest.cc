c4b9ec8d1e67b40eb8330be6d52fbe8006e41c5e
thestig <thestig@chromium.org>
2015-07-27 22:37:21 -0700

    MSAN: Disable flaky TouchInputBrowserTest on ChromeOS.
    BUG=514456
    
    Review URL: https://codereview.chromium.org/1255363002
    
    Cr-Commit-Position: refs/heads/master@{#340644}

--- content/browser/renderer_host/input/touch_input_browsertest.cc
+++ content/browser/renderer_host/input/touch_input_browsertest.cc
@@ -200,7 +200,13 @@ IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MAYBE_TouchNoHandler) {
   touch.ResetPoints();
 }
 
-IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, TouchHandlerNoConsume) {
+#if defined(OS_CHROMEOS) && defined(MEMORY_SANITIZER)
+// crbug.com/514456
+#define MAYBE_TouchHandlerNoConsume DISABLED_TouchHandlerNoConsume
+#else
+#define MAYBE_TouchHandlerNoConsume TouchHandlerNoConsume
+#endif
+IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MAYBE_TouchHandlerNoConsume) {
   LoadURLAndAddFilter();
   SyntheticWebTouchEvent touch;
 
@@ -217,7 +223,13 @@ IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, TouchHandlerNoConsume) {
   touch.ResetPoints();
 }
 
-IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, TouchHandlerConsume) {
+#if defined(OS_CHROMEOS) && defined(MEMORY_SANITIZER)
+// crbug.com/514456
+#define MAYBE_TouchHandlerConsume DISABLED_TouchHandlerConsume
+#else
+#define MAYBE_TouchHandlerConsume TouchHandlerConsume
+#endif
+IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MAYBE_TouchHandlerConsume) {
   LoadURLAndAddFilter();
   SyntheticWebTouchEvent touch;
 
@@ -233,7 +245,10 @@ IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, TouchHandlerConsume) {
   filter()->WaitForAck(WebInputEvent::TouchEnd);
 }
 
-#if defined(OS_MACOSX)
+#if defined(OS_CHROMEOS) && defined(MEMORY_SANITIZER)
+// crbug.com/514456
+#define MAYBE_MultiPointTouchPress DISABLED_MultiPointTouchPress
+#elif defined(OS_MACOSX)
 // TODO(ccameron): Failing on mac: crbug.com/346363
 #define MAYBE_MultiPointTouchPress DISABLED_MultiPointTouchPress
 #else
