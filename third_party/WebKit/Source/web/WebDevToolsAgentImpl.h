d98acd94ba2b140d282085d230892ee028c33f9d
yutak@chromium.org <yutak@chromium.org>
2015-07-28 11:04:14 +0000

    Oilpan: Convert a raw pointer in WebDevToolsAgentImpl to a Member.
    BUG=509911
    R=haraken@chromium.org, keishi@chromium.org, oilpan-reviews@chromium.org
    
    Review URL: https://codereview.chromium.org/1260213002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199575 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebDevToolsAgentImpl.h
+++ third_party/WebKit/Source/web/WebDevToolsAgentImpl.h
@@ -134,7 +134,7 @@ private:
     bool handleTouchEvent(LocalFrame*, const PlatformTouchEvent&);
 
     WebDevToolsAgentClient* m_client;
-    WebLocalFrameImpl* m_webLocalFrameImpl;
+    RawPtrWillBeMember<WebLocalFrameImpl> m_webLocalFrameImpl;
     bool m_attached;
 #if ENABLE(ASSERT)
     bool m_hasBeenDisposed;
