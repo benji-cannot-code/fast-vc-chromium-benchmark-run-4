4f7b0320e3eac31de07127d13f43147e1438224e
hyungwook.lee@navercorp.com <hyungwook.lee@navercorp.com>
2015-07-28 11:12:45 +0000

    Remove Node.localName and Node.namespaceURI
    These are no longer on the Node interface in DOM:
    https://dom.spec.whatwg.org/#interface-node
    
    Node.namespaceURI and Node.localName are shadowed on Element 
    and Attr, so in every case where the attributes on Node are accessed, 
    they will return null. 
    Removing them will instead make any access return undefined.
    
    This CL is based on http://crrev.com/753893002.
    
    BUG=504574
    
    Review URL: https://codereview.chromium.org/1254243002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199576 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
@@ -1598,11 +1598,12 @@ PassRefPtr<TypeBuilder::DOM::Node> InspectorDOMAgent::buildObjectForNode(Node* n
             nodeValue = nodeValue.left(maxTextSize) + ellipsisUChar;
         break;
     case Node::ATTRIBUTE_NODE:
-    case Node::DOCUMENT_FRAGMENT_NODE:
-    case Node::DOCUMENT_NODE:
+        localName = toAttr(node)->localName();
+        break;
     case Node::ELEMENT_NODE:
+        localName = toElement(node)->localName();
+        break;
     default:
-        localName = node->localName();
         break;
     }
 
