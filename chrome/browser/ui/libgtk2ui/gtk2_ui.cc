2fb67202bbf903cd294a3f5e2c04d2a7c37fc947
estade <estade@chromium.org>
2015-07-28 14:26:01 -0700

    Use native theme's throbber colors for tabstrip throbber (GTK theme).
    BUG=513085
    
    Review URL: https://codereview.chromium.org/1248863005
    
    Cr-Commit-Position: refs/heads/master@{#340768}

--- chrome/browser/ui/libgtk2ui/gtk2_ui.cc
+++ chrome/browser/ui/libgtk2ui/gtk2_ui.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/skbitmap_operations.h"
 #include "ui/gfx/skia_util.h"
 #include "ui/gfx/x/x11_types.h"
+#include "ui/native_theme/native_theme.h"
 #include "ui/resources/grit/ui_resources.h"
 #include "ui/views/controls/button/label_button.h"
 #include "ui/views/controls/button/label_button_border.h"
@@ -997,6 +998,13 @@ void Gtk2UI::LoadGtkValues() {
       GdkColorToSkColor(entry_style->base[GTK_STATE_ACTIVE]);
   inactive_selection_fg_color_ =
       GdkColorToSkColor(entry_style->text[GTK_STATE_ACTIVE]);
+
+  colors_[ThemeProperties::COLOR_THROBBER_SPINNING] =
+      NativeThemeGtk2::instance()->GetSystemColor(
+          ui::NativeTheme::kColorId_ThrobberSpinningColor);
+  colors_[ThemeProperties::COLOR_THROBBER_WAITING] =
+      NativeThemeGtk2::instance()->GetSystemColor(
+          ui::NativeTheme::kColorId_ThrobberWaitingColor);
 }
 
 GdkColor Gtk2UI::BuildFrameColors(GtkStyle* frame_style) {
