2044a912557f698807593e7830f61ab0a5e477df
jbroman <jbroman@chromium.org>
2015-07-28 10:43:47 -0700

    Revert of Pass NSUserDefaults over IPC to the renderer (patchset #3 id:60001 of https://codereview.chromium.org/1239363002/)
    Reason for revert:
    Breaks various fast/events and fast/scrolling layout tests on Mac >= 10.8.
    
    https://code.google.com/p/chromium/issues/detail?id=514704
    
    Original issue's description:
    > Pass NSUserDefaults over IPC to the renderer
    >
    > Blink can now accept the NSUserDefaults values via IPC. This change causes the main process to pass the default values over IPC using the new API.
    >
    > BUG=392141
    >
    > Committed: https://crrev.com/54113566ce626a9fcfe5fd1dd46f0362d01ed8ed
    > Cr-Commit-Position: refs/heads/master@{#340695}
    
    TBR=palmer@chromium.org,avi@chromium.org,rsesek@chromium.org,kerrnel@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=392141
    
    Review URL: https://codereview.chromium.org/1261933003
    
    Cr-Commit-Position: refs/heads/master@{#340714}

--- content/browser/theme_helper_mac.mm
+++ content/browser/theme_helper_mac.mm
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 
 #include "base/command_line.h"
-#include "base/mac/mac_util.h"
 #include "base/mac/sdk_forward_declarations.h"
 #include "content/common/view_messages.h"
 #include "content/public/browser/browser_thread.h"
@@ -16,36 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_types.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/common/content_switches.h"
-
-namespace {
-bool GetScrollAnimationEnabled() {
-  bool enabled = false;
-  id value = nil;
-  if (base::mac::IsOSMountainLionOrLater()) {
-    value = [[NSUserDefaults standardUserDefaults]
-        objectForKey:@"NSScrollAnimationEnabled"];
-  } else {
-    value = [[NSUserDefaults standardUserDefaults]
-        objectForKey:@"AppleScrollAnimationEnabled"];
-  }
-  if (value)
-    enabled = [value boolValue];
-  return enabled;
-}
-
-blink::ScrollbarButtonsPlacement GetButtonPlacement() {
-  NSString* scrollbar_variant = [[NSUserDefaults standardUserDefaults]
-      objectForKey:@"AppleScrollBarVariant"];
-  if ([scrollbar_variant isEqualToString:@"Single"])
-    return blink::ScrollbarButtonsPlacementSingle;
-  else if ([scrollbar_variant isEqualToString:@"DoubleMin"])
-    return blink::ScrollbarButtonsPlacementDoubleStart;
-  else if ([scrollbar_variant isEqualToString:@"DoubleBoth"])
-    return blink::ScrollbarButtonsPlacementDoubleBoth;
-  else
-    return blink::ScrollbarButtonsPlacementDoubleEnd;
-}
-} // namespace
 
 @interface ScrollbarPrefsObserver : NSObject
 
@@ -72,30 +41,6 @@ suspensionBehavior:NSNotificationSuspensionBehaviorDeliverImmediately];
              name:@"AppleNoRedisplayAppearancePreferenceChanged"
            object:nil
 suspensionBehavior:NSNotificationSuspensionBehaviorCoalesce];
-
-  if (base::mac::IsOSMountainLionOrLater()) {
-    [[NSDistributedNotificationCenter defaultCenter]
-               addObserver:self
-                  selector:@selector(behaviorPrefsChanged:)
-                      name:@"NSScrollAnimationEnabled"
-                    object:nil
-        suspensionBehavior:NSNotificationSuspensionBehaviorCoalesce];
-  } else {
-    // Register for < 10.8
-    [[NSDistributedNotificationCenter defaultCenter]
-               addObserver:self
-                  selector:@selector(behaviorPrefsChanged:)
-                      name:@"AppleScrollAnimationEnabled"
-                    object:nil
-        suspensionBehavior:NSNotificationSuspensionBehaviorCoalesce];
-  }
-
-  [[NSDistributedNotificationCenter defaultCenter]
-             addObserver:self
-                selector:@selector(appearancePrefsChanged:)
-                    name:@"AppleScrollBarVariant"
-                  object:nil
-      suspensionBehavior:NSNotificationSuspensionBehaviorDeliverImmediately];
 
   // In single-process mode, renderers will catch these notifications
   // themselves and listening for them here may trigger the DCHECK in Observe().
@@ -127,8 +72,8 @@ suspensionBehavior:NSNotificationSuspensionBehaviorCoalesce];
       [defaults floatForKey:@"NSScrollerButtonDelay"],
       [defaults floatForKey:@"NSScrollerButtonPeriod"],
       [defaults boolForKey:@"AppleScrollerPagingBehavior"],
-      content::ThemeHelperMac::GetPreferredScrollerStyle(), redraw,
-      GetScrollAnimationEnabled(), GetButtonPlacement());
+      content::ThemeHelperMac::GetPreferredScrollerStyle(),
+      redraw);
 }
 
 @end
@@ -154,17 +99,13 @@ void ThemeHelperMac::SendThemeChangeToAllRenderers(
     float autoscroll_button_delay,
     bool jump_on_track_click,
     blink::ScrollerStyle preferred_scroller_style,
-    bool redraw,
-    bool scroll_animation_enabled,
-    blink::ScrollbarButtonsPlacement button_placement) {
+    bool redraw) {
   ViewMsg_UpdateScrollbarTheme_Params params;
   params.initial_button_delay = initial_button_delay;
   params.autoscroll_button_delay = autoscroll_button_delay;
   params.jump_on_track_click = jump_on_track_click;
   params.preferred_scroller_style = preferred_scroller_style;
   params.redraw = redraw;
-  params.scroll_animation_enabled = scroll_animation_enabled;
-  params.button_placement = button_placement;
 
   for (RenderProcessHost::iterator it(RenderProcessHost::AllHostsIterator());
        !it.IsAtEnd();
@@ -202,8 +143,6 @@ void ThemeHelperMac::Observe(int type,
       [defaults boolForKey:@"AppleScrollerPagingBehavior"];
   params.preferred_scroller_style = GetPreferredScrollerStyle();
   params.redraw = false;
-  params.scroll_animation_enabled = GetScrollAnimationEnabled();
-  params.button_placement = GetButtonPlacement();
 
   rph->Send(new ViewMsg_UpdateScrollbarTheme(params));
 }
