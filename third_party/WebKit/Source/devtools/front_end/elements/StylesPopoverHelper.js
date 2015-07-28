f9933f1af5b047b00440a545f04b3494409ebf94
samli@chromium.org <samli@chromium.org>
2015-07-28 02:39:52 +0000

    Devtools Color: Add material palette & palette switching to color picker
    This change adds a material design palette to the color picker and allows
    the user to select between the default palettes.
    
    BUG=461320
    
    Review URL: https://codereview.chromium.org/1228053005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199548 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/elements/StylesPopoverHelper.js
+++ third_party/WebKit/Source/devtools/front_end/elements/StylesPopoverHelper.js
@@ -16,7 +16,7 @@ WebInspector.StylesPopoverHelper = function()
 
     this._hideProxy = this.hide.bind(this, true);
     this._boundOnKeyDown = this._onKeyDown.bind(this);
-    this._repositionBound = this._reposition.bind(this);
+    this._repositionBound = this.reposition.bind(this);
     this._boundFocusOut = this._onFocusOut.bind(this);
 }
 
@@ -58,7 +58,7 @@ WebInspector.StylesPopoverHelper.prototype = {
         this._anchorElement = anchorElement;
         this._view = view;
         this._hiddenCallback = hiddenCallback;
-        this._reposition();
+        this.reposition();
 
         var document = this._popover.element.ownerDocument;
         document.addEventListener("mousedown", this._hideProxy, false);
@@ -73,7 +73,7 @@ WebInspector.StylesPopoverHelper.prototype = {
     /**
      * @param {!Event=} event
      */
-    _reposition: function(event)
+    reposition: function(event)
     {
         if (!this._previousFocusElement)
             this._previousFocusElement = WebInspector.currentFocusElement();
@@ -293,11 +293,20 @@ WebInspector.ColorSwatchPopoverIcon.prototype = {
             format = color.format();
         this._spectrum = new WebInspector.Spectrum();
         this._spectrum.setColor(color, format);
+        this._spectrum.addEventListener(WebInspector.Spectrum.Events.SizeChanged, this._spectrumResized, this);
         this._spectrum.addEventListener(WebInspector.Spectrum.Events.ColorChanged, this._boundSpectrumChanged);
         this._stylesPopoverHelper.show(this._spectrum, this._swatch.iconElement(), this._onPopoverHidden.bind(this));
 
         this._originalPropertyText = this._treeElement.property.propertyText;
         this._treeElement.parentPane().setEditingStyle(true);
+    },
+
+    /**
+     * @param {!WebInspector.Event} event
+     */
+    _spectrumResized: function(event)
+    {
+        this._stylesPopoverHelper.reposition();
     },
 
     /**
