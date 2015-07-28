8beff55c43b16f323db46c6ef4cc1d314f1d4228
fmalita@chromium.org <fmalita@chromium.org>
2015-07-28 23:54:47 +0000

    Reland of Switch Image drawing to the SkImage API (patchset #2 id:180001 of https://codereview.chromium.org/1257073005/)
    Reason for revert:
    Relanded https://codereview.chromium.org/1257253003 w/ fix, should be safe to land this again.
    
    Original issue's description:
    > Revert of Switch Image drawing to the SkImage API (patchset #6 id:100001 of https://codereview.chromium.org/1250323005/)
    > 
    > Reason for revert:
    > Pre-requisite for reverting https://codereview.chromium.org/1257453002.
    > 
    > Original issue's description:
    > > Switch Image drawing to the SkImage API
    > > 
    > > Convert the main bitmap drawing paths (Bitmap::draw, Image::drawPattern,
    > > ImageBufferSurface::draw) to the new SkImage-based API.
    > > 
    > > Also add ImageClampingMode support to StaticBitmapImage::draw().
    > > 
    > > BUG=449197
    > > R=junov@chromium.org,schenney@chromium.org,senorblanco@chromium.org
    > > 
    > > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=199459
    > 
    > TBR=junov@chromium.org,schenney@chromium.org,senorblanco@chromium.org,scroggo@chromium.org,reed@google.com
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=449197
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=199528
    
    TBR=junov@chromium.org,schenney@chromium.org,senorblanco@chromium.org,scroggo@chromium.org,reed@google.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=449197
    
    Review URL: https://codereview.chromium.org/1253353007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199613 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/ImageBufferSurface.cpp
+++ third_party/WebKit/Source/platform/graphics/ImageBufferSurface.cpp
@@ -33,9 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/graphics/ImageBufferSurface.h"
 
-#include "platform/graphics/BitmapImage.h"
 #include "platform/graphics/GraphicsContext.h"
 #include "platform/graphics/ImageBuffer.h"
+#include "platform/graphics/StaticBitmapImage.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkDevice.h"
 #include "third_party/skia/include/core/SkImage.h"
@@ -79,11 +79,12 @@ const SkBitmap& ImageBufferSurface::bitmap()
 
 void ImageBufferSurface::draw(GraphicsContext* context, const FloatRect& destRect, const FloatRect& srcRect, SkXfermode::Mode op)
 {
-    SkBitmap bmp = bitmap();
+    RefPtr<SkImage> snapshot = newImageSnapshot();
+    if (!snapshot)
+        return;
 
-    RefPtr<Image> image = BitmapImage::create(bmp);
-
-    context->drawImage(image.get(), destRect, srcRect, op, DoNotRespectImageOrientation);
+    RefPtr<Image> image = StaticBitmapImage::create(snapshot.release());
+    context->drawImage(image.get(), destRect, srcRect, op);
 }
 
 } // namespace blink
