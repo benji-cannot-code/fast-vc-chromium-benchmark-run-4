f378641a963799f2ea2412bfc50dd0beaa0d7ef1
Nico Weber <thakis@chromium.org>
2015-07-29 12:59:00 -0700

    Fix win official builds after https://codereview.chromium.org/1269533002
    BUG=505316
    TBR=miletus@chromium.org
    
    Review URL: https://codereview.chromium.org/1269563004 .
    
    Cr-Commit-Position: refs/heads/master@{#340958}

--- chrome/browser/upgrade_detector_impl.cc
+++ chrome/browser/upgrade_detector_impl.cc
@@ -91,7 +91,7 @@ int GetCheckForUpgradeEveryMs() {
   return kCheckForUpgradeMs;
 }
 
-#if !defined(OS_WIN)
+#if !defined(OS_WIN) || defined(GOOGLE_CHROME_BUILD)
 // Return true if the current build is one of the unstable channels.
 bool IsUnstableChannel() {
   // TODO(mad): Investigate whether we still need to be on the file thread for
@@ -103,7 +103,9 @@ bool IsUnstableChannel() {
   return channel == chrome::VersionInfo::CHANNEL_DEV ||
          channel == chrome::VersionInfo::CHANNEL_CANARY;
 }
+#endif  // !defined(OS_WIN) || defined(GOOGLE_CHROME_BUILD)
 
+#if !defined(OS_WIN)
 // This task identifies whether we are running an unstable version. And then it
 // unconditionally calls back the provided task.
 void CheckForUnstableChannel(const base::Closure& callback_task,
