c9b24de323eed256e7de3b8428a31c559181c527
rockot <rockot@chromium.org>
2015-07-29 15:16:45 -0700

    Revert of Re-enable ChannelMojo (patchset #1 id:80001 of https://codereview.chromium.org/1256473002/)
    Reason for revert:
    Maybe breaking layout tests...somehow
    
    Original issue's description:
    > Re-enable ChannelMojo
    >
    > I'm pretty sure the previous perf regression was a false
    > signal. Haven't reproed with other benchmarks and in
    > retrospect turning ChannelMojo off never fixed the
    > regression in question anyway.
    >
    > BUG=500019
    >
    > Committed: https://crrev.com/ccfe630130d0b4768338a88361c98cc8fc35f758
    > Cr-Commit-Position: refs/heads/master@{#340975}
    
    TBR=jam@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=500019
    
    Review URL: https://codereview.chromium.org/1256033004
    
    Cr-Commit-Position: refs/heads/master@{#341001}

--- ipc/mojo/ipc_channel_mojo.cc
+++ ipc/mojo/ipc_channel_mojo.cc
@@ -235,7 +235,9 @@ void ChannelMojo::ChannelInfoDeleter::operator()(
 
 // static
 bool ChannelMojo::ShouldBeUsed() {
-  return true;
+  // TODO(rockot): Investigate performance bottlenecks and hopefully reenable
+  // this at some point. http://crbug.com/500019
+  return false;
 }
 
 // static
