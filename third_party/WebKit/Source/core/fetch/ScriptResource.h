d5eb56db6ff61f48900172c5a870b26a7af2e463
japhet@chromium.org <japhet@chromium.org>
2015-07-29 19:59:31 +0000

    Revert of Revalidate using the same Resource, attempt #2 (patchset #1 id:1 of https://codereview.chromium.org/1237983003/)
    Reason for revert:
    This caused several different regressions that are not quick and easy to fix.
    
    https://code.google.com/p/chromium/issues/detail?id=512459
    https://code.google.com/p/chromium/issues/detail?id=512405
    https://code.google.com/p/chromium/issues/detail?id=511939
    https://code.google.com/p/chromium/issues/detail?id=513572
    
    Original issue's description:
    > Revalidate using the same Resource, attempt #2
    > 
    > Currently, when we perform a revalidation, we create a new resource and put it in the MemoryCache in place of the resource being revalidated. If the revalidation results in a 304, we silently swap all of the clients of the revalidation to the now-revalidated resource and swap which Resource is in the MemoryCache.
    > 
    > This changes the behavior to use the existing resource to revalidate itself, which simplifies the logic quite a bit. It also gives us the option of removing ResourcePtr (which is used to implement the silent client swap in the 304 case) and make Resources simply RefCounted in a future change.
    > 
    > BUG=
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=199145
    
    TBR=mkwst@chromium.org,yoav@yoav.ws
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review URL: https://codereview.chromium.org/1269563003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199677 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/ScriptResource.h
+++ third_party/WebKit/Source/core/fetch/ScriptResource.h
@@ -58,8 +58,6 @@ public:
     void didAddClient(ResourceClient*) override;
     void appendData(const char*, unsigned) override;
 
-    void destroyDecodedDataForFailedRevalidation() override;
-
     const String& script();
 
     AtomicString mimeType() const;
