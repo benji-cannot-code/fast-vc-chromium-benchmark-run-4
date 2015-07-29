ee30021f6b73201172de389298db67c431f2d55d
maxbogue <maxbogue@chromium.org>
2015-07-29 12:29:35 -0700

    [Sync] Remove isGaia from setEncryptionPassphrase JNI.
    BUG=497518, 510610
    
    Review URL: https://codereview.chromium.org/1261933002
    
    Cr-Commit-Position: refs/heads/master@{#340948}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/ProfileSyncService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/ProfileSyncService.java
@@ -319,9 +319,9 @@ public class ProfileSyncService {
         nativeEnableEncryptEverything(mNativeProfileSyncServiceAndroid);
     }
 
-    public void setEncryptionPassphrase(String passphrase, boolean isGaia) {
+    public void setEncryptionPassphrase(String passphrase) {
         assert isSyncInitialized();
-        nativeSetEncryptionPassphrase(mNativeProfileSyncServiceAndroid, passphrase, isGaia);
+        nativeSetEncryptionPassphrase(mNativeProfileSyncServiceAndroid, passphrase);
     }
 
     public boolean isCryptographerReady() {
@@ -661,7 +661,7 @@ public class ProfileSyncService {
     private native boolean nativeSetDecryptionPassphrase(
             long nativeProfileSyncServiceAndroid, String passphrase);
     private native void nativeSetEncryptionPassphrase(
-            long nativeProfileSyncServiceAndroid, String passphrase, boolean isGaia);
+            long nativeProfileSyncServiceAndroid, String passphrase);
     private native boolean nativeIsCryptographerReady(long nativeProfileSyncServiceAndroid);
     private native int nativeGetPassphraseType(long nativeProfileSyncServiceAndroid);
     private native boolean nativeHasExplicitPassphraseTime(long nativeProfileSyncServiceAndroid);
