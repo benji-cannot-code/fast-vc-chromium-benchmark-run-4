575482b7bf6e8f8d07673d1bb9113186f1a0844f
blundell <blundell@chromium.org>
2015-07-29 11:16:20 -0700

    [Omnibox] Reland 94417e2806 and a3d1b9e964.
    These CLs were not actually the culprits for the OmniboxView browsertest
    failures on Win7; see crbug.com/514934 for details.
    
    BUG=371536,511943
    TBR=pkasting
    
    Review URL: https://codereview.chromium.org/1257663004
    
    Cr-Commit-Position: refs/heads/master@{#340923}

--- chrome/browser/ui/omnibox/chrome_omnibox_client.h
+++ chrome/browser/ui/omnibox/chrome_omnibox_client.h
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_OMNIBOX_CHROME_OMNIBOX_CLIENT_H_
 
 #include "base/compiler_specific.h"
+#include "chrome/browser/bitmap_fetcher/bitmap_fetcher_service.h"
 #include "chrome/browser/ui/omnibox/omnibox_client.h"
+#include "chrome/common/instant_types.h"
 
 class OmniboxEditController;
 class Profile;
@@ -18,6 +20,8 @@ class ChromeOmniboxClient : public OmniboxClient {
   ~ChromeOmniboxClient() override;
 
   // OmniboxClient.
+  scoped_ptr<AutocompleteProviderClient>
+      CreateAutocompleteProviderClient() override;
   bool CurrentPageExists() const override;
   const GURL& GetURL() const override;
   bool IsInstantNTP() const override;
@@ -32,13 +36,27 @@ class ChromeOmniboxClient : public OmniboxClient {
   void OnInputStateChanged() override;
   void OnFocusChanged(OmniboxFocusState state,
                       OmniboxFocusChangeReason reason) override;
+  void OnResultChanged(const AutocompleteResult& result,
+                       bool default_match_changed,
+                       const base::Callback<void(const SkBitmap& bitmap)>&
+                           on_bitmap_fetched) override;
+  void OnCurrentMatchChanged(const AutocompleteMatch& match) override;
   void OnURLOpenedFromOmnibox(OmniboxLog* log) override;
   void DoPrerender(const AutocompleteMatch& match) override;
-  void SetSuggestionToPrefetch(const InstantSuggestion& suggestion) override;
+
+  // TODO(blundell): Make private once OmniboxEditModel no longer refers to it.
+  void DoPreconnect(const AutocompleteMatch& match) override;
 
  private:
+  // Sends the current SearchProvider suggestion to the Instant page if any.
+  void SetSuggestionToPrefetch(const InstantSuggestion& suggestion);
+
+  void OnBitmapFetched(const BitmapFetchedCallback& callback,
+                       const SkBitmap& bitmap);
+
   OmniboxEditController* controller_;
   Profile* profile_;
+  BitmapFetcherService::RequestId request_id_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeOmniboxClient);
 };
--- chrome/browser/ui/omnibox/omnibox_edit_model.cc
+++ chrome/browser/ui/omnibox/omnibox_edit_model.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autocomplete/chrome_autocomplete_scheme_classifier.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/bookmarks/bookmark_stats.h"
-#include "chrome/browser/extensions/api/omnibox/omnibox_api.h"
 #include "chrome/browser/net/predictor.h"
 #include "chrome/browser/predictors/autocomplete_action_predictor.h"
 #include "chrome/browser/predictors/autocomplete_action_predictor_factory.h"
@@ -58,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/user_metrics.h"
-#include "extensions/common/constants.h"
 #include "ui/gfx/image/image.h"
 #include "url/url_util.h"
 
@@ -196,9 +194,9 @@ OmniboxEditModel::OmniboxEditModel(OmniboxView* view,
                                    OmniboxEditController* controller,
                                    scoped_ptr<OmniboxClient> client,
                                    Profile* profile)
-    : view_(view),
+    : client_(client.Pass()),
+      view_(view),
       controller_(controller),
-      client_(client.Pass()),
       focus_state_(OMNIBOX_FOCUS_NONE),
       focus_source_(INVALID),
       user_input_in_progress_(false),
@@ -211,7 +209,7 @@ OmniboxEditModel::OmniboxEditModel(OmniboxView* view,
       profile_(profile),
       in_revert_(false),
       allow_exact_keyword_match_(false) {
-  omnibox_controller_.reset(new OmniboxController(this, profile));
+  omnibox_controller_.reset(new OmniboxController(this, client_.get()));
 }
 
 OmniboxEditModel::~OmniboxEditModel() {
@@ -400,7 +398,7 @@ void OmniboxEditModel::OnChanged() {
         client_->DoPrerender(current_match);
       break;
     case AutocompleteActionPredictor::ACTION_PRECONNECT:
-      omnibox_controller_->DoPreconnect(current_match);
+      client_->DoPreconnect(current_match);
       break;
     case AutocompleteActionPredictor::ACTION_NONE:
       break;
@@ -1349,6 +1347,8 @@ void OmniboxEditModel::OnCurrentMatchChanged() {
 
   const AutocompleteMatch& match = omnibox_controller_->current_match();
 
+  client_->OnCurrentMatchChanged(match);
+
   // We store |keyword| and |is_keyword_hint| in temporary variables since
   // OnPopupDataChanged use their previous state to detect changes.
   base::string16 keyword;
@@ -1363,11 +1363,6 @@ void OmniboxEditModel::OnCurrentMatchChanged() {
   // its value across the entire call.
   const base::string16 inline_autocompletion(match.inline_autocompletion);
   OnPopupDataChanged(inline_autocompletion, NULL, keyword, is_keyword_hint);
-}
-
-void OmniboxEditModel::SetSuggestionToPrefetch(
-    const InstantSuggestion& suggestion) {
-  client_->SetSuggestionToPrefetch(suggestion);
 }
 
 // static
