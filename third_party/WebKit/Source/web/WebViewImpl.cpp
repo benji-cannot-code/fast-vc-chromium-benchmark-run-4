df0c67216fdaa034740b6f54aa08b83250b281d5
hush@chromium.org <hush@chromium.org>
2015-07-29 00:42:22 +0000

    Add WebView API for smoothScroll
    This is implemented as a page scale animation with a target
    position and duration, and no change to scale.
    
    BUG=378984
    
    Review URL: https://codereview.chromium.org/1251473003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199617 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -2875,6 +2875,12 @@ bool WebViewImpl::scrollFocusedNodeIntoRect(const WebRect& rectInViewport)
     return false;
 }
 
+void WebViewImpl::smoothScroll(int targetX, int targetY, long durationMs)
+{
+    IntPoint targetPosition(targetX, targetY);
+    startPageScaleAnimation(targetPosition, false, pageScaleFactor(), (double)durationMs / 1000);
+}
+
 void WebViewImpl::computeScaleAndScrollForFocusedNode(Node* focusedNode, bool zoomInToLegibleScale, float& newScale, IntPoint& newScroll, bool& needAnimation)
 {
     focusedNode->document().updateLayoutIgnorePendingStylesheets();
