e384903fe1dbef5e9a5c3b91d1a1af505f6b02bf
krasin <krasin@google.com>
2015-07-29 13:12:45 -0700

    CFI: Fix invalid cast to PrefRegistrySyncable.
    This bug was found by Control Flow Integrity check, see
    https://sites.google.com/a/chromium.org/dev/developers/testing/control-flow-integrity
    
    LazyBackgroundTaskQueueTest creates TestingPrefServiceSimple,
    which initializes prefs registry with PrefRegistrySimple,
    which then could be incorrectly casted to its subclass
    PrefRegistrySyncable. The solution is to initialize prefs registry
    with PrefRegistrySyncable.
    
    See more details in http://crbug.com/513953
    
    BUG=chromium:513953, chromium:457523
    
    Review URL: https://codereview.chromium.org/1269523003
    
    Cr-Commit-Position: refs/heads/master@{#340962}

--- extensions/browser/lazy_background_task_queue_unittest.cc
+++ extensions/browser/lazy_background_task_queue_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/prefs/testing_pref_service.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "components/pref_registry/testing_pref_service_syncable.h"
 #include "components/user_prefs/user_prefs.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/test/test_browser_context.h"
@@ -115,7 +116,7 @@ class LazyBackgroundTaskQueueTest : public ExtensionsTest {
  private:
   scoped_ptr<content::NotificationService> notification_service_;
 
-  TestingPrefServiceSimple testing_pref_service_;
+  user_prefs::TestingPrefServiceSyncable testing_pref_service_;
 
   // The total number of pending tasks that have been executed.
   int task_run_count_;
--- extensions/extensions_tests.gyp
+++ extensions/extensions_tests.gyp
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '../base/base.gyp:base',
         '../base/base.gyp:test_support_base',
         '../components/components.gyp:keyed_service_content',
+        '../components/components.gyp:pref_registry_test_support',
         '../components/components.gyp:user_prefs',
         '../content/content_shell_and_tests.gyp:test_support_content',
         '../device/bluetooth/bluetooth.gyp:device_bluetooth_mocks',
