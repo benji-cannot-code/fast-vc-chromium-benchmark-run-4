5e3c8d0aa34217a31f259443d25c93da7af5259c
sdefresne <sdefresne@chromium.org>
2015-07-29 10:14:00 -0700

    Add utilities to remove duplication in BookmarkClient implementations.
    Add utility free functions to //components/bookmarks/managed and
    //components/favicon/core to share common code between both
    BookmarkClient implementations (desktop and iOS).
    
    BUG=514208
    
    Review URL: https://codereview.chromium.org/1262123002
    
    Cr-Commit-Position: refs/heads/master@{#340912}

--- components/bookmarks.gypi
+++ components/bookmarks.gypi
@@ -118,6 +118,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'sources': [
         'bookmarks/managed/managed_bookmark_service.cc',
         'bookmarks/managed/managed_bookmark_service.h',
+        'bookmarks/managed/managed_bookmark_util.cc',
+        'bookmarks/managed/managed_bookmark_util.h',
         'bookmarks/managed/managed_bookmarks_tracker.cc',
         'bookmarks/managed/managed_bookmarks_tracker.h',
       ],
---
+++ components/bookmarks/managed/managed_bookmark_util.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_BOOKMARKS_MANAGED_MANAGED_BOOKMARK_UTIL_H_
+#define COMPONENTS_BOOKMARKS_MANAGED_MANAGED_BOOKMARK_UTIL_H_
+
+namespace bookmarks {
+
+class BookmarkPermanentNode;
+class ManagedBookmarkService;
+
+// Returns whether |node| is a permanent node.
+bool IsPermanentNode(const BookmarkPermanentNode* node,
+                     ManagedBookmarkService* managed_bookmark_service);
+
+// Returns whether |node| is a managed node.
+bool IsManagedNode(const BookmarkPermanentNode* node,
+                   ManagedBookmarkService* managed_bookmark_service);
+
+}  // namespace bookmarks
+
+#endif  // COMPONENTS_BOOKMARKS_MANAGED_MANAGED_BOOKMARK_UTIL_H_
