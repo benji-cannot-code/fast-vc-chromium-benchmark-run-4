f30dd2a85ca0abe95dbee2fb7a2223a5a7a42cfd
yoshiki <yoshiki@chromium.org>
2015-07-29 02:23:54 -0700

    Hide the progress if the download progress can't be retrived
    BUG=499090
    TEST=manually
    
    Review URL: https://codereview.chromium.org/1255993003
    
    Cr-Commit-Position: refs/heads/master@{#340866}

--- chrome/browser/download/notification/download_notification_browsertest.cc
+++ chrome/browser/download/notification/download_notification_browsertest.cc
@@ -929,7 +929,7 @@ IN_PROC_BROWSER_TEST_F(DownloadNotificationTest,
   EXPECT_EQ(download_incognito, downloads[0]);
 
   // Confirms the types of download notifications are correct.
-  EXPECT_EQ(message_center::NOTIFICATION_TYPE_PROGRESS,
+  EXPECT_EQ(message_center::NOTIFICATION_TYPE_BASE_FORMAT,
             GetNotification(notification_id1)->type());
   EXPECT_EQ(message_center::NOTIFICATION_TYPE_PROGRESS,
             GetNotification(notification_id2)->type());
@@ -1097,7 +1097,7 @@ IN_PROC_BROWSER_TEST_F(MultiProfileDownloadNotificationTest,
 
   // Confirms the types of download notifications are correct.
   // Normal notification for user1.
-  EXPECT_EQ(message_center::NOTIFICATION_TYPE_PROGRESS,
+  EXPECT_EQ(message_center::NOTIFICATION_TYPE_BASE_FORMAT,
             GetNotification(notification_id_user1)->type());
   // Group notification for user2.
   EXPECT_EQ(message_center::NOTIFICATION_TYPE_MULTIPLE,
@@ -1105,9 +1105,9 @@ IN_PROC_BROWSER_TEST_F(MultiProfileDownloadNotificationTest,
   EXPECT_EQ(2u,
             GetNotification(notification_id_user2_group)->items().size());
   // Normal notification for user2.
-  EXPECT_EQ(message_center::NOTIFICATION_TYPE_PROGRESS,
+  EXPECT_EQ(message_center::NOTIFICATION_TYPE_BASE_FORMAT,
             GetNotification(notification_id_user2_1)->type());
-  EXPECT_EQ(message_center::NOTIFICATION_TYPE_PROGRESS,
+  EXPECT_EQ(message_center::NOTIFICATION_TYPE_BASE_FORMAT,
             GetNotification(notification_id_user2_2)->type());
 
   // Requests to complete the downloads.
