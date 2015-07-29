4c426aea8581307c55b28eaa43dc5392c5cd0881
boliu <boliu@chromium.org>
2015-07-29 11:37:57 -0700

    Hold message in SyncMessageFilter before init
    SyncMessageFilter::send will fail before init (OnFilterAdded)
    on IO thread but provides no init signal externally. This can
    cause races during start up.
    
    Instead, before init, hold any messages in a queue and
    flush the queue in init instead.
    
    BUG=514929
    
    Review URL: https://codereview.chromium.org/1261253002
    
    Cr-Commit-Position: refs/heads/master@{#340930}

--- ipc/ipc_sync_message_filter.h
+++ ipc/ipc_sync_message_filter.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/scoped_vector.h"
 #include "base/synchronization/lock.h"
 #include "ipc/ipc_sender.h"
 #include "ipc/ipc_sync_message.h"
@@ -59,6 +60,9 @@ class IPC_EXPORT SyncMessageFilter : public MessageFilter, public Sender {
 
   typedef std::set<PendingSyncMsg*> PendingSyncMessages;
   PendingSyncMessages pending_sync_messages_;
+
+  // Messages waiting to be delivered after IO initialization.
+  ScopedVector<Message> pending_messages_;
 
   // Locks data members above.
   base::Lock lock_;
