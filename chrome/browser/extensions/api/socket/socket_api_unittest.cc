3dc4d2e1308353093c72a2fe29297ed8b889892f
krasin <krasin@google.com>
2015-07-29 13:36:32 -0700

    Fix invalid casts in CreateApiResourceManagerForTest callers.
    This bug was found by Control Flow Integrity check:
    https://sites.google.com/a/chromium.org/dev/developers/testing/control-flow-integrity
    
    BUG=chromium:514817, chromium:457523
    
    Review URL: https://codereview.chromium.org/1256863005
    
    Cr-Commit-Position: refs/heads/master@{#340968}

--- chrome/browser/extensions/api/socket/socket_api_unittest.cc
+++ chrome/browser/extensions/api/socket/socket_api_unittest.cc
@@ -20,10 +20,7 @@ namespace extensions {
 
 scoped_ptr<KeyedService> ApiResourceManagerTestFactory(
     content::BrowserContext* context) {
-  content::BrowserThread::ID id;
-  CHECK(content::BrowserThread::GetCurrentThreadIdentifier(&id));
-  return ApiResourceManager<Socket>::CreateApiResourceManagerForTest(context,
-                                                                     id);
+  return make_scoped_ptr(new ApiResourceManager<Socket>(context));
 }
 
 class SocketUnitTest : public ExtensionApiUnittest {
