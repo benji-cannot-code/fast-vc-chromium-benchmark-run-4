4b332346a8be99f55df8bdb3941112149e96d2c4
kkhorimoto <kkhorimoto@chromium.org>
2015-07-29 10:54:44 -0700

    Created UIView+SizeClassSupport and size_class_support_util.
    UIView+SizeClassSupport:
    - Defines SizeClassIdiom, which can be used to define constants as
      arrays when different values should be used according to the size
      class.
    - Exposes a UIView's width and height size class idioms as readonly
      properties.
    
    size_class_support_util:
    - Defines GetImageForSizeClass(), which fetches an image with an
      optional "_large" suffix for REGULAR size classes.  This is because
      smaller images are sometimes used on iPad when multitasking, making
      the "~ipad" naming convention no longer applicable.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1223133005
    
    Cr-Commit-Position: refs/heads/master@{#340916}

---
+++ ios/chrome/browser/ui/UIView+SizeClassSupport.mm
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/UIView+SizeClassSupport.h"
+
+namespace {
+
+SizeClassIdiom GetSizeClassIdiom(UIUserInterfaceSizeClass size_class) {
+  return size_class == UIUserInterfaceSizeClassCompact ? COMPACT : REGULAR;
+}
+
+}  // namespace
+
+@implementation UIView (SizeClassSupport)
+
+- (SizeClassIdiom)cr_widthSizeClass {
+  return GetSizeClassIdiom(self.traitCollection.horizontalSizeClass);
+}
+
+- (SizeClassIdiom)cr_heightSizeClass {
+  return GetSizeClassIdiom(self.traitCollection.verticalSizeClass);
+}
+
+@end
