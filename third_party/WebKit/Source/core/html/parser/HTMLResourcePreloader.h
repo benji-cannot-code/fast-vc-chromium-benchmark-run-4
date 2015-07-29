ac98a5a657327031325277b542bfbe12667b2d61
mostynb@opera.com <mostynb@opera.com>
2015-07-29 09:52:06 +0000

    export the entire HTMLResourcePreloader class in shared_library builds
    After https://codereview.chromium.org/1112833004/ landed, libblink_web.so
    fails to link in some configurations, giving this error:
    
    obj/third_party/WebKit/Source/core/html/parser/blink_web.HTMLResourcePreloaderTest.o:HTMLResourcePreloaderTest.cpp:function blink::HTMLResourcePreloaderTest::test(blink::PreconnectTestCase): error: undefined reference to 'vtable for blink::HTMLResourcePreloader'
    /usr/bin/ld.gold: the vtable symbol may be undefined because the class is missing its key function
    
    The blink::HTMLResourcePreloader vtable exists in lib/libwebcore_shared.so
    but only as a local symbol, because I assume that the HTMLResourcePreloader
    class has some un-exported virtual methods in these configuration.  So we
    should be able to solve this by exporting the entire HTMLResourcePreloader
    class.
    
    Review URL: https://codereview.chromium.org/1266533002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199651 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/HTMLResourcePreloader.h
+++ third_party/WebKit/Source/core/html/parser/HTMLResourcePreloader.h
@@ -37,15 +37,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class HTMLResourcePreloader final : public NoBaseWillBeGarbageCollected<HTMLResourcePreloader>, public ResourcePreloader {
+class CORE_EXPORT HTMLResourcePreloader final : public NoBaseWillBeGarbageCollected<HTMLResourcePreloader>, public ResourcePreloader {
     WTF_MAKE_NONCOPYABLE(HTMLResourcePreloader); WTF_MAKE_FAST_ALLOCATED_WILL_BE_REMOVED(HTMLResourcePreloader);
     friend class HTMLResourcePreloaderTest;
 public:
-    static CORE_EXPORT PassOwnPtrWillBeRawPtr<HTMLResourcePreloader> create(Document&);
+    static PassOwnPtrWillBeRawPtr<HTMLResourcePreloader> create(Document&);
     DECLARE_TRACE();
 
 protected:
-    CORE_EXPORT void preload(PassOwnPtr<PreloadRequest>, const NetworkHintsInterface&) override;
+    void preload(PassOwnPtr<PreloadRequest>, const NetworkHintsInterface&) override;
 
 private:
     explicit HTMLResourcePreloader(Document&);
