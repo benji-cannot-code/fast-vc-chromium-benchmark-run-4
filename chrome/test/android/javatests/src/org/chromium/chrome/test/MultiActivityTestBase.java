fa0edf46eeb2617073c5066e6a9efd001f5aecd7
skirmani <skirmani@google.com>
2015-07-30 10:04:12 -0700

    - Added RESTRICTION_TYPE_INTERNET - Removed RestrictedInstrumentationTestCase - Added RestrictionSkipCheck to BaseInstrumentationTestRunner - Added ChromeRestrictionSkipCheck to ChromeInstrumentationTestRunner
    Review URL: https://codereview.chromium.org/1259253007
    
    Cr-Commit-Position: refs/heads/master@{#341137}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/MultiActivityTestBase.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/MultiActivityTestBase.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.test;
 
 import android.content.Context;
+import android.test.InstrumentationTestCase;
 import android.text.TextUtils;
 
 import org.chromium.base.test.util.CommandLineFlags;
@@ -25,7 +26,7 @@ import org.chromium.content.browser.test.util.CriteriaHelper;
 @CommandLineFlags.Add({
         ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE
         })
-public abstract class MultiActivityTestBase extends RestrictedInstrumentationTestCase {
+public abstract class MultiActivityTestBase extends InstrumentationTestCase {
     protected static final String URL_1 = createTestUrl(1);
     protected static final String URL_2 = createTestUrl(2);
     protected static final String URL_3 = createTestUrl(3);
