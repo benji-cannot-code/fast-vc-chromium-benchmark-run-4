c226edc062a11c66ef78c5ee79ae3aa84734d7f1
avi <avi@chromium.org>
2015-07-30 11:14:45 -0700

    Marking TouchInputBrowserTest tests flaky.
    BUG=514456
    TEST=as noted
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1269793004
    
    Cr-Commit-Position: refs/heads/master@{#341161}

--- content/browser/renderer_host/input/touch_input_browsertest.cc
+++ content/browser/renderer_host/input/touch_input_browsertest.cc
@@ -200,7 +200,7 @@ IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MAYBE_TouchNoHandler) {
   touch.ResetPoints();
 }
 
-#if defined(OS_CHROMEOS) && defined(MEMORY_SANITIZER)
+#if defined(OS_CHROMEOS)
 // crbug.com/514456
 #define MAYBE_TouchHandlerNoConsume DISABLED_TouchHandlerNoConsume
 #else
@@ -223,7 +223,7 @@ IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MAYBE_TouchHandlerNoConsume) {
   touch.ResetPoints();
 }
 
-#if defined(OS_CHROMEOS) && defined(MEMORY_SANITIZER)
+#if defined(OS_CHROMEOS)
 // crbug.com/514456
 #define MAYBE_TouchHandlerConsume DISABLED_TouchHandlerConsume
 #else
@@ -245,7 +245,7 @@ IN_PROC_BROWSER_TEST_F(TouchInputBrowserTest, MAYBE_TouchHandlerConsume) {
   filter()->WaitForAck(WebInputEvent::TouchEnd);
 }
 
-#if defined(OS_CHROMEOS) && defined(MEMORY_SANITIZER)
+#if defined(OS_CHROMEOS)
 // crbug.com/514456
 #define MAYBE_MultiPointTouchPress DISABLED_MultiPointTouchPress
 #elif defined(OS_MACOSX)
