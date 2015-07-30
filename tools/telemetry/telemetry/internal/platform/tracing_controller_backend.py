13eeb6131f32ae7290d0d0df20321e446b398373
avi <avi@chromium.org>
2015-07-30 16:09:40 -0700

    Revert of Create classes_util API, change discover to return a list instead of a dict. (patchset #5 id:120001 of https://codereview.chromium.org/1244223002/)
    Reason for revert:
    This appears to break the XP telemetry tests. Details on the bug.
    
    Original issue's description:
    > Create classes_util API, change discover to return a list instead of a dict.
    >
    > BUG=498968
    > CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    >
    > Committed: https://crrev.com/e6cbec4747aa7fe3c96b1cdb89de21ae77b30ab0
    > Cr-Commit-Position: refs/heads/master@{#341129}
    
    TBR=dtu@chromium.org,bengr@chromium.org,aiolos@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=498968
    
    Review URL: https://codereview.chromium.org/1263063003
    
    Cr-Commit-Position: refs/heads/master@{#341218}

--- tools/telemetry/telemetry/internal/platform/tracing_controller_backend.py
+++ tools/telemetry/telemetry/internal/platform/tracing_controller_backend.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import os
 
+from telemetry.core import discover
 from telemetry.core import util
 from telemetry.internal.platform import tracing_agent
 from telemetry.internal.platform.tracing_agent import (
@@ -12,14 +13,14 @@ from telemetry.internal.platform.tracing_agent import (
 from telemetry.timeline import trace_data as trace_data_module
 from telemetry.timeline import tracing_category_filter
 from telemetry.timeline import tracing_options
-from telemetry.util import classes_util
 
 
 def _IterAllTracingAgentClasses():
   tracing_agent_dir = os.path.join(
       os.path.dirname(os.path.realpath(__file__)), 'tracing_agent')
-  return classes_util.DiscoverClasses(
-      tracing_agent_dir, util.GetTelemetryDir(), tracing_agent.TracingAgent)
+  return discover.DiscoverClasses(
+      tracing_agent_dir, util.GetTelemetryDir(),
+      tracing_agent.TracingAgent).itervalues()
 
 
 class TracingControllerBackend(object):
