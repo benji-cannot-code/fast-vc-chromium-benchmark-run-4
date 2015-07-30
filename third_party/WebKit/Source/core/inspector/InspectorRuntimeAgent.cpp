7839e70ca5c60183c4d59a9e6d7621f2214563a0
keishi@chromium.org <keishi@chromium.org>
2015-07-30 07:09:17 +0000

    Oilpan: Remove raw pointer to InspectorRuntimeAgent from InjectedScriptCallScope
    BUG=509911
    
    Review URL: https://codereview.chromium.org/1259863004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199711 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorRuntimeAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorRuntimeAgent.cpp
@@ -49,6 +49,7 @@ static const char runtimeEnabled[] = "runtimeEnabled";
 };
 
 class InspectorRuntimeAgent::InjectedScriptCallScope {
+    STACK_ALLOCATED();
 public:
     InjectedScriptCallScope(InspectorRuntimeAgent* agent, bool doNotPauseOnExceptionsAndMuteConsole)
         : m_agent(agent)
@@ -81,7 +82,7 @@ private:
         return presentState;
     }
 
-    InspectorRuntimeAgent* m_agent;
+    RawPtrWillBeMember<InspectorRuntimeAgent> m_agent;
     bool m_doNotPauseOnExceptionsAndMuteConsole;
     V8Debugger::PauseOnExceptionsState m_previousPauseOnExceptionsState;
 };
