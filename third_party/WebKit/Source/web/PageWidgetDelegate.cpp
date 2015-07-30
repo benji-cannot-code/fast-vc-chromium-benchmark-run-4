d68f39af6bcedab02b85d5e00bd24f740af8e686
sergeyv@chromium.org <sergeyv@chromium.org>
2015-07-30 22:17:48 +0000

    PageOverlays: do not paint overlays when accelerated compositing is disabled
    BUG=
    
    Review URL: https://codereview.chromium.org/1266533003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199761 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/PageWidgetDelegate.cpp
+++ third_party/WebKit/Source/web/PageWidgetDelegate.cpp
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/paint/SkPictureBuilder.h"
 #include "platform/transforms/AffineTransform.h"
 #include "public/web/WebInputEvent.h"
-#include "web/PageOverlayList.h"
 #include "web/WebInputEventConversion.h"
 #include "wtf/CurrentTime.h"
 
@@ -67,7 +66,7 @@ void PageWidgetDelegate::layout(Page& page, LocalFrame& root)
     page.animator().updateLayoutAndStyleForPainting(&root);
 }
 
-static void paintInternal(Page& page, PageOverlayList* overlays, WebCanvas* canvas,
+static void paintInternal(Page& page, WebCanvas* canvas,
     const WebRect& rect, LocalFrame& root, const GlobalPaintFlags globalPaintFlags)
 {
     if (rect.isEmpty())
@@ -93,8 +92,6 @@ static void paintInternal(Page& page, PageOverlayList* overlays, WebCanvas* canv
             ClipRecorder clipRecorder(paintContext, root, DisplayItem::PageWidgetDelegateClip, LayoutRect(dirtyRect));
 
             view->paint(&paintContext, globalPaintFlags, dirtyRect);
-            if (overlays)
-                overlays->paintWebFrame(paintContext);
         } else if (!DrawingRecorder::useCachedDrawingIfPossible(paintContext, root, DisplayItem::PageWidgetDelegateBackgroundFallback)) {
             DrawingRecorder drawingRecorder(paintContext, root, DisplayItem::PageWidgetDelegateBackgroundFallback, dirtyRect);
             paintContext.fillRect(dirtyRect, Color::white);
@@ -103,16 +100,16 @@ static void paintInternal(Page& page, PageOverlayList* overlays, WebCanvas* canv
     pictureBuilder.endRecording()->playback(canvas);
 }
 
-void PageWidgetDelegate::paint(Page& page, PageOverlayList* overlays, WebCanvas* canvas,
+void PageWidgetDelegate::paint(Page& page, WebCanvas* canvas,
     const WebRect& rect, LocalFrame& root)
 {
-    paintInternal(page, overlays, canvas, rect, root, GlobalPaintNormalPhase);
+    paintInternal(page, canvas, rect, root, GlobalPaintNormalPhase);
 }
 
-void PageWidgetDelegate::paintIgnoringCompositing(Page& page, PageOverlayList* overlays, WebCanvas* canvas,
+void PageWidgetDelegate::paintIgnoringCompositing(Page& page, WebCanvas* canvas,
     const WebRect& rect, LocalFrame& root)
 {
-    paintInternal(page, overlays, canvas, rect, root, GlobalPaintFlattenCompositingLayers);
+    paintInternal(page, canvas, rect, root, GlobalPaintFlattenCompositingLayers);
 }
 
 bool PageWidgetDelegate::handleInputEvent(PageWidgetEventHandler& handler, const WebInputEvent& event, LocalFrame* root)
