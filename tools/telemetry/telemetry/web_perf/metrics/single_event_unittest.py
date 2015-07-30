3078b454bcfc651129c1a22a3f7446203c0a6a60
mfomitchev <mfomitchev@chromium.org>
2015-07-30 08:55:09 -0700

    Adding telemetry tests for touch text selection performance.
    This CL adds a new page set |text_selection_sites|, which contains pages from
    top_10_mobile page set and peforms text selection and selection handle drag on
    the pages.
    
    There's two benchmarks: one for "direction" selection strategy, and another one
    for "character" selection strategy.
    
    BUG=497779
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:win_perf_bisect
    
    Review URL: https://codereview.chromium.org/1190423003
    
    Cr-Commit-Position: refs/heads/master@{#341124}

--- tools/telemetry/telemetry/web_perf/metrics/single_event_unittest.py
+++ tools/telemetry/telemetry/web_perf/metrics/single_event_unittest.py
@@ -8,44 +8,52 @@ import unittest
 from collections import namedtuple
 from telemetry.internal.results import page_test_results
 from telemetry.page import page
-from telemetry.web_perf.metrics import layout
+from telemetry.web_perf.metrics import single_event
 from telemetry.web_perf import timeline_interaction_record
 
-FakeEvent = namedtuple('Event', 'name, start, end')
+TRACE_EVENT_NAME = 'FrameView::performLayout'
+METRIC_NAME = 'layout'
+FakeEventTuple = namedtuple(
+    'Event',
+    'start, end, name, duration, thread_duration, has_thread_timestamps')
 Interaction = timeline_interaction_record.TimelineInteractionRecord
 
+class SingleEventTestMetric(single_event._SingleEventMetric):
+  def __init__(self):
+    super(SingleEventTestMetric, self).__init__(TRACE_EVENT_NAME, METRIC_NAME)
 
-def GetLayoutMetrics(events, interactions):
+def GetSingleEventMetrics(events, interactions):
   results = page_test_results.PageTestResults()
   results.WillRunPage(page.Page('file://blank.html'))
-  layout.LayoutMetric()._AddResultsInternal(events, interactions, results)
+  SingleEventTestMetric()._AddResultsInternal(events, interactions, results)
   return dict((value.name, value.values) for value in
               results.current_page_run.values)
 
-def FakeLayoutEvent(start, end):
-  return FakeEvent(layout.LayoutMetric.EVENT_NAME, start, end)
+def FakeEvent(start, end, name=TRACE_EVENT_NAME):
+  dur = end - start
+  return FakeEventTuple(start, end, name, dur, dur, True)
 
 
-class LayoutMetricUnitTest(unittest.TestCase):
-  def testLayoutMetric(self):
-    events = [FakeLayoutEvent(0, 1),
-              FakeLayoutEvent(9, 11),
-              FakeLayoutEvent(10, 13),
-              FakeLayoutEvent(20, 24),
-              FakeLayoutEvent(21, 26),
-              FakeLayoutEvent(29, 35),
-              FakeLayoutEvent(30, 37),
-              FakeLayoutEvent(40, 48),
-              FakeLayoutEvent(41, 50),
-              FakeEvent('something', 10, 13),
-              FakeEvent('FrameView::something', 20, 24),
-              FakeEvent('SomeThing::performLayout', 30, 37),
-              FakeEvent('something else', 40, 48)]
+class SingleEventMetricUnitTest(unittest.TestCase):
+  def testSingleEventMetric(self):
+    events = [FakeEvent(0, 1),
+              FakeEvent(9, 11),
+              FakeEventTuple(10, 13, TRACE_EVENT_NAME, 3, 0, False),
+              FakeEvent(20, 24),
+              FakeEvent(21, 26),
+              FakeEvent(29, 35),
+              FakeEvent(30, 37),
+              FakeEvent(40, 48),
+              FakeEvent(41, 50),
+              FakeEvent(10, 13, name='something'),
+              FakeEvent(20, 24, name='FrameView::something'),
+              FakeEvent(30, 37, name='SomeThing::performLayout'),
+              FakeEvent(40, 48, name='something else')]
     interactions = [Interaction('interaction', 10, 20),
                     Interaction('interaction', 30, 40)]
 
-    self.assertFalse(GetLayoutMetrics(events, []))
-    self.assertFalse(GetLayoutMetrics([], interactions))
+    self.assertFalse(GetSingleEventMetrics(events, []))
+    self.assertFalse(GetSingleEventMetrics([], interactions))
 
     # The first event starts before the first interaction, so it is ignored.
     # The second event starts before the first interaction, so it is ignored.
@@ -60,6 +68,6 @@ class LayoutMetricUnitTest(unittest.TestCase):
     # The eighth event starts during the second interaction, and its duration is
     # 48 - 40 = 8.
     # The ninth event starts after the last interaction, so it is ignored.
-    # The rest of the events are not layout events, so they are ignored.
-    self.assertEqual({'layout': [3, 4, 7, 8]}, GetLayoutMetrics(
+    # The rest of the events have the wrong name, so they are ignored.
+    self.assertEqual({METRIC_NAME: [3, 4, 7, 8]}, GetSingleEventMetrics(
         events, interactions))
