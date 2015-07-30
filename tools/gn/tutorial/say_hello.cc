a9b1cb3395b1a0599e0e639683a89f148e435532
andybons <andybons@chromium.org>
2015-07-30 15:38:01 -0700

    Clean up some typos in GN quick start page.
    Also adjust the sample code a bit.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1259763004
    
    Cr-Commit-Position: refs/heads/master@{#341213}

--- tools/gn/tutorial/say_hello.cc
+++ tools/gn/tutorial/say_hello.cc
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 int main() {
 #if defined(TWO_PEOPLE)
-  Hello("Bill", "Ted");
+  Hello("Bill", "Joy");
 #else
-  Hello("you guys");
+  Hello("everyone");
 #endif
   return 0;
 }
