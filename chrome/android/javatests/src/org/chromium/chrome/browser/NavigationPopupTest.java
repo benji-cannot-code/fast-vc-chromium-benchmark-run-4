38d2034b8908522890fb50930d03e801a726d500
pkotwicz <pkotwicz@chromium.org>
2015-07-30 10:57:55 -0700

    Reenable NavigationPopupTest#testItemSelection
    BUG=513497
    TEST=None
    
    Review URL: https://codereview.chromium.org/1263803003
    
    Cr-Commit-Position: refs/heads/master@{#341158}

--- chrome/android/javatests/src/org/chromium/chrome/browser/NavigationPopupTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/NavigationPopupTest.java
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser;
 
 import android.graphics.Bitmap;
+import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.DisabledTest;
+import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.chrome.test.ChromeActivityTestCaseBase;
 import org.chromium.content.browser.test.util.Criteria;
@@ -251,17 +253,14 @@ public class NavigationPopupTest extends ChromeActivityTestCaseBase<ChromeActivi
         });
     }
 
-    /*
     @SmallTest
     @Feature({"Navigation"})
-    crbug.com/513497
-    */
-    @DisabledTest
     public void testItemSelection() {
         final TestNavigationController controller = new TestNavigationController();
         final NavigationPopup popup = new NavigationPopup(
                 getActivity(), controller, true);
         popup.setWidth(300);
+        popup.setHeight(300);
         popup.setAnchorView(getActivity().getCurrentContentViewCore().getContainerView());
         ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
