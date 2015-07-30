1f361dfb06847c2c18ece02b7636330ed96a4130
yutak@chromium.org <yutak@chromium.org>
2015-07-30 07:17:08 +0000

    Revert of Delete AuthorStyleInfo and move its data into ComputedStyle.RareNonInheritedData. (patchset #15 id:280001 of https://codereview.chromium.org/1251523003/)
    Reason for revert:
    Broke Blink MSAN bot.
    
    http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20MSAN/builds/4757
    
    Original issue's description:
    > Delete AuthorStyleInfo & move its data into ComputedStyle.RareNonInheritedData.
    > 
    > This allows caching of styles that may be affected by LayoutThemes as
    > the data is now available even when cached.
    > 
    > 
    > BUG=259995
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=199708
    
    TBR=dstockwell@chromium.org,ojan@chromium.org,timloh@chromium.org,meade@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=259995
    
    Review URL: https://codereview.chromium.org/1266723004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199712 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/StyleAdjuster.h
+++ third_party/WebKit/Source/core/css/resolver/StyleAdjuster.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class AuthorStyleInfo;
 class Element;
 class HTMLElement;
 class ComputedStyle;
@@ -41,7 +42,7 @@ public:
         : m_useQuirksModeStyles(useQuirksModeStyles)
     { }
 
-    void adjustComputedStyle(ComputedStyle& styleToAdjust, const ComputedStyle& parentStyle, Element*);
+    void adjustComputedStyle(ComputedStyle& styleToAdjust, const ComputedStyle& parentStyle, Element*, const AuthorStyleInfo&);
 
 private:
     void adjustStyleForFirstLetter(ComputedStyle&);
