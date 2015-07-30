37f0f1c091d823ca1b78a7f3ef362a4863b85c3c
kochi@chromium.org <kochi@chromium.org>
2015-07-30 02:35:17 +0000

    getDestinationInsertionPoints() should return unclosed insertion points.
    Spec:
    http://w3c.github.io/webcomponents/spec/shadow/#widl-Element-getDestinationInsertionPoints-NodeList
    
    BUG=459136
    TEST=fast/dom/shadow/get-destination-insertion-points.html, get-destination-insertion-points-re-distribution.html
    
    Review URL: https://codereview.chromium.org/1264563002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199698 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Node.cpp
+++ third_party/WebKit/Source/core/dom/Node.cpp
@@ -2236,8 +2236,9 @@ PassRefPtrWillBeRawPtr<StaticNodeList> Node::getDestinationInsertionPoints()
     for (size_t i = 0; i < insertionPoints.size(); ++i) {
         InsertionPoint* insertionPoint = insertionPoints[i];
         ASSERT(insertionPoint->containingShadowRoot());
-        if (insertionPoint->containingShadowRoot()->type() != ShadowRootType::UserAgent)
-            filteredInsertionPoints.append(insertionPoint);
+        if (!insertionPoint->containingShadowRoot()->isOpen())
+            break;
+        filteredInsertionPoints.append(insertionPoint);
     }
     return StaticNodeList::adopt(filteredInsertionPoints);
 }
