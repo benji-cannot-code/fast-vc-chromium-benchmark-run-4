58690ae4c129cbf93308b2d9591ca97d37f207f6
shuchen <shuchen@chromium.org>
2015-07-30 00:29:00 -0700

    DispatchKeyEventPostIME could cause InputMethod instance being destructed, so return earlier when detecting the destruction.
    BUG=513917
    TEST=Will redo the test
    https://cluster-fuzz.appspot.com/testcase?key=4953236518207488 to verify
    whether the crash is fixed.
    
    Review URL: https://codereview.chromium.org/1268433002
    
    Cr-Commit-Position: refs/heads/master@{#341080}

--- ui/base/ime/input_method_win.cc
+++ ui/base/ime/input_method_win.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/ime/input_method_win.h"
 
+#include "base/auto_reset.h"
 #include "base/basictypes.h"
 #include "base/profiler/scoped_tracker.h"
 #include "ui/base/ime/text_input_client.h"
@@ -33,8 +34,14 @@ InputMethodWin::InputMethodWin(internal::InputMethodDelegate* delegate,
       enabled_(false),
       is_candidate_popup_open_(false),
       composing_window_handle_(NULL),
-      suppress_next_char_(false) {
+      suppress_next_char_(false),
+      destroyed_ptr_(nullptr) {
   SetDelegate(delegate);
+}
+
+InputMethodWin::~InputMethodWin() {
+  if (destroyed_ptr_)
+    *destroyed_ptr_ = true;
 }
 
 void InputMethodWin::OnFocus() {
@@ -132,8 +139,13 @@ bool InputMethodWin::DispatchKeyEvent(const ui::KeyEvent& event) {
     }
   }
 
-  suppress_next_char_ = DispatchKeyEventPostIME(event);
-  return suppress_next_char_;
+  bool destroyed = false;
+  base::AutoReset<bool*> auto_reset(&destroyed_ptr_, &destroyed);
+  bool handled = DispatchKeyEventPostIME(event);
+  if (destroyed)
+    return true;
+  suppress_next_char_ = handled;
+  return handled;
 }
 
 void InputMethodWin::OnTextInputTypeChanged(const TextInputClient* client) {
