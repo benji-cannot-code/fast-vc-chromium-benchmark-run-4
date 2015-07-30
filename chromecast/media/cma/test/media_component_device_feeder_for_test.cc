505bdd6c8ad35fe00b1d0623dca3d87d540ff1a0
cleichner <cleichner@chromium.org>
2015-07-30 11:45:34 -0700

    Revert of [Chromecast] Add explicit task_runner in CMA test (patchset #4 id:60001 of https://codereview.chromium.org/1267563003/)
    Reason for revert:
    This was actually necessary because of a bug in which I incorrectly used weak_ptr in the internal change. That problem was caught in code review so this is no longer necessary.
    
    Original issue's description:
    > [Chromecast] Use BindToCurrentLoop in CMA test
    >
    > Without this the callback posts tasks on whatever thread it's callback
    > was originally called on, which can cause it to not respect the CMA
    > pipeline's threading assumptions.
    >
    > Committed: https://crrev.com/9104732746ce7dd18dee6308581e8e7327bb99f1
    > Cr-Commit-Position: refs/heads/master@{#341043}
    
    TBR=erickung@google.com,kmackay@chromium.org,lcwu@google.com,halliwell@chromium.org,lcwu@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1254223003
    
    Cr-Commit-Position: refs/heads/master@{#341164}

--- chromecast/media/cma/test/media_component_device_feeder_for_test.cc
+++ chromecast/media/cma/test/media_component_device_feeder_for_test.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop/message_loop.h"
+#include "base/single_thread_task_runner.h"
 #include "base/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "chromecast/media/base/decrypt_context.h"
@@ -25,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/media/cma/base/decoder_buffer_base.h"
 #include "chromecast/media/cma/test/frame_segmenter_for_test.h"
 #include "media/base/audio_decoder_config.h"
-#include "media/base/bind_to_current_loop.h"
 #include "media/base/buffers.h"
 #include "media/base/decoder_buffer.h"
 #include "media/base/video_decoder_config.h"
@@ -79,11 +79,12 @@ void MediaComponentDeviceFeederForTest::Feed() {
   DCHECK(!frames_.empty());
   scoped_refptr<DecoderBufferBase> buffer = frames_.front();
 
-  MediaComponentDevice::FrameStatus status = media_component_device_->PushFrame(
-      scoped_refptr<DecryptContext>(), buffer,
-      ::media::BindToCurrentLoop(
+  MediaComponentDevice::FrameStatus status =
+      media_component_device_->PushFrame(
+          scoped_refptr<DecryptContext>(),
+          buffer,
           base::Bind(&MediaComponentDeviceFeederForTest::OnFramePushed,
-                     base::Unretained(this))));
+                     base::Unretained(this)));
   EXPECT_NE(status, MediaComponentDevice::kFrameFailed);
   frames_.pop_front();
 
