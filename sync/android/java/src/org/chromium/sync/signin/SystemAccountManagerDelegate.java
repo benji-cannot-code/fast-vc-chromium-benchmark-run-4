a1764cfe8922c8a309eccc19e33061f4d13250b3
nyquist <nyquist@chromium.org>
2015-07-30 16:55:25 -0700

    Add histogram for tracking the time for retrieving system accounts.
    On Android, it might be slow to retrieve the list of accounts, and
    as such, we should track how long time it takes to query for them,
    since this is done from the main thread.
    
    BUG=509826
    
    Review URL: https://codereview.chromium.org/1260663004
    
    Cr-Commit-Position: refs/heads/master@{#341228}

--- sync/android/java/src/org/chromium/sync/signin/SystemAccountManagerDelegate.java
+++ sync/android/java/src/org/chromium/sync/signin/SystemAccountManagerDelegate.java
@@ -13,6 +13,12 @@ import android.accounts.AuthenticatorDescription;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemClock;
+
+import org.chromium.base.library_loader.LibraryLoader;
+import org.chromium.base.metrics.RecordHistogram;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Default implementation of {@link AccountManagerDelegate} which delegates all calls to the
@@ -33,7 +39,11 @@ public class SystemAccountManagerDelegate implements AccountManagerDelegate {
         if (!AccountManagerHelper.get(mApplicationContext).hasGetAccountsPermission()) {
             return new Account[]{};
         }
-        return mAccountManager.getAccountsByType(type);
+        long now = SystemClock.elapsedRealtime();
+        Account[] accounts = mAccountManager.getAccountsByType(type);
+        long elapsed = SystemClock.elapsedRealtime() - now;
+        recordElapsedTimeHistogram("Signin.AndroidGetAccountsTime_AccountManager", elapsed);
+        return accounts;
     }
 
     @Override
@@ -57,5 +67,18 @@ public class SystemAccountManagerDelegate implements AccountManagerDelegate {
     public AccountManagerFuture<Boolean> hasFeatures(Account account, String[] features,
             AccountManagerCallback<Boolean> callback, Handler handler) {
         return mAccountManager.hasFeatures(account, features, callback, handler);
+    }
+
+    /**
+     * Records a histogram value for how long time an action has taken using
+     * {@link RecordHistogram#recordTimesHistogram(String, long, TimeUnit))} iff the browser
+     * process has been initialized.
+     *
+     * @param histogramName the name of the histogram.
+     * @param elapsedMs the elapsed time in milliseconds.
+     */
+    protected static void recordElapsedTimeHistogram(String histogramName, long elapsedMs) {
+        if (!LibraryLoader.isInitialized()) return;
+        RecordHistogram.recordTimesHistogram(histogramName, elapsedMs, TimeUnit.MILLISECONDS);
     }
 }
