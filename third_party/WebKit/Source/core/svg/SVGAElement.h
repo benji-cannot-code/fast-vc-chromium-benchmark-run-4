bb1f9e9be2726fd1ce740f1668b6302bec9e70bf
pilgrim@chromium.org <pilgrim@chromium.org>
2015-07-31 16:56:04 +0000

    Code cleanup in core/svg/ (1 of 2)
    Indentation, missing braces, and alphabetical order nits (suggested by check-webkit-style)
    
    BUG=
    TBR=eae@chromium.org
    
    Review URL: https://codereview.chromium.org/1270743002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199848 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGAElement.h
+++ third_party/WebKit/Source/core/svg/SVGAElement.h
@@ -30,8 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class CORE_EXPORT SVGAElement final : public SVGGraphicsElement,
-                          public SVGURIReference {
+class CORE_EXPORT SVGAElement final : public SVGGraphicsElement, public SVGURIReference {
     DEFINE_WRAPPERTYPEINFO();
     WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(SVGAElement);
 public:
