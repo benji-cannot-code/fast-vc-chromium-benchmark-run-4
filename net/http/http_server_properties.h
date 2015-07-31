1e9ffbb1ce0c93cd6fce5fa6b3d764d11de26b53
bnc <bnc@chromium.org>
2015-07-31 11:14:52 -0700

    Process Alt-Svc headers.
    Process Alt-Svc headers in HttpNetworkTransaction::DoReadHeadersComplete().
    
    BUG=392576
    
    Review URL: https://codereview.chromium.org/1254423004
    
    Cr-Commit-Position: refs/heads/master@{#341368}

--- net/http/http_server_properties.h
+++ net/http/http_server_properties.h
@@ -217,6 +217,7 @@ typedef base::MRUCache<HostPortPair, SettingsMap> SpdySettingsMap;
 typedef base::MRUCache<HostPortPair, ServerNetworkStats> ServerNetworkStatsMap;
 
 extern const char kAlternateProtocolHeader[];
+extern const char kAlternativeServiceHeader[];
 
 // The interface for setting/retrieving the HTTP server properties.
 // Currently, this class manages servers':
