3b2683b9a53c8f66c1b617aa27aa862b425f8d0b
jbroman@chromium.org <jbroman@chromium.org>
2015-07-31 13:10:20 +0000

    Remove X11/X.h hack in Font.h.
    The enumerator in question is now called "ComplexPath" in Blink, and the word
    "Complex" occurs nowhere else in Blink C++.
    
    Review URL: https://codereview.chromium.org/1270653002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199827 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/Font.h
+++ third_party/WebKit/Source/platform/fonts/Font.h
@@ -40,12 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/MathExtras.h"
 #include "wtf/text/CharacterNames.h"
 
-// "X11/X.h" defines Complex to 0 and conflicts
-// with Complex value in CodePath enum.
-#ifdef Complex
-#undef Complex
-#endif
-
 class SkCanvas;
 class SkPaint;
 class SkTextBlob;
