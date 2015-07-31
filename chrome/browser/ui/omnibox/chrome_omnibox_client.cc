11d0d90e41873dc9bbc921e0bd5cfe88c4e1b59b
blundell <blundell@chromium.org>
2015-07-30 23:31:10 -0700

    Remove //chrome-level dependencies from OmniboxPopupModel
    To enable this file to be componentized and shared with iOS, inject
    //chrome-level dependencies from OmniboxClient. The changes are
    straightforward.
    
    BUG=511966
    
    Review URL: https://codereview.chromium.org/1269433002
    
    Cr-Commit-Position: refs/heads/master@{#341288}

--- chrome/browser/ui/omnibox/chrome_omnibox_client.cc
+++ chrome/browser/ui/omnibox/chrome_omnibox_client.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/autocomplete/chrome_autocomplete_provider_client.h"
 #include "chrome/browser/bitmap_fetcher/bitmap_fetcher_service_factory.h"
+#include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/command_updater.h"
 #include "chrome/browser/extensions/api/omnibox/omnibox_api.h"
@@ -161,6 +162,27 @@ content::NavigationController&
 const SessionID& ChromeOmniboxClient::GetSessionID() const {
   return SessionTabHelper::FromWebContents(
       controller_->GetWebContents())->session_id();
+}
+
+bookmarks::BookmarkModel* ChromeOmniboxClient::GetBookmarkModel() {
+  return BookmarkModelFactory::GetForProfile(profile_);
+}
+
+TemplateURLService* ChromeOmniboxClient::GetTemplateURLService() {
+  return TemplateURLServiceFactory::GetForProfile(profile_);
+}
+
+gfx::Image ChromeOmniboxClient::GetIconIfExtensionMatch(
+    const AutocompleteMatch& match) const {
+  TemplateURLService* service =
+      TemplateURLServiceFactory::GetForProfile(profile_);
+  const TemplateURL* template_url = match.GetTemplateURL(service, false);
+  if (template_url &&
+      (template_url->GetType() == TemplateURL::OMNIBOX_API_EXTENSION)) {
+    return extensions::OmniboxAPI::Get(profile_)
+        ->GetOmniboxPopupIcon(template_url->GetExtensionId());
+  }
+  return gfx::Image();
 }
 
 bool ChromeOmniboxClient::ProcessExtensionKeyword(
--- chrome/browser/ui/omnibox/omnibox_popup_model.cc
+++ chrome/browser/ui/omnibox/omnibox_popup_model.cc
@@ -9,10 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "chrome/browser/bookmarks/bookmark_model_factory.h"
-#include "chrome/browser/extensions/api/omnibox/omnibox_api.h"
-#include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "chrome/browser/ui/omnibox/omnibox_client.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/omnibox/browser/autocomplete_match.h"
 #include "components/omnibox/browser/omnibox_popup_model_observer.h"
@@ -170,8 +167,7 @@ void OmniboxPopupModel::SetSelectedLine(size_t line,
   // eliminated and just become a call to the observer on the edit.
   base::string16 keyword;
   bool is_keyword_hint;
-  TemplateURLService* service =
-      TemplateURLServiceFactory::GetForProfile(edit_model_->profile());
+  TemplateURLService* service = edit_model_->client()->GetTemplateURLService();
   match.GetKeywordUIState(service, &keyword, &is_keyword_hint);
 
   if (reset_to_default) {
@@ -254,21 +250,11 @@ void OmniboxPopupModel::TryDeletingCurrentItem() {
 
 gfx::Image OmniboxPopupModel::GetIconIfExtensionMatch(
     const AutocompleteMatch& match) const {
-  Profile* profile = edit_model_->profile();
-  TemplateURLService* service =
-      TemplateURLServiceFactory::GetForProfile(profile);
-  const TemplateURL* template_url = match.GetTemplateURL(service, false);
-  if (template_url &&
-      (template_url->GetType() == TemplateURL::OMNIBOX_API_EXTENSION)) {
-    return extensions::OmniboxAPI::Get(profile)->GetOmniboxPopupIcon(
-        template_url->GetExtensionId());
-  }
-  return gfx::Image();
+  return edit_model_->client()->GetIconIfExtensionMatch(match);
 }
 
 bool OmniboxPopupModel::IsStarredMatch(const AutocompleteMatch& match) const {
-  Profile* profile = edit_model_->profile();
-  BookmarkModel* bookmark_model = BookmarkModelFactory::GetForProfile(profile);
+  BookmarkModel* bookmark_model = edit_model_->client()->GetBookmarkModel();
   return bookmark_model && bookmark_model->IsBookmarked(match.destination_url);
 }
 
