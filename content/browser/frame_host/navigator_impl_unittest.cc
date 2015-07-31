c1bcdd77d1a9007a9b3b75fbc369dd9db83233a1
jbauman <jbauman@chromium.org>
2015-07-31 15:09:16 -0700

    Fix NavigatorTestWithBrowserSideNavigation.BeginNavigation with site-per-process
    The RenderViewHostImpl holds on to a reference to the SurfaceManager, so the RenderViewHostImplTestHarness must be torn down before the ImageTransportFactory is destroyed.
    
    BUG=516042
    TBR=piman@chromium.org
    
    Review URL: https://codereview.chromium.org/1268813005
    
    Cr-Commit-Position: refs/heads/master@{#341423}

--- content/browser/frame_host/navigator_impl_unittest.cc
+++ content/browser/frame_host/navigator_impl_unittest.cc
@@ -56,10 +56,10 @@ class NavigatorTestWithBrowserSideNavigation
   }
 
   void TearDown() override {
+    RenderViewHostImplTestHarness::TearDown();
 #if !defined(OS_ANDROID)
     ImageTransportFactory::Terminate();
 #endif
-    RenderViewHostImplTestHarness::TearDown();
   }
 
   TestNavigationURLLoader* GetLoaderForNavigationRequest(
