7b62cf5958cd6eeb978dc8aeda8da959aea71189
kerrnel <kerrnel@chromium.org>
2015-07-31 10:34:21 -0700

    Pass NSUserDefaults over IPC to the renderer
    Blink can now accept the NSUserDefaults values via IPC. This change causes the main process to pass the default values over IPC using the new API.
    
    BUG=392141
    
    Committed: https://crrev.com/54113566ce626a9fcfe5fd1dd46f0362d01ed8ed
    Cr-Commit-Position: refs/heads/master@{#340695}
    
    Review URL: https://codereview.chromium.org/1239363002
    
    Cr-Commit-Position: refs/heads/master@{#341359}

--- content/shell/app/blink_test_platform_support_mac.mm
+++ content/shell/app/blink_test_platform_support_mac.mm
@@ -39,6 +39,8 @@ void SetDefaultsToLayoutTestValues(void) {
                forKey:@"AppleLanguages"];
   [defaults setBool:NO
              forKey:@"AppleScrollAnimationEnabled"];
+  [defaults setBool:NO
+             forKey:@"NSScrollAnimationEnabled"];
   [defaults setObject:@"Always"
                forKey:@"AppleShowScrollBars"];
 }
