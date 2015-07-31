9e1886ffa2628a0a90eb346f533ca6c84d6ee46d
stevenjb <stevenjb@chromium.org>
2015-07-30 17:33:52 -0700

    Replace 'Scanning' text in Network Status Area with throbber
    This CL:
    * Removes the 'Scanning' text from the network list
    * When scanning starts: Starts the throbber (which fades
      in), and fades the info button out. Clicking on the
      Throbber is the same as clicking on the info button.
    * When scanning stops: Stops the throbber (which fades out)
      and fades the info button in.
    
    BUG=490532
    
    Review URL: https://codereview.chromium.org/1270623002
    
    Cr-Commit-Position: refs/heads/master@{#341242}

--- ash/system/chromeos/network/network_state_list_detailed_view.h
+++ ash/system/chromeos/network/network_state_list_detailed_view.h
@@ -33,6 +33,7 @@ class BubbleDelegateView;
 namespace ash {
 class HoverHighlightView;
 class SystemTrayItem;
+class ThrobberView;
 class TrayPopupLabelButton;
 
 namespace tray {
@@ -56,6 +57,9 @@ class NetworkStateListDetailedView
   DetailedViewType GetViewType() const override;
   void Update() override;
 
+  // Called by the WiFi Scanning Throbber when pressed.
+  bool ThrobberPressed(views::View* sender, const ui::Event& event);
+
  protected:
   // Overridden from ButtonListener.
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
@@ -68,7 +72,6 @@ class NetworkStateListDetailedView
 
   // Create UI components.
   void CreateHeaderEntry();
-  void CreateHeaderButtons();
   void CreateNetworkExtra();
 
   // Update UI components.
@@ -119,6 +122,9 @@ class NetworkStateListDetailedView
   // Track login state.
   user::LoginStatus login_;
 
+  // Track WiFi scanning state.
+  bool wifi_scanning_;
+
   // Child views.
   TrayPopupHeaderButton* info_icon_;
   TrayPopupHeaderButton* button_wifi_;
@@ -131,6 +137,9 @@ class NetworkStateListDetailedView
 
   // A small bubble for displaying network info.
   views::BubbleDelegateView* info_bubble_;
+
+  // WiFi scanning throbber.
+  ThrobberView* scanning_throbber_;
 
   gfx::Image controlled_by_extension_icon_;
 
--- ui/chromeos/network/network_list.cc
+++ ui/chromeos/network/network_list.cc
@@ -32,7 +32,6 @@ namespace ui {
 
 NetworkListView::NetworkListView(NetworkListDelegate* delegate)
     : delegate_(delegate),
-      scanning_view_(NULL),
       no_wifi_networks_view_(NULL),
       no_cellular_networks_view_(NULL) {
   CHECK(delegate_);
@@ -193,14 +192,6 @@ bool NetworkListView::UpdateNetworkListEntries(
         UpdateInfoLabel(message_id, index, &no_wifi_networks_view_);
     if (message_id)
       ++index;
-
-    // "Wifi Scanning"
-    message_id = 0;
-    if (handler->GetScanningByType(NetworkTypePattern::WiFi()))
-      message_id = IDS_ASH_STATUS_TRAY_WIFI_SCANNING_MESSAGE;
-    needs_relayout |= UpdateInfoLabel(message_id, index, &scanning_view_);
-    if (message_id)
-      ++index;
   }
 
   // Un-highlighted networks
@@ -210,7 +201,7 @@ bool NetworkListView::UpdateNetworkListEntries(
   // No networks or other messages (fallback)
   if (index == 0) {
     needs_relayout |= UpdateInfoLabel(IDS_ASH_STATUS_TRAY_NO_NETWORKS, index,
-                                      &scanning_view_);
+                                      &no_wifi_networks_view_);
   }
 
   return needs_relayout;
