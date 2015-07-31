4448e559f0fe263f5fab9f32708ea6f63ead209d
pilgrim@chromium.org <pilgrim@chromium.org>
2015-07-31 17:28:26 +0000

    Code cleanup in core/css/ (2 of 3)
    Alphabetical sorting problems, missing braces, weird indentation, and other things spotted by check-webkit-style
    
    BUG=
    TBR=eae@chromium.org
    
    Review URL: https://codereview.chromium.org/1253073003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199853 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSBorderImage.cpp
+++ third_party/WebKit/Source/core/css/CSSBorderImage.cpp
@@ -42,8 +42,9 @@ PassRefPtrWillBeRawPtr<CSSValueList> createBorderImageValue(PassRefPtrWillBeRawP
             listSlash->append(outset);
 
         list->append(listSlash);
-    } else if (imageSlice)
+    } else if (imageSlice) {
         list->append(imageSlice);
+    }
     if (repeat)
         list->append(repeat);
     return list.release();
--- third_party/WebKit/Source/core/css/CSSCanvasValue.cpp
+++ third_party/WebKit/Source/core/css/CSSCanvasValue.cpp
@@ -80,7 +80,7 @@ IntSize CSSCanvasValue::fixedSize(const LayoutObject* layoutObject)
 
 HTMLCanvasElement* CSSCanvasValue::element(Document* document)
 {
-     if (!m_element) {
+    if (!m_element) {
         m_element = &document->getCSSCanvasElement(m_name);
         m_element->addObserver(m_canvasObserver.get());
     }
