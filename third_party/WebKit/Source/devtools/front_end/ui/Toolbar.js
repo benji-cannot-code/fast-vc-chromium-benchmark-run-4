e331e238a79c635bb61c5afe36b89d2190403dca
samli@chromium.org <samli@chromium.org>
2015-07-31 01:43:28 +0000

    Devtools UI: Change native tooltips to use tooltips UI
    This change includes changes to tooltips within the device mode page. An alignment override option is provided by the Tooltip class to ensure that the tooltips aren't behind the rendered page.
    
    BUG=513066
    
    Review URL: https://codereview.chromium.org/1258743003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199774 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/Toolbar.js
+++ third_party/WebKit/Source/devtools/front_end/ui/Toolbar.js
@@ -241,7 +241,7 @@ WebInspector.ToolbarCounter.prototype = {
             }
         }
         this.element.classList.toggle("hidden", !total);
-        this.element.title = title;
+        WebInspector.Tooltip.install(this.element, title);
     },
 
     /**
@@ -439,10 +439,7 @@ WebInspector.ToolbarButtonBase.prototype = {
         if (this._title === x)
             return;
         this._title = x;
-        if (Runtime.experiments.isEnabled("tooltips"))
-            WebInspector.Tooltip.install(this.element, x);
-        else
-            this.element.title = x;
+        WebInspector.Tooltip.install(this.element, x);
     },
 
     /**
@@ -873,7 +870,7 @@ WebInspector.ToolbarCheckbox = function(text, title, setting)
     this.element.classList.add("checkbox");
     this.inputElement = this.element.checkboxElement;
     if (title)
-        this.element.title = title;
+        WebInspector.Tooltip.install(this.element, title);
     if (setting)
         WebInspector.SettingsUI.bindCheckbox(this.inputElement, setting);
 }
