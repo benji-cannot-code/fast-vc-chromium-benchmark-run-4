702a27525d7d94dce92f0b74a53521775bd2149d
sergeyv@chromium.org <sergeyv@chromium.org>
2015-08-02 23:25:49 +0000

    PageOverlays: Support only accelerated compositing in PageOverlay
    BUG=
    
    Review URL: https://codereview.chromium.org/1268723003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199870 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/PageOverlay.h
+++ third_party/WebKit/Source/web/PageOverlay.h
@@ -64,7 +64,6 @@ public:
     ~PageOverlay();
 
     void update();
-    void paintWebFrame(GraphicsContext&);
 
     GraphicsLayer* graphicsLayer() const { return m_layer.get(); }
     DisplayItemClient displayItemClient() const { return toDisplayItemClient(this); }
@@ -76,10 +75,9 @@ public:
 
 private:
     PageOverlay(WebViewImpl*, PassOwnPtr<PageOverlay::Delegate>);
-    void invalidateWebFrame();
 
     WebViewImpl* m_viewImpl;
-    OwnPtr<PageOverlay::Delegate> m_overlay;
+    OwnPtr<PageOverlay::Delegate> m_delegate;
     OwnPtr<GraphicsLayer> m_layer;
 };
 
