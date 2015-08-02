a01e1b0f007a739892be82a99ee2f82d66d8c51a
asanka <asanka@chromium.org>
2015-08-01 21:37:55 -0700

    [SafeBrowsing] Send pings for Zip files that contain other archives.
    BUG=515216
    
    Review URL: https://codereview.chromium.org/1262753002
    
    Cr-Commit-Position: refs/heads/master@{#341475}

--- chrome/common/safe_browsing/zip_analyzer_results.h
+++ chrome/common/safe_browsing/zip_analyzer_results.h
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_COMMON_SAFE_BROWSING_ZIP_ANALYZER_RESULTS_H_
 #define CHROME_COMMON_SAFE_BROWSING_ZIP_ANALYZER_RESULTS_H_
 
+#include <vector>
+
+#include "base/files/file_path.h"
 #include "chrome/common/safe_browsing/csd.pb.h"
 
 namespace safe_browsing {
@@ -20,6 +23,7 @@ struct Results {
   bool has_archive;
   google::protobuf::RepeatedPtrField<ClientDownloadRequest_ArchivedBinary>
       archived_binary;
+  std::vector<base::FilePath::StringType> archived_archive_filetypes;
   Results();
   ~Results();
 };
