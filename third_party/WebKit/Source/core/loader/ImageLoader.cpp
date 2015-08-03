f3f235ada943048e077041bf4b3e8c1c33a5cd6b
haraken@chromium.org <haraken@chromium.org>
2015-08-03 04:50:01 +0000

    Oilpan: Make ImageLoader::Task::m_loader a WeakPersistent
    This is one of the steps to remove a raw pointer to an on-heap object.
    
    BUG=509911
    
    Review URL: https://codereview.chromium.org/1269023002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199874 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/ImageLoader.cpp
+++ third_party/WebKit/Source/core/loader/ImageLoader.cpp
@@ -118,7 +118,7 @@ public:
 
     void clearLoader()
     {
-        m_loader = 0;
+        m_loader = nullptr;
         m_scriptState.clear();
     }
 
@@ -128,7 +128,7 @@ public:
     }
 
 private:
-    ImageLoader* m_loader;
+    RawPtrWillBeWeakPersistent<ImageLoader> m_loader;
     BypassMainWorldBehavior m_shouldBypassMainWorldCSP;
     UpdateFromElementBehavior m_updateBehavior;
     RefPtr<ScriptState> m_scriptState;
@@ -165,8 +165,10 @@ void ImageLoader::dispose()
     WTF_LOG(Timers, "~ImageLoader %p; m_hasPendingLoadEvent=%d, m_hasPendingErrorEvent=%d",
         this, m_hasPendingLoadEvent, m_hasPendingErrorEvent);
 
+#if !ENABLE(OILPAN)
     if (m_pendingTask)
         m_pendingTask->clearLoader();
+#endif
 
 #if ENABLE(OILPAN)
     for (const auto& client : m_clients)
