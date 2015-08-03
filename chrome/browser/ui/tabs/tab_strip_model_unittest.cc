e5d7f39b81c0739c828be83b30dfa6c242b3fdcc
pmonette <pmonette@chromium.org>
2015-08-03 14:11:13 -0700

    Reloading a discarded tab now correctly unmarks the tab as discarded.
    BUG=514313
    
    Review URL: https://codereview.chromium.org/1250353003
    
    Cr-Commit-Position: refs/heads/master@{#341610}

--- chrome/browser/ui/tabs/tab_strip_model_unittest.cc
+++ chrome/browser/ui/tabs/tab_strip_model_unittest.cc
@@ -33,11 +33,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "content/public/test/web_contents_tester.h"
 #include "extensions/common/extension.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using content::SiteInstance;
 using content::WebContents;
+using content::WebContentsTester;
 using extensions::Extension;
 
 namespace {
@@ -2178,6 +2180,32 @@ TEST_F(TabStripModelTest, DiscardWebContentsAt) {
   tabstrip.CloseAllTabs();
 }
 
+// Makes sure that reloading a discarded tab without activating it unmarks the
+// tab as discarded so it won't reload on activation.
+TEST_F(TabStripModelTest, ReloadDiscardedTabContextMenu) {
+  TabStripDummyDelegate delegate;
+  TabStripModel tabstrip(&delegate, profile());
+
+  // Create 2 tabs because the active tab cannot be discarded.
+  tabstrip.AppendWebContents(CreateWebContents(), true);
+  content::WebContents* test_contents =
+      WebContentsTester::CreateTestWebContents(browser_context(), nullptr);
+  tabstrip.AppendWebContents(test_contents, false);  // Opened in background.
+
+  // Navigate to a web page. This is necessary to set a current entry in memory
+  // so the reload can happen.
+  WebContentsTester::For(test_contents)
+      ->NavigateAndCommit(GURL("chrome://newtab"));
+  EXPECT_FALSE(tabstrip.IsTabDiscarded(1));
+
+  tabstrip.DiscardWebContentsAt(1);
+  EXPECT_TRUE(tabstrip.IsTabDiscarded(1));
+
+  tabstrip.GetWebContentsAt(1)->GetController().Reload(false);
+  EXPECT_FALSE(tabstrip.IsTabDiscarded(1));
+
+  tabstrip.CloseAllTabs();
+}
 // Makes sure TabStripModel handles the case of deleting a tab while removing
 // another tab.
 TEST_F(TabStripModelTest, DeleteFromDestroy) {
