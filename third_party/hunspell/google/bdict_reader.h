6a064f5423fad6907f71b7f0e7ef1b680d08ac7c
tfarina <tfarina@chromium.org>
2015-08-03 12:55:35 -0700

    third_party/hunspell: Use DISALLOW_COPY_AND_ASSIGN macro.
    The EVIL is deprecated and will be removed in future.
    
    BUG=None
    R=rouslan@chromium.org
    
    Review URL: https://codereview.chromium.org/1266103004
    
    Cr-Commit-Position: refs/heads/master@{#341593}

--- third_party/hunspell/google/bdict_reader.h
+++ third_party/hunspell/google/bdict_reader.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/basictypes.h"
+#include "base/macros.h"
 #include "third_party/hunspell/google/bdict.h"
 
 namespace hunspell {
@@ -146,7 +146,7 @@ class BDictReader {
 
   const BDict::AffHeader* aff_header_;
 
-  DISALLOW_EVIL_CONSTRUCTORS(BDictReader);
+  DISALLOW_COPY_AND_ASSIGN(BDictReader);
 };
 
 }  // namespace hunspell
--- third_party/hunspell/google/bdict_writer.h
+++ third_party/hunspell/google/bdict_writer.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/basictypes.h"
+#include "base/macros.h"
 
 namespace hunspell {
 
@@ -63,7 +63,7 @@ class BDictWriter {
   // Root of the generated trie. Filled by SetWords.
   DicNode* trie_root_;
 
-  DISALLOW_EVIL_CONSTRUCTORS(BDictWriter);
+  DISALLOW_COPY_AND_ASSIGN(BDictWriter);
 };
 
 }  // namespace hunspell
