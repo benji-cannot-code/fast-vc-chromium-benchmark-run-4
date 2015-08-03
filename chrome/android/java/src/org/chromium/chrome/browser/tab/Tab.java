74f20acb509462e00cfe25c7be2fda993e0d5417
yusufo <yusufo@chromium.org>
2015-08-03 11:29:11 -0700

    Move theme color logic to Tab
    Starts caching the theme color to be used inside tab.
    This value depends on whether the tab is incognito and also
    the security state and whether the tab is showing an interstitial
    page. Changes TabObserver#ondidChangeThemeColor so that in only
    pushes actually changes from the cached value.
    
    ChromeActivity starts listening for this signal and pushes any
    color changes to ToolbarManager making all ChromeActivity
    classes aware of theme color changes.
    
    This should still not cause any visual changes.
    
    BUG=507340
    
    Review URL: https://codereview.chromium.org/1254783003
    
    Cr-Commit-Position: refs/heads/master@{#341559}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.tab;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.text.TextUtils;
@@ -318,6 +319,9 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
      * Reference to the current sadTabView if one is defined.
      */
     private View mSadTabView;
+
+    private final int mDefaultThemeColor;
+    private int mThemeColor;
 
     /**
      * A default {@link ChromeContextMenuItemDelegate} that supports some of the context menu
@@ -686,8 +690,19 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
 
         @Override
         public void didChangeThemeColor(int color) {
+            int securityLevel = getSecurityLevel();
+            if (securityLevel == ConnectionSecurityLevel.SECURITY_ERROR
+                    || securityLevel == ConnectionSecurityLevel.SECURITY_WARNING
+                    || securityLevel == ConnectionSecurityLevel.SECURITY_POLICY_WARNING) {
+                color = mDefaultThemeColor;
+            }
+            if (isShowingInterstitialPage()) color = mDefaultThemeColor;
+            if (color == Color.TRANSPARENT) color = mDefaultThemeColor;
+            color |= 0xFF000000;
+            if (mThemeColor == color) return;
+            mThemeColor = color;
             for (TabObserver observer : mObservers) {
-                observer.onDidChangeThemeColor(color);
+                observer.onDidChangeThemeColor(Tab.this, mThemeColor);
             }
         }
 
@@ -695,6 +710,7 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
         public void didAttachInterstitialPage() {
             getInfoBarContainer().setVisibility(View.INVISIBLE);
             showRenderedPage();
+            didChangeThemeColor(mDefaultThemeColor);
 
             for (TabObserver observer : mObservers) {
                 observer.onDidAttachInterstitialPage(Tab.this);
@@ -707,6 +723,7 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
         @Override
         public void didDetachInterstitialPage() {
             getInfoBarContainer().setVisibility(View.VISIBLE);
+            didChangeThemeColor(getWebContents().getThemeColor(mDefaultThemeColor));
 
             for (TabObserver observer : mObservers) {
                 observer.onDidDetachInterstitialPage(Tab.this);
@@ -783,8 +800,14 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
         mLaunchType = type;
         if (mContext != null) {
             mNumPixel16DP = (int) (DeviceDisplayInfo.create(mContext).getDIPScale() * 16);
+            Resources resources = mContext.getResources();
+            mDefaultThemeColor = mIncognito ? resources.getColor(R.color.incognito_primary_color)
+                    : resources.getColor(R.color.default_primary_color);
+            mThemeColor = mDefaultThemeColor;
+        } else {
+            mDefaultThemeColor = 0;
+            mNumPixel16DP = 16;
         }
-        if (mNumPixel16DP == 0) mNumPixel16DP = 16;
 
         // Restore data from the TabState, if it existed.
         if (frozenState == null) {
@@ -1102,6 +1125,14 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
         if (mNativePage != null) return mNativePage.getBackgroundColor();
         if (getWebContents() != null) return getWebContents().getBackgroundColor();
         return Color.WHITE;
+    }
+
+    /**
+     * @return The current theme color based on the value passed from the web contents and the
+     *         security state.
+     */
+    public int getThemeColor() {
+        return mThemeColor;
     }
 
     /**
@@ -2262,6 +2293,8 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
         mContentViewCore.onShow();
         mContentViewCore.attachImeAdapter();
         destroyNativePageInternal(previousNativePage);
+        mWebContentsObserver.didChangeThemeColor(
+                getWebContents().getThemeColor(mDefaultThemeColor));
         for (TabObserver observer : mObservers) {
             observer.onWebContentsSwapped(this, didStartLoad, didFinishLoad);
         }
