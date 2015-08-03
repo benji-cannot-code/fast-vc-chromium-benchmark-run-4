93e036d32856eedeb30643ecbfe64ed77a3e9ca7
jackhou <jackhou@chromium.org>
2015-08-03 16:52:03 -0700

    [Views] Only test rect-based targeting if it is enabled.
    Specifically, ViewTargeterTest.TargetContentsAndRootView fails on Mac
    because rect-based targeting is not enabled for Mac in
    ui/views/views_switches.cc.
    
    Make the test check that rect-based targeting is enabled.
    
    BUG=378134
    
    Review URL: https://codereview.chromium.org/1261263002
    
    Cr-Commit-Position: refs/heads/master@{#341648}

--- ui/views/view_targeter_unittest.cc
+++ ui/views/view_targeter_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/test/views_test_base.h"
 #include "ui/views/view_targeter.h"
 #include "ui/views/view_targeter_delegate.h"
+#include "ui/views/views_switches.h"
 #include "ui/views/widget/root_view.h"
 
 namespace views {
@@ -400,7 +401,13 @@ TEST_F(ViewTargeterTest, TargetContentsAndRootView) {
   center_point = bounding_box.CenterPoint();
   tap = GestureEventForTest(details);
 
-  EXPECT_EQ(content, targeter->FindTargetForEvent(root_view, &tap));
+  // This only applies if rect-based targeting is enabled.
+  if (views::switches::IsRectBasedTargetingEnabled()) {
+    EXPECT_EQ(content, targeter->FindTargetForEvent(root_view, &tap));
+  } else {
+    EXPECT_EQ(widget.GetRootView(),
+              targeter->FindTargetForEvent(root_view, &tap));
+  }
 
   // A gesture event not overlapping the contents view by at least
   // 60% and not having its center within the contents view should
