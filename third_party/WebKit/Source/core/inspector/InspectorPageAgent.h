efb1fa0db479d71493a747fd7250f1bcdcedb86d
sergeyv@chromium.org <sergeyv@chromium.org>
2015-08-03 19:02:53 +0000

    Devtools: rebuild node's highlight on the style recalc
    BUG=516323
    
    Review URL: https://codereview.chromium.org/1263713008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199906 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorPageAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorPageAgent.h
@@ -120,6 +120,7 @@ public:
     void didLayout();
     void didScroll();
     void didResizeMainFrame();
+    void didRecalculateStyle(int);
 
     // Inspector Controller API
     void disable(ErrorString*) override;
