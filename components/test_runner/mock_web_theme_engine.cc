e84d1cd9b11ea4682cad9d0f90c1b3eb4d88de08
bungeman <bungeman@chromium.org>
2015-08-03 12:24:29 -0700

    Directly include SkPath.h where needed in components/test_runner.
    A propsed Skia change will forward declare SkPath in more locations.
    The changes in this CL are needed so that the test_runner  code will
    continue to compile after the Skia change. This also adds direct
    includes for SkPaint where needed.
    
    Review URL: https://codereview.chromium.org/1267153002
    
    Cr-Commit-Position: refs/heads/master@{#341580}

--- components/test_runner/mock_web_theme_engine.cc
+++ components/test_runner/mock_web_theme_engine.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/ext/platform_canvas.h"
 #include "third_party/WebKit/public/platform/WebRect.h"
 #include "third_party/WebKit/public/platform/WebSize.h"
+#include "third_party/skia/include/core/SkPaint.h"
+#include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkRect.h"
 
 using blink::WebCanvas;
--- components/test_runner/test_plugin.cc
+++ components/test_runner/test_plugin.cc
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "third_party/skia/include/core/SkPaint.h"
+#include "third_party/skia/include/core/SkPath.h"
 
 namespace test_runner {
 
