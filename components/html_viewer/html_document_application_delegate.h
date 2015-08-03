71cfbf43f0c723dab74922459c20caf434985ea2
jam <jam@chromium.org>
2015-08-03 12:35:11 -0700

    Fix shutdown UAF in HTMLDocumentApplicationDelegate.
    Review URL: https://codereview.chromium.org/1258513008
    
    Cr-Commit-Position: refs/heads/master@{#341586}

--- components/html_viewer/html_document_application_delegate.h
+++ components/html_viewer/html_document_application_delegate.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "components/html_viewer/html_document.h"
 #include "mojo/application/public/cpp/application_delegate.h"
 #include "mojo/application/public/cpp/application_impl.h"
@@ -79,6 +80,8 @@ class HTMLDocumentApplicationDelegate : public mojo::ApplicationDelegate {
   std::set<HTMLDocumentOOPIF*> documents2_;
 
   HTMLDocumentCreationCallback html_document_creation_callback_;
+
+  base::WeakPtrFactory<HTMLDocumentApplicationDelegate> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(HTMLDocumentApplicationDelegate);
 };
