39e081b9a9beb665ab659c23051cb0ad050031e1
yoshiki <yoshiki@chromium.org>
2015-08-03 04:15:00 -0700

    [Download Notification] Add metrics for user actions
    BUG=513545
    TEST=none
    
    Review URL: https://codereview.chromium.org/1268693005
    
    Cr-Commit-Position: refs/heads/master@{#341511}

--- chrome/browser/download/notification/download_item_notification.cc
+++ chrome/browser/download/notification/download_item_notification.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/download_interrupt_reasons.h"
 #include "content/public/browser/download_item.h"
 #include "content/public/browser/page_navigator.h"
+#include "content/public/browser/user_metrics.h"
 #include "content/public/browser/web_contents.h"
 #include "grit/theme_resources.h"
 #include "net/base/mime_util.h"
@@ -31,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/codec/jpeg_codec.h"
 #include "ui/gfx/image/image.h"
 #include "ui/message_center/message_center.h"
+
+using base::UserMetricsAction;
 
 namespace {
 
@@ -52,6 +55,55 @@ std::string ReadNotificationImage(const base::FilePath& file_path) {
   DCHECK_LE(data.size(), static_cast<size_t>(kMaxImagePreviewSize));
 
   return data;
+}
+
+void RecordButtonClickAction(DownloadCommands::Command command) {
+  switch (command) {
+    case DownloadCommands::SHOW_IN_FOLDER:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Button_ShowInFolder"));
+      break;
+    case DownloadCommands::OPEN_WHEN_COMPLETE:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Button_OpenWhenComplete"));
+      break;
+    case DownloadCommands::ALWAYS_OPEN_TYPE:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Button_AlwaysOpenType"));
+      break;
+    case DownloadCommands::PLATFORM_OPEN:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Button_PlatformOpen"));
+      break;
+    case DownloadCommands::CANCEL:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Button_Cancel"));
+      break;
+    case DownloadCommands::DISCARD:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Button_Discard"));
+      break;
+    case DownloadCommands::KEEP:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Button_Keep"));
+      break;
+    case DownloadCommands::LEARN_MORE_SCANNING:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Button_LearnScanning"));
+      break;
+    case DownloadCommands::LEARN_MORE_INTERRUPTED:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Button_LearnInterrupted"));
+      break;
+    case DownloadCommands::PAUSE:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Button_Pause"));
+      break;
+    case DownloadCommands::RESUME:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Button_Resume"));
+      break;
+  }
 }
 
 }  // anonymous namespace
@@ -98,16 +150,22 @@ void DownloadItemNotification::OnNotificationClose() {
 
 void DownloadItemNotification::OnNotificationClick() {
   if (item_->IsDangerous()) {
+    content::RecordAction(
+        UserMetricsAction("DownloadNotification.Click_Dangerous"));
     // Do nothing.
     return;
   }
 
   switch (item_->GetState()) {
     case content::DownloadItem::IN_PROGRESS:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Click_InProgress"));
       item_->SetOpenWhenComplete(!item_->GetOpenWhenComplete());  // Toggle
       break;
     case content::DownloadItem::CANCELLED:
     case content::DownloadItem::INTERRUPTED:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Click_Stopped"));
       GetBrowser()->OpenURL(content::OpenURLParams(
           GURL(chrome::kChromeUIDownloadsURL), content::Referrer(),
           NEW_FOREGROUND_TAB, ui::PAGE_TRANSITION_LINK,
@@ -115,6 +173,8 @@ void DownloadItemNotification::OnNotificationClick() {
       CloseNotificationByUser();
       break;
     case content::DownloadItem::COMPLETE:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Click_Completed"));
       item_->OpenDownload();
       CloseNotificationByUser();
       break;
@@ -132,6 +192,8 @@ void DownloadItemNotification::OnNotificationButtonClick(int button_index) {
   }
 
   DownloadCommands::Command command = button_actions_->at(button_index);
+  RecordButtonClickAction(command);
+
   if (command != DownloadCommands::PAUSE &&
       command != DownloadCommands::RESUME) {
     CloseNotificationByUser();
