1ca654d197eab23eb6090854778ed6b71a3384c3
bungeman <bungeman@chromium.org>
2015-08-03 12:59:32 -0700

    Directly include SkPath.h where needed in cc/.
    A propsed Skia change will forward declare SkPath in more locations.
    The changes in this CL are needed so that the cc code will continue
    to compile after the Skia change.
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1256353003
    
    Cr-Commit-Position: refs/heads/master@{#341597}

--- cc/layers/heads_up_display_layer_impl.cc
+++ cc/layers/heads_up_display_layer_impl.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/trees/layer_tree_impl.h"
 #include "skia/ext/platform_canvas.h"
 #include "third_party/skia/include/core/SkPaint.h"
+#include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkTypeface.h"
 #include "third_party/skia/include/effects/SkColorMatrixFilter.h"
 #include "ui/gfx/geometry/point.h"
