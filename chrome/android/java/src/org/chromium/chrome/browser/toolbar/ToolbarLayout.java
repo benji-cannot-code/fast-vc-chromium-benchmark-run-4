97c3c7abf1a2df59623c13392b3c18111f5e0211
kkimlabs <kkimlabs@chromium.org>
2015-08-03 12:45:41 -0700

    [Android] Fix tablet startup crash due to a missing mProgressBar null check.
    mProgressBar could be null and
    
    https://chromium.googlesource.com/chromium/src/+/5d14d9a3c4f02548a672837c591f6cea78918b24
    
    didn't have a null checking
    
    TBR=dtrainor@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/1267163002
    
    Cr-Commit-Position: refs/heads/master@{#341591}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarLayout.java
@@ -80,6 +80,7 @@ abstract class ToolbarLayout extends FrameLayout implements Toolbar {
             removeView(mProgressBar);
             getFrameLayoutParams(mProgressBar).topMargin = mToolbarHeightWithoutShadow
                     - getFrameLayoutParams(mProgressBar).height;
+            if (isNativeLibraryReady()) mProgressBar.initializeAnimation();
         }
 
         mMenuButton = (TintedImageButton) findViewById(R.id.menu_button);
@@ -169,7 +170,7 @@ abstract class ToolbarLayout extends FrameLayout implements Toolbar {
      */
     public void onNativeLibraryReady() {
         mNativeLibraryReady = true;
-        mProgressBar.initializeAnimation();
+        if (mProgressBar != null) mProgressBar.initializeAnimation();
     }
 
     /**
--- chrome/android/java/src/org/chromium/chrome/browser/widget/ToolbarProgressBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/ToolbarProgressBar.java
@@ -48,6 +48,7 @@ public class ToolbarProgressBar extends ClipDrawableProgressBar {
     private float mTargetProgress;
     private float mTargetAlpha = 0.0f;
     AnimationLogic mAnimationLogic;
+    private boolean mAnimationInitialized;
 
     private final Runnable mHideRunnable = new Runnable() {
         @Override
@@ -95,6 +96,9 @@ public class ToolbarProgressBar extends ClipDrawableProgressBar {
      * library is ready.
      */
     public void initializeAnimation() {
+        if (mAnimationInitialized) return;
+        mAnimationInitialized = true;
+
         assert mAnimationLogic == null;
 
         String animation = CommandLine.getInstance().getSwitchValue(
