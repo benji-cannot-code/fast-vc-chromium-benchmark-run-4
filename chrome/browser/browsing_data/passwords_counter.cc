c04b921b81fb0281e276b9fae736d65b324a506a
msramek <msramek@chromium.org>
2015-08-04 08:12:37 -0700

    Prepare the infrastructure for browsing data counters.
    1. Add elements to the UI.
    2. Add an (empty) BrowsingDataCounter class that will be subclassed per datatype to do the counting, and report the results to ContentSettingsHandler.
    3. Handle when and with what text should counters be shown.
    
    BUG=510028
    
    Review URL: https://codereview.chromium.org/1243393002
    
    Cr-Commit-Position: refs/heads/master@{#341723}

---
+++ chrome/browser/browsing_data/passwords_counter.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/browsing_data/passwords_counter.h"
+#include "chrome/common/pref_names.h"
+
+PasswordsCounter::PasswordsCounter()
+    : pref_name_(prefs::kDeletePasswords) {
+}
+
+PasswordsCounter::~PasswordsCounter() {
+}
+
+const std::string& PasswordsCounter::GetPrefName() const {
+  return pref_name_;
+}
+
+void PasswordsCounter::Count() {
+  // TODO(msramek): Count the passwords of |profile|
+  // and call |ReportResult| when finished.
+}
---
+++ chrome/browser/browsing_data/passwords_counter.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_BROWSING_DATA_PASSWORDS_COUNTER_H_
+#define CHROME_BROWSER_BROWSING_DATA_PASSWORDS_COUNTER_H_
+
+#include "chrome/browser/browsing_data/browsing_data_counter.h"
+
+class PasswordsCounter: public BrowsingDataCounter {
+ public:
+  PasswordsCounter();
+  ~PasswordsCounter() override;
+
+  const std::string& GetPrefName() const override;
+
+ private:
+  const std::string pref_name_;
+
+  void Count() override;
+};
+
+#endif  // CHROME_BROWSER_BROWSING_DATA_PASSWORDS_COUNTER_H_
