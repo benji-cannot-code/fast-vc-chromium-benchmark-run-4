b0b5229b4b87a95525f4400cd39f9c9180618115
samli@chromium.org <samli@chromium.org>
2015-08-04 08:06:51 +0000

    Devtools: Add DOM breakpoints menu to the elements toolbar
    BUG=515763
    
    Review URL: https://codereview.chromium.org/1267103002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199958 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/components/DOMBreakpointsSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/components/DOMBreakpointsSidebarPane.js
@@ -72,8 +72,9 @@ WebInspector.DOMBreakpointsSidebarPane.prototype = {
     /**
      * @param {!WebInspector.DOMNode} node
      * @param {!WebInspector.ContextMenu} contextMenu
+     * @param {boolean} createSubMenu
      */
-    populateNodeContextMenu: function(node, contextMenu)
+    populateNodeContextMenu: function(node, contextMenu, createSubMenu)
     {
         if (node.pseudoType())
             return;
@@ -98,11 +99,11 @@ WebInspector.DOMBreakpointsSidebarPane.prototype = {
             this._saveBreakpoints();
         }
 
-        var breakPointSubMenu = contextMenu.appendSubMenuItem(WebInspector.UIString("Break on..."));
+        var breakpointsMenu = createSubMenu ? contextMenu.appendSubMenuItem(WebInspector.UIString("Break on...")) : contextMenu;
         for (var key in this._breakpointTypes) {
             var type = this._breakpointTypes[key];
             var label = this._contextMenuLabels[type];
-            breakPointSubMenu.appendCheckboxItem(label, toggleBreakpoint.bind(this, type), nodeBreakpoints[type]);
+            breakpointsMenu.appendCheckboxItem(label, toggleBreakpoint.bind(this, type), nodeBreakpoints[type]);
         }
     },
 
