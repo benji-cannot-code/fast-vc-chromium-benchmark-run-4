f7032596e5dc84af42ce6402ca9ee9b9ede4a835
samli@chromium.org <samli@chromium.org>
2015-08-04 06:19:57 +0000

    Devtools: Add hide element and edit as HTML buttons to elements toolbar
    This change adds two buttons to the experimental elements toolbar. Both of these buttons now have associated actions and shortcuts.
    
    BUG=515763
    
    Review URL: https://codereview.chromium.org/1269873002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199943 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/elements/ElementsTreeOutline.js
+++ third_party/WebKit/Source/devtools/front_end/elements/ElementsTreeOutline.js
@@ -58,7 +58,6 @@ WebInspector.ElementsTreeOutline = function(domModel, omitRootDOMNode, selectEna
     this._element.addEventListener("dragleave", this._ondragleave.bind(this), false);
     this._element.addEventListener("drop", this._ondrop.bind(this), false);
     this._element.addEventListener("dragend", this._ondragend.bind(this), false);
-    this._element.addEventListener("keydown", this._onkeydown.bind(this), false);
     this._element.addEventListener("webkitAnimationEnd", this._onAnimationEnd.bind(this), false);
     this._element.addEventListener("contextmenu", this._contextMenuEventFired.bind(this), false);
 
@@ -898,25 +897,6 @@ WebInspector.ElementsTreeOutline.prototype = {
         }
     },
 
-    /**
-     * @param {!Event} event
-     */
-    _onkeydown: function(event)
-    {
-        var keyboardEvent = /** @type {!KeyboardEvent} */ (event);
-        var node = /** @type {!WebInspector.DOMNode} */ (this.selectedDOMNode());
-        console.assert(node);
-        var treeElement = node[this._treeElementSymbol];
-        if (!treeElement)
-            return;
-
-        if (!treeElement.isEditing() && WebInspector.KeyboardShortcut.hasNoModifiers(keyboardEvent) && keyboardEvent.keyCode === WebInspector.KeyboardShortcut.Keys.H.code) {
-            this._toggleHideShortcut(node);
-            event.consume(true);
-            return;
-        }
-    },
-
     _contextMenuEventFired: function(event)
     {
         var treeElement = this._treeElementFromEvent(event);
@@ -962,12 +942,6 @@ WebInspector.ElementsTreeOutline.prototype = {
         if (!treeElement)
             return;
 
-        if (event.keyIdentifier === "F2" && treeElement.hasEditableNode()) {
-            this._toggleEditAsHTML(node);
-            event.handled = true;
-            return;
-        }
-
         if (WebInspector.KeyboardShortcut.eventHasCtrlOrMeta(event) && node.parentNode) {
             if (event.keyIdentifier === "Up" && node.previousSibling) {
                 node.moveTo(node.parentNode, node.previousSibling, this.selectNodeAfterEdit.bind(this, treeElement.expanded));
@@ -984,11 +958,13 @@ WebInspector.ElementsTreeOutline.prototype = {
 
     /**
      * @param {!WebInspector.DOMNode} node
+     * @param {boolean=} startEditing
+     * @param {function()=} callback
      */
-    _toggleEditAsHTML: function(node)
+    toggleEditAsHTML: function(node, startEditing, callback)
     {
         var treeElement = node[this._treeElementSymbol];
-        if (!treeElement)
+        if (!treeElement || !treeElement.hasEditableNode())
             return;
 
         if (node.pseudoType())
@@ -998,7 +974,7 @@ WebInspector.ElementsTreeOutline.prototype = {
         var index = node.index;
         var wasExpanded = treeElement.expanded;
 
-        treeElement.toggleEditAsHTML(editingFinished.bind(this));
+        treeElement.toggleEditAsHTML(editingFinished.bind(this), startEditing);
 
         /**
          * @this {WebInspector.ElementsTreeOutline}
@@ -1006,6 +982,8 @@ WebInspector.ElementsTreeOutline.prototype = {
          */
         function editingFinished(success)
         {
+            if (callback)
+                callback();
             if (!success)
                 return;
 
@@ -1063,7 +1041,7 @@ WebInspector.ElementsTreeOutline.prototype = {
      * @param {!WebInspector.DOMNode} node
      * @param {function(?WebInspector.RemoteObject, boolean=)=} userCallback
      */
-    _toggleHideShortcut: function(node, userCallback)
+    toggleHideElement: function(node, userCallback)
     {
         var pseudoType = node.pseudoType();
         var effectiveNode = pseudoType ? node.parentNode : node;
