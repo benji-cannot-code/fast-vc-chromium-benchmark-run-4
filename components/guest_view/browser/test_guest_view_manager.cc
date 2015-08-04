53293d56df3623c69dc61114aeb149e3fbaeb9bd
dzhioev <dzhioev@chromium.org>
2015-08-04 14:29:22 -0700

    SAMLEnrollmentTest-s addapted for the webview flow.
    We need to distinguish the enrollment webview and the webview
    used for regular sign in in this test somehow. I exposed
    <webview>'s name property on C++ side for that. Now
    InlineLogin::GetAuthFrame return webview which name matches
    with |parent_frame_name| argument (this argument was taken into
    account only for iframe case before).
    
    BUG=513955
    
    Review URL: https://codereview.chromium.org/1273443002
    
    Cr-Commit-Position: refs/heads/master@{#341801}

--- components/guest_view/browser/test_guest_view_manager.cc
+++ components/guest_view/browser/test_guest_view_manager.cc
@@ -65,6 +65,12 @@ content::WebContents* TestGuestViewManager::WaitForSingleGuestCreated() {
   return GetLastGuestCreated();
 }
 
+content::WebContents* TestGuestViewManager::WaitForNextGuestCreated() {
+  created_message_loop_runner_ = new content::MessageLoopRunner();
+  created_message_loop_runner_->Run();
+  return GetLastGuestCreated();
+}
+
 void TestGuestViewManager::WaitForNumGuestsCreated(size_t count) {
   if (count == num_guests_created_)
     return;
@@ -72,8 +78,8 @@ void TestGuestViewManager::WaitForNumGuestsCreated(size_t count) {
   waiting_for_guests_created_ = true;
   expected_num_guests_created_ = count;
 
-  created_message_loop_runner_ = new content::MessageLoopRunner;
-  created_message_loop_runner_->Run();
+  num_created_message_loop_runner_ = new content::MessageLoopRunner;
+  num_created_message_loop_runner_->Run();
 }
 
 void TestGuestViewManager::WaitForViewGarbageCollected() {
@@ -94,14 +100,17 @@ void TestGuestViewManager::AddGuest(int guest_instance_id,
       linked_ptr<content::WebContentsDestroyedWatcher>(
           new content::WebContentsDestroyedWatcher(guest_web_contents)));
 
+  if (created_message_loop_runner_.get())
+    created_message_loop_runner_->Quit();
+
   ++num_guests_created_;
   if (!waiting_for_guests_created_ &&
       num_guests_created_ != expected_num_guests_created_) {
     return;
   }
 
-  if (created_message_loop_runner_.get())
-    created_message_loop_runner_->Quit();
+  if (num_created_message_loop_runner_.get())
+    num_created_message_loop_runner_->Quit();
 }
 
 void TestGuestViewManager::GetGuestWebContentsList(
