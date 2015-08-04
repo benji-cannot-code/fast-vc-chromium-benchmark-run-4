4c82e8edf64284fd3237e0b620d84190813b93c0
blundell <blundell@chromium.org>
2015-08-04 04:56:26 -0700

    Use destination URL to compute search engine type in OmniboxEditModel
    This CL switches OmniboxEditModel from using the two-arg variant of
    GetEngineType() that takes in the template URL to the one-arg variant that
    takes in only the destination URL. The goal is to avoid needing to abstract
    usage of UIThreadSearchTermsData for componentization by instead removing that
    usage.
    
    Review URL: https://codereview.chromium.org/1269053002
    
    Cr-Commit-Position: refs/heads/master@{#341711}

--- chrome/browser/ui/omnibox/omnibox_edit_model.cc
+++ chrome/browser/ui/omnibox/omnibox_edit_model.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_stats.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
-#include "chrome/browser/search_engines/ui_thread_search_terms_data.h"
 #include "chrome/browser/ui/omnibox/omnibox_popup_model.h"
 #include "chrome/browser/ui/omnibox/omnibox_view.h"
 #include "chrome/common/pref_names.h"
@@ -754,11 +753,11 @@ void OmniboxEditModel::OpenMatch(AutocompleteMatch match,
       // in template_url.h.
     }
 
-    UMA_HISTOGRAM_ENUMERATION(
-        "Omnibox.SearchEngineType",
-        TemplateURLPrepopulateData::GetEngineType(
-            *template_url, UIThreadSearchTermsData(profile_)),
-        SEARCH_ENGINE_MAX);
+    SearchEngineType search_engine_type = match.destination_url.is_valid() ?
+        TemplateURLPrepopulateData::GetEngineType(match.destination_url) :
+        SEARCH_ENGINE_OTHER;
+    UMA_HISTOGRAM_ENUMERATION("Omnibox.SearchEngineType", search_engine_type,
+                              SEARCH_ENGINE_MAX);
   }
 
   // Get the current text before we call RevertAll() which will clear it.
