f29011a7de3ad661ce698b99b0e11ab52eeee86b
sky <sky@chromium.org>
2015-08-04 09:51:21 -0700

    Fixes shutdown with multiple documents using the same frame tree
    web_widget_->close() isn't always enough.
    
    BUG=479172,490221
    TEST=covered by tests
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/1269503006
    
    Cr-Commit-Position: refs/heads/master@{#341738}

--- components/html_viewer/html_frame.h
+++ components/html_viewer/html_frame.h
@@ -137,7 +137,10 @@ class HTMLFrame : public blink::WebFrameClient,
   void SetView(mojo::View* view);
 
   // Creates the appropriate WebWidget implementation for the Frame.
-  void CreateWebWidget();
+  void CreateRootWebWidget();
+  void CreateLocalRootWebWidget(blink::WebLocalFrame* local_frame);
+
+  void InitializeWebWidget();
 
   void UpdateFocus();
 
@@ -232,6 +235,8 @@ class HTMLFrame : public blink::WebFrameClient,
 
   HTMLFrameTreeManager* frame_tree_manager_;
   HTMLFrame* parent_;
+  // |view_| is non-null for local frames or remote frames that were once
+  // local.
   mojo::View* view_;
   // The id for this frame. If there is a view, this is the same id as the
   // view has.
