da3cde081069f9d9dd56de8e1e068945f461d13c
dcastagna <dcastagna@chromium.org>
2015-08-03 17:45:18 -0700

    Implement and test task runners in RasterWorkerPool.
    This CL moves RasterWorkerPool outside render_thread_impl.h and
    in its own file, implements TaskRunner and SequencedTaskRunner
    on top of it and properly tests the two implementations.
    
    BUG=
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1254003003
    
    Cr-Commit-Position: refs/heads/master@{#341654}

--- cc/raster/task_graph_runner.h
+++ cc/raster/task_graph_runner.h
@@ -138,6 +138,9 @@ class CC_EXPORT TaskGraphRunner {
   // Warning: if the TaskGraphRunner remains busy, it may never quit.
   void Shutdown();
 
+  // Wait for all the tasks to finish running on all the namespaces.
+  void FlushForTesting();
+
  private:
   struct PrioritizedTask {
     typedef std::vector<PrioritizedTask> Vector;
