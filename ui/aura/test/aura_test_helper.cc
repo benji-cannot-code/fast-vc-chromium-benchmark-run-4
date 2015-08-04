18f2646ba1b0c093b1c5557201c3bffebf7bc689
sadrul <sadrul@chromium.org>
2015-08-04 06:44:33 -0700

    aura-test: Create the Env instance only if one doesn't already exist.
    Some tests manage the aura::Env instance's lifetime (e.g. content-unittests), so
    update AuraTestHelper to only create the Env instance if one hasn't already been
    created.
    
    BUG=515031
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1268063002
    
    Cr-Commit-Position: refs/heads/master@{#341714}

--- ui/aura/test/aura_test_helper.cc
+++ ui/aura/test/aura_test_helper.cc
@@ -35,7 +35,8 @@ namespace test {
 
 AuraTestHelper::AuraTestHelper(base::MessageLoopForUI* message_loop)
     : setup_called_(false),
-      teardown_called_(false) {
+      teardown_called_(false),
+      env_created_(false) {
   DCHECK(message_loop);
   message_loop_ = message_loop;
   // Disable animations during tests.
@@ -57,7 +58,10 @@ AuraTestHelper::~AuraTestHelper() {
 void AuraTestHelper::SetUp(ui::ContextFactory* context_factory) {
   setup_called_ = true;
 
-  Env::CreateInstance(true);
+  if (!Env::GetInstanceDontCreate()) {
+    env_created_ = true;
+    Env::CreateInstance(true);
+  }
   Env::GetInstance()->set_context_factory(context_factory);
   // Unit tests generally don't want to query the system, rather use the state
   // from RootWindow.
@@ -97,7 +101,8 @@ void AuraTestHelper::TearDown() {
 
   ui::ShutdownInputMethodForTesting();
 
-  Env::DeleteInstance();
+  if (env_created_)
+    Env::DeleteInstance();
 }
 
 void AuraTestHelper::RunAllPendingInMessageLoop() {
