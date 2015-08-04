6c161ecc5146cba5bc8c9de2a3cce28d49524f2b
cbiesinger@chromium.org <cbiesinger@chromium.org>
2015-08-04 22:07:10 +0000

    Make percentageLogicalHeightIsResolvable() a member function
    All of its callers only pass "this". This simplifies the callers a bit
    and allows the function to be called elsewhere.
    
    No new tests, just some minor code cleanup.
    
    R=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/1263283003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199996 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -4266,11 +4266,6 @@ bool LayoutBox::hasDefiniteLogicalWidth() const
     return logicalWidthIsResolvable(*this);
 }
 
-inline static bool percentageLogicalHeightIsResolvable(const LayoutBox* box)
-{
-    return LayoutBox::percentageLogicalHeightIsResolvableFromBlock(box->containingBlock(), box->isOutOfFlowPositioned());
-}
-
 bool LayoutBox::percentageLogicalHeightIsResolvableFromBlock(const LayoutBlock* containingBlock, bool isOutOfFlowPositioned)
 {
     // In quirks mode, blocks with auto height are skipped, and we keep looking for an enclosing
@@ -4318,6 +4313,11 @@ bool LayoutBox::percentageLogicalHeightIsResolvableFromBlock(const LayoutBlock* 
     return false;
 }
 
+bool LayoutBox::percentageLogicalHeightIsResolvable() const
+{
+    return LayoutBox::percentageLogicalHeightIsResolvableFromBlock(containingBlock(), isOutOfFlowPositioned());
+}
+
 bool LayoutBox::hasDefiniteLogicalHeight() const
 {
     const Length& logicalHeight = style()->logicalHeight();
@@ -4332,7 +4332,7 @@ bool LayoutBox::hasDefiniteLogicalHeight() const
     if (hasOverrideContainingBlockLogicalHeight())
         return overrideContainingBlockContentLogicalHeight() != -1;
 
-    return percentageLogicalHeightIsResolvable(this);
+    return percentageLogicalHeightIsResolvable();
 }
 
 bool LayoutBox::hasUnsplittableScrollingOverflow() const
@@ -4348,8 +4348,8 @@ bool LayoutBox::hasUnsplittableScrollingOverflow() const
     // conditions, but it should work out to be good enough for common cases. Paginating overflow
     // with scrollbars present is not the end of the world and is what we used to do in the old model anyway.
     return !style()->logicalHeight().isIntrinsicOrAuto()
-        || (!style()->logicalMaxHeight().isIntrinsicOrAuto() && !style()->logicalMaxHeight().isMaxSizeNone() && (!style()->logicalMaxHeight().hasPercent() || percentageLogicalHeightIsResolvable(this)))
-        || (!style()->logicalMinHeight().isIntrinsicOrAuto() && style()->logicalMinHeight().isPositive() && (!style()->logicalMinHeight().hasPercent() || percentageLogicalHeightIsResolvable(this)));
+        || (!style()->logicalMaxHeight().isIntrinsicOrAuto() && !style()->logicalMaxHeight().isMaxSizeNone() && (!style()->logicalMaxHeight().hasPercent() || percentageLogicalHeightIsResolvable()))
+        || (!style()->logicalMinHeight().isIntrinsicOrAuto() && style()->logicalMinHeight().isPositive() && (!style()->logicalMinHeight().hasPercent() || percentageLogicalHeightIsResolvable()));
 }
 
 bool LayoutBox::isUnsplittableForPagination() const
