d8ff2cbdc49f58f5aea90d90b3573bc2572ca2eb
tkent@chromium.org <tkent@chromium.org>
2015-08-04 22:47:27 +0000

    Dispatch input/change events asynchronously for menulist SELECTs.
    This might be a fix for crbug.com/515899.  If a JavaScript dialog is opened
    while a SELECT popup is shown, and the SELECT popup is hidden before the dialog
    is hidden, window focus is not correctly restored.  So, we guess delaying change
    events solves the problem.
    
    Implementations of enqueue*Event() is similar to HTMLFormControlElement
    ::dispatchFormControl*Event().
    
    This CL removes fast/events/onchange-select-popup.html. We stopped
    the behavior expected by the test some years ago.  The test expected
    Firefox-compatible behavior, pressing Enter key dispatches a 'change'
    event.  Out current behavior is that type-ahead dispatches a 'change'
    event, and it's compatible with IE.
    
    BUG=380452,515899
    
    Review URL: https://codereview.chromium.org/1265983009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200000 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLFormControlElementWithState.cpp
+++ third_party/WebKit/Source/core/html/HTMLFormControlElementWithState.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/html/HTMLFormControlElementWithState.h"
 
+#include "core/events/Event.h"
 #include "core/frame/FrameHost.h"
 #include "core/frame/LocalFrame.h"
 #include "core/html/HTMLFormElement.h"
@@ -94,6 +95,24 @@ void HTMLFormControlElementWithState::finishParsingChildren()
 bool HTMLFormControlElementWithState::isFormControlElementWithState() const
 {
     return true;
+}
+
+void HTMLFormControlElementWithState::enqueueInputEvent()
+{
+    RefPtrWillBeRawPtr<Event> event = Event::createBubble(EventTypeNames::input);
+    event->setTarget(this);
+    document().enqueueAnimationFrameEvent(event.release());
+    // We need to dispatch a change event on blur.
+    setChangedSinceLastFormControlChangeEvent(true);
+}
+
+void HTMLFormControlElementWithState::enqueueChangeEvent()
+{
+    RefPtrWillBeRawPtr<Event> event = Event::createBubble(EventTypeNames::change);
+    event->setTarget(this);
+    document().enqueueAnimationFrameEvent(event.release());
+    // We don't need to dispatch a change event on blur.
+    setChangedSinceLastFormControlChangeEvent(false);
 }
 
 } // namespace blink
