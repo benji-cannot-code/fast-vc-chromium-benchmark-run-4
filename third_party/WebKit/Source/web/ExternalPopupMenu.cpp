4cfcaa8c47d1bb8e13f2309b1ae80f5b46a0c07c
tkent@chromium.org <tkent@chromium.org>
2015-08-04 03:31:10 +0000

    Remove some member functions of PopupMenuClient.
    Remove the following member functions:
     - itemToolTip
     - itemAccessibilityText
     - itemIsEnabled
     - itemIsSeparator
     - itemIsLabel
     - itemIsSelected
    
    Use itemElement and Element functions instead.
    
    This CL has no behavior changes.
    
    BUG=515013
    
    Review URL: https://codereview.chromium.org/1260283005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199935 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/ExternalPopupMenu.cpp
+++ third_party/WebKit/Source/web/ExternalPopupMenu.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/FrameHost.h"
 #include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
+#include "core/html/HTMLOptionElement.h"
 #include "core/html/forms/PopupMenuClient.h"
 #include "core/page/Page.h"
 #include "core/style/ComputedStyle.h"
@@ -199,17 +200,20 @@ void ExternalPopupMenu::getPopupMenuInfo(WebPopupMenuInfo& info, PopupMenuClient
         if (popupMenuClient.itemIsDisplayNone(i))
             continue;
 
+        Element& itemElement = popupMenuClient.itemElement(i);
         WebMenuItemInfo& popupItem = items[count++];
         popupItem.label = popupMenuClient.itemText(i);
-        popupItem.toolTip = popupMenuClient.itemToolTip(i);
-        if (popupMenuClient.itemIsSeparator(i))
+        popupItem.toolTip = itemElement.title();
+        popupItem.checked = false;
+        if (isHTMLHRElement(itemElement)) {
             popupItem.type = WebMenuItemInfo::Separator;
-        else if (popupMenuClient.itemIsLabel(i))
+        } else if (isHTMLOptGroupElement(itemElement)) {
             popupItem.type = WebMenuItemInfo::Group;
-        else
+        } else {
             popupItem.type = WebMenuItemInfo::Option;
-        popupItem.enabled = popupMenuClient.itemIsEnabled(i);
-        popupItem.checked = popupMenuClient.itemIsSelected(i);
+            popupItem.checked = toHTMLOptionElement(itemElement).selected();
+        }
+        popupItem.enabled = !itemElement.isDisabledFormControl();
         const ComputedStyle& style = *popupMenuClient.computedStyleForItem(i);
         popupItem.textDirection = toWebTextDirection(style.direction());
         popupItem.hasTextDirectionOverride = isOverride(style.unicodeBidi());
