db653e4e4cffcb20d691af10b0f695b026ce7110
sigbjornf@opera.com <sigbjornf@opera.com>
2015-08-05 10:15:11 +0000

    Oilpan: have CSSImageGeneratorValue use SelfKeepAlive<>.
    Canonicalize CSSImageGeneratorValue's self-referential persistent as
    a SelfKeepAlive<>.
    
    R=haraken
    BUG=366546
    
    Review URL: https://codereview.chromium.org/1261253004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200044 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSImageGeneratorValue.cpp
+++ third_party/WebKit/Source/core/css/CSSImageGeneratorValue.cpp
@@ -51,7 +51,7 @@ void CSSImageGeneratorValue::addClient(LayoutObject* layoutObject, const IntSize
 #else
     if (m_clients.isEmpty()) {
         ASSERT(!m_keepAlive);
-        m_keepAlive = adoptPtr(new Persistent<CSSImageGeneratorValue>(this));
+        m_keepAlive = this;
     }
 #endif
 
@@ -90,7 +90,7 @@ void CSSImageGeneratorValue::removeClient(LayoutObject* layoutObject)
 #else
     if (m_clients.isEmpty()) {
         ASSERT(m_keepAlive);
-        m_keepAlive = nullptr;
+        m_keepAlive.clear();
     }
 #endif
 }
