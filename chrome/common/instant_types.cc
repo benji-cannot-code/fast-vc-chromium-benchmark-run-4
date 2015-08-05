324cc1fbef57fcf284af3d0c2fb69f076db5d838
fserb <fserb@chromium.org>
2015-08-05 13:26:08 -0700

    Adds an experiment that enables SuggestionsService suggestions (MostLikely) on LocalNTP
    BUG=514752
    
    Review URL: https://codereview.chromium.org/1260113002
    
    Cr-Commit-Position: refs/heads/master@{#341958}

--- chrome/common/instant_types.cc
+++ chrome/common/instant_types.cc
@@ -89,6 +89,10 @@ const char kRLZParameterKey[] = "rlz";
 const char kInputEncodingKey[] = "ie";
 const char kAssistedQueryStatsKey[] = "aqs";
 
+InstantMostVisitedItem::InstantMostVisitedItem() {}
+
+InstantMostVisitedItem::~InstantMostVisitedItem() {}
+
 EmbeddedSearchRequestParams::EmbeddedSearchRequestParams() {
 }
 
