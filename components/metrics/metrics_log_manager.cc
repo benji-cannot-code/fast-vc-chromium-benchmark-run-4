8bf13875866a30694cc363e209fd6401bc4f7758
isherman <isherman@chromium.org>
2015-08-05 15:20:31 -0700

    [UMA, Cleanup] Remove two not very helpful timing histograms.
    These histograms show that the events are generally quite fast, and any future
    slowness should be identified with a profiler, rather than by remembering to
    check these histograms.
    
    BUG=517206
    TEST=none
    R=asvitkine@chromium.org
    
    Review URL: https://codereview.chromium.org/1274893002
    
    Cr-Commit-Position: refs/heads/master@{#341988}

--- components/metrics/metrics_log_manager.cc
+++ components/metrics/metrics_log_manager.cc
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_util.h"
-#include "base/timer/elapsed_timer.h"
 #include "components/metrics/metrics_log.h"
 #include "components/metrics/metrics_pref_names.h"
 
@@ -119,18 +117,13 @@ void MetricsLogManager::PersistUnsentLogs() {
   if (!unsent_logs_loaded_)
     return;
 
-  base::ElapsedTimer timer;
   initial_log_queue_.SerializeLogs();
   ongoing_log_queue_.SerializeLogs();
-  UMA_HISTOGRAM_TIMES("UMA.StoreLogsTime", timer.Elapsed());
 }
 
 void MetricsLogManager::LoadPersistedUnsentLogs() {
-  base::ElapsedTimer timer;
   initial_log_queue_.DeserializeLogs();
   ongoing_log_queue_.DeserializeLogs();
-  UMA_HISTOGRAM_TIMES("UMA.LoadLogsTime", timer.Elapsed());
-
   unsent_logs_loaded_ = true;
 }
 
