c98a6e8980e571e2d84b867694fa69cefbeedab4
bzanotti <bzanotti@chromium.org>
2015-08-05 10:37:15 -0700

    Move Add/RemovePolicyDecider to WebState.
    The methods were initially put into WebStateImpl to respect the TODO
    on Add/RemoveObserver in WebState, but this is harmful: it makes it
    impossible to use a TestWebState with a WebStatePolicyDecider.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1276553002
    
    Cr-Commit-Position: refs/heads/master@{#341917}

--- ios/web/public/web_state/web_state.h
+++ ios/web/public/web_state/web_state.h
@@ -46,6 +46,7 @@ class BrowserState;
 class NavigationManager;
 class WebInterstitial;
 class WebStateObserver;
+class WebStatePolicyDecider;
 
 // Core interface for interaction with the web.
 class WebState : public base::SupportsUserData {
@@ -193,6 +194,7 @@ class WebState : public base::SupportsUserData {
 
  protected:
   friend class WebStateObserver;
+  friend class WebStatePolicyDecider;
 
   // Adds and removes observers for page navigation notifications. The order in
   // which notifications are sent to observers is undefined. Clients must be
@@ -200,6 +202,13 @@ class WebState : public base::SupportsUserData {
   // TODO(droger): Move these methods to WebStateImpl once it is in ios/.
   virtual void AddObserver(WebStateObserver* observer) = 0;
   virtual void RemoveObserver(WebStateObserver* observer) = 0;
+
+  // Adds and removes policy deciders for navigation actions. The order in which
+  // deciders are called is undefined, and will stop on the first decider that
+  // refuses a navigation. Clients must be sure to remove the deciders before
+  // they go away.
+  virtual void AddPolicyDecider(WebStatePolicyDecider* decider) = 0;
+  virtual void RemovePolicyDecider(WebStatePolicyDecider* decider) = 0;
 
   WebState() {}
 };
