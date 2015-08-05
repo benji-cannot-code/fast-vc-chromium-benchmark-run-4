57ffea8cb181988a762de44ff2643476dad016c6
apacible <apacible@chromium.org>
2015-08-05 16:35:16 -0700

    Update Media Router cast mode list.
    This change includes:
    - Show different icons based on cast mode type.
    - Add "Share your screen" subheading between the default cast mode(s) and non-default cast mode(s).
    - Show the URL host instead of "Cast" for the default cast mode.
    
    Changes (pixels, colors, icons) taken from redlines or from offline conversation with UX.
    
    BUG=501452
    
    Review URL: https://codereview.chromium.org/1263303002
    
    Cr-Commit-Position: refs/heads/master@{#342009}

--- chrome/browser/resources/media_router/media_router_data.js
+++ chrome/browser/resources/media_router/media_router_data.js
@@ -9,6 +9,16 @@ cr.define('media_router', function() {
   'use strict';
 
   /**
+   * This corresponds to the C++ MediaCastMode.
+   * @enum {number}
+   */
+  var CastModeType = {
+    DEFAULT: 0,
+    TAB_MIRROR: 1,
+    DESKTOP_MIRROR: 2,
+  };
+
+  /**
    * @enum {string}
    */
   var SinkStatus = {
@@ -19,8 +29,7 @@ cr.define('media_router', function() {
 
 
   /**
-   * @param {number} type The type of cast mode. This corresponds to the
-   *   C++ MediaCastMode.
+   * @param {media_router.CastModeType} type The type of cast mode.
    * @param {string} title The title of the cast mode.
    * @param {string} description The description of the cast mode.
    * @param {string} host The hostname of the site to cast.
@@ -156,6 +165,7 @@ cr.define('media_router', function() {
   };
 
   return {
+    CastModeType: CastModeType,
     SinkStatus: SinkStatus,
     CastMode: CastMode,
     Issue: Issue,
