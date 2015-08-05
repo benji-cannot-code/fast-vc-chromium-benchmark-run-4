a161d6497fc95ae37d5814c568173202af564951
bashi <bashi@chromium.org>
2015-08-05 04:40:38 -0700

    Stop using WebString in SetRuntimeFeaturesDefaultsAndUpdateFromArgs()
    We call SetRuntimeFeaturesDefaultsAndUpdateFromArgs() before
    blink::initialize() so we can't use WebString in this function.
    
    BUG=501171
    
    Review URL: https://codereview.chromium.org/1258023003
    
    Cr-Commit-Position: refs/heads/master@{#341887}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -203,9 +203,8 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
 
   if (command_line.HasSwitch(switches::kEnableWebVR)) {
     WebRuntimeFeatures::enableWebVR(true);
-    // TODO(bashi): Remove blink::WebString() cast.
     WebRuntimeFeatures::enableFeatureFromString(
-        blink::WebString("GeometryInterfaces"), true);
+        std::string("GeometryInterfaces"), true);
   }
 
   // Enable explicitly enabled features, and then disable explicitly disabled
@@ -216,8 +215,7 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
         command_line.GetSwitchValueASCII(switches::kEnableBlinkFeatures), ',',
         &enabled_features);
     for (const std::string& feature : enabled_features) {
-      WebRuntimeFeatures::enableFeatureFromString(
-          blink::WebString::fromLatin1(feature), true);
+      WebRuntimeFeatures::enableFeatureFromString(feature, true);
     }
   }
   if (command_line.HasSwitch(switches::kDisableBlinkFeatures)) {
@@ -226,8 +224,7 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
         command_line.GetSwitchValueASCII(switches::kDisableBlinkFeatures), ',',
         &disabled_features);
     for (const std::string& feature : disabled_features) {
-      WebRuntimeFeatures::enableFeatureFromString(
-          blink::WebString::fromLatin1(feature), false);
+      WebRuntimeFeatures::enableFeatureFromString(feature, false);
     }
   }
 }
