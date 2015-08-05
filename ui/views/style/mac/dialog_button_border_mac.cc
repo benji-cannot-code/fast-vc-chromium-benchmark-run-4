11275e9403c4b00d7b5a391d4c8016ce478dd4d9
bungeman <bungeman@chromium.org>
2015-08-05 13:48:52 -0700

    Directly include Skia headers where needed.
    A propsed Skia change will forward declare more types. The changes
    in this CL are needed so that ui/views will continue to compile on
    Mac after the Skia change.
    
    Review URL: https://codereview.chromium.org/1272693002
    
    Cr-Commit-Position: refs/heads/master@{#341968}

--- ui/views/style/mac/dialog_button_border_mac.cc
+++ ui/views/style/mac/dialog_button_border_mac.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "skia/ext/refptr.h"
 #include "third_party/skia/include/core/SkCanvas.h"
+#include "third_party/skia/include/core/SkPaint.h"
+#include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/effects/SkGradientShader.h"
 #include "ui/gfx/canvas.h"
 #include "ui/views/border.h"
