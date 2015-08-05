46d94e6b5fac202f77970d0f8ffb263458da58f8
rob <rob@robwu.nl>
2015-08-05 12:30:12 -0700

    Hide requests in an extension from other extensions
    BUG=510802
    
    Review URL: https://codereview.chromium.org/1267183003
    
    Cr-Commit-Position: refs/heads/master@{#341939}

---
+++ chrome/test/data/extensions/api_test/webrequest_extensions/app/background.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.getConfig(function(config) {
+  var baseUrl = 'http://example.com:' + config.testServer.port;
+
+  var x = new XMLHttpRequest();
+  x.open('GET', baseUrl + '/extensions/test_file.txt?app');
+  x.onloadend = function() {
+    // Just a sanity check to ensure that the server is running.
+    // The test does not change the response.
+    chrome.test.assertEq('Hello!', x.responseText);
+    chrome.test.sendMessage('app_done');
+  };
+  x.send();
+});
---
+++ chrome/test/data/extensions/api_test/webrequest_extensions/extension/contentscript.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// location.origin will be equivalent to 'http://example.com:PORT';
+var baseUrl = location.origin;
+
+chrome.test.sendMessage('contentscript_ready', function() {
+  var x = new XMLHttpRequest();
+  x.open('GET', baseUrl + '/extensions/test_file.txt?contentscript');
+  x.onloadend = function() {
+    chrome.test.assertEq('Hello!', x.responseText);
+
+    chrome.test.sendMessage('contentscript_done', function() {
+      var frame = document.createElement('iframe');
+      frame.src = chrome.runtime.getURL('frame.html');
+      document.body.appendChild(frame);
+    });
+  };
+  x.send();
+});
