4a0c9f406ea5ce5c816058e5b41c3e02058ba3e4
vabr <vabr@chromium.org>
2015-08-05 04:20:47 -0700

    Disable MSE_**EncryptedMediaTest.* on Win dbg
    Tests are flaky, more info on the bug.
    
    TBR=dalecurtis@chromium.org
    BUG=517018
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1276433002
    
    Cr-Commit-Position: refs/heads/master@{#341883}

--- chrome/browser/media/encrypted_media_browsertest.cc
+++ chrome/browser/media/encrypted_media_browsertest.cc
@@ -535,6 +535,8 @@ INSTANTIATE_TEST_CASE_P(SRC_ClearKey,
                                 Values(UNPREFIXED)));
 #endif  // !defined(OS_ANDROID)
 
+#if !defined(OS_WIN) || defined(NDEBUG)
+// Tests flaky on Win 7 dbg. http://crbug.com/517018
 INSTANTIATE_TEST_CASE_P(MSE_ClearKey_Prefixed,
                         EncryptedMediaTest,
                         Combine(Values(kPrefixedClearKeyKeySystem),
@@ -545,6 +547,7 @@ INSTANTIATE_TEST_CASE_P(MSE_ClearKey,
                         Combine(Values(kClearKeyKeySystem),
                                 Values(MSE),
                                 Values(UNPREFIXED)));
+#endif  // !defined(OS_WIN) || defined(NDEBUG)
 
 // External Clear Key is currently only used on platforms that use Pepper CDMs.
 #if defined(ENABLE_PEPPER_CDMS)
@@ -558,6 +561,8 @@ INSTANTIATE_TEST_CASE_P(SRC_ExternalClearKey,
                         Combine(Values(kExternalClearKeyKeySystem),
                                 Values(SRC),
                                 Values(UNPREFIXED)));
+#if !defined(OS_WIN) || defined(NDEBUG)
+// Tests flaky on Win 7 dbg. http://crbug.com/517018
 INSTANTIATE_TEST_CASE_P(MSE_ExternalClearKey_Prefixed,
                         EncryptedMediaTest,
                         Combine(Values(kExternalClearKeyKeySystem),
@@ -579,6 +584,7 @@ INSTANTIATE_TEST_CASE_P(MSE_ExternalClearKeyDecryptOnly,
                         Combine(Values(kExternalClearKeyDecryptOnlyKeySystem),
                                 Values(MSE),
                                 Values(UNPREFIXED)));
+#endif  // !defined(OS_WIN) || defined(NDEBUG)
 #endif  // defined(ENABLE_PEPPER_CDMS)
 
 #if defined(WIDEVINE_CDM_AVAILABLE)
