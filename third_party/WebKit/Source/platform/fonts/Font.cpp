735e720992c1012ea92403d4702628af661ae865
bungeman@chromium.org <bungeman@chromium.org>
2015-08-05 21:53:55 +0000

    Remove use of SkAutoSTMalloc in Blink.
    Skia is seeing if it can move SkTemplates.h to be private, since its
    contents aren't designed for direct external use. This removes a use
    in Blink and replaces it with Vector, which should be equivalent.
    
    Review URL: https://codereview.chromium.org/1272383002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200069 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/Font.cpp
+++ third_party/WebKit/Source/platform/fonts/Font.cpp
@@ -26,8 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "platform/fonts/Font.h"
 
-#include "SkPaint.h"
-#include "SkTemplates.h"
 #include "platform/LayoutUnit.h"
 #include "platform/RuntimeEnabledFeatures.h"
 #include "platform/fonts/Character.h"
@@ -46,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/text/TextRunIterator.h"
 #include "platform/transforms/AffineTransform.h"
 #include "third_party/skia/include/core/SkCanvas.h"
+#include "third_party/skia/include/core/SkPaint.h"
 #include "wtf/MainThread.h"
 #include "wtf/StdLibExtras.h"
 #include "wtf/text/CharacterNames.h"
@@ -656,12 +655,11 @@ void Font::drawGlyphs(SkCanvas* canvas, const SkPaint& paint, const SimpleFontDa
     ASSERT(glyphBuffer.size() >= from + numGlyphs);
 
     if (!glyphBuffer.hasVerticalOffsets()) {
-        SkAutoSTMalloc<64, SkScalar> storage(numGlyphs);
-        SkScalar* xpos = storage.get();
+        Vector<SkScalar, 64> xpos(numGlyphs);
         for (unsigned i = 0; i < numGlyphs; i++)
             xpos[i] = SkFloatToScalar(point.x() + glyphBuffer.xOffsetAt(from + i));
 
-        paintGlyphsHorizontal(canvas, paint, font, glyphBuffer.glyphs(from), numGlyphs, xpos,
+        paintGlyphsHorizontal(canvas, paint, font, glyphBuffer.glyphs(from), numGlyphs, xpos.data(),
             SkFloatToScalar(point.y()), textRect, deviceScaleFactor);
         return;
     }
@@ -678,15 +676,14 @@ void Font::drawGlyphs(SkCanvas* canvas, const SkPaint& paint, const SimpleFontDa
     const float verticalBaselineXOffset = drawVertically ? SkFloatToScalar(font->fontMetrics().floatAscent() - font->fontMetrics().floatAscent(IdeographicBaseline)) : 0;
 
     ASSERT(glyphBuffer.hasVerticalOffsets());
-    SkAutoSTMalloc<32, SkPoint> storage(numGlyphs);
-    SkPoint* pos = storage.get();
+    Vector<SkPoint, 32> pos(numGlyphs);
     for (unsigned i = 0; i < numGlyphs; i++) {
         pos[i].set(
             SkFloatToScalar(point.x() + verticalBaselineXOffset + glyphBuffer.xOffsetAt(from + i)),
             SkFloatToScalar(point.y() + glyphBuffer.yOffsetAt(from + i)));
     }
 
-    paintGlyphs(canvas, paint, font, glyphBuffer.glyphs(from), numGlyphs, pos, textRect, deviceScaleFactor);
+    paintGlyphs(canvas, paint, font, glyphBuffer.glyphs(from), numGlyphs, pos.data(), textRect, deviceScaleFactor);
     canvas->restoreToCount(canvasStackLevel);
 }
 
