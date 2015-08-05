5acdf936270443afa6bc1965f139f6af0f95f274
falken <falken@chromium.org>
2015-08-05 05:54:32 -0700

    Fix bug in EmbeddedWorkerRegistry::RemoveWorker
    RemoveWorker would remove the worker's process id from the process map, but
    other workers may live in that process.
    
    Review URL: https://codereview.chromium.org/1274573002
    
    Cr-Commit-Position: refs/heads/master@{#341890}

--- content/browser/service_worker/embedded_worker_registry.h
+++ content/browser/service_worker/embedded_worker_registry.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -109,6 +110,9 @@ class CONTENT_EXPORT EmbeddedWorkerRegistry
  private:
   friend class base::RefCounted<EmbeddedWorkerRegistry>;
   friend class EmbeddedWorkerInstance;
+  friend class EmbeddedWorkerInstanceTest;
+  FRIEND_TEST_ALL_PREFIXES(EmbeddedWorkerInstanceTest,
+                           RemoveWorkerInSharedProcess);
 
   typedef std::map<int, EmbeddedWorkerInstance*> WorkerInstanceMap;
   typedef std::map<int, IPC::Sender*> ProcessToSenderMap;
