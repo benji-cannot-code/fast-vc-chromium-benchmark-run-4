ae40e69dcfe68367b720ea539a44f367efa30877
yawano <yawano@chromium.org>
2015-08-05 03:18:20 -0700

    Gallery: fix button animations.
    * Change animation of edit operation buttons to paper-ripple based
      animation from files-toggle-ripple animation.
    * Remove labels from edit operation buttons.
    * Remove toggle ripple animation from crop aspect ratio buttons.
    * Fix font size of crop aspect ratio buttons.
    * Clean up CSS and JS code.
    
    BUG=516468
    TEST=none
    
    Review URL: https://codereview.chromium.org/1271943002
    
    Cr-Commit-Position: refs/heads/master@{#341876}

--- ui/file_manager/externs/paper_elements.js
+++ ui/file_manager/externs/paper_elements.js
@@ -14,3 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function PaperRipple() {}
 
 PaperRipple.prototype.simulatedRipple = function() {};
+
+/**
+ * @param {Event=} event
+ */
+PaperRipple.prototype.downAction = function(event) {};
+
+/**
+ * @param {Event=} event
+ */
+PaperRipple.prototype.upAction = function(event) {};
