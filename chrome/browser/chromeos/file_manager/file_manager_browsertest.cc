8f9ea160bb4b050014b09e5fe5d364c3f2a26c6e
vabr <vabr@chromium.org>
2015-08-06 01:08:35 -0700

    Fix disabling in file_manager_browsertest.cc
    The tests are disabled on slow bots, but due to typos, some of them are in fact not. This CL fixes the typos.
    
    BUG=327719
    TBR=yawano@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1276633005
    
    Cr-Commit-Position: refs/heads/master@{#342075}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -280,11 +280,11 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
 #else
 #define MAYBE_ExecuteDefaultTaskOnDrive ExecuteDefaultTaskOnDrive
 #endif
-INSTANTIATE_TEST_CASE_P(
+WRAPPED_INSTANTIATE_TEST_CASE_P(
     MAYBE_ExecuteDefaultTaskOnDrive,
     FileManagerBrowserTest,
-    ::testing::Values(
-        TestParameter(NOT_IN_GUEST_MODE, "executeDefaultTaskOnDrive")));
+    ::testing::Values(TestParameter(NOT_IN_GUEST_MODE,
+                                    "executeDefaultTaskOnDrive")));
 
 #if defined(DISABLE_SLOW_FILESAPP_TESTS)
 #define MAYBE_DefaultActionDialog DISABLED_DefaultActionDialog
@@ -341,10 +341,10 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
 #else
 #define MAYBE_TabIndex TabIndex
 #endif
-INSTANTIATE_TEST_CASE_P(MAYBE_TabIndex,
-                        FileManagerBrowserTest,
-                        ::testing::Values(TestParameter(NOT_IN_GUEST_MODE,
-                                                        "searchBoxFocus")));
+WRAPPED_INSTANTIATE_TEST_CASE_P(
+    MAYBE_TabIndex,
+    FileManagerBrowserTest,
+    ::testing::Values(TestParameter(NOT_IN_GUEST_MODE, "searchBoxFocus")));
 
 #if defined(DISABLE_SLOW_FILESAPP_TESTS)
 #define MAYBE_TabindexFocus DISABLED_TabindexFocus
@@ -362,7 +362,7 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
 #else
 #define MAYBE_TabindexFocusDownloads TabindexFocusDownloads
 #endif
-INSTANTIATE_TEST_CASE_P(
+WRAPPED_INSTANTIATE_TEST_CASE_P(
     MAYBE_TabindexFocusDownloads,
     FileManagerBrowserTest,
     ::testing::Values(TestParameter(NOT_IN_GUEST_MODE,
