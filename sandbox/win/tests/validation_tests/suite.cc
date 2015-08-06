495b97e41b241bf14897bd4d961cef486d0ed653
scottmg <scottmg@chromium.org>
2015-08-06 07:22:09 -0700

    Fix version check in ValidationSuite
    This was previously default constructing a base::win::Version enum,
    resulting in comparisons of "0 < VERSION_VISTA", so these tests were
    always skipped.
    
    (They have been disabled for >3 years, so if this causes problems,
    feel free to revert this CL.)
    
    R=wfh@chromium.org
    
    Review URL: https://codereview.chromium.org/1212893008
    
    Cr-Commit-Position: refs/heads/master@{#342109}

--- sandbox/win/tests/validation_tests/suite.cc
+++ sandbox/win/tests/validation_tests/suite.cc
@@ -168,7 +168,7 @@ TEST(ValidationSuite, TestProcessDenyLockdown) {
 // to the integrity label changing after startup via SetDelayedIntegrityLevel).
 TEST(ValidationSuite, TestProcessDenyLowIntegrity) {
   // This test applies only to Vista and above.
-  if (base::win::Version() < base::win::VERSION_VISTA)
+  if (base::win::GetVersion() < base::win::VERSION_VISTA)
     return;
 
   TestRunner runner;
@@ -189,7 +189,7 @@ TEST(ValidationSuite, TestProcessDenyLowIntegrity) {
 // Tests that a locked-down process cannot open a low-integrity process.
 TEST(ValidationSuite, TestProcessDenyBelowLowIntegrity) {
   //  This test applies only to Vista and above.
-  if (base::win::Version() < base::win::VERSION_VISTA)
+  if (base::win::GetVersion() < base::win::VERSION_VISTA)
     return;
 
   TestRunner runner;
