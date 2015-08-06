5fe13e4e02386cf1498ee673c1962664321fed24
enne <enne@chromium.org>
2015-08-06 11:46:11 -0700

    cc: Fix flaky RSLLMembershipWithScale test
    Update this test to reflect the removal of twin updating.  Instead of
    having tilings removed, tilings currently just stay stale if their
    layer owner is not in the RSLL and updated.
    
    The test was going to the next step before a pinch was complete, and so
    would sometimes end up with an extra draw from source frame 0 before
    the commit from source frame 1 would occur.  This is easily repro'd by
    adding a sleep(1) to the draw function in the test.
    
    This is avoided by tracking NotifyReadyToDraw and only going to step #2
    after the draw following it.
    
    R=danakj@chromium.org,vmpstr@chromium.org
    BUG=460581
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1276683002
    
    Cr-Commit-Position: refs/heads/master@{#342158}

--- cc/test/fake_picture_layer_impl.h
+++ cc/test/fake_picture_layer_impl.h
@@ -84,7 +84,6 @@ class FakePictureLayerImpl : public PictureLayerImpl {
   using PictureLayerImpl::CanHaveTilings;
   using PictureLayerImpl::MinimumContentsScale;
   using PictureLayerImpl::SanityCheckTilingState;
-  using PictureLayerImpl::GetRecycledTwinLayer;
   using PictureLayerImpl::UpdateRasterSource;
 
   using PictureLayerImpl::UpdateIdealScales;
