b537dddb85262f4907bbb75ed5595bc1604ec03d
yusufo <yusufo@chromium.org>
2015-08-06 12:06:09 -0700

    Revert of Add custom tabs tests using intents with non-null sessions (patchset #3 id:40001 of https://codereview.chromium.org/1267243003/ )
    Reason for revert:
    Test flaking on dowsntream ToT due to deferred startup not finishing.
    
    Original issue's description:
    > Add custom tabs tests using intents with non-null sessions
    >
    > All current tests either test the service side session creation without
    > launching intents or the UI customization with no sessions. This change
    > add a trivial test that uses a valid session to launch an intent and also
    > another more complex test that reloads a new URL in the same tab when the
    > same session is used inside the intent.
    >
    > Note that the service side is interfaced directly through CustomTabConnection
    > and this doesn't exactly replicate the actual scenario since all calls come
    > from the same process.
    >
    > Committed: https://crrev.com/a69c3f7ba69a30aef2143945b8b2bbf8e6421080
    > Cr-Commit-Position: refs/heads/master@{#342131}
    
    TBR=newt@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1276993002
    
    Cr-Commit-Position: refs/heads/master@{#342169}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabsConnectionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabsConnectionTest.java
@@ -8,6 +8,8 @@ package org.chromium.chrome.browser.customtabs;
 import android.app.Application;
 import android.content.Context;
 import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
 import android.support.customtabs.ICustomTabsCallback;
 import android.test.InstrumentationTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
@@ -32,6 +34,17 @@ public class CustomTabsConnectionTest extends InstrumentationTestCase {
         mCustomTabsConnection.cleanupAll();
     }
 
+    private ICustomTabsCallback newDummyCallback() {
+        return new ICustomTabsCallback.Stub() {
+            @Override
+            public void onNavigationEvent(int navigationEvent, Bundle extras) {}
+            @Override
+            public IBinder asBinder() {
+                return this;
+            }
+        };
+    }
+
     /**
      * Tests that we can create a new session. Registering with a null callback
      * fails, as well as multiple sessions with the same callback.
@@ -39,7 +52,7 @@ public class CustomTabsConnectionTest extends InstrumentationTestCase {
     @SmallTest
     public void testNewSession() {
         assertEquals(false, mCustomTabsConnection.newSession(null));
-        ICustomTabsCallback cb = CustomTabsTestUtils.newDummyCallback();
+        ICustomTabsCallback cb = newDummyCallback();
         assertEquals(true, mCustomTabsConnection.newSession(cb));
         assertEquals(false, mCustomTabsConnection.newSession(cb));
     }
@@ -62,7 +75,7 @@ public class CustomTabsConnectionTest extends InstrumentationTestCase {
             ICustomTabsCallback cb, String url, boolean shouldSucceed) {
         mCustomTabsConnection.warmup(0);
         if (cb == null) {
-            cb = CustomTabsTestUtils.newDummyCallback();
+            cb = newDummyCallback();
             mCustomTabsConnection.newSession(cb);
         }
         boolean succeeded = mCustomTabsConnection.mayLaunchUrl(cb, Uri.parse(url), null, null);
@@ -72,13 +85,12 @@ public class CustomTabsConnectionTest extends InstrumentationTestCase {
 
     /**
      * Tests that
-     * {@link CustomTabsConnection#mayLaunchUrl(
-     * ICustomTabsCallback, Uri, android.os.Bundle, java.util.List)}
+     * {@link CustomTabsConnection#mayLaunchUrl(long, String, Bundle, List<Bundle>)}
      * returns an error when called with an invalid session ID.
      */
     @SmallTest
     public void testNoMayLaunchUrlWithInvalidSessionId() {
-        assertWarmupAndMayLaunchUrl(CustomTabsTestUtils.newDummyCallback(), URL, false);
+        assertWarmupAndMayLaunchUrl(newDummyCallback(), URL, false);
     }
 
     /**
