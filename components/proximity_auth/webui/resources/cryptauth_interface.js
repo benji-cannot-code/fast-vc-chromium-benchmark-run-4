1dd22cebed4c4fa3ab599bc57522d5b461d282b8
tengs <tengs@chromium.org>
2015-08-06 10:48:21 -0700

    Add toggling devices as unlock keys to chrome://proximity-auth.
    To receive GCM messages from CryptAuth, this CL also sets the
    device_software_package field to the same as the GCM app id during enrollment.
    
    BUG=409158
    TEST=manual
    
    Review URL: https://codereview.chromium.org/1252323008
    
    Cr-Commit-Position: refs/heads/master@{#342143}

--- components/proximity_auth/webui/resources/cryptauth_interface.js
+++ components/proximity_auth/webui/resources/cryptauth_interface.js
@@ -24,7 +24,9 @@ CryptAuthInterface = {
    * Removes an observer.
    */
   removeObserver: function(observer) {
-    CryptAuthInterface.observers_.remove(observer);
+    var index = CryptAuthInterface.observers_.indexOf(observer);
+    if (observer)
+      CryptAuthInterface.observers_.splice(index, 1);
   },
 
   /**
@@ -42,6 +44,14 @@ CryptAuthInterface = {
    */
   findReachableDevices: function() {
     chrome.send('findReachableDevices');
+  },
+
+  /**
+   * Makes the device with |publicKey| an unlock key if |makeUnlockKey| is true.
+   * Otherwise, the device will be removed as an unlock key.
+   */
+  toggleUnlockKey: function(publicKey, makeUnlockKey) {
+    chrome.send('toggleUnlockKey', [publicKey, makeUnlockKey]);
   },
 
   /**
@@ -67,7 +77,7 @@ CryptAuthInterface = {
     });
   },
 
-  /**
+  /*
    * Called by the browser when the reachable devices flow completes
    * successfully.
    * @param {Array<DeviceInfo>} reachableDevices
@@ -76,6 +86,16 @@ CryptAuthInterface = {
     CryptAuthInterface.observers_.forEach(function(observer) {
       if (observer.onGotReachableDevices != null)
         observer.onGotReachableDevices(reachableDevices);
+    });
+  },
+
+  /**
+   * Called by the browser when an unlock key is toggled.
+   */
+  onUnlockKeyToggled: function() {
+    CryptAuthInterface.observers_.forEach(function(observer) {
+      if (observer.onUnlockKeyToggled != null)
+        observer.onUnlockKeyToggled();
     });
   },
 };
