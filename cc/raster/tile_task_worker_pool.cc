4ecb4843e088854a2c63cdc58d65122d4b7527ea
danakj <danakj@chromium.org>
2015-08-05 17:25:40 -0700

    cc: Do not raster images into low res tilings.
    Low res tilings should be fast to raster, but if they have to wait on
    image decodes, they can take as long (or longer cuz they touch more
    images) than high res tilings.
    
    Just don't raster images in low res tiles, this still gives us a good
    approx of the page for scrolling, and will also keep them faster to
    raster.
    
    R=enne, vmpstr
    BUG=516500
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1271313005
    
    Cr-Commit-Position: refs/heads/master@{#342024}

--- cc/raster/tile_task_worker_pool.cc
+++ cc/raster/tile_task_worker_pool.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/playback/raster_source.h"
 #include "skia/ext/refptr.h"
 #include "third_party/skia/include/core/SkCanvas.h"
+#include "third_party/skia/include/core/SkDrawFilter.h"
 #include "third_party/skia/include/core/SkSurface.h"
 
 namespace cc {
@@ -154,6 +155,29 @@ static bool IsSupportedPlaybackToMemoryFormat(ResourceFormat format) {
   return false;
 }
 
+class SkipImageFilter : public SkDrawFilter {
+ public:
+  bool PaintHasBitmap(const SkPaint& paint) {
+    SkShader* shader = paint.getShader();
+    if (!shader)
+      return false;
+    if (shader->asAGradient(nullptr) == SkShader::kNone_GradientType)
+      return false;
+    if (shader->asABitmap(nullptr, nullptr, nullptr) !=
+        SkShader::kNone_BitmapType)
+      return true;
+    return false;
+  }
+
+  bool filter(SkPaint* paint, Type type) override {
+    if (type == kBitmap_Type)
+      return false;
+    if (PaintHasBitmap(*paint))
+      return false;
+    return true;
+  }
+};
+
 // static
 void TileTaskWorkerPool::PlaybackToMemory(void* memory,
                                           ResourceFormat format,
@@ -162,7 +186,8 @@ void TileTaskWorkerPool::PlaybackToMemory(void* memory,
                                           const RasterSource* raster_source,
                                           const gfx::Rect& canvas_bitmap_rect,
                                           const gfx::Rect& canvas_playback_rect,
-                                          float scale) {
+                                          float scale,
+                                          bool include_images) {
   DCHECK(IsSupportedPlaybackToMemoryFormat(format)) << format;
 
   // Uses kPremul_SkAlphaType since the result is not known to be opaque.
@@ -182,10 +207,15 @@ void TileTaskWorkerPool::PlaybackToMemory(void* memory,
     stride = info.minRowBytes();
   DCHECK_GT(stride, 0u);
 
+  skia::RefPtr<SkDrawFilter> image_filter;
+  if (!include_images)
+    image_filter = skia::AdoptRef(new SkipImageFilter);
+
   if (!needs_copy) {
     skia::RefPtr<SkSurface> surface = skia::AdoptRef(
         SkSurface::NewRasterDirect(info, memory, stride, &surface_props));
     skia::RefPtr<SkCanvas> canvas = skia::SharePtr(surface->getCanvas());
+    canvas->setDrawFilter(image_filter.get());
     raster_source->PlaybackToCanvas(canvas.get(), canvas_bitmap_rect,
                                     canvas_playback_rect, scale);
     return;
@@ -194,6 +224,7 @@ void TileTaskWorkerPool::PlaybackToMemory(void* memory,
   skia::RefPtr<SkSurface> surface =
       skia::AdoptRef(SkSurface::NewRaster(info, &surface_props));
   skia::RefPtr<SkCanvas> canvas = skia::SharePtr(surface->getCanvas());
+  canvas->setDrawFilter(image_filter.get());
   raster_source->PlaybackToCanvas(canvas.get(), canvas_bitmap_rect,
                                   canvas_playback_rect, scale);
 
