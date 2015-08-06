ff342e7b3a0d6d582cd2ef3d34513549d78d5352
brettw <brettw@chromium.org>
2015-08-06 15:54:16 -0700

    Replace StringToLowerASCII with base::ToLowerASCII
    Standardize on using string pieces and returning strings. Remove in-place version (this was only used in a couple places and they were not performance-critical).
    
    De-templatize the character versions of ToUpperASCII/ToLowerASCII. This would lead to bizarre errors if you pass other things (like a string). This is so little code, it's now just duplicated.
    
    I renamed StringToLowerASCII to just be ToLowerASCII so you can pass whatever you want to ToLowerASCII and it does the right thing. This seems simpler to me.
    
    This replaces all calls of StringToUpperASCII to the new form. The lowercase version is more common and will be done in a separate pass.
    
    Review URL: https://codereview.chromium.org/1280473002
    
    Cr-Commit-Position: refs/heads/master@{#342219}

--- net/http/http_util_unittest.cc
+++ net/http/http_util_unittest.cc
@@ -51,8 +51,7 @@ TEST(HttpUtilTest, IsSafeHeader) {
   for (size_t i = 0; i < arraysize(unsafe_headers); ++i) {
     EXPECT_FALSE(HttpUtil::IsSafeHeader(unsafe_headers[i]))
       << unsafe_headers[i];
-    EXPECT_FALSE(HttpUtil::IsSafeHeader(
-        base::StringToUpperASCII(std::string(unsafe_headers[i]))))
+    EXPECT_FALSE(HttpUtil::IsSafeHeader(base::ToUpperASCII(unsafe_headers[i])))
         << unsafe_headers[i];
   }
   static const char* const safe_headers[] = {
@@ -97,8 +96,7 @@ TEST(HttpUtilTest, IsSafeHeader) {
   };
   for (size_t i = 0; i < arraysize(safe_headers); ++i) {
     EXPECT_TRUE(HttpUtil::IsSafeHeader(safe_headers[i])) << safe_headers[i];
-    EXPECT_TRUE(HttpUtil::IsSafeHeader(
-        base::StringToUpperASCII(std::string(safe_headers[i]))))
+    EXPECT_TRUE(HttpUtil::IsSafeHeader(base::ToUpperASCII(safe_headers[i])))
         << safe_headers[i];
   }
 }
