f8470652818ac62caae168ffaa3274996e3ca185
miletus <miletus@chromium.org>
2015-08-06 14:16:21 -0700

    Add latency tracking for mouse move/keyboard event
    BUG=515553
    
    Review URL: https://codereview.chromium.org/1262553005
    
    Cr-Commit-Position: refs/heads/master@{#342198}

--- content/browser/renderer_host/event_with_latency_info.h
+++ content/browser/renderer_host/event_with_latency_info.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/latency_info.h"
 
 #include "content/common/input/web_input_event_traits.h"
+#include "content/public/browser/native_web_keyboard_event.h"
 
 namespace blink {
 class WebGestureEvent;
@@ -49,6 +50,8 @@ class EventWithLatencyInfo {
   }
 };
 
+typedef EventWithLatencyInfo<NativeWebKeyboardEvent>
+    NativeWebKeyboardEventWithLatencyInfo;
 typedef EventWithLatencyInfo<blink::WebGestureEvent>
     GestureEventWithLatencyInfo;
 typedef EventWithLatencyInfo<blink::WebMouseWheelEvent>
--- content/browser/renderer_host/input/mock_input_ack_handler.h
+++ content/browser/renderer_host/input/mock_input_ack_handler.h
@@ -19,8 +19,10 @@ class MockInputAckHandler : public InputAckHandler {
   ~MockInputAckHandler() override;
 
   // InputAckHandler
-  void OnKeyboardEventAck(const NativeWebKeyboardEvent& event,
+  void OnKeyboardEventAck(const NativeWebKeyboardEventWithLatencyInfo& event,
                           InputEventAckState ack_result) override;
+  void OnMouseEventAck(const MouseEventWithLatencyInfo& event,
+                       InputEventAckState ack_result) override;
   void OnWheelEventAck(const MouseWheelEventWithLatencyInfo& event,
                        InputEventAckState ack_result) override;
   void OnTouchEventAck(const TouchEventWithLatencyInfo& event,
@@ -77,6 +79,7 @@ class MockInputAckHandler : public InputAckHandler {
   blink::WebMouseWheelEvent acked_wheel_event_;
   TouchEventWithLatencyInfo acked_touch_event_;
   blink::WebGestureEvent acked_gesture_event_;
+  blink::WebMouseEvent acked_mouse_event_;
 
   scoped_ptr<GestureEventWithLatencyInfo> gesture_followup_event_;
   scoped_ptr<TouchEventWithLatencyInfo> touch_followup_event_;
