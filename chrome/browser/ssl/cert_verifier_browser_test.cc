4e0f0706e9d973c23d2de6f0f418cad5b6514a23
estark <estark@chromium.org>
2015-08-05 18:15:24 -0700

    Allow browser tests to use a MockCertVerifier
    Introduces a ProfileIOData::SetCertVerifierForTesting() hook, called by
    the CertVerifierBrowserTest class. CertVerifierBrowserTest tests can use
    mock_cert_verifier() to set mock cert verify results.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1227943002
    
    Cr-Commit-Position: refs/heads/master@{#342038}

---
+++ chrome/browser/ssl/cert_verifier_browser_test.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ssl/cert_verifier_browser_test.h"
+
+#include "chrome/browser/profiles/profile_io_data.h"
+#include "net/cert/mock_cert_verifier.h"
+
+CertVerifierBrowserTest::CertVerifierBrowserTest()
+    : InProcessBrowserTest(),
+      mock_cert_verifier_(new net::MockCertVerifier()) {}
+
+CertVerifierBrowserTest::~CertVerifierBrowserTest() {}
+
+void CertVerifierBrowserTest::SetUpInProcessBrowserTestFixture() {
+  ProfileIOData::SetCertVerifierForTesting(mock_cert_verifier_.get());
+}
+
+void CertVerifierBrowserTest::TearDownInProcessBrowserTestFixture() {
+  ProfileIOData::SetCertVerifierForTesting(nullptr);
+}
+
+net::MockCertVerifier* CertVerifierBrowserTest::mock_cert_verifier() {
+  return mock_cert_verifier_.get();
+}
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -404,6 +404,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/ssl/captive_portal_blocking_page_browsertest.cc',
       'browser/ssl/certificate_reporting_test_utils.cc',
       'browser/ssl/certificate_reporting_test_utils.h',
+      'browser/ssl/cert_verifier_browser_test.cc',
+      'browser/ssl/cert_verifier_browser_test.h',
       'browser/ssl/chrome_ssl_host_state_delegate_test.cc',
       'browser/ssl/ssl_browser_tests.cc',
       'browser/ssl/ssl_client_certificate_selector_test.cc',
