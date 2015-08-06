19bcc1000a36a6650d7245c0dcf1b9a7de3d3918
thestig <thestig@chromium.org>
2015-08-06 14:45:27 -0700

    Cleanup nits in ImportantFileWriter and JsonPrefStore.
    Review URL: https://codereview.chromium.org/1265363002
    
    Cr-Commit-Position: refs/heads/master@{#342207}

--- base/files/important_file_writer.h
+++ base/files/important_file_writer.h
@@ -63,9 +63,13 @@ class BASE_EXPORT ImportantFileWriter : public NonThreadSafe {
   // |task_runner| is the SequencedTaskRunner instance where on which we will
   // execute file I/O operations.
   // All non-const methods, ctor and dtor must be called on the same thread.
-  ImportantFileWriter(
-      const FilePath& path,
-      const scoped_refptr<base::SequencedTaskRunner>& task_runner);
+  ImportantFileWriter(const FilePath& path,
+                      const scoped_refptr<SequencedTaskRunner>& task_runner);
+
+  // Same as above, but with a custom commit interval.
+  ImportantFileWriter(const FilePath& path,
+                      const scoped_refptr<SequencedTaskRunner>& task_runner,
+                      TimeDelta interval);
 
   // You have to ensure that there are no pending writes at the moment
   // of destruction.
@@ -78,7 +82,7 @@ class BASE_EXPORT ImportantFileWriter : public NonThreadSafe {
   bool HasPendingWrite() const;
 
   // Save |data| to target filename. Does not block. If there is a pending write
-  // scheduled by ScheduleWrite, it is cancelled.
+  // scheduled by ScheduleWrite(), it is cancelled.
   void WriteNow(scoped_ptr<std::string> data);
 
   // Schedule a save to target filename. Data will be serialized and saved
@@ -95,14 +99,10 @@ class BASE_EXPORT ImportantFileWriter : public NonThreadSafe {
   // Registers |on_next_successful_write| to be called once, on the next
   // successful write event. Only one callback can be set at once.
   void RegisterOnNextSuccessfulWriteCallback(
-      const base::Closure& on_next_successful_write);
+      const Closure& on_next_successful_write);
 
   TimeDelta commit_interval() const {
     return commit_interval_;
-  }
-
-  void set_commit_interval(const TimeDelta& interval) {
-    commit_interval_ = interval;
   }
 
  private:
@@ -114,13 +114,13 @@ class BASE_EXPORT ImportantFileWriter : public NonThreadSafe {
   void ForwardSuccessfulWrite(bool result);
 
   // Invoked once and then reset on the next successful write event.
-  base::Closure on_next_successful_write_;
+  Closure on_next_successful_write_;
 
   // Path being written to.
   const FilePath path_;
 
   // TaskRunner for the thread on which file I/O can be done.
-  const scoped_refptr<base::SequencedTaskRunner> task_runner_;
+  const scoped_refptr<SequencedTaskRunner> task_runner_;
 
   // Timer used to schedule commit after ScheduleWrite.
   OneShotTimer<ImportantFileWriter> timer_;
@@ -129,7 +129,7 @@ class BASE_EXPORT ImportantFileWriter : public NonThreadSafe {
   DataSerializer* serializer_;
 
   // Time delta after which scheduled data will be written to disk.
-  TimeDelta commit_interval_;
+  const TimeDelta commit_interval_;
 
   WeakPtrFactory<ImportantFileWriter> weak_factory_;
 
