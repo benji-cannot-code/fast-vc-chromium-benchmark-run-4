a850cec446fbb072f72bf4ef9cc17014374707fa
sullivan <sullivan@chromium.org>
2015-08-05 17:05:22 -0700

    Chartjson: create new benchmark_metadata field, and move name and description there, and also add type.
    BUG=513840
    
    Review URL: https://codereview.chromium.org/1260553002
    
    Cr-Commit-Position: refs/heads/master@{#342018}

--- tools/telemetry/telemetry/internal/results/chart_json_output_formatter_unittest.py
+++ tools/telemetry/telemetry/internal/results/chart_json_output_formatter_unittest.py
@@ -67,8 +67,11 @@ class ChartJsonTest(unittest.TestCase):
         summary_values)
 
     self.assertEquals(d['format_version'], '0.1')
-    self.assertEquals(d['benchmark_name'], 'benchmark_name')
-    self.assertEquals(d['benchmark_description'], 'benchmark_description')
+    self.assertEquals(d['next_version'], '0.2')
+    self.assertEquals(d['benchmark_metadata']['name'], 'benchmark_name')
+    self.assertEquals(d['benchmark_metadata']['description'],
+                      'benchmark_description')
+    self.assertEquals(d['benchmark_metadata']['type'], 'telemetry_benchmark')
 
   def testAsChartDictNoDescription(self):
     page_specific_values = []
@@ -79,7 +82,7 @@ class ChartJsonTest(unittest.TestCase):
         page_specific_values,
         summary_values)
 
-    self.assertEquals('', d['benchmark_description'])
+    self.assertEquals('', d['benchmark_metadata']['description'])
 
   def testAsChartDictPageSpecificValuesSamePage(self):
     v0 = scalar.ScalarValue(self._story_set[0], 'foo', 'seconds', 3)
