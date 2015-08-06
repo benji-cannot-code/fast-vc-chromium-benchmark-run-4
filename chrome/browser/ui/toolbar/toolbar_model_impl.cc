101c49f9196fdb8cfa2a25f872e020616e9032a9
sdefresne <sdefresne@chromium.org>
2015-08-06 01:58:48 -0700

    Remove useless #include.
    BUG=None
    
    Review URL: https://codereview.chromium.org/1274783002
    
    Cr-Commit-Position: refs/heads/master@{#342081}

--- chrome/browser/ui/toolbar/toolbar_model_impl.cc
+++ chrome/browser/ui/toolbar/toolbar_model_impl.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/prefs/pref_service.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
-#include "chrome/browser/autocomplete/autocomplete_classifier_factory.h"
 #include "chrome/browser/autocomplete/chrome_autocomplete_scheme_classifier.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/search.h"
