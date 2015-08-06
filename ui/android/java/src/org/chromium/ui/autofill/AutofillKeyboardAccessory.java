83f81b4d80bcb4e09c9930edd80d5856a42691f9
rouslan <rouslan@chromium.org>
2015-08-06 11:31:36 -0700

    Bold font for autofill keyboard accessory label on pre-L devices.
    There is no medium font on Android device before L. Use bold font
    instead.
    
    BUG=428087
    
    Review URL: https://codereview.chromium.org/1279633002
    
    Cr-Commit-Position: refs/heads/master@{#342153}

--- ui/android/java/src/org/chromium/ui/autofill/AutofillKeyboardAccessory.java
+++ ui/android/java/src/org/chromium/ui/autofill/AutofillKeyboardAccessory.java
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.ui.autofill;
 
 import android.annotation.SuppressLint;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -95,6 +97,9 @@ public class AutofillKeyboardAccessory extends LinearLayout
 
             if (!TextUtils.isEmpty(suggestion.getLabel())) {
                 label.setText(suggestion.getLabel());
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                    label.setTypeface(Typeface.DEFAULT_BOLD);
+                }
 
                 if (!TextUtils.isEmpty(suggestion.getSublabel())) {
                     TextView sublabel = (TextView) touchTarget.findViewById(
