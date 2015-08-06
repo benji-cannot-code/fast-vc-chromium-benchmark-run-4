a507f786a31b3b360168f426cf9dbd07daddf7d4
sdefresne <sdefresne@chromium.org>
2015-08-06 05:56:40 -0700

    Refactor ClipboardRecentContent singleton creation.
    Move the initialization of the ClipboardRecentContent from the component
    to the embedder since embedder specific information is required
    (only the application scheme currently, but more embedder
    specific information will soon be required).
    
    Add an helper method to create a ClipboadRecentContentIOS instance from
    a C++ file (ClipboardRecentContentIOS is defined in an Objective-C++
    header). The instance returned is fully initialized and can be registered
    as the singleton instance.
    
    BUG=517017
    
    Review URL: https://codereview.chromium.org/1274713003
    
    Cr-Commit-Position: refs/heads/master@{#342102}

---
+++ ios/chrome/browser/open_from_clipboard/create_clipboard_recent_content.mm
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/open_from_clipboard/create_clipboard_recent_content.h"
+
+#import "components/open_from_clipboard/clipboard_recent_content_ios.h"
+#include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
+
+scoped_ptr<ClipboardRecentContent> CreateClipboadRecentContentIOS() {
+  return make_scoped_ptr(new ClipboardRecentContentIOS(
+      ios::GetChromeBrowserProvider()->GetChromeUIScheme()));
+}
