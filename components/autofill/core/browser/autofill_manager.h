8e97df79a2138aa3361a45c4e8b2c5567f898cf6
brettw <brettw@chromium.org>
2015-08-06 12:45:22 -0700

    Add optional logic for checking autofill data quality.
    When the flag --dump-autofill-data is passed, this will write some autofill session data to the file autofill_debug_dump.txt on the user's desktop. This is for initial exploration on understanding session data for credit card saving.
    
    This is not hooked up to about:flags and is not available on mobile (since there's no good way to view the file). People shouldn't be setting this, it's just for initial exploration purposes for developers to manually test on their machines.
    
    The autofill manager used a lot of difficult-to-read iterator loops, which I changed to use range-based for loops for clarity.
    
    NOPRESUBMIT=true
    (due to use of ScopedAllowIO)
    
    Review URL: https://codereview.chromium.org/1272373002
    
    Cr-Commit-Position: refs/heads/master@{#342181}

--- components/autofill/core/browser/autofill_manager.h
+++ components/autofill/core/browser/autofill_manager.h
@@ -31,6 +31,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/wallet/real_pan_wallet_client.h"
 #include "components/autofill/core/common/form_data.h"
 
+// This define protects some debugging code (see DumpAutofillData). This
+// is here to make it easier to delete this code when the test is complete,
+// and to prevent adding the code on mobile where there is no desktop (the
+// debug dump file is written to the desktop) or command-line flags to enable.
+#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
+#define ENABLE_FORM_DEBUG_DUMP
+#endif
+
 class ChromeUIWebViewWebTest;
 class ChromeWKWebViewWebTest;
 
@@ -378,6 +386,11 @@ class AutofillManager : public AutofillDownloadManager::Observer,
   void EmitIsFromAddressBookMetric(int unique_id);
 #endif  // defined(OS_MACOSX) && !defined(OS_IOS)
 
+#ifdef ENABLE_FORM_DEBUG_DUMP
+  // Dumps the cached forms to a file on disk.
+  void DumpAutofillData(bool imported_cc) const;
+#endif
+
   // Provides driver-level context to the shared code of the component. Must
   // outlive this object.
   AutofillDriver* driver_;
@@ -444,6 +457,13 @@ class AutofillManager : public AutofillDownloadManager::Observer,
   // Masked copies of recently unmasked cards, to help avoid double-asking to
   // save the card (in the prompt and in the infobar after submit).
   std::vector<CreditCard> recently_unmasked_cards_;
+
+#ifdef ENABLE_FORM_DEBUG_DUMP
+  // The last few autofilled forms (key/value pairs) submitted, for debugging.
+  // TODO(brettw) this should be removed. See DumpAutofillData.
+  std::vector<std::map<std::string, base::string16>>
+      recently_autofilled_forms_;
+#endif
 
   // Suggestion backend ID to ID mapping. We keep two maps to convert back and
   // forth. These should be used only by BackendIDToInt and IntToBackendID.
