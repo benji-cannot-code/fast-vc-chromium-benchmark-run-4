6d1ac97626e74267d89b9c9c00b7709300d4ead3
reed <reed@google.com>
2015-08-06 13:37:16 -0700

    hook up blink's codecs so skia can access them
    The intent is to register this factory as early as possible (per process).
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1232433003
    
    Cr-Commit-Position: refs/heads/master@{#342191}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -132,6 +132,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/port_util.h"
 #include "skia/ext/event_tracer_impl.h"
 #include "skia/ext/skia_memory_dump_provider.h"
+#include "third_party/WebKit/public/platform/WebImageGenerator.h"
 #include "third_party/WebKit/public/platform/WebString.h"
 #include "third_party/WebKit/public/platform/WebThread.h"
 #include "third_party/WebKit/public/web/WebCache.h"
@@ -1144,6 +1145,10 @@ void RenderThreadImpl::EnsureWebKitInitialized() {
 
   SkGraphics::SetResourceCacheSingleAllocationByteLimit(
       kImageCacheSingleAllocationByteLimit);
+
+  // Hook up blink's codecs so skia can call them
+  SkGraphics::SetImageGeneratorFromEncodedFactory(
+      blink::WebImageGenerator::create);
 
   if (command_line.HasSwitch(switches::kMemoryMetrics)) {
     memory_observer_.reset(new MemoryObserver());
