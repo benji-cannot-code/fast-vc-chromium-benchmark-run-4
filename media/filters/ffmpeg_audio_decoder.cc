b65e250026cefbeab0c80ec081e948fd1c528dd3
dalecurtis <dalecurtis@chromium.org>
2015-08-06 19:59:02 -0700

    Fix incorrect opus seek preroll and flaky pre-skip removal.
    Opus seek preroll was being treated as decoder delay when it really
    just means that for seeking to a time T, we need to decode packets
    starting from time "T - seek_preroll" and throw them away. Instead
    we were discarding time [T, T+seek_preroll] and shifting the
    timestamps backwards to accommodate the deletion.
    
    For pre-skip, we would assume the first packet seen by the decoder
    was the timestamp for which pre-skip should be applied.  However,
    this is incorrect and pre-skip should always be dropped because
    timestamp zero represents the beginning of the pre-skip, which
    causes all future timestamps to be displaced by pre-skip duration.
    
    This patch fixes pre-skip and seek preroll for src= and pre-skip only
    for MSE. ChunkDemuxer needs to be modified to support preroll. src=
    is fixed by expanding the hacks in place for negative ogg timestamps
    to handle negative timestamps from ffmpeg in a more general manner.
    
    This patch also adds test cases for the seeking cases (one of which MSE
    can pass because the test clip has a codec delay > seek preroll).
    Since these tests would otherwise take some time to run, this also
    adds support for clockless hashed pipeline integration tests.
    
    BUG=509894
    TEST=OpusAudioDecoder and OPUS PipelineIntegration tests pass
    w/o expectations modification.  New trimming pipeline tests.
    New FFmpegDemuxerTest to verify audio/video timestamp sync.
    
    Review URL: https://codereview.chromium.org/1260193005
    
    Cr-Commit-Position: refs/heads/master@{#342274}

--- media/filters/ffmpeg_audio_decoder.cc
+++ media/filters/ffmpeg_audio_decoder.cc
@@ -137,10 +137,8 @@ FFmpegAudioDecoder::FFmpegAudioDecoder(
 FFmpegAudioDecoder::~FFmpegAudioDecoder() {
   DCHECK(task_runner_->BelongsToCurrentThread());
 
-  if (state_ != kUninitialized) {
+  if (state_ != kUninitialized)
     ReleaseFFmpegResources();
-    ResetTimestampState();
-  }
 }
 
 std::string FFmpegAudioDecoder::GetDisplayName() const {
@@ -372,8 +370,6 @@ bool FFmpegAudioDecoder::ConfigureDecoder() {
 
   // Success!
   av_frame_.reset(av_frame_alloc());
-  discard_helper_.reset(new AudioDiscardHelper(config_.samples_per_second(),
-                                               config_.codec_delay()));
   av_sample_format_ = codec_context_->sample_fmt;
 
   if (codec_context_->channels !=
@@ -392,6 +388,8 @@ bool FFmpegAudioDecoder::ConfigureDecoder() {
 }
 
 void FFmpegAudioDecoder::ResetTimestampState() {
+  discard_helper_.reset(new AudioDiscardHelper(config_.samples_per_second(),
+                                               config_.codec_delay()));
   discard_helper_->Reset(config_.codec_delay());
 }
 
