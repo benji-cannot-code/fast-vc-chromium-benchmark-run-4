788618687b3710996b7614f696f9a7c28f71c2b5
peter@chromium.org <peter@chromium.org>
2015-08-07 06:04:26 +0000

    Generalize validation of developer input for Web Notifications
    Next to removing a whole bunch of duplicated logic, this CL
    also adds unit tests verifying the developer-data to a
    WebNotificationData conversion.
    
    Additionally, as a follow-up on the following CL, the "auto"
    value will now be propagated as expected for persistent notifications.
    
    https://codereview.chromium.org/1262023003/
    
    BUG=516339
    
    Review URL: https://codereview.chromium.org/1260793007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200150 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/modules/notifications/NotificationData.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NotificationData_h
+#define NotificationData_h
+
+#include "modules/ModulesExport.h"
+#include "public/platform/modules/notifications/WebNotificationData.h"
+#include "wtf/text/WTFString.h"
+
+namespace blink {
+
+class ExceptionState;
+class ExecutionContext;
+class NotificationOptions;
+
+// Creates a WebNotificationData object based on the developer-provided notification data. When the
+// data is deemed invalid, an exception will be thrown to the passed exception state.
+MODULES_EXPORT WebNotificationData createWebNotificationData(ExecutionContext*, const String& title, const NotificationOptions&, ExceptionState&);
+
+} // namespace blink
+
+#endif // NotificationData_h
