c213dfe20bcb75fa6e14143c18e11baf7f691c9f
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-08-07 15:47:50 -0700

    Revert of [Reland][Extensions Page] Send profile changed event when developer mode changes (patchset #2 id:20001 of https://codereview.chromium.org/1280513003/ )
    Reason for revert:
    Still causing breakages.
    
    Original issue's description:
    > [Reland][Extensions Page] Send profile changed event when developer mode changes
    >
    > Developer mode can affect the presence of certain items on the page, like the
    > collect errors checkbox. Send an event notifying of the profile change when this
    > happens.
    >
    > BUG=516583
    > TBR=kalman@chromium.org
    >
    > Committed: https://crrev.com/69b8ba6eb23728f8bd8d5a3b9ba0780772798487
    > Cr-Commit-Position: refs/heads/master@{#342423}
    
    TBR=kalman@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=516583
    
    Review URL: https://codereview.chromium.org/1282613003
    
    Cr-Commit-Position: refs/heads/master@{#342468}

--- chrome/browser/extensions/api/developer_private/developer_private_api.h
+++ chrome/browser/extensions/api/developer_private/developer_private_api.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file.h"
 #include "base/memory/weak_ptr.h"
-#include "base/prefs/pref_change_registrar.h"
 #include "base/scoped_observer.h"
 #include "chrome/browser/extensions/api/commands/command_service.h"
 #include "chrome/browser/extensions/api/developer_private/entry_picker.h"
@@ -122,9 +121,6 @@ class DeveloperPrivateEventRouter : public ExtensionRegistryObserver,
   void ExtensionWarningsChanged(
       const ExtensionIdSet& affected_extensions) override;
 
-  // Handles a profile preferance change.
-  void OnProfilePrefChanged();
-
   // Broadcasts an event to all listeners.
   void BroadcastItemStateChanged(api::developer_private::EventType event_type,
                                  const std::string& id);
@@ -164,8 +160,6 @@ class DeveloperPrivateEventRouter : public ExtensionRegistryObserver,
   // update. In particular, we want to avoid entering a loop, which could happen
   // when, e.g., the Apps Developer Tool throws an error.
   std::set<std::string> extension_ids_;
-
-  PrefChangeRegistrar pref_change_registrar_;
 
   base::WeakPtrFactory<DeveloperPrivateEventRouter> weak_factory_;
 
--- chrome/browser/extensions/api/developer_private/extension_info_generator.cc
+++ chrome/browser/extensions/api/developer_private/extension_info_generator.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/extensions/extension_icon_source.h"
 #include "chrome/common/extensions/command.h"
 #include "chrome/common/extensions/manifest_handlers/app_launch_info.h"
-#include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
 #include "content/public/browser/render_frame_host.h"
 #include "extensions/browser/extension_error.h"
@@ -405,12 +404,9 @@ void ExtensionInfoGenerator::CreateExtensionInfoHelper(
   info->incognito_access.is_active =
       util::IsIncognitoEnabled(extension.id(), browser_context_);
 
-  // Install warnings, but only if unpacked, the error console isn't enabled
-  // (otherwise it shows these), and we're in developer mode (normal users don't
-  // need to see these).
+  // Install warnings (only if unpacked and no error console).
   if (!error_console_enabled &&
-      Manifest::IsUnpackedLocation(extension.location()) &&
-      profile->GetPrefs()->GetBoolean(prefs::kExtensionsUIDeveloperMode)) {
+      Manifest::IsUnpackedLocation(extension.location())) {
     const std::vector<InstallWarning>& install_warnings =
         extension.install_warnings();
     for (const InstallWarning& warning : install_warnings)
