d3caeaa5b38e5fde7c01b06ef8e3d6add4bca2cc
brettw <brettw@chromium.org>
2015-08-06 17:11:28 -0700

    Update SplitString calls to new form
    Uses the new form for most (but not quite all) of the remaining users of the old form.
    
    Changes media mime util codec list parsing to expect no result from the string "," rather than two empty strings. The old SplitString call had a special case where if the input was empty, it would return empty, but if it had one split character, it would return two empty strings as results.
    
    The new one lets you choose but the options are either (1) empty string -> one empty string and "," -> two empty strings, or (2) map both to no results for when you don't want empty results. I'm pretty sure media codec parsing actually wants the latter behavior, so I updated the call to discard empty results and MimeUtilTest.ParseCodecString is updated.
    
    Review URL: https://codereview.chromium.org/1272823003
    
    Cr-Commit-Position: refs/heads/master@{#342238}

--- media/base/mime_util_unittest.cc
+++ media/base/mime_util_unittest.cc
@@ -121,7 +121,7 @@ TEST(MimeUtilTest, ParseCodecString) {
     { "",                           0, { }                    },
     { "\"\"",                       0, { }                    },
     { "\"   \"",                    0, { }                    },
-    { ",",                          2, { "", "" }             },
+    { ",",                          0, { }                    },
   };
 
   for (size_t i = 0; i < arraysize(tests); ++i) {
