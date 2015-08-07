e9d6785960e5384b9863c35b8851184f1c5856a0
sdefresne <sdefresne@chromium.org>
2015-08-07 00:30:24 -0700

    Upstream updated interface for ChromeBrowserState.
    Upstream two methods of ChromeBrowserState that were added downstream
    only: FromWebUIIOS and HasOffTheRecordChromeBrowserState.
    
    Add new dependency in ios_provider_chrome.gyp and add DEPS file.
    
    BUG=517446
    
    Review URL: https://codereview.chromium.org/1278623003
    
    Cr-Commit-Position: refs/heads/master@{#342311}

--- ios/provider/ios_provider_chrome.gyp
+++ ios/provider/ios_provider_chrome.gyp
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'dependencies': [
         '../../base/base.gyp:base',
         '../../components/components.gyp:autofill_core_browser',
+        '../web/ios_web.gyp:ios_web',
+        'ios_provider_web.gyp:ios_provider_web',
       ],
     },
   ],
--- ios/public/provider/chrome/browser/browser_state/chrome_browser_state.cc
+++ ios/public/provider/chrome/browser/browser_state/chrome_browser_state.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/public/provider/chrome/browser/browser_state/chrome_browser_state.h"
 
+#include "ios/public/provider/web/web_ui_ios.h"
+#include "ios/web/public/web_state/web_state.h"
+
 namespace ios {
 
 // static
@@ -12,6 +15,11 @@ ChromeBrowserState* ChromeBrowserState::FromBrowserState(
     web::BrowserState* browser_state) {
   // This is safe; this is the only implementation of BrowserState.
   return static_cast<ChromeBrowserState*>(browser_state);
+}
+
+// static
+ChromeBrowserState* ChromeBrowserState::FromWebUIIOS(web::WebUIIOS* web_ui) {
+  return FromBrowserState(web_ui->GetWebState()->GetBrowserState());
 }
 
 }  // namespace ios
