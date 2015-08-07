8eeb5a411dabc2c9759b851bf158f56cacce8ee6
skobes@chromium.org <skobes@chromium.org>
2015-08-07 02:54:54 +0000

    Clear the scroll anchor when the root layer scrolls.
    Also refactor FrameView to clear the scroll anchor in scrollTo, instead of
    setScrollPosition and setWasScrolledByUser.  (This paves the way to move
    m_wasScrolledByUser into DocumentLoader::InitialScrollState.)
    
    BUG=417782
    
    Review URL: https://codereview.chromium.org/1275663004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200138 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -178,7 +178,7 @@ DEFINE_TRACE(FrameView)
 #if ENABLE(OILPAN)
     visitor->trace(m_frame);
     visitor->trace(m_nodeToDraw);
-    visitor->trace(m_maintainScrollPositionAnchor);
+    visitor->trace(m_scrollAnchor);
     visitor->trace(m_scrollableAreas);
     visitor->trace(m_animatingScrollableAreas);
     visitor->trace(m_autoSizeInfo);
@@ -216,7 +216,7 @@ void FrameView::reset()
     m_visuallyNonEmptyPixelCount = 0;
     m_isVisuallyNonEmpty = false;
     m_firstVisuallyNonEmptyLayoutCallbackPending = true;
-    m_maintainScrollPositionAnchor = nullptr;
+    clearScrollAnchor();
     m_viewportConstrainedObjects.clear();
     m_layoutSubtreeRootList.clear();
 }
@@ -1459,9 +1459,8 @@ bool FrameView::processUrlFragmentHelper(const String& name, UrlFragmentBehavior
 
 void FrameView::maintainScrollPositionAtAnchor(Node* anchorNode)
 {
-    m_maintainScrollPositionAnchor = anchorNode;
-    if (!m_maintainScrollPositionAnchor)
-        return;
+    ASSERT(anchorNode);
+    m_scrollAnchor = anchorNode;
 
     // We need to update the layout before scrolling, otherwise we could
     // really mess things up if an anchor scroll comes at a bad moment.
@@ -1474,10 +1473,13 @@ void FrameView::maintainScrollPositionAtAnchor(Node* anchorNode)
         scrollToAnchor();
 }
 
+void FrameView::clearScrollAnchor()
+{
+    m_scrollAnchor = nullptr;
+}
+
 void FrameView::setScrollPosition(const DoublePoint& scrollPoint, ScrollType scrollType, ScrollBehavior scrollBehavior)
 {
-    m_maintainScrollPositionAnchor = nullptr;
-
     DoublePoint newScrollPosition = clampScrollPosition(scrollPoint);
     if (newScrollPosition == scrollPositionDouble())
         return;
@@ -1704,7 +1706,7 @@ void FrameView::handleLoadCompleted()
     // reduce the size of the frame.
     if (m_autoSizeInfo)
         m_autoSizeInfo->autoSizeIfNeeded();
-    maintainScrollPositionAtAnchor(0);
+    clearScrollAnchor();
 }
 
 void FrameView::clearLayoutSubtreeRoot(const LayoutObject& root)
@@ -1853,7 +1855,7 @@ void FrameView::updateBackgroundRecursively(const Color& backgroundColor, bool t
 
 void FrameView::scrollToAnchor()
 {
-    RefPtrWillBeRawPtr<Node> anchorNode = m_maintainScrollPositionAnchor;
+    RefPtrWillBeRawPtr<Node> anchorNode = m_scrollAnchor;
     if (!anchorNode)
         return;
 
@@ -1884,8 +1886,8 @@ void FrameView::scrollToAnchor()
     if (AXObjectCache* cache = m_frame->document()->existingAXObjectCache())
         cache->handleScrolledToAnchor(anchorNode.get());
 
-    // scrollRectToVisible can call into setScrollPosition(), which resets m_maintainScrollPositionAnchor.
-    m_maintainScrollPositionAnchor = anchorNode;
+    // scrollRectToVisible can call into setScrollPosition(), which resets m_scrollAnchor.
+    m_scrollAnchor = anchorNode;
 }
 
 bool FrameView::updateWidgets()
@@ -2144,6 +2146,7 @@ void FrameView::scrollTo(const DoublePoint& newPosition)
     if (!scrollbarsSuppressed())
         m_pendingScrollDelta += scrollDelta;
 
+    clearScrollAnchor();
     updateLayersAndCompositingAfterScrollIfNeeded();
     scrollPositionChanged();
     frame().loader().client()->didChangeScrollOffset();
@@ -2420,7 +2423,6 @@ bool FrameView::wasScrolledByUser() const
 
 void FrameView::setWasScrolledByUser(bool wasScrolledByUser)
 {
-    m_maintainScrollPositionAnchor = nullptr;
     m_wasScrolledByUser = wasScrolledByUser;
 }
 
--- third_party/WebKit/Source/core/paint/DeprecatedPaintLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/paint/DeprecatedPaintLayerScrollableArea.cpp
@@ -456,8 +456,10 @@ void DeprecatedPaintLayerScrollableArea::setScrollOffset(const DoublePoint& newS
     box().view()->clearHitTestCache();
 
     // Inform the FrameLoader of the new scroll position, so it can be restored when navigating back.
-    if (layer()->isRootLayer())
+    if (layer()->isRootLayer()) {
         frameView->frame().loader().saveScrollState();
+        frameView->clearScrollAnchor();
+    }
 }
 
 IntPoint DeprecatedPaintLayerScrollableArea::scrollPosition() const
