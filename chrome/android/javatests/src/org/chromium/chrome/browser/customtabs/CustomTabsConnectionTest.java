4438909feadf0115556663b3c5eb698ef96742ce
lizeb <lizeb@chromium.org>
2015-08-07 03:19:30 -0700

    customtabs: Add throttling to mayLaunchUrl.
    This adds a simple throttling policy for mayLaunchUrl(). Each
    accepted mayLaunchUrl() request increases the delay before accepting the
    next one. Initial delay is 100ms, and the maximum delay is 10s. Waiting
    at least 2*current_interval resets the interval to 100ms, as well as
    correctly predicting the destination URL.
    
    Note that the tracking is done by UID (~app) instead of session, as a
    misbehaving application can open a large number of sessions.
    
    BUG=514825
    
    Review URL: https://codereview.chromium.org/1264013005
    
    Cr-Commit-Position: refs/heads/master@{#342329}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabsConnectionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabsConnectionTest.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Process;
 import android.support.customtabs.ICustomTabsCallback;
 import android.test.InstrumentationTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
@@ -26,6 +27,7 @@ public class CustomTabsConnectionTest extends InstrumentationTestCase {
         super.setUp();
         Context context = getInstrumentation().getTargetContext().getApplicationContext();
         mCustomTabsConnection = CustomTabsConnection.getInstance((Application) context);
+        mCustomTabsConnection.resetThrottling(Process.myUid());
     }
 
     @Override
@@ -121,7 +123,9 @@ public class CustomTabsConnectionTest extends InstrumentationTestCase {
     @SmallTest
     public void testMultipleMayLaunchUrl() {
         ICustomTabsCallback cb = assertWarmupAndMayLaunchUrl(null, URL, true);
+        mCustomTabsConnection.resetThrottling(Process.myUid());
         assertWarmupAndMayLaunchUrl(cb, URL, true);
+        mCustomTabsConnection.resetThrottling(Process.myUid());
         assertWarmupAndMayLaunchUrl(cb, URL2, true);
     }
 
