dce640c7876f233154c842add224bf540b0a9940
yawano <yawano@chromium.org>
2015-08-06 22:22:40 -0700

    Gallery: resize rename field depending on its content.
    * Minimum width is 160px.
    * Expands as its content becomes longer to the half of window width.
    
    BUG=516468
    TEST=none
    
    Review URL: https://codereview.chromium.org/1265393004
    
    Cr-Commit-Position: refs/heads/master@{#342296}

--- ui/file_manager/gallery/js/gallery.js
+++ ui/file_manager/gallery/js/gallery.js
@@ -84,12 +84,27 @@ function Gallery(volumeManager) {
   this.filenameSpacer_ = queryRequiredElement(this.topToolbar_,
       '.filename-spacer');
 
-  this.filenameEdit_ = queryRequiredElement(this.filenameSpacer_,
-      'paper-input');
+  /**
+   * @private {PaperInput}
+   * @const
+   */
+  this.filenameEdit_ = /** @type {PaperInput} */
+      (queryRequiredElement(this.filenameSpacer_, 'paper-input'));
+
+  this.filenameCanvas_ = document.createElement('canvas');
+  this.filenameCanvasContext_ = this.filenameCanvas_.getContext('2d');
+
+  // Set font style of canvas context to same font style with rename field.
+  var filenameEditComputedStyle = window.getComputedStyle(
+      this.filenameEdit_.inputElement);
+  this.filenameCanvasContext_.font = filenameEditComputedStyle.font;
+
   this.filenameEdit_.addEventListener('blur',
       this.onFilenameEditBlur_.bind(this));
   this.filenameEdit_.addEventListener('focus',
       this.onFilenameFocus_.bind(this));
+  this.filenameEdit_.addEventListener('input',
+      this.resizeRenameField_.bind(this));
   this.filenameEdit_.addEventListener('keydown',
       this.onFilenameEditKeydown_.bind(this));
 
@@ -166,6 +181,8 @@ function Gallery(volumeManager) {
       'externally-unmounted', this.onExternallyUnmountedBound_);
   // The 'pagehide' event is called when the app window is closed.
   window.addEventListener('pagehide', this.onPageHide_.bind(this));
+
+  window.addEventListener('resize', this.resizeRenameField_.bind(this));
 }
 
 /**
@@ -700,6 +717,7 @@ Gallery.prototype.updateSelectionAndState_ = function() {
       this.filenameEdit_.disabled = selectedItem.getLocationInfo().isReadOnly;
       this.filenameEdit_.value =
           ImageUtil.getDisplayNameFromName(this.selectedEntry_.name);
+      this.resizeRenameField_();
       this.shareButton_.hidden = !selectedItem.getLocationInfo().isDriveBased;
     } else {
       if (this.context_.curDirEntry) {
@@ -713,6 +731,7 @@ Gallery.prototype.updateSelectionAndState_ = function() {
       this.filenameEdit_.disabled = true;
       this.filenameEdit_.value =
           strf('GALLERY_ITEMS_SELECTED', numSelectedItems);
+      this.resizeRenameField_();
       this.shareButton_.hidden = true;
     }
   } else {
@@ -720,6 +739,7 @@ Gallery.prototype.updateSelectionAndState_ = function() {
     this.filenameEdit_.disabled = true;
     this.deleteButton_.disabled = true;
     this.filenameEdit_.value = '';
+    this.resizeRenameField_();
     this.shareButton_.hidden = true;
   }
 
@@ -766,6 +786,7 @@ Gallery.prototype.onFilenameEditBlur_ = function(event) {
         return Promise.resolve();
       this.filenameEdit_.value =
           ImageUtil.getDisplayNameFromName(item.getEntry().name);
+      this.resizeRenameField_();
       this.filenameEdit_.focus();
       if (typeof error === 'string')
         this.prompt_.showStringAt('center', error, 5000);
@@ -781,6 +802,32 @@ Gallery.prototype.onFilenameEditBlur_ = function(event) {
 };
 
 /**
+ * Minimum width of rename field.
+ * @const {number}
+ */
+Gallery.MIN_WIDTH_RENAME_FIELD = 160; // px
+
+/**
+ * End padding for rename field.
+ * @const {number}
+ */
+Gallery.END_PADDING_RENAME_FIELD = 20; // px
+
+/**
+ * Resize rename field depending on its content.
+ * @private
+ */
+Gallery.prototype.resizeRenameField_ = function() {
+  var size = this.filenameCanvasContext_.measureText(this.filenameEdit_.value);
+
+  var width = Math.min(Math.max(
+      size.width + Gallery.END_PADDING_RENAME_FIELD,
+      Gallery.MIN_WIDTH_RENAME_FIELD), window.innerWidth / 2);
+
+  this.filenameEdit_.style.width = width + 'px';
+};
+
+/**
  * Keydown event handler on filename edit box
  * @param {!Event} event A keyboard event.
  * @private
@@ -790,6 +837,7 @@ Gallery.prototype.onFilenameEditKeydown_ = function(event) {
   switch (event.keyCode) {
     case 27:  // Escape
       this.filenameEdit_.value = this.filenameEdit_.originalValue;
+      this.resizeRenameField_();
       this.filenameEdit_.blur();
       break;
 
