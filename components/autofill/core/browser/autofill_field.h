0233b2a09f7c04de8c4c249b5fd723528a4ec036
mathp <mathp@chromium.org>
2015-08-07 14:19:44 -0700

    [Autofill] Log the number of edited autofilled fields at form submission.
    BUG=513053
    TEST=AutofillMetricsTest
    
    Review URL: https://codereview.chromium.org/1278843002
    
    Cr-Commit-Position: refs/heads/master@{#342441}

--- components/autofill/core/browser/autofill_field.h
+++ components/autofill/core/browser/autofill_field.h
@@ -38,6 +38,7 @@ class AutofillField : public FormFieldData {
   HtmlFieldMode html_mode() const { return html_mode_; }
   const ServerFieldTypeSet& possible_types() const { return possible_types_; }
   PhonePart phone_part() const { return phone_part_; }
+  bool previously_autofilled() const { return previously_autofilled_; }
 
   // Setters for the detected type and section for this field.
   void set_section(const std::string& section) { section_ = section; }
@@ -47,6 +48,9 @@ class AutofillField : public FormFieldData {
     possible_types_ = possible_types;
   }
   void SetHtmlType(HtmlFieldType type, HtmlFieldMode mode);
+  void set_previously_autofilled(bool previously_autofilled) {
+    previously_autofilled_ = previously_autofilled;
+  }
 
   // This function automatically chooses between server and heuristic autofill
   // type, depending on the data available.
@@ -130,6 +134,9 @@ class AutofillField : public FormFieldData {
   // Used to hold the position of the first digit to be copied as a substring
   // from credit card number.
   size_t credit_card_number_offset_;
+
+  // Whether the field was autofilled then later edited.
+  bool previously_autofilled_;
 
   DISALLOW_COPY_AND_ASSIGN(AutofillField);
 };
