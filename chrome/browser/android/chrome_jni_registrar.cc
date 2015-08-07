1ae4f9b4ec4b7e20b2dd482945c1590f9f0d3b0b
tbansal <tbansal@chromium.org>
2015-08-07 11:40:19 -0700

    Upstream changes for NetworkQualityProvider
    Upstream changes for NetworkQualityProvider so that the
    native code can call the APIs provided by operating system.
    Downstream changes will come later for Chrome on Android.
    
    BUG=472685
    
    Review URL: https://codereview.chromium.org/1235373006
    
    Cr-Commit-Position: refs/heads/master@{#342395}

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/metrics/uma_utils.h"
 #include "chrome/browser/android/metrics/variations_session.h"
 #include "chrome/browser/android/most_visited_sites.h"
+#include "chrome/browser/android/net/network_quality_provider.h"
 #include "chrome/browser/android/new_tab_page_prefs.h"
 #include "chrome/browser/android/offline_pages/offline_page_bridge.h"
 #include "chrome/browser/android/omnibox/answers_image_bridge.h"
@@ -254,6 +255,7 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
     {"MostVisitedSites", MostVisitedSites::Register},
     {"NativeInfoBar", RegisterNativeInfoBar},
     {"NavigationPopup", NavigationPopup::RegisterNavigationPopup},
+    {"NetworkQualityProvider", RegisterNetworkQualityProvider},
     {"NewTabPagePrefs", NewTabPagePrefs::RegisterNewTabPagePrefs},
     {"NotificationUIManager",
      NotificationUIManagerAndroid::RegisterNotificationUIManager},
