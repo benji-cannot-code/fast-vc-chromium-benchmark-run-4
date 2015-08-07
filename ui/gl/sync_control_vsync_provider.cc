580050d542d08ec1a4e3a7ae6116c49fd45a8524
brianderson <brianderson@chromium.org>
2015-08-06 17:32:01 -0700

    gpu: Trace event for GetVSyncParameters
    This will make it obvious in traces if GetVSyncParameters ever
    blocks unexpectedly.
    
    BUG=517024
    
    Review URL: https://codereview.chromium.org/1277813005
    
    Cr-Commit-Position: refs/heads/master@{#342243}

--- ui/gl/sync_control_vsync_provider.cc
+++ ui/gl/sync_control_vsync_provider.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/time/time.h"
+#include "base/trace_event/trace_event.h"
 
 #if defined(OS_LINUX)
 // These constants define a reasonable range for a calculated refresh interval.
@@ -36,6 +37,7 @@ SyncControlVSyncProvider::~SyncControlVSyncProvider() {}
 
 void SyncControlVSyncProvider::GetVSyncParameters(
     const UpdateVSyncCallback& callback) {
+  TRACE_EVENT0("gpu", "SyncControlVSyncProvider::GetVSyncParameters");
 #if defined(OS_LINUX)
   base::TimeTicks timebase;
 
