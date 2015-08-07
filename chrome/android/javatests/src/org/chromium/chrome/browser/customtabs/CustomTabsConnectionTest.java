f633f63499627b00f82ae7eee6d251e32bbf13ca
lizeb <lizeb@chromium.org>
2015-08-07 07:10:40 -0700

    customtabs: Allows CustomTabsConnection to be subclassed.
    This change makes it possible to provide a Service derived from
    CustomTabsConnectionService and make some fields and methods protected
    in CustomTabsConnection.
    
    BUG=504479
    
    Review URL: https://codereview.chromium.org/1261893002
    
    Cr-Commit-Position: refs/heads/master@{#342351}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabsConnectionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabsConnectionTest.java
@@ -41,6 +41,8 @@ public class CustomTabsConnectionTest extends InstrumentationTestCase {
             @Override
             public void onNavigationEvent(int navigationEvent, Bundle extras) {}
             @Override
+            public void extraCallback(String callbackName, Bundle args) {}
+            @Override
             public IBinder asBinder() {
                 return this;
             }
