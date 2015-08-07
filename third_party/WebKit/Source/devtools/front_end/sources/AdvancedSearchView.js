f53cb17b6465548e57bcf08d6848eac6b610b679
pfeldman@chromium.org <pfeldman@chromium.org>
2015-08-07 22:15:58 +0000

    DevTools: do not focus tabs upon all user gestures (regressed in r199533).
    TBR=dgozman
    BUG=515416
    
    Review URL: https://codereview.chromium.org/1271183003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200199 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sources/AdvancedSearchView.js
+++ third_party/WebKit/Source/devtools/front_end/sources/AdvancedSearchView.js
@@ -362,6 +362,7 @@ WebInspector.AdvancedSearchView.ActionDelegate.prototype = {
             WebInspector.inspectorView.setCurrentPanel(WebInspector.SourcesPanel.instance());
             this._searchView._toggle(queryCandidate);
             WebInspector.inspectorView.showCloseableViewInDrawer("sources.search", WebInspector.UIString("Search"), this._searchView);
+            this._searchView.focus();
         }
     }
 }
