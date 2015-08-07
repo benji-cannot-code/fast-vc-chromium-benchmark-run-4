50f0f63defb7ea9bea5c2c2e0c517a63f2679f6c
thakis <thakis@chromium.org>
2015-08-07 09:46:03 -0700

    Revert of GC plugin: consider references equal to raw pointers. (patchset #2 id:20001 of https://codereview.chromium.org/1274403002/ )
    Reason for revert:
    Turned the clang/win tot bots red: http://build.chromium.org/p/chromium.fyi/builders/ClangToTWin/builds/2327/steps/compile/logs/stdio
    
    ..\..\third_party\WebKit\Source\core/css/parser/CSSSupportsParser.h(13,1) :  error: [blink-gc] Class 'CSSSupportsParser' contains invalid fields.
    class CSSSupportsParser {
    ^
    ..\..\third_party\WebKit\Source\core/css/parser/CSSSupportsParser.h(31,5) :  note: [blink-gc] Stack-allocated field 'm_parser' declared here:
        CSSParserImpl& m_parser;
        ^
    
    Original issue's description:
    > GC plugin: consider references equal to raw pointers.
    >
    > Unmanaged raw pointers to managed(GC) classes aren't safe across GCs,
    > hence fields with raw pointer types to such GCed classes are identified
    > and reported as errors or warnings.
    >
    > Extend that checking to also apply to reference pointer types.
    >
    > R=haraken
    > BUG=420515
    >
    > Committed: https://crrev.com/70fe23896f81cd66ba1a205b0336f91c64e3fa4e
    > Cr-Commit-Position: refs/heads/master@{#342320}
    
    TBR=oilpan-reviews@chromium.org,sigbjornf@opera.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=420515
    
    Review URL: https://codereview.chromium.org/1275383002
    
    Cr-Commit-Position: refs/heads/master@{#342366}

--- tools/clang/blink_gc_plugin/tests/raw_ptr_to_gc_managed_class.h
+++ tools/clang/blink_gc_plugin/tests/raw_ptr_to_gc_managed_class.h
@@ -15,11 +15,7 @@ class HeapObject;
 class PartObject {
     DISALLOW_ALLOCATION();
 private:
-    PartObject();
-
     RawPtr<HeapObject> m_obj;
-    HeapObject* m_rawObj;
-    HeapObject& m_refObj;
 };
 
 class HeapObject : public GarbageCollected<HeapObject> {
