2c2c039be69a2d6f8023fef6fa0c8c76226070c2
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-08-07 13:14:18 -0700

    [Reland][Extensions Page] Send profile changed event when developer mode changes
    Developer mode can affect the presence of certain items on the page, like the
    collect errors checkbox. Send an event notifying of the profile change when this
    happens.
    
    BUG=516583
    TBR=kalman@chromium.org
    
    Review URL: https://codereview.chromium.org/1280513003
    
    Cr-Commit-Position: refs/heads/master@{#342423}

--- chrome/browser/resources/extensions/extensions.js
+++ chrome/browser/resources/extensions/extensions.js
@@ -330,7 +330,10 @@ cr.define('extensions', function() {
      */
     updateDevControlsVisibility_: function(animated) {
       var showDevControls = $('toggle-dev-on').checked;
-      $('extension-settings').classList.toggle('dev-mode', showDevControls);
+      var extensionSettings = $('extension-settings');
+      var didChange =
+          showDevControls != extensionSettings.classList.contains('dev-mode');
+      extensionSettings.classList.toggle('dev-mode', showDevControls);
 
       var devControls = $('dev-controls');
       devControls.classList.toggle('animated', animated);
@@ -346,7 +349,8 @@ cr.define('extensions', function() {
         devControls.style.height = !showDevControls ? '' :
             buttons.offsetHeight + 'px';
 
-        document.dispatchEvent(new Event('devControlsVisibilityUpdated'));
+        if (didChange)
+          document.dispatchEvent(new Event('devControlsVisibilityUpdated'));
       }.bind(this));
     },
 
