3d8c92c96cd96b80cbab7bc9c1e8a35d6e3456f5
yurys@chromium.org <yurys@chromium.org>
2015-08-07 00:44:53 +0000

    DevTools: store reference to ScriptState as RefPtr rather than raw pointer in async call tracker
    This is a speculative fix for the crash in blink::ScriptState::removeObserver caused by V8AsyncCallTracker::resetAsyncOperations().
    
    BUG=514785
    
    Review URL: https://codereview.chromium.org/1278523006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200132 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/V8AsyncCallTracker.h
+++ third_party/WebKit/Source/core/inspector/V8AsyncCallTracker.h
@@ -46,7 +46,7 @@ private:
     void willHandleV8AsyncTask(ScriptState*, const String& eventName, int id);
 
     class V8ContextAsyncOperations;
-    WillBeHeapHashMap<ScriptState*, OwnPtrWillBeMember<V8ContextAsyncOperations> > m_contextAsyncOperationMap;
+    WillBeHeapHashMap<RefPtr<ScriptState>, OwnPtrWillBeMember<V8ContextAsyncOperations>> m_contextAsyncOperationMap;
     RawPtrWillBeMember<InspectorDebuggerAgent> m_debuggerAgent;
 };
 
