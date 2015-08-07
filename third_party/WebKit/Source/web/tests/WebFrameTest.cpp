9559b233c8e8089265bde4780aff15ca72c90a1e
sigbjornf@opera.com <sigbjornf@opera.com>
2015-08-07 12:05:49 +0000

    Fix WebRemoteFrame leak in DetachRemoteFrame unit test.
    TBR=haraken
    BUG=none
    
    Review URL: https://codereview.chromium.org/1282443002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200160 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/web/tests/WebFrameTest.cpp
@@ -7600,6 +7600,7 @@ TEST_P(ParameterizedWebFrameTest, DetachRemoteFrame)
     WebRemoteFrame* childFrame = view->mainFrame()->toWebRemoteFrame()->createRemoteChild(WebTreeScopeType::Document, "", WebSandboxFlags::None, &childFrameClient);
     childFrame->detach();
     view->close();
+    childFrame->close();
 }
 
 class TestConsoleMessageWebFrameClient : public FrameTestHelpers::TestWebFrameClient {
