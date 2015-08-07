cdd59ed038e8ff11b99c9515343ccdcde519c69b
kbr <kbr@chromium.org>
2015-08-06 21:32:56 -0700

    Revert of cc: Use worker context for one-copy tile initialization. (patchset #20 id:380001 of https://codereview.chromium.org/1230203007/ )
    Reason for revert:
    Suspected of colliding with another patch and causing Issue 517719.
    
    Original issue's description:
    > cc: Use worker context for one-copy tile initialization.
    >
    > This moves management of staging resources to
    > OneCopyTileTaskWorkerPool class. This makes it possible
    > to use a worker context to issue and detect when copy
    > operations complete.
    >
    > BUG=490295
    > CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    >
    > Committed: https://crrev.com/d5157ac5afec145b85746b42f2d86383483a8222
    > Cr-Commit-Position: refs/heads/master@{#342235}
    
    TBR=piman@chromium.org,primiano@chromium.org,vmiura@chromium.org,vmpstr@chromium.org,reveman@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=490295,517719
    
    Review URL: https://codereview.chromium.org/1273163004
    
    Cr-Commit-Position: refs/heads/master@{#342284}

--- ui/compositor/test/in_process_context_provider.h
+++ ui/compositor/test/in_process_context_provider.h
@@ -50,7 +50,6 @@ class InProcessContextProvider : public cc::ContextProvider {
 
   // cc::ContextProvider:
   bool BindToCurrentThread() override;
-  void DetachFromThread() override;
   Capabilities ContextCapabilities() override;
   gpu::gles2::GLES2Interface* ContextGL() override;
   gpu::ContextSupport* ContextSupport() override;
