0124c25256042169fbf8dea72104bd213d675d4c
brettw <brettw@chromium.org>
2015-08-07 12:55:17 -0700

    Fix some crashes in GN.
    GN would read past the end of the file if the last line was a comment that didn't end in a newline.
    
    It also would get confused if an end-of-file was reached after an assignment or a unary operator.
    
    BUG=478169
    
    Review URL: https://codereview.chromium.org/1275853006
    
    Cr-Commit-Position: refs/heads/master@{#342417}

--- tools/gn/parser_unittest.cc
+++ tools/gn/parser_unittest.cc
@@ -182,6 +182,9 @@ TEST(Parser, UnaryOp) {
   DoExpressionPrintTest("!foo",
       "UNARY(!)\n"
       " IDENTIFIER(foo)\n");
+
+  // No contents for binary operator.
+  DoExpressionErrorTest("a = !", 1, 5);
 }
 
 TEST(Parser, List) {
@@ -216,6 +219,8 @@ TEST(Parser, Assignment) {
                     " BINARY(=)\n"
                     "  IDENTIFIER(a)\n"
                     "  LITERAL(2)\n");
+
+  DoExpressionErrorTest("a = ", 1, 3);
 }
 
 TEST(Parser, Accessor) {
