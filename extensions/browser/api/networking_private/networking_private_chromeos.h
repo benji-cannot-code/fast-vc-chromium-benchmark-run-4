a42b2639647008773c918c49e7cbd0b75a796175
stevenjb <stevenjb@chromium.org>
2015-08-07 11:36:13 -0700

    Allow networkingPrivate to request configure UI
    Currently if networkingPrivate attempts to connect
    to an unconfigured network, the request fails and the
    UI is expected to handle configuraiton.
    
    Until network configuration is fully integrated into the
    new Settings UI, we need Chrome to handle the configuration
    UI.
    
    This CL:
    * Adds HandleConnectFailed to NetworkingPrivateDelegate::UIDelegate
    * Suppresses a redundant notification when a connect to an
      unconfigured network fails.
    Also:
    * Updates the logging in the notification code
    * Handles "expected" errors in network_summary.js to avoid "unhandled lastError" messages when requesting a connect to an unconfigured network.
    
    BUG=515987
    
    Review URL: https://codereview.chromium.org/1272003002
    
    Cr-Commit-Position: refs/heads/master@{#342391}

--- extensions/browser/api/networking_private/networking_private_chromeos.h
+++ extensions/browser/api/networking_private/networking_private_chromeos.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EXTENSIONS_BROWSER_API_NETWORKING_PRIVATE_NETWORKING_PRIVATE_CHROMEOS_H_
 #define EXTENSIONS_BROWSER_API_NETWORKING_PRIVATE_NETWORKING_PRIVATE_CHROMEOS_H_
 
+#include "base/memory/weak_ptr.h"
 #include "extensions/browser/api/networking_private/networking_private_delegate.h"
 
 namespace context {
@@ -89,7 +90,14 @@ class NetworkingPrivateChromeOS : public NetworkingPrivateDelegate {
   bool RequestScan() override;
 
  private:
+  void ConnectFailureCallback(const std::string& guid,
+                              const VoidCallback& success_callback,
+                              const FailureCallback& failure_callback,
+                              const std::string& error_name,
+                              scoped_ptr<base::DictionaryValue> error_data);
+
   content::BrowserContext* browser_context_;
+  base::WeakPtrFactory<NetworkingPrivateChromeOS> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(NetworkingPrivateChromeOS);
 };
