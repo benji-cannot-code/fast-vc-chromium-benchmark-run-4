749fbdd1e841cb9d01c0394e0ad7e6d542fb0753
dzhioev <dzhioev@chromium.org>
2015-08-06 18:52:07 -0700

    [cleanup] Removed iframe version of the enrollment screen.
    BUG=470893
    
    Review URL: https://codereview.chromium.org/1267223003
    
    Cr-Commit-Position: refs/heads/master@{#342262}

--- chrome/browser/ui/webui/chromeos/login/enrollment_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/enrollment_screen_handler.h
@@ -10,13 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/memory/scoped_vector.h"
 #include "chrome/browser/chromeos/login/enrollment/enrollment_screen_actor.h"
 #include "chrome/browser/chromeos/login/enrollment/enterprise_enrollment_helper.h"
 #include "chrome/browser/chromeos/login/screens/network_error_model.h"
 #include "chrome/browser/chromeos/login/ui/webui_login_view.h"
 #include "chrome/browser/chromeos/policy/enrollment_config.h"
-#include "chrome/browser/extensions/signin/scoped_gaia_auth_extension.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_screen_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/network_state_informer.h"
 #include "net/base/net_errors.h"
@@ -134,9 +132,6 @@ class EnrollmentScreenHandler
   NetworkErrorModel* network_error_model_;
 
   scoped_ptr<ErrorScreensHistogramHelper> histogram_helper_;
-
-  // GAIA extension loader.
-  scoped_ptr<ScopedGaiaAuthExtension> auth_extension_;
 
   // Help application used for help dialogs.
   scoped_refptr<HelpAppLauncher> help_app_;
