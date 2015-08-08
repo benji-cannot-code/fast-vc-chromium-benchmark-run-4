fd9e15e135862bf22131993daffbc289f967f7d4
kkimlabs <kkimlabs@chromium.org>
2015-08-08 11:50:19 -0700

    [Android] Add progress bar linear animation option.
    For page loading time and battery efficiency, it's good to produce fewer
    frames. Compared to other smooth animations we already have, linear
    animation looks OK with fewer frames as it doesn't spend frames on
    smaller increments.
    
    BUG=352839
    
    Review URL: https://codereview.chromium.org/1279873003
    
    Cr-Commit-Position: refs/heads/master@{#342536}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/ProgressAnimationLinear.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.widget;
+
+/**
+ * Progress bar animation logic that has linear speed.
+ */
+class ProgressAnimationLinear implements ToolbarProgressBar.AnimationLogic {
+    // The speed unit is progress per second where 0 <= progress <= 1.
+    private static final float NORMAL_SPEED = 0.4f;
+    private static final float FINISHING_SPEED = 2.0f;
+
+    private float mProgress;
+
+    @Override
+    public void reset() {
+        mProgress = 0.0f;
+    }
+
+    @Override
+    public float updateProgress(float targetProgress, float frameTimeSec, int resolution) {
+        assert mProgress <= targetProgress;
+
+        mProgress = Math.min(targetProgress, mProgress
+                + frameTimeSec * (targetProgress == 1.0f ? FINISHING_SPEED : NORMAL_SPEED));
+        return mProgress;
+    }
+}
