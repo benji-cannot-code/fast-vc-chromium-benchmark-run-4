1b02f07193b53d79013d13e1b819e4f7ad904b1b
thakis <thakis@chromium.org>
2015-08-08 15:45:57 -0700

    Revert of Roll thirdparty/class-dump to fix build for the latest Xcode (patchset #2 id:20001 of https://codereview.chromium.org/1270383005/ )
    Reason for revert:
    Broke two main waterfall bots:
    http://build.chromium.org/p/chromium.mac/builders/iOS_Simulator_%28dbg%29/builds/29951
    http://build.chromium.org/p/chromium.mac/builders/iOS_Device/builds/15885
    
    ../../../third_party/class-dump/src/Source/CDLCEncryptionInfo.m:12:39:error: field has incomplete type 'struct encryption_info_command_64'
        struct encryption_info_command_64 _encryptionInfoCommand;
                                          ^
    ../../../third_party/class-dump/src/Source/CDLCEncryptionInfo.m:12:12: note: forward declaration of 'struct encryption_info_command_64'
        struct encryption_info_command_64 _encryptionInfoCommand;
               ^
    ../../../third_party/class-dump/src/Source/CDLCEncryptionInfo.m:24:43:error: use of undeclared identifier 'LC_ENCRYPTION_INFO_64'
            if (_encryptionInfoCommand.cmd == LC_ENCRYPTION_INFO_64) {
                                              ^
    
    ../../../third_party/class-dump/src/Source/CDLCSegment.m:278:54:error: use of undeclared identifier 'kCCAlgorithmAES'; did you mean 'kCCAlgorithmDES'?
                    status = CCCryptorCreate(kCCDecrypt, kCCAlgorithmAES, 0, keyData,      32, NULL, &cryptor1);
                                                         ^~~~~~~~~~~~~~~
                                                         kCCAlgorithmDES
    /Applications/Xcode6.3.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk/usr/include/CommonCrypto/CommonCryptor.h:151:5: note: 'kCCAlgorithmDES' declared here
        kCCAlgorithmDES,
        ^
    ../../../third_party/class-dump/src/Source/CDLCSegment.m:281:54:error: use of undeclared identifier 'kCCAlgorithmAES'; did you mean 'kCCAlgorithmDES'?
                    status = CCCryptorCreate(kCCDecrypt, kCCAlgorithmAES, 0, keyData + 32, 32, NULL, &cryptor2);
                                                         ^~~~~~~~~~~~~~~
                                                         kCCAlgorithmDES
    /Applications/Xcode6.3.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk/usr/include/CommonCrypto/CommonCryptor.h:151:5: note: 'kCCAlgorithmDES' declared here
        kCCAlgorithmDES,
        ^
    
    Original issue's description:
    > Roll thirdparty/class-dump to fix build for the latest Xcode
    >
    > BUG=517930
    >
    > Committed: https://crrev.com/60fc2cd20f1fc6371d04dcfc16d838508896c4a1
    > Cr-Commit-Position: refs/heads/master@{#342541}
    
    TBR=rohitrao@chromium.org,justincohen@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=517930
    
    Review URL: https://codereview.chromium.org/1273103003
    
    Cr-Commit-Position: refs/heads/master@{#342543}

--- testing/iossim/iossim.mm
+++ testing/iossim/iossim.mm
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @class DVTSimulatorApplication;
 @class SimDeviceSet;
 @class SimDeviceType;
-@class SimProfilesPathMonitor;
 @class SimRuntime;
 @class SimServiceConnectionManager;
 #import "CoreSimulator.h"
