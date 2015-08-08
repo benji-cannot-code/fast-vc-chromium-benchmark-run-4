1593d61bf954ae67b52004d0d8c40f956fcf37d7
thakis@chromium.org <thakis@chromium.org>
2015-08-08 19:50:31 +0000

    clang/win: Try to fix 64-bit build after https://codereview.chromium.org/1261953003.
    BUG=505316
    TBR=cevans
    
    Review URL: https://codereview.chromium.org/1273693004.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200223 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/PartitionAllocTest.cpp
+++ third_party/WebKit/Source/wtf/PartitionAllocTest.cpp
@@ -86,6 +86,7 @@ void TestShutdown()
     EXPECT_TRUE(genericAllocator.shutdown());
 }
 
+#if !CPU(64BIT) || OS(POSIX)
 bool SetAddressSpaceLimit()
 {
 #if !CPU(64BIT)
@@ -125,6 +126,7 @@ bool ClearAddressSpaceLimit()
     return false;
 #endif
 }
+#endif
 
 PartitionPage* GetFullPage(size_t size)
 {
