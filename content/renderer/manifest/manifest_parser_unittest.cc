b4448a1288f946fb6cb1fefb8c8f003350d14074
lalitm <lalitm@google.com>
2015-08-09 06:50:51 -0700

    content: perform theme color conversion to signed int earlier
    * it is easier and clearer to do the conversion explicitly in C++ rather than a hidden implicit conversion in Java.
    
    BUG=510402
    
    Review URL: https://codereview.chromium.org/1246953002
    
    Cr-Commit-Position: refs/heads/master@{#342559}

--- content/renderer/manifest/manifest_parser_unittest.cc
+++ content/renderer/manifest/manifest_parser_unittest.cc
@@ -11,6 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+namespace {
+
+uint32_t ExtractColor(int64_t color) {
+  return reinterpret_cast<uint32_t&>(color);
+}
+
+} // anonymous namespace
+
 class ManifestParserTest : public testing::Test  {
  protected:
   ManifestParserTest() {}
@@ -989,7 +997,7 @@ TEST_F(ManifestParserTest, ThemeColorParserRules) {
   // Smoke test.
   {
     Manifest manifest = ParseManifest("{ \"theme_color\": \"#FF0000\" }");
-    EXPECT_EQ(manifest.theme_color, 0xFFFF0000);
+    EXPECT_EQ(ExtractColor(manifest.theme_color), 0xFFFF0000);
     EXPECT_FALSE(manifest.IsEmpty());
     EXPECT_EQ(0u, GetErrorCount());
   }
@@ -997,7 +1005,7 @@ TEST_F(ManifestParserTest, ThemeColorParserRules) {
   // Trim whitespaces.
   {
     Manifest manifest = ParseManifest("{ \"theme_color\": \"  blue   \" }");
-    EXPECT_EQ(manifest.theme_color, 0xFF0000FF);
+    EXPECT_EQ(ExtractColor(manifest.theme_color), 0xFF0000FF);
     EXPECT_EQ(0u, GetErrorCount());
   }
 
@@ -1105,35 +1113,35 @@ TEST_F(ManifestParserTest, ThemeColorParserRules) {
   // Accept CSS color keyword format.
   {
     Manifest manifest = ParseManifest("{ \"theme_color\": \"blue\" }");
-    EXPECT_EQ(manifest.theme_color, 0xFF0000FF);
+    EXPECT_EQ(ExtractColor(manifest.theme_color), 0xFF0000FF);
     EXPECT_EQ(0u, GetErrorCount());
   }
 
   // Accept CSS color keyword format.
   {
     Manifest manifest = ParseManifest("{ \"theme_color\": \"chartreuse\" }");
-    EXPECT_EQ(manifest.theme_color, 0xFF7FFF00);
+    EXPECT_EQ(ExtractColor(manifest.theme_color), 0xFF7FFF00);
     EXPECT_EQ(0u, GetErrorCount());
   }
 
   // Accept CSS RGB format.
   {
     Manifest manifest = ParseManifest("{ \"theme_color\": \"#FFF\" }");
-    EXPECT_EQ(manifest.theme_color, 0xFFFFFFFF);
+    EXPECT_EQ(ExtractColor(manifest.theme_color), 0xFFFFFFFF);
     EXPECT_EQ(0u, GetErrorCount());
   }
 
   // Accept CSS RGB format.
   {
     Manifest manifest = ParseManifest("{ \"theme_color\": \"#ABC\" }");
-    EXPECT_EQ(manifest.theme_color, 0xFFAABBCC);
+    EXPECT_EQ(ExtractColor(manifest.theme_color), 0xFFAABBCC);
     EXPECT_EQ(0u, GetErrorCount());
   }
 
   // Accept CSS RRGGBB format.
   {
     Manifest manifest = ParseManifest("{ \"theme_color\": \"#FF0000\" }");
-    EXPECT_EQ(manifest.theme_color, 0xFFFF0000);
+    EXPECT_EQ(ExtractColor(manifest.theme_color), 0xFFFF0000);
     EXPECT_EQ(0u, GetErrorCount());
   }
 }
