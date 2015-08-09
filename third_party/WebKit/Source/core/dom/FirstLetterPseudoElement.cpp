12c87939f08fd9b04964c196b6b6a958c886edb8
shivamidow@gmail.com <shivamidow@gmail.com>
2015-08-09 06:39:05 +0000

    Elements whose contents start with an astral Unicode symbol disappear when CSS `::first-letter` is applied to them.
    This CL is inspired by http://trac.webkit.org/changeset/172513
    written by Myles C. Maxfield.
    The previous code assumed that all "characters" are exactly 1 16-bit code unit wide.
    Instead, use numCharactersInGraphemeClusters().
    
    For blink, we add countCharactersAndGraphemesInCluster to bring a similar
    functionality to blink. This api is based on countGraphemesInCluster and improved
    to figure out the number of characters and grapheme at the same time in the given clusters.
    
    BUG=401945
    
    Review URL: https://codereview.chromium.org/1133853006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200227 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/FirstLetterPseudoElement.cpp
+++ third_party/WebKit/Source/core/dom/FirstLetterPseudoElement.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/LayoutObjectInlines.h"
 #include "core/layout/LayoutText.h"
 #include "core/layout/LayoutTextFragment.h"
+#include "platform/text/TextBreakIterator.h"
 #include "wtf/TemporaryChange.h"
 #include "wtf/text/WTFString.h"
 #include "wtf/text/icu/UnicodeIcu.h"
@@ -78,7 +79,10 @@ unsigned FirstLetterPseudoElement::firstLetterLength(const String& text)
         return 0;
 
     // Account the next character for first letter.
-    length++;
+    const String substring = text.substring(length);
+    const unsigned substringLength = substring.length();
+    const UChar* normalizedBuffer = substring.is8Bit() ? String::make16BitFrom8BitSource(substring.characters8(), substringLength).characters16() : substring.characters16();
+    length += countCharactersAndGraphemesInCluster(normalizedBuffer, substringLength, 0, 1).characters;
 
     // Keep looking for allowed punctuation for the :first-letter.
     for (; length < textLength; ++length) {
