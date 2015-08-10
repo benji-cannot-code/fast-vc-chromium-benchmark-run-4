baab7e785cb54fe0417d3df2ec61ad5b212433b5
bnc <bnc@chromium.org>
2015-08-10 08:46:05 -0700

    Fix some HPACK comments, update links to RFC.
    This CL lands server change 99395244 by rjshade.
    
    BUG=345769
    
    Review URL: https://codereview.chromium.org/1265413003
    
    Cr-Commit-Position: refs/heads/master@{#342625}

--- net/spdy/hpack/hpack_encoder.h
+++ net/spdy/hpack/hpack_encoder.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/spdy/spdy_protocol.h"
 
 // An HpackEncoder encodes header sets as outlined in
-// http://tools.ietf.org/html/draft-ietf-httpbis-header-compression-08
+// http://tools.ietf.org/html/rfc7541.
 
 namespace net {
 
