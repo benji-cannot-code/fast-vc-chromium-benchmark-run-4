f72476883a33e21900ce9468b0937db2caa2075b
ekaramad <ekaramad@google.com>
2015-08-10 12:25:52 -0700

    Implements "chrome://appcache-internals" based on WebUI API
    Redid the "chrome://appcache-internals" page so that the new implementation is:
     1- Based on "WebUIController" and similar to "chrome://indexed-db-internals".
     2- Looks into "all" storage paritions when fetching application cache data.
     This was previously reported as a bug since the application cache for <webview>
     was not being shown on "chrome://appcache-internals".
    
    BUG=343683
    
    Review URL: https://codereview.chromium.org/1191383004
    
    Cr-Commit-Position: refs/heads/master@{#342671}

--- content/browser/webui/content_web_ui_controller_factory.cc
+++ content/browser/webui/content_web_ui_controller_factory.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/webui/content_web_ui_controller_factory.h"
 
 #include "content/browser/accessibility/accessibility_ui.h"
+#include "content/browser/appcache/appcache_internals_ui.h"
 #include "content/browser/gpu/gpu_internals_ui.h"
 #include "content/browser/indexed_db/indexed_db_internals_ui.h"
 #include "content/browser/media/media_internals_ui.h"
@@ -35,7 +36,8 @@ WebUI::TypeID ContentWebUIControllerFactory::GetWebUIType(
       url.host() == kChromeUIIndexedDBInternalsHost ||
       url.host() == kChromeUIMediaInternalsHost ||
       url.host() == kChromeUIServiceWorkerInternalsHost ||
-      url.host() == kChromeUIAccessibilityHost) {
+      url.host() == kChromeUIAccessibilityHost ||
+      url.host() == kChromeUIAppCacheInternalsHost) {
     return const_cast<ContentWebUIControllerFactory*>(this);
   }
   return WebUI::kNoWebUI;
@@ -56,6 +58,8 @@ WebUIController* ContentWebUIControllerFactory::CreateWebUIControllerForURL(
   if (!url.SchemeIs(kChromeUIScheme))
     return nullptr;
 
+  if (url.host() == kChromeUIAppCacheInternalsHost)
+    return new AppCacheInternalsUI(web_ui);
   if (url.host() == kChromeUIGpuHost)
     return new GpuInternalsUI(web_ui);
   if (url.host() == kChromeUIIndexedDBInternalsHost)
