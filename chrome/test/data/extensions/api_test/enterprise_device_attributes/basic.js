2449d4514bbe3efc626657fafc812a447741e8a1
pbond <pbond@chromium.org>
2015-08-10 02:20:46 -0700

    Added browser tests to chrome.enterprise.deviceAttributes API
    Modified the existing tests to use an actual extension.
    This allowed adding a new test to ensure that only extensions pre-installed by policy can use the new API.
    
    BUG=502330
    
    Review URL: https://codereview.chromium.org/1281893002
    
    Cr-Commit-Position: refs/heads/master@{#342594}

---
+++ chrome/test/data/extensions/api_test/enterprise_device_attributes/basic.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Must be packed to ../enterprise_device_attributes.crx using the private key
+// ../enterprise_device_attributes.pem .
+
+// The expected directory device id, that is passed by caller.
+var expected_value = location.search.substring(1);
+
+var assertEq = chrome.test.assertEq;
+var assertTrue = chrome.test.assertTrue;
+var callbackPass = chrome.test.callbackPass
+var succeed = chrome.test.succeed;
+
+assertTrue(chrome.enterprise.deviceAttributes.getDirectoryDeviceId(
+    callbackPass(function(device_id) {
+      assertEq(expected_value, device_id);
+    })));
+
+succeed();
