8642d27e983de1677df9e5ebe99bdaf2ddc790a6
mstensho@opera.com <mstensho@opera.com>
2015-08-10 22:50:52 +0000

    Prepare height calculation methods in LayoutFlowThread for nested multicol.
    Rename pageLogicalHeight() in LayoutMultiColumnSet to
    pageLogicalHeightForOffset(), so that it matches the naming of
    pageRemainingLogicalHeightForOffset().
    
    Move the innards to LayoutMultiColumnSet. And some 0 -> LayoutUnit() cleanup.
    
    Had to adjust some call sites because of this and introduce
    isPageLogicalHeightKnown().
    
    No behavioral changes intended for now.
    
    R=jchaffraix@chromium.org,leviw@chromium.org
    BUG=447718
    
    Review URL: https://codereview.chromium.org/1282353002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200272 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutMultiColumnFlowThread.cpp
+++ third_party/WebKit/Source/core/layout/LayoutMultiColumnFlowThread.cpp
@@ -920,7 +920,7 @@ bool LayoutMultiColumnFlowThread::addForcedColumnBreak(LayoutUnit offset, Layout
 bool LayoutMultiColumnFlowThread::isPageLogicalHeightKnown() const
 {
     if (LayoutMultiColumnSet* columnSet = lastMultiColumnSet())
-        return columnSet->pageLogicalHeight();
+        return columnSet->isPageLogicalHeightKnown();
     return false;
 }
 
