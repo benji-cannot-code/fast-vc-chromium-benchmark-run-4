5b3d52a53156c0dc91c10e445a0b01cbf34ce22e
dcheng@chromium.org <dcheng@chromium.org>
2015-08-10 21:27:24 +0000

    Add a unit test hook into WebURLLoaderClient callbacks.
    Once the Chrome-side patch is landed too, this allows unit tests
    fine-grained control over how and when callbacks for receiving data are
    invoked.
    
    BUG=515876
    
    Review URL: https://codereview.chromium.org/1272293006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200266 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebUnitTestSupport.h
+++ third_party/WebKit/public/platform/WebUnitTestSupport.h
@@ -37,8 +37,17 @@ namespace blink {
 
 class WebLayerTreeView;
 class WebURL;
+class WebURLLoader;
 class WebURLResponse;
+class WebURLLoaderClient;
 struct WebURLError;
+
+class WebURLLoaderTestDelegate {
+public:
+    ~WebURLLoaderTestDelegate() { }
+
+    virtual void didReceiveData(WebURLLoaderClient* originalClient, WebURLLoader*, const char* data, int dataLength, int encodedDataLength) { }
+};
 
 class WebUnitTestSupport {
 public:
@@ -54,6 +63,9 @@ public:
     // Causes all pending asynchronous requests to be served. When this method
     // returns all the pending requests have been processed.
     virtual void serveAsynchronousMockedRequests() { }
+
+    // Set a delegate that allows callbacks for all WebURLLoaderClients to be intercepted.
+    virtual void setLoaderDelegate(WebURLLoaderTestDelegate*) {}
 
     // Returns the root directory of the WebKit code.
     virtual WebString webKitRootDir() { return WebString(); }
