1139c0f825dbe87edf6ea3cac529da9be9679ae7
timav <timav@chromium.org>
2015-08-10 11:51:50 -0700

    Disable some unittests for MediaCodecPlayer temporarily
    Disable till we figure out the reason of failure.
    
    BUG=518900
    
    Review URL: https://codereview.chromium.org/1275323004
    
    Cr-Commit-Position: refs/heads/master@{#342655}

--- media/base/android/media_codec_player_unittest.cc
+++ media/base/android/media_codec_player_unittest.cc
@@ -640,7 +640,8 @@ TEST_F(MediaCodecPlayerTest, VideoPlayTillCompletion) {
   EXPECT_LE(duration, manager_.pts_stat_.max());
 }
 
-TEST_F(MediaCodecPlayerTest, AudioSeekAfterStop) {
+// http://crbug.com/518900
+TEST_F(MediaCodecPlayerTest, DISABLED_AudioSeekAfterStop) {
   SKIP_TEST_IF_MEDIA_CODEC_BRIDGE_IS_NOT_AVAILABLE();
 
   // Play for 300 ms, then Pause, then Seek to beginning. The playback should
@@ -772,7 +773,8 @@ TEST_F(MediaCodecPlayerTest, AudioSeekThenPlayThenConfig) {
       &MockMediaPlayerManager::IsSeekCompleted, base::Unretained(&manager_))));
 }
 
-TEST_F(MediaCodecPlayerTest, AudioSeekWhilePlaying) {
+// http://crbug.com/518900
+TEST_F(MediaCodecPlayerTest, DISABLED_AudioSeekWhilePlaying) {
   SKIP_TEST_IF_MEDIA_CODEC_BRIDGE_IS_NOT_AVAILABLE();
 
   // Play for 300 ms, then issue several Seek commands in the row.
@@ -900,7 +902,8 @@ TEST_F(MediaCodecPlayerTest, VideoRemoveAndSetSurface) {
   EXPECT_LE(max_pts_before_removal, manager_.pts_stat_.max());
 }
 
-TEST_F(MediaCodecPlayerTest, VideoReleaseAndStart) {
+// http://crbug.com/518900
+TEST_F(MediaCodecPlayerTest, DISABLED_VideoReleaseAndStart) {
   SKIP_TEST_IF_MEDIA_CODEC_BRIDGE_IS_NOT_AVAILABLE();
 
   base::TimeDelta duration = base::TimeDelta::FromMilliseconds(1000);
