28ea72b6785614a9b9fbe0fd2d1146b836a34b68
pfeldman@chromium.org <pfeldman@chromium.org>
2015-08-10 20:10:21 +0000

    DevTools: polish experimental elements toolbar. change the order of the buttons, bump up the button width, fix minor glitches.
    BUG=515763
    
    Review URL: https://codereview.chromium.org/1272403002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200263 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
@@ -47,7 +47,7 @@ WebInspector.StylesSidebarPane = function(requestShowCallback)
     filterContainerElement.appendChild(this._filterInput);
 
     var toolbar = new WebInspector.ExtensibleToolbar("styles-sidebarpane-toolbar", hbox);
-    toolbar.element.classList.add("styles-pane-toolbar");
+    toolbar.element.classList.add("styles-pane-toolbar", "toolbar-gray-toggled");
     this._currentToolbarPane = null;
 
     this._requestShowCallback = requestShowCallback;
