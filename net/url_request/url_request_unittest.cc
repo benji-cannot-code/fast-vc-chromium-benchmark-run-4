76c435539c621ccb14f3c0ad50547c63dfc72592
tbansal <tbansal@chromium.org>
2015-08-11 15:06:08 -0700

    Cleanup NQE
    Make NetworkQuality private. Remove NetworkQuality from all
    public APIs since we decided to have a separate Getter for
    each metric.
    
    Future CL will template the Observations/Estimates.
    
    BUG=502415,519637
    
    Committed: https://crrev.com/402963c809947a9a53983cadfcb91b63e3ac87e7
    Cr-Commit-Position: refs/heads/master@{#342851}
    
    Review URL: https://codereview.chromium.org/1277483004
    
    Cr-Commit-Position: refs/heads/master@{#342913}

--- net/url_request/url_request_unittest.cc
+++ net/url_request/url_request_unittest.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/base/net_module.h"
 #include "net/base/net_util.h"
-#include "net/base/network_quality.h"
 #include "net/base/network_quality_estimator.h"
 #include "net/base/request_priority.h"
 #include "net/base/test_data_directory.h"
@@ -4116,11 +4115,13 @@ TEST_F(URLRequestTestHTTP, NetworkQualityEstimator) {
 
   base::RunLoop().Run();
 
-  NetworkQuality network_quality =
-      context.network_quality_estimator()->GetPeakEstimate();
-  EXPECT_GE(network_quality.rtt(), base::TimeDelta());
-  EXPECT_LT(network_quality.rtt(), base::TimeDelta::Max());
-  EXPECT_GT(network_quality.downstream_throughput_kbps(), 0);
+  base::TimeDelta rtt;
+  int32_t kbps;
+  EXPECT_TRUE(estimator.GetRTTEstimate(&rtt));
+  EXPECT_TRUE(estimator.GetDownlinkThroughputKbpsEstimate(&kbps));
+  EXPECT_GE(rtt, base::TimeDelta());
+  EXPECT_LT(rtt, base::TimeDelta::Max());
+  EXPECT_GT(kbps, 0);
 
   // Verify that histograms are not populated. They should populate only when
   // there is a change in ConnectionType.
