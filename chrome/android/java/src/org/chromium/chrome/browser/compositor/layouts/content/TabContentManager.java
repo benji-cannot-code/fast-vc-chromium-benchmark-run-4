d2465f4a36c13084760bf5ecd346d6a6f0e347a1
dfalcantara <dfalcantara@chromium.org>
2015-08-11 16:26:30 -0700

    Attempt to fix migration again
    Hold onto the Callbacks in a static list.  Add logs to see the order
    in which things are being finalized.
    
    BUG=513130
    
    Review URL: https://codereview.chromium.org/1291523002
    
    Cr-Commit-Position: refs/heads/master@{#342930}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/content/TabContentManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/content/TabContentManager.java
@@ -12,6 +12,7 @@ import android.util.SparseArray;
 import android.view.View;
 
 import org.chromium.base.CommandLine;
+import org.chromium.base.Log;
 import org.chromium.base.PathUtils;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
@@ -33,6 +34,8 @@ import java.util.List;
  */
 @JNINamespace("chrome::android")
 public class TabContentManager {
+    private static final String TAG = "cr.content";
+
     private final Context mContext;
     private final float mThumbnailScale;
     private final int mFullResThumbnailsMaxSize;
@@ -84,6 +87,12 @@ public class TabContentManager {
             val = count;
         }
         return val;
+    }
+
+    protected void finalize() throws Throwable {
+        // TODO(dfalcantara): Remove this log.  crbug.com/513130
+        Log.w(TAG, "Finalizing TabContentManager: " + this);
+        super.finalize();
     }
 
     /**
