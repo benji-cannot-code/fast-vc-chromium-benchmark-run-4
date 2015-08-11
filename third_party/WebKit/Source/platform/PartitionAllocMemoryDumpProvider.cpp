ab918c217f391bceda9ef476c976dc157b88aafe
ssid@chromium.org <ssid@chromium.org>
2015-08-11 10:34:50 +0000

    [tracing] Add committed pages size to PartitionAlloc memory dump
    Adds the total size of committed pages from each partition to the
    memory dumps, for chrome tracing.
    
    BUG=499731
    
    Review URL: https://codereview.chromium.org/1285593004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200312 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/PartitionAllocMemoryDumpProvider.cpp
+++ third_party/WebKit/Source/platform/PartitionAllocMemoryDumpProvider.cpp
@@ -47,6 +47,7 @@ void PartitionStatsDumperImpl::partitionDumpTotals(const char* partitionName, co
     String dumpName = getPartitionDumpName(partitionName);
     WebMemoryAllocatorDump* allocatorDump = m_memoryDump->createMemoryAllocatorDump(dumpName);
     allocatorDump->AddScalar("size", "bytes", memoryStats->totalMmappedBytes);
+    allocatorDump->AddScalar("committed_size", "bytes", memoryStats->totalCommittedBytes);
     allocatorDump->AddScalar("active_size", "bytes", memoryStats->totalActiveBytes);
     allocatorDump->AddScalar("resident_size", "bytes", memoryStats->totalResidentBytes);
     allocatorDump->AddScalar("decommittable_size", "bytes", memoryStats->totalDecommittableBytes);
