14571e0b22f05dd6e17bca8ad76495b75758431a
fsamuel <fsamuel@chromium.org>
2015-08-11 08:22:24 -0700

    Revert of mandoline: Sandbox mojo:browser and mojo:omnibox. (patchset #2 id:20001 of https://codereview.chromium.org/1282903002/ )
    Reason for revert:
    This broke --enable-multiprocess on mandoline.
    
    Original issue's description:
    > mandoline: Sandbox mojo:browser and mojo:omnibox.
    >
    > BUG=492524
    >
    > Committed: https://crrev.com/7cd4b784ffd31856b32ada4efd2265a4989fd575
    > Cr-Commit-Position: refs/heads/master@{#342725}
    
    TBR=jam@chromium.org,erg@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=492524
    
    Review URL: https://codereview.chromium.org/1274993003
    
    Cr-Commit-Position: refs/heads/master@{#342822}

--- components/font_service/public/cpp/font_loader.cc
+++ components/font_service/public/cpp/font_loader.cc
@@ -7,24 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/font_service/public/cpp/font_service_thread.h"
 #include "mojo/application/public/cpp/application_impl.h"
-#include "mojo/application/public/cpp/connect.h"
-#include "mojo/application/public/interfaces/shell.mojom.h"
 
 namespace font_service {
-
-FontLoader::FontLoader(mojo::Shell* shell) {
-  mojo::ServiceProviderPtr font_service_provider;
-  mojo::URLRequestPtr request(mojo::URLRequest::New());
-  request->url = mojo::String::From("mojo:font_service");
-  FontServicePtr font_service;
-  shell->ConnectToApplication(request.Pass(),
-                              GetProxy(&font_service_provider),
-                              nullptr,
-                              nullptr);
-  mojo::ConnectToService(font_service_provider.get(), &font_service);
-
-  thread_ = new internal::FontServiceThread(font_service.Pass());
-}
 
 FontLoader::FontLoader(mojo::ApplicationImpl* application_impl) {
   mojo::URLRequestPtr request(mojo::URLRequest::New());
