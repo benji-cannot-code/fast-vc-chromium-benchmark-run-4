c363588ef0bd712d2b364122606f8c0972e317ec
samli@chromium.org <samli@chromium.org>
2015-08-11 03:18:41 +0000

    Devtools UI: Behavior and UI update to tooltips
    This change removes the arrow from tooltips and changes the color from black to a light grey.
    
    This change also hides all tooltips while the mouse is pressed down. Tooltips are also left-aligned with anchor element. They also now default to the bottom instead of the top for consistency.
    
    BUG=513066
    
    Review URL: https://codereview.chromium.org/1271193002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200290 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/Toolbar.js
+++ third_party/WebKit/Source/devtools/front_end/ui/Toolbar.js
@@ -241,7 +241,7 @@ WebInspector.ToolbarCounter.prototype = {
             }
         }
         this.element.classList.toggle("hidden", !total);
-        WebInspector.Tooltip.install(this.element, title, undefined, this._actionId);
+        WebInspector.Tooltip.install(this.element, title, this._actionId);
     },
 
     /**
@@ -405,7 +405,7 @@ WebInspector.ToolbarButtonBase.prototype = {
     setAction: function(actionId)
     {
         this._actionId = actionId;
-        WebInspector.Tooltip.install(this.element, this._title, undefined, this._actionId);
+        WebInspector.Tooltip.install(this.element, this._title, this._actionId);
     },
 
     /**
@@ -441,7 +441,7 @@ WebInspector.ToolbarButtonBase.prototype = {
         if (this._title === title)
             return;
         this._title = title;
-        WebInspector.Tooltip.install(this.element, title, undefined, this._actionId);
+        WebInspector.Tooltip.install(this.element, title, this._actionId);
     },
 
     /**
