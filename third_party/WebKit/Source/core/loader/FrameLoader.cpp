354e9d5a4b534279cbc3181d578908c7cb2a61fb
dcheng@chromium.org <dcheng@chromium.org>
2015-08-11 18:47:15 +0000

    Reinitialize the FrameView when loading a JavaScript URL.
    Since Document::detach() cleans up the associated FrameView,
    reinitialize the FrameView when loading a JavaScript URL. This is
    the same fix that was needed for XSLT in r199717.
    
    BUG=518925
    
    Review URL: https://codereview.chromium.org/1272643005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200341 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -339,6 +339,7 @@ void FrameLoader::replaceDocumentWhileExecutingJavaScriptURL(const String& sourc
     if (!m_frame->page())
         return;
 
+    client()->transitionToCommittedForNewPage();
     documentLoader->replaceDocumentWhileExecutingJavaScriptURL(init, source, ownerDocument);
 }
 
