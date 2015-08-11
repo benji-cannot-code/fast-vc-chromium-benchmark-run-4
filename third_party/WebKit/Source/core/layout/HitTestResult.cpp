b0997c208d41df5c1223ec8eab27de011030d6e2
robhogan@gmail.com <robhogan@gmail.com>
2015-08-11 00:48:59 +0000

    Image maps not displaying hand pointer when hovering over them
    I missed out m_innerPossiblyPseudoNode in https://codereview.chromium.org/1094133004!
    
    BUG=517339
    
    Review URL: https://codereview.chromium.org/1275873003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200280 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/HitTestResult.cpp
+++ third_party/WebKit/Source/core/layout/HitTestResult.cpp
@@ -230,8 +230,10 @@ void HitTestResult::setInnerNode(Node* n)
     if (n && n->isPseudoElement())
         n = toPseudoElement(n)->findAssociatedNode();
     m_innerNode = n;
-    if (HTMLAreaElement* area = imageAreaForImage())
+    if (HTMLAreaElement* area = imageAreaForImage()) {
         m_innerNode = area;
+        m_innerPossiblyPseudoNode = area;
+    }
 }
 
 void HitTestResult::setURLElement(Element* n)
