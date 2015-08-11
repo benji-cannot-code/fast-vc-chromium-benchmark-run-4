fe660f26ada50a300a6c84e63aa08f67e0cbc14f
ricea <ricea@chromium.org>
2015-08-11 04:31:50 -0700

    Replace StringPrintf("%s", ...) with an explicit cast.
    StringPrintf("%s", ...) was being used to implicitly cast an unsigned
    char* string to a char* string. Use a safer reinterpret_cast<char*>
    conversion instead.
    
    BUG=
    TEST=compile
    
    Review URL: https://codereview.chromium.org/1278493008
    
    Cr-Commit-Position: refs/heads/master@{#342808}

--- media/capture/video/linux/video_capture_device_factory_linux.cc
+++ media/capture/video/linux/video_capture_device_factory_linux.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_enumerator.h"
 #include "base/files/scoped_file.h"
 #include "base/posix/eintr_wrapper.h"
-#include "base/strings/stringprintf.h"
 #if defined(OS_CHROMEOS)
 #include "media/capture/video/linux/video_capture_device_chromeos.h"
 #endif
@@ -186,7 +185,7 @@ void VideoCaptureDeviceFactoryLinux::GetDeviceNames(
          !(cap.capabilities & V4L2_CAP_VIDEO_OUTPUT_MPLANE)) &&
         HasUsableFormats(fd.get(), cap.capabilities)) {
       device_names->push_back(VideoCaptureDevice::Name(
-          base::StringPrintf("%s", cap.card), unique_id,
+          reinterpret_cast<char*>(cap.card), unique_id,
           (cap.capabilities & V4L2_CAP_VIDEO_CAPTURE_MPLANE)
               ? VideoCaptureDevice::Name::V4L2_MULTI_PLANE
               : VideoCaptureDevice::Name::V4L2_SINGLE_PLANE));
