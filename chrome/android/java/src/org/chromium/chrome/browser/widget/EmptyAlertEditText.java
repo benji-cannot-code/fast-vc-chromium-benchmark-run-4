43de7673257c9b257583a63a2a65931420f6fb5a
kkimlabs <kkimlabs@chromium.org>
2015-08-11 09:06:05 -0700

    Revert of [Android] Make enhanced bookmark editing UI more straightforward. (patchset #8 id:140001 of https://codereview.chromium.org/1230313009/ )
    Reason for revert:
    http://crbug.com/519368
    
    Original issue's description:
    > [Android] Make enhanced bookmark editing UI more straightforward.
    >
    > - We didn't save when a dialog is dismissed by tapping outside on
    >   tablet, which can be also confusing as there is no save UI concept.
    >   So save onPause always.
    >
    > - User should know when we won't save what user typed if it fails
    >   validation. But blocking dialog exit is too invasive, and sometimes
    >   we don't have a control (tapping outside on tablet.) So show warning
    >   as user types.
    >
    > BUG=510253
    >
    > Committed: https://crrev.com/5886fc8beb1ae5714f25a8bb1c76a42bfe88272c
    > Cr-Commit-Position: refs/heads/master@{#342719}
    
    TBR=ianwen@chromium.org,newt@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=510253
    BUT=519368
    
    Review URL: https://codereview.chromium.org/1278963004
    
    Cr-Commit-Position: refs/heads/master@{#342826}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/EmptyAlertEditText.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/EmptyAlertEditText.java
@@ -44,11 +44,7 @@ public class EmptyAlertEditText extends AppCompatEditText {
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (isEmpty()) {
-                    if (getError() != null) setError(null);
-                } else {
-                    setError(mAlertMessage);
-                }
+                if (s.toString().trim().length() != 0 && getError() != null) setError(null);
             }
         });
     }
@@ -68,9 +64,14 @@ public class EmptyAlertEditText extends AppCompatEditText {
     }
 
     /**
+     * Checks whether the content is empty. If empty, an alert message will be shown.
      * @return Whether the content is empty.
      */
-    public boolean isEmpty() {
-        return getTrimmedText().length() > 0;
+    public boolean validate() {
+        if (getTrimmedText().length() == 0) {
+            setError(mAlertMessage);
+            return false;
+        }
+        return true;
     }
 }
