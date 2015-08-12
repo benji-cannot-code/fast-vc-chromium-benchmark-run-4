79ec6f31eddeda3b27a2a54d562186b6875e5cc4
wjmaclean <wjmaclean@chromium.org>
2015-08-12 00:31:29 -0700

    Rename BasicURLRequestContext.
    The name "BasicURLRequestContext" is misleading, as it is *more* than
    just a URLRequestContext. Instead, it's a URLRequestContext with added
    storage to contain owned-pointers to the context's components.
    
    This rename better reflects that distinction.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1273363006
    
    Cr-Commit-Position: refs/heads/master@{#342984}

--- net/url_request/url_request_context_builder.cc
+++ net/url_request/url_request_context_builder.cc
@@ -126,9 +126,11 @@ class BasicNetworkDelegate : public NetworkDelegateImpl {
   DISALLOW_COPY_AND_ASSIGN(BasicNetworkDelegate);
 };
 
-class BasicURLRequestContext : public URLRequestContext {
+// Define a context class that can self-manage the ownership of its components
+// via a UrlRequestContextStorage object.
+class ContainerURLRequestContext : public URLRequestContext {
  public:
-  explicit BasicURLRequestContext(
+  explicit ContainerURLRequestContext(
       const scoped_refptr<base::SingleThreadTaskRunner>& file_task_runner)
       : file_task_runner_(file_task_runner), storage_(this) {}
 
@@ -154,7 +156,7 @@ class BasicURLRequestContext : public URLRequestContext {
   }
 
  protected:
-  ~BasicURLRequestContext() override { AssertNoURLRequests(); }
+  ~ContainerURLRequestContext() override { AssertNoURLRequests(); }
 
  private:
   // The thread should be torn down last.
@@ -164,7 +166,7 @@ class BasicURLRequestContext : public URLRequestContext {
   URLRequestContextStorage storage_;
   scoped_ptr<TransportSecurityPersister> transport_security_persister_;
 
-  DISALLOW_COPY_AND_ASSIGN(BasicURLRequestContext);
+  DISALLOW_COPY_AND_ASSIGN(ContainerURLRequestContext);
 };
 
 }  // namespace
@@ -253,8 +255,8 @@ void URLRequestContextBuilder::SetHttpServerProperties(
 }
 
 URLRequestContext* URLRequestContextBuilder::Build() {
-  BasicURLRequestContext* context =
-      new BasicURLRequestContext(file_task_runner_);
+  ContainerURLRequestContext* context =
+      new ContainerURLRequestContext(file_task_runner_);
   URLRequestContextStorage* storage = context->storage();
 
   storage->set_http_user_agent_settings(new StaticHttpUserAgentSettings(
