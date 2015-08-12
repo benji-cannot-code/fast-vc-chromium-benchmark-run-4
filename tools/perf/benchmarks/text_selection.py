f44f7780c7ad77e416f5204068946f42a0e091b4
vollick <vollick@chromium.org>
2015-08-12 14:53:52 -0700

    Disable text selection benchmark
    It's timing out on android.
    
    TBR=mfomitchev@chromium.org
    BUG=520079
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1283213002
    
    Cr-Commit-Position: refs/heads/master@{#343103}

--- tools/perf/benchmarks/text_selection.py
+++ tools/perf/benchmarks/text_selection.py
@@ -37,7 +37,8 @@ class _TextSelection(perf_benchmark.PerfBenchmark):
     return value.values != None
 
 
-@benchmark.Enabled('android')
+# See crbug.com/519044
+@benchmark.Disabled()
 class TextSelectionDirection(_TextSelection):
   """Measure text selection metrics while dragging a touch selection handle on a
   subset of top ten mobile sites and using the 'direction' touch selection
@@ -50,7 +51,8 @@ class TextSelectionDirection(_TextSelection):
     return 'text_selection.direction'
 
 
-@benchmark.Enabled('android')
+# See crbug.com/519044
+@benchmark.Disabled()
 class TextSelectionCharacter(_TextSelection):
   """Measure text selection metrics while dragging a touch selection handle on a
   subset of top ten mobile sites and using the 'character' touch selection
