f86c6ce1d8a8cb9a477b340e727bb1dcbd719579
newt <newt@chromium.org>
2015-08-11 17:14:13 -0700

    Avoid partial rows of most visited icons on the NTP.
    This limits the number of rows of most visited tiles that will be
    displayed on the new tab page, and hence ensures that we don't show
    partial rows.
    
    Previously on narrow devices, we'd show 8 tiles over three rows, which
    feels unbalanced. Now we show just 2 rows (which happens to be 6 tiles).
    In non-Google mode, we show 3 rows (up to 12 tiles).
    
    Before:
     X X X
     X X X
     X X
    
    After:
     X X X
     X X X
    
    BUG=504367
    
    Review URL: https://codereview.chromium.org/1275083003
    
    Cr-Commit-Position: refs/heads/master@{#342946}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -272,6 +272,7 @@ public class NewTabPageView extends FrameLayout
         ViewStub mostVisitedLayoutStub = (ViewStub) findViewById(R.id.most_visited_layout_stub);
         mostVisitedLayoutStub.setLayoutResource(mMostVisitedDesign.getMostVisitedLayoutId());
         mMostVisitedLayout = (ViewGroup) mostVisitedLayoutStub.inflate();
+        mMostVisitedDesign.initMostVisitedLayout(mMostVisitedLayout, searchProviderHasLogo);
 
         mSearchProviderLogoView = (LogoView) findViewById(R.id.search_provider_logo);
         mSearchBoxView = findViewById(R.id.search_box);
@@ -833,6 +834,7 @@ public class NewTabPageView extends FrameLayout
         int getNumberOfTiles(boolean searchProviderHasLogo);
         int getMostVisitedLayoutId();
         int getMostVisitedLayoutBleed();
+        void initMostVisitedLayout(ViewGroup mostVisitedLayout, boolean searchProviderHasLogo);
         void setSearchProviderHasLogo(View mostVisitedLayout, boolean hasLogo);
         View createMostVisitedItemView(LayoutInflater inflater, String url, String title,
                 String displayTitle, int index, boolean isInitialLoad);
@@ -877,6 +879,11 @@ public class NewTabPageView extends FrameLayout
         @Override
         public int getMostVisitedLayoutBleed() {
             return 0;
+        }
+
+        @Override
+        public void initMostVisitedLayout(ViewGroup mostVisitedLayout,
+                boolean searchProviderHasLogo) {
         }
 
         @Override
@@ -950,6 +957,8 @@ public class NewTabPageView extends FrameLayout
 
         private static final int NUM_TILES = 8;
         private static final int NUM_TILES_NO_LOGO = 12;
+        private static final int MAX_ROWS = 2;
+        private static final int MAX_ROWS_NO_LOGO = 3;
 
         private static final int ICON_CORNER_RADIUS_DP = 4;
         private static final int ICON_TEXT_SIZE_DP = 20;
@@ -992,6 +1001,13 @@ public class NewTabPageView extends FrameLayout
         @Override
         public int getMostVisitedLayoutBleed() {
             return mMostVisitedLayoutBleed;
+        }
+
+        @Override
+        public void initMostVisitedLayout(ViewGroup mostVisitedLayout,
+                boolean searchProviderHasLogo) {
+            ((IconMostVisitedLayout) mostVisitedLayout).setMaxRows(
+                    searchProviderHasLogo ? MAX_ROWS : MAX_ROWS_NO_LOGO);
         }
 
         @Override
