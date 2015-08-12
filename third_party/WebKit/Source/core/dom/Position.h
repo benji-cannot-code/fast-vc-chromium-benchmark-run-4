d929d5b88e4ea02d1d4e6f146692f07cd63dbc32
yosin@chromium.org <yosin@chromium.org>
2015-08-12 06:17:22 +0000

    Introduce associatedElementOf() as replacement of Position::element()
    This patch introduces |associatedElementOf()| as replacement of
    |Position::element()| to simplify source code for improving code health,
    since it is used only for DOM position and we don't need to have composed tree
    version.
    
    Following patch will move |associatedElementOf()| to "htmlediting.cpp" since
    it is used only in "editing/".
    
    BUG=518738
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1283933002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200383 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Position.h
+++ third_party/WebKit/Source/core/dom/Position.h
@@ -174,8 +174,6 @@ public:
     bool isNotNull() const { return m_anchorNode; }
     bool isOrphan() const { return m_anchorNode && !m_anchorNode->inDocument(); }
 
-    Element* element() const;
-
     // Move up or down the DOM by one position.
     // Offsets are computed using layout text for nodes that have layoutObjects - but note that even when
     // using composed characters, the result may be inside a single user-visible character if a ligature is formed.
@@ -260,6 +258,10 @@ extern template class CORE_EXTERN_TEMPLATE_EXPORT PositionAlgorithm<EditingInCom
 
 using Position = PositionAlgorithm<EditingStrategy>;
 using PositionInComposedTree = PositionAlgorithm<EditingInComposedTreeStrategy>;
+
+// TODO(yosin) We should move |associatedElementOf()| to "hemltediting.cpp",
+// since it is used only in "editing/"
+CORE_EXPORT Element* associatedElementOf(const Position&);
 
 inline Position createLegacyEditingPosition(PassRefPtrWillBeRawPtr<Node> node, int offset)
 {
