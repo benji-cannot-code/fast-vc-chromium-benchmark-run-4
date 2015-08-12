7612d366a65d598f711b60a89f1ce9c2c9251478
sdefresne <sdefresne@chromium.org>
2015-08-12 01:32:10 -0700

    Upstream iOS implementation of Autocomplete factories.
    Provide iOS implementation for AutocompleteClassifier, InMemoryURLIndex
    and ShortcutsBackend factories as BrowserStateKeyedServiceFactory.
    
    Provide iOS implementation for AutocompleteProviderClient and
    AutocompleteSchemeClassifier.
    
    BUG=516635
    
    Review URL: https://codereview.chromium.org/1277543003
    
    Cr-Commit-Position: refs/heads/master@{#342991}

--- ios/chrome/browser/pref_names.h
+++ ios/chrome/browser/pref_names.h
@@ -14,6 +14,7 @@ namespace prefs {
 extern const char kAcceptLanguages[];
 extern const char kHomePage[];
 extern const char kSavingBrowserHistoryDisabled[];
+extern const char kSearchSuggestEnabled[];
 
 }  // namespace prefs
 }  // namespace ios
