bd301e5ad36d3e8269d94c9c57b7283e221d67d5
jbudorick <jbudorick@chromium.org>
2015-08-11 19:15:56 -0700

    [telemetry][Android] Tear everything down on Browser creation failure.
    BUG=
    
    Review URL: https://codereview.chromium.org/1282903005
    
    Cr-Commit-Position: refs/heads/master@{#342965}

--- tools/telemetry/telemetry/internal/backends/chrome/android_browser_finder.py
+++ tools/telemetry/telemetry/internal/backends/chrome/android_browser_finder.py
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import logging
 import os
+import sys
 
 from telemetry.core import exceptions
 from telemetry.core import platform
@@ -120,8 +121,20 @@ class PossibleAndroidBrowser(possible_browser.PossibleBrowser):
         output_profile_path=finder_options.output_profile_path,
         extensions_to_load=finder_options.extensions_to_load,
         target_arch=finder_options.target_arch)
-    return browser.Browser(
-        browser_backend, self._platform_backend, self._credentials_path)
+    try:
+      return browser.Browser(
+          browser_backend, self._platform_backend, self._credentials_path)
+    except Exception:
+      logging.exception('Failure while creating Android browser.')
+      original_exception = sys.exc_info()
+
+      try:
+        browser_backend.Close()
+      except Exception:
+        logging.exception('Secondary failure while closing browser backend.')
+
+      raise original_exception
+
 
   def SupportsOptions(self, finder_options):
     if len(finder_options.extensions_to_load) != 0:
