27981de38e55df52ecce282fa88809d8fdbb5516
davve@opera.com <davve@opera.com>
2015-08-12 09:23:55 +0000

    Remove effective zoom from font metrics before length calculations
    Since https://codereview.chromium.org/308123010 and
    https://codereview.chromium.org/323053002, the effectiveZoom is
    included in the style's font. This patch adds missing code to
    SVGLengthContext to remove the effective zoom before computing ex
    (x-height) and ch (zero-width) units.
    
    BUG=519133
    
    Review URL: https://codereview.chromium.org/1283463003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200393 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGLengthContext.cpp
+++ third_party/WebKit/Source/core/svg/SVGLengthContext.cpp
@@ -289,7 +289,7 @@ float SVGLengthContext::convertValueFromUserUnitsToCHS(float value) const
     if (!style)
         return 0;
 
-    float zeroWidth = style->fontMetrics().zeroWidth();
+    float zeroWidth = style->fontMetrics().zeroWidth() / style->effectiveZoom();
     if (!zeroWidth)
         return 0;
 
@@ -302,7 +302,7 @@ float SVGLengthContext::convertValueFromCHSToUserUnits(float value) const
     if (!style)
         return 0;
 
-    return value * style->fontMetrics().zeroWidth();
+    return value * style->fontMetrics().zeroWidth() / style->effectiveZoom();
 }
 
 float SVGLengthContext::convertValueFromUserUnitsToEXS(float value) const
@@ -313,7 +313,7 @@ float SVGLengthContext::convertValueFromUserUnitsToEXS(float value) const
 
     // Use of ceil allows a pixel match to the W3Cs expected output of coords-units-03-b.svg
     // if this causes problems in real world cases maybe it would be best to remove this
-    float xHeight = ceilf(style->fontMetrics().xHeight());
+    float xHeight = ceilf(style->fontMetrics().xHeight() / style->effectiveZoom());
     if (!xHeight)
         return 0;
 
@@ -328,7 +328,7 @@ float SVGLengthContext::convertValueFromEXSToUserUnits(float value) const
 
     // Use of ceil allows a pixel match to the W3Cs expected output of coords-units-03-b.svg
     // if this causes problems in real world cases maybe it would be best to remove this
-    return value * ceilf(style->fontMetrics().xHeight());
+    return value * ceilf(style->fontMetrics().xHeight() / style->effectiveZoom());
 }
 
 bool SVGLengthContext::determineViewport(FloatSize& viewportSize) const
