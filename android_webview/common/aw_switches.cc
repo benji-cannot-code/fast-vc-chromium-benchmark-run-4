7965bc54660f0acda8a614de5cbde420d0e4ef18
boliu <boliu@chromium.org>
2015-08-12 14:37:41 -0700

    aw: Reverse ipc command buffer switch
    IPC command buffer is on by default now. Change the switch to revert
    back to in-process command buffer instead so it's still useful without
    recompiling code.
    
    BUG=509702
    
    Review URL: https://codereview.chromium.org/1287853006
    
    Cr-Commit-Position: refs/heads/master@{#343098}

--- android_webview/common/aw_switches.cc
+++ android_webview/common/aw_switches.cc
@@ -7,6 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace switches {
 
-const char kUseIpcCommandBuffer[] = "use-ipc-command-buffer";
+const char kUseInProcCommandBuffer[] = "use-in-proc-command-buffer";
 
 }  // namespace switches
