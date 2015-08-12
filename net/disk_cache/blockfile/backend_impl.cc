1c4d046da55387466d6b359b2ce06bffccbd4ad0
ricea <ricea@chromium.org>
2015-08-12 13:21:48 -0700

    Replace use of ternary operator in void context.
    Use of the ternary operator in a void context is very concise, but not
    necessarily readable.
    
    Replace it with a boring, verbose if statement.
    
    BUG=
    TEST=net_unittests
    
    Review URL: https://codereview.chromium.org/1283203003
    
    Cr-Commit-Position: refs/heads/master@{#343083}

--- net/disk_cache/blockfile/backend_impl.cc
+++ net/disk_cache/blockfile/backend_impl.cc
@@ -1687,7 +1687,11 @@ EntryImpl* BackendImpl::MatchEntry(const std::string& key, uint32 hash,
   if (cache_entry.get() && (find_parent || !found))
     cache_entry = NULL;
 
-  find_parent ? parent_entry.swap(&tmp) : cache_entry.swap(&tmp);
+  if (find_parent)
+    parent_entry.swap(&tmp);
+  else
+    cache_entry.swap(&tmp);
+
   FlushIndex();
   return tmp;
 }
