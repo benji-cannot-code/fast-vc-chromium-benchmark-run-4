5279b58448489e7759c1f3935f7af9b0942001e1
jam@chromium.org <jam@chromium.org>
2015-08-12 03:11:58 +0000

    Revert of Fix the drift in repeating timers. (patchset #2 id:20001 of https://codereview.chromium.org/1261993006/ )
    Reason for revert:
    this change caused a large amount of flakes, see https://code.google.com/p/chromium/issues/detail?id=517488
    
    and also graphs on 
    http://chrome-monitor.appspot.com/view_graph/tryserver.chromium.linux%20Builder%20linux_chromium_rel_ng%20Success%20Rate%20(Last%20100%20Builds)
    http://chrome-monitor.appspot.com/view_graph/tryserver.chromium.win%20Builder%20win_chromium_x64_rel_ng%20Success%20Rate%20(Last%20100%20Builds)
    http://chrome-monitor.appspot.com/view_graph/tryserver.chromium.win%20Builder%20win_chromium_rel_ng%20Success%20Rate%20(Last%20100%20Builds)
    
    this was hard to track down because browser_tests weren't printing renderer crashes. i fixed that in an unlanded cl and tried reenabling all the tests and they all had
    ASSERTION FAILED: now >= m_unalignedNextFireTime
    see the try runs on this patchset
    https://codereview.chromium.org/1284083002/#ps20001
    i.e. one example:
    http://build.chromium.org/p/tryserver.chromium.win/builders/win_chromium_rel_ng/builds/92022/steps/browser_tests%20%28with%20patch%29/logs/MSE_ClearKey_Prefixed_EncryptedMediaTest.Playback_Multiple_VideoAudio_WebM_0
    
    BUG=517488
    
    Original issue's description:
    > Fix the drift in repeating timers.
    > 
    > Currently repeating blink timers ignore the difference between when the
    > timer was due to fire and when it actually did.  In addition the delay
    > reported to chromium was rounded up to the nearest millisecond,
    > introducing unnecessary extra delay.
    > 
    > (Note seperatly DOMTimers have additional clamping).
    > 
    > This fairly broken behaviour has been there for a long time, and I
    > really hope nothing has come to depend on it!
    > 
    > Judging by
    > http://www.duckware.com/test/chrome/timeraccuracy.html this patch makes
    > things a lot better at least on linux. The actual delays are much
    > closer to the requested ones, and the run time modulo interval shows
    > the drift is gone.
    > 
    > BUG=328700
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=199903
    
    TBR=jochen@chromium.org,skyostil@chromium.org,alexclarke@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=328700
    
    Review URL: https://codereview.chromium.org/1290633002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200375 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/TimerTest.cpp
+++ third_party/WebKit/Source/platform/TimerTest.cpp
@@ -42,10 +42,10 @@ private:
 
 class DelayedTask {
 public:
-    DelayedTask(WebThread::Task* task, double delaySecs)
+    DelayedTask(WebThread::Task* task, long long delayMs)
         : m_task(adoptRef(new RefCountedTaskContainer(task)))
-        , m_runTimeSecs(monotonicallyIncreasingTime() + delaySecs)
-        , m_delaySecs(delaySecs) { }
+        , m_runTimeSecs(monotonicallyIncreasingTime() + 0.001 * static_cast<double>(delayMs))
+        , m_delayMs(delayMs) { }
 
     bool operator<(const DelayedTask& other) const
     {
@@ -62,15 +62,15 @@ public:
         return m_runTimeSecs;
     }
 
-    double delaySecs() const
+    long long delayMs() const
     {
-        return m_delaySecs;
+        return m_delayMs;
     }
 
 private:
     RefPtr<RefCountedTaskContainer> m_task;
     double m_runTimeSecs;
-    double m_delaySecs;
+    long long m_delayMs;
 };
 
 class MockWebScheduler : public WebScheduler {
@@ -104,9 +104,9 @@ public:
     {
     }
 
-    void postTimerTask(const WebTraceLocation&, WebThread::Task* task, double delaySecs) override
+    void postTimerTask(const WebTraceLocation&, WebThread::Task* task, long long delayMs) override
     {
-        m_timerTasks.push(DelayedTask(task, delaySecs));
+        m_timerTasks.push(DelayedTask(task, delayMs));
     }
 
     void postTimerTaskAt(const WebTraceLocation&, WebThread::Task* task, double monotonicTime) override
@@ -136,23 +136,15 @@ public:
         }
     }
 
-    void runPendingTasks()
-    {
-        while (!m_timerTasks.empty() && m_timerTasks.top().runTimeSecs() <= gCurrentTimeSecs) {
-            m_timerTasks.top().run();
-            m_timerTasks.pop();
-        }
-    }
-
     bool hasOneTimerTask() const
     {
         return m_timerTasks.size() == 1;
     }
 
-    double nextTimerTaskDelaySecs() const
+    long nextTimerTaskDelayMillis() const
     {
         ASSERT(hasOneTimerTask());
-        return m_timerTasks.top().delaySecs();
+        return m_timerTasks.top().delayMs();
     }
 
 private:
@@ -233,11 +225,6 @@ public:
         mockScheduler()->runUntilIdle();
     }
 
-    void runPendingTasks()
-    {
-        mockScheduler()->runPendingTasks();
-    }
-
     void runUntilIdleOrDeadlinePassed(double deadline)
     {
         mockScheduler()->runUntilIdleOrDeadlinePassed(deadline);
@@ -248,9 +235,9 @@ public:
         return mockScheduler()->hasOneTimerTask();
     }
 
-    double nextTimerTaskDelaySecs() const
+    long nextTimerTaskDelayMillis() const
     {
-        return mockScheduler()->nextTimerTaskDelaySecs();
+        return mockScheduler()->nextTimerTaskDelayMillis();
     }
 
 private:
@@ -286,11 +273,6 @@ public:
         m_runTimes.push_back(monotonicallyIncreasingTime());
     }
 
-    void recordNextFireTimeTask(Timer<TimerTest>* timer)
-    {
-        m_nextFireTimes.push_back(monotonicallyIncreasingTime() + timer->nextFireInterval());
-    }
-
     void advanceTimeBy(double timeSecs)
     {
         gCurrentTimeSecs += timeSecs;
@@ -299,11 +281,6 @@ public:
     void runUntilIdle()
     {
         m_platform->runUntilIdle();
-    }
-
-    void runPendingTasks()
-    {
-        m_platform->runPendingTasks();
     }
 
     void runUntilIdleOrDeadlinePassed(double deadline)
@@ -316,15 +293,14 @@ public:
         return m_platform->hasOneTimerTask();
     }
 
-    double nextTimerTaskDelaySecs() const
+    long nextTimerTaskDelayMillis() const
     {
-        return m_platform->nextTimerTaskDelaySecs();
+        return m_platform->nextTimerTaskDelayMillis();
     }
 
 protected:
     double m_startTime;
     std::vector<double> m_runTimes;
-    std::vector<double> m_nextFireTimes;
 
 private:
     OwnPtr<TimerTestPlatform> m_platform;
@@ -337,7 +313,7 @@ TEST_F(TimerTest, StartOneShot_Zero)
     timer.startOneShot(0, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(0.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(0ll, nextTimerTaskDelayMillis());
 
     runUntilIdle();
     EXPECT_THAT(m_runTimes, ElementsAre(m_startTime));
@@ -349,7 +325,7 @@ TEST_F(TimerTest, StartOneShot_ZeroAndCancel)
     timer.startOneShot(0, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(0.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(0ll, nextTimerTaskDelayMillis());
 
     timer.stop();
 
@@ -363,7 +339,7 @@ TEST_F(TimerTest, StartOneShot_ZeroAndCancelThenRepost)
     timer.startOneShot(0, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(0.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(0ll, nextTimerTaskDelayMillis());
 
     timer.stop();
 
@@ -373,7 +349,7 @@ TEST_F(TimerTest, StartOneShot_ZeroAndCancelThenRepost)
     timer.startOneShot(0, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(0.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(0ll, nextTimerTaskDelayMillis());
 
     runUntilIdle();
     EXPECT_THAT(m_runTimes, ElementsAre(m_startTime));
@@ -385,7 +361,7 @@ TEST_F(TimerTest, StartOneShot_Zero_RepostingAfterRunning)
     timer.startOneShot(0, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(0.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(0ll, nextTimerTaskDelayMillis());
 
     runUntilIdle();
     EXPECT_THAT(m_runTimes, ElementsAre(m_startTime));
@@ -393,7 +369,7 @@ TEST_F(TimerTest, StartOneShot_Zero_RepostingAfterRunning)
     timer.startOneShot(0, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(0.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(0ll, nextTimerTaskDelayMillis());
 
     runUntilIdle();
     EXPECT_THAT(m_runTimes, ElementsAre(m_startTime, m_startTime));
@@ -405,7 +381,7 @@ TEST_F(TimerTest, StartOneShot_NonZero)
     timer.startOneShot(10.0, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(10.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(10000ll, nextTimerTaskDelayMillis());
 
     runUntilIdle();
     EXPECT_THAT(m_runTimes, ElementsAre(m_startTime + 10.0));
@@ -417,7 +393,7 @@ TEST_F(TimerTest, StartOneShot_NonZeroAndCancel)
     timer.startOneShot(10, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(10.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(10000ll, nextTimerTaskDelayMillis());
 
     timer.stop();
 
@@ -431,7 +407,7 @@ TEST_F(TimerTest, StartOneShot_NonZeroAndCancelThenRepost)
     timer.startOneShot(10, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(10.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(10000ll, nextTimerTaskDelayMillis());
 
     timer.stop();
 
@@ -442,7 +418,7 @@ TEST_F(TimerTest, StartOneShot_NonZeroAndCancelThenRepost)
     timer.startOneShot(10, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(10.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(10000ll, nextTimerTaskDelayMillis());
 
     runUntilIdle();
     EXPECT_THAT(m_runTimes, ElementsAre(secondPostTime + 10.0));
@@ -454,7 +430,7 @@ TEST_F(TimerTest, StartOneShot_NonZero_RepostingAfterRunning)
     timer.startOneShot(10, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(10.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(10000ll, nextTimerTaskDelayMillis());
 
     runUntilIdle();
     EXPECT_THAT(m_runTimes, ElementsAre(m_startTime + 10.0));
@@ -462,7 +438,7 @@ TEST_F(TimerTest, StartOneShot_NonZero_RepostingAfterRunning)
     timer.startOneShot(20, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(20.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(20000ll, nextTimerTaskDelayMillis());
 
     runUntilIdle();
     EXPECT_THAT(m_runTimes, ElementsAre(m_startTime + 10.0, m_startTime + 30.0));
@@ -475,7 +451,7 @@ TEST_F(TimerTest, PostingTimerTwiceWithSameRunTimeDoesNothing)
     timer.startOneShot(10, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(10.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(10000ll, nextTimerTaskDelayMillis());
 
     runUntilIdle();
     EXPECT_THAT(m_runTimes, ElementsAre(m_startTime + 10.0));
@@ -507,7 +483,7 @@ TEST_F(TimerTest, StartRepeatingTask)
     timer.startRepeating(1.0, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(1.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(1000ll, nextTimerTaskDelayMillis());
 
     runUntilIdleOrDeadlinePassed(m_startTime + 5.5);
     EXPECT_THAT(m_runTimes, ElementsAre(
@@ -520,7 +496,7 @@ TEST_F(TimerTest, StartRepeatingTask_ThenCancel)
     timer.startRepeating(1.0, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(1.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(1000ll, nextTimerTaskDelayMillis());
 
     runUntilIdleOrDeadlinePassed(m_startTime + 2.5);
     EXPECT_THAT(m_runTimes, ElementsAre(m_startTime + 1.0, m_startTime + 2.0));
@@ -537,7 +513,7 @@ TEST_F(TimerTest, StartRepeatingTask_ThenPostOneShot)
     timer.startRepeating(1.0, FROM_HERE);
 
     ASSERT(hasOneTimerTask());
-    EXPECT_EQ(1.0, nextTimerTaskDelaySecs());
+    EXPECT_EQ(1000ll, nextTimerTaskDelayMillis());
 
     runUntilIdleOrDeadlinePassed(m_startTime + 2.5);
     EXPECT_THAT(m_runTimes, ElementsAre(m_startTime + 1.0, m_startTime + 2.0));
@@ -761,42 +737,6 @@ TEST_F(TimerTest, DidChangeAlignmentInterval)
     EXPECT_FLOAT_EQ(m_startTime, timer.lastFireTime());
 }
 
-TEST_F(TimerTest, RepeatingTimerDoesNotDrift)
-{
-    Timer<TimerTest> timer(this, &TimerTest::recordNextFireTimeTask);
-    timer.startRepeating(2.0, FROM_HERE);
-
-    ASSERT(hasOneTimerTask());
-    recordNextFireTimeTask(&timer); // Scheduled to run at m_startTime + 2.0
-
-    advanceTimeBy(2.0);
-    runPendingTasks(); // Scheduled to run at m_startTime + 4.0
-
-    advanceTimeBy(2.1);
-    runPendingTasks(); // Scheduled to run at m_startTime + 6.0
-
-    advanceTimeBy(2.9);
-    runPendingTasks(); // Scheduled to run at m_startTime + 8.0
-
-    advanceTimeBy(3.1);
-    runPendingTasks(); // Scheduled to run at m_startTime + 12.0 (skips a beat)
-
-    advanceTimeBy(4.0);
-    runPendingTasks(); // Scheduled to run at m_startTime + 16.0 (skips a beat)
-
-    advanceTimeBy(10.0); // Scheduled to run at m_startTime + 22.0 (skips a few beats)
-    runPendingTasks();
-
-    runUntilIdleOrDeadlinePassed(m_startTime + 5.5);
-    EXPECT_THAT(m_nextFireTimes, ElementsAre(
-        m_startTime + 2.0,
-        m_startTime + 4.0,
-        m_startTime + 6.0,
-        m_startTime + 8.0,
-        m_startTime + 12.0,
-        m_startTime + 16.0,
-        m_startTime + 26.0));
-}
 
 } // namespace
 } // namespace blink
