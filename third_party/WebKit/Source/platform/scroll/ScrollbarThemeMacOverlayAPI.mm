b40fc3821b93646606f2ab3a5292257627317327
dtapuska@chromium.org <dtapuska@chromium.org>
2015-08-12 23:43:14 +0000

    Fix crash in Mac scrollbar observer.
    If [super init] failed be sure to return nil.
    
    BUG=520024
    
    Review URL: https://codereview.chromium.org/1284223002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200437 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/scroll/ScrollbarThemeMacOverlayAPI.mm
+++ third_party/WebKit/Source/platform/scroll/ScrollbarThemeMacOverlayAPI.mm
@@ -51,7 +51,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (id)initWithScrollbar:(blink::ScrollbarThemeClient*)scrollbar painter:(ScrollbarPainter)painter
 {
-    [super init];
+    if (!(self = [super init]))
+        return nil;
     _scrollbar = scrollbar;
     _scrollbarPainter = painter;
 
