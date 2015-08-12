e5737dc21d37fab2882c55361d14a321b2428615
imcheng <imcheng@google.com>
2015-08-11 18:13:30 -0700

    [Presentation API] Add missing field initialization to AvailabilityStatus.
    Initialize listening_state to INACTIVE in AvailabilityStatus ctor.
    
    BUG=510814
    
    Review URL: https://codereview.chromium.org/1288563002
    
    Cr-Commit-Position: refs/heads/master@{#342954}

--- content/renderer/presentation/presentation_dispatcher.cc
+++ content/renderer/presentation/presentation_dispatcher.cc
@@ -513,9 +513,10 @@ PresentationDispatcher::CreateSendBinaryMessageRequest(
 }
 
 PresentationDispatcher::AvailabilityStatus::AvailabilityStatus(
-    const std::string& availability_url) :
-  url(availability_url), last_known_availability(false) {
-}
+    const std::string& availability_url)
+    : url(availability_url),
+      last_known_availability(false),
+      listening_state(ListeningState::INACTIVE) {}
 
 PresentationDispatcher::AvailabilityStatus::~AvailabilityStatus() {
 }
