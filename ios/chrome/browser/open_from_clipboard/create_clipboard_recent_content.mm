ea45e58927178a61727762e6386dc711224c69e3
olivierrobin <olivierrobin@chromium.org>
2015-08-13 06:44:10 -0700

    Add hash to OFC pasteboard change detection
    Due to a bug in iOS SDK, the change count is not enough to detect
    pasteboard change after a reboot.
    
    BUG=510364
    
    Review URL: https://codereview.chromium.org/1288733002
    
    Cr-Commit-Position: refs/heads/master@{#343189}

--- ios/chrome/browser/open_from_clipboard/create_clipboard_recent_content.mm
+++ ios/chrome/browser/open_from_clipboard/create_clipboard_recent_content.mm
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/open_from_clipboard/create_clipboard_recent_content.h"
 
 #import "components/open_from_clipboard/clipboard_recent_content_ios.h"
+#include "ios/chrome/common/app_group/app_group_constants.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 
 scoped_ptr<ClipboardRecentContent> CreateClipboardRecentContentIOS() {
   return make_scoped_ptr(new ClipboardRecentContentIOS(
-      ios::GetChromeBrowserProvider()->GetChromeUIScheme()));
+      ios::GetChromeBrowserProvider()->GetChromeUIScheme(),
+      app_group::GetGroupUserDefaults()));
 }
