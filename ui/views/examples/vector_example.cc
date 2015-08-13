3594b6196b4563765ea3d5fa030000ba696be6b4
estade <estade@chromium.org>
2015-08-13 10:46:58 -0700

    Vectorize tab audio/muting image for Views.
    BUG=515183
    
    Review URL: https://codereview.chromium.org/1282933003
    
    Cr-Commit-Position: refs/heads/master@{#343229}

--- ui/views/examples/vector_example.cc
+++ ui/views/examples/vector_example.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/vector_icons_public2.h"
+#include "ui/views/border.h"
 #include "ui/views/controls/button/blue_button.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/image_view.h"
@@ -31,6 +32,7 @@ class VectorIconGallery : public View,
  public:
   VectorIconGallery()
       : image_view_(new ImageView()),
+        image_view_container_(new views::View()),
         size_input_(new Textfield()),
         color_input_(new Textfield()),
         file_chooser_(new Textfield()),
@@ -40,7 +42,21 @@ class VectorIconGallery : public View,
         color_(SK_ColorRED) {
     AddChildView(size_input_);
     AddChildView(color_input_);
-    AddChildView(image_view_);
+
+    image_view_container_->AddChildView(image_view_);
+    BoxLayout* image_layout = new BoxLayout(BoxLayout::kHorizontal, 0, 0, 0);
+    image_layout->set_cross_axis_alignment(
+        BoxLayout::CROSS_AXIS_ALIGNMENT_CENTER);
+    image_layout->set_main_axis_alignment(
+        BoxLayout::MAIN_AXIS_ALIGNMENT_CENTER);
+    image_view_container_->SetLayoutManager(image_layout);
+    image_view_->SetBorder(
+        Border::CreateSolidSidedBorder(1, 1, 1, 1, SK_ColorBLACK));
+    AddChildView(image_view_container_);
+
+    BoxLayout* box = new BoxLayout(BoxLayout::kVertical, 10, 10, 10);
+    SetLayoutManager(box);
+    box->SetFlexForView(image_view_container_, 1);
 
     file_chooser_->set_placeholder_text(
         base::ASCIIToUTF16("Or enter a file to read"));
@@ -57,9 +73,6 @@ class VectorIconGallery : public View,
     color_input_->set_placeholder_text(base::ASCIIToUTF16("Color (AARRGGBB)"));
     color_input_->set_controller(this);
 
-    BoxLayout* box = new BoxLayout(BoxLayout::kVertical, 10, 10, 10);
-    SetLayoutManager(box);
-    box->SetFlexForView(image_view_, 1);
     UpdateImage();
   }
 
@@ -67,7 +80,7 @@ class VectorIconGallery : public View,
 
   // View implementation.
   bool OnMousePressed(const ui::MouseEvent& event) override {
-    if (GetEventHandlerForPoint(event.location()) == image_view_) {
+    if (GetEventHandlerForPoint(event.location()) == image_view_container_) {
       vector_id_ = ((vector_id_ + 1) %
                     static_cast<int>(gfx::VectorIconId::VECTOR_ICON_NONE));
       UpdateImage();
@@ -116,10 +129,12 @@ class VectorIconGallery : public View,
   void UpdateImage() {
     image_view_->SetImage(gfx::CreateVectorIcon(
         static_cast<gfx::VectorIconId>(vector_id_), size_, color_));
+    Layout();
   }
 
  private:
   ImageView* image_view_;
+  View* image_view_container_;
   Textfield* size_input_;
   Textfield* color_input_;
   Textfield* file_chooser_;
