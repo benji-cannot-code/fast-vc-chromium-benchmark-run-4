920a8ec268dcd24f38e43b7935e387d69fdd4d7a
oshima <oshima@chromium.org>
2015-08-13 16:42:50 -0700

    Partial revert of r330042 "Draw the offscreen texture to reflector's surface without extra copy"
    Revert the SwapBufferComplete signal back to use sync point, rather than waiting for CompositionStart events on mirroring displays.
    
    Reason for revert:
     This is causing flickering (crbug.com/515332).
    
    BUG=515332
    
    Review URL: https://codereview.chromium.org/1275543006
    
    Cr-Commit-Position: refs/heads/master@{#343302}

--- content/browser/compositor/offscreen_browser_compositor_output_surface.cc
+++ content/browser/compositor/offscreen_browser_compositor_output_surface.cc
@@ -134,6 +134,15 @@ void OffscreenBrowserCompositorOutputSurface::SwapBuffers(
   }
 
   client_->DidSwapBuffers();
+
+  // TODO(oshima): sync with the reflector's SwapBuffersComplete
+  // (crbug.com/520567).
+  // The original implementation had a flickering issue (crbug.com/515332).
+  uint32_t sync_point =
+      context_provider_->ContextGL()->InsertSyncPointCHROMIUM();
+  context_provider_->ContextSupport()->SignalSyncPoint(
+      sync_point, base::Bind(&OutputSurface::OnSwapBuffersComplete,
+                             weak_ptr_factory_.GetWeakPtr()));
 }
 
 void OffscreenBrowserCompositorOutputSurface::OnReflectorChanged() {
@@ -143,8 +152,7 @@ void OffscreenBrowserCompositorOutputSurface::OnReflectorChanged() {
 
 base::Closure
 OffscreenBrowserCompositorOutputSurface::CreateCompositionStartedCallback() {
-  return base::Bind(&OutputSurface::OnSwapBuffersComplete,
-                    weak_ptr_factory_.GetWeakPtr());
+  return base::Closure();
 }
 
 #if defined(OS_MACOSX)
