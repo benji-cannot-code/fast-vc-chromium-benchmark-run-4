f34a4cfa8c1885a621852a27aaee71b7ee6a34b8
msramek <msramek@chromium.org>
2015-08-13 12:22:11 -0700

    Add a passwords counter to the Clear Browsing Data dialog.
    The counting is restarted whenever the deletion preference or the contents of the password store change.
    
    This CL also contains some polishing of the parent BrowsingDataCounter class.
    
    BUG=510028
    
    Review URL: https://codereview.chromium.org/1275593002
    
    Cr-Commit-Position: refs/heads/master@{#343254}

--- chrome/browser/browsing_data/browsing_data_counter.cc
+++ chrome/browser/browsing_data/browsing_data_counter.cc
@@ -25,14 +25,22 @@ void BrowsingDataCounter::Init(
   pref_.Init(
       GetPrefName(),
       profile_->GetPrefs(),
-      base::Bind(&BrowsingDataCounter::OnPrefChanged,
+      base::Bind(&BrowsingDataCounter::RestartCounting,
                  base::Unretained(this)));
 
   initialized_ = true;
-  OnPrefChanged();
+  OnInitialized();
+  RestartCounting();
 }
 
-void BrowsingDataCounter::OnPrefChanged() {
+Profile* BrowsingDataCounter::GetProfile() const {
+  return profile_;
+}
+
+void BrowsingDataCounter::OnInitialized() {
+}
+
+void BrowsingDataCounter::RestartCounting() {
   DCHECK(initialized_);
 
   // If this data type was unchecked for deletion, we do not need to count it.
@@ -47,10 +55,6 @@ void BrowsingDataCounter::OnPrefChanged() {
 
   counting_ = true;
   Count();
-}
-
-// virtual
-void BrowsingDataCounter::Count() {
 }
 
 void BrowsingDataCounter::ReportResult(uint32 value) {
--- chrome/browser/browsing_data/passwords_counter.h
+++ chrome/browser/browsing_data/passwords_counter.h
@@ -7,8 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_BROWSING_DATA_PASSWORDS_COUNTER_H_
 
 #include "chrome/browser/browsing_data/browsing_data_counter.h"
+#include "components/password_manager/core/browser/password_store.h"
+#include "components/password_manager/core/browser/password_store_consumer.h"
 
-class PasswordsCounter: public BrowsingDataCounter {
+class PasswordsCounter: public BrowsingDataCounter,
+                        public password_manager::PasswordStoreConsumer,
+                        public password_manager::PasswordStore::Observer {
  public:
   PasswordsCounter();
   ~PasswordsCounter() override;
@@ -17,6 +21,20 @@ class PasswordsCounter: public BrowsingDataCounter {
 
  private:
   const std::string pref_name_;
+  base::CancelableTaskTracker cancelable_task_tracker_;
+  password_manager::PasswordStore* store_;
+
+  void OnInitialized() override;
+
+  // Counting is done asynchronously in a request to PasswordStore.
+  // This callback returns the results, which are subsequently reported.
+  void OnGetPasswordStoreResults(
+      ScopedVector<autofill::PasswordForm> results) override;
+
+  // Called when the contents of the password store change. Triggers new
+  // counting.
+  void OnLoginsChanged(
+      const password_manager::PasswordStoreChangeList& changes) override;
 
   void Count() override;
 };
