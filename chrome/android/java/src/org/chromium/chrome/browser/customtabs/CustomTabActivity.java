b1f472076cd3d69eb046e93f9e10ce79d282626f
yusufo <yusufo@chromium.org>
2015-08-13 11:07:25 -0700

    Add signals to make callback more reliable
    Adds notifying the client on
    pageLoadFailed: on security error and actual load fail
    pageLoadAborted: on user starting new navigation while one is unfinished
    tabVisible and  tabHidden.
    
    BUG=517894
    
    Review URL: https://codereview.chromium.org/1292493002
    
    Cr-Commit-Position: refs/heads/master@{#343232}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.customtabs;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.IBinder;
+import android.support.customtabs.CustomTabsCallback;
 import android.support.customtabs.CustomTabsIntent;
 import android.text.TextUtils;
 import android.view.MenuItem;
@@ -222,6 +223,14 @@ public class CustomTabActivity extends ChromeActivity {
             RecordHistogram.recordEnumeratedHistogram("CustomTabs.ClientAppId",
                     externalId.ordinal(), ExternalAppId.INDEX_BOUNDARY.ordinal());
         }
+    }
+
+
+    @Override
+    public void onPauseWithNative() {
+        super.onPauseWithNative();
+        CustomTabsConnection.getInstance(getApplication()).notifyNavigationEvent(
+                mSession, CustomTabsCallback.TAB_HIDDEN);
     }
 
     @Override
