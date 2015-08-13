06faedb8bdab3513ca846d5aaa1a54fea43de59b
rsesek <rsesek@chromium.org>
2015-08-13 16:42:09 -0700

    Rename chrome/utility/safe_browsing/mac/test_utils.* so GN does not complain.
    """
    ERROR at //testing/test.gni:189:5: Duplicate object file
        executable(target_name) {
        ^------------------------
    The target //chrome/test:unit_tests
    generates two object files with the same name:
      obj/chrome/test/unit_tests/test_utils.o
    
    It could be you accidentally have a file listed twice in the
    sources. Or, depending on how your toolchain maps sources to
    object files, two source files with the same name in different
    directories could map to the same object file.
    
    In the latter case, either rename one of the files or move one of
    the sources to a separate source_set to avoid them both being in
    the same target.
    """
    
    R=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1287033009
    
    Cr-Commit-Position: refs/heads/master@{#343301}

--- chrome/utility/safe_browsing/mac/read_stream_unittest.cc
+++ chrome/utility/safe_browsing/mac/read_stream_unittest.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/path_service.h"
 #include "chrome/common/chrome_paths.h"
-#include "chrome/utility/safe_browsing/mac/test_utils.h"
+#include "chrome/utility/safe_browsing/mac/dmg_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace safe_browsing {
