d06863d6e0976d90e610205abb4d4a0e3b38dd3d
yurys@chromium.org <yurys@chromium.org>
2015-08-13 19:05:11 +0000

    DevTools: remove InspectorDOMAgent::Listener
    InspectorDOMDebuggerAgent now doesn't listen for changes in DOM and Debugger agent enablement state. It checks their state only when a breakpoint is added/removed or instrumented event occurs.
    
    BUG=520334
    
    Review URL: https://codereview.chromium.org/1284213006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200482 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
@@ -317,7 +317,6 @@ InspectorDOMAgent::InspectorDOMAgent(InspectorPageAgent* pageAgent, InjectedScri
     , m_lastNodeId(1)
     , m_searchingForNode(NotSearching)
     , m_suppressAttributeModifiedEvent(false)
-    , m_listener(nullptr)
     , m_backendNodeIdToInspect(0)
 {
 }
@@ -554,8 +553,6 @@ void InspectorDOMAgent::innerEnable()
     m_domEditor = adoptPtrWillBeNoop(new DOMEditor(m_history.get()));
     m_document = m_pageAgent->inspectedFrame()->document();
     m_instrumentingAgents->setInspectorDOMAgent(this);
-    if (m_listener)
-        m_listener->domAgentWasEnabled();
     if (m_backendNodeIdToInspect)
         frontend()->inspectNodeRequested(m_backendNodeIdToInspect);
     m_backendNodeIdToInspect = 0;
@@ -586,8 +583,6 @@ void InspectorDOMAgent::disable(ErrorString* errorString)
     m_history.clear();
     m_domEditor.clear();
     setDocument(nullptr);
-    if (m_listener)
-        m_listener->domAgentWasDisabled();
 }
 
 void InspectorDOMAgent::getDocument(ErrorString* errorString, RefPtr<TypeBuilder::DOM::Node>& root)
@@ -2247,7 +2242,6 @@ DEFINE_TRACE(InspectorDOMAgent)
     visitor->trace(m_hoveredNodeForInspectMode);
     visitor->trace(m_history);
     visitor->trace(m_domEditor);
-    visitor->trace(m_listener);
     InspectorBaseAgent::trace(visitor);
 }
 
