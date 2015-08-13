8c53a5e02861535872b80abbbb787384822c16a0
skirmani <skirmani@google.com>
2015-08-12 17:02:41 -0700

    Added Generic Parametrized Testing
    Usage and Info in this doc:
    https://docs.google.com/a/google.com/document/d/1ItTezv49Idns-r_ZcIJNCLsc3xy0h1uwUMQjCJ9vcCM/edit?usp=sharing
    
    Added a ParameterizedTest annotion with ability to have extra setup and teardown functionality
    specific to parameters. Has an array of valid Parameter objects to run. Each Parameter object can
    have an array of ParameterArgument objects. ParameterArgument objects can be set to have a default,
    otherwise they are required.
    
    CQ-DEPEND=CL:1259253007
    
    Review URL: https://codereview.chromium.org/1219683014
    
    Cr-Commit-Position: refs/heads/master@{#343131}

---
+++ base/test/android/javatests/src/org/chromium/base/test/util/parameter/Parameterizable.java
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.base.test.util.parameter;
+
+import java.util.Map;
+
+/**
+ * An interface to implement on test cases to run {@link ParameterizedTest}s.
+ */
+public interface Parameterizable {
+    Map<String, BaseParameter> getAvailableParameters();
+    void setParameterReader(Parameter.Reader parameterReader);
+}
