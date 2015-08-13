33c3762acba9c66c8d773c2127456d336e7eb2b5
sdefresne <sdefresne@chromium.org>
2015-08-13 02:45:22 -0700

    Add helper function to share code between //ios and //chrome.
    Both implementation of AutocompleteProviderClient used the same code
    for HasTabSyncEnabledAndUnencrypted() which is kind of sad. Instead
    introduce an helper function to share the code.
    
    BUG=519555
    
    Review URL: https://codereview.chromium.org/1289793002
    
    Cr-Commit-Position: refs/heads/master@{#343173}

--- chrome/browser/autocomplete/chrome_autocomplete_provider_client.cc
+++ chrome/browser/autocomplete/chrome_autocomplete_provider_client.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/omnibox/browser/autocomplete_classifier.h"
+#include "components/sync_driver/sync_service_utils.h"
 #include "content/public/browser/notification_service.h"
 
 #if defined(ENABLE_EXTENSIONS)
@@ -204,14 +205,9 @@ bool ChromeAutocompleteProviderClient::BookmarkBarIsVisible() const {
 }
 
 bool ChromeAutocompleteProviderClient::TabSyncEnabledAndUnencrypted() const {
-  // Check field trials and settings allow sending the URL on suggest requests.
-  ProfileSyncService* service =
-      ProfileSyncServiceFactory::GetInstance()->GetForProfile(profile_);
-  sync_driver::SyncPrefs sync_prefs(profile_->GetPrefs());
-  return service && service->CanSyncStart() &&
-         sync_prefs.GetPreferredDataTypes(syncer::UserTypes())
-             .Has(syncer::PROXY_TABS) &&
-         !service->GetEncryptedDataTypes().Has(syncer::SESSIONS);
+  return sync_driver::IsTabSyncEnabledAndUnencrypted(
+      ProfileSyncServiceFactory::GetInstance()->GetForProfile(profile_),
+      profile_->GetPrefs());
 }
 
 void ChromeAutocompleteProviderClient::Classify(
