3cc58daa9b31116b3442a333ec9228246b7a2b4e
ben <ben@chromium.org>
2015-08-13 15:04:02 -0700

    Use CapabilityFilter to restrict what HTMLViewers can connect to.
    R=sky@chromium.org
    http://crbug.com/510210
    
    Review URL: https://codereview.chromium.org/1287043004
    
    Cr-Commit-Position: refs/heads/master@{#343284}

--- mojo/shell/application_instance.cc
+++ mojo/shell/application_instance.cc
@@ -15,6 +15,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 namespace shell {
 
+// It's valid to specify mojo: URLs in the filter either as mojo:foo or
+// mojo://foo/ - but we store the filter in the latter form.
+CapabilityFilter CanonicalizeFilter(const CapabilityFilter& filter) {
+  CapabilityFilter canonicalized;
+  for (CapabilityFilter::const_iterator it = filter.begin();
+       it != filter.end();
+       ++it) {
+    if (it->first == "*")
+      canonicalized[it->first] = it->second;
+    else
+      canonicalized[GURL(it->first).spec()] = it->second;
+  }
+  return canonicalized;
+}
+
 ApplicationInstance::QueuedClientRequest::QueuedClientRequest()
     : originator(nullptr) {}
 
@@ -31,7 +46,7 @@ ApplicationInstance::ApplicationInstance(
     : manager_(manager),
       originator_identity_(originator_identity),
       identity_(identity),
-      filter_(filter),
+      filter_(CanonicalizeFilter(filter)),
       allow_any_application_(filter.size() == 1 && filter.count("*") == 1),
       on_application_end_(on_application_end),
       application_(application.Pass()),
@@ -96,11 +111,12 @@ void ApplicationInstance::ConnectToApplication(
     ServiceProviderPtr exposed_services,
     CapabilityFilterPtr filter) {
   std::string url_string = app_request->url.To<std::string>();
-  if (!GURL(url_string).is_valid()) {
+  GURL url(url_string);
+  if (!url.is_valid()) {
     LOG(ERROR) << "Error: invalid URL: " << url_string;
     return;
   }
-  if (allow_any_application_ || filter_.find(url_string) != filter_.end()) {
+  if (allow_any_application_ || filter_.find(url.spec()) != filter_.end()) {
     CapabilityFilter capability_filter = GetPermissiveCapabilityFilter();
     if (!filter.is_null())
       capability_filter = filter->filter.To<CapabilityFilter>();
@@ -109,8 +125,8 @@ void ApplicationInstance::ConnectToApplication(
                                    exposed_services.Pass(), capability_filter,
                                    base::Closure());
   } else {
-    DVLOG(1) << "CapabilityFilter prevented connection from: " <<
-        identity_.url << " to: " << url_string;
+    LOG(WARNING) << "CapabilityFilter prevented connection from: " <<
+        identity_.url << " to: " << url.spec();
   }
 }
 
