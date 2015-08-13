e2c5d3199e4a628ebeed45470ea9642ac4bd46b2
servolk@chromium.org <servolk@chromium.org>
2015-08-13 20:29:31 +0000

    Do the codec string parsing for MSE in media, instead of Blink
    Other code paths parse the codecs string in media/ code
    using media::ParseCodecString (see
    KeySystemConfigSelector::IsSupportedContentType and
    RendererBlinkPlatformImpl::MimeRegistry::supportsMediaMIMEType).
    I created a new overload of WebSourceBufferImpl::addSourceBuffer that
    does the same (https://codereview.chromium.org/1281663003/), now we can
    remove blink::ContentType::codecs to consolidate all code paths.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1283573002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200489 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/mediasource/MediaSource.h
+++ third_party/WebKit/Source/modules/mediasource/MediaSource.h
@@ -117,7 +117,7 @@ private:
 
     bool isUpdating() const;
 
-    PassOwnPtr<WebSourceBuffer> createWebSourceBuffer(const String& type, const Vector<String>& codecs, ExceptionState&);
+    PassOwnPtr<WebSourceBuffer> createWebSourceBuffer(const String& type, const String& codecs, ExceptionState&);
     void scheduleEvent(const AtomicString& eventName);
     void endOfStreamInternal(const WebMediaSource::EndOfStreamStatus, ExceptionState&);
 
