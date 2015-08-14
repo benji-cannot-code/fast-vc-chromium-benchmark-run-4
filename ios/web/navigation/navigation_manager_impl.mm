32bb655b40ff89415627b4886f37cc4285732d33
kkhorimoto <kkhorimoto@chromium.org>
2015-08-14 11:07:35 -0700

    Move DiscardNonCommittedItems to NavigationManager's public interface.
    This CL also renames DiscardNonCommittedEntries to
    DiscardNonCommittedItems.
    
    BUG=516982
    
    Review URL: https://codereview.chromium.org/1287793002
    
    Cr-Commit-Position: refs/heads/master@{#343431}

--- ios/web/navigation/navigation_manager_impl.mm
+++ ios/web/navigation/navigation_manager_impl.mm
@@ -131,6 +131,10 @@ NavigationItem* NavigationManagerImpl::GetPendingItem() const {
   return [[session_controller_ pendingEntry] navigationItem];
 }
 
+void NavigationManagerImpl::DiscardNonCommittedItems() {
+  [session_controller_ discardNonCommittedEntries];
+}
+
 NavigationItem* NavigationManagerImpl::GetTransientItem() const {
   return [[session_controller_ transientEntry] navigationItem];
 }
@@ -171,10 +175,6 @@ bool NavigationManagerImpl::RemoveEntryAtIndex(int index) {
 
   [session_controller_ removeEntryAtIndex:index];
   return true;
-}
-
-void NavigationManagerImpl::DiscardNonCommittedEntries() {
-  [session_controller_ discardNonCommittedEntries];
 }
 
 NavigationItem* NavigationManagerImpl::GetLastUserItem() const {
