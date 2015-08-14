bbb8054dae74af0ce0045f12f1541062ae5413d8
estark <estark@chromium.org>
2015-08-14 09:49:27 -0700

    Add mixed content and secure URL info to SecurityStyleExplanations
    content::SecurityStyleExplanations is used to convey an explanation for
    the lock icon (calculated by //chrome) to the DevTools security panel
    (whose protocol handler is in //content).
    
    To be able to more flexibly explain mixed content in the security panel,
    this CL gives the security panel more structured information than the
    opaque human-readable mixed content string that it was previously
    getting. Namely, this CL gives devtools information about whether mixed
    content was displayed/run on the page (so that the security panel can
    display a link to the Network panel to show mixed resources), and what
    security style //chrome uses to represent displaying/running insecure
    content (so that the security panel can display the appropriate icon
    next to a list of mixed resources).
    
    This CL also adds a |scheme_is_cryptographic| boolean field to the information
    that the security panel gets. This is to allow devtools to unambiguously
    distinguish HTTP page loads from HTTPS, now that
    SECURITY_STYLE_UNAUTHENTICATED is no longer reserved for HTTP only (see
    https://crbug.com/513863).
    
    The corresponding Blink changes (must land first) are in crrev.com/1275723005
    
    BUG=518065,517186
    
    Review URL: https://codereview.chromium.org/1275793004
    
    Cr-Commit-Position: refs/heads/master@{#343411}

--- content/public/browser/security_style_explanations.cc
+++ content/public/browser/security_style_explanations.cc
@@ -7,8 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-SecurityStyleExplanations::SecurityStyleExplanations() {
-}
+SecurityStyleExplanations::SecurityStyleExplanations()
+    : ran_insecure_content(false),
+      displayed_insecure_content(false),
+      ran_insecure_content_style(SECURITY_STYLE_UNKNOWN),
+      displayed_insecure_content_style(SECURITY_STYLE_UNKNOWN),
+      scheme_is_cryptographic(false) {}
 
 SecurityStyleExplanations::~SecurityStyleExplanations() {
 }
