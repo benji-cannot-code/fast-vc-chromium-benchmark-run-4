47574b9e400575c645de474812c4521f0573d09e
andresantoso <andresantoso@chromium.org>
2015-08-14 14:45:57 -0700

    Remove #if around PIXEL_FORMAT_NV12
    PIXEL_FORMAT_NV12 is only supported on Mac and ChromeOS, but we can still allow
    it to be part of the enum.
    
    BUG=510260
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1288003004
    
    Cr-Commit-Position: refs/heads/master@{#343503}

--- media/base/video_types.cc
+++ media/base/video_types.cc
@@ -27,10 +27,8 @@ std::string VideoPixelFormatToString(VideoPixelFormat format) {
       return "PIXEL_FORMAT_ARGB";
     case PIXEL_FORMAT_XRGB:
       return "PIXEL_FORMAT_XRGB";
-#if defined(OS_MACOSX) || defined(OS_CHROMEOS)
     case PIXEL_FORMAT_NV12:
       return "PIXEL_FORMAT_NV12";
-#endif
   }
   NOTREACHED() << "Invalid VideoPixelFormat provided: " << format;
   return "";
@@ -43,9 +41,7 @@ bool IsYuvPlanar(VideoPixelFormat format) {
     case PIXEL_FORMAT_YV16:
     case PIXEL_FORMAT_YV12A:
     case PIXEL_FORMAT_YV24:
-#if defined(OS_MACOSX) || defined(OS_CHROMEOS)
     case PIXEL_FORMAT_NV12:
-#endif
       return true;
 
     case PIXEL_FORMAT_UNKNOWN:
