be28e32834c9c488d5ff8f225d324ef8baf04178
pkasting <pkasting@chromium.org>
2015-08-14 16:40:23 -0700

    Pre-emptive fix for new protobuf version.
    Xlib.h #defines "Status", which then breaks .pb.h files.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1294973002
    
    Cr-Commit-Position: refs/heads/master@{#343530}

--- remoting/host/input_injector_x11.cc
+++ remoting/host/input_injector_x11.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <X11/extensions/XTest.h>
 #include <X11/Xlib.h>
 #include <X11/XKBlib.h>
+#undef Status  // Xlib.h #defines this, which breaks protobuf headers.
 
 #include <set>
 
