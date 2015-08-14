699d890632a4894a5d5e88cd90ab0657a3f40acc
senorblanco <senorblanco@chromium.org>
2015-08-14 13:09:19 -0700

    Explicitly include SkDrawLooper.h.
    This was being transitively included through a file from which I'd like
    to remove it in Skia.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1289623011
    
    Cr-Commit-Position: refs/heads/master@{#343470}

--- ui/gfx/render_text.cc
+++ ui/gfx/render_text.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "third_party/icu/source/common/unicode/rbbi.h"
 #include "third_party/icu/source/common/unicode/utf16.h"
+#include "third_party/skia/include/core/SkDrawLooper.h"
 #include "third_party/skia/include/core/SkTypeface.h"
 #include "third_party/skia/include/effects/SkGradientShader.h"
 #include "ui/gfx/canvas.h"
