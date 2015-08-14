2cd9dad2dfbdb0c3096703aea0e892a322e4548a
rvargas <rvargas@chromium.org>
2015-08-14 13:38:31 -0700

    Sandbox: Check on NTClose failure.
    This also slightly improves error handling and removes duplicated logging from
    ResolveSymbolicLink.
    
    BUG=480639
    
    Review URL: https://codereview.chromium.org/1288613003
    
    Cr-Commit-Position: refs/heads/master@{#343483}

--- sandbox/win/src/registry_policy.cc
+++ sandbox/win/src/registry_policy.cc
@@ -44,7 +44,7 @@ NTSTATUS TranslateMaximumAllowed(OBJECT_ATTRIBUTES* obj_attributes,
   OBJECT_BASIC_INFORMATION info = {0};
   status = NtQueryObject(handle, ObjectBasicInformation, &info, sizeof(info),
                          NULL);
-  NtClose(handle);
+  CHECK(NT_SUCCESS(NtClose(handle)));
   if (!NT_SUCCESS(status))
     return status;
 
