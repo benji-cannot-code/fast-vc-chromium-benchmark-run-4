3e504787ec5d12de3d1d84d742e155142b931939
sdefresne <sdefresne@chromium.org>
2015-08-14 02:10:50 -0700

    Remove unnecessary #includes in //chrome/browser/prefs.
    BUG=520542
    
    Review URL: https://codereview.chromium.org/1285253003
    
    Cr-Commit-Position: refs/heads/master@{#343355}

--- chrome/browser/prefs/pref_model_associator.cc
+++ chrome/browser/prefs/pref_model_associator.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/prefs/pref_service_syncable.h"
 #include "chrome/common/pref_names.h"
 #include "components/content_settings/core/browser/website_settings_info.h"
--- chrome/browser/prefs/profile_pref_store_manager.cc
+++ chrome/browser/prefs/profile_pref_store_manager.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/prefs/pref_registry_simple.h"
 #include "base/sequenced_task_runner.h"
 #include "chrome/common/chrome_constants.h"
-#include "chrome/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/user_prefs/tracked/pref_hash_store_impl.h"
 #include "components/user_prefs/tracked/pref_service_hash_store_contents.h"
