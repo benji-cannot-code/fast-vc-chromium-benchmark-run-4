c0f4cb5507c4efcfa881620b6633beade15fe00d
petrcermak <petrcermak@chromium.org>
2015-08-14 08:18:21 -0700

    [telemetry] Re-enable TracingBackendMemoryTest.testDumpMemorySuccess
    The test was recently disabled due to flakiness
    (https://codereview.chromium.org/1291533003/). The issue
    (crbug.com/520663) that was most likely causing it to be flaky has now
    been fixed (https://codereview.chromium.org/1290333003).
    
    BUG=520544,499731
    
    Review URL: https://codereview.chromium.org/1287763003
    
    Cr-Commit-Position: refs/heads/master@{#343385}

--- tools/telemetry/telemetry/internal/backends/chrome_inspector/tracing_backend_unittest.py
+++ tools/telemetry/telemetry/internal/backends/chrome_inspector/tracing_backend_unittest.py
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import unittest
 
-from telemetry import decorators
 from telemetry.core import util
 from telemetry.internal.backends.chrome_inspector import tracing_backend
 from telemetry.internal.backends.chrome_inspector import websocket
@@ -128,7 +127,6 @@ class TracingBackendMemoryTest(TracingBackendTest):
     if not self._browser.supports_memory_dumping:
       self.skipTest('Browser does not support memory dumping, skipping test.')
 
-  @decorators.Disabled
   def testDumpMemorySuccess(self):
     # Check that dumping memory before tracing starts raises an exception.
     self.assertRaises(Exception, self._browser.DumpMemory)
