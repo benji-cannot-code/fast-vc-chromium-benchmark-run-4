11ba31f5167844386cf045313c67f720296d31b2
vasilii <vasilii@chromium.org>
2015-08-14 01:54:37 -0700

    Drop an obsolete Keychain disclaimer in the import dialog.
    BUG=466638
    
    Review URL: https://codereview.chromium.org/1290183002
    
    Cr-Commit-Position: refs/heads/master@{#343354}

--- chrome/browser/resources/options/import_data_overlay.js
+++ chrome/browser/resources/options/import_data_overlay.js
@@ -51,7 +51,6 @@ cr.define('options', function() {
       $('import-browsers').onchange = function() {
         self.updateCheckboxes_();
         self.validateCommitButton_();
-        self.updateBottomBar_();
       };
 
       $('import-data-commit').onclick = function() {
@@ -104,9 +103,6 @@ cr.define('options', function() {
         this.setUpCheckboxState_(checkboxes[i], enabled);
       $('import-data-commit').disabled = !enabled;
       $('import-choose-file').hidden = !enabled;
-<if expr="is_macosx">
-      $('mac-password-keychain').hidden = !enabled;
-</if>
     },
 
     /**
@@ -147,21 +143,6 @@ cr.define('options', function() {
     },
 
     /**
-     * Show or hide gray message at the bottom.
-     * @private
-     */
-    updateBottomBar_: function() {
-      var index = $('import-browsers').selectedIndex;
-      var browserProfile;
-      if (this.browserProfiles.length > index)
-        browserProfile = this.browserProfiles[index];
-      var enable = browserProfile && browserProfile['show_bottom_bar'];
-<if expr="is_macosx">
-      $('mac-password-keychain').hidden = !enable;
-</if>
-    },
-
-    /**
      * Update the supported browsers popup with given entries.
      * @param {Array} browsers List of supported browsers name.
      * @private
@@ -188,7 +169,6 @@ cr.define('options', function() {
 
         this.updateCheckboxes_();
         this.validateCommitButton_();
-        this.updateBottomBar_();
       }
     },
 
