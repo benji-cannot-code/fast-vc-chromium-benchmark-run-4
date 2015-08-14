7139bfadd404b6821d6afac32d70d5354f6c904b
chrishtr@chromium.org <chrishtr@chromium.org>
2015-08-14 21:38:56 +0000

    Fix paint invalidation rect tracking within composited scrolling layers.
    Previously, paint invalidation rect tracking (aka repaint-after-layout rects)
    were always stored in the space of the paint invalidation container. If the
    paint invalidation container is a composited scroller, this is incorrect. They
    should be stored in the space of the graphics layer backing the content. In
    the case of a composited scroller, the graphics layer backing never moves when
    scrolls occur.
    
    This had the effect that any paint invalidation anywhere in the scroller that
    happens after a scroll would end up invalidating the entire scrolled content,
    because it appeared to have moved. This change is a huge performance improvement
    in some such cases. In addition, this patch fixes incorrect invalidations which
    may result in missed invalidations after scrolling.
    
    BUG=516016
    
    Review URL: https://codereview.chromium.org/1287413002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200567 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -738,11 +738,6 @@ bool LayoutBox::canBeProgramaticallyScrolled() const
     return node && node->hasEditableStyle();
 }
 
-bool LayoutBox::usesCompositedScrolling() const
-{
-    return hasOverflowClip() && hasLayer() && layer()->scrollableArea()->usesCompositedScrolling();
-}
-
 void LayoutBox::autoscroll(const IntPoint& positionInRootFrame)
 {
     LocalFrame* frame = this->frame();
@@ -1394,7 +1389,7 @@ PaintInvalidationReason LayoutBox::invalidatePaintIfNeeded(PaintInvalidationStat
     }
 
     // This is for the next invalidatePaintIfNeeded so must be at the end.
-    savePreviousBoxSizesIfNeeded();
+    savePreviousBoxSizesIfNeeded(newPaintInvalidationContainer);
     return reason;
 }
 
@@ -4572,13 +4567,13 @@ void LayoutBox::setPageLogicalOffset(LayoutUnit offset)
     ensureRareData().m_pageLogicalOffset = offset;
 }
 
-bool LayoutBox::needToSavePreviousBoxSizes()
+bool LayoutBox::needToSavePreviousBoxSizes(const LayoutBoxModelObject& paintInvalidationContainer)
 {
     // If m_rareData is already created, always save.
     if (m_rareData)
         return true;
 
-    LayoutSize paintInvalidationSize = previousPaintInvalidationRect().size();
+    LayoutSize paintInvalidationSize = previousPaintInvalidationRectIncludingCompositedScrolling(paintInvalidationContainer).size();
     // Don't save old box sizes if the paint rect is empty because we'll
     // full invalidate once the paint rect becomes non-empty.
     if (paintInvalidationSize.isEmpty())
@@ -4602,9 +4597,9 @@ bool LayoutBox::needToSavePreviousBoxSizes()
     return false;
 }
 
-void LayoutBox::savePreviousBoxSizesIfNeeded()
+void LayoutBox::savePreviousBoxSizesIfNeeded(const LayoutBoxModelObject& paintInvalidationContainer)
 {
-    if (!needToSavePreviousBoxSizes())
+    if (!needToSavePreviousBoxSizes(paintInvalidationContainer))
         return;
 
     LayoutBoxRareData& rareData = ensureRareData();
