d790c1f6d689395335f8778d616d5a463459d8a2
alph@chromium.org <alph@chromium.org>
2015-08-14 23:59:29 +0000

    DevTools: Fix keyboard navigation on timeline tree view
    R=yurys
    BUG=463547
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1294833003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200579 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineTreeView.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineTreeView.js
@@ -250,6 +250,7 @@ WebInspector.TimelineTreeView.GridNode = function(profileNode, grandTotalTime)
         return WebInspector.UIString("%.2f\u2009%%", value);
     }
 
+    this._populated = false;
     this._profileNode = profileNode;
     this._totalTime = grandTotalTime;
     var selfTime = profileNode.totalTime;
@@ -335,6 +336,9 @@ WebInspector.TimelineTreeView.GridNode.prototype = {
      */
     populate: function()
     {
+        if (this._populated)
+            return;
+        this._populated = true;
         if (!this._profileNode.children)
             return;
         for (var node of this._profileNode.children.values()) {
