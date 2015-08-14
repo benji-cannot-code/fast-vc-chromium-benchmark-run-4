cac19a0a052fe0a936f2a2d36b9a0054f84d2b03
tfarina <tfarina@chromium.org>
2015-08-13 22:25:34 -0700

    net: Cleanup includes of dns_util.h.
    These files are not using any of the three functions from dns_util.h.
    
    BUG=None
    TEST=net_unittests
    R=mmenke@chromium.org
    
    Review URL: https://codereview.chromium.org/1286933003
    
    Cr-Commit-Position: refs/heads/master@{#343342}

--- net/dns/dns_query_unittest.cc
+++ net/dns/dns_query_unittest.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/dns/dns_query.h"
 
-#include "base/bind.h"
-#include "net/base/dns_util.h"
 #include "net/base/io_buffer.h"
 #include "net/dns/dns_protocol.h"
 #include "testing/gtest/include/gtest/gtest.h"
--- net/dns/record_rdata.cc
+++ net/dns/record_rdata.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/record_rdata.h"
 
 #include "base/big_endian.h"
-#include "net/base/dns_util.h"
 #include "net/base/ip_address_number.h"
 #include "net/dns/dns_protocol.h"
 #include "net/dns/dns_response.h"
