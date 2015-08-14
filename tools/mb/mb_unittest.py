aea1e82640390836a3632856aca99360aaad460c
dpranke <dpranke@chromium.org>
2015-08-13 19:32:59 -0700

    Add support for the GYP_CROSSCOMPILE env var to MB.
    This is needed to support building the "Linux ARM" configurations w/ MB,
    which are cross-compiles but the target os is linux, not android or iOS.
    Theoretically it might be needed for a real CrOS compile as well. You
    can now specify that a cross-compile is needed with the 'crosscompile'
    mixin.
    
    We don't normally need this flag for android and ios builds, because
    gyp_chromium sets it for us when target_os={android,ios}.
    
    R=thakis@chromium.org
    BUG=481692
    
    Review URL: https://codereview.chromium.org/1294663002
    
    Cr-Commit-Position: refs/heads/master@{#343327}

--- tools/mb/mb_unittest.py
+++ tools/mb/mb_unittest.py
@@ -20,6 +20,7 @@ class FakeMBW(mb.MetaBuildWrapper):
     self.files = {}
     self.calls = []
     self.cmds = []
+    self.cross_compile = None
     self.out = ''
     self.err = ''
     self.platform = 'linux2'
@@ -41,7 +42,9 @@ class FakeMBW(mb.MetaBuildWrapper):
   def WriteFile(self, path, contents):
     self.files[path] = contents
 
-  def Call(self, cmd):
+  def Call(self, cmd, env=None):
+    if env:
+      self.cross_compile = env.get('GYP_CROSSCOMPILE')
     self.calls.append(cmd)
     if self.cmds:
       return self.cmds.pop(0)
@@ -83,7 +86,7 @@ TEST_CONFIG = """\
     'gyp_rel_bot': ['gyp', 'rel', 'goma'],
     'gn_debug': ['gn', 'debug'],
     'gn_rel_bot': ['gn', 'rel', 'goma'],
-    'private': ['gyp', 'fake_feature1'],
+    'private': ['gyp', 'rel', 'fake_feature1'],
     'unsupported': ['gn', 'fake_feature2'],
   },
   'masters': {
@@ -95,6 +98,7 @@ TEST_CONFIG = """\
   'mixins': {
     'fake_feature1': {
       'gn_args': 'enable_doom_melon=true',
+      'gyp_crosscompile': True,
       'gyp_defines': 'doom_melon=1',
     },
     'fake_feature2': {
@@ -150,7 +154,7 @@ class UnitTest(unittest.TestCase):
              }"""}
 
     mbw = self.fake_mbw(files)
-    mbw.Call = lambda cmd: (0, 'out/Default/foo_unittests\n', '')
+    mbw.Call = lambda cmd, env=None: (0, 'out/Default/foo_unittests\n', '')
 
     self.check(['analyze', '-c', 'gn_debug', '//out/Default',
                 '/tmp/in.json', '/tmp/out.json'], mbw=mbw, ret=0)
@@ -167,7 +171,7 @@ class UnitTest(unittest.TestCase):
                "targets": ["all", "bar_unittests"]
              }"""}
     mbw = self.fake_mbw(files)
-    mbw.Call = lambda cmd: (0, 'out/Default/foo_unittests\n', '')
+    mbw.Call = lambda cmd, env=None: (0, 'out/Default/foo_unittests\n', '')
     self.check(['analyze', '-c', 'gn_debug', '//out/Default',
                 '/tmp/in.json', '/tmp/out.json'], mbw=mbw, ret=0)
     out = json.loads(mbw.files['/tmp/out.json'])
@@ -202,7 +206,7 @@ class UnitTest(unittest.TestCase):
 
   def test_gn_gen_fails(self):
     mbw = self.fake_mbw()
-    mbw.Call = lambda cmd: (1, '', '')
+    mbw.Call = lambda cmd, env=None: (1, '', '')
     self.check(['gen', '-c', 'gn_debug', '//out/Default'], mbw=mbw, ret=1)
 
   def test_gn_gen_swarming(self):
@@ -244,12 +248,17 @@ class UnitTest(unittest.TestCase):
                      ret=0)
     self.assertIn('analyzer', mbw.calls[0])
 
+  def test_gyp_crosscompile(self):
+    mbw = self.fake_mbw()
+    self.check(['gen', '-c', 'private', '//out/Release'], mbw=mbw)
+    self.assertTrue(mbw.cross_compile)
+
   def test_gyp_gen(self):
     self.check(['gen', '-c', 'gyp_rel_bot', '//out/Release'], ret=0)
 
   def test_gyp_gen_fails(self):
     mbw = self.fake_mbw()
-    mbw.Call = lambda cmd: (1, '', '')
+    mbw.Call = lambda cmd, env=None: (1, '', '')
     self.check(['gen', '-c', 'gyp_rel_bot', '//out/Release'], mbw=mbw, ret=1)
 
   def test_gyp_lookup_goma_dir_expansion(self):
