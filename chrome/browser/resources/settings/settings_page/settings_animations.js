b4db7f561fd3aa781da3786f788961f7b9bf8e53
tommycli <tommycli@chromium.org>
2015-08-14 16:23:19 -0700

    Settings: Implement sub-subpages, and clean up some animations.
    BUG=425627
    
    Review URL: https://codereview.chromium.org/1290583002
    
    Cr-Commit-Position: refs/heads/master@{#343524}

--- chrome/browser/resources/settings/settings_page/settings_animations.js
+++ chrome/browser/resources/settings/settings_page/settings_animations.js
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * 'settings-animated-pages'.
  */
 Polymer({
-  is: 'next-page-enter-animation',
+  is: 'settings-fade-in-animation',
 
   behaviors: [
     Polymer.NeonAnimationBehavior
@@ -31,7 +31,7 @@ Polymer({
 });
 
 Polymer({
-  is: 'next-page-exit-animation',
+  is: 'settings-fade-out-animation',
 
   behaviors: [
     Polymer.NeonAnimationBehavior
