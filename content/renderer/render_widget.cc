5edd388fcfeb28be5ab923030e924cf900b8a45c
andresantoso <andresantoso@chromium.org>
2015-08-13 23:00:03 -0700

    Mac: Fix Japanese IME regression
    This is a partial revert of https://codereview.chromium.org/1240943002.
    Setting handling_input_event_ to true in OnImeSetComposition caused this
    regression. This was needed to fix Korean IME on 10.11 (crbug.com/500376),
    but when I tested with the latest 10.11 beta 15A244d, Korean IMA works just
    fine without this change. Looks like Apple fixed something in the latest beta.
    
    BUG=516882,500376
    
    Review URL: https://codereview.chromium.org/1291253002
    
    Cr-Commit-Position: refs/heads/master@{#343347}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -1681,8 +1681,6 @@ void RenderWidget::OnImeSetComposition(
   if (!ShouldHandleImeEvent())
     return;
   ImeEventGuard guard(this);
-  base::AutoReset<bool> handling_input_event_resetter(&handling_input_event_,
-                                                      true);
   if (!webwidget_->setComposition(
       text, WebVector<WebCompositionUnderline>(underlines),
       selection_start, selection_end)) {
