d746ae7fc84ce04a4c8b153112e76ea31f40c676
sdefresne <sdefresne@chromium.org>
2015-08-14 08:53:07 -0700

    Remove unused #include in chrome/common/variations/variations_util.cc.
    Nothing from chrome/common/child_process_logging.h is used and thus
    the #include is obsolete and can be removed.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1294803002
    
    Cr-Commit-Position: refs/heads/master@{#343394}

--- chrome/common/variations/variations_util.cc
+++ chrome/common/variations/variations_util.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/field_trial.h"
 #include "base/strings/string_split.h"
-#include "chrome/common/child_process_logging.h"
 #include "chrome/common/crash_keys.h"
 #include "chrome/common/variations/fieldtrial_testing_config.h"
 #include "components/variations/active_field_trials.h"
