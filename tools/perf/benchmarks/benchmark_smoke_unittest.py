853ab4e034778b994a19312f8e1d2b1acf6a9321
nednguyen <nednguyen@google.com>
2015-08-14 19:58:25 -0700

    [Telemetry/perf] Disable smoke coverage for memory benchmarks due to flakiness
    BUG=513767
    TBR=zhenw@chromium.org
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1296003003
    
    Cr-Commit-Position: refs/heads/master@{#343554}

--- tools/perf/benchmarks/benchmark_smoke_unittest.py
+++ tools/perf/benchmarks/benchmark_smoke_unittest.py
@@ -87,6 +87,7 @@ _BLACK_LIST_TEST_MODULES = {
     speedometer,  # Takes 101 seconds.
     jetstream,  # Take 206 seconds.
     text_selection, # Always fails on cq bot.
+    memory  # Flaky on bots, crbug.com/513767
 }
 
 # Some smoke benchmark tests that run quickly on desktop platform can be very
@@ -94,7 +95,6 @@ _BLACK_LIST_TEST_MODULES = {
 _ANDROID_BLACK_LIST_MODULES = {
     kraken,  # Takes 275 seconds on Android.
     sunspider,  # Takes 163 seconds on Android.
-    memory  # Often times out on bots, crbug.com/513767
 }
 
 
