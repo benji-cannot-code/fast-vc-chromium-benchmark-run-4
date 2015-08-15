benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 237006cdee675e54d2b41594f7163c170559f08a

Images from content:url() aren't loading if the alt attribute is present
This is a second attempt at supporting fallback content on images that use
content: to provide image data. The first attempt was https://codereview.chromium.org/1134903002.

With this patch when we have this markup:

<style>
  #img {content:url('resources/non-existent.png')}
</style>
<img id='img' alt='test image'>

we will first load the image from the content:url() while parsing the style info.
Then when we attach the element to the DOM we will create a layout object for it.
Eventually the image will fail to load and will notify imageChanged(), which will
reattach it and mark the element to load fallback content. A separate image load
will happen for the img element and when that finds there's no src attribute it
will also attempt to reattach the element for fallback content - but it will return
early when it finds the element is already displaying fallback content.

BUG=516239

Review URL: https://codereview.chromium.org/1277013005

git-svn-id: svn://svn.chromium.org/blink/trunk@200510 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/html/HTMLImageElement.cpp
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/LayoutBlockFlow.h"
 #include "core/layout/LayoutImage.h"
 #include "core/page/Page.h"
+#include "core/style/ContentData.h"
 #include "platform/ContentType.h"
 #include "platform/EventDispatchForbiddenScope.h"
 #include "platform/MIMETypeRegistry.h"
@@ -331,11 +332,12 @@ ImageCandidate HTMLImageElement::findBestFitImageFromPictureParent()
 
 LayoutObject* HTMLImageElement::createLayoutObject(const ComputedStyle& style)
 {
+    const ContentData* contentData = style.contentData();
+    if (contentData && contentData->isImage() && !toImageContentData(contentData)->image()->cachedImage()->errorOccurred())
+        return LayoutObject::createObject(this, style);
+
     if (m_useFallbackContent)
         return new LayoutBlockFlow(this);
-
-    if (style.hasContent())
-        return LayoutObject::createObject(this, style);
 
     LayoutImage* image = new LayoutImage(this);
     image->setImageResource(LayoutImageResource::create());
@@ -698,6 +700,12 @@ const KURL& HTMLImageElement::sourceURL() const
 void HTMLImageElement::didAddUserAgentShadowRoot(ShadowRoot&)
 {
     HTMLImageFallbackHelper::createAltTextShadowTree(*this);
+}
+
+void HTMLImageElement::ensureFallbackForGeneratedContent()
+{
+    setUseFallbackContent();
+    reattachFallbackContent();
 }
 
 void HTMLImageElement::ensureFallbackContent()

third_party/WebKit/LayoutTests/fast/images/content-url-broken-image-with-alt-text-expected.html
@@ -1,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!DOCTYPE html>
+<img id='img' alt='test image'>
+<p>crbug.com/516239: images set by content: url() should behave correctly with alt text.

third_party/WebKit/LayoutTests/fast/images/content-url-broken-image-with-alt-text.html
@@ -1,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!DOCTYPE html>
+<style>
+#img {content:url('ImageDoesNotExist')}
+</style>
+<img id='img' alt='test image'>
+<p>crbug.com/516239: images set by content: url() should behave correctly with alt text.

third_party/WebKit/LayoutTests/fast/images/content-url-image-with-alt-text-dynamic-2-expected.html
@@ -1,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!DOCTYPE html>
+<img id='img' alt='test image'>
+<p>crbug.com/516239: images set by content: url() should behave correctly with alt text.

third_party/WebKit/LayoutTests/fast/images/content-url-image-with-alt-text-dynamic-2.html
@@ -1,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!DOCTYPE html>
+<style>
+#img {content:url('resources/lenna.png')}
+</style>
+<img id='img' alt='test image'>
+<script>
+    document.getElementById('img').style.content = "url('ImageDoesNotExist')";
+    document.getElementById('img').style.content = "url('resources/lenna.png')";
+    document.getElementById('img').style.content = "url('ImageDoesNotExist')";
+</script>
+<p>crbug.com/516239: images set by content: url() should behave correctly with alt text.

third_party/WebKit/LayoutTests/fast/images/content-url-image-with-alt-text-dynamic-expected.html
@@ -1,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!DOCTYPE html>
+<img id='img' alt='test image' src="resources/lenna.png">
+<p>crbug.com/516239: images set by content: url() should behave correctly with alt text.

third_party/WebKit/LayoutTests/fast/images/content-url-image-with-alt-text-dynamic.html
@@ -1,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!DOCTYPE html>
+<style>
+#img {content:url('ImageDoesNotExist')}
+</style>
+<img id='img' alt='test image'>
+<script>
+    document.getElementById('img').style.content = "url('resources/lenna.png')";
+    document.getElementById('img').style.content = "url('ImageDoesNotExist')";
+    document.getElementById('img').style.content = "url('resources/lenna.png')";
+</script>
+<p>crbug.com/516239: images set by content: url() should behave correctly with alt text.

third_party/WebKit/LayoutTests/fast/images/content-url-image-with-alt-text-expected.html
@@ -1,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!DOCTYPE html>
+<img id='img' alt='test image' src="resources/lenna.png">
+<p>crbug.com/516239: images set by content: url() should behave correctly with alt text.

commit 0fdd12d6b9ca6372f782532f43aca7ff15a0896f

Fix crash when content() url is -webkit-canvas
Introduced by r200510.

BUG=521152

Review URL: https://codereview.chromium.org/1293023002

git-svn-id: svn://svn.chromium.org/blink/trunk@200614 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/html/HTMLImageElement.cpp
@@ -333,8 +333,12 @@ ImageCandidate HTMLImageElement::findBestFitImageFromPictureParent()
 LayoutObject* HTMLImageElement::createLayoutObject(const ComputedStyle& style)
 {
     const ContentData* contentData = style.contentData();
-    if (contentData && contentData->isImage() && !toImageContentData(contentData)->image()->cachedImage()->errorOccurred())
-        return LayoutObject::createObject(this, style);
+    if (contentData && contentData->isImage()) {
+        const StyleImage* contentImage = toImageContentData(contentData)->image();
+        bool errorOccurred = contentImage && contentImage->cachedImage() && contentImage->cachedImage()->errorOccurred();
+        if (!errorOccurred)
+            return LayoutObject::createObject(this, style);
+    }
 
     if (m_useFallbackContent)
         return new LayoutBlockFlow(this);

third_party/WebKit/LayoutTests/fast/images/content-url-canvas-expected.html
@@ -1,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!DOCTYPE html>
+<img src="resources/green-24x24.jpg">
+<p> crbug.com/521152: Tests for crash introduced by r200510: should display a green square.</p>

third_party/WebKit/LayoutTests/fast/images/content-url-canvas.html
@@ -1,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!DOCTYPE html>
+<style>
+    img { content: -webkit-canvas(img); width: 24px; height: 24px; }
+</style>
+<img>
+<p> crbug.com/521152: Tests for crash introduced by r200510: should display a green square.</p>
+<script>
+    var ctx = document.getCSSCanvasContext("2d", "img", 24, 24);
+    ctx.fillStyle = "rgb(0,255,1)";
+    ctx.fillRect (0, 0, 24, 24);
+</script>