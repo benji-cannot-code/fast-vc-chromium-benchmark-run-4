e7f5653cd509fd2b3c43e8ecb412adcc6a12b620
dcheng@chromium.org <dcheng@chromium.org>
2015-08-15 06:51:51 +0000

    Better handle reentrancy into DocumentLoader::dataReceived().
    dataReceived() can be called reentrantly if it triggers a nested message
    loop when detaching a Document or parsing script for the new Document.
    Since dataReceived() isn't safe for reentrancy, any nested invocations
    queue the received data to be processed by the top-level invocation of
    dataReceived().
    
    BUG=515876
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=200436
    
    Review URL: https://codereview.chromium.org/1263363005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200603 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/DocumentLoader.h
+++ third_party/WebKit/Source/core/loader/DocumentLoader.h
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/loader/DocumentWriter.h"
 #include "core/loader/FrameLoaderTypes.h"
 #include "core/loader/NavigationPolicy.h"
+#include "platform/SharedBuffer.h"
 #include "platform/network/ResourceError.h"
 #include "platform/network/ResourceRequest.h"
 #include "platform/network/ResourceResponse.h"
@@ -178,6 +179,7 @@ private:
     void updateRequest(Resource*, const ResourceRequest&) final;
     void responseReceived(Resource*, const ResourceResponse&, PassOwnPtr<WebDataConsumerHandle>) final;
     void dataReceived(Resource*, const char* data, unsigned length) final;
+    void processData(const char* data, unsigned length);
     void notifyFinished(Resource*) final;
 
     bool maybeLoadEmpty();
@@ -234,6 +236,10 @@ private:
         MainResourceDone
     };
     State m_state;
+
+    // Used to protect against reentrancy into dataReceived().
+    bool m_inDataReceived;
+    RefPtr<SharedBuffer> m_dataBuffer;
 };
 
 DECLARE_WEAK_IDENTIFIER_MAP(DocumentLoader);
