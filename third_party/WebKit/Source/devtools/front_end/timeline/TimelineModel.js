f8458c4739b5d95d2e66174b7542382c711a3986
alph@chromium.org <alph@chromium.org>
2015-08-15 09:14:25 +0000

    DevTools: Add total time column to the timeline tree view
    BUG=463547
    
    Review URL: https://codereview.chromium.org/1292943002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200604 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineModel.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineModel.js
@@ -1562,6 +1562,7 @@ WebInspector.TimelineModel.buildTopDownTree = function(events, startTime, endTim
 WebInspector.TimelineModel.buildBottomUpTree = function(topDownTree, groupingCallback)
 {
     var buRoot = new WebInspector.TimelineModel.ProfileTreeNode();
+    buRoot.selfTime = 0;
     buRoot.totalTime = 0;
     buRoot.name = WebInspector.UIString("Bottom-Up Chart");
     /** @type {!Map<string,!WebInspector.TimelineModel.ProfileTreeNode>} */
@@ -1587,8 +1588,10 @@ WebInspector.TimelineModel.buildBottomUpTree = function(topDownTree, groupingCal
      */
     function appendNode(tdNode, buParent)
     {
-        var time = tdNode.selfTime;
-        buParent.totalTime += time;
+        var selfTime = tdNode.selfTime;
+        var totalTime = tdNode.totalTime;
+        buParent.selfTime += selfTime;
+        buParent.totalTime += selfTime;
         while (tdNode.parent) {
             if (!buParent.children)
                 buParent.children = /** @type {!Map<string,!WebInspector.TimelineModel.ProfileTreeNode>} */ (new Map());
@@ -1596,13 +1599,15 @@ WebInspector.TimelineModel.buildBottomUpTree = function(topDownTree, groupingCal
             var buNode = buParent.children.get(id);
             if (!buNode) {
                 buNode = new WebInspector.TimelineModel.ProfileTreeNode();
-                buNode.totalTime = time;
+                buNode.selfTime = selfTime;
+                buNode.totalTime = totalTime;
                 buNode.name = tdNode.name;
                 buNode.event = tdNode.event;
                 buNode.id = id;
                 buParent.children.set(id, buNode);
             } else {
-                buNode.totalTime += time;
+                buNode.selfTime += selfTime;
+                buNode.totalTime += totalTime;
             }
             tdNode = tdNode.parent;
             buParent = buNode;
