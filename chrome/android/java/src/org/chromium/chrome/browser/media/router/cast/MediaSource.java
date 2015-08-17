33e90300c83bdec3b7a65804dbcdbf63d99b6205
avayvod <avayvod@chromium.org>
2015-08-17 06:53:50 -0700

    [Presentation API, Android] Implemented media sinks observers for Cast
    Second try of landing https://codereview.chromium.org/1288083002
    I didn't find the reason the tests fail on the bots so I will submit them later.
    
    BUG=517091
    
    Review URL: https://codereview.chromium.org/1293683004
    
    Cr-Commit-Position: refs/heads/master@{#343674}

--- chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/MediaSource.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/MediaSource.java
@@ -9,6 +9,8 @@ import android.support.v7.media.MediaRouteSelector;
 
 import com.google.android.gms.cast.CastMediaControlIntent;
 
+import org.chromium.base.VisibleForTesting;
+
 import java.util.Locale;
 
 import javax.annotation.Nullable;
@@ -46,7 +48,15 @@ public class MediaSource {
                 .build();
     }
 
-    private MediaSource(String applicationId) {
+    /**
+     * @return the Cast application id corresponding to the source.
+     */
+    public String getApplicationId() {
+        return mApplicationId;
+    }
+
+    @VisibleForTesting
+    public MediaSource(String applicationId) {
         mApplicationId = applicationId;
     }
 
