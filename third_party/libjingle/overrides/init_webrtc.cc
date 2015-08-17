bdcc67bc0f505b2ad34f46ea6d8e74d5a78decee
tommi <tommi@chromium.org>
2015-08-17 10:20:20 -0700

    Fetch and cache the number of cores before sandbox initialization. This is necessary on Linux since trying to get this information after the sandbox has been enabled, will give incorrect information (always 1).
    BUG=176522,521621
    
    Review URL: https://codereview.chromium.org/1291583003
    
    Cr-Commit-Position: refs/heads/master@{#343704}

--- third_party/libjingle/overrides/init_webrtc.cc
+++ third_party/libjingle/overrides/init_webrtc.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "third_party/webrtc/overrides/webrtc/base/basictypes.h"
 #include "third_party/webrtc/overrides/webrtc/base/logging.h"
+#include "third_party/webrtc/system_wrappers/interface/cpu_info.h"
 #include "third_party/webrtc/system_wrappers/interface/event_tracer.h"
 
 const unsigned char* GetCategoryGroupEnabled(const char* category_group) {
@@ -78,11 +79,11 @@ void HistogramAdd(
 }  // namespace metrics
 }  // namespace webrtc
 
-// libpeerconnection is being compiled as a static lib.  In this case
-// we don't need to do any initializing but to keep things simple we
-// provide an empty intialization routine so that this #ifdef doesn't
-// have to be in other places.
 bool InitializeWebRtcModule() {
+  // Workaround for crbug.com/176522
+  // On Linux, we can't fetch the number of cores after the sandbox has been
+  // initialized, so we call DetectNumberOfCores() here, to cache the value.
+  webrtc::CpuInfo::DetectNumberOfCores();
   webrtc::SetupEventTracer(&GetCategoryGroupEnabled, &AddTraceEvent);
   return true;
 }
