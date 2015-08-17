1572026fe0794f731ba904422e68978093d03c10
peria@chromium.org <peria@chromium.org>
2015-08-17 11:05:04 +0000

    Move WorkerScriptController to Oilpan heap on Oilpan build
    BUG=509911
    
    Review URL: https://codereview.chromium.org/1291313002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200641 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/workers/WorkerGlobalScope.h
+++ third_party/WebKit/Source/core/workers/WorkerGlobalScope.h
@@ -168,7 +168,7 @@ private:
     EventTarget* errorEventTarget() final;
     void didUpdateSecurityOrigin() final { }
 
-    void clearScript() { m_script.clear(); }
+    void clearScript();
     void clearInspector();
 
     static void removeURLFromMemoryCacheInternal(const KURL&);
@@ -183,7 +183,7 @@ private:
 
     mutable UseCounter::CountBits m_deprecationWarningBits;
 
-    OwnPtr<WorkerScriptController> m_script;
+    OwnPtrWillBeMember<WorkerScriptController> m_script;
     WorkerThread* m_thread;
 
     RefPtrWillBeMember<WorkerInspectorController> m_workerInspectorController;
