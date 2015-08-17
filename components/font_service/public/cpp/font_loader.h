88a0e683fcfb8159a64bb7782528a4ff122b1597
erg <erg@chromium.org>
2015-08-17 15:48:25 -0700

    mandoline: Reland "Sandbox mojo:browser and mojo:omnibox."
    This is a straight reland of the previous patch; as the manual
    usage of icu was removed in https://codereview.chromium.org/1281343003/.
    
    BUG=492524
    Previous Review URL: https://codereview.chromium.org/1282903002
    TBR=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/1286773002
    
    Cr-Commit-Position: refs/heads/master@{#343765}

--- components/font_service/public/cpp/font_loader.h
+++ components/font_service/public/cpp/font_loader.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace mojo {
 class ApplicationImpl;
+class Shell;
 }
 
 namespace font_service {
@@ -33,6 +34,7 @@ class FontServiceThread;
 class FontLoader : public SkFontConfigInterface,
                    public internal::MappedFontFile::Observer {
  public:
+  explicit FontLoader(mojo::Shell* shell);
   explicit FontLoader(mojo::ApplicationImpl* application_impl);
   ~FontLoader() override;
 
