16ec62bc50771a46571ae951246ee06c650dc5d3
lucinka.brozkova <lucinka.brozkova@gmail.com>
2015-08-17 09:17:55 -0700

    Content settings clean-up: removing unused include
    BUG=399895
    
    Review URL: https://codereview.chromium.org/747953002
    
    Cr-Commit-Position: refs/heads/master@{#343693}

--- chrome/browser/content_settings/content_settings_policy_provider_unittest.cc
+++ chrome/browser/content_settings/content_settings_policy_provider_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_profile.h"
 #include "components/content_settings/core/browser/content_settings_rule.h"
 #include "components/content_settings/core/browser/content_settings_utils.h"
+#include "components/content_settings/core/common/content_settings_pattern.h"
 #include "components/content_settings/core/test/content_settings_test_utils.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
