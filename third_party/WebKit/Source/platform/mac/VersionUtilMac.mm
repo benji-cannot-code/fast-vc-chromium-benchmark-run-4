dc272298eae63f542f66518ac1ff21288e5c3f8e
yurys@chromium.org <yurys@chromium.org>
2015-08-17 15:41:51 +0000

    Revert of Add a short term solution to fix OSX 10.11 system font rendering problems. (patchset #2 id:20001 of https://codereview.chromium.org/1284993004/ )
    Reason for revert:
    Newly added test has been failing since its introduction
    
    http://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=blink_platform_unittests&showExpectations=true&tests=FontFamilyMatcherMacTest.YosemiteFontWeights
    
    Original issue's description:
    > Add a short term solution to fix OSX 10.11 system font rendering problems.
    > 
    > I landed a CL (https://codereview.chromium.org/1276003003/) which fixes system
    > font problems on OSX 10.9 and OSX 10.10. The CL also fixed MatchNSFontFamily()
    > to correctly return the system font on 10.11, but this causes glyph rendering
    > problems. In the short term, I'm adding logic to MatchNSFontFamily() to return
    > nil on 10.11, which was the original behavior before I landed that CL.
    > 
    > BUG=521034
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=200618
    
    TBR=tkent@chromium.org,keishi@chromium.org,erikchen@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=521034
    
    Review URL: https://codereview.chromium.org/1296243002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200646 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/mac/VersionUtilMac.mm
+++ third_party/WebKit/Source/platform/mac/VersionUtilMac.mm
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NSAppKitVersionNumber10_9 1265
 #endif
 
-#ifndef NSAppKitVersionNumber10_10
-#define NSAppKitVersionNumber10_10 1343
-#endif
-
 namespace blink {
 
 bool IsOSMavericksOrEarlier()
@@ -26,11 +22,6 @@ bool IsOSMavericksOrEarlier()
 bool IsOSMavericks()
 {
     return floor(NSAppKitVersionNumber) == NSAppKitVersionNumber10_9;
-}
-
-bool IsOSYosemiteOrEarlier()
-{
-    return floor(NSAppKitVersionNumber) <= NSAppKitVersionNumber10_10;
 }
 
 } // namespace blink
