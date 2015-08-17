21bd9f03485ff51319b84d7be32780927e0f9458
dtseng <dtseng@chromium.org>
2015-08-17 16:13:31 -0700

    Fix aria-label test.
    BUG=520946
    
    Review URL: https://codereview.chromium.org/1294963002
    
    Cr-Commit-Position: refs/heads/master@{#343768}

--- chrome/browser/resources/chromeos/chromevox/common/chrome_extension_externs.js
+++ chrome/browser/resources/chromeos/chromevox/common/chrome_extension_externs.js
@@ -409,6 +409,11 @@ chrome.automation.AutomationNode.prototype.indexInParent;
  */
 chrome.automation.AutomationNode.prototype.name;
 
+/**
+ * @type {string}
+ */
+chrome.automation.AutomationNode.prototype.description;
+
 
 /**
  * @type {string}
