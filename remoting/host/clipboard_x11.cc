d3ed3d41df536689f442be955d06111c4f6520c7
pkasting <pkasting@chromium.org>
2015-08-17 13:30:57 -0700

    Pre-emptive fix for new protobuf version.
    Xlib.h #defines "Status", which then breaks .pb.h files.
    
    BUG=none
    TEST=none
    TBR=wez
    
    Review URL: https://codereview.chromium.org/1298753002
    
    Cr-Commit-Position: refs/heads/master@{#343744}

--- remoting/host/clipboard_x11.cc
+++ remoting/host/clipboard_x11.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/clipboard.h"
 
 #include <X11/Xlib.h>
+#undef Status  // Xlib.h #defines this, which breaks protobuf headers.
 
 #include "base/bind.h"
 #include "base/logging.h"
