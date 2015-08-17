9ebb549dbd53e5f327592c33e7cb17503cd6c70b
esprehn@chromium.org <esprehn@chromium.org>
2015-08-17 20:14:06 +0000

    Move dispatchDidFirstVisuallyNonEmptyLayout into WebViewImpl.
    This removes the last user of didLayoutWithPendingStylesheets() and instead moves
    the call to dispatchDidFirstVisuallyNonEmptyLayout into WebViewImpl so the
    notification to the embedder is tied to the pumping of frames. This changes the
    behavior of this notification to only dispatch for the top level frame,
    but Chromium already has a check for that:
    
    https://chromium.googlesource.com/chromium/src/+/976d4d9a2735bdc11e5a641c84b6382566d48f1d/content/renderer/render_frame_impl.cc#3472
    
    RenderFrameImpl::didFirstVisuallyNonEmptyLayout's first check is
    
    if (frame->parent())
        return;
    
    so we already ignore all frames that are not the main frame. Future patches will
    clean up this interface so it's obvious this really only happens for the main
    frame.
    
    BUG=521692
    R=dglazkov@chromium.org
    
    Review URL: https://codereview.chromium.org/1295053002 .
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200664 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/FrameView.h
+++ third_party/WebKit/Source/core/frame/FrameView.h
@@ -234,6 +234,7 @@ public:
 
     void incrementVisuallyNonEmptyCharacterCount(unsigned);
     void incrementVisuallyNonEmptyPixelCount(const IntSize&);
+    bool isVisuallyNonEmpty() const { return m_isVisuallyNonEmpty; }
     void setIsVisuallyNonEmpty() { m_isVisuallyNonEmpty = true; }
     void enableAutoSizeMode(const IntSize& minSize, const IntSize& maxSize);
     void disableAutoSizeMode();
@@ -778,7 +779,6 @@ private:
     unsigned m_visuallyNonEmptyCharacterCount;
     unsigned m_visuallyNonEmptyPixelCount;
     bool m_isVisuallyNonEmpty;
-    bool m_firstVisuallyNonEmptyLayoutCallbackPending;
 
     RefPtrWillBeMember<Node> m_scrollAnchor;
 
