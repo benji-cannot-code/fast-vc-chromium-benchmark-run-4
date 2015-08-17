c81d921a165d84d2556e72f747121c828252d4a5
blundell <blundell@chromium.org>
2015-08-17 05:17:46 -0700

    Componentize OmniboxMetricsProvider for sharing with iOS
    The class has no problematic dependencies, so componentization is
    straightforward.
    
    BUG=508022
    
    Review URL: https://codereview.chromium.org/1274393002
    
    Cr-Commit-Position: refs/heads/master@{#343668}

--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -1847,8 +1847,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/metrics/metrics_reporting_state.h',
       'browser/metrics/metrics_services_manager.cc',
       'browser/metrics/metrics_services_manager.h',
-      'browser/metrics/omnibox_metrics_provider.cc',
-      'browser/metrics/omnibox_metrics_provider.h',
       'browser/metrics/perf_provider_chromeos.cc',
       'browser/metrics/perf_provider_chromeos.h',
       'browser/metrics/signin_status_metrics_provider.cc',
--- components/omnibox/browser/omnibox_metrics_provider.h
+++ components/omnibox/browser/omnibox_metrics_provider.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_METRICS_OMNIBOX_METRICS_PROVIDER_H_
-#define CHROME_BROWSER_METRICS_OMNIBOX_METRICS_PROVIDER_H_
+#ifndef COMPONENTS_OMNIBOX_BROWSER_OMNIBOX_METRICS_PROVIDER_H_
+#define COMPONENTS_OMNIBOX_BROWSER_OMNIBOX_METRICS_PROVIDER_H_
 
 #include "base/basictypes.h"
 #include "components/metrics/metrics_provider.h"
@@ -49,4 +49,4 @@ class OmniboxMetricsProvider : public metrics::MetricsProvider {
   DISALLOW_COPY_AND_ASSIGN(OmniboxMetricsProvider);
 };
 
-#endif  // CHROME_BROWSER_METRICS_OMNIBOX_METRICS_PROVIDER_H_
+#endif  // COMPONENTS_OMNIBOX_BROWSER_OMNIBOX_METRICS_PROVIDER_H_
