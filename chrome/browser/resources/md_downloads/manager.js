d301f3e10b38983214e762eecf7fe3ed9a8c6ff3
dbeam <dbeam@chromium.org>
2015-08-17 22:25:11 -0700

    MD Download: add "Nothing to see here..." empty downloads message
    BUG=520709
    R=groby@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1300473002
    
    Cr-Commit-Position: refs/heads/master@{#343826}

--- chrome/browser/resources/md_downloads/manager.js
+++ chrome/browser/resources/md_downloads/manager.js
@@ -161,6 +161,7 @@ cr.define('downloads', function() {
         this.$.toolbar.downloadsShowing = this.hasDownloads_;
 
       this.onResize_();
+      this.$.panel.classList.remove('loading');
     },
 
     /**
