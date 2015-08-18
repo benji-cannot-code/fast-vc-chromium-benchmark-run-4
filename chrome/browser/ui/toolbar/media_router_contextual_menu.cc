ebff584d82993203fd28f27384263ba3cb5c2f44
haibinlu <haibinlu@chromium.org>
2015-08-17 22:09:05 -0700

    [MediaRouter] Implements extension feedback page when users right click Media Router icon and select "Report an issue".
    Feedback page itself is included in the Media Router external component extension.
    
    Review URL: https://codereview.chromium.org/1296753002
    
    Cr-Commit-Position: refs/heads/master@{#343800}

--- chrome/browser/ui/toolbar/media_router_contextual_menu.cc
+++ chrome/browser/ui/toolbar/media_router_contextual_menu.cc
@@ -5,10 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "chrome/app/chrome_command_ids.h"
+#include "chrome/browser/media/router/media_router_factory.h"
+#include "chrome/browser/media/router/media_router_mojo_impl.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/singleton_tabs.h"
 #include "chrome/browser/ui/toolbar/media_router_contextual_menu.h"
 #include "chrome/grit/generated_resources.h"
+#include "extensions/common/constants.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/menu_model_delegate.h"
 
@@ -88,9 +92,25 @@ void MediaRouterContextualMenu::ExecuteCommand(int command_id,
       chrome::ShowSingletonTab(browser_, GURL(kCastLearnMorePageUrl));
       break;
     case IDC_MEDIA_ROUTER_REPORT_ISSUE:
-      NOTIMPLEMENTED();
+      ReportIssue();
       break;
     default:
       NOTREACHED();
   }
+}
+
+void MediaRouterContextualMenu::ReportIssue() {
+  // Opens feedback page loaded from the media router extension.
+  // This is temporary until feedback UI is redesigned.
+  media_router::MediaRouterMojoImpl* media_router =
+      static_cast<media_router::MediaRouterMojoImpl*>(
+          media_router::MediaRouterFactory::GetApiForBrowserContext(
+              static_cast<content::BrowserContext*>(browser_->profile())));
+  if (media_router->media_route_provider_extension_id().empty())
+    return;
+  std::string feedback_url(extensions::kExtensionScheme +
+                           std::string(url::kStandardSchemeSeparator) +
+                           media_router->media_route_provider_extension_id() +
+                           "/feedback.html");
+  chrome::ShowSingletonTab(browser_, GURL(feedback_url));
 }
