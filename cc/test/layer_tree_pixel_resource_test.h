752f3df4bd08ceb200ba14de4be9cb9717b6a6d9
reveman <reveman@chromium.org>
2015-08-18 10:18:51 -0700

    Re-land: cc: Use worker context for one-copy tile initialization.
    This moves management of staging resources to
    OneCopyTileTaskWorkerPool class. This makes it possible
    to use a worker context to issue and detect when copy
    operations complete.
    
    BUG=490295
    TBR=jamesr@chromium.org, sky@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1230203007
    
    Cr-Commit-Position: refs/heads/master@{#343947}

--- cc/test/layer_tree_pixel_resource_test.h
+++ cc/test/layer_tree_pixel_resource_test.h
@@ -39,8 +39,7 @@ class LayerTreeHostPixelResourceTest : public LayerTreePixelTest {
   void CreateResourceAndTileTaskWorkerPool(
       LayerTreeHostImpl* host_impl,
       scoped_ptr<TileTaskWorkerPool>* tile_task_worker_pool,
-      scoped_ptr<ResourcePool>* resource_pool,
-      scoped_ptr<ResourcePool>* staging_resource_pool) override;
+      scoped_ptr<ResourcePool>* resource_pool) override;
 
   void RunPixelResourceTest(scoped_refptr<Layer> content_root,
                             base::FilePath file_name);
@@ -54,7 +53,6 @@ class LayerTreeHostPixelResourceTest : public LayerTreePixelTest {
   };
 
  protected:
-  unsigned staging_texture_target_;
   unsigned draw_texture_target_;
   TileTaskWorkerPoolOption resource_pool_option_;
   bool initialized_;
--- gpu/command_buffer/client/cmd_buffer_helper.cc
+++ gpu/command_buffer/client/cmd_buffer_helper.cc
@@ -199,7 +199,8 @@ bool CommandBufferHelper::Finish() {
   if (put_ == get_offset()) {
     return true;
   }
-  DCHECK(HaveRingBuffer());
+  DCHECK(HaveRingBuffer() ||
+         error::IsError(command_buffer_->GetLastState().error));
   Flush();
   if (!WaitForGetOffsetInRange(put_, put_))
     return false;
