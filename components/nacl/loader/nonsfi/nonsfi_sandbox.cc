5aadbb119bc2ace19179be46ec5fed08304f2d61
lhchavez <lhchavez@chromium.org>
2015-08-17 22:13:43 -0700

    Non-SFI mode: Sandbox support for NaCl async-signals.
    In preparation for the async-signal support change in
    https://codereview.chromium.org/1212613002, this CL allows two new
    syscalls:
    
    * clone() with CLONE_PARENT_SETTID.
    * tgkill() with the current pid as tgid and SIGUSR1 as signal number.
    
    TEST=nacl_loader_unittests
    TEST=Ran bots
    TEST=ARC async-signals work
    BUG=nativeclient:4065
    R=mseaborn@chromium.org
    
    Review URL: https://codereview.chromium.org/1295513003
    
    Cr-Commit-Position: refs/heads/master@{#343807}

--- components/nacl/loader/nonsfi/nonsfi_sandbox.cc
+++ components/nacl/loader/nonsfi/nonsfi_sandbox.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/linux/seccomp-bpf-helpers/sigsys_handlers.h"
 #include "sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions.h"
 #include "sandbox/linux/system_headers/linux_futex.h"
+#include "sandbox/linux/system_headers/linux_signal.h"
 #include "sandbox/linux/system_headers/linux_syscalls.h"
 
 // Chrome OS Daisy (ARM) build environment and PNaCl toolchain do not define
@@ -84,7 +85,11 @@ ResultExpr RestrictClone() {
   clone_flags |= CLONE_PARENT_SETTID | CLONE_CHILD_CLEARTID;
 #endif
   const Arg<int> flags(0);
-  return If(flags == clone_flags, Allow()).Else(CrashSIGSYSClone());
+  // TODO(lhchavez): Add CLONE_PARENT_SETTID unconditionally to the allowed
+  // flags after the NaCl roll.
+  return If(flags == clone_flags ||
+                flags == (clone_flags | CLONE_PARENT_SETTID),
+            Allow()).Else(CrashSIGSYSClone());
 }
 
 ResultExpr RestrictFutexOperation() {
@@ -144,6 +149,18 @@ ResultExpr RestrictMmap() {
   const uint64_t kAllowedProtMask = PROT_READ | PROT_WRITE;
   const Arg<int> prot(2), flags(3);
   return If((prot & ~kAllowedProtMask) == 0 && (flags & ~kAllowedFlagMask) == 0,
+            Allow()).Else(CrashSIGSYS());
+}
+
+ResultExpr RestrictTgkill(int policy_pid) {
+  const Arg<int> tgid(0), tid(1), signum(2);
+  // Only sending SIGUSR1 to a thread in the same process is allowed.
+  return If(tgid == policy_pid &&
+            // Arg does not support a greater-than operator, so two separate
+            // checks are needed to ensure tid is positive.
+            tid != 0 &&
+            (tid & (1u << 31)) == 0 &&  // tid is non-negative.
+            signum == LINUX_SIGUSR1,
             Allow()).Else(CrashSIGSYS());
 }
 
@@ -211,6 +228,16 @@ void RunSandboxSanityChecks() {
 }
 
 }  // namespace
+
+NaClNonSfiBPFSandboxPolicy::NaClNonSfiBPFSandboxPolicy()
+    : policy_pid_(getpid()) {
+}
+
+NaClNonSfiBPFSandboxPolicy::~NaClNonSfiBPFSandboxPolicy() {
+  // Make sure that this policy is created, used and destroyed by a single
+  // process.
+  DCHECK_EQ(getpid(), policy_pid_);
+}
 
 ResultExpr NaClNonSfiBPFSandboxPolicy::EvaluateSyscall(int sysno) const {
   switch (sysno) {
@@ -304,6 +331,9 @@ ResultExpr NaClNonSfiBPFSandboxPolicy::EvaluateSyscall(int sysno) const {
       return RestrictSocketpair();
 #endif
 #endif
+
+    case __NR_tgkill:
+      return RestrictTgkill(policy_pid_);
 
     case __NR_brk:
       // The behavior of brk on Linux is different from other system
