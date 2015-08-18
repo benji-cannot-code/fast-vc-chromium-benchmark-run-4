dc56e92502a65cf29405683cde68e2dce6d865f5
skyostil <skyostil@chromium.org>
2015-08-18 10:55:57 -0700

    telemetry: Fix an import path in the Android screen recorder
    Review URL: https://codereview.chromium.org/1301613004
    
    Cr-Commit-Position: refs/heads/master@{#343960}

--- tools/telemetry/telemetry/internal/platform/profiler/android_screen_recorder_profiler.py
+++ tools/telemetry/telemetry/internal/platform/profiler/android_screen_recorder_profiler.py
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import os
 import subprocess
 
+from telemetry.core import util
 from telemetry.internal.platform import profiler
-from telemetry.internal import util
 from telemetry.internal.backends.chrome import android_browser_finder
 
 
