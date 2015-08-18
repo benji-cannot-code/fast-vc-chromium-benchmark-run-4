9e4644f0929471026eddc89202891f970de77a62
sydli <sydli@google.com>
2015-08-17 22:12:31 -0700

    Remove extraneous GetValidLocales calls.
    GetValidLocales is already called on first extension install. There's no need to check the locales again (i.e. on extension loading) until a new install. GetValidLocales can be costly since it stats all subdirectories and messages.json of an extension's _locales subfolder.
    
    BUG=516745
    
    Review URL: https://codereview.chromium.org/1281523005
    
    Cr-Commit-Position: refs/heads/master@{#343805}

--- extensions/common/extension_l10n_util.cc
+++ extensions/common/extension_l10n_util.cc
@@ -352,6 +352,7 @@ bool GetValidLocales(const base::FilePath& locale_path,
     }
     if (!AddLocale(
             chrome_locales, locale_folder, locale_name, valid_locales, error)) {
+      valid_locales->clear();
       return false;
     }
   }
@@ -368,7 +369,6 @@ extensions::MessageBundle* LoadMessageCatalogs(
     const base::FilePath& locale_path,
     const std::string& default_locale,
     const std::string& application_locale,
-    const std::set<std::string>& valid_locales,
     std::string* error) {
   std::vector<std::string> all_fallback_locales;
   GetAllFallbackLocales(
@@ -377,7 +377,9 @@ extensions::MessageBundle* LoadMessageCatalogs(
   std::vector<linked_ptr<base::DictionaryValue> > catalogs;
   for (size_t i = 0; i < all_fallback_locales.size(); ++i) {
     // Skip all parent locales that are not supplied.
-    if (valid_locales.find(all_fallback_locales[i]) == valid_locales.end())
+    base::FilePath this_locale_path =
+        locale_path.AppendASCII(all_fallback_locales[i]);
+    if (!base::PathExists(this_locale_path))
       continue;
     linked_ptr<base::DictionaryValue> catalog(
         LoadMessageFile(locale_path, all_fallback_locales[i], error));
