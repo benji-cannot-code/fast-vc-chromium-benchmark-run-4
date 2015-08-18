fd6cf775e3204035aff7c3cff6bc76bbd68a3d42
stevenjb <stevenjb@chromium.org>
2015-08-18 00:56:13 -0700

    Elim unused Chrome OS networking UI code
    BUG=none
    
    Review URL: https://codereview.chromium.org/1285373004
    
    Cr-Commit-Position: refs/heads/master@{#343852}

--- chrome/browser/ui/webui/options/chromeos/internet_options_handler.cc
+++ chrome/browser/ui/webui/options/chromeos/internet_options_handler.cc
@@ -55,7 +55,6 @@ namespace {
 
 // Keys for the initial "localized" dictionary values.
 const char kLoggedInAsOwnerKey[] = "loggedInAsOwner";
-const char kShowCarrierSelectKey[] = "showCarrierSelect";
 
 // Functions we call in JavaScript.
 const char kSetVPNProvidersFunction[] = "options.VPNProviders.setProviders";
@@ -141,9 +140,6 @@ void InternetOptionsHandler::GetLocalizedValues(
   bool logged_in_as_owner = LoginState::Get()->GetLoggedInUserType() ==
                             LoginState::LOGGED_IN_USER_OWNER;
   localized_strings->SetBoolean(kLoggedInAsOwnerKey, logged_in_as_owner);
-  // TODO(anujsharma): Remove kShowCarrierSelectKey, as it is not
-  // required anymore.
-  localized_strings->SetBoolean(kShowCarrierSelectKey, false);
 }
 
 void InternetOptionsHandler::InitializePage() {
--- extensions/browser/api/networking_private/networking_private_chromeos.cc
+++ extensions/browser/api/networking_private/networking_private_chromeos.cc
@@ -294,7 +294,7 @@ void NetworkingPrivateChromeOS::GetNetworks(
       chromeos::onc::NetworkTypePatternFromOncType(network_type);
   scoped_ptr<base::ListValue> network_properties_list =
       chromeos::network_util::TranslateNetworkListToONC(
-          pattern, configured_only, visible_only, limit, false /* debugging */);
+          pattern, configured_only, visible_only, limit);
   success_callback.Run(network_properties_list.Pass());
 }
 
