5ebce1d2ed2849a9b7648001b822fd20f7f7ce06
ukai <ukai@chromium.org>
2015-08-18 00:55:50 -0700

    include <OpenGL/gl.h> instead of <OpenGL/GL.h>
    actual file names in OpenGL.framework/Headers is gl.h, not GL.h,
    so <OpenGL/GL.h> may fail on case-sensitive filesystem.
    
    BUG=521449
    
    Review URL: https://codereview.chromium.org/1288953006
    
    Cr-Commit-Position: refs/heads/master@{#343851}

--- content/common/gpu/image_transport_surface_overlay_mac.mm
+++ content/common/gpu/image_transport_surface_overlay_mac.mm
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <IOSurface/IOSurface.h>
 #include <OpenGL/CGLRenderers.h>
 #include <OpenGL/CGLTypes.h>
-#include <OpenGL/GL.h>
+#include <OpenGL/gl.h>
 
 // This type consistently causes problem on Mac, and needs to be dealt with
 // in a systemic way.
