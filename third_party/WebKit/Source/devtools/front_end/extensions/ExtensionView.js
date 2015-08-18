ebbb39207a91f25a25ec086ae09e2ea9da538893
pfeldman@chromium.org <pfeldman@chromium.org>
2015-08-18 17:14:18 +0000

    DevTools: rely upon shouldHideOnDetach when considering re-layout of elements and sources panels.
    BUG=521202
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1299763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200732 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/extensions/ExtensionView.js
+++ third_party/WebKit/Source/devtools/front_end/extensions/ExtensionView.js
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.ExtensionView = function(server, id, src, className)
 {
     WebInspector.Widget.call(this);
+    this.setHideOnDetach();
     this.element.className = "vbox flex-auto"; // Override
 
     this._server = server;
--- third_party/WebKit/Source/devtools/front_end/sources/SourcesPanel.js
+++ third_party/WebKit/Source/devtools/front_end/sources/SourcesPanel.js
@@ -1143,6 +1143,9 @@ WebInspector.SourcesPanel.prototype = {
         if (this.sidebarPaneView && vertically === !this._splitWidget.isVertical())
             return;
 
+        if (this.sidebarPaneView && this.sidebarPaneView.shouldHideOnDetach())
+            return; // We can't reparent extension iframes.
+
         if (this.sidebarPaneView)
             this.sidebarPaneView.detach();
 
@@ -1231,10 +1234,8 @@ WebInspector.SourcesPanel.prototype = {
      */
     _addExtensionSidebarPane: function(pane)
     {
-        if (pane.panelName() === this.name) {
-            this.setHideOnDetach();
+        if (pane.panelName() === this.name)
             this._extensionSidebarPanesContainer.addPane(pane);
-        }
     },
 
     /**
