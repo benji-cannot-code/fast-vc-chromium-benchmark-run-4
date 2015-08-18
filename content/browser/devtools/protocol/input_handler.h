da98c31d0d0c0090a3cc1bcb81d33d07aa1fc1a9
alexclarke <alexclarke@chromium.org>
2015-08-18 03:43:14 -0700

    Make synthesizeScrollGesture repetable and emit interaction markers
    Adds new optional parameters to Input.synthesizeScrollGesture to make it
    possible to generate repeating scrolls from the browser process.  This
    functionality will be used by smoothness.scrolling_tough_ad_cases.
    
    Patch 1 of 4 to fix the overly long delay between
    each scroll in smoothness.scrolling_tough_ad_cases caused by the render
    thread being unresponsive.
    
    Patch 2: https://codereview.chromium.org/1296993002/
    Patch 3: https://codereview.chromium.org/1291513004/
    Patch 4: https://codereview.chromium.org/1285133008/
    
    BUG=510398
    
    Review URL: https://codereview.chromium.org/1299643004
    
    Cr-Commit-Position: refs/heads/master@{#343878}

--- content/browser/devtools/protocol/input_handler.h
+++ content/browser/devtools/protocol/input_handler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "content/browser/devtools/protocol/devtools_protocol_dispatcher.h"
 #include "content/browser/renderer_host/input/synthetic_gesture.h"
+#include "content/common/input/synthetic_smooth_scroll_gesture_params.h"
 #include "ui/gfx/geometry/size_f.h"
 
 namespace cc {
@@ -76,6 +77,7 @@ class InputHandler {
                                   const int* relative_speed,
                                   const std::string* gesture_source_type);
 
+  // TODO(alexclarke): remove this once the protocol patch lands.
   Response SynthesizeScrollGesture(DevToolsCommandId command_id,
                                    int x,
                                    int y,
@@ -86,6 +88,20 @@ class InputHandler {
                                    const bool* prevent_fling,
                                    const int* speed,
                                    const std::string* gesture_source_type);
+
+  Response SynthesizeScrollGesture(DevToolsCommandId command_id,
+                                   int x,
+                                   int y,
+                                   const int* x_distance,
+                                   const int* y_distance,
+                                   const int* x_overscroll,
+                                   const int* y_overscroll,
+                                   const bool* prevent_fling,
+                                   const int* speed,
+                                   const std::string* gesture_source_type,
+                                   const int* repeat_count,
+                                   const int* repeat_delay_ms,
+                                   const std::string* interaction_marker_name);
 
   Response SynthesizeTapGesture(DevToolsCommandId command_id,
                                 int x,
@@ -104,6 +120,20 @@ class InputHandler {
   void SendSynthesizeTapGestureResponse(DevToolsCommandId command_id,
                                         bool send_success,
                                         SyntheticGesture::Result result);
+
+  void SynthesizeRepeatingScroll(
+      SyntheticSmoothScrollGestureParams gesture_params,
+      int repeat_count,
+      base::TimeDelta repeat_delay,
+      std::string interaction_marker_name,
+      DevToolsCommandId command_id);
+
+  void OnScrollFinished(SyntheticSmoothScrollGestureParams gesture_params,
+                        int repeat_count,
+                        base::TimeDelta repeat_delay,
+                        std::string interaction_marker_name,
+                        DevToolsCommandId command_id,
+                        SyntheticGesture::Result result);
 
   RenderWidgetHostImpl* host_;
   scoped_ptr<Client> client_;
