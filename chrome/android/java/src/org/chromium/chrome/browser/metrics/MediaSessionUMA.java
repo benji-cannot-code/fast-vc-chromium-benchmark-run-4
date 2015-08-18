241e588784e078187d20fc8e218ad0e81e2e7687
mlamouri <mlamouri@chromium.org>
2015-08-18 16:13:17 -0700

    Android Media Notifications: add UMA for play/pause from notification.
    This is adding a bare minimum in order to be able to cherry-pick to the
    beta branch. More UMA will be added on trunk.
    
    BUG=521537
    
    Review URL: https://codereview.chromium.org/1296183003
    
    Cr-Commit-Position: refs/heads/master@{#344068}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/metrics/MediaSessionUMA.java
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.metrics;
+
+import org.chromium.base.metrics.RecordHistogram;
+
+/**
+ * Centralizes UMA data collection for Android-specific MediaSession features.
+ */
+public class MediaSessionUMA {
+    // MediaSessionAction defined in tools/metrics/histograms/histograms.xml.
+    public static final int MEDIA_SESSION_ACTION_SOURCE_MEDIA_NOTIFICATION = 0;
+    public static final int MEDIA_SESSION_ACTION_SOURCE_MAX = 1;
+
+    public static void recordPlay(int action) {
+        assert action >= 0 && action < MEDIA_SESSION_ACTION_SOURCE_MAX;
+        RecordHistogram.recordEnumeratedHistogram("Media.Session.Play", action,
+                MEDIA_SESSION_ACTION_SOURCE_MAX);
+    }
+
+    public static void recordPause(int action) {
+        assert action >= 0 && action < MEDIA_SESSION_ACTION_SOURCE_MAX;
+        RecordHistogram.recordEnumeratedHistogram("Media.Session.Pause", action,
+                MEDIA_SESSION_ACTION_SOURCE_MAX);
+    }
+}
