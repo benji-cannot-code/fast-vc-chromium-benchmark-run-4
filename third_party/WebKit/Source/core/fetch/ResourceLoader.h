f1d79465205df9fee99f32208f01c5426e855e7f
horo@chromium.org <horo@chromium.org>
2015-08-18 15:15:43 +0000

    Don't use m_fallbackRequestForServiceWorker in ResourceLoader.
    We can use m_request instead.
    
    BUG=519825
    
    Review URL: https://codereview.chromium.org/1284383003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/ResourceLoader.h
+++ third_party/WebKit/Source/core/fetch/ResourceLoader.h
@@ -113,7 +113,6 @@ private:
 
     bool m_defersLoading;
     bool m_loadingMultipartContent;
-    OwnPtr<ResourceRequest> m_fallbackRequestForServiceWorker;
     ResourceRequest m_deferredRequest;
     ResourceLoaderOptions m_options;
 
