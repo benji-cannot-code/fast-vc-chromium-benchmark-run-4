8a5cade808ebd930400685d2a3378da212cf7b36
henrika <henrika@chromium.org>
2015-08-19 00:14:17 -0700

    Revert of MediaStreamManager and VideoCaptureMessageFilter minor cleanup (patchset #2 id:60001 of https://codereview.chromium.org/1293473002/ )
    Reason for revert:
    This patch seems to create JSON parse error on all WebRTC-in-Chrome bots.
    
    See e.g. https://build.chromium.org/p/chromium.webrtc/builders/Linux%20Tester/builds/6071
    
    Original issue's description:
    > MediaStreamManager and VideoCaptureMessageFilter minor cleanup
    >
    > MSM: rewrite an utility function for clarity
    > VCMF: style and dropping some consts.
    >
    > Committed: https://crrev.com/1cac67e2ff0f6469729162077f6e541a4fa5a7a9
    > Cr-Commit-Position: refs/heads/master@{#343992}
    
    TBR=ajose@chromium.org,dalecurtis@chromium.org,mcasas@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1300063003
    
    Cr-Commit-Position: refs/heads/master@{#344170}

--- content/renderer/media/video_capture_message_filter.cc
+++ content/renderer/media/video_capture_message_filter.cc
@@ -93,17 +93,18 @@ void VideoCaptureMessageFilter::OnChannelClosing() {
 
 VideoCaptureMessageFilter::~VideoCaptureMessageFilter() {}
 
-VideoCaptureMessageFilter::Delegate*
-VideoCaptureMessageFilter::find_delegate(int device_id) const {
+VideoCaptureMessageFilter::Delegate* VideoCaptureMessageFilter::find_delegate(
+    int device_id) const {
   Delegates::const_iterator i = delegates_.find(device_id);
-  return i != delegates_.end() ? i->second : nullptr;
+  return i != delegates_.end() ? i->second : NULL;
 }
 
-void VideoCaptureMessageFilter::OnBufferCreated(int device_id,
-                                                base::SharedMemoryHandle handle,
-                                                int length,
-                                                int buffer_id) {
-  Delegate* const delegate = find_delegate(device_id);
+void VideoCaptureMessageFilter::OnBufferCreated(
+    int device_id,
+    base::SharedMemoryHandle handle,
+    int length,
+    int buffer_id) {
+  Delegate* delegate = find_delegate(device_id);
   if (!delegate) {
     DLOG(WARNING) << "OnBufferCreated: Got video SHM buffer for a "
                      "non-existent or removed video capture.";
@@ -142,9 +143,10 @@ void VideoCaptureMessageFilter::OnBufferReceived(
                              params.mailbox_holder);
 }
 
-void VideoCaptureMessageFilter::OnBufferDestroyed(int device_id,
-                                                  int buffer_id) {
-  Delegate* const delegate = find_delegate(device_id);
+void VideoCaptureMessageFilter::OnBufferDestroyed(
+    int device_id,
+    int buffer_id) {
+  Delegate* delegate = find_delegate(device_id);
   if (!delegate) {
     DLOG(WARNING) << "OnBufferDestroyed: Instructed to free buffer for a "
         "non-existent or removed video capture.";
@@ -157,7 +159,7 @@ void VideoCaptureMessageFilter::OnBufferDestroyed(int device_id,
 void VideoCaptureMessageFilter::OnDeviceStateChanged(
     int device_id,
     VideoCaptureState state) {
-  Delegate* const delegate = find_delegate(device_id);
+  Delegate* delegate = find_delegate(device_id);
   if (!delegate) {
     DLOG(WARNING) << "OnDeviceStateChanged: Got video capture event for a "
         "non-existent or removed video capture.";
@@ -169,7 +171,7 @@ void VideoCaptureMessageFilter::OnDeviceStateChanged(
 void VideoCaptureMessageFilter::OnDeviceSupportedFormatsEnumerated(
     int device_id,
     const media::VideoCaptureFormats& supported_formats) {
-  Delegate* const delegate = find_delegate(device_id);
+  Delegate* delegate = find_delegate(device_id);
   if (!delegate) {
     DLOG(WARNING) << "OnDeviceFormatsEnumerated: unknown device";
     return;
@@ -180,7 +182,7 @@ void VideoCaptureMessageFilter::OnDeviceSupportedFormatsEnumerated(
 void VideoCaptureMessageFilter::OnDeviceFormatsInUseReceived(
     int device_id,
     const media::VideoCaptureFormats& formats_in_use) {
-  Delegate* const delegate = find_delegate(device_id);
+  Delegate* delegate = find_delegate(device_id);
   if (!delegate) {
     DLOG(WARNING) << "OnDeviceFormatInUse: unknown device";
     return;
