f4eacca28e8c827f713bf0b8b94658547534c7db
junov@chromium.org <junov@chromium.org>
2015-08-19 00:56:58 +0000

    Reland: 2D canvas: remain in deferred rendering mode with canvas to canvas drawImage
    If the source canvas is not animated, no need to exit deferred rendering
    because there is no risk of memory bloat due to snapshotting.
    
    Fixed reson for original revert: m_snapshotState was not initialized
    
    BUG=521001, 521732
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1289423003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200771 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.h
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.h
@@ -139,6 +139,7 @@ public:
     AffineTransform baseTransform() const;
 
     bool is3D() const;
+    bool isAnimated2D() const;
 
     bool hasImageBuffer() const { return m_imageBuffer; }
     void discardImageBuffer();
