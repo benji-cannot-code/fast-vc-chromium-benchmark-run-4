79601e40aff75d4fae0c210f16d12dc12311e25c
kalyan.kondapally <kalyan.kondapally@intel.com>
2015-08-19 11:40:21 -0700

    Remove unused member variable
    Review URL: https://codereview.chromium.org/1292703003
    
    Cr-Commit-Position: refs/heads/master@{#344283}

--- ui/ozone/platform/drm/gpu/crtc_controller.cc
+++ ui/ozone/platform/drm/gpu/crtc_controller.cc
@@ -17,7 +17,6 @@ CrtcController::CrtcController(const scoped_refptr<DrmDevice>& drm,
                                uint32_t crtc,
                                uint32_t connector)
     : drm_(drm),
-      overlay_plane_manager_(nullptr),
       crtc_(crtc),
       connector_(connector) {}
 
