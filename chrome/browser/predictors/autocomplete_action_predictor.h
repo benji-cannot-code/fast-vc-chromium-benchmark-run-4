e43e83014ea38179c3a41a3cdec5c0ed2999eab5
blundell <blundell@chromium.org>
2015-08-19 09:02:22 -0700

    Eliminate OMNIBOX_OPENED_URL notification and its last usage
    The usage of this notification is impeding iOS upstreaming (see bug for
    details). This CL eliminates the last listener by having the two senders of the
    notification instead call the listener directly. It additionally eliminates the
    notification itself.
    
    BUG=517885
    
    Review URL: https://codereview.chromium.org/1281933002
    
    Cr-Commit-Position: refs/heads/master@{#344240}

--- chrome/browser/predictors/autocomplete_action_predictor.h
+++ chrome/browser/predictors/autocomplete_action_predictor.h
@@ -115,6 +115,9 @@ class AutocompleteActionPredictor
   // abandoned.
   bool IsPrerenderAbandonedForTesting();
 
+  // Should be called when a URL is opened from the omnibox.
+  void OnOmniboxOpenedUrl(const OmniboxLog& log);
+
  private:
   friend class AutocompleteActionPredictorTest;
   friend class ::PredictorsHandler;
@@ -171,9 +174,6 @@ class AutocompleteActionPredictor
 
   // Removes rows from the database and caches that contain a URL in |rows|.
   void DeleteRowsWithURLs(const history::URLRows& rows);
-
-  // Called when NOTIFICATION_OMNIBOX_OPENED_URL is observed.
-  void OnOmniboxOpenedUrl(const OmniboxLog& log);
 
   // Adds and updates rows in the database and caches.
   void AddAndUpdateRows(
--- chrome/browser/ui/omnibox/chrome_omnibox_client.cc
+++ chrome/browser/ui/omnibox/chrome_omnibox_client.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bitmap_fetcher/bitmap_fetcher_service_factory.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/bookmarks/bookmark_stats.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/command_updater.h"
 #include "chrome/browser/extensions/api/omnibox/omnibox_api.h"
 #include "chrome/browser/net/predictor.h"
@@ -44,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/template_url_service.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_entry.h"
-#include "content/public/browser/notification_service.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/common/constants.h"
 #include "ui/base/window_open_disposition.h"
@@ -403,10 +401,8 @@ void ChromeOmniboxClient::OnRevert() {
 }
 
 void ChromeOmniboxClient::OnURLOpenedFromOmnibox(OmniboxLog* log) {
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_OMNIBOX_OPENED_URL,
-      content::Source<Profile>(profile_),
-      content::Details<OmniboxLog>(log));
+  predictors::AutocompleteActionPredictorFactory::GetForProfile(profile_)
+      ->OnOmniboxOpenedUrl(*log);
 }
 
 void ChromeOmniboxClient::OnBookmarkLaunched() {
