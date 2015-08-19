910af7b84b90715794fe1c470ffaaa0647fc118d
wnwen <wnwen@chromium.org>
2015-08-19 14:37:26 -0700

    Remove unused isCoveredByChildActivity from tabs.
    This was left over from the NavigationTransitions cleanup.
    
    BUG=370696
    
    Review URL: https://codereview.chromium.org/1293383004
    
    Cr-Commit-Position: refs/heads/master@{#344335}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/document/DocumentTabModelImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/document/DocumentTabModelImpl.java
@@ -409,12 +409,6 @@ public class DocumentTabModelImpl extends TabModelJniBridge implements DocumentT
     }
 
     @Override
-    public boolean isCoveredByChildActivity(int tabId) {
-        Entry entry = mEntryMap.get(tabId);
-        return entry == null ? false : entry.isCoveredByChildActivity;
-    }
-
-    @Override
     public void addInitializationObserver(InitializationObserver observer) {
         ThreadUtils.assertOnUiThread();
         mInitializationObservers.addObserver(observer);
@@ -450,13 +444,11 @@ public class DocumentTabModelImpl extends TabModelJniBridge implements DocumentT
         Entry currentEntry = mEntryMap.get(id);
         String currentUrl = tab.getUrl();
         boolean canGoBack = tab.canGoBack();
-        boolean isCoveredByChildActivity = tab.isCoveredByChildActivity();
         TabState state = tab.getState();
         if (currentEntry != null
                 && currentEntry.tabId == id
                 && TextUtils.equals(currentEntry.currentUrl, currentUrl)
                 && currentEntry.canGoBack == canGoBack
-                && currentEntry.isCoveredByChildActivity == isCoveredByChildActivity
                 && currentEntry.getTabState() == state
                 && !tab.isTabStateDirty()) {
             return;
@@ -469,7 +461,6 @@ public class DocumentTabModelImpl extends TabModelJniBridge implements DocumentT
         currentEntry.isDirty = true;
         currentEntry.currentUrl = currentUrl;
         currentEntry.canGoBack = canGoBack;
-        currentEntry.isCoveredByChildActivity = isCoveredByChildActivity;
         currentEntry.setTabState(state);
 
         // TODO(dfalcantara): This is different from how the normal Tab determines when to save its
@@ -722,7 +713,6 @@ public class DocumentTabModelImpl extends TabModelJniBridge implements DocumentT
                     DocumentEntry docEntry = new DocumentEntry();
                     docEntry.tabId = entry.tabId;
                     docEntry.canGoBack = entry.canGoBack;
-                    docEntry.isCoveredByChildActivity = entry.isCoveredByChildActivity;
 
                     entriesList.add(docEntry);
                 }
