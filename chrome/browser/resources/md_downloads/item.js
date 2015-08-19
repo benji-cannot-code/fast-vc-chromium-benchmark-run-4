eaf7f90def45a2f7dd737268289ce1c6a7f00bd4
dbeam <dbeam@chromium.org>
2015-08-18 18:59:26 -0700

    MD Downloads: only show "Today" or "Yesterday" when available
    Otherwise show the date
    
    R=groby@chromium.org
    BUG=520722
    
    Review URL: https://codereview.chromium.org/1295113004
    
    Cr-Commit-Position: refs/heads/master@{#344126}

--- chrome/browser/resources/md_downloads/item.js
+++ chrome/browser/resources/md_downloads/item.js
@@ -54,8 +54,9 @@ cr.define('downloads', function() {
       this.isIncognito_ = data.otr;
 
       // Danger-independent UI and controls.
-      this.ensureTextIs_(this.$.since, data.since_string);
-      this.ensureTextIs_(this.$.date, data.date_string);
+      var since = data.since_string;
+      this.ensureTextIs_(this.$.since, since);
+      this.ensureTextIs_(this.$.date, since ? '' : data.date_string);
 
       /** @const */ var isActive =
           data.state != downloads.States.CANCELLED &&
@@ -142,7 +143,8 @@ cr.define('downloads', function() {
     },
 
     /**
-     * Overwrite |el|'s textContent if it differs from |text|.
+     * Overwrite |el|'s textContent if it differs from |text|. This is done
+     * generally so quickly updating text can be copied via text selection.
      * @param {!Element} el
      * @param {string} text
      * @private
