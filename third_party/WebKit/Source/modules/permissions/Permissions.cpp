14b5369216ec74071c1fa9eacdc8034d0deb5729
lalitm@google.com <lalitm@google.com>
2015-08-19 16:50:08 +0000

    permissions: fix small mistakes in error messages
    BUG=(none)
    
    Review URL: https://codereview.chromium.org/1300143002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200833 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/permissions/Permissions.cpp
+++ third_party/WebKit/Source/modules/permissions/Permissions.cpp
@@ -109,7 +109,7 @@ ScriptPromise Permissions::request(ScriptState* scriptState, const Dictionary& r
 {
     WebPermissionClient* client = getClient(scriptState->executionContext());
     if (!client)
-        return ScriptPromise::rejectWithDOMException(scriptState, DOMException::create(InvalidStateError, "In its current state, the global scope can't query permissions."));
+        return ScriptPromise::rejectWithDOMException(scriptState, DOMException::create(InvalidStateError, "In its current state, the global scope can't request permissions."));
 
     TrackExceptionState exceptionState;
     PermissionDescriptor permission = NativeValueTraits<PermissionDescriptor>::nativeValue(scriptState->isolate(), rawPermission.v8Value(), exceptionState);
@@ -132,7 +132,7 @@ ScriptPromise Permissions::request(ScriptState* scriptState, const Vector<Dictio
 {
     WebPermissionClient* client = getClient(scriptState->executionContext());
     if (!client)
-        return ScriptPromise::rejectWithDOMException(scriptState, DOMException::create(InvalidStateError, "In its current state, the global scope can't query permissions."));
+        return ScriptPromise::rejectWithDOMException(scriptState, DOMException::create(InvalidStateError, "In its current state, the global scope can't request permissions."));
 
     TrackExceptionState exceptionState;
     OwnPtr<WebVector<WebPermissionType>> permissions = adoptPtr(new WebVector<WebPermissionType>(rawPermissions.size()));
@@ -164,7 +164,7 @@ ScriptPromise Permissions::revoke(ScriptState* scriptState, const Dictionary& ra
 {
     WebPermissionClient* client = getClient(scriptState->executionContext());
     if (!client)
-        return ScriptPromise::rejectWithDOMException(scriptState, DOMException::create(InvalidStateError, "In its current state, the global scope can't query permissions."));
+        return ScriptPromise::rejectWithDOMException(scriptState, DOMException::create(InvalidStateError, "In its current state, the global scope can't revoke permissions."));
 
     TrackExceptionState exceptionState;
     PermissionDescriptor permission = NativeValueTraits<PermissionDescriptor>::nativeValue(scriptState->isolate(), rawPermission.v8Value(), exceptionState);
