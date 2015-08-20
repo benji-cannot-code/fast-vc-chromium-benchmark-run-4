993a37d3cb5d38112fbf9d6f7b041a3468c6235d
yosin@chromium.org <yosin@chromium.org>
2015-08-20 15:34:47 +0000

    Move computeInlineBoxPosition() out from PositionAlgorithm template class
    This patch moves |computeInlineBoxPosition()| out from |PositionAlgorithm|
    template class to reduce |InlineBox| dependency in |PositionAlgorithm| class
    and simplify |PositionAlgorithm| class for improving code health.
    
    Following patch will move |computeInlineBoxPosition()| to "VisibleUnits.cpp".
    
    BUG=518738
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1295893006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200906 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/VisiblePosition.cpp
+++ third_party/WebKit/Source/core/editing/VisiblePosition.cpp
@@ -125,7 +125,7 @@ Position VisiblePosition::leftVisuallyDistinctCandidate() const
     TextDirection primaryDirection = primaryDirectionOf(*p.anchorNode());
 
     while (true) {
-        InlineBoxPosition boxPosition = p.computeInlineBoxPosition(m_affinity, primaryDirection);
+        InlineBoxPosition boxPosition = computeInlineBoxPosition(p, m_affinity, primaryDirection);
         InlineBox* box = boxPosition.inlineBox;
         int offset = boxPosition.offsetInBox;
         if (!box)
@@ -162,7 +162,7 @@ Position VisiblePosition::leftVisuallyDistinctCandidate() const
                     if (positionOnLeft.isNull())
                         return Position();
 
-                    InlineBox* boxOnLeft = positionOnLeft.computeInlineBoxPosition(m_affinity, primaryDirection).inlineBox;
+                    InlineBox* boxOnLeft = computeInlineBoxPosition(positionOnLeft, m_affinity, primaryDirection).inlineBox;
                     if (boxOnLeft && boxOnLeft->root() == box->root())
                         return Position();
                     return positionOnLeft;
@@ -284,7 +284,7 @@ Position VisiblePosition::rightVisuallyDistinctCandidate() const
     TextDirection primaryDirection = primaryDirectionOf(*p.anchorNode());
 
     while (true) {
-        InlineBoxPosition boxPosition = p.computeInlineBoxPosition(m_affinity, primaryDirection);
+        InlineBoxPosition boxPosition = computeInlineBoxPosition(p, m_affinity, primaryDirection);
         InlineBox* box = boxPosition.inlineBox;
         int offset = boxPosition.offsetInBox;
         if (!box)
@@ -321,7 +321,7 @@ Position VisiblePosition::rightVisuallyDistinctCandidate() const
                     if (positionOnRight.isNull())
                         return Position();
 
-                    InlineBox* boxOnRight = positionOnRight.computeInlineBoxPosition(m_affinity, primaryDirection).inlineBox;
+                    InlineBox* boxOnRight = computeInlineBoxPosition(positionOnRight, m_affinity, primaryDirection).inlineBox;
                     if (boxOnRight && boxOnRight->root() == box->root())
                         return Position();
                     return positionOnRight;
