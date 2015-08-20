620366837e1f826b967240d228d0e90da4cb1625
yosin@chromium.org <yosin@chromium.org>
2015-08-20 09:12:29 +0000

    Move a member function inRenderedText() out from PositionAlgorithm template class
    This patch moves |inRenderedText()| out from |PositionAlgorithm| template class
    as a preparation of moving |inRenderedText()| to "VisibileUnits.cpp" to reduce
    dependency of |LayoutObject| in "Position.cpp" for improving code health.
    
    BUG=518738
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1301643003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200890 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/Position.h
+++ third_party/WebKit/Source/core/editing/Position.h
@@ -192,7 +192,6 @@ public:
     PositionAlgorithm<Strategy> downstream(EditingBoundaryCrossingRule = CannotCrossEditingBoundary) const;
 
     bool isCandidate() const;
-    bool inRenderedText() const;
 
     InlineBoxPosition computeInlineBoxPosition(TextAffinity) const;
     InlineBoxPosition computeInlineBoxPosition(TextAffinity, TextDirection primaryDirection) const;
@@ -448,6 +447,11 @@ inline PositionInComposedTree fromPositionInDOMTree<EditingInComposedTreeStrateg
 // TODO(yosin) We should move |hasRenderedNonAnonymousDescendantsWithHeight|
 // to "VisibleUnits.cpp" to reduce |LayoutObject| dependency in "Position.cpp"
 bool hasRenderedNonAnonymousDescendantsWithHeight(LayoutObject*);
+
+// TODO(yosin) We should move |inRenderedText()| to "VisibleUnits.h" for
+// reduce dependency of |LayoutObject| in |Position| class.
+CORE_EXPORT bool inRenderedText(const Position&);
+CORE_EXPORT bool inRenderedText(const PositionInComposedTree&);
 
 } // namespace blink
 
