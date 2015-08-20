102d98093074d295dba896ff09d4e57edcad032e
yosin@chromium.org <yosin@chromium.org>
2015-08-20 08:07:22 +0000

    Move nodeIsUserSelectAll() out from PositionAlgorithm template class
    This patch moves |nodeIsUserSelectAll()| out from |PositionAlgorithm| template
    class to "EditingUtilities.cpp" to simplify |PositionAlgorithm| class for
    improving code health, since |nodeIsUserSelectAll()| doesn't depend on
    |Strategy| template parameter.
    
    BUG=518738
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1307523002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200882 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/VisibleUnits.cpp
+++ third_party/WebKit/Source/core/editing/VisibleUnits.cpp
@@ -1144,7 +1144,7 @@ VisiblePosition startOfParagraph(const VisiblePosition& c, EditingBoundaryCrossi
     Node* n = startNode;
     bool startNodeIsEditable = startNode->hasEditableStyle();
     while (n) {
-        if (boundaryCrossingRule == CannotCrossEditingBoundary && !Position::nodeIsUserSelectAll(n) && n->hasEditableStyle() != startNodeIsEditable)
+        if (boundaryCrossingRule == CannotCrossEditingBoundary && !nodeIsUserSelectAll(n) && n->hasEditableStyle() != startNodeIsEditable)
             break;
         if (boundaryCrossingRule == CanSkipOverEditingBoundary) {
             while (n && n->hasEditableStyle() != startNodeIsEditable)
@@ -1220,7 +1220,7 @@ VisiblePosition endOfParagraph(const VisiblePosition &c, EditingBoundaryCrossing
     Node* n = startNode;
     bool startNodeIsEditable = startNode->hasEditableStyle();
     while (n) {
-        if (boundaryCrossingRule == CannotCrossEditingBoundary && !Position::nodeIsUserSelectAll(n) && n->hasEditableStyle() != startNodeIsEditable)
+        if (boundaryCrossingRule == CannotCrossEditingBoundary && !nodeIsUserSelectAll(n) && n->hasEditableStyle() != startNodeIsEditable)
             break;
         if (boundaryCrossingRule == CanSkipOverEditingBoundary) {
             while (n && n->hasEditableStyle() != startNodeIsEditable)
