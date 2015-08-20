815ef4b1296e498a59fd6541e512274eef4db234
reveman <reveman@chromium.org>
2015-08-20 06:41:38 -0700

    Re-land: cc: Use worker context for one-copy tile initialization.
    This moves management of staging resources to
    OneCopyTileTaskWorkerPool class. This makes it possible
    to use a worker context to issue and detect when copy
    operations complete.
    
    BUG=490295
    TBR=jamesr@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1230203007
    
    Cr-Commit-Position: refs/heads/master@{#344490}

--- gpu/command_buffer/build_gles2_cmd_buffer.py
+++ gpu/command_buffer/build_gles2_cmd_buffer.py
@@ -4007,7 +4007,7 @@ _FUNCTION_INFO = {
   'OrderingBarrierCHROMIUM': {
     'impl_func': False,
     'gen_cmd': False,
-    'extension': True,
+    'extension': "CHROMIUM_miscellaneous",
     'chromium': True,
     'client_test': False,
   },
