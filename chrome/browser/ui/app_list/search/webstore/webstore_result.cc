43778d6b0ae7b5707f61036b68b89a46e48e1bf2
yawano <yawano@chromium.org>
2015-08-19 18:28:51 -0700

    App Launcher: use the general badging API in webstore result.
    Removes custom badging code and replace it with the general badging API.
    With the API, badge position of webstore icon is shifted a little to
    match the spec.
    
    BUG=516943
    TEST=none
    
    Review URL: https://codereview.chromium.org/1303473003
    
    Cr-Commit-Position: refs/heads/master@{#344398}

--- chrome/browser/ui/app_list/search/webstore/webstore_result.cc
+++ chrome/browser/ui/app_list/search/webstore/webstore_result.cc
@@ -31,32 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/url_util.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
-#include "ui/gfx/canvas.h"
-#include "ui/gfx/image/canvas_image_source.h"
-
-namespace {
-
-// BadgedImageSource adds a webstore badge to a webstore app icon.
-class BadgedIconSource : public gfx::CanvasImageSource {
- public:
-  BadgedIconSource(const gfx::ImageSkia& icon, const gfx::Size& icon_size)
-      : CanvasImageSource(icon_size, false), icon_(icon) {}
-
-  void Draw(gfx::Canvas* canvas) override {
-    canvas->DrawImageInt(icon_, 0, 0);
-    const gfx::ImageSkia& badge = *ui::ResourceBundle::GetSharedInstance().
-         GetImageSkiaNamed(IDR_WEBSTORE_ICON_16);
-    canvas->DrawImageInt(
-        badge, icon_.width() - badge.width(), icon_.height() - badge.height());
-  }
-
- private:
-  gfx::ImageSkia icon_;
-
-  DISALLOW_COPY_AND_ASSIGN(BadgedIconSource);
-};
-
-}  // namespace
 
 namespace app_list {
 
@@ -177,14 +151,15 @@ void WebstoreResult::SetDefaultDetails() {
 }
 
 void WebstoreResult::OnIconLoaded() {
+  // Set webstore badge.
+  SetBadgeIcon(*ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
+      IDR_WEBSTORE_ICON_16));
+
   // Remove the existing image reps since the icon data is loaded and they
   // need to be re-created.
   const std::vector<gfx::ImageSkiaRep>& image_reps = icon_.image_reps();
   for (size_t i = 0; i < image_reps.size(); ++i)
     icon_.RemoveRepresentation(image_reps[i].scale());
-  int icon_dimension = GetPreferredIconDimension();
-  gfx::Size icon_size(icon_dimension, icon_dimension);
-  icon_ = gfx::ImageSkia(new BadgedIconSource(icon_, icon_size), icon_size);
 
   SetIcon(icon_);
 }
