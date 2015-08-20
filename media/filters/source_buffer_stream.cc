fd1f6c5a3a9141aa398ac53c64b3c63fcd35aabc
servolk <servolk@chromium.org>
2015-08-19 17:36:12 -0700

    [Media,MSE] Fix a DCHECK when printing empty ranges
    RangeToString helper currently doesn't handle empty ranges correctly,
    it hits a DCHECK in SourceBufferRange::GetStartTimestamp.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1296923003
    
    Cr-Commit-Position: refs/heads/master@{#344383}

--- media/filters/source_buffer_stream.cc
+++ media/filters/source_buffer_stream.cc
@@ -70,6 +70,9 @@ base::TimeDelta kSeekToStartFudgeRoom() {
 
 // Helper method for logging, converts a range into a readable string.
 std::string RangeToString(const SourceBufferRange& range) {
+  if (range.size_in_bytes() == 0) {
+    return "[]";
+  }
   std::stringstream ss;
   ss << "[" << range.GetStartTimestamp().InSecondsF()
      << ";" << range.GetEndTimestamp().InSecondsF()
