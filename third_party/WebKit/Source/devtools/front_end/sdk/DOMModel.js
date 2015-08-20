8750b98df069efd637931c8e6788a3e3c3455a47
sergeyv@chromium.org <sergeyv@chromium.org>
2015-08-20 02:27:35 +0000

    Devtools: Create a separate mode to turn LayoutEditor on
    BUG=501896
    
    Review URL: https://codereview.chromium.org/1293793008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200872 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/DOMModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/DOMModel.js
@@ -1734,9 +1734,7 @@ WebInspector.DOMModel.prototype = {
     highlightDOMNodeForTwoSeconds: function(nodeId)
     {
         this.highlightDOMNode(nodeId);
-
-        if (!Runtime.experiments.isEnabled("layoutEditor"))
-            this._hideDOMNodeHighlightTimeout = setTimeout(WebInspector.DOMModel.hideDOMNodeHighlight.bind(WebInspector.DOMModel), 2000);
+        this._hideDOMNodeHighlightTimeout = setTimeout(WebInspector.DOMModel.hideDOMNodeHighlight.bind(WebInspector.DOMModel), 2000);
     },
 
     /**
@@ -1799,7 +1797,6 @@ WebInspector.DOMModel.prototype = {
             highlightConfig.eventTargetColor = WebInspector.Color.PageHighlight.EventTarget.toProtocolRGBA();
             highlightConfig.shapeColor = WebInspector.Color.PageHighlight.Shape.toProtocolRGBA();
             highlightConfig.shapeMarginColor = WebInspector.Color.PageHighlight.ShapeMargin.toProtocolRGBA();
-            highlightConfig.showLayoutEditor = Runtime.experiments.isEnabled("layoutEditor");
             highlightConfig.displayAsMaterial = Runtime.experiments.isEnabled("materialDesign");
         }
         return highlightConfig;
