0f9a30c8b467f4d636837eb0e695fd1c1161481d
pkotwicz <pkotwicz@chromium.org>
2015-08-20 13:12:13 -0700

    Make "Escape" only close the context menu if the menu was opened via the "Menu" key
    Pressing "Escape" only closes the context menu if the menu was opened via right
    clicking. This CL makes context menus opened via the "Menu" key behave the same.
    
    BUG=522601
    TEST=BookmarkBarViewTest24.*
    
    Review URL: https://codereview.chromium.org/1299303002
    
    Cr-Commit-Position: refs/heads/master@{#344573}

--- ui/views/controls/menu/menu_controller.cc
+++ ui/views/controls/menu/menu_controller.cc
@@ -1105,7 +1105,6 @@ bool MenuController::OnKeyDown(ui::KeyboardCode key_code) {
       if (hot_view) {
         hot_view->ShowContextMenu(hot_view->GetKeyboardContextMenuLocation(),
                                   ui::MENU_SOURCE_KEYBOARD);
-        return false;
       } else if (pending_state_.item->enabled() &&
                  pending_state_.item->GetRootMenuItem() !=
                      pending_state_.item) {
@@ -1117,7 +1116,6 @@ bool MenuController::OnKeyDown(ui::KeyboardCode key_code) {
         ShowContextMenu(pending_state_.item,
                         pending_state_.item->GetKeyboardContextMenuLocation(),
                         ui::MENU_SOURCE_KEYBOARD);
-        return false;
       }
       break;
     }
