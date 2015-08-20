27cac40c05bcbd44d0f90b956627c5b51a911488
horo@chromium.org <horo@chromium.org>
2015-08-20 07:19:27 +0000

    Add LayoutTests for the behavior of navigation redirect with Service Worker.
    This CL depends on https://codereview.chromium.org/1271733002/ (chromium) and https://codereview.chromium.org/1280733002/ (blink).
    
    BUG=518713,510650
    
    Review URL: https://codereview.chromium.org/1286153003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200881 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/navigation-redirect-scope1.php
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+if (isset($_GET['url'])) {
+  header("HTTP/1.1 302");
+  $url = $_GET['url'];
+  header("Location: $url");
+  exit;
+}
+?>
+<!DOCTYPE html>
+<script>
+  window.parent.postMessage(
+      {
+        id: 'last_url',
+        result: location.href
+      },
+      'http://127.0.0.1:8000');
+</script>
