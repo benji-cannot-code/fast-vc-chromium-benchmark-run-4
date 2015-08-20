e0d803f2ca11dedd2b63bd4fbf87fb687f9cf029
nasko <nasko@chromium.org>
2015-08-20 10:28:16 -0700

    Prepare browser plugin code for swapped out removal.
    When swapped out state is removed, there will be no longer creation of
    swapped out RenderViews, which browser plugin relies on. This CL makes
    the codepath conditional on the IsSwappedOutStateForbidden() result and
    makes the switch away from swapped out much cleaner.
    
    BUG=357747
    
    Review URL: https://codereview.chromium.org/1299323003
    
    Cr-Commit-Position: refs/heads/master@{#344528}

--- content/browser/browser_plugin/browser_plugin_guest.cc
+++ content/browser/browser_plugin/browser_plugin_guest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/child_process_security_policy_impl.h"
 #include "content/browser/compositor/surface_utils.h"
 #include "content/browser/frame_host/render_frame_host_impl.h"
+#include "content/browser/frame_host/render_frame_proxy_host.h"
 #include "content/browser/frame_host/render_widget_host_view_guest.h"
 #include "content/browser/loader/resource_dispatcher_host_impl.h"
 #include "content/browser/renderer_host/render_view_host_impl.h"
@@ -30,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/drag_messages.h"
 #include "content/common/host_shared_bitmap_manager.h"
 #include "content/common/input_messages.h"
+#include "content/common/site_isolation_policy.h"
 #include "content/common/view_messages.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_plugin_guest_manager.h"
@@ -129,8 +131,17 @@ int BrowserPluginGuest::GetGuestProxyRoutingID() {
   // |guest_proxy_routing_id_| and perform any necessary cleanup on Detach
   // to enable this.
   SiteInstance* owner_site_instance = owner_web_contents_->GetSiteInstance();
-  guest_proxy_routing_id_ =
-      GetWebContents()->CreateSwappedOutRenderView(owner_site_instance);
+  if (SiteIsolationPolicy::IsSwappedOutStateForbidden()) {
+    int proxy_routing_id =
+        GetWebContents()->GetFrameTree()->root()->render_manager()->
+            CreateRenderFrameProxy(owner_site_instance);
+    guest_proxy_routing_id_ = RenderFrameProxyHost::FromID(
+        owner_site_instance->GetProcess()->GetID(), proxy_routing_id)
+            ->GetRenderViewHost()->GetRoutingID();
+  } else {
+    guest_proxy_routing_id_ =
+        GetWebContents()->CreateSwappedOutRenderView(owner_site_instance);
+  }
 
   return guest_proxy_routing_id_;
 }
