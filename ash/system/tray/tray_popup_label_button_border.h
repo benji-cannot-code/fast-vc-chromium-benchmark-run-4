a10e93eec4269842cd202ffaf02eba69a49be314
glevin <glevin@chromium.org>
2015-08-21 15:55:38 -0700

    Fix hover highlight on Tray menu buttons
    BUG=517785
    TEST=Mouse hover over SystemTray menu's Sign Out button, or horizontal
    text buttons in submenus (e.g. Join other|Mobile|Settings in Network
    submenu).  The hover-over style should be a light grey square with a
    slightly darker border.  See Expected_focus.png in issue description.
    
    Review URL: https://codereview.chromium.org/1308853006
    
    Cr-Commit-Position: refs/heads/master@{#344894}

--- ash/system/tray/tray_popup_label_button_border.h
+++ ash/system/tray/tray_popup_label_button_border.h
@@ -19,7 +19,7 @@ class TrayPopupLabelButtonBorder : public views::LabelButtonAssetBorder {
   TrayPopupLabelButtonBorder();
   ~TrayPopupLabelButtonBorder() override;
 
-  // views::LabelButtonBorder:
+  // views::LabelButtonAssetBorder:
   void Paint(const views::View& view, gfx::Canvas* canvas) override;
 
  private:
