ee201624cdadb142c69cdbcb6e6ac47a0906221b
newt <newt@chromium.org>
2015-08-21 16:52:38 -0700

    Delete obsolete auto_login_infobar_delegate.
    This was once used on Android, but Android no longer uses
    autologin infobars.
    
    TBR=cpu@chromium.org
    BUG=422495
    
    Review URL: https://codereview.chromium.org/1305233002
    
    Cr-Commit-Position: refs/heads/master@{#344922}

--- components/infobars/core/infobar_delegate.h
+++ components/infobars/core/infobar_delegate.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "ui/base/window_open_disposition.h"
 
-class AutoLoginInfoBarDelegate;
 class ConfirmInfoBarDelegate;
 class InsecureContentInfoBarDelegate;
 class MediaStreamInfoBarDelegate;
@@ -112,7 +111,6 @@ class InfoBarDelegate {
   virtual void InfoBarDismissed();
 
   // Type-checking downcast routines:
-  virtual AutoLoginInfoBarDelegate* AsAutoLoginInfoBarDelegate();
   virtual ConfirmInfoBarDelegate* AsConfirmInfoBarDelegate();
   virtual InsecureContentInfoBarDelegate* AsInsecureContentInfoBarDelegate();
   virtual MediaStreamInfoBarDelegate* AsMediaStreamInfoBarDelegate();
