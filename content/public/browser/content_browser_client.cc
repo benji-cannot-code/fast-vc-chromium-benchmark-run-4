6e2e95ef58bb02cd5896e8271477f6a350568234
jmadill <jmadill@chromium.org>
2015-08-21 11:16:13 -0700

    Initialize the GPU shader disk cache in the browser.
    The disk cache allows us to load the compositor shaders on startup,
    saving quite a bit of time on platforms that support caching.
    
    BUG=510151
    
    Review URL: https://codereview.chromium.org/1274763003
    
    Cr-Commit-Position: refs/heads/master@{#344816}

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -292,6 +292,10 @@ std::string ContentBrowserClient::GetDefaultDownloadName() {
   return std::string();
 }
 
+base::FilePath ContentBrowserClient::GetShaderDiskCacheDirectory() {
+  return base::FilePath();
+}
+
 BrowserPpapiHost*
     ContentBrowserClient::GetExternalBrowserPpapiHost(int plugin_process_id) {
   return nullptr;
