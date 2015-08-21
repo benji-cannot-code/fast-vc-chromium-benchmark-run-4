f38760dcb39f52a0a8075b118e86b961b77c509f
droger <droger@chromium.org>
2015-08-21 09:03:57 -0700

    Cleanup dependencies in web_resource
    This CL removes the dependencies on channel_info,
    browser_process and content metrics.
    The goal of this work is to enable componentization of this code,
    so that it can be shared on iOS.
    
    notification_promo_helper is introduced so that code in //chrome
    does not have to explicitly pass all the arguments to the static
    methods in NotificationPromo.
    This will also avoid updating all the callsites when future CLs
    add even more arguments.
    
    BUG=371477
    TBR=sky
    
    Review URL: https://codereview.chromium.org/1288933002
    
    Cr-Commit-Position: refs/heads/master@{#344779}

--- chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ntp/new_tab_page_handler.h"
 #include "chrome/browser/ui/webui/ntp/new_tab_ui.h"
 #include "chrome/browser/web_resource/notification_promo.h"
+#include "chrome/browser/web_resource/notification_promo_helper.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
@@ -507,9 +508,10 @@ void NTPResourceCache::CreateNewTabHTML() {
   // Disable the promo if this is the first run, otherwise set the promo string
   // for display if there is a valid outstanding promo.
   if (first_run::IsChromeFirstRun()) {
-    NotificationPromo::HandleClosed(NotificationPromo::NTP_NOTIFICATION_PROMO);
+    web_resource::HandleNotificationPromoClosed(
+        NotificationPromo::NTP_NOTIFICATION_PROMO);
   } else {
-    NotificationPromo notification_promo;
+    NotificationPromo notification_promo(g_browser_process->local_state());
     notification_promo.InitFromPrefs(NotificationPromo::NTP_NOTIFICATION_PROMO);
     if (notification_promo.CanShow()) {
       load_time_data.SetString("notificationPromoText",
@@ -517,7 +519,7 @@ void NTPResourceCache::CreateNewTabHTML() {
       DVLOG(1) << "Notification promo:" << notification_promo.promo_text();
     }
 
-    NotificationPromo bubble_promo;
+    NotificationPromo bubble_promo(g_browser_process->local_state());
     bubble_promo.InitFromPrefs(NotificationPromo::NTP_BUBBLE_PROMO);
     if (bubble_promo.CanShow()) {
       load_time_data.SetString("bubblePromoText",
