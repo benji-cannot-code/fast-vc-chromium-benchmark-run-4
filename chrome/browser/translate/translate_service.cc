2762de00d5945323811270325f79bee72cab58ac
groby <groby@chromium.org>
2015-08-21 14:45:39 -0700

    [Translate] Add support for Field Trial for translate bubble.
    Adds both field trial support and a flag so users can force-disable
    participation in the field trial. Flags tag precedence over field
    trial.
    
    BUG=507442
    
    TEST=Run Chromium with a fresh profile and the command line flags below.
         Each time, load page in a foreign language (not system language)
    
         <no flags>
             Chromium should display a translate infobar
    
         --force-fieldtrials=TranslateNewUX/Enabled
             Chromium should display a translate bubble.
    
         --force-fieldtrials=TranslateNewUX/Enabled --disable-translate-new-ux
             Chromium should display a translate Infobar
    
          --enable-translate-new-ux
             Chromium should display a translate bubble.
    
    Review URL: https://codereview.chromium.org/1302833003
    
    Cr-Commit-Position: refs/heads/master@{#344869}

--- chrome/browser/translate/translate_service.cc
+++ chrome/browser/translate/translate_service.cc
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/metrics/field_trial.h"
 #include "base/prefs/pref_service.h"
 #include "base/strings/string_split.h"
+#include "base/strings/string_util.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
@@ -97,9 +99,15 @@ bool TranslateService::IsTranslateBubbleEnabled() {
 #if defined(USE_AURA)
   return true;
 #elif defined(OS_MACOSX)
-  // The bubble UX is experimental on Mac OS X.
-  return base::CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableTranslateNewUX);
+  const std::string group_name =
+      base::FieldTrialList::FindFullName("TranslateNewUX");
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kDisableTranslateNewUX))
+    return false;
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableTranslateNewUX))
+    return true;
+  return base::StartsWith(group_name, "Enabled", base::CompareCase::SENSITIVE);
 #else
   // The bubble UX is not implemented on other platforms.
   return false;
