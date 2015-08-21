cf65f791213da6e86d5ef9094aae1063d7855ce3
benwells <benwells@chromium.org>
2015-08-20 19:55:08 -0700

    Remove command line flag to install an ephemeral app.
    As ephemeral apps were never released and are being removed, this is no
    longer needed.
    
    BUG=517735
    
    Review URL: https://codereview.chromium.org/1291793005
    
    Cr-Commit-Position: refs/heads/master@{#344664}

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -187,7 +187,6 @@ extern const char kHostResolverRetryAttempts[];
 extern const char kIgnoreUrlFetcherCertRequests[];
 extern const char kIncognito[];
 extern const char kInstallChromeApp[];
-extern const char kInstallEphemeralAppFromWebstore[];
 extern const char kInstallSupervisedUserWhitelists[];
 extern const char kInstantProcess[];
 extern const char kInvalidationUseGCMChannel[];
