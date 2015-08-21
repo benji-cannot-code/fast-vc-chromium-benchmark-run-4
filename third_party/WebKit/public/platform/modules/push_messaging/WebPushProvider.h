e056dbe6ebc878f6f8eface614a02b3305698acf
yhirano@chromium.org <yhirano@chromium.org>
2015-08-21 04:35:35 +0000

    [PushMessaging] Use appopriate type parameters for WebCallbacks (3/3).
    This series of CLs fixes WebCallback parameter types used in PushMessaging.
    
     - WebPushSubscription* (ownership transferred)
      => PassOwnPtr<WebPushSubscription>
     - WebPushPermissionStatus* (ownership not transferred)
      => WebPushPermissionStatus
     - WebPushError* (ownership transferred)
      => const WebPushError&
    
    1/3: https://codereview.chromium.org/1304723002/
    2/3: https://codereview.chromium.org/1291023005/
    3/3: https://codereview.chromium.org/1302833004/
    
    BUG=493531
    
    Review URL: https://codereview.chromium.org/1302833004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200959 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/modules/push_messaging/WebPushProvider.h
+++ third_party/WebKit/public/platform/modules/push_messaging/WebPushProvider.h
@@ -8,43 +8,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "public/platform/WebCallbacks.h"
 #include "public/platform/WebPassOwnPtr.h"
-#include "public/platform/modules/push_messaging/WebPushError.h"
 #include "public/platform/modules/push_messaging/WebPushPermissionStatus.h"
 #include "public/platform/modules/push_messaging/WebPushSubscription.h"
 
 namespace blink {
 
 class WebServiceWorkerRegistration;
+struct WebPushError;
 struct WebPushSubscriptionOptions;
 
-class WebPushSubscriptionCallbacks : public WebCallbacks<WebPassOwnPtr<WebPushSubscription>, const WebPushError&> {
-public:
-    void onSuccess(WebPushSubscription* r)
-    {
-        onSuccess(adoptWebPtr(r));
-    }
-    void onError(WebPushError* e)
-    {
-        onError(*e);
-        delete e;
-    }
-    void onSuccess(WebPassOwnPtr<WebPushSubscription>) override {}
-    void onError(const WebPushError&) override {}
-};
-class WebPushPermissionStatusCallbacks : public WebCallbacks<WebPushPermissionStatus, const WebPushError&> {
-public:
-    void onSuccess(WebPushPermissionStatus* r)
-    {
-        onSuccess(*r);
-    }
-    void onError(WebPushError* e)
-    {
-        onError(*e);
-        delete e;
-    }
-    void onSuccess(WebPushPermissionStatus) override {}
-    void onError(const WebPushError&) override {}
-};
+using WebPushSubscriptionCallbacks = WebCallbacks<WebPassOwnPtr<WebPushSubscription>, const WebPushError&>;
+using WebPushPermissionStatusCallbacks = WebCallbacks<WebPushPermissionStatus, const WebPushError&>;
 using WebPushUnsubscribeCallbacks = WebCallbacks<bool, const WebPushError&>;
 
 class WebPushProvider {
