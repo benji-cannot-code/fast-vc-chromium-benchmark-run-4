27e0f5dc4c4b4841a2e6ecd5e2bc01ffc3a36df1
brucedawson <brucedawson@chromium.org>
2015-08-20 22:42:29 -0700

    Remove 1 ms rounding up of task delays on Windows
    Prior to a couple of attempts at fixing bug 487724 the message pump
    would sometimes end up spinning for up to a ms when waiting for a
    message's time to arrive. This was unintentional and non-obvious.
    
    The eventual fix was to round up the delay time to the next ms.
    Unfortunately this caused a regression in smoothness.top_25_smooth
    which has not been resolved (investigation was delayed due to some
    confusion about the results). Therefore this change reverts both
    fix attempts in order to leave time for a proper investigation and
    any necessary fixes.
    
    It is worth mentioning that the delay introduced by rounding up to 1
    ms is actually worse than expected on some operating systems. On Windows
    7 a call to Sleep(1) will, if the timer frequency is raised, return
    within a ms. On Windows 8.1 it will often take two ms to return, which
    increases the delay. If the timer frequency is at its default interval
    of 15.625 ms then the delay can be longer.
    
    BUG=487724,497536
    
    Review URL: https://codereview.chromium.org/1305873002
    
    Cr-Commit-Position: refs/heads/master@{#344688}

--- base/message_loop/message_pump_default.cc
+++ base/message_loop/message_pump_default.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/message_loop/message_pump_default.h"
 
-#include <algorithm>
-
 #include "base/logging.h"
 #include "base/threading/thread_restrictions.h"
 
@@ -55,13 +53,6 @@ void MessagePumpDefault::Run(Delegate* delegate) {
       event_.Wait();
     } else {
       TimeDelta delay = delayed_work_time_ - TimeTicks::Now();
-#if defined(OS_WIN)
-      // If the delay is greater than zero and under 1 ms we need to round up to
-      // 1 ms or else we will end up spinning until it counts down to zero
-      // because sub-ms waits aren't supported on Windows.
-      if (delay > TimeDelta())
-        delay = std::max(delay, TimeDelta::FromMilliseconds(1));
-#endif
       if (delay > TimeDelta()) {
         event_.TimedWait(delay);
       } else {
