9b5b9a6550eb28be4e99c95b8243a9cee3f6a709
jianli <jianli@chromium.org>
2015-08-21 15:31:08 -0700

    Show "storage almost full" message when a page is saved offline
    BUG=491352
    
    Review URL: https://codereview.chromium.org/1305883002
    
    Cr-Commit-Position: refs/heads/master@{#344885}

--- chrome/android/java/src/org/chromium/chrome/browser/offline_pages/OfflinePageBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offline_pages/OfflinePageBridge.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.offline_pages;
 
+import android.os.Environment;
+
 import org.chromium.base.ObserverList;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
@@ -23,6 +25,8 @@ import java.util.List;
  */
 @JNINamespace("offline_pages::android")
 public final class OfflinePageBridge {
+
+    private static final long STORAGE_ALMOST_FULL_THRESHOLD_BYTES = 10L * (1 << 20);  // 10M
 
     private long mNativeOfflinePageBridge;
     private boolean mIsNativeOfflinePageModelLoaded;
@@ -92,6 +96,15 @@ public final class OfflinePageBridge {
                     && BookmarksBridge.isEnhancedBookmarksEnabled();
         }
         return sIsEnabled;
+    }
+
+    /**
+     * Returns true if the stoarge is almost full which indicates that the user probably needs to
+     * free up some space.
+     */
+    public static boolean isStorageAlmostFull() {
+        return Environment.getExternalStorageDirectory().getUsableSpace()
+                < STORAGE_ALMOST_FULL_THRESHOLD_BYTES;
     }
 
     /**
