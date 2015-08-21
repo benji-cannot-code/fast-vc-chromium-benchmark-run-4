088090261d9e8e3d5ae8578c842be8434e9c84fd
nektar@chromium.org <nektar@chromium.org>
2015-08-21 00:24:31 +0000

    Fixed AXLayoutObject::getSelection() so that it doesn't return nullptr for anchor and focus objects.
    BUG=491027
    R=dtseng@chromium.org, dglazkov@chromium.org
    
    Review URL: https://codereview.chromium.org/1300113006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200941 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
@@ -1867,7 +1867,8 @@ AXObject::AXRange AXLayoutObject::selection() const
     // Find the closest node that has a corresponding AXObject.
     // This is because some nodes may be aria hidden or might not even have
     // a layout object if they are part of the shadow DOM.
-    while (anchorNode && !getUnignoredObjectFromNode(*anchorNode)) {
+    while (anchorNode
+        && !(anchorObject = getUnignoredObjectFromNode(*anchorNode))) {
         if (anchorNode->nextSibling())
             anchorNode = anchorNode->nextSibling();
         else
@@ -1880,7 +1881,8 @@ AXObject::AXRange AXLayoutObject::selection() const
     ASSERT(focusNode);
 
     RefPtrWillBeRawPtr<AXObject> focusObject = nullptr;
-    while (focusNode && !getUnignoredObjectFromNode(*focusNode)) {
+    while (focusNode
+        && !(focusObject = getUnignoredObjectFromNode(*focusNode))) {
         if (focusNode->previousSibling())
             focusNode = focusNode->previousSibling();
         else
