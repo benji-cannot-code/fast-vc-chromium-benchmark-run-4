efdb8141d8ca75309b58b8caf19e5c40cf164dab
yhirano <yhirano@chromium.org>
2015-08-20 20:17:09 -0700

    [PushMessaging] Use appopriate type parameters for WebCallbacks (2/3).
    This series of CLs fixes WebCallback parameter types used in PushMessaging.
    
     - WebPushSubscription* (ownership transferred)
      => PassOwnPtr<WebPushSubscription>
     - WebPushPermissionStatus* (ownership not transferred)
      => WebPushPermissionStatus
     - WebPushError* (ownership transferred)
      => const WebPushError&
    
    1/3: https://codereview.chromium.org/1304723002/
    2/3: https://codereview.chromium.org/1291023005/
    3/3: https://codereview.chromium.org/1302833004/
    
    BUG=493531
    
    Review URL: https://codereview.chromium.org/1291023005
    
    Cr-Commit-Position: refs/heads/master@{#344673}

--- content/child/push_messaging/push_provider.cc
+++ content/child/push_messaging/push_provider.cc
@@ -156,9 +156,8 @@ void PushProvider::OnSubscribeFromWorkerSuccess(
   if (!callbacks)
     return;
 
-  scoped_ptr<blink::WebPushSubscription> subscription(
-      new blink::WebPushSubscription(endpoint, curve25519dh));
-  callbacks->onSuccess(subscription.release());
+  callbacks->onSuccess(blink::adoptWebPtr(
+      new blink::WebPushSubscription(endpoint, curve25519dh)));
 
   subscription_callbacks_.Remove(request_id);
 }
@@ -170,10 +169,9 @@ void PushProvider::OnSubscribeFromWorkerError(int request_id,
   if (!callbacks)
     return;
 
-  scoped_ptr<blink::WebPushError> error(new blink::WebPushError(
+  callbacks->onError(blink::WebPushError(
       blink::WebPushError::ErrorTypeAbort,
       blink::WebString::fromUTF8(PushRegistrationStatusToString(status))));
-  callbacks->onError(error.release());
 
   subscription_callbacks_.Remove(request_id);
 }
@@ -213,9 +211,8 @@ void PushProvider::OnGetRegistrationSuccess(
   if (!callbacks)
     return;
 
-  scoped_ptr<blink::WebPushSubscription> subscription(
-      new blink::WebPushSubscription(endpoint, curve25519dh));
-  callbacks->onSuccess(subscription.release());
+  callbacks->onSuccess(blink::adoptWebPtr(
+      new blink::WebPushSubscription(endpoint, curve25519dh)));
 
   subscription_callbacks_.Remove(request_id);
 }
@@ -242,7 +239,7 @@ void PushProvider::OnGetPermissionStatusSuccess(
   if (!callbacks)
     return;
 
-  callbacks->onSuccess(&status);
+  callbacks->onSuccess(status);
 
   permission_status_callbacks_.Remove(request_id);
 }
@@ -262,10 +259,8 @@ void PushProvider::OnGetPermissionStatusError(
         "supported.";
   }
 
-  scoped_ptr<blink::WebPushError> web_error(new blink::WebPushError(
+  callbacks->onError(blink::WebPushError(
       error, blink::WebString::fromUTF8(error_message)));
-
-  callbacks->onError(web_error.release());
 
   permission_status_callbacks_.Remove(request_id);
 }
