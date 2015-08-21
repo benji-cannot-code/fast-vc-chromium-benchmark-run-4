e617f5e907ea2dafb43b2792b342447c71a97bd6
sigbjornf@opera.com <sigbjornf@opera.com>
2015-08-21 05:22:25 +0000

    CompositorWorker is now always on the heap.
    Consequently, update its IDL attribute to [GarbageCollected] and adjust
    the return type of its constructor.
    
    R=haraken
    BUG=522949
    
    Review URL: https://codereview.chromium.org/1309463002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200964 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/compositorworker/CompositorWorker.cpp
+++ third_party/WebKit/Source/modules/compositorworker/CompositorWorker.cpp
@@ -21,7 +21,7 @@ inline CompositorWorker::CompositorWorker(ExecutionContext* context)
 {
 }
 
-PassRefPtrWillBeRawPtr<CompositorWorker> CompositorWorker::create(ExecutionContext* context, const String& url, ExceptionState& exceptionState)
+CompositorWorker* CompositorWorker::create(ExecutionContext* context, const String& url, ExceptionState& exceptionState)
 {
     ASSERT(isMainThread());
     Document* document = toDocument(context);
@@ -29,9 +29,9 @@ PassRefPtrWillBeRawPtr<CompositorWorker> CompositorWorker::create(ExecutionConte
         exceptionState.throwDOMException(InvalidAccessError, "The context provided is invalid.");
         return nullptr;
     }
-    RefPtrWillBeRawPtr<CompositorWorker> worker = adoptRefWillBeNoop(new CompositorWorker(context));
+    CompositorWorker* worker = new CompositorWorker(context);
     if (worker->initialize(context, url, exceptionState))
-        return worker.release();
+        return worker;
     return nullptr;
 }
 
