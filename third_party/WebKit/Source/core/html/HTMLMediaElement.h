5572b64f96f759b3331ea2a0aefd3adf513ee634
fs@opera.com <fs@opera.com>
2015-08-21 14:09:58 +0000

    Drop unused showingTrackWithSameKind declaration from HTMLMediaElement
    Review URL: https://codereview.chromium.org/1304183003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200985 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLMediaElement.h
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.h
@@ -382,8 +382,6 @@ private:
     void executeDeferredLoad();
     void deferredLoadTimerFired(Timer<HTMLMediaElement>*);
 
-    HTMLTrackElement* showingTrackWithSameKind(HTMLTrackElement*) const;
-
     void markCaptionAndSubtitleTracksAsUnconfigured();
 
     // This does not check user gesture restrictions.
