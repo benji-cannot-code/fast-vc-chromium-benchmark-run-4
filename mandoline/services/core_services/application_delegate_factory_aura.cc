ec0e75c1bd59e01174cb71f5f4a0d51fc0c19246
ben <ben@chromium.org>
2015-08-20 19:37:34 -0700

    Merge Browser and DesktopUI.
    - Eliminates AndroidUI.
    - Eliminates Browser, folding into DesktopUI.
    
    Subsequent steps will be to shift this class up one level & rename to something better.
    
    R=sky@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/1308553003
    
    Cr-Commit-Position: refs/heads/master@{#344662}

--- mandoline/services/core_services/application_delegate_factory_aura.cc
+++ mandoline/services/core_services/application_delegate_factory_aura.cc
@@ -5,14 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mandoline/services/core_services/application_delegate_factory.h"
 
+#include "mandoline/ui/browser/browser_manager.h"
 #include "mandoline/ui/omnibox/omnibox_impl.h"
 
 namespace core_services {
 
 scoped_ptr<mojo::ApplicationDelegate> CreateApplicationDelegateAura(
     const std::string& url) {
-  return url == "mojo://omnibox/" ? make_scoped_ptr(new mandoline::OmniboxImpl)
-                                  : nullptr;
+  if (url == "mojo://browser/")
+    return make_scoped_ptr(new mandoline::BrowserManager);
+  else if (url == "mojo://omnibox/")
+    return make_scoped_ptr(new mandoline::OmniboxImpl);
+  return nullptr;
 }
 
 }  // namespace core_services
--- mandoline/services/core_services/core_services_application_delegate.cc
+++ mandoline/services/core_services/core_services_application_delegate.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/filesystem/file_system_app.h"
 #include "mandoline/services/core_services/application_delegate_factory.h"
 #include "mandoline/tab/web_view_application_delegate.h"
-#include "mandoline/ui/browser/browser_manager.h"
 #include "mojo/application/public/cpp/application_connection.h"
 #include "mojo/application/public/cpp/application_impl.h"
 #include "mojo/application/public/cpp/application_runner.h"
@@ -116,9 +115,7 @@ void CoreServicesApplicationDelegate::StartApplication(
   const std::string url = response->url;
 
   scoped_ptr<mojo::ApplicationDelegate> delegate;
-  if (url == "mojo://browser/") {
-    delegate.reset(new mandoline::BrowserManager);
-  } else if (url == "mojo://clipboard/") {
+  if (url == "mojo://clipboard/") {
     delegate.reset(new clipboard::ClipboardApplicationDelegate);
   } else if (url == "mojo://filesystem/") {
     delegate.reset(new filesystem::FileSystemApp);
