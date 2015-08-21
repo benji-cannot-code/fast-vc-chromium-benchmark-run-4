7f9325702d1a2bb9e16a7eefa6fad91dce4461f2
joleksy <joleksy@opera.com>
2015-08-21 06:10:06 -0700

    Allow default font size changing on Linux.
    Currently base rendering font size on Linux is hardcoded to 16.0 with no possibility of changing it. This patch add such possibility in RendererPreferences.
    
    Webkit part: https://codereview.chromium.org/570243002/
    
    BUG=
    
    Review URL: https://codereview.chromium.org/549303004
    
    Cr-Commit-Position: refs/heads/master@{#344746}

--- chrome/browser/renderer_preferences_util.cc
+++ chrome/browser/renderer_preferences_util.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_LINUX) || defined(OS_ANDROID)
 #include "ui/gfx/font_render_params.h"
+#include "ui/gfx/platform_font.h"
 #endif
 
 #if !defined(OS_ANDROID)
@@ -111,6 +112,12 @@ void UpdateFromSystemSettings(content::RendererPreferences* prefs,
   prefs->use_autohinter = params.autohinter;
   prefs->use_bitmaps = params.use_bitmaps;
   prefs->subpixel_rendering = params.subpixel_rendering;
+
+#if defined(OS_LINUX)
+  scoped_refptr<gfx::PlatformFont> default_font(
+      gfx::PlatformFont::CreateDefault());
+  prefs->default_font_size = default_font->GetFontSize();
+#endif  // OS_LINUX
 #endif
 
 #if !defined(OS_MACOSX)
--- content/renderer/render_view_linux.cc
+++ content/renderer/render_view_linux.cc
@@ -15,6 +15,8 @@ namespace content {
 
 namespace {
 
+const int kMaxDefaultFontSize = 999;
+
 SkPaint::Hinting RendererPreferencesToSkiaHinting(
     const RendererPreferences& prefs) {
   if (!prefs.should_antialias_text) {
@@ -63,6 +65,10 @@ void RenderViewImpl::UpdateFontRenderingFromRendererPrefs() {
       prefs.subpixel_rendering !=
       gfx::FontRenderParams::SUBPIXEL_RENDERING_NONE);
   WebFontRendering::setSubpixelPositioning(prefs.use_subpixel_positioning);
+  if (prefs.default_font_size > 0 &&
+      prefs.default_font_size <= kMaxDefaultFontSize) {
+    WebFontRendering::setDefaultFontSize(prefs.default_font_size);
+  }
 }
 
 }  // namespace content
