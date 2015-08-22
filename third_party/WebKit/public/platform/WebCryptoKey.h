1eeac42f5490cf46f88a24c566be49a1ac1c0da8
olli.raula@intel.com <olli.raula@intel.com>
2015-08-22 07:45:03 +0000

    move KeyUsageAllows to more logical place(part1)
    move KeyUsageAllows to the class where its data is. That way the 
    function and its contexts is much more understandable. 
    This is first part, second part can be found here:
    https://crrev.com/1181413009/
    
    Review URL: https://codereview.chromium.org/1184123007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201040 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebCryptoKey.h
+++ third_party/WebKit/public/platform/WebCryptoKey.h
@@ -121,6 +121,8 @@ public:
 
     BLINK_PLATFORM_EXPORT bool isNull() const;
 
+    BLINK_PLATFORM_EXPORT bool keyUsageAllows(const blink::WebCryptoKeyUsage) const;
+
 private:
     BLINK_PLATFORM_EXPORT void assign(const WebCryptoKey& other);
     BLINK_PLATFORM_EXPORT void reset();
