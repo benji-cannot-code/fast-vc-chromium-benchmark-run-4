d7061f7e7474366853a62ebc19534f9572b1f96f
yosin@chromium.org <yosin@chromium.org>
2015-08-22 02:45:55 +0000

    Move {next,previous}PositionOf() to EditingUtilities.{cpp,h} from Position.{cpp,h}
    This patch moves |{next,previous}PositionOf()| to "EditingUtilities.{cpp,h}"
    from "Position.{cpp,h}" to simplify "Position.{cpp,h}" for improving code
    health since |{next,previous}PositionOf()| don't depend of implementation of
    |PositionAlgorithm| template class.
    
    BUG=518738
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1310533002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201033 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/EditingUtilities.h
+++ third_party/WebKit/Source/core/editing/EditingUtilities.h
@@ -37,6 +37,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+enum class PositionMoveType {
+    CodePoint, // Move by a single code point.
+    Character, // Move to the next Unicode character break.
+    BackwardDeletion // Subject to platform conventions.
+};
+
 class Document;
 class Element;
 class HTMLBRElement;
@@ -48,7 +54,6 @@ class Node;
 class Range;
 class VisiblePosition;
 class VisibleSelection;
-
 
 // This file contains a set of helper functions used by the editing commands
 
@@ -177,6 +182,18 @@ inline Position lastPositionInOrAfterNode(Node* node)
 
 Position lastEditablePositionBeforePositionInRoot(const Position&, Node*);
 PositionInComposedTree lastEditablePositionBeforePositionInRoot(const PositionInComposedTree&, Node*);
+
+// Move up or down the DOM by one position.
+// Offsets are computed using layout text for nodes that have layoutObjects -
+// but note that even when using composed characters, the result may be inside
+// a single user-visible character if a ligature is formed.
+CORE_EXPORT Position previousPositionOf(const Position&, PositionMoveType);
+CORE_EXPORT Position nextPositionOf(const Position&, PositionMoveType);
+CORE_EXPORT PositionInComposedTree previousPositionOf(const PositionInComposedTree&, PositionMoveType);
+CORE_EXPORT PositionInComposedTree nextPositionOf(const PositionInComposedTree&, PositionMoveType);
+
+CORE_EXPORT int uncheckedPreviousOffset(const Node*, int current);
+CORE_EXPORT int uncheckedNextOffset(const Node*, int current);
 
 // comparision functions on Position
 
