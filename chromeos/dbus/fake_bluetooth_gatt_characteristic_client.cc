30f52a6fcb4089c7f727f18d69b3d9d5247b1677
beaufort.francois <beaufort.francois@gmail.com>
2015-08-24 13:09:37 -0700

    Fixed bluetooth read write not supported errors
    BUG=522847
    
    Review URL: https://codereview.chromium.org/1307153002
    
    Cr-Commit-Position: refs/heads/master@{#345158}

--- chromeos/dbus/fake_bluetooth_gatt_characteristic_client.cc
+++ chromeos/dbus/fake_bluetooth_gatt_characteristic_client.cc
@@ -150,7 +150,7 @@ void FakeBluetoothGattCharacteristicClient::ReadValue(
   }
 
   if (object_path.value() == heart_rate_control_point_path_) {
-    error_callback.Run("org.bluez.Error.ReadNotPermitted",
+    error_callback.Run("org.bluez.Error.NotPermitted",
                        "Reads of this value are not allowed");
     return;
   }
@@ -227,7 +227,7 @@ void FakeBluetoothGattCharacteristicClient::WriteValue(
   }
 
   if (object_path.value() != heart_rate_control_point_path_) {
-    error_callback.Run("org.bluez.Error.WriteNotPermitted",
+    error_callback.Run("org.bluez.Error.NotPermitted",
                        "Writes of this value are not allowed");
     return;
   }
