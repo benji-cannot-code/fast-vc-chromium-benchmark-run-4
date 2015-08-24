1db0ecfac45a0b89ffec018e4f56e4f6ab89b4c2
Hans Wennborg <hans@chromium.org>
2015-08-24 10:59:38 -0700

    Fix Clang style-plugin errors in memory_pressure component
    From http://build.chromium.org/p/chromium.fyi/builders/CrWinClang/builds/2796/steps/compile/logs/stdio:
    
    ..\..\components/memory_pressure/filtered_memory_pressure_calculator.h(31,3) :
    error: [chromium-style] Complex destructor has an inline body.
      ~FilteredMemoryPressureCalculator() {}
      ^
    ..\..\components/memory_pressure/filtered_memory_pressure_calculator.h(31,39) :
    error: [chromium-style] Overriding method must be marked with 'override' or 'final'.
      ~FilteredMemoryPressureCalculator() {}
                                          ^
                                           override
    ..\..\components/memory_pressure/direct_memory_pressure_calculator.h(41,45) :
    error: [chromium-style] Overriding method must be marked with 'override' or 'final'.
      virtual ~DirectMemoryPressureCalculator() {}
                                                ^
                                                 override
    
    BUG=82385, 520962
    TBR=chrisha
    
    Review URL: https://codereview.chromium.org/1314553003 .
    
    Cr-Commit-Position: refs/heads/master@{#345121}

--- components/memory_pressure/filtered_memory_pressure_calculator.cc
+++ components/memory_pressure/filtered_memory_pressure_calculator.cc
@@ -33,6 +33,9 @@ FilteredMemoryPressureCalculator::FilteredMemoryPressureCalculator(
       samples_taken_(false),
       cooldown_in_progress_(false) {}
 
+FilteredMemoryPressureCalculator::~FilteredMemoryPressureCalculator() {
+}
+
 FilteredMemoryPressureCalculator::MemoryPressureLevel
 FilteredMemoryPressureCalculator::CalculateCurrentPressureLevel() {
   base::TimeTicks now = tick_clock_->NowTicks();
