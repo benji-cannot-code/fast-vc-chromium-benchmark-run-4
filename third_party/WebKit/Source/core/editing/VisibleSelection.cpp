2295846b7c0fef13aae07a8b2c3cf9edd346d0dd
yosin@chromium.org <yosin@chromium.org>
2015-08-24 15:13:27 +0000

    Introduce createRange(const EphemeralRange&)
    This patch introduces |createRange(const EphemeralRange&)| to replace
    |Range::create()| with |EphemeralRange| patterns for simplify source code.
    
    This patch is a preparation of making selection to handle granularity for web
    component, http://crrev.com/1277863002
    
    BUG=513568
    TEST=n/a; No behavior changes
    
    Review URL: https://codereview.chromium.org/1315543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201062 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/VisibleSelection.cpp
+++ third_party/WebKit/Source/core/editing/VisibleSelection.cpp
@@ -243,10 +243,7 @@ bool VisibleSelection::intersectsNode(Node* node) const
 
 PassRefPtrWillBeRawPtr<Range> VisibleSelection::toNormalizedRange() const
 {
-    const EphemeralRange range = toNormalizedEphemeralRange();
-    if (range.isNull())
-        return nullptr;
-    return Range::create(range.document(), range.startPosition(), range.endPosition());
+    return createRange(toNormalizedEphemeralRange());
 }
 
 template <typename Strategy>
