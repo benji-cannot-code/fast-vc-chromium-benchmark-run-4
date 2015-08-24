632facaff2b3b03771af48836fad7399c3e4e947
gsennton <gsennton@chromium.org>
2015-08-24 08:55:55 -0700

    shouldOverrideUrlLoading should not be called on reloads in WebView
    Fix unintented behaviour where shouldOverrideUrlLoading is called for
    reloads (in earlier WebView versions shouldOverrideUrlLoading was not
    called for reloads and there are apps relying on this behaviour). This
    behaviour was introduced in https://codereview.chromium.org/1155713005/.
    
    BUG=522743
    
    Review URL: https://codereview.chromium.org/1308073003
    
    Cr-Commit-Position: refs/heads/master@{#345079}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -286,10 +286,6 @@ public class AwContents implements SmartClipProvider,
 
     private PostMessageSender mPostMessageSender;
 
-    // This flag indicates that ShouldOverrideUrlNavigation should be posted
-    // through the resourcethrottle. This is only used for popup windows.
-    private boolean mDeferredShouldOverrideUrlLoadingIsPendingForPopup;
-
     // This is a workaround for some qualcomm devices discarding buffer on
     // Activity restore.
     private boolean mInvalidateRootViewOnNextDraw;
@@ -501,7 +497,8 @@ public class AwContents implements SmartClipProvider,
             boolean ignoreNavigation = false;
             // Any navigation from loadUrl, goBack/Forward, or reload, are considered application
             // initiated and hence will not yield a shouldOverrideUrlLoading() callback.
-            if ((!isLoadUrl || isRedirect) && !isBackForward && !navigationParams.isPost) {
+            if ((!isLoadUrl || isRedirect) && !isBackForward && !isReload
+                    && !navigationParams.isPost) {
                 if (!mContentsClient.hasWebViewClient()) {
                     ignoreNavigation = AwContentsClient.sendBrowsingIntent(mContext, url,
                             navigationParams.hasUserGesture
@@ -1011,7 +1008,6 @@ public class AwContents implements SmartClipProvider,
     // Recap: supplyContentsForPopup() is called on the parent window's content, this method is
     // called on the popup window's content.
     private void receivePopupContents(long popupNativeAwContents) {
-        mDeferredShouldOverrideUrlLoadingIsPendingForPopup = true;
         // Save existing view state.
         final boolean wasAttached = mIsAttachedToWindow;
         final boolean wasViewVisible = mIsViewVisible;
