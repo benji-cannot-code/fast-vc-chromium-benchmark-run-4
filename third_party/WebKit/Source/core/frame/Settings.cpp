2e557e9291f84d7d011239303390a5388b56b083
jbroman@chromium.org <jbroman@chromium.org>
2015-08-24 17:00:07 +0000

    Inline default false setting for unifiedTextCheckerEnabled setting.
    The default has been false for at least two years now. Chromium can turn it on,
    but it's appropriate for the initial value to be inlined into Settings.in, like
    most of the default-off settings.
    
    Review URL: https://codereview.chromium.org/1288073003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201065 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/Settings.cpp
+++ third_party/WebKit/Source/core/frame/Settings.cpp
@@ -53,7 +53,6 @@ static EditingBehaviorType editingBehaviorTypeForPlatform()
     ;
 }
 
-static const bool defaultUnifiedTextCheckerEnabled = false;
 #if OS(WIN)
 static const bool defaultSelectTrailingWhitespaceEnabled = true;
 #else
