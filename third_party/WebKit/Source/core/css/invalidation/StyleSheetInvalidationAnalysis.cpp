d7a20ffb83ddcef0f9dbdbabcea740c19559f4c4
sigbjornf@opera.com <sigbjornf@opera.com>
2015-08-24 13:08:32 +0000

    Oilpan: fix build after r201058.
    TBR=oilpan-reviews
    BUG=522860
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1303953005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201060 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/invalidation/StyleSheetInvalidationAnalysis.cpp
+++ third_party/WebKit/Source/core/css/invalidation/StyleSheetInvalidationAnalysis.cpp
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 StyleSheetInvalidationAnalysis::StyleSheetInvalidationAnalysis(const TreeScope& treeScope, const WillBeHeapVector<RawPtrWillBeMember<StyleSheetContents>>& sheets)
-    : m_treeScope(treeScope)
+    : m_treeScope(&treeScope)
 {
     for (unsigned i = 0; i < sheets.size() && !m_dirtiesAllStyle; ++i)
         analyzeStyleSheet(sheets[i]);
@@ -140,7 +140,7 @@ void StyleSheetInvalidationAnalysis::analyzeStyleSheet(StyleSheetContents* style
             return;
     }
 
-    if (m_treeScope.rootNode().isShadowRoot()) {
+    if (m_treeScope->rootNode().isShadowRoot()) {
         if (hasDistributedRule(styleSheetContents))
             m_hasDistributedRules = true;
         return;
@@ -190,8 +190,8 @@ void StyleSheetInvalidationAnalysis::invalidateStyle()
 {
     ASSERT(!m_dirtiesAllStyle);
 
-    if (m_treeScope.rootNode().isShadowRoot()) {
-        ContainerNode* invalidationRoot = &m_treeScope.rootNode();
+    if (m_treeScope->rootNode().isShadowRoot()) {
+        ContainerNode* invalidationRoot = &m_treeScope->rootNode();
         if (m_hasDistributedRules)
             invalidationRoot = outermostShadowHost(*toShadowRoot(invalidationRoot));
         invalidationRoot->setNeedsStyleRecalc(SubtreeStyleChange, StyleChangeReasonForTracing::create(StyleChangeReason::StyleSheetChange));
@@ -200,7 +200,7 @@ void StyleSheetInvalidationAnalysis::invalidateStyle()
 
     if (m_idScopes.isEmpty() && m_classScopes.isEmpty())
         return;
-    Element* element = ElementTraversal::firstWithin(m_treeScope.document());
+    Element* element = ElementTraversal::firstWithin(m_treeScope->document());
     while (element) {
         if (elementMatchesSelectorScopes(element, m_idScopes, m_classScopes)) {
             element->setNeedsStyleRecalc(SubtreeStyleChange, StyleChangeReasonForTracing::create(StyleChangeReason::StyleSheetChange));
--- third_party/WebKit/Source/core/css/invalidation/StyleSheetInvalidationAnalysis.h
+++ third_party/WebKit/Source/core/css/invalidation/StyleSheetInvalidationAnalysis.h
@@ -51,7 +51,7 @@ private:
 
     void analyzeStyleSheet(StyleSheetContents*);
 
-    const TreeScope& m_treeScope;
+    RawPtrWillBeMember<const TreeScope> m_treeScope;
     HashSet<StringImpl*> m_idScopes;
     HashSet<StringImpl*> m_classScopes;
 
