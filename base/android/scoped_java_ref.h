0efa5af8b440d20dae6c293bdaa7a540dfa6c0b5
torne <torne@chromium.org>
2015-08-24 10:41:34 -0700

    Inline constructors/destructor for JavaRef.
    JavaRef<object> only contains a POD type (jobject); the destructor does
    nothing and the default constructor just NULL-initializes. Inline both.
    Also inline the non-default constructor when !DCHECK_IS_ON (but leave it
    out of line otherwise as it contains a nontrivial DCHECK).
    
    This saves 8736 bytes in the .text section as of the time of writing,
    but will save 40kB or more as we wrap more JNI method parameters in
    JavaRef.
    
    BUG=519562
    
    Review URL: https://codereview.chromium.org/1308413003
    
    Cr-Commit-Position: refs/heads/master@{#345114}

--- base/android/scoped_java_ref.h
+++ base/android/scoped_java_ref.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 #include "base/basictypes.h"
+#include "base/logging.h"
 
 namespace base {
 namespace android {
@@ -46,14 +47,21 @@ class BASE_EXPORT JavaRef<jobject> {
   bool is_null() const { return obj_ == NULL; }
 
  protected:
-  // Initializes a NULL reference.
-  JavaRef();
+  // Initializes a NULL reference. Don't add anything else here; it's inlined.
+  JavaRef() : obj_(NULL) {}
 
   // Takes ownership of the |obj| reference passed; requires it to be a local
   // reference type.
+#if DCHECK_IS_ON()
+  // Implementation contains a DCHECK; implement out-of-line when DCHECK_IS_ON.
   JavaRef(JNIEnv* env, jobject obj);
+#else
+  // Don't add anything else here; it's inlined.
+  JavaRef(JNIEnv* env, jobject obj) : obj_(obj) {}
+#endif
 
-  ~JavaRef();
+  // Don't add anything else here; it's inlined.
+  ~JavaRef() {}
 
   // The following are implementation detail convenience methods, for
   // use by the sub-classes.
