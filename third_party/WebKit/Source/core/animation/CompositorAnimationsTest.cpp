7c35c5e09ca653763e897a7905b03f530c2dcf76
jbroman@chromium.org <jbroman@chromium.org>
2015-08-24 16:25:52 +0000

    Remove testing::CloneToPassOwnPtr; it is unused.
    Review URL: https://codereview.chromium.org/1310453004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201064 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/CompositorAnimationsTest.cpp
+++ third_party/WebKit/Source/core/animation/CompositorAnimationsTest.cpp
@@ -62,7 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-using ::testing::CloneToPassOwnPtr;
 using ::testing::ExpectationSet;
 using ::testing::Ref;
 using ::testing::Return;
