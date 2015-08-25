f58fadcdc57bbdd01da082c7f5ee88333c697269
sashab@chromium.org <sashab@chromium.org>
2015-08-25 09:10:47 +0000

    Remove default case from CSSValue::equals
    Remove the default case from the CSSValue::equals method, to enable the
    compiler to check that every enum value is in the switch statement.
    
    Review URL: https://codereview.chromium.org/1311843003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201114 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSValue.cpp
+++ third_party/WebKit/Source/core/css/CSSValue.cpp
@@ -93,7 +93,7 @@ inline static bool compareCSSValues(const CSSValue& first, const CSSValue& secon
 bool CSSValue::equals(const CSSValue& other) const
 {
     if (m_classType == other.m_classType) {
-        switch (m_classType) {
+        switch (classType()) {
         case BorderImageSliceClass:
             return compareCSSValues<CSSBorderImageSliceValue>(*this, other);
         case CanvasClass:
@@ -150,10 +150,9 @@ bool CSSValue::equals(const CSSValue& other) const
             return compareCSSValues<CSSSVGDocumentValue>(*this, other);
         case CSSContentDistributionClass:
             return compareCSSValues<CSSContentDistributionValue>(*this, other);
-        default:
-            ASSERT_NOT_REACHED();
-            return false;
         }
+        ASSERT_NOT_REACHED();
+        return false;
     }
     return false;
 }
