8e9864ec5bd447c97f05b5dce5b26ddbfb38ed3d
yosin@chromium.org <yosin@chromium.org>
2015-08-25 10:39:12 +0000

    Make WebLocalFrameImpl to use VisibleSelection::toNormalizedEphemeralRange() instead of toNormalizedRange()
    This patch changes |WebLocalFrameImpl| class to use |toNormalizedEphemeralRange()|
    in |VisibleSelection| class instead of |toNormalizedRange()| as a preparation
    of templatizing |VisibleSelection| to use templatized positions instead of
    DOM position to represent range.
    
    This patch is a preparation of making selection to handle granularity for web
    component, http://crrev.com/1277863002
    
    BUG=513568
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1313013002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201123 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
@@ -1241,7 +1241,7 @@ bool WebLocalFrameImpl::hasSelection() const
 
 WebRange WebLocalFrameImpl::selectionRange() const
 {
-    return frame()->selection().toNormalizedRange();
+    return createRange(frame()->selection().selection().toNormalizedEphemeralRange());
 }
 
 WebString WebLocalFrameImpl::selectionAsText() const
