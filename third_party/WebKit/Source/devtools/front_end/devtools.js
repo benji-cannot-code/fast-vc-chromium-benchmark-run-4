b7dc48fc90e36af9ac160bf14dcb9b82ce00e8cf
dgozman@chromium.org <dgozman@chromium.org>
2015-08-25 05:44:30 +0000

    [DevTools] Fix one more backwards compatibility breakage.
    With recent changes, it's dangerous to inject /deep/ selectors
    to the page, as they get high priority. This patch stops injecting
    backwards compatilibity code to the all-new frontned.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1310653003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201106 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/devtools.js
+++ third_party/WebKit/Source/devtools/front_end/devtools.js
@@ -783,6 +783,9 @@ window.InspectorFrontendHost = new InspectorFrontendHostImpl();
  */
 function installBackwardsCompatibility()
 {
+    if (window.location.search.indexOf("remoteFrontend") === -1)
+        return;
+
     /**
      * @this {CSSStyleDeclaration}
      */
