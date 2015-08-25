45ddbdb009418ab712960ee855098b81c5622b60
aberent <aberent@chromium.org>
2015-08-25 08:37:29 -0700

    Remove --disable-cast flag
    BUG=350354
    
    Review URL: https://codereview.chromium.org/1311673003
    
    Cr-Commit-Position: refs/heads/master@{#345335}

--- chrome/android/java/src/org/chromium/chrome/browser/media/remote/FullscreenMediaRouteButton.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/remote/FullscreenMediaRouteButton.java
@@ -41,8 +41,6 @@ public class FullscreenMediaRouteButton extends MediaRouteButton {
 
     @Override
     public void setEnabled(boolean enabled) {
-        if (!RemoteMediaPlayerController.isRemotePlaybackEnabled()) return;
-
         // TODO(aberent) not sure if this is still used, and in particular if mVisibilityRequest
         // is still used.
 
