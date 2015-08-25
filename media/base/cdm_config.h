bfc9564f2d44c2a38413fa308fe04b4791fa349a
hans <hans@chromium.org>
2015-08-25 15:09:26 -0700

    media/base/cdm_config.h: drop unused #include
    BUG=488634
    
    Review URL: https://codereview.chromium.org/1316733002
    
    Cr-Commit-Position: refs/heads/master@{#345451}

--- media/base/cdm_config.h
+++ media/base/cdm_config.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_BASE_CDM_CONFIG_H_
 #define MEDIA_BASE_CDM_CONFIG_H_
 
-#include "media/base/media_export.h"
-
 namespace media {
 
 // The runtime configuration for new CDM instances as computed by
