2dae7263a350128f4b905f1806e8cd440c491ddb
lazyboy@chromium.org <lazyboy@chromium.org>
2015-08-25 22:14:54 +0000

    On remote frame's (re)attachment, connect its widget.
    A remote frame can be detached by setting its .style.display = 'none'.
      If we attach it again by setting its .style.display = '', the frame
      becomes widget/FrameView-less. This CL sets the widget of the frame.
    
    BUG=517291
    Test=With --site-per-process, navigate an <iframe> to cross site.
      See that resizing the frame works correctly, e.g.:
        document.querySelector('iframe').style.width = '400px';
      Now hide the iframe, and then make it visible:
        document.querySelector('iframe').style.display = 'none';
        document.querySelector('iframe').style.display = '';
      See that resizing the frame still works correctly. It will fail
        without this CL.
    
    Review URL: https://codereview.chromium.org/1309343002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201161 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLFrameElementBase.cpp
+++ third_party/WebKit/Source/core/html/HTMLFrameElementBase.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/RemoteFrame.h"
+#include "core/frame/RemoteFrameView.h"
 #include "core/html/parser/HTMLParserIdioms.h"
 #include "core/layout/LayoutPart.h"
 #include "core/loader/FrameLoader.h"
@@ -171,6 +172,8 @@ void HTMLFrameElementBase::attach(const AttachContext& context)
         if (Frame* frame = contentFrame()) {
             if (frame->isLocalFrame())
                 setWidget(toLocalFrame(frame)->view());
+            else if (frame->isRemoteFrame())
+                setWidget(toRemoteFrame(frame)->view());
         }
     }
 }
