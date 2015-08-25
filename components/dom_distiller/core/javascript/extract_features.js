ca2b75ca0a286d4862fe05ce65538ece730a0013
wychen <wychen@chromium.org>
2015-08-25 11:28:22 -0700

    Stop feature extraction for reader mode when <body> isn't there yet
    BUG=495318
    
    Review URL: https://codereview.chromium.org/1251293002
    
    Cr-Commit-Position: refs/heads/master@{#345389}

--- components/dom_distiller/core/javascript/extract_features.js
+++ components/dom_distiller/core/javascript/extract_features.js
@@ -16,6 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   var body = document.body;
+  if (!body) {
+    return false;
+  }
   return JSON.stringify({
      'opengraph': hasOGArticle(),
      'url': document.location.href,
