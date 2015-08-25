bb6fac4736d08c8295624caceca3b1b739a9abbd
nhiroki <nhiroki@chromium.org>
2015-08-25 00:02:31 -0700

    ServiceWorker: Make APIs that return ServiceWorkerRegistration coin a new JS object (2/3)
    The spec requires register/getRegistration/getRegistrations to return a new
    registration object, but the current implementation can return existing one.
    This CL makes them always coin a new registration object.
    
    Spec: https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html
    
    (1) Blink: https://codereview.chromium.org/1311113002/
    (2) Chromium: THIS PATCH
    (3) Blink: https://codereview.chromium.org/1311103002/
    
    BUG=523904
    
    Review URL: https://codereview.chromium.org/1307133003
    
    Cr-Commit-Position: refs/heads/master@{#345277}

--- content/browser/service_worker/service_worker_provider_host.cc
+++ content/browser/service_worker/service_worker_provider_host.cc
@@ -624,7 +624,7 @@ void ServiceWorkerProviderHost::SendAssociateRegistrationMessage() {
     return;
 
   ServiceWorkerRegistrationHandle* handle =
-      dispatcher_host_->GetOrCreateRegistrationHandle(
+      dispatcher_host_->CreateRegistrationHandle(
           AsWeakPtr(), associated_registration_.get());
 
   ServiceWorkerVersionAttributes attrs;
