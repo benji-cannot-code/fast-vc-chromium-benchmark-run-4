a51f186df9b248b3afe37f3c254b2a8598f3a642
pkasting <pkasting@chromium.org>
2015-08-24 18:06:16 -0700

    Pull new webrtc.
    BUG=none
    TEST=none
    TBR=sergeyu
    
    Review URL: https://codereview.chromium.org/1312913002
    
    Cr-Commit-Position: refs/heads/master@{#345231}

--- content/renderer/media/webrtc/webrtc_local_audio_track_adapter_unittest.cc
+++ content/renderer/media/webrtc/webrtc_local_audio_track_adapter_unittest.cc
@@ -26,7 +26,7 @@ class MockWebRtcAudioSink : public webrtc::AudioTrackSinkInterface {
                             int bits_per_sample,
                             int sample_rate,
                             int number_of_channels,
-                            int number_of_frames));
+                            size_t number_of_frames));
 };
 
 }  // namespace
