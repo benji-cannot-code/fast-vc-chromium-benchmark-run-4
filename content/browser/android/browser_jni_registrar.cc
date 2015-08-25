83ac4307c810aa85aeee9086fe7115c993f0d849
bajones <bajones@chromium.org>
2015-08-25 09:59:52 -0700

    Android WebVR implementation, Cardboard 0.5.5 SDK
    Previously part of https://codereview.chromium.org/829803003/
    
    BUG=389343
    
    Committed: https://crrev.com/1d9c45547709e77d599c218d76317895389db129
    Cr-Commit-Position: refs/heads/master@{#342496}
    
    Review URL: https://codereview.chromium.org/1200303002
    
    Cr-Commit-Position: refs/heads/master@{#345356}

--- content/browser/android/browser_jni_registrar.cc
+++ content/browser/android/browser_jni_registrar.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/screen_orientation/screen_orientation_delegate_android.h"
 #include "content/browser/speech/speech_recognizer_impl_android.h"
 #include "content/browser/time_zone_monitor_android.h"
+#include "content/browser/vr/android/cardboard/cardboard_vr_device.h"
 #include "content/browser/web_contents/web_contents_android.h"
 #include "mojo/android/system/core_impl.h"
 
@@ -53,6 +54,10 @@ base::android::RegistrationMethod kContentRegisteredMethods[] = {
     {"BrowserAccessibilityManager",
      content::RegisterBrowserAccessibilityManager},
     {"BrowserStartupController", content::RegisterBrowserStartupController},
+#if defined(ENABLE_WEBVR)
+    {"CardboardVRDevice",
+     content::CardboardVRDevice::RegisterCardboardVRDevice},
+#endif
     {"ChildProcessLauncher", content::RegisterChildProcessLauncher},
     {"ContentReadbackHandler",
      content::ContentReadbackHandler::RegisterContentReadbackHandler},
--- content/content_browser.gypi
+++ content/content_browser.gypi
@@ -2153,5 +2153,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/vr/vr_device_provider.h',
       ]
     }],
+    ['enable_webvr==1 and OS=="android"', {
+      'sources': [
+        'browser/vr/android/cardboard/cardboard_vr_device.cc',
+        'browser/vr/android/cardboard/cardboard_vr_device.h',
+        'browser/vr/android/cardboard/cardboard_vr_device_provider.cc',
+        'browser/vr/android/cardboard/cardboard_vr_device_provider.h',
+      ],
+    }],
   ],
 }
