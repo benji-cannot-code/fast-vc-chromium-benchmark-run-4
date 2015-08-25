6febdcdc385dfabd59628a12e929a5fb2d2d2a7b
ssid <ssid@chromium.org>
2015-08-25 08:33:20 -0700

    Add support to create memory allocator dump in base::DiscardableMemory
    The clients that use discardable memory need to make a memory dump for
    tracing. But, the client do not know if the memory is purged or not. So,
    discardable manager dumps the live segments and the clients would dump
    all the allocations, without the knowledge from the manager. To make the
    memory dumps consistent, new api is added to the base::discardable
    memory interface to create a memory allocator. this method creates dump
    only if the memory is live according to the manager. Note: This still
    does not mean that the memory is resident in the system, and the OS
    could have purged this discardable segment. This method only helps to
    make the memory dumps consistent across all systems. Also, the clients
    may not keep track the size of each segment they allocate. So, this api
    adds the size when creating the dump. See doc https://goo.gl/AFSTRP.
    
    BUG=503168
    
    Review URL: https://codereview.chromium.org/1306243003
    
    Cr-Commit-Position: refs/heads/master@{#345332}

--- components/html_viewer/discardable_memory_allocator.cc
+++ components/html_viewer/discardable_memory_allocator.cc
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/discardable_memory.h"
 #include "base/stl_util.h"
+#include "base/trace_event/memory_allocator_dump.h"
+#include "base/trace_event/memory_dump_manager.h"
+#include "base/trace_event/process_memory_dump.h"
 
 namespace html_viewer {
 
@@ -52,6 +55,21 @@ class DiscardableMemoryAllocator::DiscardableMemoryChunkImpl
       return data_.get();
     }
     return nullptr;
+  }
+
+  base::trace_event::MemoryAllocatorDump* CreateMemoryAllocatorDump(
+      const char* name,
+      base::trace_event::ProcessMemoryDump* pmd) const override {
+    base::trace_event::MemoryAllocatorDump* dump =
+        pmd->CreateAllocatorDump(name);
+    dump->AddScalar(base::trace_event::MemoryAllocatorDump::kNameSize,
+                    base::trace_event::MemoryAllocatorDump::kUnitsBytes, size_);
+
+    // Memory is allocated from system allocator (malloc).
+    pmd->AddSuballocation(dump->guid(),
+                          base::trace_event::MemoryDumpManager::GetInstance()
+                              ->system_allocator_pool_name());
+    return dump;
   }
 
   size_t size() const { return size_; }
