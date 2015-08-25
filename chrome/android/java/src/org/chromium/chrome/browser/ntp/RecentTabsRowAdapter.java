acc7baf9be6ee975912353dfbf7fe121607f97de
pkotwicz <pkotwicz@chromium.org>
2015-08-25 10:41:10 -0700

    This CL: - Removes the unnecessary iconType parameter from   FaviconHelper#getLocalFaviconImageForURL() - Makes all callers of FaviconHelper#getLocalFaviconImageForURL() use   R.default_favicon_size to compute the desired favicon size
    BUG=524316
    TEST=None
    
    Review URL: https://codereview.chromium.org/1312013002
    
    Cr-Commit-Position: refs/heads/master@{#345368}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsRowAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsRowAdapter.java
@@ -669,7 +669,7 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
 
         Resources resources = activity.getResources();
         mDefaultFavicon = ApiCompatibilityUtils.getDrawable(resources, R.drawable.default_favicon);
-        mFaviconSize = mDefaultFavicon.getIntrinsicHeight();
+        mFaviconSize = resources.getDimensionPixelSize(R.dimen.default_favicon_size);
     }
 
     private static FaviconCache buildFaviconCache(int size) {
