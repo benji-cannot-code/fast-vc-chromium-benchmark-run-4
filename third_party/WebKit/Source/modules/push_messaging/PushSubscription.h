4f784ca4da6ea83c140bd2b27e382382def5ce99
peter@chromium.org <peter@chromium.org>
2015-08-25 17:48:23 +0000

    Change PushSubscription.curve25519dh to PushSubscription.getKey()
    Using a method is deemed more readable than having a weirdly named
    parameter. The "curve25519dh" value still needs to be passed in as the
    first and only argument of the getKey() method.
    
    This CL is part of a three-sided patch:
      [1] This CL.
      [2] https://codereview.chromium.org/1314893002/
      [3] https://codereview.chromium.org/1316693002/
    
    BUG=486040
    
    Review URL: https://codereview.chromium.org/1309723004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201141 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/push_messaging/PushSubscription.h
+++ third_party/WebKit/Source/modules/push_messaging/PushSubscription.h
@@ -33,6 +33,7 @@ public:
     KURL endpoint() const;
     PassRefPtr<DOMArrayBuffer> curve25519dh() const;
 
+    PassRefPtr<DOMArrayBuffer> getKey(const AtomicString& name) const;
     ScriptPromise unsubscribe(ScriptState*);
 
     ScriptValue toJSONForBinding(ScriptState*);
