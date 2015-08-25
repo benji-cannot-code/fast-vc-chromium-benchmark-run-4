bfece07e6aec0eaf98edd9eef0551f495beebd8e
yosin@chromium.org <yosin@chromium.org>
2015-08-25 14:52:06 +0000

    Make DOMSelection to use VisibleSelection::toNormalizedEphemeralRange() instead of toNormalizedRange()
    This patch changes |DOMSelection| class to use |toNormalizedEphemeralRange()|
    in |VisibleSelection| class instead of |toNormalizedRange()| as a preparation
    of templatizing |VisibleSelection| to use templatized positions instead of DOM
    position to represent range.
    
    This patch is a preparation of making selection to handle granularity for web
    component, http://crrev.com/1277863002
    
    BUG=513568
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1315813002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201133 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/DOMSelection.cpp
+++ third_party/WebKit/Source/core/editing/DOMSelection.cpp
@@ -446,7 +446,7 @@ void DOMSelection::deleteFromDocument()
     if (selection.isNone())
         return;
 
-    RefPtrWillBeRawPtr<Range> selectedRange = selection.selection().toNormalizedRange();
+    RefPtrWillBeRawPtr<Range> selectedRange = createRange(selection.selection().toNormalizedEphemeralRange());
     if (!selectedRange)
         return;
 
