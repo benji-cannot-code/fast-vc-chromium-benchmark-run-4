1e8fc91bc6a327620e500867f1fa91a45e38df07
bajones <bajones@chromium.org>
2015-08-25 15:59:22 -0700

    Make the WebGL conformance tests run in 'quiet mode' to reduce runtime
    I found that WebGL conformance runtime on a Retina Mac could be reduced by
    ~3 minutes by preventing the tests from constantly updating the DOM with
    "PASSED" messages, which the test harness doesn't pay any attention to anyway.
    
    BUG=522234
    
    Review URL: https://codereview.chromium.org/1308033005
    
    Cr-Commit-Position: refs/heads/master@{#345470}

--- content/test/gpu/gpu_tests/webgl_conformance.py
+++ content/test/gpu/gpu_tests/webgl_conformance.py
@@ -58,6 +58,7 @@ conformance_harness_script = r"""
     testHarness.reportResults(null, false, message);
     testHarness.notifyFinished(null);
   };
+  window.quietMode = function() { return true; }
 """
 
 def _DidWebGLTestSucceed(tab):
