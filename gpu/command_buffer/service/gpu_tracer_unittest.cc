71541913c44cd674085324e9297e7ec7423c720a
dyen <dyen@chromium.org>
2015-08-26 14:05:01 -0700

    Have GPUTracer process traces during CommandBuffer Idle time.
    ThreadTaskRunnerHandle is not always available to execute GPUTracer
    process commands (IE on android webview), switch to use the command
    buffer idle time instead.
    
    R=vmiura@chromium.org
    BUG=None
    TEST=trybots
    
    Review URL: https://codereview.chromium.org/1314053002
    
    Cr-Commit-Position: refs/heads/master@{#345672}

--- gpu/command_buffer/service/gpu_tracer_unittest.cc
+++ gpu/command_buffer/service/gpu_tracer_unittest.cc
@@ -77,13 +77,7 @@ class GPUTracerTester : public GPUTracer {
     return new MockOutputter();
   }
 
-  void PostTask() override {
-    // Process synchronously.
-    Process();
-  }
-
   unsigned char tracing_enabled_;
-
   scoped_refptr<Outputter> set_outputter_;
 };
 
@@ -427,7 +421,6 @@ class BaseGpuTracerTest : public BaseGpuTest {
       std::string source_trace_name = trace_name + num_char;
 
       const bool valid_timer = gpu_timing_client_->IsAvailable();
-
       const GpuTracerSource source = static_cast<GpuTracerSource>(i);
       ExpectOutputterEndMocks(outputter_ref_.get(), source, source_category,
                               source_trace_name, expect_start_time + i,
@@ -439,6 +432,7 @@ class BaseGpuTracerTest : public BaseGpuTest {
       ASSERT_TRUE(tracer.End(source));
     }
     ASSERT_TRUE(tracer.EndDecoding());
+    tracer.ProcessTraces();
     outputter_ref_ = NULL;
   }
 
@@ -514,6 +508,7 @@ class BaseGpuTracerTest : public BaseGpuTest {
         (5 * base::Time::kNanosecondsPerMicrosecond));
     g_fakeCPUTime = expect_start_time + 5;
     ASSERT_TRUE(tracer.EndDecoding());
+    tracer.ProcessTraces();
   }
 
   void DoDisjointTest() {
@@ -574,6 +569,7 @@ class BaseGpuTracerTest : public BaseGpuTest {
 
     ASSERT_TRUE(tracer.End(source));
     ASSERT_TRUE(tracer.EndDecoding());
+    tracer.ProcessTraces();
 
     outputter_ref_ = NULL;
   }
@@ -630,6 +626,7 @@ class BaseGpuTracerTest : public BaseGpuTest {
 
     ASSERT_TRUE(tracer.End(source));
     ASSERT_TRUE(tracer.EndDecoding());
+    tracer.ProcessTraces();
   }
 };
 
