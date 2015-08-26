0fbd129ffb76b4bcfeaa6a11763a5d0d06032b7c
trchen@chromium.org <trchen@chromium.org>
2015-08-26 06:08:35 +0000

    Remove transparency clip, use 'bound' in CompositingRecorder instead.
    The purpose of the transparency clip is to give Skia raster hints so that
    a transparent layer can be sized according to the bounding box of contents
    being composited. The clip itself should be no-op, but will actually cause
    anti-alias artifact for transformed contents.
    
    Removing the transparency clip and use compositing bound will allow Skia
    to take account of anti-aliasing to properly size the transparent layer.
    
    BUG=524859
    
    Review URL: https://codereview.chromium.org/1311633004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/paint/DisplayItem.h
+++ third_party/WebKit/Source/platform/graphics/paint/DisplayItem.h
@@ -126,7 +126,6 @@ public:
         ClipPopupListBoxFrame,
         ClipSelectionImage,
         PageWidgetDelegateClip,
-        TransparencyClip,
         ClipPrintedPage,
         ClipLast = ClipPrintedPage,
 
