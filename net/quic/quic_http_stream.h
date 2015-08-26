9d8fc4e3ff4c280f52f113de1e5bff448efed2d4
rtenneti <rtenneti@chromium.org>
2015-08-25 17:10:16 -0700

    QUIC - initialize closed_stream_received_bytes_ before setting stream_ to NULL.
    Moved the common code into a method and called it.
    
    BUG=522263
    R=sclittle@chromium.org
    TBR=rch@chromium.org
    
    Review URL: https://codereview.chromium.org/1312903003
    
    Cr-Commit-Position: refs/heads/master@{#345495}

--- net/quic/quic_http_stream.h
+++ net/quic/quic_http_stream.h
@@ -126,6 +126,8 @@ class NET_EXPORT_PRIVATE QuicHttpStream
 
   SpdyMajorVersion GetSpdyVersion();
 
+  void ResetStream();
+
   // TODO(rtenneti): Temporary while investigating crbug.com/468529
   void CrashIfInvalid() const;
 
