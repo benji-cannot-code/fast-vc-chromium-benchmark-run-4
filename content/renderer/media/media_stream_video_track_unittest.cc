226112ccbfc2daacdb7017b1fe19cfab98885e35
ajose <ajose@chromium.org>
2015-08-25 17:14:41 -0700

    Reland: Change VideoCaptureDeliverFrameCB's signature
    Relanding https://codereview.chromium.org/1304393004/
    
    Original description ---------------------------------------------------
    Change VideoCaptureDeliverFrameCB's signature from
    |const base::TimeTicks&| to |base::TimeTicks|.
    
    ------------------------------------------------------------------------
    
    TBR=dalecurtis@chromium.org, bbudge@chromium.org
    Rationale: already LGTMd PS1, new changes resulted
    from collision with mcasas@'s code while landing.
    BUG=524589, 524719
    
    Review URL: https://codereview.chromium.org/1315993003
    
    Cr-Commit-Position: refs/heads/master@{#345497}

--- content/renderer/media/media_stream_video_track_unittest.cc
+++ content/renderer/media/media_stream_video_track_unittest.cc
@@ -138,7 +138,7 @@ class CheckThreadHelper {
 void CheckThreadVideoFrameReceiver(
     CheckThreadHelper* helper,
     const scoped_refptr<media::VideoFrame>& frame,
-    const base::TimeTicks& estimated_capture_time) {
+    base::TimeTicks estimated_capture_time) {
   // Do nothing.
 }
 
