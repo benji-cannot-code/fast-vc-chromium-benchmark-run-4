c254f2a4c816856bd8222de4e6bc3c53647cd2bc
jbroman@chromium.org <jbroman@chromium.org>
2015-08-26 04:27:26 +0000

    Remove unused include of RetainPtr.h from PlatformSpeechSynthesizer.cpp.
    This file does not use RetainPtr.
    
    Review URL: https://codereview.chromium.org/1313733002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201195 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/speech/PlatformSpeechSynthesizer.cpp
+++ third_party/WebKit/Source/platform/speech/PlatformSpeechSynthesizer.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/WebSpeechSynthesisUtterance.h"
 #include "public/platform/WebSpeechSynthesizer.h"
 #include "public/platform/WebSpeechSynthesizerClient.h"
-#include "wtf/RetainPtr.h"
 
 namespace blink {
 
