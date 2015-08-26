7a6a4e336f21ecc39acb9090a00308f9844e5a55
fsamuel <fsamuel@chromium.org>
2015-08-26 16:47:20 -0700

    SurfaceFactory needs to know about CompositorFrame for scoped_ptr
    This was fine by accident because we included compositor_frame elsewhere.
    
    BUG=none
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1308113004
    
    Cr-Commit-Position: refs/heads/master@{#345722}

--- cc/surfaces/surface_factory.h
+++ cc/surfaces/surface_factory.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/scoped_ptr_hash_map.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "cc/output/compositor_frame.h"
 #include "cc/surfaces/surface_id.h"
 #include "cc/surfaces/surface_resource_holder.h"
 #include "cc/surfaces/surface_sequence.h"
@@ -22,7 +23,6 @@ class Size;
 }
 
 namespace cc {
-class CompositorFrame;
 class CopyOutputRequest;
 class Surface;
 class SurfaceFactoryClient;
