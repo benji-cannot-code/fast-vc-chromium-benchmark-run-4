d092b2a8036184b13f5d90ee65435d5b4e095005
jeremyim <jeremyim@chromium.org>
2015-08-26 16:50:02 -0700

    Remove TaskRunner from DataReductionProxyCompressionStats.
    Moving from a TaskRunner to a OneShotTimer makes the code a little
    simpler, and removes the TaskRunner reference from CompressionStats,
    which is the convention for DataReductionProxy leaf classes to
    make threading and object lifetime more manageable.
    
    BUG=472290
    
    Review URL: https://codereview.chromium.org/1295813007
    
    Cr-Commit-Position: refs/heads/master@{#345726}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_compression_stats.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_compression_stats.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/prefs/pref_change_registrar.h"
 #include "base/prefs/pref_service.h"
 #include "base/prefs/scoped_user_pref_update.h"
-#include "base/sequenced_task_runner.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
@@ -350,13 +349,10 @@ class DailyDataSavingUpdate {
 DataReductionProxyCompressionStats::DataReductionProxyCompressionStats(
     DataReductionProxyService* service,
     PrefService* prefs,
-    const scoped_refptr<base::SequencedTaskRunner>& task_runner,
     base::TimeDelta delay)
     : service_(service),
       pref_service_(prefs),
-      task_runner_(task_runner),
       delay_(delay),
-      delayed_task_posted_(false),
       pref_change_registrar_(new PrefChangeRegistrar()),
       data_usage_map_is_dirty_(false),
       data_usage_loaded_(NOT_LOADED),
@@ -548,8 +544,6 @@ void DataReductionProxyCompressionStats::WritePrefs() {
     TransferList(*(iter->second),
                  ListPrefUpdate(pref_service_, iter->first).Get());
   }
-
-  delayed_task_posted_ = false;
 }
 
 base::Value*
@@ -693,17 +687,11 @@ void DataReductionProxyCompressionStats::ClearDataSavingStatistics() {
 }
 
 void DataReductionProxyCompressionStats::DelayedWritePrefs() {
-  // Only write after the first time posting the task.
-  if (delayed_task_posted_)
+  if (pref_writer_timer_.IsRunning())
     return;
 
-  task_runner_->PostDelayedTask(
-      FROM_HERE,
-      base::Bind(&DataReductionProxyCompressionStats::WritePrefs,
-                 weak_factory_.GetWeakPtr()),
-      delay_);
-
-  delayed_task_posted_ = true;
+  pref_writer_timer_.Start(FROM_HERE, delay_, this,
+                           &DataReductionProxyCompressionStats::WritePrefs);
 }
 
 void DataReductionProxyCompressionStats::TransferList(
