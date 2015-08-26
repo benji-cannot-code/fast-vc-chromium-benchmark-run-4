0cd242f5f0146522a3f64f92ed4c52474cb19e97
yzshen <yzshen@chromium.org>
2015-08-26 15:13:17 -0700

    Mandoline: fix the issue of same-URL navigation.
    Sometimes, typing the same URL as the current one into the omnibox doesn't load the page again (but loses the previous load).
    
    This is actually caused by three bugs:
    - The identity used for application instance registration and lookup are actually constructed differently. So the application manager is confused. In some cases, it creates/registers multiple instances under the same identity.
    
    - HTMLDocumentApplicationDelegate doesn't retain the application while waiting for URL load completion. So if there is no other HTMLDocumentOOPIF instances are alive, the app will be shutdown.
    
    - When an app for "about:blank" already exists, it is reused and we end up in HTMLDocumentApplicationDelegate::ConfigureIncomingConnection(), trying to fetch the URL. We cannot fetch the URL using mojo::URLLoader because it is not an actual Web resource.
    
    BUG=524539
    
    Review URL: https://codereview.chromium.org/1313253002
    
    Cr-Commit-Position: refs/heads/master@{#345694}

--- mojo/shell/identity.cc
+++ mojo/shell/identity.cc
@@ -10,15 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 namespace shell {
 
-Identity::Identity(const GURL& url, const std::string& qualifier)
-    : url(GetBaseURLAndQuery(url, nullptr)),
-      qualifier(qualifier.empty() ? url.spec() : qualifier) {
-}
+Identity::Identity(const GURL& in_url, const std::string& in_qualifier)
+    : url(GetBaseURLAndQuery(in_url, nullptr)),
+      qualifier(in_qualifier.empty() ? url.spec() : in_qualifier) {}
 
 // explicit
-Identity::Identity(const GURL& base_url)
-    : url(GetBaseURLAndQuery(base_url, nullptr)), qualifier(url.spec()) {
-}
+Identity::Identity(const GURL& in_url)
+    : url(GetBaseURLAndQuery(in_url, nullptr)), qualifier(url.spec()) {}
 
 bool Identity::operator<(const Identity& other) const {
   if (url != other.url)
