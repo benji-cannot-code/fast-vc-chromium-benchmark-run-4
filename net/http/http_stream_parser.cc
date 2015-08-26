20a922d5766c8878e0d8c72e657e4d37df78ed4f
ellyjones <ellyjones@chromium.org>
2015-08-26 07:56:03 -0700

    HttpStreamParser: do lookaside status line validation
    Feed raw status lines into a strict, RFC7230-validating parser, and upload
    histograms for detected but correctable spec violations.
    
    BUG=486768
    
    Review URL: https://codereview.chromium.org/1286323003
    
    Cr-Commit-Position: refs/heads/master@{#345610}

--- net/http/http_stream_parser.cc
+++ net/http/http_stream_parser.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_request_headers.h"
 #include "net/http/http_request_info.h"
 #include "net/http/http_response_headers.h"
+#include "net/http/http_status_line_validator.h"
 #include "net/http/http_util.h"
 #include "net/socket/client_socket_handle.h"
 #include "net/socket/ssl_client_socket.h"
@@ -941,8 +942,11 @@ int HttpStreamParser::ParseResponseHeaders(int end_offset) {
 
   if (response_header_start_offset_ >= 0) {
     received_bytes_ += end_offset;
-    headers = new HttpResponseHeaders(HttpUtil::AssembleRawHeaders(
-        read_buf_->StartOfBuffer(), end_offset));
+    std::string raw_headers =
+        HttpUtil::AssembleRawHeaders(read_buf_->StartOfBuffer(), end_offset);
+    ValidateStatusLine(
+        std::string(read_buf_->StartOfBuffer(), raw_headers.find('\0')));
+    headers = new HttpResponseHeaders(raw_headers);
   } else {
     // Enough data was read -- there is no status line.
     headers = new HttpResponseHeaders(std::string("HTTP/0.9 200 OK"));
@@ -1125,6 +1129,13 @@ bool HttpStreamParser::ShouldMergeRequestHeadersAndBody(
       return true;
   }
   return false;
+}
+
+void HttpStreamParser::ValidateStatusLine(const std::string& status_line) {
+  HttpStatusLineValidator::StatusLineStatus status =
+      HttpStatusLineValidator::ValidateStatusLine(status_line);
+  UMA_HISTOGRAM_ENUMERATION("Net.HttpStatusLineStatus", status,
+                            HttpStatusLineValidator::STATUS_LINE_MAX);
 }
 
 }  // namespace net
