838b7b6aefc75edb4d6261065896a197e2519b07
nednguyen <nednguyen@google.com>
2015-08-26 12:39:10 -0700

    [Telemetry] Allow TimelineBasedMeasurement user to pick which timeline based metric to be used. (Reland)
    Make sure that smoothness benchmark only output Smoothness related metrics.
    
    BUG=507852
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1314183002
    
    Cr-Commit-Position: refs/heads/master@{#345642}

--- tools/telemetry/telemetry/web_perf/timeline_based_measurement.py
+++ tools/telemetry/telemetry/web_perf/timeline_based_measurement.py
@@ -2,13 +2,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
+import collections
 from collections import defaultdict
 
 from telemetry.timeline import model as model_module
 from telemetry.timeline import tracing_category_filter
 from telemetry.timeline import tracing_options
 from telemetry.value import trace
+from telemetry.web_perf.metrics import timeline_based_metric
 from telemetry.web_perf.metrics import blob_timeline
 from telemetry.web_perf.metrics import gpu_timeline
 from telemetry.web_perf.metrics import layout
@@ -108,11 +109,12 @@ def _GetRendererThreadsToInteractionRecordsMap(model):
 
 class _TimelineBasedMetrics(object):
   def __init__(self, model, renderer_thread, interaction_records,
-               results_wrapper):
+               results_wrapper, metrics):
     self._model = model
     self._renderer_thread = renderer_thread
     self._interaction_records = interaction_records
     self._results_wrapper = results_wrapper
+    self._all_metrics = metrics
 
   def AddResults(self, results):
     interactions_by_label = defaultdict(list)
@@ -132,7 +134,7 @@ class _TimelineBasedMetrics(object):
     if not interactions:
       return
 
-    for metric in _GetAllTimelineBasedMetrics():
+    for metric in self._all_metrics:
       metric.AddResults(self._model, self._renderer_thread,
                         interactions, wrapped_results)
 
@@ -142,6 +144,10 @@ class Options(object):
 
   This is created and returned by
   Benchmark.CreateTimelineBasedMeasurementOptions.
+
+  By default, all the timeline based metrics in telemetry/web_perf/metrics are
+  used (see _GetAllTimelineBasedMetrics above).
+  To customize your metric needs, use SetTimelineBasedMetrics().
   """
 
   def __init__(self, overhead_level=NO_OVERHEAD_LEVEL):
@@ -174,6 +180,7 @@ class Options(object):
     self._tracing_options = tracing_options.TracingOptions()
     self._tracing_options.enable_chrome_trace = True
     self._tracing_options.enable_platform_display_trace = True
+    self._timeline_based_metrics = _GetAllTimelineBasedMetrics()
 
 
   def ExtendTraceCategoryFilter(self, filters):
@@ -191,6 +198,15 @@ class Options(object):
   @tracing_options.setter
   def tracing_options(self, value):
     self._tracing_options = value
+
+  def SetTimelineBasedMetrics(self, metrics):
+    assert isinstance(metrics, collections.Iterable)
+    for m in metrics:
+      assert isinstance(m, timeline_based_metric.TimelineBasedMetric)
+    self._timeline_based_metrics = metrics
+
+  def GetTimelineBasedMetrics(self):
+    return self._timeline_based_metrics
 
 
 class TimelineBasedMeasurement(story_test.StoryTest):
@@ -269,7 +285,7 @@ class TimelineBasedMeasurement(story_test.StoryTest):
         threads_to_records_map.iteritems()):
       meta_metrics = _TimelineBasedMetrics(
           model, renderer_thread, interaction_records,
-          self._results_wrapper)
+          self._results_wrapper, self._tbm_options.GetTimelineBasedMetrics())
       meta_metrics.AddResults(results)
 
   def DidRunStoryForPageTest(self, tracing_controller):
