6e307ec05836080e62169311d3b6c685161b4de5
hashimoto <hashimoto@chromium.org>
2015-08-26 13:53:34 -0700

    Prevent base::debug::BreakDebugger() from being folded by the linker
    Linker's ICF feature may merge BreakDebugger() with other functions
    and it may confuse the crash report processor.
    
    BUG=508489
    
    Review URL: https://codereview.chromium.org/1316003004
    
    Cr-Commit-Position: refs/heads/master@{#345663}

--- base/debug/debugger_posix.cc
+++ base/debug/debugger_posix.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ostream>
 
 #include "base/basictypes.h"
+#include "base/debug/alias.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/posix/eintr_wrapper.h"
@@ -237,6 +238,12 @@ void DebugBreak() {
 void BreakDebugger() {
   // NOTE: This code MUST be async-signal safe (it's used by in-process
   // stack dumping signal handler). NO malloc or stdio is allowed here.
+
+  // Linker's ICF feature may merge this function with other functions with the
+  // same definition (e.g. any function whose sole job is to call abort()) and
+  // it may confuse the crash report processing system. http://crbug.com/508489
+  static int static_variable_to_make_this_function_unique = 0;
+  base::debug::Alias(&static_variable_to_make_this_function_unique);
 
   DEBUG_BREAK();
 #if defined(OS_ANDROID) && !defined(OFFICIAL_BUILD)
