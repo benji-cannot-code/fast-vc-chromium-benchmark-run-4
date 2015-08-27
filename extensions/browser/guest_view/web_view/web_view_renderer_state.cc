3a3b1d48cbb4f741b839061fa32d9fa1111cd0bf
paulmeyer <paulmeyer@chromium.org>
2015-08-27 10:48:01 -0700

    This patch allows WebViewRendererState to be accessible from both the UI and IO threads. This access is mediated by locks for the two internal maps in the class.
    This patch is tested by a sebsequent patch that depends on it: https://codereview.chromium.org/1312653003/
    
    Review URL: https://codereview.chromium.org/1313673003
    
    Cr-Commit-Position: refs/heads/master@{#345917}

--- extensions/browser/guest_view/web_view/web_view_renderer_state.cc
+++ extensions/browser/guest_view/web_view/web_view_renderer_state.cc
@@ -27,8 +27,8 @@ WebViewRendererState::WebViewRendererState() {
 WebViewRendererState::~WebViewRendererState() {
 }
 
-bool WebViewRendererState::IsGuest(int render_process_id) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+bool WebViewRendererState::IsGuest(int render_process_id) const {
+  base::AutoLock auto_lock(web_view_partition_id_map_lock_);
   return web_view_partition_id_map_.find(render_process_id) !=
          web_view_partition_id_map_.end();
 }
@@ -36,7 +36,9 @@ bool WebViewRendererState::IsGuest(int render_process_id) {
 void WebViewRendererState::AddGuest(int guest_process_id,
                                     int guest_routing_id,
                                     const WebViewInfo& web_view_info) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  base::AutoLock auto_lock(web_view_info_map_lock_);
+  base::AutoLock auto_lock2(web_view_partition_id_map_lock_);
+
   RenderId render_id(guest_process_id, guest_routing_id);
   bool updating =
       web_view_info_map_.find(render_id) != web_view_info_map_.end();
@@ -55,7 +57,9 @@ void WebViewRendererState::AddGuest(int guest_process_id,
 
 void WebViewRendererState::RemoveGuest(int guest_process_id,
                                        int guest_routing_id) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  base::AutoLock auto_lock(web_view_info_map_lock_);
+  base::AutoLock auto_lock2(web_view_partition_id_map_lock_);
+
   RenderId render_id(guest_process_id, guest_routing_id);
   web_view_info_map_.erase(render_id);
   auto iter = web_view_partition_id_map_.find(guest_process_id);
@@ -69,8 +73,9 @@ void WebViewRendererState::RemoveGuest(int guest_process_id,
 
 bool WebViewRendererState::GetInfo(int guest_process_id,
                                    int guest_routing_id,
-                                   WebViewInfo* web_view_info) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+                                   WebViewInfo* web_view_info) const {
+  base::AutoLock auto_lock(web_view_info_map_lock_);
+
   RenderId render_id(guest_process_id, guest_routing_id);
   auto iter = web_view_info_map_.find(render_id);
   if (iter != web_view_info_map_.end()) {
@@ -83,7 +88,8 @@ bool WebViewRendererState::GetInfo(int guest_process_id,
 bool WebViewRendererState::GetOwnerInfo(int guest_process_id,
                                         int* owner_process_id,
                                         std::string* owner_host) const {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  base::AutoLock auto_lock(web_view_info_map_lock_);
+
   // TODO(fsamuel): Store per-process info in WebViewPartitionInfo instead of in
   // WebViewInfo.
   for (const auto& info : web_view_info_map_) {
@@ -99,8 +105,8 @@ bool WebViewRendererState::GetOwnerInfo(int guest_process_id,
 }
 
 bool WebViewRendererState::GetPartitionID(int guest_process_id,
-                                          std::string* partition_id) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+                                          std::string* partition_id) const {
+  base::AutoLock auto_lock(web_view_partition_id_map_lock_);
 
   auto iter = web_view_partition_id_map_.find(guest_process_id);
   if (iter != web_view_partition_id_map_.end()){
@@ -114,7 +120,7 @@ void WebViewRendererState::AddContentScriptIDs(
     int embedder_process_id,
     int view_instance_id,
     const std::set<int>& script_ids) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  base::AutoLock auto_lock(web_view_info_map_lock_);
 
   for (auto& render_id_info : web_view_info_map_) {
     WebViewInfo& info = render_id_info.second;
@@ -131,7 +137,7 @@ void WebViewRendererState::RemoveContentScriptIDs(
     int embedder_process_id,
     int view_instance_id,
     const std::set<int>& script_ids) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  base::AutoLock auto_lock(web_view_info_map_lock_);
 
   for (auto& render_id_info : web_view_info_map_) {
     WebViewInfo& info = render_id_info.second;
