9e33757b525a376d5b9f2ae5c3243a1fa4472eaf
thakis <thakis@chromium.org>
2015-08-26 20:49:17 -0700

    Revert of Pull newest webrtc to fix MIPS build error. (patchset #2 id:20001 of https://codereview.chromium.org/1315193002/ )
    Reason for revert:
    Added a static initializer:
    
    http://build.chromium.org/p/chromium/builders/Linux/builds/66153/steps/sizes/logs/stdio
    
    # sctpdataengine.cc _GLOBAL__sub_I_sctpdataengine.cc+0xf
    # sctpdataengine.cc __cxa_atexit@plt [registers a dtor to run at exit]
    
    Original issue's description:
    > Pull newest webrtc to fix MIPS build error.
    >
    > BUG=524885
    > TEST=none
    > TBR=tommi
    >
    > Committed: https://crrev.com/6e3d2360717f4b96d0172614f25fc81673cdf02f
    > Cr-Commit-Position: refs/heads/master@{#345737}
    
    TBR=tommi@chromium.org,pkasting@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=524885
    
    Review URL: https://codereview.chromium.org/1306163004
    
    Cr-Commit-Position: refs/heads/master@{#345786}

--- content/renderer/media/webrtc/webrtc_video_frame_adapter.h
+++ content/renderer/media/webrtc/webrtc_video_frame_adapter.h
@@ -25,6 +25,8 @@ class WebRtcVideoFrameAdapter : public webrtc::VideoFrameBuffer {
 
   const uint8_t* data(webrtc::PlaneType type) const override;
 
+  uint8_t* data(webrtc::PlaneType type) override;
+
   int stride(webrtc::PlaneType type) const override;
 
   void* native_handle() const override;
