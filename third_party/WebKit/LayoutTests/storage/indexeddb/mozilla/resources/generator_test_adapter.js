e5c2508a1d9236ef47750dcdc62e9330e7df85ff
jsbell@chromium.org <jsbell@chromium.org>
2015-08-27 00:11:32 +0000

    Import Mozilla's IDBObjectStore.openKeyCursor() test
    It's an experimental "IDB v2" API implemented by both Firefox and
    Chrome. Import the Moz test to ensure we're compatible.
    
    R=cmumford@chromium.org
    
    Review URL: https://codereview.chromium.org/1316203002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201264 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/storage/indexeddb/mozilla/resources/generator_test_adapter.js
+++ third_party/WebKit/LayoutTests/storage/indexeddb/mozilla/resources/generator_test_adapter.js
@@ -32,6 +32,9 @@ function grabEventAndContinueHandler(e) {
 function executeSoon(f) {
     setTimeout(f, 0);
 }
+function continueToNextStepSync() {
+    testGenerator.next();
+}
 function finishTest() {
     finishJSTest();
 }
