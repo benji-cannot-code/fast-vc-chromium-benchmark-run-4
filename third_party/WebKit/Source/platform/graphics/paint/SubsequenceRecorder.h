24e0ee7fa7e0bae340274da74b248f2f3d119d5b
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2015-08-27 03:03:05 +0000

    Simplify subtree (now subsequence) caching
    We no longer need paint-phase-based subtree display items because we'll
    use paint layers as units of subtrees. This greatly simplifies subtree
    caching.
    
    Renamed subtree to subsequence.
    
    BUG=410087
    
    Review URL: https://codereview.chromium.org/1313223002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201282 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/platform/graphics/paint/SubsequenceRecorder.h
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SubsequenceRecorder_h
+#define SubsequenceRecorder_h
+
+#include "platform/graphics/paint/DisplayItemClient.h"
+
+namespace blink {
+
+class DisplayItemList;
+class GraphicsContext;
+
+// SubsequenceRecorder records BeginSubsequenceDisplayItem and EndSubsequenceDisplayItem
+// sentinels at either end of a continguous sequence of DisplayItems, and supports
+// caching via a CachedDisplayItem with the CachedSubsequence DisplayItem type.
+//
+// Also note that useCachedSubsequenceIfPossible is not sufficient to determine whether a
+// CachedSubsequence can be used. In particular, the client is responsible for checking that
+// none of the DisplayItemClients that contribute to the subsequence have been invalidated.
+//
+class PLATFORM_EXPORT SubsequenceRecorder {
+public:
+    static bool useCachedSubsequenceIfPossible(GraphicsContext&, const DisplayItemClientWrapper&);
+
+    SubsequenceRecorder(GraphicsContext&, const DisplayItemClientWrapper&);
+    ~SubsequenceRecorder();
+
+private:
+    DisplayItemList* m_displayItemList;
+    DisplayItemClientWrapper m_client;
+};
+
+} // namespace blink
+
+#endif // SubsequenceRecorder_h
