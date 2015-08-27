a0c805cc0a4416047d811b2cfa1916a013afcc3b
kbr <kbr@chromium.org>
2015-08-26 19:19:55 -0700

    Fix TODOs associated with bare exception clauses.
    Pointed out by nednguyen in previous code review for this file.
    
    BUG=495870
    
    Review URL: https://codereview.chromium.org/1311903003
    
    Cr-Commit-Position: refs/heads/master@{#345770}

--- content/test/gpu/gpu_tests/gpu_test_base.py
+++ content/test/gpu/gpu_tests/gpu_test_base.py
@@ -33,8 +33,7 @@ def _PageOperationWrapper(page, tab, expectations,
       'Skip expectations should have been handled at a higher level')
   try:
     inner_func()
-  # TODO(kbr): change this to "except Exception".
-  except:
+  except Exception:
     page.SetHadError()
     if expectation == 'pass':
       raise
@@ -119,8 +118,7 @@ class ValidatorBase(page_test.PageTest):
         page, tab, page.GetExpectations(), True,
         lambda: self.ValidateAndMeasurePageInner(page, tab, results),
         results=results)
-    # TODO(kbr): change this to "except Exception".
-    except:
+    except Exception:
       # If we're going to re-execute a flaky test in the finally
       # clause, then we have to squelch this exception.
       if num_retries == 0:
@@ -146,8 +144,7 @@ class ValidatorBase(page_test.PageTest):
               page, tab, page.GetExpectations(), True,
               lambda: self.ValidateAndMeasurePageInner(page, tab, results),
               results=results)
-          # TODO(kbr): change this to "except Exception".
-          except:
+          except Exception:
             # Squelch any exceptions from any but the last retry.
             if ii == num_retries - 1:
               raise
