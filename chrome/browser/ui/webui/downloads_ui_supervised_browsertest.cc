c7f2679e03a56ecacad962201e17a3d0ff3bdcdf
dbeam <dbeam@chromium.org>
2015-08-26 17:07:13 -0700

    Enable Material Design downloads on Canary/Chromium
    R=groby@chromium.org
    BUG=425626
    
    Review URL: https://codereview.chromium.org/1314793003
    
    Cr-Commit-Position: refs/heads/master@{#345732}

--- chrome/browser/ui/webui/downloads_ui_supervised_browsertest.cc
+++ chrome/browser/ui/webui/downloads_ui_supervised_browsertest.cc
@@ -12,5 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 void DownloadsWebUIForSupervisedUsersTest::SetUpCommandLine(
     base::CommandLine* command_line) {
+  DownloadsUIBrowserTest::SetUpCommandLine(command_line);
   command_line->AppendSwitchASCII(switches::kSupervisedUserId, "asdf");
 }
