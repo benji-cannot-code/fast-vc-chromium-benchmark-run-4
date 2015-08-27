e6bb3541902c66249ced4da40ff1f19cbab8af88
hendrikw <hendrikw@chromium.org>
2015-08-26 21:49:43 -0700

    cc: Allow background filters on renderpasses with transparent background
    This is needed to make backdrop filters mostly work.
    
    Logged https://code.google.com/p/chromium/issues/detail?id=525099 to capture
    any possible issues that may come up related to this.
    
    BUG=497522
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1321483002
    
    Cr-Commit-Position: refs/heads/master@{#345797}

--- cc/output/gl_renderer.cc
+++ cc/output/gl_renderer.cc
@@ -760,16 +760,12 @@ void GLRenderer::RestoreBlendFuncToDefault(SkXfermode::Mode blend_mode) {
   }
 }
 
-bool GLRenderer::ShouldApplyBackgroundFilters(DrawingFrame* frame,
-                                              const RenderPassDrawQuad* quad) {
+bool GLRenderer::ShouldApplyBackgroundFilters(const RenderPassDrawQuad* quad) {
   if (quad->background_filters.IsEmpty())
     return false;
 
-  // TODO(danakj): We only allow background filters on an opaque render surface
-  // because other surfaces may contain translucent pixels, and the contents
-  // behind those translucent pixels wouldn't have the filter applied.
-  if (frame->current_render_pass->has_transparent_background)
-    return false;
+  // TODO(hendrikw): Look into allowing background filters to see pixels from
+  // other render targets.  See crbug.com/314867.
 
   return true;
 }
@@ -825,7 +821,7 @@ gfx::Rect GLRenderer::GetBackdropBoundingBoxForRenderPassQuad(
   gfx::Rect backdrop_rect = gfx::ToEnclosingRect(MathUtil::MapClippedRect(
       contents_device_transform, scaled_region.BoundingBox()));
 
-  if (ShouldApplyBackgroundFilters(frame, quad)) {
+  if (ShouldApplyBackgroundFilters(quad)) {
     int top, right, bottom, left;
     quad->background_filters.GetOutsets(&top, &right, &bottom, &left);
     backdrop_rect.Inset(-left, -top, -right, -bottom);
@@ -861,7 +857,7 @@ skia::RefPtr<SkImage> GLRenderer::ApplyBackgroundFilters(
     DrawingFrame* frame,
     const RenderPassDrawQuad* quad,
     ScopedResource* background_texture) {
-  DCHECK(ShouldApplyBackgroundFilters(frame, quad));
+  DCHECK(ShouldApplyBackgroundFilters(quad));
   skia::RefPtr<SkImageFilter> filter = RenderSurfaceFilters::BuildImageFilter(
       quad->background_filters, background_texture->size());
 
@@ -911,7 +907,7 @@ void GLRenderer::DrawRenderPassQuad(DrawingFrame* frame,
   SkXfermode::Mode blend_mode = quad->shared_quad_state->blend_mode;
   bool use_shaders_for_blending =
       !CanApplyBlendModeUsingBlendFunc(blend_mode) ||
-      ShouldApplyBackgroundFilters(frame, quad) ||
+      ShouldApplyBackgroundFilters(quad) ||
       settings_->force_blending_with_shaders;
 
   scoped_ptr<ScopedResource> background_texture;
@@ -936,7 +932,7 @@ void GLRenderer::DrawRenderPassQuad(DrawingFrame* frame,
       // LayerTreeHost::CalculateMemoryForRenderSurfaces.
       background_texture = GetBackdropTexture(background_rect);
 
-      if (ShouldApplyBackgroundFilters(frame, quad) && background_texture) {
+      if (ShouldApplyBackgroundFilters(quad) && background_texture) {
         // Apply the background filters to R, so that it is applied in the
         // pixels' coordinate space.
         background_image =
@@ -956,7 +952,7 @@ void GLRenderer::DrawRenderPassQuad(DrawingFrame* frame,
       if (!quad->mask_resource_id())
         background_texture.reset();
     } else if (CanApplyBlendModeUsingBlendFunc(blend_mode) &&
-               ShouldApplyBackgroundFilters(frame, quad)) {
+               ShouldApplyBackgroundFilters(quad)) {
       // Something went wrong with applying background filters to the backdrop.
       use_shaders_for_blending = false;
       background_texture.reset();
