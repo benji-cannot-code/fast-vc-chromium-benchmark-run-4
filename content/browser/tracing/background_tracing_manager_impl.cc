aef63c8544c239ea56df64982b1b0217307828c4
simonhatch <simonhatch@chromium.org>
2015-08-27 07:58:25 -0700

    Deep Reports: Implement new reactive rule to randomly sample.
    When this rule is specified, we'll capture short traces at random intervals.
    
    Works as follows:
    1. Pick a time value randomly in the range [kReactiveTraceRandomTimeoutMin, kReactiveTraceRandomTimeoutMax].
    2. When that time has elapsed, call TriggerNamedEvent() to start the trace.
    3. Change the reactive timeout to a value in the range [kReactiveTraceRandomStartTimeMin, kReactiveTraceRandomStartTimeMax] seconds, and allow that to finalize the trace.
    
    BUG=524593
    
    Review URL: https://codereview.chromium.org/1317863003
    
    Cr-Commit-Position: refs/heads/master@{#345876}

--- content/browser/tracing/background_tracing_manager_impl.cc
+++ content/browser/tracing/background_tracing_manager_impl.cc
@@ -78,10 +78,10 @@ BackgroundTracingManagerImpl::TracingTimer::TracingTimer(
 BackgroundTracingManagerImpl::TracingTimer::~TracingTimer() {
 }
 
-void BackgroundTracingManagerImpl::TracingTimer::StartTimer() {
-  const int kTimeoutSecs = 10;
-  tracing_timer_.Start(FROM_HERE, base::TimeDelta::FromSeconds(kTimeoutSecs),
-      this, &BackgroundTracingManagerImpl::TracingTimer::TracingTimerFired);
+void BackgroundTracingManagerImpl::TracingTimer::StartTimer(int seconds) {
+  tracing_timer_.Start(
+      FROM_HERE, base::TimeDelta::FromSeconds(seconds), this,
+      &BackgroundTracingManagerImpl::TracingTimer::TracingTimerFired);
 }
 
 void BackgroundTracingManagerImpl::TracingTimer::CancelTimer() {
@@ -110,8 +110,8 @@ BackgroundTracingManagerImpl::BackgroundTracingManagerImpl()
       is_gathering_(false),
       is_tracing_(false),
       requires_anonymized_data_(true),
-      trigger_handle_ids_(0) {
-}
+      trigger_handle_ids_(0),
+      reactive_triggered_handle_(-1) {}
 
 BackgroundTracingManagerImpl::~BackgroundTracingManagerImpl() {
   NOTREACHED();
@@ -263,6 +263,13 @@ void BackgroundTracingManagerImpl::TriggerNamedEvent(
     RecordBackgroundTracingMetric(PREEMPTIVE_TRIGGERED);
     BeginFinalizing(callback);
   } else {
+    // A different reactive config tried to trigger.
+    if (is_tracing_ && handle != reactive_triggered_handle_) {
+      if (!callback.is_null())
+        callback.Run(false);
+      return;
+    }
+
     RecordBackgroundTracingMetric(REACTIVE_TRIGGERED);
     if (is_tracing_) {
       tracing_timer_->CancelTimer();
@@ -274,8 +281,9 @@ void BackgroundTracingManagerImpl::TriggerNamedEvent(
     EnableRecording(GetCategoryFilterStringForCategoryPreset(
                         triggered_rule->GetCategoryPreset()),
                     base::trace_event::RECORD_UNTIL_FULL);
-        tracing_timer_.reset(new TracingTimer(callback));
-        tracing_timer_->StartTimer();
+    tracing_timer_.reset(new TracingTimer(callback));
+    tracing_timer_->StartTimer(triggered_rule->GetReactiveTimeout());
+    reactive_triggered_handle_ = handle;
   }
 }
 
@@ -440,6 +448,7 @@ void BackgroundTracingManagerImpl::BeginFinalizing(
     StartedFinalizingCallback callback) {
   is_gathering_ = true;
   is_tracing_ = false;
+  reactive_triggered_handle_ = -1;
 
   bool is_allowed_finalization =
       !delegate_ || (config_ &&
@@ -471,6 +480,7 @@ void BackgroundTracingManagerImpl::BeginFinalizing(
 
 void BackgroundTracingManagerImpl::AbortScenario() {
   is_tracing_ = false;
+  reactive_triggered_handle_ = -1;
   config_.reset();
 
   content::TracingController::GetInstance()->DisableRecording(nullptr);
