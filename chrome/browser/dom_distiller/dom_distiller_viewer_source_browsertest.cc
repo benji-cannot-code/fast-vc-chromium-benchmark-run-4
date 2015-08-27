3417cb4405ea96031e0899bd99ba13938211bab9
wychen <wychen@chromium.org>
2015-08-27 11:47:56 -0700

    Fix theme-color meta tag for toolbar in reader mode
    In previous CL: "In reader mode, set theme-color meta tag for toolbar",
    the meta tag was not properly initialized.
    
    TEST=browser_tests --gtest_filter="*Distill*.PrefChange"
    BUG=485171
    
    Review URL: https://codereview.chromium.org/1293993002
    
    Cr-Commit-Position: refs/heads/master@{#345936}

--- chrome/browser/dom_distiller/dom_distiller_viewer_source_browsertest.cc
+++ chrome/browser/dom_distiller/dom_distiller_viewer_source_browsertest.cc
@@ -66,6 +66,8 @@ const char kGetBodyClass[] =
     "window.domAutomationController.send("
         "document.body.className)";
 
+const unsigned kDarkToolbarThemeColor = 0xFF1A1A1A;
+
 void AddEntry(const ArticleEntry& e, FakeDB<ArticleEntry>::EntryMap* map) {
   (*map)[e.entry_id()] = e;
 }
@@ -439,6 +441,9 @@ IN_PROC_BROWSER_TEST_F(DomDistillerViewerSourceBrowserTest, PrefChange) {
   EXPECT_TRUE(content::ExecuteScriptAndExtractString(
       contents, kGetBodyClass, &result));
   EXPECT_EQ("dark sans-serif", result);
+
+  // Verify that the theme color for the tab is updated as well.
+  EXPECT_EQ(kDarkToolbarThemeColor, contents->GetThemeColor());
 
   distilled_page_prefs->SetFontFamily(DistilledPagePrefs::SERIF);
   base::RunLoop().RunUntilIdle();
