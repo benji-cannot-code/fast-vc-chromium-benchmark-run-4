8fce59445e3aa97f461cd46f8bc473c8d0200824
csharrison <csharrison@chromium.org>
2015-08-26 17:20:03 -0700

    Move report_raw_headers from net load flags into ResourceRequestInfo
    RequestInfo gets a bit set from WebURLRequest, which gets put into
    a ResourceHostMsg_Request and sent via RDH to the browser, where
    it gets set in a ResourceRequestInfo object, which is read by the
    devtools system
    
    BUG=517114
    
    Review URL: https://codereview.chromium.org/1295523005
    
    Cr-Commit-Position: refs/heads/master@{#345741}

--- content/browser/loader/resource_request_info_impl.cc
+++ content/browser/loader/resource_request_info_impl.cc
@@ -68,6 +68,7 @@ void ResourceRequestInfo::AllocateForTesting(net::URLRequest* request,
           blink::WebPageVisibilityStateVisible,  // visibility_state
           context,                           // context
           base::WeakPtr<ResourceMessageFilter>(),  // filter
+          false,                             // report_raw_headers
           is_async);                         // is_async
   info->AssociateWithRequest(request);
 }
@@ -126,6 +127,7 @@ ResourceRequestInfoImpl::ResourceRequestInfoImpl(
     blink::WebPageVisibilityState visibility_state,
     ResourceContext* context,
     base::WeakPtr<ResourceMessageFilter> filter,
+    bool report_raw_headers,
     bool is_async)
     : cross_site_handler_(NULL),
       detachable_handler_(NULL),
@@ -156,6 +158,7 @@ ResourceRequestInfoImpl::ResourceRequestInfoImpl(
       visibility_state_(visibility_state),
       context_(context),
       filter_(filter),
+      report_raw_headers_(report_raw_headers),
       is_async_(is_async) {
 }
 
@@ -246,6 +249,10 @@ bool ResourceRequestInfoImpl::IsAsync() const {
 
 bool ResourceRequestInfoImpl::IsDownload() const {
   return is_download_;
+}
+
+bool ResourceRequestInfoImpl::ShouldReportRawHeaders() const {
+  return report_raw_headers_;
 }
 
 void ResourceRequestInfoImpl::AssociateWithRequest(net::URLRequest* request) {
--- content/browser/loader/resource_request_info_impl.h
+++ content/browser/loader/resource_request_info_impl.h
@@ -65,6 +65,7 @@ class ResourceRequestInfoImpl : public ResourceRequestInfo,
       blink::WebPageVisibilityState visibility_state,
       ResourceContext* context,
       base::WeakPtr<ResourceMessageFilter> filter,
+      bool report_raw_headers,
       bool is_async);
   ~ResourceRequestInfoImpl() override;
 
@@ -89,6 +90,7 @@ class ResourceRequestInfoImpl : public ResourceRequestInfo,
                                 int* render_frame_id) const override;
   bool IsAsync() const override;
   bool IsDownload() const override;
+  bool ShouldReportRawHeaders() const;
 
   CONTENT_EXPORT void AssociateWithRequest(net::URLRequest* request);
 
@@ -218,6 +220,7 @@ class ResourceRequestInfoImpl : public ResourceRequestInfo,
   // The filter might be deleted without deleting this object if the process
   // exits during a transfer.
   base::WeakPtr<ResourceMessageFilter> filter_;
+  bool report_raw_headers_;
   bool is_async_;
 
   DISALLOW_COPY_AND_ASSIGN(ResourceRequestInfoImpl);
