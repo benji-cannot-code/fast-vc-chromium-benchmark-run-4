290fb7ec9139f75904bef361bf46879682b51f27
lalitm <lalitm@google.com>
2015-08-27 13:17:50 -0700

    webapps: introduce helper class to store extended set of data
    Storing data in the intent is not scalable and leads to
    ugly methods which accept many parameters.
    
    Here, a new class is introduced which allows more data to
    be stored about webapps without pushing them all into the
    intent.
    
    Other usecases include updating this data from the manifest
    every time the webapp is opened and this is especially
    useful for the introduction of the splashscreen icon as it
    can be put here after the intent is created.
    
    This is a part of a group of CLs:
    (1) https://codereview.chromium.org/1286973003 (this)
    (2) https://codereview.chromium.org/1310223002
    (3) https://codereview.chromium.org/1239923002
    
    BUG=508627
    
    Review URL: https://codereview.chromium.org/1286973003
    
    Cr-Commit-Position: refs/heads/master@{#345970}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappInfo.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappInfo.java
@@ -7,10 +7,7 @@ package org.chromium.chrome.browser.webapps;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.text.TextUtils;
-import android.util.Base64;
 import android.util.Log;
 
 import org.chromium.chrome.browser.ShortcutHelper;
@@ -198,10 +195,7 @@ public class WebappInfo {
      */
     public Bitmap icon() {
         if (mDecodedIcon != null) return mDecodedIcon;
-        if (TextUtils.isEmpty(mEncodedIcon)) return null;
-
-        byte[] decoded = Base64.decode(mEncodedIcon, Base64.DEFAULT);
-        mDecodedIcon = BitmapFactory.decodeByteArray(decoded, 0, decoded.length);
+        mDecodedIcon = ShortcutHelper.decodeBitmapFromString(mEncodedIcon);
         return mDecodedIcon;
     }
 
