304cb7d900527a10f05b034603c1973da0ff5e79
mcasas <mcasas@chromium.org>
2015-08-26 19:49:48 -0700

    VideoSourceHandlerTest and some related unittests minor cleanup
    Simplification of video_source_handler_unittest.cc
    and other minor cleanups related to it. Found them
    while studying how to use MockMediaStreamRegistry.
    
    Review URL: https://codereview.chromium.org/1315833009
    
    Cr-Commit-Position: refs/heads/master@{#345776}

--- content/renderer/media/video_source_handler.h
+++ content/renderer/media/video_source_handler.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <string>
 
-#include "base/compiler_specific.h"
-#include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/threading/thread_checker.h"
@@ -21,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 class MediaStreamRegistryInterface;
-class MediaStreamVideoSink;
 class PpFrameReceiver;
 
 // Interface used by the effects pepper plugin to get captured frames
@@ -53,7 +50,7 @@ class CONTENT_EXPORT VideoSourceHandler {
   bool Close(FrameReaderInterface* reader);
 
  private:
-  FRIEND_TEST_ALL_PREFIXES(VideoSourceHandlerTest, OpenClose);
+  friend class VideoSourceHandlerTest;
 
   struct SourceInfo {
     SourceInfo(const blink::WebMediaStreamTrack& blink_track,
