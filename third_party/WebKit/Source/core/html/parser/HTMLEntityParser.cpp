c1adb5c5fae3f2974f3546c848e675ca607d91e5
kouhei@chromium.org <kouhei@chromium.org>
2015-08-27 07:27:09 +0000

    SegmentedString::push() should always push a char in front
    Before this CL, SegmentedString::push() had an exotic behavior, where
    two consecutive push would swap its order.
    
    This CL changes the push() implementation so that it would always push
    the new char in front of the SegmentedString.
    This CL also updates the HTMLEntityParser code which relied on the behavior.
    
    BUG=None
    TEST={SegmentedStringTest,HTMLEntityParserTest}.*
    
    Review URL: https://codereview.chromium.org/1308573006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201294 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/HTMLEntityParser.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLEntityParser.cpp
@@ -96,8 +96,8 @@ static void unconsumeCharacters(SegmentedString& source, ConsumedCharacterBuffer
     if (consumedCharacters.size() == 1)
         source.push(consumedCharacters[0]);
     else if (consumedCharacters.size() == 2) {
-        source.push(consumedCharacters[0]);
         source.push(consumedCharacters[1]);
+        source.push(consumedCharacters[0]);
     } else
         source.prepend(SegmentedString(String(consumedCharacters)));
 }
@@ -213,8 +213,8 @@ bool consumeHTMLEntity(SegmentedString& source, DecodedHTMLEntity& decodedEntity
                 entityState = Hex;
                 continue;
             }
-            source.push('#');
             source.push('x');
+            source.push('#');
             return false;
         }
         case MaybeHexUpperCaseX: {
@@ -222,8 +222,8 @@ bool consumeHTMLEntity(SegmentedString& source, DecodedHTMLEntity& decodedEntity
                 entityState = Hex;
                 continue;
             }
-            source.push('#');
             source.push('X');
+            source.push('#');
             return false;
         }
         case Hex: {
