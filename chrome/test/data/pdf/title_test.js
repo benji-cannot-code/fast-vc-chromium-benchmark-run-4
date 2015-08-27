cd257e9a5ae5a6571f2a2de6e00558ad1200bc7a
sammc <sammc@chromium.org>
2015-08-27 03:25:35 -0700

    PDF: Use PDF metadata for the title instead of the last path element.
    Review URL: https://codereview.chromium.org/1303103003
    
    Cr-Commit-Position: refs/heads/master@{#345838}

---
+++ chrome/test/data/pdf/title_test.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var tests = [
+  /**
+   * Test that the correct title is displayed for test-title.pdf.
+   */
+  function testHasCorrectTitle() {
+    chrome.test.assertEq('PDF title test', document.title);
+
+    chrome.test.succeed();
+  }
+];
+
+var scriptingAPI = new PDFScriptingAPI(window, window);
+scriptingAPI.setLoadCallback(function() {
+  chrome.test.runTests(tests);
+});
