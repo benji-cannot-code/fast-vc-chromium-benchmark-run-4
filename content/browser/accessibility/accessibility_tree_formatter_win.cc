f93a7bc5955f4ee3187df411e3c25da551030d8c
thakis <thakis@chromium.org>
2015-08-26 21:21:33 -0700

    Revert of Re-enabled contenteditable and text-changed tests after content editable work has been submitted. (patchset #13 id:240001 of https://codereview.chromium.org/1292193004/ )
    Reason for revert:
    Turned most Win main waterfall tester bots red:
    http://build.chromium.org/p/chromium.win/builders/XP%20Tests%20%281%29/builds/39725
    http://build.chromium.org/p/chromium.win/builders/Vista%20Tests%20%281%29/builds/58987
    http://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%281%29/builds/41734
    http://build.chromium.org/p/chromium.win/builders/Win%207%20Tests%20x64%20%281%29/builds/5062
    http://build.chromium.org/p/chromium.win/builders/Win8%20Aura/builds/32787
    
    Original issue's description:
    > Re-enabled contenteditable and text-changed tests after content editable work has been submitted.
    >
    > BUG=491027
    > R=dmazzoni@chromium.org
    >
    > Committed: https://crrev.com/2a08efd44ef185753219067e059deec217fcf4ea
    > Cr-Commit-Position: refs/heads/master@{#344684}
    >
    > Committed: https://crrev.com/4ed230441a93a55e7d90bfcd5d5f4cceb4f4e326
    > Cr-Commit-Position: refs/heads/master@{#345734}
    
    TBR=dmazzoni@chromium.org,nektar@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=491027
    
    Review URL: https://codereview.chromium.org/1316973005
    
    Cr-Commit-Position: refs/heads/master@{#345792}

--- content/browser/accessibility/accessibility_tree_formatter_win.cc
+++ content/browser/accessibility/accessibility_tree_formatter_win.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/iaccessible2/ia2_api_all.h"
 #include "ui/base/win/atl_module.h"
 
+using base::StringPrintf;
 
 namespace content {
 
@@ -103,12 +104,8 @@ base::string16 GetIA2Hypertext(BrowserAccessibilityWin& ax_object) {
       DCHECK(SUCCEEDED(ax_embed->get_indexInParent(&child_index)));
     }
 
-    base::string16 child_index_str(L"<obj");
-    if (child_index >= 0) {
-      base::StringAppendF(&child_index_str, L"%d>", child_index);
-    } else {
-      base::StringAppendF(&child_index_str, L">");
-    }
+    base::string16 child_index_str = base::StringPrintf(L"<obj%d>",
+        child_index);
     base::ReplaceFirstSubstringAfterOffset(&ia2_hypertext, hypertext_index,
         embedded_character, child_index_str);
     ++character_index;
@@ -307,7 +304,7 @@ base::string16 AccessibilityTreeFormatter::ToString(
         base::string16 string_value;
         value->GetAsString(&string_value);
         WriteAttribute(false,
-                       base::StringPrintf(L"%ls='%ls'",
+                       StringPrintf(L"%ls='%ls'",
                                     base::UTF8ToUTF16(attribute_name).c_str(),
                                     string_value.c_str()),
                        &line);
