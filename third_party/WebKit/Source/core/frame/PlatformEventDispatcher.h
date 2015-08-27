7e6da1e2fd57f8e5be3866ac4abdf1e570117761
peria@chromium.org <peria@chromium.org>
2015-08-27 14:11:45 +0000

    Remove a raw pointer to a PlatformEventController instance in PlatformEventDispatcher on Oilpan build
    BUG=509911
    
    Review URL: https://codereview.chromium.org/1301543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201313 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/frame/PlatformEventDispatcher.h
+++ third_party/WebKit/Source/core/frame/PlatformEventDispatcher.h
@@ -31,13 +31,9 @@ protected:
 private:
     void purgeControllers();
 
-#if ENABLE(OILPAN)
-    void clearWeakMembers(Visitor*);
-#endif
-
-    WillBeHeapVector<PlatformEventController*> m_controllers;
-    bool m_needsPurge;
+    WillBeHeapHashSet<RawPtrWillBeWeakMember<PlatformEventController>> m_controllers;
     bool m_isDispatching;
+    bool m_isListening;
 };
 
 } // namespace blink
