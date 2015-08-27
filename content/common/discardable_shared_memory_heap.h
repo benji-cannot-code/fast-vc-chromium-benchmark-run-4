f386978853ae2ce64e4f5a2bf97fb02a0a11c58a
ssid <ssid@chromium.org>
2015-08-27 04:14:59 -0700

    Fix the size dumped by child discardable manager for tracing.
    The child discardable manager dumps the size of the discardable memory
    allocated as length of span, which is actually the number of blocks.
    This CL fixes that using the length * block_size. The sub-allocations
    are also added under allocated_objects dump.
    
    BUG=503168
    
    Review URL: https://codereview.chromium.org/1308213005
    
    Cr-Commit-Position: refs/heads/master@{#345846}

--- content/common/discardable_shared_memory_heap.h
+++ content/common/discardable_shared_memory_heap.h
@@ -119,6 +119,7 @@ class CONTENT_EXPORT DiscardableSharedMemoryHeap {
 
     base::trace_event::MemoryAllocatorDump* CreateMemoryAllocatorDump(
         Span* span,
+        size_t block_size,
         const char* name,
         base::trace_event::ProcessMemoryDump* pmd) const;
 
