8ce5822e0d0243cdb0eb4c2262a3992722711ebe
qinmin <qinmin@chromium.org>
2015-08-27 11:52:39 -0700

    Check for storage permission if context menu download is intercepted.
    For OMA download initiated through context menu, it is intercepted by DownloadManager in java side.
    As a result, it won't go through the native DownloadResourceThrottle to check for storage permissions.
    This change fixes the behavior for this special case.
    
    BUG=525270
    
    Review URL: https://codereview.chromium.org/1317963002
    
    Cr-Commit-Position: refs/heads/master@{#345938}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ChromeDownloadDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ChromeDownloadDelegate.java
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.download;
 
+import android.Manifest.permission;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
@@ -26,6 +28,8 @@ import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.content.browser.ContentViewDownloadDelegate;
 import org.chromium.content.browser.DownloadInfo;
 import org.chromium.content_public.browser.WebContents;
+import org.chromium.ui.base.WindowAndroid;
+import org.chromium.ui.base.WindowAndroid.PermissionCallback;
 import org.chromium.ui.widget.Toast;
 
 import java.io.File;
@@ -501,8 +505,24 @@ public class ChromeDownloadDelegate
         // OMA downloads have extension "dm" or "dd". For the latter, it
         // can be handled when native download completes.
         if (path != null && (path.endsWith(".dm"))) {
-            DownloadInfo downloadInfo = new DownloadInfo.Builder().setUrl(url).build();
-            onDownloadStartNoStream(downloadInfo);
+            final DownloadInfo downloadInfo = new DownloadInfo.Builder().setUrl(url).build();
+            if (mTab == null) return true;
+            WindowAndroid window = mTab.getWindowAndroid();
+            if (window.hasPermission(permission.WRITE_EXTERNAL_STORAGE)) {
+                onDownloadStartNoStream(downloadInfo);
+            } else if (window.canRequestPermission(permission.WRITE_EXTERNAL_STORAGE)) {
+                PermissionCallback permissionCallback = new PermissionCallback() {
+                    @Override
+                    public void onRequestPermissionsResult(
+                            String[] permissions, int[] grantResults) {
+                        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                            onDownloadStartNoStream(downloadInfo);
+                        }
+                    }
+                };
+                window.requestPermissions(
+                        new String[] {permission.WRITE_EXTERNAL_STORAGE}, permissionCallback);
+            }
             return true;
         }
         return false;
