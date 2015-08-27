e0d06146401ade0ce725df9b9abc407ca41ee727
ymalik <ymalik@chromium.org>
2015-08-27 07:44:19 -0700

    Remove enable viewport meta flag
    BUG=513214
    
    Review URL: https://codereview.chromium.org/1319513002
    
    Cr-Commit-Position: refs/heads/master@{#345872}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1320,7 +1320,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kEnableTouchEditing,
     switches::kEnableUnsafeES3APIs,
     switches::kEnableViewport,
-    switches::kEnableViewportMeta,
     switches::kInvertViewportScrollOrder,
     switches::kEnableVtune,
     switches::kEnableWebBluetooth,
