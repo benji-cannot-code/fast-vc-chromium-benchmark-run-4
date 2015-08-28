1653ee74aece7a1a6b3bf338fe072eb4eb3c493c
xiyuan <xiyuan@chromium.org>
2015-08-28 09:50:57 -0700

    eraser: Remove deprecated kClearSiteDataOnExit and settings UI
    kClearSiteDataOnExit is deprecated. Migration code has already
    been removed with http://crbug.com/384869. Remove the pref, its
    hidden settings UI and related strings.
    
    BUG=129349
    
    Review URL: https://codereview.chromium.org/1312473013
    
    Cr-Commit-Position: refs/heads/master@{#346168}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -331,7 +331,6 @@ extern const char kUseCustomChromeFrame[];
 #if !defined(OS_ANDROID)
 extern const char kContentSettingsPluginWhitelist[];
 #endif
-extern const char kClearSiteDataOnExit[];
 extern const char kPartitionDefaultZoomLevel[];
 extern const char kPartitionPerHostZoomLevels[];
 extern const char kAutofillDialogAutofillDefault[];
