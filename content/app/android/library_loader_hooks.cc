0f840be33900a0c78a6c59b49f2056dd6f3ce7a4
sergeyv <sergeyv@chromium.org>
2015-08-28 12:12:13 -0700

    Revert of Reland [Startup Tracing] Add --trace-config-file flag (patchset #2 id:20001 of https://codereview.chromium.org/1317333002/ )
    Reason for revert:
    Broke build at Win x64 GN. Failure sample:
    http://build.chromium.org/p/chromium.win/builders/Win%20x64%20GN%20%28dbg%29/builds/8575
    
    Original issue's description:
    > Reland [Startup Tracing] Add --trace-config-file flag
    >
    > Original CL: https://codereview.chromium.org/1315463002/
    >
    > This CL adds --trace-config-file flag. Please see trace_config_file.h for details.
    >
    > Design doc:
    > https://docs.google.com/document/d/1PgdXUOJF3WtEmYWUyGRbC2Fz2ICCZKO9jPvpLPRSHH8/edit?usp=sharing
    >
    > BUG=317481, 482098
    > TBR=dsinclair@chromium.org,blundell@chromium.org,sievers@chromium.org,sky@chromium.org,msw@chromium.org
    >
    > Committed: https://crrev.com/a556ab616a3817468a9bd2163383dafe24ffdc96
    > Cr-Commit-Position: refs/heads/master@{#346189}
    
    TBR=dsinclair@chromium.org,blundell@chromium.org,sievers@chromium.org,msw@chromium.org,sky@chromium.org,zhenw@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=317481, 482098
    
    Review URL: https://codereview.chromium.org/1312833007
    
    Cr-Commit-Position: refs/heads/master@{#346206}

--- content/app/android/library_loader_hooks.cc
+++ content/app/android/library_loader_hooks.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/trace_event/trace_event.h"
 #include "base/tracked_objects.h"
-#include "components/tracing/trace_config_file.h"
+#include "components/tracing/startup_tracing.h"
 #include "components/tracing/tracing_switches.h"
 #include "content/app/android/app_jni_registrar.h"
 #include "content/browser/android/browser_jni_registrar.h"
@@ -95,11 +95,7 @@ bool LibraryLoaded(JNIEnv* env, jclass clazz) {
     base::trace_event::TraceLog::GetInstance()->SetEnabled(
         trace_config, base::trace_event::TraceLog::RECORDING_MODE);
   } else {
-    if (tracing::TraceConfigFile::GetInstance()->IsEnabled()) {
-      base::trace_event::TraceLog::GetInstance()->SetEnabled(
-          tracing::TraceConfigFile::GetInstance()->GetTraceConfig(),
-          base::trace_event::TraceLog::RECORDING_MODE);
-    }
+    tracing::EnableStartupTracingIfConfigFileExists();
   }
 
   // Android's main browser loop is custom so we set the browser
--- content/browser/plugin_process_host.cc
+++ content/browser/plugin_process_host.cc
@@ -223,7 +223,6 @@ bool PluginProcessHost::Init(const WebPluginInfo& info) {
     switches::kLogPluginMessages,
     switches::kNoSandbox,
     switches::kPluginStartupDialog,
-    switches::kTraceConfigFile,
     switches::kTraceStartup,
     switches::kUseGL,
     switches::kForceDeviceScaleFactor,
