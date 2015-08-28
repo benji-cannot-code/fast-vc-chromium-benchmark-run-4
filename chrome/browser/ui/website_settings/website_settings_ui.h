34ec8cb96ebc1f1b932596fdb788a15a8f9d6c1c
palmer <palmer@chromium.org>
2015-08-28 16:53:15 -0700

    Further polish for the Origin Info Bubble.
    Simplify the view of cookies into a single line in the display, per UX team.
    
    BUG=444244
    
    Review URL: https://codereview.chromium.org/1113253002
    
    Cr-Commit-Position: refs/heads/master@{#346278}

--- chrome/browser/ui/website_settings/website_settings_ui.h
+++ chrome/browser/ui/website_settings/website_settings_ui.h
@@ -35,7 +35,7 @@ class Image;
 // etc.).
 class WebsiteSettingsUI {
  public:
-  // The Website Settings UI contains several tabs. Each tab is assiciated with
+  // The Website Settings UI contains several tabs. Each tab is associated with
   // a unique tab id. The enum |TabId| contains all the ids for the tabs.
   enum TabId {
     TAB_ID_PERMISSIONS = 0,
@@ -44,7 +44,7 @@ class WebsiteSettingsUI {
   };
 
   // |CookieInfo| contains information about the cookies from a specific source.
-  // A source can for example be a specific origin or an entire domain.
+  // A source can for example be a specific origin or an entire wildcard domain.
   struct CookieInfo {
     CookieInfo();
 
@@ -54,6 +54,10 @@ class WebsiteSettingsUI {
     int allowed;
     // The number of blocked cookies.
     int blocked;
+
+    // Whether these cookies are from the current top-level origin as seen by
+    // the user, or from third-party origins.
+    bool is_first_party;
   };
 
   // |PermissionInfo| contains information about a single permission |type| for
