2952ac48c13d1e1fdf6711c755d0bd0b9e6956c2
mcasas <mcasas@chromium.org>
2015-08-28 13:54:07 -0700

    Rename VideoDestinationHandler to PepperToVideoTrackAdapter and moved PpFrameWriter to .cc file
    Only mechanical changes except:
    
    - some comment rewrite: it said that
    video_destination_handler was used by "the effects
    pepper plugin", IMHO any (whitelisted) Pepper plugin
    could use this adapter.
    - Also I moved the inner class PpFrameWriter to
    the .cc file (let's see the bots).
    - moved them out of webrtc/ folder since they don't
    user any webrtc resource.
    - BUILD.gn: removed part redundant with content_renderer.gypi
    
    BUG=525817
    
    Review URL: https://codereview.chromium.org/1313423003
    
    Cr-Commit-Position: refs/heads/master@{#346231}

--- content/content_tests.gypi
+++ content/content_tests.gypi
@@ -708,7 +708,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/renderer_host/pepper/pepper_printing_host_unittest.cc',
       'browser/renderer_host/pepper/quota_reservation_unittest.cc',
       'child/npapi/plugin_lib_unittest.cc',
-      'renderer/media/webrtc/video_destination_handler_unittest.cc',
+      'renderer/media/pepper_to_video_track_adapter_unittest.cc',
       'renderer/npapi/webplugin_impl_unittest.cc',
       'renderer/pepper/event_conversion_unittest.cc',
       'renderer/pepper/host_var_tracker_unittest.cc',
@@ -1094,7 +1094,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           ]
         }, {
           'sources!': [
-            'renderer/media/webrtc/video_destination_handler_unittest.cc',
+            'renderer/media/pepper_to_video_track_adapter_unittest.cc',
           ],
         }],
         ['enable_webrtc==1 and (OS=="linux" or OS=="mac" or OS=="win")', {
--- content/renderer/media/pepper_to_video_track_adapter_unittest.cc
+++ content/renderer/media/pepper_to_video_track_adapter_unittest.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/media_stream_video_track.h"
 #include "content/renderer/media/mock_media_stream_registry.h"
 #include "content/renderer/media/mock_media_stream_video_sink.h"
-#include "content/renderer/media/webrtc/video_destination_handler.h"
+#include "content/renderer/media/pepper_to_video_track_adapter.h"
 #include "content/renderer/pepper/pepper_plugin_instance_impl.h"
 #include "content/renderer/pepper/ppb_image_data_impl.h"
 #include "content/test/ppapi_unittest.h"
@@ -33,9 +33,9 @@ ACTION_P(RunClosure, closure) {
 static const std::string kTestStreamUrl = "stream_url";
 static const std::string kUnknownStreamUrl = "unknown_stream_url";
 
-class VideoDestinationHandlerTest : public PpapiUnittest {
+class PepperToVideoTrackAdapterTest : public PpapiUnittest {
  public:
-  VideoDestinationHandlerTest() : registry_(new MockMediaStreamRegistry()) {
+  PepperToVideoTrackAdapterTest() : registry_(new MockMediaStreamRegistry()) {
     registry_->Init(kTestStreamUrl);
   }
 
@@ -53,20 +53,20 @@ class VideoDestinationHandlerTest : public PpapiUnittest {
   scoped_ptr<MockMediaStreamRegistry> registry_;
 };
 
-TEST_F(VideoDestinationHandlerTest, Open) {
+TEST_F(PepperToVideoTrackAdapterTest, Open) {
   // |frame_writer| is a proxy and is owned by whoever call Open.
   FrameWriterInterface* frame_writer = nullptr;
   // Unknow url will return false.
-  EXPECT_FALSE(VideoDestinationHandler::Open(registry_.get(),
+  EXPECT_FALSE(PepperToVideoTrackAdapter::Open(registry_.get(),
                                              kUnknownStreamUrl, &frame_writer));
-  EXPECT_TRUE(VideoDestinationHandler::Open(registry_.get(),
+  EXPECT_TRUE(PepperToVideoTrackAdapter::Open(registry_.get(),
                                             kTestStreamUrl, &frame_writer));
   delete frame_writer;
 }
 
-TEST_F(VideoDestinationHandlerTest, PutFrame) {
+TEST_F(PepperToVideoTrackAdapterTest, PutFrame) {
   FrameWriterInterface* frame_writer = NULL;
-  EXPECT_TRUE(VideoDestinationHandler::Open(registry_.get(),
+  EXPECT_TRUE(PepperToVideoTrackAdapter::Open(registry_.get(),
                                             kTestStreamUrl, &frame_writer));
   ASSERT_TRUE(frame_writer);
 
