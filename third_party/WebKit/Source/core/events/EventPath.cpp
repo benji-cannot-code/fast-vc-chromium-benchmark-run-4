19e377c66e868240d0c639e6937ac5ed40f115cc
kochi@chromium.org <kochi@chromium.org>
2015-08-28 10:36:16 +0000

    Fix for calculation of event.path for closed mode shadow root
    Old code assumed that only one level of UA shadow tree
    nodes should be excluded from event path, while it is
    no longer true after introduction of shadow root
    closed mode.
    
    This change properly hides all the nodes behind closed
    shadow root in the event path.
    
    BUG=459136
    TEST=event-path-closed-shadowroot.html, event-path-closed-shadowroot2.html
    
    Review URL: https://codereview.chromium.org/1305733002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201391 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/events/EventPath.cpp
+++ third_party/WebKit/Source/core/events/EventPath.cpp
@@ -95,7 +95,7 @@ void EventPath::initialize()
         return;
     calculatePath();
     calculateAdjustedTargets();
-    calculateTreeScopePrePostOrderNumbers();
+    calculateTreeOrderAndSetNearestAncestorClosedTree();
 }
 
 void EventPath::calculatePath()
@@ -156,7 +156,7 @@ void EventPath::calculatePath()
     }
 }
 
-void EventPath::calculateTreeScopePrePostOrderNumbers()
+void EventPath::calculateTreeOrderAndSetNearestAncestorClosedTree()
 {
     // Precondition:
     //   - TreeScopes in m_treeScopeEventContexts must be *connected* in the same tree of trees.
@@ -167,7 +167,8 @@ void EventPath::calculateTreeScopePrePostOrderNumbers()
     TreeScopeEventContext* rootTree = 0;
     for (const auto& treeScopeEventContext : m_treeScopeEventContexts) {
         // Use olderShadowRootOrParentTreeScope here for parent-child relationships.
-        // See the definition of trees of trees in the Shado DOM spec: http://w3c.github.io/webcomponents/spec/shadow/
+        // See the definition of trees of trees in the Shadow DOM spec:
+        // http://w3c.github.io/webcomponents/spec/shadow/
         TreeScope* parent = treeScopeEventContext.get()->treeScope().olderShadowRootOrParentTreeScope();
         if (!parent) {
             ASSERT(!rootTree);
@@ -178,7 +179,7 @@ void EventPath::calculateTreeScopePrePostOrderNumbers()
         treeScopeEventContextMap.find(parent)->value->addChild(*treeScopeEventContext.get());
     }
     ASSERT(rootTree);
-    rootTree->calculatePrePostOrderNumber(0);
+    rootTree->calculateTreeOrderAndSetNearestAncestorClosedTree(0, nullptr);
 }
 
 TreeScopeEventContext* EventPath::ensureTreeScopeEventContext(Node* currentTarget, TreeScope* treeScope, TreeScopeEventContextMap& treeScopeEventContextMap)
