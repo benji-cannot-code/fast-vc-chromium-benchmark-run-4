21e19db4b4824fe02c29881026f342fce8f84101
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2015-08-28 18:25:44 +0000

    Include the whole outline into visual overflow
    This is another preparation CL for combining outline and focus ring
    paths (https://codereview.chromium.org/1224933002/).
    
    Not including the whole outline into visual overflow caused
    incorrect layout and painting of outlines when the element having
    outline has clipping or is composited because the overflow doesn't
    cover the whole outline.
    
    Not including outlines of block continuations into visual overflow
    required us to
    - propagate outline style to block continuations on style change;
    - paint outlines for block continuations separately, causing
      unconnected or duplicated outline edges.
    Both of the above caused complex code.
    
    With this CL, we always include the whole outline into visual
    overflow of the object having outline style.
    - For LayoutBoxes, combine code paths about overflow for focus ring
      and outline;
    - For LayoutBlockFlows, we need to include the visual overflow of
      descendant LayoutInlines having continuation and outline. Let the
      containing LayoutBlockFlow record LayoutInlines with outline and
      continuation, and include the outline rects of continuations
      into visual overflow of the block.
    
    (FYI, cases that we already included outlines into visual overflow:
     - LayoutBlockFlow containing descendant LayoutInlines having outline
       but not continuation: we already included such outlines in visual
       overflow of InlineFlowBoxes and in turn into the LayoutBlockFlow;
     - LayoutInlines having outline: previously we include outlines by
       calling rectWithOutlineForPaintInvalidation(); with this CL we
       call addOutlineRects().)
    
    This CL will have real effect with
    https://codereview.chromium.org/1224933002/.
    
    BUG=506669
    
    Review URL: https://codereview.chromium.org/1278543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201418 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutBlockFlowLine.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlockFlowLine.cpp
@@ -1493,6 +1493,11 @@ void LayoutBlockFlow::computeInlinePreferredLogicalWidths(LayoutUnit& minLogical
     maxLogicalWidth = std::max(maxLogicalWidth, inlineMax);
 }
 
+static bool isInlineWithOutlineAndContinuation(const LayoutObject& o)
+{
+    return o.isLayoutInline() && o.styleRef().hasOutline() && !o.isElementContinuation() && toLayoutInline(o).continuation();
+}
+
 void LayoutBlockFlow::layoutInlineChildren(bool relayoutChildren, LayoutUnit& paintInvalidationLogicalTop, LayoutUnit& paintInvalidationLogicalBottom, LayoutUnit afterEdge)
 {
     LayoutFlowThread* flowThread = flowThreadContainingBlock();
@@ -1567,6 +1572,9 @@ void LayoutBlockFlow::layoutInlineChildren(bool relayoutChildren, LayoutUnit& pa
                     dirtyLineBoxesForObject(o, layoutState.isFullLayout());
                 o->clearNeedsLayout();
             }
+
+            if (isInlineWithOutlineAndContinuation(*o))
+                setContainsInlineWithOutlineAndContinuation(true);
         }
 
         for (size_t i = 0; i < replacedChildren.size(); i++)
@@ -1855,6 +1863,24 @@ void LayoutBlockFlow::addOverflowFromInlineChildren()
         LayoutRect visualOverflow = curr->visualOverflowRect(curr->lineTop(), curr->lineBottom());
         addContentsVisualOverflow(visualOverflow);
     }
+
+    if (!containsInlineWithOutlineAndContinuation())
+        return;
+
+    // Add outline rects of continuations of descendant inlines into visual overflow of this block.
+    LayoutRect outlineBoundsOfAllContinuations;
+    for (InlineWalker walker(this); !walker.atEnd(); walker.advance()) {
+        const LayoutObject& o = *walker.current();
+        if (!isInlineWithOutlineAndContinuation(o))
+            continue;
+
+        Vector<LayoutRect> outlineRects;
+        toLayoutInline(o).addOutlineRectsForContinuations(outlineRects, LayoutPoint());
+        LayoutRect outlineBounds = unionRect(outlineRects);
+        outlineBounds.inflate(o.styleRef().outlineOutsetExtent());
+        outlineBoundsOfAllContinuations.unite(outlineBounds);
+    }
+    addContentsVisualOverflow(outlineBoundsOfAllContinuations);
 }
 
 void LayoutBlockFlow::deleteEllipsisLineBoxes()
