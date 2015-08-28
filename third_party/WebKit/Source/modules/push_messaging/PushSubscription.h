59aae4eb58177fd9f084649c21434a7de9e05517
peter@chromium.org <peter@chromium.org>
2015-08-28 14:41:13 +0000

    Remove left-overs of PushSubscription.curve25519dh
    These had to remain in place while a Chromium test got updated. This can
    be safely removed entirely now.
    
    This CL is part of a three-sided patch:
      [1] https://codereview.chromium.org/1309723004/
      [2] https://codereview.chromium.org/1314893002/
      [3] This CL.
    
    BUG=486040
    
    Review URL: https://codereview.chromium.org/1316693002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201403 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/push_messaging/PushSubscription.h
+++ third_party/WebKit/Source/modules/push_messaging/PushSubscription.h
@@ -31,7 +31,6 @@ public:
     virtual ~PushSubscription();
 
     KURL endpoint() const;
-    PassRefPtr<DOMArrayBuffer> curve25519dh() const;
 
     PassRefPtr<DOMArrayBuffer> getKey(const AtomicString& name) const;
     ScriptPromise unsubscribe(ScriptState*);
