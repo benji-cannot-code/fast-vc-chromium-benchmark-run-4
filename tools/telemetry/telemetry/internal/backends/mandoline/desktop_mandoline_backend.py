5e92ca76389c13629506e72d615c05b188ff9735
msw <msw@chromium.org>
2015-08-27 18:13:11 -0700

    Support Mandoline desktop temp profiles for telemetry.
    Use browser_options.profile_dir (or a temp profile dir).
    This mostly matches Chrome's desktop_browser_backend.py
    
    Required by telemetry's startup.cold.blank_page perf test.
    (browser.py clears the system cache for the profile dir)
    
    Also, minor cleanup of outdated function documentation.
    TODO: Similar support for the Android backend.
    
    BUG=513779
    TEST="./tools/perf/run_benchmark --browser=mandoline-debug startup.cold.blank_page --also-run-disabled-tests" passes.
    R=yzshen@chromium.org,nednguyen@google.com
    
    Review URL: https://codereview.chromium.org/1308573005
    
    Cr-Commit-Position: refs/heads/master@{#346040}

--- tools/telemetry/telemetry/internal/backends/mandoline/desktop_mandoline_backend.py
+++ tools/telemetry/telemetry/internal/backends/mandoline/desktop_mandoline_backend.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import logging
 import os
+import shutil
 import subprocess
 import sys
 import tempfile
@@ -31,6 +32,11 @@ class DesktopMandolineBackend(
     self._proc = None
     self._tmp_output_file = None
 
+    self._tmp_profile_dir = None
+    if not self.browser_options.profile_dir:
+      self._tmp_profile_dir = tempfile.mkdtemp()
+    logging.info("Using profile directory:'%s'." % self.profile_directory)
+
     self._executable = executable
     if not self._executable:
       raise Exception('Cannot create browser, no executable found!')
@@ -53,6 +59,7 @@ class DesktopMandolineBackend(
       self._port = util.GetUnreservedAvailableLocalPort()
     logging.info('Requested remote debugging port: %d' % self._port)
     args.append('--remote-debugging-port=%i' % self._port)
+    args.append('--user-data-dir=%s' % self.profile_directory)
     return args
 
   def Start(self):
@@ -99,7 +106,7 @@ class DesktopMandolineBackend(
 
   @property
   def profile_directory(self):
-    raise NotImplementedError()
+    return self.browser_options.profile_dir or self._tmp_profile_dir
 
   def IsBrowserRunning(self):
     return self._proc and self._proc.poll() == None
@@ -145,3 +152,7 @@ class DesktopMandolineBackend(
     if self.IsBrowserRunning():
       self._proc.kill()
     self._proc = None
+
+    if self._tmp_profile_dir and os.path.exists(self._tmp_profile_dir):
+      shutil.rmtree(self._tmp_profile_dir, ignore_errors=True)
+      self._tmp_profile_dir = None
