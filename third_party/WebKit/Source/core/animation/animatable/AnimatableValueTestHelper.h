95c90cfe5f377b4a7b2c496afdeb09a113bb2cfd
jbroman@chromium.org <jbroman@chromium.org>
2015-08-28 00:24:38 +0000

    Remove EXPECT_REFV_EQ macro. It is unused.
    Review URL: https://codereview.chromium.org/1323433002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201347 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/animatable/AnimatableValueTestHelper.h
+++ third_party/WebKit/Source/core/animation/animatable/AnimatableValueTestHelper.h
@@ -52,11 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/CSSValueTestHelper.h"
 
-// FIXME: Move to something like core/wtf/WTFTestHelpers.h
-// Compares the targets of two RefPtrs for equality.
-// (Objects still need an operator== defined for this to work).
-#define EXPECT_REFV_EQ(a, b) EXPECT_EQ(*(a.get()), *(b.get()))
-
 namespace blink {
 
 bool operator==(const AnimatableValue&, const AnimatableValue&);
