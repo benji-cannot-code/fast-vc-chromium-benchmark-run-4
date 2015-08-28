a151a15e31b1d4fb3e9f6f06034e4974c33d00ee
noel@chromium.org <noel@chromium.org>
2015-08-28 04:22:38 +0000

    Remove JPEG encoder SkBitmap API
    SkBitmap is being removed from Chrome. There are no longer any callers
    of JPEGImageEncoder::encode(SkBitmap,...) so we can remove it and also
    the support code it required.
    
    Note: The libjpeg-turbo specialization was the fastest encoder, but it
    required premultiplied data (SkBitmap). Thus some performance was lost
    when callers were switched over to use the ImageDataBuffer encoder (in
    the SkImage-replaces-SkBitmap endeavor).
    
    No change in behavior, no new tests.
    
    BUG=449197
    
    Review URL: https://codereview.chromium.org/1314763008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201367 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/image-encoders/skia/JPEGImageEncoder.h
+++ third_party/WebKit/Source/platform/image-encoders/skia/JPEGImageEncoder.h
@@ -34,16 +34,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "wtf/Vector.h"
 
-class SkBitmap;
-
 namespace blink {
 
 struct ImageDataBuffer;
 
 class JPEGImageEncoder {
 public:
-    // Encode the input data with a compression quality in [0-100].
-    static bool encode(const SkBitmap&, int quality, Vector<unsigned char>*);
+    // Encode the image data with a compression quality in [0-100].
     static bool encode(const ImageDataBuffer&, int quality, Vector<unsigned char>*);
 
     // For callers: provide a reasonable compression quality default.
