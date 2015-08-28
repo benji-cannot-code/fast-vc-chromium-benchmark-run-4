31289bb1e4208447daae470fcaec6b5231c442e1
blundell <blundell@chromium.org>
2015-08-28 02:59:17 -0700

    Turn VariationsService::GetVariationsServerURL into instance method
    This will allow for followup work that uses |client_| within that
    method.
    
    BUG=516678
    TBR=bartfab
    
    Review URL: https://codereview.chromium.org/1312423003
    
    Cr-Commit-Position: refs/heads/master@{#346111}

--- chrome/browser/metrics/variations/variations_service_unittest.cc
+++ chrome/browser/metrics/variations/variations_service_unittest.cc
@@ -265,20 +265,22 @@ TEST_F(VariationsServiceTest, GetVariationsServerURL) {
       VariationsService::GetDefaultVariationsServerURLForTesting();
 
   std::string value;
-  GURL url = VariationsService::GetVariationsServerURL(prefs, std::string());
+  TestVariationsService service(
+      new web_resource::TestRequestAllowedNotifier(prefs), prefs);
+  GURL url = service.GetVariationsServerURL(prefs, std::string());
   EXPECT_TRUE(base::StartsWith(url.spec(), default_variations_url,
                                base::CompareCase::SENSITIVE));
   EXPECT_FALSE(net::GetValueForKeyInQuery(url, "restrict", &value));
 
   prefs_store.SetVariationsRestrictParameterPolicyValue("restricted");
-  url = VariationsService::GetVariationsServerURL(prefs, std::string());
+  url = service.GetVariationsServerURL(prefs, std::string());
   EXPECT_TRUE(base::StartsWith(url.spec(), default_variations_url,
                                base::CompareCase::SENSITIVE));
   EXPECT_TRUE(net::GetValueForKeyInQuery(url, "restrict", &value));
   EXPECT_EQ("restricted", value);
 
   // The override value should take precedence over what's in prefs.
-  url = VariationsService::GetVariationsServerURL(prefs, "override");
+  url = service.GetVariationsServerURL(prefs, "override");
   EXPECT_TRUE(base::StartsWith(url.spec(), default_variations_url,
                                base::CompareCase::SENSITIVE));
   EXPECT_TRUE(net::GetValueForKeyInQuery(url, "restrict", &value));
@@ -288,8 +290,9 @@ TEST_F(VariationsServiceTest, GetVariationsServerURL) {
 TEST_F(VariationsServiceTest, VariationsURLHasOSNameParam) {
   TestingPrefServiceSimple prefs;
   VariationsService::RegisterPrefs(prefs.registry());
-  const GURL url =
-      VariationsService::GetVariationsServerURL(&prefs, std::string());
+  TestVariationsService service(
+      new web_resource::TestRequestAllowedNotifier(&prefs), &prefs);
+  const GURL url = service.GetVariationsServerURL(&prefs, std::string());
 
   std::string value;
   EXPECT_TRUE(net::GetValueForKeyInQuery(url, "osname", &value));
@@ -337,7 +340,7 @@ TEST_F(VariationsServiceTest, SeedStoredWhenOKStatus) {
   TestVariationsService service(
       new web_resource::TestRequestAllowedNotifier(&prefs), &prefs);
   service.variations_server_url_ =
-      VariationsService::GetVariationsServerURL(&prefs, std::string());
+      service.GetVariationsServerURL(&prefs, std::string());
   service.set_intercepts_fetch(false);
 
   net::TestURLFetcherFactory factory;
@@ -368,7 +371,7 @@ TEST_F(VariationsServiceTest, SeedNotStoredWhenNonOKStatus) {
       make_scoped_ptr(new ChromeVariationsServiceClient()),
       new web_resource::TestRequestAllowedNotifier(&prefs), &prefs, NULL);
   service.variations_server_url_ =
-      VariationsService::GetVariationsServerURL(&prefs, std::string());
+      service.GetVariationsServerURL(&prefs, std::string());
   for (size_t i = 0; i < arraysize(non_ok_status_codes); ++i) {
     net::TestURLFetcherFactory factory;
     service.DoActualFetch();
@@ -389,7 +392,7 @@ TEST_F(VariationsServiceTest, CountryHeader) {
   TestVariationsService service(
       new web_resource::TestRequestAllowedNotifier(&prefs), &prefs);
   service.variations_server_url_ =
-      VariationsService::GetVariationsServerURL(&prefs, std::string());
+      service.GetVariationsServerURL(&prefs, std::string());
   service.set_intercepts_fetch(false);
 
   net::TestURLFetcherFactory factory;
