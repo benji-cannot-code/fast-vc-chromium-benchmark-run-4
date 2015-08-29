b6f1fd3a2e228e70a44b082cfef34cadbbb4b5ca
lushnikov@chromium.org <lushnikov@chromium.org>
2015-08-29 02:06:19 +0000

    DevTools: forward Up/Down keys to profiles sidebar from FlameChart
    The patch:
    - Adds forwarding of Up/Down events to Profile panel's sidebar pane. This happens if up/down is pressed while FlameChart is focused.
    - Fixes occasional bug which makes FlameChart frozen/dead.
    
    BUG=511072
    R=pfeldman
    
    Review URL: https://codereview.chromium.org/1313443005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201445 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui_lazy/FlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/ui_lazy/FlameChart.js
@@ -105,6 +105,8 @@ WebInspector.FlameChart = function(dataProvider, flameChartDelegate, isTopDown)
     this._selectedEntryIndex = -1;
     this._rawTimelineDataLength = 0;
     this._textWidth = {};
+
+    this._lastMouseOffsetX = 0;
 }
 
 WebInspector.FlameChart.DividersBarHeight = 18;
