a842459d1736e1057a06dba8036b760d5bbc11e4
alph@chromium.org <alph@chromium.org>
2015-08-29 08:36:52 +0000

    DevTools: Introduce timeline recording info dialog
    The rationale is:
    
    1. Give the user more detailed information on what's going on.
    2. Makes it more clear that the rest of UI is disabled during recording.
    3. Fixes the problem with unintentional click to stop timeline starts a new timeline recording.
    
    BUG=525750
    
    Review URL: https://codereview.chromium.org/1321683003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201452 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ui/Dialog.js
+++ third_party/WebKit/Source/devtools/front_end/ui/Dialog.js
@@ -33,11 +33,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @constructor
  * @param {!Element} relativeToElement
  * @param {!WebInspector.DialogDelegate} delegate
+ * @param {boolean=} modal
  */
-WebInspector.Dialog = function(relativeToElement, delegate)
+WebInspector.Dialog = function(relativeToElement, delegate, modal)
 {
     this._delegate = delegate;
     this._relativeToElement = relativeToElement;
+    this._modal = modal;
 
     this._glassPane = new WebInspector.GlassPane(/** @type {!Document} */ (relativeToElement.ownerDocument));
     WebInspector.GlassPane.DefaultFocusedViewStack.push(this);
@@ -72,12 +74,14 @@ WebInspector.Dialog.currentInstance = function()
 /**
  * @param {?Element} relativeToElement
  * @param {!WebInspector.DialogDelegate} delegate
+ * @param {boolean=} modal
  */
-WebInspector.Dialog.show = function(relativeToElement, delegate)
+WebInspector.Dialog.show = function(relativeToElement, delegate, modal)
 {
     if (WebInspector.Dialog._instance)
         return;
-    WebInspector.Dialog._instance = new WebInspector.Dialog(relativeToElement || WebInspector.Dialog.modalHostView().element, delegate);
+    WebInspector.Dialog._instance = new WebInspector.Dialog(relativeToElement || WebInspector.Dialog.modalHostView().element, delegate, modal);
+    WebInspector.Dialog._instance.focus();
 }
 
 WebInspector.Dialog.hide = function()
@@ -106,8 +110,13 @@ WebInspector.Dialog.prototype = {
         this._glassPane.dispose();
     },
 
+    /**
+     * @param {!Event} event
+     */
     _onGlassPaneFocus: function(event)
     {
+        if (this._modal)
+            return;
         this._hide();
     },
 
@@ -130,6 +139,8 @@ WebInspector.Dialog.prototype = {
 
         if (event.keyCode === WebInspector.KeyboardShortcut.Keys.Enter.code)
             this._delegate.onEnter(event);
+
+        this._delegate.onKeyDown(event);
 
         if (!event.handled && this._closeKeys.indexOf(event.keyCode) >= 0) {
             this._hide();
@@ -179,7 +190,15 @@ WebInspector.DialogDelegate.prototype = {
 
     focus: function() { },
 
+    /**
+     * @param {!KeyboardEvent} event
+     */
     onEnter: function(event) { },
+
+    /**
+     * @param {!KeyboardEvent} event
+     */
+    onKeyDown: function(event) { },
 
     willHide: function() { },
 
