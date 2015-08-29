ee1d225873cc92426fcdb20b46f263e5538ae655
samli@chromium.org <samli@chromium.org>
2015-08-29 01:19:08 +0000

    Devtools UI: Add shortcut for activating/deactivating breakpoints
    This adds a Ctrl+F8 (Mac Cmd+F8) shortcut for toggling breakpoints
    functionality.
    
    BUG=525879
    
    Review URL: https://codereview.chromium.org/1302323005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201442 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sources/SourcesPanel.js
+++ third_party/WebKit/Source/devtools/front_end/sources/SourcesPanel.js
@@ -693,7 +693,7 @@ WebInspector.SourcesPanel.prototype = {
         rawLocation.continueToLocation();
     },
 
-    _toggleBreakpointsClicked: function(event)
+    _toggleBreakpointsActive: function()
     {
         WebInspector.breakpointManager.setBreakpointsActive(!WebInspector.breakpointManager.breakpointsActive());
     },
@@ -754,9 +754,8 @@ WebInspector.SourcesPanel.prototype = {
         debugToolbar.appendSeparator();
 
         // Toggle Breakpoints
-        this._toggleBreakpointsButton = new WebInspector.ToolbarButton(WebInspector.UIString("Deactivate breakpoints"), "breakpoint-toolbar-item");
+        this._toggleBreakpointsButton = WebInspector.ToolbarButton.createActionButton("debugger.toggle-breakpoints-active");
         this._toggleBreakpointsButton.setToggled(false);
-        this._toggleBreakpointsButton.addEventListener("click", this._toggleBreakpointsClicked, this);
         debugToolbar.appendToolbarItem(this._toggleBreakpointsButton);
 
         // Pause on Exception
@@ -1418,7 +1417,9 @@ WebInspector.SourcesPanel.DebuggingActionDelegate.prototype = {
         case "debugger.run-snippet":
             panel._runSnippet();
             break;
-
+        case "debugger.toggle-breakpoints-active":
+            panel._toggleBreakpointsActive();
+            break;
         }
     }
 }
