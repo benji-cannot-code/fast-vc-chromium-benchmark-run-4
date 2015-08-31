8e02ab51dbc1796d837f4478413851e012bb08da
danakj <danakj@chromium.org>
2015-08-31 16:56:56 -0700

    content/gpu: Stop converting Rect to RectF implicitly.
    This makes the conversions explicit, in the hopes of removing the
    ability to do implicit int->float gfx::Rect conversions. This
    patch hits the content/ and gpu/ directories.
    
    R=sievers@chromium.org
    BUG=342848
    
    Review URL: https://codereview.chromium.org/1324573003
    
    Cr-Commit-Position: refs/heads/master@{#346526}

--- content/renderer/render_view_impl.h
+++ content/renderer/render_view_impl.h
@@ -58,6 +58,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebSecurityOrigin.h"
 #include "third_party/WebKit/public/web/WebViewClient.h"
 #include "ui/base/window_open_disposition.h"
+#include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/rect_f.h"
 #include "ui/surface/transport_dib.h"
 
 #if defined(OS_ANDROID)
