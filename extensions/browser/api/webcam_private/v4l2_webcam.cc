a786b173f352fa0a7545a02c97a24f2d1581208b
xdai <xdai@chromium.org>
2015-08-31 15:34:37 -0700

    Add support to control the pan/tilt speed of VISCA camera.
    BUG=520628
    
    Review URL: https://codereview.chromium.org/1293863002
    
    Cr-Commit-Position: refs/heads/master@{#346496}

--- extensions/browser/api/webcam_private/v4l2_webcam.cc
+++ extensions/browser/api/webcam_private/v4l2_webcam.cc
@@ -104,11 +104,15 @@ void V4L2Webcam::GetZoom(const GetPTZCompleteCallback& callback) {
   callback.Run(success, value);
 }
 
-void V4L2Webcam::SetPan(int value, const SetPTZCompleteCallback& callback) {
+void V4L2Webcam::SetPan(int value,
+                        int pan_speed,
+                        const SetPTZCompleteCallback& callback) {
   callback.Run(SetWebcamParameter(fd_.get(), V4L2_CID_PAN_ABSOLUTE, value));
 }
 
-void V4L2Webcam::SetTilt(int value, const SetPTZCompleteCallback& callback) {
+void V4L2Webcam::SetTilt(int value,
+                         int tilt_speed,
+                         const SetPTZCompleteCallback& callback) {
   callback.Run(SetWebcamParameter(fd_.get(), V4L2_CID_TILT_ABSOLUTE, value));
 }
 
@@ -117,6 +121,7 @@ void V4L2Webcam::SetZoom(int value, const SetPTZCompleteCallback& callback) {
 }
 
 void V4L2Webcam::SetPanDirection(PanDirection direction,
+                                 int pan_speed,
                                  const SetPTZCompleteCallback& callback) {
   int direction_value = 0;
   switch (direction) {
@@ -137,6 +142,7 @@ void V4L2Webcam::SetPanDirection(PanDirection direction,
 }
 
 void V4L2Webcam::SetTiltDirection(TiltDirection direction,
+                                  int tilt_speed,
                                   const SetPTZCompleteCallback& callback) {
   int direction_value = 0;
   switch (direction) {
