2d13d1508803ba7a9e1edc405efd291d0dd8aafa
zhenw <zhenw@chromium.org>
2015-08-31 10:28:17 -0700

    Reland again [Startup Tracing] Add --trace-config-file flag
    Original CL: https://codereview.chromium.org/1315463002/
    1st reland: https://codereview.chromium.org/1317333002/
    
    --------
    
    This CL adds --trace-config-file flag. Please see trace_config_file.h for details.
    
    Design doc:
    https://docs.google.com/document/d/1PgdXUOJF3WtEmYWUyGRbC2Fz2ICCZKO9jPvpLPRSHH8/edit?usp=sharing
    
    BUG=317481, 482098
    TBR=dsinclair@chromium.org,blundell@chromium.org,sievers@chromium.org,sky@chromium.org,msw@chromium.org
    
    Review URL: https://codereview.chromium.org/1309243004
    
    Cr-Commit-Position: refs/heads/master@{#346407}

--- content/browser/plugin_process_host.cc
+++ content/browser/plugin_process_host.cc
@@ -223,6 +223,7 @@ bool PluginProcessHost::Init(const WebPluginInfo& info) {
     switches::kLogPluginMessages,
     switches::kNoSandbox,
     switches::kPluginStartupDialog,
+    switches::kTraceConfigFile,
     switches::kTraceStartup,
     switches::kUseGL,
     switches::kForceDeviceScaleFactor,
--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1348,6 +1348,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kTestType,
     switches::kTouchEvents,
     switches::kTouchTextSelectionStrategy,
+    switches::kTraceConfigFile,
     switches::kTraceToConsole,
     // This flag needs to be propagated to the renderer process for
     // --in-process-webgl.
@@ -1417,7 +1418,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
                                  arraysize(kSwitchNames));
 
   if (browser_cmd.HasSwitch(switches::kTraceStartup) &&
-      BrowserMainLoop::GetInstance()->is_tracing_startup()) {
+      BrowserMainLoop::GetInstance()->is_tracing_startup_for_duration()) {
     // Pass kTraceStartup switch to renderer only if startup tracing has not
     // finished.
     renderer_cmd->AppendSwitchASCII(
