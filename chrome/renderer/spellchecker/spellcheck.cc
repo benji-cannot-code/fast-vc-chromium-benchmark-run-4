05948f276d032f8f11764d0a576f3aa0364a66a0
dylanking <dylanking@google.com>
2015-08-31 16:06:28 -0700

    Disable Clank spellchecking on Svelte devices
    Disable Clank spellchecking on Svelte devices
    because it's a feature that uses a nontrivial
    amount of RAM.
    
    BUG=415302
    
    Review URL: https://codereview.chromium.org/1316263002
    
    Cr-Commit-Position: refs/heads/master@{#346509}

--- chrome/renderer/spellchecker/spellcheck.cc
+++ chrome/renderer/spellchecker/spellcheck.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/single_thread_task_runner.h"
+#include "base/sys_info.h"
 #include "base/thread_task_runner_handle.h"
 #include "chrome/common/channel_info.h"
 #include "chrome/common/chrome_switches.h"
@@ -596,6 +597,9 @@ void SpellCheck::CreateTextCheckingResults(
 
 bool SpellCheck::IsSpellcheckEnabled() {
 #if defined(OS_ANDROID)
+  if (base::SysInfo::IsLowEndDevice())
+    return false;
+
   version_info::Channel channel = chrome::GetChannel();
   if (channel == version_info::Channel::DEV ||
       channel == version_info::Channel::CANARY) {
