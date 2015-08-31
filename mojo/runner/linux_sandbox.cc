eb1268b6150256ed223df0cd902f450a69749487
erg <erg@chromium.org>
2015-08-31 14:11:18 -0700

    mandoline: lock down the linux sandbox more.
    Previously, the mandoline sandbox allowed all system calls except for
    access()/open()/faccessat()/openat(). This patch now uses the baseline
    sandboxing policy (which will error on many common syscalls and will
    crash on unwhitelisted calls). Added a few syscalls that we need for the
    compositor to the explicit allow list.
    
    BUG=492524
    
    Review URL: https://codereview.chromium.org/1318063006
    
    Cr-Commit-Position: refs/heads/master@{#346469}

--- mojo/runner/linux_sandbox.cc
+++ mojo/runner/linux_sandbox.cc
@@ -15,7 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sys_info.h"
 #include "sandbox/linux/bpf_dsl/policy.h"
 #include "sandbox/linux/bpf_dsl/trap_registry.h"
+#include "sandbox/linux/seccomp-bpf-helpers/baseline_policy.h"
 #include "sandbox/linux/seccomp-bpf-helpers/sigsys_handlers.h"
+#include "sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions.h"
 #include "sandbox/linux/seccomp-bpf-helpers/syscall_sets.h"
 #include "sandbox/linux/seccomp-bpf/sandbox_bpf.h"
 #include "sandbox/linux/services/credentials.h"
@@ -63,7 +65,7 @@ intptr_t SandboxSIGSYSHandler(const struct sandbox::arch_seccomp_data& args,
   }
 }
 
-class SandboxPolicy : public sandbox::bpf_dsl::Policy {
+class SandboxPolicy : public sandbox::BaselinePolicy {
  public:
   explicit SandboxPolicy(sandbox::syscall_broker::BrokerProcess* broker_process)
       : broker_process_(broker_process) {}
@@ -78,9 +80,15 @@ class SandboxPolicy : public sandbox::bpf_dsl::Policy {
       case __NR_faccessat:
       case __NR_openat:
         return sandbox::bpf_dsl::Trap(SandboxSIGSYSHandler, broker_process_);
+      case __NR_sched_getaffinity:
+        return sandbox::RestrictSchedTarget(policy_pid(), sysno);
+      case __NR_ftruncate:
+      case __NR_getrlimit:
+      case __NR_uname:
+        return sandbox::bpf_dsl::Allow();
     }
 
-    return sandbox::bpf_dsl::Allow();
+    return BaselinePolicy::EvaluateSyscall(sysno);
   }
 
  private:
