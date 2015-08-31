36bba1ba33795ef1192b8cc0fe0a064f7552948a
dyen <dyen@chromium.org>
2015-08-31 13:52:34 -0700

    Added Channel ID to GPU Channel Host.
    In preperation for the new sync point system, we need to identify
    sync points using the channel ID the sync point originates from. This
    CL adds the extra information necessary.
    
    BUG=514815
    
    Review URL: https://codereview.chromium.org/1319273002
    
    Cr-Commit-Position: refs/heads/master@{#346465}

--- content/common/gpu/client/gpu_channel_host.h
+++ content/common/gpu/client/gpu_channel_host.h
@@ -84,6 +84,7 @@ class GpuChannelHost : public IPC::Sender,
   // Must be called on the main thread (as defined by the factory).
   static scoped_refptr<GpuChannelHost> Create(
       GpuChannelHostFactory* factory,
+      int channel_id,
       const gpu::GPUInfo& gpu_info,
       const IPC::ChannelHandle& channel_handle,
       base::WaitableEvent* shutdown_event,
@@ -95,6 +96,8 @@ class GpuChannelHost : public IPC::Sender,
     DCHECK(channel_filter_.get());
     return channel_filter_->IsLost();
   }
+
+  int channel_id() const { return channel_id_; }
 
   // The GPU stats reported by the GPU process.
   const gpu::GPUInfo& gpu_info() const { return gpu_info_; }
@@ -237,6 +240,7 @@ class GpuChannelHost : public IPC::Sender,
   };
 
   GpuChannelHost(GpuChannelHostFactory* factory,
+                 int channel_id,
                  const gpu::GPUInfo& gpu_info,
                  gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager);
   ~GpuChannelHost() override;
@@ -253,6 +257,7 @@ class GpuChannelHost : public IPC::Sender,
   // - |channel_| and |stream_flush_info_|, protected by |context_lock_|
   GpuChannelHostFactory* const factory_;
 
+  const int channel_id_;
   const gpu::GPUInfo gpu_info_;
 
   scoped_refptr<MessageFilter> channel_filter_;
--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -1694,6 +1694,7 @@ GpuChannelHost* RenderThreadImpl::EstablishGpuChannelSync(
 
   gpu_channel_ =
       GpuChannelHost::Create(this,
+                             client_id,
                              gpu_info,
                              channel_handle,
                              ChildProcess::current()->GetShutDownEvent(),
