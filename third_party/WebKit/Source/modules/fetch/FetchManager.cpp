fae3e376d19748d17bdf98c6bd7c15cd8737f658
kalman@chromium.org <kalman@chromium.org>
2015-08-31 18:54:22 +0000

    Allowing registering arbitrary schemes as supporting the fetch API.
    Currently it's only exposed to the HTTP family, but now that Chrome
    extensions support service workers they need to support the fetch API.
    This patch adds registerURLSchemeAsSupportingFetchAPI to the Blink API,
    queried by the fetch API code as shouldTreatURLSchemeAsSupportingFetchAPI.
    
    BUG=466876
    R=yhirano@chromium.org, dcheng@chromium.org, pdr@chromium.org
    
    Review URL: https://codereview.chromium.org/1313733006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201492 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/fetch/FetchManager.cpp
+++ third_party/WebKit/Source/modules/fetch/FetchManager.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/network/ResourceError.h"
 #include "platform/network/ResourceRequest.h"
 #include "platform/network/ResourceResponse.h"
+#include "platform/weborigin/SchemeRegistry.h"
 #include "platform/weborigin/SecurityOrigin.h"
 #include "public/platform/WebURLRequest.h"
 #include "wtf/HashSet.h"
@@ -397,7 +398,9 @@ void FetchManager::Loader::start()
     }
 
     // "- |request|'s url's scheme is not one of 'http' and 'https'"
-    if (!m_request->url().protocolIsInHTTPFamily()) {
+    // This may include other HTTP-like schemes if the embedder has added them
+    // to SchemeRegistry::registerURLSchemeAsSupportingFetchAPI.
+    if (!SchemeRegistry::shouldTreatURLSchemeAsSupportingFetchAPI(m_request->url().protocol())) {
         // "A network error."
         performNetworkError("Fetch API cannot load " + m_request->url().string() + ". URL scheme must be \"http\" or \"https\" for CORS request.");
         return;
@@ -441,7 +444,7 @@ void FetchManager::Loader::performBasicFetch()
 {
     // "To perform a basic fetch using |request|, switch on |request|'s url's
     // scheme, and run the associated steps:"
-    if (m_request->url().protocolIsInHTTPFamily()) {
+    if (SchemeRegistry::shouldTreatURLSchemeAsSupportingFetchAPI(m_request->url().protocol())) {
         // "Return the result of performing an HTTP fetch using |request|."
         performHTTPFetch(false, false);
     } else {
@@ -457,7 +460,7 @@ void FetchManager::Loader::performNetworkError(const String& message)
 
 void FetchManager::Loader::performHTTPFetch(bool corsFlag, bool corsPreflightFlag)
 {
-    ASSERT(m_request->url().protocolIsInHTTPFamily());
+    ASSERT(SchemeRegistry::shouldTreatURLSchemeAsSupportingFetchAPI(m_request->url().protocol()));
     // CORS preflight fetch procedure is implemented inside DocumentThreadableLoader.
 
     // "1. Let |HTTPRequest| be a copy of |request|, except that |HTTPRequest|'s
