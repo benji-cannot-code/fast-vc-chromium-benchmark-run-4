15c18da12e0f9c034b686f5ecd2f11cc3785caee
kbr <kbr@chromium.org>
2015-08-31 12:51:06 -0700

    Fix two small bugs in transform feedback support.
    Support NULL arg to bindBufferBase and
    TRANSFORM_FEEDBACK_PRIMITIVES_WRITTEN query.
    
    Makes more of the transformfeedback test pass, but it's still
    generating zeroed data.
    
    BUG=526748
    
    Review URL: https://codereview.chromium.org/1320463007
    
    Cr-Commit-Position: refs/heads/master@{#346445}

--- gpu/command_buffer/build_gles2_cmd_buffer.py
+++ gpu/command_buffer/build_gles2_cmd_buffer.py
@@ -2144,7 +2144,7 @@ _FUNCTION_INFO = {
   },
   'BindBufferBase': {
     'type': 'Bind',
-    'id_mapping': [ 'Buffer' ],
+    'decoder_func': 'DoBindBufferBase',
     'gen_func': 'GenBuffersARB',
     'unsafe': True,
   },
