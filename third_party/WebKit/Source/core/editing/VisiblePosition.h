98a6c9a4e9a00b75c2151bea276e85da32730c37
yosin@chromium.org <yosin@chromium.org>
2015-08-31 20:01:47 +0000

    Move {next,previous}PositionOf() for VisiblePosition to VisibleUnits.cpp
    This patch moves |{next,previous}PositionOf()| for |VisiblePosition| to
    "VisibleUnits.cpp" form "VisiblePosition.cpp" to make "VisiblePosition.cpp"
    to simpler for improving code health and ease of templatizing |VisiblePosition|
    class to introduce composed tree version.
    
    This patch is a preparation of making selection to handle granularity for web
    component, http://crrev.com/1277863002
    
    BUG=513568
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1307023005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201500 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/VisiblePosition.h
+++ third_party/WebKit/Source/core/editing/VisiblePosition.h
@@ -28,11 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define VisiblePosition_h
 
 #include "core/CoreExport.h"
-#include "core/editing/EditingBoundary.h"
 #include "core/editing/PositionWithAffinity.h"
 #include "core/editing/TextAffinity.h"
 #include "platform/heap/Handle.h"
-#include "platform/text/TextDirection.h"
 
 namespace blink {
 
@@ -147,11 +145,6 @@ VisiblePosition honorEditingBoundaryAtOrBefore(const VisiblePosition&, const Pos
 PositionWithAffinity honorEditingBoundaryAtOrBeforeOf(const PositionWithAffinity&, const Position& anchor);
 PositionInComposedTreeWithAffinity honorEditingBoundaryAtOrBeforeOf(const PositionInComposedTreeWithAffinity&, const PositionInComposedTree& anchor);
 VisiblePosition honorEditingBoundaryAtOrAfter(const VisiblePosition&, const Position& anchor);
-
-// TODO(yosin) We should move |nextPositionOf()| to "VisibleUnits.cpp".
-VisiblePosition nextPositionOf(const VisiblePosition&, EditingBoundaryCrossingRule = CanCrossEditingBoundary);
-// TODO(yosin) We should move |previousPositionOf()| to "VisibleUnits.cpp"
-VisiblePosition previousPositionOf(const VisiblePosition&, EditingBoundaryCrossingRule = CanCrossEditingBoundary);
 
 CORE_EXPORT VisiblePosition visiblePositionOf(const Position&, TextAffinity = VP_DEFAULT_AFFINITY);
 CORE_EXPORT VisiblePosition visiblePositionOf(const PositionInComposedTree&, TextAffinity = VP_DEFAULT_AFFINITY);
