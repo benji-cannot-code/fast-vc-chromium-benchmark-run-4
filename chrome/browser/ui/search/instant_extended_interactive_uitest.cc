1ed4a00bafb7b10341883a332f0e5bcf803f4321
daniimms <daniimms@amazon.com>
2015-08-31 16:30:05 -0700

    Don't run InstantExtendedPrefetchTest when search suggest is disabled
    InstantExtendedPrefetchTest.* was failing on machines that used a device policy
    to disable the prefs::kSearchSuggestEnabled preferences. This change attempts
    to set the preference to true for each test, if it cannot the tests will be
    aborted and pass successfully.
    
    BUG=NONE
    
    Review URL: https://codereview.chromium.org/1292583007
    
    Cr-Commit-Position: refs/heads/master@{#346516}

--- chrome/browser/ui/search/instant_extended_interactive_uitest.cc
+++ chrome/browser/ui/search/instant_extended_interactive_uitest.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/theme_source.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/instant_types.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/interactive_test_utils.h"
@@ -815,6 +816,12 @@ IN_PROC_BROWSER_TEST_F(InstantExtendedTest,
 #define MAYBE_SetPrefetchQuery SetPrefetchQuery
 #endif
 IN_PROC_BROWSER_TEST_F(InstantExtendedPrefetchTest, MAYBE_SetPrefetchQuery) {
+  // Skip the test if suggest support is disabled, since this is generally due
+  // to policy and can't be overridden.
+  if (!browser()->profile()->GetPrefs()->GetBoolean(
+      prefs::kSearchSuggestEnabled)) {
+    return;
+  }
   ASSERT_NO_FATAL_FAILURE(SetupInstant(browser()));
   FocusOmnibox();
 
@@ -884,6 +891,12 @@ IN_PROC_BROWSER_TEST_F(InstantExtendedPrefetchTest, MAYBE_SetPrefetchQuery) {
 #endif
 IN_PROC_BROWSER_TEST_F(InstantExtendedPrefetchTest,
                        MAYBE_ClearPrefetchedResults) {
+  // Skip the test if suggest support is disabled, since this is generally due
+  // to policy and can't be overridden.
+  if (!browser()->profile()->GetPrefs()->GetBoolean(
+      prefs::kSearchSuggestEnabled)) {
+    return;
+  }
   ASSERT_NO_FATAL_FAILURE(SetupInstant(browser()));
   FocusOmnibox();
 
