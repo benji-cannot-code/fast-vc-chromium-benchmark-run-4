6655d533a6a7b64b0a42bc4c49fa8fd7f540f986
tdanderson <tdanderson@chromium.org>
2015-08-31 08:12:58 -0700

    Update colour of inactive tab text for material design
    Update colour of inactive tab text to
    black for material design.
    
    BUG=524621
    TEST=manual
    
    Review URL: https://codereview.chromium.org/1322623005
    
    Cr-Commit-Position: refs/heads/master@{#346385}

--- chrome/browser/themes/theme_properties.cc
+++ chrome/browser/themes/theme_properties.cc
@@ -51,7 +51,8 @@ const SkColor kDefaultColorTabText = SK_ColorBLACK;
 #if defined(OS_MACOSX)
 const SkColor kDefaultColorBackgroundTabText = SK_ColorBLACK;
 #else
-const SkColor kDefaultColorBackgroundTabText = SkColorSetRGB(64, 64, 64);
+const SkColor kDefaultColorBackgroundTabText[] = {
+    SkColorSetRGB(64, 64, 64), SK_ColorBLACK };
 #endif  // OS_MACOSX
 
 const SkColor kDefaultColorBookmarkText = SK_ColorBLACK;
@@ -340,7 +341,11 @@ SkColor ThemeProperties::GetDefaultColor(int id) {
     case COLOR_TAB_TEXT:
       return kDefaultColorTabText;
     case COLOR_BACKGROUND_TAB_TEXT:
+#if defined(OS_MACOSX)
       return kDefaultColorBackgroundTabText;
+#else
+      return kDefaultColorBackgroundTabText[mode];
+#endif  // OS_MACOSX
     case COLOR_BOOKMARK_TEXT:
       return kDefaultColorBookmarkText;
     case COLOR_NTP_BACKGROUND:
