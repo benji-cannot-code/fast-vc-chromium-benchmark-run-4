a670c16df648ddf047335f36d06f78ef57bddb91
sebsg <sebsg@chromium.org>
2015-08-30 18:48:39 -0700

    Changed the code handling chrome-search://thumb2/ so it can support a fallback url.
    TEST=ThumbnailSourceTest
    
    Review URL: https://codereview.chromium.org/1284933003
    
    Cr-Commit-Position: refs/heads/master@{#346330}

--- chrome/browser/search/suggestions/image_fetcher_impl.cc
+++ chrome/browser/search/suggestions/image_fetcher_impl.cc
@@ -15,7 +15,7 @@ namespace suggestions {
 
 ImageFetcherImpl::ImageFetcherImpl(
     net::URLRequestContextGetter* url_request_context)
-    : url_request_context_(url_request_context) {}
+    : delegate_(NULL), url_request_context_(url_request_context) {}
 
 ImageFetcherImpl::~ImageFetcherImpl() {}
 
