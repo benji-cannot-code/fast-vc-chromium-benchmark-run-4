d5adc2f673856fef74a1ce3ddb311edba6385e45
sammc <sammc@chromium.org>
2015-08-30 23:54:09 -0700

    Always access ProxyResolverV8Tracing::Bindings from the origin thread.
    If a ProxyResolverV8Tracing Job is cancelled, the Job may be deleted on
    the worker thread. This causes the ProxyResolverV8Tracing::Bindings to
    be deleted on the worker thread, violating the contract that it is
    deleted on the origin thread. This changes the bindings interface to
    only be accessed from the origin thread and deleted when the request
    completes or is cancelled.
    
    BUG=523908
    
    Review URL: https://codereview.chromium.org/1311743002
    
    Cr-Commit-Position: refs/heads/master@{#346349}

--- net/proxy/proxy_resolver_v8_tracing.cc
+++ net/proxy/proxy_resolver_v8_tracing.cc
@@ -194,8 +194,9 @@ class Job : public base::RefCountedThreadSafe<Job>,
   void HandleAlertOrError(bool is_alert, int line_number,
                           const base::string16& message);
   void DispatchBufferedAlertsAndErrors();
-  void DispatchAlertOrError(bool is_alert, int line_number,
-                            const base::string16& message);
+  void DispatchAlertOrErrorOnOriginThread(bool is_alert,
+                                          int line_number,
+                                          const base::string16& message);
 
   // The thread which called into ProxyResolverV8TracingImpl, and on which the
   // completion callback is expected to run.
@@ -396,6 +397,7 @@ void Job::Cancel() {
   // The worker thread might be blocked waiting for DNS.
   event_.Signal();
 
+  bindings_.reset();
   owned_self_reference_ = NULL;
 }
 
@@ -411,6 +413,7 @@ LoadState Job::GetLoadState() const {
 Job::~Job() {
   DCHECK(!pending_dns_);
   DCHECK(callback_.is_null());
+  DCHECK(!bindings_);
 }
 
 void Job::CheckIsOnWorkerThread() const {
@@ -464,6 +467,13 @@ void Job::NotifyCallerOnOriginLoop(int result) {
   if (cancelled_.IsSet())
     return;
 
+  DispatchBufferedAlertsAndErrors();
+
+  // This isn't the ordinary execution flow, however it is exercised by
+  // unit-tests.
+  if (cancelled_.IsSet())
+    return;
+
   DCHECK(!callback_.is_null());
   DCHECK(!pending_dns_);
 
@@ -475,6 +485,7 @@ void Job::NotifyCallerOnOriginLoop(int result) {
   ReleaseCallback();
   callback.Run(result);
 
+  bindings_.reset();
   owned_self_reference_ = NULL;
 }
 
@@ -531,7 +542,6 @@ void Job::ExecuteNonBlocking() {
   if (abandoned_)
     return;
 
-  DispatchBufferedAlertsAndErrors();
   NotifyCaller(result);
 }
 
@@ -705,11 +715,8 @@ void Job::DoDnsOperation() {
   // Check if the request was cancelled as a side-effect of calling into the
   // HostResolver. This isn't the ordinary execution flow, however it is
   // exercised by unit-tests.
-  if (cancelled_.IsSet()) {
-    if (!pending_dns_completed_synchronously_)
-      host_resolver()->CancelRequest(dns_request);
+  if (cancelled_.IsSet())
     return;
-  }
 
   if (pending_dns_completed_synchronously_) {
     OnDnsOperationComplete(result);
@@ -842,7 +849,9 @@ void Job::HandleAlertOrError(bool is_alert,
 
   if (blocking_dns_) {
     // In blocking DNS mode the events can be dispatched immediately.
-    DispatchAlertOrError(is_alert, line_number, message);
+    origin_runner_->PostTask(
+        FROM_HERE, base::Bind(&Job::DispatchAlertOrErrorOnOriginThread, this,
+                              is_alert, line_number, message));
     return;
   }
 
@@ -858,6 +867,7 @@ void Job::HandleAlertOrError(bool is_alert,
   // memory. Consider a script which does megabytes worth of alerts().
   // Avoid this by falling back to blocking mode.
   if (alerts_and_errors_byte_cost_ > kMaxAlertsAndErrorsBytes) {
+    alerts_and_errors_.clear();
     ScheduleRestartWithBlockingDns();
     return;
   }
@@ -867,28 +877,18 @@ void Job::HandleAlertOrError(bool is_alert,
 }
 
 void Job::DispatchBufferedAlertsAndErrors() {
-  CheckIsOnWorkerThread();
-  DCHECK(!blocking_dns_);
-  DCHECK(!abandoned_);
-
+  CheckIsOnOriginThread();
   for (size_t i = 0; i < alerts_and_errors_.size(); ++i) {
     const AlertOrError& x = alerts_and_errors_[i];
-    DispatchAlertOrError(x.is_alert, x.line_number, x.message);
+    DispatchAlertOrErrorOnOriginThread(x.is_alert, x.line_number, x.message);
   }
 }
 
-void Job::DispatchAlertOrError(bool is_alert,
-                               int line_number,
-                               const base::string16& message) {
-  CheckIsOnWorkerThread();
+void Job::DispatchAlertOrErrorOnOriginThread(bool is_alert,
+                                             int line_number,
+                                             const base::string16& message) {
+  CheckIsOnOriginThread();
 
-  // Note that the handling of cancellation is racy with regard to
-  // alerts/errors. The request might get cancelled shortly after this
-  // check! (There is no lock being held to guarantee otherwise).
-  //
-  // If this happens, then some information will be logged needlessly, however
-  // the Bindings are responsible for handling this case so it shouldn't cause
-  // problems.
   if (cancelled_.IsSet())
     return;
 
