6dac5949640dc5d2e07ab56386367e5ae77875f4
benwells <benwells@chromium.org>
2015-09-01 16:38:52 -0700

    Disable material design downloads UI from some tests.
    The new material design downloads UI is very slow, and causes some tests
    to timeout on memory bots.
    
    BUG=526577
    
    Review URL: https://codereview.chromium.org/1311333005
    
    Cr-Commit-Position: refs/heads/master@{#346785}

--- chrome/browser/ui/find_bar/find_bar_host_browsertest.cc
+++ chrome/browser/ui/find_bar/find_bar_host_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/command_line.h"
 #include "base/files/file_util.h"
 #include "base/message_loop/message_loop.h"
 #include "base/prefs/pref_service.h"
@@ -25,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/find_bar/find_notification_details.h"
 #include "chrome/browser/ui/find_bar/find_tab_helper.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/find_in_page_observer.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -82,6 +84,13 @@ class FindInPageControllerTest : public InProcessBrowserTest {
  public:
   FindInPageControllerTest() {
     chrome::DisableFindBarAnimationsDuringTesting(true);
+  }
+
+  // Disable new downloads UI as it is very very slow. https://crbug.com/526577
+  // TODO(dbeam): remove this once the downloads UI is not slow.
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    InProcessBrowserTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitch(switches::kDisableMaterialDesignDownloads);
   }
 
  protected:
--- chrome/test/base/web_ui_browser_test_browsertest.cc
+++ chrome/test/base/web_ui_browser_test_browsertest.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/command_line.h"
 #include "base/values.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "chrome/test/base/web_ui_browser_test.h"
@@ -28,6 +30,13 @@ class WebUIBrowserExpectFailTest : public WebUIBrowserTest {
   WebUIBrowserExpectFailTest() {
     EXPECT_FALSE(s_test_);
     s_test_ = this;
+  }
+
+  // Disable new downloads UI as it is very very slow. https://crbug.com/526577
+  // TODO(dbeam): remove this once the downloads UI is not slow.
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    WebUIBrowserTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitch(switches::kDisableMaterialDesignDownloads);
   }
 
  protected:
