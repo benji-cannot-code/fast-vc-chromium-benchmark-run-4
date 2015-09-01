217933f30a3177925843bf3315dfd263e0440fc5
alexclarke@chromium.org <alexclarke@chromium.org>
2015-09-01 17:24:49 +0000

    Make m_executeScriptsWaitingForResourcesTimer a loading task
    We would like to be able to prioritize loading tasks, but in order to
    do that we need to make sure loading tasks are posted to the right queue
    to make sure tasks run in the expected order.  If this task is posted as
    a timer, and loading tasks are prioritzed, it's possible
    FrameHostMsg_DidStopLoading will be sent before the corresponding
    FrameHostMsg_DidStartLoading ipc which causes various browser tests to
    break.
    
    BUG=497761,510398
    
    Review URL: https://codereview.chromium.org/1312353004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201563 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -213,6 +213,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/Platform.h"
 #include "wtf/CurrentTime.h"
 #include "wtf/DateMath.h"
+#include "wtf/Functional.h"
 #include "wtf/HashFunctions.h"
 #include "wtf/MainThread.h"
 #include "wtf/StdLibExtras.h"
@@ -382,7 +383,7 @@ Document::Document(const DocumentInit& initializer, DocumentClassFlags documentC
     , m_paginatedForScreen(false)
     , m_compatibilityMode(NoQuirksMode)
     , m_compatibilityModeLocked(false)
-    , m_executeScriptsWaitingForResourcesTimer(this, &Document::executeScriptsWaitingForResourcesTimerFired)
+    , m_executeScriptsWaitingForResourcesTask(WTF::bind(&Document::executeScriptsWaitingForResources, this))
     , m_hasAutofocused(false)
     , m_clearFocusedElementTimer(this, &Document::clearFocusedElementTimerFired)
     , m_domTreeVersion(++s_globalTreeVersion)
@@ -2962,7 +2963,8 @@ void Document::didRemoveAllPendingStylesheet()
 
 void Document::didLoadAllScriptBlockingResources()
 {
-    m_executeScriptsWaitingForResourcesTimer.startOneShot(0, FROM_HERE);
+    Platform::current()->currentThread()->scheduler()->postLoadingTask(
+        FROM_HERE, m_executeScriptsWaitingForResourcesTask.cancelAndCreate());
 
     if (frame())
         frame()->loader().client()->didRemoveAllPendingStylesheet();
@@ -2971,7 +2973,7 @@ void Document::didLoadAllScriptBlockingResources()
         view()->processUrlFragment(m_url);
 }
 
-void Document::executeScriptsWaitingForResourcesTimerFired(Timer<Document>*)
+void Document::executeScriptsWaitingForResources()
 {
     if (!isRenderingReady())
         return;
