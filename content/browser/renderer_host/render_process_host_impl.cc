71cbee30d47d80c76b7702817567981eb095b81f
watk <watk@chromium.org>
2015-09-01 15:55:29 -0700

    Remove flag --enable-overlay-fullscreen-video
    The flag --force-overlay-fullscreen-video replaces this for forcing
    overlay video for testing. Otherwise, the overlay policy is determined
    dynamically depending by the WebMediaPlayer.
    
    Depends on blink change https://codereview.chromium.org/1314983004
    
    BUG=511376
    TEST=virtual/android/fullscreen layout test, manual
    
    Review URL: https://codereview.chromium.org/1309673005
    
    Cr-Commit-Position: refs/heads/master@{#346767}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1295,7 +1295,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kEnableLogging,
     switches::kEnableMemoryBenchmarking,
     switches::kEnableNetworkInformation,
-    switches::kEnableOverlayFullscreenVideo,
     switches::kEnableOverlayScrollbar,
     switches::kEnablePinch,
     switches::kEnablePluginPlaceholderTesting,
--- content/shell/browser/blink_test_controller.cc
+++ content/shell/browser/blink_test_controller.cc
@@ -532,8 +532,6 @@ void BlinkTestController::OnImageDump(const std::string& actual_pixel_hash,
 
     bool discard_transparency = true;
     if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-            switches::kEnableOverlayFullscreenVideo) ||
-        base::CommandLine::ForCurrentProcess()->HasSwitch(
             switches::kForceOverlayFullscreenVideo))
       discard_transparency = false;
 
