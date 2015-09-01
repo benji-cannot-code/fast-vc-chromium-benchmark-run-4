8601320840ca700527b2bae7b07db50db53c4c4c
mathp <mathp@chromium.org>
2015-09-01 07:10:50 -0700

    [Autofill] Trigger Autofill submit  messages on AJAX and same page navigations.
    BUG=526777
    TEST=FormAutocompleteTest
    
    Review URL: https://codereview.chromium.org/1325513005
    
    Cr-Commit-Position: refs/heads/master@{#346648}

--- components/autofill/content/renderer/autofill_agent.h
+++ components/autofill/content/renderer/autofill_agent.h
@@ -56,7 +56,8 @@ class AutofillAgent : public content::RenderFrameObserver,
   virtual ~AutofillAgent();
 
  private:
-  // Functor used as a simplified comparison function for FormData.
+  // Functor used as a simplified comparison function for FormData. Only
+  // compares forms at a high level (notably name, origin, action).
   struct FormDataCompare {
     bool operator()(const FormData& lhs, const FormData& rhs) const;
   };
@@ -162,6 +163,9 @@ class AutofillAgent : public content::RenderFrameObserver,
   void OnPreviewPasswordSuggestion(const base::string16& username,
                                    const base::string16& password);
 
+  // Called when a same-page navigation is detected.
+  void OnSamePageNavigationCompleted();
+
   // Called when interactive autocomplete finishes. |message| is printed to
   // the console if non-empty.
   void OnRequestAutocompleteResult(
@@ -249,6 +253,9 @@ class AutofillAgent : public content::RenderFrameObserver,
 
   // The form element currently requesting an interactive autocomplete.
   blink::WebFormElement in_flight_request_form_;
+
+  // Last form which was interacted with by the user.
+  blink::WebFormElement last_interacted_form_;
 
   // Was the query node autofilled prior to previewing the form?
   bool was_query_node_autofilled_;
