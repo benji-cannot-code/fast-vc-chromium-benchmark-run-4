5a58943a45c06c2333f58edd219adc998aacfebb
jdduke <jdduke@chromium.org>
2015-09-01 16:07:30 -0700

    [Android] Decouple layer attachment and overscroll activation
    Currently, RWHVAndroid will disable the overscroll glow if the delegated
    layer is detached. However, there are valid usescases for detaching
    the delegated layer while the web content area is still active and
    focused. There's really no need to disable the overscroll glow in this
    case, so just remove the code that does so.
    
    BUG=518177
    
    Review URL: https://codereview.chromium.org/1309973007
    
    Cr-Commit-Position: refs/heads/master@{#346774}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -1403,8 +1403,6 @@ void RenderWidgetHostViewAndroid::AttachLayers() {
     return;
 
   content_view_core_->AttachLayer(layer_);
-  if (overscroll_controller_)
-    overscroll_controller_->Enable();
   layer_->SetHideLayerAndSubtree(!is_showing_);
 }
 
@@ -1416,8 +1414,6 @@ void RenderWidgetHostViewAndroid::RemoveLayers() {
     return;
 
   content_view_core_->RemoveLayer(layer_);
-  if (overscroll_controller_)
-    overscroll_controller_->Disable();
 }
 
 void RenderWidgetHostViewAndroid::RequestVSyncUpdate(uint32 requests) {
