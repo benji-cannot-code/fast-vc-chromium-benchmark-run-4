33d3c73af9dc9af5d8b5910ff13c3f1f8ae2a4c6
fukino <fukino@chromium.org>
2015-09-01 03:00:05 -0700

    Add visibility to AddNewDirectoryOptions.
    This CL adds "visibility" property to AddNewDirectoryOptions, to pass visibility parameter from AddNewDirectory() to FilesInsertRequst.
    
    BUG=518750
    TEST=none
    
    Review URL: https://codereview.chromium.org/1317283002
    
    Cr-Commit-Position: refs/heads/master@{#346611}

--- components/drive/service/drive_service_interface.cc
+++ components/drive/service/drive_service_interface.cc
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace drive {
 
-AddNewDirectoryOptions::AddNewDirectoryOptions() {
+AddNewDirectoryOptions::AddNewDirectoryOptions()
+    : visibility(google_apis::drive::FILE_VISIBILITY_DEFAULT) {
 }
 
 AddNewDirectoryOptions::~AddNewDirectoryOptions() {
