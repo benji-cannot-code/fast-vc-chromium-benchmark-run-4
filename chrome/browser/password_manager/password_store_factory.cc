243be7cd89dc5dcf220568c8749850c2f63acfa7
vabr <vabr@chromium.org>
2015-09-01 05:13:21 -0700

    [PasswordManager] Move the sync-password-related methods to the sync layer
    Instead of asking PasswordManagerClient about whether a password is a sync credential, and the client delegating this to sync_metrics in //chrome/browser/*, the client interface should delegate this to the code in the sync layer of the password_manager component.
    
    TODOs for follow-up CLs:
    (1) SyncStoreResultFilter needs to be renamed to SyncCredentialsFilter.
    (2) PasswordManager.SyncCredentialFiltered histogram needs a better description.
    
    BUG=515108,474577,514230
    
    Review URL: https://codereview.chromium.org/1282023002
    
    Cr-Commit-Position: refs/heads/master@{#346631}

--- chrome/browser/password_manager/password_store_factory.cc
+++ chrome/browser/password_manager/password_store_factory.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/prefs/pref_service.h"
 #include "base/thread_task_runner_handle.h"
-#include "chrome/browser/password_manager/sync_metrics.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/glue/sync_start_util.h"
