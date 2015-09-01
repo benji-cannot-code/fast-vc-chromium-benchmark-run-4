6ec338d6638a94663b4f8f038180a4ecd45ad264
keishi@chromium.org <keishi@chromium.org>
2015-09-01 10:44:57 +0000

    Oilpan: Remove raw pointer to ExecutionContext from WorkerInspectorProxy
    Prepares moving WorkerInspectorProxy and WorkerInspectorProxy::PageInspector to the heap.
    
    BUG=509911
    
    Review URL: https://codereview.chromium.org/1253293002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201548 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorWorkerAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorWorkerAgent.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CoreExport.h"
 #include "core/InspectorFrontend.h"
 #include "core/inspector/InspectorBaseAgent.h"
+#include "core/workers/WorkerInspectorProxy.h"
 #include "wtf/Forward.h"
 #include "wtf/HashMap.h"
 
@@ -46,6 +47,7 @@ class WorkerInspectorProxy;
 typedef String ErrorString;
 
 class CORE_EXPORT InspectorWorkerAgent final : public InspectorBaseAgent<InspectorWorkerAgent, InspectorFrontend::Worker>, public InspectorBackendDispatcher::WorkerCommandHandler {
+    WTF_MAKE_NONCOPYABLE(InspectorWorkerAgent);
 public:
     static PassOwnPtrWillBeRawPtr<InspectorWorkerAgent> create(PageConsoleAgent*);
     ~InspectorWorkerAgent() override;
@@ -69,6 +71,32 @@ public:
 
     void setTracingSessionId(const String&);
 
+    class WorkerAgentClient final : public WorkerInspectorProxy::PageInspector {
+        WTF_MAKE_FAST_ALLOCATED_WILL_BE_REMOVED(InspectorWorkerAgent::WorkerAgentClient);
+    public:
+        static PassOwnPtrWillBeRawPtr<WorkerAgentClient> create(InspectorFrontend::Worker*, WorkerInspectorProxy*, const String& id, PageConsoleAgent*);
+        WorkerAgentClient(InspectorFrontend::Worker*, WorkerInspectorProxy*, const String& id, PageConsoleAgent*);
+        ~WorkerAgentClient() override;
+        DECLARE_VIRTUAL_TRACE();
+
+        String id() const { return m_id; }
+        WorkerInspectorProxy* proxy() const { return m_proxy; }
+
+        void connectToWorker();
+        void dispose();
+
+    private:
+        // WorkerInspectorProxy::PageInspector implementation
+        void dispatchMessageFromWorker(const String& message) override;
+        void workerConsoleAgentEnabled(WorkerGlobalScopeProxy*) override;
+
+        InspectorFrontend::Worker* m_frontend;
+        RawPtrWillBeMember<WorkerInspectorProxy> m_proxy;
+        String m_id;
+        bool m_connected;
+        RawPtrWillBeMember<PageConsoleAgent> m_consoleAgent;
+    };
+
 private:
     InspectorWorkerAgent(PageConsoleAgent*);
     void createWorkerAgentClientsForExistingWorkers();
@@ -82,8 +110,8 @@ private:
         String url;
         String id;
     };
-    class WorkerAgentClient;
-    typedef HashMap<String, WorkerAgentClient*> WorkerClients;
+
+    typedef WillBeHeapHashMap<String, OwnPtrWillBeMember<WorkerAgentClient>> WorkerClients;
     WorkerClients m_idToClient;
     typedef HashMap<WorkerInspectorProxy*, WorkerInfo> WorkerInfos;
     WorkerInfos m_workerInfos;
