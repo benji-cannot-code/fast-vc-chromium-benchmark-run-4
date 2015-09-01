6c7cbc2347428c9e554f451d026e3c5ad567c747
ricea@chromium.org <ricea@chromium.org>
2015-09-01 10:02:15 +0000

    Remove StaleWhileRevalidateCacheControl feature.
    This feature will not be used in Blink in the immediate future, so
    remove it.
    
    There are no callers or users.
    
    BUG=348877
    TEST=compiled
    
    Review URL: https://codereview.chromium.org/1325613002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201546 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
@@ -310,11 +310,6 @@ void WebRuntimeFeatures::enablePushMessagingData(bool enable)
     RuntimeEnabledFeatures::setPushMessagingDataEnabled(enable);
 }
 
-void WebRuntimeFeatures::enableStaleWhileRevalidateCacheControl(bool enable)
-{
-    RuntimeEnabledFeatures::setStaleWhileRevalidateCacheControlEnabled(enable);
-}
-
 void WebRuntimeFeatures::enableUnsafeES3APIs(bool enable)
 {
     RuntimeEnabledFeatures::setUnsafeES3APIsEnabled(enable);
