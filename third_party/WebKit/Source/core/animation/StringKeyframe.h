c166eb52b33260c4bf943c5a8fd0bf8ca7fcb741
alancutter@chromium.org <alancutter@chromium.org>
2015-09-01 06:39:25 +0000

    Web Animations API: Don't allow quirky CSS values in keyframes
    This patch makes the use of quirky CSS values like -webkit-text color
    and unitless dimensions fail to parse when used in element.animate().
    
    BUG=526921
    
    Review URL: https://codereview.chromium.org/1305893005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201524 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/StringKeyframe.h
+++ third_party/WebKit/Source/core/animation/StringKeyframe.h
@@ -96,7 +96,7 @@ public:
 
 private:
     StringKeyframe()
-        : m_propertySet(MutableStylePropertySet::create(HTMLQuirksMode))
+        : m_propertySet(MutableStylePropertySet::create(HTMLStandardMode))
     { }
 
     StringKeyframe(const StringKeyframe& copyFrom);
