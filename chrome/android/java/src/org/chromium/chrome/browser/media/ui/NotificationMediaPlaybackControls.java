e851c8baedc0d65262489e15c2dc1fc50c02a7cb
mlamouri <mlamouri@chromium.org>
2015-09-01 06:36:50 -0700

    Android Media Notifications: add some more metrics.
    This is adding an UMA to check when the user dismisses the notification
    or press the stop button. In addition, it is adding new enum values for
    play/pause requests coming from Android's MediaSession (ie. remote
    controls).
    
    BUG=526750
    
    Review URL: https://codereview.chromium.org/1325473004
    
    Cr-Commit-Position: refs/heads/master@{#346642}

--- chrome/android/java/src/org/chromium/chrome/browser/media/ui/NotificationMediaPlaybackControls.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/ui/NotificationMediaPlaybackControls.java
@@ -111,17 +111,25 @@ public class NotificationMediaPlaybackControls {
                 switch (event.getKeyCode()) {
                     case KeyEvent.KEYCODE_MEDIA_PLAY:
                         if (!sInstance.mMediaNotificationInfo.isPaused) break;
+                        MediaSessionUMA.recordPlay(
+                                MediaSessionUMA.MEDIA_SESSION_ACTION_SOURCE_MEDIA_SESSION);
                         sInstance.onPlaybackStateChanged(PLAYBACK_STATE_PLAYING);
                         break;
                     case KeyEvent.KEYCODE_MEDIA_PAUSE:
                         if (sInstance.mMediaNotificationInfo.isPaused) break;
+                        MediaSessionUMA.recordPause(
+                                MediaSessionUMA.MEDIA_SESSION_ACTION_SOURCE_MEDIA_SESSION);
                         sInstance.onPlaybackStateChanged(PLAYBACK_STATE_PAUSED);
                         break;
                     case KeyEvent.KEYCODE_HEADSETHOOK:
                     case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
                         if (sInstance.mMediaNotificationInfo.isPaused) {
+                            MediaSessionUMA.recordPlay(
+                                    MediaSessionUMA.MEDIA_SESSION_ACTION_SOURCE_MEDIA_SESSION);
                             sInstance.onPlaybackStateChanged(PLAYBACK_STATE_PLAYING);
                         } else {
+                            MediaSessionUMA.recordPause(
+                                    MediaSessionUMA.MEDIA_SESSION_ACTION_SOURCE_MEDIA_SESSION);
                             sInstance.onPlaybackStateChanged(PLAYBACK_STATE_PAUSED);
                         }
                         break;
@@ -132,6 +140,8 @@ public class NotificationMediaPlaybackControls {
             }
 
             if (ACTION_STOP.equals(action)) {
+                MediaSessionUMA.recordStop(
+                        MediaSessionUMA.MEDIA_SESSION_ACTION_SOURCE_MEDIA_NOTIFICATION);
                 sInstance.mMediaNotificationInfo.listener.onStop();
                 stopSelf();
                 return START_NOT_STICKY;
@@ -235,12 +245,16 @@ public class NotificationMediaPlaybackControls {
                 @Override
                 public void onPlay() {
                     if (!sInstance.mMediaNotificationInfo.isPaused) return;
+                    MediaSessionUMA.recordPlay(
+                            MediaSessionUMA.MEDIA_SESSION_ACTION_SOURCE_MEDIA_SESSION);
                     sInstance.onPlaybackStateChanged(PLAYBACK_STATE_PLAYING);
                 }
 
                 @Override
                 public void onPause() {
                     if (sInstance.mMediaNotificationInfo.isPaused) return;
+                    MediaSessionUMA.recordPause(
+                            MediaSessionUMA.MEDIA_SESSION_ACTION_SOURCE_MEDIA_SESSION);
                     sInstance.onPlaybackStateChanged(PLAYBACK_STATE_PAUSED);
                 }
     };
