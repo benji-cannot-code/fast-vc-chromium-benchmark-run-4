f18aa627fee6623cf50db604db590fb0a30c7d6b
michaelpg <michaelpg@chromium.org>
2015-09-01 15:00:53 -0700

    Refactor prefs.js for MD-Settings
    Motivations:
    * Make easier to understand and extend.
    * Eliminate potential infinite loops.
    * Be more precise and identify error states.
    * Remove extraneous set() calls (prevent excess Polymer notifications).
    
    Assumptions:
    * Elements should use Polymer.Base.set to update prefs
      to ensure they propagate.
    * Preferences should serialize to the same strings in
      JS and the C++ JSON store.
    
    BUG=512479
    R=dbeam@chromium.org,stevenjb@chromium.org
    TBR=stevenjb@chromium.org # because we talked extensively and this shouldn't wait another week
    
    Review URL: https://codereview.chromium.org/1287913005
    
    Cr-Commit-Position: refs/heads/master@{#346747}

--- chrome/browser/resources/settings/checkbox/checkbox.js
+++ chrome/browser/resources/settings/checkbox/checkbox.js
@@ -69,7 +69,7 @@ Polymer({
   /** @private */
   checkedChanged_: function() {
     if (this.pref) {
-      this.pref.value = this.getNewValue_(this.checked);
+      this.set('pref.value', this.getNewValue_(this.checked));
     }
   },
 
