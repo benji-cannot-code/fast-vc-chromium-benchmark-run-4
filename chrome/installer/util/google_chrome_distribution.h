2f830d53f21c78b6c78dbc09b675735d1c604b2c
gab <gab@chromium.org>
2015-09-02 07:44:10 -0700

    Make GetCommandExecuteImplClsid return the CLSID as a string directly.
    BUG=Unjustified single out-param.
    
    Review URL: https://codereview.chromium.org/1321143002
    
    Cr-Commit-Position: refs/heads/master@{#346915}

--- chrome/installer/util/google_chrome_distribution.h
+++ chrome/installer/util/google_chrome_distribution.h
@@ -66,7 +66,7 @@ class GoogleChromeDistribution : public BrowserDistribution {
 
   base::string16 GetUninstallRegPath() override;
 
-  bool GetCommandExecuteImplClsid(base::string16* handler_class_uuid) override;
+  base::string16 GetCommandExecuteImplClsid() override;
 
   void UpdateInstallStatus(
       bool system_install,
