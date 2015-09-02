17cc256417edd65e6268f0b20434f52ab50a19f3
guoweis <guoweis@chromium.org>
2015-09-01 17:25:36 -0700

    Add support for MediaPermissionDispatcher to use other permission types.
    MediaPermissionDispatcher becomes the common way for renderer process to query permission status through mojo services.
    
    BUG=520101
    
    Review URL: https://codereview.chromium.org/1314923007
    
    Cr-Commit-Position: refs/heads/master@{#346797}

--- content/renderer/media/media_permission_dispatcher.cc
+++ content/renderer/media/media_permission_dispatcher.cc
@@ -12,6 +12,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebUserGestureIndicator.h"
 #include "url/gurl.h"
 
+namespace {
+
+using Type = media::MediaPermission::Type;
+
+content::PermissionName MediaPermissionTypeToPermissionName(Type type) {
+  switch (type) {
+    case Type::PROTECTED_MEDIA_IDENTIFIER:
+      return content::PERMISSION_NAME_PROTECTED_MEDIA_IDENTIFIER;
+    case Type::AUDIO_CAPTURE:
+      return content::PERMISSION_NAME_AUDIO_CAPTURE;
+    case Type::VIDEO_CAPTURE:
+      return content::PERMISSION_NAME_VIDEO_CAPTURE;
+  }
+  NOTREACHED();
+  return content::PERMISSION_NAME_PROTECTED_MEDIA_IDENTIFIER;
+}
+
+}  // namespace
+
 namespace content {
 
 MediaPermissionDispatcher::MediaPermissionDispatcher(RenderFrame* render_frame)
@@ -33,7 +52,6 @@ void MediaPermissionDispatcher::HasPermission(
     const GURL& security_origin,
     const PermissionStatusCB& permission_status_cb) {
   DCHECK(thread_checker_.CalledOnValidThread());
-  CHECK_EQ(PROTECTED_MEDIA_IDENTIFIER, type);
 
   if (!permission_service_.get()) {
     render_frame()->GetServiceRegistry()->ConnectToRemoteService(
@@ -47,7 +65,7 @@ void MediaPermissionDispatcher::HasPermission(
   DVLOG(2) << __FUNCTION__ << ": request ID " << request_id;
 
   permission_service_->HasPermission(
-      PERMISSION_NAME_PROTECTED_MEDIA_IDENTIFIER, security_origin.spec(),
+      MediaPermissionTypeToPermissionName(type), security_origin.spec(),
       base::Bind(&MediaPermissionDispatcher::OnPermissionStatus,
                  base::Unretained(this), request_id));
 }
@@ -57,7 +75,6 @@ void MediaPermissionDispatcher::RequestPermission(
     const GURL& security_origin,
     const PermissionStatusCB& permission_status_cb) {
   DCHECK(thread_checker_.CalledOnValidThread());
-  CHECK_EQ(PROTECTED_MEDIA_IDENTIFIER, type);
 
   if (!permission_service_.get()) {
     render_frame()->GetServiceRegistry()->ConnectToRemoteService(
@@ -71,7 +88,7 @@ void MediaPermissionDispatcher::RequestPermission(
   DVLOG(2) << __FUNCTION__ << ": request ID " << request_id;
 
   permission_service_->RequestPermission(
-      PERMISSION_NAME_PROTECTED_MEDIA_IDENTIFIER, security_origin.spec(),
+      MediaPermissionTypeToPermissionName(type), security_origin.spec(),
       blink::WebUserGestureIndicator::isProcessingUserGesture(),
       base::Bind(&MediaPermissionDispatcher::OnPermissionStatus,
                  base::Unretained(this), request_id));
