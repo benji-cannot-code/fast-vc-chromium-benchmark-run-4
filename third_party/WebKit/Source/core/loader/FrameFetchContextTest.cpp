2e44f15d9a23f032ee6a1ce148c1a5f88d6af52a
sigbjornf@opera.com <sigbjornf@opera.com>
2015-09-02 21:51:17 +0000

    Oilpan: fix build after r201667.
    TBR=oilpan-reviews
    BUG=none
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1314773009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201670 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/FrameFetchContextTest.cpp
+++ third_party/WebKit/Source/core/loader/FrameFetchContextTest.cpp
@@ -75,6 +75,11 @@ private:
 class StubFrameOwner : public NoBaseWillBeGarbageCollectedFinalized<StubFrameOwner>, public FrameOwner {
     WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(StubFrameOwner);
 public:
+    static PassOwnPtrWillBeRawPtr<StubFrameOwner> create()
+    {
+        return adoptPtrWillBeNoop(new StubFrameOwner);
+    }
+
     DEFINE_INLINE_VIRTUAL_TRACE() { FrameOwner::trace(visitor); }
 
     bool isLocal() const override { return false; }
@@ -92,6 +97,7 @@ protected:
         documentLoader = DocumentLoader::create(&dummyPageHolder->frame(), ResourceRequest("http://www.example.com"), SubstituteData());
         document = toHTMLDocument(&dummyPageHolder->document());
         fetchContext = static_cast<FrameFetchContext*>(&documentLoader->fetcher()->context());
+        owner = StubFrameOwner::create();
         FrameFetchContext::provideDocumentToContext(*fetchContext, document.get());
     }
 
@@ -110,7 +116,7 @@ protected:
     FrameFetchContext* createChildFrame()
     {
         childClient = StubFrameLoaderClientWithParent::create(document->frame());
-        childFrame = LocalFrame::create(childClient.get(), document->frame()->host(), &owner);
+        childFrame = LocalFrame::create(childClient.get(), document->frame()->host(), owner.get());
         childFrame->setView(FrameView::create(childFrame.get(), IntSize(500, 500)));
         childFrame->init();
         childDocumentLoader = DocumentLoader::create(childFrame.get(), ResourceRequest("http://www.example.com"), SubstituteData());
@@ -127,12 +133,11 @@ protected:
     RefPtrWillBePersistent<Document> document;
     Persistent<FrameFetchContext> fetchContext;
 
-    OwnPtrWillBeRawPtr<StubFrameLoaderClientWithParent> childClient;
-    RefPtrWillBeRawPtr<LocalFrame> childFrame;
+    OwnPtrWillBePersistent<StubFrameLoaderClientWithParent> childClient;
+    RefPtrWillBePersistent<LocalFrame> childFrame;
     RefPtrWillBePersistent<DocumentLoader> childDocumentLoader;
     RefPtrWillBePersistent<Document> childDocument;
-    StubFrameOwner owner;
-
+    OwnPtrWillBePersistent<StubFrameOwner> owner;
 };
 
 class FrameFetchContextUpgradeTest : public FrameFetchContextTest {
