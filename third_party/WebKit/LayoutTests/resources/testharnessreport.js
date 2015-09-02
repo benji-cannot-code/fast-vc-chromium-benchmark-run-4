a4449644b2f230656446a12f1c4942c31ead2ead
tkent@chromium.org <tkent@chromium.org>
2015-09-02 08:04:30 +0000

    Fix flakiness of imported/web-platform-tests/html/syntax/parsing/html5lib_testsNN.html.
    Root cause:
    Our testharnessreport.js removes unnecessary text nodes in a callback registered
    by add_completion_callback() [1]. The html5lib tests add other completion
    callbacks to show failure reasons, and they run after the callback of
    testharnessreport.js.  So, the failure reasons remain in *-expected.txt.
    
    Resolution:
    testharness.js doesn't provide a way to specify that "This callback must run at
    the end of callbacks." So we delay the text nodes cleanup by setTimeout(done, 0).
    
    [1] https://github.com/w3c/testharness.js/blob/master/docs/api.md#callback-api
    
    BUG=527262
    
    Review URL: https://codereview.chromium.org/1303313006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201628 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/resources/testharnessreport.js
+++ third_party/WebKit/LayoutTests/resources/testharnessreport.js
@@ -127,6 +127,6 @@ add_completion_callback(function (tests, harness_status) {
     if (document.readyState === 'loading') {
         window.addEventListener('load', done);
     } else {
-        done();
+        setTimeout(done, 0);
     }
 });
