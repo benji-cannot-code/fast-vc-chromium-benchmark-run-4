cf924b6503be784e9040601f8cc1549e43c5bfa5
dcheng <dcheng@chromium.org>
2015-09-02 13:08:55 -0700

    Don't hardcode RenderView routing IDs.
    As it turns out, the RenderView's routing ID is *not* always 2.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1308033010
    
    Cr-Commit-Position: refs/heads/master@{#347002}

--- chrome/renderer/safe_browsing/phishing_classifier_delegate_browsertest.cc
+++ chrome/renderer/safe_browsing/phishing_classifier_delegate_browsertest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_message_filter.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/renderer/render_view.h"
 #include "content/public/test/browser_test_utils.h"
@@ -52,9 +53,6 @@ using ::testing::StrictMock;
 namespace safe_browsing {
 
 namespace {
-
-// The RenderFrame is routing ID 1, and the RenderView is 2.
-const int kRenderViewRoutingId = 2;
 
 class MockPhishingClassifier : public PhishingClassifier {
  public:
@@ -154,8 +152,7 @@ class PhishingClassifierDelegateTest : public InProcessBrowserTest {
 
   void SetUpOnMainThread() override {
     intercepting_filter_ = new InterceptingMessageFilter();
-    content::RenderView* render_view =
-        content::RenderView::FromRoutingID(kRenderViewRoutingId);
+    content::RenderView* render_view = GetRenderView();
 
     GetWebContents()->GetRenderProcessHost()->AddFilter(
         intercepting_filter_.get());
@@ -224,6 +221,11 @@ class PhishingClassifierDelegateTest : public InProcessBrowserTest {
     return browser()->tab_strip_model()->GetActiveWebContents();
   }
 
+  content::RenderView* GetRenderView() {
+    return content::RenderView::FromRoutingID(
+        GetWebContents()->GetRenderViewHost()->GetRoutingID());
+  }
+
   // Returns the URL that was loaded.
   GURL LoadHtml(const std::string& host, const std::string& content) {
     GURL::Replacements replace_host;
@@ -242,8 +244,7 @@ class PhishingClassifierDelegateTest : public InProcessBrowserTest {
   }
 
   void NavigateMainFrameInternal(const GURL& url) {
-    content::RenderView* render_view =
-        content::RenderView::FromRoutingID(kRenderViewRoutingId);
+    content::RenderView* render_view = GetRenderView();
     render_view->GetWebView()->mainFrame()->firstChild()->loadRequest(
         blink::WebURLRequest(url));
   }
