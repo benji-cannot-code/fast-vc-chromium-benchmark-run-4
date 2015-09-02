498b91823c81aa038ebc9e72e2f47b63cf01abe8
kalman <kalman@chromium.org>
2015-09-02 11:38:56 -0700

    Revert of Pass the v8::Context through the willDestroyServiceWorkerContextOnWorkerThread event. (patchset #3 id:60001 of https://codereview.chromium.org/1318643002/ )
    Reason for revert:
    Canary is frequently crashing, likely due to threading issues with this patch.
    
    Original issue's description:
    > Pass the v8::Context through the willDestroyServiceWorkerContextOnWorkerThread event.
    >
    > Currently it only passes a URL, and this isn't enough to uniquely identify the worker.
    > Update extension's dispatcher.cc to use the context.
    >
    > BUG=501569, 525965
    > R=kinuko@chromium.org, thestig@chromium.org
    >
    > Committed: https://crrev.com/4a0bb926cb1d83fab27b6ad866c04ba481f66268
    > Cr-Commit-Position: refs/heads/master@{#346424}
    
    TBR=thestig@chromium.org,kinuko@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=501569, 525965
    
    Review URL: https://codereview.chromium.org/1319603003
    
    Cr-Commit-Position: refs/heads/master@{#346978}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -1645,10 +1645,8 @@ ChromeContentRendererClient::DidInitializeServiceWorkerContextOnWorkerThread(
 }
 
 void ChromeContentRendererClient::WillDestroyServiceWorkerContextOnWorkerThread(
-    v8::Local<v8::Context> context,
     const GURL& url) {
 #if defined(ENABLE_EXTENSIONS)
-  extensions::Dispatcher::WillDestroyServiceWorkerContextOnWorkerThread(context,
-                                                                        url);
+  extensions::Dispatcher::WillDestroyServiceWorkerContextOnWorkerThread(url);
 #endif
 }
