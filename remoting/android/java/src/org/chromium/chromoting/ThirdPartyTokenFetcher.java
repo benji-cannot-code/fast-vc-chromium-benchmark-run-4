05a06d7561470f8877b99e936f3df4bdbd239184
lambroslambrou <lambroslambrou@chromium.org>
2015-09-02 10:40:44 -0700

    Fix deprecation warnings in remoting/android/
    ActionBarActivity replaced with AppCompatActivity
    Added getColor() to ApiCompatibilityUtils.
    
    Review URL: https://codereview.chromium.org/1322933002
    
    Cr-Commit-Position: refs/heads/master@{#346959}

--- remoting/android/java/src/org/chromium/chromoting/ThirdPartyTokenFetcher.java
+++ remoting/android/java/src/org/chromium/chromoting/ThirdPartyTokenFetcher.java
@@ -12,7 +12,6 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
@@ -217,7 +216,7 @@ public class ThirdPartyTokenFetcher {
      * together with its intent filter, by default. |OAuthRedirectActivity| is only enabled when
      * there is a pending token fetch request.
      */
-    public static class OAuthRedirectActivity extends ActionBarActivity {
+    public static class OAuthRedirectActivity extends Activity {
         @Override
         public void onStart() {
             super.onStart();
