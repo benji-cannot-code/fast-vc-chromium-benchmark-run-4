4797a1de284e190f190b32b15ddf277264f2dc60
bnc <bnc@chromium.org>
2015-09-02 05:25:03 -0700

    Remove kProtoHTTP2MinimumVersion, kProtoHTTP2MaximumVersion.
    Remove kProtoHTTP2MinimumVersion, kProtoHTTP2MaximumVersion values from enum
    NextProto.
    
    BUG=345769
    
    Review URL: https://codereview.chromium.org/1310553009
    
    Cr-Commit-Position: refs/heads/master@{#346896}

--- net/spdy/spdy_session_unittest.cc
+++ net/spdy/spdy_session_unittest.cc
@@ -1253,7 +1253,7 @@ TEST_P(SpdySessionTest, OnSettings) {
   int seq = 0;
   std::vector<MockWrite> writes;
   scoped_ptr<SpdyFrame> settings_ack(spdy_util_.ConstructSpdySettingsAck());
-  if (GetParam() >= kProtoHTTP2MinimumVersion) {
+  if (GetParam() == kProtoHTTP2) {
     writes.push_back(CreateMockWrite(*settings_ack, ++seq));
   }
 
@@ -1450,8 +1450,7 @@ TEST_P(SpdySessionTest, SendInitialDataOnNewSession) {
   scoped_ptr<SpdyFrame> settings_frame(
       spdy_util_.ConstructSpdySettings(settings));
   std::vector<MockWrite> writes;
-  if ((GetParam() >= kProtoHTTP2MinimumVersion) &&
-      (GetParam() <= kProtoHTTP2MaximumVersion)) {
+  if (GetParam() == kProtoHTTP2) {
     writes.push_back(
         MockWrite(ASYNC,
                   kHttp2ConnectionHeaderPrefix,
@@ -3395,7 +3394,7 @@ TEST_P(SpdySessionTest, SessionFlowControlInactiveStream) {
 // (including optional pad length and padding) is.
 TEST_P(SpdySessionTest, SessionFlowControlPadding) {
   // Padding only exists in HTTP/2.
-  if (GetParam() < kProtoHTTP2MinimumVersion)
+  if (GetParam() < kProtoHTTP2)
     return;
 
   session_deps_.host_resolver->set_synchronous_mode(true);
