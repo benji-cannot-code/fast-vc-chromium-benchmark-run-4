26919ea1c83f035408f6ae5518ebb7fbe8f19e50
achuith <achuith@chromium.org>
2015-09-02 05:06:43 -0700

    Remove comments
    BUG=450651
    
    Review URL: https://codereview.chromium.org/1317523003
    
    Cr-Commit-Position: refs/heads/master@{#346893}

--- chrome/browser/chromeos/policy/device_system_use_24hour_clock_browsertest.cc
+++ chrome/browser/chromeos/policy/device_system_use_24hour_clock_browsertest.cc
@@ -106,7 +106,6 @@ class SystemUse24HourClockPolicyTest
   DISALLOW_COPY_AND_ASSIGN(SystemUse24HourClockPolicyTest);
 };
 
-// Disabled due to flakiness - http://crbug.com/450651.
 IN_PROC_BROWSER_TEST_F(SystemUse24HourClockPolicyTest, CheckUnset) {
   bool system_use_24hour_clock;
   EXPECT_FALSE(CrosSettings::Get()->GetBoolean(kSystemUse24HourClock,
@@ -120,7 +119,6 @@ IN_PROC_BROWSER_TEST_F(SystemUse24HourClockPolicyTest, CheckUnset) {
   EXPECT_EQ(base::k12HourClock, TestGetPrimarySystemTrayDateHourType());
 }
 
-// Disabled due to flakiness - http://crbug.com/450651.
 IN_PROC_BROWSER_TEST_F(SystemUse24HourClockPolicyTest, CheckTrue) {
   bool system_use_24hour_clock = true;
   EXPECT_FALSE(CrosSettings::Get()->GetBoolean(kSystemUse24HourClock,
@@ -147,7 +145,6 @@ IN_PROC_BROWSER_TEST_F(SystemUse24HourClockPolicyTest, CheckTrue) {
   EXPECT_EQ(base::k24HourClock, TestGetPrimarySystemTrayDateHourType());
 }
 
-// Disabled due to flakiness - http://crbug.com/450651.
 IN_PROC_BROWSER_TEST_F(SystemUse24HourClockPolicyTest, CheckFalse) {
   bool system_use_24hour_clock = true;
   EXPECT_FALSE(CrosSettings::Get()->GetBoolean(kSystemUse24HourClock,
