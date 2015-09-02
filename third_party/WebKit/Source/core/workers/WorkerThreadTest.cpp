8543e77bb043f68ef19fac7fe2f52afb6b0bf9b0
kinuko@chromium.org <kinuko@chromium.org>
2015-09-02 02:52:58 +0000

    Force running GC on the worker thread before the unit test exits
    For the main thread we seem to always run
    v8::Isolate::RequestGarbageCollectionForTesting but it's
    not the case on the worker thread, which seems to cause leaks
    on the context object.
    
    BUG=526424
    
    Review URL: https://codereview.chromium.org/1316233004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201601 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/workers/WorkerThreadTest.cpp
+++ third_party/WebKit/Source/core/workers/WorkerThreadTest.cpp
@@ -10,10 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/workers/WorkerReportingProxy.h"
 #include "core/workers/WorkerThreadStartupData.h"
 #include "platform/NotImplemented.h"
+#include "platform/heap/Heap.h"
 #include "public/platform/WebScheduler.h"
 #include "public/platform/WebWaitableEvent.h"
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <v8.h>
 
 using testing::_;
 using testing::AtMost;
@@ -109,6 +111,12 @@ public:
     WebThreadSupportingGC& backingThread() override
     {
         return *m_thread;
+    }
+    void willDestroyIsolate() override
+    {
+        v8::Isolate::GetCurrent()->RequestGarbageCollectionForTesting(v8::Isolate::kFullGarbageCollection);
+        Heap::collectAllGarbage();
+        WorkerThread::willDestroyIsolate();
     }
 
     MOCK_METHOD1(doIdleGc, bool(double deadlineSeconds));
--- third_party/WebKit/Source/modules/compositorworker/CompositorWorkerManagerTest.cpp
+++ third_party/WebKit/Source/modules/compositorworker/CompositorWorkerManagerTest.cpp
@@ -15,10 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/compositorworker/CompositorWorkerThread.h"
 #include "platform/NotImplemented.h"
 #include "platform/ThreadSafeFunctional.h"
+#include "platform/heap/Heap.h"
 #include "platform/testing/UnitTestHelpers.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebWaitableEvent.h"
 #include <gtest/gtest.h>
+#include <v8.h>
 
 namespace blink {
 namespace {
@@ -49,6 +51,12 @@ private:
         CompositorWorkerThread::terminateV8Execution();
         if (m_v8TerminationCallback)
             (*m_v8TerminationCallback)();
+    }
+    void willDestroyIsolate() override
+    {
+        v8::Isolate::GetCurrent()->RequestGarbageCollectionForTesting(v8::Isolate::kFullGarbageCollection);
+        Heap::collectAllGarbage();
+        CompositorWorkerThread::willDestroyIsolate();
     }
 
     WebWaitableEvent* m_startEvent;
