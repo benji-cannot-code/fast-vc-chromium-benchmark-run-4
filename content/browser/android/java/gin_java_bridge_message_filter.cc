1393f65bb7d9256ebc851b9587b9b282715ef5ce
mnaganov <mnaganov@chromium.org>
2015-09-02 08:37:09 -0700

    [Android] Fix RenderFrameHost registration issue in Java Bridge
    1. Ensure that all current RenderFrameHosts get registered in
       GinJavaBridgeMessageFilter before providing named objects to the
       renderer.
    
    2. Fix output parameters initialization in synchronous message handlers.
    
    BUG=526876
    
    Review URL: https://codereview.chromium.org/1326753002
    
    Cr-Commit-Position: refs/heads/master@{#346924}

--- content/browser/android/java/gin_java_bridge_message_filter.cc
+++ content/browser/android/java/gin_java_bridge_message_filter.cc
@@ -113,8 +113,11 @@ void GinJavaBridgeMessageFilter::OnGetMethods(
     std::set<std::string>* returned_method_names) {
   DCHECK(JavaBridgeThread::CurrentlyOn());
   GinJavaBridgeDispatcherHost* host = FindHost();
-  if (host)
+  if (host) {
     host->OnGetMethods(object_id, returned_method_names);
+  } else {
+    *returned_method_names = std::set<std::string>();
+  }
 }
 
 void GinJavaBridgeMessageFilter::OnHasMethod(
@@ -123,8 +126,11 @@ void GinJavaBridgeMessageFilter::OnHasMethod(
     bool* result) {
   DCHECK(JavaBridgeThread::CurrentlyOn());
   GinJavaBridgeDispatcherHost* host = FindHost();
-  if (host)
+  if (host) {
     host->OnHasMethod(object_id, method_name, result);
+  } else {
+    *result = false;
+  }
 }
 
 void GinJavaBridgeMessageFilter::OnInvokeMethod(
@@ -138,6 +144,9 @@ void GinJavaBridgeMessageFilter::OnInvokeMethod(
   if (host) {
     host->OnInvokeMethod(current_routing_id_, object_id, method_name, arguments,
                          wrapped_result, error_code);
+  } else {
+    wrapped_result->Append(base::Value::CreateNullValue());
+    *error_code = kGinJavaBridgeRenderFrameDeleted;
   }
 }
 
