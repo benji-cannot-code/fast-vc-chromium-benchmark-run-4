f01f400930d50a4755f982b8293400c2413f5265
fsamuel <fsamuel@chromium.org>
2015-09-02 15:17:07 -0700

    Mandoline: Fix use after free in WebViewImpl
    Make sure we clear content_ in WebViewImpl if it's destroyed so we don't
    end up accessing it later. This resulted in a use-after-free during tear down.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1301333011
    
    Cr-Commit-Position: refs/heads/master@{#347042}

--- components/web_view/web_view_impl.cc
+++ components/web_view/web_view_impl.cc
@@ -127,8 +127,10 @@ void WebViewImpl::OnViewBoundsChanged(mojo::View* view,
 
 void WebViewImpl::OnViewDestroyed(mojo::View* view) {
   // |FrameTree| cannot outlive the content view.
-  if (view == content_)
+  if (view == content_) {
     frame_tree_.reset();
+    content_ = nullptr;
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
