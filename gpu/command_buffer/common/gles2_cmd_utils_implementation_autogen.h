028de16452fc24a410a596c42fc98913395685e3
kkinnunen <kkinnunen@nvidia.com>
2015-09-01 23:05:36 -0700

    command_buffer: Implement EXT_multisample_compatibility
    Implement EXT_multisample_compatibility OpenGL ES extension in command
    buffer.
    
    Skia needs this in order to enable and disable multisampling when
    using GL path rendering.
    
    BUG=506765
    
    Review URL: https://codereview.chromium.org/1231673006
    
    Cr-Commit-Position: refs/heads/master@{#346847}

--- gpu/command_buffer/common/gles2_cmd_utils_implementation_autogen.h
+++ gpu/command_buffer/common/gles2_cmd_utils_implementation_autogen.h
@@ -1823,6 +1823,9 @@ static const GLES2Util::EnumToString enum_to_string_table[] = {
         0x8C1D, "GL_TEXTURE_BINDING_2D_ARRAY",
     },
     {
+        0x809F, "GL_SAMPLE_ALPHA_TO_ONE_EXT",
+    },
+    {
         0x8DDF, "GL_MAX_GEOMETRY_UNIFORM_COMPONENTS_EXT",
     },
     {
@@ -3357,6 +3360,9 @@ static const GLES2Util::EnumToString enum_to_string_table[] = {
     },
     {
         0x8823, "GL_WRITEONLY_RENDERING_QCOM",
+    },
+    {
+        0x809D, "GL_MULTISAMPLE_EXT",
     },
     {
         0x8824, "GL_MAX_DRAW_BUFFERS_EXT",
--- ui/gl/gl_enums_implementation_autogen.h
+++ ui/gl/gl_enums_implementation_autogen.h
@@ -248,6 +248,9 @@ static const GLEnums::EnumToString enum_to_string_table[] = {
         0x8A53, "GL_SYNC_OBJECT_APPLE",
     },
     {
+        0x78FB, "GL_RGB_YCBCR_422_CHROMIUM",
+    },
+    {
         0x8DF8, "GL_SHADER_BINARY_FORMATS",
     },
     {
@@ -404,7 +407,7 @@ static const GLEnums::EnumToString enum_to_string_table[] = {
         0x821A, "GL_DEPTH_STENCIL_ATTACHMENT",
     },
     {
-        0x8A40, "GL_UNIFORM_BLOCK_DATA_SIZE",
+        0x809D, "GL_MULTISAMPLE_EXT",
     },
     {
         0x821D, "GL_NUM_EXTENSIONS",
@@ -1826,6 +1829,9 @@ static const GLEnums::EnumToString enum_to_string_table[] = {
         0x8C1D, "GL_TEXTURE_BINDING_2D_ARRAY",
     },
     {
+        0x809F, "GL_SAMPLE_ALPHA_TO_ONE_EXT",
+    },
+    {
         0x8DDF, "GL_MAX_GEOMETRY_UNIFORM_COMPONENTS_EXT",
     },
     {
@@ -2805,6 +2811,9 @@ static const GLEnums::EnumToString enum_to_string_table[] = {
     },
     {
         0x8051, "GL_RGB8_OES",
+    },
+    {
+        0x8A40, "GL_UNIFORM_BLOCK_DATA_SIZE",
     },
     {
         0x8CAD, "GL_DEPTH32F_STENCIL8",
