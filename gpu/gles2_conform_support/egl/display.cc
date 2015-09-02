c01f0730c9e10788034ff3708248cfd6d644123b
jmadill <jmadill@chromium.org>
2015-09-02 07:35:23 -0700

    Revert of Add command_buffer_gles2 (patchset #13 id:290001 of https://codereview.chromium.org/1220883008/ )
    Reason for revert:
    Broke the gles2_conform_tests, visible on the GPU FYI waterfall.
    
    Original issue's description:
    > Add command_buffer_gles2
    >
    > This is a very simple library that exposes some egl-like functions that
    > can be used by skia to use the command buffer as a backend for their
    > tests
    >
    > Committed: https://crrev.com/604b615373e4e9f873db37cef9c6416176fc35f3
    > Cr-Commit-Position: refs/heads/master@{#346251}
    >
    > Committed: https://crrev.com/efa3a446183fbbff2034a03989f12543d0713e39
    > Cr-Commit-Position: refs/heads/master@{#346840}
    
    TBR=piman@chromium.org,dpranke@chromium.org,sievers@chromium.org,hendrikw@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1319453005
    
    Cr-Commit-Position: refs/heads/master@{#346914}

--- gpu/gles2_conform_support/egl/display.cc
+++ gpu/gles2_conform_support/egl/display.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/gles2_conform_support/egl/display.h"
 
 #include <vector>
-#include "base/at_exit.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "gpu/command_buffer/client/gles2_implementation.h"
@@ -33,8 +32,8 @@ Display::Display(EGLNativeDisplayType display_id)
       is_initialized_(false),
       create_offscreen_(false),
       create_offscreen_width_(0),
-      create_offscreen_height_(0),
-      exit_manager_(new base::AtExitManager) {}
+      create_offscreen_height_(0) {
+}
 
 Display::~Display() {
   gles2::Terminate();
