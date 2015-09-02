8b9827aad90bd678118b5afa578ac3e825942b63
shess <shess@chromium.org>
2015-09-02 12:37:43 -0700

    [sql] Drop sqlite3_* proxy hacks.
    Now that sqlite is a shlib under component build, the hacks to proxy
    functions are not necessary.
    
    BUG=489444
    TBR=michaeln@chromium.org
    
    Review URL: https://codereview.chromium.org/1303423009
    
    Cr-Commit-Position: refs/heads/master@{#346994}

--- sql/connection_unittest.cc
+++ sql/connection_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sql/connection.h"
 #include "sql/correct_sql_test_base.h"
 #include "sql/meta_table.h"
-#include "sql/proxy.h"
 #include "sql/statement.h"
 #include "sql/test/error_callback_support.h"
 #include "sql/test/scoped_error_ignorer.h"
@@ -87,12 +86,12 @@ class ScopedScalarFunction {
       int args,
       base::Callback<void(sqlite3_context*,int,sqlite3_value**)> cb)
       : db_(db.db_), function_name_(function_name), cb_(cb) {
-    sql::sqlite3_create_function_v2(db_, function_name, args, SQLITE_UTF8,
-                                    this, &Run, NULL, NULL, NULL);
+    sqlite3_create_function_v2(db_, function_name, args, SQLITE_UTF8,
+                               this, &Run, NULL, NULL, NULL);
   }
   ~ScopedScalarFunction() {
-    sql::sqlite3_create_function_v2(db_, function_name_, 0, SQLITE_UTF8,
-                                    NULL, NULL, NULL, NULL, NULL);
+    sqlite3_create_function_v2(db_, function_name_, 0, SQLITE_UTF8,
+                               NULL, NULL, NULL, NULL, NULL);
   }
 
  private:
@@ -116,10 +115,10 @@ class ScopedCommitHook {
                    base::Callback<int(void)> cb)
       : db_(db.db_),
         cb_(cb) {
-    sql::sqlite3_commit_hook(db_, &Run, this);
+    sqlite3_commit_hook(db_, &Run, this);
   }
   ~ScopedCommitHook() {
-    sql::sqlite3_commit_hook(db_, NULL, NULL);
+    sqlite3_commit_hook(db_, NULL, NULL);
   }
 
  private:
