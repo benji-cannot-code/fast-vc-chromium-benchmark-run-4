14b5662fdbe4da21e772cc2b803d62ee58af93f8
imcheng <imcheng@chromium.org>
2015-09-02 15:57:44 -0700

    [Media Router] Cleanup: s/Maybe//g from CreatePresentationSessionRequest
    Also updated documentation to reflect the DCHECK.
    
    BUG=526800
    
    Review URL: https://codereview.chromium.org/1321253002
    
    Cr-Commit-Position: refs/heads/master@{#347053}

--- chrome/browser/media/router/create_presentation_session_request.cc
+++ chrome/browser/media/router/create_presentation_session_request.cc
@@ -33,7 +33,7 @@ CreatePresentationSessionRequest::~CreatePresentationSessionRequest() {
   }
 }
 
-void CreatePresentationSessionRequest::MaybeInvokeSuccessCallback(
+void CreatePresentationSessionRequest::InvokeSuccessCallback(
     const std::string& presentation_id,
     const MediaRoute::Id& route_id) {
   DCHECK(!cb_invoked_);
@@ -47,7 +47,7 @@ void CreatePresentationSessionRequest::MaybeInvokeSuccessCallback(
   }
 }
 
-void CreatePresentationSessionRequest::MaybeInvokeErrorCallback(
+void CreatePresentationSessionRequest::InvokeErrorCallback(
     const content::PresentationError& error) {
   DCHECK(!cb_invoked_);
   if (!cb_invoked_) {
@@ -63,11 +63,11 @@ void CreatePresentationSessionRequest::HandleRouteResponse(
     const std::string& presentation_id,
     const std::string& error) {
   if (!route) {
-    presentation_request->MaybeInvokeErrorCallback(
+    presentation_request->InvokeErrorCallback(
         content::PresentationError(content::PRESENTATION_ERROR_UNKNOWN, error));
   } else {
-    presentation_request->MaybeInvokeSuccessCallback(
-        presentation_id, route->media_route_id());
+    presentation_request->InvokeSuccessCallback(presentation_id,
+                                                route->media_route_id());
   }
 }
 
