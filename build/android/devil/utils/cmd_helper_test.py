a7462fc63fcb38bd6e3258b68cccd0a9ee4d3fd4
jbudorick <jbudorick@chromium.org>
2015-09-01 18:00:30 -0700

    [Android] Move some pylib modules into devil/
    BUG=476719
    TBR=maruel@chromium.org
    
    Review URL: https://codereview.chromium.org/1314913009
    
    Cr-Commit-Position: refs/heads/master@{#346806}

--- build/android/devil/utils/cmd_helper_test.py
+++ build/android/devil/utils/cmd_helper_test.py
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import unittest
 import subprocess
 
-from pylib import cmd_helper
+from devil.utils import cmd_helper
 
 
 class CmdHelperSingleQuoteTest(unittest.TestCase):
--- build/android/pylib/flag_changer.py
+++ build/android/pylib/flag_changer.py
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import logging
 
-import pylib.device.device_utils
-
 from pylib.device import device_errors
 
 
