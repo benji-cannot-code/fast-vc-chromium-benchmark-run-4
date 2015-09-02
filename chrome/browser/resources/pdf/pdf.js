7a170d0f508b801e62c6d51347dd37a41a780176
tsergeant <tsergeant@chromium.org>
2015-09-01 20:45:46 -0700

    Material PDF: Prevent toolbars showing until after the plugin is ready
    This fixes an issue where toolbars would be visible but inactive on top
    of the PDF plugin disabled message. It also affects the error and
    password dialogs, bringing behaviour in line with non-material PDF.
    
    BUG=526934
    
    Review URL: https://codereview.chromium.org/1308223004
    
    Cr-Commit-Position: refs/heads/master@{#346836}

--- chrome/browser/resources/pdf/pdf.js
+++ chrome/browser/resources/pdf/pdf.js
@@ -554,6 +554,7 @@ PDFViewer.prototype = {
         if (this.isMaterial_) {
           this.materialToolbar_.docLength =
               this.documentDimensions_.pageDimensions.length;
+          this.toolbarManager_.enableToolbars();
         } else {
           this.pageIndicator_.initialFadeIn();
           this.toolbar_.initialFadeIn();
