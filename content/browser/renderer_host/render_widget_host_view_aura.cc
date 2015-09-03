d8d406a7a7f5d955dab7349bc7a3ce60d733a49b
kenrb <kenrb@chromium.org>
2015-09-03 11:13:13 -0700

    Fix regression for mouse event movement not getting correctly set
    r347008 introduced a regression by causing the browser to incorrectly
    track mouse movement deltas. This was breaking some hover effects.
    
    This patch restores a line that was erroneously deleted.
    
    BUG=527799
    
    Review URL: https://codereview.chromium.org/1316763008
    
    Cr-Commit-Position: refs/heads/master@{#347193}

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -2122,6 +2122,7 @@ void RenderWidgetHostViewAura::OnMouseEvent(ui::MouseEvent* event) {
         FinishImeCompositionSession();
 
       blink::WebMouseEvent mouse_event = MakeWebMouseEvent(*event);
+      ModifyEventMovementAndCoords(&mouse_event);
       if (host_->delegate() && host_->delegate()->GetInputEventRouter()) {
         host_->delegate()->GetInputEventRouter()->RouteMouseEvent(this,
                                                                   &mouse_event);
