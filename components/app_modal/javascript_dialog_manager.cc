64d4045789afe48d5f2ad16b5934049ec6685301
palmer <palmer@chromium.org>
2015-09-03 16:38:41 -0700

    Use FormatUrlForSecurityDisplay to show the site in JavaScript alert boxes.
    Instead of FormatUrl. Showing the URL in alert boxes give people a chance to
    associate the alert with the origin that opened it; FormatUrlForSecurityDisplay
    is the canonical function for showing the security-relevant parts of URLs.
    
    BUG=496965
    
    Review URL: https://codereview.chromium.org/1328663004
    
    Cr-Commit-Position: refs/heads/master@{#347285}

--- components/app_modal/javascript_dialog_manager.cc
+++ components/app_modal/javascript_dialog_manager.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/app_modal/javascript_dialog_extensions_client.h"
 #include "components/app_modal/javascript_native_dialog_factory.h"
 #include "components/app_modal/native_app_modal_dialog.h"
-#include "components/url_formatter/url_formatter.h"
+#include "components/url_formatter/elide_url.h"
 #include "content/public/common/javascript_message_type.h"
 #include "grit/components_strings.h"
 #include "net/base/net_util.h"
@@ -193,13 +193,6 @@ base::string16 JavaScriptDialogManager::GetTitle(
     const GURL& origin_url,
     const std::string& accept_lang,
     bool is_alert) {
-  // If the URL hasn't any host, return the default string.
-  if (!origin_url.has_host()) {
-      return l10n_util::GetStringUTF16(
-          is_alert ? IDS_JAVASCRIPT_ALERT_DEFAULT_TITLE
-                   : IDS_JAVASCRIPT_MESSAGEBOX_DEFAULT_TITLE);
-  }
-
   // For extensions, show the extension name, but only if the origin of
   // the alert matches the top-level WebContents.
   std::string name;
@@ -207,8 +200,9 @@ base::string16 JavaScriptDialogManager::GetTitle(
     return base::UTF8ToUTF16(name);
 
   // Otherwise, return the formatted URL.
-  // In this case, force URL to have LTR directionality.
-  base::string16 url_string = url_formatter::FormatUrl(origin_url, accept_lang);
+  base::string16 url_string =
+      url_formatter::FormatUrlForSecurityDisplay(origin_url, accept_lang);
+
   return l10n_util::GetStringFUTF16(
       is_alert ? IDS_JAVASCRIPT_ALERT_TITLE
       : IDS_JAVASCRIPT_MESSAGEBOX_TITLE,
