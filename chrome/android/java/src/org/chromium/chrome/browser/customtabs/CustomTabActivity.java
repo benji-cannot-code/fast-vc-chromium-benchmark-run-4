d6051a5a595069f152a5f3857b2fa96339e161f4
twellington <twellington@chromium.org>
2015-09-03 10:58:48 -0700

    Check for null before calling ContextualSearchManager.setFindToolbarManager()
    BUG=527663
    
    Review URL: https://codereview.chromium.org/1321203007
    
    Cr-Commit-Position: refs/heads/master@{#347185}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -187,7 +187,9 @@ public class CustomTabActivity extends ChromeActivity {
                 (ViewGroup) findViewById(android.R.id.content), controlContainer);
         mFindToolbarManager = new FindToolbarManager(this, getTabModelSelector(),
                 getToolbarManager().getActionModeController().getActionModeCallback());
-        getContextualSearchManager().setFindToolbarManager(mFindToolbarManager);
+        if (getContextualSearchManager() != null) {
+            getContextualSearchManager().setFindToolbarManager(mFindToolbarManager);
+        }
         getToolbarManager().initializeWithNative(getTabModelSelector(), getFullscreenManager(),
                 mFindToolbarManager, null, layoutDriver, null, null, null,
                 new OnClickListener() {
--- chrome/android/java/src/org/chromium/chrome/browser/document/DocumentActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/document/DocumentActivity.java
@@ -521,7 +521,9 @@ public class DocumentActivity extends ChromeActivity {
                 getToolbarManager().getActionModeController()
                         .getActionModeCallback());
 
-        getContextualSearchManager().setFindToolbarManager(mFindToolbarManager);
+        if (getContextualSearchManager() != null) {
+            getContextualSearchManager().setFindToolbarManager(mFindToolbarManager);
+        }
 
         getToolbarManager().initializeWithNative(getTabModelSelector(), getFullscreenManager(),
                 mFindToolbarManager, null, layoutDriver, null, null, null, null);
