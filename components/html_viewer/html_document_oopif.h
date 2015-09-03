1c5454508c69adf10dd422819aa1d63569c5fd19
sky <sky@chromium.org>
2015-09-03 10:59:49 -0700

    Fixes possible race in new connection setup
    The frame tree uses change_ids to track the tree status. When a
    connection is made to a frame it is entirely possible for the new
    connection to have a change_id > change_id seen by tree. To deal with
    this HTMLFrameTreeManager gets an observer. DocumentResourceWaiter, as
    necessary, adds itself as an observer and waits for the change ids to
    match. Once they match, loading continues.
    
    BUG=479172,490221
    TEST=none
    R=fsamuel@chromium.org
    
    Review URL: https://codereview.chromium.org/1325903003
    
    Cr-Commit-Position: refs/heads/master@{#347186}

--- components/html_viewer/html_document_oopif.h
+++ components/html_viewer/html_document_oopif.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/html_viewer/ax_provider_impl.h"
 #include "components/html_viewer/html_frame_delegate.h"
 #include "components/html_viewer/public/interfaces/test_html_viewer.mojom.h"
-#include "components/view_manager/public/cpp/view_observer.h"
 #include "components/view_manager/public/cpp/view_tree_delegate.h"
 #include "components/web_view/public/interfaces/frame_tree.mojom.h"
 #include "mojo/application/public/cpp/app_lifetime_helper.h"
@@ -41,7 +40,6 @@ class DocumentResourceWaiter;
 class GlobalState;
 class HTMLFactory;
 class HTMLFrame;
-class HTMLFrameTreeManager;
 class TestHTMLViewerImpl;
 class ViewTreeDelegateImpl;
 class WebLayerTreeViewImpl;
@@ -53,7 +51,6 @@ class WebLayerTreeViewImpl;
 // . Explicitly by way of Destroy().
 class HTMLDocumentOOPIF
     : public mojo::ViewTreeDelegate,
-      public mojo::ViewObserver,
       public HTMLFrameDelegate,
       public mojo::InterfaceFactory<mojo::AxProvider>,
       public mojo::InterfaceFactory<web_view::FrameTreeClient>,
@@ -78,7 +75,7 @@ class HTMLDocumentOOPIF
   void Destroy();
 
  private:
-  friend class DocumentResourceWaiter;  // So it can call LoadIfNecessary().
+  friend class DocumentResourceWaiter;  // So it can call Load().
 
   // Requests for interfaces before the document is loaded go here. Once
   // loaded the requests are bound and BeforeLoadCache is deleted.
@@ -106,7 +103,6 @@ class HTMLDocumentOOPIF
 
   ~HTMLDocumentOOPIF() override;
 
-  void LoadIfNecessary();
   void Load();
 
   BeforeLoadCache* GetBeforeLoadCache();
@@ -114,13 +110,6 @@ class HTMLDocumentOOPIF
   // ViewTreeDelegate:
   void OnEmbed(mojo::View* root) override;
   void OnConnectionLost(mojo::ViewTreeConnection* connection) override;
-
-  // ViewObserver:
-  void OnViewViewportMetricsChanged(
-      mojo::View* view,
-      const mojo::ViewportMetrics& old_metrics,
-      const mojo::ViewportMetrics& new_metrics) override;
-  void OnViewDestroyed(mojo::View* view) override;
 
   // HTMLFrameDelegate:
   mojo::ApplicationImpl* GetApp() override;
--- components/html_viewer/html_frame_tree_manager.cc
+++ components/html_viewer/html_frame_tree_manager.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/html_viewer/html_factory.h"
 #include "components/html_viewer/html_frame.h"
 #include "components/html_viewer/html_frame_delegate.h"
+#include "components/html_viewer/html_frame_tree_manager_observer.h"
 #include "components/view_manager/public/cpp/view_tree_connection.h"
 #include "components/web_view/web_view_switches.h"
 #include "third_party/WebKit/public/web/WebLocalFrame.h"
@@ -44,6 +45,24 @@ bool FindFrameDataIndex(const mojo::Array<web_view::FrameDataPtr>& frame_data,
 
 }  // namespace
 
+// Object that calls OnHTMLFrameTreeManagerChangeIdAdvanced() from the
+// destructor.
+class HTMLFrameTreeManager::ChangeIdAdvancedNotifier {
+ public:
+  explicit ChangeIdAdvancedNotifier(
+      base::ObserverList<HTMLFrameTreeManagerObserver>* observers)
+      : observers_(observers) {}
+  ~ChangeIdAdvancedNotifier() {
+    FOR_EACH_OBSERVER(HTMLFrameTreeManagerObserver, *observers_,
+                      OnHTMLFrameTreeManagerChangeIdAdvanced());
+  }
+
+ private:
+  base::ObserverList<HTMLFrameTreeManagerObserver>* observers_;
+
+  DISALLOW_COPY_AND_ASSIGN(ChangeIdAdvancedNotifier);
+};
+
 // static
 HTMLFrameTreeManager::TreeMap* HTMLFrameTreeManager::instances_ = nullptr;
 
@@ -70,13 +89,8 @@ HTMLFrame* HTMLFrameTreeManager::CreateFrameAndAttachToTree(
 
   on_connect_callback.Run();
 
-  HTMLFrameTreeManager* frame_tree = nullptr;
-
-  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
-          web_view::switches::kOOPIFAlwaysCreateNewFrameTree) &&
-      instances_->count(frame_data[0]->frame_id)) {
-    frame_tree = (*instances_)[frame_data[0]->frame_id];
-  }
+  HTMLFrameTreeManager* frame_tree =
+      FindFrameTreeWithRoot(frame_data[0]->frame_id);
 
   if (view_connect_type == web_view::VIEW_CONNECT_TYPE_USE_EXISTING &&
       !frame_tree) {
@@ -141,8 +155,27 @@ HTMLFrame* HTMLFrameTreeManager::CreateFrameAndAttachToTree(
   return frame;
 }
 
+// static
+HTMLFrameTreeManager* HTMLFrameTreeManager::FindFrameTreeWithRoot(
+    uint32_t root_frame_id) {
+  return (!base::CommandLine::ForCurrentProcess()->HasSwitch(
+              web_view::switches::kOOPIFAlwaysCreateNewFrameTree) &&
+          instances_ && instances_->count(root_frame_id))
+             ? (*instances_)[root_frame_id]
+             : nullptr;
+}
+
 blink::WebView* HTMLFrameTreeManager::GetWebView() {
   return root_->web_view();
+}
+
+void HTMLFrameTreeManager::AddObserver(HTMLFrameTreeManagerObserver* observer) {
+  observers_.AddObserver(observer);
+}
+
+void HTMLFrameTreeManager::RemoveObserver(
+    HTMLFrameTreeManagerObserver* observer) {
+  observers_.RemoveObserver(observer);
 }
 
 void HTMLFrameTreeManager::OnFrameDestroyed(HTMLFrame* frame) {
@@ -170,6 +203,9 @@ HTMLFrameTreeManager::HTMLFrameTreeManager(GlobalState* global_state)
 HTMLFrameTreeManager::~HTMLFrameTreeManager() {
   DCHECK(!root_ || !local_root_);
   RemoveFromInstances();
+
+  FOR_EACH_OBSERVER(HTMLFrameTreeManagerObserver, observers_,
+                    OnHTMLFrameTreeManagerDestroyed());
 }
 
 void HTMLFrameTreeManager::Init(
@@ -250,6 +286,8 @@ void HTMLFrameTreeManager::ProcessOnFrameAdded(
   if (!PrepareForStructureChange(source, change_id))
     return;
 
+  ChangeIdAdvancedNotifier notifier(&observers_);
+
   HTMLFrame* parent = root_->FindFrame(frame_data->parent_id);
   if (!parent) {
     DVLOG(1) << "Received invalid parent in OnFrameAdded "
@@ -279,6 +317,8 @@ void HTMLFrameTreeManager::ProcessOnFrameRemoved(HTMLFrame* source,
                                                  uint32_t frame_id) {
   if (!PrepareForStructureChange(source, change_id))
     return;
+
+  ChangeIdAdvancedNotifier notifier(&observers_);
 
   pending_remove_ids_.erase(frame_id);
 
