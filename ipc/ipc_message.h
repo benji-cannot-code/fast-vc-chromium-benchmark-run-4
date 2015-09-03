e993f9eda89879acd3e6aa4450851cb53e6bd74b
erikchen <erikchen@chromium.org>
2015-09-03 14:41:15 -0700

    Reland #1 "ipc: Add a new field num_brokered_attachments to the message header."
    This otherwise innocuous CL is causing an unusual crash in Canary. At first
    glance, the crash appears related to IPC, but further digging indicates that
    it's probably related to corruption of a ResourceBuffer. I've updated that class
    to use CHECKs instead of DCHECKs https://codereview.chromium.org/1325153002/.
    
    I expect this CL to cause ~100 crashes in the next Chrome Canary. I intend to
    revert this CL after a single Canary release. This CL will help me better
    diagnose the exact nature of the crash.
    
    BUG=493414
    
    Review URL: https://codereview.chromium.org/1330563002
    
    Cr-Commit-Position: refs/heads/master@{#347250}

--- ipc/ipc_message.h
+++ ipc/ipc_message.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/pickle.h"
 #include "base/trace_event/trace_event.h"
+#include "ipc/attachment_broker.h"
 #include "ipc/ipc_export.h"
 
 #if !defined(NDEBUG)
@@ -223,6 +224,12 @@ class IPC_EXPORT Message : public base::Pickle {
     int32 routing;  // ID of the view that this message is destined for
     uint32 type;    // specifies the user-defined message type
     uint32 flags;   // specifies control flags for the message
+#if USE_ATTACHMENT_BROKER
+    // The number of brokered attachments included with this message. The
+    // ids of the brokered attachment ids are sent immediately after the pickled
+    // message, before the next pickled message is sent.
+    uint32 num_brokered_attachments;
+#endif
 #if defined(OS_POSIX)
     uint16 num_fds; // the number of descriptors included with this message
     uint16 pad;     // explicitly initialize this to appease valgrind
