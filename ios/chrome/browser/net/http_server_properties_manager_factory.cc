df7e5ac34750613d7ba855168a2acab629160533
droger <droger@chromium.org>
2015-09-03 06:46:01 -0700

    [iOS] implementation of HttpServerPropertiesManagerFactory
    Review URL: https://codereview.chromium.org/1329813002
    
    Cr-Commit-Position: refs/heads/master@{#347148}

---
+++ ios/chrome/browser/net/http_server_properties_manager_factory.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/net/http_server_properties_manager_factory.h"
+
+#include "components/pref_registry/pref_registry_syncable.h"
+#include "ios/chrome/browser/pref_names.h"
+#include "ios/web/public/web_thread.h"
+#include "net/http/http_server_properties_manager.h"
+
+// static
+net::HttpServerPropertiesManager*
+HttpServerPropertiesManagerFactory::CreateManager(PrefService* pref_service) {
+  return new net::HttpServerPropertiesManager(
+      pref_service, ios::prefs::kHttpServerProperties,
+      web::WebThread::GetTaskRunnerForThread(web::WebThread::IO));
+}
+
+// static
+void HttpServerPropertiesManagerFactory::RegisterProfilePrefs(
+    user_prefs::PrefRegistrySyncable* registry) {
+  registry->RegisterDictionaryPref(ios::prefs::kHttpServerProperties);
+}
--- ios/chrome/ios_chrome.gyp
+++ ios/chrome/ios_chrome.gyp
@@ -282,6 +282,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/net/chrome_cookie_store_ios_client.mm',
         'browser/net/connection_type_observer_bridge.h',
         'browser/net/connection_type_observer_bridge.mm',
+        'browser/net/http_server_properties_manager_factory.cc',
+        'browser/net/http_server_properties_manager_factory.h',
         'browser/net/image_fetcher.h',
         'browser/net/image_fetcher.mm',
         'browser/net/metrics_network_client.h',
