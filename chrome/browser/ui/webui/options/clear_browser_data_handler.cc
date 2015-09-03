d21c2d03ae7bc9d9b5bb57a4d329df77874c08d1
msramek <msramek@chromium.org>
2015-09-03 15:50:15 -0700

    Add metrics for CBD dialog initiated deletion of cookies and cache.
    The metric added in https://codereview.chromium.org/1259953003 is measured in BrowsingDataRemover, and includes deletions initiatied from anywhere.
    
    This new metric focuses specifically on deletions initiated from the Clear Browsing Data dialog.
    
    BUG=527372
    
    Review URL: https://codereview.chromium.org/1325083002
    
    Cr-Commit-Position: refs/heads/master@{#347275}

--- chrome/browser/ui/webui/options/clear_browser_data_handler.cc
+++ chrome/browser/ui/webui/options/clear_browser_data_handler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/command_line.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/prefs/pref_service.h"
 #include "base/strings/string16.h"
 #include "base/values.h"
@@ -214,6 +215,20 @@ void ClearBrowserDataHandler::HandleClearBrowserData(
     remove_mask |= site_data_mask;
     origin_mask |= BrowsingDataHelper::PROTECTED_WEB;
   }
+
+  // Record the deletion of cookies and cache.
+  BrowsingDataRemover::CookieOrCacheDeletionChoice choice =
+      BrowsingDataRemover::NEITHER_COOKIES_NOR_CACHE;
+  if (prefs->GetBoolean(prefs::kDeleteCookies)) {
+    choice = prefs->GetBoolean(prefs::kDeleteCache)
+        ? BrowsingDataRemover::BOTH_COOKIES_AND_CACHE
+        : BrowsingDataRemover::ONLY_COOKIES;
+  } else if (prefs->GetBoolean(prefs::kDeleteCache)) {
+    choice = BrowsingDataRemover::ONLY_CACHE;
+  }
+  UMA_HISTOGRAM_ENUMERATION(
+      "History.ClearBrowsingData.UserDeletedCookieOrCacheFromDialog",
+      choice, BrowsingDataRemover::MAX_CHOICE_VALUE);
 
   // BrowsingDataRemover deletes itself when done.
   int period_selected = prefs->GetInteger(prefs::kDeleteTimePeriod);
