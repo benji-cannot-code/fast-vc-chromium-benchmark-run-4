5219e9202963bb38abfd5dde4dbcb84248d55625
alph@chromium.org <alph@chromium.org>
2015-09-03 01:16:44 +0000

    DevTools: negative time on network filmstrip when reloading page while loading.
    BUG=526826
    
    Review URL: https://codereview.chromium.org/1326053002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201688 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/components_lazy/FilmStripModel.js
+++ third_party/WebKit/Source/devtools/front_end/components_lazy/FilmStripModel.js
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @constructor
  * @param {!WebInspector.TracingModel} tracingModel
+ * @param {number=} zeroTime
  */
-WebInspector.FilmStripModel = function(tracingModel)
+WebInspector.FilmStripModel = function(tracingModel, zeroTime)
 {
     this._tracingModel = tracingModel;
+    this._zeroTime = zeroTime || tracingModel.minimumRecordTime();
 
     /** @type {!Array<!WebInspector.FilmStripModel.Frame>} */
     this._frames = [];
@@ -25,15 +27,17 @@ WebInspector.FilmStripModel = function(tracingModel)
 
     var events = mainThread.events();
     for (var i = 0; i < events.length; ++i) {
-        if (!events[i].hasCategory(WebInspector.FilmStripModel._category))
+        var event = events[i];
+        if (event.startTime < this._zeroTime)
             continue;
-
-        if (events[i].name === WebInspector.FilmStripModel.TraceEvents.CaptureFrame) {
-            var data = events[i].args["data"];
+        if (!event.hasCategory(WebInspector.FilmStripModel._category))
+            continue;
+        if (event.name === WebInspector.FilmStripModel.TraceEvents.CaptureFrame) {
+            var data = event.args["data"];
             if (data)
-                this._frames.push(WebInspector.FilmStripModel.Frame._fromEvent(this, events[i], this._frames.length));
-        } else if (events[i].name === WebInspector.FilmStripModel.TraceEvents.Screenshot) {
-            this._frames.push(WebInspector.FilmStripModel.Frame._fromSnapshot(this, /** @type {!WebInspector.TracingModel.ObjectSnapshot} */ (events[i]), this._frames.length));
+                this._frames.push(WebInspector.FilmStripModel.Frame._fromEvent(this, event, this._frames.length));
+        } else if (event.name === WebInspector.FilmStripModel.TraceEvents.Screenshot) {
+            this._frames.push(WebInspector.FilmStripModel.Frame._fromSnapshot(this, /** @type {!WebInspector.TracingModel.ObjectSnapshot} */ (event), this._frames.length));
         }
     }
 }
@@ -59,7 +63,7 @@ WebInspector.FilmStripModel.prototype = {
      */
     zeroTime: function()
     {
-        return this._tracingModel.minimumRecordTime();
+        return this._zeroTime;
     },
 
     /**
