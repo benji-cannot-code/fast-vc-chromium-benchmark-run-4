6121dff91ca4f6090310a9b78714a2db9ebbecc7
twellington <twellington@chromium.org>
2015-09-03 12:41:50 -0700

    [Contextual Search] Animate panel text on search term resolution
    Create distinct views for the search context and resolved search
    term. When the search term is resolved, the search context fades
    out while the resolved search term fades in.
    
    BUG=526230
    
    Review URL: https://codereview.chromium.org/1321783005
    
    Cr-Commit-Position: refs/heads/master@{#347215}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/ContextualSearchSceneLayer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/ContextualSearchSceneLayer.java
@@ -48,7 +48,8 @@ public class ContextualSearchSceneLayer extends SceneLayer {
 
         float searchBarMarginSide = mSearchPanel.getSearchBarMarginSide();
         float searchBarHeight = mSearchPanel.getSearchBarHeight();
-        float searchBarTextOpacity = mSearchPanel.getSearchBarTextOpacity();
+        float searchContextOpacity = mSearchPanel.getBottomBarSearchContextOpacity();
+        float searchTermOpacity = mSearchPanel.getBottomBarSearchTermOpacity();
 
         boolean searchBarBorderVisible = mSearchPanel.isSearchBarBorderVisible();
         float searchBarBorderY = mSearchPanel.getSearchBarBorderY();
@@ -71,7 +72,8 @@ public class ContextualSearchSceneLayer extends SceneLayer {
 
         nativeUpdateContextualSearchLayer(mNativePtr,
                 R.drawable.contextual_search_bar_background,
-                R.id.contextual_search_view,
+                R.id.contextual_search_context_view,
+                R.id.contextual_search_term_view,
                 R.drawable.contextual_search_bar_shadow,
                 R.drawable.google_icon,
                 R.drawable.breadcrumb_arrow,
@@ -89,7 +91,8 @@ public class ContextualSearchSceneLayer extends SceneLayer {
                 searchPanelHeight * mDpToPx,
                 searchBarMarginSide * mDpToPx,
                 searchBarHeight * mDpToPx,
-                searchBarTextOpacity,
+                searchContextOpacity,
+                searchTermOpacity,
                 searchBarBorderVisible,
                 searchBarBorderY * mDpToPx,
                 searchBarBorderHeight * mDpToPx,
@@ -128,7 +131,8 @@ public class ContextualSearchSceneLayer extends SceneLayer {
     private native void nativeUpdateContextualSearchLayer(
             long nativeContextualSearchSceneLayer,
             int searchBarBackgroundResourceId,
-            int searchBarTextResourceId,
+            int searchContextResourceId,
+            int searchTermResourceId,
             int searchBarShadowResourceId,
             int searchProviderIconResourceId,
             int arrowUpResourceId,
@@ -146,7 +150,8 @@ public class ContextualSearchSceneLayer extends SceneLayer {
             float searchPanelHeight,
             float searchBarMarginSide,
             float searchBarHeight,
-            float searchBarTextOpacity,
+            float searchContextOpacity,
+            float searchTermOpacity,
             boolean searchBarBorderVisible,
             float searchBarBorderY,
             float searchBarBorderHeight,
