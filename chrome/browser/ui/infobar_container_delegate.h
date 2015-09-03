82da2c7add9a062820484c63ed0a5661768bb3a6
estade <estade@chromium.org>
2015-09-03 14:03:44 -0700

    Infobar material design refresh: layout
    a) change style of buttons
    b) change height
    c) change font
    
    BUG=520266
    
    Review URL: https://codereview.chromium.org/1312633005
    
    Cr-Commit-Position: refs/heads/master@{#347238}

--- chrome/browser/ui/infobar_container_delegate.h
+++ chrome/browser/ui/infobar_container_delegate.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class InfoBarContainerDelegate : public infobars::InfoBarContainer::Delegate {
  public:
   static const int kDefaultBarTargetHeight;
+  static const int kDefaultBarTargetHeightMd;
   static const int kSeparatorLineHeight;
   static const int kDefaultArrowTargetHeight;
   static const int kMaximumArrowTargetHeight;
--- chrome/browser/ui/views/infobars/confirm_infobar.h
+++ chrome/browser/ui/views/infobars/confirm_infobar.h
@@ -16,6 +16,7 @@ class ElevationIconSetter;
 
 namespace views {
 class Label;
+class LabelButton;
 }
 
 // An infobar that shows a message, up to two optional buttons, and an optional,
