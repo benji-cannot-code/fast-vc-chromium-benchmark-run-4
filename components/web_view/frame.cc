0aa7f47a08494883e6476aa52852e765e4314427
sky <sky@chromium.org>
2015-09-03 14:40:13 -0700

    Makes frame not attempt to reuse view if app_id is invalid
    If a Frame with an app_id of kInvalidContentHandlerID is asked to
    navigate and the new app has an id of kInvalidContentHandlerID we
    shouldn't attempt to reuse the view. We only want to reuse the view if
    the apps are the same, but kInvalidContentHandlerID means we can't
    determine the app, so that we shouldn't assume the app at the end of
    the two connections is the same.
    
    BUG=479172,490221
    TEST=none
    R=fsamuel@chromium.org
    
    Review URL: https://codereview.chromium.org/1316233012
    
    Cr-Commit-Position: refs/heads/master@{#347249}

--- components/web_view/frame.cc
+++ components/web_view/frame.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/web_view/frame_tree.h"
 #include "components/web_view/frame_tree_delegate.h"
 #include "components/web_view/frame_user_data.h"
+#include "mojo/application/public/interfaces/shell.mojom.h"
 
 using mojo::View;
 
@@ -273,7 +274,8 @@ void Frame::OnCanNavigateFrame(uint32_t app_id,
                                FrameTreeClient* frame_tree_client,
                                scoped_ptr<FrameUserData> user_data,
                                mojo::ViewTreeClientPtr view_tree_client) {
-  if (app_id == app_id_ && !FrameTree::AlwaysCreateNewFrameTree()) {
+  if (app_id == app_id_ && app_id != mojo::Shell::kInvalidContentHandlerID &&
+      !FrameTree::AlwaysCreateNewFrameTree()) {
     // The app currently rendering the frame will continue rendering it. In this
     // case we do not use the ViewTreeClient (because the app has a View already
     // and ends up reusing it).
