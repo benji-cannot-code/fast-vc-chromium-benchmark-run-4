8c88b899b2f38fdc07143364128e28459815606e
jbudorick <jbudorick@chromium.org>
2015-09-02 17:40:21 -0700

    [Android] Move Android version codes into devil/.
    BUG=476719
    
    Review URL: https://codereview.chromium.org/1325143002
    
    Cr-Commit-Position: refs/heads/master@{#347074}

--- build/android/devil/utils/mock_calls_test.py
+++ build/android/devil/utils/mock_calls_test.py
@@ -13,6 +13,7 @@ import os
 import sys
 import unittest
 
+from devil.android.sdk import version_codes
 from devil.utils import mock_calls
 from pylib import constants
 
@@ -42,7 +43,7 @@ class _DummyAdb(object):
   @property
   def build_version_sdk(self):
     logging.debug('(device %s) getting build_version_sdk', self)
-    return constants.ANDROID_SDK_VERSION_CODES.LOLLIPOP
+    return version_codes.LOLLIPOP
 
 
 class TestCaseWithAssertCallsTest(mock_calls.TestCase):
@@ -98,15 +99,12 @@ class TestCaseWithAssertCallsTest(mock_calls.TestCase):
         self.adb.Shell('echo hello')
 
   def testPatchCall_property(self):
-    self.assertEquals(constants.ANDROID_SDK_VERSION_CODES.LOLLIPOP,
-                      self.adb.build_version_sdk)
+    self.assertEquals(version_codes.LOLLIPOP, self.adb.build_version_sdk)
     with self.patch_call(
         self.call.adb.build_version_sdk,
-        return_value=constants.ANDROID_SDK_VERSION_CODES.KITKAT):
-      self.assertEquals(constants.ANDROID_SDK_VERSION_CODES.KITKAT,
-                        self.adb.build_version_sdk)
-    self.assertEquals(constants.ANDROID_SDK_VERSION_CODES.LOLLIPOP,
-                      self.adb.build_version_sdk)
+        return_value=version_codes.KITKAT):
+      self.assertEquals(version_codes.KITKAT, self.adb.build_version_sdk)
+    self.assertEquals(version_codes.LOLLIPOP, self.adb.build_version_sdk)
 
   def testAssertCalls_succeeds_simple(self):
     self.assertEquals(42, self.get_answer())
