d24a5570bceafbaa797bb355e2c600da3cb0cde1
jbroman@chromium.org <jbroman@chromium.org>
2015-09-04 16:44:32 +0000

    Replace use of scoped_ptr with OwnPtr.
    Use of the base namespace in Blink is not currently recommended. Fortunately
    WTF::OwnPtr is a drop-in replacement here.
    
    Review URL: https://codereview.chromium.org/1310743006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201802 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webgl/WebGL2RenderingContextBase.cpp
+++ third_party/WebKit/Source/modules/webgl/WebGL2RenderingContextBase.cpp
@@ -24,8 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/webgl/WebGLTransformFeedback.h"
 #include "modules/webgl/WebGLUniformLocation.h"
 #include "modules/webgl/WebGLVertexArrayObject.h"
-
 #include "platform/NotImplemented.h"
+#include "wtf/OwnPtr.h"
+#include "wtf/PassOwnPtr.h"
 
 namespace blink {
 
@@ -177,7 +178,7 @@ ScriptValue WebGL2RenderingContextBase::getInternalformatParameter(ScriptState* 
             if (length <= 0)
                 return WebGLAny(scriptState, DOMInt32Array::create(0));
 
-            scoped_ptr<GLint[]> values(new GLint[length]);
+            OwnPtr<GLint[]> values = adoptArrayPtr(new GLint[length]);
             for (GLint ii = 0; ii < length; ++ii)
                 values[ii] = 0;
             webContext()->getInternalformativ(target, internalformat, GL_SAMPLES, length, values.get());
@@ -1784,7 +1785,7 @@ String WebGL2RenderingContextBase::getActiveUniformBlockName(WebGLProgram* progr
         synthesizeGLError(GL_INVALID_VALUE, "getActiveUniformBlockName", "invalid uniform block index");
         return String();
     }
-    scoped_ptr<GLchar[]> name(new GLchar[maxNameLength]);
+    OwnPtr<GLchar[]> name = adoptArrayPtr(new GLchar[maxNameLength]);
     GLsizei length;
     webContext()->getActiveUniformBlockName(objectOrZero(program), uniformBlockIndex, maxNameLength, &length, name.get());
     return String(name.get(), length);
