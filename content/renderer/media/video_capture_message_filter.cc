ea4d80833c3e42b1169136b960c09043a02b75a5
emircan <emircan@chromium.org>
2015-09-03 19:43:28 -0700

    Passing multiple textures to renderer processes
    This is a CL for Video Capture using GpuMemoryBuffers plan[0]. The plans for future CLs can be seen on [1].
    
    [0] https://docs.google.com/document/d/1lobWf168_Kq05TLNLX81gRQwY5oe4k2t3oP-XTBEpIo/edit#heading=h.lflt7hoq3oui
    [1] https://docs.google.com/document/d/1lobWf168_Kq05TLNLX81gRQwY5oe4k2t3oP-XTBEpIo/edit#heading=h.exy0h9917c1d
    
    - Changed IPC call VideoCaptureMsg_BufferReady_Params to handle a vector of MailboxHolders to pass multiple mailboxes
    - Modified VideoCaptureImpl::OnBufferReceived to handle 3 cases appropriately: ShMem backed I420 frame, 1 texture backed ARGB frame, 3 R_8 textures backed I420 frame
    
    BUG=440843, 503835
    TEST=
    - Without the kUseGpuMemoryBuffersForCapture flag, AppRTC loopback runs as before.
    - With the flag, local playback works.
    - With the flag loopback crashes in webrtc::VideoFrame::ConvertNativeToI420Frame as expected.
    
    Review URL: https://codereview.chromium.org/1323073003
    
    Cr-Commit-Position: refs/heads/master@{#347322}

--- content/renderer/media/video_capture_message_filter.cc
+++ content/renderer/media/video_capture_message_filter.cc
@@ -161,7 +161,7 @@ void VideoCaptureMessageFilter::OnBufferReceived(
                              params.storage_type,
                              params.coded_size,
                              params.visible_rect,
-                             params.mailbox_holder);
+                             params.mailbox_holders);
 }
 
 void VideoCaptureMessageFilter::OnBufferDestroyed(int device_id,
