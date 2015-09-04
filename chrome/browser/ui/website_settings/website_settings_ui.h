7a0a805f675f41bb0b53bb6308e56f9f3ac26ad7
palmer <palmer@chromium.org>
2015-09-03 18:02:59 -0700

    Show a better string in the OIB when the site ran mixed script.
    Make GetSecuritySummary more accurate.
    
    BUG=517589
    
    Review URL: https://codereview.chromium.org/1324073002
    
    Cr-Commit-Position: refs/heads/master@{#347297}

--- chrome/browser/ui/website_settings/website_settings_ui.h
+++ chrome/browser/ui/website_settings/website_settings_ui.h
@@ -15,13 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "ui/gfx/native_widget_types.h"
 
-
 class GURL;
 class Profile;
 class WebsiteSettings;
-namespace content {
-struct SSLStatus;
-}
 
 namespace gfx {
 class Image;
