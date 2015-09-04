8855273e78e254f629f6cd047bb38352031d4fe3
dyen <dyen@chromium.org>
2015-09-04 09:54:49 -0700

    GPU Channel's now maintain a global order number for each processed IPC.
    Each IPC that a GPU Channel processes now is assigned a global order
    ID which is associated to each. As they get processed each channel
    can be queried what they are currently processing, what they have
    already processed, and what they will eventually process.
    
    The global order number also replaces the number of messages processed
    number that was used before. Technically they do not contain the same
    information (IE it is impossible to query how many messages a channel
    has processed now), but the old use cases didn't use the variable for
    this way. The old use cases can be replaced with the new order number,
    which was whether an IPC message has been processed already, and
    whether a channel has been active.
    
    BUG=514815
    
    Review URL: https://codereview.chromium.org/1308913004
    
    Cr-Commit-Position: refs/heads/master@{#347428}

--- content/common/gpu/gpu_channel.h
+++ content/common/gpu/gpu_channel.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_COMMON_GPU_GPU_CHANNEL_H_
 #define CONTENT_COMMON_GPU_GPU_CHANNEL_H_
 
-#include <deque>
 #include <string>
 
 #include "base/containers/scoped_ptr_hash_map.h"
@@ -51,6 +50,7 @@ class MessageFilter;
 namespace content {
 class GpuChannelManager;
 class GpuChannelMessageFilter;
+class GpuChannelMessageQueue;
 class GpuJpegDecodeAccelerator;
 class GpuWatchdog;
 
@@ -145,9 +145,6 @@ class CONTENT_EXPORT GpuChannel
 
   gpu::PreemptionFlag* GetPreemptionFlag();
 
-  bool handle_messages_scheduled() const { return handle_messages_scheduled_; }
-  uint64 messages_processed() const { return messages_processed_; }
-
   // If |preemption_flag->IsSet()|, any stub on this channel
   // should stop issuing GL commands. Setting this to NULL stops deferral.
   void SetPreemptByFlag(
@@ -176,6 +173,10 @@ class CONTENT_EXPORT GpuChannel
     return pending_valuebuffer_state_.get();
   }
 
+  uint32_t GetCurrentOrderNum() const { return current_order_num_; }
+  uint32_t GetProcessedOrderNum() const { return processed_order_num_; }
+  uint32_t GetUnprocessedOrderNum() const;
+
  protected:
   // The message filter on the io thread.
   scoped_refptr<GpuChannelMessageFilter> filter_;
@@ -185,6 +186,7 @@ class CONTENT_EXPORT GpuChannel
 
  private:
   friend class GpuChannelMessageFilter;
+  friend class GpuChannelMessageQueue;
 
   void OnDestroy();
 
@@ -201,8 +203,8 @@ class CONTENT_EXPORT GpuChannel
   void OnDestroyCommandBuffer(int32 route_id);
   void OnCreateJpegDecoder(int32 route_id, IPC::Message* reply_msg);
 
-  // Decrement the count of unhandled IPC messages and defer preemption.
-  void MessageProcessed();
+  // Update processed order number and defer preemption.
+  void MessageProcessed(uint32_t order_number);
 
   // The lifetime of objects of this class is managed by a GpuChannelManager.
   // The GpuChannelManager destroy all the GpuChannels that they own when they
@@ -217,8 +219,6 @@ class CONTENT_EXPORT GpuChannel
   // Used to implement message routing functionality to CommandBuffer objects
   MessageRouter router_;
 
-  uint64 messages_processed_;
-
   // Whether the processing of IPCs on this channel is stalled and we should
   // preempt other GpuChannels.
   scoped_refptr<gpu::PreemptionFlag> preempting_flag_;
@@ -227,7 +227,7 @@ class CONTENT_EXPORT GpuChannel
   // commands (via their GpuScheduler) when preempted_flag_->IsSet()
   scoped_refptr<gpu::PreemptionFlag> preempted_flag_;
 
-  std::deque<IPC::Message*> deferred_messages_;
+  scoped_refptr<GpuChannelMessageQueue> message_queue_;
 
   // The id of the client who is on the other side of the channel.
   int client_id_;
@@ -254,8 +254,12 @@ class CONTENT_EXPORT GpuChannel
   gpu::gles2::DisallowedFeatures disallowed_features_;
   GpuWatchdog* watchdog_;
   bool software_;
-  bool handle_messages_scheduled_;
-  IPC::Message* currently_processing_message_;
+
+  // Current IPC order number being processed.
+  uint32_t current_order_num_;
+
+  // Last finished IPC order number.
+  uint32_t processed_order_num_;
 
   size_t num_stubs_descheduled_;
 
--- content/common/gpu/gpu_channel_manager.cc
+++ content/common/gpu/gpu_channel_manager.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/common/gpu/gpu_channel_manager.h"
 
+#include <algorithm>
+
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/location.h"
@@ -248,24 +250,28 @@ void GpuChannelManager::OnLoadedShader(std::string program_proto) {
     program_cache()->LoadProgram(program_proto);
 }
 
-bool GpuChannelManager::HandleMessagesScheduled() {
+uint32_t GpuChannelManager::ProcessedOrderNumber() {
+  uint32_t processed_order_num = 0;
   for (auto& kv : gpu_channels_) {
-    if (kv.second->handle_messages_scheduled())
-      return true;
+    processed_order_num =
+        std::max(processed_order_num, kv.second->GetProcessedOrderNum());
   }
-  return false;
+  return processed_order_num;
 }
 
-uint64 GpuChannelManager::MessagesProcessed() {
-  uint64 messages_processed = 0;
-  for (auto& kv : gpu_channels_)
-    messages_processed += kv.second->messages_processed();
-  return messages_processed;
+uint32_t GpuChannelManager::UnprocessedOrderNumber() {
+  uint32_t unprocessed_order_num = 0;
+  for (auto& kv : gpu_channels_) {
+    unprocessed_order_num =
+        std::max(unprocessed_order_num, kv.second->GetUnprocessedOrderNum());
+  }
+  return unprocessed_order_num;
 }
 
 void GpuChannelManager::LoseAllContexts() {
-  for (auto& kv : gpu_channels_)
+  for (auto& kv : gpu_channels_) {
     kv.second->MarkAllContextsLost();
+  }
   task_runner_->PostTask(FROM_HERE,
                          base::Bind(&GpuChannelManager::OnLoseAllContexts,
                                     weak_factory_.GetWeakPtr()));
