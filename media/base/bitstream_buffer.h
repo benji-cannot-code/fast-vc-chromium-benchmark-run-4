ca5e0b698ec0e55aa2bd15ad72367ba0f8eca10c
watk <watk@chromium.org>
2015-09-04 10:53:07 -0700

    Pass the bitstream buffer timestamp to AndroidVideoDecodeAccelerator
    On certain devices MediaCodec relies on the input buffer timestamps to
    order the output frames. Previously AVDA was abusing the timestamp
    field to pass through the bitstream buffer id, which counts up in
    decode order. This results in output frames being delivered in decode
    order (bitstream buffer id order) by at least one decoder.
    
    Now BitstreamBuffer has an optional timestamp field which AVDA
    requires to be valid. Buffers are given to MediaCodec with the correct
    timestamps and mapped back to ids when returning output Pictures to the
    client.
    
    This is can be cleaned up when the VDA interface is updated to include
    the timestamp (http://crbug.com/522298)
    
    BUG=523053
    TEST=Manually, including apprtc.appspot.com
    
    Review URL: https://codereview.chromium.org/1315323004
    
    Cr-Commit-Position: refs/heads/master@{#347444}

--- media/base/bitstream_buffer.h
+++ media/base/bitstream_buffer.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/shared_memory.h"
+#include "base/time/time.h"
+#include "media/base/buffers.h"
 
 namespace media {
 
@@ -18,17 +20,36 @@ class BitstreamBuffer {
   BitstreamBuffer(int32 id, base::SharedMemoryHandle handle, size_t size)
       : id_(id),
         handle_(handle),
-        size_(size) {
-  }
+        size_(size),
+        presentation_timestamp_(kNoTimestamp()) {}
+
+  BitstreamBuffer(int32 id,
+                  base::SharedMemoryHandle handle,
+                  size_t size,
+                  base::TimeDelta presentation_timestamp)
+      : id_(id),
+        handle_(handle),
+        size_(size),
+        presentation_timestamp_(presentation_timestamp) {}
 
   int32 id() const { return id_; }
   base::SharedMemoryHandle handle() const { return handle_; }
   size_t size() const { return size_; }
 
+  // The timestamp is only valid if it's not equal to |media::kNoTimestamp()|.
+  base::TimeDelta presentation_timestamp() const {
+    return presentation_timestamp_;
+  }
+
  private:
   int32 id_;
   base::SharedMemoryHandle handle_;
   size_t size_;
+
+  // This is only set when necessary. For example, AndroidVideoDecodeAccelerator
+  // needs the timestamp because the underlying decoder may require it to
+  // determine the output order.
+  base::TimeDelta presentation_timestamp_;
 
   // Allow compiler-generated copy & assign constructors.
 };
