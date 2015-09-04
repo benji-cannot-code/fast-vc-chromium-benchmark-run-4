85205f16bf49a21f89a3177391b3f9ea22f3cdc9
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2015-09-04 20:33:51 +0000

    Fix table cell background caching issue about interest rect
    For all display items for one object, if we check for interect rect
    intersection and early return, we must use the same rule. Otherwise,
    we may generate display item A but not display item B in some cases;
    and once we generate display item B again, we'll mistakenly generate
    CachedDisplayItem for it because we think the object is validly cached.
    
    Table cell may generate more than two display items for backgrounds,
    one drawing backgrounds from containers, one drawing its own background.
    Previously they use different early return rules.
    
    Now let them use the same rule (BlockPainter::isVisibleInPaintRect()).
    
    Also add DisplayItem::TableCellBackgroundFromContainers because the
    display items are issued by TableSectionPainter which doesn't know if
    TableCellPainter::paint() would use the phase for other display items.
    
    BUG=522338
    TEST=DisplayItemListPaintTest.TableCellBackbroundInterestRect
    
    Review URL: https://codereview.chromium.org/1312493007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201820 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/TableSectionPainter.cpp
+++ third_party/WebKit/Source/core/paint/TableSectionPainter.cpp
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/LayoutTableCell.h"
 #include "core/layout/LayoutTableCol.h"
 #include "core/layout/LayoutTableRow.h"
+#include "core/paint/BlockPainter.h"
 #include "core/paint/BoxClipper.h"
 #include "core/paint/LayoutObjectDrawingRecorder.h"
 #include "core/paint/ObjectPainter.h"
@@ -161,7 +162,8 @@ void TableSectionPainter::paintCell(LayoutTableCell* cell, const PaintInfo& pain
     PaintPhase paintPhase = paintInfo.phase;
     LayoutTableRow* row = toLayoutTableRow(cell->parent());
 
-    if (paintPhase == PaintPhaseBlockBackground || paintPhase == PaintPhaseChildBlockBackground) {
+    if ((paintPhase == PaintPhaseBlockBackground || paintPhase == PaintPhaseChildBlockBackground)
+        && BlockPainter(*cell).intersectsPaintRect(paintInfo, paintOffset)) {
         // We need to handle painting a stack of backgrounds. This stack (from bottom to top) consists of
         // the column group, column, row group, row, and then the cell.
         LayoutTableCol* column = m_layoutTableSection.table()->colElement(cell->col());
@@ -174,8 +176,8 @@ void TableSectionPainter::paintCell(LayoutTableCell* cell, const PaintInfo& pain
 
         if (columnHasBackground || columnGroupHasBackground || sectionHasBackground || rowHasBackground) {
             TableCellPainter tableCellPainter(*cell);
-            if (!LayoutObjectDrawingRecorder::useCachedDrawingIfPossible(*paintInfo.context, *cell, paintPhase, paintOffset)) {
-                LayoutObjectDrawingRecorder recorder(*paintInfo.context, *cell, paintPhase, tableCellPainter.paintBounds(cellPoint, TableCellPainter::AddOffsetFromParent), paintOffset);
+            if (!LayoutObjectDrawingRecorder::useCachedDrawingIfPossible(*paintInfo.context, *cell, DisplayItem::TableCellBackgroundFromContainers, paintOffset)) {
+                LayoutObjectDrawingRecorder recorder(*paintInfo.context, *cell, DisplayItem::TableCellBackgroundFromContainers, tableCellPainter.paintBounds(cellPoint, TableCellPainter::AddOffsetFromParent), paintOffset);
                 // Column groups and columns first.
                 // FIXME: Columns and column groups do not currently support opacity, and they are being painted "too late" in
                 // the stack, since we have already opened a transparency layer (potentially) for the table row group.
