a5b424b8af1fd67b775ee6207b5078cce1dc2055
dgozman@chromium.org <dgozman@chromium.org>
2015-09-04 16:20:57 +0000

    Store parsed meta viewport even when it's currently disabled.
    By storing parsed meta viewport, we can apply it without reload when emulating mobile device.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1328553003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201799 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/inspector/device-emulation/device-emulation-test.js
+++ third_party/WebKit/LayoutTests/inspector/device-emulation/device-emulation-test.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-function setMetaViewport()
+function setMetaViewport(override)
 {
     var VIEWPORTS = {
         "w=320": "width=320",
@@ -15,6 +15,8 @@ function setMetaViewport()
             break;
         }
     }
+    if (override)
+        viewport = VIEWPORTS[override];
     if (viewport != VIEWPORTS["none"])
         document.write('<meta name="viewport" content="'+viewport+'">');
 }
@@ -188,7 +190,7 @@ InspectorTest.getPageMetrics = function(full, callback)
     InspectorTest.evaluateInPage("dumpMetrics(" + full + ")", callback);
 }
 
-InspectorTest.applyEmulationAndReload = function(enabled, width, height, deviceScaleFactor, viewport, insets, callback)
+InspectorTest.applyEmulationAndReload = function(enabled, width, height, deviceScaleFactor, viewport, insets, noReload, callback)
 {
     function PageResizer()
     {
@@ -219,15 +221,18 @@ InspectorTest.applyEmulationAndReload = function(enabled, width, height, deviceS
         var warning = WebInspector.overridesSupport.warningMessage();
         if (warning)
             InspectorTest._deviceEmulationResults.push("Emulation warning: " + warning);
-        InspectorTest.navigate(InspectorTest._deviceEmulationPageUrl + "?" + viewport, callback);
+        if (noReload)
+            callback();
+        else
+            InspectorTest.navigate(InspectorTest._deviceEmulationPageUrl + "?" + viewport, callback);
     }
 };
 
-InspectorTest.emulateAndGetMetrics = function(width, height, deviceScaleFactor, viewport, insets, callback)
+InspectorTest.emulateAndGetMetrics = function(width, height, deviceScaleFactor, viewport, insets, noReload, callback)
 {
     InspectorTest._deviceEmulationResults.push("Emulating device: " + width + "x" + height + "x" + deviceScaleFactor + " viewport='" + viewport + "'");
     var full = !!width && !!height && !!deviceScaleFactor;
-    InspectorTest.applyEmulationAndReload(true, width, height, deviceScaleFactor, viewport, insets, InspectorTest.getPageMetrics.bind(InspectorTest, full, printMetrics));
+    InspectorTest.applyEmulationAndReload(true, width, height, deviceScaleFactor, viewport, insets, noReload, InspectorTest.getPageMetrics.bind(InspectorTest, full, printMetrics));
 
     function printMetrics(metrics)
     {
@@ -236,11 +241,11 @@ InspectorTest.emulateAndGetMetrics = function(width, height, deviceScaleFactor, 
     }
 };
 
-InspectorTest.testDeviceEmulation = function(pageUrl, width, height, deviceScaleFactor, viewport, insets)
+InspectorTest.testDeviceEmulation = function(pageUrl, width, height, deviceScaleFactor, viewport, insets, noReload)
 {
     InspectorTest._deviceEmulationPageUrl = pageUrl;
     InspectorTest._deviceEmulationResults = [];
-    InspectorTest.emulateAndGetMetrics(width, height, deviceScaleFactor, viewport, insets, callback);
+    InspectorTest.emulateAndGetMetrics(width, height, deviceScaleFactor, viewport, insets, noReload, callback);
 
     function callback()
     {
