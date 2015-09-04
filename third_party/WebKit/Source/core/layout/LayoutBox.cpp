17c5c8d4fd115bcdbce55a056c5491bbe3f6b3e2
pdr@chromium.org <pdr@chromium.org>
2015-09-04 17:08:25 +0000

    Invalidate background obscuration after loading background images
    This patch fixes a bug where a background would be considered obscured
    during loading but would not get reexamined for the background
    obscuration bit after the image loaded. Other loading codepaths
    (e.g., <images>) had this case covered but background images were
    missing this check.
    
    BUG=422272
    
    Review URL: https://codereview.chromium.org/1312943008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201804 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -1354,6 +1354,9 @@ bool LayoutBox::invalidatePaintOfLayerRectsForImage(WrappedImagePtr image, const
                 setShouldDoFullPaintInvalidation(PaintInvalidationDelayedFull);
             else
                 setShouldDoFullPaintInvalidation();
+
+            if (drawingBackground)
+                invalidateBackgroundObscurationStatus();
             return true;
         }
     }
