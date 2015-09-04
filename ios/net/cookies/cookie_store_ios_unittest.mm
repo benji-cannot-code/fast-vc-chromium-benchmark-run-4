41a9e6506693029beda0898506e017aff65ca608
ellyjones <ellyjones@chromium.org>
2015-09-04 10:22:24 -0700

    CookieStoreIOS: handle dotted domains correctly
    On iOS <9.0, NSHTTPCookie used to leave leading dots in place on the Domain
    attribute of cookies, and CookieStoreIOS used this behavior to differentiate
    host-only cookies (those with no Domain attribute) from those with an explicit
    Domain attribute that happened to match the hostname. On iOS >=9.0, this
    behavior has changed and it is no longer possible to differentiate cookies in
    this manner. Instead, CookieStoreIOS now uses ParsedCookie to check for a
    Domain attribute's presence directly.
    
    This causes CookieStoreIOS to begin supporting trailing dots on URLs: since
    NSHTTPCookie strips trailing dots from the Domain attribute, the previous
    approach of checking the cookie's domain against the URL's host failed when
    the URL's host had a trailing dot. Since CookieStoreIOS now checks directly for
    Domain this problem no longer occurs and the behavior of CookieStoreIOS is
    closer to that of CookieMonster.
    
    This CL also introduces a new flag 'preserves_trailing_dots' into
    CookieStoreTestTraits. This flag indicates that the cookie store does not fold
    the domains 'foo.com.' and 'foo.com' together. CookieMonster does preserve
    trailing dots (ie, does not fold those domains); CookieStoreIOS does not
    preserve trailing dots (ie, does fold those domains).
    
    BUG=516603
    
    Review URL: https://codereview.chromium.org/1318063002
    
    Cr-Commit-Position: refs/heads/master@{#347439}

--- ios/net/cookies/cookie_store_ios_unittest.mm
+++ ios/net/cookies/cookie_store_ios_unittest.mm
@@ -39,7 +39,7 @@ struct CookieStoreIOSTestTraits {
   static const bool is_cookie_monster = false;
   static const bool supports_http_only = false;
   static const bool supports_non_dotted_domains = false;
-  static const bool supports_trailing_dots = false;
+  static const bool preserves_trailing_dots = false;
   static const bool filters_schemes = false;
   static const bool has_path_prefix_bug = true;
   static const int creation_time_granularity_in_ms = 1000;
@@ -55,7 +55,7 @@ struct InactiveCookieStoreIOSTestTraits {
   static const bool is_cookie_monster = false;
   static const bool supports_http_only = false;
   static const bool supports_non_dotted_domains = true;
-  static const bool supports_trailing_dots = true;
+  static const bool preserves_trailing_dots = true;
   static const bool filters_schemes = false;
   static const bool has_path_prefix_bug = false;
   static const int creation_time_granularity_in_ms = 0;
@@ -165,7 +165,7 @@ struct RoundTripTestCookieStoreTraits {
   static const bool is_cookie_monster = false;
   static const bool supports_http_only = false;
   static const bool supports_non_dotted_domains = false;
-  static const bool supports_trailing_dots = false;
+  static const bool preserves_trailing_dots = false;
   static const bool filters_schemes = false;
   static const bool has_path_prefix_bug = true;
   static const int creation_time_granularity_in_ms = 1000;
