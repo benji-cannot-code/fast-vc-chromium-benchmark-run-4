f92c35648a9528f925a2f4c57a5853275892e39c
tedchoc <tedchoc@chromium.org>
2015-09-03 19:40:55 -0700

    Ensure native pages are snapshots in the SimpleAnimationLayout.
    This ensures a snapshot is taken if you long press, open tab in background while on a native page.
    
    BUG=527015
    
    Review URL: https://codereview.chromium.org/1309563005
    
    Cr-Commit-Position: refs/heads/master@{#347318}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/phone/SimpleAnimationLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/phone/SimpleAnimationLayout.java
@@ -22,6 +22,7 @@ import org.chromium.chrome.browser.compositor.layouts.phone.stack.StackAnimation
 import org.chromium.chrome.browser.compositor.scene_layer.SceneLayer;
 import org.chromium.chrome.browser.compositor.scene_layer.TabListSceneLayer;
 import org.chromium.chrome.browser.fullscreen.ChromeFullscreenManager;
+import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.ui.interpolators.BakedBezierInterpolator;
 import org.chromium.ui.resources.ResourceManager;
@@ -88,6 +89,12 @@ public class SimpleAnimationLayout
     @Override
     public void show(long time, boolean animate) {
         super.show(time, animate);
+
+        if (mTabModelSelector != null && mTabContentManager != null) {
+            Tab tab = mTabModelSelector.getCurrentTab();
+            if (tab != null && tab.isNativePage()) mTabContentManager.cacheTabThumbnail(tab);
+        }
+
         reset();
     }
 
