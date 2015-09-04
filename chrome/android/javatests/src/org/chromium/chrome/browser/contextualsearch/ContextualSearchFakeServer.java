bbb9183c71b79d128ce0bb39c0cc92a0eedc6d56
mdjones <mdjones@chromium.org>
2015-09-04 15:16:59 -0700

    Move functionality for ContentViewCore to ContextualSearchPanel
    This change moves code related to the content of the contextual
    search panel to the panel class.
    
    BUG=521773
    
    Review URL: https://codereview.chromium.org/1304013002
    
    Cr-Commit-Position: refs/heads/master@{#347501}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchFakeServer.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchFakeServer.java
@@ -19,9 +19,11 @@ import javax.annotation.Nullable;
  *              be something like ContextualSearchFakeEnvironment.
  */
 @VisibleForTesting
-class ContextualSearchFakeServer implements ContextualSearchNetworkCommunicator {
+class ContextualSearchFakeServer implements ContextualSearchNetworkCommunicator,
+        ContextualSearchContentController {
 
     private final ContextualSearchNetworkCommunicator mBaseManager;
+    private final ContextualSearchContentController mContentController;
     private String mLoadedUrl;
     private String mSearchTermRequested;
     private boolean mShouldUseHttps;
@@ -33,8 +35,10 @@ class ContextualSearchFakeServer implements ContextualSearchNetworkCommunicator 
      * @param baseManager The manager to call back to for server responses.
      */
     @VisibleForTesting
-    ContextualSearchFakeServer(ContextualSearchNetworkCommunicator baseManager) {
+    ContextualSearchFakeServer(ContextualSearchNetworkCommunicator baseManager,
+            ContextualSearchContentController contentController) {
         mBaseManager = baseManager;
+        mContentController = contentController;
     }
 
     @Override
@@ -49,7 +53,7 @@ class ContextualSearchFakeServer implements ContextualSearchNetworkCommunicator 
         mLoadedUrlCount++;
         // This will not actually load a URL because no Search Content View will be created
         // when under test -- see comments in createNewSearchContentView.
-        mBaseManager.loadUrl(url);
+        mContentController.loadUrl(url);
     }
 
     @Override
@@ -76,12 +80,7 @@ class ContextualSearchFakeServer implements ContextualSearchNetworkCommunicator 
     }
 
     @Override
-    public void handleDidNavigateMainFrame(String url, int httpResultCode) {
-        mBaseManager.handleDidNavigateMainFrame(url, httpResultCode);
-    }
-
-    @Override
-    public void createNewSearchContentView() {
+    public void createNewContentView() {
         mIsSearchContentViewCreated = true;
         // Don't call the super method because that will cause loadUrl to make a live request!
         // This method is only called by loadUrl, which will subseqently check if the CV was
@@ -90,9 +89,9 @@ class ContextualSearchFakeServer implements ContextualSearchNetworkCommunicator 
     }
 
     @Override
-    public void destroySearchContentView() {
+    public void destroyContentView() {
         mIsSearchContentViewCreated = false;
-        mBaseManager.destroySearchContentView();
+        mContentController.destroyContentView();
     }
 
     /**
