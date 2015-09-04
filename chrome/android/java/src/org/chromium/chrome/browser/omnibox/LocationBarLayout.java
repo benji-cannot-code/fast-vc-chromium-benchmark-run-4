faa97e4cd8c61a0bec14cd0fc592114f4ffd48d9
palmer <palmer@chromium.org>
2015-09-04 15:48:10 -0700

    Make sure that mixed content does not result in green HTTPS.
    BUG=513863
    
    Review URL: https://codereview.chromium.org/1329903003
    
    Cr-Commit-Position: refs/heads/master@{#347513}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -1209,10 +1209,10 @@ public class LocationBarLayout extends FrameLayout implements OnClickListener,
             updateSecurityButton(false);
         } else {
             updateSecurityButton(true);
-            // Since we emphasize the schema of the URL based on the security type, we need to
-            // refresh the emphasis.
-            mUrlBar.deEmphasizeUrl();
         }
+        // Since we emphasize the schema of the URL based on the security type, we need to
+        // refresh the emphasis.
+        mUrlBar.deEmphasizeUrl();
         emphasizeUrl();
     }
 
