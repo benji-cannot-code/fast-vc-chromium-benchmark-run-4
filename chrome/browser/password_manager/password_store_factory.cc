9ff3450287ed63438180e5a4a5ea05976d213aea
mkwst <mkwst@chromium.org>
2015-09-07 06:16:48 -0700

    Expose PasswordStore's AffiliatedMatchHelper.
    The Credential Manager API needs access to the affiliated match helper
    in order to synthesize credentials for an origin. This patch exposes the
    member by changing |HasAffiliatedMatchHelper()| into a getter. No
    behavioral changes in this patch.
    
    BUG=528312
    
    Review URL: https://codereview.chromium.org/1333453002
    
    Cr-Commit-Position: refs/heads/master@{#347627}

--- chrome/browser/password_manager/password_store_factory.cc
+++ chrome/browser/password_manager/password_store_factory.cc
@@ -168,10 +168,10 @@ void PasswordStoreFactory::OnPasswordsSyncedStatePotentiallyChanged(
     return;
 
   if (ShouldAffiliationBasedMatchingBeActive(profile) &&
-      !password_store->HasAffiliatedMatchHelper()) {
+      !password_store->affiliated_match_helper()) {
     ActivateAffiliationBasedMatching(password_store.get(), profile);
   } else if (!ShouldAffiliationBasedMatchingBeActive(profile) &&
-             password_store->HasAffiliatedMatchHelper()) {
+             password_store->affiliated_match_helper()) {
     password_store->SetAffiliatedMatchHelper(
         make_scoped_ptr<password_manager::AffiliatedMatchHelper>(nullptr));
   }
@@ -181,7 +181,7 @@ void PasswordStoreFactory::OnPasswordsSyncedStatePotentiallyChanged(
 void PasswordStoreFactory::TrimOrDeleteAffiliationCache(Profile* profile) {
   scoped_refptr<PasswordStore> password_store =
       GetForProfile(profile, ServiceAccessType::EXPLICIT_ACCESS);
-  if (password_store && password_store->HasAffiliatedMatchHelper()) {
+  if (password_store && password_store->affiliated_match_helper()) {
     password_store->TrimAffiliationCache();
   } else {
     scoped_refptr<base::SingleThreadTaskRunner> db_thread_runner(
