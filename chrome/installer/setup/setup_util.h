9347aa17463163bf9eb02af6179cd948b0094b6c
grt <grt@chromium.org>
2015-09-07 06:36:12 -0700

    Delete Chrome state in registry on uninstall when the user profile is being deleted.
    BUG=none
    R=robertshield@chromium.org
    
    Review URL: https://codereview.chromium.org/1327793004
    
    Cr-Commit-Position: refs/heads/master@{#347631}

--- chrome/installer/setup/setup_util.h
+++ chrome/installer/setup/setup_util.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <windows.h>
 
+#include <vector>
+
 #include "base/basictypes.h"
 #include "base/strings/string16.h"
 #include "base/win/scoped_handle.h"
@@ -124,6 +126,14 @@ bool IsProcessorSupported();
 base::string16 GetRegistrationDataCommandKey(
     const AppRegistrationData& reg_data,
     const wchar_t* name);
+
+// Deletes all values and subkeys of the key |path| under |root|, preserving
+// the keys named in |keys_to_preserve| (each of which must be an ASCII string).
+// The key itself is deleted if no subkeys are preserved.
+void DeleteRegistryKeyPartial(
+    HKEY root,
+    const base::string16& path,
+    const std::vector<base::string16>& keys_to_preserve);
 
 // This class will enable the privilege defined by |privilege_name| on the
 // current process' token. The privilege will be disabled upon the
