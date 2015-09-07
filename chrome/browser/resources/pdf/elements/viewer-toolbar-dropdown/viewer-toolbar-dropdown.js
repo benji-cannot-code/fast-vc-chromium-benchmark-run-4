a228a3a89aca6ccf9c491fd7f3ff8c3abb312227
tsergeant <tsergeant@chromium.org>
2015-09-06 19:21:06 -0700

    Material PDF: Variety of minor UI bugfixes
    * Fix regression where dropdowns would not be sized correctly on first
      open.
    * Make page selector only accept numerical inputs
    * Position top toolbar z-level above zoom toolbar
    * Ellipsize long bookmark names
    
    BUG=527776,527798,526981
    
    Review URL: https://codereview.chromium.org/1310353003
    
    Cr-Commit-Position: refs/heads/master@{#347597}

--- chrome/browser/resources/pdf/elements/viewer-toolbar-dropdown/viewer-toolbar-dropdown.js
+++ chrome/browser/resources/pdf/elements/viewer-toolbar-dropdown/viewer-toolbar-dropdown.js
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var DROPDOWN_OUTER_PADDING = 2;
 
   /** Minimum height of toolbar dropdowns (px). */
-  var MIN_DROPDOWN_HEIGHT = 300;
+  var MIN_DROPDOWN_HEIGHT = 200;
 
   Polymer({
     is: 'viewer-toolbar-dropdown',
--- chrome/browser/resources/pdf/toolbar_manager.js
+++ chrome/browser/resources/pdf/toolbar_manager.js
@@ -63,7 +63,6 @@ function ToolbarManager(window, toolbar, zoomToolbar) {
   this.sideToolbarAllowedOnlyTimer_ = null;
 
   this.window_.addEventListener('resize', this.resizeDropdowns_.bind(this));
-  this.resizeDropdowns_();
 }
 
 ToolbarManager.prototype = {
@@ -75,6 +74,7 @@ ToolbarManager.prototype = {
   enableToolbars: function() {
     this.toolbar_.hidden = false;
     this.zoomToolbar_.hidden = false;
+    this.resizeDropdowns_();
   },
 
   showToolbarsForMouseMove: function(e) {
