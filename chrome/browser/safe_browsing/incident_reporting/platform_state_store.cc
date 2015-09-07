9f3ca7df94f3c587f2f57d2a2f4f7c8951e2d718
Nico Weber <thakis@chromium.org>
2015-09-07 09:34:11 -0700

    win/clang: Try to fix build after https://codereview.chromium.org/1243293003
    chrome\browser\safe_browsing\incident_reporting\platform_state_store.cc(139,11):
          error: enumeration value 'NUM_RESULTS' not handled in switch
                 [-Werror,-Wswitch]
      switch (result) {
              ^
    
    BUG=82385,496744
    TBR=grt
    
    Review URL: https://codereview.chromium.org/1320093003 .
    
    Cr-Commit-Position: refs/heads/master@{#347637}

--- chrome/browser/safe_browsing/incident_reporting/platform_state_store.cc
+++ chrome/browser/safe_browsing/incident_reporting/platform_state_store.cc
@@ -150,6 +150,9 @@ scoped_ptr<base::DictionaryValue> Load(Profile* profile) {
       // Return null for all error cases.
       value_dict.reset();
       break;
+    case PlatformStateStoreLoadResult::NUM_RESULTS:
+      NOTREACHED();
+      break;
   }
   UMA_HISTOGRAM_ENUMERATION(
       "SBIRS.PSSLoadResult", static_cast<uint32_t>(result),
