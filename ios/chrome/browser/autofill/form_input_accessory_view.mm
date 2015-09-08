fb76764b7a10ac2f18340ba617c8e407a9a0cfe3
sdefresne <sdefresne@chromium.org>
2015-09-08 02:16:15 -0700

    Fix name of constants in ios/chrome/app/strings/ios_strings.grd.
    All iOS specific strings should be referenced by a constant named
    IDS_IOS_* so fix the newly introduced constants to use the correct
    name.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1330813003
    
    Cr-Commit-Position: refs/heads/master@{#347682}

--- ios/chrome/browser/autofill/form_input_accessory_view.mm
+++ ios/chrome/browser/autofill/form_input_accessory_view.mm
@@ -267,7 +267,7 @@ UIImage* ButtonImage(NSString* name) {
                                 height:CGRectGetHeight(frame)];
   [previousButton
       setAccessibilityLabel:l10n_util::GetNSString(
-                                IDS_AUTOFILL_ACCNAME_PREVIOUS_FIELD)];
+                                IDS_IOS_AUTOFILL_ACCNAME_PREVIOUS_FIELD)];
   [navView addSubview:previousButton];
   currentX += kNavigationButtonWidth;
 
@@ -291,7 +291,7 @@ UIImage* ButtonImage(NSString* name) {
                                originY:firstRow
                                 height:CGRectGetHeight(frame)];
   [nextButton setAccessibilityLabel:l10n_util::GetNSString(
-                                        IDS_AUTOFILL_ACCNAME_NEXT_FIELD)];
+                                        IDS_IOS_AUTOFILL_ACCNAME_NEXT_FIELD)];
   [navView addSubview:nextButton];
   currentX += kNavigationButtonWidth;
 
@@ -320,8 +320,9 @@ UIImage* ButtonImage(NSString* name) {
                                  originX:currentX
                                  originY:firstRow
                                   height:CGRectGetHeight(frame)];
-    [closeButton setAccessibilityLabel:l10n_util::GetNSString(
-                                           IDS_AUTOFILL_ACCNAME_HIDE_KEYBOARD)];
+    [closeButton
+        setAccessibilityLabel:l10n_util::GetNSString(
+                                  IDS_IOS_AUTOFILL_ACCNAME_HIDE_KEYBOARD)];
     [navView addSubview:closeButton];
     currentX += kNavigationButtonWidth;
   }
