0e0a1bd546c4f6b1cc0697ac5e022d0b30e84365
nednguyen <nednguyen@google.com>
2015-09-08 16:38:13 -0700

    [Telemetry] Set telemetry's run_test verbosity to 3
    BUG=457446
    
    Review URL: https://codereview.chromium.org/1318003007
    
    Cr-Commit-Position: refs/heads/master@{#347827}

--- tools/telemetry/telemetry/testing/run_tests.py
+++ tools/telemetry/telemetry/testing/run_tests.py
@@ -132,7 +132,7 @@ class RunTestsCommand(command_line.OptparseCommand):
 
     # Always print out these info for the ease of debugging.
     runner.args.timing = True
-    runner.args.verbose = 2
+    runner.args.verbose = 3
 
     runner.classifier = GetClassifier(args, possible_browser)
     runner.context = args
