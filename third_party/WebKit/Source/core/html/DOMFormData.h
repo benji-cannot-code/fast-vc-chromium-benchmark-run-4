f55c7e822db71b1a33b272775e53569546580860
tkent@chromium.org <tkent@chromium.org>
2015-09-08 06:42:42 +0000

    Move FormDataList::setEntry to DOMFormData.
    Also, remove FormDataList::setData and setBlob.  This simplifies the code.
    This CL has no behavior changes.
    
    BUG=528840
    
    Review URL: https://codereview.chromium.org/1320463010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201898 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/DOMFormData.h
+++ third_party/WebKit/Source/core/html/DOMFormData.h
@@ -81,6 +81,7 @@ public:
 private:
     explicit DOMFormData(const WTF::TextEncoding&);
     explicit DOMFormData(HTMLFormElement*);
+    void setEntry(const Item&);
 
     IterationSource* startIteration(ScriptState*, ExceptionState&) override;
     bool m_opaque;
--- third_party/WebKit/Source/core/html/FormDataList.h
+++ third_party/WebKit/Source/core/html/FormDataList.h
@@ -108,8 +108,6 @@ public:
     Entry getEntry(size_t index) const;
     HeapVector<Entry> getAll(const String& key) const;
     bool hasEntry(const String& key) const;
-    void setBlob(const String& key, Blob*, const String& filename);
-    void setData(const String& key, const String& value);
     size_t size() const { return m_items.size(); }
 
     const FormDataListItems& items() const { return m_items; }
@@ -122,17 +120,17 @@ public:
 
 protected:
     explicit FormDataList(const WTF::TextEncoding&);
+    CString encodeAndNormalize(const String& key) const;
+
+    FormDataListItems m_items;
 
 private:
     void appendKeyValuePairItemsTo(FormData*, const WTF::TextEncoding&, bool isMultiPartForm, FormData::EncodingType = FormData::FormURLEncoded);
 
     void appendItem(const Item&);
-    void setEntry(const Item&);
     Entry itemsToEntry(const Item&) const;
-    CString encodeAndNormalize(const String& key) const;
 
     WTF::TextEncoding m_encoding;
-    FormDataListItems m_items;
 };
 
 } // namespace blink
