6d07dea597fd71d25a6587e22029f5497d2d1abb
sbc <sbc@chromium.org>
2015-09-08 15:53:49 -0700

    [NaCl SDK] Add msan support to SDK build system
    Also fix config name for asan builds
    
    CQ_EXTRA_TRYBOTS=tryserver.chromium.linux:linux_nacl_sdk;tryserver.chromium.mac:mac_nacl_sdk
    
    Review URL: https://codereview.chromium.org/1331443008
    
    Cr-Commit-Position: refs/heads/master@{#347817}

--- native_client_sdk/src/tools/host_gcc.mk
+++ native_client_sdk/src/tools/host_gcc.mk
@@ -16,6 +16,10 @@ ifdef TSAN
 CLANG = 1
 endif
 
+ifdef MSAN
+CLANG = 1
+endif
+
 #
 # Macros for TOOLS
 #
@@ -68,6 +72,11 @@ endif
 ifdef TSAN
 HOST_CFLAGS += -fsanitize=thread
 HOST_LDFLAGS += -pie -fsanitize=thread
+endif
+
+ifdef MSAN
+HOST_CFLAGS += -fsanitize=memory
+HOST_LDFLAGS += -pie -fsanitize=memory
 endif
 
 #
