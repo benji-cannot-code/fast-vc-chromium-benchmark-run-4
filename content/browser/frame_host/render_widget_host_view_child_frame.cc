1fe74f04818f1c8b55d862684d9bf0c3569b3156
lfg <lfg@chromium.org>
2015-09-08 13:48:45 -0700

    Fix OOPIF rendering on HiDPI devices.
    When creating the backing surface, we need to multiply the frame rect by
    the device scale factor.
    
    BUG=522237
    
    Review URL: https://codereview.chromium.org/1325063005
    
    Cr-Commit-Position: refs/heads/master@{#347784}

--- content/browser/frame_host/render_widget_host_view_child_frame.cc
+++ content/browser/frame_host/render_widget_host_view_child_frame.cc
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/view_messages.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/common/browser_plugin_guest_mode.h"
+#include "ui/gfx/geometry/size_conversions.h"
+#include "ui/gfx/geometry/size_f.h"
 
 namespace content {
 
@@ -128,8 +130,11 @@ void RenderWidgetHostViewChildFrame::SetBackgroundColor(SkColor color) {
 
 gfx::Size RenderWidgetHostViewChildFrame::GetPhysicalBackingSize() const {
   gfx::Size size;
-  if (frame_connector_)
-    size = frame_connector_->ChildFrameRect().size();
+  if (frame_connector_) {
+    size = gfx::ToCeiledSize(
+        gfx::ScaleSize(frame_connector_->ChildFrameRect().size(),
+                       frame_connector_->device_scale_factor()));
+  }
   return size;
 }
 
