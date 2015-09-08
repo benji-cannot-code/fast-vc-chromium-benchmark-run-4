b0cc4457ce04d79167c8f40577d37c1959ca3008
maybelle <maybelle@chromium.org>
2015-09-08 07:28:25 -0700

    Use country code to get country specific suggestion if default search engine is Google
    BUG=528188
    
    Review URL: https://codereview.chromium.org/1326493004
    
    Cr-Commit-Position: refs/heads/master@{#347699}

--- chrome/browser/android/popular_sites.h
+++ chrome/browser/android/popular_sites.h
@@ -24,6 +24,7 @@ class URLRequestContextGetter;
 }
 
 class FileDownloader;
+class Profile;
 
 // Downloads and provides a list of suggested popular sites, for display on
 // the NTP when there are not enough personalized suggestions. Caches the
@@ -40,7 +41,8 @@ class PopularSites {
 
   using FinishedCallback = base::Callback<void(bool /* success */)>;
 
-  PopularSites(const std::string& filename,
+  PopularSites(Profile* profile,
+               const std::string& filename,
                net::URLRequestContextGetter* request_context,
                const FinishedCallback& callback);
   ~PopularSites();
