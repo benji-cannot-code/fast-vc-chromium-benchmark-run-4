a3a0f27cf04ee68e8f7b1d734fe8f4eedd541411
mkwst <mkwst@chromium.org>
2015-09-08 05:54:18 -0700

    CREDENTIAL: Teach the API about affiliated credentials.
    This patch injects AffiliatedMatchHelper logic into the Credential
    Manager API's dispatcher code so that calls to `get()` will correctly
    populate the list of credentials which we offer to users with those that
    they've saved on affiliated platforms.
    
    BUG=528312
    R=engedy@chromium.org
    
    Review URL: https://codereview.chromium.org/1311323006
    
    Cr-Commit-Position: refs/heads/master@{#347694}

--- components/components_tests.gyp
+++ components/components_tests.gyp
@@ -403,6 +403,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'password_manager/core/browser/import/csv_reader_unittest.cc',
       'password_manager/core/browser/log_router_unittest.cc',
       'password_manager/core/browser/login_database_unittest.cc',
+      'password_manager/core/browser/mock_affiliated_match_helper.cc',
+      'password_manager/core/browser/mock_affiliated_match_helper.h',
       'password_manager/core/browser/password_autofill_manager_unittest.cc',
       'password_manager/core/browser/password_bubble_experiment_unittest.cc',
       'password_manager/core/browser/password_form_manager_unittest.cc',
--- components/password_manager/content/browser/credential_manager_dispatcher.cc
+++ components/password_manager/content/browser/credential_manager_dispatcher.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/content/browser/content_password_manager_driver.h"
 #include "components/password_manager/content/browser/content_password_manager_driver_factory.h"
 #include "components/password_manager/content/common/credential_manager_messages.h"
+#include "components/password_manager/core/browser/affiliated_match_helper.h"
 #include "components/password_manager/core/browser/password_manager_client.h"
 #include "components/password_manager/core/browser/password_store.h"
 #include "components/password_manager/core/common/credential_manager_types.h"
@@ -27,7 +28,7 @@ namespace password_manager {
 CredentialManagerDispatcher::CredentialManagerDispatcher(
     content::WebContents* web_contents,
     PasswordManagerClient* client)
-    : WebContentsObserver(web_contents), client_(client) {
+    : WebContentsObserver(web_contents), client_(client), weak_factory_(this) {
   DCHECK(web_contents);
   auto_signin_enabled_.Init(prefs::kPasswordManagerAutoSignin,
                             client_->GetPrefs());
@@ -131,13 +132,33 @@ void CredentialManagerDispatcher::OnRequestCredential(
     return;
   }
 
+  if (store->affiliated_match_helper()) {
+    store->affiliated_match_helper()->GetAffiliatedAndroidRealms(
+        GetSynthesizedFormForOrigin(),
+        base::Bind(&CredentialManagerDispatcher::ScheduleRequestTask,
+                   weak_factory_.GetWeakPtr(), request_id, zero_click_only,
+                   federations));
+  } else {
+    std::vector<std::string> no_affiliated_realms;
+    ScheduleRequestTask(request_id, zero_click_only, federations,
+                        no_affiliated_realms);
+  }
+}
+
+void CredentialManagerDispatcher::ScheduleRequestTask(
+    int request_id,
+    bool zero_click_only,
+    const std::vector<GURL>& federations,
+    const std::vector<std::string>& android_realms) {
+  DCHECK(GetPasswordStore());
   pending_request_.reset(new CredentialManagerPendingRequestTask(
       this, request_id, zero_click_only,
-      web_contents()->GetLastCommittedURL().GetOrigin(), federations));
+      web_contents()->GetLastCommittedURL().GetOrigin(), federations,
+      android_realms));
 
   // This will result in a callback to
   // PendingRequestTask::OnGetPasswordStoreResults().
-  store->GetAutofillableLogins(pending_request_.get());
+  GetPasswordStore()->GetAutofillableLogins(pending_request_.get());
 }
 
 PasswordStore* CredentialManagerDispatcher::GetPasswordStore() {
@@ -186,6 +207,17 @@ void CredentialManagerDispatcher::SendCredential(int request_id,
 
 PasswordManagerClient* CredentialManagerDispatcher::client() const {
   return client_;
+}
+
+autofill::PasswordForm
+CredentialManagerDispatcher::GetSynthesizedFormForOrigin() const {
+  autofill::PasswordForm synthetic_form;
+  synthetic_form.origin = web_contents()->GetLastCommittedURL().GetOrigin();
+  synthetic_form.signon_realm = synthetic_form.origin.spec();
+  synthetic_form.scheme = autofill::PasswordForm::SCHEME_HTML;
+  synthetic_form.ssl_valid = synthetic_form.origin.SchemeIsCryptographic() &&
+                             !client_->DidLastPageLoadEncounterSSLErrors();
+  return synthetic_form;
 }
 
 void CredentialManagerDispatcher::DoneRequiringUserMediation() {
