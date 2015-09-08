a5f8e8d4111850952b23fed28cccd1fb7f5d95b1
rtenneti <rtenneti@chromium.org>
2015-09-08 12:57:00 -0700

    QUIC - Cancel loading of server config from disk cache if it takes longer than 50ms. When we ran the initial experiments with 50ms as the timeout QUIC performed better than when there is no cancellation. We always cancel if disk read takes more than 0.25 RTT. This change puts an upper bound for the cancellation time.
    R=rch@chromium.org
    
    Review URL: https://codereview.chromium.org/1319453008
    
    Cr-Commit-Position: refs/heads/master@{#347772}

--- net/quic/quic_stream_factory.cc
+++ net/quic/quic_stream_factory.cc
@@ -53,6 +53,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/cpu.h"
 #endif
 
+using std::min;
+
 namespace net {
 
 namespace {
@@ -395,10 +397,15 @@ int QuicStreamFactory::Job::DoLoadServerInfo() {
   // To mitigate the effects of disk cache taking too long to load QUIC server
   // information, set up a timer to cancel WaitForDataReady's callback.
   if (factory_->load_server_info_timeout_srtt_multiplier_ > 0) {
+    const int kMaxLoadServerInfoTimeoutMs = 50;
+    // Wait for DiskCache a maximum of 50ms.
     int64 load_server_info_timeout_ms =
-        (factory_->load_server_info_timeout_srtt_multiplier_ *
-         factory_->GetServerNetworkStatsSmoothedRttInMicroseconds(server_id_)) /
-        1000;
+        min(static_cast<int>(
+                (factory_->load_server_info_timeout_srtt_multiplier_ *
+                 factory_->GetServerNetworkStatsSmoothedRttInMicroseconds(
+                     server_id_)) /
+                1000),
+            kMaxLoadServerInfoTimeoutMs);
     if (load_server_info_timeout_ms > 0) {
       factory_->task_runner_->PostDelayedTask(
           FROM_HERE,
