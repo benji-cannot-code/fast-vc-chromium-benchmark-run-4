b5250e8ce4da1e7791d21ed05e1a5f62895cc4dc
servolk <servolk@chromium.org>
2015-09-09 11:26:06 -0700

    Move VideoCodec/VideoCodecProfile enums into video_codecs.h
    As discussed on HEVC CL https://codereview.chromium.org/816353010/
    we could move VideoCodec/VideoCodecProfile enums into a separate
    header video_codecs.h, since we don't always need the full
    VideoDecoderConfig.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1333573003
    
    Cr-Commit-Position: refs/heads/master@{#347961}

--- media/ffmpeg/ffmpeg_common.h
+++ media/ffmpeg/ffmpeg_common.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/audio_decoder_config.h"
 #include "media/base/channel_layout.h"
 #include "media/base/media_export.h"
-#include "media/base/video_decoder_config.h"
+#include "media/base/video_codecs.h"
 #include "media/base/video_frame.h"
 #include "media/ffmpeg/ffmpeg_deleters.h"
 
--- media/formats/mp4/box_definitions.h
+++ media/formats/mp4/box_definitions.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "media/base/media_export.h"
 #include "media/base/media_log.h"
-#include "media/base/video_decoder_config.h"
+#include "media/base/video_codecs.h"
 #include "media/formats/mp4/aac.h"
 #include "media/formats/mp4/avc.h"
 #include "media/formats/mp4/box_reader.h"
