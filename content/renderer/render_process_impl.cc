22de800aa8ebc81b402b1d4accd00fe3b13f9672
boliu <boliu@chromium.org>
2015-09-09 12:20:23 -0700

    Call SkGraphics::Init for in-process renderer init
    Move the call from RendererMain to RenderProcessImpl constructor so it
    is called for both in-process and multi-process. This is important for
    Android WebView to initialize skia correctly.
    
    BUG=525844
    
    Review URL: https://codereview.chromium.org/1325303005
    
    Cr-Commit-Position: refs/heads/master@{#347974}

--- content/renderer/render_process_impl.cc
+++ content/renderer/render_process_impl.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/public/renderer/content_renderer_client.h"
 #include "third_party/WebKit/public/web/WebFrame.h"
+#include "third_party/skia/include/core/SkGraphics.h"
 #include "v8/include/v8.h"
 
 namespace content {
@@ -43,6 +44,8 @@ RenderProcessImpl::RenderProcessImpl()
     }
   }
 #endif
+
+  SkGraphics::Init();
 
   if (base::SysInfo::IsLowEndDevice()) {
     std::string optimize_flag("--optimize-for-size");
