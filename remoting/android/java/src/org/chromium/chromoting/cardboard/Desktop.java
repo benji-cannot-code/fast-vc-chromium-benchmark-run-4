c2c637bf044fac79394b0923b11a4fe1e7ee6e6d
shichengfeng <shichengfeng@google.com>
2015-09-09 12:38:10 -0700

    Android Chromoting: Swap height and width.
    BUG=516871
    
    Review URL: https://codereview.chromium.org/1324153006
    
    Cr-Commit-Position: refs/heads/master@{#347981}

--- remoting/android/java/src/org/chromium/chromoting/cardboard/Desktop.java
+++ remoting/android/java/src/org/chromium/chromoting/cardboard/Desktop.java
@@ -203,8 +203,8 @@ public class Desktop {
      */
     public Point getFrameSizePixels() {
         synchronized (mVideoFrameLock) {
-            return new Point(mVideoFrame == null ? 0 : mVideoFrame.getHeight(),
-                    mVideoFrame == null ? 0 : mVideoFrame.getWidth());
+            return new Point(mVideoFrame == null ? 0 : mVideoFrame.getWidth(),
+                    mVideoFrame == null ? 0 : mVideoFrame.getHeight());
         }
     }
 
