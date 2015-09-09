b5c3cf7031463d2a9b3d2540fe243770808799ed
mtklein <mtklein@chromium.org>
2015-09-09 08:55:07 -0700

    Remove internal Skia header from skia_memory_dump_provider.cc
    Luckily, it looks like it includes the header but doesn't actually need it.
    
    TBR=reed@google.com
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1333603003
    
    Cr-Commit-Position: refs/heads/master@{#347940}

--- skia/ext/skia_memory_dump_provider.cc
+++ skia/ext/skia_memory_dump_provider.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/process_memory_dump.h"
 #include "skia/ext/SkTraceMemoryDump_chrome.h"
 #include "third_party/skia/include/core/SkGraphics.h"
-#include "third_party/skia/src/core/SkResourceCache.h"
 
 namespace skia {
 
