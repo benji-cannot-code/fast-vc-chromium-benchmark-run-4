780ee585c25a1b8603dfee4939f4adeb37f23672
phajdan.jr@chromium.org <phajdan.jr@chromium.org>
2015-09-09 15:15:35 +0000

    Fix build with system icu
    BUG=529776
    
    Review URL: https://codereview.chromium.org/1330163005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201980 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/fonts/ScriptRunIterator.cpp
+++ third_party/WebKit/Source/platform/fonts/ScriptRunIterator.cpp
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/Logging.h"
 #include "wtf/Threading.h"
 
-#include <ubidi_props.h>
-
 namespace blink {
 
 typedef ScriptData::PairedBracketType PairedBracketType;
