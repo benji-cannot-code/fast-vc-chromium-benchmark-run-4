015b31be1c98ac5f091ceb68006b82ccd28b7851
estark@chromium.org <estark@chromium.org>
2015-09-09 21:11:22 +0000

    Link to the mixed content filter from security panel
    BUG=504537
    
    Review URL: https://codereview.chromium.org/1327593005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202002 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/network/NetworkLogView.js
+++ third_party/WebKit/Source/devtools/front_end/network/NetworkLogView.js
@@ -1036,6 +1036,15 @@ WebInspector.NetworkLogView.prototype = {
     },
 
     /**
+      * @param {!WebInspector.NetworkLogView.FilterType} filterType
+      * @param {string} filterValue
+      */
+    setTextFilterValue: function(filterType, filterValue)
+    {
+        this._textFilterUI.setValue(filterType + ":" + filterValue);
+    },
+
+    /**
      * @param {!WebInspector.Event} event
      */
     _onRequestStarted: function(event)
