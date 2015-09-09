60c283c8a019c71d271d1d51ca2fd9110d0785be
jeremyarcher@google.com <jeremyarcher@google.com>
2015-09-09 04:20:15 +0000

    Add suggestion to use "no-cors" with Fetch fails CORS check.
    See https://code.google.com/p/chromium/issues/detail?id=452745#c17 for details.
    
    BUG=452745
    
    Review URL: https://codereview.chromium.org/1300083002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201949 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/Resource.cpp
+++ third_party/WebKit/Source/core/fetch/Resource.cpp
@@ -329,7 +329,7 @@ bool Resource::passesAccessControlCheck(SecurityOrigin* securityOrigin) const
 
 bool Resource::passesAccessControlCheck(SecurityOrigin* securityOrigin, String& errorDescription) const
 {
-    return blink::passesAccessControlCheck(m_response, resourceRequest().allowStoredCredentials() ? AllowStoredCredentials : DoNotAllowStoredCredentials, securityOrigin, errorDescription);
+    return blink::passesAccessControlCheck(m_response, resourceRequest().allowStoredCredentials() ? AllowStoredCredentials : DoNotAllowStoredCredentials, securityOrigin, errorDescription, resourceRequest().requestContext());
 }
 
 bool Resource::isEligibleForIntegrityCheck(SecurityOrigin* securityOrigin) const
