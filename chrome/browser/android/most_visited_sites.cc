06fb341adcb502c597c9541f66cd932338b30807
treib <treib@chromium.org>
2015-09-09 06:25:59 -0700

    Popular sites on the NTP: check that experiment group StartsWith (rather than IS) "Enabled"
    This will give us the option of creating additional "enabled" groups in the future
    
    BUG=516618
    
    Review URL: https://codereview.chromium.org/1314503008
    
    Cr-Commit-Position: refs/heads/master@{#347920}

--- chrome/browser/android/most_visited_sites.cc
+++ chrome/browser/android/most_visited_sites.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/metrics/sparse_histogram.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
@@ -122,7 +123,8 @@ bool ShouldShowPopularSites() {
     return false;
   if (cmd_line->HasSwitch(switches::kEnableNTPPopularSites))
     return true;
-  return group_name == "Enabled";
+  return base::StartsWith(group_name, "Enabled",
+                          base::CompareCase::INSENSITIVE_ASCII);
 }
 
 std::string GetPopularSitesFilename() {
