3eae5351a3a335a3f71031ba8a17e6fe7f6008bc
haraken@chromium.org <haraken@chromium.org>
2015-09-10 14:48:22 +0000

    Oilpan: An Oilpan idle GC should follow a V8 major GC
    In bug 530064, V8 contexts remain for a long time because Oilpan's GC isn't scheduled
    after V8 major GCs. To avoid that from happening, this CL forces an idle GC after
    a V8 major GC finishes weak processings.
    
    This CL won't affect performance of micro-benchmarks, since those benchmarks don't have
    a chance to schedule idle GCs anyway.
    
    BUG=530064
    
    Review URL: https://codereview.chromium.org/1334683002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202055 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/ThreadState.h
+++ third_party/WebKit/Source/platform/heap/ThreadState.h
@@ -227,6 +227,11 @@ public:
     };
 #endif
 
+    enum V8GCType {
+        V8MinorGC,
+        V8MajorGC,
+    };
+
     // The NoAllocationScope class is used in debug mode to catch unwanted
     // allocations. E.g. allocations during GC.
     class NoAllocationScope final {
@@ -329,7 +334,7 @@ public:
     void scheduleIdleGC();
     void scheduleIdleLazySweep();
     void schedulePreciseGC();
-    void scheduleV8FollowupGCIfNeeded();
+    void scheduleV8FollowupGCIfNeeded(V8GCType);
     void schedulePageNavigationGCIfNeeded(float estimatedRemovalRatio);
     void schedulePageNavigationGC();
     void scheduleGCIfNeeded();
