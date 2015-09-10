6dc560ae2cd7c3eaa4f7f951930d3a3a3274c66f
lgarron@chromium.org <lgarron@chromium.org>
2015-09-10 01:30:13 +0000

    Remove unused securityState code in WebInspector.SecurityModel.
    The Chrome-side security handler now sends a SecurityStyleChanged event upon being enabled, so it is not necessary for SecurityModel to do anything to keep track of security state anymore.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1309953003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202016 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/security/SecurityModel.js
+++ third_party/WebKit/Source/devtools/front_end/security/SecurityModel.js
@@ -15,8 +15,6 @@ WebInspector.SecurityModel = function(target)
     this._securityAgent = target.securityAgent();
     target.registerSecurityDispatcher(this._dispatcher);
     this._securityAgent.enable();
-
-    this._securityState = SecurityAgent.SecurityState.Unknown;
 }
 
 WebInspector.SecurityModel.EventTypes = {
@@ -24,14 +22,6 @@ WebInspector.SecurityModel.EventTypes = {
 }
 
 WebInspector.SecurityModel.prototype = {
-    /**
-     * @return {!SecurityAgent.SecurityState} securityState
-     */
-    securityState: function()
-    {
-        return /** @type {!SecurityAgent.SecurityState} */ (this._securityState);
-    },
-
     __proto__: WebInspector.SDKModel.prototype
 }
 
