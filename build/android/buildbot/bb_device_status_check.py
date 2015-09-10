30d379b1f7698ec95921423e68f8b50d87a46575
jbudorick <jbudorick@chromium.org>
2015-09-10 07:37:46 -0700

    [Android] Fix low-battery blacklisting in device_status_check.
    BUG=
    TBR=rnephew@chromium.org
    
    Review URL: https://codereview.chromium.org/1337463002
    
    Cr-Commit-Position: refs/heads/master@{#348152}

--- build/android/buildbot/bb_device_status_check.py
+++ build/android/buildbot/bb_device_status_check.py
@@ -69,7 +69,7 @@ def _BatteryStatus(device, blacklist):
       if not battery.GetCharging():
         battery.SetCharging(True)
       if blacklist:
-        blacklist.Extend([device.GetDeviceSerial()])
+        blacklist.Extend([device.adb.GetDeviceSerial()])
 
   except device_errors.CommandFailedError:
     logging.exception('Failed to get battery information for %s',
@@ -256,11 +256,11 @@ def RecoverDevices(devices, blacklist):
       except device_errors.CommandFailedError:
         logging.exception('Failure while waiting for %s.', str(device))
         if blacklist:
-          blacklist.Extend([str(device)])
+          blacklist.Extend([device.adb.GetDeviceSerial()])
       except device_errors.CommandTimeoutError:
         logging.exception('Timed out while waiting for %s. ', str(device))
         if blacklist:
-          blacklist.Extend([str(device)])
+          blacklist.Extend([device.adb.GetDeviceSerial()])
 
   device_utils.DeviceUtils.parallel(devices).pMap(blacklisting_recovery)
 
