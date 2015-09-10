1e05f07341feab03acf56beb4a476385cc50763e
kundaji <kundaji@chromium.org>
2015-09-10 10:15:00 -0700

    Remove connection type from PerConnectionDataUsage proto.
    For V1 of Data Saver, we will not breakdown data usage by connection
    type since this data is not always accurate. We plan to explore this
    breakdown for future versions, so keeping the PerConnectionDataUsage
    in the schema.
    
    Tests for RecordDataUsage and GetDataUsage in compression stats.
    
    BUG=482442
    
    Review URL: https://codereview.chromium.org/1319873006
    
    Cr-Commit-Position: refs/heads/master@{#348171}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_test_utils.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_test_utils.cc
@@ -174,12 +174,11 @@ MockDataReductionProxyService::MockDataReductionProxyService(
     : DataReductionProxyService(settings,
                                 prefs,
                                 request_context,
-                                make_scoped_ptr(new DataStore()),
+                                make_scoped_ptr(new TestDataStore()),
                                 task_runner,
                                 task_runner,
                                 task_runner,
-                                base::TimeDelta()) {
-}
+                                base::TimeDelta()) {}
 
 MockDataReductionProxyService::~MockDataReductionProxyService() {
 }
@@ -543,7 +542,7 @@ DataReductionProxyTestContext::CreateDataReductionProxyServiceInternal(
   } else {
     return make_scoped_ptr(new DataReductionProxyService(
         settings, simple_pref_service_.get(), request_context_getter_.get(),
-        make_scoped_ptr(new DataStore()), task_runner_, task_runner_,
+        make_scoped_ptr(new TestDataStore()), task_runner_, task_runner_,
         task_runner_, base::TimeDelta()));
   }
 }
