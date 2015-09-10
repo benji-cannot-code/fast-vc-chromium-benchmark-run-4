7506f1223f7236311d2471d9c2d8febd711ee4d1
nyquist <nyquist@chromium.org>
2015-09-09 22:10:35 -0700

    Reland "Make blimp/common:blimp_common compile for Linux."
    This reverts commit 7d445bc580b1de93263235d16a28731cc54b8916.
    
    In addition to the compilation fix, it fixes the following GN issues:
    - Moves blimp_apk from 'both_gn_and_gyp' group to 'gn_only'.
    - Adds a top-level blimp GN group.
    - Ensures //blimp is built on linux.
    
    The previous CL landed after: https://codereview.chromium.org/1314353005
    which made it break the build.
    
    Original review: https://codereview.chromium.org/1329323002/
    (also same as patch set 1)
    Revert review: https://codereview.chromium.org/1331773002
    
    BUG=None
    TBR=dpranke@chromium.org,kmarshall@chromium.org
    
    Review URL: https://codereview.chromium.org/1330223002
    
    Cr-Commit-Position: refs/heads/master@{#348098}

--- blimp/common/compositor/blimp_layer_tree_settings.cc
+++ blimp/common/compositor/blimp_layer_tree_settings.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/buffer_types.h"
 #include "ui/gl/gl_switches.h"
+#include "ui/native_theme/native_theme_switches.h"
 
 namespace {
 
@@ -137,12 +138,12 @@ void PopulateCommonLayerTreeSettings(cc::LayerTreeSettings* settings) {
         &settings->use_image_texture_targets);
   }
 
-  settings->gather_pixel_refs = false;
+  settings->gather_images = false;
   if (cmd.HasSwitch(switches::kNumRasterThreads)) {
     int num_raster_threads = 0;
     GetSwitchValueAsInt(cmd, switches::kNumRasterThreads, 0,
                         std::numeric_limits<int>::max(), &num_raster_threads);
-    settings->gather_pixel_refs = num_raster_threads > 1;
+    settings->gather_images = num_raster_threads > 1;
   }
 
   if (cmd.HasSwitch(cc::switches::kTopControlsShowThreshold)) {
@@ -254,7 +255,8 @@ void PopulateCommonLayerTreeSettings(cc::LayerTreeSettings* settings) {
   // When pinching in, only show the pinch-viewport overlay scrollbars if the
   // page scale is at least some threshold away from the minimum. i.e. don't
   // show the pinch scrollbars when at minimum scale.
-  settings->scrollbar_show_scale_threshold = 1.05f;
+  // TODO(dtrainor): Update this since https://crrev.com/1267603004 landed.
+  // settings->scrollbar_show_scale_threshold = 1.05f;
 #endif
 
   if (cmd.HasSwitch(switches::kEnableLowResTiling))
