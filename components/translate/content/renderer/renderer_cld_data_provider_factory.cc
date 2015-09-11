bd919a3e5fdbd3808ce0b8faf9361f7ac2c59871
dglazkov <dglazkov@chromium.org>
2015-09-11 07:42:21 -0700

    Revert of Remove RenderViewObserver as RendererCldDataProviderFactory arg. (patchset #1 id:1 of https://codereview.chromium.org/1310303007/ )
    Reason for revert:
    Breaks DataFileRendererCldDataProviderthat's not currently built, but will be in the future.
    
    Original issue's description:
    > Remove RenderViewObserver as RendererCldDataProviderFactory arg.
    >
    > It is not used and will just be in the way of moving TranslateHelper
    > to be a RenderFrameObserver.
    >
    > BUG=521116
    > R=toyoshim@chromium.org
    >
    > Committed: https://crrev.com/0252669f88c5ad3faa51d8376e5bfdbd12b76ceb
    > Cr-Commit-Position: refs/heads/master@{#348330}
    
    TBR=toyoshim@chromium.org,andrewhayden@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=521116
    
    Review URL: https://codereview.chromium.org/1337043002
    
    Cr-Commit-Position: refs/heads/master@{#348396}

--- components/translate/content/renderer/renderer_cld_data_provider_factory.cc
+++ components/translate/content/renderer/renderer_cld_data_provider_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/lazy_instance.h"
 #include "components/translate/content/renderer/renderer_cld_data_provider.h"
+#include "content/public/renderer/render_view_observer.h"
 
 namespace {
 
@@ -24,7 +25,8 @@ base::LazyInstance<translate::RendererCldDataProviderFactory>::Leaky
 namespace translate {
 
 scoped_ptr<RendererCldDataProvider>
-RendererCldDataProviderFactory::CreateRendererCldDataProvider() {
+RendererCldDataProviderFactory::CreateRendererCldDataProvider(
+    content::RenderViewObserver* render_view_observer) {
   return scoped_ptr<RendererCldDataProvider>(
       new RendererCldDataProvider());
 }
