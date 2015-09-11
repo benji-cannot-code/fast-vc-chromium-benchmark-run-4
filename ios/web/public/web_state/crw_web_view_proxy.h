5e6b3a2d30be3283ecce6082239c13454ce96f64
jdonnelly <jdonnelly@chromium.org>
2015-09-10 19:35:30 -0700

    Restore moving to the next Autofill form element on iPads running iOS 9.
    In this case, the keyboard's next element button is located not on the
    keyboard accessory but on the shortcut bar (part of the new
    UITextInputAssistantItem).
    
    BUG=529954
    
    Review URL: https://codereview.chromium.org/1334073002
    
    Cr-Commit-Position: refs/heads/master@{#348317}

--- ios/web/public/web_state/crw_web_view_proxy.h
+++ ios/web/public/web_state/crw_web_view_proxy.h
@@ -49,6 +49,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Returns the currently visible keyboard accessory, or nil.
 - (UIView*)getKeyboardAccessory;
 
+// Returns the currently visible keyboard input assistant item, or nil. Only
+// valid on iOS 9 or above.
+#if defined(__IPHONE_9_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_9_0
+- (UITextInputAssistantItem*)inputAssistantItem;
+#endif
+
 @end
 
 #endif  // IOS_WEB_PUBLIC_WEB_STATE_CRW_WEB_VIEW_PROXY_H_
