a90cb0951ba36237ef1c608bc004b0154a05cc30
fsamuel <fsamuel@chromium.org>
2015-09-11 12:53:33 -0700

    cc: Refactor SurfaceHittest for readability
    BUG=492389
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1327073002
    
    Cr-Commit-Position: refs/heads/master@{#348469}

--- cc/surfaces/surface_hittest.h
+++ cc/surfaces/surface_hittest.h
@@ -13,10 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 class Point;
+class Transform;
 }
 
 namespace cc {
+class DrawQuad;
+class QuadList;
 class RenderPass;
+class RenderPassId;
 class SurfaceManager;
 
 // Performs a hittest in surface quads.
@@ -28,19 +32,28 @@ class CC_SURFACES_EXPORT SurfaceHittest {
   // Hittests against Surface with SurfaceId |surface_id|, return the contained
   // surface that the point is hitting and the |transformed_point| in the
   // surface space.
-  SurfaceId Hittest(SurfaceId surface_id,
-                    const gfx::Point& point,
-                    gfx::Point* transformed_point);
+  SurfaceId GetTargetSurfaceAtPoint(SurfaceId surface_id,
+                                    const gfx::Point& point,
+                                    gfx::Point* transformed_point);
 
  private:
-  bool HittestInternal(SurfaceId surface_id,
-                       const RenderPass* render_pass,
-                       const gfx::Point& point,
-                       SurfaceId* out_surface_id,
-                       gfx::Point* out_transformed_point);
+  bool GetTargetSurfaceAtPointInternal(
+      SurfaceId surface_id,
+      const RenderPassId& render_pass_id,
+      const gfx::Point& point_in_root_target,
+      std::set<const RenderPass*>* referenced_passes,
+      SurfaceId* out_surface_id,
+      gfx::Point* out_transformed_point);
+
+  const RenderPass* GetRenderPassForSurfaceById(
+      SurfaceId surface_id,
+      const RenderPassId& render_pass_id);
+
+  bool PointInQuad(const DrawQuad* quad,
+                   const gfx::Point& point_in_render_pass_space,
+                   gfx::Point* point_in_quad_space);
 
   SurfaceManager* const manager_;
-  std::set<const RenderPass*> referenced_passes_;
 };
 }  // namespace cc
 
