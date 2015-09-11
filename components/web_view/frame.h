555856ab2be439d620359bd4c9ce5d344b9dd740
sky <sky@chromium.org>
2015-09-11 11:05:14 -0700

    Increases security checks of finding target frame
    And adds a test.
    
    BUG=479172,490221
    TEST=none
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/1333333004
    
    Cr-Commit-Position: refs/heads/master@{#348433}

--- components/web_view/frame.h
+++ components/web_view/frame.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web_view {
 
+class FrameTest;
 class FrameTree;
 class FrameTreeClient;
 class FrameUserData;
@@ -107,6 +108,7 @@ class Frame : public mojo::ViewObserver, public FrameTreeServer {
   double GatherProgress(int* frame_count) const;
 
  private:
+  friend class FrameTest;
   friend class FrameTree;
 
   // Identifies whether the FrameTreeClient is from the same app or a different
