783758b718cb363fcfddbb679c1207842b8e7d2f
mdjones <mdjones@chromium.org>
2015-09-11 11:07:50 -0700

    Clean up WebContents on tab close
    This change adds an observer to a tab that reader mode is
    active in to watch for tab close/destruction. This allows
    reader mode to clean up any remaining WebContents it may
    have been using.
    
    BUG=496653
    
    Review URL: https://codereview.chromium.org/1333203002
    
    Cr-Commit-Position: refs/heads/master@{#348434}

--- chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModePanel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModePanel.java
@@ -18,6 +18,7 @@ import org.chromium.chrome.browser.compositor.layouts.ChromeAnimation.Animatable
 import org.chromium.chrome.browser.compositor.layouts.eventfilter.EdgeSwipeEventFilter.ScrollDirection;
 import org.chromium.chrome.browser.dom_distiller.ReaderModeButtonView.ReaderModeButtonViewDelegate;
 import org.chromium.chrome.browser.tab.ChromeTab;
+import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.util.MathUtils;
 import org.chromium.content.browser.ContentView;
@@ -182,6 +183,16 @@ public class ReaderModePanel implements ChromeAnimation.Animatable<ReaderModePan
 
     public ReaderModePanel(ReaderModePanelHost readerModeHost) {
         mReaderModeHost = readerModeHost;
+
+        // Make sure all WebContents are destroyed when a tab is closed: crbug.com/496653
+        mReaderModeHost.getTab().addObserver(new EmptyTabObserver() {
+            @Override
+            public void onDestroyed(Tab tab) {
+                destroyCachedOriginalWebContent();
+                destroyDistilledContentViewCore();
+            }
+        });
+
         mAllowAnimatedButton = mReaderModeHost.allowReaderModeButtonAnimation();
 
         mLayoutWidth = 0.0f;
