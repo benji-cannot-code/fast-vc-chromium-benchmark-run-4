84a6bee08d44819b5d95f03c775b65e911498e62
jbudorick <jbudorick@chromium.org>
2015-09-11 10:23:19 -0700

    [Android] Don't use a device blacklist if one isn't provided. (RELAND 2)
    original: https://codereview.chromium.org/1314823011
    reland 1: https://codereview.chromium.org/1304303006
    
    BUG=517709
    TBR=rnephew@chromium.org,phajdan.jr@chromium.org
    
    Review URL: https://codereview.chromium.org/1331213004
    
    Cr-Commit-Position: refs/heads/master@{#348418}

--- build/android/buildbot/bb_device_status_check.py
+++ build/android/buildbot/bb_device_status_check.py
@@ -56,6 +56,10 @@ def KillAllAdb():
       pass
 
 
+def _IsBlacklisted(serial, blacklist):
+  return blacklist and serial in blacklist.Read()
+
+
 def _BatteryStatus(device, blacklist):
   battery_info = {}
   try:
@@ -139,7 +143,7 @@ def DeviceStatus(devices, blacklist):
     }
 
     if adb_status == 'device':
-      if not serial in blacklist.Read():
+      if not _IsBlacklisted(serial, blacklist):
         try:
           build_product = device.build_product
           build_id = device.build_id
@@ -181,7 +185,7 @@ def DeviceStatus(devices, blacklist):
     elif blacklist:
       blacklist.Extend([serial])
 
-    device_status['blacklisted'] = serial in blacklist.Read()
+    device_status['blacklisted'] = _IsBlacklisted(serial, blacklist)
 
     return device_status
 
@@ -224,7 +228,8 @@ def RecoverDevices(devices, blacklist):
   for d in should_reboot_device:
     logging.debug('  %s', d)
 
-  blacklist.Reset()
+  if blacklist:
+    blacklist.Reset()
 
   if should_restart_adb:
     KillAllAdb()
@@ -237,7 +242,7 @@ def RecoverDevices(devices, blacklist):
         blacklist.Extend([serial])
 
   def blacklisting_recovery(device):
-    if blacklist and device.adb.GetDeviceSerial() in blacklist.Read():
+    if _IsBlacklisted(device.adb.GetDeviceSerial(), blacklist):
       logging.debug('%s is blacklisted, skipping recovery.', str(device))
       return
 
@@ -283,11 +288,9 @@ def main():
 
   run_tests_helper.SetLogLevel(args.verbose)
 
-  if args.blacklist_file:
-    blacklist = device_blacklist.Blacklist(args.blacklist_file)
-  else:
-    # TODO(jbudorick): Remove this once bots pass the blacklist file.
-    blacklist = device_blacklist.Blacklist(device_blacklist.BLACKLIST_JSON)
+  blacklist = (device_blacklist.Blacklist(args.blacklist_file)
+               if args.blacklist_file
+               else None)
 
   last_devices_path = os.path.join(
       args.out_dir, device_list.LAST_DEVICES_FILENAME)
@@ -359,7 +362,7 @@ def main():
 
   live_devices = [status['serial'] for status in statuses
                   if (status['adb_status'] == 'device'
-                      and status['serial'] not in blacklist.Read())]
+                      and not _IsBlacklisted(status['serial'], blacklist))]
 
   # If all devices failed, or if there are no devices, it's an infra error.
   return 0 if live_devices else constants.INFRA_EXIT_CODE
