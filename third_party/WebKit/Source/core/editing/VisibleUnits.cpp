3a177c1e93d448e9aeb9df875cbf68028459db9e
yosin@chromium.org <yosin@chromium.org>
2015-09-11 13:26:45 +0000

    Introduce composed tree version of isEndOfLine()
    This patch introduces composed tree version of |isEndOfLine()| by 
    templatizing original |isEndOfLine()| to work both DOM tree position and
    composed tree position version for introducing composed tree version of
    |VisibleSelection| class.
    
    This patch is a preparation of making selection to handle granularity for web
    component, http://crrev.com/1277863002
    
    BUG=513568
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1334173002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202134 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/VisibleUnits.cpp
+++ third_party/WebKit/Source/core/editing/VisibleUnits.cpp
@@ -1191,9 +1191,20 @@ bool isStartOfLine(const VisiblePositionInComposedTree& p)
     return isStartOfLineAlgorithm<EditingInComposedTreeStrategy>(p);
 }
 
-bool isEndOfLine(const VisiblePosition& p)
+template <typename Strategy>
+bool isEndOfLineAlgorithm(const VisiblePositionTemplate<Strategy>& p)
 {
     return p.isNotNull() && p.deepEquivalent() == endOfLine(p).deepEquivalent();
+}
+
+bool isEndOfLine(const VisiblePosition& p)
+{
+    return isEndOfLineAlgorithm<EditingStrategy>(p);
+}
+
+bool isEndOfLine(const VisiblePositionInComposedTree& p)
+{
+    return isEndOfLineAlgorithm<EditingInComposedTreeStrategy>(p);
 }
 
 bool isLogicalEndOfLine(const VisiblePosition& p)
