42185ec7c9ec001a310b0c829835f3738a74e155
jam <jam@chromium.org>
2015-09-11 11:26:59 -0700

    Revert of Optimize for TouchStart responsiveness (patchset #22 id:410001 of https://codereview.chromium.org/1320633002/ )
    Reason for revert:
    adds flaky test, causes other test to be flaky
    
    BUG=530250
    
    Original issue's description:
    > Optimize for TouchStart responsiveness by modeling what the user is doing.
    > We have come to realize that TouchStart responsiveness is really critical
    > for a good user experience.
    >
    > This patch introduces a (initially very simple) system for modeling
    > what the user is doing and predicting when they are likely to initiate a
    > new gesture (taps are ignored).  We order task priority accordingly.
    >
    > In addition we extend the estimation of task cost to loading tasks
    > which are often a significant source of jank.
    >
    > Load tasks can be very long (easily up to 1000ms). There is
    > no good way to cooperatively schedule such a task but the
    > least worst user experience seems to be if we get them out
    > of the way as fast as possible.  Ideally we'd prioritize them
    > but the order of IPC and LoadingTasks currently needs to
    > be more or less in lockstep.  Instead we deprioritize compositor
    > tasks inside a compositor gesture, which has the effect of
    > prioritizing other work.
    >
    > Expected changes on Perf bots:
    > 1. first_gesture_scroll_update_latency in smoothness.scrolling_tough_ad_cases
    > should go down.  This should be highly user visible.
    > 2. It's possible some compositor metrics such as queueing_durations
    > may regress.  I don't expect this to be user visible.
    >
    > BUG=510398
    > CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    >
    > Committed: https://crrev.com/464b1a5144ddb5135dad845aceb5b1f074221387
    > Cr-Commit-Position: refs/heads/master@{#347937}
    
    TBR=jochen@chromium.org,skyostil@chromium.org,alexclarke@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=510398
    
    Review URL: https://codereview.chromium.org/1337993002
    
    Cr-Commit-Position: refs/heads/master@{#348440}

--- components/scheduler/renderer/task_cost_estimator_unittest.cc
+++ components/scheduler/renderer/task_cost_estimator_unittest.cc
@@ -45,20 +45,7 @@ TEST_F(TaskCostEstimatorTest, BasicEstimation) {
 
   EXPECT_EQ(base::TimeDelta::FromMilliseconds(500),
             estimator.expected_task_duration());
-}
-
-TEST_F(TaskCostEstimatorTest, Clear) {
-  TaskCostEstimatorForTest estimator(&clock_, 1, 100);
-  base::PendingTask task(FROM_HERE, base::Closure());
-
-  estimator.WillProcessTask(task);
-  clock_.Advance(base::TimeDelta::FromMilliseconds(500));
-  estimator.DidProcessTask(task);
-
-  estimator.Clear();
-
-  EXPECT_EQ(base::TimeDelta(), estimator.expected_task_duration());
-}
+};
 
 TEST_F(TaskCostEstimatorTest, NestedRunLoop) {
   TaskCostEstimatorForTest estimator(&clock_, 1, 100);
@@ -74,6 +61,6 @@ TEST_F(TaskCostEstimatorTest, NestedRunLoop) {
 
   EXPECT_EQ(base::TimeDelta::FromMilliseconds(1000),
             estimator.expected_task_duration());
-}
+};
 
 }  // namespace scheduler
