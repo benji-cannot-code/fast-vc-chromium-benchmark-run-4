90aa7e3344092805e7db4e0ea0b75290247b3aa8
nednguyen <nednguyen@google.com>
2015-09-11 11:47:31 -0700

    Disable WebViewTest.TestConfirmDialog on Mac
    BUG=530660
    
    TBR=wjmaclean@chromium.org
    
    Review URL: https://codereview.chromium.org/1341433002
    
    Cr-Commit-Position: refs/heads/master@{#348451}

--- chrome/browser/apps/guest_view/web_view_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_browsertest.cc
@@ -2245,7 +2245,15 @@ IN_PROC_BROWSER_TEST_F(WebViewTest, Dialog_TestAlertDialog) {
   TestHelper("testAlertDialog", "web_view/dialog", NO_TEST_SERVER);
 }
 
-IN_PROC_BROWSER_TEST_F(WebViewTest, TestConfirmDialog) {
+#if defined(OS_MACOSX)
+// http://crbug.com/530660
+#define MAYBE_TestConfirmDialog\
+    DISABLED_TestConfirmDialog
+#else
+#define MAYBE_TestConfirmDialog\
+   TestConfirmDialog
+#endif
+IN_PROC_BROWSER_TEST_F(WebViewTest, MAYBE_TestConfirmDialog) {
   TestHelper("testConfirmDialog", "web_view/dialog", NO_TEST_SERVER);
 }
 
