586bb8696efbd4da8163b4a39a997369ddd321a5
fs@opera.com <fs@opera.com>
2015-09-11 17:27:45 +0000

    Rename LayoutSVGTransformableContainer::m_lastTranslation
    To m_additionalTranslation, since 'last' is ambiguous. The 'additional'
    is from spec wording: "An additional transformation translate(x,y) ...".
    
    Review URL: https://codereview.chromium.org/1337063002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202147 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGTransformableContainer.h
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGTransformableContainer.h
@@ -35,7 +35,7 @@ public:
 
     bool isOfType(LayoutObjectType type) const override { return type == LayoutObjectSVGTransformableContainer || LayoutSVGContainer::isOfType(type); }
     const AffineTransform& localToParentTransform() const override { return m_localTransform; }
-    const FloatSize& additionalTranslation() const { return m_lastTranslation; }
+    const FloatSize& additionalTranslation() const { return m_additionalTranslation; }
 
     void setNeedsTransformUpdate() override { m_needsTransformUpdate = true; }
     bool didTransformToRootUpdate() override { return m_didTransformToRootUpdate; }
@@ -47,7 +47,7 @@ private:
     bool m_needsTransformUpdate : 1;
     bool m_didTransformToRootUpdate : 1;
     AffineTransform m_localTransform;
-    FloatSize m_lastTranslation;
+    FloatSize m_additionalTranslation;
 };
 
 DEFINE_LAYOUT_OBJECT_TYPE_CASTS(LayoutSVGTransformableContainer, isSVGTransformableContainer());
