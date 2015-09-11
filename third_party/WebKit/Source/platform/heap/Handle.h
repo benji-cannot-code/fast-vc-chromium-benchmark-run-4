6055e693de1370dd4faf7a855a6a75457e16ef04
haraken@chromium.org <haraken@chromium.org>
2015-09-11 04:12:49 +0000

    Oilpan: The number of normal persistent handles shouldn't be used for estimating heap size
    Currently we're estimating the live heap size as follows:
    
      (live heap size) = (current heap size) - ((heap size at last GC) / (# of persistent handles at last GC) * (# of persistent handles collected since last GC))
    
    In this equation, "persistent handles" contains V8 wrappers and normal persistent handles.
    This CL drops the normal persistent handles from the "persistent handles".
    
    The reason is that it doesn't make much sense to take into account the number of normal persistent handles
    because normal persistent handles can frequently be allocated and deallocated on stack.
    Those on-stack persistent handles can even be nullptr. It doesn't make sense to assume that
    these persistent handles are holding a substantial amount of memory.
    
    I've sometimes observed that conservative GCs get scheduled a bit too aggressively
    because the memory held by the persistent handles is overestimated. This CL fixes the issue.
    
    BUG=474470
    
    Review URL: https://codereview.chromium.org/1335493003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202104 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/heap/Handle.h
+++ third_party/WebKit/Source/platform/heap/Handle.h
@@ -212,7 +212,6 @@ private:
             ThreadState* state = ThreadStateFor<ThreadingTrait<T>::Affinity>::state();
             ASSERT(state->checkThread());
             m_persistentNode = state->persistentRegion()->allocatePersistentNode(this, traceCallback);
-            state->persistentAllocated();
 #if ENABLE(ASSERT)
             m_state = state;
 #endif
@@ -229,7 +228,6 @@ private:
             // Persistent handle must be created and destructed in the same thread.
             ASSERT(m_state == state);
             state->persistentRegion()->freePersistentNode(m_persistentNode);
-            state->persistentFreed();
         }
     }
 
