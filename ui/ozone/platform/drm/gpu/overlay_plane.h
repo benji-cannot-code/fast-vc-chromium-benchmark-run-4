7c1972e20fbda9e0c278115116188244f653f4c7
danakj <danakj@chromium.org>
2015-09-11 11:50:19 -0700

    Stop implicit conversion from Rect to RectF.
    Fixes a few more cases in:
    - ui/ozone
    - mandoline
    - image_transport_surface_overlay_mac
    
    NOTRY=true
    TBR=ccameron, sky
    BUG=342848
    
    Review URL: https://codereview.chromium.org/1333323004
    
    Cr-Commit-Position: refs/heads/master@{#348454}

--- ui/ozone/platform/drm/gpu/overlay_plane.h
+++ ui/ozone/platform/drm/gpu/overlay_plane.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/overlay_transform.h"
 #include "ui/ozone/ozone_export.h"
 
