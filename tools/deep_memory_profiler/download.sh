b7b6747c78e90efe68b61b1dc2c0f475f888f7ae
kouhei <kouhei@chromium.org>
2015-09-11 07:56:02 -0700

    Remove Deep Memory Profiler commandline tools from tree
    The tool no longer works nor is maintained.
    
    BUG=490464
    
    Review URL: https://codereview.chromium.org/1336993003
    
    Cr-Commit-Position: refs/heads/master@{#348397}

--- tools/deep_memory_profiler/download.sh
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/bin/sh
-# Copyright (c) 2012 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-svn checkout --force \
-    http://src.chromium.org/chrome/trunk/src/tools/deep_memory_profiler \
-    deep_memory_profiler
