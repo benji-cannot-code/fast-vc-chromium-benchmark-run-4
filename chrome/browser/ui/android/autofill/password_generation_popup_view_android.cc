36bb6a565104c29189cb70563694de1d662e7326
danakj <danakj@chromium.org>
2015-09-11 17:44:26 -0700

    android: Include rect_f.h to use RectF.
    The file includes rect.h now but that's the wrong one.
    
    TBR=aurimas@chromium.org
    BUG=342848
    
    Review URL: https://codereview.chromium.org/1338093002
    
    Cr-Commit-Position: refs/heads/master@{#348513}

--- chrome/browser/ui/android/autofill/password_generation_popup_view_android.cc
+++ chrome/browser/ui/android/autofill/password_generation_popup_view_android.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "jni/PasswordGenerationPopupBridge_jni.h"
 #include "ui/android/view_android.h"
 #include "ui/android/window_android.h"
-#include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/range/range.h"
 
 namespace autofill {
