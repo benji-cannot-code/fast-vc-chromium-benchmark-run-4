f6f7dc6f6d61f8d3f7b60ad746d456f7021e6131
thestig <thestig@chromium.org>
2015-09-12 18:42:30 -0700

    Remove an unused field from MediaPlayerHostMsg_Initialize_Params.
    Review URL: https://codereview.chromium.org/1332203004
    
    Cr-Commit-Position: refs/heads/master@{#348533}

--- content/common/media/media_player_messages_android.h
+++ content/common/media/media_player_messages_android.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Multiply-included message file, hence no include guard.
 
 #include "base/basictypes.h"
-#include "base/memory/shared_memory.h"
 #include "base/time/time.h"
 #include "content/common/content_export.h"
 #include "content/common/media/media_player_messages_enums_android.h"
@@ -71,7 +70,6 @@ IPC_ENUM_TRAITS_MAX_VALUE(MediaPlayerHostMsg_Initialize_Type,
 // Parameters to describe a media player
 IPC_STRUCT_BEGIN(MediaPlayerHostMsg_Initialize_Params)
   IPC_STRUCT_MEMBER(MediaPlayerHostMsg_Initialize_Type, type)
-  IPC_STRUCT_MEMBER(base::SharedMemoryHandle, metafile_data_handle)
   IPC_STRUCT_MEMBER(int, player_id)
   IPC_STRUCT_MEMBER(int, demuxer_client_id)
   IPC_STRUCT_MEMBER(GURL, url)
