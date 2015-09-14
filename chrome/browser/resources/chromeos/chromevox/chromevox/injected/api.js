6fed7467f6a9720f4b50a0a489d99bbdd14b5ce4
dmazzoni <dmazzoni@chromium.org>
2015-09-13 21:48:00 -0700

    Rename some earcons based on actual usage
    BUG=491243
    
    Review URL: https://codereview.chromium.org/1311463007
    
    Cr-Commit-Position: refs/heads/master@{#348559}

--- chrome/browser/resources/chromeos/chromevox/chromevox/injected/api.js
+++ chrome/browser/resources/chromeos/chromevox/chromevox/injected/api.js
@@ -276,13 +276,10 @@ if (typeof(goog) != 'undefined' && goog.require) {
     * Valid names are:
     *   ALERT_MODAL
     *   ALERT_NONMODAL
-    *   BULLET
-    *   BUSY_PROGRESS_LOOP
     *   BUTTON
     *   CHECK_OFF
     *   CHECK_ON
     *   EDITABLE_TEXT
-    *   FONT_CHANGE
     *   INVALID_KEYPRESS
     *   LINK
     *   LISTBOX
@@ -290,8 +287,9 @@ if (typeof(goog) != 'undefined' && goog.require) {
     *   OBJECT_CLOSE
     *   OBJECT_OPEN
     *   OBJECT_SELECT
-    *   PARAGRAPH_BREAK
-    *   TASK_SUCCESS
+    *   PAGE_START_LOADING
+    *   RECOVER_FOCUS
+    *   SKIP
     *   WRAP
     *   WRAP_EDGE
     * This list may expand over time.
