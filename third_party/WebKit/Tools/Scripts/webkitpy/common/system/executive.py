52cee4c1467f4efc6e36fb7a35663944044e1e6a
joelo@chromium.org <joelo@chromium.org>
2015-09-14 19:05:03 +0000

    Fix failing running_pids test on Win XP.
    BUG=531316
    TBR=jsbell@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1341073002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/system/executive.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/system/executive.py
@@ -266,7 +266,7 @@ class Executive(object):
                 pid = line[1]
                 if process_name_filter(process_name):
                     running_pids.append(int(pid))
-            except ValueError, e:
+            except (ValueError, IndexError), e:
                 pass
 
         return sorted(running_pids)
