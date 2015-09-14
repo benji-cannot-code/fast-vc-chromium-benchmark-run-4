f3e77ddffaa2eab55e0aa9fff6e789717bbd06d3
jin.a.yang@intel.com <jin.a.yang@intel.com>
2015-09-14 05:00:23 +0000

    Add GL_CHROMIUM_screen_space_antialiasing to support alternative AA
    MSAA is very expensive on some platforms. We introduce extention
    GL_CHROMIUM_screen_space_antialiasing to support other antialiasing 
    approach. It will perform antialiasing to the color attachments of
    the currently bound draw framebuffer. As an example, we implement it
    using GL_INTEL_framebuffer_CMAA which results in similar quality of
    MSAA at better performance.
    
    
    The chromium side change is in below link:
    https://codereview.chromium.org/1298523003/
    
    BUG=524285
    
    Review URL: https://codereview.chromium.org/1301923002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202174 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/platform/graphics/gpu/DrawingBuffer.cpp
+++ third_party/WebKit/Source/platform/graphics/gpu/DrawingBuffer.cpp
@@ -155,7 +155,7 @@ DrawingBuffer::DrawingBuffer(PassOwnPtr<WebGraphicsContext3D> context,
     , m_contentsChanged(true)
     , m_contentsChangeCommitted(false)
     , m_bufferClearNeeded(false)
-    , m_multisampleMode(None)
+    , m_antiAliasingMode(None)
     , m_internalColorFormat(0)
     , m_colorFormat(0)
     , m_internalRenderbufferFormat(0)
@@ -250,7 +250,7 @@ bool DrawingBuffer::prepareMailbox(WebExternalTextureMailbox* outMailbox, WebExt
         return false;
 
     // Resolve the multisampled buffer into m_colorBuffer texture.
-    if (m_multisampleMode != None)
+    if (m_antiAliasingMode != None)
         commit();
 
     if (bitmap) {
@@ -289,7 +289,7 @@ bool DrawingBuffer::prepareMailbox(WebExternalTextureMailbox* outMailbox, WebExt
         // If this stops being true at some point, we should track the current framebuffer binding in the DrawingBuffer and restore
         // it after attaching the new back buffer here.
         m_context->bindFramebuffer(GL_FRAMEBUFFER, m_fbo);
-        if (m_multisampleMode == ImplicitResolve)
+        if (m_antiAliasingMode == MSAAImplicitResolve)
             m_context->framebufferTexture2DMultisampleEXT(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colorBuffer.textureId, 0, m_sampleCount);
         else
             m_context->framebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colorBuffer.textureId, 0);
@@ -431,12 +431,15 @@ bool DrawingBuffer::initialize(const IntSize& size)
     m_context->getIntegerv(GL_MAX_TEXTURE_SIZE, &m_maxTextureSize);
 
     int maxSampleCount = 0;
-    m_multisampleMode = None;
+    m_antiAliasingMode = None;
     if (m_requestedAttributes.antialias && m_multisampleExtensionSupported) {
         m_context->getIntegerv(GL_MAX_SAMPLES_ANGLE, &maxSampleCount);
-        m_multisampleMode = ExplicitResolve;
-        if (m_extensionsUtil->supportsExtension("GL_EXT_multisampled_render_to_texture"))
-            m_multisampleMode = ImplicitResolve;
+        m_antiAliasingMode = MSAAExplicitResolve;
+        if (m_extensionsUtil->supportsExtension("GL_EXT_multisampled_render_to_texture")) {
+            m_antiAliasingMode = MSAAImplicitResolve;
+        } else if (m_extensionsUtil->supportsExtension("GL_CHROMIUM_screen_space_antialiasing")) {
+            m_antiAliasingMode = ScreenSpaceAntialiasing;
+        }
     }
     m_sampleCount = std::min(4, maxSampleCount);
 
@@ -444,7 +447,7 @@ bool DrawingBuffer::initialize(const IntSize& size)
 
     m_context->bindFramebuffer(GL_FRAMEBUFFER, m_fbo);
     m_colorBuffer.textureId = createColorTexture();
-    if (m_multisampleMode == ImplicitResolve)
+    if (m_antiAliasingMode == MSAAImplicitResolve)
         m_context->framebufferTexture2DMultisampleEXT(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colorBuffer.textureId, 0, m_sampleCount);
     else
         m_context->framebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colorBuffer.textureId, 0);
@@ -483,7 +486,7 @@ bool DrawingBuffer::copyToPlatformTexture(WebGraphicsContext3D* context, Platfor
     GLenum destType, GLint level, bool premultiplyAlpha, bool flipY, SourceDrawingBuffer sourceBuffer)
 {
     if (m_contentsChanged) {
-        if (m_multisampleMode != None) {
+        if (m_antiAliasingMode != None) {
             commit();
             restoreFramebufferBindings();
         }
@@ -621,7 +624,7 @@ unsigned DrawingBuffer::createColorTexture()
 void DrawingBuffer::createSecondaryBuffers()
 {
     // create a multisample FBO
-    if (m_multisampleMode == ExplicitResolve) {
+    if (m_antiAliasingMode == MSAAExplicitResolve) {
         m_multisampleFBO = m_context->createFramebuffer();
         m_context->bindFramebuffer(GL_FRAMEBUFFER, m_multisampleFBO);
         m_multisampleColorBuffer = m_context->createRenderbuffer();
@@ -637,14 +640,14 @@ bool DrawingBuffer::resizeFramebuffer(const IntSize& size)
 
     allocateTextureMemory(&m_colorBuffer, size);
 
-    if (m_multisampleMode == ImplicitResolve)
+    if (m_antiAliasingMode == MSAAImplicitResolve)
         m_context->framebufferTexture2DMultisampleEXT(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colorBuffer.textureId, 0, m_sampleCount);
     else
         m_context->framebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colorBuffer.textureId, 0);
 
     m_context->bindTexture(GL_TEXTURE_2D, 0);
 
-    if (m_multisampleMode != ExplicitResolve)
+    if (m_antiAliasingMode != MSAAExplicitResolve)
         resizeDepthStencil(size);
     if (m_context->checkFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
         return false;
@@ -654,7 +657,7 @@ bool DrawingBuffer::resizeFramebuffer(const IntSize& size)
 
 bool DrawingBuffer::resizeMultisampleFramebuffer(const IntSize& size)
 {
-    if (m_multisampleMode == ExplicitResolve) {
+    if (m_antiAliasingMode == MSAAExplicitResolve) {
         m_context->bindFramebuffer(GL_FRAMEBUFFER, m_multisampleFBO);
 
         m_context->bindRenderbuffer(GL_RENDERBUFFER, m_multisampleColorBuffer);
@@ -681,9 +684,9 @@ void DrawingBuffer::resizeDepthStencil(const IntSize& size)
         if (!m_depthStencilBuffer)
             m_depthStencilBuffer = m_context->createRenderbuffer();
         m_context->bindRenderbuffer(GL_RENDERBUFFER, m_depthStencilBuffer);
-        if (m_multisampleMode == ImplicitResolve)
+        if (m_antiAliasingMode == MSAAImplicitResolve)
             m_context->renderbufferStorageMultisampleEXT(GL_RENDERBUFFER, m_sampleCount, GL_DEPTH24_STENCIL8_OES, size.width(), size.height());
-        else if (m_multisampleMode == ExplicitResolve)
+        else if (m_antiAliasingMode == MSAAExplicitResolve)
             m_context->renderbufferStorageMultisampleCHROMIUM(GL_RENDERBUFFER, m_sampleCount, GL_DEPTH24_STENCIL8_OES, size.width(), size.height());
         else
             m_context->renderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8_OES, size.width(), size.height());
@@ -694,9 +697,9 @@ void DrawingBuffer::resizeDepthStencil(const IntSize& size)
             if (!m_depthBuffer)
                 m_depthBuffer = m_context->createRenderbuffer();
             m_context->bindRenderbuffer(GL_RENDERBUFFER, m_depthBuffer);
-            if (m_multisampleMode == ImplicitResolve)
+            if (m_antiAliasingMode == MSAAImplicitResolve)
                 m_context->renderbufferStorageMultisampleEXT(GL_RENDERBUFFER, m_sampleCount, GL_DEPTH_COMPONENT16, size.width(), size.height());
-            else if (m_multisampleMode == ExplicitResolve)
+            else if (m_antiAliasingMode == MSAAExplicitResolve)
                 m_context->renderbufferStorageMultisampleCHROMIUM(GL_RENDERBUFFER, m_sampleCount, GL_DEPTH_COMPONENT16, size.width(), size.height());
             else
                 m_context->renderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, size.width(), size.height());
@@ -706,9 +709,9 @@ void DrawingBuffer::resizeDepthStencil(const IntSize& size)
             if (!m_stencilBuffer)
                 m_stencilBuffer = m_context->createRenderbuffer();
             m_context->bindRenderbuffer(GL_RENDERBUFFER, m_stencilBuffer);
-            if (m_multisampleMode == ImplicitResolve)
+            if (m_antiAliasingMode == MSAAImplicitResolve)
                 m_context->renderbufferStorageMultisampleEXT(GL_RENDERBUFFER, m_sampleCount, GL_STENCIL_INDEX8, size.width(), size.height());
-            else if (m_multisampleMode == ExplicitResolve)
+            else if (m_antiAliasingMode == MSAAExplicitResolve)
                 m_context->renderbufferStorageMultisampleCHROMIUM(GL_RENDERBUFFER, m_sampleCount, GL_STENCIL_INDEX8, size.width(), size.height());
             else
                 m_context->renderbufferStorage(GL_RENDERBUFFER, GL_STENCIL_INDEX8, size.width(), size.height());
@@ -816,6 +819,9 @@ void DrawingBuffer::commit()
     }
 
     m_context->bindFramebuffer(GL_FRAMEBUFFER, m_fbo);
+    if (m_antiAliasingMode == ScreenSpaceAntialiasing) {
+        m_context->applyScreenSpaceAntialiasingCHROMIUM();
+    }
     m_contentsChangeCommitted = true;
 }
 
@@ -845,7 +851,7 @@ void DrawingBuffer::restoreFramebufferBindings()
 
 bool DrawingBuffer::multisample() const
 {
-    return m_multisampleMode != None;
+    return m_antiAliasingMode != None;
 }
 
 void DrawingBuffer::bind(GLenum target)
--- third_party/WebKit/Source/platform/graphics/gpu/DrawingBuffer.h
+++ third_party/WebKit/Source/platform/graphics/gpu/DrawingBuffer.h
@@ -181,7 +181,7 @@ public:
     bool paintRenderingResultsToImageData(int&, int&, SourceDrawingBuffer, WTF::ArrayBufferContents&);
 
     int sampleCount() const { return m_sampleCount; }
-    bool explicitResolveOfMultisampleData() const { return m_multisampleMode == ExplicitResolve; }
+    bool explicitResolveOfMultisampleData() const { return m_antiAliasingMode == MSAAExplicitResolve; }
 
     // Bind to m_drawFramebufferBinding or m_readFramebufferBinding if it's not 0.
     // Otherwise, bind to the default FBO.
@@ -282,13 +282,14 @@ private:
     bool m_contentsChangeCommitted;
     bool m_bufferClearNeeded;
 
-    enum MultisampleMode {
+    enum AntialiasingMode {
         None,
-        ImplicitResolve,
-        ExplicitResolve,
+        MSAAImplicitResolve,
+        MSAAExplicitResolve,
+        ScreenSpaceAntialiasing,
     };
 
-    MultisampleMode m_multisampleMode;
+    AntialiasingMode m_antiAliasingMode;
 
     WebGraphicsContext3D::Attributes m_actualAttributes;
     unsigned m_internalColorFormat;
