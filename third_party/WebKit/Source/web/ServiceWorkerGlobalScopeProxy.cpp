f6d7fe2a2ccb2eb4536fc458138b7239e0481549
nhiroki@chromium.org <nhiroki@chromium.org>
2015-09-14 07:24:12 +0000

    ServiceWorker: Pass WebPassOwnPtr instead of rawptr (1)
    To clarify ownership transfer, these CLs make WebServiceWorkerContextProxy
    receive WebPassOwnPtr instead of rawptr.
    
    (1) Blink: THIS PATCH
    (2) Chromium: https://codereview.chromium.org/1344473002/
    (3) Blink: https://codereview.chromium.org/1344483002/
    
    BUG=531271
    
    Review URL: https://codereview.chromium.org/1339933002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202187 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/ServiceWorkerGlobalScopeProxy.cpp
+++ third_party/WebKit/Source/web/ServiceWorkerGlobalScopeProxy.cpp
@@ -82,7 +82,7 @@ ServiceWorkerGlobalScopeProxy::~ServiceWorkerGlobalScopeProxy()
 {
 }
 
-void ServiceWorkerGlobalScopeProxy::setRegistration(WebServiceWorkerRegistration::Handle* handle)
+void ServiceWorkerGlobalScopeProxy::setRegistration(WebPassOwnPtr<WebServiceWorkerRegistration::Handle> handle)
 {
     ASSERT(m_workerGlobalScope);
     m_workerGlobalScope->setRegistration(handle);
