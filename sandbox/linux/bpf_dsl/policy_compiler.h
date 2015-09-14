7e0669afca2588e98dceecf8246b99f2a528b76c
mdempsky <mdempsky@chromium.org>
2015-09-14 11:29:45 -0700

    sandbox/linux: remove some gratuitous pointers
    Instead of unnecessary pointers (and scoped_ptrs), just take advantage
    of return-value optimization and C++11 move semantics.
    
    BUG=414363
    
    Review URL: https://codereview.chromium.org/1310803002
    
    Cr-Commit-Position: refs/heads/master@{#348661}

--- sandbox/linux/bpf_dsl/policy_compiler.h
+++ sandbox/linux/bpf_dsl/policy_compiler.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
-#include "base/memory/scoped_ptr.h"
 #include "sandbox/linux/bpf_dsl/bpf_dsl_forward.h"
 #include "sandbox/linux/bpf_dsl/codegen.h"
 #include "sandbox/linux/bpf_dsl/trap_registry.h"
@@ -33,7 +32,7 @@ class SANDBOX_EXPORT PolicyCompiler {
 
   // Compile registers any trap handlers needed by the policy and
   // compiles the policy to a BPF program, which it returns.
-  scoped_ptr<CodeGen::Program> Compile();
+  CodeGen::Program Compile();
 
   // DangerousSetEscapePC sets the "escape PC" that is allowed to issue any
   // system calls, regardless of policy.
