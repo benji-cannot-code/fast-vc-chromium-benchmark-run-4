061ba52ee07f1a62e430b888cc937f54c07071c4
eugenebut <eugenebut@chromium.org>
2015-09-14 13:14:15 -0700

    Reland: WKWebView: Added cert verification API to web controller.
    This code is just a skeleton for verification and verification method
    is not used for making security decisions or presenting security UI.
    
    The decision to use CertVerifier instead of iOS cert verification API
    has not been made yet. But using CertVerifier is easier for
    -[WKWebView certificateChain] verification, so this CL uses
    CertVerifier.
    
    Original URL: https://codereview.chromium.org/1230033005/
    
    BUG=462427,462425
    
    Review URL: https://codereview.chromium.org/1316063008
    
    Cr-Commit-Position: refs/heads/master@{#348699}

--- ios/web/ios_web.gyp
+++ ios/web/ios_web.gyp
@@ -131,6 +131,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'net/cookie_notification_bridge.mm',
         'net/crw_cert_policy_cache.h',
         'net/crw_cert_policy_cache.mm',
+        'net/crw_cert_verification_controller.h',
+        'net/crw_cert_verification_controller.mm',
         'net/crw_request_tracker_delegate.h',
         'net/crw_url_verifying_protocol_handler.h',
         'net/crw_url_verifying_protocol_handler.mm',
--- ios/web/web_state/ui/crw_wk_web_view_web_controller.mm
+++ ios/web/web_state/ui/crw_wk_web_view_web_controller.mm
@@ -20,6 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/navigation/crw_session_entry.h"
 #include "ios/web/navigation/navigation_item_impl.h"
 #include "ios/web/navigation/web_load_params.h"
+#import "ios/web/net/crw_cert_verification_controller.h"
+#include "ios/web/public/cert_store.h"
+#include "ios/web/public/navigation_item.h"
+#include "ios/web/public/ssl_status.h"
 #include "ios/web/public/web_client.h"
 #import "ios/web/public/web_state/js/crw_js_injection_manager.h"
 #import "ios/web/public/web_state/ui/crw_native_content_provider.h"
@@ -37,18 +41,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/web_state/ui/wk_web_view_configuration_provider.h"
 #import "ios/web/web_state/web_state_impl.h"
 #import "ios/web/web_state/web_view_internal_creation_util.h"
-#import "ios/web/webui/crw_web_ui_manager.h"
-#import "net/base/mac/url_conversions.h"
-#include "url/url_constants.h"
-
-#if !defined(ENABLE_CHROME_NET_STACK_FOR_WKWEBVIEW)
-#include "ios/web/public/cert_store.h"
-#include "ios/web/public/navigation_item.h"
-#include "ios/web/public/ssl_status.h"
 #import "ios/web/web_state/wk_web_view_security_util.h"
+#import "ios/web/webui/crw_web_ui_manager.h"
 #include "net/cert/x509_certificate.h"
+#import "net/base/mac/url_conversions.h"
 #include "net/ssl/ssl_info.h"
-#endif
+#include "url/url_constants.h"
 
 namespace {
 // Extracts Referer value from WKNavigationAction request header.
@@ -136,6 +134,12 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
 
   // CRWWebUIManager object for loading WebUI pages.
   base::scoped_nsobject<CRWWebUIManager> _webUIManager;
+
+  // Controller used for certs verification to help with blocking requests with
+  // bad SSL cert, presenting SSL interstitials and determining SSL status for
+  // Navigation Items.
+  base::scoped_nsobject<CRWCertVerificationController>
+      _certVerificationController;
 
   // Whether the pending navigation has been directly cancelled in
   // |decidePolicyForNavigationAction| or |decidePolicyForNavigationResponse|.
@@ -294,7 +298,14 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
 #pragma mark CRWWebController public methods
 
 - (instancetype)initWithWebState:(scoped_ptr<web::WebStateImpl>)webState {
-  return [super initWithWebState:webState.Pass()];
+  DCHECK(webState);
+  web::BrowserState* browserState = webState->GetBrowserState();
+  self = [super initWithWebState:webState.Pass()];
+  if (self) {
+    _certVerificationController.reset([[CRWCertVerificationController alloc]
+        initWithBrowserState:browserState]);
+  }
+  return self;
 }
 
 - (BOOL)keyboardDisplayRequiresUserAction {
@@ -334,6 +345,11 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
   // TODO(eugenebut): implement dialogs/windows suppression using
   // WKNavigationDelegate methods where possible.
   [super setPageDialogOpenPolicy:policy];
+}
+
+- (void)close {
+  [_certVerificationController shutDown];
+  [super close];
 }
 
 #pragma mark -
@@ -1345,8 +1361,22 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
                     completionHandler:
         (void (^)(NSURLSessionAuthChallengeDisposition disposition,
                   NSURLCredential *credential))completionHandler {
-  NOTIMPLEMENTED();
-  completionHandler(NSURLSessionAuthChallengeRejectProtectionSpace, nil);
+  if (![challenge.protectionSpace.authenticationMethod
+          isEqual:NSURLAuthenticationMethodServerTrust]) {
+    completionHandler(NSURLSessionAuthChallengePerformDefaultHandling, nil);
+    return;
+  }
+
+  SecTrustRef trust = challenge.protectionSpace.serverTrust;
+  scoped_refptr<net::X509Certificate> cert = web::CreateCertFromTrust(trust);
+  [_certVerificationController
+      decidePolicyForCert:cert
+                     host:challenge.protectionSpace.host
+        completionHandler:^(web::CertAcceptPolicy policy,
+                            net::CertStatus status) {
+          completionHandler(NSURLSessionAuthChallengeRejectProtectionSpace,
+                            nil);
+        }];
 }
 
 - (void)webViewWebContentProcessDidTerminate:(WKWebView*)webView {
