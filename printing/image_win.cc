44251f37bfc7db8c1b225314282a1c1afee75ef4
thestig <thestig@chromium.org>
2015-09-14 11:16:33 -0700

    Cleanup code in printing/
    - Use stdint.h types.
    - Fix lint errors.
    
    Review URL: https://codereview.chromium.org/1343593002
    
    Cr-Commit-Position: refs/heads/master@{#348660}

--- printing/image_win.cc
+++ printing/image_win.cc
@@ -24,7 +24,7 @@ namespace {
 // destructor.
 class DisableFontSmoothing {
  public:
-  explicit DisableFontSmoothing() : enable_again_(false) {
+  DisableFontSmoothing() : enable_again_(false) {
     BOOL enabled;
     if (SystemParametersInfo(SPI_GETFONTSMOOTHING, 0, &enabled, 0) &&
         enabled) {
@@ -78,7 +78,7 @@ bool Image::LoadMetafile(const Metafile& metafile) {
 
   bool success = metafile.Playback(hdc.Get(), NULL);
 
-  row_length_ = size_.width() * sizeof(uint32);
+  row_length_ = size_.width() * sizeof(uint32_t);
   size_t bytes = row_length_ * size_.height();
   DCHECK(bytes);
 
