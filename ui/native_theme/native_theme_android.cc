7493ec9804c181afce0833a0467c49898d9700b5
mohsen <mohsen@chromium.org>
2015-09-13 21:33:46 -0700

    Make checkboxes/radio buttons larger on Android
    In order to make interacting with checkboxes and radio buttons easier
    with touch, their default size is changed from 13px by 13px (in which
    1px in each dimension is for the drop shadow) to 16px by 16px (in which
    2px in each dimension is for a padding around them, including the dorp
    shadow).
    
    BUG=459055
    
    Review URL: https://codereview.chromium.org/1341453003
    
    Cr-Commit-Position: refs/heads/master@{#348554}

--- ui/native_theme/native_theme_android.cc
+++ ui/native_theme/native_theme_android.cc
@@ -7,8 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/logging.h"
+#include "ui/gfx/geometry/size.h"
 
 namespace ui {
+
+namespace {
+// These are the default dimensions of radio buttons and checkboxes on Android.
+const int kCheckboxAndRadioWidth = 16;
+const int kCheckboxAndRadioHeight = 16;
+}
 
 #if !defined(USE_AURA)
 // static
@@ -23,9 +30,23 @@ NativeThemeAndroid* NativeThemeAndroid::instance() {
   return &s_native_theme;
 }
 
+gfx::Size NativeThemeAndroid::GetPartSize(Part part,
+                                          State state,
+                                          const ExtraParams& extra) const {
+  if (part == kCheckbox || part == kRadio)
+    return gfx::Size(kCheckboxAndRadioWidth, kCheckboxAndRadioHeight);
+  return NativeThemeBase::GetPartSize(part, state, extra);
+}
+
 SkColor NativeThemeAndroid::GetSystemColor(ColorId color_id) const {
   NOTIMPLEMENTED();
   return SK_ColorBLACK;
+}
+
+void NativeThemeAndroid::AdjustCheckboxRadioRectForPadding(SkRect* rect) const {
+  // Take 1px for padding around the checkbox/radio button.
+  rect->iset(rect->x() + 1, rect->y() + 1, rect->right() - 1,
+             rect->bottom() - 1);
 }
 
 NativeThemeAndroid::NativeThemeAndroid() {
