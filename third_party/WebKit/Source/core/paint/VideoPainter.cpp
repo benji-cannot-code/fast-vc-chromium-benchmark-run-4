1790bd8246cb912a588b3c74a5e201e2ab152a43
chrishtr@chromium.org <chrishtr@chromium.org>
2015-09-14 16:34:19 +0000

    Fix pixel snapping of clips for images, table cells, and videos.
    Also cleans up some obsolete comments.
    
    BUG=530647
    
    Review URL: https://codereview.chromium.org/1336083003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202206 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/VideoPainter.cpp
+++ third_party/WebKit/Source/core/paint/VideoPainter.cpp
@@ -10,11 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/FrameView.h"
 #include "core/html/HTMLVideoElement.h"
 #include "core/layout/LayoutVideo.h"
-#include "core/paint/FloatClipRecorder.h"
 #include "core/paint/ImagePainter.h"
 #include "core/paint/LayoutObjectDrawingRecorder.h"
 #include "core/paint/PaintInfo.h"
 #include "platform/geometry/LayoutPoint.h"
+#include "platform/graphics/paint/ClipRecorder.h"
 
 namespace blink {
 
@@ -34,10 +34,9 @@ void VideoPainter::paintReplaced(const PaintInfo& paintInfo, const LayoutPoint& 
     LayoutRect contentRect = m_layoutVideo.contentBoxRect();
     contentRect.moveBy(paintOffset);
 
-    Optional<FloatClipRecorder> clipRecorder;
+    Optional<ClipRecorder> clipRecorder;
     if (!contentRect.contains(rect)) {
-        // TODO(chrishtr): this should be pixel-snapped.
-        clipRecorder.emplace(*context, m_layoutVideo, paintInfo.phase, FloatRect(contentRect));
+        clipRecorder.emplace(*context, m_layoutVideo, paintInfo.displayItemTypeForClipping(), contentRect);
     }
 
     if (LayoutObjectDrawingRecorder::useCachedDrawingIfPossible(*context, m_layoutVideo, paintInfo.phase, paintOffset))
