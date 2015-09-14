cb9754f19e0fb7f4f7cfe7cfce09b69f556646a8
dstockwell@chromium.org <dstockwell@chromium.org>
2015-09-14 07:31:56 +0000

    Web Animations: Don't request recalc in no-op timing update
    BUG=418137
    
    Review URL: https://codereview.chromium.org/1340683003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202188 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/KeyframeEffectModel.h
+++ third_party/WebKit/Source/core/animation/KeyframeEffectModel.h
@@ -86,7 +86,7 @@ public:
     }
 
     // EffectModel implementation.
-    void sample(int iteration, double fraction, double iterationDuration, OwnPtr<Vector<RefPtr<Interpolation>>>&) const override;
+    bool sample(int iteration, double fraction, double iterationDuration, OwnPtr<Vector<RefPtr<Interpolation>>>&) const override;
 
     bool isKeyframeEffectModel() const override { return true; }
 
@@ -119,7 +119,11 @@ public:
 
 protected:
     KeyframeEffectModelBase(PassRefPtr<TimingFunction> neutralKeyframeEasing)
-        : m_neutralKeyframeEasing(neutralKeyframeEasing)
+        : m_lastIteration(0)
+        , m_lastFraction(std::numeric_limits<double>::quiet_NaN())
+        , m_lastIterationDuration(0)
+        , m_neutralKeyframeEasing(neutralKeyframeEasing)
+        , m_hasSyntheticKeyframes(false)
     {
     }
 
@@ -137,6 +141,9 @@ protected:
     using KeyframeGroupMap = HashMap<PropertyHandle, OwnPtr<PropertySpecificKeyframeGroup>>;
     mutable OwnPtr<KeyframeGroupMap> m_keyframeGroups;
     mutable RefPtr<InterpolationEffect> m_interpolationEffect;
+    mutable int m_lastIteration;
+    mutable double m_lastFraction;
+    mutable double m_lastIterationDuration;
     RefPtr<TimingFunction> m_neutralKeyframeEasing;
 
     mutable bool m_hasSyntheticKeyframes;
