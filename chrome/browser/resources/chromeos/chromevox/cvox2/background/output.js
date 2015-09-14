cd9c12459687e47d718af432edf6736b40605fa8
dmazzoni <dmazzoni@chromium.org>
2015-09-14 12:05:41 -0700

    Initial commit of new ChromeVox earcon engine.
    A follow-up changelist will hook up Output to this new engine.
    
    BUG=491243
    
    Review URL: https://codereview.chromium.org/1273363004
    
    Cr-Commit-Position: refs/heads/master@{#348677}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/output.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/output.js
@@ -11,6 +11,7 @@ goog.provide('Output');
 goog.provide('Output.EventType');
 
 goog.require('AutomationUtil.Dir');
+goog.require('EarconEngine');
 goog.require('cursors.Cursor');
 goog.require('cursors.Range');
 goog.require('cursors.Unit');
