0ec7b3010cc2294d2ffb580e5d957177357f11d7
hush <hush@chromium.org>
2015-09-14 16:36:42 -0700

    Make sure FBO is correct for both kModeProcess and kModeDraw
    This is achieved by setting the correct FBO before each kModeDraw and
    hardware renderer won't reset it at the end of the draw so that
    kModeProcess between kModeDraws also has the correct FBO.
    
    This CL is to solve a bug where kModeProcess could run some GL commands
    that binds the wrong FBO, and the following kModeDraw is messed up.
    
    BUG=527113
    
    Review URL: https://codereview.chromium.org/1335033004
    
    Cr-Commit-Position: refs/heads/master@{#348766}

--- android_webview/browser/aw_gl_surface.h
+++ android_webview/browser/aw_gl_surface.h
@@ -27,7 +27,6 @@ class GL_EXPORT AwGLSurface : public gfx::GLSurface {
   void* GetDisplay() override;
 
   void SetBackingFrameBufferObject(unsigned int fbo);
-  void ResetBackingFrameBufferObject();
 
  protected:
   ~AwGLSurface() override;
