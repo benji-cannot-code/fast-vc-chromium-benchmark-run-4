a1164f388df3d0e13e6da9fd03b5e635b4092c71
sdefresne <sdefresne@chromium.org>
2015-09-14 07:34:32 -0700

    Partially fix compilation of src/ui unittests with Xcode 7 (OS X 10.11 SDK).
    NSNotFound is defined as an NSInteger and causes compilation error
    when used with DCHECK_EQ/DCHECK_NE so changes the code to instead
    use DCHECK().
    
    Cast the "modifiers" value passed to CGEventSetFlags to the right
    type to avoid a compilation warning as the NSUInteger no longer
    implicitly converts to the enumeration type.
    
    Pass an empty dictionary instead of a nil value as OS X 10.11 SDK
    now uses nullable/nonnullable annotation everywhere and the method
    -[NSBitmapImageRep representationUsingType:properties:] requires a
    valid value for properties.
    
    BUG=517914
    
    Review URL: https://codereview.chromium.org/1337823004
    
    Cr-Commit-Position: refs/heads/master@{#348615}

--- ui/base/cocoa/nsview_additions.mm
+++ ui/base/cocoa/nsview_additions.mm
@@ -28,10 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   NSArray* subviews = [[self superview] subviews];
 
   NSUInteger selfIndex = [subviews indexOfObject:self];
-  DCHECK_NE(NSNotFound, selfIndex);
+  DCHECK(NSNotFound != selfIndex);
 
   NSUInteger otherIndex = [subviews indexOfObject:otherView];
-  DCHECK_NE(NSNotFound, otherIndex);
+  DCHECK(NSNotFound != otherIndex);
 
   return selfIndex < otherIndex;
 }
