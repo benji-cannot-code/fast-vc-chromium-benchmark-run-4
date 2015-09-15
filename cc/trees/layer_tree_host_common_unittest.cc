cee356c1d48b998d5ad287b3e2e9b937f0dd7eb4
jaydasika <jaydasika@chromium.org>
2015-09-14 18:26:41 -0700

    Set clip children explicitly in LayerTreeHostCommonTest.RenderSurfaceWithUnclippedDescendantsClipsSubtree.
    clip parent is set in the test. We also need to explicitly
    set clip_children of clip parent. The test crashes with this
    change. The fix is to set unclipped surface of a clip node
    to be equal to the negation of ancestor clips subtree.
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1339923006
    
    Cr-Commit-Position: refs/heads/master@{#348805}

--- cc/trees/layer_tree_host_common_unittest.cc
+++ cc/trees/layer_tree_host_common_unittest.cc
@@ -7890,6 +7890,9 @@ TEST_F(LayerTreeHostCommonTest,
 
   test_layer->SetDrawsContent(true);
   render_surface->SetClipParent(clip_parent);
+  scoped_ptr<std::set<LayerImpl*>> clip_children(new std::set<LayerImpl*>);
+  clip_children->insert(render_surface);
+  clip_parent->SetClipChildren(clip_children.release());
   SetLayerPropertiesForTesting(root, identity_matrix, gfx::Point3F(),
                                gfx::PointF(), gfx::Size(30, 30), true, false,
                                true);
@@ -7908,7 +7911,7 @@ TEST_F(LayerTreeHostCommonTest,
 
   ExecuteCalculateDrawProperties(root);
 
-  EXPECT_EQ(gfx::Rect(-4, -4, 30, 30), test_layer->clip_rect());
+  EXPECT_EQ(gfx::Rect(30, 30), test_layer->clip_rect());
 }
 
 }  // namespace
