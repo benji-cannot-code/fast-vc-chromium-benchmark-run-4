78cb4f78c6fb93e991319e2644d397b1fc0d5ddc
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-09-15 12:25:35 -0700

    Revert of Fix BlueButton on gtk3 (patchset #1 id:1 of https://codereview.chromium.org/1347523004/ )
    Reason for revert:
    This looks like it broke Linux Tests bot:
    [17974:17974:0915/121314:FATAL:browser_main_loop.cc(247)] Gtk: /build/buildd/gtk+2.0-2.24.10/gtk/gtkwidget.c:5691: widget not within a GtkWindow
    #0 0x0000eb2118b4 base::debug::StackTrace::StackTrace()
    #1 0x0000eb2793df logging::LogMessage::~LogMessage()
    #2 0x0000f14c8633 content::(anonymous namespace)::GLibLogHandler()
    #3 0x0000e7606623 g_logv
    #4 0x0000e76067e3 g_log
    #5 0x0000e26d82fe gtk_widget_grab_default
    #6 0x0000ea5856f0 libgtk2ui::TurnButtonBlue()
    #7 0x0000ea58be78 libgtk2ui::NativeThemeGtk2::GetBlueButton()
    #8 0x0000ea58a967 libgtk2ui::NativeThemeGtk2::GetSystemColor()
    #9 0x0000f59540fe views::BlueButton::ResetColorsFromNativeTheme()
    #10 0x0000f595e10d views::LabelButton::OnNativeThemeChanged()
    #11 0x0000f5a4757d views::View::PropagateNativeThemeChanged()
    #12 0x0000f5a47550 views::View::PropagateNativeThemeChanged()
    #13 0x0000f5a4676d views::View::AddChildViewAt()
    #14 0x0000f5a46256 views::View::AddChildView()
    
    https://build.chromium.org/p/chromium.linux/builders/Linux%20Tests%20%28dbg%29%281%29%2832%29/builds/19611
    
    Original issue's description:
    > Fix BlueButton on gtk3
    >
    > The "suggested-action" style class exists for this purpose.
    >
    > R=erg@chromium.org
    >
    > Committed: https://crrev.com/af942ae9adc0ede50d33464c7769c1002d8086ee
    > Cr-Commit-Position: refs/heads/master@{#348942}
    
    TBR=erg@chromium.org,knthzh@gmail.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1342953003
    
    Cr-Commit-Position: refs/heads/master@{#348958}

--- chrome/browser/ui/libgtk2ui/native_theme_gtk2.h
+++ chrome/browser/ui/libgtk2ui/native_theme_gtk2.h
@@ -38,7 +38,6 @@ class NativeThemeGtk2 : public ui::NativeThemeBase {
   GtkWidget* GetEntry() const;
   GtkWidget* GetLabel() const;
   GtkWidget* GetButton() const;
-  GtkWidget* GetBlueButton() const;
   GtkWidget* GetTree() const;
   GtkWidget* GetTooltip() const;
   GtkWidget* GetMenu() const;
@@ -52,7 +51,6 @@ class NativeThemeGtk2 : public ui::NativeThemeBase {
   mutable OwnedWidgetGtk fake_entry_;
   mutable OwnedWidgetGtk fake_label_;
   mutable OwnedWidgetGtk fake_button_;
-  mutable OwnedWidgetGtk fake_bluebutton_;
   mutable OwnedWidgetGtk fake_tree_;
   mutable OwnedWidgetGtk fake_tooltip_;
   mutable OwnedWidgetGtk fake_menu_;
