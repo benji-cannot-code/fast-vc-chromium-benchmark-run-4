4bdaedc75901910c450b3e91d9d6b3517d55f5db
dtapuska <dtapuska@chromium.org>
2015-09-15 13:56:10 -0700

    [KeyEvent Mac] Move WebInputEventFactory into chromium.
    There was an inconsistency on the Mac platform that the WebInputEventFactory
    (old WebKit code) was being used. Other platforms have migrated to using
    builders and it was weird for the Mac code path to have some code on one
    side of the blink divide that others didn't. This change brings the Mac
    platform inline with the others by moving the input event
    factory from blink into chromium.
    
    blink change: https://codereview.chromium.org/1335673002/
    
    BUG=493833
    
    Review URL: https://codereview.chromium.org/1331013004
    
    Cr-Commit-Position: refs/heads/master@{#348989}

--- content/browser/renderer_host/input/web_input_event_util.cc
+++ content/browser/renderer_host/input/web_input_event_util.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_constants.h"
 #include "ui/events/gesture_detection/gesture_event_data.h"
 #include "ui/events/gesture_detection/motion_event.h"
+#include "ui/events/keycodes/dom/keycode_converter.h"
 #include "ui/gfx/geometry/safe_integer_conversions.h"
 
 using blink::WebGestureEvent;
@@ -186,6 +187,21 @@ int WebEventModifiersToEventFlags(int modifiers) {
     flags |= ui::EF_IS_REPEAT;
 
   return flags;
+}
+
+blink::WebInputEvent::Modifiers DomCodeToWebInputEventModifiers(
+    ui::DomCode code) {
+  switch (ui::KeycodeConverter::DomCodeToLocation(code)) {
+    case ui::DomKeyLocation::LEFT:
+      return blink::WebInputEvent::IsLeft;
+    case ui::DomKeyLocation::RIGHT:
+      return blink::WebInputEvent::IsRight;
+    case ui::DomKeyLocation::NUMPAD:
+      return blink::WebInputEvent::IsKeyPad;
+    case ui::DomKeyLocation::STANDARD:
+      break;
+  }
+  return static_cast<blink::WebInputEvent::Modifiers>(0);
 }
 
 }  // namespace content
