1f1dcc98a4ee609c316df11ba9a7b7fe0fe1dc19
pkasting <pkasting@chromium.org>
2015-09-15 12:58:13 -0700

    Create c/b/ui/views/layout_constants.*.
    This becomes a global place to get views layout constants which need some kind
    of parametrized getter, in these cases because of Material Design.  Previously
    these were accessible from ThemeService, but that required having an instance of
    a ThemeProvider (infeasible in some contexts) and implied these were
    theme-related in some way, which isn't true.
    
    BUG=531544
    TEST=none
    
    Review URL: https://codereview.chromium.org/1338103002
    
    Cr-Commit-Position: refs/heads/master@{#348967}

--- chrome/browser/ui/views/toolbar/wrench_toolbar_button.cc
+++ chrome/browser/ui/views/toolbar/wrench_toolbar_button.cc
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/thread_task_runner_handle.h"
 #include "base/time/time.h"
-#include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/views/extensions/browser_action_drag_data.h"
+#include "chrome/browser/ui/views/layout_constants.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "extensions/common/feature_switch.h"
 #include "grit/theme_resources.h"
@@ -54,9 +54,8 @@ gfx::Size WrenchToolbarButton::GetPreferredSize() const {
     gfx::Size size(image()->GetPreferredSize());
     ui::ThemeProvider* provider = GetThemeProvider();
     if (provider && provider->UsingSystemTheme()) {
-      int inset = provider->GetDisplayProperty(
-          ThemeProperties::PROPERTY_TOOLBAR_BUTTON_BORDER_INSET);
-      size.Enlarge(2 * inset, 2 * inset);
+      gfx::Insets insets(GetLayoutInsets(TOOLBAR_BUTTON));
+      size.Enlarge(insets.width(), insets.height());
     }
     return size;
   }
--- chrome/chrome_browser_ui.gypi
+++ chrome/chrome_browser_ui.gypi
@@ -2213,6 +2213,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/ui/views/infobars/infobar_view.cc',
       'browser/ui/views/infobars/infobar_view.h',
       'browser/ui/views/infobars/save_password_infobar.cc',
+      'browser/ui/views/layout_constants.cc',
+      'browser/ui/views/layout_constants.h',
       'browser/ui/views/load_complete_listener.cc',
       'browser/ui/views/load_complete_listener.h',
       'browser/ui/views/location_bar/bubble_icon_view.cc',
