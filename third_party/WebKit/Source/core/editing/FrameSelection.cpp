1ee6b6cc17a5a4bfcfb423a1d52c47d830c55dd9
sigbjornf@opera.com <sigbjornf@opera.com>
2015-09-15 09:47:33 +0000

    Oilpan: fix build after r202264.
    TBR=oilpan-reviews
    BUG=513568
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1339303002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202265 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/core/editing/FrameSelection.cpp
@@ -1269,6 +1269,7 @@ void FrameSelection::showTreeForThis() const
 DEFINE_TRACE(FrameSelection)
 {
     visitor->trace(m_frame);
+    visitor->trace(m_pendingSelection);
     visitor->trace(m_selectionEditor);
     visitor->trace(m_originalBase);
     visitor->trace(m_previousCaretNode);
