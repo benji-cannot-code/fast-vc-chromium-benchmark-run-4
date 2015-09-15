659e09f8a58b409469b3a55e12be151d20906e9f
pkasting <pkasting@chromium.org>
2015-09-15 13:53:32 -0700

    Fix #include guard in header after https://codereview.chromium.org/1321123002 .
    BUG=509811
    TEST=none
    
    Review URL: https://codereview.chromium.org/1340203002
    
    Cr-Commit-Position: refs/heads/master@{#348986}

--- ui/gfx/vector_icons_public.h
+++ ui/gfx/vector_icons_public.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef UI_GFX_VECTOR_ICONS_PUBLIC2_H_
-#define UI_GFX_VECTOR_ICONS_PUBLIC2_H_
+#ifndef UI_GFX_VECTOR_ICONS_PUBLIC_H_
+#define UI_GFX_VECTOR_ICONS_PUBLIC_H_
 
 #include "build/build_config.h"
 
@@ -19,4 +19,4 @@ enum class VectorIconId {
 }  // namespace gfx
 #endif
 
-#endif  // UI_GFX_VECTOR_ICONS_PUBLIC2_H_
+#endif  // UI_GFX_VECTOR_ICONS_PUBLIC_H_
