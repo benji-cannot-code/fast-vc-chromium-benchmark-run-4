de66ee7df13efacb1c2092e42fc7bc559a915e3a
sky <sky@chromium.org>
2015-09-15 06:20:32 -0700

    Changes webview not to delete frame when embedded app disconnects
    This breaks how html works. I'm changing Frame to insteall call to the
    delegate, which may take action (say show a sad tab page if the frame
    is the root frame).
    
    BUG=530812
    TEST=covered by tests
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/1340113003
    
    Cr-Commit-Position: refs/heads/master@{#348879}

--- components/web_view/frame_tree_delegate.cc
+++ components/web_view/frame_tree_delegate.cc
@@ -9,5 +9,6 @@ namespace web_view {
 
 void FrameTreeDelegate::DidCreateFrame(Frame* frame) {}
 void FrameTreeDelegate::DidDestroyFrame(Frame* frame) {}
+void FrameTreeDelegate::OnViewEmbeddedInFrameDisconnected(Frame* frame) {}
 
 }  // namespace web_view
