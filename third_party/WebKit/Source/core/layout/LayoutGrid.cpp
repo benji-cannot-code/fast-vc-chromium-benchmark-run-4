09357c85d6c07c879b7ba18e893089836d848c79
rego@igalia.com <rego@igalia.com>
2015-09-15 15:25:54 +0000

    [css-grid] Grid container's height should include scrollbar
    Add scrollbar's size in the grid container's height calculation at
    LayoutGrid::layoutGridItems().
    
    Add test to check both height and width (which was already working as
    expected) with margins, borders, paddings and scrollbars.
    
    BUG=532032
    TEST=fast/css-grid-layout/grid-container-margin-border-padding-scrollbar.html
    
    Review URL: https://codereview.chromium.org/1343083002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202274 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/LayoutGrid.cpp
+++ third_party/WebKit/Source/core/layout/LayoutGrid.cpp
@@ -1373,7 +1373,7 @@ void LayoutGrid::layoutGridItems()
     for (const auto& row : sizingData.rowTracks)
         setLogicalHeight(logicalHeight() + row.baseSize());
 
-    LayoutUnit height = logicalHeight() + borderAndPaddingLogicalHeight();
+    LayoutUnit height = logicalHeight() + borderAndPaddingLogicalHeight() + scrollbarLogicalHeight();
     if (hasLineIfEmpty())
         height = std::max(height, minimumLogicalHeightForEmptyLine());
 
