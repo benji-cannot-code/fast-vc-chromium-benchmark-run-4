bc25c968e48b8918d554473614dfc057c8782516
sergeyu <sergeyu@chromium.org>
2015-09-15 17:11:44 -0700

    Add ScopedAllowIO in GnubbyAuthHandlerPosix::CreateAuthorizationSocket().
    GnubbyAuthHandlerPosix listens to a unix socket on the IO the remoting
    host. It needs to be able to unlink() previously created socket.
    Deletion of a unix socket is a blocking operation because it may
    require disk IO, but moving GnubbyAuthHandlerPosix on a different
    thread would be overkill.
    
    BUG=509807
    
    Review URL: https://codereview.chromium.org/1324763007
    
    Cr-Commit-Position: refs/heads/master@{#349038}

--- remoting/host/gnubby_auth_handler_posix.cc
+++ remoting/host/gnubby_auth_handler_posix.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/stl_util.h"
+#include "base/threading/thread_restrictions.h"
 #include "base/values.h"
 #include "net/base/net_errors.h"
 #include "net/socket/unix_domain_server_socket_posix.h"
@@ -241,8 +242,15 @@ void GnubbyAuthHandlerPosix::CreateAuthorizationSocket() {
   DCHECK(CalledOnValidThread());
 
   if (!g_gnubby_socket_name.Get().empty()) {
-    // If the file already exists, a socket in use error is returned.
-    base::DeleteFile(g_gnubby_socket_name.Get(), false);
+    {
+      // DeleteFile() is a blocking operation, but so is creation of the unix
+      // socket below. Consider moving this class to a different thread if this
+      // causes any problems. See crbug.com/509807 .
+      base::ThreadRestrictions::ScopedAllowIO allow_io;
+
+      // If the file already exists, a socket in use error is returned.
+      base::DeleteFile(g_gnubby_socket_name.Get(), false);
+    }
 
     HOST_LOG << "Listening for gnubby requests on "
              << g_gnubby_socket_name.Get().value();
