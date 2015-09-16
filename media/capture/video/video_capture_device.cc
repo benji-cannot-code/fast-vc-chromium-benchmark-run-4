28b45f7d7e84e8449351755e6073458b4ad9cb72
msu.koo <msu.koo@samsung.com>
2015-09-16 09:54:01 -0700

    Fixed not to append the model id when fake video device is enabled.
    BUG=526633
    
    Review URL: https://codereview.chromium.org/1326663002
    
    Cr-Commit-Position: refs/heads/master@{#349146}

--- media/capture/video/video_capture_device.cc
+++ media/capture/video/video_capture_device.cc
@@ -5,17 +5,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/capture/video/video_capture_device.h"
 
+#include "base/command_line.h"
 #include "base/i18n/timezone.h"
 #include "base/strings/string_util.h"
+#include "media/base/media_switches.h"
 
 namespace media {
 
+// TODO(msu.koo): http://crbug.com/532272, remove checking the switch in favour
+// of deferring GetModel() call to the actual VideoCaptureDevice object.
 const std::string VideoCaptureDevice::Name::GetNameAndModel() const {
   const std::string model_id = GetModel();
   if (model_id.empty())
     return device_name_;
   const std::string suffix = " (" + model_id + ")";
-  if (base::EndsWith(device_name_, suffix, base::CompareCase::SENSITIVE))
+  if (base::EndsWith(device_name_, suffix, base::CompareCase::SENSITIVE) ||
+      base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kUseFakeDeviceForMediaStream))
+    // Ignore |model_id| if |kUseFakeDeviceForMediaStream| flag is present.
     return device_name_;
   return device_name_ + suffix;
 }
