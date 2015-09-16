76618cda3683204fc15a050f6f9d9e528bd21f1b
grunell <grunell@chromium.org>
2015-09-16 08:03:27 -0700

    Add three missing includes.
    Also fixed ambiguous call for a dcheck.
    
    Found these problem when working on removing WebRTC's dependency on Chromium and this should make the WebRTC Chromium FYI bots green.
    
    BUG=468375,532001
    TBR=tommi@chromium.org,sergeyu@chromium.org
    
    Review URL: https://codereview.chromium.org/1349743002
    
    Cr-Commit-Position: refs/heads/master@{#349124}

--- content/renderer/media/mock_data_channel_impl.cc
+++ content/renderer/media/mock_data_channel_impl.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/renderer/media/mock_data_channel_impl.h"
 
+#include "base/logging.h"
+
 namespace content {
 
 MockDataChannel::MockDataChannel(const std::string& label,
