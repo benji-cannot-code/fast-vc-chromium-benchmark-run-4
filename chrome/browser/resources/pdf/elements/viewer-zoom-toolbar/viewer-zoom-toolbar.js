0880e45030180af4feec7c9b4b7306d79dee30c5
tsergeant <tsergeant@chromium.org>
2015-09-15 22:19:32 -0700

    Revert of Material PDF: Prevent toolbars showing until after the plugin is ready (patchset #2 id:20001 of https://codereview.chromium.org/1308223004/ )
    Reason for revert:
    This CL incorrectly causes the toolbars to not show up at all while the PDF is loading.
    
    Original issue's description:
    > Material PDF: Prevent toolbars showing until after the plugin is ready
    >
    > This fixes an issue where toolbars would be visible but inactive on top
    > of the PDF plugin disabled message. It also affects the error and
    > password dialogs, bringing behaviour in line with non-material PDF.
    >
    > BUG=526934
    >
    > Committed: https://crrev.com/94149c519d2fecd68f02979cdf5119293faf6fc3
    > Cr-Commit-Position: refs/heads/master@{#346836}
    
    TBR=raymes@chromium.org
    BUG=526934
    
    Review URL: https://codereview.chromium.org/1339283002
    
    Cr-Commit-Position: refs/heads/master@{#349080}

--- chrome/browser/resources/pdf/elements/viewer-zoom-toolbar/viewer-zoom-toolbar.js
+++ chrome/browser/resources/pdf/elements/viewer-zoom-toolbar/viewer-zoom-toolbar.js
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       }
     },
 
-    hostAttributes: {
-      hidden: true  // Toolbars are explicitly shown once the plugin is ready.
-    },
-
     fitToggle: function() {
       if (this.$['fit-button'].activeIndex == FIT_TO_WIDTH)
         this.fire('fit-to-width');
