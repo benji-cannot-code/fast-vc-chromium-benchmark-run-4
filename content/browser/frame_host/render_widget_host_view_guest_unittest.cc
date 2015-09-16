8e3e7d20b27eb920b50192bf36e4da9de6ab3112
lazyboy <lazyboy@chromium.org>
2015-09-16 13:07:20 -0700

    Fix OOP <webview> resize and autosize.
    This CL removes the dependency of updating guest's size based
      on compositor frame: BPG::UpdateGuestSizeIfNecessary(). This
      info is not readily available in --site-per-process since we
      do not have a RWHVGuest. Instead, this CL uses
      WebContentsObserver::ResizeDueToAutoResize to resize guest when
      autosize is turned on.
    
    This CL also changes the test to look at actual guest window
      resizing in one test: testResizeEvents().
    
    I've seen two tests seeing window.onresize with original guest
      size (300x300) some times, I've added an exception for this
      initial size, which should help deflake these tests. These tests
      are:
    testResizeWebviewResizesContent()
    testResizeWebviewWithDisplayNoneResizesContent()
    
    There's a dependent blink patch for this CL:
    https://chromiumcodereview.appspot.com/1309343002/
    
    TBR=jam@chromium.org testing/buildbot/chromium.fyi.json
    BUG=517291
    Test=With --site-per-process, run a <webview> in a chrome app.
    1) Notice resizing a <webview> works, e.g.
      document.querySelector('webview').style.width = '1000px';
    2) Notice that turning on autosize also works, e.g.
      var w = document.querySelector('webview');
      w.minheight = '400', w.maxheight = '800';
      w.minwidth = '500', w.maxheight = '600';
      w.setAttribute('autosize', true);
    
    Review URL: https://codereview.chromium.org/1262553003
    
    Cr-Commit-Position: refs/heads/master@{#349205}

--- content/browser/frame_host/render_widget_host_view_guest_unittest.cc
+++ content/browser/frame_host/render_widget_host_view_guest_unittest.cc
@@ -109,13 +109,10 @@ class TestBrowserPluginGuest : public BrowserPluginGuest {
                          BrowserPluginGuestDelegate* delegate):
       BrowserPluginGuest(web_contents->HasOpener(), web_contents, delegate),
       last_scale_factor_received_(0.f),
-      update_scale_factor_received_(0.f),
       received_delegated_frame_(false) {}
   ~TestBrowserPluginGuest() override {}
 
   void ResetTestData() {
-    update_frame_size_received_= gfx::Size();
-    update_scale_factor_received_ = 0.f;
     last_surface_id_received_ = cc::SurfaceId();
     last_frame_size_received_ = gfx::Size();
     last_scale_factor_received_ = 0.f;
@@ -129,12 +126,6 @@ class TestBrowserPluginGuest : public BrowserPluginGuest {
 
   void set_attached(bool attached) {
     BrowserPluginGuest::set_attached_for_test(attached);
-  }
-
-  void UpdateGuestSizeIfNecessary(const gfx::Size& frame_size,
-                                  float scale_factor) override {
-    update_frame_size_received_= frame_size;
-    update_scale_factor_received_ = scale_factor;
   }
 
   void SwapCompositorFrame(uint32_t output_surface_id,
@@ -163,7 +154,6 @@ class TestBrowserPluginGuest : public BrowserPluginGuest {
 
   cc::SurfaceId last_surface_id_received_;
   gfx::Size last_frame_size_received_;
-  gfx::Size update_frame_size_received_;
   float last_scale_factor_received_;
   float update_scale_factor_received_;
 
@@ -271,10 +261,6 @@ TEST_F(RenderWidgetHostViewGuestSurfaceTest, TestGuestSurface) {
   view_->OnSwapCompositorFrame(
       0, CreateDelegatedFrame(scale_factor, view_size, view_rect));
 
-  EXPECT_EQ(view_size, browser_plugin_guest_->update_frame_size_received_);
-  EXPECT_EQ(scale_factor,
-            browser_plugin_guest_->update_scale_factor_received_);
-
   if (UseSurfacesEnabled()) {
     cc::SurfaceId id = surface_id();
     if (!id.is_null()) {
@@ -334,8 +320,6 @@ TEST_F(RenderWidgetHostViewGuestSurfaceTest, TestGuestSurface) {
 
   view_->OnSwapCompositorFrame(
       0, CreateDelegatedFrame(scale_factor, view_size, view_rect));
-  EXPECT_EQ(gfx::Size(), browser_plugin_guest_->update_frame_size_received_);
-  EXPECT_EQ(0.f, browser_plugin_guest_->update_scale_factor_received_);
   if (UseSurfacesEnabled())
     EXPECT_TRUE(surface_id().is_null());
 }
