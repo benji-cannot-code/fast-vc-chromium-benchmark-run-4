25b48c41ad9f411daf543aa5928b08fcdce7dcd2
thestig <thestig@chromium.org>
2015-09-15 18:53:18 -0700

    Revert "Populate EEP estimate in NQE"
    This reverts commit 9ee5d296239e3bc21b5c70259242ce6c4cefc96b.
    
    NetworkQualityEstimatorTest failing on multiple bots.
    
    BUG=472685
    TBR=tbansal@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1348523003
    
    Cr-Commit-Position: refs/heads/master@{#349065}

--- chrome/browser/android/net/external_estimate_provider_android.cc
+++ chrome/browser/android/net/external_estimate_provider_android.cc
@@ -34,6 +34,13 @@ ExternalEstimateProviderAndroid::~ExternalEstimateProviderAndroid() {
   net::NetworkChangeNotifier::RemoveConnectionTypeObserver(this);
 }
 
+void ExternalEstimateProviderAndroid::RequestUpdate() const {
+  DCHECK(thread_checker_.CalledOnValidThread());
+  JNIEnv* env = base::android::AttachCurrentThread();
+  Java_ExternalEstimateProviderAndroid_requestUpdate(
+      env, j_external_estimate_provider_.obj());
+}
+
 bool ExternalEstimateProviderAndroid::GetRTT(base::TimeDelta* rtt) const {
   DCHECK(thread_checker_.CalledOnValidThread());
   JNIEnv* env = base::android::AttachCurrentThread();
@@ -90,23 +97,6 @@ bool ExternalEstimateProviderAndroid::GetTimeSinceLastUpdate(
   return true;
 }
 
-void ExternalEstimateProviderAndroid::SetUpdatedEstimateDelegate(
-    net::ExternalEstimateProvider::UpdatedEstimateDelegate* delegate) {
-  delegate_ = delegate;
-}
-
-void ExternalEstimateProviderAndroid::Update() const {
-  DCHECK(thread_checker_.CalledOnValidThread());
-  JNIEnv* env = base::android::AttachCurrentThread();
-  Java_ExternalEstimateProviderAndroid_requestUpdate(
-      env, j_external_estimate_provider_.obj());
-}
-
-void ExternalEstimateProviderAndroid::OnConnectionTypeChanged(
-    net::NetworkChangeNotifier::ConnectionType type) {
-  Update();
-}
-
 void ExternalEstimateProviderAndroid::
     NotifyExternalEstimateProviderAndroidUpdate(JNIEnv* env, jobject obj) {
   if (!task_runner_)
@@ -124,8 +114,18 @@ void ExternalEstimateProviderAndroid::NotifyUpdatedEstimateAvailable() const {
     delegate_->OnUpdatedEstimateAvailable();
 }
 
+void ExternalEstimateProviderAndroid::OnConnectionTypeChanged(
+    net::NetworkChangeNotifier::ConnectionType type) {
+  RequestUpdate();
+}
+
 bool RegisterExternalEstimateProviderAndroid(JNIEnv* env) {
   return RegisterNativesImpl(env);
+}
+
+void ExternalEstimateProviderAndroid::SetUpdatedEstimateDelegate(
+    net::ExternalEstimateProvider::UpdatedEstimateDelegate* delegate) {
+  delegate_ = delegate;
 }
 
 }  // namespace android
