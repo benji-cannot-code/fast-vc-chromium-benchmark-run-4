aeb334bf02a78b83f25ee5abec83c3ea44ac31a4
michaelpg <michaelpg@chromium.org>
2015-09-16 16:08:55 -0700

    Add Polymer tests for cr-settings-prefs
    This lays the groundwork for testing our prefs element and includes
    some basic tests.
    
    The test includes a mock of settingsPrivate. It would be good to
    come up with a better solution for making cr-settings-prefs use
    the mock; right now I have it working with a global variable which is
    less than ideal.
    
    After this CL, I'll add sanity tests for list and dictionary prefs.
    These CLs shouldn't *block* adding list & dict prefs, but I don't want
    to commit new features in prefs.js without at least having tests locally.
    So I may as well upload the tests for review as well.
    
    BUG=532270
    TESTS=CrSettingsBrowserTest
    
    Review URL: https://codereview.chromium.org/1310843010
    
    Cr-Commit-Position: refs/heads/master@{#349268}

--- chrome/browser/resources/settings/prefs/prefs_types.js
+++ chrome/browser/resources/settings/prefs/prefs_types.js
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /** @type {{INITIALIZED: string, isInitialized: boolean}} */
 var CrSettingsPrefs;
 
+// TODO(michaelpg): provide a Promise for initialization.
 if (CrSettingsPrefs === undefined) {
   CrSettingsPrefs = {
     /**
