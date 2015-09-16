cfe997d592ee31fc3c94d661b518423cf5a7678e
pdr@chromium.org <pdr@chromium.org>
2015-09-16 02:25:07 +0000

    Remove WebRuntimeFeatures::enableSlimmingPaint now that it's on my default
    BUG=524314
    
    Review URL: https://codereview.chromium.org/1343203002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202314 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebRuntimeFeatures.h
+++ third_party/WebKit/public/web/WebRuntimeFeatures.h
@@ -118,7 +118,6 @@ public:
 
     BLINK_EXPORT static void enableScriptedSpeech(bool);
 
-    BLINK_EXPORT static void enableSlimmingPaint(bool);
     BLINK_EXPORT static void enableSlimmingPaintV2(bool);
 
     BLINK_EXPORT static void enableTouch(bool);
