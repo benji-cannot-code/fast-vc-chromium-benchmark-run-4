4ae842df28e452b716741a9ddc77dfbc1e72e379
kochi <kochi@chromium.org>
2015-09-15 18:19:19 -0700

    Revert of [KeyEvent Mac] Move WebInputEventFactory into chromium. (patchset #7 id:120001 of https://codereview.chromium.org/1331013004/ )
    Reason for revert:
    This caused compile failure on MacOSX 10.9 Retina
    bot.
    
    The relevant error log:
    
    ../../content/browser/renderer_host/input/web_input_event_builders_mac.mm:889:10: error: use of undeclared identifier 'NSEventTypeSmartMagnify'; did you mean 'NSEventTypeMagnify'?
        case NSEventTypeSmartMagnify:
             ^~~~~~~~~~~~~~~~~~~~~~~
             NSEventTypeMagnify
    /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk/System/Library/Frameworks/AppKit.framework/Headers/NSEvent.h:48:5: note: 'NSEventTypeMagnify' declared here
        NSEventTypeMagnify          = 30,
        ^
    ../../content/browser/renderer_host/input/web_input_event_builders_mac.mm:889:10: error: duplicate case value 'NSEventTypeMagnify'
        case NSEventTypeSmartMagnify:
             ^
    ../../content/browser/renderer_host/input/web_input_event_builders_mac.mm:885:10: note: previous case defined here
        case NSEventTypeMagnify:
             ^
    2 errors generated.
    ninja: build stopped: subcommand failed.
    
    Original issue's description:
    > [KeyEvent Mac] Move WebInputEventFactory into chromium.
    >
    > There was an inconsistency on the Mac platform that the WebInputEventFactory
    > (old WebKit code) was being used. Other platforms have migrated to using
    > builders and it was weird for the Mac code path to have some code on one
    > side of the blink divide that others didn't. This change brings the Mac
    > platform inline with the others by moving the input event
    > factory from blink into chromium.
    >
    > blink change: https://codereview.chromium.org/1335673002/
    >
    > BUG=493833,237267
    >
    > Committed: https://crrev.com/8db5ca887d4560ed15bf5e6e2dce69dc85868f25
    > Cr-Commit-Position: refs/heads/master@{#348989}
    
    TBR=wez@chromium.org,sadrul@chromium.org,asvitkine@chromium.org,mnaganov@chromium.org,piman@chromium.org,dtapuska@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=493833,237267
    
    Review URL: https://codereview.chromium.org/1345043003
    
    Cr-Commit-Position: refs/heads/master@{#349058}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/frame_host/frame_tree_node.h"
 #include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/browser/gpu/compositor_util.h"
-#include "content/browser/renderer_host/input/web_input_event_builders_mac.h"
 #include "content/browser/renderer_host/render_view_host_impl.h"
 #include "content/browser/renderer_host/render_widget_helper.h"
 #include "content/browser/renderer_host/render_widget_host_delegate.h"
@@ -65,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/ext/skia_utils_mac.h"
 #include "third_party/WebKit/public/platform/WebScreenInfo.h"
 #include "third_party/WebKit/public/web/WebInputEvent.h"
+#include "third_party/WebKit/public/web/mac/WebInputEventFactory.h"
 #import "third_party/mozilla/ComplexTextInputPanel.h"
 #include "ui/accelerated_widget_mac/io_surface_layer.h"
 #include "ui/accelerated_widget_mac/surface_handle_types.h"
@@ -98,10 +98,8 @@ using content::RenderWidgetHostViewMac;
 using content::RenderWidgetHostViewMacEditCommandHelper;
 using content::TextInputClientMac;
 using content::WebContents;
-using content::WebGestureEventBuilder;
-using content::WebMouseEventBuilder;
-using content::WebMouseWheelEventBuilder;
 using blink::WebInputEvent;
+using blink::WebInputEventFactory;
 using blink::WebMouseEvent;
 using blink::WebMouseWheelEvent;
 using blink::WebGestureEvent;
@@ -1289,7 +1287,8 @@ void RenderWidgetHostViewMac::SetShowingContextMenu(bool showing) {
                                    eventNumber:0
                                     clickCount:0
                                       pressure:0];
-  WebMouseEvent web_event = WebMouseEventBuilder::Build(event, cocoa_view_);
+  WebMouseEvent web_event =
+      WebInputEventFactory::mouseEvent(event, cocoa_view_);
   if (showing)
     web_event.type = WebInputEvent::MouseLeave;
   ForwardMouseEvent(web_event);
@@ -1983,7 +1982,8 @@ void RenderWidgetHostViewMac::OnDisplayMetricsChanged(
   if ([self shouldIgnoreMouseEvent:theEvent]) {
     // If this is the first such event, send a mouse exit to the host view.
     if (!mouseEventWasIgnored_ && renderWidgetHostView_->render_widget_host_) {
-      WebMouseEvent exitEvent = WebMouseEventBuilder::Build(theEvent, self);
+      WebMouseEvent exitEvent =
+          WebInputEventFactory::mouseEvent(theEvent, self);
       exitEvent.type = WebInputEvent::MouseLeave;
       exitEvent.button = WebMouseEvent::ButtonNone;
       renderWidgetHostView_->ForwardMouseEvent(exitEvent);
@@ -1996,7 +1996,8 @@ void RenderWidgetHostViewMac::OnDisplayMetricsChanged(
     // If this is the first mouse event after a previous event that was ignored
     // due to the hitTest, send a mouse enter event to the host view.
     if (renderWidgetHostView_->render_widget_host_) {
-      WebMouseEvent enterEvent = WebMouseEventBuilder::Build(theEvent, self);
+      WebMouseEvent enterEvent =
+          WebInputEventFactory::mouseEvent(theEvent, self);
       enterEvent.type = WebInputEvent::MouseMove;
       enterEvent.button = WebMouseEvent::ButtonNone;
       if (renderWidgetHostView_->render_widget_host_->delegate() &&
@@ -2035,7 +2036,7 @@ void RenderWidgetHostViewMac::OnDisplayMetricsChanged(
     [self confirmComposition];
   }
 
-  WebMouseEvent event = WebMouseEventBuilder::Build(theEvent, self);
+  WebMouseEvent event = WebInputEventFactory::mouseEvent(theEvent, self);
   if (renderWidgetHostView_->render_widget_host_->delegate() &&
       renderWidgetHostView_->render_widget_host_->delegate()
           ->GetInputEventRouter()) {
@@ -2368,7 +2369,7 @@ void RenderWidgetHostViewMac::OnDisplayMetricsChanged(
     // Allow rubber-banding in both directions.
     bool canRubberbandLeft = true;
     bool canRubberbandRight = true;
-    WebMouseWheelEvent webEvent = WebMouseWheelEventBuilder::Build(
+    WebMouseWheelEvent webEvent = WebInputEventFactory::mouseWheelEvent(
         event, self, canRubberbandLeft, canRubberbandRight);
     webEvent.railsMode = mouseWheelFilter_.UpdateRailsMode(webEvent);
     renderWidgetHostView_->render_widget_host_->ForwardWheelEvent(webEvent);
@@ -2383,7 +2384,7 @@ void RenderWidgetHostViewMac::OnDisplayMetricsChanged(
 - (void)beginGestureWithEvent:(NSEvent*)event {
   [responderDelegate_ beginGestureWithEvent:event];
   gestureBeginEvent_.reset(
-      new WebGestureEvent(WebGestureEventBuilder::Build(event, self)));
+      new WebGestureEvent(WebInputEventFactory::gestureEvent(event, self)));
 
   // If the page is at the minimum zoom level, require a threshold be reached
   // before the pinch has an effect.
@@ -2401,7 +2402,7 @@ void RenderWidgetHostViewMac::OnDisplayMetricsChanged(
     return;
 
   if (gestureBeginPinchSent_) {
-    WebGestureEvent endEvent(WebGestureEventBuilder::Build(event, self));
+    WebGestureEvent endEvent(WebInputEventFactory::gestureEvent(event, self));
     endEvent.type = WebInputEvent::GesturePinchEnd;
     renderWidgetHostView_->render_widget_host_->ForwardGestureEvent(endEvent);
     gestureBeginPinchSent_ = NO;
@@ -2426,7 +2427,7 @@ void RenderWidgetHostViewMac::OnDisplayMetricsChanged(
 
 - (void)smartMagnifyWithEvent:(NSEvent*)event {
   const WebGestureEvent& smartMagnifyEvent =
-      WebGestureEventBuilder::Build(event, self);
+      WebInputEventFactory::gestureEvent(event, self);
   if (renderWidgetHostView_ && renderWidgetHostView_->render_widget_host_) {
     renderWidgetHostView_->render_widget_host_->ForwardGestureEvent(
         smartMagnifyEvent);
@@ -2499,7 +2500,7 @@ void RenderWidgetHostViewMac::OnDisplayMetricsChanged(
   if (renderWidgetHostView_->render_widget_host_) {
     BOOL canRubberbandLeft = [responderDelegate_ canRubberbandLeft:self];
     BOOL canRubberbandRight = [responderDelegate_ canRubberbandRight:self];
-    WebMouseWheelEvent webEvent = WebMouseWheelEventBuilder::Build(
+    WebMouseWheelEvent webEvent = WebInputEventFactory::mouseWheelEvent(
         event, self, canRubberbandLeft, canRubberbandRight);
     webEvent.railsMode = mouseWheelFilter_.UpdateRailsMode(webEvent);
     if (renderWidgetHostView_->render_widget_host_->delegate() &&
@@ -2540,7 +2541,8 @@ void RenderWidgetHostViewMac::OnDisplayMetricsChanged(
   }
 
   // Send a GesturePinchUpdate event.
-  WebGestureEvent updateEvent = WebGestureEventBuilder::Build(event, self);
+  WebGestureEvent updateEvent =
+      WebInputEventFactory::gestureEvent(event, self);
   updateEvent.data.pinchUpdate.zoomDisabled = !pinchHasReachedZoomThreshold_;
   renderWidgetHostView_->render_widget_host_->ForwardGestureEvent(updateEvent);
 }
