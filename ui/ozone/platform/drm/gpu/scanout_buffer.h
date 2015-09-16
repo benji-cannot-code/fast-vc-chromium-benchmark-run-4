a0944122b1f25cac9419e00701457f3fe4b43a17
kalyan.kondapally <kalyan.kondapally@intel.com>
2015-09-16 11:03:23 -0700

    Explicitly pass format to ScanoutBufferGenerator
    Currently, we assume it's always going to be RGBX format and create
    appropriate buffer. However, we also want to support creation of buffers
    with other formats(i.e YUV) to test overlay candidates in TestPageFlip.
    This patch adds needed support for this.
    
    BUG=370522
    
    Review URL: https://codereview.chromium.org/1348643002
    
    Cr-Commit-Position: refs/heads/master@{#349160}

--- ui/ozone/platform/drm/gpu/scanout_buffer.h
+++ ui/ozone/platform/drm/gpu/scanout_buffer.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/memory/ref_counted.h"
+#include "ui/gfx/buffer_types.h"
 #include "ui/gfx/geometry/size.h"
 
 namespace ui {
@@ -44,6 +45,7 @@ class ScanoutBufferGenerator {
 
   virtual scoped_refptr<ScanoutBuffer> Create(
       const scoped_refptr<DrmDevice>& drm,
+      gfx::BufferFormat format,
       const gfx::Size& size) = 0;
 };
 
