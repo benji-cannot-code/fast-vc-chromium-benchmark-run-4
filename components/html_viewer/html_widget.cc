1a829035f4eab36462d217787dec1b86e39a71c7
penghuang <penghuang@chromium.org>
2015-09-16 13:11:35 -0700

    This change adds WebGL support in mandoline. It also adds GL chromium_framebuffer_multisample extension which is need by WebGL.
    TODO:
      * Pass attributes to GPU when create WebGL context.
      * Get GPU info and fill it into glinfo.
    
    TEST= mandoline --enable-webgl http://learningwebgl.com/lessons/lesson03/index.html
    BUG=525159
    NOPRESUBMIT=true
    
    Review URL: https://codereview.chromium.org/1338433002
    
    Cr-Commit-Position: refs/heads/master@{#349206}

--- components/html_viewer/html_widget.cc
+++ components/html_viewer/html_widget.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/html_viewer/html_widget.h"
 
+#include "base/command_line.h"
 #include "components/html_viewer/global_state.h"
 #include "components/html_viewer/ime_controller.h"
 #include "components/html_viewer/stats_collection_controller.h"
@@ -20,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace html_viewer {
 namespace {
+
+const char kDisableWebGLSwitch[] = "disable-webgl";
 
 scoped_ptr<WebLayerTreeViewImpl> CreateWebLayerTreeView(
     GlobalState* global_state) {
@@ -58,6 +61,10 @@ void ConfigureSettings(blink::WebSettings* settings) {
   settings->setDefaultFontSize(16);
   settings->setLoadsImagesAutomatically(true);
   settings->setJavaScriptEnabled(true);
+
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  settings->setExperimentalWebGLEnabled(
+      !command_line->HasSwitch(kDisableWebGLSwitch));
 }
 
 }  // namespace
