6b8ce8fec75aaf97d511b73fe17ba39abf9bdf93
pfeldman@chromium.org <pfeldman@chromium.org>
2015-09-16 23:38:38 +0000

    DevTools: do not produce space while incrementally editing styles with no trailing semicolon.
    Review URL: https://codereview.chromium.org/1352593002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202398 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/CSSStyleModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/CSSStyleModel.js
@@ -1320,7 +1320,7 @@ WebInspector.CSSProperty.prototype = {
         var tokenize = tokenizerFactory.createTokenizer("text/css");
 
         tokenize("*{" + styleText + "}", processToken);
-        result = result.slice(0, result.length - 1);
+
         return result + (indentation ? "\n" + endIndentation : "");
 
         /**
@@ -1331,6 +1331,10 @@ WebInspector.CSSProperty.prototype = {
          */
         function processToken(token, tokenType, column, newColumn)
         {
+            if (token === "}" || token === ";")
+                result = result.trimRight();  // collect trailing space before } and ;
+            if (token === "}")
+                return;
             if (newColumn <= 2)
                 return;
             var isSemicolon = token === ";";
