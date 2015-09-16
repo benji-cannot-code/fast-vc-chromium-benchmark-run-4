7afc7374c527184316dbb07bf2bcb052795628ef
shiva.jm <shiva.jm@samsung.com>
2015-09-16 03:38:23 -0700

    Update tests for chrome, as MediaStream label, ended attributes and stop() method will be removed
    Also CL: https://codereview.chromium.org/1312213006/
    depends on these CL to commit first.
    
    Intent to Remove:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/LcZGlM9OBqs
    
    Intent to Remove, which became an Intent to Deprecate:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/s0UsMk8i2YM/WpepA4w3mPEJ
    
    BUG=338500
    
    Review URL: https://codereview.chromium.org/1344753002
    
    Cr-Commit-Position: refs/heads/master@{#349094}

--- chrome/test/data/extensions/api_test/tab_capture/active_tab_chrome_pages.js
+++ chrome/test/data/extensions/api_test/tab_capture/active_tab_chrome_pages.js
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var afterTabOpened = function() {
   chrome.tabCapture.capture({audio: true, video: true}, function(stream) {
     chrome.test.assertTrue(!!stream);
-    stream.stop();
+    stream.getVideoTracks()[0].stop();
+    stream.getAudioTracks()[0].stop();
     chrome.test.succeed();
   });
 };
--- chrome/test/data/extensions/api_test/tab_capture/api_tests.js
+++ chrome/test/data/extensions/api_test/tab_capture/api_tests.js
@@ -25,7 +25,8 @@ chrome.test.runTests([
 
     tabCapture.capture({audio: true, video: true}, function(stream) {
       chrome.test.assertTrue(!!stream);
-      stream.stop();
+      stream.getVideoTracks()[0].stop();
+      stream.getAudioTracks()[0].stop();
     });
   },
 
@@ -67,7 +68,8 @@ chrome.test.runTests([
       // returns 'active' capturing status for the second tab.
       var capturedTabsAfterStartCapture = function(infos) {
         checkInfoForSecondTabHasStatus(infos, 'active');
-        activeStream.stop();
+        activeStream.getVideoTracks()[0].stop();
+        activeStream.getAudioTracks()[0].stop();
         tabCapture.getCapturedTabs(capturedTabsAfterStopCapture);
       };
 
@@ -87,7 +89,8 @@ chrome.test.runTests([
       chrome.test.assertLastError(
           'Cannot capture a tab with an active stream.');
       chrome.test.assertTrue(!stream);
-      stream1.stop();
+      stream1.getVideoTracks()[0].stop();
+      stream1.getAudioTracks()[0].stop();
       chrome.test.succeed();
     };
 
@@ -111,7 +114,7 @@ chrome.test.runTests([
         }
         else if ((changeInfo.mutedInfo.reason == "capture") &&
                  (changeInfo.mutedInfo.muted === false)) {
-          stream1.stop();
+          stream1.getAudioTracks()[0].stop();
           stopListener();
         }
       });
@@ -123,7 +126,7 @@ chrome.test.runTests([
   function onlyVideo() {
     tabCapture.capture({video: true}, function(stream) {
       chrome.test.assertTrue(!!stream);
-      stream.stop();
+      stream.getVideoTracks()[0].stop();
       chrome.test.succeed();
     });
   },
@@ -131,7 +134,7 @@ chrome.test.runTests([
   function onlyAudio() {
     tabCapture.capture({audio: true}, function(stream) {
       chrome.test.assertTrue(!!stream);
-      stream.stop();
+      stream.getAudioTracks()[0].stop();
       chrome.test.succeed();
     });
   },
