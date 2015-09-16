f4525365ec9a6b65a1fc0441abf391ae11212d96
sclittle <sclittle@chromium.org>
2015-09-16 11:33:53 -0700

    Added polling in DRP integration tests to support Data Saver API.
    When the Data Saver API is in use, there is currently a race between the
    Data Saver API fetch and the test pages loaded by the integration tests.
    This change makes the tests wait until the Data Saver API fetch
    finishes.
    
    This CL should fix the flakiness in the DRP integration tests caused by
    this race.
    
    BUG=526827
    
    Review URL: https://codereview.chromium.org/1349493002
    
    Cr-Commit-Position: refs/heads/master@{#349167}

--- tools/chrome_proxy/live_tests/chrome_proxy_measurements.py
+++ tools/chrome_proxy/live_tests/chrome_proxy_measurements.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import logging
 
 import chrome_proxy_metrics as metrics
+from common import chrome_proxy_measurements as measurements
 from telemetry.core import exceptions
 from telemetry.page import page_test
 
@@ -18,10 +19,12 @@ class ChromeProxyLatencyBase(page_test.PageTest):
 
   def WillNavigateToPage(self, page, tab):
     tab.ClearCache(force=True)
+    self._metrics.Start(page, tab)
 
   def ValidateAndMeasurePage(self, page, tab, results):
     # Wait for the load event.
     tab.WaitForJavaScriptExpression('performance.timing.loadEventStart', 300)
+    self._metrics.Stop(page, tab)
     self._metrics.AddResultsForLatency(tab, results)
 
 
@@ -32,6 +35,11 @@ class ChromeProxyLatency(ChromeProxyLatencyBase):
     super(ChromeProxyLatency, self).__init__(*args, **kwargs)
 
   def CustomizeBrowserOptions(self, options):
+    # NOTE: When using the Data Saver API, the first few requests for this test
+    # could go over direct instead of through the Data Reduction Proxy if the
+    # Data Saver API fetch is slow to finish. This test can't just use
+    # measurements.WaitForViaHeader(tab) since that would affect the results of
+    # the latency measurement, e.g. Chrome would have a hot proxy connection.
     options.AppendExtraBrowserArgs('--enable-spdy-proxy-auth')
 
 
@@ -67,6 +75,10 @@ class ChromeProxyDataSaving(ChromeProxyDataSavingBase):
 
   def CustomizeBrowserOptions(self, options):
     options.AppendExtraBrowserArgs('--enable-spdy-proxy-auth')
+
+  def WillNavigateToPage(self, page, tab):
+    measurements.WaitForViaHeader(tab)
+    super(ChromeProxyDataSaving, self).WillNavigateToPage(page, tab)
 
 
 class ChromeProxyDataSavingDirect(ChromeProxyDataSavingBase):
