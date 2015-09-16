c040f667729b0540d8b3a9c606b7c4fac7e85acd
primiano <primiano@chromium.org>
2015-09-16 11:59:07 -0700

    [tracing] Fix races in TraceLog's EnabledStateObserver
    This CL makes TraceLog Add/RemoveEnabledStateObserver thread safe
    as observers can be registered on different threads.
    Furthermore this CL fixes another race in
    ChildMemoryDumpManagerDelegateImpl, ensuring that the check to
    post the memory dump request on the right thread is itself
    thread-safe.
    
    BUG=530578
    
    Review URL: https://codereview.chromium.org/1348613006
    
    Cr-Commit-Position: refs/heads/master@{#349176}

--- components/tracing/child_memory_dump_manager_delegate_impl.h
+++ components/tracing/child_memory_dump_manager_delegate_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "base/memory/singleton.h"
+#include "base/synchronization/lock.h"
 
 namespace base {
 class SingleThreadTaskRunner;
@@ -64,6 +65,10 @@ class ChildMemoryDumpManagerDelegateImpl
   // The SingleThreadTaskRunner where the |ctmf_| lives.
   // It is NULL iff |cmtf_| is NULL.
   scoped_refptr<base::SingleThreadTaskRunner> ctmf_task_runner_;
+
+  // Protects from concurrent access to |ctmf_task_runner_| to allow
+  // RequestGlobalMemoryDump to be called from arbitrary threads.
+  base::Lock lock_;
 
   // The unique id of the child process, created for tracing and is expected to
   // be valid only when tracing is enabled.
