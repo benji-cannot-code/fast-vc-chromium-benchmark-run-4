9657fc49a87e5d90b2868ee5ef3c9d2b3c79a239
blundell <blundell@chromium.org>
2015-09-16 05:46:33 -0700

    Introduce IOSChromeSessionTabHelper
    This class is the //ios/chrome analogue to //chrome's SessionTabHelper.
    
    BUG=371476
    
    Review URL: https://codereview.chromium.org/1350693002
    
    Cr-Commit-Position: refs/heads/master@{#349104}

---
+++ ios/chrome/browser/sessions/ios_chrome_session_tab_helper.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/sessions/ios_chrome_session_tab_helper.h"
+
+DEFINE_WEB_STATE_USER_DATA_KEY(IOSChromeSessionTabHelper);
+
+IOSChromeSessionTabHelper::IOSChromeSessionTabHelper(web::WebState* web_state) {
+}
+
+IOSChromeSessionTabHelper::~IOSChromeSessionTabHelper() {}
+
+void IOSChromeSessionTabHelper::SetWindowID(const SessionID& id) {
+  window_id_ = id;
+}
