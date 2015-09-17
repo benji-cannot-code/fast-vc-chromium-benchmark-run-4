0f2d975e306c39534f004cca1ceed1f5ba79a4d3
nednguyen <nednguyen@google.com>
2015-09-16 19:04:25 -0700

    [Telemetry] Raise ValueError if no interaction was found.
    BUG=531696
    
    Review URL: https://codereview.chromium.org/1342923002
    
    Cr-Commit-Position: refs/heads/master@{#349317}

--- tools/telemetry/telemetry/web_perf/story_test.py
+++ tools/telemetry/telemetry/web_perf/story_test.py
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+
+class Failure(Exception):
+  """StoryTest Exception raised when an undesired but designed-for problem."""
+
+
 class StoryTest(object):
   """A class for creating story tests.
 
