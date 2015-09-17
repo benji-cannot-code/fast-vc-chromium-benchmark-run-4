f19ca8c49ea49c9c9cfdb3666684c09556d88241
tfarina <tfarina@chromium.org>
2015-09-17 00:10:19 -0700

    chrome/browser: Remove unneeded WebKit includes.
    The types of those includes are not being used and thus we can remove
    these includes from third_party/WebKit. Generally we try hard to avoid
    including from third_party/WebKit (Blink Rendering Engine) from
    chrome/browser. If needed, includes from WebKit should be usually done
    from chrome/renderer.
    
    BUG=237267
    R=thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1349453002
    
    Cr-Commit-Position: refs/heads/master@{#349362}

--- chrome/browser/chromeos/policy/consumer_management_notifier.cc
+++ chrome/browser/chromeos/policy/consumer_management_notifier.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
-#include "third_party/WebKit/public/web/WebTextDirection.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/page_transition_types.h"
 #include "ui/base/resource/resource_bundle.h"
--- chrome/browser/signin/signin_error_notifier_ash.cc
+++ chrome/browser/signin/signin_error_notifier_ash.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "grit/theme_resources.h"
-#include "third_party/WebKit/public/web/WebTextDirection.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/message_center/notification.h"
