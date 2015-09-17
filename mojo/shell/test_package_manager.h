a85beca89f208bdf0c06e16e503e0decfecb1ab4
yosin <yosin@chromium.org>
2015-09-17 00:03:24 -0700

    Compilation error in http://crrev.com/1352663002
    FAILED: /b/build/goma/gomacc ../../third_party/llvm-build/Release+Asserts/bin/clang++ -MMD -MF obj/mojo/shell/mojo_shell_unittests/test_package_manager.o.d ...omit... -c ../../mojo/shell/test_package_manager.cc -o obj/mojo/shell/mojo_shell_unittests/test_package_manager.o
    In file included from ../../mojo/shell/test_package_manager.cc:5:
    ../../mojo/shell/test_package_manager.h:23:32: error: [chromium-style] Overriding method must be marked with 'override' or 'final'.
      virtual ~TestPackageManager();
                                   ^
                                    override
    1 error generated.
    
    BUG=n/a
    TBR=yzshen@chromium.org,ben@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1351833002
    
    Cr-Commit-Position: refs/heads/master@{#349358}

--- mojo/shell/test_package_manager.h
+++ mojo/shell/test_package_manager.h
@@ -21,7 +21,7 @@ namespace shell {
 class TestPackageManager : public PackageManager {
  public:
   TestPackageManager();
-  virtual ~TestPackageManager();
+  ~TestPackageManager() override;
 
   void RegisterContentHandler(const std::string& mime_type,
                               const GURL& content_handler_url);
