7551dc60da54b8970638bebdbb998524b18c2150
thakis <thakis@chromium.org>
2015-09-16 19:40:38 -0700

    Revert of Mandoline: Fix View Manager tests (patchset #1 id:1 of https://codereview.chromium.org/1350973002/ )
    Reason for revert:
    looks like the fix didn't help
    
    Original issue's description:
    > Mandoline: Fix View Manager tests
    >
    > BUG=531533
    > TBR=sky@chromium.org
    > NOTRY=true
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    >
    > Committed: https://crrev.com/9e5344fa946efeabc99e32d97c44b7c275a052c5
    > Cr-Commit-Position: refs/heads/master@{#349279}
    
    TBR=fsamuel@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=531533
    
    Review URL: https://codereview.chromium.org/1353653002
    
    Cr-Commit-Position: refs/heads/master@{#349329}

--- components/mus/main.cc
+++ components/mus/main.cc
@@ -7,7 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/application/public/cpp/application_runner.h"
 #include "third_party/mojo/src/mojo/public/c/system/main.h"
 
+namespace mus {
+
 MojoResult MojoMain(MojoHandle shell_handle) {
-  mojo::ApplicationRunner runner(new mus::MandolineUIServicesApp);
+  mojo::ApplicationRunner runner(new MandolineUIServicesApp);
   return runner.Run(shell_handle);
 }
+
+}  // namespace mus
