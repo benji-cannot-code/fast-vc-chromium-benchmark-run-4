60ba6a8c2e2f6ba848dca2690339d7674d5ee7fc
aurimas <aurimas@chromium.org>
2015-09-17 15:00:39 -0700

    Add a flag to enable theme-color in tabbed mode of Chrome.
    BUG=482207
    TBR=jwd@chromium.org
    
    Review URL: https://codereview.chromium.org/1351623002
    
    Cr-Commit-Position: refs/heads/master@{#349507}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
@@ -25,6 +25,7 @@ import org.chromium.base.ActivityState;
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ApplicationState;
 import org.chromium.base.ApplicationStatus;
+import org.chromium.base.CommandLine;
 import org.chromium.base.ObserverList;
 import org.chromium.base.ObserverList.RewindableIterator;
 import org.chromium.base.TraceEvent;
@@ -35,6 +36,7 @@ import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.AccessibilityUtil;
 import org.chromium.chrome.browser.ChromeActivity;
+import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.ChromeWebContentsDelegateAndroid;
 import org.chromium.chrome.browser.FrozenNativePage;
 import org.chromium.chrome.browser.IntentHandler.TabOpenType;
@@ -778,7 +780,10 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
                 color = mDefaultThemeColor;
             }
             if (isShowingInterstitialPage()) color = mDefaultThemeColor;
-            if (!FeatureUtilities.isDocumentMode(mApplicationContext)) color = mDefaultThemeColor;
+            if (!FeatureUtilities.isDocumentMode(mApplicationContext)
+                    && !isThemeColorEnabledInTabbedMode()) {
+                color = mDefaultThemeColor;
+            }
             if (color == Color.TRANSPARENT) color = mDefaultThemeColor;
             color |= 0xFF000000;
             if (mThemeColor == color) return;
@@ -2911,6 +2916,11 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
         intent.putExtra(TabOpenType.BRING_TAB_TO_FRONT.name(), tabId);
         intent.setPackage(packageName);
         return intent;
+    }
+
+    private static boolean isThemeColorEnabledInTabbedMode() {
+        CommandLine commandLine = CommandLine.getInstance();
+        return commandLine.hasSwitch(ChromeSwitches.ENABLE_THEME_COLOR_IN_TABBED_MODE);
     }
 
     /**
