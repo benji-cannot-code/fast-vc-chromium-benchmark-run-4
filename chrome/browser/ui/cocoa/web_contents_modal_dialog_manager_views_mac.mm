4d9738feba3c0edaf1f4c8b1e654db4f2abedaf5
tapted <tapted@chromium.org>
2015-09-17 16:23:26 -0700

    MacViews: Restore the show animation on toolkit-views WebModal dialogs
    Currently the views dialogs just pop in.
    
    This regressed in r347690 which moved some of the hide/show
    responsibility from the ConstrainedWindowSheetController to the
    cross-platform web_modal APIs.
    
    The refactoring actually tickled the existing toolkit-views
    WebContentsModalDialogManagerViewsMacTest browser tests, but the fix
    wasn't quite right. The fix restored the visibility states, but the
    initial show was missing the animation (hard to test for..).
    
    To fix, detect the initial show and use Widget::Show() for that. We
    don't always use Widget::Show() because it interferes with hide/unhide
    sequences when switching tabs. For those cases, a re-show shouldn't
    animate.
    
    BUG=485854
    
    Review URL: https://codereview.chromium.org/1349113002
    
    Cr-Commit-Position: refs/heads/master@{#349535}

--- chrome/browser/ui/cocoa/web_contents_modal_dialog_manager_views_mac.mm
+++ chrome/browser/ui/cocoa/web_contents_modal_dialog_manager_views_mac.mm
@@ -83,7 +83,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)makeSheetKeyAndOrderFront {
-  [customWindow_ makeKeyAndOrderFront:nil];
+  // If the window is not visible, do nothing. Widget::Show() is responsible for
+  // showing, and it may want to animate it.
+  if ([customWindow_ isVisible])
+    [customWindow_ makeKeyAndOrderFront:nil];
 }
 
 - (void)updateSheetPosition {
@@ -130,6 +133,9 @@ void SingleWebContentsDialogManagerViewsMac::Show() {
   [[ConstrainedWindowSheetController controllerForParentWindow:parent_window]
           showSheet:sheet_
       forParentView:parent_view];
+
+  if (!widget_->IsVisible())
+    widget_->Show();
 }
 
 void SingleWebContentsDialogManagerViewsMac::Hide() {
