bf30b71554a4fd1a1efa7f77b46773fd4f6ed77d
yosin@chromium.org <yosin@chromium.org>
2015-09-17 04:33:13 +0000

    Introduce equalSelectionsInDOMTree() to replace of VisibleSlection::InDOMTree::equalSelections()
    This patch introduces |equalSelectionsInDOMTree()| to replace
    |VisibleSlection::InDOMTree::equalSelections()| as preparation of introducing
    |VisibleSlectionTemplate| for reducing patch size rather than replacing at
    once.
    
    This patch is a preparation of making selection to handle granularity for web
    component, http://crrev.com/1277863002
    
    BUG=513568
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1346813004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202426 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/SelectionController.cpp
+++ third_party/WebKit/Source/core/editing/SelectionController.cpp
@@ -69,7 +69,7 @@ namespace {
 
 void setSelectionIfNeeded(FrameSelection& selection, const VisibleSelection& newSelection)
 {
-    if (VisibleSelection::InDOMTree::equalSelections(selection.selection(), newSelection))
+    if (equalSelectionsInDOMTree(selection.selection(), newSelection))
         return;
     selection.setSelection(newSelection);
 }
