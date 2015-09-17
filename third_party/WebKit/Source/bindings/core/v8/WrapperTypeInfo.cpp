a8b01fe18ad313d80c6eb93bfbd47e3c5f0766d7
yukishiino@chromium.org <yukishiino@chromium.org>
2015-09-17 06:42:09 +0000

    bindings/oilpan: Stops casting to ScriptWrappable* in DOMWrapperMap.
    CFI(Control Flow Integrity) detects bad casting to ScriptWrappable*
    from void* which points to a dead object.  We need almost the same fix
    as http://crrev.com/1340513002 in DOMWrapperMap, too.
    
    BUG=531057
    
    Review URL: https://codereview.chromium.org/1336323002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202431 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/core/v8/WrapperTypeInfo.cpp
+++ third_party/WebKit/Source/bindings/core/v8/WrapperTypeInfo.cpp
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+static_assert(offsetof(struct WrapperTypeInfo, ginEmbedder) == offsetof(struct gin::WrapperInfo, embedder), "offset of WrapperTypeInfo.ginEmbedder must be the same as gin::WrapperInfo.embedder");
+
 EventTarget* WrapperTypeInfo::toEventTarget(v8::Local<v8::Object> object) const
 {
     if (eventTargetInheritance == NotInheritFromEventTarget)
