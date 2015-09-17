6813acfa69260de899eb93ba3c76c7157224242c
felt <felt@chromium.org>
2015-09-17 13:33:59 -0700

    Create a component for SSL error handling
    Classes like SSLErrorInfo and SSLErrorClassification are
    tightly coupled with the SSL interstitial. Since the interstitial UI is being
    componentized, I want to move the SSL error handling classes into a component
    too. This CL begins with SSLErrorInfo.
    
    BUG=488673
    TBR=stuartmorgan@chromium.org
    
    Committed: https://crrev.com/7f1410648c7767ae8d466ada4385e78176f5db06
    Cr-Commit-Position: refs/heads/master@{#349282}
    
    Review URL: https://codereview.chromium.org/1341923002
    
    Cr-Commit-Position: refs/heads/master@{#349486}

--- chrome/common/localized_error.cc
+++ chrome/common/localized_error.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/url_formatter/url_formatter.h"
 #include "grit/components_chromium_strings.h"
 #include "grit/components_google_chrome_strings.h"
+#include "grit/components_strings.h"
 #include "net/base/escape.h"
 #include "net/base/net_errors.h"
 #include "third_party/WebKit/public/platform/WebURLError.h"
@@ -269,8 +270,8 @@ const LocalizedErrorMap net_error_options[] = {
   {net::ERR_SSL_PINNED_KEY_NOT_IN_CERT_CHAIN,
    IDS_ERRORPAGES_TITLE_LOAD_FAILED,
    IDS_ERRORPAGES_HEADING_PINNING_FAILURE,
-   IDS_ERRORPAGES_SUMMARY_PINNING_FAILURE,
-   IDS_ERRORPAGES_DETAILS_PINNING_FAILURE,
+   IDS_CERT_ERROR_SUMMARY_PINNING_FAILURE_DETAILS,
+   IDS_CERT_ERROR_SUMMARY_PINNING_FAILURE_DESCRIPTION,
    SUGGEST_NONE,
   },
   {net::ERR_TEMPORARILY_THROTTLED,
