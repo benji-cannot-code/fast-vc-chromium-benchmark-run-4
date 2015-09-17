f19efb1e695e421224a88f19f0281c2f7fe2ebe6
raymes <raymes@chromium.org>
2015-09-17 00:15:50 -0700

    Change the print preview UI to use the material design style buttons for zoom
    This changes print preview to use the material buttons for zooming. It also
    reconciles the codepaths so that print preview uses the same codepath as
    the material design viewer which will allow us to remove a bunch of code once
    the material UI is switched on by default.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1332273003
    
    Cr-Commit-Position: refs/heads/master@{#349371}

--- chrome/browser/resources/pdf/toolbar_manager.js
+++ chrome/browser/resources/pdf/toolbar_manager.js
@@ -69,7 +69,7 @@ function ToolbarManager(window, toolbar, zoomToolbar) {
 ToolbarManager.prototype = {
 
   showToolbarsForMouseMove: function(e) {
-    this.isMouseNearTopToolbar_ = isMouseNearTopToolbar(e);
+    this.isMouseNearTopToolbar_ = this.toolbar_ && isMouseNearTopToolbar(e);
     this.isMouseNearSideToolbar_ = isMouseNearSideToolbar(e);
 
     // Allow the top toolbar to be shown if the mouse moves away from the side
@@ -97,7 +97,8 @@ ToolbarManager.prototype = {
    * Display both UI toolbars.
    */
   showToolbars: function() {
-    this.toolbar_.show();
+    if (this.toolbar_)
+      this.toolbar_.show();
     this.zoomToolbar_.show();
   },
 
@@ -107,11 +108,15 @@ ToolbarManager.prototype = {
    * elements.
    */
   hideToolbarsIfAllowed: function() {
-    if (!(this.isMouseNearTopToolbar_ || this.isMouseNearSideToolbar_ ||
-        this.toolbar_.shouldKeepOpen())) {
+    if (this.isMouseNearSideToolbar_ || this.isMouseNearTopToolbar_)
+      return;
+
+    if (this.toolbar_ && this.toolbar_.shouldKeepOpen())
+      return;
+
+    if (this.toolbar_)
       this.toolbar_.hide();
-      this.zoomToolbar_.hide();
-    }
+    this.zoomToolbar_.hide();
   },
 
   /**
@@ -129,7 +134,7 @@ ToolbarManager.prototype = {
    * hides the basic toolbars otherwise.
    */
   hideSingleToolbarLayer: function() {
-    if (!this.toolbar_.hideDropdowns())
+    if (!this.toolbar_ || !this.toolbar_.hideDropdowns())
       this.hideToolbarsIfAllowed();
   },
 
@@ -142,6 +147,8 @@ ToolbarManager.prototype = {
    * of the screen.
    */
   forceHideTopToolbar: function() {
+    if (!this.toolbar_)
+      return;
     this.toolbar_.hide();
     this.sideToolbarAllowedOnly_ = true;
     this.sideToolbarAllowedOnlyTimer_ = this.window_.setTimeout(function() {
@@ -155,6 +162,8 @@ ToolbarManager.prototype = {
    * @private
    */
   resizeDropdowns_: function() {
+    if (!this.toolbar_)
+      return;
     var lowerBound = this.window_.innerHeight - this.zoomToolbar_.clientHeight;
     this.toolbar_.setDropdownLowerBound(lowerBound);
   }
