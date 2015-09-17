526506e19f9c346e89aecd81cc09f71c2e711197
simonb <simonb@chromium.org>
2015-09-17 07:58:20 -0700

    Ensure low-memory renderers retry failed loads correctly.
    LibraryLoader uses isUsingBrowserSharedRelros() to decide whether to retry
    a fixed address load for the browser. On a low-memory target, this function
    will currently return true for both browser and renderer processes. This
    means that any renderer fixed load retry for low-memory will also occur in
    LibraryLoader, rather than in ChildProcessService.
    
    This works, but a side effect is that UMA statistics for failed renderer
    fixed address load are not updated correctly by ChildProcessService (such
    an event will however not register as a failed browser load either,
    because the function to push UMA values on browser load is not called for
    a child process).
    
    Fixed by making isUsingBrowserSharedRelros() return false if not in the
    browser process, regardless of any low-memory/normal-memory setting.
    
    Additionally, make ...ForTesting functions final, and clean up a few
    portions of testing code for tighter operation.
    
    BUG=460499
    
    Review URL: https://codereview.chromium.org/1349673003
    
    Cr-Commit-Position: refs/heads/master@{#349420}

--- base/android/java/src/org/chromium/base/library_loader/LegacyLinker.java
+++ base/android/java/src/org/chromium/base/library_loader/LegacyLinker.java
@@ -127,7 +127,7 @@ class LegacyLinker extends Linker {
     public boolean isUsingBrowserSharedRelros() {
         synchronized (mLock) {
             ensureInitializedLocked();
-            return mBrowserUsesSharedRelro;
+            return mInBrowserProcess && mBrowserUsesSharedRelro;
         }
     }
 
