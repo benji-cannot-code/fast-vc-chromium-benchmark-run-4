4200a766a72319abe45dbf8d0144f90382c469b8
alancutter@chromium.org <alancutter@chromium.org>
2015-09-17 01:21:10 +0000

    Add composite() method to InterpolationType interface
    This change refactors composition of InterpolationValues to be handled by
    the InterpolationType::composite() virtual method.
    This allows different InterpolationTypes to handle composition of their
    InterpolationValues in their own way e.g. lists of shadows may force
    underlying values to become repeated to a lowest common multiple.
    
    This introduces no change in behaviour.
    
    BUG=437696
    
    Review URL: https://codereview.chromium.org/1322123003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/InterpolationType.h
+++ third_party/WebKit/Source/core/animation/InterpolationType.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/animation/NonInterpolableValue.h"
 #include "core/animation/PrimitiveInterpolation.h"
 #include "core/animation/StringKeyframe.h"
+#include "core/animation/UnderlyingValue.h"
 #include "platform/heap/Handle.h"
 #include "wtf/Allocator.h"
 
@@ -48,12 +49,7 @@ public:
         OwnPtr<InterpolationValue> endValue = maybeConvertSingle(endKeyframe, state, conversionCheckers);
         if (!endValue)
             return nullptr;
-        ASSERT(!startValue->nonInterpolableValue());
-        ASSERT(!endValue->nonInterpolableValue());
-        return PairwisePrimitiveInterpolation::create(*this,
-            startValue->mutableComponent().interpolableValue.release(),
-            endValue->mutableComponent().interpolableValue.release(),
-            nullptr);
+        return mergeSingleConversions(*startValue, *endValue);
     }
 
     virtual PassOwnPtr<InterpolationValue> maybeConvertSingle(const CSSPropertySpecificKeyframe& keyframe, const StyleResolverState* state, ConversionCheckers& conversionCheckers) const
@@ -70,6 +66,13 @@ public:
 
     virtual PassOwnPtr<InterpolationValue> maybeConvertUnderlyingValue(const StyleResolverState&) const = 0;
 
+    virtual void composite(UnderlyingValue& underlyingValue, double underlyingFraction, const InterpolationValue& value) const
+    {
+        ASSERT(!underlyingValue->nonInterpolableValue());
+        ASSERT(!value.nonInterpolableValue());
+        underlyingValue.mutableComponent().interpolableValue->scaleAndAdd(underlyingFraction, value.interpolableValue());
+    }
+
     virtual void apply(const InterpolableValue&, const NonInterpolableValue*, StyleResolverState&) const = 0;
 
     // Implement reference equality checking via pointer equality checking as these are singletons
@@ -85,6 +88,16 @@ protected:
     virtual PassOwnPtr<InterpolationValue> maybeConvertInitial() const { ASSERT_NOT_REACHED(); return nullptr; }
     virtual PassOwnPtr<InterpolationValue> maybeConvertInherit(const StyleResolverState* state, ConversionCheckers& conversionCheckers) const { ASSERT_NOT_REACHED(); return nullptr; }
     virtual PassOwnPtr<InterpolationValue> maybeConvertValue(const CSSValue& value, const StyleResolverState* state, ConversionCheckers& conversionCheckers) const { ASSERT_NOT_REACHED(); return nullptr; }
+
+    virtual PassOwnPtr<PairwisePrimitiveInterpolation> mergeSingleConversions(InterpolationValue& startValue, InterpolationValue& endValue) const
+    {
+        ASSERT(!startValue.nonInterpolableValue());
+        ASSERT(!endValue.nonInterpolableValue());
+        return PairwisePrimitiveInterpolation::create(*this,
+            startValue.mutableComponent().interpolableValue.release(),
+            endValue.mutableComponent().interpolableValue.release(),
+            nullptr);
+    }
 
     const CSSPropertyID m_property;
 };
--- third_party/WebKit/Source/core/core.gypi
+++ third_party/WebKit/Source/core/core.gypi
@@ -931,6 +931,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'animation/TimingInput.h',
             'animation/TransformSVGInterpolation.cpp',
             'animation/TransformSVGInterpolation.h',
+            'animation/UnderlyingValue.h',
             'animation/VisibilityStyleInterpolation.cpp',
             'animation/VisibilityStyleInterpolation.h',
             'animation/animatable/AnimatableClipPathOperation.cpp',
