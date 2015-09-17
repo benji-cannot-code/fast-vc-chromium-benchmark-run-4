d6436df66ce8c79dcf9734d8c6a68751105fd61e
sky <sky@chromium.org>
2015-09-17 16:20:06 -0700

    Adds ability to pause processing of incoming messages to Binding
    BUG=533077
    TEST=covered by tests
    R=yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/1349303002
    
    Cr-Commit-Position: refs/heads/master@{#349532}

--- third_party/mojo/src/mojo/public/cpp/bindings/lib/connector.h
+++ third_party/mojo/src/mojo/public/cpp/bindings/lib/connector.h
@@ -72,6 +72,10 @@ class Connector : public MessageReceiver {
   // been delivered, |false| otherwise.
   bool WaitForIncomingMessage(MojoDeadline deadline);
 
+  // See Binding for details of pause/resume.
+  void PauseIncomingMethodCallProcessing();
+  void ResumeIncomingMethodCallProcessing();
+
   // MessageReceiver implementation:
   bool Accept(Message* message) override;
 
@@ -104,6 +108,8 @@ class Connector : public MessageReceiver {
   bool error_;
   bool drop_writes_;
   bool enforce_errors_from_incoming_receiver_;
+
+  bool paused_;
 
   // If non-null, this will be set to true when the Connector is destroyed.  We
   // use this flag to allow for the Connector to be destroyed as a side-effect
