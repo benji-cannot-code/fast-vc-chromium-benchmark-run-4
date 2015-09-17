b682d18259287a6f4e358e5f02b87ac18008f3ad
peconn <peconn@chromium.org>
2015-09-17 10:28:59 -0700

    If a website becomes blocked while the user is viewing it, block it.
    BUG=527763
    
    Review URL: https://codereview.chromium.org/1342543006
    
    Cr-Commit-Position: refs/heads/master@{#349440}

--- chrome/browser/supervised_user/supervised_user_navigation_observer.h
+++ chrome/browser/supervised_user/supervised_user_navigation_observer.h
@@ -9,9 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/scoped_vector.h"
+#include "chrome/browser/supervised_user/supervised_user_service_observer.h"
 #include "chrome/browser/supervised_user/supervised_user_url_filter.h"
 #include "chrome/browser/supervised_user/supervised_users.h"
 #include "content/public/browser/web_contents_user_data.h"
+
+class SupervisedUserService;
 
 namespace content {
 class NavigationEntry;
@@ -19,7 +22,8 @@ class WebContents;
 }
 
 class SupervisedUserNavigationObserver
-    : public content::WebContentsUserData<SupervisedUserNavigationObserver> {
+    : public content::WebContentsUserData<SupervisedUserNavigationObserver>,
+      public SupervisedUserServiceObserver {
  public:
   ~SupervisedUserNavigationObserver() override;
 
@@ -36,6 +40,15 @@ class SupervisedUserNavigationObserver
       SupervisedUserURLFilter::FilteringBehaviorReason reason,
       const base::Callback<void(bool)>& callback);
 
+  // SupervisedUserServiceObserver implementation.
+  void OnURLFilterChanged() override;
+
+  void URLFilterCheckCallback(
+      const GURL& url,
+      SupervisedUserURLFilter::FilteringBehavior behavior,
+      SupervisedUserURLFilter::FilteringBehaviorReason reason,
+      bool uncertain);
+
  private:
   friend class content::WebContentsUserData<SupervisedUserNavigationObserver>;
 
@@ -48,7 +61,12 @@ class SupervisedUserNavigationObserver
   // Owned by SupervisedUserService.
   const SupervisedUserURLFilter* url_filter_;
 
+  // Owned by SupervisedUserServiceFactory (lifetime of Profile).
+  SupervisedUserService* supervised_user_service_;
+
   ScopedVector<const content::NavigationEntry> blocked_navigations_;
+
+  base::WeakPtrFactory<SupervisedUserNavigationObserver> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(SupervisedUserNavigationObserver);
 };
