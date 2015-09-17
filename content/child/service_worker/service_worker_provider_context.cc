501b1d629f0c9850904bb0bedc5d5795f3ee3758
nhiroki <nhiroki@chromium.org>
2015-09-16 17:30:58 -0700

    ServiceWorker: Stop ServiceWorkerProviderContext from tracking SetVersionAttributes events
    This is a cleanup CL. ServiceWorkerProviderContext does not have to track
    SetVersionAttributes events:
    
    * For ServiceWorkerGlobalScope, SetVersionAttributes events are queued up in
    ServiceWorkerProviderHost until the worker thread is started and then the events
    are directly delivered to WebServiceWorkerRegistrationImpl.
    
    * For (controlled) Document, the context does not care versions of the
    associated registration.
    
    BUG=532098
    
    Review URL: https://codereview.chromium.org/1343113002
    
    Cr-Commit-Position: refs/heads/master@{#349293}

--- content/child/service_worker/service_worker_provider_context.cc
+++ content/child/service_worker/service_worker_provider_context.cc
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/child/service_worker/service_worker_provider_context.h"
 
-#include "base/bind.h"
-#include "base/location.h"
-#include "base/stl_util.h"
 #include "base/thread_task_runner_handle.h"
 #include "content/child/child_thread_impl.h"
 #include "content/child/service_worker/service_worker_dispatcher.h"
@@ -15,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/child/service_worker/service_worker_registration_handle_reference.h"
 #include "content/child/thread_safe_sender.h"
 #include "content/child/worker_task_runner.h"
-#include "content/common/service_worker/service_worker_messages.h"
 
 namespace content {
 
@@ -63,27 +59,6 @@ bool ServiceWorkerProviderContext::GetRegistrationInfoAndVersionAttributes(
   return true;
 }
 
-void ServiceWorkerProviderContext::SetVersionAttributes(
-    ChangedVersionAttributesMask mask,
-    const ServiceWorkerVersionAttributes& attrs) {
-  base::AutoLock lock(lock_);
-  DCHECK(main_thread_task_runner_->RunsTasksOnCurrentThread());
-  DCHECK(registration_);
-
-  if (mask.installing_changed()) {
-    installing_ = ServiceWorkerHandleReference::Adopt(
-        attrs.installing, thread_safe_sender_.get());
-  }
-  if (mask.waiting_changed()) {
-    waiting_ = ServiceWorkerHandleReference::Adopt(
-        attrs.waiting, thread_safe_sender_.get());
-  }
-  if (mask.active_changed()) {
-    active_ = ServiceWorkerHandleReference::Adopt(
-        attrs.active, thread_safe_sender_.get());
-  }
-}
-
 void ServiceWorkerProviderContext::OnAssociateRegistration(
     const ServiceWorkerRegistrationObjectInfo& info,
     const ServiceWorkerVersionAttributes& attrs) {
@@ -126,18 +101,6 @@ void ServiceWorkerProviderContext::OnSetControllerServiceWorker(
 
   // TODO(kinuko): We can forward the message to other threads here
   // when we support navigator.serviceWorker in dedicated workers.
-}
-
-int ServiceWorkerProviderContext::controller_handle_id() const {
-  DCHECK(main_thread_task_runner_->RunsTasksOnCurrentThread());
-  return controller_ ? controller_->info().handle_id
-                     : kInvalidServiceWorkerHandleId;
-}
-
-int ServiceWorkerProviderContext::registration_handle_id() const {
-  DCHECK(main_thread_task_runner_->RunsTasksOnCurrentThread());
-  return registration_ ? registration_->info().handle_id
-                       : kInvalidServiceWorkerRegistrationHandleId;
 }
 
 void ServiceWorkerProviderContext::DestructOnMainThread() const {
--- content/renderer/shared_worker/embedded_shared_worker_stub.cc
+++ content/renderer/shared_worker/embedded_shared_worker_stub.cc
@@ -91,8 +91,7 @@ class WebServiceWorkerNetworkProviderImpl
       blink::WebDataSource& data_source) {
     ServiceWorkerNetworkProvider* provider =
         GetNetworkProviderFromDataSource(&data_source);
-    return provider->context()->controller_handle_id() !=
-        kInvalidServiceWorkerHandleId;
+    return provider->context()->controller() != nullptr;
   }
 
   virtual int64_t serviceWorkerID(
