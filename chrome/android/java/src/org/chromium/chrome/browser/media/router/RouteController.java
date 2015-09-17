51f3c15f565ca3b64cde41b0c0f9a3d7845876a2
avayvod <avayvod@chromium.org>
2015-09-17 07:00:21 -0700

    [Presentation API, Android] Dependency injections for providing and controlling Cast routes
    ChromeMediaRouter now maintains a list of MediaRouteProvider instances and redirects calls
    to the appropriate provider.
    Cast specific logic has moved to CastMediaRouteProvider that implements the MediaRouteProvider interface and talks to ChromeMediaRouter via MediaRouteManager.
    It also implements DiscoveryDelegate to get updates from DiscoveryCallback and RouteDelegate
    to get updates from both CreateRouteRequest and SessionWrapper.
    The latter implements RouteController interface.
    
    Also, CastMediaRouteProvider is not added to ChromeMediaRouter if Android support library doesn't provide the MediaRouter service. And Cast.Listener implementation has moved to CreateRouteRequest.
    
    BUG=None
    TESTED=Verified that the Cast test page and YouTube experiment still work as before.
    
    Review URL: https://codereview.chromium.org/1349433002
    
    Cr-Commit-Position: refs/heads/master@{#349412}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/media/router/RouteController.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.media.router;
+
+/**
+ * Interface defining operations related to a single {@link MediaRoute}.
+ */
+public interface RouteController {
+    /**
+     * Close the route.
+     */
+    void close();
+
+    /**
+     * Send a string message to the route and invokes the {@link RouteDelegate} with the
+     * passed callback id on success or failure.
+     * @param message The message to send.
+     * @param nativeCallbackId The id of the callback handling the result.
+     */
+    void sendStringMessage(String message, int nativeCallbackId);
+}
