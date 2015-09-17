580d5221d018db3931865d8f2aa467513bc9e276
stevenjb <stevenjb@chromium.org>
2015-09-17 16:55:15 -0700

    Use chrome.management to manage the list of third-party VPN providers.
    * Uses chrome.management instead of custom chrome.send calls in
      current Settings UI to get the list of third-party VPN
      providers.
    
    BUG=516796
    For third_party/closure_compiler/externs/management.js (kalman@ should probably be an owner):
    TBR=dbeam@
    
    Review URL: https://codereview.chromium.org/1292053004
    
    Cr-Commit-Position: refs/heads/master@{#349544}

--- chrome/browser/ui/webui/options/chromeos/internet_options_handler_strings.cc
+++ chrome/browser/ui/webui/options/chromeos/internet_options_handler_strings.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/chromeos/strings/grit/ui_chromeos_strings.h"
 
 namespace chromeos {
 namespace internet_options_strings {
@@ -32,6 +33,7 @@ StringResource kStringResources[] = {
      IDS_OPTIONS_SETTINGS_SECTION_THIRD_PARTY_VPN_NAME_TEMPLATE},
     {"defaultThirdPartyProviderName",
      IDS_OPTIONS_SETTINGS_SECTION_DEFAULT_THIRD_PARTY_PROVIDER_NAME},
+    {"vpnBuiltInProvider", IDS_NETWORK_VPN_BUILT_IN_PROVIDER},
     {"joinOtherNetwork", IDS_OPTIONS_SETTINGS_NETWORK_OTHER},
     {"networkDisabled", IDS_OPTIONS_SETTINGS_NETWORK_DISABLED},
     {"turnOffWifi", IDS_OPTIONS_SETTINGS_NETWORK_DISABLE_WIFI},
