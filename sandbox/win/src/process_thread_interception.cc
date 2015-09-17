51e14d0fdda854b881d5cc44eab7045669b9819f
brucedawson <brucedawson@chromium.org>
2015-09-17 13:21:44 -0700

    Fixes to possible GetLastError bugs
    GetLastError needs to be called immediately after the function whose
    error code it is retrieving. This has been made particularly important
    by VS 2015 which sometimes clears the error code when allocating memory.
    See bug 528394 for details of the underlying issue.
    
    These changes came from code inspection that looked for patterns that
    appeared wrong. None of the issues look critical, although that depends
    on what callers do with the error codes.
    
    BUG=529981
    
    Review URL: https://codereview.chromium.org/1337223002
    
    Cr-Commit-Position: refs/heads/master@{#349481}

--- sandbox/win/src/process_thread_interception.cc
+++ sandbox/win/src/process_thread_interception.cc
@@ -280,6 +280,8 @@ BOOL WINAPI TargetCreateProcessW(CreateProcessWFunction orig_CreateProcessW,
   if (!SandboxFactory::GetTargetServices()->GetState()->InitCalled())
     return FALSE;
 
+  // Don't call GetLastError before InitCalled() succeeds because kernel32 may
+  // not be mapped yet.
   DWORD original_error = ::GetLastError();
 
   do {
@@ -339,6 +341,8 @@ BOOL WINAPI TargetCreateProcessA(CreateProcessAFunction orig_CreateProcessA,
   if (!SandboxFactory::GetTargetServices()->GetState()->InitCalled())
     return FALSE;
 
+  // Don't call GetLastError before InitCalled() succeeds because kernel32 may
+  // not be mapped yet.
   DWORD original_error = ::GetLastError();
 
   do {
--- sandbox/win/src/restricted_token.cc
+++ sandbox/win/src/restricted_token.cc
@@ -141,6 +141,7 @@ DWORD RestrictedToken::GetRestrictedToken(
                                 SecurityIdentification, TokenPrimary,
                                 &new_token_handle);
   }
+  auto last_error = ::GetLastError();
 
   if (deny_only_array)
     delete[] deny_only_array;
@@ -152,7 +153,7 @@ DWORD RestrictedToken::GetRestrictedToken(
     delete[] privileges_to_disable_array;
 
   if (!result)
-    return ::GetLastError();
+    return last_error;
 
   base::win::ScopedHandle new_token(new_token_handle);
 
