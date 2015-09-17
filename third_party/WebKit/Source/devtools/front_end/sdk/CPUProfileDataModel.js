2e23fcc78c76c2c5f1e519dce09d8ee261d7c1ea
alph@chromium.org <alph@chromium.org>
2015-09-17 02:13:45 +0000

    DevTools: Add option to show native function in JS profile.
    Review URL: https://codereview.chromium.org/1341423004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202414 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/CPUProfileDataModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/CPUProfileDataModel.js
@@ -22,7 +22,8 @@ WebInspector.CPUProfileDataModel = function(profile)
         this._fixMissingSamples();
         this._fixLineAndColumnNumbers();
     }
-    this._filterNativeFrames();
+    if (!WebInspector.moduleSetting("showNativeFunctionsInJSProfile").get())
+        this._filterNativeFrames();
     this._assignDepthsInProfile();
     this._calculateTimes(profile);
 }
