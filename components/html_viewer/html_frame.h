600ac1469d39ee2af760b49f2601d527207d0373
sky <sky@chromium.org>
2015-09-17 15:28:51 -0700

    Renames variable to better indicate use and removes TODO
    Also updates a TODO as the old one is out of date, but there is a new
    one (*SIGH*).
    
    BUG=479172,490221
    TEST=none
    R=fsamuel@chromium.org
    
    Review URL: https://codereview.chromium.org/1352033002
    
    Cr-Commit-Position: refs/heads/master@{#349522}

--- components/html_viewer/html_frame.h
+++ components/html_viewer/html_frame.h
@@ -85,7 +85,7 @@ class HTMLFrame : public blink::WebFrameClient,
           view(view),
           properties(properties),
           delegate(delegate),
-          allow_local_shared_frame(false) {}
+          is_local_create_child(false) {}
     ~CreateParams() {}
 
     HTMLFrameTreeManager* manager;
@@ -98,8 +98,9 @@ class HTMLFrame : public blink::WebFrameClient,
    private:
     friend class HTMLFrame;
 
-    // TODO(sky): nuke.
-    bool allow_local_shared_frame;
+    // True if the creation is the result of createChildFrame(). That is, a
+    // local parent is creating a new child frame.
+    bool is_local_create_child;
   };
 
   explicit HTMLFrame(CreateParams* params);
