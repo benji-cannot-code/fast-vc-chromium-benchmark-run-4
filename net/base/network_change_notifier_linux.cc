ccdaef50f4c1cfe3c849a138809f2af84a0cf472
jkarlin <jkarlin@chromium.org>
2015-09-18 08:14:07 -0700

    This CL adds ConnectionType to the MaxBandwidthObserver notification as a convenience for its only client (in the downstream CL), NetInfo.
    This CL also ensures that MaxBandwidthObserver is called on every platform when the network type changes.
    
    Upstream of https://codereview.chromium.org/1306423004/
    
    BUG=412741
    
    Review URL: https://codereview.chromium.org/1306653003
    
    Cr-Commit-Position: refs/heads/master@{#349673}

--- net/base/network_change_notifier_linux.cc
+++ net/base/network_change_notifier_linux.cc
@@ -83,6 +83,11 @@ void NetworkChangeNotifierLinux::Thread::OnLinkChanged() {
   if (last_type_ != GetCurrentConnectionType()) {
     NetworkChangeNotifier::NotifyObserversOfConnectionTypeChange();
     last_type_ = GetCurrentConnectionType();
+    double max_bandwidth_mbps =
+        NetworkChangeNotifier::GetMaxBandwidthForConnectionSubtype(
+            last_type_ == CONNECTION_NONE ? SUBTYPE_NONE : SUBTYPE_UNKNOWN);
+    NetworkChangeNotifier::NotifyObserversOfMaxBandwidthChange(
+        max_bandwidth_mbps, last_type_);
   }
 }
 
