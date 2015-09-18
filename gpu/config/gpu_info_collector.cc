94a95563f79209e55ee77f67ac54a500fa7d68b2
senorblanco <senorblanco@chromium.org>
2015-09-18 09:18:03 -0700

    Add a new histogram for Max. MSAA sample count, and collection for it.
    BUG=533095
    
    Review URL: https://codereview.chromium.org/1349303003
    
    Cr-Commit-Position: refs/heads/master@{#349686}

--- gpu/config/gpu_info_collector.cc
+++ gpu/config/gpu_info_collector.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/metrics/sparse_histogram.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
@@ -105,6 +106,7 @@ CollectInfoResult CollectGraphicsInfoGL(GPUInfo* gpu_info) {
   GLint max_samples = 0;
   glGetIntegerv(GL_MAX_SAMPLES, &max_samples);
   gpu_info->max_msaa_samples = base::IntToString(max_samples);
+  UMA_HISTOGRAM_SPARSE_SLOWLY("GPU.MaxMSAASampleCount", max_samples);
 
   gfx::GLWindowSystemBindingInfo window_system_binding_info;
   if (GetGLWindowSystemBindingInfo(&window_system_binding_info)) {
