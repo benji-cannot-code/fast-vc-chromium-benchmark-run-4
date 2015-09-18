e26811bf9e87a5ee3cc66813d26ea4e7fdf817a8
ben <ben@chromium.org>
2015-09-17 19:46:40 -0700

    Some more minor cleanup to ApplicationManager.
    R=yzshen@chromium.org
    http://crbug.com/533085
    
    Committed: https://crrev.com/c6bde172a35b65b5a735675e840fab55921d28f8
    Cr-Commit-Position: refs/heads/master@{#349515}
    
    Review URL: https://codereview.chromium.org/1351963002
    
    Cr-Commit-Position: refs/heads/master@{#349590}

--- mojo/package_manager/package_manager_impl.cc
+++ mojo/package_manager/package_manager_impl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/fetcher/switches.h"
 #include "mojo/fetcher/update_fetcher.h"
 #include "mojo/shell/application_manager.h"
+#include "mojo/shell/connect_util.h"
 #include "mojo/shell/query_util.h"
 #include "mojo/shell/switches.h"
 #include "mojo/util/filename_util.h"
@@ -80,8 +81,8 @@ void PackageManagerImpl::FetchRequest(
     // LocalFetcher uses the network service to infer MIME types from URLs.
     // Skip this for mojo URLs to avoid recursively loading the network service.
     if (!network_service_ && !url.SchemeIs("mojo")) {
-      application_manager_->ConnectToService(GURL("mojo:network_service"),
-                                            &network_service_);
+      shell::ConnectToService(application_manager_,
+                              GURL("mojo:network_service"), &network_service_);
     }
     // Ownership of this object is transferred to |loader_callback|.
     // TODO(beng): this is eff'n weird.
@@ -100,7 +101,8 @@ void PackageManagerImpl::FetchRequest(
   if (url.SchemeIs("mojo") &&
       base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kUseUpdater)) {
-    application_manager_->ConnectToService(GURL("mojo:updater"), &updater_);
+    shell::ConnectToService(application_manager_, GURL("mojo:updater"),
+                            &updater_);
     // Ownership of this object is transferred to |loader_callback|.
     // TODO(beng): this is eff'n weird.
     new fetcher::UpdateFetcher(url, updater_.get(), loader_callback);
@@ -109,8 +111,8 @@ void PackageManagerImpl::FetchRequest(
 #endif
 
   if (!url_loader_factory_) {
-    application_manager_->ConnectToService(GURL("mojo:network_service"),
-                                           &url_loader_factory_);
+    shell::ConnectToService(application_manager_, GURL("mojo:network_service"),
+                            &url_loader_factory_);
   }
 
   // Ownership of this object is transferred to |loader_callback|.
