6933f413fe2d19ce37dd4f4e6a5888a7c1693e60
amistry <amistry@chromium.org>
2015-09-17 20:38:21 -0700

    Fix MojoApplicationHost assertion when navigating from RenderProcessGone.
    This is a roll forward of crrev.com/101f015eb605098054517fdc98de1665a9bb1431.
    Original commit message:
    Termination of a renderer process results in a call to
    WebContentsObserver::RenderProcessGone. Navigating from
    RenderProcessGone using NavigationControllerImpl::Reload or LoadURL
    results in an assertion due to MojoApplicationHost::Init() being called
    again on an already initialized host. Reset |mojo_application_host_| in
    RenderProcessHostImpl::ProcessDied before calling
    ViewHostMsg_RenderProcessGone message handlers to avoid this problem.
    
    The original change had to be rolled back due to crashes in Mojo that
    were being caused by a race in the browser<->renderer Mojo channel
    shutdown process (i.e. crbug.com/456224). This race has been fixed in
    https://crrev.com/317a93a4df02f269127cb4267df2555bc19fe12a
    and so it should be safe to roll this forward.
    
    BUG=448435
    
    Review URL: https://codereview.chromium.org/1339253002
    
    Cr-Commit-Position: refs/heads/master@{#349603}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -2197,6 +2197,10 @@ void RenderProcessHostImpl::ProcessDied(bool already_dead,
   message_port_message_filter_ = NULL;
   RemoveUserData(kSessionStorageHolderKey);
 
+  // RenderProcessGone handlers might navigate or perform other actions that
+  // require a connection. Ensure that there is one before calling them.
+  mojo_application_host_.reset(new MojoApplicationHost);
+
   IDMap<IPC::Listener>::iterator iter(&listeners_);
   while (!iter.IsAtEnd()) {
     iter.GetCurrentValue()->OnMessageReceived(
@@ -2205,8 +2209,6 @@ void RenderProcessHostImpl::ProcessDied(bool already_dead,
                                        exit_code));
     iter.Advance();
   }
-
-  mojo_application_host_.reset(new MojoApplicationHost);
 
   // It's possible that one of the calls out to the observers might have caused
   // this object to be no longer needed.
