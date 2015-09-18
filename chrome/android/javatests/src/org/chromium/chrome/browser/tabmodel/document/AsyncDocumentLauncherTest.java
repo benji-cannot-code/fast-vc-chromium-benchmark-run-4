abaa4d3da7d345ea6afbf09dd4e2e0b6f6347e85
dfalcantara <dfalcantara@chromium.org>
2015-09-17 20:49:10 -0700

    Revert of Re-enable AsyncDocumentLauncherTest (patchset #1 id:1 of https://chromiumcodereview.appspot.com/1348533004/ )
    Reason for revert:
    Svelte bots are STILL running the tests.
    
    Original issue's description:
    > Re-enable AsyncDocumentLauncherTest
    >
    > This reverts commit 805bd70b6d197d76174d476ec2e8a3772b11d717.
    >
    > Increases launch timeouts; apparently Activity launching is
    > much faster on M.  Also makes the tests run on non-Svelte devices
    > only because tab launching there is a little different.
    >
    > BUG=526238,532852
    >
    > Committed: https://crrev.com/79869a857063f10d1a86b7ce9bf32c73aeaf79be
    > Cr-Commit-Position: refs/heads/master@{#349462}
    
    TBR=mariakhomenko@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=526238,532852
    
    Review URL: https://codereview.chromium.org/1349413002
    
    Cr-Commit-Position: refs/heads/master@{#349606}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/document/AsyncDocumentLauncherTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/document/AsyncDocumentLauncherTest.java
@@ -5,17 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tabmodel.document;
 
-import static org.chromium.base.test.util.Restriction.RESTRICTION_TYPE_NON_LOW_END_DEVICE;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
-import android.test.suitebuilder.annotation.MediumTest;
+//import android.test.suitebuilder.annotation.MediumTest;
 import android.text.TextUtils;
 
 import org.chromium.base.ApplicationStatus;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
-import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeApplication;
 import org.chromium.chrome.browser.document.DocumentActivity;
 import org.chromium.chrome.browser.document.DocumentModeTestBase;
@@ -32,8 +30,8 @@ import org.chromium.content_public.browser.LoadUrlParams;
 @MinAndroidSdkLevel(Build.VERSION_CODES.LOLLIPOP)
 @DisableInTabbedMode
 public class AsyncDocumentLauncherTest extends DocumentModeTestBase {
-    @MediumTest
-    @Restriction({RESTRICTION_TYPE_NON_LOW_END_DEVICE})
+    // @MediumTest
+    @DisabledTest
     public void testLaunchingMultipleUnparented() throws Exception {
         AsyncTabCreationParams initialParams = new AsyncTabCreationParams(new LoadUrlParams(URL_1));
         AsyncTabCreationParams secondParams = new AsyncTabCreationParams(new LoadUrlParams(URL_2));
@@ -54,7 +52,7 @@ public class AsyncDocumentLauncherTest extends DocumentModeTestBase {
 
                 return TextUtils.equals(URL_3, documentActivity.getActivityTab().getUrl());
             }
-        }, 10000, 50));
+        }));
 
         TabModelSelector selector = ChromeApplication.getDocumentTabModelSelector();
         assertEquals(3, selector.getTotalTabCount());
@@ -63,8 +61,8 @@ public class AsyncDocumentLauncherTest extends DocumentModeTestBase {
         assertEquals(URL_3, selector.getModel(false).getTabAt(2).getUrl());
     }
 
-    @MediumTest
-    @Restriction({RESTRICTION_TYPE_NON_LOW_END_DEVICE})
+    // @MediumTest
+    @DisabledTest
     public void testLaunchingMultipleParented() throws Exception {
         // Create an Activity that will be credited with creating the child Activities.
         int parentId = launchViaViewIntent(false, URL_1, "Page 1");
@@ -88,7 +86,7 @@ public class AsyncDocumentLauncherTest extends DocumentModeTestBase {
 
                 return TextUtils.equals(URL_4, documentActivity.getActivityTab().getUrl());
             }
-        }, 10000, 50));
+        }));
 
         TabModelSelector selector = ChromeApplication.getDocumentTabModelSelector();
         assertEquals(4, selector.getTotalTabCount());
@@ -98,8 +96,8 @@ public class AsyncDocumentLauncherTest extends DocumentModeTestBase {
         assertEquals(URL_4, selector.getModel(false).getTabAt(3).getUrl());
     }
 
-    @MediumTest
-    @Restriction({RESTRICTION_TYPE_NON_LOW_END_DEVICE})
+    // @MediumTest
+    @DisabledTest
     public void testFailedLaunch() throws Exception {
         // Bloat up the parent Intent so that launching the child Activity will fail.
         int parentId = launchViaViewIntent(false, URL_1, "Page 1");
@@ -130,7 +128,7 @@ public class AsyncDocumentLauncherTest extends DocumentModeTestBase {
 
                 return TextUtils.equals(URL_4, documentActivity.getActivityTab().getUrl());
             }
-        }, 10000, 50));
+        }));
 
         TabModelSelector selector = ChromeApplication.getDocumentTabModelSelector();
         assertEquals(3, selector.getTotalTabCount());
