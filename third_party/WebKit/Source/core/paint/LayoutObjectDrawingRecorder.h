4c5e2495cf062964fd39531de7813ac16dcb8712
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2015-09-18 00:56:55 +0000

    Remove dependency on m_previousPositionFromPaintInvalidationBacking for spv2
    - Set layer needsRepaint when any paint invalidation flag is set, so
      that we can check paint offset changes during paint.
    - Always use paintsWithTransform in paintLayer to avoid paint offset
      changes of descendants when the layer is moved-only.
    
    BUG=508383,510908
    
    Review URL: https://codereview.chromium.org/1312423006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202486 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/paint/LayoutObjectDrawingRecorder.h
+++ third_party/WebKit/Source/core/paint/LayoutObjectDrawingRecorder.h
@@ -54,7 +54,7 @@ public:
 
     LayoutObjectDrawingRecorder(GraphicsContext& context, const LayoutObject& layoutObject, DisplayItem::Type displayItemType, const FloatRect& clip, const LayoutPoint& paintOffset)
     {
-        updatePaintOffsetIfNeeded(context.displayItemList(), layoutObject.displayItemClient(), paintOffset);
+        updatePaintOffsetIfNeeded(context.displayItemList(), layoutObject, paintOffset);
         // We may paint a delayed-invalidation object before it's actually invalidated.
         if (layoutObject.fullPaintInvalidationReason() == PaintInvalidationDelayedFull)
             m_cacheSkipper.emplace(context);
@@ -75,7 +75,7 @@ public:
 
     LayoutObjectDrawingRecorder(GraphicsContext& context, const InlineBox& inlineBox, DisplayItem::Type displayItemType, const FloatRect& clip, const LayoutPoint& paintOffset)
     {
-        updatePaintOffsetIfNeeded(context.displayItemList(), inlineBox.displayItemClient(), paintOffset);
+        updatePaintOffsetIfNeeded(context.displayItemList(), inlineBox, paintOffset);
         m_drawingRecorder.emplace(context, inlineBox, displayItemType, clip);
     }
 
@@ -93,17 +93,17 @@ public:
 #endif
 
 private:
-    static void updatePaintOffsetIfNeeded(DisplayItemList* displayItemList, DisplayItemClient displayItemClient, const LayoutPoint& paintOffset)
+    static void updatePaintOffsetIfNeeded(DisplayItemList* displayItemList, const DisplayItemClientWrapper& client, const LayoutPoint& paintOffset)
     {
         if (!RuntimeEnabledFeatures::slimmingPaintV2Enabled())
             return;
 
         // TODO(pdr): The paint offset cache should be stored on LayoutObject but is temporarily on the DisplayItemList.
-        if (!displayItemList->paintOffsetIsUnchanged(displayItemClient, paintOffset)) {
-            displayItemList->recordPaintOffset(displayItemClient, paintOffset);
-            displayItemList->invalidatePaintOffset(displayItemClient);
+        if (!displayItemList->paintOffsetIsUnchanged(client.displayItemClient(), paintOffset)) {
+            displayItemList->recordPaintOffset(client.displayItemClient(), paintOffset);
+            displayItemList->invalidatePaintOffset(client);
         } else {
-            ASSERT(!displayItemList->paintOffsetWasInvalidated(displayItemClient) || !displayItemList->clientCacheIsValid(displayItemClient));
+            ASSERT(!displayItemList->paintOffsetWasInvalidated(client.displayItemClient()) || !displayItemList->clientCacheIsValid(client.displayItemClient()));
         }
     }
 
