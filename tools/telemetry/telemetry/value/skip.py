b5ab219519b8c65c4a0f8db2f396a4bc6320c292
eakuefner <eakuefner@chromium.org>
2015-09-18 09:37:55 -0700

    [Telemetry] Fix value stringification and add tests.
    This CL fixes the mentioned bug and adds tests for stringification of all Value
    types. In the course of adding these tests, miscellaneous issues were uncovered
    with stringifcation that this CL repairs as well.
    
    BUG=532204
    R=nednguyen
    
    Review URL: https://codereview.chromium.org/1351173003
    
    Cr-Commit-Position: refs/heads/master@{#349693}

--- tools/telemetry/telemetry/value/skip.py
+++ tools/telemetry/telemetry/value/skip.py
@@ -20,7 +20,8 @@ class SkipValue(value_module.Value):
 
   def __repr__(self):
     page_name = self.page.display_name
-    return 'SkipValue(%s, %s)' % (page_name, self._reason)
+    return 'SkipValue(%s, %s, description=%s)' % (page_name, self._reason,
+                                                  self.description)
 
   @property
   def reason(self):
