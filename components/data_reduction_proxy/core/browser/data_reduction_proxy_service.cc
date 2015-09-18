338cc0ce1451c6e5665a2d5ececf92950da0984c
kundaji <kundaji@chromium.org>
2015-09-18 16:11:24 -0700

    Delete historical data usage from store when user clears data usage.
    Delete data usage in LevelDB in addition to clearing stats store in
    browser preferences.
    
    BUG=482442,461022
    
    Review URL: https://codereview.chromium.org/1346623004
    
    Cr-Commit-Position: refs/heads/master@{#349805}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_service.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_service.cc
@@ -54,6 +54,7 @@ DataReductionProxyService::DataReductionProxyService(
 
 DataReductionProxyService::~DataReductionProxyService() {
   DCHECK(CalledOnValidThread());
+  compression_stats_.reset();
   db_task_runner_->DeleteSoon(FROM_HERE, db_data_owner_.release());
 }
 
@@ -285,6 +286,12 @@ void DataReductionProxyService::StoreCurrentDataUsageBucket(
       FROM_HERE,
       base::Bind(&DBDataOwner::StoreCurrentDataUsageBucket,
                  db_data_owner_->GetWeakPtr(), base::Passed(&current)));
+}
+
+void DataReductionProxyService::DeleteHistoricalDataUsage() {
+  db_task_runner_->PostTask(FROM_HERE,
+                            base::Bind(&DBDataOwner::DeleteHistoricalDataUsage,
+                                       db_data_owner_->GetWeakPtr()));
 }
 
 void DataReductionProxyService::AddObserver(
