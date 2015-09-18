9aa404f6b3d04c460cab95d30923568cbb101d62
huangs <huangs@chromium.org>
2015-09-18 11:52:56 -0700

    [Courgette] Refactor: Adding Rel32FinderWin32X86_Basic and Unittests
    We're planning to improve heuristic to find Rel32 addresses for Win32 x86.
    First refactor by by extraacting the feature into its own class. This also
    lead to the extraction of RVA and various helpers from disassembler.h to
    image_utils.h.
    
    Also adding unittests for Rel32FinderWin32X86_Basic. The test is
    data-driven by adding a simple parser for test data.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1344173003
    
    Cr-Commit-Position: refs/heads/master@{#349727}

--- courgette/disassembler.h
+++ courgette/disassembler.h
@@ -9,18 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 
 #include "courgette/courgette.h"
-
-// COURGETTE_HISTOGRAM_TARGETS prints out a histogram of how frequently
-// different target addresses are referenced.  Purely for debugging.
-#define COURGETTE_HISTOGRAM_TARGETS 0
+#include "courgette/image_utils.h"
 
 namespace courgette {
 
 class AssemblyProgram;
-
-// A Relative Virtual Address is the address in the image file after it is
-// loaded into memory relative to the image load address.
-typedef uint32 RVA;
 
 class Disassembler {
  public:
@@ -60,31 +53,6 @@ class Disassembler {
     return offset <= length() && elements <= (length() - offset) / element_size;
   }
 
-  // These helper functions avoid the need for casts in the main code.
-  uint16 ReadU16(const uint8* address, size_t offset) {
-    return *reinterpret_cast<const uint16*>(address + offset);
-  }
-
-  uint32 ReadU32(const uint8* address, size_t offset) {
-    return *reinterpret_cast<const uint32*>(address + offset);
-  }
-
-  uint64 ReadU64(const uint8* address, size_t offset) {
-    return *reinterpret_cast<const uint64*>(address + offset);
-  }
-
-  static uint32 Read32LittleEndian(const void* address) {
-    return *reinterpret_cast<const uint32*>(address);
-  }
-
-  static uint64 Read64LittleEndian(const void* address) {
-    return *reinterpret_cast<const uint64*>(address);
-  }
-
-  static uint16 Read16LittleEndian(const void* address) {
-    return *reinterpret_cast<const uint16*>(address);
-  }
-
   // Reduce the length of the image in memory. Does not actually free
   // (or realloc) any memory. Usually only called via ParseHeader()
   void ReduceLength(size_t reduced_length);
@@ -105,4 +73,5 @@ class Disassembler {
 };
 
 }  // namespace courgette
+
 #endif  // COURGETTE_DISASSEMBLER_H_
--- courgette/typedrva_unittest.cc
+++ courgette/typedrva_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "courgette/base_test_unittest.h"
 #include "courgette/disassembler_elf_32_arm.h"
 #include "courgette/disassembler_elf_32_x86.h"
+#include "courgette/image_utils.h"
 
 class TypedRVATest : public BaseTest {
  public:
