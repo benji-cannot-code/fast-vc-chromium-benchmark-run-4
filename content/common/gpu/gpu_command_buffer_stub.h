3eb64f99bd8146e6988d1bf5ee28ba94fe0cbd3f
sunnyps <sunnyps@chromium.org>
2015-09-17 19:23:43 -0700

    content/gpu: Simplify gpu channel message handling.
    This CL makes the following changes to gpu channel message handling:
    1. The GpuChannelMessageQueue interface is exposed in the header so that
       it may be used by the upcoming gpu scheduler.
    2. Removed friend classes in GpuChannel.
    3. Messages are no longer popped from the message queue and reinserted
       when retrying. Instead messages are only popped after they have been
       handled.
    4. As a result, we no longer synthesize the Reschedule and
       RetireSyncPoint IPC messages.
    
    BUG=514813
    
    Review URL: https://codereview.chromium.org/1336623004
    
    Cr-Commit-Position: refs/heads/master@{#349584}

--- content/common/gpu/gpu_command_buffer_stub.h
+++ content/common/gpu/gpu_command_buffer_stub.h
@@ -106,11 +106,6 @@ class GpuCommandBufferStub
   // Whether this command buffer can currently handle IPC messages.
   bool IsScheduled();
 
-  // If the command buffer is pre-empted and cannot process commands.
-  bool IsPreempted() const {
-    return scheduler_.get() && scheduler_->IsPreempted();
-  }
-
   // Whether there are commands in the buffer that haven't been processed.
   bool HasUnprocessedCommands();
 
@@ -147,7 +142,7 @@ class GpuCommandBufferStub
 
   // Associates a sync point to this stub. When the stub is destroyed, it will
   // retire all sync points that haven't been previously retired.
-  void AddSyncPoint(uint32 sync_point);
+  void AddSyncPoint(uint32 sync_point, bool retire);
 
   void SetPreemptByFlag(scoped_refptr<gpu::PreemptionFlag> flag);
 
@@ -187,7 +182,6 @@ class GpuCommandBufferStub
                                  IPC::Message* reply_message);
   void OnAsyncFlush(int32 put_offset, uint32 flush_count,
                     const std::vector<ui::LatencyInfo>& latency_info);
-  void OnRescheduled();
   void OnRegisterTransferBuffer(int32 id,
                                 base::SharedMemoryHandle transfer_buffer,
                                 uint32 size);
