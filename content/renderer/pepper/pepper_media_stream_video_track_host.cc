c82c3d1309ce06e1372ab1fb02a927c1da93a717
grunell <grunell@chromium.org>
2015-09-18 01:17:41 -0700

    Added some missing includes, removed one include and removed an undef.
    Found this when working on removing WebRTC's dependency on Chromium.
    
    These updates makes the WebRTC Chromium FYI bots green.
    
    BUG=468375
    TBR=sergeyu@chromium.org,nick@chromium.org
    
    Review URL: https://codereview.chromium.org/1346883003
    
    Cr-Commit-Position: refs/heads/master@{#349631}

--- content/renderer/pepper/pepper_media_stream_video_track_host.cc
+++ content/renderer/pepper/pepper_media_stream_video_track_host.cc
@@ -19,11 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/host/host_message_context.h"
 #include "ppapi/proxy/ppapi_messages.h"
 #include "ppapi/shared_impl/media_stream_buffer.h"
-
-// IS_ALIGNED is also defined in
-// third_party/webrtc/overrides/webrtc/base/basictypes.h
-// TODO(ronghuawu): Avoid undef.
-#undef IS_ALIGNED
 #include "third_party/libyuv/include/libyuv.h"
 
 using media::VideoFrame;
