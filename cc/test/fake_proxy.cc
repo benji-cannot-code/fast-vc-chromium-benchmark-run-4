593ac525f2048355995850e836645b4a2892bca0
sohan.jyoti <sohan.jyoti@samsung.com>
2015-09-18 15:32:55 -0700

    This sets up API to release OutputSurface from LTHClient. It will be helpful in Android where we can release OutputSurface without destroying LTHClient, thereby making OutputSurface switches have less glitches.
    BUG=374906
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1287043002
    
    Cr-Commit-Position: refs/heads/master@{#349787}

--- cc/test/fake_proxy.cc
+++ cc/test/fake_proxy.cc
@@ -25,6 +25,10 @@ RendererCapabilities& FakeProxy::GetRendererCapabilities() {
   return capabilities_;
 }
 
+scoped_ptr<OutputSurface> FakeProxy::ReleaseOutputSurface() {
+  return nullptr;
+}
+
 bool FakeProxy::BeginMainFrameRequested() const { return false; }
 
 bool FakeProxy::CommitRequested() const { return false; }
--- cc/trees/thread_proxy.h
+++ cc/trees/thread_proxy.h
@@ -178,6 +178,7 @@ class CC_EXPORT ThreadProxy : public Proxy,
   bool MainFrameWillHappenForTesting() override;
   void SetChildrenNeedBeginFrames(bool children_need_begin_frames) override;
   void SetAuthoritativeVSyncInterval(const base::TimeDelta& interval) override;
+  scoped_ptr<OutputSurface> ReleaseOutputSurface() override;
 
   // LayerTreeHostImplClient implementation
   void UpdateRendererCapabilitiesOnImplThread() override;
@@ -275,6 +276,9 @@ class CC_EXPORT ThreadProxy : public Proxy,
   void DeleteContentsTexturesOnImplThread(CompletionEvent* completion);
   void InitializeOutputSurfaceOnImplThread(
       scoped_ptr<OutputSurface> output_surface);
+  void ReleaseOutputSurfaceOnImplThread(
+      CompletionEvent* completion,
+      scoped_ptr<OutputSurface>* output_surface);
   void FinishGLOnImplThread(CompletionEvent* completion);
   void LayerTreeHostClosedOnImplThread(CompletionEvent* completion);
   DrawResult DrawSwapInternal(bool forced_draw);
