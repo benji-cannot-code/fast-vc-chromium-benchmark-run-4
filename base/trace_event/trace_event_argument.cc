960df3460aa3b15ba652674d8379bc6765edd7da
primiano <primiano@chromium.org>
2015-09-18 02:57:40 -0700

    [tracing] Don't use dot-based path expansion in TracedValue
    The current implementation of TracedValue still relies on a conversion
    to base::Value in the last stage to create the JSON.
    The current implementation can degenerate the dictionary if key names
    contain dots, thanks to the default implementation of base::Value.
    This fixes the issue by using the *WithoutPathExpansion methods.
    
    BUG=532838
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1354493003
    
    Cr-Commit-Position: refs/heads/master@{#349640}

--- base/trace_event/trace_event_argument.cc
+++ base/trace_event/trace_event_argument.cc
@@ -358,7 +358,8 @@ scoped_ptr<base::Value> TracedValue::ToBaseValue() const {
       case kTypeStartDict: {
         auto new_dict = new DictionaryValue();
         if (cur_dict) {
-          cur_dict->Set(ReadKeyName(it), make_scoped_ptr(new_dict));
+          cur_dict->SetWithoutPathExpansion(ReadKeyName(it),
+                                            make_scoped_ptr(new_dict));
           stack.push_back(cur_dict);
           cur_dict = new_dict;
         } else {
@@ -382,7 +383,8 @@ scoped_ptr<base::Value> TracedValue::ToBaseValue() const {
       case kTypeStartArray: {
         auto new_list = new ListValue();
         if (cur_dict) {
-          cur_dict->Set(ReadKeyName(it), make_scoped_ptr(new_list));
+          cur_dict->SetWithoutPathExpansion(ReadKeyName(it),
+                                            make_scoped_ptr(new_list));
           stack.push_back(cur_dict);
           cur_dict = nullptr;
           cur_list = new_list;
@@ -397,7 +399,7 @@ scoped_ptr<base::Value> TracedValue::ToBaseValue() const {
         bool value;
         CHECK(it.ReadBool(&value));
         if (cur_dict) {
-          cur_dict->SetBoolean(ReadKeyName(it), value);
+          cur_dict->SetBooleanWithoutPathExpansion(ReadKeyName(it), value);
         } else {
           cur_list->AppendBoolean(value);
         }
@@ -407,7 +409,7 @@ scoped_ptr<base::Value> TracedValue::ToBaseValue() const {
         int value;
         CHECK(it.ReadInt(&value));
         if (cur_dict) {
-          cur_dict->SetInteger(ReadKeyName(it), value);
+          cur_dict->SetIntegerWithoutPathExpansion(ReadKeyName(it), value);
         } else {
           cur_list->AppendInteger(value);
         }
@@ -417,7 +419,7 @@ scoped_ptr<base::Value> TracedValue::ToBaseValue() const {
         double value;
         CHECK(it.ReadDouble(&value));
         if (cur_dict) {
-          cur_dict->SetDouble(ReadKeyName(it), value);
+          cur_dict->SetDoubleWithoutPathExpansion(ReadKeyName(it), value);
         } else {
           cur_list->AppendDouble(value);
         }
@@ -427,7 +429,7 @@ scoped_ptr<base::Value> TracedValue::ToBaseValue() const {
         std::string value;
         CHECK(it.ReadString(&value));
         if (cur_dict) {
-          cur_dict->SetString(ReadKeyName(it), value);
+          cur_dict->SetStringWithoutPathExpansion(ReadKeyName(it), value);
         } else {
           cur_list->AppendString(value);
         }
