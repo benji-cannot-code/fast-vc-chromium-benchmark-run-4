77aeaac810a108bf0595737d560042b6ac5f4e3f
yosin@chromium.org <yosin@chromium.org>
2015-09-18 18:26:41 +0000

    Utilize VisibleSelectionTemplate in PendingSelection
    This patch changes |PendingSelection| to use |VisibleSelectionTemplate| as
    preparation of introducing full version of |VisibleSelectionTemplate| with
    adding member functions to |VisibleSelectionTemplate| used by
    |PendingSelection|.
    
    We'll have following steps to introduce full version of
    |VisibleSelectionTemplate|:
     1. Introduce wrapper version of |VisibleSelectionTemplate|;
        http://crrev.com/this patch
     2. Utilize in |SelectionController|; http://crrev.com/1356853002
     3. Utilize in |FrameSelection|; http://crrev.com/1353673005
     4. Utilize in |PendingSelection|; this patch
       - |VisibleSelection::InDOMTree| and |InComposedTree| are gone.
       - No behavior changes between step 1 to step 4
     5. Introduce full version of |VisibleSelectionTemplate|
       - Double-click to select word in composed tree
     6. Change keyboard navigation to work in composed tree
    
    This patch is a preparation of making selection to handle granularity for web
    component, http://crrev.com/1277863002
    
    BUG=513568
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1350303002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202533 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/VisibleSelection.cpp
+++ third_party/WebKit/Source/core/editing/VisibleSelection.cpp
@@ -1158,6 +1158,12 @@ VisibleSelectionTemplate<Strategy>::VisibleSelectionTemplate(const PositionAlgor
 }
 
 template <typename Strategy>
+VisibleSelectionTemplate<Strategy>::VisibleSelectionTemplate(const VisiblePositionTemplate<Strategy>& base, const VisiblePositionTemplate<Strategy>& extent)
+    : VisibleSelectionTemplate(VisibleSelection(base.deepEquivalent(), extent.deepEquivalent(), base.affinity()))
+{
+}
+
+template <typename Strategy>
 VisibleSelectionTemplate<Strategy>::VisibleSelectionTemplate(const VisibleSelection& visibleSelection)
     : m_visibleSelection(visibleSelection)
 {
@@ -1166,6 +1172,11 @@ VisibleSelectionTemplate<Strategy>::VisibleSelectionTemplate(const VisibleSelect
 template <typename Strategy>
 VisibleSelectionTemplate<Strategy>::VisibleSelectionTemplate(const VisiblePositionTemplate<Strategy>& visiblePosition)
     : VisibleSelectionTemplate(VisibleSelection(visiblePosition.deepEquivalent(), visiblePosition.deepEquivalent(), visiblePosition.affinity()))
+{
+}
+
+template <typename Strategy>
+VisibleSelectionTemplate<Strategy>::VisibleSelectionTemplate()
 {
 }
 
@@ -1185,6 +1196,12 @@ template <typename Strategy>
 void VisibleSelectionTemplate<Strategy>::setExtent(const VisiblePositionTemplate<Strategy>& newExtent)
 {
     return setExtent(newExtent.deepEquivalent());
+}
+
+template <typename Strategy>
+void VisibleSelectionTemplate<Strategy>::setWithoutValidation(const PositionAlgorithm<Strategy>& base, const PositionAlgorithm<Strategy>& extent)
+{
+    m_visibleSelection.setWithoutValidation(base, extent);
 }
 
 template <>
