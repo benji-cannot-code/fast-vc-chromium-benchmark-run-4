4ae705be66345f2d30353a4b5e6e2272fc884c92
yutak <yutak@chromium.org>
2015-09-17 23:34:33 -0700

    BlinkGCPlugin: Factor out CollectVisitor from BlinkGCPlugin.cpp.
    Part 3 of dividing a 2000-line cpp file.
    
    CollectVisitor is a small class, and there are only obvious code moves
    in this patch.
    
    BUG=531879
    R=haraken@chromium.org, oilpan-reviews@chromium.org, sigbjornf@opera.com
    
    Review URL: https://codereview.chromium.org/1351783003
    
    Cr-Commit-Position: refs/heads/master@{#349627}

---
+++ tools/clang/blink_gc_plugin/CollectVisitor.cpp
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "CollectVisitor.h"
+
+#include "Config.h"
+
+using namespace clang;
+
+CollectVisitor::CollectVisitor() {
+}
+
+CollectVisitor::RecordVector& CollectVisitor::record_decls() {
+  return record_decls_;
+}
+
+CollectVisitor::MethodVector& CollectVisitor::trace_decls() {
+  return trace_decls_;
+}
+
+bool CollectVisitor::VisitCXXRecordDecl(CXXRecordDecl* record) {
+  if (record->hasDefinition() && record->isCompleteDefinition())
+    record_decls_.push_back(record);
+  return true;
+}
+
+bool CollectVisitor::VisitCXXMethodDecl(CXXMethodDecl* method) {
+  if (method->isThisDeclarationADefinition() && Config::IsTraceMethod(method))
+    trace_decls_.push_back(method);
+  return true;
+}
