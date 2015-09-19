839a16716cffaef9885914b602648f5597aa0f84
pfeldman@chromium.org <pfeldman@chromium.org>
2015-09-19 01:39:31 +0000

    DevTools: format meta values while editing styles
    Review URL: https://codereview.chromium.org/1346843004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202559 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/sdk/CSSStyleModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/CSSStyleModel.js
@@ -1366,7 +1366,7 @@ WebInspector.CSSProperty.prototype = {
             if (isSemicolon)
                 insideProperty = false;
 
-            if (tokenType && (tokenType.includes("css-meta") || (tokenType.includes("css-property") && !lastWasMeta))) {
+            if (!insideProperty && tokenType && (tokenType.includes("css-meta") || (tokenType.includes("css-property") && !lastWasMeta))) {
                 result += "\n" + indentation;
                 insideProperty = true;
             }
