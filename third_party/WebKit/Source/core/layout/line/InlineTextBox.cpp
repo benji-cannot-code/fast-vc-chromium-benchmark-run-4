3be6ea4e0a05d52ac7c9da565f97336168e51f0c
wkorman@chromium.org <wkorman@chromium.org>
2015-09-19 03:13:46 +0000

    Disable newline selection highlighting for vertical and RTL text.
    BUG=474759
    
    Review URL: https://codereview.chromium.org/1355913004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202563 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/layout/line/InlineTextBox.cpp
+++ third_party/WebKit/Source/core/layout/line/InlineTextBox.cpp
@@ -190,6 +190,9 @@ bool InlineTextBox::hasWrappedSelectionNewline() const
 {
     SelectionState state = selectionState();
     return RuntimeEnabledFeatures::selectionPaintingWithoutSelectionGapsEnabled()
+        // TODO(wkorman): Remove horizontal and RTL restrictions once operational.
+        && isHorizontal()
+        && isLeftToRightDirection()
         && (root().lastSelectedBox() == this)
         && (state == SelectionStart || state == SelectionInside);
 }
