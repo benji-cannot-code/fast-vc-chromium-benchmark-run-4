77b41b9925f9b030a08b79b7d1ec8e25be29c76f
palmer <palmer@chromium.org>
2015-09-20 16:16:16 -0700

    Expose FormatUrlForSecurityDisplay to Java on Android.
    BUG=508371
    
    Review URL: https://codereview.chromium.org/1357563002
    
    Cr-Commit-Position: refs/heads/master@{#349886}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappUrlBarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappUrlBarTest.java
@@ -37,7 +37,7 @@ public class WebappUrlBarTest extends WebappActivityTestBase {
     @MediumTest
     @Feature({"Webapps"})
     public void testUrlDisplay() {
-        final String scheme = "somescheme://";
+        final String scheme = "https://";
         final String host = "lorem.com";
         final String path = "/stuff/and/things.html";
         final String url = scheme + host + path;
@@ -50,7 +50,7 @@ public class WebappUrlBarTest extends WebappActivityTestBase {
                 ConnectionSecurityLevel.SECURITY_ERROR};
 
         for (int i : securityLevels) {
-            // http://crbug.com/297249
+            // TODO(palmer): http://crbug.com/297249
             if (i == ConnectionSecurityLevel.SECURITY_POLICY_WARNING) continue;
             mUrlBar.update(url, i);
 
