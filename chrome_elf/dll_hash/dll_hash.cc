1b4194d2c4b4f1e343f15aaea445250e5a4a283c
ki.stfu <ki.stfu@gmail.com>
2015-09-20 16:20:02 -0700

    Cleanup: Pass std::string as const reference from chrome_elf/
    Passing std::string by reference can prevent extra copying of object.
    
    BUG=367418
    TEST=
    R=caitkp@chromium.org,robertshield@chromium.org
    
    Review URL: https://codereview.chromium.org/1352793003
    
    Cr-Commit-Position: refs/heads/master@{#349887}

--- chrome_elf/dll_hash/dll_hash.cc
+++ chrome_elf/dll_hash/dll_hash.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/hash.h"
 #include "chrome_elf/dll_hash/dll_hash.h"
 
-int DllNameToHash(std::string dll_name) {
+int DllNameToHash(const std::string& dll_name) {
   uint32 data = base::Hash(dll_name);
 
   // Strip off the signed bit because UMA doesn't support negative values,
--- chrome_elf/dll_hash/dll_hash.h
+++ chrome_elf/dll_hash/dll_hash.h
@@ -9,6 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 // Convert a dll name to a hash that can be sent via UMA.
-int DllNameToHash(std::string dll_name);
+int DllNameToHash(const std::string& dll_name);
 
 #endif  // CHROME_ELF_DLL_HASH_DLL_HASH_H_
