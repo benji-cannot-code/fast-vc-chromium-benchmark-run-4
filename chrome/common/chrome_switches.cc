d8dadd369ef9a1823434bf3fe6d56fa243282bb9
mdjones <mdjones@chromium.org>
2015-09-21 14:30:37 -0700

    Remove Reader Mode toolbar icon flag
    This cleans up references to the reader mode toolbar icon
    flag on android since it is no longer in use.
    
    BUG=530277
    
    Review URL: https://codereview.chromium.org/1347833003
    
    Cr-Commit-Position: refs/heads/master@{#350024}

--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -1166,10 +1166,6 @@ const char kEnableDomDistillerButtonAnimation[] =
 // Enables chrome hosted mode for Android.
 const char kEnableHostedMode[] = "enable-hosted-mode";
 
-// Enables context-sensitive reader mode button in the toolbar.
-const char kEnableReaderModeToolbarIcon[] =
-    "enable-reader-mode-toolbar-icon";
-
 // Specifies Android phone page loading progress bar animation.
 const char kProgressBarAnimation[]          = "progress-bar-animation";
 
