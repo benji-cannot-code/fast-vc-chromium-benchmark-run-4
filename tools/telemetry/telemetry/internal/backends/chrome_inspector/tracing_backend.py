6a7b4029b32a81b239c42d5ec7eb6c27b5907a68
zhenw <zhenw@chromium.org>
2015-09-21 13:28:48 -0700

    Reland again [Telemetry] Refactor Chrome devtools tracing
    This CL tries to reland https://crrev.com/1347563002/ with the fix.
    
    Platform-level tracing doc:
    https://docs.google.com/document/d/1Uv8SzOq2ugvzLiOcP85g7qPiaxwsn4tgOdxpe3YjUkM/edit?usp=sharing
    
    BUG=515323, 531755, 534233
    
    Review URL: https://codereview.chromium.org/1355193002
    
    Cr-Commit-Position: refs/heads/master@{#350009}

--- tools/telemetry/telemetry/internal/backends/chrome_inspector/tracing_backend.py
+++ tools/telemetry/telemetry/internal/backends/chrome_inspector/tracing_backend.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import json
 import socket
+import sys
 import time
 
 from telemetry import decorators
@@ -31,6 +32,32 @@ class TracingHasNotRunException(Exception):
 
 class TracingUnexpectedResponseException(Exception):
   pass
+
+
+
+def IsInspectorWebsocketAvailable(port):
+  """Returns True if inspector websocket is available on the given port."""
+  inspector_websocket_instance = inspector_websocket.InspectorWebsocket()
+  try:
+    return _IsInspectorWebsocketAvailable(inspector_websocket_instance, port)
+  finally:
+    inspector_websocket_instance.Disconnect()
+
+def _IsInspectorWebsocketAvailable(inspector_websocket_instance, port):
+  try:
+    inspector_websocket_instance.Connect(
+        'ws://127.0.0.1:%i/devtools/browser' % port)
+  except websocket.WebSocketException:
+    return False
+  except socket.error:
+    return False
+  except Exception as e:
+    sys.stderr.write('Unidentified exception while checking if wesocket is'
+                     'available on port %i. Exception message: %s\n' %
+                     (port, e.message))
+    return False
+  else:
+    return True
 
 
 class _DevToolsStreamReader(object):
