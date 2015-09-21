6e5c31889d76b4d635d90d1fedbc14d2a2673037
twifkak <twifkak@chromium.org>
2015-09-21 11:48:35 -0700

    Record Precache.Fetch.IntentReceived user action.
    Add a non-user-triggered user action that records when the
    Precache.Fetch.FailureReason metric was recorded. Without this, analysis
    relies on an approximation based on times reported in other user
    actions.
    
    BUG=499532
    
    Review URL: https://codereview.chromium.org/1355723002
    
    Cr-Commit-Position: refs/heads/master@{#349977}

--- chrome/android/java/src/org/chromium/chrome/browser/precache/PrecacheServiceLauncher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/precache/PrecacheServiceLauncher.java
@@ -22,6 +22,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.components.precache.DeviceState;
 
 import java.util.ArrayDeque;
@@ -171,6 +172,7 @@ public class PrecacheServiceLauncher extends BroadcastReceiver {
             while ((reasonsToRecord = mFailureReasonsToRecord.poll()) != null) {
                 RecordHistogram.recordSparseSlowlyHistogram(
                         "Precache.Fetch.FailureReasons", reasonsToRecord);
+                RecordUserAction.record("Precache.Fetch.IntentReceived");
             }
         }
     }
