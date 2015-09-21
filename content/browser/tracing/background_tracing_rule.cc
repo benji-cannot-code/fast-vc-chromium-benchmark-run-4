98bc6ee1eb2e175fce2edc598201a474e819f3c6
simonhatch <simonhatch@chromium.org>
2015-09-21 13:05:28 -0700

    Bulk Reports: Add support for histogram range triggers.
    Previously we could only specify a lower bound for histogram triggers. This CL introduces an upper bound for triggering, allowing for more fine grained control over when traces are captured.
    
    BUG=525655
    
    Review URL: https://codereview.chromium.org/1349313005
    
    Cr-Commit-Position: refs/heads/master@{#350002}

--- content/browser/tracing/background_tracing_rule.cc
+++ content/browser/tracing/background_tracing_rule.cc
@@ -24,7 +24,9 @@ const char kConfigCategoryKey[] = "category";
 const char kConfigRuleTriggerNameKey[] = "trigger_name";
 
 const char kConfigRuleHistogramNameKey[] = "histogram_name";
-const char kConfigRuleHistogramValueKey[] = "histogram_value";
+const char kConfigRuleHistogramValueOldKey[] = "histogram_value";
+const char kConfigRuleHistogramValue1Key[] = "histogram_lower_value";
+const char kConfigRuleHistogramValue2Key[] = "histogram_upper_value";
 
 const char kConfigRuleRandomIntervalTimeoutMin[] = "timeout_min";
 const char kConfigRuleRandomIntervalTimeoutMax[] = "timeout_max";
@@ -90,8 +92,12 @@ class NamedTriggerRule : public BackgroundTracingRule {
 class HistogramRule : public BackgroundTracingRule,
                       public TracingControllerImpl::TraceMessageFilterObserver {
  public:
-  HistogramRule(const std::string& histogram_name, int histogram_value)
-      : histogram_name_(histogram_name), histogram_value_(histogram_value) {}
+  HistogramRule(const std::string& histogram_name,
+                int histogram_lower_value,
+                int histogram_upper_value)
+      : histogram_name_(histogram_name),
+        histogram_lower_value_(histogram_lower_value),
+        histogram_upper_value_(histogram_upper_value) {}
 
   ~HistogramRule() override {
     base::StatisticsRecorder::ClearCallback(histogram_name_);
@@ -104,7 +110,8 @@ class HistogramRule : public BackgroundTracingRule,
     base::StatisticsRecorder::SetCallback(
         histogram_name_,
         base::Bind(&HistogramRule::OnHistogramChangedCallback,
-                   base::Unretained(this), histogram_name_, histogram_value_));
+                   base::Unretained(this), histogram_name_,
+                   histogram_lower_value_, histogram_upper_value_));
 
     TracingControllerImpl::GetInstance()->AddTraceMessageFilterObserver(this);
   }
@@ -113,7 +120,8 @@ class HistogramRule : public BackgroundTracingRule,
     DCHECK(dict);
     dict->SetString(kConfigRuleKey, kPreemptiveConfigRuleMonitorHistogram);
     dict->SetString(kConfigRuleHistogramNameKey, histogram_name_.c_str());
-    dict->SetInteger(kConfigRuleHistogramValueKey, histogram_value_);
+    dict->SetInteger(kConfigRuleHistogramValue1Key, histogram_lower_value_);
+    dict->SetInteger(kConfigRuleHistogramValue2Key, histogram_upper_value_);
   }
 
   void OnHistogramTrigger(const std::string& histogram_name) const override {
@@ -129,8 +137,8 @@ class HistogramRule : public BackgroundTracingRule,
 
   // TracingControllerImpl::TraceMessageFilterObserver implementation
   void OnTraceMessageFilterAdded(TraceMessageFilter* filter) override {
-    filter->Send(
-        new TracingMsg_SetUMACallback(histogram_name_, histogram_value_));
+    filter->Send(new TracingMsg_SetUMACallback(
+        histogram_name_, histogram_lower_value_, histogram_upper_value_));
   }
 
   void OnTraceMessageFilterRemoved(TraceMessageFilter* filter) override {
@@ -138,9 +146,11 @@ class HistogramRule : public BackgroundTracingRule,
   }
 
   void OnHistogramChangedCallback(const std::string& histogram_name,
-                                  base::Histogram::Sample reference_value,
+                                  base::Histogram::Sample reference_lower_value,
+                                  base::Histogram::Sample reference_upper_value,
                                   base::Histogram::Sample actual_value) {
-    if (reference_value > actual_value)
+    if (reference_lower_value > actual_value ||
+        reference_upper_value < actual_value)
       return;
 
     OnHistogramTrigger(histogram_name);
@@ -148,7 +158,8 @@ class HistogramRule : public BackgroundTracingRule,
 
  private:
   std::string histogram_name_;
-  int histogram_value_;
+  int histogram_lower_value_;
+  int histogram_upper_value_;
 };
 
 class ReactiveTraceForNSOrTriggerOrFullRule : public BackgroundTracingRule {
@@ -299,12 +310,27 @@ scoped_ptr<BackgroundTracingRule> BackgroundTracingRule::PreemptiveRuleFromDict(
     if (!dict->GetString(kConfigRuleHistogramNameKey, &histogram_name))
       return nullptr;
 
+    // Check for the old naming.
     int histogram_value;
-    if (!dict->GetInteger(kConfigRuleHistogramValueKey, &histogram_value))
+    if (dict->GetInteger(kConfigRuleHistogramValueOldKey, &histogram_value))
+      return scoped_ptr<BackgroundTracingRule>(new HistogramRule(
+          histogram_name, histogram_value, std::numeric_limits<int>::max()));
+
+    int histogram_lower_value;
+    if (!dict->GetInteger(kConfigRuleHistogramValue1Key,
+                          &histogram_lower_value))
       return nullptr;
 
-    return scoped_ptr<BackgroundTracingRule>(
-        new HistogramRule(histogram_name, histogram_value));
+    int histogram_upper_value;
+    if (!dict->GetInteger(kConfigRuleHistogramValue2Key,
+                          &histogram_upper_value))
+      histogram_upper_value = std::numeric_limits<int>::max();
+
+    if (histogram_lower_value >= histogram_upper_value)
+      return nullptr;
+
+    return scoped_ptr<BackgroundTracingRule>(new HistogramRule(
+        histogram_name, histogram_lower_value, histogram_upper_value));
   }
 
   return nullptr;
