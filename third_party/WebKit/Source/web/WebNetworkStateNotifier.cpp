09ecb755da97c1ab74528311300af25355a4c575
jkarlin@chromium.org <jkarlin@chromium.org>
2015-09-21 17:57:06 +0000

    [NetInfo] Clean up after adding connection.downlinkMax
    Patch 1) https://crrev.com/1308943005
    Patch 2) https://crrev.com/1306423004
    Patch 3) https://crrev.com/1309903006 (this)
    
    BUG=412741
    
    Review URL: https://codereview.chromium.org/1309903006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202589 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebNetworkStateNotifier.cpp
+++ third_party/WebKit/Source/web/WebNetworkStateNotifier.cpp
@@ -41,11 +41,6 @@ void WebNetworkStateNotifier::setOnLine(bool onLine)
     networkStateNotifier().setOnLine(onLine);
 }
 
-void WebNetworkStateNotifier::setWebConnectionType(WebConnectionType type)
-{
-    setWebConnection(type, 0.0);
-}
-
 void WebNetworkStateNotifier::setWebConnection(WebConnectionType type, double maxBandwidthMbps)
 {
     networkStateNotifier().setWebConnection(type, maxBandwidthMbps);
