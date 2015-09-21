3c2c7e8d2b78129e1e0a9dce5fc6a10207b53e1f
michaelbai <michaelbai@chromium.org>
2015-09-21 14:15:06 -0700

    WebView: set version number in JNI initialization
    BUG=534518
    
    Review URL: https://codereview.chromium.org/1359713002
    
    Cr-Commit-Position: refs/heads/master@{#350016}

--- android_webview/lib/main/webview_jni_onload.cc
+++ android_webview/lib/main/webview_jni_onload.cc
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/lib/main/webview_jni_onload.h"
 
+#include "android_webview/common/aw_version_info_values.h"
 #include "android_webview/lib/main/aw_main_delegate.h"
 #include "android_webview/native/android_webview_jni_registrar.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_registrar.h"
+#include "base/android/library_loader/library_loader_hooks.h"
 #include "base/bind.h"
 #include "components/external_video_surface/component_jni_registrar.h"
 #include "components/navigation_interception/component_jni_registrar.h"
@@ -46,6 +48,7 @@ bool RegisterJNI(JNIEnv* env) {
 }
 
 bool Init() {
+  base::android::SetVersionNumber(PRODUCT_VERSION);
   content::SetContentMainDelegate(new android_webview::AwMainDelegate());
 
   // Initialize url lib here while we are still single-threaded, in case we use
