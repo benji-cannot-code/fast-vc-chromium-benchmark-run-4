51ff6b8f0fbe5f0ca0f0088e79e88b10e107bfb9
alancutter@chromium.org <alancutter@chromium.org>
2015-09-21 08:02:35 +0000

    Refactor fill and stroke animation to use PaintInterpolationType
    This patch is part of a refactor away from using StyleInterpolation subclasses
    to using per property InterpolationType singletons.
    This migrates uses of ColorStyleInterpolation over to PaintInterpolationType for
    the stroke and fill SVG paint properties.
    
    Change in behaviour:
    Neutral keyframes now represent unvisited underlying paint values.
    
    BUG=437696
    
    Review URL: https://codereview.chromium.org/1320343007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202577 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/core/animation/PaintPropertyFunctions.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PaintPropertyFunctions_h
+#define PaintPropertyFunctions_h
+
+#include "core/CSSPropertyNames.h"
+
+namespace blink {
+
+class ComputedStyle;
+class StyleColor;
+class Color;
+
+class PaintPropertyFunctions {
+public:
+    static bool getInitialColor(CSSPropertyID, StyleColor& result);
+    static bool getColor(CSSPropertyID, const ComputedStyle&, StyleColor& result);
+    static void setColor(CSSPropertyID, ComputedStyle&, const Color&);
+};
+
+} // namespace blink
+
+#endif // PaintPropertyFunctions_h
