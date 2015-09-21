530e0ebddeb8941afe2717f403b42206c46b911c
dnicoara <dnicoara@chromium.org>
2015-09-21 13:09:53 -0700

    [Ozone-Demo] Fix surfaceless rendering
    Need to schedule overlays on GLSurface since it provides the necessary
    queuing to schedule the overlays on the correct frame.
    
    Also added proper handling of swap result.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1354443004
    
    Cr-Commit-Position: refs/heads/master@{#350004}

--- ui/ozone/demo/surfaceless_gl_renderer.h
+++ ui/ozone/demo/surfaceless_gl_renderer.h
@@ -27,15 +27,17 @@ class SurfacelessGlRenderer : public GlRenderer {
  private:
   // GlRenderer:
   void RenderFrame() override;
+  void PostRenderFrameTask(gfx::SwapResult result) override;
 
   class BufferWrapper {
    public:
     BufferWrapper();
     ~BufferWrapper();
 
+    gfx::GLImage* image() const { return image_.get(); }
+
     bool Initialize(gfx::AcceleratedWidget widget, const gfx::Size& size);
     void BindFramebuffer();
-    void SchedulePlane();
 
    private:
     gfx::AcceleratedWidget widget_ = gfx::kNullAcceleratedWidget;
@@ -46,7 +48,7 @@ class SurfacelessGlRenderer : public GlRenderer {
     unsigned int gl_tex_ = 0;
   };
 
-  BufferWrapper buffers_[2];
+  scoped_ptr<BufferWrapper> buffers_[2];
   int back_buffer_ = 0;
 
   base::WeakPtrFactory<SurfacelessGlRenderer> weak_ptr_factory_;
