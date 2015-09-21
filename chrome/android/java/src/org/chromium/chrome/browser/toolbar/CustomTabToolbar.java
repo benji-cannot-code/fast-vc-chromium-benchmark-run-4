0721152399dc8a60dd0a662c2d47e618d49bb96c
ianwen <ianwen@chromium.org>
2015-09-21 13:59:48 -0700

    Let Custom Tabs Use Light Progressbar for Dark Toolbar
    Users requested us to make our progressbar more friendly for darker
    toolbar colors. This CL implements a white progressbar with a different
    background color, depending on the toolbar color.
    
    BUG=531622
    
    Review URL: https://codereview.chromium.org/1352133002
    
    Cr-Commit-Position: refs/heads/master@{#350014}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/CustomTabToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/CustomTabToolbar.java
@@ -61,8 +61,10 @@ public class CustomTabToolbar extends ToolbarLayout implements LocationBar,
     private ImageButton mCustomActionButton;
     private int mSecurityIconType;
     private boolean mShouldShowTitle;
-    private boolean mUseDarkColors;
     private ImageButton mCloseButton;
+
+    // Whether dark tint should be applied to icons and text.
+    private boolean mUseDarkColors;
 
     private CustomTabToolbarAnimationDelegate mAnimDelegate;
     private boolean mBackgroundColorSet;
@@ -294,10 +296,17 @@ public class CustomTabToolbar extends ToolbarLayout implements LocationBar,
         mTitleBar.setTextColor(titleTextColor);
 
         if (getProgressBar() != null) {
-            int progressBarBackgroundColorResource = mUseDarkColors
-                    ? R.color.progress_bar_background : R.color.progress_bar_background_white;
-            getProgressBar().setBackgroundColor(
-                    ApiCompatibilityUtils.getColor(resources, progressBarBackgroundColorResource));
+            if (mBackgroundColorSet && !mUseDarkColors) {
+                getProgressBar().setBackgroundColor(ColorUtils
+                        .getLightProgressbarBackground(getToolbarDataProvider().getPrimaryColor()));
+                getProgressBar().setForegroundColor(ApiCompatibilityUtils.getColor(resources,
+                        R.color.progress_bar_foreground_white));
+            } else {
+                int progressBarBackgroundColorResource = mUseDarkColors
+                        ? R.color.progress_bar_background : R.color.progress_bar_background_white;
+                getProgressBar().setBackgroundColor(ApiCompatibilityUtils.getColor(resources,
+                        progressBarBackgroundColorResource));
+            }
         }
     }
 
@@ -372,11 +381,11 @@ public class CustomTabToolbar extends ToolbarLayout implements LocationBar,
     @Override
     protected void onPrimaryColorChanged() {
         if (mBackgroundColorSet) return;
+        mBackgroundColorSet = true;
         int primaryColor = getToolbarDataProvider().getPrimaryColor();
         getBackground().setColor(primaryColor);
         mUseDarkColors = !ColorUtils.shoudUseLightForegroundOnBackground(primaryColor);
         updateVisualsForState();
-        mBackgroundColorSet = true;
     }
 
     @Override
