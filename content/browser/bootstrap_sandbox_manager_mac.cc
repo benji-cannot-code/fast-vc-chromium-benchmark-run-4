b74b6f6df2c8ebd5032389dba8f4e792e75ab5a9
rsesek <rsesek@chromium.org>
2015-09-21 16:41:41 -0700

    Revert of Enable the bootstrap sandbox by default. (patchset #3 id:40001 of https://codereview.chromium.org/1352623003/ )
    Reason for revert:
    Caused Mac 10.10 layout failures: http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.10/builds/4593
    
    Original issue's description:
    > Enable the bootstrap sandbox by default.
    >
    > This will place renderer processes under the bootstrap sandbox. This can be
    > disabled with --disable-bootstrap-sandbox.
    >
    > BUG=367863
    >
    > Committed: https://crrev.com/2dded5291bc5e9427721e1a06c74f2a80a86e8fa
    > Cr-Commit-Position: refs/heads/master@{#350006}
    
    TBR=avi@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=367863
    
    Review URL: https://codereview.chromium.org/1355043004
    
    Cr-Commit-Position: refs/heads/master@{#350065}

--- content/browser/bootstrap_sandbox_manager_mac.cc
+++ content/browser/bootstrap_sandbox_manager_mac.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/bootstrap_sandbox_manager_mac.h"
 
-#include "base/command_line.h"
 #include "base/logging.h"
 #include "base/mac/mac_util.h"
 #include "content/browser/browser_io_surface_manager_mac.h"
@@ -14,15 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/child_process_data.h"
 #include "content/public/browser/render_process_host.h"
-#include "content/public/common/content_switches.h"
 #include "sandbox/mac/bootstrap_sandbox.h"
 
 namespace content {
 
 // static
 bool BootstrapSandboxManager::ShouldEnable() {
-  return !base::CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kDisableBootstrapSandbox);
+  return false;
 }
 
 // static
