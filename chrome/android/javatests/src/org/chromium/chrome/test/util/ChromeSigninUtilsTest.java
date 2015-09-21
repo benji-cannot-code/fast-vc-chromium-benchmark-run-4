f4e377fdf9b159a5e7609160bd823af0b74b8ca4
rouslan <rouslan@chromium.org>
2015-09-21 10:03:23 -0700

    Clear Wallet data on sign out.
    Signing out should clear sync data before stopping sync controllers.
    This ensures that Wallet data is cleared. Desktop already does this.
    
    BUG=524158
    
    Review URL: https://codereview.chromium.org/1314673004
    
    Cr-Commit-Position: refs/heads/master@{#349947}

--- chrome/android/javatests/src/org/chromium/chrome/test/util/ChromeSigninUtilsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/test/util/ChromeSigninUtilsTest.java
@@ -31,7 +31,7 @@ public class ChromeSigninUtilsTest extends InstrumentationTestCase {
         super.setUp();
         mSigninUtil = new ChromeSigninUtils(getInstrumentation());
         mSigninController = ChromeSigninController.get(getInstrumentation().getTargetContext());
-        mSigninController.clearSignedInUser();
+        mSigninController.setSignedInAccountName(null);
         mSigninUtil.removeAllFakeAccountsFromOs();
         mSigninUtil.removeAllGoogleAccountsFromOs();
     }
@@ -146,7 +146,7 @@ public class ChromeSigninUtilsTest extends InstrumentationTestCase {
 
     @Override
     protected void tearDown() throws Exception {
-        mSigninController.clearSignedInUser();
+        mSigninController.setSignedInAccountName(null);
         mSigninUtil.removeAllFakeAccountsFromOs();
         mSigninUtil.removeAllGoogleAccountsFromOs();
         super.tearDown();
