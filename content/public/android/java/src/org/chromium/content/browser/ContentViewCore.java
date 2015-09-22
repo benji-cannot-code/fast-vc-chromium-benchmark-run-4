0fca010f43b078a3ff5703e44203578b363412da
s.singapati <s.singapati@gmail.com>
2015-09-22 14:09:15 -0700

    [Android] Fix <select> popup items alignment when "dir=rtl" on tablets.
    This CL plumbs "right_aligned" bit from WebContentsViewAndroid to
    ContentViewCore -> SelectPopupDropdown.java and sets the layout direction accordingly.
    
    BUG=375267
    
    Review URL: https://codereview.chromium.org/1350903005
    
    Cr-Commit-Position: refs/heads/master@{#350222}

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -2509,7 +2509,7 @@ public class ContentViewCore implements
     @SuppressWarnings("unused")
     @CalledByNative
     private void showSelectPopup(long nativeSelectPopupSourceFrame, Rect bounds, String[] items,
-            int[] enabled, boolean multiple, int[] selectedIndices) {
+            int[] enabled, boolean multiple, int[] selectedIndices, boolean rightAligned) {
         if (mContainerView.getParent() == null || mContainerView.getVisibility() != View.VISIBLE) {
             mNativeSelectPopupSourceFrame = nativeSelectPopupSourceFrame;
             selectPopupMenuItems(null);
@@ -2525,7 +2525,8 @@ public class ContentViewCore implements
             popupItems.add(new SelectPopupItem(items[i], enabled[i]));
         }
         if (DeviceFormFactor.isTablet(mContext) && !multiple && !isTouchExplorationEnabled()) {
-            mSelectPopup = new SelectPopupDropdown(this, popupItems, bounds, selectedIndices);
+            mSelectPopup = new SelectPopupDropdown(
+                    this, popupItems, bounds, selectedIndices, rightAligned);
         } else {
             mSelectPopup = new SelectPopupDialog(this, popupItems, multiple, selectedIndices);
         }
