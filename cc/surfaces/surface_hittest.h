fcbe57243daa68b761a892f4d0707aa93d7b4a9f
fsamuel <fsamuel@chromium.org>
2015-09-22 14:16:30 -0700

    Refactor Surfacehittest to return gfx::Transform
    I'm working on adding input capture to OOPIFs in Mandoline.
    When a view has capture, all input events with locations go
    to that view, bypassing most of hit testing. Input events
    need to be transformed before sending to the View, so this
    CL changes surface hittesting to give us a gfx::Transform
    that we can cache for later use.
    
    This CL also tweaks the way some of the tests are written
    to allow easily writing more points to test and reducing
    code duplication.
    
    BUG=496953
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1346323005
    
    Cr-Commit-Position: refs/heads/master@{#350231}

--- cc/surfaces/surface_hittest.h
+++ cc/surfaces/surface_hittest.h
@@ -34,7 +34,7 @@ class CC_SURFACES_EXPORT SurfaceHittest {
   // surface space.
   SurfaceId GetTargetSurfaceAtPoint(SurfaceId surface_id,
                                     const gfx::Point& point,
-                                    gfx::Point* transformed_point);
+                                    gfx::Transform* transform);
 
  private:
   bool GetTargetSurfaceAtPointInternal(
@@ -43,7 +43,7 @@ class CC_SURFACES_EXPORT SurfaceHittest {
       const gfx::Point& point_in_root_target,
       std::set<const RenderPass*>* referenced_passes,
       SurfaceId* out_surface_id,
-      gfx::Point* out_transformed_point);
+      gfx::Transform* out_transform);
 
   const RenderPass* GetRenderPassForSurfaceById(
       SurfaceId surface_id,
@@ -51,6 +51,7 @@ class CC_SURFACES_EXPORT SurfaceHittest {
 
   bool PointInQuad(const DrawQuad* quad,
                    const gfx::Point& point_in_render_pass_space,
+                   gfx::Transform* target_to_quad_transform,
                    gfx::Point* point_in_quad_space);
 
   SurfaceManager* const manager_;
