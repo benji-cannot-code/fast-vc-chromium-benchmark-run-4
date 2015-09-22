a26d2d9d3cac2abcff2ef476b687a64125bfcf2d
paritosh.in <paritosh.in@samsung.com>
2015-09-21 21:28:17 -0700

    Returning scoped_ptr<> instead of raw pointer in WebSocket::CreateWebSocket
    BUG=479898
    
    Review URL: https://codereview.chromium.org/1357693003
    
    Cr-Commit-Position: refs/heads/master@{#350106}

--- net/server/web_socket.h
+++ net/server/web_socket.h
@@ -28,9 +28,10 @@ class WebSocket final {
     FRAME_ERROR
   };
 
-  static WebSocket* CreateWebSocket(HttpServer* server,
-                                    HttpConnection* connection,
-                                    const HttpServerRequestInfo& request);
+  static scoped_ptr<WebSocket> CreateWebSocket(
+      HttpServer* server,
+      HttpConnection* connection,
+      const HttpServerRequestInfo& request);
 
   void Accept(const HttpServerRequestInfo& request);
   ParseResult Read(std::string* message);
