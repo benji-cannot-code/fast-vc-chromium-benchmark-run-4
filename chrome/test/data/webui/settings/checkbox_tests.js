e953d6cbb0f50a6b2a2bfb6cdb1b44ed44735f00
stevenjb <stevenjb@chromium.org>
2015-09-21 20:34:58 -0700

    Elim cr-checkbox
    cr-checkbox doesn't have any behaviors that we are concerned about or
    specific styling so it can be simply removed for the reasons mentioned
    in the issue.
    
    BUG=532540
    
    Review URL: https://codereview.chromium.org/1344303004
    
    Cr-Commit-Position: refs/heads/master@{#350102}

--- chrome/test/data/webui/settings/checkbox_tests.js
+++ chrome/test/data/webui/settings/checkbox_tests.js
@@ -3,43 +3,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/** @fileoverview Suite of tests for cr-checkbox. */
-cr.define('cr_checkbox', function() {
+/** @fileoverview Suite of tests for cr-settings-checkbox. */
+cr.define('cr_settings_checkbox', function() {
   function registerTests() {
-    suite('CrCheckbox', function() {
+    suite('CrSettingsCheckbox', function() {
       /**
        * Checkbox created before each test.
-       * @type {CrCheckbox}
+       * @type {CrSettingsCheckbox}
        */
       var checkbox;
 
-      // Import cr_checkbox.html before running suite.
+      /**
+       * Pref value used in tests, should reflect checkbox 'checked' attribute.
+       * @type {CrSettingsCheckbox}
+       */
+      var pref = {
+        key: 'test',
+        type: chrome.settingsPrivate.PrefType.BOOLEAN,
+        value: true
+      };
+
+      // Import cr_settings_checkbox.html before running suite.
       suiteSetup(function() {
         return PolymerTest.importHtml(
-            'chrome://resources/cr_elements/v1_0/cr_checkbox/cr_checkbox.html');
+            'chrome://md-settings/checkbox/checkbox.html');
       });
 
-      // Initialize a checked cr-checkbox before each test.
-      setup(function(done) {
+      // Initialize a checked cr-settings-checkbox before each test.
+      setup(function() {
         PolymerTest.clearBody();
-        checkbox = document.createElement('cr-checkbox');
-        checkbox.setAttribute('checked', '');
+        checkbox = document.createElement('cr-settings-checkbox');
+        checkbox.set('pref', pref);
         document.body.appendChild(checkbox);
-
-        // Allow for the checkbox to be created and attached.
-        setTimeout(done);
-      });
-
-      test('can toggle', function() {
-        assertTrue(checkbox.checked);
-
-        checkbox.toggle();
-        assertFalse(checkbox.checked);
-        assertFalse(checkbox.hasAttribute('checked'));
-
-        checkbox.toggle();
-        assertTrue(checkbox.checked);
-        assertTrue(checkbox.hasAttribute('checked'));
       });
 
       test('responds to checked attribute', function() {
@@ -47,9 +42,11 @@ cr.define('cr_checkbox', function() {
 
         checkbox.removeAttribute('checked');
         assertFalse(checkbox.checked);
+        assertFalse(pref.value);
 
         checkbox.setAttribute('checked', '');
         assertTrue(checkbox.checked);
+        assertTrue(pref.value);
       });
 
       test('fires a change event', function(done) {
@@ -64,6 +61,7 @@ cr.define('cr_checkbox', function() {
         checkbox.checked = false;
         checkbox.setAttribute('disabled', '');
         assertTrue(checkbox.disabled);
+        assertTrue(checkbox.$.checkbox.disabled);
 
         MockInteractions.tap(checkbox.$.checkbox);
         assertFalse(checkbox.checked);
--- ui/webui/resources/cr_elements/v1_0/compiled_resources.gyp
+++ ui/webui/resources/cr_elements/v1_0/compiled_resources.gyp
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'target_name': 'cr_elements_resources',
       'type': 'none',
       'dependencies': [
-        'cr_checkbox/compiled_resources.gyp:*',
         'network/compiled_resources.gyp:*',
         'policy/compiled_resources.gyp:*',
       ],
