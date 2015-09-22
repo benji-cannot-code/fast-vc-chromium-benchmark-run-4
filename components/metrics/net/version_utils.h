03f338e5f5b12a7657fd2642ffa79db07c3d2302
sdefresne <sdefresne@chromium.org>
2015-09-22 11:30:35 -0700

    Componentize chrome/common/metrics/version_utils.{cc,h}.
    Since version_info.{h,cc} files have been componentized those two files
    have no bad dependency and can be moved into metrics component (to allow
    sharing the code with iOS).
    
    Move them to //components/metrics:net target as they indirectly depends
    on //net (via //components/version_info dependency on //ui/base).
    
    BUG=534435
    
    Review URL: https://codereview.chromium.org/1359643004
    
    Cr-Commit-Position: refs/heads/master@{#350198}

--- components/metrics/net/version_utils.h
+++ components/metrics/net/version_utils.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_COMMON_METRICS_VERSION_UTILS_H_
-#define CHROME_COMMON_METRICS_VERSION_UTILS_H_
+#ifndef COMPONENTS_METRICS_NET_VERSION_UTILS_H_
+#define COMPONENTS_METRICS_NET_VERSION_UTILS_H_
 
 #include <string>
 
@@ -27,4 +27,4 @@ SystemProfileProto::Channel AsProtobufChannel(
 
 }  // namespace metrics
 
-#endif  // CHROME_COMMON_METRICS_VERSION_UTILS_H_
+#endif  // COMPONENTS_METRICS_NET_VERSION_UTILS_H_
