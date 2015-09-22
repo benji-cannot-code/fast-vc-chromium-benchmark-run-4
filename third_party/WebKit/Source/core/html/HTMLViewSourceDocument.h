6a4aa76f2d3964eec6bad1af53bd52fc0dab80a7
loyso@chromium.org <loyso@chromium.org>
2015-09-22 08:03:44 +0000

    Non-Oilpan Memory: Fix HTMLViewSourceDocument and WebLeakDetector.
    1) RenderViewImplTest.DidFailProvisionalLoadWithErrorForCancellation test was leaking with HTMLViewSourceDocument.
    2) V8PerIsolateData::from(isolate)->ensureScriptRegexpContext() was leaking in WebLeakDetectorImpl::collectGarbageAndGetDOMCounts
    because of cycle.
    
    This is required by chromium part:
    https://codereview.chromium.org/1363523002
    
    BUG=506433
    
    Review URL: https://codereview.chromium.org/1360723003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLViewSourceDocument.h
+++ third_party/WebKit/Source/core/html/HTMLViewSourceDocument.h
@@ -48,6 +48,10 @@ public:
 
     void addSource(const String&, HTMLToken&, SourceAnnotation);
 
+#if !ENABLE(OILPAN)
+    void dispose() override;
+#endif
+
     DECLARE_VIRTUAL_TRACE();
 
 private:
