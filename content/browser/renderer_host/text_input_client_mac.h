1091d65d75a3aa9acbfc8dc2eefd284693838eba
shuchen <shuchen@chromium.org>
2015-09-22 10:28:55 -0700

    Clean up the useless GetAttributedSubstringFromRange() and the related IPC.
    This is based on 2 CLs:
     - https://codereview.chromium.org/1313553006
     - https://codereview.chromium.org/1327743002
    
    BUG=121917
    
    Review URL: https://codereview.chromium.org/1326763003
    
    Cr-Commit-Position: refs/heads/master@{#350182}

--- content/browser/renderer_host/text_input_client_mac.h
+++ content/browser/renderer_host/text_input_client_mac.h
@@ -70,7 +70,6 @@ class CONTENT_EXPORT TextInputClientMac {
   // allow the Get*() methods to continue/return.
   void SetCharacterIndexAndSignal(NSUInteger index);
   void SetFirstRectAndSignal(NSRect first_rect);
-  void SetSubstringAndSignal(NSAttributedString* string);
 
   // This async method is invoked from RenderWidgetHostViewCocoa's
   // -quickLookWithEvent:, when the user taps a word using 3 fingers.
@@ -116,7 +115,6 @@ class CONTENT_EXPORT TextInputClientMac {
 
   NSUInteger character_index_;
   NSRect first_rect_;
-  base::scoped_nsobject<NSAttributedString> substring_;
 
   base::Lock lock_;
   base::ConditionVariable condition_;
