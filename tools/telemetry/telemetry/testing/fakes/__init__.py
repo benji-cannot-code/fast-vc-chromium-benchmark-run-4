2ef40805c69e1a2a46f3357fbb6908f976e92101
kbr <kbr@chromium.org>
2015-09-22 15:13:53 -0700

    Add unit test for glsl-construct-vec-mat-index's expectations.
    Add the ability to override the browser_type of the fake browser in
    telemetry.testing.fakes.
    
    The unit test is working as expected. Need to run this test repeatedly
    on Windows to understand whether the test expectation isn't being
    picked up, or whether the flaky test handling is unable to recover
    from the kind of failure that's happening.
    
    BUG=534697
    
    Review URL: https://codereview.chromium.org/1358943002
    
    Cr-Commit-Position: refs/heads/master@{#350254}

--- tools/telemetry/telemetry/testing/fakes/__init__.py
+++ tools/telemetry/telemetry/testing/fakes/__init__.py
@@ -143,6 +143,7 @@ class _FakeBrowser(object):
     self._tabs = _FakeTabList(self)
     self._returned_system_info = FakeSystemInfo()
     self._platform = platform
+    self._browser_type = 'release'
 
   @property
   def platform(self):
@@ -166,6 +167,16 @@ class _FakeBrowser(object):
     Incoming argument must be an instance of FakeSystemInfo."""
     assert isinstance(incoming, FakeSystemInfo)
     self._returned_system_info = incoming
+
+  @property
+  def browser_type(self):
+    """The browser_type this browser claims to be ('debug', 'release', etc.)"""
+    return self._browser_type
+
+  @browser_type.setter
+  def browser_type(self, incoming):
+    """Allows setting of the browser_type."""
+    self._browser_type = incoming
 
   @property
   def credentials(self):
