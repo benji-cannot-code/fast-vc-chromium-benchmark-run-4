2ffe300c852b7077483f6ab5017a51695503fb01
edwardjung <edwardjung@chromium.org>
2015-09-22 03:32:35 -0700

    Enable ability to disable the offline easter egg to all platforms.
    Was previously only available to CrOS devices.
    
    BUG=471738
    
    Review URL: https://codereview.chromium.org/1351203004
    
    Cr-Commit-Position: refs/heads/master@{#350136}

--- chrome/common/localized_error.cc
+++ chrome/common/localized_error.cc
@@ -586,7 +586,6 @@ void LocalizedError::GetStrings(int error_code,
     error_strings->SetString("primaryParagraph",
         l10n_util::GetStringUTF16(options.summary_resource_id));
 
-#if defined(OS_CHROMEOS)
     base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
 
     // Check if easter egg should be disabled.
@@ -595,8 +594,6 @@ void LocalizedError::GetStrings(int error_code,
       error_strings->SetString("disabledEasterEgg",
           l10n_util::GetStringUTF16(IDS_ERRORPAGE_FUN_DISABLED));
     }
-#endif
-
   } else {
     // Set summary message in the details.
     summary->SetString("msg",
