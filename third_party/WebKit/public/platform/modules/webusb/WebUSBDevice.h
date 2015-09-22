96121b055080a7948fc5aeda0ddf7d1395ea1832
reillyg@chromium.org <reillyg@chromium.org>
2015-09-22 04:41:25 +0000

    Add getConfiguration method to USBDevice.
    This allows a site to determine the current configuration state of a
    device. WebUSBDevice::getConfiguration is left as non-pure until
    Chromium provides an implementation.
    
    BUG=492204
    
    Review URL: https://codereview.chromium.org/1352283005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202621 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/modules/webusb/WebUSBDevice.h
+++ third_party/WebKit/public/platform/modules/webusb/WebUSBDevice.h
@@ -18,6 +18,7 @@ struct WebUSBTransferInfo;
 
 using WebUSBDeviceOpenCallbacks = WebCallbacks<void, const WebUSBError&>;
 using WebUSBDeviceCloseCallbacks = WebCallbacks<void, const WebUSBError&>;
+using WebUSBDeviceGetConfigurationCallbacks = WebCallbacks<uint8_t, const WebUSBError&>;
 using WebUSBDeviceSetConfigurationCallbacks = WebCallbacks<void, const WebUSBError&>;
 using WebUSBDeviceClaimInterfaceCallbacks = WebCallbacks<void, const WebUSBError&>;
 using WebUSBDeviceReleaseInterfaceCallbacks = WebCallbacks<void, const WebUSBError&>;
@@ -72,6 +73,10 @@ public:
     // Closes the device.
     // Ownership of the WebUSBDeviceCloseCallbacks is transferred to the client.
     virtual void close(WebUSBDeviceCloseCallbacks*) = 0;
+
+    // Gets the active configuration of the device.
+    // Ownership of the WebUSBDeviceGetConfigurationCallbacks is transferred to the client.
+    virtual void getConfiguration(WebUSBDeviceGetConfigurationCallbacks*) { }
 
     // Sets the active configuration for the device.
     // Ownership of the WebUSBDeviceSetConfigurationCallbacks is transferred to the client.
