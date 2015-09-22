3cd9ab4abae8913cc13e97db197caf5990aeb7e8
ki.stfu <ki.stfu@gmail.com>
2015-09-21 17:56:52 -0700

    Cleanup: Pass std::string as const reference from chromeos/
    Passing std::string by reference can prevent extra copying of object.
    
    BUG=367418
    TEST=
    R=zelidrag@chromium.org,derat@chromium.org,gauravsh@chromium.org
    
    Review URL: https://codereview.chromium.org/1353323002
    
    Cr-Commit-Position: refs/heads/master@{#350078}

--- chromeos/network/onc/onc_certificate_importer_impl_unittest.cc
+++ chromeos/network/onc/onc_certificate_importer_impl_unittest.cc
@@ -98,7 +98,8 @@ class ONCCertificateImporterImplTest : public testing::Test {
     web_trust_certificates_ = onc_trusted_certificates;
   }
 
-  void AddCertificatesFromFile(std::string filename, bool expected_success) {
+  void AddCertificatesFromFile(const std::string& filename,
+                               bool expected_success) {
     scoped_ptr<base::DictionaryValue> onc =
         test_utils::ReadTestDictionary(filename);
     scoped_ptr<base::Value> certificates_value;
@@ -123,7 +124,7 @@ class ONCCertificateImporterImplTest : public testing::Test {
     private_list_ = ListCertsInPrivateSlot();
   }
 
-  void AddCertificateFromFile(std::string filename,
+  void AddCertificateFromFile(const std::string& filename,
                               net::CertType expected_type,
                               std::string* guid) {
     std::string guid_temporary;
