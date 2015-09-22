02f094fef802c56ba56739bd770f670ae4e20ad7
ben <ben@chromium.org>
2015-09-22 14:47:30 -0700

    Make CapabilityFilter be part of Identity
    R=yzshen@chromium.org
    http://crbug.com/533085
    
    Committed: https://crrev.com/5403e28c9d9bfcf00b12721b6dc96c00f6d1ed24
    Cr-Commit-Position: refs/heads/master@{#349815}
    
    Review URL: https://codereview.chromium.org/1354003002
    
    Cr-Commit-Position: refs/heads/master@{#350241}

--- mojo/shell/application_manager_unittest.cc
+++ mojo/shell/application_manager_unittest.cc
@@ -688,8 +688,7 @@ TEST_F(ApplicationManagerTest, TestEndApplicationClosure) {
 
   bool called = false;
   scoped_ptr<ConnectToApplicationParams> params(new ConnectToApplicationParams);
-  params->SetURLInfo(GURL("test:test"));
-  params->set_filter(GetPermissiveCapabilityFilter());
+  params->SetTargetURL(GURL("test:test"));
   params->set_on_application_end(
       base::Bind(&QuitClosure, base::Unretained(&called)));
   application_manager_->ConnectToApplication(params.Pass());
@@ -717,10 +716,8 @@ TEST(ApplicationManagerTest2, ContentHandlerConnectionGetsRequestorURL) {
 
   bool called = false;
   scoped_ptr<ConnectToApplicationParams> params(new ConnectToApplicationParams);
-  params->set_originator_identity(Identity(requestor_url));
-  params->set_originator_filter(GetPermissiveCapabilityFilter());
-  params->SetURLInfo(GURL("test:test"));
-  params->set_filter(GetPermissiveCapabilityFilter());
+  params->set_source(Identity(requestor_url));
+  params->SetTargetURL(GURL("test:test"));
   params->set_on_application_end(
       base::Bind(&QuitClosure, base::Unretained(&called)));
   application_manager.ConnectToApplication(params.Pass());
@@ -789,10 +786,8 @@ TEST(ApplicationManagerTest2,
     base::RunLoop run_loop;
     scoped_ptr<ConnectToApplicationParams> params(
         new ConnectToApplicationParams);
-    params->set_originator_identity(Identity(requestor_url));
-    params->set_originator_filter(GetPermissiveCapabilityFilter());
-    params->SetURLInfo(GURL("test:test"));
-    params->set_filter(GetPermissiveCapabilityFilter());
+    params->set_source(Identity(requestor_url));
+    params->SetTargetURL(GURL("test:test"));
     params->set_connect_callback([&content_handler_id, &run_loop](uint32_t t) {
       content_handler_id = t;
       run_loop.Quit();
@@ -807,10 +802,8 @@ TEST(ApplicationManagerTest2,
     base::RunLoop run_loop;
     scoped_ptr<ConnectToApplicationParams> params(
         new ConnectToApplicationParams);
-    params->set_originator_identity(Identity(requestor_url));
-    params->set_originator_filter(GetPermissiveCapabilityFilter());
-    params->SetURLInfo(GURL("test:test"));
-    params->set_filter(GetPermissiveCapabilityFilter());
+    params->set_source(Identity(requestor_url));
+    params->SetTargetURL(GURL("test:test"));
     params->set_connect_callback([&content_handler_id2, &run_loop](uint32_t t) {
       content_handler_id2 = t;
       run_loop.Quit();
@@ -847,10 +840,8 @@ TEST(ApplicationManagerTest2, DifferedContentHandlersGetDifferentIDs) {
     base::RunLoop run_loop;
     scoped_ptr<ConnectToApplicationParams> params(
         new ConnectToApplicationParams);
-    params->set_originator_identity(Identity(requestor_url));
-    params->set_originator_filter(GetPermissiveCapabilityFilter());
-    params->SetURLInfo(GURL("test:test"));
-    params->set_filter(GetPermissiveCapabilityFilter());
+    params->set_source(Identity(requestor_url));
+    params->SetTargetURL(GURL("test:test"));
     params->set_connect_callback([&content_handler_id, &run_loop](uint32_t t) {
       content_handler_id = t;
       run_loop.Quit();
@@ -879,10 +870,8 @@ TEST(ApplicationManagerTest2, DifferedContentHandlersGetDifferentIDs) {
     base::RunLoop run_loop;
     scoped_ptr<ConnectToApplicationParams> params(
         new ConnectToApplicationParams);
-    params->set_originator_identity(Identity(requestor_url));
-    params->set_originator_filter(GetPermissiveCapabilityFilter());
-    params->SetURLInfo(GURL("test2:test2"));
-    params->set_filter(GetPermissiveCapabilityFilter());
+    params->set_source(Identity(requestor_url));
+    params->SetTargetURL(GURL("test2:test2"));
     params->set_connect_callback([&content_handler_id2, &run_loop](uint32_t t) {
       content_handler_id2 = t;
       run_loop.Quit();
@@ -902,8 +891,7 @@ TEST_F(ApplicationManagerTest,
 
   uint32_t content_handler_id = 1u;
   scoped_ptr<ConnectToApplicationParams> params(new ConnectToApplicationParams);
-  params->SetURLInfo(GURL("test:test"));
-  params->set_filter(GetPermissiveCapabilityFilter());
+  params->SetTargetURL(GURL("test:test"));
   params->set_connect_callback(
       [&content_handler_id](uint32_t t) { content_handler_id = t; });
   application_manager_->ConnectToApplication(params.Pass());
