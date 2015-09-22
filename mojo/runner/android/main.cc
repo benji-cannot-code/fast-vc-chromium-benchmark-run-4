462b150346891a3f01b10fe53f43603fb029c907
msw <msw@chromium.org>
2015-09-22 16:30:01 -0700

    Init Android Mojo Shell Context before configuring.
    Context's ApplicationManager is null until Init is called.
    The ApplicationManager construction was delayed by:
      https://codereview.chromium.org/1352663002/diff/140001/mojo/runner/context.cc
    
    This should Android crashes on running mandoline, mojo shell, and most apptests:
      mandoline/tools/android/run_mandoline.py
      mojo/tools/android_mojo_shell.py --apk MojoRunnerApptests.apk --gtest_list_tests mojo:clipboard_apptests
      mojo/tools/apptest_runner.py out/android_Debug
      http://build.chromium.org/p/chromium.mojo/builders/Chromium%20Mojo%20Android
      (resource provider apptests still fail locally after this change)
    
    BUG=533595
    TEST=Fewer Android crashes for Mojo.
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/1358283002
    
    Cr-Commit-Position: refs/heads/master@{#350263}

--- mojo/runner/android/main.cc
+++ mojo/runner/android/main.cc
@@ -56,6 +56,7 @@ LazyInstance<base::android::ScopedJavaGlobalRef<jobject>> g_main_activiy =
     LAZY_INSTANCE_INITIALIZER;
 
 void ConfigureAndroidServices(Context* context) {
+  CHECK(context->application_manager());
   context->application_manager()->SetLoaderForURL(
       make_scoped_ptr(
           new UIApplicationLoader(make_scoped_ptr(new mus::AndroidLoader()),
@@ -141,8 +142,8 @@ static void Init(JNIEnv* env,
   g_java_message_loop.Get().reset(new base::MessageLoopForUI);
   base::MessageLoopForUI::current()->Start();
 
-  ConfigureAndroidServices(shell_context);
   shell_context->Init();
+  ConfigureAndroidServices(shell_context);
 
   // This is done after the main message loop is started since it may post
   // tasks. This is consistent with the ordering from the desktop version of
