8adbb6a63c74b546bb60c73199b4e1c6a2b35674
hendrikw <hendrikw@chromium.org>
2015-09-22 14:18:20 -0700

    gpu: Add flag to disable virtual contexts
    We've run into a few bugs that we suspect were caused by
    virtual contexts, and it would be nice to have a quick
    way to turn them off to confirm.
    
    Review URL: https://codereview.chromium.org/1354473002
    
    Cr-Commit-Position: refs/heads/master@{#350233}

--- gpu/command_buffer/service/gpu_switches.cc
+++ gpu/command_buffer/service/gpu_switches.cc
@@ -77,6 +77,10 @@ const char kEmulateShaderPrecision[] = "emulate-shader-precision";
 // and driver. This is used in GPU rasterization.
 const char kEnableGLPathRendering[] = "enable-gl-path-rendering";
 
+// Provide an override to disable virtual contexts, some systems may be
+// experiencing problems with it, and we need an easy way to confirm.
+const char kDisableVirtualContexts[] = "disable-virtual-contexts";
+
 const char* kGpuSwitches[] = {
     kCompileShaderAlwaysSucceeds,
     kDisableGLErrorLimit,
@@ -97,6 +101,7 @@ const char* kGpuSwitches[] = {
     kGLShaderIntermOutput,
     kEmulateShaderPrecision,
     kEnableGLPathRendering,
+    kDisableVirtualContexts,
 };
 
 const int kNumGpuSwitches = arraysize(kGpuSwitches);
