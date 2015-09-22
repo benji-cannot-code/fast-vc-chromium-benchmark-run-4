916346f21678234a64cfe9813ec2634f91f6601d
twifkak <twifkak@chromium.org>
2015-09-22 11:12:57 -0700

    Fix precache tests on svelte and reenable.
    Remove the call to PrivacyPreferencesManager#shouldPrerender() in
    setUp(), which was breaking on svelte bots. As of
    http://crrev.com/c8e7c27, it's no longer a requirement.
    
    These tests were disabled in http://crrev.com/cc6ee39.
    
    BUG=534297
    
    Review URL: https://codereview.chromium.org/1357013007
    
    Cr-Commit-Position: refs/heads/master@{#350194}

--- chrome/android/javatests/src/org/chromium/chrome/browser/precache/PrecacheLauncherTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/precache/PrecacheLauncherTest.java
@@ -6,11 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.precache;
 
 import android.content.Context;
+import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.base.ThreadUtils;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
-import org.chromium.chrome.browser.preferences.privacy.PrivacyPreferencesManager;
 import org.chromium.chrome.browser.sync.ProfileSyncService;
 import org.chromium.content.browser.test.NativeLibraryTestBase;
 
@@ -90,11 +89,6 @@ public class PrecacheLauncherTest extends NativeLibraryTestBase {
                 // on the fly.
                 mSync = new StubProfileSyncService();
                 ProfileSyncService.overrideForTests(mSync);
-
-                // This is currently the default, but let's verify that, lest it ever change and we
-                // get confusing test failures later.
-                assertTrue(PrivacyPreferencesManager.getInstance(getTargetContext())
-                                   .shouldPrerender());
             }
         });
     }
@@ -106,8 +100,7 @@ public class PrecacheLauncherTest extends NativeLibraryTestBase {
         super.tearDown();
     }
 
-    // @SmallTest
-    @DisabledTest //  crbug.com/534297
+    @SmallTest
     @Feature({"Precache"})
     public void testUpdateEnabled_SyncNotReady_ThenDisabled() {
         mLauncher.updateEnabled(getTargetContext());
@@ -124,8 +117,7 @@ public class PrecacheLauncherTest extends NativeLibraryTestBase {
         assertEquals(EnumSet.of(FailureReason.NATIVE_SHOULD_RUN_IS_FALSE), failureReasons());
     }
 
-    // @SmallTest
-    @DisabledTest //  crbug.com/534297
+    @SmallTest
     @Feature({"Precache"})
     public void testUpdateEnabled_SyncNotReady_ThenEnabled() {
         mLauncher.updateEnabled(getTargetContext());
@@ -143,8 +135,7 @@ public class PrecacheLauncherTest extends NativeLibraryTestBase {
         assertEquals(EnumSet.noneOf(FailureReason.class), failureReasons());
     }
 
-    // @SmallTest
-    @DisabledTest //  crbug.com/534297
+    @SmallTest
     @Feature({"Precache"})
     public void testUpdateEnabled_Disabled_ThenEnabled() {
         setSyncInitialized(true);
@@ -159,8 +150,7 @@ public class PrecacheLauncherTest extends NativeLibraryTestBase {
         assertEquals(EnumSet.noneOf(FailureReason.class), failureReasons());
     }
 
-    // @SmallTest
-    @DisabledTest //  crbug.com/534297
+    @SmallTest
     @Feature({"Precache"})
     public void testUpdateEnabled_Enabled_ThenDisabled() {
         mLauncher.setShouldRun(true);
