8656c12355f96ac61bc5a52b4139c1de3bff2d65
calamity <calamity@chromium.org>
2015-09-22 22:10:43 -0700

    Fix crash in SearchController::OpenResult.
    BUG=534772
    
    Review URL: https://codereview.chromium.org/1364553003
    
    Cr-Commit-Position: refs/heads/master@{#350300}

--- ui/app_list/views/search_result_tile_item_view.cc
+++ ui/app_list/views/search_result_tile_item_view.cc
@@ -82,7 +82,8 @@ void SearchResultTileItemView::OnResultDestroying() {
 
   if (item_)
     item_->RemoveObserver(this);
-  item_ = NULL;
+
+  SetSearchResult(nullptr);
 }
 
 void SearchResultTileItemView::ShowContextMenuForView(
