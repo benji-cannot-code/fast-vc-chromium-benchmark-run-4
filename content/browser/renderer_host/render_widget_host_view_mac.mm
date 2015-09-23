518f03c9e136b422d5a8ce3cea3e42f62a667341
ericrk <ericrk@chromium.org>
2015-09-22 23:12:11 -0700

    Enable pinch-zoom telemetry on Mac
    This change adds a SyntheticGestureTargetMac which is able to interpret the
    touches sent from telemetry's pinch simulator and convert these into trackpad
    zoom gestures.
    
    BUG=
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_10_10_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1349813002
    
    Cr-Commit-Position: refs/heads/master@{#350316}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_widget_helper.h"
 #include "content/browser/renderer_host/render_widget_host_delegate.h"
 #include "content/browser/renderer_host/render_widget_host_input_event_router.h"
+#import "content/browser/renderer_host/input/synthetic_gesture_target_mac.h"
 #import "content/browser/renderer_host/render_widget_host_view_mac_dictionary_helper.h"
 #import "content/browser/renderer_host/render_widget_host_view_mac_editcommand_helper.h"
 #include "content/browser/renderer_host/render_widget_resize_helper_mac.h"
@@ -1637,6 +1638,14 @@ void RenderWidgetHostViewMac::WheelEventAck(
   // to see it (no-op wheel events are ignored by the event dispatcher)
   if (event.deltaX || event.deltaY)
     [cocoa_view_ processedWheelEvent:event consumed:consumed];
+}
+
+scoped_ptr<SyntheticGestureTarget>
+RenderWidgetHostViewMac::CreateSyntheticGestureTarget() {
+  RenderWidgetHostImpl* host =
+      RenderWidgetHostImpl::From(GetRenderWidgetHost());
+  return scoped_ptr<SyntheticGestureTarget>(
+      new SyntheticGestureTargetMac(host, cocoa_view_));
 }
 
 uint32_t RenderWidgetHostViewMac::GetSurfaceIdNamespace() {
