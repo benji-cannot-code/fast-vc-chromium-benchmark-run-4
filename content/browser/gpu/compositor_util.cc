ac6790779c9e462a1c212c667bef731591cd4b62
andresantoso <andresantoso@chromium.org>
2015-09-23 15:57:15 -0700

    Mac: Enable zero copy rasterization on Mac
    This is needed for promoting tile quads into overlays.
    
    BUG=533634
    
    Review URL: https://codereview.chromium.org/1350953005
    
    Cr-Commit-Position: refs/heads/master@{#350379}

--- content/browser/gpu/compositor_util.cc
+++ content/browser/gpu/compositor_util.cc
@@ -202,7 +202,11 @@ int NumberOfRendererRasterThreads() {
 bool IsZeroCopyUploadEnabled() {
   const base::CommandLine& command_line =
       *base::CommandLine::ForCurrentProcess();
+#if defined(OS_MACOSX)
+  return !command_line.HasSwitch(switches::kDisableZeroCopy);
+#else
   return command_line.HasSwitch(switches::kEnableZeroCopy);
+#endif
 }
 
 bool IsPersistentGpuMemoryBufferEnabled() {
