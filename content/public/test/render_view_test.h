387f6d33babfb268291a5f3fef937c13f70c7276
loyso <loyso@chromium.org>
2015-09-22 21:29:49 -0700

    RenderViewTests: Implement proper shutdown.
    Interleave gc cycles with message loop pumping.
    Sequentially calling GC didn't work so we had memory leaks.
    
    Depends on blink part: https://codereview.chromium.org/1360723003/
    
    BUG=506433
    BUG=46571
    BUG=484760
    
    Review URL: https://codereview.chromium.org/1363523002
    
    Cr-Commit-Position: refs/heads/master@{#350298}

--- content/public/test/render_view_test.h
+++ content/public/test/render_view_test.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/WebKit/public/platform/Platform.h"
 #include "third_party/WebKit/public/web/WebFrame.h"
+#include "third_party/WebKit/public/web/WebLeakDetector.h"
 
 struct ViewMsg_Resize_Params;
 
@@ -46,7 +47,7 @@ class RendererMainPlatformDelegate;
 class RendererBlinkPlatformImplNoSandboxImpl;
 class RenderView;
 
-class RenderViewTest : public testing::Test {
+class RenderViewTest : public testing::Test, blink::WebLeakDetectorClient {
  public:
   // A special BlinkPlatformImpl class for getting rid off the dependency to the
   // sandbox, which is not available in RenderViewTest.
@@ -172,6 +173,9 @@ class RenderViewTest : public testing::Test {
   void SetUp() override;
 
   void TearDown() override;
+
+  // blink::WebLeakDetectorClient implementation.
+  void onLeakDetectionComplete(const Result& result) override;
 
   base::MessageLoop msg_loop_;
   scoped_ptr<FakeCompositorDependencies> compositor_deps_;
