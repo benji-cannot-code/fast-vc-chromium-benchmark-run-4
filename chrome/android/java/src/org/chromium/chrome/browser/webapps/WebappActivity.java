4dfcf55725084e02ad00fe3b91339130215534bb
lalitm <lalitm@google.com>
2015-09-24 09:51:48 -0700

    webapps: Add cleanup task when opening up WebappActivity to clean old web apps
    Since there is no sure way of knowing when a web app has been removed from
    the Home screen, a heuristic has to be used instead.
    
    The heuristic is that if the web app has not been opened in 3 months then it
    can probably be safely removed. The full cleanup check is run once a month when
    starting a web app so we don't choke the system every time a web app is opened.
    
    BUG=535174
    
    Review URL: https://codereview.chromium.org/1359383002
    
    Cr-Commit-Position: refs/heads/master@{#350579}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
@@ -54,6 +54,7 @@ public class WebappActivity extends FullScreenActivity {
 
     private final WebappInfo mWebappInfo;
     private AsyncTask<Void, Void, Void> mCleanupTask;
+    private boolean mOldWebappCleanupStarted;
 
     private WebContentsObserver mWebContentsObserver;
 
@@ -157,6 +158,13 @@ public class WebappActivity extends FullScreenActivity {
             updateTaskDescription();
         }
         super.onResume();
+
+        // Kick off the old web app cleanup (if we haven't already) now that we have queued the
+        // current web app's storage to be opened.
+        if (!mOldWebappCleanupStarted) {
+            WebappRegistry.unregisterOldWebapps(this, System.currentTimeMillis());
+            mOldWebappCleanupStarted = true;
+        }
     }
 
     @Override
