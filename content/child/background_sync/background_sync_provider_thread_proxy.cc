31230a3b21cccea67220213d8153d0b5ba7a78e8
amistry <amistry@chromium.org>
2015-09-23 17:11:38 -0700

    Fix a use of the non-thread-safe mojo::Callback.
    mojo::Callback is not thread-safe. It must be created, copied, and
    destroyed on the same thread. It is not safe to post a task to another
    task runner which has a mojo::Callback as a bound parameter. Because of
    this, a level of indirection is necessary to ensure a mojo::Callback is
    always used on its original thread.
    
    For BackgroundSyncClientImpl, a map is used to save the callback to the
    Sync method and associate that callback to an id. That id is then safe
    to be passed around between threads.
    
    BUG=532058
    TESTED=Locally augmented mojo::internal::SharedData with a ThreadChecker to
           ensure mojo::Callbacks are only referenced on a single thread.
    
    Review URL: https://codereview.chromium.org/1363533002
    
    Cr-Commit-Position: refs/heads/master@{#350400}

--- content/child/background_sync/background_sync_provider_thread_proxy.cc
+++ content/child/background_sync/background_sync_provider_thread_proxy.cc
@@ -77,7 +77,8 @@ LazyInstance<ThreadLocalPointer<BackgroundSyncProviderThreadProxy>>::Leaky
     g_sync_provider_tls = LAZY_INSTANCE_INITIALIZER;
 
 void DuplicateRegistrationHandleCallbackOnSWThread(
-    const BackgroundSyncService::DuplicateRegistrationHandleCallback& callback,
+    const BackgroundSyncProviderThreadProxy::
+        DuplicateRegistrationHandleCallback& callback,
     BackgroundSyncError error,
     SyncRegistrationPtr registration) {
   callback.Run(error, registration.Pass());
@@ -85,7 +86,8 @@ void DuplicateRegistrationHandleCallbackOnSWThread(
 
 void DuplicateRegistrationHandleCallbackOnMainThread(
     int worker_thread_id,
-    const BackgroundSyncService::DuplicateRegistrationHandleCallback& callback,
+    const BackgroundSyncProviderThreadProxy::
+        DuplicateRegistrationHandleCallback& callback,
     BackgroundSyncError error,
     SyncRegistrationPtr registration) {
   WorkerTaskRunner::Instance()->PostTask(
@@ -228,8 +230,7 @@ void BackgroundSyncProviderThreadProxy::notifyWhenDone(
 
 void BackgroundSyncProviderThreadProxy::DuplicateRegistrationHandle(
     int64 handle_id,
-    const BackgroundSyncService::DuplicateRegistrationHandleCallback&
-        callback) {
+    const DuplicateRegistrationHandleCallback& callback) {
   main_thread_task_runner_->PostTask(
       FROM_HERE,
       base::Bind(&BackgroundSyncProvider::DuplicateRegistrationHandle,
