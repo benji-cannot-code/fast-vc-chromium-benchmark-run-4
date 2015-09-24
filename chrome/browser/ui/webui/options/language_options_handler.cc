07b36c9b6b08aa7c785656cad1bd51b22a7a2f84
michaelpg <michaelpg@chromium.org>
2015-09-24 13:28:12 -0700

    Remove unused LanguageOptionsHandler::GetProductName()
    Review URL: https://codereview.chromium.org/1363263002
    
    Cr-Commit-Position: refs/heads/master@{#350648}

--- chrome/browser/ui/webui/options/language_options_handler.cc
+++ chrome/browser/ui/webui/options/language_options_handler.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
-#include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "content/public/browser/user_metrics.h"
 #include "content/public/browser/web_ui.h"
@@ -113,10 +112,6 @@ base::ListValue* LanguageOptionsHandler::GetLanguageList() {
   }
 
   return language_list;
-}
-
-base::string16 LanguageOptionsHandler::GetProductName() {
-  return l10n_util::GetStringUTF16(IDS_PRODUCT_NAME);
 }
 
 void LanguageOptionsHandler::SetApplicationLocale(
