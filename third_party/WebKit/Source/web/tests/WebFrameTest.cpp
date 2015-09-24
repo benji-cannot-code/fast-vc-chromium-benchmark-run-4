82d2ade8fc71301fcbfbc5d02d2e8f86a9ce2eee
pdr <pdr@chromium.org>
2015-09-24 10:46:43 -0700

    Deflake WebFrameTest.ContextNotificationsLoadUnload
    This patch marks ContextNotificationsLoadUnload as not flaky any longer.
    
    BUG=239878
    
    Review URL: https://codereview.chromium.org/1363063003
    
    Cr-Commit-Position: refs/heads/master@{#350598}

--- third_party/WebKit/Source/web/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/web/tests/WebFrameTest.cpp
@@ -3462,8 +3462,7 @@ public:
     }
 };
 
-// TODO(aa): Deflake this test.
-TEST_P(ParameterizedWebFrameTest, FLAKY_ContextNotificationsLoadUnload)
+TEST_P(ParameterizedWebFrameTest, ContextNotificationsLoadUnload)
 {
     v8::HandleScope handleScope(v8::Isolate::GetCurrent());
 
