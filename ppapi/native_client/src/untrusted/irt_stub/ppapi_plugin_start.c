eccb31d4b4fb05bd05376c65368aa0336052cfde
phosek <phosek@chromium.org>
2015-09-23 20:23:22 -0700

    Build PPAPI NaCl Glibc tests with GN
    Support building PPAPI NaCl tests with GCC/Glibc-based toolchain.
    
    BUG=462791
    
    Review URL: https://codereview.chromium.org/1333673002
    
    Cr-Commit-Position: refs/heads/master@{#350467}

--- ppapi/native_client/src/untrusted/irt_stub/ppapi_plugin_start.c
+++ ppapi/native_client/src/untrusted/irt_stub/ppapi_plugin_start.c
@@ -17,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/native_client/src/untrusted/irt_stub/thread_creator.h"
 
 static void fatal_error(const char *message) {
-  write(2, message, strlen(message));
+  ssize_t wrote __attribute__((unused));
+  wrote = write(2, message, strlen(message));
   _exit(127);
 }
 
