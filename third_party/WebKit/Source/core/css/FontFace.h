349fc556bda2f9b1d734956da6701bd423d38862
rob.buis <rob.buis@samsung.com>
2015-09-23 21:57:22 -0700

    Restrict font-family in @font-face to <family-name>
    Restrict font-family in @font-face to <family-name> by calling
    consumeFamilyName directly. To make sure no generic-name is being
    parsed add the test fontfaceset-invalid-family-names.html.
    
    Behavior matches FF.
    
    BUG=499780
    
    Review URL: https://codereview.chromium.org/1363233002
    
    Cr-Commit-Position: refs/heads/master@{#350479}

--- third_party/WebKit/Source/core/css/FontFace.h
+++ third_party/WebKit/Source/core/css/FontFace.h
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class CSSFontFace;
-class CSSValueList;
+class CSSPrimitiveValue;
 class DOMArrayBuffer;
 class DOMArrayBufferView;
 class Document;
@@ -126,7 +126,7 @@ private:
     void setPropertyFromString(const Document*, const String&, CSSPropertyID, ExceptionState* = 0);
     bool setPropertyFromStyle(const StylePropertySet&, CSSPropertyID);
     bool setPropertyValue(PassRefPtrWillBeRawPtr<CSSValue>, CSSPropertyID);
-    bool setFamilyValue(CSSValueList*);
+    bool setFamilyValue(CSSPrimitiveValue*);
     void loadInternal(ExecutionContext*);
     ScriptPromise fontStatusPromise(ScriptState*);
 
