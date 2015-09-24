63fcf7af56571ce6a08da4b581086e71852bcfeb
knn <knn@chromium.org>
2015-09-24 08:44:02 -0700

    [Android]Don't check for child account/EDU device for every activity.
    Extract a Forced Signin Processor out of FirstRun Signin Processor which
    checks for child accounts and EDU device and is only triggered:
    -Once per Chrome Application startup.
    -On updates to accounts on the device.
    
    Invariants:
    FRE ToS checks and FRE pending signin checks still happen on every activity for now.
    
    Reland r1357643002
    w/o case-sensitive FirstRunSignInProcessor.java -> FirstRunSigninProcessor.java
    
    Dependent patchsets:
    http://crrev.com/1369503002
    http://crrev.com/1353393002
    
    TBR=bauerb@chromium.org
    
    BUG=518830
    
    Review URL: https://codereview.chromium.org/1368663002
    
    Cr-Commit-Position: refs/heads/master@{#350562}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencer.java
@@ -75,7 +75,7 @@ public abstract class FirstRunFlowSequencer  {
                 mHasChildAccount = hasChildAccount();
                 processFreEnvironment();
             }
-        }.start(mActivity);
+        }.start(mActivity.getApplicationContext());
     }
 
     /**
@@ -174,6 +174,10 @@ public abstract class FirstRunFlowSequencer  {
         freProperties.putBoolean(AccountFirstRunFragment.IS_CHILD_ACCOUNT, mHasChildAccount);
 
         onFlowIsKnown(mActivity, freProperties);
+        if (mHasChildAccount || forceEduSignIn) {
+            // Child and Edu forced signins are processed independently.
+            FirstRunSignInProcessor.setFirstRunFlowSignInComplete(context, true);
+        }
     }
 
     /**
