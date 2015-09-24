e1fb40747a32e07761192ece6d5cf4378f12e74b
reveman <reveman@chromium.org>
2015-09-23 22:08:58 -0700

    mojo: Add proper implementation of DetachFromClient to OutputSurfaceMojo.
    BUG=523411,525811
    TBR=sky@chromium.org
    TEST=mojo_runner --use-temporary-user-data-dir --enable-html-viewer-test-interface --use-x11-test-config --override-use-gl-with-osmesa-for-tests --gtest_filter=AXProviderTest.HelloWorld mojo:html_viewer_apptests
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1362243002
    
    Cr-Commit-Position: refs/heads/master@{#350483}

--- mojo/cc/output_surface_mojo.cc
+++ mojo/cc/output_surface_mojo.cc
@@ -31,6 +31,11 @@ bool OutputSurfaceMojo::BindToClient(cc::OutputSurfaceClient* client) {
   return cc::OutputSurface::BindToClient(client);
 }
 
+void OutputSurfaceMojo::DetachFromClient() {
+  surface_.reset();
+  cc::OutputSurface::DetachFromClient();
+}
+
 void OutputSurfaceMojo::SwapBuffers(cc::CompositorFrame* frame) {
   // TODO(fsamuel, rjkroege): We should probably throttle compositor frames.
   client_->DidSwapBuffers();
