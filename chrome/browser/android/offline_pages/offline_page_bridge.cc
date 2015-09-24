d743b055b6908f2ef98a6503cd477cdc5bf9b738
jianli <jianli@chromium.org>
2015-09-23 17:59:31 -0700

    Update access info when an offline page is being visited
    We need to update last access time and increase access count.
    
    BUG=491352
    
    Patch
    
    Review URL: https://codereview.chromium.org/1345043002
    
    Cr-Commit-Position: refs/heads/master@{#350425}

--- chrome/browser/android/offline_pages/offline_page_bridge.cc
+++ chrome/browser/android/offline_pages/offline_page_bridge.cc
@@ -62,7 +62,8 @@ void ToJavaOfflinePageList(JNIEnv* env,
         ConvertUTF8ToJavaString(env, offline_page.url.spec()).obj(),
         offline_page.bookmark_id,
         ConvertUTF8ToJavaString(env, offline_page.GetOfflineURL().spec()).obj(),
-        offline_page.file_size);
+        offline_page.file_size,
+        offline_page.access_count);
   }
 }
 
@@ -127,7 +128,8 @@ ScopedJavaLocalRef<jobject> OfflinePageBridge::GetPageByBookmarkId(
       env, ConvertUTF8ToJavaString(env, offline_page->url.spec()).obj(),
       offline_page->bookmark_id,
       ConvertUTF8ToJavaString(env, offline_page->GetOfflineURL().spec()).obj(),
-      offline_page->file_size);
+      offline_page->file_size,
+      offline_page->access_count);
 }
 
 void OfflinePageBridge::SavePage(JNIEnv* env,
@@ -152,6 +154,12 @@ void OfflinePageBridge::SavePage(JNIEnv* env,
   offline_page_model_->SavePage(
       url, bookmark_id, archiver.Pass(),
       base::Bind(&SavePageCallback, j_callback_ref, url));
+}
+
+void OfflinePageBridge::MarkPageAccessed(JNIEnv* env,
+                                         jobject obj,
+                                         jlong bookmark_id) {
+  offline_page_model_->MarkPageAccessed(bookmark_id);
 }
 
 void OfflinePageBridge::DeletePage(JNIEnv* env,
