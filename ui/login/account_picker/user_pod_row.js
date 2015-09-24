6b18c67b6c9a180aea339f95d42f88e02a2eb6fe
rogerta <rogerta@chromium.org>
2015-09-23 17:49:44 -0700

    Put back code that was incorrectly removed in r342867.
    While testing I found that CL https://codereview.chromium.org/1243213002/ broke saving passwords on desktop.  This might also explain crbug.com/533401
    
    BUG=520469,533401
    
    Review URL: https://codereview.chromium.org/1364473005
    
    Cr-Commit-Position: refs/heads/master@{#350420}

--- ui/login/account_picker/user_pod_row.js
+++ ui/login/account_picker/user_pod_row.js
@@ -1943,6 +1943,14 @@ cr.define('login', function() {
     },
 
     /** @override */
+    get mainInput() {
+      if (this.user.needsSignin)
+        return this.passwordElement;
+      else
+        return this.nameElement;
+    },
+
+    /** @override */
     update: function() {
       this.imageElement.src = this.user.userImage;
       this.nameElement.textContent = this.user.displayName;
