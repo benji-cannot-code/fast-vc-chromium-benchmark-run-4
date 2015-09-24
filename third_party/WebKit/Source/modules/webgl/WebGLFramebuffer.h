905d4f9f766484b6f79f148636d6ed5d869ce00b
kbr <kbr@chromium.org>
2015-09-24 11:39:51 -0700

    Fixed crash during destruction of WebGLFramebuffer objects.
    Somewhat speculative fix based on feedback from sigbjornf@. Have not
    yet verified this with the CL that introduced the random crashes.
    
    BUG=534524
    
    Review URL: https://codereview.chromium.org/1364893002
    
    Cr-Commit-Position: refs/heads/master@{#350613}

--- third_party/WebKit/Source/modules/webgl/WebGLFramebuffer.h
+++ third_party/WebKit/Source/modules/webgl/WebGLFramebuffer.h
@@ -136,6 +136,7 @@ private:
     typedef HeapHashMap<GLenum, Member<WebGLAttachment>> AttachmentMap;
 
     AttachmentMap m_attachments;
+    bool m_destructionInProgress;
 
     bool m_hasEverBeenBound;
 
