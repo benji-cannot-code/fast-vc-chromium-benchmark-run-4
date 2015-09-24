bc3fe2abd29d4090229a3824a1be75ccd3069c38
nparker <nparker@chromium.org>
2015-09-23 19:30:46 -0700

    Don't check URLs against Safe Browsing if not supported.
    Default all android builds to safe_browsing=3 mode.
    
    This will fix the crash in chrome_public_apk when built with //clank
    checkout.  It'll also provide more consistency between public and private
    builds since both will use safe_browsing==3.  At a later
    date, the public build will get support for safe browsing.
    
    In a follow-up CL I'd like to disable the Safe Browsing settings UI
    when it's not supported, assuming we can determine that before the UI
    is created.
    
    BUG=533105
    
    Review URL: https://codereview.chromium.org/1343293004
    
    Cr-Commit-Position: refs/heads/master@{#350429}

--- chrome/browser/safe_browsing/test_database_manager.cc
+++ chrome/browser/safe_browsing/test_database_manager.cc
@@ -11,6 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 
+bool TestSafeBrowsingDatabaseManager::IsSupported() const {
+  NOTIMPLEMENTED();
+  return false;
+}
+
 bool TestSafeBrowsingDatabaseManager::CanCheckUrl(const GURL& url) const {
   NOTIMPLEMENTED();
   return false;
