58d7bb2a4520773bc486b2c30344cdae5bfd366e
dbeam <dbeam@chromium.org>
2015-09-23 19:54:29 -0700

    MD Downloads: fix controlled-by color and flicker issues
    R=michaelpg@chromium.org
    BUG=425626
    
    NOTRY=true  # see try runs from patchset 1
    
    Review URL: https://codereview.chromium.org/1366743003
    
    Cr-Commit-Position: refs/heads/master@{#350453}

--- chrome/browser/resources/md_downloads/item.js
+++ chrome/browser/resources/md_downloads/item.js
@@ -41,6 +41,12 @@ cr.define('downloads', function() {
         value: true,
       },
 
+      controlledBy_: {
+        computed: 'computeControlledBy_(data_.by_ext_id, data_.by_ext_name)',
+        type: String,
+        value: '',
+      },
+
       i18n_: {
         readOnly: true,
         type: Object,
@@ -102,7 +108,9 @@ cr.define('downloads', function() {
     },
 
     observers: [
-      'observeControlledBy_(data_.by_ext_id, data_.by_ext_name)',
+      // TODO(dbeam): this gets called way more when I observe data_.by_ext_id
+      // and data_.by_ext_name directly. Why?
+      'observeControlledBy_(controlledBy_)',
     ],
 
     ready: function() {
@@ -140,6 +148,16 @@ cr.define('downloads', function() {
     computeCompletelyOnDisk_: function() {
       return this.data_.state == downloads.States.COMPLETE &&
              !this.data_.file_externally_removed;
+    },
+
+    /** @private */
+    computeControlledBy_: function() {
+      if (!this.data_.by_ext_id || !this.data_.by_ext_name)
+        return '';
+
+      var url = 'chrome://extensions#' + this.data_.by_ext_id;
+      var name = this.data_.by_ext_name;
+      return loadTimeData.getStringF('controlledByUrl', url, name);
     },
 
     /** @private */
@@ -252,13 +270,7 @@ cr.define('downloads', function() {
 
     /** @private */
     observeControlledBy_: function() {
-      var html = '';
-      if (this.data_.by_ext_id && this.data_.by_ext_name) {
-        var url = 'chrome://extensions#' + this.data_.by_ext_id;
-        var name = this.data_.by_ext_name;
-        html = loadTimeData.getStringF('controlledByUrl', url, name);
-      }
-      this.$['controlled-by'].innerHTML = html;
+      this.$['controlled-by'].innerHTML = this.controlledBy_;
     },
 
     /** @private */
