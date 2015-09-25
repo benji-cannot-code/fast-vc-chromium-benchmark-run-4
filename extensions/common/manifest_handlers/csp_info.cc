ec947e39df3456883f6c8b69684503943597ae4b
jww <jww@chromium.org>
2015-09-25 16:45:36 -0700

    Disallow CSP source * matching of data:, blob:, and filesystem: URLs
    The CSP spec specifically excludes matching of data:, blob:, and
    filesystem: URLs with the source '*' wildcard. This adds checks to make
    sure that doesn't happen, along with tests.
    
    BUG=534570
    R=mkwst@chromium.org
    
    Review URL: https://codereview.chromium.org/1361763005
    
    Cr-Commit-Position: refs/heads/master@{#350950}

--- extensions/common/manifest_handlers/csp_info.cc
+++ extensions/common/manifest_handlers/csp_info.cc
@@ -35,7 +35,7 @@ const char kDefaultPlatformAppContentSecurityPolicy[] =
     // Platform apps can only use local resources by default.
     "default-src 'self' blob: filesystem: chrome-extension-resource:;"
     // For remote resources, they can fetch them via XMLHttpRequest.
-    " connect-src *;"
+    " connect-src * data: blob: filesystem:;"
     // And serve them via data: or same-origin (blob:, filesystem:) URLs
     " style-src " PLATFORM_APP_LOCAL_CSP_SOURCES " 'unsafe-inline';"
     " img-src " PLATFORM_APP_LOCAL_CSP_SOURCES ";"
@@ -46,7 +46,7 @@ const char kDefaultPlatformAppContentSecurityPolicy[] =
     //    spotty connectivity.
     // 2. Fetching via XHR and serving via blob: URLs currently does not allow
     //    streaming or partial buffering.
-    " media-src *;";
+    " media-src * data: blob: filesystem:;";
 
 int GetValidatorOptions(Extension* extension) {
   int options = csp_validator::OPTIONS_NONE;
