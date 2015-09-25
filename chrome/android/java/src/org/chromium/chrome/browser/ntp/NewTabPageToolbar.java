979e7a45c815d8c716d27095453d890d110addd5
aurimas <aurimas@chromium.org>
2015-09-25 15:39:20 -0700

    Remove MenuTrim experiment.
    BUG=512327
    
    Review URL: https://codereview.chromium.org/1371503003
    
    Cr-Commit-Position: refs/heads/master@{#350933}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageToolbar.java
@@ -16,10 +16,8 @@ import android.widget.TextView;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.appmenu.ChromeAppMenuPropertiesDelegate;
 import org.chromium.chrome.browser.offlinepages.OfflinePageBridge;
 import org.chromium.chrome.browser.widget.TintedDrawable;
-import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.widget.Toast;
 
 /**
@@ -59,13 +57,6 @@ public class NewTabPageToolbar extends LinearLayout implements OnLongClickListen
         TintedDrawable icon = TintedDrawable.constructTintedDrawable(getResources(), drawableId);
         ApiCompatibilityUtils.setCompoundDrawablesRelativeWithIntrinsicBounds(
                 textView, icon, null, null, null);
-        if (!DeviceFormFactor.isTablet(getContext())
-                && !ChromeAppMenuPropertiesDelegate.isMenuTrimmingExperimentEnabled()) {
-            // On phones, no text is shown, but long pressing shows a tooltip.
-            textView.setText("");
-            textView.setCompoundDrawablePadding(0);
-            button.setOnLongClickListener(this);
-        }
 
         return button;
     }
