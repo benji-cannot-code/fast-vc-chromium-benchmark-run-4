a78a03a217e8e0ff183165563c4ed1402b4595ca
reillyg <reillyg@chromium.org>
2015-09-24 19:44:12 -0700

    Always add direction flag to endpoint number when submitting transfers.
    While the Chrome USB API documentation says that the "endpoint"
    parameter should be the endpoint address, not the endpoint number, this
    code used to always make sure the high bit was set when the transfer
    direction was IN. This change reinforces that pattern and cleans up some
    duplicated transfer submission code.
    
    BUG=535169
    
    Review URL: https://codereview.chromium.org/1366043003
    
    Cr-Commit-Position: refs/heads/master@{#350752}

--- device/usb/usb_device_handle_impl.h
+++ device/usb/usb_device_handle_impl.h
@@ -69,7 +69,7 @@ class UsbDeviceHandleImpl : public UsbDeviceHandle {
                        const TransferCallback& callback) override;
 
   void IsochronousTransfer(UsbEndpointDirection direction,
-                           uint8_t endpoint,
+                           uint8_t endpoint_number,
                            scoped_refptr<net::IOBuffer> buffer,
                            size_t length,
                            unsigned int packets,
@@ -78,7 +78,7 @@ class UsbDeviceHandleImpl : public UsbDeviceHandle {
                            const TransferCallback& callback) override;
 
   void GenericTransfer(UsbEndpointDirection direction,
-                       uint8_t endpoint,
+                       uint8_t endpoint_number,
                        scoped_refptr<net::IOBuffer> buffer,
                        size_t length,
                        unsigned int timeout,
@@ -144,27 +144,8 @@ class UsbDeviceHandleImpl : public UsbDeviceHandle {
       scoped_refptr<base::TaskRunner> callback_task_runner,
       const TransferCallback& callback);
 
-  void BulkTransferInternal(
-      UsbEndpointDirection direction,
-      uint8_t endpoint,
-      scoped_refptr<net::IOBuffer> buffer,
-      size_t length,
-      unsigned int timeout,
-      scoped_refptr<base::TaskRunner> callback_task_runner,
-      const TransferCallback& callback);
-
-  void InterruptTransferInternal(
-      UsbEndpointDirection direction,
-      uint8_t endpoint,
-      scoped_refptr<net::IOBuffer> buffer,
-      size_t length,
-      unsigned int timeout,
-      scoped_refptr<base::TaskRunner> callback_task_runner,
-      const TransferCallback& callback);
-
   void IsochronousTransferInternal(
-      UsbEndpointDirection direction,
-      uint8_t endpoint,
+      uint8_t endpoint_address,
       scoped_refptr<net::IOBuffer> buffer,
       size_t length,
       unsigned int packets,
@@ -174,8 +155,7 @@ class UsbDeviceHandleImpl : public UsbDeviceHandle {
       const TransferCallback& callback);
 
   void GenericTransferInternal(
-      UsbEndpointDirection direction,
-      uint8_t endpoint,
+      uint8_t endpoint_address,
       scoped_refptr<net::IOBuffer> buffer,
       size_t length,
       unsigned int timeout,
