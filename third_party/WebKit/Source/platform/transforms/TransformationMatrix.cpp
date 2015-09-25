8e48e3cab309426ba1ffee949327d6909b05959d
thakis <thakis@chromium.org>
2015-09-25 15:31:11 -0700

    Allocate TransformationMatrix on PartitionAlloc
    TransformationMatrix needs to be 16-byte aligned on 64-bit platforms
    due to use of vector instructions. PartitionAlloc has an ability to allocate
    a multiple-of-16-byte object with 16-byte alignment, but Oilpan doesn't have
    the ability.
    
    So this CL changes CSSMatrix and DOMMatrixReadOnly so that they hold
    TransformationMatrix via an OwnPtr, which makes sure that TransformationMatrix
    is allocated on PartitionAlloc.
    
    Patch by haraken@chromium.org, reviewed at
    https://codereview.chromium.org/1361373002/
    This is identical to this patch, except that the check in
    checkAlignment() is done conditionally.
    
    BUG=534853
    TBR=haraken
    
    Review URL: https://codereview.chromium.org/1359363005
    
    Cr-Commit-Position: refs/heads/master@{#350931}

--- third_party/WebKit/Source/platform/transforms/TransformationMatrix.cpp
+++ third_party/WebKit/Source/platform/transforms/TransformationMatrix.cpp
@@ -626,6 +626,7 @@ static void slerp(double qa[4], const double qb[4], double t)
 
 TransformationMatrix::TransformationMatrix(const AffineTransform& t)
 {
+    checkAlignment();
     setMatrix(t.a(), t.b(), t.c(), t.d(), t.e(), t.f());
 }
 
