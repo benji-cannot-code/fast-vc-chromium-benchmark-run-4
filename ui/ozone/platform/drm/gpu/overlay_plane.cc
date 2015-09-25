14ea8db51f55152d96ecfda8f7bff22e4ccc96a1
dnicoara <dnicoara@chromium.org>
2015-09-25 15:49:51 -0700

    Pass scanout buffers as const references
    BUG=none
    
    Review URL: https://codereview.chromium.org/1370843002
    
    Cr-Commit-Position: refs/heads/master@{#350936}

--- ui/ozone/platform/drm/gpu/overlay_plane.cc
+++ ui/ozone/platform/drm/gpu/overlay_plane.cc
@@ -5,18 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/ozone/platform/drm/gpu/overlay_plane.h"
 
-#include "base/logging.h"
 #include "ui/ozone/platform/drm/gpu/scanout_buffer.h"
 
 namespace ui {
 
-OverlayPlane::OverlayPlane(scoped_refptr<ScanoutBuffer> buffer)
+OverlayPlane::OverlayPlane(const scoped_refptr<ScanoutBuffer>& buffer)
     : buffer(buffer),
       plane_transform(gfx::OVERLAY_TRANSFORM_INVALID),
       display_bounds(gfx::Point(), buffer->GetSize()),
       crop_rect(0, 0, 1, 1) {}
 
-OverlayPlane::OverlayPlane(scoped_refptr<ScanoutBuffer> buffer,
+OverlayPlane::OverlayPlane(const scoped_refptr<ScanoutBuffer>& buffer,
                            int z_order,
                            gfx::OverlayTransform plane_transform,
                            const gfx::Rect& display_bounds,
@@ -25,8 +24,7 @@ OverlayPlane::OverlayPlane(scoped_refptr<ScanoutBuffer> buffer,
       z_order(z_order),
       plane_transform(plane_transform),
       display_bounds(display_bounds),
-      crop_rect(crop_rect) {
-}
+      crop_rect(crop_rect) {}
 
 OverlayPlane::~OverlayPlane() {
 }
