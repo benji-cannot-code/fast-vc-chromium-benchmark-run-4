2038748ccda5699700acbdd48a37b423ceacd3ba
xlai <xlai@chromium.org>
2015-09-25 14:05:24 -0700

    Implement Asynchronous image encoding for Canvas.toBlob
    Main thread creates a new thread to carry out the computation-intensive asynchronous image encoding
    operation. Afterwards the encoded image is passed back to the main thread to create blob and invoke user
    callback.
    
    Empty blob is passed to user callback in the senarios when the canvas has zero pixels or when the image
    encoding fails.
    
    This code review issue continues from the previous issue at https://codereview.chromium.org/1331443009/
    before the blink and chromium are merged.
    
    BUG=523414
    BUG=67587
    
    Review URL: https://codereview.chromium.org/1355333005
    
    Cr-Commit-Position: refs/heads/master@{#350910}

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.h
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptValue.h"
 #include "bindings/core/v8/UnionTypesCore.h"
 #include "core/CoreExport.h"
+#include "core/dom/DOMTypedArray.h"
 #include "core/dom/Document.h"
 #include "core/dom/DocumentVisibilityObserver.h"
 #include "core/fileapi/FileCallback.h"
@@ -43,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/GraphicsTypes3D.h"
 #include "platform/graphics/ImageBufferClient.h"
 #include "platform/heap/Handle.h"
+#include "public/platform/WebThread.h"
 
 #define CanvasDefaultInterpolationQuality InterpolationLow
 
@@ -78,6 +80,8 @@ class CORE_EXPORT HTMLCanvasElement final : public HTMLElement, public DocumentV
 public:
     DECLARE_NODE_FACTORY(HTMLCanvasElement);
     ~HTMLCanvasElement() override;
+
+    static WebThread* getToBlobThreadInstance();
 
     void addObserver(CanvasObserver*);
     void removeObserver(CanvasObserver*);
@@ -115,7 +119,7 @@ public:
     String toDataURL(const String& mimeType, const ScriptValue& qualityArgument, ExceptionState&) const;
     String toDataURL(const String& mimeType, ExceptionState& exceptionState) const { return toDataURL(mimeType, ScriptValue(), exceptionState); }
 
-    void toBlob(FileCallback*, const String& mimeType, const ScriptValue& qualityArgument, ExceptionState&) const;
+    void toBlob(FileCallback*, const String& mimeType, const ScriptValue& qualityArgument, ExceptionState&);
     void toBlob(FileCallback* callback, const String& mimeType, ExceptionState& exceptionState) { return toBlob(callback, mimeType, ScriptValue(), exceptionState); }
 
     // Used for rendering
@@ -212,7 +216,10 @@ private:
     bool paintsIntoCanvasBuffer() const;
 
     ImageData* toImageData(SourceDrawingBuffer) const;
-    String toDataURLInternal(const String& mimeType, const double* quality, SourceDrawingBuffer) const;
+    String toDataURLInternal(const String& mimeType, const double& quality, SourceDrawingBuffer) const;
+
+    static void encodeImageAsync(DOMUint8ClampedArray* imagedata, IntSize imageSize, FileCallback*, const String& mimeType, double quality);
+    static void createBlobAndCall(PassOwnPtr<Vector<char>> encodedImage, const String& mimeType, FileCallback*);
 
     WillBeHeapHashSet<RawPtrWillBeWeakMember<CanvasObserver>> m_observers;
 
