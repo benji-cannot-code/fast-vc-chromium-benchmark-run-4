9404b492fb10674b53b92a6d09c50c9dc492a066
cmumford <cmumford@chromium.org>
2015-09-25 11:15:59 -0700

    Revert of [NaCl SDK] nacl_io: Add support for basic socketpairs. (patchset #5 id:80001 of https://codereview.chromium.org/1335783005/ )
    Reason for revert:
    undefined reference to 'nacl_io::UnixNode::UnixNode(nacl_io::Filesystem*, nacl_io::UnixNode const&)'
    
    Original issue's description:
    > [NaCl SDK] nacl_io: Add support for basic socketpairs.
    >
    > This is a very basic implementation of a unix socketpair. The socket
    > nodes have type S_IFSOCK.
    >
    > The event emitter are set up in a master/slave configuration, the master
    > owns the fifos and slave handles. The slave has a strong reference to
    > its master (since it depends on it, cannot survive alone). Each emitter
    > in the pair has their own status, but share the pair of fifos and use a
    > single lock in the master to synchronize their access.
    >
    > Shutdown should be rather simple to bolt on to this implementation.
    >
    > CQ_EXTRA_TRYBOTS=tryserver.chromium.linux:linux_nacl_sdk;tryserver.chromium.mac:mac_nacl_sdk
    > BUG=532095
    >
    > Committed: https://crrev.com/e40c14e14250a5b3ea235ae0ec846b84d2be73ae
    > Cr-Commit-Position: refs/heads/master@{#350849}
    
    TBR=binji@chromium.org,bradnelson@chromium.org,sbc@chromium.org,avallee@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=532095
    
    Review URL: https://codereview.chromium.org/1373563003
    
    Cr-Commit-Position: refs/heads/master@{#350865}

--- native_client_sdk/src/libraries/nacl_io/socket/socket_node.cc
+++ native_client_sdk/src/libraries/nacl_io/socket/socket_node.cc
@@ -376,8 +376,6 @@ Error SocketNode::RecvFrom(const HandleAttr& attr,
                            socklen_t* addrlen,
                            int* out_len) {
   PP_Resource addr = 0;
-  if (0 == socket_resource_)
-    return EBADF;
   Error err = RecvHelper(attr, buf, len, flags, &addr, out_len);
   if (0 == err && 0 != addr) {
     if (src_addr)
@@ -395,6 +393,9 @@ Error SocketNode::RecvHelper(const HandleAttr& attr,
                              int flags,
                              PP_Resource* addr,
                              int* out_len) {
+  if (0 == socket_resource_)
+    return EBADF;
+
   if (TestStreamFlags(SSF_RECV_ENDOFSTREAM)) {
     *out_len = 0;
     return 0;
@@ -429,11 +430,6 @@ Error SocketNode::Send(const HandleAttr& attr,
                        size_t len,
                        int flags,
                        int* out_len) {
-  if (0 == socket_resource_)
-    return EBADF;
-
-  if (0 == remote_addr_)
-    return ENOTCONN;
   return SendHelper(attr, buf, len, flags, remote_addr_, out_len);
 }
 
@@ -451,9 +447,6 @@ Error SocketNode::SendTo(const HandleAttr& attr,
   if (0 == addr)
     return EINVAL;
 
-  if (0 == socket_resource_)
-    return EBADF;
-
   Error err = SendHelper(attr, buf, len, flags, addr, out_len);
   filesystem_->ppapi()->ReleaseResource(addr);
   return err;
@@ -465,6 +458,12 @@ Error SocketNode::SendHelper(const HandleAttr& attr,
                              int flags,
                              PP_Resource addr,
                              int* out_len) {
+  if (0 == socket_resource_)
+    return EBADF;
+
+  if (0 == addr)
+    return ENOTCONN;
+
   int ms = write_timeout_;
   if ((flags & MSG_DONTWAIT) || !attr.IsBlocking())
     ms = 0;
