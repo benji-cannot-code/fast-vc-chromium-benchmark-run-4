d5c9e91e461a781ce1e13c8962e0085c9cb015bb
brettw <brettw@chromium.org>
2015-09-24 18:16:22 -0700

    Add StringPiece getters for GURL, minor cleanup.
    This adds *_piece() getters for each URL component which avoids intermediate string copies.
    
    Does some extra commenting and cleanup in gurl.h with new comments. The getters and test functions for each component type are now grouped. I removed references to future additions to encoding parameters which we will never do at this point. This also removes an unnecessary lower-casing step in GURL::SchemeIs since things are known lowercase, so scheme comparisons should be faster.
    
    I did a quick grep for obvious cases of some of the getters that can be replaced with the *_piece versions and updated them. In net_util there is an additional cleanup where a more complicated test could be replaced with EndsWith.
    
    Review URL: https://codereview.chromium.org/1360863003
    
    Cr-Commit-Position: refs/heads/master@{#350733}

--- content/browser/appcache/appcache_histograms.cc
+++ content/browser/appcache/appcache_histograms.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 static std::string OriginToCustomHistogramSuffix(const GURL& origin_url) {
-  if (origin_url.host() == "docs.google.com")
+  if (origin_url.host_piece() == "docs.google.com")
     return ".Docs";
   return std::string();
 }
