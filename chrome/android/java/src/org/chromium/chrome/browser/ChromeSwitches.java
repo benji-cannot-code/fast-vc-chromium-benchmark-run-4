8a3f3d0118d227f9fadf34ce91d3c768e3da4342
tbansal <tbansal@chromium.org>
2015-09-25 16:08:10 -0700

    Provide a switch to disable LoFi snackbar
    This will make it possible to Speed Index tests with and
    without Lo-Fi.
    
    BUG=535776
    
    Review URL: https://codereview.chromium.org/1364683006
    
    Cr-Commit-Position: refs/heads/master@{#350939}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeSwitches.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeSwitches.java
@@ -83,6 +83,11 @@ public abstract class ChromeSwitches {
      */
     public static final String DISABLE_READER_MODE_BOTTOM_BAR = "disable-reader-mode-bottom-bar";
 
+    /**
+     * Disable Lo-Fi snackbar.
+     */
+    public static final String DISABLE_LOFI_SNACKBAR = "disable-lo-fi-snackbar";
+
     ///////////////////////////////////////////////////////////////////////////////////////////////
     // Native Switches
     ///////////////////////////////////////////////////////////////////////////////////////////////
