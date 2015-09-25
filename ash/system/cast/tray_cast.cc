62f51cb93810f741cea0676985c36ff52ea093f7
jdufault <jdufault@chromium.org>
2015-09-25 15:06:42 -0700

    Do not show the cast status tray item if there are no cast receivers.
    The tray item was visible by default until it got notification from the
    extension that there are no devices. If the Chromecast extension does not use
    the cast.devicesPrivate API, then the device update notification will never
    arrive, and hence the tray will remain visible.
    
    With this CL, we no longer wait for the notification before hiding the tray
    item.
    
    BUG=534678
    
    Review URL: https://codereview.chromium.org/1362043004
    
    Cr-Commit-Position: refs/heads/master@{#350924}

--- ash/system/cast/tray_cast.cc
+++ ash/system/cast/tray_cast.cc
@@ -639,7 +639,7 @@ void TrayCast::OnReceiversUpdated(
 }
 
 void TrayCast::UpdatePrimaryView() {
-  if (HasCastExtension()) {
+  if (HasCastExtension() && !receivers_and_activities_.empty()) {
     if (default_) {
       if (is_casting_)
         default_->ActivateCastView();
