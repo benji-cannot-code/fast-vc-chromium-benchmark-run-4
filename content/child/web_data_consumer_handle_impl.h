d59c95dc356df71f8ca10c0f043e0cc8f8230f8c
avi <avi@chromium.org>
2015-09-25 21:31:19 -0700

    Enforce marking "override" for functions overriding Blink in content/child/.
    BUG=535367
    TEST=it all stays working
    
    Review URL: https://codereview.chromium.org/1369043002
    
    Cr-Commit-Position: refs/heads/master@{#350982}

--- content/child/web_data_consumer_handle_impl.h
+++ content/child/web_data_consumer_handle_impl.h
@@ -23,12 +23,15 @@ class CONTENT_EXPORT WebDataConsumerHandleImpl final
   class CONTENT_EXPORT ReaderImpl final : public NON_EXPORTED_BASE(Reader) {
    public:
     ReaderImpl(scoped_refptr<Context> context, Client* client);
-    virtual ~ReaderImpl();
-    virtual Result read(void* data, size_t size, Flags flags, size_t* readSize);
-    virtual Result beginRead(const void** buffer,
-                             Flags flags,
-                             size_t* available);
-    virtual Result endRead(size_t readSize);
+    ~ReaderImpl() override;
+    Result read(void* data,
+                size_t size,
+                Flags flags,
+                size_t* readSize) override;
+    Result beginRead(const void** buffer,
+                     Flags flags,
+                     size_t* available) override;
+    Result endRead(size_t readSize) override;
 
    private:
     Result HandleReadResult(MojoResult);
@@ -42,10 +45,10 @@ class CONTENT_EXPORT WebDataConsumerHandleImpl final
   scoped_ptr<Reader> ObtainReader(Client* client);
 
   explicit WebDataConsumerHandleImpl(Handle handle);
-  virtual ~WebDataConsumerHandleImpl();
+  ~WebDataConsumerHandleImpl() override;
 
  private:
-  virtual ReaderImpl* obtainReaderInternal(Client* client);
+  ReaderImpl* obtainReaderInternal(Client* client) override;
   const char* debugName() const override;
 
   scoped_refptr<Context> context_;
