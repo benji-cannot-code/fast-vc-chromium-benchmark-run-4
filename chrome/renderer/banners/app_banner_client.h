c6e2d18b5cc2aaf28455b547c0db5d769a5a4de9
avi <avi@chromium.org>
2015-09-25 19:32:46 -0700

    Enforce marking "override" for functions overriding Blink in chrome/.
    BUG=535367
    TEST=it all stays working
    
    Review URL: https://codereview.chromium.org/1367283002
    
    Cr-Commit-Position: refs/heads/master@{#350973}

--- chrome/renderer/banners/app_banner_client.h
+++ chrome/renderer/banners/app_banner_client.h
@@ -22,7 +22,7 @@ class AppBannerClient : public content::RenderFrameObserver,
                         public blink::WebAppBannerClient {
  public:
   explicit AppBannerClient(content::RenderFrame* render_frame);
-  virtual ~AppBannerClient();
+  ~AppBannerClient() override;
 
  private:
   // content::RenderFrame::Observer implementation.
@@ -34,7 +34,7 @@ class AppBannerClient : public content::RenderFrameObserver,
   void registerBannerCallbacks(int request_id,
                                blink::WebAppBannerCallbacks*) override;
 
-  void showAppBanner(int request_id);
+  void showAppBanner(int request_id) override;
 
   void ResolveEvent(int request_id,
                     const std::string& platform,
