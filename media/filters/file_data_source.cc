f3a4136031efb29721d0d6d833d6adefd3e6886f
chcunningham <chcunningham@chromium.org>
2015-09-25 19:51:50 -0700

    Adding FFmpegDemuxer unittest to detect mp3 seek regressions.
    This seek behavior was recently regressed when FFmpeg changed
    how the usetoc flag was set/interpretted. A fix for this
    regression is already landed here:
    http://crrev.com/1352573003
    
    BUG=530043
    
    Review URL: https://codereview.chromium.org/1365173006
    
    Cr-Commit-Position: refs/heads/master@{#350975}

--- media/filters/file_data_source.cc
+++ media/filters/file_data_source.cc
@@ -13,12 +13,14 @@ namespace media {
 
 FileDataSource::FileDataSource()
     : force_read_errors_(false),
-      force_streaming_(false) {
+      force_streaming_(false),
+      bytes_read_(0) {
 }
 
 FileDataSource::FileDataSource(base::File file)
     : force_read_errors_(false),
-      force_streaming_(false) {
+      force_streaming_(false),
+      bytes_read_(0) {
   file_.Initialize(file.Pass());
 }
 
@@ -48,6 +50,7 @@ void FileDataSource::Read(int64 position, int size, uint8* data,
   int64 clamped_size = std::min(static_cast<int64>(size), file_size - position);
 
   memcpy(data, file_.data() + position, clamped_size);
+  bytes_read_ += clamped_size;
   read_cb.Run(clamped_size);
 }
 
