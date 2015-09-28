697a5eb95c0ac34d82a73be7df351676fa31d702
brettw <brettw@chromium.org>
2015-09-28 10:06:13 -0700

    GN: Schedule config loads for sub-configs.
    Previously the Builder did not schedule file loads for configs of configs.
    
    This meant that if a BUILD file contained only configs referenced by other
    configs (rather than configs referenced by targets, or targets referenced by
    other targets), it would never be loaded. The result will be a failed run with
    an undefined object.
    
    BUG=536844
    
    Review URL: https://codereview.chromium.org/1373903002
    
    Cr-Commit-Position: refs/heads/master@{#351089}

--- tools/gn/builder_unittest.cc
+++ tools/gn/builder_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "testing/gtest/include/gtest/gtest.h"
 #include "tools/gn/builder.h"
+#include "tools/gn/config.h"
 #include "tools/gn/loader.h"
 #include "tools/gn/target.h"
 #include "tools/gn/test_with_scope.h"
@@ -33,8 +34,17 @@ class MockLoader : public Loader {
     return files_.empty();
   }
 
-  // Returns true if two loads have been requested and they match the given
-  // file. This will clear the records so it will be empty for the next call.
+  // Returns true if one/two loads have been requested and they match the given
+  // file(s). This will clear the records so it will be empty for the next call.
+  bool HasLoadedOne(const SourceFile& file) {
+    if (files_.size() != 1u) {
+      files_.clear();
+      return false;
+    }
+    bool match = (files_[0] == file);
+    files_.clear();
+    return match;
+  }
   bool HasLoadedTwo(const SourceFile& a, const SourceFile& b) {
     if (files_.size() != 2u) {
       files_.clear();
@@ -216,4 +226,25 @@ TEST_F(BuilderTest, ShouldGenerate) {
 
   // It should have gotten pushed to B.
   EXPECT_TRUE(b_record->should_generate());
+}
+
+// Tests that configs applied to a config get loaded (bug 536844).
+TEST_F(BuilderTest, ConfigLoad) {
+  SourceDir toolchain_dir = settings_.toolchain_label().dir();
+  std::string toolchain_name = settings_.toolchain_label().name();
+
+  // Construct a dependency chain: A -> B -> C. Define A first with a
+  // forward-reference to B, then C, then B to test the different orders that
+  // the dependencies are hooked up.
+  Label a_label(SourceDir("//a/"), "a", toolchain_dir, toolchain_name);
+  Label b_label(SourceDir("//b/"), "b", toolchain_dir, toolchain_name);
+  Label c_label(SourceDir("//c/"), "c", toolchain_dir, toolchain_name);
+
+  // The builder will take ownership of the pointers.
+  Config* a = new Config(&settings_, a_label);
+  a->configs().push_back(LabelConfigPair(b_label));
+  builder_->ItemDefined(scoped_ptr<Item>(a));
+
+  // Should have requested that B is loaded.
+  EXPECT_TRUE(loader_->HasLoadedOne(SourceFile("//b/BUILD.gn")));
 }
