94e5a4eeedc34200da76b146c170cd90ed3efda9
rune <rune@opera.com>
2015-09-27 22:12:26 -0700

    No need to recalc style before StyleResolver::computeFont.
    StyleResolver::computeFont does not rely on the computed style of any
    elements in the document tree. The updateLayoutTree call was there to
    avoid an assert due to an unnecessary composed tree traversal for a
    parent node. That traversal was removed by [1], so the updateLayoutTree
    call should no longer be needed.
    
    This change gets rid of synchronous style recalcs when FontFaceSet.load()
    is called.
    
    [1] https://codereview.chromium.org/1367743002/
    
    BUG=536625
    
    Review URL: https://codereview.chromium.org/1372823002
    
    Cr-Commit-Position: refs/heads/master@{#351025}

--- third_party/WebKit/Source/core/css/FontFaceSet.cpp
+++ third_party/WebKit/Source/core/css/FontFaceSet.cpp
@@ -500,7 +500,6 @@ bool FontFaceSet::resolveFontStyle(const String& fontString, Font& font)
 
     style->font().update(style->font().fontSelector());
 
-    document()->updateLayoutTreeIfNeeded();
     document()->ensureStyleResolver().computeFont(style.get(), *parsedStyle);
 
     font = style->font();
