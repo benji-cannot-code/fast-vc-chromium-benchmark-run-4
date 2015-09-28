d99171ea52efb9842f60888932348f7056d6ccba
rogerta <rogerta@chromium.org>
2015-09-28 14:18:31 -0700

    Make sure links in the profile unlock reauth don't break out of popup.
    BUG=528847
    
    Review URL: https://codereview.chromium.org/1365813003
    
    Cr-Commit-Position: refs/heads/master@{#351159}

--- chrome/browser/resources/gaia_auth_host/authenticator.js
+++ chrome/browser/resources/gaia_auth_host/authenticator.js
@@ -209,6 +209,7 @@ cr.define('cr.login', function() {
     this.gapsCookie_ = data.gapsCookie;
     this.gapsCookieSent_ = false;
     this.newGapsCookie_ = null;
+    this.dontResizeNonEmbeddedPages = data.dontResizeNonEmbeddedPages;
 
     this.initialFrameUrl_ = this.constructInitialFrameUrl_(data);
     this.reloadUrl_ = data.frameUrl || this.initialFrameUrl_;
@@ -316,7 +317,12 @@ cr.define('cr.login', function() {
           }
         }
       }
-      if (!isEmbeddedPage) {
+
+      // In some cases, non-embedded pages should not be resized.  For
+      // example, on desktop when reauthenticating for purposes of unlocking
+      // a profile, resizing would cause a browser window to open in the
+      // system profile, which is not allowed.
+      if (!isEmbeddedPage && !this.dontResizeNonEmbeddedPages) {
         this.dispatchEvent(new CustomEvent('resize', {detail: currentUrl}));
         return;
       }
