42ce6c800fa676a56803b4b7611d2c62bd2a5868
l.gombos <l.gombos@samsung.com>
2015-09-28 09:13:00 -0700

    Remove unused macros from config.h.
    HAVE_SIGNAL_H, HAVE_TM_GMTOFF, HAVE_TM_ZONE and HAVE_TIMEGM macros are no longer used; so remove them.
    
    HAVE_SYS_TIME_H has been only used in DateMath.cpp, but in other parts of blink we use !OS(WIN) to guard including <sys/time.h>, so lets do the same in DateMath.cpp as well.
    
    Review URL: https://codereview.chromium.org/1369133002
    
    Cr-Commit-Position: refs/heads/master@{#351079}

--- third_party/WebKit/Source/wtf/DateMath.cpp
+++ third_party/WebKit/Source/wtf/DateMath.cpp
@@ -90,9 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if OS(WIN)
 #include <windows.h>
-#endif
-
-#if HAVE(SYS_TIME_H)
+#else
 #include <sys/time.h>
 #endif
 
