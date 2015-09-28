dbf353ab8a36d0483a73c29446772e908b88a653
pfeldman <pfeldman@chromium.org>
2015-09-28 16:44:45 -0700

    DevTools: merge excluded folder manager into isolated file system.
    BUG=529471
    
    Review URL: https://codereview.chromium.org/1369063002
    
    Cr-Commit-Position: refs/heads/master@{#351203}

--- third_party/WebKit/LayoutTests/http/tests/inspector/workspace-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/workspace-test.js
@@ -5,7 +5,6 @@ InspectorTest.createWorkspace = function(ignoreEvents)
 {
     WebInspector.settings.createSetting("fileSystemMapping", {}).set({});
     InspectorTest.testFileSystemMapping = new WebInspector.FileSystemMapping();
-    InspectorTest.testExcludedFolderManager = new WebInspector.ExcludedFolderManager();
     InspectorTest.testFileSystemMapping._fileSystemMappingSetting = new InspectorTest.MockSetting({});
     InspectorTest.testFileSystemMapping._excludedFoldersSetting = new InspectorTest.MockSetting({});
 
--- third_party/WebKit/Source/devtools/devtools.gypi
+++ third_party/WebKit/Source/devtools/devtools.gypi
@@ -168,7 +168,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'front_end/sdk/WorkerManager.js',
         ],
         'devtools_workspace_js_files': [
-            'front_end/workspace/ExcludedFolderManager.js',
             'front_end/workspace/FileManager.js',
             'front_end/workspace/FileSystemMapping.js',
             'front_end/workspace/IsolatedFileSystem.js',
