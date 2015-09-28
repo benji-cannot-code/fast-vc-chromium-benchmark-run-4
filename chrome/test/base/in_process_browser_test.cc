aae90d9969c6349ca0faa590556e28bf814746c9
oshima <oshima@chromium.org>
2015-09-28 09:13:54 -0700

    Set the browser window bounds to fit to the Xvfb's size.
    Chrome for ChromeOS currently places the window at 200x200 with 1366x768 size by default. This puts the parts of UI outside of Xvfb window, where the mouse cursor cannot be moved into.
    
    Default size is 1280x1024 and I'm using the 1280x800 that is one of resolution actually used on the device.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1360233005
    
    Cr-Commit-Position: refs/heads/master@{#351080}

--- chrome/test/base/in_process_browser_test.cc
+++ chrome/test/base/in_process_browser_test.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/test/base/in_process_browser_test.h"
 
+#include "ash/ash_switches.h"
 #include "base/auto_reset.h"
 #include "base/basictypes.h"
 #include "base/bind.h"
@@ -234,6 +235,12 @@ void InProcessBrowserTest::SetUp() {
   base::CreateDirectory(log_dir);
   // Disable IME extension loading to avoid many browser tests failures.
   chromeos::input_method::DisableExtensionLoading();
+  if (!command_line->HasSwitch(ash::switches::kAshHostWindowBounds)) {
+    // Adjusting window location & size so that the ash desktop window fits
+    // inside the Xvfb'x defualt resolution.
+    command_line->AppendSwitchASCII(ash::switches::kAshHostWindowBounds,
+                                    "0+0-1280x800");
+  }
 #endif  // defined(OS_CHROMEOS)
 
 #if defined(OS_MACOSX)
