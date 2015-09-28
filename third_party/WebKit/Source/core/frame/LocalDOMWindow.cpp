8b9386eb316b02320e03524829a0b7cb8b5a19a2
skyostil <skyostil@chromium.org>
2015-09-28 12:16:39 -0700

    scheduler: Disable per-frame timer scheduling
    Disable per-frame timer scheduling for now since it is causing crashes
    in situations where the underlying timer task runner becomes null.
    
    BUG=536656,510398
    
    Review URL: https://codereview.chromium.org/1370193002
    
    Cr-Commit-Position: refs/heads/master@{#351096}

--- third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
+++ third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
@@ -150,11 +150,6 @@ public:
         SuspendableTimer::trace(visitor);
     }
 
-    WebTaskRunner* timerTaskRunner() override
-    {
-        return m_window->frame()->frameScheduler()->timerTaskRunner();
-    }
-
 private:
     void fired() override
     {
