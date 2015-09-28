5ca3bc93eda6a043811612f943e10ed3b61000a7
thestig <thestig@chromium.org>
2015-09-28 16:54:46 -0700

    Pull sfntly from GitHub instead of code.google.com.
    - Pick up latest release
    - Adjust build files
    - Update README.chromium
    
    BUG=497302
    
    Review URL: https://codereview.chromium.org/1367323002
    
    Cr-Commit-Position: refs/heads/master@{#351207}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -91,7 +91,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     PDF documents.
  */
 #define SK_SFNTLY_SUBSETTER \
-    "third_party/sfntly/cpp/src/sample/chromium/font_subsetter.h"
+    "third_party/sfntly/src/cpp/src/sample/chromium/font_subsetter.h"
 
 /*  To write debug messages to a console, skia will call SkDebugf(...) following
     printf conventions (e.g. const char* format, ...). If you want to redirect
