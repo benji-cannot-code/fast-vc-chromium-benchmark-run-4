01962f0a6991376d058fffbecb45e22d3f13e2ae
kinuko <kinuko@chromium.org>
2015-09-28 18:35:51 -0700

    Use absolute header path in WebURLResponse.h
    And fix gn build deps for components/renderer_context_menu to get it compiling.
    
    Otherwise this leads to a compile error like http://goo.gl/BEAG40
    
    (Split from https://codereview.chromium.org/1373693002/)
    
    BUG=n/a
    
    Review URL: https://codereview.chromium.org/1371133002
    
    Cr-Commit-Position: refs/heads/master@{#351227}

--- third_party/WebKit/public/platform/WebURLResponse.h
+++ third_party/WebKit/public/platform/WebURLResponse.h
@@ -32,9 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebURLResponse_h
 #define WebURLResponse_h
 
-#include "WebCommon.h"
-#include "WebPrivateOwnPtr.h"
-#include "WebServiceWorkerResponseType.h"
+#include "public/platform/WebCommon.h"
+#include "public/platform/WebPrivateOwnPtr.h"
+#include "public/platform/WebServiceWorkerResponseType.h"
 
 namespace blink {
 
