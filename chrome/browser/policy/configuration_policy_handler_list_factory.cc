887e376b6bc5b40119db6216cefe4e4e16914bef
blundell <blundell@chromium.org>
2015-09-29 05:33:35 -0700

    Move pref and switch in preparation for metrics componentization
    Code will shortly be componentized into the metrics component that uses
    prefs::kMetricsReportingEnabled and switches::kForceFieldTrials. To enable that
    componentization, this CL does the following:
    
    - Moves prefs::kMetricsReportingEnabled into the metrics component.
    - Moves switches:kForceFieldTrials from //content to //base to enable it to be
      used both by //content and by the metrics component.
    
    BUG=508014
    
    Committed: https://crrev.com/71f7da5a323ce64e8bc5b3ff2a62fc60e3c71bd0
    Cr-Commit-Position: refs/heads/master@{#351268}
    
    Review URL: https://codereview.chromium.org/1373823002
    
    Cr-Commit-Position: refs/heads/master@{#351294}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -120,7 +120,7 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
     prefs::kPrintPreviewDisabled,
     base::Value::TYPE_BOOLEAN },
   { key::kMetricsReportingEnabled,
-    prefs::kMetricsReportingEnabled,
+    metrics::prefs::kMetricsReportingEnabled,
     base::Value::TYPE_BOOLEAN },
   { key::kApplicationLocaleValue,
     prefs::kApplicationLocale,
