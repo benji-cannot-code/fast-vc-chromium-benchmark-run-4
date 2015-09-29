f69fbcf5d10dcf4ca6f612e808a6cbb96953c2d7
tedchoc <tedchoc@chromium.org>
2015-09-29 13:44:35 -0700

    Fix captive portal compile issues for android.
    Caused by:
    https://codereview.chromium.org/1365733005
    
    TBR=felt@chromium.org
    NOTRY=true
    BUG=488673
    
    Review URL: https://codereview.chromium.org/1377943004
    
    Cr-Commit-Position: refs/heads/master@{#351391}

--- chrome/browser/interstitials/chrome_metrics_helper.cc
+++ chrome/browser/interstitials/chrome_metrics_helper.cc
@@ -8,13 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ssl/captive_portal_metrics_recorder.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/rappor/rappor_service.h"
 #include "content/public/browser/web_contents.h"
 
 #if defined(ENABLE_EXTENSIONS)
 #include "chrome/browser/extensions/api/experience_sampling_private/experience_sampling.h"
+#endif
+
+#if defined(ENABLE_CAPTIVE_PORTAL_DETECTION)
+#include "chrome/browser/ssl/captive_portal_metrics_recorder.h"
 #endif
 
 ChromeMetricsHelper::ChromeMetricsHelper(
@@ -38,15 +41,19 @@ ChromeMetricsHelper::ChromeMetricsHelper(
 ChromeMetricsHelper::~ChromeMetricsHelper() {}
 
 void ChromeMetricsHelper::StartRecordingCaptivePortalMetrics(bool overridable) {
+#if defined(ENABLE_CAPTIVE_PORTAL_DETECTION)
   captive_portal_recorder_.reset(
       new CaptivePortalMetricsRecorder(web_contents_, overridable));
+#endif
 }
 
 void ChromeMetricsHelper::RecordExtraShutdownMetrics() {
+#if defined(ENABLE_CAPTIVE_PORTAL_DETECTION)
   // The captive portal metrics should be recorded when the interstitial is
   // closing (or destructing).
   if (captive_portal_recorder_)
     captive_portal_recorder_->RecordCaptivePortalUMAStatistics();
+#endif
 }
 
 void ChromeMetricsHelper::RecordExtraUserDecisionMetrics(
