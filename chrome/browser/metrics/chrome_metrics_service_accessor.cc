ecf3cce4865c97ce0e1028f1186fffc42339e84d
blundell <blundell@chromium.org>
2015-09-29 07:09:50 -0700

    Componentize guts of ChromeMetricsServiceAccessor field trial functions
    This CL turns ChromeMetricsServiceAccessor's field trials-related functions
    into just thin wrappers around MetricsServiceAccessor functions in preparation
    for decoupling the iOS port from ChromeMetricsServiceAccessor.
    
    BUG=508014
    
    Review URL: https://codereview.chromium.org/1374663003
    
    Cr-Commit-Position: refs/heads/master@{#351308}

--- chrome/browser/metrics/chrome_metrics_service_accessor.cc
+++ chrome/browser/metrics/chrome_metrics_service_accessor.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "components/metrics/metrics_service.h"
-#include "components/variations/metrics_util.h"
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/settings/cros_settings.h"
@@ -53,16 +52,15 @@ bool ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled() {
 bool ChromeMetricsServiceAccessor::RegisterSyntheticFieldTrial(
     const std::string& trial_name,
     const std::string& group_name) {
-  return RegisterSyntheticFieldTrialWithNameHash(metrics::HashName(trial_name),
-                                                 group_name);
+  return metrics::MetricsServiceAccessor::RegisterSyntheticFieldTrial(
+      g_browser_process->metrics_service(), trial_name, group_name);
 }
 
 // static
 bool ChromeMetricsServiceAccessor::RegisterSyntheticFieldTrialWithNameHash(
     uint32_t trial_name_hash,
     const std::string& group_name) {
-  return metrics::MetricsServiceAccessor::RegisterSyntheticFieldTrial(
-      g_browser_process->metrics_service(),
-      trial_name_hash,
-      metrics::HashName(group_name));
+  return metrics::MetricsServiceAccessor::
+      RegisterSyntheticFieldTrialWithNameHash(
+          g_browser_process->metrics_service(), trial_name_hash, group_name);
 }
