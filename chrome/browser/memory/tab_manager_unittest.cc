b34d17d449a428002c1e56de8605e37fba27cce6
georgesak <georgesak@chromium.org>
2015-09-29 09:25:59 -0700

    Protect tabs with text entry from being discarded.
    BUG=514340
    
    Review URL: https://codereview.chromium.org/1375453003
    
    Cr-Commit-Position: refs/heads/master@{#351332}

--- chrome/browser/memory/tab_manager_unittest.cc
+++ chrome/browser/memory/tab_manager_unittest.cc
@@ -26,6 +26,7 @@ enum TestIndicies {
   kPinned,
   kApp,
   kPlayingAudio,
+  kFormEntry,
   kRecent,
   kOld,
   kReallyOld,
@@ -63,6 +64,14 @@ TEST_F(TabManagerTest, Comparator) {
     stats.last_active = now;
     stats.is_playing_audio = true;
     stats.child_process_host_id = kPlayingAudio;
+    test_list.push_back(stats);
+  }
+
+  {
+    TabStats stats;
+    stats.last_active = now;
+    stats.has_form_entry = true;
+    stats.child_process_host_id = kFormEntry;
     test_list.push_back(stats);
   }
 
@@ -117,6 +126,7 @@ TEST_F(TabManagerTest, Comparator) {
 
   int index = 0;
   EXPECT_EQ(kSelected, test_list[index++].child_process_host_id);
+  EXPECT_EQ(kFormEntry, test_list[index++].child_process_host_id);
   EXPECT_EQ(kPlayingAudio, test_list[index++].child_process_host_id);
   EXPECT_EQ(kPinned, test_list[index++].child_process_host_id);
   EXPECT_EQ(kOldButPinned, test_list[index++].child_process_host_id);
