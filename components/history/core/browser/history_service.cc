e350c1af49a1dba9db02df930b9a350af5e1dc59
msramek <msramek@chromium.org>
2015-09-29 06:05:00 -0700

    Enable history counting for time ranges.
    GetHistoryCount() previously counted all of browsing history. This can be now achieved by calling GetHistoryCount(base::Time(), base::Time::Max()). However, it's also possible to specify a narrower time interval.
    
    BUG=510028
    
    Review URL: https://codereview.chromium.org/1370493002
    
    Cr-Commit-Position: refs/heads/master@{#351298}

--- components/history/core/browser/history_service.cc
+++ components/history/core/browser/history_service.cc
@@ -671,6 +671,8 @@ base::CancelableTaskTracker::TaskId HistoryService::QueryURL(
 // Statistics ------------------------------------------------------------------
 
 base::CancelableTaskTracker::TaskId HistoryService::GetHistoryCount(
+    const Time& begin_time,
+    const Time& end_time,
     const GetHistoryCountCallback& callback,
     base::CancelableTaskTracker* tracker) {
   DCHECK(thread_) << "History service being called after cleanup";
@@ -678,7 +680,10 @@ base::CancelableTaskTracker::TaskId HistoryService::GetHistoryCount(
 
   return tracker->PostTaskAndReplyWithResult(
       thread_->task_runner().get(), FROM_HERE,
-      base::Bind(&HistoryBackend::GetHistoryCount, history_backend_.get()),
+      base::Bind(&HistoryBackend::GetHistoryCount,
+                 history_backend_.get(),
+                 begin_time,
+                 end_time),
       callback);
 }
 
