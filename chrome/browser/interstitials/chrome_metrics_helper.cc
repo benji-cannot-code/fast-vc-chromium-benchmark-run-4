f8e956c55dd8b8185367a4f60abb2c14806c7f11
felt <felt@chromium.org>
2015-09-29 11:31:05 -0700

    Split captive portal metrics out of SSLErrorClassification
    SSLErrorClassification can be shared cross-platform if the Chrome-specific
    captive portal logic is moved to its own helper. This also is a nice clean
    separation of purposes, since the captive portal logic was fairly disjoint
    from the other work being done in SSLErrorClassification. Now the captive
    portal logic is handled as a metrics helper.
    
    I also made the ownership of the metrics helper clearer by changing to use a
    scoped_ptr.
    
    BUG=488673
    R=estark@chromium.org
    TBR=mattm@chromium.org
    
    Committed: https://crrev.com/5a31c78de2d5a1829910ec09d86ec10ae16e5c4c
    Cr-Commit-Position: refs/heads/master@{#351195}
    
    Committed: https://crrev.com/c0a91fac22f1d3ff3f8d052eb5dbffa70ef9835f
    Cr-Commit-Position: refs/heads/master@{#351246}
    
    Review URL: https://codereview.chromium.org/1365733005
    
    Cr-Commit-Position: refs/heads/master@{#351358}

--- chrome/browser/interstitials/chrome_metrics_helper.cc
+++ chrome/browser/interstitials/chrome_metrics_helper.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ssl/captive_portal_metrics_recorder.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/rappor/rappor_service.h"
 #include "content/public/browser/web_contents.h"
@@ -35,6 +36,18 @@ ChromeMetricsHelper::ChromeMetricsHelper(
 }
 
 ChromeMetricsHelper::~ChromeMetricsHelper() {}
+
+void ChromeMetricsHelper::StartRecordingCaptivePortalMetrics(bool overridable) {
+  captive_portal_recorder_.reset(
+      new CaptivePortalMetricsRecorder(web_contents_, overridable));
+}
+
+void ChromeMetricsHelper::RecordExtraShutdownMetrics() {
+  // The captive portal metrics should be recorded when the interstitial is
+  // closing (or destructing).
+  if (captive_portal_recorder_)
+    captive_portal_recorder_->RecordCaptivePortalUMAStatistics();
+}
 
 void ChromeMetricsHelper::RecordExtraUserDecisionMetrics(
     security_interstitials::MetricsHelper::Decision decision) {
