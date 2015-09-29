0865d331c51e8bfe289c228c4e75753581d0d0cf
nednguyen <nednguyen@google.com>
2015-09-29 09:42:24 -0700

    [Telemetry] Remove local_server_controller from browser
    BUG=404771
    
    Review URL: https://codereview.chromium.org/1364853006
    
    Cr-Commit-Position: refs/heads/master@{#351337}

--- tools/telemetry/telemetry/page/shared_page_state.py
+++ tools/telemetry/telemetry/page/shared_page_state.py
@@ -265,7 +265,7 @@ class SharedPageState(story.SharedState):
   def _PreparePage(self):
     self._current_tab = self._test.TabForPage(self._current_page, self.browser)
     if self._current_page.is_file:
-      self.browser.SetHTTPServerDirectories(
+      self.platform.SetHTTPServerDirectories(
           self._current_page.page_set.serving_dirs |
           set([self._current_page.serving_dir]))
 
@@ -311,6 +311,7 @@ class SharedPageState(story.SharedState):
       self._migrated_profile = None
 
     self._StopBrowser()
+    self.platform.StopAllLocalServers()
 
   def _StopBrowser(self):
     if self._browser:
