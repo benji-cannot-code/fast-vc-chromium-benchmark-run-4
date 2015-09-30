c8bf5ed0939913d44a3f2ecd5c3bcc10569925f9
philipj <philipj@opera.com>
2015-09-30 06:12:18 -0700

    Remove SVGSVGElement.pixelUnitToMillimeterX and friends
    Intent to Deprecate and Remove:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/uJCmB7SzYNo/cBASMfJuBAAJ
    
    BUG=537177
    R=fs@opera.com, timvolodine@chromium.org
    
    Review URL: https://codereview.chromium.org/1374233003
    
    Cr-Commit-Position: refs/heads/master@{#351540}

--- third_party/WebKit/Source/core/svg/SVGSVGElement.h
+++ third_party/WebKit/Source/core/svg/SVGSVGElement.h
@@ -58,11 +58,6 @@ public:
     // 'SVGSVGElement' functions
     PassRefPtrWillBeRawPtr<SVGRectTearOff> viewport() const;
 
-    float pixelUnitToMillimeterX() const;
-    float pixelUnitToMillimeterY() const;
-    float screenPixelToMillimeterX() const;
-    float screenPixelToMillimeterY() const;
-
     bool useCurrentView() const { return m_useCurrentView; }
     SVGViewSpec* currentView();
 
