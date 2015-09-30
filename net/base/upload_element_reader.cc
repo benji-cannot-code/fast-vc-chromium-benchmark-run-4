288a82616cd2c57480d2b85550ebb44b7d1334da
kinuko <kinuko@chromium.org>
2015-09-29 20:56:42 -0700

    Revert of [Blob] BlobReader class & tests, and removal of all redundant reading. (patchset #15 id:280001 of https://codereview.chromium.org/1337153002/ )
    Reason for revert:
    BlobReaderTest.StateErrors failing on multiple bots.
    
    ../../content/browser/fileapi/blob_reader_unittest.cc:985: Failure
    Death test: reader_->Read(buffer.get(), 10, &bytes_read, base::Bind(&SetValue<int>, &async_bytes_read))
        Result: failed to die.
     Error msg:
    [  DEATH   ]
    
    Original issue's description:
    > [Blob] BlobReader class & tests, and removal of all redundant reading.
    >
    > * New BlobReader class & tests
    > * Removal of other reading code, which now uses the BlobReader
    > * Removal of unnecessary UploadDiskCacheEntryElementReader
    > * Removal of blob expansion logic in UploadDataStreamBuilder
    >
    > Now it's very easy for anyone in browserland to read blobs instead of
    > having to do url requests, and it's also easy for anyone to add new
    > blob backing storage.
    >
    > This is a prerequisite for the new blob async transportation, see here:
    > goto/BlobPaging
    >
    > BUG=138051,375297
    >
    > Committed: https://crrev.com/02561552a57ed8792a8ebb2676bad485e1d99605
    > Cr-Commit-Position: refs/heads/master@{#351470}
    
    TBR=michaeln@chromium.org,mmenke@chromium.org,dmurph@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=138051,375297
    
    Review URL: https://codereview.chromium.org/1376123002
    
    Cr-Commit-Position: refs/heads/master@{#351485}

--- net/base/upload_element_reader.cc
+++ net/base/upload_element_reader.cc
@@ -7,6 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
+const UploadDiskCacheEntryElementReader*
+UploadElementReader::AsDiskCacheEntryReaderForTests() const {
+  return nullptr;
+}
+
 const UploadBytesElementReader* UploadElementReader::AsBytesReader() const {
   return nullptr;
 }
