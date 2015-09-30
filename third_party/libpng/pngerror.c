f53e2822f3e9e45ab22d4d31e1403e24269097e9
mdempsky <mdempsky@chromium.org>
2015-09-29 17:11:26 -0700

    Update to libpng 1.2.52 (rollup change only)
    Verified that both of the fixes mentioned in README.chromium were
    fixed in upstream.  Upstream used a slightly different approach to
    detecting whether "prefix_size + expanded_size + 1" overflows in
    pngrutil.c, but their solution looks okay too.
    
    TBR=darin@chromium.org
    BUG=532758
    
    Review URL: https://codereview.chromium.org/1372313004
    
    Cr-Commit-Position: refs/heads/master@{#351445}

--- third_party/libpng/pngerror.c
+++ third_party/libpng/pngerror.c
@@ -2,8 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /* pngerror.c - stub functions for i/o and memory allocation
  *
- * Last changed in libpng 1.2.45 [July 7, 2011]
- * Copyright (c) 1998-2011 Glenn Randers-Pehrson
+ * Last changed in libpng 1.2.51 [February 6, 2014]
+ * Copyright (c) 1998-2014 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
  *
@@ -301,7 +301,7 @@ png_default_error(png_structp png_ptr, png_const_charp error_message)
    /* Here if not setjmp support or if png_ptr is null. */
    PNG_ABORT();
 #ifndef PNG_CONSOLE_IO_SUPPORTED
-   error_message = error_message; /* Make compiler happy */
+   PNG_UNUSED(error_message) /* Make compiler happy */
 #endif
 }
 
@@ -347,9 +347,9 @@ png_default_warning(png_structp png_ptr, png_const_charp warning_message)
      fprintf(stderr, PNG_STRING_NEWLINE);
    }
 #else
-   warning_message = warning_message; /* Make compiler happy */
+   PNG_UNUSED(warning_message) /* Make compiler happy */
 #endif
-   png_ptr = png_ptr; /* Make compiler happy */
+   PNG_UNUSED(png_ptr) /* Make compiler happy */
 }
 #endif /* PNG_WARNINGS_SUPPORTED */
 
