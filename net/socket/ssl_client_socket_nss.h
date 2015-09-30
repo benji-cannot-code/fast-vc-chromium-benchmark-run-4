fb1c9422db2e40ee8e0605cb640eda385b27be61
davidben <davidben@chromium.org>
2015-09-30 08:27:12 -0700

    Remove the NSS worker pool from ClientSocketFactory.
    This is no longer used. This doesn't attempt to unwind
    the threading logic from SSLClientSocketNSS as that will
    be a much more involved endeavor. (And possibly not worth
    it since the code is, ultimately, doomed.)
    
    BUG=519504
    
    Review URL: https://codereview.chromium.org/1379683003
    
    Cr-Commit-Position: refs/heads/master@{#351562}

--- net/socket/ssl_client_socket_nss.h
+++ net/socket/ssl_client_socket_nss.h
@@ -31,10 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/ssl/channel_id_service.h"
 #include "net/ssl/ssl_config_service.h"
 
-namespace base {
-class SequencedTaskRunner;
-}
-
 namespace net {
 
 class BoundNetLog;
@@ -55,14 +51,7 @@ class SSLClientSocketNSS : public SSLClientSocket {
   // authentication is requested, the host_and_port field of SSLCertRequestInfo
   // will be populated with |host_and_port|.  |ssl_config| specifies
   // the SSL settings.
-  //
-  // Because calls to NSS may block, such as due to needing to access slow
-  // hardware or needing to synchronously unlock protected tokens, calls to
-  // NSS may optionally be run on a dedicated thread. If synchronous/blocking
-  // behaviour is desired, for performance or compatibility, the current task
-  // runner should be supplied instead.
-  SSLClientSocketNSS(base::SequencedTaskRunner* nss_task_runner,
-                     scoped_ptr<ClientSocketHandle> transport_socket,
+  SSLClientSocketNSS(scoped_ptr<ClientSocketHandle> transport_socket,
                      const HostPortPair& host_and_port,
                      const SSLConfig& ssl_config,
                      const SSLClientSocketContext& context);
@@ -156,8 +145,6 @@ class SSLClientSocketNSS : public SSLClientSocket {
   // the |ssl_info|.signed_certificate_timestamps list.
   void AddSCTInfoToSSLInfo(SSLInfo* ssl_info) const;
 
-  // The task runner used to perform NSS operations.
-  scoped_refptr<base::SequencedTaskRunner> nss_task_runner_;
   scoped_ptr<ClientSocketHandle> transport_;
   HostPortPair host_and_port_;
   SSLConfig ssl_config_;
