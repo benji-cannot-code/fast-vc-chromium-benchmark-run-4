13f42fc507eb28b0e7b7bbc54e9b31309e3bc117
tanay.c <tanay.c@samsung.com>
2015-09-30 10:34:31 -0700

    Remove unused methods AXLayoutObject::ariaSelectedrows() and AXLayoutObject::ariaListboxSelectedChildren()
    BUG=439559
    
    Review URL: https://codereview.chromium.org/1373813002
    
    Cr-Commit-Position: refs/heads/master@{#351590}

--- third_party/WebKit/Source/modules/accessibility/AXLayoutObject.h
+++ third_party/WebKit/Source/modules/accessibility/AXLayoutObject.h
@@ -203,7 +203,6 @@ protected:
 
 private:
     AXObject* treeAncestorDisallowingChild() const;
-    void ariaListboxSelectedChildren(AccessibilityChildrenVector&);
     bool nodeIsTextControl(const Node*) const;
     bool isTabItemSelected() const;
     bool isValidSelectionBound(const AXObject*) const;
@@ -223,7 +222,6 @@ private:
     void addRemoteSVGChildren();
     void addInlineTextBoxChildren(bool force);
 
-    void ariaSelectedRows(AccessibilityChildrenVector&);
     bool elementAttributeValue(const QualifiedName&) const;
     LayoutRect computeElementRect() const;
     AXRange textControlSelection() const;
