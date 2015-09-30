d434b11f9fe292b309fff2e63527119060edac30
nick <nick@chromium.org>
2015-09-30 14:06:10 -0700

    Fix some stale documentation references to a class that doesn't exist anymore.
    BUG=None
    
    Review URL: https://codereview.chromium.org/1355353004
    
    Cr-Commit-Position: refs/heads/master@{#351632}

--- content/browser/renderer_host/media/audio_renderer_host.h
+++ content/browser/renderer_host/media/audio_renderer_host.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // AudioRendererHost serves audio related requests from AudioRenderer which
 // lives inside the render process and provide access to audio hardware.
 //
-// This class is owned by BrowserRenderProcessHost, and instantiated on UI
+// This class is owned by RenderProcessHostImpl, and instantiated on UI
 // thread, but all other operations and method calls happen on IO thread, so we
 // need to be extra careful about the lifetime of this object. AudioManager is a
 // singleton and created in IO thread, audio output streams are also created in
