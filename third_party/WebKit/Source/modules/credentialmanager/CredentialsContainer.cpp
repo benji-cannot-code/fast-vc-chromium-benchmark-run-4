48955d1ba39a0d48422a13b89dc11ca5b2e754e2
mkwst <mkwst@chromium.org>
2015-09-30 05:49:42 -0700

    CREDENTIAL: Add metrics to Credential Management APIs.
    BUG=529303
    R=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/1379573002
    
    Cr-Commit-Position: refs/heads/master@{#351536}

--- third_party/WebKit/Source/modules/credentialmanager/CredentialsContainer.cpp
+++ third_party/WebKit/Source/modules/credentialmanager/CredentialsContainer.cpp
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/DOMException.h"
 #include "core/dom/ExceptionCode.h"
 #include "core/dom/ExecutionContext.h"
+#include "core/frame/UseCounter.h"
 #include "modules/credentialmanager/Credential.h"
 #include "modules/credentialmanager/CredentialManagerClient.h"
 #include "modules/credentialmanager/CredentialRequestOptions.h"
@@ -132,6 +133,10 @@ ScriptPromise CredentialsContainer::get(ScriptState* scriptState, const Credenti
                 providers.append(url);
         }
     }
+
+    UseCounter::count(scriptState->executionContext(),
+                      options.suppressUI() ? UseCounter::CredentialManagerGetWithoutUI
+                                           : UseCounter::CredentialManagerGetWithUI);
 
     CredentialManagerClient::from(scriptState->executionContext())->dispatchGet(options.suppressUI(), providers, new RequestCallbacks(resolver));
     return promise;
