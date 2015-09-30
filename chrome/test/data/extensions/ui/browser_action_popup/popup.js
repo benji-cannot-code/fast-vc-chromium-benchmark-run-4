ea60aed526dd6ed9b8522ba3b0eb5f4ce1491f85
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-09-30 10:04:05 -0700

    [Extensions UI Views] Fix browser action dismissal on click
    Clicking on a browser action icon when the popup is open should result in
    closing the popup, but right now it closes it and then immediately re-opens it.
    This is because we close on mouse pressed, but activate on mouse released
    (because the browser actions are draggable). Imitate logic in MenuButton to
    account for this, and add a regression test.
    
    Also clean up the implementation of ToolbarActionView, so that this becomes
    easier to read and understand. Currently, the button's methods are split into
    if-statements depending on if a popup will open, since LabelButtons and
    MenuButtons function differently (opening on release vs press). However, this
    becomes moot when the buttons are draggable, so the end result is similar to
    MenuButton behavior, and it adds a ton of unneeded complexity. Finally remove
    it.
    
    BUG=458655
    
    Review URL: https://codereview.chromium.org/1363743002
    
    Cr-Commit-Position: refs/heads/master@{#351582}

--- chrome/test/data/extensions/ui/browser_action_popup/popup.js
+++ chrome/test/data/extensions/ui/browser_action_popup/popup.js
@@ -3,4 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.test.sendMessage('Popup opened');
+// Wait for the window to fully load, since that's when the popup is shown.
+window.onload = function() {
+  window.setTimeout(function() {
+    chrome.test.sendMessage('Popup opened');
+  }, 0);
+};
