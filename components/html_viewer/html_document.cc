6206af4a9ffc93d186826c3d247b81a309a216e5
yzshen <yzshen@chromium.org>
2015-09-29 18:00:27 -0700

    Mandoline: html_frame returns WebNavigationPolicyHandledByClient to blink when the navigation request needs to go to the frame server side.
    This prevents prematurely dispatching load event.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1375083002
    
    Cr-Commit-Position: refs/heads/master@{#351459}

--- components/html_viewer/html_document.cc
+++ components/html_viewer/html_document.cc
@@ -186,7 +186,7 @@ void HTMLDocument::Load() {
   web_request.setURL(url);
   web_request.setExtraData(extra_data.release());
 
-  frame_->web_frame()->toWebLocalFrame()->loadRequest(web_request);
+  frame_->LoadRequest(web_request);
 }
 
 HTMLDocument::BeforeLoadCache* HTMLDocument::GetBeforeLoadCache() {
--- components/html_viewer/html_frame.h
+++ components/html_viewer/html_frame.h
@@ -148,6 +148,8 @@ class HTMLFrame : public blink::WebFrameClient,
   // Returns true if this or one of the frames descendants is local.
   bool HasLocalDescendant() const;
 
+  void LoadRequest(const blink::WebURLRequest& request);
+
  protected:
   virtual ~HTMLFrame();
 
@@ -168,6 +170,7 @@ class HTMLFrame : public blink::WebFrameClient,
   virtual blink::WebCookieJar* cookieJar(blink::WebLocalFrame* frame);
   virtual blink::WebNavigationPolicy decidePolicyForNavigation(
       const NavigationPolicyInfo& info);
+  virtual bool hasPendingNavigation(blink::WebLocalFrame* frame);
   virtual void didHandleOnloadEvents(blink::WebLocalFrame* frame);
   virtual void didAddMessageToConsole(const blink::WebConsoleMessage& message,
                                       const blink::WebString& source_name,
@@ -332,6 +335,10 @@ class HTMLFrame : public blink::WebFrameClient,
       startup_performance_data_collector_;
 
   scoped_ptr<DevToolsAgentImpl> devtools_agent_;
+
+  // A navigation request has been sent to the frame server side, and we haven't
+  // received response to it.
+  bool pending_navigation_;
 
   base::WeakPtrFactory<HTMLFrame> weak_factory_;
 
