87a3efc4991eba24d4a2f35de5f3e5183b42b38d
pfeldman <pfeldman@chromium.org>
2015-09-30 11:27:00 -0700

    DevTools: expand the document element node unconditionally.
    BUG=530114
    TBR=dgozman
    
    Review URL: https://codereview.chromium.org/1381613002
    
    Cr-Commit-Position: refs/heads/master@{#351602}

--- third_party/WebKit/Source/devtools/front_end/ui/treeoutline.js
+++ third_party/WebKit/Source/devtools/front_end/ui/treeoutline.js
@@ -824,9 +824,10 @@ TreeElement.prototype = {
         this._listItemNode.classList.add("expanded");
         this._childrenListNode.classList.add("expanded");
 
-        this.onexpand();
-        if (this.treeOutline)
+        if (this.treeOutline) {
+            this.onexpand();
             this.treeOutline.dispatchEventToListeners(TreeOutline.Events.ElementExpanded, this);
+        }
     },
 
     /**
@@ -944,7 +945,7 @@ TreeElement.prototype = {
 
     _populateIfNeeded: function()
     {
-        if (this._expandable && !this._children) {
+        if (this.treeOutline && this._expandable && !this._children) {
             this._children = [];
             this.onpopulate();
         }
