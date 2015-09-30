cd45a25c5b3909e6098f577d45ab5537054424fb
samli <samli@chromium.org>
2015-09-30 14:39:16 -0700

    Devtools UI: Add a toggle button to each DOM element to launch menu
    This toggle button in the DOM view launches the same menu as the context
    menu shown on right click.
    
    BUG=528387
    
    Review URL: https://codereview.chromium.org/1378433002
    
    Cr-Commit-Position: refs/heads/master@{#351647}

--- third_party/WebKit/Source/devtools/front_end/elements/ElementsTreeElement.js
+++ third_party/WebKit/Source/devtools/front_end/elements/ElementsTreeElement.js
@@ -42,8 +42,8 @@ WebInspector.ElementsTreeElement = function(node, elementCloseTag)
     this._node = node;
 
     this._gutterContainer = this.listItemElement.createChild("div", "gutter-container");
+    this._gutterContainer.addEventListener("click", this._showContextMenu.bind(this));
     this._decorationsElement = this._gutterContainer.createChild("div", "hidden");
-    this._decorationsElement.addEventListener("mousedown", this._decorationsClicked.bind(this));
 
     this._elementCloseTag = elementCloseTag;
 
@@ -161,19 +161,6 @@ WebInspector.ElementsTreeElement.prototype = {
     isEditing: function()
     {
         return !!this._editing;
-    },
-
-    /**
-     * @return {!Element}
-     */
-    gutterElement: function()
-    {
-        return this._gutterContainer;
-    },
-
-    _decorationsClicked: function()
-    {
-        this.treeOutline.dispatchEventToListeners(WebInspector.ElementsTreeOutline.Events.DecorationsClicked, this._node);
     },
 
     /**
@@ -522,6 +509,14 @@ WebInspector.ElementsTreeElement.prototype = {
             return this._addNewAttribute();
 
         return false;
+    },
+
+    /**
+     * @param {!Event} event
+     */
+    _showContextMenu: function(event)
+    {
+        this.treeOutline.showContextMenu(this, event);
     },
 
     /**
