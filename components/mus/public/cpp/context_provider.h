ccada47a5ffa787848d39a33af50a40dc0590b96
fsamuel <fsamuel@chromium.org>
2015-09-30 08:05:18 -0700

    mus: Missed a couple of things in mojo/cc move
    BUG=none
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1382613002
    
    Cr-Commit-Position: refs/heads/master@{#351560}

--- components/mus/public/cpp/context_provider.h
+++ components/mus/public/cpp/context_provider.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MOJO_CC_CONTEXT_PROVIDER_MOJO_H_
-#define MOJO_CC_CONTEXT_PROVIDER_MOJO_H_
+#ifndef COMPONENTS_MUS_PUBLIC_CPP_CONTEXT_PROVIDER_H_
+#define COMPONENTS_MUS_PUBLIC_CPP_CONTEXT_PROVIDER_H_
 
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
@@ -59,4 +59,4 @@ class ContextProvider : public cc::ContextProvider {
 
 }  // namespace mus
 
-#endif  // MOJO_CC_CONTEXT_PROVIDER_MOJO_H_
+#endif  // COMPONENTS_MUS_PUBLIC_CPP_CONTEXT_PROVIDER_H_
--- components/mus/public/cpp/output_surface.h
+++ components/mus/public/cpp/output_surface.h
@@ -38,6 +38,7 @@ class OutputSurface : public cc::OutputSurface, public ViewSurfaceClient {
 
   DISALLOW_COPY_AND_ASSIGN(OutputSurface);
 };
-}
+
+}  // namespace mus
 
 #endif  // COMPONENTS_MUS_PUBLIC_CPP_OUTPUT_SURFACE_H_
