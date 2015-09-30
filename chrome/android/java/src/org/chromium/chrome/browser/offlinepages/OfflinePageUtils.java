88a8d16e1c4e94b5fcf4cf498a6b466414570bcd
fgorski <fgorski@chromium.org>
2015-09-30 15:01:32 -0700

    [Offline pages] Fixing NullPointerException when visiting offline page
    When visiting an offline page, when device is offline, dismiss (time-triggered)
    of the snackbar causes NullPointerException.
    
    BUG=491352
    
    Review URL: https://codereview.chromium.org/1367343003
    
    Cr-Commit-Position: refs/heads/master@{#351652}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/OfflinePageUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/OfflinePageUtils.java
@@ -147,6 +147,7 @@ public class OfflinePageUtils {
 
             @Override
             public void onDismissNoAction(Object actionData) {
+                if (actionData == null) return;
                 SnackbarButtonType buttonType = (SnackbarButtonType) actionData;
                 switch (buttonType) {
                     case NONE:
