77d10c60a2f272885a1f2b532283efa626f02c98
aurimas <aurimas@chromium.org>
2015-09-29 18:27:02 -0700

    Extract TabChromeWebContentsDelegateAndroid into a separate class.
    BUG=532216
    
    Review URL: https://codereview.chromium.org/1358923002
    
    Cr-Commit-Position: refs/heads/master@{#351464}

--- chrome/android/java/src/org/chromium/chrome/browser/document/DocumentTab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/document/DocumentTab.java
@@ -9,6 +9,7 @@ import android.graphics.Bitmap;
 
 import org.chromium.base.ObserverList.RewindableIterator;
 import org.chromium.base.VisibleForTesting;
+import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeApplication;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.TabState;
@@ -18,6 +19,7 @@ import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
 import org.chromium.chrome.browser.tab.ChromeTab;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tab.TabChromeWebContentsDelegateAndroid;
 import org.chromium.chrome.browser.tab.TabObserver;
 import org.chromium.chrome.browser.tab.TabUma;
 import org.chromium.chrome.browser.tab.TabUma.TabCreationState;
@@ -146,6 +148,10 @@ public class DocumentTab extends ChromeTab {
      */
     public class DocumentTabChromeWebContentsDelegateAndroid
             extends TabChromeWebContentsDelegateAndroid {
+        public DocumentTabChromeWebContentsDelegateAndroid(Tab tab, ChromeActivity activity) {
+            super(tab, activity);
+        }
+
         /**
          * TODO(dfalcantara): Remove this when DocumentActivity.getTabModelSelector()
          *                    can return a TabModelSelector that activateContents() can use.
@@ -158,7 +164,7 @@ public class DocumentTab extends ChromeTab {
 
     @Override
     protected TabChromeWebContentsDelegateAndroid createWebContentsDelegate() {
-        return new DocumentTabChromeWebContentsDelegateAndroid();
+        return new DocumentTabChromeWebContentsDelegateAndroid(this, mActivity);
     }
 
     /**
