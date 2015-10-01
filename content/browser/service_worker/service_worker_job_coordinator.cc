753aea4e2fbe0bb96245f785355afd9140c4eb3d
horo <horo@chromium.org>
2015-10-01 08:34:09 -0700

    Skip script comparison while "force" updating Service Worker on page load.
    As https://crbug.com/500428#c10 the fetched Service Worker script should be treated as byte different even if it isn't.
    
    BUG=500428
    
    Review URL: https://codereview.chromium.org/1358163007
    
    Cr-Commit-Position: refs/heads/master@{#351812}

--- content/browser/service_worker/service_worker_job_coordinator.cc
+++ content/browser/service_worker/service_worker_job_coordinator.cc
@@ -131,12 +131,14 @@ void ServiceWorkerJobCoordinator::Update(
   job_queues_[registration->pattern()].Push(
       make_scoped_ptr<ServiceWorkerRegisterJobBase>(
           new ServiceWorkerRegisterJob(context_, registration,
-                                       force_bypass_cache)));
+                                       force_bypass_cache,
+                                       false /* skip_script_comparison */)));
 }
 
 void ServiceWorkerJobCoordinator::Update(
     ServiceWorkerRegistration* registration,
     bool force_bypass_cache,
+    bool skip_script_comparison,
     ServiceWorkerProviderHost* provider_host,
     const ServiceWorkerRegisterJob::RegistrationCallback& callback) {
   DCHECK(registration);
@@ -145,7 +147,8 @@ void ServiceWorkerJobCoordinator::Update(
       job_queues_[registration->pattern()].Push(
           make_scoped_ptr<ServiceWorkerRegisterJobBase>(
               new ServiceWorkerRegisterJob(context_, registration,
-                                           force_bypass_cache))));
+                                           force_bypass_cache,
+                                           skip_script_comparison))));
   queued_job->AddCallback(callback, provider_host);
 }
 
