cd095a28edd2c6e42bc50cdedda3dc43beaff3fd
pfeldman <pfeldman@chromium.org>
2015-09-30 18:41:36 -0700

    DevTools: migrate excluded folder settings to the new (flat) format.
    BUG=529471
    
    Review URL: https://codereview.chromium.org/1373033003
    
    Cr-Commit-Position: refs/heads/master@{#351700}

--- third_party/WebKit/Source/devtools/front_end/common/Settings.js
+++ third_party/WebKit/Source/devtools/front_end/common/Settings.js
@@ -334,7 +334,7 @@ WebInspector.VersionController = function()
 }
 
 WebInspector.VersionController._currentVersionName = "inspectorVersion";
-WebInspector.VersionController.currentVersion = 14;
+WebInspector.VersionController.currentVersion = 15;
 
 WebInspector.VersionController.prototype = {
     updateVersion: function()
@@ -579,6 +579,19 @@ WebInspector.VersionController.prototype = {
     {
         var defaultValue = { "throughput": -1, "latency": 0 };
         WebInspector.settings.createSetting("networkConditions", defaultValue).set(defaultValue);
+    },
+
+    _updateVersionFrom14To15: function()
+    {
+        var setting = WebInspector.settings.createLocalSetting("workspaceExcludedFolders", {});
+        var oldValue = setting.get();
+        var newValue = {};
+        for (var fileSystemPath in oldValue) {
+            newValue[fileSystemPath] = [];
+            for (var entry of oldValue[fileSystemPath])
+                newValue[fileSystemPath].push(entry.path);
+        }
+        setting.set(newValue);
     },
 
     _migrateSettingsFromLocalStorage: function()
