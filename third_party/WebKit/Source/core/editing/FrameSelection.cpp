3019b80f0fdca6a643297d6b4e0309a7cb61d99c
wkorman <wkorman@chromium.org>
2015-09-30 20:23:54 -0700

    Fix minor typo in FrameSelection::isAppearanceDirty method name.
    Review URL: https://codereview.chromium.org/1377253002
    
    Cr-Commit-Position: refs/heads/master@{#351716}

--- third_party/WebKit/Source/core/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/core/editing/FrameSelection.cpp
@@ -960,7 +960,7 @@ inline static bool shouldStopBlinkingDueToTypingCommand(LocalFrame* frame)
     return frame->editor().lastEditCommand() && frame->editor().lastEditCommand()->shouldStopCaretBlinking();
 }
 
-bool FrameSelection::isApperanceDirty() const
+bool FrameSelection::isAppearanceDirty() const
 {
     return m_pendingSelection->hasPendingSelection();
 }
