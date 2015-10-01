1d0865d916de64b2f37cdf0bd97737d9409d2800
mithro <mithro@mithis.com>
2015-09-30 17:19:17 -0700

    Upgrading binutils to 2.25 for -ggnu-pubnames
    To make static_library builds fast we need binutils 2.25 to enable
    -ggnu-pubnames support.
    
    The ehframe-race.patch is removed because it is now included upstream. Dates
    and expected release versions have been added for the remaining patches to make
    it easier to upgrade in the future.
    
    (Also use a precise chroot rather than lucid for building binutils.)
    
    BUG=374952
    
    Committed: https://crrev.com/1a4e8c12aa264a54432b38fa009b0aa4814caea1
    Cr-Commit-Position: refs/heads/master@{#351019}
    
    Review URL: https://codereview.chromium.org/1368233002
    
    Cr-Commit-Position: refs/heads/master@{#351682}

--- third_party/binutils/build-all.sh
+++ third_party/binutils/build-all.sh
@@ -23,7 +23,7 @@ if [ ! -d "$OUTPUTDIR" ]; then
 fi
 
 # Download the source
-VERSION=2.24
+VERSION=2.25
 wget -c http://ftp.gnu.org/gnu/binutils/binutils-$VERSION.tar.bz2
 
 # Verify the signature
@@ -44,14 +44,21 @@ if [ ! -d binutils-$VERSION ]; then
   # Patch the source
   (
     cd binutils-$VERSION
-    patch -p1 < ../ehframe-race.patch
+    echo "unlock-thin.patch"
+    echo "=================================="
     patch -p1 < ../unlock-thin.patch
+    echo "----------------------------------"
+    echo
+    echo "plugin-dso-fix.patch"
+    echo "=================================="
     patch -p1 < ../plugin-dso-fix.patch
+    echo "----------------------------------"
+    echo
   )
 fi
 
 for ARCH in i386 amd64; do
-  if [ ! -d lucid-chroot-$ARCH ]; then
+  if [ ! -d precise-chroot-$ARCH ]; then
     # Refresh sudo credentials
     sudo -v
 
@@ -62,11 +69,11 @@ for ARCH in i386 amd64; do
     sudo debootstrap \
         --arch=$ARCH \
         --include=build-essential,flex,bison \
-        lucid lucid-chroot-$ARCH
+        precise precise-chroot-$ARCH
     echo "============================="
   fi
 
-  BUILDDIR=lucid-chroot-$ARCH/build
+  BUILDDIR=precise-chroot-$ARCH/build
 
   # Clean up any previous failed build attempts inside chroot
   if [ -d "$BUILDDIR" ]; then
@@ -93,7 +100,7 @@ for ARCH in i386 amd64; do
   echo ""
   echo "Building binutils for $ARCH"
   LOGFILE="$OUTPUTDIR/build-$ARCH.log"
-  if ! sudo $PREFIX chroot lucid-chroot-$ARCH /build/build-one.sh /build/binutils-$VERSION > $LOGFILE 2>&1; then
+  if ! sudo $PREFIX chroot precise-chroot-$ARCH /build/build-one.sh /build/binutils-$VERSION > $LOGFILE 2>&1; then
     echo "Build failed! See $LOGFILE for details."
     exit 1
   fi
