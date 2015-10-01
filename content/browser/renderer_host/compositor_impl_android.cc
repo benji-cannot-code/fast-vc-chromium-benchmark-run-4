7c7528d2553ff2775e54381666a726d63efc5492
brianderson <brianderson@chromium.org>
2015-10-01 10:40:13 -0700

    android: Change OutputSurfaceWithoutParent max swaps to 1.
    BUG=493854, 537335
    
    Review URL: https://codereview.chromium.org/1262163004
    
    Cr-Commit-Position: refs/heads/master@{#351836}

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -68,7 +68,8 @@ namespace content {
 
 namespace {
 
-const unsigned int kMaxSwapBuffers = 2U;
+const unsigned int kMaxUiSwapBuffers = 1U;
+const unsigned int kMaxDisplaySwapBuffers = 1U;
 
 // Used to override capabilities_.adjust_deadline_for_parent to false
 class OutputSurfaceWithoutParent : public cc::OutputSurface,
@@ -86,7 +87,7 @@ class OutputSurfaceWithoutParent : public cc::OutputSurface,
             base::Bind(&OutputSurfaceWithoutParent::OnSwapBuffersCompleted,
                        base::Unretained(this))) {
     capabilities_.adjust_deadline_for_parent = false;
-    capabilities_.max_frames_pending = 2;
+    capabilities_.max_frames_pending = kMaxDisplaySwapBuffers;
   }
 
   ~OutputSurfaceWithoutParent() override { compositor_->RemoveObserver(this); }
@@ -330,11 +331,11 @@ void CompositorImpl::Composite(CompositingTrigger trigger) {
   DCHECK(needs_composite_);
   DCHECK(!DidCompositeThisFrame());
 
-  DCHECK_LE(pending_swapbuffers_, kMaxSwapBuffers);
+  DCHECK_LE(pending_swapbuffers_, kMaxUiSwapBuffers);
   // Swap Ack accounting is unreliable if the OutputSurface was lost.
   // In that case still attempt to composite, which will cause creation of a
   // new OutputSurface and reset pending_swapbuffers_.
-  if (pending_swapbuffers_ == kMaxSwapBuffers &&
+  if (pending_swapbuffers_ == kMaxUiSwapBuffers &&
       !host_->output_surface_lost()) {
     TRACE_EVENT0("compositor", "CompositorImpl_SwapLimit");
     return;
@@ -731,7 +732,7 @@ void CompositorImpl::DidPostSwapBuffers() {
 void CompositorImpl::DidCompleteSwapBuffers() {
   TRACE_EVENT0("compositor", "CompositorImpl::DidCompleteSwapBuffers");
   DCHECK_GT(pending_swapbuffers_, 0U);
-  if (pending_swapbuffers_-- == kMaxSwapBuffers && needs_composite_)
+  if (pending_swapbuffers_-- == kMaxUiSwapBuffers && needs_composite_)
     PostComposite(COMPOSITE_IMMEDIATELY);
   client_->OnSwapBuffersCompleted(pending_swapbuffers_);
 }
