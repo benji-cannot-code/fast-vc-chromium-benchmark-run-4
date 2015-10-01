a37700f96436a9ae17698cf99e44405f2b69ac5c
bcwhite <bcwhite@chromium.org>
2015-10-01 04:24:37 -0700

    Reset OAuth backoff when network changes.
    BUG=526854
    
    Review URL: https://codereview.chromium.org/1364333004
    
    Cr-Commit-Position: refs/heads/master@{#351772}

--- chrome/browser/signin/mutable_profile_oauth2_token_service_delegate.h
+++ chrome/browser/signin/mutable_profile_oauth2_token_service_delegate.h
@@ -15,10 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/webdata/common/web_data_service_base.h"
 #include "components/webdata/common/web_data_service_consumer.h"
 #include "net/base/backoff_entry.h"
+#include "net/base/network_change_notifier.h"
 
 class MutableProfileOAuth2TokenServiceDelegate
     : public OAuth2TokenServiceDelegate,
-      public WebDataServiceConsumer {
+      public WebDataServiceConsumer,
+      public net::NetworkChangeNotifier::NetworkChangeObserver {
  public:
   MutableProfileOAuth2TokenServiceDelegate(
       SigninClient* client,
@@ -52,6 +54,10 @@ class MutableProfileOAuth2TokenServiceDelegate
 
   // Overridden from OAuth2TokenServiceDelegate.
   void Shutdown() override;
+
+  // Overridden from NetworkChangeObserver.
+  void OnNetworkChanged(net::NetworkChangeNotifier::ConnectionType type)
+      override;
 
  private:
   friend class MutableProfileOAuth2TokenServiceDelegateTest;
