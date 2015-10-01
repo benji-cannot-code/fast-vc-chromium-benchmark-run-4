3e7c0bcc5e44dcb2f21fa8eb1ac52c9cbb6c0be5
sigbjornf <sigbjornf@opera.com>
2015-10-01 14:18:32 -0700

    Oilpan: fix build after r351868.
    We do not have compiler support for traceable Oilpan heap references,
    hence undo the Member<> to ref conversion that r351868 made.
    
    TBR=oilpan-reviews
    BUG=
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1370143007
    
    Cr-Commit-Position: refs/heads/master@{#351887}

--- third_party/WebKit/Source/core/paint/FramePainter.h
+++ third_party/WebKit/Source/core/paint/FramePainter.h
@@ -20,7 +20,7 @@ class FramePainter {
     STACK_ALLOCATED();
     WTF_MAKE_NONCOPYABLE(FramePainter);
 public:
-    explicit FramePainter(const FrameView& frameView) : m_frameView(frameView) { }
+    explicit FramePainter(const FrameView& frameView) : m_frameView(&frameView) { }
 
     void paint(GraphicsContext*, const GlobalPaintFlags, const IntRect&);
     void paintScrollbars(GraphicsContext*, const IntRect&);
@@ -30,7 +30,9 @@ public:
 private:
     void paintScrollbar(GraphicsContext*, Scrollbar*, const IntRect&);
 
-    const FrameView& m_frameView;
+    const FrameView& frameView();
+
+    RawPtrWillBeMember<const FrameView> m_frameView;
     static bool s_inPaintContents;
 };
 
