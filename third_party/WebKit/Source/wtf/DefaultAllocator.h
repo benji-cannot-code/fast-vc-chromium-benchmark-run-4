fecbc88fccf716b1089e19d7ffa0bfe2d8697e59
sigbjornf <sigbjornf@opera.com>
2015-09-30 23:53:06 -0700

    Remove unused Allocator::{enter,leave}NoAllocationScope().
    R=haraken
    BUG=
    
    Review URL: https://codereview.chromium.org/1381493006
    
    Cr-Commit-Position: refs/heads/master@{#351740}

--- third_party/WebKit/Source/wtf/DefaultAllocator.h
+++ third_party/WebKit/Source/wtf/DefaultAllocator.h
@@ -177,8 +177,6 @@ public:
         return *other;
     }
 
-    static void enterNoAllocationScope() { }
-    static void leaveNoAllocationScope() { }
     static void enterGCForbiddenScope() { }
     static void leaveGCForbiddenScope() { }
 
