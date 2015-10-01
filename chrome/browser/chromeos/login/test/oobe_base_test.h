12d3ca7511dec73c62bc9f591b7f97e69a36be16
afakhry <afakhry@chromium.org>
2015-10-01 14:17:34 -0700

    Revert of ChromeOS: fix user signin by alias. (patchset #4 id:60001 of https://codereview.chromium.org/1370913002/ )
    Reason for revert:
    This Cl is causing the test login_OwnershipNotRetaken to fail on ChromePFQ. https://code.google.com/p/chromium/issues/detail?id=537986.
    
    Original issue's description:
    > THis CL fixes the issue with login using alternative e-mails.
    >
    > BUG=535369
    > TEST=manual
    >
    > Committed: https://crrev.com/0f37b696b7a814d4157401c304643594a4ae9731
    > Cr-Commit-Position: refs/heads/master@{#351482}
    
    TBR=achuith@chromium.org,alemate@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=535369
    
    Review URL: https://codereview.chromium.org/1384533003
    
    Cr-Commit-Position: refs/heads/master@{#351886}

--- chrome/browser/chromeos/login/test/oobe_base_test.h
+++ chrome/browser/chromeos/login/test/oobe_base_test.h
@@ -40,7 +40,6 @@ class OobeBaseTest : public ExtensionApiTest {
 
   static const char kFakeUserEmail[];
   static const char kFakeUserPassword[];
-  static const char kFakeUserGaiaId[];
 
   // FakeGaia is configured to return these cookies for kFakeUserEmail.
   static const char kFakeSIDCookie[];
