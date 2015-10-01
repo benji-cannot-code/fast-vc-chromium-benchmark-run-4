02580b9b4bd0b669b676e58b0c5edcce53ea8d2c
cco3 <cco3@chromium.org>
2015-09-30 19:14:27 -0700

    Add the Physical Web to Chrome on Android
    This change introduces a framework to receive URLs broadcasted by
    BLE and other technologies.  Those URLs are surfaced to the user
    who is then offered the chance to visit them via Chrome.
    
    BUG=529962
    
    Review URL: https://codereview.chromium.org/1326593006
    
    Cr-Commit-Position: refs/heads/master@{#351706}

--- chrome/android/java/src/org/chromium/chrome/browser/DeferredStartupHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/DeferredStartupHandler.java
@@ -21,6 +21,7 @@ import org.chromium.chrome.browser.crash.MinidumpUploadService;
 import org.chromium.chrome.browser.download.DownloadManagerService;
 import org.chromium.chrome.browser.media.MediaCaptureNotificationService;
 import org.chromium.chrome.browser.partnerbookmarks.PartnerBookmarksShim;
+import org.chromium.chrome.browser.physicalweb.PhysicalWeb;
 import org.chromium.chrome.browser.precache.PrecacheLauncher;
 import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
 import org.chromium.chrome.browser.preferences.privacy.PrivacyPreferencesManager;
@@ -125,6 +126,11 @@ public class DeferredStartupHandler {
         } else if (customTabsTrialGroupName.equals("Enabled")
                 || customTabsTrialGroupName.equals("DisablePrerender")) {
             ChromePreferenceManager.getInstance(application).setCustomTabsEnabled(true);
+        }
+
+        // Start Physical Web
+        if (PhysicalWeb.featureIsEnabled()) {
+            PhysicalWeb.startPhysicalWeb(application);
         }
 
         mDeferredStartupComplete = true;
