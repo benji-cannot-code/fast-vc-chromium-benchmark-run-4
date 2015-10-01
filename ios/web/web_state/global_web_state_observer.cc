6c783a4125aa544623086ce4de818a5ffc547729
blundell <blundell@chromium.org>
2015-10-01 02:17:57 -0700

    [ios] Add a mechanism to observe all WebState instances
    This CL adds a mechanism via which a client can observe *all* WebState
    instances. This mechanism is intended for usage only by code that has to
    mimic //chrome flows that listen for WebContents and/or NavigationController
    notifications from all sources.
    
    Specifically, this CL does the following:
    - Adds a GlobalWebStateObserver interface.
    - Adds a GlobalWebStateEventTracker singleton that is notified when WebState
      instances are created, observes all those WebStates via a helper
      WebStateObserver, and forwards the resulting callbacks to its registered
      GlobalWebStateObservers.
    
    Review URL: https://codereview.chromium.org/1374763007
    
    Cr-Commit-Position: refs/heads/master@{#351758}

---
+++ ios/web/web_state/global_web_state_observer.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/web/public/web_state/global_web_state_observer.h"
+
+#include "ios/web/web_state/global_web_state_event_tracker.h"
+
+namespace web {
+
+GlobalWebStateObserver::GlobalWebStateObserver() {
+  GlobalWebStateEventTracker::GetInstance()->AddObserver(this);
+}
+
+GlobalWebStateObserver::~GlobalWebStateObserver() {
+  GlobalWebStateEventTracker::GetInstance()->RemoveObserver(this);
+}
+
+}  // namespace web
