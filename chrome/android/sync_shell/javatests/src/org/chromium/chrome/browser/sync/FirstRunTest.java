881f0dac1ab12c9a048525866472a16a096ffc77
phoglund <phoglund@chromium.org>
2015-10-01 04:32:32 -0700

    Try to rename test since @DisableTest didn't do the job.
    BUG=517590
    TBR=nyquist@chromium.org
    
    Review URL: https://codereview.chromium.org/1382833002
    
    Cr-Commit-Position: refs/heads/master@{#351775}

--- chrome/android/sync_shell/javatests/src/org/chromium/chrome/browser/sync/FirstRunTest.java
+++ chrome/android/sync_shell/javatests/src/org/chromium/chrome/browser/sync/FirstRunTest.java
@@ -35,7 +35,7 @@ public class FirstRunTest extends SyncTestBase {
     @SmallTest
     @Feature({"Sync"})
     @DisabledTest
-    public void testFreSignin() throws Exception {
+    public void disabledTestFreSignin() throws Exception {
         Account testAccount = setUpTestAccount();
         SyncTestUtil.verifySyncIsSignedOut(mContext);
         assertFalse(AndroidSyncSettings.isChromeSyncEnabled(mContext));
