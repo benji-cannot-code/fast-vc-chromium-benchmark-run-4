312b3c72b162d4b30d3e2f7e1ef99ded88925c16
jbudorick <jbudorick@chromium.org>
2015-10-01 12:53:55 -0700

    [Android] Make @DisabledTest behave in a sane way.
    BUG=538111
    
    Review URL: https://codereview.chromium.org/1376233008
    
    Cr-Commit-Position: refs/heads/master@{#351867}

--- build/android/pylib/instrumentation/instrumentation_test_instance.py
+++ build/android/pylib/instrumentation/instrumentation_test_instance.py
@@ -29,6 +29,8 @@ _ACTIVITY_RESULT_OK = -1
 _DEFAULT_ANNOTATIONS = [
     'Smoke', 'SmallTest', 'MediumTest', 'LargeTest',
     'EnormousTest', 'IntegrationTest']
+_EXCLUDE_UNLESS_REQUESTED_ANNOTATIONS = [
+    'DisabledTest', 'FlakyTest']
 _EXTRA_ENABLE_HTTP_SERVER = (
     'org.chromium.chrome.test.ChromeInstrumentationTestRunner.'
         + 'EnableTestHttpServer')
@@ -268,6 +270,12 @@ class InstrumentationTestInstance(test_instance.TestInstance):
           for a in args.exclude_annotation_str.split(','))
     else:
       self._excluded_annotations = {}
+
+    self._excluded_annotations.update(
+        {
+          a: None for a in _EXCLUDE_UNLESS_REQUESTED_ANNOTATIONS
+          if a not in self._annotations
+        })
 
   def _initializeFlagAttributes(self, args):
     self._flags = ['--disable-fre', '--enable-test-intents']
