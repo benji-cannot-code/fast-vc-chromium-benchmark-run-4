fe4fe8e83e31fa1cf8159072808a0fe1cd7bc6f6
haraken <haraken@chromium.org>
2015-10-01 07:42:17 -0700

    Oilpan: An idle GC should be scheduled after a V8 major GC, not a V8 minor GC
    This is a bug introduced in https://codereview.chromium.org/1334683002/.
    The CL was intending to schedule an idle GC after a V8 major GC,
    but it actually scheduledd an idle GC after a V8 minor GC.
    
    This CL fixes it.
    
    BUG=474470
    
    Review URL: https://codereview.chromium.org/1382823002
    
    Cr-Commit-Position: refs/heads/master@{#351798}

--- third_party/WebKit/Source/platform/heap/ThreadState.cpp
+++ third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -684,7 +684,7 @@ void ThreadState::scheduleV8FollowupGCIfNeeded(V8GCType gcType)
         schedulePreciseGC();
         return;
     }
-    if (gcType == V8MinorGC) {
+    if (gcType == V8MajorGC) {
         scheduleIdleGC();
         return;
     }
