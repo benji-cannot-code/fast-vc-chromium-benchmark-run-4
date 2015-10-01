f3976924258cddf8021caf966c42ff04b8ec326b
ajm <ajm@chromium.org>
2015-10-01 09:38:08 -0700

    Restore the beamforming experiment control.
    It will now determine which virtual device users are defaulted to. In
    the experiment means the "beamforming on" device will appear first in
    the list and otherwise the "beamforming off" device appears first.
    
    BUG=497001,471938
    TEST=The beamforming off device appears first in the list for me on a
    swanky. I tried to force enable the experiment via --force-fieldtrials
    using the method described here:
    https://www.chromium.org/for-testers/command-line-flags
    but it didn't have any effect.
    
    Review URL: https://codereview.chromium.org/1368293011
    
    Cr-Commit-Position: refs/heads/master@{#351821}

--- media/audio/cras/audio_manager_cras.cc
+++ media/audio/cras/audio_manager_cras.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/environment.h"
 #include "base/logging.h"
+#include "base/metrics/field_trial.h"
 #include "base/nix/xdg_util.h"
 #include "base/stl_util.h"
 #include "chromeos/audio/audio_device.h"
@@ -43,6 +44,11 @@ const int kDefaultInputBufferSize = 1024;
 const char kBeamformingOnUniqueId[] = "beamforming-on";
 const char kBeamformingOffUniqueId[] = "beamforming-off";
 
+bool IsBeamformingDefaultedOn() {
+  return base::FieldTrialList::FindFullName("ChromebookBeamforming") ==
+         "Enabled";
+}
+
 void AddDefaultDevice(AudioDeviceNames* device_names) {
   DCHECK(device_names->empty());
 
@@ -54,13 +60,22 @@ void AddDefaultDevice(AudioDeviceNames* device_names) {
 // Adds the beamforming on and off devices to |device_names|.
 void AddBeamformingDevices(AudioDeviceNames* device_names) {
   DCHECK(device_names->empty());
-
-  device_names->push_back(AudioDeviceName(
+  const AudioDeviceName beamforming_on_device(
       GetLocalizedStringUTF8(BEAMFORMING_ON_DEFAULT_AUDIO_INPUT_DEVICE_NAME),
-      kBeamformingOnUniqueId));
-  device_names->push_back(AudioDeviceName(
+      kBeamformingOnUniqueId);
+  const AudioDeviceName beamforming_off_device(
       GetLocalizedStringUTF8(BEAMFORMING_OFF_DEFAULT_AUDIO_INPUT_DEVICE_NAME),
-      kBeamformingOffUniqueId));
+      kBeamformingOffUniqueId);
+
+  if (IsBeamformingDefaultedOn()) {
+    // Users in the experiment will have the "beamforming on" device appear
+    // first in the list. This causes it to be selected by default.
+    device_names->push_back(beamforming_on_device);
+    device_names->push_back(beamforming_off_device);
+  } else {
+    device_names->push_back(beamforming_off_device);
+    device_names->push_back(beamforming_on_device);
+  }
 }
 
 // Returns a mic positions string if the machine has a beamforming capable
