e49cfb2110d8c4bcd5268948ea4c8986ee14fba1
maniscalco <maniscalco@chromium.org>
2015-10-02 13:15:38 -0700

    Disable two more flaky WebContentsTests
    Disabling testDestroyingAWebContentsAfterSerializingDoesNotDeserialize
    and testSerializingADestroyedWebContentsDoesNotDeserialize
    
    BUG=538625
    
    TBR=dtrainor
    
    Review URL: https://codereview.chromium.org/1384083002
    
    Cr-Commit-Position: refs/heads/master@{#352110}

--- content/public/android/javatests/src/org/chromium/content/browser/webcontents/WebContentsTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/webcontents/WebContentsTest.java
@@ -200,7 +200,11 @@ public class WebContentsTest extends ContentShellTestBase {
      * @throws InterruptedException
      * @throws ExecutionException
      */
+    /*
     @SmallTest
+    https://crbug.com/538625
+    */
+    @DisabledTest
     public void testSerializingADestroyedWebContentsDoesNotDeserialize()
             throws InterruptedException, ExecutionException {
         ContentShellActivity activity = launchContentShellWithUrl(TEST_URL_1);
@@ -235,7 +239,11 @@ public class WebContentsTest extends ContentShellTestBase {
      * @throws InterruptedException
      * @throws ExecutionException
      */
+    /*
     @SmallTest
+    https://crbug.com/538625
+    */
+    @DisabledTest
     public void testDestroyingAWebContentsAfterSerializingDoesNotDeserialize()
             throws InterruptedException, ExecutionException {
         ContentShellActivity activity = launchContentShellWithUrl(TEST_URL_1);
