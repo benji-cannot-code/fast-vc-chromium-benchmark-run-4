009c5f5cc7a64b458f1b4d5b85ad1a7de0cb02a2
estade <estade@chromium.org>
2015-10-02 12:51:02 -0700

    Delete IDC_CONTENT_CONTEXT_ADDSEARCHENGINE
    This is no longer used anywhere. I'll follow up by removing the dialog
    code it invoked.
    
    BUG=506874,521128
    TBR=grt@chromium.org
    
    Review URL: https://codereview.chromium.org/1380833003
    
    Cr-Commit-Position: refs/heads/master@{#352103}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/chrome_pages.h"
-#include "chrome/browser/ui/search_engines/search_engine_tab_helper.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/chrome_constants.h"
@@ -226,7 +225,6 @@ const struct UmaEnumCommandIdPair {
     {45, -1, IDC_CONTENT_CONTEXT_GOTOURL},
     {46, -1, IDC_CONTENT_CONTEXT_LANGUAGE_SETTINGS},
     {47, -1, IDC_CONTENT_CONTEXT_PROTOCOL_HANDLER_SETTINGS},
-    {48, -1, IDC_CONTENT_CONTEXT_ADDSEARCHENGINE},
     {52, -1, IDC_CONTENT_CONTEXT_OPENLINKWITH},
     {53, -1, IDC_CHECK_SPELLING_WHILE_TYPING},
     {54, -1, IDC_SPELLCHECK_MENU},
@@ -1386,9 +1384,6 @@ bool RenderViewContextMenu::IsCommandIdEnabled(int id) const {
       return true;
 #endif
 
-    case IDC_CONTENT_CONTEXT_ADDSEARCHENGINE:
-      return !params_.keyword_url.is_empty();
-
     case IDC_SPELLCHECK_MENU:
       return true;
 
@@ -1860,34 +1855,6 @@ void RenderViewContextMenu::ExecuteCommand(int id, int event_flags) {
           ForceNewTabDispositionFromEventFlags(event_flags);
       GURL url = chrome::GetSettingsUrl(chrome::kHandlerSettingsSubPage);
       OpenURL(url, GURL(), disposition, ui::PAGE_TRANSITION_LINK);
-      break;
-    }
-
-    case IDC_CONTENT_CONTEXT_ADDSEARCHENGINE: {
-      // Make sure the model is loaded.
-      TemplateURLService* model =
-          TemplateURLServiceFactory::GetForProfile(GetProfile());
-      if (!model)
-        return;
-      model->Load();
-
-      SearchEngineTabHelper* search_engine_tab_helper =
-          SearchEngineTabHelper::FromWebContents(source_web_contents_);
-      if (search_engine_tab_helper &&
-          search_engine_tab_helper->delegate()) {
-        base::string16 keyword(TemplateURL::GenerateKeyword(
-            params_.page_url,
-            GetProfile()->GetPrefs()->GetString(prefs::kAcceptLanguages)));
-        TemplateURLData data;
-        data.SetShortName(keyword);
-        data.SetKeyword(keyword);
-        data.SetURL(params_.keyword_url.spec());
-        data.favicon_url =
-            TemplateURL::GenerateFaviconURL(params_.page_url.GetOrigin());
-        // Takes ownership of the TemplateURL.
-        search_engine_tab_helper->delegate()->ConfirmAddSearchProvider(
-            new TemplateURL(data), GetProfile());
-      }
       break;
     }
 
