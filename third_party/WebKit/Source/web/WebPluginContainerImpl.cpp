1c74099cc8d701a0e3f6f24798498162816dce1a
mkwst <mkwst@chromium.org>
2015-10-02 09:47:57 -0700

    Flash's 'getURL()' should run through a page's CSP before executing JavaScript
    BUG=538255
    
    Review URL: https://codereview.chromium.org/1371153004
    
    Cr-Commit-Position: refs/heads/master@{#352055}

--- third_party/WebKit/Source/web/WebPluginContainerImpl.cpp
+++ third_party/WebKit/Source/web/WebPluginContainerImpl.cpp
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/EventHandlerRegistry.h"
 #include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
+#include "core/frame/csp/ContentSecurityPolicy.h"
 #include "core/html/HTMLFormElement.h"
 #include "core/html/HTMLPlugInElement.h"
 #include "core/input/EventHandler.h"
@@ -465,6 +466,9 @@ WebString WebPluginContainerImpl::executeScriptURL(const WebURL& url, bool popup
 {
     LocalFrame* frame = m_element->document().frame();
     if (!frame)
+        return WebString();
+
+    if (!m_element->document().contentSecurityPolicy()->allowJavaScriptURLs(m_element->document().url(), OrdinalNumber()))
         return WebString();
 
     const KURL& kurl = url;
