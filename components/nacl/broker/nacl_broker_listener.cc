84853cd44db0cde356c5cbb36112102a13fa40c5
erikchen <erikchen@chromium.org>
2015-10-02 09:19:32 -0700

    Update components/nacl/ for attachment brokering.
    This CL creates an unprivileged attachment broker in NaclListener, and
    NaclBrokerListener that is responsible for brokering attachments between the
    nacl process and the browser process.
    
    BUG=493414
    
    Review URL: https://codereview.chromium.org/1258353002
    
    Cr-Commit-Position: refs/heads/master@{#352048}

--- components/nacl/broker/nacl_broker_listener.cc
+++ components/nacl/broker/nacl_broker_listener.cc
@@ -19,9 +19,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/nacl/common/nacl_switches.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/sandbox_init.h"
+#include "ipc/attachment_broker_unprivileged.h"
 #include "ipc/ipc_channel.h"
 #include "ipc/ipc_switches.h"
 #include "sandbox/win/src/sandbox_policy.h"
+
+#if defined(OS_WIN)
+#include "ipc/attachment_broker_unprivileged_win.h"
+#endif
 
 namespace {
 
@@ -32,6 +37,10 @@ void SendReply(IPC::Channel* channel, int32 pid, bool result) {
 }  // namespace
 
 NaClBrokerListener::NaClBrokerListener() {
+#if defined(OS_WIN)
+  attachment_broker_.reset(new IPC::AttachmentBrokerUnprivilegedWin);
+  IPC::AttachmentBroker::SetGlobal(attachment_broker_.get());
+#endif
 }
 
 NaClBrokerListener::~NaClBrokerListener() {
@@ -42,6 +51,8 @@ void NaClBrokerListener::Listen() {
       base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
           switches::kProcessChannelID);
   channel_ = IPC::Channel::CreateClient(channel_name, this);
+  if (attachment_broker_.get())
+    attachment_broker_->DesignateBrokerCommunicationChannel(channel_.get());
   CHECK(channel_->Connect());
   base::MessageLoop::current()->Run();
 }
--- components/nacl/loader/nacl_listener.cc
+++ components/nacl/loader/nacl_listener.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/nacl/loader/nacl_ipc_adapter.h"
 #include "components/nacl/loader/nacl_validation_db.h"
 #include "components/nacl/loader/nacl_validation_query.h"
+#include "ipc/attachment_broker_unprivileged.h"
 #include "ipc/ipc_channel_handle.h"
 #include "ipc/ipc_switches.h"
 #include "ipc/ipc_sync_channel.h"
@@ -45,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <io.h>
 
 #include "content/public/common/sandbox_init.h"
+#include "ipc/attachment_broker_unprivileged_win.h"
 #endif
 
 namespace {
@@ -195,17 +197,21 @@ class BrowserValidationDBProxy : public NaClValidationDB {
   NaClListener* listener_;
 };
 
-
-NaClListener::NaClListener() : shutdown_event_(true, false),
-                               io_thread_("NaCl_IOThread"),
+NaClListener::NaClListener()
+    : shutdown_event_(true, false),
+      io_thread_("NaCl_IOThread"),
 #if defined(OS_LINUX)
-                               prereserved_sandbox_size_(0),
+      prereserved_sandbox_size_(0),
 #endif
 #if defined(OS_POSIX)
-                               number_of_cores_(-1),  // unknown/error
+      number_of_cores_(-1),  // unknown/error
 #endif
-                               main_loop_(NULL),
-                               is_started_(false) {
+      main_loop_(NULL),
+      is_started_(false) {
+#if defined(OS_WIN)
+  attachment_broker_.reset(new IPC::AttachmentBrokerUnprivilegedWin);
+  IPC::AttachmentBroker::SetGlobal(attachment_broker_.get());
+#endif
   io_thread_.StartWithOptions(
       base::Thread::Options(base::MessageLoop::TYPE_IO, 0));
   DCHECK(g_listener == NULL);
@@ -266,6 +272,8 @@ void NaClListener::Listen() {
   filter_ = channel_->CreateSyncMessageFilter();
   channel_->AddFilter(new FileTokenMessageFilter());
   channel_->Init(channel_name, IPC::Channel::MODE_CLIENT, true);
+  if (attachment_broker_.get())
+    attachment_broker_->DesignateBrokerCommunicationChannel(channel_.get());
   main_loop_ = base::MessageLoop::current();
   main_loop_->Run();
 }
