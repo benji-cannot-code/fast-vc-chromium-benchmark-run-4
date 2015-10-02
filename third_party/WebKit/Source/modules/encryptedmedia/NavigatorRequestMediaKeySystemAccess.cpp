d31257c91ecf4c404f4d4e14e47a2c8e2163b409
Xiaohan Wang <xhwang@chromium.org>
2015-10-02 13:31:19 -0700

    Add warning message for empty robustness.
    Add a warning message when
    a. the key system is widevine, and
    b. any video capability passed into requestMediaKeySystemAccess() has empty robustness level.
    
    This warning message will be removed after issue 482277 is fixed. See the bug
    for more details.
    
    Also add UMA to report the empty robustness level.
    
    BUG=482277
    TEST=Tested with clearkey and widevine.
    R=ddorwin@chromium.org, isherman@chromium.org
    
    Review URL: https://codereview.chromium.org/1381463003 .
    
    Cr-Commit-Position: refs/heads/master@{#352118}

--- third_party/WebKit/Source/modules/encryptedmedia/NavigatorRequestMediaKeySystemAccess.cpp
+++ third_party/WebKit/Source/modules/encryptedmedia/NavigatorRequestMediaKeySystemAccess.cpp
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/ExceptionCode.h"
 #include "core/frame/OriginsUsingFeatures.h"
 #include "core/frame/UseCounter.h"
+#include "core/inspector/ConsoleMessage.h"
 #include "modules/encryptedmedia/EncryptedMediaUtils.h"
 #include "modules/encryptedmedia/MediaKeySession.h"
 #include "modules/encryptedmedia/MediaKeySystemAccess.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/EncryptedMediaRequest.h"
 #include "platform/Logging.h"
 #include "platform/network/ParsedContentType.h"
+#include "public/platform/Platform.h"
 #include "public/platform/WebEncryptedMediaClient.h"
 #include "public/platform/WebEncryptedMediaRequest.h"
 #include "public/platform/WebMediaKeySystemConfiguration.h"
@@ -104,6 +106,13 @@ public:
     }
 
 private:
+    // For widevine key system, generate warning and report to UMA if
+    // |m_supportedConfigurations| contains any video capability with empty
+    // robustness string.
+    // TODO(xhwang): Remove after we handle empty robustness correctly.
+    // See http://crbug.com/482277
+    void checkVideoCapabilityRobustness() const;
+
     Member<ScriptPromiseResolver> m_resolver;
     const String m_keySystem;
     WebVector<WebMediaKeySystemConfiguration> m_supportedConfigurations;
@@ -141,6 +150,8 @@ MediaKeySystemAccessInitializer::MediaKeySystemAccessInitializer(ScriptState* sc
         webConfig.label = config.label();
         m_supportedConfigurations[i] = webConfig;
     }
+
+    checkVideoCapabilityRobustness();
 }
 
 void MediaKeySystemAccessInitializer::requestSucceeded(WebContentDecryptionModuleAccess* access)
@@ -153,6 +164,42 @@ void MediaKeySystemAccessInitializer::requestNotSupported(const WebString& error
 {
     m_resolver->reject(DOMException::create(NotSupportedError, errorMessage));
     m_resolver.clear();
+}
+
+void MediaKeySystemAccessInitializer::checkVideoCapabilityRobustness() const
+{
+    // Only check for widevine key system.
+    if (keySystem() != "com.widevine.alpha")
+        return;
+
+    bool hasVideoCapabilities = false;
+    bool hasEmptyRobustness = false;
+
+    for (const auto& config : m_supportedConfigurations) {
+        if (!config.hasVideoCapabilities)
+            continue;
+
+        hasVideoCapabilities = true;
+
+        for (const auto& capability : config.videoCapabilities) {
+            if (capability.robustness.isEmpty()) {
+                hasEmptyRobustness = true;
+                break;
+            }
+        }
+
+        if (hasEmptyRobustness)
+            break;
+    }
+
+    if (hasVideoCapabilities)
+        Platform::current()->histogramEnumeration("Media.EME.Widevine.VideoCapability.HasEmptyRobustness", hasEmptyRobustness, 2);
+
+    if (hasEmptyRobustness) {
+        m_resolver->executionContext()->addConsoleMessage(ConsoleMessage::create(JSMessageSource, WarningMessageLevel,
+            "It is recommended that a robustness level be specified. Not specifying the robustness level could "
+            "result in unexpected behavior in the future, potentially including failure to play."));
+    }
 }
 
 } // namespace
@@ -192,7 +239,6 @@ ScriptPromise NavigatorRequestMediaKeySystemAccess::requestMediaKeySystemAccess(
         // TODO(ddorwin): Implement the following:
         // Reject promise with a new DOMException whose name is NotSupportedError.
     }
-
 
     // 5. Let origin be the origin of document.
     //    (Passed with the execution context in step 7.)
