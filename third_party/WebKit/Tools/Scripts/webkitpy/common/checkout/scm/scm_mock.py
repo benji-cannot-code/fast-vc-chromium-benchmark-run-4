8538866be54ad282cbf3965c6f629842d8af19e2
joelo <joelo@chromium.org>
2015-10-02 13:30:51 -0700

    Fix commit_position to be an int instead of a string.
    This caused rebaseline-o-matic to fail when comparing revisions.
    
    BUG=535351
    TBR=dpranke@chromium.org,ojan@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1380293003
    
    Cr-Commit-Position: refs/heads/master@{#352117}

--- third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/scm/scm_mock.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/scm/scm_mock.py
@@ -78,7 +78,7 @@ class MockSCM(object):
         return self._filesystem.join(self.checkout_root, *comps)
 
     def commit_position(self, path):
-        return '5678'
+        return 5678
 
     def commit_position_from_git_commit(self, git_commit):
         if git_commit == '6469e754a1':
