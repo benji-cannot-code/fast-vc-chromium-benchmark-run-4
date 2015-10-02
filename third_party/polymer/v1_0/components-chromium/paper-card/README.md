e51b68ac6e36efd2ff6d09d688619be37dd27550
fhorschig <fhorschig@chromium.org>
2015-10-02 01:50:46 -0700

    Add paper-cards from polymer-project.
    The paper-cards used in https://codereview.chromium.org/1371073003/
    are added now to keep the reviews handy.
    
    BUG=134849
    
    Review URL: https://codereview.chromium.org/1377083003
    
    Cr-Commit-Position: refs/heads/master@{#351978}

---
+++ third_party/polymer/v1_0/components-chromium/paper-card/README.md
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# paper-card
+
+Material Design: <a href="http://www.google.com/design/spec/components/cards.html">Cards</a>
+
+`paper-card` is a container with a drop shadow.
+
+Example:
+```html
+<paper-card heading="Card Title">
+  <div class="card-content">Some content</div>
+  <div class="card-actions">
+    <paper-button>Some action</paper-button>
+  </div>
+</paper-card>
+```
+
+### Accessibility
+By default, the `aria-label` will be set to the value of the `heading` attribute.
+
+### Styling
+
+The following custom properties and mixins are available for styling:
+
+Custom property | Description | Default
+----------------|-------------|----------
+`--paper-card-header-color` | The color of the header text | `#000`
+`--paper-card-header` | Mixin applied to the card header section | `{}`
+`--paper-card-content` | Mixin applied to the card content section| `{}`
+`--paper-card-actions` | Mixin applied to the card action section | `{}`
+`--paper-card` | Mixin applied to the card | `{}`
---
+++ third_party/polymer/v1_0/components-chromium/paper-card/paper-card-extracted.js
@@ -0,0 +1,54 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+Polymer({
+
+    is: 'paper-card',
+
+    properties: {
+
+      /**
+       * The title of the card.
+       */
+      heading: {
+        type: String,
+        value: '',
+        observer: '_headingChanged'
+      },
+
+      /**
+       * The url of the title image of the card.
+       */
+      image: {
+        type: String,
+        value: ''
+      },
+
+      /**
+       * The z-depth of the card, from 0-5.
+       */
+      elevation: {
+        type: Number,
+        value: 1
+      },
+
+      /**
+       * Set this to true to animate the card shadow when setting a new
+       * `z` value.
+       */
+      animatedShadow: {
+        type: Boolean,
+        value: false
+      }
+    },
+
+    _headingChanged: function(heading) {
+      var label = this.getAttribute('aria-label');
+      this.setAttribute('aria-label', heading);
+    },
+
+    _computeHeadingClass: function(image) {
+      var cls = 'title-text';
+      if (image)
+        cls += ' over-image';
+      return cls;
+    }
+  });
