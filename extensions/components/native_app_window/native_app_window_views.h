430f87255baf52ad309b29fe94dc13a4efd1d65d
wittman <wittman@chromium.org>
2015-10-02 14:33:57 -0700

    NativeWebContentsModalDialogManagerViews: avoid showing dialogs during host shutdown
    Fixes a bug where showing a dialog during NativeWidgetAura shutdown
    resulted in accessing a null activation client.
    
    BUG=484923
    
    Review URL: https://codereview.chromium.org/1382043002
    
    Cr-Commit-Position: refs/heads/master@{#352137}

--- extensions/components/native_app_window/native_app_window_views.h
+++ extensions/components/native_app_window/native_app_window_views.h
@@ -116,6 +116,7 @@ class NativeAppWindowViews : public extensions::NativeAppWindow,
       const gfx::Point& location) override;
 
   // WidgetObserver implementation.
+  void OnWidgetDestroying(views::Widget* widget) override;
   void OnWidgetVisibilityChanged(views::Widget* widget, bool visible) override;
   void OnWidgetActivationChanged(views::Widget* widget, bool active) override;
 
