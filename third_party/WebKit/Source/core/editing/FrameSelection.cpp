c57b60e21332685c747d1ca3e660022bf9163508
peria <peria@chromium.org>
2015-10-02 00:07:21 -0700

    Fix build error on Oilpan bot after r351960
    [949/4706] CXX obj/third_party/WebKit/Source/core/inspector/webcore_shared.InspectorTaskRunner.o
    ../../third_party/WebKit/Source/core/editing/FrameSelection.cpp:1311:1: error: [blink-gc] Class 'FrameSelection' has untraced fields that require tracing.
    DEFINE_TRACE(FrameSelection)
    ^
    ../../third_party/WebKit/Source/platform/heap/Visitor.h:83:5: note: expanded from macro 'DEFINE_TRACE'
        ALWAYS_INLINE void T::traceImpl(VisitorDispatcher visitor)
        ^
    ../../third_party/WebKit/Source/wtf/Compiler.h:71:23: note: expanded from macro 'ALWAYS_INLINE'
    #define ALWAYS_INLINE inline
                          ^
    ../../third_party/WebKit/Source/core/editing/FrameSelection.h:288:5: note: [blink-gc] Untraced field 'm_originalBaseInComposedTree' declared here:
        VisiblePositionInComposedTree m_originalBaseInComposedTree;
        ^
    1 error generated.
    ninja: build stopped: subcommand failed.
    
    BUG=None
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1374193004
    
    Cr-Commit-Position: refs/heads/master@{#351971}

--- third_party/WebKit/Source/core/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/core/editing/FrameSelection.cpp
@@ -1315,6 +1315,7 @@ DEFINE_TRACE(FrameSelection)
     visitor->trace(m_pendingSelection);
     visitor->trace(m_selectionEditor);
     visitor->trace(m_originalBase);
+    visitor->trace(m_originalBaseInComposedTree);
     visitor->trace(m_previousCaretNode);
     visitor->trace(m_typingStyle);
 }
