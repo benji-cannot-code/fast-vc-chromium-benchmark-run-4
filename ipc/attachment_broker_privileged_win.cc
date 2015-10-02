6421a5027b144f614f9fcbf29d4daa1de9500f6e
Hans Wennborg <hans@chromium.org>
2015-10-01 17:39:38 -0700

    Fix another 'enumerator not handled in switch' Win-Clang warning
    BUG=82385
    TBR=erikchen
    
    Review URL: https://codereview.chromium.org/1386553002 .
    
    Cr-Commit-Position: refs/heads/master@{#351922}

--- ipc/attachment_broker_privileged_win.cc
+++ ipc/attachment_broker_privileged_win.cc
@@ -35,6 +35,7 @@ bool AttachmentBrokerPrivilegedWin::SendAttachmentToProcess(
       RouteDuplicatedHandle(new_wire_format);
       return true;
     }
+    case BrokerableAttachment::MACH_PORT:
     case BrokerableAttachment::PLACEHOLDER:
       NOTREACHED();
       return false;
