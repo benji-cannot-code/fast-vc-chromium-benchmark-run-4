8a765a8d92e0f164df6c7f871fdd412ccc4b16fe
mkwst <mkwst@chromium.org>
2015-10-02 06:20:25 -0700

    CREDENTIAL: Take affiliations into account when clearing the zero-click bit.
    Currently, we're only looking at the origin when processing a call to
    'requireUserMediation()'. This leaves affiliated origins out, which isn't
    at all what we ought to be doing.
    
    BUG=537144
    R=engedy@chromium.org
    NOPRESUBMIT=true
    
    Review URL: https://codereview.chromium.org/1378583002
    
    Cr-Commit-Position: refs/heads/master@{#352017}

--- components/password_manager/core/browser/credential_manager_pending_require_user_mediation_task.cc
+++ components/password_manager/core/browser/credential_manager_pending_require_user_mediation_task.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/credential_manager_pending_require_user_mediation_task.h"
 
 #include "components/autofill/core/common/password_form.h"
+#include "components/password_manager/core/browser/affiliated_match_helper.h"
 #include "components/password_manager/core/browser/password_store.h"
 #include "url/gurl.h"
 
@@ -14,8 +15,10 @@ namespace password_manager {
 CredentialManagerPendingRequireUserMediationTask::
     CredentialManagerPendingRequireUserMediationTask(
         CredentialManagerPendingRequireUserMediationTaskDelegate* delegate,
-        const GURL& origin)
-    : delegate_(delegate) {
+        const GURL& origin,
+        const std::vector<std::string>& affiliated_realms)
+    : delegate_(delegate),
+      affiliated_realms_(affiliated_realms.begin(), affiliated_realms.end()) {
   origins_.insert(origin.spec());
 }
 
@@ -31,7 +34,9 @@ void CredentialManagerPendingRequireUserMediationTask::
     OnGetPasswordStoreResults(ScopedVector<autofill::PasswordForm> results) {
   PasswordStore* store = delegate_->GetPasswordStore();
   for (autofill::PasswordForm* form : results) {
-    if (origins_.count(form->origin.spec())) {
+    if (origins_.count(form->origin.spec()) ||
+        (affiliated_realms_.count(form->signon_realm) &&
+         AffiliatedMatchHelper::IsValidAndroidCredential(*form))) {
       form->skip_zero_click = true;
       // Note that UpdateLogin ends up copying the form while posting a task to
       // update the PasswordStore, so it's fine to let |results| delete the
