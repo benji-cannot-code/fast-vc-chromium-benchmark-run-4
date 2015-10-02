0627526eaa0232e3fee4a591d11ecd4d3d12121b
melandory <melandory@chromium.org>
2015-10-02 02:22:24 -0700

    [Smart Lock, UI] Do not use "remember" strings description for Password Manager setting.
    As UI review concluded remember strings are confusing and we want tostay
    with old strings for M47.
    
    BUG=517087
    
    Review URL: https://codereview.chromium.org/1382653004
    
    Cr-Commit-Position: refs/heads/master@{#351987}

--- chrome/browser/ui/passwords/manage_passwords_view_utils_desktop.cc
+++ chrome/browser/ui/passwords/manage_passwords_view_utils_desktop.cc
@@ -12,17 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 int GetPasswordManagerSettingsStringId(
     const sync_driver::SyncService* sync_service) {
-  int smart_lock_users_ids;
-  int non_smart_lock_users_ids;
+  int smart_lock_users_ids = IDS_OPTIONS_PASSWORD_MANAGER_SMART_LOCK_ENABLE;
+  int non_smart_lock_users_ids = IDS_OPTIONS_PASSWORD_MANAGER_ENABLE;
 
-  if (password_manager::IsSettingsBehaviorChangeActive()) {
-    smart_lock_users_ids =
-        IDS_OPTIONS_PASSWORD_MANAGER_SMART_LOCK_REMEMBER_ENABLE;
-    non_smart_lock_users_ids = IDS_OPTIONS_PASSWORD_MANAGER_REMEMBER_ENABLE;
-  } else {
-    smart_lock_users_ids = IDS_OPTIONS_PASSWORD_MANAGER_SMART_LOCK_ENABLE;
-    non_smart_lock_users_ids = IDS_OPTIONS_PASSWORD_MANAGER_ENABLE;
-  }
   if (password_bubble_experiment::IsSmartLockBrandingEnabled(sync_service))
     return smart_lock_users_ids;
   if (password_bubble_experiment::IsSmartLockUser(sync_service) &&
