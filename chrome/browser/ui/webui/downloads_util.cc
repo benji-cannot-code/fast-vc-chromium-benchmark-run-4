e53f340d546e1c440ccfc3e9f3dbbe966104ff50
dbeam <dbeam@chromium.org>
2015-10-02 18:13:49 -0700

    MD Downloads: enable based on Finch trial
    R=groby@chromium.org
    BUG=526577
    
    Review URL: https://codereview.chromium.org/1377513003
    
    Cr-Commit-Position: refs/heads/master@{#352219}

--- chrome/browser/ui/webui/downloads_util.cc
+++ chrome/browser/ui/webui/downloads_util.cc
@@ -6,25 +6,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/downloads_util.h"
 
 #include "base/command_line.h"
+#include "base/metrics/field_trial.h"
+#include "base/strings/string_util.h"
 #include "chrome/common/channel_info.h"
 #include "chrome/common/chrome_switches.h"
 #include "components/version_info/version_info.h"
 
-bool MdDownloadsEnabled() {
-  base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
+const char kMaterialDesignDownloadsFinchTrialName[] = "MaterialDesignDownloads";
 
+bool MdDownloadsEnabled() {
+  // Intentionally call this before checking command line to assign a group.
+  std::string group = base::FieldTrialList::FindFullName(
+      kMaterialDesignDownloadsFinchTrialName);
+
+  base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
   if (cl->HasSwitch(switches::kEnableMaterialDesignDownloads))
     return true;
 
   if (cl->HasSwitch(switches::kDisableMaterialDesignDownloads))
     return false;
 
-  switch (chrome::GetChannel()) {
-    case version_info::Channel::DEV:
-    case version_info::Channel::CANARY:
-    case version_info::Channel::UNKNOWN:
-      return true;
-    default:
-      return false;
-  }
+  return base::StartsWith(group, "Enabled",
+                          base::CompareCase::INSENSITIVE_ASCII);
 }
--- chrome/chrome_tests_unit.gypi
+++ chrome/chrome_tests_unit.gypi
@@ -1579,6 +1579,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/ui/toolbar/wrench_menu_model_unittest.cc',
       'browser/ui/website_settings/permission_bubble_manager_unittest.cc',
       'browser/ui/website_settings/permission_menu_model_unittest.cc',
+      'browser/ui/webui/downloads_util_unittest.cc',
       'browser/ui/webui/help/version_updater_chromeos_unittest.cc',
       'browser/ui/webui/ntp/ntp_user_data_logger_unittest.cc',
       'browser/ui/webui/options/autofill_options_handler_unittest.cc',
