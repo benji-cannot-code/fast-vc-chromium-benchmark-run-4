ed5cd9b1882e3c9290ea378c4248625a9565c7ba
jungkee.song <jungkee.song@samsung.com>
2015-10-02 20:36:30 -0700

    Make registration.update() no longer force bypassing the HTTP cache
    As per the last f2f and the follow-up discussion,
    ServiceWorkerRegistration.update() should not always bypass the HTTP cache but
    bypass it only when 24 hours have passed since the last update check. So, this
    patch involves a change in the behavior of ServiceWorkerRegistration.update().
    
    Spec: https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-registration-update
    Spec discussion: https://github.com/slightlyoff/ServiceWorker/issues/514#issuecomment-130664373
    
    This CL merged two CLs that were created and under review before Blink and
    Chromium repos were merged:
     1. Blink: https://codereview.chromium.org/1332303002/
     2. Chromium: https://codereview.chromium.org/1339623002/
    
    BUG=530540
    
    Review URL: https://codereview.chromium.org/1380323002
    
    Cr-Commit-Position: refs/heads/master@{#352232}

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/ServiceWorkerGlobalScope/resources/update-worker.php
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/ServiceWorkerGlobalScope/resources/update-worker.php
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-// Force the browser to cache this script. update() should always bypass this
-// cache and fetch a new version.
-header('Cache-Control: max-age=86400');
+// update() does not bypass cache so set the max-age to 0 such that update()
+// can find a new version in the network.
+header('Cache-Control: max-age=0');
 
 // Return a different script for each access.
 header('Content-Type:application/javascript');
