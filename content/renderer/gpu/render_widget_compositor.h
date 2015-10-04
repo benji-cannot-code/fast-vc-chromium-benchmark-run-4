5d1db031cbce5fafa6578bbd72a2c0c70f261a4c
oshima <oshima@chromium.org>
2015-10-03 19:06:21 -0700

    Remove unused dsf related method
    BUG=485650
    
    Review URL: https://codereview.chromium.org/1382863003
    
    Cr-Commit-Position: refs/heads/master@{#352267}

--- content/renderer/gpu/render_widget_compositor.h
+++ content/renderer/gpu/render_widget_compositor.h
@@ -94,7 +94,6 @@ class CONTENT_EXPORT RenderWidgetCompositor
   virtual blink::WebFloatPoint adjustEventPointForPinchZoom(
       const blink::WebFloatPoint& point) const;
   void setDeviceScaleFactor(float device_scale) override;
-  float deviceScaleFactor() const override;
   void setBackgroundColor(blink::WebColor color) override;
   void setHasTransparentBackground(bool transparent) override;
   void setVisible(bool visible) override;
