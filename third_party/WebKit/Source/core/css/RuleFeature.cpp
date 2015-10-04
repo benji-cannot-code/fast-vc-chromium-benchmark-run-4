2a6830ae9dc6a66025394c913b2d2bc37d4dbc22
rune <rune@opera.com>
2015-10-04 13:40:27 -0700

    Missing clear() for pseudo invalidation sets.
    Missing clear in RuleFeatureSet::clear caused rules removed from shadow
    trees to still affect invalidation after removal.
    
    R=sigbjornf@opera.com
    BUG=538586
    
    Review URL: https://codereview.chromium.org/1368313005
    
    Cr-Commit-Position: refs/heads/master@{#352278}

--- third_party/WebKit/Source/core/css/RuleFeature.cpp
+++ third_party/WebKit/Source/core/css/RuleFeature.cpp
@@ -548,6 +548,7 @@ void RuleFeatureSet::clear()
     m_classInvalidationSets.clear();
     m_attributeInvalidationSets.clear();
     m_idInvalidationSets.clear();
+    m_pseudoInvalidationSets.clear();
 }
 
 void RuleFeatureSet::collectInvalidationSetsForClass(InvalidationSetVector& invalidationSets, Element& element, const AtomicString& className) const
