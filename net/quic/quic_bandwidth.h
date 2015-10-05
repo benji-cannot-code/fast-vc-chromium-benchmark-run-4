002190c04ac2fc7900bb496d4bbd6017840f1036
zhongyi <zhongyi@chromium.org>
2015-10-05 12:23:24 -0700

    Landing Recent QUIC changes until 9/15/2015 14:26 UTC
    relnote: Change the QuicAckNotifierDelegate to add a per-packet notification method.  Not yet used, so no functional change.
    
    Merge internal change: 103090848
    https://codereview.chromium.org/1381463004/
    
    relnote: N/A. Adds compile warning for unused results.
    
    Merge internal change: 103064667
    https://codereview.chromium.org/1381073004/
    
    Review URL: https://codereview.chromium.org/1385923002
    
    Cr-Commit-Position: refs/heads/master@{#352384}

--- net/quic/quic_bandwidth.h
+++ net/quic/quic_bandwidth.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NET_QUIC_QUIC_BANDWIDTH_H_
 
 #include "base/basictypes.h"
+#include "base/compiler_specific.h"
 #include "net/quic/quic_time.h"
 
 namespace net {
@@ -51,11 +52,11 @@ class NET_EXPORT_PRIVATE QuicBandwidth {
 
   bool IsZero() const;
 
-  QuicBandwidth Add(const QuicBandwidth& delta) const;
+  QuicBandwidth Add(const QuicBandwidth& delta) const WARN_UNUSED_RESULT;
 
-  QuicBandwidth Subtract(const QuicBandwidth& delta) const;
+  QuicBandwidth Subtract(const QuicBandwidth& delta) const WARN_UNUSED_RESULT;
 
-  QuicBandwidth Scale(float scale_factor) const;
+  QuicBandwidth Scale(float scale_factor) const WARN_UNUSED_RESULT;
 
   QuicTime::Delta TransferTime(QuicByteCount bytes) const;
 
