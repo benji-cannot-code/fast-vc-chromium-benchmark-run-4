a6ddee23884f8c84cb641c686e2f7de1ea1c6a65
ananta <ananta@chromium.org>
2015-10-05 13:18:52 -0700

    Add a flag use_software_compositing to the Widget::InitParams structure.
    This flag when set will force the widget to use the software compositor on Windows. Currently only the menu
    omnibox drop down and status bubble widgets set this flag. The flag defaults to false.
    
    This removes the old hack of using the menu type to determine whether the software compositor should be used
    on Windows.
    
    BUG=508717
    R=sky
    
    Review URL: https://codereview.chromium.org/1380973003
    
    Cr-Commit-Position: refs/heads/master@{#352405}

--- ui/views/widget/widget.cc
+++ ui/views/widget/widget.cc
@@ -124,7 +124,8 @@ Widget::InitParams::InitParams()
       desktop_window_tree_host(NULL),
       layer_type(ui::LAYER_TEXTURED),
       context(NULL),
-      force_show_in_taskbar(false) {
+      force_show_in_taskbar(false),
+      force_software_compositing(false) {
 }
 
 Widget::InitParams::InitParams(Type type)
@@ -147,7 +148,8 @@ Widget::InitParams::InitParams(Type type)
       desktop_window_tree_host(NULL),
       layer_type(ui::LAYER_TEXTURED),
       context(NULL),
-      force_show_in_taskbar(false) {
+      force_show_in_taskbar(false),
+      force_software_compositing(false) {
 }
 
 Widget::InitParams::~InitParams() {
