285787b58e5ee56f93f65819bc688dde8b7bcb45
tommycli <tommycli@chromium.org>
2015-10-05 14:51:29 -0700

    Settings Rewrite: Restore id of default id to 'main' from ''.
    Previously, the default subpage was marked as:
    
    <neon-animatable id="">
      ...
    </neon-animatable>
    
    However, this is confusing. This patch changes the default subpage to:
    
    <neon-animatable id="main">
      ...
    </neon-animatable>
    
    BUG=535013
    
    Review URL: https://codereview.chromium.org/1374563010
    
    Cr-Commit-Position: refs/heads/master@{#352438}

--- chrome/browser/resources/settings/settings_page/settings_animated_pages.js
+++ chrome/browser/resources/settings/settings_page/settings_animated_pages.js
@@ -77,7 +77,7 @@ Polymer({
     }
 
     this.$.animatedPages.selected =
-        newRouteIsSubpage ? newRoute.subpage.slice(-1)[0] : '';
+        newRouteIsSubpage ? newRoute.subpage.slice(-1)[0] : 'main';
   },
 
   /**
