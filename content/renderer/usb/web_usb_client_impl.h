9ef02cf76014dc5a2dfd670ec372cac6e719e4fd
finnur <finnur@chromium.org>
2015-10-05 04:10:02 -0700

    Reland of Provide the DeviceManager service to the renderer directly, no app. (patchset #1 id:1 of https://codereview.chromium.org/1382383002/ )
    Reason for revert:
    Revert the revert, since it doesn't apply cleanly (despite passing the CQ)  (sigh)
    
    Original issue's description:
    > Revert of Provide the DeviceManager service to the renderer directly, no app. (patchset #5 id:80001 of https://codereview.chromium.org/1371793004/ )
    >
    > Reason for revert:
    > It is causing a number of layout tests to fail with error:
    >
    > 01:16:18.824 21620   [4:4:1005/011618:1968830910:ERROR:web_usb_client_impl.cc(72)] Device manager connection failed.
    >
    > See for example:
    > http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.6/builds/45170/steps/webkit_tests/logs/stdio
    >
    > Original issue's description:
    > > Provide the DeviceManager service to the renderer directly, no app.
    > >
    > > The Mojo app model forces us to create a new thread on which to run the
    > > device manager. Inside Chrome this then means that we have to post tasks
    > > to the UI thread in order to use the UsbService instance it is
    > > implemented with. This patch removes the devices app from Chrome,
    > > instead registering a factory function for DeviceManagers directly with
    > > the RenderFrameHost. DeviceManagerImpl can then live on the UI thread
    > > which greatly simplfies its implementation. Otherwise we would have to
    > > implement similar thread hopping in DeviceImpl and that would be a lot
    > > of unnecessary code.
    > >
    > > Committed: https://crrev.com/869af1c5170c48b8987b7ce4e2c70c9b37f9beb7
    > > Cr-Commit-Position: refs/heads/master@{#352222}
    >
    > TBR=rockot@chromium.org,thestig@chromium.org,jam@chromium.org,reillyg@chromium.org
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    >
    > Committed: https://crrev.com/66407392ea95965f34b9c69993d0da3a674ca60b
    > Cr-Commit-Position: refs/heads/master@{#352312}
    
    TBR=rockot@chromium.org,thestig@chromium.org,jam@chromium.org,reillyg@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1387823002
    
    Cr-Commit-Position: refs/heads/master@{#352315}

--- content/renderer/usb/web_usb_client_impl.h
+++ content/renderer/usb/web_usb_client_impl.h
@@ -6,17 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_RENDERER_USB_WEB_USB_CLIENT_IMPL_H_
 #define CONTENT_RENDERER_USB_WEB_USB_CLIENT_IMPL_H_
 
-#include "base/id_map.h"
 #include "base/macros.h"
 #include "device/devices_app/usb/public/interfaces/device_manager.mojom.h"
-#include "mojo/application/public/interfaces/service_provider.mojom.h"
 #include "third_party/WebKit/public/platform/modules/webusb/WebUSBClient.h"
 
 namespace content {
 
+class ServiceRegistry;
+
 class WebUSBClientImpl : public blink::WebUSBClient {
  public:
-  explicit WebUSBClientImpl(mojo::ServiceProviderPtr device_services);
+  explicit WebUSBClientImpl(content::ServiceRegistry* service_registry);
   ~WebUSBClientImpl() override;
 
  private:
@@ -30,7 +30,6 @@ class WebUSBClientImpl : public blink::WebUSBClient {
   void OnDeviceChangeNotification(
       device::usb::DeviceChangeNotificationPtr notification);
 
-  mojo::ServiceProviderPtr device_services_;
   device::usb::DeviceManagerPtr device_manager_;
   Observer* observer_ = nullptr;
 
--- device/devices_app/devices_app.h
+++ device/devices_app/devices_app.h
@@ -13,10 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/application/public/cpp/application_delegate.h"
 #include "mojo/application/public/cpp/interface_factory.h"
 
-namespace base {
-class SequencedTaskRunner;
-}
-
 namespace mojo {
 class ApplicationImpl;
 }
@@ -30,8 +26,7 @@ class DeviceManager;
 class DevicesApp : public mojo::ApplicationDelegate,
                    public mojo::InterfaceFactory<usb::DeviceManager> {
  public:
-  explicit DevicesApp(
-      scoped_refptr<base::SequencedTaskRunner> service_task_runner);
+  DevicesApp();
   ~DevicesApp() override;
 
  private:
@@ -57,7 +52,6 @@ class DevicesApp : public mojo::ApplicationDelegate,
 
   mojo::ApplicationImpl* app_impl_;
   scoped_ptr<USBServiceInitializer> service_initializer_;
-  scoped_refptr<base::SequencedTaskRunner> service_task_runner_;
   size_t active_device_manager_count_;
 
   // Callback used to shut down the app after a period of inactivity.
