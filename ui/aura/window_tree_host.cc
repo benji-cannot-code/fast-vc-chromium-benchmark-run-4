fc31eea0534fa337f2cf7ea3401718465aac0904
sievers <sievers@chromium.org>
2015-10-05 16:38:02 -0700

    aura: Defer OutputSurface creation until widget is available
    Instead of using setLayerTreeHostClientReady() just defer
    creation of the OutputSurface.
    
    BUG=538713,538714
    TBR=sadrul@chromium.org for renamed method call sites
    
    Review URL: https://codereview.chromium.org/1373033004
    
    Cr-Commit-Position: refs/heads/master@{#352463}

--- ui/aura/window_tree_host.cc
+++ ui/aura/window_tree_host.cc
@@ -257,7 +257,7 @@ void WindowTreeHost::CreateCompositor() {
 }
 
 void WindowTreeHost::OnAcceleratedWidgetAvailable() {
-  compositor_->SetAcceleratedWidgetAndStartCompositor(GetAcceleratedWidget());
+  compositor_->SetAcceleratedWidget(GetAcceleratedWidget());
   prop_.reset(new ui::ViewProp(GetAcceleratedWidget(),
                                kWindowTreeHostForAcceleratedWidget, this));
 }
