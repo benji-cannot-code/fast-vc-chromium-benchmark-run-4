fa900eccb5a26c182697986ab51dbf689270bc9a
dgozman <dgozman@chromium.org>
2015-10-05 15:35:14 -0700

    [DevTools] Do not show blocked resource twice if it was preloaded.
    BUG=535047
    
    Review URL: https://codereview.chromium.org/1380243002
    
    Cr-Commit-Position: refs/heads/master@{#352446}

--- third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
+++ third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
@@ -353,7 +353,8 @@ bool FrameFetchContext::canRequest(Resource::Type type, const ResourceRequest& r
 {
     ResourceRequestBlockedReason reason = canRequestInternal(type, resourceRequest, url, options, forPreload, originRestriction);
     if (reason != ResourceRequestBlockedReasonNone) {
-        InspectorInstrumentation::didBlockRequest(frame(), resourceRequest, ensureLoaderForNotifications(), options.initiatorInfo, reason);
+        if (!forPreload)
+            InspectorInstrumentation::didBlockRequest(frame(), resourceRequest, ensureLoaderForNotifications(), options.initiatorInfo, reason);
         return false;
     }
     return true;
