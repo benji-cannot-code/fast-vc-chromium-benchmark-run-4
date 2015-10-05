d31ae671507d81cd4abc40bda7f3fecbcf0ae3ce
primiano <primiano@chromium.org>
2015-10-05 15:49:10 -0700

    [tracing] Expose a GetOrCreateAllocatorDump for memory tracing
    Small non-functional refactor in preparation of upcoming CLs.
    
    BUG=
    TBR=reed@google.com
    
    Review URL: https://codereview.chromium.org/1387833004
    
    Cr-Commit-Position: refs/heads/master@{#352453}

--- skia/ext/skia_trace_memory_dump_impl.cc
+++ skia/ext/skia_trace_memory_dump_impl.cc
@@ -38,7 +38,7 @@ void SkiaTraceMemoryDumpImpl::dumpNumericValue(const char* dumpName,
                                                const char* valueName,
                                                const char* units,
                                                uint64_t value) {
-  auto dump = GetOrCreateAllocatorDump(dumpName);
+  auto dump = process_memory_dump_->GetOrCreateAllocatorDump(dumpName);
   dump->AddScalar(valueName, units, value);
 }
 
@@ -46,7 +46,7 @@ void SkiaTraceMemoryDumpImpl::setMemoryBacking(const char* dumpName,
                                                const char* backingType,
                                                const char* backingObjectId) {
   if (strcmp(backingType, kMallocBackingType) == 0) {
-    auto dump = GetOrCreateAllocatorDump(dumpName);
+    auto dump = process_memory_dump_->GetOrCreateAllocatorDump(dumpName);
     const char* system_allocator_name =
         base::trace_event::MemoryDumpManager::GetInstance()
             ->system_allocator_pool_name();
@@ -74,15 +74,6 @@ void SkiaTraceMemoryDumpImpl::setDiscardableMemoryBacking(
 SkTraceMemoryDump::LevelOfDetail SkiaTraceMemoryDumpImpl::getRequestedDetails()
     const {
   return request_level_;
-}
-
-base::trace_event::MemoryAllocatorDump*
-SkiaTraceMemoryDumpImpl::GetOrCreateAllocatorDump(const char* dumpName) {
-  std::string name = dump_name_prefix_ + dumpName;
-  auto dump = process_memory_dump_->GetAllocatorDump(name);
-  if (!dump)
-    dump = process_memory_dump_->CreateAllocatorDump(name);
-  return dump;
 }
 
 }  // namespace skia
