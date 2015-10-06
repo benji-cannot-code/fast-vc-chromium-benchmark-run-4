e350196f2e2c8d0d96cba89bf98eea073de48061
schenney <schenney@chromium.org>
2015-10-06 10:56:08 -0700

    Debugging info for document lifecycles.
    Problems with DocumentLifecycle states are hard to debug without
    information about which state you're in. Here we add debug printf
    information available during debug builds.
    
    An additional assert is added to ensure we do not leave layout
    when the document lifecycle thinks layout still needs to happen. This
    catches lifecycle problems sooner than they otherwise would fail.
    
    R=leviw@chromium.org,chrishtr@chromium.org,pdr@chromium.org
    BUG=537465
    
    Review URL: https://codereview.chromium.org/1384993002
    
    Cr-Commit-Position: refs/heads/master@{#352639}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -1722,8 +1722,10 @@ void Document::updateLayoutTree(StyleRecalcChange change)
     if (!view() || !isActive())
         return;
 
-    if (change != Force && !needsLayoutTreeUpdate())
+    if (change != Force && !needsLayoutTreeUpdate()) {
+        ASSERT(lifecycle().state() != DocumentLifecycle::VisualUpdatePending);
         return;
+    }
 
     if (inStyleRecalc())
         return;
--- third_party/WebKit/Source/core/dom/DocumentLifecycle.h
+++ third_party/WebKit/Source/core/dom/DocumentLifecycle.h
@@ -158,6 +158,10 @@ public:
         m_detachCount--;
     }
 
+#if ENABLE(ASSERT)
+    static const char* stateAsDebugString(const State);
+#endif
+
 private:
 #if ENABLE(ASSERT)
     bool canAdvanceTo(State) const;
