1b87bdf2af0a886beca11f27349aa07a1be943d6
cmumford <cmumford@chromium.org>
2015-10-06 13:52:17 -0700

    IndexedDB: Check for negative (invalid) schema values.
    Negative values would indicate a corrupt database.
    
    Review URL: https://codereview.chromium.org/1386943003
    
    Cr-Commit-Position: refs/heads/master@{#352679}

--- content/browser/indexed_db/indexed_db_backing_store.cc
+++ content/browser/indexed_db/indexed_db_backing_store.cc
@@ -326,6 +326,8 @@ WARN_UNUSED_RESULT static bool IsSchemaKnown(LevelDBDatabase* db, bool* known) {
     *known = true;
     return true;
   }
+  if (db_schema_version < 0)
+    return false;  // Only corruption should cause this.
   if (db_schema_version > kLatestKnownSchemaVersion) {
     *known = false;
     return true;
@@ -341,7 +343,8 @@ WARN_UNUSED_RESULT static bool IsSchemaKnown(LevelDBDatabase* db, bool* known) {
     *known = true;
     return true;
   }
-
+  if (db_data_version < 0)
+    return false;  // Only corruption should cause this.
   if (db_data_version > latest_known_data_version) {
     *known = false;
     return true;
