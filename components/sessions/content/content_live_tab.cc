e17a1957c7d49ea7408fef76d3f5634ec37efe51
creis <creis@chromium.org>
2015-10-05 17:40:24 -0700

    Add a LiveTab::IsInitialNavigation method.
    This is a prerequisite for creating a NavigationEntry for the initial
    about:blank page, since GetEntryCount() will never be 0.  Callers can
    check IsInitialNavigation to see if the tab needs to be restored, etc.
    
    BUG=524208
    TEST=No behavior change.
    
    Review URL: https://codereview.chromium.org/1380443005
    
    Cr-Commit-Position: refs/heads/master@{#352488}

--- components/sessions/content/content_live_tab.cc
+++ components/sessions/content/content_live_tab.cc
@@ -29,6 +29,10 @@ ContentLiveTab::ContentLiveTab(content::WebContents* contents)
 
 ContentLiveTab::~ContentLiveTab() {}
 
+bool ContentLiveTab::IsInitialNavigation() {
+  return navigation_controller().IsInitialNavigation();
+}
+
 int ContentLiveTab::GetCurrentEntryIndex() {
   return navigation_controller().GetCurrentEntryIndex();
 }
