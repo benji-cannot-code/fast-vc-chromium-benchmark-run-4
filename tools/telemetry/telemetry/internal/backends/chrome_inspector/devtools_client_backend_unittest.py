5c34d96ad1b8f8d594ed71b3f7ec1aed75cd27a1
petrcermak <petrcermak@chromium.org>
2015-10-06 12:51:57 -0700

    [telemetry] Move browser inspector websocket from TracingBackend to DevToolsClientBackend
    As discussed in https://codereview.chromium.org/1375633006/, this patch
    moves the ownership of the browser inspector websocket to
    DevToolsClientBackend so that it could be reused by other backends.
    
    BUG=516776
    
    Review URL: https://codereview.chromium.org/1392503004
    
    Cr-Commit-Position: refs/heads/master@{#352668}

--- tools/telemetry/telemetry/internal/backends/chrome_inspector/devtools_client_backend_unittest.py
+++ tools/telemetry/telemetry/internal/backends/chrome_inspector/devtools_client_backend_unittest.py
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from telemetry import decorators
 from telemetry.testing import browser_test_case
+from telemetry.timeline import model
+from telemetry.timeline import trace_data
+from telemetry.timeline import tracing_options
 
 
 class DevToolsClientBackendTest(browser_test_case.BrowserTestCase):
@@ -73,3 +76,18 @@ class DevToolsClientBackendTest(browser_test_case.BrowserTestCase):
     self.assertEqual(len(c2.contexts), 1)
     self.assertTrue('blank.html' in c2.contexts[0]['url'])
     self.assertEqual(c2.GetInspectorBackend(context_id), backend)
+
+  def testTracing(self):
+    devtools_client = self._devtools_client
+    if not devtools_client.IsChromeTracingSupported():
+      self.skipTest('Browser does not support tracing, skipping test.')
+
+    # Start Chrome tracing.
+    options = tracing_options.TracingOptions()
+    options.enable_chrome_trace = True
+    devtools_client.StartChromeTracing(options)
+
+    # Stop Chrome tracing and check that the resulting data is valid.
+    builder = trace_data.TraceDataBuilder()
+    devtools_client.StopChromeTracing(builder)
+    model.TimelineModel(builder.AsData())
