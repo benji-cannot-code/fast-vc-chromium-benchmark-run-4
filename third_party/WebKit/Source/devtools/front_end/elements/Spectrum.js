8748d0a192cd0b93aec79e45b8dc8def0231db62
samli <samli@chromium.org>
2015-10-06 12:40:59 -0700

    Devtools Color: Improve load performance of the color picker
    BUG=477242
    
    Review URL: https://codereview.chromium.org/1383323002
    
    Cr-Commit-Position: refs/heads/master@{#352665}

--- third_party/WebKit/Source/devtools/front_end/elements/Spectrum.js
+++ third_party/WebKit/Source/devtools/front_end/elements/Spectrum.js
@@ -130,6 +130,7 @@ WebInspector.Spectrum = function()
     addColorButton.addEventListener("click", this._addColorToCustomPalette.bind(this));
     this._addColorToolbar.appendToolbarItem(addColorButton);
 
+    this._loadPalettes();
     new WebInspector.Spectrum.PaletteGenerator(this._generatedPaletteLoaded.bind(this));
 
     /**
@@ -267,7 +268,6 @@ WebInspector.Spectrum.prototype = {
         var numItems = palette.colors.length;
         if (palette.mutable)
             numItems++;
-        var rowsNeeded = Math.max(1, Math.ceil(numItems / WebInspector.Spectrum._itemsPerPaletteRow));
         if (palette.mutable) {
             this._paletteContainer.appendChild(this._addColorToolbar.element);
             this._paletteContainer.appendChild(this._deleteIconToolbar.element);
@@ -277,10 +277,6 @@ WebInspector.Spectrum.prototype = {
         }
 
         this._togglePalettePanel(false);
-        var paletteColorHeight = 12;
-        var paletteMargin = 12;
-        this.element.style.height = (this._paletteContainer.offsetTop + paletteMargin + (paletteColorHeight + paletteMargin) * rowsNeeded) + "px";
-        this.dispatchEventToListeners(WebInspector.Spectrum.Events.SizeChanged);
     },
 
     /**
@@ -386,13 +382,8 @@ WebInspector.Spectrum.prototype = {
         this._deleteButton.setToggled(false);
     },
 
-    /**
-     * @param {!WebInspector.Spectrum.Palette} generatedPalette
-     */
-    _generatedPaletteLoaded: function(generatedPalette)
+    _loadPalettes: function()
     {
-        if (generatedPalette.colors.length)
-            this._palettes.set(generatedPalette.title, generatedPalette);
         this._palettes.set(WebInspector.Spectrum.MaterialPalette.title, WebInspector.Spectrum.MaterialPalette);
         /** @type {!WebInspector.Spectrum.Palette} */
         var defaultCustomPalette = { title: "Custom", colors: [], mutable: true };
@@ -400,10 +391,28 @@ WebInspector.Spectrum.prototype = {
         this._palettes.set(this._customPaletteSetting.get().title, this._customPaletteSetting.get());
 
         this._selectedColorPalette = WebInspector.settings.createSetting("selectedColorPalette", WebInspector.Spectrum.GeneratedPaletteTitle);
-        var paletteToShow = this._palettes.get(this._selectedColorPalette.get() || WebInspector.Spectrum.GeneratedPaletteTitle)
-            || this._palettes.get("Material");
-        if (paletteToShow)
-            this._showPalette(paletteToShow, true);
+        var palette = this._palettes.get(this._selectedColorPalette.get());
+        if (palette) {
+            this._resizeForSelectedPalette();
+            this._showPalette(palette, true);
+        }
+    },
+
+    /**
+     * @param {!WebInspector.Spectrum.Palette} generatedPalette
+     */
+    _generatedPaletteLoaded: function(generatedPalette)
+    {
+        if (generatedPalette.colors.length)
+            this._palettes.set(generatedPalette.title, generatedPalette);
+        if (this._selectedColorPalette.get() !== generatedPalette.title) {
+            return;
+        } else if (!generatedPalette.colors.length) {
+            this._paletteSelected(WebInspector.Spectrum.MaterialPalette);
+            return;
+        }
+        this._resizeForSelectedPalette();
+        this._showPalette(generatedPalette, true);
     },
 
     /**
@@ -430,7 +439,24 @@ WebInspector.Spectrum.prototype = {
     _paletteSelected: function(palette)
     {
         this._selectedColorPalette.set(palette.title);
+        this._resizeForSelectedPalette();
         this._showPalette(palette, true);
+    },
+
+    _resizeForSelectedPalette: function()
+    {
+        var palette = this._palettes.get(this._selectedColorPalette.get());
+        if (!palette)
+            return;
+        var rowsNeeded = Math.max(1, Math.ceil(palette.colors.length / WebInspector.Spectrum._itemsPerPaletteRow));
+        if (this._numPaletteRowsShown === rowsNeeded)
+            return;
+        this._numPaletteRowsShown = rowsNeeded;
+        var paletteColorHeight = 12;
+        var paletteMargin = 12;
+        var paletteTop = 235;
+        this.element.style.height = (paletteTop + paletteMargin + (paletteColorHeight + paletteMargin) * rowsNeeded) + "px";
+        this.dispatchEventToListeners(WebInspector.Spectrum.Events.SizeChanged);
     },
 
     /**
