68512d526a0374c2cb97e90d4a3d8bbb889a3cd1
rch <rch@chromium.org>
2015-10-05 20:40:21 -0700

    Make QUIC epoll client work with cert verification.
    BUG=539622
    
    Review URL: https://codereview.chromium.org/1372193006
    
    Cr-Commit-Position: refs/heads/master@{#352530}

--- net/tools/quic/quic_client_bin.cc
+++ net/tools/quic/quic_client_bin.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/message_loop/message_loop.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
@@ -177,6 +178,7 @@ int main(int argc, char *argv[]) {
           << " initial_mtu: " << FLAGS_initial_mtu;
 
   base::AtExitManager exit_manager;
+  base::MessageLoopForIO message_loop;
 
   // Determine IP address to connect to from supplied hostname.
   net::IPAddressNumber ip_addr;
