b259264598a4bf56ddb56fbda6fe5d1e66e5bcc1
brucedawson <brucedawson@chromium.org>
2015-10-06 12:22:00 -0700

    Detecting and fixing stringprintf.h format bugs
    The print functions in stringprintf.h were not annotated for /analyze so
    13 Windows specific format-string bugs accumulated. This annotates the
    functions so that the /analyze builder will find the problems and fixes
    the bugs.
    
    R=thestig@chromium.org,wfh@chromium.org,jam@chromium.org
    Skipping wstring presubmit checks - no new wstring usage is added
    NOPRESUBMIT=true
    BUG=427616
    
    Review URL: https://codereview.chromium.org/1372153002
    
    Cr-Commit-Position: refs/heads/master@{#352659}

--- base/files/file_path.h
+++ base/files/file_path.h
@@ -125,6 +125,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define FILE_PATH_USES_WIN_SEPARATORS
 #endif  // OS_WIN
 
+// To print path names portably use PRIsFP (based on PRIuS and friends from
+// C99 and format_macros.h) like this:
+// base::StringPrintf("Path is %" PRIsFP ".\n", path.value().c_str());
+#if defined(OS_POSIX)
+#define PRIsFP "s"
+#elif defined(OS_WIN)
+#define PRIsFP "ls"
+#endif  // OS_WIN
+
 namespace base {
 
 class Pickle;
