e93a655a988e7f0e229f14b9775ad3613e6da91b
toyoshim <toyoshim@chromium.org>
2015-10-06 08:47:17 -0700

    Web MIDI (ALSA): finalize on the IO thread that ran initialization
    Now the MidiManagerAlsa creates a dedicated thread on the IO thread,
    and stops it on the browser main thread. This is wrong usage, and the
    thread should be stopped on the same thread that creates it.
    
    This patch move destruction code to Finalize() that ran on the IO thread
    before the destructor is called on the browser main thread.
    
    BUG=374341
    
    Review URL: https://codereview.chromium.org/1329233002
    
    Cr-Commit-Position: refs/heads/master@{#352610}

--- media/midi/midi_manager_alsa.h
+++ media/midi/midi_manager_alsa.h
@@ -31,6 +31,7 @@ class MIDI_EXPORT MidiManagerAlsa final : public MidiManager {
 
   // MidiManager implementation.
   void StartInitialization() override;
+  void Finalize() override;
   void DispatchSendMidiData(MidiManagerClient* client,
                             uint32 port_index,
                             const std::vector<uint8>& data,
