8e0447ce2bacb6e871b6b18c602435028ab21a55
davidben <davidben@chromium.org>
2015-10-05 21:38:29 -0700

    Call EnsureNSSHttpIOInit in the chimera build.
    Otherwise we end up using the default NSS client and not
    SystemURLRequestContext.
    
    This is a minimal fix to be merged onto release branches.
    A follow-up will revise this to be somewhat less error-prone.
    
    BUG=539520
    TEST=Open about:net-internals on Linux or Chrome OS
         Visit https://incomplete-chain.badssl.com/ in a new tab
         The Events tab of about:net-internals should show a fetch for http://crt.comodoca.com/COMODORSADomainValidationSecureServerCA.crt
    
    Review URL: https://codereview.chromium.org/1384343002
    
    Cr-Commit-Position: refs/heads/master@{#352536}

--- net/socket/ssl_client_socket_openssl.cc
+++ net/socket/ssl_client_socket_openssl.cc
@@ -56,6 +56,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/ssl/ssl_platform_key.h"
 #endif
 
+#if defined(USE_NSS_CERTS) || defined(OS_IOS)
+#include "net/cert_net/nss_ocsp.h"
+#endif
+
 namespace net {
 
 namespace {
@@ -791,6 +795,14 @@ int SSLClientSocketOpenSSL::SetSendBufferSize(int32 size) {
 int SSLClientSocketOpenSSL::Init() {
   DCHECK(!ssl_);
   DCHECK(!transport_bio_);
+
+#if defined(USE_NSS_CERTS) || defined(OS_IOS)
+  if (ssl_config_.cert_io_enabled) {
+    // TODO(davidben): Move this out of SSLClientSocket. See
+    // https://crbug.com/539520.
+    EnsureNSSHttpIOInit();
+  }
+#endif
 
   SSLContext* context = SSLContext::GetInstance();
   crypto::OpenSSLErrStackTracer err_tracer(FROM_HERE);
