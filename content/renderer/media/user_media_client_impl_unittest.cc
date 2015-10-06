2c5efc460942d7cafbf2f9239394d4301d2e6a64
peria <peria@chromium.org>
2015-10-05 17:05:36 -0700

    Use WebHeap::collectAllGarbageForTesting instead of WebHeap::collectGarbageForTesting.
    Background:
    Blink memory team is shipping a mark-and-sweep GC (Oilpan) to
    media stream classes, and those classes have many references
    between Chromium and Blink.
    If a reference chain has multiple references between Chromium
    and Blink, collectGarbageForTesting collects one of them.
    Instead, collectAllGarageForTesting is expected to collect
    all references.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1383803003
    
    Cr-Commit-Position: refs/heads/master@{#352471}

--- content/renderer/media/user_media_client_impl_unittest.cc
+++ content/renderer/media/user_media_client_impl_unittest.cc
@@ -372,7 +372,7 @@ TEST_F(UserMediaClientImplTest, StopSourceWhenMediaStreamGoesOutOfScope) {
   // Makes sure the test itself don't hold a reference to the created
   // MediaStream.
   used_media_impl_->ClearLastGeneratedStream();
-  blink::WebHeap::collectGarbageForTesting();
+  blink::WebHeap::collectAllGarbageForTesting();
 
   // Expect the sources to be stopped when the MediaStream goes out of scope.
   EXPECT_EQ(1, ms_dispatcher_->stop_audio_device_counter());
@@ -386,6 +386,7 @@ TEST_F(UserMediaClientImplTest, FrameWillClose) {
   blink::WebMediaStream mixed_desc = RequestLocalMediaStream();
   blink::WebMediaStream desc2 = RequestLocalMediaStream();
   used_media_impl_->FrameWillClose();
+  blink::WebHeap::collectAllGarbageForTesting();
   EXPECT_EQ(1, ms_dispatcher_->stop_audio_device_counter());
   EXPECT_EQ(1, ms_dispatcher_->stop_video_device_counter());
 }
@@ -399,7 +400,7 @@ TEST_F(UserMediaClientImplTest, MediaVideoSourceFailToStart) {
             used_media_impl_->request_state());
   EXPECT_EQ(MEDIA_DEVICE_TRACK_START_FAILURE,
             used_media_impl_->error_reason());
-  blink::WebHeap::collectGarbageForTesting();
+  blink::WebHeap::collectAllGarbageForTesting();
   EXPECT_EQ(1, ms_dispatcher_->request_stream_counter());
   EXPECT_EQ(1, ms_dispatcher_->stop_audio_device_counter());
   EXPECT_EQ(1, ms_dispatcher_->stop_video_device_counter());
@@ -415,7 +416,7 @@ TEST_F(UserMediaClientImplTest, MediaAudioSourceFailToInitialize) {
             used_media_impl_->request_state());
   EXPECT_EQ(MEDIA_DEVICE_TRACK_START_FAILURE,
             used_media_impl_->error_reason());
-  blink::WebHeap::collectGarbageForTesting();
+  blink::WebHeap::collectAllGarbageForTesting();
   EXPECT_EQ(1, ms_dispatcher_->request_stream_counter());
   EXPECT_EQ(1, ms_dispatcher_->stop_audio_device_counter());
   EXPECT_EQ(1, ms_dispatcher_->stop_video_device_counter());
@@ -463,6 +464,7 @@ TEST_F(UserMediaClientImplTest, StopTrackAfterReload) {
   blink::WebMediaStream mixed_desc = RequestLocalMediaStream();
   EXPECT_EQ(1, ms_dispatcher_->request_stream_counter());
   used_media_impl_->FrameWillClose();
+  blink::WebHeap::collectAllGarbageForTesting();
   EXPECT_EQ(1, ms_dispatcher_->stop_audio_device_counter());
   EXPECT_EQ(1, ms_dispatcher_->stop_video_device_counter());
 
