843e17df5d4860c1702c8ba306f7661f4fe2544b
kuan <kuan@chromium.org>
2015-10-06 15:08:33 -0700

    Revert of Enable Wallet metadata sync. (patchset #2 id:20001 of https://codereview.chromium.org/1387193003/ )
    Reason for revert:
    broke builds:
    
    https://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%281%29/builds/10568
    
    https://build.chromium.org/p/chromium.linux/builders/Linux%20GN/builds/32936
    
    etc...
    
    Original issue's description:
    > Enable Wallet metadata sync.
    >
    > BUG=481595
    >
    > Committed: https://crrev.com/90c4e9472b95803cf8220cd749a25523aa80b0b3
    > Cr-Commit-Position: refs/heads/master@{#352686}
    
    TBR=zea@chromium.org,estade@chromium.org,rouslan@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=481595
    
    Review URL: https://codereview.chromium.org/1375323006
    
    Cr-Commit-Position: refs/heads/master@{#352698}

--- chrome/browser/sync/test/integration/single_client_wallet_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_wallet_sync_test.cc
@@ -138,7 +138,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientWalletSyncTest, EnabledViaPreference) {
       syncer::AUTOFILL_WALLET_DATA));
   // TODO(pvalenzuela): Assert that the local root node for AUTOFILL_WALLET_DATA
   // exists.
-  ASSERT_TRUE(GetClient(0)->service()->GetActiveDataTypes().Has(
+  ASSERT_FALSE(GetClient(0)->service()->GetActiveDataTypes().Has(
       syncer::AUTOFILL_WALLET_METADATA));
 }
 
@@ -158,7 +158,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientWalletSyncTest,
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed";
   ASSERT_TRUE(GetClient(0)->service()->GetActiveDataTypes().Has(
       syncer::AUTOFILL_WALLET_DATA));
-  ASSERT_TRUE(GetClient(0)->service()->GetActiveDataTypes().Has(
+  ASSERT_FALSE(GetClient(0)->service()->GetActiveDataTypes().Has(
       syncer::AUTOFILL_WALLET_METADATA));
 }
 
@@ -186,7 +186,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientWalletSyncTest,
   ASSERT_FALSE(enabled_checker.TimedOut());
   ASSERT_TRUE(GetClient(0)->service()->GetActiveDataTypes().Has(
       syncer::AUTOFILL_WALLET_DATA));
-  ASSERT_TRUE(GetClient(0)->service()->GetActiveDataTypes().Has(
+  ASSERT_FALSE(GetClient(0)->service()->GetActiveDataTypes().Has(
       syncer::AUTOFILL_WALLET_METADATA));
 
   // Then disable the experiment.
