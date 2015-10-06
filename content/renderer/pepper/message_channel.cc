cde515fe19021f0d2f4ec74bdfc132a8ad788d15
tommycli <tommycli@chromium.org>
2015-10-05 17:22:54 -0700

    Plugin-Pepper: Coalesce JavaScript message queue draining.
    See bug for a trace with pathological behavior. We can (probably) solve
    this at least partially by not re-scheduling a message queue draining
    call when one is already scheduled.
    
    This simple patch just adds a boolean. Sure would be embarassing if my
    first try is incorrect for something so simple.
    
    BUG=534826
    
    Review URL: https://codereview.chromium.org/1371093005
    
    Cr-Commit-Position: refs/heads/master@{#352478}

--- content/renderer/pepper/message_channel.cc
+++ content/renderer/pepper/message_channel.cc
@@ -187,6 +187,7 @@ MessageChannel::MessageChannel(PepperPluginInstanceImpl* instance)
     : gin::NamedPropertyInterceptor(instance->GetIsolate(), this),
       instance_(instance),
       js_message_queue_state_(WAITING_TO_START),
+      drain_js_message_queue_scheduled_(false),
       blocking_message_depth_(0),
       plugin_message_queue_state_(WAITING_TO_START),
       var_converter_(instance->pp_instance(),
@@ -433,6 +434,9 @@ void MessageChannel::DrainCompletedPluginMessages() {
 }
 
 void MessageChannel::DrainJSMessageQueue() {
+  DCHECK(drain_js_message_queue_scheduled_);
+  drain_js_message_queue_scheduled_ = false;
+
   if (!instance_)
     return;
   if (js_message_queue_state_ == SEND_DIRECTLY)
@@ -450,9 +454,13 @@ void MessageChannel::DrainJSMessageQueue() {
 }
 
 void MessageChannel::DrainJSMessageQueueSoon() {
+  if (drain_js_message_queue_scheduled_)
+    return;
+
   base::ThreadTaskRunnerHandle::Get()->PostTask(
       FROM_HERE, base::Bind(&MessageChannel::DrainJSMessageQueue,
                             weak_ptr_factory_.GetWeakPtr()));
+  drain_js_message_queue_scheduled_ = true;
 }
 
 void MessageChannel::UnregisterSyncMessageStatusObserver() {
