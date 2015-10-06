78c83e414e5f2e12476a72d12bacce28ac7a2355
sigbjornf <sigbjornf@opera.com>
2015-10-06 00:37:55 -0700

    Remove unused LinkedStack.h include.
    One of the few ones left in Blink, and not used.
    
    R=haraken
    BUG=none
    
    Review URL: https://codereview.chromium.org/1385173002
    
    Cr-Commit-Position: refs/heads/master@{#352561}

--- third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.h
+++ third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/ImageBufferSurface.h"
 #include "public/platform/WebThread.h"
 #include "third_party/skia/include/core/SkCanvas.h"
-#include "wtf/LinkedStack.h"
 #include "wtf/OwnPtr.h"
 #include "wtf/RefPtr.h"
 
