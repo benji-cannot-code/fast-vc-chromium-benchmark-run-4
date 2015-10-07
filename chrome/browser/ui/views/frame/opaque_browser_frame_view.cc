7dc837a1309ecf2a8cfec9714fb41c8373ded432
Peter Kasting <pkasting@google.com>
2015-10-06 20:03:41 -0700

    Remove ShouldLeaveOffsetNearTopBorder().
    This function is unconditionally implemented as "!IsMaximized()" so just replace
    calls with that.
    
    BUG=none
    TEST=none
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1388243002 .
    
    Cr-Commit-Position: refs/heads/master@{#352752}

--- chrome/browser/ui/views/frame/opaque_browser_frame_view.cc
+++ chrome/browser/ui/views/frame/opaque_browser_frame_view.cc
@@ -407,10 +407,6 @@ int OpaqueBrowserFrameView::GetIconSize() const {
 #endif
 }
 
-bool OpaqueBrowserFrameView::ShouldLeaveOffsetNearTopBorder() const {
-  return frame()->ShouldLeaveOffsetNearTopBorder();
-}
-
 gfx::Size OpaqueBrowserFrameView::GetBrowserViewMinimumSize() const {
   return browser_view()->GetMinimumSize();
 }
--- chrome/browser/ui/views/frame/opaque_browser_frame_view.h
+++ chrome/browser/ui/views/frame/opaque_browser_frame_view.h
@@ -75,7 +75,6 @@ class OpaqueBrowserFrameView : public BrowserNonClientFrameView,
   bool ShouldShowWindowTitle() const override;
   base::string16 GetWindowTitle() const override;
   int GetIconSize() const override;
-  bool ShouldLeaveOffsetNearTopBorder() const override;
   gfx::Size GetBrowserViewMinimumSize() const override;
   bool ShouldShowCaptionButtons() const override;
   bool ShouldShowAvatar() const override;
