51f2deeff3e819f39160decad8d7e1a59e5c90b5
nednguyen <nednguyen@google.com>
2015-10-06 20:39:49 -0700

    [Telemetry] Move listing stray processes to more appropriate places.
    We move telemetry's stray processes listing registration to telemetry's
    running entry points, i.e: benchmark_runner & run_tests instead of
    telemetry/__init__.py to avoid pylint script picking up this registration
    hook.
    
    BUG=536862
    
    Review URL: https://codereview.chromium.org/1379063003
    
    Cr-Commit-Position: refs/heads/master@{#352757}

--- tools/telemetry/telemetry/benchmark_runner.py
+++ tools/telemetry/telemetry/benchmark_runner.py
@@ -32,6 +32,7 @@ from telemetry.internal.browser import browser_finder
 from telemetry.internal.browser import browser_options
 from telemetry.internal.util import binary_manager
 from telemetry.internal.util import command_line
+from telemetry.internal.util import ps_util
 
 
 def _IsBenchmarkEnabled(benchmark_class, possible_browser):
@@ -388,6 +389,7 @@ def _GetJsonBenchmarkList(possible_browser, possible_reference_browser,
 
 
 def main(environment):
+  ps_util.EnableListingStrayProcessesUponExitHook()
   # Get the command name from the command line.
   if len(sys.argv) > 1 and sys.argv[1] == '--help':
     sys.argv[1] = 'help'
