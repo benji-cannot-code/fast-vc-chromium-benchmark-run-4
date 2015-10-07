a01b4c70cd4f825cb0ddde0fe86910840be16e48
xjz <xjz@chromium.org>
2015-10-06 20:42:50 -0700

    Add h264 quantizer parser.
    The zero configuration needs the quantizer value used by the encoder.
    Currently the HW h264 encoder does not provide this value.
    This CL decodes the averaged slice quantizer value for each frame
    encoded by h264 HW encoder.
    
    BUG=516089
    
    Review URL: https://codereview.chromium.org/1378353003
    
    Cr-Commit-Position: refs/heads/master@{#352758}

--- media/cast/sender/external_video_encoder.h
+++ media/cast/sender/external_video_encoder.h
@@ -110,7 +110,7 @@ class SizeAdaptableExternalVideoEncoder : public SizeAdaptableVideoEncoderBase {
 class QuantizerEstimator {
  public:
   enum {
-    NO_RESULT = 0,
+    NO_RESULT = -1,
     MIN_VP8_QUANTIZER = 4,
     MAX_VP8_QUANTIZER = 63,
   };
