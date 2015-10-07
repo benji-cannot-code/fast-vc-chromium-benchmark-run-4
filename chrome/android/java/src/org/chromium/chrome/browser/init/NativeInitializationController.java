101c5dfde2958e36b72eb7860f6146a4ccb83e6f
tedchoc <tedchoc@chromium.org>
2015-10-07 13:38:44 -0700

    [Android] Fix a timing issue on startup that could leave the UI inoperable.
    If you start up Chrome on Android, immediately hit the home button,
    wait long enough for the native libraries to fully load, and then return
    to Chrome, it will sometimes leave the UI unfunctional with no tab
    content shown.  You'll be able to focus the omnibox, but nothing beyond
    that works.
    
    This was caused by a slightly different timing of the predraw listener
    used to signal first draw and the activities logic of determining
    whether the first draw occured.  This removes the activities handling
    and makes the predraw listener the only signal.
    
    BUG=538341
    
    Review URL: https://codereview.chromium.org/1384373002
    
    Cr-Commit-Position: refs/heads/master@{#352934}

--- chrome/android/java/src/org/chromium/chrome/browser/init/NativeInitializationController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/NativeInitializationController.java
@@ -38,6 +38,7 @@ class NativeInitializationController {
     private List<Intent> mPendingNewIntents;
     private List<ActivityResult> mPendingActivityResults;
     private boolean mWaitingForFirstDraw;
+    private boolean mHasDoneFirstDraw;
     private boolean mInitializationComplete;
 
     /**
@@ -110,7 +111,7 @@ class NativeInitializationController {
     }
 
     private void onLibraryLoaded() {
-        if (mActivityDelegate.hasDoneFirstDraw()) {
+        if (mHasDoneFirstDraw) {
             // First draw is done
             onNativeLibraryLoaded();
         } else {
@@ -123,6 +124,8 @@ class NativeInitializationController {
      * load has to be completed to start the chromium browser process.
      */
     public void firstDrawComplete() {
+        mHasDoneFirstDraw = true;
+
         if (mWaitingForFirstDraw) {
             mWaitingForFirstDraw = false;
             // Allow the UI thread to continue its initialization
