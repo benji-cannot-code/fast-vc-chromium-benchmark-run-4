14daf27b960eebe45eecd784c87df977079c8e2e
isheriff <isheriff@chromium.org>
2015-10-07 10:51:13 -0700

    cast: Fix lint error to include string cast: Fix sending adaptive target delay on RTP
    - Send target delay when we do have encryption turned on. Currently,
    it is being sent only in the case when we have a non-encrypted session.
    The result of this was that chromecast was not receiving any adaptive delay
    values while the local receiver app did.
    
    - Add extension details only on the first packet of the frame as per
    the spec. This issue was causing chromecast to crash in debug mode.
    
    - Added a unittest to ensure only first packet on frame has extensions.
    
    BUG: 405339
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1380143003
    
    Cr-Commit-Position: refs/heads/master@{#352885}

--- media/cast/net/rtp/rtp_packetizer.cc
+++ media/cast/net/rtp/rtp_packetizer.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/cast/net/rtp/rtp_packetizer.h"
 
+#include <string>
+
 #include "base/big_endian.h"
 #include "base/logging.h"
 #include "media/cast/net/pacing/paced_sender.h"
@@ -56,6 +58,12 @@ void RtpPacketizer::SendFrameAsPackets(const EncodedFrame& frame) {
 
   size_t remaining_size = frame.data.size();
   std::string::const_iterator data_iter = frame.data.begin();
+
+  uint8 num_extensions = 0;
+  if (frame.new_playout_delay_ms)
+    num_extensions++;
+  DCHECK_LE(num_extensions, kCastExtensionCountmask);
+
   while (remaining_size > 0) {
     PacketRef packet(new base::RefCountedData<Packet>);
 
@@ -69,14 +77,12 @@ void RtpPacketizer::SendFrameAsPackets(const EncodedFrame& frame) {
     // Build Cast header.
     // TODO(miu): Should we always set the ref frame bit and the ref_frame_id?
     DCHECK_NE(frame.dependency, EncodedFrame::UNKNOWN_DEPENDENCY);
-    uint8 num_extensions = 0;
-    if (frame.new_playout_delay_ms)
-      num_extensions++;
     uint8 byte0 = kCastReferenceFrameIdBitMask;
     if (frame.dependency == EncodedFrame::KEY)
       byte0 |= kCastKeyFrameBitMask;
-    DCHECK_LE(num_extensions, kCastExtensionCountmask);
-    byte0 |= num_extensions;
+    // Extensions only go on the first packet of the frame
+    if (packet_id_ == 0)
+      byte0 |= num_extensions;
     packet->data.push_back(byte0);
     packet->data.push_back(static_cast<uint8>(frame.frame_id));
     size_t start_size = packet->data.size();
@@ -86,7 +92,8 @@ void RtpPacketizer::SendFrameAsPackets(const EncodedFrame& frame) {
     big_endian_writer.WriteU16(packet_id_);
     big_endian_writer.WriteU16(static_cast<uint16>(num_packets - 1));
     packet->data.push_back(static_cast<uint8>(frame.referenced_frame_id));
-    if (frame.new_playout_delay_ms) {
+    // Add extension details only on the first packet of the frame
+    if (packet_id_ == 0 && frame.new_playout_delay_ms) {
       packet->data.push_back(kCastRtpExtensionAdaptiveLatency << 2);
       packet->data.push_back(2);  // 2 bytes
       packet->data.push_back(
