c28c57030fc29209428e467089dbe9908af83576
sdefresne <sdefresne@chromium.org>
2015-10-07 03:52:13 -0700

    Provides iOS implementation of some metrics services.
    Add a method GetNetworkTimeTracker() to ApplicationContext, implement it
    in ApplicationContextImpl and change BrowserProcess to delegate to this
    method instead.
    
    Provide an implementation of VariationsServiceClient on iOS now that all
    necessary method are available via IOSChromeVariationsServiceClient.
    
    Add support for generating UIStringOverrider on iOS based on the generated
    grit headers for //components and //ios strings.
    
    BUG=516659
    
    Review URL: https://codereview.chromium.org/1392593002
    
    Cr-Commit-Position: refs/heads/master@{#352813}

--- ios/chrome/browser/application_context.h
+++ ios/chrome/browser/application_context.h
@@ -26,6 +26,10 @@ namespace net_log {
 class ChromeNetLog;
 }
 
+namespace network_time {
+class NetworkTimeTracker;
+}
+
 namespace policy {
 class BrowserPolicyConnector;
 }
@@ -68,6 +72,9 @@ class ApplicationContext {
 
   // Gets the ChromeNetLog.
   virtual net_log::ChromeNetLog* GetNetLog() = 0;
+
+  // Gets the NetworkTimeTracker.
+  virtual network_time::NetworkTimeTracker* GetNetworkTimeTracker() = 0;
 
  protected:
   // Sets the global ApplicationContext instance.
