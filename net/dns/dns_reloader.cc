a79178c3f112eeaa99aa3b950b03b06a250ef582
tfarina <tfarina@chromium.org>
2015-10-07 16:08:35 -0700

    net: Move dns_reloader.* into dns directory.
    It is DNS specific so it makes more sense to have it in net/dns rather
    than in the generic net/base directory.
    
    BUG=None
    R=mmenke@chromium.org
    
    Review URL: https://codereview.chromium.org/1392943003
    
    Cr-Commit-Position: refs/heads/master@{#352967}

--- net/dns/dns_reloader.cc
+++ net/dns/dns_reloader.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "net/base/dns_reloader.h"
+#include "net/dns/dns_reloader.h"
 
 #if defined(OS_POSIX) && !defined(OS_MACOSX) && !defined(OS_OPENBSD) && \
     !defined(OS_ANDROID)
--- net/dns/dns_reloader.h
+++ net/dns/dns_reloader.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_BASE_DNS_RELOADER_H_
-#define NET_BASE_DNS_RELOADER_H_
+#ifndef NET_DNS_DNS_RELOADER_H_
+#define NET_DNS_DNS_RELOADER_H_
 
 #include "build/build_config.h"
 
@@ -21,4 +21,4 @@ void DnsReloaderMaybeReload();
 }  // namespace net
 #endif  // defined(OS_POSIX) && !defined(OS_MACOSX) && !defined(OS_OPENBSD)
 
-#endif  // NET_BASE_DNS_RELOADER_H_
+#endif  // NET_DNS_DNS_RELOADER_H_
