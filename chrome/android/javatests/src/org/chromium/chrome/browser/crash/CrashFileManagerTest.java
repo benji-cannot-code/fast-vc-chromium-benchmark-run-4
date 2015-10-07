eb16cfa707803cfd416bd38a75fe48bf0c16bf8f
mimee <mimee@chromium.org>
2015-10-07 13:39:19 -0700

    Fixed file comparator bug in CrashFileManager and added test.
    We want the file comparator to prioritize last modified file for sorting purposes, but previous
    file comparator logic had it backwards.
    
    In uploadLastCrash() we use this file comparator to sort all files and take the 0th one to upload. Previously, we always uploaded the 'first' crash instead of the last one.
    
    BUG=539507
    R=tedchoc@chromium.org,yfriedman@chromium.org,nyquist@chromium.org
    
    Review URL: https://codereview.chromium.org/1372393005
    
    Cr-Commit-Position: refs/heads/master@{#352935}

--- chrome/android/javatests/src/org/chromium/chrome/browser/crash/CrashFileManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/crash/CrashFileManagerTest.java
@@ -5,12 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.crash;
 
+import android.test.MoreAsserts;
+import android.test.suitebuilder.annotation.MediumTest;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.base.annotations.SuppressFBWarnings;
 import org.chromium.base.test.util.Feature;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 /**
@@ -21,6 +25,7 @@ public class CrashFileManagerTest extends CrashTestCase {
 
     private File mTmpFile1;
     private File mTmpFile2;
+    private File mTmpFile3;
 
     private File mDmpFile1;
     private File mDmpFile2;
@@ -39,6 +44,9 @@ public class CrashFileManagerTest extends CrashTestCase {
 
         mTmpFile2 = new File(mCrashDir, "abcde12345" + CrashFileManager.TMP_SUFFIX);
         mTmpFile2.createNewFile();
+
+        mTmpFile3 = new File(mCrashDir, "abcdefghi" + CrashFileManager.TMP_SUFFIX);
+        mTmpFile3.createNewFile();
 
         mDmpFile1 = new File(mCrashDir, "123_abc.dmp0");
         mDmpFile1.createNewFile();
@@ -73,25 +81,30 @@ public class CrashFileManagerTest extends CrashTestCase {
         Pattern testPattern = Pattern.compile("^123");
         File[] actualFiles = crashFileManager.getMatchingFiles(testPattern);
         assertNotNull(actualFiles);
-        assertEquals(expectedFiles.length, actualFiles.length);
-        for (int i = 0; i < expectedFiles.length; i++) {
-            // Test to see if files are properly ordered.
-            assertEquals(expectedFiles[i], actualFiles[i]);
-        }
+        MoreAsserts.assertEquals("Failed to match file by pattern", expectedFiles, actualFiles);
+    }
+
+    @MediumTest
+    @Feature({"Android-AppBase"})
+    public void testFileComparator() throws IOException {
+        CrashFileManager crashFileManager = new CrashFileManager(mCacheDir);
+        File[] expectedFiles = new File[] {mTmpFile3, mTmpFile2, mTmpFile1};
+        File[] originalFiles = new File[] {mTmpFile1, mTmpFile2, mTmpFile3};
+        Arrays.sort(originalFiles, crashFileManager.sFileComparator);
+        assertNotNull(originalFiles);
+        MoreAsserts.assertEquals("File comparator failed to prioritize last modified file",
+                expectedFiles, originalFiles);
     }
 
     @SmallTest
     @Feature({"Android-AppBase"})
     public void testGetAllMinidumpFilesSorted() {
         CrashFileManager crashFileManager = new CrashFileManager(mCacheDir);
-        File[] expectedFiles = new File[] {mDmpFile1, mDmpFile2};
+        File[] expectedFiles = new File[] {mDmpFile2, mDmpFile1};
         File[] actualFiles = crashFileManager.getAllMinidumpFilesSorted();
         assertNotNull(actualFiles);
-        assertEquals(expectedFiles.length, actualFiles.length);
-        for (int i = 0; i < expectedFiles.length; i++) {
-            // Test to see if files are properly ordered.
-            assertEquals(expectedFiles[i], actualFiles[i]);
-        }
+        MoreAsserts.assertEquals("Failed to sort minidumps by modification time", expectedFiles,
+                actualFiles);
     }
 
     @SmallTest
@@ -106,6 +119,7 @@ public class CrashFileManagerTest extends CrashTestCase {
     @SmallTest
     @Feature({"Android-AppBase"})
     public void testDeleteFile() {
+        assertTrue(mTmpFile1.exists());
         assertTrue(CrashFileManager.deleteFile(mTmpFile1));
         assertFalse(mTmpFile1.exists());
     }
@@ -117,11 +131,8 @@ public class CrashFileManagerTest extends CrashTestCase {
         File[] expectedFiles = new File[] { mDmpFile1, mDmpFile2 };
         File[] actualFiles = crashFileManager.getAllMinidumpFiles();
         assertNotNull(actualFiles);
-        assertEquals(expectedFiles.length, actualFiles.length);
-        for (int i = 0; i < expectedFiles.length; i++) {
-            // Test to see if files are properly ordered.
-            assertEquals(expectedFiles[i], actualFiles[i]);
-        }
+        MoreAsserts.assertEquals("Failed to get the correct minidump files in directory",
+                expectedFiles, actualFiles);
     }
 
     @SmallTest
@@ -131,11 +142,8 @@ public class CrashFileManagerTest extends CrashTestCase {
         File[] expectedFiles = new File[] { mUpFile1, mUpFile2 };
         File[] actualFiles = crashFileManager.getAllUploadedFiles();
         assertNotNull(actualFiles);
-        assertEquals(expectedFiles.length, actualFiles.length);
-        for (int i = 0; i < expectedFiles.length; i++) {
-            // Test to see if files are properly ordered.
-            assertEquals(expectedFiles[i], actualFiles[i]);
-        }
+        MoreAsserts.assertEquals("Failed to get the correct uploaded files in directory",
+                expectedFiles, actualFiles);
     }
 
     @SmallTest
@@ -166,7 +174,7 @@ public class CrashFileManagerTest extends CrashTestCase {
     @SmallTest
     @Feature({"Android-AppBase"})
     public void testCleanAllMiniDumps() {
-        assertEquals(6, mCrashDir.listFiles().length);
+        assertEquals(7, mCrashDir.listFiles().length);
 
         CrashFileManager crashFileManager = new CrashFileManager(mCacheDir);
         crashFileManager.cleanAllMiniDumps();
