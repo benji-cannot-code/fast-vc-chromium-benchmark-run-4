9f3b08e27adb8c15da0c2f3cdf2c07b8eb20aed9
kalyan.kondapally <kalyan.kondapally@intel.com>
2015-10-07 00:30:56 -0700

    Remove unused local variable
    We dont use resources for anything here, remove it.
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1391033002
    
    Cr-Commit-Position: refs/heads/master@{#352794}

--- cc/output/gl_renderer.cc
+++ cc/output/gl_renderer.cc
@@ -3509,7 +3509,6 @@ void GLRenderer::ScheduleOverlays(DrawingFrame* frame) {
   if (!frame->overlay_list.size())
     return;
 
-  ResourceProvider::ResourceIdArray resources;
   OverlayCandidateList& overlays = frame->overlay_list;
   for (const OverlayCandidate& overlay : overlays) {
     unsigned texture_id = 0;
