2c0be771fc830d51c115904f0fd71cca8180d1a4
pbos <pbos@chromium.org>
2015-10-07 07:27:19 -0700

    Roll WebRTC 10183:10197, Libjingle 10176:10196
    Also adds some #include "base/basictypes.h" to remoting/ (uses uint16).
    
    WebRTC 10183:10197
    
    Changes: https://chromium.googlesource.com/external/webrtc/trunk/webrtc.git/+log/e2c433c..dd1d405
    
    Libjingle 10176:10196
    
    Changes: https://chromium.googlesource.com/external/webrtc/trunk/talk.git/+log/bfcf08f..cb124ee
    
    BUG=webrtc:5024
    R=henrika@chromium.org
    TBR=sergeyu@chromium.org, mcasas@chromium.org
    
    Review URL: https://codereview.chromium.org/1392713003
    
    Cr-Commit-Position: refs/heads/master@{#352835}

--- remoting/protocol/chromium_socket_factory.h
+++ remoting/protocol/chromium_socket_factory.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_PROTOCOL_CHROMIUM_SOCKET_FACTORY_H_
 #define REMOTING_PROTOCOL_CHROMIUM_SOCKET_FACTORY_H_
 
+#include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "third_party/webrtc/p2p/base/packetsocketfactory.h"
