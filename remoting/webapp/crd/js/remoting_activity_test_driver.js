f6c3c7027a257938581fdab34493fe4f5f3a0893
kelvinp <kelvinp@chromium.org>
2015-10-07 14:19:51 -0700

    Improve reconnect behavior.
    Summary of changes:
    1. Dispose the desktop activity before reconnecting, otherwise
       the desktop activity will be set to null AFTER the
       re-connection.
    2. Drop the session after 30s of suspension as we cannot
       recover from the connection loss after 30s.
    3. Add an integration test for the reconnect behavior.
    4. Implements a NetworkConnectivityDetector that provides a
       reliability way of detecting network connectivity with
       unittest.
    5. Simplify the SmartReconnector to use NetworkConnectivityDetector.
    6. Add a dispose method on AutoRetryXhr.
    
    BUG=537812,536877
    
    Review URL: https://codereview.chromium.org/1382593007
    
    Cr-Commit-Position: refs/heads/master@{#352948}

--- remoting/webapp/crd/js/remoting_activity_test_driver.js
+++ remoting/webapp/crd/js/remoting_activity_test_driver.js
@@ -54,6 +54,21 @@ MockDesktopConnectedView.prototype.dispose = function() {};
 MockDesktopConnectedView.prototype.setRemapKeys = function() {};
 
 /**
+ * @constructor
+ * @extends {remoting.NetworkConnectivityDetector}
+ */
+var MockNetworkConnectivityDetector = function() {};
+/** @override */
+MockNetworkConnectivityDetector.prototype.waitForOnline = function() {
+  return Promise.resolve();
+};
+
+/** @override */
+MockNetworkConnectivityDetector.prototype.cancel = function() {};
+/** @override */
+MockNetworkConnectivityDetector.prototype.dispose = function() {};
+
+/**
  * A test driver that mocks out the UI components that are required by the
  * DesktopRemotingActivity.
  *
@@ -78,6 +93,12 @@ var BaseTestDriver = function(mockHTML) {
   this.eventWriterMock_ = sinon.mock(remoting.TelemetryEventWriter.Client);
   /** @private */
   this.setModeStub_ = sinon.stub(remoting, 'setMode');
+  /** @private */
+  this.createNetworkConnectivityDetectorStub_ =
+      sinon.stub(remoting.NetworkConnectivityDetector, 'create', function(){
+        return new MockNetworkConnectivityDetector();
+      });
+
   /**
    * Use fake timers to prevent the generation of session ID expiration events.
    * @private
@@ -104,7 +125,7 @@ BaseTestDriver.prototype.dispose = function() {
   this.setModeStub_.restore();
   this.eventWriterMock_.restore();
   this.desktopConnectedViewCreateStub_.restore();
-
+  this.createNetworkConnectivityDetectorStub_.restore();
   if (Boolean(this.mockConnection_)) {
     this.mockConnection_.restore();
     this.mockConnection_ = null;
@@ -204,6 +225,9 @@ remoting.Me2MeTestDriver.prototype.mockHostList = function() {
 /** @return {remoting.Me2MeActivity} */
 remoting.Me2MeTestDriver.prototype.me2meActivity = function() {
   return this.me2meActivity_;
+};
+
+remoting.Me2MeTestDriver.prototype.mockOnline = function() {
 };
 
 /** @return {Promise} */
