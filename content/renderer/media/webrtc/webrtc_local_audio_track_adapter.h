e391b9bef2ffbe1f785add3da3d35ef65c171595
solenberg <solenberg@chromium.org>
2015-10-08 08:42:38 -0700

    Remove unused implementations of AddChannel/RemoveChannel from WebRtcLocalAudioTrackAdapter. It no longer needs to implement cricket::AudioRenderer.
    BUG=webrtc:4690
    
    Review URL: https://codereview.chromium.org/1400563002
    
    Cr-Commit-Position: refs/heads/master@{#353062}

--- content/renderer/media/webrtc/webrtc_local_audio_track_adapter.h
+++ content/renderer/media/webrtc/webrtc_local_audio_track_adapter.h
@@ -33,8 +33,7 @@ class WebRtcAudioSinkAdapter;
 class WebRtcLocalAudioTrack;
 
 class CONTENT_EXPORT WebRtcLocalAudioTrackAdapter
-    : NON_EXPORTED_BASE(public cricket::AudioRenderer),
-      NON_EXPORTED_BASE(
+    : NON_EXPORTED_BASE(
           public webrtc::MediaStreamTrack<webrtc::AudioTrackInterface>) {
  public:
   static scoped_refptr<WebRtcLocalAudioTrackAdapter> Create(
@@ -49,8 +48,6 @@ class CONTENT_EXPORT WebRtcLocalAudioTrackAdapter
   ~WebRtcLocalAudioTrackAdapter() override;
 
   void Initialize(WebRtcLocalAudioTrack* owner);
-
-  std::vector<int> VoeChannels() const;
 
   // Called on the audio thread by the WebRtcLocalAudioTrack to set the signal
   // level of the audio data.
@@ -74,12 +71,6 @@ class CONTENT_EXPORT WebRtcLocalAudioTrackAdapter
   bool GetSignalLevel(int* level) override;
   rtc::scoped_refptr<webrtc::AudioProcessorInterface> GetAudioProcessor()
       override;
-
-  // cricket::AudioCapturer implementation.
-  void AddChannel(int channel_id) override;
-  void RemoveChannel(int channel_id) override;
-
-  // webrtc::AudioTrackInterface implementation.
   webrtc::AudioSourceInterface* GetSource() const override;
   cricket::AudioRenderer* GetRenderer() override;
 
