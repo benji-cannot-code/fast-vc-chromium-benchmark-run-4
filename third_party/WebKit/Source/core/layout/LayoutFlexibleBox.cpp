70f9632dd3a87cc0411c797a00a5edb333aa25e8
cbiesinger <cbiesinger@chromium.org>
2015-10-08 13:44:25 -0700

    [flexbox] Set height to tallest column, not last one.
    R=leviw@chromium.org
    BUG=525051
    
    Review URL: https://codereview.chromium.org/1394703002
    
    Cr-Commit-Position: refs/heads/master@{#353132}

--- third_party/WebKit/Source/core/layout/LayoutFlexibleBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutFlexibleBox.cpp
@@ -1234,7 +1234,7 @@ void LayoutFlexibleBox::layoutAndPlaceChildren(LayoutUnit& crossAxisOffset, cons
     }
 
     if (isColumnFlow())
-        setLogicalHeight(mainAxisOffset + flowAwareBorderEnd() + flowAwarePaddingEnd() + scrollbarLogicalHeight());
+        setLogicalHeight(std::max(logicalHeight(), mainAxisOffset + flowAwareBorderEnd() + flowAwarePaddingEnd() + scrollbarLogicalHeight()));
 
     if (style()->flexDirection() == FlowColumnReverse) {
         // We have to do an extra pass for column-reverse to reposition the flex items since the start depends
