b3452d8b0dfe8a908e99769706ecb98a7906f3a9
jchaffraix <jchaffraix@chromium.org>
2015-10-08 02:43:57 -0700

    Improve preferred logical widths documentation.
    The documentation was spread around so moved centralized it to
    LayoutObject and added some pointers to make it more consistent.
    
    Review URL: https://codereview.chromium.org/1379013002
    
    Cr-Commit-Position: refs/heads/master@{#353023}

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -957,6 +957,7 @@ LayoutUnit LayoutBox::minPreferredLogicalWidth() const
         SetLayoutNeededForbiddenScope layoutForbiddenScope(const_cast<LayoutBox&>(*this));
 #endif
         const_cast<LayoutBox*>(this)->computePreferredLogicalWidths();
+        ASSERT(!preferredLogicalWidthsDirty());
     }
 
     return m_minPreferredLogicalWidth;
@@ -969,6 +970,7 @@ LayoutUnit LayoutBox::maxPreferredLogicalWidth() const
         SetLayoutNeededForbiddenScope layoutForbiddenScope(const_cast<LayoutBox&>(*this));
 #endif
         const_cast<LayoutBox*>(this)->computePreferredLogicalWidths();
+        ASSERT(!preferredLogicalWidthsDirty());
     }
 
     return m_maxPreferredLogicalWidth;
