b6a6273d3ea881d4339c74ced58566108c1ef5bf
guoweis <guoweis@chromium.org>
2015-10-08 11:51:41 -0700

    Remove stun server in webrtc peerconnection.js
    Specifying the stun server which can't be accessed in the test environment disallows WebRTC to generate loopback candidates. The current logic for generating loopback candidates is that if the permission is denied, the RtcConfiguration has to be null or undefined.
    
    This prevents the turning on of IP Permission check as in https://codereview.chromium.org/1380323005/
    
    BUG=538815
    
    Review URL: https://codereview.chromium.org/1395553003
    
    Cr-Commit-Position: refs/heads/master@{#353113}

--- chrome/test/data/webrtc/peerconnection.js
+++ chrome/test/data/webrtc/peerconnection.js
@@ -6,12 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 /**
- * We need a STUN server for some API calls.
- * @private
- */
-var STUN_SERVER = 'stun.l.google.com:19302';
-
-/**
  * The one and only peer connection in this page.
  * @private
  */
@@ -41,7 +35,7 @@ function preparePeerConnection() {
   if (gPeerConnection != null)
     throw failTest('creating peer connection, but we already have one.');
 
-  gPeerConnection = createPeerConnection_(STUN_SERVER);
+  gPeerConnection = createPeerConnection_();
   returnToTest('ok-peerconnection-created');
 }
 
@@ -235,10 +229,9 @@ function hasSeenCryptoInSdp() {
 // Internals.
 
 /** @private */
-function createPeerConnection_(stun_server) {
-  servers = {iceServers: [{url: 'stun:' + stun_server}]};
+function createPeerConnection_() {
   try {
-    peerConnection = new RTCPeerConnection(servers, {});
+    peerConnection = new RTCPeerConnection(null, {});
   } catch (exception) {
     throw failTest('Failed to create peer connection: ' + exception);
   }
