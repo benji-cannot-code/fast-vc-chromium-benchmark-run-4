04ea092a2c72e2145b994149de15b48a5e28c74f
dewittj <dewittj@chromium.org>
2015-10-08 11:39:21 -0700

    Notifications: Fixes crash due to re-entrancy of MarkSinglePopupAsShown.
    Due to the interaction between notification blockers and MessageCenterObservers,
    it was possible to get a reentrancy condition in MessageCenterImpl that would
    try to dereference a NULL pointer.
    
    BUG=538321
    
    Review URL: https://codereview.chromium.org/1391493002
    
    Cr-Commit-Position: refs/heads/master@{#353101}

--- chrome/browser/notifications/notification_browsertest.cc
+++ chrome/browser/notifications/notification_browsertest.cc
@@ -46,7 +46,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/window_open_disposition.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/message_center_observer.h"
+#include "ui/message_center/message_center_style.h"
+#include "ui/message_center/notification_blocker.h"
 #include "url/gurl.h"
+
+namespace {
+
+class ToggledNotificationBlocker : public message_center::NotificationBlocker {
+ public:
+  ToggledNotificationBlocker()
+      : message_center::NotificationBlocker(
+            message_center::MessageCenter::Get()),
+        notifications_enabled_(true) {}
+  ~ToggledNotificationBlocker() override {}
+
+  void SetNotificationsEnabled(bool enabled) {
+    if (notifications_enabled_ != enabled) {
+      notifications_enabled_ = enabled;
+      NotifyBlockingStateChanged();
+    }
+  }
+
+  // NotificationBlocker overrides:
+  bool ShouldShowNotificationAsPopup(
+      const message_center::NotifierId& notifier_id) const override {
+    return notifications_enabled_;
+  }
+
+ private:
+  bool notifications_enabled_;
+
+  DISALLOW_COPY_AND_ASSIGN(ToggledNotificationBlocker);
+};
+
+}  // namespace
 
 namespace {
 
@@ -398,6 +431,24 @@ IN_PROC_BROWSER_TEST_F(NotificationsTest, TestCreateSimpleNotification) {
             (*notifications.rbegin())->title());
   EXPECT_EQ(base::ASCIIToUTF16("My Body"),
             (*notifications.rbegin())->message());
+}
+
+IN_PROC_BROWSER_TEST_F(NotificationsTest, NotificationBlockerTest) {
+  ToggledNotificationBlocker blocker;
+
+  ASSERT_TRUE(embedded_test_server()->InitializeAndWaitUntilReady());
+
+  // Creates a simple notification.
+  AllowAllOrigins();
+  ui_test_utils::NavigateToURL(browser(), GetTestPageURL());
+
+  std::string result = CreateSimpleNotification(browser(), true);
+  EXPECT_NE("-1", result);
+  result = CreateSimpleNotification(browser(), true);
+  EXPECT_NE("-1", result);
+
+  blocker.SetNotificationsEnabled(false);
+  EXPECT_EQ(0, GetNotificationPopupCount());
 }
 
 IN_PROC_BROWSER_TEST_F(NotificationsTest, TestCloseNotification) {
