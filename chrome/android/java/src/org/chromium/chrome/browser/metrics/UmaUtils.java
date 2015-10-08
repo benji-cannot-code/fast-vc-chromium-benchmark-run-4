83ecf3b1abaeff87610394ad0db87208d2acc46f
aurimas <aurimas@chromium.org>
2015-10-08 11:23:52 -0700

    Merge two different TabWebContentsObserver implementations
    BUG=532216
    
    Review URL: https://codereview.chromium.org/1388843002
    
    Cr-Commit-Position: refs/heads/master@{#353087}

--- chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaUtils.java
@@ -48,7 +48,7 @@ public class UmaUtils {
     }
 
     @CalledByNative
-    private static long getMainEntryPointTime() {
+    public static long getMainEntryPointTime() {
         return sApplicationStartWallClockMs;
     }
 
