8bcff2f4cd3bd0ffdae9a913b712c8828cf35593
yurys <yurys@chromium.org>
2015-10-08 15:15:16 -0700

    DevTools: change labels for 'exception' and 'return value'
    This CL makes the property names less cryptic:
    
    <exception> -> "Exception"
    <return> -> "Return Value"
    
    Also the names are displayed with italic font (see screenshots attached to the bug).
    
    BUG=155730
    
    Review URL: https://codereview.chromium.org/1397643002
    
    Cr-Commit-Position: refs/heads/master@{#353150}

--- third_party/WebKit/Source/devtools/front_end/sdk/RemoteObject.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/RemoteObject.js
@@ -1097,9 +1097,10 @@ WebInspector.ScopeRef = function(number, callFrameId, functionId)
  * @param {boolean=} writable
  * @param {boolean=} isOwn
  * @param {boolean=} wasThrown
+ * @param {boolean=} synthetic
  * @param {?WebInspector.RemoteObject=} symbol
  */
-WebInspector.RemoteObjectProperty = function(name, value, enumerable, writable, isOwn, wasThrown, symbol)
+WebInspector.RemoteObjectProperty = function(name, value, enumerable, writable, isOwn, wasThrown, symbol, synthetic)
 {
     this.name = name;
     if (value !== null)
@@ -1110,6 +1111,7 @@ WebInspector.RemoteObjectProperty = function(name, value, enumerable, writable, 
     this.wasThrown = !!wasThrown;
     if (symbol)
         this.symbol = symbol;
+    this.synthetic = !!synthetic;
 }
 
 WebInspector.RemoteObjectProperty.prototype = {
