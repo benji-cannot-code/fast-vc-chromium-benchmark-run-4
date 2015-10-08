c3581971aff973ea2abef6b78104901be640883a
thestig <thestig@chromium.org>
2015-10-08 00:39:40 -0700

    Move assorted function in application_lifetime to where they are used.
    BUG=159847
    
    Review URL: https://codereview.chromium.org/1388303002
    
    Cr-Commit-Position: refs/heads/master@{#353016}

--- chrome/browser/browser_shutdown.cc
+++ chrome/browser/browser_shutdown.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread.h"
 #include "base/time/time.h"
+#include "base/trace_event/trace_event.h"
 #include "chrome/browser/about_flags.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/common/switch_utils.h"
 #include "components/metrics/metrics_service.h"
+#include "components/tracing/tracing_switches.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_process_host.h"
 
@@ -113,7 +115,7 @@ void OnShutdownStarting(ShutdownType type) {
 #if !defined(OS_CHROMEOS)
   // Start the shutdown tracing. Note that On ChromeOS this has already been
   // called in AttemptUserExit().
-  chrome::StartShutdownTracing();
+  StartShutdownTracing();
 #endif
 
   g_shutdown_type = type;
@@ -219,7 +221,7 @@ void ShutdownPostThreadsStop(bool restart_last_session) {
 
 #if defined(OS_WIN)
   if (!browser_util::IsBrowserAlreadyRunning() &&
-      g_shutdown_type != browser_shutdown::END_SESSION) {
+      g_shutdown_type != END_SESSION) {
     upgrade_util::SwapNewChromeExeIfPresent();
   }
 #endif
@@ -345,6 +347,19 @@ void SetTryingToQuit(bool quitting) {
 
 bool IsTryingToQuit() {
   return g_trying_to_quit;
+}
+
+void StartShutdownTracing() {
+  const base::CommandLine& command_line =
+      *base::CommandLine::ForCurrentProcess();
+  if (command_line.HasSwitch(switches::kTraceShutdown)) {
+    base::trace_event::TraceConfig trace_config(
+        command_line.GetSwitchValueASCII(switches::kTraceShutdown), "");
+    base::trace_event::TraceLog::GetInstance()->SetEnabled(
+        trace_config,
+        base::trace_event::TraceLog::RECORDING_MODE);
+  }
+  TRACE_EVENT0("shutdown", "StartShutdownTracing");
 }
 
 }  // namespace browser_shutdown
--- chrome/browser/browser_shutdown.h
+++ chrome/browser/browser_shutdown.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_BROWSER_SHUTDOWN_H__
-#define CHROME_BROWSER_BROWSER_SHUTDOWN_H__
+#ifndef CHROME_BROWSER_BROWSER_SHUTDOWN_H_
+#define CHROME_BROWSER_BROWSER_SHUTDOWN_H_
 
 class PrefRegistrySimple;
 
@@ -69,6 +69,11 @@ void SetTryingToQuit(bool quitting);
 // General accessor.
 bool IsTryingToQuit();
 
+// Starts to collect shutdown traces. On ChromeOS this will start immediately
+// on AttemptUserExit() and all other systems will start once all tabs are
+// closed.
+void StartShutdownTracing();
+
 }  // namespace browser_shutdown
 
-#endif  // CHROME_BROWSER_BROWSER_SHUTDOWN_H__
+#endif  // CHROME_BROWSER_BROWSER_SHUTDOWN_H_
