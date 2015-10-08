8e9e02ce6767b286d243a68a3b6a5cb7095034ed
apacible <apacible@chromium.org>
2015-10-08 16:49:23 -0700

    Update Media Router route-details element style.
    This change brings the route-details element closer to spec to match the final mocks. This also fixes the missing bottom part of the button.
    
    Redlines for the custom controller and fallback route information (if no controls) still not finalized.
    
    BUG=501452
    
    Review URL: https://codereview.chromium.org/1395593002
    
    Cr-Commit-Position: refs/heads/master@{#353175}

--- chrome/browser/resources/media_router/elements/route_details/route_details.js
+++ chrome/browser/resources/media_router/elements/route_details/route_details.js
@@ -19,6 +19,16 @@ Polymer({
     },
 
     /**
+     * The name of the icon used as the back button. This is set once, when
+     * the |this| is ready.
+     * @private {string}
+     */
+    arrowIcon_: {
+      type: String,
+      value: '',
+    },
+
+    /**
      * The route to show.
      * @type {?media_router.Route}
      */
@@ -56,6 +66,12 @@ Polymer({
       type: Boolean,
       value: true,
     },
+  },
+
+  attached: function() {
+    // isRTL() only works after i18n_template.js runs to set <html dir>.
+    // Set the back button icon based on text direction.
+    this.arrowIcon_ = isRTL() ? 'arrow-forward' : 'arrow-back';
   },
 
   /**
