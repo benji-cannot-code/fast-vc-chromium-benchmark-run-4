4ee8a16b8242c64f6cce67515e414cbb12c4a653
estade <estade@chromium.org>
2015-10-08 14:25:57 -0700

    Fix explicit search engine adding (window.external.AddSearchProvider)
    This was broken by 230a6cc1034e196 --- we actually need both
    OnMessageReceived functions because the AddSearchProvider IPC is still
    the RenderView-specific version (only <link> was updated to the
    RenderFrame-specific IPC).
    
    BUG=540854
    
    Review URL: https://codereview.chromium.org/1396903002
    
    Cr-Commit-Position: refs/heads/master@{#353142}

--- chrome/browser/ui/search_engines/search_engine_tab_helper.h
+++ chrome/browser/ui/search_engines/search_engine_tab_helper.h
@@ -31,6 +31,7 @@ class SearchEngineTabHelper
   void DidNavigateMainFrame(
       const content::LoadCommittedDetails& details,
       const content::FrameNavigateParams& params) override;
+  bool OnMessageReceived(const IPC::Message& message) override;
   bool OnMessageReceived(const IPC::Message& message,
                          content::RenderFrameHost* rfh) override;
 
