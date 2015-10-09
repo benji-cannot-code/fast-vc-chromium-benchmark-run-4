9152364d60db8dc26720242b07a42e40623dda20
sigbjornf <sigbjornf@opera.com>
2015-10-09 06:38:13 -0700

    Oilpan: fix build after r353269.
    TBR=oilpan-reviews
    BUG=539883
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1398103002
    
    Cr-Commit-Position: refs/heads/master@{#353281}

--- third_party/WebKit/Source/modules/presentation/Presentation.h
+++ third_party/WebKit/Source/modules/presentation/Presentation.h
@@ -18,6 +18,8 @@ class PresentationRequest;
 
 // Implements the main entry point of the Presentation API corresponding to the Presentation.idl
 // See https://w3c.github.io/presentation-api/#navigatorpresentation for details.
+//
+// TODO(Oilpan): switch to GarbageCollected<Presentation> once object is unconditionally on the Oilpan heap.
 class Presentation final
     : public GarbageCollectedFinalized<Presentation>
     , public ScriptWrappable
@@ -26,7 +28,9 @@ class Presentation final
     DEFINE_WRAPPERTYPEINFO();
 public:
     static Presentation* create(LocalFrame*);
+#if !ENABLE(OILPAN)
     ~Presentation() override = default;
+#endif
 
     DECLARE_VIRTUAL_TRACE();
 
