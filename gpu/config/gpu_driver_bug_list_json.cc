d0a5d9ff99ad05c203b7ef1ad81ca2359cf6f23d
hendrikw <hendrikw@chromium.org>
2015-10-08 17:27:40 -0700

    gpu: Enable virtual contexts with nvidia drivers only
    virtual contexts were enabled to combat slow MakeCurrent calls with
    NVIDIA drivers, and possibly others (others were not tested).
    
    Unfortunately, this seems to have caused some problems with the
    Intel opensource MESA drivers.
    
    As a result, I'm disabling it for everything but NVIDIA.
    
    I'm trying to get a system set up where we can hopefully repro these
    issues on linux, maybe we'll be able to fix it, or we can contact the
    driver devs.
    
    BUG=446530
    BUG=514510
    
    Review URL: https://codereview.chromium.org/1399653002
    
    Cr-Commit-Position: refs/heads/master@{#353184}

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -20,7 +20,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "8.26",
+  "version": "8.27",
   "entries": [
     {
       "id": 1,
@@ -1271,10 +1271,11 @@ LONG_STRING_CONST(
     },
     {
       "id": 109,
-      "description": "MakeCurrent is slow on Linux",
+      "description": "MakeCurrent is slow on Linux with NVIDIA drivers",
       "os": {
         "type": "linux"
       },
+      "gl_vendor": "NVIDIA.*",
       "features": [
         "use_virtualized_gl_contexts"
       ]
