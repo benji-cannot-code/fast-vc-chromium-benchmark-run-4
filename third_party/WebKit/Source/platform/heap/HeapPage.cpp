9b207680dee0713049972d7ec205c736761649df
sigbjornf <sigbjornf@opera.com>
2015-10-09 10:39:36 -0700

    Oilpan: always shrink tail-allocated backing storage.
    When shrinking backing storage allocations, some care is taken not to
    introduce excessive fragmentation and/or limit later expansion of the
    same object. That logic does not need to apply if the backing store is
    at the tail of the current heap chunk: unconditionally reset the
    allocation point, if it is.
    
    R=haraken
    BUG=420515
    
    Review URL: https://codereview.chromium.org/1393863004
    
    Cr-Commit-Position: refs/heads/master@{#353321}

--- third_party/WebKit/Source/platform/heap/HeapPage.cpp
+++ third_party/WebKit/Source/platform/heap/HeapPage.cpp
@@ -700,7 +700,7 @@ bool NormalPageHeap::expandObject(HeapObjectHeader* header, size_t newSize)
     size_t allocationSize = Heap::allocationSizeFromSize(newSize);
     ASSERT(allocationSize > header->size());
     size_t expandSize = allocationSize - header->size();
-    if (header->payloadEnd() == m_currentAllocationPoint && expandSize <= m_remainingAllocationSize) {
+    if (isObjectAllocatedAtAllocationPoint(header) && expandSize <= m_remainingAllocationSize) {
         m_currentAllocationPoint += expandSize;
         m_remainingAllocationSize -= expandSize;
 
@@ -720,7 +720,7 @@ bool NormalPageHeap::shrinkObject(HeapObjectHeader* header, size_t newSize)
     size_t allocationSize = Heap::allocationSizeFromSize(newSize);
     ASSERT(header->size() > allocationSize);
     size_t shrinkSize = header->size() - allocationSize;
-    if (header->payloadEnd() == m_currentAllocationPoint) {
+    if (isObjectAllocatedAtAllocationPoint(header)) {
         m_currentAllocationPoint -= shrinkSize;
         m_remainingAllocationSize += shrinkSize;
         SET_MEMORY_INACCESSIBLE(m_currentAllocationPoint, shrinkSize);
