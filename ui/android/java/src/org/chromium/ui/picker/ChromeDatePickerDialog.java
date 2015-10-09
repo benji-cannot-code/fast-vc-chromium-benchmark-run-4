d7d82ab74c2b3f34ff8547b6106d1c7953bd7689
newt <newt@chromium.org>
2015-10-09 11:53:55 -0700

    Remove title from date picker dialog.
    On Android L+, the date picker dialog uses a design that looks bad with
    a title at the top. By default, the DatePickerDialog on L+ doesn't show
    a title. However, Chrome triggers a bug in DatePickerDialog (by calling
    updateDate() before the dialog has been shown), which causes the title to
    appear. This CL works around that bug.
    
    BUG=541350
    
    Review URL: https://codereview.chromium.org/1395913003
    
    Cr-Commit-Position: refs/heads/master@{#353343}

--- ui/android/java/src/org/chromium/ui/picker/ChromeDatePickerDialog.java
+++ ui/android/java/src/org/chromium/ui/picker/ChromeDatePickerDialog.java
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.ui.picker;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.widget.DatePicker;
 
 /**
@@ -15,7 +17,7 @@ import android.widget.DatePicker;
  * outside). This class will call the listener instead of the DatePickerDialog only when the
  * BUTTON_POSITIVE has been clicked.
  */
-class ChromeDatePickerDialog extends android.app.DatePickerDialog {
+class ChromeDatePickerDialog extends DatePickerDialog {
     private final OnDateSetListener mCallBack;
 
     public ChromeDatePickerDialog(Context context,
@@ -23,7 +25,8 @@ class ChromeDatePickerDialog extends android.app.DatePickerDialog {
             int year,
             int monthOfYear,
             int dayOfMonth) {
-        super(context, 0, callBack, year, monthOfYear, dayOfMonth);
+        super(context, callBack, year, monthOfYear, dayOfMonth);
+
         mCallBack = callBack;
     }
 
@@ -39,5 +42,16 @@ class ChromeDatePickerDialog extends android.app.DatePickerDialog {
             mCallBack.onDateSet(datePicker, datePicker.getYear(),
                     datePicker.getMonth(), datePicker.getDayOfMonth());
         }
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        // On Android L+, the dialog shouldn't have a title. This works around a bug in
+        // DatePickerDialog where calling updateDate() before the dialog has been shown causes
+        // a title to appear. http://crbug.com/541350
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            title = "";
+        }
+        super.setTitle(title);
     }
 }
