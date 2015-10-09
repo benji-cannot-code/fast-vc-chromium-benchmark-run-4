e97a6199550aca3ef536d8c1d602a216be0ebe5a
pkasting <pkasting@chromium.org>
2015-10-09 16:21:51 -0700

    Win 10 adjustments to DWM/client inset code.
    * Make the code in GetClientAreaInsets() match that in GetDWMFrameMargins().
    
    The apparent effect of this is that the resize border on the left, right, and
    bottom of the window gets 3 px bigger on Win 10.  (Or, more accurately, the
    visible window size shrinks by 3 px on each of those sides, and the space that
    was used for drawing is now part of the invisible resize border.)
    
    This seems to match native apps more accurately.
    
    * Eliminate the 3 px DWM offset down into the toolbar on Win 10.
    
    This isn't needed because the toolbar doesn't have rounded corners in Win 10.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1403443002
    
    Cr-Commit-Position: refs/heads/master@{#353421}

--- chrome/browser/ui/views/frame/browser_desktop_window_tree_host_win.cc
+++ chrome/browser/ui/views/frame/browser_desktop_window_tree_host_win.cc
@@ -29,9 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const int kClientEdgeThickness = 3;
-// We need to offset the DWMFrame into the toolbar so that the blackness
-// doesn't show up on our rounded corners.
-const int kDWMFrameTopOffset = 3;
 
 // DesktopThemeProvider maps resource ids using MapThemeImage(). This is
 // necessary for BrowserDesktopWindowTreeHostWin so that it uses the windows
@@ -150,7 +147,7 @@ bool BrowserDesktopWindowTreeHostWin::GetClientAreaInsets(
   // client edge over part of the default frame.
   if (GetWidget()->IsFullscreen())
     border_thickness = 0;
-  else if (!IsMaximized())
+  else if (!IsMaximized() && base::win::GetVersion() < base::win::VERSION_WIN10)
     border_thickness -= kClientEdgeThickness;
   insets->Set(0, border_thickness, border_thickness, border_thickness);
   return true;
@@ -325,7 +322,14 @@ MARGINS BrowserDesktopWindowTreeHostWin::GetDWMFrameMargins() const {
       gfx::Rect tabstrip_bounds(
           browser_frame_->GetBoundsForTabStrip(browser_view_->tabstrip()));
       tabstrip_bounds = gfx::win::DIPToScreenRect(tabstrip_bounds);
-      margins.cyTopHeight = tabstrip_bounds.bottom() + kDWMFrameTopOffset;
+      margins.cyTopHeight = tabstrip_bounds.bottom();
+
+      // On pre-Win 10, we need to offset the DWM frame into the toolbar so that
+      // the blackness doesn't show up on our rounded toolbar corners.  In Win
+      // 10 and above there are no rounded corners, so this is unnecessary.
+      const int kDWMFrameTopOffset = 3;
+      if (base::win::GetVersion() < base::win::VERSION_WIN10)
+        margins.cyTopHeight += kDWMFrameTopOffset;
     }
   }
   return margins;
