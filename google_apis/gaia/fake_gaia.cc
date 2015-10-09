8ae7aae6fdcd8a60d2e5a72809e28be7d005b203
dzhioev <dzhioev@chromium.org>
2015-10-09 01:09:02 -0700

    Do not rely on a continue URL when the new GAIA flow is used.
    When I tried to disable the GAIA extension in Chrome OS, I noticed that this
    makes several SAML tests to fail. That happens because:
    a) FakeGAIA is configured to redirect to
       chrome-extension://<gaia-extension-hash>/success.html on SAML authentication
       success.
    b) The logic that reacts on "success.html" redirect is still enabled in
       authenticator.js.
    
    As it doesn't matter for the new flow which continue URL is used (in fact we
    don't even pass "continue" to GAIA anymore), I disabled this logic for the new
    flow.
    
    TBR=bartfab@chromium.org
    BUG=470893
    TEST=existing tests
    
    Review URL: https://codereview.chromium.org/1361423005
    
    Cr-Commit-Position: refs/heads/master@{#353248}

--- google_apis/gaia/fake_gaia.cc
+++ google_apis/gaia/fake_gaia.cc
@@ -67,6 +67,8 @@ const char kAuthHeaderOAuth[] = "OAuth ";
 const char kListAccountsResponseFormat[] =
     "[\"gaia.l.a.r\",[[\"gaia.l.a\",1,\"\",\"%s\",\"\",1,1,0,0,1,\"12345\"]]]";
 
+const char kDummySAMLContinuePath[] = "DummySAMLContinue";
+
 typedef std::map<std::string, std::string> CookieMap;
 
 // Parses cookie name-value map our of |request|.
@@ -263,6 +265,10 @@ void FakeGaia::Initialize() {
 
   // Handles /SSO GAIA call (not GAIA, made up for SAML tests).
   REGISTER_PATH_RESPONSE_HANDLER("/SSO", HandleSSO);
+
+  REGISTER_RESPONSE_HANDLER(
+      gaia_urls->gaia_url().Resolve(kDummySAMLContinuePath),
+      HandleDummySAMLContinue);
 
   // Handles /oauth2/v4/token GAIA call.
   REGISTER_RESPONSE_HANDLER(
@@ -581,9 +587,11 @@ void FakeGaia::HandleEmbeddedLookupAccountLookup(
 
   GURL url(saml_account_idp_map_[email]);
   url = net::AppendQueryParameter(url, "SAMLRequest", "fake_request");
-  url = net::AppendQueryParameter(
-      url, "RelayState",
-      GaiaUrls::GetInstance()->signin_completed_continue_url().spec());
+  url = net::AppendQueryParameter(url, "RelayState",
+                                  GaiaUrls::GetInstance()
+                                      ->gaia_url()
+                                      .Resolve(kDummySAMLContinuePath)
+                                      .spec());
   std::string redirect_url = url.spec();
   http_response->AddCustomHeader("Google-Accounts-SAML", "Start");
 
@@ -627,6 +635,12 @@ void FakeGaia::HandleSSO(const HttpRequest& request,
 
   if (issue_oauth_code_cookie_)
     SetOAuthCodeCookie(http_response);
+}
+
+void FakeGaia::HandleDummySAMLContinue(const HttpRequest& request,
+                                       BasicHttpResponse* http_response) {
+  http_response->set_content("");
+  http_response->set_code(net::HTTP_OK);
 }
 
 void FakeGaia::HandleAuthToken(const HttpRequest& request,
