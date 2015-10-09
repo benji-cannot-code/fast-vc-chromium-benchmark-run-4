d469c4dfc34f3eb8b79da65375cc8c07d6c5a6cb
dongie.agnir <dongie.agnir@gmail.com>
2015-10-09 00:19:28 -0700

    input[type=color]: Update view if value attr set and dirty flag is false.
    The value attribute serves as the default value of the input element if
    its dirty flag is false. If the flag is false, the view should be
    updated when the attribute is changed.
    
    BUG=522815
    
    Review URL: https://codereview.chromium.org/1397693002
    
    Cr-Commit-Position: refs/heads/master@{#353245}

--- third_party/WebKit/Source/core/html/forms/ColorInputType.h
+++ third_party/WebKit/Source/core/html/forms/ColorInputType.h
@@ -58,6 +58,7 @@ public:
 
 private:
     ColorInputType(HTMLInputElement& element) : BaseClickableWithKeyInputType(element) { }
+    void valueAttributeChanged() override;
     void countUsage() override;
     const AtomicString& formControlType() const override;
     bool supportsRequired() const override;
