af2aeb3b6282a3f9dca55f1265cd0a0d22e54bd1
mathp <mathp@chromium.org>
2015-10-09 15:12:30 -0700

    [Autofill] Log the accepted suggestion index for autocomplete suggestions
    BUG=537813, 452240
    TEST=AutofillExternalDelegateTest
    
    Review URL: https://codereview.chromium.org/1390833002
    
    Cr-Commit-Position: refs/heads/master@{#353398}

--- components/autofill/core/browser/autofill_metrics.cc
+++ components/autofill/core/browser/autofill_metrics.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/autofill_metrics.h"
 
+#include <algorithm>
+
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/sparse_histogram.h"
@@ -608,8 +610,17 @@ void AutofillMetrics::LogAddressSuggestionsCount(size_t num_suggestions) {
 }
 
 // static
-void AutofillMetrics::LogSuggestionAcceptedIndex(int index) {
-  UMA_HISTOGRAM_SPARSE_SLOWLY("Autofill.SuggestionAcceptedIndex", index);
+void AutofillMetrics::LogAutofillSuggestionAcceptedIndex(int index) {
+  // A maximum of 50 is enforced to minimize the number of buckets generated.
+  UMA_HISTOGRAM_SPARSE_SLOWLY("Autofill.SuggestionAcceptedIndex",
+                              std::min(index, 50));
+}
+
+// static
+void AutofillMetrics::LogAutocompleteSuggestionAcceptedIndex(int index) {
+  // A maximum of 50 is enforced to minimize the number of buckets generated.
+  UMA_HISTOGRAM_SPARSE_SLOWLY("Autofill.SuggestionAcceptedIndex.Autocomplete",
+                              std::min(index, 50));
 }
 
 // static
