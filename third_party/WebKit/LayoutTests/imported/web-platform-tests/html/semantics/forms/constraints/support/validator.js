daafb1d393127243e961d3a61c9b1e248c55c5cf
kojii <kojii@chromium.org>
2015-10-08 21:14:38 -0700

    update-w3c-deps import using blink d9b074fe31794b3500d8e7ccf4d9ab7b15a7e86d:
    imported csswg-test@dc546342df5a876c3efd193edd3b16a65f538dbc
    imported web-platform-tests@5601d854cd195beba10d48af2e835e798d65f85b
    
    css-writing-mode-3
    - Some new and renaming in upstream.
    
    html/browsers
    - Great number of new tests from a commit:
      "Add whole load of tests for "Loading Web Pages""
      https://github.com/w3c/web-platform-tests/commit/dfe897f0bacf6c6ce339357698bd1b03c8f1ed84
      many of them require window.open() and thus skipped.
    
    TBR=dpranke@chromium.org,jsbell@chromium.org,tkent@chromium.org,hayato@chromium.org,kochi@chromium.org
    BUG=490511, 492664, 490940
    
    Review URL: https://codereview.chromium.org/1394423002
    
    Cr-Commit-Position: refs/heads/master@{#353227}

--- third_party/WebKit/LayoutTests/imported/web-platform-tests/html/semantics/forms/constraints/support/validator.js
+++ third_party/WebKit/LayoutTests/imported/web-platform-tests/html/semantics/forms/constraints/support/validator.js
@@ -280,7 +280,7 @@ var validator = {
       ctl.removeAttribute(item);
     });
     for (var attr in obj) {
-      if (obj[attr] || obj[attr] === "")
+      if (attr === "checked" || obj[attr] || obj[attr] === "")
         ctl[attr] = obj[attr];
     }
   },
