91b9a20b50a2e8889137d2b4504e07da27521d1a
erikchen <erikchen@chromium.org>
2015-10-09 18:10:24 -0700

    ipc: Add missing includes for USE_ATTACHMENT_BROKER.
    The preprocessor variable is defined in ipc/attachment_broker.h. There were
    three files that were using the variable without including the header.
    
    BUG=535711
    
    Review URL: https://codereview.chromium.org/1392713004
    
    Cr-Commit-Position: refs/heads/master@{#353448}

--- content/common/child_process_host_impl.cc
+++ content/common/child_process_host_impl.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/child_process_host_delegate.h"
 #include "content/public/common/content_paths.h"
 #include "content/public/common/content_switches.h"
+#include "ipc/attachment_broker.h"
 #include "ipc/ipc_channel.h"
 #include "ipc/ipc_logging.h"
 #include "ipc/message_filter.h"
--- ipc/ipc_message_unittest.cc
+++ ipc/ipc_message_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
+#include "ipc/attachment_broker.h"
 #include "ipc/ipc_message_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -149,7 +150,7 @@ TEST(IPCMessageTest, FindNext) {
   // (but contains the message header)
   IPC::Message::FindNext(data_start, data_end - 1, &next);
   EXPECT_FALSE(next.message_found);
-#if USE_ATTACHMENT_BROKER
+#if USE_ATTACHMENT_BROKER && defined(OS_MACOSX) && !defined(OS_IOS)
   EXPECT_EQ(next.message_size, 0u);
 #else
   EXPECT_EQ(next.message_size, message.size());
@@ -185,7 +186,7 @@ TEST(IPCMessageTest, FindNextOverflow) {
   message.header()->payload_size = static_cast<uint32_t>(-1);
   IPC::Message::FindNext(data_start, data_end, &next);
   EXPECT_FALSE(next.message_found);
-#if USE_ATTACHMENT_BROKER
+#if USE_ATTACHMENT_BROKER && defined(OS_MACOSX) && !defined(OS_IOS)
   EXPECT_EQ(next.message_size, 0u);
 #else
   if (sizeof(size_t) > sizeof(uint32_t)) {
@@ -203,7 +204,7 @@ TEST(IPCMessageTest, FindNextOverflow) {
   message.header()->payload_size = std::numeric_limits<int32_t>::max();
   IPC::Message::FindNext(data_start, data_end, &next);
   EXPECT_FALSE(next.message_found);
-#if USE_ATTACHMENT_BROKER
+#if USE_ATTACHMENT_BROKER && defined(OS_MACOSX) && !defined(OS_IOS)
   EXPECT_EQ(next.message_size, 0u);
 #else
   EXPECT_EQ(next.message_size,
