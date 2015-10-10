74f8a046e075252e7b07fb4e1e0267f16fc745df
pkasting <pkasting@chromium.org>
2015-10-09 17:39:48 -0700

    Remove unnecessary #includes of "windowsx.h" from various files.
    These still compile without this.  I ran into this when I discovered that
    windowsx.h #defines symbols like IsMaximized, which causes problems for various
    UI code and made tab_strip.cc a bit of an unnecessary time bomb, which I set off
    locally with a seemingly-innocuous #include addition to it.
    
    BUG=none
    TEST=none
    TBR=sky
    
    Review URL: https://codereview.chromium.org/1396213002
    
    Cr-Commit-Position: refs/heads/master@{#353436}

--- ui/views/controls/menu/menu_message_loop_aura.cc
+++ ui/views/controls/menu/menu_message_loop_aura.cc
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views/controls/menu/menu_message_loop_aura.h"
 
-#if defined(OS_WIN)
-#include <windowsx.h>
-#endif
-
 #include "base/run_loop.h"
 #include "ui/aura/client/screen_position_client.h"
 #include "ui/aura/window.h"
