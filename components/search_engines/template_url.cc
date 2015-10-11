88859ce4ead0a10c112580f740b750de42a90f5a
tfarina <tfarina@chromium.org>
2015-10-11 13:19:29 -0700

    Fix review nits that were missed from https://crrev.com/d7241e515462c10b29a0470c3c3df647ae34be4b.
    * net_util.h include is not necessary.
    * || around text.
    
    BUG=488531
    R=pkasting@chromium.org
    
    Review URL: https://codereview.chromium.org/1394183005
    
    Cr-Commit-Position: refs/heads/master@{#353476}

--- components/search_engines/template_url.cc
+++ components/search_engines/template_url.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/google_api_keys.h"
 #include "net/base/escape.h"
 #include "net/base/mime_util.h"
-#include "net/base/net_util.h"
 #include "ui/base/device_form_factor.h"
 #include "url/gurl.h"
 
