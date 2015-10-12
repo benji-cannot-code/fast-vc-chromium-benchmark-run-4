bb78ecbe30d356f0b193369c5e85622208f70c3c
maybelle <maybelle@chromium.org>
2015-10-12 03:11:58 -0700

    Download fallback site list in case the country specific list doesn't exist.
    Fixed issue with FileDownloader where downloading a second file
    after the first one fails would fail due to async cleanup of
    the first file.
    
    BUG=528188
    
    Review URL: https://codereview.chromium.org/1398453003
    
    Cr-Commit-Position: refs/heads/master@{#353503}

--- chrome/browser/net/file_downloader.h
+++ chrome/browser/net/file_downloader.h
@@ -7,13 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_NET_FILE_DOWNLOADER_H_
 
 #include "base/callback.h"
+#include "base/files/file_path.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "net/url_request/url_fetcher_delegate.h"
-
-namespace base {
-class FilePath;
-}  // namespace base
 
 namespace net {
 class URLFetcher;
@@ -45,9 +42,13 @@ class FileDownloader : public net::URLFetcherDelegate {
 
   void OnFileExistsCheckDone(bool exists);
 
+  void OnFileMoveDone(bool success);
+
   DownloadFinishedCallback callback_;
 
   scoped_ptr<net::URLFetcher> fetcher_;
+
+  base::FilePath local_path_;
 
   base::WeakPtrFactory<FileDownloader> weak_ptr_factory_;
 
