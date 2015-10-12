01d2dd28058237e3288620fc25d31d078229a82b
fhorschig <fhorschig@chromium.org>
2015-10-12 02:13:24 -0700

    Update 'Enterprise Override' string description.
    Quote from translation team:
    'Source and description seem to contradict. Source mentions "override"
    while description says "can not be overridden".'
    
    TBR=bartfab
    
    Review URL: https://codereview.chromium.org/1378283002
    
    Cr-Commit-Position: refs/heads/master@{#353494}

--- chrome/browser/chromeos/policy/device_local_account_policy_provider.cc
+++ chrome/browser/chromeos/policy/device_local_account_policy_provider.cc
@@ -61,7 +61,7 @@ DeviceLocalAccountPolicyProvider::Create(
         key::kLidCloseAction,
         POLICY_LEVEL_MANDATORY,
         POLICY_SCOPE_MACHINE,
-        POLICY_SOURCE_ENTERPRISE_OVERRIDE,
+        POLICY_SOURCE_PUBLIC_SESSION_OVERRIDE,
         new base::FundamentalValue(
             chromeos::PowerPolicyController::ACTION_STOP_SESSION),
         NULL);
@@ -71,7 +71,7 @@ DeviceLocalAccountPolicyProvider::Create(
         key::kShelfAutoHideBehavior,
         POLICY_LEVEL_MANDATORY,
         POLICY_SCOPE_MACHINE,
-        POLICY_SOURCE_ENTERPRISE_OVERRIDE,
+        POLICY_SOURCE_PUBLIC_SESSION_OVERRIDE,
         new base::StringValue("Never"),
         NULL);
     // Force the |ShowLogoutButtonInTray| policy to |true|, ensuring that a big,
@@ -80,7 +80,7 @@ DeviceLocalAccountPolicyProvider::Create(
         key::kShowLogoutButtonInTray,
         POLICY_LEVEL_MANDATORY,
         POLICY_SCOPE_MACHINE,
-        POLICY_SOURCE_ENTERPRISE_OVERRIDE,
+        POLICY_SOURCE_PUBLIC_SESSION_OVERRIDE,
         new base::FundamentalValue(true),
         NULL);
     // Force the |FullscreenAllowed| policy to |false|, ensuring that the ash
@@ -89,7 +89,7 @@ DeviceLocalAccountPolicyProvider::Create(
         key::kFullscreenAllowed,
         POLICY_LEVEL_MANDATORY,
         POLICY_SCOPE_MACHINE,
-        POLICY_SOURCE_ENTERPRISE_OVERRIDE,
+        POLICY_SOURCE_PUBLIC_SESSION_OVERRIDE,
         new base::FundamentalValue(false),
         NULL);
   }
