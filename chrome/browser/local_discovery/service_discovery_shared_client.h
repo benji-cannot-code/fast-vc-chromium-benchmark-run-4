1905a9c015bfd9c8ded232977417c38ebe86d26c
treib <treib@chromium.org>
2015-10-12 02:04:09 -0700

    Revert of Always delete ServiceDiscoverySharedClient on UI thread. (patchset #4 id:60001 of https://codereview.chromium.org/1386623005/ )
    Reason for revert:
    Seems to have broken GcdPrivateAPITest.AddAfter and AddRemove on Win7 dbg (tests still pass, but leak memory):
    https://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%28dbg%29%281%29/builds/42455
    
    Original issue's description:
    > Always delete ServiceDiscoverySharedClient on UI thread.
    >
    > BUG=444884
    >
    > Committed: https://crrev.com/d24e8e858ca0d5c4224b5cef8832d691b50d6ad4
    > Cr-Commit-Position: refs/heads/master@{#353486}
    
    TBR=alekseys@chromium.org,vitalybuka@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=444884
    
    Review URL: https://codereview.chromium.org/1385393004
    
    Cr-Commit-Position: refs/heads/master@{#353493}

--- chrome/browser/local_discovery/service_discovery_shared_client.h
+++ chrome/browser/local_discovery/service_discovery_shared_client.h
@@ -6,14 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_LOCAL_DISCOVERY_SERVICE_DISCOVERY_SHARED_CLIENT_H_
 #define CHROME_BROWSER_LOCAL_DISCOVERY_SERVICE_DISCOVERY_SHARED_CLIENT_H_
 
-#include "base/memory/ref_counted_delete_on_message_loop.h"
-
 #include "chrome/common/local_discovery/service_discovery_client.h"
 
 namespace local_discovery {
 
 class ServiceDiscoverySharedClient
-    : public base::RefCountedDeleteOnMessageLoop<ServiceDiscoverySharedClient>,
+    : public base::RefCounted<ServiceDiscoverySharedClient>,
       public ServiceDiscoveryClient {
  public:
   static scoped_refptr<ServiceDiscoverySharedClient> GetInstance();
@@ -27,9 +25,7 @@ class ServiceDiscoverySharedClient
   ~ServiceDiscoverySharedClient() override;
 
  private:
-  friend class base::RefCountedDeleteOnMessageLoop<
-      ServiceDiscoverySharedClient>;
-  friend class base::DeleteHelper<ServiceDiscoverySharedClient>;
+  friend class base::RefCounted<ServiceDiscoverySharedClient>;
 
   DISALLOW_COPY_AND_ASSIGN(ServiceDiscoverySharedClient);
 };
