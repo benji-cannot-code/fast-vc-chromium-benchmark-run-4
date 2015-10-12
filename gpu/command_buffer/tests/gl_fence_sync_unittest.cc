6e695129d8e8eb4bdef0a5f88dd4f6555d9b0356
dyen <dyen@chromium.org>
2015-10-12 11:07:09 -0700

    Reland: Added SyncToken command buffer trait to help with IPC messages
    Revert "Revert of Added SyncToken command buffer trait to help with IPC messages. (patchset #1 id:1 of https://codereview.chromium.org/1394543003/ )"
    
    This reverts commit 77970f1c3cd71d535a248d6e15f14081312b300a.
    
    This CL is relands the SyncToken command buffer trait changes but also
    fixes the alignment issues found on android.
    
    R=piman@chromium.org
    BUG=514815
    
    Review URL: https://codereview.chromium.org/1399173002
    
    Cr-Commit-Position: refs/heads/master@{#353555}

--- gpu/command_buffer/tests/gl_fence_sync_unittest.cc
+++ gpu/command_buffer/tests/gl_fence_sync_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <GLES2/gl2extchromium.h>
 
 #include "base/memory/scoped_ptr.h"
+#include "gpu/command_buffer/common/sync_token.h"
 #include "gpu/command_buffer/service/sync_point_manager.h"
 #include "gpu/command_buffer/tests/gl_manager.h"
 #include "gpu/command_buffer/tests/gl_test_utils.h"
@@ -45,9 +46,9 @@ TEST_F(GLFenceSyncTest, SimpleReleaseWait) {
   gl1_.MakeCurrent();
 
   const GLuint64 fence_sync = glInsertFenceSyncCHROMIUM();
-  GLbyte sync_token[GL_SYNC_TOKEN_SIZE_CHROMIUM];
+  SyncToken sync_token;
   glFlush();
-  glGenSyncTokenCHROMIUM(fence_sync, sync_token);
+  glGenSyncTokenCHROMIUM(fence_sync, sync_token.GetData());
   ASSERT_TRUE(GL_NO_ERROR == glGetError());
 
   // Make sure it is actually released.
@@ -57,7 +58,7 @@ TEST_F(GLFenceSyncTest, SimpleReleaseWait) {
   EXPECT_TRUE(gl1_client_state->IsFenceSyncReleased(fence_sync));
 
   gl2_.MakeCurrent();
-  glWaitSyncTokenCHROMIUM(sync_token);
+  glWaitSyncTokenCHROMIUM(sync_token.GetConstData());
   glFinish();
 
   // gl2 should not have released anything.
