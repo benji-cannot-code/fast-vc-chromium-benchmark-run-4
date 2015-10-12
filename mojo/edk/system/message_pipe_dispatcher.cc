b00f1382af6a4228107403e4501b0dedebdf6b43
jam <jam@chromium.org>
2015-10-12 10:08:29 -0700

    Fix /analyze warnings in src/mojo/edk from r352123.
    BUG=478251
    
    Review URL: https://codereview.chromium.org/1394993005
    
    Cr-Commit-Position: refs/heads/master@{#353540}

--- mojo/edk/system/message_pipe_dispatcher.cc
+++ mojo/edk/system/message_pipe_dispatcher.cc
@@ -267,7 +267,7 @@ scoped_refptr<MessagePipeDispatcher> MessagePipeDispatcher::Deserialize(
 
     // TODO(jam): Copied below from RawChannelWin. See commment above
     // GetReadPlatformHandles.
-    ScopedPlatformHandleVectorPtr platform_handles;
+    ScopedPlatformHandleVectorPtr temp_platform_handles;
     if (message_view.transport_data_buffer()) {
       size_t num_platform_handles;
       const void* platform_handle_table;
@@ -276,10 +276,10 @@ scoped_refptr<MessagePipeDispatcher> MessagePipeDispatcher::Deserialize(
           &platform_handle_table);
 
       if (num_platform_handles > 0) {
-        platform_handles =
+        temp_platform_handles =
             GetReadPlatformHandles(num_platform_handles,
                                     platform_handle_table).Pass();
-        if (!platform_handles) {
+        if (!temp_platform_handles) {
           LOG(ERROR) << "Invalid number of platform handles received";
           return nullptr;
         }
@@ -293,7 +293,8 @@ scoped_refptr<MessagePipeDispatcher> MessagePipeDispatcher::Deserialize(
       DCHECK(message_view.transport_data_buffer());
       message->SetDispatchers(TransportData::DeserializeDispatchers(
           message_view.transport_data_buffer(),
-          message_view.transport_data_buffer_size(), platform_handles.Pass()));
+          message_view.transport_data_buffer_size(),
+          temp_platform_handles.Pass()));
     }
 
     rv->message_queue_.AddMessage(message.Pass());
