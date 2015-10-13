9fdc1c830d9e63b9794695ef6bbd6a6fa6fb782c
nasko <nasko@chromium.org>
2015-10-13 13:58:14 -0700

    Disable support for swapped out RenderFrame(Host) and add instrumentation
    This CL is aiming to collect information about cases where the
    FocusController's focused frame is set to a RemoteFrame. This is the root
    cause of the crash in https://crbug.com/541578.
    
    BUG=541578, 357747
    
    Review URL: https://codereview.chromium.org/1397323004
    
    Cr-Commit-Position: refs/heads/master@{#353847}

--- content/common/site_isolation_policy.cc
+++ content/common/site_isolation_policy.cc
@@ -74,7 +74,7 @@ bool SiteIsolationPolicy::UseSubframeNavigationEntries() {
 
 // static
 bool SiteIsolationPolicy::IsSwappedOutStateForbidden() {
-  return AreCrossProcessFramesPossible();
+  return true;
 }
 
 // static
