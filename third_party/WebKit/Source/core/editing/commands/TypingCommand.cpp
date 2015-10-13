ff2c8c5468257229147e90ee0836aa16e2aa093e
aelias <aelias@chromium.org>
2015-10-13 11:25:32 -0700

    Fix deleteSelection on last typing command.
    TypingCommand::deleteSelection was missing the
    updateSelectionIfDifferentFromCurrentSelection() call which was present
    in the similar codeblocks in deleteKeyPressed() and
    forwardDeleteKeyPressed().  As a result, the pattern of setting a
    selection in InputMethodController::setComposition followed by
    TypingCommand::deleteSelection had no effect, since the selection only
    applied to the latest typing command, not the old one.
    
    With this fix, the "empty composition workaround" on the browser side
    can be deleted.
    
    BUG=373934
    TEST=webkit_unit_tests --gtest_filter=InputMethodControllerTest.DeleteBySettingEmptyComposition
    
    Review URL: https://codereview.chromium.org/1392023005
    
    Cr-Commit-Position: refs/heads/master@{#353799}

--- third_party/WebKit/Source/core/editing/commands/TypingCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/TypingCommand.cpp
@@ -102,6 +102,8 @@ void TypingCommand::deleteSelection(Document& document, Options options)
         return;
 
     if (RefPtrWillBeRawPtr<TypingCommand> lastTypingCommand = lastTypingCommandIfStillOpenForTyping(frame)) {
+        updateSelectionIfDifferentFromCurrentSelection(lastTypingCommand.get(), frame);
+
         lastTypingCommand->setShouldPreventSpellChecking(options & PreventSpellChecking);
         lastTypingCommand->deleteSelection(options & SmartDelete);
         return;
