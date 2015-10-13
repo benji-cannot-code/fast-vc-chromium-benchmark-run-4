b073e3d2b71ae1bed2cfabc6784fc1ce7906c2d1
avi <avi@chromium.org>
2015-10-12 20:36:57 -0700

    Fix dependency.
    content_favicon_driver_unittest.cc relies on SkBitmap, so explicitly state that.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1392373005
    
    Cr-Commit-Position: refs/heads/master@{#353670}

--- components/favicon/content/content_favicon_driver_unittest.cc
+++ components/favicon/content/content_favicon_driver_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/favicon_url.h"
 #include "content/public/test/test_renderer_host.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/favicon_size.h"
 
 namespace favicon {
