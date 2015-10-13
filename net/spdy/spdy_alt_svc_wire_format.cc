4f2e3fb9fc04b49070f56bb205efcb2ecbe48687
bnc <bnc@chromium.org>
2015-10-12 20:59:30 -0700

    Implement "clear" keyword in SpdyAltSvcWireFormat.
    Note that the "clear" keyword was introduced to the Alternative Services
    specification in draft-08.
    
    This CL lands server change 104884972 by bnc.
    
    BUG=392575
    
    Review URL: https://codereview.chromium.org/1402633003
    
    Cr-Commit-Position: refs/heads/master@{#353672}

--- net/spdy/spdy_alt_svc_wire_format.cc
+++ net/spdy/spdy_alt_svc_wire_format.cc
@@ -55,7 +55,14 @@ SpdyAltSvcWireFormat::AlternativeService::AlternativeService(
 bool SpdyAltSvcWireFormat::ParseHeaderFieldValue(
     StringPiece value,
     AlternativeServiceVector* altsvc_vector) {
+  // Empty value is invalid according to the specification.
+  if (value.empty()) {
+    return false;
+  }
   altsvc_vector->clear();
+  if (value == StringPiece("clear")) {
+    return true;
+  }
   StringPiece::const_iterator c = value.begin();
   while (c != value.end()) {
     // Parse protocol-id.
@@ -157,6 +164,9 @@ bool SpdyAltSvcWireFormat::ParseHeaderFieldValue(
 // static
 std::string SpdyAltSvcWireFormat::SerializeHeaderFieldValue(
     const AlternativeServiceVector& altsvc_vector) {
+  if (altsvc_vector.empty()) {
+    return std::string("clear");
+  }
   const char kNibbleToHex[] = "0123456789ABCDEF";
   std::string value;
   for (const AlternativeService& altsvc : altsvc_vector) {
