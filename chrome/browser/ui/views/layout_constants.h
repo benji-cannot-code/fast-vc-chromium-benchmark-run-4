40c15260bbc8613eb62d2df41add60d77fa23b83
varkha <varkha@chromium.org>
2015-10-13 08:45:21 -0700

    Fixes BrowserViewHostedAppTest.Layout when using MD
    Find bar is pushed further above the content then the test expeected.
    Also changes the layout to use LayoutConstant(FIND_BAR_VERTICAL_OFFSET).
    
    BUG=538629
    
    Review URL: https://codereview.chromium.org/1395193002
    
    Cr-Commit-Position: refs/heads/master@{#353758}

--- chrome/browser/ui/views/layout_constants.h
+++ chrome/browser/ui/views/layout_constants.h
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/insets.h"
 
 enum LayoutConstant {
+  // Vertical offset from top of content to the top of find bar.
+  FIND_BAR_TOOLBAR_OVERLAP,
+
   // Horizontal padding applied between items of icon-label views.
   ICON_LABEL_VIEW_INTERNAL_PADDING,
 
