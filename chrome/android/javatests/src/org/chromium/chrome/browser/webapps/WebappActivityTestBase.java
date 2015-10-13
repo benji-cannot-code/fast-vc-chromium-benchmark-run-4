3accd9fe11ca0ac05b822fc73e0eba4d327d8ab5
mlamouri <mlamouri@chromium.org>
2015-10-13 14:46:07 -0700

    WebApps splashscreen: show Manifest theme_color while loading.
    This is fixing a regression caused by
    https://codereview.chromium.org/1375693002
    
    BUG=542773
    
    Review URL: https://codereview.chromium.org/1403683006
    
    Cr-Commit-Position: refs/heads/master@{#353862}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappActivityTestBase.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappActivityTestBase.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.webapps;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 
 import org.chromium.base.ThreadUtils;
@@ -47,6 +48,7 @@ public abstract class WebappActivityTestBase extends ChromeActivityTestCaseBase<
         Intent intent = new Intent(getInstrumentation().getTargetContext(), WebappActivity0.class);
         intent.setData(Uri.parse(WebappActivity.WEBAPP_SCHEME + "://" + WEBAPP_ID));
         intent.putExtra(ShortcutHelper.EXTRA_ID, WEBAPP_ID);
+        intent.putExtra(ShortcutHelper.EXTRA_THEME_COLOR, (long) Color.MAGENTA);
         intent.putExtra(ShortcutHelper.EXTRA_URL, "about:blank");
         setActivityIntent(intent);
 
