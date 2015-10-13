20bc1201425fa8fa7f1a5b94d99dc923a9c84d5c
imansourov <imansourov@google.com>
2015-10-13 01:32:30 -0700

    Added code to pass detailed reason why application is not going to be installed from management policy to JavaScript handler to display to the user for better user experience
    BUG=406477
    
    Review URL: https://codereview.chromium.org/1338733004
    
    Cr-Commit-Position: refs/heads/master@{#353705}

--- chrome/browser/extensions/api/webstore_private/webstore_private_api.cc
+++ chrome/browser/extensions/api/webstore_private/webstore_private_api.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/prefs/pref_service.h"
 #include "base/strings/stringprintf.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/thread_task_runner_handle.h"
 #include "base/values.h"
 #include "base/version.h"
@@ -171,7 +172,6 @@ const char kInvalidIdError[] = "Invalid id";
 const char kInvalidManifestError[] = "Invalid manifest";
 const char kNoPreviousBeginInstallWithManifestError[] =
     "* does not match a previous call to beginInstallWithManifest3";
-const char kBlockedByPolicyError[] = "Blocked by policy";
 const char kUserCancelledError[] = "User cancelled install";
 const char kIncognitoError[] =
     "Apps cannot be installed in guest/incognito mode";
@@ -304,12 +304,12 @@ void WebstorePrivateBeginInstallWithManifest3Function::OnWebstoreParseSuccess(
   }
 
   // Check the management policy before the installation process begins
+  base::string16 policy_error;
   bool allow = ExtensionSystem::Get(chrome_details_.GetProfile())->
-      management_policy()->UserMayLoad(extension_.get(),
-                                       NULL);
+      management_policy()->UserMayLoad(dummy_extension_.get(), &policy_error);
   if (!allow) {
     Respond(BuildResponse(api::webstore_private::RESULT_BLOCKED_BY_POLICY,
-                          kBlockedByPolicyError));
+                          base::UTF16ToUTF8(policy_error)));
     // Matches the AddRef in Run().
     Release();
     return;
