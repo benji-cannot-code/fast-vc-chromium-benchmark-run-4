ce856712d3cfc7e4feb07de2144bc3094b69fc2b
drott <drott@chromium.org>
2015-10-13 08:56:47 -0700

    Add two more runs to ScriptRunIteratorTest
    BUG=
    R=eae
    
    Review URL: https://codereview.chromium.org/1402893003
    
    Cr-Commit-Position: refs/heads/master@{#353762}

--- third_party/WebKit/Source/platform/fonts/ScriptRunIteratorTest.cpp
+++ third_party/WebKit/Source/platform/fonts/ScriptRunIteratorTest.cpp
@@ -518,6 +518,12 @@ TEST_F(ScriptRunIteratorTest, QuoteParenChineseParenLatinQuote)
         { "Unicode\"", USCRIPT_LATIN } });
 }
 
+// Emojies are resolved to the leading script.
+TEST_F(ScriptRunIteratorTest, EmojiCommon)
+{
+    CHECK_RUNS({ { "百家姓🌱🌲🌳🌴", USCRIPT_HAN } });
+}
+
 // Unmatched close brace gets leading context
 TEST_F(ScriptRunIteratorTest, UnmatchedClose)
 {
@@ -663,6 +669,22 @@ TEST_F(ScriptRunIteratorTest, HanSpaceUdatta)
 {
     CHECK_RUNS({ { "萬國碼", USCRIPT_HAN },
         { " \xE0\xA5\x91", USCRIPT_DEVANAGARI } });
+}
+
+// Corresponds to one test in RunSegmenter, where orientation of the
+// space character is sidesways in vertical.
+TEST_F(ScriptRunIteratorTest, Hangul)
+{
+    CHECK_RUNS({ { "키스의 고유조건은", USCRIPT_HANGUL } });
+}
+
+// Corresponds to one test in RunSegmenter, which tests that the punctuation
+// characters mixed in are actually sideways in vertical. The ScriptIterator
+// should report one run, but the RunSegmenter should report three, with the
+// middle one rotated sideways.
+TEST_F(ScriptRunIteratorTest, HiraganaMixedPunctuation)
+{
+    CHECK_RUNS({ { "いろはに.…¡ほへと", USCRIPT_HIRAGANA } });
 }
 
 // Make sure Mock code works too.
