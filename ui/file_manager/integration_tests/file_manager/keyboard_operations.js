dfffd758c85238dc31ecb624b09f7dceb88c6ce7
yawano <yawano@chromium.org>
2015-10-13 03:41:16 -0700

    Gallery: select multiple images with shift key.
    BUG=541073
    TEST=browser_tests:GalleryBrowserTest.SelectMultipleImagesWithShiftKeyOnDownloads
    
    Review URL: https://codereview.chromium.org/1402813003
    
    Cr-Commit-Position: refs/heads/master@{#353715}

--- ui/file_manager/integration_tests/file_manager/keyboard_operations.js
+++ ui/file_manager/integration_tests/file_manager/keyboard_operations.js
@@ -213,7 +213,8 @@ function renameFile(windowId, oldName, newName) {
   return remoteCall.callRemoteTestUtil('selectFile', windowId, [oldName]).
     then(function() {
       // Push Ctrl+Enter.
-      return remoteCall.fakeKeyDown(windowId, '#detail-table', 'Enter', true);
+      return remoteCall.fakeKeyDown(
+          windowId, '#detail-table', 'Enter', true, false);
     }).then(function() {
       // Wait for rename text field.
       return remoteCall.waitForElement(windowId, 'input.rename');
@@ -223,7 +224,8 @@ function renameFile(windowId, oldName, newName) {
           'inputText', windowId, ['input.rename', newName]);
     }).then(function() {
       // Push Enter.
-      return remoteCall.fakeKeyDown(windowId, 'input.rename', 'Enter', false);
+      return remoteCall.fakeKeyDown(
+          windowId, 'input.rename', 'Enter', false, false);
     });
 }
 
@@ -243,7 +245,7 @@ function testRenameNewDirectory(path, initialEntrySet, pathInBreadcrumb) {
     var windowId = results.windowId;
     return remoteCall.waitForFiles(windowId, expectedRows).then(function() {
       return remoteCall.fakeKeyDown(
-          windowId, '#list-container', 'U+0045', true);
+          windowId, '#list-container', 'U+0045', true, false);
     }).then(function() {
       // Wait for rename text field.
       return remoteCall.waitForElement(windowId, 'input.rename');
@@ -253,11 +255,12 @@ function testRenameNewDirectory(path, initialEntrySet, pathInBreadcrumb) {
           'inputText', windowId, ['input.rename', 'foo']);
     }).then(function() {
       // Press Enter.
-      return remoteCall.fakeKeyDown(windowId, 'input.rename', 'Enter', false);
+      return remoteCall.fakeKeyDown(
+          windowId, 'input.rename', 'Enter', false, false);
     }).then(function() {
       // Press Enter again to try to get into the new directory.
-      return remoteCall.fakeKeyDown(windowId, '#list-container', 'Enter',
-          false);
+      return remoteCall.fakeKeyDown(
+          windowId, '#list-container', 'Enter', false, false);
     }).then(function() {
       // Confirm that it doesn't move the directory since it's in renaming
       // process.
@@ -269,7 +272,7 @@ function testRenameNewDirectory(path, initialEntrySet, pathInBreadcrumb) {
     }).then(function() {
       // Press Enter again.
       return remoteCall.fakeKeyDown(windowId, '#list-container', 'Enter',
-          false);
+          false, false);
     }).then(function() {
       // Confirm that it moves to renamed directory.
       return remoteCall.waitUntilCurrentDirectoryIsChanged(windowId,
