35aed64b9e8599c2cdaae07e41c1deff1c2fa924
skyostil <skyostil@chromium.org>
2015-10-13 05:09:53 -0700

    cc: Ensure animations schedule a layer update
    If a main thread animation changes either the transform or opacity of
    a layer, tree, we should perform a layer update to ensure any newly
    exposed content gets painted.
    
    BUG=535798
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1366403006
    
    Cr-Commit-Position: refs/heads/master@{#353723}

--- cc/layers/layer.cc
+++ cc/layers/layer.cc
@@ -1437,7 +1437,12 @@ void Layer::OnFilterAnimated(const FilterOperations& filters) {
 }
 
 void Layer::OnOpacityAnimated(float opacity) {
+  if (opacity_ == opacity)
+    return;
   opacity_ = opacity;
+  // Changing the opacity may make a previously hidden layer visible, so a new
+  // recording may be needed.
+  SetNeedsUpdate();
   if (layer_tree_host_) {
     if (EffectNode* node = layer_tree_host_->property_trees()->effect_tree.Node(
             effect_tree_index())) {
@@ -1454,6 +1459,9 @@ void Layer::OnTransformAnimated(const gfx::Transform& transform) {
     return;
   transform_ = transform;
   transform_is_invertible_ = transform.IsInvertible();
+  // Changing the transform may change the visible part of this layer, so a new
+  // recording may be needed.
+  SetNeedsUpdate();
   if (layer_tree_host_) {
     if (TransformNode* node =
             layer_tree_host_->property_trees()->transform_tree.Node(
