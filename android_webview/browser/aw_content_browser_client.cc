5dc62a4ed6a4f65fc46e3dadf3f2688994d1aba7
mnaganov <mnaganov@chromium.org>
2015-10-14 16:41:07 -0700

    [Android WebView] Pass kDisablePageVisibility to renderer child
    Review URL: https://codereview.chromium.org/1407803002
    
    Cr-Commit-Position: refs/heads/master@{#354152}

--- android_webview/browser/aw_content_browser_client.cc
+++ android_webview/browser/aw_content_browser_client.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/browser/net_disk_cache_remover.h"
 #include "android_webview/browser/renderer_host/aw_resource_dispatcher_host_delegate.h"
 #include "android_webview/common/aw_descriptors.h"
+#include "android_webview/common/aw_switches.h"
 #include "android_webview/common/render_view_messages.h"
 #include "android_webview/common/url_constants.h"
 #include "base/android/locale_utils.h"
@@ -294,6 +295,14 @@ void AwContentBrowserClient::AppendExtraCommandLineSwitches(
     // The only kind of a child process WebView can have is renderer.
     DCHECK_EQ(switches::kRendererProcess,
               command_line->GetSwitchValueASCII(switches::kProcessType));
+
+    const base::CommandLine& browser_command_line =
+        *base::CommandLine::ForCurrentProcess();
+    static const char* const kCommonSwitchNames[] = {
+      switches::kDisablePageVisibility,
+    };
+    command_line->CopySwitchesFrom(browser_command_line, kCommonSwitchNames,
+                                   arraysize(kCommonSwitchNames));
   }
 }
 
