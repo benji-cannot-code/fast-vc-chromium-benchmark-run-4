6ce30da3529b48f722b58d191afa6c65f4694039
tbansal <tbansal@chromium.org>
2015-10-14 16:05:49 -0700

    Change access restriction from private to protected.
    This is needed for the implementation class in the downstream
    code to access or override these variables.
    
    BUG=540061
    
    Review URL: https://codereview.chromium.org/1403893006
    
    Cr-Commit-Position: refs/heads/master@{#354142}

--- chrome/android/java/src/org/chromium/chrome/browser/datausage/ExternalDataUseObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/datausage/ExternalDataUseObserver.java
@@ -20,7 +20,7 @@ public class ExternalDataUseObserver {
     /**
      * Pointer to the native ExternalDataUseObserver object.
      */
-    private long mNativeExternalDataUseObserver;
+    protected long mNativeExternalDataUseObserver;
 
     @CalledByNative
     private static ExternalDataUseObserver create(Context context, long nativePtr) {
@@ -36,7 +36,7 @@ public class ExternalDataUseObserver {
     }
 
     @CalledByNative
-    private void onDataUse(String tag, long bytesDownloaded, long bytesUploaded) {}
+    protected void onDataUse(String tag, long bytesDownloaded, long bytesUploaded) {}
 
     /**
      * Creates an instance of {@link #ExternalDataUseObserver}.
