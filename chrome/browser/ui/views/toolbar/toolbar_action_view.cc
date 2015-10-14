0f87d5b310afc30fe954d7ef8addf5d11a010abd
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-10-14 15:09:47 -0700

    [Views] Forward mouse events in menus to the proper root view
    Mouse events in menus get sent to the main menu's root view, even if there's a
    nested menu running. Add logic to forward these events to the proper root view.
    
    BUG=538414
    BUG=541021
    
    Review URL: https://codereview.chromium.org/1395803003
    
    Cr-Commit-Position: refs/heads/master@{#354129}

--- chrome/browser/ui/views/toolbar/toolbar_action_view.cc
+++ chrome/browser/ui/views/toolbar/toolbar_action_view.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/auto_reset.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sessions/session_tab_helper.h"
@@ -28,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/resources/grit/ui_resources.h"
 #include "ui/views/controls/button/label_button_border.h"
 #include "ui/views/controls/menu/menu_controller.h"
+#include "ui/views/controls/menu/menu_model_adapter.h"
 #include "ui/views/controls/menu/menu_runner.h"
 #include "ui/views/mouse_constants.h"
 #include "ui/views/resources/grit/views_resources.h"
@@ -46,6 +48,9 @@ const int kBorderInset = 0;
 // safe to have this be a global singleton.
 ToolbarActionView* context_menu_owner = nullptr;
 
+// The callback to call directly before showing the context menu.
+ToolbarActionView::ContextMenuCallback* context_menu_callback = nullptr;
+
 }  // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -61,6 +66,7 @@ ToolbarActionView::ToolbarActionView(
       delegate_(delegate),
       called_register_command_(false),
       wants_to_run_(false),
+      menu_(nullptr),
       weak_factory_(this) {
   set_id(VIEW_ID_BROWSER_ACTION);
   view_controller_->SetDelegate(this);
@@ -194,6 +200,11 @@ gfx::ImageSkia ToolbarActionView::GetIconForTest() {
   return GetImage(views::Button::STATE_NORMAL);
 }
 
+void ToolbarActionView::set_context_menu_callback_for_testing(
+    base::Callback<void(ToolbarActionView*)>* callback) {
+  context_menu_callback = callback;
+}
+
 views::View* ToolbarActionView::GetAsView() {
   return this;
 }
@@ -210,7 +221,7 @@ views::View* ToolbarActionView::GetReferenceViewForPopup() {
 }
 
 bool ToolbarActionView::IsMenuRunning() const {
-  return menu_runner_.get() != nullptr;
+  return menu_ != nullptr;
 }
 
 content::WebContents* ToolbarActionView::GetCurrentWebContents() const {
@@ -270,7 +281,6 @@ void ToolbarActionView::DoShowContextMenu(
 
   DCHECK(visible());  // We should never show a context menu for a hidden item.
   DCHECK(!context_menu_owner);
-  context_menu_owner = this;
 
   gfx::Point screen_loc;
   ConvertPointToScreen(this, &screen_loc);
@@ -286,18 +296,25 @@ void ToolbarActionView::DoShowContextMenu(
       delegate_->GetOverflowReferenceView()->GetWidget() :
       GetWidget();
 
-  menu_runner_.reset(new views::MenuRunner(context_menu_model, run_types));
+  views::MenuModelAdapter adapter(context_menu_model);
+  views::MenuItemView* menu_root = adapter.CreateMenu();
 
-  if (menu_runner_->RunMenuAt(parent,
-                              this,
-                              gfx::Rect(screen_loc, size()),
+  {
+    base::AutoReset<views::MenuItemView*> menu_resetter(&menu_, menu_root);
+    base::AutoReset<ToolbarActionView*> menu_owner_resetter(&context_menu_owner,
+                                                            this);
+
+    views::MenuRunner menu_runner(menu_, run_types);
+
+    if (context_menu_callback)
+      context_menu_callback->Run(this);
+    if (menu_runner.RunMenuAt(parent, this, gfx::Rect(screen_loc, size()),
                               views::MENU_ANCHOR_TOPLEFT,
                               source_type) == views::MenuRunner::MENU_DELETED) {
-    return;
+      return;
+    }
   }
 
-  context_menu_owner = nullptr;
-  menu_runner_.reset();
   view_controller_->OnContextMenuClosed();
 
   // If another extension action wants to show its context menu, allow it to.
