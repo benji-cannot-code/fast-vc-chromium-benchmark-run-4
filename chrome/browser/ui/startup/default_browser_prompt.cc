c1694af0e399aac09893fec1638697eb108285e3
estade <estade@chromium.org>
2015-10-14 12:48:59 -0700

    Change default browser infobar back to yellow on Windows.
    BUG=539987
    
    Review URL: https://codereview.chromium.org/1398433003
    
    Cr-Commit-Position: refs/heads/master@{#354088}

--- chrome/browser/ui/startup/default_browser_prompt.cc
+++ chrome/browser/ui/startup/default_browser_prompt.cc
@@ -170,7 +170,11 @@ DefaultBrowserInfoBarDelegate::~DefaultBrowserInfoBarDelegate() {
 
 infobars::InfoBarDelegate::Type DefaultBrowserInfoBarDelegate::GetInfoBarType()
     const {
+#if defined(OS_WIN)
+  return WARNING_TYPE;
+#else
   return PAGE_ACTION_TYPE;
+#endif
 }
 
 int DefaultBrowserInfoBarDelegate::GetIconId() const {
