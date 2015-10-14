2842dfcca4df38e05a2e85e41ec92017fb42327f
peria <peria@chromium.org>
2015-10-13 21:36:46 -0700

    Fix wrong usage of HashSet<InstrumentingAgents>
    BUG=515524
    
    Review URL: https://codereview.chromium.org/1396633003
    
    Cr-Commit-Position: refs/heads/master@{#353960}

--- third_party/WebKit/Source/core/inspector/InspectorInstrumentation.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorInstrumentation.cpp
@@ -50,7 +50,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 namespace {
-static HashSet<InstrumentingAgents*>* instrumentingAgentsSet = 0;
+
+WillBePersistentHeapHashSet<RawPtrWillBeWeakMember<InstrumentingAgents>>& instrumentingAgentsSet()
+{
+    DEFINE_STATIC_LOCAL(WillBePersistentHeapHashSet<RawPtrWillBeWeakMember<InstrumentingAgents>>, instrumentingAgentsSet, ());
+    return instrumentingAgentsSet;
+}
+
 }
 
 namespace InspectorInstrumentation {
@@ -110,9 +116,7 @@ void continueWithPolicyIgnoreImpl(LocalFrame* frame, DocumentLoader* loader, uns
 void willDestroyResourceImpl(Resource* cachedResource)
 {
     ASSERT(isMainThread());
-    if (!instrumentingAgentsSet)
-        return;
-    for (InstrumentingAgents* instrumentingAgents: *instrumentingAgentsSet) {
+    for (InstrumentingAgents* instrumentingAgents: instrumentingAgentsSet()) {
         if (InspectorResourceAgent* inspectorResourceAgent = instrumentingAgents->inspectorResourceAgent())
             inspectorResourceAgent->willDestroyResource(cachedResource);
     }
@@ -121,9 +125,7 @@ void willDestroyResourceImpl(Resource* cachedResource)
 bool collectingHTMLParseErrorsImpl(InstrumentingAgents* instrumentingAgents)
 {
     ASSERT(isMainThread());
-    if (!instrumentingAgentsSet)
-        return false;
-    return instrumentingAgentsSet->contains(instrumentingAgents);
+    return instrumentingAgentsSet().contains(instrumentingAgents);
 }
 
 void appendAsyncCallStack(ExecutionContext* executionContext, ScriptCallStack* callStack)
@@ -145,21 +147,14 @@ bool consoleAgentEnabled(ExecutionContext* executionContext)
 void registerInstrumentingAgents(InstrumentingAgents* instrumentingAgents)
 {
     ASSERT(isMainThread());
-    if (!instrumentingAgentsSet)
-        instrumentingAgentsSet = new HashSet<InstrumentingAgents*>();
-    instrumentingAgentsSet->add(instrumentingAgents);
+    instrumentingAgentsSet().add(instrumentingAgents);
 }
 
 void unregisterInstrumentingAgents(InstrumentingAgents* instrumentingAgents)
 {
     ASSERT(isMainThread());
-    if (!instrumentingAgentsSet)
-        return;
-    instrumentingAgentsSet->remove(instrumentingAgents);
-    if (instrumentingAgentsSet->isEmpty()) {
-        delete instrumentingAgentsSet;
-        instrumentingAgentsSet = 0;
-    }
+    ASSERT(instrumentingAgentsSet().contains(instrumentingAgents));
+    instrumentingAgentsSet().remove(instrumentingAgents);
 }
 
 InstrumentingAgents* instrumentingAgentsFor(LocalFrame* frame)
@@ -222,4 +217,3 @@ InstrumentingAgents* instrumentationForWorkerGlobalScope(WorkerGlobalScope* work
 }
 
 } // namespace blink
-
