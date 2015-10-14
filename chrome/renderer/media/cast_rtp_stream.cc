bca8d926f8c0aa08848119ecd5a3413949d9f18f
mcasas <mcasas@chromium.org>
2015-10-14 08:41:27 -0700

    Cast: extracting RTP Payload type and Video min/max bitrate into constants
    This CL is a cleanup that extracts a few constants:
    - RTP Video Payload, 96
    - RTP Audio Payload 127
    - Video Min/Max by-default bitrate (this is less pervasive though).
    
    The constants are moved to media/cast/cast_defines.h
    
    No new code is added.
    
    BUG=522661
    
    Review URL: https://codereview.chromium.org/1399273006
    
    Cr-Commit-Position: refs/heads/master@{#354029}

--- chrome/renderer/media/cast_rtp_stream.cc
+++ chrome/renderer/media/cast_rtp_stream.cc
@@ -50,7 +50,7 @@ const int kBitrateMultiplier = 1000;
 
 CastRtpPayloadParams DefaultOpusPayload() {
   CastRtpPayloadParams payload;
-  payload.payload_type = 127;
+  payload.payload_type = media::cast::kDefaultRtpAudioPayloadType;
   payload.max_latency_ms = media::cast::kDefaultRtpMaxDelayMs;
   payload.ssrc = 1;
   payload.feedback_ssrc = 2;
@@ -66,13 +66,13 @@ CastRtpPayloadParams DefaultOpusPayload() {
 
 CastRtpPayloadParams DefaultVp8Payload() {
   CastRtpPayloadParams payload;
-  payload.payload_type = 96;
+  payload.payload_type = media::cast::kDefaultRtpVideoPayloadType;
   payload.max_latency_ms = media::cast::kDefaultRtpMaxDelayMs;
   payload.ssrc = 11;
   payload.feedback_ssrc = 12;
   payload.clock_rate = media::cast::kVideoFrequency;
-  payload.max_bitrate = 2000;
-  payload.min_bitrate = 50;
+  payload.max_bitrate = media::cast::kDefaultMaxVideoBitRate;
+  payload.min_bitrate = media::cast::kDefaultMinVideoBitRate;
   payload.channels = 1;
   payload.max_frame_rate = media::cast::kDefaultMaxFrameRate;
   payload.codec_name = kCodecNameVp8;
@@ -86,8 +86,8 @@ CastRtpPayloadParams DefaultH264Payload() {
   payload.ssrc = 11;
   payload.feedback_ssrc = 12;
   payload.clock_rate = media::cast::kVideoFrequency;
-  payload.max_bitrate = 2000;
-  payload.min_bitrate = 50;
+  payload.max_bitrate = media::cast::kDefaultMaxVideoBitRate;
+  payload.min_bitrate = media::cast::kDefaultMinVideoBitRate;
   payload.channels = 1;
   payload.max_frame_rate = media::cast::kDefaultMaxFrameRate;
   payload.codec_name = kCodecNameH264;
