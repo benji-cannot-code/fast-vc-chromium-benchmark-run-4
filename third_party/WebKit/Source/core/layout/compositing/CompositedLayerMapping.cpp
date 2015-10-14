a6024db6f140afd865b4d18ecdbb0cc314c6fa11
skobes <skobes@chromium.org>
2015-10-14 10:15:46 -0700

    Remove CompositedLayerMapping::hasUnpositionedOverflowControlsLayers.
    "!layer->drawsContent()" is not a good indicator of whether a scrollbar
    GraphicsLayer's geometry is up to date, for two reasons:
    
    (1) Coordinated scrollbars (backed by a WebScrollbarLayer) have m_drawsContent
        == false at all times, so they are already having their geometry recomputed
        on every compositing update.
    
    (2) ScrollingCoordinator will call setDrawsContent(true) on a newly created
        scrollbar layer, if the scrollbar is visible but disabled.  This tricks
        hasUnpositionedOverflowControlsLayers into thinking the scrollbar has
        already been positioned, when it hasn't.
    
    GraphicsLayerUpdater now calls positionOverflowControls whenever the PaintLayer
    has a ScrollableArea.
    
    BUG=541119
    
    Review URL: https://codereview.chromium.org/1405683002
    
    Cr-Commit-Position: refs/heads/master@{#354049}

--- third_party/WebKit/Source/core/layout/compositing/CompositedLayerMapping.cpp
+++ third_party/WebKit/Source/core/layout/compositing/CompositedLayerMapping.cpp
@@ -1369,26 +1369,6 @@ void CompositedLayerMapping::positionOverflowControlsLayers()
     }
 }
 
-bool CompositedLayerMapping::hasUnpositionedOverflowControlsLayers() const
-{
-    if (GraphicsLayer* layer = layerForHorizontalScrollbar()) {
-        if (!layer->drawsContent())
-            return true;
-    }
-
-    if (GraphicsLayer* layer = layerForVerticalScrollbar()) {
-        if (!layer->drawsContent())
-            return true;
-    }
-
-    if (GraphicsLayer* layer = layerForScrollCorner()) {
-        if (!layer->drawsContent())
-            return true;
-    }
-
-    return false;
-}
-
 enum ApplyToGraphicsLayersModeFlags {
     ApplyToLayersAffectedByPreserve3D = (1 << 0),
     ApplyToSquashingLayer = (1 << 1),
