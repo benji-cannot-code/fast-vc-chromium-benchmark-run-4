9991f942c9c136062a3dcd989e823f4591fbc691
pkasting <pkasting@chromium.org>
2015-10-13 17:09:05 -0700

    Switch tab.cc to use an empty border for the contents.
    This is a small code simplification.
    
    Also clean up border.cc a bit -- use gfx::Insets more consistently, make the
    class naming (SolidSidedBorder) match the function naming
    (CreateSolidSidedBorder()), and be consistent about whether or not to inline
    class methods.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1401933004
    
    Cr-Commit-Position: refs/heads/master@{#353918}

--- chrome/browser/ui/views/tabs/tab.cc
+++ chrome/browser/ui/views/tabs/tab.cc
@@ -391,6 +391,8 @@ Tab::Tab(TabController* controller)
 
   set_id(VIEW_ID_TAB);
 
+  SetBorder(views::Border::CreateEmptyBorder(GetLayoutInsets(TAB)));
+
   title_->SetHorizontalAlignment(gfx::ALIGN_TO_HEAD);
   title_->SetElideBehavior(gfx::FADE_TAIL);
   title_->SetHandlesTooltips(false);
@@ -780,7 +782,7 @@ void Tab::OnPaint(gfx::Canvas* canvas) {
 }
 
 void Tab::Layout() {
-  gfx::Rect lb = GetInteriorBounds();
+  gfx::Rect lb = GetContentsBounds();
   if (lb.IsEmpty())
     return;
 
@@ -1347,7 +1349,7 @@ void Tab::PaintIcon(gfx::Canvas* canvas) {
   gfx::Rect bounds = favicon_bounds_;
   bounds.set_x(GetMirroredXForRect(bounds));
   bounds.Offset(0, favicon_hiding_offset_);
-  bounds.Intersect(GetInteriorBounds());
+  bounds.Intersect(GetContentsBounds());
   if (bounds.IsEmpty())
     return;
 
@@ -1520,12 +1522,6 @@ void Tab::ScheduleIconPaint() {
     bounds.set_height(height() - bounds.y());
   bounds.set_x(GetMirroredXForRect(bounds));
   SchedulePaintInRect(bounds);
-}
-
-gfx::Rect Tab::GetInteriorBounds() const {
-  gfx::Rect bounds(GetContentsBounds());
-  bounds.Inset(GetLayoutInsets(TAB));
-  return bounds;
 }
 
 gfx::Rect Tab::GetImmersiveBarRect() const {
