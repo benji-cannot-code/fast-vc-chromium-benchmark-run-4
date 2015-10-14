ad4a21c56abee58706d777c55f0654bad81b22f7
siggi <siggi@chromium.org>
2015-10-14 11:20:59 -0700

    Clear out vestigal exit funnel instrumentation.
    BUG=442526
    
    Review URL: https://codereview.chromium.org/1400263002
    
    Cr-Commit-Position: refs/heads/master@{#354068}

--- chrome/browser/background/background_mode_manager.cc
+++ chrome/browser/background/background_mode_manager.cc
@@ -62,10 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 
-#if defined(OS_WIN)
-#include "components/browser_watcher/exit_funnel_win.h"
-#endif
-
 using base::UserMetricsAction;
 using extensions::Extension;
 
@@ -670,10 +666,6 @@ void BackgroundModeManager::ExecuteCommand(int command_id, int event_flags) {
       break;
     case IDC_EXIT:
       RecordMenuItemClick(MENU_ITEM_EXIT);
-#if defined(OS_WIN)
-      browser_watcher::ExitFunnel::RecordSingleEvent(
-            chrome::kBrowserExitCodesRegistryPath, L"TraybarExit");
-#endif
       content::RecordAction(UserMetricsAction("Exit"));
       chrome::CloseAllBrowsers();
       break;
@@ -796,11 +788,6 @@ void BackgroundModeManager::UpdateKeepAliveAndTrayIcon() {
     if (!keeping_alive_) {
       keeping_alive_ = true;
       chrome::IncrementKeepAliveCount();
-
-#if defined(OS_WIN)
-      browser_watcher::ExitFunnel::RecordSingleEvent(
-            chrome::kBrowserExitCodesRegistryPath, L"BackgroundOn");
-#endif
     }
     CreateStatusTrayIcon();
     return;
@@ -810,11 +797,6 @@ void BackgroundModeManager::UpdateKeepAliveAndTrayIcon() {
   if (keeping_alive_) {
     keeping_alive_ = false;
     chrome::DecrementKeepAliveCount();
-
-#if defined(OS_WIN)
-    browser_watcher::ExitFunnel::RecordSingleEvent(
-          chrome::kBrowserExitCodesRegistryPath, L"BackgroundOff");
-#endif
   }
 }
 
