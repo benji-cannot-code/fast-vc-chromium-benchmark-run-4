bc7f9df54a2377e27bb525dcc91dc12bc1fea468
mmenke <mmenke@chromium.org>
2015-10-14 16:11:50 -0700

    Show network diagnostics dialog button on OSX.
    I accidentally broke it in https://codereview.chromium.org/1277053003/
    when refactoring the code.
    
    BUG=543316
    
    Review URL: https://codereview.chromium.org/1404823003
    
    Cr-Commit-Position: refs/heads/master@{#354145}

--- chrome/browser/net/net_error_diagnostics_dialog_mac.cc
+++ chrome/browser/net/net_error_diagnostics_dialog_mac.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 
 bool CanShowNetworkDiagnosticsDialog() {
-  return false;
+  return true;
 }
 
 void ShowNetworkDiagnosticsDialog(content::WebContents* web_contents,
