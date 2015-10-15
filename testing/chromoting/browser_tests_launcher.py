f3c99dca08904497c412ee09684dcc830423f2c9
anandc <anandc@google.com>
2015-10-15 13:41:59 -0700

    Clean up user-profile dir when re-running test on host JID mismatch. When the connect-to-remote host test was retried upon a host-JID mismatch, the re-attempt would always fail because of junk left in the user-profile dir being used by the test. This fixes that problem.
    BUG=
    
    Review URL: https://codereview.chromium.org/1395693010
    
    Cr-Commit-Position: refs/heads/master@{#354347}

--- testing/chromoting/browser_tests_launcher.py
+++ testing/chromoting/browser_tests_launcher.py
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import argparse
 import time
 
+from chromoting_test_utilities import CleanupUserProfileDir
 from chromoting_test_utilities import GetJidListFromTestResults
 from chromoting_test_utilities import InitialiseTestMachineForLinux
 from chromoting_test_utilities import PrintHostLogContents
@@ -49,7 +50,10 @@ def LaunchBTCommand(args, command):
     # It returns the file-name of the me2me host log.
     # If we are attempting to run this test because of a JID-mismatch, don't
     # restart host.
-    if not host_jid_mismatch:
+    if host_jid_mismatch:
+      # Cleanup user-profile directory, but don't restart host.
+      CleanupUserProfileDir(args)
+    else:
       host_log_file_names.append(TestCaseSetup(args))
       # Parse the me2me host log to obtain the JID that the host registered.
       host_jid = None
