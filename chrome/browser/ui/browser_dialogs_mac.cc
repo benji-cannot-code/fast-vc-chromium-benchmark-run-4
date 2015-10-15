e2f49103f8d32ee22d7a649085bf3d832309c5e2
tapted <tapted@chromium.org>
2015-10-15 12:35:25 -0700

    Fix typo in browser_dialogs_mac.cc
    It #includes a .cc file. It shouldn't.
    
    Review URL: https://codereview.chromium.org/1403143002
    
    Cr-Commit-Position: refs/heads/master@{#354326}

--- chrome/browser/ui/browser_dialogs_mac.cc
+++ chrome/browser/ui/browser_dialogs_mac.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_dialogs.h"
 
 #include "base/command_line.h"
-#include "chrome/common/chrome_switches.cc"
+#include "chrome/common/chrome_switches.h"
 
 namespace chrome {
 
