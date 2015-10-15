639a82df352d82a3788bbd0b861b2bb49b500c65
jam <jam@chromium.org>
2015-10-14 18:24:26 -0700

    Last set of fixes to make the src/mojo/edk pass the page cycler.
    -remove the different code paths in RawChannelWin for using IOCP or manual reset. We know we definitely can't use the former. The latter is too slow and we know for sure now that auto-reset works.
    -after calling CancelIO(Ex) synchronously wait for the result. Otherwise we would sometimes miss notification that a page was read, leading to gaps in in data read from the pipes.
    -call RegisterWaitForSingleObject directly instead of using ObjectWatcher, to make the above possible. It would have made the API for ObjectWatcher too ugly to add this support there.
    -ensure that if there's a pending post task to set the error state, it is serialized as well. otherwise the consumer of the mojo primitive will never see a peer-closed signal, leading to hangs.
    -ensure we don't call the delegate's OnError more than once. this could lead to UAFs.
    -ensure that the read/write lock are acquired before modifying pending_read_/pending_write_, otherwise there are races where the IO thread set it to false before calling into RawChannel::OnReadCompleted/OnWriteCompleted, and ReleaseHandle didn't see that there was pending IO
    
    BUG=478251
    
    Review URL: https://codereview.chromium.org/1403033003
    
    Cr-Commit-Position: refs/heads/master@{#354191}

--- mojo/edk/system/message_pipe_dispatcher.cc
+++ mojo/edk/system/message_pipe_dispatcher.cc
@@ -119,6 +119,8 @@ void MessagePipeDispatcher::Init(
       InitOnIO();
     }
     // TODO(jam): optimize for when running on IO thread?
+  } else {
+    error_ = true;
   }
 }
 
@@ -637,6 +639,8 @@ bool MessagePipeDispatcher::EndSerializeAndCloseImplNoLock(
 
     serialization->shared_memory_handle_index = platform_handles->size();
     platform_handles->push_back(shared_buffer->PassPlatformHandle().release());
+  } else {
+    serialization->shared_memory_handle_index = kInvalidMessagePipeHandleIndex;
   }
 
   *actual_size = sizeof(SerializedMessagePipeHandleDispatcher);
@@ -722,6 +726,13 @@ void MessagePipeDispatcher::OnError(Error error) {
   error_ = true;
   if (started_transport_.Try()) {
     base::AutoLock locker(lock());
+    // We can get two OnError callbacks before the post task below completes.
+    // Although RawChannel still has a pointer to this object until Shutdown is
+    // called, that is safe since this class always does a PostTask to the IO
+    // thread to self destruct.
+    if (!channel_)
+      return;
+
     awakable_list_.AwakeForStateChange(GetHandleSignalsStateImplNoLock());
 
     base::MessageLoop::current()->PostTask(
