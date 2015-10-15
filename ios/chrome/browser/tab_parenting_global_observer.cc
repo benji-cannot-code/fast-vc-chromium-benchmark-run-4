06c75a84778e59b29d843a031f2cc10c154011f6
blundell <blundell@chromium.org>
2015-10-15 08:29:30 -0700

    [ios] Introduce TabParentingGlobalObserver
    This CL introduces TabParentingGlobalObserver, which allows clients to observe
    every instance of a tab (i.e., web::WebState) being parented. Its purpose is to
    allow for replication of //chrome flows that observe the TAB_PARENTED
    notification from all sources. It should not be used for any other reason.
    
    BUG=512421
    
    Review URL: https://codereview.chromium.org/1395273008
    
    Cr-Commit-Position: refs/heads/master@{#354276}

---
+++ ios/chrome/browser/tab_parenting_global_observer.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/tab_parenting_global_observer.h"
+
+#include "base/memory/singleton.h"
+
+TabParentingGlobalObserver* TabParentingGlobalObserver::GetInstance() {
+  return base::Singleton<TabParentingGlobalObserver>::get();
+}
+
+scoped_ptr<base::CallbackList<void(web::WebState*)>::Subscription>
+TabParentingGlobalObserver::RegisterCallback(const OnTabParentedCallback& cb) {
+  return on_tab_parented_callback_list_.Add(cb);
+}
+
+void TabParentingGlobalObserver::OnTabParented(web::WebState* web_state) {
+  on_tab_parented_callback_list_.Notify(web_state);
+}
+
+TabParentingGlobalObserver::TabParentingGlobalObserver() {}
+
+TabParentingGlobalObserver::~TabParentingGlobalObserver() {}
--- ios/chrome/ios_chrome.gyp
+++ ios/chrome/ios_chrome.gyp
@@ -402,6 +402,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/sync/sync_setup_service.h',
         'browser/sync/sync_setup_service_factory.cc',
         'browser/sync/sync_setup_service_factory.h',
+        'browser/tab_parenting_global_observer.cc',
+        'browser/tab_parenting_global_observer.h',
         'browser/translate/after_translate_infobar_controller.h',
         'browser/translate/after_translate_infobar_controller.mm',
         'browser/translate/before_translate_infobar_controller.h',
