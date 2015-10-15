cc475ed8a40b56c7c7fab90d5b5d8b16d935bf2b
dbeam <dbeam@chromium.org>
2015-10-14 20:49:55 -0700

    options: Fix async font fetching crash
    R=thestig@chromium.org
    BUG=none
    TEST=less crashes with this signature
    
    Review URL: https://codereview.chromium.org/1397903004
    
    Cr-Commit-Position: refs/heads/master@{#354215}

--- chrome/browser/ui/webui/options/font_settings_handler.cc
+++ chrome/browser/ui/webui/options/font_settings_handler.cc
@@ -65,7 +65,8 @@ const char kAdvancedFontSettingsExtensionId[] =
 namespace options {
 
 FontSettingsHandler::FontSettingsHandler()
-    : extension_registry_observer_(this) {
+    : extension_registry_observer_(this),
+      weak_ptr_factory_(this) {
 }
 
 FontSettingsHandler::~FontSettingsHandler() {
@@ -191,7 +192,7 @@ void FontSettingsHandler::OnExtensionUnloaded(
 void FontSettingsHandler::HandleFetchFontsData(const base::ListValue* args) {
   content::GetFontListAsync(
       base::Bind(&FontSettingsHandler::FontsListHasLoaded,
-                 base::Unretained(this)));
+                 weak_ptr_factory_.GetWeakPtr()));
 }
 
 void FontSettingsHandler::FontsListHasLoaded(
--- chrome/browser/ui/webui/options/font_settings_handler.h
+++ chrome/browser/ui/webui/options/font_settings_handler.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_OPTIONS_FONT_SETTINGS_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_OPTIONS_FONT_SETTINGS_HANDLER_H_
 
+#include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/prefs/pref_member.h"
 #include "base/scoped_observer.h"
 #include "chrome/browser/ui/webui/options/options_ui.h"
@@ -80,6 +82,8 @@ class FontSettingsHandler : public OptionsPageUIHandler,
   ScopedObserver<extensions::ExtensionRegistry,
                  extensions::ExtensionRegistryObserver>
       extension_registry_observer_;
+
+  base::WeakPtrFactory<FontSettingsHandler> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(FontSettingsHandler);
 };
