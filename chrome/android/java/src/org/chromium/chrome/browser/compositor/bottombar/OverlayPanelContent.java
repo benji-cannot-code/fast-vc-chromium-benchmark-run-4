fde5117ed17709a87d67c3764dec528b71bd60f8
pedrosimonetti <pedrosimonetti@chromium.org>
2015-10-15 13:38:46 -0700

    [Contextual Search] Removing debugging logs.
    I forgot to remove some debugging logs before submitting
    my previous change: https://codereview.chromium.org/1403813003/
    
    This CL removes them.
    
    BUG=542785
    BUG=542786
    BUG=542790
    
    Review URL: https://codereview.chromium.org/1393803005
    
    Cr-Commit-Position: refs/heads/master@{#354342}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/OverlayPanelContent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/OverlayPanelContent.java
@@ -166,7 +166,6 @@ public class OverlayPanelContent {
             destroyContentView();
         }
 
-        System.out.println("ctxs --- OverlayPanelContent.createNewContentView");
         mContentViewCore = new ContentViewCore(mActivity);
 
         if (mContentViewClient == null) {
@@ -228,7 +227,6 @@ public class OverlayPanelContent {
      */
     private void destroyContentView() {
         if (mContentViewCore != null) {
-            System.out.println("ctxs --- OverlayPanelContent.destroyContentView");
             nativeDestroyWebContents(mNativeOverlayPanelContentPtr);
             mContentViewCore.getWebContents().destroy();
             mContentViewCore.destroy();
@@ -261,7 +259,6 @@ public class OverlayPanelContent {
      * @param url The URL that should be loaded.
      */
     public void loadUrl(String url) {
-        System.out.println("ctxs --- OverlayPanelContent.loadUrl");
         createNewContentView();
 
         if (mContentViewCore != null && mContentViewCore.getWebContents() != null) {
@@ -343,7 +340,6 @@ public class OverlayPanelContent {
             // your Web History (if enabled). For this reason, onShow() should only be called
             // when we know for sure the page will be seen by the user.
             if (mContentViewCore != null) mContentViewCore.onShow();
-            if (mContentViewCore != null) System.out.println("ctxs --- mContentViewCore.onShow");
 
             mContentDelegate.onContentViewSeen();
         } else {
