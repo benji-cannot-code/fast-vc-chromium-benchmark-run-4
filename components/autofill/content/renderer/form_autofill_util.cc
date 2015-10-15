d4582227613ef26c1c77a408b7dcf1ce45988de2
vabr <vabr@chromium.org>
2015-10-14 23:59:12 -0700

    password_form_conversion_util: Always include non-autofillable elements in SyntheticForm
    Since https://codereview.chromium.org/1404113002, SyntheticForm needs to include also hidden fields to allow us to correctly recognise GAIA reauth pages.
    
    The cited CL thus change what has been put into SyntheticForm::control_elements for the case of elements inside a <form> element.
    
    This CL does the same for unowned elements, just for the sake of consistency.
    
    BUG=543006
    
    Review URL: https://codereview.chromium.org/1404053003
    
    Cr-Commit-Position: refs/heads/master@{#354231}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -1356,8 +1356,7 @@ bool WebFormElementToFormData(
                                    extract_mask, form, field);
 }
 
-std::vector<WebFormControlElement>
-GetUnownedAutofillableFormFieldElements(
+std::vector<WebFormControlElement> GetUnownedFormFieldElements(
     const WebElementCollection& elements,
     std::vector<WebElement>* fieldsets) {
   std::vector<WebFormControlElement> unowned_fieldset_children;
@@ -1375,7 +1374,14 @@ GetUnownedAutofillableFormFieldElements(
       fieldsets->push_back(element);
     }
   }
-  return ExtractAutofillableElementsFromSet(unowned_fieldset_children);
+  return unowned_fieldset_children;
+}
+
+std::vector<WebFormControlElement> GetUnownedAutofillableFormFieldElements(
+    const WebElementCollection& elements,
+    std::vector<WebElement>* fieldsets) {
+  return ExtractAutofillableElementsFromSet(
+      GetUnownedFormFieldElements(elements, fieldsets));
 }
 
 bool UnownedCheckoutFormElementsAndFieldSetsToFormData(
