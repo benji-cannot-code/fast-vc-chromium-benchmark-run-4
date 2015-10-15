f769dba682ba0af33288f891ad8288e802fc7f67
yoshiki <yoshiki@chromium.org>
2015-10-14 22:47:10 -0700

    Fix MSAN test failure of BrowserCloseManagerWithDownloadsBrowserTest
    This patch fixes the failure in the following tests:
    - BrowserCloseManagerWithDownloadsBrowserTest/BrowserCloseManagerWithDownloadsBrowserTest.TestWithDangerousUrlDownload/0
    - BrowserCloseManagerWithDownloadsBrowserTest/BrowserCloseManagerWithDownloadsBrowserTest.TestWithDangerousUrlDownload/1
    
    Log:
    http://build.chromium.org/p/chromium.memory.fyi/builders/Linux%20ChromeOS%20MSan%20Tests/builds/4630/steps/browser_tests/logs/BrowserCloseManagerWithDownloadsBrowserTest_BrowserCloseManagerWithDownloadsBrowserTest.TestWithDangerousUrlDownload_0
    
    BUG=none
    TEST=manual tested
    TBR=robertshield@chromium.org
    
    Review URL: https://codereview.chromium.org/1402923006
    
    Cr-Commit-Position: refs/heads/master@{#354226}

--- chrome/browser/download/notification/download_item_notification.cc
+++ chrome/browser/download/notification/download_item_notification.cc
@@ -205,6 +205,7 @@ void DownloadItemNotification::OnNotificationClose() {
   if (item_ && item_->IsDangerous() && !item_->IsDone()) {
     content::RecordAction(
         UserMetricsAction("DownloadNotification.Close_Dangerous"));
+    closed_ = true;  // Should be set before cancelling the download.
     item_->Cancel(true /* by_user */);
     return;
   }
@@ -332,7 +333,7 @@ void DownloadItemNotification::Update() {
        (download_state == content::DownloadItem::INTERRUPTED &&
         previous_download_state_ != content::DownloadItem::INTERRUPTED));
 
-  if (IsNotificationVisible()) {
+  if (IsNotificationVisible() && !closed_) {
     UpdateNotificationData(popup ? UPDATE_AND_POPUP : UPDATE);
   } else {
     if (show_next_ || popup)
@@ -417,6 +418,7 @@ void DownloadItemNotification::UpdateNotificationData(
   notification_->set_buttons(notification_actions);
 
   if (type == ADD) {
+    closed_ = false;
     g_browser_process->notification_ui_manager()->
         Add(*notification_, profile());
   } else if (type == UPDATE ||
@@ -444,6 +446,7 @@ void DownloadItemNotification::UpdateNotificationData(
         Update(*notification_, profile());
   } else if (type == UPDATE_AND_POPUP) {
     CloseNotificationByNonUser();
+    closed_ = false;
     g_browser_process->notification_ui_manager()->
         Add(*notification_, profile());
   } else {
@@ -575,6 +578,7 @@ void DownloadItemNotification::DisablePopup() {
   // doesn't pop-up itself so this logic works as disabling pop-up.
   CloseNotificationByNonUser();
   notification_->set_priority(message_center::LOW_PRIORITY);
+  closed_ = false;
   g_browser_process->notification_ui_manager()->Add(*notification_, profile());
 }
 
