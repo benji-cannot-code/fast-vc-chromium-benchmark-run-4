ab230d2b99140991a83c0460e5c3999ae0393abc
zmo <zmo@chromium.org>
2015-10-15 10:23:27 -0700

    Update WebGL 2 conformance test expectations.
    BUG=483282
    TEST=GPU FYI bots
    TBR=kbr@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1405573004
    
    Cr-Commit-Position: refs/heads/master@{#354290}

--- content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
@@ -82,7 +82,6 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
 
     self.Fail('conformance2/buffers/uniform-buffers.html', bug=483282)
     self.Fail('conformance2/glsl3/array-complex-indexing.html', bug=483282)
-    self.Fail('conformance2/glsl3/frag-depth.html', bug=483282)
     self.Fail('conformance2/glsl3/invalid-default-precision.html', bug=483282)
     self.Fail('conformance2/glsl3/sequence-operator-returns-non-constant.html',
         bug=483282)
@@ -104,6 +103,8 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
         ['win'], bug=483282)
     self.Fail('conformance2/glsl3/array-in-complex-expression.html',
         ['win'], bug=483282)
+    self.Fail('conformance2/glsl3/frag-depth.html',
+        ['win'], bug=483282)
     self.Fail('conformance2/glsl3/short-circuiting-in-loop-condition.html',
         ['win'], bug=483282)
     self.Fail('conformance2/reading/read-pixels-from-fbo-test.html',
@@ -111,8 +112,6 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
     self.Fail('conformance2/renderbuffers/framebuffer-object-attachment.html',
         ['win'], bug=1082) # angle bug ID
     self.Fail('conformance2/rendering/draw-buffers.html',
-        ['win'], bug=483282)
-    self.Fail('conformance2/state/gl-get-calls.html',
         ['win'], bug=483282)
     self.Fail('conformance2/state/gl-object-get-calls.html',
         ['win'], bug=483282)
