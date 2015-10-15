029aa5bafb45bc1d6046f2b303335674d376d108
tkent <tkent@chromium.org>
2015-10-14 20:49:18 -0700

    Do not always apply LTR direction to compute text widths in multiple-fields INPUT types.
    Probably this CL has no behavior changes because they are fixed strings
    retrieved from the platform.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1395563007
    
    Cr-Commit-Position: refs/heads/master@{#354214}

--- third_party/WebKit/Source/core/html/shadow/DateTimeFieldElement.h
+++ third_party/WebKit/Source/core/html/shadow/DateTimeFieldElement.h
@@ -35,7 +35,6 @@ namespace blink {
 
 class DateComponents;
 class DateTimeFieldsState;
-class Font;
 
 // DateTimeFieldElement is base class of date time field element.
 class DateTimeFieldElement : public HTMLSpanElement {
@@ -66,7 +65,7 @@ public:
     void defaultEventHandler(Event*) override;
     virtual bool hasValue() const = 0;
     bool isDisabled() const;
-    virtual float maximumWidth(const Font&);
+    virtual float maximumWidth(const ComputedStyle&);
     virtual void populateDateTimeFieldsState(DateTimeFieldsState&) = 0;
     void removeEventHandler() { m_fieldOwner = nullptr; }
     void setDisabled();
@@ -79,6 +78,8 @@ public:
     virtual String value() const = 0;
     virtual String visibleValue() const = 0;
     DECLARE_VIRTUAL_TRACE();
+
+    static float computeTextWidth(const ComputedStyle&, const String&);
 
 protected:
     DateTimeFieldElement(Document&, FieldOwner&);
