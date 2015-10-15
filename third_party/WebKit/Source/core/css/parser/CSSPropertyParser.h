df3a896aee065ae97b28fdf8b21feefaec4409cd
rob.buis <rob.buis@samsung.com>
2015-10-15 10:44:47 -0700

    Move touch-action property into CSSPropertyParser
    Move touch-action property handling from LegacyCSSPropertyParser
    into CSSPropertyParser.
    
    BUG=499780
    
    Review URL: https://codereview.chromium.org/1406763002
    
    Cr-Commit-Position: refs/heads/master@{#354296}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
@@ -259,8 +259,6 @@ private:
 
     PassRefPtrWillBeRawPtr<CSSValue> parseTextEmphasisStyle();
 
-    PassRefPtrWillBeRawPtr<CSSValue> parseTouchAction();
-
     PassRefPtrWillBeRawPtr<CSSValue> parseTextDecoration();
 
     bool parseCalculation(CSSParserValue*, ValueRange);
