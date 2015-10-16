120c4793f08beb1fdc1709b40a075e32d2f64362
sbc <sbc@chromium.org>
2015-10-16 14:38:22 -0700

    [NaCl SDK] Add support for SO_TYPE to getsockopt
    CQ_EXTRA_TRYBOTS=tryserver.chromium.linux:linux_nacl_sdk;tryserver.chromium.mac:mac_nacl_sdk;tryserver.chromium.win:win_nacl_sdk
    BUG=447869
    
    Review URL: https://codereview.chromium.org/1413683002
    
    Cr-Commit-Position: refs/heads/master@{#354608}

--- native_client_sdk/src/libraries/nacl_io/socket/unix_node.cc
+++ native_client_sdk/src/libraries/nacl_io/socket/unix_node.cc
@@ -16,13 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace nacl_io {
 
 UnixNode::UnixNode(Filesystem* filesystem)
-    : SocketNode(filesystem),
+    : SocketNode(SOCK_STREAM, filesystem),
       emitter_(UnixEventEmitter::MakeUnixEventEmitter(65536)) {
   emitter_->AttachStream(this);
 }
 
 UnixNode::UnixNode(Filesystem* filesystem, const UnixNode& peer)
-    : SocketNode(filesystem), emitter_(peer.emitter_->GetPeerEmitter()) {
+    : SocketNode(SOCK_STREAM, filesystem),
+      emitter_(peer.emitter_->GetPeerEmitter()) {
   emitter_->AttachStream(this);
 }
 
