931d59f9b2066a8dad9b92d62a58347a9db5861d
boliu <boliu@chromium.org>
2015-10-16 12:16:08 -0700

    aw: Add debug logging for GC test
    Timeout do not reproduce locally, so add debug logging to help debug on
    bot. Logs are only in the test itself, or behind --v=2 switch.
    
    BUG=544098
    
    Review URL: https://codereview.chromium.org/1407323002
    
    Cr-Commit-Position: refs/heads/master@{#354558}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsGarbageCollectionTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsGarbageCollectionTest.java
@@ -10,11 +10,14 @@ import android.content.ContextWrapper;
 import android.os.Build;
 import android.test.FlakyTest;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.test.suitebuilder.annotation.SmallTest;
 
 import static org.chromium.base.test.util.ScalableTimeout.scaleTimeout;
 
 import org.chromium.android_webview.AwContents;
+import org.chromium.base.Log;
 import org.chromium.base.annotations.SuppressFBWarnings;
+import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.content.browser.test.util.Criteria;
@@ -29,6 +32,8 @@ import java.util.concurrent.Callable;
  */
 @MinAndroidSdkLevel(Build.VERSION_CODES.KITKAT)
 public class AwContentsGarbageCollectionTest extends AwTestBase {
+    private static final String TAG = "AwGcTest";
+
     // The system retains a strong ref to the last focused view (in InputMethodManager)
     // so allow for 1 'leaked' instance.
     private static final int MAX_IDLE_INSTANCES = 1;
@@ -85,13 +90,13 @@ public class AwContentsGarbageCollectionTest extends AwTestBase {
         }
     }
 
-    /*
     @SmallTest
     @Feature({"AndroidWebView"})
-    Bug: https://crbug.com/544098
-    */
-    @FlakyTest
+    @CommandLineFlags.Add("v=2")
+    // Enabled to obtain debug logging. Feel free to disable after first failure.
+    // See https://crbug.com/544098
     public void testCreateAndGcOneTime() throws Throwable {
+        Log.d(TAG, "testCreateAndGcOneTime start");
         gcAndCheckAllAwContentsDestroyed();
 
         TestAwContentsClient client = new TestAwContentsClient();
@@ -100,10 +105,13 @@ public class AwContentsGarbageCollectionTest extends AwTestBase {
             containerViews[i] = createAwTestContainerViewOnMainSync(client);
             loadUrlAsync(containerViews[i].getAwContents(), "about:blank");
         }
+        Log.d(TAG, "testCreateAndGcOneTime create views done");
 
         containerViews = null;
         removeAllViews();
+        Log.d(TAG, "testCreateAndGcOneTime remove views done");
         gcAndCheckAllAwContentsDestroyed();
+        Log.d(TAG, "testCreateAndGcOneTime end");
     }
 
     /*
@@ -193,6 +201,7 @@ public class AwContentsGarbageCollectionTest extends AwTestBase {
     }
 
     private void gcAndCheckAllAwContentsDestroyed() throws InterruptedException {
+        Log.d(TAG, "Cause GC");
         Runtime.getRuntime().gc();
 
         Criteria criteria = new Criteria() {
@@ -202,7 +211,9 @@ public class AwContentsGarbageCollectionTest extends AwTestBase {
                     return runTestOnUiThreadAndGetResult(new Callable<Boolean>() {
                         @Override
                         public Boolean call() {
-                            return AwContents.getNativeInstanceCount() <= MAX_IDLE_INSTANCES;
+                            int count = AwContents.getNativeInstanceCount();
+                            Log.d(TAG, "NativeInstanceCount = " + count);
+                            return count <= MAX_IDLE_INSTANCES;
                         }
                     });
                 } catch (Exception e) {
@@ -220,6 +231,7 @@ public class AwContentsGarbageCollectionTest extends AwTestBase {
             if (CriteriaHelper.pollForCriteria(criteria, timeoutBetweenGcMs, CHECK_INTERVAL)) {
                 break;
             } else {
+                Log.d(TAG, "Cause GC");
                 Runtime.getRuntime().gc();
             }
         }
