45f27df2eccfbad9f8c08414cc29db830fe5e118
johnme <johnme@chromium.org>
2015-10-16 06:13:02 -0700

    Mark 3 tests from AwContentsGarbageCollectionTest flaky
    BUG=544098
    NOTRY=true
    TBR=boliu@chromium.org
    
    Review URL: https://codereview.chromium.org/1409073003
    
    Cr-Commit-Position: refs/heads/master@{#354503}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsGarbageCollectionTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsGarbageCollectionTest.java
@@ -8,8 +8,8 @@ package org.chromium.android_webview.test;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.os.Build;
+import android.test.FlakyTest;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.test.suitebuilder.annotation.SmallTest;
 
 import static org.chromium.base.test.util.ScalableTimeout.scaleTimeout;
 
@@ -85,8 +85,12 @@ public class AwContentsGarbageCollectionTest extends AwTestBase {
         }
     }
 
+    /*
     @SmallTest
     @Feature({"AndroidWebView"})
+    Bug: https://crbug.com/544098
+    */
+    @FlakyTest
     public void testCreateAndGcOneTime() throws Throwable {
         gcAndCheckAllAwContentsDestroyed();
 
@@ -102,8 +106,12 @@ public class AwContentsGarbageCollectionTest extends AwTestBase {
         gcAndCheckAllAwContentsDestroyed();
     }
 
+    /*
     @SmallTest
     @Feature({"AndroidWebView"})
+    Bug: https://crbug.com/544098
+    */
+    @FlakyTest
     public void testReferenceFromClient() throws Throwable {
         gcAndCheckAllAwContentsDestroyed();
 
@@ -119,8 +127,12 @@ public class AwContentsGarbageCollectionTest extends AwTestBase {
         gcAndCheckAllAwContentsDestroyed();
     }
 
+    /*
     @SmallTest
     @Feature({"AndroidWebView"})
+    Bug: https://crbug.com/544098
+    */
+    @FlakyTest
     public void testReferenceFromContext() throws Throwable {
         gcAndCheckAllAwContentsDestroyed();
 
