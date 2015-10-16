8efc504814ef0844150c1ee56a75e34a3944714d
kkimlabs <kkimlabs@chromium.org>
2015-10-16 11:23:53 -0700

    [Enhanced Bookmarks] Hide keyboard when the search is done.
    BUG=523199
    
    Review URL: https://codereview.chromium.org/1407073003
    
    Cr-Commit-Position: refs/heads/master@{#354553}

--- chrome/android/java/src/org/chromium/chrome/browser/enhancedbookmarks/EnhancedBookmarkSearchView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/enhancedbookmarks/EnhancedBookmarkSearchView.java
@@ -284,6 +284,8 @@ public class EnhancedBookmarkSearchView extends LinearLayout implements OnItemCl
     @Override
     public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
         if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+            UiUtils.hideKeyboard(v);
+
             // History is saved either when the user clicks search button or a search result is
             // clicked.
             saveSearchHistory();
