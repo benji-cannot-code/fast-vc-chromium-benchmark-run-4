2baa22717bb7f54914f45c9e8eb708069d30b17e
nasko <nasko@chromium.org>
2015-10-15 18:48:45 -0700

    Ignore InputMsg IPCs if RenderView is in swapped out state.
    InputMsg IPCs are dispatched to the compositor thread when they arrive
    in the renderer process. If not handled there, they are sent back to the
    main thread. This behavior allows for IPCs that are processed on the
    main thread to come in order different than the browser process has sent
    them in.
    
    The crash in 541578 occurs when InputMsg_SetFocus is sent right before
    FrameMsg_SwapOut. On the renderer side, the InputMsg_SetFocus message is
    sent to the compositor thread and in the meantime the FrameMsg_SwapOut
    message is processed on the main thread. After swapping out is complete
    the top-level frame for the swapped out RenderView becomes a RemoteFrame
    when swapped out RenderFrameHost usage is disabled. This causes the
    FocusController to return RemoteFrame and that violates assumptions
    elsewhere in code that all focused frames are LocalFrame.
    
    Overall, swapped out RenderView should not be processing input messages
    at all, so this CL implements this restriction.
    
    BUG=357747, 541578
    
    Review URL: https://codereview.chromium.org/1408873002
    
    Cr-Commit-Position: refs/heads/master@{#354426}

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -1277,6 +1277,11 @@ bool RenderViewImpl::OnMessageReceived(const IPC::Message& message) {
   if (main_frame && main_frame->isWebLocalFrame())
     GetContentClient()->SetActiveURL(main_frame->document().url());
 
+  // Input IPC messages must not be processed if the RenderView is in
+  // swapped out state.
+  if (is_swapped_out_ && IPC_MESSAGE_ID_CLASS(message.type()) == InputMsgStart)
+    return false;
+
   base::ObserverListBase<RenderViewObserver>::Iterator it(&observers_);
   RenderViewObserver* observer;
   while ((observer = it.GetNext()) != NULL)
@@ -3067,6 +3072,9 @@ GURL RenderViewImpl::GetURLForGraphicsContext3D() {
 }
 
 void RenderViewImpl::OnSetFocus(bool enable) {
+  // This message must always be received when the main frame is a
+  // WebLocalFrame.
+  CHECK(webview()->mainFrame()->isWebLocalFrame());
   RenderWidget::OnSetFocus(enable);
 
 #if defined(ENABLE_PLUGINS)
