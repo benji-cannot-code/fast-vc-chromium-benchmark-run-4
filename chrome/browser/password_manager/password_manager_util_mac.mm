db516a71233c2925b5d27d1bc3943bfcb512c136
vabr <vabr@chromium.org>
2015-10-16 01:48:10 -0700

    [Mac] Use user-level reauth to show passwords
    Currently, Chrome requires an admin's password to show stored passwords in about:settings/passwords. This is not intentional, and is caused by Chrome using a custom "right" name (<chrome's ID>.show-passwords), which somehow by default is associated with admin-level authorization.
    
    Instead, Chrome should request a session-user-level "right".
    
    One option to do so is to use an existing "right", e.g., system.login.screensaver. That one is exactly expressing the original intention of the reauth -- to let the viewing user prove they are the legitimate viewer in the very same way, as screen lock checks the returning user.
    
    If we want to keep the customised right, we need to first define it with the AuthorizationDB, using the flag kAuthorizationRuleAuthenticateAsSessionUser.
    
    The CL currently implements the first option, not so much because it is easier, but rather because copying screen lock was the original intention. Screenshot at http://crbug.com/387120#c11.
    
    This CL does not add any tests. First, there is no good way to check whether admin or session-user reauth is requested. Second, this is not adding any logic, just tweaking the system call.
    
    R=mark@chromium.org
    BUG=387120
    
    Review URL: https://codereview.chromium.org/1394373004
    
    Cr-Commit-Position: refs/heads/master@{#354476}

--- chrome/browser/password_manager/password_manager_util_mac.mm
+++ chrome/browser/password_manager/password_manager_util_mac.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/mac/authorization_util.h"
-#include "base/mac/bundle_locations.h"
 #include "base/mac/foundation_util.h"
 #include "base/mac/scoped_authorizationref.h"
 #include "chrome/grit/chromium_strings.h"
@@ -20,12 +19,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace password_manager_util_mac {
 
 bool AuthenticateUser() {
-  NSString* identifier = [base::mac::MainBundle() bundleIdentifier];
-  AuthorizationString name =
-      [[identifier stringByAppendingString:@".show-passwords"] UTF8String];
-  AuthorizationItem right_items[] = {
-    {name, 0, NULL, 0}
-  };
+  // Use the system-defined "system.login.screensaver" access right rather than
+  // creating our own. The screensaver does exactly the same check we need --
+  // verifying whether the legitimate session user is present. If we needed to
+  // create a separate access right, we would have to define it with the
+  // AuthorizationDB, using the flag
+  // kAuthorizationRuleAuthenticateAsSessionUser, to ensure that the session
+  // user password, as opposed to an admin's password, is required.
+  AuthorizationItem right_items[] = {{"system.login.screensaver", 0, NULL, 0}};
   AuthorizationRights rights = {arraysize(right_items), right_items};
 
   NSString* prompt =
