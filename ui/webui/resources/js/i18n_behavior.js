bae20fe816753f96fa61d2bd773280c0eba02c55
fhorschig <fhorschig@chromium.org>
2015-10-16 00:39:22 -0700

    Extract i18n behavior from settings.
    The i18n behavior previously defined for md-settings seems to be usefull
    for other CLs (e.g. https://codereview.chromium.org/1371073003/).
    Therefore, it is moved to resources/{js,html}.
    
    BUG=134849
    
    Review URL: https://codereview.chromium.org/1404593002
    
    Cr-Commit-Position: refs/heads/master@{#354464}

--- ui/webui/resources/js/i18n_behavior.js
+++ ui/webui/resources/js/i18n_behavior.js
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *     I18nBehavior,
  *   ],
  *
- * @group Chrome Settings Elements
+ * @group Chrome UI Behavior
  */
 
 var I18nBehavior = {
