9955a39fbdca885b7b3a8e2eb348dfdadc7cc40c
vabr <vabr@chromium.org>
2015-10-16 08:03:09 -0700

    Address linter comments in PasswordStoreFactory
    This is a clean-up after git cl lint told me on another CL of mine:
    
    chrome/browser/password_manager/password_store_factory.cc:204:  Consider using rand_r(...) instead of rand(...) for improved thread safety.  [runtime/threadsafe_fn] [2]
    chrome/browser/password_manager/password_store_factory.cc:376:  Add #include <string> for string  [build/include_what_you_use] [4]
    chrome/browser/password_manager/password_store_factory.h:98:  Add #include <string> for string  [build/include_what_you_use] [4]
    
    The rand_r is only available on POSIX, but that code containing rand() is #ifdefed to USE_X11, which should include POSIX. Strictly speaking we probably don't need the reentrancy guarantees of rand_r, but I also don't see how it would hurt.
    
    BUG=514183
    
    Review URL: https://codereview.chromium.org/1413603002
    
    Cr-Commit-Position: refs/heads/master@{#354512}

--- chrome/browser/password_manager/password_store_factory.cc
+++ chrome/browser/password_manager/password_store_factory.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/prefs/pref_service.h"
+#include "base/rand_util.h"
 #include "base/thread_task_runner_handle.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
@@ -213,10 +214,9 @@ LocalProfileId PasswordStoreFactory::GetLocalProfileId(
     // that it would be repeated twice on a single machine. It is still possible
     // for that to occur though, so the potential results of it actually
     // happening should be considered when using this value.
-    static const LocalProfileId kLocalProfileIdMask =
-        static_cast<LocalProfileId>((1 << 24) - 1);
+    static const int kLocalProfileIdMask = (1 << 24) - 1;
     do {
-      id = rand() & kLocalProfileIdMask;
+      id = base::RandInt(0, kLocalProfileIdMask);
       // TODO(mdm): scan other profiles to make sure they are not using this id?
     } while (id == kInvalidLocalProfileId);
     prefs->SetInteger(password_manager::prefs::kLocalProfileId, id);
