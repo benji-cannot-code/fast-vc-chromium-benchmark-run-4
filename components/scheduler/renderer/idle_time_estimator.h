973fc45594838c18ecdb268bac556486599daa53
alexclarke <alexclarke@chromium.org>
2015-10-16 06:29:11 -0700

    Make IdleTimeEstimator ignore nested tasks
    Unfortunately you can run a modal dialog inside a RAF so nesting of
    compositor tasks is possible.  Like other places in the scheduler we
    ignore nested tasks for timing computations.
    
    BUG=543894
    
    Review URL: https://codereview.chromium.org/1409073002
    
    Cr-Commit-Position: refs/heads/master@{#354505}

--- components/scheduler/renderer/idle_time_estimator.h
+++ components/scheduler/renderer/idle_time_estimator.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace scheduler {
 
-// Estimates how much idle time there is available.
+// Estimates how much idle time there is available.  Ignores nested tasks.
 class SCHEDULER_EXPORT IdleTimeEstimator
     : public base::MessageLoop::TaskObserver {
  public:
