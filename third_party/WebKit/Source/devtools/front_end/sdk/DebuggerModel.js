c9ffaa7738db0c0c5c78dd696c0354f7467b417c
kozyatinskiy <kozyatinskiy@chromium.org>
2015-10-16 21:44:46 -0700

    [DevTools] Added function name for closure scopes
    Show function name for closure on pause at breakpoint.
    
    BUG=493156
    TBR=yurys@chromium.org
    
    Review URL: https://codereview.chromium.org/1415473002
    
    Cr-Commit-Position: refs/heads/master@{#354673}

--- third_party/WebKit/Source/devtools/front_end/sdk/DebuggerModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/DebuggerModel.js
@@ -1313,6 +1313,7 @@ WebInspector.DebuggerModel.Scope = function(callFrame, ordinal)
     this._callFrame = callFrame;
     this._payload = callFrame._payload.scopeChain[ordinal];
     this._type = this._payload.type;
+    this._name = this._payload.name;
     this._ordinal = ordinal;
 }
 
@@ -1323,6 +1324,14 @@ WebInspector.DebuggerModel.Scope.prototype = {
     type: function()
     {
         return this._type;
+    },
+
+    /**
+     * @return {string|undefined}
+     */
+    name: function()
+    {
+        return this._name;
     },
 
     /**
