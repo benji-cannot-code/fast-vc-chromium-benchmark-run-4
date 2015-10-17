0987453950366207c2ba977927dc987a1939ea47
wkorman <wkorman@chromium.org>
2015-10-16 18:56:00 -0700

    Lists: Render marker text closer to list items.
    More closely matches rendering behavior of other browsers.
    
    BUG=472084
    
    Review URL: https://codereview.chromium.org/1407003005
    
    Cr-Commit-Position: refs/heads/master@{#354660}

--- third_party/WebKit/Source/core/layout/LayoutListMarker.h
+++ third_party/WebKit/Source/core/layout/LayoutListMarker.h
@@ -95,6 +95,7 @@ private:
     LayoutRect selectionRectForPaintInvalidation(const LayoutBoxModelObject* paintInvalidationContainer) const override;
     bool canBeSelectionLeaf() const override { return true; }
 
+    LayoutUnit getWidthOfTextWithSuffix() const;
     void updateMargins();
     void updateContent();
 
