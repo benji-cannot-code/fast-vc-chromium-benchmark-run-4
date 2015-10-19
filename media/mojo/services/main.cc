8e3a43f44412e1675f1f7c7fb4c355929781ffb1
xhwang <xhwang@chromium.org>
2015-10-19 15:19:13 -0700

    media: Selectively call InitLogging() in MojoMediaApplication::Initialize().
    InitLogging() should not be called more than twice [1]. If MojoMediaApplication
    is hosted in a process where logging is already initialized twice, the
    InitLogging() call could cause the CHECK to fire [2].
    
    Unfortunately there's no way to check how many times InitLogging() has been
    called.
    
    This CL adds an option for MojoMediaApplication to decide whether to enable
    logging. This decision can be made during application registration time.
    
    [1] https://code.google.com/p/chromium/codesearch#chromium/src/base/logging.h&l=225
    [2] https://code.google.com/p/chromium/codesearch#chromium/src/base/logging.cc&rcl=1444980234&l=347
    
    BUG=545110
    
    Review URL: https://codereview.chromium.org/1415713003
    
    Cr-Commit-Position: refs/heads/master@{#354885}

--- media/mojo/services/main.cc
+++ media/mojo/services/main.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/mojo/src/mojo/public/c/system/main.h"
 
 MojoResult MojoMain(MojoHandle mojo_handle) {
-  mojo::ApplicationRunner runner(new media::MojoMediaApplication());
+  // Create MojoMediaApplication and enable logging.
+  mojo::ApplicationRunner runner(new media::MojoMediaApplication(true));
   return runner.Run(mojo_handle);
 }
--- media/mojo/services/mojo_media_application.h
+++ media/mojo/services/mojo_media_application.h
@@ -19,7 +19,7 @@ class MojoMediaApplication
  public:
   static scoped_ptr<mojo::ApplicationDelegate> CreateApp();
 
-  MojoMediaApplication();
+  explicit MojoMediaApplication(bool enable_logging);
   ~MojoMediaApplication() final;
 
  private:
@@ -32,6 +32,7 @@ class MojoMediaApplication
   void Create(mojo::ApplicationConnection* connection,
               mojo::InterfaceRequest<interfaces::ServiceFactory> request) final;
 
+  bool enable_logging_;
   mojo::ApplicationImpl* app_impl_;
   scoped_refptr<MediaLog> media_log_;
 };
