3df6a8c126187c2fc1d8d2ac4918add8e8416681
miguelg <miguelg@chromium.org>
2015-10-19 02:35:05 -0700

    Revert of [Android] Call Tab.hide() when the activity is stopped (patchset #6 id:140001 of https://codereview.chromium.org/1387433003/ )
    Reason for revert:
    Spoeculative revert, I think this broke https://uberchromegw.corp.google.com/i/internal.client.clank/builders/instrumentation-occam-svelte-clankium downstream.
    
    Original issue's description:
    > [Android] Call Tab.hide() when the activity is stopped
    >
    > There's some residual bookkeeping that relies on the Tab.hide() signal
    > being called when Chrome is backgrounded. Restore that call when
    > the activity is stopped.
    >
    > BUG=434401
    >
    > Committed: https://crrev.com/d315c171ac0594a730185b37b804cf0d34fb44e2
    > Cr-Commit-Position: refs/heads/master@{#354532}
    
    TBR=tedchoc@chromium.org,jdduke@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=434401
    
    Review URL: https://codereview.chromium.org/1417513002
    
    Cr-Commit-Position: refs/heads/master@{#354746}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -320,8 +320,6 @@ RenderWidgetHostViewAndroid::RenderWidgetHostViewAndroid(
     : host_(widget_host),
       outstanding_vsync_requests_(0),
       is_showing_(!widget_host->is_hidden()),
-      is_window_visible_(true),
-      is_window_activity_started_(true),
       content_view_core_(nullptr),
       content_view_core_window_android_(nullptr),
       ime_adapter_android_(this),
@@ -534,7 +532,10 @@ void RenderWidgetHostViewAndroid::Hide() {
     return;
 
   is_showing_ = false;
-  HideInternal();
+
+  bool hide_frontbuffer = true;
+  bool stop_observing_root_window = true;
+  HideInternal(hide_frontbuffer, stop_observing_root_window);
 }
 
 bool RenderWidgetHostViewAndroid::IsShowing() {
@@ -1360,10 +1361,7 @@ void RenderWidgetHostViewAndroid::OnFrameMetadataUpdated(
 }
 
 void RenderWidgetHostViewAndroid::ShowInternal() {
-  bool show = is_showing_ && is_window_activity_started_ && is_window_visible_;
-  if (!show)
-    return;
-
+  DCHECK(is_showing_);
   if (!host_ || !host_->is_hidden())
     return;
 
@@ -1383,20 +1381,9 @@ void RenderWidgetHostViewAndroid::ShowInternal() {
   }
 }
 
-void RenderWidgetHostViewAndroid::HideInternal() {
-  DCHECK(!is_showing_ || !is_window_activity_started_ || !is_window_visible_)
-      << "Hide called when the widget should be shown.";
-
-  // Only preserve the frontbuffer if the activity was stopped while the
-  // window is still visible. This avoids visual artificts when transitioning
-  // between activities.
-  bool hide_frontbuffer = is_window_activity_started_ || !is_window_visible_;
-
-  // Only stop observing the root window if the widget has been explicitly
-  // hidden and the frontbuffer is being cleared. This allows window visibility
-  // notifications to eventually clear the frontbuffer.
-  bool stop_observing_root_window = !is_showing_ && hide_frontbuffer;
-
+void RenderWidgetHostViewAndroid::HideInternal(
+    bool hide_frontbuffer,
+    bool stop_observing_root_window) {
   if (hide_frontbuffer) {
     if (layer_.get() && locks_on_frame_count_ == 0)
       layer_->SetHideLayerAndSubtree(true);
@@ -1404,10 +1391,8 @@ void RenderWidgetHostViewAndroid::HideInternal() {
     frame_evictor_->SetVisible(false);
   }
 
-  if (stop_observing_root_window) {
-    DCHECK(!is_showing_);
+  if (stop_observing_root_window)
     StopObservingRootWindow();
-  }
 
   if (!host_ || host_->is_hidden())
     return;
@@ -1482,9 +1467,6 @@ void RenderWidgetHostViewAndroid::StopObservingRootWindow() {
   if (!observing_root_window_)
     return;
 
-  // Reset window state variables to their defaults.
-  is_window_activity_started_ = true;
-  is_window_visible_ = true;
   observing_root_window_ = false;
   content_view_core_window_android_->RemoveObserver(this);
 }
@@ -1841,19 +1823,14 @@ void RenderWidgetHostViewAndroid::OnCompositingDidCommit() {
 }
 
 void RenderWidgetHostViewAndroid::OnRootWindowVisibilityChanged(bool visible) {
-  TRACE_EVENT1("browser",
-               "RenderWidgetHostViewAndroid::OnRootWindowVisibilityChanged",
-               "visible", visible);
-  DCHECK(observing_root_window_);
-  if (is_window_visible_ == visible)
-    return;
-
-  is_window_visible_ = visible;
-
-  if (visible)
+  DCHECK(is_showing_);
+  if (visible) {
     ShowInternal();
-  else
-    HideInternal();
+  } else {
+    bool hide_frontbuffer = true;
+    bool stop_observing_root_window = false;
+    HideInternal(hide_frontbuffer, stop_observing_root_window);
+  }
 }
 
 void RenderWidgetHostViewAndroid::OnAttachCompositor() {
@@ -1900,15 +1877,15 @@ void RenderWidgetHostViewAndroid::OnAnimate(base::TimeTicks begin_frame_time) {
 
 void RenderWidgetHostViewAndroid::OnActivityStopped() {
   TRACE_EVENT0("browser", "RenderWidgetHostViewAndroid::OnActivityStopped");
-  DCHECK(observing_root_window_);
-  is_window_activity_started_ = false;
-  HideInternal();
+  DCHECK(is_showing_);
+  bool hide_frontbuffer = false;
+  bool stop_observing_root_window = false;
+  HideInternal(hide_frontbuffer, stop_observing_root_window);
 }
 
 void RenderWidgetHostViewAndroid::OnActivityStarted() {
   TRACE_EVENT0("browser", "RenderWidgetHostViewAndroid::OnActivityStarted");
-  DCHECK(observing_root_window_);
-  is_window_activity_started_ = true;
+  DCHECK(is_showing_);
   ShowInternal();
 }
 
