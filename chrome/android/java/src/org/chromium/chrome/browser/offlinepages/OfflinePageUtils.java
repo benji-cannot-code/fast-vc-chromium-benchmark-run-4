c3b33cb59888df434f5c569ea87deddeb3e66ae3
fgorski <fgorski@chromium.org>
2015-10-19 15:12:02 -0700

    [Offline pages] Requesting for runtime storage permission
    On Marshmallow offline pages won't save if Chromium does not have
    storage permission. Requesting a runtime permission before saving.
    
    BUG=539954
    
    Review URL: https://codereview.chromium.org/1409253002
    
    Cr-Commit-Position: refs/heads/master@{#354884}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/OfflinePageUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/OfflinePageUtils.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.offlinepages;
 
+import android.Manifest.permission;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -18,8 +19,10 @@ import org.chromium.chrome.browser.snackbar.Snackbar;
 import org.chromium.chrome.browser.snackbar.SnackbarManager.SnackbarController;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.ui.base.PageTransition;
+import org.chromium.ui.base.WindowAndroid.PermissionCallback;
 
 /**
  * A class holding static util functions for offline pages.
@@ -106,6 +109,28 @@ public class OfflinePageUtils {
         } else {
             showOfflineSnackbar(activity, tab.getId(), save);
         }
+    }
+
+    /**
+     * Returns whether file access is allowed.
+     *
+     * @param view The ContentViewCore to access the file system.
+     * @return true if allowed, or false otherwise.
+     */
+    public static boolean hasFileAccessPermission(ContentViewCore view) {
+        return view.getWindowAndroid().hasPermission(permission.WRITE_EXTERNAL_STORAGE);
+    }
+
+    /**
+     * Called to prompt user with the file access permission.
+     *
+     * @param view The ContentViewCore to access the file system.
+     * @param callback Callback for the permission request.
+     */
+    public static void requestFileAccessPermission(
+            ContentViewCore view, PermissionCallback callback) {
+        view.getWindowAndroid().requestPermissions(
+                new String[] {android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, callback);
     }
 
     /**
