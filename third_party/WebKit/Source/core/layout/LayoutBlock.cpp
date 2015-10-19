1eb9a8a838a2a16ece176f7945bfecaa36641d5a
mstensho <mstensho@opera.com>
2015-10-19 12:08:27 -0700

    Need to reposition an out-of-flow object *before* re-paginating it.
    https://codereview.chromium.org/1343163005 removed code that was actually
    needed. Instead of simply reverting the necessary parts, I rewrote them.
    The old code called updateLogicalHeight() before layout, when all it wanted to
    do was set the logical top. This made me a bit uneasy, because that could in
    theory prevent the height change from being detected during layout() (and
    LayoutBlockFlow::layoutBlockFlow() would for instance fail to set
    |relayoutChildren| before calling layoutPositionedObjects()). Furthermore, it
    used to check if the child establishes a writing mode root, and update the
    logical *width* instead of height in that case. That's unnecessary and
    potentially harmful (preventing the width change from being detected during
    layout()). If the child establishes a new writing mode, it becomes opaque as
    far as pagination is concerned, so no need to handle this.
    
    BUG=544783
    R=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/1415623002
    
    Cr-Commit-Position: refs/heads/master@{#354815}

--- third_party/WebKit/Source/core/layout/LayoutBlock.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlock.cpp
@@ -1194,6 +1194,8 @@ void LayoutBlock::layoutPositionedObjects(bool relayoutChildren, PositionedLayou
     if (!positionedDescendants)
         return;
 
+    bool isPaginated = view()->layoutState()->isPaginated();
+
     for (auto* positionedObject : *positionedDescendants) {
         positionedObject->setMayNeedPaintInvalidation();
 
@@ -1214,6 +1216,20 @@ void LayoutBlock::layoutPositionedObjects(bool relayoutChildren, PositionedLayou
         if (relayoutChildren && positionedObject->needsPreferredWidthsRecalculation())
             positionedObject->setPreferredLogicalWidthsDirty(MarkOnlyThis);
 
+        LayoutUnit logicalTopEstimate;
+        bool needsBlockDirectionLocationSetBeforeLayout = isPaginated && !positionedObject->isUnsplittableForPagination();
+        if (needsBlockDirectionLocationSetBeforeLayout) {
+            // Out-of-flow objects are normally positioned after layout (while in-flow objects are
+            // positioned before layout). If the child object is paginated in the same context as
+            // we are, estimate its logical top now. We need to know this up-front, to correctly
+            // evaluate if we need to mark for relayout, and, if our estimate is correct, we'll
+            // even be able to insert correct pagination struts on the first attempt.
+            LogicalExtentComputedValues computedValues;
+            positionedObject->computeLogicalHeight(positionedObject->logicalHeight(), positionedObject->logicalTop(), computedValues);
+            logicalTopEstimate = computedValues.m_position;
+            positionedObject->setLogicalTop(logicalTopEstimate);
+        }
+
         if (!positionedObject->needsLayout())
             positionedObject->markForPaginationRelayoutIfNeeded(layoutScope);
 
@@ -1223,6 +1239,10 @@ void LayoutBlock::layoutPositionedObjects(bool relayoutChildren, PositionedLayou
             positionedObject->setNeedsLayout(LayoutInvalidationReason::AncestorMoved, MarkOnlyThis);
 
         positionedObject->layoutIfNeeded();
+
+        // Lay out again if our estimate was wrong.
+        if (needsBlockDirectionLocationSetBeforeLayout && logicalTopEstimate != logicalTopForChild(*positionedObject))
+            positionedObject->forceChildLayout();
     }
 }
 
