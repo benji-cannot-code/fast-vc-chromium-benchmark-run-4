76f6dac3b2129ee5ff0a48f5ae786568e5b6855b
haraken <haraken@chromium.org>
2015-10-18 22:25:04 -0700

    Oilpan: Add more static/runtime ASSERTs about ShouldBeTraced
    BUG=475801
    
    Review URL: https://codereview.chromium.org/1407143003
    
    Cr-Commit-Position: refs/heads/master@{#354726}

--- third_party/WebKit/Source/platform/heap/TraceTraits.h
+++ third_party/WebKit/Source/platform/heap/TraceTraits.h
@@ -102,7 +102,10 @@ struct TraceIfEnabled;
 template<typename T>
 struct TraceIfEnabled<T, false>  {
     template<typename VisitorDispatcher>
-    static void trace(VisitorDispatcher, T&) { }
+    static void trace(VisitorDispatcher, T&)
+    {
+        static_assert(!WTF::NeedsTracing<T>::value, "T should not be traced");
+    }
 };
 
 template<typename T>
@@ -110,6 +113,7 @@ struct TraceIfEnabled<T, true> {
     template<typename VisitorDispatcher>
     static void trace(VisitorDispatcher visitor, T& t)
     {
+        static_assert(WTF::NeedsTracing<T>::value || WTF::IsWeak<T>::value, "T should be traced");
         visitor->trace(t);
     }
 };
@@ -119,7 +123,11 @@ template<bool needsTracing, WTF::WeakHandlingFlag weakHandlingFlag, WTF::ShouldW
 template<WTF::ShouldWeakPointersBeMarkedStrongly strongify, typename T, typename Traits>
 struct TraceCollectionIfEnabled<false, WTF::NoWeakHandlingInCollections, strongify, T, Traits> {
     template<typename VisitorDispatcher>
-    static bool trace(VisitorDispatcher, T&) { return false; }
+    static bool trace(VisitorDispatcher, T&)
+    {
+        static_assert(!WTF::NeedsTracingTrait<Traits>::value, "Traits should not be traced");
+        return false;
+    }
 };
 
 template<bool needsTracing, WTF::WeakHandlingFlag weakHandlingFlag, WTF::ShouldWeakPointersBeMarkedStrongly strongify, typename T, typename Traits>
@@ -127,6 +135,7 @@ struct TraceCollectionIfEnabled {
     template<typename VisitorDispatcher>
     static bool trace(VisitorDispatcher visitor, T& t)
     {
+        static_assert(WTF::NeedsTracingTrait<Traits>::value || weakHandlingFlag == WTF::WeakHandlingInCollections, "Traits should be traced");
         return WTF::TraceInCollectionTrait<weakHandlingFlag, strongify, T, Traits>::trace(visitor, t);
     }
 };
@@ -160,6 +169,7 @@ template<typename T> class TraceTrait<const T> : public TraceTrait<T> { };
 template<typename T>
 void TraceTrait<T>::trace(Visitor* visitor, void* self)
 {
+    static_assert(WTF::NeedsTracing<T>::value || WTF::IsWeak<T>::value, "T should be traced");
     if (visitor->markingMode() == Visitor::GlobalMarking) {
         // Switch to inlined global marking dispatch.
         static_cast<T*>(self)->trace(InlinedGlobalMarkingVisitor(visitor));
@@ -171,6 +181,7 @@ void TraceTrait<T>::trace(Visitor* visitor, void* self)
 template<typename T>
 void TraceTrait<T>::trace(InlinedGlobalMarkingVisitor visitor, void* self)
 {
+    static_assert(WTF::NeedsTracing<T>::value || WTF::IsWeak<T>::value, "T should be traced");
     static_cast<T*>(self)->trace(visitor);
 }
 
@@ -339,6 +350,7 @@ struct TraceInCollectionTrait<NoWeakHandlingInCollections, strongify, T, Traits>
     template<typename VisitorDispatcher>
     static bool trace(VisitorDispatcher visitor, T& t)
     {
+        ASSERT(NeedsTracingTrait<Traits>::value);
         visitor->trace(t);
         return false;
     }
@@ -420,6 +432,7 @@ struct TraceInCollectionTrait<NoWeakHandlingInCollections, strongify, blink::Hea
     {
         static_assert(strongify == WTF::WeakPointersActStrong, "An on-stack HeapHashTable needs to be visited strongly.");
 
+        ASSERT(NeedsTracingTrait<Traits>::value || Traits::weakHandlingFlag == WeakHandlingInCollections);
         Value* array = reinterpret_cast<Value*>(self);
         blink::HeapObjectHeader* header = blink::HeapObjectHeader::fromPayload(self);
         ASSERT(header->checkHeader());
@@ -550,6 +563,7 @@ struct TraceInCollectionTrait<NoWeakHandlingInCollections, strongify, ListHashSe
     template<typename VisitorDispatcher>
     static bool trace(VisitorDispatcher visitor, Node* node)
     {
+        ASSERT(NeedsTracingTrait<Traits>::value);
         traceListHashSetValue(visitor, node->m_value);
         // Just mark the node without tracing because we already traced the
         // contents, and there is no need to trace the next and prev fields
