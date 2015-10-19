ddca6329e0cdb8519bf3b37aac31a6d27d46d52e
toyoshim <toyoshim@chromium.org>
2015-10-19 02:24:22 -0700

    CSS Font Loading: make FontFaceSet Setlike
    Declare FontFaceSet IDL with setlike<FontFaceSet> to add
    entries(), keys(), and values().
    
    This patch adds entries(), keys(), and values() methods,
    and switches to use Iterable's implementation for forEach()
    and has().
    
    BUG=392075
    
    Review URL: https://codereview.chromium.org/1409433003
    
    Cr-Commit-Position: refs/heads/master@{#354742}

--- third_party/WebKit/Source/core/css/FontFaceSet.h
+++ third_party/WebKit/Source/core/css/FontFaceSet.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef FontFaceSet_h
 #define FontFaceSet_h
 
+#include "bindings/core/v8/Iterable.h"
 #include "bindings/core/v8/ScriptPromise.h"
 #include "core/css/FontFace.h"
 #include "core/css/FontFaceSetForEachCallback.h"
@@ -58,12 +59,14 @@ class FontFaceCache;
 class FontResource;
 class ExecutionContext;
 
+using FontFaceSetIterable = PairIterable<RefPtrWillBeMember<FontFace>, RefPtrWillBeMember<FontFace>>;
+
 #if ENABLE(OILPAN)
-class FontFaceSet final : public EventTargetWithInlineData, public HeapSupplement<Document>, public ActiveDOMObject {
+class FontFaceSet final : public EventTargetWithInlineData, public HeapSupplement<Document>, public ActiveDOMObject, public FontFaceSetIterable {
     USING_GARBAGE_COLLECTED_MIXIN(FontFaceSet);
     using SupplementType = HeapSupplement<Document>;
 #else
-class FontFaceSet final : public EventTargetWithInlineData, public RefCountedSupplement<Document, FontFaceSet>, public ActiveDOMObject {
+class FontFaceSet final : public EventTargetWithInlineData, public RefCountedSupplement<Document, FontFaceSet>, public ActiveDOMObject, public FontFaceSetIterable {
     REFCOUNTED_EVENT_TARGET(FontFaceSet);
     using SupplementType = RefCountedSupplement<Document, FontFaceSet>;
 #endif
@@ -82,11 +85,9 @@ public:
     void add(FontFace*, ExceptionState&);
     void clear();
     bool remove(FontFace*, ExceptionState&);
-    void forEach(FontFaceSetForEachCallback*, const ScriptValue& thisArg) const;
-    void forEach(FontFaceSetForEachCallback*) const;
-    bool has(FontFace*, ExceptionState&) const;
+    bool hasForBinding(ScriptState*, FontFace*, ExceptionState&) const;
 
-    unsigned long size() const;
+    size_t size() const;
     AtomicString status() const;
 
     ExecutionContext* executionContext() const override;
@@ -117,6 +118,26 @@ private:
         return adoptRefWillBeNoop(new FontFaceSet(document));
     }
 
+    FontFaceSetIterable::IterationSource* startIteration(ScriptState*, ExceptionState&) override;
+
+    class IterationSource final : public FontFaceSetIterable::IterationSource {
+    public:
+        explicit IterationSource(const WillBeHeapVector<RefPtrWillBeMember<FontFace>>& fontFaces)
+            : m_index(0)
+            , m_fontFaces(fontFaces) { }
+        bool next(ScriptState*, RefPtrWillBeMember<FontFace>&, RefPtrWillBeMember<FontFace>&, ExceptionState&) override;
+
+        DEFINE_INLINE_VIRTUAL_TRACE()
+        {
+            visitor->trace(m_fontFaces);
+            FontFaceSetIterable::IterationSource::trace(visitor);
+        }
+
+    private:
+        size_t m_index;
+        WillBeHeapVector<RefPtrWillBeMember<FontFace>> m_fontFaces;
+    };
+
     class FontLoadHistogram {
         DISALLOW_ALLOCATION();
     public:
@@ -135,7 +156,6 @@ private:
     FontFaceSet(Document&);
 
     bool inActiveDocumentContext() const;
-    void forEachInternal(FontFaceSetForEachCallback*, const ScriptValue* thisArg) const;
     void addToLoadingFonts(PassRefPtrWillBeRawPtr<FontFace>);
     void removeFromLoadingFonts(PassRefPtrWillBeRawPtr<FontFace>);
     void fireLoadingEvent();
