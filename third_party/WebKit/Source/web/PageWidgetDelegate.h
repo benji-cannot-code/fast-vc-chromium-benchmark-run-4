a23c4a64ad4f39f448eba149aa3f27d5358ba445
yoichio <yoichio@chromium.org>
2015-10-19 01:27:37 -0700

    Add tracing for keyboard event.
    Trace WebViewImpl::handleKeyEvent and ::handleCharEvent including key value.
    
    Plus refactor inputTypeToName() functions into one static function in WebViewImpl.
    
    Review URL: https://codereview.chromium.org/1383523004
    
    Cr-Commit-Position: refs/heads/master@{#354738}

--- third_party/WebKit/Source/web/PageWidgetDelegate.h
+++ third_party/WebKit/Source/web/PageWidgetDelegate.h
@@ -61,7 +61,7 @@ public:
     virtual bool handleTouchEvent(LocalFrame& mainFrame, const WebTouchEvent&);
     virtual ~PageWidgetEventHandler() { }
 protected:
-    String inputTypeToName(WebInputEvent::Type);
+    const char* inputTypeToName(WebInputEvent::Type);
 };
 
 
