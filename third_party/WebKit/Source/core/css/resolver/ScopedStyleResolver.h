b9332385c7824571a3ef087e10907017c81f4110
kochi <kochi@chromium.org>
2015-10-19 02:26:48 -0700

    Skip irrelevant ScopedStyleResolvers when collecting tree boundary crossing rules.
    This is basically same as rune@opera's CL
    https://codereview.chromium.org/1393653004/
    I added some comments to make more understandable.
    
    The old code always added all shadow TreeScopes with style
    as candidates for tree-boundary crossing rules collection.
    
    This caused when N shadow trees exists, styleForElement()
    take O(N^2) time.
    
    This change eliminates those TreeScopes whose
    tree-boundary crossing rules will never apply to
    reduce the number of total rule collection.
    
    This reduced the time for recalculate style in the
    example in crbug.com/540991 on z620 from ~450ms to ~60ms.
    
    The worst case is still O(N^2) when e.g. N shadow trees
    are all nested.
    
    BUG=540991
    
    Review URL: https://codereview.chromium.org/1403243002
    
    Cr-Commit-Position: refs/heads/master@{#354743}

--- third_party/WebKit/Source/core/css/resolver/ScopedStyleResolver.h
+++ third_party/WebKit/Source/core/css/resolver/ScopedStyleResolver.h
@@ -65,6 +65,7 @@ public:
     void collectFeaturesTo(RuleFeatureSet&, WillBeHeapHashSet<RawPtrWillBeMember<const StyleSheetContents>>& visitedSharedStyleSheetContents) const;
     void resetAuthorStyle();
     void collectViewportRulesTo(ViewportStyleResolver*) const;
+    bool hasDeepOrShadowSelector() const { return m_hasDeepOrShadowSelector; }
 
     DECLARE_TRACE();
 
@@ -110,6 +111,7 @@ private:
     using CSSStyleSheetRuleSubSet = WillBeHeapVector<OwnPtrWillBeMember<RuleSubSet>>;
 
     OwnPtrWillBeMember<CSSStyleSheetRuleSubSet> m_treeBoundaryCrossingRuleSet;
+    bool m_hasDeepOrShadowSelector = false;
 };
 
 } // namespace blink
