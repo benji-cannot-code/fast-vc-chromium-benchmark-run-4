f52a6f7323b3fe7c3febfacc5f7b2b79da414ce2
vabr <vabr@chromium.org>
2015-10-19 12:55:42 -0700

    password_manager_switches.h: Remove dangling declarations
    The definitions of kDisablePasswordLink and
    kEnablePasswordLink were removed in
    https://codereview.chromium.org/1396403004/, but the
    declarations were kept by mistake. This CL removes the
    dangling declarations.
    
    R=eugenebut@chromium.org
    
    BUG=480552
    
    Review URL: https://codereview.chromium.org/1418483002
    
    Cr-Commit-Position: refs/heads/master@{#354836}

--- components/password_manager/core/common/password_manager_switches.h
+++ components/password_manager/core/common/password_manager_switches.h
@@ -17,7 +17,6 @@ extern const char kAllowAutofillSyncCredential[];
 extern const char kDisableAffiliationBasedMatching[];
 extern const char kDisableDropSyncCredential[];
 extern const char kDisableManagerForSyncSignin[];
-extern const char kDisablePasswordLink[];
 extern const char kDisallowAutofillSyncCredential[];
 extern const char kDisallowAutofillSyncCredentialForReauth[];
 extern const char kEnableAffiliationBasedMatching[];
@@ -26,7 +25,6 @@ extern const char kEnableDropSyncCredential[];
 extern const char kEnableManagerForSyncSignin[];
 extern const char kEnablePasswordChangeSupport[];
 extern const char kEnablePasswordForceSaving[];
-extern const char kEnablePasswordLink[];
 
 }  // namespace switches
 
