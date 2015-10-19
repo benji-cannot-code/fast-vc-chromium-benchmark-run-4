10b73be422493a1a1ebda98cca87110afbc11d42
dtapuska <dtapuska@chromium.org>
2015-10-19 07:34:09 -0700

    bindings: Generation of dictionary IDLs with RuntimeEnabled attributes.
    Add support to be able to generate dictionaries with RuntimeEnabled
    attributes.
    
    BUG=441569
    
    Review URL: https://codereview.chromium.org/1412433004
    
    Cr-Commit-Position: refs/heads/master@{#354769}

--- third_party/WebKit/Source/bindings/tests/results/core/V8TestDictionary.cpp
+++ third_party/WebKit/Source/bindings/tests/results/core/V8TestDictionary.cpp
@@ -364,6 +364,22 @@ void V8TestDictionary::toImpl(v8::Isolate* isolate, v8::Local<v8::Value> v8Value
         }
     }
 
+    if (RuntimeEnabledFeatures::runtimeFeatureEnabled()) {
+        v8::Local<v8::Value> runtimeMemberValue;
+        if (!v8Object->Get(isolate->GetCurrentContext(), v8String(isolate, "runtimeMember")).ToLocal(&runtimeMemberValue)) {
+            exceptionState.rethrowV8Exception(block.Exception());
+            return;
+        }
+        if (runtimeMemberValue.IsEmpty() || runtimeMemberValue->IsUndefined()) {
+            // Do nothing.
+        } else {
+            bool runtimeMember = toBoolean(isolate, runtimeMemberValue, exceptionState);
+            if (exceptionState.hadException())
+                return;
+            impl.setRuntimeMember(runtimeMember);
+        }
+    }
+
     {
         v8::Local<v8::Value> stringArrayMemberValue;
         if (!v8Object->Get(isolate->GetCurrentContext(), v8String(isolate, "stringArrayMember")).ToLocal(&stringArrayMemberValue)) {
@@ -766,6 +782,11 @@ bool toV8TestDictionary(const TestDictionary& impl, v8::Local<v8::Object> dictio
             return false;
     } else {
         if (!v8CallBoolean(dictionary->CreateDataProperty(isolate->GetCurrentContext(), v8String(isolate, "restrictedDoubleMember"), v8::Number::New(isolate, 3.14))))
+            return false;
+    }
+
+    if (impl.hasRuntimeMember()) {
+        if (!v8CallBoolean(dictionary->CreateDataProperty(isolate->GetCurrentContext(), v8String(isolate, "runtimeMember"), v8Boolean(impl.runtimeMember(), isolate))))
             return false;
     }
 
