9daaf5bb14c5234d44111b2dca0a0f9844005886
tfarina <tfarina@chromium.org>
2015-10-19 11:42:31 -0700

    crypto: Remove duplicated list include from rsa_private_key.cc.
    As any includes present in the related header do not need to be included
    again in the corresponding source file (i.e., foo.cc can rely on foo.h's includes).
    
    From style guide:
    https://google-styleguide.googlecode.com/svn/trunk/cppguide.html#Names_and_Order_of_Includes
    
    "3. However, any includes present in the related header do not need to be
    included again in the related cc (i.e., foo.cc can rely on foo.h's includes)."
    
    This patch was generated on Linux with the following command line:
    
    $ git ls-files crypto/ | grep "\.h$" | python ../remove_duplicate_includes.py
    
    BUG=460988
    R=agl@chromium.org
    
    Review URL: https://codereview.chromium.org/1414503004
    
    Cr-Commit-Position: refs/heads/master@{#354808}

--- crypto/rsa_private_key.cc
+++ crypto/rsa_private_key.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/rsa_private_key.h"
 
 #include <algorithm>
-#include <list>
 
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
