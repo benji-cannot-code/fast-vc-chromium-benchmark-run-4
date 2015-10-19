171c823d8d8203afb43697d94613433445a04237
estark <estark@chromium.org>
2015-10-19 12:46:36 -0700

    Implement $Secure- cookie prefix
    This CL implements the rule that cookies whose names start with
    $Secure- can only be set if the Secure attribute is enabled. The
    implementation is a runtime-enabled web platform feature for now.
    
    Intent to Implement:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/IU5t6eLuS2Y/Uq-7Kat9BwAJ
    
    BUG=541511
    TBR=droger@chromium.org
    
    Committed: https://crrev.com/467a098174e062d4380eb0ad8f7b2f0b0b7ed5fa
    Cr-Commit-Position: refs/heads/master@{#354676}
    
    Review URL: https://codereview.chromium.org/1393193005
    
    Cr-Commit-Position: refs/heads/master@{#354832}

--- ios/net/cookies/cookie_store_ios_unittest.mm
+++ ios/net/cookies/cookie_store_ios_unittest.mm
@@ -43,6 +43,7 @@ struct CookieStoreIOSTestTraits {
   static const bool filters_schemes = false;
   static const bool has_path_prefix_bug = true;
   static const int creation_time_granularity_in_ms = 1000;
+  static const int enforces_prefixes = true;
 
   base::MessageLoop loop_;
 };
@@ -59,6 +60,7 @@ struct InactiveCookieStoreIOSTestTraits {
   static const bool filters_schemes = false;
   static const bool has_path_prefix_bug = false;
   static const int creation_time_granularity_in_ms = 0;
+  static const int enforces_prefixes = true;
 
   base::MessageLoop loop_;
 };
@@ -169,6 +171,7 @@ struct RoundTripTestCookieStoreTraits {
   static const bool filters_schemes = false;
   static const bool has_path_prefix_bug = true;
   static const int creation_time_granularity_in_ms = 1000;
+  static const int enforces_prefixes = true;
 };
 
 }  // namespace net
