176ced2f748d33a0e101e93edc73ea311eea6c8b
Yuli Huang <yuli@chromium.org>
2015-10-19 16:20:03 +0800

    Disable Cinema, Tilt Shift, Beauty and Funky effects on alex.
    TEST=Tested manually by not seeing black screen effects on alex.
    BUG=chromium:523216
    
    Change-Id: I27e7db0417e16910e0af7b11c44d783a8a3f2293
    Reviewed-on: https://chromium-review.googlesource.com/308120
    Reviewed-by: Tomasz Mikolajewski <mtomasz@chromium.org>
    Tested-by: Tomasz Mikolajewski <mtomasz@chromium.org>

--- chrome/browser/resources/chromeos/camera/src/js/effects/pinch.js
+++ chrome/browser/resources/chromeos/camera/src/js/effects/pinch.js
@@ -95,6 +95,17 @@ camera.effects.BigHead = function() {
   camera.effects.Pinch.call(this, 0, 1.0, 1.0);
 };
 
+/**
+ * Returns true if the effect is available on the platform.
+ *
+ * @param {Array.<string>} webglExtensions Supported WebGL extensions on the
+ *     platform.
+ * @return {boolean} True if available, false otherwise.
+ */
+camera.effects.BigHead.isAvailable = function(webglExtensions) {
+  return true;
+};
+
 camera.effects.BigHead.prototype = {
   __proto__: camera.effects.Pinch.prototype
 };
@@ -114,6 +125,17 @@ camera.effects.BigJaw = function() {
   camera.effects.Pinch.call(this, 0.9, 0.6, 1.0);
 };
 
+/**
+ * Returns true if the effect is available on the platform.
+ *
+ * @param {Array.<string>} webglExtensions Supported WebGL extensions on the
+ *     platform.
+ * @return {boolean} True if available, false otherwise.
+ */
+camera.effects.BigJaw.isAvailable = function(webglExtensions) {
+  return true;
+};
+
 camera.effects.BigJaw.prototype = {
   __proto__: camera.effects.Pinch.prototype
 };
@@ -131,6 +153,17 @@ camera.effects.BigJaw.prototype.getTitle = function() {
  */
 camera.effects.BunnyHead = function() {
   camera.effects.Pinch.call(this, 0.8, 0.8, -0.7);
+};
+
+/**
+ * Returns true if the effect is available on the platform.
+ *
+ * @param {Array.<string>} webglExtensions Supported WebGL extensions on the
+ *     platform.
+ * @return {boolean} True if available, false otherwise.
+ */
+camera.effects.BunnyHead.isAvailable = function(webglExtensions) {
+  return true;
 };
 
 camera.effects.BunnyHead.prototype = {
