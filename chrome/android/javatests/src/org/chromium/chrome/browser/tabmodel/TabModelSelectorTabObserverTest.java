10baaae02d1e82ab725e76bf4ff5fb9b4daebedb
mlamouri <mlamouri@chromium.org>
2015-10-19 13:10:29 -0700

    Webapps splashscreen: add moar tests.
    This is adding many tests that were required for the splashscreen feature,
    checking that the theme-color behaviour works as expected, that the
    splashscreen properly disappears when the page is loaded, crashes or paints.
    
    This is testing a few recent CLs and some older ones.
    
    BUG=542773,531519,542626,543604
    
    Review URL: https://codereview.chromium.org/1405023002
    
    Cr-Commit-Position: refs/heads/master@{#354844}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorTabObserverTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorTabObserverTest.java
@@ -207,7 +207,7 @@ public class TabModelSelectorTabObserverTest extends NativeLibraryTestBase {
 
         // Exists to expose the method to the test.
         @Override
-        protected ObserverList.RewindableIterator<TabObserver> getTabObservers() {
+        public ObserverList.RewindableIterator<TabObserver> getTabObservers() {
             return super.getTabObservers();
         }
     }
--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappSplashScreenThemeColorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappSplashScreenThemeColorTest.java
@@ -11,9 +11,14 @@ import android.graphics.Color;
 import android.os.Build;
 import android.test.suitebuilder.annotation.SmallTest;
 
+import org.chromium.base.ObserverList.RewindableIterator;
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.ShortcutHelper;
+import org.chromium.chrome.browser.tab.TabObserver;
 import org.chromium.chrome.browser.util.ColorUtils;
+import org.chromium.content.browser.test.util.Criteria;
+import org.chromium.content.browser.test.util.CriteriaHelper;
 
 /**
  * Tests for splashscreen.
@@ -23,6 +28,7 @@ public class WebappSplashScreenThemeColorTest extends WebappActivityTestBase {
     @Override
     protected Intent createIntent() {
         Intent intent = super.createIntent();
+        intent.putExtra(ShortcutHelper.EXTRA_URL, "http://localhost");
         intent.putExtra(ShortcutHelper.EXTRA_THEME_COLOR, (long) Color.MAGENTA);
         return intent;
     }
@@ -30,10 +36,38 @@ public class WebappSplashScreenThemeColorTest extends WebappActivityTestBase {
     @SmallTest
     @Feature({"Webapps"})
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public void testSplashscreenThemeColorWhenSpecified() {
+    public void testThemeColorWhenSpecified() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) return;
 
         assertEquals(ColorUtils.getDarkenedColorForStatusBar(Color.MAGENTA),
                 getActivity().getWindow().getStatusBarColor());
+    }
+
+    @SmallTest
+    @Feature({"Webapps"})
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void testThemeColorNotUsedIfPagesHasOne() throws InterruptedException {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) return;
+
+        ThreadUtils.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                RewindableIterator<TabObserver> observers =
+                        getActivity().getActivityTab().getTabObservers();
+                while (observers.hasNext()) {
+                    observers.next().onDidChangeThemeColor(
+                            getActivity().getActivityTab(), Color.GREEN);
+                }
+            }
+        });
+
+        // Waits for theme-color to change so the test doesn't rely on system timing.
+        assertTrue(CriteriaHelper.pollForCriteria(new Criteria() {
+                @Override
+                public boolean isSatisfied() {
+                    return getActivity().getWindow().getStatusBarColor()
+                            == ColorUtils.getDarkenedColorForStatusBar(Color.GREEN);
+                }
+            }));
     }
 }
