6d50babf09dea32a90756b8bf9a67b9afb5586c5
chasej <chasej@chromium.org>
2015-10-19 15:22:13 -0700

    Implement stub of Blink API to check for enabled experiments
    Allows APIs implemented in Blink to check (from native code), if the
    API is enabled for the current context.  Stub implementation will
    always return that the API is disabled.
    
    API is synchronous so it can be called from API implementations that are
    synchronous.
    
    BUG=543140
    
    Review URL: https://codereview.chromium.org/1404733003
    
    Cr-Commit-Position: refs/heads/master@{#354887}

---
+++ third_party/WebKit/Source/core/experiments/Experiments.cpp
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "core/experiments/Experiments.h"
+
+#include "core/dom/ExceptionCode.h"
+
+namespace blink {
+
+bool Experiments::isApiEnabled(ExecutionContext* executionContext, const String& apiName)
+{
+    return false;
+}
+
+DOMException* Experiments::createApiDisabledException(const String& apiName)
+{
+    // TODO(chasej): Update message with URL to experiments site, when live
+    return DOMException::create(NotSupportedError, "The '" + apiName + "' API is currently enabled in limited experiments. Please see [Chrome experiments website URL] for information on enabling this experiment on your site.");
+}
+
+} // namespace blink
