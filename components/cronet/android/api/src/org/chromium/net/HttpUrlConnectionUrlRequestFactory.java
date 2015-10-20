c6b530de8ce4be8e98a05c18a239d17891898ed0
pauljensen <pauljensen@chromium.org>
2015-10-20 09:37:32 -0700

    [Cronet] Move UserAgent API to CronetEngine.Builder.getDefaultUserAgent
    This has a few benefits:
    1) The top-level UserAgent class can be removed, making the API simpler
       and cleaner.
    2) The CronetEngine.Builder already has a Context pointer so one does
       not need to be passed into the new API, making the API simpler and
       cleaner.
    3) Generally the UserAgent is queried from this API, modified, and
       passed back into CronetEngine.Builder.setUserAgent().  By moving
       the API to construct the default UserAgent string next to the
       API to set the UserAgent it should be easier for embedders to
       understand and use.
    The old API is @deprecated and embedders should transition from it.
    
    BUG=531538
    
    Review URL: https://codereview.chromium.org/1398753002
    
    Cr-Commit-Position: refs/heads/master@{#355077}

--- components/cronet/android/api/src/org/chromium/net/HttpUrlConnectionUrlRequestFactory.java
+++ components/cronet/android/api/src/org/chromium/net/HttpUrlConnectionUrlRequestFactory.java
@@ -24,9 +24,10 @@ class HttpUrlConnectionUrlRequestFactory extends HttpUrlRequestFactory {
 
     public HttpUrlConnectionUrlRequestFactory(Context context, CronetEngine.Builder config) {
         mContext = context;
-        String userAgent = config.userAgent();
+        String userAgent = config.getUserAgent();
         if (userAgent.isEmpty()) {
-            userAgent = UserAgent.from(mContext);
+            // Cannot use config.getDefaultUserAgent() as config.mContext may be null.
+            userAgent = new CronetEngine.Builder(mContext).getDefaultUserAgent();
         }
         mDefaultUserAgent = userAgent;
     }
