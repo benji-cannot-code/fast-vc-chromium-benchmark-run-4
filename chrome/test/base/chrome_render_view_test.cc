d55ca101cb431fe3fe71e2acfd89d118f17bb9e9
thestig <thestig@chromium.org>
2015-10-19 17:10:00 -0700

    Move more extensions code into ChromeExtensionsRendererClient.
    Review URL: https://codereview.chromium.org/1394283003
    
    Cr-Commit-Position: refs/heads/master@{#354921}

--- chrome/test/base/chrome_render_view_test.cc
+++ chrome/test/base/chrome_render_view_test.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(ENABLE_EXTENSIONS)
 #include "chrome/renderer/extensions/chrome_extensions_dispatcher_delegate.h"
+#include "chrome/renderer/extensions/chrome_extensions_renderer_client.h"
 #include "extensions/browser/extension_function_dispatcher.h"
 #include "extensions/common/extension.h"
 #include "extensions/renderer/dispatcher.h"
@@ -110,9 +111,9 @@ void ChromeRenderViewTest::SetUp() {
 void ChromeRenderViewTest::TearDown() {
   base::RunLoop().RunUntilIdle();
 #if defined(ENABLE_EXTENSIONS)
-  ChromeContentRendererClient* client =
-      static_cast<ChromeContentRendererClient*>(content_renderer_client_.get());
-  client->GetExtensionDispatcherForTest()->OnRenderProcessShutdown();
+  ChromeExtensionsRendererClient* ext_client =
+      ChromeExtensionsRendererClient::GetInstance();
+  ext_client->GetExtensionDispatcherForTest()->OnRenderProcessShutdown();
 #endif
 
 #if defined(LEAK_SANITIZER)
@@ -138,8 +139,11 @@ ChromeRenderViewTest::CreateContentRendererClient() {
 #if defined(ENABLE_EXTENSIONS)
   extension_dispatcher_delegate_.reset(
       new ChromeExtensionsDispatcherDelegate());
-  client->SetExtensionDispatcherForTest(
-      new extensions::Dispatcher(extension_dispatcher_delegate_.get()));
+  ChromeExtensionsRendererClient* ext_client =
+      ChromeExtensionsRendererClient::GetInstance();
+  ext_client->SetExtensionDispatcherForTest(
+      make_scoped_ptr(
+          new extensions::Dispatcher(extension_dispatcher_delegate_.get())));
 #endif
 #if defined(ENABLE_SPELLCHECK)
   client->SetSpellcheck(new SpellCheck());
