321ff56e29800d90dcdad928b80a36bced1f3116
ccameron <ccameron@chromium.org>
2015-10-19 21:05:11 -0700

    cc: Fix texture coordinates for rectangle targets
    Add a texture size argument to the GL texture lock. We can later use
    this property to remove the texture size from TextureDrawQuad, and
    switch that over to use normalized coordiantes.
    
    BUG=533677
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1411383003
    
    Cr-Commit-Position: refs/heads/master@{#354973}

--- cc/resources/resource_provider.h
+++ cc/resources/resource_provider.h
@@ -220,6 +220,7 @@ class CC_EXPORT ResourceProvider
 
     unsigned texture_id() const { return resource_->gl_id; }
     GLenum target() const { return resource_->target; }
+    const gfx::Size& texture_size() const { return resource_->size; }
 
    protected:
     ResourceProvider* resource_provider_;
