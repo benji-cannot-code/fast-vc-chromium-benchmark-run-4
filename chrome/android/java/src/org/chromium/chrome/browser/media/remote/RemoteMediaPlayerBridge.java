3a2538ad72993b9be95d14f73faf1557267486bd
aberent <aberent@chromium.org>
2015-10-20 04:06:50 -0700

    Add null checks for mNativeRemoteMediaPlayer
    The null check in release() (which fixes the bug) became necessary
    because of the recent refactoring. Although the current C++ code
    should mean that some of the other members don't need null checks
    this could change in future, so it seems sensible to put them in
    throughout.
    
    BUG=544717
    
    Review URL: https://codereview.chromium.org/1412083003
    
    Cr-Commit-Position: refs/heads/master@{#355033}

--- chrome/android/java/src/org/chromium/chrome/browser/media/remote/RemoteMediaPlayerBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/remote/RemoteMediaPlayerBridge.java
@@ -203,6 +203,7 @@ public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
     @CalledByNative
     private void requestRemotePlayback() {
         if (mDebug) Log.i(TAG, "requestRemotePlayback");
+        if (mRouteController == null) return;
         // Clear out the state
         mPauseRequested = false;
         mSeekRequested = false;
@@ -222,6 +223,7 @@ public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
     @CalledByNative
     private void setNativePlayer() {
         if (mDebug) Log.i(TAG, "setNativePlayer");
+        if (mRouteController == null) return;
         mRouteController.setMediaStateListener(mMediaStateListener);
         mActive = true;
     }
@@ -229,17 +231,15 @@ public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
     @CalledByNative
     private void onPlayerCreated() {
         if (mDebug) Log.i(TAG, "onPlayerCreated");
-        if (mRouteController != null) {
-            mRouteController.addMediaStateListener(mMediaStateListener);
-        }
+        if (mRouteController == null) return;
+        mRouteController.addMediaStateListener(mMediaStateListener);
     }
 
     @CalledByNative
     private void onPlayerDestroyed() {
         if (mDebug) Log.i(TAG, "onPlayerDestroyed");
-        if (mRouteController != null) {
-            mRouteController.removeMediaStateListener(mMediaStateListener);
-        }
+        if (mRouteController == null) return;
+        mRouteController.removeMediaStateListener(mMediaStateListener);
     }
 
     /**
@@ -251,24 +251,28 @@ public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
      */
     @CalledByNative
     private void setPosterBitmap(Bitmap bitmap) {
+        if (mRouteController == null) return;
         mPosterBitmap = bitmap;
     }
 
     @Override
     @CalledByNative
     protected boolean isPlaying() {
+        if (mRouteController == null) return false;
         return mRouteController.isPlaying();
     }
 
     @Override
     @CalledByNative
     protected int getCurrentPosition() {
+        if (mRouteController == null) return 0;
         return mRouteController.getPosition();
     }
 
     @Override
     @CalledByNative
     protected int getDuration() {
+        if (mRouteController == null) return 0;
         return mRouteController.getDuration();
     }
 
@@ -277,7 +281,7 @@ public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
     protected void release() {
         // Remove the state change listeners. Release does mean that Chrome is no longer interested
         // in events from the media player.
-        mRouteController.setMediaStateListener(null);
+        if (mRouteController != null) mRouteController.setMediaStateListener(null);
         mActive = false;
     }
 
@@ -290,14 +294,14 @@ public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
     @CalledByNative
     protected void start() throws IllegalStateException {
         mPauseRequested = false;
-        if (mRouteController.isBeingCast()) mRouteController.resume();
+        if (mRouteController != null && mRouteController.isBeingCast()) mRouteController.resume();
     }
 
     @Override
     @CalledByNative
     protected void pause() throws IllegalStateException {
         mPauseRequested = true;
-        if (mRouteController.isBeingCast()) mRouteController.pause();
+        if (mRouteController != null && mRouteController.isBeingCast()) mRouteController.pause();
     }
 
     @Override
@@ -305,7 +309,9 @@ public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
     protected void seekTo(int msec) throws IllegalStateException {
         mSeekRequested = true;
         mSeekLocation = msec;
-        if (mRouteController.isBeingCast()) mRouteController.seekTo(msec);
+        if (mRouteController != null && mRouteController.isBeingCast()) {
+            mRouteController.seekTo(msec);
+        }
     }
 
     @Override
