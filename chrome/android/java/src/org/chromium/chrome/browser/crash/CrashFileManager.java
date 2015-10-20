741ab3678f55e34def119813c945e99636af439c
mimee <mimee@chromium.org>
2015-10-20 10:50:23 -0700

    More accurate function names and javadoc within crash upload logic.
    BUG=
    R=acleung@chromium.org, dtrainor@chromium.org
    
    Review URL: https://codereview.chromium.org/1381543008
    
    Cr-Commit-Position: refs/heads/master@{#355097}

--- chrome/android/java/src/org/chromium/chrome/browser/crash/CrashFileManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/crash/CrashFileManager.java
@@ -81,7 +81,7 @@ public class CrashFileManager {
     }
 
     public static String tryIncrementAttemptNumber(File mFileToUpload) {
-        String newName = incrementAttemptNumber(mFileToUpload.getPath());
+        String newName = filenameWithIncrementedAttemptNumber(mFileToUpload.getPath());
         return mFileToUpload.renameTo(new File(newName)) ? newName : null;
     }
 
@@ -89,7 +89,7 @@ public class CrashFileManager {
      * @return The file name to rename to after an addition attempt to upload
      */
     @VisibleForTesting
-    public static String incrementAttemptNumber(String filename) {
+    public static String filenameWithIncrementedAttemptNumber(String filename) {
         int numTried = readAttemptNumber(filename);
         if (numTried > 0) {
             int newCount = numTried + 1;
