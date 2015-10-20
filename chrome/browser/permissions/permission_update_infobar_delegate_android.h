ab3f68e5c759e2c01485de331cb950d2795b40a4
tedchoc <tedchoc@chromium.org>
2015-10-20 09:34:22 -0700

    Fix crash in android update permission handling.
    Do not call the permission callback in the dtor as it is
    handled elsewhere.
    
    Adds a test for the geolocation case to ensure this crash will
    not reappear.
    
    Added the support to mock out the permission APIs to Android that allow
    testing of this without mucking with the actual runtime permissions.
    This also allows testing on pre-M phones to get better coverage.
    
    BUG=543801
    
    Review URL: https://codereview.chromium.org/1408293002
    
    Cr-Commit-Position: refs/heads/master@{#355075}

--- chrome/browser/permissions/permission_update_infobar_delegate_android.h
+++ chrome/browser/permissions/permission_update_infobar_delegate_android.h
@@ -30,6 +30,8 @@ class PermissionUpdateInfoBarDelegate : public ConfirmInfoBarDelegate {
   // The necessary runtime permissions are generated based on the list of
   // ContentSettingsTypes passed in. Returns the infobar if it was successfully
   // added.
+  //
+  // The |callback| will not be triggered if this is deleted.
   static infobars::InfoBar* Create(
       content::WebContents* web_contents,
       const std::vector<ContentSettingsType>& content_settings_types,
@@ -37,6 +39,8 @@ class PermissionUpdateInfoBarDelegate : public ConfirmInfoBarDelegate {
 
   // Creates an infobar to resolve conflicts in Android runtime permissions.
   // Returns the infobar if it was successfully added.
+  //
+  // The |callback| will not be triggered if this is deleted.
   static infobars::InfoBar* Create(
       content::WebContents* web_contents,
       const std::vector<std::string>& android_permissions,
