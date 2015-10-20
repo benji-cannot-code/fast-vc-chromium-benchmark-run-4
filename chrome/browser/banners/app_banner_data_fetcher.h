0d71b9bb17c6096f85d93eef45435b1165113a5f
dominickn <dominickn@chromium.org>
2015-10-19 21:29:38 -0700

    Make app banners distinguish between missing and empty manifests.
    This CL uses the new WebContents::HasManifest() method to allow app
    banners to first check whether sites actually have a manifest link URL
    before attempting to download the manifest. This clarifies the debug
    message shown in the console upon a manifest error.
    
    BUG=540496
    
    Review URL: https://codereview.chromium.org/1399693004
    
    Cr-Commit-Position: refs/heads/master@{#354975}

--- chrome/browser/banners/app_banner_data_fetcher.h
+++ chrome/browser/banners/app_banner_data_fetcher.h
@@ -137,6 +137,7 @@ class AppBannerDataFetcher
 
  private:
   // Callbacks for data retrieval.
+  void OnDidHasManifest(bool has_manifest);
   void OnDidGetManifest(const content::Manifest& manifest);
   void OnDidCheckHasServiceWorker(bool has_service_worker);
   void OnAppIconFetched(const SkBitmap& bitmap);
