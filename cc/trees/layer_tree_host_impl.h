5dace46488352a1fab4ed73112cadda6659c8d36
skobes <skobes@chromium.org>
2015-10-20 12:16:34 -0700

    Let LayerTreeHostImpl::ScrollAnimated scroll the outer viewport.
    Scroll offset animations don't go through cc::Viewport, so we can't rely on it
    to distribute to the outer viewport; FindScrollLayerForDeviceViewportPoint needs
    to return the layer that actually scrolls.
    
    This regressed with http://crrev.com/1365343002.
    
    BUG=537393
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1418493003
    
    Cr-Commit-Position: refs/heads/master@{#355125}

--- cc/trees/layer_tree_host_impl.h
+++ cc/trees/layer_tree_host_impl.h
@@ -188,7 +188,7 @@ class CC_EXPORT LayerTreeHostImpl
                                float page_scale,
                                base::TimeDelta duration);
   void SetNeedsAnimateInput() override;
-  bool IsCurrentlyScrollingRoot() const override;
+  bool IsCurrentlyScrollingInnerViewport() const override;
   bool IsCurrentlyScrollingLayerAt(
       const gfx::Point& viewport_point,
       InputHandler::ScrollInputType type) const override;
