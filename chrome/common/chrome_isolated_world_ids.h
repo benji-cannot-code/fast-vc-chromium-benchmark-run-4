d611058ed039cf72073d8e0bb33eb96449ace088
thestig <thestig@chromium.org>
2015-10-20 01:59:56 -0700

    Mark RFH::AllowInjectingJavaScriptForAndroidWebView as Android only.
    Also do IYWU for chrome_isolated_world_ids.h.
    
    Review URL: https://codereview.chromium.org/1412243003
    
    Cr-Commit-Position: refs/heads/master@{#355014}

--- chrome/common/chrome_isolated_world_ids.h
+++ chrome/common/chrome_isolated_world_ids.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_COMMON_CHROME_ISOLATED_WORLD_IDS_H_
 #define CHROME_COMMON_CHROME_ISOLATED_WORLD_IDS_H_
 
+#include "build/build_config.h"
 #include "content/public/common/isolated_world_ids.h"
 
 namespace chrome {
