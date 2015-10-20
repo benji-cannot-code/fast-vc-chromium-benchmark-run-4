bcabeb38be152dff66f7fbff9d64437958926e93
erikchen <erikchen@chromium.org>
2015-10-20 10:22:59 -0700

    Revert of ipc: Add a member to IPC::Message::Header on Windows. (patchset #1 id:1 of https://codereview.chromium.org/1400713002/ )
    Reason for revert:
    Likely causes crashes:
    https://code.google.com/p/chromium/issues/detail?id=527588#c57
    
    Original issue's description:
    > ipc: Add a member to IPC::Message::Header on Windows.
    >
    > According to my most recent debugging experiments, the root cause of the crashes
    > that happened the first time I tried to make this change have magically
    > disappeared. See the bug for more details.
    >
    > BUG=527588
    >
    > Committed: https://crrev.com/c32bea67c022c6c385463f0eec027f18b2915300
    > Cr-Commit-Position: refs/heads/master@{#354889}
    
    TBR=tsepez@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=527588
    
    Review URL: https://codereview.chromium.org/1414293002
    
    Cr-Commit-Position: refs/heads/master@{#355089}

--- ipc/ipc_message.cc
+++ ipc/ipc_message.cc
@@ -51,7 +51,7 @@ Message::~Message() {
 Message::Message() : base::Pickle(sizeof(Header)) {
   header()->routing = header()->type = 0;
   header()->flags = GetRefNumUpper24();
-#if USE_ATTACHMENT_BROKER
+#if defined(OS_MACOSX)
   header()->num_brokered_attachments = 0;
 #endif
 #if defined(OS_POSIX)
@@ -67,7 +67,7 @@ Message::Message(int32_t routing_id, uint32_t type, PriorityValue priority)
   header()->type = type;
   DCHECK((priority & 0xffffff00) == 0);
   header()->flags = priority | GetRefNumUpper24();
-#if USE_ATTACHMENT_BROKER
+#if defined(OS_MACOSX)
   header()->num_brokered_attachments = 0;
 #endif
 #if defined(OS_POSIX)
