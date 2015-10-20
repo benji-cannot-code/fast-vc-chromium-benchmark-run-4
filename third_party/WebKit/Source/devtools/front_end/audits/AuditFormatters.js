1025df034a908a83f9cf287e0f25b74ddc430410
kozyatinskiy <kozyatinskiy@chromium.org>
2015-10-20 14:19:46 -0700

    [DevTools] Add missing column number for message without stack
    BUG=144888
    R=yurys@chromium.org,dgozman@chromium.org
    TBR=horo@chromium.org
    
    Review URL: https://codereview.chromium.org/1404413003
    
    Cr-Commit-Position: refs/heads/master@{#355154}

--- third_party/WebKit/Source/devtools/front_end/audits/AuditFormatters.js
+++ third_party/WebKit/Source/devtools/front_end/audits/AuditFormatters.js
@@ -88,7 +88,7 @@ WebInspector.AuditFormatters.Registry = {
     resourceLink: function(url, line)
     {
         // FIXME: use WebInspector.Linkifier
-        return WebInspector.linkifyResourceAsNode(url, line, "resource-url webkit-html-resource-link");
+        return WebInspector.linkifyResourceAsNode(url, line, undefined, "resource-url webkit-html-resource-link");
     }
 };
 
