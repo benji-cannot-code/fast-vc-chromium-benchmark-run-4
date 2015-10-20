8b054f3ccfac5ca81c595dcd779a78f17c38572d
treib <treib@chromium.org>
2015-10-20 11:15:42 -0700

    Apps&Extensions for Supervised Users: send permission request on outdated re-enables
    Also add a test to verify that re-enabling works as expected
    
    BUG=503106
    
    Review URL: https://codereview.chromium.org/1200833004
    
    Cr-Commit-Position: refs/heads/master@{#355106}

--- chrome/browser/extensions/extension_service.h
+++ chrome/browser/extensions/extension_service.h
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "Extensions must be enabled"
 #endif
 
-class ExtensionSyncService;
 class GURL;
 class HostContentSettingsMap;
 class Profile;
--- chrome/browser/extensions/extension_sync_service.cc
+++ chrome/browser/extensions/extension_sync_service.cc
@@ -32,6 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sync/api/sync_change.h"
 #include "sync/api/sync_error_factory.h"
 
+#if defined(ENABLE_SUPERVISED_USERS)
+#include "chrome/browser/supervised_user/supervised_user_service.h"
+#include "chrome/browser/supervised_user/supervised_user_service_factory.h"
+#endif
+
 using extensions::AppSorting;
 using extensions::Extension;
 using extensions::ExtensionPrefs;
@@ -139,6 +144,17 @@ void ExtensionSyncService::SyncExtensionChangeIfNeeded(
     if (extension_service()->is_ready() && !flare_.is_null())
       flare_.Run(type);  // Tell sync to start ASAP.
   }
+}
+
+bool ExtensionSyncService::HasPendingReenable(
+    const std::string& id,
+    const base::Version& version) const {
+  auto it = pending_updates_.find(id);
+  if (it == pending_updates_.end())
+    return false;
+  const PendingUpdate& pending = it->second;
+  return pending.version.Equals(version) &&
+         pending.grant_permissions_and_reenable;
 }
 
 syncer::SyncMergeResult ExtensionSyncService::MergeDataAndStartSyncing(
@@ -340,6 +356,14 @@ void ExtensionSyncService::ApplySyncData(
           extension_service()->EnableExtension(id);
         else if (extension_sync_data.supports_disable_reasons())
           reenable_after_update = true;
+
+#if defined(ENABLE_SUPERVISED_USERS)
+        if (is_privilege_increase && version_compare_result > 0 &&
+            extensions::util::IsExtensionSupervised(extension, profile_)) {
+          SupervisedUserServiceFactory::GetForProfile(profile_)
+              ->AddExtensionUpdateRequest(id, *extension->version());
+        }
+#endif
       }
     } else {
       // The extension is not installed yet. Set it to enabled; we'll check for
