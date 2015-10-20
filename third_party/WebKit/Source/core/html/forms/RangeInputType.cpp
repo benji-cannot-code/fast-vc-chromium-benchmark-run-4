e2836ed67dc6beff677df5acdf74521317b86d83
tkent <tkent@chromium.org>
2015-10-20 01:10:05 -0700

    input[type=range]: Reverse left/right arrow key roles in RTL.
    BUG=370171
    
    Review URL: https://codereview.chromium.org/1418573003
    
    Cr-Commit-Position: refs/heads/master@{#355005}

--- third_party/WebKit/Source/core/html/forms/RangeInputType.cpp
+++ third_party/WebKit/Source/core/html/forms/RangeInputType.cpp
@@ -191,8 +191,10 @@ void RangeInputType::handleKeydownEvent(KeyboardEvent* event)
     const Decimal step = equalIgnoringCase(element().fastGetAttribute(stepAttr), "any") ? (stepRange.maximum() - stepRange.minimum()) / 100 : stepRange.step();
     const Decimal bigStep = std::max((stepRange.maximum() - stepRange.minimum()) / 10, step);
 
+    TextDirection dir = LTR;
     bool isVertical = false;
     if (element().layoutObject()) {
+        dir = computedTextDirection();
         ControlPart part = element().layoutObject()->style()->appearance();
         isVertical = part == SliderVerticalPart;
     }
@@ -203,9 +205,9 @@ void RangeInputType::handleKeydownEvent(KeyboardEvent* event)
     else if (key == "Down")
         newValue = current - step;
     else if (key == "Left")
-        newValue = isVertical ? current + step : current - step;
+        newValue = (isVertical || dir == RTL) ? current + step : current - step;
     else if (key == "Right")
-        newValue = isVertical ? current - step : current + step;
+        newValue = (isVertical || dir == RTL) ? current - step : current + step;
     else if (key == "PageUp")
         newValue = current + bigStep;
     else if (key == "PageDown")
