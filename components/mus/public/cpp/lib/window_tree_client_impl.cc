915b16c47a395b017bd7085084010f12e52d9bc7
fsamuel <fsamuel@chromium.org>
2015-10-20 08:13:37 -0700

    mus: Fix some comments after renaming View => Window
    BUG=542848
    TBR=ben@chromium.org
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/1418613003
    
    Cr-Commit-Position: refs/heads/master@{#355062}

--- components/mus/public/cpp/lib/window_tree_client_impl.cc
+++ components/mus/public/cpp/lib/window_tree_client_impl.cc
@@ -166,8 +166,7 @@ void WindowTreeClientImpl::SetClientArea(Id window_id,
 
 void WindowTreeClientImpl::SetFocus(Id window_id) {
   // In order for us to get here we had to have exposed a window, which implies
-  // we
-  // got a connection.
+  // we got a connection.
   DCHECK(tree_);
   tree_->SetFocus(window_id);
 }
@@ -366,8 +365,7 @@ void WindowTreeClientImpl::OnWindowHierarchyChanged(
   BuildWindowTree(this, windows, initial_parent);
 
   // If the window was not known, then BuildWindowTree() will have created it
-  // and
-  // parented the window.
+  // and parented the window.
   if (!was_window_known)
     return;
 
