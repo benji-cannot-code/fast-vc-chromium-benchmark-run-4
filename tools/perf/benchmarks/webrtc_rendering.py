a6e4008b5fcd4e7bf861c260dd24885c0b14375a
vollick <vollick@chromium.org>
2015-10-19 17:20:51 -0700

    Disable webrtc_rendering on mac
    This is consistently failing. Eg,
    http://build.chromium.org/p/chromium.perf/builders/Mac%2010.10%20Perf%20%281%29/builds/398
    
    TBR=cpaulin
    BUG=545119
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_10_10_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1408873004
    
    Cr-Commit-Position: refs/heads/master@{#354931}

--- tools/perf/benchmarks/webrtc_rendering.py
+++ tools/perf/benchmarks/webrtc_rendering.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from core import perf_benchmark
+from telemetry import benchmark
 from telemetry.timeline import tracing_category_filter
 from telemetry.web_perf import timeline_based_measurement
 
@@ -11,6 +12,7 @@ import page_sets
 
 BENCHMARK_VALUES = 'WebRTCRendering_'
 
+@benchmark.Disabled('mac')  # http://crbug.com/545119
 class WebRTCRendering(perf_benchmark.PerfBenchmark):
   """Timeline based benchmark for the WebRtc rendering."""
 
