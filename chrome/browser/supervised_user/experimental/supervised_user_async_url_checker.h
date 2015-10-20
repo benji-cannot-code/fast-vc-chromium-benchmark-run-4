8764a61cb61508e115c56ead1cdd62240bd657fe
treib <treib@chromium.org>
2015-10-20 06:00:07 -0700

    Supervised user SafeSites: don't cache results for more than one hour
    BUG=475513
    
    Review URL: https://codereview.chromium.org/1403153004
    
    Cr-Commit-Position: refs/heads/master@{#355048}

--- chrome/browser/supervised_user/experimental/supervised_user_async_url_checker.h
+++ chrome/browser/supervised_user/experimental/supervised_user_async_url_checker.h
@@ -6,12 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SUPERVISED_USER_EXPERIMENTAL_SUPERVISED_USER_ASYNC_URL_CHECKER_H_
 #define CHROME_BROWSER_SUPERVISED_USER_EXPERIMENTAL_SUPERVISED_USER_ASYNC_URL_CHECKER_H_
 
-#include <string>
-
 #include "base/callback_forward.h"
 #include "base/containers/mru_cache.h"
 #include "base/macros.h"
 #include "base/memory/scoped_vector.h"
+#include "base/time/time.h"
 #include "chrome/browser/supervised_user/supervised_user_url_filter.h"
 #include "net/url_request/url_fetcher_delegate.h"
 #include "url/gurl.h"
@@ -21,16 +20,16 @@ class URLFetcher;
 class URLRequestContextGetter;
 }
 
-// This class checks against an online service (currently a Custom Search
-// Engine using SafeSearch) whether a given URL is safe to visit for a
-// supervised user, and returns the result asynchronously via a callback.
+// This class checks against an online service (the SafeSearch API) whether a
+// given URL is safe to visit for a supervised user, and returns the result
+// asynchronously via a callback.
 class SupervisedUserAsyncURLChecker : net::URLFetcherDelegate {
  public:
   // Returns whether |url| should be blocked. Called from CheckURL.
-  typedef base::Callback<void(const GURL&,
-                              SupervisedUserURLFilter::FilteringBehavior,
-                              bool /* uncertain */)>
-      CheckCallback;
+  using CheckCallback =
+      base::Callback<void(const GURL&,
+                          SupervisedUserURLFilter::FilteringBehavior,
+                          bool /* uncertain */)>;
 
   SupervisedUserAsyncURLChecker(net::URLRequestContextGetter* context);
   SupervisedUserAsyncURLChecker(net::URLRequestContextGetter* context,
@@ -40,6 +39,10 @@ class SupervisedUserAsyncURLChecker : net::URLFetcherDelegate {
   // Returns whether |callback| was run synchronously.
   bool CheckURL(const GURL& url, const CheckCallback& callback);
 
+  void SetCacheTimeoutForTesting(const base::TimeDelta& timeout) {
+    cache_timeout_ = timeout;
+  }
+
  private:
   struct Check;
   struct CheckResult {
@@ -47,6 +50,7 @@ class SupervisedUserAsyncURLChecker : net::URLFetcherDelegate {
                 bool uncertain);
     SupervisedUserURLFilter::FilteringBehavior behavior;
     bool uncertain;
+    base::TimeTicks timestamp;
   };
 
   // net::URLFetcherDelegate implementation.
@@ -57,6 +61,7 @@ class SupervisedUserAsyncURLChecker : net::URLFetcherDelegate {
   ScopedVector<Check> checks_in_progress_;
 
   base::MRUCache<GURL, CheckResult> cache_;
+  base::TimeDelta cache_timeout_;
 
   DISALLOW_COPY_AND_ASSIGN(SupervisedUserAsyncURLChecker);
 };
