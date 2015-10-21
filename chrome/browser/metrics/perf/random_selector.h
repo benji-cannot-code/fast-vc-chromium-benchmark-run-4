435f382933b9be61e132eb04ccb31ce638b26801
dhsharp <dhsharp@chromium.org>
2015-10-21 12:00:44 -0700

    PerfProvider: Get collection parameters from variation parameters
    Add a field trial for perf collection that allows overriding the
    collection parameters and perf command lines. This will allow fast
    reconfiguration of these parameters instead of waiting for the full
    ChromeOS release cycle.
    
    BUG=538759
    
    Review URL: https://codereview.chromium.org/1392153003
    
    Cr-Commit-Position: refs/heads/master@{#355359}

--- chrome/browser/metrics/perf/random_selector.h
+++ chrome/browser/metrics/perf/random_selector.h
@@ -36,6 +36,10 @@ class RandomSelector {
       : weight(weight), value(value) {
     }
 
+    bool operator==(const WeightAndValue& other) const {
+      return weight == other.weight && value == other.value;
+    }
+
     // Probability weight for selecting this value.
     double weight;
     // Value to be returned by Select(), if selected.
@@ -45,8 +49,10 @@ class RandomSelector {
   RandomSelector();
   virtual ~RandomSelector();
 
-  // Set the probabilities for various strings.
-  void SetOdds(const std::vector<WeightAndValue>& odds);
+  // Set the probabilities for various strings. Returns false and doesn't
+  // modify the values if odds contains any invalid weights (<=0.0) or if
+  // odds is empty.
+  bool SetOdds(const std::vector<WeightAndValue>& odds);
 
   // Randomly select one of the values from the set.
   const std::string& Select();
@@ -56,7 +62,10 @@ class RandomSelector {
     return odds_.size();
   }
 
-  // Sum of the |weight| fields in the vector.
+  const std::vector<WeightAndValue>& odds() const { return odds_; }
+
+  // Sum of the |weight| fields in the vector. Returns -1.0 if odds contains any
+  // weight <= 0.0.
   static double SumWeights(const std::vector<WeightAndValue>& odds);
 
  private:
@@ -76,5 +85,8 @@ class RandomSelector {
 
   DISALLOW_COPY_AND_ASSIGN(RandomSelector);
 };
+
+::std::ostream& operator<<(
+    ::std::ostream& os, const RandomSelector::WeightAndValue& value);
 
 #endif  // CHROME_BROWSER_METRICS_PERF_RANDOM_SELECTOR_H_
