adccb93e82a8e611314c215a12b010e55e1f5aef
ruuda <ruuda@google.com>
2015-10-21 09:13:16 -0700

    [Tracing] Add hook to PartitionAlloc for heap profiling
    This modifies PartitionAlloc to call a function on allocation and free,
    which will be used for heap profiling. Care has been taken to impact the
    fast path as little as possible. https://crbug.com/530502 suggests that
    there is no significant performance impact.
    
    This is part of the heap profiler in chrome://tracing.
    
    BUG=524631
    
    Review URL: https://codereview.chromium.org/1391933004
    
    Cr-Commit-Position: refs/heads/master@{#355304}

--- base/trace_event/memory_dump_manager.cc
+++ base/trace_event/memory_dump_manager.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/thread_task_runner_handle.h"
 #include "base/trace_event/memory_dump_provider.h"
 #include "base/trace_event/memory_dump_session_state.h"
+#include "base/trace_event/memory_profiler_allocation_context.h"
 #include "base/trace_event/process_memory_dump.h"
 #include "base/trace_event/trace_event_argument.h"
 #include "build/build_config.h"
@@ -115,6 +116,9 @@ MemoryDumpManager::MemoryDumpManager()
                                 ? CommandLine::ForCurrentProcess()->HasSwitch(
                                       switches::kEnableHeapProfiling)
                                 : false;
+
+  if (heap_profiling_enabled_)
+    AllocationContextTracker::SetCaptureEnabled(true);
 }
 
 MemoryDumpManager::~MemoryDumpManager() {
