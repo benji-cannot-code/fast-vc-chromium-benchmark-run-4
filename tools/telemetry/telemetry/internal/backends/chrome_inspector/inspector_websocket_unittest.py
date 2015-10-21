5c041922abd7799aae542e381996eb6518a9f9cb
erikchen <erikchen@chromium.org>
2015-10-21 16:43:35 -0700

    telemetry: Handle errno.EAGAIN in inspector_websocket.py
    BUG=545853
    
    Review URL: https://codereview.chromium.org/1413573005
    
    Cr-Commit-Position: refs/heads/master@{#355437}

--- tools/telemetry/telemetry/internal/backends/chrome_inspector/inspector_websocket_unittest.py
+++ tools/telemetry/telemetry/internal/backends/chrome_inspector/inspector_websocket_unittest.py
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import errno
+import socket
 import unittest
 
 from telemetry import decorators
@@ -38,6 +40,8 @@ class FakeSocket(object):
       raise websocket.WebSocketTimeoutException()
 
     self._mock_timer.SetTime(time)
+    if isinstance(response, Exception):
+      raise response
     return response
 
   def settimeout(self, timeout):
@@ -156,3 +160,16 @@ class InspectorWebsocketUnittest(unittest.TestCase):
     fake_socket.AddResponse('{"id": 6666666, "result": {}}', 1)
     inspector.DispatchNotifications()
     self.assertEqual(2, response_count[0])
+
+  def testEAGAIN(self):
+    inspector = inspector_websocket.InspectorWebsocket()
+    fake_socket = FakeSocket(self._mock_timer)
+    # pylint: disable=protected-access
+    inspector._socket = fake_socket
+
+    error = socket.error(errno.EAGAIN, "error string")
+    fake_socket.AddResponse(error, 4)
+    fake_socket.AddResponse('{"asdf": "qwer"}', 5)
+
+    result = inspector._Receive()
+    self.assertEqual(result, {"asdf" : "qwer"})
