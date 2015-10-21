40b7c70481e526d8025b424323a0e2c0e19527c9
hayato <hayato@chromium.org>
2015-10-20 22:22:36 -0700

    Disable insertion points in v1 shadow trees.
    Make <content> and <shadow> inert in v1 shadow trees.
    In other words, v0 distribution doesn't happen in v1 shadow trees.
    
    See the rule2 of the doc [1].
    
    [1] https://github.com/w3c/webcomponents/blob/gh-pages/proposals/shadow-dom-v1-in-blink.md#unified-distribution
    
    BUG=531990
    
    Review URL: https://codereview.chromium.org/1408203003
    
    Cr-Commit-Position: refs/heads/master@{#355239}

--- third_party/WebKit/Source/core/dom/shadow/ElementShadow.cpp
+++ third_party/WebKit/Source/core/dom/shadow/ElementShadow.cpp
@@ -277,6 +277,14 @@ const DestinationInsertionPoints* ElementShadow::destinationInsertionPointsFor(c
 
 void ElementShadow::distribute()
 {
+    if (isV1())
+        distributeV1();
+    else
+        distributeV0();
+}
+
+void ElementShadow::distributeV0()
+{
     host()->setNeedsStyleRecalc(SubtreeStyleChange, StyleChangeReasonForTracing::create(StyleChangeReason::Shadow));
     WillBeHeapVector<RawPtrWillBeMember<HTMLShadowElement>, 32> shadowInsertionPoints;
     DistributionPool pool(*host());
@@ -317,6 +325,11 @@ void ElementShadow::distribute()
             shadow->setNeedsDistributionRecalc();
     }
     InspectorInstrumentation::didPerformElementShadowDistribution(host());
+}
+
+void ElementShadow::distributeV1()
+{
+    // TODO(hayato): Implement this
 }
 
 void ElementShadow::didDistributeNode(const Node* node, InsertionPoint* insertionPoint)
