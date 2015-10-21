d4d589e41eb5d468fa9c119af5ad2dc36a24b246
danakj <danakj@chromium.org>
2015-10-21 10:53:41 -0700

    Make conversions from gfx::Point to PointF explicit in ui/gfx/.
    This implicit conversion operator will be going away. Also add includes
    for Point and PointF to render_text_unittest.cc since it uses those
    types.
    
    R=enne, vmpstr
    BUG=342848
    
    Review URL: https://codereview.chromium.org/1417043004
    
    Cr-Commit-Position: refs/heads/master@{#355333}

--- ui/gfx/render_text_unittest.cc
+++ ui/gfx/render_text_unittest.cc
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/font.h"
+#include "ui/gfx/geometry/point.h"
+#include "ui/gfx/geometry/point_f.h"
 #include "ui/gfx/range/range.h"
 #include "ui/gfx/range/range_f.h"
 #include "ui/gfx/render_text_harfbuzz.h"
