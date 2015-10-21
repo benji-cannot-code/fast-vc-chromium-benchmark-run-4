d6c49d3f73c10bab6ef52665205dba81fc56c2a2
kmackay <kmackay@chromium.org>
2015-10-20 17:40:44 -0700

    [Chromecast] Fix a crash in CastAudioOutputStream
    If OnDecoderError() was called by the backend, and then the
    backend completes a PushBuffer call, the completion callback
    would be null, resulting in a segfault. This is now avoided
    by checking for a null callback.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1417043003
    
    Cr-Commit-Position: refs/heads/master@{#355204}

--- chromecast/media/audio/cast_audio_output_stream.cc
+++ chromecast/media/audio/cast_audio_output_stream.cc
@@ -136,9 +136,9 @@ class CastAudioOutputStream::Backend : public MediaPipelineBackend::Delegate {
 
     backend_buffer_.set_buffer(decoder_buffer);
 
+    completion_cb_ = completion_cb;
     MediaPipelineBackend::BufferStatus status =
         decoder_->PushBuffer(nullptr /* decrypt_context */, &backend_buffer_);
-    completion_cb_ = completion_cb;
     if (status != MediaPipelineBackend::kBufferPending)
       OnPushBufferComplete(decoder_, status);
   }
@@ -158,6 +158,10 @@ class CastAudioOutputStream::Backend : public MediaPipelineBackend::Delegate {
       MediaPipelineBackend::BufferStatus status) override {
     DCHECK(thread_checker_.CalledOnValidThread());
     DCHECK_NE(status, MediaPipelineBackend::kBufferPending);
+
+    // |completion_cb_| may be null if OnDecoderError was called.
+    if (completion_cb_.is_null())
+      return;
 
     base::ResetAndReturn(&completion_cb_)
         .Run(status == MediaPipelineBackend::kBufferSuccess);
