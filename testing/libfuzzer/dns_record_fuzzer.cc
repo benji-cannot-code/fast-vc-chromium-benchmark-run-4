26a7e14595898f92df9be78a20085961a4cb5c01
krasin <krasin@google.com>
2015-10-20 18:37:43 -0700

    Add the first bunch of libFuzzer fuzzers to feed to ClusterFuzz.
    Some of them find unfixed bugs, none of them are security bugs.
    
    Note: some of these fuzzers require -fsanitize=signed-integer-overflow
    flag to be set in order to find a bug.
    
    See https://crbug.com/542881 for example.
    
    BUG=539572
    
    Review URL: https://codereview.chromium.org/1419583004
    
    Cr-Commit-Position: refs/heads/master@{#355215}

---
+++ testing/libfuzzer/dns_record_fuzzer.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include "net/dns/dns_response.h"
+
+// Entry point for LibFuzzer.
+extern "C" void LLVMFuzzerTestOneInput(const unsigned char* data,
+                                       unsigned long size) {
+  std::string out;
+  net::DnsRecordParser parser(data, size, 0);
+  if (!parser.IsValid()) {
+    return;
+  }
+  net::DnsResourceRecord record;
+  while (parser.ReadRecord(&record)) {
+  }
+}
