eaa09eafa65f646354097c9361a0c375dbb50fa2
Ian Wen <ianwen@chromium.org>
2015-10-21 16:05:17 -0700

    Add Rounded Corners to snackbars on Tablet
    Per Material Design, floating snackbars should have rounded corners with
    radius of 2dp. This CL implements this detail.
    
    BUG=545258
    R=dfalcantara@chromium.org
    
    Review URL: https://codereview.chromium.org/1414423002 .
    
    Cr-Commit-Position: refs/heads/master@{#355424}

--- chrome/android/java/src/org/chromium/chrome/browser/snackbar/SnackbarPopupWindow.java
+++ chrome/android/java/src/org/chromium/chrome/browser/snackbar/SnackbarPopupWindow.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.snackbar;
 
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
 import org.chromium.ui.base.DeviceFormFactor;
 
@@ -68,8 +70,23 @@ class SnackbarPopupWindow extends PopupWindow {
         mMessageView.setTemplate(snackbar.getTemplateText());
         setViewText(mMessageView, snackbar.getText(), animate);
         String actionText = snackbar.getActionText();
+
+        View view = getContentView();
+        int backgroundColor = snackbar.getBackgroundColor();
+        if (backgroundColor == 0) {
+            backgroundColor = ApiCompatibilityUtils.getColor(view.getResources(),
+                    R.color.snackbar_background_color);
+        }
+
+        if (DeviceFormFactor.isTablet(view.getContext())) {
+            // On tablet, snackbar popups have rounded corners.
+            view.setBackgroundResource(R.drawable.snackbar_background);
+            ((GradientDrawable) view.getBackground()).setColor(backgroundColor);
+        } else {
+            view.setBackgroundColor(backgroundColor);
+        }
+
         if (snackbar.getBackgroundColor() != 0) {
-            View view = getContentView();
             view.setBackgroundColor(snackbar.getBackgroundColor());
         }
         if (actionText != null) {
