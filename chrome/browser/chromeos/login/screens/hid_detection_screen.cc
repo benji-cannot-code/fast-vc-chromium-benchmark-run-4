83e9d9d60003ed5c374788e42df1f3025941e82c
glevin <glevin@chromium.org>
2015-10-21 07:32:34 -0700

    Fix OOBE detected mouse type
    BUG=499882
    TEST=Start a nyan-kitty device with no USB mouse.  When OOBE device
    screen appears, verify that it doesn't report a connected USB mouse.
    See bug for details.
    
    Review URL: https://codereview.chromium.org/1414763006
    
    Cr-Commit-Position: refs/heads/master@{#355289}

--- chrome/browser/chromeos/login/screens/hid_detection_screen.cc
+++ chrome/browser/chromeos/login/screens/hid_detection_screen.cc
@@ -20,7 +20,8 @@ namespace {
 
 // Possible ui-states for device-blocks.
 const char kSearchingState[] = "searching";
-const char kUSBConnectedState[] = "connected";
+const char kUSBState[] = "usb";
+const char kConnectedState[] = "connected";
 const char kBTPairedState[] = "paired";
 const char kBTPairingState[] = "pairing";
 
@@ -321,8 +322,10 @@ void HIDDetectionScreen::SendPointingDeviceNotification() {
     state = kSearchingState;
   else if (pointing_device_connect_type_ == InputDeviceInfo::TYPE_BLUETOOTH)
     state = kBTPairedState;
+  else if (pointing_device_connect_type_ == InputDeviceInfo::TYPE_USB)
+    state = kUSBState;
   else
-    state = kUSBConnectedState;
+    state = kConnectedState;
   GetContextEditor().SetString(kContextKeyMouseState, state)
                     .SetBoolean(
       kContextKeyContinueButtonEnabled,
@@ -352,7 +355,7 @@ void HIDDetectionScreen::SendKeyboardDeviceNotification() {
                     IDS_HID_DETECTION_PAIRED_BLUETOOTH_KEYBOARD,
                     base::UTF8ToUTF16(keyboard_device_name_)));
     } else {
-      editor.SetString(kContextKeyKeyboardState, kUSBConnectedState);
+      editor.SetString(kContextKeyKeyboardState, kUSBState);
     }
   }
   editor.SetString(kContextKeyKeyboardDeviceName, keyboard_device_name_)
