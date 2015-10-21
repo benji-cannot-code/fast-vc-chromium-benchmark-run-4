6761d699ff5bdca41cbb0fcf8d1040cfb844b6f6
ortuno <ortuno@chromium.org>
2015-10-20 17:20:13 -0700

    bluetooth: Refactor repeated code used to find a Bluetooth object from its ID.
    getCharacteristic, getCharacteristics, getService, getServices, readValue,
    writeValue, startNotifications, etc use the same code to find a Bluetooth Object
    based on the object's instance ID. We refactor bluetooth_dispatcher_host so that
    we don't have to repeat so much code.
    
    BUG=545153
    
    Review URL: https://codereview.chromium.org/1397983004
    
    Cr-Commit-Position: refs/heads/master@{#355199}

--- content/browser/bluetooth/bluetooth_metrics.cc
+++ content/browser/bluetooth/bluetooth_metrics.cc
@@ -104,6 +104,11 @@ void RecordConnectGATTOutcome(UMAConnectGATTOutcome outcome) {
                             static_cast<int>(UMAConnectGATTOutcome::COUNT));
 }
 
+void RecordConnectGATTOutcome(CacheQueryOutcome outcome) {
+  DCHECK(outcome == CacheQueryOutcome::NO_DEVICE);
+  RecordConnectGATTOutcome(UMAConnectGATTOutcome::NO_DEVICE);
+}
+
 void RecordConnectGATTTimeSuccess(const base::TimeDelta& duration) {
   UMA_HISTOGRAM_MEDIUM_TIMES("Bluetooth.Web.ConnectGATT.TimeSuccess", duration);
 }
@@ -127,12 +132,35 @@ void RecordGetPrimaryServiceOutcome(UMAGetPrimaryServiceOutcome outcome) {
       static_cast<int>(UMAGetPrimaryServiceOutcome::COUNT));
 }
 
+void RecordGetPrimaryServiceOutcome(CacheQueryOutcome outcome) {
+  DCHECK(outcome == CacheQueryOutcome::NO_DEVICE);
+  RecordGetPrimaryServiceOutcome(UMAGetPrimaryServiceOutcome::NO_DEVICE);
+}
+
 // getCharacteristic
 
 void RecordGetCharacteristicOutcome(UMAGetCharacteristicOutcome outcome) {
   UMA_HISTOGRAM_ENUMERATION(
       "Bluetooth.Web.GetCharacteristic.Outcome", static_cast<int>(outcome),
       static_cast<int>(UMAGetCharacteristicOutcome::COUNT));
+}
+
+void RecordGetCharacteristicOutcome(CacheQueryOutcome outcome) {
+  switch (outcome) {
+    case CacheQueryOutcome::SUCCESS:
+    case CacheQueryOutcome::BAD_RENDERER:
+      NOTREACHED() << "No need to record a success or renderer crash";
+      return;
+    case CacheQueryOutcome::NO_DEVICE:
+      RecordGetCharacteristicOutcome(UMAGetCharacteristicOutcome::NO_DEVICE);
+      return;
+    case CacheQueryOutcome::NO_SERVICE:
+      RecordGetCharacteristicOutcome(UMAGetCharacteristicOutcome::NO_SERVICE);
+      return;
+    case CacheQueryOutcome::NO_CHARACTERISTIC:
+      NOTREACHED();
+      return;
+  }
 }
 
 void RecordGetCharacteristicCharacteristic(const std::string& characteristic) {
@@ -161,6 +189,24 @@ void RecordGATTOperationOutcome(UMAGATTOperation operation,
   NOTREACHED();
 }
 
+static UMAGATTOperationOutcome TranslateCacheQueryOutcomeToGATTOperationOutcome(
+    CacheQueryOutcome outcome) {
+  switch (outcome) {
+    case CacheQueryOutcome::SUCCESS:
+    case CacheQueryOutcome::BAD_RENDERER:
+      NOTREACHED() << "No need to record success or renderer crash";
+      return UMAGATTOperationOutcome::NOT_SUPPORTED;
+    case CacheQueryOutcome::NO_DEVICE:
+      return UMAGATTOperationOutcome::NO_DEVICE;
+    case CacheQueryOutcome::NO_SERVICE:
+      return UMAGATTOperationOutcome::NO_SERVICE;
+    case CacheQueryOutcome::NO_CHARACTERISTIC:
+      return UMAGATTOperationOutcome::NO_CHARACTERISTIC;
+  }
+  NOTREACHED() << "No need to record success or renderer crash";
+  return UMAGATTOperationOutcome::NOT_SUPPORTED;
+}
+
 // Characteristic.readValue
 
 // static
@@ -168,6 +214,11 @@ void RecordCharacteristicReadValueOutcome(UMAGATTOperationOutcome outcome) {
   UMA_HISTOGRAM_ENUMERATION("Bluetooth.Web.Characteristic.ReadValue.Outcome",
                             static_cast<int>(outcome),
                             static_cast<int>(UMAGATTOperationOutcome::COUNT));
+}
+
+void RecordCharacteristicReadValueOutcome(CacheQueryOutcome outcome) {
+  RecordCharacteristicReadValueOutcome(
+      TranslateCacheQueryOutcomeToGATTOperationOutcome(outcome));
 }
 
 // Characteristic.writeValue
@@ -178,12 +229,22 @@ void RecordCharacteristicWriteValueOutcome(UMAGATTOperationOutcome outcome) {
                             static_cast<int>(UMAGATTOperationOutcome::COUNT));
 }
 
+void RecordCharacteristicWriteValueOutcome(CacheQueryOutcome outcome) {
+  RecordCharacteristicWriteValueOutcome(
+      TranslateCacheQueryOutcomeToGATTOperationOutcome(outcome));
+}
+
 // Characteristic.startNotifications
 void RecordStartNotificationsOutcome(UMAGATTOperationOutcome outcome) {
   UMA_HISTOGRAM_ENUMERATION(
       "Bluetooth.Web.Characteristic.StartNotifications.Outcome",
       static_cast<int>(outcome),
       static_cast<int>(UMAGATTOperationOutcome::COUNT));
+}
+
+void RecordStartNotificationsOutcome(CacheQueryOutcome outcome) {
+  RecordStartNotificationsOutcome(
+      TranslateCacheQueryOutcomeToGATTOperationOutcome(outcome));
 }
 
 }  // namespace content
