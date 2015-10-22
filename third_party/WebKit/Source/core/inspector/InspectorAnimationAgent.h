5b1cfd90ba7cca50edfdaa3fa4e5eb07f5be49e5
samli <samli@chromium.org>
2015-10-22 10:54:13 -0700

    Devtools Animations: Perform group matching based on CSS rules
    This change improves the matching process to look at the CSS rules which
    resulted in the applied animation. If the same CSS rules are used to apply
    the same animations, they are considered a match, even if they are applied
    to different elements.
    
    By performing this matching, semantic animation effects can be grouped
    together allowing the user to efficiently navigate animation effects.
    
    BUG=447082
    
    Review URL: https://codereview.chromium.org/1396353002
    
    Cr-Commit-Position: refs/heads/master@{#355578}

--- third_party/WebKit/Source/core/inspector/InspectorAnimationAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorAnimationAgent.h
@@ -21,15 +21,16 @@ class AnimationTimeline;
 class Element;
 class InjectedScriptManager;
 class InspectedFrames;
+class InspectorCSSAgent;
 class InspectorDOMAgent;
 class TimingFunction;
 
 class CORE_EXPORT InspectorAnimationAgent final : public InspectorBaseAgent<InspectorAnimationAgent, InspectorFrontend::Animation>, public InspectorBackendDispatcher::AnimationCommandHandler {
     WTF_MAKE_NONCOPYABLE(InspectorAnimationAgent);
 public:
-    static PassOwnPtrWillBeRawPtr<InspectorAnimationAgent> create(InspectedFrames* inspectedFrames, InspectorDOMAgent* domAgent, InjectedScriptManager* injectedScriptManager)
+    static PassOwnPtrWillBeRawPtr<InspectorAnimationAgent> create(InspectedFrames* inspectedFrames, InspectorDOMAgent* domAgent, InspectorCSSAgent* cssAgent, InjectedScriptManager* injectedScriptManager)
     {
-        return adoptPtrWillBeNoop(new InspectorAnimationAgent(inspectedFrames, domAgent, injectedScriptManager));
+        return adoptPtrWillBeNoop(new InspectorAnimationAgent(inspectedFrames, domAgent, cssAgent, injectedScriptManager));
     }
 
     // Base agent methods.
@@ -59,7 +60,7 @@ public:
     DECLARE_VIRTUAL_TRACE();
 
 private:
-    InspectorAnimationAgent(InspectedFrames*, InspectorDOMAgent*, InjectedScriptManager*);
+    InspectorAnimationAgent(InspectedFrames*, InspectorDOMAgent*, InspectorCSSAgent*, InjectedScriptManager*);
 
     typedef TypeBuilder::Animation::Animation::Type::Enum AnimationType;
 
@@ -68,9 +69,11 @@ private:
     double normalizedStartTime(Animation&);
     AnimationTimeline& referenceTimeline();
     Animation* animationClone(Animation*);
+    String createCSSId(Animation&);
 
     InspectedFrames* m_inspectedFrames;
     RawPtrWillBeMember<InspectorDOMAgent> m_domAgent;
+    RawPtrWillBeMember<InspectorCSSAgent> m_cssAgent;
     RawPtrWillBeMember<InjectedScriptManager> m_injectedScriptManager;
     PersistentHeapHashMapWillBeHeapHashMap<String, Member<Animation>> m_idToAnimation;
     PersistentHeapHashMapWillBeHeapHashMap<String, Member<Animation>> m_idToAnimationClone;
