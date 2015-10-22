a4fbba06f84b6291c6c28ec42460ed8d4ae2396a
jaydasika <jaydasika@chromium.org>
2015-10-22 10:46:44 -0700

    gfx::Transform::IsBackVisible compares (cofactor33*determinant) with 0 to determine if a transform is back face visible. In the bug, one of the layer's transform ends up with an extremely small close to zero negative number though the actual value should be 0. This results in the function returning the wrong value.
    BUG=538290
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1421603002
    
    Cr-Commit-Position: refs/heads/master@{#355574}

--- ui/gfx/transform.cc
+++ ui/gfx/transform.cc
@@ -25,8 +25,7 @@ namespace gfx {
 
 namespace {
 
-// Taken from SkMatrix44.
-const SkMScalar kEpsilon = 1e-8f;
+const SkMScalar kEpsilon = std::numeric_limits<float>::epsilon();
 
 SkMScalar TanDegrees(double degrees) {
   double radians = degrees * M_PI / 180;
@@ -271,7 +270,7 @@ bool Transform::IsBackFaceVisible() const {
   double determinant = matrix_.determinant();
 
   // If matrix was not invertible, then just assume back face is not visible.
-  if (std::abs(determinant) <= kEpsilon)
+  if (determinant == 0)
     return false;
 
   // Compute the cofactor of the 3rd row, 3rd column.
@@ -304,7 +303,7 @@ bool Transform::IsBackFaceVisible() const {
   // Technically the transformed z component is cofactor33 / determinant.  But
   // we can avoid the costly division because we only care about the resulting
   // +/- sign; we can check this equivalently by multiplication.
-  return cofactor33 * determinant < 0;
+  return cofactor33 * determinant < -kEpsilon;
 }
 
 bool Transform::GetInverse(Transform* transform) const {
