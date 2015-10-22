f2b3a1f7a355acd6d4bd3be98b82e2bbd870ae8f
lpique <lpique@chromium.org>
2015-10-22 16:20:42 -0700

    Promote PPB_VideoDecoder 1.1 to Stable
    Works just fine in ARC.
    
    BUG=520323
    
    Review URL: https://codereview.chromium.org/1369203003
    
    Cr-Commit-Position: refs/heads/master@{#355667}

--- ppapi/c/ppb_video_decoder.h
+++ ppapi/c/ppb_video_decoder.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From ppb_video_decoder.idl modified Thu Aug  6 14:15:48 2015. */
+/* From ppb_video_decoder.idl modified Mon Sep 28 15:23:30 2015. */
 
 #ifndef PPAPI_C_PPB_VIDEO_DECODER_H_
 #define PPAPI_C_PPB_VIDEO_DECODER_H_
@@ -23,8 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PPB_VIDEODECODER_INTERFACE_0_1 "PPB_VideoDecoder;0.1"
 #define PPB_VIDEODECODER_INTERFACE_0_2 "PPB_VideoDecoder;0.2"
 #define PPB_VIDEODECODER_INTERFACE_1_0 "PPB_VideoDecoder;1.0"
-#define PPB_VIDEODECODER_INTERFACE_1_1 "PPB_VideoDecoder;1.1" /* dev */
-#define PPB_VIDEODECODER_INTERFACE PPB_VIDEODECODER_INTERFACE_1_0
+#define PPB_VIDEODECODER_INTERFACE_1_1 "PPB_VideoDecoder;1.1"
+#define PPB_VIDEODECODER_INTERFACE PPB_VIDEODECODER_INTERFACE_1_1
 
 /**
  * @file
@@ -59,7 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Chrome and ChromeOS: aac, h264.
  * ChromeOS: mpeg4.
  */
-struct PPB_VideoDecoder_1_1 { /* dev */
+struct PPB_VideoDecoder_1_1 {
   /**
    * Creates a new video decoder resource.
    *
@@ -231,6 +231,8 @@ struct PPB_VideoDecoder_1_1 { /* dev */
                    struct PP_CompletionCallback callback);
 };
 
+typedef struct PPB_VideoDecoder_1_1 PPB_VideoDecoder;
+
 struct PPB_VideoDecoder_0_1 {
   PP_Resource (*Create)(PP_Instance instance);
   PP_Bool (*IsVideoDecoder)(PP_Resource resource);
@@ -302,8 +304,6 @@ struct PPB_VideoDecoder_1_0 {
   int32_t (*Reset)(PP_Resource video_decoder,
                    struct PP_CompletionCallback callback);
 };
-
-typedef struct PPB_VideoDecoder_1_0 PPB_VideoDecoder;
 /**
  * @}
  */
