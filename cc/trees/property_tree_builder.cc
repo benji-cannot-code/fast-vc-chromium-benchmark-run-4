96096c9d775c310821cb154e5bfd267e53d10189
jaydasika <jaydasika@chromium.org>
2015-10-22 13:44:08 -0700

    Update layer scale factor without a property tree rebuild.
    Right now, we store the page scale factor on the transform tree but
    store the device scale factor and layer scale factor on the transform
    node. So, the layer scale factor becomes incorrect when the page scale
    factor gets upadated on the compositor thread because we don't rebuild
    property trees. This CL moves the device scale factor on to the tranform
    tree and calculates layer scale factor on the fly using device scale
    factor and page scale factor.
    
    BUG=546126
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1420613004
    
    Cr-Commit-Position: refs/heads/master@{#355635}

--- cc/trees/property_tree_builder.cc
+++ cc/trees/property_tree_builder.cc
@@ -39,7 +39,6 @@ struct DataForRecursion {
   const LayerType* inner_viewport_scroll_layer;
   const LayerType* outer_viewport_scroll_layer;
   float page_scale_factor;
-  float device_scale_factor;
   bool in_subtree_of_page_scale_layer;
   bool affected_by_inner_viewport_bounds_delta;
   bool affected_by_outer_viewport_bounds_delta;
@@ -307,6 +306,8 @@ bool AddTransformNodeIfNeeded(
 
   node->data.scrolls = is_scrollable;
   node->data.flattens_inherited_transform = data_for_children->should_flatten;
+  node->data.in_subtree_of_page_scale_layer =
+      data_for_children->in_subtree_of_page_scale_layer;
 
   // Surfaces inherently flatten transforms.
   data_for_children->should_flatten =
@@ -341,7 +342,8 @@ bool AddTransformNodeIfNeeded(
   float post_local_scale_factor = 1.0f;
   if (is_root) {
     node->data.post_local = *data_from_ancestor.device_transform;
-    post_local_scale_factor = data_from_ancestor.device_scale_factor;
+    post_local_scale_factor =
+        data_for_children->transform_tree->device_scale_factor();
   }
 
   if (is_page_scale_layer) {
@@ -350,12 +352,8 @@ bool AddTransformNodeIfNeeded(
         data_from_ancestor.page_scale_factor);
   }
 
-  if (has_surface && !is_root) {
+  if (has_surface && !is_root)
     node->data.needs_sublayer_scale = true;
-    node->data.layer_scale_factor = data_from_ancestor.device_scale_factor;
-    if (data_from_ancestor.in_subtree_of_page_scale_layer)
-      node->data.layer_scale_factor *= data_from_ancestor.page_scale_factor;
-  }
 
   node->data.source_node_id = source_index;
   if (is_root) {
@@ -558,7 +556,6 @@ void BuildPropertyTreesTopLevelInternal(
   data_for_recursion.inner_viewport_scroll_layer = inner_viewport_scroll_layer;
   data_for_recursion.outer_viewport_scroll_layer = outer_viewport_scroll_layer;
   data_for_recursion.page_scale_factor = page_scale_factor;
-  data_for_recursion.device_scale_factor = device_scale_factor;
   data_for_recursion.in_subtree_of_page_scale_layer = false;
   data_for_recursion.affected_by_inner_viewport_bounds_delta = false;
   data_for_recursion.affected_by_outer_viewport_bounds_delta = false;
@@ -570,6 +567,8 @@ void BuildPropertyTreesTopLevelInternal(
   data_for_recursion.clip_tree->clear();
   data_for_recursion.effect_tree->clear();
   data_for_recursion.sequence_number = property_trees->sequence_number;
+  data_for_recursion.transform_tree->set_device_scale_factor(
+      device_scale_factor);
 
   ClipNode root_clip;
   root_clip.data.resets_clip = true;
