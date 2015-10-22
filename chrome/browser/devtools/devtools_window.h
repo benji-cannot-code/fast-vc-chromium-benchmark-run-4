1cd5481bd8d1ef46237db4c642bb6f2331d4550f
dgozman <dgozman@chromium.org>
2015-10-22 12:56:28 -0700

    [DevTools] Route "Inspect element" context menu item to the right agent.
    BUG=470451
    TBR=avi
    
    Review URL: https://codereview.chromium.org/1423443003
    
    Cr-Commit-Position: refs/heads/master@{#355623}

--- chrome/browser/devtools/devtools_window.h
+++ chrome/browser/devtools/devtools_window.h
@@ -20,6 +20,7 @@ class DevToolsEventForwarder;
 namespace content {
 class DevToolsAgentHost;
 struct NativeWebKeyboardEvent;
+class RenderFrameHost;
 class RenderViewHost;
 }
 
@@ -109,7 +110,7 @@ class DevToolsWindow : public DevToolsUIBindings::Delegate,
       Profile* profile,
       const scoped_refptr<content::DevToolsAgentHost>& worker_agent);
 
-  static void InspectElement(content::WebContents* inspected_web_contents,
+  static void InspectElement(content::RenderFrameHost* inspected_frame_host,
                              int x,
                              int y);
 
