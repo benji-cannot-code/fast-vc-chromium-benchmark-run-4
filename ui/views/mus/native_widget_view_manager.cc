1b06ad0499dc27a950269725b2e62d3ab5555912
sadrul <sadrul@chromium.org>
2015-10-21 18:30:24 -0700

    mus: Call back to NativeWidgetAura in a few missing places.
    BUG=545035
    
    Review URL: https://codereview.chromium.org/1423513002
    
    Cr-Commit-Position: refs/heads/master@{#355472}

--- ui/views/mus/native_widget_view_manager.cc
+++ ui/views/mus/native_widget_view_manager.cc
@@ -167,6 +167,7 @@ void NativeWidgetViewManager::CenterWindow(const gfx::Size& size) {
 }
 
 void NativeWidgetViewManager::SetBounds(const gfx::Rect& bounds) {
+  NativeWidgetAura::SetBounds(bounds);
   if (!window_manager_)
     return;
   window_manager_->SetBounds(window_->id(), mojo::Rect::From(bounds),
@@ -174,6 +175,7 @@ void NativeWidgetViewManager::SetBounds(const gfx::Rect& bounds) {
 }
 
 void NativeWidgetViewManager::SetSize(const gfx::Size& size) {
+  NativeWidgetAura::SetSize(size);
   if (!window_manager_)
     return;
   mojo::RectPtr bounds(mojo::Rect::New());
