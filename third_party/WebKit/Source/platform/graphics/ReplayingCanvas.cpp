afd474ab670b83043212d281705cd1ac4fecc541
tomhudson <tomhudson@google.com>
2015-10-22 12:40:06 -0700

    Prune unused SkBitmapDevice includes
    None of these files depends on SkBitmapDevice any more.
    
    R=schenney@chromium.org
    
    Review URL: https://codereview.chromium.org/1399363005
    
    Cr-Commit-Position: refs/heads/master@{#355611}

--- third_party/WebKit/Source/platform/graphics/ReplayingCanvas.cpp
+++ third_party/WebKit/Source/platform/graphics/ReplayingCanvas.cpp
@@ -32,8 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "platform/graphics/ReplayingCanvas.h"
 
-#include "third_party/skia/include/core/SkBitmapDevice.h"
-
 namespace blink {
 
 CanvasInterceptor<ReplayingCanvas>::~CanvasInterceptor()
--- third_party/WebKit/Source/web/tests/WebViewTest.cpp
+++ third_party/WebKit/Source/web/tests/WebViewTest.cpp
@@ -89,7 +89,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/web/WebWidget.h"
 #include "public/web/WebWidgetClient.h"
 #include "third_party/skia/include/core/SkBitmap.h"
-#include "third_party/skia/include/core/SkBitmapDevice.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "web/WebLocalFrameImpl.h"
 #include "web/WebSettingsImpl.h"
