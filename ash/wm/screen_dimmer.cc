9def46e0649db067a27e39ea0175f66d4f74d8e6
oshima <oshima@chromium.org>
2015-10-21 19:33:06 -0700

    Add dimming to the background during sign in
    * Add option to put the dim layer at the bottom. Login screen put this dim layer at the bottom of login container containers so that
     dim layer stays during login transition.
    
    BUG=478438
    TEST=ScreenDimmer.DimAtBottom
    
    Review URL: https://codereview.chromium.org/1400153002
    
    Cr-Commit-Position: refs/heads/master@{#355481}

--- ash/wm/screen_dimmer.cc
+++ ash/wm/screen_dimmer.cc
@@ -54,7 +54,10 @@ ScreenDimmer* ScreenDimmer::GetForRoot() {
 }
 
 ScreenDimmer::ScreenDimmer(int container_id)
-    : container_id_(container_id), target_opacity_(0.5f), is_dimming_(false) {
+    : container_id_(container_id),
+      target_opacity_(0.5f),
+      is_dimming_(false),
+      at_bottom_(false) {
   Shell::GetInstance()->AddShellObserver(this);
 }
 
@@ -94,6 +97,10 @@ void ScreenDimmer::Update(bool should_dim) {
         dim = new DimWindow(container);
         dim->SetDimOpacity(target_opacity_);
       }
+      if (at_bottom_)
+        dim->parent()->StackChildAtBottom(dim);
+      else
+        dim->parent()->StackChildAtTop(dim);
       dim->Show();
     } else {
       if (dim) {
