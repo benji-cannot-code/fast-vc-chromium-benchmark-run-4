c66f8baccd947e2ad6d5f4776bb99b2b2cca2049
reveman <reveman@chromium.org>
2015-10-22 09:43:06 -0700

    Re-land: ui: Move GLImage::BindTexImage fallback from GLImage implementations to GLES2CmdDecoder.
    This allows the GPU service to properly track the memory usage
    image backed textures.
    
    It also reduces the complexity of GLImage implementations
    significantly and makes it easier to support format and
    buffer types that require a copy or conversion of data to
    be used for sampling.
    
    This change also includes a few minor GLImage cleanups such
    as removing gfx:: namespace prefix in places where it's not
    needed and making the CopyTexImage GLImage test not part of
    the core GLImage tests as it's optional to support that
    function.
    
    BUG=526298
    TEST=gl_tests --gtest_filter=GpuMemoryBuffer*, gpu_unittests, gl_unittests --gtest_filter=GLImage*
    
    Review URL: https://codereview.chromium.org/1401423003
    
    Cr-Commit-Position: refs/heads/master@{#355552}

--- gpu/command_buffer/service/framebuffer_manager.cc
+++ gpu/command_buffer/service/framebuffer_manager.cc
@@ -88,8 +88,6 @@ class RenderbufferAttachment
     renderbuffer_->AddToSignature(signature);
   }
 
-  void OnWillRenderTo() const override {}
-  void OnDidRenderTo() const override {}
   bool FormsFeedbackLoop(TextureRef* /* texture */,
                          GLint /*level */) const override {
     return false;
@@ -180,7 +178,6 @@ class TextureAttachment
 
   void DetachFromFramebuffer(Framebuffer* framebuffer) const override {
     texture_ref_->texture()->DetachFromFramebuffer();
-    framebuffer->OnTextureRefDetached(texture_ref_.get());
   }
 
   bool ValidForAttachmentType(GLenum attachment_type,
@@ -215,14 +212,6 @@ class TextureAttachment
         texture_ref_.get(), target_, level_, signature);
   }
 
-  void OnWillRenderTo() const override {
-    texture_ref_->texture()->OnWillModifyPixels();
-  }
-
-  void OnDidRenderTo() const override {
-    texture_ref_->texture()->OnDidModifyPixels();
-  }
-
   bool FormsFeedbackLoop(TextureRef* texture, GLint level) const override {
     return texture == texture_ref_.get() && level == level_;
   }
@@ -238,10 +227,6 @@ class TextureAttachment
 
   DISALLOW_COPY_AND_ASSIGN(TextureAttachment);
 };
-
-FramebufferManager::TextureDetachObserver::TextureDetachObserver() {}
-
-FramebufferManager::TextureDetachObserver::~TextureDetachObserver() {}
 
 FramebufferManager::FramebufferManager(
     uint32 max_draw_buffers,
@@ -714,28 +699,6 @@ const Framebuffer::Attachment* Framebuffer::GetReadBufferAttachment() const {
   return GetAttachment(read_buffer_);
 }
 
-void Framebuffer::OnTextureRefDetached(TextureRef* texture) {
-  manager_->OnTextureRefDetached(texture);
-}
-
-void Framebuffer::OnWillRenderTo(GLenum attachment) const {
-  for (AttachmentMap::const_iterator it = attachments_.begin();
-       it != attachments_.end(); ++it) {
-    if (attachment == 0 || attachment == it->first) {
-      it->second->OnWillRenderTo();
-    }
-  }
-}
-
-void Framebuffer::OnDidRenderTo(GLenum attachment) const {
-  for (AttachmentMap::const_iterator it = attachments_.begin();
-       it != attachments_.end(); ++it) {
-    if (attachment == 0 || attachment == it->first) {
-      it->second->OnDidRenderTo();
-    }
-  }
-}
-
 bool FramebufferManager::GetClientId(
     GLuint service_id, GLuint* client_id) const {
   // This doesn't need to be fast. It's only used during slow queries.
@@ -771,16 +734,6 @@ bool FramebufferManager::IsComplete(
   DCHECK(framebuffer);
   return framebuffer->framebuffer_complete_state_count_id() ==
       framebuffer_state_change_count_;
-}
-
-void FramebufferManager::OnTextureRefDetached(TextureRef* texture) {
-  for (TextureDetachObserverVector::iterator it =
-           texture_detach_observers_.begin();
-       it != texture_detach_observers_.end();
-       ++it) {
-    TextureDetachObserver* observer = *it;
-    observer->OnTextureRefDetachedFromFramebuffer(texture);
-  }
 }
 
 }  // namespace gles2
