39433d24da3a3db9416bcf700c89444864b30c59
tkent <tkent@chromium.org>
2015-10-22 15:47:26 -0700

    Revert of Use FrameSelection::selectedText where possible. (patchset #6 id:100001 of https://codereview.chromium.org/1377963004/ )
    Reason for revert:
    Regressed speedometer significantly.  crbug.com/546248
    
    Original issue's description:
    > Use FrameSelection::selectedText where possible.
    >
    > Substitute occurrences of selection -> range -> (text or html) conversions with
    > direct selection -> (text or html) conversions using existing functions.
    >
    > BUG=
    >
    > Committed: https://crrev.com/9c2436064ebdcb9192dfacac96944f2753a2ce78
    > Cr-Commit-Position: refs/heads/master@{#355247}
    
    TBR=yosin@chromium.org,kotenkov@yandex-team.ru
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=546248
    
    Review URL: https://codereview.chromium.org/1417363002
    
    Cr-Commit-Position: refs/heads/master@{#355661}

--- third_party/WebKit/Source/core/editing/FrameSelection.h
+++ third_party/WebKit/Source/core/editing/FrameSelection.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/EphemeralRange.h"
 #include "core/editing/VisiblePosition.h"
 #include "core/editing/VisibleSelection.h"
-#include "core/editing/iterators/TextIteratorFlags.h"
 #include "core/layout/ScrollAlignment.h"
 #include "platform/Timer.h"
 #include "platform/geometry/IntRect.h"
@@ -226,7 +225,7 @@ public:
     void clearTypingStyle();
 
     String selectedHTMLForClipboard() const;
-    String selectedText(TextIteratorBehavior = TextIteratorDefaultBehavior) const;
+    String selectedText() const;
     String selectedTextForClipboard() const;
 
     // The bounds are clipped to the viewport as this is what callers expect.
