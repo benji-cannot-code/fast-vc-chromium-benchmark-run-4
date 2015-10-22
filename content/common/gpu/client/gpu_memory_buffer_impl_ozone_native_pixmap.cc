6ef66ba1f73858e822c0a0e04a1c1d4fd97561aa
mcasas <mcasas@chromium.org>
2015-10-22 13:18:28 -0700

    Reland: GpuMemoryBuffer interface change: Map(**) to Map() and memory(plane); stride(plane)
    Relanding http://crrev.com/1401183003 which had a compile
    error.
    
    Original reviewers:
    TBR=avi@chromium.org, bajones@chromium.org, reveman@chromium.org, dalecurtis@chromium.org, fsamuel@chromium.org, miu@chromium.org
    
    Original CL description:-----------------------------------
    
    GpuMemoryBuffer interface change: Map(**) --> Map() + memory(plane); GetStride(*) --> stride(plane)
    
    And at the same time clean up/simplify/homogeneise
    uses of those APIs.
    
    buffer_format_util.cc:RowSizeForBufferFormatChecked()
    is only used in unittests and internally to the file, this CL
    restricts it to the .cc file. Also a function there is only
    used in test --> added ForTesting() suffix.
    
    BUG=542225
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Committed: https://crrev.com/a11ca23b4ae411b4dc6cd8ff755e366680e80776
    Cr-Commit-Position: refs/heads/master@{#355396}
    
    Review URL: https://codereview.chromium.org/1412223009
    
    Cr-Commit-Position: refs/heads/master@{#355630}

--- content/common/gpu/client/gpu_memory_buffer_impl_ozone_native_pixmap.cc
+++ content/common/gpu/client/gpu_memory_buffer_impl_ozone_native_pixmap.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/gpu/client/gpu_memory_buffer_impl_ozone_native_pixmap.h"
 
 #include "content/common/gpu/gpu_memory_buffer_factory_ozone_native_pixmap.h"
+#include "ui/gfx/buffer_format_util.h"
 #include "ui/ozone/public/client_native_pixmap_factory.h"
 #include "ui/ozone/public/native_pixmap.h"
 #include "ui/ozone/public/ozone_platform.h"
@@ -70,19 +71,31 @@ base::Closure GpuMemoryBufferImplOzoneNativePixmap::AllocateForTesting(
   return base::Bind(&FreeNativePixmapForTesting, pixmap);
 }
 
-bool GpuMemoryBufferImplOzoneNativePixmap::Map(void** data) {
-  *data = pixmap_->Map();
+bool GpuMemoryBufferImplOzoneNativePixmap::Map() {
+  DCHECK(!mapped_);
+  if (!pixmap_->Map())
+    return false;
   mapped_ = true;
   return mapped_;
 }
 
+void* GpuMemoryBufferImplOzoneNativePixmap::memory(size_t plane) {
+  DCHECK(mapped_);
+  DCHECK_LT(plane, gfx::NumberOfPlanesForBufferFormat(format_));
+  return pixmap_->Map();
+}
+
 void GpuMemoryBufferImplOzoneNativePixmap::Unmap() {
+  DCHECK(mapped_);
   pixmap_->Unmap();
   mapped_ = false;
 }
 
-void GpuMemoryBufferImplOzoneNativePixmap::GetStride(int* stride) const {
-  pixmap_->GetStride(stride);
+int GpuMemoryBufferImplOzoneNativePixmap::stride(size_t plane) const {
+  DCHECK_LT(plane, gfx::NumberOfPlanesForBufferFormat(format_));
+  int stride;
+  pixmap_->GetStride(&stride);
+  return stride;
 }
 
 gfx::GpuMemoryBufferHandle GpuMemoryBufferImplOzoneNativePixmap::GetHandle()
