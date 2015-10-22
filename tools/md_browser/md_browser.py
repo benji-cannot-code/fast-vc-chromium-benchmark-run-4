447c6772d0b0efbb566bf03f6bb08552a98c8999
yutak <yutak@chromium.org>
2015-10-21 22:00:49 -0700

    md_browser: Implement Gitiles blocks notation.
    This patch implements an extention for Python-Markdown which processes
    Gitiles' extended blocks notation, such as:
    
        *** note
        ...
        ***
    
    Manually tested with existing documentation containing this syntax, such
    as docs/linux_debugging.md.
    
    R=nodir@chromium.org, dpranke@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/1414863003
    
    Cr-Commit-Position: refs/heads/master@{#355490}

--- tools/md_browser/md_browser.py
+++ tools/md_browser/md_browser.py
@@ -75,6 +75,7 @@ class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         'markdown.extensions.fenced_code',
         'markdown.extensions.tables',
         'markdown.extensions.toc',
+        'gitiles_ext_blocks',
     ]
 
     contents = self._Read(path[1:])
