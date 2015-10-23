57749f3e27d8fab5adac622dc3aa980d5737c825
dalecurtis <dalecurtis@chromium.org>
2015-10-23 14:16:42 -0700

    Revert of Add dimming to the background during sign in  (patchset #4 id:160001 of https://codereview.chromium.org/1400153002/ )
    Reason for revert:
    https://code.google.com/p/chromium/issues/detail?id=547178
    
    Original issue's description:
    > Add dimming to the background during sign in
    >
    > * Add option to put the dim layer at the bottom. Login screen put this dim layer at the bottom of login container containers so that
    >  dim layer stays during login transition.
    >
    > BUG=478438
    > TEST=ScreenDimmer.DimAtBottom
    >
    > Committed: https://crrev.com/c527600749bfc6970ba39e4ed6b24404b0f7b256
    > Cr-Commit-Position: refs/heads/master@{#355481}
    
    TBR=dzhioev@chromium.org,alemate@chromium.org,oshima@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=478438
    
    Review URL: https://codereview.chromium.org/1424513003
    
    Cr-Commit-Position: refs/heads/master@{#355885}

--- chrome/browser/ui/webui/chromeos/login/oobe_ui.cc
+++ chrome/browser/ui/webui/chromeos/login/oobe_ui.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
 
-#include "ash/shell_window_ids.h"
-#include "ash/wm/screen_dimmer.h"
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
@@ -384,9 +382,6 @@ OobeUI::OobeUI(content::WebUI* web_ui, const GURL& url)
 OobeUI::~OobeUI() {
   core_handler_->SetDelegate(nullptr);
   network_dropdown_handler_->RemoveObserver(error_screen_handler_);
-  ash::ScreenDimmer::GetForContainer(
-      ash::kShellWindowId_LockScreenContainersContainer)
-      ->SetDimming(false);
 }
 
 CoreOobeActor* OobeUI::GetCoreOobeActor() {
@@ -545,12 +540,6 @@ void OobeUI::InitializeScreenMaps() {
   screen_names_[SCREEN_OOBE_HOST_PAIRING] = kScreenHostPairing;
   screen_names_[SCREEN_DEVICE_DISABLED] = kScreenDeviceDisabled;
 
-  dim_overlay_screen_ids_.push_back(SCREEN_CONFIRM_PASSWORD);
-  dim_overlay_screen_ids_.push_back(SCREEN_GAIA_SIGNIN);
-  dim_overlay_screen_ids_.push_back(SCREEN_OOBE_ENROLLMENT);
-  dim_overlay_screen_ids_.push_back(SCREEN_PASSWORD_CHANGED);
-  dim_overlay_screen_ids_.push_back(SCREEN_USER_IMAGE_PICKER);
-
   screen_ids_.clear();
   for (size_t i = 0; i < screen_names_.size(); ++i)
     screen_ids_[screen_names_[i]] = static_cast<Screen>(i);
@@ -646,17 +635,7 @@ void OobeUI::OnCurrentScreenChanged(const std::string& screen) {
   previous_screen_ = current_screen_;
   DCHECK(screen_ids_.count(screen))
       << "Screen should be registered in InitializeScreenMaps()";
-  LOG(ERROR) << "OnCurrentScreenChanged:" << screen;
   Screen new_screen = screen_ids_[screen];
-
-  bool should_dim =
-      std::find(dim_overlay_screen_ids_.begin(), dim_overlay_screen_ids_.end(),
-                new_screen) != dim_overlay_screen_ids_.end();
-  ash::ScreenDimmer* screen_dimmer = ash::ScreenDimmer::GetForContainer(
-      ash::kShellWindowId_LockScreenContainersContainer);
-  screen_dimmer->set_at_bottom(true);
-  screen_dimmer->SetDimming(should_dim);
-
   FOR_EACH_OBSERVER(Observer,
                     observer_list_,
                     OnCurrentScreenChanged(current_screen_, new_screen));
