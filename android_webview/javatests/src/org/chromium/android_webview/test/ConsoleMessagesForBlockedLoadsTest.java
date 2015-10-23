5acc562e0ca55002fefba3f60b7ef35dbfb1c595
hush <hush@chromium.org>
2015-10-22 18:56:29 -0700

    Use constants defined in WebSettings instead of hardcoded values.
    The old code was only necessary before L release.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1422563004
    
    Cr-Commit-Position: refs/heads/master@{#355705}

--- android_webview/javatests/src/org/chromium/android_webview/test/ConsoleMessagesForBlockedLoadsTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/ConsoleMessagesForBlockedLoadsTest.java
@@ -8,6 +8,7 @@ package org.chromium.android_webview.test;
 import android.test.suitebuilder.annotation.SmallTest;
 import android.util.Pair;
 import android.webkit.ConsoleMessage;
+import android.webkit.WebSettings;
 
 import org.chromium.android_webview.AwContents;
 import org.chromium.android_webview.AwSettings;
@@ -84,7 +85,7 @@ public class ConsoleMessagesForBlockedLoadsTest extends AwTestBase {
         startWebServer();
         TestWebServer httpsServer = null;
         AwSettings settings = getAwSettingsOnUiThread(mAwContents);
-        settings.setMixedContentMode(AwSettings.MIXED_CONTENT_NEVER_ALLOW);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_NEVER_ALLOW);
         try {
             httpsServer = TestWebServer.startSsl();
             final String imageUrl = mWebServer.setResponseBase64(
