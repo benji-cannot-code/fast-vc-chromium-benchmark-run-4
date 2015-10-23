e6de0de8a3ffd2376e840587091250001c1b618c
dhaddock <dhaddock@chromium.org>
2015-10-22 19:33:12 -0700

    Fix for failing gearmenu browser_tests
    The lastmodified field strikes again! Waiting for files list is failing
    due to a mismatch in last modified field. I've added to ignore it while
    wating for files.
    
    BUG=546567
    
    Review URL: https://codereview.chromium.org/1422773002
    
    Cr-Commit-Position: refs/heads/master@{#355712}

--- ui/file_manager/integration_tests/file_manager/gear_menu.js
+++ ui/file_manager/integration_tests/file_manager/gear_menu.js
@@ -103,7 +103,8 @@ function getTestCaseStepsForHiddenFiles(basicSet, hiddenEntrySet) {
     // Check the hidden files are displayed.
     function(result) {
       remoteCall.waitForFiles(appId, TestEntryInfo.getExpectedRows(
-          hiddenEntrySet)).then(this.next);
+          hiddenEntrySet), {ignoreFileSize: true, ignoreLastModifiedTime: true})
+          .then(this.next);
     },
     // Repeat steps to toggle again.
     function(inAppId) {
@@ -115,8 +116,8 @@ function getTestCaseStepsForHiddenFiles(basicSet, hiddenEntrySet) {
     },
     function(result) {
       chrome.test.assertTrue(result);
-      remoteCall.waitForElement(appId, '#gear-menu:not([hidden])')
-      .then(this.next);
+      remoteCall.waitForElement(appId, '#gear-menu:not([hidden])').then(
+          this.next);
     },
     function(result) {
       remoteCall.waitForElement(appId,
@@ -137,8 +138,8 @@ function getTestCaseStepsForHiddenFiles(basicSet, hiddenEntrySet) {
           '#gear-menu-toggle-hidden-files:not([checked])').then(this.next);
     },
     function(result) {
-      remoteCall.waitForFiles(appId, TestEntryInfo.getExpectedRows(basicSet))
-      .then(this.next);
+      remoteCall.waitForFiles(appId, TestEntryInfo.getExpectedRows(basicSet),
+          {ignoreFileSize: true, ignoreLastModifiedTime: true}).then(this.next);
     },
     function() {
       checkIfNoErrorsOccured(this.next);
@@ -245,7 +246,8 @@ testcase.hideGoogleDocs = function() {
     function(result) {
       chrome.test.assertTrue(result);
       remoteCall.waitForFiles(appId, TestEntryInfo.getExpectedRows(
-          BASIC_DRIVE_ENTRY_SET_WITHOUT_GDOCS)).then(this.next);
+          BASIC_DRIVE_ENTRY_SET_WITHOUT_GDOCS), {ignoreFileSize: true,
+          ignoreLastModifiedTime: true}).then(this.next);
     },
     function(inAppId) {
       remoteCall.waitForElement(appId, '#gear-button').then(this.next);
@@ -269,7 +271,8 @@ testcase.hideGoogleDocs = function() {
     },
     function(result) {
       remoteCall.waitForFiles(appId, TestEntryInfo.getExpectedRows(
-          BASIC_DRIVE_ENTRY_SET)).then(this.next);
+          BASIC_DRIVE_ENTRY_SET), {ignoreFileSize: true,
+          ignoreLastModifiedTime: true}).then(this.next);
     },
     function() {
       checkIfNoErrorsOccured(this.next);
