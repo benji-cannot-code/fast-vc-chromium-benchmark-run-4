13a59085647bd72b5d2bc66a72a3cfad3668272e
nednguyen <nednguyen@google.com>
2015-10-22 20:34:42 -0700

    [GPU_TESTS] Enable browser logging for trace_test
    Example log: https://console.developers.google.com/m/cloudstorage/b/chrome-telemetry-output/o/log_c9c3cb95-359c-4cfa-9b1c-022a4da00136
    
    BUG=546519
    
    Review URL: https://codereview.chromium.org/1419443005
    
    Cr-Commit-Position: refs/heads/master@{#355724}

--- tools/telemetry/telemetry/internal/backends/browser_backend.py
+++ tools/telemetry/telemetry/internal/backends/browser_backend.py
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import uuid
 import sys
 
 from telemetry.core import platform
@@ -49,9 +50,11 @@ class BrowserBackend(app_backend.AppBackend):
     Check supports_uploading_logs before calling this method.
     """
     assert self.supports_uploading_logs
+    remote_path = (self.browser_options.logs_cloud_remote_path or
+                   'log_%s' % uuid.uuid4())
     cloud_url = cloud_storage.Insert(
         bucket=self.browser_options.logs_cloud_bucket,
-        remote_path=self.browser_options.logs_cloud_remote_path,
+        remote_path=remote_path,
         local_path=self.log_file_path)
     sys.stderr.write('Uploading browser log to %s\n' % cloud_url)
 
