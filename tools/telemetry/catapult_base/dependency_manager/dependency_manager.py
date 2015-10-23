c17a15c03a1cdf51cdf3bc194262ada42a52aaf8
aiolos <aiolos@chromium.org>
2015-10-23 13:34:21 -0700

    Add Updating of cloud storage information in BaseConfigs.
    This is the first step in having the reference build use the BinaryManager.
    
    BUG=544258
    
    Review URL: https://codereview.chromium.org/1414453004
    
    Cr-Commit-Position: refs/heads/master@{#355872}

--- tools/telemetry/catapult_base/dependency_manager/dependency_manager.py
+++ tools/telemetry/catapult_base/dependency_manager/dependency_manager.py
@@ -90,8 +90,6 @@ class DependencyManager(object):
     """
     dependency_info = self._GetDependencyInfo(dependency, platform)
     if not dependency_info:
-      logging.error(
-          'The dependency_manager was not initialized with the dependency.')
       if not try_support_binaries:
         raise exceptions.NoPathFoundError(dependency, platform)
       # TODO(aiolos): Remove the support_binaries call and always raise
@@ -104,8 +102,8 @@ class DependencyManager(object):
       assert len(platform_parts) == 2
       platform_os, platform_arch = platform_parts
       logging.info('Calling into support_binaries with dependency %s, platform '
-                   '%s and arch %s.' % (dependency, platform_os,
-                                        platform_arch))
+                   '%s and arch %s. support_binaries is deprecated.'
+                   % (dependency, platform_os, platform_arch))
       return support_binaries.FindPath(dependency, platform_arch,
                                        platform_os)
     path = self._LocalPath(dependency_info)
@@ -142,8 +140,6 @@ class DependencyManager(object):
     # system.
     dependency_info = self._GetDependencyInfo(dependency, platform)
     if not dependency_info:
-      logging.error(
-          'The dependency_manager was not initialized with the dependency.')
       if not try_support_binaries:
         raise exceptions.NoPathFoundError(dependency, platform)
       return support_binaries.FindLocallyBuiltPath(dependency)
