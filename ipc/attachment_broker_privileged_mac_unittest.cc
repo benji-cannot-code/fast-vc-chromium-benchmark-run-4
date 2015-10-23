0ba1fc73081b05def1625a94bb63a74ac99e1905
erikchen <erikchen@chromium.org>
2015-10-23 15:34:01 -0700

    Revert of mac: Add auto-close and share-read-only functionality to SharedMemory. (patchset #5 id:80001 of https://codereview.chromium.org/1418113003/ )
    Reason for revert:
    New unit-tests failing on 10.8
    
    http://build.chromium.org/p/chromium.mac/builders/Mac10.8%20Tests/builds/10139/steps/base_unittests/logs/SharedMemoryMacMultiProcessTest.MachShareToProcessReadonly
    
    Original issue's description:
    > mac: Add auto-close and share-read-only functionality to Mach based SharedMemory.
    >
    > base::FileDescriptor has a property |auto_close| that is used to indicate that
    > when the object is passed to an IPC message, the message takes ownership of the
    > underlying OS handle. Since SharedMemoryHandle needs to be interchangeable with
    > base::FileDescriptor, I added a property with similar functionality named
    > |ownership_passes_to_ipc_|.
    >
    > The method ShareToProcess() is used to lower the current and maximum protection
    > of the underlying OS handle before it is transferred to a different process. I
    > implemented this functionality for Mach memory objects.
    >
    > BUG=535711
    >
    > Committed: https://crrev.com/033bbbcb63cab781552dfb435c035131c423de30
    > Cr-Commit-Position: refs/heads/master@{#355880}
    
    TBR=mark@chromium.org,tsepez@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=535711
    
    Review URL: https://codereview.chromium.org/1421933002
    
    Cr-Commit-Position: refs/heads/master@{#355908}

--- ipc/attachment_broker_privileged_mac_unittest.cc
+++ ipc/attachment_broker_privileged_mac_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "base/command_line.h"
-#include "base/mac/mac_util.h"
 #include "base/mac/mach_logging.h"
 #include "base/mac/scoped_mach_port.h"
 #include "base/memory/shared_memory.h"
@@ -167,10 +166,6 @@ class AttachmentBrokerPrivilegedMacMultiProcessTest
 // The attachment broker inserts a right for a memory object into the
 // destination task.
 TEST_F(AttachmentBrokerPrivilegedMacMultiProcessTest, InsertRight) {
-  // Mach-based SharedMemory isn't support on OSX 10.6.
-  if (base::mac::IsOSSnowLeopard())
-    return;
-
   SetUpChild("InsertRightClient");
   mach_msg_type_number_t original_name_count = GetActiveNameCount();
   IPC::AttachmentBrokerPrivilegedMac broker(nullptr);
@@ -234,10 +229,6 @@ MULTIPROCESS_TEST_MAIN(InsertRightClient) {
 // The attachment broker inserts the right for a memory object into the
 // destination task twice.
 TEST_F(AttachmentBrokerPrivilegedMacMultiProcessTest, InsertSameRightTwice) {
-  // Mach-based SharedMemory isn't support on OSX 10.6.
-  if (base::mac::IsOSSnowLeopard())
-    return;
-
   SetUpChild("InsertSameRightTwiceClient");
   mach_msg_type_number_t original_name_count = GetActiveNameCount();
   IPC::AttachmentBrokerPrivilegedMac broker(nullptr);
@@ -330,10 +321,6 @@ MULTIPROCESS_TEST_MAIN(InsertSameRightTwiceClient) {
 // The attachment broker inserts the rights for two memory objects into the
 // destination task.
 TEST_F(AttachmentBrokerPrivilegedMacMultiProcessTest, InsertTwoRights) {
-  // Mach-based SharedMemory isn't support on OSX 10.6.
-  if (base::mac::IsOSSnowLeopard())
-    return;
-
   SetUpChild("InsertTwoRightsClient");
   mach_msg_type_number_t original_name_count = GetActiveNameCount();
   IPC::AttachmentBrokerPrivilegedMac broker(nullptr);
