5ddfaf2c287dde3bd0d4422f1771a9160835a62b
krasin <krasin@google.com>
2015-10-23 15:15:56 -0700

    Another batch of fuzzers.
    BUG=539572
    
    Review URL: https://codereview.chromium.org/1417033005
    
    Cr-Commit-Position: refs/heads/master@{#355903}

---
+++ testing/libfuzzer/unescape_url_component_fuzzer.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include "net/base/escape.h"
+
+
+static const int kMaxUnescapeRule = 31;
+
+
+// Entry point for LibFuzzer.
+extern "C" int LLVMFuzzerTestOneInput(const unsigned char* data,
+                                      unsigned long size) {
+  std::string path(reinterpret_cast<const char*>(data), size);
+  for (int i = 0; i <= kMaxUnescapeRule; i++) {
+    (void)net::UnescapeURLComponent(path,
+                                    static_cast<net::UnescapeRule::Type>(i));
+  }
+  return 0;
+}
