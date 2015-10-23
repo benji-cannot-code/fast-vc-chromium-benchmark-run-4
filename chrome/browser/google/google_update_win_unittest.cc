39c6f138f8633e29f001104484b1f84085ea362d
jbroman <jbroman@chromium.org>
2015-10-23 11:07:56 -0700

    Include base/bind.h in google_update_win_unittest.cc
    BUG=546805
    TBR=pkasting
    
    Review URL: https://codereview.chromium.org/1416073004
    
    Cr-Commit-Position: refs/heads/master@{#355831}

--- chrome/browser/google/google_update_win_unittest.cc
+++ chrome/browser/google/google_update_win_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <queue>
 
 #include "base/base_paths.h"
+#include "base/bind.h"
 #include "base/memory/ref_counted.h"
 #include "base/path_service.h"
 #include "base/strings/stringprintf.h"
