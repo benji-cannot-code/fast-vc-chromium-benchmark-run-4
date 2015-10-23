f5310f6472f0c51bf8b63c9189d1f1d6f9015a2c
vabr <vabr@chromium.org>
2015-10-23 00:06:17 -0700

    Fix linter issues in random passwords-related files
    I was told by git cl lint to fix some errors which were not related to my original CL, but still seemed like a good idea to fix.
    
    R=engedy@chromium.org
    BUG=541519
    
    Review URL: https://codereview.chromium.org/1418983004
    
    Cr-Commit-Position: refs/heads/master@{#355745}

--- chrome/browser/ui/passwords/manage_passwords_ui_controller.h
+++ chrome/browser/ui/passwords/manage_passwords_ui_controller.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_PASSWORDS_MANAGE_PASSWORDS_UI_CONTROLLER_H_
 #define CHROME_BROWSER_UI_PASSWORDS_MANAGE_PASSWORDS_UI_CONTROLLER_H_
 
+#include <vector>
+
 #include "base/memory/scoped_vector.h"
 #include "base/timer/elapsed_timer.h"
 #include "chrome/browser/ui/passwords/manage_passwords_state.h"
