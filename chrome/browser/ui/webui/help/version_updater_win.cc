e769397e0ee71525e37a6d226f1330fe2a4566aa
grt <grt@chromium.org>
2015-10-23 08:45:58 -0700

    Use up-to-date icon for on-demand updates blocked by policy.
    BUG=545105
    
    Review URL: https://codereview.chromium.org/1416103002
    
    Cr-Commit-Position: refs/heads/master@{#355791}

--- chrome/browser/ui/webui/help/version_updater_win.cc
+++ chrome/browser/ui/webui/help/version_updater_win.cc
@@ -138,13 +138,18 @@ void VersionUpdaterWin::OnError(GoogleUpdateErrorCode error_code,
                                 const base::string16& new_version) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   base::string16 message;
+  Status status = FAILED;
 
   switch (error_code) {
     case GOOGLE_UPDATE_DISABLED_BY_POLICY:
+      // Display this as if Chrome is up to date, but with a custom message.
+      status = UPDATED;
       message =
           l10n_util::GetStringUTF16(IDS_UPGRADE_DISABLED_BY_POLICY);
       break;
     case GOOGLE_UPDATE_DISABLED_BY_POLICY_AUTO_ONLY:
+      // Display this as if Chrome is up to date, but with a custom message.
+      status = UPDATED;
       message =
           l10n_util::GetStringUTF16(IDS_UPGRADE_DISABLED_BY_POLICY_MANUAL);
       break;
@@ -158,7 +163,7 @@ void VersionUpdaterWin::OnError(GoogleUpdateErrorCode error_code,
       }
       break;
   }
-  callback_.Run(FAILED, 0, message);
+  callback_.Run(status, 0, message);
 }
 
 void VersionUpdaterWin::BeginUpdateCheckOnFileThread(
