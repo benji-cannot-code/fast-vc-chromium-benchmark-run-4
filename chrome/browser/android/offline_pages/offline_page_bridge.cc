547b03cbd2b7a099e3b260ee044f0b7c4af14c22
fgorski <fgorski@chromium.org>
2015-10-23 12:09:29 -0700

    [Offline pages] Updating sort order on Saved offline filter
    The new sort order is going to be Most recently used page first.
    
    BUG=491352
    
    Review URL: https://codereview.chromium.org/1403373011
    
    Cr-Commit-Position: refs/heads/master@{#355852}

--- chrome/browser/android/offline_pages/offline_page_bridge.cc
+++ chrome/browser/android/offline_pages/offline_page_bridge.cc
@@ -61,7 +61,8 @@ void ToJavaOfflinePageList(JNIEnv* env,
         offline_page.bookmark_id,
         ConvertUTF8ToJavaString(env, offline_page.GetOfflineURL().spec()).obj(),
         offline_page.file_size,
-        offline_page.access_count);
+        offline_page.access_count,
+        offline_page.last_access_time.ToJavaTime());
   }
 }
 
@@ -151,7 +152,8 @@ ScopedJavaLocalRef<jobject> OfflinePageBridge::GetPageByBookmarkId(
       offline_page->bookmark_id,
       ConvertUTF8ToJavaString(env, offline_page->GetOfflineURL().spec()).obj(),
       offline_page->file_size,
-      offline_page->access_count);
+      offline_page->access_count,
+      offline_page->last_access_time.ToJavaTime());
 }
 
 void OfflinePageBridge::SavePage(JNIEnv* env,
