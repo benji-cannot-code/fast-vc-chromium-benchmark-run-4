1c19891f558ebebd8b5aa369f21777d67d4273ac
erikchen <erikchen@chromium.org>
2015-10-23 17:46:54 -0700

    Reland 1: "mac: Add auto-close and share-read-only functionality to SharedMemory."
    The original CL had a bug in the unit tests where an incorrect
    parameter was passed to mach_vm_region.
    
    > base::FileDescriptor has a property |auto_close| that is used to indicate that
    > when the object is passed to an IPC message, the message takes ownership of the
    > underlying OS handle. Since SharedMemoryHandle needs to be interchangeable with
    > base::FileDescriptor, I added a property with similar functionality named
    > |ownership_passes_to_ipc_|.
    >
    > The method ShareToProcess() is used to lower the current and maximum protection
    > of the underlying OS handle before it is transferred to a different process. I
    > implemented this functionality for Mach memory objects.
    >
    > BUG=535711
    > Committed: https://crrev.com/033bbbcb63cab781552dfb435c035131c423de30
    > Cr-Commit-Position: refs/heads/master@{#355880}
    
    BUG=535711
    TBR=tsepez@chromium.org, mark@chromium.org
    
    Review URL: https://codereview.chromium.org/1422873002
    
    Cr-Commit-Position: refs/heads/master@{#355941}

--- ipc/test_util_mac.cc
+++ ipc/test_util_mac.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ipc/test_util_mac.h"
 
+#include <mach/mach_vm.h>
 #include <servers/bootstrap.h>
 
 #include "base/mac/mach_logging.h"
@@ -113,6 +114,30 @@ mach_port_urefs_t GetMachRefCount(mach_port_name_t name,
 void IncrementMachRefCount(mach_port_name_t name, mach_port_right_t right) {
   kern_return_t kr = mach_port_mod_refs(mach_task_self(), name, right, 1);
   MACH_CHECK(kr == KERN_SUCCESS, kr) << "GetRefCount";
+}
+
+bool GetMachProtections(void* address, size_t size, int* current, int* max) {
+  vm_region_info_t region_info;
+  mach_vm_address_t mem_address = reinterpret_cast<mach_vm_address_t>(address);
+  mach_vm_size_t mem_size = size;
+  vm_region_basic_info_64 basic_info;
+
+  region_info = reinterpret_cast<vm_region_recurse_info_t>(&basic_info);
+  vm_region_flavor_t flavor = VM_REGION_BASIC_INFO_64;
+  memory_object_name_t memory_object;
+  mach_msg_type_number_t count = VM_REGION_BASIC_INFO_COUNT_64;
+
+  kern_return_t kr =
+      mach_vm_region(mach_task_self(), &mem_address, &mem_size, flavor,
+                     region_info, &count, &memory_object);
+  if (kr != KERN_SUCCESS) {
+    MACH_LOG(ERROR, kr) << "Failed to get region info.";
+    return false;
+  }
+
+  *current = basic_info.protection;
+  *max = basic_info.max_protection;
+  return true;
 }
 
 }  // namespace IPC
