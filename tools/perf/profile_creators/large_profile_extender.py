0404e5b14f0122a991cf9deb51c7cb3075804a85
dalecurtis <dalecurtis@chromium.org>
2015-10-23 17:33:37 -0700

    Remove unused history profile extender and tests.
    BUG=547170
    TEST=none
    
    Review URL: https://codereview.chromium.org/1417303004
    
    Cr-Commit-Position: refs/heads/master@{#355940}

--- tools/perf/profile_creators/large_profile_extender.py
+++ tools/perf/profile_creators/large_profile_extender.py
@@ -4,17 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from profile_creators import cookie_profile_extender
-from profile_creators import history_profile_extender
 from profile_creators import profile_extender
 
 class LargeProfileExtender(profile_extender.ProfileExtender):
   """This class creates a large profile by performing a large number of url
   navigations."""
   def Run(self):
-    extender = history_profile_extender.HistoryProfileExtender(
-        self.finder_options)
-    extender.Run()
-
     extender = cookie_profile_extender.CookieProfileExtender(
         self.finder_options)
     extender.Run()
