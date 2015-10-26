17c4facbae15c114c0756573a1879920ec36433b
mstensho <mstensho@opera.com>
2015-10-26 06:17:10 -0700

    Never paginate fixed-positioned objects.
    They are supposed to be repeated on every page anyway. Not that we currently do
    that, but there's still no need to paginate them. It can only do harm.
    
    Added a flexbox test, which is what the bug report was about. This regressed
    for flexbox (and other types of non-block-container blocks as well) with
    https://codereview.chromium.org/1360753002 , but this has actually been broken
    for ages for regular block containers, so I threw in a bonus test for that too.
    
    BUG=535163
    R=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/1426443003
    
    Cr-Commit-Position: refs/heads/master@{#356041}

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -4409,7 +4409,10 @@ bool LayoutBox::hasUnsplittableScrollingOverflow() const
 
 bool LayoutBox::isUnsplittableForPagination() const
 {
-    return isReplaced() || hasUnsplittableScrollingOverflow() || (parent() && isWritingModeRoot());
+    return isReplaced()
+        || hasUnsplittableScrollingOverflow()
+        || (parent() && isWritingModeRoot())
+        || (isOutOfFlowPositioned() && style()->position() == FixedPosition);
 }
 
 LayoutUnit LayoutBox::lineHeight(bool /*firstLine*/, LineDirectionMode direction, LinePositionMode /*linePositionMode*/) const
