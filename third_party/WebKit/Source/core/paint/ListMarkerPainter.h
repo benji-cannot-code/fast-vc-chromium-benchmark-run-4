c5f1e981c68a9e4f6587120bfc4260c9f37d8b33
pdr <pdr@chromium.org>
2015-10-26 15:25:59 -0700

    Remove ensureRootTransformLayer, getCanonicalPath, and getPath
    This patch removes some deadcode in the paint system. No change in
    behavior.
    
    Review URL: https://codereview.chromium.org/1407233012
    
    Cr-Commit-Position: refs/heads/master@{#356151}

--- third_party/WebKit/Source/core/paint/ListMarkerPainter.h
+++ third_party/WebKit/Source/core/paint/ListMarkerPainter.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 struct PaintInfo;
-class Path;
 class LayoutListMarker;
 class LayoutPoint;
 
@@ -23,9 +22,6 @@ public:
     void paint(const PaintInfo&, const LayoutPoint& paintOffset);
 
 private:
-    Path getCanonicalPath() const;
-    Path getPath(const LayoutPoint& origin) const;
-
     const LayoutListMarker& m_layoutListMarker;
 };
 
