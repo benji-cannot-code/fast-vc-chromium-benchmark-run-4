530596fc65136ebbc34688b08851900664fe4dc7
dgozman <dgozman@chromium.org>
2015-10-26 17:24:12 -0700

    [DevTools] Reduce relayouts in DataGrid and ViewportControl.
    DataGrid tried to restore scroll position for non-scrollable element.
    ViewportControl tried to restore collapsed selection, which does not help user.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1413103004
    
    Cr-Commit-Position: refs/heads/master@{#356199}

--- third_party/WebKit/Source/devtools/front_end/ui_lazy/DataGrid.js
+++ third_party/WebKit/Source/devtools/front_end/ui_lazy/DataGrid.js
@@ -87,6 +87,9 @@ WebInspector.DataGrid = function(columnsArray, editCallback, deleteCallback, ref
     this._bottomFillerRow = createElementWithClass("tr", "data-grid-filler-row revealed");
     this.setVerticalPadding(0, 0);
 
+    /** @type {boolean} */
+    this._inline = false;
+
     /** @type {!Array.<!WebInspector.DataGrid.ColumnDescriptor>} */
     this._columnsArray = columnsArray;
     /** @type {!Array.<!WebInspector.DataGrid.ColumnDescriptor>} */
@@ -315,6 +318,7 @@ WebInspector.DataGrid.prototype = {
     {
         this.element.classList.add("inline");
         this._cornerWidth = 0;
+        this._inline = true;
         this.updateWidths();
     },
 
@@ -693,6 +697,15 @@ WebInspector.DataGrid.prototype = {
     get scrollContainer()
     {
         return this._scrollContainer;
+    },
+
+    /**
+     * @override
+     * @return {!Array.<!Element>}
+     */
+    elementsToRestoreScrollPositionsFor: function()
+    {
+        return this._inline ? [] : [this._scrollContainer];
     },
 
     _positionResizers: function()
