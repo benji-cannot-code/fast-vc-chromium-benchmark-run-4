8c92fe39b79a4fd190909e04e21a0d14d068ac97
mcgrathr <mcgrathr@chromium.org>
2015-10-27 11:11:30 -0700

    GN: Strip NaCl IRT and save its .debug file
    With a simple tweak, the toolchain already knows how to strip the
    nexe and leave the unstripped nexe where it can be found.  After
    that, the unstripped nexe can just be copied as the .debug file and
    a single objcopy command suffices to annotate the stripped file with
    a pointer to the .debug file.
    
    Also factor out computations about toolchain directory names into
    //build/config/nacl/config.gni where they can be reused.
    
    BUG= 546352
    R=bbudge@chromium.org, dpranke@chromium.org
    
    Review URL: https://codereview.chromium.org/1415293005
    
    Cr-Commit-Position: refs/heads/master@{#356340}

---
+++ ppapi/native_client/irt_debuglink.py
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+# Copyright (c) 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Run objcopy --add-gnu-debuglink for the NaCl IRT.
+"""
+
+import subprocess
+import sys
+
+
+def Main(args):
+  objcopy, debug_file, stripped_file, output_file = args
+  return subprocess.call([
+      objcopy, '--add-gnu-debuglink', debug_file, stripped_file, output_file
+      ])
+
+
+if __name__ == '__main__':
+  sys.exit(Main(sys.argv[1:]))
