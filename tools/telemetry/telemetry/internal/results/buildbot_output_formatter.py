dcadbe236732a6c8fa3aef25e85739aa02c44db6
eakuefner <eakuefner@chromium.org>
2015-10-27 13:57:24 -0700

    [Telemetry] Deprecate BuildbotOutputFormatter
    There are a few consumers left using BuildbotOutputFormatter, but in general,
    it's on its way out.
    
    BUG=477056
    
    Review URL: https://codereview.chromium.org/1427523003
    
    Cr-Commit-Position: refs/heads/master@{#356392}

--- tools/telemetry/telemetry/internal/results/buildbot_output_formatter.py
+++ tools/telemetry/telemetry/internal/results/buildbot_output_formatter.py
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from telemetry import decorators
 from telemetry.internal.results import output_formatter
 from telemetry.util import perf_tests_helper
 from telemetry import value as value_module
@@ -10,6 +11,8 @@ from telemetry.value import summary as summary_module
 
 
 class BuildbotOutputFormatter(output_formatter.OutputFormatter):
+  @decorators.Deprecated(2016, 2, 29, 'Chart JSON is a supported alternative. '
+                         'See https://goo.gl/8daFav .')
   def __init__(self, output_stream, trace_tag=''):
     super(BuildbotOutputFormatter, self).__init__(output_stream)
     self._trace_tag = trace_tag
