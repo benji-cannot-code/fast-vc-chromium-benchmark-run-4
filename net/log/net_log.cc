f2add259a649012270ec5f577f361aa0b0bd8b19
tfarina <tfarina@chromium.org>
2015-10-27 06:11:28 -0700

    net: Rename NetLog's IntegerCallback method to IntCallback.
    This fixes mmenke's TODO for it.
    
    BUG=None
    R=mmenke@chromium.org,eroman@chromium.org
    
    Review URL: https://codereview.chromium.org/1424733004
    
    Cr-Commit-Position: refs/heads/master@{#356279}

--- net/log/net_log.cc
+++ net/log/net_log.cc
@@ -52,7 +52,7 @@ scoped_ptr<base::Value> NetLogBoolCallback(
   return event_params.Pass();
 }
 
-scoped_ptr<base::Value> NetLogIntegerCallback(
+scoped_ptr<base::Value> NetLogIntCallback(
     const char* name,
     int value,
     NetLogCaptureMode /* capture_mode */) {
@@ -348,9 +348,8 @@ NetLog::ParametersCallback NetLog::BoolCallback(const char* name, bool value) {
 }
 
 // static
-NetLog::ParametersCallback NetLog::IntegerCallback(const char* name,
-                                                   int value) {
-  return base::Bind(&NetLogIntegerCallback, name, value);
+NetLog::ParametersCallback NetLog::IntCallback(const char* name, int value) {
+  return base::Bind(&NetLogIntCallback, name, value);
 }
 
 // static
@@ -447,7 +446,7 @@ void BoundNetLog::AddEventWithNetErrorCode(NetLog::EventType event_type,
   if (net_error >= 0) {
     AddEvent(event_type);
   } else {
-    AddEvent(event_type, NetLog::IntegerCallback("net_error", net_error));
+    AddEvent(event_type, NetLog::IntCallback("net_error", net_error));
   }
 }
 
@@ -457,7 +456,7 @@ void BoundNetLog::EndEventWithNetErrorCode(NetLog::EventType event_type,
   if (net_error >= 0) {
     EndEvent(event_type);
   } else {
-    EndEvent(event_type, NetLog::IntegerCallback("net_error", net_error));
+    EndEvent(event_type, NetLog::IntCallback("net_error", net_error));
   }
 }
 
--- net/url_request/url_request.cc
+++ net/url_request/url_request.cc
@@ -1006,7 +1006,7 @@ void URLRequest::SetPriority(RequestPriority priority) {
   priority_ = priority;
   if (job_.get()) {
     net_log_.AddEvent(NetLog::TYPE_URL_REQUEST_SET_PRIORITY,
-                      NetLog::IntegerCallback("priority", priority_));
+                      NetLog::IntCallback("priority", priority_));
     job_->SetPriority(priority_);
   }
 }
