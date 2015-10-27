751ff21a41441b69bf596bcd6910a2645ed03261
skobes <skobes@chromium.org>
2015-10-27 08:05:50 -0700

    Make compositor input-driven smooth scrolling smoother and snappier.
    The animation time is now constant instead of proportional to the delta, which
    felt sluggish.
    
    The velocity matching in UpdateTarget now handles direction changes correctly,
    and scales the first control point on the y axis instead of rotating it, which
    gives a smoother velocity transition.
    
    BUG=575
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1420943004
    
    Cr-Commit-Position: refs/heads/master@{#356291}

--- cc/animation/scroll_offset_animation_curve.h
+++ cc/animation/scroll_offset_animation_curve.h
@@ -18,9 +18,11 @@ class TimingFunction;
 
 class CC_EXPORT ScrollOffsetAnimationCurve : public AnimationCurve {
  public:
+  enum class DurationBehavior { DELTA_BASED, CONSTANT };
   static scoped_ptr<ScrollOffsetAnimationCurve> Create(
       const gfx::ScrollOffset& target_value,
-      scoped_ptr<TimingFunction> timing_function);
+      scoped_ptr<TimingFunction> timing_function,
+      DurationBehavior = DurationBehavior::DELTA_BASED);
 
   ~ScrollOffsetAnimationCurve() override;
 
@@ -36,7 +38,8 @@ class CC_EXPORT ScrollOffsetAnimationCurve : public AnimationCurve {
 
  private:
   ScrollOffsetAnimationCurve(const gfx::ScrollOffset& target_value,
-                             scoped_ptr <TimingFunction> timing_function);
+                             scoped_ptr<TimingFunction> timing_function,
+                             DurationBehavior);
 
   gfx::ScrollOffset initial_value_;
   gfx::ScrollOffset target_value_;
@@ -46,6 +49,7 @@ class CC_EXPORT ScrollOffsetAnimationCurve : public AnimationCurve {
   base::TimeDelta last_retarget_;
 
   scoped_ptr<TimingFunction> timing_function_;
+  DurationBehavior duration_behavior_;
 
   DISALLOW_COPY_AND_ASSIGN(ScrollOffsetAnimationCurve);
 };
