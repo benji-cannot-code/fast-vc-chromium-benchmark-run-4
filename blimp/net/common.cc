f5b758e0778957f753b93cdbc87ae447b8f3bead
kmarshall <kmarshall@chromium.org>
2015-10-27 16:30:01 -0700

    Created BlimpMessageWriter, which frames and sends data over a Socket. The conversion from BlimpMessages to bytes will be handled in another class as part of a followup CL. Added unit tests for BlimpMessageWriter.
    R=wez@chromium.org
    CC=dtrainor@chromium.org,haibinlu@chromium.org,nyquist@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1392753002
    
    Cr-Commit-Position: refs/heads/master@{#356431}

--- blimp/net/common.cc
+++ blimp/net/common.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blimp {
 
-const size_t kMaxPacketPayloadSizeBytes = 1 << 16;  // 64kb
+const size_t kMaxPacketPayloadSizeBytes = 1 << 16;  // 64KB
 const size_t kPacketHeaderSizeBytes = 4;
 
 }  // namespace blimp
