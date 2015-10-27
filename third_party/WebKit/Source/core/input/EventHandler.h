7e82bf7351291803a346ff446e09090c7654e4b9
mustaq <mustaq@chromium.org>
2015-10-27 11:10:22 -0700

    Fire pointerup/down events for corresponding mouse events.
    Includes tests for pointerup/down event properties and for prevent
    default behavior.
    
    Also fixed a crack in eventSender that kept MouseEvent buttons unset.
    
    BUG=476571,541311
    
    Review URL: https://codereview.chromium.org/1391393002
    
    Cr-Commit-Position: refs/heads/master@{#356339}

--- third_party/WebKit/Source/core/input/EventHandler.h
+++ third_party/WebKit/Source/core/input/EventHandler.h
@@ -284,7 +284,7 @@ private:
     void updateMouseEventTargetNode(Node*, const PlatformMouseEvent&);
 
     // Returns true when the sent PE has defaultPrevented or defaultHandled set.
-    bool dispatchPointerEventForMouseEvent(Node* target, const AtomicString& eventType, const PlatformMouseEvent&, Node* relatedTarget);
+    bool dispatchPointerEventForMouseEvent(Node* target, const AtomicString& eventType, const PlatformMouseEvent&, Node* relatedTarget = nullptr);
 
     // Dispatches mouseover, mouseout, mouseenter and mouseleave events to appropriate nodes when the mouse pointer moves from one node to another.
     void sendMouseEventsForNodeTransition(Node*, Node*, const PlatformMouseEvent&);
@@ -332,6 +332,8 @@ private:
     AutoscrollController* autoscrollController() const;
     bool panScrollInProgress() const;
     void setLastKnownMousePosition(const PlatformMouseEvent&);
+
+    void conditionallyEnableMouseEventForPointerTypeMouse(const PlatformMouseEvent&);
 
     bool shouldTopControlsConsumeScroll(FloatSize) const;
 
@@ -414,6 +416,11 @@ private:
     bool m_touchPressed;
 
     PointerIdManager m_pointerIdManager;
+
+    // Prevents firing mousedown, mousemove & mouseup in-between a canceled pointerdown and next pointerup/pointercancel.
+    // See "PREVENT MOUSE EVENT flag" in the spec:
+    //   https://w3c.github.io/pointerevents/#compatibility-mapping-with-mouse-events
+    bool m_preventMouseEventForPointerTypeMouse;
 
     // This is set upon sending a pointercancel for touch, prevents PE dispatches for touches until
     // all touch-points become inactive.
