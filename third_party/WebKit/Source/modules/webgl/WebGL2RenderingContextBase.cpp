b51e793106f00fd8dac6f731d4f5ffbd776894ae
qiankun.miao <qiankun.miao@intel.com>
2015-10-27 02:25:21 -0700

    Add GPU fast path for texSubImage3D
    This is similar as that in texSubImage2D. This CL enhances
    texImageCanvasByGPU to support texSubImage3D.
    
    BUG=532708
    TEST=conformance2/textures/canvas
    
    Review URL: https://codereview.chromium.org/1406153006
    
    Cr-Commit-Position: refs/heads/master@{#356257}

--- third_party/WebKit/Source/modules/webgl/WebGL2RenderingContextBase.cpp
+++ third_party/WebKit/Source/modules/webgl/WebGL2RenderingContextBase.cpp
@@ -791,7 +791,11 @@ void WebGL2RenderingContextBase::texSubImage3D(GLenum target, GLint level, GLint
     if (isContextLost() || !validateHTMLCanvasElement("texSubImage3D", canvas, exceptionState))
         return;
 
-    if (!canvas->renderingContext() || !canvas->renderingContext()->isAccelerated()) {
+    WebGLTexture* texture = validateTextureBinding("texSubImage3D", target, false);
+    ASSERT(texture);
+    GLenum internalformat = texture->getInternalFormat(target, level);
+
+    if (!canvas->renderingContext() || !canvas->renderingContext()->isAccelerated() || !canUseTexImageCanvasByGPU(internalformat, type)) {
         ASSERT(!canvas->renderingContext() || canvas->renderingContext()->is2d());
         // 2D canvas has only FrontBuffer.
         texSubImage3DImpl(target, level, xoffset, yoffset, zoffset, format, type, canvas->copiedImage(FrontBuffer, PreferAcceleration).get(),
@@ -799,8 +803,7 @@ void WebGL2RenderingContextBase::texSubImage3D(GLenum target, GLint level, GLint
         return;
     }
 
-    // FIXME: Can we do an accelerated copy in this case?
-    notImplemented();
+    texImageCanvasByGPU(TexSubImage3DByGPU, texture, target, level, GL_RGBA, type, xoffset, yoffset, zoffset, canvas);
 }
 
 void WebGL2RenderingContextBase::texSubImage3D(GLenum target, GLint level, GLint xoffset, GLint yoffset, GLint zoffset, GLenum format, GLenum type, HTMLVideoElement* video, ExceptionState& exceptionState)
