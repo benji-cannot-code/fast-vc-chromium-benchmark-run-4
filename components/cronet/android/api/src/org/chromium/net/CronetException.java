4e09be650b38ccc68a3147eb3aba23dd6d547c68
mef <mef@chromium.org>
2015-10-27 11:49:48 -0700

    [Cronet] Define BidirectionalStream interface for advanced use of HTTP/2 and QUIC connections.
    BUG=516342
    
    Review URL: https://codereview.chromium.org/1377603004
    
    Cr-Commit-Position: refs/heads/master@{#356356}

---
+++ components/cronet/android/api/src/org/chromium/net/CronetException.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.net;
+
+/**
+ * Exception reported from UrlRequest or BidirectionalStream.
+ */
+// TODO(mef): Will replace UrlRequestException soon.
+public class CronetException extends UrlRequestException {
+    CronetException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    CronetException(String message, int netError) {
+        super(message, null);
+    }
+}
