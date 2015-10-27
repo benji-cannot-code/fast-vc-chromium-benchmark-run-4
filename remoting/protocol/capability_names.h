c0aeb8bb2fd2ebbb0c1e82f62fb840815d58f02d
joedow <joedow@chromium.org>
2015-10-27 12:22:45 -0700

    Fixing typo in header macro guard.
    BUG=
    
    Review URL: https://codereview.chromium.org/1420703005
    
    Cr-Commit-Position: refs/heads/master@{#356367}

--- remoting/protocol/capability_names.h
+++ remoting/protocol/capability_names.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef REMOTING_PROTOCOL_CAPABILITY_NAMeS_H_
-#define REMOTING_PROTOCOL_CAPABILITY_NAMeS_H_
+#ifndef REMOTING_PROTOCOL_CAPABILITY_NAMES_H_
+#define REMOTING_PROTOCOL_CAPABILITY_NAMES_H_
 
 namespace remoting {
 namespace protocol {
@@ -18,4 +18,4 @@ const char kRateLimitResizeRequests[] = "rateLimitResizeRequests";
 }  // namespace protocol
 }  // namespace remoting
 
-#endif  // REMOTING_PROTOCOL_CAPABILITY_NAMeS_H_
+#endif  // REMOTING_PROTOCOL_CAPABILITY_NAMES_H_
