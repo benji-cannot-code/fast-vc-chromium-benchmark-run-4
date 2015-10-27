f0cdba11602fb1e53f0730cba7143b41511d208a
aurimas <aurimas@chromium.org>
2015-10-26 17:19:07 -0700

    Revert "Remove "Search by Image" feature on Chrome for Android."
    This reverts https://chromiumcodereview.appspot.com/1394343002
    
    Turns out we want to keep this feature after all.
    
    BUG=483685
    
    Review URL: https://codereview.chromium.org/1425683002
    
    Cr-Commit-Position: refs/heads/master@{#356195}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabContextMenuItemDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabContextMenuItemDelegate.java
@@ -99,6 +99,11 @@ public class TabContextMenuItemDelegate implements ContextMenuItemDelegate {
         mTab.loadUrl(loadUrlParams);
     }
 
+    @Override
+    public void onSearchByImageInNewTab() {
+        mTab.triggerSearchByImage();
+    }
+
     /**
      * Checks if spdy proxy is enabled for input url.
      * @param url Input url to check for spdy setting.
