61f6d0a03a32b72099f8cba51226e9388730eec9
tfarina <tfarina@chromium.org>
2015-10-27 14:37:34 -0700

    Output webkit_version.h into the target_gen_dir.
    And update callers to include it like build/util/webkit_version.h.
    
    BUG=None
    R=brettw@chromium.org
    
    Review URL: https://codereview.chromium.org/1418623009
    
    Cr-Commit-Position: refs/heads/master@{#356401}

--- content/common/user_agent.cc
+++ content/common/user_agent.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/sys_info.h"
 #include "build/build_config.h"
+#include "build/util/webkit_version.h"
 
 #if defined(OS_POSIX) && !defined(OS_MACOSX)
 #include <sys/utsname.h>
@@ -18,9 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include "base/win/windows_version.h"
 #endif
-
-// Generated
-#include "webkit_version.h"  // NOLINT
 
 namespace content {
 
