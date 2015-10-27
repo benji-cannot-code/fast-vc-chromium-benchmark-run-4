f5805a0a77296643a1c32f853e6fe52fe00204a6
vabr <vabr@chromium.org>
2015-10-27 00:49:48 -0700

    Introduce PasswordStoreFactory for iOS
    Also, add vabr to ios/chrome/browser/passwords/OWNERS.
    
    (This is the upstream part of CL 273887013.)
    
    BUG=514183
    
    Review URL: https://codereview.chromium.org/1401343002
    
    Cr-Commit-Position: refs/heads/master@{#356251}

--- ios/chrome/browser/browser_state/browser_state_keyed_service_factories.mm
+++ ios/chrome/browser/browser_state/browser_state_keyed_service_factories.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/history/history_service_factory.h"
 #include "ios/chrome/browser/history/top_sites_factory.h"
 #include "ios/chrome/browser/history/web_history_service_factory.h"
+#include "ios/chrome/browser/passwords/ios_chrome_password_store_factory.h"
 #include "ios/chrome/browser/search_engines/template_url_service_factory.h"
 #include "ios/chrome/browser/signin/about_signin_internals_factory.h"
 #include "ios/chrome/browser/signin/account_consistency_service_factory.h"
@@ -75,6 +76,7 @@ void EnsureBrowserStateKeyedServiceFactoriesBuilt() {
   ios::TopSitesFactory::GetInstance();
   ios::WebDataServiceFactory::GetInstance();
   ios::WebHistoryServiceFactory::GetInstance();
+  IOSChromePasswordStoreFactory::GetInstance();
   OAuth2TokenServiceFactory::GetInstance();
   PersonalDataManagerFactory::GetInstance();
   SigninClientFactory::GetInstance();
--- ios/chrome/ios_chrome.gyp
+++ ios/chrome/ios_chrome.gyp
@@ -323,6 +323,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/net/retryable_url_fetcher.mm',
         'browser/open_from_clipboard/create_clipboard_recent_content.h',
         'browser/open_from_clipboard/create_clipboard_recent_content.mm',
+        'browser/passwords/ios_chrome_password_store_factory.cc',
+        'browser/passwords/ios_chrome_password_store_factory.h',
         'browser/passwords/password_generation_utils.h',
         'browser/passwords/password_generation_utils.mm',
         'browser/pref_names.cc',
