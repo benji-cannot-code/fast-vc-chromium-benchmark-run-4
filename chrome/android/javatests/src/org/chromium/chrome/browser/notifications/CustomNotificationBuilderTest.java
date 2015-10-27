707de8ad2fcb225d1d5385def354a69072b65016
mvanouwerkerk <mvanouwerkerk@chromium.org>
2015-10-27 04:02:10 -0700

    Suppress deprecation warnings for icon and largeIcon.
    BUG=546503
    
    Review URL: https://codereview.chromium.org/1419993002
    
    Cr-Commit-Position: refs/heads/master@{#356265}

--- chrome/android/javatests/src/org/chromium/chrome/browser/notifications/CustomNotificationBuilderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/notifications/CustomNotificationBuilderTest.java
@@ -25,6 +25,7 @@ import org.chromium.chrome.R;
 /**
  * Instrumentation unit tests for CustomNotificationBuilder.
  */
+@SuppressWarnings("deprecation") // For the |icon| and |largeIcon| properties of Notification.
 public class CustomNotificationBuilderTest extends InstrumentationTestCase {
     @SmallTest
     @Feature({"Browser", "Notifications"})
