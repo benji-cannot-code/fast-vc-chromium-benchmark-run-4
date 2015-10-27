8f468f4dbbe00262be1ca23f01e1f5240a2e0de3
lpy <lpy@google.com>
2015-10-27 16:55:22 -0700

    Add Tracing around frame creation
    Frame creation can be costly. this puts a trace event call around one of those
    costs so it is clearer in traces what is going on.
    
    BUG=544277
    
    Review URL: https://codereview.chromium.org/1406383002
    
    Cr-Commit-Position: refs/heads/master@{#356441}

--- third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
@@ -1748,6 +1748,7 @@ PassRefPtrWillBeRawPtr<LocalFrame> WebLocalFrameImpl::createChildFrame(const Fra
     const AtomicString& name, HTMLFrameOwnerElement* ownerElement)
 {
     ASSERT(m_client);
+    TRACE_EVENT0("blink", "WebLocalFrameImpl::createChildframe");
     WebTreeScopeType scope = frame()->document() == ownerElement->treeScope()
         ? WebTreeScopeType::Document
         : WebTreeScopeType::Shadow;
