e5570925aab99cff9caf559c28c772d9d71f4ea9
blundell <blundell@chromium.org>
2015-10-28 08:47:20 -0700

    [sync] Eliminate SYNC_CONFIGURE_DONE notification
    This CL ports the listeners of the SYNC_CONFIGURE_DONE notification to instead
    observe the equivalent SyncServiceObserver callback and eliminates that
    notification from the codebase.
    
    The concrete motivation is to remove //content dependencies from
    ProfileSyncService in order to enable clean integration on iOS.
    
    BUG=518825
    TBR=jochen
    
    Review URL: https://codereview.chromium.org/1422813002
    
    Cr-Commit-Position: refs/heads/master@{#356560}

--- chrome/browser/android/foreign_session_helper.h
+++ chrome/browser/android/foreign_session_helper.h
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <jni.h>
 
 #include "base/android/scoped_java_ref.h"
+#include "base/scoped_observer.h"
 #include "chrome/browser/profiles/profile.h"
+#include "components/sync_driver/sync_service_observer.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 
@@ -21,7 +23,12 @@ namespace browser_sync {
 struct SyncedSession;
 }  // namespace browser_sync
 
-class ForeignSessionHelper : public content::NotificationObserver {
+namespace sync_driver {
+class SyncService;
+}  // namespace sync_driver
+
+class ForeignSessionHelper : public content::NotificationObserver,
+                             public sync_driver::SyncServiceObserver {
  public:
   explicit ForeignSessionHelper(Profile* profile);
   void Destroy(JNIEnv* env, jobject obj);
@@ -42,14 +49,23 @@ class ForeignSessionHelper : public content::NotificationObserver {
                const content::NotificationSource& source,
                const content::NotificationDetails& details) override;
 
+  // sync_driver::SyncServiceObserver implementation
+  void OnStateChanged() override {}
+  void OnSyncConfigurationCompleted() override;
+
   static bool RegisterForeignSessionHelper(JNIEnv* env);
 
  private:
   ~ForeignSessionHelper() override;
 
+  // Fires |callback_| if it is not null.
+  void FireForeignSessionCallback();
+
   Profile* profile_;  // weak
   base::android::ScopedJavaGlobalRef<jobject> callback_;
   content::NotificationRegistrar registrar_;
+  ScopedObserver<sync_driver::SyncService, sync_driver::SyncServiceObserver>
+      scoped_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(ForeignSessionHelper);
 };
