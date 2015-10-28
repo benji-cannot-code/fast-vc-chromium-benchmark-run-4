7e7483f96f5fe13c0a0dad8e3f20dca63d7aefa3
brucedawson <brucedawson@chromium.org>
2015-10-27 18:56:26 -0700

    Add missing include of <iterator>
    Some VS 2015 builds fail if you use back_inserter without explicitly
    including <iterator>. This has broken the VS 2015 build machines for a
    while (unclear exact time due to a separate build break).
    
    I cannot reproduce the compile failure locally but I believe that this
    fix is appropriate and will probably fix the build machines.
    
    See http://en.cppreference.com/w/cpp/iterator/back_inserter.
    
    BUG=440500
    
    Review URL: https://codereview.chromium.org/1424473005
    
    Cr-Commit-Position: refs/heads/master@{#356486}

--- base/trace_event/memory_profiler_heap_dump_writer.cc
+++ base/trace_event/memory_profiler_heap_dump_writer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/trace_event/memory_profiler_heap_dump_writer.h"
 
+#include <iterator>
 #include <numeric>
 
 #include "base/format_macros.h"
