90ee1eae9c0bb9b4f4b0e7e2649f3be66eb06a08
nednguyen <nednguyen@google.com>
2015-10-28 11:19:41 -0700

    [Telemetry/Perf] Switch os.pardir to '..' everywhere
    Rationale: the two are essentially the same, and '..' make the code more
    readable.
    
    Review URL: https://codereview.chromium.org/1414673003
    
    Cr-Commit-Position: refs/heads/master@{#356582}

--- tools/perf/fetch_benchmark_deps.py
+++ tools/perf/fetch_benchmark_deps.py
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, 'telemetry'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'telemetry'))
 
 from catapult_base import cloud_storage
 from telemetry import benchmark_runner
