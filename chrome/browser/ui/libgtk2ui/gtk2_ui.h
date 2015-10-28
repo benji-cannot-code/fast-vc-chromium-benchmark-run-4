04460fb2d06b4586a84cf08b3b89bf2ef85ff3c1
estade <estade@chromium.org>
2015-10-28 15:00:32 -0700

    Fix colors of toolbar buttons on GTK themes in MD mode.
    It would be intuitive to just pipe GetTint from ThemeSupplier to LinuxUi
    and return the tint values Gtk2UI has already calculated. This doesn't
    create a satisfying result because we end up with a somewhat dimmed
    version of text/button highlight. For example, dim orange. Even undimmed
    the orange would be too strong for buttons that are always visible, so
    it's better to stick with a color derived from the font.
    
    This CL also cleans up a lot of places where we calculate a tint but never
    read it.
    
    BUG=547143
    
    Review URL: https://codereview.chromium.org/1408923006
    
    Cr-Commit-Position: refs/heads/master@{#356661}

--- chrome/browser/ui/libgtk2ui/gtk2_ui.h
+++ chrome/browser/ui/libgtk2ui/gtk2_ui.h
@@ -134,8 +134,8 @@ class Gtk2UI : public views::LinuxUI {
   // or generates them per our fallback algorithm.
   SkColor BuildFrameColors();
 
-  // Sets the underlying theme tints.
-  void SetThemeTint(int id, SkColor color);
+  // Gets a tint which depends on the default for |id| as well as |color|.
+  color_utils::HSL ColorToTint(int id, SkColor color);
 
   // Lazily generates each image used in the gtk theme.
   gfx::Image GenerateGtkThemeImage(int id) const;
@@ -175,10 +175,9 @@ class Gtk2UI : public views::LinuxUI {
   // Updates |default_font_*|.
   void UpdateDefaultFont();
 
-  // Tints and colors calculated by LoadGtkValues() that are given to the
+  // Colors calculated by LoadGtkValues() that are given to the
   // caller while |use_gtk_| is true.
   ColorMap colors_;
-  TintMap tints_;
 
   // Colors used to tint certain icons.
   color_utils::HSL button_tint_;
