b13f83502f4fe10dcfa2d2e00a00649849f359bd
peria <peria@chromium.org>
2015-10-27 22:28:09 -0700

    Fix wrong usage of Vector<MockScriptLoader*>
    BUG=515524
    
    Review URL: https://codereview.chromium.org/1427723002
    
    Cr-Commit-Position: refs/heads/master@{#356515}

--- third_party/WebKit/Source/core/dom/ScriptRunnerTest.cpp
+++ third_party/WebKit/Source/core/dom/ScriptRunnerTest.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Document.h"
 #include "core/dom/Element.h"
 #include "core/dom/ScriptLoader.h"
+#include "platform/heap/Handle.h"
 #include "platform/scheduler/CancellableTaskFactory.h"
 #include "public/platform/Platform.h"
 #include <gmock/gmock.h>
@@ -197,7 +198,7 @@ TEST_F(ScriptRunnerTest, QueueMultipleScripts_InOrder)
     OwnPtrWillBeRawPtr<MockScriptLoader> scriptLoader2 = MockScriptLoader::create(m_element.get());
     OwnPtrWillBeRawPtr<MockScriptLoader> scriptLoader3 = MockScriptLoader::create(m_element.get());
 
-    WTF::Vector<MockScriptLoader*> scriptLoaders;
+    WillBeHeapVector<RawPtrWillBeMember<MockScriptLoader>> scriptLoaders;
     scriptLoaders.append(scriptLoader1.get());
     scriptLoaders.append(scriptLoader2.get());
     scriptLoaders.append(scriptLoader3.get());
