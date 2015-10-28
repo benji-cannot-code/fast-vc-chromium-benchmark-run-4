4e263f04d739a9662e69ba38a8d3b31d3e3e5aa1
dbeam <dbeam@chromium.org>
2015-10-27 20:26:58 -0700

    Add DownloadQuery::Skip()
    This is part of implementing infinite scroll on chrome://downloads
    
    R=asanka@chromium.org
    BUG=526577
    
    Review URL: https://codereview.chromium.org/1423123004
    
    Cr-Commit-Position: refs/heads/master@{#356506}

--- chrome/browser/download/download_query.cc
+++ chrome/browser/download/download_query.cc
@@ -238,12 +238,8 @@ static ComparisonType Compare(
 
 }  // anonymous namespace
 
-DownloadQuery::DownloadQuery()
-  : limit_(kuint32max) {
-}
-
-DownloadQuery::~DownloadQuery() {
-}
+DownloadQuery::DownloadQuery() : limit_(kuint32max), skip_(0U) {}
+DownloadQuery::~DownloadQuery() {}
 
 // AddFilter() pushes a new FilterCallback to filters_. Most FilterCallbacks are
 // Callbacks to FieldMatches<>(). Search() iterates over given DownloadItems,
@@ -433,11 +429,21 @@ void DownloadQuery::AddSorter(DownloadQuery::SortType type,
 }
 
 void DownloadQuery::FinishSearch(DownloadQuery::DownloadVector* results) const {
-  if (!sorters_.empty())
-    std::partial_sort(results->begin(),
-                      results->begin() + std::min(limit_, results->size()),
-                      results->end(),
-                      DownloadComparator(sorters_));
+  if (skip_ >= results->size()) {
+    results->clear();
+    return;
+  }
+
+  if (!sorters_.empty()) {
+    std::partial_sort(
+        results->begin(),
+        results->begin() + std::min(limit_ + skip_, results->size()),
+        results->end(),
+        DownloadComparator(sorters_));
+  }
+
+  results->erase(results->begin(), results->begin() + skip_);
+
   if (results->size() > limit_)
     results->resize(limit_);
 }
