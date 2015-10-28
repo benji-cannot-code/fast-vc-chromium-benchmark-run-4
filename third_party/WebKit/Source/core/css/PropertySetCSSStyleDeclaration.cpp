7166a61287e3b059fe7b28352756371ef9646791
nainar <nainar@chromium.org>
2015-10-27 17:18:11 -0700

    Ensure invalidation set scheduling for CSSStyleDeclaration changes
    Currently Chrome is missing invalidation set scheduling for changes made
    to CSSStyleDeclaration. This patch fixes that by ensuring that we
    schedule invalidation sets when setting a property's value in
    AbstractPropertySetCSSStyleDeclaration::setPropertyInternal.
    
    This patch fixes the bug whereby setting an attribute selector wasn't
    affecting the relevant sibling selectors.
    
    BUG=156666
    
    Review URL: https://codereview.chromium.org/1413993004
    
    Cr-Commit-Position: refs/heads/master@{#356448}

--- third_party/WebKit/Source/core/css/PropertySetCSSStyleDeclaration.cpp
+++ third_party/WebKit/Source/core/css/PropertySetCSSStyleDeclaration.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Element.h"
 #include "core/dom/MutationObserverInterestGroup.h"
 #include "core/dom/MutationRecord.h"
+#include "core/dom/StyleEngine.h"
 #include "core/inspector/InspectorInstrumentation.h"
 #include "platform/RuntimeEnabledFeatures.h"
 
@@ -256,8 +257,13 @@ void AbstractPropertySetCSSStyleDeclaration::setPropertyInternal(CSSPropertyID u
 
     didMutate(changed ? PropertyChanged : NoChanges);
 
-    if (changed)
-        mutationScope.enqueueMutationRecord();
+    if (!changed)
+        return;
+
+    Element* parent = parentElement();
+    if (parent && parent->inActiveDocument() && parent->document().styleResolver())
+        parent->document().styleEngine().attributeChangedForElement(HTMLNames::styleAttr, *parent);
+    mutationScope.enqueueMutationRecord();
 }
 
 StyleSheetContents* AbstractPropertySetCSSStyleDeclaration::contextStyleSheet() const
