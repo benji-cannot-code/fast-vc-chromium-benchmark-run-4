f484ea0ca13bd0d3ca07d8147bf04a250572f908
ben <ben@chromium.org>
2015-10-28 14:49:51 -0700

    Remove network service from ws
    R=sky@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1430623003
    
    Cr-Commit-Position: refs/heads/master@{#356657}

--- components/mus/ws/window_tree_impl.cc
+++ components/mus/ws/window_tree_impl.cc
@@ -23,8 +23,6 @@ using mojo::Array;
 using mojo::Callback;
 using mojo::InterfaceRequest;
 using mojo::Rect;
-using mojo::ServiceProvider;
-using mojo::ServiceProviderPtr;
 using mojo::String;
 
 namespace mus {
@@ -172,15 +170,6 @@ bool WindowTreeImpl::Embed(const WindowId& window_id,
   if (is_embed_root_)
     *connection_id = new_connection->id();
   return true;
-}
-
-void WindowTreeImpl::Embed(const WindowId& window_id,
-                           mojo::URLRequestPtr request) {
-  if (!CanEmbed(window_id, WindowTree::ACCESS_POLICY_DEFAULT))
-    return;
-  PrepareForEmbed(window_id);
-  connection_manager_->EmbedAtWindow(
-      id_, window_id, mojom::WindowTree::ACCESS_POLICY_DEFAULT, request.Pass());
 }
 
 void WindowTreeImpl::ProcessWindowBoundsChanged(const ServerWindow* window,
