703ae873eab2ec24ace055d5b00561b0b8713782
oysteine <oysteine@chromium.org>
2015-10-28 16:34:17 -0700

    Tracing: Add support for PII whitelisting of individual trace event arguments
    R=dsinclair,shatch
    BUG=546093
    
    Review URL: https://codereview.chromium.org/1415013003
    
    Cr-Commit-Position: refs/heads/master@{#356690}

--- chrome/common/trace_event_args_whitelist.h
+++ chrome/common/trace_event_args_whitelist.h
@@ -6,9 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_COMMON_TRACE_EVENT_ARGS_WHITELIST_H_
 #define CHROME_COMMON_TRACE_EVENT_ARGS_WHITELIST_H_
 
+#include "base/trace_event/trace_event_impl.h"
+
 // Used to filter trace event arguments against a whitelist of events that
 // have been manually vetted to not include any PII.
-bool IsTraceEventArgsWhitelisted(const char* category_group_name,
-                                 const char* event_name);
+bool IsTraceEventArgsWhitelisted(
+    const char* category_group_name,
+    const char* event_name,
+    base::trace_event::ArgumentNameFilterPredicate* arg_name_filter);
 
 #endif  // CHROME_COMMON_TRACE_EVENT_ARGS_WHITELIST_H_
