abe4b1cca3f11de653b674ca408e4805146a2bc1
sclittle <sclittle@chromium.org>
2015-10-27 17:20:07 -0700

    Use the TimeTicks value of request start time for data use accounting.
    This change makes data use accounting pull the TimeTicks value of the
    request start time out of the LoadTimingInfo for the URLRequest, and use
    that to annotate the DataUse objects instead of the base::Time value
    that was being used previously. This will make it easier to combine
    DataUse timelines with timelines of events from other threads, such as
    UI events.
    
    BUG=548351
    
    Review URL: https://codereview.chromium.org/1416243006
    
    Cr-Commit-Position: refs/heads/master@{#356449}

--- components/data_usage/core/data_use.cc
+++ components/data_usage/core/data_use.cc
@@ -8,14 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace data_usage {
 
 DataUse::DataUse(const GURL& url,
-                 const base::Time& request_time,
+                 const base::TimeTicks& request_start,
                  const GURL& first_party_for_cookies,
                  int32_t tab_id,
                  net::NetworkChangeNotifier::ConnectionType connection_type,
                  int64_t tx_bytes,
                  int64_t rx_bytes)
     : url(url),
-      request_time(request_time),
+      request_start(request_start),
       first_party_for_cookies(first_party_for_cookies),
       tab_id(tab_id),
       connection_type(connection_type),
@@ -23,7 +23,7 @@ DataUse::DataUse(const GURL& url,
       rx_bytes(rx_bytes) {}
 
 bool DataUse::CanCombineWith(const DataUse& other) const {
-  return url == other.url && request_time == other.request_time &&
+  return url == other.url && request_start == other.request_start &&
          first_party_for_cookies == other.first_party_for_cookies &&
          tab_id == other.tab_id && connection_type == other.connection_type;
 }
