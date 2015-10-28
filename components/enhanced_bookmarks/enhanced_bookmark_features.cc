ed1ce3ab5e80e4afc8d1808c9d3c9421d7fa1d54
jianli <jianli@chromium.org>
2015-10-27 23:54:12 -0700

    Always enable enhanced bookmarks features for offline pages
    BUG=491352
    
    Review URL: https://codereview.chromium.org/1422183002
    
    Cr-Commit-Position: refs/heads/master@{#356519}

--- components/enhanced_bookmarks/enhanced_bookmark_features.cc
+++ components/enhanced_bookmarks/enhanced_bookmark_features.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "components/enhanced_bookmarks/enhanced_bookmark_switches.h"
+#include "components/offline_pages/offline_page_feature.h"
 #include "components/variations/variations_associated_data.h"
 
 #if defined(OS_IOS) || defined(OS_ANDROID)
@@ -19,6 +20,13 @@ const char kFieldTrialName[] = "EnhancedBookmarks";
 }  // namespace
 
 bool IsEnhancedBookmarksEnabled() {
+#if defined(OS_ANDROID)
+  // If offline pages feature is enabled, also enable enhanced bookmarks feature
+  // regardless its state.
+  if (offline_pages::IsOfflinePagesEnabled())
+    return true;
+#endif
+
   // kEnhancedBookmarksExperiment flag could have values "", "1" and "0".  "" -
   // default, "0" - user opted out, "1" - user opted in.  Tests also use the
   // command line flag to force enhanced bookmark to be on.
