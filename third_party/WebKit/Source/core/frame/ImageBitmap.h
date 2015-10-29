52c94031414222565f2000226d8314912c42d59b
xidachen <xidachen@chromium.org>
2015-10-29 07:45:54 -0700

    Revert of Make ImageBitmap Transferable (patchset #9 id:160001 of https://codereview.chromium.org/1409243005/ )
    Reason for revert:
    There are tests that are crashed: https://storage.googleapis.com/chromium-layout-test-archives/WebKit_Linux_Oilpan_Leak/14727/layout-test-results/results.html
    
    Original issue's description:
    > Make ImageBitmap Transferable
    >
    > Including a layout test that does 2 things:
    > 1. transfer an ImageBitmap to the worker and let worker transfer back to
    >    main, the main thread exam the transfered-back ImageBitmap to make
    >    sure the data is the same as the original ImageBitmap.
    > 2. When worker receives the ImageBitmap from main, worker calls
    >    createImageBimmap() to create a new ImageBitmap and send it back to
    >    main.
    >
    > BUG=532142
    >
    > Committed: https://crrev.com/5d8ed50933226127fdc64744d86ab3d9b7d57276
    > Cr-Commit-Position: refs/heads/master@{#356575}
    
    TBR=jochen@chromium.org,chrishtr@chromium.org,junov@chromium.org,haraken@chromium.org,sigbjornf@opera.com,yutak@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=532142
    
    Review URL: https://codereview.chromium.org/1413723004
    
    Cr-Commit-Position: refs/heads/master@{#356840}

--- third_party/WebKit/Source/core/frame/ImageBitmap.h
+++ third_party/WebKit/Source/core/frame/ImageBitmap.h
@@ -42,9 +42,6 @@ public:
     int height() const { return m_cropRect.height(); }
     IntSize size() const { return m_cropRect.size(); }
 
-    bool isNeutered() const { return !m_bitmap && !m_imageElement; }
-    PassRefPtrWillBeRawPtr<ImageBitmap> transfer();
-
     ~ImageBitmap() override;
 
     // CanvasImageSource implementation
@@ -79,6 +76,7 @@ private:
     // ImageBitmaps constructed from HTMLImageElements reference the entire ImageResource and may have a non-zero bitmap offset.
     // ImageBitmaps not constructed from HTMLImageElements always pre-crop and store the image at (0, 0).
     IntPoint m_bitmapOffset;
+
 };
 
 } // namespace blink
