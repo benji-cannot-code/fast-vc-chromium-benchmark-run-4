b62151c4b1d6d6eccfeec857dddd59ebda389a51
pfeldman <pfeldman@chromium.org>
2015-10-29 10:34:08 -0700

    DevTools: differentiate between user and fs-initiated working copy commits, remove script edited event.
    BUG=529471
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1430633004
    
    Cr-Commit-Position: refs/heads/master@{#356880}

--- third_party/WebKit/Source/devtools/front_end/extensions/ExtensionServer.js
+++ third_party/WebKit/Source/devtools/front_end/extensions/ExtensionServer.js
@@ -866,7 +866,7 @@ WebInspector.ExtensionServer.prototype = {
          */
         function addFirstEventListener()
         {
-            WebInspector.workspace.addEventListener(WebInspector.Workspace.Events.UISourceCodeContentCommitted, handler, this);
+            WebInspector.workspace.addEventListener(WebInspector.Workspace.Events.WorkingCopyCommittedByUser, handler, this);
             WebInspector.workspace.setHasResourceContentTrackingExtensions(true);
         }
 
@@ -876,7 +876,7 @@ WebInspector.ExtensionServer.prototype = {
         function removeLastEventListener()
         {
             WebInspector.workspace.setHasResourceContentTrackingExtensions(false);
-            WebInspector.workspace.removeEventListener(WebInspector.Workspace.Events.UISourceCodeContentCommitted, handler, this);
+            WebInspector.workspace.removeEventListener(WebInspector.Workspace.Events.WorkingCopyCommittedByUser, handler, this);
         }
 
         this._registerSubscriptionHandler(WebInspector.extensionAPI.Events.ResourceContentCommitted,
