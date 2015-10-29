8369eece35b3b163c024f394aed7bb6f33a26566
hbos <hbos@chromium.org>
2015-10-29 03:10:38 -0700

    PeerConnectionIdentityStore with KeyParams.
    This enables generating parameterized key types in the Chromium implementation of webrtc::DtlsIdentityStoreInterface. It lets RTCCertificateGenerator use the parameterization received from RTCPeerConnection.generateCertificate in JavaScript.
    
    This also allows us to in follow up WebRTC-CLs switch over to the parameterized version of RequestIdentity and remove the old one.
    
    BUG=544902
    
    Review URL: https://codereview.chromium.org/1425453002
    
    Cr-Commit-Position: refs/heads/master@{#356799}

--- content/renderer/media/webrtc/peer_connection_dependency_factory.cc
+++ content/renderer/media/webrtc/peer_connection_dependency_factory.cc
@@ -296,7 +296,6 @@ PeerConnectionDependencyFactory::GetPcFactory() {
   return pc_factory_;
 }
 
-
 void PeerConnectionDependencyFactory::WillDestroyCurrentMessageLoop() {
   CleanupPeerConnectionFactory();
 }
@@ -428,6 +427,8 @@ PeerConnectionDependencyFactory::CreatePeerConnection(
 
   rtc::scoped_ptr<PeerConnectionIdentityStore> identity_store(
       new PeerConnectionIdentityStore(
+          base::ThreadTaskRunnerHandle::Get(),
+          GetWebRtcSignalingThread(),
           GURL(web_frame->document().url()),
           GURL(web_frame->document().firstPartyForCookies())));
 
@@ -738,6 +739,11 @@ PeerConnectionDependencyFactory::CreateAudioCapturer(
   return WebRtcAudioCapturer::CreateCapturer(
       render_frame_id, device_info, constraints, GetWebRtcAudioDevice(),
       audio_source);
+}
+
+void PeerConnectionDependencyFactory::EnsureInitialized() {
+  DCHECK(CalledOnValidThread());
+  GetPcFactory();
 }
 
 scoped_refptr<base::SingleThreadTaskRunner>
--- content/renderer/media/webrtc/peer_connection_dependency_factory.h
+++ content/renderer/media/webrtc/peer_connection_dependency_factory.h
@@ -130,6 +130,7 @@ class CONTENT_EXPORT PeerConnectionDependencyFactory
 
   WebRtcAudioDeviceImpl* GetWebRtcAudioDevice();
 
+  void EnsureInitialized();
   scoped_refptr<base::SingleThreadTaskRunner> GetWebRtcWorkerThread() const;
   scoped_refptr<base::SingleThreadTaskRunner> GetWebRtcSignalingThread() const;
 
