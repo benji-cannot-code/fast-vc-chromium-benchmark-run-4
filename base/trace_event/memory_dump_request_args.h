7613a4346d54bd7e46923a84682f84d73392e3d5
primiano <primiano@chromium.org>
2015-10-29 04:28:45 -0700

    [tracing] Add run-time instrumentation of memory-infra
    Add TRACE_EVENT macros to keep track of the evolution of the
    memory dumps in the trace. This can be useful to debug dump
    providers taking too long or being stuck.
    
    The flow sequence of memory-infra events is, for each process:
    - All dumps start with CreateProcessDump()
    - 0..N dump providers are invoked in ContinueAsyncProcessDump()
    - FinalizeDumpAndAddToTrace is called at the end()
    
    On top of that, the coordinator (read browser process) has an
    outer GlobalMemoryDump event, which spans for the entire duration
    of the multi-process dump coordinated via IPC.
    
    BUG=547764
    
    Review URL: https://codereview.chromium.org/1425793002
    
    Cr-Commit-Position: refs/heads/master@{#356811}

--- base/trace_event/memory_dump_request_args.h
+++ base/trace_event/memory_dump_request_args.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This file defines the types and structs used to issue memory dump requests.
 // These are also used in the IPCs for coordinating inter-process memory dumps.
 
+#include <stdint.h>
 #include <string>
 
 #include "base/base_export.h"
@@ -43,13 +44,13 @@ struct BASE_EXPORT MemoryDumpRequestArgs {
   // Globally unique identifier. In multi-process dumps, all processes issue a
   // local dump with the same guid. This allows the trace importers to
   // reconstruct the global dump.
-  uint64 dump_guid;
+  uint64_t dump_guid;
 
   MemoryDumpType dump_type;
   MemoryDumpLevelOfDetail level_of_detail;
 };
 
-using MemoryDumpCallback = Callback<void(uint64 dump_guid, bool success)>;
+using MemoryDumpCallback = Callback<void(uint64_t dump_guid, bool success)>;
 
 BASE_EXPORT const char* MemoryDumpTypeToString(const MemoryDumpType& dump_type);
 
