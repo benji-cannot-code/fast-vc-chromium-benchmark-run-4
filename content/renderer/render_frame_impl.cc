7617cc227793236d20fb046d1d7c5097f3e016f1
rockot <rockot@chromium.org>
2015-10-29 10:00:45 -0700

    Lazily initialize render frame mojo shell connection
    BUG=
    R=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/1423553004
    
    Cr-Commit-Position: refs/heads/master@{#356869}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -790,8 +790,6 @@ RenderFrameImpl::RenderFrameImpl(const CreateParams& params)
 #endif
 
   manifest_manager_ = new ManifestManager(this);
-
-  GetServiceRegistry()->ConnectToRemoteService(mojo::GetProxy(&mojo_shell_));
 }
 
 RenderFrameImpl::~RenderFrameImpl() {
@@ -5282,7 +5280,8 @@ void RenderFrameImpl::RegisterMojoServices() {
 
 mojo::ServiceProviderPtr RenderFrameImpl::ConnectToApplication(
     const GURL& url) {
-  DCHECK(mojo_shell_);
+  if (!mojo_shell_)
+    GetServiceRegistry()->ConnectToRemoteService(mojo::GetProxy(&mojo_shell_));
   mojo::ServiceProviderPtr service_provider;
   mojo::URLRequestPtr request(mojo::URLRequest::New());
   request->url = mojo::String::From(url);
