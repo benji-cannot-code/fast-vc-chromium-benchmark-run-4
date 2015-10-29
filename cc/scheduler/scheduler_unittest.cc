7e7f000ad9bfdc9d57012a53cd4dffa4c2e3c5bd
danakj <danakj@chromium.org>
2015-10-29 12:34:53 -0700

    cc: Decouple SetNeedsRedraw from SetNeedsAnimate.
    This removes a TODO in SchedulerStateMachine::WillAnimate along with a
    call to SetNeedsRedraw there. Instead, the client of the scheduler
    must call SetNeedsRedraw inside the Animate step if anything ticked and
    it wants the scheduler to draw at the frame's deadline.
    
    This is achieved by having LayerTreeHostImpl ask each of the things
    it animates if they changed anything. If they did, and we're animating
    for the active tree (not for the pending tree after a commit), then we
    request a draw for the end of the frame.
    
    This unblocks removing the SetNeedsAnimate state from the scheduler,
    instead always calling Animate() at the front of the impl frame.
    Without this change, we'd have to SetNeedsRedraw at the front of every
    frame which would prevent us from avoiding drawing if there's nothing
    to do.
    
    R=ajuma@chromium.org, boliu, sunnyps
    BUG=522658
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1430563002
    
    Cr-Commit-Position: refs/heads/master@{#356913}

--- cc/scheduler/scheduler_unittest.cc
+++ cc/scheduler/scheduler_unittest.cc
@@ -57,6 +57,8 @@ class FakeSchedulerClient : public SchedulerClient {
   void Reset() {
     actions_.clear();
     states_.clear();
+    animate_causes_redraw_ = false;
+    animate_causes_animate_ = false;
     draw_will_happen_ = true;
     swap_will_happen_if_draw_happens_ = true;
     num_draws_ = 0;
@@ -87,6 +89,12 @@ class FakeSchedulerClient : public SchedulerClient {
     return ActionIndex(action) >= 0;
   }
 
+  void SetAnimateCausesRedraw(bool animate_causes_redraw) {
+    animate_causes_redraw_ = animate_causes_redraw;
+  }
+  void SetAnimateCausesAnimate(bool animate_causes_animate) {
+    animate_causes_animate_ = animate_causes_animate;
+  }
   void SetDrawWillHappen(bool draw_will_happen) {
     draw_will_happen_ = draw_will_happen;
   }
@@ -107,6 +115,10 @@ class FakeSchedulerClient : public SchedulerClient {
   }
   void ScheduledActionAnimate() override {
     PushAction("ScheduledActionAnimate");
+    if (animate_causes_redraw_)
+      scheduler_->SetNeedsRedraw();
+    if (animate_causes_animate_)
+      scheduler_->SetNeedsAnimate();
   }
   DrawResult ScheduledActionDrawAndSwapIfPossible() override {
     PushAction("ScheduledActionDrawAndSwapIfPossible");
@@ -179,6 +191,8 @@ class FakeSchedulerClient : public SchedulerClient {
     return scheduler_->BeginImplFrameDeadlinePending() == state;
   }
 
+  bool animate_causes_redraw_;
+  bool animate_causes_animate_;
   bool draw_will_happen_;
   bool swap_will_happen_if_draw_happens_;
   bool automatic_swap_ack_;
@@ -3082,24 +3096,10 @@ TEST_F(SchedulerTest, SynchronousCompositorAnimation) {
   EXPECT_SINGLE_ACTION("SetNeedsBeginFrames(true)", client_);
   client_->Reset();
 
-  // Next vsync.
-  AdvanceFrame();
-  EXPECT_ACTION("WillBeginImplFrame", client_, 0, 3);
-  EXPECT_ACTION("ScheduledActionAnimate", client_, 1, 3);
-  EXPECT_ACTION("ScheduledActionInvalidateOutputSurface", client_, 2, 3);
-  EXPECT_FALSE(scheduler_->BeginImplFrameDeadlinePending());
-  client_->Reset();
-
-  // Continue with animation.
-  scheduler_->SetNeedsAnimate();
-  EXPECT_NO_ACTION(client_);
-
-  // Android onDraw.
-  scheduler_->SetNeedsRedraw();
-  scheduler_->OnDrawForOutputSurface();
-  EXPECT_SINGLE_ACTION("ScheduledActionDrawAndSwapIfPossible", client_);
-  EXPECT_FALSE(scheduler_->BeginImplFrameDeadlinePending());
-  client_->Reset();
+  // Testing the case where the animate ticks a fling scroll.
+  client_->SetAnimateCausesRedraw(true);
+  // The animation isn't done so it'll cause another tick in the future.
+  client_->SetAnimateCausesAnimate(true);
 
   // Next vsync.
   AdvanceFrame();
@@ -3109,6 +3109,25 @@ TEST_F(SchedulerTest, SynchronousCompositorAnimation) {
   EXPECT_FALSE(scheduler_->BeginImplFrameDeadlinePending());
   client_->Reset();
 
+  // Android onDraw. This doesn't consume the animate tick.
+  scheduler_->SetNeedsRedraw();
+  scheduler_->OnDrawForOutputSurface();
+  EXPECT_SINGLE_ACTION("ScheduledActionDrawAndSwapIfPossible", client_);
+  EXPECT_FALSE(scheduler_->BeginImplFrameDeadlinePending());
+  client_->Reset();
+
+  // The animate changes stuff on the screen, but ends here, so does not
+  // cause another animate step.
+  client_->SetAnimateCausesRedraw(true);
+
+  // Next vsync.
+  AdvanceFrame();
+  EXPECT_ACTION("WillBeginImplFrame", client_, 0, 3);
+  EXPECT_ACTION("ScheduledActionAnimate", client_, 1, 3);
+  EXPECT_ACTION("ScheduledActionInvalidateOutputSurface", client_, 2, 3);
+  EXPECT_FALSE(scheduler_->BeginImplFrameDeadlinePending());
+  client_->Reset();
+
   // Android onDraw.
   scheduler_->SetNeedsRedraw();
   scheduler_->OnDrawForOutputSurface();
@@ -3116,7 +3135,7 @@ TEST_F(SchedulerTest, SynchronousCompositorAnimation) {
   EXPECT_FALSE(scheduler_->BeginImplFrameDeadlinePending());
   client_->Reset();
 
-  // Idle on next vsync.
+  // Idle on next vsync, as the animate has completed.
   AdvanceFrame();
   EXPECT_ACTION("WillBeginImplFrame", client_, 0, 3);
   EXPECT_ACTION("SetNeedsBeginFrames(false)", client_, 1, 3);
--- cc/trees/layer_tree_host_impl.h
+++ cc/trees/layer_tree_host_impl.h
@@ -230,9 +230,10 @@ class CC_EXPORT LayerTreeHostImpl
   virtual void BeginMainFrameAborted(CommitEarlyOutReason reason);
   virtual void BeginCommit();
   virtual void CommitComplete();
-  virtual void Animate();
   virtual void UpdateAnimationState(bool start_ready_animations);
   void ActivateAnimations();
+  void Animate();
+  void AnimatePendingTreeAfterCommit();
   void MainThreadHasStoppedFlinging();
   void DidAnimateScrollOffset();
   void SetViewportDamage(const gfx::Rect& damage_rect);
@@ -613,7 +614,7 @@ class CC_EXPORT LayerTreeHostImpl
       int id);
 
   // Virtual for testing.
-  virtual void AnimateLayers(base::TimeTicks monotonic_time);
+  virtual bool AnimateLayers(base::TimeTicks monotonic_time);
 
   bool is_likely_to_require_a_draw() const {
     return is_likely_to_require_a_draw_;
@@ -639,6 +640,8 @@ class CC_EXPORT LayerTreeHostImpl
   void ReleaseTreeResources();
   void RecreateTreeResources();
 
+  void AnimateInternal(bool active_tree);
+
   void UpdateGpuRasterizationStatus();
 
   Viewport* viewport() { return viewport_.get(); }
@@ -654,9 +657,9 @@ class CC_EXPORT LayerTreeHostImpl
       LayerImpl* scrolling_layer_impl,
       InputHandler::ScrollInputType type);
 
-  void AnimatePageScale(base::TimeTicks monotonic_time);
-  void AnimateScrollbars(base::TimeTicks monotonic_time);
-  void AnimateTopControls(base::TimeTicks monotonic_time);
+  bool AnimatePageScale(base::TimeTicks monotonic_time);
+  bool AnimateScrollbars(base::TimeTicks monotonic_time);
+  bool AnimateTopControls(base::TimeTicks monotonic_time);
 
   void TrackDamageForAllSurfaces(
       LayerImpl* root_draw_layer,
