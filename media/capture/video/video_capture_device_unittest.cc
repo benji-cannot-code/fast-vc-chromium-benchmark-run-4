50819343ec00b7673431c88240fc8fbe46814045
dalecurtis <dalecurtis@chromium.org>
2015-10-30 11:41:41 -0700

    Disable VideoCaptureDeviceTest.ReAllocateCamera test.
    Fails consistently on my Linux workstation and also seems to have
    trouble on a variety of bots that actually have camera hardware,
    so disable everywhere.
    
    The primary cause seems to be that this will flake other tests when
    run in parallel.
    
    BUG=417824
    TEST=all media_unittests pass when a camera is attached.
    
    Review URL: https://codereview.chromium.org/1428853003
    
    Cr-Commit-Position: refs/heads/master@{#357147}

--- media/capture/video/video_capture_device_unittest.cc
+++ media/capture/video/video_capture_device_unittest.cc
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // main thread for tests. This results in no frame received by
 // VideoCaptureAndroid.
 #define MAYBE_AllocateBadSize DISABLED_AllocateBadSize
-#define ReAllocateCamera DISABLED_ReAllocateCamera
 #define DeAllocateCameraWhileRunning DISABLED_DeAllocateCameraWhileRunning
 #define DeAllocateCameraWhileRunning DISABLED_DeAllocateCameraWhileRunning
 #define MAYBE_CaptureMjpeg DISABLED_CaptureMjpeg
@@ -330,7 +329,7 @@ TEST_P(VideoCaptureDeviceTest, CaptureWithSize) {
 }
 
 #if !defined(OS_ANDROID)
-INSTANTIATE_TEST_CASE_P(MAYBE_VideoCaptureDeviceTests,
+INSTANTIATE_TEST_CASE_P(VideoCaptureDeviceTests,
                         VideoCaptureDeviceTest,
                         testing::ValuesIn(kCaptureSizes));
 #endif
@@ -362,14 +361,8 @@ TEST_F(VideoCaptureDeviceTest, MAYBE_AllocateBadSize) {
     EXPECT_EQ(input_size.GetArea(), last_format().frame_size.GetArea());
 }
 
-// Cause hangs on Windows Debug. http://crbug.com/417824
-#if defined(OS_WIN) && !defined(NDEBUG)
-#define MAYBE_ReAllocateCamera DISABLED_ReAllocateCamera
-#else
-#define MAYBE_ReAllocateCamera ReAllocateCamera
-#endif
-
-TEST_F(VideoCaptureDeviceTest, MAYBE_ReAllocateCamera) {
+// Cause hangs on Windows, Linux. Fails Android. http://crbug.com/417824
+TEST_F(VideoCaptureDeviceTest, DISABLED_ReAllocateCamera) {
   names_ = EnumerateDevices();
   if (names_->empty()) {
     DVLOG(1) << "No camera available. Exiting test.";
