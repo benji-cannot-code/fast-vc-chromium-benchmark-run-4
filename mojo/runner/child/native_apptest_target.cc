345080a2a8d5b078fa07e6e725d3032c04782834
erg <erg@chromium.org>
2015-10-30 11:37:34 -0700

    mandoline: Fix mandoline release build on Linux.
    BUG=548694
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/1425693004
    
    Cr-Commit-Position: refs/heads/master@{#357141}

--- mojo/runner/child/native_apptest_target.cc
+++ mojo/runner/child/native_apptest_target.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <windows.h>
-
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/debug/stack_trace.h"
@@ -25,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/runner/init.h"
 #include "third_party/mojo/src/mojo/edk/embedder/embedder.h"
 #include "third_party/mojo/src/mojo/edk/embedder/process_delegate.h"
+
+#if defined(OS_WIN)
+#include <windows.h>
+#endif
 
 namespace {
 
