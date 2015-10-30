8b53b77b6a0a29d32b8ae364a2b0588b414e0e65
ksakamoto <ksakamoto@chromium.org>
2015-10-30 01:20:20 -0700

    Fix corrupted LayoutTests/http/tests/resources/Ahem.ttf
    Copied correct file from LayoutTests/resources/Ahem.ttf.
    Also, updated http tests to use the fixed one.
    
    BUG=548543
    
    Review URL: https://codereview.chromium.org/1417943005
    
    Cr-Commit-Position: refs/heads/master@{#357053}

--- third_party/WebKit/LayoutTests/http/tests/css/resources/cors-ahem.php
+++ third_party/WebKit/LayoutTests/http/tests/css/resources/cors-ahem.php
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 if (isset($_SERVER["HTTP_IF_MODIFIED_SINCE"])) {
     header("HTTP/1.1 304 Not Modified");
 } else {
-    $font = "../../../../resources/Ahem.ttf";
+    $font = "../../resources/Ahem.ttf";
 
     header("Cache-Control: public, max-age=86400");
     header('Last-Modified: ' . gmdate("D, d M Y H:i:s", filemtime($font)) . " GMT");
--- third_party/WebKit/LayoutTests/http/tests/security/resources/cors-font.php
+++ third_party/WebKit/LayoutTests/http/tests/security/resources/cors-font.php
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
 
-$font = "../../../../resources/Ahem.ttf";
+$font = "../../resources/Ahem.ttf";
 
 header("Cache-Control: public, max-age=86400");
 header('Last-Modified: ' . gmdate("D, d M Y H:i:s", filemtime($font)) . " GMT");
