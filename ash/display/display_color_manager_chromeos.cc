f89898857ebbd8b5fdee6d0de8ba3ce1dbc983e9
robert.bradford <robert.bradford@intel.com>
2015-10-30 08:42:15 -0700

    ash: Remove --internal-display-color-profile-file
    This was useful for debugging and early integration efforts in ChromeOS
    and is no longer required.
    
    BUG=549558
    TEST=On amd64-generic_freon observe that loading from named .icc file
    functions correctly.
    
    Review URL: https://codereview.chromium.org/1428963002
    
    Cr-Commit-Position: refs/heads/master@{#357110}

--- ash/display/display_color_manager_chromeos.cc
+++ ash/display/display_color_manager_chromeos.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_runner_util.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "chromeos/chromeos_paths.h"
-#include "chromeos/chromeos_switches.h"
 #include "third_party/qcms/src/qcms.h"
 #include "ui/display/types/display_snapshot.h"
 #include "ui/display/types/gamma_ramp_rgb_entry.h"
@@ -68,17 +67,6 @@ bool ParseFile(const base::FilePath& path,
 }
 
 base::FilePath PathForDisplaySnapshot(const ui::DisplaySnapshot* snapshot) {
-  if (gfx::Display::IsInternalDisplayId(snapshot->display_id())) {
-    const base::CommandLine* command_line =
-        base::CommandLine::ForCurrentProcess();
-    if (command_line->HasSwitch(
-            chromeos::switches::kInternalDisplayColorProfileFile)) {
-      const base::FilePath& path = command_line->GetSwitchValuePath(
-          chromeos::switches::kInternalDisplayColorProfileFile);
-      return base::FilePath(path);
-    }
-  }
-
   base::FilePath path;
   CHECK(
       PathService::Get(chromeos::DIR_DEVICE_COLOR_CALIBRATION_PROFILES, &path));
