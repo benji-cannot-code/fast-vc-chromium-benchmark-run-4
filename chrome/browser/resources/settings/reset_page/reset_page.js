06b908557617d5502f3cf4225c20f5595c162b00
dpapad <dpapad@chromium.org>
2015-10-30 12:35:19 -0700

    MD Settings: Adding reset page Material UI dialog.
    BUG=546840
    
    Review URL: https://codereview.chromium.org/1418073008
    
    Cr-Commit-Position: refs/heads/master@{#357173}

---
+++ chrome/browser/resources/settings/reset_page/reset_page.js
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * 'settings-reset-page' is the settings page containing reset
+ * settings.
+ *
+ * Example:
+ *
+ *    <iron-animated-pages>
+ *      <settings-reset-page prefs="{{prefs}}">
+ *      </settings-reset-page>
+ *      ... other pages ...
+ *    </iron-animated-pages>
+ *
+ * @group Chrome Settings Elements
+ * @element settings-reset-page
+ */
+Polymer({
+  is: 'settings-reset-page',
+
+  /** @private */
+  onShowDialog_: function() {
+    this.$.resetDialog.open();
+  },
+
+  /** @private */
+  onCancelTap_: function() {
+    this.$.resetDialog.close();
+  },
+
+  /** @private */
+  onResetTap_: function() {
+    // TODO(dpapad): Set up C++ handlers and figure out when it is OK to close
+    // the dialog.
+    this.$.resetDialog.close();
+  },
+
+  /** @private */
+  onLearnMoreTap_: function() {
+    window.open(loadTimeData.getString('resetPageLearnMoreUrl'));
+  }
+});
