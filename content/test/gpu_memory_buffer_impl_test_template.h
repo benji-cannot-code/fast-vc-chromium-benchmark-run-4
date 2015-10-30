ffede744dfbe19c820b09975717c692097b071ae
reveman <reveman@chromium.org>
2015-10-30 10:05:44 -0700

    ui: Revert gfx::BufferUsage::SCANOUT to gfx::BufferUsage::GPU_READ_WRITE change.
    BUG=538325
    
    Review URL: https://codereview.chromium.org/1417753007
    
    Cr-Commit-Position: refs/heads/master@{#357128}

--- content/test/gpu_memory_buffer_impl_test_template.h
+++ content/test/gpu_memory_buffer_impl_test_template.h
@@ -47,7 +47,7 @@ TYPED_TEST_P(GpuMemoryBufferImplTest, CreateFromHandle) {
 
   for (auto format : gfx::GetBufferFormatsForTesting()) {
     gfx::BufferUsage usages[] = {
-        gfx::BufferUsage::GPU_READ, gfx::BufferUsage::GPU_READ_WRITE,
+        gfx::BufferUsage::GPU_READ, gfx::BufferUsage::SCANOUT,
         gfx::BufferUsage::GPU_READ_CPU_READ_WRITE,
         gfx::BufferUsage::GPU_READ_CPU_READ_WRITE_PERSISTENT};
     for (auto usage : usages) {
--- ui/ozone/platform/drm/gpu/gbm_buffer.cc
+++ ui/ozone/platform/drm/gpu/gbm_buffer.cc
@@ -26,10 +26,7 @@ namespace ui {
 GbmBuffer::GbmBuffer(const scoped_refptr<GbmDevice>& gbm,
                      gbm_bo* bo,
                      gfx::BufferUsage usage)
-    : GbmBufferBase(gbm,
-                    bo,
-                    usage == gfx::BufferUsage::GPU_READ ||
-                        usage == gfx::BufferUsage::GPU_READ_WRITE),
+    : GbmBufferBase(gbm, bo, usage == gfx::BufferUsage::SCANOUT),
       usage_(usage) {}
 
 GbmBuffer::~GbmBuffer() {
@@ -45,12 +42,10 @@ scoped_refptr<GbmBuffer> GbmBuffer::CreateBuffer(
     gfx::BufferUsage usage) {
   TRACE_EVENT2("drm", "GbmBuffer::CreateBuffer", "device",
                gbm->device_path().value(), "size", size.ToString());
-  bool with_cpu_access =
-      usage == gfx::BufferUsage::GPU_READ_CPU_READ_WRITE ||
-      usage == gfx::BufferUsage::GPU_READ_CPU_READ_WRITE_PERSISTENT;
+  bool use_scanout = (usage == gfx::BufferUsage::SCANOUT);
   unsigned flags = 0;
   // GBM_BO_USE_SCANOUT is the hint of x-tiling.
-  if (!with_cpu_access)
+  if (use_scanout)
     flags = GBM_BO_USE_SCANOUT | GBM_BO_USE_RENDERING;
   gbm_bo* bo = gbm_bo_create(gbm->device(), size.width(), size.height(),
                              GetFourCCFormatFromBufferFormat(format), flags);
@@ -58,7 +53,7 @@ scoped_refptr<GbmBuffer> GbmBuffer::CreateBuffer(
     return nullptr;
 
   scoped_refptr<GbmBuffer> buffer(new GbmBuffer(gbm, bo, usage));
-  if (!with_cpu_access && !buffer->GetFramebufferId())
+  if (use_scanout && !buffer->GetFramebufferId())
     return nullptr;
 
   return buffer;
@@ -146,8 +141,7 @@ bool GbmPixmap::ScheduleOverlayPlane(gfx::AcceleratedWidget widget,
     return false;
   }
 
-  DCHECK(buffer_->GetUsage() == gfx::BufferUsage::GPU_READ ||
-         buffer_->GetUsage() == gfx::BufferUsage::GPU_READ_WRITE);
+  DCHECK(buffer_->GetUsage() == gfx::BufferUsage::SCANOUT);
   surface_manager_->GetSurface(widget)->QueueOverlayPlane(OverlayPlane(
       buffer_, plane_z_order, plane_transform, display_bounds, crop_rect));
   return true;
