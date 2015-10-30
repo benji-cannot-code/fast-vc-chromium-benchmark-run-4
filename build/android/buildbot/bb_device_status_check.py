39a77fcac7d6ca3532a40ed201266a5f4988651c
bpastene <bpastene@chromium.org>
2015-10-30 16:35:50 -0700

    make the device status a little more accurate
    this was mixing 'offline' and 'unauthorized' into the same status; now it should be more precise
    
    BUG=482259
    
    Review URL: https://codereview.chromium.org/1413033008
    
    Cr-Commit-Position: refs/heads/master@{#357225}

--- build/android/buildbot/bb_device_status_check.py
+++ build/android/buildbot/bb_device_status_check.py
@@ -183,7 +183,7 @@ def DeviceStatus(devices, blacklist):
             blacklist.Extend([serial], reason='status_check_timeout')
 
     elif blacklist:
-      blacklist.Extend([serial], reason='offline')
+      blacklist.Extend([serial], reason=adb_status)
 
     device_status['blacklisted'] = _IsBlacklisted(serial, blacklist)
 
