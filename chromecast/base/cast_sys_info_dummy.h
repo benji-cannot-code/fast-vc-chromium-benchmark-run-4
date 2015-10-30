8e871dfc18ea089bb09502ba4b02d0a4693c005d
slan <slan@chromium.org>
2015-10-30 16:51:13 -0700

    [Chromecast] Clean up GN so that gn check passes.
    gn check will be automated via the CQ. Update dependencies to make gn
    check pass upstream. This change makes //chromecast/media:media a
    meta-target to prevent long deps lists of individual targets.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1427993002
    
    Cr-Commit-Position: refs/heads/master@{#357231}

--- chromecast/base/cast_sys_info_dummy.h
+++ chromecast/base/cast_sys_info_dummy.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMECAST_BASE_CAST_SYS_INFO_DUMMY_H_
 #define CHROMECAST_BASE_CAST_SYS_INFO_DUMMY_H_
 
-#include "base/macros.h"
+// Note(slan): This file is needed by internal targets which cannot depend on
+// "//base". Amend this include with a comment so gn check ignores it.
+#include "base/macros.h"  // nogncheck
 #include "chromecast/public/cast_sys_info.h"
 
 namespace chromecast {
