4cbc2c12b40410c460596a211d57d033fcdcff47
oysteine <oysteine@chromium.org>
2015-10-30 15:35:49 -0700

    Background tracing: Added trace_chance
    This allows trigger rules to be configured to only successfully trigger a certain percentage of the time.
    
    R=shatch
    BUG=546193
    
    Review URL: https://codereview.chromium.org/1427873003
    
    Cr-Commit-Position: refs/heads/master@{#357204}

--- content/browser/tracing/background_tracing_manager_impl.cc
+++ content/browser/tracing/background_tracing_manager_impl.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_writer.h"
 #include "base/macros.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/rand_util.h"
 #include "base/sys_info.h"
 #include "base/time/time.h"
 #include "content/browser/tracing/background_tracing_rule.h"
@@ -322,6 +323,13 @@ void BackgroundTracingManagerImpl::OnRuleTriggered(
     const BackgroundTracingRule* triggered_rule,
     StartedFinalizingCallback callback) {
   CHECK(config_);
+
+  double trigger_chance = triggered_rule->trigger_chance();
+  if (trigger_chance < 1.0 && base::RandDouble() > trigger_chance) {
+    if (!callback.is_null())
+      callback.Run(false);
+    return;
+  }
 
   int trace_timeout = triggered_rule->GetTraceTimeout();
 
