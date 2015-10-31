c6e325ca9b47849447ad76ac99b95f02ef9fc5d6
reillyg <reillyg@chromium.org>
2015-10-31 12:25:17 -0700

    Make DeviceArray in USB.cpp STATIC_ONLY.
    WTF_MAKE_NONCOPYABLE was not correct here. It should be STATIC_ONLY.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1408763003
    
    Cr-Commit-Position: refs/heads/master@{#357279}

--- third_party/WebKit/Source/modules/webusb/USB.cpp
+++ third_party/WebKit/Source/modules/webusb/USB.cpp
@@ -60,7 +60,7 @@ void convertDeviceRequestOptions(const USBDeviceRequestOptions& options, WebUSBD
 // Allows using a CallbackPromiseAdapter with a WebVector to resolve the
 // getDevices() promise with a HeapVector owning USBDevices.
 class DeviceArray {
-    WTF_MAKE_NONCOPYABLE(DeviceArray);
+    STATIC_ONLY(DeviceArray);
 public:
     using WebType = OwnPtr<WebVector<WebUSBDevice*>>;
 
@@ -71,9 +71,6 @@ public:
             devices.append(USBDevice::create(adoptPtr(webDevice)));
         return devices;
     }
-
-private:
-    DeviceArray() = delete;
 };
 
 } // namespace
