dfba6b55d08e5e2f12754863b5db27e2c5c9cfef
vmpstr <vmpstr@chromium.org>
2015-10-30 18:06:33 -0700

    Remove std::bind2nd uses.
    This patch removes std::bind2nd uses, since this is one of the functions
    that was deprecated in c++11.
    
    Review URL: https://codereview.chromium.org/1421893004
    
    Cr-Commit-Position: refs/heads/master@{#357250}

--- ui/compositor/layer.cc
+++ ui/compositor/layer.cc
@@ -824,12 +824,11 @@ void Layer::OnAnimationStarted(const cc::AnimationEvent& event) {
 }
 
 void Layer::CollectAnimators(
-    std::vector<scoped_refptr<LayerAnimator> >* animators) {
+    std::vector<scoped_refptr<LayerAnimator>>* animators) {
   if (IsAnimating())
     animators->push_back(animator_);
-  std::for_each(children_.begin(), children_.end(),
-                std::bind2nd(std::mem_fun(&Layer::CollectAnimators),
-                             animators));
+  for (auto* child : children_)
+    child->CollectAnimators(animators);
 }
 
 void Layer::StackRelativeTo(Layer* child, Layer* other, bool above) {
@@ -1083,11 +1082,8 @@ void Layer::AddAnimatorsInTreeToCollection(
   DCHECK(collection);
   if (IsAnimating())
     animator_->AddToCollection(collection);
-  std::for_each(
-      children_.begin(),
-      children_.end(),
-      std::bind2nd(std::mem_fun(&Layer::AddAnimatorsInTreeToCollection),
-                   collection));
+  for (auto* child : children_)
+    child->AddAnimatorsInTreeToCollection(collection);
 }
 
 void Layer::RemoveAnimatorsInTreeFromCollection(
@@ -1095,11 +1091,8 @@ void Layer::RemoveAnimatorsInTreeFromCollection(
   DCHECK(collection);
   if (IsAnimating())
     animator_->RemoveFromCollection(collection);
-  std::for_each(
-      children_.begin(),
-      children_.end(),
-      std::bind2nd(std::mem_fun(&Layer::RemoveAnimatorsInTreeFromCollection),
-                   collection));
+  for (auto* child : children_)
+    child->RemoveAnimatorsInTreeFromCollection(collection);
 }
 
 bool Layer::IsAnimating() const {
