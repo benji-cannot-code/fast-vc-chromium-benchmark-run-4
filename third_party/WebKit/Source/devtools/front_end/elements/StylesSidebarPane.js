2e48dda310c2ef4a04f0b34b1d2e1a0b0ad93d34
lushnikov <lushnikov@chromium.org>
2015-10-30 17:48:21 -0700

    DevTools: remove logic from WI.StylesSectionModel
    In future, we will not have WI.StylesSectionModel class; instead,
    a simple WI.CSSStyleDeclaration will be used. The difference between the
    two is that WI.CSSStyleDeclaration is totally unaware of the cascade
    it belongs to, whereis WI.StylesSectionModel is.
    
    The patch removes WI.StylesSectionModel.isPropertyInCascade and
    WI.StylesSectionModel.isPropertyOverloaded method. Instead, a single
    WI.SectionCascade.propertyState(property) method is introduced.
    
    The method returns either:
    - null, if the property doesn't take part in the cascade (e.g. the rule
      does not have matching selectors).
    - WI.SectionCascade.PropertyState.Overloaded, if the property is overloaded
      with some other property in the cascade.
    - WI.SectionCascade.PropertyState.Active, if the property is effective.
    
    The patch also removes the "allUsedProperties" from SectionCascade and moves logic into ComputedStyleWidget.
    
    BUG=537827
    
    Review URL: https://codereview.chromium.org/1409733005
    
    Cr-Commit-Position: refs/heads/master@{#357242}

--- third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
@@ -1100,7 +1100,8 @@ WebInspector.StylePropertiesSection.prototype = {
         } else {
             var child = this.propertiesTreeOutline.firstChild();
             while (child) {
-                child.setOverloaded(this.styleRule.isPropertyOverloaded(child.name));
+                var overloaded = this.styleRule.cascade().propertyState(child.property) === WebInspector.SectionCascade.PropertyState.Overloaded;
+                child.setOverloaded(overloaded);
                 child = child.traverseNextTreeElement(false, null, true);
             }
         }
@@ -1126,7 +1127,7 @@ WebInspector.StylePropertiesSection.prototype = {
         for (var property of style.leadingProperties()) {
             var isShorthand = !!WebInspector.CSSMetadata.cssPropertiesMetainfo.longhands(property.name);
             var inherited = this.isPropertyInherited(property.name);
-            var overloaded = this.styleRule.isPropertyOverloaded(property.name);
+            var overloaded = this.styleRule.cascade().propertyState(property) === WebInspector.SectionCascade.PropertyState.Overloaded;
             var item = new WebInspector.StylePropertyTreeElement(this._parentPane, this.styleRule, property, isShorthand, inherited, overloaded);
             this.propertiesTreeOutline.appendChild(item);
         }
@@ -2014,7 +2015,7 @@ WebInspector.StylePropertyTreeElement.prototype = {
             var section = this.section();
             if (section) {
                 inherited = section.isPropertyInherited(name);
-                overloaded = section.styleRule.isPropertyOverloaded(name);
+                overloaded = section.styleRule.cascade().propertyState(longhandProperties[i]) === WebInspector.SectionCascade.PropertyState.Overloaded;
             }
 
             var item = new WebInspector.StylePropertyTreeElement(this._parentPane, this._styleRule, longhandProperties[i], false, inherited, overloaded);
