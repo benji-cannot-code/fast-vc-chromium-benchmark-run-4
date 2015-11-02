0f02d40984f1d346def922cda48130c3fe08f829
ianwen <ianwen@chromium.org>
2015-11-02 12:15:31 -0800

    Add progressbar to animated doodle
    EnhancedBookmarkLoadingView is moved to widget folder in this CL so that
    it could be used by ntp.
    
    BUG=536210
    
    Review URL: https://codereview.chromium.org/1398423002
    
    Cr-Commit-Position: refs/heads/master@{#357414}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -527,6 +527,13 @@ public class NewTabPageView extends FrameLayout
     }
 
     /**
+     * Shows a progressbar indicating the animated logo is being downloaded.
+     */
+    void showLogoLoadingView() {
+        mSearchProviderLogoView.showLoadingView();
+    }
+
+    /**
      * Starts playing the given animated GIF logo.
      */
     void playAnimatedLogo(BaseGifImage gifImage) {
