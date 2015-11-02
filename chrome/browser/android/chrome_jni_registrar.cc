339f30263d7772959f757d533600eeb518919e1b
megjablon <megjablon@chromium.org>
2015-11-02 14:57:42 -0800

    Create a Java SessionTabHelper
    Create a Java SessionTabHelper with idForTab so that the
    DataUseTabUIManager in Java can get the C++ notion of a tab ID from
    a Java tab.
    
    TBR=thestig@chromium.org
    
    BUG=547256
    
    Review URL: https://codereview.chromium.org/1423933002
    
    Cr-Commit-Position: refs/heads/master@{#357462}

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/rappor/rappor_service_bridge.h"
 #include "chrome/browser/android/recently_closed_tabs_bridge.h"
 #include "chrome/browser/android/rlz/revenue_stats.h"
+#include "chrome/browser/android/sessions/session_tab_helper_android.h"
 #include "chrome/browser/android/shortcut_helper.h"
 #include "chrome/browser/android/signin/account_management_screen_helper.h"
 #include "chrome/browser/android/signin/account_tracker_service_android.h"
@@ -316,6 +317,7 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
     {"SavePasswordInfoBar", SavePasswordInfoBar::Register},
     {"SceneLayer", chrome::android::RegisterSceneLayer},
     {"ServiceTabLauncher", service_tab_launcher::RegisterServiceTabLauncherJni},
+    {"SessionTabHelper", RegisterSessionTabHelper},
     {"SigninManager", SigninManagerAndroid::Register},
     {"SingleTabModel", RegisterSingleTabModel},
     {"SpellCheckerSessionBridge", SpellCheckerSessionBridge::RegisterJNI},
---
+++ chrome/browser/android/sessions/session_tab_helper_android.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/android/sessions/session_tab_helper_android.h"
+
+#include "base/android/jni_android.h"
+#include "chrome/browser/sessions/session_tab_helper.h"
+#include "content/public/browser/web_contents.h"
+#include "jni/SessionTabHelper_jni.h"
+
+// static
+jint IdForTab(JNIEnv* env,
+              const JavaParamRef<jclass>& clazz,
+              const JavaParamRef<jobject>& java_web_contents) {
+  content::WebContents* web_contents =
+      content::WebContents::FromJavaWebContents(java_web_contents);
+  CHECK(web_contents);
+  return SessionTabHelper::IdForTab(web_contents);
+}
+
+bool RegisterSessionTabHelper(JNIEnv* env) {
+  return RegisterNativesImpl(env);
+}
