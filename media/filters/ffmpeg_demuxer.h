c125b88d3d2ed883e602dffcf7d654092d9bdf1f
dalecurtis <dalecurtis@chromium.org>
2015-11-02 15:12:26 -0800

    Fix race on demuxer memory usage. Reuse previous calculation.
    GetMemoryUsage() is called from the render thread, so access to
    FFmpegDemuxer internals must be procted.  Adding locking everywhere
    would be a big change, so simply cache the last value when a new
    packet is added and protect it via a lock.
    
    BUG=550375
    TEST=all tests pass, no tsan issue.
    
    Review URL: https://codereview.chromium.org/1419753007
    
    Cr-Commit-Position: refs/heads/master@{#357464}

--- media/filters/ffmpeg_demuxer.h
+++ media/filters/ffmpeg_demuxer.h
@@ -234,8 +234,9 @@ class MEDIA_EXPORT FFmpegDemuxer : public Demuxer {
   // go over capacity depending on how the file is muxed.
   bool StreamsHaveAvailableCapacity();
 
-  // Returns true if the maximum allowed memory usage has been reached.
-  bool IsMaxMemoryUsageReached() const;
+  // Updates |stream_memory_usage_| to the memory usage in bytes of all
+  // FFmpegDemuxerStreams.  Returns the current memory usage.
+  int64_t UpdateMemoryUsage();
 
   // Signal all FFmpegDemuxerStreams that the stream has ended.
   void StreamHasEnded();
@@ -323,6 +324,10 @@ class MEDIA_EXPORT FFmpegDemuxer : public Demuxer {
   scoped_ptr<FFmpegGlue> glue_;
 
   const EncryptedMediaInitDataCB encrypted_media_init_data_cb_;
+
+  // Last stream size as calculated by UpdateMemoryUsage().
+  mutable base::Lock stream_memory_usage_lock_;
+  int64_t stream_memory_usage_;
 
   // NOTE: Weak pointers must be invalidated before all other member variables.
   base::WeakPtrFactory<FFmpegDemuxer> weak_factory_;
