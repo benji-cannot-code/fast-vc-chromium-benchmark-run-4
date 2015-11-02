7458be63a1399dc85cb8b0a620ba751f7a144298
kerrnel <kerrnel@chromium.org>
2015-11-02 15:40:53 -0800

    Implement anonymous, opt-in, collection of OS X binary integrity incidents.
    This change implements anonymous collection of data about binary integrity
    incidents on OS X. A binary integrity incident means that Chrome's application
    bundle was modified on disk by some unauthorized source. This change collects
    information about modified files. The checks are enabled on a sampling of user's
    browsers and run at browser startup if enabled.
    
    BUG=530314
    
    Review URL: https://codereview.chromium.org/1363613004
    
    Cr-Commit-Position: refs/heads/master@{#357469}

--- chrome/browser/safe_browsing/incident_reporting/binary_integrity_analyzer.h
+++ chrome/browser/safe_browsing/incident_reporting/binary_integrity_analyzer.h
@@ -6,12 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SAFE_BROWSING_INCIDENT_REPORTING_BINARY_INTEGRITY_ANALYZER_H_
 #define CHROME_BROWSER_SAFE_BROWSING_INCIDENT_REPORTING_BINARY_INTEGRITY_ANALYZER_H_
 
-#include <vector>
+#include <string>
 
 #include "base/memory/scoped_ptr.h"
 
 namespace base {
 class FilePath;
+class TimeDelta;
 }  // namespace base
 
 namespace safe_browsing {
@@ -27,8 +28,14 @@ void RegisterBinaryIntegrityAnalysis();
 // service will decide when to start the analysis.
 void VerifyBinaryIntegrity(scoped_ptr<IncidentReceiver> incident_receiver);
 
-// Returns a vector containing the paths to all the binaries to verify.
-std::vector<base::FilePath> GetCriticalBinariesPath();
+// Record how long the signature verification took.
+void RecordSignatureVerificationTime(size_t file_index,
+                                     const base::TimeDelta& verification_time);
+
+// Clear past incident reports for a file or bundle. This is used if the code
+// object is now integral, as it will allow future incidents to be reported.
+void ClearBinaryIntegrityForFile(IncidentReceiver* incident_receiver,
+                                 const std::string& basename);
 
 }  // namespace safe_browsing
 
