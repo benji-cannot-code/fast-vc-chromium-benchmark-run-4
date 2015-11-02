cddd3dc81454037e71d8b32cf957287ae712361b
alancutter <alancutter@chromium.org>
2015-11-02 14:39:18 -0800

    Web Animations: Add SVGNumberInterpolationType
    This change migrates the following SVG attribute animations over to
    SVGNumberInterpolationType and enables additive keyframe composite modes
    as well as neutral keyframes:
    amplitude, azimuth, bias, diffuseConstant, divisor, elevation, exponent,
    intercept, k1, k2, k3, k4, limitingConeAngle, offset, pathLength,
    pointsAtX, pointsAtY, pointsAtZ, scale, seed, slope, specularConstant,
    specularExponent, surfaceScale, z.
    
    BUG=530436
    
    Review URL: https://codereview.chromium.org/1410643011
    
    Cr-Commit-Position: refs/heads/master@{#357454}

---
+++ third_party/WebKit/Source/core/animation/NumberAttributeFunctions.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NumberAttributeFunctions_h
+#define NumberAttributeFunctions_h
+
+#include "core/SVGNames.h"
+
+namespace blink {
+
+class NumberAttributeFunctions {
+public:
+    static bool isNonNegative(const QualifiedName& attribute)
+    {
+        return attribute == SVGNames::pathLengthAttr;
+    }
+};
+
+} // namespace blink
+
+#endif // NumberAttributeFunctions_h
---
+++ third_party/WebKit/Source/core/animation/SVGNumberInterpolationType.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SVGNumberInterpolationType_h
+#define SVGNumberInterpolationType_h
+
+#include "core/animation/NumberAttributeFunctions.h"
+#include "core/animation/SVGInterpolationType.h"
+
+namespace blink {
+
+class SVGNumberInterpolationType : public SVGInterpolationType {
+public:
+    SVGNumberInterpolationType(const QualifiedName& attribute)
+        : SVGInterpolationType(attribute)
+        , m_isNonNegative(NumberAttributeFunctions::isNonNegative(attribute))
+    { }
+
+private:
+    PassOwnPtr<InterpolationValue> maybeConvertNeutral() const final;
+    PassOwnPtr<InterpolationValue> maybeConvertSVGValue(const SVGPropertyBase& svgValue) const final;
+    PassOwnPtr<InterpolationValue> maybeConvertUnderlyingValue(const InterpolationEnvironment&) const final;
+    RefPtrWillBeRawPtr<SVGPropertyBase> appliedSVGValue(const InterpolableValue&, const NonInterpolableValue*) const final;
+
+    bool m_isNonNegative;
+};
+
+} // namespace blink
+
+#endif // SVGNumberInterpolationType_h
