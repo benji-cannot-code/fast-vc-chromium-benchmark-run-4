c596df4a9d8e6eb85f42aac84297d21d8e9558c4
wangxianzhu <wangxianzhu@chromium.org>
2015-11-02 11:05:40 -0800

    Don't paint GraphicsLayer if !drawsContent
    Fix crash running some browser tests with sync painting enabled.
    (http://build.chromium.org/p/tryserver.chromium.linux/builders/linux_chromium_rel_ng/builds/133341).
    E.g. painting non-drawsContent scrollbar layer causes crashes because of
    display item caching and invalidation issues (we don't invalidate
    a non-drawsContent GraphicsLayer).
    
    BUG=536999
    
    Review URL: https://codereview.chromium.org/1418533011
    
    Cr-Commit-Position: refs/heads/master@{#357395}

--- third_party/WebKit/Source/platform/graphics/GraphicsLayer.cpp
+++ third_party/WebKit/Source/platform/graphics/GraphicsLayer.cpp
@@ -297,6 +297,7 @@ void GraphicsLayer::setOffsetDoubleFromLayoutObject(const DoubleSize& offset, Sh
 void GraphicsLayer::paint(GraphicsContext& context, const IntRect* clip)
 {
     ASSERT(clip || RuntimeEnabledFeatures::slimmingPaintSynchronizedPaintingEnabled());
+    ASSERT(drawsContent());
 
     if (!m_client)
         return;
@@ -874,6 +875,9 @@ void GraphicsLayer::setDrawsContent(bool drawsContent)
 
     m_drawsContent = drawsContent;
     updateLayerIsDrawable();
+
+    if (!drawsContent && m_paintController)
+        m_paintController.clear();
 }
 
 void GraphicsLayer::setContentsVisible(bool contentsVisible)
