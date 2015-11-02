07595d4087f750e9636f47b1599fb56660227220
bnc <bnc@chromium.org>
2015-11-02 05:39:20 -0800

    Add new, unused methods to SpdyFramerVisitorInterface.
    The new methods will allow SpdyFramer to pass headers to visitors through a
    SpdyHeadersHandlerInterface.
    
    There are so many descendants of SpdyFramerVisitorInterface that I thought it
    was best to start with the interface change, and add an unused stub
    implementation everywhere.
    
    This CL lands server change 106411694 by birenroy.
    
    BUG=488484
    
    Committed: https://crrev.com/5d0c7230e6d1e8da9597c5a967a7b92638a58442
    Cr-Commit-Position: refs/heads/master@{#357084}
    
    Review URL: https://codereview.chromium.org/1413683005
    
    Cr-Commit-Position: refs/heads/master@{#357345}

--- net/spdy/spdy_framer_test.cc
+++ net/spdy/spdy_framer_test.cc
@@ -100,6 +100,16 @@ class SpdyFramerTestUtil {
       LOG(FATAL);
     }
 
+    SpdyHeadersHandlerInterface* OnHeaderFrameStart(
+        SpdyStreamId stream_id) override {
+      LOG(FATAL);
+      return nullptr;
+    }
+
+    void OnHeaderFrameEnd(SpdyStreamId stream_id, bool end_headers) override {
+      LOG(FATAL);
+    }
+
     bool OnControlFrameHeaderData(SpdyStreamId stream_id,
                                   const char* header_data,
                                   size_t len) override {
@@ -318,6 +328,16 @@ class TestSpdyVisitor : public SpdyFramerVisitorInterface,
     EXPECT_EQ(header_stream_id_, stream_id);
     data_bytes_ += len;
     LOG(INFO) << "OnStreamPadding(" << stream_id << ", " << len << ")\n";
+  }
+
+  SpdyHeadersHandlerInterface* OnHeaderFrameStart(
+      SpdyStreamId stream_id) override {
+    LOG(FATAL);
+    return nullptr;
+  }
+
+  void OnHeaderFrameEnd(SpdyStreamId stream_id, bool end_headers) override {
+    LOG(FATAL);
   }
 
   bool OnControlFrameHeaderData(SpdyStreamId stream_id,
