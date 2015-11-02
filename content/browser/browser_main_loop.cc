835586bbc2d378484a595ead6d069498b80c7bb7
asvitkine <asvitkine@chromium.org>
2015-11-02 13:52:08 -0800

    Register a default instance of FeatureList for content browser.
    Normally, this is done in chrome_browser_main.cc in Chrome, but
    given that we can't expect the embedder to necessarily initialize
    the FeatureList, provide a default one if the embedder didn't.
    
    This fixes an error when running Content Shell encountered by CLs
    starting to use FeatureList, which didn't initialize the FeatureList
    prior to this CL.
    
    BUG=548289
    
    Review URL: https://codereview.chromium.org/1409933007
    
    Cr-Commit-Position: refs/heads/master@{#357437}

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/memory/memory_pressure_monitor.h"
@@ -692,6 +693,10 @@ int BrowserMainLoop::PreCreateThreads() {
 
     result_code_ = parts_->PreCreateThreads();
   }
+
+  // Initialize an instance of FeatureList. This will be a no-op if an instance
+  // was already set up by the embedder.
+  base::FeatureList::InitializeInstance();
 
   // TODO(chrisha): Abstract away this construction mess to a helper function,
   // once MemoryPressureMonitor is made a concrete class.
