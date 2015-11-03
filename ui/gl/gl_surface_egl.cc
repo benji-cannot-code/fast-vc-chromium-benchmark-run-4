974d4b3c3aa191cdff14c505ea3394d2ad3edef7
jbauman <jbauman@chromium.org>
2015-11-02 23:45:31 -0800

    Add UMA histogram to determine whether EGL display is created with D3D9 or 11
    Review URL: https://codereview.chromium.org/1410053005
    
    Cr-Commit-Position: refs/heads/master@{#357536}

--- ui/gl/gl_surface_egl.cc
+++ ui/gl/gl_surface_egl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop/message_loop.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "ui/gfx/geometry/rect.h"
@@ -436,6 +437,8 @@ EGLDisplay GLSurfaceEGL::InitializeDisplay() {
                  << " failed with error " << GetLastEGLErrorString()
                  << (is_last ? "" : ", trying next display type");
     } else {
+      UMA_HISTOGRAM_ENUMERATION("GPU.EGLDisplay", display_type,
+                                DISPLAY_TYPE_MAX);
       g_display = display;
       break;
     }
