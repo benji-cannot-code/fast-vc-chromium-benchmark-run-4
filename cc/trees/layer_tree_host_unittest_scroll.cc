c9c38525210793d8bab40035ae4259d65bea56d8
enne <enne@chromium.org>
2015-11-03 14:20:20 -0800

    Revert of Clamp clip layer to outer viewport size for scrollbars. (patchset #2 id:20001 of https://codereview.chromium.org/1414573007/ )
    Reason for revert:
    Causing crashes in layout tests
    
    Original issue's description:
    > Clamp clip layer to outer viewport size for scrollbars.
    >
    > When there is a nonoverlay scrollbar, the inner viewport is larger than
    > the outer by the size of the scrollbar.  The outer viewport can still be
    > free to scroll within the content area, so we should take the minimum of
    > both sizes.
    >
    > BUG=547668
    > CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    >
    > Committed: https://crrev.com/2b363bf9893e193fbce9ed93821c8fcdb961668b
    > Cr-Commit-Position: refs/heads/master@{#357613}
    
    TBR=bokan@chromium.org,aelias@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=547668
    
    Review URL: https://codereview.chromium.org/1426273004
    
    Cr-Commit-Position: refs/heads/master@{#357650}

--- cc/trees/layer_tree_host_unittest_scroll.cc
+++ cc/trees/layer_tree_host_unittest_scroll.cc
@@ -1035,7 +1035,8 @@ class LayerTreeHostScrollTestScrollZeroMaxScrollOffset
   void AfterTest() override {}
 };
 
-MULTI_THREAD_TEST_F(LayerTreeHostScrollTestScrollZeroMaxScrollOffset);
+SINGLE_AND_MULTI_THREAD_TEST_F(
+    LayerTreeHostScrollTestScrollZeroMaxScrollOffset);
 
 class ThreadCheckingInputHandlerClient : public InputHandlerClient {
  public:
