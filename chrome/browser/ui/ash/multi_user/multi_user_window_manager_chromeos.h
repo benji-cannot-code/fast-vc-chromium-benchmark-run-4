bfe699096f267ac9a316ca5e801bfe7fe3b42855
alemate <alemate@chromium.org>
2015-11-03 14:56:57 -0800

    MultiUserWindowManagerChromeOS should not crash when keyed service is destroyed.
    MultiUserWindowManagerChromeOS is using SigninKeyedService, and should
    be notified when this KeyedService is destroyed.
    
    BUG=532535
    TEST=manual
    
    Review URL: https://codereview.chromium.org/1407903006
    
    Cr-Commit-Position: refs/heads/master@{#357665}

--- chrome/browser/ui/ash/multi_user/multi_user_window_manager_chromeos.h
+++ chrome/browser/ui/ash/multi_user/multi_user_window_manager_chromeos.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Browser;
 class MultiUserNotificationBlockerChromeOS;
 class MultiUserNotificationBlockerChromeOSTest;
+class Profile;
 
 namespace content {
 class BrowserContext;
@@ -226,6 +227,12 @@ class MultiUserWindowManagerChromeOS
   // Get the animation time in milliseconds dependent on the |AnimationSpeed|
   // from the passed |default_time_in_ms|.
   int GetAdjustedAnimationTimeInMS(int default_time_in_ms) const;
+
+  // This is called when KeyedService (for |iser_id| and |profile|) is
+  // destroyed, or when MultiUserWindowManagerChromeOS is destroyed.
+  // This happens on shutdown, before profile prefs are stored to
+  // disk.
+  void RemoveUser(const std::string& user_id, Profile* profile);
 
   // A lookup to see to which user the given window belongs to, where and if it
   // should get shown.
