040def56e4a35b023414243733312ad311df4a9e
lazyboy <lazyboy@chromium.org>
2015-11-03 01:04:52 -0800

    Correctly (hopefully) check if <webview> is running inside WebUI.
    A chrome signin tabs's WebContents->GetWebUI() returns non NULL [1]
    Other tab's WebContents->GetWebUI() returns NULL [2]
    An extension's WebContents->GetWebUI() always returns non NULL [3]
    
    Previously, the code was written with incorrect assumption that [3]
    will return NULL. Therefore, we used to just check WebContents::GetWebUI()
    to determine whether a webview is running inside a chrome signin page
    webui or not.
    The intention is to hide "Inspect Element" for chrome signin page.
    However, because of the flaw described above, we incorrectly also
    removed the element from chrome apps. This CL changes the condition to
    first check whether the webview is running inside an extension/app before
    performing the GetWebUI() check.
    
    TBR=fsamuel@chromium.org for context_menu_content_type_web_view.cc
    BUG=474251
    Test=In a chrome app, load a <webview>. Right-click context menu
    should show "Inspect" if you're running tott/canary.
    In chrome://chrome-signin page,
    
    Review URL: https://codereview.chromium.org/1390043003
    
    Cr-Commit-Position: refs/heads/master@{#357538}

--- chrome/browser/ui/webui/webui_webview_browsertest.cc
+++ chrome/browser/ui/webui/webui_webview_browsertest.cc
@@ -5,12 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/path_service.h"
+#include "chrome/app/chrome_command_ids.h"
+#include "chrome/browser/renderer_context_menu/render_view_context_menu_test_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "chrome/test/base/web_ui_browser_test.h"
+#include "content/public/common/context_menu_params.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 
 class WebUIWebViewBrowserTest : public WebUIBrowserTest {
@@ -163,3 +166,12 @@ IN_PROC_BROWSER_TEST_F(WebUIWebViewBrowserTest, AddContentScriptIncognito) {
       new base::StringValue(GetTestUrl("empty.html").spec())));
 }
 #endif
+
+IN_PROC_BROWSER_TEST_F(WebUIWebViewBrowserTest, ContextMenuInspectElement) {
+  ui_test_utils::NavigateToURL(browser(), GetWebViewEnabledWebUIURL());
+  content::ContextMenuParams params;
+  TestRenderViewContextMenu menu(
+      browser()->tab_strip_model()->GetActiveWebContents()->GetMainFrame(),
+      params);
+  EXPECT_FALSE(menu.IsItemPresent(IDC_CONTENT_CONTEXT_INSPECTELEMENT));
+}
