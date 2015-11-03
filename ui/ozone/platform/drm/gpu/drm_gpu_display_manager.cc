992349440d85fe6e910577a1d41af34bf60b5f2d
oshima <oshima@chromium.org>
2015-11-03 12:03:29 -0800

    Use persistent index to generate display id
    BUG=549743,547280
    R=dnicoara@chromium.org
    
    Review URL: https://codereview.chromium.org/1421433006
    
    Cr-Commit-Position: refs/heads/master@{#357596}

--- ui/ozone/platform/drm/gpu/drm_gpu_display_manager.cc
+++ ui/ozone/platform/drm/gpu/drm_gpu_display_manager.cc
@@ -71,8 +71,7 @@ std::vector<DisplaySnapshot_Params> DrmGpuDisplayManager::GetDisplays() {
   std::vector<DisplaySnapshot_Params> params_list;
 
   const DrmDeviceVector& devices = drm_device_manager_->GetDrmDevices();
-  // Unique identifier used to create the display id.
-  size_t index = 0;
+  size_t device_index = 0;
   for (const auto& drm : devices) {
     ScopedVector<HardwareDisplayControllerInfo> display_infos =
         GetAvailableDisplayControllerInfos(drm->get_fd());
@@ -87,9 +86,10 @@ std::vector<DisplaySnapshot_Params> DrmGpuDisplayManager::GetDisplays() {
       } else {
         displays_.push_back(new DrmDisplay(screen_manager_, drm));
       }
-
-      params_list.push_back(displays_.back()->Update(display_info, index++));
+      params_list.push_back(
+          displays_.back()->Update(display_info, device_index));
     }
+    device_index++;
   }
 
   NotifyScreenManager(displays_.get(), old_displays.get());
