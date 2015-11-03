84544ab3b06f500f7b2f5db129e51edceb5eab5a
ccameron <ccameron@chromium.org>
2015-11-02 16:50:38 -0800

    Mac: Pass correct vsync parameters in software mode
    Make the Mac software output device correctly pick up the vsync
    parameters from the RWHVMac (via the AcceleratedWidgetMac).
    
    Fix a bug in AcceleratedWidgetMac where we failed to ack swaps.
    
    BUG=546691
    
    Review URL: https://codereview.chromium.org/1412473003
    
    Cr-Commit-Position: refs/heads/master@{#357494}

--- content/browser/compositor/software_output_device_mac.h
+++ content/browser/compositor/software_output_device_mac.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_cftyperef.h"
 #include "cc/output/software_output_device.h"
 #include "third_party/skia/include/core/SkRegion.h"
+#include "ui/gfx/vsync_provider.h"
 
 namespace gfx {
 class Canvas;
@@ -22,16 +23,24 @@ class Compositor;
 
 namespace content {
 
-class SoftwareOutputDeviceMac : public cc::SoftwareOutputDevice {
+class SoftwareOutputDeviceMac :
+    public cc::SoftwareOutputDevice,
+    public gfx::VSyncProvider {
  public:
   explicit SoftwareOutputDeviceMac(ui::Compositor* compositor);
   ~SoftwareOutputDeviceMac() override;
 
+  // cc::SoftwareOutputDevice implementation.
   void Resize(const gfx::Size& pixel_size, float scale_factor) override;
   SkCanvas* BeginPaint(const gfx::Rect& damage_rect) override;
   void EndPaint() override;
   void DiscardBackbuffer() override;
   void EnsureBackbuffer() override;
+  gfx::VSyncProvider* GetVSyncProvider() override;
+
+  // gfx::VSyncProvider implementation.
+  void GetVSyncParameters(
+      const gfx::VSyncProvider::UpdateVSyncCallback& callback) override;
 
  private:
   bool EnsureBuffersExist();
@@ -55,6 +64,8 @@ class SoftwareOutputDeviceMac : public cc::SoftwareOutputDevice {
   // The SkCanvas wrapps the mapped current IOSurface. It is valid only between
   // BeginPaint and EndPaint.
   skia::RefPtr<SkCanvas> canvas_;
+
+  gfx::VSyncProvider::UpdateVSyncCallback update_vsync_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(SoftwareOutputDeviceMac);
 };
