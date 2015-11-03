4692c39eeb68786d809e988f75b9b0bc2070e3a8
danakj <danakj@chromium.org>
2015-11-03 15:14:25 -0800

    cc: Add SetNeedsOneBeginImplFrame to the Scheduler
    This requests another BeginImplFrame to happen. Once
    the requests for new ones stop coming, the scheduler
    will put the begin frame source to sleep.
    
    This will be used to replace SetNeedsAnimate to
    request new frames while an animation wants to tick.
    
    BUG=522658
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1428133003
    
    Cr-Commit-Position: refs/heads/master@{#357667}

--- cc/scheduler/scheduler_state_machine.h
+++ cc/scheduler/scheduler_state_machine.h
@@ -208,6 +208,10 @@ class CC_EXPORT SchedulerStateMachine {
   void SetNeedsBeginMainFrame();
   bool needs_begin_main_frame() const { return needs_begin_main_frame_; }
 
+  // Requests a single impl frame (after the current frame if there is one
+  // active).
+  void SetNeedsOneBeginImplFrame();
+
   // Call this only in response to receiving an ACTION_SEND_BEGIN_MAIN_FRAME
   // from NextAction.
   // Indicates that all painting is complete.
@@ -322,6 +326,7 @@ class CC_EXPORT SchedulerStateMachine {
   bool needs_animate_;
   bool needs_prepare_tiles_;
   bool needs_begin_main_frame_;
+  bool needs_one_begin_impl_frame_;
   bool visible_;
   bool resourceless_draw_;
   bool can_draw_;
