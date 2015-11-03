6adff80dea2eceb55e5581d3f563268c77063d10
dgozman <dgozman@chromium.org>
2015-11-02 16:56:19 -0800

    [DevTools] Only listen to main target control events in network log.
    These include MainFrameNavigated, Load, DOMContentLoaded.
    
    BUG=536617
    
    Review URL: https://codereview.chromium.org/1409263005
    
    Cr-Commit-Position: refs/heads/master@{#357497}

--- third_party/WebKit/Source/devtools/front_end/network/NetworkLogView.js
+++ third_party/WebKit/Source/devtools/front_end/network/NetworkLogView.js
@@ -105,10 +105,6 @@ WebInspector.NetworkLogView = function(filterBar, progressBarContainer, networkL
     WebInspector.targetManager.addModelListener(WebInspector.NetworkManager, WebInspector.NetworkManager.EventTypes.RequestStarted, this._onRequestStarted, this);
     WebInspector.targetManager.addModelListener(WebInspector.NetworkManager, WebInspector.NetworkManager.EventTypes.RequestUpdated, this._onRequestUpdated, this);
     WebInspector.targetManager.addModelListener(WebInspector.NetworkManager, WebInspector.NetworkManager.EventTypes.RequestFinished, this._onRequestUpdated, this);
-
-    WebInspector.targetManager.addModelListener(WebInspector.ResourceTreeModel, WebInspector.ResourceTreeModel.EventTypes.MainFrameNavigated, this._mainFrameNavigated, this);
-    WebInspector.targetManager.addModelListener(WebInspector.ResourceTreeModel, WebInspector.ResourceTreeModel.EventTypes.Load, this._loadEventFired, this);
-    WebInspector.targetManager.addModelListener(WebInspector.ResourceTreeModel, WebInspector.ResourceTreeModel.EventTypes.DOMContentLoaded, this._domContentLoadedEventFired, this);
 }
 
 WebInspector.NetworkLogView._isFilteredOutSymbol = Symbol("isFilteredOut");
@@ -212,6 +208,11 @@ WebInspector.NetworkLogView.prototype = {
      */
     targetAdded: function(target)
     {
+        if (!target.parentTarget()) {
+            target.resourceTreeModel.addEventListener(WebInspector.ResourceTreeModel.EventTypes.MainFrameNavigated, this._mainFrameNavigated, this);
+            target.resourceTreeModel.addEventListener(WebInspector.ResourceTreeModel.EventTypes.Load, this._loadEventFired, this);
+            target.resourceTreeModel.addEventListener(WebInspector.ResourceTreeModel.EventTypes.DOMContentLoaded, this._domContentLoadedEventFired, this);
+        }
         target.networkLog.requests().forEach(this._appendRequest.bind(this));
     },
 
@@ -221,6 +222,11 @@ WebInspector.NetworkLogView.prototype = {
      */
     targetRemoved: function(target)
     {
+        if (!target.parentTarget()) {
+            target.resourceTreeModel.removeEventListener(WebInspector.ResourceTreeModel.EventTypes.MainFrameNavigated, this._mainFrameNavigated, this);
+            target.resourceTreeModel.removeEventListener(WebInspector.ResourceTreeModel.EventTypes.Load, this._loadEventFired, this);
+            target.resourceTreeModel.removeEventListener(WebInspector.ResourceTreeModel.EventTypes.DOMContentLoaded, this._domContentLoadedEventFired, this);
+        }
     },
 
     /**
