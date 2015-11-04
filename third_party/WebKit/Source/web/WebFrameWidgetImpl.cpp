f70128381500eee7211e1f3615bcaadd2518b4df
wangxianzhu <wangxianzhu@chromium.org>
2015-11-04 12:13:46 -0800

    Change two callers of PageWidgetDelegete::layout() to updateAllLifecyclePhases()
    This is a follow-up of https://codereview.chromium.org/1425373002/
    which changed PageWidgetDelegate::layout() to layout-only, and added
    PageWidgetDelegate::updateAllLifecyclePhases() doing what the previous
    layout() did. I should have updated previous callers (other than
    WebViewImpl) of layout() to use updateAllLifecyclePhases() to keep
    the original behavior. This CL does this.
    
    BTW, rename PageWidgetDelegate::layout() and
    PageAnimator::updateStyleAndLayoutForPainting()
    to updateLifecycleToCompositingCleanPlusScrolling().
    
    BUG=536999
    TEST=fast/forms/* (popups etc. were broken in sync painting mode)
    
    Review URL: https://codereview.chromium.org/1408343009
    
    Cr-Commit-Position: refs/heads/master@{#357881}

--- third_party/WebKit/Source/web/WebFrameWidgetImpl.cpp
+++ third_party/WebKit/Source/web/WebFrameWidgetImpl.cpp
@@ -259,7 +259,7 @@ void WebFrameWidgetImpl::updateAllLifecyclePhases()
     if (!m_localRoot)
         return;
 
-    PageWidgetDelegate::layout(*page(), *m_localRoot->frame());
+    PageWidgetDelegate::updateAllLifecyclePhases(*page(), *m_localRoot->frame());
     updateLayerTreeBackgroundColor();
 }
 
