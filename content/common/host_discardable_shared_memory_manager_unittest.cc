d6b7b2b323efdd357bd807cfcc11178ec5b0924a
reveman <reveman@chromium.org>
2015-11-03 22:35:28 -0800

    content: Move HostDiscardableSharedMemoryManager::current() to PostMainMessageLoopStart.
    This fixes the problem where HostDiscardableSharedMemoryManager would be
    initialized without a message loop current and prevents this from regressing
    in the future by requiring test code to also have a message loop.
    
    BUG=549764
    TEST=content_unittests --gtest_filter=HostDiscardableSharedMemoryManagerTest.*
    
    Review URL: https://codereview.chromium.org/1414573010
    
    Cr-Commit-Position: refs/heads/master@{#357773}

--- content/common/host_discardable_shared_memory_manager_unittest.cc
+++ content/common/host_discardable_shared_memory_manager_unittest.cc
@@ -61,6 +61,8 @@ class HostDiscardableSharedMemoryManagerTest : public testing::Test {
     manager_.reset(new TestHostDiscardableSharedMemoryManager);
   }
 
+  // HostDiscardableSharedMemoryManager requires a message loop.
+  base::MessageLoop message_loop_;
   scoped_ptr<TestHostDiscardableSharedMemoryManager> manager_;
 };
 
@@ -242,7 +244,7 @@ class HostDiscardableSharedMemoryManagerScheduleEnforceMemoryPolicyTest
     manager_.reset(new HostDiscardableSharedMemoryManager);
   }
 
-  // This test requires a message loop.
+  // HostDiscardableSharedMemoryManager requires a message loop.
   base::MessageLoop message_loop_;
   scoped_ptr<HostDiscardableSharedMemoryManager> manager_;
 };
