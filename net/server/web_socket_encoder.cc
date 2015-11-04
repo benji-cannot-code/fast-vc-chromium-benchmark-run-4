cd4b3e98b6c772c03234d19cc6ab2e05112127df
olli.raula <olli.raula@intel.com>
2015-11-04 00:45:52 -0800

    Make WebSocketEncoder::CreateClient return scoped_ptr
    Make WebSocketEncoder::CreateClient return scoped_ptr
    increase pointer safety and because all consumers(only in unittests) are using scoped_ptr already.
    
    Review URL: https://codereview.chromium.org/1425593005
    
    Cr-Commit-Position: refs/heads/master@{#357782}

--- net/server/web_socket_encoder.cc
+++ net/server/web_socket_encoder.cc
@@ -236,7 +236,7 @@ scoped_ptr<WebSocketEncoder> WebSocketEncoder::CreateServer(
 }
 
 // static
-WebSocketEncoder* WebSocketEncoder::CreateClient(
+scoped_ptr<WebSocketEncoder> WebSocketEncoder::CreateClient(
     const std::string& response_extensions) {
   // TODO(yhirano): Add a way to return an error.
 
@@ -247,12 +247,12 @@ WebSocketEncoder* WebSocketEncoder::CreateClient(
     // 2) There is a malformed Sec-WebSocketExtensions header.
     // We should return a deflate-disabled encoder for the former case and
     // fail the connection for the latter case.
-    return new WebSocketEncoder(FOR_CLIENT, nullptr, nullptr);
+    return make_scoped_ptr(new WebSocketEncoder(FOR_CLIENT, nullptr, nullptr));
   }
   if (parser.extensions().size() != 1) {
     // Only permessage-deflate extension is supported.
     // TODO (yhirano): Fail the connection.
-    return new WebSocketEncoder(FOR_CLIENT, nullptr, nullptr);
+    return make_scoped_ptr(new WebSocketEncoder(FOR_CLIENT, nullptr, nullptr));
   }
   const auto& extension = parser.extensions()[0];
   WebSocketDeflateParameters params;
@@ -260,7 +260,7 @@ WebSocketEncoder* WebSocketEncoder::CreateClient(
   if (!params.Initialize(extension, &failure_message) ||
       !params.IsValidAsResponse(&failure_message)) {
     // TODO (yhirano): Fail the connection.
-    return new WebSocketEncoder(FOR_CLIENT, nullptr, nullptr);
+    return make_scoped_ptr(new WebSocketEncoder(FOR_CLIENT, nullptr, nullptr));
   }
 
   auto deflater = make_scoped_ptr(
@@ -270,10 +270,11 @@ WebSocketEncoder* WebSocketEncoder::CreateClient(
   if (!deflater->Initialize(params.PermissiveClientMaxWindowBits()) ||
       !inflater->Initialize(params.PermissiveServerMaxWindowBits())) {
     // TODO (yhirano): Fail the connection.
-    return new WebSocketEncoder(FOR_CLIENT, nullptr, nullptr);
+    return make_scoped_ptr(new WebSocketEncoder(FOR_CLIENT, nullptr, nullptr));
   }
 
-  return new WebSocketEncoder(FOR_CLIENT, deflater.Pass(), inflater.Pass());
+  return make_scoped_ptr(
+      new WebSocketEncoder(FOR_CLIENT, deflater.Pass(), inflater.Pass()));
 }
 
 WebSocketEncoder::WebSocketEncoder(Type type,
