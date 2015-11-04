fd07d353231da69d68693cc78583c4df56526aa5
xhwang <xhwang@chromium.org>
2015-11-03 22:21:55 -0800

    media: Allows GetCdmContext to return null.
    CdmContext is an interface representing the context that a media pipeline needs
    from a content decryption module (CDM) to decrypt (and decode) encrypted
    buffers.
    
    Currently it supports Decryptor and CDM ID. The former is used on desktop
    chromium to support decrypting (and decoding) encrypted buffers by the CDM
    directly. The latter is used to locate a CDM running out-of-process.
    
    These are the only two cases how a media pipeline uses a CDM in the render
    process, where this model works pretty well. This is why currently we require
    that a MediaKeys must return a non-null CdmContext.
    
    However, now MediaKeys becomes a generic interface representing the CDM in
    Chromium. It is used not only in the render process, but can also be used in
    other processes such as browser, utility and gpu. In these cases the CDM is
    usually backed by a platform specific CDM module, which doesn't necessarily
    support the Decryptor interface. For example, we use MediaCrypto on Android, and
    use DecryptContext on Cast.
    
    This CL updates MediaKeys::GetCdmContext() to allow it to return null, which
    makes it more generic. We still assume all CDMs used in the render process to
    support a CdmContext. We may loose this restriction in the future if needed.
    
    BUG=511040
    TEST=No functionality change.
    
    Review URL: https://codereview.chromium.org/1416303004
    
    Cr-Commit-Position: refs/heads/master@{#357770}

--- media/base/media_keys.cc
+++ media/base/media_keys.cc
@@ -11,6 +11,10 @@ MediaKeys::MediaKeys() {}
 
 MediaKeys::~MediaKeys() {}
 
+CdmContext* MediaKeys::GetCdmContext() {
+  return nullptr;
+}
+
 void MediaKeys::DeleteOnCorrectThread() const {
   delete this;
 }
