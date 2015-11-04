97901b72cd61ca18edc8c41ff5f0b0b492f63e4d
fukino <fukino@chromium.org>
2015-11-03 23:35:28 -0800

    Read Polymer elements' properties directly in closure compilation of Files/AudioPlayer.
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1416373006
    
    Cr-Commit-Position: refs/heads/master@{#357777}

--- ui/file_manager/file_manager/foreground/elements/files_toast.js
+++ ui/file_manager/file_manager/foreground/elements/files_toast.js
@@ -32,7 +32,7 @@ var FilesToast = Polymer({
    */
   created: function() {
     /**
-     * @private {{text: string, callback: function()}}
+     * @private {?{text: string, callback: function()}}
      */
     this.action_ = null;
 
