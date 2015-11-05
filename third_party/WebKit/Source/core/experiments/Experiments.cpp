08ebae14d5197692e95e9886d5b66fbe3342aff6
chasej <chasej@chromium.org>
2015-11-04 17:10:37 -0800

    Add runtime enabled feature for Experimental Framework
    The EF will likely be used with APIs that start out
    behind the experimental-web-platform-features flag.
    Until the framework is further along, it should be
    possible to enable/disable the framework independently
    from APIs that are using it.
    
    In subsequent CL(s), it will be possible for an API to
    integrate with the framework, but still be functional
    even if the framework is disabled.
    
    For now, the runtime enabled feature is intentionally off by default.
    
    BUG=543141
    
    Review URL: https://codereview.chromium.org/1429103003
    
    Cr-Commit-Position: refs/heads/master@{#357966}

--- third_party/WebKit/Source/core/experiments/Experiments.cpp
+++ third_party/WebKit/Source/core/experiments/Experiments.cpp
@@ -7,11 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/experiments/Experiments.h"
 
 #include "core/dom/ExceptionCode.h"
+#include "platform/RuntimeEnabledFeatures.h"
 
 namespace blink {
 
 bool Experiments::isApiEnabled(ExecutionContext* executionContext, const String& apiName)
 {
+    if (!RuntimeEnabledFeatures::experimentalFrameworkEnabled()) {
+        return false;
+    }
     return false;
 }
 
