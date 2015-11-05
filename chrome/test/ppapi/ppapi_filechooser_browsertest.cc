327f83a35d06b8ad2da127df464016b362635102
krasin <krasin@google.com>
2015-11-05 13:50:26 -0800

    Fix invalid cast in SafeBrowsingService::GetProtocolManagerDelegate.
    FakeDatabaseManager does not implement LocalSafeBrowsingDatabaseManager,
    and thus TestSafeBrowsingService must return NULL.
    
    This bug was found by Control Flow Integrity check:
    https://sites.google.com/a/chromium.org/dev/developers/testing/control-flow-integrity
    
    It has broken the CFI buildbot:
    http://build.chromium.org/p/chromium.fyi/builders/CFI%20Linux
    BUG=chromium:551611
    
    Review URL: https://codereview.chromium.org/1422803004
    
    Cr-Commit-Position: refs/heads/master@{#358140}

--- chrome/test/ppapi/ppapi_filechooser_browsertest.cc
+++ chrome/test/ppapi/ppapi_filechooser_browsertest.cc
@@ -144,6 +144,11 @@ class TestSafeBrowsingService : public SafeBrowsingService {
 
  protected:
   ~TestSafeBrowsingService() override {}
+
+  SafeBrowsingProtocolManagerDelegate* GetProtocolManagerDelegate() override {
+    // Our FakeDatabaseManager doesn't implement this delegate.
+    return NULL;
+  }
 };
 
 class TestSafeBrowsingServiceFactory : public SafeBrowsingServiceFactory {
