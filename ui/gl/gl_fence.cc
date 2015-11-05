418b39d133c7f2fa44421c28bb7105dc8723096f
ccameron <ccameron@chromium.org>
2015-11-05 13:36:08 -0800

    Fix GLFence when using core profile
    BUG=551483
    
    Review URL: https://codereview.chromium.org/1418423011
    
    Cr-Commit-Position: refs/heads/master@{#358135}

--- ui/gl/gl_fence.cc
+++ ui/gl/gl_fence.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_fence_egl.h"
 #include "ui/gl/gl_fence_nv.h"
 #include "ui/gl/gl_gl_api_implementation.h"
+#include "ui/gl/gl_implementation.h"
 #include "ui/gl/gl_version_info.h"
 
 #if defined(OS_MACOSX)
@@ -29,6 +30,7 @@ GLFence::~GLFence() {
 bool GLFence::IsSupported() {
   DCHECK(GetGLVersionInfo());
   return g_driver_gl.ext.b_GL_ARB_sync || GetGLVersionInfo()->is_es3 ||
+         GetGLImplementation() == kGLImplementationDesktopGLCoreProfile ||
 #if defined(OS_MACOSX)
          g_driver_gl.ext.b_GL_APPLE_fence ||
 #else
@@ -44,7 +46,8 @@ GLFence* GLFence::Create() {
   scoped_ptr<GLFence> fence;
   // Prefer ARB_sync which supports server-side wait.
   if (g_driver_gl.ext.b_GL_ARB_sync ||
-      GetGLVersionInfo()->is_es3) {
+      GetGLVersionInfo()->is_es3 ||
+      GetGLImplementation() == kGLImplementationDesktopGLCoreProfile) {
     fence.reset(new GLFenceARB);
 #if defined(OS_MACOSX)
   } else if (g_driver_gl.ext.b_GL_APPLE_fence) {
