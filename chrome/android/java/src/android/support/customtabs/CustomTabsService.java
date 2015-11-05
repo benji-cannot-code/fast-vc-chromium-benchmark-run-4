dfa4b59fc101860ce017c5f4144a473e729e7ea2
lizeb <lizeb@chromium.org>
2015-11-05 05:06:18 -0800

    customtabs: Add a "low confidence" version of mayLaunchUrl().
    A client application can call mayLaunchUrl() with a null or empty
    primary URL, and a list of other URLs to express a low confidence about
    the destination. This is used here to avoid triggering a prerender.
    
    Also remove the call to
    WarmupManager#maybePrefetchDnsForUrlInBackground() from mayLaunchUrl()
    as it is unnecessary. mayLaunchUrl() is indeed always called with the
    native library loaded, and the native preconnection code needs to issue
    a DNS query first. Doing it from Java is thus useless.
    
    BUG=549514
    
    Review URL: https://codereview.chromium.org/1427953003
    
    Cr-Commit-Position: refs/heads/master@{#358041}

---
+++ chrome/android/java/src/android/support/customtabs/CustomTabsService.java
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package android.support.customtabs;
+
+/**
+ * Partial mirror of the matching support library class. To be nuked.
+ */
+public class CustomTabsService {
+    public static final String KEY_URL = "android.support.customtabs.otherurls.URL";
+}
