c7f24c3b357d1b58e8db289f515c5f93d53030dd
michaeln <michaeln@chromium.org>
2015-11-05 13:07:21 -0800

    Revert of Parse outline shorthand in CSSPropertyParser with CSSParserTokens (patchset #5 id:80001 of https://codereview.chromium.org/1406343008/ )
    Reason for revert:
    Reverting in order to revert a suspected top crasher. See the bug for details.
    
    BUG=551638
    
    Original issue's description:
    > Parse outline shorthand in CSSPropertyParser with CSSParserTokens
    >
    > Move outline shorthand property handling from
    > LegacyCSSPropertyParser into CSSPropertyParser. Also
    > move its related longhands.
    >
    > BUG=499780
    >
    > Committed: https://crrev.com/3447a99f8dce411d4e07c55e6b81dd2068a50390
    > Cr-Commit-Position: refs/heads/master@{#358065}
    
    TBR=timloh@chromium.org,rob.buis@samsung.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=499780
    
    Review URL: https://codereview.chromium.org/1419143008
    
    Cr-Commit-Position: refs/heads/master@{#358127}

--- third_party/WebKit/Source/core/css/parser/LegacyCSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/LegacyCSSPropertyParser.cpp
@@ -421,6 +421,14 @@ bool CSSPropertyParser::parseValue(CSSPropertyID unresolvedProperty, bool import
         }
         break;
 
+    case CSSPropertyOutlineColor:        // <color> | invert | inherit
+        // Outline color has "invert" as additional keyword.
+        // Also, we want to allow the special focus color even in HTML Standard parsing mode.
+        if (id == CSSValueInvert || id == CSSValueWebkitFocusRingColor) {
+            validPrimitive = true;
+            break;
+        }
+        /* nobreak */
     case CSSPropertyBackgroundColor: // <color> | inherit
     case CSSPropertyBorderTopColor: // <color> | inherit
     case CSSPropertyBorderRightColor:
@@ -602,6 +610,7 @@ bool CSSPropertyParser::parseValue(CSSPropertyID unresolvedProperty, bool import
             unitless = FUnitlessQuirk;
         // fall through
     case CSSPropertyWebkitTextStrokeWidth:
+    case CSSPropertyOutlineWidth: // <border-width> | inherit
     case CSSPropertyWebkitBorderStartWidth:
     case CSSPropertyWebkitBorderEndWidth:
     case CSSPropertyWebkitBorderBeforeWidth:
@@ -742,6 +751,9 @@ bool CSSPropertyParser::parseValue(CSSPropertyID unresolvedProperty, bool import
     case CSSPropertyBorderRadius:
     case CSSPropertyAliasWebkitBorderRadius:
         return parseBorderRadius(unresolvedProperty, important);
+    case CSSPropertyOutlineOffset:
+        validPrimitive = validUnit(value, FLength);
+        break;
     case CSSPropertyWebkitBoxReflect:
         if (id == CSSValueNone)
             validPrimitive = true;
@@ -1032,6 +1044,9 @@ bool CSSPropertyParser::parseValue(CSSPropertyID unresolvedProperty, bool import
         return parseShorthand(propId, webkitBorderBeforeShorthand(), important);
     case CSSPropertyWebkitBorderAfter:
         return parseShorthand(propId, webkitBorderAfterShorthand(), important);
+    case CSSPropertyOutline:
+        // [ 'outline-color' || 'outline-style' || 'outline-width' ] | inherit
+        return parseShorthand(propId, outlineShorthand(), important);
     case CSSPropertyBorderColor:
         // <color>{1,4} | inherit
         return parse4Values(propId, borderColorShorthand().properties(), important);
@@ -1196,10 +1211,6 @@ bool CSSPropertyParser::parseValue(CSSPropertyID unresolvedProperty, bool import
     case CSSPropertyWebkitTextDecorationsInEffect:
     case CSSPropertyTextDecorationLine:
     case CSSPropertyTextDecoration:
-    case CSSPropertyOutline:
-    case CSSPropertyOutlineColor:
-    case CSSPropertyOutlineWidth:
-    case CSSPropertyOutlineOffset:
         validPrimitive = false;
         break;
 
