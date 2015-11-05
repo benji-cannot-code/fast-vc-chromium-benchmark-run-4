ffd1531874d722f92e69fd0b367e9e7a977cdbcf
alancutter <alancutter@chromium.org>
2015-11-04 17:36:06 -0800

    Fix CSSValueInterpolationTypes leak in applicableTypesForProperty()
    This change fixes a (tiny) leak of CSSValueInterpolationTypes and changes
    the storage of singleton InterpolationTypes to be memory managed OwnPtrs
    instead of raw pointers.
    This patch introduces no change in behaviour in binaries other than LSAN.
    
    BUG=550369
    
    Review URL: https://codereview.chromium.org/1419793005
    
    Cr-Commit-Position: refs/heads/master@{#357972}

--- third_party/WebKit/Source/core/animation/InvalidatableInterpolation.h
+++ third_party/WebKit/Source/core/animation/InvalidatableInterpolation.h
@@ -13,16 +13,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+using InterpolationTypes = Vector<OwnPtr<const InterpolationType>>;
+
 // TODO(alancutter): This class will replace *StyleInterpolation, SVGInterpolation, Interpolation.
 // For now it needs to distinguish itself during the refactor and temporarily has an ugly name.
 class CORE_EXPORT InvalidatableInterpolation : public Interpolation {
 public:
     static PassRefPtr<InvalidatableInterpolation> create(
-        const Vector<const InterpolationType*>& InterpolationTypes,
+        const InterpolationTypes& interpolationTypes,
         const PropertySpecificKeyframe& startKeyframe,
         const PropertySpecificKeyframe& endKeyframe)
     {
-        return adoptRef(new InvalidatableInterpolation(InterpolationTypes, startKeyframe, endKeyframe));
+        return adoptRef(new InvalidatableInterpolation(interpolationTypes, startKeyframe, endKeyframe));
     }
 
     PropertyHandle property() const final { return m_interpolationTypes.first()->property(); }
@@ -35,7 +37,7 @@ public:
 
 private:
     InvalidatableInterpolation(
-        const Vector<const InterpolationType*>& interpolationTypes,
+        const InterpolationTypes& interpolationTypes,
         const PropertySpecificKeyframe& startKeyframe,
         const PropertySpecificKeyframe& endKeyframe)
         : Interpolation(nullptr, nullptr)
@@ -56,7 +58,7 @@ private:
     void setFlagIfInheritUsed(InterpolationEnvironment&) const;
     double underlyingFraction() const;
 
-    const Vector<const InterpolationType*>& m_interpolationTypes;
+    const InterpolationTypes& m_interpolationTypes;
     const PropertySpecificKeyframe* m_startKeyframe;
     const PropertySpecificKeyframe* m_endKeyframe;
     double m_currentFraction;
