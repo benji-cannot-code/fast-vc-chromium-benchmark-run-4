76f6df8ecbc503d632539b935d09ac9b4c6601b7
mostynb <mostynb@opera.com>
2015-11-05 03:03:06 -0800

    include what you use: errno.h in process_memory_dump.cc
    Followup to https://codereview.chromium.org/1398163003 - include errno.h
    since we use errno in process_memory_dump.cc.
    
    BUG=542503
    
    Review URL: https://codereview.chromium.org/1417743004
    
    Cr-Commit-Position: refs/heads/master@{#358024}

--- base/trace_event/process_memory_dump.cc
+++ base/trace_event/process_memory_dump.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/trace_event/process_memory_dump.h"
 
+#include <errno.h>
 #include <vector>
 
 #include "base/process/process_metrics.h"
