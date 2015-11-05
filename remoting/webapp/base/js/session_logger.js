4993023eef87842efbaf9ec0e58835f8b88f0f1a
kelvinp <kelvinp@chromium.org>
2015-11-04 16:23:16 -0800

    Reports PNaCl plugin error to the telemetry service.
    For two releases in a row, We have observed a spike in plugin load
    failure during the first week of a new webapp release roll-out.
    
    This CL reports the lastError string of the plugin element when the
    plugin fails to load.
    
    BUG=547865
    
    Review URL: https://codereview.chromium.org/1420203006
    
    Cr-Commit-Position: refs/heads/master@{#357942}

--- remoting/webapp/base/js/session_logger.js
+++ remoting/webapp/base/js/session_logger.js
@@ -53,6 +53,8 @@ remoting.SessionLogger = function(role, writeLogEntry) {
   this.mode_ = remoting.ChromotingEvent.Mode.ME2ME;
   /** @private {remoting.ChromotingEvent.AuthMethod} */
   this.authMethod_;
+  /** @private */
+  this.pluginError_ = '';
 
   this.setSessionId_();
 };
@@ -127,6 +129,13 @@ remoting.SessionLogger.prototype.setLogEntryMode = function(mode) {
  */
 remoting.SessionLogger.prototype.setAuthMethod = function(method) {
   this.authMethod_ = method;
+};
+
+/**
+ * @param {string} error  The error string of the plugin error.
+ */
+remoting.SessionLogger.prototype.setPluginError = function(error) {
+  this.pluginError_ = error;
 };
 
 /**
@@ -311,6 +320,9 @@ remoting.SessionLogger.prototype.fillEvent_ = function(entry) {
   }
   if (this.authMethod_ != undefined) {
     entry.auth_method = this.authMethod_;
+  }
+  if (Boolean(this.pluginError_)) {
+    entry.raw_plugin_error = this.pluginError_;
   }
   entry.host_version = this.hostVersion_;
   entry.host_os = this.hostOs_;
