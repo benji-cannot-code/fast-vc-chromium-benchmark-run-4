d1a6c806b080f1099c6c70db4267cf1e716b8dec
dfalcantara <dfalcantara@chromium.org>
2015-11-05 15:08:17 -0800

    Begin adding class for laying out InfoBar controls
    * Add a skeleton ViewGroup (extending FrameLayout for now) for
      containing standardized InfoBar controls.  It currently only
      allows a way to create a standardized toggle control obeying
      the new UX spec.
    
    * TranslateInfoBar classes now use the new InfoBarLayout to
      create its "Always translate <blah>" checkbox.  The checkbox
      no longer immediately closes the InfoBar -- it instead waits
      until the user hits "done" or "show original".  Users opting
      to ignore the InfoBar or closing it by hitting the "X" don't
      trigger this save.
    
    BUG=543205
    
    Review URL: https://codereview.chromium.org/1411853007
    
    Cr-Commit-Position: refs/heads/master@{#358167}

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/TranslateInfoBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/TranslateInfoBar.java
@@ -12,7 +12,9 @@ import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
 import android.text.style.ClickableSpan;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.Switch;
 
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.chrome.R;
@@ -190,9 +192,9 @@ public class TranslateInfoBar extends InfoBar implements SubPanelListener {
         if (getInfoBarType() == AFTER_TRANSLATE_INFOBAR
                 && !needsAlwaysPanel()
                 && !mOptions.triggeredFromMenu()) {
-            // Long always translate version
-            TranslateCheckBox checkBox = new TranslateCheckBox(context, mOptions, this);
-            layout.setCustomContent(checkBox);
+            // Fully expanded version of the "Always Translate" InfoBar.
+            ViewGroup subLayout = TranslateAlwaysPanel.createAlwaysToggle(context, mOptions);
+            layout.setCustomContent(subLayout);
         }
     }
 
@@ -230,6 +232,13 @@ public class TranslateInfoBar extends InfoBar implements SubPanelListener {
     @Override
     public void onOptionsChanged() {
         if (mNativeTranslateInfoBarPtr == 0) return;
+
+        // Handle the "Always Translate" checkbox.
+        if (getInfoBarType() == AFTER_TRANSLATE_INFOBAR) {
+            Switch alwaysSwitch =
+                    (Switch) getContentWrapper().findViewById(R.id.translate_infobar_always_toggle);
+            mOptions.toggleAlwaysTranslateLanguageState(alwaysSwitch.isChecked());
+        }
 
         if (mOptions.optionsChanged()) {
             nativeApplyTranslateOptions(mNativeTranslateInfoBarPtr, mOptions.sourceLanguageIndex(),
