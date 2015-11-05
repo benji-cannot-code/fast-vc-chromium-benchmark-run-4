057d04b4835270b6b302f427275f7bae4a904f7c
kcarattini <kcarattini@chromium.org>
2015-11-04 16:38:29 -0800

    Global Permission Kill Switch: Suppress UI and log to console.
    Suppress the url bar icon and log to the developer console when the kill
    switch is enabled.
    
    BUG=539736
    
    Review URL: https://codereview.chromium.org/1429813003
    
    Cr-Commit-Position: refs/heads/master@{#357951}

--- chrome/browser/permissions/permission_context_base.cc
+++ chrome/browser/permissions/permission_context_base.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/prefs/pref_service.h"
+#include "base/strings/stringprintf.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/permissions/permission_request_id.h"
 #include "chrome/browser/permissions/permission_uma_util.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/website_settings_registry.h"
 #include "components/variations/variations_associated_data.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/origin_util.h"
 
@@ -60,10 +62,13 @@ void PermissionContextBase::RequestPermission(
 
   // First check if this permission has been disabled.
   if (IsPermissionKillSwitchOn()) {
+    // Log to the developer console.
+    web_contents->GetMainFrame()->AddMessageToConsole(
+        content::CONSOLE_MESSAGE_LEVEL_LOG,
+        base::StringPrintf("%s permission has been blocked.",
+            PermissionUtil::GetPermissionString(permission_type_).c_str()));
     // The kill switch is enabled for this permission; Block all requests.
-    NotifyPermissionSet(id, requesting_frame.GetOrigin(),
-        web_contents->GetLastCommittedURL().GetOrigin(), callback,
-        false /* persist */, CONTENT_SETTING_BLOCK);
+    callback.Run(CONTENT_SETTING_BLOCK);
     return;
   }
 
