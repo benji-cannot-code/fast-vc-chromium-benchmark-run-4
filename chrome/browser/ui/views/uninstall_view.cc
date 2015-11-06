e82278217a05620bd4d1c582248c1464e75331cf
zmin <zmin@chromium.org>
2015-11-06 13:57:47 -0800

    Remove default browser setting from uninstallation UI on Win10+
    BUG=551970
    
    Review URL: https://codereview.chromium.org/1412063016
    
    Cr-Commit-Position: refs/heads/master@{#358417}

--- chrome/browser/ui/views/uninstall_view.cc
+++ chrome/browser/ui/views/uninstall_view.cc
@@ -81,8 +81,10 @@ void UninstallView::SetupControls() {
   if (dist->GetDefaultBrowserControlPolicy() !=
           BrowserDistribution::DEFAULT_BROWSER_UNSUPPORTED &&
       ShellIntegration::GetDefaultBrowser() == ShellIntegration::IS_DEFAULT &&
-      (ShellIntegration::CanSetAsDefaultBrowser() !=
-          ShellIntegration::SET_DEFAULT_INTERACTIVE)) {
+      (ShellIntegration::CanSetAsDefaultBrowser() ==
+          ShellIntegration::SET_DEFAULT_NOT_ALLOWED ||
+       ShellIntegration::CanSetAsDefaultBrowser() ==
+          ShellIntegration::SET_DEFAULT_UNATTENDED)) {
     browsers_.reset(new BrowsersMap());
     ShellUtil::GetRegisteredBrowsers(dist, browsers_.get());
     if (!browsers_->empty()) {
