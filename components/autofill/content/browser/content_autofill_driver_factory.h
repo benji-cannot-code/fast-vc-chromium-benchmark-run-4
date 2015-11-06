36c8e72de56fe4411f0f09e50e4cc34e7ee859b8
vabr <vabr@chromium.org>
2015-11-06 05:00:45 -0800

    Revert of Use ScopedPtrMap in ContentAutofillDriverFactory (patchset #1 id:20001 of https://codereview.chromium.org/1407273008/ )
    Reason for revert:
    BUG=552358 describes recent crashes magically connected to this change. I have been observing something similar on the parallel change for password manager (https://codereview.chromium.org/1420403006/), so I will revert this first, and once I know more, will reland with a fix.
    
    Original issue's description:
    > Use ScopedPtrMap in ContentAutofillDriverFactory
    >
    > ...instead of manually deleting held objects.
    >
    > This came up as an analogue to https://codereview.chromium.org/1420403006/.
    >
    > R=isherman@chromium.org
    > BUG=549167
    >
    > Committed: https://crrev.com/814cd19723bcf6681de1be9d56998379de7a8dac
    > Cr-Commit-Position: refs/heads/master@{#358018}
    
    TBR=isherman@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=549167
    
    Review URL: https://codereview.chromium.org/1414033012
    
    Cr-Commit-Position: refs/heads/master@{#358312}

--- components/autofill/content/browser/content_autofill_driver_factory.h
+++ components/autofill/content/browser/content_autofill_driver_factory.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/containers/scoped_ptr_map.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/supports_user_data.h"
 #include "components/autofill/content/browser/request_autocomplete_manager.h"
@@ -76,8 +75,7 @@ class ContentAutofillDriverFactory : public content::WebContentsObserver,
   std::string app_locale_;
   AutofillManager::AutofillDownloadManagerState enable_download_manager_;
 
-  base::ScopedPtrMap<content::RenderFrameHost*,
-                     scoped_ptr<ContentAutofillDriver>> frame_driver_map_;
+  std::map<content::RenderFrameHost*, ContentAutofillDriver*> frame_driver_map_;
 };
 
 }  // namespace autofill
