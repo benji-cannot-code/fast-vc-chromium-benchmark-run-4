795687698639a89752d3b40d97be6dabc7e1e888
sigbjornf <sigbjornf@opera.com>
2015-11-06 14:28:41 -0800

    Add missing prefinalizer registration after r358299.
    TBR=oilpan-reviews
    BUG=541338
    
    Review URL: https://codereview.chromium.org/1427023007
    
    Cr-Commit-Position: refs/heads/master@{#358425}

--- third_party/WebKit/Source/platform/mediastream/MediaStreamComponent.cpp
+++ third_party/WebKit/Source/platform/mediastream/MediaStreamComponent.cpp
@@ -57,6 +57,7 @@ MediaStreamComponent::MediaStreamComponent(const String& id, MediaStreamSource* 
     , m_muted(false)
 {
     ASSERT(m_id.length());
+    ThreadState::current()->registerPreFinalizer(this);
 }
 
 void MediaStreamComponent::dispose()
