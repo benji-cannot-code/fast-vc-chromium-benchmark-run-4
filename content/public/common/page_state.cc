f027a8dd800ac75add346dbf1d668c04ff086cb2
creis <creis@chromium.org>
2015-11-06 11:05:53 -0800

    Revert of Gather more debug info for the switch away from page id for titles and state. (patchset #4 id:60001 of https://codereview.chromium.org/1410543014/ )
    Reason for revert:
    We're getting crashes for this on the 48.0.2556.0 canary (in both UpdateState and UpdateTitle).
    
    Original issue's description:
    > Gather more debug info for the switch away from page id for titles and state.
    >
    > This is temporary and can be reverted after we observe crashes (or none).
    >
    > BUG=369661
    > TEST=Watch for crashes in UpdateState and UpdateTitle
    >
    > Committed: https://crrev.com/a67bd94373bfe81c29d2a0cfd7ae956f899eeece
    > Cr-Commit-Position: refs/heads/master@{#358213}
    
    TBR=avi@chromium.org,rsesek@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=369661
    
    Review URL: https://codereview.chromium.org/1412833009
    
    Cr-Commit-Position: refs/heads/master@{#358367}

--- content/public/common/page_state.cc
+++ content/public/common/page_state.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/page_state.h"
 
 #include "base/files/file_path.h"
-#include "base/logging.h"
 #include "base/strings/utf_string_conversions.h"
 #include "content/common/page_state_serialization.h"
 
@@ -119,15 +118,6 @@ bool PageState::Equals(const PageState& other) const {
 
 const std::string& PageState::ToEncodedData() const {
   return data_;
-}
-
-std::string PageState::GetTopLevelUrlStringTemporaryForBug369661() const {
-  ExplodedPageState state;
-  CHECK(DecodePageState(data_, &state));
-
-  base::NullableString16& url_string = state.top.url_string;
-  CHECK(!url_string.is_null());
-  return base::UTF16ToUTF8(url_string.string());
 }
 
 std::vector<base::FilePath> PageState::GetReferencedFiles() const {
