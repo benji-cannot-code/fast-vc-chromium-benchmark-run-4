6dfcbb593d8fe2689ca3d68e89c2042d39bbcad1
rkc <rkc@chromium.org>
2015-11-05 19:47:32 -0800

    Fix blink bluetooth layout tests.
    A recent refactoring of Bluetooth code to separate out BlueZ DBus clients
    from the rest of DBus clients led to a test crash in the bluetooth layout
    tests. This is due to us calling only DBusThreadManager::Initialize() (which
    sets up most of our DBus clients, but not bluetooth DBus clients).
    
    This CL adds a call to BluezDbusManager::Initialize to also initialize the
    bluetooth DBus clients, which are needed later on in the tests (and possibly
    other code that may try to use Bluetooth via content_shell also).
    
    R=avi@chromium.org
    BUG=551985
    
    Review URL: https://codereview.chromium.org/1405953007
    
    Cr-Commit-Position: refs/heads/master@{#358245}

--- content/shell/browser/shell_views.cc
+++ content/shell/browser/shell_views.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS)
 #include "chromeos/dbus/dbus_thread_manager.h"
+#include "device/bluetooth/dbus/bluez_dbus_manager.h"
 #include "ui/aura/test/test_screen.h"
 #include "ui/wm/test/wm_test_helper.h"
 #else  // !defined(OS_CHROMEOS)
@@ -413,6 +414,10 @@ void Shell::PlatformInitialize(const gfx::Size& default_window_size) {
 #endif
 #if defined(OS_CHROMEOS)
   chromeos::DBusThreadManager::Initialize();
+  bluez::BluezDBusManager::Initialize(
+      chromeos::DBusThreadManager::Get()->GetSystemBus(),
+      chromeos::DBusThreadManager::Get()->IsUsingStub(
+          chromeos::DBusClientBundle::BLUETOOTH));
   test_screen_ = aura::TestScreen::Create(gfx::Size());
   gfx::Screen::SetScreenInstance(gfx::SCREEN_TYPE_NATIVE, test_screen_);
   wm_test_helper_ = new wm::WMTestHelper(default_window_size,
@@ -438,6 +443,7 @@ void Shell::PlatformExit() {
   platform_ = NULL;
 #if defined(OS_CHROMEOS)
   device::BluetoothAdapterFactory::Shutdown();
+  bluez::BluezDBusManager::Shutdown();
   chromeos::DBusThreadManager::Shutdown();
 #endif
   aura::Env::DeleteInstance();
