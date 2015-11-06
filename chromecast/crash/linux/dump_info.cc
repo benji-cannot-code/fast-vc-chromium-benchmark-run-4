ca139f4c2c0d0c373b7fe87ecbe86bffbf66b08f
mostynb <mostynb@opera.com>
2015-11-06 15:57:11 -0800

    include what you use: errno.h in chromecast/
    Adding some missing errno.h includes for errno in chromecast/
    
    Review URL: https://codereview.chromium.org/1429373002
    
    Cr-Commit-Position: refs/heads/master@{#358454}

--- chromecast/crash/linux/dump_info.cc
+++ chromecast/crash/linux/dump_info.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 #include "chromecast/crash/linux/dump_info.h"
 
+#include <errno.h>
 #include <stdlib.h>
 
 #include "base/logging.h"
