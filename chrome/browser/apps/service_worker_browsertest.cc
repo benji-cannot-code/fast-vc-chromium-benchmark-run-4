03705bea22fe19aa644dd0782d522c7163bb36d3
lazyboy <lazyboy@chromium.org>
2015-11-05 21:01:25 -0800

    Add basic platform app ServiceWorker tests: registration & postMessage.
    BUG=545531
    Test=None, internal test only change.
    
    Review URL: https://codereview.chromium.org/1407863007
    
    Cr-Commit-Position: refs/heads/master@{#358261}

---
+++ chrome/browser/apps/service_worker_browsertest.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/apps/app_browsertest_util.h"
+
+using ServiceWorkerAppTest = extensions::PlatformAppBrowserTest;
+
+IN_PROC_BROWSER_TEST_F(ServiceWorkerAppTest, RegisterAndPostMessage) {
+  ASSERT_TRUE(RunPlatformAppTest("platform_apps/service_worker")) << message_;
+}
---
+++ chrome/test/data/extensions/platform_apps/service_worker/app.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  function registerAndPostMessage() {
+    navigator.serviceWorker.register('sw.js').then(function() {
+      return navigator.serviceWorker.ready;
+    }).then(function(r) {
+      var mc = new MessageChannel();
+      mc.port1.onmessage = function(e) {
+        mc.port1.onmessage = null;
+        if (e.data && e.data.response === 'pong') {
+          chrome.test.succeed();
+        } else {
+          chrome.test.fail();
+        }
+      };
+      var sw = r.active;
+      sw.postMessage({request: 'ping'}, [mc.port2]);
+    }).catch(function(err) {
+      chrome.test.fail();
+    });
+  }
+]);
