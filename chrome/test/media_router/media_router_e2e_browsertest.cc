8a18a2700d69a69b223ec4576df71bb620400624
imcheng <imcheng@chromium.org>
2015-11-06 14:19:57 -0800

    [Media Router] Replace --enable-media-router with trivalue switch
    We need a way for users to explicitly disable Media Router if it were
    enabled by experiment. This is currently not possible since Media Router
    uses FeatureSwitch, and --enable-media-router is considered a "legacy"
    switch, which means explicit disabling that overrides experiment
    value is not supported (because the fallback value is to disable).
    Changing to a tri value flag (1, 0, default) will solve this problem
    and allow explicit enabling/disabling of MediaRouter overriding
    experiment group.
    
    BUG=552144
    
    Review URL: https://codereview.chromium.org/1420423008
    
    Cr-Commit-Position: refs/heads/master@{#358422}

--- chrome/test/media_router/media_router_e2e_browsertest.cc
+++ chrome/test/media_router/media_router_e2e_browsertest.cc
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   --enable-logging=stderr
 //   --whitelisted-extension-id=enhhojjnijigcajfphajepfemndkmdlo
 //   --ui-test-action-timeout=200000
-//   --enable-media-router
+//   --media-router=1
 
 namespace {
 // Command line argument to specify receiver,
