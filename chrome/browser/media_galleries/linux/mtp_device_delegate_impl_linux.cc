ceacf945c238488e5209babcd24d83a5379f43d3
mostynb <mostynb@opera.com>
2015-11-06 16:14:22 -0800

    include what you use: errno.h in chrome/
    Adding some missing errno.h includes for errno in chrome/
    
    Review URL: https://codereview.chromium.org/1413373010
    
    Cr-Commit-Position: refs/heads/master@{#358463}

--- chrome/browser/media_galleries/linux/mtp_device_delegate_impl_linux.cc
+++ chrome/browser/media_galleries/linux/mtp_device_delegate_impl_linux.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/media_galleries/linux/mtp_device_delegate_impl_linux.h"
 
+#include <errno.h>
 #include <fcntl.h>
 #include <algorithm>
 #include <vector>
--- chrome/common/mac/mock_launchd.cc
+++ chrome/common/mac/mock_launchd.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/mac/mock_launchd.h"
 
 #include <CoreFoundation/CoreFoundation.h>
+#include <errno.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 
