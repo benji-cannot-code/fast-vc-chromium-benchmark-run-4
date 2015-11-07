75b292dfb1e18011c24daae0cbafdeffa0df723a
lushnikov <lushnikov@chromium.org>
2015-11-06 20:30:13 -0800

    DevTools: kill WebInspector.StylesSectionModel class
    Class WI.StylesSectionModel was a presentation model used by
    StyleSidebarPane to render its sections.
    
    The patch removes WI.StylesSectionModel class and starts
    using plain WI.CSSStyleDeclaration objects in WI.StylePropertiesSection
    for its rendering.
    
    BUG=537827
    R=pfeldman, dgozman
    
    Review URL: https://codereview.chromium.org/1434613002
    
    Cr-Commit-Position: refs/heads/master@{#358515}

--- third_party/WebKit/Source/devtools/front_end/sdk/CSSStyleModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/CSSStyleModel.js
@@ -658,19 +658,6 @@ WebInspector.CSSStyleDeclaration.Type = {
     Attributes: "Attributes"
 }
 
-/**
- * @param {!WebInspector.CSSStyleModel} cssModel
- * @return {!WebInspector.CSSStyleDeclaration}
- */
-WebInspector.CSSStyleDeclaration.createDummyStyle = function(cssModel)
-{
-    var dummyPayload = {
-        shorthandEntries: [],
-        cssProperties: []
-    };
-    return new WebInspector.CSSStyleDeclaration(cssModel, null, dummyPayload, WebInspector.CSSStyleDeclaration.Type.Regular);
-}
-
 WebInspector.CSSStyleDeclaration.prototype = {
     /**
      * @param {!CSSAgent.CSSStyle} payload
@@ -1044,6 +1031,27 @@ WebInspector.CSSRule = function(cssModel, payload, matchingSelectors)
         this.media = WebInspector.CSSMedia.parseMediaArrayPayload(cssModel, payload.media);
 }
 
+/**
+ * @param {!WebInspector.CSSStyleModel} cssModel
+ * @param {string} selectorText
+ * @return {!WebInspector.CSSRule}
+ */
+WebInspector.CSSRule.createDummyRule = function(cssModel, selectorText)
+{
+    var dummyPayload = {
+        selectorList: {
+            selectors: [{ value: selectorText}],
+        },
+        style: {
+            styleSheetId: "0",
+            range: new WebInspector.TextRange(0, 0, 0, 0),
+            shorthandEntries: [],
+            cssProperties: []
+        }
+    };
+    return new WebInspector.CSSRule(cssModel, /** @type {!CSSAgent.CSSRule} */(dummyPayload));
+}
+
 WebInspector.CSSRule.prototype = {
     /**
      * @param {!DOMAgent.NodeId} nodeId
@@ -1244,7 +1252,7 @@ WebInspector.CSSRule.prototype = {
 
 /**
  * @constructor
- * @param {?WebInspector.CSSStyleDeclaration} ownerStyle
+ * @param {!WebInspector.CSSStyleDeclaration} ownerStyle
  * @param {number} index
  * @param {string} name
  * @param {string} value
@@ -1271,7 +1279,7 @@ WebInspector.CSSProperty = function(ownerStyle, index, name, value, important, d
 }
 
 /**
- * @param {?WebInspector.CSSStyleDeclaration} ownerStyle
+ * @param {!WebInspector.CSSStyleDeclaration} ownerStyle
  * @param {number} index
  * @param {!CSSAgent.CSSProperty} payload
  * @return {!WebInspector.CSSProperty}
@@ -2061,6 +2069,7 @@ WebInspector.CSSStyleModel.MatchedStyleResult = function(cssModel, nodeId, inlin
 
     this._nodeStyles = [];
     this._nodeForStyle = new Map();
+    this._inheritedStyles = new Set();
 
     /**
      * @this {WebInspector.CSSStyleModel.MatchedStyleResult}
@@ -2107,6 +2116,7 @@ WebInspector.CSSStyleModel.MatchedStyleResult = function(cssModel, nodeId, inlin
         if (inheritedInlineStyle && this._containsInherited(inheritedInlineStyle)) {
             this._nodeForStyle.set(inheritedInlineStyle, parentNode);
             this._nodeStyles.push(inheritedInlineStyle);
+            this._inheritedStyles.add(inheritedInlineStyle);
         }
 
         for (var j = inheritedMatchedCSSRules.length - 1; j >= 0; --j) {
@@ -2115,6 +2125,7 @@ WebInspector.CSSStyleModel.MatchedStyleResult = function(cssModel, nodeId, inlin
                 continue;
             this._nodeForStyle.set(inheritedRule.style, parentNode);
             this._nodeStyles.push(inheritedRule.style);
+            this._inheritedStyles.add(inheritedRule.style);
         }
         parentNode = parentNode.parentNode;
     }
@@ -2185,7 +2196,7 @@ WebInspector.CSSStyleModel.MatchedStyleResult.prototype = {
      */
     isInherited: function(style)
     {
-        return this.nodeForStyle(style) !== this._node;
+        return this._inheritedStyles.has(style);
     }
 }
 
