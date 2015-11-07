495de0617e4e7d7fc98ab8d1154b416f33c9b26e
thakis <thakis@chromium.org>
2015-11-06 19:35:44 -0800

    Fix ipc_fuzzer build after https://codereview.chromium.org/1423053002
    BUG=552680
    TBR=alexmos
    
    Review URL: https://codereview.chromium.org/1436463002
    
    Cr-Commit-Position: refs/heads/master@{#358509}

--- content/common/frame_messages.h
+++ content/common/frame_messages.h
@@ -657,7 +657,7 @@ IPC_MESSAGE_ROUTED1(FrameMsg_DidUpdateOrigin, url::Origin /* origin */)
 
 // Notifies this frame or proxy that it is now focused.  This is used to
 // support cross-process focused frame changes.
-IPC_MESSAGE_ROUTED0(FrameMsg_SetFocusedFrame);
+IPC_MESSAGE_ROUTED0(FrameMsg_SetFocusedFrame)
 
 // Send to the RenderFrame to set text tracks state and style settings.
 // Sent for top-level frames.
