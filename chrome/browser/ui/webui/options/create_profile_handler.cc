cc36a59cd419c356b9ad7a08ec82898472b50599
lwchkg <lwchkg@gmail.com>
2015-11-06 17:47:56 -0800

    Background information: If the GAIA picture is selected as the icon, some of the profile creation code is not executed and errors will happen. See the bug for more information.
    BUG=529669
    
    Review URL: https://codereview.chromium.org/1363053002
    
    Cr-Commit-Position: refs/heads/master@{#358495}

--- chrome/browser/ui/webui/options/create_profile_handler.cc
+++ chrome/browser/ui/webui/options/create_profile_handler.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/value_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/profiles/profile_avatar_icon_util.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_metrics.h"
 #include "chrome/browser/profiles/profiles_state.h"
@@ -87,11 +88,16 @@ void CreateProfileHandler::CreateProfile(const base::ListValue* args) {
   profile_creation_start_time_ = base::TimeTicks::Now();
 
   base::string16 name;
-  std::string icon;
+  std::string icon_url;
   bool create_shortcut = false;
-  if (args->GetString(0, &name) && args->GetString(1, &icon)) {
+  if (args->GetString(0, &name) && args->GetString(1, &icon_url)) {
+    DCHECK(base::IsStringASCII(icon_url));
     base::TrimWhitespace(name, base::TRIM_ALL, &name);
     CHECK(!name.empty());
+#ifndef NDEBUG
+    size_t icon_index;
+    DCHECK(profiles::IsDefaultAvatarIconUrl(icon_url, &icon_index));
+#endif
     args->GetBoolean(2, &create_shortcut);
   }
   std::string supervised_user_id;
@@ -103,7 +109,7 @@ void CreateProfileHandler::CreateProfile(const base::ListValue* args) {
   ProfileMetrics::LogProfileAddNewUser(ProfileMetrics::ADD_NEW_USER_DIALOG);
 
   profile_path_being_created_ = ProfileManager::CreateMultiProfileAsync(
-      name, icon,
+      name, icon_url,
       base::Bind(&CreateProfileHandler::OnProfileCreated,
                  weak_ptr_factory_.GetWeakPtr(),
                  create_shortcut,
