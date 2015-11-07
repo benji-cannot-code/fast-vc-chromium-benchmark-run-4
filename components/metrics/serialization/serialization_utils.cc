d5b48dfc4ba7e53a00596e5b7ca36725fe231026
mostynb <mostynb@opera.com>
2015-11-07 01:32:27 -0800

    include what you use: errno.h in components/
    Adding some missing errno.h includes for errno in components/
    
    NOPRESUBMIT=TRUE
    
    Review URL: https://codereview.chromium.org/1424273004
    
    Cr-Commit-Position: refs/heads/master@{#358522}

--- components/metrics/serialization/serialization_utils.cc
+++ components/metrics/serialization/serialization_utils.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/metrics/serialization/serialization_utils.h"
 
+#include <errno.h>
 #include <sys/file.h>
 
 #include <string>
