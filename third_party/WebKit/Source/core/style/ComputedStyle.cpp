79f4e532b06d783aeab6f77534922c2d29707d60
kojii <kojii@chromium.org>
2015-11-09 10:36:22 -0800

    Add logical-over and under to margin/border/padding
    This patch adds logical-over and under variations to
    margin/padding/border() families.
    
    Logical over and under are defined in Line-Relative Directions in CSS
    Writing Modes spec[1].
    
    Distinguishing line-relative directions (over/under) from block-relative
    directions (before/after) improves line-flipped mode such as vertical-lr.
    
    [1] https://drafts.csswg.org/css-writing-modes-3/#line-over
    
    BUG=552955
    
    Review URL: https://codereview.chromium.org/1408193014
    
    Cr-Commit-Position: refs/heads/master@{#358613}

--- third_party/WebKit/Source/core/style/ComputedStyle.cpp
+++ third_party/WebKit/Source/core/style/ComputedStyle.cpp
@@ -1639,6 +1639,16 @@ int ComputedStyle::borderEndWidth() const
     return isLeftToRightDirection() ? borderBottomWidth() : borderTopWidth();
 }
 
+int ComputedStyle::borderOverWidth() const
+{
+    return isHorizontalWritingMode() ? borderTopWidth() : borderRightWidth();
+}
+
+int ComputedStyle::borderUnderWidth() const
+{
+    return isHorizontalWritingMode() ? borderBottomWidth() : borderLeftWidth();
+}
+
 void ComputedStyle::setMarginStart(const Length& margin)
 {
     if (isHorizontalWritingMode()) {
