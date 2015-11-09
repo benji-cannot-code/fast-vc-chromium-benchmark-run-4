6405e3caf2283db874be9f04b1710dfa5d3ba119
timav <timav@chromium.org>
2015-11-09 15:37:25 -0800

    Added default case to fix Desktop.java compilation.
    This is needed to make Android ToT builder bot happy.
    
    BUG= 551584, 553666
    
    Review URL: https://codereview.chromium.org/1434743002
    
    Cr-Commit-Position: refs/heads/master@{#358704}

--- remoting/android/java/src/org/chromium/chromoting/Desktop.java
+++ remoting/android/java/src/org/chromium/chromoting/Desktop.java
@@ -241,6 +241,10 @@ public class Desktop extends AppCompatActivity implements View.OnSystemUiVisibil
                     case MotionEvent.ACTION_UP:
                         startActionBarAutoHideTimer();
                         break;
+
+                    default:
+                        // Ignore.
+                        break;
                 }
 
                 return false;
