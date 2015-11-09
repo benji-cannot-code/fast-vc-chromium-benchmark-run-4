aa4dc0b33f382e5b808a65160dd99d3b724b29be
henrika <henrika@chromium.org>
2015-11-09 04:02:02 -0800

    Revert of Improved error handling for low-latency audio input on Mac (patchset #1 id:1 of https://codereview.chromium.org/1420723010/ )
    Reason for revert:
    I missed that we did in fact have an OK support for "stop and close down when error happens" even before this patch. Calls to HandleError() does clean things up for us and there was no similar functionality for output.
    
    Also, I have seen an increase in number of reported issues after this patch was landed.
    
    And finally, main main approach was:
    
    1) Add stats and try to find reason for why input audio fails.
    2) Take action and observer if issue reports goes down.
    
    This patch added and action a bit too early (before stats) had stabilized and I want to revert it for now until I understand the issue better. Might be added again later.
    
    Original issue's description:
    > Improved error handling for low-latency audio input on Mac
    >
    > BUG=549021
    >
    > Committed: https://crrev.com/c1faecd179e2934a67f10c66623b4c27ab869ece
    > Cr-Commit-Position: refs/heads/master@{#357556}
    
    TBR=tommi@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=549021
    
    Review URL: https://codereview.chromium.org/1434663003
    
    Cr-Commit-Position: refs/heads/master@{#358580}

--- media/audio/mac/audio_low_latency_input_mac.cc
+++ media/audio/mac/audio_low_latency_input_mac.cc
@@ -153,7 +153,6 @@ bool AUAudioInputStream::Open() {
                                 &enableIO,  // enable
                                 sizeof(enableIO));
   if (result != noErr) {
-    CloseAudioUnit();
     HandleError(result);
     return false;
   }
@@ -167,7 +166,6 @@ bool AUAudioInputStream::Open() {
                                 &enableIO,  // disable
                                 sizeof(enableIO));
   if (result != noErr) {
-    CloseAudioUnit();
     HandleError(result);
     return false;
   }
@@ -181,7 +179,6 @@ bool AUAudioInputStream::Open() {
                                 &input_device_id_,
                                 sizeof(input_device_id_));
   if (result != noErr) {
-    CloseAudioUnit();
     HandleError(result);
     return false;
   }
@@ -196,7 +193,6 @@ bool AUAudioInputStream::Open() {
                                 &format_,
                                 sizeof(format_));
   if (result != noErr) {
-    CloseAudioUnit();
     HandleError(result);
     return false;
   }
@@ -204,7 +200,6 @@ bool AUAudioInputStream::Open() {
   if (!manager_->MaybeChangeBufferSize(input_device_id_, audio_unit_, 1,
                                        number_of_frames_,
                                        &buffer_size_was_changed_)) {
-    CloseAudioUnit();
     return false;
   }
   DLOG_IF(WARNING, buffer_size_was_changed_) << "IO buffer size was changed to "
@@ -223,7 +218,6 @@ bool AUAudioInputStream::Open() {
                                 &callback,
                                 sizeof(callback));
   if (result != noErr) {
-    CloseAudioUnit();
     HandleError(result);
     return false;
   }
@@ -233,7 +227,6 @@ bool AUAudioInputStream::Open() {
   // it can produce in response to a single render call.
   result = AudioUnitInitialize(audio_unit_);
   if (result != noErr) {
-    CloseAudioUnit();
     HandleError(result);
     return false;
   }
