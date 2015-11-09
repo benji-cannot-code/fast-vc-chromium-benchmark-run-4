aa00a343c44d4ececbf31e64170913ebd27d197b
mostynb <mostynb@opera.com>
2015-11-09 06:57:06 -0800

    include what you use: errno.h in ui/ozone/
    Add missing errno.h include.
    
    Review URL: https://codereview.chromium.org/1417503013
    
    Cr-Commit-Position: refs/heads/master@{#358589}

--- ui/ozone/platform/drm/gpu/hardware_display_plane_manager_legacy.cc
+++ ui/ozone/platform/drm/gpu/hardware_display_plane_manager_legacy.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/ozone/platform/drm/gpu/hardware_display_plane_manager_legacy.h"
 
+#include <errno.h>
+
 #include "base/bind.h"
 #include "ui/ozone/platform/drm/gpu/crtc_controller.h"
 #include "ui/ozone/platform/drm/gpu/drm_device.h"
