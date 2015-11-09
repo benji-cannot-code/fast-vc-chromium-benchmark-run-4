ab32b51bab49fa59d4f2b4f611f923008fca26c3
wangxianzhu <wangxianzhu@chromium.org>
2015-11-09 09:06:53 -0800

    Revert of Enable slimming paint synchronized painting (patchset #10 id:180001 of https://codereview.chromium.org/1414633006/ )
    Reason for revert:
    Caused many regression bugs.
    
    BUG=536999,553045,553147,553261,553275,553281,553327,553356,553392
    
    Original issue's description:
    > Enable slimming paint synchronized painting
    >
    > BUG=536999
    >
    > Committed: https://crrev.com/70533f0464b41fcdfd9ca42d5e0c417edae748a6
    > Cr-Commit-Position: refs/heads/master@{#358271}
    
    TBR=chrishtr@chromium.org,caseq@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=536999
    
    Review URL: https://codereview.chromium.org/1436533002
    
    Cr-Commit-Position: refs/heads/master@{#358600}

--- third_party/WebKit/Source/core/layout/compositing/PaintLayerCompositor.cpp
+++ third_party/WebKit/Source/core/layout/compositing/PaintLayerCompositor.cpp
@@ -805,7 +805,7 @@ void PaintLayerCompositor::paintContents(const GraphicsLayer* graphicsLayer, Gra
 {
     IntRect defaultClip;
     if (RuntimeEnabledFeatures::slimmingPaintSynchronizedPaintingEnabled() && !clip) {
-        defaultClip.setSize(expandedIntSize(graphicsLayer->size()));
+        defaultClip.setSize(m_layoutView.layoutSize(IncludeScrollbars));
         clip = &defaultClip;
     }
     ASSERT(clip);
