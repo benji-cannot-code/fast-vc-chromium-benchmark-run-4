75d2619102dbdd3fe4c6363f3a09bd162826fef7
dalecurtis <dalecurtis@chromium.org>
2015-11-09 15:54:14 -0800

    Stall audio thread after device changes to avoid 3rd party deadlock.
    Speculative change to see if stalling callbacks into the Windows
    audio subsystem after a device change is detected will reduce the
    frequency of deadlock in 3rd party WDM drivers.
    
    This change reroutes both input and output device changes through
    the AudioManagerWin, where a simple sleep on the audio thread
    stalls any outstanding activity.
    
    The stall should be fine as the audio thread is used primarily as
    an asychronous control channel. I've chosen a stall over a post
    delayed task to ensure that any outstanding tasks are prevented.
    
    If this CL is successful we can experiment with loosening to a
    PostDelayedTask will still bear fruit.
    
    BUG=422522
    TEST=device change works as normal.
    
    Review URL: https://codereview.chromium.org/1428413003
    
    Cr-Commit-Position: refs/heads/master@{#358707}

--- media/audio/win/audio_device_listener_win.cc
+++ media/audio/win/audio_device_listener_win.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/system_monitor/system_monitor.h"
 #include "base/time/default_tick_clock.h"
 #include "base/win/scoped_co_mem.h"
 #include "base/win/windows_version.h"
@@ -32,7 +31,7 @@ static std::string RoleToString(ERole role) {
   }
 }
 
-AudioDeviceListenerWin::AudioDeviceListenerWin(const base::Closure& listener_cb)
+AudioDeviceListenerWin::AudioDeviceListenerWin(const ListenerCB& listener_cb)
     : listener_cb_(listener_cb), tick_clock_(new base::DefaultTickClock()) {
   CHECK(CoreAudioUtil::IsSupported());
 
@@ -99,10 +98,7 @@ STDMETHODIMP AudioDeviceListenerWin::OnDeviceRemoved(LPCWSTR device_id) {
 
 STDMETHODIMP AudioDeviceListenerWin::OnDeviceStateChanged(LPCWSTR device_id,
                                                           DWORD new_state) {
-  base::SystemMonitor* monitor = base::SystemMonitor::Get();
-  if (monitor)
-    monitor->ProcessDevicesChanged(base::SystemMonitor::DEVTYPE_AUDIO_CAPTURE);
-
+  listener_cb_.Run(kInputDeviceChange);
   return S_OK;
 }
 
@@ -132,7 +128,7 @@ STDMETHODIMP AudioDeviceListenerWin::OnDefaultDeviceChanged(
       now - last_device_change_time_ >
           base::TimeDelta::FromMilliseconds(kDeviceChangeLimitMs)) {
     last_device_change_time_ = now;
-    listener_cb_.Run();
+    listener_cb_.Run(kOutputDeviceChange);
     did_run_listener_cb = true;
   }
 
