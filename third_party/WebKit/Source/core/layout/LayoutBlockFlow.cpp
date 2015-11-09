d0eb29b598441dd6a9d14f1aba652b891ac3f440
pilgrim <pilgrim@chromium.org>
2015-11-09 13:47:08 -0800

    [Line Layout API] Add LineLayoutItem::isEqual method
    Several ASSERTs throughout layout code check whether a LayoutObject is
    equal to another LayoutObject (generally inside iterators). As we transition
    to the line layout API, some of those objects will no longer have a publicly
    accessible LayoutObject (because they only offer a LineLayoutItem). Thus,
    this method will allow easy transition for those objects.
    
    Levi voted against making this an operator, on the basis that the line
    layout API is all about making types as explicit as possible.
    
    BUG=499321
    
    Review URL: https://codereview.chromium.org/1437513002
    
    Cr-Commit-Position: refs/heads/master@{#358667}

--- third_party/WebKit/Source/core/layout/LayoutBlockFlow.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlockFlow.cpp
@@ -941,7 +941,7 @@ void LayoutBlockFlow::rebuildFloatsFromIntruding()
                     }
 
                     if (oldFloatingObject->originatingLine() && !selfNeedsLayout()) {
-                        ASSERT(oldFloatingObject->originatingLine()->layoutObject() == this);
+                        ASSERT(oldFloatingObject->originatingLine()->lineLayoutItem().isEqual(this));
                         oldFloatingObject->originatingLine()->markDirty();
                     }
 
@@ -2256,7 +2256,7 @@ void LayoutBlockFlow::removeFloatingObject(LayoutBox* floatBox)
                 }
                 if (floatingObject.originatingLine()) {
                     if (!selfNeedsLayout()) {
-                        ASSERT(floatingObject.originatingLine()->layoutObject() == this);
+                        ASSERT(floatingObject.originatingLine()->lineLayoutItem().isEqual(this));
                         floatingObject.originatingLine()->markDirty();
                     }
 #if ENABLE(ASSERT)
