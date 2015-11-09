8ad4b9ee110625c8a6f2a5631d57d902c69a402a
yoshiki <yoshiki@chromium.org>
2015-11-09 11:23:07 -0800

    [Download Notification] Fix flakiness in the tests
    This patch fixes the flakiness:
    - Disable the popup timer
    - Add a method to watch only specific ID in NotificationUpdateObserver
    - Wait over multiple updates by adding the Reset() method
    - In DownloadNotificationTest.DownloadMultipleFiles test,
      make |download1| and |download2| object distinguished.
    
    BUG=522643
    TEST=browser_test passed
    
    Review URL: https://codereview.chromium.org/1414953007
    
    Cr-Commit-Position: refs/heads/master@{#358623}

--- ui/message_center/message_center.h
+++ ui/message_center/message_center.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/notification_list.h"
 
 class DownloadNotification;
+class DownloadNotificationTestBase;
 
 namespace base {
 class DictionaryValue;
@@ -182,6 +183,7 @@ class MESSAGE_CENTER_EXPORT MessageCenter {
 
  protected:
   friend class ::DownloadNotification;
+  friend class ::DownloadNotificationTestBase;
   friend class MessageCenterImplTest;
   friend class MessageCenterImplTestWithChangeQueue;
   friend class MessageCenterImplTestWithoutChangeQueue;
