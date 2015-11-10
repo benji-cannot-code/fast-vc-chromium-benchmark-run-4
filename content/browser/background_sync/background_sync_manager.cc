0e72f23cd62236877458149b8af28e4d1f8e0f4e
jkarlin <jkarlin@chromium.org>
2015-11-10 10:27:20 -0800

    [BackgroundSync] Handle reregistering a sync mid-fire.
    If a sync is reregistered in the midst of firing, reset the state of the registration after it's done firing
    
    BUG=551435
    
    Review URL: https://codereview.chromium.org/1411783013
    
    Cr-Commit-Position: refs/heads/master@{#358856}

--- content/browser/background_sync/background_sync_manager.cc
+++ content/browser/background_sync/background_sync_manager.cc
@@ -475,6 +475,11 @@ void BackgroundSyncManager::RegisterImpl(
         BackgroundSyncMetrics::REGISTRATION_IS_DUPLICATE,
         BACKGROUND_SYNC_STATUS_OK);
 
+    if (existing_registration->IsFiring()) {
+      existing_registration->set_sync_state(
+          BACKGROUND_SYNC_STATE_REREGISTERED_WHILE_FIRING);
+    }
+
     base::ThreadTaskRunnerHandle::Get()->PostTask(
         FROM_HERE,
         base::Bind(
@@ -1256,9 +1261,12 @@ void BackgroundSyncManager::EventCompleteImpl(
   }
 
   if (registration->options()->periodicity == SYNC_ONE_SHOT) {
-    if (status_code != SERVICE_WORKER_OK) {
+    if (registration->sync_state() ==
+        BACKGROUND_SYNC_STATE_REREGISTERED_WHILE_FIRING) {
+      registration->set_sync_state(BACKGROUND_SYNC_STATE_PENDING);
+      registration->set_num_attempts(0);
+    } else if (status_code != SERVICE_WORKER_OK) {  // Sync failed
       bool can_retry = registration->num_attempts() < max_sync_attempts_;
-
       if (registration->sync_state() ==
           BACKGROUND_SYNC_STATE_UNREGISTERED_WHILE_FIRING) {
         registration->set_sync_state(can_retry
@@ -1271,11 +1279,11 @@ void BackgroundSyncManager::EventCompleteImpl(
             clock_->Now() +
             base::TimeDelta::FromMinutes(kInitialRetryDelayInMins) *
                 pow(kRetryDelayFactor, registration->num_attempts() - 1));
-      } else {  // can't retry
+      } else {
         registration->set_sync_state(BACKGROUND_SYNC_STATE_FAILED);
         registration->RunFinishedCallbacks();
       }
-    } else {  // sync succeeded
+    } else {  // Sync succeeded
       registration->set_sync_state(BACKGROUND_SYNC_STATE_SUCCESS);
       registration->RunFinishedCallbacks();
     }
--- content/browser/background_sync/background_sync_registration.h
+++ content/browser/background_sync/background_sync_registration.h
@@ -35,6 +35,7 @@ class CONTENT_EXPORT BackgroundSyncRegistration {
   void AddFinishedCallback(const StateCallback& callback);
   void RunFinishedCallbacks();
   bool HasCompleted() const;
+  bool IsFiring() const;
 
   // If the registration is currently firing, sets its state to
   // BACKGROUND_SYNC_STATE_UNREGISTERED_WHILE_FIRING. If it is firing, it sets
