9279d7aff231d42620fd233587e869efbd85ebaf
nednguyen <nednguyen@google.com>
2015-11-09 17:13:09 -0800

    [tools/perf] Add dummy benchmark that produces Gaussian value with a known mean and std
    *Perf sherrif: these benchmarks are used for testing
    telemetry integration with perf dashboard & bisect bot
    only, and do not reflect actual performance data of the
    browser. Please feel free to ignore these benchmarks.
    
    BUG=537676
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_10_10_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    The cycle times of these benchmarks are relative short:
    
    On linux desktop:
    real	0m4.617s
    user	0m2.234s
    sys	0m2.325s
    
    On Android nexus 5:
    real	0m16.390s
    user	0m2.471s
    sys	0m4.416s
    
    Review URL: https://codereview.chromium.org/1410683014
    
    Cr-Commit-Position: refs/heads/master@{#358724}

---
+++ tools/perf/page_sets/dummy_story_set.py
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from telemetry.page import page as page_module
+from telemetry import story
+
+
+class DummyPage(page_module.Page):
+
+  def __init__(self, page_set):
+    super(DummyPage, self).__init__(
+      url='file://dummy_page.html',
+      page_set=page_set)
+
+  def PerformPageInteractions(self, action_runner):
+    assert action_runner.EvaluateJavaScript('1 + window.__dummy_value') == 2
+
+
+class DummyStorySet(story.StorySet):
+
+  def __init__(self):
+    super(DummyStorySet, self).__init__()
+    self.AddStory(DummyPage(self))
