5d0353d45acd304bac1450248dc7c912c32c38fb
cmumford <cmumford@chromium.org>
2015-11-09 16:54:39 -0800

    LeveldbValueStore: Deleting db when open fails due to corruption.
    Corrupted databases would never be deleted. This change will delete
    the leveldb database iff an open attempt fails as a result of
    corruption.
    
    This class is currently used by the Extensions component in Chrome.
    
    BUG=549177
    
    Review URL: https://codereview.chromium.org/1428843002
    
    Cr-Commit-Position: refs/heads/master@{#358722}

--- extensions/browser/value_store/leveldb_value_store.h
+++ extensions/browser/value_store/leveldb_value_store.h
@@ -93,7 +93,7 @@ class LeveldbValueStore : public ValueStore,
 
   // Removes the on-disk database at |db_path_|. Any file system locks should
   // be released before calling this method.
-  void DeleteDbFile();
+  bool DeleteDbFile();
 
   // Returns whether the database is empty.
   bool IsEmpty();
@@ -104,6 +104,7 @@ class LeveldbValueStore : public ValueStore,
   // leveldb backend.
   scoped_ptr<leveldb::DB> db_;
   base::HistogramBase* open_histogram_;
+  base::HistogramBase* restore_histogram_;
 
   DISALLOW_COPY_AND_ASSIGN(LeveldbValueStore);
 };
