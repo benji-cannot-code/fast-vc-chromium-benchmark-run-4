1e9ed24efe8c34962cffcaa66cf2bc401f37055f
hbos <hbos@chromium.org>
2015-11-10 03:33:59 -0800

    Removes RTCCertificate.expires attribute.
    It should be re-added as a DOMTimeStamp once WebRTC layer certificates (rtc::RTCCertificate) correctly implement expires.
    
    BUG=544894
    
    Review URL: https://codereview.chromium.org/1418563004
    
    Cr-Commit-Position: refs/heads/master@{#358820}

--- third_party/WebKit/Source/modules/mediastream/RTCCertificate.cpp
+++ third_party/WebKit/Source/modules/mediastream/RTCCertificate.cpp
@@ -34,11 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-double RTCCertificate::expires() const
-{
-    return m_certificate->expires();
-}
-
 RTCCertificate::RTCCertificate(WebRTCCertificate* certificate)
     : m_certificate(adoptPtr(certificate))
 {
