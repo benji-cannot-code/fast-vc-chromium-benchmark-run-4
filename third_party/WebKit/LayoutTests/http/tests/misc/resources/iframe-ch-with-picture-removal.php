245260ef22c41004b621a674497721d96b342067
yoav <yoav@yoav.ws>
2015-11-10 01:19:17 -0800

    Fix Width hints values when picture is involved.
    In HTMLImageElement, sizes related calculations for resourceWidth purposes
    were performed based on the HTMLImageElement, rather than based on the
    source that was picked in the source selection algorithm.
    This CL fixes that and adds tests.
    
    BUG=535979
    
    Review URL: https://codereview.chromium.org/1430273003
    
    Cr-Commit-Position: refs/heads/master@{#358812}

---
+++ third_party/WebKit/LayoutTests/http/tests/misc/resources/iframe-ch-with-picture-removal.php
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+    header("ACCEPT-CH: DPR, Width, Viewport-Width");
+?>
+<!DOCTYPE html>
+<body>
+<script>
+    window.addEventListener("message", function (message) {
+        var pic = document.getElementById("pic");
+        pic.removeChild(pic.childNodes[0]);
+        // TODO(yoav): this should trigger a load, but doesn't. See https://crbug.com/418903
+        success();
+    });
+
+    var fail = function(num) {
+        parent.postMessage("fail "+ num, "*");
+    };
+
+    var success = function() {
+        parent.postMessage("success", "*");
+    };
+
+    var remove = function() {
+        parent.postMessage("remove", "*");
+    };
+
+    var counter = 1;
+    var error = function() {
+        fail(counter);
+    }
+    var load = function() {
+        if (counter == 1) {
+            ++counter;
+            remove();
+            return;
+        }
+        success();
+    }
+</script>
+<picture id=pic>
+    <source sizes="50vw" media="(min-width: 800px)" srcset="image-checks-for-width.php?rw=400">
+    <source sizes="50vw" srcset="image-checks-for-width.php?rw=300">
+    <img onerror="error()" onload="load()">
+</picture>
