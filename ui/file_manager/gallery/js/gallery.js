b0aafd76614a5af30e1fced329d3193ec152ebbd
yawano <yawano@chromium.org>
2015-11-09 18:41:08 -0800

    Gallery: focus to thumbnail view after it opens.
    BUG=552330
    TEST=browser_tests:GalleryBrowserTest*.OpenMultipleImages*
    
    Review URL: https://codereview.chromium.org/1433653002
    
    Cr-Commit-Position: refs/heads/master@{#358761}

--- ui/file_manager/gallery/js/gallery.js
+++ ui/file_manager/gallery/js/gallery.js
@@ -429,6 +429,7 @@ Gallery.prototype.loadInternal_ = function(entries, selectedEntries) {
         // Do the initialization for each mode.
         if (shouldShowThumbnail) {
           self.thumbnailMode_.show();
+          self.thumbnailMode_.focus();
         } else {
           self.slideMode_.enter(
               null,
