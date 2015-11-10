0f3c8628351fb538b17932d1ee939a1e0e127bae
Ian Wen <ianwen@chromium.org>
2015-11-10 11:54:00 -0800

    Disable Flaky AndroidSyncSettingsTest
    AndroidSyncSettingsTest#testAccountInitialization() is flaky on Android
    dbg buildbot.
    
    BUG=554154
    TBR=nyquist@chromium.org
    
    Review URL: https://codereview.chromium.org/1423593008 .
    
    Cr-Commit-Position: refs/heads/master@{#358883}

--- sync/android/javatests/src/org/chromium/sync/AndroidSyncSettingsTest.java
+++ sync/android/javatests/src/org/chromium/sync/AndroidSyncSettingsTest.java
@@ -8,6 +8,7 @@ package org.chromium.sync;
 import android.accounts.Account;
 import android.content.Context;
 import android.os.Bundle;
+import android.test.FlakyTest;
 import android.test.InstrumentationTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
 
@@ -145,8 +146,12 @@ public class AndroidSyncSettingsTest extends InstrumentationTestCase {
         });
     }
 
-    @SmallTest
-    @Feature({"Sync"})
+    @FlakyTest
+    /*
+     * http://crbug.com/554154
+     * @SmallTest
+     * @Feature({"Sync"})
+     */
     public void testAccountInitialization() throws InterruptedException {
         // mAccount was set to be syncable and not have periodic syncs.
         assertEquals(1, mSyncContentResolverDelegate.mSetIsSyncableCalls);
