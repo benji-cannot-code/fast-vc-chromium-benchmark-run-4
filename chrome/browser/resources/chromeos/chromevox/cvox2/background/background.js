3b70ccd89a45e6c3a5be179b8346e281c79ed986
dtseng <dtseng@chromium.org>
2015-11-10 15:37:10 -0800

    Support activation and deactivation for ChromeVox Next
    In ChromeVox Classic, the notion of active vs inactive means:
    - no output.
    - no reaction to events (besides limited response to keyboard events).
    - explicitly skip in Chrome OS (there's already a dedicated key to turn on/off, so no need for activation/deactivation).
    
    In Next, as of this cl, it will mean:
    - no response to automation events.
    - no response to most keyboard events (except for the 'toggleChromeVox' command). This is the same codepath as Classic.
    - no support on Mac because of no desktop automation on Mac.
    
    When activation/deactivation happens in Classic:
    - old codepath gets hit (content script -> Classic background page, prefs broadcasts to all frames/content scripts)
    - Classic background page sets cvox.ChromeVox.isActive which now deactivates the ChromeVox Next background object (Next installs a setter on cvox.ChromeVox.isActive).
    
    When activation/deactivation happens on Next:
    - Next background page handles it directly.
    - it sets the source of truth on the activeness of ChromeVox in localStorage (which persists for Classic as well). This doesn't matter because one cannot toggle ChromeVox from Next to Classic and vice versa while deactivated.
    
    BUG=496796
    TEST=chromevox_tests
    and
    - build ChromeVox as a webstore extension. Install on Linux, try cvox+a+a repeatedly; verify no feedback in address bar, web area, etc.
    - toggle to Next; do the same as above.
    - on Chrome OS, verify command doesn't work regardless of Next/Classic state.
    
    Review URL: https://codereview.chromium.org/1415463009
    
    Cr-Commit-Position: refs/heads/master@{#358950}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
@@ -82,27 +82,6 @@ Background = function() {
       this[func] = this[func].bind(this);
   }
 
-  chrome.automation.getDesktop(this.onGotDesktop);
-
-  // Handle messages directed to the Next background page.
-  cvox.ExtensionBridge.addMessageListener(function(msg, port) {
-    var target = msg['target'];
-    var action = msg['action'];
-
-    switch (target) {
-      case 'next':
-        if (action == 'getIsClassicEnabled') {
-          var url = msg['url'];
-          var isClassicEnabled = this.shouldEnableClassicForUrl_(url);
-          port.postMessage({
-            target: 'next',
-            isClassicEnabled: isClassicEnabled
-          });
-        }
-        break;
-    }
-  }.bind(this));
-
   /** @type {!cvox.AbstractEarcons} @private */
   this.classicEarcons_ = cvox.ChromeVox.earcons || new cvox.ClassicEarcons();
 
@@ -130,12 +109,25 @@ Background = function() {
     }.bind(this)
   });
 
+  Object.defineProperty(cvox.ChromeVox, 'isActive', {
+    get: function() {
+      return localStorage['active'] !== 'false';
+    },
+    set: function(value) {
+      localStorage['active'] = value;
+    }
+  });
+
+  cvox.ExtensionBridge.addMessageListener(this.onMessage_);
+
   document.addEventListener(
       'keydown', cvox.ChromeVoxKbHandler.basicKeyDownActionsListener, true);
   cvox.ChromeVoxKbHandler.commandHandler = this.onGotCommand.bind(this);
 
   // Classic keymap.
   cvox.ChromeVoxKbHandler.handlerKeyMap = cvox.KeyMap.fromDefaults();
+
+  chrome.automation.addTreeChangeObserver(this.onTreeChange);
 };
 
 Background.prototype = {
@@ -157,15 +149,6 @@ Background.prototype = {
       return;
 
     this.currentRange_ = value;
-  },
-
-  /**
-   * Handles all setup once a new automation tree appears.
-   * @param {chrome.automation.AutomationNode} desktop
-   */
-  onGotDesktop: function(desktop) {
-    // Register a tree change observer.
-    chrome.automation.addTreeChangeObserver(this.onTreeChange);
   },
 
   /**
@@ -364,6 +347,17 @@ Background.prototype = {
       case 'showOptionsPage':
         chrome.runtime.openOptionsPage();
         break;
+      case 'toggleChromeVox':
+        if (cvox.ChromeVox.isChromeOS)
+          return false;
+
+        cvox.ChromeVox.isActive = !cvox.ChromeVox.isActive;
+        if (!cvox.ChromeVox.isActive) {
+          var msg = Msgs.getMsg('chromevox_inactive');
+          cvox.ChromeVox.tts.speak(msg, cvox.QueueMode.FLUSH);
+          return false;
+        }
+        break;
       case 'toggleChromeVoxVersion':
         var newMode;
         if (this.mode_ == ChromeVoxMode.FORCE_NEXT) {
@@ -491,7 +485,7 @@ Background.prototype = {
    * @param {chrome.automation.TreeChange} treeChange
    */
   onTreeChange: function(treeChange) {
-    if (this.mode_ === ChromeVoxMode.CLASSIC)
+    if (this.mode_ === ChromeVoxMode.CLASSIC || !cvox.ChromeVox.isActive)
       return;
 
     var node = treeChange.target;
@@ -639,6 +633,29 @@ Background.prototype = {
     if (selectionSpan) {
       var start = text.getSpanStart(selectionSpan);
       actionNode.setSelection(position - start, position - start);
+    }
+  },
+
+  /**
+   * @param {Object} msg A message sent from a content script.
+   * @param {Port} port
+   * @private
+   */
+  onMessage_: function(msg, port) {
+    var target = msg['target'];
+    var action = msg['action'];
+
+    switch (target) {
+      case 'next':
+        if (action == 'getIsClassicEnabled') {
+          var url = msg['url'];
+          var isClassicEnabled = this.shouldEnableClassicForUrl_(url);
+          port.postMessage({
+            target: 'next',
+            isClassicEnabled: isClassicEnabled
+          });
+        }
+        break;
     }
   }
 };
