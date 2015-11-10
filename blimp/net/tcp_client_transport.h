fa08d932e267496b85e5174bfb057219a9ffaaf5
kmarshall <kmarshall@chromium.org>
2015-11-10 11:53:24 -0800

    Change BlimpTransport to vend BlimpConnections. This removes the dependency StreamSocket from BlimpTransport. Make PacketReader and PacketWriter stream/datagram agnostic, moving framing logic to the new classes StreamPacketReader and StreamPacketWriter. Add channel reader/writer objects and fields to BlimpConnection, and create a specialization of BlimpConnection for receiving ownership of StreamSocket.
    R=wez@chromium.org
    BUG=
    
    Committed: https://crrev.com/90e22697d8d163452c067e88c2bae8b476380077
    Cr-Commit-Position: refs/heads/master@{#358142}
    
    Review URL: https://codereview.chromium.org/1430963002
    
    Cr-Commit-Position: refs/heads/master@{#358881}

--- blimp/net/tcp_client_transport.h
+++ blimp/net/tcp_client_transport.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 class NetLog;
+class StreamSocket;
 }  // namespace net
 
 namespace blimp {
@@ -37,7 +38,7 @@ class TCPClientTransport : public BlimpTransport {
 
   net::AddressList addresses_;
   net::NetLog* net_log_;
-  scoped_ptr<BlimpConnection> connection_;
+  scoped_ptr<net::StreamSocket> socket_;
   net::CompletionCallback connect_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(TCPClientTransport);
