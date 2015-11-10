0db2c07a824cc31d8a27a0b6989ca75b76f2ae16
hcarmona <hcarmona@chromium.org>
2015-11-10 14:05:45 -0800

    Add the initial page for the passwords and autofill settings.
    Adds strings from the mock and a page with the headings that will be
    filled in with the passwords and autofill sub-sections.
    
    BUG=546836
    
    Review URL: https://codereview.chromium.org/1421603006
    
    Cr-Commit-Position: refs/heads/master@{#358923}

---
+++ chrome/browser/resources/settings/passwords_and_forms_page/passwords_and_forms_page.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview 'settings-passwords-and-forms-page' is the settings page
+ * for passwords and auto fill.
+ *
+ * @group Chrome Settings Elements
+ * @element settings-passwords-and-forms-page
+ */
+(function() {
+'use strict';
+
+Polymer({
+  is: 'settings-passwords-and-forms-page',
+});
+})();
