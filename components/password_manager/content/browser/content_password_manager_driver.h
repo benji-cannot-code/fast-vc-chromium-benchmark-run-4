e22a07ee4fc3070126151587454ecc8a64928ef0
vabr <vabr@chromium.org>
2015-11-11 08:36:49 -0800

    Add #includes to password code
    These were suggested by git cl lint. Let's get them sorted out so that git cl lint can be used on other CLs without an avalanche of unrelated warnings.
    
    R=vasilii@chromium.org
    BUG=549167
    
    Review URL: https://codereview.chromium.org/1435883002
    
    Cr-Commit-Position: refs/heads/master@{#359099}

--- components/password_manager/content/browser/content_password_manager_driver.h
+++ components/password_manager/content/browser/content_password_manager_driver.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PASSWORD_MANAGER_CONTENT_BROWSER_CONTENT_PASSWORD_MANAGER_DRIVER_H_
 #define COMPONENTS_PASSWORD_MANAGER_CONTENT_BROWSER_CONTENT_PASSWORD_MANAGER_DRIVER_H_
 
+#include <map>
+#include <vector>
+
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "components/autofill/core/common/password_form_field_prediction_map.h"
--- components/password_manager/core/browser/password_form_manager.cc
+++ components/password_manager/core/browser/password_form_manager.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_form_manager.h"
 
 #include <algorithm>
+#include <map>
 #include <set>
 
 #include "base/metrics/histogram_macros.h"
