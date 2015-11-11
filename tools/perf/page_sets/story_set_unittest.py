b646d84c82264a8e0edd93c9c4b92b7f853e62df
jam <jam@chromium.org>
2015-11-10 21:59:34 -0800

    Disable flaky page_sets.story_set_unittest.StorySetUnitTest.testSmoke page_sets.story_set_unittest.StorySetUnitTest.testNoPageDefinedSyntheticDelay
    BUG=513086
    TBR=hinoka@chromium.org
    
    Review URL: https://codereview.chromium.org/1439513002
    
    Cr-Commit-Position: refs/heads/master@{#359058}

--- tools/perf/page_sets/story_set_unittest.py
+++ tools/perf/page_sets/story_set_unittest.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright 2014 The Chromium Authors. All rights reserved.
+﻿# Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -19,12 +19,14 @@ class StorySetUnitTest(story_set_smoke_test.StorySetSmokeTest):
     self.top_level_dir = os.path.dirname(self.story_sets_dir)
 
   # TODO(tbarzic): crbug.com/386416.
-  @decorators.Disabled('chromeos')
+  # http://crbug.com/513086
+  @decorators.Disabled('all')
   def testSmoke(self):
     self.RunSmokeTest(self.story_sets_dir, self.top_level_dir)
 
   # TODO(nednguyen): Remove this test once crbug.com/508538 is fixed.
-  @decorators.Disabled('chromeos')
+  # http://crbug.com/513086
+  @decorators.Disabled('all')
   def testNoPageDefinedSyntheticDelay(self):
     for story_set_class in self.GetAllStorySetClasses(self.story_sets_dir,
                                                       self.top_level_dir):
