74cd51c778c15f3c6686f0b491a00bdfcf66dc6d
tkent <tkent@chromium.org>
2015-11-11 08:40:26 -0800

    Show form validation message as a fallback tooltip.
    If a form control has no |title| content attribute, show a validation message as a
    tooltip.
    The new behavior is compatible with Firefox.
    
    BUG=550493
    
    Review URL: https://codereview.chromium.org/1435773002
    
    Cr-Commit-Position: refs/heads/master@{#359100}

--- third_party/WebKit/Source/core/html/forms/InputType.cpp
+++ third_party/WebKit/Source/core/html/forms/InputType.cpp
@@ -690,7 +690,7 @@ bool InputType::supportsReadOnly() const
 
 String InputType::defaultToolTip() const
 {
-    return String();
+    return validationMessage();
 }
 
 Decimal InputType::findClosestTickMarkValue(const Decimal&)
