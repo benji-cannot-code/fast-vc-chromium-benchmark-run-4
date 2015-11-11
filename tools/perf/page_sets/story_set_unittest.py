5987e591962cc7fb3a3c1adfda7cb2fff0396652
nednguyen <nednguyen@google.com>
2015-11-11 10:42:16 -0800

    [tools/perf] Ignore CloudStorateError for page_sets.story_set_unittest.StorySetUnitTest.testSmoke  and page_sets.story_set_unittest.StorySetUnitTest.testNoPageDefinedSyntheticDelay instead of disabling them both.
    Add logging that notify when these errors are swallowed so we know whether this
    still happens on bots.
    
    TBR=jam@chromium.org
    
    BUG=513086
    
    Review URL: https://codereview.chromium.org/1439613005
    
    Cr-Commit-Position: refs/heads/master@{#359128}

--- tools/perf/page_sets/story_set_unittest.py
+++ tools/perf/page_sets/story_set_unittest.py
@@ -4,12 +4,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import os
+import logging
 
 import page_sets
 
 from telemetry import decorators
 from telemetry import page
 from telemetry.testing import story_set_smoke_test
+
+from catapult_base import cloud_storage
 
 
 class StorySetUnitTest(story_set_smoke_test.StorySetSmokeTest):
@@ -19,10 +22,15 @@ class StorySetUnitTest(story_set_smoke_test.StorySetSmokeTest):
     self.top_level_dir = os.path.dirname(self.story_sets_dir)
 
   # TODO(tbarzic): crbug.com/386416.
-  # http://crbug.com/513086
-  @decorators.Disabled('all')
+  @decorators.Disabled('chromeos')
   def testSmoke(self):
-    self.RunSmokeTest(self.story_sets_dir, self.top_level_dir)
+    try:
+      self.RunSmokeTest(self.story_sets_dir, self.top_level_dir)
+    # Swallow cloud_storage.CredentialsError due to http://crbug.com/513086.
+    # TODO(nednguyen): remove the try except block once that bug is fixed.
+    except cloud_storage.CredentialsError:
+      logging.warning(
+          'CredentialsError was ignored for StorySetUnitTest.testSmoke')
 
   # TODO(nednguyen): Remove this test once crbug.com/508538 is fixed.
   # http://crbug.com/513086
@@ -31,12 +39,18 @@ class StorySetUnitTest(story_set_smoke_test.StorySetSmokeTest):
     for story_set_class in self.GetAllStorySetClasses(self.story_sets_dir,
                                                       self.top_level_dir):
       if story_set_class is page_sets.ToughSchedulingCasesPageSet:
-       continue
-      story_set = story_set_class()
-      for story in story_set:
-        if isinstance(story, page.Page):
-          self.assertFalse(
-            story.synthetic_delays,
-            'Page %s in page set %s has non empty synthetic delay. '
-            'Synthetic delay is no longer supported. See crbug.com/508538.' %
-            (story.display_name, story_set.Name()))
+        continue
+      try:
+        story_set = story_set_class()
+        for story in story_set:
+          if isinstance(story, page.Page):
+            self.assertFalse(
+              story.synthetic_delays,
+              'Page %s in page set %s has non empty synthetic delay. '
+              'Synthetic delay is no longer supported. See crbug.com/508538.' %
+              (story.display_name, story_set.Name()))
+      # Swallow cloud_storage.CredentialsError due to http://crbug.com/513086.
+      # TODO(nednguyen): remove the try except block once that bug is fixed.
+      except cloud_storage.CredentialsError:
+        logging.warning('CredentialsError was ignored for '
+                        'StorySetUnitTest.testNoPageDefinedSyntheticDelay')
