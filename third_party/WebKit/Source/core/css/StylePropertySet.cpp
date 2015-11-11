3a55544c513091d8b18f9e19937efdf9e1897de1
peria <peria@chromium.org>
2015-11-10 20:17:33 -0800

    Replace a raw pointer to RawPtrWillBeMember to estimate size explicitly.
    This is a follow-up CL of https://crrev.com/7818e07434df5da87d6cdfe0d30830bf2191097e .
    
    BUG=550325
    
    Review URL: https://codereview.chromium.org/1433843002
    
    Cr-Commit-Position: refs/heads/master@{#359049}

--- third_party/WebKit/Source/core/css/StylePropertySet.cpp
+++ third_party/WebKit/Source/core/css/StylePropertySet.cpp
@@ -43,7 +43,7 @@ namespace blink {
 
 static size_t sizeForImmutableStylePropertySetWithPropertyCount(unsigned count)
 {
-    return sizeof(ImmutableStylePropertySet) - sizeof(void*) + sizeof(CSSValue*) * count + sizeof(StylePropertyMetadata) * count;
+    return sizeof(ImmutableStylePropertySet) - sizeof(void*) + sizeof(RawPtrWillBeMember<CSSValue>) * count + sizeof(StylePropertyMetadata) * count;
 }
 
 PassRefPtrWillBeRawPtr<ImmutableStylePropertySet> ImmutableStylePropertySet::create(const CSSProperty* properties, unsigned count, CSSParserMode cssParserMode)
