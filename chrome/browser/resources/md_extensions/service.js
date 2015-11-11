570a7261e0b349c1b057b10f916ee4d7a813ebac
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-11-10 17:09:27 -0800

    [MD Extensions] Support inspecting extension views
    Add functionality to inspect extension views. Also properly display the inspect
    view link (using the path instead of the full chrome-extension://<id>/ url).
    
    BUG=529395
    
    Review URL: https://codereview.chromium.org/1417703014
    
    Cr-Commit-Position: refs/heads/master@{#358991}

--- chrome/browser/resources/md_extensions/service.js
+++ chrome/browser/resources/md_extensions/service.js
@@ -164,6 +164,16 @@ cr.define('extensions', function() {
     },
 
     /** @override */
+    inspectItemView: function(id, view) {
+      chrome.developerPrivate.openDevTools({
+        extensionId: id,
+        renderProcessId: view.renderProcessId,
+        renderViewId: view.renderViewId,
+        incognito: view.incognito,
+      });
+    },
+
+    /** @override */
     setProfileInDevMode: function(inDevMode) {
       chrome.developerPrivate.updateProfileConfiguration(
           {inDeveloperMode: inDevMode});
