546fec3a424aef840cc2dfb36f0033dc3567d8ac
dgozman <dgozman@chromium.org>
2015-11-11 13:30:40 -0800

    [DevTools] Initial Device Mode v2 implementation.
    BUG=540864
    
    Review URL: https://codereview.chromium.org/1413093008
    
    Cr-Commit-Position: refs/heads/master@{#359160}

--- third_party/WebKit/Source/devtools/front_end/network/NetworkConfigView.js
+++ third_party/WebKit/Source/devtools/front_end/network/NetworkConfigView.js
@@ -57,6 +57,7 @@ WebInspector.NetworkConfigView.prototype = {
         this._autoUserAgent.type = "text";
         this._autoUserAgent.disabled = true;
         WebInspector.multitargetNetworkManager.addEventListener(WebInspector.MultitargetNetworkManager.Events.UserAgentChanged, this._userAgentChanged, this);
+        this._userAgentChanged();
 
         radio = createRadioLabel("custom-user-agent", WebInspector.UIString("Custom user agent"), false);
         this._customUserAgentSetting = WebInspector.settings.createSetting("customUserAgent", "");
@@ -74,12 +75,9 @@ WebInspector.NetworkConfigView.prototype = {
         this._userAgentTypeChanged(this._autoUserAgentRadio);
     },
 
-    /**
-     * @param {!WebInspector.Event} event
-     */
-    _userAgentChanged: function(event)
+    _userAgentChanged: function()
     {
-        this._autoUserAgent.value = /** @type {string} */(event.data) || WebInspector.UIString("Default");
+        this._autoUserAgent.value = WebInspector.multitargetNetworkManager.userAgentOverride() || WebInspector.UIString("Default");
     },
 
     _customUserAgentChanged: function()
