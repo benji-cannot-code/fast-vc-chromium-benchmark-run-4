b52c7b7868c769f60e7cee776113420888f61fac
tkent <tkent@chromium.org>
2015-11-11 09:03:52 -0800

    LABEL elements with tabindex should be focusable.
    Remove layoutObjectIsFocusable() override, and do not focus on labeled control
    in focus() if the LABEL element is focusable.
    The new behavior matches to Firefox.
    
    BUG=443725
    
    Review URL: https://codereview.chromium.org/1435803002
    
    Cr-Commit-Position: refs/heads/master@{#359102}

--- third_party/WebKit/LayoutTests/fast/events/resources/tabindex-focus-blur-all.js
+++ third_party/WebKit/LayoutTests/fast/events/resources/tabindex-focus-blur-all.js
@@ -54,7 +54,7 @@ function test()
     var homeBase = window.frames[1].document.getElementsByClassName('homebase');
     homeBase[0].focus();
 
-    var resultSummary = focusCount+" focus / "+blurCount+" blur events dispatched, and should be 333 / 333 ";
+    var resultSummary = focusCount+" focus / "+blurCount+" blur events dispatched, and should be 336 / 336 ";
     resultSummary += (focusCount==blurCount) ? "<span style='color:green'>PASSED</span><br>" : "<span style='color:red'>FAILED</span><br>";
     resultSummary += "Total of "+failedTestCount+" focus test(s) failed.";
     if (failedTestCount)
@@ -100,7 +100,7 @@ Array.prototype.find = function(element) {
 function testProgrammaticFocus(elem)
 {
     var elemThatShouldFocus = null;
-    var OKtoFocusOtherElement = false;
+    var shouldFocusOtherElement = false;
     focusedElem = null;
 
     if (elem.tabIndex == elem.getAttribute("tabindex")) // this means tabindex was explicitly set
@@ -116,13 +116,11 @@ function testProgrammaticFocus(elem)
     if (elem.tabIndex == -1 && elem.tagName == "AREA")
         elemThatShouldFocus = null;
 
-    if (tagNamesTransferFocused.find(elem.tagName)) {
-        elemThatShouldFocus = null;
-        OKtoFocusOtherElement = true;
-    }
+    if (tagNamesTransferFocused.find(elem.tagName) && elemThatShouldFocus == null)
+        shouldFocusOtherElement = true;
 
     elem.focus();
-    if (elemThatShouldFocus == focusedElem || (!elemThatShouldFocus && OKtoFocusOtherElement))
+    if ((!shouldFocusOtherElement && elemThatShouldFocus == focusedElem) || (shouldFocusOtherElement && elem != focusedElem))
         printToConsole("<"+elem.tagName+"> "+elem.id+" PASSED focus test");
     else {
         failedTestCount++;
