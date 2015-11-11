f0626ce44f38f931e0c72cb775a13a7561c5eacd
mcdavid <mcdavid@amazon.com>
2015-11-10 17:01:21 -0800

    ChromePublic: Remove text formatting when pasting into the find in page query text box
    Removes any styling from copied text when pasted into the find in page
    query box, similar to what is now being done for the omnibox.
    
    BUG=553758
    
    Review URL: https://codereview.chromium.org/1418303004
    
    Cr-Commit-Position: refs/heads/master@{#358989}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/findinpage/FindToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/findinpage/FindToolbar.java
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.widget.findinpage;
 
 import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.graphics.Rect;
 import android.os.Handler;
@@ -14,6 +16,7 @@ import android.provider.Settings;
 import android.support.v4.view.accessibility.AccessibilityEventCompat;
 import android.text.Editable;
 import android.text.InputType;
+import android.text.Selection;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.ActionMode;
@@ -122,6 +125,39 @@ public class FindToolbar extends LinearLayout
                 return true;
             }
             return super.onKeyDown(keyCode, event);
+        }
+
+        @Override
+        public boolean onTextContextMenuItem(int id) {
+            if (id == android.R.id.paste) {
+                ClipboardManager clipboard = (ClipboardManager) getContext()
+                        .getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clipData = clipboard.getPrimaryClip();
+                if (clipData != null) {
+                    // Convert the clip data to a simple string
+                    StringBuilder builder = new StringBuilder();
+                    for (int i = 0; i < clipData.getItemCount(); i++) {
+                        builder.append(clipData.getItemAt(i).coerceToText(getContext()));
+                    }
+
+                    // Identify how much of the original text should be replaced
+                    int min = 0;
+                    int max = getText().length();
+
+                    if (isFocused()) {
+                        final int selStart = getSelectionStart();
+                        final int selEnd = getSelectionEnd();
+
+                        min = Math.max(0, Math.min(selStart, selEnd));
+                        max = Math.max(0, Math.max(selStart, selEnd));
+                    }
+
+                    Selection.setSelection(getText(), max);
+                    getText().replace(min, max, builder.toString());
+                    return true;
+                }
+            }
+            return super.onTextContextMenuItem(id);
         }
     }
 
