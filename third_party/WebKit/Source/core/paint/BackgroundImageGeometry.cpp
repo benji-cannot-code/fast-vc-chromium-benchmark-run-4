cdde4b3f9a3f4e24856642f6df20b649656b8e7d
davve <davve@opera.com>
2015-11-11 08:57:07 -0800

    Avoid truncation to int when setting background-position coordinates
    This error was part of the original
    https://bugs.webkit.org/show_bug.cgi?id=103440 implementation of
    background-position but then only for non-repeating backgrounds. When
    also used for repeating backgrounds through
    https://codereview.chromium.org/1287293002 it has been noticed that
    the background-position was sometimes a pixel off when zooming.
    
    BUG=535504
    
    Review URL: https://codereview.chromium.org/1431103005
    
    Cr-Commit-Position: refs/heads/master@{#359101}

--- third_party/WebKit/Source/core/paint/BackgroundImageGeometry.cpp
+++ third_party/WebKit/Source/core/paint/BackgroundImageGeometry.cpp
@@ -303,7 +303,7 @@ void BackgroundImageGeometry::calculate(const LayoutBoxModelObject& obj, const L
     }
 
     if (backgroundRepeatX == RepeatFill) {
-        int xOffset = fillLayer.backgroundXOrigin() == RightEdge ? availableWidth - computedXPosition : computedXPosition;
+        LayoutUnit xOffset = fillLayer.backgroundXOrigin() == RightEdge ? availableWidth - computedXPosition : computedXPosition;
         setPhaseX(tileSize().width() ? tileSize().width() - roundToInt(xOffset + left) % tileSize().width() : 0);
         setSpaceSize(IntSize());
     } else if (backgroundRepeatX == SpaceFill && fillTileSize.width() > 0) {
@@ -319,13 +319,13 @@ void BackgroundImageGeometry::calculate(const LayoutBoxModelObject& obj, const L
         }
     }
     if (backgroundRepeatX == NoRepeatFill) {
-        int xOffset = fillLayer.backgroundXOrigin() == RightEdge ? availableWidth - computedXPosition : computedXPosition;
+        LayoutUnit xOffset = fillLayer.backgroundXOrigin() == RightEdge ? availableWidth - computedXPosition : computedXPosition;
         setNoRepeatX(left + xOffset);
         setSpaceSize(IntSize(0, spaceSize().height()));
     }
 
     if (backgroundRepeatY == RepeatFill) {
-        int yOffset = fillLayer.backgroundYOrigin() == BottomEdge ? availableHeight - computedYPosition : computedYPosition;
+        LayoutUnit yOffset = fillLayer.backgroundYOrigin() == BottomEdge ? availableHeight - computedYPosition : computedYPosition;
         setPhaseY(tileSize().height() ? tileSize().height() - roundToInt(yOffset + top) % tileSize().height() : 0);
         setSpaceSize(IntSize(spaceSize().width(), 0));
     } else if (backgroundRepeatY == SpaceFill && fillTileSize.height() > 0) {
@@ -341,7 +341,7 @@ void BackgroundImageGeometry::calculate(const LayoutBoxModelObject& obj, const L
         }
     }
     if (backgroundRepeatY == NoRepeatFill) {
-        int yOffset = fillLayer.backgroundYOrigin() == BottomEdge ? availableHeight - computedYPosition : computedYPosition;
+        LayoutUnit yOffset = fillLayer.backgroundYOrigin() == BottomEdge ? availableHeight - computedYPosition : computedYPosition;
         setNoRepeatY(top + yOffset);
         setSpaceSize(IntSize(spaceSize().width(), 0));
     }
