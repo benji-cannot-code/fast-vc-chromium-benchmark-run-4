5cf188beba5f45999df5290cf652ee0122097610
jam <jam@chromium.org>
2015-11-11 13:18:12 -0800

    Fix tracing message pipe outliving the IO thread.
    MessagePipeDispatcher's close methods posts a task to the IO thread to destruct the RawChannel (with the new EDK). This means that we need to ensure that Tracer is destructed before the IO thread.
    
    I've moved the initialization of tracing to inside Context to reduce code duplication.
    
    BUG=478251
    
    Review URL: https://codereview.chromium.org/1437963002
    
    Cr-Commit-Position: refs/heads/master@{#359156}

--- mojo/runner/context.h
+++ mojo/runner/context.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "mojo/runner/scoped_user_data_dir.h"
 #include "mojo/runner/task_runners.h"
+#include "mojo/runner/tracer.h"
 #include "mojo/shell/application_manager.h"
 #include "third_party/mojo/src/mojo/edk/embedder/process_delegate.h"
 #include "url/gurl.h"
@@ -25,14 +26,13 @@ class PackageManagerImpl;
 namespace runner {
 
 class NativeApplicationLoader;
-class Tracer;
 
 // The "global" context for the shell's main process.
 // TODO(use_chrome_edk)
 //class Context : public edk::ProcessDelegate {
 class Context : public embedder::ProcessDelegate {
  public:
-  Context(const base::FilePath& shell_file_root, Tracer* tracer);
+  Context();
   ~Context() override;
 
   static void EnsureEmbedderIsInitialized();
@@ -40,7 +40,7 @@ class Context : public embedder::ProcessDelegate {
   // This must be called with a message loop set up for the current thread,
   // which must remain alive until after Shutdown() is called. Returns true on
   // success.
-  bool Init();
+  bool Init(const base::FilePath& shell_file_root);
 
   // If Init() was called and succeeded, this must be called before destruction.
   void Shutdown();
@@ -74,8 +74,9 @@ class Context : public embedder::ProcessDelegate {
   ScopedUserDataDir scoped_user_data_dir;
   std::set<GURL> app_urls_;
   scoped_ptr<TaskRunners> task_runners_;
-  base::FilePath shell_file_root_;
-  Tracer* const tracer_;
+  // Ensure this is destructed before task_runners_ since it owns a message pipe
+  // that needs the IO thread to destruct cleanly.
+  Tracer tracer_;
   // Owned by |application_manager_|.
   package_manager::PackageManagerImpl* package_manager_;
   scoped_ptr<shell::ApplicationManager> application_manager_;
