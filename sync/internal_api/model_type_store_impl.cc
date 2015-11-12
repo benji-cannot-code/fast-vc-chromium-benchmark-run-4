c819118e40b9e8979f3fb52fda076f1a85228b20
pavely <pavely@chromium.org>
2015-11-12 15:55:27 -0800

    [Sync] Implementation of ModelTypeStoreBackend
    I'm adding implementation of ModelTypeStoreBackend. The object lives on
    backend thread and performs actual calls to leveldb. ModelTypeStoreImpl
    will use interface of backend to work with database.
    
    Implementation of ModelTypeStoreImpl methods that work with this backend
    will be landed in separate CL.
    
    R=stanisc@chromium.org
    BUG=517663
    TEST=Covered by sync_unit_tests. No externally observable behavior.
    
    Review URL: https://codereview.chromium.org/1436573002
    
    Cr-Commit-Position: refs/heads/master@{#359442}

--- sync/internal_api/model_type_store_impl.cc
+++ sync/internal_api/model_type_store_impl.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_runner_util.h"
 #include "base/thread_task_runner_handle.h"
 #include "sync/internal_api/public/model_type_store_backend.h"
+#include "third_party/leveldatabase/src/include/leveldb/env.h"
 
 namespace syncer_v2 {
 
@@ -45,10 +46,22 @@ void ModelTypeStoreImpl::CreateInMemoryStoreForTest(
       base::ThreadTaskRunnerHandle::Get();
 
   scoped_ptr<ModelTypeStoreBackend> backend(new ModelTypeStoreBackend());
+  scoped_ptr<leveldb::Env> env = ModelTypeStoreBackend::CreateInMemoryEnv();
+
+  std::string path;
+  env->GetTestDirectory(&path);
+  path += "/in-memory";
+
+  // Env ownership will be passed to backend, but we still need to keep pointer
+  // for Init call.
+  leveldb::Env* env_ptr = env.get();
+  backend->TakeEnvOwnership(env.Pass());
   scoped_ptr<ModelTypeStoreImpl> store(
       new ModelTypeStoreImpl(backend.Pass(), task_runner));
-  auto task = base::Bind(&ModelTypeStoreBackend::Init,
-                         base::Unretained(store->backend_.get()));
+
+  auto task =
+      base::Bind(&ModelTypeStoreBackend::Init,
+                 base::Unretained(store->backend_.get()), path, env_ptr);
   auto reply = base::Bind(&ModelTypeStoreImpl::BackendInitDone, callback,
                           base::Passed(&store));
 
--- sync/sync_tests.gypi
+++ sync/sync_tests.gypi
@@ -301,6 +301,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'internal_api/js_mutation_event_observer_unittest.cc',
         'internal_api/js_sync_encryption_handler_observer_unittest.cc',
         'internal_api/js_sync_manager_observer_unittest.cc',
+        'internal_api/model_type_store_backend_unittest.cc',
         'internal_api/model_type_store_impl_unittest.cc',
         'internal_api/protocol_event_buffer_unittest.cc',
         'internal_api/public/base/attachment_id_proto_unittest.cc',
