6ffc5e609d3a882adf379b40cbc6dd840fdfe437
jam <jam@chromium.org>
2015-11-11 20:39:00 -0800

    Fix racy crash in Mandoline child processes.
    The problem is with the new EDK, the IO task runner pointer needs to be initialized before MojoMessagePump is created (in child_process.cc). The rest of the change is removing the delegate thread from the InitIPCSupport function.
    
    BUG=478251
    
    Review URL: https://codereview.chromium.org/1439863002
    
    Cr-Commit-Position: refs/heads/master@{#359266}

--- third_party/mojo/src/mojo/edk/embedder/embedder_unittest.cc
+++ third_party/mojo/src/mojo/edk/embedder/embedder_unittest.cc
@@ -141,6 +141,7 @@ class EmbedderTest : public testing::Test {
 
   void TearDown() override { EXPECT_TRUE(test::Shutdown()); }
 
+  base::MessageLoop message_loop_;
   base::TestIOThread test_io_thread_;
 
   MOJO_DISALLOW_COPY_AND_ASSIGN(EmbedderTest);
@@ -495,6 +496,7 @@ TEST_F(EmbedderTest, ChannelShutdownRace_MessagePipePassing) {
 }
 
 MOJO_MULTIPROCESS_TEST_CHILD_TEST(MultiprocessMasterSlave) {
+  base::MessageLoop message_loop;
   ScopedPlatformHandle client_platform_handle =
       mojo::test::MultiprocessTestHelper::client_platform_handle.Pass();
   EXPECT_TRUE(client_platform_handle.is_valid());
@@ -690,6 +692,7 @@ TEST_F(EmbedderTest, MAYBE_MultiprocessChannels) {
 }
 
 MOJO_MULTIPROCESS_TEST_CHILD_TEST(MultiprocessChannelsClient) {
+  base::MessageLoop message_loop;
   ScopedPlatformHandle client_platform_handle =
       mojo::test::MultiprocessTestHelper::client_platform_handle.Pass();
   EXPECT_TRUE(client_platform_handle.is_valid());
