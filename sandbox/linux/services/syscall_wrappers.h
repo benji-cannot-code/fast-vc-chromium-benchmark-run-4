ab0f8b95fabe812dbc909fa452cc19a3dcb225da
danakj <danakj@chromium.org>
2015-11-11 19:44:54 -0800

    Replace decltype(nullptr) with std::nullptr_t.
    We have C++11 library support and this is allowed now, so use it.
    
    R=mdempsky@chromium.org
    
    Review URL: https://codereview.chromium.org/1440863003
    
    Cr-Commit-Position: refs/heads/master@{#359248}

--- sandbox/linux/services/syscall_wrappers.h
+++ sandbox/linux/services/syscall_wrappers.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <sys/types.h>
 
+#include <cstddef>
+
 #include "sandbox/sandbox_export.h"
 
 struct sock_fprog;
@@ -33,10 +35,10 @@ SANDBOX_EXPORT long sys_clone(unsigned long flags);
 // nullptr, since otherwise this function cannot safely return. As a
 // consequence, this function does not support CLONE_VM.
 SANDBOX_EXPORT long sys_clone(unsigned long flags,
-                              decltype(nullptr) child_stack,
+                              std::nullptr_t child_stack,
                               pid_t* ptid,
                               pid_t* ctid,
-                              decltype(nullptr) regs);
+                              std::nullptr_t regs);
 
 SANDBOX_EXPORT void sys_exit_group(int status);
 
@@ -72,7 +74,7 @@ SANDBOX_EXPORT int sys_unshare(int flags);
 // because of some ABI gap between toolchain's and Linux's.
 SANDBOX_EXPORT int sys_sigprocmask(int how,
                                    const sigset_t* set,
-                                   decltype(nullptr) oldset);
+                                   std::nullptr_t oldset);
 
 // Some libcs do not expose a sigaction().
 SANDBOX_EXPORT int sys_sigaction(int signum,
