0a53aa99f8f52211ff2b0b0456880da1226b7315
battre <battre@chromium.org>
2015-11-12 13:25:46 -0800

    Remove URLs from chrome.tabs.executeScript permission warning.
    This CL removes the URL error messages triggered by chrome.tabs.executeScript
    when the extension lacks the permission to access the respective host. This is
    necessary to prevent that an extension can see the list of open URLs of all
    tabs without having asked for appropriate permission.
    
    BUG=551626
    
    Review URL: https://codereview.chromium.org/1414223005
    
    Cr-Commit-Position: refs/heads/master@{#359392}

--- extensions/common/permissions/permissions_data.cc
+++ extensions/common/permissions/permissions_data.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/manifest.h"
 #include "extensions/common/manifest_constants.h"
 #include "extensions/common/manifest_handlers/permissions_parser.h"
+#include "extensions/common/permissions/api_permission.h"
 #include "extensions/common/permissions/permission_message_provider.h"
 #include "extensions/common/switches.h"
 #include "extensions/common/url_pattern_set.h"
@@ -90,9 +91,13 @@ bool PermissionsData::IsRestrictedUrl(const GURL& document_url,
   if (!URLPattern::IsValidSchemeForExtensions(document_url.scheme()) &&
       document_url.spec() != url::kAboutBlankURL) {
     if (error) {
-      *error = ErrorUtils::FormatErrorMessage(
-                   manifest_errors::kCannotAccessPage,
-                   document_url.spec());
+      if (extension->permissions_data()->active_permissions().HasAPIPermission(
+              APIPermission::kTab)) {
+        *error = ErrorUtils::FormatErrorMessage(
+            manifest_errors::kCannotAccessPageWithUrl, document_url.spec());
+      } else {
+        *error = manifest_errors::kCannotAccessPage;
+      }
     }
     return true;
   }
@@ -350,9 +355,15 @@ PermissionsData::AccessType PermissionsData::CanRunOnPage(
     return ACCESS_WITHHELD;
 
   if (error) {
-    *error = ErrorUtils::FormatErrorMessage(manifest_errors::kCannotAccessPage,
-                                            document_url.spec());
+    if (extension->permissions_data()->active_permissions().HasAPIPermission(
+            APIPermission::kTab)) {
+      *error = ErrorUtils::FormatErrorMessage(
+          manifest_errors::kCannotAccessPageWithUrl, document_url.spec());
+    } else {
+      *error = manifest_errors::kCannotAccessPage;
+    }
   }
+
   return ACCESS_DENIED;
 }
 
