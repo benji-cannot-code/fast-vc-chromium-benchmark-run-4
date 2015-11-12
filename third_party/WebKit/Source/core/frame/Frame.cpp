2bdceb672ad7b675c285720f60618fed0e095527
sigbjornf <sigbjornf@opera.com>
2015-11-11 18:40:56 -0800

    Oilpan: turn Frame::m_client into the Member it is.
    R=haraken
    BUG=553613
    
    Review URL: https://codereview.chromium.org/1437973002
    
    Cr-Commit-Position: refs/heads/master@{#359210}

--- third_party/WebKit/Source/core/frame/Frame.cpp
+++ third_party/WebKit/Source/core/frame/Frame.cpp
@@ -86,6 +86,7 @@ DEFINE_TRACE(Frame)
     visitor->trace(m_treeNode);
     visitor->trace(m_host);
     visitor->trace(m_owner);
+    visitor->trace(m_client);
 }
 
 void Frame::detach(FrameDetachType type)
