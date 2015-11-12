0d2d73197f744b0a6cf7e81e56b87d6649f7ccdf
lambroslambrou <lambroslambrou@chromium.org>
2015-11-11 18:11:03 -0800

    Android Chromoting: Refactor help-and-feedback implementation.
    Other Google-released apps use a common Help + Feedback system which is
    implemented by a closed-source library and backend. This CL refactors
    the code behind an interface, to allow official builds to substitute an
    alternative implementation.
    
    This also updates the URLs used in the open-source implementation, as
    they appear to redirect to the wrong sections of the Help pages. The
    official backend will use an alternative system that isn't based on
    URLs. To enable this, the help context is abstracted behind a
    HelpContext enumeration.
    
    BUG=548929
    
    Review URL: https://codereview.chromium.org/1406413004
    
    Cr-Commit-Position: refs/heads/master@{#359208}

---
+++ remoting/android/java/src/org/chromium/chromoting/help/HelpContext.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chromoting.help;
+
+/**
+ * Enumeration of contexts from which the user could request help in the application. The
+ * HelpAndFeedback implementation is responsible for displaying an appropriate Help article for
+ * each context.
+ */
+public enum HelpContext {
+  // Help for the host-list screen.
+  HOST_LIST,
+
+  // Help on setting up a new host.
+  HOST_SETUP,
+
+  // Help for the connected desktop screen, including touch gestures.
+  DESKTOP,
+}
