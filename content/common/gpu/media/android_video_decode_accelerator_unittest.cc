a725e952d52d3438c08c066a2b62fd7389a13d83
pkotwicz <pkotwicz@chromium.org>
2015-11-12 12:21:39 -0800

    Fix AndroidVideoDecodeAcceleratorTest.ConfigureSupportedCodec
    TEST=None
    
    Review URL: https://codereview.chromium.org/1434793003
    
    Cr-Commit-Position: refs/heads/master@{#359374}

--- content/common/gpu/media/android_video_decode_accelerator_unittest.cc
+++ content/common/gpu/media/android_video_decode_accelerator_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/message_loop/message_loop.h"
 #include "content/common/gpu/media/android_copying_backing_strategy.h"
 #include "content/common/gpu/media/android_video_decode_accelerator.h"
 #include "gpu/command_buffer/service/gles2_cmd_decoder_mock.h"
@@ -60,6 +61,11 @@ class AndroidVideoDecodeAcceleratorTest : public testing::Test {
     media::RegisterJni(env);
     // TODO(felipeg): fix GL bindings, so that the decoder can perform GL
     // calls.
+
+    // Start message loop because
+    // AndroidVideoDecodeAccelerator::ConfigureMediaCodec() starts a timer task.
+    message_loop_.reset(new base::MessageLoop());
+
     scoped_ptr<gpu::gles2::MockGLES2Decoder> decoder(
         new gpu::gles2::MockGLES2Decoder());
     scoped_ptr<MockVideoDecodeAcceleratorClient> client(
@@ -79,6 +85,7 @@ class AndroidVideoDecodeAcceleratorTest : public testing::Test {
 
  private:
   scoped_ptr<media::VideoDecodeAccelerator> accelerator_;
+  scoped_ptr<base::MessageLoop> message_loop_;
 };
 
 TEST_F(AndroidVideoDecodeAcceleratorTest, ConfigureUnsupportedCodec) {
