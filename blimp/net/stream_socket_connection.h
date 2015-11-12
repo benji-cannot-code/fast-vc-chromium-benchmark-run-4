99904bacf329f16c28bb5587695151f5792c25b8
kmarshall <kmarshall@chromium.org>
2015-11-12 14:29:37 -0800

    Add interfaces for most major Blimp net components.
    This CL contains interfaces and implementation stubs for network components. It will be used as a foundation for implementation work done in parallel.
    Other changes made to existing net code include:
    Rename BlimpMessageReceiver to BlimpMessageProcessor.
    Make BlimpMessageProcessor async-only.
    
    BUG=555012
    R=wez@chromium.org,haibinlu@chromium.org
    
    Committed: https://crrev.com/3f89b699c0672552f6471e6d085c81500b40d4af
    Cr-Commit-Position: refs/heads/master@{#359375}
    
    Review URL: https://codereview.chromium.org/1429193002
    
    Cr-Commit-Position: refs/heads/master@{#359411}

--- blimp/net/stream_socket_connection.h
+++ blimp/net/stream_socket_connection.h
@@ -20,7 +20,8 @@ namespace blimp {
 class StreamSocketConnection : public BlimpConnection {
  public:
   explicit StreamSocketConnection(scoped_ptr<net::StreamSocket> socket);
-  virtual ~StreamSocketConnection();
+
+  ~StreamSocketConnection() override;
 
  private:
   scoped_ptr<net::StreamSocket> socket_;
