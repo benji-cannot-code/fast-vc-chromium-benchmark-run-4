a339d051eae902af9d045e7e65f49aeb7ad6438d
dominickn <dominickn@chromium.org>
2015-11-12 14:59:44 -0800

    Fix a potential heap use after free in BookmarkAppHelper.
    When creating a bookmark app, BookmarkAppHelper performs the initial
    icon downloading and manifest validation, before asking the browser
    window to show a bubble asking the user to confirm. The window is
    provided with a callback object to run once the bubble or its parent window
    closes, which indicates to the helper what occurred. The callback
    references an instance method on the helper.
    
    On Aura platforms, invoking ExitHandler::ExitWhenPossibleOnUIThread()
    with fast tab unload enabled destroys all WebContentsImpls before the
    window widget is destroyed. However, the BookmarkAppHelper class
    lifetime is tied to WebContentsImpl, but the bubble and referring
    callback are tied to the widget, causing a potential, occasional
    heap-use-after-free crash when the helper callback is invoked at
    window widget closing.
    
    This CL addresses the problem by adding a weak pointer factory to
    BookmarkAppHelper, and passing weak pointers to base::Bind when
    creating callbacks. This prevents calls back to BookmarkAppHelper
    after it has been freed. Any other views delegates which override
    WidgetDelegate::WindowClosing and rely on a WebContents scoped
    object must also address this lifetime issue.
    
    BUG=534994
    R=benwells
    
    Review URL: https://codereview.chromium.org/1421863003
    
    Cr-Commit-Position: refs/heads/master@{#359418}

--- chrome/browser/extensions/bookmark_app_helper.h
+++ chrome/browser/extensions/bookmark_app_helper.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/common/web_application_info.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
@@ -155,6 +156,11 @@ class BookmarkAppHelper : public content::NotificationObserver {
   scoped_refptr<extensions::CrxInstaller> crx_installer_;
 
   content::NotificationRegistrar registrar_;
+
+  // With fast tab unloading enabled, shutting down can cause BookmarkAppHelper
+  // to be destroyed before the bookmark creation bubble. Use weak pointers to
+  // prevent a heap-use-after free in this instance (https://crbug.com/534994).
+  base::WeakPtrFactory<BookmarkAppHelper> weak_factory_;
 };
 
 // Creates or updates a bookmark app from the given |web_app_info|. Icons will
