7a8dcb988d059b631f28c9073d7a1fa80409ab72
scottmg <scottmg@chromium.org>
2015-11-11 20:27:02 -0800

    Make about:help show XP/Vista deprecation message on Windows
    Follows https://codereview.chromium.org/1409403006/.
    
    BUG=554726
    R=estade@chromium.org
    
    Review URL: https://codereview.chromium.org/1434103003
    
    Cr-Commit-Position: refs/heads/master@{#359260}

--- chrome/browser/resources/help/help_page.js
+++ chrome/browser/resources/help/help_page.js
@@ -428,7 +428,7 @@ cr.define('help', function() {
      * @private
      */
     setObsoleteSystem_: function(obsolete) {
-      if (cr.isMac && $('update-obsolete-system-container')) {
+      if ($('update-obsolete-system-container')) {
         $('update-obsolete-system-container').hidden = !obsolete;
       }
     },
@@ -439,8 +439,7 @@ cr.define('help', function() {
      * @private
      */
     setObsoleteSystemEndOfTheLine_: function(endOfTheLine) {
-      if (cr.isMac &&
-          $('update-obsolete-system-container') &&
+      if ($('update-obsolete-system-container') &&
           !$('update-obsolete-system-container').hidden &&
           $('update-status-message')) {
         $('update-status-message').hidden = endOfTheLine;
