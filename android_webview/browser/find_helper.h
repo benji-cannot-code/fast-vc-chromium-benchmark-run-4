8bd401a5d614960127ddd86e39251944bd0adbf6
thestig <thestig@chromium.org>
2015-11-12 15:01:33 -0800

    Prevent guest views from issuing a search for empty text.
    Do the same for Android webview.
    
    Set up roadblocks along the way to prevent this from happening again.
    
    BUG=535086
    
    Review URL: https://codereview.chromium.org/1365563003
    
    Cr-Commit-Position: refs/heads/master@{#359419}

--- android_webview/browser/find_helper.h
+++ android_webview/browser/find_helper.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ANDROID_WEBVIEW_BROWSER_FIND_HELPER_H_
 #define ANDROID_WEBVIEW_BROWSER_FIND_HELPER_H_
 
-#include "base/memory/weak_ptr.h"
 #include "content/public/browser/web_contents_observer.h"
 
 namespace android_webview {
@@ -29,7 +28,7 @@ class FindHelper : public content::WebContentsObserver {
   ~FindHelper() override;
 
   // Sets the listener to receive find result updates.
-  // Does not own the listener and must set to NULL when invalid.
+  // Does not own the listener and must set to nullptr when invalid.
   void SetListener(Listener* listener);
 
   // Asynchronous API.
@@ -45,6 +44,7 @@ class FindHelper : public content::WebContentsObserver {
 
  private:
   void StartNewRequest(const base::string16& search_string);
+  bool MaybeHandleEmptySearch(const base::string16& search_string);
   void NotifyResults(int active_ordinal, int match_count, bool finished);
 
   // Listener results are reported to.
@@ -52,7 +52,6 @@ class FindHelper : public content::WebContentsObserver {
 
   // Used to check the validity of FindNext operations.
   bool async_find_started_;
-  bool sync_find_started_;
 
   // Used to provide different ids to each request and for result
   // verification in asynchronous calls.
@@ -63,9 +62,6 @@ class FindHelper : public content::WebContentsObserver {
   base::string16 last_search_string_;
   int last_match_count_;
   int last_active_ordinal_;
-
-  // Used to post synchronous result notifications to ourselves.
-  base::WeakPtrFactory<FindHelper> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(FindHelper);
 };
