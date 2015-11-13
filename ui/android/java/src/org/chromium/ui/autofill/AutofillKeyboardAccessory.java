9c694514747c882abc9a41ff7f472dd4b05125cd
rouslan <rouslan@chromium.org>
2015-11-13 15:25:35 -0800

    Limit the width only for suggestions that can be filled into the form.
    The warning message "Automatic credit card filling is disabled because
    this form does not use a secure connection" should not be truncated to
    50% of the screen width.
    
    BUG=555725
    
    Review URL: https://codereview.chromium.org/1435153006
    
    Cr-Commit-Position: refs/heads/master@{#359686}

--- ui/android/java/src/org/chromium/ui/autofill/AutofillKeyboardAccessory.java
+++ ui/android/java/src/org/chromium/ui/autofill/AutofillKeyboardAccessory.java
@@ -73,10 +73,8 @@ public class AutofillKeyboardAccessory extends LinearLayout
             AutofillSuggestion suggestion = suggestions[i];
             assert !TextUtils.isEmpty(suggestion.getLabel());
 
-            // Negative suggestion ID indiciates a tool like "settings" or "scan credit card."
-            // Non-negative suggestion ID indicates suggestions that can be filled into the form.
             View touchTarget;
-            if (suggestion.getSuggestionId() < 0 && suggestion.getIconId() != 0) {
+            if (!suggestion.isFillable() && suggestion.getIconId() != 0) {
                 touchTarget = LayoutInflater.from(getContext()).inflate(
                         R.layout.autofill_keyboard_accessory_icon, this, false);
 
@@ -91,7 +89,11 @@ public class AutofillKeyboardAccessory extends LinearLayout
 
                 TextView label = (TextView) touchTarget.findViewById(
                         R.id.autofill_keyboard_accessory_item_label);
-                label.setMaxWidth(mMaximumLabelWidthPx);
+
+                if (suggestion.isFillable()) {
+                    label.setMaxWidth(mMaximumLabelWidthPx);
+                }
+
                 label.setText(suggestion.getLabel());
                 if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
                     label.setTypeface(Typeface.DEFAULT_BOLD);
@@ -103,6 +105,7 @@ public class AutofillKeyboardAccessory extends LinearLayout
                 }
 
                 if (!TextUtils.isEmpty(suggestion.getSublabel())) {
+                    assert suggestion.isFillable();
                     TextView sublabel = (TextView) touchTarget.findViewById(
                             R.id.autofill_keyboard_accessory_item_sublabel);
                     sublabel.setText(suggestion.getSublabel());
