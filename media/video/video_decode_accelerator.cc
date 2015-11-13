a4f68f417fdd94efe31d07bdb0a6f7d43df8d5af
xhwang <xhwang@chromium.org>
2015-11-13 15:57:09 -0800

    media: Support SetCdm() on VideoDecodeAccelerator interface.
    This is needed to support encrypted stream handling in VideoDecodeAccelerator
    (VDA) implmentations on certain platforms (e.g. Android).
    
    BUG=545099
    
    Review URL: https://codereview.chromium.org/1438063002
    
    Cr-Commit-Position: refs/heads/master@{#359696}

--- media/video/video_decode_accelerator.cc
+++ media/video/video_decode_accelerator.cc
@@ -10,7 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+void VideoDecodeAccelerator::Client::NotifyCdmAttached(bool success) {
+  NOTREACHED() << "By default CDM is not supported.";
+}
+
 VideoDecodeAccelerator::~VideoDecodeAccelerator() {}
+
+void VideoDecodeAccelerator::SetCdm(int cdm_id) {
+  NOTREACHED() << "By default CDM is not supported.";
+}
 
 bool VideoDecodeAccelerator::CanDecodeOnIOThread() {
   // GPU process subclasses must override this.
