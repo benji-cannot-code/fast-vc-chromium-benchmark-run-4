7e7deec207591f02cd993aab9bd64235e3853296
Ken Rockot <rockot@chromium.org>
2015-11-12 17:50:55 -0800

    Fix third_party/mojo/src/mojo/public refs
    These have now moved to mojo/public.
    
    TBR=sky@chromium.org
    BUG=None
    
    Review URL: https://codereview.chromium.org/1437363002 .
    
    Cr-Commit-Position: refs/heads/master@{#359464}

--- components/mus/public/cpp/lib/in_flight_change.h
+++ components/mus/public/cpp/lib/in_flight_change.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
-#include "third_party/mojo/src/mojo/public/cpp/bindings/array.h"
+#include "mojo/public/cpp/bindings/array.h"
 #include "ui/gfx/geometry/rect.h"
 
 namespace mus {
--- components/mus/public/cpp/lib/window_private.h
+++ components/mus/public/cpp/lib/window_private.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_MUS_PUBLIC_CPP_LIB_WINDOW_PRIVATE_H_
 
 #include "components/mus/public/cpp/window.h"
-#include "third_party/mojo/src/mojo/public/cpp/bindings/array.h"
+#include "mojo/public/cpp/bindings/array.h"
 
 namespace mus {
 
