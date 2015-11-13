7f39ec3ca0b475ab1dbd22ff1051bf5e376e97e5
imcheng <imcheng@chromium.org>
2015-11-13 14:12:50 -0800

    [MR tests] The last patch to fix integration test.
    Fixed integration test script to fall back to listening for
    availability, because it may return false initially if SinkAvailability
    is UNAVAILABLE.
    
    BUG=554148
    
    Review URL: https://codereview.chromium.org/1441793006
    
    Cr-Commit-Position: refs/heads/master@{#359659}

--- chrome/test/media_router/resources/common.js
+++ chrome/test/media_router/resources/common.js
@@ -31,7 +31,10 @@ function waitUntilDeviceAvailable() {
     if (availability.value) {
       sendResult(true, '');
     } else {
-      sendResult(false, 'device unavailable');
+      availability.onchange = function(newAvailability) {
+        if (newAvailability)
+          sendResult(true, '');
+      }
     }
   }).catch(function(){
     sendResult(false, 'got error');
