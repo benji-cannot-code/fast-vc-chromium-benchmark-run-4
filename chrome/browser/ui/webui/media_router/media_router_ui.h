35718c01ce242c0eba81dab5d513ec2e83e02710
jlklein <jlklein@chromium.org>
2015-11-13 14:09:40 -0800

    Add extension name support to the media router ui.
    BUG=552437
    R=mfoltz@chromium.org
    
    Review URL: https://codereview.chromium.org/1435213002
    
    Cr-Commit-Position: refs/heads/master@{#359654}

--- chrome/browser/ui/webui/media_router/media_router_ui.h
+++ chrome/browser/ui/webui/media_router/media_router_ui.h
@@ -28,6 +28,10 @@ namespace content {
 class WebContents;
 }  // namespace content
 
+namespace extensions {
+class ExtensionRegistry;
+}  // namespace extensions
+
 namespace media_router {
 
 class IssuesObserver;
@@ -105,7 +109,8 @@ class MediaRouterUI : public ConstrainedWebDialogUI,
   void ClearIssue(const Issue::Id& issue_id);
 
   // Returns the hostname of the default source's parent frame URL.
-  std::string GetFrameURLHost() const;
+  std::string GetPresentationRequestSourceName() const;
+  std::string GetTruncatedPresentationRequestSourceName() const;
   bool HasPendingRouteRequest() const {
     return current_route_request_id_ != -1;
   }
@@ -120,6 +125,11 @@ class MediaRouterUI : public ConstrainedWebDialogUI,
  private:
   FRIEND_TEST_ALL_PREFIXES(MediaRouterUITest,
                            UIMediaRoutesObserverFiltersNonDisplayRoutes);
+  FRIEND_TEST_ALL_PREFIXES(MediaRouterUITest, GetExtensionNameExtensionPresent);
+  FRIEND_TEST_ALL_PREFIXES(MediaRouterUITest,
+                           GetExtensionNameEmptyWhenNotInstalled);
+  FRIEND_TEST_ALL_PREFIXES(MediaRouterUITest,
+                           GetExtensionNameEmptyWhenNotExtensionURL);
 
   class UIIssuesObserver;
   class UIMediaRoutesObserver : public MediaRoutesObserver {
@@ -139,6 +149,9 @@ class MediaRouterUI : public ConstrainedWebDialogUI,
 
     DISALLOW_COPY_AND_ASSIGN(UIMediaRoutesObserver);
   };
+
+  static std::string GetExtensionName(const GURL& url,
+                                      extensions::ExtensionRegistry* registry);
 
   // QueryResultManager::Observer
   void OnResultsUpdated(
