f45b24aa815afb87c18f864441882eef15fb24b6
wychen <wychen@chromium.org>
2015-11-13 00:03:28 -0800

    Add a new distillability model for the new set of page features
    This model is for the new set of features derived from
    blink::WebDistillabilityFeatures (added in http://crrev.com/1419033004/).
    
    The old features are extracted by JavaScript code in an isolated world,
    so it's slow and consumes more memory than we could afford. Even if we
    extract the old features in native code, it will still be too slow, so
    we switch to this new set of features.
    
    BUG=509869, 471439
    
    Review URL: https://codereview.chromium.org/1405233009
    
    Cr-Commit-Position: refs/heads/master@{#359512}

--- components/dom_distiller/core/distillable_page_detector.h
+++ components/dom_distiller/core/distillable_page_detector.h
@@ -20,6 +20,7 @@ namespace dom_distiller {
 class DistillablePageDetector {
  public:
   static const DistillablePageDetector* GetDefault();
+  static const DistillablePageDetector* GetNewModel();
   explicit DistillablePageDetector(scoped_ptr<AdaBoostProto> proto);
   ~DistillablePageDetector();
 
