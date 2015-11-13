dfaad036d467bd50ca2a022361c87f7671ff9b3b
jbroman <jbroman@chromium.org>
2015-11-12 18:12:11 -0800

    Remove Tools/Scripts/webkitruby/check-for-inappropriate-macros-in-external-headers-tests.
    The tool that they test no longer exists.
    
    Review URL: https://codereview.chromium.org/1443523002
    
    Cr-Commit-Position: refs/heads/master@{#359477}

--- third_party/WebKit/Tools/Scripts/webkitruby/check-for-inappropriate-macros-in-external-headers-tests/resources/Fake.framework/Headers/Pass.h
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// A macro word in a #error should not matter, that is just a coincidence.
-#error PLATFORM
-
-// There are references to a OS2, but that is not the OS() macro.
-#if defined(__OS2__) || defined(OS2)
-#endif
