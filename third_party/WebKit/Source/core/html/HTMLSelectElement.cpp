e63ea662faa94033f40b310556ba7ca83b1dc453
tkent <tkent@chromium.org>
2015-11-12 18:09:55 -0800

    Dragging into a listbox SELECT element from another element should not clear existing selection.
    We should not change selection in mousemove handling if mousedown event didn't
    happen in the element.
    
    BUG=507601
    
    Review URL: https://codereview.chromium.org/1435093004
    
    Cr-Commit-Position: refs/heads/master@{#359474}

--- third_party/WebKit/Source/core/html/HTMLSelectElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLSelectElement.cpp
@@ -1069,6 +1069,7 @@ void HTMLSelectElement::dispatchBlurEvent(Element* newFocusedElement, WebFocusTy
     // This matches other browsers' behavior.
     if (usesMenuList())
         dispatchInputAndChangeEventForMenuList();
+    m_lastOnChangeSelection.clear();
     HTMLFormControlElementWithState::dispatchBlurEvent(newFocusedElement, type, sourceCapabilities);
 }
 
@@ -1495,6 +1496,9 @@ void HTMLSelectElement::listBoxDefaultEventHandler(Event* event)
 
         if (Page* page = document().page())
             page->autoscrollController().startAutoscrollForSelection(layoutObject());
+        // Mousedown didn't happen in this element.
+        if (m_lastOnChangeSelection.isEmpty())
+            return;
 
         int listIndex = listIndexForEventTargetOption(*mouseEvent);
         if (listIndex >= 0) {
