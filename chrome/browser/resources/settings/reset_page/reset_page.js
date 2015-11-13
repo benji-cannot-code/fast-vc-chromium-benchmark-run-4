7bb085e889ce699be055b37fa7a2bc9c012b2291
dpapad <dpapad@chromium.org>
2015-11-12 18:08:36 -0800

    MD Settings: Migrating C++ handler logic for Powerwash (factory reset).
    BUG=546840
    
    Review URL: https://codereview.chromium.org/1439673003
    
    Cr-Commit-Position: refs/heads/master@{#359472}

--- chrome/browser/resources/settings/reset_page/reset_page.js
+++ chrome/browser/resources/settings/reset_page/reset_page.js
@@ -22,6 +22,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'settings-reset-page',
 
+  properties: {
+    allowPowerwash_: {
+      type: Boolean,
+      value: loadTimeData.getBoolean('allowPowerwash')
+    },
+  },
 
   /** @private */
   onShowResetProfileDialog_: function() {
