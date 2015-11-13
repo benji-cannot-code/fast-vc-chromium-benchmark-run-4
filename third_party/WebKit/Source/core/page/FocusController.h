47baadef6f76722e25576ef832baa546867ddbf3
alexmos <alexmos@chromium.org>
2015-11-13 12:28:44 -0800

    Fix document.hasFocus() to work with OOPIF.
    Previously, Document::hasFocus() ignored focused RemoteFrames, so it
    failed to return true for a frame when its descendant RemoteFrame had
    focus (which is necessary per
    http://www.w3.org/TR/html5/editing.html#dom-document-hasfocus).  This
    CL moves the corresponding logic into FocusController, which tracks
    remote focused frames and can use this in the descendant check.
    
    BUG=341918
    
    Review URL: https://codereview.chromium.org/1440043002
    
    Cr-Commit-Position: refs/heads/master@{#359623}

--- third_party/WebKit/Source/core/page/FocusController.h
+++ third_party/WebKit/Source/core/page/FocusController.h
@@ -39,6 +39,7 @@ namespace blink {
 
 struct FocusCandidate;
 struct FocusParams;
+class Document;
 class Element;
 class Frame;
 class HTMLFrameOwnerElement;
@@ -61,6 +62,10 @@ public:
     // An HTMLFrameOwnerElement is considered focused if the frame it owns, or
     // one of its descendant frames, is currently focused.
     HTMLFrameOwnerElement* focusedFrameOwnerElement(LocalFrame& currentFrame) const;
+
+    // Determines whether the provided Document has focus according to
+    // http://www.w3.org/TR/html5/editing.html#dom-document-hasfocus
+    bool isDocumentFocused(const Document&) const;
 
     bool setInitialFocus(WebFocusType);
     bool advanceFocus(WebFocusType type, InputDeviceCapabilities* sourceCapabilities = nullptr) { return advanceFocus(type, false, sourceCapabilities); }
