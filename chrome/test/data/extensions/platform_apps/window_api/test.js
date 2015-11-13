92abafbd0f1d8c9561b45be8acb57c2b99ded63b
matthewtff <matthewtff@yandex-team.ru>
2015-11-13 02:23:42 -0800

    Fix undefined behaviour for windowPosition test
    BUG=
    
    Review URL: https://codereview.chromium.org/1431943004
    
    Cr-Commit-Position: refs/heads/master@{#359524}

--- chrome/test/data/extensions/platform_apps/window_api/test.js
+++ chrome/test/data/extensions/platform_apps/window_api/test.js
@@ -275,7 +275,7 @@ function testCreate() {
 function testDeprecatedBounds() {
   chrome.test.runTests([
     function contentSize() {
-      var options = { bounds: { left: 0, top: 0, width: 250, height: 200 } };
+      var options = { bounds: { left: 0, top: 50, width: 250, height: 200 } };
       chrome.app.window.create('test.html', options, callbackPass(
       function(win) {
         var bounds = win.getBounds();
@@ -288,7 +288,7 @@ function testDeprecatedBounds() {
     },
 
     function windowPosition() {
-      var options = { bounds: { left: 0, top: 0, left: 250, top: 200 } };
+      var options = { bounds: { left: 0, top: 50, width: 250, height: 200 } };
       chrome.app.window.create('test.html', options, callbackPass(
       function(win) {
         var bounds = win.getBounds();
@@ -302,7 +302,7 @@ function testDeprecatedBounds() {
 
     function minSize() {
       var options = {
-        bounds: { left: 0, top: 0, width: 250, height: 250 },
+        bounds: { left: 0, top: 50, width: 250, height: 250 },
         minWidth: 400, minHeight: 450
       };
       chrome.app.window.create('test.html', options, callbackPass(
@@ -316,7 +316,7 @@ function testDeprecatedBounds() {
 
     function maxSize() {
       var options = {
-        bounds: { left: 0, top: 0, width: 250, height: 250 },
+        bounds: { left: 0, top: 50, width: 250, height: 250 },
         maxWidth: 200, maxHeight: 150
       };
       chrome.app.window.create('test.html', options, callbackPass(
@@ -330,7 +330,7 @@ function testDeprecatedBounds() {
 
     function minAndMaxSize() {
       var options = {
-        bounds: { left: 0, top: 0, width: 250, height: 250 },
+        bounds: { left: 0, top: 50, width: 250, height: 250 },
         minWidth: 400, minHeight: 450,
         maxWidth: 200, maxHeight: 150
       };
@@ -345,7 +345,7 @@ function testDeprecatedBounds() {
 
     function simpleSetBounds() {
       chrome.app.window.create('test.html', {
-        bounds: { left: 0, top: 0, width: 250, height: 200 }
+        bounds: { left: 0, top: 50, width: 250, height: 200 }
       }, callbackPass(function(win) {
         var newBounds = {width: 400, height: 450};
         win.setBounds(newBounds);
@@ -360,7 +360,7 @@ function testDeprecatedBounds() {
 
     function heightOnlySetBounds() {
       chrome.app.window.create('test.html', {
-        bounds: { left: 0, top: 0, width: 300, height: 256 }
+        bounds: { left: 0, top: 50, width: 300, height: 256 }
       }, callbackPass(function(win) {
         win.setBounds({ height: 300 });
         chrome.test.waitForRoundTrip('msg', callbackPass(function() {
