1ebc26078be75634a4606ff5ec25d6caa72717d6
sdefresne <sdefresne@chromium.org>
2015-11-13 05:08:11 -0800

    Fix dependency of //components/enhanced_bookmarks.
    Remove stale #include in bookmark_server_cluster_service.h and remove
    the useless dependency on //components/keyed_service/content from the
    BUILD.gn file.
    
    BUG=459705
    
    Review URL: https://codereview.chromium.org/1446463002
    
    Cr-Commit-Position: refs/heads/master@{#359539}

--- components/enhanced_bookmarks/bookmark_server_cluster_service.h
+++ components/enhanced_bookmarks/bookmark_server_cluster_service.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "components/enhanced_bookmarks/bookmark_server_service.h"
-#include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "components/signin/core/browser/signin_manager_base.h"
 #include "components/sync_driver/sync_service_observer.h"
 #include "net/url_request/url_fetcher.h"
