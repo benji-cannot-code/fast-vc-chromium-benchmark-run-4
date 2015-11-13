f3b3e092687d6e473e8c4392aa2a8ec450166910
chrisha <chrisha@chromium.org>
2015-11-13 14:15:19 -0800

    Create per-child-process dispatch mechanism for memory pressure notifications.
    This modifies the browser-side MemoryMessageFilter so that they explicitly track the child-process with which they are associated. It then adds a method to MemoryPressureController which allows selectively dispatching messages to a single process at a time.
    
    A further CL will make use of this mechanism to dispatch memory pressure notifications from the browser.
    
    BUG=483964
    
    Review URL: https://codereview.chromium.org/1442463003
    
    Cr-Commit-Position: refs/heads/master@{#359662}

--- content/browser/memory/memory_message_filter.cc
+++ content/browser/memory/memory_message_filter.cc
@@ -10,8 +10,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-MemoryMessageFilter::MemoryMessageFilter()
-    : BrowserMessageFilter(MemoryMsgStart) {}
+MemoryMessageFilter::MemoryMessageFilter(
+    const BrowserChildProcessHost* child_process_host,
+    ProcessType process_type)
+    : BrowserMessageFilter(MemoryMsgStart),
+      process_host_(child_process_host),
+      process_type_(process_type) {
+  DCHECK_NE(process_type_, PROCESS_TYPE_RENDERER);
+}
+
+MemoryMessageFilter::MemoryMessageFilter(
+    const RenderProcessHost* render_process_host)
+    : BrowserMessageFilter(MemoryMsgStart),
+      process_host_(render_process_host),
+      process_type_(PROCESS_TYPE_RENDERER) {}
 
 MemoryMessageFilter::~MemoryMessageFilter() {}
 
