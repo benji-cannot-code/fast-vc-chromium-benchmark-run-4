6528dc47964f8bfb81ec639b1c8172d17da8aacd
haibinlu <haibinlu@chromium.org>
2015-11-12 18:05:46 -0800

    [Blimp] Add tcp server transport.
    Review URL: https://codereview.chromium.org/1432213004
    
    Cr-Commit-Position: refs/heads/master@{#359468}

--- blimp/net/tcp_engine_transport.h
+++ blimp/net/tcp_engine_transport.h
@@ -3,19 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef BLIMP_NET_TCP_CLIENT_TRANSPORT_H_
-#define BLIMP_NET_TCP_CLIENT_TRANSPORT_H_
+#ifndef BLIMP_NET_TCP_ENGINE_TRANSPORT_H_
+#define BLIMP_NET_TCP_ENGINE_TRANSPORT_H_
 
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
 #include "blimp/net/blimp_net_export.h"
 #include "blimp/net/blimp_transport.h"
-#include "net/base/address_list.h"
+#include "net/base/ip_endpoint.h"
 #include "net/base/net_errors.h"
 
 namespace net {
 class NetLog;
+class ServerSocket;
 class StreamSocket;
 }  // namespace net
 
@@ -23,28 +24,31 @@ namespace blimp {
 
 class BlimpConnection;
 
-// BlimpTransport which creates a TCP connection to one of the specified
-// |addresses| on each call to Connect().
-class BLIMP_NET_EXPORT TCPClientTransport : public BlimpTransport {
+// BlimpTransport which listens for a TCP connection at |address|.
+class BLIMP_NET_EXPORT TCPEngineTransport : public BlimpTransport {
  public:
-  TCPClientTransport(const net::AddressList& addresses, net::NetLog* net_log);
-  ~TCPClientTransport() override;
+  // Caller retains the ownership of |net_log|.
+  TCPEngineTransport(const net::IPEndPoint& address, net::NetLog* net_log);
+  ~TCPEngineTransport() override;
 
   // BlimpTransport implementation.
   int Connect(const net::CompletionCallback& callback) override;
   scoped_ptr<BlimpConnection> TakeConnection() override;
 
- private:
-  void OnTCPConnectComplete(int result);
+  int GetLocalAddressForTesting(net::IPEndPoint* address) const;
 
-  net::AddressList addresses_;
+ private:
+  void OnTCPConnectAccepted(int result);
+
+  const net::IPEndPoint address_;
   net::NetLog* net_log_;
-  scoped_ptr<net::StreamSocket> socket_;
+  scoped_ptr<net::ServerSocket> server_socket_;
+  scoped_ptr<net::StreamSocket> accepted_socket_;
   net::CompletionCallback connect_callback_;
 
-  DISALLOW_COPY_AND_ASSIGN(TCPClientTransport);
+  DISALLOW_COPY_AND_ASSIGN(TCPEngineTransport);
 };
 
 }  // namespace blimp
 
-#endif  // BLIMP_NET_TCP_CLIENT_TRANSPORT_H_
+#endif  // BLIMP_NET_TCP_ENGINE_TRANSPORT_H_
