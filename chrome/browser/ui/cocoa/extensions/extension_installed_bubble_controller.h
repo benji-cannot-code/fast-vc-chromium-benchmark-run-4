8848bdeed133a04c3bd387095bb952bd807e97ea
hcarmona <hcarmona@chromium.org>
2015-11-13 13:26:35 -0800

    Make the Extension Installed Bubble use the Bubble API.
    Consolidated the ShowExtensionInstalledBubble static function because
    it will be the same on all platforms.
    
    ExtensionInstalledBubbleUi::Create is implemented in views and cocoa to
    provide the correct UI object.
    
    ExtensionInstalledBubble is the BubbleDelegate.
    ExtensionInstalledBubbleUi is the BubbleUi.
    
    ExtensionInstalledBubbleBridge and ExtensionInstalledBubbleView are the
    platform dependent implementations of ExtensionInstalledBubbleUi.
    
    Observing if the extension is removed or the browser is closed has now
    moved to ExtensionInstalledBubbleObserver. This class owns itself and
    will delete itself when if first encounters one of these conditions:
      - Browser is closed.
      - Extension is removed
      - Animation is over and bubble is given to the BubbleManager.
    
    Ownership of objects:
      Owned by BubbleManager
        ExtensionInstalledBubbleBridge (cocoa)
        ExtensionInstalledBubbleView (views)
        ExtensionInstalledBubble
      Owned by UI
        ExtensionInstalledBubbleController (cocoa)
        ExtensionInstalledBubbleDelegateView (views)
    
    BUG=496955
    
    Review URL: https://codereview.chromium.org/1371383002
    
    Cr-Commit-Position: refs/heads/master@{#359645}

--- chrome/browser/ui/cocoa/extensions/extension_installed_bubble_controller.h
+++ chrome/browser/ui/cocoa/extensions/extension_installed_bubble_controller.h
@@ -9,13 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 
 #include "base/mac/scoped_nsobject.h"
-#include "base/memory/scoped_ptr.h"
 #import "chrome/browser/ui/cocoa/base_bubble_controller.h"
-#include "third_party/skia/include/core/SkBitmap.h"
 
 class Browser;
 class ExtensionInstalledBubble;
-class ExtensionInstalledBubbleBridge;
 @class HyperlinkTextView;
 @class HoverCloseButton;
 
@@ -70,11 +67,8 @@ typedef enum {
   // doesn't overlap browser destruction.
   BOOL pageActionPreviewShowing_;
 
-  // The bridge to the C++ object that performs shared logic across platforms,
-  // like listening for the notification that the extension is loaded. This
-  // tells us when to show the bubble.
-  scoped_ptr<ExtensionInstalledBubbleBridge> installedBubbleBridge_;
-  scoped_ptr<ExtensionInstalledBubble> installedBubble_;
+  // A weak reference to the bubble. It's owned by the BubbleManager.
+  ExtensionInstalledBubble* installedBubble_;
 
   // References below are weak, being obtained from the nib.
   IBOutlet HoverCloseButton* closeButton_;
@@ -104,14 +98,12 @@ typedef enum {
 @property(nonatomic, readonly) const extensions::BundleInstaller* bundle;
 @property(nonatomic) BOOL pageActionPreviewShowing;
 
-// Initialize the window, and then create observers to wait for the extension
-// to complete loading, or the browser window to close.
-// Takes ownership of |extensionBubble|.
+// Initialize the window. It will be shown by the BubbleManager.
 - (id)initWithParentWindow:(NSWindow*)parentWindow
            extensionBubble:(ExtensionInstalledBubble*)extensionBubble;
 
-// Initialize the window, and then create observers to wait for the extension
-// to complete loading, or the browser window to close.
+// Initialize the window, and show it. BubbleManager is not currently used for
+// displaying the "Bundle Installed" bubble.
 - (id)initWithParentWindow:(NSWindow*)parentWindow
                     bundle:(const extensions::BundleInstaller*)bundle
                    browser:(Browser*)browser;
@@ -148,6 +140,7 @@ typedef enum {
 - (NSRect)frameOfSigninPromo;
 - (BOOL)showSyncPromo;
 - (NSButton*)appInstalledShortcutLink;
+- (void)updateAnchorPosition;
 
 @end  // ExtensionInstalledBubbleController(ExposedForTesting)
 
