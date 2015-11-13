888056d2ded188867ae24531eab6ccfec52b9962
vabr <vabr@chromium.org>
2015-11-13 08:48:28 -0800

    git cl lint clean-up in passwords UI
    Fixes some linter issues. This is a pre-requisite to a more bug-related CL.
    
    BUG=555132
    
    Review URL: https://codereview.chromium.org/1444593002
    
    Cr-Commit-Position: refs/heads/master@{#359569}

--- chrome/browser/ui/passwords/password_ui_view.h
+++ chrome/browser/ui/passwords/password_ui_view.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_PASSWORDS_PASSWORD_UI_VIEW_H_
 #define CHROME_BROWSER_UI_PASSWORDS_PASSWORD_UI_VIEW_H_
 
+#include <string>
+
 #include "base/memory/scoped_vector.h"
 #include "ui/gfx/native_widget_types.h"
 
