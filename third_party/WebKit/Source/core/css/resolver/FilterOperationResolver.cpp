e8b4e2c9f040376fb2c0a327592940765a27a763
jbroman <jbroman@chromium.org>
2015-11-13 17:12:29 -0800

    Add UseCounter features for CSS filter values.
    BUG=555680
    
    Review URL: https://codereview.chromium.org/1437253003
    
    Cr-Commit-Position: refs/heads/master@{#359711}

--- third_party/WebKit/Source/core/css/resolver/FilterOperationResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/FilterOperationResolver.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/CSSPrimitiveValueMappings.h"
 #include "core/css/CSSShadowValue.h"
 #include "core/css/resolver/StyleResolverState.h"
+#include "core/frame/UseCounter.h"
 #include "core/layout/svg/ReferenceFilterBuilder.h"
 #include "core/svg/SVGElement.h"
 #include "core/svg/SVGURIReference.h"
@@ -72,6 +73,52 @@ static FilterOperation::OperationType filterOperationForType(CSSValueID type)
     }
 }
 
+static void countFilterUse(FilterOperation::OperationType operationType, const Document& document)
+{
+    // This variable is always reassigned, but MSVC thinks it might be left
+    // uninitialized.
+    UseCounter::Feature feature = UseCounter::NumberOfFeatures;
+    switch (operationType) {
+    case FilterOperation::NONE:
+        ASSERT_NOT_REACHED();
+        return;
+    case FilterOperation::REFERENCE:
+        feature = UseCounter::CSSFilterReference;
+        break;
+    case FilterOperation::GRAYSCALE:
+        feature = UseCounter::CSSFilterGrayscale;
+        break;
+    case FilterOperation::SEPIA:
+        feature = UseCounter::CSSFilterSepia;
+        break;
+    case FilterOperation::SATURATE:
+        feature = UseCounter::CSSFilterSaturate;
+        break;
+    case FilterOperation::HUE_ROTATE:
+        feature = UseCounter::CSSFilterHueRotate;
+        break;
+    case FilterOperation::INVERT:
+        feature = UseCounter::CSSFilterInvert;
+        break;
+    case FilterOperation::OPACITY:
+        feature = UseCounter::CSSFilterOpacity;
+        break;
+    case FilterOperation::BRIGHTNESS:
+        feature = UseCounter::CSSFilterBrightness;
+        break;
+    case FilterOperation::CONTRAST:
+        feature = UseCounter::CSSFilterContrast;
+        break;
+    case FilterOperation::BLUR:
+        feature = UseCounter::CSSFilterBlur;
+        break;
+    case FilterOperation::DROP_SHADOW:
+        feature = UseCounter::CSSFilterDropShadow;
+        break;
+    };
+    UseCounter::count(document, feature);
+}
+
 FilterOperations FilterOperationResolver::createFilterOperations(StyleResolverState& state, const CSSValue& inValue)
 {
     FilterOperations operations;
@@ -85,6 +132,7 @@ FilterOperations FilterOperationResolver::createFilterOperations(StyleResolverSt
     for (auto& currValue : toCSSValueList(inValue)) {
         CSSFunctionValue* filterValue = toCSSFunctionValue(currValue.get());
         FilterOperation::OperationType operationType = filterOperationForType(filterValue->functionType());
+        countFilterUse(operationType, state.document());
         ASSERT(filterValue->length() <= 1);
 
         if (operationType == FilterOperation::REFERENCE) {
