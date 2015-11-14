d440977a28f7ed95e1a8258783680b787ca27122
justincohen <justincohen@chromium.org>
2015-11-13 18:05:32 -0800

    Revert ScopedTracker changes to  measure the jank in BrowserCrApplication sendEvent.
    BUG=463272
    
    Review URL: https://codereview.chromium.org/1440373002
    
    Cr-Commit-Position: refs/heads/master@{#359734}

--- chrome/browser/chrome_browser_application_mac.mm
+++ chrome/browser/chrome_browser_application_mac.mm
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/mac/scoped_nsobject.h"
 #import "base/mac/scoped_objc_class_swizzler.h"
 #import "base/metrics/histogram.h"
-#include "base/profiler/scoped_tracker.h"
 #include "base/strings/stringprintf.h"
 #import "base/strings/sys_string_conversions.h"
 #import "chrome/browser/app_controller_mac.h"
@@ -335,9 +334,6 @@ void CancelTerminate() {
 
 - (void)sendEvent:(NSEvent*)event {
   base::mac::CallWithEHFrame(^{
-    // tracked_objects::ScopedTracker does not support parameterized
-    // instrumentations, so a big switch with each bunch instrumented is
-    // required.
     switch (event.type) {
       case NSLeftMouseDown:
       case NSRightMouseDown: {
@@ -350,80 +346,8 @@ void CancelTerminate() {
         if (kioskMode && ([event type] == NSRightMouseDown || ctrlDown))
           break;
       }
-      // FALL THROUGH
-      case NSLeftMouseUp:
-      case NSRightMouseUp:
-      case NSMouseMoved:
-      case NSLeftMouseDragged:
-      case NSRightMouseDragged:
-      case NSMouseEntered:
-      case NSMouseExited:
-      case NSOtherMouseDown:
-      case NSOtherMouseUp:
-      case NSOtherMouseDragged: {
-        tracked_objects::ScopedTracker tracking_profile(
-            FROM_HERE_WITH_EXPLICIT_FUNCTION(
-                "463272 -[BrowserCrApplication sendEvent:] Mouse"));
-        base::mac::ScopedSendingEvent sendingEventScoper;
-        [super sendEvent:event];
-        break;
-      }
-
-      case NSKeyDown:
-      case NSKeyUp: {
-        tracked_objects::ScopedTracker tracking_profile(
-            FROM_HERE_WITH_EXPLICIT_FUNCTION(
-                "463272 -[BrowserCrApplication sendEvent:] Key"));
-        base::mac::ScopedSendingEvent sendingEventScoper;
-        [super sendEvent:event];
-        break;
-      }
-
-      case NSScrollWheel: {
-        tracked_objects::ScopedTracker tracking_profile(
-            FROM_HERE_WITH_EXPLICIT_FUNCTION(
-                "463272 -[BrowserCrApplication sendEvent:] ScrollWheel"));
-        base::mac::ScopedSendingEvent sendingEventScoper;
-        [super sendEvent:event];
-        break;
-      }
-
-      case NSEventTypeGesture:
-      case NSEventTypeMagnify:
-      case NSEventTypeSwipe:
-      case NSEventTypeRotate:
-      case NSEventTypeBeginGesture:
-      case NSEventTypeEndGesture: {
-        tracked_objects::ScopedTracker tracking_profile(
-            FROM_HERE_WITH_EXPLICIT_FUNCTION(
-                "463272 -[BrowserCrApplication sendEvent:] Gesture"));
-        base::mac::ScopedSendingEvent sendingEventScoper;
-        [super sendEvent:event];
-        break;
-      }
-
-      case NSAppKitDefined: {
-        tracked_objects::ScopedTracker tracking_profile(
-            FROM_HERE_WITH_EXPLICIT_FUNCTION(
-                "463272 -[BrowserCrApplication sendEvent:] AppKit"));
-        base::mac::ScopedSendingEvent sendingEventScoper;
-        [super sendEvent:event];
-        break;
-      }
-
-      case NSSystemDefined: {
-        tracked_objects::ScopedTracker tracking_profile(
-            FROM_HERE_WITH_EXPLICIT_FUNCTION(
-                "463272 -[BrowserCrApplication sendEvent:] System"));
-        base::mac::ScopedSendingEvent sendingEventScoper;
-        [super sendEvent:event];
-        break;
-      }
 
       default: {
-        tracked_objects::ScopedTracker tracking_profile(
-            FROM_HERE_WITH_EXPLICIT_FUNCTION(
-                "463272 -[BrowserCrApplication sendEvent:] Other"));
         base::mac::ScopedSendingEvent sendingEventScoper;
         [super sendEvent:event];
       }
