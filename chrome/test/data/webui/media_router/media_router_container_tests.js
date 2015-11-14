8b09f9eaaae9492b48d2756489de34d3e93d0867
apacible <apacible@chromium.org>
2015-11-13 16:37:57 -0800

    [Media Router] Fix media-router-container computeSinkListHidden_ usage.
    The function used for visibility for the sink list in media-router-container was not being used correctly. Fixed the parameters and updated tests. This resolves the scenario where we show the sink list (which fills the height of the dialog) at the same time as the "Device missing?" div.
    
    BUG=555772
    
    Review URL: https://codereview.chromium.org/1448593003
    
    Cr-Commit-Position: refs/heads/master@{#359703}

--- chrome/test/data/webui/media_router/media_router_container_tests.js
+++ chrome/test/data/webui/media_router/media_router_container_tests.js
@@ -392,21 +392,18 @@ cr.define('media_router_container', function() {
         container.showCastModeList_();
         checkElementsVisibleWithId(['cast-mode-list',
                                     'container-header',
-                                    'device-missing',
-                                    'sink-list']);
+                                    'device-missing']);
 
         // Set a non-blocking issue. The issue should stay hidden.
         container.issue = fakeNonBlockingIssue;
         checkElementsVisibleWithId(['cast-mode-list',
                                     'container-header',
-                                    'device-missing',
-                                    'sink-list']);
+                                    'device-missing']);
 
         // Set a blocking issue. The issue should stay hidden.
         container.issue = fakeBlockingIssue;
         checkElementsVisibleWithId(['container-header',
-                                    'device-missing',
-                                    'sink-list']);
+                                    'device-missing']);
       });
 
 
@@ -415,8 +412,7 @@ cr.define('media_router_container', function() {
         container.showRouteDetails_();
         checkElementsVisibleWithId(['container-header',
                                     'device-missing',
-                                    'route-details',
-                                    'sink-list']);
+                                    'route-details']);
       });
 
       // Tests for expected visible UI when the view is ROUTE_DETAILS, and there
@@ -430,8 +426,7 @@ cr.define('media_router_container', function() {
           checkElementsVisibleWithId(['container-header',
                                       'device-missing',
                                       'issue-banner',
-                                      'route-details',
-                                      'sink-list']);
+                                      'route-details']);
           done();
         });
       });
@@ -447,8 +442,7 @@ cr.define('media_router_container', function() {
         setTimeout(function() {
           checkElementsVisibleWithId(['container-header',
                                       'device-missing',
-                                      'issue-banner',
-                                      'sink-list']);
+                                      'issue-banner']);
           done();
          });
       });
@@ -458,7 +452,6 @@ cr.define('media_router_container', function() {
         container.showSinkList_();
         checkElementsVisibleWithId(['container-header',
                                     'device-missing',
-                                    'sink-list',
                                     'sink-list-view']);
 
         // Set an non-empty sink list.
