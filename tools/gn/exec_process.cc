b56b567da1bd1e87153a173d2ae25f502af54432
tfarina <tfarina@chromium.org>
2015-11-14 18:40:17 -0800

    tools/gn: include exec_process.h in the source file.
    exec_process.cc is the source file implementation of exec_process.h,
    but somehow we forgot to include its corresponding header file. This
    patch fixes that.
    
    It also add it to the unittest file where it was also missing.
    
    build_config.h was also included for the usage of OS_XXX macros.
    
    BUG=None
    R=mostynb@opera.com
    TBR=brettw@chromium.org
    
    Review URL: https://codereview.chromium.org/1447513002
    
    Cr-Commit-Position: refs/heads/master@{#359752}

--- tools/gn/exec_process.cc
+++ tools/gn/exec_process.cc
@@ -3,12 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "tools/gn/exec_process.h"
+
 #include "base/command_line.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/process/kill.h"
 #include "base/process/launch.h"
 #include "base/process/process.h"
+#include "build/build_config.h"
 
 #if defined(OS_WIN)
 #include <windows.h>
--- tools/gn/exec_process_unittest.cc
+++ tools/gn/exec_process_unittest.cc
@@ -3,11 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "tools/gn/exec_process.h"
+
 #include "base/command_line.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/strings/string_util.h"
+#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "tools/gn/exec_process.h"
 
 #if defined(OS_WIN)
 #include "base/strings/utf_string_conversions.h"
