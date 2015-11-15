ed95dea86b8b972f7c52f4a279aae5b445aea61f
sigbjornf <sigbjornf@opera.com>
2015-11-14 18:38:55 -0800

    Oilpan: track ScriptStateForTesting's ExecutionContext reference better.
    R=haraken
    BUG=553613
    
    Review URL: https://codereview.chromium.org/1445913002
    
    Cr-Commit-Position: refs/heads/master@{#359750}

--- third_party/WebKit/Source/bindings/core/v8/V8BindingForTesting.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8BindingForTesting.cpp
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/V8BindingForTesting.h"
 
 #include "bindings/core/v8/DOMWrapperWorld.h"
+#include "core/dom/ExecutionContext.h"
 
 namespace blink {
 
