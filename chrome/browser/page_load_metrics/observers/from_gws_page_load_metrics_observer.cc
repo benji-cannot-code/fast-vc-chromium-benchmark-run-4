8da5831a1896e7be8e9eda2640214e661f058d10
bmcquade <bmcquade@chromium.org>
2015-11-16 13:32:46 -0800

    Add additional FromGWS metrics, and fix suffix name.
    BUG=546116
    
    Review URL: https://codereview.chromium.org/1442983006
    
    Cr-Commit-Position: refs/heads/master@{#359922}

--- chrome/browser/page_load_metrics/observers/from_gws_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/from_gws_page_load_metrics_observer.cc
@@ -13,6 +13,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+base::TimeDelta GetFirstContentfulPaint(
+    const page_load_metrics::PageLoadTiming& timing) {
+  if (timing.first_text_paint.is_zero())
+    return timing.first_image_paint;
+  if (timing.first_image_paint.is_zero())
+    return timing.first_text_paint;
+  return std::min(timing.first_text_paint, timing.first_image_paint);
+}
+
 bool IsFromGoogle(const GURL& url) {
   const char kGoogleSearchHostnamePrefix[] = "www.";
   std::string domain = net::registry_controlled_domains::GetDomainAndRegistry(
@@ -98,6 +107,22 @@ void FromGWSPageLoadMetricsObserver::OnComplete(
     PAGE_LOAD_HISTOGRAM(
         "PageLoad.Clients.FromGWS.Timing2.NavigationToFirstTextPaint",
         timing.first_text_paint);
+  }
+  if (ShouldLogEvent(timing.first_image_paint, first_background)) {
+    PAGE_LOAD_HISTOGRAM(
+        "PageLoad.Clients.FromGWS.Timing2.NavigationToFirstImagePaint",
+        timing.first_image_paint);
+  }
+  if (ShouldLogEvent(timing.first_paint, first_background)) {
+    PAGE_LOAD_HISTOGRAM(
+        "PageLoad.Clients.FromGWS.Timing2.NavigationToFirstPaint",
+        timing.first_paint);
+  }
+  base::TimeDelta first_contentful_paint = GetFirstContentfulPaint(timing);
+  if (ShouldLogEvent(first_contentful_paint, first_background)) {
+    PAGE_LOAD_HISTOGRAM(
+        "PageLoad.Clients.FromGWS.Timing2.NavigationToFirstContentfulPaint",
+        first_contentful_paint);
   }
 }
 
