4cf4e110fa3e3e39aebfa443620f6cbf0eeb89dd
cmumford <cmumford@chromium.org>
2015-11-16 10:20:43 -0800

    Extensions: Checking for failure when reading settings from db.
    Previously SettingsStorageQuotaEnforcer::CalculateUsage() made one
    attempt to restore the db, but assumed that a second call to Get()
    would always return a valid settings dictionary.
    
    BUG=554657
    
    Review URL: https://codereview.chromium.org/1434133002
    
    Cr-Commit-Position: refs/heads/master@{#359866}

--- extensions/browser/api/storage/settings_storage_quota_enforcer.cc
+++ extensions/browser/api/storage/settings_storage_quota_enforcer.cc
@@ -257,7 +257,9 @@ void SettingsStorageQuotaEnforcer::CalculateUsage() {
     if (maybe_settings->error().code == ValueStore::CORRUPTION &&
         delegate_->Restore()) {
       maybe_settings = delegate_->Get();
-    } else {
+    }
+
+    if (maybe_settings->HasError()) {
       LOG(WARNING) << "Failed to get settings for quota:"
                    << maybe_settings->error().message;
       return;
