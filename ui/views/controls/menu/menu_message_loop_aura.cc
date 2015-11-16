9bdf96723b6d7e6fad48466a615a7671667d0be5
fsamuel <fsamuel@chromium.org>
2015-11-16 10:58:54 -0800

    mustash: Make popups not crash
    BUG=none
    
    Review URL: https://codereview.chromium.org/1448903003
    
    Cr-Commit-Position: refs/heads/master@{#359881}

--- ui/views/controls/menu/menu_message_loop_aura.cc
+++ ui/views/controls/menu/menu_message_loop_aura.cc
@@ -146,7 +146,7 @@ void MenuMessageLoopAura::Run(MenuController* controller,
     message_loop_quit_ = run_loop.QuitClosure();
     run_loop.Run();
   } else {
-    base::MessageLoopForUI* loop = base::MessageLoopForUI::current();
+    base::MessageLoop* loop = base::MessageLoop::current();
     base::MessageLoop::ScopedNestableTaskAllower allow(loop);
     base::RunLoop run_loop(&nested_dispatcher);
     message_loop_quit_ = run_loop.QuitClosure();
@@ -166,7 +166,7 @@ void MenuMessageLoopAura::Run(MenuController* controller,
     menu_event_filter.reset(new MenuKeyEventHandler);
   }
 
-  base::MessageLoopForUI* loop = base::MessageLoopForUI::current();
+  base::MessageLoop* loop = base::MessageLoop::current();
   base::MessageLoop::ScopedNestableTaskAllower allow(loop);
   base::RunLoop run_loop;
   message_loop_quit_ = run_loop.QuitClosure();
