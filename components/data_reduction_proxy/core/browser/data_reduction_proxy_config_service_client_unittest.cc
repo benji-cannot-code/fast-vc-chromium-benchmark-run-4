4195b214bf29674b2207dc5fd6fe6610a89f06ec
rajendrant <rajendrant@chromium.org>
2015-11-16 14:15:03 -0800

    Remove ScopedVector from data reduction proxy unittests
    ScopedVector is deprecated. Pls see http://crbug.com/554289
    
    BUG=554289
    
    Review URL: https://codereview.chromium.org/1446143002
    
    Cr-Commit-Position: refs/heads/master@{#359936}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client_unittest.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client_unittest.cc
@@ -497,11 +497,12 @@ TEST_F(DataReductionProxyConfigServiceClientTest,
       },
   };
 
-  ScopedVector<net::SocketDataProvider> socket_data_providers;
+  std::vector<scoped_ptr<net::SocketDataProvider>> socket_data_providers;
   for (net::MockRead* mock_reads : mock_reads_array) {
-    socket_data_providers.push_back(
-        new net::StaticSocketDataProvider(mock_reads, 3, nullptr, 0));
-    mock_socket_factory()->AddSocketDataProvider(socket_data_providers.back());
+    socket_data_providers.push_back(make_scoped_ptr(
+        new net::StaticSocketDataProvider(mock_reads, 3, nullptr, 0)));
+    mock_socket_factory()->AddSocketDataProvider(
+        socket_data_providers.back().get());
   }
 
   config_client()->SetConfigServiceURL(GURL("http://configservice.com"));
@@ -534,11 +535,12 @@ TEST_F(DataReductionProxyConfigServiceClientTest, OnIPAddressChange) {
       net::MockRead(net::SYNCHRONOUS, net::OK),
   };
 
-  ScopedVector<net::SocketDataProvider> socket_data_providers;
+  std::vector<scoped_ptr<net::SocketDataProvider>> socket_data_providers;
   for (int i = 0; i < kFailureCount; ++i) {
-    socket_data_providers.push_back(
-        new net::StaticSocketDataProvider(failure_reads, 3, nullptr, 0));
-    mock_socket_factory()->AddSocketDataProvider(socket_data_providers.back());
+    socket_data_providers.push_back(make_scoped_ptr(
+        new net::StaticSocketDataProvider(failure_reads, 3, nullptr, 0)));
+    mock_socket_factory()->AddSocketDataProvider(
+        socket_data_providers.back().get());
     config_client()->RetrieveConfig();
     RunUntilIdle();
   }
@@ -611,12 +613,13 @@ TEST_F(DataReductionProxyConfigServiceClientTest, AuthFailure) {
        net::MockRead(net::SYNCHRONOUS, net::OK),
       },
   };
-  ScopedVector<net::SocketDataProvider> socket_data_providers;
+  std::vector<scoped_ptr<net::SocketDataProvider>> socket_data_providers;
   base::HistogramTester histogram_tester;
   for (net::MockRead* mock_reads : mock_reads_array) {
-    socket_data_providers.push_back(
-        new net::StaticSocketDataProvider(mock_reads, 3, nullptr, 0));
-    mock_socket_factory()->AddSocketDataProvider(socket_data_providers.back());
+    socket_data_providers.push_back(make_scoped_ptr(
+        new net::StaticSocketDataProvider(mock_reads, 3, nullptr, 0)));
+    mock_socket_factory()->AddSocketDataProvider(
+        socket_data_providers.back().get());
   }
 
   config_client()->SetConfigServiceURL(GURL("http://configservice.com"));
