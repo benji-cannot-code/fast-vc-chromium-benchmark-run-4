835eda40582f7220784a972f519fbf54435b37eb
vabr <vabr@chromium.org>
2015-11-16 07:47:17 -0800

    Move passwords_generation_{edit,offer}_view.* upstream
    BUG=514241
    
    Review URL: https://codereview.chromium.org/1439413003
    
    Cr-Commit-Position: refs/heads/master@{#359843}

---
+++ ios/chrome/browser/passwords/password_generation_offer_view.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PASSWORDS_PASSWORD_GENERATION_OFFER_VIEW_H_
+#define IOS_CHROME_BROWSER_PASSWORDS_PASSWORD_GENERATION_OFFER_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+// Handles user interaction with the PasswordGenerationOfferView.
+@protocol PasswordGenerationOfferDelegate<NSObject>
+// Indicates that the user wants an auto-generated password.
+- (void)generatePassword;
+@end
+
+// A view that offers to generate a password automatically.
+@interface PasswordGenerationOfferView : UIView
+// Initializes a view with the specified |delegate|.
+- (instancetype)initWithDelegate:(id<PasswordGenerationOfferDelegate>)delegate;
+@end
+
+#endif  // IOS_CHROME_BROWSER_PASSWORDS_PASSWORD_GENERATION_OFFER_VIEW_H_
