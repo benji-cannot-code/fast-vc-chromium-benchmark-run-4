f2291804643c9df0b6753fea6498eda3b60c722d
meacer <meacer@chromium.org>
2015-11-16 10:57:21 -0800

    Only proceed HTTP auth interstitial if it's still valid
    This is the alternative approach to https://codereview.chromium.org/1399623005/
    
    BUG=540949
    
    Review URL: https://codereview.chromium.org/1416543004
    
    Cr-Commit-Position: refs/heads/master@{#359879}

--- chrome/browser/ui/login/login_interstitial_delegate.h
+++ chrome/browser/ui/login/login_interstitial_delegate.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback.h"
+#include "base/memory/weak_ptr.h"
 #include "content/public/browser/interstitial_page.h"
 #include "content/public/browser/interstitial_page_delegate.h"
 #include "url/gurl.h"
@@ -36,6 +37,10 @@ class LoginInterstitialDelegate : public content::InterstitialPageDelegate {
 
   ~LoginInterstitialDelegate() override;
 
+  void Proceed();
+  void DontProceed();
+  base::WeakPtr<LoginInterstitialDelegate> GetWeakPtr();
+
   // content::InterstitialPageDelegate:
   void CommandReceived(const std::string& command) override;
   content::InterstitialPageDelegate::TypeID GetTypeForTesting() const override;
@@ -45,6 +50,8 @@ class LoginInterstitialDelegate : public content::InterstitialPageDelegate {
 
  private:
   base::Closure callback_;
+  content::InterstitialPage* interstitial_page_;
+  base::WeakPtrFactory<LoginInterstitialDelegate> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(LoginInterstitialDelegate);
 };
--- chrome/browser/ui/login/login_prompt.h
+++ chrome/browser/ui/login/login_prompt.h
@@ -9,12 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
+#include "base/memory/weak_ptr.h"
 #include "base/synchronization/lock.h"
 #include "components/password_manager/core/browser/password_manager.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/resource_dispatcher_host_login_delegate.h"
 
 class GURL;
+class LoginInterstitialDelegate;
 
 namespace content {
 class RenderViewHostDelegate;
@@ -56,6 +58,11 @@ class LoginHandler : public content::ResourceDispatcherHostLoginDelegate,
   // CreateLoginPrompt() which creates tasks.
   static LoginHandler* Create(net::AuthChallengeInfo* auth_info,
                               net::URLRequest* request);
+
+  void SetInterstitialDelegate(
+      const base::WeakPtr<LoginInterstitialDelegate> delegate) {
+    interstitial_delegate_ = delegate;
+  }
 
   // ResourceDispatcherHostLoginDelegate implementation:
   void OnRequestCancelled() override;
@@ -190,6 +197,8 @@ class LoginHandler : public content::ResourceDispatcherHostLoginDelegate,
   // Observes other login handlers so this login handler can respond.
   // This is only accessed on the UI thread.
   scoped_ptr<content::NotificationRegistrar> registrar_;
+
+  base::WeakPtr<LoginInterstitialDelegate> interstitial_delegate_;
 };
 
 // Details to provide the content::NotificationObserver.  Used by the automation
