fb69c579fcf1eac7450374d76d4f86ed8d1e0956
csharrison <csharrison@chromium.org>
2015-11-16 10:48:07 -0800

    OOPIF: Update RenderView browsertest to stop using HistoryController That was updated in https://codereview.chromium.org/1425303002 but this test was added using the old approach, breaking the BrowserNavigationStartNotUsedForHistoryNavigation test using the --site-per-process flag.
    BUG=548335
    
    Review URL: https://codereview.chromium.org/1444333003
    
    Cr-Commit-Position: refs/heads/master@{#359878}

--- content/renderer/render_view_browsertest.cc
+++ content/renderer/render_view_browsertest.cc
@@ -2365,11 +2365,9 @@ TEST_F(RenderViewImplTest, BrowserNavigationStartNotUsedForReload) {
 TEST_F(RenderViewImplTest, BrowserNavigationStartNotUsedForHistoryNavigation) {
   LoadHTML("<div id=pagename>Page A</div>");
   LoadHTML("<div id=pagename>Page B</div>");
-  PageState back_state =
-      HistoryEntryToPageState(view()->history_controller()->GetCurrentEntry());
+  PageState back_state = GetCurrentPageState();
   LoadHTML("<div id=pagename>Page C</div>");
-  PageState forward_state =
-      HistoryEntryToPageState(view()->history_controller()->GetCurrentEntry());
+  PageState forward_state = GetCurrentPageState();
   ProcessPendingMessages();
   render_thread_->sink().ClearMessages();
 
