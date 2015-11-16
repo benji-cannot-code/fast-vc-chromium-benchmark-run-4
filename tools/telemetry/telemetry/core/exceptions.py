aaa9c10801322648a61373eefcada77498f5e003
nednguyen <nednguyen@google.com>
2015-11-16 10:30:40 -0800

    [Telemetry] Include browser stdout in the error message when browser crashes
    These logs are useful for figuring out missing dynamically linked libs.
    
    BUG=551886,554046
    
    Review URL: https://codereview.chromium.org/1443123002
    
    Cr-Commit-Position: refs/heads/master@{#359870}

--- tools/telemetry/telemetry/core/exceptions.py
+++ tools/telemetry/telemetry/core/exceptions.py
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
+import logging
 import sys
 
 
@@ -55,15 +55,31 @@ class AppCrashException(Error):
   def __init__(self, app=None, msg=''):
     super(AppCrashException, self).__init__(msg)
     self._msg = msg
-    self._stack_trace = app.GetStackTrace() if app else None
+    self._stack_trace = []
+    self._app_stdout = []
+    if app:
+      try:
+        self._stack_trace = app.GetStackTrace().splitlines()
+      except Exception as err:
+        logging.error('Problem when trying to gather stack trace: %s' % err)
+      try:
+        self._app_stdout = app.GetStandardOutput().splitlines()
+      except Exception as err:
+        logging.error('Problem when trying to gather standard output: %s' % err)
 
   def __str__(self):
-    if not self._stack_trace:
-      return super(AppCrashException, self).__str__()
     divider = '*' * 80
-    return '%s\nStack Trace:\n%s\n\t%s\n%s' % (
-        super(AppCrashException, self).__str__(), divider,
-        self._stack_trace.replace('\n', '\n\t'), divider)
+    debug_messages = []
+    debug_messages.append(super(AppCrashException, self).__str__())
+    debug_messages.append('Stack Trace:')
+    debug_messages.append(divider)
+    debug_messages.extend(('\t%s' % l) for l in self._stack_trace)
+    debug_messages.append(divider)
+    debug_messages.append('Standard output:')
+    debug_messages.append(divider)
+    debug_messages.extend(('\t%s' % l) for l in self._app_stdout)
+    debug_messages.append(divider)
+    return '\n'.join(debug_messages)
 
 
 class DevtoolsTargetCrashException(AppCrashException):
