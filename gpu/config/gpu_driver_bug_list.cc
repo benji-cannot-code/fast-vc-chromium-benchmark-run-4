0a20879383975e97232e3d7e3836a455552aa350
j.isorce <j.isorce@samsung.com>
2015-11-16 09:36:42 -0800

    When called from gpu process, gpu_util.cc::ApplyGpuDriverBugWorkarounds overwrites existing workarounds that have been notified from browser process.
    Currently when passing --use_virtualized_gl_contexts to command line,
    the workaround id is visible in --gpu-driver-bug-workarounds of gpu process
    and in chrome://gpu but not actually taken into account.
    
    Simply adding a trace in content/common/gpu/gpu_command_buffer_stub.cc
    shows that the wanted workaround is not enabled.
    
    BUG=553524
    
    R=kbr@chromium.org,jbauman@chromium.org,hendrikw@chromium.org
    
    Review URL: https://codereview.chromium.org/1415603009
    
    TEST= out/Release/chrome --use_virtualized_gl_contexts
    
    Review URL: https://codereview.chromium.org/1415603009
    
    Cr-Commit-Position: refs/heads/master@{#359857}

--- gpu/config/gpu_driver_bug_list.cc
+++ gpu/config/gpu_driver_bug_list.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/logging.h"
 #include "gpu/config/gpu_driver_bug_workaround_type.h"
+#include "gpu/config/gpu_switches.h"
+#include "gpu/config/gpu_util.h"
 
 namespace gpu {
 
@@ -59,6 +61,15 @@ void GpuDriverBugList::AppendWorkaroundsFromCommandLine(
     std::set<int>* workarounds,
     const base::CommandLine& command_line) {
   DCHECK(workarounds);
+
+  if (command_line.HasSwitch(switches::kGpuDriverBugWorkarounds)) {
+    std::string cmd_workarounds_str =
+        command_line.GetSwitchValueASCII(switches::kGpuDriverBugWorkarounds);
+    std::set<int> cmd_workarounds;
+    gpu::StringToFeatureSet(cmd_workarounds_str, &cmd_workarounds);
+    workarounds->insert(cmd_workarounds.begin(), cmd_workarounds.end());
+  }
+
   for (int i = 0; i < NUMBER_OF_GPU_DRIVER_BUG_WORKAROUND_TYPES; i++) {
     if (command_line.HasSwitch(kFeatureList[i].name)) {
       // Check for disabling workaround flag.
