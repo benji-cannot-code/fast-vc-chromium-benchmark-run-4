6bc19f18ed2e3795a0496c8dc3feea8c311ec898
michaeln <michaeln@chromium.org>
2015-11-16 13:49:15 -0800

    AppCache: Use WeakPtr<> to fix a potential uaf bug.
    BUG=554908
    
    Review URL: https://codereview.chromium.org/1441683004
    
    Cr-Commit-Position: refs/heads/master@{#359930}

--- content/browser/appcache/appcache_dispatcher_host.h
+++ content/browser/appcache/appcache_dispatcher_host.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/process/process.h"
 #include "content/browser/appcache/appcache_backend_impl.h"
 #include "content/browser/appcache/appcache_frontend_proxy.h"
@@ -69,6 +70,8 @@ class AppCacheDispatcherHost : public BrowserMessageFilter {
 
   // The corresponding ChildProcessHost object's id().
   int process_id_;
+
+  base::WeakPtrFactory<AppCacheDispatcherHost> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(AppCacheDispatcherHost);
 };
