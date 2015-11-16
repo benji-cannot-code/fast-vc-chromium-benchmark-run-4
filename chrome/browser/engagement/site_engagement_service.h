58bf89e777735ed97e90e132bddcd0af6c89cf33
calamity <calamity@chromium.org>
2015-11-15 17:39:45 -0800

    Implement a finer grained deletion for Site Engagement data on History deletion.
    This CL makes the Site Engagement data deletion query the HistoryService to see
    if  History has any other URLs that fall beneath the origin. This means that
    data will only get deleted if all URLs beneath the origin are deleted rather
    than the original behavior of deleting origin data every time any URL beneath
    it is deleted.
    
    BUG=553728
    
    Review URL: https://codereview.chromium.org/1423253003
    
    Cr-Commit-Position: refs/heads/master@{#359795}

--- chrome/browser/engagement/site_engagement_service.h
+++ chrome/browser/engagement/site_engagement_service.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "chrome/browser/engagement/site_engagement_metrics.h"
+#include "components/history/core/browser/history_service_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "ui/base/page_transition_types.h"
 
@@ -130,6 +131,7 @@ class SiteEngagementScoreProvider {
 // such as rejecting permission prompts or not responding to notifications, will
 // decrease the site engagement score.
 class SiteEngagementService : public KeyedService,
+                              public history::HistoryServiceObserver,
                               public SiteEngagementScoreProvider {
  public:
   // The name of the site engagement variation field trial.
@@ -139,10 +141,6 @@ class SiteEngagementService : public KeyedService,
 
   // Returns whether or not the SiteEngagementService is enabled.
   static bool IsEnabled();
-
-  // Clears engagement scores for the given origins.
-  static void ClearHistoryForURLs(Profile* profile,
-                                  const std::set<GURL>& origins);
 
   explicit SiteEngagementService(Profile* profile);
   ~SiteEngagementService() override;
@@ -163,6 +161,13 @@ class SiteEngagementService : public KeyedService,
   // tab.
   void HandleMediaPlaying(const GURL& url, bool is_hidden);
 
+  // Overridden from history::HistoryServiceObserver:
+  void OnURLsDeleted(history::HistoryService* history_service,
+                     bool all_history,
+                     bool expired,
+                     const history::URLRows& deleted_rows,
+                     const std::set<GURL>& favicon_urls) override;
+
   // Overridden from SiteEngagementScoreProvider:
   double GetScore(const GURL& url) override;
   double GetTotalEngagementPoints() override;
@@ -174,6 +179,8 @@ class SiteEngagementService : public KeyedService,
   FRIEND_TEST_ALL_PREFIXES(SiteEngagementServiceTest, GetMedianEngagement);
   FRIEND_TEST_ALL_PREFIXES(SiteEngagementServiceTest, GetTotalNavigationPoints);
   FRIEND_TEST_ALL_PREFIXES(SiteEngagementServiceTest, GetTotalUserInputPoints);
+  FRIEND_TEST_ALL_PREFIXES(SiteEngagementServiceTest,
+                           CleanupOriginsOnHistoryDeletion);
 
   // Only used in tests.
   SiteEngagementService(Profile* profile, scoped_ptr<base::Clock> clock);
@@ -195,6 +202,9 @@ class SiteEngagementService : public KeyedService,
   // respectively.
   int OriginsWithMaxDailyEngagement();
   int OriginsWithMaxEngagement(std::map<GURL, double>& score_map);
+
+  void GetCountsForOriginsComplete(
+      const history::OriginCountMap& origin_counts);
 
   Profile* profile_;
 
