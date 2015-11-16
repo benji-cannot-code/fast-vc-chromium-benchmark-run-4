4f7006ae36e441bd0b7bfbcf08f97e13e32deffe
raymes <raymes@chromium.org>
2015-11-15 17:22:41 -0800

    Make ScopingType a property of WebsiteSettingsInfo.
    Currently, whenever a client calls HostContentSettingsMap::SetWebsiteSetting
    they need to provide ContentSettingsPatterns, even though these patterns
    should be consistent across all calls for a given ContentSettingsType.
    Moreover, it is unclear what the meaning of primary pattern and secondary
    patterns are for each ContentSettingType. This CL creates a propery of
    WebsiteSettingsInfo which specifies what the scoping of that setting is. This
    will hopefully make the code more understandable and more consistent.
    
    The ScopingType is not used in this CL but followup CLs will use this to
    generate ContentSettingsPatterns from URLs rather than passing them in.
    
    BUG=551747
    
    Review URL: https://codereview.chromium.org/1418743007
    
    Cr-Commit-Position: refs/heads/master@{#359794}

--- components/content_settings/core/browser/website_settings_registry.h
+++ components/content_settings/core/browser/website_settings_registry.h
@@ -47,7 +47,8 @@ class WebsiteSettingsRegistry {
       const std::string& name,
       scoped_ptr<base::Value> initial_default_value,
       WebsiteSettingsInfo::SyncStatus sync_status,
-      WebsiteSettingsInfo::LossyStatus lossy_status);
+      WebsiteSettingsInfo::LossyStatus lossy_status,
+      WebsiteSettingsInfo::ScopingType scoping_type);
 
   const_iterator begin() const;
   const_iterator end() const;
