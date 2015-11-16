440680b7e4a20dc209afd8179ebceb94be30696f
ruuda <ruuda@google.com>
2015-11-16 03:24:31 -0800

    [Tracing] Make AllocationContext constructor private
    This is to prevent unintentionally constructing an uninitialized
    instance. It is still possible to construct an |AllocationContext|
    (apart from copy constructing) by using an initializer list. This is
    fine, because the instance will not be filled with garbage; it will
    be zeroed out. By design, a zeroed instance represents a context with
    unknown type ID and empty backtrace.
    
    Review URL: https://codereview.chromium.org/1416833008
    
    Cr-Commit-Position: refs/heads/master@{#359817}

--- base/trace_event/heap_profiler_allocation_context.cc
+++ base/trace_event/heap_profiler_allocation_context.cc
@@ -8,9 +8,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstring>
 
 #include "base/hash.h"
+#include "base/macros.h"
 
 namespace base {
 namespace trace_event {
+
+// Constructor that does not initialize members.
+AllocationContext::AllocationContext() {}
+
+// static
+AllocationContext AllocationContext::Empty() {
+  AllocationContext ctx;
+
+  for (size_t i = 0; i < arraysize(ctx.backtrace.frames); i++)
+    ctx.backtrace.frames[i] = nullptr;
+
+  ctx.type_id = 0;
+
+  return ctx;
+}
 
 bool operator==(const Backtrace& lhs, const Backtrace& rhs) {
   // Pointer equality of the stack frames is assumed, so instead of doing a deep
