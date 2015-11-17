a66f2e960ad258af48179128e73eded319edce8d
sigbjornf <sigbjornf@opera.com>
2015-11-17 04:04:30 -0800

    Oilpan: tidy up CSSCustomFontData::m_fontFaceSource back ref somewhat.
    R=haraken
    BUG=553613
    
    Review URL: https://codereview.chromium.org/1439033002
    
    Cr-Commit-Position: refs/heads/master@{#360062}

--- third_party/WebKit/Source/core/css/CSSCustomFontData.h
+++ third_party/WebKit/Source/core/css/CSSCustomFontData.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/CSSFontFaceSource.h"
 #include "platform/fonts/CustomFontData.h"
+#include "platform/heap/Handle.h"
 
 namespace blink {
 
@@ -67,11 +68,13 @@ private:
             m_isLoading = source->isLoading();
     }
 
-    RemoteFontFaceSource* m_fontFaceSource;
+    // TODO(Oilpan): consider moving (Custom)FontFace hierarchy to the heap,
+    // thereby making this reference a Member<>.
+    RawPtrWillBeWeakPersistent<RemoteFontFaceSource> m_fontFaceSource;
     FallbackVisibility m_fallbackVisibility;
     mutable bool m_isLoading;
 };
 
-}
+} // namespace blink
 
 #endif // CSSCustomFontData_h
