e599fda5543d2a81318264afd582621aa1f15fbd
sadrul <sadrul@chromium.org>
2015-11-16 18:42:22 -0800

    views/mus: Move ui::ContextFactory creation out of WindowTreeHostMus.
    Create the SurfaceContextFactory from NativeWidgetMus, and set it up in
    aura::Env instance, so that when WindowTreeHostMus is created, it gets the
    correct factory.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1453463002
    
    Cr-Commit-Position: refs/heads/master@{#359983}

--- ui/views/mus/window_tree_host_mus.h
+++ ui/views/mus/window_tree_host_mus.h
@@ -30,7 +30,6 @@ namespace views {
 class InputMethodMUS;
 class NativeWidgetMus;
 class PlatformWindowMus;
-class SurfaceContextFactory;
 
 class VIEWS_MUS_EXPORT WindowTreeHostMus : public aura::WindowTreeHostPlatform {
  public:
@@ -52,7 +51,6 @@ class VIEWS_MUS_EXPORT WindowTreeHostMus : public aura::WindowTreeHostPlatform {
 
   NativeWidgetMus* native_widget_;
   scoped_ptr<InputMethodMUS> input_method_;
-  scoped_ptr<SurfaceContextFactory> context_factory_;
   ui::PlatformWindowState show_state_;
 
   DISALLOW_COPY_AND_ASSIGN(WindowTreeHostMus);
