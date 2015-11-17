f0bb4f1bd26d81cecac0bc38930893e8374a1666
jrummell <jrummell@chromium.org>
2015-11-17 13:17:04 -0800

    Remove async=false when loading scripts for EME tests
    async is a boolean attribute, so any specification of it makes it true.
    Since we want the scripts to execute in order, it shouldn't be
    mentioned at all.
    
    BUG=555744
    TEST=affected tests pass
    
    Review URL: https://codereview.chromium.org/1454803002
    
    Cr-Commit-Position: refs/heads/master@{#360161}

--- media/test/data/eme_player_js/app_loader.js
+++ media/test/data/eme_player_js/app_loader.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Adds a Javascript source tag to the document.
 function addScriptTag(src) {
   document.write(
-      '<script type="text/javascript" async="false" src="eme_player_js/' + src +
+      '<script type="text/javascript" src="eme_player_js/' + src +
       '"></script>');
 }
 
