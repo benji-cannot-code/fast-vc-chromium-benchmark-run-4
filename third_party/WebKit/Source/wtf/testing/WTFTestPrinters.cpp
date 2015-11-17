46202f2b0149de2fd7cab58fa392cc7f5c55c671
dominicc <dominicc@chromium.org>
2015-11-16 21:21:44 -0800

    Setting link.sizes.value should be reflected in the sizes attribute.
    BUG=545518
    
    Review URL: https://codereview.chromium.org/1435223002
    
    Cr-Commit-Position: refs/heads/master@{#360008}

--- third_party/WebKit/Source/wtf/testing/WTFTestPrinters.cpp
+++ third_party/WebKit/Source/wtf/testing/WTFTestPrinters.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
+#include "wtf/text/AtomicString.h"
 #include "wtf/text/WTFString.h"
 #include <ostream> // NOLINT
 
@@ -77,6 +78,11 @@ std::ostream& operator<<(std::ostream& out, const String& string)
         }
     }
     return out << '"';
+}
+
+std::ostream& operator<<(std::ostream& out, const AtomicString& s)
+{
+    return out << s.string();
 }
 
 } // namespace WTF
