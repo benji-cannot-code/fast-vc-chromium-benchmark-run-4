a2a3f3a3e7cda95d952607ceba63e30bfdf2ef64
mdw <mdw@chromium.org>
2015-11-16 21:35:10 -0800

    Add UMA to count the number of Google CAPTCHA pages shown and solved by users.
    This is intended to track down problems where a large number of mobile
    users in certain countries are apparently seeing CAPTCHA pages.
    (http://b2.corp.google.com/23081866 for context.)
    
    I realize this may not be the ideal place for this code - happy for suggestions as to where else this might go.
    
    BUG=552470
    
    Review URL: https://codereview.chromium.org/1441393002
    
    Cr-Commit-Position: refs/heads/master@{#360011}

--- chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/page_load_metrics/observers/from_gws_page_load_metrics_observer.h"
+#include "chrome/browser/page_load_metrics/observers/google_captcha_observer.h"
 #include "chrome/browser/prerender/prerender_contents.h"
+#include "components/page_load_metrics/browser/metrics_web_contents_observer.h"
 #include "components/rappor/rappor_service.h"
 #include "content/public/browser/web_contents.h"
 
@@ -20,6 +22,8 @@ void RegisterPageLoadMetricsObservers(
   // This is a self-destruct class, and will delete itself when triggered by
   // OnPageLoadMetricsGoingAway.
   metrics->AddObserver(new FromGWSPageLoadMetricsObserver(metrics));
+  metrics->AddObserver(
+      new google_captcha_observer::GoogleCaptchaObserver(metrics));
 }
 
 }  // namespace
