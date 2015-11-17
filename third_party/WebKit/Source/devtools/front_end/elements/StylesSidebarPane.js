6188d34729fae5a486ece3b68cdc7f5598132f28
lushnikov <lushnikov@chromium.org>
2015-11-17 03:19:05 -0800

    DevTools: long-hover selector highlight should work in shadow DOM
    R=dgozman
    
    Review URL: https://codereview.chromium.org/1441253002
    
    Cr-Commit-Position: refs/heads/master@{#360055}

--- third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
@@ -922,7 +922,6 @@ WebInspector.StylePropertiesSection.prototype = {
         var domModel = node.domModel();
         var selectors = this._style.parentRule ? this._style.parentRule.selectorText() : undefined;
         domModel.highlightDOMNodeWithConfig(node.id, { mode: "all", showInfo: undefined, selectors: selectors });
-        this._activeHighlightDOMModel = domModel;
     },
 
     /**
