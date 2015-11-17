ce094f1899b77af8dadbfca856b773e7f1707f8e
sievers <sievers@chromium.org>
2015-11-17 15:55:21 -0800

    aura: Create compositor in WindowTreeHostPlaftorm constructor
    On desktop platforms the widget happens to become available
    instantly when the PlatformWindow is created. On Android this is
    not the case, and we might crash with a null compositor() if we
    try to do anything in the meantime.
    (So this should be a nonfunctional change on desktop, since
    PlatformWindow is already being created in the constructor.)
    
    ui::Compositor now handles this sequence, see
    https://codereview.chromium.org/1373033004/
    
    Also, now that WTHPlatform supporting OnAcceleratedWidgetDestroyed()
    means that OnAcceleratedWidgetAvailable()
    can be called more than once, recreating the compositor
    from in there will create state mismatch bugs.
    
    BUG=549762,538713
    
    Review URL: https://codereview.chromium.org/1429013003
    
    Cr-Commit-Position: refs/heads/master@{#360207}

--- ui/aura/window_tree_host_platform.cc
+++ ui/aura/window_tree_host_platform.cc
@@ -50,7 +50,9 @@ WindowTreeHostPlatform::WindowTreeHostPlatform(const gfx::Rect& bounds)
 
 WindowTreeHostPlatform::WindowTreeHostPlatform()
     : widget_(gfx::kNullAcceleratedWidget),
-      current_cursor_(ui::kCursorNull) {}
+      current_cursor_(ui::kCursorNull) {
+  CreateCompositor();
+}
 
 void WindowTreeHostPlatform::SetPlatformWindow(
     scoped_ptr<ui::PlatformWindow> window) {
@@ -167,7 +169,6 @@ void WindowTreeHostPlatform::OnAcceleratedWidgetAvailable(
     gfx::AcceleratedWidget widget,
     float device_pixel_ratio) {
   widget_ = widget;
-  CreateCompositor();
   WindowTreeHost::OnAcceleratedWidgetAvailable();
 }
 
