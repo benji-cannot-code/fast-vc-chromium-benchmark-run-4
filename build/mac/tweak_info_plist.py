aa9c45b769edd6093aad856de7bcf7aa5376f8f6
mark <mark@chromium.org>
2015-11-17 12:39:08 -0800

    mac: Remove vestiges of 32-bit deprecation
    32-bit support was dropped in Chrome 39, a year ago. It seems like a
    good time to stop carrying this stuff around.
    
    Review URL: https://codereview.chromium.org/1438313003
    
    Cr-Commit-Position: refs/heads/master@{#360153}

--- build/mac/tweak_info_plist.py
+++ build/mac/tweak_info_plist.py
@@ -160,7 +160,7 @@ def _RemoveBreakpadKeys(plist):
 def _TagSuffixes():
   # Keep this list sorted in the order that tag suffix components are to
   # appear in a tag value. That is to say, it should be sorted per ASCII.
-  components = ('32bit', 'full')
+  components = ('full',)
   assert tuple(sorted(components)) == components
 
   components_len = len(components)
