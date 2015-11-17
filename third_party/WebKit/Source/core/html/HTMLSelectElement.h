0b79e57a07b315eebda8f01f52cab541aede0c65
tkent <tkent@chromium.org>
2015-11-16 21:59:42 -0800

    Improve performance of adding selected OPTIONs to a listbox SELECT element.
    This CL makes scrollToIndex asynchronous.
    blink_perf.dom:select-multiple-add is 200x faster with this CL.
    
    BUG=554978
    
    Review URL: https://codereview.chromium.org/1448113003
    
    Cr-Commit-Position: refs/heads/master@{#360018}

--- third_party/WebKit/Source/core/html/HTMLSelectElement.h
+++ third_party/WebKit/Source/core/html/HTMLSelectElement.h
@@ -240,6 +240,7 @@ private:
     int nextSelectableListIndexPageAway(int startIndex, SkipDirection) const;
     int listIndexForEventTargetOption(const Event&);
     AutoscrollController* autoscrollController() const;
+    void scrollToIndexTask();
 
     void childrenChanged(const ChildrenChange&) override;
     bool areAuthorShadowsAllowed() const override { return false; }
@@ -259,6 +260,7 @@ private:
     RefPtrWillBeMember<HTMLOptionElement> m_lastOnChangeOption;
     int m_activeSelectionAnchorIndex;
     int m_activeSelectionEndIndex;
+    RefPtrWillBeMember<HTMLOptionElement> m_optionToScrollTo;
     bool m_multiple;
     bool m_activeSelectionState;
     mutable bool m_shouldRecalcListItems;
