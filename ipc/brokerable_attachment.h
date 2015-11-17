57178cae3be52f92f5c97ba1e9f65ec3d8947f58
jsbell <jsbell@chromium.org>
2015-11-17 10:16:37 -0800

    IPC: Use <algorithm> helpers for comparing arrays
    Use std::equal and std::lexicographical_compare for comparing nonce
    arrays, rather than manual iteration and comparison.
    
    R=tsepez@chromium.org
    
    Review URL: https://codereview.chromium.org/1451173002
    
    Cr-Commit-Position: refs/heads/master@{#360115}

--- ipc/brokerable_attachment.h
+++ ipc/brokerable_attachment.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <algorithm>
+
 #include "base/macros.h"
 #include "ipc/ipc_export.h"
 #include "ipc/ipc_message_attachment.h"
@@ -38,21 +40,12 @@ class IPC_EXPORT BrokerableAttachment : public MessageAttachment {
     void SerializeToBuffer(char* start_address, size_t size);
 
     bool operator==(const AttachmentId& rhs) const {
-      for (size_t i = 0; i < kNonceSize; ++i) {
-        if (nonce[i] != rhs.nonce[i])
-          return false;
-      }
-      return true;
+      return std::equal(nonce, nonce + kNonceSize, rhs.nonce);
     }
 
     bool operator<(const AttachmentId& rhs) const {
-      for (size_t i = 0; i < kNonceSize; ++i) {
-        if (nonce[i] < rhs.nonce[i])
-          return true;
-        if (nonce[i] > rhs.nonce[i])
-          return false;
-      }
-      return false;
+      return std::lexicographical_compare(nonce, nonce + kNonceSize, rhs.nonce,
+                                          rhs.nonce + kNonceSize);
     }
   };
 
