8fddde94835910c4c063567d9730a8b6df088638
pkasting <pkasting@chromium.org>
2015-11-18 11:47:41 -0800

    Fix window mask to be correct shape and account for scaling.
    BUG=none
    TEST=Install the "Dots" theme from the webstore, look at the bottom right corner of the window, it should look smoothly rounded and not like it's missing a pixel.
    
    Review URL: https://codereview.chromium.org/1457713002
    
    Cr-Commit-Position: refs/heads/master@{#360394}

--- chrome/browser/ui/views/frame/opaque_browser_frame_view.cc
+++ chrome/browser/ui/views/frame/opaque_browser_frame_view.cc
@@ -275,7 +275,8 @@ void OpaqueBrowserFrameView::GetWindowMask(const gfx::Size& size,
   if (layout_->IsTitleBarCondensed() || frame()->IsFullscreen())
     return;
 
-  views::GetDefaultWindowMask(size, window_mask);
+  views::GetDefaultWindowMask(
+      size, frame()->GetCompositor()->device_scale_factor(), window_mask);
 }
 
 void OpaqueBrowserFrameView::ResetWindowControls() {
