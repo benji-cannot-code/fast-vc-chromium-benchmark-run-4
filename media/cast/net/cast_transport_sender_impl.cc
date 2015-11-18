2f53b4363b4889e810f4394a50c4ea989bfd90f0
miu <miu@chromium.org>
2015-11-17 18:24:31 -0800

    Cast Streaming: Enable non-blocking IO by default for Windows.
    BUG=556910
    
    Review URL: https://codereview.chromium.org/1458753002
    
    Cr-Commit-Position: refs/heads/master@{#360268}

--- media/cast/net/cast_transport_sender_impl.cc
+++ media/cast/net/cast_transport_sender_impl.cc
@@ -20,7 +20,7 @@ namespace {
 // See header file for what these mean.
 const char kOptionDscp[] = "DSCP";
 #if defined(OS_WIN)
-const char kOptionNonBlockingIO[] = "non_blocking_io";
+const char kOptionDisableNonBlockingIO[] = "disable_non_blocking_io";
 #endif
 const char kOptionPacerTargetBurstSize[] = "pacer_target_burst_size";
 const char kOptionPacerMaxBurstSize[] = "pacer_max_burst_size";
@@ -135,7 +135,7 @@ CastTransportSenderImpl::CastTransportSenderImpl(
       transport_->SetDscp(net::DSCP_AF41);
     }
 #if defined(OS_WIN)
-    if (options->HasKey(kOptionNonBlockingIO)) {
+    if (!options->HasKey(kOptionDisableNonBlockingIO)) {
       transport_->UseNonBlockingIO();
     }
 #endif
