48b36c7368ce4bbe3d451ab8fe7c50eeca13cc85
yzshen <yzshen@chromium.org>
2015-11-18 09:53:46 -0800

    Revert the work of supporting WorkerPool clean shutdown on POSIX.
    This feature was previously requested by the mandoline project. Now the project doesn't need it anymore.
    
    This reverts the following commits with conflict resolution:
    da8f981cd01fc15b878c6bfd692adbd3c151855a
    455f7e6bde0882fcc8fcfeac1abaeb35e28709e3
    2338784958b986a1280d7aa67e34dc0766b746d2
    
    BUG=498986
    
    Review URL: https://codereview.chromium.org/1449553003
    
    Cr-Commit-Position: refs/heads/master@{#360360}

--- base/threading/worker_pool_win.cc
+++ base/threading/worker_pool_win.cc
@@ -71,10 +71,4 @@ bool WorkerPool::RunsTasksOnCurrentThread() {
   return g_worker_pool_running_on_this_thread.Get().Get();
 }
 
-// static
-void WorkerPool::ShutDownCleanly() {
-  // TODO(yzshen): implement it.
-  NOTIMPLEMENTED();
-}
-
 }  // namespace base
