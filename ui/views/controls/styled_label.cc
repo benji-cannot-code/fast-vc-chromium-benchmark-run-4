1a488d61f9715d84084902f5b72789383158ead8
estade <estade@chromium.org>
2015-11-18 14:40:52 -0800

    Use correct native theme for StyledLabel labels and links.
    BUG=556772
    
    Review URL: https://codereview.chromium.org/1451933005
    
    Cr-Commit-Position: refs/heads/master@{#360432}

--- ui/views/controls/styled_label.cc
+++ ui/views/controls/styled_label.cc
@@ -47,7 +47,8 @@ scoped_ptr<Label> CreateLabelRange(
     result.reset(new Label(text));
   }
 
-  result->SetEnabledColor(style_info.color);
+  if (style_info.color != SK_ColorTRANSPARENT)
+    result->SetEnabledColor(style_info.color);
   result->SetFontList(font_list);
 
   if (!style_info.tooltip.empty())
@@ -67,8 +68,7 @@ scoped_ptr<Label> CreateLabelRange(
 
 StyledLabel::RangeStyleInfo::RangeStyleInfo()
     : font_style(gfx::Font::NORMAL),
-      color(ui::NativeTheme::instance()->GetSystemColor(
-          ui::NativeTheme::kColorId_LabelEnabledColor)),
+      color(SK_ColorTRANSPARENT),
       disable_line_wrapping(false),
       is_link(false) {}
 
@@ -79,8 +79,6 @@ StyledLabel::RangeStyleInfo StyledLabel::RangeStyleInfo::CreateForLink() {
   RangeStyleInfo result;
   result.disable_line_wrapping = true;
   result.is_link = true;
-  result.color = ui::NativeTheme::instance()->GetSystemColor(
-      ui::NativeTheme::kColorId_LinkEnabled);
   return result;
 }
 
