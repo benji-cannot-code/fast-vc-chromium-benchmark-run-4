a419567925a859d6e19ebc86c4fc033e7851e49f
yolandyan <yolandyan@google.com>
2015-11-17 16:11:41 -0800

    Change user agent in TelemetryActivity in system webview shell for perf testing with Extra String that comes with the intent
    This is to fix one of the issues in android_webview_aosp_perf. Android
    WebView doesn't support "--user-agent" commandline flag and it causing
    some of the pages to reload in Telemetry tests.
    
    BUG=554430
    
    Review URL: https://codereview.chromium.org/1433193002
    
    Cr-Commit-Position: refs/heads/master@{#360219}

--- android_webview/tools/WebViewShell/src/org/chromium/webview_shell/TelemetryActivity.java
+++ android_webview/tools/WebViewShell/src/org/chromium/webview_shell/TelemetryActivity.java
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.webview_shell;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.CookieManager;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -25,7 +27,14 @@ public class TelemetryActivity extends Activity {
 
         WebView webView = (WebView) findViewById(R.id.webview);
         CookieManager.setAcceptFileSchemeCookies(true);
-        webView.getSettings().setJavaScriptEnabled(true);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        Intent intent = getIntent();
+        String userAgentString = intent.getStringExtra("userAgent");
+        if (userAgentString != null) {
+            settings.setUserAgentString(userAgentString);
+        }
 
         webView.setWebViewClient(new WebViewClient() {
                 @Override
