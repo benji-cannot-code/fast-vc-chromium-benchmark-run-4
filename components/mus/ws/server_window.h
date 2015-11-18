67598bf3e71a5161145882fe5d9e2a7d234fe51e
sadrul <sadrul@chromium.org>
2015-11-17 23:23:13 -0800

    mus: Introduce SetCanFocus to Window/ServerWindow.
    Allow the client to tell mus whether a window can be focused/activated
    or not.
    
    BUG=548422
    
    Review URL: https://codereview.chromium.org/1458613002
    
    Cr-Commit-Position: refs/heads/master@{#360300}

--- components/mus/ws/server_window.h
+++ components/mus/ws/server_window.h
@@ -120,6 +120,9 @@ class ServerWindow {
     return text_input_state_;
   }
 
+  void set_can_focus(bool can_focus) { can_focus_ = can_focus; }
+  bool can_focus() const { return can_focus_; }
+
   // Returns true if this window is attached to a root and all ancestors are
   // visible.
   bool IsDrawn() const;
@@ -171,6 +174,7 @@ class ServerWindow {
   gfx::Insets client_area_;
   scoped_ptr<ServerWindowSurfaceManager> surface_manager_;
   float opacity_;
+  bool can_focus_;
   gfx::Transform transform_;
   ui::TextInputState text_input_state_;
 
