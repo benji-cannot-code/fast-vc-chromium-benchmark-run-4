45966842c0c3f5900bf9f6b8189a8c87fc18f97d
kjellander <kjellander@chromium.org>
2015-11-17 23:35:56 -0800

    Update common_video/interface -> common_video/include.
    This was recently renamed in WebRTC (https://codereview.webrtc.org/1418913006/).
    and should have been updated as part of the roll in
    https://codereview.chromium.org/1451363003
    
    TBR=tommi@chromium.org
    BUG=webrtc:5095
    
    Review URL: https://codereview.chromium.org/1454053002
    
    Cr-Commit-Position: refs/heads/master@{#360302}

--- content/renderer/media/webrtc/webrtc_video_capturer_adapter.cc
+++ content/renderer/media/webrtc/webrtc_video_capturer_adapter.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/libjingle/source/talk/media/webrtc/webrtcvideoframe.h"
 #include "third_party/libyuv/include/libyuv/convert_from.h"
 #include "third_party/libyuv/include/libyuv/scale.h"
-#include "third_party/webrtc/common_video/interface/video_frame_buffer.h"
+#include "third_party/webrtc/common_video/include/video_frame_buffer.h"
 #include "third_party/webrtc/common_video/rotation.h"
 
 namespace content {
--- content/renderer/media/webrtc/webrtc_video_frame_adapter.h
+++ content/renderer/media/webrtc/webrtc_video_frame_adapter.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_RENDERER_MEDIA_WEBRTC_WEBRTC_VIDEO_FRAME_ADAPTER_H_
 
 #include "media/base/video_frame.h"
-#include "third_party/webrtc/common_video/interface/video_frame_buffer.h"
+#include "third_party/webrtc/common_video/include/video_frame_buffer.h"
 
 namespace content {
 // Thin adapter from media::VideoFrame to webrtc::VideoFrameBuffer. This
