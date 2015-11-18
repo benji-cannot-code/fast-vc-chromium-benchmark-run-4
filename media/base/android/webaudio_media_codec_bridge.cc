52e37de43555ba55d3f36732a9e014d01fb73fa4
mostynb <mostynb@opera.com>
2015-11-17 19:06:54 -0800

    include what you use: errno.h and string.h in media/
    Add missing errno.h includes for errno and string.h includes for
    strerror.
    
    TBR=qinmin
    
    Review URL: https://codereview.chromium.org/1429383002
    
    Cr-Commit-Position: refs/heads/master@{#360276}

--- media/base/android/webaudio_media_codec_bridge.cc
+++ media/base/android/webaudio_media_codec_bridge.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <errno.h>
 #include <fcntl.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
