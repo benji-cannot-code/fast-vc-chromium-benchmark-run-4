669b93c744a476c197c5ade4c4b3eadb3d4934ec
peter <peter@chromium.org>
2015-11-18 10:56:27 -0800

    Remove some FIXMEs from Push code
    R=mvanouwerkerk
    BUG=440431
    
    Review URL: https://codereview.chromium.org/1460613003
    
    Cr-Commit-Position: refs/heads/master@{#360379}

--- third_party/WebKit/Source/modules/push_messaging/PushManager.cpp
+++ third_party/WebKit/Source/modules/push_messaging/PushManager.cpp
@@ -64,7 +64,6 @@ ScriptPromise PushManager::subscribe(ScriptState* scriptState, const PushSubscri
     // different contexts can succeed.
     if (scriptState->executionContext()->isDocument()) {
         Document* document = toDocument(scriptState->executionContext());
-        // FIXME: add test coverage for this condition - https://crbug.com/440431
         if (!document->domWindow() || !document->frame())
             return ScriptPromise::rejectWithDOMException(scriptState, DOMException::create(InvalidStateError, "Document is detached from window."));
         PushController::clientFrom(document->frame()).subscribe(m_registration->webRegistration(), toWebPushSubscriptionOptions(options), new PushSubscriptionCallbacks(resolver, m_registration));
@@ -88,7 +87,6 @@ ScriptPromise PushManager::permissionState(ScriptState* scriptState, const PushS
 {
     if (scriptState->executionContext()->isDocument()) {
         Document* document = toDocument(scriptState->executionContext());
-        // FIXME: add test coverage for this condition - https://crbug.com/440431
         if (!document->domWindow() || !document->frame())
             return ScriptPromise::rejectWithDOMException(scriptState, DOMException::create(InvalidStateError, "Document is detached from window."));
     }
