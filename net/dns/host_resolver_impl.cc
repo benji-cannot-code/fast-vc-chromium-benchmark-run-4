00540ec7f9e9ab5c524e13979f6926de1994e6ed
olli.raula <olli.raula@intel.com>
2015-11-18 04:44:04 -0800

    Remove deprecated ScopedVector from host_resolver_impl
    Also removed typedef as it is not usefull anymore with
    c++11 forloops. There is still one ScopedVector but I will
    remove it as separated patch as it is not so easy as this is.
    
    BUG=554289
    
    Review URL: https://codereview.chromium.org/1446983002
    
    Cr-Commit-Position: refs/heads/master@{#360324}

--- net/dns/host_resolver_impl.cc
+++ net/dns/host_resolver_impl.cc
@@ -1287,9 +1287,7 @@ class HostResolverImpl::Job : public PrioritizedDispatcher::Job,
     // else CompleteRequests logged EndEvent.
 
     // Log any remaining Requests as cancelled.
-    for (RequestsList::const_iterator it = requests_.begin();
-         it != requests_.end(); ++it) {
-      Request* req = *it;
+    for (const scoped_ptr<Request>& req : requests_) {
       if (req->was_canceled())
         continue;
       DCHECK_EQ(this, req->job());
@@ -1731,10 +1729,7 @@ class HostResolverImpl::Job : public PrioritizedDispatcher::Job,
       resolver_->CacheResult(key_, entry, ttl);
 
     // Complete all of the requests that were attached to the job.
-    for (RequestsList::const_iterator it = requests_.begin();
-         it != requests_.end(); ++it) {
-      Request* req = *it;
-
+    for (const scoped_ptr<Request>& req : requests_) {
       if (req->was_canceled())
         continue;
 
@@ -1808,7 +1803,7 @@ class HostResolverImpl::Job : public PrioritizedDispatcher::Job,
   scoped_ptr<DnsTask> dns_task_;
 
   // All Requests waiting for the result of this Job. Some can be canceled.
-  RequestsList requests_;
+  std::vector<scoped_ptr<Request>> requests_;
 
   // A handle used in |HostResolverImpl::dispatcher_|.
   PrioritizedDispatcher::Handle handle_;
--- net/dns/host_resolver_impl.h
+++ net/dns/host_resolver_impl.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/memory/scoped_vector.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/non_thread_safe.h"
 #include "base/time/time.h"
@@ -152,7 +151,6 @@ class NET_EXPORT HostResolverImpl
   class Request;
   typedef HostCache::Key Key;
   typedef std::map<Key, Job*> JobMap;
-  typedef ScopedVector<Request> RequestsList;
 
   // Number of consecutive failures of DnsTask (with successful fallback to
   // ProcTask) before the DnsClient is disabled until the next DNS change.
