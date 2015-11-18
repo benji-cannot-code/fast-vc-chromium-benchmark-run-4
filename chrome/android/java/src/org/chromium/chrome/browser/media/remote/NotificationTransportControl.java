12dfe2aadd1210eaa0be5122f0c22881dc3d6f5b
avayvod <avayvod@chromium.org>
2015-11-18 13:04:47 -0800

    Don't crash if the Cast Notification Service in the wrong state
    It seems that old versions of Android sometimes incorrectly restart
    services. Previously there was an explicit check for this in the
    Cast Notification Service that crashed Chrome if it happened (hence
    giving user visible crashes). Replace this with a check that simply
    closes down the service.
    
    BUG=555266
    TBR=aberent,dgn
    
    patch from issue 1458903002 at patchset 1 (http://crrev.com/1458903002#ps1)
    
    Review URL: https://codereview.chromium.org/1459473004
    
    Cr-Commit-Position: refs/heads/master@{#360411}

--- chrome/android/java/src/org/chromium/chrome/browser/media/remote/NotificationTransportControl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/remote/NotificationTransportControl.java
@@ -71,8 +71,9 @@ public class NotificationTransportControl
 
         @Override
         public void onCreate() {
-            checkState(sInstance != null);
-            checkState(sInstance.mService == null);
+            // If Android recreates the service the state can be wrong, see
+            // https://crbug.com/555266
+            if (sInstance == null || sInstance.mService != null) return;
             super.onCreate();
 
             sInstance.mService = this;
@@ -90,8 +91,7 @@ public class NotificationTransportControl
 
         @Override
         public void onDestroy() {
-            checkState(sInstance != null);
-            checkState(sInstance.mService != null);
+            if (sInstance == null || sInstance.mService == null) return;
             stop();
             sInstance.mService = null;
         }
@@ -103,7 +103,11 @@ public class NotificationTransportControl
 
         @Override
         public int onStartCommand(Intent intent, int flags, int startId) {
-            checkState(sInstance != null);
+            if (sInstance == null) {
+                stopSelf();
+                return START_NOT_STICKY;
+            }
+
             if (intent != null) {
                 String action = intent.getAction();
                 if (action != null && action.startsWith(ACTION_PREFIX)) {
@@ -331,19 +335,6 @@ public class NotificationTransportControl
     @VisibleForTesting
     static NotificationTransportControl getIfExists() {
         return sInstance;
-    }
-
-    /**
-     * Ensures the truth of an expression involving the state of the calling instance, but not
-     * involving any parameters to the calling method.
-     *
-     * @param expression a boolean expression
-     * @throws IllegalStateException if {@code expression} is false
-     */
-    private static void checkState(boolean expression) {
-        if (!expression) {
-            throw new IllegalStateException();
-        }
     }
 
     /**
