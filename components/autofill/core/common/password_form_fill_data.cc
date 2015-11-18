f2b27597724860e02536169499444ac79877f8a4
jsbell <jsbell@chromium.org>
2015-11-18 10:31:01 -0800

    Use std::tie() for operator< in components/
    Simplify the code for operator< when comparing multiple members using
    a common std::tie idiom.
    
    BUG=555171
    R=thakis@chromium.org,vabr@chromium.org,jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/1447153002
    
    Cr-Commit-Position: refs/heads/master@{#360367}

--- components/autofill/core/common/password_form_fill_data.cc
+++ components/autofill/core/common/password_form_fill_data.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/common/password_form_fill_data.h"
 
+#include <tuple>
+
 #include "base/strings/utf_string_conversions.h"
 #include "components/autofill/core/common/form_field_data.h"
 
@@ -16,11 +18,8 @@ UsernamesCollectionKey::~UsernamesCollectionKey() {}
 
 bool UsernamesCollectionKey::operator<(
     const UsernamesCollectionKey& other) const {
-  if (username != other.username)
-    return username < other.username;
-  if (password != other.password)
-    return password < other.password;
-  return realm < other.realm;
+  return std::tie(username, password, realm) <
+         std::tie(other.username, other.password, other.realm);
 }
 
 PasswordFormFillData::PasswordFormFillData()
