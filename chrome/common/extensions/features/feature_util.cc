57462e53ff47cc17dc74aac7fa9e1e1e44fa41d1
alemate <alemate@chromium.org>
2015-11-18 03:25:13 -0800

    Reland of [Extensions] Finish migrating ServiceWorker off the Behavior Feature system (patchset #1 id:1 of https://codereview.chromium.org/1461563002/ )
    Reason for revert:
    Reverting speculative revert as it didn't help tests.
    
    Original issue's description:
    > Revert of [Extensions] Finish migrating ServiceWorker off the Behavior Feature system (patchset #1 id:40001 of https://codereview.chromium.org/1442183003/ )
    >
    > Reason for revert:
    > Speculatively reverting this CL because of cros_trunk build bot failure. See crbug.com/557449 for details.
    >
    > Original issue's description:
    > > [Extensions] Finish migrating ServiceWorker off the Behavior Feature system
    > >
    > > The feature system is flaky for some reason and causing crashes. Move at least
    > > service worker off it, since it's easy and is causing the most crashes.
    > >
    > > BUG=461915
    > >
    > > Committed: https://crrev.com/9a55fac787ba88f4139c2db41ea87be669272246
    > > Cr-Commit-Position: refs/heads/master@{#360173}
    >
    > TBR=rockot@chromium.org,thestig@chromium.org,rdevlin.cronin@chromium.org
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=461915,557449
    >
    > Committed: https://crrev.com/cf65af41f474e5118c2c8bfdd00999b39e24f291
    > Cr-Commit-Position: refs/heads/master@{#360311}
    
    TBR=rockot@chromium.org,thestig@chromium.org,rdevlin.cronin@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=461915,557449
    
    Review URL: https://codereview.chromium.org/1455133002
    
    Cr-Commit-Position: refs/heads/master@{#360321}

---
+++ chrome/common/extensions/features/feature_util.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/common/extensions/features/feature_util.h"
+
+#include "chrome/common/extensions/features/feature_channel.h"
+#include "components/version_info/version_info.h"
+
+namespace extensions {
+namespace feature_util {
+
+bool ExtensionServiceWorkersEnabled() {
+  return GetCurrentChannel() == version_info::Channel::UNKNOWN;
+}
+
+}  // namespace feature_util
+}  // namespace extensions
