835f9970042501fd6686fcda699cd25f0805d7ff
sigbjornf <sigbjornf@opera.com>
2015-11-19 07:11:40 -0800

    More regular Platform implementations in unit tests.
    R=haraken,jbroman
    BUG=
    
    Review URL: https://codereview.chromium.org/1456873003
    
    Cr-Commit-Position: refs/heads/master@{#360583}

--- third_party/WebKit/Source/core/animation/CompositorAnimationsTestHelper.h
+++ third_party/WebKit/Source/core/animation/CompositorAnimationsTestHelper.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CompositorAnimationsTestHelper_h
 
 #include "core/animation/CompositorAnimations.h"
-#include "public/platform/Platform.h"
+#include "platform/testing/TestingPlatformSupport.h"
 #include "public/platform/WebCompositorAnimationPlayer.h"
 #include "public/platform/WebCompositorAnimationTimeline.h"
 #include "public/platform/WebCompositorSupport.h"
@@ -153,31 +153,13 @@ public:
     };
 
 private:
-    class PlatformProxy : public Platform {
+    class PlatformProxy : public TestingPlatformSupport {
     public:
-        PlatformProxy(WebCompositorSupportMock** compositor) : m_platform(Platform::current()), m_compositor(compositor) { }
-
-        ~PlatformProxy()
-        {
-            blink::Platform::initialize(m_platform);
-        }
-
-        virtual void cryptographicallyRandomValues(unsigned char* buffer, size_t length) { ASSERT_NOT_REACHED(); }
-        const unsigned char* getTraceCategoryEnabledFlag(const char* categoryName) override
-        {
-            static const unsigned char tracingIsDisabled = 0;
-            return &tracingIsDisabled;
-        }
-
-        WebThread* currentThread() override
-        {
-            return m_platform->currentThread();
-        }
-
+        explicit PlatformProxy(WebCompositorSupportMock** compositor) : m_compositor(compositor) { }
     private:
-        blink::Platform* m_platform; // Not owned.
-        WebCompositorSupportMock** m_compositor;
         WebCompositorSupport* compositorSupport() override { return *m_compositor; }
+
+        WebCompositorSupportMock** m_compositor;
     };
 
     WebCompositorSupportMock* m_mockCompositor;
