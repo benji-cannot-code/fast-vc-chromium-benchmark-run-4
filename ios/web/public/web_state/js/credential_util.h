907ba9db4b64b5fe71d5c822b048bb751a52efb2
vabr <vabr@chromium.org>
2015-11-19 09:42:48 -0800

    Move JS-related password manager code upstream
    Moves
    credential_manager.h
    credential_manager.mm
    js_credential_manager.h
    js_credential_manager.mm
    js_password_manager.h
    js_password_manager.mm
    resources/credential_manager.js
    resources/password_controller.js
    from the downstream to ios/chrome/browser/passwords.
    
    This is the upstream part of the internal CL 310987013.
    
    BUG=514241
    
    Review URL: https://codereview.chromium.org/1456983002
    
    Cr-Commit-Position: refs/heads/master@{#360617}

--- ios/web/public/web_state/js/credential_util.h
+++ ios/web/public/web_state/js/credential_util.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_WEB_WEB_STATE_JS_CREDENTIAL_UTIL_H_
-#define IOS_WEB_WEB_STATE_JS_CREDENTIAL_UTIL_H_
+#ifndef IOS_WEB_PUBLIC_WEB_STATE_JS_CREDENTIAL_UTIL_H_
+#define IOS_WEB_PUBLIC_WEB_STATE_JS_CREDENTIAL_UTIL_H_
 
 namespace base {
 class DictionaryValue;
@@ -45,4 +45,4 @@ void CredentialToDictionaryValue(const Credential& credential,
 
 }  // namespace web
 
-#endif  // IOS_WEB_WEB_STATE_JS_CREDENTIAL_UTIL_H_
+#endif  // IOS_WEB_PUBLIC_WEB_STATE_JS_CREDENTIAL_UTIL_H_
--- ios/web/web_state/js/credential_util_unittest.mm
+++ ios/web/web_state/js/credential_util_unittest.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ios/web/web_state/js/credential_util.h"
+#include "ios/web/public/web_state/js/credential_util.h"
 
 #include "base/memory/scoped_ptr.h"
 #include "base/strings/utf_string_conversions.h"
