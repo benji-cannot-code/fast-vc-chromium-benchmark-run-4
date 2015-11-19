2402d3063669df1257aed6d15443fa8b33329edc
davidben <davidben@chromium.org>
2015-11-18 17:44:35 -0800

    EnsureOpenSSLInit before calling into WebRTC.
    BoringSSL may currently only be initialized by one piece of code, so full
    initialization must happen in Chromium code, rather than WebRTC.
    
    Historically, this was because the locking callbacks needed to be configured.
    This is gone, but EnsureOpenSSLInit still configures NEON support on Android
    and detects a CPU with known broken NEON support. Until that's moved to inside
    the library (https://crbug.com/542879), Chromium must initialize some of the
    stack ourselves.
    
    BUG=556462
    
    Review URL: https://codereview.chromium.org/1462443003
    
    Cr-Commit-Position: refs/heads/master@{#360466}

--- content/renderer/media/webrtc/peer_connection_dependency_factory.cc
+++ content/renderer/media/webrtc/peer_connection_dependency_factory.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/render_frame_impl.h"
 #include "content/renderer/render_thread_impl.h"
 #include "content/renderer/render_view_impl.h"
+#include "crypto/openssl_util.h"
 #include "jingle/glue/thread_wrapper.h"
 #include "media/base/media_permission.h"
 #include "media/renderers/gpu_video_accelerator_factories.h"
@@ -361,6 +362,12 @@ void PeerConnectionDependencyFactory::CreatePeerConnectionFactory() {
   CHECK(worker_thread_);
 
   // Init SSL, which will be needed by PeerConnection.
+  //
+  // TODO(davidben): BoringSSL must be initialized by Chromium code. If the
+  // initialization requirement is removed or when different libraries are
+  // allowed to call CRYPTO_library_init concurrently, remove this line and
+  // initialize within WebRTC. See https://crbug.com/542879.
+  crypto::EnsureOpenSSLInit();
   if (!rtc::InitializeSSL()) {
     LOG(ERROR) << "Failed on InitializeSSL.";
     NOTREACHED();
