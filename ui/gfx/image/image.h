0f9387882294626c327afbadb71b22bf16157351
limasdf <limasdf@gmail.com>
2015-11-18 17:18:27 -0800

    ScopedPtrMap -> std::map from /ui/gfx/image
    C++ 11 enables containers that contain move-only type, scoped_ptr.
    So, Use std::map<key, scoped_ptr<Foo>> instead of ScopedPtrMap.
    
    also use std::move() instead of scoped_ptr::Pass().
    
    BUG=554291, 557422
    
    Review URL: https://codereview.chromium.org/1456703004
    
    Cr-Commit-Position: refs/heads/master@{#360461}

--- ui/gfx/image/image.h
+++ ui/gfx/image/image.h
@@ -20,10 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GFX_IMAGE_IMAGE_H_
 #define UI_GFX_IMAGE_IMAGE_H_
 
+#include <map>
 #include <vector>
 
 #include "base/basictypes.h"
-#include "base/containers/scoped_ptr_map.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/memory/scoped_ptr.h"
 #include "ui/gfx/gfx_export.h"
@@ -54,8 +54,8 @@ class GFX_EXPORT Image {
     kImageRepPNG,
   };
 
-  typedef base::ScopedPtrMap<RepresentationType, scoped_ptr<internal::ImageRep>>
-      RepresentationMap;
+  using RepresentationMap =
+      std::map<RepresentationType, scoped_ptr<internal::ImageRep>>;
 
   // Creates an empty image with no representations.
   Image();
