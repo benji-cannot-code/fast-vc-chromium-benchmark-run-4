b86670f942183717835a979dce5eff66b20e04f0
mcasas <mcasas@chromium.org>
2015-11-19 10:21:32 -0800

    media::FFmpegVideoDecoder: remove ctor param |task_runner| and use a ThreadChecker instead.
    This CL removed the |task_runner| passed on construction to
    FFmpegVideoDecoder since: "... is used in just one location
    for PostTask()ing. The class itself is otherwise mostly single
    threaded (except perhaps construction), and passing a
    |task_runner| as ctor argument is confusingly indicating the
    opposite. A |thread_checker_| basically tells that a(ll) methods
    are run in a given thread, whichever this is."
    (from discussion in http://crrev.com/1438233003, which has
    sadly been reverted for unrelated issues, and is going to be
    relanded as http://crrev.com/1447343004).
    
    Removal of this |task_runner| is also necessary for using these
    two classes from WebRtc, see the linekd bug.
    
    BUG=554737
    
    TEST=This is a tiny change, all unittest and content_browsertests should cover it.
    
    Review URL: https://codereview.chromium.org/1456993004
    
    Cr-Commit-Position: refs/heads/master@{#360628}

--- media/filters/ffmpeg_video_decoder.h
+++ media/filters/ffmpeg_video_decoder.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/threading/thread_checker.h"
 #include "media/base/video_decoder.h"
 #include "media/base/video_decoder_config.h"
 #include "media/base/video_frame_pool.h"
@@ -30,8 +31,7 @@ class MEDIA_EXPORT FFmpegVideoDecoder : public VideoDecoder {
  public:
   static bool IsCodecSupported(VideoCodec codec);
 
-  explicit FFmpegVideoDecoder(
-      const scoped_refptr<base::SingleThreadTaskRunner>& task_runner);
+  FFmpegVideoDecoder();
   ~FFmpegVideoDecoder() override;
 
   // Allow decoding of individual NALU. Entire frames are required by default.
@@ -76,7 +76,7 @@ class MEDIA_EXPORT FFmpegVideoDecoder : public VideoDecoder {
   // and resets them to NULL.
   void ReleaseFFmpegResources();
 
-  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+  base::ThreadChecker thread_checker_;
 
   DecoderState state_;
 
