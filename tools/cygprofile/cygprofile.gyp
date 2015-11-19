c5e2194dfe6938e6ec251101f28269e3d4f45f11
pasko <pasko@chromium.org>
2015-11-19 12:25:56 -0800

    Mechanically change TODO(azarchs) into TODO(pasko)
    .. since azarchs left the team
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1462503002
    
    Cr-Commit-Position: refs/heads/master@{#360653}

--- tools/cygprofile/cygprofile.gyp
+++ tools/cygprofile/cygprofile.gyp
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'cygprofile_unittest.cc',
       ],
       'cflags!': [ '-finstrument-functions' ],
-      # TODO(azarchs): Fix the underlying problem (crbug/485542)
+      # TODO(lizeb,pasko): Fix the underlying problem (crbug/485542)
       'ldflags': [ '-Wl,--no-fatal-warnings' ],
       'dependencies': [
         'cygprofile',
--- tools/cygprofile/profile_android_startup.py
+++ tools/cygprofile/profile_android_startup.py
@@ -80,7 +80,7 @@ class WprManager(object):
   def Start(self):
     """Set up the device and host for WPR."""
     self.Stop()
-    #TODO(azarchs): make self._InstallTestCa() work
+    # TODO(lizeb,pasko): make self._InstallTestCa() work
     self._BringUpWpr()
     self._StartForwarder()
 
@@ -88,7 +88,7 @@ class WprManager(object):
     """Clean up the device and host's WPR setup."""
     self._StopForwarder()
     self._StopWpr()
-    #TODO(azarchs): make self._RemoveTestCa() work
+    # TODO(lizeb,pasko): make self._RemoveTestCa() work
 
   def __enter__(self):
     self.Start()
