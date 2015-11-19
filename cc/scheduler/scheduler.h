a5f6649e35f6a7409799f6b9094ba0d3dcc56c8a
brianderson <brianderson@chromium.org>
2015-11-18 18:36:40 -0800

    cc: Don't attempt main thread synchronization if it is slow.
    This patch:
    1) Adds a critical_begin_main_frame_to_activate_is_fast flag.
    2) Separates impl_latency_takes_priority_ into tree_priority_
       and a scroll_handler_state_.
    3) Replaces impl_latency_takes_priority with
       ImplLatencyTakesPriority(), which is now a function of
       the above and is now true when the main thread is too slow.
    
    There is also a small change to the latency recovery logic that
    estimates the main thread response time depending on whether
    the the BeginMainFrame is critical or not.
    
    BUG=469953
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1425973003
    
    Cr-Commit-Position: refs/heads/master@{#360500}

--- cc/scheduler/scheduler.h
+++ cc/scheduler/scheduler.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/scheduler/draw_result.h"
 #include "cc/scheduler/scheduler_settings.h"
 #include "cc/scheduler/scheduler_state_machine.h"
+#include "cc/tiles/tile_priority.h"
 
 namespace base {
 namespace trace_event {
@@ -99,7 +100,8 @@ class CC_EXPORT Scheduler : public BeginFrameObserverBase {
   void DidSwapBuffers();
   void DidSwapBuffersComplete();
 
-  void SetImplLatencyTakesPriority(bool impl_latency_takes_priority);
+  void SetTreePrioritiesAndScrollState(TreePriority tree_priority,
+                                       ScrollHandlerState scroll_handler_state);
 
   void NotifyReadyToCommit();
   void BeginMainFrameAborted(CommitEarlyOutReason reason);
@@ -126,7 +128,7 @@ class CC_EXPORT Scheduler : public BeginFrameObserverBase {
     return !begin_impl_frame_deadline_task_.IsCancelled();
   }
   bool ImplLatencyTakesPriority() const {
-    return state_machine_.impl_latency_takes_priority();
+    return state_machine_.ImplLatencyTakesPriority();
   }
 
   // Pass in a main_thread_start_time of base::TimeTicks() if it is not
@@ -201,9 +203,13 @@ class CC_EXPORT Scheduler : public BeginFrameObserverBase {
   void DrawAndSwapForced();
   void ProcessScheduledActions();
   void UpdateCompositorTimingHistoryRecordingEnabled();
-  bool ShouldRecoverMainLatency(const BeginFrameArgs& args) const;
-  bool ShouldRecoverImplLatency(const BeginFrameArgs& args) const;
-  bool CanCommitAndActivateBeforeDeadline(const BeginFrameArgs& args) const;
+  bool ShouldRecoverMainLatency(const BeginFrameArgs& args,
+                                bool can_activate_before_deadline) const;
+  bool ShouldRecoverImplLatency(const BeginFrameArgs& args,
+                                bool can_activate_before_deadline) const;
+  bool CanBeginMainFrameAndActivateBeforeDeadline(
+      const BeginFrameArgs& args,
+      base::TimeDelta bmf_to_activate_estimate) const;
   void AdvanceCommitStateIfPossible();
   bool IsBeginMainFrameSentOrStarted() const;
   void BeginRetroFrame();
