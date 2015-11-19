7128de1f1813dcbce12de8dc45a9d819c5fe2631
moshayedi <moshayedi@chromium.org>
2015-11-18 16:59:01 -0800

    Aura on Android: Stub Implementation of NativeProcessLauncher.
    BUG=548797
    
    Review URL: https://codereview.chromium.org/1434663004
    
    Cr-Commit-Position: refs/heads/master@{#360457}

---
+++ chrome/browser/extensions/api/messaging/native_process_launcher_android.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/api/messaging/native_process_launcher.h"
+
+namespace extensions {
+
+// static
+base::FilePath NativeProcessLauncher::FindManifest(
+    const std::string& host_name,
+    bool allow_user_level_hosts,
+    std::string* error_message) {
+  return base::FilePath();
+}
+
+// static
+bool NativeProcessLauncher::LaunchNativeProcess(
+    const base::CommandLine& command_line,
+    base::Process* process,
+    base::File* read_file,
+    base::File* write_file) {
+  return false;
+}
+
+}  // namespace extensions
