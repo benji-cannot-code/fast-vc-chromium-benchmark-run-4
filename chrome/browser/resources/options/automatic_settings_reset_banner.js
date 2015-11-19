b7dd530483631ef6f412ffad7e497d215e9b50c9
robertshield <robertshield@chromium.org>
2015-11-19 08:28:19 -0800

    Fix JS whoopsies from https://crrev.com/e2f61d09c3f
    BUG=370966,558025
    
    Review URL: https://codereview.chromium.org/1455063003
    
    Cr-Commit-Position: refs/heads/master@{#360599}

--- chrome/browser/resources/options/automatic_settings_reset_banner.js
+++ chrome/browser/resources/options/automatic_settings_reset_banner.js
@@ -12,7 +12,6 @@ cr.define('options', function() {
    * AutomaticSettingsResetBanner class
    * Provides encapsulated handling of the Reset Profile Settings banner.
    * @constructor
-   * @extends {options.SettingsBannerBase}
    */
   function AutomaticSettingsResetBanner() {}
 
@@ -32,13 +31,13 @@ cr.define('options', function() {
      * our show() method. This would mean that the banner would be first
      * dismissed, then shown. We want to prevent this.
      *
-     * @type {boolean}
-     * @private
+     * @private {boolean}
      */
-    hadBeenDismissed_: false,
+    wasDismissed_: false,
 
     /**
      * Metric name to send when a show event occurs.
+     * @private {string}
      */
     showMetricName_: '',
 
@@ -49,8 +48,9 @@ cr.define('options', function() {
 
     /**
      * DOM element whose visibility is set when setVisibility_ is called.
+     * @private {?HTMLElement}
      */
-    setVisibilibyDomElement_: null,
+    visibleElement_: null,
 
     /**
      * Initializes the banner's event handlers.
@@ -67,7 +67,8 @@ cr.define('options', function() {
       this.dismissNativeCallbackName_ =
           'onDismissedAutomaticSettingsResetBanner';
 
-      this.setVisibilibyDomElement_ = $('automatic-settings-reset-banner');
+      this.visibleElement_ = getRequiredElement(
+          'automatic-settings-reset-banner');
 
       $('automatic-settings-reset-banner-close').onclick = function(event) {
         chrome.send('metricsHandler:recordAction',
@@ -92,7 +93,7 @@ cr.define('options', function() {
      * @protected
      */
     setVisibility: function(show) {
-      this.setVisibilibyDomElement_.hidden = !show;
+      this.visibleElement_.hidden = !show;
     },
 
     /**
@@ -100,7 +101,7 @@ cr.define('options', function() {
      * @private
      */
     show_: function() {
-      if (!this.hadBeenDismissed_) {
+      if (!this.wasDismissed_) {
         chrome.send('metricsHandler:recordAction', [this.showMetricName_]);
         this.setVisibility(true);
       }
@@ -113,8 +114,8 @@ cr.define('options', function() {
      * @private
      */
     dismiss_: function() {
-      chrome.send(this.dismissNativeCallbackName_);
-      this.hadBeenDismissed_ = true;
+      chrome.send(assert(this.dismissNativeCallbackName_));
+      this.wasDismissed_ = true;
       this.setVisibility(false);
     },
   };
