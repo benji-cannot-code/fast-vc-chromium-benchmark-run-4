5511e3b55668a68dc8696dd541067b7f872e6c62
fukino <fukino@chromium.org>
2015-11-18 19:17:11 -0800

    VideoPlayer: VideoPlayer: Fix calculation of window size.
    This is a reland of crrev.com/1451143002, with updating externs file.
    The diff in externs has already been included in upstream (cl/108204037)
    
    New MD player uses system header, so we need to set inner window size rather than outer one.
    Along with the fix, we moved to innerBounds and outerBounds since resizeTo and moveTo is deprecated.
    
    BUG=556394
    TEST=manually
    
    Review URL: https://codereview.chromium.org/1457993002
    
    Cr-Commit-Position: refs/heads/master@{#360511}

--- third_party/closure_compiler/externs/chrome_extensions.js
+++ third_party/closure_compiler/externs/chrome_extensions.js
@@ -460,6 +460,14 @@ chrome.app.window.AppWindow.prototype.onRestored;
 chrome.app.window.AppWindow.prototype.contentWindow;
 
 
+/** @type {!chrome.app.window.Bounds} */
+chrome.app.window.AppWindow.prototype.innerBounds;
+
+
+/** @type {!chrome.app.window.Bounds} */
+chrome.app.window.AppWindow.prototype.outerBounds;
+
+
 /**
  * @typedef {?{
  *   left: number,
