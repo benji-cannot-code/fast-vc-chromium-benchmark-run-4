40708fe1768819149531bfe948baadbc25a3c3c3
pmonette <pmonette@chromium.org>
2015-11-19 12:29:31 -0800

    Make the histograms for setting the default browser consistent
    BUG=
    
    Review URL: https://codereview.chromium.org/1426663005
    
    Cr-Commit-Position: refs/heads/master@{#360654}

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/browser_command_controller.h"
 
+#include <string>
+
 #include "base/command_line.h"
 #include "base/prefs/pref_service.h"
 #include "chrome/app/chrome_command_ids.h"
@@ -166,7 +168,6 @@ class SwitchToMetroUIHandler
       return;
     }
     first_check_ = false;
-    default_browser_worker_->StartCheckIsDefault();
   }
 
   bool IsInteractiveSetDefaultPermitted() override {
