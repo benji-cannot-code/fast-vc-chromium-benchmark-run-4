38d9cd401b143138e0ddbb083b68caa5a2b0bfb3
dalecurtis <dalecurtis@chromium.org>
2015-11-19 15:12:05 -0800

    Avoid resampling for basic playback on Android.
    Just like ChromeOS the hardware can handle the resampling for us without
    any issues, so don't spend CPU cycles on it.  For resampling of MSE content
    we'll still resample for low sample rates (<44.1kHz) to ensure sample rate
    adaptations don't get locked into the low sample rate.
    
    Res      mA    New mA   Delta
    480p30	293	281	-4.10%
    720p30	307	293	-4.56%
    1080p60	442	431	-2.49%
    
    BUG=none
    TEST=2.5-4.5% power delta (mA)
    
    Review URL: https://codereview.chromium.org/1452063002
    
    Cr-Commit-Position: refs/heads/master@{#360679}

--- media/renderers/audio_renderer_impl.cc
+++ media/renderers/audio_renderer_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/single_thread_task_runner.h"
 #include "base/time/default_tick_clock.h"
+#include "build/build_config.h"
 #include "media/base/audio_buffer.h"
 #include "media/base/audio_buffer_converter.h"
 #include "media/base/audio_hardware_config.h"
@@ -358,7 +359,17 @@ void AudioRendererImpl::Initialize(
         // (http://crbug.com/379288), platform specific issues around channel
         // layouts (http://crbug.com/266674), and unnecessary upmixing overhead.
         stream->audio_decoder_config().channel_layout(),
-        hw_params.sample_rate(), hw_params.bits_per_sample(),
+#if defined(OS_CHROMEOS) || defined(OS_ANDROID)
+        // On ChromeOS and Android let the OS level resampler handle resampling
+        // unless the initial sample rate is too low; this allows support for
+        // sample rate adaptations where necessary.
+        stream->audio_decoder_config().samples_per_second() < 44100
+            ? hw_params.sample_rate()
+            : stream->audio_decoder_config().samples_per_second(),
+#else
+        hw_params.sample_rate(),
+#endif
+        hw_params.bits_per_sample(),
         hardware_config_.GetHighLatencyBufferSize());
   }
 
