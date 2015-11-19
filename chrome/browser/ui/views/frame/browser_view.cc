2c734720ddba1ed8e98bcce91f84cbee5ceb61a3
estade <estade@chromium.org>
2015-11-18 18:35:42 -0800

    Vectorize MD otr frame icon
    BUG=501377
    
    Review URL: https://codereview.chromium.org/1452923002
    
    Cr-Commit-Position: refs/heads/master@{#360499}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -134,8 +134,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/geometry/rect_conversions.h"
+#include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/scoped_canvas.h"
 #include "ui/gfx/screen.h"
+#include "ui/gfx/vector_icons_public.h"
 #include "ui/strings/grit/ui_strings.h"
 #include "ui/views/controls/button/menu_button.h"
 #include "ui/views/controls/textfield/textfield.h"
@@ -157,6 +159,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include "base/win/windows_version.h"
 #include "chrome/browser/jumplist_win.h"
+#include "ui/gfx/color_palette.h"
 #include "ui/views/win/scoped_fullscreen_visibility.h"
 #endif
 
@@ -682,7 +685,19 @@ WebContents* BrowserView::GetActiveWebContents() const {
 }
 
 gfx::ImageSkia BrowserView::GetOTRAvatarIcon() const {
-  return *GetThemeProvider()->GetImageSkiaNamed(IDR_OTR_ICON);
+  if (ui::MaterialDesignController::IsModeMaterial()) {
+    SkColor icon_color = SK_ColorWHITE;
+#if defined(OS_WIN)
+    // On Windows 10, we can't change the frame color so must assume it's white.
+    if (base::win::GetVersion() == base::win::VERSION_WIN10)
+      icon_color = gfx::kChromeIconGrey;
+#endif
+    return gfx::CreateVectorIcon(gfx::VectorIconId::INCOGNITO, 24, icon_color);
+  }
+
+  // GetThemeProvider() can return null if this view is not yet part of the
+  // widget hierarchy, but the frame's theme provider is always valid.
+  return *frame_->GetThemeProvider()->GetImageSkiaNamed(IDR_OTR_ICON);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
