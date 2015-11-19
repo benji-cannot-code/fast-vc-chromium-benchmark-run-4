a2b3051f9ac84a2c7b5224523fc57e8cfe437264
pilgrim <pilgrim@chromium.org>
2015-11-18 17:44:53 -0800

    [Line Layout API] Convert LayoutBlockFlowLine to use new line layout API
    There are just a few calls and one comparison. No functional changes.
    
    This CL is not dependent on any other pending CLs.
    
    BUG=499321
    
    Review URL: https://codereview.chromium.org/1441443003
    
    Cr-Commit-Position: refs/heads/master@{#360467}

--- third_party/WebKit/Source/core/layout/api/LineLayoutItem.h
+++ third_party/WebKit/Source/core/layout/api/LineLayoutItem.h
@@ -305,6 +305,11 @@ public:
         return m_layoutObject->hitTest(result, locationInContainer, accumulatedOffset, filter);
     }
 
+    SelectionState selectionState() const
+    {
+        return m_layoutObject->selectionState();
+    }
+
     Color selectionBackgroundColor() const
     {
         return m_layoutObject->selectionBackgroundColor();
--- third_party/WebKit/Source/core/layout/api/LineLayoutText.h
+++ third_party/WebKit/Source/core/layout/api/LineLayoutText.h
@@ -128,11 +128,6 @@ public:
         return toText()->hyphenWidth(font, textDirection);
     }
 
-    SelectionState selectionState() const
-    {
-        return toText()->selectionState();
-    }
-
     void selectionStartEnd(int& spos, int& epos) const
     {
         return toText()->selectionStartEnd(spos, epos);
