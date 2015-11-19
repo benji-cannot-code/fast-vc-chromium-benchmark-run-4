a0e905c7a0bec0e99ec80cf5c908813d49eece11
dtu <dtu@chromium.org>
2015-11-19 13:19:47 -0800

    Use full data set for legacy bisect.
    Right now, it takes the mean of each benchmark run, leading to low confidence for benchmarks that have multiple iterations.
    
    BUG=548183, 553696
    
    Review URL: https://codereview.chromium.org/1459643004
    
    Cr-Commit-Position: refs/heads/master@{#360664}

--- tools/auto_bisect/bisect_perf_regression.py
+++ tools/auto_bisect/bisect_perf_regression.py
@@ -1335,7 +1335,7 @@ class BisectPerformanceMetrics(object):
       if metric and self._IsBisectModeUsingMetric():
         parsed_metric = _ParseMetricValuesFromOutput(metric, output)
         if parsed_metric:
-          metric_values.append(math_utils.Mean(parsed_metric))
+          metric_values += parsed_metric
         # If we're bisecting on a metric (ie, changes in the mean or
         # standard deviation) and no metric values are produced, bail out.
         if not metric_values:
