d66750ed0da3c916b5e76fcc6079fb16689c014b
dcheng <dcheng@chromium.org>
2015-11-19 10:37:07 -0800

    Try to remove <algorithm> header from scoped_ptr.h, again.
    Hopefully this is really the last one... unfortunately, several
    waterfall configurations lack trybot coverage, and practical
    experience has shown that it's possible for new regressions to be
    introduced in the time taken to build the different configs on the
    waterfall.
    
    BUG=none
    R=danakj
    TBR=jrummell
    
    Review URL: https://codereview.chromium.org/1462843002
    
    Cr-Commit-Position: refs/heads/master@{#360632}

--- media/formats/mp2t/ts_section_psi.cc
+++ media/formats/mp2t/ts_section_psi.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/formats/mp2t/ts_section_psi.h"
 
+#include <algorithm>
+
 #include "base/basictypes.h"
 #include "base/logging.h"
 #include "media/base/bit_reader.h"
