33aad39ed23e5c2ef945e2f9509f412a35e8500c
nhiroki <nhiroki@chromium.org>
2015-11-19 02:44:40 -0800

    ServiceWorker: Fix messed registration attributes on ServiceWorkerGlobalScope
    This is the fix for regression introduced by
    https://codereview.chromium.org/1344783002
    
    After the CL, SWProviderContext::ControllerDelegate::AssociateRegistration()
    wrongly set its |installing_| field to the active worker and |active_| field
    to nullptr. These messed fields eventually show up as
    ServiceWorkerGlobalScope.registration's version attributes.
    
    This CL fixes the wrong field assignment and adds layout tests.
    
    BUG=532098, 557551
    
    Review URL: https://codereview.chromium.org/1460653002
    
    Cr-Commit-Position: refs/heads/master@{#360557}

---
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/ServiceWorkerGlobalScope/resources/registration-attribute-newer-worker.js
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// TODO(nhiroki): stop using global states because service workers can be killed
+// at any point. Instead, we could post a message to the page on each event via
+// Client object (http://crbug.com/558244).
+var results = [];
+
+function stringify(worker) {
+  return worker ? worker.scriptURL : 'empty';
+}
+
+function record(event_name) {
+  results.push(event_name);
+  results.push('  installing: ' + stringify(self.registration.installing));
+  results.push('  waiting: ' + stringify(self.registration.waiting));
+  results.push('  active: ' + stringify(self.registration.active));
+}
+
+record('evaluate');
+
+self.registration.addEventListener('updatefound', function() {
+    record('updatefound');
+    var worker = self.registration.installing;
+    self.registration.installing.addEventListener('statechange', function() {
+        record('statechange(' + worker.state + ')');
+      });
+  });
+
+self.addEventListener('install', function(e) { record('install'); });
+
+self.addEventListener('activate', function(e) { record('activate'); });
+
+self.addEventListener('message', function(e) {
+    e.data.port.postMessage(results);
+  });
