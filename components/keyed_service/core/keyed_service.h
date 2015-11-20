639ce2431396459201bd040f3742a23729fd84d4
sdefresne <sdefresne@chromium.org>
2015-11-20 02:56:58 -0800

    Fix a style guide violation in KeyedService.
    According to the style guide [1], constructors and destructor should come
    before any other method declaration. Fixes KeyedService method ordering
    to follow said guide.
    
    [1]: https://google.github.io/styleguide/cppguide.html#Declaration_Order
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1459843002
    
    Cr-Commit-Position: refs/heads/master@{#360797}

--- components/keyed_service/core/keyed_service.h
+++ components/keyed_service/core/keyed_service.h
@@ -18,11 +18,11 @@ class KEYED_SERVICE_EXPORT KeyedService {
  public:
   KeyedService();
 
-  // The first pass is to call Shutdown on a KeyedService.
-  virtual void Shutdown();
-
   // The second pass is the actual deletion of each object.
   virtual ~KeyedService();
+
+  // The first pass is to call Shutdown on a KeyedService.
+  virtual void Shutdown();
 };
 
 #endif  // COMPONENTS_KEYED_SERVICE_CORE_KEYED_SERVICE_H_
