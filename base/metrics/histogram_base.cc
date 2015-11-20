578565afff9f445f890aca42a5f5b0ee9a9ce24a
asvitkine <asvitkine@chromium.org>
2015-11-19 18:37:56 -0800

    Use subtle::NoBarrier* statements for Histogram flags_ field.
    This is similar to this CL which provided the same treatment to
    some of the other histogram internal fields:
    https://codereview.chromium.org/116983004
    
    This field can be accessed from multiple threads which causes
    TSAN to report data races. Using NoBarrier instructions makes
    this intention explicit and silences the TSAN errors.
    
    Here's some context for the data race and why it's harmless:
    https://code.google.com/p/chromium/issues/detail?id=535359#c14
    
    BUG=535359
    
    Review URL: https://codereview.chromium.org/1445283004
    
    Cr-Commit-Position: refs/heads/master@{#360723}

--- base/metrics/histogram_base.cc
+++ base/metrics/histogram_base.cc
@@ -73,11 +73,13 @@ void HistogramBase::CheckName(const StringPiece& name) const {
 }
 
 void HistogramBase::SetFlags(int32 flags) {
-  flags_ |= flags;
+  HistogramBase::Count old_flags = subtle::NoBarrier_Load(&flags_);
+  subtle::NoBarrier_Store(&flags_, old_flags | flags);
 }
 
 void HistogramBase::ClearFlags(int32 flags) {
-  flags_ &= ~flags;
+  HistogramBase::Count old_flags = subtle::NoBarrier_Load(&flags_);
+  subtle::NoBarrier_Store(&flags_, old_flags & ~flags);
 }
 
 void HistogramBase::AddTime(const TimeDelta& time) {
@@ -120,7 +122,7 @@ void HistogramBase::WriteJSON(std::string* output) const {
 }
 
 void HistogramBase::FindAndRunCallback(HistogramBase::Sample sample) const {
-  if ((flags_ & kCallbackExists) == 0)
+  if ((flags() & kCallbackExists) == 0)
     return;
 
   StatisticsRecorder::OnSampleCallback cb =
