d8f49a475b72c8604c11880c35e51026fb1af856
msw <msw@chromium.org>
2015-11-19 19:38:46 -0800

    Fetch Mandoline components from Google Storage as needed.
    Proof-of-concept for fetching optional components.
    Return LocalFetcher redirect URLs for missing components.
    
    This uses NetworkFetcher to access Google Storage files:
    https://pantheon.corp.google.com/storage/browser/mandoline/
    
    For example, delete pdf_viewer.mojo locally, and run:
    $ mandoline http://www.pdf995.com/samples/pdf.pdf --predictable-app-filenames
    
    TODO: Integrate with Mandoline's component updater? crbug.com/479169
    TODO: Integrate with planned Omaha fetcher and updater work.
    TODO: Support fetching apps with resource files.
    TODO: Handle broken URLs. (unavailable component files, etc.)
    TODO: Avoid kPredictableAppFilenames switch? (need executable bit set)
    
    BUG=479169
    TEST=Mandoline can fetch the pdf_viewer as needed.
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/1442203002
    
    Cr-Commit-Position: refs/heads/master@{#360735}

--- mojo/fetcher/local_fetcher.h
+++ mojo/fetcher/local_fetcher.h
@@ -24,7 +24,10 @@ class LocalFetcher : public shell::Fetcher {
   LocalFetcher(NetworkService* network_service,
                const GURL& url,
                const GURL& url_without_query,
+               const base::FilePath& shell_file_root,
                const FetchCallback& loader_callback);
+
+  ~LocalFetcher() override;
 
   void GetMimeTypeFromFileCallback(const mojo::String& mime_type);
 
@@ -48,6 +51,7 @@ class LocalFetcher : public shell::Fetcher {
 
   GURL url_;
   base::FilePath path_;
+  base::FilePath shell_file_root_;
   std::string mime_type_;
 
   DISALLOW_COPY_AND_ASSIGN(LocalFetcher);
