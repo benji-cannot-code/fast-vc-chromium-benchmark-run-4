b9634d916846be91dda989f018cd96a1a7d012e3
pfeldman <pfeldman@chromium.org>
2015-11-20 00:38:13 -0800

    DevTools: control touch emulation while screencasting from the front-end.
    Review URL: https://codereview.chromium.org/1457223002
    
    Cr-Commit-Position: refs/heads/master@{#360778}

--- content/browser/devtools/protocol/page_handler.h
+++ content/browser/devtools/protocol/page_handler.h
@@ -32,12 +32,6 @@ class PageHandler : public NotificationObserver {
  public:
   typedef DevToolsProtocolClient::Response Response;
 
-  class ScreencastListener {
-   public:
-    virtual ~ScreencastListener() { }
-    virtual void ScreencastEnabledChanged() = 0;
-  };
-
   PageHandler();
   ~PageHandler() override;
 
@@ -49,7 +43,6 @@ class PageHandler : public NotificationObserver {
       frame_metadata);
   void DidAttachInterstitialPage();
   void DidDetachInterstitialPage();
-  void SetScreencastListener(ScreencastListener* listener);
   bool screencast_enabled() const { return enabled_ && screencast_enabled_; }
 
   Response Enable();
@@ -126,7 +119,6 @@ class PageHandler : public NotificationObserver {
 
   RenderFrameHostImpl* host_;
   scoped_ptr<Client> client_;
-  ScreencastListener* screencast_listener_;
   NotificationRegistrar registrar_;
   base::WeakPtrFactory<PageHandler> weak_factory_;
 
