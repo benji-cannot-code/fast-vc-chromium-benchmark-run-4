32bfad89935ac94a6f9197dfb3d42a44a39ccc75
horo <horo@chromium.org>
2015-11-19 19:14:57 -0800

    [CSP] Don't check the path component of the URL when the response was fetched via ServiceWorker.
    In ResourceFetcher::didReceiveResponse() we check the response URL when the response is fetched via ServiceWorker.
    But the match algorithm should ignore the path component of the URL.
    
    BUG=542060
    
    Review URL: https://codereview.chromium.org/1454003003
    
    Cr-Commit-Position: refs/heads/master@{#360733}

--- third_party/WebKit/Source/core/loader/FrameFetchContext.h
+++ third_party/WebKit/Source/core/loader/FrameFetchContext.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CoreExport.h"
 #include "core/fetch/FetchContext.h"
 #include "core/fetch/ResourceFetcher.h"
+#include "core/frame/csp/ContentSecurityPolicy.h"
 #include "platform/heap/Handle.h"
 #include "platform/network/ResourceRequest.h"
 #include "wtf/PassOwnPtr.h"
@@ -87,6 +88,7 @@ public:
     void addResourceTiming(const ResourceTimingInfo&) override;
     bool allowImage(bool imagesEnabled, const KURL&) const override;
     bool canRequest(Resource::Type, const ResourceRequest&, const KURL&, const ResourceLoaderOptions&, bool forPreload, FetchRequest::OriginRestriction) const override;
+    bool allowResponse(Resource::Type, const ResourceRequest&, const KURL&, const ResourceLoaderOptions&) const override;
 
     bool isControlledByServiceWorker() const override;
     int64_t serviceWorkerID() const override;
@@ -119,7 +121,7 @@ private:
 
     LocalFrame* frame() const; // Can be null
     void printAccessDeniedMessage(const KURL&) const;
-    ResourceRequestBlockedReason canRequestInternal(Resource::Type, const ResourceRequest&, const KURL&, const ResourceLoaderOptions&, bool forPreload, FetchRequest::OriginRestriction) const;
+    ResourceRequestBlockedReason canRequestInternal(Resource::Type, const ResourceRequest&, const KURL&, const ResourceLoaderOptions&, bool forPreload, FetchRequest::OriginRestriction, ContentSecurityPolicy::RedirectStatus) const;
 
     // FIXME: Oilpan: Ideally this should just be a traced Member but that will
     // currently leak because ComputedStyle and its data are not on the heap.
