85b94be77c6f2e0ae8ae86eca99f6a953f1bfcaa
charliea <charliea@chromium.org>
2015-11-20 12:28:57 -0800

    Adds header guards to the battor agent .h files
    /facepalm
    
    BUG=542837
    
    Review URL: https://codereview.chromium.org/1464773004
    
    Cr-Commit-Position: refs/heads/master@{#360905}

--- tools/battor_agent/battor_agent.h
+++ tools/battor_agent/battor_agent.h
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef TOOLS_BATTOR_AGENT_BATTOR_AGENT_H_
+#define TOOLS_BATTOR_AGENT_BATTOR_AGENT_H_
+
 #include <string>
 
 #include "base/macros.h"
@@ -74,3 +77,5 @@ class BattOrAgent {
 };
 
 }  // namespace battor
+
+#endif  // TOOLS_BATTOR_AGENT_BATTOR_AGENT_H_
--- tools/battor_agent/battor_connection.h
+++ tools/battor_agent/battor_connection.h
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef TOOLS_BATTOR_AGENT_BATTOR_CONNECTION_H_
+#define TOOLS_BATTOR_AGENT_BATTOR_CONNECTION_H_
+
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "device/serial/serial_io_handler.h"
@@ -32,3 +35,5 @@ class BattOrConnection {
 };
 
 }  // namespace battor
+
+#endif  // TOOLS_BATTOR_AGENT_BATTOR_CONNECTION_H_
