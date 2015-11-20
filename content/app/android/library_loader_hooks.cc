97bcff5f6fff1ff629af0e8fe2cfe7ec8962bb8b
moshayedi <moshayedi@chromium.org>
2015-11-20 15:17:13 -0800

    Aura on Android: Fix link errors when building content_unittests_apk.
    * Do ui::RegisterUIAndroidJni() only when not using Aura.
    * Exclude desktop_aura from the build.
    
    BUG=507792
    
    Review URL: https://codereview.chromium.org/1466463004
    
    Cr-Commit-Position: refs/heads/master@{#360942}

--- content/app/android/library_loader_hooks.cc
+++ content/app/android/library_loader_hooks.cc
@@ -63,11 +63,6 @@ bool EnsureJniRegistered(JNIEnv* env) {
     if (!ui::events::android::RegisterJni(env))
       return false;
 
-#if !defined(USE_AURA)
-    if (!ui::shell_dialogs::RegisterJni(env))
-      return false;
-#endif
-
     if (!content::android::RegisterCommonJni(env))
       return false;
 
@@ -86,8 +81,13 @@ bool EnsureJniRegistered(JNIEnv* env) {
     if (!media::midi::RegisterJni(env))
       return false;
 
+#if !defined(USE_AURA)
+    if (!ui::shell_dialogs::RegisterJni(env))
+      return false;
+
     if (!ui::RegisterUIAndroidJni(env))
       return false;
+#endif
 
     g_jni_init_done = true;
   }
