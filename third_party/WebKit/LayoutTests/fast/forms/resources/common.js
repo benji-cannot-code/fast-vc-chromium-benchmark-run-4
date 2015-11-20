395abb9869c05a68e59d030676e8a6d5344b4a69
tkent <tkent@chromium.org>
2015-11-19 22:34:01 -0800

    document.execCommand() should not count as a "user edit" for ValidityState.tooLong and tooShort.
    execCommand() is a programatic change, and is not "user edit".
    Some existing tests are updated for evenSender.keyDown() instead of
    execCommand().
    
    BUG=540978
    
    Review URL: https://codereview.chromium.org/1462283002
    
    Cr-Commit-Position: refs/heads/master@{#360767}

--- third_party/WebKit/LayoutTests/fast/forms/resources/common.js
+++ third_party/WebKit/LayoutTests/fast/forms/resources/common.js
@@ -216,3 +216,13 @@ function waitUntilLoadedAndAutofocused(callback) {
             callback();
     }, false);
 }
+
+function sendString(str) {
+    if (!window.eventSender) {
+        console.log('Require eventSender.');
+        return;
+    }
+    for (var i = 0; i < str.length; ++i)
+        eventSender.keyDown(str.charAt(i));
+}
+
--- third_party/WebKit/Source/core/html/HTMLTextFormControlElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLTextFormControlElement.cpp
@@ -100,7 +100,7 @@ void HTMLTextFormControlElement::dispatchBlurEvent(Element* newFocusedElement, W
 void HTMLTextFormControlElement::defaultEventHandler(Event* event)
 {
     if (event->type() == EventTypeNames::webkitEditableContentChanged && layoutObject() && layoutObject()->isTextControl()) {
-        m_lastChangeWasUserEdit = true;
+        m_lastChangeWasUserEdit = !document().isRunningExecCommand();
         subtreeHasChanged();
         return;
     }
