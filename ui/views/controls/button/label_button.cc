1f00024d8cafaa7ffba118550c8016db7b308fd3
varkha <varkha@chromium.org>
2015-11-19 18:39:20 -0800

    Fixes ink ripple and focus rectangle centering in RTL layouts
    Corrects positioning and size of back and app menu buttons when a
    browser is maximized. With this CL the buttons visible size and
    alignment is maintained same and only the click / touch target is
    enlarged.
    
    BUG=549683
    TEST=
    Run on Chrome OS with --top-chrome-md=material --lang=iw
    Maximize the browser.
    Test that the following elements of app menu and back buttons are
    centered and square:
    1. Pressed button border
    2. Hovered button border
    4. Focus rectangle (Alt+Shift+T to show, Tab to shift)
    
    Test that the following elements of app menu and back buttons are
    centered:
    1. Button's icon
    2. Ink ripple animation visible on click / long press
    
    Repeat with --top-chrome-md=material --lang=en
    Repeat with --lang=iw but without --top-chrome-md
    Repeat with --lang=en but without --top-chrome-md
    Repeat all 4 cases with non-maximized browser.
    
    Test that when maximizing / unmaximizing browser window both back
    and app menu buttons stay in the same place relative to other
    toolbar buttons, i.e. the gap between back and forward buttons as
    well as the gap between app menu button and URL bar doesn't change.
    
    Review URL: https://codereview.chromium.org/1454353002
    
    Cr-Commit-Position: refs/heads/master@{#360725}

--- ui/views/controls/button/label_button.cc
+++ ui/views/controls/button/label_button.cc
@@ -53,8 +53,7 @@ namespace views {
 
 // static
 const int LabelButton::kHoverAnimationDurationMs = 170;
-
-// static
+const int LabelButton::kFocusRectInset = 3;
 const char LabelButton::kViewClassName[] = "LabelButton";
 
 LabelButton::LabelButton(ButtonListener* listener, const base::string16& text)
@@ -82,8 +81,8 @@ LabelButton::LabelButton(ButtonListener* listener, const base::string16& text)
   label_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
 
   // Inset the button focus rect from the actual border; roughly match Windows.
-  SetFocusPainter(
-      Painter::CreateDashedFocusPainterWithInsets(gfx::Insets(3, 3, 3, 3)));
+  SetFocusPainter(Painter::CreateDashedFocusPainterWithInsets(gfx::Insets(
+      kFocusRectInset, kFocusRectInset, kFocusRectInset, kFocusRectInset)));
 }
 
 LabelButton::~LabelButton() {}
