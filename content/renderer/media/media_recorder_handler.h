9e04fa92cbc5decfb40f62f892ba19c9a9ffaebf
haraken <haraken@chromium.org>
2015-11-20 07:27:37 -0800

    Revert "Update MediaRecorderHandler to use AudioTrackRecorder"
    This reverts commit a664ea923f135c74b9494acb36ee524e4c9f8448.
    
    fast/mediarecorder/MediaRecorder-canRecordMimeType.html started failing
    after this CL.
    
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.7%20%28dbg%29/builds/25644
    
    BUG=
    TBR=dcheng@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1461323002
    
    Cr-Commit-Position: refs/heads/master@{#360823}

--- content/renderer/media/media_recorder_handler.h
+++ content/renderer/media/media_recorder_handler.h
@@ -21,8 +21,6 @@ class WebString;
 }  // namespace blink
 
 namespace media {
-class AudioBus;
-class AudioParameters;
 class VideoFrame;
 class WebmMuxer;
 }  // namespace media
@@ -30,7 +28,6 @@ class WebmMuxer;
 namespace content {
 
 class VideoTrackRecorder;
-class AudioTrackRecorder;
 
 // MediaRecorderHandler orchestrates the creation, lifetime management and
 // mapping between:
@@ -41,6 +38,7 @@ class AudioTrackRecorder;
 // All methods are called on the same thread as construction and destruction,
 // i.e. the Main Render thread. (Note that a BindToCurrentLoop is used to
 // guarantee this, since VideoTrackRecorder sends back frames on IO thread.)
+// TODO(mcasas): http://crbug.com/528519 Implement audio recording.
 class CONTENT_EXPORT MediaRecorderHandler final
     : public NON_EXPORTED_BASE(blink::WebMediaRecorderHandler) {
  public:
@@ -65,16 +63,10 @@ class CONTENT_EXPORT MediaRecorderHandler final
                       scoped_ptr<std::string> encoded_data,
                       base::TimeTicks timestamp,
                       bool is_key_frame);
-  void OnEncodedAudio(const media::AudioParameters& params,
-                      scoped_ptr<std::string> encoded_data,
-                      base::TimeTicks timestamp);
   void WriteData(base::StringPiece data);
 
   void OnVideoFrameForTesting(const scoped_refptr<media::VideoFrame>& frame,
                               const base::TimeTicks& timestamp);
-  void OnAudioBusForTesting(const media::AudioBus& audio_bus,
-                            const base::TimeTicks& timestamp);
-  void SetAudioFormatForTesting(const media::AudioParameters& params);
 
   // Bound to the main render thread.
   base::ThreadChecker main_render_thread_checker_;
@@ -95,7 +87,6 @@ class CONTENT_EXPORT MediaRecorderHandler final
   blink::WebMediaRecorderHandlerClient* client_;
 
   ScopedVector<VideoTrackRecorder> video_recorders_;
-  ScopedVector<AudioTrackRecorder> audio_recorders_;
 
 #if !defined(MEDIA_DISABLE_LIBWEBM)
   // Worker class doing the actual Webm Muxing work.
