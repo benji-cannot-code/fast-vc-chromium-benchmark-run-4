fdfa1a9a6a20e19e820f2e99673ee8f0f7254407
erikchen <erikchen@chromium.org>
2015-11-20 12:14:40 -0800

    Remove jank profiling from Context::Notify.
    Profiling is not being followed up on, so should be removed.
    
    BUG=465841
    
    Review URL: https://codereview.chromium.org/1459263003
    
    Cr-Commit-Position: refs/heads/master@{#360899}

--- content/browser/browser_child_process_host_impl.cc
+++ content/browser/browser_child_process_host_impl.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/metrics/histogram.h"
-#include "base/profiler/scoped_tracker.h"
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
 #include "base/synchronization/waitable_event.h"
@@ -398,11 +397,6 @@ void BrowserChildProcessHostImpl::OnProcessLaunchFailed() {
 void BrowserChildProcessHostImpl::OnProcessLaunched() {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
-  // TODO(erikchen): Remove ScopedTracker below once http://crbug.com/465841
-  // is fixed.
-  tracked_objects::ScopedTracker tracking_profile(
-      FROM_HERE_WITH_EXPLICIT_FUNCTION(
-          "465841 BrowserChildProcessHostImpl::OnProcessLaunched"));
   const base::Process& process = child_process_->GetProcess();
   DCHECK(process.IsValid());
 
