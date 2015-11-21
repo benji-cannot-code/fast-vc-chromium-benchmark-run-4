03e4894e061717e0629341ea05324b06aefb0a70
joone.hur <joone.hur@intel.com>
2015-11-20 16:26:52 -0800

    Make File-Picker modal on Linux
    Chromium for Linux opens a Gtk file-picker for file browsing, but it is not
    modal because there is no GtkWindow as parent of file-picker.
    
    This patch allows the X11 host window to disable input event handling to make
    a file-picker modal
    
    The original CL was reverted due to wrong DCHECK with modal_dialog_xid_:
    https://codereview.chromium.org/1243503002
    
    BUG=408481
    TEST=BrowserSelectFileDialogTest, ModalTest
    
    Review URL: https://codereview.chromium.org/1233913009
    
    Cr-Commit-Position: refs/heads/master@{#360961}

--- chrome/browser/ui/libgtk2ui/select_file_dialog_impl_gtk2.cc
+++ chrome/browser/ui/libgtk2ui/select_file_dialog_impl_gtk2.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/libgtk2ui/select_file_dialog_impl_gtk2.h"
 
+#include <gdk/gdkx.h>
 #include <gtk/gtk.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -32,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/shell_dialogs/select_file_dialog.h"
 #include "ui/strings/grit/ui_strings.h"
+#include "ui/views/widget/desktop_aura/desktop_window_tree_host_x11.h"
 #include "ui/views/widget/desktop_aura/x11_desktop_handler.h"
 
 namespace {
@@ -164,9 +166,11 @@ void SelectFileDialogImplGTK::SelectFileImpl(
 
   params_map_[dialog] = params;
 
-  // TODO(erg): Figure out how to fake GTK window-to-parent modality without
-  // having the parent be a real GtkWindow.
-  gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
+  // Disable input events handling in the host window to make this dialog modal.
+  views::DesktopWindowTreeHostX11* host =
+      views::DesktopWindowTreeHostX11::GetHostForXID(
+      owning_window->GetHost()->GetAcceleratedWidget());
+  host->DisableEventListening(GDK_WINDOW_XID(gtk_widget_get_window(dialog)));
 
   gtk_widget_show_all(dialog);
 
@@ -425,6 +429,12 @@ void SelectFileDialogImplGTK::FileDialogDestroyed(GtkWidget* dialog) {
   aura::Window* parent = GetAuraTransientParent(dialog);
   if (!parent)
     return;
+
+  views::DesktopWindowTreeHostX11* host =
+      views::DesktopWindowTreeHostX11::GetHostForXID(
+      parent->GetHost()->GetAcceleratedWidget());
+  host->EnableEventListening();
+
   std::set<aura::Window*>::iterator iter = parents_.find(parent);
   if (iter != parents_.end()) {
     (*iter)->RemoveObserver(this);
