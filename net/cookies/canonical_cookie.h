5bcb1781771039b5a640ca10c0394b8ec5082427
estark <estark@chromium.org>
2015-11-20 16:59:16 -0800

    Add cookie prefix metrics
    Measure the number of naturally occurring cookies that begin with
    $Secure- or $Host- so that we know if cookie prefixes will break real
    sites.
    
    BUG=557083
    
    Review URL: https://codereview.chromium.org/1455693007
    
    Cr-Commit-Position: refs/heads/master@{#360969}

--- net/cookies/canonical_cookie.h
+++ net/cookies/canonical_cookie.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/gtest_prod_util.h"
 #include "base/time/time.h"
 #include "net/base/net_export.h"
 #include "net/cookies/cookie_constants.h"
@@ -160,6 +161,32 @@ class NET_EXPORT CanonicalCookie {
   bool FullCompare(const CanonicalCookie& other) const;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(CanonicalCookiePrefixHistogramTest, TestHistograms);
+
+  // The special cookie prefixes as defined in
+  // https://tools.ietf.org/html/draft-west-cookie-prefixes
+  //
+  // This enum is being histogrammed; do not reorder or remove values.
+  enum CookiePrefix {
+    COOKIE_PREFIX_NONE = 0,
+    COOKIE_PREFIX_SECURE,
+    COOKIE_PREFIX_HOST,
+    COOKIE_PREFIX_LAST
+  };
+
+  // Returns the CookiePrefix (or COOKIE_PREFIX_NONE if none) that
+  // applies to the given cookie |name|.
+  static CookiePrefix GetCookiePrefix(const std::string& name);
+  // Records histograms to measure how often cookie prefixes appear in
+  // the wild and how often they would be blocked.
+  static void RecordCookiePrefixMetrics(CookiePrefix prefix,
+                                        bool is_cookie_valid);
+  // Returns true if a prefixed cookie does not violate any of the rules
+  // for that cookie.
+  static bool IsCookiePrefixValid(CookiePrefix prefix,
+                                  const GURL& url,
+                                  const ParsedCookie& parsed_cookie);
+
   // The source member of a canonical cookie is the origin of the URL that tried
   // to set this cookie.  This field is not persistent though; its only used in
   // the in-tab cookies dialog to show the user the source URL. This is used for
