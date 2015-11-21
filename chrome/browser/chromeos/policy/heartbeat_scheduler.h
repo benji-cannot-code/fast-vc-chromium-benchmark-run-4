79ace5f7072a45b49b561fb49c9e27d45ee83a77
binjin <binjin@chromium.org>
2015-11-20 17:28:02 -0800

    Signup for GCM upstream notifications
    Also set the default heartbeats interval for GCM driver.
    
    BUG=517466
    
    Review URL: https://codereview.chromium.org/1267983002
    
    Cr-Commit-Position: refs/heads/master@{#360978}

--- chrome/browser/chromeos/policy/heartbeat_scheduler.h
+++ chrome/browser/chromeos/policy/heartbeat_scheduler.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "components/gcm_driver/gcm_app_handler.h"
 #include "components/gcm_driver/gcm_client.h"
+#include "components/gcm_driver/gcm_connection_observer.h"
 #include "components/policy/core/common/cloud/cloud_policy_client.h"
 
 namespace base {
@@ -34,7 +35,8 @@ class HeartbeatRegistrationHelper;
 
 // Class responsible for periodically sending heartbeats to the policy service
 // for monitoring device connectivity.
-class HeartbeatScheduler : public gcm::GCMAppHandler {
+class HeartbeatScheduler : public gcm::GCMAppHandler,
+                           gcm::GCMConnectionObserver {
  public:
   // Default interval for how often we send up a heartbeat.
   static const int64 kDefaultHeartbeatIntervalMs;
@@ -64,6 +66,9 @@ class HeartbeatScheduler : public gcm::GCMAppHandler {
   void OnSendAcknowledged(const std::string& app_id,
                           const std::string& message_id) override;
 
+  // GCMConnectionObserver overrides.
+  void OnConnected(const net::IPEndPoint&) override;
+
  private:
   // Callback invoked periodically to send a heartbeat to the policy service.
   void SendHeartbeat();
@@ -74,6 +79,10 @@ class HeartbeatScheduler : public gcm::GCMAppHandler {
   // Invoked after a heartbeat has been sent to the server.
   void OnHeartbeatSent(const std::string& message_id,
                        gcm::GCMClient::Result result);
+
+  // Invoked after a upstream notification sign up message has been sent.
+  void OnUpstreamNotificationSent(const std::string& message_id,
+                                  gcm::GCMClient::Result result);
 
   // Helper method that figures out when the next heartbeat should
   // be scheduled.
@@ -92,6 +101,9 @@ class HeartbeatScheduler : public gcm::GCMAppHandler {
 
   // Callback for the GCM id update request.
   void OnGcmIdUpdateRequestSent(bool status);
+
+  // Helper function to signup for upstream notification.
+  void SignUpUpstreamNotification();
 
   // TaskRunner used for scheduling heartbeats.
   const scoped_refptr<base::SequencedTaskRunner> task_runner_;
