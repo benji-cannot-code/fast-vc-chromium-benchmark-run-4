8cab79cf7721302b4755846c28946de660cb0147
fsamuel <fsamuel@chromium.org>
2015-11-22 15:38:03 -0800

    Mustash: Move RenderWidgetHostViewMus from frame_host => renderer_host
    We are not yet addressing the subframe case so renderer_host is a better
    place for this file.
    
    BUG=551250
    
    Review URL: https://codereview.chromium.org/1467083002
    
    Cr-Commit-Position: refs/heads/master@{#361057}

--- content/browser/web_contents/web_contents_view_mus.cc
+++ content/browser/web_contents/web_contents_view_mus.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/web_contents/web_contents_view_mus.h"
 
 #include "build/build_config.h"
-#include "content/browser/frame_host/render_widget_host_view_mus.h"
+#include "content/browser/renderer_host/render_widget_host_view_mus.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "third_party/WebKit/public/web/WebDragOperation.h"
