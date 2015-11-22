811e1ffb2e3337fa4f678199e37eeaf46ad653fc
davidben <davidben@chromium.org>
2015-11-21 20:52:47 -0800

    Remove unnecessary include.
    This got left in there from https://codereview.chromium.org/1422573008
    
    BUG=394131
    
    Review URL: https://codereview.chromium.org/1465793004
    
    Cr-Commit-Position: refs/heads/master@{#361033}

--- net/socket/ssl_client_socket_openssl.cc
+++ net/socket/ssl_client_socket_openssl.cc
@@ -53,10 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_version.h"
 #endif
 
-#if !defined(OS_NACL)
-#include "net/ssl/ssl_platform_key.h"
-#endif
-
 #if defined(USE_NSS_CERTS) || defined(OS_IOS)
 #include "net/cert_net/nss_ocsp.h"
 #endif
