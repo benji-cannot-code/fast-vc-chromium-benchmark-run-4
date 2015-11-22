be482fa817c80bbd42779196d99700895b7a66bc
mathp <mathp@chromium.org>
2015-11-22 12:16:58 -0800

    [Autofill] Clicking datalist chooser arrow now shows Autofill/Autocomplete
    Previous implementation had a flash where we would see datalist only, then the full list with Autofill (see bug for description). Behavior now is to show the full list of suggestions that we have (datalist + Autofill/autocomplete, if available).
    
    BUG=558530
    TEST=manual
    
    Review URL: https://codereview.chromium.org/1447283006
    
    Cr-Commit-Position: refs/heads/master@{#361044}

--- components/autofill/content/renderer/autofill_agent.cc
+++ components/autofill/content/renderer/autofill_agent.cc
@@ -133,7 +133,6 @@ void TrimStringVectorForIPC(std::vector<base::string16>* strings) {
 AutofillAgent::ShowSuggestionsOptions::ShowSuggestionsOptions()
     : autofill_on_empty_values(false),
       requires_caret_at_end(false),
-      datalist_only(false),
       show_full_suggestion_list(false),
       show_password_suggestions_only(false) {
 }
@@ -461,7 +460,6 @@ void AutofillAgent::textFieldDidReceiveKeyDown(const WebInputElement& element,
 void AutofillAgent::openTextDataListChooser(const WebInputElement& element) {
   ShowSuggestionsOptions options;
   options.autofill_on_empty_values = true;
-  options.datalist_only = true;
   ShowSuggestions(element, options);
 }
 
@@ -648,14 +646,14 @@ void AutofillAgent::ShowSuggestions(const WebFormControlElement& element,
                                     const ShowSuggestionsOptions& options) {
   if (!element.isEnabled() || element.isReadOnly())
     return;
-  if (!options.datalist_only && !element.suggestedValue().isEmpty())
+  if (!element.suggestedValue().isEmpty())
     return;
 
   const WebInputElement* input_element = toWebInputElement(&element);
   if (input_element) {
     if (!input_element->isTextField())
       return;
-    if (!options.datalist_only && !input_element->suggestedValue().isEmpty())
+    if (!input_element->suggestedValue().isEmpty())
       return;
   } else {
     DCHECK(form_util::IsTextAreaElement(element));
@@ -666,12 +664,11 @@ void AutofillAgent::ShowSuggestions(const WebFormControlElement& element,
   // Don't attempt to autofill with values that are too large or if filling
   // criteria are not met.
   WebString value = element.editingValue();
-  if (!options.datalist_only &&
-      (value.length() > kMaxDataLength ||
-       (!options.autofill_on_empty_values && value.isEmpty()) ||
-       (options.requires_caret_at_end &&
-        (element.selectionStart() != element.selectionEnd() ||
-         element.selectionEnd() != static_cast<int>(value.length()))))) {
+  if (value.length() > kMaxDataLength ||
+      (!options.autofill_on_empty_values && value.isEmpty()) ||
+      (options.requires_caret_at_end &&
+       (element.selectionStart() != element.selectionEnd() ||
+        element.selectionEnd() != static_cast<int>(value.length())))) {
     // Any popup currently showing is obsolete.
     HidePopup();
     return;
@@ -697,12 +694,11 @@ void AutofillAgent::ShowSuggestions(const WebFormControlElement& element,
   if (input_element && input_element->isPasswordField())
     return;
 
-  QueryAutofillSuggestions(element, options.datalist_only);
+  QueryAutofillSuggestions(element);
 }
 
 void AutofillAgent::QueryAutofillSuggestions(
-    const WebFormControlElement& element,
-    bool datalist_only) {
+    const WebFormControlElement& element) {
   if (!element.document().frame())
     return;
 
@@ -719,8 +715,6 @@ void AutofillAgent::QueryAutofillSuggestions(
     // at providing suggestions.
     WebFormControlElementToFormField(element, form_util::EXTRACT_VALUE, &field);
   }
-  if (datalist_only)
-    field.should_autocomplete = false;
 
   gfx::RectF bounding_box_scaled = form_util::GetScaledBoundingBox(
       render_frame()->GetRenderView()->GetWebView()->pageScaleFactor(),
