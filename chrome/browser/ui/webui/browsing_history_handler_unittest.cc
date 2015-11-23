a684a0cfb1cb52178488a0a67cec9bdc8b7db2a4
tsergeant <tsergeant@chromium.org>
2015-11-22 16:26:32 -0800

    Split BrowsingHistoryHandler out of history_ui.cc
    This cleans up history_ui.cc in preparation for adding a new WebUI for Material
    design history.
    
    BUG=425625
    
    Review URL: https://codereview.chromium.org/1462813003
    
    Cr-Commit-Position: refs/heads/master@{#361060}

--- chrome/browser/ui/webui/browsing_history_handler_unittest.cc
+++ chrome/browser/ui/webui/browsing_history_handler_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/webui/history_ui.h"
+#include "chrome/browser/ui/webui/browsing_history_handler.h"
 
 #include "base/strings/utf_string_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -50,7 +50,7 @@ bool ResultEquals(
 
 // Tests that the MergeDuplicateResults method correctly removes duplicate
 // visits to the same URL on the same day.
-TEST(HistoryUITest, MergeDuplicateResults) {
+TEST(BrowsingHistoryHandlerTest, MergeDuplicateResults) {
   {
     // Basic test that duplicates on the same day are removed.
     TestResult test_data[] = {
--- chrome/chrome_browser_ui.gypi
+++ chrome/chrome_browser_ui.gypi
@@ -204,6 +204,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/ui/website_settings/website_settings_ui.h',
       'browser/ui/webui/about_ui.cc',
       'browser/ui/webui/about_ui.h',
+      'browser/ui/webui/browsing_history_handler.cc',
+      'browser/ui/webui/browsing_history_handler.h',
       'browser/ui/webui/chrome_web_ui_controller_factory.cc',
       'browser/ui/webui/chrome_web_ui_controller_factory.h',
       'browser/ui/webui/chromeos/bluetooth_pairing_ui.cc',
