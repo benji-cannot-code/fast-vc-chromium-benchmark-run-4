61da4b461838089cde77f8c41766810bf435090a
aizatsky <aizatsky@chromium.org>
2015-11-23 13:12:27 -0800

    //base:json reader fuzzer
    BUG=
    
    Review URL: https://codereview.chromium.org/1465283002
    
    Cr-Commit-Position: refs/heads/master@{#361184}

---
+++ testing/libfuzzer/fuzzers/base_json_reader_fuzzer.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include "base/json/json_reader.h"
+#include "base/values.h"
+
+// Entry point for LibFuzzer.
+extern "C" int LLVMFuzzerTestOneInput(const unsigned char* data,
+                                      unsigned long size) {
+  base::JSONReader reader;
+  reader.Read(std::string(reinterpret_cast<const char*>(data), size));
+  return 0;
+}
+
