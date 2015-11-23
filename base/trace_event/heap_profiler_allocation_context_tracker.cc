84512b79c8fff7f052e6bd7366654c947fb733ae
ruuda <ruuda@google.com>
2015-11-23 13:46:57 -0800

    [StyleGuide] Allow begin and end non-member functions
    Allow usage of |std::begin| and |std::end|. This is useful for iterating
    fixed-size arrays.
    
    Discussion thread: https://groups.google.com/a/chromium.org/d/topic/cxx/5iFNE8P5qT4/discussion
    
    Furthermore, this CL replaces home-brewn |std::end|-like functions in
    base/trace_event with |std::end| to verify that <iterator> is supported
    on all platforms.
    
    Review URL: https://codereview.chromium.org/1471683002
    
    Cr-Commit-Position: refs/heads/master@{#361192}

--- base/trace_event/heap_profiler_allocation_context_tracker.cc
+++ base/trace_event/heap_profiler_allocation_context_tracker.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/heap_profiler_allocation_context_tracker.h"
 
 #include <algorithm>
+#include <iterator>
 
 #include "base/atomicops.h"
 #include "base/threading/thread_local_storage.h"
@@ -24,13 +25,6 @@ ThreadLocalStorage::StaticSlot g_tls_alloc_ctx_tracker = TLS_INITIALIZER;
 // thread exits.
 void DestructAllocationContextTracker(void* alloc_ctx_tracker) {
   delete static_cast<AllocationContextTracker*>(alloc_ctx_tracker);
-}
-
-// Returns a pointer past the end of the fixed-size array |array| of |T| of
-// length |N|, identical to C++11 |std::end|.
-template <typename T, int N>
-T* End(T(&array)[N]) {
-  return array + N;
 }
 
 }  // namespace
@@ -100,9 +94,9 @@ AllocationContext AllocationContextTracker::GetContextSnapshot() {
   // Fill the backtrace.
   {
     auto src = tracker->pseudo_stack_.begin();
-    auto dst = ctx.backtrace.frames;
+    auto dst = std::begin(ctx.backtrace.frames);
     auto src_end = tracker->pseudo_stack_.end();
-    auto dst_end = End(ctx.backtrace.frames);
+    auto dst_end = std::end(ctx.backtrace.frames);
 
     // Copy as much of the bottom of the pseudo stack into the backtrace as
     // possible.
