b9856ba22c4ee2892bae81ceba0dacb077719e82
ericwilligers <ericwilligers@chromium.org>
2015-11-22 16:30:49 -0800

    SVG animateMotion paths that only cause offsets are no longer ignored
    Chrome was not applying animateMotion paths that do not actually express
    motion.
    
    Similarly, motion-path paths like "M 100 100 L 100 100" were not being
    applied.
    
    The SkPathMeasure constructed from such paths is empty, but we can
    directly read the starting point from the path.
    
    BUG=321022
    
    Review URL: https://codereview.chromium.org/1418663004
    
    Cr-Commit-Position: refs/heads/master@{#361061}

--- third_party/WebKit/Source/platform/graphics/Path.cpp
+++ third_party/WebKit/Source/platform/graphics/Path.cpp
@@ -173,11 +173,11 @@ float Path::length() const
     return SkScalarToFloat(length);
 }
 
-FloatPoint Path::pointAtLength(float length, bool& ok) const
+FloatPoint Path::pointAtLength(float length) const
 {
     FloatPoint point;
     float normal;
-    ok = pointAndNormalAtLength(length, point, normal);
+    pointAndNormalAtLength(length, point, normal);
     return point;
 }
 
@@ -202,16 +202,15 @@ static bool calculatePointAndNormalOnPath(SkPathMeasure& measure, SkScalar lengt
     return false;
 }
 
-bool Path::pointAndNormalAtLength(float length, FloatPoint& point, float& normal) const
+void Path::pointAndNormalAtLength(float length, FloatPoint& point, float& normal) const
 {
     SkPathMeasure measure(m_path, false);
-
     if (calculatePointAndNormalOnPath(measure, WebCoreFloatToSkScalar(length), point, normal))
-        return true;
+        return;
 
+    SkPoint position = m_path.getPoint(0);
+    point = FloatPoint(SkScalarToFloat(position.fX), SkScalarToFloat(position.fY));
     normal = 0;
-    point = FloatPoint(0, 0);
-    return false;
 }
 
 Path::PositionCalculator::PositionCalculator(const Path& path)
@@ -221,7 +220,7 @@ Path::PositionCalculator::PositionCalculator(const Path& path)
 {
 }
 
-bool Path::PositionCalculator::pointAndNormalAtLength(float length, FloatPoint& point, float& normalAngle)
+void Path::PositionCalculator::pointAndNormalAtLength(float length, FloatPoint& point, float& normalAngle)
 {
     SkScalar skLength = WebCoreFloatToSkScalar(length);
     if (skLength >= 0) {
@@ -234,12 +233,13 @@ bool Path::PositionCalculator::pointAndNormalAtLength(float length, FloatPoint& 
         }
 
         if (calculatePointAndNormalOnPath(m_pathMeasure, skLength, point, normalAngle, &m_accumulatedLength))
-            return true;
+            return;
     }
 
+    SkPoint position = m_path.getPoint(0);
+    point = FloatPoint(SkScalarToFloat(position.fX), SkScalarToFloat(position.fY));
     normalAngle = 0;
-    point = FloatPoint(0, 0);
-    return false;
+    return;
 }
 
 void Path::clear()
