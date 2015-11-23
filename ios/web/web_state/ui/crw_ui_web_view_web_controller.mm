56b575bfadeb9b981b1207d4f3639584d0a48f2d
stkhapugin <stkhapugin@chromium.org>
2015-11-23 05:49:34 -0800

    Fixes document type detection on iOS 9.
    On iOS 9, evaluating '' + document always results in [object
    HTMLDocument], even for PDFs. This introduces another way of checking
    document type on iOS 9. Unfortunately, it doesn't work on iOS 8, so the
    old implementation needs to stay too.
    
    BUG=549604
    
    Review URL: https://codereview.chromium.org/1458703004
    
    Cr-Commit-Position: refs/heads/master@{#361095}

--- ios/web/web_state/ui/crw_ui_web_view_web_controller.mm
+++ ios/web/web_state/ui/crw_ui_web_view_web_controller.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web/web_state/ui/crw_ui_web_view_web_controller.h"
 
+#import "base/ios/ios_util.h"
 #import "base/ios/ns_error_util.h"
 #import "base/ios/weak_nsobject.h"
 #include "base/json/json_reader.h"
@@ -551,13 +552,25 @@ const size_t kMaxMessageQueueSize = 262144;
   if (!_uiWebView) {
     return web::WEB_VIEW_DOCUMENT_TYPE_GENERIC;
   }
-  NSString* documentType =
-      [_uiWebView stringByEvaluatingJavaScriptFromString:
-          @"'' + document"];
-  if ([documentType isEqualToString:@"[object HTMLDocument]"])
-    return web::WEB_VIEW_DOCUMENT_TYPE_HTML;
-  else if ([documentType isEqualToString:@"[object Document]"])
-    return web::WEB_VIEW_DOCUMENT_TYPE_GENERIC;
+
+  if (base::ios::IsRunningOnIOS9OrLater()) {
+    // On iOS 9, evaluating '' + document always results in [object
+    // HTMLDocument], even for PDFs. However, document.contentType is properly
+    // defined.
+    NSString* MIMEType = [_uiWebView
+        stringByEvaluatingJavaScriptFromString:@"document.contentType"];
+    return [self documentTypeFromMIMEType:MIMEType];
+  } else {
+    // On iOS 8 and below, document.contentType is always undefined. Use this
+    // instead.
+    NSString* documentType =
+        [_uiWebView stringByEvaluatingJavaScriptFromString:@"'' + document"];
+    if ([documentType isEqualToString:@"[object HTMLDocument]"])
+      return web::WEB_VIEW_DOCUMENT_TYPE_HTML;
+    else if ([documentType isEqualToString:@"[object Document]"])
+      return web::WEB_VIEW_DOCUMENT_TYPE_GENERIC;
+  }
+
   return web::WEB_VIEW_DOCUMENT_TYPE_UNKNOWN;
 }
 
