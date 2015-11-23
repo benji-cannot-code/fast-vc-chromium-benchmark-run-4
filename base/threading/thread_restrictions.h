0cda0acde1e3bcb605736ded8d3a8b287cfd4fab
sadrul <sadrul@chromium.org>
2015-11-23 11:25:16 -0800

    views/mus: Fix showing menu in chrome in mus.
    For menus in chrome-in-mus, we attempt to create a Widget hosted in a native
    window. For desktop builds, this means we attempt to host in a HWND or an
    X11 window created by DesktopNativeWidgetAura. But for mus, we want to use
    NativeWidgetMus. To do this, use the native-widget factory first if it's
    available.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1460003004
    
    Cr-Commit-Position: refs/heads/master@{#361151}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -76,6 +76,10 @@ namespace ui {
 class WindowResizeHelperMac;
 }
 
+namespace views {
+class WindowManagerConnection;
+}
+
 namespace base {
 
 namespace android {
@@ -221,6 +225,7 @@ class BASE_EXPORT ThreadRestrictions {
   friend class ::NativeBackendKWallet;            // http://crbug.com/125331
 #if !defined(OFFICIAL_BUILD)
   friend class content::SoftwareOutputDeviceMus;  // Interim non-production code
+  friend class views::WindowManagerConnection;
 #endif
 // END USAGE THAT NEEDS TO BE FIXED.
 
