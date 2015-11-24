381d3c75d5d1911ad99f504efeedab845004c86b
Yuli Huang <yuli@chromium.org>
2015-11-24 16:39:21 +0800

    Fix taken pictures don't have ghost effect applied.
    TEST=Tested manually on daisy.
    BUG=chromium:560612
    
    Change-Id: I02d999eee4b40fb072ba5a017504d0c058313626
    Reviewed-on: https://chromium-review.googlesource.com/313858
    Reviewed-by: Tomasz Mikolajewski <mtomasz@chromium.org>
    Tested-by: Tomasz Mikolajewski <mtomasz@chromium.org>

--- chrome/browser/resources/chromeos/camera/src/js/effects/ghost.js
+++ chrome/browser/resources/chromeos/camera/src/js/effects/ghost.js
@@ -66,3 +66,10 @@ camera.effects.Ghost.prototype.getTitle = function() {
   return chrome.i18n.getMessage('ghostEffect');
 };
 
+/**
+ * @override
+ */
+camera.effects.Ghost.prototype.isMultiframe = function() {
+  return true;
+};
+
