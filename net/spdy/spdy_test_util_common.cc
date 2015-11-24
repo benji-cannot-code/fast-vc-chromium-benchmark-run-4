dbdf726f55619d630e8290e6f69b1a7c28ae3c71
bnc <bnc@chromium.org>
2015-11-24 05:33:18 -0800

    Re-enable HTTP/2 over NPN (for OpenSSL).
    BUG=557197
    
    Committed: https://crrev.com/0eb771aa98ba2bd4db0e2b82241afe8f5760c32e
    Cr-Commit-Position: refs/heads/master@{#361053}
    
    Review URL: https://codereview.chromium.org/1453903002
    
    Cr-Commit-Position: refs/heads/master@{#361330}

--- net/spdy/spdy_test_util_common.cc
+++ net/spdy/spdy_test_util_common.cc
@@ -349,6 +349,7 @@ SpdySessionDependencies::SpdySessionDependencies(NextProto protocol)
       enable_compression(false),
       enable_ping(false),
       enable_user_alternate_protocol_ports(false),
+      enable_npn(true),
       protocol(protocol),
       session_max_recv_window_size(
           SpdySession::GetDefaultInitialWindowSize(protocol)),
@@ -384,6 +385,7 @@ SpdySessionDependencies::SpdySessionDependencies(
       enable_compression(false),
       enable_ping(false),
       enable_user_alternate_protocol_ports(false),
+      enable_npn(true),
       protocol(protocol),
       session_max_recv_window_size(
           SpdySession::GetDefaultInitialWindowSize(protocol)),
@@ -442,6 +444,7 @@ HttpNetworkSession::Params SpdySessionDependencies::CreateSessionParams(
   params.enable_spdy_ping_based_connection_checking = session_deps->enable_ping;
   params.enable_user_alternate_protocol_ports =
       session_deps->enable_user_alternate_protocol_ports;
+  params.enable_npn = session_deps->enable_npn;
   params.spdy_default_protocol = session_deps->protocol;
   params.spdy_session_max_recv_window_size =
       session_deps->session_max_recv_window_size;
