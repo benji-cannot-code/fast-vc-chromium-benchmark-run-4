62df3b82138bcf6e31e48f5498087e76136701f0
alancutter <alancutter@chromium.org>
2015-11-24 00:07:23 -0800

    Web Animations: Add SVGNumberOptionalNumberInterpolationType
    This change migrates the following SVG attribute animations over to
    SVGNumberOptionalNumberInterpolationType and enables additive keyframe
    composite modes as well as neutral keyframes:
     - baseFrequency
     - kernelUnitLength
     - radius
     - stdDeviation
    
    This is a rebased reupload of: https://codereview.chromium.org/1419623009
    
    BUG=530436
    
    Review URL: https://codereview.chromium.org/1418193007
    
    Cr-Commit-Position: refs/heads/master@{#361281}

--- third_party/WebKit/Source/core/animation/StringKeyframe.cpp
+++ third_party/WebKit/Source/core/animation/StringKeyframe.cpp
@@ -32,13 +32,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/animation/LengthStyleInterpolation.h"
 #include "core/animation/ListSVGInterpolation.h"
 #include "core/animation/ListStyleInterpolation.h"
-#include "core/animation/NumberOptionalNumberSVGInterpolation.h"
 #include "core/animation/NumberSVGInterpolation.h"
 #include "core/animation/PathSVGInterpolation.h"
 #include "core/animation/RectSVGInterpolation.h"
 #include "core/animation/SVGAngleInterpolationType.h"
 #include "core/animation/SVGIntegerInterpolationType.h"
 #include "core/animation/SVGNumberInterpolationType.h"
+#include "core/animation/SVGNumberOptionalNumberInterpolationType.h"
 #include "core/animation/SVGPointListInterpolationType.h"
 #include "core/animation/SVGStrokeDasharrayStyleInterpolation.h"
 #include "core/animation/SVGValueInterpolationType.h"
@@ -299,6 +299,11 @@ const InterpolationTypes* applicableTypesForProperty(PropertyHandle property)
             || attribute == SVGNames::surfaceScaleAttr
             || attribute == SVGNames::zAttr) {
             applicableTypes->append(adoptPtr(new SVGNumberInterpolationType(attribute)));
+        } else if (attribute == SVGNames::baseFrequencyAttr
+            || attribute == SVGNames::kernelUnitLengthAttr
+            || attribute == SVGNames::radiusAttr
+            || attribute == SVGNames::stdDeviationAttr) {
+            applicableTypes->append(adoptPtr(new SVGNumberOptionalNumberInterpolationType(attribute)));
         } else if (attribute == SVGNames::pointsAttr) {
             applicableTypes->append(adoptPtr(new SVGPointListInterpolationType(attribute)));
         } else if (attribute == HTMLNames::classAttr
@@ -569,8 +574,6 @@ PassRefPtr<Interpolation> createSVGInterpolation(SVGPropertyBase* fromValue, SVG
     case AnimatedLengthList:
         interpolation = ListSVGInterpolation<LengthSVGInterpolation>::maybeCreate(fromValue, toValue, attribute);
         break;
-    case AnimatedNumberOptionalNumber:
-        return NumberOptionalNumberSVGInterpolation::create(fromValue, toValue, attribute);
     case AnimatedNumberList:
         interpolation = ListSVGInterpolation<NumberSVGInterpolation>::maybeCreate(fromValue, toValue, attribute);
         break;
@@ -587,6 +590,7 @@ PassRefPtr<Interpolation> createSVGInterpolation(SVGPropertyBase* fromValue, SVG
     case AnimatedAngle:
     case AnimatedInteger:
     case AnimatedNumber:
+    case AnimatedNumberOptionalNumber:
     case AnimatedPoints:
         ASSERT_NOT_REACHED();
         // Fallthrough.
