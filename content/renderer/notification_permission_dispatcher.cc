ef8b513d17135d5394e63072283358ad6f8c25c8
peter <peter@chromium.org>
2015-11-24 02:55:10 -0800

    Stop using base::IDMap in the NotificationPermissionDispatcher
    BUG=
    
    Review URL: https://codereview.chromium.org/1471833002
    
    Cr-Commit-Position: refs/heads/master@{#361310}

--- content/renderer/notification_permission_dispatcher.cc
+++ content/renderer/notification_permission_dispatcher.cc
@@ -13,38 +13,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/web/WebUserGestureIndicator.h"
 #include "third_party/WebKit/public/web/modules/notifications/WebNotificationPermissionCallback.h"
 
+using blink::WebNotificationPermissionCallback;
+
 namespace content {
 
 NotificationPermissionDispatcher::NotificationPermissionDispatcher(
-    RenderFrame* render_frame) : RenderFrameObserver(render_frame) {
-}
+    RenderFrame* render_frame)
+    : RenderFrameObserver(render_frame) {}
 
-NotificationPermissionDispatcher::~NotificationPermissionDispatcher() {
-}
+NotificationPermissionDispatcher::~NotificationPermissionDispatcher() {}
 
 void NotificationPermissionDispatcher::RequestPermission(
     const blink::WebSecurityOrigin& origin,
-    blink::WebNotificationPermissionCallback* callback) {
+    WebNotificationPermissionCallback* callback) {
   if (!permission_service_.get()) {
     render_frame()->GetServiceRegistry()->ConnectToRemoteService(
         mojo::GetProxy(&permission_service_));
   }
 
-  int request_id = pending_requests_.Add(callback);
+  scoped_ptr<WebNotificationPermissionCallback> owned_callback(callback);
 
+  // base::Unretained is safe here because the Mojo channel, with associated
+  // callbacks, will be deleted before the "this" instance is deleted.
   permission_service_->RequestPermission(
-      PERMISSION_NAME_NOTIFICATIONS,
-      origin.toString().utf8(),
+      PERMISSION_NAME_NOTIFICATIONS, origin.toString().utf8(),
       blink::WebUserGestureIndicator::isProcessingUserGesture(),
       base::Bind(&NotificationPermissionDispatcher::OnPermissionRequestComplete,
-                 base::Unretained(this),
-                 request_id));
+                 base::Unretained(this), base::Passed(owned_callback.Pass())));
 }
 
 void NotificationPermissionDispatcher::OnPermissionRequestComplete(
-    int request_id, PermissionStatus status) {
-  blink::WebNotificationPermissionCallback* callback =
-      pending_requests_.Lookup(request_id);
+    scoped_ptr<WebNotificationPermissionCallback> callback,
+    PermissionStatus status) {
   DCHECK(callback);
 
   blink::WebNotificationPermission permission =
@@ -62,7 +62,6 @@ void NotificationPermissionDispatcher::OnPermissionRequestComplete(
   }
 
   callback->permissionRequestComplete(permission);
-  pending_requests_.Remove(request_id);
 }
 
 }  // namespace content
--- content/renderer/notification_permission_dispatcher.h
+++ content/renderer/notification_permission_dispatcher.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_RENDERER_NOTIFICATION_PERMISSION_DISPATCHER_H_
 #define CONTENT_RENDERER_NOTIFICATION_PERMISSION_DISPATCHER_H_
 
-#include "base/id_map.h"
+#include "base/memory/scoped_ptr.h"
 #include "content/common/permission_service.mojom.h"
 #include "content/public/renderer/render_frame_observer.h"
 
@@ -17,6 +17,8 @@ class WebSecurityOrigin;
 
 namespace content {
 
+class RenderFrame;
+
 // Dispatcher for Web Notification permission requests.
 class NotificationPermissionDispatcher : public RenderFrameObserver {
  public:
@@ -26,18 +28,13 @@ class NotificationPermissionDispatcher : public RenderFrameObserver {
   // Requests permission to display Web Notifications for |origin|. The callback
   // will be invoked when the permission status is available. This class will
   // take ownership of |callback|.
-  void RequestPermission(
-      const blink::WebSecurityOrigin& origin,
-      blink::WebNotificationPermissionCallback* callback);
+  void RequestPermission(const blink::WebSecurityOrigin& origin,
+                         blink::WebNotificationPermissionCallback* callback);
 
  private:
   void OnPermissionRequestComplete(
-      int request_id, PermissionStatus status);
-
-  // Tracks the active notification permission requests. This class takes
-  // ownership of the created WebNotificationPermissionCallback objects.
-  IDMap<blink::WebNotificationPermissionCallback, IDMapOwnPointer>
-      pending_requests_;
+      scoped_ptr<blink::WebNotificationPermissionCallback> callback,
+      PermissionStatus status);
 
   PermissionServicePtr permission_service_;
 
