7d908f283fe9d4cd6e0ae753ea144d520a1e6d69
qiankun.miao <qiankun.miao@intel.com>
2015-11-24 10:39:07 -0800

    Check framebuffer texture attachments are cube complete
    BUG=559362
    TEST=conformance/textures/misc/cube-incomplete-fbo.html
    
    Review URL: https://codereview.chromium.org/1465173002
    
    Cr-Commit-Position: refs/heads/master@{#361397}

--- third_party/WebKit/Source/modules/webgl/WebGLFramebuffer.cpp
+++ third_party/WebKit/Source/modules/webgl/WebGLFramebuffer.cpp
@@ -51,6 +51,7 @@ private:
     GLsizei height() const override;
     GLenum format() const override;
     GLenum type() const override;
+    bool isCubeComplete() const override;
     WebGLSharedObject* object() const override;
     bool isSharedObject(WebGLSharedObject*) const override;
     bool valid() const override;
@@ -144,6 +145,12 @@ GLenum WebGLRenderbufferAttachment::type() const
     return WebGLTexture::getValidTypeForInternalFormat(m_renderbuffer->internalFormat());
 }
 
+bool WebGLRenderbufferAttachment::isCubeComplete() const
+{
+    ASSERT_NOT_REACHED();
+    return false;
+}
+
 class WebGLTextureAttachment final : public WebGLFramebuffer::WebGLAttachment {
 public:
     static WebGLFramebuffer::WebGLAttachment* create(WebGLTexture*, GLenum target, GLint level, GLint layer);
@@ -158,6 +165,7 @@ private:
     GLsizei height() const override;
     GLenum format() const override;
     GLenum type() const override;
+    bool isCubeComplete() const override;
     WebGLSharedObject* object() const override;
     bool isSharedObject(WebGLSharedObject*) const override;
     bool valid() const override;
@@ -253,6 +261,11 @@ void WebGLTextureAttachment::unattach(WebGraphicsContext3D* context, GLenum targ
 GLenum WebGLTextureAttachment::type() const
 {
     return m_texture->getType(m_target, m_level);
+}
+
+bool WebGLTextureAttachment::isCubeComplete() const
+{
+    return m_texture->isCubeComplete();
 }
 
 bool isColorRenderable(GLenum internalformat, bool includesFloat)
@@ -451,6 +464,12 @@ bool WebGLFramebuffer::isAttachmentComplete(WebGLAttachment* attachedObject, GLe
         *reason = "attachment has a 0 dimension";
         return false;
     }
+
+    if (attachedObject->object()->isTexture() && !attachedObject->isCubeComplete()) {
+        *reason = "attachment is not cube complete";
+        return false;
+    }
+
     return true;
 }
 
