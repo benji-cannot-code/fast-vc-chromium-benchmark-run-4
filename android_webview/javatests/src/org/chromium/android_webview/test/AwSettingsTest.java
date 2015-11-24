b6e74c561d33c7b9b63e1fd24e53396e2860f4b5
boliu <boliu@chromium.org>
2015-11-24 15:41:13 -0800

    Enable testMediaPlaybackWithoutUserGesture
    Test is passing.
    
    BUG=304549
    
    Review URL: https://codereview.chromium.org/1476623002
    
    Cr-Commit-Position: refs/heads/master@{#361490}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
@@ -10,6 +10,7 @@ import android.graphics.Point;
 import android.net.http.SslError;
 import android.os.Build;
 import android.os.SystemClock;
+import android.test.suitebuilder.annotation.LargeTest;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.test.suitebuilder.annotation.SmallTest;
 import android.view.WindowManager;
@@ -29,7 +30,6 @@ import org.chromium.android_webview.test.util.CommonResources;
 import org.chromium.android_webview.test.util.ImagePageGenerator;
 import org.chromium.android_webview.test.util.VideoTestUtil;
 import org.chromium.android_webview.test.util.VideoTestWebServer;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.TestFileUtil;
 import org.chromium.base.test.util.UrlUtils;
@@ -2717,13 +2717,9 @@ public class AwSettingsTest extends AwTestBase {
         assertEquals(defaultScale, getPixelScaleOnUiThread(awContents), .01f);
     }
 
-    /*
     @DisableHardwareAccelerationForTest
     @LargeTest
     @Feature({"AndroidWebView", "Preferences"})
-    http://crbug.com/304549
-    */
-    @DisabledTest
     public void testMediaPlaybackWithoutUserGesture() throws Throwable {
         assertTrue(VideoTestUtil.runVideoTest(this, false, false, WAIT_TIMEOUT_MS));
     }
