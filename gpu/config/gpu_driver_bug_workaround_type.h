9fa7f60e266af32b6697c409014830f199b2bae1
bajones <bajones@chromium.org>
2015-11-24 09:06:10 -0800

    Ensured that texture completeness takes into account base and max level
    BUG=295792
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1465993003
    
    Cr-Commit-Position: refs/heads/master@{#361373}

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -147,6 +147,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          wake_up_gpu_before_drawing)                         \
   GPU_OP(UNBIND_EGL_CONTEXT_TO_FLUSH_DRIVER_CACHES,          \
          unbind_egl_context_to_flush_driver_caches)          \
+  GPU_OP(SET_ZERO_LEVEL_BEFORE_GENERATING_MIPMAP,            \
+         set_zero_level_before_generating_mipmap)            \
 
 namespace gpu {
 
