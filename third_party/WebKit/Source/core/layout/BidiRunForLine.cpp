4eaaeb928d35ae05aaa5f2b876136e43805e95a2
kojii <kojii@chromium.org>
2015-11-24 00:13:49 -0800

    Ignore unicode-bidi: isolate for visual order elements
    This patch makes visual order elements to ignore bidi-isolate[1]. As a
    result, such elements with unicode-bidi: isolate no longer bidi-reorder
    their content.
    
    The behavior of bidi-isolate for visual order (iso-8859-8) is not well
    defined. It is reasonable to ignore bidi-isolate, however, given visual
    order documents ignore dir attributes today.
    
    Without this fix, when bidi-isolate is turned on by default in
    crbug.com/391260, dir attributes start to effect, which is likely to
    break web-compat.
    
    [1] https://drafts.csswg.org/css-writing-modes-3/#bidi-isolate
    
    BUG=559257
    
    Review URL: https://codereview.chromium.org/1463893002
    
    Cr-Commit-Position: refs/heads/master@{#361283}

--- third_party/WebKit/Source/core/layout/BidiRunForLine.cpp
+++ third_party/WebKit/Source/core/layout/BidiRunForLine.cpp
@@ -36,7 +36,7 @@ static LayoutObject* firstLayoutObjectForDirectionalityDetermination(
 {
     LayoutObject* next = current;
     while (current) {
-        if (isIsolated(current->style()->unicodeBidi())
+        if (treatAsIsolated(current->styleRef())
             && (current->isLayoutInline() || current->isLayoutBlock())) {
             if (current != root)
                 current = nullptr;
@@ -57,7 +57,7 @@ static LayoutObject* firstLayoutObjectForDirectionalityDetermination(
             break;
 
         if (!isIteratorTarget(LineLayoutItem(current))
-            && !isIsolated(current->style()->unicodeBidi()))
+            && !treatAsIsolated(current->styleRef()))
             next = current->slowFirstChild();
 
         if (!next) {
