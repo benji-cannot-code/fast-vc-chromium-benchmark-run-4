8ee89689962fd41a126006455e8c7fcd89b7ec63
vmpstr <vmpstr@chromium.org>
2015-11-24 11:43:15 -0800

    cc: Skip animation-checkerboard frames even when animation is done
    This patch ensures that we continue to skip checkerboard frames that
    were checkerboarding since the last animation. This means that even if
    animation is over, we will continue to drop frames since those checker-
    boards were the result of the animation.
    
    BUG=554924
    R=danakj@chromium.org, ajuma@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1461243003
    
    Cr-Commit-Position: refs/heads/master@{#361413}

--- cc/layers/layer_impl.cc
+++ cc/layers/layer_impl.cc
@@ -71,6 +71,7 @@ LayerImpl::LayerImpl(LayerTreeImpl* tree_impl,
       transform_is_invertible_(true),
       is_container_for_fixed_position_layers_(false),
       is_affected_by_page_scale_(true),
+      was_ever_ready_since_last_transform_animation_(true),
       background_color_(0),
       opacity_(1.0),
       blend_mode_(SkXfermode::kSrcOver_Mode),
@@ -934,6 +935,7 @@ void LayerImpl::OnOpacityAnimated(float opacity) {
 void LayerImpl::OnTransformAnimated(const gfx::Transform& transform) {
   SetTransform(transform);
   UpdatePropertyTreeTransform();
+  was_ever_ready_since_last_transform_animation_ = false;
 }
 
 void LayerImpl::OnScrollOffsetAnimated(const gfx::ScrollOffset& scroll_offset) {
@@ -952,6 +954,7 @@ void LayerImpl::OnAnimationWaitingForDeletion() {}
 
 void LayerImpl::OnTransformIsPotentiallyAnimatingChanged(bool is_animating) {
   UpdatePropertyTreeTransformIsAnimated(is_animating);
+  was_ever_ready_since_last_transform_animation_ = false;
 }
 
 bool LayerImpl::IsActive() const {
