c59372ec5ca59be24936dd5fa4507aaf2aa7379b
ukai <ukai@chromium.org>
2015-11-23 21:26:51 -0800

    resource_bundle_mac_unittest.mm: fix case for AppKit/AppKit.h
    in SDK, it is AppKit.framework/Versions/C/Headers/AppKit.h.
    Use the same case, so it would work on case sensitive filesystem.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1454113004
    
    Cr-Commit-Position: refs/heads/master@{#361259}

--- ui/base/resource/resource_bundle_mac_unittest.mm
+++ ui/base/resource/resource_bundle_mac_unittest.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/resource/resource_bundle.h"
 
-#import <Appkit/Appkit.h>
+#import <AppKit/AppKit.h>
 
 #include "base/base_paths.h"
 #include "base/big_endian.h"
