1899d8f3c45807fe2c272e20fee976991c9792ff
sdefresne <sdefresne@chromium.org>
2015-11-25 04:40:05 -0800

    Merge fixes for CRWContentView.
    - Defer adding views to CRWWebControllerContainerView until |-layoutSubviews|.
    - Remove DISABLED_ prefix for fixed tests.
    - Introduced delegate for CRWWebControllerContainerView header
      height.
    
    BUG=503010,503014
    
    Review URL: https://codereview.chromium.org/1469923002
    
    Cr-Commit-Position: refs/heads/master@{#361634}

--- ios/web/public/test/test_web_view_content_view.mm
+++ ios/web/public/test/test_web_view_content_view.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ios/web/public/test/test_web_view_content_view.h"
+#import "ios/web/public/test/test_web_view_content_view.h"
 
 #include "base/logging.h"
 #include "base/mac/scoped_nsobject.h"
