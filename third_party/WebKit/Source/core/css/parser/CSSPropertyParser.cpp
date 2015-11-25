fc2c92d4aef8ef9be4c5d798377512b838651d75
timloh <timloh@chromium.org>
2015-11-24 21:03:19 -0800

    Pull CSS-wide keyword handling out of LegacyCSSPropertyParser
    This patch moves the handling for the css-wide keywords (initial,
    inherit and unset) out of the LegacyCSSPropertyParser and into the
    CSSPropertyParser where it can be parsed with CSSParserTokens. This
    patch also adds an additional function between the public parseValue
    and the internal parseValue.
    
    Subsequent patches will make this new function also responsible for
    handling CSS variables and calling out to parseSingleValue and
    parseShorthand, instead of having these called from the legacy paths.
    
    BUG=499780
    
    Review URL: https://codereview.chromium.org/1459633004
    
    Cr-Commit-Position: refs/heads/master@{#361577}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -33,10 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-CSSPropertyParser::CSSPropertyParser(CSSParserValueList* valueList, const CSSParserTokenRange& range,
+CSSPropertyParser::CSSPropertyParser(const CSSParserTokenRange& range,
     const CSSParserContext& context, WillBeHeapVector<CSSProperty, 256>& parsedProperties)
-    : m_valueList(valueList)
-    , m_range(range)
+    : m_range(range)
     , m_context(context)
     , m_parsedProperties(parsedProperties)
     , m_inParseShorthand(0)
@@ -65,10 +64,7 @@ bool CSSPropertyParser::parseValue(CSSPropertyID unresolvedProperty, bool import
         return false;
     int parsedPropertiesSize = parsedProperties.size();
 
-    CSSParserValueList valueList(range);
-    if (!valueList.size())
-        return false; // Parser error
-    CSSPropertyParser parser(&valueList, range, context, parsedProperties);
+    CSSPropertyParser parser(range, context, parsedProperties);
     CSSPropertyID resolvedProperty = resolveCSSPropertyID(unresolvedProperty);
     bool parseSuccess;
 
@@ -78,7 +74,7 @@ bool CSSPropertyParser::parseValue(CSSPropertyID unresolvedProperty, bool import
     } else if (ruleType == StyleRule::FontFace) {
         parseSuccess = parser.parseFontFaceDescriptor(resolvedProperty);
     } else {
-        parseSuccess = parser.parseValue(unresolvedProperty, important);
+        parseSuccess = parser.parseValueStart(unresolvedProperty, important);
     }
 
     // This doesn't count UA style sheets
@@ -96,6 +92,40 @@ bool CSSPropertyParser::isValidNumericValue(double value)
     return std::isfinite(value)
         && value >= -std::numeric_limits<float>::max()
         && value <= std::numeric_limits<float>::max();
+}
+
+bool CSSPropertyParser::parseValueStart(CSSPropertyID unresolvedProperty, bool important)
+{
+    if (consumeCSSWideKeyword(unresolvedProperty, important))
+        return true;
+
+    CSSParserValueList valueList(m_range);
+    if (!valueList.size())
+        return false;
+    m_valueList = &valueList;
+    return parseValue(unresolvedProperty, important);
+}
+
+bool CSSPropertyParser::consumeCSSWideKeyword(CSSPropertyID unresolvedProperty, bool important)
+{
+    CSSParserTokenRange rangeCopy = m_range;
+    CSSValueID id = rangeCopy.consumeIncludingWhitespace().id();
+    if (!rangeCopy.atEnd())
+        return false;
+
+    RefPtrWillBeRawPtr<CSSValue> value;
+    if (id == CSSValueInitial)
+        value = cssValuePool().createExplicitInitialValue();
+    else if (id == CSSValueInherit)
+        value = cssValuePool().createInheritedValue();
+    else if (id == CSSValueUnset)
+        value = cssValuePool().createUnsetValue();
+    else
+        return false;
+
+    addExpandedPropertyForValue(resolveCSSPropertyID(unresolvedProperty), value.release(), important);
+    m_range = rangeCopy;
+    return true;
 }
 
 // Helper methods for consuming tokens starts here.
