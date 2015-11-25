d4c239d53d6fcdde07cfc4990f4f5e4d1c77e918
bondd <bondd@chromium.org>
2015-11-24 17:42:17 -0800

    Autofill: Display legal message footer when data is available.
    Previous CLs https://codereview.chromium.org/1412073004/ and
    https://codereview.chromium.org/1407093007/ download the legal message
    from a server and provide UI to display it, respectively. This CL
    connects the previous two CLs, so that the downloaded message is
    actually displayed to the user.
    
    BUG=552667
    
    Review URL: https://codereview.chromium.org/1463323003
    
    Cr-Commit-Position: refs/heads/master@{#361518}

--- chrome/browser/ui/autofill/save_card_bubble_controller_impl_unittest.cc
+++ chrome/browser/ui/autofill/save_card_bubble_controller_impl_unittest.cc
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
+#include "chrome/browser/ui/autofill/save_card_bubble_view.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -16,28 +19,30 @@ namespace autofill {
 typedef SaveCardBubbleController::LegalMessageLine LegalMessageLine;
 typedef SaveCardBubbleController::LegalMessageLines LegalMessageLines;
 
-class SaveCardBubbleControllerImplTest
-    : public ChromeRenderViewHostTestHarness {
+class SaveCardBubbleControllerImplTest : public BrowserWithTestWindowTest {
  public:
   SaveCardBubbleControllerImplTest() {}
 
   void SetUp() override {
-    ChromeRenderViewHostTestHarness::SetUp();
-    SaveCardBubbleControllerImpl::CreateForWebContents(web_contents());
+    BrowserWithTestWindowTest::SetUp();
+    AddTab(browser(), GURL("about:blank"));
+    SaveCardBubbleControllerImpl::CreateForWebContents(
+        browser()->tab_strip_model()->GetActiveWebContents());
   }
 
-  void SetLegalMessageExpectSuccess(const std::string& message_json) {
-    scoped_ptr<base::Value> value(base::JSONReader::Read(message_json));
-    const base::ListValue* lines;
-    ASSERT_TRUE(value->GetAsList(&lines));
-    EXPECT_TRUE(controller()->SetLegalMessage(*lines));
+  BrowserWindow* CreateBrowserWindow() override {
+    return new SaveCardBubbleTestBrowserWindow();
   }
 
-  void SetLegalMessageExpectFailure(const std::string& message_json) {
+  void SetLegalMessage(const std::string& message_json) {
     scoped_ptr<base::Value> value(base::JSONReader::Read(message_json));
-    const base::ListValue* lines;
-    ASSERT_TRUE(value->GetAsList(&lines));
-    EXPECT_FALSE(controller()->SetLegalMessage(*lines));
+    ASSERT_TRUE(value);
+    base::DictionaryValue* dictionary;
+    ASSERT_TRUE(value->GetAsDictionary(&dictionary));
+    scoped_ptr<base::DictionaryValue> legal_message =
+        dictionary->CreateDeepCopy();
+    controller()->ShowBubbleForUpload(base::Bind(&SaveCardCallback),
+                                      legal_message.Pass());
   }
 
   // Returns true if lines are the same.
@@ -70,18 +75,42 @@ class SaveCardBubbleControllerImplTest
 
  protected:
   SaveCardBubbleControllerImpl* controller() {
-    return SaveCardBubbleControllerImpl::FromWebContents(web_contents());
+    return SaveCardBubbleControllerImpl::FromWebContents(
+        browser()->tab_strip_model()->GetActiveWebContents());
   }
 
  private:
+  class TestSaveCardBubbleView : public SaveCardBubbleView {
+    void Hide() override{};
+  };
+
+  class SaveCardBubbleTestBrowserWindow : public TestBrowserWindow {
+   public:
+    SaveCardBubbleView* ShowSaveCreditCardBubble(
+        content::WebContents* contents,
+        SaveCardBubbleController* controller,
+        bool user_gesture) override {
+      if (!save_card_bubble_view_)
+        save_card_bubble_view_.reset(new TestSaveCardBubbleView());
+      return save_card_bubble_view_.get();
+    }
+
+   private:
+    scoped_ptr<TestSaveCardBubbleView> save_card_bubble_view_;
+  };
+
+  static void SaveCardCallback() {}
+
   DISALLOW_COPY_AND_ASSIGN(SaveCardBubbleControllerImplTest);
 };
 
 TEST_F(SaveCardBubbleControllerImplTest, NoParameters) {
-  SetLegalMessageExpectSuccess(
-      "[ {"
-      "   \"template\": \"This is the entire message.\""
-      "} ]");
+  SetLegalMessage(
+      "{"
+      "  \"line\" : [ {"
+      "     \"template\": \"This is the entire message.\""
+      "  } ]"
+      "}");
 
   LegalMessageLine expected_line;
   expected_line.text = base::ASCIIToUTF16("This is the entire message.");
@@ -91,14 +120,16 @@ TEST_F(SaveCardBubbleControllerImplTest, NoParameters) {
 }
 
 TEST_F(SaveCardBubbleControllerImplTest, SingleParameter) {
-  SetLegalMessageExpectSuccess(
-      "[ {"
-      "   \"template\": \"Panda {0}.\","
-      "   \"template_parameter\": [ {"
-      "      \"display_text\": \"bears are fuzzy\","
-      "      \"url\": \"http://www.example.com\""
-      "   } ]"
-      "} ]");
+  SetLegalMessage(
+      "{"
+      "  \"line\" : [ {"
+      "     \"template\": \"Panda {0}.\","
+      "     \"template_parameter\": [ {"
+      "        \"display_text\": \"bears are fuzzy\","
+      "        \"url\": \"http://www.example.com\""
+      "     } ]"
+      "  } ]"
+      "}");
 
   LegalMessageLine expected_line;
   expected_line.text = base::ASCIIToUTF16("Panda bears are fuzzy.");
@@ -111,38 +142,46 @@ TEST_F(SaveCardBubbleControllerImplTest, SingleParameter) {
 }
 
 TEST_F(SaveCardBubbleControllerImplTest, MissingUrl) {
-  SetLegalMessageExpectFailure(
-      "[ {"
-      "   \"template\": \"Panda {0}.\","
-      "   \"template_parameter\": [ {"
-      "      \"display_text\": \"bear\""
-      "   } ]"
-      "} ]");
+  SetLegalMessage(
+      "{"
+      "  \"line\" : [ {"
+      "     \"template\": \"Panda {0}.\","
+      "     \"template_parameter\": [ {"
+      "        \"display_text\": \"bear\""
+      "     } ]"
+      "  } ]"
+      "}");
+  // Legal message is invalid so GetLegalMessageLines() should return no lines.
   EXPECT_TRUE(CompareLegalMessages(LegalMessageLines(),
                                    controller()->GetLegalMessageLines()));
 }
 
 TEST_F(SaveCardBubbleControllerImplTest, MissingDisplayText) {
-  SetLegalMessageExpectFailure(
-      "[ {"
-      "   \"template\": \"Panda {0}.\","
-      "   \"template_parameter\": [ {"
+  SetLegalMessage(
+      "{"
+      "  \"line\" : [ {"
+      "    \"template\": \"Panda {0}.\","
+      "    \"template_parameter\": [ {"
       "      \"url\": \"http://www.example.com\""
-      "   } ]"
-      "} ]");
+      "     } ]"
+      "  } ]"
+      "}");
+  // Legal message is invalid so GetLegalMessageLines() should return no lines.
   EXPECT_TRUE(CompareLegalMessages(LegalMessageLines(),
                                    controller()->GetLegalMessageLines()));
 }
 
 TEST_F(SaveCardBubbleControllerImplTest, EscapeCharacters) {
-  SetLegalMessageExpectSuccess(
-      "[ {"
-      "   \"template\": \"Panda '{'{0}'}' '{1}' don't $1.\","
-      "   \"template_parameter\": [ {"
+  SetLegalMessage(
+      "{"
+      "  \"line\" : [ {"
+      "    \"template\": \"Panda '{'{0}'}' '{1}' don't $1.\","
+      "    \"template_parameter\": [ {"
       "      \"display_text\": \"bears\","
       "      \"url\": \"http://www.example.com\""
-      "   } ]"
-      "} ]");
+      "     } ]"
+      "  } ]"
+      "}");
 
   LegalMessageLine expected_line;
   expected_line.text = base::ASCIIToUTF16("Panda {bears} {1} don't $1.");
@@ -155,10 +194,12 @@ TEST_F(SaveCardBubbleControllerImplTest, EscapeCharacters) {
 }
 
 TEST_F(SaveCardBubbleControllerImplTest, ConsecutiveDollarSigns) {
-  SetLegalMessageExpectSuccess(
-      "[ {"
-      "   \"template\": \"$$\""
-      "} ]");
+  SetLegalMessage(
+      "{"
+      "  \"line\" : [ {"
+      "     \"template\": \"$$\""
+      "  } ]"
+      "}");
 
   // Consecutive dollar signs do not expand correctly (see comment in
   // ReplaceTemplatePlaceholders() in save_card_bubble_controller_impl.cc).
@@ -179,31 +220,38 @@ TEST_F(SaveCardBubbleControllerImplTest, DollarAndParenthesis) {
   // If this is fixed and this test starts to fail, please update the
   // "Caveats" section of the SaveCardBubbleControllerImpl::SetLegalMessage()
   // header file comment.
-  SetLegalMessageExpectFailure(
-      "[ {"
-      "   \"template\": \"${0}\","
-      "   \"template_parameter\": [ {"
+  SetLegalMessage(
+      "{"
+      "  \"line\" : [ {"
+      "    \"template\": \"${0}\","
+      "    \"template_parameter\": [ {"
       "      \"display_text\": \"bears\","
       "      \"url\": \"http://www.example.com\""
-      "   } ]"
-      "} ]");
+      "    } ]"
+      "  } ]"
+      "}");
+  // Legal message is invalid so GetLegalMessageLines() should return no lines.
+  EXPECT_TRUE(CompareLegalMessages(LegalMessageLines(),
+                                   controller()->GetLegalMessageLines()));
 }
 
 TEST_F(SaveCardBubbleControllerImplTest, MultipleParameters) {
-  SetLegalMessageExpectSuccess(
-      "[ {"
-      "   \"template\": \"Panda {0} like {2} eat {1}.\","
-      "   \"template_parameter\": [ {"
+  SetLegalMessage(
+      "{"
+      "  \"line\" : [ {"
+      "    \"template\": \"Panda {0} like {2} eat {1}.\","
+      "    \"template_parameter\": [ {"
       "      \"display_text\": \"bears\","
       "      \"url\": \"http://www.example.com/0\""
-      "   }, {"
+      "    }, {"
       "      \"display_text\": \"bamboo\","
       "      \"url\": \"http://www.example.com/1\""
-      "   }, {"
+      "    }, {"
       "      \"display_text\": \"to\","
       "      \"url\": \"http://www.example.com/2\""
-      "   } ]"
-      "} ]");
+      "    } ]"
+      "  } ]"
+      "}");
 
   LegalMessageLine expected_line;
   expected_line.text = base::ASCIIToUTF16("Panda bears like to eat bamboo.");
@@ -218,29 +266,31 @@ TEST_F(SaveCardBubbleControllerImplTest, MultipleParameters) {
 }
 
 TEST_F(SaveCardBubbleControllerImplTest, MultipleLineElements) {
-  SetLegalMessageExpectSuccess(
-      "[ {"
-      "   \"template\": \"Panda {0}\","
-      "   \"template_parameter\": [ {"
+  SetLegalMessage(
+      "{"
+      "  \"line\" : [ {"
+      "    \"template\": \"Panda {0}\","
+      "    \"template_parameter\": [ {"
       "      \"display_text\": \"bears\","
       "      \"url\": \"http://www.example.com/line_0_param_0\""
-      "   } ]"
-      "}, {"
-      "   \"template\": \"like {1} eat {0}.\","
-      "   \"template_parameter\": [ {"
+      "    } ]"
+      "  }, {"
+      "    \"template\": \"like {1} eat {0}.\","
+      "    \"template_parameter\": [ {"
       "      \"display_text\": \"bamboo\","
       "      \"url\": \"http://www.example.com/line_1_param_0\""
-      "   }, {"
+      "    }, {"
       "      \"display_text\": \"to\","
       "      \"url\": \"http://www.example.com/line_1_param_1\""
-      "   } ]"
-      "}, {"
-      "   \"template\": \"The {0}.\","
-      "   \"template_parameter\": [ {"
+      "    } ]"
+      "  }, {"
+      "    \"template\": \"The {0}.\","
+      "    \"template_parameter\": [ {"
       "      \"display_text\": \"end\","
       "      \"url\": \"http://www.example.com/line_2_param_0\""
-      "   } ]"
-      "} ]");
+      "    } ]"
+      "  } ]"
+      "}");
 
   // Line 0.
   LegalMessageLine expected_line_0;
@@ -271,23 +321,25 @@ TEST_F(SaveCardBubbleControllerImplTest, MultipleLineElements) {
 }
 
 TEST_F(SaveCardBubbleControllerImplTest, EmbeddedNewlines) {
-  SetLegalMessageExpectSuccess(
-      "[ {"
-      "   \"template\": \"Panda {0}\nlike {2} eat {1}.\nThe {3}.\","
-      "   \"template_parameter\": [ {"
+  SetLegalMessage(
+      "{"
+      "  \"line\" : [ {"
+      "    \"template\": \"Panda {0}\nlike {2} eat {1}.\nThe {3}.\","
+      "    \"template_parameter\": [ {"
       "      \"display_text\": \"bears\","
       "      \"url\": \"http://www.example.com/0\""
-      "   }, {"
+      "    }, {"
       "      \"display_text\": \"bamboo\","
       "      \"url\": \"http://www.example.com/1\""
-      "   }, {"
+      "    }, {"
       "      \"display_text\": \"to\","
       "      \"url\": \"http://www.example.com/2\""
-      "   }, {"
+      "    }, {"
       "      \"display_text\": \"end\","
       "      \"url\": \"http://www.example.com/3\""
-      "   } ]"
-      "} ]");
+      "    } ]"
+      "  } ]"
+      "}");
 
   LegalMessageLine expected_line;
   expected_line.text =
@@ -304,32 +356,34 @@ TEST_F(SaveCardBubbleControllerImplTest, EmbeddedNewlines) {
 }
 
 TEST_F(SaveCardBubbleControllerImplTest, MaximumPlaceholders) {
-  SetLegalMessageExpectSuccess(
-      "[ {"
-      "   \"template\": \"a{0} b{1} c{2} d{3} e{4} f{5} g{6}\","
-      "   \"template_parameter\": [ {"
+  SetLegalMessage(
+      "{"
+      "  \"line\" : [ {"
+      "    \"template\": \"a{0} b{1} c{2} d{3} e{4} f{5} g{6}\","
+      "    \"template_parameter\": [ {"
       "      \"display_text\": \"A\","
       "      \"url\": \"http://www.example.com/0\""
-      "   }, {"
+      "    }, {"
       "      \"display_text\": \"B\","
       "      \"url\": \"http://www.example.com/1\""
-      "   }, {"
+      "    }, {"
       "      \"display_text\": \"C\","
       "      \"url\": \"http://www.example.com/2\""
-      "   }, {"
+      "    }, {"
       "      \"display_text\": \"D\","
       "      \"url\": \"http://www.example.com/3\""
-      "   }, {"
+      "    }, {"
       "      \"display_text\": \"E\","
       "      \"url\": \"http://www.example.com/4\""
-      "   }, {"
+      "    }, {"
       "      \"display_text\": \"F\","
       "      \"url\": \"http://www.example.com/5\""
-      "   }, {"
+      "    }, {"
       "      \"display_text\": \"G\","
       "      \"url\": \"http://www.example.com/6\""
-      "   } ]"
-      "} ]");
+      "    } ]"
+      "  } ]"
+      "}");
 
   LegalMessageLine expected_line;
   expected_line.text = base::ASCIIToUTF16("aA bB cC dD eE fF gG");
