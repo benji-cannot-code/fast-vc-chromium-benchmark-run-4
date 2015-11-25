df99871ab30d7c0a6b5d7643b088a902324be510
torne <torne@chromium.org>
2015-11-25 10:04:30 -0800

    jni: Pass method parameters as JavaParamRef in printing.
    Pass all object parameters to JNI methods in JavaParamRef<> wrappers.
    This matches previous changes made to do this for JNI non-method
    functions.
    
    BUG=519562
    R=avayvod@chromium.org
    
    Review URL: https://codereview.chromium.org/1477783002
    
    Cr-Commit-Position: refs/heads/master@{#361696}

--- printing/printing_context_android.cc
+++ printing/printing_context_android.cc
@@ -104,9 +104,10 @@ void PrintingContextAndroid::AskUserForSettings(
   }
 }
 
-void PrintingContextAndroid::AskUserForSettingsReply(JNIEnv* env,
-                                                     jobject obj,
-                                                     jboolean success) {
+void PrintingContextAndroid::AskUserForSettingsReply(
+    JNIEnv* env,
+    const JavaParamRef<jobject>& obj,
+    jboolean success) {
   if (!success) {
     // TODO(cimamoglu): Differentiate between FAILED And CANCEL.
     callback_.Run(FAILED);
@@ -138,8 +139,9 @@ void PrintingContextAndroid::AskUserForSettingsReply(JNIEnv* env,
   callback_.Run(OK);
 }
 
-void PrintingContextAndroid::ShowSystemDialogDone(JNIEnv* env,
-                                                  jobject obj) {
+void PrintingContextAndroid::ShowSystemDialogDone(
+    JNIEnv* env,
+    const JavaParamRef<jobject>& obj) {
   // Settings are not updated, callback is called only to unblock javascript.
   callback_.Run(CANCEL);
 }
