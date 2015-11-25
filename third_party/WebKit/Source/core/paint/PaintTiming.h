8311cb3d6e69e13447ba7e36636976bb99773a6b
ksakamoto <ksakamoto@chromium.org>
2015-11-24 19:55:25 -0800

    GCC build fix after r361307
    Document.h declares some attributes for WillBeHeapSupplement<Document>,
    so it must be included from header file of Document supplement.
    
    BUG=544811
    
    Review URL: https://codereview.chromium.org/1468053006
    
    Cr-Commit-Position: refs/heads/master@{#361566}

--- third_party/WebKit/Source/core/paint/PaintTiming.h
+++ third_party/WebKit/Source/core/paint/PaintTiming.h
@@ -6,12 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PaintTiming_h
 #define PaintTiming_h
 
+#include "core/dom/Document.h"
 #include "platform/Supplementable.h"
 #include "platform/heap/Handle.h"
 
 namespace blink {
 
-class Document;
 class LocalFrame;
 
 class PaintTiming final : public NoBaseWillBeGarbageCollectedFinalized<PaintTiming>, public WillBeHeapSupplement<Document> {
