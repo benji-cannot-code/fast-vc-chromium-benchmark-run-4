395de57ecbeb2172e3655496b2f118e4f6a21609
dtseng <dtseng@chromium.org>
2015-11-24 19:12:21 -0800

    Makes ChromeVox ignore some elements in views that are invisible, or zero width and height.
    TEST=navigate through system tray using ChromeVox+left/right; observe ordering is the same as tabbing and that we skip invisible controls. Also, verify that we can reach volume slider. Verify that splitters can still be reached (giving grouping information not found in tabbing).
    In Chrome build with webstore ChromeVox, verify items in toolbar are skipped appropriately.
    BUG=532164
    
    Review URL: https://codereview.chromium.org/1475643005
    
    Cr-Commit-Position: refs/heads/master@{#361533}

--- ash/system/tray/tray_background_view.cc
+++ ash/system/tray/tray_background_view.cc
@@ -332,7 +332,7 @@ void TrayBackgroundView::ChildPreferredSizeChanged(views::View* child) {
 }
 
 void TrayBackgroundView::GetAccessibleState(ui::AXViewState* state) {
-  state->role = ui::AX_ROLE_BUTTON;
+  ActionableView::GetAccessibleState(state);
   state->name = GetAccessibleNameForTray();
 }
 
