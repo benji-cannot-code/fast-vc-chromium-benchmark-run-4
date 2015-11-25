b1dc74503ba386b5eb09d01bfb03ca26afcd6d19
jyw <jyw@chromium.org>
2015-11-24 19:20:55 -0800

    Enable pcm_s32le audio decoding.
    To accomodate this change, a new kSampleFormatS24 has been introduced.
    It is only used with S24LE PCM input. FFmpeg treats pcm_s24le as an
    encoded format that can be decoded into s32le samples; kSampleFormatS24
    only exists on the Chrome side to distinguish FFmpeg's pcm_s24le from
    pcm_s32le, both of which use Chrome's catch-all kCodecPCM codec and
    the same real sample format.
    
    BUG=557170,internal b/22378279
    TEST=media_unittests --gtest_filter=AudioBufferTest.*
    media_unittests --gtest_filter=PipelineIntegrationTest.S32PlaybackHashed
    
    Review URL: https://codereview.chromium.org/1422113002
    
    Cr-Commit-Position: refs/heads/master@{#361545}

--- media/ffmpeg/ffmpeg_common.cc
+++ media/ffmpeg/ffmpeg_common.cc
@@ -75,6 +75,7 @@ static AudioCodec CodecIDToAudioCodec(AVCodecID codec_id) {
     case AV_CODEC_ID_PCM_U8:
     case AV_CODEC_ID_PCM_S16LE:
     case AV_CODEC_ID_PCM_S24LE:
+    case AV_CODEC_ID_PCM_S32LE:
     case AV_CODEC_ID_PCM_F32LE:
       return kCodecPCM;
     case AV_CODEC_ID_PCM_S16BE:
@@ -118,8 +119,10 @@ static AVCodecID AudioCodecToCodecID(AudioCodec audio_codec,
           return AV_CODEC_ID_PCM_U8;
         case kSampleFormatS16:
           return AV_CODEC_ID_PCM_S16LE;
-        case kSampleFormatS32:
+        case kSampleFormatS24:
           return AV_CODEC_ID_PCM_S24LE;
+        case kSampleFormatS32:
+          return AV_CODEC_ID_PCM_S32LE;
         case kSampleFormatF32:
           return AV_CODEC_ID_PCM_F32LE;
         default:
@@ -245,14 +248,18 @@ static int VideoCodecProfileToProfileID(VideoCodecProfile profile) {
   return FF_PROFILE_UNKNOWN;
 }
 
-SampleFormat AVSampleFormatToSampleFormat(AVSampleFormat sample_format) {
+SampleFormat AVSampleFormatToSampleFormat(AVSampleFormat sample_format,
+                                          AVCodecID codec_id) {
   switch (sample_format) {
     case AV_SAMPLE_FMT_U8:
       return kSampleFormatU8;
     case AV_SAMPLE_FMT_S16:
       return kSampleFormatS16;
     case AV_SAMPLE_FMT_S32:
-      return kSampleFormatS32;
+      if (codec_id == AV_CODEC_ID_PCM_S24LE)
+        return kSampleFormatS24;
+      else
+        return kSampleFormatS32;
     case AV_SAMPLE_FMT_FLT:
       return kSampleFormatF32;
     case AV_SAMPLE_FMT_S16P:
@@ -273,6 +280,8 @@ static AVSampleFormat SampleFormatToAVSampleFormat(SampleFormat sample_format) {
       return AV_SAMPLE_FMT_U8;
     case kSampleFormatS16:
       return AV_SAMPLE_FMT_S16;
+    // pcm_s24le is treated as a codec with sample format s32 in ffmpeg
+    case kSampleFormatS24:
     case kSampleFormatS32:
       return AV_SAMPLE_FMT_S32;
     case kSampleFormatF32:
@@ -294,8 +303,8 @@ bool AVCodecContextToAudioDecoderConfig(const AVCodecContext* codec_context,
 
   AudioCodec codec = CodecIDToAudioCodec(codec_context->codec_id);
 
-  SampleFormat sample_format =
-      AVSampleFormatToSampleFormat(codec_context->sample_fmt);
+  SampleFormat sample_format = AVSampleFormatToSampleFormat(
+      codec_context->sample_fmt, codec_context->codec_id);
 
   ChannelLayout channel_layout = ChannelLayoutToChromeChannelLayout(
       codec_context->channel_layout, codec_context->channels);
