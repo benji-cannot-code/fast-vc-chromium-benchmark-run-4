7c324378bdc99937c86cfcb37df050752916d88d
sigbjornf <sigbjornf@opera.com>
2015-11-25 05:13:22 -0800

    Split up leak detector into two stages for better leak reporting.
    The leak detector clears out resources along with issuing a sequence of GCs
    before taking object census. It then counting up resources that are left and
    reporting these as leaking.
    
    With Oilpan enabled, RenderViewTest needs to carefully orchestrate its shutdown
    to reliably not report the frame(s) attached to the view as leaking. (With Oilpan
    enabled, frames will delayed'ly release resources upon frame close()ing requiring
    a follow-on GC to clear out those resources.)
    
    Accommodate that by splitting out the leak detector into two -- with RenderViewTest
    injecting the clearing of its view in between those.
    
    R=haraken, jochen, hajimehoshi
    BUG=561293
    
    Review URL: https://codereview.chromium.org/1472943004
    
    Cr-Commit-Position: refs/heads/master@{#361638}

--- content/public/test/render_view_test.cc
+++ content/public/test/render_view_test.cc
@@ -294,7 +294,7 @@ void RenderViewTest::TearDown() {
   scoped_ptr<blink::WebLeakDetector> leak_detector =
       make_scoped_ptr(blink::WebLeakDetector::create(this));
 
-  leak_detector->collectGarbageAndGetDOMCounts(GetMainFrame());
+  leak_detector->prepareForLeakDetection(GetMainFrame());
 
   view_ = NULL;
   mock_process_.reset();
@@ -308,6 +308,8 @@ void RenderViewTest::TearDown() {
   // Needs to run before blink::shutdown().
   autorelease_pool_.reset(NULL);
 #endif
+
+  leak_detector->collectGarbageAndReport();
 
   blink_platform_impl_.Shutdown();
   blink::shutdown();
