ce41a970a1d409c0e03804be0ac48f04ca04a241
blundell <blundell@chromium.org>
2015-11-25 02:50:15 -0800

    [iOS] Add API to //components/sessions/ios
    This CL adds a ToNavigationItems() API to IOSSerializedNavigationBuilder. This
    API is currently used only downstream, but will be used upstream in the future.
    
    Review URL: https://codereview.chromium.org/1471283004
    
    Cr-Commit-Position: refs/heads/master@{#361620}

--- components/sessions/ios/ios_serialized_navigation_builder.h
+++ components/sessions/ios/ios_serialized_navigation_builder.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/scoped_vector.h"
 
 namespace web {
 class NavigationItem;
@@ -31,6 +32,13 @@ class IOSSerializedNavigationBuilder {
   // PAGE_TRANSITION_RELOAD and a new unique ID.
   static scoped_ptr<web::NavigationItem> ToNavigationItem(
       const SerializedNavigationEntry* navigation);
+
+  // Converts a set of SerializedNavigationEntrys into a list of
+  // NavigationItems with sequential page IDs.
+  // TODO(crbug.com/561329): Change this API to return a
+  // std::vector<scoped_ptr> in coordination with changing downstream clients.
+  static ScopedVector<web::NavigationItem> ToNavigationItems(
+      const std::vector<SerializedNavigationEntry>& navigations);
 };
 
 }  // namespace sessions
