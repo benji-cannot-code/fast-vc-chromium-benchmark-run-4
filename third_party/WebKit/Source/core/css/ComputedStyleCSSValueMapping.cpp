840f0d720277b5998b0976e1badfd90ae02d537f
ericwilligers <ericwilligers@chromium.org>
2015-11-25 20:06:07 -0800

    CSS: Retire runtime flag CSSMotionPath
    CSS Motion Path has shipped and no longer needs to be controlled
    by a flag.
    
    BUG=496518
    
    Review URL: https://codereview.chromium.org/1481683002
    
    Cr-Commit-Position: refs/heads/master@{#361811}

--- third_party/WebKit/Source/core/css/ComputedStyleCSSValueMapping.cpp
+++ third_party/WebKit/Source/core/css/ComputedStyleCSSValueMapping.cpp
@@ -2420,11 +2420,9 @@ PassRefPtrWillBeRawPtr<CSSValue> ComputedStyleCSSValueMapping::get(CSSPropertyID
         return nullptr;
 
     case CSSPropertyMotion:
-        ASSERT(RuntimeEnabledFeatures::cssMotionPathEnabled());
         return valuesForShorthandProperty(motionShorthand(), style, layoutObject, styledNode, allowVisitedStyle);
 
     case CSSPropertyMotionPath: {
-        ASSERT(RuntimeEnabledFeatures::cssMotionPathEnabled());
         const StyleMotionPath* styleMotionPath = style.motionPath();
         if (!styleMotionPath)
             return cssValuePool().createIdentifierValue(CSSValueNone);
@@ -2434,11 +2432,9 @@ PassRefPtrWillBeRawPtr<CSSValue> ComputedStyleCSSValueMapping::get(CSSPropertyID
     }
 
     case CSSPropertyMotionOffset:
-        ASSERT(RuntimeEnabledFeatures::cssMotionPathEnabled());
         return zoomAdjustedPixelValueForLength(style.motionOffset(), style);
 
     case CSSPropertyMotionRotation: {
-        ASSERT(RuntimeEnabledFeatures::cssMotionPathEnabled());
         RefPtrWillBeRawPtr<CSSValueList> list = CSSValueList::createSpaceSeparated();
         if (style.motionRotationType() == MotionRotationAuto)
             list->append(cssValuePool().createIdentifierValue(CSSValueAuto));
