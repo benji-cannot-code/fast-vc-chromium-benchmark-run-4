4247373b2a135f7330446de2ddba018fef797d19
alph <alph@chromium.org>
2015-11-25 16:19:37 -0800

    Revert of DevTools: Support distance based drag start threshold. (patchset #4 id:60001 of https://codereview.chromium.org/1477563002/ )
    Reason for revert:
    Broke timeline overview window selection.
    
    Original issue's description:
    > DevTools: Support distance based drag start threshold.
    >
    > Committed: https://crrev.com/5636fefe1d743cc2a8af65f78eaeed4b98f0012b
    > Cr-Commit-Position: refs/heads/master@{#361609}
    
    TBR=pfeldman@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1478913002
    
    Cr-Commit-Position: refs/heads/master@{#361753}

--- third_party/WebKit/Source/devtools/front_end/ui_lazy/FlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/ui_lazy/FlameChart.js
@@ -75,8 +75,8 @@ WebInspector.FlameChart = function(dataProvider, flameChartDelegate, isTopDown)
     this._canvas.addEventListener("mousewheel", this._onMouseWheel.bind(this), false);
     this._canvas.addEventListener("click", this._onClick.bind(this), false);
     this._canvas.addEventListener("keydown", this._onKeyDown.bind(this), false);
-    WebInspector.installDragHandle(this._canvas, this._startCanvasDragging.bind(this), this._canvasDragging.bind(this), this._endCanvasDragging.bind(this), "-webkit-grabbing", null, 0, 5);
-    WebInspector.installDragHandle(this._canvas, this._startRangeSelection.bind(this), this._rangeSelectionDragging.bind(this), this._endRangeSelection.bind(this), "text", null, 0, 5);
+    WebInspector.installDragHandle(this._canvas, this._startCanvasDragging.bind(this), this._canvasDragging.bind(this), this._endCanvasDragging.bind(this), "-webkit-grabbing", null);
+    WebInspector.installDragHandle(this._canvas, this._startRangeSelection.bind(this), this._rangeSelectionDragging.bind(this), this._endRangeSelection.bind(this), "text", null);
 
     this._vScrollElement = this.contentElement.createChild("div", "flame-chart-v-scroll");
     this._vScrollContent = this._vScrollElement.createChild("div");
@@ -549,6 +549,35 @@ WebInspector.FlameChart.prototype = {
 
     /**
      * @param {!MouseEvent} event
+     */
+    _initMaxDragOffset: function(event)
+    {
+        this._maxDragOffsetSquared = 0;
+        this._dragStartX = event.pageX;
+        this._dragStartY = event.pageY;
+    },
+
+    /**
+     * @param {!MouseEvent} event
+     */
+    _updateMaxDragOffset: function(event)
+    {
+        var dx = event.pageX - this._dragStartX;
+        var dy = event.pageY - this._dragStartY;
+        var dragOffsetSquared = dx * dx + dy * dy;
+        this._maxDragOffsetSquared = Math.max(this._maxDragOffsetSquared, dragOffsetSquared);
+    },
+
+    /**
+     * @return {number}
+     */
+    _maxDragOffset: function()
+    {
+        return Math.sqrt(this._maxDragOffsetSquared);
+    },
+
+    /**
+     * @param {!MouseEvent} event
      * @return {boolean}
      */
     _startCanvasDragging: function(event)
@@ -558,6 +587,7 @@ WebInspector.FlameChart.prototype = {
         if (!this._timelineData() || this._timeWindowRight === Infinity)
             return false;
         this._isDragging = true;
+        this._initMaxDragOffset(event);
         this._dragStartPointX = event.pageX;
         this._dragStartPointY = event.pageY;
         this._dragStartScrollTop = this._vScrollElement.scrollTop;
@@ -581,6 +611,7 @@ WebInspector.FlameChart.prototype = {
 
         var pixelScroll = this._dragStartPointY - event.pageY;
         this._vScrollElement.scrollTop = this._dragStartScrollTop + pixelScroll;
+        this._updateMaxDragOffset(event);
     },
 
     _endCanvasDragging: function()
@@ -597,6 +628,7 @@ WebInspector.FlameChart.prototype = {
         if (!event.shiftKey)
             return false;
         this._isDragging = true;
+        this._initMaxDragOffset(event);
         this._selectionOffsetShiftX = event.offsetX - event.pageX;
         this._selectionOffsetShiftY = event.offsetY - event.pageY;
         this._selectionStartX = event.offsetX;
@@ -625,6 +657,7 @@ WebInspector.FlameChart.prototype = {
      */
     _rangeSelectionDragging: function(event)
     {
+        this._updateMaxDragOffset(event);
         var x = Number.constrain(event.pageX + this._selectionOffsetShiftX, 0, this._offsetWidth);
         var start = this._cursorTime(this._selectionStartX);
         var end = this._cursorTime(x);
@@ -714,6 +747,7 @@ WebInspector.FlameChart.prototype = {
     _hideHighlight: function()
     {
         this._entryInfo.removeChildren();
+        this._canvas.style.cursor = "default";
         this._highlightedEntryIndex = -1;
         this._updateElementPosition(this._highlightElement, this._highlightedEntryIndex);
     },
@@ -721,7 +755,11 @@ WebInspector.FlameChart.prototype = {
     _onClick: function()
     {
         this.focus();
-        if (this._isDragging)
+        // onClick comes after dragStart and dragEnd events.
+        // So if there was drag (mouse move) in the middle of that events
+        // we skip the click. Otherwise we jump to the sources.
+        const clickThreshold = 5;
+        if (this._maxDragOffset() > clickThreshold)
             return;
         this._hideRangeSelection();
         this.dispatchEventToListeners(WebInspector.FlameChart.Events.EntrySelected, this._highlightedEntryIndex);
