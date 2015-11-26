03c616fd0463772f47931216ca4d6a2af85bdf13
krasin <krasin@google.com>
2015-11-25 16:51:14 -0800

    Fix downloading of LLVM Gold plugin for the official Chrome.
    The build needs the plugin to make an LTO build, which will soon
    be the default for the official Chrome as a part of
    Control Flow Integrity launch. See
    https://sites.google.com/a/chromium.org/dev/developers/testing/control-flow-integrity
    for more details.
    
    BUG=chromium:464797
    
    Review URL: https://codereview.chromium.org/1475173002
    
    Cr-Commit-Position: refs/heads/master@{#361774}

--- tools/clang/scripts/update.sh
+++ tools/clang/scripts/update.sh
@@ -54,6 +54,8 @@ if [[ -z "$LLVM_DOWNLOAD_GOLD_PLUGIN" ]]; then
   LLVM_DOWNLOAD_GOLD_PLUGIN=
 fi
 
+OS="$(uname -s)"
+
 if [[ "${OS}" == "Linux" ]] && \
    [[ "$GYP_DEFINES" =~ .*buildtype=Official.* ]] && \
    [[ "$GYP_DEFINES" =~ .*branding=Chrome.* ]] ; then
@@ -72,8 +74,6 @@ if [[ -n ${LLVM_FORCE_HEAD_REVISION:-''} ]]; then
       | grep 'Revision:' | awk '{ printf $2; }')
   PACKAGE_VERSION="${CLANG_REVISION}-0"
 fi
-
-OS="$(uname -s)"
 
 # Parse command line options.
 if_needed=
