4fc16fb012ca2026ae64973f75d806ca0388a4cc
reveman <reveman@chromium.org>
2015-11-25 21:56:58 -0800

    exo: Make sure preferred size changes propagate to the widget.
    Changes to buffer size were reflected in preferred size and layer size
    but the widget and native window was never updated.
    
    BUG=549781
    TEST=exo_unittests --gtest_filter=ShellSurfaceTest.Show
    TBR=piman@chromium.org
    
    Review URL: https://codereview.chromium.org/1475323002
    
    Cr-Commit-Position: refs/heads/master@{#361828}

--- components/exo/surface_unittest.cc
+++ components/exo/surface_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/khronos/GLES2/gl2.h"
 #include "ui/gfx/gpu_memory_buffer.h"
-#include "ui/views/test/widget_test.h"
 
 namespace exo {
 namespace {
