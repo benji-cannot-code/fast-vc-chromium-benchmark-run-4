4923d0ba48969fc66bb19a2355f71f06f947b717
tkent <tkent@chromium.org>
2015-11-25 16:52:37 -0800

    Remove code related to Subversion in third_party/WebKit/Tools/Scripts/webkitpy/.
    SCMDetector returns Git or None after this CL.
    
    BUG=561148
    
    Review URL: https://codereview.chromium.org/1480513002
    
    Cr-Commit-Position: refs/heads/master@{#361776}

--- third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/scm/__init__.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/scm/__init__.py
@@ -5,4 +5,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from .detection import SCMDetector
 from .git import Git, AmbiguousCommitError
 from .scm import SCM
-from .svn import SVN
