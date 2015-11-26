580eda58b8e6e3f7a5980addf5e4bf737cf2bb56
dnicoara <dnicoara@chromium.org>
2015-11-26 13:46:42 -0800

    Fix undefined DesktopWindowTreeHost::GetNativeTheme for Ozone desktop
    Using the Aura native theme as it is the default or the fallback on most
    platforms.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1473973004
    
    Cr-Commit-Position: refs/heads/master@{#361929}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_ozone.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_ozone.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ui/aura/window_tree_host.h"
+#include "ui/native_theme/native_theme_aura.h"
 #include "ui/views/widget/desktop_aura/desktop_factory_ozone.h"
 #include "ui/views/widget/desktop_aura/desktop_window_tree_host.h"
 
@@ -16,6 +17,11 @@ DesktopWindowTreeHost* DesktopWindowTreeHost::Create(
 
   return d_factory->CreateWindowTreeHost(native_widget_delegate,
                                          desktop_native_widget_aura);
+}
+
+// static
+ui::NativeTheme* DesktopWindowTreeHost::GetNativeTheme(aura::Window* window) {
+  return ui::NativeThemeAura::instance();
 }
 
 }  // namespace views
