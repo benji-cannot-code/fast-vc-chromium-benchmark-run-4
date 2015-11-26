a7074cfde17cf1a85c63d890311a8b77cc238fa6
apacible <apacible@chromium.org>
2015-11-25 18:16:38 -0800

    [a11y] Bring accessibility-audit up to date: v2.10.0 release.
    This change pulls in the latest a11y devtools release.
    
    With the update of third_party/accessibility_audit to release v2.10.0, a number of browser tests had accessibility audit failures and warnings. These are all disabled/ignored and bugs filed for each set of audit failures.
    
    The accessibility audit repository can be found at: https://github.com/GoogleChrome/accessibility-developer-tools
    
    TBR=sky@chromium.org
    BUG=555753
    
    Review URL: https://codereview.chromium.org/1447693002
    
    Cr-Commit-Position: refs/heads/master@{#361792}

--- chrome/browser/ui/webui/options/multilanguage_options_webui_browsertest.js
+++ chrome/browser/ui/webui/options/multilanguage_options_webui_browsertest.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+GEN_INCLUDE(['options_browsertest_base.js']);
 GEN('#include "chrome/browser/ui/webui/options/' +
     'multilanguage_options_browsertest.h"');
 
@@ -14,7 +15,7 @@ GEN('#include "chrome/browser/ui/webui/options/' +
 function MultilanguageOptionsWebUIBrowserTest() {}
 
 MultilanguageOptionsWebUIBrowserTest.prototype = {
-  __proto__: testing.Test.prototype,
+  __proto__: OptionsBrowsertestBase.prototype,
 
   /** @override */
   browsePreload: 'chrome://settings-frame/languages',
@@ -33,7 +34,7 @@ MultilanguageOptionsWebUIBrowserTest.prototype = {
 
   /** @override */
   setUp: function() {
-    testing.Test.prototype.setUp.call(this);
+    OptionsBrowsertestBase.prototype.setUp.call(this);
 
     assertTrue(loadTimeData.getBoolean('enableMultilingualSpellChecker'));
     assertFalse(cr.isMac);
@@ -41,6 +42,29 @@ MultilanguageOptionsWebUIBrowserTest.prototype = {
     expectFalse($('edit-custom-dictionary-button').hidden);
     this.expectEnableSpellcheckCheckboxHidden();
     this.expectCurrentlySelected('fr');
+
+    var requiredOwnedAriaRoleMissingSelectors = [
+      '#default-search-engine-list',
+      '#other-search-engine-list',
+    ];
+
+    // Enable when failure is resolved.
+    // AX_ARIA_08: http://crbug.com/559320
+    this.accessibilityAuditConfig.ignoreSelectors(
+        'requiredOwnedAriaRoleMissing',
+        requiredOwnedAriaRoleMissingSelectors);
+
+    // Enable when failure is resolved.
+    // AX_ARIA_10: http://crbug.com/559266
+    this.accessibilityAuditConfig.ignoreSelectors(
+        'unsupportedAriaAttribute',
+        '#language-options-list');
+
+    // Enable when failure is resolved.
+    // AX_TEXT_04: http://crbug.com/559271
+    this.accessibilityAuditConfig.ignoreSelectors(
+        'linkWithUnclearPurpose',
+        '#languagePage > .content-area > .language-options-header > A');
   },
 
   /** @override */
@@ -141,7 +165,7 @@ MultilanguagePreferenceWebUIBrowserTest.prototype = {
 
   /** @override */
   setUp: function() {
-    testing.Test.prototype.setUp.call(this);
+    OptionsBrowsertestBase.prototype.setUp.call(this);
 
     assertTrue(loadTimeData.getBoolean('enableMultilingualSpellChecker'));
     assertFalse(cr.isMac);
@@ -150,6 +174,18 @@ MultilanguagePreferenceWebUIBrowserTest.prototype = {
     this.expectEnableSpellcheckCheckboxHidden();
     this.expectCurrentlySelected('');
     this.expectRegisteredDictionariesPref('');
+
+    // Enable when failure is resolved.
+    // AX_ARIA_10: http://crbug.com/559266
+    this.accessibilityAuditConfig.ignoreSelectors(
+        'unsupportedAriaAttribute',
+        '#language-options-list');
+
+    // Enable when failure is resolved.
+    // AX_TEXT_04: http://crbug.com/559271
+    this.accessibilityAuditConfig.ignoreSelectors(
+        'linkWithUnclearPurpose',
+        '#languagePage > .content-area > .language-options-header > A');
   },
 };
 
