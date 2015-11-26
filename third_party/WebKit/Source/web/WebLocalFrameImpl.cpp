563c07fe1a834f68543bebd61fe08fdeb45da338
creis <creis@chromium.org>
2015-11-25 18:43:55 -0800

    OOPIF: Use the same uniqueName when swapping between local and remote.
    When swapping from a RemoteFrame to a LocalFrame, we now preserve both
    the name and unique name.
    
    BUG=502317
    TEST=Unique name doesn't change when an OOPIF navigates into parent process.
    
    Review URL: https://codereview.chromium.org/1413093007
    
    Cr-Commit-Position: refs/heads/master@{#361805}

--- third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/web/WebLocalFrameImpl.cpp
@@ -119,6 +119,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/LocalDOMWindow.h"
 #include "core/frame/FrameHost.h"
 #include "core/frame/FrameView.h"
+#include "core/frame/RemoteFrame.h"
 #include "core/frame/Settings.h"
 #include "core/frame/UseCounter.h"
 #include "core/html/HTMLAnchorElement.h"
@@ -1992,9 +1993,13 @@ void WebLocalFrameImpl::initializeToReplaceRemoteFrame(WebRemoteFrame* oldWebFra
     OwnPtrWillBeRawPtr<FrameOwner> tempOwner = RemoteBridgeFrameOwner::create(nullptr, SandboxNone, WebFrameOwnerProperties());
     RefPtrWillBeRawPtr<LocalFrame> frame = LocalFrame::create(m_frameLoaderClientImpl.get(), oldFrame->host(), tempOwner.get());
     frame->setOwner(oldFrame->owner());
-    frame->tree().setName(name);
     setParent(oldWebFrame->parent());
     setOpener(oldWebFrame->opener());
+
+    // Set the name and unique name directly.
+    // TODO(creis): Remove |name| and use the oldWebFrame's name.
+    frame->tree().setNameForReplacementFrame(name, toWebRemoteFrameImpl(oldWebFrame)->frame()->tree().uniqueName());
+
     setCoreFrame(frame);
 
     if (frame->owner() && !frame->owner()->isLocal()) {
