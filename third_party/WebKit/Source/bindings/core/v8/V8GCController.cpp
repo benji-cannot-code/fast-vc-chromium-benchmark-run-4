563e8e762c248e04f468cf8dcdfaeb7a2dca2351
jochen <jochen@chromium.org>
2015-11-27 09:04:04 -0800

    Use non-deprecated methods to install GC callbacks
    R=ulan@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/1481023003
    
    Cr-Commit-Position: refs/heads/master@{#362016}

--- third_party/WebKit/Source/bindings/core/v8/V8GCController.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8GCController.cpp
@@ -280,7 +280,7 @@ void gcPrologueForMajorGC(v8::Isolate* isolate, bool constructRetainedObjectInfo
 
 }
 
-void V8GCController::gcPrologue(v8::GCType type, v8::GCCallbackFlags flags)
+void V8GCController::gcPrologue(v8::Isolate* isolate, v8::GCType type, v8::GCCallbackFlags flags)
 {
     if (isMainThread()) {
         ScriptForbiddenScope::enter();
@@ -293,9 +293,6 @@ void V8GCController::gcPrologue(v8::GCType type, v8::GCCallbackFlags flags)
     // if (ThreadState::current())
     //     ThreadState::current()->willStartV8GC();
 
-    // TODO(haraken): It would be nice if the GC callbacks passed the Isolate
-    // directly.
-    v8::Isolate* isolate = v8::Isolate::GetCurrent();
     v8::HandleScope scope(isolate);
     switch (type) {
     case v8::kGCTypeScavenge:
@@ -329,11 +326,8 @@ void V8GCController::gcPrologue(v8::GCType type, v8::GCCallbackFlags flags)
     }
 }
 
-void V8GCController::gcEpilogue(v8::GCType type, v8::GCCallbackFlags flags)
+void V8GCController::gcEpilogue(v8::Isolate* isolate, v8::GCType type, v8::GCCallbackFlags flags)
 {
-    // TODO(haraken): It would be nice if the GC callbacks passed the Isolate
-    // directly.
-    v8::Isolate* isolate = v8::Isolate::GetCurrent();
     switch (type) {
     case v8::kGCTypeScavenge:
         TRACE_EVENT_END1("devtools.timeline,v8", "MinorGC", "usedHeapSizeAfter", usedHeapSize(isolate));
