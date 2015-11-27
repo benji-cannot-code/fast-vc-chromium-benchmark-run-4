493fa1a58bf9ff66ca5d19d3cb2e520423400675
hartmanng <hartmanng@chromium.org>
2015-11-27 11:20:30 -0800

    Fix strict mode violation on document mode initialization.
    Loading shared prefs requires an async disk access. If it
    doesn't finish in time, it can cause the UI thread to block,
    violating strict mode. This patch pre-loads the document-mode
    shared prefs, as we already do for the main Chrome default
    shared prefs, to ensure that it's ready before we need it.
    
    BUG=543201
    
    Review URL: https://codereview.chromium.org/1464313010
    
    Cr-Commit-Position: refs/heads/master@{#362031}

--- chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
@@ -23,6 +23,7 @@ import org.chromium.chrome.browser.ChromeStrictMode;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.FileProviderHelper;
 import org.chromium.chrome.browser.device.DeviceClassManager;
+import org.chromium.chrome.browser.tabmodel.document.DocumentTabModelImpl;
 import org.chromium.content.app.ContentApplication;
 import org.chromium.content.browser.BrowserStartupController;
 import org.chromium.content.browser.DeviceUtils;
@@ -103,6 +104,15 @@ public class ChromeBrowserInitializer {
     }
 
 
+    /**
+     * Pre-load shared prefs to avoid being blocked on the
+     * disk access async task in the future.
+     */
+    private void warmUpSharedPrefs() {
+        PreferenceManager.getDefaultSharedPreferences(mApplication);
+        DocumentTabModelImpl.warmUpSharedPrefs(mApplication);
+    }
+
     private void preInflationStartup() {
         ThreadUtils.assertOnUiThread();
         if (mPreInflationStartupComplete) return;
@@ -114,8 +124,7 @@ public class ChromeBrowserInitializer {
         waitForDebuggerIfNeeded();
         ChromeStrictMode.configureStrictMode();
 
-        // Warm up the shared prefs stored.
-        PreferenceManager.getDefaultSharedPreferences(mApplication);
+        warmUpSharedPrefs();
 
         DeviceUtils.addDeviceSpecificUserAgentSwitch(mApplication);
 
