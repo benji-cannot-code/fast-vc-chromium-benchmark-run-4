da4817a5cbe1a5c5a24ee9fc83162a0ba16ff039
tasak <tasak@google.com>
2015-11-27 04:08:33 -0800

    Make PlatformEvent to use USING_FAST_MALLOC.
    BUG=562431
    
    Review URL: https://codereview.chromium.org/1479153002
    
    Cr-Commit-Position: refs/heads/master@{#361979}

--- third_party/WebKit/Source/platform/PlatformEvent.h
+++ third_party/WebKit/Source/platform/PlatformEvent.h
@@ -27,9 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PlatformEvent_h
 #define PlatformEvent_h
 
+#include "wtf/Allocator.h"
+
 namespace blink {
 
 class PlatformEvent {
+    USING_FAST_MALLOC(PlatformEvent);
 public:
     enum Type {
         NoType = 0,
