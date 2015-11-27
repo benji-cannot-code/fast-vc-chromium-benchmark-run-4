2c2daa99c3cbae2a305e515e76a9c0b635db3ef7
yosin <yosin@chromium.org>
2015-11-26 16:28:23 -0800

    Make selection not to accept Range not in document
    This patch changes selection not to accept Range not in document in
      - DOMSelection::addRange(); JavaScript API Selection.addRange
      - FrameSelection::setSelectedRange()
    by introducing Range::inDocument() function. This patch also introduces
    an assertion in |EphemeralRange| constructor with |Range| parameter not to
    construct |EphemeralRange| with |Range| not in Document for |EphemeralRange|
    version of |FrameSelection::setSelectedRange()|.
    
    Note: Changes of addRange-failures.html is inspired by sigbjornf@opera.com's
    work in http://crrev.com/1469273004/
    
    BUG=560748
    TEST=LayoutTests/editing/selection/addRange-failures.html
    
    Review URL: https://codereview.chromium.org/1478063002
    
    Cr-Commit-Position: refs/heads/master@{#361936}

--- third_party/WebKit/Source/core/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/core/editing/FrameSelection.cpp
@@ -852,7 +852,7 @@ void FrameSelection::selectAll()
 
 bool FrameSelection::setSelectedRange(Range* range, TextAffinity affinity, SelectionDirectionalMode directional, SetSelectionOptions options)
 {
-    if (!range || !range->startContainer() || !range->endContainer())
+    if (!range || !range->inDocument())
         return false;
     ASSERT(range->startContainer()->document() == range->endContainer()->document());
     return setSelectedRange(EphemeralRange(range), affinity, directional, options);
