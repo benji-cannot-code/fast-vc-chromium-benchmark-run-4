0aa155db3a67d1147aaa4fbbe554339ee960b3fa
sadrul <sadrul@chromium.org>
2015-11-30 15:30:48 -0800

    mash: Close a window if the app crashes.
    BUG=none
    
    Review URL: https://codereview.chromium.org/1487453002
    
    Cr-Commit-Position: refs/heads/master@{#362268}

--- mash/wm/window_manager_application.cc
+++ mash/wm/window_manager_application.cc
@@ -55,7 +55,7 @@ void WindowManagerApplication::AddAccelerators() {
 void WindowManagerApplication::Initialize(mojo::ApplicationImpl* app) {
   app_ = app;
   tracing_.Initialize(app);
-  window_manager_.reset(new WindowManagerImpl(this));
+  window_manager_.reset(new WindowManagerImpl());
   // Don't bind to the WindowManager immediately. Wait for OnEmbed() first.
   mus::mojom::WindowManagerPtr window_manager;
   requests_.push_back(new mojo::InterfaceRequest<mus::mojom::WindowManager>(
@@ -102,6 +102,7 @@ void WindowManagerApplication::OnEmbed(mus::Window* root) {
 
   ui_init_.reset(new ui::mojo::UIInit(views::GetDisplaysFromWindow(root)));
   aura_init_.reset(new views::AuraInit(app_, "mash_wm_resources.pak"));
+  window_manager_->Initialize(this);
 
   for (auto request : requests_)
     window_manager_binding_.AddBinding(window_manager_.get(), request->Pass());
