61661c3d068153ff414bfe2bc2b8a4dfb37169e3
haibinlu <haibinlu@chromium.org>
2015-11-30 11:29:31 -0800

    [Blimp Net] Adds BrowserConnectionHandler.
    It Contains a demultiplexer for routing incoming messages to their respective
    features, and a multiplexer/buffer object for writing outgoing messages.
    
    It is created on browser startup, and persists for the lifetime of the application.
    
    BUG=561207
    
    Review URL: https://codereview.chromium.org/1474583002
    
    Cr-Commit-Position: refs/heads/master@{#362192}

--- blimp/net/blimp_message_pump.cc
+++ blimp/net/blimp_message_pump.cc
@@ -27,10 +27,11 @@ BlimpMessagePump::BlimpMessagePump(PacketReader* reader)
 BlimpMessagePump::~BlimpMessagePump() {}
 
 void BlimpMessagePump::SetMessageProcessor(BlimpMessageProcessor* processor) {
-  DCHECK(processor);
-  bool reading_packet = !!processor_;
+  process_msg_callback_.Cancel();
   processor_ = processor;
-  if (!reading_packet) {
+  if (!processor_) {
+    read_packet_callback_.Cancel();
+  } else if (read_packet_callback_.IsCancelled()) {
     buffer_->SetCapacity(kMaxPacketPayloadSizeBytes);
     ReadNextPacket();
   }
@@ -50,6 +51,7 @@ void BlimpMessagePump::ReadNextPacket() {
 }
 
 void BlimpMessagePump::OnReadPacketComplete(int result) {
+  read_packet_callback_.Cancel();
   if (result > 0) {
     // The result is the size of the packet in bytes.
     scoped_ptr<BlimpMessage> message(new BlimpMessage);
@@ -71,6 +73,7 @@ void BlimpMessagePump::OnReadPacketComplete(int result) {
 void BlimpMessagePump::OnProcessMessageComplete(int result) {
   // No error is expected from the message receiver.
   DCHECK_EQ(result, net::OK);
+  process_msg_callback_.Cancel();
   ReadNextPacket();
 }
 
