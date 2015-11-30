5efb3c7fc1bfcef050564be47199e91022d7cf50
lushnikov <lushnikov@chromium.org>
2015-11-30 14:15:17 -0800

    DevTools: [SSP] auto-focus "Add New Class" input field
    The patch auto-focuses the "add new class" field as the pane gets shown.
    
    BUG=557638
    R=pfeldman, dgozman
    
    Review URL: https://codereview.chromium.org/1457103004
    
    Cr-Commit-Position: refs/heads/master@{#362238}

--- third_party/WebKit/Source/devtools/front_end/elements/ElementsPanel.js
+++ third_party/WebKit/Source/devtools/front_end/elements/ElementsPanel.js
@@ -126,6 +126,7 @@ WebInspector.ElementsPanel.prototype = {
             this._currentToolbarPane.detach();
             widget.show(this._toolbarPaneElement);
             this._currentToolbarPane = widget;
+            this._currentToolbarPane.focus();
             return;
         }
 
@@ -154,6 +155,8 @@ WebInspector.ElementsPanel.prototype = {
                 this._currentToolbarPane.detach();
 
             this._currentToolbarPane = this._animatedToolbarPane;
+            if (this._currentToolbarPane)
+                this._currentToolbarPane.focus();
             delete this._animatedToolbarPane;
 
             if (this._pendingWidget !== undefined) {
