86c1d54b3f7ef8193ff8ba0355743439f522a7f0
reillyg <reillyg@chromium.org>
2015-11-30 11:37:45 -0800

    Add a GetAllGrantedObjects method to ChooserContextBase.
    This is a wrapper around HostContentSettingsMap::GetSettingsForOneType
    that will return all the objects of a particular type thta the user has
    granted sites permissions to access.
    
    The existing GetGrantedObjects method is updated to match the new method
    by take advantaging of support for scoped_ptrs in std::vector and unit
    tests have been added for the whole thing.
    
    BUG=529950
    
    Review URL: https://codereview.chromium.org/1434113002
    
    Cr-Commit-Position: refs/heads/master@{#362194}

--- chrome/browser/usb/usb_chooser_context.cc
+++ chrome/browser/usb/usb_chooser_context.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/usb/usb_chooser_context.h"
 
+#include <vector>
+
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
@@ -25,12 +27,12 @@ bool CanStorePersistentEntry(const scoped_refptr<const UsbDevice>& device) {
 }
 
 const base::DictionaryValue* FindForDevice(
-    const ScopedVector<base::DictionaryValue>& device_list,
+    const std::vector<scoped_ptr<base::DictionaryValue>>& device_list,
     const scoped_refptr<const UsbDevice>& device) {
   const std::string utf8_serial_number =
       base::UTF16ToUTF8(device->serial_number());
 
-  for (const base::DictionaryValue* device_dict : device_list) {
+  for (const scoped_ptr<base::DictionaryValue>& device_dict : device_list) {
     int vendor_id;
     int product_id;
     std::string serial_number;
@@ -40,7 +42,7 @@ const base::DictionaryValue* FindForDevice(
         device->product_id() == product_id &&
         device_dict->GetString(kSerialNumberKey, &serial_number) &&
         utf8_serial_number == serial_number) {
-      return device_dict;
+      return device_dict.get();
     }
   }
   return nullptr;
@@ -93,7 +95,7 @@ void UsbChooserContext::RevokeDevicePermission(const GURL& requesting_origin,
   if (!device)
     return;
 
-  ScopedVector<base::DictionaryValue> device_list =
+  std::vector<scoped_ptr<base::DictionaryValue>> device_list =
       GetGrantedObjects(requesting_origin, embedding_origin);
   const base::DictionaryValue* entry = FindForDevice(device_list, device);
   if (entry)
@@ -111,7 +113,7 @@ bool UsbChooserContext::HasDevicePermission(const GURL& requesting_origin,
   if (!device)
     return false;
 
-  ScopedVector<base::DictionaryValue> device_list =
+  std::vector<scoped_ptr<base::DictionaryValue>> device_list =
       GetGrantedObjects(requesting_origin, embedding_origin);
   return FindForDevice(device_list, device) != nullptr;
 }
--- chrome/chrome_tests_unit.gypi
+++ chrome/chrome_tests_unit.gypi
@@ -167,6 +167,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/password_manager/password_store_win_unittest.cc',
       'browser/password_manager/password_store_x_unittest.cc',
       'browser/password_manager/simple_password_store_mac_unittest.cc',
+      'browser/permissions/chooser_context_base_unittest.cc',
       'browser/permissions/permission_context_base_unittest.cc',
       'browser/permissions/permission_manager_unittest.cc',
       'browser/predictors/autocomplete_action_predictor_table_unittest.cc',
