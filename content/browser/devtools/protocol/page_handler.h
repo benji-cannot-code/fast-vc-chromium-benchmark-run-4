0d75d37640276537c0118425bd636333fe8616ed
sergeyv <sergeyv@chromium.org>
2015-11-30 14:41:35 -0800

    Devtools: remove Page.canScreencast command
    BUG=
    
    Review URL: https://codereview.chromium.org/1479733002
    
    Cr-Commit-Position: refs/heads/master@{#362249}

--- content/browser/devtools/protocol/page_handler.h
+++ content/browser/devtools/protocol/page_handler.h
@@ -62,7 +62,6 @@ class PageHandler : public NotificationObserver {
 
   Response CaptureScreenshot(DevToolsCommandId command_id);
 
-  Response CanScreencast(bool* result);
   Response StartScreencast(const std::string* format,
                            const int* quality,
                            const int* max_width,
