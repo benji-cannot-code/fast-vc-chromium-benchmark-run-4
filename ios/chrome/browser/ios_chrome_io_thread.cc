75eed796f94c0ea52669d7fe5f16c859f16da492
sdefresne <sdefresne@chromium.org>
2015-11-30 08:40:56 -0800

    Remove "negotiate" from support auth scheme on iOS.
    The support for "negotiate" is disabled on iOS when building //net,
    so remove the scheme from the list of supported scheme (lying is a
    terrible sin).
    
    BUG=556566
    
    Review URL: https://codereview.chromium.org/1489513002
    
    Cr-Commit-Position: refs/heads/master@{#362142}

--- ios/chrome/browser/ios_chrome_io_thread.cc
+++ ios/chrome/browser/ios_chrome_io_thread.cc
@@ -88,7 +88,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const char kSupportedAuthSchemes[] = "basic,digest,ntlm,negotiate";
+const char kSupportedAuthSchemes[] = "basic,digest,ntlm";
 
 const char kTCPFastOpenFieldTrialName[] = "TCPFastOpen";
 const char kTCPFastOpenHttpsEnabledGroupName[] = "HttpsEnabled";
