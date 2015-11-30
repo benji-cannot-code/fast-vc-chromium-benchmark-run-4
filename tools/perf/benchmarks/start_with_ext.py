6808ffa54e03e97994a911060fe4dc0f0ac314e3
petrcermak <petrcermak@chromium.org>
2015-11-30 07:19:41 -0800

    Disable start_with_ext.{cold,warm}.blank_page on Mac
    The benchmarks keep failing on Mac perf bots.
    
    TBR=pasko@chromium.org,erikchen@chromium.org
    BUG=563424
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_10_10_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1480253003
    
    Cr-Commit-Position: refs/heads/master@{#362133}

--- tools/perf/benchmarks/start_with_ext.py
+++ tools/perf/benchmarks/start_with_ext.py
@@ -31,7 +31,7 @@ class _StartWithExt(perf_benchmark.PerfBenchmark):
 
 
 @benchmark.Enabled('has tabs')
-@benchmark.Enabled('mac') # Currently only works on mac.
+@benchmark.Disabled('mac')  # crbug.com/563424
 @benchmark.Disabled('win', 'linux', 'reference', 'android')
 class StartWithExtCold(_StartWithExt):
   """Measure time to start Chrome cold with extensions."""
@@ -44,7 +44,7 @@ class StartWithExtCold(_StartWithExt):
 
 
 @benchmark.Enabled('has tabs')
-@benchmark.Enabled('mac') # Currently only works on mac.
+@benchmark.Disabled('mac')  # crbug.com/563424
 @benchmark.Disabled('win', 'linux', 'reference', 'android')
 class StartWithExtWarm(_StartWithExt):
   """Measure time to start Chrome warm with extensions."""
