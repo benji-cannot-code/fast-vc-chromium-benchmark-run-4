7c34446ce573a2897c395bbaafa7036e14b9c3fc
peter <peter@chromium.org>
2015-11-30 07:33:52 -0800

    Add URLSearchParams to the WebView interface expectations
    Also add a missing annotation to testBatteryApi that was creating warnings.
    
    TBR=torne
    BUG=563472, 506603
    
    Review URL: https://codereview.chromium.org/1483043002
    
    Cr-Commit-Position: refs/heads/master@{#362134}

--- android_webview/tools/WebViewShellTest/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
+++ android_webview/tools/WebViewShellTest/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
@@ -192,6 +192,7 @@ public class WebViewLayoutTest
         mTestActivity.setGrantPermission(false);
     }
 
+    @MediumTest
     public void testBatteryApi() throws Exception {
         runWebViewLayoutTest("blink-apis/battery-status/battery-callback.html",
                 "blink-apis/battery-status/battery-callback-expected.txt");
