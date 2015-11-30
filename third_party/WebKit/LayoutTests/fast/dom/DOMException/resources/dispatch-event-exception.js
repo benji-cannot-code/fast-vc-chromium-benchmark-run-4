27a9d3b2cafda9c6172301f3237b1c4c735808c4
philipj <philipj@opera.com>
2015-11-30 09:38:11 -0800

    Drop [LegacyInterfaceTypeChecking] for EventTarget's dispatchEvent()
    This changes the exception for InvalidStateError to TypeError, which is
    per spec and what Gecko does. IE and Safari throw a generic Error.
    
    BUG=561338
    
    Review URL: https://codereview.chromium.org/1479063003
    
    Cr-Commit-Position: refs/heads/master@{#362153}

--- third_party/WebKit/LayoutTests/fast/dom/DOMException/resources/dispatch-event-exception.js
+++ third_party/WebKit/LayoutTests/fast/dom/DOMException/resources/dispatch-event-exception.js
@@ -1,17 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-description("Tests the properties of the exception thrown by dispatchEvent.")
+description("Tests that dispatchEvent's argument is required to be an Event.")
 
-var e;
-try {
-    document.dispatchEvent(null);
-    // raises a InvalidStateError
-} catch (err) {
-    e = err;
-}
-
-shouldBeEqualToString("e.toString()", "InvalidStateError: Failed to execute 'dispatchEvent' on 'EventTarget': The event provided is null.");
-shouldBeEqualToString("Object.prototype.toString.call(e)", "[object DOMException]");
-shouldBeEqualToString("Object.prototype.toString.call(e.__proto__)", "[object DOMExceptionPrototype]");
-shouldBeEqualToString("e.constructor.toString()", "function DOMException() { [native code] }");
-shouldBe("e.constructor", "window.DOMException");
-
+shouldThrow("document.dispatchEvent(null)");
+shouldThrow("document.dispatchEvent(document)");
