76d69c3ddfa488e803b5f5e7b98b85ef0e074428
estark <estark@chromium.org>
2015-12-01 14:57:10 -0800

    Change cookie prefix from $ to __
    Changes the Secure/Host cookie prefix from $Secure/$Host to
    __Secure/__Host for improved compatibility.
    
    Design doc: https://twitter.com/tbroyer/status/670779401135616000
    
    BUG=541511
    
    Review URL: https://codereview.chromium.org/1485763003
    
    Cr-Commit-Position: refs/heads/master@{#362540}

--- net/url_request/url_request_unittest.cc
+++ net/url_request/url_request_unittest.cc
@@ -2727,7 +2727,7 @@ TEST_F(URLRequestTest, FirstPartyOnlyCookiesDisabled) {
   }
 }
 
-// Tests that $Secure- cookies can't be set on non-secure origins.
+// Tests that __Secure- cookies can't be set on non-secure origins.
 TEST_F(URLRequestTest, SecureCookiePrefixOnNonsecureOrigin) {
   EmbeddedTestServer http_server;
   http_server.AddDefaultHandlers(
@@ -2743,12 +2743,12 @@ TEST_F(URLRequestTest, SecureCookiePrefixOnNonsecureOrigin) {
   context.set_network_delegate(&network_delegate);
   context.Init();
 
-  // Try to set a Secure $Secure- cookie, with experimental features
+  // Try to set a Secure __Secure- cookie, with experimental features
   // enabled.
   {
     TestDelegate d;
     scoped_ptr<URLRequest> req(context.CreateRequest(
-        http_server.GetURL("/set-cookie?$Secure-nonsecure-origin=1;Secure"),
+        http_server.GetURL("/set-cookie?__Secure-nonsecure-origin=1;Secure"),
         DEFAULT_PRIORITY, &d));
     req->Start();
     base::RunLoop().Run();
@@ -2764,7 +2764,7 @@ TEST_F(URLRequestTest, SecureCookiePrefixOnNonsecureOrigin) {
     req->Start();
     base::RunLoop().Run();
 
-    EXPECT_TRUE(d.data_received().find("$Secure-nonsecure-origin=1") ==
+    EXPECT_TRUE(d.data_received().find("__Secure-nonsecure-origin=1") ==
                 std::string::npos);
     EXPECT_EQ(0, network_delegate.blocked_get_cookies_count());
     EXPECT_EQ(0, network_delegate.blocked_set_cookie_count());
@@ -2783,13 +2783,14 @@ TEST_F(URLRequestTest, SecureCookiePrefixNonexperimental) {
   context.Init();
 
   // Without experimental features, there should be no restrictions on
-  // $Secure- cookies.
+  // __Secure- cookies.
 
-  // Set a non-Secure cookie with the $Secure- prefix.
+  // Set a non-Secure cookie with the __Secure- prefix.
   {
     TestDelegate d;
     scoped_ptr<URLRequest> req(context.CreateRequest(
-        https_server.GetURL("/set-cookie?$Secure-nonsecure-not-experimental=1"),
+        https_server.GetURL(
+            "/set-cookie?__Secure-nonsecure-not-experimental=1"),
         DEFAULT_PRIORITY, &d));
     req->Start();
     base::RunLoop().Run();
@@ -2797,12 +2798,12 @@ TEST_F(URLRequestTest, SecureCookiePrefixNonexperimental) {
     EXPECT_EQ(0, network_delegate.blocked_set_cookie_count());
   }
 
-  // Set a Secure cookie with the $Secure- prefix.
+  // Set a Secure cookie with the __Secure- prefix.
   {
     TestDelegate d;
     scoped_ptr<URLRequest> req(context.CreateRequest(
         https_server.GetURL(
-            "/set-cookie?$Secure-secure-not-experimental=1;Secure"),
+            "/set-cookie?__Secure-secure-not-experimental=1;Secure"),
         DEFAULT_PRIORITY, &d));
     req->Start();
     base::RunLoop().Run();
@@ -2819,10 +2820,10 @@ TEST_F(URLRequestTest, SecureCookiePrefixNonexperimental) {
     req->Start();
     base::RunLoop().Run();
 
-    EXPECT_TRUE(d.data_received().find("$Secure-secure-not-experimental=1") !=
+    EXPECT_TRUE(d.data_received().find("__Secure-secure-not-experimental=1") !=
                 std::string::npos);
     EXPECT_TRUE(
-        d.data_received().find("$Secure-nonsecure-not-experimental=1") !=
+        d.data_received().find("__Secure-nonsecure-not-experimental=1") !=
         std::string::npos);
     EXPECT_EQ(0, network_delegate.blocked_get_cookies_count());
     EXPECT_EQ(0, network_delegate.blocked_set_cookie_count());
@@ -2840,12 +2841,12 @@ TEST_F(URLRequestTest, SecureCookiePrefixExperimentalNonsecure) {
   context.set_network_delegate(&network_delegate);
   context.Init();
 
-  // Try to set a non-Secure $Secure- cookie, with experimental features
+  // Try to set a non-Secure __Secure- cookie, with experimental features
   // enabled.
   {
     TestDelegate d;
     scoped_ptr<URLRequest> req(
-        context.CreateRequest(https_server.GetURL("/set-cookie?$Secure-foo=1"),
+        context.CreateRequest(https_server.GetURL("/set-cookie?__Secure-foo=1"),
                               DEFAULT_PRIORITY, &d));
     req->Start();
     base::RunLoop().Run();
@@ -2861,7 +2862,7 @@ TEST_F(URLRequestTest, SecureCookiePrefixExperimentalNonsecure) {
     req->Start();
     base::RunLoop().Run();
 
-    EXPECT_TRUE(d.data_received().find("$Secure-foo=1") == std::string::npos);
+    EXPECT_TRUE(d.data_received().find("__Secure-foo=1") == std::string::npos);
     EXPECT_EQ(0, network_delegate.blocked_get_cookies_count());
     EXPECT_EQ(0, network_delegate.blocked_set_cookie_count());
   }
@@ -2878,12 +2879,12 @@ TEST_F(URLRequestTest, SecureCookiePrefixExperimentalSecure) {
   context.set_network_delegate(&network_delegate);
   context.Init();
 
-  // Try to set a Secure $Secure- cookie, with experimental features
+  // Try to set a Secure __Secure- cookie, with experimental features
   // enabled.
   {
     TestDelegate d;
     scoped_ptr<URLRequest> req(context.CreateRequest(
-        https_server.GetURL("/set-cookie?$Secure-bar=1;Secure"),
+        https_server.GetURL("/set-cookie?__Secure-bar=1;Secure"),
         DEFAULT_PRIORITY, &d));
     req->Start();
     base::RunLoop().Run();
@@ -2899,7 +2900,7 @@ TEST_F(URLRequestTest, SecureCookiePrefixExperimentalSecure) {
     req->Start();
     base::RunLoop().Run();
 
-    EXPECT_TRUE(d.data_received().find("$Secure-bar=1") != std::string::npos);
+    EXPECT_TRUE(d.data_received().find("__Secure-bar=1") != std::string::npos);
     EXPECT_EQ(0, network_delegate.blocked_get_cookies_count());
     EXPECT_EQ(0, network_delegate.blocked_set_cookie_count());
   }
