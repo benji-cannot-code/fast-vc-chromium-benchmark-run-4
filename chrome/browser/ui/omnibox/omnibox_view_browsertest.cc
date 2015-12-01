934a734cce95b178bf95d43d0cc83c5f3f2a7e7a
mpearson <mpearson@chromium.org>
2015-12-01 14:20:10 -0800

    Make Entering Keyword Mode by Space + Backspace Keep the Space
    Type google.com
    Press space.
    Realize you entered keyword mode accidentally.  You were going to search
    for something about google.
    Press backspace to leave keyword mode.
    "google.com " should remain the omnibox so you can continue typing a
    search query.
    
    BUG=393371
    
    Review URL: https://codereview.chromium.org/1429953007
    
    Cr-Commit-Position: refs/heads/master@{#362528}

--- chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
+++ chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
@@ -566,7 +566,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, MAYBE_BackspaceInKeywordMode) {
   ASSERT_NO_FATAL_FAILURE(SendKey(ui::VKEY_BACK, 0));
   ASSERT_FALSE(omnibox_view->model()->is_keyword_hint());
   ASSERT_EQ(base::string16(), omnibox_view->model()->keyword());
-  ASSERT_EQ(std::string(kSearchKeyword) + kSearchText,
+  ASSERT_EQ(std::string(kSearchKeyword) + ' ' + kSearchText,
             UTF16ToUTF8(omnibox_view->GetText()));
 }
 
@@ -899,13 +899,13 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, AcceptKeywordBySpace) {
   omnibox_view->model()->ClearKeyword();
   ASSERT_TRUE(omnibox_view->model()->is_keyword_hint());
   ASSERT_EQ(search_keyword, omnibox_view->model()->keyword());
-  ASSERT_EQ(search_keyword, omnibox_view->GetText());
+  ASSERT_EQ(search_keyword + base::char16(' '), omnibox_view->GetText());
 
   // Keyword should also be accepted by typing an ideographic space.
   omnibox_view->OnBeforePossibleChange();
   omnibox_view->SetWindowTextAndCaretPos(search_keyword +
       base::WideToUTF16(L"\x3000"), search_keyword.length() + 1, false, false);
-  omnibox_view->OnAfterPossibleChange();
+  omnibox_view->OnAfterPossibleChange(true);
   ASSERT_FALSE(omnibox_view->model()->is_keyword_hint());
   ASSERT_EQ(search_keyword, omnibox_view->model()->keyword());
   ASSERT_TRUE(omnibox_view->GetText().empty());
@@ -914,7 +914,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, AcceptKeywordBySpace) {
   omnibox_view->model()->ClearKeyword();
   ASSERT_TRUE(omnibox_view->model()->is_keyword_hint());
   ASSERT_EQ(search_keyword, omnibox_view->model()->keyword());
-  ASSERT_EQ(search_keyword, omnibox_view->GetText());
+  ASSERT_EQ(search_keyword + base::char16(' '), omnibox_view->GetText());
 
   // Keyword shouldn't be accepted by pressing space with a trailing
   // whitespace.
@@ -961,7 +961,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, AcceptKeywordBySpace) {
   omnibox_view->model()->OnPaste();
   omnibox_view->SetWindowTextAndCaretPos(search_keyword +
       ASCIIToUTF16(" bar"), search_keyword.length() + 4, false, false);
-  omnibox_view->OnAfterPossibleChange();
+  omnibox_view->OnAfterPossibleChange(true);
   ASSERT_FALSE(omnibox_view->model()->is_keyword_hint());
   ASSERT_TRUE(omnibox_view->model()->keyword().empty());
   ASSERT_EQ(search_keyword + ASCIIToUTF16(" bar"), omnibox_view->GetText());
@@ -979,7 +979,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, AcceptKeywordBySpace) {
   omnibox_view->OnBeforePossibleChange();
   omnibox_view->OnInlineAutocompleteTextMaybeChanged(
       search_keyword + ASCIIToUTF16("  "), search_keyword.length());
-  omnibox_view->OnAfterPossibleChange();
+  omnibox_view->OnAfterPossibleChange(true);
   ASSERT_TRUE(omnibox_view->model()->is_keyword_hint());
   ASSERT_EQ(search_keyword, omnibox_view->model()->keyword());
   ASSERT_EQ(search_keyword + ASCIIToUTF16("  "), omnibox_view->GetText());
@@ -1769,7 +1769,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, BeginningShownAfterBlur) {
   omnibox_view->OnBeforePossibleChange();
   omnibox_view->SetWindowTextAndCaretPos(ASCIIToUTF16("data:text/plain,test"),
       5U, false, false);
-  omnibox_view->OnAfterPossibleChange();
+  omnibox_view->OnAfterPossibleChange(true);
   EXPECT_TRUE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
   size_t start, end;
   omnibox_view->GetSelectionBounds(&start, &end);
