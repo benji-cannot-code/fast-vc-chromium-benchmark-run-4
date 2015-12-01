b4d3eb67c46dc94e8bfc60eb13263afaca55cdaf
mpearson <mpearson@chromium.org>
2015-12-01 02:51:46 -0800

    iOS Omnibox: Add Missing DependsOn Call
    When I submitted
    https://codereview.chromium.org/1477563003/
    with TBR=droger
    "for trivial API change that affects
    ios/chrome/browser/autocomplete/in_memory_url_index_factory.cc
    "
    
    I made a mistake.  Dang.
    
    ios/chrome/browser/autocomplete/in_memory_url_index_factory.cc
    is analogous to
    chrome/browser/autocomplete/in_memory_url_index_factory.cc
    
    Comparing the changes I made to the latter, I forgot a DependsOn()
    call in the former.  This change fixes that problem.
    
    This reminds me that I shouldn't use TBR for changes that I think
    are trivial.  They might not be, and it's possible there's something
    missing or wrong.
    
    BUG=180113
    
    Review URL: https://codereview.chromium.org/1489643002
    
    Cr-Commit-Position: refs/heads/master@{#362380}

--- ios/chrome/browser/autocomplete/in_memory_url_index_factory.cc
+++ ios/chrome/browser/autocomplete/in_memory_url_index_factory.cc
@@ -63,6 +63,7 @@ InMemoryURLIndexFactory::InMemoryURLIndexFactory()
           BrowserStateDependencyManager::GetInstance()) {
   DependsOn(ios::BookmarkModelFactory::GetInstance());
   DependsOn(ios::HistoryServiceFactory::GetInstance());
+  DependsOn(ios::TemplateURLServiceFactory::GetInstance());
 }
 
 InMemoryURLIndexFactory::~InMemoryURLIndexFactory() {}
