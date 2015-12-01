31d561682dbd7d15931b1317b3621d5eae6c3342
reillyg <reillyg@chromium.org>
2015-11-30 16:42:15 -0800

    Add USB devices to the content settings page.
    This patch adds infrastructure for displaying chooser-style permissions
    such as USB devices selected by the user on the content settings page.
    These permissions are used by the WebUSB API. This infrastructure will
    be used for per-device WebBluetooth permissions as well.
    
    BUG=424667,529950
    
    Review URL: https://codereview.chromium.org/1447813002
    
    Cr-Commit-Position: refs/heads/master@{#362290}

--- chrome/browser/ui/webui/options/content_settings_handler.h
+++ chrome/browser/ui/webui/options/content_settings_handler.h
@@ -32,6 +32,8 @@ class ContentSettingsHandler : public OptionsPageUIHandler,
                                public content::NotificationObserver,
                                public PepperFlashSettingsManager::Client {
  public:
+  struct ChooserTypeNameEntry;
+
   ContentSettingsHandler();
   ~ContentSettingsHandler() override;
 
@@ -150,6 +152,14 @@ class ContentSettingsHandler : public OptionsPageUIHandler,
   // Clobbers and rebuilds just the MIDI SysEx exception table.
   void UpdateMIDISysExExceptionsView();
 
+  // Clobbers and rebuilds all chooser-based exception tables.
+  void UpdateAllChooserExceptionsViewsFromModel();
+
+  // Clobbers and rebuilds the exception table for a particular chooser-based
+  // permission.
+  void UpdateChooserExceptionsViewFromModel(
+      const ChooserTypeNameEntry& chooser_type);
+
   // Modifies the zoom level exceptions list to display correct chrome
   // signin page entry. When the legacy (non-WebView-based) signin page
   // goes away, this function can be removed.
@@ -187,6 +197,11 @@ class ContentSettingsHandler : public OptionsPageUIHandler,
   // Removes one zoom level exception. |args| contains the parameters passed to
   // RemoveException().
   void RemoveZoomLevelException(const base::ListValue* args);
+
+  // Removes one exception for a chooser-based permission. |args| contains the
+  // parameters passed to RemoveException().
+  void RemoveChooserException(const ChooserTypeNameEntry* chooser_type,
+                              const base::ListValue* args);
 
   // Callbacks used by the page ------------------------------------------------
 
@@ -228,7 +243,9 @@ class ContentSettingsHandler : public OptionsPageUIHandler,
 
   // Returns exceptions constructed from the policy-set allowed URLs
   // for the content settings |type| mic or camera.
-  scoped_ptr<base::ListValue> GetPolicyAllowedUrls(ContentSettingsType type);
+  void GetPolicyAllowedUrls(
+      ContentSettingsType type,
+      std::vector<scoped_ptr<base::DictionaryValue>>* exceptions);
 
   // Fills in |exceptions| with Values for the given |type| from |map|.
   void GetExceptionsFromHostContentSettingsMap(
