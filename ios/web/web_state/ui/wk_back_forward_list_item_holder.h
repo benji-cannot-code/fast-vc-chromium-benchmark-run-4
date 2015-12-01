4501a664cd501b190aaf784ce2c3edf5f28222ff
stuartmorgan <stuartmorgan@chromium.org>
2015-11-30 17:09:56 -0800

    Preserve and restore MIME type for WKWebView fast back/forward
    Since the MIME type is only available the response callback that's not
    called for fast back/forward, store it with the back/forward list item
    and use that value when doing a back-forward.
    
    BUG=548863
    
    Review URL: https://codereview.chromium.org/1488613006
    
    Cr-Commit-Position: refs/heads/master@{#362302}

--- ios/web/web_state/ui/wk_back_forward_list_item_holder.h
+++ ios/web/web_state/ui/wk_back_forward_list_item_holder.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebKit/WebKit.h>
 
 #include "base/ios/weak_nsobject.h"
+#import "base/mac/scoped_nsobject.h"
 #include "base/macros.h"
 #include "base/supports_user_data.h"
 
@@ -18,10 +19,9 @@ class NavigationItem;
 
 // This class is a wrapper for information needed to implement native
 // WKWebView navigation. WKBackForwardListItemHolder is attached to
-// NavigationItem via the SupportsUserData interface and holds two
-// values:
-// (1) the NavigationItem's corresponding WKBackForwardListItem,
-// (2) the NavigationItem's corresponding WKNavigationType
+// NavigationItem via the SupportsUserData interface and holds the corresponding
+// WKBackForwardListItem, as well as any state that is inaccessible later and
+// thus needs to be preserved (e.g., WKNavigationType, MIME type).
 class WKBackForwardListItemHolder : public base::SupportsUserData::Data {
  public:
   // Returns the WKBackForwardListItemHolder for the NavigationItem |item|.
@@ -43,6 +43,12 @@ class WKBackForwardListItemHolder : public base::SupportsUserData::Data {
   WKNavigationType navigation_type() const { return navigation_type_; }
   void set_navigation_type(WKNavigationType type) { navigation_type_ = type; }
 
+  // Gets/sets the MIME type of the page corresponding to this item.
+  NSString* mime_type() const { return mime_type_.get(); }
+  void set_mime_type(NSString* mime_type) {
+    mime_type_.reset([mime_type copy]);
+  }
+
  private:
   WKBackForwardListItemHolder();
   ~WKBackForwardListItemHolder() override;
@@ -53,6 +59,9 @@ class WKBackForwardListItemHolder : public base::SupportsUserData::Data {
 
   // The navigation type for the associated NavigationItem.
   WKNavigationType navigation_type_;
+
+  // The MIME type of the page content.
+  base::scoped_nsobject<NSString> mime_type_;
 
   DISALLOW_COPY_AND_ASSIGN(WKBackForwardListItemHolder);
 };
