5a10862d0f40753a43c1dbf8041b05ff50e05c06
kkinnunen <kkinnunen@nvidia.com>
2015-12-01 05:34:14 -0800

    command_buffer: Implement EXT_multisample_compatibility
    Implement EXT_multisample_compatibility OpenGL ES extension in command
    buffer.
    
    Skia needs this in order to enable and disable multisampling when
    using GL path rendering.
    
    BUG=506765,529315
    
    Committed: https://crrev.com/e3237bbb682e52f92d5bd18dcab672677e4e3bc4
    Cr-Commit-Position: refs/heads/master@{#346847}
    
    Committed: https://crrev.com/28e57fb8caf8c0daf4be0692777aa9229cfdaa8b
    Cr-Commit-Position: refs/heads/master@{#347390}
    
    Review URL: https://codereview.chromium.org/1231673006
    
    Cr-Commit-Position: refs/heads/master@{#362403}

--- ui/gl/gl_enums_implementation_autogen.h
+++ ui/gl/gl_enums_implementation_autogen.h
@@ -404,7 +404,7 @@ static const GLEnums::EnumToString enum_to_string_table[] = {
         0x821A, "GL_DEPTH_STENCIL_ATTACHMENT",
     },
     {
-        0x8A40, "GL_UNIFORM_BLOCK_DATA_SIZE",
+        0x809D, "GL_MULTISAMPLE_EXT",
     },
     {
         0x821D, "GL_NUM_EXTENSIONS",
@@ -1844,6 +1844,9 @@ static const GLEnums::EnumToString enum_to_string_table[] = {
         0x8C1D, "GL_TEXTURE_BINDING_2D_ARRAY",
     },
     {
+        0x809F, "GL_SAMPLE_ALPHA_TO_ONE_EXT",
+    },
+    {
         0x8DDF, "GL_MAX_GEOMETRY_UNIFORM_COMPONENTS_EXT",
     },
     {
@@ -2832,6 +2835,9 @@ static const GLEnums::EnumToString enum_to_string_table[] = {
     },
     {
         0x8051, "GL_RGB8_OES",
+    },
+    {
+        0x8A40, "GL_UNIFORM_BLOCK_DATA_SIZE",
     },
     {
         0x8CAD, "GL_DEPTH32F_STENCIL8",
