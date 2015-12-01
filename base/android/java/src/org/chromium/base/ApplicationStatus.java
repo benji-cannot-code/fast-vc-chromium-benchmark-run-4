2979fc6516111a3645b7f3b4cdf09ce129178adc
wnwen <wnwen@chromium.org>
2015-12-01 13:51:25 -0800

    Re-land "Add stats to distinguish android renderer crashes."
    This CL is a precursor to expanding Clank stability scope
    to all OOM protected renderers that crash while Chrome is
    in the foreground.
    
    The difference in scope is effectively the number of renderers
    that crash as its corresponding tabs are all backgrounded.
    This transition time is one second before the OOM protection
    is dropped. Most, if not all, such renderer crashes are due
    to the Android OOM killer. Overall difference in proposed
    stability stats will not be much different, and will be easily
    seen with the new histogram.
    
    Expanding scope allows us to have native record which of
    the renderer crashes actually results in a minidump file
    being generated, thus demystifying whether the renderer
    was OOM killed or actually crashed. This native stat can
    be made more robust in the future, but this CL paves the
    foundation, assuming Tab.RendererExitStatus is comparable
    to Tab.RendererCrashStatus.
    
    When the renderer is cleanly shutdown, there are no details
    and no need to record any stats. Thus we skip stat recording
    for NOTIFICATION_RENDERER_PROCESS_TERMINATED.
    
    BUG=510327,560813
    
    Review URL: https://codereview.chromium.org/1473753002
    
    Cr-Commit-Position: refs/heads/master@{#362511}

--- base/android/java/src/org/chromium/base/ApplicationStatus.java
+++ base/android/java/src/org/chromium/base/ApplicationStatus.java
@@ -310,6 +310,7 @@ public class ApplicationStatus {
     /**
      * @return The state of the application (see {@link ApplicationState}).
      */
+    @CalledByNative
     public static int getStateForApplication() {
         synchronized (sCachedApplicationStateLock) {
             if (sCachedApplicationState == null) {
