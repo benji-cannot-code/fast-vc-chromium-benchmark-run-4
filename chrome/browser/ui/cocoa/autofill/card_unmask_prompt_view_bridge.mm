897e18100c3515d7f1838fe39e78829677d067c8
estade <estade@chromium.org>
2015-11-30 19:07:51 -0800

    Co-locate all NativeTheme Aura colors.
    Before this patch, colors chosen to look good for Aura were split between
    CommonThemeGetSystemColor and NativeThemeAura. This moves all the colors
    to the former and changes the signature a bit.
    
    There should be no effective change on any platform with this CL.
    
    BUG=530645
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1476663002
    
    Cr-Commit-Position: refs/heads/master@{#362326}

--- chrome/browser/ui/cocoa/autofill/card_unmask_prompt_view_bridge.mm
+++ chrome/browser/ui/cocoa/autofill/card_unmask_prompt_view_bridge.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/message_loop/message_loop.h"
 #include "base/strings/sys_string_conversions.h"
-#include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/autofill/autofill_dialog_models.h"
 #include "chrome/browser/ui/autofill/autofill_dialog_types.h"
 #include "chrome/browser/ui/chrome_style.h"
@@ -30,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ui/base/cocoa/controls/hyperlink_button_cell.h"
 #include "ui/base/cocoa/window_size_constants.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/base/theme_provider.h"
+#include "ui/native_theme/native_theme_mac.h"
 
 namespace {
 
@@ -660,11 +659,9 @@ void CardUnmaskPromptViewBridge::PerformClose() {
   [mainView addSubview:progressOverlayView_];
 
   progressOverlayLabel_.reset([constrained_window::CreateLabel() retain]);
-  NSView* webContentView = bridge_->GetWebContents()->GetNativeView();
-  ui::ThemeProvider* themeProvider = [[webContentView window] themeProvider];
-  DCHECK(themeProvider);
-  NSColor* throbberBlueColor = themeProvider->GetNSColor(
-      ThemeProperties::COLOR_THROBBER_SPINNING);
+  NSColor* throbberBlueColor = gfx::SkColorToCalibratedNSColor(
+      ui::NativeThemeMac::instance()->GetSystemColor(
+          ui::NativeTheme::kColorId_ThrobberSpinningColor));
   [progressOverlayLabel_ setTextColor:throbberBlueColor];
   [progressOverlayView_ addSubview:progressOverlayLabel_];
 
