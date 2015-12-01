740ea9d34c2b17066b1aa275e98596c320f0d7b7
davidben <davidben@chromium.org>
2015-11-30 22:46:00 -0800

    Revert "Treat '...' as a non-parent path component outside of Windows."
    This reverts commit f61e0a571a76b94eb5ce059ea25babff6a2bb3a3
    (https://codereview.chromium.org/1469153006). It broke some Blink tests as
    FilePath::ReferencesParent is also used in things relating to the HTML
    FileSystem API.
    
    Ideally, that API should be immune to bizarre Windows-isms and use a different
    notion of FilePath from the platform FilePath (which is sensitive to bizarre
    Windows-ism and others). But that'll require some work to disentangle. In the
    meantime, revert the problematic change so the tests pass.
    
    BUG=464760,563588
    TBR=mark@chromium.org,jschuh@chromium.org
    
    Review URL: https://codereview.chromium.org/1488873002
    
    Cr-Commit-Position: refs/heads/master@{#362362}

--- content/browser/fileapi/sandbox_file_system_backend_delegate_unittest.cc
+++ content/browser/fileapi/sandbox_file_system_backend_delegate_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/thread_task_runner_handle.h"
-#include "build/build_config.h"
 #include "content/public/test/test_file_system_options.h"
 #include "storage/browser/fileapi/file_system_url.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -68,7 +67,6 @@ TEST_F(SandboxFileSystemBackendDelegateTest, IsAccessValid) {
   EXPECT_FALSE(IsAccessValid(CreateFileSystemURL(".")));
   EXPECT_FALSE(IsAccessValid(CreateFileSystemURL("..")));
 
-#if defined(OS_WIN)
   // This is also disallowed due to Windows XP parent path handling.
   EXPECT_FALSE(IsAccessValid(CreateFileSystemURL("...")));
 
@@ -76,11 +74,6 @@ TEST_F(SandboxFileSystemBackendDelegateTest, IsAccessValid) {
   // on Windows.
   EXPECT_FALSE(IsAccessValid(CreateFileSystemURL(" ..")));
   EXPECT_FALSE(IsAccessValid(CreateFileSystemURL(".. ")));
-#else
-  EXPECT_TRUE(IsAccessValid(CreateFileSystemURL("...")));
-  EXPECT_TRUE(IsAccessValid(CreateFileSystemURL(" ..")));
-  EXPECT_TRUE(IsAccessValid(CreateFileSystemURL(".. ")));
-#endif
 
   // Similar but safe cases.
   EXPECT_TRUE(IsAccessValid(CreateFileSystemURL(" .")));
