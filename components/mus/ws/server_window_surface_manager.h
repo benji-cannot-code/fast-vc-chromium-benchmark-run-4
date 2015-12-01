c2b1569545cc122f107440c0435828b226128e1e
sky <sky@chromium.org>
2015-12-01 13:31:36 -0800

    Wire up inset of SetUnderlaySurfaceOffsetAndExtendedHitArea
    There were a number of bugs in the non-surfaces hit testing code. I've
    fixed those as part of this, and added tests.
    
    BUG=548424
    TEST=none
    R=fsamuel@chromium.org
    
    Review URL: https://codereview.chromium.org/1489923002
    
    Cr-Commit-Position: refs/heads/master@{#362504}

--- components/mus/ws/server_window_surface_manager.h
+++ components/mus/ws/server_window_surface_manager.h
@@ -21,6 +21,7 @@ namespace ws {
 
 class ServerWindow;
 class ServerWindowSurface;
+class ServerWindowSurfaceManagerTestApi;
 
 // ServerWindowSurfaceManager tracks the surfaces associated with a
 // ServerWindow.
@@ -40,8 +41,10 @@ class ServerWindowSurfaceManager {
   ServerWindowSurface* GetDefaultSurface();
   ServerWindowSurface* GetUnderlaySurface();
   ServerWindowSurface* GetSurfaceByType(mojom::SurfaceType type);
+  bool HasSurfaceOfType(mojom::SurfaceType type);
 
  private:
+  friend class ServerWindowSurfaceManagerTestApi;
   friend class ServerWindowSurface;
 
   cc::SurfaceId GenerateId();
--- components/mus/ws/window_tree_unittest.cc
+++ components/mus/ws/window_tree_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/mus/ws/display_manager_factory.h"
 #include "components/mus/ws/ids.h"
 #include "components/mus/ws/server_window.h"
+#include "components/mus/ws/server_window_surface_manager_test_api.h"
 #include "components/mus/ws/test_change_tracker.h"
 #include "components/mus/ws/window_tree_host_connection.h"
 #include "components/mus/ws/window_tree_impl.h"
@@ -410,6 +411,7 @@ TEST_F(WindowTreeTest, FocusOnPointer) {
   ServerWindow* v1 = connection1->GetWindow(child1);
   v1->SetVisible(true);
   v1->SetBounds(gfx::Rect(20, 20, 20, 20));
+  EnableHitTest(v1);
 
   TestWindowTreeClient* connection1_client = last_window_tree_client();
   connection1_client->tracker()->changes()->clear();
@@ -498,6 +500,7 @@ TEST_F(WindowTreeTest, BasicInputEventTarget) {
   ServerWindow* v1 = connection1->GetWindow(child1);
   v1->SetVisible(true);
   v1->SetBounds(gfx::Rect(20, 20, 20, 20));
+  EnableHitTest(v1);
 
   TestWindowTreeClient* embed_connection = last_window_tree_client();
   embed_connection->tracker()->changes()->clear();
