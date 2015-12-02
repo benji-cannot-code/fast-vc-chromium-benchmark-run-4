b8b2ae4b96e7179613a345d349976253a555c502
dbeam <dbeam@chromium.org>
2015-12-01 20:25:08 -0800

    Stringify an object key in options code to shut closure up
    TBR=reillyg@chromium.org
    BUG=none
    TEST=closer to green closure
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1486373003
    
    Cr-Commit-Position: refs/heads/master@{#362629}

--- chrome/browser/resources/options/content_settings_exceptions_area.js
+++ chrome/browser/resources/options/content_settings_exceptions_area.js
@@ -161,7 +161,7 @@ cr.define('options.contentSettings', function() {
         this.deletable = true;
 
         var objectLabel = cr.doc.createElement('span');
-        objectLabel.textContent = this.dataItem.objectName;
+        objectLabel.textContent = this.dataItem['objectName'];
         objectLabel.className = 'exception-setting';
         objectLabel.setAttribute('displaymode', 'static');
         this.contentElement.appendChild(objectLabel);
