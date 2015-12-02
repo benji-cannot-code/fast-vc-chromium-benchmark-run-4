6231e8ed954e1a56c481fb8f39656fffd4f16ecc
sergeyu <sergeyu@chromium.org>
2015-12-02 13:50:19 -0800

    Add support for WebRTC protocol in SessionConfig.
    BUG=547158
    
    Review URL: https://codereview.chromium.org/1492553002
    
    Cr-Commit-Position: refs/heads/master@{#362801}

--- remoting/protocol/content_description_unittest.cc
+++ remoting/protocol/content_description_unittest.cc
@@ -25,7 +25,7 @@ TEST(ContentDescriptionTest, FormatAndParse) {
   scoped_ptr<buzz::XmlElement> xml(description.ToXml());
   LOG(ERROR) << xml->Str();
   scoped_ptr<ContentDescription> parsed(
-      ContentDescription::ParseXml(xml.get()));
+      ContentDescription::ParseXml(xml.get(), false));
   ASSERT_TRUE(parsed.get());
   EXPECT_TRUE(description.config()->control_configs() ==
               parsed->config()->control_configs());
@@ -44,6 +44,7 @@ TEST(ContentDescriptionTest, FormatAndParse) {
 TEST(ContentDescriptionTest, ParseUnknown) {
   std::string kTestDescription =
       "<description xmlns=\"google:remoting\">"
+      "  <standard-ice/>"
       "  <control transport=\"stream\" version=\"2\"/>"
       "  <event transport=\"stream\" version=\"2\"/>"
       "  <event transport=\"new_awesome_transport\" version=\"3\"/>"
@@ -52,7 +53,7 @@ TEST(ContentDescriptionTest, ParseUnknown) {
       "</description>";
   scoped_ptr<buzz::XmlElement> xml(buzz::XmlElement::ForStr(kTestDescription));
   scoped_ptr<ContentDescription> parsed(
-      ContentDescription::ParseXml(xml.get()));
+      ContentDescription::ParseXml(xml.get(), false));
   ASSERT_TRUE(parsed.get());
   EXPECT_EQ(1U, parsed->config()->event_configs().size());
   EXPECT_TRUE(parsed->config()->event_configs().front() ==
@@ -66,6 +67,7 @@ TEST(ContentDescriptionTest, ParseUnknown) {
 TEST(ContentDescriptionTest, NoneTransport) {
   std::string kTestDescription =
       "<description xmlns=\"google:remoting\">"
+      "  <standard-ice/>"
       "  <control transport=\"stream\" version=\"2\"/>"
       "  <event transport=\"stream\" version=\"2\"/>"
       "  <event transport=\"stream\" version=\"2\"/>"
@@ -75,7 +77,7 @@ TEST(ContentDescriptionTest, NoneTransport) {
       "</description>";
   scoped_ptr<buzz::XmlElement> xml(buzz::XmlElement::ForStr(kTestDescription));
   scoped_ptr<ContentDescription> parsed(
-      ContentDescription::ParseXml(xml.get()));
+      ContentDescription::ParseXml(xml.get(), false));
   ASSERT_TRUE(parsed.get());
   EXPECT_EQ(1U, parsed->config()->audio_configs().size());
   EXPECT_TRUE(parsed->config()->audio_configs().front() == ChannelConfig());
@@ -86,6 +88,7 @@ TEST(ContentDescriptionTest, NoneTransport) {
 TEST(ContentDescriptionTest, NoneTransportWithCodec) {
   std::string kTestDescription =
       "<description xmlns=\"google:remoting\">"
+      "  <standard-ice/>"
       "  <control transport=\"stream\" version=\"2\"/>"
       "  <event transport=\"stream\" version=\"2\"/>"
       "  <event transport=\"stream\" version=\"2\"/>"
@@ -95,7 +98,7 @@ TEST(ContentDescriptionTest, NoneTransportWithCodec) {
       "</description>";
   scoped_ptr<buzz::XmlElement> xml(buzz::XmlElement::ForStr(kTestDescription));
   scoped_ptr<ContentDescription> parsed(
-      ContentDescription::ParseXml(xml.get()));
+      ContentDescription::ParseXml(xml.get(), false));
   ASSERT_TRUE(parsed.get());
   EXPECT_EQ(1U, parsed->config()->audio_configs().size());
   EXPECT_TRUE(parsed->config()->audio_configs().front() == ChannelConfig());
--- remoting/remoting_test.gypi
+++ remoting/remoting_test.gypi
@@ -329,6 +329,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'protocol/ppapi_module_stub.cc',
         'protocol/pseudotcp_adapter_unittest.cc',
         'protocol/quic_channel_factory_unittest.cc',
+        'protocol/session_config_unittest.cc',
         'protocol/ssl_hmac_channel_authenticator_unittest.cc',
         'protocol/third_party_authenticator_unittest.cc',
         'protocol/v2_authenticator_unittest.cc',
