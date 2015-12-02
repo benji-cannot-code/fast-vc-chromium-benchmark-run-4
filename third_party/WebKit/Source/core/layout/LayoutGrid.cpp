b7ddcfbda70bb22ae048e2a179aca1088f7fd57d
jfernandez <jfernandez@igalia.com>
2015-12-01 17:41:17 -0800

    [css-grid] The auto-margins alignment must respect specified margins
    When resolving auto margins so that they use the available space, in the
    corresponding axis, we must consider that there might be other specified
    margins, which account to determine the actual available space.
    
    BUG=558789
    
    Review URL: https://codereview.chromium.org/1490803002
    
    Cr-Commit-Position: refs/heads/master@{#362573}

--- third_party/WebKit/Source/core/layout/LayoutGrid.cpp
+++ third_party/WebKit/Source/core/layout/LayoutGrid.cpp
@@ -1733,7 +1733,7 @@ void LayoutGrid::updateAutoMarginsInRowAxisIfNeeded(LayoutBox& child)
 {
     ASSERT(!child.isOutOfFlowPositioned());
 
-    LayoutUnit availableAlignmentSpace = child.overrideContainingBlockContentLogicalWidth() - child.logicalWidth();
+    LayoutUnit availableAlignmentSpace = child.overrideContainingBlockContentLogicalWidth() - child.logicalWidth() - child.marginLogicalWidth();
     if (availableAlignmentSpace <= 0)
         return;
 
@@ -1754,7 +1754,7 @@ void LayoutGrid::updateAutoMarginsInColumnAxisIfNeeded(LayoutBox& child)
 {
     ASSERT(!child.isOutOfFlowPositioned());
 
-    LayoutUnit availableAlignmentSpace = child.overrideContainingBlockContentLogicalHeight() - child.logicalHeight();
+    LayoutUnit availableAlignmentSpace = child.overrideContainingBlockContentLogicalHeight() - child.logicalHeight() - child.marginLogicalHeight();
     if (availableAlignmentSpace <= 0)
         return;
 
