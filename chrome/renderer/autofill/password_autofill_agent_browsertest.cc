56df8ae81b6ffaa4d1b1d71a6b96bd5c46961fef
ramya.v <ramya.v@samsung.com>
2015-12-01 18:24:46 -0800

    new URL('') should throw TypeError
    As per the spec https://url.spec.whatwg.org/#no-scheme-state
    If base url is null or not relative and starting character of
    url is not # return failure. So empty url should also throw
    exception in this case.
    
    Few tests related to autofill were failing after this
    change, because empty action url is by default resolved to
    base url before this patch. Made appropriate code changes
    as empty action url can be equal to base url only for
    relative base urls.
    
    BUG=463961
    
    Review URL: https://codereview.chromium.org/1409293007
    
    Cr-Commit-Position: refs/heads/master@{#362589}

--- chrome/renderer/autofill/password_autofill_agent_browsertest.cc
+++ chrome/renderer/autofill/password_autofill_agent_browsertest.cc
@@ -554,7 +554,7 @@ TEST_F(PasswordAutofillAgentTest, InitialAutocompleteForEmptyAction) {
 
   // Set the expected form origin and action URLs.
   UpdateOriginForHTML(kEmptyActionFormHTML);
-  fill_data_.action = fill_data_.origin;
+  fill_data_.action = GURL();
 
   // Simulate the browser sending back the login info, it triggers the
   // autocomplete.
@@ -1401,7 +1401,7 @@ TEST_F(PasswordAutofillAgentTest,
 TEST_F(PasswordAutofillAgentTest,
        RememberLastNonEmptyUsernameAndPasswordOnSubmit_New) {
   const char kNewPasswordFormHTML[] =
-      "<FORM name='LoginTestForm'>"
+      "<FORM name='LoginTestForm' action='http://www.bidule.com'>"
       "  <INPUT type='text' id='username' autocomplete='username'/>"
       "  <INPUT type='password' id='password' autocomplete='new-password'/>"
       "  <INPUT type='submit' value='Login'/>"
--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -1106,7 +1106,11 @@ bool IsFormVisible(blink::WebFrame* frame,
   frame->document().forms(forms);
 
 #if !defined(OS_MACOSX) && !defined(OS_ANDROID)
-  const bool action_is_empty = canonical_action == canonical_origin;
+  // Omitting the action attribute would result in |canonical_origin| for
+  // hierarchical schemes like http:, and in an empty URL for non-hierarchical
+  // schemes like about: or data: etc.
+  const bool action_is_empty = canonical_action.is_empty()
+                               || canonical_action == canonical_origin;
 #endif
 
   // Since empty or unspecified action fields are automatically set to page URL,
@@ -1121,7 +1125,8 @@ bool IsFormVisible(blink::WebFrame* frame,
 
     GURL iter_canonical_action = GetCanonicalActionForForm(form);
 #if !defined(OS_MACOSX) && !defined(OS_ANDROID)
-    bool form_action_is_empty = iter_canonical_action == frame_url;
+    bool form_action_is_empty = iter_canonical_action.is_empty()
+                                || iter_canonical_action == frame_url;
 
     if (action_is_empty != form_action_is_empty)
       continue;
