59b11dada9a6103747aa7a2eefd8a3df851043fb
rune <rune@opera.com>
2015-12-02 02:15:03 -0800

    Use invalidation sets for :lang changes.
    Modifying lang or xml:lang attributes caused a LocalStyleChange for the
    corresponding element in the presentational attribute check, but other
    elements may be affected using selector combinators.
    
    Use invalidation sets like we do for other pseudo classes.
    
    BUG=564331
    
    Review URL: https://codereview.chromium.org/1485363002
    
    Cr-Commit-Position: refs/heads/master@{#362675}

--- third_party/WebKit/Source/core/css/RuleFeature.cpp
+++ third_party/WebKit/Source/core/css/RuleFeature.cpp
@@ -306,6 +306,7 @@ InvalidationSet* RuleFeatureSet::invalidationSetForSelector(const CSSSelector& s
         case CSSSelector::PseudoInvalid:
         case CSSSelector::PseudoIndeterminate:
         case CSSSelector::PseudoTarget:
+        case CSSSelector::PseudoLang:
         case CSSSelector::PseudoInRange:
         case CSSSelector::PseudoOutOfRange:
         case CSSSelector::PseudoUnresolved:
