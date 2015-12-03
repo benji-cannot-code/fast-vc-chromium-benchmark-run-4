0925ec49c2066ac6bd8205b6208c6600e5901908
tfarina <tfarina@chromium.org>
2015-12-03 13:07:18 -0800

    tools/gn: forward declare some base types in trace.h
    CommandLine and FilePath are being passed by reference, so they can be
    forward declared and their header includes be moved to the source file
    instead.
    
    BUG=None
    TEST=gn gen + gn_unittests
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/1492813002
    
    Cr-Commit-Position: refs/heads/master@{#363056}

--- tools/gn/trace.cc
+++ tools/gn/trace.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sstream>
 #include <vector>
 
+#include "base/command_line.h"
+#include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/json/string_escape.h"
 #include "base/logging.h"
--- tools/gn/trace.h
+++ tools/gn/trace.h
@@ -8,13 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/command_line.h"
-#include "base/files/file_path.h"
 #include "base/macros.h"
 #include "base/threading/platform_thread.h"
 #include "base/time/time.h"
 
 class Label;
+
+namespace base {
+class CommandLine;
+class FilePath;
+}
 
 class TraceItem {
  public:
