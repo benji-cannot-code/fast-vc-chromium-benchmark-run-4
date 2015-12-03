270fad5aa23ae70411b29eeebf4bad23b7a16170
dbeam <dbeam@chromium.org>
2015-12-03 10:46:53 -0800

    Use content::TestBrowserThreadBundle in DownloadsListTracker unit_tests
    R=dpapad@chromium.org
    BUG=none
    TEST=less code
    
    Review URL: https://codereview.chromium.org/1492253002
    
    Cr-Commit-Position: refs/heads/master@{#363019}

--- chrome/browser/ui/webui/md_downloads/downloads_list_tracker_unittest.cc
+++ chrome/browser/ui/webui/md_downloads/downloads_list_tracker_unittest.cc
@@ -9,14 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/message_loop/message_loop.h"
 #include "base/stl_util.h"
 #include "base/time/time.h"
 #include "chrome/browser/download/download_item_model.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/test/mock_download_item.h"
 #include "content/public/test/mock_download_manager.h"
-#include "content/public/test/test_browser_thread.h"
+#include "content/public/test/test_browser_thread_bundle.h"
 #include "content/public/test/test_web_ui.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -94,8 +93,7 @@ class TestDownloadsListTracker : public DownloadsListTracker {
 // A fixture to test DownloadsListTracker.
 class DownloadsListTrackerTest : public testing::Test {
  public:
-  DownloadsListTrackerTest()
-      : ui_thread_(content::BrowserThread::UI, &message_loop_) {}
+  DownloadsListTrackerTest() {}
   ~DownloadsListTrackerTest() override {
     for (auto* mock_item : mock_items_)
       testing::Mock::VerifyAndClear(mock_item);
@@ -139,8 +137,7 @@ class DownloadsListTrackerTest : public testing::Test {
   }
 
   // NOTE: The initialization order of these members matters.
-  base::MessageLoopForUI message_loop_;
-  content::TestBrowserThread ui_thread_;
+  content::TestBrowserThreadBundle thread_bundle_;
   TestingProfile profile_;
 
   testing::NiceMock<content::MockDownloadManager> manager_;
