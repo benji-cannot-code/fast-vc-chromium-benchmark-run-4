df880f8ddc2353f5f3671cb936432c174799a33e
oshima <oshima@chromium.org>
2015-12-03 09:38:21 -0800

    Use WindowTracker when resizing root window as it may delete other window in unit tests.
    This so far can happen in the test. The CL https://codereview.chromium.org/1483083003/ changes the timing of animation completion, which led to this state.
    
    BUG=None
    TEST=RootWindowLayoutManagerTest.DeleteChildDuringResize
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1484383003
    
    Cr-Commit-Position: refs/heads/master@{#363004}

--- ash/ash.gyp
+++ ash/ash.gyp
@@ -909,6 +909,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'wm/panels/panel_layout_manager_unittest.cc',
       'wm/panels/panel_window_resizer_unittest.cc',
       'wm/resize_shadow_and_cursor_unittest.cc',
+      'wm/root_window_layout_manager_unittest.cc',
       'wm/screen_dimmer_unittest.cc',
       'wm/session_state_animator_impl_unittest.cc',
       'wm/stacking_controller_unittest.cc',
--- ash/wm/root_window_layout_manager.cc
+++ ash/wm/root_window_layout_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/desktop_background/desktop_background_widget_controller.h"
 #include "ash/root_window_controller.h"
 #include "ui/aura/window_event_dispatcher.h"
+#include "ui/aura/window_tracker.h"
 #include "ui/compositor/layer.h"
 #include "ui/views/widget/widget.h"
 
@@ -33,12 +34,13 @@ void RootWindowLayoutManager::OnWindowResized() {
 
   // Resize both our immediate children (the containers-of-containers animated
   // by PowerButtonController) and their children (the actual containers).
-  aura::Window::Windows::const_iterator i;
-  for (i = owner_->children().begin(); i != owner_->children().end(); ++i) {
-    (*i)->SetBounds(fullscreen_bounds);
-    aura::Window::Windows::const_iterator j;
-    for (j = (*i)->children().begin(); j != (*i)->children().end(); ++j)
-      (*j)->SetBounds(fullscreen_bounds);
+  aura::WindowTracker children_tracker(owner_->children());
+  while (children_tracker.has_windows()) {
+    aura::Window* child = children_tracker.Pop();
+    child->SetBounds(fullscreen_bounds);
+    aura::WindowTracker grandchildren_tracker(child->children());
+    while (grandchildren_tracker.has_windows())
+      grandchildren_tracker.Pop()->SetBounds(fullscreen_bounds);
   }
   RootWindowController* root_window_controller =
       GetRootWindowController(owner_);
