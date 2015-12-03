ae9850dc8c4d0e1cebf539ce6608b8d7a8654805
yhirano <yhirano@chromium.org>
2015-12-02 23:28:13 -0800

    [Fetch API] Use FetchFormDataConsumerHandle in Response constructor.
    Use FetchFormDataConsumerHandle in Response::create to simplify the code.
    
    This CL also includes some minor refactoring.
     - Rename "responseInit" with "init" for the simplicity
     - Replace 0 with nullptr
     - Wrap comments in 80 cols
    
    BUG=564479
    
    Review URL: https://codereview.chromium.org/1490243002
    
    Cr-Commit-Position: refs/heads/master@{#362929}

--- third_party/WebKit/Source/modules/fetch/Response.h
+++ third_party/WebKit/Source/modules/fetch/Response.h
@@ -16,12 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/fetch/Headers.h"
 #include "platform/blob/BlobData.h"
 #include "platform/heap/Handle.h"
+#include "wtf/PassOwnPtr.h"
 
 namespace blink {
 
 class Blob;
 class DOMArrayBuffer;
 class ExceptionState;
+class FetchDataConsumerHandle;
 class ResponseInit;
 class WebServiceWorkerResponse;
 
@@ -37,7 +39,7 @@ public:
     static Response* create(ExecutionContext*, ExceptionState&);
     static Response* create(ExecutionContext*, const BodyInit&, const Dictionary&, ExceptionState&);
 
-    static Response* create(ExecutionContext*, Blob*, const ResponseInit&, ExceptionState&);
+    static Response* create(ExecutionContext*, PassOwnPtr<FetchDataConsumerHandle> bodyHandle, const String& contentType, const ResponseInit&, ExceptionState&);
     static Response* create(ExecutionContext*, FetchResponseData*);
     static Response* create(ExecutionContext*, const WebServiceWorkerResponse&);
 
