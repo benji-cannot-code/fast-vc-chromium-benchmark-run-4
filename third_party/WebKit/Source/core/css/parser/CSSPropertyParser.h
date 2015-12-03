bf56c40004b7a924aa0fad6bcea5625557fabb17
rob.buis <rob.buis@samsung.com>
2015-12-03 05:43:09 -0800

    Move cursor property into CSSPropertyParser
    Move cursor property handling from LegacyCSSPropertyParser
    into CSSPropertyParser.
    
    BUG=499780
    
    Review URL: https://codereview.chromium.org/1457873002
    
    Cr-Commit-Position: refs/heads/master@{#362974}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/CSSColorValue.h"
 #include "core/css/CSSGradientValue.h"
 #include "core/css/CSSGridTemplateAreasValue.h"
+#include "core/css/CSSImageValue.h"
 #include "core/css/CSSPropertySourceData.h"
 #include "core/css/parser/CSSParserTokenRange.h"
 #include "platform/Length.h"
@@ -155,6 +156,8 @@ private:
 
     bool consumeColumns(bool important);
 
+    PassRefPtrWillBeRawPtr<CSSValue> consumeCursor(CSSParserTokenRange&);
+
     PassRefPtrWillBeRawPtr<CSSValue> parseGridPosition();
     bool parseIntegerOrCustomIdentFromGridPosition(RefPtrWillBeRawPtr<CSSPrimitiveValue>& numericValue, RefPtrWillBeRawPtr<CSSCustomIdentValue>& gridLineName);
     bool parseGridItemPositionShorthand(CSSPropertyID, bool important);
@@ -228,6 +231,7 @@ private:
     bool parseCrossfade(CSSParserValueList*, RefPtrWillBeRawPtr<CSSValue>&);
 
     PassRefPtrWillBeRawPtr<CSSValue> parseImageSet(CSSParserValueList*);
+    PassRefPtrWillBeRawPtr<CSSValue> consumeImageSet(CSSParserTokenRange&);
 
     PassRefPtrWillBeRawPtr<CSSValueList> parseFilter();
     PassRefPtrWillBeRawPtr<CSSFunctionValue> parseBuiltinFilterArguments(CSSParserValueList*, CSSValueID);
@@ -242,7 +246,13 @@ private:
     PassRefPtrWillBeRawPtr<CSSStringValue> createPrimitiveStringValue(CSSParserValue*);
     PassRefPtrWillBeRawPtr<CSSCustomIdentValue> createPrimitiveCustomIdentValue(CSSParserValue*);
 
-    PassRefPtrWillBeRawPtr<CSSValue> createCSSImageValueWithReferrer(const AtomicString& rawValue, const KURL&);
+    // TODO(rwlbuis): move to CSSPropertyParser.cpp once CSSParserToken conversion is done.
+    inline PassRefPtrWillBeRawPtr<CSSValue> createCSSImageValueWithReferrer(const AtomicString& rawValue, const KURL& url)
+    {
+        RefPtrWillBeRawPtr<CSSValue> imageValue = CSSImageValue::create(rawValue, url);
+        toCSSImageValue(imageValue.get())->setReferrer(m_context.referrer());
+        return imageValue;
+    }
 
     PassRefPtrWillBeRawPtr<CSSBasicShapeInsetValue> parseInsetRoundedCorners(PassRefPtrWillBeRawPtr<CSSBasicShapeInsetValue>, CSSParserValueList*);
 
