7d3b9b0a9413c259d939b095edba551926c8fa6c
brettw <brettw@chromium.org>
2015-12-02 16:47:12 -0800

    Add all components to "gn check".
    All components now pass a "gn check" step. Whitelist with a wildcard.
    
    Reland of https://codereview.chromium.org/1490113006
    Reland of https://codereview.chromium.org/1491743002
    TBR=dpranke@chromium.org
    
    Review URL: https://codereview.chromium.org/1496493003
    
    Cr-Commit-Position: refs/heads/master@{#362841}

--- components/version_info/version_info.cc
+++ components/version_info/version_info.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/components_strings.h"
 
 #if defined(USE_UNOFFICIAL_VERSION_NUMBER)
-#include "ui/base/l10n/l10n_util.h"
+#include "ui/base/l10n/l10n_util.h"  // nogncheck
 #endif  // USE_UNOFFICIAL_VERSION_NUMBER
 
 namespace version_info {
