6f196c215f35aed212fe4aff2dce7df8ad26d57b
dyen <dyen@chromium.org>
2015-12-04 08:47:22 -0800

    Added an extra sync token field for extra command buffer identification.
    This CL adds an extra field within sync tokens without changing the size
    of the sync token. Currently this extra field is only used for the
    GPU channel implementation, storing the stream of the identified command
    buffer.
    
    BUG=514815
    
    Review URL: https://codereview.chromium.org/1489573003
    
    Cr-Commit-Position: refs/heads/master@{#363231}

--- gpu/command_buffer/service/mailbox_manager_unittest.cc
+++ gpu/command_buffer/service/mailbox_manager_unittest.cc
@@ -20,6 +20,7 @@ namespace gles2 {
 using namespace ::testing;
 
 static const SyncToken g_sync_token(gpu::CommandBufferNamespace::GPU_IO,
+                                    0,
                                     123,
                                     0);
 
