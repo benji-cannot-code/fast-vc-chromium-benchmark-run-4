182489a4db2cf5f12abeaab893d79fed4c45718c
gyzhou <gyzhou@chromium.org>
2015-12-04 11:40:28 -0800

    This is the first of three CLs for desktop chrome tab capture. This Cl changes the webRTC's JS code for internal manual test. A receiver video window for webRTC is added and audio and video are transmitted to receiver side.
    WebRTC today supports media capture and sharing for "screen" and "window" types through Javascript APIs chooseDesktopMedia/GetUserMedia. This Cl adds support for chrome "tab" type for the same JS APIs.
    
    The design document can be found at https://goo.gl/uoHnv0
    
    BUG=557222
    
    Review URL: https://codereview.chromium.org/1498863002
    
    Cr-Commit-Position: refs/heads/master@{#363270}

--- chrome/common/extensions/docs/examples/api/desktopCapture/background.js
+++ chrome/common/extensions/docs/examples/api/desktopCapture/background.js
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     bounds: {
-      width: 700,
-      height: 600
+      width: 1080,
+      height: 420
     }
   });
 });
