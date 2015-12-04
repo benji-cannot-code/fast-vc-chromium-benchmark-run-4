a0487208348c64345ed584ad13e058d15a41e8dc
srirama.m <srirama.m@samsung.com>
2015-12-04 05:29:26 -0800

    Remove userCancelledLoad and refactor the code at the call sites
    The function userCancelledLoad isn't called when the user cancels
    the load (there's no way to do that) and yet it implements that
    bit of the spec. So removing this function and adding only
    the necessary bits at the call sites.
    
    Review URL: https://codereview.chromium.org/1481913002
    
    Cr-Commit-Position: refs/heads/master@{#363193}

--- third_party/WebKit/Source/core/html/HTMLMediaElement.h
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.h
@@ -357,7 +357,6 @@ private:
     WebMediaPlayer::LoadType loadType() const;
     void scheduleNextSourceChild();
     void loadNextSourceChild();
-    void userCancelledLoad();
     void clearMediaPlayer(int flags);
     void clearMediaPlayerAndAudioSourceProviderClientWithoutLocking();
     bool havePotentialSourceChild();
