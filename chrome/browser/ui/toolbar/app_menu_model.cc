15d3d8c4bfe8ead0a925411a52fbdcc53e7862f7
brettw <brettw@chromium.org>
2015-12-03 23:03:24 -0800

    Move ENABLE_PROFILING to build flags system.
    This flag is only used in 3 files so having a global define is unnecessary.
    
    Moves the build flag to compiler.gni so it can be included by the new target in base that generates the header, as well as the tcmalloc target.
    
    The tcmalloc target now conditionally compiles the profiler depending on this flag. This is something the GYP build does but was inconsistent in GN. This results in the tcmalloc source files not being compiled in normal builds.
    
    This should remove the static initializer in tcmalloc profiling in the normal build.
    
    Independently, remove an iostream use in gin logging. Use printf instead.
    
    BUG=559766
    NOPRESUBMIT=true
    (on account of changing the printf, which I did not add)
    
    Review URL: https://codereview.chromium.org/1499773002
    
    Cr-Commit-Position: refs/heads/master@{#363156}

--- chrome/browser/ui/toolbar/app_menu_model.cc
+++ chrome/browser/ui/toolbar/app_menu_model.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cmath>
 
 #include "base/command_line.h"
+#include "base/debug/debugging_flags.h"
 #include "base/metrics/histogram.h"
 #include "base/prefs/pref_service.h"
 #include "base/strings/string_number_conversions.h"
@@ -323,7 +324,7 @@ void ToolsMenuModel::Build(Browser* browser) {
   AddSeparator(ui::NORMAL_SEPARATOR);
   AddItemWithStringId(IDC_DEV_TOOLS, IDS_DEV_TOOLS);
 
-#if defined(ENABLE_PROFILING) && !defined(NO_TCMALLOC)
+#if BUILDFLAG(ENABLE_PROFILING) && !defined(NO_TCMALLOC)
   AddSeparator(ui::NORMAL_SEPARATOR);
   AddCheckItemWithStringId(IDC_PROFILING_ENABLED, IDS_PROFILING_ENABLED);
 #endif
