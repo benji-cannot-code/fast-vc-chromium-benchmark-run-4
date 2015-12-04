afa3a49c959de7a472c579f3ee3fc3b5aef887a8
pkotwicz <pkotwicz@chromium.org>
2015-12-04 08:25:04 -0800

    Fix tools/android/heap_profiler:heap_dump GN compile
    BUG=507294
    
    Review URL: https://codereview.chromium.org/1500883002
    
    Cr-Commit-Position: refs/heads/master@{#363221}

--- tools/android/heap_profiler/heap_dump.c
+++ tools/android/heap_profiler/heap_dump.c
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unistd.h>
 #include <sys/ptrace.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 
 #include "tools/android/heap_profiler/heap_profiler.h"
 
@@ -235,8 +236,6 @@ static void lseek_abs(int fd, size_t off) {
 static ssize_t read_safe(int fd, void* buf, size_t count) {
   ssize_t res;
   size_t bytes_read = 0;
-  if (count < 0)
-    return -1;
   do {
     do {
       res = read(fd, buf + bytes_read, count - bytes_read);
