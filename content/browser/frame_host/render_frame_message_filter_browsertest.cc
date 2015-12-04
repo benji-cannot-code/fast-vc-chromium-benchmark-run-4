1295dda193defde3d4fcb67b03cd30409e6a8866
nick <nick@chromium.org>
2015-12-04 10:47:45 -0800

    Add message filter to catch FrameHostMsg_RenderProcessGone messages originating from the renderer.
    Rename render_message_filter_browsertest to render_frame_message_filter_browsertest (all the existing tests exercise stuff that had been move to RFMF).
    Add a PwnMessageReceived test to validate the new behavior.
    
    BUG=556351
    TEST=content_browsertests
    
    Review URL: https://codereview.chromium.org/1500903002
    
    Cr-Commit-Position: refs/heads/master@{#363259}

--- content/browser/frame_host/render_frame_message_filter_browsertest.cc
+++ content/browser/frame_host/render_frame_message_filter_browsertest.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
+#include "base/test/histogram_tester.h"
+#include "content/browser/bad_message.h"
 #include "content/browser/frame_host/frame_tree.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/common/frame_messages.h"
@@ -22,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_security_test_util.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace content {
@@ -37,11 +40,11 @@ std::string GetCookieFromJS(RenderFrameHost* frame) {
 
 }  // namespace
 
-using RenderMessageFilterBrowserTest = ContentBrowserTest;
+using RenderFrameMessageFilterBrowserTest = ContentBrowserTest;
 
 // Exercises basic cookie operations via javascript, including an http page
 // interacting with secure cookies.
-IN_PROC_BROWSER_TEST_F(RenderMessageFilterBrowserTest, Cookies) {
+IN_PROC_BROWSER_TEST_F(RenderFrameMessageFilterBrowserTest, Cookies) {
   host_resolver()->AddRule("*", "127.0.0.1");
   ASSERT_TRUE(embedded_test_server()->Start());
   SetupCrossSiteRedirector(embedded_test_server());
@@ -50,8 +53,8 @@ IN_PROC_BROWSER_TEST_F(RenderMessageFilterBrowserTest, Cookies) {
   https_server.ServeFilesFromSourceDirectory("content/test/data");
   ASSERT_TRUE(https_server.Start());
 
-  // The server sends a HttpOnly cookie. The RenderMessageFilter should never
-  // allow this to be sent to any renderer process.
+  // The server sends a HttpOnly cookie. The RenderFrameMessageFilter should
+  // never allow this to be sent to any renderer process.
   GURL https_url = https_server.GetURL("/set-cookie?notforjs=1;HttpOnly");
   GURL http_url = embedded_test_server()->GetURL("/frame_with_load_event.html");
 
@@ -101,9 +104,10 @@ IN_PROC_BROWSER_TEST_F(RenderMessageFilterBrowserTest, Cookies) {
   EXPECT_EQ("B=2; D=4", GetCookieFromJS(web_contents_http->GetMainFrame()));
 }
 
-// The RenderMessageFilter will kill processes when they access the cookies of
-// sites other than the site the process is dedicated to, under site isolation.
-IN_PROC_BROWSER_TEST_F(RenderMessageFilterBrowserTest,
+// The RenderFrameMessageFilter will kill processes when they access the cookies
+// of sites other than the site the process is dedicated to, under site
+// isolation.
+IN_PROC_BROWSER_TEST_F(RenderFrameMessageFilterBrowserTest,
                        CrossSiteCookieSecurityEnforcement) {
   // The code under test is only active under site isolation.
   if (!AreAllSitesIsolatedForTesting()) {
@@ -172,6 +176,38 @@ IN_PROC_BROWSER_TEST_F(RenderMessageFilterBrowserTest,
       " Site A\n"
       "Where A = http://127.0.0.1/ (no process)",
       v.DepictFrameTree(tab->GetFrameTree()->root()));
+}
+
+// FrameHostMsg_RenderProcessGone is a synthetic message that's really an
+// implementation detail of RenderProcessHostImpl's crash recovery. It should be
+// ignored if it arrives over the IPC channel.
+IN_PROC_BROWSER_TEST_F(RenderFrameMessageFilterBrowserTest, RenderProcessGone) {
+  GURL web_url("http://foo.com/simple_page.html");
+  NavigateToURL(shell(), web_url);
+  RenderFrameHost* web_rfh = shell()->web_contents()->GetMainFrame();
+
+  base::HistogramTester uma;
+
+  ASSERT_TRUE(web_rfh->IsRenderFrameLive());
+  RenderProcessHostWatcher web_process_killed(
+      web_rfh->GetProcess(), RenderProcessHostWatcher::WATCH_FOR_PROCESS_EXIT);
+  IPC::IpcSecurityTestUtil::PwnMessageReceived(
+      web_rfh->GetProcess()->GetChannel(),
+      FrameHostMsg_RenderProcessGone(
+          web_rfh->GetRoutingID(), base::TERMINATION_STATUS_NORMAL_TERMINATION,
+          0));
+
+  EXPECT_THAT(uma.GetAllSamples("Stability.BadMessageTerminated.Content"),
+              testing::ElementsAre(base::Bucket(
+                  bad_message::RFMF_RENDERER_FAKED_ITS_OWN_DEATH, 1)));
+
+  // If the message had gone through, we'd have marked the RFH as dead but
+  // left the RPH and its connection alive, and the Wait below would hang.
+  web_process_killed.Wait();
+
+  ASSERT_FALSE(web_rfh->GetProcess()->HasConnection());
+  ASSERT_FALSE(web_rfh->IsRenderFrameLive());
+  ASSERT_FALSE(web_process_killed.did_exit_normally());
 }
 
 }  // namespace content
