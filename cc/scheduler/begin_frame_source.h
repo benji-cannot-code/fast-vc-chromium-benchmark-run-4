9df5a37781f816dd51ae9fefbbce5fd8f40b6e82
mithro <mithro@mithis.com>
2015-12-03 21:58:46 -0800

    cc: Move the MockBeginFrameObserver (and helper macros) into cc/test directory.
    This allows usage in other tests which interface with observers.
    
    The change also refactors the EXPECT_* macros so they can be used with further expectations and closer match the EXPECT_CALL form.
    
    The EXPECT_* macros also now work with a second implicit sequence such as;
    {
      ::testing::InSequence seq;
    
      EXPECT_BEGIN_FRAME_USED(obs, 100, 200, 300);
      EXPECT_CALL(other, Function());
      EXPECT_BEGIN_FRAME_USED(obs, 400, 500, 600);
    }
    
    R=brianderson
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1493233002
    
    Cr-Commit-Position: refs/heads/master@{#363147}

--- cc/scheduler/begin_frame_source.h
+++ cc/scheduler/begin_frame_source.h
@@ -14,6 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/output/begin_frame_args.h"
 #include "cc/scheduler/delay_based_time_source.h"
 
+#ifdef NDEBUG
+#define DEBUG_FRAMES(...)
+#else
+#define DEBUG_FRAMES(name, arg1_name, arg1_val, arg2_name, arg2_val)         \
+  TRACE_EVENT2(TRACE_DISABLED_BY_DEFAULT("cc.debug.scheduler.frames"), name, \
+               arg1_name, arg1_val, arg2_name, arg2_val);
+#endif
+
 namespace cc {
 
 // (Pure) Interface for observing BeginFrame messages from BeginFrameSource
---
+++ cc/test/mock_helper.h
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CC_TEST_MOCK_HELPER_H_
+#define CC_TEST_MOCK_HELPER_H_
+
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest-spi.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+#define EXPECT_MOCK_FAILURE(statement)                            \
+  do {                                                            \
+    class GTestExpectMockFailureHelper {                          \
+     public:                                                      \
+      static void Execute() { statement; }                        \
+    };                                                            \
+    ::testing::TestPartResultArray gtest_failures;                \
+    {                                                             \
+      ::testing::ScopedFakeTestPartResultReporter gtest_reporter( \
+          ::testing::ScopedFakeTestPartResultReporter::           \
+              INTERCEPT_ONLY_CURRENT_THREAD,                      \
+          &gtest_failures);                                       \
+      GTestExpectMockFailureHelper::Execute();                    \
+    }                                                             \
+    EXPECT_GT(gtest_failures.size(), 0);                          \
+  } while (::testing::internal::AlwaysFalse())
+
+#endif  // CC_TEST_MOCK_HELPER_H_
