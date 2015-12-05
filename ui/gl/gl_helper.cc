e0ca1eb1a17c5a3ef8d274677cf410d6f34375b2
dcastagna <dcastagna@chromium.org>
2015-12-04 16:45:36 -0800

    Dedupe code to draw a gl quad.
    Extract common code to draw a simple gl quad without transformation and
    move it to gl_helper.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1496153002
    
    Cr-Commit-Position: refs/heads/master@{#363320}

--- ui/gl/gl_helper.cc
+++ ui/gl/gl_helper.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/logging.h"
+#include "ui/gl/scoped_binders.h"
 
 namespace gfx {
 
@@ -67,6 +68,31 @@ GLuint GLHelper::SetupProgram(GLuint vertex_shader, GLuint fragment_shader) {
     program = 0;
   }
   return program;
+}
+
+// static
+GLuint GLHelper::SetupQuadVertexBuffer() {
+  GLuint vertex_buffer = 0;
+  glGenBuffersARB(1, &vertex_buffer);
+  gfx::ScopedBufferBinder buffer_binder(GL_ARRAY_BUFFER, vertex_buffer);
+  GLfloat data[] = {-1.f, -1.f, 1.f, -1.f, -1.f, 1.f, 1.f, 1.f};
+  glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+  return vertex_buffer;
+}
+
+// static
+void GLHelper::DrawQuad(GLuint vertex_buffer) {
+  gfx::ScopedBufferBinder buffer_binder(GL_ARRAY_BUFFER, vertex_buffer);
+  gfx::ScopedVertexAttribArray vertex_attrib_array(0, 2, GL_FLOAT, GL_FALSE,
+                                                   sizeof(GLfloat) * 2, 0);
+  gfx::ScopedCapability disable_blending(GL_BLEND, GL_FALSE);
+  gfx::ScopedCapability disable_culling(GL_CULL_FACE, GL_FALSE);
+  gfx::ScopedCapability disable_dithering(GL_DITHER, GL_FALSE);
+  gfx::ScopedCapability disable_depth_test(GL_DEPTH_TEST, GL_FALSE);
+  gfx::ScopedCapability disable_scissor_test(GL_SCISSOR_TEST, GL_FALSE);
+  gfx::ScopedColorMask color_mask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+
+  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
 
 }  // namespace gfx
