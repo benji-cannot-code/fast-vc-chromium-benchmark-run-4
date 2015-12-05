1e295fb38c1b2884a9b54299626c15dfca70883d
jschuh <jschuh@chromium.org>
2015-12-05 13:41:00 -0800

    Switch wtf/SpinLock to std::atomic
    Tsan identified a racy read in the existing spin lock loop.
    Tsan is correct in the absolute sense, even though the read is
    safe in practice. However, the architectures we support might
    change, or the behavior of the compiler might change, so the
    the best fix is to just use the proper std::atomic type,
    because that is guaranteed to always be safe. And the compiled
    code should be effectively unchanged.
    
    BUG=556469
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/1463683002
    
    Cr-Commit-Position: refs/heads/master@{#363347}

--- third_party/WebKit/Source/web/tests/SpinLockTest.cpp
+++ third_party/WebKit/Source/web/tests/SpinLockTest.cpp
@@ -45,7 +45,7 @@ namespace blink {
 
 static const size_t bufferSize = 16;
 
-static int lock = 0;
+static SpinLock lock;
 
 static void fillBuffer(volatile char* buffer, char fillPattern)
 {
@@ -70,9 +70,8 @@ static void changeAndCheckBuffer(volatile char* buffer)
 static void threadMain(volatile char* buffer)
 {
     for (int i = 0; i < 500000; ++i) {
-        spinLockLock(&lock);
+        SpinLock::Guard guard(lock);
         changeAndCheckBuffer(buffer);
-        spinLockUnlock(&lock);
     }
 }
 
--- third_party/WebKit/Source/wtf/SpinLock.cpp
+++ third_party/WebKit/Source/wtf/SpinLock.cpp
@@ -54,7 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-void slowSpinLockLock(int volatile* lock)
+void SpinLock::lockSlow()
 {
     // The value of kYieldProcessorTries is cargo culted from TCMalloc, Windows
     // critical section defaults, and various other recommendations.
@@ -65,14 +65,14 @@ void slowSpinLockLock(int volatile* lock)
             for (int count = 0; count < kYieldProcessorTries; ++count) {
                 // Let the Processor know we're spinning.
                 YIELD_PROCESSOR;
-                if (!*lock && LIKELY(!atomicTestAndSetToOne(lock)))
+                if (!m_lock.load(std::memory_order_relaxed) && LIKELY(!m_lock.exchange(true, std::memory_order_acq_rel)))
                     return;
             }
 
             // Give the OS a chance to schedule something on this core.
             YIELD_THREAD;
-        } while (*lock);
-    } while (UNLIKELY(atomicTestAndSetToOne(lock)));
+        } while (m_lock.load(std::memory_order_relaxed));
+    } while (UNLIKELY(m_lock.exchange(true, std::memory_order_acq_rel)));
 }
 
 } // namespace WTF
