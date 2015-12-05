552a0448942479f01339fcd649e9fd7b7d2f915c
jrummell <jrummell@chromium.org>
2015-12-04 17:29:10 -0800

    Refactor cdm_helpers
    Splitting the code into 2 parts as the generic part will be used when
    updating cdm_adapter to support Decryptor. The generic code is in
    cdm_helpers, moved up one directory. The Pepper specific part is left
    in ppapi/cdm_helpers.
    
    No changes to the pepper part. However, the compiler requires that
    the new cdm_helpers class not have any code in the header. Since the
    diff most likely won't work, the only code changes are to use the
    PLATFORM_CHECK macro and change the buffer types from PpbBuffer
    to the generic cdm::Buffer.
    
    BUG=510088
    TEST=EME browser_tests pass
    
    Review URL: https://codereview.chromium.org/1497123002
    
    Cr-Commit-Position: refs/heads/master@{#363328}

--- media/cdm/ppapi/ppapi_cdm_buffer.cc
+++ media/cdm/ppapi/ppapi_cdm_buffer.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "media/cdm/ppapi/cdm_helpers.h"
+#include "media/cdm/ppapi/ppapi_cdm_buffer.h"
 
 #include <algorithm>
 
@@ -57,8 +57,7 @@ pp::Buffer_Dev PpbBuffer::TakeBuffer() {
 PpbBuffer::PpbBuffer(pp::Buffer_Dev buffer,
                      uint32_t buffer_id,
                      PpbBufferAllocator* allocator)
-    : buffer_(buffer), buffer_id_(buffer_id), size_(0), allocator_(allocator) {
-}
+    : buffer_(buffer), buffer_id_(buffer_id), size_(0), allocator_(allocator) {}
 
 PpbBuffer::~PpbBuffer() {
   PP_DCHECK(!buffer_id_ == buffer_.is_null());
@@ -129,21 +128,6 @@ pp::Buffer_Dev PpbBufferAllocator::AllocateNewBuffer(uint32_t capacity) {
   // Creation of pp::Buffer_Dev is expensive! It involves synchronous IPC calls.
   // That's why we try to avoid AllocateNewBuffer() as much as we can.
   return pp::Buffer_Dev(instance_, capacity + kBufferPadding);
-}
-
-VideoFrameImpl::VideoFrameImpl()
-    : format_(cdm::kUnknownVideoFormat),
-      frame_buffer_(NULL),
-      timestamp_(0) {
-  for (uint32_t i = 0; i < kMaxPlanes; ++i) {
-    plane_offsets_[i] = 0;
-    strides_[i] = 0;
-  }
-}
-
-VideoFrameImpl::~VideoFrameImpl() {
-  if (frame_buffer_)
-    frame_buffer_->Destroy();
 }
 
 }  // namespace media
