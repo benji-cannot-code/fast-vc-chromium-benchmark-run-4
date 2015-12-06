342824050b039b98ea6e7195195b021cb60d0eac
tfarina <tfarina@chromium.org>
2015-12-06 05:25:41 -0800

    base: get rid of deprecated TrimWhitespace() function
    This patch updates the callers to call TrimWhitespaceASCII() when possible.
    
    BUG=None
    R=brettw@chromium.org
    
    Review URL: https://codereview.chromium.org/1493503002
    
    Cr-Commit-Position: refs/heads/master@{#363369}

--- base/command_line.cc
+++ base/command_line.cc
@@ -76,7 +76,11 @@ void AppendSwitchesAndArguments(CommandLine* command_line,
   bool parse_switches = true;
   for (size_t i = 1; i < argv.size(); ++i) {
     CommandLine::StringType arg = argv[i];
+#if defined(OS_WIN)
     TrimWhitespace(arg, TRIM_ALL, &arg);
+#else
+    TrimWhitespaceASCII(arg, TRIM_ALL, &arg);
+#endif
 
     CommandLine::StringType switch_string;
     CommandLine::StringType switch_value;
@@ -264,7 +268,11 @@ FilePath CommandLine::GetProgram() const {
 }
 
 void CommandLine::SetProgram(const FilePath& program) {
+#if defined(OS_WIN)
   TrimWhitespace(program.value(), TRIM_ALL, &argv_[0]);
+#else
+  TrimWhitespaceASCII(program.value(), TRIM_ALL, &argv_[0]);
+#endif
 }
 
 bool CommandLine::HasSwitch(const base::StringPiece& switch_string) const {
