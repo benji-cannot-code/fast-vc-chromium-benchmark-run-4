c5c63a224144c9c3ca62ac0c4dab93a5c09f9d6f
dgn <dgn@chromium.org>
2015-12-07 09:59:35 -0800

    Allow replacing the HttpAuthHandlerFactory in URLRequestContextBuilder
    Replace add_extra_http_auth_handler_factory by
    set_http_auth_handler_factory, that allows completely replacing
    the factory rather than specifying add-ons to the one created
    by the builder.
    
    BUG=533513
    
    Review URL: https://codereview.chromium.org/1492943002
    
    Cr-Commit-Position: refs/heads/master@{#363509}

--- net/url_request/url_request_context_builder_unittest.cc
+++ net/url_request/url_request_context_builder_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "build/build_config.h"
 #include "net/base/request_priority.h"
+#include "net/http/http_auth_challenge_tokenizer.h"
 #include "net/http/http_auth_handler.h"
 #include "net/http/http_auth_handler_factory.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
@@ -27,8 +28,8 @@ namespace {
 
 class MockHttpAuthHandlerFactory : public HttpAuthHandlerFactory {
  public:
-  explicit MockHttpAuthHandlerFactory(int return_code) :
-      return_code_(return_code) {}
+  MockHttpAuthHandlerFactory(std::string supported_scheme, int return_code)
+      : return_code_(return_code), supported_scheme_(supported_scheme) {}
   ~MockHttpAuthHandlerFactory() override {}
 
   int CreateAuthHandler(HttpAuthChallengeTokenizer* challenge,
@@ -39,11 +40,15 @@ class MockHttpAuthHandlerFactory : public HttpAuthHandlerFactory {
                         const BoundNetLog& net_log,
                         scoped_ptr<HttpAuthHandler>* handler) override {
     handler->reset();
-    return return_code_;
+
+    return challenge->scheme() == supported_scheme_
+               ? return_code_
+               : ERR_UNSUPPORTED_AUTH_SCHEME;
   }
 
  private:
   int return_code_;
+  std::string supported_scheme_;
 };
 
 class URLRequestContextBuilderTest : public PlatformTest {
@@ -90,15 +95,25 @@ TEST_F(URLRequestContextBuilderTest, UserAgent) {
   EXPECT_EQ("Bar", delegate.data_received());
 }
 
-TEST_F(URLRequestContextBuilderTest, ExtraHttpAuthHandlerFactory) {
+TEST_F(URLRequestContextBuilderTest, DefaultHttpAuthHandlerFactory) {
+  GURL gurl("www.google.com");
+  scoped_ptr<HttpAuthHandler> handler;
+  scoped_ptr<URLRequestContext> context(builder_.Build());
+
+  // Verify that the default basic handler is present
+  EXPECT_EQ(OK,
+            context->http_auth_handler_factory()->CreateAuthHandlerFromString(
+                "basic", HttpAuth::AUTH_SERVER, gurl, BoundNetLog(), &handler));
+}
+
+TEST_F(URLRequestContextBuilderTest, CustomHttpAuthHandlerFactory) {
   GURL gurl("www.google.com");
   const int kBasicReturnCode = OK;
-  MockHttpAuthHandlerFactory* mock_factory_basic =
-      new MockHttpAuthHandlerFactory(kBasicReturnCode);
   scoped_ptr<HttpAuthHandler> handler;
-  builder_.add_http_auth_handler_factory("ExtraScheme", mock_factory_basic);
+  builder_.SetHttpAuthHandlerFactory(make_scoped_ptr(
+      new MockHttpAuthHandlerFactory("ExtraScheme", kBasicReturnCode)));
   scoped_ptr<URLRequestContext> context(builder_.Build());
-  // Verify that a handler is returned for and added scheme.
+  // Verify that a handler is returned for a custom scheme.
   EXPECT_EQ(kBasicReturnCode,
             context->http_auth_handler_factory()->CreateAuthHandlerFromString(
                 "ExtraScheme",
@@ -106,6 +121,12 @@ TEST_F(URLRequestContextBuilderTest, ExtraHttpAuthHandlerFactory) {
                 gurl,
                 BoundNetLog(),
                 &handler));
+
+  // Verify that the default basic handler isn't present
+  EXPECT_EQ(ERR_UNSUPPORTED_AUTH_SCHEME,
+            context->http_auth_handler_factory()->CreateAuthHandlerFromString(
+                "basic", HttpAuth::AUTH_SERVER, gurl, BoundNetLog(), &handler));
+
   // Verify that a handler isn't returned for a bogus scheme.
   EXPECT_EQ(ERR_UNSUPPORTED_AUTH_SCHEME,
             context->http_auth_handler_factory()->CreateAuthHandlerFromString(
