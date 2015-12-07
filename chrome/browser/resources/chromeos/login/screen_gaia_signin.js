0dfe113395723bc2f4eb611f84c3bce96517fb46
dzhioev <dzhioev@chromium.org>
2015-12-07 14:52:40 -0800

    Remove unused GAIA params.
    BUG=470893
    
    Review URL: https://codereview.chromium.org/1492733003
    
    Cr-Commit-Position: refs/heads/master@{#363612}

--- chrome/browser/resources/chromeos/login/screen_gaia_signin.js
+++ chrome/browser/resources/chromeos/login/screen_gaia_signin.js
@@ -138,13 +138,6 @@ login.createScreen('GaiaSigninScreen', 'gaia-signin', function() {
     },
 
     /**
-     * Whether we should show user pods on the login screen.
-     * @type {boolean}
-     * @private
-     */
-    isShowUsers_: undefined,
-
-    /**
      * SAML password confirmation attempt count.
      * @type {number}
      */
@@ -500,9 +493,6 @@ login.createScreen('GaiaSigninScreen', 'gaia-signin', function() {
       params.chromeType = data.chromeType;
       params.isNewGaiaFlow = true;
 
-      if (data.gaiaEndpoint)
-        params.gaiaPath = data.gaiaEndpoint;
-
       // Screen size could have been changed because of 'full-width' classes.
       if (Oobe.getInstance().currentScreen === this)
         Oobe.getInstance().updateScreenSize(this);
@@ -533,7 +523,6 @@ login.createScreen('GaiaSigninScreen', 'gaia-signin', function() {
           data.supervisedUsersCanCreate;
       $('login-header-bar').showGuestButton = data.guestSignin;
 
-      this.isShowUsers_ = data.isShowUsers;
       this.updateControlsState();
 
       this.classList.toggle('full-width', false);
