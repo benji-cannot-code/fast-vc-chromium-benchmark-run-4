e171d35679477b25f6b56ff5508e035af972a180
timloh <timloh@chromium.org>
2015-12-06 23:56:07 -0800

    Move -webkit-perspective-origin-x,y and -webkit-transform-origin-x,y,z across to CSSPropertyParser
    This patch moves the properties -webkit-perspective-origin-x,y and
    -webkit-transform-origin-x,y,z out of the LegacyCSSProperyParser and
    to the CSSPropertyParser. The logic for parsing the x and y components
    of position types is still needed in the old parser for the
    background-position-x,y properties.
    
    BUG=499780
    
    Review URL: https://codereview.chromium.org/1498643004
    
    Cr-Commit-Position: refs/heads/master@{#363406}

--- third_party/WebKit/Source/core/css/parser/LegacyCSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/LegacyCSSPropertyParser.cpp
@@ -569,21 +569,6 @@ bool CSSPropertyParser::parseValue(CSSPropertyID unresolvedProperty, bool import
         validPrimitive = validUnit(value, FInteger);
         break;
 
-    case CSSPropertyWebkitPerspectiveOriginX:
-    case CSSPropertyWebkitTransformOriginX:
-        parsedValue = parseFillPositionX(m_valueList);
-        if (parsedValue)
-            m_valueList->next();
-        break;
-    case CSSPropertyWebkitPerspectiveOriginY:
-    case CSSPropertyWebkitTransformOriginY:
-        parsedValue = parseFillPositionY(m_valueList);
-        if (parsedValue)
-            m_valueList->next();
-        break;
-    case CSSPropertyWebkitTransformOriginZ:
-        validPrimitive = validUnit(value, FLength);
-        break;
     case CSSPropertyPerspective:
         if (id == CSSValueNone) {
             validPrimitive = true;
@@ -766,6 +751,11 @@ bool CSSPropertyParser::parseValue(CSSPropertyID unresolvedProperty, bool import
         break;
 
     // These were not accepted by the new path above so we should return false.
+    case CSSPropertyWebkitPerspectiveOriginX:
+    case CSSPropertyWebkitTransformOriginX:
+    case CSSPropertyWebkitPerspectiveOriginY:
+    case CSSPropertyWebkitTransformOriginY:
+    case CSSPropertyWebkitTransformOriginZ:
     case CSSPropertyWebkitMarginCollapse:
     case CSSPropertyWillChange:
     case CSSPropertyPage:
