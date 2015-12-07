c5baf2afb900d75be1be510b4d4c8169aa438e00
flackr <flackr@chromium.org>
2015-12-07 07:43:05 -0800

    Replace multi-value keyboard flags with single value flag types.
    Use the new disable value flags to replace multi-value keyboard flags. This has
    the property that the multi-value flag had (can change without a string update)
    and makes it clear to the user which value is default.
    
    BUG=245319
    
    Review URL: https://codereview.chromium.org/1488303005
    
    Cr-Commit-Position: refs/heads/master@{#363484}

--- ash/virtual_keyboard_controller_unittest.cc
+++ ash/virtual_keyboard_controller_unittest.cc
@@ -52,8 +52,8 @@ class VirtualKeyboardControllerTest : public AshTestBase {
   }
 
   void SetUp() override {
-    base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
-        keyboard::switches::kSmartVirtualKeyboard, "disabled");
+    base::CommandLine::ForCurrentProcess()->AppendSwitch(
+        keyboard::switches::kDisableSmartVirtualKeyboard);
     AshTestBase::SetUp();
     UpdateKeyboardDevices(std::vector<ui::KeyboardDevice>());
     UpdateTouchscreenDevices(std::vector<ui::TouchscreenDevice>());
