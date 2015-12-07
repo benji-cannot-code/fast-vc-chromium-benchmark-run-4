283105ae7cd8ac0ae6b27439854b46f1a6a099d7
davve <davve@opera.com>
2015-12-07 09:22:09 -0800

    Disallow [Measure] and [MeasureAs] on interfaces without a constructor
    For the instances where MeasureAs was specified on interfaces without
    a constructor, simply remove the MeasureAs. Since they can't be
    created by script, it seems more worthwhile want to measure where
    these object are returned rather than the use of the interfaces
    themselves.
    
    The only change in generated code will be less includes of
    core/frame/UseCounter.h, as can be seen in the binding testsuite
    update.
    
    BUG=415074, 567015
    
    Review URL: https://codereview.chromium.org/1509493002
    
    Cr-Commit-Position: refs/heads/master@{#363503}

--- third_party/WebKit/Source/bindings/tests/results/modules/V8TestInterfacePartial.cpp
+++ third_party/WebKit/Source/bindings/tests/results/modules/V8TestInterfacePartial.cpp
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/ContextFeatures.h"
 #include "core/dom/Document.h"
 #include "core/frame/LocalFrame.h"
-#include "core/frame/UseCounter.h"
 #include "platform/RuntimeEnabledFeatures.h"
 #include "platform/ScriptForbiddenScope.h"
 #include "platform/TraceEvent.h"
