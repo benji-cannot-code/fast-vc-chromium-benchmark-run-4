1c6086d080a1057360f808584aa49123bad66d45
davve <davve@opera.com>
2015-12-07 05:27:06 -0800

    Teach SVGImageForContainer::imageForCurrentFrame about the URL
    Pass URL from SVGImageForContainer to SVGImage when fetching an
    snapshot for current frame.
    
    BUG=565282
    
    Review URL: https://codereview.chromium.org/1498683003
    
    Cr-Commit-Position: refs/heads/master@{#363466}

--- third_party/WebKit/Source/core/svg/graphics/SVGImageForContainer.cpp
+++ third_party/WebKit/Source/core/svg/graphics/SVGImageForContainer.cpp
@@ -50,7 +50,7 @@ void SVGImageForContainer::drawPattern(GraphicsContext* context, const FloatRect
 
 PassRefPtr<SkImage> SVGImageForContainer::imageForCurrentFrame()
 {
-    return m_image->imageForCurrentFrame();
+    return m_image->imageForCurrentFrameForContainer(m_url);
 }
 
 } // namespace blink
