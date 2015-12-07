bc93ec1241362a344251c976b4991d1378624031
mmenke <mmenke@chromium.org>
2015-12-07 10:20:18 -0800

    Add integration tests for HttpStreamParser::CanReuseConnection logic.
    This requires modifying a fair number of other tests and test
    infrastructure to safely handle SocketDataProviders that are destroyed
    before the mock socket that uses them.
    
    BUG=565582,544255
    
    Review URL: https://codereview.chromium.org/1499073002
    
    Cr-Commit-Position: refs/heads/master@{#363516}

--- net/spdy/spdy_network_transaction_unittest.cc
+++ net/spdy/spdy_network_transaction_unittest.cc
@@ -902,7 +902,8 @@ TEST_P(SpdyNetworkTransactionTest, ThreeGets) {
       MockRead(ASYNC, 0, 12),  // EOF
   };
   SequencedSocketData data(reads, arraysize(reads), writes, arraysize(writes));
-  SequencedSocketData data_placeholder(NULL, 0, NULL, 0);
+  SequencedSocketData data_placeholder1(NULL, 0, NULL, 0);
+  SequencedSocketData data_placeholder2(NULL, 0, NULL, 0);
 
   BoundNetLog log;
   TransactionHelperResult out;
@@ -913,8 +914,8 @@ TEST_P(SpdyNetworkTransactionTest, ThreeGets) {
   // We require placeholder data because three get requests are sent out at
   // the same time which results in three sockets being connected. The first
   // on will negotiate SPDY and will be used for all requests.
-  helper.AddData(&data_placeholder);
-  helper.AddData(&data_placeholder);
+  helper.AddData(&data_placeholder1);
+  helper.AddData(&data_placeholder2);
   scoped_ptr<HttpNetworkTransaction> trans1(
       new HttpNetworkTransaction(DEFAULT_PRIORITY, helper.session()));
   scoped_ptr<HttpNetworkTransaction> trans2(
@@ -3551,7 +3552,6 @@ TEST_P(SpdyNetworkTransactionTest, DecompressFailureOnSynReply) {
   helper.RunToCompletion(&data);
   TransactionHelperResult out = helper.output();
   EXPECT_EQ(ERR_SPDY_COMPRESSION_ERROR, out.rv);
-  data.Reset();
 }
 
 // Test that the NetLog contains good data for a simple GET request.
