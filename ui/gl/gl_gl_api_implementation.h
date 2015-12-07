4e09eb28fc42eedfd6cc80bb88c068833b208075
dyen <dyen@chromium.org>
2015-12-07 14:17:36 -0800

    Fix disabled extensions for virtual contexts and respect command line.
    This CL fixes an issue with disabled extensions not working on devices
    which use glGetStringi to get the extensions. It also now merges
    extensions disabled on the command line instead of being overwritten by
    extensions disabled in the bug workaround list.
    
    R=zmo@chromium.org, sievers@chromium.org
    BUG=554909
    
    Review URL: https://codereview.chromium.org/1497863002
    
    Cr-Commit-Position: refs/heads/master@{#363600}

--- ui/gl/gl_gl_api_implementation.h
+++ ui/gl/gl_gl_api_implementation.h
@@ -125,11 +125,13 @@ class VirtualGLApi : public GLApiBase {
 
   void OnReleaseVirtuallyCurrent(GLContext* virtual_context);
 
-private:
+ private:
   // Overridden functions from GLApiBase
- const GLubyte* glGetStringFn(GLenum name) override;
- void glFinishFn() override;
- void glFlushFn() override;
+  void glGetIntegervFn(GLenum pname, GLint* params) override;
+  const GLubyte* glGetStringFn(GLenum name) override;
+  const GLubyte* glGetStringiFn(GLenum name, GLuint index) override;
+  void glFinishFn() override;
+  void glFlushFn() override;
 
   // The real context we're running on.
   GLContext* real_context_;
@@ -139,6 +141,7 @@ private:
 
   // The supported extensions being advertised for this virtual context.
   std::string extensions_;
+  std::vector<std::string> extensions_vec_;
 };
 
 }  // namespace gfx
