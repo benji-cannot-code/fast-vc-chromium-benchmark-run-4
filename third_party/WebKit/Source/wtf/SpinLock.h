afdfc1c9f5fbae0575284ab27f51a64f14b972fe
Nico Weber <thakis@chromium.org>
2015-12-07 15:25:19 -0500

    Remove a few static initializers on Windows.
    atomic<int> causes a static initializer when used in a global object
    (like SpinLock is every now and then), because MSVS2013 doesn't have
    constexpr yet.
    
    However, atomic_int doesn't, so use that.
    
    BUG=566751
    R=hans@chromium.org
    
    Review URL: https://codereview.chromium.org/1507773002 .
    
    Cr-Commit-Position: refs/heads/master@{#363566}

--- third_party/WebKit/Source/wtf/SpinLock.h
+++ third_party/WebKit/Source/wtf/SpinLock.h
@@ -68,7 +68,7 @@ private:
     // slower, but has a much better scheduling and power consumption behavior.
     WTF_EXPORT void lockSlow();
 
-    std::atomic<int> m_lock;
+    std::atomic_int m_lock;
 };
 
 
