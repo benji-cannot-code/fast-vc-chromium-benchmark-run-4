8a6205d3ec885ea28322c5618f90edfb01421636
msramek <msramek@chromium.org>
2015-12-07 10:13:07 -0800

    Remove the cleanup code after the content settings migration.
    After the content settings migration (from all settings synced to some synced and some local), we left a cleanup code to delete the old (now unused) preferences.
    
    Three milestones have passed, and it should now be OK to remove the cleanup code (and reduce the code complexity).
    
    BUG=452388
    
    Review URL: https://codereview.chromium.org/1489033002
    
    Cr-Commit-Position: refs/heads/master@{#363513}

--- components/content_settings/core/browser/content_settings_pref_provider.cc
+++ components/content_settings/core/browser/content_settings_pref_provider.cc
@@ -33,11 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Obsolete prefs.
-// TODO(msramek): Remove the cleanup code after two releases (i.e. in M48).
-const char kObsoleteContentSettingsPatternPairs[] =
-    "profile.content_settings.pattern_pairs";
-const char kObsoleteMigratedContentSettingsPatternPairs[] =
-    "profile.migrated_content_settings_exceptions";
 // TODO(msramek): Remove the cleanup code after two releases (i.e. in M50).
 const char kObsoleteMetroSwitchToDesktopExceptions[] =
     "profile.content_settings.exceptions.metro_switch_to_desktop";
@@ -66,11 +61,6 @@ void PrefProvider::RegisterProfilePrefs(
 
   // Obsolete prefs ----------------------------------------------------------
 
-  registry->RegisterDictionaryPref(
-      kObsoleteContentSettingsPatternPairs,
-      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
-  registry->RegisterBooleanPref(kObsoleteMigratedContentSettingsPatternPairs,
-                                false);
   registry->RegisterDictionaryPref(
       kObsoleteMetroSwitchToDesktopExceptions,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
@@ -206,8 +196,6 @@ void PrefProvider::Notify(
 }
 
 void PrefProvider::DiscardObsoletePreferences() {
-  prefs_->ClearPref(kObsoleteContentSettingsPatternPairs);
-  prefs_->ClearPref(kObsoleteMigratedContentSettingsPatternPairs);
   prefs_->ClearPref(kObsoleteMetroSwitchToDesktopExceptions);
 }
 
