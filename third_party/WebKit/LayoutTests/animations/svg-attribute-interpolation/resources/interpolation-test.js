c293fd58dc329a751242d036d4d604eea1706fdd
alancutter <alancutter@chromium.org>
2015-12-06 20:13:20 -0800

    Web Animations: Make SVG animation attributes require an "svg-" prefix
    This change makes SVG animations using the Web Animations API require
    SVG attributes be prefixed with "svg-" to avoid name collisions with CSS
    properties.
    
    Previously we would require a camelcasing "svg" prefix e.g. "svgAttribute-name".
    
    BUG=547646
    
    Review URL: https://codereview.chromium.org/1407373004
    
    Cr-Commit-Position: refs/heads/master@{#363398}

--- third_party/WebKit/LayoutTests/animations/svg-attribute-interpolation/resources/interpolation-test.js
+++ third_party/WebKit/LayoutTests/animations/svg-attribute-interpolation/resources/interpolation-test.js
@@ -321,8 +321,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         }
         break;
       case 'Web Animations':
-        // Replace 'transform' with 'svgTransform', etc. This avoids collisions with CSS properties or the Web Animations API (offset).
-        var prefixedProperty = 'svg' + params.property[0].toUpperCase() + params.property.slice(1);
+        // Replace 'transform' with 'svg-transform', etc. This avoids collisions with CSS properties or the Web Animations API (offset).
+        var prefixedProperty = 'svg-' + params.property;
         var keyframes = [];
         if (!isNeutralKeyframe(params.from)) {
           keyframes.push({
