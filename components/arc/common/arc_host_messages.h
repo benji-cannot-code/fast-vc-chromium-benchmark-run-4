29bf718fe24191572a8c8fb2ac1be944c44ee520
ejcaruso <ejcaruso@chromium.org>
2015-12-07 11:25:03 -0800

    libarcbridge: add IPC message types for wake locks
    We're going to need these messages so Android apps can request
    wake locks from powerd on the Chrome OS side.
    
    BUG=b:24671115
    TEST=make
    
    Review URL: https://codereview.chromium.org/1488293002
    
    Cr-Commit-Position: refs/heads/master@{#363543}

--- components/arc/common/arc_host_messages.h
+++ components/arc/common/arc_host_messages.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IPC_MESSAGE_START ArcInstanceHostMsgStart
 
 IPC_ENUM_TRAITS_MAX_VALUE(arc::InstanceBootPhase, arc::InstanceBootPhase::LAST)
+IPC_ENUM_TRAITS_MAX_VALUE(arc::DisplayWakeLockType,
+                          arc::DisplayWakeLockType::LAST)
 
 IPC_MESSAGE_CONTROL1(ArcInstanceHostMsg_InstanceBootPhase,
                      arc::InstanceBootPhase)
@@ -50,3 +52,9 @@ IPC_MESSAGE_CONTROL1(ArcInstanceHostMsg_NotificationPosted,
 // |key| is the identifier of the notification.
 IPC_MESSAGE_CONTROL1(ArcInstanceHostMsg_NotificationRemoved,
                      std::string /* key */);
+
+// Acquire and release wake locks on the host side.
+IPC_MESSAGE_CONTROL1(ArcInstanceHostMsg_AcquireDisplayWakeLock,
+                     arc::DisplayWakeLockType /* type */)
+IPC_MESSAGE_CONTROL1(ArcInstanceHostMsg_ReleaseDisplayWakeLock,
+                     arc::DisplayWakeLockType /* type */)
