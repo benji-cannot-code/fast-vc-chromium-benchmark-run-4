09befbdae010b03bb42ab4e27a0149702375eab1
piman <piman@chromium.org>
2015-12-07 13:10:59 -0800

    Remove --show-fps-counter and about:flags entry, and --ui-show-fps-counter
    BUG=359328
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1504533002
    
    Cr-Commit-Position: refs/heads/master@{#363582}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1453,7 +1453,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     cc::switches::kEnableGpuBenchmarking,
     cc::switches::kEnableMainFrameBeforeActivation,
     cc::switches::kShowCompositedLayerBorders,
-    cc::switches::kShowFPSCounter,
     cc::switches::kShowLayerAnimationBounds,
     cc::switches::kShowPropertyChangedRects,
     cc::switches::kShowReplicaScreenSpaceRects,
--- content/browser/resources/gpu/browser_bridge_tests.js
+++ content/browser/resources/gpu/browser_bridge_tests.js
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 var commandLineFlags = ['--flag-switches-begin',
                         '--show-composited-layer-borders',
-                        '--show-fps-counter',
                         '--flag-switches-end'];
 var commandLineStr = './out/Debug/chrome ' + commandLineFlags.join(' ');
 
