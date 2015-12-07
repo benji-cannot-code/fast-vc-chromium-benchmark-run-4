dee0f058f5db08baffead97705f17a28e12bf4dc
alexander.shalamov <alexander.shalamov@intel.com>
2015-12-07 05:22:10 -0800

    [webnfc] Add public interfaces.
    Add public interfaces for embedder.
    WebNFCClient.h
    WebNFCError.h
    WebNFCMessage.h
    WebNFCObserver.h
    WebNFCPushOptions.h
    WebNFCPushTarget.h
    WebNFCWatchOptions.h
    
    BUG=520391
    
    Review URL: https://codereview.chromium.org/1481763005
    
    Cr-Commit-Position: refs/heads/master@{#363465}

---
+++ third_party/WebKit/public/platform/modules/nfc/WebNFCPushOptions.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WebNFCPushOptions_h
+#define WebNFCPushOptions_h
+
+#include "public/platform/modules/nfc/WebNFCPushTarget.h"
+
+namespace blink {
+
+// Contains members of NFCPushOption dictionary as specified in the IDL.
+struct WebNFCPushOptions {
+    WebNFCPushOptions(const WebNFCPushTarget& target = WebNFCPushTarget::Any, double timeout = std::numeric_limits<double>::infinity(), bool ignoreRead = true)
+        : target(target), timeout(timeout), ignoreRead(ignoreRead)
+    {
+    }
+
+    WebNFCPushTarget target;
+    double timeout; // in milliseconds
+    bool ignoreRead;
+};
+
+} // namespace blink
+
+#endif // WebNFCPushOptions_h
