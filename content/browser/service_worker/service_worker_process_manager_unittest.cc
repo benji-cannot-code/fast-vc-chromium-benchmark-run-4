7180c7afa85333d25ff09aca6364a675da14ee3f
nhiroki <nhiroki@chromium.org>
2015-12-07 00:43:50 -0800

    ServiceWorker: Abort process allocation when ServiceWorkerProcessManager is in shutdown
    Before this CL, ServiceWorkerProcessManager::AllocateWorkerProcess() returns an
    existing process w/o checking its shutdown state. After this CL, the function
    respects the state and aborts process allocation when in shutdown.
    
    BUG=n/a
    
    Review URL: https://codereview.chromium.org/1502123002
    
    Cr-Commit-Position: refs/heads/master@{#363423}

--- content/browser/service_worker/service_worker_process_manager_unittest.cc
+++ content/browser/service_worker/service_worker_process_manager_unittest.cc
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/basictypes.h"
+#include "base/bind.h"
+#include "base/run_loop.h"
 #include "content/browser/service_worker/service_worker_process_manager.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -12,6 +14,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+namespace {
+
+void DidAllocateWorkerProcess(const base::Closure& quit_closure,
+                              ServiceWorkerStatusCode* status_out,
+                              int* process_id_out,
+                              bool* is_new_process_out,
+                              ServiceWorkerStatusCode status,
+                              int process_id,
+                              bool is_new_process) {
+  *status_out = status;
+  *process_id_out = process_id;
+  *is_new_process_out = is_new_process;
+  quit_closure.Run();
+}
+
+}  // namespace
+
 class ServiceWorkerProcessManagerTest : public testing::Test {
  public:
   ServiceWorkerProcessManagerTest() {}
@@ -19,6 +38,7 @@ class ServiceWorkerProcessManagerTest : public testing::Test {
   void SetUp() override {
     process_manager_.reset(new ServiceWorkerProcessManager(NULL));
     pattern_ = GURL("http://www.example.com/");
+    script_url_ = GURL("http://www.example.com/sw.js");
   }
 
   void TearDown() override { process_manager_.reset(); }
@@ -26,6 +46,7 @@ class ServiceWorkerProcessManagerTest : public testing::Test {
  protected:
   scoped_ptr<ServiceWorkerProcessManager> process_manager_;
   GURL pattern_;
+  GURL script_url_;
 
  private:
   content::TestBrowserThreadBundle thread_bundle_;
@@ -50,6 +71,26 @@ TEST_F(ServiceWorkerProcessManagerTest, SortProcess) {
   // Scores for each process: 2 : 3, 3 : 1, process 1 is removed.
   EXPECT_THAT(process_manager_->SortProcessesForPattern(pattern_),
               testing::ElementsAre(2, 3));
+}
+
+TEST_F(ServiceWorkerProcessManagerTest, AllocateWorkerProcess_InShutdown) {
+  ASSERT_TRUE(process_manager_->IsShutdown());
+
+  base::RunLoop run_loop;
+  ServiceWorkerStatusCode status = SERVICE_WORKER_ERROR_MAX_VALUE;
+  int process_id = -10;
+  bool is_new_process = true;
+  process_manager_->AllocateWorkerProcess(
+      1, pattern_, script_url_,
+      base::Bind(&DidAllocateWorkerProcess, run_loop.QuitClosure(), &status,
+                 &process_id, &is_new_process));
+  run_loop.Run();
+
+  // Allocating a process in shutdown should abort.
+  EXPECT_EQ(SERVICE_WORKER_ERROR_ABORT, status);
+  EXPECT_EQ(-1, process_id);
+  EXPECT_FALSE(is_new_process);
+  EXPECT_TRUE(process_manager_->instance_info_.empty());
 }
 
 }  // namespace content
