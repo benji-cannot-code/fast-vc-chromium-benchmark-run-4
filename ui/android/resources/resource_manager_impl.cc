a204f96e195e542e2ab56da641aea0a2783dadf3
torne <torne@chromium.org>
2015-12-07 04:49:14 -0800

    Pass method parameters as JavaParamRef in ui.
    Pass all object parameters to JNI methods in JavaParamRef<> wrappers.
    This matches previous changes made to do this for JNI non-method
    functions.
    
    BUG=506850
    TBR=qinmin@chromium.org
    
    Review URL: https://codereview.chromium.org/1494173002
    
    Cr-Commit-Position: refs/heads/master@{#363460}

--- ui/android/resources/resource_manager_impl.cc
+++ ui/android/resources/resource_manager_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 
 using base::android::JavaArrayOfIntArrayToIntVector;
+using base::android::JavaRef;
 
 namespace ui {
 
@@ -85,10 +86,10 @@ void ResourceManagerImpl::PreloadResource(AndroidResourceType res_type,
 }
 
 void ResourceManagerImpl::OnResourceReady(JNIEnv* env,
-                                          jobject jobj,
+                                          const JavaRef<jobject>& jobj,
                                           jint res_type,
                                           jint res_id,
-                                          jobject bitmap,
+                                          const JavaRef<jobject>& bitmap,
                                           jint padding_left,
                                           jint padding_top,
                                           jint padding_right,
@@ -109,7 +110,7 @@ void ResourceManagerImpl::OnResourceReady(JNIEnv* env,
     resources_[res_type].AddWithID(resource, res_id);
   }
 
-  gfx::JavaBitmap jbitmap(bitmap);
+  gfx::JavaBitmap jbitmap(bitmap.obj());
   resource->size = jbitmap.size();
   resource->padding.SetRect(padding_left, padding_top,
                             padding_right - padding_left,
@@ -140,24 +141,24 @@ CrushedSpriteResource* ResourceManagerImpl::GetCrushedSpriteResource(
 
 void ResourceManagerImpl::OnCrushedSpriteResourceReady(
     JNIEnv* env,
-    jobject jobj,
+    const JavaRef<jobject>& jobj,
     jint bitmap_res_id,
-    jobject bitmap,
-    jobjectArray frame_rects,
+    const JavaRef<jobject>& bitmap,
+    const JavaRef<jobjectArray>& frame_rects,
     jint unscaled_sprite_width,
     jint unscaled_sprite_height,
     jfloat scaled_sprite_width,
     jfloat scaled_sprite_height) {
-
   // Construct source and destination rectangles for each frame from
   // |frame_rects|.
   std::vector<std::vector<int>> all_frame_rects_vector;
-  JavaArrayOfIntArrayToIntVector(env, frame_rects, &all_frame_rects_vector);
+  JavaArrayOfIntArrayToIntVector(env, frame_rects.obj(),
+                                 &all_frame_rects_vector);
   CrushedSpriteResource::SrcDstRects src_dst_rects =
       ProcessCrushedSpriteFrameRects(all_frame_rects_vector);
 
   SkBitmap skbitmap =
-      gfx::CreateSkBitmapFromJavaBitmap(gfx::JavaBitmap(bitmap));
+      gfx::CreateSkBitmapFromJavaBitmap(gfx::JavaBitmap(bitmap.obj()));
 
   CrushedSpriteResource* resource = new CrushedSpriteResource(
       skbitmap,
@@ -203,9 +204,9 @@ ResourceManagerImpl::ProcessCrushedSpriteFrameRects(
 
 void ResourceManagerImpl::OnCrushedSpriteResourceReloaded(
     JNIEnv* env,
-    jobject jobj,
+    const JavaRef<jobject>& jobj,
     jint bitmap_res_id,
-    jobject bitmap) {
+    const JavaRef<jobject>& bitmap) {
   CrushedSpriteResource* resource =
       crushed_sprite_resources_.Lookup(bitmap_res_id);
   if (!resource) {
@@ -213,7 +214,7 @@ void ResourceManagerImpl::OnCrushedSpriteResourceReloaded(
     return;
   }
   SkBitmap skbitmap =
-      gfx::CreateSkBitmapFromJavaBitmap(gfx::JavaBitmap(bitmap));
+      gfx::CreateSkBitmapFromJavaBitmap(gfx::JavaBitmap(bitmap.obj()));
   resource->SetBitmap(skbitmap);
 }
 
