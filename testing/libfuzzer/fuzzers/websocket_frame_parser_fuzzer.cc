af08704cc11202fff4c67545a7e03046dccb4a56
mmoroz <mmoroz@chromium.org>
2015-12-07 17:26:23 -0800

    fuzzer for WebSocketFrameParser
    R=inferno@chromium.org
    TBR=krasin@chromium.org
    BUG=566504
    
    Review URL: https://codereview.chromium.org/1504073002
    
    Cr-Commit-Position: refs/heads/master@{#363686}

---
+++ testing/libfuzzer/fuzzers/websocket_frame_parser_fuzzer.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <vector>
+
+#include "net/websockets/websocket_frame_parser.h"
+
+// Entry point for LibFuzzer.
+extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
+  net::WebSocketFrameParser parser;
+  std::vector<scoped_ptr<net::WebSocketFrameChunk>> frame_chunks;
+  parser.Decode(reinterpret_cast<const char*>(data), size, &frame_chunks);
+
+  return 0;
+}
