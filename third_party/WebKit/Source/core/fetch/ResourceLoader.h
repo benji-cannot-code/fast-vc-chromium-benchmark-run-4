c69879468db4ad83ad99c61689d0871faf2d0120
yoav <yoav@yoav.ws>
2015-12-07 16:51:12 -0800

    Add asserts making sure that ResourceLoader is properly initialized
    While going over the code I noticed that ResourceLoader assumes that it
    got a non-null Resource as well as a ResourceFetcher, without verifying that.
    This CL adds ASSERTs that make this assumption explicit.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1508693002
    
    Cr-Commit-Position: refs/heads/master@{#363642}

--- third_party/WebKit/Source/core/fetch/ResourceLoader.h
+++ third_party/WebKit/Source/core/fetch/ResourceLoader.h
@@ -91,6 +91,7 @@ public:
     bool loadingMultipartContent() const { return m_loadingMultipartContent; }
 
 private:
+    // Assumes ResourceFetcher and Resource are non-null.
     ResourceLoader(ResourceFetcher*, Resource*, const ResourceLoaderOptions&);
 
     void init(const ResourceRequest&);
