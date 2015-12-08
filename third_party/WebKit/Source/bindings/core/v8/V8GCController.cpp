810adff7bffa644d695469f161ca7daf30aae956
haraken <haraken@chromium.org>
2015-12-07 18:53:33 -0800

    Oilapn: Re-enable ThreadState::current()->willStartV8GC() in a GC callback
    - It is important to enable the ThreadState::current()->willStartV8GC()
    because this affects Oilpan's performance and Oilpan is going to be shipped
    very soon.
    
    - It is safe to enable the ThreadState::current()->willStartV8GC()
    because we've already removed code that modifies V8 object's layout
    in V8AbstractEventListener's destructor.
    
    - It is not nice to enable the ThreadState::current()->willStartV8GC()
    because it invokes destructors in GC callbacks. So I added TODOs.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1505663002
    
    Cr-Commit-Position: refs/heads/master@{#363718}

--- third_party/WebKit/Source/bindings/core/v8/V8GCController.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8GCController.cpp
@@ -285,12 +285,13 @@ void V8GCController::gcPrologue(v8::Isolate* isolate, v8::GCType type, v8::GCCal
     if (isMainThread())
         ScriptForbiddenScope::enter();
 
-    // TODO(haraken): It is not safe to run finalizers in a prologue callback
-    // because V8AbstractEventListener's destructor cann call into V8. We
-    // should post a task to schedule willStartV8GC() and avoid running it
-    // inside the prologue callback.
-    // if (ThreadState::current())
-    //     ThreadState::current()->willStartV8GC();
+    // TODO(haraken): A GC callback is not allowed to re-enter V8. This means
+    // that it's unsafe to run Oilpan's GC in the GC callback because it may
+    // run finalizers that call into V8. To avoid the risk, we should post
+    // a task to schedule the Oilpan's GC.
+    // (In practice, there is no finalizer that calls into V8 and thus is safe.)
+    if (ThreadState::current())
+        ThreadState::current()->willStartV8GC();
 
     v8::HandleScope scope(isolate);
     switch (type) {
@@ -333,6 +334,7 @@ void V8GCController::gcEpilogue(v8::Isolate* isolate, v8::GCType type, v8::GCCal
         if (isMainThread()) {
             TRACE_EVENT_SET_NONCONST_SAMPLING_STATE(V8PerIsolateData::from(isolate)->previousSamplingState());
         }
+        // TODO(haraken): Remove this. See the comment in gcPrologue.
         if (ThreadState::current())
             ThreadState::current()->scheduleV8FollowupGCIfNeeded(BlinkGC::V8MinorGC);
         break;
@@ -353,6 +355,7 @@ void V8GCController::gcEpilogue(v8::Isolate* isolate, v8::GCType type, v8::GCCal
         if (isMainThread()) {
             TRACE_EVENT_SET_NONCONST_SAMPLING_STATE(V8PerIsolateData::from(isolate)->previousSamplingState());
         }
+        // TODO(haraken): Remove this. See the comment in gcPrologue.
         if (ThreadState::current())
             ThreadState::current()->scheduleV8FollowupGCIfNeeded(BlinkGC::V8MajorGC);
         break;
