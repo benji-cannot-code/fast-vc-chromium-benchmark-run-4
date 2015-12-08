66c3348f8ac4e7210989c86826876a5f213f41cc
reveman <reveman@chromium.org>
2015-12-07 21:34:05 -0800

    exo: Initial Wayland input support.
    This adds support for pointer events to Exosphere and the
    Wayland bindings. Key and touch events will be implemented
    added by follow up patches.
    
    BUG=549781
    TEST=exo_unittest --gtest_filter=PointerTest.*
    
    Review URL: https://codereview.chromium.org/1495393002
    
    Cr-Commit-Position: refs/heads/master@{#363749}

--- components/exo/surface.cc
+++ components/exo/surface.cc
@@ -14,14 +14,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/surface_delegate.h"
 #include "components/exo/surface_observer.h"
 #include "ui/aura/window_delegate.h"
+#include "ui/aura/window_property.h"
 #include "ui/base/cursor/cursor.h"
 #include "ui/base/hit_test.h"
 #include "ui/compositor/layer.h"
 #include "ui/gfx/buffer_format_util.h"
 #include "ui/gfx/gpu_memory_buffer.h"
 
+DECLARE_WINDOW_PROPERTY_TYPE(exo::Surface*);
+
 namespace exo {
 namespace {
+
+// A property key containing the surface that is associated with
+// window. If unset, no surface is associated with window.
+DEFINE_WINDOW_PROPERTY_KEY(Surface*, kSurfaceKey, nullptr);
 
 // Helper function that returns an iterator to the first entry in |list|
 // with |key|.
@@ -91,6 +98,7 @@ Surface::Surface()
   SetType(ui::wm::WINDOW_TYPE_CONTROL);
   SetName("ExoSurface");
   Init(ui::LAYER_SOLID_COLOR);
+  SetProperty(kSurfaceKey, this);
   set_owned_by_parent(false);
 }
 
@@ -109,6 +117,11 @@ Surface::~Surface() {
                                  frame_callbacks_);
   for (const auto& frame_callback : active_frame_callbacks_)
     frame_callback.Run(base::TimeTicks());
+}
+
+// static
+Surface* Surface::AsSurface(aura::Window* window) {
+  return window->GetProperty(kSurfaceKey);
 }
 
 void Surface::Attach(Buffer* buffer) {
