4b3fd9b947e1b06d6e871af3d3c8b5ead4e2465e
malets <malets@yandex-team.ru>
2015-12-07 16:54:19 -0800

    Fixed potential and real socket descriptor leaks in devtools client
    Socket file descriptors leak one per browser run in perf tests, which
    may lead to exhaustion and browser crashes if there are more than about
    1000 browser startups in a single run.
    
    R=aiolos@chromium.org, chrishenry@google.com
    BUG=565929
    
    Review URL: https://codereview.chromium.org/1493083003
    
    Cr-Commit-Position: refs/heads/master@{#363647}

--- tools/telemetry/telemetry/internal/backends/chrome_inspector/devtools_client_backend.py
+++ tools/telemetry/telemetry/internal/backends/chrome_inspector/devtools_client_backend.py
@@ -43,7 +43,7 @@ def IsDevToolsAgentAvailable(port, app_backend):
 
   devtools_http_instance = devtools_http.DevToolsHttp(port)
   try:
-    return _IsDevToolsAgentAvailable(devtools_http.DevToolsHttp(port))
+    return _IsDevToolsAgentAvailable(devtools_http_instance)
   finally:
     devtools_http_instance.Disconnect()
 
@@ -165,7 +165,8 @@ class DevToolsClientBackend(object):
 
   def IsAlive(self):
     """Whether the DevTools server is available and connectable."""
-    return _IsDevToolsAgentAvailable(self._devtools_http)
+    return (self._devtools_http and
+        _IsDevToolsAgentAvailable(self._devtools_http))
 
   def Close(self):
     if self._tracing_backend:
@@ -183,6 +184,10 @@ class DevToolsClientBackend(object):
     if self._browser_inspector_websocket:
       self._browser_inspector_websocket.Disconnect()
       self._browser_inspector_websocket = None
+
+    assert self._devtools_http
+    self._devtools_http.Disconnect()
+    self._devtools_http = None
 
 
   @decorators.Cache
