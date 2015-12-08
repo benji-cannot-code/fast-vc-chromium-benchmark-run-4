076844f0c3bca30dd8d622b79e50c722cd108c6c
alexmos <alexmos@chromium.org>
2015-12-07 17:28:05 -0800

    Fix --isolate-extensions expectations in VerifySiteInstanceCountInBrowsingInstanceWithExtensions.
    This is a follow-up to nasko@'s
    https://codereview.chromium.org/1495993002/, which put all subframes
    in the same BrowsingInstance as the parent and updated this test's
    expectations for --site-per-process.  This CL further updates the
    test's expections so it passes with --isolate-extensions.
    
    BUG=522302
    
    Review URL: https://codereview.chromium.org/1508823003
    
    Cr-Commit-Position: refs/heads/master@{#363689}

--- chrome/browser/site_details_browsertest.cc
+++ chrome/browser/site_details_browsertest.cc
@@ -1149,7 +1149,7 @@ IN_PROC_BROWSER_TEST_F(
   } else if (extensions::IsIsolateExtensionsEnabled()) {
     EXPECT_THAT(details->uma()->GetAllSamples(
                     "SiteIsolation.SiteInstancesPerBrowsingInstance"),
-                ElementsAre(Bucket(1, 2)));
+                ElementsAre(Bucket(2, 1)));
   } else {
     EXPECT_THAT(details->uma()->GetAllSamples(
                     "SiteIsolation.SiteInstancesPerBrowsingInstance"),
@@ -1168,7 +1168,7 @@ IN_PROC_BROWSER_TEST_F(
   } else if (extensions::IsIsolateExtensionsEnabled()) {
     EXPECT_THAT(details->uma()->GetAllSamples(
                     "SiteIsolation.SiteInstancesPerBrowsingInstance"),
-                ElementsAre(Bucket(1, 3)));
+                ElementsAre(Bucket(1, 1), Bucket(2, 1)));
   } else {
     EXPECT_THAT(details->uma()->GetAllSamples(
                     "SiteIsolation.SiteInstancesPerBrowsingInstance"),
@@ -1188,7 +1188,7 @@ IN_PROC_BROWSER_TEST_F(
   } else if (extensions::IsIsolateExtensionsEnabled()) {
     EXPECT_THAT(details->uma()->GetAllSamples(
                     "SiteIsolation.SiteInstancesPerBrowsingInstance"),
-                ElementsAre(Bucket(1, 4)));
+                ElementsAre(Bucket(1, 1), Bucket(3, 1)));
   } else {
     EXPECT_THAT(details->uma()->GetAllSamples(
                     "SiteIsolation.SiteInstancesPerBrowsingInstance"),
