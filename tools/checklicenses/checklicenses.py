42e3051a2ac643d88badde4d5db9759c3a8f48b2
aiolos <aiolos@chromium.org>
2015-12-08 13:50:09 -0800

    Remove telemetry/third_party/gsutil from the checklicenses whitelist.
    Review URL: https://codereview.chromium.org/1506053002
    
    Cr-Commit-Position: refs/heads/master@{#363796}

--- tools/checklicenses/checklicenses.py
+++ tools/checklicenses/checklicenses.py
@@ -610,10 +610,6 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
     'tools/telemetry/third_party/altgraph': [
         'UNKNOWN',
     ],
-    # Not shipped, downloaded on trybots sometimes.
-    'tools/telemetry/third_party/gsutil': [
-        'UNKNOWN',
-    ],
     # Not shipped, MIT license but the header files contain no licensing info.
     'tools/telemetry/third_party/modulegraph': [
         'UNKNOWN',
