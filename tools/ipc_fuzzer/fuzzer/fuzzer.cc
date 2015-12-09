422ea72ba43bb758d4c0007005eb3b58542a6771
dyen <dyen@chromium.org>
2015-12-08 18:23:30 -0800

    Reland "Added an extra sync token field for extra command buffer..."
    This reverts commit 5097a2103f86741a7d0e6567e57759be939182b0.
    
    R=dalecurtis@chromium.org, dcheng@chromium.org, piman@chromium.org, sky@chromium.org
    BUG=514815, 566291
    
    Review URL: https://codereview.chromium.org/1499813003
    
    Cr-Commit-Position: refs/heads/master@{#363922}

--- tools/ipc_fuzzer/fuzzer/fuzzer.cc
+++ tools/ipc_fuzzer/fuzzer/fuzzer.cc
@@ -1205,6 +1205,7 @@ struct FuzzTraits<gpu::SyncToken> {
     bool verified_flush = false;
     gpu::CommandBufferNamespace namespace_id =
         gpu::CommandBufferNamespace::INVALID;
+    int32_t extra_data_field = 0;
     uint64_t command_buffer_id = 0;
     uint64_t release_count = 0;
 
@@ -1212,13 +1213,15 @@ struct FuzzTraits<gpu::SyncToken> {
       return false;
     if (!FuzzParam(&namespace_id, fuzzer))
       return false;
+    if (!FuzzParam(&extra_data_field, fuzzer))
+      return false;
     if (!FuzzParam(&command_buffer_id, fuzzer))
       return false;
     if (!FuzzParam(&release_count, fuzzer))
       return false;
 
     p->Clear();
-    p->Set(namespace_id, command_buffer_id, release_count);
+    p->Set(namespace_id, extra_data_field, command_buffer_id, release_count);
     if (verified_flush)
       p->SetVerifyFlush();
     return true;
