e49206209e6f3e129c9898d68898614495c41889
zmin <zmin@chromium.org>
2015-12-09 14:59:08 -0800

    Emit correct NewTabPage.SuggestionsType metrics
    Review URL: https://codereview.chromium.org/1396143002
    
    Cr-Commit-Position: refs/heads/master@{#364190}

--- chrome/browser/ui/webui/ntp/ntp_user_data_logger.h
+++ chrome/browser/ui/webui/ntp/ntp_user_data_logger.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/gtest_prod_util.h"
 #include "base/strings/string16.h"
 #include "base/time/time.h"
 #include "chrome/common/ntp_logging_events.h"
@@ -63,6 +64,11 @@ class NTPUserDataLogger
 
  private:
   friend class content::WebContentsUserData<NTPUserDataLogger>;
+
+  FRIEND_TEST_ALL_PREFIXES(SearchTabHelperTest,
+                           OnMostVisitedItemsChangedFromServer);
+  FRIEND_TEST_ALL_PREFIXES(SearchTabHelperTest,
+                           OnMostVisitedItemsChangedFromClient);
 
   // True if at least one iframe came from a server-side suggestion.
   bool has_server_side_suggestions_;
