020b5962b14d48318dd3d6a8a92b55dbe2ba07b0
penghuang <penghuang@chromium.org>
2015-12-09 08:48:29 -0800

    mustash: gpu: Remove CommandBufferSyncClient & CommandBufferSyncPointClient.
    Remove CommandBufferSyncClient & CommandBufferSyncPointClient by using
    mojo function callback.
    
    BUG=565553
    
    Committed: https://crrev.com/27939d283a5a45a8a15a7b6a01df728ed7373ab9
    Cr-Commit-Position: refs/heads/master@{#363401}
    
    Review URL: https://codereview.chromium.org/1497783004
    
    Cr-Commit-Position: refs/heads/master@{#364085}

--- components/mus/gles2/command_buffer_driver.h
+++ components/mus/gles2/command_buffer_driver.h
@@ -11,8 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/single_thread_task_runner.h"
-#include "base/timer/timer.h"
 #include "components/mus/public/interfaces/command_buffer.mojom.h"
+#include "gpu/command_buffer/common/capabilities.h"
+#include "gpu/command_buffer/common/command_buffer.h"
 #include "gpu/command_buffer/common/constants.h"
 #include "ui/gfx/geometry/size.h"
 
@@ -51,15 +52,12 @@ class CommandBufferDriver {
 
   void set_client(scoped_ptr<Client> client) { client_ = std::move(client); }
 
-  void Initialize(
-      mojo::InterfacePtrInfo<mojom::CommandBufferSyncClient> sync_client,
-      mojo::InterfacePtrInfo<mojom::CommandBufferLostContextObserver>
-          loss_observer,
-      mojo::ScopedSharedBufferHandle shared_state,
-      mojo::Array<int32_t> attribs);
+  bool Initialize(mojo::InterfacePtrInfo<
+                      mojom::CommandBufferLostContextObserver> loss_observer,
+                  mojo::ScopedSharedBufferHandle shared_state,
+                  mojo::Array<int32_t> attribs);
   void SetGetBuffer(int32_t buffer);
   void Flush(int32_t put_offset);
-  void MakeProgress(int32_t last_get_offset);
   void RegisterTransferBuffer(int32_t id,
                               mojo::ScopedSharedBufferHandle transfer_buffer,
                               uint32_t size);
@@ -73,6 +71,12 @@ class CommandBufferDriver {
   void DestroyImage(int32_t id);
   bool IsScheduled() const;
   bool HasUnprocessedCommands() const;
+  gpu::Capabilities GetCapabilities();
+  gpu::CommandBuffer::State GetLastState();
+  gpu::CommandBufferNamespace GetNamespaceID() const {
+    return gpu::CommandBufferNamespace::MOJO;
+  }
+  uint64_t GetCommandBufferID() const { return command_buffer_id_; }
   gpu::SyncPointOrderData* sync_point_order_data() {
     return sync_point_order_data_.get();
   }
@@ -93,7 +97,6 @@ class CommandBufferDriver {
 
   const uint64_t command_buffer_id_;
   scoped_ptr<Client> client_;
-  mojom::CommandBufferSyncClientPtr sync_client_;
   mojom::CommandBufferLostContextObserverPtr loss_observer_;
   scoped_ptr<gpu::CommandBufferService> command_buffer_;
   scoped_ptr<gpu::gles2::GLES2Decoder> decoder_;
