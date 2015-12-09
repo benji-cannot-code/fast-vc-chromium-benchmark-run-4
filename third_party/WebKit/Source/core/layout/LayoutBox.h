e450070111f8f9abf24ae9cd44007f47af6bff09
wangxianzhu <wangxianzhu@chromium.org>
2015-12-08 23:05:08 -0800

    Fix several corner case issues of scrollbar paint invalidation
    - Invalidate composited scrollbars also during paint invalidation to
      avoid unnecessary invalidation on intermediate changes;
    
    - Invalidate also on the containing box for moved/resized composited
      non-overlay scrollbars. This ensures the expanded/shrunk areas of the
      box because of scrollbar existence/width change are invalidated. This
      is the root cause of bug 535161.
    
    - Avoid unnecessary invalidations on overlay scrollbar changes.
    
    BUG=535161,560418
    
    Review URL: https://codereview.chromium.org/1491193003
    
    Cr-Commit-Position: refs/heads/master@{#363998}

--- third_party/WebKit/Source/core/layout/LayoutBox.h
+++ third_party/WebKit/Source/core/layout/LayoutBox.h
@@ -847,6 +847,8 @@ public:
     void mapLocalToContainer(const LayoutBoxModelObject* paintInvalidationContainer, TransformState&, MapCoordinatesFlags = ApplyContainerFlip, bool* wasFixed = nullptr, const PaintInvalidationState* = nullptr) const override;
     void mapAbsoluteToLocalPoint(MapCoordinatesFlags, TransformState&) const override;
 
+    void clearPreviousPaintInvalidationRects() override;
+
 protected:
     void willBeDestroyed() override;
 
