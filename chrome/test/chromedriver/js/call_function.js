d9932a7c14994533600117d61848f6da0cef4ee7
stgao <stgao@chromium.org>
2015-12-08 21:39:45 -0800

    [chromedriver] Do not wrap or unwrap functions of an object.
    TBR=samuong@chromium.org
    BUG=chromedriver:1276
    
    Review URL: https://codereview.chromium.org/1510783002
    
    Cr-Commit-Position: refs/heads/master@{#363990}

--- chrome/test/chromedriver/js/call_function.js
+++ chrome/test/chromedriver/js/call_function.js
@@ -161,8 +161,17 @@ function wrap(value) {
     }
 
     var obj = (typeof(value.length) == 'number') ? [] : {};
-    for (var prop in value)
-      obj[prop] = wrap(value[prop]);
+    for (var prop in value) {
+      if (typeof(value[prop]) === 'function') {
+        // For functions, no need for further wrapping.
+        var wrapped = {};
+        wrapped[ELEMENT_KEY] = getPageCache().storeItem(value[prop]);
+        obj[prop] = wrapped;
+        continue;
+      }
+
+      obj[prop] = wrap(value[prop])
+    }
     return obj;
   }
   return value;
@@ -182,8 +191,14 @@ function unwrap(value, cache) {
       return cache.retrieveItem(value[ELEMENT_KEY]);
 
     var obj = (typeof(value.length) == 'number') ? [] : {};
-    for (var prop in value)
+    for (var prop in value) {
+      if (typeof(value[prop]) === 'function') {
+        obj[prop] = value[prop];
+        continue;
+      }
+
       obj[prop] = unwrap(value[prop], cache);
+    }
     return obj;
   }
   return value;
