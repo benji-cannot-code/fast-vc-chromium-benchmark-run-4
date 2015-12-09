8d0ad824a7d0707569ee92dc26563c44d9a830ac
mseaborn <mseaborn@chromium.org>
2015-12-08 18:41:38 -0800

    NaCl: Remove last #include of nacl_check.h; use base/logging.h instead
    Both provide CHECK and DCHECK macros, but nacl_check.h conflicts with
    base/logging.h and hence with other Chromium headers.
    
    A recent DEPS roll makes this change possible now that
    nacl_sync_raii.h no longer conflicts with base/logging.h.
    
    BUG=302078
    TEST=build
    
    Review URL: https://codereview.chromium.org/1514493002
    
    Cr-Commit-Position: refs/heads/master@{#363940}

--- components/nacl/renderer/plugin/pnacl_translate_thread.cc
+++ components/nacl/renderer/plugin/pnacl_translate_thread.cc
@@ -8,12 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iterator>
 #include <sstream>
 
+#include "base/logging.h"
 #include "components/nacl/renderer/plugin/plugin.h"
 #include "components/nacl/renderer/plugin/plugin_error.h"
 #include "components/nacl/renderer/plugin/srpc_params.h"
 #include "components/nacl/renderer/plugin/temporary_file.h"
 #include "components/nacl/renderer/plugin/utility.h"
-#include "native_client/src/shared/platform/nacl_check.h"
 #include "native_client/src/shared/platform/nacl_sync_raii.h"
 #include "native_client/src/trusted/desc/nacl_desc_wrapper.h"
 #include "ppapi/cpp/var.h"
