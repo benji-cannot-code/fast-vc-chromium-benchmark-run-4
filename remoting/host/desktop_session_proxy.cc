0372dcf0e340d7b8d48ace20fb0c68560362840a
sergeyu <sergeyu@chromium.org>
2015-12-08 18:32:14 -0800

    Remove dependency on webrtc::SharedMemory::handle()
    webrtc::SharedMemory::handle() is never useful, so it will need to be
    removed. This changed removes dependency on that field so it can be
    removed later.
    
    Review URL: https://codereview.chromium.org/1509723003
    
    Cr-Commit-Position: refs/heads/master@{#363934}

--- remoting/host/desktop_session_proxy.cc
+++ remoting/host/desktop_session_proxy.cc
@@ -68,14 +68,6 @@ class DesktopSessionProxy::IpcSharedBufferCore
   int id() { return id_; }
   size_t size() { return size_; }
   void* memory() { return shared_memory_.memory(); }
-  webrtc::SharedMemory::Handle handle() {
-#if defined(OS_WIN)
-    return shared_memory_.handle().GetHandle();
-#else
-    return base::SharedMemory::GetFdFromSharedMemoryHandle(
-        shared_memory_.handle());
-#endif
-  }
 
  private:
   virtual ~IpcSharedBufferCore() {}
@@ -91,10 +83,8 @@ class DesktopSessionProxy::IpcSharedBufferCore
 class DesktopSessionProxy::IpcSharedBuffer : public webrtc::SharedMemory {
  public:
   IpcSharedBuffer(scoped_refptr<IpcSharedBufferCore> core)
-      : SharedMemory(core->memory(), core->size(),
-                     core->handle(), core->id()),
-        core_(core) {
-  }
+      : SharedMemory(core->memory(), core->size(), 0, core->id()),
+        core_(core) {}
 
  private:
   scoped_refptr<IpcSharedBufferCore> core_;
