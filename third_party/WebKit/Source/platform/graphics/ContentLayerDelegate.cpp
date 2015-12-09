170bc9b24771f14c04c91f8b6364fd308a34abb1
wkorman <wkorman@chromium.org>
2015-12-09 11:09:34 -0800

    Pass visual rects for link highlights and paint artifact picture.
    BUG=529938
    
    Review URL: https://codereview.chromium.org/1507193006
    
    Cr-Commit-Position: refs/heads/master@{#364113}

--- third_party/WebKit/Source/platform/graphics/ContentLayerDelegate.cpp
+++ third_party/WebKit/Source/platform/graphics/ContentLayerDelegate.cpp
@@ -35,11 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/GraphicsContext.h"
 #include "platform/graphics/paint/PaintArtifactToSkCanvas.h"
 #include "platform/graphics/paint/PaintController.h"
-#include "platform/transforms/AffineTransform.h"
-#include "platform/transforms/TransformationMatrix.h"
 #include "public/platform/WebDisplayItemList.h"
-#include "public/platform/WebFloatRect.h"
-#include "third_party/skia/include/core/SkCanvas.h"
+#include "public/platform/WebRect.h"
 #include "third_party/skia/include/core/SkPicture.h"
 #include "ui/gfx/geometry/rect.h"
 
@@ -63,8 +60,7 @@ static void paintArtifactToWebDisplayItemList(WebDisplayItemList* list, const Pa
         // one big flat SkPicture.
         SkRect skBounds = SkRect::MakeXYWH(bounds.x(), bounds.y(), bounds.width(), bounds.height());
         RefPtr<SkPicture> picture = paintArtifactToSkPicture(artifact, skBounds);
-        // TODO(wkorman): Pass actual visual rect with the drawing item.
-        list->appendDrawingItem(IntRect(), picture.get());
+        list->appendDrawingItem(WebRect(bounds.x(), bounds.y(), bounds.width(), bounds.height()), picture.get());
         return;
     }
     artifact.appendToWebDisplayItemList(list);
--- third_party/WebKit/Source/web/LinkHighlightImpl.cpp
+++ third_party/WebKit/Source/web/LinkHighlightImpl.cpp
@@ -271,7 +271,8 @@ void LinkHighlightImpl::paintContents(WebDisplayItemList* webDisplayItemList, We
         return;
 
     SkPictureRecorder recorder;
-    SkCanvas* canvas = recorder.beginRecording(paintableRegion().width(), paintableRegion().height());
+    gfx::Rect visualRect = paintableRegion();
+    SkCanvas* canvas = recorder.beginRecording(visualRect.width(), visualRect.height());
 
     SkPaint paint;
     paint.setStyle(SkPaint::kFill_Style);
@@ -280,8 +281,7 @@ void LinkHighlightImpl::paintContents(WebDisplayItemList* webDisplayItemList, We
     canvas->drawPath(m_path.skPath(), paint);
 
     RefPtr<const SkPicture> picture = adoptRef(recorder.endRecording());
-    // TODO(wkorman): Pass actual visual rect with the drawing item.
-    webDisplayItemList->appendDrawingItem(IntRect(), picture.get());
+    webDisplayItemList->appendDrawingItem(WebRect(visualRect.x(), visualRect.y(), visualRect.width(), visualRect.height()), picture.get());
 }
 
 void LinkHighlightImpl::startHighlightAnimationIfNeeded()
