fd75c7f76775631cddf0d7bafa2c2eef19874daf
tkent <tkent@chromium.org>
2015-12-09 01:26:27 -0800

    Remove HTMLOptionElement::m_disabled.
    We don't need to keep the value because parseAttribute() can determine its
    previous value.
    
    BUG=561251
    
    Review URL: https://codereview.chromium.org/1509353003
    
    Cr-Commit-Position: refs/heads/master@{#364022}

--- third_party/WebKit/Source/core/html/HTMLOptionElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLOptionElement.cpp
@@ -51,7 +51,6 @@ using namespace HTMLNames;
 
 HTMLOptionElement::HTMLOptionElement(Document& document)
     : HTMLElement(optionTag, document)
-    , m_disabled(false)
     , m_isSelected(false)
 {
     setHasCustomStyleCallbacks();
@@ -205,9 +204,7 @@ void HTMLOptionElement::parseAttribute(const QualifiedName& name, const AtomicSt
         if (HTMLDataListElement* dataList = ownerDataListElement())
             dataList->optionElementChildrenChanged();
     } else if (name == disabledAttr) {
-        bool oldDisabled = m_disabled;
-        m_disabled = !value.isNull();
-        if (oldDisabled != m_disabled) {
+        if (oldValue.isNull() != value.isNull()) {
             pseudoStateChanged(CSSSelector::PseudoDisabled);
             pseudoStateChanged(CSSSelector::PseudoEnabled);
             if (layoutObject())
@@ -367,6 +364,11 @@ String HTMLOptionElement::textIndentedToRespectGroupLabel() const
     if (parent && isHTMLOptGroupElement(*parent))
         return "    " + displayLabel();
     return displayLabel();
+}
+
+bool HTMLOptionElement::ownElementDisabled() const
+{
+    return fastHasAttribute(disabledAttr);
 }
 
 bool HTMLOptionElement::isDisabledFormControl() const
