2629bde64fe3db7a6b25febd2adb3f0a53b2aad8
hta <hta@chromium.org>
2015-12-08 22:45:51 -0800

    Isolate the constraint mechanism to content/renderer/media.
    This removes the StreamOptions class, and replaces it with a simpler
    StreamControls.
    This is in preparation for removing the map<string, string> version
    of Constraints.
    
    BUG=webrtc:4906
    
    Review URL: https://codereview.chromium.org/1479813002
    
    Cr-Commit-Position: refs/heads/master@{#363997}

--- content/renderer/media/mock_media_stream_dispatcher.h
+++ content/renderer/media/mock_media_stream_dispatcher.h
@@ -22,7 +22,7 @@ class MockMediaStreamDispatcher : public MediaStreamDispatcher {
   void GenerateStream(
       int request_id,
       const base::WeakPtr<MediaStreamDispatcherEventHandler>& event_handler,
-      const StreamOptions& components,
+      const StreamControls& controls,
       const GURL& url) override;
   void CancelGenerateStream(
       int request_id,
