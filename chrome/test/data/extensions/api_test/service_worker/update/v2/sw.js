be139356c89457ebb7cb822bd4cc3b0151670fea
lazyboy <lazyboy@chromium.org>
2015-12-09 15:09:58 -0800

    Add a test to cover service worker update codepath when extension is updated.
    The updated extension's service worker calls skipWaiting() on its oninstall
    handler to let the new service worker control its clients right away.
    
    BUG=533065
    
    Review URL: https://codereview.chromium.org/1432823003
    
    Cr-Commit-Position: refs/heads/master@{#364195}

---
+++ chrome/test/data/extensions/api_test/service_worker/update/v2/sw.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+this.onmessage = function(e) {
+  e.ports[0].postMessage('Pong from version 2');
+};
+
+this.oninstall = function(e) {
+  e.waitUntil(self.skipWaiting());
+};
