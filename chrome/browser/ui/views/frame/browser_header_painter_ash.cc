300e8c83f371347fe06d96170b71a1c7663405d4
tdanderson <tdanderson@chromium.org>
2015-12-08 18:26:38 -0800

    Remove the restored window highlight in Ash material design
    In material design, remove the highlight that
    appears along the inner border of a restored
    Ash window.
    
    BUG=567820
    TEST=manual
    
    Review URL: https://codereview.chromium.org/1505353002
    
    Cr-Commit-Position: refs/heads/master@{#363927}

--- chrome/browser/ui/views/frame/browser_header_painter_ash.cc
+++ chrome/browser/ui/views/frame/browser_header_painter_ash.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColor.h"
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkPath.h"
+#include "ui/base/resource/material_design/material_design_controller.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/theme_provider.h"
 #include "ui/gfx/animation/slide_animation.h"
@@ -219,8 +220,12 @@ void BrowserHeaderPainterAsh::PaintHeader(gfx::Canvas* canvas, Mode mode) {
         ash::HeaderPainterUtil::GetThemeBackgroundXInset());
   }
 
-  if (!frame_->IsMaximized() && !frame_->IsFullscreen())
+  if (!ui::MaterialDesignController::IsModeMaterial() &&
+      !frame_->IsMaximized() &&
+      !frame_->IsFullscreen()) {
     PaintHighlightForRestoredWindow(canvas);
+  }
+
   if (frame_->widget_delegate() &&
       frame_->widget_delegate()->ShouldShowWindowTitle()) {
     PaintTitleBar(canvas);
