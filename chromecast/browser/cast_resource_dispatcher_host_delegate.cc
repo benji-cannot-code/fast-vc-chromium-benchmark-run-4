dcdd255a73284154cc8ac6f554776c311b9e3be6
servolk <servolk@chromium.org>
2015-12-09 09:08:43 -0800

    [Chromecast] Suppress error logging for cancelled URL requests
    This should avoid having net::ERR_ABORTED logs for each direct URL
    playback that we currently have in our log files.
    The problem was that direct URL playback code path creates and then
    cancels URLRequest, and currently we log an error for each URLRequest
    that didn't succeeded. We should adjust our logic slightly and do what
    net::URLRequest::~URLRequest does:
      // Log error only on failure, not cancellation, as even successful
      // requests are "cancelled" on destruction.
      if (status_.status() == URLRequestStatus::FAILED)
        net_error = status_.error();
    
    BUG=567962
    
    Review URL: https://codereview.chromium.org/1509273004
    
    Cr-Commit-Position: refs/heads/master@{#364090}

--- chromecast/browser/cast_resource_dispatcher_host_delegate.cc
+++ chromecast/browser/cast_resource_dispatcher_host_delegate.cc
@@ -9,13 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/net/connectivity_checker.h"
 #include "net/base/net_errors.h"
 #include "net/url_request/url_request.h"
+#include "net/url_request/url_request_status.h"
 
 namespace chromecast {
 namespace shell {
 
 void CastResourceDispatcherHostDelegate::RequestComplete(
     net::URLRequest* url_request) {
-  if (!url_request->status().is_success()) {
+  if (url_request->status().status() == net::URLRequestStatus::FAILED) {
     LOG(ERROR) << "Failed to load resource " << url_request->url()
                << "; status:" << url_request->status().status() << ", error:"
                << net::ErrorToShortString(url_request->status().error());
