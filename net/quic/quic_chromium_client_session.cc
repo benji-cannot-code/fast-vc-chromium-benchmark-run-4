da67c48b699722b55f038d0b38ae467ed2f823cf
rch <rch@chromium.org>
2015-12-09 15:40:29 -0800

    Add a new histogram Net.QuicSession.RejectLength for The length in bytes of a QUIC REJ message received from the server.
    Review URL: https://codereview.chromium.org/1502443002
    
    Cr-Commit-Position: refs/heads/master@{#364216}

--- net/quic/quic_chromium_client_session.cc
+++ net/quic/quic_chromium_client_session.cc
@@ -662,6 +662,12 @@ void QuicChromiumClientSession::OnCryptoHandshakeEvent(
 void QuicChromiumClientSession::OnCryptoHandshakeMessageSent(
     const CryptoHandshakeMessage& message) {
   logger_->OnCryptoHandshakeMessageSent(message);
+
+  if (message.tag() == kREJ || message.tag() == kSREJ) {
+    UMA_HISTOGRAM_CUSTOM_COUNTS("Net.QuicSession.RejectLength",
+                                message.GetSerialized().length(), 1000, 10000,
+                                50);
+  }
 }
 
 void QuicChromiumClientSession::OnCryptoHandshakeMessageReceived(
