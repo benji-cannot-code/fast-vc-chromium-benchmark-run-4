1b5f0efdd5b1cabbd85cf5651fcd20e75810df24
lukasza <lukasza@chromium.org>
2015-12-10 10:08:53 -0800

    Speculative fix for crash in serializeFrame when url of a frame is null.
    Before crrev.com/1441553002 frames with invalid url would be stored in
    m_resourceURLs with a fake, frame-id-based url (i.e.  wyciwyg://frame/42).
    After crrev.com/1441553002, we try to put such frames into
    m_resourceURLs using their original URL and crash because KURLHash
    doesn't support null KURLs.
    
    The good news is that after crrev.com/1441553002, we no longer need to
    dedupe frames (because now each frame gets serialized with a unique
    content id) and therefore we no longer need to put frames into
    m_resourceURLs.  Continuing to add frames into m_resourceURLs was a
    mistake/ommission crrev.com/1441553002 and fixing this mistake should
    fix the crash.  Note that this is a bit speculative, because I was not
    able to repro the crash using ToT build (i.e. the fix is based on the
    callstack from the repro used by the original ClusterFizz report).
    
    I notice that StringHash explicitly documents that it crashes for null
    strings, so I think it would be good to add similar comment to KURLHash
    (I was surprised that hashing and equality are not working for all
    possible KURL values).
    
    BUG=566245
    
    Review URL: https://codereview.chromium.org/1512853006
    
    Cr-Commit-Position: refs/heads/master@{#364398}

--- third_party/WebKit/Source/platform/weborigin/KURLHash.h
+++ third_party/WebKit/Source/platform/weborigin/KURLHash.h
@@ -33,6 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+// KURLHash doesn't support null KURLs.  get(), contains(), and add() on
+// HashMap<KURL,..., KURLHash> cause a null-pointer dereference when passed null
+// KURLs.
+
 struct KURLHash {
     static unsigned hash(const KURL& key)
     {
