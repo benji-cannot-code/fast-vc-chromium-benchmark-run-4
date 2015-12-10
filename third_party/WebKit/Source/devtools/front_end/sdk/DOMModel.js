69f27459a80fbf764f74e313d962ca52e68b16ab
pfeldman <pfeldman@chromium.org>
2015-12-10 03:53:05 -0800

    DevTools: Console displays XML nodes in lower-case
    BUG=565225
    
    Review URL: https://codereview.chromium.org/1493893008
    
    Cr-Commit-Position: refs/heads/master@{#364334}

--- third_party/WebKit/Source/devtools/front_end/sdk/DOMModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/DOMModel.js
@@ -55,6 +55,7 @@ WebInspector.DOMNode = function(domModel, doc, isInShadowTree, payload)
     this._pseudoType = payload.pseudoType;
     this._shadowRootType = payload.shadowRootType;
     this._frameId = payload.frameId || null;
+    this._xmlVersion = payload.xmlVersion;
 
     this._shadowRoots = [];
 
@@ -785,7 +786,7 @@ WebInspector.DOMNode.prototype = {
      */
     isXMLNode: function()
     {
-        return !!this.ownerDocument && !!this.ownerDocument.xmlVersion;
+        return !!this._xmlVersion;
     },
 
     /**
@@ -1059,7 +1060,6 @@ WebInspector.DOMDocument = function(domModel, payload)
     WebInspector.DOMNode.call(this, domModel, this, false, payload);
     this.documentURL = payload.documentURL || "";
     this.baseURL = payload.baseURL || "";
-    this.xmlVersion = payload.xmlVersion;
     this._listeners = {};
 }
 
