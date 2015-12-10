0319cef8b220c6aeedb58119b8b7482fce0c549c
gab <gab@chromium.org>
2015-12-09 16:06:12 -0800

    Remove support for alternate_shortcut_text master_preference.
    BUG=555087
    
    Review URL: https://codereview.chromium.org/1497023002
    
    Cr-Commit-Position: refs/heads/master@{#364224}

--- chrome/installer/util/browser_distribution.h
+++ chrome/installer/util/browser_distribution.h
@@ -34,7 +34,6 @@ class BrowserDistribution {
 
   enum ShortcutType {
     SHORTCUT_CHROME,
-    SHORTCUT_CHROME_ALTERNATE,
     SHORTCUT_APP_LAUNCHER
   };
 
@@ -83,8 +82,7 @@ class BrowserDistribution {
   virtual base::string16 GetDisplayName();
 
   // Returns the localized name of the shortcut identified by |shortcut_type|
-  // for this distribution or empty string if |shortcut_type| is unsupported
-  // by this BrowserDistribution.
+  // for this distribution.
   virtual base::string16 GetShortcutName(ShortcutType shortcut_type);
 
   // Returns the index of the icon for the product identified by
