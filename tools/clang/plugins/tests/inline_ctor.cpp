9635bc73eea47b0f710c675440b68f00121f5140
jyasskin <jyasskin@chromium.org>
2015-12-10 09:00:49 -0800

    Follow macro invocations when checking if a violation happened in a .cc file.
    BUG=156590
    
    Review URL: https://codereview.chromium.org/1504033010
    
    Cr-Commit-Position: refs/heads/master@{#364385}

--- tools/clang/plugins/tests/inline_ctor.cpp
+++ tools/clang/plugins/tests/inline_ctor.cpp
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#define MACRO_FROM_CPP INLINE_CTORS_IN_A_MACRO(InlineCtorsInvolvingCppAreOK)
+
 #include "inline_ctor.h"
 
 #include <string>
@@ -19,8 +21,13 @@ class InlineInCPPOK {
   std::vector<std::string> two_;
 };
 
+INLINE_CTORS_IN_A_MACRO(InlineCtorsBehindAMacroAreOKInCpp);
+
 int main() {
   InlineInCPPOK one;
   InlineCtorsArentOKInHeader two;
+  InlineCtorsBehindAMacroArentOKInHeader three;
+  InlineCtorsBehindAMacroAreOKInCpp four;
+  InlineCtorsInvolvingCppAreOK five;
   return 0;
 }
