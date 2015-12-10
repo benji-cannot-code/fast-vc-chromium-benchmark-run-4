e7633972f5bf891d02691192977d19bea838a6a6
olli.raula <olli.raula@intel.com>
2015-12-10 00:20:11 -0800

    Remove ScopedVector from url_request_ftp_job_unittest
    BUG=554289
    
    Review URL: https://codereview.chromium.org/1512583002
    
    Cr-Commit-Position: refs/heads/master@{#364315}

--- net/url_request/url_request_ftp_job_unittest.cc
+++ net/url_request/url_request_ftp_job_unittest.cc
@@ -5,9 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/url_request/url_request_ftp_job.h"
 
+#include <vector>
+
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/memory/scoped_vector.h"
 #include "base/run_loop.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/load_states.h"
@@ -257,19 +258,19 @@ class URLRequestFtpJobTest : public testing::Test {
 
   void AddSocket(MockRead* reads, size_t reads_size,
                  MockWrite* writes, size_t writes_size) {
-    SequencedSocketData* socket_data =
-        new SequencedSocketData(reads, reads_size, writes, writes_size);
+    scoped_ptr<SequencedSocketData> socket_data(make_scoped_ptr(
+        new SequencedSocketData(reads, reads_size, writes, writes_size)));
     socket_data->set_connect_data(MockConnect(SYNCHRONOUS, OK));
-    socket_factory_.AddSocketDataProvider(socket_data);
+    socket_factory_.AddSocketDataProvider(socket_data.get());
 
-    socket_data_.push_back(socket_data);
+    socket_data_.push_back(std::move(socket_data));
   }
 
   FtpTestURLRequestContext* request_context() { return &request_context_; }
   TestNetworkDelegate* network_delegate() { return &network_delegate_; }
 
  private:
-  ScopedVector<SequencedSocketData> socket_data_;
+  std::vector<scoped_ptr<SequencedSocketData>> socket_data_;
   MockClientSocketFactory socket_factory_;
   TestNetworkDelegate network_delegate_;
   MockFtpTransactionFactory ftp_transaction_factory_;
