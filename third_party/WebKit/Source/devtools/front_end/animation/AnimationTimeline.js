a2722f5331e5d1d1be1bec7880bdc560637aaa70
samli <samli@chromium.org>
2015-12-09 22:45:22 -0800

    Devtools Animations: Improve affordance of animations in timeline
    BUG=495551
    
    Review URL: https://codereview.chromium.org/1500373002
    
    Cr-Commit-Position: refs/heads/master@{#364302}

--- third_party/WebKit/Source/devtools/front_end/animation/AnimationTimeline.js
+++ third_party/WebKit/Source/devtools/front_end/animation/AnimationTimeline.js
@@ -711,6 +711,8 @@ WebInspector.AnimationTimeline.NodeUI.prototype = {
         this._node = node;
         this._nodeChanged();
         this._description.appendChild(WebInspector.DOMPresentationUtils.linkifyNodeReference(node));
+        if (!node.ownerDocument)
+            this.nodeRemoved();
     },
 
     /**
@@ -724,6 +726,7 @@ WebInspector.AnimationTimeline.NodeUI.prototype = {
     nodeRemoved: function()
     {
         this.element.classList.add("animation-node-removed");
+        this._node = null;
     },
 
     _nodeChanged: function()
