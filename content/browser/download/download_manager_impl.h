710659a49ad49b892b44dbf8c77e41b5e36fdf41
svaldez <svaldez@chromium.org>
2015-12-10 10:57:33 -0800

    Adding WebContent-free Download
    BUG=225901,7648
    
    Review URL: https://codereview.chromium.org/1418663010
    
    Cr-Commit-Position: refs/heads/master@{#364414}

--- content/browser/download/download_manager_impl.h
+++ content/browser/download/download_manager_impl.h
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <set>
 #include <string>
+#include <vector>
 
 #include "base/callback_forward.h"
 #include "base/containers/hash_tables.h"
+#include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -19,7 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequenced_task_runner_helpers.h"
 #include "base/synchronization/lock.h"
 #include "content/browser/download/download_item_impl_delegate.h"
+#include "content/browser/download/url_downloader.h"
 #include "content/common/content_export.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/download_manager.h"
 #include "content/public/browser/download_manager_delegate.h"
 #include "content/public/browser/download_url_parameters.h"
@@ -110,6 +114,8 @@ class CONTENT_EXPORT DownloadManagerImpl : public DownloadManager,
       scoped_ptr<DownloadFileFactory> file_factory);
   virtual DownloadFileFactory* GetDownloadFileFactoryForTesting();
 
+  void RemoveUrlDownloader(UrlDownloader* downloader);
+
  private:
   typedef std::set<DownloadItem*> DownloadSet;
   typedef base::hash_map<uint32, DownloadItemImpl*> DownloadMap;
@@ -169,6 +175,9 @@ class CONTENT_EXPORT DownloadManagerImpl : public DownloadManager,
   void ShowDownloadInShell(DownloadItemImpl* download) override;
   void DownloadRemoved(DownloadItemImpl* download) override;
 
+  void AddUrlDownloader(
+      scoped_ptr<UrlDownloader, BrowserThread::DeleteOnIOThread> downloader);
+
   // Factory for creation of downloads items.
   scoped_ptr<DownloadItemFactory> item_factory_;
 
@@ -196,6 +205,9 @@ class CONTENT_EXPORT DownloadManagerImpl : public DownloadManager,
   DownloadManagerDelegate* delegate_;
 
   net::NetLog* net_log_;
+
+  std::vector<scoped_ptr<UrlDownloader, BrowserThread::DeleteOnIOThread>>
+      url_downloaders_;
 
   base::WeakPtrFactory<DownloadManagerImpl> weak_factory_;
 
