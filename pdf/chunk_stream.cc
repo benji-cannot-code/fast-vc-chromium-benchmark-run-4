18d0874a33b93f7a37c1cfa512adf3eb779d1edd
spelchat <spelchat@chromium.org>
2015-12-09 16:44:15 -0800

    Minimize the number of range requests made by PDFium
    Previously, Chrome would make a series of range requests for linearized PDFs,
    starting at 32 KB and slowly increasing. On high latency connections, this is
    considerably slower than using a single request. Now Chrome will make range
    requests as large as possible and cancel them if the renderer needs some data in
    a different place in the document. This significantly reduces the number of
    range requests performed by Chrome.
    
    BUG=460201,78264
    
    Review URL: https://codereview.chromium.org/1506023002
    
    Cr-Commit-Position: refs/heads/master@{#364235}

--- pdf/chunk_stream.cc
+++ pdf/chunk_stream.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome_pdf {
 
-ChunkStream::ChunkStream() {
+ChunkStream::ChunkStream() : stream_size_(0) {
 }
 
 ChunkStream::~ChunkStream() {
@@ -27,10 +27,12 @@ ChunkStream::~ChunkStream() {
 void ChunkStream::Clear() {
   chunks_.clear();
   data_.clear();
+  stream_size_ = 0;
 }
 
 void ChunkStream::Preallocate(size_t stream_size) {
   data_.reserve(stream_size);
+  stream_size_ = stream_size;
 }
 
 size_t ChunkStream::GetSize() {
@@ -151,7 +153,7 @@ size_t ChunkStream::GetFirstMissingByte() const {
   return begin->first > 0 ? 0 : begin->second;
 }
 
-size_t ChunkStream::GetLastByteBefore(size_t offset) const {
+size_t ChunkStream::GetFirstMissingByteInInterval(size_t offset) const {
   if (chunks_.empty())
     return 0;
   std::map<size_t, size_t>::const_iterator it = chunks_.upper_bound(offset);
@@ -161,13 +163,13 @@ size_t ChunkStream::GetLastByteBefore(size_t offset) const {
   return it->first + it->second;
 }
 
-size_t ChunkStream::GetFirstByteAfter(size_t offset) const {
+size_t ChunkStream::GetLastMissingByteInInterval(size_t offset) const {
   if (chunks_.empty())
-    return 0;
+    return stream_size_ - 1;
   std::map<size_t, size_t>::const_iterator it = chunks_.upper_bound(offset);
   if (it == chunks_.end())
-    return data_.size();
-  return it->first;
+    return stream_size_ - 1;
+  return it->first - 1;
 }
 
 }  // namespace chrome_pdf
