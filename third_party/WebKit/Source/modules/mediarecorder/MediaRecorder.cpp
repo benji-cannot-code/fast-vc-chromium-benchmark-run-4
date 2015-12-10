9101bcb501fbcd02083a681c3c22eaa155256b2d
mcasas <mcasas@chromium.org>
2015-12-10 10:44:00 -0800

    MediaRecorder: avoid recording Remote Audio Tracks (unsupported), ignore them and log JS warning
    http://crbug.com/344303 and line of code [1] prevents supporting
    such recording. While we figure out exactly what needs to be done
    for this, is better to record only Video than nothing at all, so
    this CL avoids recording Audio from Remote Tracks.
    
    [1] https://code.google.com/p/chromium/codesearch#chromium/src/content/public/renderer/media_stream_audio_sink.cc&q=media_stream_audio_sink&sq=package:chromium&type=cs&l=20
    
    BUG=567960
    
    TEST= Navigate to
    https://rawgit.com/webrtc/samples/gh-pages/src/content/peerconnection/pc1/index.html
    Press buttons 'Start' and 'Call' in succession, to establish a remote call.
    Open the JS console and copy/type
     var mr = new MediaRecorder(remoteVideo.srcObject);
    With this CL, a warning should pop up indicating that
    remote audio tracks are not supported. Type/copy:
     mr.ondataavailable=function(event){console.log("data available");}; mr.start(10)
    the JS console should start typing "data available" messages,
    whereas without this patch, no such messages would show up.
    
    Review URL: https://codereview.chromium.org/1506343004
    
    Cr-Commit-Position: refs/heads/master@{#364411}

--- third_party/WebKit/Source/modules/mediarecorder/MediaRecorder.cpp
+++ third_party/WebKit/Source/modules/mediarecorder/MediaRecorder.cpp
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/DOMError.h"
 #include "core/fileapi/Blob.h"
+#include "core/frame/ConsoleTypes.h"
+#include "core/inspector/ConsoleMessage.h"
 #include "modules/EventModules.h"
 #include "modules/EventTargetModules.h"
 #include "modules/mediarecorder/BlobEvent.h"
@@ -64,6 +66,11 @@ MediaRecorder::MediaRecorder(ExecutionContext* context, MediaStream* stream, con
     , m_dispatchScheduledEventRunner(this, &MediaRecorder::dispatchScheduledEvent)
 {
     ASSERT(m_stream->getTracks().size());
+
+    // Recording remote Audio streams is not supported: http://crbug.com/121673.
+    if (!stream->getAudioTracks().isEmpty() && stream->getAudioTracks()[0]->remote()) {
+        context->addConsoleMessage(ConsoleMessage::create(JSMessageSource, WarningMessageLevel, "Recording remote audio tracks is not supported, ignoring them."));
+    }
 
     m_recorderHandler = adoptPtr(Platform::current()->createMediaRecorderHandler());
     ASSERT(m_recorderHandler);
