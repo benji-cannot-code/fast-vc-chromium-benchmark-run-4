d18eac137175e2de06b30731d37db039a12baeed
christiank <christiank@opera.com>
2015-12-10 13:31:31 -0800

    Allow one-copy and zero-copy task tile worker pools to use compressed textures.
    BUG=434699
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Committed: https://crrev.com/7d60ce9a132a88ead407a2a58c91edc431e68259
    Cr-Commit-Position: refs/heads/master@{#364326}
    
    Review URL: https://codereview.chromium.org/1379783002
    
    Cr-Commit-Position: refs/heads/master@{#364484}

--- cc/output/renderer_settings.h
+++ cc/output/renderer_settings.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "cc/base/cc_export.h"
+#include "cc/resources/resource_format.h"
 
 namespace cc {
 
@@ -30,9 +31,9 @@ class CC_EXPORT RendererSettings {
   bool delay_releasing_overlay_resources;
   double refresh_rate;
   int highp_threshold_min;
-  bool use_rgba_4444_textures;
   size_t texture_id_allocation_chunk_size;
   bool use_gpu_memory_buffer_resources;
+  ResourceFormat preferred_tile_format;
 
   void ToProtobuf(proto::RendererSettings* proto) const;
   void FromProtobuf(const proto::RendererSettings& proto);
