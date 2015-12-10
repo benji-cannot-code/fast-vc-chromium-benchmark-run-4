ad9591607d54b87d514c2ab50f8987e3629b1bfa
mmoroz <mmoroz@chromium.org>
2015-12-10 13:15:52 -0800

    fuzzer for net::HttpChunkedDecoder implemented
    R=aizatsky@chromium.org, inferno@chromium.org, krasin@chromium.org
    BUG=566504
    
    Review URL: https://codereview.chromium.org/1509163002
    
    Cr-Commit-Position: refs/heads/master@{#364478}

---
+++ testing/libfuzzer/fuzzers/http_chunked_decoder_fuzzer.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <vector>
+
+#include "net/http/http_chunked_decoder.h"
+
+// Entry point for LibFuzzer.
+extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
+  const char* data_ptr = reinterpret_cast<const char*>(data);
+  std::vector<char> buffer(data_ptr, data_ptr + size);
+  net::HttpChunkedDecoder decoder;
+  decoder.FilterBuf(buffer.data(), buffer.size());
+
+  return 0;
+}
