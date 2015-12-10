e87099870caf8bffab61e989bea433f5bace0eab
kbr <kbr@chromium.org>
2015-12-10 15:46:01 -0800

    Expose WebGL EXT_disjoint_timer_query extension unconditionally.
    This extension's now community approved and should be exposed.
    
    Tested with the existing
    conformance/extensions/ext-disjoint-timer-query.html test.
    
    BUG=560620
    
    Review URL: https://codereview.chromium.org/1475503002
    
    Cr-Commit-Position: refs/heads/master@{#364529}

--- third_party/WebKit/Source/modules/webgl/WebGL2RenderingContext.cpp
+++ third_party/WebKit/Source/modules/webgl/WebGL2RenderingContext.cpp
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/loader/FrameLoaderClient.h"
 #include "modules/webgl/CHROMIUMSubscribeUniform.h"
 #include "modules/webgl/EXTColorBufferFloat.h"
+#include "modules/webgl/EXTDisjointTimerQuery.h"
 #include "modules/webgl/EXTTextureFilterAnisotropic.h"
 #include "modules/webgl/OESTextureFloatLinear.h"
 #include "modules/webgl/WebGLCompressedTextureASTC.h"
@@ -81,6 +82,7 @@ void WebGL2RenderingContext::registerContextExtensions()
     // Register extensions.
     registerExtension<CHROMIUMSubscribeUniform>(m_chromiumSubscribeUniform);
     registerExtension<EXTColorBufferFloat>(m_extColorBufferFloat, DraftExtension);
+    registerExtension<EXTDisjointTimerQuery>(m_extDisjointTimerQuery);
     registerExtension<EXTTextureFilterAnisotropic>(m_extTextureFilterAnisotropic);
     registerExtension<OESTextureFloatLinear>(m_oesTextureFloatLinear);
     registerExtension<WebGLCompressedTextureASTC>(m_webglCompressedTextureASTC);
@@ -97,6 +99,7 @@ DEFINE_TRACE(WebGL2RenderingContext)
 {
     visitor->trace(m_chromiumSubscribeUniform);
     visitor->trace(m_extColorBufferFloat);
+    visitor->trace(m_extDisjointTimerQuery);
     visitor->trace(m_extTextureFilterAnisotropic);
     visitor->trace(m_oesTextureFloatLinear);
     visitor->trace(m_webglCompressedTextureASTC);
