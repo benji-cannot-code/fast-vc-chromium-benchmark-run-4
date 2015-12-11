1ea97c52f92b4f09bc0e2f9230c3c4d66d1dfb10
Nico Weber <thakis@chromium.org>
2015-12-11 17:25:11 -0500

    Cherry-pick crashpad b9e732d3180f9eadc74d7229090e613d0700b8a6.
    BUG=567877
    R=rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/1515183004 .
    
    Cr-Commit-Position: refs/heads/master@{#364821}

--- third_party/crashpad/crashpad/util/win/exception_handler_server.cc
+++ third_party/crashpad/crashpad/util/win/exception_handler_server.cc
@@ -348,7 +348,7 @@ std::wstring ExceptionHandlerServer::CreatePipe() {
 void ExceptionHandlerServer::Run(Delegate* delegate) {
   uint64_t shutdown_token = base::RandUint64();
   ScopedKernelHANDLE thread_handles[kPipeInstances];
-  for (int i = 0; i < arraysize(thread_handles); ++i) {
+  for (size_t i = 0; i < arraysize(thread_handles); ++i) {
     HANDLE pipe;
     if (first_pipe_instance_.is_valid()) {
       pipe = first_pipe_instance_.release();
@@ -400,7 +400,7 @@ void ExceptionHandlerServer::Run(Delegate* delegate) {
   }
 
   // Signal to the named pipe instances that they should terminate.
-  for (int i = 0; i < arraysize(thread_handles); ++i) {
+  for (size_t i = 0; i < arraysize(thread_handles); ++i) {
     ClientToServerMessage message;
     memset(&message, 0, sizeof(message));
     message.type = ClientToServerMessage::kShutdown;
