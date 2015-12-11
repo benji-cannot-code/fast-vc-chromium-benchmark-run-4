ae7a637999c8c53ba45b59d223c2dc4820c2bfa5
dbeam <dbeam@chromium.org>
2015-12-10 16:13:20 -0800

    MD Downloads: declare/initialize ActionService#searchTerms_ in ctor
    This prevents weirdness with multiple instances of ActionService. This
    probably shouldn't affect a singleton class much, but doesn't hurt and
    also changes how the property is set up in the __proto__ chain (i.e.
    setting it and deleting it would affect the same property descriptor).
    
    R=esprehn@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/1513483008
    
    Cr-Commit-Position: refs/heads/master@{#364539}

--- chrome/browser/resources/md_downloads/action_service.js
+++ chrome/browser/resources/md_downloads/action_service.js
@@ -13,7 +13,10 @@ cr.define('downloads', function() {
   }
 
   /** @constructor */
-  function ActionService() {}
+  function ActionService() {
+    /** @private {Array<string>} */
+    this.searchTerms_ = [];
+  }
 
   /**
    * @param {string} s
@@ -37,9 +40,6 @@ cr.define('downloads', function() {
   };
 
   ActionService.prototype = {
-    /** @private {Array<string>} */
-    searchTerms_: [],
-
     /** @param {string} id ID of the download to cancel. */
     cancel: chromeSendWithId('cancel'),
 
--- chrome/browser/resources/md_downloads/crisper.js
+++ chrome/browser/resources/md_downloads/crisper.js
@@ -1530,7 +1530,10 @@ cr.define('downloads', function() {
   }
 
   /** @constructor */
-  function ActionService() {}
+  function ActionService() {
+    /** @private {Array<string>} */
+    this.searchTerms_ = [];
+  }
 
   /**
    * @param {string} s
@@ -1554,9 +1557,6 @@ cr.define('downloads', function() {
   };
 
   ActionService.prototype = {
-    /** @private {Array<string>} */
-    searchTerms_: [],
-
     /** @param {string} id ID of the download to cancel. */
     cancel: chromeSendWithId('cancel'),
 
