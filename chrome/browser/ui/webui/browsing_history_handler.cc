202a87ed51fd13d1e734308555b3f776f52d2acb
msramek <msramek@chromium.org>
2015-12-11 01:20:11 -0800

    Remove entries from "Recent tabs" if corresponding history is deleted.
    Suppose a user deletes a history entry "embarassingsite.com". We should understand that the user's intention is to hide their tracks and remove the corresponding entries from the "Recent tabs" menu. Note that we already do this with the "Delete browsing data" button, just not with individual deletions.
    
    An entry for "embarassingsite.com" in the history UI consists of potentially several visits, represented as:
    ["embarassingsite.com", last visit timestamp]
    ["embarassingsite.com", previous visit timestamp]
    [ ... ]
    
    We remove every tab from the "Recent tabs" menu whose navigation *ended* with one of these visits. Furthermore, if there are any windows in the "Recent tabs" menu, we remove the tab from them.
    
    The result is that opening the "Recent tabs" menu, and even restoring all windows from there will not give away that the user once visited "embarassingsite.com".
    
    What this CL doesn't do:
    1. The other way around: if a history entry is deleted and a tab is closed after, it will still appear in "Recent tabs". I can fix this in a follow-up CL, but I think this is the more probable order of actions in the typical "I'm going to hide my tracks" use case.
    2. On any restored tab, it's still possible to use the "Back" button to get to "embarassingsite.com", if the tab's navigation didn't end there. Nevertheless, it's not so obvious from the "Recent tabs" menu, and more importantly it's better what we currently have.
    
    BUG=458078
    
    Review URL: https://codereview.chromium.org/954723003
    
    Cr-Commit-Position: refs/heads/master@{#364653}

--- chrome/browser/ui/webui/browsing_history_handler.cc
+++ chrome/browser/ui/webui/browsing_history_handler.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/history_utils.h"
 #include "chrome/browser/history/web_history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/sessions/tab_restore_service_factory.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/chrome_pages.h"
@@ -40,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/web_history_service.h"
 #include "components/keyed_service/core/service_access_type.h"
 #include "components/query_parser/snippet.h"
+#include "components/sessions/core/tab_restore_service.h"
 #include "components/sync_driver/device_info.h"
 #include "components/sync_driver/device_info_tracker.h"
 #include "components/url_formatter/url_formatter.h"
@@ -447,6 +449,8 @@ void BrowsingHistoryHandler::HandleRemoveVisits(const base::ListValue* args) {
                                            ServiceAccessType::EXPLICIT_ACCESS);
   history::WebHistoryService* web_history =
       WebHistoryServiceFactory::GetForProfile(profile);
+  sessions::TabRestoreService* tab_service =
+      TabRestoreServiceFactory::GetForProfile(profile);
 
   base::Time now = base::Time::Now();
   std::vector<history::ExpireHistoryArgs> expire_list;
@@ -484,8 +488,8 @@ void BrowsingHistoryHandler::HandleRemoveVisits(const base::ListValue* args) {
         continue;
       }
       base::Time visit_time = base::Time::FromJsTime(timestamp);
+      GURL gurl(url);
       if (!expire_args) {
-        GURL gurl(url);
         expire_list.resize(expire_list.size() + 1);
         expire_args = &expire_list.back();
         expire_args->SetTimeRangeForOneDay(visit_time);
@@ -494,6 +498,9 @@ void BrowsingHistoryHandler::HandleRemoveVisits(const base::ListValue* args) {
       }
       // The local visit time is treated as a global ID for the visit.
       global_id_directive->add_global_id(visit_time.ToInternalValue());
+
+      // Remove this entry from the recent tabs menu.
+      tab_service->RemoveTabByLastVisit(gurl, visit_time);
     }
 
     // Set the start and end time in microseconds since the Unix epoch.
