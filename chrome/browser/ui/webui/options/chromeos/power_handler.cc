628ed22ef05755cf91f3a3667b5745a61d121420
michaelpg <michaelpg@chromium.org>
2015-12-11 10:07:30 -0800

    Move USB Type-C device descriptions to Ash
    This moves the strings to Ash so they can be used for notifications.
    This also has the advantage of making dependents more flexible for
    when we move from using descriptions based solely on the port, to
    more useful descriptions based on the device name and whatnot.
    
    BUG=547260
    
    Review URL: https://codereview.chromium.org/1509673010
    
    Cr-Commit-Position: refs/heads/master@{#364733}

--- chrome/browser/ui/webui/options/chromeos/power_handler.cc
+++ chrome/browser/ui/webui/options/chromeos/power_handler.cc
@@ -22,40 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using ash::PowerStatus;
 
 namespace chromeos {
-namespace {
-
-// Returns the message ID corresponding to the port location of a source.
-int GetPowerSourceDescription(const PowerStatus::PowerSource& source) {
-  switch (source.port) {
-    case PowerStatus::UNKNOWN_PORT:
-      return IDS_OPTIONS_POWER_SOURCE_PORT_UNKNOWN;
-    case PowerStatus::LEFT_PORT:
-      return IDS_OPTIONS_POWER_SOURCE_PORT_LEFT;
-    case PowerStatus::RIGHT_PORT:
-      return IDS_OPTIONS_POWER_SOURCE_PORT_RIGHT;
-    case PowerStatus::BACK_PORT:
-      return IDS_OPTIONS_POWER_SOURCE_PORT_BACK;
-    case PowerStatus::FRONT_PORT:
-      return IDS_OPTIONS_POWER_SOURCE_PORT_FRONT;
-    case PowerStatus::LEFT_FRONT_PORT:
-      return IDS_OPTIONS_POWER_SOURCE_PORT_LEFT_FRONT;
-    case PowerStatus::LEFT_BACK_PORT:
-      return IDS_OPTIONS_POWER_SOURCE_PORT_LEFT_BACK;
-    case PowerStatus::RIGHT_FRONT_PORT:
-      return IDS_OPTIONS_POWER_SOURCE_PORT_RIGHT_FRONT;
-    case PowerStatus::RIGHT_BACK_PORT:
-      return IDS_OPTIONS_POWER_SOURCE_PORT_RIGHT_BACK;
-    case PowerStatus::BACK_LEFT_PORT:
-      return IDS_OPTIONS_POWER_SOURCE_PORT_BACK_LEFT;
-    case PowerStatus::BACK_RIGHT_PORT:
-      return IDS_OPTIONS_POWER_SOURCE_PORT_BACK_RIGHT;
-  }
-  NOTREACHED();
-  return IDS_OPTIONS_POWER_SOURCE_PORT_UNKNOWN;
-}
-
-}  // namespace
-
 namespace options {
 
 PowerHandler::PowerHandler() {
@@ -188,8 +154,8 @@ void PowerHandler::UpdatePowerSources() {
     scoped_ptr<base::DictionaryValue> dict(new base::DictionaryValue());
     dict->SetString("id", source.id);
     dict->SetInteger("type", source.type);
-    int message_id = GetPowerSourceDescription(source);
-    dict->SetString("description", l10n_util::GetStringUTF16(message_id));
+    dict->SetString("description",
+                    l10n_util::GetStringUTF16(source.description_id));
     sources_list.Append(dict.release());
   }
 
