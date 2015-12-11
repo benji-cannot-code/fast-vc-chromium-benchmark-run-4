f9392d47f06fbda805e9100cde55082959e1d49b
mnaganov <mnaganov@chromium.org>
2015-12-11 11:12:49 -0800

    [Android WebView] Update microdump generation for sandboxed renderers
    Now microdump handling has three modes depending on the process type:
    
    "webview"    -- displays crash dialog, supports dump on demand;
    "" (browser) -- displays crash dialog, doesn't support dump on demand;
    "renderer"   -- doesn't display crash dialog, no dump on demand.
    
    Dump on demand may be enabled later if we figure out how to do it
    properly for sandboxed renderers.
    
    Exported `InitMicrodumpCrashHandlerIfNecessary` as calling
    `InitNonBrowserCrashReporterForAndroid` for that purpose is misleading,
    and WebView doesn't need any of the functionality that it can enable.
    
    BUG=568825
    
    Review URL: https://codereview.chromium.org/1518913002
    
    Cr-Commit-Position: refs/heads/master@{#364766}

--- android_webview/crash_reporter/aw_microdump_crash_reporter.h
+++ android_webview/crash_reporter/aw_microdump_crash_reporter.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace android_webview {
 namespace crash_reporter {
 
-void EnableMicrodumpCrashReporter();
+void EnableMicrodumpCrashReporter(const std::string& process_type);
 void AddGpuFingerprintToMicrodumpCrashHandler(
     const std::string& gpu_fingerprint);
 bool DumpWithoutCrashingToFd(int fd);
