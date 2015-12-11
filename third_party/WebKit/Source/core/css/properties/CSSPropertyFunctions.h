ba9b4a2f8abcc567fb47249fe98bfbac45297735
sashab <sashab@chromium.org>
2015-12-10 19:38:00 -0800

    Add the CSS Property Functions API
    Add the CSS Property Functions API to the core/css/properties/ folder,
    with no callsites.
    
    This API is implemented as a parent class with static methods. Child
    classes implement static methods with the same name and signature
    and these are copied into 'descriptor' objects, which are groups of
    function pointers. Later, these descriptors will also include any
    necessary state from runtime property registration, which will be
    added as parameters to all necessary API functions.
    
    BUG=545324
    
    Review URL: https://codereview.chromium.org/1431773002
    
    Cr-Commit-Position: refs/heads/master@{#364604}

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyFunctions.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CSSPropertyFunctions_h
+#define CSSPropertyFunctions_h
+
+#include "core/CSSPropertyNames.h"
+#include "core/css/CSSValue.h"
+#include "wtf/PassRefPtr.h"
+
+namespace blink {
+
+class CSSParserContext;
+class CSSParserTokenRange;
+
+// The parent class for all properties that implement the CSSPropertyDescriptor API.
+// Contains static function signatures as a reference of what needs to be implemented
+// in the child classes.
+class CSSPropertyFunctions {
+    STATIC_ONLY(CSSPropertyFunctions);
+public:
+    static CSSPropertyID id();
+
+    // Consumes and parses a single value for this property from by the token range,
+    // returning the corresponding CSSValue. This function does not check for the end
+    // of the token range. Returns nullptr if the input is invalid.
+    static PassRefPtrWillBeRawPtr<CSSValue> parseSingleValue(CSSParserTokenRange&, const CSSParserContext&);
+    using parseSingleValueFunction = decltype(&parseSingleValue);
+};
+
+} // namespace blink
+
+#endif // CSSPropertyFunctions_h
