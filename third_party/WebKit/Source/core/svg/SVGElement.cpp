cf307f9cbbd5fd074ad236c1c1db236f51216f96
alancutter <alancutter@chromium.org>
2015-12-11 02:29:16 -0800

    Fix crash in stable when reading Web Animated SVG animVals
    This change fixes a crash when reading an SVG element's animVal with
    Web Animations in stable where SVG attribute Web Animations are not yet
    enabled by adding additional RuntimeEnabledFeature flag checks.
    
    BUG=568916
    
    Review URL: https://codereview.chromium.org/1520813002
    
    Cr-Commit-Position: refs/heads/master@{#364659}

--- third_party/WebKit/Source/core/svg/SVGElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGElement.cpp
@@ -939,6 +939,9 @@ void SVGElement::svgAttributeBaseValChanged(const QualifiedName& attribute)
 
 void SVGElement::ensureAttributeAnimValUpdated()
 {
+    if (!RuntimeEnabledFeatures::webAnimationsSVGEnabled())
+        return;
+
     if ((hasSVGRareData() && svgRareData()->webAnimatedAttributesDirty())
         || (elementAnimations() && DocumentAnimations::needsAnimationTimingUpdate(document()))) {
         DocumentAnimations::updateAnimationTimingIfNeeded(document());
