5ee558e6fcf7180fbecb99d290c61249675fec27
dgn <dgn@chromium.org>
2015-12-11 11:41:47 -0800

    Support setting dynamic http auth policies on WebView
    Port of the chrome CL that added dynamic updating of auth policies in
    https://crrev.com/bba302d0ced354b71853bbf3582f4034ea825b9b
    
    BUG=533513
    
    Review URL: https://codereview.chromium.org/1499713002
    
    Cr-Commit-Position: refs/heads/master@{#364777}

--- android_webview/browser/net/aw_url_request_context_getter.h
+++ android_webview/browser/net/aw_url_request_context_getter.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/files/file_path.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/prefs/pref_member.h"
 #include "content/public/browser/content_browser_client.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "net/url_request/url_request_job_factory.h"
@@ -74,6 +75,10 @@ class AwURLRequestContextGetter : public net::URLRequestContextGetter {
   scoped_ptr<net::HttpAuthHandlerFactory> CreateAuthHandlerFactory(
       net::HostResolver* resolver);
 
+  // Update methods for the auth related preferences
+  void UpdateServerWhitelist();
+  void UpdateAndroidAuthNegotiateAccountType();
+
   const base::FilePath cache_path_;
 
   scoped_ptr<net::NetLog> net_log_;
@@ -87,8 +92,8 @@ class AwURLRequestContextGetter : public net::URLRequestContextGetter {
   scoped_ptr<net::URLRequestContext> url_request_context_;
 
   // Store HTTP Auth-related policies in this thread.
-  std::string auth_android_negotiate_account_type_;
-  std::string auth_server_whitelist_;
+  StringPrefMember auth_android_negotiate_account_type_;
+  StringPrefMember auth_server_whitelist_;
 
   // ProtocolHandlers and interceptors are stored here between
   // SetHandlersAndInterceptors() and the first GetURLRequestContext() call.
