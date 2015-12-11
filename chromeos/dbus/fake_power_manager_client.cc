d264b56aae99149003484d7b56f8c6a0dc89a277
michaelpg <michaelpg@chromium.org>
2015-12-10 22:16:59 -0800

    Add power sources in Device Emulator battery settings
    This lets us add fake power sources (A/C adapters, USB adapters,
    USB TYpe-C devices) for manually testing power UI.
    
    BUG=547260
    
    Review URL: https://codereview.chromium.org/1505403002
    
    Cr-Commit-Position: refs/heads/master@{#364631}

--- chromeos/dbus/fake_power_manager_client.cc
+++ chromeos/dbus/fake_power_manager_client.cc
@@ -13,6 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
+namespace {
+// Minimum power for a USB power source to be classified as AC.
+const double kUsbMinAcWatts = 24;
+}
+
 FakePowerManagerClient::FakePowerManagerClient()
     : num_request_restart_calls_(0),
       num_request_shutdown_calls_(0),
@@ -112,7 +117,22 @@ void FakePowerManagerClient::SetIsProjecting(bool is_projecting) {
   is_projecting_ = is_projecting;
 }
 
-void FakePowerManagerClient::SetPowerSource(const std::string& id) {}
+void FakePowerManagerClient::SetPowerSource(const std::string& id) {
+  props_.set_external_power_source_id(id);
+  props_.set_external_power(
+      power_manager::PowerSupplyProperties_ExternalPower_DISCONNECTED);
+  for (const auto& source : props_.available_external_power_source()) {
+    if (source.id() == id) {
+      props_.set_external_power(
+          !source.active_by_default() || source.max_power() < kUsbMinAcWatts
+              ? power_manager::PowerSupplyProperties_ExternalPower_USB
+              : power_manager::PowerSupplyProperties_ExternalPower_AC);
+      break;
+    }
+  }
+
+  NotifyObservers();
+}
 
 base::Closure FakePowerManagerClient::GetSuspendReadinessCallback() {
   ++num_pending_suspend_readiness_callbacks_;
