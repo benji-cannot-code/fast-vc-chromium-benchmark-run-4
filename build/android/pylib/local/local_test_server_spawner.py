bf1e39afc6c775a73a0eb20a826e8961bfec0d89
jbudorick <jbudorick@chromium.org>
2015-12-11 11:09:59 -0800

    [Android] Move forwarder.py into devil/.
    BUG=476719
    
    Review URL: https://codereview.chromium.org/1516363002
    
    Cr-Commit-Position: refs/heads/master@{#364762}

--- build/android/pylib/local/local_test_server_spawner.py
+++ build/android/pylib/local/local_test_server_spawner.py
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from devil.android import forwarder
 from pylib import chrome_test_server_spawner
-from pylib import forwarder
 from pylib.base import test_server
 
 
