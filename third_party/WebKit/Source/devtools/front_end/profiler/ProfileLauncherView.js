dbdc3b080ac9120ab5551f62f5d422d107f45a30
dgozman <dgozman@chromium.org>
2015-12-10 16:55:24 -0800

    [DevTools] Remove ActionToolbarButton, use regular ToolbarButton instead.
    BUG=none
    
    Review URL: https://codereview.chromium.org/1511553006
    
    Cr-Commit-Position: refs/heads/master@{#364554}

--- third_party/WebKit/Source/devtools/front_end/profiler/ProfileLauncherView.js
+++ third_party/WebKit/Source/devtools/front_end/profiler/ProfileLauncherView.js
@@ -107,7 +107,7 @@ WebInspector.ProfileLauncherView.prototype = {
 
     _controlButtonClicked: function()
     {
-        this._panel.toggleRecordButton();
+        this._panel.toggleRecord();
     },
 
     _loadButtonClicked: function()
@@ -220,7 +220,7 @@ WebInspector.MultiProfileLauncherView.prototype = {
 
     _controlButtonClicked: function()
     {
-        this._panel.toggleRecordButton();
+        this._panel.toggleRecord();
     },
 
     _updateControls: function()
