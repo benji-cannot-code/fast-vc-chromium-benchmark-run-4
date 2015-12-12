3a8b0aaf0ee7a8dee6c95ad982559107d1fb3ee1
sergeyu <sergeyu@chromium.org>
2015-12-11 22:33:43 -0800

    Cleanup SessionManager interface.
    Removed SessionManager::Listener::OnSessionManagerReady(). It's not
    needed because it was used only in ConnectionToClientImpl and that
    class listents for state change notifications directly from
    SignalStrategy.
    
    Committed: https://crrev.com/74e642a43bece7db86e261a0e111a42f4243e8e9
    Cr-Commit-Position: refs/heads/master@{#364596}
    
    Review URL: https://codereview.chromium.org/1513943003
    
    Cr-Commit-Position: refs/heads/master@{#364915}

--- remoting/protocol/jingle_session_manager.h
+++ remoting/protocol/jingle_session_manager.h
@@ -6,27 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_PROTOCOL_JINGLE_SESSION_MANAGER_H_
 #define REMOTING_PROTOCOL_JINGLE_SESSION_MANAGER_H_
 
-#include <list>
 #include <map>
 #include <string>
 
-#include "base/memory/ref_counted.h"
-#include "net/cert/x509_certificate.h"
 #include "remoting/protocol/jingle_messages.h"
 #include "remoting/protocol/session_manager.h"
 #include "remoting/signaling/signal_strategy.h"
 
-namespace pp {
-class Instance;
-}  // namespace pp
-
 namespace buzz {
 class XmlElement;
 }  // namespace buzz
-
-namespace rtc {
-class SocketAddress;
-}  // namespace rtc
 
 namespace remoting {
 
@@ -67,11 +56,6 @@ class JingleSessionManager : public SessionManager,
 
   typedef std::map<std::string, JingleSession*> SessionsMap;
 
-  void OnJingleInfo(
-      const std::string& relay_token,
-      const std::vector<std::string>& relay_hosts,
-      const std::vector<rtc::SocketAddress>& stun_hosts);
-
   IqSender* iq_sender() { return iq_sender_.get(); }
   void SendReply(const buzz::XmlElement* original_stanza,
                  JingleMessageReply::ErrorType error);
@@ -88,8 +72,6 @@ class JingleSessionManager : public SessionManager,
   scoped_ptr<AuthenticatorFactory> authenticator_factory_;
   scoped_ptr<IqSender> iq_sender_;
   SessionManager::Listener* listener_;
-
-  bool ready_;
 
   SessionsMap sessions_;
 
