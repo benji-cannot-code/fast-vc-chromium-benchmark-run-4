75824fc73cd700876672341e4ad629de2b36771a
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-12-11 19:39:35 -0800

    [Extensions UI Mac] Make the installed bubble match the views version
    Make the Mac extension installed bubble more closely match the views version.
    Specifically:
    - Don't show the "how to manage" text with a keybinding.
    - Don't right-align the "manage shortcuts" link.
    
    Also move more of the logic out of the platform-specific implementations into
    the platform abstract class so this happens less frequently.
    
    XIB Changes:
    * Move the manage shortcuts link to be aligned with the other text.
    
    BUG=569115
    BUG=458301
    
    Review URL: https://codereview.chromium.org/1518043002
    
    Cr-Commit-Position: refs/heads/master@{#364879}

--- chrome/browser/ui/cocoa/extensions/extension_installed_bubble_controller.h
+++ chrome/browser/ui/cocoa/extensions/extension_installed_bubble_controller.h
@@ -37,14 +37,11 @@ const int kInnerVerticalMargin = 10;
 const int kAppsBubbleArrowOffset = 4;
 
 // We use a different kind of notification for each of these extension types.
-typedef enum {
+enum ExtensionType {
   kApp,
-  kBrowserAction,
-  kGeneric,
-  kOmniboxKeyword,
-  kPageAction,
+  kExtension,
   kBundle,
-} ExtensionType;
+};
 
 }  // namespace extension_installed_bubble
 
@@ -96,6 +93,7 @@ typedef enum {
 }
 
 @property(nonatomic, readonly) const extensions::BundleInstaller* bundle;
+@property(nonatomic, readonly) ExtensionInstalledBubble* installedBubble;
 @property(nonatomic) BOOL pageActionPreviewShowing;
 
 // Initialize the window. It will be shown by the BubbleManager.
@@ -141,7 +139,6 @@ typedef enum {
 - (NSRect)frameOfHowToUse;
 - (NSRect)frameOfHowToManage;
 - (NSRect)frameOfSigninPromo;
-- (BOOL)showSyncPromo;
 - (NSButton*)appInstalledShortcutLink;
 - (void)updateAnchorPosition;
 
