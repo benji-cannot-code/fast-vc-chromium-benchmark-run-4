5b66eeac86814c1e3762a76cb4887fbd051aeecf
sigbjornf <sigbjornf@opera.com>
2015-12-13 04:25:37 -0800

    Oilpan: fixup Handle.h include.
    R=haraken
    BUG=357163
    
    Review URL: https://codereview.chromium.org/1520083002
    
    Cr-Commit-Position: refs/heads/master@{#364950}

--- third_party/WebKit/Source/core/dom/TreeShared.h
+++ third_party/WebKit/Source/core/dom/TreeShared.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef TreeShared_h
 #define TreeShared_h
 
-#include "public/platform/WebPrivatePtr.h"
+#include "platform/heap/Handle.h"
 #include "wtf/Assertions.h"
 #include "wtf/MainThread.h"
 #include "wtf/Noncopyable.h"
