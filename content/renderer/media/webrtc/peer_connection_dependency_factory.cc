a0cbf52ed6d492c4308eaecf7b49fc61c87c350b
tommi <tommi@chromium.org>
2015-12-13 12:08:52 -0800

    Add support for cloning remote MediaStreamAudioTracks. This also includes support for per-track enabled state for sinks that receive unmixed audio. The shared state of a remote WebRTC source continues to be shared and is affected by changing any of the tracks' enabled state that are associated with the same source.
    BUG=569369
    
    Review URL: https://codereview.chromium.org/1524633002
    
    Cr-Commit-Position: refs/heads/master@{#364952}

--- content/renderer/media/webrtc/peer_connection_dependency_factory.cc
+++ content/renderer/media/webrtc/peer_connection_dependency_factory.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/rtc_video_decoder_factory.h"
 #include "content/renderer/media/rtc_video_encoder_factory.h"
 #include "content/renderer/media/webaudio_capturer_source.h"
+#include "content/renderer/media/webrtc/media_stream_remote_audio_track.h"
 #include "content/renderer/media/webrtc/stun_field_trial.h"
 #include "content/renderer/media/webrtc/webrtc_local_audio_track_adapter.h"
 #include "content/renderer/media/webrtc/webrtc_video_capturer_adapter.h"
@@ -571,6 +572,7 @@ void PeerConnectionDependencyFactory::CreateLocalAudioTrack(
     const blink::WebMediaStreamTrack& track) {
   blink::WebMediaStreamSource source = track.source();
   DCHECK_EQ(source.type(), blink::WebMediaStreamSource::TypeAudio);
+  DCHECK(!source.remote());
   MediaStreamAudioSource* source_data =
       static_cast<MediaStreamAudioSource*>(source.extraData());
 
@@ -583,9 +585,7 @@ void PeerConnectionDependencyFactory::CreateLocalAudioTrack(
       source_data =
           static_cast<MediaStreamAudioSource*>(source.extraData());
     } else {
-      // TODO(perkj): Implement support for sources from
-      // remote MediaStreams.
-      NOTIMPLEMENTED();
+      NOTREACHED() << "Local track missing source extra data.";
       return;
     }
   }
@@ -608,6 +608,18 @@ void PeerConnectionDependencyFactory::CreateLocalAudioTrack(
   // Pass the ownership of the native local audio track to the blink track.
   blink::WebMediaStreamTrack writable_track = track;
   writable_track.setExtraData(audio_track.release());
+}
+
+void PeerConnectionDependencyFactory::CreateRemoteAudioTrack(
+    const blink::WebMediaStreamTrack& track) {
+  blink::WebMediaStreamSource source = track.source();
+  DCHECK_EQ(source.type(), blink::WebMediaStreamSource::TypeAudio);
+  DCHECK(source.remote());
+  DCHECK(source.extraData());
+
+  blink::WebMediaStreamTrack writable_track = track;
+  writable_track.setExtraData(
+      new MediaStreamRemoteAudioTrack(source, track.isEnabled()));
 }
 
 void PeerConnectionDependencyFactory::StartLocalAudioTrack(
