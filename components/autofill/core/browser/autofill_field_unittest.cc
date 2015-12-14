4a255111fc58b0b2409471cea6b15f735ae2a02e
sebsg <sebsg@chromium.org>
2015-12-14 09:47:30 -0800

    [Autofill] Change Fev to Feb in autofill_field_unittest.cc
    Introduced in https://codereview.chromium.org/1507113004/
    
    Review URL: https://codereview.chromium.org/1522963002
    
    Cr-Commit-Position: refs/heads/master@{#365044}

--- components/autofill/core/browser/autofill_field_unittest.cc
+++ components/autofill/core/browser/autofill_field_unittest.cc
@@ -27,13 +27,13 @@ namespace {
 #endif
 
 const std::vector<const char*> kNotNumericMonthsContentsNoPlaceholder = {
-    "Jan", "Fev", "Mar", "Apr", "May", "Jun",
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
 const std::vector<const char*> kNotNumericMonthsContentsWithPlaceholder = {
     "Select a Month",
     "Jan",
-    "Fev",
+    "Feb",
     "Mar",
     "Apr",
     "May",
