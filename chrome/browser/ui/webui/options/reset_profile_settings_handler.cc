56dc3c58412575cad162476bcbef35ec7aa66e85
courage <courage@chromium.org>
2015-12-14 14:58:59 -0800

    Revert of Migrate sending reset reports to a new endpoint using protobufs. (patchset #7 id:120001 of https://codereview.chromium.org/1503953002/ )
    Reason for revert:
    This looks like it's breaking Windows GN compiles:
    
    https://build.chromium.org/p/chromium.win/buildstatus?builder=Win%20x64%20GN%20%28dbg%29&number=15132
    https://build.chromium.org/p/chromium.win/buildstatus?builder=Win8%20GN%20%28dbg%29&number=18329
    
    (not related to PrerenderBrowserTests, which I think are fixed now)
    
    Original issue's description:
    > Migrate sending reset reports to a new endpoint using protobufs.
    >
    > TBR=dbeam@chromium.org
    > BUG=235037
    >
    > Committed: https://crrev.com/0b9b347bef4480ca5393afdcd583678591298900
    > Cr-Commit-Position: refs/heads/master@{#365100}
    
    TBR=jochen@chromium.org,dbeam@chromium.org,vasilii@chromium.org,battre@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=235037
    
    Review URL: https://codereview.chromium.org/1524673004
    
    Cr-Commit-Position: refs/heads/master@{#365116}

--- chrome/browser/ui/webui/options/reset_profile_settings_handler.cc
+++ chrome/browser/ui/webui/options/reset_profile_settings_handler.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/google/google_brand.h"
 #include "chrome/browser/profile_resetter/brandcode_config_fetcher.h"
 #include "chrome/browser/profile_resetter/brandcoded_default_settings.h"
-#include "chrome/browser/profile_resetter/profile_reset_report.pb.h"
 #include "chrome/browser/profile_resetter/profile_resetter.h"
 #include "chrome/browser/profile_resetter/resettable_settings_snapshot.h"
 #include "chrome/browser/profiles/profile.h"
@@ -151,12 +150,6 @@ void ResetProfileSettingsHandler::OnResetProfileSettingsDone(
       std::string report = SerializeSettingsReport(*setting_snapshot_,
                                                    difference);
       SendSettingsFeedback(report, profile);
-
-      // Send the same report as a protobuf to a different endpoint.
-      scoped_ptr<reset_report::ChromeResetReport> report_proto =
-          SerializeSettingsReportToProto(*setting_snapshot_, difference);
-      if (report_proto)
-        SendSettingsFeedbackProto(*report_proto, profile);
     }
   }
   setting_snapshot_.reset();
