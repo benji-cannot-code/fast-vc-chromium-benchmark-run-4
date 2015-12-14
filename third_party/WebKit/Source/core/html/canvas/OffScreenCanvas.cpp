1ac27fc12205941d96f1b70d94fa922da5f4975a
xidachen <xidachen@chromium.org>
2015-12-14 12:57:51 -0800

    Create an empty interface OffScreenCanvas
    This CL creates an interface OffScreenCanvas.idl according to the proposal:
    https://wiki.whatwg.org/wiki/OffscreenCanvas
    
    At this moment, the interface only has a constructor, and its implementation is empty.
    
    Also, a layout test is added to verify that the constructor
    can be successfully called from JS, and verify the object's
    width&height, together with verifying the constructor's behavior when
    the argument is invalid.
    
    BUG=563819
    
    Review URL: https://codereview.chromium.org/1488763002
    
    Cr-Commit-Position: refs/heads/master@{#365095}

---
+++ third_party/WebKit/Source/core/html/canvas/OffScreenCanvas.cpp
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "core/html/canvas/OffScreenCanvas.h"
+
+#include "wtf/MathExtras.h"
+
+namespace blink {
+
+OffScreenCanvas* OffScreenCanvas::create(unsigned width, unsigned height)
+{
+    return new OffScreenCanvas(IntSize(clampTo<int>(width), clampTo<int>(height)));
+}
+
+void OffScreenCanvas::setWidth(unsigned width)
+{
+    m_size.setWidth(clampTo<int>(width));
+}
+
+void OffScreenCanvas::setHeight(unsigned height)
+{
+    m_size.setHeight(clampTo<int>(height));
+}
+
+OffScreenCanvas::OffScreenCanvas(const IntSize& size)
+    : m_size(size)
+{
+}
+
+DEFINE_TRACE(OffScreenCanvas)
+{
+}
+
+} // namespace blink
