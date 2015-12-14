7d90048a00ad4f48ed71b856b1ffe2dd1b6e9da8
sdefresne <sdefresne@chromium.org>
2015-12-14 03:00:45 -0800

    Add factory to create WeakPtr<WebState>.
    Usage of this class should be limited to supporting features forked
    out of //chrome until they can be properly cleaned up to use proper
    lifetime management of they pointer to WebState.
    
    BUG=567727
    
    Review URL: https://codereview.chromium.org/1517213003
    
    Cr-Commit-Position: refs/heads/master@{#364998}

---
+++ ios/web/web_state/web_state_weak_ptr_factory.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/web/web_state/web_state_weak_ptr_factory.h"
+
+#include "ios/web/public/web_state/web_state.h"
+
+namespace web {
+
+// static
+base::WeakPtr<WebState> WebStateWeakPtrFactory::CreateWeakPtr(
+    WebState* web_state) {
+  if (!web_state)
+    return base::WeakPtr<WebState>();
+  return web_state->AsWeakPtr();
+}
+
+}  // namespace web
