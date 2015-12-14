c74b1e49909763c6fae1c378024e7be9ed02751d
karandeepb <karandeepb@chromium.org>
2015-12-13 21:43:36 -0800

    Mac: Fix browser crash on CloseAllAppWindows with MacViews native app windows.
    Currently, with MacViews native app windows enabled, the call to
    AppWindowRegistryUtil::CloseAllAppWindows() causes a crash. This is
    because, the Close() operation on MacViews native app windows is
    async. This CL fixes the crash by simply iterating over the app window
    list, and closing them one by one. Also, changes are done, to enable
    app windows on Mac to close, even if the app has file dialogs open.
    
    BUG=506115
    
    Review URL: https://codereview.chromium.org/1514453009
    
    Cr-Commit-Position: refs/heads/master@{#364969}

--- chrome/browser/ui/cocoa/apps/quit_with_apps_controller_mac_interactive_uitest.mm
+++ chrome/browser/ui/cocoa/apps/quit_with_apps_controller_mac_interactive_uitest.mm
@@ -31,14 +31,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// The param selects whether to use ChromeNativeAppWindowViewsMac, otherwise it
+// will use NativeAppWindowCocoa.
 class QuitWithAppsControllerInteractiveTest
-    : public extensions::PlatformAppBrowserTest {
+    : public testing::WithParamInterface<bool>,
+      public extensions::PlatformAppBrowserTest {
  protected:
   QuitWithAppsControllerInteractiveTest() : app_(NULL) {}
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
     PlatformAppBrowserTest::SetUpCommandLine(command_line);
     command_line->AppendSwitch(switches::kAppsKeepChromeAliveInTests);
+    command_line->AppendSwitch(
+        GetParam() ? switches::kEnableMacViewsNativeAppWindows
+                   : switches::kDisableMacViewsNativeAppWindows);
   }
 
   const extensions::Extension* app_;
@@ -50,7 +56,7 @@ class QuitWithAppsControllerInteractiveTest
 }  // namespace
 
 // Test that quitting while apps are open shows a notification instead.
-IN_PROC_BROWSER_TEST_F(QuitWithAppsControllerInteractiveTest, QuitBehavior) {
+IN_PROC_BROWSER_TEST_P(QuitWithAppsControllerInteractiveTest, QuitBehavior) {
   scoped_refptr<QuitWithAppsController> controller =
       new QuitWithAppsController();
   const Notification* notification;
@@ -113,6 +119,9 @@ IN_PROC_BROWSER_TEST_F(QuitWithAppsControllerInteractiveTest, QuitBehavior) {
   EXPECT_FALSE(chrome::BrowserIterator().done());
   EXPECT_TRUE(AppWindowRegistryUtil::IsAppWindowVisibleInAnyProfile(0));
 
+  // Get a reference to the open app window before the browser closes.
+  extensions::AppWindow* app_window = GetFirstAppWindow();
+
   // Quitting should not quit but close all browsers
   content::WindowedNotificationObserver observer(
       chrome::NOTIFICATION_BROWSER_CLOSED,
@@ -135,14 +144,19 @@ IN_PROC_BROWSER_TEST_F(QuitWithAppsControllerInteractiveTest, QuitBehavior) {
   content::WindowedNotificationObserver quit_observer(
       chrome::NOTIFICATION_APP_TERMINATING,
       content::NotificationService::AllSources());
+
+  // Since closing app windows may be an async operation, use a watcher.
+  content::WebContentsDestroyedWatcher destroyed_watcher(
+      app_window->web_contents());
   notification->delegate()->ButtonClick(0);
+  destroyed_watcher.Wait();
   message_center->RemoveAllNotifications(false);
   EXPECT_FALSE(AppWindowRegistryUtil::IsAppWindowVisibleInAnyProfile(0));
   quit_observer.Wait();
 }
 
 // Test that, when powering off, Chrome will quit even if there are apps open.
-IN_PROC_BROWSER_TEST_F(QuitWithAppsControllerInteractiveTest, QuitOnPowerOff) {
+IN_PROC_BROWSER_TEST_P(QuitWithAppsControllerInteractiveTest, QuitOnPowerOff) {
   // Open an app window.
   app_ = LoadAndLaunchPlatformApp("minimal_id", "Launched");
 
@@ -163,3 +177,7 @@ IN_PROC_BROWSER_TEST_F(QuitWithAppsControllerInteractiveTest, QuitOnPowerOff) {
   [NSApp terminate:nil];
   EXPECT_TRUE(browser_shutdown::IsTryingToQuit());
 }
+
+INSTANTIATE_TEST_CASE_P(QuitWithAppsControllerInteractiveTestInstance,
+                        QuitWithAppsControllerInteractiveTest,
+                        ::testing::Bool());
