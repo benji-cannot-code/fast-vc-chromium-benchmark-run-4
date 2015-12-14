78288ef0f98208792b7c946ea938f6ef7c1bd065
fukino <fukino@chromium.org>
2015-12-13 23:49:10 -0800

    AudioPlayer: Set initial volume level to 50%.
    AudioPlayer used to have defalut 50% for volume level.
    However, when I removed <volume-controller> and integrate the function into
    <control-panel>, I missed to move the default volume level.
    This CL sets the default volume level as a part of control panel's spec.
    
    BUG=568866
    TEST=manually
    
    Review URL: https://codereview.chromium.org/1520143002
    
    Cr-Commit-Position: refs/heads/master@{#364982}

--- ui/file_manager/audio_player/elements/control_panel.js
+++ ui/file_manager/audio_player/elements/control_panel.js
@@ -91,6 +91,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
        */
       volume: {
         type: Number,
+        value: 50,
         notify: true,
         reflectToAttribute: true,
         observer: 'volumeChanged_'
