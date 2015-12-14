cdc6bace8d01ca9e95eb056a1d98470e555958d2
junov <junov@chromium.org>
2015-12-14 12:52:33 -0800

    Fix state restoration logic in canvas 2d wakeup from hibernation
    This fixes an important detail that was overlooked in
    https://codereview.chromium.org/1507863005/: After a Canvas2DLayerBridge
    wakes up from hibernation, if deferred rendering is disabled, the
    matrix+clip stack must be reconstructed on the new SkCanas. Same
    thing when a hibernating canvas switches to software rendering mode for
    rendering in the background.
    
    BUG=567161
    
    Review URL: https://codereview.chromium.org/1520123002
    
    Cr-Commit-Position: refs/heads/master@{#365093}

--- third_party/WebKit/Source/platform/graphics/ImageBuffer.h
+++ third_party/WebKit/Source/platform/graphics/ImageBuffer.h
@@ -73,7 +73,7 @@ public:
     static PassOwnPtr<ImageBuffer> create(const IntSize&, OpacityMode = NonOpaque, ImageInitializationMode = InitializeImagePixels);
     static PassOwnPtr<ImageBuffer> create(PassOwnPtr<ImageBufferSurface>);
 
-    ~ImageBuffer();
+    virtual ~ImageBuffer();
 
     void setClient(ImageBufferClient* client) { m_client = client; }
 
@@ -91,8 +91,9 @@ public:
     void setFilterQuality(SkFilterQuality filterQuality) { m_surface->setFilterQuality(filterQuality); }
     void setIsHidden(bool hidden) { m_surface->setIsHidden(hidden); }
 
-    // Called by subclasses of ImageBufferSurface to install a new canvas object
-    void resetCanvas(SkCanvas*) const;
+    // Called by subclasses of ImageBufferSurface to install a new canvas object.
+    // Virtual for mocking
+    virtual void resetCanvas(SkCanvas*) const;
 
     SkCanvas* canvas() const;
     void disableDeferral() const;
@@ -136,9 +137,10 @@ public:
     static intptr_t getGlobalGPUMemoryUsage() { return s_globalGPUMemoryUsage; }
     intptr_t getGPUMemoryUsage() { return m_gpuMemoryUsage; }
 
-private:
+protected:
     ImageBuffer(PassOwnPtr<ImageBufferSurface>);
 
+private:
     enum SnapshotState {
         InitialSnapshotState,
         DidAcquireSnapshot,
