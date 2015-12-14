22a065dc0171b27a940a05beac2ddc3bb98a0b1f
jbudorick <jbudorick@chromium.org>
2015-12-14 13:35:46 -0800

    [telemetry] Catch NoAdbError when killing the adb server.
    BUG=567797,568151
    
    Review URL: https://codereview.chromium.org/1526633003
    
    Cr-Commit-Position: refs/heads/master@{#365102}

--- build/android/devil/android/sdk/adb_wrapper.py
+++ build/android/devil/android/sdk/adb_wrapper.py
@@ -116,7 +116,7 @@ class AdbWrapper(object):
           timeout_retry.CurrentTimeoutThreadGroup().GetRemainingTime())
     except OSError as e:
       if e.errno in (errno.ENOENT, errno.ENOEXEC):
-        raise device_errors.NoAdbError()
+        raise device_errors.NoAdbError(msg=str(e))
       else:
         raise
 
