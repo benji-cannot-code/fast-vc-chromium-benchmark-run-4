a54cdc973f5406c4e4e167d5774efbb8167d1e8c
cmumford <cmumford@chromium.org>
2015-12-14 13:24:39 -0800

    Extensions: No longer creating a snapshot when iterating values.
    Being that there are no simultaneous writes, etc. eliminating the
    unnecessary leveldb snapshot. Additionally, setting leveldb's verify
    checksums flag to detect leveldb corruption.
    
    Review URL: https://codereview.chromium.org/1527613004
    
    Cr-Commit-Position: refs/heads/master@{#365099}

--- extensions/browser/value_store/leveldb_value_store.h
+++ extensions/browser/value_store/leveldb_value_store.h
@@ -75,8 +75,7 @@ class LeveldbValueStore : public ValueStore,
   ValueStore::Status EnsureDbIsOpen();
 
   // Reads a setting from the database.
-  ValueStore::Status ReadFromDb(leveldb::ReadOptions options,
-                                const std::string& key,
+  ValueStore::Status ReadFromDb(const std::string& key,
                                 // Will be reset() with the result, if any.
                                 scoped_ptr<base::Value>* setting);
 
@@ -107,6 +106,7 @@ class LeveldbValueStore : public ValueStore,
   // The location of the leveldb backend.
   const base::FilePath db_path_;
   leveldb::Options open_options_;
+  leveldb::ReadOptions read_options_;
 
   // leveldb backend.
   scoped_ptr<leveldb::DB> db_;
