6fdba455bb05882b9ecc527a34eb41bd6db8ba4a
kulshin <kulshin@chromium.org>
2015-12-14 15:12:54 -0800

    Create direct write font proxy classes and unit tests.
    BUG=525142
    
    Committed: https://crrev.com/42290cb91a66566637a1715737b1961a8480b934
    Cr-Commit-Position: refs/heads/master@{#363785}
    
    Committed: https://crrev.com/772bb2e55ce932909dc9ac934eabb4c47dd638d4
    Cr-Commit-Position: refs/heads/master@{#364445}
    
    Review URL: https://codereview.chromium.org/1438603002
    
    Cr-Commit-Position: refs/heads/master@{#365122}

--- ui/gfx/win/direct_write.h
+++ ui/gfx/win/direct_write.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GFX_WIN_DIRECT_WRITE_H_
 #define UI_GFX_WIN_DIRECT_WRITE_H_
 
+#include <dwrite.h>
+
 #include "ui/gfx/gfx_export.h"
 
 namespace gfx {
@@ -18,6 +20,9 @@ GFX_EXPORT void MaybeInitializeDirectWrite();
 
 // Returns true if we are using DirectWrite for font metrics and rendering.
 GFX_EXPORT bool IsDirectWriteEnabled();
+
+// Creates a DirectWrite factory, if using DirectWrite.
+GFX_EXPORT void CreateDWriteFactory(IDWriteFactory** factory);
 
 }  // namespace win
 }  // namespace gfx
