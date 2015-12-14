7ea520a8c8cfb565ee07f42eb8bc4a65fdf60247
wangxianzhu <wangxianzhu@chromium.org>
2015-12-14 11:19:21 -0800

    Fix viewport scrollbar invalidation issue
    https://codereview.chromium.org/1491193003/ moved composited scrollbar
    invalidation into PaintInvalidationCapableScrollableArea, but not all
    ScrollableAreas are paint invalidation capable. Move the invalidation
    back into ScrollableArea, while keep the path to handle composited
    scrollbar position change affecting box invalidation.
    
    TEST=ScrollableAreaTest.ScrollbarGraphicsLayerInvalidation
    BUG=569181
    
    Review URL: https://codereview.chromium.org/1522603002
    
    Cr-Commit-Position: refs/heads/master@{#365063}

--- third_party/WebKit/Source/platform/scroll/ScrollableAreaTest.cpp
+++ third_party/WebKit/Source/platform/scroll/ScrollableAreaTest.cpp
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "platform/scroll/ScrollableArea.h"
 
+#include "platform/graphics/GraphicsLayer.h"
 #include "platform/scroll/ScrollbarTheme.h"
 #include "platform/scroll/ScrollbarThemeMock.h"
 #include "platform/testing/TestingPlatformSupport.h"
@@ -18,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 namespace {
+
+using testing::Return;
 
 class MockScrollableArea : public NoBaseWillBeGarbageCollectedFinalized<MockScrollableArea>, public ScrollableArea {
     WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(MockScrollableArea);
@@ -36,6 +39,7 @@ public:
     MOCK_CONST_METHOD1(visibleContentRect, IntRect(IncludeScrollbarsInRect));
     MOCK_CONST_METHOD0(contentsSize, IntSize());
     MOCK_CONST_METHOD0(scrollableAreaBoundingBox, IntRect());
+    MOCK_CONST_METHOD0(layerForHorizontalScrollbar, GraphicsLayer*());
 
     bool userInputScrollable(ScrollbarOrientation) const override { return true; }
     bool scrollbarsCanBeActive () const override { return true; }
@@ -89,7 +93,8 @@ public:
 // We need just enough scaffolding for the Timer constructor to not segfault.
 class FakePlatform : public TestingPlatformSupport {
 public:
-    FakePlatform() { }
+    explicit FakePlatform(Config& config) : TestingPlatformSupport(config) { }
+
     ~FakePlatform() override { }
 
     WebThread* currentThread() override
@@ -110,16 +115,20 @@ public:
     void SetUp() override
     {
         m_oldPlatform = Platform::current();
-        Platform::initialize(&m_fakePlatform);
+        TestingPlatformSupport::Config config;
+        config.compositorSupport = m_oldPlatform->compositorSupport();
+        m_fakePlatform = adoptPtr(new FakePlatform(config));
+        Platform::initialize(m_fakePlatform.get());
     }
 
     void TearDown() override
     {
         Platform::initialize(m_oldPlatform);
+        m_fakePlatform = nullptr;
     }
 
 private:
-    FakePlatform m_fakePlatform;
+    OwnPtr<FakePlatform> m_fakePlatform;
     Platform* m_oldPlatform; // Not owned.
 };
 
@@ -160,6 +169,36 @@ TEST_F(ScrollableAreaTest, ScrollbarTrackAndThumbRepaint)
     scrollbar->setNeedsPaintInvalidation();
     EXPECT_FALSE(scrollbar->trackNeedsRepaint());
     EXPECT_FALSE(scrollbar->thumbNeedsRepaint());
+}
+
+class MockGraphicsLayerClient : public GraphicsLayerClient {
+public:
+    IntRect computeInterestRect(const GraphicsLayer*, const IntRect&) const { return IntRect(); }
+    void paintContents(const GraphicsLayer*, GraphicsContext&, GraphicsLayerPaintingPhase, const IntRect&) const override { }
+    String debugName(const GraphicsLayer*) const override { return String(); }
+    bool isTrackingPaintInvalidations() const override { return true; }
+};
+
+class MockGraphicsLayer : public GraphicsLayer {
+public:
+    explicit MockGraphicsLayer(GraphicsLayerClient* client) : GraphicsLayer(client) { }
+};
+
+TEST_F(ScrollableAreaTest, ScrollbarGraphicsLayerInvalidation)
+{
+    ScrollbarTheme::setMockScrollbarsEnabled(true);
+    OwnPtrWillBeRawPtr<MockScrollableArea> scrollableArea = MockScrollableArea::create(IntPoint(0, 100));
+    MockGraphicsLayerClient graphicsLayerClient;
+    MockGraphicsLayer graphicsLayer(&graphicsLayerClient);
+    graphicsLayer.setDrawsContent(true);
+    graphicsLayer.setSize(FloatSize(111, 222));
+
+    EXPECT_CALL(*scrollableArea, layerForHorizontalScrollbar()).WillRepeatedly(Return(&graphicsLayer));
+
+    RefPtrWillBeRawPtr<Scrollbar> scrollbar = Scrollbar::create(scrollableArea.get(), HorizontalScrollbar, RegularScrollbar);
+    graphicsLayer.resetTrackedPaintInvalidations();
+    scrollbar->setNeedsPaintInvalidation();
+    EXPECT_TRUE(graphicsLayer.hasTrackedPaintInvalidations());
 }
 
 } // namespace blink
