4235115f5a9e87649547faefd3a44d66f44fe8ab
nhiroki <nhiroki@chromium.org>
2015-12-13 22:47:06 -0800

    ServiceWorker: Add tests for the case where SWProcessManager allocates an existing process
    BUG=566986
    
    Review URL: https://codereview.chromium.org/1506013004
    
    Cr-Commit-Position: refs/heads/master@{#364975}

--- content/public/test/mock_render_process_host.h
+++ content/public/test/mock_render_process_host.h
@@ -98,6 +98,8 @@ class MockRenderProcessHost : public RenderProcessHost {
                                          int cdm_id) const override;
 #endif
   bool IsProcessBackgrounded() const override;
+  void IncrementWorkerRefCount() override;
+  void DecrementWorkerRefCount() override;
 
   // IPC::Sender via RenderProcessHost.
   bool Send(IPC::Message* msg) override;
@@ -127,6 +129,8 @@ class MockRenderProcessHost : public RenderProcessHost {
   void GetAudioOutputControllers(
       const GetAudioOutputControllersCallback& callback) const override {}
 
+  int worker_ref_count() const { return worker_ref_count_; }
+
  private:
   // Stores IPC messages that would have been sent to the renderer.
   IPC::TestSink sink_;
@@ -145,6 +149,7 @@ class MockRenderProcessHost : public RenderProcessHost {
   bool is_for_guests_only_;
   bool is_process_backgrounded_;
   scoped_ptr<base::ProcessHandle> process_handle;
+  int worker_ref_count_;
 
   DISALLOW_COPY_AND_ASSIGN(MockRenderProcessHost);
 };
