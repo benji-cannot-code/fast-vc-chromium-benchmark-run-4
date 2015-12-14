a5fbf586428c534c58666074036b6fdf298672bc
jyasskin <jyasskin@chromium.org>
2015-12-14 15:15:04 -0800

    Revert of [Sync] Enable ProfileSyncServiceAutofillTest.ServerChangeRace (patchset #1 id:1 of https://codereview.chromium.org/1511333003/ )
    Reason for revert:
    This is broken on the DrMemory bot: http://build.chromium.org/p/chromium.memory.fyi/builders/Windows%20Unit%20%28DrMemory%29/builds/3417/steps/memory%20test%3A%20unit/logs/stdio
    
    [ RUN      ] ProfileSyncServiceAutofillTest.ServerChangeRace
    
    GMOCK WARNING:
    Uninteresting mock function call - returning directly.
        Function call: RegisterDataTypes(@05A1F340 8-byte object <C0-B7 D2-11 0E-BB 12-00>)
    NOTE: You can safely ignore the above warning unless this call should not happen.  Do not suppress it by blindly adding an EXPECT_CALL() if you don't mean to enforce the call.  See http://code.google.com/p/googlemock/wiki/CookBook#Knowing_When_to_Expect for details.
    [3560:2356:1210/173205:14452494:WARNING:sync_backend_registrar.cc(95)] No history worker -- removing TYPED_URLS
    [3560:2356:1210/173205:14452494:WARNING:sync_backend_registrar.cc(99)] No password worker -- removing PASSWORDS
    [3560:2356:1210/173205:14452494:WARNING:sync_encryption_handler_impl.cc(989)] Nigori had empty encryption keybag.
    [3560:2436:1210/173205:14452697:WARNING:sync_backend_registrar.cc(95)] No history worker -- removing TYPED_URLS
    [3560:2436:1210/173205:14452697:WARNING:sync_backend_registrar.cc(99)] No password worker -- removing PASSWORDS
    [3560:2436:1210/173205:14452697:WARNING:sync_backend_registrar.cc(95)] No history worker -- removing TYPED_URLS
    [3560:2436:1210/173205:14452697:WARNING:sync_backend_registrar.cc(99)] No password worker -- removing PASSWORDS
    c:\b\build\slave\drm-cr\build\src\chrome\browser\sync\profile_sync_service_autofill_unittest.cc(1318): error: Value of: sync_entries.size()
      Actual: 2
    Expected: 3U
    Which is: 3
    c:\b\build\slave\drm-cr\build\src\chrome\browser\sync\profile_sync_service_autofill_unittest.cc(1284): error: Actual function call count doesn't match EXPECT_CALL(*personal_data_manager_, Refresh())...
             Expected: to be called 3 times
               Actual: called twice - unsatisfied and active
    [  FAILED  ] ProfileSyncServiceAutofillTest.ServerChangeRace (356 ms)
    
    Original issue's description:
    > [Sync] Enable ProfileSyncServiceAutofillTest.ServerChangeRace
    >
    > Test was disabled because number of calls to
    > PersonalDataManager::Refresh didn't match expectation. I think this
    > happened because calls to Refresh were posted to UI thread, but test
    > didn't run message loop at the end of the test.
    >
    > BUG=57884
    > R=zea@chromium.org
    >
    > Committed: https://crrev.com/14e9197c285a1edc95dc7e2f314f814fcc0a853f
    > Cr-Commit-Position: refs/heads/master@{#364439}
    
    TBR=zea@chromium.org,pavely@chromium.org
    BUG=57884
    
    Review URL: https://codereview.chromium.org/1523083002
    
    Cr-Commit-Position: refs/heads/master@{#365123}

--- chrome/browser/sync/profile_sync_service_autofill_unittest.cc
+++ chrome/browser/sync/profile_sync_service_autofill_unittest.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/run_loop.h"
+#include "base/message_loop/message_loop.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/synchronization/waitable_event.h"
@@ -527,9 +527,7 @@ class ProfileSyncServiceAutofillTest
 
     sync_service_->RegisterDataTypeController(CreateDataTypeController(type));
     sync_service_->Initialize();
-
-    base::RunLoop run_loop;
-    run_loop.Run();
+    base::MessageLoop::current()->Run();
 
     // It's possible this test triggered an unrecoverable error, in which case
     // we can't get the sync count.
@@ -1269,8 +1267,8 @@ TEST_F(ProfileSyncServiceAutofillTest, ProcessUserChangeRemoveProfile) {
   ASSERT_EQ(0U, new_sync_profiles.size());
 }
 
-// crbug.com/569551
-TEST_F(ProfileSyncServiceAutofillTest, FLAKY_ServerChangeRace) {
+// http://crbug.com/57884
+TEST_F(ProfileSyncServiceAutofillTest, DISABLED_ServerChangeRace) {
   // Once for MergeDataAndStartSyncing() and twice for ProcessSyncChanges(), via
   // LoadAutofillData().
   EXPECT_CALL(autofill_table_, GetAllAutofillEntries(_)).
@@ -1306,10 +1304,6 @@ TEST_F(ProfileSyncServiceAutofillTest, FLAKY_ServerChangeRace) {
   // Make another entry to ensure nothing broke afterwards and wait for finish
   // to clean up.
   updater->CreateNewEntryAndWait(MakeAutofillEntry("server2", "entry2", 3));
-
-  // Let callbacks posted on UI thread execute.
-  base::RunLoop run_loop;
-  run_loop.RunUntilIdle();
 
   std::vector<AutofillEntry> sync_entries;
   std::vector<AutofillProfile> sync_profiles;
