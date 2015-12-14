37225d24b2b1583977ba7743b79bc5ed44036c15
spang <spang@chromium.org>
2015-12-14 14:37:49 -0800

    Fix GN deps for various ozone bits
    This fixes various missing dependencies found by "gn check". In
    addition to added deps, many files are moved to ozone_base so that
    they're visible to the platforms.
    
    There's still a remaining issue where ozone_platform_foo.cc includes
    ozone_platform.h, but does not depend on it (and cannot without creating
    a cycle). //ui/ozone/platform/foo is actually source_set that's intended
    solely to add more files to the //ui/ozone component, so while this is a
    GN target cycle it is not a component (.so) cycle.
    
    We could break this target cycle by changing the platform build files to
    .gni files, so that they merely list their files instead of putting them
    into a source_set.
    
    We could also break the cycle by splitting ozone_platform.h into two
    files, and renaming OzonePlatform::GetInstance() to ui::GetOzonePlatform()
    so that it can be declared in the 2nd file. Then the implementation can
    depend on the interface from //ui/ozone:ozone_base, and the global getter
    can depend on the implementation(s) from //ui/ozone.
    
    For now, this just exempts these includes by adding a "// nogncheck"
    comment; they are not causing a real issue.
    
    Review URL: https://codereview.chromium.org/1511773008
    
    Cr-Commit-Position: refs/heads/master@{#365113}

--- ui/events/ozone/evdev/touch_event_converter_evdev.cc
+++ ui/events/ozone/evdev/touch_event_converter_evdev.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/ozone/evdev/touch_evdev_types.h"
 #include "ui/events/ozone/evdev/touch_noise/touch_noise_finder.h"
 #include "ui/ozone/public/input_controller.h"
-#include "ui/ozone/public/ozone_platform.h"
 
 namespace {
 
--- ui/ozone/platform/drm/client_native_pixmap_factory_gbm.cc
+++ ui/ozone/platform/drm/client_native_pixmap_factory_gbm.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/drm/client_native_pixmap_factory_gbm.h"
 
 #include "ui/gfx/native_pixmap_handle_ozone.h"
-#include "ui/ozone/public/client_native_pixmap_factory.h"
+#include "ui/ozone/public/client_native_pixmap_factory.h"  // nogncheck
 
 #if defined(USE_VGEM_MAP)
 #include "ui/ozone/platform/drm/common/client_native_pixmap_vgem.h"
