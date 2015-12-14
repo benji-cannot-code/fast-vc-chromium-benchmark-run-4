4425ba76db1c1262499f52561c1a9c3be6000259
zqzhang <zqzhang@chromium.org>
2015-12-14 09:58:47 -0800

    Update media notification when page title changes
    This CL makes the media notification keep in sync when the page title
    changes.
    
    BUG=550424
    
    Committed: https://crrev.com/d0b0d5fe705a29ff989dd3708d61154e8c600024
    Cr-Commit-Position: refs/heads/master@{#364938}
    
    Review URL: https://codereview.chromium.org/1417743005
    
    Cr-Commit-Position: refs/heads/master@{#365047}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/ChromeActivityTestCaseBase.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/ChromeActivityTestCaseBase.java
@@ -353,8 +353,8 @@ public abstract class ChromeActivityTestCaseBase<T extends ChromeActivity>
      * Load a url in a new tab. The {@link Tab} will pretend to be created from a link.
      * @param url The url of the page to load.
      */
-    public void loadUrlInNewTab(final String url) throws InterruptedException {
-        loadUrlInNewTab(url, false);
+    public Tab loadUrlInNewTab(final String url) throws InterruptedException {
+        return loadUrlInNewTab(url, false);
     }
 
     /**
@@ -362,7 +362,7 @@ public abstract class ChromeActivityTestCaseBase<T extends ChromeActivity>
      * @param url The url of the page to load.
      * @param incognito Whether the new tab should be incognito.
      */
-    public void loadUrlInNewTab(final String url, final boolean incognito)
+    public Tab loadUrlInNewTab(final String url, final boolean incognito)
             throws InterruptedException {
         Tab tab = null;
         if (FeatureUtilities.isDocumentMode(getInstrumentation().getContext())) {
@@ -406,6 +406,7 @@ public abstract class ChromeActivityTestCaseBase<T extends ChromeActivity>
         }
         ChromeTabUtils.waitForTabPageLoaded(tab, url);
         getInstrumentation().waitForIdleSync();
+        return tab;
     }
 
     /**
