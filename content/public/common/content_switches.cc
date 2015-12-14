c1044a29ca841fd662db2f8b1291a00b7f54722d
penghuang <penghuang@chromium.org>
2015-12-14 09:58:23 -0800

    Move kEnableMojoShellConnection to right position.
    BUG=551250
    
    Review URL: https://codereview.chromium.org/1522543003
    
    Cr-Commit-Position: refs/heads/master@{#365046}

--- content/public/common/content_switches.cc
+++ content/public/common/content_switches.cc
@@ -383,6 +383,9 @@ const char kEnableLogging[]                 = "enable-logging";
 // Enables the memory benchmarking extension
 const char kEnableMemoryBenchmarking[]      = "enable-memory-benchmarking";
 
+// Enable the Mojo shell connection in renderers.
+const char kEnableMojoShellConnection[] = "enable-mojo-shell-connection";
+
 // Enables the network information API.
 const char kEnableNetworkInformation[]      = "enable-network-information";
 
@@ -988,9 +991,6 @@ const char kMemoryPressureThresholdsMb[] = "memory-pressure-thresholds-mb";
 // Windows Vista and later.
 const char kTraceExportEventsToETW[] = "trace-export-events-to-etw";
 #endif
-
-// Enable the Mojo shell connection in renderers.
-const char kEnableMojoShellConnection[] = "enable-mojo-shell-connection";
 
 // Don't dump stuff here, follow the same order as the header.
 
