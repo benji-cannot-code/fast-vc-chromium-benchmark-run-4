df754b78ef66a18c385b94b6b9dc0d0b7b80a20a
courage <courage@chromium.org>
2015-12-14 18:27:40 -0800

    Reland of Update media notification when page title changes (patchset #1 id:1 of https://codereview.chromium.org/1528563003/ )
    Reason for revert:
    revert broke the build, which seems worse than breaking the tests.
    
    Original issue's description:
    > Revert of Update media notification when page title changes (patchset #26 id:590001 of https://codereview.chromium.org/1417743005/ )
    >
    > Reason for revert:
    > It looks to me like this change is causing failures in the ChromeSyncShellTest, e.g.:
    > https://build.chromium.org/p/chromium.linux/builders/Android%20Tests%20%28dbg%29/builds/31876
    > https://build.chromium.org/p/chromium.linux/builders/Android%20Tests%20%28dbg%29/builds/31877
    >
    > Not sure why this builds successfully, but the signature change to ChromeActivityTestCaseBase.loadUrlInNewTab causes OpenTabsTest to fail at runtime.
    >
    > C   97.565s Main  [FAIL] org.chromium.chrome.browser.sync.OpenTabsTest#testUploadAndCloseOpenTab:
    > C   97.565s Main  java.lang.NoSuchMethodError: org.chromium.chrome.browser.sync.OpenTabsTest.loadUrlInNewTab
    > C   97.565s Main  	at org.chromium.chrome.browser.sync.OpenTabsTest.testUploadAndCloseOpenTab(OpenTabsTest.java:108)
    > C   97.566s Main  	at java.lang.reflect.Method.invokeNative(Native Method)
    > C   97.566s Main  	at android.test.InstrumentationTestCase.runMethod(InstrumentationTestCase.java:214)
    > C   97.566s Main  	at android.test.InstrumentationTestCase.runTest(InstrumentationTestCase.java:199)
    >
    > Original issue's description:
    > > Update media notification when page title changes
    > >
    > > This CL makes the media notification keep in sync when the page title
    > > changes.
    > >
    > > BUG=550424
    > >
    > > Committed: https://crrev.com/d0b0d5fe705a29ff989dd3708d61154e8c600024
    > > Cr-Commit-Position: refs/heads/master@{#364938}
    > >
    > > Committed: https://crrev.com/bb18959a99173041dd3f7bbf99b437749b9b79f6
    > > Cr-Commit-Position: refs/heads/master@{#365047}
    >
    > TBR=mlamouri@chromium.org,avayvod@chromium.org,tedchoc@chromium.org,zqzhang@chromium.org
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=550424
    >
    > Committed: https://crrev.com/022ddd8bed4270541d3858edf36d184b86b447bc
    > Cr-Commit-Position: refs/heads/master@{#365135}
    
    TBR=mlamouri@chromium.org,avayvod@chromium.org,tedchoc@chromium.org,zqzhang@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=550424
    
    Review URL: https://codereview.chromium.org/1523173003
    
    Cr-Commit-Position: refs/heads/master@{#365137}

--- content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsObserverProxy.java
+++ content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsObserverProxy.java
@@ -8,6 +8,7 @@ package org.chromium.content.browser.webcontents;
 import org.chromium.base.ObserverList;
 import org.chromium.base.ObserverList.RewindableIterator;
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.MainDex;
@@ -60,6 +61,14 @@ class WebContentsObserverProxy extends WebContentsObserver {
      */
     boolean hasObservers() {
         return !mObservers.isEmpty();
+    }
+
+    /**
+     * @return The list of proxied observers.
+     */
+    @VisibleForTesting
+    public ObserverList.RewindableIterator<WebContentsObserver> getObserversForTesting() {
+        return mObservers.rewindableIterator();
     }
 
     @Override
