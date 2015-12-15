c694baabdaeb4f007e854ec1472c44a467163577
dmurph <dmurph@chromium.org>
2015-12-15 13:21:49 -0800

    [Blob] Fix for resetting reader.
    R=michaeln
    BUG=567692
    
    Committed-First-Time: https://crrev.com/ebf7a6b19a7a468195a5606fbd2bbd6d07a26ea2
    Cr-Commit-Position-First-Time: refs/heads/master@{#365177}
    
    Review URL: https://codereview.chromium.org/1513783005
    
    Cr-Commit-Position: refs/heads/master@{#365316}

--- storage/browser/blob/upload_blob_element_reader.h
+++ storage/browser/blob/upload_blob_element_reader.h
@@ -7,10 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define STORAGE_BROWSER_BLOB_UPLOAD_BLOB_ELEMENT_READER_H_
 
 #include "base/macros.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "net/base/completion_callback.h"
 #include "net/base/upload_element_reader.h"
 #include "storage/browser/storage_browser_export.h"
+
+namespace base {
+class SingleThreadTaskRunner;
+}
 
 namespace net {
 class IOBuffer;
@@ -19,6 +24,7 @@ class IOBuffer;
 namespace storage {
 class BlobDataHandle;
 class BlobReader;
+class FileSystemContext;
 
 // This class is a wrapper around the BlobReader to make it conform
 // to the net::UploadElementReader interface, and it also holds around the
@@ -26,8 +32,9 @@ class BlobReader;
 class STORAGE_EXPORT UploadBlobElementReader
     : NON_EXPORTED_BASE(public net::UploadElementReader) {
  public:
-  UploadBlobElementReader(scoped_ptr<BlobReader> reader,
-                          scoped_ptr<BlobDataHandle> handle);
+  UploadBlobElementReader(scoped_ptr<BlobDataHandle> handle,
+                          FileSystemContext* file_system_context,
+                          base::SingleThreadTaskRunner* file_task_runner);
   ~UploadBlobElementReader() override;
 
   int Init(const net::CompletionCallback& callback) override;
@@ -45,8 +52,10 @@ class STORAGE_EXPORT UploadBlobElementReader
   const std::string& uuid() const;
 
  private:
-  scoped_ptr<BlobReader> reader_;
   scoped_ptr<BlobDataHandle> handle_;
+  scoped_refptr<FileSystemContext> file_system_context_;
+  scoped_refptr<base::SingleThreadTaskRunner> file_runner_;
+  scoped_ptr<BlobReader> reader_;
 
   DISALLOW_COPY_AND_ASSIGN(UploadBlobElementReader);
 };
