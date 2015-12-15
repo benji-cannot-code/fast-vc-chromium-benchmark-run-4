834a204217cfc50d674a46c1060c0e3c6bca1e0b
nick <nick@chromium.org>
2015-12-15 15:22:31 -0800

    Add a rappor metric to track extension oopif cases.
    The hope here is to see a ranked list of extensions that are
    actually affected by --isolate-extensions
    
    BUG=546050
    TEST=None
    
    Review URL: https://codereview.chromium.org/1517803002
    
    Cr-Commit-Position: refs/heads/master@{#365382}

--- chrome/browser/extensions/chrome_extension_web_contents_observer.cc
+++ chrome/browser/extensions/chrome_extension_web_contents_observer.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/chrome_extension_web_contents_observer.h"
 
 #include "base/command_line.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/error_console/error_console.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/window_controller.h"
@@ -13,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/chrome_extension_messages.h"
 #include "chrome/common/extensions/extension_process_policy.h"
 #include "chrome/common/url_constants.h"
+#include "components/rappor/rappor_service.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/child_process_security_policy.h"
 #include "content/public/browser/render_frame_host.h"
@@ -206,6 +208,13 @@ void ChromeExtensionWebContentsObserver::SetExtensionIsolationTrial(
     } else {
       ChromeMetricsServiceAccessor::RegisterSyntheticFieldTrial(
           "SiteIsolationExtensionsActive", "Default");
+    }
+
+    if (rappor::RapporService* rappor = g_browser_process->rappor_service()) {
+      const std::string& extension_id =
+          parent_is_extension ? parent_url.host() : frame_url.host();
+      rappor->RecordSample("Extensions.AffectedByIsolateExtensions",
+                           rappor::UMA_RAPPOR_TYPE, extension_id);
     }
   }
 }
