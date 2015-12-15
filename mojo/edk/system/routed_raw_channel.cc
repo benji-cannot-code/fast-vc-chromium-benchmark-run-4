b38f5da3f13d5f11f929a447f336e605b2d5a540
jam <jam@chromium.org>
2015-12-15 10:08:45 -0800

    Followup fix to r365175 for new Mojo EDK.
    We need to ensure the channel is shutdown before RoutedRawChannel destructs.
    
    BUG=561803
    
    Review URL: https://codereview.chromium.org/1526093002
    
    Cr-Commit-Position: refs/heads/master@{#365286}

--- mojo/edk/system/routed_raw_channel.cc
+++ mojo/edk/system/routed_raw_channel.cc
@@ -86,6 +86,7 @@ void RoutedRawChannel::RemoveRoute(uint64_t route_id) {
 }
 
 RoutedRawChannel::~RoutedRawChannel() {
+  DCHECK(!channel_);
   destruct_callback_.Run(this);
 }
 
@@ -135,7 +136,7 @@ void RoutedRawChannel::OnError(Error error) {
 
   // This needs to match non-multiplexed MessagePipeDispatcher's destruction of
   // the channel only when read errors occur.
-  if (error != ERROR_WRITE) {
+  if (error != ERROR_WRITE || routes_.empty()) {
     channel_->Shutdown();
     channel_ = nullptr;
   }
