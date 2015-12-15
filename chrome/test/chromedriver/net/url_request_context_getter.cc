138d30b673bb3f508334bc0678a77233fdaa654d
olli.raula <olli.raula@intel.com>
2015-12-15 02:16:22 -0800

    Remove ScopedVector from url_request_context_builder
    Also added missing include to
    chrome/test/chromedriver/net/url_request_context_getter.cc
    
    TBR=samuong@chromium.org
    
    BUG=554289
    
    Review URL: https://codereview.chromium.org/1504383002
    
    Cr-Commit-Position: refs/heads/master@{#365221}

--- chrome/test/chromedriver/net/url_request_context_getter.cc
+++ chrome/test/chromedriver/net/url_request_context_getter.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/memory/scoped_ptr.h"
 #include "net/proxy/proxy_config_service_fixed.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_builder.h"
