e5aec0eeadd80e52dc424fe002858c8514800622
juncai <juncai@chromium.org>
2015-12-15 13:22:33 -0800

    Update BaseBubbleController's dealloc function to use unregisterFromNotifications
    This patch updated BaseBubbleController's dealloc function to use
    unregisterFromNotifications and updated code that uses it.
    
    Review URL: https://codereview.chromium.org/1529713002
    
    Cr-Commit-Position: refs/heads/master@{#365317}

--- chrome/browser/ui/cocoa/base_bubble_controller.mm
+++ chrome/browser/ui/cocoa/base_bubble_controller.mm
@@ -117,7 +117,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)dealloc {
-  [[NSNotificationCenter defaultCenter] removeObserver:self];
+  [self unregisterFromNotifications];
   [super dealloc];
 }
 
