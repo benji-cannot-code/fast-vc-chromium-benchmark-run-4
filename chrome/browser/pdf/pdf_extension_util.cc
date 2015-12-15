410c7db5290ee2858c92ac8a128f968119d0af2b
tsergeant <tsergeant@chromium.org>
2015-12-14 23:57:29 -0800

    PDF: Delete non-material viewer
    Removes all the viewer code for the old design, the --enable-pdf-material-ui
    flag, and the associated tests.
    
    BUG=439114
    
    Review URL: https://codereview.chromium.org/1521913002
    
    Cr-Commit-Position: refs/heads/master@{#365205}

--- chrome/browser/pdf/pdf_extension_util.cc
+++ chrome/browser/pdf/pdf_extension_util.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/string_util.h"
 #include "chrome/common/chrome_content_client.h"
-#include "chrome/common/chrome_switches.h"
 #include "chrome/grit/browser_resources.h"
 #include "ui/base/resource/resource_bundle.h"
 
@@ -17,12 +16,6 @@ namespace {
 
 // Tags in the manifest to be replaced.
 const char kNameTag[] = "<NAME>";
-const char kIndexTag[] = "<INDEX>";
-
-// The index html pages to load for the material and non-material version of
-// the viewer.
-const char kRegularIndex[] = "index.html";
-const char kMaterialIndex[] = "index-material.html";
 
 }  // namespace
 
@@ -42,10 +35,6 @@ std::string GetManifest() {
   base::ReplaceFirstSubstringAfterOffset(
       &manifest_contents, 0, kNameTag, ChromeContentClient::kPDFPluginName);
 
-  DCHECK(manifest_contents.find(kIndexTag) != std::string::npos);
-  std::string index = switches::PdfMaterialUIEnabled() ?
-      kMaterialIndex : kRegularIndex;
-  base::ReplaceSubstringsAfterOffset(&manifest_contents, 0, kIndexTag, index);
   return manifest_contents;
 }
 
