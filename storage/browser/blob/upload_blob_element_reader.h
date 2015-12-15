d4101cb0510d2fec834a9bd7092859969e007739
yoichio <yoichio@chromium.org>
2015-12-14 22:40:32 -0800

    Revert of [Blob] Fix for resetting reader. (patchset #8 id:130001 of https://codereview.chromium.org/1513783005/ )
    Reason for revert:
    CL was duplicated and compile failed.
    https://build.chromium.org/p/chromium.mac/builders/Mac%20GN/builds/22509
    https://chromium.googlesource.com/chromium/src/+log/ebf7a6b19a7a468195a5606fbd2bbd6d07a26ea2
    
    Original issue's description:
    > [Blob] Fix for resetting reader.
    >
    > R=michaeln
    > BUG=567692
    >
    > Committed: https://crrev.com/ebf7a6b19a7a468195a5606fbd2bbd6d07a26ea2
    > Cr-Commit-Position: refs/heads/master@{#365177}
    
    TBR=michaeln@chromium.org,mmenke@chromium.org,dmurph@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=567692
    
    Review URL: https://codereview.chromium.org/1529743002
    
    Cr-Commit-Position: refs/heads/master@{#365183}

--- storage/browser/blob/upload_blob_element_reader.h
+++ storage/browser/blob/upload_blob_element_reader.h
@@ -7,15 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define STORAGE_BROWSER_BLOB_UPLOAD_BLOB_ELEMENT_READER_H_
 
 #include "base/macros.h"
-#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "net/base/completion_callback.h"
 #include "net/base/upload_element_reader.h"
 #include "storage/browser/storage_browser_export.h"
-
-namespace base {
-class SingleThreadTaskRunner;
-}
 
 namespace net {
 class IOBuffer;
@@ -24,7 +19,6 @@ class IOBuffer;
 namespace storage {
 class BlobDataHandle;
 class BlobReader;
-class FileSystemContext;
 
 // This class is a wrapper around the BlobReader to make it conform
 // to the net::UploadElementReader interface, and it also holds around the
@@ -32,9 +26,8 @@ class FileSystemContext;
 class STORAGE_EXPORT UploadBlobElementReader
     : NON_EXPORTED_BASE(public net::UploadElementReader) {
  public:
-  UploadBlobElementReader(scoped_ptr<BlobDataHandle> handle,
-                          FileSystemContext* file_system_context,
-                          base::SingleThreadTaskRunner* file_task_runner);
+  UploadBlobElementReader(scoped_ptr<BlobReader> reader,
+                          scoped_ptr<BlobDataHandle> handle);
   ~UploadBlobElementReader() override;
 
   int Init(const net::CompletionCallback& callback) override;
@@ -52,10 +45,8 @@ class STORAGE_EXPORT UploadBlobElementReader
   const std::string& uuid() const;
 
  private:
-  scoped_ptr<BlobDataHandle> handle_;
-  scoped_refptr<FileSystemContext> file_system_context_;
-  scoped_refptr<base::SingleThreadTaskRunner> file_runner_;
   scoped_ptr<BlobReader> reader_;
+  scoped_ptr<BlobDataHandle> handle_;
 
   DISALLOW_COPY_AND_ASSIGN(UploadBlobElementReader);
 };
