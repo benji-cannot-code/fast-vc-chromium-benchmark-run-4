4680c3a5f86f974bb58e9a7252ddb57f23ded688
qiankun.miao <qiankun.miao@intel.com>
2015-12-14 22:21:34 -0800

    Replace Checked type with CheckedInt to stick with one style
    BUG=295792
    
    Review URL: https://codereview.chromium.org/1521973002
    
    Cr-Commit-Position: refs/heads/master@{#365180}

--- third_party/WebKit/Source/modules/webgl/WebGL2RenderingContextBase.cpp
+++ third_party/WebKit/Source/modules/webgl/WebGL2RenderingContextBase.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/webgl/WebGLTransformFeedback.h"
 #include "modules/webgl/WebGLUniformLocation.h"
 #include "modules/webgl/WebGLVertexArrayObject.h"
+#include "platform/CheckedInt.h"
 #include "platform/NotImplemented.h"
 #include "wtf/OwnPtr.h"
 #include "wtf/PassOwnPtr.h"
@@ -906,14 +907,14 @@ bool WebGL2RenderingContextBase::validateTexSubImage3D(const char* functionName,
     }
 
     // Before checking if it is in the range, check if overflow happens first.
-    Checked<GLint, RecordOverflow> maxX = xoffset, maxY = yoffset, maxZ = zoffset;
+    CheckedInt<GLint> maxX = xoffset, maxY = yoffset, maxZ = zoffset;
     maxX += width;
     maxY += height;
     maxZ += depth;
-    if (maxX.hasOverflowed() || maxY.hasOverflowed() || maxZ.hasOverflowed()
-        || maxX.unsafeGet() > tex->getWidth(target, level)
-        || maxY.unsafeGet() > tex->getHeight(target, level)
-        || maxZ.unsafeGet() > tex->getDepth(target, level)) {
+    if (!maxX.isValid() || !maxY.isValid() || !maxZ.isValid()
+        || maxX.value() > tex->getWidth(target, level)
+        || maxY.value() > tex->getHeight(target, level)
+        || maxZ.value() > tex->getDepth(target, level)) {
         synthesizeGLError(GL_INVALID_VALUE, functionName, "dimensions out of range");
         return false;
     }
