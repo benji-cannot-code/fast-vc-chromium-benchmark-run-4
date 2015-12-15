72aefcd441a6c96ec76163a27c0010741a816aaf
dmazzoni <dmazzoni@chromium.org>
2015-12-15 01:43:31 -0800

    Handle aria-owns and presentational children in AX name calc.
    Also renames rawFirstSibling to rawNextSibling, I think this was
    an accidental typo in a previous patch.
    
    BUG=561766
    
    Review URL: https://codereview.chromium.org/1477043003
    
    Cr-Commit-Position: refs/heads/master@{#365215}

--- third_party/WebKit/Source/modules/accessibility/AXARIAGrid.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXARIAGrid.cpp
@@ -95,7 +95,7 @@ void AXARIAGrid::addChildren()
         return;
 
     HeapVector<Member<AXObject>> children;
-    for (AXObject* child = rawFirstChild(); child; child = child->rawFirstSibling())
+    for (AXObject* child = rawFirstChild(); child; child = child->rawNextSibling())
         children.append(child);
     computeAriaOwnsChildren(children);
 
