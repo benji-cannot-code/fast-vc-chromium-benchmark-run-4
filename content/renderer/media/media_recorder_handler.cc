c4c9950495aa39d3145d112e6b359de2fb7d7697
mcasas <mcasas@chromium.org>
2015-12-14 23:08:20 -0800

    MediaRecorder: allow recording remote Audio Tracks
    After tommi@ Fixed http://crbug.com/121673, there's no need to
    avoid recording remote audio tracks, so this CL removes the
    extra code added previously.
    
    BUG=568819
    
    TEST= Navigate to
    https://rawgit.com/webrtc/samples/gh-pages/src/content/peerconnection/pc1/index.html
    Press buttons 'Start' and 'Call' in succession, to establish a remote call.
    Open the JS console and copy/type
     var mr = new MediaRecorder(remoteVideo.srcObject);
    then
     mr.ondataavailable=function(event){console.log("data available");}; mr.start(10)
    the JS console should start typing "data available" messages.
    
    Review URL: https://codereview.chromium.org/1520423002
    
    Cr-Commit-Position: refs/heads/master@{#365198}

--- content/renderer/media/media_recorder_handler.cc
+++ content/renderer/media/media_recorder_handler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_tokenizer.h"
 #include "base/strings/string_util.h"
 #include "content/renderer/media/audio_track_recorder.h"
+#include "content/renderer/media/media_stream_audio_track.h"
 #include "content/renderer/media/media_stream_track.h"
 #include "content/renderer/media/video_track_recorder.h"
 #include "content/renderer/media/webrtc_uma_histograms.h"
@@ -104,17 +105,14 @@ bool MediaRecorderHandler::start(int timeslice) {
   media_stream_.videoTracks(video_tracks);
   media_stream_.audioTracks(audio_tracks);
 
-
   if (video_tracks.isEmpty() && audio_tracks.isEmpty()) {
     LOG(WARNING) << __FUNCTION__ << ": no media tracks.";
     return false;
   }
 
-  // We cannot add ourselves as sink to a remote audio track, see
-  // http://crbug.com/121673 and MediaStreamAudioSink::AddToAudioTrack();
-  const bool use_audio_tracks = !audio_tracks.isEmpty() &&
-      MediaStreamTrack::GetTrack(audio_tracks[0]) &&
-      MediaStreamTrack::GetTrack(audio_tracks[0])->is_local_track();
+  const bool use_audio_tracks =
+      !audio_tracks.isEmpty() &&
+      MediaStreamAudioTrack::GetTrack(audio_tracks[0]);
 
   webm_muxer_.reset(new media::WebmMuxer(
       use_vp9_ ? media::kCodecVP9 : media::kCodecVP8,
