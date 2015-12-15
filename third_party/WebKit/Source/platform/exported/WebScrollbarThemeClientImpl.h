dff33c1de57f06c32a8f5eda0b04e47d0e48a716
jbroman <jbroman@chromium.org>
2015-12-15 14:07:51 -0800

    Remove Scrollbar::m_isAlphaLocked.
    I don't know what it used to do, but it doesn't seem to have done anything for
    several years (this variable can never become true).
    
    Review URL: https://codereview.chromium.org/1524303002
    
    Cr-Commit-Position: refs/heads/master@{#365353}

--- third_party/WebKit/Source/platform/exported/WebScrollbarThemeClientImpl.h
+++ third_party/WebKit/Source/platform/exported/WebScrollbarThemeClientImpl.h
@@ -76,8 +76,6 @@ public:
     bool enabled() const override;
     void setEnabled(bool) override;
     bool isOverlayScrollbar() const override;
-    bool isAlphaLocked() const override;
-    void setIsAlphaLocked(bool) override;
     float elasticOverscroll() const override;
     void setElasticOverscroll(float) override;
     bool trackNeedsRepaint() const override;
