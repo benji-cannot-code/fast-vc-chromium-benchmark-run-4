d7e04123609869af47c4d77b7037a8e4c19ac192
hcarmona <hcarmona@chromium.org>
2015-12-15 10:54:27 -0800

    Revert of Track where WebContents are created in order to better understand issue. (patchset #2 id:20001 of https://codereview.chromium.org/1488883002/ )
    Reason for revert:
    This was meant to be a temporary patch so that we could get crash dumps. We now have these crash dumps.
    
    Original issue's description:
    > Track where WebContents are created in order to better understand issue.
    >
    > This is a temporary change to track down a beta blocking crash.
    > This CL doesn't fix the crash, but will give us more information.
    >
    > The crash is happening when we attempt to show a modal dialog on some
    > unknown WebContents that doesn't have a WebModalDialogManager. Not all
    > WebContents are expecting to show modal dialogs and the creator of the
    > WebModalDialog doesn't control what WebContents they will get at
    > runtime. So, because of this loose coupling, we don't know which
    > WebContents is triggering the crash.
    >
    > By adding this trace we expect to learn where the WebContents are
    > created in order to make sure they have a WebModalDialogManager.
    >
    > BUG=538612
    >
    > Committed: https://crrev.com/a97dd34e9c8af59325e8478201487df0cac5c2af
    > Cr-Commit-Position: refs/heads/master@{#363595}
    
    TBR=wittman@chromium.org,nick@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=538612
    
    Review URL: https://codereview.chromium.org/1525263002
    
    Cr-Commit-Position: refs/heads/master@{#365299}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -2870,10 +2870,6 @@ void WebContentsImpl::ResumeLoadingCreatedWebContents() {
   }
 }
 
-base::debug::StackTrace WebContentsImpl::GetCreationStackTrace() {
-  return stack_trace_;
-}
-
 bool WebContentsImpl::FocusLocationBarByDefault() {
   NavigationEntry* entry = controller_.GetVisibleEntry();
   if (entry && entry->GetURL() == GURL(url::kAboutBlankURL))
