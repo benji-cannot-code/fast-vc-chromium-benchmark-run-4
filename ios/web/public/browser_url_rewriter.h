caf75b6fc30651673a667492b72dc90d593a44ab
sdefresne <sdefresne@chromium.org>
2015-12-15 09:55:44 -0800

    Upstream iOS implementation of security interstitial pages.
    Add an implementation for NavigationManager::Reload() based on what is
    done in NavigationControllerIOS downstream.
    
    Add missing #include.
    
    BUG=567727,350894
    
    Review URL: https://codereview.chromium.org/1522363003
    
    Cr-Commit-Position: refs/heads/master@{#365280}

--- ios/web/public/browser_url_rewriter.h
+++ ios/web/public/browser_url_rewriter.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_WEB_PUBLIC_BROWSER_URL_REWRITER_H_
 #define IOS_WEB_PUBLIC_BROWSER_URL_REWRITER_H_
 
+#include <vector>
+
 class GURL;
 
 namespace web {
