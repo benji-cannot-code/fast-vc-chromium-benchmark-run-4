aabcd698f9e349e33fe11a1e6660859db00ca64d
dgn <dgn@chromium.org>
2015-12-16 09:18:00 -0800

    Cleaning up and refactoring gms update/preprocess scripts
    Also, add a pylib.utils.argpars_utils.CustomHelpAction
    to support multiple help commands
    
    Changelog for the android_tools roll:
    https://chromium.googlesource.com/android_tools/+/54492f99c84cab0826a8e656efeb33a1b1bf5a04..f4c36ad89b2696b37d9cd7ca7d984b691888b188
    
    BUG=541727
    
    Review URL: https://codereview.chromium.org/1469913002
    
    Cr-Commit-Position: refs/heads/master@{#365548}

--- build/install-build-deps-android.sh
+++ build/install-build-deps-android.sh
@@ -15,11 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 args="$@"
 
-# TODO(dgn) remove this this argument from calls (http://crbug.com/541727)
-if test "$1" = "--skip-sdk-packages"; then
-  args="${@:2}"
-fi
-
 if ! uname -m | egrep -q "i686|x86_64"; then
   echo "Only x86 architectures are currently supported" >&2
   exit
