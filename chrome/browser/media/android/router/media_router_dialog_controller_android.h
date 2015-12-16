31ee6bde7ef7229047e4bbbfef31507880ee449e
avayvod <avayvod@chromium.org>
2015-12-16 14:57:12 -0800

    [Cast, Android, Presentation API] Fix the logic for showing controller vs chooser dialog
    Historically, logic relied on having just one route at all. Now that there might be many routes per connection,
    if there's a known route with the same source id, we show the chooser dialog, otherwise the picker dialog.
    
    BUG=570448
    TEST=Manual+e2e tests
    
    Review URL: https://codereview.chromium.org/1534553002
    
    Cr-Commit-Position: refs/heads/master@{#365654}

--- chrome/browser/media/android/router/media_router_dialog_controller_android.h
+++ chrome/browser/media/android/router/media_router_dialog_controller_android.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/media/router/media_router_dialog_controller.h"
-#include "chrome/browser/media/router/media_routes_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
 namespace media_router {
@@ -20,8 +19,7 @@ namespace media_router {
 // Android implementation of the MediaRouterDialogController.
 class MediaRouterDialogControllerAndroid
     : public content::WebContentsUserData<MediaRouterDialogControllerAndroid>,
-      public MediaRouterDialogController,
-      public MediaRoutesObserver {
+      public MediaRouterDialogController {
  public:
   ~MediaRouterDialogControllerAndroid() override;
 
@@ -59,17 +57,9 @@ class MediaRouterDialogControllerAndroid
   void CloseMediaRouterDialog() override;
   bool IsShowingMediaRouterDialog() const override;
 
-  // MediaRoutesObserver:
-  void OnRoutesUpdated(const std::vector<MediaRoute>& routes) override;
-
   void CancelPresentationRequest();
 
   base::android::ScopedJavaGlobalRef<jobject> java_dialog_controller_;
-
-  // Null if no routes or more than one route exist. If there's only one route,
-  // keeps a copy to determine if the route controller dialog needs to be shown
-  // vs. the route chooser one.
-  scoped_ptr<MediaRoute> single_existing_route_;
 
   DISALLOW_COPY_AND_ASSIGN(MediaRouterDialogControllerAndroid);
 };
