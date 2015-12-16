82f5da86ff0c77a70ffbf01b77c5f13ded4ca851
pbos <pbos@chromium.org>
2015-12-16 14:27:01 -0800

    Remove dependency on base from drfuzz_main.cc.
    Permits building drfuzz_main.cc in webrtc infrastructure.
    
    BUG=chromium:566930, webrtc:4771
    R=aizatsky@chromium.org, zhaoqin@chromium.org
    
    Review URL: https://codereview.chromium.org/1526093005
    
    Cr-Commit-Position: refs/heads/master@{#365640}

--- testing/libfuzzer/drfuzz_main.cc
+++ testing/libfuzzer/drfuzz_main.cc
@@ -2,7 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright (c) 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-#include "base/memory/scoped_ptr.h"
+
+#include <stddef.h>
 
 extern "C" int LLVMFuzzerTestOneInput(const unsigned char* data, size_t size);
 
@@ -10,7 +11,9 @@ extern "C" int LLVMFuzzerTestOneInput(const unsigned char* data, size_t size);
 int main(int argc, char **argv)
 {
   static const size_t kFuzzInputMaxSize = 1024;
-  scoped_ptr<unsigned char[]> fuzz_input(new unsigned char[kFuzzInputMaxSize]);
+  unsigned char* fuzz_input = new unsigned char[kFuzzInputMaxSize]();
   // The buffer and size arguments can be changed by Dr. Fuzz.
-  return LLVMFuzzerTestOneInput(fuzz_input.get(), kFuzzInputMaxSize);
+  int result = LLVMFuzzerTestOneInput(fuzz_input, kFuzzInputMaxSize);
+  delete[] fuzz_input;
+  return result;
 }
