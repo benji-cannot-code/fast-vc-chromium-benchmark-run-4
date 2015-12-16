6f09fb4588189702dff7f32b4f6ce50f16dd96c3
shadi <shadi@chromium.org>
2015-12-16 11:52:30 -0800

    Have MakeProfileForUISignin accessible from tests
    Signin E2E tests need the abilitiy to create profiles for signin with
    real backend servers.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1528143002
    
    Cr-Commit-Position: refs/heads/master@{#365592}

--- chrome/browser/sync/test/integration/sync_test.h
+++ chrome/browser/sync/test/integration/sync_test.h
@@ -276,6 +276,10 @@ class SyncTest : public InProcessBrowserTest {
   // before the Profile object is created.
   void SetPreexistingPreferencesFileContents(const std::string& contents);
 
+  // Helper to ProfileManager::CreateProfileAsync that creates a new profile
+  // used for UI Signin. Blocks until profile is created.
+  static Profile* MakeProfileForUISignin(base::FilePath profile_path);
+
   // GAIA account used by the test case.
   std::string username_;
 
@@ -292,10 +296,6 @@ class SyncTest : public InProcessBrowserTest {
   // Handles Profile creation for given index. Profile's path and type is
   // determined at runtime based on server type.
   void CreateProfile(int index);
-
-  // Helper to ProfileManager::CreateProfileAsync that creates a new profile
-  // used for UI Signin. Blocks until profile is created.
-  static Profile* MakeProfileForUISignin(base::FilePath profile_path);
 
   // Callback for MakeProfileForUISignin() method. It runs the quit_closure once
   // profile is created successfully.
