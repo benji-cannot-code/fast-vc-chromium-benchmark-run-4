07ab2cc75927c75c90ed33e1874017c8104622ee
honghaiz <honghaiz@google.com>
2015-12-16 15:27:17 -0800

    Make sure that peer connection states are updated properly. Update the state right before the respective javascript events are fired. This fixed the problem that when two state changes happen in quick succession, the js client may see two events both with the same last-changed state.
    BUG=371804
    
    Review URL: https://codereview.chromium.org/1523213002
    
    Cr-Commit-Position: refs/heads/master@{#365666}

--- third_party/WebKit/Source/modules/mediastream/RTCPeerConnection.h
+++ third_party/WebKit/Source/modules/mediastream/RTCPeerConnection.h
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/WebRTCPeerConnectionHandlerClient.h"
 
 namespace blink {
-
 class ExceptionState;
 class MediaStreamTrack;
 class RTCConfiguration;
@@ -158,17 +157,36 @@ public:
     DECLARE_VIRTUAL_TRACE();
 
 private:
+    typedef Function<bool()> BoolFunction;
+    class EventWrapper {
+    public:
+        EventWrapper(PassRefPtrWillBeRawPtr<Event>, PassOwnPtrWillBeRawPtr<BoolFunction>);
+        // Returns true if |m_setupFunction| returns true or it is null.
+        // |m_event| will only be fired if setup() returns true;
+        bool setup();
+
+        RefPtrWillBeMember<Event> m_event;
+
+    private:
+        PassOwnPtrWillBeRawPtr<BoolFunction> m_setupFunction;
+    };
+
     RTCPeerConnection(ExecutionContext*, RTCConfiguration*, WebMediaConstraints, ExceptionState&);
 
     static RTCConfiguration* parseConfiguration(const Dictionary&, ExceptionState&);
     static RTCOfferOptions* parseOfferOptions(const Dictionary&, ExceptionState&);
 
     void scheduleDispatchEvent(PassRefPtrWillBeRawPtr<Event>);
+    void scheduleDispatchEvent(PassRefPtrWillBeRawPtr<Event>, PassOwnPtrWillBeRawPtr<BoolFunction>);
     void dispatchScheduledEvent();
     bool hasLocalStreamWithTrackId(const String& trackId);
 
     void changeSignalingState(WebRTCPeerConnectionHandlerClient::SignalingState);
     void changeIceGatheringState(WebRTCPeerConnectionHandlerClient::ICEGatheringState);
+    // Changes the state immediately; does not fire an event.
+    // Returns true if the state was changed.
+    bool setIceConnectionState(WebRTCPeerConnectionHandlerClient::ICEConnectionState);
+    // Changes the state asynchronously and fires an event immediately after changing the state.
     void changeIceConnectionState(WebRTCPeerConnectionHandlerClient::ICEConnectionState);
 
     void closeInternal();
@@ -185,7 +203,7 @@ private:
     OwnPtr<WebRTCPeerConnectionHandler> m_peerHandler;
 
     AsyncMethodRunner<RTCPeerConnection> m_dispatchScheduledEventRunner;
-    WillBeHeapVector<RefPtrWillBeMember<Event>> m_scheduledEvents;
+    WillBeHeapVector<EventWrapper> m_scheduledEvents;
 
     bool m_stopped;
     bool m_closed;
