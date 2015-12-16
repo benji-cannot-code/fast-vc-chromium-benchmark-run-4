dae50c1e1c5ff23974eaf95478ea4aaa00f7e13f
kimwjdalsl <kimwjdalsl@gmail.com>
2015-12-15 16:11:10 -0800

    Remove ScopedVector from test_reg_util_win
    BUG=554289
    
    Review URL: https://codereview.chromium.org/1527603002
    
    Cr-Commit-Position: refs/heads/master@{#365399}

--- base/test/test_reg_util_win.h
+++ base/test/test_reg_util_win.h
@@ -7,9 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_TEST_TEST_REG_UTIL_WIN_H_
 
 // Registry utility functions used only by tests.
+#include <vector>
 
 #include "base/basictypes.h"
-#include "base/memory/scoped_vector.h"
+#include "base/memory/scoped_ptr.h"
 #include "base/strings/string16.h"
 #include "base/time/time.h"
 #include "base/win/registry.h"
@@ -64,7 +65,7 @@ class RegistryOverrideManager {
   base::string16 guid_;
 
   base::string16 test_key_root_;
-  ScopedVector<ScopedRegistryKeyOverride> overrides_;
+  std::vector<scoped_ptr<ScopedRegistryKeyOverride>> overrides_;
 
   DISALLOW_COPY_AND_ASSIGN(RegistryOverrideManager);
 };
