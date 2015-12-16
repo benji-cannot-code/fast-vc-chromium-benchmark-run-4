e276ad732e59a031ed8ca6d9a34309915b524a99
creis <creis@chromium.org>
2015-12-15 20:38:22 -0800

    Shortcut cross-process transfers to avoid relying on OpenURLFromTab.
    Some navigations transfer to a different process during the network
    stack, such as server redirects or cross-site subframes in OOPIF-enabled
    modes.  These should not depend on going through WebContentsDelegate's
    OpenURLFromTab, since many WebContentsDelegate implementations do not
    handle this case properly (e.g., panels, DevTools).
    
    This is one step toward further shortcutting, such as not using pending
    NavigationEntries or NavigationEntries at all for transfers.
    
    BUG=495161, 568357
    TEST=OOPIFs work in panels and DevTools; test landing separately.
    
    Review URL: https://codereview.chromium.org/1519943002
    
    Cr-Commit-Position: refs/heads/master@{#365443}

--- content/browser/frame_host/navigator_delegate.cc
+++ content/browser/frame_host/navigator_delegate.cc
@@ -11,6 +11,10 @@ bool NavigatorDelegate::CanOverscrollContent() const {
   return false;
 }
 
+bool NavigatorDelegate::ShouldTransferNavigation() {
+  return true;
+}
+
 bool NavigatorDelegate::ShouldPreserveAbortedURLs() {
   return false;
 }
