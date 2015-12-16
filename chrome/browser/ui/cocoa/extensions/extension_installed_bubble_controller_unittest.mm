d375793f1aa3e404306861f601d9503423ce86ec
rdevlin.cronin <rdevlin.cronin@chromium.org>
2015-12-16 14:45:20 -0800

    [Extensions UI Mac] Remove ExtensionInstalledBubbleController testing interface
    The ExtensionInstalledBubbleController testing category wasn't actually used for
    testing. Move it to be a class extension in the .mm file.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1525323002
    
    Cr-Commit-Position: refs/heads/master@{#365650}

--- chrome/browser/ui/cocoa/extensions/extension_installed_bubble_controller_unittest.mm
+++ chrome/browser/ui/cocoa/extensions/extension_installed_bubble_controller_unittest.mm
@@ -334,7 +334,7 @@ TEST_F(ExtensionInstalledBubbleControllerTest, AppTest) {
   ExtensionInstalledBubbleController* controller = CreateController();
   EXPECT_TRUE(controller);
 
-  int height = [controller calculateWindowHeight];
+  int height = NSHeight([[controller window] frame]);
 
   // Make sure there is always enough room for the icon and margin.
   int minHeight = extension_installed_bubble::kIconSize +
