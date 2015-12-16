ed8fcbf4b450b24269444a66af87a6e114100bb5
nhiroki <nhiroki@chromium.org>
2015-12-15 22:50:24 -0800

    ServiceWorker: Remove ServiceWorkerDiskCacheMigrator
    ServiceWorkerDiskCacheMigrator was introduced in M45 in order to switch the
    backend implementation of ServiceWorkerDiskCache (http://crbug.com/487482).
    This migration happens only one time on browser startup and we may expect that
    almost all clients complete it by M49.
    
    If users will not have completed the mirgation by M49,
    ServiceWorkerReadFromCacheJob fails to read cached scripts and deletes the
    registration. Then, the service worker system works again as usual.
    
    BUG=539713
    
    Review URL: https://codereview.chromium.org/1514813002
    
    Cr-Commit-Position: refs/heads/master@{#365490}

--- content/browser/service_worker/service_worker_disk_cache.cc
+++ content/browser/service_worker/service_worker_disk_cache.cc
@@ -7,21 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-scoped_ptr<ServiceWorkerDiskCache>
-ServiceWorkerDiskCache::CreateWithBlockFileBackend() {
-  return make_scoped_ptr(
-      new ServiceWorkerDiskCache(false /* use_simple_cache*/));
-}
-
-scoped_ptr<ServiceWorkerDiskCache>
-ServiceWorkerDiskCache::CreateWithSimpleBackend() {
-  return make_scoped_ptr(
-      new ServiceWorkerDiskCache(true /* use_simple_cache */));
-}
-
-ServiceWorkerDiskCache::ServiceWorkerDiskCache(bool use_simple_cache)
-    : AppCacheDiskCache(use_simple_cache) {
-}
+ServiceWorkerDiskCache::ServiceWorkerDiskCache()
+    : AppCacheDiskCache(true /* use_simple_cache */) {}
 
 ServiceWorkerResponseReader::ServiceWorkerResponseReader(
     int64 resource_id,
