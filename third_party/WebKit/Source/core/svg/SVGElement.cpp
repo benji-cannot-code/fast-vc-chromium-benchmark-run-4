22a5afb4ad514dd3a3dcd34bb45655f226c4471d
alancutter <alancutter@chromium.org>
2015-12-15 21:53:53 -0800

    Fix crash when applying CSS Animations to SVG elements with no attributes
    BUG=570161
    
    Review URL: https://codereview.chromium.org/1533433002
    
    Cr-Commit-Position: refs/heads/master@{#365481}

--- third_party/WebKit/Source/core/svg/SVGElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGElement.cpp
@@ -249,7 +249,7 @@ void SVGElement::setWebAnimationsPending()
 {
     document().accessSVGExtensions().addWebAnimationsPendingSVGElement(*this);
     ensureSVGRareData()->setWebAnimatedAttributesDirty(true);
-    elementData()->m_animatedSVGAttributesAreDirty = true;
+    ensureUniqueElementData().m_animatedSVGAttributesAreDirty = true;
 }
 
 static bool isSVGAttributeHandle(const PropertyHandle& propertyHandle)
