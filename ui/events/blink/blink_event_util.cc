44205ab93bca256f37af58b7a34eaa53019c4695
bratell <bratell@opera.com>
2015-12-16 06:22:02 -0800

    Include <algorithm> if you use functions from <algorithm>.
    I'm trying to remove #include <algorithm> from common headers because
    it takes a long time to compile, but then code that actually need
    <algorithm> need to include that header themselves.
    
    This will bring no functional change.
    
    BUG=563433
    
    Review URL: https://codereview.chromium.org/1531703002
    
    Cr-Commit-Position: refs/heads/master@{#365524}

--- ui/events/blink/blink_event_util.cc
+++ ui/events/blink/blink_event_util.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/events/blink/blink_event_util.h"
 
+#include <algorithm>
 #include <cmath>
 
 #include "base/time/time.h"
--- ui/events/blink/input_scroll_elasticity_controller.cc
+++ ui/events/blink/input_scroll_elasticity_controller.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <math.h>
 
+#include <algorithm>
+
 #include "base/bind.h"
 #include "cc/input/input_handler.h"
 #include "ui/gfx/geometry/vector2d_conversions.h"
