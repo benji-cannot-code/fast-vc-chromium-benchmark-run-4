6687414bd0bc5b0fea211e2a60d3211eb2e01863
hashimoto <hashimoto@chromium.org>
2015-12-15 21:40:39 -0800

    Add transaction response handling code to CommandStream
    TransactionDataFromDriver is responsible to free transaction data payload.
    constants.h is to provide transaction code constants.
    
    BUG=563282
    
    Review URL: https://codereview.chromium.org/1528833002
    
    Cr-Commit-Position: refs/heads/master@{#365474}

--- chromeos/binder/driver.h
+++ chromeos/binder/driver.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace binder {
 
 // Use this class to communicate with the binder driver provided by the kernel.
+// This class is stateless and it's safe to access this class from multiple
+// threads.
 class Driver {
  public:
   Driver();
