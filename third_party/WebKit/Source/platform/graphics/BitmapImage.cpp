99843aebc702a7a95fdbdfc22fea2184ce3f5044
noel <noel@chromium.org>
2015-12-15 21:38:42 -0800

    Move BitmapImage metrics into its own file
    Per the instructions on the tin, this onion has layers and so it helps
    to keep separate things separate.
    
    Also add explicit values to each enumerated value, refer to the review
    comment #18 [1] for rationale.
    
    No change in behavior, no new tests.
    
    [1] https://codereview.chromium.org/1529793002/#msg18
    
    BUG=513523
    
    Review URL: https://codereview.chromium.org/1529793002
    
    Cr-Commit-Position: refs/heads/master@{#365471}

--- third_party/WebKit/Source/platform/graphics/BitmapImage.cpp
+++ third_party/WebKit/Source/platform/graphics/BitmapImage.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/Timer.h"
 #include "platform/TraceEvent.h"
 #include "platform/geometry/FloatRect.h"
+#include "platform/graphics/BitmapImageMetrics.h"
 #include "platform/graphics/DeferredImageDecoder.h"
 #include "platform/graphics/ImageObserver.h"
 #include "platform/graphics/StaticBitmapImage.h"
@@ -328,24 +329,6 @@ size_t BitmapImage::frameCount()
     return m_frameCount;
 }
 
-enum DecodedImageType {
-    // These values are histogrammed over time; do not change their ordinal
-    // values.  When deleting an image type, replace it with a dummy value;
-    // when adding an image type, do so at the bottom (and update LastDecodedImageType
-    // so that it equals the last real image type).
-    // Also, this enum should be in sync with the 'DecodedImageType' enum in
-    // src/tools/metrics/histograms/histograms.xml
-    ImageUnknown = 0,
-    ImageJPEG,
-    ImagePNG,
-    ImageGIF,
-    ImageWebP,
-    ImageICO,
-    ImageBMP,
-
-    LastDecodedImageType = ImageBMP
-};
-
 static inline bool hasVisibleImageSize(IntSize size)
 {
     return (size.width() > 1 || size.height() > 1);
@@ -359,17 +342,8 @@ bool BitmapImage::isSizeAvailable()
     m_sizeAvailable = m_source.isSizeAvailable();
 
     if (m_sizeAvailable && hasVisibleImageSize(size())) {
-        String fileExtention = m_source.filenameExtension();
-        DecodedImageType type =
-            fileExtention == "jpg"  ? ImageJPEG :
-            fileExtention == "png"  ? ImagePNG :
-            fileExtention == "gif"  ? ImageGIF :
-            fileExtention == "webp" ? ImageWebP :
-            fileExtention == "ico"  ? ImageICO :
-            fileExtention == "bmp"  ? ImageBMP :
-            ImageUnknown;
-        Platform::current()->histogramEnumeration("Blink.DecodedImageType", type, LastDecodedImageType + 1);
-        Platform::current()->histogramEnumeration("Blink.DecodedImage.Orientation", m_source.orientationAtIndex(0).orientation(), ImageOrientationEnumEnd);
+        BitmapImageMetrics::countDecodedImageType(m_source.filenameExtension());
+        BitmapImageMetrics::countImageOrientation(m_source.orientationAtIndex(0).orientation());
     }
 
     return m_sizeAvailable;
---
+++ third_party/WebKit/Source/platform/graphics/BitmapImageMetrics.cpp
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "platform/graphics/BitmapImageMetrics.h"
+
+#include "public/platform/Platform.h"
+#include "wtf/text/WTFString.h"
+
+namespace blink {
+
+void BitmapImageMetrics::countDecodedImageType(const String& type)
+{
+    enum DecodedImageType { // Values synced with 'DecodedImageType' in src/tools/metrics/histograms/histograms.xml
+        ImageUnknown = 0,
+        ImageJPEG = 1,
+        ImagePNG = 2,
+        ImageGIF = 3,
+        ImageWebP = 4,
+        ImageICO = 5,
+        ImageBMP = 6,
+        DecodedImageTypeEnumEnd = ImageBMP + 1
+    };
+
+    DecodedImageType decodedImageType =
+        type == "jpg"  ? ImageJPEG :
+        type == "png"  ? ImagePNG  :
+        type == "gif"  ? ImageGIF  :
+        type == "webp" ? ImageWebP :
+        type == "ico"  ? ImageICO  :
+        type == "bmp"  ? ImageBMP  : DecodedImageType::ImageUnknown;
+
+    Platform::current()->histogramEnumeration("Blink.DecodedImageType", decodedImageType, DecodedImageTypeEnumEnd);
+}
+
+void BitmapImageMetrics::countImageOrientation(const ImageOrientationEnum orientation)
+{
+    Platform::current()->histogramEnumeration("Blink.DecodedImage.Orientation", orientation, ImageOrientationEnumEnd);
+}
+
+} // namespace blink
