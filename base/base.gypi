7bab4956e1d1e9848e2ed4c722230001d6297bdf
wfh <wfh@chromium.org>
2015-12-16 12:15:31 -0800

    Move handle hooks into base/win and add tests.
    Also enable handle verifier on base_unittests for both 32-bit and 64-bit.
    
    This is a prerequisite for adding more test coverage.
    
    The only intended behavior change for Chrome in this CL is that if handle hooks cannot be installed then handle verifier is disabled. A future CL intends to enable handle verifier more widely in Chrome and across tests.
    
    BUG=480639, 459544
    
    Review URL: https://codereview.chromium.org/1502523004
    
    Cr-Commit-Position: refs/heads/master@{#365601}

--- base/base.gypi
+++ base/base.gypi
@@ -148,6 +148,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'debug/alias.h',
           'debug/asan_invalid_access.cc',
           'debug/asan_invalid_access.h',
+          'debug/close_handle_hook_win.cc',
+          'debug/close_handle_hook_win.h',
           'debug/crash_logging.cc',
           'debug/crash_logging.h',
           'debug/debugger.cc',
--- base/win/scoped_handle.cc
+++ base/win/scoped_handle.cc
@@ -42,7 +42,7 @@ base::LazyInstance<NativeLock>::Leaky g_lock = LAZY_INSTANCE_INITIALIZER;
 
 bool CloseHandleWrapper(HANDLE handle) {
   if (!::CloseHandle(handle))
-    CHECK(false);
+    LOG(FATAL) << "CloseHandle failed.";
   return true;
 }
 
@@ -164,7 +164,7 @@ void ActiveVerifier::StartTracking(HANDLE handle, const void* owner,
   if (!result.second) {
     Info other = result.first->second;
     base::debug::Alias(&other);
-    CHECK(false);
+    LOG(FATAL) << "Attempt to start tracking already tracked handle.";
   }
 }
 
@@ -176,12 +176,12 @@ void ActiveVerifier::StopTracking(HANDLE handle, const void* owner,
   AutoNativeLock lock(*lock_);
   HandleMap::iterator i = map_.find(handle);
   if (i == map_.end())
-    CHECK(false);
+    LOG(FATAL) << "Attempting to close an untracked handle.";
 
   Info other = i->second;
   if (other.owner != owner) {
     base::debug::Alias(&other);
-    CHECK(false);
+    LOG(FATAL) << "Attempting to close a handle not owned by opener.";
   }
 
   map_.erase(i);
@@ -202,7 +202,7 @@ void ActiveVerifier::OnHandleBeingClosed(HANDLE handle) {
 
   Info other = i->second;
   base::debug::Alias(&other);
-  CHECK(false);
+  LOG(FATAL) << "CloseHandle called on tracked handle.";
 }
 
 }  // namespace
