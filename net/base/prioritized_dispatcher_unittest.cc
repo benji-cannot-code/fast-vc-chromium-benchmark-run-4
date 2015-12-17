a54b3980077aef0a551d1070349a1d8674e5d175
olli.raula <olli.raula@intel.com>
2015-12-17 00:55:12 -0800

    Remove ScopedVector from prioritized_dispatcher_unittest
    ScopedVector is deprecated.
    
    BUG=554289
    
    Review URL: https://codereview.chromium.org/1510513002
    
    Cr-Commit-Position: refs/heads/master@{#365788}

--- net/base/prioritized_dispatcher_unittest.cc
+++ net/base/prioritized_dispatcher_unittest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/memory/scoped_vector.h"
 #include "net/base/prioritized_dispatcher.h"
 #include "net/base/request_priority.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -140,16 +139,16 @@ class PrioritizedDispatcherTest : public testing::Test {
     dispatcher_.reset(new PrioritizedDispatcher(limits));
   }
 
-  TestJob* AddJob(char data, Priority priority) {
-    TestJob* job = new TestJob(dispatcher_.get(), data, priority, &log_);
-    jobs_.push_back(job);
+  scoped_ptr<TestJob> AddJob(char data, Priority priority) {
+    scoped_ptr<TestJob> job(
+        new TestJob(dispatcher_.get(), data, priority, &log_));
     job->Add(false);
     return job;
   }
 
-  TestJob* AddJobAtHead(char data, Priority priority) {
-    TestJob* job = new TestJob(dispatcher_.get(), data, priority, &log_);
-    jobs_.push_back(job);
+  scoped_ptr<TestJob> AddJobAtHead(char data, Priority priority) {
+    scoped_ptr<TestJob> job(
+        new TestJob(dispatcher_.get(), data, priority, &log_));
     job->Add(true);
     return job;
   }
@@ -163,7 +162,6 @@ class PrioritizedDispatcherTest : public testing::Test {
 
   std::string log_;
   scoped_ptr<PrioritizedDispatcher> dispatcher_;
-  ScopedVector<TestJob> jobs_;
 };
 
 TEST_F(PrioritizedDispatcherTest, GetLimits) {
@@ -205,10 +203,10 @@ TEST_F(PrioritizedDispatcherTest, AddAFIFO) {
   PrioritizedDispatcher::Limits limits(NUM_PRIORITIES, 1);
   Prepare(limits);
 
-  TestJob* job_a = AddJob('a', IDLE);
-  TestJob* job_b = AddJob('b', IDLE);
-  TestJob* job_c = AddJob('c', IDLE);
-  TestJob* job_d = AddJob('d', IDLE);
+  scoped_ptr<TestJob> job_a = AddJob('a', IDLE);
+  scoped_ptr<TestJob> job_b = AddJob('b', IDLE);
+  scoped_ptr<TestJob> job_c = AddJob('c', IDLE);
+  scoped_ptr<TestJob> job_d = AddJob('d', IDLE);
 
   ASSERT_TRUE(job_a->running());
   job_a->Finish();
@@ -226,11 +224,11 @@ TEST_F(PrioritizedDispatcherTest, AddPriority) {
   PrioritizedDispatcher::Limits limits(NUM_PRIORITIES, 1);
   Prepare(limits);
 
-  TestJob* job_a = AddJob('a', IDLE);
-  TestJob* job_b = AddJob('b', MEDIUM);
-  TestJob* job_c = AddJob('c', HIGHEST);
-  TestJob* job_d = AddJob('d', HIGHEST);
-  TestJob* job_e = AddJob('e', MEDIUM);
+  scoped_ptr<TestJob> job_a = AddJob('a', IDLE);
+  scoped_ptr<TestJob> job_b = AddJob('b', MEDIUM);
+  scoped_ptr<TestJob> job_c = AddJob('c', HIGHEST);
+  scoped_ptr<TestJob> job_d = AddJob('d', HIGHEST);
+  scoped_ptr<TestJob> job_e = AddJob('e', MEDIUM);
 
   ASSERT_TRUE(job_a->running());
   job_a->Finish();
@@ -250,12 +248,12 @@ TEST_F(PrioritizedDispatcherTest, AddAtHead) {
   PrioritizedDispatcher::Limits limits(NUM_PRIORITIES, 1);
   Prepare(limits);
 
-  TestJob* job_a = AddJob('a', MEDIUM);
-  TestJob* job_b = AddJobAtHead('b', MEDIUM);
-  TestJob* job_c = AddJobAtHead('c', HIGHEST);
-  TestJob* job_d = AddJobAtHead('d', HIGHEST);
-  TestJob* job_e = AddJobAtHead('e', MEDIUM);
-  TestJob* job_f = AddJob('f', MEDIUM);
+  scoped_ptr<TestJob> job_a = AddJob('a', MEDIUM);
+  scoped_ptr<TestJob> job_b = AddJobAtHead('b', MEDIUM);
+  scoped_ptr<TestJob> job_c = AddJobAtHead('c', HIGHEST);
+  scoped_ptr<TestJob> job_d = AddJobAtHead('d', HIGHEST);
+  scoped_ptr<TestJob> job_e = AddJobAtHead('e', MEDIUM);
+  scoped_ptr<TestJob> job_f = AddJob('f', MEDIUM);
 
   ASSERT_TRUE(job_a->running());
   job_a->Finish();
@@ -281,14 +279,14 @@ TEST_F(PrioritizedDispatcherTest, EnforceLimits) {
   limits.reserved_slots[LOW] = 1;
   Prepare(limits);
 
-  TestJob* job_a = AddJob('a', IDLE);    // Uses unreserved slot.
-  TestJob* job_b = AddJob('b', IDLE);    // Uses unreserved slot.
-  TestJob* job_c = AddJob('c', LOWEST);  // Must wait.
-  TestJob* job_d = AddJob('d', LOW);     // Uses reserved slot.
-  TestJob* job_e = AddJob('e', MEDIUM);  // Must wait.
-  TestJob* job_f = AddJob('f', HIGHEST); // Uses reserved slot.
-  TestJob* job_g = AddJob('g', HIGHEST); // Uses reserved slot.
-  TestJob* job_h = AddJob('h', HIGHEST); // Must wait.
+  scoped_ptr<TestJob> job_a = AddJob('a', IDLE);     // Uses unreserved slot.
+  scoped_ptr<TestJob> job_b = AddJob('b', IDLE);     // Uses unreserved slot.
+  scoped_ptr<TestJob> job_c = AddJob('c', LOWEST);   // Must wait.
+  scoped_ptr<TestJob> job_d = AddJob('d', LOW);      // Uses reserved slot.
+  scoped_ptr<TestJob> job_e = AddJob('e', MEDIUM);   // Must wait.
+  scoped_ptr<TestJob> job_f = AddJob('f', HIGHEST);  // Uses reserved slot.
+  scoped_ptr<TestJob> job_g = AddJob('g', HIGHEST);  // Uses reserved slot.
+  scoped_ptr<TestJob> job_h = AddJob('h', HIGHEST);  // Must wait.
 
   EXPECT_EQ(5u, dispatcher_->num_running_jobs());
   EXPECT_EQ(3u, dispatcher_->num_queued_jobs());
@@ -321,11 +319,11 @@ TEST_F(PrioritizedDispatcherTest, ChangePriority) {
   limits.reserved_slots[HIGHEST] = 1;
   Prepare(limits);
 
-  TestJob* job_a = AddJob('a', IDLE);
-  TestJob* job_b = AddJob('b', LOW);
-  TestJob* job_c = AddJob('c', MEDIUM);
-  TestJob* job_d = AddJob('d', MEDIUM);
-  TestJob* job_e = AddJob('e', IDLE);
+  scoped_ptr<TestJob> job_a = AddJob('a', IDLE);
+  scoped_ptr<TestJob> job_b = AddJob('b', LOW);
+  scoped_ptr<TestJob> job_c = AddJob('c', MEDIUM);
+  scoped_ptr<TestJob> job_d = AddJob('d', MEDIUM);
+  scoped_ptr<TestJob> job_e = AddJob('e', IDLE);
 
   ASSERT_FALSE(job_b->running());
   ASSERT_FALSE(job_c->running());
@@ -356,11 +354,11 @@ TEST_F(PrioritizedDispatcherTest, Cancel) {
   PrioritizedDispatcher::Limits limits(NUM_PRIORITIES, 1);
   Prepare(limits);
 
-  TestJob* job_a = AddJob('a', IDLE);
-  TestJob* job_b = AddJob('b', IDLE);
-  TestJob* job_c = AddJob('c', IDLE);
-  TestJob* job_d = AddJob('d', IDLE);
-  TestJob* job_e = AddJob('e', IDLE);
+  scoped_ptr<TestJob> job_a = AddJob('a', IDLE);
+  scoped_ptr<TestJob> job_b = AddJob('b', IDLE);
+  scoped_ptr<TestJob> job_c = AddJob('c', IDLE);
+  scoped_ptr<TestJob> job_d = AddJob('d', IDLE);
+  scoped_ptr<TestJob> job_e = AddJob('e', IDLE);
 
   ASSERT_FALSE(job_b->running());
   ASSERT_FALSE(job_d->running());
@@ -381,14 +379,14 @@ TEST_F(PrioritizedDispatcherTest, Evict) {
   PrioritizedDispatcher::Limits limits(NUM_PRIORITIES, 1);
   Prepare(limits);
 
-  TestJob* job_a = AddJob('a', IDLE);
-  TestJob* job_b = AddJob('b', LOW);
-  TestJob* job_c = AddJob('c', HIGHEST);
-  TestJob* job_d = AddJob('d', LOW);
-  TestJob* job_e = AddJob('e', HIGHEST);
+  scoped_ptr<TestJob> job_a = AddJob('a', IDLE);
+  scoped_ptr<TestJob> job_b = AddJob('b', LOW);
+  scoped_ptr<TestJob> job_c = AddJob('c', HIGHEST);
+  scoped_ptr<TestJob> job_d = AddJob('d', LOW);
+  scoped_ptr<TestJob> job_e = AddJob('e', HIGHEST);
 
-  EXPECT_EQ(job_b, dispatcher_->EvictOldestLowest());
-  EXPECT_EQ(job_d, dispatcher_->EvictOldestLowest());
+  EXPECT_EQ(job_b.get(), dispatcher_->EvictOldestLowest());
+  EXPECT_EQ(job_d.get(), dispatcher_->EvictOldestLowest());
 
   ASSERT_TRUE(job_a->running());
   job_a->Finish();
@@ -411,9 +409,9 @@ TEST_F(PrioritizedDispatcherTest, AddWhileZeroLimits) {
   Prepare(limits);
 
   dispatcher_->SetLimitsToZero();
-  TestJob* job_a = AddJob('a', LOW);
-  TestJob* job_b = AddJob('b', MEDIUM);
-  TestJob* job_c = AddJobAtHead('c', MEDIUM);
+  scoped_ptr<TestJob> job_a = AddJob('a', LOW);
+  scoped_ptr<TestJob> job_b = AddJob('b', MEDIUM);
+  scoped_ptr<TestJob> job_c = AddJobAtHead('c', MEDIUM);
 
   EXPECT_EQ(0u, dispatcher_->num_running_jobs());
   EXPECT_EQ(3u, dispatcher_->num_queued_jobs());
@@ -438,11 +436,11 @@ TEST_F(PrioritizedDispatcherTest, ReduceLimitsWhileJobQueued) {
   PrioritizedDispatcher::Limits initial_limits(NUM_PRIORITIES, 2);
   Prepare(initial_limits);
 
-  TestJob* job_a = AddJob('a', MEDIUM);
-  TestJob* job_b = AddJob('b', MEDIUM);
-  TestJob* job_c = AddJob('c', MEDIUM);
-  TestJob* job_d = AddJob('d', MEDIUM);
-  TestJob* job_e = AddJob('e', MEDIUM);
+  scoped_ptr<TestJob> job_a = AddJob('a', MEDIUM);
+  scoped_ptr<TestJob> job_b = AddJob('b', MEDIUM);
+  scoped_ptr<TestJob> job_c = AddJob('c', MEDIUM);
+  scoped_ptr<TestJob> job_d = AddJob('d', MEDIUM);
+  scoped_ptr<TestJob> job_e = AddJob('e', MEDIUM);
 
   EXPECT_EQ(2u, dispatcher_->num_running_jobs());
   EXPECT_EQ(3u, dispatcher_->num_queued_jobs());
@@ -482,9 +480,9 @@ TEST_F(PrioritizedDispatcherTest, ZeroLimitsThenCancel) {
   PrioritizedDispatcher::Limits limits(NUM_PRIORITIES, 1);
   Prepare(limits);
 
-  TestJob* job_a = AddJob('a', IDLE);
-  TestJob* job_b = AddJob('b', IDLE);
-  TestJob* job_c = AddJob('c', IDLE);
+  scoped_ptr<TestJob> job_a = AddJob('a', IDLE);
+  scoped_ptr<TestJob> job_b = AddJob('b', IDLE);
+  scoped_ptr<TestJob> job_c = AddJob('c', IDLE);
   dispatcher_->SetLimitsToZero();
 
   ASSERT_TRUE(job_a->running());
@@ -512,8 +510,8 @@ TEST_F(PrioritizedDispatcherTest, ZeroLimitsThenIncreasePriority) {
   limits.reserved_slots[HIGHEST] = 1;
   Prepare(limits);
 
-  TestJob* job_a = AddJob('a', IDLE);
-  TestJob* job_b = AddJob('b', IDLE);
+  scoped_ptr<TestJob> job_a = AddJob('a', IDLE);
+  scoped_ptr<TestJob> job_b = AddJob('b', IDLE);
   EXPECT_TRUE(job_a->running());
   EXPECT_FALSE(job_b->running());
   dispatcher_->SetLimitsToZero();
@@ -538,7 +536,7 @@ TEST_F(PrioritizedDispatcherTest, CancelMissing) {
   PrioritizedDispatcher::Limits limits(NUM_PRIORITIES, 1);
   Prepare(limits);
   AddJob('a', IDLE);
-  TestJob* job_b = AddJob('b', IDLE);
+  scoped_ptr<TestJob> job_b = AddJob('b', IDLE);
   PrioritizedDispatcher::Handle handle = job_b->handle();
   ASSERT_FALSE(handle.is_null());
   dispatcher_->Cancel(handle);
