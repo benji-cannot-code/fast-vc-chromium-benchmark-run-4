b212c5b05786b82c5df82fbadb92d9b8a10f4589
ianwen <ianwen@chromium.org>
2015-12-16 21:25:22 -0800

    Fix a snackbar crash about leaking window
    Sometimes the DownloadService will call showSnackbars() even when the activity is not visible. To track the activity's visibility, SnackbarManager now needs to know some activity's lifecycle signals. After this CL, all activities managing snackbars should call onStart and onStop of the SnackbarManager.
    
    BUG=553569
    
    Review URL: https://codereview.chromium.org/1518373002
    
    Cr-Commit-Position: refs/heads/master@{#365751}

--- chrome/android/java/src/org/chromium/chrome/browser/enhancedbookmarks/EnhancedBookmarkActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/enhancedbookmarks/EnhancedBookmarkActivity.java
@@ -52,9 +52,15 @@ public class EnhancedBookmarkActivity extends EnhancedBookmarkActivityBase imple
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        mSnackbarManager.onStart();
+    }
+
+    @Override
     protected void onStop() {
         super.onStop();
-        mSnackbarManager.dismissAllSnackbars(false);
+        mSnackbarManager.onStop();
     }
 
     @Override
