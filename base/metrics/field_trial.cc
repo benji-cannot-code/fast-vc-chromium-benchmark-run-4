7e261c49a80969b12da64d97b1929548c3dce2cc
davidben <davidben@chromium.org>
2015-12-16 19:49:35 -0800

    Remove unused #include.
    base/sha1.h isn't being used by base/metrics/field_trial.cc anymore.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1532643004
    
    Cr-Commit-Position: refs/heads/master@{#365724}

--- base/metrics/field_trial.cc
+++ base/metrics/field_trial.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/build_time.h"
 #include "base/logging.h"
 #include "base/rand_util.h"
-#include "base/sha1.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
