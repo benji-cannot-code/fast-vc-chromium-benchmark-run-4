bcc2226edecd5780fed1fd44fb63ca2731524715
philipj <philipj@opera.com>
2015-12-17 15:07:30 -0800

    Simplify Node.prototype.baseURI to match the DOM spec
    https://dom.spec.whatwg.org/#dom-node-baseuri
    
    This simplification was made possible by the removal of xml:base in spec
    and implementation:
    https://www.w3.org/Bugs/Public/show_bug.cgi?id=20976
    https://code.google.com/p/chromium/issues/detail?id=341854
    
    BUG=570679
    
    Review URL: https://codereview.chromium.org/1529363004
    
    Cr-Commit-Position: refs/heads/master@{#365934}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -1128,11 +1128,6 @@ void Document::setXMLStandalone(bool standalone, ExceptionState& exceptionState)
     m_xmlStandalone = standalone ? Standalone : NotStandalone;
 }
 
-KURL Document::baseURI() const
-{
-    return m_baseURL;
-}
-
 void Document::setContent(const String& content)
 {
     open();
--- third_party/WebKit/Source/core/dom/Node.cpp
+++ third_party/WebKit/Source/core/dom/Node.cpp
@@ -1062,9 +1062,9 @@ Document* Node::ownerDocument() const
     return doc == this ? nullptr : doc;
 }
 
-KURL Node::baseURI() const
+const KURL& Node::baseURI() const
 {
-    return parentNode() ? parentNode()->baseURI() : KURL();
+    return document().baseURL();
 }
 
 bool Node::isEqualNode(Node* other) const
