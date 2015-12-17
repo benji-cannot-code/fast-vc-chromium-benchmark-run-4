51602d41537e1f02c1b5dd0b6daf76fad3a47c1b
landell <landell@opera.com>
2015-12-17 09:00:25 -0800

    Include missing errno.h
    BUG=
    
    Review URL: https://codereview.chromium.org/1532833002
    
    Cr-Commit-Position: refs/heads/master@{#365839}

--- net/dns/dns_util.cc
+++ net/dns/dns_util.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/dns/dns_util.h"
 
+#include <errno.h>
+
 #include <cstring>
 
 #include "build/build_config.h"
