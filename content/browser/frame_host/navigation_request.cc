0932e30a84d6f5ba7093a39fa66e4f8aec9ddad7
clamy <clamy@chromium.org>
2015-12-17 08:06:23 -0800

    Revert "Merge DidRedirectNavigation and WillRedirectNavigation in NavigationHandle"
    This reverts commit 314c15b4182d6c1c12c02dc2b2a2adc66dd4ed1e.
    This CL is suspected to cause crashes on Android, reverting to see if it is the
    cause.
    
    BUG=570200
    TBR=csharrison, nasko
    
    Review URL: https://codereview.chromium.org/1538553002
    
    Cr-Commit-Position: refs/heads/master@{#365832}

--- content/browser/frame_host/navigation_request.cc
+++ content/browser/frame_host/navigation_request.cc
@@ -317,6 +317,7 @@ void NavigationRequest::OnRedirectChecksComplete(
   }
 
   loader_->FollowRedirect();
+  navigation_handle_->DidRedirectNavigation(common_params_.url);
 }
 
 void NavigationRequest::InitializeServiceWorkerHandleIfNeeded() {
