c48c355628a4a6fa1ff981f64a940793b5bb9f27
paul.sapunaru <paul.sapunaru@intel.com>
2015-12-17 01:30:03 -0800

    Use const reference instead of value in WebSocketDeflateStreamTest.
    Small cleanup which replaces the const value with const reference return value
    of the callback() functions in ReadFramesStub and WriteFramesStub classes.
    
    BUG=568959
    TEST=net_unittests
    
    Review URL: https://codereview.chromium.org/1531653003
    
    Cr-Commit-Position: refs/heads/master@{#365789}

--- net/websockets/websocket_deflate_stream_test.cc
+++ net/websockets/websocket_deflate_stream_test.cc
@@ -316,7 +316,7 @@ class ReadFramesStub {
   }
 
   int result() const { return result_; }
-  const CompletionCallback callback() const { return callback_; }
+  const CompletionCallback& callback() const { return callback_; }
   std::vector<scoped_ptr<WebSocketFrame>>* frames_passed() {
     return frames_passed_;
   }
@@ -348,7 +348,7 @@ class WriteFramesStub {
   }
 
   int result() const { return result_; }
-  const CompletionCallback callback() const { return callback_; }
+  const CompletionCallback& callback() const { return callback_; }
   std::vector<scoped_ptr<WebSocketFrame>>* frames() { return &frames_; }
 
  private:
