16ea406dd96b00862fad1515ced292b264fe6f34
martina.kollarova <martina.kollarova@intel.com>
2015-12-17 01:41:10 -0800

    Replace .Pass() with std::move in gpu/ dir
    BUG=557422
    
    Review URL: https://codereview.chromium.org/1528983003
    
    Cr-Commit-Position: refs/heads/master@{#365790}

--- gpu/command_buffer/common/buffer.cc
+++ gpu/command_buffer/common/buffer.cc
@@ -14,7 +14,7 @@ namespace gpu {
 SharedMemoryBufferBacking::SharedMemoryBufferBacking(
     scoped_ptr<base::SharedMemory> shared_memory,
     size_t size)
-    : shared_memory_(shared_memory.Pass()), size_(size) {}
+    : shared_memory_(std::move(shared_memory)), size_(size) {}
 
 SharedMemoryBufferBacking::~SharedMemoryBufferBacking() {}
 
@@ -25,7 +25,7 @@ void* SharedMemoryBufferBacking::GetMemory() const {
 size_t SharedMemoryBufferBacking::GetSize() const { return size_; }
 
 Buffer::Buffer(scoped_ptr<BufferBacking> backing)
-    : backing_(backing.Pass()),
+    : backing_(std::move(backing)),
       memory_(backing_->GetMemory()),
       size_(backing_->GetSize()) {
   DCHECK(memory_) << "The memory must be mapped to create a Buffer";
