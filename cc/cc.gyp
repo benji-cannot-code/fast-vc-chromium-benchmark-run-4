2fd4b7dc8bd74297d9af7763ee11ae09d70f96df
yutak <yutak@chromium.org>
2015-12-16 21:12:45 -0800

    Revert of TaskGraphRunner Group support (patchset #12 id:360001 of https://codereview.chromium.org/1489233003/ )
    Reason for revert:
    Broke compile on Win x64 GN
    
    https://build.chromium.org/p/chromium.win/builders/Win%20x64%20GN/builds/9223
    
    Original issue's description:
    > TaskGraphRunner Group support
    >
    > Adds a group to TaskGraphRunner, this can be used by an
    > implementation along with priority in order to decide how to execute
    > tasks.
    >
    > This is a prerequisite patch for thread affinity changes which are
    > coming in a follow-up.
    >
    > This patch causes a moderate (~15%) regression in cc_perftests for TaskGraphRunner.
    >
    > The main regressions I'm seeing are:
    >
    > In ScheduleTasks, we're seeing an additional 3% time spent in operator[] for the two new maps vs ToT.
    >
    > We're also seeing an additional 5.5% of time spent in test code which builds the task graph.
    >
    > Other regressions are less clear, but probably have to do with the additional cost of ScheduleTasks in the function body itself (inlined looping).
    >
    > BUG=
    > CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    >
    > Committed: https://crrev.com/05ca4bc9df381e6345f9cf7ee29cdfa075be30c4
    > Cr-Commit-Position: refs/heads/master@{#365140}
    >
    > Committed: https://crrev.com/3b4338fc32cf20703baa8066bdad7ce07c5ae696
    > Cr-Commit-Position: refs/heads/master@{#365646}
    
    TBR=haraken@chromium.org,oilpan-reviews@chromium.org,reveman@chromium.org,sievers@chromium.org,ericrk@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review URL: https://codereview.chromium.org/1538433002
    
    Cr-Commit-Position: refs/heads/master@{#365745}

--- cc/cc.gyp
+++ cc/cc.gyp
@@ -436,7 +436,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'raster/single_thread_task_graph_runner.h',
         'raster/synchronous_task_graph_runner.cc',
         'raster/synchronous_task_graph_runner.h',
-        'raster/task_category.h',
         'raster/task_graph_runner.cc',
         'raster/task_graph_runner.h',
         'raster/task_graph_work_queue.cc',
--- content/renderer/raster_worker_pool.cc
+++ content/renderer/raster_worker_pool.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/renderer/raster_worker_pool.h"
 
-#include <utility>
-
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/trace_event/trace_event.h"
@@ -50,8 +48,7 @@ class RasterWorkerPool::RasterWorkerPoolSequencedTaskRunner
       if (!graph_.nodes.empty())
         dependencies = 1;
 
-      cc::TaskGraph::Node node(graph_task.get(), 0u /* category */,
-                               0u /* priority */, dependencies);
+      cc::TaskGraph::Node node(graph_task.get(), 0, dependencies);
       if (dependencies) {
         graph_.edges.push_back(
             cc::TaskGraph::Edge(graph_.nodes.back().task, node.task));
@@ -152,9 +149,7 @@ bool RasterWorkerPool::PostDelayedTask(
   tasks_.push_back(make_scoped_refptr(new ClosureTask(task)));
   graph_.Reset();
   for (const auto& graph_task : tasks_)
-    graph_.nodes.push_back(
-        cc::TaskGraph::Node(graph_task.get(), 0u /* category */,
-                            0u /* priority */, 0u /* dependencies */));
+    graph_.nodes.push_back(cc::TaskGraph::Node(graph_task.get(), 0, 0));
 
   ScheduleTasksWithLockAcquired(namespace_token_, &graph_);
   completed_tasks_.clear();
@@ -170,7 +165,7 @@ void RasterWorkerPool::Run() {
   base::AutoLock lock(lock_);
 
   while (true) {
-    if (!RunTaskWithLockAcquired()) {
+    if (!work_queue_.HasReadyToRunTasks()) {
       // Exit when shutdown is set and no more tasks are pending.
       if (shutdown_)
         break;
@@ -179,6 +174,8 @@ void RasterWorkerPool::Run() {
       has_ready_to_run_tasks_cv_.Wait();
       continue;
     }
+
+    RunTaskWithLockAcquired();
   }
 }
 
@@ -262,28 +259,12 @@ void RasterWorkerPool::CollectCompletedTasksWithLockAcquired(
   work_queue_.CollectCompletedTasks(token, completed_tasks);
 }
 
-bool RasterWorkerPool::RunTaskWithLockAcquired() {
+void RasterWorkerPool::RunTaskWithLockAcquired() {
   TRACE_EVENT0("toplevel", "TaskGraphRunner::RunTask");
 
   lock_.AssertAcquired();
 
-  // Find the first category with any tasks to run. This task graph runner
-  // treats categories as an additional priority.
-  // TODO(ericrk): Add more category/thread logic.
-  const auto& ready_to_run_namespaces = work_queue_.ready_to_run_namespaces();
-  auto found = std::find_if(
-      ready_to_run_namespaces.cbegin(), ready_to_run_namespaces.cend(),
-      [](const std::pair<uint16_t,
-                         cc::TaskGraphWorkQueue::TaskNamespace::Vector>& pair) {
-        return !pair.second.empty();
-      });
-
-  if (found == ready_to_run_namespaces.cend()) {
-    return false;
-  }
-
-  const uint16_t category = found->first;
-  auto prioritized_task = work_queue_.GetNextTaskToRun(category);
+  auto prioritized_task = work_queue_.GetNextTaskToRun();
   cc::Task* task = prioritized_task.task;
 
   // There may be more work available, so wake up another worker thread.
@@ -308,8 +289,6 @@ bool RasterWorkerPool::RunTaskWithLockAcquired() {
   if (work_queue_.HasFinishedRunningTasksInNamespace(
           prioritized_task.task_namespace))
     has_namespaces_with_finished_running_tasks_cv_.Broadcast();
-
-  return true;
 }
 
 RasterWorkerPool::ClosureTask::ClosureTask(const base::Closure& closure)
