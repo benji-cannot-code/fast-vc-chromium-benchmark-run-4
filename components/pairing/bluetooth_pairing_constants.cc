9f16196a0275ff589780ea454418cea620827705
xdai <xdai@chromium.org>
2015-12-16 19:09:52 -0800

    Introduce new strings for the bootstrapping project. The old shark/remora pairing strings are still kept.
    BUG=537354
    
    Review URL: https://codereview.chromium.org/1468313008
    
    Cr-Commit-Position: refs/heads/master@{#365693}

--- components/pairing/bluetooth_pairing_constants.cc
+++ components/pairing/bluetooth_pairing_constants.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace pairing_chromeos {
 
 const char* kPairingServiceUUID = "f035ff16-c025-468e-bb16-967704d141ea";
-const char* kPairingServiceName = "ChromeboxPairing";
-const char* kDeviceNamePrefix = "Chromebox_";
+const char* kPairingServiceName = "ChromeOSDevicePairing";
+const char* kDeviceNamePrefix = "Device_";
 const char* kErrorInvalidProtocol = "Invalid protocol";
 const char* kErrorEnrollmentFailed = "Enrollment failed";
 const int kPairingAPIVersion = 1;
