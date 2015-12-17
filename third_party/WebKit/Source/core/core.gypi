738ce717ba103a31b8fa65d2df025d8a4bad1d60
esprehn <esprehn@chromium.org>
2015-12-16 20:07:07 -0800

    Merge StyleResourceLoader into ElementStyleResources.
    StyleResourceLoader doesn't really have any data members, it's just a
    container of static methods and a document pointer, it then uses the
    ElementStyleResources object for all it's behavior. Instead we should
    merge it into the ElementStyleResources, since it only needs to live
    that long anyway, and leverage having the data in one place. This makes
    the code more clear.
    
    This patch has no behavior changes, it just merges the two classes
    together, and uses the internal m_document pointer instead of passing
    it at the call sites.
    
    Review URL: https://codereview.chromium.org/1531093002
    
    Cr-Commit-Position: refs/heads/master@{#365743}

--- third_party/WebKit/Source/core/core.gypi
+++ third_party/WebKit/Source/core/core.gypi
@@ -1327,8 +1327,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'css/resolver/StyleResolverState.h',
             'css/resolver/StyleResolverStats.cpp',
             'css/resolver/StyleResolverStats.h',
-            'css/resolver/StyleResourceLoader.cpp',
-            'css/resolver/StyleResourceLoader.h',
             'css/resolver/TransformBuilder.cpp',
             'css/resolver/TransformBuilder.h',
             'css/resolver/ViewportStyleResolver.cpp',
--- third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
@@ -147,7 +147,6 @@ StyleResolver::StyleResolver(Document& document)
     , m_viewportStyleResolver(ViewportStyleResolver::create(&document))
     , m_needCollectFeatures(false)
     , m_printMediaType(false)
-    , m_styleResourceLoader(&document)
     , m_styleSharingDepth(0)
     , m_accessCount(0)
 {
@@ -555,7 +554,7 @@ void StyleResolver::adjustComputedStyle(StyleResolverState& state, Element* elem
 // Start loading resources referenced by this style.
 void StyleResolver::loadPendingResources(StyleResolverState& state)
 {
-    m_styleResourceLoader.loadPendingResources(state.style(), state.elementStyleResources());
+    state.elementStyleResources().loadPendingResources(state.style());
     document().styleEngine().fontSelector()->fontLoader()->loadPendingFonts();
 }
 
@@ -1559,7 +1558,6 @@ DEFINE_TRACE(StyleResolver)
     visitor->trace(m_uncommonAttributeRuleSet);
     visitor->trace(m_watchedSelectorsRules);
     visitor->trace(m_treeBoundaryCrossingScopes);
-    visitor->trace(m_styleResourceLoader);
     visitor->trace(m_styleSharingLists);
     visitor->trace(m_pendingStyleSheets);
     visitor->trace(m_document);
