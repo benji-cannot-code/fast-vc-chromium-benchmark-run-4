300eb32345ef47024325b5b506bf867a76cd1800
adrian.belgun <adrian.belgun@intel.com>
2015-12-18 01:49:39 -0800

    Add bindings for GL_[ARB/EXT]_shader_image_load_store
    This is needed to analyze the performance of CMAA implementation in Chromium, compared to the possible Mesa version.
    
    BUG=535198
    
    Review URL: https://codereview.chromium.org/1523233003
    
    Cr-Commit-Position: refs/heads/master@{#366068}

--- ui/gl/gl_bindings_autogen_gl.h
+++ ui/gl/gl_bindings_autogen_gl.h
@@ -45,6 +45,13 @@ typedef void(GL_BINDING_CALL* glBindFragDataLocationIndexedProc)(
     const char* name);
 typedef void(GL_BINDING_CALL* glBindFramebufferEXTProc)(GLenum target,
                                                         GLuint framebuffer);
+typedef void(GL_BINDING_CALL* glBindImageTextureEXTProc)(GLuint index,
+                                                         GLuint texture,
+                                                         GLint level,
+                                                         GLboolean layered,
+                                                         GLint layer,
+                                                         GLenum access,
+                                                         GLint format);
 typedef void(GL_BINDING_CALL* glBindRenderbufferEXTProc)(GLenum target,
                                                          GLuint renderbuffer);
 typedef void(GL_BINDING_CALL* glBindSamplerProc)(GLuint unit, GLuint sampler);
@@ -608,6 +615,7 @@ typedef void*(GL_BINDING_CALL* glMapBufferRangeProc)(GLenum target,
 typedef void(GL_BINDING_CALL* glMatrixLoadfEXTProc)(GLenum matrixMode,
                                                     const GLfloat* m);
 typedef void(GL_BINDING_CALL* glMatrixLoadIdentityEXTProc)(GLenum matrixMode);
+typedef void(GL_BINDING_CALL* glMemoryBarrierEXTProc)(GLbitfield barriers);
 typedef void(GL_BINDING_CALL* glPathCommandsNVProc)(GLuint path,
                                                     GLsizei numCommands,
                                                     const GLubyte* commands,
@@ -1043,6 +1051,7 @@ struct ExtensionsGL {
   bool b_GL_ARB_occlusion_query;
   bool b_GL_ARB_program_interface_query;
   bool b_GL_ARB_robustness;
+  bool b_GL_ARB_shader_image_load_store;
   bool b_GL_ARB_sync;
   bool b_GL_ARB_texture_storage;
   bool b_GL_ARB_timer_query;
@@ -1063,6 +1072,7 @@ struct ExtensionsGL {
   bool b_GL_EXT_multisampled_render_to_texture;
   bool b_GL_EXT_occlusion_query_boolean;
   bool b_GL_EXT_robustness;
+  bool b_GL_EXT_shader_image_load_store;
   bool b_GL_EXT_texture_storage;
   bool b_GL_EXT_timer_query;
   bool b_GL_IMG_multisampled_render_to_texture;
@@ -1092,6 +1102,7 @@ struct ProcsGL {
   glBindFragDataLocationProc glBindFragDataLocationFn;
   glBindFragDataLocationIndexedProc glBindFragDataLocationIndexedFn;
   glBindFramebufferEXTProc glBindFramebufferEXTFn;
+  glBindImageTextureEXTProc glBindImageTextureEXTFn;
   glBindRenderbufferEXTProc glBindRenderbufferEXTFn;
   glBindSamplerProc glBindSamplerFn;
   glBindTextureProc glBindTextureFn;
@@ -1282,6 +1293,7 @@ struct ProcsGL {
   glMapBufferRangeProc glMapBufferRangeFn;
   glMatrixLoadfEXTProc glMatrixLoadfEXTFn;
   glMatrixLoadIdentityEXTProc glMatrixLoadIdentityEXTFn;
+  glMemoryBarrierEXTProc glMemoryBarrierEXTFn;
   glPathCommandsNVProc glPathCommandsNVFn;
   glPathParameterfNVProc glPathParameterfNVFn;
   glPathParameteriNVProc glPathParameteriNVFn;
@@ -1431,6 +1443,13 @@ class GL_EXPORT GLApi {
                                                GLuint index,
                                                const char* name) = 0;
   virtual void glBindFramebufferEXTFn(GLenum target, GLuint framebuffer) = 0;
+  virtual void glBindImageTextureEXTFn(GLuint index,
+                                       GLuint texture,
+                                       GLint level,
+                                       GLboolean layered,
+                                       GLint layer,
+                                       GLenum access,
+                                       GLint format) = 0;
   virtual void glBindRenderbufferEXTFn(GLenum target, GLuint renderbuffer) = 0;
   virtual void glBindSamplerFn(GLuint unit, GLuint sampler) = 0;
   virtual void glBindTextureFn(GLenum target, GLuint texture) = 0;
@@ -1924,6 +1943,7 @@ class GL_EXPORT GLApi {
                                    GLbitfield access) = 0;
   virtual void glMatrixLoadfEXTFn(GLenum matrixMode, const GLfloat* m) = 0;
   virtual void glMatrixLoadIdentityEXTFn(GLenum matrixMode) = 0;
+  virtual void glMemoryBarrierEXTFn(GLbitfield barriers) = 0;
   virtual void glPathCommandsNVFn(GLuint path,
                                   GLsizei numCommands,
                                   const GLubyte* commands,
@@ -2317,6 +2337,8 @@ class GL_EXPORT GLApi {
 #define glBindFragDataLocationIndexed \
   ::gfx::g_current_gl_context->glBindFragDataLocationIndexedFn
 #define glBindFramebufferEXT ::gfx::g_current_gl_context->glBindFramebufferEXTFn
+#define glBindImageTextureEXT \
+  ::gfx::g_current_gl_context->glBindImageTextureEXTFn
 #define glBindRenderbufferEXT \
   ::gfx::g_current_gl_context->glBindRenderbufferEXTFn
 #define glBindSampler ::gfx::g_current_gl_context->glBindSamplerFn
@@ -2562,6 +2584,7 @@ class GL_EXPORT GLApi {
 #define glMatrixLoadfEXT ::gfx::g_current_gl_context->glMatrixLoadfEXTFn
 #define glMatrixLoadIdentityEXT \
   ::gfx::g_current_gl_context->glMatrixLoadIdentityEXTFn
+#define glMemoryBarrierEXT ::gfx::g_current_gl_context->glMemoryBarrierEXTFn
 #define glPathCommandsNV ::gfx::g_current_gl_context->glPathCommandsNVFn
 #define glPathParameterfNV ::gfx::g_current_gl_context->glPathParameterfNVFn
 #define glPathParameteriNV ::gfx::g_current_gl_context->glPathParameteriNVFn
