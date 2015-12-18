bf623929da6b6d364df450a7547933caa0925620
gchatz <gchatz@chromium.org>
2015-12-18 11:11:02 -0800

    Add Switches for Tab Eviction.
    Adds switches and command line flags for Tab Eviction.
    
    BUG=569266
    
    Review URL: https://codereview.chromium.org/1531333002
    
    Cr-Commit-Position: refs/heads/master@{#366135}

--- ios/chrome/browser/chrome_switches.h
+++ ios/chrome/browser/chrome_switches.h
@@ -22,6 +22,7 @@ extern const char kDisableNTPFavicons[];
 extern const char kDisableOfflineAutoReload[];
 extern const char kDisableTabSwitcher[];
 extern const char kDisableLRUSnapshotCache[];
+extern const char kDisableTabEviction[];
 
 extern const char kEnableContextualSearch[];
 extern const char kEnableCredentialManagerAPI[];
@@ -35,6 +36,7 @@ extern const char kEnableOfflineAutoReload[];
 extern const char kEnableReaderModeToolbarIcon[];
 extern const char kEnableTabSwitcher[];
 extern const char kEnableLRUSnapshotCache[];
+extern const char kEnableTabEviction[];
 
 extern const char kIOSForceVariationIds[];
 extern const char kIOSMetricsRecordingOnly[];
