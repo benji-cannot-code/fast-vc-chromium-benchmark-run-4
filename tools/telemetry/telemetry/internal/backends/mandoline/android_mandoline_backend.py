1e6675d349814eaf09312720b76546af0862f398
jbudorick <jbudorick@chromium.org>
2015-12-17 17:20:11 -0800

    [Android] Remove all references to pylib from telemetry.
    BUG=476719
    
    Review URL: https://codereview.chromium.org/1526743004
    
    Cr-Commit-Position: refs/heads/master@{#365960}

--- tools/telemetry/telemetry/internal/backends/mandoline/android_mandoline_backend.py
+++ tools/telemetry/telemetry/internal/backends/mandoline/android_mandoline_backend.py
@@ -18,7 +18,7 @@ from telemetry.internal.platform import android_platform_backend as \
   android_platform_backend_module
 
 try:
-  from pylib import constants
+  from devil.android.sdk import keyevent
 except ImportError:
   pass
 
@@ -92,7 +92,7 @@ class AndroidMandolineBackend(
     logging_process = shell.ShowLogs(output)
 
     # Unlock device screen.
-    self.device.SendKeyEvent(constants.keyevent.KEYCODE_MENU)
+    self.device.SendKeyEvent(keyevent.KEYCODE_MENU)
     shell.StartActivity(self.activity, args, output, logging_process.terminate)
 
     try:
