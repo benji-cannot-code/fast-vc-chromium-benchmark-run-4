86b685275de9794841f9a38e1f228bf70ae258e1
oshima <oshima@chromium.org>
2015-12-18 00:14:29 -0800

    Compute the popup location/size correctly when use-zoom-for-dsf is enabled.
    * Add WebWidgetClient::convertViewportToWindow and updated Popup code to use them when computing the anchor rect.
    * Apply scale parameter to zoomFactor used to scale the popup in js.
    * Changed picker JS code to use screen coordinates always rather than mix of viewport and screen.
    * Refactored RenderViewImpl/RenderWidget so that the device scale factor gets set in content rather than from Blink.
    
    BUG=485650
    TEST=-dsf2.html, -dsf2-with-zoom.html added for popup-menu, calendar-picker and suggestion picker
    TBR=rjkroege@chromium.org
    
    Review URL: https://codereview.chromium.org/1456753002
    
    Cr-Commit-Position: refs/heads/master@{#366048}

--- components/html_viewer/web_test_delegate_impl.cc
+++ components/html_viewer/web_test_delegate_impl.cc
@@ -198,6 +198,10 @@ void WebTestDelegateImpl::SetDeviceScaleFactor(float factor) {
   NOTIMPLEMENTED();
 }
 
+void WebTestDelegateImpl::EnableUseZoomForDSF() {
+  NOTIMPLEMENTED();
+}
+
 void WebTestDelegateImpl::SetDeviceColorProfile(const std::string& name) {
   NOTIMPLEMENTED();
 }
