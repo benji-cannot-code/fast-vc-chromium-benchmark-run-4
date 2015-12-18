dc2c8c6cd99dba51f52863e1f8e755a1748a83b5
chasej <chasej@chromium.org>
2015-12-18 00:35:07 -0800

    Add chrome flag for Experimental Framework
    This CL adds a chrome flag to turn on the Experimental
    Framework. The flag will also turn on the existing
    RuntimeEnabled feature on the Blink side. This allows
    the framework to be enabled more easily on Android.
    
    BUG=543141
    
    Review URL: https://codereview.chromium.org/1416043003
    
    Cr-Commit-Position: refs/heads/master@{#366053}

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -5,12 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/common/content_features.h"
 
-namespace content {
+namespace features {
+
+// All features in alphabetical order.
+
+// The Experimental Framework for controlling access to API experiments.
+const base::Feature kExperimentalFramework{"ExperimentalFramework",
+                                           base::FEATURE_DISABLED_BY_DEFAULT};
 
 #if defined(OS_ANDROID)
-const base::Feature kSeccompSandboxAndroidFeature = {
-  "SeccompSandboxAndroid", base::FEATURE_DISABLED_BY_DEFAULT
-};
+// FeatureList definition for the Seccomp field trial.
+const base::Feature kSeccompSandboxAndroid{"SeccompSandboxAndroid",
+                                           base::FEATURE_DISABLED_BY_DEFAULT};
 #endif
 
-}  // namespace content
+}  // namespace features
