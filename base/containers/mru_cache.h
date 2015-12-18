32a831665a1d88f90e1fec1a4569a24e50cd49cc
rtenneti <rtenneti@chromium.org>
2015-12-17 19:05:43 -0800

    MRUCacheBase - Added Swap method to exchange contents of two MRUCache objects.
    BUG=568386 (comment#10)
    R=brettw@chromium.org,thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/1515243003
    
    Cr-Commit-Position: refs/heads/master@{#366000}

--- base/containers/mru_cache.h
+++ base/containers/mru_cache.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_CONTAINERS_MRU_CACHE_H_
 #define BASE_CONTAINERS_MRU_CACHE_H_
 
+#include <algorithm>
 #include <list>
 #include <map>
 #include <utility>
@@ -136,6 +137,14 @@ class MRUCacheBase {
     if (index_iter == index_.end())
       return end();
     return index_iter->second;
+  }
+
+  // Exchanges the contents of |this| by the contents of the |other|.
+  void Swap(MRUCacheBase& other) {
+    ordering_.swap(other.ordering_);
+    index_.swap(other.index_);
+    std::swap(deletor_, other.deletor_);
+    std::swap(max_size_, other.max_size_);
   }
 
   // Erases the item referenced by the given iterator. An iterator to the item
