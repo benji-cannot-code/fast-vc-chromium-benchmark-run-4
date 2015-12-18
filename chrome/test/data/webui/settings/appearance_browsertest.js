088f462b3d913af347e32b08d7f5c1d437d03542
stevenjb <stevenjb@chromium.org>
2015-12-18 14:04:25 -0800

    Add FakeChromeEvent
    This will be needed for bluetooth and networking tests (and any other
    tests that rely on extension APIs with events).
    Initially use it in FakeSettingsPrivate.
    
    BUG=425627
    
    Review URL: https://codereview.chromium.org/1532503003
    
    Cr-Commit-Position: refs/heads/master@{#366181}

--- chrome/test/data/webui/settings/appearance_browsertest.js
+++ chrome/test/data/webui/settings/appearance_browsertest.js
@@ -17,11 +17,6 @@ function AppearanceSettingsBrowserTest() {}
 AppearanceSettingsBrowserTest.prototype = {
   __proto__: SettingsPageBrowserTest.prototype,
 
-  /** @override */
-  extraLibraries: PolymerTest.getLibraries(ROOT_PATH).concat([
-    'fake_settings_private.js',
-  ]),
-
   /** @return {string} */
   appearancePage: function(selector) {
     var section = this.getSection(this.getPage('basic'), 'appearance');
