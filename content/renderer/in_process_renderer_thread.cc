d3f33d730efa1a02c705c77aa8757303b59b208a
hush <hush@chromium.org>
2015-12-17 16:41:42 -0800

    Assign thread name to in_proc renderer and gpu thread.
    This would be very helpful for analyzing traces.txt of ANR logs.
    
    BUG=570819
    
    Review URL: https://codereview.chromium.org/1518213002
    
    Cr-Commit-Position: refs/heads/master@{#365953}

--- content/renderer/in_process_renderer_thread.cc
+++ content/renderer/in_process_renderer_thread.cc
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/render_process_impl.h"
 #include "content/renderer/render_thread_impl.h"
 
+#if defined(OS_ANDROID)
+#include "base/android/jni_android.h"
+#endif
+
 namespace content {
 
 InProcessRendererThread::InProcessRendererThread(
@@ -21,6 +25,13 @@ InProcessRendererThread::~InProcessRendererThread() {
 }
 
 void InProcessRendererThread::Init() {
+  // Call AttachCurrentThreadWithName, before any other AttachCurrentThread()
+  // calls. The latter causes Java VM to assign Thread-??? to the thread name.
+  // Please note calls to AttachCurrentThreadWithName after AttachCurrentThread
+  // will not change the thread name kept in Java VM.
+#if defined(OS_ANDROID)
+  base::android::AttachCurrentThreadWithName(thread_name());
+#endif
   render_process_.reset(new RenderProcessImpl());
   RenderThreadImpl::Create(params_);
 }
