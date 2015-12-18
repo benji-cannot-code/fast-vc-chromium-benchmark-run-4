56decf4f55aeef9498ab559668422d7a5b77447b
lushnikov <lushnikov@chromium.org>
2015-12-17 16:57:13 -0800

    DevTools: respect color format setting in computed tab
    BUG=563952
    R=pfeldman
    
    Review URL: https://codereview.chromium.org/1527183002
    
    Cr-Commit-Position: refs/heads/master@{#365954}

--- third_party/WebKit/Source/devtools/front_end/elements/ComputedStyleWidget.js
+++ third_party/WebKit/Source/devtools/front_end/elements/ComputedStyleWidget.js
@@ -119,6 +119,7 @@ WebInspector.ComputedStyleWidget.prototype = {
             return createTextNode(text);
         var swatch = WebInspector.ColorSwatch.create();
         swatch.setColorText(text);
+        swatch.setFormat(WebInspector.Color.detectColorFormat(color));
         return swatch;
     },
 
