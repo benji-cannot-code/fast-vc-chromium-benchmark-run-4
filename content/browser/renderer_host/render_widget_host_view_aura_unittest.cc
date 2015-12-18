a194fb8d32cc9edbf048573a3dc541ab40ae0c94
sadrul <sadrul@chromium.org>
2015-12-18 00:24:18 -0800

    content: Fix destruction of RenderWidgetHostViewAura.
    RenderWidgetHostViewAura.::Destroy() would destroy the aura::Window it creates,
    and thus implicitly detroy itself (through the OnWindowDestroyed() callback from
    the aura::WindowDelegate interface). However, crrev.com/362663 made a change so
    the aura::Window would not be created until the RenderWidgetHostViewAura is
    initialized (with a call to one of the InitAs*() methods). As a result,
    ::Destroy() would no longer actually delete the RenderWidgetHostViewAura. Fix
    this by making ::Destroy() delete itself if the aura::Window has not been
    created. Also, move resetting |RenderWidgetHostImpl::view_| from
    OnWindowDestroyed() into ~RenderWidgetHostViewAura() to make sure it gets
    reset properly when the destruction happens without proper initialization.
    
    BUG=568797
    
    Review URL: https://codereview.chromium.org/1538513003
    
    Cr-Commit-Position: refs/heads/master@{#366050}

--- content/browser/renderer_host/render_widget_host_view_aura_unittest.cc
+++ content/browser/renderer_host/render_widget_host_view_aura_unittest.cc
@@ -380,7 +380,6 @@ class RenderWidgetHostViewAuraTest : public testing::Test {
   RenderWidgetHostViewAuraTest()
       : widget_host_uses_shutdown_to_destroy_(false),
         is_guest_view_hack_(false),
-        explicit_delete_view_(false),
         browser_thread_for_ui_(BrowserThread::UI, &message_loop_) {}
 
   void SetUpEnvironment() {
@@ -418,11 +417,16 @@ class RenderWidgetHostViewAuraTest : public testing::Test {
   void TearDownEnvironment() {
     sink_ = NULL;
     process_host_ = NULL;
-    if (view_)
+    if (view_) {
+      // For guest-views, |view_| is not the view used by |widget_host_|.
+      if (!is_guest_view_hack_) {
+        EXPECT_EQ(view_, widget_host_->GetView());
+      }
       view_->Destroy();
-
-    if (explicit_delete_view_)
-      delete view_;
+      if (!is_guest_view_hack_) {
+        EXPECT_EQ(nullptr, widget_host_->GetView());
+      }
+    }
 
     if (widget_host_uses_shutdown_to_destroy_)
       widget_host_->Shutdown();
@@ -446,10 +450,6 @@ class RenderWidgetHostViewAuraTest : public testing::Test {
 
   void set_widget_host_uses_shutdown_to_destroy(bool use) {
     widget_host_uses_shutdown_to_destroy_ = use;
-  }
-
-  void set_explicit_delete_view(bool val) {
-    explicit_delete_view_ = val;
   }
 
   void SimulateMemoryPressure(
@@ -516,8 +516,6 @@ class RenderWidgetHostViewAuraTest : public testing::Test {
   bool widget_host_uses_shutdown_to_destroy_;
 
   bool is_guest_view_hack_;
-
-  bool explicit_delete_view_;
 
   base::MessageLoopForUI message_loop_;
   BrowserThreadImpl browser_thread_for_ui_;
@@ -778,8 +776,10 @@ class RenderWidgetHostViewAuraShutdownTest
   DISALLOW_COPY_AND_ASSIGN(RenderWidgetHostViewAuraShutdownTest);
 };
 
-TEST_F(RenderWidgetHostViewAuraTest, CrashInDestructor) {
-  set_explicit_delete_view(true);
+// Checks that RenderWidgetHostViewAura can be destroyed before it is properly
+// initialized.
+TEST_F(RenderWidgetHostViewAuraTest, DestructionBeforeProperInitialization) {
+  // Terminate the test without initializing |view_|.
 }
 
 // Checks that a fullscreen view has the correct show-state and receives the
