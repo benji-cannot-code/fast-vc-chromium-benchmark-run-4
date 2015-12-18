05d7c8c75154473d706cd63b54fac021cff716d0
eugenebut <eugenebut@chromium.org>
2015-12-18 15:49:58 -0800

    [ios] Implemented basic HTTP Authentication for WKWebView.
    This implements HTTP auth only in web layer, and still requires changes
    in downstream Chrome code, which will be done in a separate CL.
    
    BUG=567446
    
    Review URL: https://codereview.chromium.org/1515223002
    
    Cr-Commit-Position: refs/heads/master@{#366215}

--- ios/web/shell/view_controller.mm
+++ ios/web/shell/view_controller.mm
@@ -42,7 +42,7 @@ NSString* const kWebShellBackButtonAccessibilityLabel = @"Back";
 NSString* const kWebShellForwardButtonAccessibilityLabel = @"Forward";
 NSString* const kWebShellAddressFieldAccessibilityLabel = @"Address field";
 
-@interface ViewController () {
+@interface ViewController ()<CRWWebUserInterfaceDelegate> {
   web::BrowserState* _browserState;
   base::scoped_nsobject<CRWWebController> _webController;
   scoped_ptr<web::RequestTrackerFactoryImpl> _requestTrackerFactory;
@@ -127,6 +127,7 @@ NSString* const kWebShellAddressFieldAccessibilityLabel = @"Address field";
       UseWKWebView() ? web::WK_WEB_VIEW_TYPE : web::UI_WEB_VIEW_TYPE;
   _webController.reset(web::CreateWebController(webViewType, webState.Pass()));
   [_webController setDelegate:self];
+  [_webController setUIDelegate:self];
   [_webController setWebUsageEnabled:YES];
 
   [[_webController view] setFrame:[_containerView bounds]];
@@ -358,6 +359,19 @@ NSString* const kWebShellAddressFieldAccessibilityLabel = @"Address field";
                  callback:
                      (const web::WebState::ImageDownloadCallback&)callback {
   return -1;
+}
+
+// -----------------------------------------------------------------------
+// CRWWebUserInterfaceDelegate implementation.
+
+- (void)webController:(CRWWebController*)webController
+    runAuthDialogForProtectionSpace:(NSURLProtectionSpace*)protectionSpace
+                 proposedCredential:(NSURLCredential*)credential
+                  completionHandler:
+                      (void (^)(NSString* user, NSString* password))handler {
+  // Calling |handler| with nil objects is the same as not implemeting it. This
+  // method is implemented to make testing easier.
+  handler(nil, nil);
 }
 
 @end
