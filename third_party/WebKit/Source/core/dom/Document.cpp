0a06c8c966ea4dd70df98de4131a1ee4bbb61d01
mharanczyk <mharanczyk@opera.com>
2015-12-18 03:48:56 -0800

    Change how DOM Inspector fetches document's base URL.
    With integration of https://codereview.chromium.org/1409293007 appending
    empty string is invalid operation for non hierarchical base urls. Since
    DOM Inspector Agent actually want to determine base URL ask for that
    data directly.
    Without this change base url for non hierarchical (data:) urls was
    always empty for inspector, which in turn caused webdriver to freeze
    when trying to communicate with such documents, because it wrongly
    assumed it is still in loading state (base url was empty),
    so it waited for load complete.
    
    Review URL: https://codereview.chromium.org/1530153002
    
    Cr-Commit-Position: refs/heads/master@{#366079}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -4308,6 +4308,15 @@ KURL Document::completeURLWithOverride(const String& url, const KURL& baseURLOve
     if (url.isNull())
         return KURL();
     // This logic is deliberately spread over many statements in an attempt to track down http://crbug.com/312410.
+    const KURL& baseURL = baseURLForOverride(baseURLOverride);
+    if (!encoding().isValid())
+        return KURL(baseURL, url);
+    return KURL(baseURL, url, encoding());
+}
+
+const KURL& Document::baseURLForOverride(const KURL& baseURLOverride) const
+{
+    // This logic is deliberately spread over many statements in an attempt to track down http://crbug.com/312410.
     const KURL* baseURLFromParent = 0;
     bool shouldUseParentBaseURL = baseURLOverride.isEmpty();
     if (!shouldUseParentBaseURL) {
@@ -4318,10 +4327,7 @@ KURL Document::completeURLWithOverride(const String& url, const KURL& baseURLOve
         if (Document* parent = parentDocument())
             baseURLFromParent = &parent->baseURL();
     }
-    const KURL& baseURL = baseURLFromParent ? *baseURLFromParent : baseURLOverride;
-    if (!encoding().isValid())
-        return KURL(baseURL, url);
-    return KURL(baseURL, url, encoding());
+    return baseURLFromParent ? *baseURLFromParent : baseURLOverride;
 }
 
 // Support for Javascript execCommand, and related methods
