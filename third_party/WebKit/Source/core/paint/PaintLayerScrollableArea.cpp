b956465b3f4f35633428818a45375145b973d5bf
skobes <skobes@chromium.org>
2015-12-18 15:50:17 -0800

    Call WebFrameClient::didChangeScrollOffset for root layer scrolls.
    BUG=490942
    
    Review URL: https://codereview.chromium.org/1537053002
    
    Cr-Commit-Position: refs/heads/master@{#366216}

--- third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/LayoutView.h"
 #include "core/layout/compositing/CompositedLayerMapping.h"
 #include "core/layout/compositing/PaintLayerCompositor.h"
+#include "core/loader/FrameLoaderClient.h"
 #include "core/page/ChromeClient.h"
 #include "core/page/FocusController.h"
 #include "core/page/Page.h"
@@ -403,8 +404,10 @@ void PaintLayerScrollableArea::setScrollOffset(const DoublePoint& newScrollOffse
     box().view()->clearHitTestCache();
 
     // Inform the FrameLoader of the new scroll position, so it can be restored when navigating back.
-    if (layer()->isRootLayer())
+    if (layer()->isRootLayer()) {
         frameView->frame().loader().saveScrollState();
+        frame->loader().client()->didChangeScrollOffset();
+    }
 
     // All scrolls clear the scroll anchor.
     frameView->clearScrollAnchor();
--- third_party/WebKit/Source/web/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/web/tests/WebFrameTest.cpp
@@ -5658,7 +5658,7 @@ private:
     bool m_didScrollFrame;
 };
 
-TEST_F(WebFrameTest, CompositorScrollIsUserScrollLongPage)
+TEST_P(ParameterizedWebFrameTest, CompositorScrollIsUserScrollLongPage)
 {
     registerMockedHttpURLLoad("long_scroll.html");
     TestScrolledFrameClient client;
