a4bad35485af1bd2ed3a8339e63dac887df10042
nick <nick@chromium.org>
2015-12-17 17:44:12 -0800

    Enhance the WebContentsObserverSanityChecker to validate tree structure
    In particular, assert the following:
     - Parents are created/committed before children
     - Children are deleted/detached before parents
     - The value returned by GetParent() does not change over an RFH's lifetime.
    
    This exposed two problematic and surprising behaviors:
     - Calling GetParent() during FrameDeleted/RenderFrameDeleted would sometimes
       return a different node from GetParent() during creation (!!!). This is
       because we deleted the children of the old frame after setting the new frame
       as the current.
     - Parents were getting detached before their children, due to our reliance
       on ScopedVector's cleanup of children in FrameTreeNode.
    
    Review URL: https://codereview.chromium.org/1410373008
    
    Cr-Commit-Position: refs/heads/master@{#365976}

--- content/browser/frame_host/navigation_controller_impl_unittest.cc
+++ content/browser/frame_host/navigation_controller_impl_unittest.cc
@@ -2045,8 +2045,9 @@ TEST_F(NavigationControllerTest, NewSubframe) {
 
   // Prereq: add a subframe with an initial auto-subframe navigation.
   main_test_rfh()->OnCreateChildFrame(
-      MSG_ROUTING_NONE, blink::WebTreeScopeType::Document, std::string(),
-      blink::WebSandboxFlags::None, blink::WebFrameOwnerProperties());
+      process()->GetNextRoutingID(), blink::WebTreeScopeType::Document,
+      std::string(), blink::WebSandboxFlags::None,
+      blink::WebFrameOwnerProperties());
   RenderFrameHostImpl* subframe =
       contents()->GetFrameTree()->root()->child_at(0)->current_frame_host();
   const GURL subframe_url("http://foo1/subframe");
@@ -2125,8 +2126,9 @@ TEST_F(NavigationControllerTest, AutoSubframe) {
 
   // Add a subframe and navigate it.
   main_test_rfh()->OnCreateChildFrame(
-      MSG_ROUTING_NONE, blink::WebTreeScopeType::Document, std::string(),
-      blink::WebSandboxFlags::None, blink::WebFrameOwnerProperties());
+      process()->GetNextRoutingID(), blink::WebTreeScopeType::Document,
+      std::string(), blink::WebSandboxFlags::None,
+      blink::WebFrameOwnerProperties());
   RenderFrameHostImpl* subframe =
       contents()->GetFrameTree()->root()->child_at(0)->current_frame_host();
   const GURL url2("http://foo/2");
@@ -2170,8 +2172,9 @@ TEST_F(NavigationControllerTest, AutoSubframe) {
 
   // Add a second subframe and navigate.
   main_test_rfh()->OnCreateChildFrame(
-      MSG_ROUTING_NONE, blink::WebTreeScopeType::Document, std::string(),
-      blink::WebSandboxFlags::None, blink::WebFrameOwnerProperties());
+      process()->GetNextRoutingID(), blink::WebTreeScopeType::Document,
+      std::string(), blink::WebSandboxFlags::None,
+      blink::WebFrameOwnerProperties());
   RenderFrameHostImpl* subframe2 =
       contents()->GetFrameTree()->root()->child_at(1)->current_frame_host();
   const GURL url3("http://foo/3");
@@ -2214,7 +2217,7 @@ TEST_F(NavigationControllerTest, AutoSubframe) {
   }
 
   // Add a nested subframe and navigate.
-  subframe->OnCreateChildFrame(MSG_ROUTING_NONE,
+  subframe->OnCreateChildFrame(process()->GetNextRoutingID(),
                                blink::WebTreeScopeType::Document, std::string(),
                                blink::WebSandboxFlags::None,
                                blink::WebFrameOwnerProperties());
@@ -2280,8 +2283,9 @@ TEST_F(NavigationControllerTest, BackSubframe) {
 
   // Prereq: add a subframe with an initial auto-subframe navigation.
   main_test_rfh()->OnCreateChildFrame(
-      MSG_ROUTING_NONE, blink::WebTreeScopeType::Document, std::string(),
-      blink::WebSandboxFlags::None, blink::WebFrameOwnerProperties());
+      process()->GetNextRoutingID(), blink::WebTreeScopeType::Document,
+      std::string(), blink::WebSandboxFlags::None,
+      blink::WebFrameOwnerProperties());
   RenderFrameHostImpl* subframe =
       contents()->GetFrameTree()->root()->child_at(0)->current_frame_host();
   const GURL subframe_url("http://foo1/subframe");
@@ -3713,8 +3717,9 @@ TEST_F(NavigationControllerTest, SameSubframe) {
 
   // Add and navigate a subframe that would normally count as in-page.
   main_test_rfh()->OnCreateChildFrame(
-      MSG_ROUTING_NONE, blink::WebTreeScopeType::Document, std::string(),
-      blink::WebSandboxFlags::None, blink::WebFrameOwnerProperties());
+      process()->GetNextRoutingID(), blink::WebTreeScopeType::Document,
+      std::string(), blink::WebSandboxFlags::None,
+      blink::WebFrameOwnerProperties());
   RenderFrameHostImpl* subframe =
       contents()->GetFrameTree()->root()->child_at(0)->current_frame_host();
   const GURL subframe_url("http://www.google.com/#");
@@ -3879,8 +3884,9 @@ TEST_F(NavigationControllerTest, SubframeWhilePending) {
   // Send a subframe update from the first page, as if one had just
   // automatically loaded. Auto subframes don't increment the page ID.
   main_test_rfh()->OnCreateChildFrame(
-      MSG_ROUTING_NONE, blink::WebTreeScopeType::Document, std::string(),
-      blink::WebSandboxFlags::None, blink::WebFrameOwnerProperties());
+      process()->GetNextRoutingID(), blink::WebTreeScopeType::Document,
+      std::string(), blink::WebSandboxFlags::None,
+      blink::WebFrameOwnerProperties());
   RenderFrameHostImpl* subframe =
       contents()->GetFrameTree()->root()->child_at(0)->current_frame_host();
   const GURL url1_sub("http://foo/subframe");
--- content/test/web_contents_observer_sanity_checker.h
+++ content/test/web_contents_observer_sanity_checker.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/supports_user_data.h"
+#include "content/browser/loader/global_routing_id.h"
 #include "content/public/browser/web_contents_observer.h"
 
 namespace content {
@@ -98,12 +99,18 @@ class WebContentsObserverSanityChecker : public WebContentsObserver,
 
   bool NavigationIsOngoing(NavigationHandle* navigation_handle);
 
-  std::set<std::pair<int, int>> current_hosts_;
-  std::set<std::pair<int, int>> live_routes_;
-  std::set<std::pair<int, int>> deleted_routes_;
+  void EnsureStableParentValue(RenderFrameHost* render_frame_host);
+  bool HasAnyChildren(RenderFrameHost* render_frame_host);
+
+  std::set<GlobalRoutingID> current_hosts_;
+  std::set<GlobalRoutingID> live_routes_;
+  std::set<GlobalRoutingID> deleted_routes_;
 
   std::set<NavigationHandle*> ongoing_navigations_;
   std::vector<MediaPlayerId> active_media_players_;
+
+  // Remembers parents to make sure RenderFrameHost::GetParent() never changes.
+  std::map<GlobalRoutingID, GlobalRoutingID> parent_ids_;
 
   bool web_contents_destroyed_;
 
