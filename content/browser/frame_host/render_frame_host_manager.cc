6e6b8cb114bdc205ea5abcec86bf8d70e36c9644
nick <nick@chromium.org>
2015-12-17 17:39:37 -0800

    kFrameTreeNodeInvalidID -> kFrameTreeNodeInvalidId
    Change "ID" to "Id" to conform with Chromium's coding style.
    
    As discussed at https://groups.google.com/a/chromium.org/d/msg/site-isolation-dev/NsYJyqLXPjQ/7FEPeewRAAAJ
    
    Review URL: https://codereview.chromium.org/1513293002
    
    Cr-Commit-Position: refs/heads/master@{#365973}

--- content/browser/frame_host/render_frame_host_manager.cc
+++ content/browser/frame_host/render_frame_host_manager.cc
@@ -316,7 +316,7 @@ RenderWidgetHostView* RenderFrameHostManager::GetRenderWidgetHostView() const {
 
 bool RenderFrameHostManager::ForInnerDelegate() {
   return delegate_->GetOuterDelegateFrameTreeNodeId() !=
-      FrameTreeNode::kFrameTreeNodeInvalidID;
+      FrameTreeNode::kFrameTreeNodeInvalidId;
 }
 
 RenderWidgetHostImpl*
