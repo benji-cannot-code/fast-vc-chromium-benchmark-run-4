e2a6fb6cce9596bf9c23694cc97405417204a4b9
tommycli <tommycli@chromium.org>
2015-12-17 20:59:36 -0800

    Plugin Power Saver: Fix a use-after-free / ambiguous lifetime bugs.
    This patch fixes some issues where we are using a plugin container
    after it has been destroyed.
    
    This also adds some comments clarifying these relationships.
    
    BUG=549510
    
    Review URL: https://codereview.chromium.org/1511373002
    
    Cr-Commit-Position: refs/heads/master@{#366016}

--- third_party/WebKit/Source/web/WebPluginContainerImpl.cpp
+++ third_party/WebKit/Source/web/WebPluginContainerImpl.cpp
@@ -269,6 +269,7 @@ void WebPluginContainerImpl::setParent(Widget* widget)
 
 void WebPluginContainerImpl::setPlugin(WebPlugin* plugin)
 {
+    RELEASE_ASSERT(!m_inDispose);
     if (plugin != m_webPlugin) {
         m_element->resetInstance();
         m_webPlugin = plugin;
