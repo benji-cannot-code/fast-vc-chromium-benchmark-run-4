2a8a17ff406a4669d759a7a6c97ea6e075bb81c1
sebsg <sebsg@chromium.org>
2015-12-18 13:57:18 -0800

    Do a case insensitive comparison when determining possible fields to upload.
    BUG=555931
    
    Review URL: https://codereview.chromium.org/1536043003
    
    Cr-Commit-Position: refs/heads/master@{#366179}

--- components/autofill/core/browser/form_group.cc
+++ components/autofill/core/browser/form_group.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/form_group.h"
 
 #include "components/autofill/core/browser/autofill_type.h"
+#include "components/autofill/core/common/autofill_l10n_util.h"
 
 namespace autofill {
 
@@ -17,10 +18,11 @@ void FormGroup::GetMatchingTypes(const base::string16& text,
     return;
   }
 
+  l10n::CaseInsensitiveCompare compare;
   ServerFieldTypeSet types;
   GetSupportedTypes(&types);
   for (const auto& type : types) {
-    if (GetInfo(AutofillType(type), app_locale) == text)
+    if (compare.StringsEqual(text, GetInfo(AutofillType(type), app_locale)))
       matching_types->insert(type);
   }
 }
