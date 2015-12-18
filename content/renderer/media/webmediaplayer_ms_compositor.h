c1549a214fd90768f287589272edd37fd044e910
qiangchen <qiangchen@chromium.org>
2015-12-18 10:07:41 -0800

    Reland 1417533006: Unit Test For WebMediaPlayerMS The original review thread is in https://codereview.chromium.org/1417533006/
    We add a unit test for WebMediaPlayerMS in this CL.
    
    CQ_INCLUDE_TRYBOTS=
    BUG=
    
    Review URL: https://codereview.chromium.org/1537493002
    
    Cr-Commit-Position: refs/heads/master@{#366124}

--- content/renderer/media/webmediaplayer_ms_compositor.h
+++ content/renderer/media/webmediaplayer_ms_compositor.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/threading/thread_checker.h"
 #include "cc/layers/video_frame_provider.h"
+#include "content/common/content_export.h"
 
 namespace base {
 class SingleThreadTaskRunner;
@@ -45,13 +46,18 @@ class WebMediaPlayerMS;
 // smoothness, if REFERENCE_TIMEs are populated for incoming VideoFrames.
 // Otherwise, WebMediaPlayerMSCompositor will simply store the most recent
 // frame, and submit it whenever asked by the compositor.
-class WebMediaPlayerMSCompositor : public cc::VideoFrameProvider {
+class CONTENT_EXPORT WebMediaPlayerMSCompositor
+    : public NON_EXPORTED_BASE(cc::VideoFrameProvider) {
  public:
-  // This |url| represents the media stream we are rendering.
+  // This |url| represents the media stream we are rendering. |url| is used to
+  // find out what web stream this WebMediaPlayerMSCompositor is playing, and
+  // together with flag "--disable-rtc-smoothness-algorithm" determine whether
+  // we enable algorithm or not.
   WebMediaPlayerMSCompositor(
       const scoped_refptr<base::SingleThreadTaskRunner>& compositor_task_runner,
       const blink::WebURL& url,
       const base::WeakPtr<WebMediaPlayerMS>& player);
+
   ~WebMediaPlayerMSCompositor() override;
 
   void EnqueueFrame(const scoped_refptr<media::VideoFrame>& frame);
@@ -76,6 +82,8 @@ class WebMediaPlayerMSCompositor : public cc::VideoFrameProvider {
   void ReplaceCurrentFrameWithACopy();
 
  private:
+  friend class WebMediaPlayerMSTest;
+
   bool MapTimestampsToRenderTimeTicks(
       const std::vector<base::TimeDelta>& timestamps,
       std::vector<base::TimeTicks>* wall_clock_times);
@@ -88,6 +96,8 @@ class WebMediaPlayerMSCompositor : public cc::VideoFrameProvider {
 
   void StartRenderingInternal();
   void StopRenderingInternal();
+
+  void SetAlgorithmEnabledForTesting(bool algorithm_enabled);
 
   // Used for DCHECKs to ensure method calls executed in the correct thread.
   base::ThreadChecker thread_checker_;
