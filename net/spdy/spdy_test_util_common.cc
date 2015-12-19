6086b90da8597bd3819e8b34b10c07a2dcea8192
mmenke <mmenke@chromium.org>
2015-12-18 20:16:33 -0800

    Remove DeterministicSocketData, replacing it with SequencedSocketData.
    DeterministicSocketData is dificult to debug, and does a number of weird
    and unfortunate things, including using a weird way of spinning message
    loops.  It also fails to keep tests honest - tests with bugs in some of
    their set up code can still pass, which can reduce test coverage.
    
    SequencedSocketData was written a while back to replace it.
    
    This CL replaces all remaining uses of DeterministicSocketData (All for
    HTTP/2 testing) with SequencedSocketData.
    
    BUG=492684, 492688, 492691, 492697
    
    Review URL: https://codereview.chromium.org/1538973003
    
    Cr-Commit-Position: refs/heads/master@{#366262}

--- net/spdy/spdy_test_util_common.cc
+++ net/spdy/spdy_test_util_common.cc
@@ -342,7 +342,6 @@ SpdySessionDependencies::SpdySessionDependencies(NextProto protocol)
       proxy_service(ProxyService::CreateDirect()),
       ssl_config_service(new SSLConfigServiceDefaults),
       socket_factory(new MockClientSocketFactory),
-      deterministic_socket_factory(new DeterministicMockClientSocketFactory),
       http_auth_handler_factory(
           HttpAuthHandlerFactory::CreateDefault(host_resolver.get())),
       enable_ip_pooling(true),
@@ -378,7 +377,6 @@ SpdySessionDependencies::SpdySessionDependencies(
       proxy_service(proxy_service.Pass()),
       ssl_config_service(new SSLConfigServiceDefaults),
       socket_factory(new MockClientSocketFactory),
-      deterministic_socket_factory(new DeterministicMockClientSocketFactory),
       http_auth_handler_factory(
           HttpAuthHandlerFactory::CreateDefault(host_resolver.get())),
       enable_ip_pooling(true),
@@ -404,19 +402,6 @@ scoped_ptr<HttpNetworkSession> SpdySessionDependencies::SpdyCreateSession(
     SpdySessionDependencies* session_deps) {
   HttpNetworkSession::Params params = CreateSessionParams(session_deps);
   params.client_socket_factory = session_deps->socket_factory.get();
-  scoped_ptr<HttpNetworkSession> http_session(new HttpNetworkSession(params));
-  SpdySessionPoolPeer pool_peer(http_session->spdy_session_pool());
-  pool_peer.SetEnableSendingInitialData(false);
-  return http_session.Pass();
-}
-
-// static
-scoped_ptr<HttpNetworkSession>
-SpdySessionDependencies::SpdyCreateSessionDeterministic(
-    SpdySessionDependencies* session_deps) {
-  HttpNetworkSession::Params params = CreateSessionParams(session_deps);
-  params.client_socket_factory =
-      session_deps->deterministic_socket_factory.get();
   scoped_ptr<HttpNetworkSession> http_session(new HttpNetworkSession(params));
   SpdySessionPoolPeer pool_peer(http_session->spdy_session_pool());
   pool_peer.SetEnableSendingInitialData(false);
