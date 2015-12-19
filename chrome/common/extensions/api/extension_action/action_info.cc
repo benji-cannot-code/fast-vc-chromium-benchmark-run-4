6ad7cebb18921f235232da218d1f8545cf605fe7
estade <estade@chromium.org>
2015-12-18 18:39:37 -0800

    Change extension icon load errors to warnings
    During the Extension parsing step, check if the icon file exists and if not, remove that entry from the dictionary.
    
    Keep the same check during the validation phase and don't apply the workaround to unpacked extensions. This will more strongly discourage new extensions from making this mistake.
    
    BUG=570249
    
    Review URL: https://codereview.chromium.org/1537473003
    
    Cr-Commit-Position: refs/heads/master@{#366253}

--- chrome/common/extensions/api/extension_action/action_info.cc
+++ chrome/common/extensions/api/extension_action/action_info.cc
@@ -53,7 +53,7 @@ ActionInfo::~ActionInfo() {
 }
 
 // static
-scoped_ptr<ActionInfo> ActionInfo::Load(const Extension* extension,
+scoped_ptr<ActionInfo> ActionInfo::Load(Extension* extension,
                                         const base::DictionaryValue* dict,
                                         base::string16* error) {
   scoped_ptr<ActionInfo> result(new ActionInfo());
@@ -93,7 +93,7 @@ scoped_ptr<ActionInfo> ActionInfo::Load(const Extension* extension,
     std::string default_icon;
     if (dict->GetDictionary(keys::kPageActionDefaultIcon, &icons_value)) {
       if (!manifest_handler_helpers::LoadIconsFromDictionary(
-              icons_value, &result->default_icon, error)) {
+              extension, icons_value, &result->default_icon, error)) {
         return scoped_ptr<ActionInfo>();
       }
     } else if (dict->GetString(keys::kPageActionDefaultIcon, &default_icon) &&
