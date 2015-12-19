1a5034c5b1ff71bdeda0d55f89cb3f351f557475
mcasas <mcasas@chromium.org>
2015-12-18 16:42:58 -0800

    MediaRecorder: correct MIME type parsing
    Currently the expected/supported MIME types are in the form of
    'video/vp8', 'audio/opus' etc, this CL changes that to the
    MIME conforming 'video/webm;codecs=vp8', 'video/webm;codecs=vp9',
    'audio/webm;codecs=ops' following webm container guidelines [1]
    
    BUG=564950, 262211, 571107
    [1] http://www.webmproject.org/docs/container/
    
    TBR=esprehn@chromium.org: Tiny modification in the method
    signature in public/platform/WebMediaRecorderHandler.h, where
    one argument is splitted into two.
    
    Review URL: https://codereview.chromium.org/1534553003
    
    Cr-Commit-Position: refs/heads/master@{#366232}

--- third_party/WebKit/Source/modules/mediarecorder/MediaRecorder.cpp
+++ third_party/WebKit/Source/modules/mediarecorder/MediaRecorder.cpp
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/EventTargetModules.h"
 #include "modules/mediarecorder/BlobEvent.h"
 #include "modules/mediarecorder/MediaRecorderErrorEvent.h"
+#include "platform/ContentType.h"
 #include "platform/NotImplemented.h"
 #include "platform/blob/BlobData.h"
 #include "public/platform/Platform.h"
@@ -74,7 +75,8 @@ MediaRecorder::MediaRecorder(ExecutionContext* context, MediaStream* stream, con
         exceptionState.throwDOMException(NotSupportedError, "No MediaRecorder handler can be created.");
         return;
     }
-    if (!m_recorderHandler->initialize(this, stream->descriptor(), m_mimeType)) {
+    ContentType contentType(m_mimeType);
+    if (!m_recorderHandler->initialize(this, stream->descriptor(), contentType.type(), contentType.parameter("codecs"))) {
         exceptionState.throwDOMException(NotSupportedError, "Failed to initialize native MediaRecorder, the type provided " + m_mimeType + "is unsupported." );
         return;
     }
@@ -167,7 +169,8 @@ bool MediaRecorder::isTypeSupported(const String& type)
     // specified MIME type. Recording may still fail if sufficient resources are
     // not available to support the concrete media encoding.
     // [1] https://w3c.github.io/mediacapture-record/MediaRecorder.html#methods
-    return handler->canSupportMimeType(type);
+    ContentType contentType(type);
+    return handler->canSupportMimeType(contentType.type(), contentType.parameter("codecs"));
 }
 
 const AtomicString& MediaRecorder::interfaceName() const
