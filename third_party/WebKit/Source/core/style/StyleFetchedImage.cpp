fd9f866dbc6b1fd6ba7359f6db3da4f9ee640a05
sigbjornf <sigbjornf@opera.com>
2015-12-18 23:43:49 -0800

    Oilpan: prefinalize StyleFetchedImage* image resource clients.
    Followup r366092 and switch finalization mechanism for
    StyleFetchedImage and StyleFetchedImageSet to prefinalizers.
    
    Having them be eagerly finalized conflicted in some cases with
    another eagerly finalized object (a FrameView scrollable area),
    as these StyleFetchedImage objects cannot be allowed to
    touch another eagerly finalized object.
    
    Avoid the finalization (non-)ordering issue by switching these
    to prefinalized objects instead.
    
    R=haraken
    BUG=
    
    Review URL: https://codereview.chromium.org/1536113003
    
    Cr-Commit-Position: refs/heads/master@{#366275}

--- third_party/WebKit/Source/core/style/StyleFetchedImage.cpp
+++ third_party/WebKit/Source/core/style/StyleFetchedImage.cpp
@@ -40,9 +40,19 @@ StyleFetchedImage::StyleFetchedImage(ImageResource* image, Document* document, c
 {
     m_isImageResource = true;
     m_image->addClient(this);
+#if ENABLE(OILPAN)
+    ThreadState::current()->registerPreFinalizer(this);
+#endif
 }
 
 StyleFetchedImage::~StyleFetchedImage()
+{
+#if !ENABLE(OILPAN)
+    dispose();
+#endif
+}
+
+void StyleFetchedImage::dispose()
 {
     m_image->removeClient(this);
 }
