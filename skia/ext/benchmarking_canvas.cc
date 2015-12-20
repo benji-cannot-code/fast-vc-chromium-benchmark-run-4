4549b586fdbe114dae42bc9874a426ca7ddca813
reed <reed@google.com>
2015-12-19 16:54:55 -0800

    switch to using SaveLayerFlags (SaveFlags are deprecated)
    prepares the way for https://codereview.chromium.org/1535993003
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1537203002
    
    Cr-Commit-Position: refs/heads/master@{#366288}

--- skia/ext/benchmarking_canvas.cc
+++ skia/ext/benchmarking_canvas.cc
@@ -275,14 +275,12 @@ scoped_ptr<base::Value> AsValue(const SkPaint& paint) {
 }
 
 WARN_UNUSED_RESULT
-scoped_ptr<base::Value> AsValue(SkCanvas::SaveFlags flags) {
+scoped_ptr<base::Value> SaveLayerFlagsAsValue(SkCanvas::SaveLayerFlags flags) {
   FlagsBuilder builder('|');
-  builder.addFlag(flags & SkCanvas::kHasAlphaLayer_SaveFlag,
-                  "kHasAlphaLayer");
-  builder.addFlag(flags & SkCanvas::kFullColorLayer_SaveFlag,
-                  "kFullColorLayer");
-  builder.addFlag(flags & SkCanvas::kClipToLayer_SaveFlag,
-                  "kClipToLayer");
+  builder.addFlag(flags & SkCanvas::kIsOpaque_SaveLayerFlag,
+                  "kIsOpaque");
+  builder.addFlag(flags & SkCanvas::kPreserveLCDText_SaveLayerFlag,
+                  "kPreserveLCDText");
 
   scoped_ptr<base::StringValue> val(new base::StringValue(builder.str()));
 
@@ -551,7 +549,7 @@ SkCanvas::SaveLayerStrategy BenchmarkingCanvas::getSaveLayerStrategy(
   if (rec.fBounds)
     op.addParam("bounds", AsValue(*rec.fBounds));
   if (rec.fSaveLayerFlags)
-    op.addParam("flags", AsValue(rec.fSaveLayerFlags));
+    op.addParam("flags", SaveLayerFlagsAsValue(rec.fSaveLayerFlags));
 
   return INHERITED::getSaveLayerStrategy(rec);
 }
