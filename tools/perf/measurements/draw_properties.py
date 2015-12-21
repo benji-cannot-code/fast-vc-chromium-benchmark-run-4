36f3de675d1acb77476157ddd205a8fde7bbe07e
rnephew <rnephew@chromium.org>
2015-12-21 15:08:48 -0800

    [Telemetry] moving tracing options/category filters to tracing config
    Currently they are using options and category filter as 2 seperate
    things being passed. This changes it to use Tracing Config, which has
    both of those.
    
    There will be a follow-up CL that gets rid of tracing_options completely.
    
    BUG=570489
    TBR=dtu@chromium.org
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_10_10_perf_bisect;tryserver.chromium.perf:win_perf_bisect
    
    Review URL: https://codereview.chromium.org/1538663005
    
    Cr-Commit-Position: refs/heads/master@{#366486}

--- tools/perf/measurements/draw_properties.py
+++ tools/perf/measurements/draw_properties.py
@@ -5,8 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from telemetry.page import page_test
 from telemetry.timeline import model
-from telemetry.timeline import tracing_category_filter
-from telemetry.timeline import tracing_options
+from telemetry.timeline import tracing_config
 from telemetry.value import scalar
 
 
@@ -20,11 +19,11 @@ class DrawProperties(page_test.PageTest):
     ])
 
   def WillNavigateToPage(self, page, tab):
-    options = tracing_options.TracingOptions()
-    options.enable_chrome_trace = True
-    category_filter = tracing_category_filter.TracingCategoryFilter(
+    config = tracing_config.TracingConfig()
+    config.tracing_category_filter.AddDisabledByDefault(
         'disabled-by-default-cc.debug.cdp-perf')
-    tab.browser.platform.tracing_controller.Start(options, category_filter)
+    config.tracing_options.enable_chrome_trace = True
+    tab.browser.platform.tracing_controller.Start(config)
 
   def ComputeAverageOfDurations(self, timeline_model, name):
     events = timeline_model.GetAllEventsOfName(name)
