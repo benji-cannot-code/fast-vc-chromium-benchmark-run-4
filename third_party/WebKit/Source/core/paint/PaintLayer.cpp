327fa1bd6fefc41767769d0b7f42410d82ad1cec
tkent <tkent@chromium.org>
2015-12-21 02:10:31 -0800

    Remove config.h inclusion from third_party/WebKit/Source/core/paint/.
    This CL has no behavior changes.
    
    BUG=570234
    TBR=yutak@chromium.org
    
    Review URL: https://codereview.chromium.org/1537393002
    
    Cr-Commit-Position: refs/heads/master@{#366361}

--- third_party/WebKit/Source/core/paint/PaintLayer.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayer.cpp
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * version of this file under any of the LGPL, the MPL or the GPL.
  */
 
-#include "config.h"
 #include "core/paint/PaintLayer.h"
 
 #include "core/CSSPropertyNames.h"
