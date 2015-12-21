d9b46361e75d5f2069caa39f4320ee1cb05be13e
tkent <tkent@chromium.org>
2015-12-21 01:08:10 -0800

    Remove config.h inclusion from third_party/WebKit/Source/core/inspector/.
    This CL has no behavior changes.
    
    BUG=570234
    TBR=yutak@chromium.org
    
    Review URL: https://codereview.chromium.org/1537373002
    
    Cr-Commit-Position: refs/heads/master@{#366353}

--- third_party/WebKit/Source/core/inspector/InspectorTracingAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorTracingAgent.cpp
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 //
 
-#include "config.h"
-
 #include "core/inspector/InspectorTracingAgent.h"
 
 #include "core/frame/LocalFrame.h"
