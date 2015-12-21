bc63ba4e57a0343a5596d0f54c1eace19e926730
loyso <loyso@chromium.org>
2015-12-21 02:03:24 -0800

    CC Animations: Port UI Browser Compositor to use compositor animation timelines.
    Use AnimationPlayer to add animations if new system enabled.
    
    BUG=394777
    
    Review URL: https://codereview.chromium.org/1533643003
    
    Cr-Commit-Position: refs/heads/master@{#366360}

--- ui/compositor/compositor.cc
+++ ui/compositor/compositor.cc
@@ -16,6 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/sys_info.h"
 #include "base/trace_event/trace_event.h"
+#include "cc/animation/animation_host.h"
+#include "cc/animation/animation_id_provider.h"
+#include "cc/animation/animation_timeline.h"
 #include "cc/base/switches.h"
 #include "cc/input/input_handler.h"
 #include "cc/layers/layer.h"
@@ -191,6 +194,12 @@ Compositor::Compositor(ui::ContextFactory* context_factory,
   host_ = cc::LayerTreeHost::CreateSingleThreaded(this, &params);
   UMA_HISTOGRAM_TIMES("GPU.CreateBrowserCompositor",
                       base::TimeTicks::Now() - before_create);
+
+  if (settings.use_compositor_animation_timelines) {
+    animation_timeline_ = cc::AnimationTimeline::Create(
+        cc::AnimationIdProvider::NextTimelineId());
+    host_->animation_host()->AddAnimationTimeline(animation_timeline_.get());
+  }
   host_->SetRootLayer(root_web_layer_);
   host_->set_surface_id_namespace(surface_id_allocator_->id_namespace());
   host_->SetVisible(true);
@@ -210,6 +219,9 @@ Compositor::~Compositor() {
 
   if (root_layer_)
     root_layer_->ResetCompositor();
+
+  if (animation_timeline_)
+    host_->animation_host()->RemoveAnimationTimeline(animation_timeline_.get());
 
   // Stop all outstanding draws before telling the ContextFactory to tear
   // down any contexts that the |host_| may rely upon.
@@ -237,6 +249,10 @@ void Compositor::SetRootLayer(Layer* root_layer) {
   root_web_layer_->RemoveAllChildren();
   if (root_layer_)
     root_layer_->SetCompositor(this, root_web_layer_);
+}
+
+cc::AnimationTimeline* Compositor::GetAnimationTimeline() const {
+  return animation_timeline_.get();
 }
 
 void Compositor::SetHostHasTransparentBackground(
--- ui/compositor/layer_animator.h
+++ ui/compositor/layer_animator.h
@@ -21,6 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/animation/tween.h"
 
 namespace cc {
+class Animation;
+class AnimationPlayer;
+class AnimationTimeline;
 class Layer;
 }
 
@@ -31,6 +34,7 @@ class Transform;
 }
 
 namespace ui {
+class Compositor;
 class Layer;
 class LayerAnimationSequence;
 class LayerAnimationDelegate;
@@ -107,6 +111,16 @@ class COMPOSITOR_EXPORT LayerAnimator
 
   // Unsubscribe from |cc_layer_| and subscribe to |new_layer|.
   void SwitchToLayer(scoped_refptr<cc::Layer> new_layer);
+
+  // Attach AnimationPlayer to Layer and AnimationTimeline
+  void SetCompositor(Compositor* compositor);
+  // Detach AnimationPlayer from Layer and AnimationTimeline
+  void ResetCompositor(Compositor* compositor);
+
+  // TODO(loyso): Rework it as an implementation for
+  // LayerThreadedAnimationDelegate and make it private.
+  void AddThreadedAnimation(scoped_ptr<cc::Animation> animation);
+  void RemoveThreadedAnimation(int animation_id);
 
   // Sets the animation preemption strategy. This determines the behaviour if
   // a property is set during an animation. The default is
@@ -325,11 +339,17 @@ class COMPOSITOR_EXPORT LayerAnimator
   // LayerAnimationEventObserver
   void OnAnimationStarted(const cc::AnimationEvent& event) override;
 
+  void AttachLayerToAnimationPlayer(int layer_id);
+  void DetachLayerFromAnimationPlayer();
+
   // This is the queue of animations to run.
   AnimationQueue animation_queue_;
 
   // The target of all layer animations.
   LayerAnimationDelegate* delegate_;
+
+  // Plays CC animations.
+  scoped_refptr<cc::AnimationPlayer> animation_player_;
 
   // The currently running animations.
   RunningAnimations running_animations_;
