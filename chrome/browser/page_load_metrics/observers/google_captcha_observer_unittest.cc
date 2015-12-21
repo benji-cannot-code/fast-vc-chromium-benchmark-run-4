86087609f3094065187c64d228abce8bd2c69b4f
csharrison <csharrison@chromium.org>
2015-12-21 06:17:32 -0800

    [page_load_metrics] Split up unittests for individual observers This change adds a test class for PageLoadMetricsObservers to use for their unittests. It also splits up the unittests that were all in the same file to their own separate file.
    BUG=555898
    
    Review URL: https://codereview.chromium.org/1523663004
    
    Cr-Commit-Position: refs/heads/master@{#366376}

---
+++ chrome/browser/page_load_metrics/observers/google_captcha_observer_unittest.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/page_load_metrics/observers/google_captcha_observer.h"
+#include "chrome/browser/page_load_metrics/observers/page_load_metrics_observer_test_harness.h"
+#include "url/gurl.h"
+
+class GoogleCaptchaPageLoadMetricsObserverTest
+    : public page_load_metrics::PageLoadMetricsObserverTestHarness {};
+
+TEST_F(GoogleCaptchaPageLoadMetricsObserverTest, IsGoogleCaptcha) {
+  struct {
+    std::string url;
+    bool expected;
+  } test_cases[] = {
+      {"", false},
+      {"http://www.google.com/", false},
+      {"http://www.cnn.com/", false},
+      {"http://ipv4.google.com/", false},
+      {"https://ipv4.google.com/sorry/IndexRedirect?continue=http://a", true},
+      {"https://ipv6.google.com/sorry/IndexRedirect?continue=http://a", true},
+      {"https://ipv7.google.com/sorry/IndexRedirect?continue=http://a", false},
+  };
+  for (const auto& test : test_cases) {
+    EXPECT_EQ(test.expected,
+              google_captcha_observer::IsGoogleCaptcha(GURL(test.url)))
+        << "for URL: " << test.url;
+  }
+}
