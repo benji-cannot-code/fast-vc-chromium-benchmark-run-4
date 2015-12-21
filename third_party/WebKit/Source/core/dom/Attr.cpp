f53b30b2a0ea220efdd3e6975dee675ee7b73893
tkent <tkent@chromium.org>
2015-12-21 01:16:59 -0800

    Remove config.h inclusion from third_party/WebKit/Source/core/dom/.
    This CL has no behavior changes.
    
    BUG=570234
    TBR=yutak@chromium.org
    
    Review URL: https://codereview.chromium.org/1540853002
    
    Cr-Commit-Position: refs/heads/master@{#366358}

--- third_party/WebKit/Source/core/dom/Attr.cpp
+++ third_party/WebKit/Source/core/dom/Attr.cpp
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
  */
-#include "config.h"
+
 #include "core/dom/Attr.h"
 
 #include "bindings/core/v8/ExceptionState.h"
--- third_party/WebKit/Source/core/dom/SpaceSplitString.cpp
+++ third_party/WebKit/Source/core/dom/SpaceSplitString.cpp
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Boston, MA 02111-1307, USA.
  */
 
-#include "config.h"
 #include "core/dom/SpaceSplitString.h"
 
 #include "core/html/parser/HTMLParserIdioms.h"
