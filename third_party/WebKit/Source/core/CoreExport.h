f6b672801b28459df7b269379ed0099e0ca1b50c
tkent <tkent@chromium.org>
2015-12-20 22:51:24 -0800

    Remove config.h inclusion from third_party/WebKit/Source/core/animation/.
    This CL has no behavior changes.
    
    BUG=570234
    TBR=yutak@chromium.org
    
    Review URL: https://codereview.chromium.org/1536263003
    
    Cr-Commit-Position: refs/heads/master@{#366341}

--- third_party/WebKit/Source/core/CoreExport.h
+++ third_party/WebKit/Source/core/CoreExport.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CoreExport_h
 #define CoreExport_h
 
+#include "wtf/Compiler.h"
+
 #if !defined(LINK_CORE_MODULES_SEPARATELY)
 #define LINK_CORE_MODULES_SEPARATELY 0
 #endif
