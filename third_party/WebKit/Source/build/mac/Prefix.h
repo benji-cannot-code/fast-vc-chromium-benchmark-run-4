c55ffc0c77013566901a7f04a4cb0a58ab0b04b6
tkent <tkent@chromium.org>
2015-12-20 17:39:17 -0800

    Mac: Clean up third_party/WebKit/Source/build/mac/Prefix.h
    - Update a comment
    - Remove NULL definition
    - Remove <ciso646> inclusion
    - Remove a workaround for libc++.
    
    Review URL: https://codereview.chromium.org/1533263002
    
    Cr-Commit-Position: refs/heads/master@{#366317}

--- third_party/WebKit/Source/build/mac/Prefix.h
+++ third_party/WebKit/Source/build/mac/Prefix.h
@@ -19,20 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
-/* This prefix file is for use on Mac OS X only. This prefix file should contain only:
- *    1) files to precompile for faster builds
- *    2) in one case at least: OS-X-specific performance bug workarounds
- *    3) the special trick to catch us using new or delete without including "config.h"
- * The project should be able to build without this header, although we rarely test that.
+/* This prefix file is for use on Mac OS X only. This prefix file should contain
+ * only files to precompile for faster builds. The project should be able to
+ * build without this header, although we rarely test that.
  */
-
-/* Things that need to be defined globally should go into "config.h". */
-
-#ifdef __cplusplus
-#define NULL __null
-#else
-#define NULL ((void *)0)
-#endif
 
 #include <pthread.h>
 #include <sys/types.h>
@@ -49,35 +39,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unistd.h>
 
 #ifdef __cplusplus
-#include <ciso646>
-
-#if defined(_LIBCPP_VERSION)
-
-// Add work around for a bug in libc++ that caused standard heap
-// APIs to not compile <rdar://problem/10858112>.
-
-#include <type_traits>
-
-namespace blink {
-class TimerHeapReference;
-}
-
-_LIBCPP_BEGIN_NAMESPACE_STD
-
-inline _LIBCPP_INLINE_VISIBILITY
-const blink::TimerHeapReference& move(const blink::TimerHeapReference& t)
-{
-    return t;
-}
-
-_LIBCPP_END_NAMESPACE_STD
-
-#endif // defined(_LIBCPP_VERSION)
-
 #include <algorithm>
 #include <cstddef>
 #include <new>
-
 #endif // __cplusplus
 
 #include <sys/param.h>
