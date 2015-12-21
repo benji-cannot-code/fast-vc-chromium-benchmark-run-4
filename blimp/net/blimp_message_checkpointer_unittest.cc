47841eac66d27af420f3839486d2f25178b9a809
wez <wez@chromium.org>
2015-12-21 09:42:37 -0800

    Remove unused using declarations from unit-test.
    Review URL: https://codereview.chromium.org/1534333002
    
    Cr-Commit-Position: refs/heads/master@{#366411}

--- blimp/net/blimp_message_checkpointer_unittest.cc
+++ blimp/net/blimp_message_checkpointer_unittest.cc
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 using testing::_;
-using testing::InvokeArgument;
-using testing::Return;
 using testing::SaveArg;
 
 namespace blimp {
