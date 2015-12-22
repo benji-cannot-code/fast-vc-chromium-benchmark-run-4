736d7e232ce5bd7d52832f813ad215730302ffac
pedrosimonetti <pedrosimonetti@chromium.org>
2015-12-22 11:50:09 -0800

    [Contextual Search] Allow disabling remotely.
    Also, allow disabling just the resolution remotely.
    
    Finally, it simplifies the logic that detects whether
    the feature is enabled to make it less confusing. This
    included removing a check for local builds.
    
    For now on, if the feature is disabled remotely it
    will also disable for local builds. This is fine since
    there's still a way to force enable the feature via
    command line flags or about:flags.
    
    BUG=571527
    
    Review URL: https://codereview.chromium.org/1549463002
    
    Cr-Commit-Position: refs/heads/master@{#366634}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchPolicy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchPolicy.java
@@ -134,6 +134,10 @@ class ContextualSearchPolicy {
      * @return Whether the previous tap should resolve.
      */
     boolean shouldPreviousTapResolve(@Nullable URL url) {
+        if (!ContextualSearchFieldTrial.isSearchTermResolutionEnabled()) {
+            return false;
+        }
+
         if (isTapResolveBeyondTheLimit()) {
             return false;
         }
