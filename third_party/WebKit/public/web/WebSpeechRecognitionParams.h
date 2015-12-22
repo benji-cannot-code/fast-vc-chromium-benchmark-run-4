4f1872977a3234bc736c229aaa3765341ad6c30b
kirtia <kirtia@google.com>
2015-12-22 12:57:23 -0800

    Reverting serviceURI since it is not supported anymore.
    BUG=480516
    
    Review URL: https://codereview.chromium.org/1544683002
    
    Cr-Commit-Position: refs/heads/master@{#366645}

--- third_party/WebKit/public/web/WebSpeechRecognitionParams.h
+++ third_party/WebKit/public/web/WebSpeechRecognitionParams.h
@@ -39,10 +39,9 @@ class WebSpeechGrammar;
 
 class WebSpeechRecognitionParams {
 public:
-    WebSpeechRecognitionParams(const WebVector<WebSpeechGrammar>& grammars, const WebString& language, const WebString& serviceURI, bool continuous, bool interimResults, unsigned long maxAlternatives, const WebMediaStreamTrack& audioTrack, const WebSecurityOrigin& origin)
+    WebSpeechRecognitionParams(const WebVector<WebSpeechGrammar>& grammars, const WebString& language, bool continuous, bool interimResults, unsigned long maxAlternatives, const WebMediaStreamTrack& audioTrack, const WebSecurityOrigin& origin)
         : m_grammars(grammars)
         , m_language(language)
-        , m_serviceURI(serviceURI)
         , m_continuous(continuous)
         , m_interimResults(interimResults)
         , m_maxAlternatives(maxAlternatives)
@@ -53,7 +52,6 @@ public:
 
     const WebVector<WebSpeechGrammar>& grammars() const { return m_grammars; }
     const WebString& language() const { return m_language; }
-    const WebString& serviceURI() const { return m_serviceURI; }
     bool continuous() const { return m_continuous; }
     bool interimResults() const { return m_interimResults; }
     unsigned long maxAlternatives() const { return m_maxAlternatives; }
@@ -63,7 +61,6 @@ public:
 private:
     WebVector<WebSpeechGrammar> m_grammars;
     WebString m_language;
-    WebString m_serviceURI;
     bool m_continuous;
     bool m_interimResults;
     unsigned long m_maxAlternatives;
