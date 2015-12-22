16af7b14a904a9423de42bd3d967d7c62cd683be
kkhorimoto <kkhorimoto@chromium.org>
2015-12-22 12:18:38 -0800

    Fix content size bug for WKWebView.
    WKWebView was erroneously resetting the contentSize of its scroll view
    to an invalid state upon rotation.  This CL detects contentSize resets
    to invalid values (the width of the contentSize for a web view scroll
    view will only be less than the width of its bounds in zooming
    conditions).
    
    BUG=541563
    
    Review URL: https://codereview.chromium.org/1491853005
    
    Cr-Commit-Position: refs/heads/master@{#366636}

--- ios/web/public/web_state/crw_web_view_scroll_view_proxy.h
+++ ios/web/public/web_state/crw_web_view_scroll_view_proxy.h
@@ -79,6 +79,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         (CRWWebViewScrollViewProxy*)webViewScrollViewProxy;
 - (void)webViewScrollViewDidZoom:
         (CRWWebViewScrollViewProxy*)webViewScrollViewProxy;
+- (void)webViewScrollViewDidResetContentSize:
+    (CRWWebViewScrollViewProxy*)webViewScrollViewProxy;
 @end
 
 // A protocol to be implemented by objects to listen for changes to the
--- ios/web/web_state/crw_web_view_scroll_view_proxy.mm
+++ ios/web/web_state/crw_web_view_scroll_view_proxy.mm
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/foundation_util.h"
 #import "base/mac/scoped_nsobject.h"
 
-@implementation CRWWebViewScrollViewProxy {
+@interface CRWWebViewScrollViewProxy () {
   base::WeakNSObject<UIScrollView> _scrollView;
   base::scoped_nsobject<id> _observers;
   // When |_ignoreScroll| is set to YES, do not pass on -scrollViewDidScroll
@@ -21,6 +21,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   BOOL _ignoreScroll;
 }
 
+// Returns the key paths that need to be observed for UIScrollView.
++ (NSArray*)scrollViewObserverKeyPaths;
+
+// Adds and removes |self| as an observer for |scrollView| with key paths
+// returned by |+scrollViewObserverKeyPaths|.
+- (void)startObservingScrollView:(UIScrollView*)scrollView;
+- (void)stopObservingScrollView:(UIScrollView*)scrollView;
+
+@end
+
+@implementation CRWWebViewScrollViewProxy
+
 - (instancetype)init {
   self = [super init];
   if (self) {
@@ -29,6 +41,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         [[CRBProtocolObservers observersWithProtocol:protocol] retain]);
   }
   return self;
+}
+
+- (void)dealloc {
+  [self stopObservingScrollView:_scrollView];
+  [super dealloc];
 }
 
 - (void)addGestureRecognizer:(UIGestureRecognizer*)gestureRecognizer {
@@ -51,10 +68,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (_scrollView == scrollView)
     return;
   [_scrollView setDelegate:nil];
-  if (scrollView) {
-    DCHECK(!scrollView.delegate);
-    scrollView.delegate = self;
-  }
+  [self stopObservingScrollView:_scrollView];
+  DCHECK(!scrollView.delegate);
+  scrollView.delegate = self;
+  [self startObservingScrollView:scrollView];
   _scrollView.reset(scrollView);
   [_observers webViewScrollViewProxyDidSetScrollView:self];
 }
@@ -222,6 +239,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)scrollViewDidZoom:(UIScrollView*)scrollView {
   DCHECK_EQ(_scrollView, scrollView);
   [_observers webViewScrollViewDidZoom:self];
+}
+
+#pragma mark -
+
++ (NSArray*)scrollViewObserverKeyPaths {
+  return @[ @"contentSize" ];
+}
+
+- (void)startObservingScrollView:(UIScrollView*)scrollView {
+  for (NSString* keyPath in [[self class] scrollViewObserverKeyPaths])
+    [scrollView addObserver:self forKeyPath:keyPath options:0 context:nil];
+}
+
+- (void)stopObservingScrollView:(UIScrollView*)scrollView {
+  for (NSString* keyPath in [[self class] scrollViewObserverKeyPaths])
+    [scrollView removeObserver:self forKeyPath:keyPath];
+}
+
+- (void)observeValueForKeyPath:(NSString*)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary*)change
+                       context:(void*)context {
+  DCHECK_EQ(object, _scrollView.get());
+  if ([keyPath isEqualToString:@"contentSize"])
+    [_observers webViewScrollViewDidResetContentSize:self];
 }
 
 @end
