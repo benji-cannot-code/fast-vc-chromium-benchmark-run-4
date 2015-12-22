edc739766767f6ca19ea6372c0dcbe0920d149df
avayvod <avayvod@chromium.org>
2015-12-22 12:44:28 -0800

    [Cast,Android] Remove the origin and tab id from CastSession
    BUG=None
    
    CastSession doesn't have to keep origin and tab id as it's not used after construction.
    With the exception of hiding the notification but it's not required there.
    
    Review URL: https://codereview.chromium.org/1549563002
    
    Cr-Commit-Position: refs/heads/master@{#366641}

--- chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/CastSession.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/CastSession.java
@@ -151,8 +151,6 @@ public class CastSession implements MediaNotificationListener {
         }
     }
 
-    private final String mOrigin;
-    private final int mTabId;
     private final CastMessagingChannel mMessageChannel;
     private final CastMediaRouteProvider mRouteProvider;
     private final CastDevice mCastDevice;
@@ -196,8 +194,6 @@ public class CastSession implements MediaNotificationListener {
             CastMediaRouteProvider routeProvider) {
         mApiClient = apiClient;
         mSessionId = sessionId;
-        mOrigin = origin;
-        mTabId = tabId;
         mSource = source;
         mRouteProvider = routeProvider;
         mApplicationMetadata = metadata;
@@ -242,6 +238,8 @@ public class CastSession implements MediaNotificationListener {
                 .setTitle(mCastDevice.getFriendlyName())
                 .setPaused(false)
                 .setOrigin(origin)
+                // TODO(avayvod): the same session might have more than one tab id. Should we track
+                // the last foreground alive tab and update the notification with it?
                 .setTabId(tabId)
                 // TODO(avayvod): pass true here if initiated from the incognito mode.
                 // MediaRouter is disabled for Incognito mode for now, see https://crbug.com/525215
@@ -307,8 +305,7 @@ public class CastSession implements MediaNotificationListener {
                         mRouteProvider.onSessionClosed();
                         mStoppingApplication = false;
 
-                        MediaNotificationManager.hide(
-                                mTabId, R.id.presentation_notification);
+                        MediaNotificationManager.clear(R.id.presentation_notification);
                     }
                 });
     }
@@ -326,14 +323,6 @@ public class CastSession implements MediaNotificationListener {
 
     public String getSinkId() {
         return mCastDevice.getDeviceId();
-    }
-
-    public String getOrigin() {
-        return mOrigin;
-    }
-
-    public int getTabId() {
-        return mTabId;
     }
 
     /////////////////////////////////////////////////////////////////////////////////////////////
