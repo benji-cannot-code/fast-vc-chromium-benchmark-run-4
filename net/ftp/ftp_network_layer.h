d60b1e3c35d3b2e2a362842bc1876f4ee8c6a283
tfh <tfh@skip.org>
2015-12-24 03:49:35 -0800

    Convert raw FtpTransaction pointers to scoped_ptr<FtpTransaction>.
    BUG=479898
    
    Review URL: https://codereview.chromium.org/1549723003
    
    Cr-Commit-Position: refs/heads/master@{#366819}

--- net/ftp/ftp_network_layer.h
+++ net/ftp/ftp_network_layer.h
@@ -23,7 +23,7 @@ class NET_EXPORT FtpNetworkLayer : public FtpTransactionFactory {
   ~FtpNetworkLayer() override;
 
   // FtpTransactionFactory methods:
-  FtpTransaction* CreateTransaction() override;
+  scoped_ptr<FtpTransaction> CreateTransaction() override;
   void Suspend(bool suspend) override;
 
  private:
--- net/ftp/ftp_transaction_factory.h
+++ net/ftp/ftp_transaction_factory.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_FTP_FTP_TRANSACTION_FACTORY_H_
 #define NET_FTP_FTP_TRANSACTION_FACTORY_H_
 
+#include "base/memory/scoped_ptr.h"
 #include "net/base/net_export.h"
 
 namespace net {
@@ -18,7 +19,7 @@ class NET_EXPORT FtpTransactionFactory {
   virtual ~FtpTransactionFactory() {}
 
   // Creates a FtpTransaction object.
-  virtual FtpTransaction* CreateTransaction() = 0;
+  virtual scoped_ptr<FtpTransaction> CreateTransaction() = 0;
 
   // Suspends the creation of new transactions. If |suspend| is false, creation
   // of new transactions is resumed.
