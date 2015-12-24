4e175d52d72572dd068dd2f034652ef1f61de6e9
xlai <xlai@chromium.org>
2015-12-23 17:59:46 -0800

    Revert of Fixing Oilpan error because of missing macro. (patchset #5 id:80001 of https://codereview.chromium.org/1545033002/ )
    Reason for revert:
    Patch 1545033002 and Patch 1529803005 are both causing a heap-use-after-free error on Oilpan ASAN bot. Therefore reverting them until the solution is found.
    
    Original issue's description:
    > Fixing Oilpan error because of missing macro.
    >
    > NOTRY=true
    > BUG=None
    >
    > Committed: https://crrev.com/f8a41cca97153432bb2a27d80531f0d8ec80975a
    > Cr-Commit-Position: refs/heads/master@{#366772}
    
    TBR=junov@chromium.org,sigbjornf@opera.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=None
    
    Review URL: https://codereview.chromium.org/1548013002
    
    Cr-Commit-Position: refs/heads/master@{#366788}

--- third_party/WebKit/Source/core/html/canvas/CanvasAsyncBlobCreator.cpp
+++ third_party/WebKit/Source/core/html/canvas/CanvasAsyncBlobCreator.cpp
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/Task.h"
 #include "platform/ThreadSafeFunctional.h"
 #include "platform/graphics/ImageBuffer.h"
-#include "platform/heap/Handle.h"
 #include "platform/image-encoders/skia/PNGImageEncoder.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebScheduler.h"
@@ -33,29 +32,26 @@ bool isDeadlineNearOrPassed(double deadlineSeconds)
 
 } // anonymous namespace
 
-class CanvasAsyncBlobCreator::ContextObserver final : public NoBaseWillBeGarbageCollected<CanvasAsyncBlobCreator::ContextObserver>, public ContextLifecycleObserver {
-    WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(CanvasAsyncBlobCreator::ContextObserver);
-public:
-    ContextObserver(ExecutionContext* executionContext, CanvasAsyncBlobCreator* asyncBlobCreator)
-        : ContextLifecycleObserver(executionContext)
-        , m_asyncBlobCreator(asyncBlobCreator)
-    {
-    }
+class CanvasAsyncBlobCreator::ContextObserver final : public ContextLifecycleObserver {
+    public:
+        ContextObserver(ExecutionContext* executionContext, CanvasAsyncBlobCreator* asyncBlobCreator)
+            : ContextLifecycleObserver(executionContext)
+            , m_asyncBlobCreator(asyncBlobCreator)
+        {
+        }
 
-    DECLARE_VIRTUAL_TRACE();
+        void contextDestroyed() override
+        {
+            ContextLifecycleObserver::contextDestroyed();
+            ASSERT(m_asyncBlobCreator);
+            m_asyncBlobCreator->m_cancelled = true;
+            // After sending cancel signal to asyncBlobCreator, the observer has
+            // done its job and thus deref asyncBlobCreator for proper destruction
+            m_asyncBlobCreator = nullptr;
+        }
 
-    void contextDestroyed() override
-    {
-        ContextLifecycleObserver::contextDestroyed();
-        ASSERT(m_asyncBlobCreator);
-        m_asyncBlobCreator->m_cancelled = true;
-        // After sending cancel signal to asyncBlobCreator, the observer has
-        // done its job and thus deref asyncBlobCreator for proper destruction
-        m_asyncBlobCreator = nullptr;
-    }
-
-private:
-    CanvasAsyncBlobCreator* m_asyncBlobCreator;
+    private:
+        CanvasAsyncBlobCreator* m_asyncBlobCreator;
 };
 
 PassRefPtr<CanvasAsyncBlobCreator> CanvasAsyncBlobCreator::create(PassRefPtr<DOMUint8ClampedArray> unpremultipliedRGBAImageData, const String& mimeType, const IntSize& size, FileCallback* callback, ExecutionContext* executionContext)
@@ -79,11 +75,6 @@ CanvasAsyncBlobCreator::CanvasAsyncBlobCreator(PassRefPtr<DOMUint8ClampedArray> 
 }
 
 CanvasAsyncBlobCreator::~CanvasAsyncBlobCreator() { }
-
-DEFINE_TRACE(CanvasAsyncBlobCreator::ContextObserver)
-{
-    ContextLifecycleObserver::trace(visitor);
-}
 
 void CanvasAsyncBlobCreator::scheduleAsyncBlobCreation(bool canUseIdlePeriodScheduling, double quality)
 {
