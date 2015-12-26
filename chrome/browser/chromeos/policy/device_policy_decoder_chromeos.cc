b69d29a1f8957e40a1199ae83f182f670ccc3329
avi <avi@chromium.org>
2015-12-25 17:34:23 -0800

    Undo int64 -> int64_t.
    This is wrong.
    
    BUG=138542
    TBR=stevenjb@chromium.org
    
    Review URL: https://codereview.chromium.org/1548143002
    
    Cr-Commit-Position: refs/heads/master@{#366881}

--- chrome/browser/chromeos/policy/device_policy_decoder_chromeos.cc
+++ chrome/browser/chromeos/policy/device_policy_decoder_chromeos.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/policy/device_policy_decoder_chromeos.h"
 
-#include <stdint.h>
-
 #include <limits>
 #include <string>
 
@@ -40,7 +38,7 @@ namespace {
 
 // Decodes a protobuf integer to an IntegerValue. Returns NULL in case the input
 // value is out of bounds.
-scoped_ptr<base::Value> DecodeIntegerValue(google::protobuf::int64_t value) {
+scoped_ptr<base::Value> DecodeIntegerValue(google::protobuf::int64 value) {
   if (value < std::numeric_limits<int>::min() ||
       value > std::numeric_limits<int>::max()) {
     LOG(WARNING) << "Integer value " << value
