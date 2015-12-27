8edd867883edc9cd5c91acd57422ab9f227a94a9
tkent <tkent@chromium.org>
2015-12-27 06:41:22 -0800

    Fix a focus issue after closing PagePopups by opening the hotdog menu.
    This CL affects any PagePopups such as SELECT popup and calendar popup.
    
    If the hotdog menu is opened while a PagePopup is shown,
    WebPagePopupImpl::close() is called. In this case, we didn't clear
    WebViewImpl::m_pagePopup, and the WebView didn't handle any input
    events.  We should clear WebViewImmpl::m_pagePopup.
    
    Also, WebViewImpl::setFocus(false) is called on Windows in this
    case. It stopped rendering focus rings.  This CL avoids it.
    
    BUG=568101
    TEST=manual; See crbug.com/568101
    
    Review URL: https://codereview.chromium.org/1525943002
    
    Cr-Commit-Position: refs/heads/master@{#366929}

--- third_party/WebKit/Source/web/WebViewImpl.h
+++ third_party/WebKit/Source/web/WebViewImpl.h
@@ -427,6 +427,7 @@ public:
 
     PagePopup* openPagePopup(PagePopupClient*);
     void closePagePopup(PagePopup*);
+    void cleanupPagePopup();
     LocalDOMWindow* pagePopupWindow() const;
 
     // Returns the input event we're currently processing. This is used in some
