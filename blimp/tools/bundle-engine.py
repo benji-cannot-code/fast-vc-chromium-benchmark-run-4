7ad4fc019e43643bde9c321075d3a4d9a4156805
maniscalco <maniscalco@chromium.org>
2015-12-28 10:28:33 -0800

    Make blimp_engine_bundle action silent when successful.
    Don't write to stdout. Only write to stderr when there's an error.
    
    BUG=572396
    
    Review URL: https://codereview.chromium.org/1550793002
    
    Cr-Commit-Position: refs/heads/master@{#366991}

--- blimp/tools/bundle-engine.py
+++ blimp/tools/bundle-engine.py
@@ -15,6 +15,7 @@ import argparse
 import errno
 import os
 import subprocess
+import sys
 import tarfile
 
 def ReadDependencies(manifest):
@@ -61,9 +62,8 @@ def main():
         tarball.add(dep)
       except OSError as e:
         if e.errno == errno.ENOENT:
-          print dep + " not found (did you build the engine?)"
+          print >> sys.stderr, dep + " not found (did you build the engine?)"
           exit(1)
-  print 'Created ' + args.output
 
 if __name__ == "__main__":
   main()
