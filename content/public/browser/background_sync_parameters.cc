657a2866169ed5d844b7b8b2cfe1686cae2a4b90
jkarlin <jkarlin@chromium.org>
2015-12-28 07:00:53 -0800

    [BackgroundSync] Change the default number of sync attempts to 3
    Review URL: https://codereview.chromium.org/1539913002
    
    Cr-Commit-Position: refs/heads/master@{#366973}

--- content/public/browser/background_sync_parameters.cc
+++ content/public/browser/background_sync_parameters.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 namespace {
-const int kMaxSyncAttempts = 5;
+const int kMaxSyncAttempts = 3;
 const int kInitialRetryDelayMins = 5;
 const int kRetryDelayFactor = 3;
 const int64_t kMinSyncRecoveryTimeMs = 1000 * 60 * 6;  // 6 minutes
