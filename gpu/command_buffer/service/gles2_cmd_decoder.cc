1c5d0f5a52c73f6d4bb60802fa322698f6be2cad
tzik <tzik@chromium.org>
2015-12-27 18:48:11 -0800

    Add a missing switch-case break in GLES2Decoder
    BUG=
    
    Review URL: https://codereview.chromium.org/1544343002
    
    Cr-Commit-Position: refs/heads/master@{#366957}

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -6454,6 +6454,7 @@ void GLES2DecoderImpl::DoGetRenderbufferParameteriv(
         glGetRenderbufferParameterivEXT(target, GL_RENDERBUFFER_SAMPLES_EXT,
             params);
       }
+      break;
     default:
       glGetRenderbufferParameterivEXT(target, pname, params);
       break;
