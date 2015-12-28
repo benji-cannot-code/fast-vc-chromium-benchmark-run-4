b3106d9df6121f79b3a85da5c76278ed1f911976
dcheng <dcheng@chromium.org>
2015-12-28 08:06:42 -0800

    Convert Pass()→std::move() in generated devtools protocol handler.
    ❆(੭ु ◜◡‾)੭ु⁾☃❆
    
    BUG=557422
    R=avi@chromium.org
    TBR=dgozman@chromium.org
    
    Review URL: https://codereview.chromium.org/1549363002
    
    Cr-Commit-Position: refs/heads/master@{#366979}

--- content/browser/devtools/protocol/devtools_protocol_handler_generator.py
+++ content/browser/devtools/protocol/devtools_protocol_handler_generator.py
@@ -30,6 +30,8 @@ template_h = string.Template(header + """\
 #ifndef CONTENT_BROWSER_DEVTOOLS_PROTOCOL_DEVTOOLS_PROTOCOL_DISPATCHER_H_
 #define CONTENT_BROWSER_DEVTOOLS_PROTOCOL_DEVTOOLS_PROTOCOL_DISPATCHER_H_
 
+#include <utility>
+
 #include "content/browser/devtools/protocol/devtools_protocol_client.h"
 
 namespace content {
@@ -335,7 +337,7 @@ ${prep}\
     return false;
   scoped_ptr<base::DictionaryValue> result(new base::DictionaryValue());
 ${wrap}\
-  client_.SendSuccess(command_id, result.Pass());
+  client_.SendSuccess(command_id, std::move(result));
   return true;
 }
 """)
@@ -422,7 +424,7 @@ tmpl_event_impl = string.Template("""\
 void Client::${Command}(
     scoped_refptr<${Command}Params> params) {
   SendNotification("${Domain}.${command}",
-                   params->ToValue().Pass());
+                   params->ToValue());
 }
 """)
 
@@ -430,7 +432,7 @@ tmpl_response_impl = string.Template("""\
 void Client::Send${Command}Response(
     DevToolsCommandId command_id,
     scoped_refptr<${Command}Response> params) {
-  SendSuccess(command_id, params->ToValue().Pass());
+  SendSuccess(command_id, params->ToValue());
 }
 """)
 
