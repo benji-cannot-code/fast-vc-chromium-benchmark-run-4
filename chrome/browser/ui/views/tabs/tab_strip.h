933d016810a60899eb5a6898c1f3879278d56474
pkasting <pkasting@chromium.org>
2015-12-28 02:24:31 -0800

    Compute the correct height above the tabstrip when dragging tabs.
    The code wanted the difference between the height in a restored windows vs. a
    maximized window, but instead used the height difference between the top of the
    tabstrip and the top of the new tab button.
    
    The effect of this is that when dragging a tab out of a maximized tabstrip, the
    new window is created about 5 px higher (at 100% scale) than before.
    
    This allows moving a public constant in TabStrip to become file-scope to the .cc
    file.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1545863002
    
    Cr-Commit-Position: refs/heads/master@{#366964}

--- chrome/browser/ui/views/tabs/tab_strip.h
+++ chrome/browser/ui/views/tabs/tab_strip.h
@@ -59,10 +59,6 @@ class TabStrip : public views::View,
                  public views::ViewTargeterDelegate,
                  public TabController {
  public:
-  // The vertical offset of the tab strip button. This offset applies only to
-  // restored windows.
-  static const int kNewTabButtonVerticalOffset;
-
   explicit TabStrip(TabStripController* controller);
   ~TabStrip() override;
 
