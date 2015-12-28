4a3dd5cfef96192a8c5f8ebf705ffbdfec60888e
sorin <sorin@chromium.org>
2015-12-28 11:22:36 -0800

    Remove update_client dependency on base/ui
    BUG=571104
    
    Review URL: https://codereview.chromium.org/1536083003
    
    Cr-Commit-Position: refs/heads/master@{#366998}

--- components/update_client/background_downloader_win.cc
+++ components/update_client/background_downloader_win.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/thread_task_runner_handle.h"
 #include "base/win/scoped_co_mem.h"
 #include "components/update_client/utils.h"
-#include "ui/base/win/atl_module.h"
 #include "url/gurl.h"
 
 using base::win::ScopedCoMem;
