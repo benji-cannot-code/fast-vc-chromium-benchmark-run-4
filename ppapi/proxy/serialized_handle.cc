46ebb6e7b6a02c1366d244a9e439da209d1b4909
tzik <tzik@chromium.org>
2015-12-28 14:11:26 -0800

    Add a missing switch-case break in PPAPI SerializedHandle
    BUG=
    
    Review URL: https://codereview.chromium.org/1545343002
    
    Cr-Commit-Position: refs/heads/master@{#367015}

--- ppapi/proxy/serialized_handle.cc
+++ ppapi/proxy/serialized_handle.cc
@@ -128,6 +128,7 @@ bool SerializedHandle::ReadHeader(base::PickleIterator* iter, Header* hdr) {
       hdr->open_flags = open_flags;
       hdr->file_io = file_io;
       valid_type = true;
+      break;
     }
     case SOCKET:
     case INVALID:
