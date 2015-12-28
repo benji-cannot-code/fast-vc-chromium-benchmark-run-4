f90516d335f238c0db67c2e5475ba6d8c9cfad5d
dcheng <dcheng@chromium.org>
2015-12-28 08:06:27 -0800

    Convert Pass()→std::move() in generated JS schema bindings.
    ❆(੭ु ◜◡‾)੭ु⁾☃❆
    
    BUG=557422
    R=avi@chromium.org
    TBR=rdevlin.cronin@chromium.org
    
    Review URL: https://codereview.chromium.org/1549353002
    
    Cr-Commit-Position: refs/heads/master@{#366978}

--- tools/json_schema_compiler/cc_generator.py
+++ tools/json_schema_compiler/cc_generator.py
@@ -49,6 +49,7 @@ class _Generator(object):
       .Append('#include "%s/%s.h"' %
               (self._namespace.source_file_dir, self._namespace.short_filename))
       .Append('#include <set>')
+      .Append('#include <utility>')
       .Cblock(self._type_helper.GenerateIncludes(include_soft=True))
       .Append()
       .Append('using base::UTF8ToUTF16;')
@@ -332,7 +333,7 @@ class _Generator(object):
       .Append('  if (!Populate(%s))' % self._GenerateArgs(
           ('value', 'out.get()')))
       .Append('    return scoped_ptr<%s>();' % classname)
-      .Append('  return out.Pass();')
+      .Append('  return out;')
       .Append('}')
     )
     return c
@@ -408,7 +409,7 @@ class _Generator(object):
         )
 
     return (c.Append()
-             .Append('return value.Pass();')
+             .Append('return value;')
            .Eblock('}'))
 
   def _GenerateChoiceTypeToValue(self, cpp_namespace, type_):
@@ -434,7 +435,7 @@ class _Generator(object):
       )
     (c.Append('DCHECK(result) << "Must set at least one choice for %s";' %
                   type_.unix_name)
-      .Append('return result.Pass();')
+      .Append('return result;')
       .Eblock('}')
     )
     return c
@@ -646,7 +647,7 @@ class _Generator(object):
           .Eblock('}'))
       c.Substitute({'value_var': value_var, 'i': i, 'key': param.name})
     (c.Append()
-      .Append('return params.Pass();')
+      .Append('return params;')
       .Eblock('}')
       .Append()
     )
@@ -735,7 +736,7 @@ class _Generator(object):
         )
         (c.Append('}')
           .Append('else')
-          .Append('  %(dst_var)s = temp.Pass();')
+         .Append('  %(dst_var)s = std::move(temp);')
           .Eblock('}')
         )
       else:
@@ -796,7 +797,7 @@ class _Generator(object):
           .Append('if (!%%(cpp_type)s::Populate(%s))' % self._GenerateArgs(
             ('*%(src_var)s', 'temp.get()')))
           .Append('  return %(failure_value)s;')
-          .Append('%(dst_var)s = temp.Pass();')
+          .Append('%(dst_var)s = std::move(temp);')
         )
       else:
         (c.Append('if (!%%(cpp_type)s::Populate(%s))' % self._GenerateArgs(
@@ -1014,7 +1015,7 @@ class _Generator(object):
                                          param.name,
                                          param.type_,
                                          param.unix_name))
-    c.Append('return create_results.Pass();')
+    c.Append('return create_results;')
     c.Eblock('}')
     c.Substitute({
         'function_scope': ('%s::' % function_scope) if function_scope else '',
