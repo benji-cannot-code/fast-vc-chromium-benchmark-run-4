3737c8122baa91aa39f98623512d4a2d1e7156c6
boliu <boliu@chromium.org>
2015-12-28 17:40:19 -0800

    IPC-based sync compositor software draw optimization
    Instead of allocating and mapping a new chunk of shared memory segment
    in each draw synchronously, re-use the same segment if the sizes match
    exactly.
    
    Break software draws into 3 IPCs:
    SetSharedMemory: synchronous, pass the shared memory handle to be used
                     for subsequent software draws to renderer
    DemandDrawSw: synchronous, the draw itself
    ZeroSharedMemory: asynchronous, zeros shared memory on renderer side,
                      sent after each DemandDrawSw after browser is done
                      reading from the shared memory.
    
    ZeroSharedMemory is asynchronous so zero-ing the memory can be done in
    parallel. Note messages on renderer are ordered, so ZeroSharedMemory
    is guaranteed to be handled before the next DemandDrawSw.
    
    BUG=545611
    
    Review URL: https://codereview.chromium.org/1541203003
    
    Cr-Commit-Position: refs/heads/master@{#367036}

--- content/common/android/sync_compositor_messages.cc
+++ content/common/android/sync_compositor_messages.cc
@@ -30,6 +30,9 @@ SyncCompositorDemandDrawHwParams::SyncCompositorDemandDrawHwParams(
 
 SyncCompositorDemandDrawHwParams::~SyncCompositorDemandDrawHwParams() {}
 
+SyncCompositorSetSharedMemoryParams::SyncCompositorSetSharedMemoryParams()
+    : buffer_size(0u) {}
+
 SyncCompositorDemandDrawSwParams::SyncCompositorDemandDrawSwParams() {}
 
 SyncCompositorDemandDrawSwParams::~SyncCompositorDemandDrawSwParams() {}
