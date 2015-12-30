aa55b9998f46c26fd373c1598835f2ce148cd548
nednguyen <nednguyen@google.com>
2015-12-30 13:37:32 -0800

    [Telemetry] Disable tracing related tests on win
    BUG=570955
    TBR=sullivan@chromium.org
    
    Review URL: https://codereview.chromium.org/1556583003
    
    Cr-Commit-Position: refs/heads/master@{#367187}

--- tools/telemetry/telemetry/internal/backends/chrome_inspector/tracing_backend_unittest.py
+++ tools/telemetry/telemetry/internal/backends/chrome_inspector/tracing_backend_unittest.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import time
 import unittest
 
+from telemetry import decorators
 from telemetry.internal.backends.chrome_inspector import tracing_backend
 from telemetry.internal.backends.chrome_inspector.tracing_backend import _DevToolsStreamReader
 from telemetry.testing import fakes
@@ -40,6 +41,7 @@ class TracingBackendTest(tab_test_case.TabTestCase):
     if not self._browser.supports_memory_dumping:
       self.skipTest('Browser does not support memory dumping, skipping test.')
 
+  @decorators.Disabled('win')  # crbug.com/570955
   def testDumpMemorySuccess(self):
     # Check that dumping memory before tracing starts raises an exception.
     self.assertRaises(Exception, self._browser.DumpMemory)
@@ -74,6 +76,7 @@ class TracingBackendTest(tab_test_case.TabTestCase):
     actual_dump_ids = [d.dump_id for d in model.IterGlobalMemoryDumps()]
     self.assertEqual(actual_dump_ids, expected_dump_ids)
 
+  @decorators.Disabled('win')  # crbug.com/570955
   def testDumpMemoryFailure(self):
     # Check that dumping memory before tracing starts raises an exception.
     self.assertRaises(Exception, self._browser.DumpMemory)
