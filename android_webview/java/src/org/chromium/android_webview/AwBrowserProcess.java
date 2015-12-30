0a52d2e4a1bbe596110223a304ac498a5a57562b
mnaganov <mnaganov@chromium.org>
2015-12-29 16:37:22 -0800

    [Android WebView] Forbid launching of two browser processes in the app
    Use FileLock in the data directory to enforce exclusive access to it.
    
    Attempting to share the data directory between two browser processes
    will most likely lead to data corruption.
    
    BUG=558377
    
    Review URL: https://codereview.chromium.org/1555513003
    
    Cr-Commit-Position: refs/heads/master@{#367120}

--- android_webview/java/src/org/chromium/android_webview/AwBrowserProcess.java
+++ android_webview/java/src/org/chromium/android_webview/AwBrowserProcess.java
@@ -9,6 +9,7 @@ import android.content.Context;
 
 import org.chromium.android_webview.policy.AwPolicyProvider;
 import org.chromium.base.CommandLine;
+import org.chromium.base.Log;
 import org.chromium.base.PathUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.library_loader.LibraryLoader;
@@ -17,6 +18,11 @@ import org.chromium.base.library_loader.ProcessInitException;
 import org.chromium.content.browser.BrowserStartupController;
 import org.chromium.policy.CombinedPolicyProvider;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileLock;
+
 /**
  * Wrapper for the steps needed to initialize the java and native sides of webview chromium.
  */
@@ -24,6 +30,8 @@ public abstract class AwBrowserProcess {
     public static final String PRIVATE_DATA_DIRECTORY_SUFFIX = "webview";
 
     private static final String TAG = "AwBrowserProcess";
+    private static final String EXCLUSIVE_LOCK_FILE = "webview_data.lock";
+    private static FileLock sExclusiveFileLock;
 
     /**
      * Loads the native library, and performs basic static construction of objects needed
@@ -51,6 +59,7 @@ public abstract class AwBrowserProcess {
      * @param context The Android application context
      */
     public static void start(final Context context) {
+        tryObtainingDataDirLockOrDie(context);
         // We must post to the UI thread to cover the case that the user
         // has invoked Chromium startup by using the (thread-safe)
         // CookieManager rather than creating a WebView.
@@ -71,5 +80,24 @@ public abstract class AwBrowserProcess {
                 }
             }
         });
+    }
+
+    private static void tryObtainingDataDirLockOrDie(Context context) {
+        String dataPath = PathUtils.getDataDirectory(context);
+        File lockFile = new File(dataPath, EXCLUSIVE_LOCK_FILE);
+        boolean success = false;
+        try {
+            // Note that the file is not closed intentionally.
+            RandomAccessFile file = new RandomAccessFile(lockFile, "rw");
+            sExclusiveFileLock = file.getChannel().tryLock();
+            success = sExclusiveFileLock != null;
+        } catch (IOException e) {
+            Log.w(TAG, "Failed to create lock file " + lockFile, e);
+        }
+        if (!success) {
+            throw new RuntimeException(
+                    "Could not obtain an exclusive lock on the data dir. The app may have "
+                    + "another WebView opened in a separate process");
+        }
     }
 }
