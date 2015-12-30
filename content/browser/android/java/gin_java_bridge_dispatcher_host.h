c98599b05d12f5a40230f2c75d143537ed137a16
pritam.nikam <pritam.nikam@samsung.com>
2015-12-30 10:36:19 -0800

    [Android] Fix GinJavaBridgeMessageFilter registration issue in Java Bridge.
    With current implementation for Java Bridge, there are 2 user scenarios related
    to renderer freeze caused as message filter registration not working as
    intended, namely:
    
    A. On web page opened in tab Render process getting exited silently (or manually
    killed from adb console) and user opt to reload on same tab so that new sandbox
    renderer associates with the opened tab.
    
    B. On web page opened in tab user opens a "chrome://" schema page and navigates
    back (this triggers creation of new sandbox renderer for navigation to
    "chrome://" and creation of another renderer on navigating back).
    
    This patch addresses both these problems by overriding the RenderProcessGone()
    and RenderViewHostChanged() WebContentsObserver interfaces respectively, and
    initiates the re-registration and filter object creation if required.
    
    BUG=572053
    
    Review URL: https://codereview.chromium.org/1547043002
    
    Cr-Commit-Position: refs/heads/master@{#367165}

--- content/browser/android/java/gin_java_bridge_dispatcher_host.h
+++ content/browser/android/java/gin_java_bridge_dispatcher_host.h
@@ -51,6 +51,9 @@ class GinJavaBridgeDispatcherHost
   void RenderFrameCreated(RenderFrameHost* render_frame_host) override;
   void DocumentAvailableInMainFrame() override;
   void WebContentsDestroyed() override;
+  void RenderProcessGone(base::TerminationStatus status) override;
+  void RenderViewHostChanged(RenderViewHost* old_host,
+                             RenderViewHost* new_host) override;
 
   // GinJavaMethodInvocationHelper::DispatcherDelegate
   JavaObjectWeakGlobalRef GetObjectWeakRef(
