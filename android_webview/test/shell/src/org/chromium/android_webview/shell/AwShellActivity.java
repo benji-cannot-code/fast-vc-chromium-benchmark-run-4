1e23220a31c501a9c0c00de87071c18f41b8fe88
mnaganov <mnaganov@chromium.org>
2015-12-30 09:14:41 -0800

    [Android] Call DevToolsServer.destroy where needed
    [Aw]DevToolsServer is initialized in AwShellActivity, ChromeApplication,
    and WebViewChromiumFactoryProvider. Make sure it is destroyed in the first
    two instances. WebView lacks shutdown path, thus we can't do much there.
    
    BUG=368171
    
    Review URL: https://codereview.chromium.org/1559433002
    
    Cr-Commit-Position: refs/heads/master@{#367159}

--- android_webview/test/shell/src/org/chromium/android_webview/shell/AwShellActivity.java
+++ android_webview/test/shell/src/org/chromium/android_webview/shell/AwShellActivity.java
@@ -120,6 +120,15 @@ public class AwShellActivity extends Activity {
         }
     }
 
+    @Override
+    public void onDestroy() {
+        if (mDevToolsServer != null) {
+            mDevToolsServer.destroy();
+            mDevToolsServer = null;
+        }
+        super.onDestroy();
+    }
+
     private AwTestContainerView createAwTestContainerView() {
         AwBrowserProcess.start(this);
         AwTestContainerView testContainerView = new AwTestContainerView(this, true);
