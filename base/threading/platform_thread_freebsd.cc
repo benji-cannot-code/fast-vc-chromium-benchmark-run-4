2b95d840dc5c3300c4eb3dd5061c7af5d99cb77c
l2d4y3 <l2d4y3@gmail.com>
2015-12-29 21:06:28 -0800

    Fix platform_thread_freebsd.cc
    R=thakis@chromium.org, thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/1556623002
    
    Cr-Commit-Position: refs/heads/master@{#367140}

--- base/threading/platform_thread_freebsd.cc
+++ base/threading/platform_thread_freebsd.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/lazy_instance.h"
 #include "base/logging.h"
+#include "base/threading/platform_thread_internal_posix.h"
 #include "base/threading/thread_id_name_manager.h"
 #include "base/tracked_objects.h"
 #include "build/build_config.h"
