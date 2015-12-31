e9f2617e8323b6dc0df09ba7837ac6edd5d71924
dmazzoni <dmazzoni@chromium.org>
2015-12-30 16:39:22 -0800

    Fix crash when cached parent of AXLayoutObject is stale.
    BUG=537412
    
    Review URL: https://codereview.chromium.org/1550113003
    
    Cr-Commit-Position: refs/heads/master@{#367214}

--- third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
@@ -1640,8 +1640,10 @@ void AXLayoutObject::addChildren()
     computeAriaOwnsChildren(ownedChildren);
 
     for (AXObject* obj = rawFirstChild(); obj; obj = obj->rawNextSibling()) {
-        if (!axObjectCache().isAriaOwned(obj))
+        if (!axObjectCache().isAriaOwned(obj)) {
+            obj->setParent(this);
             addChild(obj);
+        }
     }
 
     addHiddenChildren();
