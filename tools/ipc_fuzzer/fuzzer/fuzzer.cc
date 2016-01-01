67b5ba5fd6dffb964b44d3a8bf1a7e001c42365a
dcheng <dcheng@chromium.org>
2016-01-01 15:13:22 -0800

    Build fix for Pass()→std::move() conversion in //tools/ipc_fuzzer
    BUG=557422,573594
    TBR=inferno@chromium.org
    
    Review URL: https://codereview.chromium.org/1553953002
    
    Cr-Commit-Position: refs/heads/master@{#367268}

--- tools/ipc_fuzzer/fuzzer/fuzzer.cc
+++ tools/ipc_fuzzer/fuzzer/fuzzer.cc
@@ -776,7 +776,7 @@ struct FuzzTraits<cc::RenderPassList> {
       scoped_ptr<cc::RenderPass> render_pass = cc::RenderPass::Create();
       if (!FuzzParam(render_pass.get(), fuzzer))
         return false;
-      p->push_back(render_pass.Pass());
+      p->push_back(std::move(render_pass));
     }
     return true;
   }
@@ -986,7 +986,7 @@ struct FuzzTraits<content::SyntheticGesturePacket> {
         break;
       }
     }
-    p->set_gesture_params(gesture_params.Pass());
+    p->set_gesture_params(std::move(gesture_params));
     return true;
   }
 };
