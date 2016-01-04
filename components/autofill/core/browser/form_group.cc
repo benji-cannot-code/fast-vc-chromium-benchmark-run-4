f92361cc5b311287f2e8abfc53b21d0e90b21197
sebsg <sebsg@chromium.org>
2016-01-04 08:40:13 -0800

    [Autofill] Do a canonicalized comparison when determining possible fields to upload.
    BUG=571625
    
    Review URL: https://codereview.chromium.org/1541063004
    
    Cr-Commit-Position: refs/heads/master@{#367316}

--- components/autofill/core/browser/form_group.cc
+++ components/autofill/core/browser/form_group.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/form_group.h"
 
+#include "components/autofill/core/browser/autofill_profile.h"
 #include "components/autofill/core/browser/autofill_type.h"
 #include "components/autofill/core/common/autofill_l10n_util.h"
 
@@ -18,11 +19,18 @@ void FormGroup::GetMatchingTypes(const base::string16& text,
     return;
   }
 
+  base::string16 canonicalized_text =
+      AutofillProfile::CanonicalizeProfileString(text);
+
+  // TODO(crbug.com/574086): Investigate whether to use |app_locale| in case
+  // insensitive comparisons.
   l10n::CaseInsensitiveCompare compare;
   ServerFieldTypeSet types;
   GetSupportedTypes(&types);
   for (const auto& type : types) {
-    if (compare.StringsEqual(text, GetInfo(AutofillType(type), app_locale)))
+    if (compare.StringsEqual(canonicalized_text,
+                             AutofillProfile::CanonicalizeProfileString(
+                                 GetInfo(AutofillType(type), app_locale))))
       matching_types->insert(type);
   }
 }
