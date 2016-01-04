460c98985571d21ae61c91c84524d5f24586a8ae
jbroman <jbroman@chromium.org>
2016-01-04 10:47:56 -0800

    Revert "Reland: Make ScrollbarThemeAura selectively invalidate scrollbar parts."
    This reverts commit 54342454be5d73c64aa47b57facd880ba8df2b36.
    
    Caused a Mac 10.10 perf regression, and possibly at fault for scrollbar
    invalidation issues.
    
    BUG=573266,549277
    TBR=skobes@chromium.org,ccameron@chromium.org
    
    Review URL: https://codereview.chromium.org/1553133002
    
    Cr-Commit-Position: refs/heads/master@{#367331}

--- third_party/WebKit/Source/platform/exported/WebScrollbarThemePainter.cpp
+++ third_party/WebKit/Source/platform/exported/WebScrollbarThemePainter.cpp
@@ -67,7 +67,8 @@ void WebScrollbarThemePainter::paintTrackBackground(WebCanvas* canvas, const Web
     pictureBuilder.context().setDeviceScaleFactor(m_deviceScaleFactor);
     m_theme->paintTrackBackground(pictureBuilder.context(), *m_scrollbar, intRect);
     pictureBuilder.endRecording()->playback(canvas);
-    m_scrollbar->setTrackNeedsRepaint(false);
+    if (!m_theme->shouldRepaintAllPartsOnInvalidation())
+        m_scrollbar->setTrackNeedsRepaint(false);
 }
 
 void WebScrollbarThemePainter::paintBackTrackPart(WebCanvas* canvas, const WebRect& rect)
@@ -140,7 +141,8 @@ void WebScrollbarThemePainter::paintThumb(WebCanvas* canvas, const WebRect& rect
     pictureBuilder.context().setDeviceScaleFactor(m_deviceScaleFactor);
     m_theme->paintThumb(pictureBuilder.context(), *m_scrollbar, intRect);
     pictureBuilder.endRecording()->playback(canvas);
-    m_scrollbar->setThumbNeedsRepaint(false);
+    if (!m_theme->shouldRepaintAllPartsOnInvalidation())
+        m_scrollbar->setThumbNeedsRepaint(false);
 }
 
 WebScrollbarThemePainter::WebScrollbarThemePainter(ScrollbarTheme& theme, Scrollbar& scrollbar, float deviceScaleFactor)
