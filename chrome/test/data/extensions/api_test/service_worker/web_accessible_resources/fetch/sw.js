2f34c30e4b5125bf729306e09194665e46b52821
lazyboy <lazyboy@chromium.org>
2016-01-04 13:37:07 -0800

    Extension SW: Add tests that serve web_accessible_resources from a SW.
    This CL also fixes an issue in DocumentThreadableLoader: DTL used to
    only intercept http and https protocol requests. This CL expands it
    to match content/ by using
    SchemeRegistry::shouldTreatURLSchemeAsAllowingServiceWorkers().
    Without this change fetch interception from extension SW would always
    hit an ASSERT (in debug).
    
    ServiceWorkerTest.WebAccessibleResourcesFetch:
    Tests that a Fetch request is served correctly by a SW when
    SW is controlling an extension.
    
    ServiceWorkerTest.WebAccessibleResourcesIframeSrc:
    From a page outside an extension, an iframe is loaded with a .src of an
    extension URL that is listed in the extension's web_accessible_resources.
    The test verifies that if a Service Worker is controlling the extension,
    it will also control the iframe's requested extension resource.
    
    BUG=545535, 567382
    
    Review URL: https://codereview.chromium.org/1532633003
    
    Cr-Commit-Position: refs/heads/master@{#367375}

---
+++ chrome/test/data/extensions/api_test/service_worker/web_accessible_resources/fetch/sw.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+self.onfetch = function(e) {
+  var url = new URL(e.request.url);
+  if (url.pathname == '/data_for_extension') {
+    e.respondWith(new Response('SW served data'));
+  }
+};
+
+self.onmessage = function(e) {
+  var request = e.data;
+  switch (request) {
+    case 'claim':
+      self.clients.claim().then(function() {
+        e.ports[0].postMessage('clients claimed');
+      }).catch(function(err) {
+        e.ports[0].postMessage('FAIL' + err);
+      });
+      break;
+    default:
+      e.ports[0].postMessage('FAIL: Incorrect request.');
+      break;
+  }
+};
---
+++ chrome/test/data/extensions/api_test/service_worker/web_accessible_resources/iframe_src/background.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+window.registerServiceWorker = function() {
+  navigator.serviceWorker.register('sw.js').then(function(registration) {
+    return navigator.serviceWorker.ready;
+  }).then(function(registration) {
+    chrome.test.sendMessage('SW_READY');
+  }).catch(function(err) {
+    chrome.test.log('SW registration failure: ' + err);
+    chrome.test.sendMessage('SW_FAILED');
+  });
+  return 'true';
+};
