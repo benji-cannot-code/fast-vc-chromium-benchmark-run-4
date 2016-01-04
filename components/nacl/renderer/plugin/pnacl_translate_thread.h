d9f06e804af994865409d7a22c7339ae11cac420
mseaborn <mseaborn@chromium.org>
2016-01-04 13:14:13 -0800

    PNaCl: Use Chrome IPC to talk to the linker process, instead of SRPC
    This is the second-to-last remaining use of SRPC.
    
     * Change pnacl_translate_thread.cc to send its request using Chrome
       IPC instead of SRPC.  Similarly, change
       irt_pnacl_translator_link.cc to receive its request this way.
    
     * Add plumbing so that pnacl_translate_thread.cc can acquire a Chrome
       IPC channel object.  Change LaunchSelLdr() (in ppb_nacl_private.h)
       to return this channel, and plumb it through service_runtime.h too.
    
     * Extend nacl_message_scanner.cc to handle a couple of things our new
       IPC message needs:
        * File handles inside of std::vector<>s.
        * File handles inside of sync messages.  (Previously, only sync
          replies were handled.)
    
     * Change a BUILD.gn to account for #includes of
       content/public/common/sandbox_init.h and
       ppapi/proxy/{serialized_handle.h,ppapi_messages.h}.
    
    BUG=302078
    TEST=e.g. NaClBrowserTestPnacl.PPAPICore (tests PNaCl translation)
    
    Review URL: https://codereview.chromium.org/1512733003
    
    Cr-Commit-Position: refs/heads/master@{#367369}

--- components/nacl/renderer/plugin/pnacl_translate_thread.h
+++ components/nacl/renderer/plugin/pnacl_translate_thread.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "native_client/src/shared/platform/nacl_sync_checked.h"
 #include "native_client/src/shared/platform/nacl_threads.h"
 #include "ppapi/cpp/completion_callback.h"
+#include "ppapi/proxy/serialized_handle.h"
 
 struct PP_PNaClOptions;
 
@@ -85,6 +86,9 @@ class PnaclTranslateThread {
   bool started() const { return coordinator_ != NULL; }
 
  private:
+  ppapi::proxy::SerializedHandle GetHandleForSubprocess(TempFile* file,
+                                                        int32_t open_flags);
+
   // Helper thread entry point for compilation. Takes a pointer to
   // PnaclTranslateThread and calls DoCompile().
   static void WINAPI DoCompileThread(void* arg);
@@ -150,6 +154,15 @@ class PnaclTranslateThread {
   PP_PNaClOptions* pnacl_options_;
   std::string architecture_attributes_;
   PnaclCoordinator* coordinator_;
+
+  // This IPC::SyncChannel can only be used and freed by the parent thread.
+  scoped_ptr<IPC::SyncChannel> ld_channel_;
+  // This IPC::SyncMessageFilter can be used by the child thread.
+  scoped_refptr<IPC::SyncMessageFilter> ld_channel_filter_;
+  // PID of the subprocess, needed for copying handles to the subprocess on
+  // Windows.  This is used by the child thread.
+  base::ProcessId ld_channel_peer_pid_;
+
  private:
   NACL_DISALLOW_COPY_AND_ASSIGN(PnaclTranslateThread);
 };
