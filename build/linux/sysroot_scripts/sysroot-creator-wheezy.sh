50b539abbae82f7cdcca85230a293f3195aca1aa
raphael.kubo.da.costa <raphael.kubo.da.costa@intel.com>
2016-01-04 03:04:27 -0800

    Add more packages to the Wheezy sysroot.
    x11proto-xinerama-dev is needed by libxinerama-dev, libxdmcp-dev is used
    by xcb.
    
    With this change (and after regenerating the sysroot), it is possible to
    run gn and configure the build without having a single pkg-config in the
    host system (tested on amd64).
    
    R=sbc@chromium.org,dpranke@chromium.org
    
    Review URL: https://codereview.chromium.org/1539393002
    
    Cr-Commit-Position: refs/heads/master@{#367302}

--- build/linux/sysroot_scripts/sysroot-creator-wheezy.sh
+++ build/linux/sysroot_scripts/sysroot-creator-wheezy.sh
@@ -143,6 +143,7 @@ DEBIAN_PACKAGES="\
   libxdamage1
   libxdamage-dev
   libxdmcp6
+  libxdmcp-dev
   libxext6
   libxext-dev
   libxfixes3
@@ -176,6 +177,7 @@ DEBIAN_PACKAGES="\
   x11proto-render-dev
   x11proto-scrnsaver-dev
   x11proto-xext-dev
+  x11proto-xinerama-dev
   zlib1g
   zlib1g-dev
 "
