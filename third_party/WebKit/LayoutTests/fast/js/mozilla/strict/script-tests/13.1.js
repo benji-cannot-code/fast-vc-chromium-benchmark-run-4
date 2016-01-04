d89bc035bab3ea9f233fab45c7fcdab81ad06f11
littledan <littledan@chromium.org>
2016-01-04 15:06:09 -0800

    Change some Mozilla test expectations for shipping --harmony-sloppy
    Some edge cases in V8's SyntaxErrors are changing with shipping the
    --harmony-sloppy flag. This patch updates the tests and marks the test
    temporarily as NeedsManualRebaseline
    
    BUG=574196
    R=adamk
    
    Review URL: https://codereview.chromium.org/1552363002
    
    Cr-Commit-Position: refs/heads/master@{#367397}

--- third_party/WebKit/LayoutTests/fast/js/mozilla/strict/script-tests/13.1.js
+++ third_party/WebKit/LayoutTests/fast/js/mozilla/strict/script-tests/13.1.js
@@ -230,12 +230,15 @@ assertEq(testLenientAndStrict('function f(arguments){}',
                               parsesSuccessfully,
                               parseRaisesException(SyntaxError)),
          true);
+// BUG(v8:4577): In sloppy mode, we currently throw when destructuring
+// parameters include 'arguments', even if a direct argument named
+// 'arguments' is accepted, once the --harmony-sloppy flag is shipped.
 assertEq(testLenientAndStrict('function f([arguments]){}',
-                              parsesSuccessfully,
+                              parseRaisesException(SyntaxError),
                               parseRaisesException(SyntaxError)),
          true);
 assertEq(testLenientAndStrict('function f({x:arguments}){}',
-                              parsesSuccessfully,
+                              parseRaisesException(SyntaxError),
                               parseRaisesException(SyntaxError)),
          true);
 assertEq(testLenientAndStrict('function arguments(){}',
@@ -265,11 +268,11 @@ assertEq(testLenientAndStrict('(function f(arguments){})',
                               parseRaisesException(SyntaxError)),
          true);
 assertEq(testLenientAndStrict('(function f([arguments]){})',
-                              parsesSuccessfully,
+                              parseRaisesException(SyntaxError),
                               parseRaisesException(SyntaxError)),
          true);
 assertEq(testLenientAndStrict('(function f({x:arguments}){})',
-                              parsesSuccessfully,
+                              parseRaisesException(SyntaxError),
                               parseRaisesException(SyntaxError)),
          true);
 assertEq(testLenientAndStrict('(function arguments(){})',
@@ -319,11 +322,11 @@ assertEq(testLenientAndStrict('({set x(arguments){}})',
                               parseRaisesException(SyntaxError)),
          true);
 assertEq(testLenientAndStrict('({set x([arguments]){}})',
-                              parsesSuccessfully,
+                              parseRaisesException(SyntaxError),
                               parseRaisesException(SyntaxError)),
          true);
 assertEq(testLenientAndStrict('({set x({x:arguments}){}})',
-                              parsesSuccessfully,
+                              parseRaisesException(SyntaxError),
                               parseRaisesException(SyntaxError)),
          true);
 assertEq(testLenientAndStrict('({set x(arguments){"use strict";}})',
