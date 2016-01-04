d27bd3dbb4ccb10fd77c79a36e187bf77e3c3bb1
jdufault <jdufault@chromium.org>
2016-01-04 11:41:12 -0800

    Temporarily disable Chromecast media router integration in system tray.
    BUG=571111
    
    Review URL: https://codereview.chromium.org/1545593002
    
    Cr-Commit-Position: refs/heads/master@{#367342}

--- chrome/browser/ui/ash/system_tray_tray_cast_browsertest_media_router_chromeos.cc
+++ chrome/browser/ui/ash/system_tray_tray_cast_browsertest_media_router_chromeos.cc
@@ -106,7 +106,7 @@ IN_PROC_BROWSER_TEST_F(SystemTrayTrayCastMediaRouterChromeOSTest,
 // Verifies that we only show the tray view if there are available cast
 // targets/sinks.
 IN_PROC_BROWSER_TEST_F(SystemTrayTrayCastMediaRouterChromeOSTest,
-                       VerifyCorrectVisiblityWithSinks) {
+                       DISABLED_VerifyCorrectVisiblityWithSinks) {
   ash::TrayCast* tray = GetTrayCast();
   ash::TrayCastTestAPI test_api(tray);
   EXPECT_TRUE(test_api.IsTrayInitialized());
@@ -146,7 +146,7 @@ IN_PROC_BROWSER_TEST_F(SystemTrayTrayCastMediaRouterChromeOSTest,
 // we display the correct cast session if there are multiple active casting
 // sessions.
 IN_PROC_BROWSER_TEST_F(SystemTrayTrayCastMediaRouterChromeOSTest,
-                       VerifyCastingShowsCastView) {
+                       DISABLED_VerifyCastingShowsCastView) {
   ash::TrayCast* tray = GetTrayCast();
   ash::TrayCastTestAPI test_api(tray);
   EXPECT_TRUE(test_api.IsTrayInitialized());
