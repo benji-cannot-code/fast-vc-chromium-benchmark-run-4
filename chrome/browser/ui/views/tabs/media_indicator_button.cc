5468a53e1ae6bda863044651e1770592e89cd651
miu <miu@chromium.org>
2016-01-04 16:27:16 -0800

    Fix for unclickable media indicator while NumLock is on.
    MediaIndicatorButton assumed all ui::Event flags were either mouse or
    key "held down" properties.  However, NumLock/CapsLock/etc status is not
    a modifier key.
    
    BUG=550570
    TEST=Run chrome with --enable-tab-audio-muting on Windows platform.  Play audio and confirm clicking the tab media indicator toggles mute, both when NumLock is on and when it is off.
    
    Review URL: https://codereview.chromium.org/1552373002
    
    Cr-Commit-Position: refs/heads/master@{#367425}

--- chrome/browser/ui/views/tabs/media_indicator_button.cc
+++ chrome/browser/ui/views/tabs/media_indicator_button.cc
@@ -25,6 +25,13 @@ namespace {
 const int kMinMouseSelectableAreaPercent = 250;
 const int kMinGestureSelectableAreaPercent = 400;
 
+// Returns true if either Shift or Control are being held down.  In this case,
+// mouse events are delegated to the Tab, to perform tab selection in the tab
+// strip instead.
+bool IsShiftOrControlDown(const ui::Event& event) {
+  return (event.flags() & (ui::EF_SHIFT_DOWN | ui::EF_CONTROL_DOWN)) != 0;
+}
+
 }  // namespace
 
 const char MediaIndicatorButton::kViewClassName[] = "MediaIndicatorButton";
@@ -149,7 +156,7 @@ bool MediaIndicatorButton::OnMousePressed(const ui::MouseEvent& event) {
   // Do not handle this mouse event when anything but the left mouse button is
   // pressed or when any modifier keys are being held down.  Instead, the Tab
   // should react (e.g., middle-click for close, right-click for context menu).
-  if (event.flags() != ui::EF_LEFT_MOUSE_BUTTON) {
+  if (!event.IsOnlyLeftMouseButton() || IsShiftOrControlDown(event)) {
     if (state() != views::CustomButton::STATE_DISABLED)
       SetState(views::CustomButton::STATE_NORMAL);  // Turn off hover.
     return false;  // Event to be handled by Tab.
@@ -169,7 +176,7 @@ bool MediaIndicatorButton::OnMouseDragged(const ui::MouseEvent& event) {
 void MediaIndicatorButton::OnMouseEntered(const ui::MouseEvent& event) {
   // If any modifier keys are being held down, do not turn on hover.
   if (state() != views::CustomButton::STATE_DISABLED &&
-      event.flags() != ui::EF_NONE) {
+      IsShiftOrControlDown(event)) {
     SetState(views::CustomButton::STATE_NORMAL);
     return;
   }
@@ -179,7 +186,7 @@ void MediaIndicatorButton::OnMouseEntered(const ui::MouseEvent& event) {
 void MediaIndicatorButton::OnMouseMoved(const ui::MouseEvent& event) {
   // If any modifier keys are being held down, turn off hover.
   if (state() != views::CustomButton::STATE_DISABLED &&
-      event.flags() != ui::EF_NONE) {
+      IsShiftOrControlDown(event)) {
     SetState(views::CustomButton::STATE_NORMAL);
     return;
   }
@@ -224,7 +231,9 @@ void MediaIndicatorButton::NotifyClick(const ui::Event& event) {
 bool MediaIndicatorButton::IsTriggerableEvent(const ui::Event& event) {
   // For mouse events, only trigger on the left mouse button and when no
   // modifier keys are being held down.
-  if (event.IsMouseEvent() && event.flags() != ui::EF_LEFT_MOUSE_BUTTON)
+  if (event.IsMouseEvent() &&
+      (!static_cast<const ui::MouseEvent*>(&event)->IsOnlyLeftMouseButton() ||
+       IsShiftOrControlDown(event)))
     return false;
 
   // For gesture events on an inactive tab, require an even wider tab before
