db375b2e3f677f82f013a5b09afdd916d254c181
ikilpatrick <ikilpatrick@chromium.org>
2016-01-05 08:55:37 -0800

    Remove FontFaceSetForEachCallback.
    This class appears to be dead? This has no behaviour changes.
    
    Usage of FontFaceSetForEachCallback was dropped in
    https://codereview.chromium.org/1409433003
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1559013003
    
    Cr-Commit-Position: refs/heads/master@{#367552}

--- third_party/WebKit/Source/core/css/FontFaceSet.h
+++ third_party/WebKit/Source/core/css/FontFaceSet.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/Iterable.h"
 #include "bindings/core/v8/ScriptPromise.h"
 #include "core/css/FontFace.h"
-#include "core/css/FontFaceSetForEachCallback.h"
 #include "core/dom/ActiveDOMObject.h"
 #include "core/events/EventListener.h"
 #include "core/events/EventTarget.h"
