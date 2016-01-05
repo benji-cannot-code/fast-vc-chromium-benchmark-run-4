e56b312dc630b1051ff49923609604627fef735e
sigbjornf <sigbjornf@opera.com>
2016-01-04 22:21:51 -0800

    Reduce risk of MediaQueryEvaluator-induced leaks.
    With Oilpan, having MediaQueryEvaluator keep a Persistent<> reference
    to MediaValues isn't necessary and by doing so, increases the risk
    of creating inadvertent leaks. Move MediaQueryEvaluator to the heap
    instead.
    
    R=haraken
    BUG=509911
    
    Review URL: https://codereview.chromium.org/1555993002
    
    Cr-Commit-Position: refs/heads/master@{#367489}

--- third_party/WebKit/Source/core/css/MediaQueryMatcher.cpp
+++ third_party/WebKit/Source/core/css/MediaQueryMatcher.cpp
@@ -54,12 +54,12 @@ void MediaQueryMatcher::documentDetached()
     m_evaluator = nullptr;
 }
 
-PassOwnPtr<MediaQueryEvaluator> MediaQueryMatcher::createEvaluator() const
+PassOwnPtrWillBeRawPtr<MediaQueryEvaluator> MediaQueryMatcher::createEvaluator() const
 {
     if (!m_document || !m_document->frame())
         return nullptr;
 
-    return adoptPtr(new MediaQueryEvaluator(m_document->frame()));
+    return adoptPtrWillBeNoop(new MediaQueryEvaluator(m_document->frame()));
 }
 
 bool MediaQueryMatcher::evaluate(const MediaQuerySet* media)
@@ -148,6 +148,7 @@ DEFINE_TRACE(MediaQueryMatcher)
 {
 #if ENABLE(OILPAN)
     visitor->trace(m_document);
+    visitor->trace(m_evaluator);
     visitor->trace(m_mediaLists);
     visitor->trace(m_viewportListeners);
 #endif
