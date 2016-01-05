cc039b9cde990207793ec343f63deb78447b2282
tommycli <tommycli@chromium.org>
2016-01-04 18:16:36 -0800

    Plugin Power Saver: Fix flaky pixel tests on ChromeOS=1 on Linux.
    This fixes the pixel test flakiness (at least on my machine).
    
    Flakiness exhibited here: http://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=browser_tests&tests=PluginPowerSaver
    
    BUG=574266
    TEST=MANUAL
    
    Review URL: https://codereview.chromium.org/1554213003
    
    Cr-Commit-Position: refs/heads/master@{#367460}

--- chrome/browser/plugins/plugin_power_saver_browsertest.cc
+++ chrome/browser/plugins/plugin_power_saver_browsertest.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/screen.h"
 #include "ui/gfx/switches.h"
+#include "ui/gl/gl_switches.h"
 
 namespace {
 
@@ -284,9 +285,13 @@ class PluginPowerSaverBrowserTest : public InProcessBrowserTest {
     // Allows us to use the same reference image on HiDPI/Retina displays.
     command_line->AppendSwitchASCII(switches::kForceDeviceScaleFactor, "1");
 
-#if !defined(OS_CHROMEOS)
-    // These pixel tests are flaky on MSan bots with hardware rendering.
-    // However, ChromeOS does not support software compositing.
+#if defined(OS_CHROMEOS)
+    // ChromeOS builds running on Linux use OSMesa by default. This is flaky
+    // with pixel tests, so force these environments to use hardware GPU.
+    command_line->AppendSwitch(switches::kUseGpuInTests);
+#else
+    // Software rendering is faster and more reliable for these tests.
+    // Unfortunately ChromeOS does not support software rendering.
     command_line->AppendSwitch(switches::kDisableGpu);
 #endif
   }
