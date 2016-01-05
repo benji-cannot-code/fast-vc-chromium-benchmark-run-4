ddb5f8f80ed41f696ed66c550ca44c5a922bc8ea
samli <samli@chromium.org>
2016-01-04 17:44:25 -0800

    Devtools: Add metrics for drawer views being shown
    Given that drawer views are taking an increasingly dominant UI position,
    broad metric should be added like panels shown.
    
    Review URL: https://codereview.chromium.org/1520403004
    
    Cr-Commit-Position: refs/heads/master@{#367455}

--- third_party/WebKit/Source/devtools/front_end/components/Drawer.js
+++ third_party/WebKit/Source/devtools/front_end/components/Drawer.js
@@ -69,6 +69,7 @@ WebInspector.Drawer.prototype = {
     showView: function(id, immediate)
     {
         this._innerShow(immediate);
+        WebInspector.userMetrics.drawerShown(id);
         return this._extensibleTabbedPaneController.showTab(id);
     },
 
