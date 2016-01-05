3141c9d5affe34f43f4c43bc4ef00cd7cfbe6330
haraken <haraken@chromium.org>
2016-01-05 06:08:28 -0800

    Remove ModuleProxy
    It is unused.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1561493002
    
    Cr-Commit-Position: refs/heads/master@{#367523}

--- third_party/WebKit/Source/bindings/core/v8/v8.gypi
+++ third_party/WebKit/Source/bindings/core/v8/v8.gypi
@@ -39,8 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'ExceptionStatePlaceholder.h',
             'Iterable.h',
             'Maplike.h',
-            'ModuleProxy.cpp',
-            'ModuleProxy.h',
             'NativeValueTraits.h',
             'NPV8Object.cpp',
             'NPV8Object.h',
--- third_party/WebKit/Source/bindings/modules/v8/ModuleBindingsInitializer.cpp
+++ third_party/WebKit/Source/bindings/modules/v8/ModuleBindingsInitializer.cpp
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/modules/v8/ModuleBindingsInitializer.h"
 
-#include "bindings/core/v8/ModuleProxy.h"
 #include "bindings/core/v8/V8PerIsolateData.h"
 #include "bindings/modules/v8/SerializedScriptValueForModulesFactory.h"
 #include "core/dom/ExecutionContext.h"
