09a6c8726db31c8cd53d206d71f50efdd52dc593
olli.raula <olli.raula@intel.com>
2016-01-05 01:19:47 -0800

    Make FactoryMap use scoped_ptr in http_auth_handler_factory
    Make FactoryMap use scoped_ptr in http_auth_handler_factory
    because is allows some code deletions and cleanings and is safer.
    
    Review URL: https://codereview.chromium.org/1551863004
    
    Cr-Commit-Position: refs/heads/master@{#367501}

--- net/http/http_auth_handler_factory.cc
+++ net/http/http_auth_handler_factory.cc
@@ -101,8 +101,6 @@ HttpAuthHandlerRegistryFactory::HttpAuthHandlerRegistryFactory() {
 }
 
 HttpAuthHandlerRegistryFactory::~HttpAuthHandlerRegistryFactory() {
-  STLDeleteContainerPairSecondPointers(factory_map_.begin(),
-                                       factory_map_.end());
 }
 
 void HttpAuthHandlerRegistryFactory::SetHttpAuthPreferences(
@@ -118,14 +116,10 @@ void HttpAuthHandlerRegistryFactory::RegisterSchemeFactory(
     HttpAuthHandlerFactory* factory) {
   factory->set_http_auth_preferences(http_auth_preferences());
   std::string lower_scheme = base::ToLowerASCII(scheme);
-  FactoryMap::iterator it = factory_map_.find(lower_scheme);
-  if (it != factory_map_.end()) {
-    delete it->second;
-  }
   if (factory)
-    factory_map_[lower_scheme] = factory;
+    factory_map_[lower_scheme] = make_scoped_ptr(factory);
   else
-    factory_map_.erase(it);
+    factory_map_.erase(lower_scheme);
 }
 
 HttpAuthHandlerFactory* HttpAuthHandlerRegistryFactory::GetSchemeFactory(
@@ -135,7 +129,7 @@ HttpAuthHandlerFactory* HttpAuthHandlerRegistryFactory::GetSchemeFactory(
   if (it == factory_map_.end()) {
     return NULL;                  // |scheme| is not registered.
   }
-  return it->second;
+  return it->second.get();
 }
 
 // static
@@ -159,7 +153,7 @@ HttpAuthHandlerRegistryFactory::Create(const HttpAuthPreferences* prefs,
   scoped_ptr<HttpAuthHandlerRegistryFactory> registry_factory(
       CreateAuthHandlerRegistryFactory(*prefs, host_resolver));
   registry_factory->set_http_auth_preferences(prefs);
-  for (auto factory_entry : registry_factory->factory_map_) {
+  for (auto& factory_entry : registry_factory->factory_map_) {
     factory_entry.second->set_http_auth_preferences(prefs);
   }
   return registry_factory;
