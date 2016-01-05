bc003116339831379f12c9318e45d35f6013c490
haraken <haraken@chromium.org>
2016-01-04 16:52:24 -0800

    Oilpan: CustomElementMicrotaskRunQueue should take a WeakPersistent
    The WeakPtr doesn't cause a real issue (for some complicated reason) but it's better to use a WeakPersistent.
    
    BUG=567997
    
    Review URL: https://codereview.chromium.org/1513983002
    
    Cr-Commit-Position: refs/heads/master@{#367434}

--- third_party/WebKit/Source/core/dom/custom/CustomElementMicrotaskRunQueue.cpp
+++ third_party/WebKit/Source/core/dom/custom/CustomElementMicrotaskRunQueue.cpp
@@ -15,10 +15,12 @@ namespace blink {
 DEFINE_EMPTY_DESTRUCTOR_WILL_BE_REMOVED(CustomElementMicrotaskRunQueue)
 
 CustomElementMicrotaskRunQueue::CustomElementMicrotaskRunQueue()
-    : m_weakFactory(this)
-    , m_syncQueue(CustomElementSyncMicrotaskQueue::create())
+    : m_syncQueue(CustomElementSyncMicrotaskQueue::create())
     , m_asyncQueue(CustomElementAsyncImportMicrotaskQueue::create())
     , m_dispatchIsPending(false)
+#if !ENABLE(OILPAN)
+    , m_weakFactory(this)
+#endif
 {
 }
 
@@ -36,7 +38,7 @@ void CustomElementMicrotaskRunQueue::enqueue(HTMLImportLoader* parentLoader, Pas
     requestDispatchIfNeeded();
 }
 
-void CustomElementMicrotaskRunQueue::dispatchIfAlive(WeakPtr<CustomElementMicrotaskRunQueue> self)
+void CustomElementMicrotaskRunQueue::dispatchIfAlive(WeakPtrWillBeWeakPersistent<CustomElementMicrotaskRunQueue> self)
 {
     if (self.get()) {
         RefPtrWillBeRawPtr<CustomElementMicrotaskRunQueue> protect(self.get());
@@ -48,7 +50,11 @@ void CustomElementMicrotaskRunQueue::requestDispatchIfNeeded()
 {
     if (m_dispatchIsPending || isEmpty())
         return;
+#if ENABLE(OILPAN)
+    Microtask::enqueueMicrotask(WTF::bind(&CustomElementMicrotaskRunQueue::dispatchIfAlive, WeakPersistent<CustomElementMicrotaskRunQueue>(this)));
+#else
     Microtask::enqueueMicrotask(WTF::bind(&CustomElementMicrotaskRunQueue::dispatchIfAlive, m_weakFactory.createWeakPtr()));
+#endif
     m_dispatchIsPending = true;
 }
 
