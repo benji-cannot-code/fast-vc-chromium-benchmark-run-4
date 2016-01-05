160d7400d683d4525824b50f1f6a093e7b406466
lhchavez <lhchavez@chromium.org>
2016-01-05 11:43:13 -0800

    arc-bridge: Restart ARC instance on crash
    There have been cases where the instance crashes. This change makes it
    so that if there is a crash and the connection gets lost, the instance
    will be cleanly shut down and restarted afterwards.
    
    BUG=569446
    TEST=components_unittests
    
    Review URL: https://codereview.chromium.org/1548833002
    
    Cr-Commit-Position: refs/heads/master@{#367613}

--- components/arc/arc_bridge_service_impl.cc
+++ components/arc/arc_bridge_service_impl.cc
@@ -76,6 +76,9 @@ void ArcBridgeServiceImpl::StopInstance() {
   }
 
   SetState(State::STOPPING);
+  instance_ptr_.reset();
+  if (binding_.is_bound())
+    binding_.Close();
   bootstrap_->Stop();
 }
 
@@ -96,6 +99,8 @@ void ArcBridgeServiceImpl::OnConnectionEstablished(
   }
 
   instance_ptr_ = std::move(instance);
+  instance_ptr_.set_connection_error_handler(base::Bind(
+      &ArcBridgeServiceImpl::OnChannelClosed, weak_factory_.GetWeakPtr()));
 
   ArcBridgeHostPtr host;
   binding_.Bind(GetProxy(&host));
@@ -107,6 +112,24 @@ void ArcBridgeServiceImpl::OnConnectionEstablished(
 void ArcBridgeServiceImpl::OnStopped() {
   DCHECK(CalledOnValidThread());
   SetState(State::STOPPED);
+  if (reconnect_) {
+    // There was a previous invocation and it crashed for some reason. Try
+    // starting the container again.
+    reconnect_ = false;
+    PrerequisitesChanged();
+  }
+}
+
+void ArcBridgeServiceImpl::OnChannelClosed() {
+  DCHECK(CalledOnValidThread());
+  if (state() == State::STOPPED || state() == State::STOPPING) {
+    // This will happen when the instance is shut down. Ignore that case.
+    return;
+  }
+  VLOG(1) << "Mojo connection lost";
+  CloseAllChannels();
+  reconnect_ = true;
+  StopInstance();
 }
 
 }  // namespace arc
