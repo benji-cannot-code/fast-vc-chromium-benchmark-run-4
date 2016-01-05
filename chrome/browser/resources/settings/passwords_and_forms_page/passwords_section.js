ffd3ab169c321f739162ccde23077441d5079a09
hcarmona <hcarmona@chromium.org>
2016-01-04 16:56:53 -0800

    Add the "Saved Passwords" section to "Manage passwords" in md-settings.
    The current implementation returns fake data. This will ensure that the
    UI (this CL) and the data layer can be tested separately.
    
    BUG=546836
    
    Review URL: https://codereview.chromium.org/1506743004
    
    Cr-Commit-Position: refs/heads/master@{#367437}

---
+++ chrome/browser/resources/settings/passwords_and_forms_page/passwords_section.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview 'passwords-section' is the collapsible section containing
+ * the list of saved passwords as well as the list of sites that will never
+ * save any passwords.
+ *
+ * @group Chrome Settings Elements
+ * @element passwords-section
+ */
+(function() {
+'use strict';
+
+Polymer({
+  is: 'passwords-section',
+
+  properties: {
+    /**
+     * An array of passwords to display.
+     */
+    savedPasswords: {
+      type: Array,
+      value: function() { return []; },
+    },
+  },
+});
+})();
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -997,6 +997,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'test/data/webui/settings/bluetooth_page_browsertest_chromeos.js',
       'test/data/webui/settings/cr_settings_browsertest.js',
       'test/data/webui/settings/settings_page_browsertest.js',
+      'test/data/webui/settings/settings_passwords_section_browsertest.js',
       'test/data/webui/settings/settings_subpage_browsertest.js',
     ],
     # TODO(rockot) bug 505926: These should be moved to a target in
