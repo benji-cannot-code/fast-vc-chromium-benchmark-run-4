c0f99a69ffb40105e9cd79da2402d15b4692a211
kimwjdalsl <kimwjdalsl@gmail.com>
2016-01-05 12:28:56 -0800

    Remove ScopedVector from web_external_texture_layer_impl
    ScopedVector is deprecated.
    
    BUG=554289
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1538323002
    
    Cr-Commit-Position: refs/heads/master@{#367630}

--- cc/blink/web_external_texture_layer_impl.h
+++ cc/blink/web_external_texture_layer_impl.h
@@ -6,10 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_BLINK_WEB_EXTERNAL_TEXTURE_LAYER_IMPL_H_
 #define CC_BLINK_WEB_EXTERNAL_TEXTURE_LAYER_IMPL_H_
 
+#include <vector>
+
 #include "base/bind.h"
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/memory/scoped_vector.h"
 #include "cc/blink/cc_blink_export.h"
 #include "cc/layers/texture_layer_client.h"
 #include "third_party/WebKit/public/platform/WebExternalTextureLayer.h"
@@ -65,7 +66,7 @@ class WebExternalTextureLayerImpl
 
   blink::WebExternalTextureLayerClient* client_;
   scoped_ptr<WebLayerImpl> layer_;
-  ScopedVector<WebExternalBitmapImpl> free_bitmaps_;
+  std::vector<scoped_ptr<WebExternalBitmapImpl>> free_bitmaps_;
 
   DISALLOW_COPY_AND_ASSIGN(WebExternalTextureLayerImpl);
 };
