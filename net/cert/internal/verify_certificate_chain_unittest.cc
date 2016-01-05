24fb595548adace8f82e269bf37d40df4077ec11
eroman <eroman@chromium.org>
2016-01-04 18:39:26 -0800

    Do full name comparison (rather than just byte-for-byte equality test) when verifying certificates.
    This applies to testing that the issuer name matches issuer's subject name, as well as checking if the certificate is self-issued.
    
    BUG=410574
    
    Review URL: https://codereview.chromium.org/1559563002
    
    Cr-Commit-Position: refs/heads/master@{#367462}

--- net/cert/internal/verify_certificate_chain_unittest.cc
+++ net/cert/internal/verify_certificate_chain_unittest.cc
@@ -122,7 +122,7 @@ void RunTest(const char* file_name) {
   for (const auto& cert_str : chain)
     input_chain.push_back(InputFromString(&cert_str));
 
-  SimpleSignaturePolicy signature_policy(2048);
+  SimpleSignaturePolicy signature_policy(1024);
 
   bool result =
       VerifyCertificateChain(input_chain, trust_store, &signature_policy, time);
@@ -216,6 +216,14 @@ TEST(VerifyCertificateChainTest, TargetHasPathlenButNotCa) {
 
 TEST(VerifyCertificateChainTest, TargetUnknownCriticalExtension) {
   RunTest("target-unknown-critical-extension.pem");
+}
+
+TEST(VerifyCertificateChainTest, IssuerAndSubjectNotByteForByteEqual) {
+  RunTest("issuer-and-subject-not-byte-for-byte-equal.pem");
+}
+
+TEST(VerifyCertificateChainTest, IssuerAndSubjectNotByteForByteEqualAnchor) {
+  RunTest("issuer-and-subject-not-byte-for-byte-equal-anchor.pem");
 }
 
 // Tests that verifying a chain with no certificates fails.
