690eebf479bd0eee9cd5c6987ce75e05e240bfd3
nhiroki <nhiroki@chromium.org>
2016-01-05 03:41:22 -0800

    ServiceWorker: Replace magic numbers with named constants
    BUG=566986
    
    Review URL: https://codereview.chromium.org/1555283002
    
    Cr-Commit-Position: refs/heads/master@{#367507}

--- content/browser/service_worker/embedded_worker_instance_unittest.cc
+++ content/browser/service_worker/embedded_worker_instance_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/service_worker/service_worker_context_core.h"
 #include "content/browser/service_worker/service_worker_context_wrapper.h"
 #include "content/common/service_worker/embedded_worker_messages.h"
+#include "content/public/common/child_process_host.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -270,8 +271,9 @@ TEST_F(EmbeddedWorkerInstanceTest, DetachDuringStart) {
   base::RunLoop run_loop;
   worker->SendStartWorker(
       std::move(params),
-      base::Bind(&SaveStatusAndCall, &status, run_loop.QuitClosure()), true, -1,
-      false);
+      base::Bind(&SaveStatusAndCall, &status, run_loop.QuitClosure()),
+      true /* is_new_process */, MSG_ROUTING_NONE,
+      false /* wait_for_debugger */);
   run_loop.Run();
   EXPECT_EQ(SERVICE_WORKER_ERROR_ABORT, status);
   // Don't expect SendStartWorker() to dispatch an OnStopped/Detached() message
@@ -285,8 +287,9 @@ TEST_F(EmbeddedWorkerInstanceTest, DetachDuringStart) {
   EmbeddedWorkerInstance* worker_ptr = worker.get();
   worker_ptr->SendStartWorker(
       std::move(params),
-      base::Bind(&DestroyWorker, base::Passed(&worker), &status), true, -1,
-      false);
+      base::Bind(&DestroyWorker, base::Passed(&worker), &status),
+      true /* is_new_process */, MSG_ROUTING_NONE,
+      false /* wait_for_debugger */);
   // No crash.
   EXPECT_EQ(SERVICE_WORKER_ERROR_ABORT, status);
 }
@@ -299,7 +302,7 @@ TEST_F(EmbeddedWorkerInstanceTest, StopDuringStart) {
   worker->AddListener(this);
   // Pretend we stop during starting before we got a process allocated.
   worker->status_ = EmbeddedWorkerInstance::STARTING;
-  worker->process_id_ = -1;
+  worker->process_id_ = ChildProcessHost::kInvalidUniqueID;
   worker->Stop();
   EXPECT_EQ(EmbeddedWorkerInstance::STOPPED, worker->status());
   EXPECT_TRUE(detached_);
--- content/browser/service_worker/service_worker_process_manager_unittest.cc
+++ content/browser/service_worker/service_worker_process_manager_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/run_loop.h"
 #include "content/browser/service_worker/service_worker_process_manager.h"
+#include "content/public/common/child_process_host.h"
 #include "content/public/test/mock_render_process_host.h"
 #include "content/public/test/test_browser_context.h"
 #include "content/public/test/test_browser_thread_bundle.h"
@@ -241,7 +242,7 @@ TEST_F(ServiceWorkerProcessManagerTest, AllocateWorkerProcess_InShutdown) {
 
   // Allocating a process in shutdown should abort.
   EXPECT_EQ(SERVICE_WORKER_ERROR_ABORT, status);
-  EXPECT_EQ(-1, process_id);
+  EXPECT_EQ(ChildProcessHost::kInvalidUniqueID, process_id);
   EXPECT_FALSE(is_new_process);
   EXPECT_TRUE(process_manager_->instance_info_.empty());
 }
