808f50852e1e465f9e1d7ed787050bc137fafd71
ricea <ricea@chromium.org>
2016-01-05 20:57:38 -0800

    Add current_send_quota() accessor to WebSocketChannel
    Make the browser's idea of how much quota it has given the renderer
    visible outside net::WebSocketChannel. This will be used for
    browser-side Blob sending functionality.
    
    See design doc at
    https://docs.google.com/document/d/1CDiXB9pBumhFVVfmIn1CRI6v6byxyqWu2urEE9xp714/edit
    
    BUG=571656
    TEST=net_unittests
    
    Review URL: https://codereview.chromium.org/1549453003
    
    Cr-Commit-Position: refs/heads/master@{#367777}

--- net/websockets/websocket_channel_test.cc
+++ net/websockets/websocket_channel_test.cc
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/websockets/websocket_channel.h"
 
 #include <limits.h>
+#include <stddef.h>
 #include <string.h>
-
 #include <iostream>
 #include <iterator>
 #include <string>
@@ -3420,6 +3420,29 @@ TEST_F(WebSocketChannelStreamTimeoutTest, ConnectionCloseTimesOut) {
   *read_frames = CreateFrameVector(frames);
   read_callback.Run(OK);
   completion.WaitForResult();
+}
+
+// Verify that current_send_quota() returns a non-zero value for a newly
+// connected channel.
+TEST_F(WebSocketChannelTest, CurrentSendQuotaNonZero) {
+  CreateChannelAndConnectSuccessfully();
+  EXPECT_GT(channel_->current_send_quota(), 0);
+}
+
+// Verify that current_send_quota() is updated when SendFrame() is called.
+TEST_F(WebSocketChannelTest, CurrentSendQuotaUpdated) {
+  const int kMessageSize = 5;
+  set_stream(make_scoped_ptr(new WriteableFakeWebSocketStream));
+  CreateChannelAndConnectSuccessfully();
+
+  int initial_send_quota = channel_->current_send_quota();
+  EXPECT_GE(initial_send_quota, kMessageSize);
+
+  channel_->SendFrame(
+      true, WebSocketFrameHeader::kOpCodeText,
+      std::vector<char>(static_cast<size_t>(kMessageSize), 'a'));
+  int new_send_quota = channel_->current_send_quota();
+  EXPECT_EQ(kMessageSize, initial_send_quota - new_send_quota);
 }
 
 }  // namespace
