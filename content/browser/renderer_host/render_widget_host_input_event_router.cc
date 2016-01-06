87915f91dac85335c0dfe73129d3973fe6733919
wjmaclean <wjmaclean@chromium.org>
2016-01-06 15:58:06 -0800

    Store WeakPointer for Touch event target.
    Since the target used for touch events must remain the same over a
    sequence (TouchStart, TouchMove, TouchEnd/TouchCancel), and since the
    target RenderWidgetHostView coulc disappear mid-sequence, we retain a
    WeakPtr to the current touch target and test it each time we use it.
    
    BUG=570646
    
    Review URL: https://codereview.chromium.org/1562523002
    
    Cr-Commit-Position: refs/heads/master@{#367962}

--- content/browser/renderer_host/render_widget_host_input_event_router.cc
+++ content/browser/renderer_host/render_widget_host_input_event_router.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 RenderWidgetHostInputEventRouter::RenderWidgetHostInputEventRouter()
-    : current_touch_target_(nullptr), active_touches_(0) {}
+    : active_touches_(0) {}
 
 RenderWidgetHostInputEventRouter::~RenderWidgetHostInputEventRouter() {
   owner_map_.clear();
@@ -42,7 +42,12 @@ RenderWidgetHostViewBase* RenderWidgetHostInputEventRouter::FindEventTarget(
   // parent frame has not sent a new compositor frame since that happened.
   if (iter == owner_map_.end())
     return root_view;
-  return iter->second.get();
+  RenderWidgetHostViewBase* target = iter->second.get();
+  // If we find the weak pointer is now null, it means the map entry is stale
+  // and should be removed to free space.
+  if (!target)
+    owner_map_.erase(iter);
+  return target;
 }
 
 void RenderWidgetHostInputEventRouter::RouteMouseEvent(
@@ -88,8 +93,14 @@ void RenderWidgetHostInputEventRouter::RouteTouchEvent(
         gfx::Point transformed_point;
         gfx::Point original_point(event->touches[0].position.x,
                                   event->touches[0].position.y);
-        current_touch_target_ =
+        RenderWidgetHostViewBase* target =
             FindEventTarget(root_view, original_point, &transformed_point);
+        if (!target)
+          return;
+
+        // Store the weak-ptr to the target, since it could disappear in the
+        // middle of a touch sequence.
+        current_touch_target_ = target->GetWeakPtr();
       }
       ++active_touches_;
       if (current_touch_target_)
@@ -107,7 +118,7 @@ void RenderWidgetHostInputEventRouter::RouteTouchEvent(
         current_touch_target_->ProcessTouchEvent(*event, latency);
       --active_touches_;
       if (!active_touches_)
-        current_touch_target_ = nullptr;
+        current_touch_target_ = WeakTarget();
       break;
     default:
       NOTREACHED();
--- content/browser/renderer_host/render_widget_host_input_event_router.h
+++ content/browser/renderer_host/render_widget_host_input_event_router.h
@@ -58,6 +58,7 @@ class CONTENT_EXPORT RenderWidgetHostInputEventRouter {
   }
 
  private:
+  using WeakTarget = base::WeakPtr<RenderWidgetHostViewBase>;
   using SurfaceIdNamespaceOwnerMap =
       base::hash_map<uint32_t, base::WeakPtr<RenderWidgetHostViewBase>>;
 
@@ -66,7 +67,7 @@ class CONTENT_EXPORT RenderWidgetHostInputEventRouter {
                                             gfx::Point* transformed_point);
 
   SurfaceIdNamespaceOwnerMap owner_map_;
-  RenderWidgetHostViewBase* current_touch_target_;
+  WeakTarget current_touch_target_;
   int active_touches_;
 
   DISALLOW_COPY_AND_ASSIGN(RenderWidgetHostInputEventRouter);
