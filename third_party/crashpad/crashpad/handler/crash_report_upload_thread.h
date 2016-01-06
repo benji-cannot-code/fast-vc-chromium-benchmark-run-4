709d1925a972e1589bce0ed930e97d116c92c1e8
scottmg <scottmg@chromium.org>
2016-01-06 13:57:05 -0800

    Update Crashpad to 330adfb02984692135fc5989dbaff49adb3b757e
    330adfb02984 Allow disabling upload rate-limiting in crashpad_handler
    
    R=mark@chromium.org
    BUG=546288
    
    Review URL: https://codereview.chromium.org/1565853003
    
    Cr-Commit-Position: refs/heads/master@{#367927}

--- third_party/crashpad/crashpad/handler/crash_report_upload_thread.h
+++ third_party/crashpad/crashpad/handler/crash_report_upload_thread.h
@@ -44,8 +44,11 @@ class CrashReportUploadThread : public WorkerThread::Delegate {
   //!
   //! \param[in] database The database to upload crash reports from.
   //! \param[in] url The URL of the server to upload crash reports to.
+  //! \param[in] rate_limit Whether uploads should be throttled to a (currently
+  //!     hardcoded) rate.
   CrashReportUploadThread(CrashReportDatabase* database,
-                          const std::string& url);
+                          const std::string& url,
+                          bool rate_limit);
   ~CrashReportUploadThread();
 
   //! \brief Starts a dedicated upload thread, which executes ThreadMain().
@@ -136,6 +139,7 @@ class CrashReportUploadThread : public WorkerThread::Delegate {
   std::string url_;
   WorkerThread thread_;
   CrashReportDatabase* database_;  // weak
+  bool rate_limit_;
 
   DISALLOW_COPY_AND_ASSIGN(CrashReportUploadThread);
 };
