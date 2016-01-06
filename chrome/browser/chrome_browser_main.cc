2a4bdc2e3d12ba10ddf14c4c7f91fe3832d2c8ea
dalecurtis <dalecurtis@chromium.org>
2016-01-05 16:12:17 -0800

    Disable non-crash dumps since we have a large enough list now.
    The top 15 bad drivers are (sent to Microsoft):
    RTKVHD64.sys : 2249
    HdAudio.sys : 1246
    CHDRT64.sys : 1210
    stwrt64.sys : 1004
    RTKVHDA.sys : 470
    viahduaa.sys : 198
    isstrtc.sys : 194
    stwrt.sys : 163
    USBAUDIO.sys : 119
    IntcDAud.sys : 61
    RtkHDAud.sys : 36
    CHDRT32.sys : 30
    BthA2DP.sys : 28
    ADIHdAud.sys : 25
    CHDART.sys : 21
    
    Query (internal only, sorry):
    https://paste.googleplex.com/5147294596857856
    
    UMA stats show 99.96% of audio threads never hang, 0.03% hang and
    0.01% eventually recover.
    
    BUG=422522
    TEST=none
    
    Review URL: https://codereview.chromium.org/1563493004
    
    Cr-Commit-Position: refs/heads/master@{#367709}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -144,7 +144,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/public/common/main_function_params.h"
 #include "grit/platform_locale_settings.h"
-#include "media/audio/audio_manager.h"
 #include "net/base/net_module.h"
 #include "net/cookies/cookie_monster.h"
 #include "net/http/http_network_layer.h"
@@ -743,14 +742,6 @@ void ChromeBrowserMainParts::SetupMetricsAndFieldTrials() {
   metrics->InitializeMetricsRecordingState();
 
   const version_info::Channel channel = chrome::GetChannel();
-
-  // TODO(dalecurtis): Remove these checks and enable for all channels once we
-  // track down the root causes of crbug.com/422522 and crbug.com/478932.
-  if (channel == version_info::Channel::UNKNOWN ||
-      channel == version_info::Channel::CANARY ||
-      channel == version_info::Channel::DEV) {
-    media::AudioManager::EnableCrashKeyLoggingForAudioThreadHangs();
-  }
 
   // Enable profiler instrumentation depending on the channel.
   switch (channel) {
