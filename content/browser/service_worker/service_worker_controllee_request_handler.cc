f74b27df02c35a2e687437b432f24537c07a2a39
falken <falken@chromium.org>
2016-01-06 02:07:25 -0800

    [service worker] Associate installing registrations with provider hosts
    If a page is loaded and there's an installing registration, if we don't
    associate the registration then the .ready promise would never resolve.
    
    BUG=513684
    
    Review URL: https://codereview.chromium.org/1562713002
    
    Cr-Commit-Position: refs/heads/master@{#367807}

--- content/browser/service_worker/service_worker_controllee_request_handler.cc
+++ content/browser/service_worker/service_worker_controllee_request_handler.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/service_worker/service_worker_controllee_request_handler.h"
 
+#include <string>
+
 #include "base/memory/scoped_ptr.h"
 #include "base/trace_event/trace_event.h"
 #include "content/browser/service_worker/service_worker_context_core.h"
@@ -245,6 +247,12 @@ ServiceWorkerControlleeRequestHandler::DidLookupRegistrationForMainResource(
     return;
   }
 
+  // A registration exists, so associate it. Note that the controller is only
+  // set if there's an active version. If there's no active version, we should
+  // still associate so the provider host can use .ready.
+  provider_host_->AssociateRegistration(registration.get(),
+                                        false /* notify_controllerchange */);
+
   if (!active_version.get() ||
       active_version->status() != ServiceWorkerVersion::ACTIVATED) {
     job_->FallbackToNetwork();
@@ -260,8 +268,6 @@ ServiceWorkerControlleeRequestHandler::DidLookupRegistrationForMainResource(
 
   ServiceWorkerMetrics::CountControlledPageLoad(stripped_url_);
 
-  provider_host_->AssociateRegistration(registration.get(),
-                                        false /* notify_controllerchange */);
   job_->ForwardToServiceWorker();
   TRACE_EVENT_ASYNC_END2(
       "ServiceWorker",
