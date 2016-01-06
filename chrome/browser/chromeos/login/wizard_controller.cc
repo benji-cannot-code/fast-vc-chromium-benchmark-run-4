87ead05c1c590c918e3834747891144208c29724
xdai <xdai@chromium.org>
2016-01-06 15:22:18 -0800

    Clean up unused OOBE exit code HOST_PAIRING_FINISHED.
    BUG=564370
    
    Review URL: https://codereview.chromium.org/1543103002
    
    Cr-Commit-Position: refs/heads/master@{#367949}

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -767,10 +767,6 @@ void WizardController::OnControllerPairingFinished() {
   ShowAutoEnrollmentCheckScreen();
 }
 
-void WizardController::OnHostPairingFinished() {
-  ShowAutoEnrollmentCheckScreen();
-}
-
 void WizardController::OnAutoEnrollmentCheckCompleted() {
   // Check whether the device is disabled. OnDeviceDisabledChecked() will be
   // invoked when the result of this check is known. Until then, the current
@@ -1052,9 +1048,6 @@ void WizardController::OnExit(BaseScreen& /* screen */,
       break;
     case CONTROLLER_PAIRING_FINISHED:
       OnControllerPairingFinished();
-      break;
-    case HOST_PAIRING_FINISHED:
-      OnHostPairingFinished();
       break;
     default:
       NOTREACHED();
