768acd3254fa281e1b573e28c6c76365e005f0c6
haibinlu <haibinlu@chromium.org>
2016-01-06 14:19:19 -0800

    [Blimp Client] Use BlimpMessageThreadPipe to register client features.
    Uses BlimpMessageThreadPipe and its proxy to relay blimp messages between features on the UI thread and network components on the IO thread.
    
    With this CL, blimp messages are being exchanged between client features and engine features.
    
    BUG=568276
    
    Review URL: https://codereview.chromium.org/1568533003
    
    Cr-Commit-Position: refs/heads/master@{#367936}

--- blimp/client/linux/blimp_main.cc
+++ blimp/client/linux/blimp_main.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "blimp/client/session/navigation_feature.h"
 
 namespace {
-const char kDefaultUrl[] = "https://www.google.com";
+const char kDefaultUrl[] = "https://example.com";
 const int kDummyTabId = 0;
 }
 
