01f8f4799bdf7898548d3a9e002e5a78095ef7d9
sunxd <sunxd@chromium.org>
2016-01-06 08:42:54 -0800

    Revert of Determine a layer's IsAffectedByPageScale property without using Layer Tree (patchset #2 id:20001 of https://codereview.chromium.org/1544103002/ )
    Reason for revert:
    Revert the CL to see if it causes an improvement on the waterfall. Related to Issue 573267:	10.3% regression in blink_perf.layout at 367076:367083.
    
    Original issue's description:
    > Determine a layer's IsAffectedByPageScale property without using Layer Tree.
    >
    > The property is retrieved from TransformTree;
    > Fix the order of updating the page scale layer and adding transform tree node;
    > Fix the problem that transform tree sequence number not correctly updated for mask layers;
    > Fix related cc unit tests where a property tree was not created.
    >
    > BUG=568777
    > CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    >
    > Committed: https://crrev.com/73c0fa89da59c8ea5e0d86d1136d7c1a7e68e097
    > Cr-Commit-Position: refs/heads/master@{#367083}
    
    BUG=568777,573267
    
    Review URL: https://codereview.chromium.org/1565783002
    
    Cr-Commit-Position: refs/heads/master@{#367837}

--- cc/layers/heads_up_display_layer_impl_unittest.cc
+++ cc/layers/heads_up_display_layer_impl_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/test/fake_output_surface.h"
 #include "cc/test/test_shared_bitmap_manager.h"
 #include "cc/test/test_task_graph_runner.h"
-#include "cc/trees/layer_tree_impl.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace cc {
@@ -48,19 +47,16 @@ TEST(HeadsUpDisplayLayerImplTest, ResourcelessSoftwareDrawAfterResourceLoss) {
     HeadsUpDisplayLayerImpl::Create(host_impl.pending_tree(), 1);
   layer->SetBounds(gfx::Size(100, 100));
 
-  HeadsUpDisplayLayerImpl* layer_ptr = layer.get();
-
-  host_impl.pending_tree()->SetRootLayer(std::move(layer));
-  host_impl.pending_tree()->BuildPropertyTreesForTesting();
-
   // Check regular hardware draw is ok.
-  CheckDrawLayer(layer_ptr, host_impl.resource_provider(), DRAW_MODE_HARDWARE);
+  CheckDrawLayer(
+      layer.get(), host_impl.resource_provider(), DRAW_MODE_HARDWARE);
 
   // Simulate a resource loss on transitioning to resourceless software mode.
-  layer_ptr->ReleaseResources();
+  layer->ReleaseResources();
 
   // Should skip resourceless software draw and not crash in UpdateHudTexture.
-  CheckDrawLayer(layer_ptr, host_impl.resource_provider(),
+  CheckDrawLayer(layer.get(),
+                 host_impl.resource_provider(),
                  DRAW_MODE_RESOURCELESS_SOFTWARE);
 }
 
