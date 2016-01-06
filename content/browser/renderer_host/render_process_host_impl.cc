6206eba4266d2249142b52b897e19b7a9748e8a8
skobes <skobes@chromium.org>
2016-01-06 14:56:22 -0800

    Enable smooth scrolling.
    BUG=574221
    
    Review URL: https://codereview.chromium.org/1554293002
    
    Cr-Commit-Position: refs/heads/master@{#367942}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1372,6 +1372,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDisableRTCSmoothnessAlgorithm,
     switches::kDisableSeccompFilterSandbox,
     switches::kDisableSharedWorkers,
+    switches::kDisableSmoothScrolling,
     switches::kDisableSpeechAPI,
     switches::kDisableThreadedCompositing,
     switches::kDisableThreadedScrolling,
