5573bb9cc0b0764128305aa6f98686b06e550c57
amistry <amistry@chromium.org>
2016-01-05 16:29:17 -0800

    Implement MessagePumpMojo using WaitSet.
    By using a wait set instead of WaitMany(), the message pump
    no longer needs to do O(n) iterations over every waiting
    handle on every iteration.
    
    BUG=556865
    
    Review URL: https://codereview.chromium.org/1467953002
    
    Cr-Commit-Position: refs/heads/master@{#367716}

--- third_party/mojo/src/mojo/edk/embedder/entrypoints.cc
+++ third_party/mojo/src/mojo/edk/embedder/entrypoints.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/c/system/data_pipe.h"
 #include "mojo/public/c/system/functions.h"
 #include "mojo/public/c/system/message_pipe.h"
+#include "mojo/public/c/system/wait_set.h"
 #include "third_party/mojo/src/mojo/edk/embedder/embedder_internal.h"
 #include "third_party/mojo/src/mojo/edk/system/core.h"
 
