a082c5847746248c3249a796e45be4e4571cdca5
maxbogue <maxbogue@chromium.org>
2016-01-05 16:49:09 -0800

    [Signin] Remove the signInSync param from SigninManager.
    The FRE is the only place that was using SIGNIN_SYNC_SETUP_IN_PROGRESS,
    but HasSyncSetupCompleted should always be false at that time so it is
    unnecessary.
    
    Also remove unnecessary calls to requestStart() (the SigninFlowObserver
    will handle starting sync) and PSS.signOut() (native will handle it).
    
    BUG=567975
    
    Review URL: https://codereview.chromium.org/1520023002
    
    Cr-Commit-Position: refs/heads/master@{#367726}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninPromoScreen.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninPromoScreen.java
@@ -119,8 +119,7 @@ public class SigninPromoScreen
         };
         SigninManager.get(getOwnerActivity().getApplicationContext())
                 .signInToSelectedAccount(getOwnerActivity(), account,
-                        SigninManager.SIGNIN_TYPE_INTERACTIVE,
-                        SigninManager.SIGNIN_SYNC_IMMEDIATELY, signInCallback);
+                        SigninManager.SIGNIN_TYPE_INTERACTIVE, signInCallback);
     }
 
     @Override
--- chrome/android/java/src/org/chromium/chrome/browser/sync/SyncController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/SyncController.java
@@ -19,6 +19,7 @@ import org.chromium.chrome.browser.identity.UniqueIdentificationGenerator;
 import org.chromium.chrome.browser.identity.UniqueIdentificationGeneratorFactory;
 import org.chromium.chrome.browser.invalidation.InvalidationController;
 import org.chromium.chrome.browser.signin.AccountManagementFragment;
+import org.chromium.chrome.browser.signin.SigninManager;
 import org.chromium.chrome.browser.sync.ui.PassphraseActivity;
 import org.chromium.sync.AndroidSyncSettings;
 import org.chromium.sync.ModelType;
@@ -103,6 +104,16 @@ public class SyncController implements ProfileSyncService.SyncStateChangedListen
         if (gmsCoreSyncListener != null) {
             mProfileSyncService.addSyncStateChangedListener(gmsCoreSyncListener);
         }
+
+        SigninManager.get(mContext).addSignInStateObserver(new SigninManager.SignInStateObserver() {
+            @Override
+            public void onSignedIn() {
+                mProfileSyncService.requestStart();
+            }
+
+            @Override
+            public void onSignedOut() {}
+        });
     }
 
     /**
