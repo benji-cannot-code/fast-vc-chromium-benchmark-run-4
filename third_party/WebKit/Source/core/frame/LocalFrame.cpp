c6549d87dd27e7cafdecd19cc750c56d1bba685e
rune <rune@opera.com>
2016-01-06 12:37:10 -0800

    [Printing] Remove unnecessary styleResolverChanged().
    styleResolverChanged is already called for media query changes in the
    following call chain:
    
    FrameView::adjustMediaTypeForPrinting() ->
    FrameView::setMediaType() ->
    Document::mediaQueryAffectingValueChanged() ->
    Document::styleResolverChanged()
    
    Calling it afterwards should not be necessary.
    
    R=mstensho@opera.com
    TEST=printing/print-media-recalc.html
    
    Review URL: https://codereview.chromium.org/1569503002
    
    Cr-Commit-Position: refs/heads/master@{#367895}

--- third_party/WebKit/Source/core/frame/LocalFrame.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrame.cpp
@@ -491,7 +491,6 @@ void LocalFrame::setPrinting(bool printing, const FloatSize& pageSize, const Flo
     document()->setPrinting(printing);
     view()->adjustMediaTypeForPrinting(printing);
 
-    document()->styleResolverChanged();
     if (shouldUsePrintingLayout()) {
         view()->forceLayoutForPagination(pageSize, originalPageSize, maximumShrinkRatio);
     } else {
