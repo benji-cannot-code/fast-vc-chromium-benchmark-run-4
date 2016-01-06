8952c7b5464649dbb1d1e1445d0c0633d335df4d
ruuda <ruuda@google.com>
2016-01-06 04:50:01 -0800

    [FastMalloc]: Annotate CSSSelector for heap profiling
    In https://crrev.com/1474213002, annotations for top consumers of
    renderer memory were added to allow profiling type names in official
    builds. |CSSSelector| was among them, but the selectors allocated by
    |CSSSelectorList| were missed.
    
    BUG=524631
    
    Review URL: https://codereview.chromium.org/1569483002
    
    Cr-Commit-Position: refs/heads/master@{#367816}

--- third_party/WebKit/Source/core/css/CSSSelectorList.cpp
+++ third_party/WebKit/Source/core/css/CSSSelectorList.cpp
@@ -31,6 +31,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Partitions.h"
 #include "wtf/text/StringBuilder.h"
 
+namespace {
+    // CSSSelector is one of the top types that consume renderer memory,
+    // so instead of using the |WTF_HEAP_PROFILER_TYPE_NAME| macro in the
+    // allocations below, pass this type name constant to allow profiling
+    // in official builds.
+    const char kCSSSelectorTypeName[] = "blink::CSSSelector";
+}
+
 namespace blink {
 
 CSSSelectorList CSSSelectorList::copy() const
@@ -38,7 +46,7 @@ CSSSelectorList CSSSelectorList::copy() const
     CSSSelectorList list;
 
     unsigned length = this->length();
-    list.m_selectorArray = reinterpret_cast<CSSSelector*>(WTF::Partitions::fastMalloc(sizeof(CSSSelector) * length, WTF_HEAP_PROFILER_TYPE_NAME(CSSSelector)));
+    list.m_selectorArray = reinterpret_cast<CSSSelector*>(WTF::Partitions::fastMalloc(sizeof(CSSSelector) * length, kCSSSelectorTypeName));
     for (unsigned i = 0; i < length; ++i)
         new (&list.m_selectorArray[i]) CSSSelector(m_selectorArray[i]);
 
@@ -55,7 +63,7 @@ CSSSelectorList CSSSelectorList::adoptSelectorVector(Vector<OwnPtr<CSSParserSele
     ASSERT(flattenedSize);
 
     CSSSelectorList list;
-    list.m_selectorArray = reinterpret_cast<CSSSelector*>(WTF::Partitions::fastMalloc(sizeof(CSSSelector) * flattenedSize, WTF_HEAP_PROFILER_TYPE_NAME(CSSSelector)));
+    list.m_selectorArray = reinterpret_cast<CSSSelector*>(WTF::Partitions::fastMalloc(sizeof(CSSSelector) * flattenedSize, kCSSSelectorTypeName));
     size_t arrayIndex = 0;
     for (size_t i = 0; i < selectorVector.size(); ++i) {
         CSSParserSelector* current = selectorVector[i].get();
