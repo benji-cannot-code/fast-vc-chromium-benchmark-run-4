9d0114f6a4cb6718bda749344c0625b77f74bbc4
agrieve <agrieve@chromium.org>
2016-01-05 19:04:10 -0800

    Fix up proguard output filtering
    Removes build noise when building release on Android.
    
    BUG=573257
    
    Review URL: https://codereview.chromium.org/1565433002
    
    Cr-Commit-Position: refs/heads/master@{#367762}

--- build/android/gyp/apk_obfuscate.py
+++ build/android/gyp/apk_obfuscate.py
@@ -70,6 +70,8 @@ def ParseArgs(argv):
                          'These will not be obfuscated.')
   parser.add_option('--multidex-configuration-path',
                     help='A JSON file containing multidex build configuration.')
+  parser.add_option('--verbose', '-v', action='store_true',
+                    help='Print all proguard output')
 
   (options, args) = parser.parse_args(argv)
 
@@ -119,7 +121,7 @@ def DoProguard(options):
     configs.append(multidex_config)
 
   proguard.configs(configs)
-  proguard.CheckOutput()
+  proguard.CheckOutput(options.verbose)
 
 
 def _PossibleMultidexConfig(options):
