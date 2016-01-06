1a1f7226ea40eed3e22b7b7bb0c03133158881f1
sky <sky@chromium.org>
2016-01-06 12:56:53 -0800

    Fixes bug in pausing connector with multiple messages queued up
    Prior to this patch if there were multiple messages queued up and in
    processing one of the queued messages the connector was paused, we
    would continue processing the messages. After this patch we stop
    processing if paused after dispatch.
    
    BUG=none
    TEST=covered by test
    R=yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/1565763003
    
    Cr-Commit-Position: refs/heads/master@{#367902}

--- mojo/public/cpp/bindings/lib/connector.cc
+++ mojo/public/cpp/bindings/lib/connector.cc
@@ -213,6 +213,7 @@ void Connector::OnHandleReady(MojoResult result) {
 
 void Connector::WaitToReadMore() {
   CHECK(!async_wait_id_);
+  CHECK(!paused_);
   async_wait_id_ = waiter_->AsyncWait(message_pipe_.get().value(),
                                       MOJO_HANDLE_SIGNAL_READABLE,
                                       MOJO_DEADLINE_INDEFINITE,
@@ -221,6 +222,8 @@ void Connector::WaitToReadMore() {
 }
 
 bool Connector::ReadSingleMessage(MojoResult* read_result) {
+  CHECK(!paused_);
+
   bool receiver_result = false;
 
   // Detect if |this| was destroyed during message dispatch. Allow for the
@@ -262,6 +265,9 @@ void Connector::ReadAllAvailableMessages() {
 
     // Return immediately if |this| was destroyed. Do not touch any members!
     if (!ReadSingleMessage(&rv))
+      return;
+
+    if (paused_)
       return;
 
     if (rv == MOJO_RESULT_SHOULD_WAIT) {
