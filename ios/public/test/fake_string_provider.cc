d8a5d0d2b2a5da74d86e0e84673e8d4452c1a115
sdefresne <sdefresne@chromium.org>
2016-01-06 08:42:13 -0800

    Turn StringProvider into an non-abstract class.
    To facilitate removing methods from StringProvider once they are no
    longer used, turn StringProvider into a non-abstract class.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1562063002
    
    Cr-Commit-Position: refs/heads/master@{#367836}

--- ios/public/test/fake_string_provider.cc
+++ ios/public/test/fake_string_provider.cc
@@ -7,62 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ios {
 
-FakeStringProvider::FakeStringProvider() {
-}
+FakeStringProvider::FakeStringProvider() {}
 
-FakeStringProvider::~FakeStringProvider() {
-}
+FakeStringProvider::~FakeStringProvider() {}
 
 std::string FakeStringProvider::GetOmniboxCopyUrlString() {
   return "Copy URL";
-}
-
-base::string16 FakeStringProvider::GetSpoofingInterstitialTitle() {
-  return base::string16();
-}
-
-base::string16 FakeStringProvider::GetSpoofingInterstitialHeadline() {
-  return base::string16();
-}
-
-base::string16 FakeStringProvider::GetSpoofingInterstitialMessage() {
-  return base::string16();
-}
-
-base::string16 FakeStringProvider::GetSpoofingInterstitialDetails() {
-  return base::string16();
-}
-
-base::string16 FakeStringProvider::GetSpoofingInterstitialFailure() {
-  return base::string16();
-}
-
-int FakeStringProvider::GetUnsafePortTitleID() {
-  return 0;
-}
-
-int FakeStringProvider::GetUnsafePortHeadlineID() {
-  return 0;
-}
-
-int FakeStringProvider::GetUnsafePortMessageID() {
-  return 0;
-}
-
-int FakeStringProvider::GetUnsafePortDetailsID() {
-  return 0;
-}
-
-base::string16 FakeStringProvider::GetDoneString() {
-  return base::string16();
-}
-
-base::string16 FakeStringProvider::GetOKString() {
-  return base::string16();
-}
-
-base::string16 FakeStringProvider::GetProductName() {
-  return base::string16();
 }
 
 }  // namespace ios
