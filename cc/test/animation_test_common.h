93ceb10a17c9fafb687decd8932b68420756ff11
loyso <loyso@chromium.org>
2016-01-05 17:36:07 -0800

    CC Animation: Port LayerTests to use animation timelines.
    We preserve the old version of tests so we can fall back at any time.
    
    BUG=394777
    R=ajuma@chromium.org
    R=vollick@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1559183002
    
    Cr-Commit-Position: refs/heads/master@{#367743}

--- cc/test/animation_test_common.h
+++ cc/test/animation_test_common.h
@@ -221,7 +221,13 @@ void AddAnimationToLayerWithExistingPlayer(
     int layer_id,
     scoped_refptr<AnimationTimeline> timeline,
     scoped_ptr<Animation> animation);
+
 void RemoveAnimationFromLayerWithExistingPlayer(
+    int layer_id,
+    scoped_refptr<AnimationTimeline> timeline,
+    int animation_id);
+
+Animation* GetAnimationFromLayerWithExistingPlayer(
     int layer_id,
     scoped_refptr<AnimationTimeline> timeline,
     int animation_id);
