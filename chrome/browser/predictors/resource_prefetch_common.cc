112931a226013af9c966b7461c07473f0057aa95
gabadie <gabadie@chromium.org>
2016-01-07 02:39:39 -0800

    Implements prerendering FINAL_STATUS_{CELLULAR_NETWORK,PRERENDERONG_DISABLED}
    Before, prerender manager was silently ignoring when a adding a
    prerender if prerendering was disabled or the mobile on cellular
    network. This adds the two new prerender final status so these
    cases get also counted on Prerender.*FinalStatus UMA histograms.
    
    BUG=565897
    
    Review URL: https://codereview.chromium.org/1496263003
    
    Cr-Commit-Position: refs/heads/master@{#368046}

--- chrome/browser/predictors/resource_prefetch_common.cc
+++ chrome/browser/predictors/resource_prefetch_common.cc
@@ -228,7 +228,8 @@ bool ResourcePrefetchPredictorConfig::IsURLPrefetchingEnabled(
     Profile* profile) const {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   if (!profile || !profile->GetPrefs() ||
-      !chrome_browser_net::CanPrefetchAndPrerenderUI(profile->GetPrefs())) {
+      chrome_browser_net::CanPrefetchAndPrerenderUI(profile->GetPrefs()) !=
+      chrome_browser_net::NetworkPredictionStatus::ENABLED) {
     return false;
   }
   return (mode & URL_PREFETCHING) > 0;
@@ -238,7 +239,8 @@ bool ResourcePrefetchPredictorConfig::IsHostPrefetchingEnabled(
     Profile* profile) const {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   if (!profile || !profile->GetPrefs() ||
-      !chrome_browser_net::CanPrefetchAndPrerenderUI(profile->GetPrefs())) {
+      chrome_browser_net::CanPrefetchAndPrerenderUI(profile->GetPrefs()) !=
+      chrome_browser_net::NetworkPredictionStatus::ENABLED) {
     return false;
   }
   return (mode & HOST_PRFETCHING) > 0;
