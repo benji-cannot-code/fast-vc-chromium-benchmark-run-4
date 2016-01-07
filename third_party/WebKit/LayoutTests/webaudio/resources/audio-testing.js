f64a2453715e9ce8a91040fb24b81f573dcb84e8
jchaffraix <jchaffraix@chromium.org>
2016-01-07 04:04:44 -0800

    Remove some unused preference
    - dom_paste_allowed and experimental_css_regions_enabled
    were leftovers from earlier removals.
    - WebKitWebAudioEnabled was not used but set.
    - WebKitOfflineWebApplicationCacheEnabled was only used by
    http/tests/appcache/disabled.html, which wasn't testing what
    it said anymore. It tested that swapCache() throws if there is
    no cache to swap with, which is already covered by
    http/tests/appcache/exceptions.html.
    
    Review URL: https://codereview.chromium.org/1561593002
    
    Cr-Commit-Position: refs/heads/master@{#368055}

--- third_party/WebKit/LayoutTests/webaudio/resources/audio-testing.js
+++ third_party/WebKit/LayoutTests/webaudio/resources/audio-testing.js
@@ -1,7 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-if (window.testRunner)
-    testRunner.overridePreference("WebKitWebAudioEnabled", "1");
-
 function writeString(s, a, offset) {
     for (var i = 0; i < s.length; ++i) {
         a[offset + i] = s.charCodeAt(i);
