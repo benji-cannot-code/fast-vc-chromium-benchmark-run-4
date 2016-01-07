e1059411e2dca66f0ffd3bc83a99ced628985de7
tkent <tkent@chromium.org>
2016-01-07 00:56:56 -0800

    testharnessreport: Support missing root element.
    A test may remove <html>.
    
    Review URL: https://codereview.chromium.org/1563153002
    
    Cr-Commit-Position: refs/heads/master@{#368039}

--- third_party/WebKit/LayoutTests/imported/web-platform-tests/resources/testharnessreport.js
+++ third_party/WebKit/LayoutTests/imported/web-platform-tests/resources/testharnessreport.js
@@ -118,8 +118,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             }
 
             // Add results element to document.
-            if (!document.body)
+            if (!document.body) {
+                if (!document.documentElement)
+                    document.appendChild(document.createElement('html'));
                 document.documentElement.appendChild(document.createElement("body"));
+            }
             document.body.appendChild(results);
 
             if (self.testRunner)
