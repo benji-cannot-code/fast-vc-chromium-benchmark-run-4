7b299e5088168773642ee6b9f8a612c7a088b52a
tobiasjs <tobiasjs@chromium.org>
2016-01-06 21:40:12 -0800

    Apply the Surface Texture transformation matrix during texture copy.
    Android Surface Textures have an associated UV transform matrix that can
    potentially vary from frame to frame. If the texture is copied, it is
    useful for the copy to have a consistent orientation, rather than having
    to propagate the UV transformation matrix with the copy as well.
    
    BUG=226218
    
    Review URL: https://codereview.chromium.org/1536713002
    
    Cr-Commit-Position: refs/heads/master@{#368017}

--- content/common/gpu/stream_texture_android.h
+++ content/common/gpu/stream_texture_android.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/android/surface_texture.h"
 #include "ui/gl/gl_image.h"
 
+namespace ui {
+class ScopedMakeCurrent;
+}
+
 namespace gfx {
 class Size;
 }
@@ -57,6 +61,10 @@ class StreamTexture : public gl::GLImage,
   // GpuCommandBufferStub::DestructionObserver implementation.
   void OnWillDestroyStub() override;
 
+  scoped_ptr<ui::ScopedMakeCurrent> MakeStubCurrent();
+
+  void UpdateTexImage();
+
   // Called when a new frame is available for the SurfaceTexture.
   void OnFrameAvailable();
 
@@ -86,6 +94,13 @@ class StreamTexture : public gl::GLImage,
   int32_t route_id_;
   bool has_listener_;
   uint32_t texture_id_;
+
+  unsigned framebuffer_;
+  unsigned vertex_shader_;
+  unsigned fragment_shader_;
+  unsigned program_;
+  unsigned vertex_buffer_;
+  int u_xform_location_;
 
   base::WeakPtrFactory<StreamTexture> weak_factory_;
   DISALLOW_COPY_AND_ASSIGN(StreamTexture);
