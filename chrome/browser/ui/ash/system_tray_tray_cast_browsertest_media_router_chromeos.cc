1f3d0899e79768a990e2cd795aa2c37bd0a0f057
jdufault <jdufault@chromium.org>
2016-01-06 17:54:42 -0800

    Fix crash when initializing CastConfigDelegateMediaRouter.
    Calling CastConfigDelegateMediaRouter::RequestDeviceRefresh allocates a
    CastDeviceCache instance if there isn't one yet. The CastDeviceCache constructor
    ended up invoking CastDeviceCache::OnSinksReceived, which ends up calling
    CastConfigDelegateMediaRouter::RequestDeviceRefresh. Since the CastDeviceCache
    constructor hasn't returned yet, RequestDeviceRefresh creates another
    CastDeviceCache instance. This continues until the stack runs out of space and
    Chrome crashes.
    
    The fix is to separate CastDeviceCache construction and initialization, so that
    CastConfigDelegateMediaRouter::RequestDeviceRefresh updates the CastDeviceCache
    pointer before RequestDeviceRefresh is invoked again. When RequestDeviceRefresh
    gets called from the Init function, it will skip constructing the
    CastDeviceCache instance since it already exists.
    
    BUG=571111
    
    Review URL: https://codereview.chromium.org/1568533004
    
    Cr-Commit-Position: refs/heads/master@{#367984}

--- chrome/browser/ui/ash/system_tray_tray_cast_browsertest_media_router_chromeos.cc
+++ chrome/browser/ui/ash/system_tray_tray_cast_browsertest_media_router_chromeos.cc
@@ -106,7 +106,7 @@ IN_PROC_BROWSER_TEST_F(SystemTrayTrayCastMediaRouterChromeOSTest,
 // Verifies that we only show the tray view if there are available cast
 // targets/sinks.
 IN_PROC_BROWSER_TEST_F(SystemTrayTrayCastMediaRouterChromeOSTest,
-                       DISABLED_VerifyCorrectVisiblityWithSinks) {
+                       VerifyCorrectVisiblityWithSinks) {
   ash::TrayCast* tray = GetTrayCast();
   ash::TrayCastTestAPI test_api(tray);
   EXPECT_TRUE(test_api.IsTrayInitialized());
@@ -146,7 +146,7 @@ IN_PROC_BROWSER_TEST_F(SystemTrayTrayCastMediaRouterChromeOSTest,
 // we display the correct cast session if there are multiple active casting
 // sessions.
 IN_PROC_BROWSER_TEST_F(SystemTrayTrayCastMediaRouterChromeOSTest,
-                       DISABLED_VerifyCastingShowsCastView) {
+                       VerifyCastingShowsCastView) {
   ash::TrayCast* tray = GetTrayCast();
   ash::TrayCastTestAPI test_api(tray);
   EXPECT_TRUE(test_api.IsTrayInitialized());
