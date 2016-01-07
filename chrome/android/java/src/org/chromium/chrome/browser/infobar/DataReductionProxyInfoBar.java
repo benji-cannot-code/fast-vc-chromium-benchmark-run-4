d29537333ac5ca9882ed3868608e628dbecd3b80
dfalcantara <dfalcantara@chromium.org>
2016-01-06 19:05:10 -0800

    Redo the InfoBarLayout measurement algorithm
    * Throws out the old measurement algorithm because the control
      layouts owned by the InfoBarLayout handle most of the wonky
      calculations now.
    
    * Adds an overrideable method to ConfirmInfoBar to allow infobar
      subclasses to add something other than a button to the bottom
      row.  In nearly all cases except app banners, this should never
      be overridden.
    
    * Changes InfoBarControlLayout to use Integer.MAX_VALUE instead
      of an arbitrary maximum width to mimic InfoBarDualControlLayout.
    
    * Fixes DataReductionProxyInfoBar so it builds itself less
      incorrectly.
    
    * Deletes code related to Groups in InfoBarLayout.
    
    * Adds a new method to InfoBarLayout that to generalize what
      Views can be added to the bottom row.
    
    BUG=543205
    
    Review URL: https://codereview.chromium.org/1563663002
    
    Cr-Commit-Position: refs/heads/master@{#367994}

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/DataReductionProxyInfoBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/DataReductionProxyInfoBar.java
@@ -37,11 +37,6 @@ public class DataReductionProxyInfoBar extends ConfirmInfoBar {
     }
 
     @Override
-    public void createContent(InfoBarLayout layout) {
-        layout.setButtons(sLinkText, null);
-    }
-
-    @Override
     public void onButtonClicked(boolean isPrimaryButton) {
         if (!isPrimaryButton) return;
         onLinkClicked();
--- chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBarControlLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBarControlLayout.java
@@ -64,7 +64,6 @@ public final class InfoBarControlLayout extends ViewGroup {
         }
     }
 
-    private final int mMaxInfoBarWidth;
     private final int mMarginBetweenRows;
     private final int mMarginBetweenColumns;
 
@@ -75,7 +74,6 @@ public final class InfoBarControlLayout extends ViewGroup {
         super(context);
 
         Resources resources = context.getResources();
-        mMaxInfoBarWidth = resources.getDimensionPixelSize(R.dimen.infobar_max_width);
         mMarginBetweenRows =
                 resources.getDimensionPixelSize(R.dimen.infobar_control_margin_between_rows);
         mMarginBetweenColumns =
@@ -88,7 +86,7 @@ public final class InfoBarControlLayout extends ViewGroup {
                 : "Height of this layout cannot be constrained.";
 
         int fullWidth = MeasureSpec.getMode(widthMeasureSpec) == MeasureSpec.UNSPECIFIED
-                ? mMaxInfoBarWidth : MeasureSpec.getSize(widthMeasureSpec);
+                ? Integer.MAX_VALUE : MeasureSpec.getSize(widthMeasureSpec);
         int columnWidth = Math.max(0, (fullWidth - mMarginBetweenColumns) / 2);
 
         int atMostFullWidthSpec = MeasureSpec.makeMeasureSpec(fullWidth, MeasureSpec.AT_MOST);
