70674a94423281502901d4ddf08f74ba141839be
zmo <zmo@chromium.org>
2016-01-06 17:58:41 -0800

    Remove glCompressedCopySubTextureCHROMIUM.
    BUG=574278
    TEST=gl_tests,gpu_unittests
    R=piman@chromium.org
    TBR=yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/1565953002
    
    Cr-Commit-Position: refs/heads/master@{#367985}

--- gpu/command_buffer/client/gles2_c_lib_autogen.h
+++ gpu/command_buffer/client/gles2_c_lib_autogen.h
@@ -1368,18 +1368,6 @@ void GL_APIENTRY GLES2CompressedCopyTextureCHROMIUM(GLenum target,
   gles2::GetGLContext()->CompressedCopyTextureCHROMIUM(target, source_id,
                                                        dest_id);
 }
-void GL_APIENTRY GLES2CompressedCopySubTextureCHROMIUM(GLenum target,
-                                                       GLenum source_id,
-                                                       GLenum dest_id,
-                                                       GLint xoffset,
-                                                       GLint yoffset,
-                                                       GLint x,
-                                                       GLint y,
-                                                       GLsizei width,
-                                                       GLsizei height) {
-  gles2::GetGLContext()->CompressedCopySubTextureCHROMIUM(
-      target, source_id, dest_id, xoffset, yoffset, x, y, width, height);
-}
 void GL_APIENTRY GLES2DrawArraysInstancedANGLE(GLenum mode,
                                                GLint first,
                                                GLsizei count,
@@ -2755,11 +2743,6 @@ extern const NameToFunc g_gles2_function_table[] = {
     {
         "glCompressedCopyTextureCHROMIUM",
         reinterpret_cast<GLES2FunctionPointer>(glCompressedCopyTextureCHROMIUM),
-    },
-    {
-        "glCompressedCopySubTextureCHROMIUM",
-        reinterpret_cast<GLES2FunctionPointer>(
-            glCompressedCopySubTextureCHROMIUM),
     },
     {
         "glDrawArraysInstancedANGLE",
