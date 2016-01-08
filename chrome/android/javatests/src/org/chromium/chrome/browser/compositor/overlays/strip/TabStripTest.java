53676c5112935dacbf982537f2c13f9318e09229
twellington <twellington@chromium.org>
2016-01-08 09:47:39 -0800

    Fix and re-enable a few TabStripTests
    Re-eanble 3 disabled tests. Two are no longer failing. The third
    was failing because of a bug in TabStripUtils where dp values
    were being passed to a function that expects px.
    
    BUG=258495
    
    Review URL: https://codereview.chromium.org/1568133002
    
    Cr-Commit-Position: refs/heads/master@{#368368}

--- chrome/android/javatests/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripTest.java
@@ -11,7 +11,6 @@ import android.content.pm.ActivityInfo;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import org.chromium.base.ThreadUtils;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.compositor.layouts.components.CompositorButton;
@@ -126,13 +125,9 @@ public class TabStripTest extends ChromeTabbedActivityTestBase {
      * activates the incognito TabStrip.
      * @throws InterruptedException
      */
-    /*
-     * @LargeTest
-     * @Restriction(RESTRICTION_TYPE_TABLET)
-     * @Feature({"TabStrip"})
-     * Bug crbug.com/258495
-     */
-    @DisabledTest
+    @LargeTest
+    @Restriction(RESTRICTION_TYPE_TABLET)
+    @Feature({"TabStrip"})
     public void testNewIncognitoTabFromMenuAtNormalStrip() throws InterruptedException {
         getInstrumentation().waitForIdleSync();
         assertFalse("Expected normal strip to be selected",
@@ -172,23 +167,21 @@ public class TabStripTest extends ChromeTabbedActivityTestBase {
      * also check that the visible tab ordering is correct.
      * @throws InterruptedException
      */
-    /*
-     * @LargeTest
-     * @Restriction(RESTRICTION_TYPE_TABLET)
-     * @Feature({"TabStrip"})
-     * Bug crbug.com/258495
-     */
-    @DisabledTest
+    @LargeTest
+    @Restriction(RESTRICTION_TYPE_TABLET)
+    @Feature({"TabStrip"})
     public void testSelectWithManyTabs() throws InterruptedException {
         ChromeTabUtils.newTabsFromMenu(getInstrumentation(), getActivity(), 4);
         getInstrumentation().waitForIdleSync();
         assertEquals("The last tab is not selected",
                 getActivity().getCurrentTabModel().index(), 4);
         compareAllTabStripsWithModel();
-        selectTab(false, getActivity().getCurrentTabModel().getTabAt(2).getId());
+        // Note: if the tab is not visible, this will fail. Currently that's not a problem, because
+        // the devices we test on are wide enough.
+        selectTab(false, getActivity().getCurrentTabModel().getTabAt(0).getId());
         getInstrumentation().waitForIdleSync();
         assertEquals("The middle tab is not selected",
-                getActivity().getCurrentTabModel().index(), 2);
+                getActivity().getCurrentTabModel().index(), 0);
         compareAllTabStripsWithModel();
     }
 
@@ -228,8 +221,6 @@ public class TabStripTest extends ChromeTabbedActivityTestBase {
     @Restriction(RESTRICTION_TYPE_TABLET)
     @Feature({"TabStrip"})
     public void testCloseTabWithManyTabs() throws InterruptedException {
-        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
         ChromeTabUtils.newTabsFromMenu(getInstrumentation(), getActivity(), 4);
         getInstrumentation().waitForIdleSync();
         assertEquals("There are not five tabs present",
@@ -237,7 +228,9 @@ public class TabStripTest extends ChromeTabbedActivityTestBase {
         assertEquals("The last tab is not selected",
                 getActivity().getCurrentTabModel().index(), 4);
         int initialSelectedId = getActivity().getActivityTab().getId();
-        closeTab(false, getActivity().getCurrentTabModel().getTabAt(2).getId());
+        // Note: if the tab is not visible, this will fail. Currently that's not a problem, because
+        // the devices we test on are wide enough.
+        closeTab(false, getActivity().getCurrentTabModel().getTabAt(0).getId());
         getInstrumentation().waitForIdleSync();
         assertEquals("There are not four tabs present",
                 getActivity().getCurrentTabModel().getCount(), 4);
@@ -352,13 +345,10 @@ public class TabStripTest extends ChromeTabbedActivityTestBase {
      * and normal TabStrips.
      * @throws InterruptedException
      */
-    /*
-     * @LargeTest
-     * @Restriction(RESTRICTION_TYPE_TABLET)
-     * @Feature({"TabStrip"})
-     * Bug crbug.com/258495
-     */
-    @DisabledTest
+
+    @LargeTest
+    @Restriction(RESTRICTION_TYPE_TABLET)
+    @Feature({"TabStrip"})
     public void testToggleIncognitoMode() throws InterruptedException {
         getInstrumentation().waitForIdleSync();
         assertFalse("Expected normal strip to be selected",
--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/TabStripUtils.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/TabStripUtils.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.test.util;
 
-import android.view.View;
-
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.compositor.layouts.components.CompositorButton;
 import org.chromium.chrome.browser.compositor.overlays.strip.StripLayoutHelper;
@@ -15,7 +13,6 @@ import org.chromium.chrome.browser.compositor.overlays.strip.StripLayoutTab;
 import org.chromium.chrome.test.ChromeTabbedActivityTestBase;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
-import org.chromium.content.browser.test.util.TestTouchUtils;
 
 /**
  * A utility class that contains methods generic to all TabStrip test classes.
@@ -73,19 +70,24 @@ public class TabStripUtils {
     }
 
     /**
-     * Click a compositor tab strip tab;
+     * Click a compositor tab strip tab.
      * @param tab The tab to click.
      * @param base The ChromeTabbedActivityTestBase where we're calling this from.
      */
     public static void clickTab(StripLayoutTab tab, ChromeTabbedActivityTestBase base) {
-        View view = base.getActivity().getTabsView();
-        final float x = tab.getDrawX() + tab.getWidth() / 2;
-        final float y = tab.getDrawY() + tab.getHeight() / 2;
-        TestTouchUtils.singleClickView(base.getInstrumentation(), view, (int) x, (int) y);
+        final StripLayoutHelperManager manager = getStripLayoutHelperManager(base.getActivity());
+        final float x = (tab.getDrawX() + tab.getWidth() / 2);
+        final float y = (tab.getDrawY() + tab.getHeight() / 2);
+        base.getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                manager.click(0, x, y, false, 0);
+            }
+        });
     }
 
     /**
-     * Click a compositor button;
+     * Click a compositor button.
      * @param button The button to click.
      * @param base The ChromeTabbedActivityTestBase where we're calling this from.
      */
