cfebf0580577936010ed92dc0923aba118f05ee4
sadrul <sadrul@chromium.org>
2016-01-08 15:49:15 -0800

    mus: Fix activation cycle direction.
    When moving activation (e.g. either because of window-switch accelerator,
    or when the active window is destroyed/hidden), make sure the previous
    active window is activated first, instead of the oldest active window.
    However, when explicitly cycling through the active windows (e.g. by using
    the window-cycle accelerator), make sure it cycles through all activatable
    windows, instead of just the two topmost windows.
    
    BUG=548422
    
    Review URL: https://codereview.chromium.org/1560063003
    
    Cr-Commit-Position: refs/heads/master@{#368460}

--- components/mus/ws/focus_controller.h
+++ components/mus/ws/focus_controller.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
 #include "components/mus/ws/server_window_drawn_tracker_observer.h"
+#include "components/mus/ws/server_window_tracker.h"
 
 namespace mus {
 
@@ -19,6 +20,7 @@ class FocusControllerDelegate;
 class FocusControllerObserver;
 class ServerWindow;
 class ServerWindowDrawnTracker;
+struct WindowId;
 
 // Describes the source of the change.
 enum class FocusControllerChangeSource {
@@ -45,7 +47,13 @@ class FocusController : public ServerWindowDrawnTrackerObserver {
   void RemoveObserver(FocusControllerObserver* observer);
 
  private:
-  void SetActiveWindow(ServerWindow* window);
+  enum class ActivationChangeReason {
+    UNKNONW,
+    CYCLE,  // Activation changed because of ActivateNextWindow().
+    FOCUS,  // Focus change required a different window to be activated.
+    DRAWN_STATE_CHANGED,  // Active window was hidden or destroyed.
+  };
+  void SetActiveWindow(ServerWindow* window, ActivationChangeReason reason);
 
   // Returns whether |window| can be focused or activated.
   bool CanBeFocused(ServerWindow* window) const;
@@ -72,6 +80,13 @@ class FocusController : public ServerWindowDrawnTrackerObserver {
   ServerWindow* root_;
   ServerWindow* focused_window_;
   ServerWindow* active_window_;
+  // Tracks what caused |active_window_| to be activated.
+  ActivationChangeReason activation_reason_;
+
+  // Keeps track of the list of windows that have already been visited during a
+  // window cycle. This is only active when |activation_reason_| is set to
+  // CYCLE.
+  scoped_ptr<ServerWindowTracker> cycle_windows_;
 
   base::ObserverList<FocusControllerObserver> observers_;
 
