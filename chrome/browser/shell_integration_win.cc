bb6cb60c51486d9237ccc6fe4bbe96dc996fb052
pmonette <pmonette@chromium.org>
2016-01-08 12:18:58 -0800

    Revert of Removed finch configuration for windows 10 set as default flow (patchset #3 id:80001 of https://codereview.chromium.org/1442483004/ )
    Reason for revert:
    Still need ability to manually enable and disable.
    
    Original issue's description:
    > Removed finch configuration for windows 10 set as default flow
    >
    > BUG=531649
    >
    > Committed: https://crrev.com/09779a91a370f923421e3114794957c2c006ff5f
    > Cr-Commit-Position: refs/heads/master@{#359853}
    
    TBR=grt@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=531649
    
    Review URL: https://codereview.chromium.org/1564923006
    
    Cr-Commit-Position: refs/heads/master@{#368423}

--- chrome/browser/shell_integration_win.cc
+++ chrome/browser/shell_integration_win.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/macros.h"
 #include "base/message_loop/message_loop.h"
+#include "base/metrics/field_trial.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/path_service.h"
 #include "base/process/launch.h"
@@ -46,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/util/util_constants.h"
 #include "chrome/installer/util/work_item.h"
 #include "chrome/installer/util/work_item_list.h"
+#include "components/variations/variations_associated_data.h"
 #include "content/public/browser/browser_thread.h"
 
 using content::BrowserThread;
@@ -53,6 +55,10 @@ using content::BrowserThread;
 namespace {
 
 const wchar_t kAppListAppNameSuffix[] = L"AppList";
+
+const char kAsyncSetAsDefaultExperimentName[] = "AsyncSetAsDefault";
+const char kEnableAsyncSetAsDefault[] = "enable-async-set-as-default";
+const char kDisableAsyncSetAsDefault[] = "disable-async-set-as-default";
 
 // Helper function for ShellIntegration::GetAppId to generates profile id
 // from profile path. "profile_id" is composed of sanitized basenames of
@@ -267,6 +273,22 @@ void ResetDefaultBrowser() {
   }
 }
 
+// Returns true if the AsyncSetAsDefault field trial is activated.
+bool IsAsyncSetAsDefaultEnabled() {
+  using base::CommandLine;
+
+  // Note: It's important to query the field trial state first, to ensure that
+  // UMA reports the correct group.
+  const std::string group_name =
+      base::FieldTrialList::FindFullName("AsyncSetAsDefault");
+  if (CommandLine::ForCurrentProcess()->HasSwitch(kDisableAsyncSetAsDefault))
+    return false;
+  if (CommandLine::ForCurrentProcess()->HasSwitch(kEnableAsyncSetAsDefault))
+    return true;
+
+  return base::StartsWith(group_name, "Enabled", base::CompareCase::SENSITIVE);
+}
+
 bool RegisterBrowser() {
   base::FilePath chrome_exe;
   if (!PathService::Get(base::FILE_EXE, &chrome_exe)) {
@@ -283,7 +305,8 @@ bool RegisterBrowser() {
 
 // static
 bool ShellIntegration::IsSetAsDefaultAsynchronous() {
-  return base::win::GetVersion() >= base::win::VERSION_WIN10;
+  return base::win::GetVersion() >= base::win::VERSION_WIN10 &&
+         IsAsyncSetAsDefaultEnabled();
 }
 
 bool ShellIntegration::SetAsDefaultBrowser() {
@@ -648,7 +671,8 @@ bool ShellIntegration::DefaultBrowserWorker::InitializeSetAsDefault() {
   //    instead call OnSetAsDefaultAttemptComplete(), passing true to indicate
   //    success.
   // 5. If Chrome is not selected, the url is opened in the selected browser.
-  //    After two minutes, we notify the observer that the process failed.
+  //    After a certain amount of time, we notify the observer that the
+  //    process failed.
 
   if (!StartupBrowserCreator::SetDefaultBrowserCallback(
           base::Bind(&DefaultBrowserWorker::OnSetAsDefaultAttemptComplete, this,
@@ -666,8 +690,15 @@ bool ShellIntegration::DefaultBrowserWorker::InitializeSetAsDefault() {
   // Start the timer.
   if (!async_timer_)
     async_timer_.reset(new base::OneShotTimer());
+  std::string value = variations::GetVariationParamValue(
+      kAsyncSetAsDefaultExperimentName, "TimerDuration");
+  int seconds = 0;
+  if (!value.empty())
+    base::StringToInt(value, &seconds);
+  if (!seconds)
+    seconds = 120;  // Default value of 2 minutes.
   async_timer_->Start(
-      FROM_HERE, base::TimeDelta::FromMinutes(2),
+      FROM_HERE, base::TimeDelta::FromSeconds(seconds),
       base::Bind(&DefaultBrowserWorker::OnSetAsDefaultAttemptComplete, this,
                  AttemptResult::FAILURE));
   return true;
