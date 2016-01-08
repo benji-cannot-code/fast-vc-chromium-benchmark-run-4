7f397dd976ede2ac2395873ba5082d6de133f4f4
dnicoara <dnicoara@chromium.org>
2016-01-08 10:27:12 -0800

    Fix Ozone GYP config to match GN and fix CrOS build
    The simple chromeos build was broken in
    https://codereview.chromium.org/1562843003/ as a result of export rules.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1570513006
    
    Cr-Commit-Position: refs/heads/master@{#368380}

--- chrome/chrome_browser_chromeos.gypi
+++ chrome/chrome_browser_chromeos.gypi
@@ -1271,6 +1271,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         ['use_ozone == 1', {
           'dependencies': [
             '../ui/ozone/ozone.gyp:ozone',
+            '../ui/ozone/ozone.gyp:ozone_base',
           ],
         }],
       ],
--- ui/ozone/common/gpu/ozone_gpu_message_params.h
+++ ui/ozone/common/gpu/ozone_gpu_message_params.h
@@ -16,12 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/overlay_transform.h"
-#include "ui/ozone/ozone_export.h"
+#include "ui/ozone/ozone_base_export.h"
 #include "ui/ozone/public/overlay_candidates_ozone.h"
 
 namespace ui {
 
-struct OZONE_EXPORT DisplayMode_Params {
+struct OZONE_BASE_EXPORT DisplayMode_Params {
   DisplayMode_Params();
   ~DisplayMode_Params();
 
@@ -30,7 +30,7 @@ struct OZONE_EXPORT DisplayMode_Params {
   float refresh_rate = 0.0f;
 };
 
-struct OZONE_EXPORT DisplaySnapshot_Params {
+struct OZONE_BASE_EXPORT DisplaySnapshot_Params {
   DisplaySnapshot_Params();
   ~DisplaySnapshot_Params();
 
@@ -50,7 +50,7 @@ struct OZONE_EXPORT DisplaySnapshot_Params {
   std::string string_representation;
 };
 
-struct OZONE_EXPORT OverlayCheck_Params {
+struct OZONE_BASE_EXPORT OverlayCheck_Params {
   OverlayCheck_Params();
   OverlayCheck_Params(
       const OverlayCandidatesOzone::OverlaySurfaceCandidate& candidate);
