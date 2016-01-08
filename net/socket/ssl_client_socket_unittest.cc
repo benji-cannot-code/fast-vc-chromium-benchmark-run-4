5a2f382e1d1913ec410a4f37a5298ad27edf839f
nharper <nharper@chromium.org>
2016-01-08 12:45:17 -0800

    Replace core of DER parsing in net/cert/asn1_util with net/der
    BUG=573907
    
    Review URL: https://codereview.chromium.org/1560083002
    
    Cr-Commit-Position: refs/heads/master@{#368432}

--- net/socket/ssl_client_socket_unittest.cc
+++ net/socket/ssl_client_socket_unittest.cc
@@ -25,6 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/ct_verifier.h"
 #include "net/cert/mock_cert_verifier.h"
 #include "net/cert/test_root_certs.h"
+#include "net/der/input.h"
+#include "net/der/parser.h"
+#include "net/der/tag.h"
 #include "net/dns/host_resolver.h"
 #include "net/http/transport_security_state.h"
 #include "net/log/net_log.h"
@@ -2381,15 +2384,14 @@ TEST_F(SSLClientSocketTest, EVCertStatusRemovedForNonCompliantCert) {
 namespace {
 
 bool IsValidOCSPResponse(const base::StringPiece& input) {
-  base::StringPiece ocsp_response = input;
-  base::StringPiece sequence, response_status, response_bytes;
-  return asn1::GetElement(&ocsp_response, asn1::kSEQUENCE, &sequence) &&
-      ocsp_response.empty() &&
-      asn1::GetElement(&sequence, asn1::kENUMERATED, &response_status) &&
-      asn1::GetElement(&sequence,
-                       asn1::kContextSpecific | asn1::kConstructed | 0,
-                       &response_status) &&
-      sequence.empty();
+  der::Parser parser(der::Input(reinterpret_cast<const uint8_t*>(input.data()),
+                                input.length()));
+  der::Parser sequence;
+  return parser.ReadSequence(&sequence) && !parser.HasMore() &&
+         sequence.SkipTag(der::kEnumerated) &&
+         sequence.SkipTag(der::kTagContextSpecific | der::kTagConstructed |
+                          0) &&
+         !sequence.HasMore();
 }
 
 }  // namespace
