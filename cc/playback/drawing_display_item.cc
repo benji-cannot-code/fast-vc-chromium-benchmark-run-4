a93bb0f1e79122b89b580d4989543d57589e5945
jbroman <jbroman@chromium.org>
2016-01-08 14:07:22 -0800

    Make cc::DrawingDisplayItem hold a const SkPicture.
    This lets us remove const_cast<SkPicture*> from callers (i.e. Blink) which
    hold SkPicture through const pointers.
    
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1564233005
    
    Cr-Commit-Position: refs/heads/master@{#368444}

--- cc/playback/drawing_display_item.cc
+++ cc/playback/drawing_display_item.cc
@@ -26,7 +26,7 @@ namespace cc {
 
 DrawingDisplayItem::DrawingDisplayItem() {}
 
-DrawingDisplayItem::DrawingDisplayItem(skia::RefPtr<SkPicture> picture) {
+DrawingDisplayItem::DrawingDisplayItem(skia::RefPtr<const SkPicture> picture) {
   SetNew(std::move(picture));
 }
 
@@ -52,7 +52,7 @@ DrawingDisplayItem::DrawingDisplayItem(const DrawingDisplayItem& item) {
 DrawingDisplayItem::~DrawingDisplayItem() {
 }
 
-void DrawingDisplayItem::SetNew(skia::RefPtr<SkPicture> picture) {
+void DrawingDisplayItem::SetNew(skia::RefPtr<const SkPicture> picture) {
   picture_ = std::move(picture);
 }
 
