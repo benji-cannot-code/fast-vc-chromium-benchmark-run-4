e439663c055536c74997c9b96eef8041acca80ba
ryoh <ryoh@chromium.org>
2016-01-07 17:42:36 -0800

    Turn off Editor when the edited file was removed.
    If a user removes the file which is being shown, Gallery.app moves another image.
    If the removed image was being edited, Gallery.app will be in edit mode at another image, but it is no appropriate.
    
    So, we must leave the edited mode when the edited image is deleted.
    
    BUG=548593
    TEST=manually
    
    Review URL: https://codereview.chromium.org/1567253002
    
    Cr-Commit-Position: refs/heads/master@{#368240}

--- ui/file_manager/gallery/js/slide_mode.js
+++ ui/file_manager/gallery/js/slide_mode.js
@@ -874,6 +874,8 @@ SlideMode.prototype.onSplice_ = function(event) {
         this.printButton_.disabled = true;
         this.editButton_.disabled = true;
         this.errorBanner_.show('GALLERY_NO_IMAGES');
+        if (this.isEditing())
+          this.toggleEditor();
       }.bind(this));
       return;
     }
@@ -887,6 +889,9 @@ SlideMode.prototype.onSplice_ = function(event) {
       // To force to dispatch a selection change event, unselect all before.
       this.selectionModel_.unselectAll();
       this.select(nextIndex);
+      // If the removed image was edit, leave the editing mode.
+      if (this.isEditing())
+        this.toggleEditor();
     }
   }.bind(this), 0);
 };
