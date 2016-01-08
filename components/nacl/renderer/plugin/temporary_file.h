947ccc1f5ef46bd29b7f8b359e73a6050422d134
mseaborn <mseaborn@chromium.org>
2016-01-08 15:53:46 -0800

    PNaCl: Remove now-unneeded use of nacl::DescWrapper in TempFile
    read_wrapper() and write_wrapper() are no longer used now that the
    PNaCl code doesn't use SRPC.
    
    Open() had the side effect of checking the validity of the
    handle/descriptor before, so preserve that, but rename it to
    CheckValidity() to clarify what it does.
    
    BUG=302078
    TEST=e.g. NaClBrowserTestPnacl.PPAPICore (tests PNaCl translation)
    
    Review URL: https://codereview.chromium.org/1575573002
    
    Cr-Commit-Position: refs/heads/master@{#368467}

--- components/nacl/renderer/plugin/temporary_file.h
+++ components/nacl/renderer/plugin/temporary_file.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
 #include "native_client/src/include/nacl_macros.h"
-#include "native_client/src/trusted/desc/nacl_desc_wrapper.h"
 
 #include "ppapi/c/private/pp_file_handle.h"
 
@@ -45,24 +44,14 @@ class TempFile {
   TempFile(Plugin* plugin, PP_FileHandle handle);
   ~TempFile();
 
-  // Opens a temporary file object and descriptor wrapper referring to the file.
-  // If |writeable| is true, the descriptor will be opened for writing, and
-  // write_wrapper will return a valid pointer, otherwise it will return NULL.
-  int32_t Open(bool writeable);
+  int32_t CheckValidity();
   // Resets file position of the handle, for reuse.
   bool Reset();
 
   // Returns the current size of this file.
   int64_t GetLength();
 
-  // Accessors.
-  // The nacl::DescWrapper* for the writeable version of the file.
-  nacl::DescWrapper* write_wrapper() { return write_wrapper_.get(); }
-  nacl::DescWrapper* read_wrapper() { return read_wrapper_.get(); }
-
-  // Returns a handle to the file, transferring ownership of it.  Note that
-  // the objects returned by write_wrapper() and read_wrapper() remain
-  // valid after TakeFileHandle() is called.
+  // Returns a handle to the file, transferring ownership of it.
   PP_FileHandle TakeFileHandle();
 
   // Returns a handle to the file, without transferring ownership of it.
@@ -73,11 +62,7 @@ class TempFile {
  private:
   NACL_DISALLOW_COPY_AND_ASSIGN(TempFile);
 
-  nacl::DescWrapper* MakeDescWrapper(int nacl_file_flags);
-
   Plugin* plugin_;
-  scoped_ptr<nacl::DescWrapper> read_wrapper_;
-  scoped_ptr<nacl::DescWrapper> write_wrapper_;
   base::File file_handle_;
 };
 
