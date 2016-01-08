e2ef43fd9482d168886f40656a74a16689ccbbce
adamk <adamk@chromium.org>
2016-01-08 09:44:57 -0800

    Mark Object.observe as deprecated
    Optimistically targetting M50 at the moment.
    
    Note that V8 only calls out to Blink once per Object.observe call per realm, but
    that seems to already match Blink's deprecation message behavior so I don't
    believe any changes are required on the V8 side.
    
    R=jochen@chromium.org
    BUG=552100
    
    Review URL: https://codereview.chromium.org/1566173003
    
    Cr-Commit-Position: refs/heads/master@{#368364}

--- third_party/WebKit/Source/bindings/core/v8/V8PerIsolateData.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8PerIsolateData.cpp
@@ -57,6 +57,7 @@ static bool runningUnitTest()
 static void useCounterCallback(v8::Isolate* isolate, v8::Isolate::UseCounterFeature feature)
 {
     UseCounter::Feature blinkFeature;
+    bool deprecated = false;
     switch (feature) {
     case v8::Isolate::kUseAsm:
         blinkFeature = UseCounter::UseAsm;
@@ -69,6 +70,7 @@ static void useCounterCallback(v8::Isolate* isolate, v8::Isolate::UseCounterFeat
         break;
     case v8::Isolate::kObjectObserve:
         blinkFeature = UseCounter::ObjectObserve;
+        deprecated = true;
         break;
     case v8::Isolate::kSloppyMode:
         blinkFeature = UseCounter::V8SloppyMode;
@@ -90,7 +92,10 @@ static void useCounterCallback(v8::Isolate* isolate, v8::Isolate::UseCounterFeat
         // does not know about. It's harmless.
         return;
     }
-    UseCounter::count(callingExecutionContext(isolate), blinkFeature);
+    if (deprecated)
+        UseCounter::countDeprecation(callingExecutionContext(isolate), blinkFeature);
+    else
+        UseCounter::count(callingExecutionContext(isolate), blinkFeature);
 }
 
 V8PerIsolateData::V8PerIsolateData()
