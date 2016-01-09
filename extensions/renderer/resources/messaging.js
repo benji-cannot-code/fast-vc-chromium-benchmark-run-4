dab71c9ef04917ffc316c67a2ea6708f4e03c3e2
rob <rob@robwu.nl>
2016-01-08 16:07:45 -0800

    Ensure that sendMessage's callback is called.
    Also: unchecked errors are now also reported to the extension's console.
    Unchecked errors were silenced before because the internal disconnect
    handler accessed chrome.runtime.lastError.
    
    BUG=439780
    TEST=browser_tests --gtest_filter=ExtensionApiTest.MessagingCrash
    
    Review URL: https://codereview.chromium.org/1567423002
    
    Cr-Commit-Position: refs/heads/master@{#368469}

--- extensions/renderer/resources/messaging.js
+++ extensions/renderer/resources/messaging.js
@@ -336,9 +336,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       return;
     }
 
-    // Ensure the callback exists for the older sendRequest API.
-    if (!responseCallback)
-      responseCallback = function() {};
+    function sendResponseAndClearCallback(response) {
+      // Save a reference so that we don't re-entrantly call responseCallback.
+      var sendResponse = responseCallback;
+      responseCallback = null;
+      sendResponse(response);
+    }
+
 
     // Note: make sure to manually remove the onMessage/onDisconnect listeners
     // that we added before destroying the Port, a workaround to a bug in Port
@@ -347,14 +351,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // http://crbug.com/320723 tracks a sustainable fix.
 
     function disconnectListener() {
-      // For onDisconnects, we only notify the callback if there was an error.
-      if (chrome.runtime && chrome.runtime.lastError)
-        responseCallback();
+      if (!responseCallback)
+        return;
+
+      if (lastError.hasError(chrome)) {
+        sendResponseAndClearCallback();
+      } else {
+        lastError.set(
+            port.name, 'The message port closed before a reponse was received.',
+            null, chrome);
+        try {
+          sendResponseAndClearCallback();
+        } finally {
+          lastError.clear(chrome);
+        }
+      }
     }
 
     function messageListener(response) {
       try {
-        responseCallback(response);
+        if (responseCallback)
+          sendResponseAndClearCallback(response);
       } finally {
         port.disconnect();
       }
