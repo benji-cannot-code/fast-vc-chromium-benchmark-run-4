38068fa62929876967c7c966924701e2bac78dd9
shuchen <shuchen@chromium.org>
2016-01-08 18:18:48 -0800

    Makes the in-activatable IME window on Windows can receive mouse events.
    This is to support IME window for the IME extension API:
    input.ime.createWindow.
    The IME window needs to be in-activatable but can still be dragged and
    clicked.
    
    BUG=517773
    TEST=None
    
    Review URL: https://codereview.chromium.org/1565713002
    
    Cr-Commit-Position: refs/heads/master@{#368510}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_win.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_win.cc
@@ -624,6 +624,10 @@ bool DesktopWindowTreeHostWin::CanActivate() const {
   return native_widget_delegate_->CanActivate();
 }
 
+bool DesktopWindowTreeHostWin::WantsMouseEventsWhenInactive() const {
+  return false;
+}
+
 bool DesktopWindowTreeHostWin::WidgetSizeIsClientSize() const {
   const Widget* widget = GetWidget()->GetTopLevelWidget();
   return IsMaximized() || (widget && widget->ShouldUseNativeFrame());
