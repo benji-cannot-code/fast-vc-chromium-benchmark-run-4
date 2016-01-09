0a67377b5267578d55d67e1a162da61e301e765c
gfhuang <gfhuang@chromium.org>
2016-01-08 16:43:52 -0800

    [chromecast] Smooth out OnNetworkChanged event to avoid rapid flipping between setup state 50 and 60.
    BUG=internal b/26017446
    
    Review URL: https://codereview.chromium.org/1496323003
    
    Cr-Commit-Position: refs/heads/master@{#368482}

--- chromecast/net/connectivity_checker_impl.h
+++ chromecast/net/connectivity_checker_impl.h
@@ -58,6 +58,8 @@ class ConnectivityCheckerImpl
   void OnNetworkChanged(
       net::NetworkChangeNotifier::ConnectionType type) override;
 
+  void OnNetworkChangedInternal();
+
   // Cancels current connectivity checking in progress.
   void Cancel();
 
@@ -75,8 +77,10 @@ class ConnectivityCheckerImpl
   scoped_ptr<net::URLRequest> url_request_;
   const scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   bool connected_;
+  net::NetworkChangeNotifier::ConnectionType connection_type_;
   // Number of connectivity check errors.
   unsigned int check_errors_;
+  bool network_changed_pending_;
   // Timeout handler for connectivity checks.
   // Note: Cancelling this timeout can cause the destructor for this class to be
   //       to be called.
