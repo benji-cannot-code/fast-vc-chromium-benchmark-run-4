7fcc97fed3b31f4f7e2f620a9a924dd14569540c
amistry <amistry@chromium.org>
2016-01-08 19:30:06 -0800

    Ensure ThreatDetails is always destroyed on the UI thread.
    Since ThreatDetails subclasses content::WebContentsObserver, it must be
    destroyed on the UI thread since ~WebContentsObserver unregisters itself
    from a WebContents.
    
    Review URL: https://codereview.chromium.org/1569993003
    
    Cr-Commit-Position: refs/heads/master@{#368518}

--- chrome/browser/safe_browsing/threat_details.h
+++ chrome/browser/safe_browsing/threat_details.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/safe_browsing/ui_manager.h"
 #include "chrome/common/safe_browsing/csd.pb.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "net/base/completion_callback.h"
 
@@ -45,7 +46,9 @@ typedef base::hash_map<
     linked_ptr<ClientSafeBrowsingReportRequest::Resource>>
     ResourceMap;
 
-class ThreatDetails : public base::RefCountedThreadSafe<ThreatDetails>,
+class ThreatDetails : public base::RefCountedThreadSafe<
+                          ThreatDetails,
+                          content::BrowserThread::DeleteOnUIThread>,
                       public content::WebContentsObserver {
  public:
   typedef SafeBrowsingUIManager::UnsafeResource UnsafeResource;
@@ -99,6 +102,9 @@ class ThreatDetails : public base::RefCountedThreadSafe<ThreatDetails>,
 
  private:
   friend class base::RefCountedThreadSafe<ThreatDetails>;
+  friend struct content::BrowserThread::DeleteOnThread<
+      content::BrowserThread::UI>;
+  friend class base::DeleteHelper<ThreatDetails>;
 
   // Starts the collection of the report.
   void StartCollection();
