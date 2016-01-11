01e0dac83d1a24aad65fe1a7ea235cc23048b65a
tfarina <tfarina@chromium.org>
2016-01-11 11:55:01 -0800

    components: fix the inclusion of some header files
    These header files are not system header files and thus do not need to
    be included using <> rather than "".
    
    This was reported by Gergely Nagy at gn-dev mailing list:
    https://groups.google.com/a/chromium.org/d/topic/gn-dev/JIM6OF6J10E/discussion
    
    BUG=None
    R=rkc@chromium.org,vabr@chromium.org,sorin@chromium.org
    
    Review URL: https://codereview.chromium.org/1575783002
    
    Cr-Commit-Position: refs/heads/master@{#368644}

--- components/update_client/utils.h
+++ components/update_client/utils.h
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_UPDATE_CLIENT_UTILS_H_
 
 #include <string>
-#include <base/memory/scoped_ptr.h>
+
+#include "base/memory/scoped_ptr.h"
 
 class GURL;
 
