5aa50a97f4ffc0c6c00cf5f24e59fc4d8a1bf8c3
mseaborn <mseaborn@chromium.org>
2016-01-11 14:38:32 -0800

    NaCl cleanup: Remove now-unneeded uses of BrokerDuplicateHandle()
    Now that we no longer use SRPC, we no longer need to supply an
    implementation of BrokerDuplicateHandle() for SRPC/IMC to use.
    
    BUG=302078
    TEST=e.g. NaClBrowserTestPnacl.PPAPICore (tests PNaCl translation)
    
    Review URL: https://codereview.chromium.org/1575453004
    
    Cr-Commit-Position: refs/heads/master@{#368695}

--- components/nacl/renderer/plugin/module_ppapi.cc
+++ components/nacl/renderer/plugin/module_ppapi.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/nacl/renderer/plugin/module_ppapi.h"
 #include "components/nacl/renderer/plugin/plugin.h"
 #include "components/nacl/renderer/plugin/utility.h"
-#include "native_client/src/shared/imc/nacl_imc_c.h"
 #include "native_client/src/shared/platform/nacl_secure_random.h"
 #include "native_client/src/shared/platform/nacl_time.h"
 #include "native_client/src/trusted/desc/nrd_all_modules.h"
@@ -53,10 +52,6 @@ bool ModulePpapi::Init() {
   // In the plugin, we don't need high resolution time of day.
   NaClAllowLowResolutionTimeOfDay();
   NaClNrdAllModulesInit();
-
-#if NACL_WINDOWS
-  NaClSetBrokerDuplicateHandleFunc(private_interface_->BrokerDuplicateHandle);
-#endif
 
   init_was_successful_ = true;
   return true;
--- components/nacl/renderer/ppb_nacl_private_impl.cc
+++ components/nacl/renderer/ppb_nacl_private_impl.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/nacl/renderer/trusted_plugin_channel.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_switches.h"
-#include "content/public/common/sandbox_init.h"
 #include "content/public/renderer/pepper_plugin_instance.h"
 #include "content/public/renderer/render_thread.h"
 #include "content/public/renderer/render_view.h"
@@ -634,20 +633,6 @@ int UrandomFD(void) {
   return base::GetUrandomFD();
 #else
   return -1;
-#endif
-}
-
-int32_t BrokerDuplicateHandle(PP_FileHandle source_handle,
-                              uint32_t process_id,
-                              PP_FileHandle* target_handle,
-                              uint32_t desired_access,
-                              uint32_t options) {
-#if defined(OS_WIN)
-  return content::BrokerDuplicateHandle(source_handle, process_id,
-                                        target_handle, desired_access,
-                                        options);
-#else
-  return 0;
 #endif
 }
 
@@ -1728,7 +1713,6 @@ void StreamPexe(PP_Instance instance,
 const PPB_NaCl_Private nacl_interface = {
   &LaunchSelLdr,
   &UrandomFD,
-  &BrokerDuplicateHandle,
   &GetReadExecPnaclFd,
   &CreateTemporaryFile,
   &GetNumberOfProcessors,
