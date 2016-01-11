53d304230c8e0759dab775bed05572f3c88409cb
servolk <servolk@chromium.org>
2016-01-11 14:11:00 -0800

    Change DecoderBuffer::data_size to type size_t
    BUG=none
    
    Review URL: https://codereview.chromium.org/1571003002
    
    Cr-Commit-Position: refs/heads/master@{#368687}

--- media/base/text_renderer.cc
+++ media/base/text_renderer.cc
@@ -182,7 +182,7 @@ void TextRenderer::BufferReady(
   }
 
   DCHECK_EQ(status, DemuxerStream::kOk);
-  DCHECK_GE(input->side_data_size(), 2);
+  DCHECK_GE(input->side_data_size(), 2u);
 
   // The side data contains both the cue id and cue settings,
   // each terminated with a NUL.
