bb8e6f2444b8c1b68d60599f7c91d7264ab96edc
dstockwell <dstockwell@chromium.org>
2016-01-11 01:12:13 -0800

    Web Animations: Make inert effects const in CSSAnimationUpdate
    This is a cleanup and should not have any functional change. May help reason
    about the associated crash.
    
    BUG=571743
    
    Review URL: https://codereview.chromium.org/1570063002
    
    Cr-Commit-Position: refs/heads/master@{#368544}

--- third_party/WebKit/Source/core/animation/InertEffect.cpp
+++ third_party/WebKit/Source/core/animation/InertEffect.cpp
@@ -48,7 +48,7 @@ InertEffect::InertEffect(EffectModel* model, const Timing& timing, bool paused, 
 {
 }
 
-void InertEffect::sample(Vector<RefPtr<Interpolation>>& result)
+void InertEffect::sample(Vector<RefPtr<Interpolation>>& result) const
 {
     updateInheritedTime(m_inheritedTime, TimingUpdateOnDemand);
     if (!isInEffect()) {
