80068fdf98b2a797842397b82d9653f6ce214e30
alancutter <alancutter@chromium.org>
2016-01-11 01:13:42 -0800

    Port CSS visibility animations to CSSInterpolationTypes
    This change refactors CSS visibility animations to use CSSInterpolationType.
    There is no difference in behaviour in this patch.
    
    BUG=530436
    
    Review URL: https://codereview.chromium.org/1548873002
    
    Cr-Commit-Position: refs/heads/master@{#368546}

--- third_party/WebKit/Source/core/animation/StringKeyframe.cpp
+++ third_party/WebKit/Source/core/animation/StringKeyframe.cpp
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/animation/CSSPaintInterpolationType.h"
 #include "core/animation/CSSShadowListInterpolationType.h"
 #include "core/animation/CSSValueInterpolationType.h"
+#include "core/animation/CSSVisibilityInterpolationType.h"
 #include "core/animation/CompositorAnimations.h"
 #include "core/animation/ConstantStyleInterpolation.h"
 #include "core/animation/DefaultSVGInterpolation.h"
@@ -260,6 +261,9 @@ const InterpolationTypes* applicableTypesForProperty(PropertyHandle property)
             break;
         case CSSPropertyFontWeight:
             applicableTypes->append(adoptPtr(new CSSFontWeightInterpolationType(cssProperty)));
+            break;
+        case CSSPropertyVisibility:
+            applicableTypes->append(adoptPtr(new CSSVisibilityInterpolationType(cssProperty)));
             break;
         default:
             // TODO(alancutter): Support all interpolable CSS properties here so we can stop falling back to the old StyleInterpolation implementation.
--- third_party/WebKit/Source/core/core.gypi
+++ third_party/WebKit/Source/core/core.gypi
@@ -859,6 +859,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'animation/CSSShadowListInterpolationType.h',
             'animation/CSSValueInterpolationType.cpp',
             'animation/CSSValueInterpolationType.h',
+            'animation/CSSVisibilityInterpolationType.cpp',
+            'animation/CSSVisibilityInterpolationType.h',
             'animation/CompositorAnimations.cpp',
             'animation/CompositorAnimations.h',
             'animation/CompositorAnimationsImpl.h',
