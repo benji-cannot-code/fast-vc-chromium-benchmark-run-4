1b9ee96514ed5917039536b7ad0ee070ee622849
sievers <sievers@chromium.org>
2016-01-11 15:40:50 -0800

    Android: Remove #FlakyTest annotation for testProcessIsolationForRenderers
    The issue has already been fixed and the dashboard does
    not seem to indicate recent failures.
    
    BUG=471122
    
    Review URL: https://codereview.chromium.org/1575213002
    
    Cr-Commit-Position: refs/heads/master@{#368717}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ProcessIsolationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ProcessIsolationTest.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser;
 
-import android.test.FlakyTest;
 import android.text.TextUtils;
 
 import org.chromium.base.BuildInfo;
@@ -34,10 +33,8 @@ public class ProcessIsolationTest extends ChromeActivityTestCaseBase<ChromeActiv
      * Verifies that process isolation works, i.e., that the browser and
      * renderer processes use different user IDs.
      * @throws InterruptedException
-     * BUG: 471122
      * @MediumTest
      */
-    @FlakyTest
     @Feature({"Browser", "Security"})
     public void testProcessIsolationForRenderers() throws InterruptedException {
         int tabsCount = getActivity().getCurrentTabModel().getCount();
