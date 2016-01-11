4ee55b44cca596dba3c82249d68e2beac72d976f
droger <droger@chromium.org>
2016-01-11 04:18:52 -0800

    Componentize the favicon for chrome://history
    TBR=thakis
    
    Review URL: https://codereview.chromium.org/1566983004
    
    Cr-Commit-Position: refs/heads/master@{#368569}

--- chrome/browser/ui/webui/md_history_ui.cc
+++ chrome/browser/ui/webui/md_history_ui.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "grit/browser_resources.h"
+#include "grit/components_scaled_resources.h"
 #include "grit/components_strings.h"
 #include "grit/theme_resources.h"
 #include "ui/base/resource/resource_bundle.h"
