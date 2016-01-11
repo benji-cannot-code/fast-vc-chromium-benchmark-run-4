685fb73648962ea6d453caa1cbe9b73d55f328c3
cnwan <cnwan@chromium.org>
2016-01-11 01:14:45 -0800

    Minimum implementation of ARC Clipboard Bridge.
    All change sets include:
     - http://crrev.com/1495723004 (Chromium)
     - http://ag/833451 (Android device/google/cheets)
     - http://ag/833422 (Android platform/frameworks/base)
    
    BUG=565283
    TEST=Build both sides. Ctrl-C/Ctrl-V from either side reflect to the other.
    
    Review URL: https://codereview.chromium.org/1495723004
    
    Cr-Commit-Position: refs/heads/master@{#368547}

--- components/arc/arc_service_manager.cc
+++ components/arc/arc_service_manager.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/arc_bridge_bootstrap.h"
 #include "components/arc/arc_bridge_service_impl.h"
 #include "components/arc/auth/arc_auth_service.h"
+#include "components/arc/clipboard/arc_clipboard_bridge.h"
 #include "components/arc/input/arc_input_bridge.h"
 #include "components/arc/power/arc_power_bridge.h"
 #include "components/arc/settings/arc_settings_bridge.h"
@@ -29,6 +30,7 @@ ArcServiceManager::ArcServiceManager(
     : arc_bridge_service_(
           new ArcBridgeServiceImpl(ArcBridgeBootstrap::Create())),
       arc_auth_service_(std::move(auth_service)),
+      arc_clipboard_bridge_(new ArcClipboardBridge(arc_bridge_service_.get())),
       arc_input_bridge_(ArcInputBridge::Create(arc_bridge_service_.get())),
       arc_settings_bridge_(std::move(settings_bridge)),
       arc_power_bridge_(new ArcPowerBridge(arc_bridge_service_.get())) {
--- components/arc/arc_service_manager.h
+++ components/arc/arc_service_manager.h
@@ -14,6 +14,7 @@ namespace arc {
 
 class ArcAuthService;
 class ArcBridgeService;
+class ArcClipboardBridge;
 class ArcInputBridge;
 class ArcSettingsBridge;
 class ArcPowerBridge;
@@ -40,6 +41,7 @@ class ArcServiceManager {
 
   // Individual services
   scoped_ptr<ArcAuthService> arc_auth_service_;
+  scoped_ptr<ArcClipboardBridge> arc_clipboard_bridge_;
   scoped_ptr<ArcInputBridge> arc_input_bridge_;
   scoped_ptr<ArcSettingsBridge> arc_settings_bridge_;
   scoped_ptr<ArcPowerBridge> arc_power_bridge_;
