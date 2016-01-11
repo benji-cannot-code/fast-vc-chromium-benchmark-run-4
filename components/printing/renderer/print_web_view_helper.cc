dd03be4570adc11c0ed469a84df30066cd4e74ef
vitalybuka <vitalybuka@chromium.org>
2016-01-11 15:10:26 -0800

    Fix crashes on multiple print requests
    Crashes can be reproduces by pressing
    Ctrl+Shift+P,Ctrl+Shift+P or Ctrl+shift+P,Ctrl+P
    
    In cases of OnPrintPages and OnPrintPreview it's quite obvious that we
    should ignore messages.
    
    For OnPrintingDone is not so obvious as if we ignore message we may keep
    renderer in pending blocking future print preview. However if we allow
    recursive OnPrintingDone it may release printing context and cause
    access of release memory. So probably blocking printing in that tab
    is safer than that kind of crash.
    
    BUG=545391
    
    Review URL: https://codereview.chromium.org/1572813002
    
    Cr-Commit-Position: refs/heads/master@{#368705}

--- components/printing/renderer/print_web_view_helper.cc
+++ components/printing/renderer/print_web_view_helper.cc
@@ -994,7 +994,8 @@ bool PrintWebViewHelper::GetPrintFrame(blink::WebLocalFrame** frame) {
 
 #if defined(ENABLE_BASIC_PRINTING)
 void PrintWebViewHelper::OnPrintPages() {
-  CHECK_LE(ipc_nesting_level_, 1);
+  if (ipc_nesting_level_> 1)
+    return;
   blink::WebLocalFrame* frame;
   if (!GetPrintFrame(&frame))
     return;
@@ -1005,7 +1006,8 @@ void PrintWebViewHelper::OnPrintPages() {
 }
 
 void PrintWebViewHelper::OnPrintForSystemDialog() {
-  CHECK_LE(ipc_nesting_level_, 1);
+  if (ipc_nesting_level_> 1)
+    return;
   blink::WebLocalFrame* frame = print_preview_context_.source_frame();
   if (!frame) {
     NOTREACHED();
@@ -1047,7 +1049,8 @@ bool PrintWebViewHelper::IsPrintToPdfRequested(
 }
 
 void PrintWebViewHelper::OnPrintPreview(const base::DictionaryValue& settings) {
-  CHECK_LE(ipc_nesting_level_, 1);
+  if (ipc_nesting_level_ > 1)
+    return;
 
   print_preview_context_.OnPrintPreview();
 
@@ -1235,7 +1238,8 @@ bool PrintWebViewHelper::FinalizePrintReadyDocument() {
 }
 
 void PrintWebViewHelper::OnPrintingDone(bool success) {
-  CHECK_LE(ipc_nesting_level_, 1);
+  if (ipc_nesting_level_ > 1)
+    return;
   notify_browser_of_print_failure_ = false;
   if (!success)
     LOG(ERROR) << "Failure in OnPrintingDone";
@@ -1247,7 +1251,8 @@ void PrintWebViewHelper::SetScriptedPrintBlocked(bool blocked) {
 }
 
 void PrintWebViewHelper::OnInitiatePrintPreview(bool selection_only) {
-  CHECK_LE(ipc_nesting_level_, 1);
+  if (ipc_nesting_level_ > 1)
+    return;
   blink::WebLocalFrame* frame = NULL;
   GetPrintFrame(&frame);
   DCHECK(frame);
