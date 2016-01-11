85b23846e6b4f9d80c88bc3907471b4e417452ba
jinho.bang <jinho.bang@samsung.com>
2016-01-11 09:16:08 -0800

    Canvas2d: Fix incorrect alignment of hit region when applied css/zoom.
    This change is fixing incorrect alignment of hit region on canvas when applied
    css transform/width/height/padding/border or device pinch-zoom or high DPI.
    Also, this CL adds/rewrites related tests.
    
    BUG=328961,497750,519603
    
    Review URL: https://codereview.chromium.org/1553373002
    
    Cr-Commit-Position: refs/heads/master@{#368607}

--- third_party/WebKit/Source/modules/canvas2d/EventHitRegion.cpp
+++ third_party/WebKit/Source/modules/canvas2d/EventHitRegion.cpp
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/Document.h"
 #include "core/html/HTMLCanvasElement.h"
-#include "core/layout/LayoutObject.h"
+#include "core/layout/LayoutBox.h"
+#include "core/page/Page.h"
 #include "modules/canvas2d/CanvasRenderingContext2D.h"
 #include "modules/canvas2d/HitRegion.h"
 
@@ -23,14 +24,11 @@ String EventHitRegion::regionIdFromAbsoluteLocation(HTMLCanvasElement& canvas, c
     document.updateLayoutTreeForNodeIfNeeded(&canvas);
 
     // Adjust offsetLocation to be relative to the canvas's position.
-    LayoutObject* layoutObject = canvas.layoutObject();
-    FloatPoint localPos = layoutObject->absoluteToLocal(FloatPoint(location), UseTransforms);
-
-    LocalFrame* frame = document.frame();
-    float zoomFactor = frame ? frame->pageZoomFactor() : 1;
-    float scaleFactor = 1 / zoomFactor;
-    if (scaleFactor != 1.0f)
-        localPos.scale(scaleFactor, scaleFactor);
+    LayoutBox* box = canvas.layoutBox();
+    FloatPoint localPos = box->absoluteToLocal(FloatPoint(location), UseTransforms);
+    if (box->hasBorderOrPadding())
+        localPos.move(-box->contentBoxOffset());
+    localPos.scale(canvas.width() / box->contentWidth(), canvas.height() / box->contentHeight());
 
     HitRegion* hitRegion = toCanvasRenderingContext2D(context)->hitRegionAtPoint(localPos);
     if (!hitRegion || hitRegion->id().isEmpty())
