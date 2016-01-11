07aa2aaf39c16ab0f60660eae9c57358d50da96c
olli.raula <olli.raula@intel.com>
2016-01-11 01:09:23 -0800

    Use std::move instead of release() + new scoped_ptr in sdch_owner
    Review URL: https://codereview.chromium.org/1567753003
    
    Cr-Commit-Position: refs/heads/master@{#368540}

--- net/sdch/sdch_owner.cc
+++ net/sdch/sdch_owner.cc
@@ -689,7 +689,7 @@ bool SdchOwner::HasDictionaryFromURLForTesting(const GURL& url) const {
 
 void SdchOwner::SetFetcherForTesting(
     scoped_ptr<SdchDictionaryFetcher> fetcher) {
-  fetcher_.reset(fetcher.release());
+  fetcher_ = std::move(fetcher);
 }
 
 void SdchOwner::OnMemoryPressure(
