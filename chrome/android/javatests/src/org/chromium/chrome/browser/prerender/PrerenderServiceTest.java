a13219ae188a2df817f42417cd1fd83eea18d603
wnwen <wnwen@chromium.org>
2016-01-12 08:40:10 -0800

    Add --strict-mode cmdline flag to test_runner.
    Default flag is --strict-mode=testing to prevent any
    regressions in instrumentation tests.
    
    BUG=564863
    
    Review URL: https://codereview.chromium.org/1540983002
    
    Cr-Commit-Position: refs/heads/master@{#368895}

--- chrome/android/javatests/src/org/chromium/chrome/browser/prerender/PrerenderServiceTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/prerender/PrerenderServiceTest.java
@@ -108,6 +108,7 @@ public class PrerenderServiceTest extends
      * @throws Exception
      */
     @SmallTest
+    @DisabledTest
     @Feature({"PrerenderService"})
     public void testPrerenderingRedirectUrl() throws Exception {
         if (SysUtils.isLowEndDevice()) return;
@@ -136,6 +137,7 @@ public class PrerenderServiceTest extends
     private void loadChromeWithUrl(final String url) throws InterruptedException {
         assertNotNull(getActivity());
         ThreadUtils.runOnUiThreadBlocking(new Runnable(){
+            @Override
             public void run() {
                 ((EditText) getActivity().findViewById(R.id.url_to_load)).setText(url);
             }
