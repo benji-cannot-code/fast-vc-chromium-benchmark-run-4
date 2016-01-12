4e8617bce87645d51f78e1e90cfb3aa2a34a9df9
imcheng <imcheng@chromium.org>
2016-01-11 17:35:56 -0800

    [Media Router] Add Connecting state to PresentationConnection.
    BUG=575351
    
    Review URL: https://codereview.chromium.org/1572433002
    
    Cr-Commit-Position: refs/heads/master@{#368761}

--- chrome/browser/media/router/presentation_service_delegate_impl.h
+++ chrome/browser/media/router/presentation_service_delegate_impl.h
@@ -36,7 +36,6 @@ namespace media_router {
 class MediaRoute;
 class MediaSinksObserver;
 class PresentationFrameManager;
-class PresentationSessionStateObserver;
 
 // Implementation of PresentationServiceDelegate that interfaces an
 // instance of WebContents with the Chrome Media Router. It uses the Media
