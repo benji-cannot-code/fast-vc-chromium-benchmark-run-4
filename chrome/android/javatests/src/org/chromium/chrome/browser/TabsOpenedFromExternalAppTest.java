9f47e146222b163bc16aace40bc3facb7e102356
hartmanng <hartmanng@chromium.org>
2016-01-12 14:10:02 -0800

    Re-enable and fix several tests in TabsOpenedFromExternalAppTest.java.
    BUG=575776,149294,576756,147960,155004,157773
    
    Review URL: https://codereview.chromium.org/1583563002
    
    Cr-Commit-Position: refs/heads/master@{#369002}

--- chrome/android/javatests/src/org/chromium/chrome/browser/TabsOpenedFromExternalAppTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/TabsOpenedFromExternalAppTest.java
@@ -9,10 +9,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.provider.Browser;
 import android.test.FlakyTest;
+import android.test.suitebuilder.annotation.LargeTest;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.text.TextUtils;
 import android.view.ContextMenu;
-import android.view.KeyEvent;
 import android.view.View;
 
 import junit.framework.Assert;
@@ -20,7 +20,6 @@ import junit.framework.Assert;
 import org.chromium.base.BaseSwitches;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
@@ -35,7 +34,6 @@ import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
 import org.chromium.content.browser.test.util.DOMUtils;
 import org.chromium.content.browser.test.util.JavaScriptUtils;
-import org.chromium.content.browser.test.util.KeyUtils;
 import org.chromium.content.browser.test.util.TouchCommon;
 
 import java.util.concurrent.Callable;
@@ -162,12 +160,8 @@ public class TabsOpenedFromExternalAppTest extends ChromeTabbedActivityTestBase 
      * Tests that URLs opened from the same external app don't create new tabs.
      * @throws InterruptedException
      */
-    /**
-     * @LargeTest
-     * @Feature({"Navigation"})
-     * Bug 5856404
-     */
-    @FlakyTest
+    @LargeTest
+    @Feature({"Navigation"})
     public void testNoNewTabForSameApp() throws InterruptedException {
         startMainActivityFromLauncher();
 
@@ -195,8 +189,12 @@ public class TabsOpenedFromExternalAppTest extends ChromeTabbedActivityTestBase 
         // And pressing back should close Clank.
         assertTrue("Window does not have focus before pressing back.",
                 getActivity().hasWindowFocus());
-        KeyUtils.singleKeyEventView(getInstrumentation(), getActivity().getActivityTab().getView(),
-                KeyEvent.KEYCODE_BACK);
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                getActivity().onBackPressed();
+            }
+        });
         getInstrumentation().waitForIdleSync();
         assertFalse("Window still has focus after pressing back.", getActivity().hasWindowFocus());
     }
@@ -205,15 +203,11 @@ public class TabsOpenedFromExternalAppTest extends ChromeTabbedActivityTestBase 
      * Tests that URLs opened from an unspecified external app (no Browser.EXTRA_APPLICATION_ID in
      * the intent extras) don't create new tabs.
      * @throws InterruptedException
-     * TODO(jcivelli): http:/b/5882419 we disabled this behavior so this test is disabled until we
-     *                 figure out what we want to do.
      */
-    /**
-     * @LargeTest
-     * @Feature({"Navigation"})
-     * Bug 5856404
-     */
-    @DisabledTest
+
+
+    @LargeTest
+    @Feature({"Navigation"})
     public void testNewTabForUnknownApp() throws InterruptedException {
         startMainActivityFromLauncher();
 
@@ -247,8 +241,12 @@ public class TabsOpenedFromExternalAppTest extends ChromeTabbedActivityTestBase 
         // And pressing back should close Clank.
         assertTrue("Window does not have focus before pressing back.",
                 getActivity().hasWindowFocus());
-        KeyUtils.singleKeyEventView(getInstrumentation(), getActivity().getActivityTab().getView(),
-                KeyEvent.KEYCODE_BACK);
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                getActivity().onBackPressed();
+            }
+        });
         getInstrumentation().waitForIdleSync();
         assertFalse("Window still has focus after pressing back.", getActivity().hasWindowFocus());
     }
@@ -258,12 +256,8 @@ public class TabsOpenedFromExternalAppTest extends ChromeTabbedActivityTestBase 
      * the intent do create new tabs.
      * @throws InterruptedException
      */
-    /**
-     * Bug: crbug.com/155004
-     * @LargeTest
-     * @Feature({"Navigation"})
-     */
-    @DisabledTest
+    @LargeTest
+    @Feature({"Navigation"})
     public void testNewTabWithNewTabExtra() throws InterruptedException {
         startMainActivityFromLauncher();
 
@@ -291,8 +285,12 @@ public class TabsOpenedFromExternalAppTest extends ChromeTabbedActivityTestBase 
         // And pressing back should close Clank.
         assertTrue("Window does not have focus before pressing back.",
                 getActivity().hasWindowFocus());
-        KeyUtils.singleKeyEventView(getInstrumentation(), getActivity().getActivityTab().getView(),
-                KeyEvent.KEYCODE_BACK);
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                getActivity().onBackPressed();
+            }
+        });
         getInstrumentation().waitForIdleSync();
         assertFalse("Window still has focus after pressing back.", getActivity().hasWindowFocus());
     }
@@ -302,12 +300,8 @@ public class TabsOpenedFromExternalAppTest extends ChromeTabbedActivityTestBase 
      * tab) from the external app.
      * @throws InterruptedException
      */
-    /**
-     * @LargeTest
-     * @Feature({"Navigation", "Main"})
-     * Bug 5856404
-     */
-    @FlakyTest
+    @LargeTest
+    @Feature({"Navigation", "Main"})
     public void testNoNewTabForSameAppOnStart() throws InterruptedException {
         String url1 = TestHttpServerClient.getUrl("chrome/test/data/android/google.html");
         String url2 = TestHttpServerClient.getUrl("chrome/test/data/android/about.html");
@@ -328,8 +322,12 @@ public class TabsOpenedFromExternalAppTest extends ChromeTabbedActivityTestBase 
         // And pressing back should close Clank.
         assertTrue("Window does not have focus before pressing back.",
                 getActivity().hasWindowFocus());
-        KeyUtils.singleKeyEventView(getInstrumentation(), getActivity().getActivityTab().getView(),
-                KeyEvent.KEYCODE_BACK);
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                getActivity().onBackPressed();
+            }
+        });
         getInstrumentation().waitForIdleSync();
         assertFalse("Window still has focus after pressing back.", getActivity().hasWindowFocus());
     }
@@ -337,11 +335,8 @@ public class TabsOpenedFromExternalAppTest extends ChromeTabbedActivityTestBase 
     /**
      * Test that URLs opened from different external apps do create new tabs.
      * @throws InterruptedException
-     *
-     * @LargeTest
-     * crbug.com/157773
      */
-    @FlakyTest
+    @LargeTest
     @Feature({"Navigation", "Main"})
     public void testNewTabForDifferentApps() throws InterruptedException {
         startMainActivityFromLauncher();
@@ -537,7 +532,12 @@ public class TabsOpenedFromExternalAppTest extends ChromeTabbedActivityTestBase 
 
         // Hitting "back" should close the tab, minimize Chrome, and select the background tab.
         // Confirm that the number of tabs is correct and that closing the tab didn't cause a crash.
-        getActivity().onBackPressed();
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                getActivity().onBackPressed();
+            }
+        });
         CriteriaHelper.pollForUIThreadCriteria(new Criteria() {
             @Override
             public boolean isSatisfied() {
