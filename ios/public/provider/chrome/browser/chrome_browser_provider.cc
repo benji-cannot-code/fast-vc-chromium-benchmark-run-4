a53b5028b133224c7aeeac63a13f1338bcf3dc64
sdefresne <sdefresne@chromium.org>
2016-01-12 14:14:48 -0800

    Clean include of in i/p/p/c/b/chrome_browser_provider.cc.
    BUG=None
    
    Review URL: https://codereview.chromium.org/1580043003
    
    Cr-Commit-Position: refs/heads/master@{#369006}

--- ios/public/provider/chrome/browser/chrome_browser_provider.cc
+++ ios/public/provider/chrome/browser/chrome_browser_provider.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "components/metrics/metrics_provider.h"
-#include "components/sync_sessions/local_session_event_router.h"
 #include "components/sync_sessions/synced_window_delegates_getter.h"
 #include "ios/public/provider/chrome/browser/browser_state/chrome_browser_state_manager.h"
 
