2465f8e4ed1d37fa9a93d14489eee5313fbf5261
vabr <vabr@chromium.org>
2016-01-12 10:12:09 -0800

    Remove an obsolete TODO.
    The TODO was declared obsolete in http://crbug.com/87185#c4.
    
    R=engedy@chromium.org
    BUG=87185
    
    Review URL: https://codereview.chromium.org/1580673003
    
    Cr-Commit-Position: refs/heads/master@{#368914}

--- components/password_manager/core/browser/password_manager_test_utils.h
+++ components/password_manager/core/browser/password_manager_test_utils.h
@@ -14,9 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_store.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
-// TODO(sync): The PasswordFormData code must eventually be refactored away --
-// http://crbug.com/87185.
-
 namespace password_manager {
 
 // This template allows creating methods with signature conforming to
