92bac7b03c19afd9d5b83abe5a2c1d0fc00925f0
rob.buis <rob.buis@samsung.com>
2016-01-12 08:47:55 -0800

    Move scroll-snap related properties into CSSPropertyParser
    Move scroll-snap-coordinate, scroll-snap-points-x and
    scroll-snap-points-y from LegacyCSSPropertyParser
    into CSSPropertyParser.
    
    BUG=499780
    
    Review URL: https://codereview.chromium.org/1576163002
    
    Cr-Commit-Position: refs/heads/master@{#368898}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
@@ -223,9 +223,6 @@ private:
 
     bool consumeFlex(bool important);
 
-    PassRefPtrWillBeRawPtr<CSSValue> parsePosition(CSSParserValueList*);
-    PassRefPtrWillBeRawPtr<CSSValueList> parsePositionList(CSSParserValueList*);
-
     // Image generators
     bool parseDeprecatedGradient(CSSParserValueList*, RefPtrWillBeRawPtr<CSSValue>&);
     bool parseDeprecatedLinearGradient(CSSParserValueList*, RefPtrWillBeRawPtr<CSSValue>&, CSSGradientRepeat repeating);
@@ -312,11 +309,6 @@ private:
     PassRefPtrWillBeRawPtr<CSSValue> parseDeprecatedGradientStopColor(const CSSParserValue*);
 
     void commitBorderImageProperty(CSSPropertyID, PassRefPtrWillBeRawPtr<CSSValue>, bool important);
-
-    PassRefPtrWillBeRawPtr<CSSValue> parseScrollSnapPoints();
-    PassRefPtrWillBeRawPtr<CSSValue> parseScrollSnapDestination();
-    PassRefPtrWillBeRawPtr<CSSValue> parseScrollSnapCoordinate();
-    PassRefPtrWillBeRawPtr<CSSValue> parseScrollSnapPosition();
 
 private:
     // Inputs:
