820a9a0c710bac28ad2f7179381957d64a92f824
lhchavez <lhchavez@chromium.org>
2016-01-11 16:16:31 -0800

    arc-bridge: Fix a race in the ARC app list
    Instead of just relying on the Observer being registered, we should also
    check for the readiness of the ARC app instance at the same time to
    avoid races.
    
    BUG=575873
    TEST=ARC app list shows correctly
    
    Review URL: https://codereview.chromium.org/1570303002
    
    Cr-Commit-Position: refs/heads/master@{#368731}

--- chrome/browser/ui/app_list/arc/arc_app_list_prefs.cc
+++ chrome/browser/ui/app_list/arc/arc_app_list_prefs.cc
@@ -116,6 +116,8 @@ ArcAppListPrefs::ArcAppListPrefs(const base::FilePath& base_path,
   }
 
   bridge_service->AddObserver(this);
+  if (bridge_service->app_instance())
+    OnAppInstanceReady();
   OnStateChanged(bridge_service->state());
 }
 
