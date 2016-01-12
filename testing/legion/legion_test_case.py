b8c7be7a43a5f7ce60b0258ed1ad63e8f2fb3dd3
mmeade <mmeade@chromium.org>
2016-01-11 17:22:53 -0800

    Doing some refactoring and adding an event mechanism
    This cl does some refactoring as well as adds an eventing mechanism and global
    shutdown hook so that tasks and threads will be called directly by the event
    callback directly instead of needing to track shutdown calls via third part
    objects.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1578903002
    
    Cr-Commit-Position: refs/heads/master@{#368755}

--- testing/legion/legion_test_case.py
+++ testing/legion/legion_test_case.py
@@ -10,10 +10,13 @@ import logging
 import sys
 import unittest
 
-#pylint: disable=relative-import
-import common_lib
-import task_controller
-import task_registration_server
+# pylint: disable=relative-import
+# Import common_lib first so we can setup the environment
+from lib import common_lib
+common_lib.SetupEnvironment()
+
+from legion.lib import task_controller
+from legion.lib import task_registration_server
 
 BANNER_WIDTH = 80
 
@@ -103,9 +106,7 @@ class TestCase(unittest.TestCase):
   @classmethod
   def _TearDownFramework(cls):
     """Perform the framework-specific teardown operations."""
-    if cls._registration_server:
-      cls._registration_server.Shutdown()
-    task_controller.TaskController.ReleaseAllTasks()
+    common_lib.Shutdown()
 
   @classmethod
   def _HandleSetUpClass(cls):
@@ -114,7 +115,6 @@ class TestCase(unittest.TestCase):
     This method performs test-wide setup such as starting the registration
     server and then calls the original setUpClass method."""
     try:
-      common_lib.InitLogging()
       cls._LogInfoBanner('setUpClass', 'Performs class level setup.')
       cls._SetUpFramework()
       cls._OriginalSetUpClassMethod()
