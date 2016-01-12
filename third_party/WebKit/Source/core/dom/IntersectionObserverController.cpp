fc02481d184e4aac5814c99e5650816d38d8ba08
sigbjornf <sigbjornf@opera.com>
2016-01-11 23:52:54 -0800

    Oilpan: fix build after r368814.
    TBR=oilpan-reviews
    BUG=540528
    NOTRY=true
    NOTREECHECKS=true
    NOPRESUBMIT=true
    
    Review URL: https://codereview.chromium.org/1575323002
    
    Cr-Commit-Position: refs/heads/master@{#368817}

--- third_party/WebKit/Source/core/dom/IntersectionObserverController.cpp
+++ third_party/WebKit/Source/core/dom/IntersectionObserverController.cpp
@@ -86,9 +86,9 @@ void IntersectionObserverController::removeTrackedObserversForRoot(const Element
 
 DEFINE_TRACE(IntersectionObserverController)
 {
-    ActiveDOMObject::trace(visitor);
     visitor->trace(m_trackedIntersectionObservers);
     visitor->trace(m_pendingIntersectionObservers);
+    ActiveDOMObject::trace(visitor);
 }
 
 } // namespace blink
--- third_party/WebKit/Source/core/dom/IntersectionObserverController.h
+++ third_party/WebKit/Source/core/dom/IntersectionObserverController.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class IntersectionObserverController : public GarbageCollectedFinalized<IntersectionObserverController>, public ActiveDOMObject {
+    USING_GARBAGE_COLLECTED_MIXIN(IntersectionObserverController);
 public:
     static IntersectionObserverController* create(Document*);
     ~IntersectionObserverController();
