45ca5e2c7688a5e815def9985ea0c5d0b2c70d00
tkent <tkent@chromium.org>
2016-01-12 01:57:22 -0800

    setRangeText shouldn't dispatch 'search' event.
    We should not call subtreeHasChanged() in HTMLTextFormControlElement::setRangeText.
    
    BUG=576454
    TEST=automated
    
    Review URL: https://codereview.chromium.org/1577243002
    
    Cr-Commit-Position: refs/heads/master@{#368841}

--- third_party/WebKit/Source/core/html/HTMLTextFormControlElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLTextFormControlElement.cpp
@@ -236,8 +236,7 @@ void HTMLTextFormControlElement::setRangeText(const String& replacement, unsigne
     else
         text.insert(replacement, start);
 
-    setInnerEditorValue(text);
-    subtreeHasChanged();
+    setValue(text, TextFieldEventBehavior::DispatchNoEvent);
 
     if (selectionMode == "select") {
         newSelectionStart = start;
