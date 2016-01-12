f2f3972d322a46e555a9ab0a302c782154d26db8
changwan <changwan@chromium.org>
2016-01-11 22:16:17 -0800

    Consider newCursorPosition better when composing or commiting text
    commitText isn't considering the case of newCursorPosition != 1. We don't
    have native code to handle it, but since
    
    commitText = setComposingText + finishComposingText,
    
    we can replace it as such.
    
    Also, as for setComposingText, there is an error in BaseInputConnection
    (Android framework), so we need to correct the selection after calling
    super.setComposingText().
    
    BUG=570920
    
    Review URL: https://codereview.chromium.org/1576303002
    
    Cr-Commit-Position: refs/heads/master@{#368809}

--- content/public/android/java/src/org/chromium/content/browser/input/ImeAdapter.java
+++ content/public/android/java/src/org/chromium/content/browser/input/ImeAdapter.java
@@ -451,7 +451,7 @@ public class ImeAdapter {
                 flags));
     }
 
-    boolean sendCompositionToNative(CharSequence text, int newCursorPosition, boolean isCommit) {
+    boolean commitText(CharSequence text) {
         if (mNativeImeAdapterAndroid == 0) return false;
         mViewEmbedder.onImeEvent();
 
@@ -459,12 +459,22 @@ public class ImeAdapter {
         nativeSendSyntheticKeyEvent(mNativeImeAdapterAndroid, WebInputEventType.RawKeyDown,
                 timestampMs, COMPOSITION_KEY_CODE, 0, 0);
 
-        if (isCommit) {
-            nativeCommitText(mNativeImeAdapterAndroid, text.toString());
-        } else {
-            nativeSetComposingText(
-                    mNativeImeAdapterAndroid, text, text.toString(), newCursorPosition);
-        }
+        nativeCommitText(mNativeImeAdapterAndroid, text.toString());
+
+        nativeSendSyntheticKeyEvent(mNativeImeAdapterAndroid, WebInputEventType.KeyUp, timestampMs,
+                COMPOSITION_KEY_CODE, 0, 0);
+        return true;
+    }
+
+    boolean setComposingText(CharSequence text, int newCursorPosition) {
+        if (mNativeImeAdapterAndroid == 0) return false;
+        mViewEmbedder.onImeEvent();
+
+        long timestampMs = SystemClock.uptimeMillis();
+        nativeSendSyntheticKeyEvent(mNativeImeAdapterAndroid, WebInputEventType.RawKeyDown,
+                timestampMs, COMPOSITION_KEY_CODE, 0, 0);
+
+        nativeSetComposingText(mNativeImeAdapterAndroid, text, text.toString(), newCursorPosition);
 
         nativeSendSyntheticKeyEvent(mNativeImeAdapterAndroid, WebInputEventType.KeyUp,
                 timestampMs, COMPOSITION_KEY_CODE, 0, 0);
