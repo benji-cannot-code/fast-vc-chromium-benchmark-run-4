ca86b4cf71a8154916d15f1c8467e7ddc467ff24
melandory <melandory@chromium.org>
2016-01-12 01:54:02 -0800

    Cleanup ManagePasswordsUIController unit tests from OS_ANDROID ifdefs.
    BUG=549219
    
    Review URL: https://codereview.chromium.org/1575973002
    
    Cr-Commit-Position: refs/heads/master@{#368839}

--- chrome/browser/ui/passwords/manage_passwords_ui_controller_unittest.cc
+++ chrome/browser/ui/passwords/manage_passwords_ui_controller_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/passwords/manage_passwords_bubble_model.h"
+#include "chrome/browser/ui/passwords/manage_passwords_icon_view.h"
 #include "chrome/browser/ui/passwords/manage_passwords_ui_controller_mock.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "chrome/test/base/testing_profile.h"
@@ -33,10 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if !defined(OS_ANDROID)
-#include "chrome/browser/ui/passwords/manage_passwords_icon_view.h"
-#endif
-
 using ::testing::ElementsAre;
 using ::testing::Pointee;
 
@@ -45,7 +42,6 @@ namespace {
 // Number of dismissals that for sure supresses the bubble.
 const int kGreatDissmisalCount = 10;
 
-#if !defined(OS_ANDROID)
 class TestManagePasswordsIconView : public ManagePasswordsIconView {
  public:
   TestManagePasswordsIconView() {}
@@ -60,7 +56,6 @@ class TestManagePasswordsIconView : public ManagePasswordsIconView {
 
   DISALLOW_COPY_AND_ASSIGN(TestManagePasswordsIconView);
 };
-#endif
 
 // This sublass is used to disable some code paths which are not essential for
 // testing.
