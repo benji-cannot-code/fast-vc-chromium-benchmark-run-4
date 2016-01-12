7ee1ce79d6b03f8ca2a44f2cbfe7c5f3c1f1e2bf
hyunjune.kim <hyunjune.kim@samsung.com>
2016-01-12 09:45:47 -0800

    Remove unused variable in LayoutSVGInlineText::positionForPoint
    Review URL: https://codereview.chromium.org/1577313002
    
    Cr-Commit-Position: refs/heads/master@{#368910}

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGInlineText.cpp
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGInlineText.cpp
@@ -178,7 +178,6 @@ PositionWithAffinity LayoutSVGInlineText::positionForPoint(const LayoutPoint& po
     const SVGTextFragment* closestDistanceFragment = nullptr;
     SVGInlineTextBox* closestDistanceBox = nullptr;
 
-    AffineTransform fragmentTransform;
     for (InlineTextBox* box = firstTextBox(); box; box = box->nextTextBox()) {
         if (!box->isSVGInlineTextBox())
             continue;
