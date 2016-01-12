6ab8ddeb1eebcd5e284210a35de638c6c3dd3fb9
ccameron <ccameron@chromium.org>
2016-01-11 18:46:53 -0800

    Mac Overlays: Initialize edge aa mask
    BUG=567946
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1573543002
    
    Cr-Commit-Position: refs/heads/master@{#368780}

--- cc/output/ca_layer_overlay.cc
+++ cc/output/ca_layer_overlay.cc
@@ -137,6 +137,7 @@ CALayerResult FromDrawQuad(ResourceProvider* resource_provider,
   }
 
   // Enable edge anti-aliasing only on layer boundaries.
+  ca_layer_overlay->edge_aa_mask = 0;
   if (quad->IsLeftEdge())
     ca_layer_overlay->edge_aa_mask |= GL_CA_LAYER_EDGE_LEFT_CHROMIUM;
   if (quad->IsRightEdge())
@@ -211,11 +212,7 @@ CALayerResult FromDrawQuad(ResourceProvider* resource_provider,
 
 }  // namespace
 
-CALayerOverlay::CALayerOverlay()
-    : contents_resource_id(0),
-      opacity(1),
-      background_color(SK_ColorTRANSPARENT),
-      transform(SkMatrix44::kIdentity_Constructor) {}
+CALayerOverlay::CALayerOverlay() {}
 
 CALayerOverlay::~CALayerOverlay() {}
 
