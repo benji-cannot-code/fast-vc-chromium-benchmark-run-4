6ad2b106dae679672695dce6c45171b5a07cf7a2
samli <samli@chromium.org>
2016-01-11 21:57:42 -0800

    Devtools CSS: Show custom variables in styles sidebar pane
    BUG=569812
    
    Review URL: https://codereview.chromium.org/1578103002
    
    Cr-Commit-Position: refs/heads/master@{#368806}

--- third_party/WebKit/Source/devtools/front_end/sdk/CSSStyleModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/CSSStyleModel.js
@@ -1441,7 +1441,7 @@ WebInspector.CSSProperty.prototype = {
         {
             if (!insideProperty) {
                 var isDisabledProperty = tokenType && tokenType.includes("css-comment") && token.includes(":");
-                var isPropertyStart = tokenType && (tokenType.includes("css-meta") || tokenType.includes("css-property"));
+                var isPropertyStart = tokenType && (tokenType.includes("css-meta") || tokenType.includes("css-property") || tokenType.includes("css-variable-2"));
                 if (isDisabledProperty) {
                     result = result.trimRight() + indentation + token;
                 } else if (isPropertyStart) {
