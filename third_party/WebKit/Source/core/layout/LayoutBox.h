2effbface8f300aac7722a4a09be933a8f1822cc
jchaffraix <jchaffraix@chromium.org>
2016-01-12 14:26:59 -0800

    Clarify what LayoutBox::m_frameRect is
    m_frameRect is the border box rectangle in the container's coordinate
    but the name didn't reflect this.
    This CL adds some documentation about this, ideally we should rename
    it but there is disagreement on the naming.
    
    BUG=554634
    
    Review URL: https://codereview.chromium.org/1511053002
    
    Cr-Commit-Position: refs/heads/master@{#369010}

--- third_party/WebKit/Source/core/layout/LayoutBox.h
+++ third_party/WebKit/Source/core/layout/LayoutBox.h
@@ -302,6 +302,9 @@ public:
         frameRectChanged();
     }
 
+    // This function is in the container's coordinate system, meaning
+    // that it includes the logical top/left offset and the
+    // inline-start/block-start margins.
     LayoutRect frameRect() const { return m_frameRect; }
     void setFrameRect(const LayoutRect& rect)
     {
@@ -311,6 +314,8 @@ public:
         frameRectChanged();
     }
 
+    // Note that those functions have their origin at this box's CSS border box.
+    // As such their location doesn't account for 'top'/'left'.
     LayoutRect borderBoxRect() const { return LayoutRect(LayoutPoint(), size()); }
     LayoutRect paddingBoxRect() const { return LayoutRect(borderLeft(), borderTop(), clientWidth(), clientHeight()); }
     IntRect pixelSnappedBorderBoxRect() const { return IntRect(IntPoint(), m_frameRect.pixelSnappedSize()); }
@@ -964,7 +969,14 @@ private:
 
     void updateBackgroundAttachmentFixedStatusAfterStyleChange();
 
-    // The width/height of the contents + borders + padding.  The x/y location is relative to our container (which is not always our parent).
+    // The CSS border box rect for this box.
+    //
+    // The rectangle is in this box's physical coordinates but with a
+    // flipped block-flow direction (see the COORDINATE SYSTEMS section
+    // in LayoutBoxModelObject). The location is the distance from this
+    // object's border edge to the container's border edge (which is not
+    // always the parent). Thus it includes any logical top/left along
+    // with this box's margins.
     LayoutRect m_frameRect;
 
     // Our intrinsic height, used for min-height: min-content etc. Maintained by
