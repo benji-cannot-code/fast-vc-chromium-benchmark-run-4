d12253a9b5f26073b4c727f8bb48767d156fefcd
piman <piman@chromium.org>
2016-01-12 07:35:31 -0800

    Allow GPU channels to be created when using SwiftShader
    When using SwiftShader, we still need a GPU channel for webgl, but we need to
    make sure the renderer compositor falls back to software.
    
    BUG=576500
    
    Review URL: https://codereview.chromium.org/1575293002
    
    Cr-Commit-Position: refs/heads/master@{#368882}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -808,6 +808,7 @@ void GpuProcessHost::CreateArcVideoAcceleratorChannel(
 void GpuProcessHost::OnInitialized(bool result, const gpu::GPUInfo& gpu_info) {
   UMA_HISTOGRAM_BOOLEAN("GPU.GPUProcessInitialized", result);
   initialized_ = result;
+  gpu_info_ = gpu_info;
 
   if (!initialized_)
     GpuDataManagerImpl::GetInstance()->OnGpuProcessInitFailure();
@@ -843,8 +844,7 @@ void GpuProcessHost::OnChannelEstablished(
     return;
   }
 
-  callback.Run(channel_handle,
-               GpuDataManagerImpl::GetInstance()->GetGPUInfo());
+  callback.Run(channel_handle, gpu_info_);
 }
 
 void GpuProcessHost::OnCommandBufferCreated(CreateCommandBufferResult result) {
