46b24b4d76521567ffcff8f5aebf16abb51becc9
nainar <nainar@chromium.org>
2016-01-12 00:44:02 -0800

    Ensure no case folding outside ASCII range in quirks mode
    According to spec (http://www.w3.org/TR/css3-selectors/#casesens) "All
    Selectors syntax is case-insensitive within the ASCII range (i.e. [a-z]
    and [A-Z] are equivalent), [...] in HTML, element names are
    case-insensitive, but in XML, they are case-sensitive." This patch
    makes sure that when matching selectors we do not perform case folding
    when outside the ASCII range in quirks mode.
    
    BUG=570663
    
    Review URL: https://codereview.chromium.org/1541723002
    
    Cr-Commit-Position: refs/heads/master@{#368827}

--- third_party/WebKit/Source/wtf/text/StringImpl.h
+++ third_party/WebKit/Source/wtf/text/StringImpl.h
@@ -347,6 +347,7 @@ public:
     float toFloat(bool* ok = 0);
 
     PassRefPtr<StringImpl> lower();
+    PassRefPtr<StringImpl> lowerASCII();
     PassRefPtr<StringImpl> upper();
     PassRefPtr<StringImpl> lower(const AtomicString& localeIdentifier);
     PassRefPtr<StringImpl> upper(const AtomicString& localeIdentifier);
