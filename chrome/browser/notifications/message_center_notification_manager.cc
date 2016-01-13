069444bf557bdaa272703a466464db3f0ef9d39a
miguelg <miguelg@chromium.org>
2016-01-13 12:18:37 -0800

    Implement native web notifications for mac
    Implementation behind the enable-native-notifications chrome://flags
    Conceptually based on a previous attempt.
    (https://codereview.chromium.org/224183002) but now using the same
    interface android uses.
    
    System and extensions notifications remain implemented through chrome
    toasts.
    
    There are a few things missing that will come in future CLs, most notably
    Buttons
    Auto dimsissal (i.e all notifications requireInteraction)
    Sounds
    
    BUG=571056
    
    Review URL: https://codereview.chromium.org/1509923002
    
    Cr-Commit-Position: refs/heads/master@{#369255}

--- chrome/browser/notifications/message_center_notification_manager.cc
+++ chrome/browser/notifications/message_center_notification_manager.cc
@@ -44,6 +44,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/web_notification/web_notification_tray.h"
 #endif
 
+// Mac does support native notifications and defines this method
+// in notification_ui_manager_mac.mm
+#if !defined(OS_MACOSX)
+// static
+NotificationUIManager*
+NotificationUIManager::CreateNativeNotificationManager() {
+  return nullptr;
+}
+#endif
+
 MessageCenterNotificationManager::MessageCenterNotificationManager(
     message_center::MessageCenter* message_center,
     scoped_ptr<message_center::NotifierSettingsProvider> settings_provider)
@@ -51,8 +61,7 @@ MessageCenterNotificationManager::MessageCenterNotificationManager(
       settings_provider_(std::move(settings_provider)),
       system_observer_(this),
       stats_collector_(message_center),
-      google_now_stats_collector_(message_center)
-{
+      google_now_stats_collector_(message_center) {
   message_center_->AddObserver(this);
   message_center_->SetNotifierSettingsProvider(settings_provider_.get());
 
--- chrome/browser/notifications/platform_notification_service_impl.h
+++ chrome/browser/notifications/platform_notification_service_impl.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
+#include "base/memory/scoped_ptr.h"
 #include "base/memory/singleton.h"
 #include "base/strings/string16.h"
 #include "chrome/browser/notifications/notification.h"
@@ -125,6 +126,10 @@ class PlatformNotificationServiceImpl
   // Returns a display name for an origin, to be used in the context message
   base::string16 DisplayNameForContextMessage(Profile* profile,
                                               const GURL& origin) const;
+
+  // Platforms that display native notification interact with them through this
+  // object.
+  scoped_ptr<NotificationUIManager> native_notification_ui_manager_;
 
   // Weak reference. Ownership maintains with the test.
   NotificationUIManager* notification_ui_manager_for_tests_;
