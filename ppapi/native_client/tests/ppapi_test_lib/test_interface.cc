b3c161c40bee8172a72a706d220d75bbc064cd0e
phosek <phosek@chromium.org>
2016-01-13 10:46:16 -0800

    Remove unused includes from PPAPI test library
    These includes are no longer needed and they cause a compilation errors
    with nacl_x86_glibc toolchain because of the unsupported C++11 features.
    
    BUG=576820
    
    Review URL: https://codereview.chromium.org/1583883002
    
    Cr-Commit-Position: refs/heads/master@{#369226}

--- ppapi/native_client/tests/ppapi_test_lib/test_interface.cc
+++ ppapi/native_client/tests/ppapi_test_lib/test_interface.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <new>
 
-#include "base/macros.h"
 #include "native_client/src/include/nacl_macros.h"
 #include "native_client/src/shared/platform/nacl_check.h"
 
