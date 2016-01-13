90987808b37e25f89481fea18dc37e16d0818eb3
charliea <charliea@chromium.org>
2016-01-13 05:16:45 -0800

    Implements retry logic for reading some messages
    This is necessary because the BattOr can't always send frames as fast
    as we can read them. This retry logic allows for some slack in the
    sample stream.
    
    Also, the EEPROM can take a bit for the BattOr to read, and adding
    these retries helps allow for that slow read.
    
    BUG=542837
    
    Review URL: https://codereview.chromium.org/1584593002
    
    Cr-Commit-Position: refs/heads/master@{#369154}

--- tools/battor_agent/battor_agent.cc
+++ tools/battor_agent/battor_agent.cc
@@ -18,6 +18,12 @@ namespace {
 // The number of seconds that it takes a BattOr to reset.
 const uint8_t kBattOrResetTimeSeconds = 2;
 
+// The maximum number of times to retry when reading a message.
+const uint8_t kMaxReadAttempts = 20;
+
+// The number of milliseconds to wait before trying to read a message again.
+const uint8_t kReadRetryDelayMilliseconds = 1;
+
 // Returns true if the specified vector of bytes decodes to a message that is an
 // ack for the specified control message type.
 bool IsAckOfControlCommand(BattOrMessageType message_type,
@@ -105,7 +111,8 @@ BattOrAgent::BattOrAgent(
                                            ui_thread_task_runner)),
       listener_(listener),
       last_action_(Action::INVALID),
-      command_(Command::INVALID) {
+      command_(Command::INVALID),
+      num_read_attempts_(0) {
   DCHECK(thread_checker_.CalledOnValidThread());
 }
 
@@ -175,9 +182,11 @@ void BattOrAgent::OnBytesSent(bool success) {
       PerformAction(Action::READ_START_TRACING_ACK);
       return;
     case Action::SEND_EEPROM_REQUEST:
+      num_read_attempts_ = 1;
       PerformAction(Action::READ_EEPROM);
       return;
     case Action::SEND_SAMPLES_REQUEST:
+      num_read_attempts_ = 1;
       PerformAction(Action::READ_CALIBRATION_FRAME);
       return;
 
@@ -190,8 +199,23 @@ void BattOrAgent::OnMessageRead(bool success,
                                 BattOrMessageType type,
                                 scoped_ptr<vector<char>> bytes) {
   if (!success) {
-    CompleteCommand(BATTOR_ERROR_RECEIVE_ERROR);
-    return;
+    switch (last_action_) {
+      case Action::READ_EEPROM:
+      case Action::READ_CALIBRATION_FRAME:
+      case Action::READ_DATA_FRAME:
+        if (++num_read_attempts_ > kMaxReadAttempts) {
+          CompleteCommand(BATTOR_ERROR_RECEIVE_ERROR);
+          return;
+        }
+
+        PerformDelayedAction(last_action_, base::TimeDelta::FromMilliseconds(
+                                               kReadRetryDelayMilliseconds));
+        return;
+
+      default:
+        CompleteCommand(BATTOR_ERROR_RECEIVE_ERROR);
+        return;
+    }
   }
 
   switch (last_action_) {
@@ -248,6 +272,7 @@ void BattOrAgent::OnMessageRead(bool success,
         return;
       }
 
+      num_read_attempts_ = 1;
       PerformAction(Action::READ_DATA_FRAME);
       return;
     }
