980ff63700cc2ff580139a2dc8f6d6433ee9810d
robhogan <robhogan@gmail.com>
2016-01-13 03:28:36 -0800

    Apply text-indent to leading out-of-flow objects
    BUG=568851
    
    Review URL: https://codereview.chromium.org/1560533003
    
    Cr-Commit-Position: refs/heads/master@{#369143}

--- third_party/WebKit/Source/core/layout/LayoutBlockFlowLine.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlockFlowLine.cpp
@@ -932,7 +932,7 @@ void LayoutBlockFlow::layoutRunsAndFloatsInRange(LineLayoutState& layoutState,
 
         if (!logicalWidthIsAvailable) {
             for (size_t i = 0; i < lineBreaker.positionedObjects().size(); ++i)
-                setStaticPositions(LineLayoutBlockFlow(this), LineLayoutBox(lineBreaker.positionedObjects()[i]));
+                setStaticPositions(LineLayoutBlockFlow(this), LineLayoutBox(lineBreaker.positionedObjects()[i]), false);
 
             if (!layoutState.lineInfo().isEmpty())
                 layoutState.lineInfo().setFirstLine(false);
