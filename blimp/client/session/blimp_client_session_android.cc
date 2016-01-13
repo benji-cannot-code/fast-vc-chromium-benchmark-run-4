0e8ab448275a12041fa380ff1c753fa07dc918be
kmarshall <kmarshall@chromium.org>
2016-01-13 11:38:33 -0800

    Blimp: add some hard-coded actions on client startup. Doing a little bit of work on app startup reduces the need for manual intervention from the developer.
    BUG=
    R=dtrainor@chromium.org
    
    Review URL: https://codereview.chromium.org/1575493004
    
    Cr-Commit-Position: refs/heads/master@{#369243}

--- blimp/client/session/blimp_client_session_android.cc
+++ blimp/client/session/blimp_client_session_android.cc
@@ -5,10 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "blimp/client/session/blimp_client_session_android.h"
 
+#include "blimp/client/session/tab_control_feature.h"
 #include "jni/BlimpClientSession_jni.h"
 
 namespace blimp {
 namespace client {
+namespace {
+const int kDummyTabId = 0;
+}  // namespace
 
 static jlong Init(JNIEnv* env, const JavaParamRef<jobject>& jobj) {
   return reinterpret_cast<intptr_t>(new BlimpClientSessionAndroid(env, jobj));
@@ -32,6 +36,10 @@ BlimpClientSessionAndroid::BlimpClientSessionAndroid(
     const base::android::JavaParamRef<jobject>& jobj)
     : BlimpClientSession() {
   java_obj_.Reset(env, jobj);
+
+  // Create a single tab's WebContents.
+  // TODO(kmarshall): Remove this once we add tab-literacy to Blimp.
+  GetTabControlFeature()->CreateTab(kDummyTabId);
 }
 
 BlimpClientSessionAndroid::~BlimpClientSessionAndroid() {}
