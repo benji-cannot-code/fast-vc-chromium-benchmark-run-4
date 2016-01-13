0390d3e16c2831e3bfca6396377c5b16285e572c
erikchen <erikchen@chromium.org>
2016-01-13 11:21:23 -0800

    Removing ipc/content debugging for ResourceMsg.
    This CL reverts commit ee68563f8c5c4781a26b0f2f5dc310cbd6aa950d.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1574283003
    
    Cr-Commit-Position: refs/heads/master@{#369235}

--- content/child/resource_scheduling_filter.cc
+++ content/child/resource_scheduling_filter.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/location.h"
 #include "content/child/resource_dispatcher.h"
-#include "content/common/resource_messages.h"
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_message_start.h"
 #include "third_party/WebKit/public/platform/WebTaskRunner.h"
@@ -53,9 +52,6 @@ ResourceSchedulingFilter::~ResourceSchedulingFilter() {
 }
 
 bool ResourceSchedulingFilter::OnMessageReceived(const IPC::Message& message) {
-  // TODO(erikchen): Temporary code to help track http://crbug.com/527588.
-  content::CheckContentsOfResourceMessage(&message);
-
   base::AutoLock lock(request_id_to_task_runner_map_lock_);
   int request_id;
 
--- content/common/resource_messages.cc
+++ content/common/resource_messages.cc
@@ -5,24 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/common/resource_messages.h"
 
-#include "base/debug/alias.h"
-#include "ipc/ipc_message.h"
 #include "net/base/load_timing_info.h"
 #include "net/http/http_response_headers.h"
-
-namespace content {
-// TODO(erikchen): Temporary code to help track http://crbug.com/527588.
-void CheckContentsOfResourceMessage(const IPC::Message* message) {
-  if (message->type() == ResourceMsg_DataReceivedDebug::ID) {
-    ResourceMsg_DataReceivedDebug::Schema::Param arg;
-    bool success = ResourceMsg_DataReceivedDebug::Read(message, &arg);
-    CHECK(success);
-    int data_offset = base::get<1>(arg);
-    CHECK_LE(data_offset, 512 * 1024);
-    base::debug::Alias(&data_offset);
-  }
-}
-}
 
 namespace IPC {
 
