0399733b9eccfcdee9f5cfb49d484af73fa63a22
sdefresne <sdefresne@chromium.org>
2016-01-13 04:27:20 -0800

    Convert IDS_TRANSLATE_INFOBAR_NEVER_MESSAGE_IOS to branded string.
    Avoid using string interpolation to insert product name into string
    IDS_TRANSLATE_INFOBAR_NEVER_MESSAGE_IOS instead provide two branded
    variants of the strings.
    
    BUG=568486
    
    Review URL: https://codereview.chromium.org/1579213004
    
    Cr-Commit-Position: refs/heads/master@{#369147}

--- ios/chrome/browser/translate/never_translate_infobar_controller.mm
+++ ios/chrome/browser/translate/never_translate_infobar_controller.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/components_strings.h"
 #include "ios/chrome/browser/translate/translate_infobar_tags.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
-#import "ios/public/provider/chrome/browser/string_provider.h"
 #import "ios/public/provider/chrome/browser/ui/infobar_view_delegate.h"
 #import "ios/public/provider/chrome/browser/ui/infobar_view_protocol.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -46,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       translateInfoBarDelegate->original_language_name();
   [infoBarView addLabel:l10n_util::GetNSStringF(
                             IDS_TRANSLATE_INFOBAR_NEVER_MESSAGE_IOS,
-                            provider->GetStringProvider()->GetProductName(),
                             originalLanguage)];
   // Close button.
   [infoBarView addCloseButtonWithTag:TranslateInfoBarIOSTag::CLOSE
