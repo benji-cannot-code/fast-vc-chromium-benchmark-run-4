38884a74492a87266010bf10edb2c911ff6cebc8
tfarina <tfarina@chromium.org>
2016-01-12 18:22:42 -0800

    url: Get rid of UTF16Char type.
    Now that Blink was merged into Chromium, this patch can be done without
    the three-patch dance for something really trivial.
    
    This patch just changes Blink code to use base::char16 type directly
    instead of using deprecated url type.
    
    BUG=None
    R=brettw@chromium.org,dglazkov@chromium.org
    
    Review URL: https://codereview.chromium.org/1381683003
    
    Cr-Commit-Position: refs/heads/master@{#369078}

--- url/third_party/mozilla/url_parse.h
+++ url/third_party/mozilla/url_parse.h
@@ -11,10 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace url {
 
-// Deprecated, but WebKit/WebCore/platform/KURLGooglePrivate.h and
-// KURLGoogle.cpp still rely on this type.
-typedef base::char16 UTF16Char;
-
 // Component ------------------------------------------------------------------
 
 // Represents a substring for URL parsing.
