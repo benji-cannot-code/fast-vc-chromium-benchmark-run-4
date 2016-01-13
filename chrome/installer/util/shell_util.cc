adf2f1af3d0da07c45a1dd29bd36c46b16483daa
Nico Weber <thakis@chromium.org>
2016-01-13 10:32:23 -0500

    Remove useless std::move().
    https://codereview.chromium.org/1579583004/ converted a Pass() to
    std::move(), which enabled the compiler to tell us that it's not needed
    and actually harmful:
    
    ..\..\chrome\installer\util\shell_util.cc(277,10) :
        error: moving a local object in a return statement prevents copy elision
        [-Werror,-Wpessimizing-move]
      return std::move(entries);
             ^
    ..\..\chrome\installer\util\shell_util.cc(277,10) :
        note: remove std::move call here
      return std::move(entries);
             ^~~~~~~~~~
    BUG=82385
    TBR=mgiuca
    
    Review URL: https://codereview.chromium.org/1581083003 .
    
    Cr-Commit-Position: refs/heads/master@{#369176}

--- chrome/installer/util/shell_util.cc
+++ chrome/installer/util/shell_util.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <limits>
 #include <string>
-#include <utility>
 
 #include "base/bind.h"
 #include "base/command_line.h"
@@ -275,7 +274,7 @@ ScopedVector<RegistryEntry> GetChromeDelegateExecuteEntries(
         sub_path, ShellUtil::kRegDelegateExecute, app_info.delegate_clsid));
   }
 
-  return std::move(entries);
+  return entries;
 }
 
 // Gets the registry entries to register an application in the Windows registry.
