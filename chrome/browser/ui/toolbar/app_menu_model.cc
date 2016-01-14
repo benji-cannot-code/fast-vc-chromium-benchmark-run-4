62ecc8eac16b866f5bfc21ef98a32c63bb9b724b
sdefresne <sdefresne@chromium.org>
2016-01-14 00:40:18 -0800

    Componentize IDS_PRINT string so that it can be shared on iOS.
    BUG=568486
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1586663004
    
    Cr-Commit-Position: refs/heads/master@{#369369}

--- chrome/browser/ui/toolbar/app_menu_model.cc
+++ chrome/browser/ui/toolbar/app_menu_model.cc
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/user_metrics.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/common/feature_switch.h"
+#include "grit/components_strings.h"
 #include "grit/theme_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/layout.h"
--- chrome/browser/ui/views/frame/global_menu_bar_x11.cc
+++ chrome/browser/ui/views/frame/global_menu_bar_x11.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/bookmarks/common/bookmark_pref_names.h"
 #include "components/history/core/browser/top_sites.h"
 #include "components/sessions/core/tab_restore_service.h"
+#include "grit/components_strings.h"
 #include "ui/base/accelerators/menu_label_accelerator_util_linux.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/events/keycodes/keyboard_code_conversion_x.h"
