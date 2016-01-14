e4cc937d06c67eebb30fe69398c3927affc91dbf
phosek <phosek@chromium.org>
2016-01-14 15:22:51 -0800

    GN: Only link libppapi_cpp.so where necessary
    Some tests check the libraries and symbols linked into the nexe and to
    make these pass, we need to replicate the GYP behavior.
    
    BUG=462791
    
    Review URL: https://codereview.chromium.org/1583043002
    
    Cr-Commit-Position: refs/heads/master@{#369604}

--- chrome/test/data/nacl/sysconf_nprocessors_onln/sysconf_nprocessors_onln_test.cc
+++ chrome/test/data/nacl/sysconf_nprocessors_onln/sysconf_nprocessors_onln_test.cc
@@ -111,6 +111,7 @@ class MyModule : public pp::Module {
 namespace pp {
 
 // Factory function for your specialization of the Module object.
+__attribute__((visibility("default")))
 Module* CreateModule() {
   return new MyModule();
 }
