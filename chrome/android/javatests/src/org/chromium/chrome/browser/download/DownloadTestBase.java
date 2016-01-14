e24fcb5ddb11c22fcfb7e7b68ebb57a1ba7109a4
boliu <boliu@chromium.org>
2016-01-14 11:36:57 -0800

    [Android] Fix and enable http POST download tests
    These tests were running fine for 50 iterations locally. However they do
    have a systematic bug with how the CallbackHelper is used, need to get
    the current count before triggering the action for the callback, so it
    doesn't race with the actual callback trigger code. Fix this as well.
    
    BUG=286315
    
    Review URL: https://codereview.chromium.org/1579393002
    
    Cr-Commit-Position: refs/heads/master@{#369507}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadTestBase.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadTestBase.java
@@ -175,7 +175,7 @@ public abstract class DownloadTestBase extends ChromeActivityTestCaseBase<Chrome
     private final EnqueueHttpGetDownloadCallbackHelper mEnqueueHttpGetDownloadCallbackHelper =
             new EnqueueHttpGetDownloadCallbackHelper();
     private String mLastDownloadFilePath;
-    private CallbackHelper mHttpDownloadFinished;
+    private final CallbackHelper mHttpDownloadFinished = new CallbackHelper();
     private DownloadManagerService mSavedDownloadManagerService;
 
     protected String getLastDownloadFile() {
@@ -197,14 +197,17 @@ public abstract class DownloadTestBase extends ChromeActivityTestCaseBase<Chrome
         return downloadName.startsWith(fileName) && downloadName.endsWith(extension);
     }
 
-    public boolean waitForChromeDownloadToFinish() throws InterruptedException {
+    public int getChromeDownloadCallCount() {
+        return mHttpDownloadFinished.getCallCount();
+    }
+
+    public boolean waitForChromeDownloadToFinish(int callCount) throws InterruptedException {
         boolean eventReceived = true;
         try {
-            mHttpDownloadFinished.waitForCallback(0, 1, 5, TimeUnit.SECONDS);
+            mHttpDownloadFinished.waitForCallback(callCount, 1, 5, TimeUnit.SECONDS);
         } catch (TimeoutException e) {
             eventReceived = false;
         }
-        mHttpDownloadFinished = new CallbackHelper();
         return eventReceived;
     }
 
@@ -252,8 +255,6 @@ public abstract class DownloadTestBase extends ChromeActivityTestCaseBase<Chrome
                         DownloadManagerService.getDownloadManagerService(context));
             }
         });
-
-        mHttpDownloadFinished = new CallbackHelper();
     }
 
     @Override
