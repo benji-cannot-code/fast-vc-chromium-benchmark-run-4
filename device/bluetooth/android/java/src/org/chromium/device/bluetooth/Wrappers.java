f465ca48b663c59b968e3cde5147e10c0b4be616
scheib <scheib@chromium.org>
2016-01-14 13:51:48 -0800

    bluetooth: android: Enable characteristic change notification events.
    Set Notify or Indicate on remote devices when enabling
    notifications, and route changed values via
    onCharacteristicChanged.
    
    BUG=551634, 566533
    
    Review URL: https://codereview.chromium.org/1502833002
    
    Cr-Commit-Position: refs/heads/master@{#369559}

--- device/bluetooth/android/java/src/org/chromium/device/bluetooth/Wrappers.java
+++ device/bluetooth/android/java/src/org/chromium/device/bluetooth/Wrappers.java
@@ -356,6 +356,14 @@ class Wrappers {
         }
 
         @Override
+        public void onCharacteristicChanged(
+                BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            Log.i(TAG, "wrapper onCharacteristicChanged.");
+            mWrapperCallback.onCharacteristicChanged(
+                    mDeviceWrapper.mCharacteristicsToWrappers.get(characteristic));
+        }
+
+        @Override
         public void onCharacteristicRead(
                 BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
             mWrapperCallback.onCharacteristicRead(
@@ -391,6 +399,8 @@ class Wrappers {
      * call.
      */
     abstract static class BluetoothGattCallbackWrapper {
+        public abstract void onCharacteristicChanged(
+                BluetoothGattCharacteristicWrapper characteristic);
         public abstract void onCharacteristicRead(
                 BluetoothGattCharacteristicWrapper characteristic, int status);
         public abstract void onCharacteristicWrite(
@@ -450,6 +460,22 @@ class Wrappers {
                 BluetoothGattCharacteristic characteristic, BluetoothDeviceWrapper deviceWrapper) {
             mCharacteristic = characteristic;
             mDeviceWrapper = deviceWrapper;
+        }
+
+        public BluetoothGattDescriptorWrapper getDescriptor(UUID uuid) {
+            BluetoothGattDescriptor descriptor = mCharacteristic.getDescriptor(uuid);
+            if (descriptor == null) {
+                return null;
+            }
+
+            BluetoothGattDescriptorWrapper descriptorWrapper =
+                    mDeviceWrapper.mDescriptorsToWrappers.get(descriptor);
+
+            if (descriptorWrapper == null) {
+                descriptorWrapper = new BluetoothGattDescriptorWrapper(descriptor);
+                mDeviceWrapper.mDescriptorsToWrappers.put(descriptor, descriptorWrapper);
+            }
+            return descriptorWrapper;
         }
 
         public List<BluetoothGattDescriptorWrapper> getDescriptors() {
