4cb2a1e00be2f49b3e1374c9c9ab01ac3d739049
hush <hush@chromium.org>
2016-01-14 10:42:51 -0800

    Make sure only frames in the current frame tree send IPC to AwRenderViewHostExt
    There are 3 IPC messages from Renderer to Browser in question:
    1) AwViewHostMsg_DocumentHasImagesResponse
    2) AwViewHostMsg_UpdateHitTestData
    3) AwViewHostMsg_OnContentsSizeChanged
    
    1) This is a response to an IPC from browser
    (AwViewMsg_DocumentHasImages) to do a query on the document of the main
    frame as the current implementation. This CL matches that
    implementation.
    2) This IPC can come from blink changing focused node, or as a response
    to Browser doing hit test at a specific location
    (AwContents::RequestNewHitTestResultAt). It could be from a main frame
    or a subframe of the current frame tree, because the focused node could
    be anywhere.
    3) This is sent from main frame whenever its content size changed.
    
    This CL moves logic around these IPC messages into AwRenderFrameExt
    (a RenderFrameObserver). Other logic in AwRenderViewExt remains, until
    RenderView is removed.
    
    BUG=570906
    
    Review URL: https://codereview.chromium.org/1564613002
    
    Cr-Commit-Position: refs/heads/master@{#369480}

--- android_webview/renderer/aw_render_frame_ext.h
+++ android_webview/renderer/aw_render_frame_ext.h
@@ -8,6 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "content/public/renderer/render_frame_observer.h"
+#include "ui/gfx/geometry/point_f.h"
+#include "ui/gfx/geometry/size.h"
+#include "ui/gfx/geometry/size_f.h"
+
+namespace blink {
+enum WebMeaningfulLayout;
+class WebView;
+}
 
 namespace android_webview {
 
@@ -21,9 +29,29 @@ class AwRenderFrameExt : public content::RenderFrameObserver {
  private:
   ~AwRenderFrameExt() override;
 
-  // RenderFrame::Observer:
+  // RenderFrameObserver:
   void DidCommitProvisionalLoad(bool is_new_navigation,
                                 bool is_same_page_navigation) override;
+
+  bool OnMessageReceived(const IPC::Message& message) override;
+  void FocusedNodeChanged(const blink::WebNode& node) override;
+
+  void OnDocumentHasImagesRequest(int id);
+  void OnDoHitTest(const gfx::PointF& touch_center,
+                   const gfx::SizeF& touch_area);
+
+  void OnSetTextZoomFactor(float zoom_factor);
+
+  void OnResetScrollAndScaleState();
+
+  void OnSetInitialPageScale(double page_scale_factor);
+
+  void OnSetBackgroundColor(SkColor c);
+
+  void OnSmoothScroll(int target_x, int target_y, long duration_ms);
+
+  blink::WebView* GetWebView();
+
   DISALLOW_COPY_AND_ASSIGN(AwRenderFrameExt);
 };
 
