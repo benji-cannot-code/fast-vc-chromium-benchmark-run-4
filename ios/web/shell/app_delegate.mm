9676a320c46809605bc14d0e75233557b014cea3
baxley <baxley@chromium.org>
2016-01-14 14:45:35 -0800

    Set the keyWindow earlier for the web shell.
    Make sure the keyWindow is set before different parts of the
    UI load, which may rely upon the keyWindow.
    
    BUG=577848
    
    Review URL: https://codereview.chromium.org/1590083002
    
    Cr-Commit-Position: refs/heads/master@{#369587}

--- ios/web/shell/app_delegate.mm
+++ ios/web/shell/app_delegate.mm
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (BOOL)application:(UIApplication*)application
     didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
   _window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+  [self.window makeKeyAndVisible];
   self.window.backgroundColor = [UIColor whiteColor];
 
   _delegate.reset(new web::ShellMainDelegate());
@@ -41,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   base::scoped_nsobject<ViewController> controller(
       [[ViewController alloc] initWithBrowserState:browserState]);
   self.window.rootViewController = controller;
-  [self.window makeKeyAndVisible];
   return YES;
 }
 
