a1adc118d91f2a6927884efd29fc92e7434f03ab
caitpotter88 <caitpotter88@gmail.com>
2016-01-14 11:06:20 -0800

    [v8] don't crash when ArrayBuffer allocation fails
    When ArrayBufferContents::Allocator fails to allocate a backing store,
    return a nullptr so that v8 knows to throw a RangeError (per
    http://tc39.github.io/ecma262/#sec-createbytedatablock). This
    replaces the current behaviour of failing an assertion and crashing
    the process
    
    BUG=536816, v8:4639
    R=jochen@chromium.org, littledan@chromium.org, haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/1577783004
    
    Cr-Commit-Position: refs/heads/master@{#369488}

--- third_party/WebKit/Source/wtf/ArrayBufferContents.h
+++ third_party/WebKit/Source/wtf/ArrayBufferContents.h
@@ -73,6 +73,7 @@ public:
     void copyTo(ArrayBufferContents& other);
 
     static void allocateMemory(size_t, InitializationPolicy, void*&);
+    static void allocateMemoryOrNull(size_t, InitializationPolicy, void*&);
     static void freeMemory(void*, size_t);
     static void setAdjustAmoutOfExternalAllocatedMemoryFunction(AdjustAmountOfExternalAllocatedMemoryFunction function)
     {
@@ -81,6 +82,7 @@ public:
     }
 
 private:
+    static void allocateMemoryWithFlags(size_t, InitializationPolicy, int, void*&);
     class DataHolder : public ThreadSafeRefCounted<DataHolder> {
         WTF_MAKE_NONCOPYABLE(DataHolder);
     public:
