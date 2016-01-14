2b9e5b9baa42ae551888fed183f8964a24790649
ellyjones <ellyjones@chromium.org>
2016-01-14 10:13:15 -0800

    InfobarController: give close button a localized title
    Without this title, the close button is read as "button" by VoiceOver.
    
    BUG=420916
    
    Review URL: https://codereview.chromium.org/1589013002
    
    Cr-Commit-Position: refs/heads/master@{#369465}

--- chrome/browser/ui/cocoa/infobars/infobar_controller.mm
+++ chrome/browser/ui/cocoa/infobars/infobar_controller.mm
@@ -16,8 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/infobars/infobar_container_controller.h"
 #import "chrome/browser/ui/cocoa/infobars/infobar_gradient_view.h"
 #import "chrome/browser/ui/cocoa/location_bar/location_bar_view_mac.h"
+#include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
 #import "ui/base/cocoa/controls/hyperlink_text_view.h"
+#include "ui/base/l10n/l10n_util_mac.h"
 #include "ui/gfx/image/image.h"
 #include "ui/resources/grit/ui_resources.h"
 
@@ -50,6 +52,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                    forButtonState:image_button_cell::kPressedState];
   [[closeButton_ cell] setImageID:IDR_CLOSE_1
                    forButtonState:image_button_cell::kDisabledState];
+
+  [closeButton_ setTitle:l10n_util::GetNSString(IDS_ACCNAME_CLOSE)];
 
   if (![self delegate]->GetIcon().IsEmpty()) {
     [image_ setImage:[self delegate]->GetIcon().ToNSImage()];
