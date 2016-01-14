68ac385df8d1b579ad68750b37145de09cf600b6
kinuko <kinuko@chromium.org>
2016-01-14 08:39:25 -0800

    Remove WebUnitTestSupport::readFromFile which is no longer used
    The implementation is moved into blink.
    
    BUG=571332
    
    Review URL: https://codereview.chromium.org/1587613002
    
    Cr-Commit-Position: refs/heads/master@{#369431}

--- third_party/WebKit/public/platform/WebUnitTestSupport.h
+++ third_party/WebKit/public/platform/WebUnitTestSupport.h
@@ -60,8 +60,6 @@ public:
     // Constructs a WebLayerTreeView set up with reasonable defaults for
     // testing.
     virtual WebLayerTreeView* createLayerTreeViewForTesting() { return nullptr; }
-
-    virtual WebData readFromFile(const WebString& path) { return WebData(); }
 };
 
 }
