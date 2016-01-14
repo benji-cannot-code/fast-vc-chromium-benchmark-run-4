57e77bba79af6dc1877930708e2fdcc1cd181e43
wierichs <wierichs@google.com>
2016-01-13 23:33:33 -0800

    Removal of ash related code from chrome_native_app_window_views_win
    BUG=558054
    
    Review URL: https://codereview.chromium.org/1588853002
    
    Cr-Commit-Position: refs/heads/master@{#369361}

--- chrome/browser/ui/views/apps/chrome_native_app_window_views_win.h
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views_win.h
@@ -31,7 +31,6 @@ class ChromeNativeAppWindowViewsWin : public ChromeNativeAppWindowViewsAura {
       const web_app::ShortcutInfo& shortcut_info);
 
   HWND GetNativeAppWindowHWND() const;
-  bool IsRunningInAsh();
   void EnsureCaptionStyleSet();
 
   // Overridden from ChromeNativeAppWindowViews:
@@ -53,7 +52,6 @@ class ChromeNativeAppWindowViewsWin : public ChromeNativeAppWindowViewsAura {
   GlassAppWindowFrameViewWin* glass_frame_view_;
 
   // The Windows Application User Model ID identifying the app.
-  // Not set for windows running inside Ash.
   base::string16 app_model_id_;
 
   // Whether the InitParams indicated that this window should be translucent.
