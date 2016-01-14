3310d272b1334aa9394e65a67b8c504d2d1a8d3f
dpapad <dpapad@chromium.org>
2016-01-14 08:58:32 -0800

    MD Settings: Make obsolete "Manage search engines" UI usable.
    This is done purely for being able to exercise the backend code. This UI should
    be replaced as soon as the new mocks are available.
    
    BUG=495858
    
    Review URL: https://codereview.chromium.org/1585783002
    
    Cr-Commit-Position: refs/heads/master@{#369435}

--- chrome/browser/resources/settings/search_page/search_page.js
+++ chrome/browser/resources/settings/search_page/search_page.js
@@ -83,9 +83,4 @@ Polymer({
   onSearchEnginesTap_: function() {
     this.$.pages.setSubpageChain(['search-engines']);
   },
-
-  /** @private */
-  onSearchEnginesAdvancedTap_: function() {
-    this.$.pages.setSubpageChain(['search-engines', 'search-engines-advanced']);
-  },
 });
