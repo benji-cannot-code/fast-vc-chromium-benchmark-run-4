38c2f626cfe6f038619fbc54c4b856c8396ac3f2
charliea <charliea@chromium.org>
2016-01-14 03:46:47 -0800

    tools/battor_agent: Fixes unit tests broken due to control type reorder
    Here's the offending CL: http://crrev.com/1577343004
    
    Moving the control types caused the "reset" command to move to 0x01.
    That means that it now needs escaping in messages, but I forgot to
    update the unit tests to reflect this. Thinking this was a trivial
    change, I also didn't run the unit tests before submitting.
    Lesson learned.
    
    BUG=542837
    
    Review URL: https://codereview.chromium.org/1582983002
    
    Cr-Commit-Position: refs/heads/master@{#369392}

--- tools/battor_agent/battor_connection_impl_unittest.cc
+++ tools/battor_agent/battor_connection_impl_unittest.cc
@@ -136,22 +136,17 @@ TEST_F(BattOrConnectionImplTest, ResetSendsCorrectBytes) {
   SendControlMessage(BATTOR_CONTROL_MESSAGE_TYPE_RESET, 0, 0);
 
   const char expected_data[] = {
-      BATTOR_CONTROL_BYTE_START,
-      BATTOR_MESSAGE_TYPE_CONTROL,
-      BATTOR_CONTROL_MESSAGE_TYPE_RESET,
-      BATTOR_CONTROL_BYTE_ESCAPE,
-      0x00,
-      BATTOR_CONTROL_BYTE_ESCAPE,
-      0x00,
-      BATTOR_CONTROL_BYTE_ESCAPE,
-      0x00,
-      BATTOR_CONTROL_BYTE_ESCAPE,
-      0x00,
+      BATTOR_CONTROL_BYTE_START,  BATTOR_MESSAGE_TYPE_CONTROL,
+      BATTOR_CONTROL_BYTE_ESCAPE, BATTOR_CONTROL_MESSAGE_TYPE_RESET,
+      BATTOR_CONTROL_BYTE_ESCAPE, 0x00,
+      BATTOR_CONTROL_BYTE_ESCAPE, 0x00,
+      BATTOR_CONTROL_BYTE_ESCAPE, 0x00,
+      BATTOR_CONTROL_BYTE_ESCAPE, 0x00,
       BATTOR_CONTROL_BYTE_END,
   };
 
   ASSERT_TRUE(GetSendSuccess());
-  ASSERT_EQ(0, std::memcmp(ReadMessageRaw(12)->data(), expected_data, 12));
+  ASSERT_EQ(0, std::memcmp(ReadMessageRaw(13)->data(), expected_data, 13));
 }
 
 TEST_F(BattOrConnectionImplTest, ReadMessageControlMessage) {
@@ -161,6 +156,7 @@ TEST_F(BattOrConnectionImplTest, ReadMessageControlMessage) {
   const char data[] = {
       BATTOR_CONTROL_BYTE_START,
       BATTOR_MESSAGE_TYPE_CONTROL,
+      BATTOR_CONTROL_BYTE_ESCAPE,
       BATTOR_CONTROL_MESSAGE_TYPE_RESET,
       0x04,
       0x04,
@@ -168,7 +164,7 @@ TEST_F(BattOrConnectionImplTest, ReadMessageControlMessage) {
       0x04,
       BATTOR_CONTROL_BYTE_END,
   };
-  SendBytesRaw(data, 8);
+  SendBytesRaw(data, 9);
   ReadMessage(BATTOR_MESSAGE_TYPE_CONTROL);
 
   const char expected[] = {BATTOR_CONTROL_MESSAGE_TYPE_RESET, 0x04, 0x04, 0x04,
@@ -187,6 +183,7 @@ TEST_F(BattOrConnectionImplTest, ReadMessageInvalidType) {
   const char data[] = {
       BATTOR_CONTROL_BYTE_START,
       UINT8_MAX,
+      BATTOR_CONTROL_BYTE_ESCAPE,
       BATTOR_CONTROL_MESSAGE_TYPE_RESET,
       0x04,
       0x04,
@@ -194,7 +191,7 @@ TEST_F(BattOrConnectionImplTest, ReadMessageInvalidType) {
       0x04,
       BATTOR_CONTROL_BYTE_END,
   };
-  SendBytesRaw(data, 6);
+  SendBytesRaw(data, 7);
   ReadMessage(BATTOR_MESSAGE_TYPE_CONTROL);
 
   ASSERT_TRUE(IsReadComplete());
@@ -206,10 +203,13 @@ TEST_F(BattOrConnectionImplTest, ReadMessageEndsMidMessageByte) {
   ASSERT_TRUE(GetOpenSuccess());
 
   const char data[] = {
-      BATTOR_CONTROL_BYTE_START, BATTOR_MESSAGE_TYPE_CONTROL,
-      BATTOR_CONTROL_MESSAGE_TYPE_RESET, 0x04,
+      BATTOR_CONTROL_BYTE_START,
+      BATTOR_MESSAGE_TYPE_CONTROL,
+      BATTOR_CONTROL_BYTE_ESCAPE,
+      BATTOR_CONTROL_MESSAGE_TYPE_RESET,
+      0x04,
   };
-  SendBytesRaw(data, 4);
+  SendBytesRaw(data, 5);
   ReadMessage(BATTOR_MESSAGE_TYPE_CONTROL);
 
   ASSERT_TRUE(IsReadComplete());
@@ -223,13 +223,14 @@ TEST_F(BattOrConnectionImplTest, ReadMessageMissingEndByte) {
   const char data[] = {
       BATTOR_CONTROL_BYTE_START,
       BATTOR_MESSAGE_TYPE_CONTROL,
+      BATTOR_CONTROL_BYTE_ESCAPE,
       BATTOR_CONTROL_MESSAGE_TYPE_RESET,
       0x04,
       0x04,
       0x04,
       0x04,
   };
-  SendBytesRaw(data, 5);
+  SendBytesRaw(data, 6);
   ReadMessage(BATTOR_MESSAGE_TYPE_CONTROL);
 
   ASSERT_TRUE(IsReadComplete());
@@ -243,6 +244,7 @@ TEST_F(BattOrConnectionImplTest, ReadMessageWithEscapeCharacters) {
   const char data[] = {
       BATTOR_CONTROL_BYTE_START,
       BATTOR_MESSAGE_TYPE_CONTROL,
+      BATTOR_CONTROL_BYTE_ESCAPE,
       BATTOR_CONTROL_MESSAGE_TYPE_RESET,
       BATTOR_CONTROL_BYTE_ESCAPE,
       0x00,
@@ -251,7 +253,7 @@ TEST_F(BattOrConnectionImplTest, ReadMessageWithEscapeCharacters) {
       0x04,
       BATTOR_CONTROL_BYTE_END,
   };
-  SendBytesRaw(data, 9);
+  SendBytesRaw(data, 10);
   ReadMessage(BATTOR_MESSAGE_TYPE_CONTROL);
 
   const char expected[] = {BATTOR_CONTROL_MESSAGE_TYPE_RESET, 0x00};
@@ -330,11 +332,14 @@ TEST_F(BattOrConnectionImplTest, ReadMessageFailsWithControlButExpectingAck) {
   ASSERT_TRUE(GetOpenSuccess());
 
   const char data[] = {
-      BATTOR_CONTROL_BYTE_START,         BATTOR_MESSAGE_TYPE_CONTROL_ACK,
-      BATTOR_CONTROL_MESSAGE_TYPE_RESET, 0x04,
+      BATTOR_CONTROL_BYTE_START,
+      BATTOR_MESSAGE_TYPE_CONTROL_ACK,
+      BATTOR_CONTROL_BYTE_ESCAPE,
+      BATTOR_CONTROL_MESSAGE_TYPE_RESET,
+      0x04,
       BATTOR_CONTROL_BYTE_END,
   };
-  SendBytesRaw(data, 5);
+  SendBytesRaw(data, 6);
   ReadMessage(BATTOR_MESSAGE_TYPE_CONTROL);
 
   ASSERT_TRUE(IsReadComplete());
