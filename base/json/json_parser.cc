6751550baab7a4a59bf0d016ca355991ba4a69e0
horo <horo@chromium.org>
2016-01-14 00:41:44 -0800

    Pass Manifest JSON paser error line and column number to console.
    BUG=540493
    
    Review URL: https://codereview.chromium.org/1571633002
    
    Cr-Commit-Position: refs/heads/master@{#369371}

--- base/json/json_parser.cc
+++ base/json/json_parser.cc
@@ -276,6 +276,14 @@ std::string JSONParser::GetErrorMessage() const {
       JSONReader::ErrorCodeToString(error_code_));
 }
 
+int JSONParser::error_line() const {
+  return error_line_;
+}
+
+int JSONParser::error_column() const {
+  return error_column_;
+}
+
 // StringBuilder ///////////////////////////////////////////////////////////////
 
 JSONParser::StringBuilder::StringBuilder()
