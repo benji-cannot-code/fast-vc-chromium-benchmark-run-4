7fbb59616e729e3d6fcec661c778c4fdabe066be
chrisha <chrisha@chromium.org>
2016-01-14 07:30:49 -0800

    [win] Create a SyzyAsan/Chrome/Kasko/Crashpad integration test.
    BUG=
    
    Review URL: https://codereview.chromium.org/1582613002
    
    Cr-Commit-Position: refs/heads/master@{#369416}

---
+++ chrome/test/kasko/py/kasko/__init__.py
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import config
+import crash_server
+import exceptions
+import process
+import report
+import util
