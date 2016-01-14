74cc62601c0f9d82860404702a0392ecc125fa17
ihf <ihf@chromium.org>
2016-01-14 15:24:45 -0800

    Remove Pepper Flash hw video field trial.
    Flash uses hardware decode internally now.
    
    BUG=None.
    TEST=Compiles.
    
    Review URL: https://codereview.chromium.org/1570983002
    
    Cr-Commit-Position: refs/heads/master@{#369605}

--- content/public/common/content_constants.cc
+++ content/public/common/content_constants.cc
@@ -41,7 +41,4 @@ const int kStatsMaxCounters = 3000;
 
 const int kHistogramSynchronizerReservedSequenceNumber = 0;
 
-const char kFlashHwVideoDecodeFieldTrialName[] = "FlashHardwareVideoDecode";
-const char kFlashHwVideoDecodeFieldTrialEnabledName[] = "HwVideo";
-
 }  // namespace content
