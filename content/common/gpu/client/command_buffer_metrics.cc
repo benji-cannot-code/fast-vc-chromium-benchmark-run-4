ca0560a3f090d0e8a87db6574aee2382ba110dc2
piman <piman@chromium.org>
2016-01-14 11:40:55 -0800

    Harden CommandBufferProxyImpl
    This avoids a class of errors where a message/reply from the GPU process
    is received that indicates invalid state.
    
    BUG=563964
    
    Review URL: https://codereview.chromium.org/1581273002
    
    Cr-Commit-Position: refs/heads/master@{#369509}

--- content/common/gpu/client/command_buffer_metrics.cc
+++ content/common/gpu/client/command_buffer_metrics.cc
@@ -25,8 +25,10 @@ enum CommandBufferContextLostReason {
   CONTEXT_LOST_UNKNOWN,
   CONTEXT_LOST_OUT_OF_MEMORY,
   CONTEXT_LOST_MAKECURRENT_FAILED,
+  CONTEXT_LOST_INVALID_GPU_MESSAGE,
   // Add new values here and update _MAX_ENUM.
-  CONTEXT_LOST_REASON_MAX_ENUM = CONTEXT_LOST_MAKECURRENT_FAILED
+  // Also update //tools/metrics/histograms/histograms.xml
+  CONTEXT_LOST_REASON_MAX_ENUM = CONTEXT_LOST_INVALID_GPU_MESSAGE
 };
 
 CommandBufferContextLostReason GetContextLostReason(
@@ -46,6 +48,8 @@ CommandBufferContextLostReason GetContextLostReason(
         return CONTEXT_LOST_MAKECURRENT_FAILED;
       case gpu::error::kGpuChannelLost:
         return CONTEXT_LOST_GPU_CHANNEL_ERROR;
+      case gpu::error::kInvalidGpuMessage:
+        return CONTEXT_LOST_INVALID_GPU_MESSAGE;
     }
   }
   switch (error) {
