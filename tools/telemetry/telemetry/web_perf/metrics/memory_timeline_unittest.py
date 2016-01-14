77670c0cd7797ad14c6b4645ce2a410a11c60cfb
ssid <ssid@chromium.org>
2016-01-14 08:30:07 -0800

    [telemetry] Add support for composable process dumps in memory-infra
    The memory-infra process dumps can be split across multiple process
    memory dump events in a trace. Telemetry should support merging of them.
    
    BUG=461788
    
    Review URL: https://codereview.chromium.org/1553183002
    
    Cr-Commit-Position: refs/heads/master@{#369428}

--- tools/telemetry/telemetry/web_perf/metrics/memory_timeline_unittest.py
+++ tools/telemetry/telemetry/web_perf/metrics/memory_timeline_unittest.py
@@ -19,6 +19,7 @@ def MockProcessDumpEvent(dump_id, name, start, memory_usage):
   process_dump.dump_id = dump_id
   process_dump.process_name = name
   process_dump.start = start
+  process_dump.end = start
   if memory_usage is None:
     memory_usage = {}
   elif not isinstance(memory_usage, dict):
