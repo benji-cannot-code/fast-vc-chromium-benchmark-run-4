5a39898c00197c3bda63817209c8823895b33f81
oshima <oshima@chromium.org>
2016-01-14 10:39:37 -0800

    Scale scrollbar in use-zoom-for-dsf mode
    * Scale the size of scrollbar
    * Compute the trackRect in ScrollbarThemeNonMacCommon using width/height of the reference scrollbar to compute thickness, instead of using scrollbarThickness
    
    BUG=485650
    TEST=manual. Layout test will be covered in a separate CL that adds virtual test suite for this mode.
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1560403002
    
    Cr-Commit-Position: refs/heads/master@{#369477}

--- third_party/WebKit/Source/platform/scroll/Scrollbar.h
+++ third_party/WebKit/Source/platform/scroll/Scrollbar.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class GraphicsContext;
+class HostWindow;
 class IntRect;
 class PlatformGestureEvent;
 class PlatformMouseEvent;
@@ -48,7 +49,7 @@ class ScrollbarTheme;
 
 class PLATFORM_EXPORT Scrollbar : public Widget, public ScrollbarThemeClient {
 public:
-    static PassRefPtrWillBeRawPtr<Scrollbar> create(ScrollableArea*, ScrollbarOrientation, ScrollbarControlSize);
+    static PassRefPtrWillBeRawPtr<Scrollbar> create(ScrollableArea*, ScrollbarOrientation, ScrollbarControlSize, HostWindow*);
 
     // Theme object ownership remains with the caller and it must outlive the scrollbar.
     static PassRefPtrWillBeRawPtr<Scrollbar> createForTesting(ScrollableArea*, ScrollbarOrientation, ScrollbarControlSize, ScrollbarTheme*);
@@ -93,6 +94,8 @@ public:
     void visibilityChanged() override;
     bool enabled() const override { return m_enabled; }
     void setEnabled(bool) override;
+
+    int scrollbarThickness() const;
 
     // Called by the ScrollableArea when the scroll offset changes.
     void offsetDidChange();
@@ -178,7 +181,7 @@ public:
     DECLARE_VIRTUAL_TRACE();
 
 protected:
-    Scrollbar(ScrollableArea*, ScrollbarOrientation, ScrollbarControlSize, ScrollbarTheme* = 0);
+    Scrollbar(ScrollableArea*, ScrollbarOrientation, ScrollbarControlSize, HostWindow* = 0, ScrollbarTheme* = 0);
 
     void autoscrollTimerFired(Timer<Scrollbar>*);
     void startTimerIfNeeded(double delay);
@@ -191,6 +194,7 @@ protected:
     ScrollbarOrientation m_orientation;
     ScrollbarControlSize m_controlSize;
     ScrollbarTheme& m_theme;
+    HostWindow* m_hostWindow;
 
     int m_visibleSize;
     int m_totalSize;
--- third_party/WebKit/Source/platform/scroll/ScrollbarThemeNonMacCommon.cpp
+++ third_party/WebKit/Source/platform/scroll/ScrollbarThemeNonMacCommon.cpp
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/GraphicsContextStateSaver.h"
 #include "platform/graphics/paint/DrawingRecorder.h"
 #include "platform/scroll/ScrollableArea.h"
-#include "platform/scroll/ScrollbarThemeClient.h"
 
 namespace blink {
 
@@ -74,15 +73,14 @@ IntRect ScrollbarThemeNonMacCommon::trackRect(const ScrollbarThemeClient& scroll
 {
     // The track occupies all space between the two buttons.
     IntSize bs = buttonSize(scrollbar);
-    int thickness = scrollbarThickness(scrollbar.controlSize());
     if (scrollbar.orientation() == HorizontalScrollbar) {
         if (scrollbar.width() <= 2 * bs.width())
             return IntRect();
-        return IntRect(scrollbar.x() + bs.width(), scrollbar.y(), scrollbar.width() - 2 * bs.width(), thickness);
+        return IntRect(scrollbar.x() + bs.width(), scrollbar.y(), scrollbar.width() - 2 * bs.width(), scrollbar.height());
     }
     if (scrollbar.height() <= 2 * bs.height())
         return IntRect();
-    return IntRect(scrollbar.x(), scrollbar.y() + bs.height(), thickness, scrollbar.height() - 2 * bs.height());
+    return IntRect(scrollbar.x(), scrollbar.y() + bs.height(), scrollbar.width(), scrollbar.height() - 2 * bs.height());
 }
 
 void ScrollbarThemeNonMacCommon::paintTrackBackground(GraphicsContext& context, const ScrollbarThemeClient& scrollbar, const IntRect& rect)
