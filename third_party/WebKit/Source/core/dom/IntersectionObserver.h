2cda10b627726015f3f5855bddcb906923b5f881
szager <szager@chromium.org>
2016-01-14 01:08:32 -0800

    Add virtual destructors to IntersectionObserverCallback
    BUG=577478,576797,577028
    R=haraken@chromium.org,dcheng@chromium.org
    
    Review URL: https://codereview.chromium.org/1583173002
    
    Cr-Commit-Position: refs/heads/master@{#369375}

--- third_party/WebKit/Source/core/dom/IntersectionObserver.h
+++ third_party/WebKit/Source/core/dom/IntersectionObserver.h
@@ -21,6 +21,7 @@ class ExceptionState;
 class IntersectionObserverCallback;
 class IntersectionObserverInit;
 
+// TODO(oilpan): Switch to GarbageCollected<> after oilpan ships
 class IntersectionObserver final : public GarbageCollectedFinalized<IntersectionObserver>, public ScriptWrappable {
     DEFINE_WRAPPERTYPEINFO();
 
