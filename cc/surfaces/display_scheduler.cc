b3917ce6786c784d3067dcc7ee2f5e6333629d68
boliu <boliu@chromium.org>
2016-01-14 10:59:55 -0800

    cc: Fix display_scheduler.cc NOTIMPLEMENTED
    OnBeginFrameSourcePausedChanged(false) can actually happen because
    BeginFrameSourceBase::AddObserver calls it. But it's never set to true
    for DisplayScheduler.
    
    BUG=577563
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1582183003
    
    Cr-Commit-Position: refs/heads/master@{#369487}

--- cc/surfaces/display_scheduler.cc
+++ cc/surfaces/display_scheduler.cc
@@ -156,7 +156,8 @@ bool DisplayScheduler::OnBeginFrameDerivedImpl(const BeginFrameArgs& args) {
 void DisplayScheduler::OnBeginFrameSourcePausedChanged(bool paused) {
   // BeginFrameSources used with DisplayScheduler do not make use of this
   // feature.
-  NOTIMPLEMENTED();
+  if (paused)
+    NOTIMPLEMENTED();
 }
 
 base::TimeTicks DisplayScheduler::DesiredBeginFrameDeadlineTime() {
