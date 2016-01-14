5cd2f486674685391c77965b60cb42e300529d26
amistry <amistry@chromium.org>
2016-01-14 02:50:52 -0800

    Small set of cleanups to MessagePumpMojo.
    These are:
    1. Change RemoveInvalidHandle to SignalHandleError to mirror SignalHandleReady.
    2. Use SignalHandleError wherever handler->OnHandleError is called.
    
    Review URL: https://codereview.chromium.org/1573383002
    
    Cr-Commit-Position: refs/heads/master@{#369389}

--- mojo/message_pump/message_pump_mojo.h
+++ mojo/message_pump/message_pump_mojo.h
@@ -107,10 +107,6 @@ class MOJO_MESSAGE_PUMP_EXPORT MessagePumpMojo : public base::MessagePump {
   // if any handles were ready and processed.
   bool ProcessReadyHandles();
 
-  // Removes the given invalid handle. This is called if MojoGetReadyHandles
-  // finds an invalid or closed handle.
-  void RemoveInvalidHandle(MojoResult result, Handle handle);
-
   // Removes any handles that have expired their deadline. Runs the handler's
   // OnHandleError() function with |MOJO_RESULT_DEADLINE_EXCEEDED| as the
   // result. Returns |true| if any handles were removed.
@@ -124,6 +120,11 @@ class MOJO_MESSAGE_PUMP_EXPORT MessagePumpMojo : public base::MessagePump {
   // Run |OnHandleReady()| for the handler registered with |handle|. |handle|
   // must be registered.
   void SignalHandleReady(Handle handle);
+
+  // Run |OnHandleError()| for the handler registered with |handle| and the
+  // error code |result|. |handle| must be registered, and will be removed
+  // before calling |OnHandleError()|.
+  void SignalHandleError(Handle handle, MojoResult result);
 
   void WillSignalHandler();
   void DidSignalHandler();
