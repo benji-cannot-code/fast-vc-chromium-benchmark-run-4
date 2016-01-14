c43bc1fb79b34420c165b535f08f06d0a654b02c
jam <jam@chromium.org>
2016-01-13 21:08:45 -0800

    Avoid shutdown leak in RawChannel and MessgePipeDispatcher.
    This is to keep LSAN bots happy.
    
    BUG=561803,574325
    TBR=ben
    
    Review URL: https://codereview.chromium.org/1589693002
    
    Cr-Commit-Position: refs/heads/master@{#369332}

--- mojo/edk/system/raw_channel.cc
+++ mojo/edk/system/raw_channel.cc
@@ -727,8 +727,13 @@ void RawChannel::CallOnReadCompleted(IOResult io_result, size_t bytes_read) {
 }
 
 void RawChannel::WillDestroyCurrentMessageLoop() {
-  base::AutoLock locker(read_lock_);
-  OnReadCompletedNoLock(IO_FAILED_SHUTDOWN, 0);
+  {
+    base::AutoLock locker(read_lock_);
+    OnReadCompletedNoLock(IO_FAILED_SHUTDOWN, 0);
+  }
+  // The PostTask inside Shutdown() will never be called, so manually call it
+  // here to avoid leaks in LSAN builds.
+  Shutdown();
 }
 
 }  // namespace edk
