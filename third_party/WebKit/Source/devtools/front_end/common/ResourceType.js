c4e7fccfaea68be67e93aa9e2d3f03529c46957c
pfeldman <pfeldman@chromium.org>
2016-01-15 08:55:26 -0800

    DevTools: recognize tsx files as scripts when debugging.
    BUG=577675
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1586773004
    
    Cr-Commit-Position: refs/heads/master@{#369773}

--- third_party/WebKit/Source/devtools/front_end/common/ResourceType.js
+++ third_party/WebKit/Source/devtools/front_end/common/ResourceType.js
@@ -216,6 +216,7 @@ WebInspector.ResourceType.mimeTypeByName = {
 WebInspector.ResourceType.mimeTypeByExtension = {
     // Web extensions
     "js": "text/javascript",
+    "jsx": "text/javascript",
     "css": "text/css",
     "html": "text/html",
     "htm": "text/html",
@@ -243,6 +244,7 @@ WebInspector.ResourceType.mimeTypeByExtension = {
 
     // TypeScript
     "ts": "text/typescript",
+    "tsx": "text/typescript",
 
     // JSON
     "json": "application/json",
