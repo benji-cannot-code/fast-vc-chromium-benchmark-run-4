4d1326ba312014ce9df49d3654e2921cbac9bdd4
tkent <tkent@chromium.org>
2016-01-14 20:49:47 -0800

    input[type=number] and input[type=range] warn if specified string is an invalid string.
    We show a console message like the following:
        The specified value "foo" is not a valid number. The value must match to the
        following regular expression: -?(\d+|\d+\.\d+|\.\d+)([eE][-+]?\d+)?
    
    BUG=397952
    
    Review URL: https://codereview.chromium.org/1587103003
    
    Cr-Commit-Position: refs/heads/master@{#369683}

--- third_party/WebKit/Source/core/html/forms/NumberInputType.cpp
+++ third_party/WebKit/Source/core/html/forms/NumberInputType.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/events/ScopedEventQueue.h"
 #include "core/html/HTMLInputElement.h"
 #include "core/html/parser/HTMLParserIdioms.h"
+#include "core/inspector/ConsoleMessage.h"
 #include "core/layout/LayoutTextControl.h"
 #include "platform/text/PlatformLocale.h"
 #include "wtf/MathExtras.h"
@@ -241,6 +242,14 @@ String NumberInputType::sanitizeValue(const String& proposedValue) const
     if (proposedValue.isEmpty())
         return proposedValue;
     return std::isfinite(parseToDoubleForNumberType(proposedValue)) ? proposedValue : emptyString();
+}
+
+void NumberInputType::warnIfValueIsInvalid(const String& value) const
+{
+    if (value.isEmpty() || !element().sanitizeValue(value).isEmpty())
+        return;
+    element().document().addConsoleMessage(ConsoleMessage::create(RenderingMessageSource, WarningMessageLevel,
+        String::format("The specified value %s is not a valid number. The value must match to the following regular expression: -?(\\d+|\\d+\\.\\d+|\\.\\d+)([eE][-+]?\\d+)?", JSONValue::quoteString(value).utf8().data())));
 }
 
 bool NumberInputType::hasBadInput() const
