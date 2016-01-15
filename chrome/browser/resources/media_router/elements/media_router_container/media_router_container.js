b0be40682fa5f858734b6b9b895be201ba75d593
apacible <apacible@chromium.org>
2016-01-15 14:19:26 -0800

    [Media Router] Add elevation to Media Router WebUI.
    This change adds a 3dp elevation "shadow" border to the dialog contents. In cases like Mac OSX 10.11+, there are no longer dialog borders, which means it's hard to tell the difference between dialog contents and the tab contents if the color is the same (notably white).
    
    BUG=577319
    
    Review URL: https://codereview.chromium.org/1584343005
    
    Cr-Commit-Position: refs/heads/master@{#369862}

--- chrome/browser/resources/media_router/elements/media_router_container/media_router_container.js
+++ chrome/browser/resources/media_router/elements/media_router_container/media_router_container.js
@@ -332,6 +332,7 @@ Polymer({
   ready: function() {
     this.elementReadyTimeMs_ = performance.now();
     this.showSinkList_();
+    this.updateMaxSinkListHeight(this.dialogHeight_);
   },
 
   attached: function() {
