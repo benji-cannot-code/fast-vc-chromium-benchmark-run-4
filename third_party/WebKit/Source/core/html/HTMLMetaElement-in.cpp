04ee44a129cc921f40ff841327670169e633ebd1
lushnikov <lushnikov@chromium.org>
2016-01-14 19:07:41 -0800

    DevTools: do not throw a warning for shrink-to-fit meta viewport argument
    BUG=569649
    R=pfeldman, dgozman
    
    Review URL: https://codereview.chromium.org/1585213006
    
    Cr-Commit-Position: refs/heads/master@{#369669}

--- third_party/WebKit/Source/core/html/HTMLMetaElement-in.cpp
+++ third_party/WebKit/Source/core/html/HTMLMetaElement-in.cpp
@@ -352,6 +352,10 @@ void HTMLMetaElement::processViewportKeyValuePair(Document* document, const Stri
             // Ignore vendor-specific argument.
             return;
         }
+        CASE("shrink-to-fit") {
+            // Ignore vendor-specific argument.
+            return;
+        }
     }
     reportViewportWarning(document, UnrecognizedViewportArgumentKeyError, keyString, String());
 }
