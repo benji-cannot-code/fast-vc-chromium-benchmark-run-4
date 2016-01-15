84ad855eb47279dcbbc056a956391a90b215c9e8
vasilii <vasilii@chromium.org>
2016-01-15 01:39:56 -0800

    Revert of Added capability on Windows to get the physical dimensions of your attached monitors. Also added th… (patchset #11 id:200001 of https://codereview.chromium.org/1563183008/ )
    Reason for revert:
    Broke compilation on Win x64 GN (dbg)
    
    https://build.chromium.org/p/chromium.win/builders/Win%20x64%20GN%20%28dbg%29/builds/16784
    
    physical_size.obj : error LNK2019: unresolved external symbol __imp_SetupDiDestroyDeviceInfoList referenced in function "public: static void __cdecl `anonymous namespace'::DeviceInfoListScopedTraits::Free(void *)" (?Free@DeviceInfoListScopedTraits@?A0xfcbad8c5@@SAXPEAX@Z)
    
    physical_size.obj : error LNK2019: unresolved external symbol __imp_SetupDiEnumDeviceInterfaces referenced in function "class std::vector<struct gfx::PhysicalDisplaySize,class std::allocator<struct gfx::PhysicalDisplaySize> > __cdecl gfx::GetPhysicalSizeForDisplays(void)" (?GetPhysicalSizeForDisplays@gfx@@YA?AV?$vector@UPhysicalDisplaySize@gfx@@V?$allocator@UPhysicalDisplaySize@gfx@@@std@@@std@@XZ)
    
    physical_size.obj : error LNK2019: unresolved external symbol __imp_SetupDiGetDeviceInterfaceDetailW referenced in function "bool __cdecl `anonymous namespace'::GetInterfaceDetailAndDeviceInfo(void *,struct _SP_DEVICE_INTERFACE_DATA *,class scoped_ptr<struct _SP_DEVICE_INTERFACE_DETAIL_DATA_W,struct base::FreeDeleter> *,struct _SP_DEVINFO_DATA *)" (?GetInterfaceDetailAndDeviceInfo@?A0xfcbad8c5@@YA_NPEAXPEAU_SP_DEVICE_INTERFACE_DATA@@PEAV?$scoped_ptr@U_SP_DEVICE_INTERFACE_DETAIL_DATA_W@@UFreeDeleter@base@@@@PEAU_SP_DEVINFO_DATA@@@Z)
    
    physical_size.obj : error LNK2019: unresolved external symbol __imp_SetupDiGetClassDevsW referenced in function "class std::vector<struct gfx::PhysicalDisplaySize,class std::allocator<struct gfx::PhysicalDisplaySize> > __cdecl gfx::GetPhysicalSizeForDisplays(void)" (?GetPhysicalSizeForDisplays@gfx@@YA?AV?$vector@UPhysicalDisplaySize@gfx@@V?$allocator@UPhysicalDisplaySize@gfx@@@std@@@std@@XZ)
    
    physical_size.obj : error LNK2019: unresolved external symbol __imp_SetupDiOpenDevRegKey referenced in function "bool __cdecl `anonymous namespace'::GetSizeFromRegistry(void *,struct _SP_DEVINFO_DATA *,int *,int *)" (?GetSizeFromRegistry@?A0xfcbad8c5@@YA_NPEAXPEAU_SP_DEVINFO_DATA@@PEAH2@Z)
    
    ./gfx.dll : fatal error LNK1120: 5 unresolved externals
    
    Original issue's description:
    > Added capability on Windows to get the physical dimensions of your attached monitors. Also added this information to about:gpu for testing.
    >
    > BUG=547914
    > TEST=go to the about:gpu URL and check for "Diagonal Monitor Size". it should show the size of your monitors
    >
    > Committed: https://crrev.com/b840e2f4ec9132b931f7ad54fd8fd20285e4eaa3
    > Cr-Commit-Position: refs/heads/master@{#369635}
    
    TBR=scottmg@chromium.org,robliao@chromium.org,sky@chromium.org,jbauman@chromium.org,thakis@chromium.org,bsep@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=547914
    
    Review URL: https://codereview.chromium.org/1593543002
    
    Cr-Commit-Position: refs/heads/master@{#369713}

--- content/browser/gpu/gpu_internals_ui.cc
+++ content/browser/gpu/gpu_internals_ui.cc
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #if defined(OS_WIN)
 #include "ui/base/win/shell.h"
-#include "ui/gfx/win/physical_size.h"
 #endif
 
 #if defined(OS_LINUX) && defined(USE_X11)
@@ -149,21 +148,6 @@ base::DictionaryValue* GpuInfoAsDictionaryValue() {
   if (GpuDataManagerImpl::GetInstance()->ShouldUseWarp()) {
     basic_info->Append(NewDescriptionValuePair("Using WARP",
         new base::FundamentalValue(true)));
-  }
-
-  std::vector<gfx::PhysicalDisplaySize> display_sizes =
-      gfx::GetPhysicalSizeForDisplays();
-  for (const auto& display_size : display_sizes) {
-    const int w = display_size.width_mm;
-    const int h = display_size.height_mm;
-    const double size_mm = sqrt(w * w + h * h);
-    const double size_inches = 0.0393701 * size_mm;
-    const double rounded_size_inches = floor(10.0 * size_inches) / 10.0;
-    std::string size_string = base::StringPrintf("%.1f\"", rounded_size_inches);
-    std::string description_string = base::StringPrintf(
-        "Diagonal Monitor Size of %s", display_size.display_name.c_str());
-    basic_info->Append(
-        NewDescriptionValuePair(description_string, size_string));
   }
 #endif
 
