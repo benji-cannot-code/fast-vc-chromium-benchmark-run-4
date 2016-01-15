bcaf2a3dfa5d60abece62268169fbce1fcf89bfd
pkasting <pkasting@chromium.org>
2016-01-14 19:17:10 -0800

    Tone down the hover effect on mousing over background tabs.
    * Reduce the peak opacity of the highlight from 50% to 25%.
    * Reduce the radius from (tab width / 3) to (tab width / 4).  Add a 16 px
      minimum radius to keep things from getting too tiny on very narrow tabs.  This
      also removes the "radius > 0" check, which should have never been false
      anyway, as that would require a tab width of 0, in which case we wouldn't be
      drawing anything.
    
    In MD, incognito windows have very dark tabs, on which the previous highlight is
    a bit out of control.  This makes things feel more manageable.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1587873004
    
    Cr-Commit-Position: refs/heads/master@{#369672}

--- ui/views/controls/glow_hover_controller.cc
+++ ui/views/controls/glow_hover_controller.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views {
 
 // Amount to scale the opacity.
-static const double kTrackOpacityScale = 0.5;
+static const double kTrackOpacityScale = 0.25;
 static const double kHighlightOpacityScale = 1.0;
 
 // How long the hover state takes.
