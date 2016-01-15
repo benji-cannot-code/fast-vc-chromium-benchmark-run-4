b0c267d3b826bef860f628a35f4d0cb0aae66976
dgn <dgn@chromium.org>
2016-01-15 09:44:25 -0800

    Fix transferring the cast start position and the associated test
    The start position transferred was the one set by on the media element
    when the page load (via media url presumably.). This sets the start
    position when we request Cast.
    
    The Cast emulator used for tests was not properly providing the
    current position when reporting its status.
    
    Renabled CastPositionTransferTest#testLocalToRemotePositionTransfer
    
    BUG=517597,568161
    
    Review URL: https://codereview.chromium.org/1586053005
    
    Cr-Commit-Position: refs/heads/master@{#369783}

--- chrome/browser/media/android/remote/remote_media_player_bridge.h
+++ chrome/browser/media/android/remote/remote_media_player_bridge.h
@@ -118,7 +118,6 @@ class RemoteMediaPlayerBridge : public media::MediaPlayerAndroid {
   // are retrieved.
   void OnCookiesRetrieved(const std::string& cookies);
 
-  long start_position_millis_;
   MediaPlayerAndroid* local_player_;
   int width_;
   int height_;
