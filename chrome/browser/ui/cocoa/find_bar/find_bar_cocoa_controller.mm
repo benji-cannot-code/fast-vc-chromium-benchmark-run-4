8156ff3bb7bd37428f7b4ff1b20ac74dbb567624
sdefresne <sdefresne@chromium.org>
2016-01-15 02:06:39 -0800

    Componentize IDS_ACCNAME_* strings shared with iOS.
    In order to share strings with iOS, componentize them. Two of them are
    iOS specific, so instead move them to ios_strings.grd.
    
    BUG=568486
    TBR=sky@chromium.org
    
    Committed: https://crrev.com/fdc5a2028505ca2b615f9045bff9c96d80e11906
    Cr-Commit-Position: refs/heads/master@{#369698}
    
    Review URL: https://codereview.chromium.org/1583803003
    
    Cr-Commit-Position: refs/heads/master@{#369716}

--- chrome/browser/ui/cocoa/find_bar/find_bar_cocoa_controller.mm
+++ chrome/browser/ui/cocoa/find_bar/find_bar_cocoa_controller.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/web_contents.h"
+#include "grit/components_strings.h"
 #include "grit/theme_resources.h"
 #import "third_party/google_toolbox_for_mac/src/AppKit/GTMNSAnimation+Duration.h"
 #import "ui/base/cocoa/find_pasteboard.h"
--- chrome/browser/ui/cocoa/infobars/infobar_controller.mm
+++ chrome/browser/ui/cocoa/infobars/infobar_controller.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/infobars/infobar_container_controller.h"
 #import "chrome/browser/ui/cocoa/infobars/infobar_gradient_view.h"
 #import "chrome/browser/ui/cocoa/location_bar/location_bar_view_mac.h"
+#include "grit/components_strings.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
 #import "ui/base/cocoa/controls/hyperlink_text_view.h"
