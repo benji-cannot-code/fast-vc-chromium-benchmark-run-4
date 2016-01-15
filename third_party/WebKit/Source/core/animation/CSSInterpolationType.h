62b479bbdecb7148bb125737515c48f53adbd46a
alancutter <alancutter@chromium.org>
2016-01-14 18:37:34 -0800

    Make uses of var() in keyframe longhand CSS properties interpolate smoothly
    This change makes the CSS interpolation system aware of var() references
    and resolve them prior to creating the animation's interpolation.
    This only works for longhand CSS properties, see crbug.com/576547 for
    the shorthand case.
    
    BUG=576541
    
    Review URL: https://codereview.chromium.org/1587813003
    
    Cr-Commit-Position: refs/heads/master@{#369665}

--- third_party/WebKit/Source/core/animation/CSSInterpolationType.h
+++ third_party/WebKit/Source/core/animation/CSSInterpolationType.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/animation/InterpolationEnvironment.h"
 #include "core/animation/InterpolationType.h"
-#include "core/animation/StringKeyframe.h"
 #include "core/css/CSSPropertyMetadata.h"
 
 namespace blink {
@@ -21,22 +20,11 @@ protected:
 
     CSSPropertyID cssProperty() const { return property().cssProperty(); }
 
-    virtual PassOwnPtr<InterpolationValue> maybeConvertSingle(const PropertySpecificKeyframe& keyframe, const InterpolationEnvironment& environment, const UnderlyingValue& underlyingValue, ConversionCheckers& conversionCheckers) const
-    {
-        const CSSValue* value = toCSSPropertySpecificKeyframe(keyframe).value();
-        if (!value)
-            return maybeConvertNeutral(underlyingValue, conversionCheckers);
-        if (value->isInitialValue() || (value->isUnsetValue() && !CSSPropertyMetadata::isInheritedProperty(cssProperty())))
-            return maybeConvertInitial();
-        if (value->isInheritedValue() || (value->isUnsetValue() && CSSPropertyMetadata::isInheritedProperty(cssProperty())))
-            return maybeConvertInherit(environment.state(), conversionCheckers);
-        return maybeConvertValue(*value, environment.state(), conversionCheckers);
-    }
-
+    virtual PassOwnPtr<InterpolationValue> maybeConvertSingle(const PropertySpecificKeyframe&, const InterpolationEnvironment&, const UnderlyingValue&, ConversionCheckers&) const;
     virtual PassOwnPtr<InterpolationValue> maybeConvertNeutral(const UnderlyingValue&, ConversionCheckers&) const { ASSERT_NOT_REACHED(); return nullptr; }
     virtual PassOwnPtr<InterpolationValue> maybeConvertInitial() const { ASSERT_NOT_REACHED(); return nullptr; }
-    virtual PassOwnPtr<InterpolationValue> maybeConvertInherit(const StyleResolverState&, ConversionCheckers& conversionCheckers) const { ASSERT_NOT_REACHED(); return nullptr; }
-    virtual PassOwnPtr<InterpolationValue> maybeConvertValue(const CSSValue& value, const StyleResolverState&, ConversionCheckers& conversionCheckers) const { ASSERT_NOT_REACHED(); return nullptr; }
+    virtual PassOwnPtr<InterpolationValue> maybeConvertInherit(const StyleResolverState&, ConversionCheckers&) const { ASSERT_NOT_REACHED(); return nullptr; }
+    virtual PassOwnPtr<InterpolationValue> maybeConvertValue(const CSSValue& value, const StyleResolverState&, ConversionCheckers&) const { ASSERT_NOT_REACHED(); return nullptr; }
 };
 
 } // namespace blink
--- third_party/WebKit/Source/core/core.gypi
+++ third_party/WebKit/Source/core/core.gypi
@@ -846,6 +846,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'animation/CSSImageInterpolationType.h',
             'animation/CSSImageListInterpolationType.cpp',
             'animation/CSSImageListInterpolationType.h',
+            'animation/CSSInterpolationType.cpp',
             'animation/CSSInterpolationType.h',
             'animation/CSSLengthInterpolationType.cpp',
             'animation/CSSLengthInterpolationType.h',
