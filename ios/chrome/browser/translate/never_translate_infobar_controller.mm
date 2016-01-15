a4f5681883f429c4fc7eaed3d8f624a2d187c3d5
sdefresne <sdefresne@chromium.org>
2016-01-15 07:39:08 -0800

    Fix placeholder numbering in iOS specific strings.
    The string IDS_TRANSLATE_INFOBAR_NEVER_MESSAGE_IOS only used a single
    placeholder but used "$2" instead of "$1" causing a DCHECK. Use the
    correct value.
    
    Rename the constant IDS_IOS_TRANSLATE_INFOBAR_NEVER_MESSAGE and move
    the string to ios/chrome/app/ios_${branding}_strings.grd as it is
    only used on iOS.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1585923007
    
    Cr-Commit-Position: refs/heads/master@{#369751}

--- ios/chrome/browser/translate/never_translate_infobar_controller.mm
+++ ios/chrome/browser/translate/never_translate_infobar_controller.mm
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/translate/core/browser/translate_infobar_delegate.h"
 #include "grit/components_strings.h"
 #include "ios/chrome/browser/translate/translate_infobar_tags.h"
+#include "ios/chrome/grit/ios_chromium_strings.h"
+#include "ios/chrome/grit/ios_google_chrome_strings.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/public/provider/chrome/browser/ui/infobar_view_delegate.h"
 #import "ios/public/provider/chrome/browser/ui/infobar_view_protocol.h"
@@ -43,9 +45,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Main text.
   base::string16 originalLanguage =
       translateInfoBarDelegate->original_language_name();
-  [infoBarView addLabel:l10n_util::GetNSStringF(
-                            IDS_TRANSLATE_INFOBAR_NEVER_MESSAGE_IOS,
-                            originalLanguage)];
+  [infoBarView
+      addLabel:l10n_util::GetNSStringF(IDS_IOS_TRANSLATE_INFOBAR_NEVER_MESSAGE,
+                                       originalLanguage)];
   // Close button.
   [infoBarView addCloseButtonWithTag:TranslateInfoBarIOSTag::CLOSE
                               target:self
