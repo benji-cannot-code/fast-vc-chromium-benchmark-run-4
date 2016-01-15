998281ddea3a8ffd2d28b68f28f87857057786d0
estark <estark@chromium.org>
2016-01-15 13:41:01 -0800

    Remove obsolete histogram values from SSL.InsecureContent
    Mixed content histogram values that depend on the URL of the page have
    since been rendered obsolete by Rappor, and they complicate mixed
    content checking with OOPIFs. This CL deprecates the histogram values
    that break down mixed content by Google property but preserves the basic
    counters for displaying or running mixed content.
    
    BUG=486936
    
    Review URL: https://codereview.chromium.org/1583813003
    
    Cr-Commit-Position: refs/heads/master@{#369851}

--- chrome/renderer/content_settings_observer.h
+++ chrome/renderer/content_settings_observer.h
@@ -81,7 +81,6 @@ class ContentSettingsObserver
   void didNotAllowScript() override;
   void didUseKeygen() override;
   bool allowDisplayingInsecureContent(bool allowed_per_settings,
-                                      const blink::WebSecurityOrigin& context,
                                       const blink::WebURL& url) override;
   bool allowRunningInsecureContent(bool allowed_per_settings,
                                    const blink::WebSecurityOrigin& context,
