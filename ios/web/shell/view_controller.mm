e1199b6821c2c3d295f99de001552280ca7f7b29
stuartmorgan <stuartmorgan@chromium.org>
2016-01-15 15:57:05 -0800

    Remove referrer code from window.open/target=_blank path
    Referrer should not be plumbed through codepaths that are only for
    opening blank tabs; this was cruft from earlier versions that opened
    tabs with a specific URL.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1592723004
    
    Cr-Commit-Position: refs/heads/master@{#369884}

--- ios/web/shell/view_controller.mm
+++ ios/web/shell/view_controller.mm
@@ -287,9 +287,7 @@ NSString* const kWebShellAddressFieldAccessibilityLabel = @"Address field";
   return nil;
 }
 
-- (CRWWebController*)webPageOrderedOpenBlankWithReferrer:
-                         (const web::Referrer&)referrer
-                                            inBackground:(BOOL)inBackground {
+- (CRWWebController*)webPageOrderedOpen {
   return nil;
 }
 
