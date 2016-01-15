2bca84825e3eae3e8d109332ffa2740fe8263fe2
mikecase <mikecase@chromium.org>
2016-01-15 11:38:22 -0800

    [Android] Fix for crash if writing logcat to preexisting dir.
    Change to catapult: https://codereview.chromium.org/1597533002/
    
    BUG=578157
    NOPRESUBMIT=true
    
    Review URL: https://codereview.chromium.org/1586273003
    
    Cr-Commit-Position: refs/heads/master@{#369814}

--- build/android/devil/android/logcat_monitor.py
+++ build/android/devil/android/logcat_monitor.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # pylint: disable=unused-argument
 
+import errno
 import logging
 import os
 import re
@@ -195,7 +196,11 @@ class LogcatMonitor(object):
     """
     self._StopRecording()
     if self._record_file and self._output_file:
-      os.makedirs(os.path.dirname(self._output_file))
+      try:
+        os.makedirs(os.path.dirname(self._output_file))
+      except OSError as e:
+        if e.errno != errno.EEXIST:
+          raise
       shutil.copy(self._record_file.name, self._output_file)
 
   def Close(self):
