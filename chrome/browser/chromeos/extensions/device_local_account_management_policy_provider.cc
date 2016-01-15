65bc007febe3e16cf4fc27a9027e175c1d5cdbdd
tnagel <tnagel@chromium.org>
2016-01-15 02:46:00 -0800

    Whitelist Scan app for Public Session.
    BUG=569813
    TEST=confirmed manually that Scan can be installed in PS
    
    Review URL: https://codereview.chromium.org/1586123004
    
    Cr-Commit-Position: refs/heads/master@{#369724}

--- chrome/browser/chromeos/extensions/device_local_account_management_policy_provider.cc
+++ chrome/browser/chromeos/extensions/device_local_account_management_policy_provider.cc
@@ -37,6 +37,7 @@ const char* const kPublicSessionWhitelist[] = {
     "ngjnkanfphagcaokhjecbgkboelgfcnf",  // Print button
     "gbchcmhmhahfdphkhkmpfmihenigjmpp",  // Chrome Remote Desktop
     "cjanmonomjogheabiocdamfpknlpdehm",  // HP printer driver
+    "pmnllmkmjilbojkpgplbdmckghmaocjh",  // Scan app by François Beaufort
 
     // Libraries:
     "aclofikceldphonlfmghmimkodjdmhck",  // Ancoris login component
