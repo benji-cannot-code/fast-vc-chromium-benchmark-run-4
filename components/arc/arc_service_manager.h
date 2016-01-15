1fb828108c7585e7acb38723a0679aa6f4bdd0fa
kinaba <kinaba@chromium.org>
2016-01-14 22:21:22 -0800

    Host-side implementation of ARC IME.
    Changes directly below components/arc/ are for adding mojo IPC definitions
    and for boilerplate set-up code for the IPC handling.
    
    components/arc/ime/arc_ime_ipc_host* concentrates in converting the
    Chrome's IME framework interface to the defined IPC, so that the upper layer
    code becomes independent from Mojo details.
    
    components/arc/ime/arc_ime_bridge* implements ui::TextInputClient for ARC
    windows. All it does is to manage focused ARC window and to forward all IME
    related things to the arc_ime_ipc_host.
    
    BUG=574311
    
    Review URL: https://codereview.chromium.org/1557943003
    
    Cr-Commit-Position: refs/heads/master@{#369695}

--- components/arc/arc_service_manager.h
+++ components/arc/arc_service_manager.h
@@ -16,6 +16,7 @@ namespace arc {
 class ArcAuthService;
 class ArcBridgeService;
 class ArcClipboardBridge;
+class ArcImeBridge;
 class ArcInputBridge;
 class ArcNotificationManager;
 class ArcPowerBridge;
@@ -49,6 +50,7 @@ class ArcServiceManager {
   // Individual services
   scoped_ptr<ArcAuthService> arc_auth_service_;
   scoped_ptr<ArcClipboardBridge> arc_clipboard_bridge_;
+  scoped_ptr<ArcImeBridge> arc_ime_bridge_;
   scoped_ptr<ArcInputBridge> arc_input_bridge_;
   scoped_ptr<ArcNotificationManager> arc_notification_manager_;
   scoped_ptr<ArcSettingsBridge> arc_settings_bridge_;
