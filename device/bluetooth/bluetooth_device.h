4167da84edacd6b6ca36f4fd7eb3c0fb4ef4f7dc
scheib <scheib@chromium.org>
2016-01-14 21:00:51 -0800

    Revert of Clear the BLE services list on disconnect. (patchset #4 id:60001 of https://codereview.chromium.org/1565773002/ )
    Reason for revert:
    bluetooth/getPrimaryService.html is timing out.
    
    http://test-results.appspot.com/dashboards/flakiness_dashboard.html#tests=bluetooth%2FgetPrimaryService.html&testType=layout-tests
    
    https://code.google.com/p/chromium/issues/detail?id=577936
    
    Original issue's description:
    > Clear the BLE services list on disconnect.
    >
    > This lets us recreate the services list correctly upon the next
    > reconnect, which fixes some problems with a device we have, which
    > disconnects automatically after 30 seconds.
    >
    > Also reset the services_discovered flag for this device on disconnect.
    > In order to accomplish this, I had to move this flag from the dispatcher
    > host to the bluetooth device instance.
    >
    > BUG=570804
    >
    > Committed: https://crrev.com/11195385967983a0b5fc3865576a9fa826d51711
    > Cr-Commit-Position: refs/heads/master@{#369593}
    
    TBR=ortuno@chromium.org,armansito@chromium.org,tommyt@opera.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=570804
    
    Review URL: https://codereview.chromium.org/1587193004
    
    Cr-Commit-Position: refs/heads/master@{#369684}

--- device/bluetooth/bluetooth_device.h
+++ device/bluetooth/bluetooth_device.h
@@ -433,12 +433,6 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothDevice {
   virtual void CreateGattConnection(const GattConnectionCallback& callback,
                                     const ConnectErrorCallback& error_callback);
 
-  // Set the gatt services discovery complete flag for this device.
-  void SetGattServicesDiscoveryComplete(bool complete);
-
-  // Indicates whether service discovery is complete for this device.
-  bool IsGattServicesDiscoveryComplete() const;
-
   // Returns the list of discovered GATT services.
   virtual std::vector<BluetoothGattService*> GetGattServices() const;
 
@@ -469,8 +463,6 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothDevice {
       BluetoothGattConnection_DisconnectGatt_SimulateDisconnect);
   FRIEND_TEST_ALL_PREFIXES(BluetoothTest,
                            BluetoothGattConnection_ErrorAfterConnection);
-  FRIEND_TEST_ALL_PREFIXES(BluetoothTest,
-                           BluetoothGattConnection_DisconnectGatt_Cleanup);
 
   BluetoothDevice(BluetoothAdapter* adapter);
 
@@ -528,7 +520,6 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothDevice {
   typedef base::ScopedPtrHashMap<std::string, scoped_ptr<BluetoothGattService>>
       GattServiceMap;
   GattServiceMap gatt_services_;
-  bool gatt_services_discovery_complete_;
 
   // Mapping from service UUID represented as a std::string of a bluetooth
   // service to
--- device/bluetooth/bluetooth_device_android.cc
+++ device/bluetooth/bluetooth_device_android.cc
@@ -211,9 +211,6 @@ void BluetoothDeviceAndroid::OnConnectionStateChange(
   if (gatt_connected_) {
     DidConnectGatt();
   } else {
-    gatt_services_.clear();
-    SetGattServicesDiscoveryComplete(false);
-
     switch (status) {   // Constants are from android.bluetooth.BluetoothGatt.
       case 0x0000008f:  // GATT_CONNECTION_CONGESTED
         return DidFailToConnectGatt(ERROR_CONNECTION_CONGESTED);
@@ -245,7 +242,6 @@ void BluetoothDeviceAndroid::OnConnectionStateChange(
 void BluetoothDeviceAndroid::OnGattServicesDiscovered(
     JNIEnv* env,
     const JavaParamRef<jobject>& jcaller) {
-  SetGattServicesDiscoveryComplete(true);
   FOR_EACH_OBSERVER(BluetoothAdapter::Observer, GetAdapter()->GetObservers(),
                     GattServicesDiscovered(GetAdapter(), this));
 }
