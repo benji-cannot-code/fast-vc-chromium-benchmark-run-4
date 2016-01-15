585335e59c29a69782b58b348952b979666c81b6
jyquinn <jyquinn@chromium.org>
2016-01-15 13:51:00 -0800

    Call stopLoading when opening an externalURL
    Previously, abortLoad was called, which did not properly handle metrics
    or returning to native pages.
    
    BUG=578264
    
    Review URL: https://codereview.chromium.org/1573243024
    
    Cr-Commit-Position: refs/heads/master@{#369853}

--- ios/web/web_state/ui/crw_wk_web_view_web_controller.mm
+++ ios/web/web_state/ui/crw_wk_web_view_web_controller.mm
@@ -471,6 +471,7 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
 
 - (void)stopLoading {
   _stoppedWKNavigation.reset(_latestWKNavigation);
+  [super stopLoading];
 }
 
 #pragma mark -
