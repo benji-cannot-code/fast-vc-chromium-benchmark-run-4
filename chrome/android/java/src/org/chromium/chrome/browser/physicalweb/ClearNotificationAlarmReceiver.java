605c987309986ce2ff07e972f04f43fffd221721
cco3 <cco3@chromium.org>
2016-01-14 17:51:51 -0800

    Clear notification after some time
    We cannot tell when the application dies, at which point we want to
    make sure that there are no stale notifications.  Therefore, we
    schedule an alarm that will clear the notification.
    
    BUG=529962
    
    Review URL: https://codereview.chromium.org/1577193007
    
    Cr-Commit-Position: refs/heads/master@{#369650}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/physicalweb/ClearNotificationAlarmReceiver.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.physicalweb;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import org.chromium.base.Log;
+
+/**
+ * A broadcast receiver that clears the UrlManager's notification.
+ * This class cannot make native calls because it may not be loaded when the
+ * alarm fires.
+ */
+public class ClearNotificationAlarmReceiver extends BroadcastReceiver {
+    private static final String TAG = "PhysicalWeb";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "Running NotificationCleanupAlarmReceiver");
+        UrlManager.getInstance(context).clearNotification();
+    }
+}
