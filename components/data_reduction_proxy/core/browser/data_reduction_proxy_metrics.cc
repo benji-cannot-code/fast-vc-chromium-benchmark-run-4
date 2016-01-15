091913697bb8b76f4b726d4b61b91b3bc012bd0e
sclittle <sclittle@chromium.org>
2016-01-15 12:48:09 -0800

    Include 0-content length responses in DRP metrics.
    Previously, Data Reduction Proxy metrics only counted received bytes for
    responses with non-zero content length. This CL expands these metrics to
    include bytes from responses with zero content length (e.g. 204s, 304s)
    as well.
    
    BUG=568452,536139
    
    Review URL: https://codereview.chromium.org/1505403004
    
    Cr-Commit-Position: refs/heads/master@{#369838}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_metrics.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_metrics.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_config.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_headers.h"
 #include "net/base/load_flags.h"
+#include "net/http/http_response_headers.h"
 #include "net/http/http_response_info.h"
+#include "net/http/http_status_code.h"
 #include "net/proxy/proxy_config.h"
 #include "net/proxy/proxy_server.h"
 #include "net/url_request/url_request.h"
@@ -28,12 +30,16 @@ DataReductionProxyRequestType GetDataReductionProxyRequestType(
     return UNKNOWN_TYPE;
   }
 
-  // Check for a Data Reduction Proxy via header before checking if proxies are
-  // bypassed, to avoid misreporting cases where the Data Reduction Proxy was
-  // bypassed between the request being sent out and the response coming in.
-  if (request.response_info().headers.get() &&
-      HasDataReductionProxyViaHeader(request.response_info().headers.get(),
-                                     NULL)) {
+  // Check if the request came through the Data Reduction Proxy before checking
+  // if proxies are bypassed, to avoid misreporting cases where the Data
+  // Reduction Proxy was bypassed between the request being sent out and the
+  // response coming in. For 304 responses, check if the request was sent to the
+  // Data Reduction Proxy, since 304s aren't required to have a Via header even
+  // if they came through the Data Reduction Proxy.
+  if (request.response_headers() &&
+      (HasDataReductionProxyViaHeader(request.response_headers(), nullptr) ||
+       (request.response_headers()->response_code() == net::HTTP_NOT_MODIFIED &&
+        config.WasDataReductionProxyUsed(&request, nullptr)))) {
     return VIA_DATA_REDUCTION_PROXY;
   }
 
