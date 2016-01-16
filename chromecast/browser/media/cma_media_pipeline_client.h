2ae5f4bcbabd914559cb3d46a914f59bb7429f0b
esum <esum@chromium.org>
2016-01-15 20:42:59 -0800

    [Chromecast] Adding pause/resume operations to VideoPlaneController.
    Also adding method for initializing resources after they have been
    granted to cast.
    
    BUG=internal b/25812819
    TEST=Revoke/Grant resources during youtube playback on redwood.
         Youtube/Netflix playback on earth (no regressions).
    
    Review URL: https://codereview.chromium.org/1531543002
    
    Cr-Commit-Position: refs/heads/master@{#369937}

--- chromecast/browser/media/cma_media_pipeline_client.h
+++ chromecast/browser/media/cma_media_pipeline_client.h
@@ -33,6 +33,7 @@ class CmaMediaPipelineClient : public base::RefCounted<CmaMediaPipelineClient>,
   virtual void OnMediaPipelineBackendDestroyed();
 
   // cast::CastResource implementation:
+  void AcquireResource(CastResource::Resource resource) override;
   void ReleaseResource(CastResource::Resource resource) override;
 
  protected:
