51a41c247f9267e72deae804e7cc94e3a3b38aad
haraken <haraken@chromium.org>
2016-01-16 08:25:53 -0800

    Oilpan: Fix weak processing for IntersectionObserver::m_root
    This is a follow-up fix for https://codereview.chromium.org/1591763003/.
    IntersectionObserver::m_root must be cleared in the weak callback when the m_root is dead.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1594813002
    
    Cr-Commit-Position: refs/heads/master@{#369956}

--- third_party/WebKit/Source/core/dom/IntersectionObserver.h
+++ third_party/WebKit/Source/core/dom/IntersectionObserver.h
@@ -28,7 +28,6 @@ class IntersectionObserver final : public GarbageCollectedFinalized<Intersection
 public:
     static IntersectionObserver* create(const IntersectionObserverInit&, IntersectionObserverCallback&, ExceptionState&);
     static void resumeSuspendedObservers();
-    static void rootDisappearedCallback(Visitor*, void*);
 
     // API methods
     void observe(Element*, ExceptionState&);
@@ -57,6 +56,9 @@ public:
 
 private:
     explicit IntersectionObserver(IntersectionObserverCallback&, Node&, const Vector<Length>& rootMargin, const Vector<float>& thresholds);
+#if ENABLE(OILPAN)
+    void clearWeakMembers(Visitor*);
+#endif
 
     Member<IntersectionObserverCallback> m_callback;
     WeakPtrWillBeWeakMember<Node> m_root;
