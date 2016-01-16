2eecf6e7bdbf8cf63c4aa98c7572bf38ffce9d9b
nolan.robin.cao <nolan.robin.cao@gmail.com>
2016-01-16 04:23:09 -0800

    Ensure the select element fire change event even when mouse is released outside
    AutoscrollController::m_autoscrollLayoutObject is used to notify the select
    element to fire change event when mouse is released outside of the list box.
    But this object will also be updated during scroll/pan/drag. To fix it, add
    a member m_pressedLayoutObject to store the initial object.
    
    BUG=571927
    TEST=fast/events/select-onchange-mouse-released-outside.html
    
    Review URL: https://codereview.chromium.org/1570863002
    
    Cr-Commit-Position: refs/heads/master@{#369951}

--- third_party/WebKit/Source/core/page/AutoscrollController.cpp
+++ third_party/WebKit/Source/core/page/AutoscrollController.cpp
@@ -52,6 +52,7 @@ PassOwnPtrWillBeRawPtr<AutoscrollController> AutoscrollController::create(Page& 
 AutoscrollController::AutoscrollController(Page& page)
     : m_page(&page)
     , m_autoscrollLayoutObject(nullptr)
+    , m_pressedLayoutObject(nullptr)
     , m_autoscrollType(NoAutoscroll)
     , m_dragAndDropAutoscrollStartTime(0)
 {
@@ -82,6 +83,8 @@ void AutoscrollController::startAutoscrollForSelection(LayoutObject* layoutObjec
         scrollable = layoutObject->isListBox() ? toLayoutListBox(layoutObject) : nullptr;
     if (!scrollable)
         return;
+
+    m_pressedLayoutObject = layoutObject && layoutObject->isBox() ? toLayoutBox(layoutObject) : nullptr;
     m_autoscrollType = AutoscrollForSelection;
     m_autoscrollLayoutObject = scrollable;
     startAutoscroll();
@@ -89,13 +92,16 @@ void AutoscrollController::startAutoscrollForSelection(LayoutObject* layoutObjec
 
 void AutoscrollController::stopAutoscroll()
 {
+    if (m_pressedLayoutObject) {
+        m_pressedLayoutObject->stopAutoscroll();
+        m_pressedLayoutObject = nullptr;
+    }
     LayoutBox* scrollable = m_autoscrollLayoutObject;
     m_autoscrollLayoutObject = nullptr;
 
     if (!scrollable)
         return;
 
-    scrollable->stopAutoscroll();
 #if OS(WIN)
     if (panScrollInProgress()) {
         if (FrameView* view = scrollable->frame()->view()) {
@@ -109,6 +115,9 @@ void AutoscrollController::stopAutoscroll()
 
 void AutoscrollController::stopAutoscrollIfNeeded(LayoutObject* layoutObject)
 {
+    if (m_pressedLayoutObject == layoutObject)
+        m_pressedLayoutObject = nullptr;
+
     if (m_autoscrollLayoutObject != layoutObject)
         return;
     m_autoscrollLayoutObject = nullptr;
@@ -134,7 +143,7 @@ void AutoscrollController::updateAutoscrollLayoutObject()
 
     LayoutBox* autoscrollLayoutObject = layoutObject && layoutObject->isBox() ? toLayoutBox(layoutObject) : nullptr;
     if (m_autoscrollLayoutObject && !autoscrollLayoutObject)
-        stopAutoscrollIfNeeded(m_autoscrollLayoutObject);
+        m_autoscrollType = NoAutoscroll;
 
     m_autoscrollLayoutObject = autoscrollLayoutObject;
 }
