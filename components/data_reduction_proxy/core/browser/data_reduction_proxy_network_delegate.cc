6dc55e80547bac9543146d08cac52a0e3b36b501
megjablon <megjablon@chromium.org>
2016-01-15 18:17:24 -0800

    Add DataReductionProxy.LoFi.TransformationType UMA which counts pageloads that received or requested, but did not receive, various Lo-Fi transformations.
    TBR=asvitkine@chromium.org
    
    BUG=558632
    
    Review URL: https://codereview.chromium.org/1587903002
    
    Cr-Commit-Position: refs/heads/master@{#369925}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_network_delegate.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_network_delegate.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_event_creator.h"
 #include "components/data_reduction_proxy/core/common/lofi_decider.h"
 #include "net/base/load_flags.h"
+#include "net/http/http_request_headers.h"
 #include "net/http/http_response_headers.h"
 #include "net/proxy/proxy_info.h"
 #include "net/proxy/proxy_server.h"
@@ -222,6 +223,7 @@ void DataReductionProxyNetworkDelegate::OnCompletedInternal(
   if (data_reduction_proxy_bypass_stats_)
     data_reduction_proxy_bypass_stats_->OnUrlRequestCompleted(request, started);
 
+  net::HttpRequestHeaders request_headers;
   if (data_reduction_proxy_io_data_ && request->response_headers() &&
       request->response_headers()->HasHeaderValue(
           chrome_proxy_header(), chrome_proxy_lo_fi_directive())) {
@@ -233,6 +235,15 @@ void DataReductionProxyNetworkDelegate::OnCompletedInternal(
                  chrome_proxy_lo_fi_preview_directive())) {
     data_reduction_proxy_io_data_->lofi_ui_service()->OnLoFiReponseReceived(
         *request, true);
+    RecordLoFiTransformationType(PREVIEW);
+  } else if (request->GetFullRequestHeaders(&request_headers) &&
+             request_headers.HasHeader(chrome_proxy_header())) {
+    std::string header_value;
+    request_headers.GetHeader(chrome_proxy_header(), &header_value);
+    if (header_value.find(chrome_proxy_lo_fi_preview_directive()) !=
+        std::string::npos) {
+      RecordLoFiTransformationType(NO_TRANSFORMATION_PREVIEW_REQUESTED);
+    }
   }
 
   if (!request->response_info().network_accessed ||
@@ -362,6 +373,12 @@ void OnResolveProxyHandler(const GURL& url,
     if (!data_reduction_proxy_info.proxy_server().is_direct())
       result->OverrideProxyList(data_reduction_proxy_info.proxy_list());
   }
+}
+
+void DataReductionProxyNetworkDelegate::RecordLoFiTransformationType(
+    LoFiTransformationType type) {
+  UMA_HISTOGRAM_ENUMERATION("DataReductionProxy.LoFi.TransformationType", type,
+                            LO_FI_TRANSFORMATION_TYPES_INDEX_BOUNDARY);
 }
 
 }  // namespace data_reduction_proxy
