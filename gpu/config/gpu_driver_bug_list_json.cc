482247a48a6bf1a2ebd74badfb4bd9ed1a7d65fe
zmo <zmo@chromium.org>
2016-01-15 17:11:23 -0800

    Revert of Add a workaround for Mac ReadPixels. (patchset #2 id:20001 of https://codereview.chromium.org/1590373002/ )
    Reason for revert:
    This caused a GL error on Linux.  Need to dig further before relanding.
    
    Original issue's description:
    > Add a workaround for Mac ReadPixels.
    >
    > BUG=563714
    > TEST=webgl2_conformance_test on mac
    > R=bajones@chromium.org,piman@chromium.org
    > NOTRY=true
    >
    > Committed: https://crrev.com/b7997d0ddd02efac87d7428794ffe2e6c8b223db
    > Cr-Commit-Position: refs/heads/master@{#369868}
    
    TBR=bajones@chromium.org,piman@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=563714
    
    Review URL: https://codereview.chromium.org/1599543002
    
    Cr-Commit-Position: refs/heads/master@{#369901}

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -20,7 +20,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "8.41",
+  "version": "8.40",
   "entries": [
     {
       "id": 1,
@@ -1711,17 +1711,6 @@ LONG_STRING_CONST(
       "disabled_extensions": [
         "GL_ARB_timer_query",
         "GL_EXT_timer_query"
-      ]
-    },
-    {
-      "id": 144,
-      "cr_bugs": [563714],
-      "description": "Pack parameters work incorrectly with pack buffer bound",
-      "os": {
-        "type": "macosx"
-      },
-      "features": [
-        "pack_parameters_workaround_with_pack_buffer"
       ]
     }
   ]
