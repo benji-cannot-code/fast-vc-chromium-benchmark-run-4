50b4bda8d0098f63104e602d8aa6e2ddcaee2e19
newt <newt@chromium.org>
2016-01-15 17:54:02 -0800

    Delete vestiges of pre-Icon-NTP.
    This cleans up a few bits of code and comments that are leftover
    from the era of two different NTP designs (icon and screenshot).
    
    BUG=554267
    
    Review URL: https://codereview.chromium.org/1591093002
    
    Cr-Commit-Position: refs/heads/master@{#369917}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -78,7 +78,7 @@ public class NewTabPageView extends FrameLayout
     private View mSearchBoxView;
     private TextView mSearchBoxTextView;
     private ImageView mVoiceSearchButton;
-    private ViewGroup mMostVisitedLayout;
+    private MostVisitedLayout mMostVisitedLayout;
     private View mMostVisitedPlaceholder;
     private View mOptOutView;
     private View mNoSearchLogoSpacer;
@@ -260,9 +260,7 @@ public class NewTabPageView extends FrameLayout
         mContentView = (ViewGroup) findViewById(R.id.ntp_content);
 
         mMostVisitedDesign = new MostVisitedDesign(getContext());
-        ViewStub mostVisitedLayoutStub = (ViewStub) findViewById(R.id.most_visited_layout_stub);
-        mostVisitedLayoutStub.setLayoutResource(R.layout.most_visited_layout);
-        mMostVisitedLayout = (ViewGroup) mostVisitedLayoutStub.inflate();
+        mMostVisitedLayout = (MostVisitedLayout) findViewById(R.id.most_visited_layout);
         mMostVisitedDesign.initMostVisitedLayout(mMostVisitedLayout, searchProviderHasLogo);
 
         mSearchProviderLogoView = (LogoView) findViewById(R.id.search_provider_logo);
@@ -879,7 +877,7 @@ public class NewTabPageView extends FrameLayout
     }
 
     /**
-     * The new-fangled design for most visited tiles, where each tile shows a large icon and title.
+     * The design for most visited tiles: each tile shows a large icon and the site's title.
      */
     private class MostVisitedDesign {
 
@@ -920,10 +918,9 @@ public class NewTabPageView extends FrameLayout
             return mMostVisitedLayoutBleed;
         }
 
-        public void initMostVisitedLayout(ViewGroup mostVisitedLayout,
+        public void initMostVisitedLayout(MostVisitedLayout mostVisitedLayout,
                 boolean searchProviderHasLogo) {
-            ((MostVisitedLayout) mostVisitedLayout).setMaxRows(
-                    searchProviderHasLogo ? MAX_ROWS : MAX_ROWS_NO_LOGO);
+            mostVisitedLayout.setMaxRows(searchProviderHasLogo ? MAX_ROWS : MAX_ROWS_NO_LOGO);
         }
 
         public void setSearchProviderHasLogo(View mostVisitedLayout, boolean hasLogo) {
