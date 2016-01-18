8b73da8087560a2132aa0f453e41cd1b7d73fe0e
treib <treib@chromium.org>
2016-01-18 02:44:05 -0800

    permission_message_util cleanup/modernization
    BUG=none
    
    Review URL: https://codereview.chromium.org/1593543003
    
    Cr-Commit-Position: refs/heads/master@{#370010}

--- extensions/common/permissions/permission_message_util.h
+++ extensions/common/permissions/permission_message_util.h
@@ -8,9 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 #include <string>
-#include <vector>
-
-#include "base/strings/string16.h"
 
 namespace extensions {
 class PermissionIDSet;
@@ -21,14 +18,7 @@ namespace permission_message_util {
 
 enum PermissionMessageProperties { kReadOnly, kReadWrite };
 
-// Get a list of hosts to display in a permission message from the given list of
-// hosts from the manifest.
-// TODO(sashab): Merge this into AddHostPermissions().
-std::vector<base::string16> GetHostListFromHosts(
-    const std::set<std::string>& hosts,
-    PermissionMessageProperties properties);
-
-// Adds the appropriate permissions from given hosts to |permissions|.
+// Adds the appropriate permissions from the given |hosts| to |permissions|.
 void AddHostPermissions(extensions::PermissionIDSet* permissions,
                         const std::set<std::string>& hosts,
                         PermissionMessageProperties properties);
