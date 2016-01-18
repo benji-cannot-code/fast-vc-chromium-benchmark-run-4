d4d2f91863629b5dca7be22d09174c1d9437a81d
eustas <eustas@chromium.org>
2016-01-18 07:13:29 -0800

    More robust brotli fuzzer.
    TLDR: same input, but +20% bits coverage.
    
    Brotli decoder has 2 "modes" fast and slow.
    Fast mode is activate when more than ~28 bytes of input are available. Slow mode does more checks and consumes input byte by byte. Always passing the whole input renders checking the most of the slow paths impossible.
    
    Feeding decoder with small chunks of data allows slow paths to be tested. This gives ~20% better bits coverage. Lines coverage increases insignificantly, as the same lines of code are used both by fast and slow mode.
    
    Last byte of input is used to choose the size of input chunk and passed to decoder as well. So input samples collected with previous fuzzer version are fully compatible.
    
    BUG=530417
    
    Review URL: https://codereview.chromium.org/1567033002
    
    Cr-Commit-Position: refs/heads/master@{#370026}

--- testing/libfuzzer/fuzzers/brotli_fuzzer.cc
+++ testing/libfuzzer/fuzzers/brotli_fuzzer.cc
@@ -9,20 +9,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const unsigned char* data, size_t size) {
-  int kBufferSize = 1024;
+  size_t addend = 0;
+  if (size > 0)
+    addend = data[size - 1] & 7;
+  const uint8_t* next_in = data;
+
+  const int kBufferSize = 1024;
   uint8_t* buffer = new uint8_t[kBufferSize];
   BrotliState* state = new BrotliState();
   BrotliStateInit(state);
 
-  size_t avail_in = size;
-  const uint8_t* next_in = data;
-  BrotliResult result = BROTLI_RESULT_NEEDS_MORE_OUTPUT;
-  while (result == BROTLI_RESULT_NEEDS_MORE_OUTPUT) {
-    size_t avail_out = kBufferSize;
-    uint8_t* next_out = buffer;
-    size_t total_out;
-    result = BrotliDecompressStream(
-        &avail_in, &next_in, &avail_out, &next_out, &total_out, state);
+  if (addend == 0)
+    addend = size;
+  /* Test both fast (addend == size) and slow (addend <= 7) decoding paths. */
+  for (size_t i = 0; i < size;) {
+    size_t next_i = i + addend;
+    if (next_i > size)
+      next_i = size;
+    size_t avail_in = next_i - i;
+    i = next_i;
+    BrotliResult result = BROTLI_RESULT_NEEDS_MORE_OUTPUT;
+    while (result == BROTLI_RESULT_NEEDS_MORE_OUTPUT) {
+      size_t avail_out = kBufferSize;
+      uint8_t* next_out = buffer;
+      size_t total_out;
+      result = BrotliDecompressStream(
+          &avail_in, &next_in, &avail_out, &next_out, &total_out, state);
+    }
+    if (result != BROTLI_RESULT_NEEDS_MORE_INPUT)
+      break;
   }
 
   BrotliStateCleanup(state);
