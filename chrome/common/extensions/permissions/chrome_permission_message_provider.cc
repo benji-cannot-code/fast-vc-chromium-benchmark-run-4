c3f5d33d9a0e7f7d17d97352eef4684a9ded0767
treib <treib@chromium.org>
2016-01-18 03:41:46 -0800

    Host permission messages: Move formatting into an actual Formatter as it should be in the new permission message system
    BUG=none
    
    Review URL: https://codereview.chromium.org/1585913006
    
    Cr-Commit-Position: refs/heads/master@{#370012}

--- chrome/common/extensions/permissions/chrome_permission_message_provider.cc
+++ chrome/common/extensions/permissions/chrome_permission_message_provider.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/common/extensions/permissions/chrome_permission_message_rules.h"
 #include "chrome/grit/generated_resources.h"
 #include "extensions/common/extensions_client.h"
@@ -168,9 +169,9 @@ void ChromePermissionMessageProvider::AddHostPermissions(
 
     std::set<std::string> hosts =
         permission_message_util::GetDistinctHosts(regular_hosts, true, true);
-    if (!hosts.empty()) {
-      permission_message_util::AddHostPermissions(
-          permission_ids, hosts, permission_message_util::kReadWrite);
+    for (const auto& host : hosts) {
+      permission_ids->insert(APIPermission::kHostReadWrite,
+                             base::UTF8ToUTF16(host));
     }
   }
 }
