20890067e54e26188ba99bdede9bce13188bb6c7
xiaochengh <xiaochengh@chromium.org>
2016-01-18 01:26:38 -0800

    Allow Casting From CompositeEditCommand to ReplaceSelectionCommand
    This is part of a previous CL [1].
    
    The final purpose is to allow retrieving the range of the pasted
    content after a |ReplaceSelectionCommand| is applied.
    
    [1] https://codereview.chromium.org/1532843002
    
    BUG=n/a
    TEST=n/a; no behavior change
    
    Review URL: https://codereview.chromium.org/1599293002
    
    Cr-Commit-Position: refs/heads/master@{#369997}

--- third_party/WebKit/Source/core/editing/commands/ReplaceSelectionCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/ReplaceSelectionCommand.cpp
@@ -1540,6 +1540,11 @@ bool ReplaceSelectionCommand::performTrivialReplace(const ReplacementFragment& f
     return true;
 }
 
+bool ReplaceSelectionCommand::isReplaceSelectionCommand() const
+{
+    return true;
+}
+
 DEFINE_TRACE(ReplaceSelectionCommand)
 {
     visitor->trace(m_startOfInsertedContent);
--- third_party/WebKit/Source/core/editing/commands/ReplaceSelectionCommand.h
+++ third_party/WebKit/Source/core/editing/commands/ReplaceSelectionCommand.h
@@ -60,6 +60,7 @@ private:
 
     void doApply() override;
     EditAction editingAction() const override;
+    bool isReplaceSelectionCommand() const override;
 
     class InsertedNodes {
         STACK_ALLOCATED();
@@ -119,6 +120,8 @@ private:
     bool m_sanitizeFragment;
     bool m_shouldMergeEnd;
 };
+
+DEFINE_TYPE_CASTS(ReplaceSelectionCommand, CompositeEditCommand, command, command->isReplaceSelectionCommand(), command.isReplaceSelectionCommand());
 
 } // namespace blink
 
