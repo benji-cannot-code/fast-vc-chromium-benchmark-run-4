4971e2305d29b7c56ec5793b35e516ab95cb787f
aizatsky <aizatsky@chromium.org>
2016-01-19 11:39:16 -0800

    [libfuzzer] disable crc checks in libpng fuzzer
    CRC checks do not let fuzzer penetrate the coverage.
    
    BUG=539572
    
    Review URL: https://codereview.chromium.org/1592023004
    
    Cr-Commit-Position: refs/heads/master@{#370165}

--- testing/libfuzzer/fuzzers/libpng_read_fuzzer.cc
+++ testing/libfuzzer/fuzzers/libpng_read_fuzzer.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback_helpers.h"
+#define PNG_INTERNAL
 #include "third_party/libpng/png.h"
 
 struct BufState {
@@ -42,6 +43,12 @@ extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
   png_structp png_ptr = png_create_read_struct
     (PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
   assert(png_ptr);
+
+  png_ptr->flags &= ~PNG_FLAG_CRC_CRITICAL_MASK;
+  png_ptr->flags |= PNG_FLAG_CRC_CRITICAL_IGNORE;
+
+  png_ptr->flags &= ~PNG_FLAG_CRC_ANCILLARY_MASK;
+  png_ptr->flags |= PNG_FLAG_CRC_ANCILLARY_NOWARN;
 
   png_infop info_ptr = png_create_info_struct(png_ptr);
   assert(info_ptr);
