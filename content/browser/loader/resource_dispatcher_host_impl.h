9a4e35f8f2ae8c7515a860b98f182f753be32e31
kinuko <kinuko@chromium.org>
2016-01-18 22:29:59 -0800

    Use scoped_ptr for loaders map in ResourceDispatcherHostImpl
    Mostly for cleanup, but this seems to have minor perf-win (5%-ish)
    on a few frequently called per-request methods like
    ResourceDispatcherHostImpl::RemovePendingRequest.
    
    (With 1000+ data points on my relatively fast Mac, the method takes
    72.77 us by average in the original code, while it takes
    66.67 us by average with scoped_ptr version.)
    
    BUG=556939
    
    Review URL: https://codereview.chromium.org/1603503002
    
    Cr-Commit-Position: refs/heads/master@{#370076}

--- content/browser/loader/resource_dispatcher_host_impl.h
+++ content/browser/loader/resource_dispatcher_host_impl.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
-#include "base/memory/linked_ptr.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
 #include "base/time/time.h"
@@ -343,7 +342,7 @@ class CONTENT_EXPORT ResourceDispatcherHostImpl
                             scoped_ptr<ResourceHandler> handler);
 
   void StartLoading(ResourceRequestInfoImpl* info,
-                    const linked_ptr<ResourceLoader>& loader);
+                    scoped_ptr<ResourceLoader> loader);
 
   // We keep track of how much memory each request needs and how many requests
   // are issued by each renderer. These are known as OustandingRequestStats.
@@ -390,7 +389,7 @@ class CONTENT_EXPORT ResourceDispatcherHostImpl
   // It may be enhanced in the future to provide some kind of prioritization
   // mechanism. We should also consider a hashtable or binary tree if it turns
   // out we have a lot of things here.
-  typedef std::map<GlobalRequestID, linked_ptr<ResourceLoader> > LoaderMap;
+  using LoaderMap = std::map<GlobalRequestID, scoped_ptr<ResourceLoader>>;
 
   // Deletes the pending request identified by the iterator passed in.
   // This function will invalidate the iterator passed in. Callers should
@@ -443,7 +442,7 @@ class CONTENT_EXPORT ResourceDispatcherHostImpl
                                 int route_id,
                                 int request_id,
                                 const ResourceHostMsg_Request& request_data,
-                                const linked_ptr<ResourceLoader>& loader);
+                                LoaderMap::iterator iter);
 
   void BeginRequest(int request_id,
                     const ResourceHostMsg_Request& request_data,
@@ -552,8 +551,9 @@ class CONTENT_EXPORT ResourceDispatcherHostImpl
   // True if the resource dispatcher host has been shut down.
   bool is_shutdown_;
 
-  typedef std::vector<linked_ptr<ResourceLoader> > BlockedLoadersList;
-  typedef std::map<GlobalRoutingID, BlockedLoadersList*> BlockedLoadersMap;
+  using BlockedLoadersList = std::vector<scoped_ptr<ResourceLoader>>;
+  using BlockedLoadersMap =
+      std::map<GlobalRoutingID, scoped_ptr<BlockedLoadersList>>;
   BlockedLoadersMap blocked_loaders_map_;
 
   // Maps the child_ids to the approximate number of bytes
