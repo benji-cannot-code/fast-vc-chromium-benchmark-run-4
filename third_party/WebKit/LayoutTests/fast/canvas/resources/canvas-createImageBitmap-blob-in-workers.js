46a2f06dfbdcce511fa27699e439ff80b92ff037
xidachen <xidachen@chromium.org>
2016-01-19 08:33:54 -0800

    Modify createImageBitmap-blob-in-worker to check race conditions
    In this CL, we modify the behavior of the layout test for createImageBitmap
    from a blob in worker in this way:
    As soon as we postMessage the blob to worker, both the main thread and the
    worker thread start createImageBitmap(blob) which means that the decoding
    will happen at the same time. We call createImageBitmap(blob) in the worker
    thread for only once but in the main thread we call it five times. The expected
    result of this is that the created ImageBitmap from the main thread and
    from the worker thread should have the same pixel data as the original blob.
    
    Review URL: https://codereview.chromium.org/1574393009
    
    Cr-Commit-Position: refs/heads/master@{#370124}

--- third_party/WebKit/LayoutTests/fast/canvas/resources/canvas-createImageBitmap-blob-in-workers.js
+++ third_party/WebKit/LayoutTests/fast/canvas/resources/canvas-createImageBitmap-blob-in-workers.js
@@ -1,16 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-importScripts('../../../resources/js-test.js');
-
-self.jsTestIsAsync = true;
-
-description('Test createImageBitmap with blob in workers.');
-
 self.addEventListener('message', function(e) {
-  createImageBitmap(e.data).then(function() {
-    testPassed('Promise fulfuilled.');
-    finishJSTest();
-  }, function() {
-    testFailed('Promise rejected.');
-    finishJSTest();
+  createImageBitmap(e.data).then(imageBitmap => {
+    postMessage({data: imageBitmap}, [imageBitmap]);
   });
 });
