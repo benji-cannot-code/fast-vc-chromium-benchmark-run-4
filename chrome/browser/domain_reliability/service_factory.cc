08f4dae9188d1350aa639b9cf146a5a5cec15d70
ttuttle <ttuttle@chromium.org>
2016-01-19 09:19:39 -0800

    Domain Reliability: Let command-line flag override UMA preference
    Assume that if the user explicitly passes --enable-domain-reliability,
    they want the browser to send Domain Reliability reports whether or not
    the UMA preference is turned on (or exists, e.g. in Chromium).
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1526793002
    
    Cr-Commit-Position: refs/heads/master@{#370131}

--- chrome/browser/domain_reliability/service_factory.cc
+++ chrome/browser/domain_reliability/service_factory.cc
@@ -32,21 +32,6 @@ const char kFieldTrialValueEnable[] = "enable";
 // Identifies Chrome as the source of Domain Reliability uploads it sends.
 const char kUploadReporterString[] = "chrome";
 
-bool IsDomainReliabilityMonitoringEnabled() {
-  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-  if (command_line->HasSwitch(switches::kDisableDomainReliability))
-    return false;
-  if (command_line->HasSwitch(switches::kEnableDomainReliability))
-    return true;
-
-  if (base::FieldTrialList::TrialExists(kFieldTrialName)) {
-    std::string value = base::FieldTrialList::FindFullName(kFieldTrialName);
-    return value == kFieldTrialValueEnable;
-  }
-
-  return kDefaultEnabled;
-}
-
 }  // namespace
 
 // static
@@ -73,14 +58,25 @@ DomainReliabilityServiceFactory::~DomainReliabilityServiceFactory() {}
 
 KeyedService* DomainReliabilityServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
-  if (!IsDomainReliabilityMonitoringEnabled())
-    return NULL;
-  // TODO(ttuttle): Move this check closer to where the data gets sent
-  // crbug.com/533486
-  if (!ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled())
+  if (!ShouldCreateService())
     return NULL;
 
   return DomainReliabilityService::Create(kUploadReporterString);
+}
+
+bool DomainReliabilityServiceFactory::ShouldCreateService() const {
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(switches::kDisableDomainReliability))
+    return false;
+  if (command_line->HasSwitch(switches::kEnableDomainReliability))
+    return true;
+  if (!ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled())
+    return false;
+  if (base::FieldTrialList::TrialExists(kFieldTrialName)) {
+    std::string value = base::FieldTrialList::FindFullName(kFieldTrialName);
+    return (value == kFieldTrialValueEnable);
+  }
+  return kDefaultEnabled;
 }
 
 }  // namespace domain_reliability
