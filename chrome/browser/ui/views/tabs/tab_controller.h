1a5749f295098ef92c336753a8776342ce51d299
pkasting <pkasting@chromium.org>
2016-01-19 14:45:03 -0800

    Misc. cleanup for tab code:
    * Move ImageCacheEntry into tab.cc; it doesn't need to be in the header.
    * Split out the metadata into a separate class, ImageCacheEntryMetadata.  This
      facilitates using standard algorithms to look through the image cache for an
      entry with matching metadata.  It also means I won't need to touch the
      functional loop (just the class definition boilerplate) when I add another
      item to the metadata, as I'll need to in an upcoming patch.
    * Minor comment and indenting fixes.
    * Shorten some code by factoring out new temps.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1588293002
    
    Cr-Commit-Position: refs/heads/master@{#370211}

--- chrome/browser/ui/views/tabs/tab_controller.h
+++ chrome/browser/ui/views/tabs/tab_controller.h
@@ -99,7 +99,7 @@ class TabController {
   // in the same window will redraw on top of the the favicon area of any tab.
   virtual bool CanPaintThrobberToLayer() const = 0;
 
-  // Returns true if tabs painted in the rectangular light-bar style.
+  // Returns true if tabs should be painted in the rectangular light-bar style.
   virtual bool IsImmersiveStyle() const = 0;
 
   // Returns the resource ID for the image to use as the tab background.
@@ -109,7 +109,7 @@ class TabController {
 
   // Adds private information to the tab's accessibility state.
   virtual void UpdateTabAccessibilityState(const Tab* tab,
-                                        ui::AXViewState* state) = 0;
+                                           ui::AXViewState* state) = 0;
 
  protected:
   virtual ~TabController() {}
