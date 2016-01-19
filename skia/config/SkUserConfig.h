840f0679773c832b0fa1fb94175c298a01d8642c
caryclark <caryclark@google.com>
2016-01-19 08:11:56 -0800

    Add legacy conic measure define for Skia
    An upcoming Skia CL improves the accuracy of measured conics, which
    are used to draw dashed circles and roundrects.
    
    Suppress the Skia CL with this guard until tests can be
    rebaselined.
    
    R=fmalita@chromium.org
    
    Review URL: https://codereview.chromium.org/1607913002
    
    Cr-Commit-Position: refs/heads/master@{#370120}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -207,6 +207,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 //
 // Remove these as we update our sites.
 //
+#ifndef    SK_SUPPORT_LEGACY_CONIC_MEASURE
+#   define SK_SUPPORT_LEGACY_CONIC_MEASURE
+#endif
+
 #ifndef    SK_SUPPORT_LEGACY_GETTOPDEVICE
 #   define SK_SUPPORT_LEGACY_GETTOPDEVICE
 #endif
