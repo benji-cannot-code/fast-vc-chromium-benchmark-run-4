be8f88a150475d1110711381b8c36a2fd2c9737c
vmpstr <vmpstr@chromium.org>
2016-01-19 13:16:10 -0800

    cc: Add image decode control in the compositor.
    This patch reworks ImageDecodeController and the surrounding
    system to give control over image decodes to the compositor.
    
    Perf note:
    
    This is disabled on android, and should only affect desktop.
    That being said, this patch changes the behavior with
    respect to image decodes, so some perf changes are expected.
    
    BUG=516751
    R=enne, reed1
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1418573002
    
    Cr-Commit-Position: refs/heads/master@{#370188}

--- cc/playback/display_list_raster_source.h
+++ cc/playback/display_list_raster_source.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cc {
 class DisplayItemList;
 class DrawImage;
+class ImageDecodeController;
 
 class CC_EXPORT DisplayListRasterSource
     : public base::RefCountedThreadSafe<DisplayListRasterSource> {
@@ -103,6 +104,10 @@ class CC_EXPORT DisplayListRasterSource
 
   scoped_refptr<DisplayListRasterSource> CreateCloneWithoutLCDText() const;
 
+  // Image decode controller should be set once. Its lifetime has to exceed that
+  // of the raster source, since the raster source will access it during raster.
+  void SetImageDecodeController(ImageDecodeController* image_decode_controller);
+
  protected:
   friend class base::RefCountedThreadSafe<DisplayListRasterSource>;
 
@@ -128,6 +133,10 @@ class CC_EXPORT DisplayListRasterSource
   // TODO(enne/vmiura): this has a read/write race between raster and compositor
   // threads with multi-threaded Ganesh.  Make this const or remove it.
   bool should_attempt_to_use_distance_field_text_;
+
+  // In practice, this is only set once before raster begins, so it's ok with
+  // respect to threading.
+  ImageDecodeController* image_decode_controller_;
 
  private:
   // Called when analyzing a tile. We can use AnalysisCanvas as
