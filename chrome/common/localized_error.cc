c544d3e5f5c63b9083d4a6b672a3a24a4ca721fa
blundell <blundell@chromium.org>
2016-01-19 00:55:09 -0800

    Remove usage of IDS_PRODUCT_NAME in localized_error.cc
    IDS_PRODUCT_NAME is used in localized_error.cc to set a |productName| parameter
    that was previously used to fill in placeholders in localized strings. After
    recent changes to the content of the strings, the |productName| parameter is no
    longer used. This CL eliminates it, and with it, the last blocker to
    componentizing LocalizedError.
    
    BUG=523362
    
    Review URL: https://codereview.chromium.org/1578363003
    
    Cr-Commit-Position: refs/heads/master@{#370085}

--- chrome/common/localized_error.cc
+++ chrome/common/localized_error.cc
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "build/build_config.h"
-#include "chrome/grit/chromium_strings.h"
-#include "chrome/grit/google_chrome_strings.h"
 #include "components/error_page/common/error_page_params.h"
 #include "components/error_page/common/error_page_switches.h"
 #include "components/error_page/common/net_error_info.h"
@@ -626,8 +624,6 @@ void LocalizedError::GetStrings(int error_code,
 
   summary->SetString("failedUrl", failed_url_string);
   summary->SetString("hostName", host_name);
-  summary->SetString("productName",
-                     l10n_util::GetStringUTF16(IDS_PRODUCT_NAME));
 
   error_strings->SetString(
       "details", l10n_util::GetStringUTF16(IDS_ERRORPAGE_NET_BUTTON_DETAILS));
