4c7b8a41dee61db7ed6442467a466314dfcb4a49
jsbell <jsbell@chromium.org>
2016-01-19 16:43:03 -0800

    IndexedDB: Remove quota checks on store/index creation
    Remove invalid/redundant quota checks on object store/index creation
    requests. These date back to the days when schema mutations where done
    synchronously. Now, quota checks need only be handled during
    transaction commit.
    
    BUG=579201
    R=cmumford@chromium.org
    
    Review URL: https://codereview.chromium.org/1606803003
    
    Cr-Commit-Position: refs/heads/master@{#370243}

--- content/browser/indexed_db/indexed_db_dispatcher_host.cc
+++ content/browser/indexed_db/indexed_db_dispatcher_host.cc
@@ -547,12 +547,6 @@ void IndexedDBDispatcherHost::DatabaseDispatcherHost::OnCreateObjectStore(
                                             params.name,
                                             params.key_path,
                                             params.auto_increment);
-  if (parent_->Context()->IsOverQuota(
-          database_url_map_[params.ipc_database_id])) {
-    connection->database()->Abort(
-        host_transaction_id,
-        IndexedDBDatabaseError(blink::WebIDBDatabaseExceptionQuotaError));
-  }
 }
 
 void IndexedDBDispatcherHost::DatabaseDispatcherHost::OnDeleteObjectStore(
@@ -921,12 +915,6 @@ void IndexedDBDispatcherHost::DatabaseDispatcherHost::OnCreateIndex(
                                       params.key_path,
                                       params.unique,
                                       params.multi_entry);
-  if (parent_->Context()->IsOverQuota(
-          database_url_map_[params.ipc_database_id])) {
-    connection->database()->Abort(
-        host_transaction_id,
-        IndexedDBDatabaseError(blink::WebIDBDatabaseExceptionQuotaError));
-  }
 }
 
 void IndexedDBDispatcherHost::DatabaseDispatcherHost::OnDeleteIndex(
