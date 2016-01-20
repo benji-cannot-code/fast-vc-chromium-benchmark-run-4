879be25704fdcb48086e7efd9cfd610e5dedf961
mseaborn <mseaborn@chromium.org>
2016-01-20 14:51:03 -0800

    PNaCl cleanup: Simplify error checking when opening temp files
    TempFile::CheckValidity() only ever returned PP_OK or PP_ERROR_FAILED.
    We can replace its result with a bool.
    
    This makes it clear that ReportPpapiError() was only ever called with
    PP_ERROR_FAILED, so we can remove it and call ReportNonPpapiError()
    instead.
    
    We can also move an error check into NexeReadDidOpen(), instead of
    having its callers check.
    
    BUG=302078
    TEST=e.g. NaClBrowserTestPnacl.PPAPICore (tests PNaCl translation)
    
    Review URL: https://codereview.chromium.org/1608313002
    
    Cr-Commit-Position: refs/heads/master@{#370518}

--- components/nacl/renderer/plugin/temporary_file.h
+++ components/nacl/renderer/plugin/temporary_file.h
@@ -43,7 +43,7 @@ class TempFile {
   TempFile(Plugin* plugin, PP_FileHandle handle);
   ~TempFile();
 
-  int32_t CheckValidity();
+  bool IsValid() { return file_handle_.IsValid(); }
   // Resets file position of the handle, for reuse.
   bool Reset();
 
