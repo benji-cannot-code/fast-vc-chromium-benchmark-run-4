4a25f657a934745a505c2e64fd957f58a9ce0249
eakuefner <eakuefner@chromium.org>
2016-01-20 11:52:49 -0800

    [Telemetry] Validate improvement_direction while serializing Summarizables
    A null improvement direction is flakily appearing in some serialized values. We
    don't currently validate the improvment direction in the constructor because
    not every Telemetry value callsite adds an improvement direction, but this
    validates the improvement direction in AsDict before serializing it.
    
    BUG=579558
    
    Review URL: https://codereview.chromium.org/1605293003
    
    Cr-Commit-Position: refs/heads/master@{#370449}

--- tools/telemetry/telemetry/value/summarizable.py
+++ tools/telemetry/telemetry/value/summarizable.py
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from telemetry import value as value_module
+from telemetry.value import (improvement_direction
+                             as improvement_direction_module)
 
 
 class SummarizableValue(value_module.Value):
@@ -23,7 +25,7 @@ class SummarizableValue(value_module.Value):
 
   def AsDict(self):
     d = super(SummarizableValue, self).AsDict()
-    if self.improvement_direction is not None:
+    if improvement_direction_module.IsValid(self.improvement_direction):
       d['improvement_direction'] = self.improvement_direction
     return d
 
