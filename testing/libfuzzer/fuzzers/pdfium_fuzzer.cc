4492e95d8549e1b52ea0f755e20c8c7f1605bfb1
aizatsky <aizatsky@chromium.org>
2016-01-19 16:00:14 -0800

    [libfuzzer] unittest-style main driver.
    Use unittest driver in non-fuzzer build.
    
    BUG=539572
    
    Review URL: https://codereview.chromium.org/1578413002
    
    Cr-Commit-Position: refs/heads/master@{#370228}

--- testing/libfuzzer/fuzzers/pdfium_fuzzer.cc
+++ testing/libfuzzer/fuzzers/pdfium_fuzzer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This fuzzer is simplified & cleaned up pdfium/samples/pdfium_test.cc
 
+#include <assert.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
