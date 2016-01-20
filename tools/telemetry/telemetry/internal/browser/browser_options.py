8df62bf1320cfbd4984f448b71797d8f5a487390
aiolos <aiolos@chromium.org>
2016-01-20 12:10:48 -0800

    Initialize the binary_manager in browser_options when browser/device=list.
    BUG=564757
    
    Review URL: https://codereview.chromium.org/1606343002
    
    Cr-Commit-Position: refs/heads/master@{#370467}

--- tools/telemetry/telemetry/internal/browser/browser_options.py
+++ tools/telemetry/telemetry/internal/browser/browser_options.py
@@ -22,6 +22,7 @@ from telemetry.internal.browser import browser_finder_exceptions
 from telemetry.internal.browser import profile_types
 from telemetry.internal.platform import device_finder
 from telemetry.internal.platform.profiler import profiler_finder
+from telemetry.internal.util import binary_manager
 from telemetry.util import wpr_modes
 
 
@@ -162,6 +163,8 @@ class BrowserFinderOptions(optparse.Values):
         logging.getLogger().setLevel(logging.WARNING)
 
       if self.device == 'list':
+        if binary_manager.NeedsInit():
+          binary_manager.InitDependencyManager(None)
         devices = device_finder.GetDevicesMatchingOptions(self)
         print 'Available devices:'
         for device in devices:
@@ -171,6 +174,8 @@ class BrowserFinderOptions(optparse.Values):
       if self.browser_executable and not self.browser_type:
         self.browser_type = 'exact'
       if self.browser_type == 'list':
+        if binary_manager.NeedsInit():
+          binary_manager.InitDependencyManager(None)
         devices = device_finder.GetDevicesMatchingOptions(self)
         if not devices:
           sys.exit(0)
