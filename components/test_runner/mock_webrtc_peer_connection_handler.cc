e3fe6311187cb40742a120db4a1e4c805052cc64
samli <samli@chromium.org>
2016-01-19 16:30:31 -0800

    Revert of Remove MockConstraints (patchset #1 id:1 of https://codereview.chromium.org/1605513002/ )
    Reason for revert:
    Appears to be causing fast/mediastream/RTCPeerConnection.html to fail across all bots.
    
    Original issue's description:
    > Remove MockConstraints
    >
    > The purpose of the MockConstraints class was to verify that only legal
    > constraint names were passed. This is now done inside Blink
    > in MediaConstraintsImpl.cpp, so this class isn't needed any more.
    >
    > BUG=543997
    >
    > Committed: https://crrev.com/dc42f8b18dbb9c8fec81e3b7a10ab0186ce36553
    > Cr-Commit-Position: refs/heads/master@{#370219}
    
    TBR=jochen@chromium.org,tommi@chromium.org,hta@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=543997
    
    Review URL: https://codereview.chromium.org/1608073002
    
    Cr-Commit-Position: refs/heads/master@{#370238}

--- components/test_runner/mock_webrtc_peer_connection_handler.cc
+++ components/test_runner/mock_webrtc_peer_connection_handler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include "components/test_runner/mock_constraints.h"
 #include "components/test_runner/mock_webrtc_data_channel_handler.h"
 #include "components/test_runner/mock_webrtc_dtmf_sender_handler.h"
 #include "components/test_runner/test_interfaces.h"
@@ -165,12 +166,16 @@ MockWebRTCPeerConnectionHandler::MockWebRTCPeerConnectionHandler(
 bool MockWebRTCPeerConnectionHandler::initialize(
     const WebRTCConfiguration& configuration,
     const WebMediaConstraints& constraints) {
-  interfaces_->GetDelegate()->PostTask(new RTCPeerConnectionStateTask(
-      this,
-      client_,
-      WebRTCPeerConnectionHandlerClient::ICEConnectionStateCompleted,
-      WebRTCPeerConnectionHandlerClient::ICEGatheringStateComplete));
-  return true;
+  if (MockConstraints::VerifyConstraints(constraints)) {
+    interfaces_->GetDelegate()->PostTask(new RTCPeerConnectionStateTask(
+        this,
+        client_,
+        WebRTCPeerConnectionHandlerClient::ICEConnectionStateCompleted,
+        WebRTCPeerConnectionHandlerClient::ICEGatheringStateComplete));
+    return true;
+  }
+
+  return false;
 }
 
 void MockWebRTCPeerConnectionHandler::createOffer(
