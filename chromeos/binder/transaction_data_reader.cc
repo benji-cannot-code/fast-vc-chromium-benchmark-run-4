b979ed8dec03c85c2c0cd932f421b43dd8e3b157
hashimoto <hashimoto@chromium.org>
2016-01-20 01:15:15 -0800

    Fix compile error on Release builds
    BUG=None
    TBR=satorux@chromium.org
    
    Review URL: https://codereview.chromium.org/1604153002
    
    Cr-Commit-Position: refs/heads/master@{#370354}

--- chromeos/binder/transaction_data_reader.cc
+++ chromeos/binder/transaction_data_reader.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <linux/android/binder.h>
 
+#include "base/logging.h"
 #include "chromeos/binder/local_object.h"
 #include "chromeos/binder/object.h"
 #include "chromeos/binder/remote_object.h"
