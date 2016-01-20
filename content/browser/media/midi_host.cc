67aad4db10e7fdc076f167a7ab2546d394cf184a
toyoshim <toyoshim@chromium.org>
2016-01-20 00:52:45 -0800

    Web MIDI: Add an UMA to take a statistics of sysex message size
    Add an UMA to learn sysex message size in real usages, and to know
    if users need better support to use a large message.
    
    BUG=578970
    
    Review URL: https://codereview.chromium.org/1608713003
    
    Cr-Commit-Position: refs/heads/master@{#370349}

--- content/browser/media/midi_host.cc
+++ content/browser/media/midi_host.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/process/process.h"
 #include "base/trace_event/trace_event.h"
 #include "content/browser/bad_message.h"
@@ -235,6 +236,7 @@ void MidiHost::Detach() {
 // static
 bool MidiHost::IsValidWebMIDIData(const std::vector<uint8_t>& data) {
   bool in_sysex = false;
+  size_t sysex_start_offset = 0;
   size_t waiting_data_length = 0;
   for (size_t i = 0; i < data.size(); ++i) {
     const uint8_t current = data[i];
@@ -247,14 +249,18 @@ bool MidiHost::IsValidWebMIDIData(const std::vector<uint8_t>& data) {
       continue;  // Found data byte as expected.
     }
     if (in_sysex) {
-      if (data[i] == kEndOfSysExByte)
+      if (data[i] == kEndOfSysExByte) {
         in_sysex = false;
-      else if (!IsDataByte(current))
+        UMA_HISTOGRAM_COUNTS("Media.Midi.SysExMessageSizeUpTo1MB",
+                             sysex_start_offset - i + 1);
+      } else if (!IsDataByte(current)) {
         return false;  // Error: |current| should have been data byte.
+      }
       continue;  // Found data byte as expected.
     }
     if (current == kSysExByte) {
       in_sysex = true;
+      sysex_start_offset = i;
       continue;  // Found SysEX
     }
     waiting_data_length = media::midi::GetMidiMessageLength(current);
