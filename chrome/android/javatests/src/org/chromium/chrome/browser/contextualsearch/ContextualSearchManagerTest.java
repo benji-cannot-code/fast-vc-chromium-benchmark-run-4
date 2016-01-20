3a4f55a3f9aff210cc9669407fa619fd0a0b206a
donnd <donnd@chromium.org>
2016-01-20 13:16:22 -0800

    [Contextual Search] Update testLongPressGestureSelects.
    Testing on a small screen (Android 1) showed a failure
    in this test, updated to wait for the panel to close
    instead of asserting that the panel closed.
    
    BUG=569123
    
    Review URL: https://codereview.chromium.org/1514113006
    
    Cr-Commit-Position: refs/heads/master@{#370489}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -1147,8 +1147,8 @@ public class ContextualSearchManagerTest extends ChromeActivityTestCaseBase<Chro
         assertLoadedNoUrl();  // No load after long-press until opening panel.
         clickNode("question-mark");
         waitForGestureProcessing();
+        waitForPanelToCloseAndSelectionDissolved();
         assertNull(getSelectedText());
-        assertPanelClosedOrUndefined();
         assertLoadedNoUrl();
     }
 
