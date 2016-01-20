6bc5096bacd32ee5730f811c06c7d02e6732c89e
aiolos <aiolos@chromium.org>
2016-01-20 10:17:49 -0800

    Update tools/perf to use cloud_storage from catapult.
    This is part of the migration work needed before we can remove the version
    in telemetry.
    
    BUG=565604
    
    Review URL: https://codereview.chromium.org/1568103002
    
    Cr-Commit-Position: refs/heads/master@{#370427}

--- tools/perf/profile_creators/__init__.py
+++ tools/perf/profile_creators/__init__.py
@@ -3,3 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """Generators for Chrome profiles for testing purposes."""
+import os
+import sys
+
+sys.path.insert(1, os.path.abspath(os.path.join(
+        __file__, '..', '..')))
+from core import path_util
+
+
+path_util.AddTelemetryToPath()
+path_util.AddCatapultBaseToPath()
