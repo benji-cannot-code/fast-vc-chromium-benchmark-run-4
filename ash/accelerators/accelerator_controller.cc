54b38e0438fa79d6385bd187f2f9020f0a3d10a2
shuchen <shuchen@chromium.org>
2016-01-19 16:38:35 -0800

    Removes ImeController::UsingFrenchInputMethod and thus ImeControlDelegate::RemapAccelerator.
    RemapAccelerator was introduced by cl https://chromiumcodereview.appspot.com/10452042
    which was to solve the issue that FR keyboard issues the number keys with key codes other
    than VKEY_0 - VKEY_9.
    That issue has been fixed in Ozone now, so FR keyboard can issue VKEY_0-VKEY_9 for number
    keys correctly. Therefore, RemapAccelerator is useless.
    
    BUG=572219
    TEST=No compile error. Issue 129017 doesn't repro.
    
    Review URL: https://codereview.chromium.org/1601063002
    
    Cr-Commit-Position: refs/heads/master@{#370239}

--- ash/accelerators/accelerator_controller.cc
+++ ash/accelerators/accelerator_controller.cc
@@ -776,10 +776,8 @@ void AcceleratorController::UnregisterAll(ui::AcceleratorTarget* target) {
 }
 
 bool AcceleratorController::Process(const ui::Accelerator& accelerator) {
-  if (ime_control_delegate_) {
-    return accelerator_manager_->Process(
-        ime_control_delegate_->RemapAccelerator(accelerator));
-  }
+  if (ime_control_delegate_)
+    return accelerator_manager_->Process(accelerator);
   return accelerator_manager_->Process(accelerator);
 }
 
@@ -790,11 +788,8 @@ bool AcceleratorController::IsRegistered(
 
 bool AcceleratorController::IsPreferred(
     const ui::Accelerator& accelerator) const {
-  const ui::Accelerator remapped_accelerator = ime_control_delegate_.get() ?
-      ime_control_delegate_->RemapAccelerator(accelerator) : accelerator;
-
   std::map<ui::Accelerator, AcceleratorAction>::const_iterator iter =
-      accelerators_.find(remapped_accelerator);
+      accelerators_.find(accelerator);
   if (iter == accelerators_.end())
     return false;  // not an accelerator.
 
@@ -803,11 +798,8 @@ bool AcceleratorController::IsPreferred(
 
 bool AcceleratorController::IsReserved(
     const ui::Accelerator& accelerator) const {
-  const ui::Accelerator remapped_accelerator = ime_control_delegate_.get() ?
-      ime_control_delegate_->RemapAccelerator(accelerator) : accelerator;
-
   std::map<ui::Accelerator, AcceleratorAction>::const_iterator iter =
-      accelerators_.find(remapped_accelerator);
+      accelerators_.find(accelerator);
   if (iter == accelerators_.end())
     return false;  // not an accelerator.
 
