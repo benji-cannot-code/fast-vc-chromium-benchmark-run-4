cbc052f2fa1223eb5aea5c1a87f1532b3802acae
alancutter <alancutter@chromium.org>
2016-01-20 00:50:02 -0800

    Avoid nesting blended transform operations when they don't depend on box sizing
    This change is a speculative stack overflow fix for sites that continuously
    retarget non-trivial transform transition animations.
    Instead of building an unbounded tree of interpolated transform operations
    this patch evaluates the final matrix result for cases that do not require
    knowledge of the element's box size.
    
    BUG=571437
    
    Review URL: https://codereview.chromium.org/1573243018
    
    Cr-Commit-Position: refs/heads/master@{#370345}

--- third_party/WebKit/Source/platform/transforms/TransformOperations.cpp
+++ third_party/WebKit/Source/platform/transforms/TransformOperations.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/geometry/FloatBox.h"
 #include "platform/transforms/IdentityTransformOperation.h"
 #include "platform/transforms/InterpolatedTransformOperation.h"
+#include "platform/transforms/Matrix3DTransformOperation.h"
 #include "platform/transforms/RotateTransformOperation.h"
 #include <algorithm>
 
@@ -91,11 +92,18 @@ TransformOperations TransformOperations::blendByMatchingOperations(const Transfo
     return result;
 }
 
-TransformOperations TransformOperations::blendByUsingMatrixInterpolation(const TransformOperations& from, double progress) const
+PassRefPtr<TransformOperation> TransformOperations::blendByUsingMatrixInterpolation(const TransformOperations& from, double progress) const
 {
-    TransformOperations result;
-    result.operations().append(InterpolatedTransformOperation::create(from, *this, progress));
-    return result;
+    if (dependsOnBoxSize() || from.dependsOnBoxSize())
+        return InterpolatedTransformOperation::create(from, *this, progress);
+
+    // Evaluate blended matrix here to avoid creating a nested data structure of unbounded depth.
+    TransformationMatrix fromTransform;
+    TransformationMatrix toTransform;
+    from.apply(FloatSize(), fromTransform);
+    apply(FloatSize(), toTransform);
+    toTransform.blend(fromTransform, progress);
+    return Matrix3DTransformOperation::create(toTransform);
 }
 
 TransformOperations TransformOperations::blend(const TransformOperations& from, double progress) const
@@ -107,7 +115,9 @@ TransformOperations TransformOperations::blend(const TransformOperations& from, 
     if (!from.size() || !size() || from.operationsMatch(*this))
         return blendByMatchingOperations(from, progress);
 
-    return blendByUsingMatrixInterpolation(from, progress);
+    TransformOperations result;
+    result.operations().append(blendByUsingMatrixInterpolation(from, progress));
+    return result;
 }
 
 static void findCandidatesInPlane(double px, double py, double nz, double* candidates, int* numCandidates)
