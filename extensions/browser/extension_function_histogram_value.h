f3e32f6d2009b681b26b794b9a1f7ff6b1c41b85
dtseng <dtseng@chromium.org>
2016-01-20 15:00:13 -0800

    Reland add a keyboard capture API for ChromeVox.
    Test failures in the original cl observed on linux chromeos dbg builds. Root cause fixed. AccessibilityManager's lifetime passes that of ExtensionRegistry. When attempting to unregister observers (via ScopedObserver), the objects no longer exist. Fix this by explicitly removing observers inside of ExtensionRegistryObserver::OnShutdown.
    
    The failures only occurred on dbg builds and had no useful output on stderr/stdout.
    
    Original issue:
    https://codereview.chromium.org/1567263002/
    
    TBR=dmazzoni@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1610213003
    
    Cr-Commit-Position: refs/heads/master@{#370520}

--- extensions/browser/extension_function_histogram_value.h
+++ extensions/browser/extension_function_histogram_value.h
@@ -1164,6 +1164,7 @@ enum HistogramValue {
   TERMINALPRIVATE_ACKOUTPUT,
   INPUT_IME_CREATEWINDOW,
   INPUTMETHODPRIVATE_SETXKBLAYOUT,
+  ACCESSIBILITY_PRIVATE_SETKEYBOARDLISTENER,
   // Last entry: Add new entries above, then run:
   // python tools/metrics/histograms/update_extension_histograms.py
   ENUM_BOUNDARY
