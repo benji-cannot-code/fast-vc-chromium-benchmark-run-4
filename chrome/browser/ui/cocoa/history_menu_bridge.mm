d5f96b933ae02304cc76c6dd7c7e5b27a55fe017
droger <droger@chromium.org>
2016-01-21 07:23:34 -0800

    Componentize the favicon for chrome://history
    TBR=thakis
    
    Committed: https://crrev.com/1f69728f5099df74ada2bf45ae72a2d294aa3cdf
    Cr-Commit-Position: refs/heads/master@{#368569}
    
    Review URL: https://codereview.chromium.org/1566983004
    
    Cr-Commit-Position: refs/heads/master@{#370696}

--- chrome/browser/ui/cocoa/history_menu_bridge.mm
+++ chrome/browser/ui/cocoa/history_menu_bridge.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sessions/tab_restore_service_factory.h"
 #import "chrome/browser/ui/cocoa/history_menu_cocoa_controller.h"
 #include "chrome/grit/generated_resources.h"
+#include "grit/components_scaled_resources.h"
 #include "grit/theme_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
