b870adec00d05dd15e40f04f41b662529c7a9d57
xiyuan <xiyuan@chromium.org>
2016-01-21 09:21:38 -0800

    kiosk: Add kiosk.required_platform_version manifest key
    BUG=577779
    TEST=KioskModeInfoManifestTest.RequiredPlatformVersion*
    
    Review URL: https://codereview.chromium.org/1595483004
    
    Cr-Commit-Position: refs/heads/master@{#370722}

--- extensions/common/manifest_handlers/kiosk_mode_info.cc
+++ extensions/common/manifest_handlers/kiosk_mode_info.cc
@@ -10,8 +10,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
+#include "base/version.h"
 #include "extensions/common/api/extensions_manifest_types.h"
 #include "extensions/common/manifest_constants.h"
+
+namespace {
+
+// Whether the given |version_string| is a valid ChromeOS platform version.
+// The acceptable format is major[.minor[.micro]].
+bool IsValidPlatformVersion(const std::string& version_string) {
+  const base::Version version(version_string);
+  return version.IsValid() && version.components().size() <= 3u;
+}
+
+}  // namespace
 
 namespace extensions {
 
@@ -20,8 +32,11 @@ namespace keys = manifest_keys;
 using api::extensions_manifest_types::KioskSecondaryAppsType;
 
 KioskModeInfo::KioskModeInfo(KioskStatus kiosk_status,
-                             const std::vector<std::string>& secondary_app_ids)
-    : kiosk_status(kiosk_status), secondary_app_ids(secondary_app_ids) {}
+                             const std::vector<std::string>& secondary_app_ids,
+                             const std::string& required_platform_version)
+    : kiosk_status(kiosk_status),
+      secondary_app_ids(secondary_app_ids),
+      required_platform_version(required_platform_version) {}
 
 KioskModeInfo::~KioskModeInfo() {
 }
@@ -51,6 +66,7 @@ bool KioskModeInfo::HasSecondaryApps(const Extension* extension) {
 }
 
 KioskModeHandler::KioskModeHandler() {
+  supported_keys_.push_back(keys::kKiosk);
   supported_keys_.push_back(keys::kKioskEnabled);
   supported_keys_.push_back(keys::kKioskOnly);
   supported_keys_.push_back(keys::kKioskSecondaryApps);
@@ -94,11 +110,10 @@ bool KioskModeHandler::Parse(Extension* extension, base::string16* error) {
 
   // Kiosk secondary apps key is optional.
   std::vector<std::string> ids;
-  if (extension->manifest()->HasKey(keys::kKioskSecondaryApps)) {
+  if (manifest->HasKey(keys::kKioskSecondaryApps)) {
     const base::Value* secondary_apps = nullptr;
     const base::ListValue* list = nullptr;
-    if (!extension->manifest()->Get(keys::kKioskSecondaryApps,
-                                    &secondary_apps) ||
+    if (!manifest->Get(keys::kKioskSecondaryApps, &secondary_apps) ||
         !secondary_apps->GetAsList(&list)) {
       *error = base::ASCIIToUTF16(manifest_errors::kInvalidKioskSecondaryApps);
       return false;
@@ -116,8 +131,20 @@ bool KioskModeHandler::Parse(Extension* extension, base::string16* error) {
     }
   }
 
-  extension->SetManifestData(keys::kKioskMode,
-                             new KioskModeInfo(kiosk_status, ids));
+  // Optional kiosk.required_platform_version key.
+  std::string required_platform_version;
+  if (manifest->HasPath(keys::kKioskRequiredPlatformVersion) &&
+      (!manifest->GetString(keys::kKioskRequiredPlatformVersion,
+                            &required_platform_version) ||
+       !IsValidPlatformVersion(required_platform_version))) {
+    *error = base::ASCIIToUTF16(
+        manifest_errors::kInvalidKioskRequiredPlatformVersion);
+    return false;
+  }
+
+  extension->SetManifestData(
+      keys::kKioskMode,
+      new KioskModeInfo(kiosk_status, ids, required_platform_version));
 
   return true;
 }
