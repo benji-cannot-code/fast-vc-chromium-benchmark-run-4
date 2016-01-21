93c00be2e96182336be52020d49132ffa0f0c7b1
mikhail.pozdnyakov <mikhail.pozdnyakov@intel.com>
2016-01-21 02:27:43 -0800

    [chrome.displaySource] DisplaySourceConnectionDelegate::Connection interface
    Extend DisplaySourceConnectionDelegate::Connection interface to support
    control message exchange with the connected sink.
    
    Also connection error notification method is added.
    
    BUG=242107
    
    Review URL: https://codereview.chromium.org/1603013002
    
    Cr-Commit-Position: refs/heads/master@{#370661}

--- extensions/browser/api/display_source/display_source_event_router.h
+++ extensions/browser/api/display_source/display_source_event_router.h
@@ -18,6 +18,9 @@ class BrowserContext;
 namespace extensions {
 
 // Observe listeners to 'onSinksUpdated' events.
+// This class initiates/stops listening for the available sinks
+// by the DisplaySourceConnectionDelegate, depending on whether
+// there are existing listeners to 'onSinksUpdated' event.
 class DisplaySourceEventRouter
     : public KeyedService,
       public EventRouter::Observer,
@@ -40,6 +43,9 @@ class DisplaySourceEventRouter
 
   // DisplaySourceConnectionDelegate::Observer overrides:
   void OnSinksUpdated(const DisplaySourceSinkInfoList& sinks) override;
+  void OnConnectionError(int sink_id,
+                         DisplaySourceErrorType type,
+                         const std::string& description) override {}
 
  private:
   void StartOrStopListeningForSinksChanges();
