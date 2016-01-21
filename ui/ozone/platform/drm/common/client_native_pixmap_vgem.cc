3dc5cb5d8baeea744a5a673579f42ae6d6202bcf
tiago.vignatti <tiago.vignatti@intel.com>
2016-01-21 05:00:44 -0800

    ui/ozone: Fix up VGEM header path
    BUG=475633
    
    Review URL: https://codereview.chromium.org/1604653002
    
    Cr-Commit-Position: refs/heads/master@{#370676}

--- ui/ozone/platform/drm/common/client_native_pixmap_vgem.cc
+++ ui/ozone/platform/drm/common/client_native_pixmap_vgem.cc
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/drm/common/client_native_pixmap_vgem.h"
 
 #include <fcntl.h>
+#include <libdrm/vgem_drm.h>
 #include <stddef.h>
 #include <sys/mman.h>
-#include <vgem_drm.h>
 #include <xf86drm.h>
 
 #include "base/process/memory.h"
