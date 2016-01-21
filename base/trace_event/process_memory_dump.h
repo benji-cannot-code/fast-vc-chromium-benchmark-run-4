b30fefceb5cd436e9e3418deb75c92f7334c542c
ssid <ssid@chromium.org>
2016-01-21 05:58:55 -0800

    Add support for CountResidentBytes in windows
    This CL adds support for CountResidentBytes on windows.
    The QueryWorkingSetEx gives the information about pages in range of
    given address that are in the working set of the process. We use that
    to count the resident bytes similar to how it's done in other
    platforms.
    
    BUG=570655
    
    Review URL: https://codereview.chromium.org/1573313002
    
    Cr-Commit-Position: refs/heads/master@{#370689}

--- base/trace_event/process_memory_dump.h
+++ base/trace_event/process_memory_dump.h
@@ -27,7 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // resident memory.
 // TODO(crbug.com/542671): COUNT_RESIDENT_BYTES_SUPPORTED is disabled on iOS
 // as it cause memory corruption on iOS 9.0+ devices.
-#if defined(OS_POSIX) && !defined(OS_NACL) && !defined(OS_IOS)
+#if (defined(OS_POSIX) && !defined(OS_NACL) && !defined(OS_IOS)) || \
+    defined(OS_WIN)
 #define COUNT_RESIDENT_BYTES_SUPPORTED
 #endif
 
