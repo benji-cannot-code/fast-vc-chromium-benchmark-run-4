e747e609061ec99c3473fc1552525518801ecb45
jaydasika <jaydasika@chromium.org>
2016-01-21 10:57:51 -0800

    cc:: Remove RenderSurfaceImpl::TargetEffectTreeIndex
    Need to remove it as it uses layer tree hierarchy information.
    It is used for computing surface draw opacity which can be computed directly
    from effect tree.
    
    BUG=568799
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1613583002
    
    Cr-Commit-Position: refs/heads/master@{#370739}

--- cc/layers/render_surface_impl.h
+++ cc/layers/render_surface_impl.h
@@ -156,7 +156,6 @@ class CC_EXPORT RenderSurfaceImpl {
   int TransformTreeIndex() const;
   int ClipTreeIndex() const;
   int EffectTreeIndex() const;
-  int TargetEffectTreeIndex() const;
 
  private:
   LayerImpl* owning_layer_;
