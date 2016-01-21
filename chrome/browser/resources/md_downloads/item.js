546694d54bc329ce1a7561a5f8aca10c1fb5a4bc
dbeam <dbeam@chromium.org>
2016-01-20 19:15:42 -0800

    MD Downloads: add back file icon scale factor
    Broken here: http://bit.ly/1KrP1nV
    
    I'll add tests, but want to make minimal merge CL first.
    
    R=tommycli@chromium.org
    BUG=549265
    
    Review URL: https://codereview.chromium.org/1612533004
    
    Cr-Commit-Position: refs/heads/master@{#370602}

--- chrome/browser/resources/md_downloads/item.js
+++ chrome/browser/resources/md_downloads/item.js
@@ -266,7 +266,8 @@ cr.define('downloads', function() {
     observeIsDangerous_: function() {
       if (this.data && !this.isDangerous_) {
         var filePath = encodeURIComponent(this.data.file_path);
-        this.$['file-icon'].src = 'chrome://fileicon/' + filePath;
+        var scaleFactor = '?scale=' + window.devicePixelRatio + 'x';
+        this.$['file-icon'].src = 'chrome://fileicon/' + filePath + scaleFactor;
       }
     },
 
