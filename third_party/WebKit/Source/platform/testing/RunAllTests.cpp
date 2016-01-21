a3bbf5bee3fbef5770fa6aa5409052ac4a0afd23
tasak <tasak@google.com>
2016-01-21 02:14:31 -0800

    Move Partitions::initialize() before Platform::initialize().
    BUG=
    
    Review URL: https://codereview.chromium.org/1619443002
    
    Cr-Commit-Position: refs/heads/master@{#370658}

--- third_party/WebKit/Source/platform/testing/RunAllTests.cpp
+++ third_party/WebKit/Source/platform/testing/RunAllTests.cpp
@@ -58,8 +58,9 @@ static int runTestSuite(base::TestSuite* testSuite)
 
 int main(int argc, char** argv)
 {
+    WTF::Partitions::initialize(nullptr);
     WTF::setAlwaysZeroRandomSourceForTesting();
-    WTF::initialize(CurrentTime, CurrentTime, nullptr, nullptr);
+    WTF::initialize(CurrentTime, CurrentTime, nullptr);
     WTF::initializeMainThread(0);
 
     blink::TestingPlatformSupport::Config platformConfig;
@@ -81,5 +82,6 @@ int main(int argc, char** argv)
     blink::Heap::shutdown();
 
     WTF::shutdown();
+    WTF::Partitions::shutdown();
     return result;
 }
--- third_party/WebKit/Source/wtf/testing/RunAllTests.cpp
+++ third_party/WebKit/Source/wtf/testing/RunAllTests.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "wtf/CryptographicallyRandomNumber.h"
 #include "wtf/MainThread.h"
+#include "wtf/Partitions.h"
 #include "wtf/WTF.h"
 #include <base/test/test_suite.h>
 #include <string.h>
@@ -42,8 +43,9 @@ static double CurrentTime()
 
 int main(int argc, char** argv)
 {
+    WTF::Partitions::initialize(nullptr);
     WTF::setAlwaysZeroRandomSourceForTesting();
-    WTF::initialize(CurrentTime, nullptr, nullptr, nullptr);
+    WTF::initialize(CurrentTime, nullptr, nullptr);
     WTF::initializeMainThread(0);
     return base::RunUnitTestsUsingBaseTestSuite(argc, argv);
 }
