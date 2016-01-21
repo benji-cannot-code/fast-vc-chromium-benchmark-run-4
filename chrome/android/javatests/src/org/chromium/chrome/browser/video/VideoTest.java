4d28f1c1f30d5c883246899a97a9eb2fb016b71b
timav <timav@chromium.org>
2016-01-20 16:16:10 -0800

    Remove @FlakyTest annotation for VideoTest.testLoadMediaUrl
    The test seems to be consistently passing the flaky bots
    according to
    http://clank-test-results.appspot.com/dashboards/flakiness_dashboard.html#tests=testLoadMediaUrl
    
    BUG=147966
    
    Review URL: https://codereview.chromium.org/1587363003
    
    Cr-Commit-Position: refs/heads/master@{#370542}

--- chrome/android/javatests/src/org/chromium/chrome/browser/video/VideoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/video/VideoTest.java
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.video;
 
-import android.test.FlakyTest;
+import android.test.suitebuilder.annotation.LargeTest;
 
+import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.test.ChromeActivityTestCaseBase;
@@ -25,11 +26,8 @@ public class VideoTest extends ChromeActivityTestCaseBase<ChromeActivity> {
         super(ChromeActivity.class);
     }
 
-    /*
-     * @LargeTest
-     * @Feature({"Media", "Media-Video", "Main"})
-     */
-    @FlakyTest
+    @Feature({"Media", "Media-Video", "Main"})
+    @LargeTest
     public void testLoadMediaUrl() throws InterruptedException, TimeoutException {
         Tab tab = getActivity().getActivityTab();
         TabTitleObserver titleObserver = new TabTitleObserver(tab, "ready_to_play");
