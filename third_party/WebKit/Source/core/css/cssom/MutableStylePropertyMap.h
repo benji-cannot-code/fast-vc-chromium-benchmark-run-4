b6c228910d2975de4d5c8e4b9bf9e92c7907d93e
meade <meade@chromium.org>
2016-01-20 22:22:45 -0800

    Add outline files for StylePropertyMap.
    Spec: https://drafts.css-houdini.org/css-typed-om/#stylepropertymap
    
    BUG=545318
    
    Review URL: https://codereview.chromium.org/1582963004
    
    Cr-Commit-Position: refs/heads/master@{#370637}

---
+++ third_party/WebKit/Source/core/css/cssom/MutableStylePropertyMap.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MutableStylePropertyMap_h
+#define MutableStylePropertyMap_h
+
+#include "core/css/cssom/StylePropertyMap.h"
+
+namespace blink {
+
+class CORE_EXPORT MutableStylePropertyMap : public StylePropertyMap {
+    WTF_MAKE_NONCOPYABLE(MutableStylePropertyMap);
+};
+
+} // namespace blink
+
+#endif
