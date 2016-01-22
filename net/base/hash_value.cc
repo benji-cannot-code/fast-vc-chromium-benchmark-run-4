01d0c39c63901a77d489b822ce3892fa9006f2c5
landell <landell@opera.com>
2016-01-22 14:22:28 -0800

    Include stdlib.h for bsearch
    BUG=
    
    Review URL: https://codereview.chromium.org/1618193002
    
    Cr-Commit-Position: refs/heads/master@{#371057}

--- net/base/hash_value.cc
+++ net/base/hash_value.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/base/hash_value.h"
 
+#include <stdlib.h>
+
 #include "base/base64.h"
 #include "base/logging.h"
 #include "base/sha1.h"
