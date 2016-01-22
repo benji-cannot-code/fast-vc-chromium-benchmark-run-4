0a4a4846b2fbbfeeceb883a04d05e27716767e36
rch <rch@chromium.org>
2016-01-22 13:07:51 -0800

    Refactor QUIC buffer allocator.
    QUIC streamable frames can now use a freelist for their packet buffers,
    Guarded by FLAGS_use_stream_frame_freelist.
    
    Merge internal change: 111443217
    
    Review URL: https://codereview.chromium.org/1611323005
    
    Cr-Commit-Position: refs/heads/master@{#371038}

--- net/tools/quic/quic_epoll_connection_helper.h
+++ net/tools/quic/quic_epoll_connection_helper.h
@@ -33,6 +33,8 @@ class RetransmissionAlarm;
 class SendAlarm;
 class TimeoutAlarm;
 
+using QuicStreamBufferAllocator = SimpleBufferAllocator;
+
 class QuicEpollConnectionHelper : public QuicConnectionHelperInterface {
  public:
   explicit QuicEpollConnectionHelper(EpollServer* eps);
@@ -57,7 +59,7 @@ class QuicEpollConnectionHelper : public QuicConnectionHelperInterface {
 
   const QuicEpollClock clock_;
   QuicRandom* random_generator_;
-  SimpleBufferAllocator buffer_allocator_;
+  QuicStreamBufferAllocator buffer_allocator_;
 
   DISALLOW_COPY_AND_ASSIGN(QuicEpollConnectionHelper);
 };
