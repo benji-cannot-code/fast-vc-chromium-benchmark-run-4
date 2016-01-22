c7a1a248e1429fa30a075df0b93bb4b0e3677944
oshima <oshima@chromium.org>
2016-01-22 10:13:14 -0800

    Use WebScreenInfo.deviceScaleFactor when computing screen metrics
    * Changed layout tests to use testRunner.setBackingScaleFactor instead. It does more correct thing than internals.setDeviceScaleFactor.
    * Removed internals.setDeviceScaleFactor as it's no longer used.
    
    BUG=485650
    
    Review URL: https://codereview.chromium.org/1619593002
    
    Cr-Commit-Position: refs/heads/master@{#370995}

--- third_party/WebKit/Source/core/frame/Screen.cpp
+++ third_party/WebKit/Source/core/frame/Screen.cpp
@@ -52,8 +52,10 @@ int Screen::height() const
     FrameHost* host = m_frame->host();
     if (!host)
         return 0;
-    if (host->settings().reportScreenSizeInPhysicalPixelsQuirk())
-        return lroundf(host->chromeClient().screenInfo().rect.height * host->deviceScaleFactor());
+    if (host->settings().reportScreenSizeInPhysicalPixelsQuirk()) {
+        WebScreenInfo screenInfo = host->chromeClient().screenInfo();
+        return lroundf(screenInfo.rect.height * screenInfo.deviceScaleFactor);
+    }
     return host->chromeClient().screenInfo().rect.height;
 }
 
@@ -64,8 +66,10 @@ int Screen::width() const
     FrameHost* host = m_frame->host();
     if (!host)
         return 0;
-    if (host->settings().reportScreenSizeInPhysicalPixelsQuirk())
-        return lroundf(host->chromeClient().screenInfo().rect.width * host->deviceScaleFactor());
+    if (host->settings().reportScreenSizeInPhysicalPixelsQuirk()) {
+        WebScreenInfo screenInfo = host->chromeClient().screenInfo();
+        return lroundf(screenInfo.rect.width * screenInfo.deviceScaleFactor);
+    }
     return host->chromeClient().screenInfo().rect.width;
 }
 
@@ -90,8 +94,10 @@ int Screen::availLeft() const
     FrameHost* host = m_frame->host();
     if (!host)
         return 0;
-    if (host->settings().reportScreenSizeInPhysicalPixelsQuirk())
-        return lroundf(host->chromeClient().screenInfo().availableRect.x * host->deviceScaleFactor());
+    if (host->settings().reportScreenSizeInPhysicalPixelsQuirk()) {
+        WebScreenInfo screenInfo = host->chromeClient().screenInfo();
+        return lroundf(screenInfo.availableRect.x * screenInfo.deviceScaleFactor);
+    }
     return static_cast<int>(host->chromeClient().screenInfo().availableRect.x);
 }
 
@@ -102,8 +108,10 @@ int Screen::availTop() const
     FrameHost* host = m_frame->host();
     if (!host)
         return 0;
-    if (host->settings().reportScreenSizeInPhysicalPixelsQuirk())
-        return lroundf(host->chromeClient().screenInfo().availableRect.y * host->deviceScaleFactor());
+    if (host->settings().reportScreenSizeInPhysicalPixelsQuirk()) {
+        WebScreenInfo screenInfo = host->chromeClient().screenInfo();
+        return lroundf(screenInfo.availableRect.y * screenInfo.deviceScaleFactor);
+    }
     return static_cast<int>(host->chromeClient().screenInfo().availableRect.y);
 }
 
@@ -114,8 +122,10 @@ int Screen::availHeight() const
     FrameHost* host = m_frame->host();
     if (!host)
         return 0;
-    if (host->settings().reportScreenSizeInPhysicalPixelsQuirk())
-        return lroundf(host->chromeClient().screenInfo().availableRect.height * host->deviceScaleFactor());
+    if (host->settings().reportScreenSizeInPhysicalPixelsQuirk()) {
+        WebScreenInfo screenInfo = host->chromeClient().screenInfo();
+        return lroundf(screenInfo.availableRect.height * screenInfo.deviceScaleFactor);
+    }
     return host->chromeClient().screenInfo().availableRect.height;
 }
 
@@ -126,8 +136,10 @@ int Screen::availWidth() const
     FrameHost* host = m_frame->host();
     if (!host)
         return 0;
-    if (host->settings().reportScreenSizeInPhysicalPixelsQuirk())
-        return lroundf(host->chromeClient().screenInfo().availableRect.width * host->deviceScaleFactor());
+    if (host->settings().reportScreenSizeInPhysicalPixelsQuirk()) {
+        WebScreenInfo screenInfo = host->chromeClient().screenInfo();
+        return lroundf(screenInfo.availableRect.width * screenInfo.deviceScaleFactor);
+    }
     return host->chromeClient().screenInfo().availableRect.width;
 }
 
