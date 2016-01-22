6748710165371100a6df06bd2a90e3fa4009e157
jsbell <jsbell@chromium.org>
2016-01-22 10:45:45 -0800

    Indexed DB: Remove unused usage/quota caching code
    A previous CL changed Indexed DB to query the quota manager on demand,
    during transaction commit. This CL cleans up the old code paths that
    proactively queried and cached the quota numbers.
    
    BUG=579201
    R=cmumford@chromium.org
    
    Review URL: https://codereview.chromium.org/1606113003
    
    Cr-Commit-Position: refs/heads/master@{#371001}

--- content/browser/indexed_db/indexed_db_context_impl.h
+++ content/browser/indexed_db/indexed_db_context_impl.h
@@ -84,7 +84,6 @@ class CONTENT_EXPORT IndexedDBContextImpl
   void ConnectionClosed(const GURL& origin_url, IndexedDBConnection* db);
   void TransactionComplete(const GURL& origin_url);
   void DatabaseDeleted(const GURL& origin_url);
-  bool WouldBeOverQuota(const GURL& origin_url, int64_t additional_bytes);
 
   // Will be null in unit tests.
   storage::QuotaManagerProxy* quota_manager_proxy() const {
@@ -135,11 +134,6 @@ class CONTENT_EXPORT IndexedDBContextImpl
   int64_t ReadUsageFromDisk(const GURL& origin_url) const;
   void EnsureDiskUsageCacheInitialized(const GURL& origin_url);
   void QueryDiskAndUpdateQuotaUsage(const GURL& origin_url);
-  void GotUsageAndQuota(const GURL& origin_url,
-                        storage::QuotaStatusCode,
-                        int64_t usage,
-                        int64_t quota);
-  void QueryAvailableQuota(const GURL& origin_url);
   base::Time GetOriginLastModified(const GURL& origin_url);
 
   std::set<GURL>* GetOriginSet();
@@ -162,7 +156,6 @@ class CONTENT_EXPORT IndexedDBContextImpl
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
   scoped_ptr<std::set<GURL> > origin_set_;
   OriginToSizeMap origin_size_map_;
-  OriginToSizeMap space_available_map_;
 
   DISALLOW_COPY_AND_ASSIGN(IndexedDBContextImpl);
 };
