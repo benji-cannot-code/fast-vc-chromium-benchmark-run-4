db44b3980e21390d3c40d3ba3598e6ca3d6138b2
stevenjb <stevenjb@chromium.org>
2016-01-21 17:55:36 -0800

    Handle touch events in the status tray network info button
    views::Views do not handle touch events, so we need to place the
    InfoIcon button in front and cross fade the icon button and
    throbber.
    
    BUG=558777
    
    Review URL: https://codereview.chromium.org/1585333002
    
    Cr-Commit-Position: refs/heads/master@{#370872}

--- ash/system/chromeos/network/network_state_list_detailed_view.h
+++ ash/system/chromeos/network/network_state_list_detailed_view.h
@@ -28,6 +28,7 @@ class NetworkListViewBase;
 
 namespace views {
 class BubbleDelegateView;
+class ImageButton;
 }
 
 namespace ash {
@@ -126,7 +127,7 @@ class NetworkStateListDetailedView
   bool wifi_scanning_;
 
   // Child views.
-  TrayPopupHeaderButton* info_icon_;
+  views::ImageButton* info_icon_;
   TrayPopupHeaderButton* button_wifi_;
   TrayPopupHeaderButton* button_mobile_;
   TrayPopupLabelButton* other_wifi_;
