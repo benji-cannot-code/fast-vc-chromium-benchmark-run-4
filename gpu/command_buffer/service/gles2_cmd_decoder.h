5bc282b3ae0fd6067cb040d3729bae3c8315f294
dyen <dyen@chromium.org>
2016-01-22 15:38:31 -0800

    Removed last references to old sync points.
    BUG=514815, 373452
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1615253006
    
    Cr-Commit-Position: refs/heads/master@{#371082}

--- gpu/command_buffer/service/gles2_cmd_decoder.h
+++ gpu/command_buffer/service/gles2_cmd_decoder.h
@@ -66,7 +66,6 @@ class GPU_EXPORT GLES2Decoder : public base::SupportsWeakPtr<GLES2Decoder>,
                                 public CommonDecoder {
  public:
   typedef error::Error Error;
-  typedef base::Callback<bool(uint32_t id)> WaitSyncPointCallback;
   typedef base::Callback<void(uint64_t release)> FenceSyncReleaseCallback;
   typedef base::Callback<bool(gpu::CommandBufferNamespace namespace_id,
                               uint64_t command_buffer_id,
@@ -232,11 +231,6 @@ class GPU_EXPORT GLES2Decoder : public base::SupportsWeakPtr<GLES2Decoder>,
 
   // A callback for messages from the decoder.
   virtual void SetShaderCacheCallback(const ShaderCacheCallback& callback) = 0;
-
-  // Sets the callback for waiting on a sync point. The callback returns the
-  // scheduling status (i.e. true if the channel is still scheduled).
-  virtual void SetWaitSyncPointCallback(
-      const WaitSyncPointCallback& callback) = 0;
 
   // Sets the callback for fence sync release and wait calls. The wait call
   // returns true if the channel is still scheduled.
