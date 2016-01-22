b66885c243f99620223e9c15cc6bbfb30ac155a5
zqzhang <zqzhang@chromium.org>
2016-01-22 04:03:37 -0800

    Remote player pause should not notify MediaSession
    This patch fixes an issue that in Cast mode, when pausing a casted media
    from the browser, it will notify the MediaSession that a player has been
    paused. This should not happen since the MediaSession should not take
    care of remote players.
    
    BUG=577819
    
    Review URL: https://codereview.chromium.org/1591603003
    
    Cr-Commit-Position: refs/heads/master@{#370949}

--- chrome/browser/media/android/remote/remote_media_player_manager.cc
+++ chrome/browser/media/android/remote/remote_media_player_manager.cc
@@ -72,6 +72,10 @@ void RemoteMediaPlayerManager::OnRequestRemotePlaybackControl(int player_id) {
     player->RequestRemotePlaybackControl();
 }
 
+bool RemoteMediaPlayerManager::IsPlayingRemotely(int player_id) {
+  return players_playing_remotely_.count(player_id) != 0;
+}
+
 int RemoteMediaPlayerManager::GetTabId() {
   if (!web_contents())
     return -1;
@@ -270,9 +274,4 @@ void RemoteMediaPlayerManager::OnMediaMetadataChanged(int player_id,
                                                       width, height, success);
   }
 }
-
-bool RemoteMediaPlayerManager::IsPlayingRemotely(int player_id) {
-  return players_playing_remotely_.count(player_id) != 0;
-}
-
 } // namespace remote_media
--- content/browser/media/android/browser_media_player_manager.cc
+++ content/browser/media/android/browser_media_player_manager.cc
@@ -623,7 +623,7 @@ void BrowserMediaPlayerManager::OnPause(
   if (player)
     player->Pause(is_media_related_action);
 
-  if (is_media_related_action)
+  if (is_media_related_action && !IsPlayingRemotely(player_id))
     MediaSession::Get(web_contents())->OnPlayerPaused(this, player_id);
 }
 
@@ -655,6 +655,10 @@ void BrowserMediaPlayerManager::OnRequestRemotePlayback(int /* player_id */) {
 void BrowserMediaPlayerManager::OnRequestRemotePlaybackControl(
     int /* player_id */) {
   // Does nothing if we don't have a remote player
+}
+
+bool BrowserMediaPlayerManager::IsPlayingRemotely(int player_id) {
+  return false;
 }
 
 void BrowserMediaPlayerManager::AddPlayer(MediaPlayerAndroid* player) {
