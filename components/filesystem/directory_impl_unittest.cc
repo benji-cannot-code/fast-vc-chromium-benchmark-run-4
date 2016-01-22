999783a9101a69172212833ce31e54645316c351
erg <erg@chromium.org>
2016-01-22 15:49:01 -0800

    mash: Add a simple, temporary preferences store.
    In the long run, we'll need a real solution to preferences, but for now,
    we replace the implementation of PersistentPrefsStore with one that
    writes JSON values to file on the mojo:filesystem. The current interface
    isn't really a match made in heaven and it doesn't really deal with all
    the complexity that we need to worry about (see 580652), but it's
    reasonably sandboxed and will work for now.
    
    BUG=557405, 580652
    
    Review URL: https://codereview.chromium.org/1624683002
    
    Cr-Commit-Position: refs/heads/master@{#371086}

--- components/filesystem/directory_impl_unittest.cc
+++ components/filesystem/directory_impl_unittest.cc
@@ -104,7 +104,7 @@ TEST_F(DirectoryImplTest, BasicRenameDelete) {
   directory->OpenFile("my_file", nullptr, kFlagRead | kFlagOpen,
                       Capture(&error));
   ASSERT_TRUE(directory.WaitForIncomingResponse());
-  EXPECT_EQ(FileError::FAILED, error);
+  EXPECT_EQ(FileError::NOT_FOUND, error);
 
   // Opening my_new_file should succeed.
   error = FileError::FAILED;
@@ -123,7 +123,7 @@ TEST_F(DirectoryImplTest, BasicRenameDelete) {
   directory->OpenFile("my_new_file", nullptr, kFlagRead | kFlagOpen,
                       Capture(&error));
   ASSERT_TRUE(directory.WaitForIncomingResponse());
-  EXPECT_EQ(FileError::FAILED, error);
+  EXPECT_EQ(FileError::NOT_FOUND, error);
 }
 
 TEST_F(DirectoryImplTest, CantOpenDirectoriesAsFiles) {
