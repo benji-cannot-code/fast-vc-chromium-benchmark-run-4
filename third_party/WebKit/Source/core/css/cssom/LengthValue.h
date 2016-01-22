8f46022c815590bed7e38b407add1d6baa2bd436
lawton <lawton@google.com>
2016-01-21 21:40:42 -0800

    Made changes to LengthValue and its subclass StyleCalcLength needed for classes that encapsulate them
    In LengthValue the clone() method was changed to const
    
    In StyleCalcLength the has() method was changed to public
    
    BUG=545318
    
    Review URL: https://codereview.chromium.org/1604263003
    
    Cr-Commit-Position: refs/heads/master@{#370914}

--- third_party/WebKit/Source/core/css/cssom/LengthValue.h
+++ third_party/WebKit/Source/core/css/cssom/LengthValue.h
@@ -45,7 +45,7 @@ public:
     static LengthValue* fromValue(double value, const String& typeStr, ExceptionState&);
     static LengthValue* fromDictionary(const CalcDictionary&, ExceptionState&);
 
-    LengthValue* clone();
+    LengthValue* clone() const;
 
 protected:
     LengthValue() {}
