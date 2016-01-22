b76356d6938a7a992a65c7e0e7335a58d789647b
lizeb <lizeb@chromium.org>
2016-01-22 02:28:05 -0800

    android: Re-enable ContextMenuLoadUrlParamsTest tests.
    These are no longer flaky locally, after 100 runs on phone and tablet,
    running L and K.
    
    BUG=413216
    
    Review URL: https://codereview.chromium.org/1611503002
    
    Cr-Commit-Position: refs/heads/master@{#370937}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/ContextMenuLoadUrlParamsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/ContextMenuLoadUrlParamsTest.java
@@ -5,9 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tabmodel;
 
-import android.test.FlakyTest;
+import android.test.suitebuilder.annotation.MediumTest;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.tab.Tab;
@@ -74,11 +75,9 @@ public class ContextMenuLoadUrlParamsTest extends ChromeTabbedActivityTestBase {
 
     /**
      * Verifies that the referrer is correctly set for "Open in new tab".
-     * Bug: crbug.com/413216
-     * @MediumTest
-     * @Feature({"Browser"})
      */
-    @FlakyTest
+    @MediumTest
+    @Feature({"Browser"})
     public void testOpenInNewTabReferrer()
             throws InterruptedException, TimeoutException {
         triggerContextMenuLoad(TEST_URL, "testLink", R.id.contextmenu_open_in_new_tab);
@@ -89,11 +88,9 @@ public class ContextMenuLoadUrlParamsTest extends ChromeTabbedActivityTestBase {
 
     /**
      * Verifies that the referrer is not set for "Open in new incognito tab".
-     * Bug: crbug.com/413216
-     * @MediumTest
-     * @Feature({"Browser"})
      */
-    @FlakyTest
+    @MediumTest
+    @Feature({"Browser"})
     public void testOpenInIncognitoTabNoReferrer()
             throws InterruptedException, TimeoutException {
         triggerContextMenuLoad(TEST_URL, "testLink", R.id.contextmenu_open_in_incognito_tab);
@@ -104,11 +101,9 @@ public class ContextMenuLoadUrlParamsTest extends ChromeTabbedActivityTestBase {
 
     /**
      * Verifies that the referrer is stripped from username and password fields.
-     * Bug: crbug.com/413216
-     * @MediumTest
-     * @Feature({"Browser"})
      */
-    @FlakyTest
+    @MediumTest
+    @Feature({"Browser"})
     public void testOpenInNewTabSanitizeReferrer()
             throws InterruptedException, TimeoutException {
         assertTrue(TEST_URL_USER_PASS.contains("pass")); // Sanity check.
