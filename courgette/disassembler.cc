3c2dc61195cb35c3f23a6e9b455e733c2571f632
huangs <huangs@chromium.org>
2016-01-21 22:03:25 -0800

    [Courgette] Fold TrimLabels() into ParseDetectedExecutable().
    TrimLabels() was a post-processing step after ParseDetectedExecutable().
    Soon we'll do trimming earlier in the flow. However, existing code calls
    TrimLabels() right after ParseDetectedExecutable(). This CL moves
    TrimLabels() into ParseDetectedExecutable() to simplify the transition.
    
    Existing flows that call ParseDetectedExecutable() are also inconsistent
    in TrimLabel() usage; {-gen, -apply, -dis} do, and debugging flows
    {-disadj, -gen1a, -gen1u} don't. This CL makes the debugging flows use
    TrimLabel(). This side effect is likely positive, since it improves
    consistently. Meanwhile, the actual impact is small: TrimLabels() is
    active only for ELF ARM anyway.
    
    Review URL: https://codereview.chromium.org/1615513002
    
    Cr-Commit-Position: refs/heads/master@{#370916}

--- courgette/disassembler.cc
+++ courgette/disassembler.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/logging.h"
-
+#include "base/memory/scoped_ptr.h"
 #include "courgette/assembly_program.h"
 #include "courgette/courgette.h"
 #include "courgette/disassembler_elf_32_arm.h"
@@ -77,24 +77,22 @@ Status DetectExecutableType(const void* buffer, size_t length,
 
 Status ParseDetectedExecutable(const void* buffer, size_t length,
                                AssemblyProgram** output) {
-  *output = NULL;
+  *output = nullptr;
 
-  Disassembler* disassembler = DetectDisassembler(buffer, length);
-
-  if (!disassembler) {
+  scoped_ptr<Disassembler> disassembler(DetectDisassembler(buffer, length));
+  if (!disassembler)
     return C_INPUT_NOT_RECOGNIZED;
-  }
 
-  AssemblyProgram* program = new AssemblyProgram(disassembler->kind());
+  scoped_ptr<AssemblyProgram> program(
+      new AssemblyProgram(disassembler->kind()));
 
-  if (!disassembler->Disassemble(program)) {
-    delete program;
-    delete disassembler;
+  if (!disassembler->Disassemble(program.get()))
     return C_DISASSEMBLY_FAILED;
-  }
 
-  delete disassembler;
-  *output = program;
+  if (!program->TrimLabels())
+    return C_TRIM_FAILED;
+
+  *output = program.release();
   return C_OK;
 }
 
