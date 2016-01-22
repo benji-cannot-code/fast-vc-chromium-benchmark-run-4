555140e1e073338b13138a5d4f5199864f27f819
rajendrant <rajendrant@chromium.org>
2016-01-22 09:26:07 -0800

    Rename data usage tab model histograms prefix to DataUsage Rename prefix of the DataUseTabModel histograms to DataUsage, since DataUse prefix is already used by histograms in a completely unrelated component.
    The previous histograms do not contain any data, hence it is okay to
    simply rename them, instead of marking them as obsolete and adding new
    histograms.
    
    (To be landed in M49)
    
    BUG=579694
    
    Review URL: https://codereview.chromium.org/1614433003
    
    Cr-Commit-Position: refs/heads/master@{#370987}

--- chrome/browser/android/data_usage/tab_data_use_entry.cc
+++ chrome/browser/android/data_usage/tab_data_use_entry.cc
@@ -16,9 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const char kUMATrackingSessionLifetimeHistogram[] =
-    "DataUse.TabModel.TrackingSessionLifetime";
+    "DataUsage.TabModel.TrackingSessionLifetime";
 const char kUMAOldInactiveSessionRemovalDurationHistogram[] =
-    "DataUse.TabModel.OldInactiveSessionRemovalDuration";
+    "DataUsage.TabModel.OldInactiveSessionRemovalDuration";
 
 }  // namespace
 
