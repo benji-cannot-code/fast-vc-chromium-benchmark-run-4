df1e879b02e27dc9b931a07116f5a2e2d22bdeb0
jam <jam@chromium.org>
2016-01-23 15:28:35 -0800

    Remove WebKit XP from Blink tooling in prepartion from removing that builder.
    BUG=579196
    
    Review URL: https://codereview.chromium.org/1612623002
    
    Cr-Commit-Position: refs/heads/master@{#371156}

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/builders.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/builders.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (C) 2011 Google Inc. All rights reserved.
+﻿# Copyright (C) 2011 Google Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -42,7 +42,6 @@ from webkitpy.common.memoized import memoized
 #      TestExpectationsParser._configuration_tokens_list
 
 _exact_matches = {
-    "WebKit XP": {"port_name": "win-xp", "specifiers": ['XP', 'Release']},
     "WebKit Win7": {"port_name": "win-win7", "specifiers": ['Win7', 'Release']},
     "WebKit Win7 (dbg)": {"port_name": "win-win7", "specifiers": ['Win7', 'Debug']},
     "WebKit Win10": {"port_name": "win-win10", "specifiers": ['Win10', 'Release']},
