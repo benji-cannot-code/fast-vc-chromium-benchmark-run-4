cbb236dec673b0e169793f2a6bd2e501450d4e31
lawton <lawton@google.com>
2016-01-24 14:32:43 -0800

    CSS Typed OM: Implemented TransformValue
    Implemented is2D and toCSSValue methods
    
    Also made the class iterable over its TransformComponents in JavaScript
    
    Note: asMatrix method is still not implemented as of this cl
    
    Spec: https://drafts.css-houdini.org/css-typed-om/#transformvalue-objects
    BUG= 545318
    
    Review URL: https://codereview.chromium.org/1601013002
    
    Cr-Commit-Position: refs/heads/master@{#371173}

--- third_party/WebKit/Source/core/css/cssom/TransformValue.cpp
+++ third_party/WebKit/Source/core/css/cssom/TransformValue.cpp
@@ -5,24 +5,63 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/cssom/TransformValue.h"
 
+#include "core/css/CSSValueList.h"
+#include "core/css/cssom/TransformComponent.h"
+
 namespace blink {
+
+namespace {
+
+class TransformValueIterationSource final : public ValueIterable<TransformComponent*>::IterationSource {
+public:
+    explicit TransformValueIterationSource(TransformValue* transformValue)
+        : m_transformValue(transformValue)
+    {
+    }
+
+    bool next(ScriptState*, TransformComponent*& value, ExceptionState&) override
+    {
+        if (m_index >= m_transformValue->size()) {
+            return false;
+        }
+        value = m_transformValue->componentAtIndex(m_index);
+        return true;
+    }
+
+    DEFINE_INLINE_VIRTUAL_TRACE()
+    {
+        visitor->trace(m_transformValue);
+        ValueIterable<TransformComponent*>::IterationSource::trace(visitor);
+    }
+
+private:
+    const Member<TransformValue> m_transformValue;
+};
+
+} // namespace
+
+ValueIterable<TransformComponent*>::IterationSource* TransformValue::startIteration(ScriptState*, ExceptionState&)
+{
+    return new TransformValueIterationSource(this);
+}
 
 bool TransformValue::is2D() const
 {
-    // TODO: implement;
+    for (size_t i = 0; i < m_transformComponents.size(); i++) {
+        if (!m_transformComponents[i]->is2DComponent()) {
+            return false;
+        }
+    }
     return true;
-}
-
-String TransformValue::cssString() const
-{
-    // TODO: implement
-    return "cssString";
 }
 
 PassRefPtrWillBeRawPtr<CSSValue> TransformValue::toCSSValue() const
 {
-    // TODO: implement
-    return nullptr;
+    RefPtrWillBeRawPtr<CSSValueList> transformCSSValue = CSSValueList::createSpaceSeparated();
+    for (size_t i = 0; i < m_transformComponents.size(); i++) {
+        transformCSSValue->append(m_transformComponents[i]->toCSSValue());
+    }
+    return transformCSSValue;
 }
 
 } // namespace blink
