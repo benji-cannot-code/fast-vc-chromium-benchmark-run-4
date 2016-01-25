620a2372b7879e8f4c87b67a0a5c9a078c188054
ellyjones <ellyjones@chromium.org>
2016-01-25 04:10:16 -0800

    mac_views_browser: fix build
    A recent change (0c8d4aa8ba54f463cc22ce21bf1088edd3d1e207) excised scoped_ptr's
    Pass() function and replaced uses of it with std::move(), but this instance in
    MacViews-specific code was missed.
    
    Another recent change (42d476e6844deab2777720f6c1e3950a71c05bb0) added a
    parameter to ThemeProvider::GetDefaultColor, but missed MacViews-specific uses
    of it.
    
    TEST=build
    BUG=None
    
    Review URL: https://codereview.chromium.org/1607893004
    
    Cr-Commit-Position: refs/heads/master@{#371230}

--- chrome/browser/ui/views/infobars/legacy_infobars_mac.cc
+++ chrome/browser/ui/views/infobars/legacy_infobars_mac.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 scoped_ptr<infobars::InfoBar> CreateSavePasswordInfoBar(
     scoped_ptr<SavePasswordInfoBarDelegate> delegate) {
-  return make_scoped_ptr(new ConfirmInfoBar(delegate.Pass()));
+  return make_scoped_ptr(new ConfirmInfoBar(std::move(delegate)));
 }
 
 scoped_ptr<infobars::InfoBar> ChromeTranslateClient::CreateInfoBar(
