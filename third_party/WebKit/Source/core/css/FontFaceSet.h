7a10d9543b86def564705b7356ee03c189c3dede
sigbjornf <sigbjornf@opera.com>
2016-01-25 10:38:49 -0800

    Fix g++ builds by avoiding early HeapSupplement<Document> instantiation.
    Building ToT with g++ (component build) currently breaks when using
    the Oilpan type HeapSupplement<Document>:
    
     ...
     error: type attributes ignored after type is already defined [-Werror=attributes]
     ...
     .../dom/Document.h:179:51: note: in expansion of macro ‘WillBeHeapSupplement’
     extern template class CORE_EXTERN_TEMPLATE_EXPORT WillBeHeapSupplement<Document>;
    
    It appears that g++ cannot be kept happy if it implicitly instantiates
    a template at a type and then later sees an extern decl like the above
    with some extra attributes attached.
    
    Hence, bring the required types into scope for FontFaceSet's declaration
    to avoid that unfortunate situation.
    
    R=haraken,thakis
    BUG=
    
    Review URL: https://codereview.chromium.org/1634683002
    
    Cr-Commit-Position: refs/heads/master@{#371270}

--- third_party/WebKit/Source/core/css/FontFaceSet.h
+++ third_party/WebKit/Source/core/css/FontFaceSet.h
@@ -31,10 +31,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptPromise.h"
 #include "core/css/FontFace.h"
 #include "core/dom/ActiveDOMObject.h"
+#include "core/dom/Document.h"
 #include "core/events/EventListener.h"
 #include "core/events/EventTarget.h"
 #include "platform/AsyncMethodRunner.h"
 #include "platform/RefCountedSupplement.h"
+#include "platform/heap/Handle.h"
 #include "wtf/Allocator.h"
 #include "wtf/PassRefPtr.h"
 #include "wtf/RefCounted.h"
@@ -51,7 +53,6 @@ class CSSFontFace;
 class CSSFontFaceSource;
 class CSSFontSelector;
 class Dictionary;
-class Document;
 class ExceptionState;
 class Font;
 class FontFaceCache;
