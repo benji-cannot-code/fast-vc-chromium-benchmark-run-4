c03ca4fd5602611a000c117df7c3d878282700af
peletskyi <peletskyi@chromium.org>
2016-01-25 07:31:05 -0800

    Removed email-based affiliation determination in EPKPChallengeKeyBase class.
    BUG=533051
    
    Review URL: https://codereview.chromium.org/1585973002
    
    Cr-Commit-Position: refs/heads/master@{#371244}

--- chrome/browser/extensions/api/enterprise_platform_keys_private/enterprise_platform_keys_private_api.cc
+++ chrome/browser/extensions/api/enterprise_platform_keys_private/enterprise_platform_keys_private_api.cc
@@ -31,7 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/settings/cros_settings_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
+#include "components/signin/core/account_id/account_id.h"
 #include "components/signin/core/browser/signin_manager.h"
+#include "components/user_manager/user.h"
+#include "components/user_manager/user_manager.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
@@ -141,10 +144,19 @@ bool EPKPChallengeKeyBase::IsExtensionWhitelisted() const {
 bool EPKPChallengeKeyBase::IsUserManaged() const {
   std::string email = GetUserEmail();
 
-  // TODO(davidyu): Use BrowserPolicyConnector::GetUserAffiliation() and fix
-  // the test.
-  return email.empty() ? false :
-      gaia::ExtractDomainName(email) == GetEnterpriseDomain();
+  if (email.empty()) {
+    return false;
+  }
+
+  const user_manager::User* const user =
+      user_manager::UserManager::Get()->FindUser(
+          AccountId::FromUserEmail(email));
+
+  if (user) {
+    return user->is_affiliated();
+  }
+
+  return false;
 }
 
 std::string EPKPChallengeKeyBase::GetEnterpriseDomain() const {
