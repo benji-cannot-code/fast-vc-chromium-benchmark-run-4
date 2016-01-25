e27b6d8410f74c8c837fd3899d306b9c98083ff0
alancutter <alancutter@chromium.org>
2016-01-24 20:58:56 -0800

    Code clean up: Make InterpolationEffect and InterpolationRecord non-RefCounted
    InterpolationEffects and InterpolationRecords are never shared or
    transferred ownership. Their RefCounted base and usage can be removed.
    Both aggregate types can be treated as inline members instead of being
    separately allocated on the heap.
    
    This patch introduces no change in behaviour.
    
    Review URL: https://codereview.chromium.org/1612153004
    
    Cr-Commit-Position: refs/heads/master@{#371186}

--- third_party/WebKit/Source/core/animation/KeyframeEffectModel.cpp
+++ third_party/WebKit/Source/core/animation/KeyframeEffectModel.cpp
@@ -59,7 +59,7 @@ void KeyframeEffectModelBase::setFrames(KeyframeVector& keyframes)
     // TODO(samli): Should also notify/invalidate the animation
     m_keyframes = keyframes;
     m_keyframeGroups = nullptr;
-    m_interpolationEffect = nullptr;
+    m_interpolationEffect.clear();
     m_lastFraction = std::numeric_limits<double>::quiet_NaN();
 }
 
@@ -68,13 +68,13 @@ bool KeyframeEffectModelBase::sample(int iteration, double fraction, double iter
     ASSERT(iteration >= 0);
     ASSERT(!isNull(fraction));
     ensureKeyframeGroups();
-    ensureInterpolationEffect();
+    ensureInterpolationEffectPopulated();
 
     bool changed = iteration != m_lastIteration || fraction != m_lastFraction || iterationDuration != m_lastIterationDuration;
     m_lastIteration = iteration;
     m_lastFraction = fraction;
     m_lastIterationDuration = iterationDuration;
-    m_interpolationEffect->getActiveInterpolations(fraction, iterationDuration, result);
+    m_interpolationEffect.getActiveInterpolations(fraction, iterationDuration, result);
     return changed;
 }
 
@@ -82,7 +82,7 @@ void KeyframeEffectModelBase::forceConversionsToAnimatableValues(Element& elemen
 {
     ensureKeyframeGroups();
     snapshotAllCompositorKeyframes(element, baseStyle);
-    ensureInterpolationEffect(&element, baseStyle);
+    ensureInterpolationEffectPopulated(&element, baseStyle);
 }
 
 bool KeyframeEffectModelBase::snapshotNeutralCompositorKeyframes(Element& element, const ComputedStyle& oldStyle, const ComputedStyle& newStyle)
@@ -199,11 +199,10 @@ void KeyframeEffectModelBase::ensureKeyframeGroups() const
     }
 }
 
-void KeyframeEffectModelBase::ensureInterpolationEffect(Element* element, const ComputedStyle* baseStyle) const
+void KeyframeEffectModelBase::ensureInterpolationEffectPopulated(Element* element, const ComputedStyle* baseStyle) const
 {
-    if (m_interpolationEffect)
+    if (m_interpolationEffect.isPopulated())
         return;
-    m_interpolationEffect = InterpolationEffect::create();
 
     for (const auto& entry : *m_keyframeGroups) {
         const PropertySpecificKeyframeVector& keyframes = entry.value->keyframes();
@@ -213,9 +212,11 @@ void KeyframeEffectModelBase::ensureInterpolationEffect(Element* element, const 
             if (applyTo == 1)
                 applyTo = std::numeric_limits<double>::infinity();
 
-            m_interpolationEffect->addInterpolationsFromKeyframes(entry.key, element, baseStyle, *keyframes[i], *keyframes[i + 1], applyFrom, applyTo);
+            m_interpolationEffect.addInterpolationsFromKeyframes(entry.key, element, baseStyle, *keyframes[i], *keyframes[i + 1], applyFrom, applyTo);
         }
     }
+
+    m_interpolationEffect.setPopulated();
 }
 
 bool KeyframeEffectModelBase::isReplaceOnly()
