0596046f651cc0c06544b545930747bf90a79abe
oshima <oshima@chromium.org>
2016-01-25 14:11:47 -0800

    Revert of Enable handle verifier for tests and add some tests. (patchset #7 id:120001 of https://codereview.chromium.org/1580873003/ )
    Reason for revert:
    A lot of test are crashing on DrMemory x64 bot, so I'm reverting this speculatively. I'll reland this if it didn't help.
    
    Original issue's description:
    > Enable handle verifier for tests and add some tests.
    >
    > BUG=571304
    >
    > Committed: https://crrev.com/ff6cc3d794e1285c68f74ec9ae6d59dc3e523d5b
    > Cr-Commit-Position: refs/heads/master@{#371075}
    
    TBR=cpu@chromium.org,wfh@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=571304,580795
    
    Review URL: https://codereview.chromium.org/1633623003
    
    Cr-Commit-Position: refs/heads/master@{#371330}

--- base/test/test_suite.cc
+++ base/test/test_suite.cc
@@ -41,10 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // OS_IOS
 #endif  // OS_MACOSX
 
-#if defined(OS_WIN)
-#include "base/debug/close_handle_hook_win.h"
-#include "base/win/windows_version.h"
-#else
+#if !defined(OS_WIN)
 #include "base/i18n/rtl.h"
 #if !defined(OS_IOS)
 #include "base/strings/string_util.h"
@@ -321,14 +318,6 @@ void TestSuite::Initialize() {
 
   CHECK(debug::EnableInProcessStackDumping());
 #if defined(OS_WIN)
-#if defined(_DEBUG)
-  // Handle hooks cause shutdown asserts in Debug on Windows 7. crbug.com/571304
-  if (base::win::GetVersion() >= base::win::VERSION_WIN8)
-    base::debug::InstallHandleHooks();
-#else
-  base::debug::InstallHandleHooks();
-#endif
-
   RouteStdioToConsole(true);
   // Make sure we run with high resolution timer to minimize differences
   // between production code and test code.
