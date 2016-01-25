febefd90e07e4fba5adea2fc8624011640c8480d
alancutter <alancutter@chromium.org>
2016-01-24 21:29:28 -0800

    Remove unused class VisibilityStyleInterpolation
    VisibilityStyleInterpolation has been superseded by
    CSSVisibilityInterpolationType.
    
    Review URL: https://codereview.chromium.org/1628083002
    
    Cr-Commit-Position: refs/heads/master@{#371190}

--- third_party/WebKit/Source/core/animation/StringKeyframe.cpp
+++ third_party/WebKit/Source/core/animation/StringKeyframe.cpp
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/animation/SVGRectInterpolationType.h"
 #include "core/animation/SVGTransformListInterpolationType.h"
 #include "core/animation/SVGValueInterpolationType.h"
-#include "core/animation/VisibilityStyleInterpolation.h"
 #include "core/animation/css/CSSAnimations.h"
 #include "core/css/CSSPropertyMetadata.h"
 #include "core/css/resolver/StyleResolver.h"
@@ -494,11 +493,6 @@ PassRefPtr<Interpolation> StringKeyframe::CSSPropertySpecificKeyframe::maybeCrea
             return interpolation.release();
         break;
     }
-    case CSSPropertyVisibility:
-        if (VisibilityStyleInterpolation::canCreateFrom(*fromCSSValue) && VisibilityStyleInterpolation::canCreateFrom(*toCSSValue) && (VisibilityStyleInterpolation::isVisible(*fromCSSValue) || VisibilityStyleInterpolation::isVisible(*toCSSValue)))
-            return VisibilityStyleInterpolation::create(*fromCSSValue, *toCSSValue, property);
-
-        break;
 
     case CSSPropertyBorderBottomLeftRadius:
     case CSSPropertyBorderBottomRightRadius:
