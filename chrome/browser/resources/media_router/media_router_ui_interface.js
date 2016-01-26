2063999d5f45dfba18516d1dea43a8411bee6791
apacible <apacible@chromium.org>
2016-01-25 20:40:54 -0800

    [Media Router] Add cloud services preferences.
    Part 6.
    
    This change adds two syncable preferences related to using cloud services with Media Router.
    
    kMediaRouterCloudServicesPrefSet tracks whether or not the user has explicitly acknowledged the first run flow with the cloud services preference present. This is important to keep track of since there are users who have acknowledged the first run flow before the work related to the cloud services lands (currently in progress). This will be used to surface a different prompt to ask if users want to enable cloud services.
    
    kMediaRouterEnableCloudServices tracks whether the user has enabled cloud services.
    
    BUG=560457
    
    Review URL: https://codereview.chromium.org/1582943003
    
    Cr-Commit-Position: refs/heads/master@{#371456}

--- chrome/browser/resources/media_router/media_router_ui_interface.js
+++ chrome/browser/resources/media_router/media_router_ui_interface.js
@@ -56,7 +56,8 @@ cr.define('media_router.ui', function() {
    *          sinks: !Array<!media_router.Sink>,
    *          routes: !Array<!media_router.Route>,
    *          castModes: !Array<!media_router.CastMode>,
-   *          wasFirstRunFlowAcknowledged: boolean}} data
+   *          wasFirstRunFlowAcknowledged: boolean,
+   *          showFirstRunFlowCloudPref: boolean}} data
    * Parameters in data:
    *   firstRunFlowCloudPrefLearnMoreUrl - url to open when the cloud services
    *       pref learn more link is clicked.
@@ -66,6 +67,8 @@ cr.define('media_router.ui', function() {
    *   castModes - list of available cast modes.
    *   wasFirstRunFlowAcknowledged - true if first run flow was previously
    *       acknowledged by user.
+   *   showFirstRunFlowCloudPref - true if the cloud pref option should be
+   *       shown.
    */
   function setInitialData(data) {
     container.firstRunFlowCloudPrefLearnMoreUrl =
@@ -75,6 +78,8 @@ cr.define('media_router.ui', function() {
     container.allSinks = data['sinks'];
     container.routeList = data['routes'];
     container.showFirstRunFlow = !data['wasFirstRunFlowAcknowledged'];
+    container.showFirstRunFlowCloudPref =
+        data['showFirstRunFlowCloudPref'];
     container.maybeShowRouteDetailsOnOpen();
     media_router.browserApi.onInitialDataReceived();
   }
@@ -135,9 +140,12 @@ cr.define('media_router.browserApi', function() {
 
   /**
    * Indicates that the user has acknowledged the first run flow.
+   *
+   * @param {boolean} optedIntoCloudServices Whether or not the user opted into
+   *                  cloud services.
    */
-  function acknowledgeFirstRunFlow() {
-    chrome.send('acknowledgeFirstRunFlow');
+  function acknowledgeFirstRunFlow(optedIntoCloudServices) {
+    chrome.send('acknowledgeFirstRunFlow', [optedIntoCloudServices]);
   }
 
   /**
