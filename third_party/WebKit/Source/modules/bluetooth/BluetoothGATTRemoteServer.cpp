0ce32d598995d3ed8e855c894a4f8f73e4d8e7e5
ortuno <ortuno@chromium.org>
2016-01-26 10:16:23 -0800

    bluetooth: Disconnect if the page becomes hidden or is closed.
    This means closing connection when:
    1. BluetoothGATTRemoteServer is destroyed.
    2. The parent document is detached.
    3. The page becomes hidden.
    
    BUG=579746
    
    Review URL: https://codereview.chromium.org/1611773002
    
    Cr-Commit-Position: refs/heads/master@{#371542}

--- third_party/WebKit/Source/modules/bluetooth/BluetoothGATTRemoteServer.cpp
+++ third_party/WebKit/Source/modules/bluetooth/BluetoothGATTRemoteServer.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptPromise.h"
 #include "bindings/core/v8/ScriptPromiseResolver.h"
 #include "core/dom/DOMException.h"
+#include "core/dom/Document.h"
 #include "core/dom/ExceptionCode.h"
 #include "modules/bluetooth/BluetoothError.h"
 #include "modules/bluetooth/BluetoothGATTService.h"
@@ -19,15 +20,56 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-BluetoothGATTRemoteServer::BluetoothGATTRemoteServer(PassOwnPtr<WebBluetoothGATTRemoteServer> webGATT)
-    : m_webGATT(webGATT)
+BluetoothGATTRemoteServer::BluetoothGATTRemoteServer(ExecutionContext* context, PassOwnPtr<WebBluetoothGATTRemoteServer> webGATT)
+    : ActiveDOMObject(context)
+    , PageLifecycleObserver(toDocument(context)->page())
+    , m_webGATT(webGATT)
 {
+    // See example in Source/platform/heap/ThreadState.h
+    ThreadState::current()->registerPreFinalizer(this);
 }
 
-BluetoothGATTRemoteServer* BluetoothGATTRemoteServer::take(ScriptPromiseResolver*, PassOwnPtr<WebBluetoothGATTRemoteServer> webGATT)
+BluetoothGATTRemoteServer* BluetoothGATTRemoteServer::take(ScriptPromiseResolver* resolver, PassOwnPtr<WebBluetoothGATTRemoteServer> webGATT)
 {
     ASSERT(webGATT);
-    return new BluetoothGATTRemoteServer(webGATT);
+    BluetoothGATTRemoteServer* server = new BluetoothGATTRemoteServer(resolver->executionContext(), webGATT);
+    if (!server->page()->isPageVisible()) {
+        server->disconnectIfConnected();
+    }
+    server->suspendIfNeeded();
+    return server;
+}
+
+void BluetoothGATTRemoteServer::dispose()
+{
+    disconnectIfConnected();
+}
+
+void BluetoothGATTRemoteServer::stop()
+{
+    disconnectIfConnected();
+}
+
+void BluetoothGATTRemoteServer::pageVisibilityChanged()
+{
+    if (!page()->isPageVisible()) {
+        disconnectIfConnected();
+    }
+}
+
+void BluetoothGATTRemoteServer::disconnectIfConnected()
+{
+    if (m_webGATT->connected) {
+        m_webGATT->connected = false;
+        WebBluetooth* webbluetooth = BluetoothSupplement::fromExecutionContext(executionContext());
+        webbluetooth->disconnect(m_webGATT->deviceId);
+    }
+}
+
+DEFINE_TRACE(BluetoothGATTRemoteServer)
+{
+    ActiveDOMObject::trace(visitor);
+    PageLifecycleObserver::trace(visitor);
 }
 
 void BluetoothGATTRemoteServer::disconnect(ScriptState* scriptState)
