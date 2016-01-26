ce5d3fab8e078df01f1fc09cf735d5cc2b77d66d
primiano <primiano@chromium.org>
2016-01-25 16:00:23 -0800

    Allocator cleanup: move tcmalloc-specific calls to base
    After crrev.com/1584893002 base/ can use directly allocator.
    The grand plan is to dismantle all the indirection layer such that:
     - base is the only thing that knows about allocator-specific details.
     - non-base code uses only base abstractions and stops looking
       at USE_TCMALLOC build flags.
    
    This CL specifically deals with tc_malloc_skip_handler and tc_set_new_mode
    and removes the unnecessary base -> content -> base hops.
    
    BUG=564618
    
    Review URL: https://codereview.chromium.org/1610153002
    
    Cr-Commit-Position: refs/heads/master@{#371355}

--- content/app/content_main_runner.cc
+++ content/app/content_main_runner.cc
@@ -116,12 +116,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/nss_util.h"
 #endif
 
-#if !defined(OS_MACOSX) && defined(USE_TCMALLOC)
-extern "C" {
-int tc_set_new_mode(int mode);
-}
-#endif
-
 namespace content {
 extern int GpuMain(const content::MainFunctionParams&);
 #if defined(ENABLE_PLUGINS)
@@ -150,10 +144,6 @@ base::LazyInstance<ContentRendererClient>
 base::LazyInstance<ContentUtilityClient>
     g_empty_content_utility_client = LAZY_INSTANCE_INITIALIZER;
 #endif  // !OS_IOS && !CHROME_MULTIPLE_DLL_BROWSER
-
-#if defined(OS_WIN)
-
-#endif  // defined(OS_WIN)
 
 #if defined(OS_POSIX) && !defined(OS_IOS)
 
@@ -436,16 +426,7 @@ class ContentMainRunnerImpl : public ContentMainRunner {
     TRACE_EVENT0("startup,benchmark", "ContentMainRunnerImpl::Initialize");
 #endif  // OS_ANDROID
 
-    // NOTE(willchan): One might ask why these TCMalloc-related calls are done
-    // here rather than in process_util_linux.cc with the definition of
-    // EnableTerminationOnOutOfMemory().  That's because base shouldn't have a
-    // dependency on TCMalloc.  Really, we ought to have our allocator shim code
-    // implement this EnableTerminationOnOutOfMemory() function.  Whateverz.
-    // This works for now.
 #if !defined(OS_MACOSX) && defined(USE_TCMALLOC)
-    // For tcmalloc, we need to tell it to behave like new.
-    tc_set_new_mode(1);
-
     // Provide optional hook for monitoring allocation quantities on a
     // per-thread basis.  Only set the hook if the environment indicates this
     // needs to be enabled.
