2256a5c220321848386ca8a1d67790d094e53826
kozyatinskiy <kozyatinskiy@chromium.org>
2016-01-25 17:38:28 -0800

    [DevTools] Fix injected_script_externs.js
    Method evaluateWithExceptionDetails was changed in https://codereview.chromium.org/1617393002/
    
    TBR=dgozman@chromium.org
    
    Review URL: https://codereview.chromium.org/1631113002
    
    Cr-Commit-Position: refs/heads/master@{#371407}

--- third_party/WebKit/Source/core/inspector/injected_script_externs.js
+++ third_party/WebKit/Source/core/inspector/injected_script_externs.js
@@ -92,9 +92,10 @@ InjectedScriptHostClass.prototype.eval = function(text) {}
 
 /**
  * @param {string} text
+ * @param {!Object=} commandLineAPI
  * @return {*}
  */
-InjectedScriptHostClass.prototype.evaluateWithExceptionDetails = function(text) {}
+InjectedScriptHostClass.prototype.evaluateWithExceptionDetails = function(text, commandLineAPI) {}
 
 /**
  * @param {*} fn
