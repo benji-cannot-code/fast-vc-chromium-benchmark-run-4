66cf26a62b97497ad4486a63099583b5f516ceaa
hartmanng <hartmanng@chromium.org>
2016-01-26 14:42:53 -0800

    Measure time spent in SharedPrefs-loading StrictMode violations.
    BUG=580262,562189
    
    Committed: https://crrev.com/5db5d9e5815c64ba4bc93fda27115c542df35316
    Cr-Commit-Position: refs/heads/master@{#371573}
    
    Review URL: https://codereview.chromium.org/1618973002
    
    Cr-Commit-Position: refs/heads/master@{#371623}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/document/DocumentTabModelImplTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/document/DocumentTabModelImplTest.java
@@ -11,6 +11,7 @@ import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.base.CommandLine;
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.AdvancedMockContext;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModel;
@@ -83,6 +84,7 @@ public class DocumentTabModelImplTest extends NativeLibraryTestBase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        RecordHistogram.disableForTests();
         CommandLine.init(null);
         loadNativeLibraryAndInitBrowserProcess();
 
