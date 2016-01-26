d004b093959f8a2d65506e67c06db19a36dfac8a
mmoroz <mmoroz@chromium.org>
2016-01-25 16:26:35 -0800

    [libfuzzer] fuzzer for uregex_open from third_party/icu implemented
    R=aizatsky@chromium.org, inferno@chromium.org, kcc@chromium.org, krasin@chromium.org
    BUG=539572
    
    Review URL: https://codereview.chromium.org/1636823002
    
    Cr-Commit-Position: refs/heads/master@{#371375}

---
+++ testing/libfuzzer/fuzzers/icu_uregex_open_fuzzer.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdint.h>
+
+#include "third_party/icu/source/i18n/unicode/regex.h"
+
+// Entry point for LibFuzzer.
+extern "C" int LLVMFuzzerTestOneInput(const unsigned char* data, size_t size) {
+  UParseError pe = { 0 };
+  UErrorCode status = U_ZERO_ERROR;
+  URegularExpression* re = uregex_open(reinterpret_cast<const UChar*>(data),
+                                       size / sizeof(UChar),
+                                       0, &pe, &status);
+  if (re)
+    uregex_close(re);
+
+  return 0;
+}
