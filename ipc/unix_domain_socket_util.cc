312e37efc732fafdf4504071a3416375a234a271
tfarina <tfarina@chromium.org>
2016-01-25 16:16:38 -0800

    ipc: remove unused variable from CreateClientUnixDomainSocket() function
    |socket_dir| variable is not unused for anything in
    CreateClientUnixDomainSocket() function, so this patch removes it. While
    at it also pass socket_path.value() directly instead of storing it in a
    temporary variable.
    
    BUG=None
    TEST=ipc_tests
    R=cpu@chromium.org
    
    Review URL: https://codereview.chromium.org/1636583002
    
    Cr-Commit-Position: refs/heads/master@{#371369}

--- ipc/unix_domain_socket_util.cc
+++ ipc/unix_domain_socket_util.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/unix_domain_socket_util.h"
 
 #include <errno.h>
-#include <stddef.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/un.h>
@@ -119,13 +118,10 @@ bool CreateClientUnixDomainSocket(const base::FilePath& socket_path,
                                   int* client_socket) {
   DCHECK(client_socket);
 
-  std::string socket_name = socket_path.value();
-  base::FilePath socket_dir = socket_path.DirName();
-
   struct sockaddr_un unix_addr;
   size_t unix_addr_len;
   base::ScopedFD fd(
-      MakeUnixAddrForPath(socket_name, &unix_addr, &unix_addr_len));
+      MakeUnixAddrForPath(socket_path.value(), &unix_addr, &unix_addr_len));
   if (!fd.is_valid())
     return false;
 
--- ipc/unix_domain_socket_util.h
+++ ipc/unix_domain_socket_util.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <sys/types.h>
 
-#include <string>
-
 #include "ipc/ipc_export.h"
 
 namespace base {
