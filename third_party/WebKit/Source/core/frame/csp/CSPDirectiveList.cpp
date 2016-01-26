7eb563a2faf08b1db8671ad464c434e889cfce2d
jww <jww@chromium.org>
2016-01-25 16:37:14 -0800

    Fix hyphen disallowed in Suborigin names
    According to the proposed spec, Suborigin names should allow
    alphanumeric characters and hyphens. This fixes a bug where hyphens were
    disallowed, and it adds tests to verify the behavior.
    
    BUG=568755
    R=mkwst@chromium.org
    
    Review URL: https://codereview.chromium.org/1620393002
    
    Cr-Commit-Position: refs/heads/master@{#371381}

--- third_party/WebKit/Source/core/frame/csp/CSPDirectiveList.cpp
+++ third_party/WebKit/Source/core/frame/csp/CSPDirectiveList.cpp
@@ -35,6 +35,11 @@ String getSha256String(const String& content)
     return "sha256-" + base64Encode(reinterpret_cast<char*>(digest.data()), digest.size(), Base64DoNotInsertLFs);
 }
 
+template<typename CharType> inline bool isASCIIAlphanumericOrHyphen(CharType c)
+{
+    return isASCIIAlphanumeric(c) || c == '-';
+}
+
 } // namespace
 
 CSPDirectiveList::CSPDirectiveList(ContentSecurityPolicy* policy, ContentSecurityPolicyHeaderType type, ContentSecurityPolicyHeaderSource source)
@@ -720,7 +725,7 @@ String CSPDirectiveList::parseSuboriginName(const String& policy)
 
     const UChar* begin = position;
 
-    skipWhile<UChar, isASCIIAlphanumeric>(position, end);
+    skipWhile<UChar, isASCIIAlphanumericOrHyphen>(position, end);
     if (position != end && !isASCIISpace(*position)) {
         m_policy->reportInvalidSuboriginFlags("Invalid character \'" + String(position, 1) + "\' in suborigin.");
         return String();
