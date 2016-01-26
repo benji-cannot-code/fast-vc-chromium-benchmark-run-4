34825a2773faee8d96b8854871b5a186bc3c6b77
wangxianzhu <wangxianzhu@chromium.org>
2016-01-25 16:54:19 -0800

    Improve positioned/percentHeight descendant/container tracking
    - Add LayoutBoxRareData::m_percentHeightContainer to track percent height
      containers.
      (rare rate: 1.52% based on top 10k sites -- Run 402
       https://ct.skia.org/chromium_perf_runs/ )
    - Still use global maps to track percent height descendants, positioned
      descendants and positioned containers, but add bit field to indicate if
      descendants set exists for a LayoutBlock to avoid hash lookups if the
      block doesn't have percent-height or positioned descendants.
    - As a positioned box has only one positioned container at a time, positioned
      container map is simplified to HashMap<LayoutBox*, LayoutBlock*> instead of
      HashMap<LayoutBox*, HashSet<LayoutBlock*>>. Also modify code to handle
      previously (unnecessarily) multiple containers situation.
    
    This will improve blink_perf.layout tests performance by 0%~20%:
    http://storage.googleapis.com/chromium-telemetry/html-results/results-2016-01-10_00-31-59
    http://storage.googleapis.com/chromium-telemetry/html-results/results-2016-01-10_11-52-52
    
    More discussions: https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/xWMmQfD_X0A
    
    Review URL: https://codereview.chromium.org/1544423002
    
    Cr-Commit-Position: refs/heads/master@{#371388}

--- third_party/WebKit/Source/core/layout/LayoutBox.h
+++ third_party/WebKit/Source/core/layout/LayoutBox.h
@@ -58,6 +58,7 @@ public:
         , m_overrideLogicalContentHeight(-1)
         , m_overrideLogicalContentWidth(-1)
         , m_previousBorderBoxSize(-1, -1)
+        , m_percentHeightContainer(nullptr)
     {
     }
 
@@ -75,6 +76,8 @@ public:
     LayoutUnit m_pageLogicalOffset;
 
     LayoutUnit m_paginationStrut;
+
+    LayoutBlock* m_percentHeightContainer;
 };
 
 // LayoutBox implements the full CSS box model.
@@ -868,6 +871,11 @@ public:
     void mapAbsoluteToLocalPoint(MapCoordinatesFlags, TransformState&) const override;
 
     void clearPreviousPaintInvalidationRects() override;
+
+    LayoutBlock* percentHeightContainer() const { return m_rareData ? m_rareData->m_percentHeightContainer : nullptr; }
+    void setPercentHeightContainer(LayoutBlock*);
+    void removeFromPercentHeightContainer();
+    void clearPercentHeightDescendants();
 
 protected:
     void willBeDestroyed() override;
