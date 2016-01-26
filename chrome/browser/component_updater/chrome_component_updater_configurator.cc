5453df193b43c45a10e75c940c2dcc9abfe74f66
j.isorce <j.isorce@samsung.com>
2016-01-26 01:08:51 -0800

    Fix build error with gcc 4.8.2 about WARN_UNUSED_RESULT
    For static methods it does not seem to work properly with older gcc.
    Indeed the compiler consider the result as not used in this example:
    
    sequence_token_ = BrowserThread::GetBlockingPool()->GetSequenceToken()
    
    "error: ignoring return value of 'static base::SequencedWorkerPool*
    content::BrowserThread::GetBlockingPool()', declared with attribute
    warn_unused_result [-Werror=unused-result]"
    
    The solution is just to call GetSequenceToken() directly as:
    base::SequencedWorkerPool::GetSequenceToken() as it is static.
    
    BUG=579461
    
    R=cpu@chromium.org, nico@chromium.org
    
    TEST= GYP_DEFINES="clang=0 use_sysroot=0"
    
    Review URL: https://codereview.chromium.org/1610103002
    
    Cr-Commit-Position: refs/heads/master@{#371479}

--- chrome/browser/component_updater/chrome_component_updater_configurator.cc
+++ chrome/browser/component_updater/chrome_component_updater_configurator.cc
@@ -132,7 +132,7 @@ scoped_refptr<base::SequencedTaskRunner>
 ChromeConfigurator::GetSequencedTaskRunner() const {
   return content::BrowserThread::GetBlockingPool()
       ->GetSequencedTaskRunnerWithShutdownBehavior(
-          content::BrowserThread::GetBlockingPool()->GetSequenceToken(),
+          base::SequencedWorkerPool::GetSequenceToken(),
           base::SequencedWorkerPool::CONTINUE_ON_SHUTDOWN);
 }
 
