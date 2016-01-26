09d270c09ea8c8a9acd17b1c925e9e4e471cc94f
hartmanng <hartmanng@chromium.org>
2016-01-26 12:05:30 -0800

    Measure time spent in SharedPrefs-loading StrictMode violations.
    BUG=580262,562189
    
    Review URL: https://codereview.chromium.org/1618973002
    
    Cr-Commit-Position: refs/heads/master@{#371573}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/ActivityAssignerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/ActivityAssignerTest.java
@@ -9,6 +9,7 @@ import android.test.InstrumentationTestCase;
 import android.test.UiThreadTest;
 import android.test.suitebuilder.annotation.SmallTest;
 
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.AdvancedMockContext;
 import org.chromium.base.test.util.Feature;
 
@@ -29,6 +30,7 @@ public class ActivityAssignerTest extends InstrumentationTestCase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        RecordHistogram.disableForTests();
         mContext = new AdvancedMockContext();
         mPreferences = new HashMap<String, Object>();
         mContext.addSharedPreferences(ActivityAssigner.PREF_PACKAGE, mPreferences);
