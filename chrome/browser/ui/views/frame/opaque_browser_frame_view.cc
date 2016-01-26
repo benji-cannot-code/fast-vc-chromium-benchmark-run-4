e112c69a7f75324dff9efc165f4c9943cb76d231
pkasting <pkasting@chromium.org>
2016-01-25 18:51:11 -0800

    Clean up frame code a bit:
    * Make more functions const
    * Tweak version check to "< Win 10" to match code constant names and the pattern of how we check this elsewhere in the UI code
    * Rename some things from "avatar" to "incognito icon"
    * Remove unnecessary member
    * Replace local constants with layout constants (only in cases where the layout constants have the same values in non-MD)
    * Simplify.  In a few cases this may look like a behavior change but isn't; e.g. "NonClientTopBorderHeight(false) + kTabstripTopShadowThickness" was replaced by "FrameBorderThickness(false)" at a place where the two are guaranteed to be the same value
    * Prepare for further tabstrip/incognito icon positioning fixes to OpaqueBrowserFrameViewLayout::LayoutIncognitoIcon()
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1624773002
    
    Cr-Commit-Position: refs/heads/master@{#371432}

--- chrome/browser/ui/views/frame/opaque_browser_frame_view.cc
+++ chrome/browser/ui/views/frame/opaque_browser_frame_view.cc
@@ -575,7 +575,8 @@ bool OpaqueBrowserFrameView::ShouldShowWindowTitleBar() const {
       IsMaximized());
 }
 
-void OpaqueBrowserFrameView::PaintRestoredFrameBorder(gfx::Canvas* canvas) {
+void OpaqueBrowserFrameView::PaintRestoredFrameBorder(
+    gfx::Canvas* canvas) const {
   frame_background_->set_frame_color(GetFrameColor());
   frame_background_->set_theme_image(GetFrameImage());
   frame_background_->set_theme_overlay_image(GetFrameOverlayImage());
@@ -600,7 +601,8 @@ void OpaqueBrowserFrameView::PaintRestoredFrameBorder(gfx::Canvas* canvas) {
   // all this in PaintRestoredClientEdge().
 }
 
-void OpaqueBrowserFrameView::PaintMaximizedFrameBorder(gfx::Canvas* canvas) {
+void OpaqueBrowserFrameView::PaintMaximizedFrameBorder(
+    gfx::Canvas* canvas) const {
   frame_background_->set_frame_color(GetFrameColor());
   frame_background_->set_theme_image(GetFrameImage());
   frame_background_->set_theme_overlay_image(GetFrameOverlayImage());
@@ -610,7 +612,7 @@ void OpaqueBrowserFrameView::PaintMaximizedFrameBorder(gfx::Canvas* canvas) {
   frame_background_->PaintMaximized(canvas, this);
 }
 
-void OpaqueBrowserFrameView::PaintToolbarBackground(gfx::Canvas* canvas) {
+void OpaqueBrowserFrameView::PaintToolbarBackground(gfx::Canvas* canvas) const {
   gfx::Rect toolbar_bounds(browser_view()->GetToolbarBounds());
   if (toolbar_bounds.IsEmpty())
     return;
@@ -715,7 +717,7 @@ void OpaqueBrowserFrameView::PaintToolbarBackground(gfx::Canvas* canvas) {
   }
 }
 
-void OpaqueBrowserFrameView::PaintClientEdge(gfx::Canvas* canvas) {
+void OpaqueBrowserFrameView::PaintClientEdge(gfx::Canvas* canvas) const {
   gfx::Rect client_bounds =
       layout_->CalculateClientAreaBounds(width(), height());
   const int x = client_bounds.x();
