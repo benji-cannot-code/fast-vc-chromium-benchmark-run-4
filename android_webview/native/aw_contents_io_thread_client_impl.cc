4fb95bd5e6d45a2469f79b87c06b1ed2c59a881a
dcheng <dcheng@chromium.org>
2016-01-27 09:48:04 -0800

    Remove unused header in aw_contents_io_thread_client_impl.cc
    BUG=none
    
    Review URL: https://codereview.chromium.org/1641533003
    
    Cr-Commit-Position: refs/heads/master@{#371817}

--- android_webview/native/aw_contents_io_thread_client_impl.cc
+++ android_webview/native/aw_contents_io_thread_client_impl.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/android/jni_weak_ref.h"
 #include "base/lazy_instance.h"
-#include "base/memory/linked_ptr.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/synchronization/lock.h"
 #include "content/public/browser/browser_thread.h"
