870594dbe762f83a9b02e90b667765af5fa7c70a
kmarshall <kmarshall@chromium.org>
2016-01-26 17:50:56 -0800

    Restructure contents of blimp/client.
    Restructure contents of blimp/client folder. See crbug.com/581506
    for more information on the eventual directory structure.
    Modify paths and ifdef guards as needed.
    Resolve various linter errors encountered along the way.
    Add TODOs for missing documentation.
    
    R=dtrainor@chromium.org,haibinlu@chromium.org
    BUG=581506
    
    Review URL: https://codereview.chromium.org/1636163002
    
    Cr-Commit-Position: refs/heads/master@{#371684}

---
+++ blimp/client/app/android/blimp_jni_registrar.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "blimp/client/app/android/blimp_jni_registrar.h"
+
+#include "base/android/jni_registrar.h"
+#include "blimp/client/app/android/blimp_client_session_android.h"
+#include "blimp/client/app/android/blimp_library_loader.h"
+#include "blimp/client/app/android/blimp_view.h"
+#include "blimp/client/app/android/tab_control_feature_android.h"
+#include "blimp/client/app/android/toolbar.h"
+
+namespace blimp {
+namespace client {
+namespace {
+
+base::android::RegistrationMethod kBlimpRegistrationMethods[] = {
+    {"BlimpLibraryLoader", RegisterBlimpLibraryLoaderJni},
+    {"Toolbar", Toolbar::RegisterJni},
+    {"BlimpView", BlimpView::RegisterJni},
+    {"BlimpClientSessionAndroid", BlimpClientSessionAndroid::RegisterJni},
+    {"TabControlFeatureAndroid", TabControlFeatureAndroid::RegisterJni},
+};
+
+}  // namespace
+
+bool RegisterBlimpJni(JNIEnv* env) {
+  return base::android::RegisterNativeMethods(
+      env, kBlimpRegistrationMethods, arraysize(kBlimpRegistrationMethods));
+}
+
+}  // namespace client
+}  // namespace blimp
--- blimp/client/session/blimp_client_session.cc
+++ blimp/client/session/blimp_client_session.cc
@@ -5,14 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "blimp/client/session/blimp_client_session.h"
 
+#include <string>
+#include <vector>
+
 #include "base/command_line.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/threading/sequenced_task_runner_handle.h"
-#include "blimp/client/blimp_client_switches.h"
-#include "blimp/client/session/navigation_feature.h"
-#include "blimp/client/session/render_widget_feature.h"
-#include "blimp/client/session/tab_control_feature.h"
+#include "blimp/client/app/blimp_client_switches.h"
+#include "blimp/client/feature/navigation_feature.h"
+#include "blimp/client/feature/render_widget_feature.h"
+#include "blimp/client/feature/tab_control_feature.h"
 #include "blimp/net/blimp_message_processor.h"
 #include "blimp/net/blimp_message_thread_pipe.h"
 #include "blimp/net/browser_connection_handler.h"
