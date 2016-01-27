90e88232a14924226465639566ba87ca95a07762
estade <estade@chromium.org>
2016-01-27 14:53:10 -0800

    Fix bug in browser action icon scaling for non-1x scale factors.
    skia::ImageOperations::Resize needs pixels, and we were passing dp.
    
    Also, ignore the icon size key and just use the icon size itself. This fixes a bug (evidenced again by Shoptagr) where an icon size such as
    
    "19": "icons/19.png"  // 19.png is actually 50x50
    
    would be scaled incorrectly. At this point, the API should take a list rather than a dict, but it's probably not possible to change that and maintain backcompat.
    
    BUG=581232
    TBR=benwells@chromium.org
    
    Review URL: https://codereview.chromium.org/1637763002
    
    Cr-Commit-Position: refs/heads/master@{#371904}

--- chrome/browser/ui/extensions/icon_with_badge_image_source.cc
+++ chrome/browser/ui/extensions/icon_with_badge_image_source.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
+#include "chrome/browser/extensions/extension_action.h"
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkTypeface.h"
 #include "ui/base/material_design/material_design_controller.h"
@@ -91,12 +92,13 @@ SkPaint* GetBadgeTextPaintSingleton() {
 }
 
 gfx::ImageSkiaRep ScaleImageSkiaRep(const gfx::ImageSkiaRep& rep,
-                                    int target_width,
+                                    int target_width_dp,
                                     float target_scale) {
+  int width_px = target_width_dp * target_scale;
   return gfx::ImageSkiaRep(
       skia::ImageOperations::Resize(rep.sk_bitmap(),
                                     skia::ImageOperations::RESIZE_BEST,
-                                    target_width, target_width),
+                                    width_px, width_px),
       target_scale);
 }
 
@@ -131,14 +133,16 @@ void IconWithBadgeImageSource::Draw(gfx::Canvas* canvas) {
   gfx::ImageSkia skia = icon_.AsImageSkia();
   gfx::ImageSkiaRep rep = skia.GetRepresentation(canvas->image_scale());
   if (rep.scale() != canvas->image_scale()) {
-    skia.AddRepresentation(
-        ScaleImageSkiaRep(rep, skia.width(), canvas->image_scale()));
+    skia.AddRepresentation(ScaleImageSkiaRep(
+        rep, ExtensionAction::ActionIconSize(), canvas->image_scale()));
   }
   if (grayscale_)
     skia = gfx::ImageSkiaOperations::CreateHSLShiftedImage(skia, {-1, 0, 0.6});
 
-  int x_offset = std::floor((size().width() - icon_.Width()) / 2.0);
-  int y_offset = std::floor((size().height() - icon_.Height()) / 2.0);
+  int x_offset =
+      std::floor((size().width() - ExtensionAction::ActionIconSize()) / 2.0);
+  int y_offset =
+      std::floor((size().height() - ExtensionAction::ActionIconSize()) / 2.0);
   canvas->DrawImageInt(skia, x_offset, y_offset);
 
   // Draw a badge on the provided browser action icon's canvas.
