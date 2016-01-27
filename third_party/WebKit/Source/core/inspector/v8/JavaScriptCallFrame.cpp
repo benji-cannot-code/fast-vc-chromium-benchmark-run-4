2ca5feb2791bddb2eb83cc771d856f63833a5a58
jochen <jochen@chromium.org>
2016-01-27 09:35:30 -0800

    Stop using Isolate::GetCurrent() in inspector/v8
    R=pfeldman@chromium.org
    BUG=580337
    
    Review URL: https://codereview.chromium.org/1637413003
    
    Cr-Commit-Position: refs/heads/master@{#371813}

--- third_party/WebKit/Source/core/inspector/v8/JavaScriptCallFrame.cpp
+++ third_party/WebKit/Source/core/inspector/v8/JavaScriptCallFrame.cpp
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 JavaScriptCallFrame::JavaScriptCallFrame(v8::Local<v8::Context> debuggerContext, v8::Local<v8::Object> callFrame)
-    : m_isolate(v8::Isolate::GetCurrent())
+    : m_isolate(debuggerContext->GetIsolate())
     , m_debuggerContext(m_isolate, debuggerContext)
     , m_callFrame(m_isolate, callFrame)
 {
