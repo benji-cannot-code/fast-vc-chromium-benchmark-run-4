42d317ae213a6692ec5f1584c36c17f4e2daee9d
ccameron <ccameron@chromium.org>
2016-01-27 13:12:04 -0800

    Mac overlays: Move CALayerPartialDamageTree to its own file
    Direct copy-paste, no changes.
    
    BUG=533681
    TBR=erikchen
    
    Review URL: https://codereview.chromium.org/1647523002
    
    Cr-Commit-Position: refs/heads/master@{#371871}

--- content/common/gpu/image_transport_surface_overlay_mac.h
+++ content/common/gpu/image_transport_surface_overlay_mac.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <list>
 #include <vector>
 
-#include "base/memory/linked_ptr.h"
 #import "base/mac/scoped_nsobject.h"
 #include "base/timer/timer.h"
 #include "content/common/gpu/gpu_command_buffer_stub.h"
