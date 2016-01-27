a67d389161a4973bd18aa6ce89b2ac6fa233662d
yuzus <yuzus@google.com>
2016-01-26 22:22:51 -0800

    Add scoped flag to Event.
    Spec is described here: http://w3c.github.io/webcomponents/spec/shadow/#dfn-scoped-flag
    Only with UA's creation of certain events(abort, error, etc.), this scoped flag is set to true.
    Note that any event created by users has scoped flag set to false by default.
    R=hayato@chromium.org
    BUG=531990, 575042
    
    Review URL: https://codereview.chromium.org/1586563005
    
    Cr-Commit-Position: refs/heads/master@{#371747}

--- third_party/WebKit/Source/core/events/Event.h
+++ third_party/WebKit/Source/core/events/Event.h
@@ -124,6 +124,7 @@ public:
 
     bool bubbles() const { return m_canBubble; }
     bool cancelable() const { return m_cancelable; }
+    bool scoped() const { return m_scoped; }
 
     // Event creation timestamp in milliseconds. If |HiResEventTimeStamp|
     // runtime feature is enabled it returns a DOMHighResTimeStamp using the
@@ -206,6 +207,8 @@ protected:
     Event();
     Event(const AtomicString& type, bool canBubble, bool cancelable);
     Event(const AtomicString& type, bool canBubble, bool cancelable, double platformTimeStamp);
+    Event(const AtomicString& type, bool canBubble, bool cancelable, bool scoped);
+    Event(const AtomicString& type, bool canBubble, bool cancelable, bool scoped, double platformTimeStamp);
     Event(const AtomicString& type, const EventInit&);
 
     virtual void receivedTarget();
@@ -217,6 +220,7 @@ private:
     AtomicString m_type;
     unsigned m_canBubble:1;
     unsigned m_cancelable:1;
+    unsigned m_scoped:1;
 
     unsigned m_propagationStopped:1;
     unsigned m_immediatePropagationStopped:1;
