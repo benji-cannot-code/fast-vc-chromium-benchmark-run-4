e62a23d279d478b1f40ba867e82b82a489c63d49
tmoniuszko <tmoniuszko@opera.com>
2016-01-27 08:28:45 -0800

    Move trace_event.h include from v8_platform.h to .cc file
    trace_event.h is redundant in header file. It also causes compilation
    issues when Blink precompiled headers are used on Windows (Bug 495697).
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1585693002
    
    Cr-Commit-Position: refs/heads/master@{#371801}

--- gin/public/v8_platform.h
+++ gin/public/v8_platform.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/lazy_instance.h"
 #include "base/macros.h"
-#include "base/trace_event/trace_event.h"
 #include "gin/gin_export.h"
 #include "v8/include/v8-platform.h"
 
--- gin/v8_platform.cc
+++ gin/v8_platform.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/location.h"
 #include "base/threading/worker_pool.h"
+#include "base/trace_event/trace_event.h"
 #include "gin/per_isolate_data.h"
 
 namespace gin {
