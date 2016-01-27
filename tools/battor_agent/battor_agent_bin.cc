8e708b8c3feb265f06879aa491142242d3d9b21c
charliea <charliea@chromium.org>
2016-01-27 14:17:40 -0800

    tools/battor_agent: Adds a tool to find connected BattOrs
    This is a resubmission of https://codereview.chromium.org/1631313002/.
    Since then, the //device/serial dependencies have been fixed.
    
    This tool will also be used by the Chromium trace controller to find
    any connected BattOrs.
    
    TBR=zhenw@chromium.org,sky@chromium.org
    BUG=542837
    
    Review URL: https://codereview.chromium.org/1642763002
    
    Cr-Commit-Position: refs/heads/master@{#371891}

--- tools/battor_agent/battor_agent_bin.cc
+++ tools/battor_agent/battor_agent_bin.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "tools/battor_agent/battor_agent.h"
 #include "tools/battor_agent/battor_error.h"
+#include "tools/battor_agent/battor_finder.h"
 
 using std::cout;
 using std::endl;
@@ -48,12 +49,11 @@ void PrintSupportsExplicitClockSync() {
   cout << battor::BattOrAgent::SupportsExplicitClockSync() << endl;
 }
 
-// Retrieves argument argnum from the argument list, printing the usage
-// guidelines and exiting with an error code if the argument doesn't exist.
+// Retrieves argument argnum from the argument list, or an empty string if the
+// argument doesn't exist.
 std::string GetArg(int argnum, int argc, char* argv[]) {
   if (argnum >= argc) {
-    PrintUsage();
-    exit(1);
+    return std::string();
   }
 
   return argv[argnum];
@@ -90,6 +90,10 @@ class BattOrAgentBin : public BattOrAgent::Listener {
   // Runs the BattOr binary and returns the exit code.
   int Run(int argc, char* argv[]) {
     std::string cmd = GetArg(1, argc, argv);
+    if (cmd.empty()) {
+      PrintUsage();
+      exit(1);
+    }
 
     // SupportsExplicitClockSync doesn't need to use the serial connection, so
     // handle it separately.
@@ -99,6 +103,18 @@ class BattOrAgentBin : public BattOrAgent::Listener {
     }
 
     std::string path = GetArg(2, argc, argv);
+    // If no path is specified, see if we can find a BattOr and use that.
+    if (path.empty())
+      path = BattOrFinder::FindBattOr();
+
+    // If we don't have any BattOr to use, exit.
+    if (path.empty()) {
+      cout << "Unable to find a BattOr, and no explicit BattOr path was "
+              "specified."
+           << endl;
+      exit(1);
+    }
+
     SetUp(path);
 
     if (cmd == "StartTracing") {
