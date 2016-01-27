cfa53636bdaab1d17ef87bf2f6d55a37d14d7a7d
rjkroege <rjkroege@chromium.org>
2016-01-26 19:06:03 -0800

    Relocate ozone gbm overlay config cache to manager
    The ozone gbm overlay candidate maintains a cache of layer configurations
    that have previously been validated by the kernel display system. This is
    unnecessary. Relocate the cache to the global overlay manager to simplify
    the implementation of pluggable host to gpu communication.
    
    BUG=558602
    
    Review URL: https://codereview.chromium.org/1631073002
    
    Cr-Commit-Position: refs/heads/master@{#371696}

--- ui/ozone/platform/drm/host/drm_window_host.cc
+++ ui/ozone/platform/drm/host/drm_window_host.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/drm/host/drm_display_host.h"
 #include "ui/ozone/platform/drm/host/drm_display_host_manager.h"
 #include "ui/ozone/platform/drm/host/drm_gpu_platform_support_host.h"
-#include "ui/ozone/platform/drm/host/drm_overlay_candidates_host.h"
+#include "ui/ozone/platform/drm/host/drm_overlay_manager.h"
 #include "ui/ozone/platform/drm/host/drm_window_host_manager.h"
 #include "ui/platform_window/platform_window_delegate.h"
 
@@ -29,14 +29,15 @@ DrmWindowHost::DrmWindowHost(PlatformWindowDelegate* delegate,
                              EventFactoryEvdev* event_factory,
                              DrmCursor* cursor,
                              DrmWindowHostManager* window_manager,
-                             DrmDisplayHostManager* display_manager)
+                             DrmDisplayHostManager* display_manager,
+                             DrmOverlayManager* overlay_manager)
     : delegate_(delegate),
       sender_(sender),
       event_factory_(event_factory),
       cursor_(cursor),
       window_manager_(window_manager),
       display_manager_(display_manager),
-      overlay_candidates_host_(nullptr),
+      overlay_manager_(overlay_manager),
       bounds_(bounds),
       widget_(window_manager->NextAcceleratedWidget()) {
   window_manager_->AddWindow(widget_, this);
@@ -196,19 +197,13 @@ void DrmWindowHost::OnChannelEstablished() {
 void DrmWindowHost::OnChannelDestroyed() {
 }
 
-void DrmWindowHost::SetOverlayCandidatesHost(
-    DrmOverlayCandidatesHostCore* host) {
-  overlay_candidates_host_ = host;
-}
-
 void DrmWindowHost::SendBoundsChange() {
   // Update the cursor before the window so that the cursor stays within the
   // window bounds when the window size shrinks.
   cursor_->CommitBoundsChange(widget_, bounds_, GetCursorConfinedBounds());
   sender_->Send(new OzoneGpuMsg_WindowBoundsChanged(widget_, bounds_));
 
-  if (overlay_candidates_host_)
-    overlay_candidates_host_->ResetCache();
+  overlay_manager_->ResetCache();
 }
 
 }  // namespace ui
