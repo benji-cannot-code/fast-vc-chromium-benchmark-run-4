bf2c18ad5daf67c0983afe4af2c75003dcecd0bf
alph <alph@chromium.org>
2016-01-28 11:44:44 -0800

    DevTools: Switch to using fast stack iterator to collect stacks during timeline recording.
    Stacks for events are now available only when JS profiler is enabled.
    
    BUG=579191
    
    Review URL: https://codereview.chromium.org/1624783002
    
    Cr-Commit-Position: refs/heads/master@{#372145}

--- third_party/WebKit/Source/core/inspector/InspectorTraceEvents.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorTraceEvents.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Vector.h"
 #include "wtf/text/TextPosition.h"
 #include <inttypes.h>
+#include <v8-profiler.h>
 #include <v8.h>
 
 namespace blink {
@@ -53,9 +54,12 @@ void setCallStack(TracedValue* value)
         traceCategoryEnabled = TRACE_EVENT_API_GET_CATEGORY_GROUP_ENABLED(TRACE_DISABLED_BY_DEFAULT("devtools.timeline.stack"));
     if (!*traceCategoryEnabled)
         return;
-    RefPtr<ScriptCallStack> scriptCallStack = currentScriptCallStack(ScriptCallStack::maxCallStackSizeToCapture);
-    if (scriptCallStack)
+    // The CPU profiler stack trace does not include call site line numbers.
+    // So we collect the top frame with the currentScriptCallStack to get the
+    // binding call site info.
+    if (RefPtr<ScriptCallStack> scriptCallStack = currentScriptCallStack(1))
         scriptCallStack->toTracedValue(value, "stackTrace");
+    v8::Isolate::GetCurrent()->GetCpuProfiler()->CollectSample();
 }
 
 namespace {
--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineUIUtils.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineUIUtils.js
@@ -217,6 +217,16 @@ WebInspector.TimelineUIUtils.isMarkerEvent = function(event)
 }
 
 /**
+ * @param {!WebInspector.TracingModel.Event} event
+ * @return {?ConsoleAgent.CallFrame}
+ */
+WebInspector.TimelineUIUtils.topStackFrame = function(event)
+{
+    var stackTrace = event.stackTrace || event.initiator && event.initiator.stackTrace;
+    return stackTrace && stackTrace.length ? stackTrace[0] : null;
+}
+
+/**
  * @enum {symbol}
  */
 WebInspector.TimelineUIUtils.NetworkCategory = {
@@ -384,16 +394,8 @@ WebInspector.TimelineUIUtils.buildDetailsTextForTraceEvent = function(event, tar
      */
     function linkifyTopCallFrameAsText()
     {
-        var stackTrace = event.stackTrace;
-        if (!stackTrace) {
-            var initiator = event.initiator;
-            if (initiator)
-                stackTrace = initiator.stackTrace;
-        }
-        if (!stackTrace || !stackTrace.length)
-            return null;
-        var callFrame = stackTrace[0];
-        return linkifyLocationAsText(callFrame.scriptId, callFrame.lineNumber, callFrame.columnNumber);
+        var frame = WebInspector.TimelineUIUtils.topStackFrame(event);
+        return frame ? linkifyLocationAsText(frame.scriptId, frame.lineNumber, frame.columnNumber) : null;
     }
 }
 
@@ -488,15 +490,8 @@ WebInspector.TimelineUIUtils.buildDetailsNodeForTraceEvent = function(event, tar
      */
     function linkifyTopCallFrame()
     {
-        var stackTrace = event.stackTrace;
-        if (!stackTrace) {
-            var initiator = event.initiator;
-            if (initiator)
-                stackTrace = initiator.stackTrace;
-        }
-        if (!stackTrace || !stackTrace.length)
-            return null;
-        return linkifier.linkifyConsoleCallFrame(target, stackTrace[0], "timeline-details");
+        var frame = WebInspector.TimelineUIUtils.topStackFrame(event);
+        return frame ? linkifier.linkifyConsoleCallFrame(target, frame, "timeline-details") : null;
     }
 }
 
