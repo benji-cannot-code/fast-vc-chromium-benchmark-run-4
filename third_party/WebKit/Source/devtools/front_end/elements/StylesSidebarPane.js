cefa8a3f5fd3678b3f3c42a8ba6be91d2ce15bd5
samli <samli@chromium.org>
2016-01-27 18:55:16 -0800

    Devtools: Fix enter to accept autocomplete in styles pane
    This bug was exposed in https://codereview.chromium.org/1611783002
    
    BUG=581909
    
    Review URL: https://codereview.chromium.org/1643833002
    
    Cr-Commit-Position: refs/heads/master@{#371976}

--- third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/elements/StylesSidebarPane.js
@@ -2730,7 +2730,8 @@ WebInspector.StylesSidebarPane.CSSPropertyPrompt.prototype = {
             }
             break;
         case "Enter":
-            if (this.autoCompleteElement && !this.autoCompleteElement.textContent.length) {
+            // Accept any available autocompletions and advance to the next field.
+            if (this.autoCompleteElement && this.autoCompleteElement.textContent.length) {
                 this.tabKeyPressed();
                 return;
             }
