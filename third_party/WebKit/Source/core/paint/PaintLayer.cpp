e741ca8e19d9cf6e87fe0a786717bd764ab05602
wangxianzhu <wangxianzhu@chromium.org>
2016-01-28 12:56:55 -0800

    Make PaintLayerClipper objects transient
    By storing the cache into PaintLayer, we can avoid PaintLayer::m_clipper
    and reduce 2 pointers to 1 pointers in PaintLayer for the clipper.
    
    BUG=581124
    
    Review URL: https://codereview.chromium.org/1640843003
    
    Cr-Commit-Position: refs/heads/master@{#372162}

--- third_party/WebKit/Source/core/paint/PaintLayer.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayer.cpp
@@ -100,7 +100,7 @@ static CompositingQueryMode gCompositingQueryMode =
 
 struct SameSizeAsPaintLayer : DisplayItemClient {
     int bitFields;
-    void* pointers[8];
+    void* pointers[9];
     LayoutUnit layoutUnits[4];
     IntSize size;
     OwnPtrWillBePersistent<PaintLayerScrollableArea> scrollableArea;
@@ -108,9 +108,6 @@ struct SameSizeAsPaintLayer : DisplayItemClient {
         IntRect rect;
         void* pointers[2];
     } ancestorCompositingInputs;
-    struct {
-        void* pointers[2];
-    } clipper;
     struct {
         IntSize size;
         void* pointer;
@@ -175,7 +172,6 @@ PaintLayer::PaintLayer(LayoutBoxModelObject* layoutObject, PaintLayerType type)
     , m_last(0)
     , m_staticInlinePosition(0)
     , m_staticBlockPosition(0)
-    , m_clipper(*layoutObject)
 {
     updateStackingNode();
 
@@ -293,7 +289,7 @@ void PaintLayer::updateLayerPositionsAfterLayout()
 {
     TRACE_EVENT0("blink,benchmark", "PaintLayer::updateLayerPositionsAfterLayout");
 
-    m_clipper.clearClipRectsIncludingDescendants();
+    clipper().clearClipRectsIncludingDescendants();
     updateLayerPositionRecursive();
 
     {
@@ -370,7 +366,7 @@ bool PaintLayer::scrollsWithRespectTo(const PaintLayer* other) const
 
 void PaintLayer::updateLayerPositionsAfterOverflowScroll(const DoubleSize& scrollDelta)
 {
-    m_clipper.clearClipRectsIncludingDescendants();
+    clipper().clearClipRectsIncludingDescendants();
     updateLayerPositionsAfterScrollRecursive(scrollDelta, isPaintInvalidationContainer());
 }
 
@@ -420,9 +416,9 @@ void PaintLayer::updateTransform(const ComputedStyle* oldStyle, const ComputedSt
             m_rareData->transform.clear();
 
         // PaintLayers with transforms act as clip rects roots, so clear the cached clip rects here.
-        m_clipper.clearClipRectsIncludingDescendants();
+        clipper().clearClipRectsIncludingDescendants();
     } else if (hasTransform) {
-        m_clipper.clearClipRectsIncludingDescendants(AbsoluteClipRects);
+        clipper().clearClipRectsIncludingDescendants(AbsoluteClipRects);
     }
 
     updateTransformationMatrix();
@@ -1271,7 +1267,7 @@ void PaintLayer::removeOnlyThisLayer()
         }
     }
 
-    m_clipper.clearClipRectsIncludingDescendants();
+    clipper().clearClipRectsIncludingDescendants();
 
     PaintLayer* nextSib = nextSibling();
 
@@ -1313,7 +1309,7 @@ void PaintLayer::insertOnlyThisLayer()
         curr->moveLayers(m_parent, this);
 
     // Clear out all the clip rects.
-    m_clipper.clearClipRectsIncludingDescendants();
+    clipper().clearClipRectsIncludingDescendants();
 }
 
 // Returns the layer reached on the walk up towards the ancestor.
