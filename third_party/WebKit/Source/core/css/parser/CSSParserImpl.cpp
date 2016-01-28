366202077f818b97de03fb9debf23ba5b4805fe9
noel <noel@chromium.org>
2016-01-28 00:56:57 -0800

    Revert of Add deprecation message for -webkit-background-composite (patchset #11 id:200001 of https://codereview.chromium.org/1585383003/ )
    Reason for revert:
    Broke fast/backgrounds/webkit-background-composite-deprecated.html most bots.  Example:
    
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.6/builds/48981
    
    Original issue's description:
    > Add deprecation message for -webkit-background-composite
    >
    > This patch is two fold:
    > 1. Adds the capability of adding deprecation messages for CSSPropertyIDs
    > as they are parsed.
    > 2. Adds the relevant deprecation message for
    > -webkit-background-composite. The reasons for which are highlighted here
    > https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/thxrbc8XOmc
    >
    > BUG=498588
    >
    > Committed: https://crrev.com/6d2d7c3b60d3e5cd9b2efd7120cae737ad1e4329
    > Cr-Commit-Position: refs/heads/master@{#372040}
    
    TBR=alancutter@chromium.org,timloh@chromium.org,nainar@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=498588
    
    Review URL: https://codereview.chromium.org/1648593002
    
    Cr-Commit-Position: refs/heads/master@{#372050}

--- third_party/WebKit/Source/core/css/parser/CSSParserImpl.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSParserImpl.cpp
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/parser/MediaQueryParser.h"
 #include "core/dom/Document.h"
 #include "core/dom/Element.h"
-#include "core/frame/Deprecation.h"
 #include "core/frame/UseCounter.h"
 #include "platform/TraceEvent.h"
 #include "wtf/BitArray.h"
@@ -721,11 +720,8 @@ void CSSParserImpl::consumeDeclaration(CSSParserTokenRange range, StyleRule::Typ
     if (important && (ruleType == StyleRule::FontFace || ruleType == StyleRule::Keyframe))
         return;
 
-    if (unresolvedProperty != CSSPropertyInvalid) {
-        if (m_styleSheet && m_styleSheet->singleOwnerDocument())
-            Deprecation::warnOnDeprecatedProperties(m_styleSheet->singleOwnerDocument()->frame(), unresolvedProperty);
+    if (unresolvedProperty != CSSPropertyInvalid)
         consumeDeclarationValue(range.makeSubRange(&range.peek(), declarationValueEnd), unresolvedProperty, important, ruleType);
-    }
 
     if (m_observerWrapper && (ruleType == StyleRule::Style || ruleType == StyleRule::Keyframe)) {
         m_observerWrapper->observer().observeProperty(
--- third_party/WebKit/Source/core/frame/FrameHost.h
+++ third_party/WebKit/Source/core/frame/FrameHost.h
@@ -46,7 +46,6 @@ namespace blink {
 
 class ChromeClient;
 class ConsoleMessageStorage;
-class Deprecation;
 class EventHandlerRegistry;
 class Page;
 class PageScaleConstraintsSet;
@@ -74,7 +73,6 @@ public:
     Settings& settings() const;
     ChromeClient& chromeClient() const;
     UseCounter& useCounter() const;
-    Deprecation& deprecation() const;
 
     // Corresponds to pixel density of the device where this Page is
     // being displayed. In multi-monitor setups this can vary between pages.
