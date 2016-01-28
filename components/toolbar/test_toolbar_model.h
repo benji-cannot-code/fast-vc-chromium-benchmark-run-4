e6cf8d88f241bda3e8ef85cffe8b523552d01617
blundell <blundell@chromium.org>
2016-01-28 08:27:52 -0800

    Componentize TestToolbarModel
    This will be needed by upcoming CL that componentizes omnibox_edit_unittest.cc.
    
    TBR=jochen
    
    Review URL: https://codereview.chromium.org/1634743003
    
    Cr-Commit-Position: refs/heads/master@{#372097}

--- components/toolbar/test_toolbar_model.h
+++ components/toolbar/test_toolbar_model.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_TOOLBAR_TEST_TOOLBAR_MODEL_H_
-#define CHROME_BROWSER_UI_TOOLBAR_TEST_TOOLBAR_MODEL_H_
+#ifndef COMPONENTS_TOOLBAR_TEST_TOOLBAR_MODEL_H_
+#define COMPONENTS_TOOLBAR_TEST_TOOLBAR_MODEL_H_
 
 #include <stddef.h>
 
@@ -66,4 +66,4 @@ class TestToolbarModel : public ToolbarModel {
   DISALLOW_COPY_AND_ASSIGN(TestToolbarModel);
 };
 
-#endif  // CHROME_BROWSER_UI_TOOLBAR_TEST_TOOLBAR_MODEL_H_
+#endif  // COMPONENTS_TOOLBAR_TEST_TOOLBAR_MODEL_H_
