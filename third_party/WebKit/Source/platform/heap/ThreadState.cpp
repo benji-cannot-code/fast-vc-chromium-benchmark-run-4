a2c03af6134f4a98d42c19f610d92372007f62ab
mseaborn <mseaborn@chromium.org>
2016-01-28 14:20:37 -0800

    Cleanup: Remove unused declaration of __libc_stack_end
    The use was moved to StackFrameDepth.cpp by an earlier change.
    
    BUG=420515
    TEST=build
    
    Review URL: https://codereview.chromium.org/1642983002
    
    Cr-Commit-Position: refs/heads/master@{#372182}

--- third_party/WebKit/Source/platform/heap/ThreadState.cpp
+++ third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -53,8 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <windows.h>
 #include <winnt.h>
-#elif defined(__GLIBC__)
-extern "C" void* __libc_stack_end;  // NOLINT
 #endif
 
 #if defined(MEMORY_SANITIZER)
