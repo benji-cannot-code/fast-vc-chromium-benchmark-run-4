cd1a3d130fcad31ce1f12a4d312c800a59bf95ef
marcinjb <marcinjb@chromium.org>
2016-01-28 15:05:02 -0800

    Set up an stunnel endpoint for the engine.
    This puts an stunnel proxy in the engine's Docker container. The client
    will connect on the same port, stunnel will intercept and forward the
    underlying traffic to the engine which is now listening on another port.
    
    BUG=575421
    
    Review URL: https://codereview.chromium.org/1638153002
    
    Cr-Commit-Position: refs/heads/master@{#372199}

---
+++ blimp/engine/start_engine.sh
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+stunnel \
+  -p /engine/data/stunnel.pem \
+  -P /engine/stunnel.pid \
+  -d 25466 -r 25467 -f &
+/engine/blimp_engine_app \
+  --disable-gpu \
+  --use-remote-compositing \
+  --disable-cached-picture-raster \
+  --blimp-client-token-path=/engine/data/client_token \
+  $@ &
+
+# Stop execution if either stunnel or blimp_engine_app die.
+wait -n
