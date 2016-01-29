3067c9b9840f49280c3bb2bc4e12e0f26b9728ce
danakj <danakj@chromium.org>
2016-01-28 18:24:10 -0800

    rewrite_to_chrome_style: Avoid asserts when checking for const-ness.
    Just skip expressions that are dependent on template parameters, treat
    them as non-constant.
    
    R=dcheng
    BUG=581218
    
    Review URL: https://codereview.chromium.org/1645243002
    
    Cr-Commit-Position: refs/heads/master@{#372256}

---
+++ tools/clang/rewrite_to_chrome_style/tests/template-original.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+namespace blink {
+
+template <typename T, int number>
+void F() {
+  // We don't assert on this, and we don't end up considering it a const for
+  // now.
+  const int maybe_a_const = sizeof(T);
+  const int is_a_const = number;
+}
+
+template <int number, typename... T>
+void F() {
+  // We don't assert on this, and we don't end up considering it a const for
+  // now.
+  const int maybe_a_const = sizeof...(T);
+  const int is_a_const = number;
+}
+
+}  // namespace blink
