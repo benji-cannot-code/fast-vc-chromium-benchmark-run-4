f8111792c5a628758054a851b7337aeb4e301811
skobes <skobes@chromium.org>
2016-01-29 15:38:10 -0800

    PaintLayerScrollableArea::setScrollPosition should propagate the ScrollType.
    Review URL: https://codereview.chromium.org/1647313002
    
    Cr-Commit-Position: refs/heads/master@{#372465}

--- third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.h
+++ third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.h
@@ -229,11 +229,13 @@ public:
     // FIXME: We shouldn't allow access to m_overflowRect outside this class.
     LayoutRect overflowRect() const { return m_overflowRect; }
 
-    void scrollToPosition(const DoublePoint& scrollPosition, ScrollOffsetClamping = ScrollOffsetUnclamped, ScrollBehavior = ScrollBehaviorInstant, ScrollType = ProgrammaticScroll);
+    void scrollToPosition(const DoublePoint& scrollPosition, ScrollOffsetClamping = ScrollOffsetUnclamped,
+        ScrollBehavior = ScrollBehaviorInstant, ScrollType = ProgrammaticScroll);
 
-    void scrollToOffset(const DoubleSize& scrollOffset, ScrollOffsetClamping clamp = ScrollOffsetUnclamped, ScrollBehavior scrollBehavior = ScrollBehaviorInstant)
+    void scrollToOffset(const DoubleSize& scrollOffset, ScrollOffsetClamping clamp = ScrollOffsetUnclamped,
+        ScrollBehavior scrollBehavior = ScrollBehaviorInstant, ScrollType scrollType = ProgrammaticScroll)
     {
-        scrollToPosition(-scrollOrigin() + scrollOffset, clamp, scrollBehavior);
+        scrollToPosition(-scrollOrigin() + scrollOffset, clamp, scrollBehavior, scrollType);
     }
 
     void scrollToXOffset(double x, ScrollOffsetClamping clamp = ScrollOffsetUnclamped, ScrollBehavior scrollBehavior = ScrollBehaviorInstant)
@@ -248,7 +250,7 @@ public:
 
     void setScrollPosition(const DoublePoint& position, ScrollType scrollType, ScrollBehavior scrollBehavior = ScrollBehaviorInstant) override
     {
-        scrollToOffset(toDoubleSize(position), ScrollOffsetClamped, scrollBehavior);
+        scrollToOffset(toDoubleSize(position), ScrollOffsetClamped, scrollBehavior, scrollType);
     }
 
     void updateScrollDimensions(DoubleSize& scrollOffset, bool& autoHorizontalScrollBarChanged, bool& autoVerticalScrollBarChanged);
