195877a9a360c25aa11327ac6274f43f4dacaa6b
estark <estark@chromium.org>
2016-01-29 07:35:17 -0800

    Handle subresource cert errors even when there is no ResourceLoader
    Previously, MixedContentChecker relied on a ResourceLoader to notify the
    embedder about subresources laoded with certificate errors. When a
    ResourceLoader wasn't present -- notably, for resources served from the
    memory cache -- the embedder wasn't getting notified at all.
    
    MixedContentChecker was using the ResourceLoader only to figure out the
    request's |frameType| and |requestContext|. This CL makes it so that
    FrameFetchContext passes in those values directly instead of passing in
    the ResourceLoader.
    
    BUG=477868
    
    Review URL: https://codereview.chromium.org/1648613003
    
    Cr-Commit-Position: refs/heads/master@{#372349}

--- third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
+++ third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
@@ -249,7 +249,7 @@ void FrameFetchContext::dispatchWillSendRequest(unsigned long identifier, Resour
     InspectorInstrumentation::willSendRequest(frame(), identifier, ensureLoaderForNotifications(), request, redirectResponse, initiatorInfo);
 }
 
-void FrameFetchContext::dispatchDidReceiveResponse(unsigned long identifier, const ResourceResponse& response, ResourceLoader* resourceLoader)
+void FrameFetchContext::dispatchDidReceiveResponse(unsigned long identifier, const ResourceResponse& response, WebURLRequest::FrameType frameType, WebURLRequest::RequestContext requestContext, ResourceLoader* resourceLoader)
 {
     LinkLoader::CanLoadResources resourceLoadingPolicy = LinkLoader::LoadResourcesAndPreconnect;
     MixedContentChecker::checkMixedPrivatePublic(frame(), response.remoteIPAddress());
@@ -263,8 +263,8 @@ void FrameFetchContext::dispatchDidReceiveResponse(unsigned long identifier, con
     }
     LinkLoader::loadLinkFromHeader(response.httpHeaderField(HTTPNames::Link), frame()->document(), NetworkHintsInterfaceImpl(), resourceLoadingPolicy);
 
-    if (response.hasMajorCertificateErrors() && resourceLoader)
-        MixedContentChecker::handleCertificateError(frame(), resourceLoader->originalRequest(), response);
+    if (response.hasMajorCertificateErrors())
+        MixedContentChecker::handleCertificateError(frame(), response, frameType, requestContext);
 
     frame()->loader().progress().incrementProgress(identifier, response);
     frame()->loader().client()->dispatchDidReceiveResponse(m_documentLoader, identifier, response);
@@ -309,8 +309,7 @@ void FrameFetchContext::dispatchDidFail(unsigned long identifier, const Resource
         frame()->console().didFailLoading(identifier, error);
 }
 
-
-void FrameFetchContext::dispatchDidLoadResourceFromMemoryCache(const Resource* resource)
+void FrameFetchContext::dispatchDidLoadResourceFromMemoryCache(const Resource* resource, WebURLRequest::FrameType frameType, WebURLRequest::RequestContext requestContext)
 {
     ResourceRequest request(resource->url());
     unsigned long identifier = createUniqueIdentifier();
@@ -319,7 +318,7 @@ void FrameFetchContext::dispatchDidLoadResourceFromMemoryCache(const Resource* r
 
     InspectorInstrumentation::markResourceAsCached(frame(), identifier);
     if (!resource->response().isNull())
-        dispatchDidReceiveResponse(identifier, resource->response());
+        dispatchDidReceiveResponse(identifier, resource->response(), frameType, requestContext);
 
     if (resource->encodedSize() > 0)
         dispatchDidReceiveData(identifier, 0, resource->encodedSize(), 0);
--- third_party/WebKit/Source/core/loader/MixedContentChecker.cpp
+++ third_party/WebKit/Source/core/loader/MixedContentChecker.cpp
@@ -475,9 +475,8 @@ Frame* MixedContentChecker::effectiveFrameForFrameType(LocalFrame* frame, WebURL
     return parentFrame;
 }
 
-void MixedContentChecker::handleCertificateError(LocalFrame* frame, const ResourceRequest& request, const ResourceResponse& response)
+void MixedContentChecker::handleCertificateError(LocalFrame* frame, const ResourceResponse& response, WebURLRequest::FrameType frameType, WebURLRequest::RequestContext requestContext)
 {
-    WebURLRequest::FrameType frameType = request.frameType();
     Frame* effectiveFrame = effectiveFrameForFrameType(frame, frameType);
     if (frameType == WebURLRequest::FrameTypeTopLevel || !effectiveFrame)
         return;
@@ -492,7 +491,6 @@ void MixedContentChecker::handleCertificateError(LocalFrame* frame, const Resour
     // distinguish mixed content signals from different frames on the
     // same page.
     FrameLoaderClient* client = frame->loader().client();
-    WebURLRequest::RequestContext requestContext = request.requestContext();
     ContextType contextType = MixedContentChecker::contextTypeFromContext(requestContext, effectiveFrame);
     if (contextType == ContextTypeBlockable) {
         client->didRunContentWithCertificateErrors(response.url(), response.getSecurityInfo(), mainResourceUrlForFrame(effectiveFrame), localEffectiveFrame->loader().documentLoader()->response().getSecurityInfo());
