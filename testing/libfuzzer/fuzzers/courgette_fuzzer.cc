f553353860ce8b32742064074651de910ffa7175
huangs <huangs@chromium.org>
2016-01-29 15:54:13 -0800

    [libfuzzer] Fix compile error from missing courgette_fuzzer.cc include.
    https://codereview.chromium.org/1629703002/ missed a spot and broke
    build with libfuzzer. This fixes it.
    
    TBR=krasin@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1654503002
    
    Cr-Commit-Position: refs/heads/master@{#372475}

--- testing/libfuzzer/fuzzers/courgette_fuzzer.cc
+++ testing/libfuzzer/fuzzers/courgette_fuzzer.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "courgette/assembly_program.h"
 #include "courgette/courgette.h"
 #include "courgette/encoded_program.h"
+#include "courgette/program_detector.h"
 
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const unsigned char* data, size_t size) {
