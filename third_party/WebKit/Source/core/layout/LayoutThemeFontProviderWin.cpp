3bf27327c71b9502822d127662de92a4cd44b5c0
jbroman <jbroman@chromium.org>
2016-01-29 10:05:43 -0800

    Remove blink::isWindowsVistaOrGreater.
    Chromium no longer supports Windows XP; see:
      https://code.google.com/p/chromium/issues/detail?id=579196
    
    BUG=579265
    
    Review URL: https://codereview.chromium.org/1609083002
    
    Cr-Commit-Position: refs/heads/master@{#372373}

--- third_party/WebKit/Source/core/layout/LayoutThemeFontProviderWin.cpp
+++ third_party/WebKit/Source/core/layout/LayoutThemeFontProviderWin.cpp
@@ -30,15 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/fonts/FontCache.h"
 #include "platform/fonts/FontDescription.h"
 #include "platform/win/HWndDC.h"
-#include "platform/win/SystemInfo.h"
 #include "wtf/text/WTFString.h"
 #include <windows.h>
-
-#define SIZEOF_STRUCT_WITH_SPECIFIED_LAST_MEMBER(structName, member) \
-    offsetof(structName, member) + \
-    (sizeof static_cast<structName*>(nullptr)->member)
-#define NONCLIENTMETRICS_SIZE_PRE_VISTA \
-    SIZEOF_STRUCT_WITH_SPECIFIED_LAST_MEMBER(NONCLIENTMETRICS, lfMessageFont)
 
 namespace blink {
 
@@ -60,10 +53,8 @@ static float pointsToPixels(float points)
 
 static bool getNonClientMetrics(NONCLIENTMETRICS* metrics)
 {
-    static UINT size = isWindowsVistaOrGreater() ?
-        sizeof(NONCLIENTMETRICS) : NONCLIENTMETRICS_SIZE_PRE_VISTA;
-    metrics->cbSize = size;
-    bool success = !!SystemParametersInfo(SPI_GETNONCLIENTMETRICS, size, metrics, 0);
+    metrics->cbSize = sizeof(NONCLIENTMETRICS);
+    bool success = !!SystemParametersInfo(SPI_GETNONCLIENTMETRICS, sizeof(NONCLIENTMETRICS), metrics, 0);
     ASSERT_UNUSED(success, success);
     return success;
 }
--- third_party/WebKit/Source/platform/blink_platform.gypi
+++ third_party/WebKit/Source/platform/blink_platform.gypi
@@ -997,8 +997,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'weborigin/SecurityPolicy.cpp',
       'weborigin/SecurityPolicy.h',
       'win/HWndDC.h',
-      'win/SystemInfo.cpp',
-      'win/SystemInfo.h',
     ],
     'platform_test_files': [
       'ClockTest.cpp',
