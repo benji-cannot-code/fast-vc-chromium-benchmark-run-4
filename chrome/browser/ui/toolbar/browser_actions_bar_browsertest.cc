d6008559a6bfabd44bdd10250703c7244d0f4f2a
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-01-29 12:45:03 -0800

    [Extensions UI Mac] Fix race condition in showing the icon surfacing bubble
    The toolbar icon surfacing bubble shows up the first time the user runs the
    browser with the --extension-action-redesign feature.  There's a race condition
    in the mac code where if the browser window starts up before the extension
    system is ready, the code incorrectly infers that there are no new extensions,
    and doesn't show the bubble.  Fix this, and add a test.
    
    BUG=582512
    
    Review URL: https://codereview.chromium.org/1643963003
    
    Cr-Commit-Position: refs/heads/master@{#372410}

--- chrome/browser/ui/toolbar/browser_actions_bar_browsertest.cc
+++ chrome/browser/ui/toolbar/browser_actions_bar_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "base/macros.h"
+#include "base/prefs/pref_service.h"
 #include "base/run_loop.h"
 #include "chrome/browser/extensions/api/extension_action/extension_action_api.h"
 #include "chrome/browser/extensions/browser_action_test_util.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/toolbar/toolbar_action_view_controller.h"
 #include "chrome/browser/ui/toolbar/toolbar_actions_bar.h"
 #include "chrome/browser/ui/toolbar/toolbar_actions_model.h"
+#include "chrome/common/pref_names.h"
 #include "components/crx_file/id_util.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/test/test_utils.h"
@@ -506,4 +508,23 @@ IN_PROC_BROWSER_TEST_F(BrowserActionsBarRedesignBrowserTest,
   browser_actions_bar()->HidePopup();
   content::RunAllBlockingPoolTasksUntilIdle();
   EXPECT_FALSE(browser_actions_bar()->HasPopup());
+}
+
+// Tests that the browser actions container correctly highlights for displaying
+// the icon surfacing bubble.
+IN_PROC_BROWSER_TEST_F(BrowserActionsBarRedesignBrowserTest,
+                       PRE_HighlightsForExtensionIconSurfacingBubble) {
+  // Add a new extension and clear the pref for the bubble being acknowledged.
+  base::FilePath path = PackExtension(test_data_dir_.AppendASCII("api_test")
+                                          .AppendASCII("page_action")
+                                          .AppendASCII("simple"));
+  InstallExtensionFromWebstore(path, 1);
+  profile()->GetPrefs()->ClearPref(
+      prefs::kToolbarIconSurfacingBubbleAcknowledged);
+}
+
+IN_PROC_BROWSER_TEST_F(BrowserActionsBarRedesignBrowserTest,
+                       HighlightsForExtensionIconSurfacingBubble) {
+  // The toolbar should be highlighting for the bubble.
+  EXPECT_TRUE(browser_actions_bar()->IsHighlightingForSurfacingBubble());
 }
