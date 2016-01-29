de6c22afa1561c5601bdbfc05ed15f65867df6a2
mseaborn <mseaborn@chromium.org>
2016-01-29 14:15:06 -0800

    NaCl cleanup: Remove now-unneeded initialisation of NaCl libraries (2nd try)
    The renderer now no longer uses these NaCl libraries, so we don't need
    to initialise them on startup in module_ppapi.cc.  This leaves
    UrandomFD() unused, so we can remove it too.
    
    This means that, on Linux, the chrome executable no longer pulls in
    any code that uses thread-local (TLS) variables (those declared with
    "__thread").  This means that it no longer references the function
    __tls_get_addr() (or ___tls_get_addr() on i386), which is defined by
    ld-linux.so with symbol version "GLIBC_2.3".  This means we must
    update the symbol version dependency expectations in
    chrome/installer/linux/rpm/expected_deps_*.
    
    For example, nacl_log.c is one source file that uses __thread
    variables on Linux, and that no longer gets linked into chrome now.
    
    Before the change:
    
      $ objdump -x out/Release/chrome
      ...
        required from ld-linux-x86-64.so.2:
          0x09691a75 0x00 47 GLIBC_2.2.5
          0x0d696913 0x00 48 GLIBC_2.3
      ...
      $ nm -D out/Release/chrome | grep tls_get
                       U __tls_get_addr
    
    After the change:
    
      $ objdump -x out/Release/chrome
      ...
        required from ld-linux-x86-64.so.2:
          0x09691a75 0x00 47 GLIBC_2.2.5
      ...
      $ nm -D out/Release/chrome | grep tls_get
    
    This is a retry of https://codereview.chromium.org/1646733002/, which
    got reverted because the symbol version expectations changed.
    
    BUG= https://bugs.chromium.org/p/nativeclient/issues/detail?id=2832
    TEST=e.g. NaClBrowserTestPnacl.PPAPICore (tests PNaCl translation)
    
    Review URL: https://codereview.chromium.org/1651583002
    
    Cr-Commit-Position: refs/heads/master@{#372437}

--- components/nacl/renderer/ppb_nacl_private_impl.cc
+++ components/nacl/renderer/ppb_nacl_private_impl.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/macros.h"
-#include "base/rand_util.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
@@ -619,14 +618,6 @@ PP_Bool StartPpapiProxy(PP_Instance instance) {
                                   "could not create instance.");
   }
   return PP_FALSE;
-}
-
-int UrandomFD(void) {
-#if defined(OS_POSIX)
-  return base::GetUrandomFD();
-#else
-  return -1;
-#endif
 }
 
 // Convert a URL to a filename for GetReadonlyPnaclFd.
@@ -1701,7 +1692,6 @@ void StreamPexe(PP_Instance instance,
 
 const PPB_NaCl_Private nacl_interface = {
   &LaunchSelLdr,
-  &UrandomFD,
   &GetReadExecPnaclFd,
   &CreateTemporaryFile,
   &GetNumberOfProcessors,
