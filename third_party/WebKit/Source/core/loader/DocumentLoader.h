bcf36ea5f4ccbb5059bd2616fcba33c8645dd036
japhet <japhet@chromium.org>
2016-01-29 10:36:59 -0800

    Simpify redirect handling in DocumentLoader
    DocumentLoader::willSendRequest() does a bunch of redirect-specific work,
    and a single security check that is done for non-redirect requests as
    well. Move the general security check to
    FrameLoader::shouldContinueForNavigationPolicy, and merge the remainder of
    willSendRequest in to DocumentLoader::redirectReceived.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1637613002
    
    Cr-Commit-Position: refs/heads/master@{#372384}

--- third_party/WebKit/Source/core/loader/DocumentLoader.h
+++ third_party/WebKit/Source/core/loader/DocumentLoader.h
@@ -174,7 +174,6 @@ private:
 
     void prepareSubframeArchiveLoadIfNeeded();
 
-    void willSendRequest(ResourceRequest&, const ResourceResponse&);
     void finishedLoading(double finishTime);
     void mainReceivedError(const ResourceError&);
     void cancelLoadAfterXFrameOptionsOrCSPDenied(const ResourceResponse&);
