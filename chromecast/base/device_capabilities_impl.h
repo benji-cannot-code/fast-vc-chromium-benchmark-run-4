40775a83fe42ac38576a01a9ca42fb66dcb503f2
jyw <jyw@chromium.org>
2016-01-28 17:44:52 -0800

    [Chromecast] Log sample rate changes and tweak audio logging output.
    Also fixed up some cpplint warnings in
    chromecast/base/device_capabilities_impl{.h,_unittest.cc}
    
    BUG=NONE
    TEST=Built and ran code; cast_base_unittests
    
    Review URL: https://codereview.chromium.org/1642143003
    
    Cr-Commit-Position: refs/heads/master@{#372244}

--- chromecast/base/device_capabilities_impl.h
+++ chromecast/base/device_capabilities_impl.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMECAST_BASE_DEVICE_CAPABILITIES_IMPL_H_
 #define CHROMECAST_BASE_DEVICE_CAPABILITIES_IMPL_H_
 
+#include <string>
+
 #include "base/containers/scoped_ptr_hash_map.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
