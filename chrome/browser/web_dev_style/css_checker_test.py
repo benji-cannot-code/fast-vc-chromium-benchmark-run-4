168c925c3c46d9e5b6bea02b3e7a3036ede0586a
dschuyler <dschuyler@chromium.org>
2016-01-28 17:22:20 -0800

    [Template Expressions] changing ${ leader to $i18n{
    This CL changes the Template Expressions from using
    a ${ to identify the start of an expression, to using
    $i18n{.  The intention is to reduce (eliminate) false
    positives when searching for expressions that need
    replacement.  This CL also prepares for the replacement
    work to be done in a background thread and used on a
    broader set of files.
    
    BUG=506009
    
    Committed: https://crrev.com/5aa7fd5f14e49c65ebba87e4248424d622b92930
    Cr-Commit-Position: refs/heads/master@{#371537}
    
    Review URL: https://codereview.chromium.org/1585923011
    
    Cr-Commit-Position: refs/heads/master@{#372239}

--- chrome/browser/web_dev_style/css_checker_test.py
+++ chrome/browser/web_dev_style/css_checker_test.py
@@ -186,8 +186,8 @@ blah /* hey! */
   100% { height: 500px; }
 }
 
-#id { /* ${TemplateExpressions} should be ignored. */
-  rule: ${someValue};
+#id { /* $i18n{TemplateExpressions} should be ignored. */
+  rule: $i18n{someValue};
   --css-mixin: {
     color: red;
   };
