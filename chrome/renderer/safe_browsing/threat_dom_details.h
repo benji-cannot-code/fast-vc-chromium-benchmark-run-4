8510103568818848de60dee04a15b7bcb265e73f
jialiul <jialiul@chromium.org>
2016-01-28 16:38:35 -0800

    Rewrite PhishingClassifierDelegateTest by using RenderViewTest, such that it can run with --site-per-process flag.
    Also resolved the flakiness as a side product.
    
    BUG=373155,568710
    
    Review URL: https://codereview.chromium.org/1585003002
    
    Cr-Commit-Position: refs/heads/master@{#372223}

--- chrome/renderer/safe_browsing/threat_dom_details.h
+++ chrome/renderer/safe_browsing/threat_dom_details.h
@@ -40,7 +40,7 @@ class ThreatDOMDetails : public content::RenderFrameObserver {
   // the RenderFrame.
   explicit ThreatDOMDetails(content::RenderFrame* render_frame);
 
-  // RenderViewObserver implementation.
+  // RenderFrameObserver implementation.
   bool OnMessageReceived(const IPC::Message& message) override;
 
   void OnGetThreatDOMDetails();
