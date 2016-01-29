9442f092c5e5839d410618e979114b48d455f1ce
samli <samli@chromium.org>
2016-01-28 16:40:17 -0800

    Devtools: Add editable keyframes to the styles sidebar pane
    BUG=266276
    
    Review URL: https://codereview.chromium.org/1577723002
    
    Cr-Commit-Position: refs/heads/master@{#372224}

--- third_party/WebKit/LayoutTests/http/tests/inspector-protocol/css-protocol-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/css-protocol-test.js
@@ -148,7 +148,7 @@ InspectorTest.loadAndDumpMatchingRulesForNode = function(nodeId, callback, omitL
 
 InspectorTest.loadAndDumpCSSAnimationsForNode = function(nodeId, callback)
 {
-    InspectorTest.sendCommandOrDie("CSS.getCSSAnimationsForNode", { "nodeId": nodeId }, cssAnimationsLoaded);
+    InspectorTest.sendCommandOrDie("CSS.getMatchedStylesForNode", { "nodeId": nodeId }, cssAnimationsLoaded);
 
     function cssAnimationsLoaded(result)
     {
--- third_party/WebKit/Source/core/inspector/InspectorStyleSheet.h
+++ third_party/WebKit/Source/core/inspector/InspectorStyleSheet.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class CSSKeyframeRule;
 class CSSMediaRule;
 class CSSStyleDeclaration;
 class CSSStyleRule;
@@ -130,6 +131,7 @@ public:
     bool setText(const String&, ExceptionState&) override;
     bool getText(String* result) override;
     RefPtrWillBeRawPtr<CSSStyleRule>  setRuleSelector(const SourceRange&, const String& selector, SourceRange* newRange, String* oldSelector, ExceptionState&);
+    PassRefPtrWillBeRawPtr<CSSKeyframeRule>  setKeyframeKey(const SourceRange&, const String& text, SourceRange* newRange, String* oldText, ExceptionState&);
     PassRefPtrWillBeRawPtr<CSSRule>  setStyleText(const SourceRange&, const String& text, SourceRange* newRange, String* oldSelector, ExceptionState&);
     RefPtrWillBeRawPtr<CSSMediaRule>  setMediaRuleText(const SourceRange&, const String& selector, SourceRange* newRange, String* oldSelector, ExceptionState&);
     RefPtrWillBeRawPtr<CSSStyleRule>  addRule(const String& ruleText, const SourceRange& location, SourceRange* addedRange, ExceptionState&);
@@ -139,6 +141,7 @@ public:
 
     PassRefPtr<TypeBuilder::CSS::CSSStyleSheetHeader> buildObjectForStyleSheetInfo();
     PassRefPtr<TypeBuilder::CSS::CSSRule> buildObjectForRuleWithoutMedia(CSSStyleRule*);
+    PassRefPtr<TypeBuilder::CSS::CSSKeyframeRule> buildObjectForKeyframeRule(CSSKeyframeRule*);
     PassRefPtr<TypeBuilder::CSS::SelectorList> buildObjectForSelectorList(CSSStyleRule*);
 
     PassRefPtr<TypeBuilder::CSS::SourceRange> ruleHeaderSourceRange(CSSRule*);
