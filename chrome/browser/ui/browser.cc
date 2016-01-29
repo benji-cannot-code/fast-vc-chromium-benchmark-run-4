90140f98a1949a13213a264ca8c8446d0a0f77d4
horo <horo@chromium.org>
2016-01-29 02:15:08 -0800

    Show message when "Request app banner" on DevTools is clicked but not enabled.
    Currently "Add To Shelf" feature is not enabled by default on Desktop.
    So we should show an debug message.
    
    BUG=540491
    
    Review URL: https://codereview.chromium.org/1646493004
    
    Cr-Commit-Position: refs/heads/master@{#372320}

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -182,6 +182,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/plugin_service.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host.h"
@@ -1454,6 +1455,11 @@ bool Browser::RequestAppBanner(content::WebContents* web_contents) {
                               web_contents->GetLastCommittedURL(), true);
     return true;
   }
+
+  web_contents->GetMainFrame()->AddMessageToConsole(
+      content::CONSOLE_MESSAGE_LEVEL_DEBUG,
+      "App banners are currently disabled. Please check chrome://flags/#" +
+          std::string(switches::kEnableAddToShelf));
   return false;
 }
 
