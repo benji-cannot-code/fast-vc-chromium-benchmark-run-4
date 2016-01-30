e1c6ef24d07e938c46e1504676460eb9027712ae
pauljensen <pauljensen@chromium.org>
2016-01-29 20:41:52 -0800

    [Cronet] Get Cronet performance test running again
    1. Enable HTTPS for QUIC
      a. Insert a MockCertVerifier to pretend our cert is in root store
      b. Insert HostResolver rules to pretend test.example.com resolves
         to the the IP address of the USB reverse-tether interface
      c. Switch from using libcronet.so to libcronet_test.so as it
         contains several utilities we need (e.g. MockCertVerifier).
         This required making a Cronet testing Java target.
      d. Pass private key and cert to quic_server
      e. Copy cert to device
    2. Call PathUtils.setPrivateDataDirectorySuffix() to avoid crash
    3. Modify proguard options to avoid crbug.com/488192
    4. Make Telemetry happy again
      a. Put basic header in config file
      b. Specify default_chrome_root
    5. Avoid Theme.NoDisplay as it fails with Android Marshmallow
    
    Review URL: https://codereview.chromium.org/1536433002
    
    Cr-Commit-Position: refs/heads/master@{#372532}

--- components/cronet/android/test/src/org/chromium/net/CronetTestUtil.java
+++ components/cronet/android/test/src/org/chromium/net/CronetTestUtil.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.net;
 
+import android.os.ConditionVariable;
+
+import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 
 /**
@@ -12,13 +15,45 @@ import org.chromium.base.annotations.JNINamespace;
  */
 @JNINamespace("cronet")
 public class CronetTestUtil {
+    private static final ConditionVariable sHostResolverBlock = new ConditionVariable();
+
     /**
-     * Start QUIC server on local host.
-     * @return non-zero QUIC server port number on success or 0 if failed.
+     * Registers customized DNS mapping for testing host names used by test servers, namely:
+     * <ul>
+     * <li>{@link QuicTestServer#getServerHost}</li>
+     * <li>{@link NativeTestServer#getSdchURL}</li>'s host
+     * </ul>
+     * @param cronetEngine {@link CronetEngine} that this mapping should apply to.
+     * @param destination host to map to (e.g. 127.0.0.1)
      */
-    public static int startQuicServer() {
-        return nativeStartQuicServer();
+    public static void registerHostResolverProc(CronetEngine cronetEngine, String destination) {
+        long contextAdapter =
+                ((CronetUrlRequestContext) cronetEngine).getUrlRequestContextAdapter();
+        nativeRegisterHostResolverProc(contextAdapter, false, destination);
+        sHostResolverBlock.block();
+        sHostResolverBlock.close();
     }
 
-    private static native int nativeStartQuicServer();
+    /**
+     * Registers customized DNS mapping for testing host names used by test servers.
+     * @param requestFactory {@link HttpUrlRequestFactory} that this mapping should apply to.
+     * @param destination host to map to (e.g. 127.0.0.1)
+     */
+    public static void registerHostResolverProc(
+            HttpUrlRequestFactory requestFactory, String destination) {
+        long contextAdapter = ((ChromiumUrlRequestFactory) requestFactory)
+                                      .getRequestContext()
+                                      .getUrlRequestContextAdapter();
+        nativeRegisterHostResolverProc(contextAdapter, true, destination);
+        sHostResolverBlock.block();
+        sHostResolverBlock.close();
+    }
+
+    @CalledByNative
+    private static void onHostResolverProcRegistered() {
+        sHostResolverBlock.open();
+    }
+
+    private static native void nativeRegisterHostResolverProc(
+            long contextAdapter, boolean isLegacyAPI, String destination);
 }
