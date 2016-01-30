f484444492a393cbd3eb157d028bb37ae9acaacc
erikchen <erikchen@chromium.org>
2016-01-29 16:06:24 -0800

    Revert of Enable attachment brokering of SharedMemoryHandle on Windows. (patchset #4 id:60001 of https://codereview.chromium.org/1588683003/ )
    Reason for revert:
    Causing Dr.Memory errors.
    
    https://code.google.com/p/chromium/issues/detail?id=580636
    
    Original issue's description:
    > Enable attachment brokering of SharedMemoryHandle on Windows.
    >
    > Whenever a SharedMemoryHandle associated with the current process is passed as a
    > parameter to a Chrome IPC message, the object is automatically brokered into the
    > destination process. This means it is no longer necessary to have a constructor
    > of SharedMemoryHandle that takes a ProcessHandle, since the SharedMemoryHandle
    > is always associated with the current process.
    >
    > BUG=493414
    >
    > Committed: https://crrev.com/7ceb8901740c35cd8ebfb92739efaf2ee441391e
    > Cr-Commit-Position: refs/heads/master@{#370193}
    
    TBR=mark@chromium.org,tsepez@chromium.org,thestig@chromium.org,avi@chromium.org,sergeyu@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=493414
    
    Review URL: https://codereview.chromium.org/1645273003
    
    Cr-Commit-Position: refs/heads/master@{#372481}

--- base/memory/shared_memory_handle_win.cc
+++ base/memory/shared_memory_handle_win.cc
@@ -55,7 +55,7 @@ bool SharedMemoryHandle::BelongsToCurrentProcess() const {
 }
 
 bool SharedMemoryHandle::NeedsBrokering() const {
-  return BelongsToCurrentProcess();
+  return false;
 }
 
 HANDLE SharedMemoryHandle::GetHandle() const {
