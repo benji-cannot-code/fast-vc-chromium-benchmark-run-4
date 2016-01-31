e9f2139d2174e3f4c0a6c3e6e36daf5eea9b9f87
mohsen <mohsen@chromium.org>
2016-01-31 10:13:58 -0800

    Reland of Move MenuKeyEventHandler out of MenuMessageLoopAura (patchset #1 id:1 of https://codereview.chromium.org/1649013002/ )
    Reason for revert:
    Relanding as the crash leading to this revert is now fixed: r372567.
    
    Original issue's description:
    > Revert of Move MenuKeyEventHandler out of MenuMessageLoopAura (patchset #4 id:80001 of https://codereview.chromium.org/1649623003/ )
    >
    > Reason for revert:
    > Reverting as this CL is causing a break on the Chromium OS ASan Tests bot:
    > https://build.chromium.org/p/chromium.memory/builders/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/builds/9185/steps/views_unittests/logs/stdio
    >
    > Original issue's description:
    > > Move MenuKeyEventHandler out of MenuMessageLoopAura
    > >
    > > This will allow us to handle key events even when the menu is async and
    > > there is no MenuMessageLoopAura.
    > >
    > > BUG=564255
    > >
    > > Committed: https://crrev.com/bef09e3b688644ee34305e0e8475d90a5ba01c0b
    > > Cr-Commit-Position: refs/heads/master@{#372364}
    >
    > TBR=sky@chromium.org,mohsen@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=564255
    >
    > Committed: https://crrev.com/3c4b22daaa8bc6f5ea7da25af0b7c1a7aa957b09
    > Cr-Commit-Position: refs/heads/master@{#372383}
    
    TBR=sky@chromium.org,joedow@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=564255
    
    Review URL: https://codereview.chromium.org/1651833002
    
    Cr-Commit-Position: refs/heads/master@{#372580}

--- ui/views/controls/menu/menu_controller_unittest.cc
+++ ui/views/controls/menu/menu_controller_unittest.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(USE_AURA)
 #include "ui/aura/scoped_window_targeter.h"
 #include "ui/aura/window.h"
+#include "ui/views/controls/menu/menu_key_event_handler.h"
 #endif
 
 #if defined(USE_X11)
@@ -394,6 +395,10 @@ class MenuControllerTest : public ViewsTestBase {
   }
 
   void RunMenu() {
+#if defined(USE_AURA)
+    scoped_ptr<MenuKeyEventHandler> key_event_handler(new MenuKeyEventHandler);
+#endif
+
     menu_controller_->message_loop_depth_++;
     menu_controller_->RunMessageLoop(false);
     menu_controller_->message_loop_depth_--;
