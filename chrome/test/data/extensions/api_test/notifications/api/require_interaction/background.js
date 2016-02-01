ca1ddff90904c0a73ef4035403c9fab741004848
peter <peter@chromium.org>
2016-02-01 04:52:55 -0800

    Implement the `requireInteraction` property for chrome.notifications
    Notifications will auto-minimize on Chrome OS, or auto-dismiss on the
    desktop platforms after a certain number of seconds since Chrome 47.
    
    Web Notifications have the `requireInteraction` property enabling them
    to stay on screen until the user interacts with them. This CL adds the
    same property to extension notifications as well.
    
    BUG=574763
    
    Review URL: https://codereview.chromium.org/1646013003
    
    Cr-Commit-Position: refs/heads/master@{#372649}

---
+++ chrome/test/data/extensions/api_test/notifications/api/require_interaction/background.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const redDot = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUA' +
+    'AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO' +
+    '9TXL0Y4OHwAAAABJRU5ErkJggg==';
+
+const options = {
+  iconUrl: redDot,
+  title: 'hello',
+  message: 'world',
+  type: 'basic',
+  requireInteraction: true
+};
+
+chrome.notifications.create('test', options, () => chrome.test.sendMessage(''));
--- chrome/test/data/extensions/api_test/notifications/api/user_gesture/background.js
+++ chrome/test/data/extensions/api_test/notifications/api/user_gesture/background.js
@@ -3,22 +3,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-const red_dot = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUA" +
-    "AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO" +
-    "9TXL0Y4OHwAAAABJRU5ErkJggg==";
+const redDot = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUA' +
+    'AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO' +
+    '9TXL0Y4OHwAAAABJRU5ErkJggg==';
 
 function userActionTest() {
-  chrome.test.sendMessage("");
+  chrome.test.sendMessage('');
 }
 
 chrome.notifications.onClicked.addListener(userActionTest);
 chrome.notifications.onButtonClicked.addListener(userActionTest);
 chrome.notifications.onClosed.addListener(userActionTest);
 
-chrome.notifications.create("test", {
-  iconUrl: red_dot,
-  title: "hi",
-  message: "there",
-  type: "basic",
-  buttons: [{title: "Button"}, {title: "Button"}],
-}, function () {});
+const options = {
+  iconUrl: redDot,
+  title: 'hi',
+  message: 'there',
+  type: 'basic',
+  buttons: [{title: 'Button'}, {title: 'Button'}]
+};
+
+chrome.notifications.create('test', options, function() {});
