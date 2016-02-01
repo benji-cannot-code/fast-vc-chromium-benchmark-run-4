bc3e94b56a2991294d70e0d36e3f5106ec3a1474
stanisc <stanisc@chromium.org>
2016-02-01 15:12:56 -0800

    Sync: DTM Null check in ProfileSyncService::ReenableDatatype
    The crash is caused by dereferencing NULL data_type_manager_.
    A null check for data_type_manager_ in ReenableDatatype should
    be consistent with the rest of the code in ProfileSyncService.
    
    BUG=582018
    
    Review URL: https://codereview.chromium.org/1653073002
    
    Cr-Commit-Position: refs/heads/master@{#372806}

--- components/browser_sync/browser/profile_sync_service.cc
+++ components/browser_sync/browser/profile_sync_service.cc
@@ -1012,7 +1012,7 @@ void ProfileSyncService::OnUnrecoverableErrorImpl(
 }
 
 void ProfileSyncService::ReenableDatatype(syncer::ModelType type) {
-  if (!backend_initialized_)
+  if (!backend_initialized_ || !data_type_manager_)
     return;
   data_type_manager_->ReenableType(type);
 }
