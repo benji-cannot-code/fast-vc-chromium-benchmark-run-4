5712eff7f52cf863d3e9598915a292bc70fc8e83
jochen <jochen@chromium.org>
2016-02-01 02:16:45 -0800

    Revert of Remove all descendant views from AXAuraObjCache when a view is removed. (patchset #3 id:40001 of https://codereview.chromium.org/1629403004/ )
    Reason for revert:
    fails on lsan bot: https://build.chromium.org/p/chromium.memory/builders/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/builds/9196/steps/views_unittests/logs/stdio
    
    Original issue's description:
    > Remove all descendant views from AXAuraObjCache when a view is removed.
    >
    > BUG=581426
    >
    > Committed: https://crrev.com/6c4af2f35251aad93cb80f3cfdd4c38d3a616719
    > Cr-Commit-Position: refs/heads/master@{#372453}
    
    TBR=dtseng@chromium.org,sky@chromium.org,dmazzoni@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=581426
    
    Review URL: https://codereview.chromium.org/1655773002
    
    Cr-Commit-Position: refs/heads/master@{#372631}

--- ui/views/accessibility/ax_widget_obj_wrapper.cc
+++ ui/views/accessibility/ax_widget_obj_wrapper.cc
@@ -52,7 +52,7 @@ void AXWidgetObjWrapper::OnWidgetDestroying(Widget* widget) {
 }
 
 void AXWidgetObjWrapper::OnWillRemoveView(Widget* widget, View* view) {
-  AXAuraObjCache::GetInstance()->RemoveViewSubtree(view);
+  AXAuraObjCache::GetInstance()->Remove(view);
 }
 
 }  // namespace views
