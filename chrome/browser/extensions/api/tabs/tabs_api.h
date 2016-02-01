de97de8b2952f1a22a35ddca5288cd3f02754fce
wjmaclean <wjmaclean@chromium.org>
2016-02-01 14:54:30 -0800

    Implement webview.captureVisibleRegion()
    This CL implements  webview.captureVisibleRegion(), an extension/apps
    API to allow WebView users to capture screenshots of the contents
    displayed in a WebView. The surfaces contents capture has been plumbed
    via RenderWidgetHostViewChildFrame so this implementation should not
    require changes when WebView switches to using OOPIF.
    
    As part of the implementation, there are two notable refactors:
    
    1) CaptureWebContentsFunction has been refactored into
    WebContentsCaptureClient to remove the extensions::AsyncExtensionFunction
    dependence so that this code can be used by both tabs.captureVisibleTab
    and webview.captureVisibleRegion, and
    
    2) common code from DelegatedFrameHost has ben moved to
    content/browser/compositor/surface_utils.* in order to avoid duplication
    as both DelegatedFrameHost and RenderWidgetHostViewChildFrame now
    use the code.
    
    Finally, this CL adds a surface-drawn callback to
    RenderWidgetHostViewChildFrame, to allow deferring a
    screen capture request until a frame has actually been
    drawn. This callback can be used to simplify some
    existing tests.
    
    BUG=326755
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review URL: https://codereview.chromium.org/1635513003
    
    Cr-Commit-Position: refs/heads/master@{#372799}

--- chrome/browser/extensions/api/tabs/tabs_api.h
+++ chrome/browser/extensions/api/tabs/tabs_api.h
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ui/zoom/zoom_controller.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
-#include "extensions/browser/api/capture_web_contents_function.h"
 #include "extensions/browser/api/execute_code_function.h"
+#include "extensions/browser/api/web_contents_capture_client.h"
 #include "extensions/common/extension_resource.h"
 #include "extensions/common/user_script.h"
 #include "url/gurl.h"
@@ -196,11 +196,17 @@ class TabsDetectLanguageFunction : public ChromeAsyncExtensionFunction,
   content::NotificationRegistrar registrar_;
   DECLARE_EXTENSION_FUNCTION("tabs.detectLanguage", TABS_DETECTLANGUAGE)
 };
+
 class TabsCaptureVisibleTabFunction
-    : public extensions::CaptureWebContentsFunction {
+    : public extensions::WebContentsCaptureClient,
+      public AsyncExtensionFunction {
  public:
   TabsCaptureVisibleTabFunction();
   static void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
+
+  // ExtensionFunction implementation.
+  bool HasPermission() override;
+  bool RunAsync() override;
 
  protected:
   ~TabsCaptureVisibleTabFunction() override {}
@@ -208,9 +214,11 @@ class TabsCaptureVisibleTabFunction
  private:
   ChromeExtensionFunctionDetails chrome_details_;
 
-  // extensions::CaptureWebContentsFunction:
+  content::WebContents* GetWebContentsForID(int window_id);
+
+  // extensions::WebContentsCaptureClient:
   bool IsScreenshotEnabled() override;
-  content::WebContents* GetWebContentsForID(int id) override;
+  void OnCaptureSuccess(const SkBitmap& bitmap) override;
   void OnCaptureFailure(FailureReason reason) override;
 
   DECLARE_EXTENSION_FUNCTION("tabs.captureVisibleTab", TABS_CAPTUREVISIBLETAB)
--- content/browser/compositor/surface_utils.h
+++ content/browser/compositor/surface_utils.h
@@ -7,8 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_COMPOSITOR_SURFACE_UTILS_H_
 
 #include "base/memory/scoped_ptr.h"
+#include "content/public/browser/readback_types.h"
+#include "third_party/skia/include/core/SkImageInfo.h"
+#include "ui/gfx/geometry/size.h"
 
 namespace cc {
+class CopyOutputResult;
 class SurfaceIdAllocator;
 class SurfaceManager;
 }  // namespace cc
@@ -18,6 +22,12 @@ namespace content {
 scoped_ptr<cc::SurfaceIdAllocator> CreateSurfaceIdAllocator();
 
 cc::SurfaceManager* GetSurfaceManager();
+
+void CopyFromCompositingSurfaceHasResult(
+    const gfx::Size& dst_size_in_pixel,
+    const SkColorType color_type,
+    const ReadbackRequestCallback& callback,
+    scoped_ptr<cc::CopyOutputResult> result);
 
 }  // namespace content
 
