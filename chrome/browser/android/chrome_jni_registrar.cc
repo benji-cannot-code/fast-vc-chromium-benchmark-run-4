0b4b5d45f2e6fc17ae9e2c5ac1094bf7c7285fd8
newt <newt@chromium.org>
2016-02-01 15:49:45 -0800

    Delete the old bookmarks page
    Goodbye to:
     - The non-"enhanced" bookmarks page
       * BookmarksPage
       * BookmarksPageView
       * etc
     - The old UI for adding and editing bookmarks
       * ManageBookmarkActivity
       * AddEditBookmarkFragment
       * SelectBookmarkFolderFragment
       * etc
    
    This has all already been replaced by the "enhanced" bookmarks UI.
    
    BUG=474719
    
    Review URL: https://codereview.chromium.org/1641253005
    
    Cr-Commit-Position: refs/heads/master@{#372820}

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/banners/app_banner_infobar_delegate_android.h"
 #include "chrome/browser/android/banners/app_banner_manager_android.h"
 #include "chrome/browser/android/bookmarks/bookmarks_bridge.h"
-#include "chrome/browser/android/bookmarks/edit_bookmark_helper.h"
 #include "chrome/browser/android/bookmarks/partner_bookmarks_reader.h"
 #include "chrome/browser/android/bottombar/overlay_panel_content.h"
 #include "chrome/browser/android/browsing_data/browsing_data_counter_bridge.h"
@@ -263,7 +262,6 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
      DownloadManagerService::RegisterDownloadManagerService},
     {"DownloadOverwriteInfoBarDelegate",
      RegisterDownloadOverwriteInfoBarDelegate},
-    {"EditBookmarkHelper", RegisterEditBookmarkHelper},
     {"ExternalDataUseObserverBridge", RegisterExternalDataUseObserver},
     {"ExternalPrerenderRequestHandler",
      prerender::ExternalPrerenderHandlerAndroid::
