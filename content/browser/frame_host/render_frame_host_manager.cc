25a3e3936858b5e24af2021cbb3bd8e6a514bd2e
creis <creis@chromium.org>
2016-02-01 13:45:31 -0800

    Diagnose renderer-side crash with browser-side stack trace.
    This will log a crash dump without crashing the browser process, letting
    us see how a main frame proxy is unexpectedly recreated.
    
    BUG=575245
    TEST=Crash dumps in RenderFrameHostManager::InitRenderFrame.
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review URL: https://codereview.chromium.org/1656603002
    
    Cr-Commit-Position: refs/heads/master@{#372772}

--- content/browser/frame_host/render_frame_host_manager.cc
+++ content/browser/frame_host/render_frame_host_manager.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/debug/crash_logging.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
 #include "base/stl_util.h"
 #include "base/trace_event/trace_event.h"
@@ -1883,8 +1885,34 @@ bool RenderFrameHostManager::InitRenderFrame(
   if (existing_proxy) {
     proxy_routing_id = existing_proxy->GetRoutingID();
     CHECK_NE(proxy_routing_id, MSG_ROUTING_NONE);
-    if (!existing_proxy->is_render_frame_proxy_live())
+    if (!existing_proxy->is_render_frame_proxy_live()) {
+      // Calling InitRenderFrameProxy on main frames seems to be causing
+      // https://crbug.com/575245, so track down how this can happen.
+      // TODO(creis): Remove this once we've found the cause.
+      if (SiteIsolationPolicy::IsSwappedOutStateForbidden() &&
+          !frame_tree_node_->parent()) {
+        base::debug::SetCrashKeyValue(
+            "initrf_frame_id",
+            base::IntToString(render_frame_host->GetRoutingID()));
+        base::debug::SetCrashKeyValue("initrf_proxy_id",
+                                      base::IntToString(proxy_routing_id));
+        base::debug::SetCrashKeyValue(
+            "initrf_view_id",
+            base::IntToString(
+                render_frame_host->render_view_host()->GetRoutingID()));
+        base::debug::SetCrashKeyValue(
+            "initrf_main_frame_id",
+            base::IntToString(render_frame_host->render_view_host()
+                                  ->main_frame_routing_id()));
+        base::debug::SetCrashKeyValue(
+            "initrf_view_is_live",
+            render_frame_host->render_view_host()->IsRenderViewLive() ? "yes"
+                                                                      : "no");
+        base::debug::DumpWithoutCrashing();
+      }
+
       existing_proxy->InitRenderFrameProxy();
+    }
   }
 
   return delegate_->CreateRenderFrameForRenderManager(
