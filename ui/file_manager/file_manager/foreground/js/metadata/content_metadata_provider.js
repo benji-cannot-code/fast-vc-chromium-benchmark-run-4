071fd55f96ee792bde9be017ee1bb80b817ed5c5
ryoh <ryoh@chromium.org>
2016-01-31 20:17:23 -0800

    we no nonger need watchdog timer
    BUG=579367
    
    Review URL: https://codereview.chromium.org/1642373002
    
    Cr-Commit-Position: refs/heads/master@{#372599}

--- ui/file_manager/file_manager/foreground/js/metadata/content_metadata_provider.js
+++ ui/file_manager/file_manager/foreground/js/metadata/content_metadata_provider.js
@@ -64,14 +64,6 @@ ContentMetadataProvider.PROPERTY_NAMES = [
   'mediaTitle'
 ];
 
-
-/**
- * Watchdog timer considers there may be an error
- * if chrome.mediaGalleries.getMetadata does not responsed.
- * @const {number}
- */
-ContentMetadataProvider.MEDIA_GALLERIES_WATCHDOG_TIMEOUT = 1000; // [msec]
-
 /**
  * Path of a worker script.
  * @public {string}
@@ -175,11 +167,6 @@ ContentMetadataProvider.prototype.getFromMediaGalleries_ =
       if (names.indexOf('contentThumbnailUrl') !== -1) {
         metadataType = 'all';
       }
-      setTimeout(function() {
-        resolve(self.createError_(entry.toURL(),
-            'parsing metadata',
-            'chrome.mediaGalleries does not respond.'));
-      }, ContentMetadataProvider.MEDIA_GALLERIES_WATCHDOG_TIMEOUT);
       chrome.mediaGalleries.getMetadata(blob, {metadataType: metadataType},
           function(metadata) {
             self.convertMediaMetadataToMetadataItem_(entry, metadata)
