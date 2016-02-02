d641f34d00210c1011fd24bb1b2ee89e73237de0
azurewei <azurewei@chromium.org>
2016-02-01 20:40:26 -0800

    Implement chrome.input.ime.onCompositionBoundsChanged() API.
    BUG=517773
    TEST=None
    
    Review URL: https://codereview.chromium.org/1614273004
    
    Cr-Commit-Position: refs/heads/master@{#372888}

--- ui/base/ime/input_method_base.h
+++ ui/base/ime/input_method_base.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_BASE_IME_INPUT_METHOD_BASE_H_
 #define UI_BASE_IME_INPUT_METHOD_BASE_H_
 
+#include <vector>
+
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
@@ -84,6 +86,9 @@ class UI_BASE_IME_EXPORT InputMethodBase
   // Convenience method to notify all observers of CaretBounds changes on
   // |client| which is the text input client with focus.
   void NotifyTextInputCaretBoundsChanged(const TextInputClient* client);
+
+  // Gets the bounds of the composition text or cursor in |client|.
+  std::vector<gfx::Rect> GetCompositionBounds(const TextInputClient* client);
 
  private:
   void SetFocusedTextInputClientInternal(TextInputClient* client);
