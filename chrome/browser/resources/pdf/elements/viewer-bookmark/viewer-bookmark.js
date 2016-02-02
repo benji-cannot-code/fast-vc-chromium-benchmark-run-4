7d0025774b08bfa5f6387f7030f68cf40d60784e
tsergeant <tsergeant@chromium.org>
2016-02-01 18:14:05 -0800

    PDF: Allow external navigation links in PDF bookmarks
    Adds support for table of contents entries which point to external URIs.
    These bookmarks will open the link in a new tab when clicked.
    
    BUG=581290
    
    Review URL: https://codereview.chromium.org/1654683002
    
    Cr-Commit-Position: refs/heads/master@{#372862}

--- chrome/browser/resources/pdf/elements/viewer-bookmark/viewer-bookmark.js
+++ chrome/browser/resources/pdf/elements/viewer-bookmark/viewer-bookmark.js
@@ -66,6 +66,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     onClick: function() {
       if (this.bookmark.hasOwnProperty('page'))
         this.fire('change-page', {page: this.bookmark.page});
+      else if (this.bookmark.hasOwnProperty('uri'))
+        this.fire('navigate', {uri: this.bookmark.uri, newtab: true});
     },
 
     onEnter_: function(e) {
