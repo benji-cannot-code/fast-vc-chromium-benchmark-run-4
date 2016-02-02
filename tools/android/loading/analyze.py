863c3488513ca5bc8e15a31d5ed45dfbf8bb1d36
mattcary <mattcary@chromium.org>
2016-02-02 06:38:38 -0800

    Loading dependency analysis
    This defines a Lens that ads synthetic load events from the trace, and
    correlates them with request events via timestamp. This hasn't been
    sucessful in identifying the cause of long edges.
    
    This CL also does some refactoring to the loading model which I think is
    ultimately useful and should be retained. The DOT drawing has been
    pulled out into its own class and unittested, for a certain very limited
    concept of unittesting.
    
    Review URL: https://codereview.chromium.org/1641203002
    
    Cr-Commit-Position: refs/heads/master@{#372939}

--- tools/android/loading/analyze.py
+++ tools/android/loading/analyze.py
@@ -27,8 +27,10 @@ from pylib import constants
 
 import content_classification_lens
 import device_setup
+import frame_load_lens
 import loading_model
 import loading_trace
+import model_graph
 import trace_recorder
 
 
@@ -156,7 +158,8 @@ def _ProcessRequests(filename, ad_rules_filename='',
     content_lens = (
         content_classification_lens.ContentClassificationLens.WithRulesFiles(
             trace, ad_rules_filename, tracking_rules_filename))
-    return loading_model.ResourceGraph(trace, content_lens)
+    frame_lens = frame_load_lens.FrameLoadLens(trace)
+    return loading_model.ResourceGraph(trace, content_lens, frame_lens)
 
 
 def InvalidCommand(cmd):
@@ -189,7 +192,6 @@ def DoPng(arg_str):
   parser.add_argument('request_json')
   parser.add_argument('png_output', nargs='?')
   parser.add_argument('--eog', action='store_true')
-  parser.add_argument('--highlight')
   parser.add_argument('--noads', action='store_true')
   parser.add_argument('--ad_rules', default='')
   parser.add_argument('--tracking_rules', default='')
@@ -198,10 +200,9 @@ def DoPng(arg_str):
       args.request_json, args.ad_rules, args.tracking_rules)
   if args.noads:
     graph.Set(node_filter=graph.FilterAds)
+  visualization = model_graph.GraphVisualization(graph)
   tmp = tempfile.NamedTemporaryFile()
-  graph.MakeGraphviz(
-      tmp,
-      highlight=args.highlight.split(',') if args.highlight else None)
+  visualization.OutputDot(tmp)
   tmp.flush()
   png_output = args.png_output
   if not png_output:
