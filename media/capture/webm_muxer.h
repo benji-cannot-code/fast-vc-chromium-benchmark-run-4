35194e6b30a42a919de5fb869a3be399a361b5a9
mcasas <mcasas@chromium.org>
2016-02-01 18:00:44 -0800

    MediaRecorder: make WebmMuxer hold on to all video frames while waiting for audio
    Currently WebmMuxer holds on to the last encoded video key frame
    while waiting for audio to arrive. This causes corruption on the
    field if there is a large distance between the keyframe and the
    non-keyframe where recording is started.
    
    This CL holds on to all encoded video frames while waiting for
    audio. To avoid this buffering from getting out of control, the
    std::queue used to store the encoded data is flushed on key frame
    arrival.
    
    Unittests updated.
    
    BUG=582165, 547948
    
    Review URL: https://codereview.chromium.org/1659443003
    
    Cr-Commit-Position: refs/heads/master@{#372855}

--- media/capture/webm_muxer.h
+++ media/capture/webm_muxer.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <deque>
+
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/numerics/safe_math.h"
@@ -116,9 +118,22 @@ class MEDIA_EXPORT WebmMuxer : public NON_EXPORTED_BASE(mkvmuxer::IMkvWriter) {
   // The MkvMuxer active element.
   mkvmuxer::Segment segment_;
 
-  // Save the most recent video keyframe if we're waiting for audio to come in.
-  scoped_ptr<std::string> most_recent_encoded_video_keyframe_;
-  base::TimeTicks saved_keyframe_timestamp_;
+  // Hold on to all encoded video frames to dump them with and when audio is
+  // received, if expected, since WebM headers can only be written once.
+  struct EncodedVideoFrame {
+    EncodedVideoFrame(scoped_ptr<std::string> data,
+                      base::TimeTicks timestamp,
+                      bool is_keyframe);
+    ~EncodedVideoFrame();
+
+    scoped_ptr<std::string> data;
+    base::TimeTicks timestamp;
+    bool is_keyframe;
+
+   private:
+    DISALLOW_IMPLICIT_CONSTRUCTORS(EncodedVideoFrame);
+  };
+  std::deque<scoped_ptr<EncodedVideoFrame>> encoded_frames_queue_;
 
   DISALLOW_COPY_AND_ASSIGN(WebmMuxer);
 };
