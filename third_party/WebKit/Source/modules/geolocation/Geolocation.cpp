2b0eaa4523c8f587c30c7df281a276363959a284
jww <jww@chromium.org>
2016-02-01 16:42:03 -0800

    Update geolocation over HTTP error to use PERMISSION_DENIED
    When geolocation is used over HTTP, we had been using
    POSITION_UNAVAILABLE, but it was pointed out that using geolocation over
    a file:// URL uses PERMISSION_DENIED instead. To be consistent, this
    updates the HTTP failure to use PERMISSION_DENIED.
    
    BUG=561641
    
    Review URL: https://codereview.chromium.org/1642903005
    
    Cr-Commit-Position: refs/heads/master@{#372841}

--- third_party/WebKit/Source/modules/geolocation/Geolocation.cpp
+++ third_party/WebKit/Source/modules/geolocation/Geolocation.cpp
@@ -198,7 +198,7 @@ void Geolocation::startRequest(GeoNotifier *notifier)
     recordOriginTypeAccess();
     String errorMessage;
     if (!frame()->settings()->allowGeolocationOnInsecureOrigins() && !executionContext()->isSecureContext(errorMessage)) {
-        notifier->setFatalError(PositionError::create(PositionError::POSITION_UNAVAILABLE, errorMessage));
+        notifier->setFatalError(PositionError::create(PositionError::PERMISSION_DENIED, errorMessage));
         return;
     }
 
@@ -207,7 +207,7 @@ void Geolocation::startRequest(GeoNotifier *notifier)
         Element* owner = document()->ownerElement();
         if (owner && owner->hasAttribute(HTMLNames::permissionsAttr)) {
             String errorMessage = "A cross-origin iframe needs its permissions attribute properly set in order to use the geolocation API.";
-            notifier->setFatalError(PositionError::create(PositionError::POSITION_UNAVAILABLE, errorMessage));
+            notifier->setFatalError(PositionError::create(PositionError::PERMISSION_DENIED, errorMessage));
             return;
         }
     }
