479368b22fbe53f8e571b1ce2c7a1b51b26ceeb3
slan <slan@chromium.org>
2016-02-02 14:18:14 -0800

    Correct __UCLIBC__ build guards in stack_trace_posix.cc
    __UCLIBC__ build guards were implemented here:
      https://codereview.chromium.org/228753003
    
    Note that there are several functions in the anonymous namespace not
    called when building with __UCLIBC__. Do not define these functions for
    these builds, as to avoid passing -Wno-unused-function
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1661593002
    
    Cr-Commit-Position: refs/heads/master@{#373054}

--- base/debug/stack_trace_posix.cc
+++ base/debug/stack_trace_posix.cc
@@ -125,6 +125,7 @@ class BacktraceOutputHandler {
   virtual ~BacktraceOutputHandler() {}
 };
 
+#if !defined(__UCLIBC__)
 void OutputPointer(void* pointer, BacktraceOutputHandler* handler) {
   // This should be more than enough to store a 64-bit number in hex:
   // 16 hex digits + 1 for null-terminator.
@@ -172,7 +173,7 @@ void ProcessBacktrace(void *const *trace,
 
     handler->HandleOutput("\n");
   }
-#elif !defined(__UCLIBC__)
+#else
   bool printed = false;
 
   // Below part is async-signal unsafe (uses malloc), so execute it only
@@ -201,6 +202,7 @@ void ProcessBacktrace(void *const *trace,
   }
 #endif  // defined(USE_SYMBOLIZE)
 }
+#endif  // !defined(__UCLIBC__)
 
 void PrintToStderr(const char* output) {
   // NOTE: This code MUST be async-signal safe (it's used by in-process
