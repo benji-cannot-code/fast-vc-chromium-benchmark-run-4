dd8fe9923c76acc582b3001b899d343c9c984e71
xiaochengh <xiaochengh@chromium.org>
2016-02-02 01:49:10 -0800

    Introduce ForwardsTextBuffer and BackwardsTextBuffer
    The two new classes wrap |WTF::Vector| such that, |ForwardsTextBuffer|
    allows efficients appending, while |BackwardsTextBuffer| allows efficient
    prepending, respectively.
    
    This is part of a bigger CL [1], which is in turn part of another even
    bigger CL [2].
    
    [1] https://codereview.chromium.org/1647483003
    [2] https://codereview.chromium.org/1604783002
    
    BUG=n/a
    TEST=webkit_unit_tests --gtest_filter=*TextBufferTest.*
    
    Review URL: https://codereview.chromium.org/1650543002
    
    Cr-Commit-Position: refs/heads/master@{#372918}

---
+++ third_party/WebKit/Source/core/editing/iterators/BackwardsTextBuffer.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BackwardsTextBuffer_h
+#define BackwardsTextBuffer_h
+
+#include "core/editing/iterators/TextBufferBase.h"
+
+namespace blink {
+
+class CORE_EXPORT BackwardsTextBuffer final : public TextBufferBase {
+    STACK_ALLOCATED();
+    WTF_MAKE_NONCOPYABLE(BackwardsTextBuffer);
+public:
+    BackwardsTextBuffer() {}
+    const UChar* data() const override;
+
+private:
+    UChar* calcDestination(size_t length) override;
+    void shiftData(size_t oldCapacity) override;
+};
+
+} // namespace blink
+
+#endif // TextBuffer_h
---
+++ third_party/WebKit/Source/core/editing/iterators/ForwardsTextBuffer.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ForwardsTextBuffer_h
+#define ForwardsTextBuffer_h
+
+#include "core/editing/iterators/TextBufferBase.h"
+
+namespace blink {
+
+class CORE_EXPORT ForwardsTextBuffer final : public TextBufferBase {
+    STACK_ALLOCATED();
+    WTF_MAKE_NONCOPYABLE(ForwardsTextBuffer);
+public:
+    ForwardsTextBuffer() {}
+    const UChar* data() const override;
+
+private:
+    UChar* calcDestination(size_t length) override;
+};
+
+} // namespace blink
+
+#endif // TextBuffer_h
