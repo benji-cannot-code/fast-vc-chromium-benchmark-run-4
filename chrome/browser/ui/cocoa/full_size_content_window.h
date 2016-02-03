a26685794a2ab76c27fed04f518db7def533cfea
spqchan <spqchan@chromium.org>
2016-02-03 13:07:02 -0800

    Ensure popups are layout correctly on OSX after exiting fullscreen Fix an edge case in which the FullSizeContentWindow does not use a chromeWindowView to replace its contentView.
    BUG=583518, 579953
    
    Review URL: https://codereview.chromium.org/1659323004
    
    Cr-Commit-Position: refs/heads/master@{#373338}

--- chrome/browser/ui/cocoa/full_size_content_window.h
+++ chrome/browser/ui/cocoa/full_size_content_window.h
@@ -36,9 +36,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                               defer:(BOOL)deferCreation
              wantsViewsOverTitlebar:(BOOL)wantsViewsOverTitlebar;
 
-// Forces the contentView to resize to the given size. This need to be forced
-// because by default, the contentView will always have the same size as the
-// window.
+// Forces |chromeWindowView_| to resize to the given size. This need to be
+// forced because by default, the contentView will always have the same size
+// as the window. If |chromeWindowView_| is empty, we will set the frame of
+// [window contentView] to the given frame.
 - (void)forceContentViewFrame:(NSRect)frame;
 
 @end
