b60a9643fd174d2ed983aaeb9a55556deb36ccbe
lgarron <lgarron@chromium.org>
2016-02-02 20:57:53 -0800

    Add an HPKP report URI for Twitter.
    BUG=
    TBR=agl@chromium.org
    
    Review URL: https://codereview.chromium.org/1665623002
    
    Cr-Commit-Position: refs/heads/master@{#373177}

--- net/http/transport_security_state_static.h
+++ net/http/transport_security_state_static.h
@@ -614,6 +614,7 @@ static const char* const kTwitterComAcceptableCerts[] = {
   kSPKIHash_Twitter1,
   NULL,
 };
+static const char kTwitterComReportURI[] = "http://l.twimg.com/i/hpkp_report";
 static const char* const kTwitterCDNAcceptableCerts[] = {
   kSPKIHash_VeriSignClass1,
   kSPKIHash_VeriSignClass3_G4,
@@ -659,6 +660,7 @@ static const char* const kTwitterCDNAcceptableCerts[] = {
   kSPKIHash_GlobalSignRootCA_R3,
   NULL,
 };
+static const char kTwitterCDNReportURI[] = "http://l.twimg.com/i/hpkp_report";
 static const char* const kDropboxAcceptableCerts[] = {
   kSPKIHash_DigiCertAssuredIDRoot,
   kSPKIHash_DigiCertGlobalRoot,
@@ -724,8 +726,8 @@ static const struct Pinset kPinsets[] = {
   {kTestAcceptableCerts, kNoRejectedPublicKeys, kNoReportURI},
   {kGoogleAcceptableCerts, kNoRejectedPublicKeys, kGoogleReportURI},
   {kTorAcceptableCerts, kNoRejectedPublicKeys, kNoReportURI},
-  {kTwitterComAcceptableCerts, kNoRejectedPublicKeys, kNoReportURI},
-  {kTwitterCDNAcceptableCerts, kNoRejectedPublicKeys, kNoReportURI},
+  {kTwitterComAcceptableCerts, kNoRejectedPublicKeys, kTwitterComReportURI},
+  {kTwitterCDNAcceptableCerts, kNoRejectedPublicKeys, kTwitterCDNReportURI},
   {kDropboxAcceptableCerts, kNoRejectedPublicKeys, kDropboxReportURI},
   {kFacebookAcceptableCerts, kNoRejectedPublicKeys, kNoReportURI},
   {kSpideroakAcceptableCerts, kNoRejectedPublicKeys, kNoReportURI},
