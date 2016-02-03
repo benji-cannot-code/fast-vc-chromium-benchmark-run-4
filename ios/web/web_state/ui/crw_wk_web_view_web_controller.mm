8512ee5f8569e975721a5a8109b35c9e2ae73be0
eugenebut <eugenebut@chromium.org>
2016-02-02 16:26:36 -0800

    [ios] Support for NTLM authentication method.
    BUG=581721
    
    Review URL: https://codereview.chromium.org/1646483003
    
    Cr-Commit-Position: refs/heads/master@{#373098}

--- ios/web/web_state/ui/crw_wk_web_view_web_controller.mm
+++ ios/web/web_state/ui/crw_wk_web_view_web_controller.mm
@@ -1207,6 +1207,7 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
   NSURLProtectionSpace* space = challenge.protectionSpace;
   DCHECK(
       [space.authenticationMethod isEqual:NSURLAuthenticationMethodHTTPBasic] ||
+      [space.authenticationMethod isEqual:NSURLAuthenticationMethodNTLM] ||
       [space.authenticationMethod isEqual:NSURLAuthenticationMethodHTTPDigest]);
 
   SEL selector = @selector(webController:
@@ -1927,6 +1928,7 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
                                   NSURLCredential*))completionHandler {
   NSString* authMethod = challenge.protectionSpace.authenticationMethod;
   if ([authMethod isEqual:NSURLAuthenticationMethodHTTPBasic] ||
+      [authMethod isEqual:NSURLAuthenticationMethodNTLM] ||
       [authMethod isEqual:NSURLAuthenticationMethodHTTPDigest]) {
     [self handleHTTPAuthForChallenge:challenge
                    completionHandler:completionHandler];
