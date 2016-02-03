bd84119edd6bb335572a8a5480ec49483fb009df
msramek <msramek@chromium.org>
2016-02-03 01:29:43 -0800

    Change the CBD dialog on Android to a PreferenceFragment
    According to the mocks (https://folio.googleplex.com/clear-browsing-data/Dialog%20-%20Android/V1#%2F00%20-%20Default%20Selection.png), the CBD dialog should look like a standard preferences page.
    
    Therefore, in this CL, we:
    1. Make ClearBrowsingDataDialogFragment inherit from PreferencesFragment instead of DialogFragment
    2. Following that change, replace CheckedTextView with CheckboxPreference, Button with ButtonPreference etc.
    3. The dialog layout is now defined in an XML file instead of being generating through an Adapter.
    4. Every ClearBrowsingDataDialogFragment.Item now corresponds to a CheckboxPreference, and thus it can delegate most of its logic (e.g. checked state, enabled state etc.) to that CheckboxPreference.
    
    Furthermore, to match the mocks, we:
    5. Change the support string at the bottom of the dialog according to the mocks, and add a "Learn more" link.
    
    What is still different from the mocks and will be done in followup CLs (as they require separate Preference subclasses):
    6. The time range combobox is not yet added
    7. The support string is not of the correct color.
    8. CheckboxPreference-s still have 1px border on top and bottom.
    
    BUG=569870
    
    Review URL: https://codereview.chromium.org/1618413002
    
    Cr-Commit-Position: refs/heads/master@{#373203}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/ConfirmAccountChangeFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/ConfirmAccountChangeFragment.java
@@ -9,6 +9,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.text.SpannableString;
@@ -21,7 +22,9 @@ import android.widget.TextView;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
-import org.chromium.chrome.browser.sync.ui.ClearSyncDataDialogFragment;
+import org.chromium.chrome.browser.preferences.Preferences;
+import org.chromium.chrome.browser.preferences.PreferencesLauncher;
+import org.chromium.chrome.browser.sync.ui.ClearSyncDataPreferences;
 import org.chromium.signin.InvestigatedScenario;
 import org.chromium.ui.text.SpanApplier;
 import org.chromium.ui.text.SpanApplier.SpanInfo;
@@ -83,7 +86,7 @@ class ConfirmAccountChangeFragment
                 message, new SpanInfo("<link>", "</link>", new ClickableSpan() {
                     @Override
                     public void onClick(View widget) {
-                        showClearSyncDataDialogFragment();
+                        showClearSyncDataPreferences();
                     }
                 }));
 
@@ -109,9 +112,11 @@ class ConfirmAccountChangeFragment
         }
     }
 
-    private void showClearSyncDataDialogFragment() {
-        ClearSyncDataDialogFragment dialogFragment = new ClearSyncDataDialogFragment();
-        dialogFragment.show(getFragmentManager(), null);
+    private void showClearSyncDataPreferences() {
+        Preferences prefActivity = (Preferences) getActivity();
+        Intent intent = PreferencesLauncher.createIntentForSettingsPage(prefActivity,
+                ClearSyncDataPreferences.class.getName());
+        prefActivity.startActivity(intent);
 
         // Cancel out of current sign in.
         SigninManager.get(getActivity()).cancelSignIn();
--- chrome/android/javatests/src/org/chromium/chrome/browser/HistoryUITest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/HistoryUITest.java
@@ -5,17 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser;
 
-import android.app.Dialog;
-import android.support.v7.app.AlertDialog;
+import android.preference.PreferenceScreen;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.util.JsonReader;
-import android.widget.Button;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Feature;
+import org.chromium.chrome.browser.preferences.ButtonPreference;
 import org.chromium.chrome.browser.preferences.Preferences;
-import org.chromium.chrome.browser.preferences.privacy.ClearBrowsingDataDialogFragment;
+import org.chromium.chrome.browser.preferences.privacy.ClearBrowsingDataPreferences;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabObserver;
@@ -27,7 +26,6 @@ import org.chromium.content.browser.test.util.CallbackHelper;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
 import org.chromium.content.browser.test.util.JavaScriptUtils;
-import org.chromium.content.browser.test.util.TestTouchUtils;
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -229,23 +227,9 @@ public class HistoryUITest extends ChromeActivityTestCaseBase<ChromeActivity> {
                 });
         assertNotNull("Could not find the preferences activity", prefActivity);
 
-        final ClearBrowsingDataDialogFragment clearBrowsingFragment =
-                ActivityUtils.waitForFragment(
-                        prefActivity, ClearBrowsingDataDialogFragment.FRAGMENT_TAG);
+        final ClearBrowsingDataPreferences clearBrowsingFragment =
+                (ClearBrowsingDataPreferences) prefActivity.getFragmentForTest();
         assertNotNull("Could not find clear browsing data fragment", clearBrowsingFragment);
-
-        Dialog dialog = clearBrowsingFragment.getDialog();
-        final Button clearButton = ((AlertDialog) dialog).getButton(
-                AlertDialog.BUTTON_POSITIVE);
-        assertNotNull("Could not find Clear button.", clearButton);
-
-        TestTouchUtils.performClickOnMainSync(getInstrumentation(), clearButton);
-        CriteriaHelper.pollForUIThreadCriteria(new Criteria("Clear browsing dialog never hidden") {
-            @Override
-            public boolean isSatisfied() {
-                return !clearBrowsingFragment.isVisible();
-            }
-        });
 
         final ChromeActivity mainActivity = ActivityUtils.waitForActivity(
                 getInstrumentation(), getActivity().getClass(), new Runnable() {
@@ -254,7 +238,13 @@ public class HistoryUITest extends ChromeActivityTestCaseBase<ChromeActivity> {
                         ThreadUtils.runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                prefActivity.finish();
+                                PreferenceScreen screen =
+                                        clearBrowsingFragment.getPreferenceScreen();
+                                ButtonPreference clearButton =
+                                        (ButtonPreference) screen.findPreference(
+                                              ClearBrowsingDataPreferences.PREF_CLEAR_BUTTON);
+                                clearButton.getOnPreferenceClickListener().onPreferenceClick(
+                                        clearButton);
                             }
                         });
                     }
@@ -263,7 +253,8 @@ public class HistoryUITest extends ChromeActivityTestCaseBase<ChromeActivity> {
         CriteriaHelper.pollForUIThreadCriteria(new Criteria("Main tab never restored") {
             @Override
             public boolean isSatisfied() {
-                return mainActivity.getActivityTab() != null
+                return !clearBrowsingFragment.isVisible()
+                        && mainActivity.getActivityTab() != null
                         && !mainActivity.getActivityTab().isFrozen();
             }
         });
