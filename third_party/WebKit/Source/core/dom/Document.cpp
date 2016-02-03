1bce26aea84d53f360259e8c3d2d5c895a7aab89
schenney <schenney@chromium.org>
2016-02-03 09:41:07 -0800

    Revert of Fix a crash in Document::setFocusedElement(). (patchset #2 id:20001 of https://codereview.chromium.org/1657573002/ )
    Reason for revert:
    Evidence suggests that this patch is causing Document leaks:
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Leak/builds/17250
    
    Original issue's description:
    > Fix a crash in Document::setFocusedElement().
    >
    > HTMLFrameOwnerElement::setFocus() can dispatch events, and event handlers can
    > update Document::m_focusedElement.
    >
    > BUG=536890
    > TEST=automated
    >
    > Committed: https://crrev.com/b6be4584b44376ba30951d03e78d683606964b26
    > Cr-Commit-Position: refs/heads/master@{#372625}
    
    TBR=kochi@chromium.org,tkent@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=536890
    
    Review URL: https://codereview.chromium.org/1661003002
    
    Cr-Commit-Position: refs/heads/master@{#373268}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -3648,11 +3648,6 @@ bool Document::setFocusedElement(PassRefPtrWillBeRawPtr<Element> prpNewFocusedEl
         m_focusedElement = newFocusedElement;
 
         m_focusedElement->setFocus(true);
-        // Element::setFocus for frames can dispatch events.
-        if (m_focusedElement != newFocusedElement) {
-            focusChangeBlocked = true;
-            goto SetFocusedElementDone;
-        }
         cancelFocusAppearanceUpdate();
         m_focusedElement->updateFocusAppearance(params.selectionBehavior);
 
