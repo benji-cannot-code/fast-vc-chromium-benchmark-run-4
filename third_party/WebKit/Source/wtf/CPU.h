332bf7f1ecf160475ca7efba2cb97f6d5e2e0e90
suzyh <suzyh@chromium.org>
2016-02-03 15:59:59 -0800

    Remove APPLE_ARMV7S-specific code from Blink.
    Blink includes some code that is specific to the ARMv7s architecture,
    intended for Apple devices, that predates the fork from WebKit. Blink is
    not built for iOS devices and this code path is not tested.
    
    This patch removes all references to the "CPU(APPLE_ARMV7S)" macro
    (and equivalent "WTF_CPU_APPLE_ARMV7S") from the code, along with all
    code gated by this macro. It does not, however, remove other references
    to "defined(__ARM_ARCH_7S__)".
    
    For more background, see
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/Z_ZPn1_Q5Z0/discussion
    
    BUG=477892
    
    Review URL: https://codereview.chromium.org/1666613002
    
    Cr-Commit-Position: refs/heads/master@{#373393}

--- third_party/WebKit/Source/wtf/CPU.h
+++ third_party/WebKit/Source/wtf/CPU.h
@@ -165,10 +165,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAVE_ARM_NEON_INTRINSICS 1
 #endif
 
-#if defined(__ARM_ARCH_7S__)
-#define WTF_CPU_APPLE_ARMV7S 1
-#endif
-
 #endif /* ARM */
 
 /* CPU(ARM64) - AArch64 64-bit */
