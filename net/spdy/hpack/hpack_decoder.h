87460fcf22fb759f16378365bffb83e40febb4bb
bnc <bnc@chromium.org>
2016-02-02 16:01:06 -0800

    Report uncompressed header bytes in OnBlockHeaderEnd().
    Modify HpackDecoder to report uncompressed header bytes when used with a
    SpdyHeadersHandlerInterface.  This makes HpackDecoder more closely aligned with
    SpdyHeadersBlockParser, which always reports the size of the uncompressed (but
    encoded) header block. There's no way to separate the compression from the
    encoding in HPACK, so this is the best we can do.
    
    This CL lands server change 113275350 by birenroy.
    
    BUG=488484
    
    Review URL: https://codereview.chromium.org/1656253004
    
    Cr-Commit-Position: refs/heads/master@{#373087}

--- net/spdy/hpack/hpack_decoder.h
+++ net/spdy/hpack/hpack_decoder.h
@@ -46,6 +46,7 @@ class NET_EXPORT_PRIVATE HpackDecoder {
   // headers to it rather than accumulating them in a SpdyHeaderBlock.
   void HandleControlFrameHeadersStart(SpdyHeadersHandlerInterface* handler) {
     handler_ = handler;
+    total_header_bytes_ = 0;
   }
 
   // Called as headers data arrives. Returns false if an error occurred.
@@ -104,6 +105,7 @@ class NET_EXPORT_PRIVATE HpackDecoder {
 
   // If non-NULL, handles decoded headers.
   SpdyHeadersHandlerInterface* handler_;
+  size_t total_header_bytes_;
 
   // Flag to keep track of having seen a regular header field.
   bool regular_header_seen_;
