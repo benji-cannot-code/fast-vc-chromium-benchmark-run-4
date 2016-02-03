7da8088dfff9c10664cf684b0d6fad00265e7143
loyso <loyso@chromium.org>
2016-02-03 11:25:16 -0800

    UI Compositor: Fix unit tests to work if no use_compositor_animation_timelines
    BUG=574859
    
    Review URL: https://codereview.chromium.org/1659283003
    
    Cr-Commit-Position: refs/heads/master@{#373300}

--- ui/compositor/layer_owner_unittest.cc
+++ ui/compositor/layer_owner_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/test/null_task_runner.h"
 #include "cc/animation/animation_player.h"
+#include "cc/layers/layer_settings.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/compositor/compositor.h"
 #include "ui/compositor/layer.h"
@@ -228,6 +229,10 @@ TEST_F(LayerOwnerTestWithCompositor, RecreateNonRootLayerDuringAnimation) {
 // Tests that if LayerOwner-derived class destroys layer, then
 // LayerAnimator's player becomes detached from compositor timeline.
 TEST_F(LayerOwnerTestWithCompositor, DetachTimelineOnAnimatorDeletion) {
+  // This test is meaningless if CC timelines disabled.
+  if (!Layer::UILayerSettings().use_compositor_animation_timelines)
+    return;
+
   scoped_ptr<Layer> root_layer(new Layer);
   compositor()->SetRootLayer(root_layer.get());
 
@@ -251,6 +256,10 @@ TEST_F(LayerOwnerTestWithCompositor, DetachTimelineOnAnimatorDeletion) {
 // then LayerAnimator's player becomes attached to timeline.
 TEST_F(LayerOwnerTestWithCompositor,
        AttachTimelineIfAnimatorCreatedAfterSetCompositor) {
+  // This test is meaningless if CC timelines disabled.
+  if (!Layer::UILayerSettings().use_compositor_animation_timelines)
+    return;
+
   scoped_ptr<Layer> root_layer(new Layer);
   compositor()->SetRootLayer(root_layer.get());
 
