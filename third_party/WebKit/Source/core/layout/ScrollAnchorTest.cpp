5267ced6fa4f5c3da92af82d8d10d276ac834700
skobes <skobes@chromium.org>
2016-02-02 19:35:47 -0800

    Introduce ScrollAnchor class.
    This class adjusts a scroll position to compensate for movement of a visible
    node as a result of layout.  See bit.ly/scroll-anchoring for more information.
    
    This patch is just the skeleton; future patches will hook it up to the layout
    code and fill out the implementation.
    
    BUG=558575
    
    Review URL: https://codereview.chromium.org/1637483003
    
    Cr-Commit-Position: refs/heads/master@{#373157}

---
+++ third_party/WebKit/Source/core/layout/ScrollAnchorTest.cpp
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/layout/ScrollAnchor.h"
+
+#include "core/layout/LayoutTestHelper.h"
+
+namespace blink {
+
+static void enableScrollAnchoring(Settings& settings)
+{
+    settings.setScrollAnchoringEnabled(true);
+}
+
+class ScrollAnchorTest : public RenderingTest {
+    FrameSettingOverrideFunction settingOverrider() const override { return enableScrollAnchoring; }
+};
+
+TEST_F(ScrollAnchorTest, Basic)
+{
+    ScrollAnchor scrollAnchor(document().view()->layoutViewportScrollableArea());
+    scrollAnchor.clear();
+    EXPECT_EQ(nullptr, scrollAnchor.anchorObject());
+}
+}
