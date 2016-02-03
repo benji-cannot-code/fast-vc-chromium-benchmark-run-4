ac7ca1b17a156dffa9bd9860d35c24ecf1e60c9a
timav <timav@chromium.org>
2016-02-03 14:43:43 -0800

    Disabling WalletServiceUrl.DefaultsToProd test
    The test fails consistently on Android bots.
    
    BUG=581880
    
    TBR=estade@chromium.org
    
    Review URL: https://codereview.chromium.org/1662113002
    
    Cr-Commit-Position: refs/heads/master@{#373367}

--- components/autofill/content/browser/wallet/wallet_service_url_unittest.cc
+++ components/autofill/content/browser/wallet/wallet_service_url_unittest.cc
@@ -42,7 +42,8 @@ TEST(WalletServiceSandboxUrl, CheckProdUrls) {
             GetManageAddressesUrl(1).spec());
 }
 
-TEST(WalletServiceUrl, DefaultsToProd) {
+// Disabling, see http://crbug.com/581880.
+TEST(WalletServiceUrl, DISABLED_DefaultsToProd) {
 #if defined(ENABLE_PROD_WALLET_SERVICE)
   EXPECT_TRUE(IsUsingProd());
 #else
