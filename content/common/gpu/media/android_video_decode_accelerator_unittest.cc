67e3f7f7f828b23f48c4eb248b0d6a0c0a40d69b
watk <watk@chromium.org>
2016-02-03 14:37:05 -0800

    AndroidVideoDecodeAccelerator can now render to a SurfaceView
    Previously AVDA would always configure MediaCodec with a SurfaceTexture.
    Now, to support efficient fullscreen video, it can render to a
    SurfaceView too.
    
    BUG=533630
    
    Review URL: https://codereview.chromium.org/1639963002
    
    Cr-Commit-Position: refs/heads/master@{#373365}

--- content/common/gpu/media/android_video_decode_accelerator_unittest.cc
+++ content/common/gpu/media/android_video_decode_accelerator_unittest.cc
@@ -32,9 +32,6 @@ bool MockMakeContextCurrent() {
 
 namespace content {
 
-// TODO(felipeg): Add more unit tests to test the ordinary behavior of
-// AndroidVideoDecodeAccelerator.
-// http://crbug.com/178647
 class MockVideoDecodeAcceleratorClient
     : public media::VideoDecodeAccelerator::Client {
  public:
@@ -61,8 +58,6 @@ class AndroidVideoDecodeAcceleratorTest : public testing::Test {
   void SetUp() override {
     JNIEnv* env = base::android::AttachCurrentThread();
     media::RegisterJni(env);
-    // TODO(felipeg): fix GL bindings, so that the decoder can perform GL
-    // calls.
 
     // Start message loop because
     // AndroidVideoDecodeAccelerator::ConfigureMediaCodec() starts a timer task.
@@ -79,7 +74,9 @@ class AndroidVideoDecodeAcceleratorTest : public testing::Test {
   bool Configure(media::VideoCodec codec) {
     AndroidVideoDecodeAccelerator* accelerator =
         static_cast<AndroidVideoDecodeAccelerator*>(accelerator_.get());
-    accelerator->surface_texture_ = gfx::SurfaceTexture::Create(0);
+    scoped_refptr<gfx::SurfaceTexture> surface_texture =
+        gfx::SurfaceTexture::Create(0);
+    accelerator->surface_ = gfx::ScopedJavaSurface(surface_texture.get());
     accelerator->codec_ = codec;
     return accelerator->ConfigureMediaCodec();
   }
