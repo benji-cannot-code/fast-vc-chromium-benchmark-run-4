2b1d349adf90d5db8a44272d5bdb28e4e64768ef
brettw <brettw@chromium.org>
2016-02-03 14:57:14 -0800

    GN: Don't write stamp files for one rule.
    When handling input deps, GN would previously always write a stamp file, even
    if that stamp file just stamped one input file. This is a wasteful extra rule.
    
    The previous code was also a bit fragile because it first computed if there
    was any output via a 6-line conditional, and then wrote that output in a way
    that the conditions must match the previous computation.
    
    This new structure is a little more clear since we accumulate the inputs
    and then check the size, so nothing must be kept in sync.
    
    Savings in Chrome Linux build is a reduction in 388 build rules and 376K of
    .ninja files.
    
    BUG=552701
    
    Review URL: https://codereview.chromium.org/1663813003
    
    Cr-Commit-Position: refs/heads/master@{#373375}

--- tools/gn/ninja_target_writer_unittest.cc
+++ tools/gn/ninja_target_writer_unittest.cc
@@ -73,10 +73,10 @@ TEST(NinjaTargetWriter, WriteInputDepsStampAndGetDep) {
     OutputFile dep =
         writer.WriteInputDepsStampAndGetDep(std::vector<const Target*>());
 
-    EXPECT_EQ("obj/foo/base.inputdeps.stamp", dep.value());
-    EXPECT_EQ("build obj/foo/base.inputdeps.stamp: stamp "
-                  "../../foo/script.py\n",
-              stream.str());
+    // Since there is only one dependency, it should just be returned and
+    // nothing written to the stream.
+    EXPECT_EQ("../../foo/script.py", dep.value());
+    EXPECT_EQ("", stream.str());
   }
 
   // Input deps for the target (should depend on the base).
@@ -86,6 +86,8 @@ TEST(NinjaTargetWriter, WriteInputDepsStampAndGetDep) {
     OutputFile dep =
         writer.WriteInputDepsStampAndGetDep(std::vector<const Target*>());
 
+    // Since there is more than one dependency, a stamp file will be returned
+    // and the rule for the stamp file will be written to the stream.
     EXPECT_EQ("obj/foo/target.inputdeps.stamp", dep.value());
     EXPECT_EQ("build obj/foo/target.inputdeps.stamp: stamp "
                   "../../foo/input.txt obj/foo/base.stamp\n",
@@ -134,8 +136,8 @@ TEST(NinjaTargetWriter, WriteInputDepsStampAndGetDepWithToolchainDeps) {
   OutputFile dep =
       writer.WriteInputDepsStampAndGetDep(std::vector<const Target*>());
 
-  EXPECT_EQ("obj/foo/target.inputdeps.stamp", dep.value());
-  EXPECT_EQ("build obj/foo/target.inputdeps.stamp: stamp "
-                "obj/foo/setup.stamp\n",
-            stream.str());
+  // Since there is more than one dependency, a stamp file will be returned
+  // and the rule for the stamp file will be written to the stream.
+  EXPECT_EQ("obj/foo/setup.stamp", dep.value());
+  EXPECT_EQ("", stream.str());
 }
