5b90dd6c7187823948604b502a3f451328e50300
guidou <guidou@chromium.org>
2016-02-03 02:14:55 -0800

    Use TaskRunner instead of WorkerTaskRunner for audio output device enumerations in WebRTC audio private API
    BUG=583010
    
    Review URL: https://codereview.chromium.org/1654883002
    
    Cr-Commit-Position: refs/heads/master@{#373206}

--- chrome/browser/extensions/api/webrtc_audio_private/webrtc_audio_private_api.cc
+++ chrome/browser/extensions/api/webrtc_audio_private/webrtc_audio_private_api.cc
@@ -111,7 +111,7 @@ WebrtcAudioPrivateFunction::~WebrtcAudioPrivateFunction() {
 
 void WebrtcAudioPrivateFunction::GetOutputDeviceNames() {
   scoped_refptr<base::SingleThreadTaskRunner> audio_manager_runner =
-      AudioManager::Get()->GetWorkerTaskRunner();
+      AudioManager::Get()->GetTaskRunner();
   if (!audio_manager_runner->BelongsToCurrentThread()) {
     DCHECK_CURRENTLY_ON(BrowserThread::UI);
     audio_manager_runner->PostTask(
@@ -428,7 +428,7 @@ bool WebrtcAudioPrivateGetAssociatedSinkFunction::RunAsync() {
 
   InitResourceContext();
 
-  AudioManager::Get()->GetWorkerTaskRunner()->PostTask(
+  AudioManager::Get()->GetTaskRunner()->PostTask(
       FROM_HERE,
       base::Bind(&WebrtcAudioPrivateGetAssociatedSinkFunction::
                  GetDevicesOnDeviceThread, this));
@@ -437,7 +437,7 @@ bool WebrtcAudioPrivateGetAssociatedSinkFunction::RunAsync() {
 }
 
 void WebrtcAudioPrivateGetAssociatedSinkFunction::GetDevicesOnDeviceThread() {
-  DCHECK(AudioManager::Get()->GetWorkerTaskRunner()->BelongsToCurrentThread());
+  DCHECK(AudioManager::Get()->GetTaskRunner()->BelongsToCurrentThread());
   AudioManager::Get()->GetAudioInputDeviceNames(&source_devices_);
 
   BrowserThread::PostTask(
@@ -473,7 +473,7 @@ WebrtcAudioPrivateGetAssociatedSinkFunction::GetRawSourceIDOnIOThread() {
     }
   }
 
-  AudioManager::Get()->GetWorkerTaskRunner()->PostTask(
+  AudioManager::Get()->GetTaskRunner()->PostTask(
       FROM_HERE,
       base::Bind(&WebrtcAudioPrivateGetAssociatedSinkFunction::
                  GetAssociatedSinkOnDeviceThread,
@@ -484,7 +484,7 @@ WebrtcAudioPrivateGetAssociatedSinkFunction::GetRawSourceIDOnIOThread() {
 void
 WebrtcAudioPrivateGetAssociatedSinkFunction::GetAssociatedSinkOnDeviceThread(
     const std::string& raw_source_id) {
-  DCHECK(AudioManager::Get()->GetWorkerTaskRunner()->BelongsToCurrentThread());
+  DCHECK(AudioManager::Get()->GetTaskRunner()->BelongsToCurrentThread());
 
   // We return an empty string if there is no associated output device.
   std::string raw_sink_id;
