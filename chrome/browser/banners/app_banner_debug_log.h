53261483ebae986844a0091bc23b92c4d6dfafd4
dominickn <dominickn@chromium.org>
2016-02-02 16:43:53 -0800

    Remove app banner console error on desktop when a webapp is installed.
    This error would never be shown to the user as the conditional
    guarding it can never be true. This CL removes the error message. When a
    app banner is installed, if the bypass-app-banner-engagement-checks flag
    is active, the installation proceeds. Otherwise, the installation will
    be canceled.
    
    BUG=578926
    
    Review URL: https://codereview.chromium.org/1659733002
    
    Cr-Commit-Position: refs/heads/master@{#373103}

--- chrome/browser/banners/app_banner_debug_log.h
+++ chrome/browser/banners/app_banner_debug_log.h
@@ -21,7 +21,6 @@ extern const char kNoManifest[];
 extern const char kCannotDetermineBestIcon[];
 extern const char kNoMatchingServiceWorker[];
 extern const char kNoIconAvailable[];
-extern const char kBannerAlreadyAdded[];
 extern const char kUserNavigatedBeforeBannerShown[];
 extern const char kStartURLNotValid[];
 extern const char kManifestMissingNameOrShortName[];
