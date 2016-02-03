eac940eda541eac5d738d51b7bb21cacba6d7652
cco3 <cco3@chromium.org>
2016-02-03 14:48:44 -0800

    Restrict the Physical Web feature to API>=18
    BUG=529962
    
    Review URL: https://codereview.chromium.org/1614203005
    
    Cr-Commit-Position: refs/heads/master@{#373370}

--- chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWeb.java
+++ chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWeb.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.physicalweb;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.preference.PreferenceManager;
 
 import org.chromium.chrome.browser.ChromeApplication;
@@ -21,6 +22,7 @@ public class PhysicalWeb {
     public static final int OPTIN_NOTIFY_MAX_TRIES = 1;
     private static final String PREF_PHYSICAL_WEB_NOTIFY_COUNT = "physical_web_notify_count";
     private static final String FEATURE_NAME = "PhysicalWeb";
+    private static final int MIN_ANDROID_VERSION = 18;
 
     /**
      * Evaluate whether the environment is one in which the Physical Web should
@@ -28,7 +30,8 @@ public class PhysicalWeb {
      * @return true if the PhysicalWeb should be enabled
      */
     public static boolean featureIsEnabled() {
-        return ChromeFeatureList.isEnabled(FEATURE_NAME);
+        return ChromeFeatureList.isEnabled(FEATURE_NAME)
+                && Build.VERSION.SDK_INT >= MIN_ANDROID_VERSION;
     }
 
     /**
