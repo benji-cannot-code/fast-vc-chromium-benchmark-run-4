ff18c312c3563cdc476f9edc0bcfc3480142846c
toyoshim <toyoshim@chromium.org>
2016-02-03 21:36:00 -0800

    Remove stale Field Trial code
    Dummy FindValue() call isn't needed to sync the Field Trial
    configuration between browser and renderers any more.
    
    BUG=n/a
    
    Review URL: https://codereview.chromium.org/1580233005
    
    Cr-Commit-Position: refs/heads/master@{#373469}

--- chrome/browser/chrome_browser_field_trials.cc
+++ chrome/browser/chrome_browser_field_trials.cc
@@ -66,7 +66,4 @@ void ChromeBrowserFieldTrials::SetupFieldTrials() {
 void ChromeBrowserFieldTrials::InstantiateDynamicTrials() {
   // Persistent histograms must be enabled as soon as possible.
   InstantiatePersistentHistograms();
-  // The following trials are used from renderer process.
-  // Mark here so they will be sync-ed.
-  base::FieldTrialList::FindValue("DisplayList2dCanvas");
 }
