a8f1c8b16415c331de976de6317b4442d653a676
dcheng <dcheng@chromium.org>
2016-02-04 14:45:59 -0800

    Change Document::detach() to RELEASE_ASSERT all subframes are gone.
    BUG=556724,577105
    
    Review URL: https://codereview.chromium.org/1667573002
    
    Cr-Commit-Position: refs/heads/master@{#373642}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -2190,7 +2190,7 @@ void Document::attach(const AttachContext& context)
 void Document::detach(const AttachContext& context)
 {
     TRACE_EVENT0("blink", "Document::detach");
-    ASSERT(!m_frame || m_frame->tree().childCount() == 0);
+    RELEASE_ASSERT(!m_frame || m_frame->tree().childCount() == 0);
     if (!isActive())
         return;
 
