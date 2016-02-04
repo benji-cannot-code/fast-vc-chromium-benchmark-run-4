31d0211bccec56e51d724070471e0e6cc432e6f9
xlai <xlai@chromium.org>
2016-02-04 10:18:57 -0800

    Reland Simplify CanvasAsyncBlobCreator by removing ContextObserver and tests
    After we confirm to use idle-tasks implementation in toBlob, the cancellation
    mechanism for async thread implementation is no longer useful. First, compositor
    thread is definitely alive when users open Chrome browser; so the alternative
    code path for PNG image encoding when compositor thread is disabled would be
    unreachable in real-world situations; as a result, PNG image encoding happens
    in idle tasks only. Second, JPEG and WEBP image formats do not have progressive
    encoding at this moment and thus cancellation mechanism is not useful to them
    either; once their progressive encoding implementations are completed, they
    would be moved over to idle-tasks implementation too.
    
    This patch removes ContextObserver and CanvasAsyncBlobCreatorTest; it also moves
    all layout tests that use toBlob on PNG image formats to virtual/threaded as
    idle tasks require compositor thread to be alive. There is no need to perform
    tests on toBlob (png) in scenarios when compositor thread is not available.
    
    BUG=581574
    
    Review URL: https://codereview.chromium.org/1669713002
    
    Cr-Commit-Position: refs/heads/master@{#373567}

--- third_party/WebKit/Source/core/html/canvas/CanvasAsyncBlobCreator.h
+++ third_party/WebKit/Source/core/html/canvas/CanvasAsyncBlobCreator.h
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/CoreExport.h"
 #include "core/dom/DOMTypedArray.h"
-#include "core/dom/ExecutionContext.h"
 #include "core/fileapi/BlobCallback.h"
 #include "platform/geometry/IntSize.h"
 #include "platform/heap/Handle.h"
@@ -21,19 +20,15 @@ class PNGImageEncoderState;
 class CORE_EXPORT CanvasAsyncBlobCreator
     : public RefCounted<CanvasAsyncBlobCreator> {
 public:
-    static PassRefPtr<CanvasAsyncBlobCreator> create(PassRefPtr<DOMUint8ClampedArray> unpremultipliedRGBAImageData, const String& mimeType, const IntSize&, BlobCallback*, ExecutionContext*);
+    static PassRefPtr<CanvasAsyncBlobCreator> create(PassRefPtr<DOMUint8ClampedArray> unpremultipliedRGBAImageData, const String& mimeType, const IntSize&, BlobCallback*);
     void scheduleAsyncBlobCreation(bool canUseIdlePeriodScheduling, double quality = 0.0);
     virtual ~CanvasAsyncBlobCreator();
 
-protected:
-    CanvasAsyncBlobCreator(PassRefPtr<DOMUint8ClampedArray> data, const String& mimeType, const IntSize&, BlobCallback*);
-    virtual void scheduleCreateBlobAndCallOnMainThread();
-    virtual void scheduleCreateNullptrAndCallOnMainThread();
-    virtual void scheduleClearSelfRefOnMainThread();
-    std::atomic<bool> m_cancelled;
-
 private:
-    friend class CanvasAsyncBlobCreatorTest;
+    CanvasAsyncBlobCreator(PassRefPtr<DOMUint8ClampedArray> data, const String& mimeType, const IntSize&, BlobCallback*);
+    void scheduleCreateBlobAndCallOnMainThread();
+    void scheduleCreateNullptrAndCallOnMainThread();
+    void scheduleClearSelfRefOnMainThread();
 
     OwnPtr<PNGImageEncoderState> m_encoderState;
     RefPtr<DOMUint8ClampedArray> m_data;
@@ -55,13 +50,6 @@ private:
     void createBlobAndCall();
 
     void encodeImageOnEncoderThread(double quality);
-    bool initializeEncodeImageOnEncoderThread();
-    void nonprogressiveEncodeImageOnEncoderThread(double quality);
-    void progressiveEncodeImageOnEncoderThread();
-
-    class ContextObserver;
-    void createContextObserver(ExecutionContext*);
-    OwnPtrWillBePersistent<ContextObserver> m_contextObserver;
 };
 
 } // namespace blink
