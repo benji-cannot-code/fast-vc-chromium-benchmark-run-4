f16ca000fb701b696671d023328b9649d7f360f3
tfarina <tfarina@chromium.org>
2016-02-04 01:53:02 -0800

    net: change ListenWithAddressAndPort() to use IPAddress class
    This patch changes ServerSocket::ListenWithAddressAndPort() member
    function to make use of the new IPAddress class and its FromIPLiteral
    helper method.
    
    By changing to IPAddress we end up using the non-deprecated
    constructor of IPEndPoint, which is also good.
    
    This patch also removes an unused net_util.h include.
    
    BUG=488531,496258
    TEST=net_unittests
    R=mmenke@chromium.org,eroman@chromium.org
    
    Review URL: https://codereview.chromium.org/1655393005
    
    Cr-Commit-Position: refs/heads/master@{#373494}

--- net/socket/server_socket.cc
+++ net/socket/server_socket.cc
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/socket/server_socket.h"
 
+#include "net/base/ip_address.h"
 #include "net/base/ip_endpoint.h"
 #include "net/base/net_errors.h"
-#include "net/base/net_util.h"
 
 namespace net {
 
@@ -20,12 +20,12 @@ ServerSocket::~ServerSocket() {
 int ServerSocket::ListenWithAddressAndPort(const std::string& address_string,
                                            uint16_t port,
                                            int backlog) {
-  IPAddressNumber address_number;
-  if (!ParseIPLiteralToNumber(address_string, &address_number)) {
+  IPAddress ip_address;
+  if (!IPAddress::FromIPLiteral(address_string, &ip_address)) {
     return ERR_ADDRESS_INVALID;
   }
 
-  return Listen(IPEndPoint(address_number, port), backlog);
+  return Listen(IPEndPoint(ip_address, port), backlog);
 }
 
 }  // namespace net
