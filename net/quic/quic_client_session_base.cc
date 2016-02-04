05094a84de03be8392e19ea8622cdca2991cd0a2
ckrasic <ckrasic@chromium.org>
2016-02-04 13:41:46 -0800

    Improvements to QUIC client-side support of server push.
    Increase test coverage.
      - Adds quic_client_push_promise_index_test
    
    API cleanups
      - replace unique_ptr params with const &
      - expunge QuicSpdyStream::Visitor from push promise classes, move small bit
        of related logic to quic*client.
      - rename OnResponse() to OnRendezvousResult()
    Other
      - Check IsClosed() earlier in Try(), so client will not hang in rendezvous (until cleanup alarm fires)  if the data stream was reset before the corresponding response headers arrived.
      - remove dead instance of |DataToResend|
    
    Merge internal change: 113642684
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1661083003
    
    Cr-Commit-Position: refs/heads/master@{#373627}

--- net/quic/quic_client_session_base.cc
+++ net/quic/quic_client_session_base.cc
@@ -63,8 +63,7 @@ void QuicClientSessionBase::OnInitialHeadersComplete(
   if (!promised)
     return;
 
-  promised->OnResponseHeaders(
-      std::unique_ptr<SpdyHeaderBlock>(new SpdyHeaderBlock(response_headers)));
+  promised->OnResponseHeaders(response_headers);
 }
 
 void QuicClientSessionBase::OnPromiseHeadersComplete(
@@ -88,9 +87,8 @@ void QuicClientSessionBase::OnPromiseHeadersComplete(
   stream->OnPromiseHeadersComplete(promised_stream_id, frame_len);
 }
 
-void QuicClientSessionBase::HandlePromised(
-    QuicStreamId id,
-    std::unique_ptr<SpdyHeaderBlock> headers) {
+void QuicClientSessionBase::HandlePromised(QuicStreamId id,
+                                           const SpdyHeaderBlock& headers) {
   // Due to pathalogical packet re-ordering, it is possible that
   // frames for the promised stream have already arrived, and the
   // promised stream could be active or closed.
@@ -108,7 +106,7 @@ void QuicClientSessionBase::HandlePromised(
     return;
   }
 
-  const string url = SpdyUtils::GetUrlFromHeaderBlock(*headers);
+  const string url = SpdyUtils::GetUrlFromHeaderBlock(headers);
   QuicClientPromisedInfo* old_promised = GetPromisedByUrl(url);
   if (old_promised) {
     DVLOG(1) << "Promise for stream " << id << " is duplicate URL " << url
@@ -130,7 +128,7 @@ void QuicClientSessionBase::HandlePromised(
   DVLOG(1) << "stream " << id << " emplace url " << url;
   (*push_promise_index_->promised_by_url())[url] = promised;
   promised_by_id_[id] = std::move(promised_owner);
-  promised->OnPromiseHeaders(std::move(headers));
+  promised->OnPromiseHeaders(headers);
 }
 
 QuicClientPromisedInfo* QuicClientSessionBase::GetPromisedByUrl(
@@ -154,11 +152,14 @@ QuicClientPromisedInfo* QuicClientSessionBase::GetPromisedById(
 
 QuicSpdyStream* QuicClientSessionBase::GetPromisedStream(
     const QuicStreamId id) {
-  DCHECK(IsOpenStream(id));
+  if (IsClosedStream(id)) {
+    return nullptr;
+  }
   StreamMap::iterator it = dynamic_streams().find(id);
   if (it != dynamic_streams().end()) {
     return static_cast<QuicSpdyStream*>(it->second);
   }
+  QUIC_BUG << "Open promised stream " << id << " is missing!";
   return nullptr;
 }
 
---
+++ net/quic/test_tools/mock_quic_client_promised_info.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/quic/test_tools/mock_quic_client_promised_info.h"
+
+namespace net {
+namespace test {
+
+MockQuicClientPromisedInfo::MockQuicClientPromisedInfo(
+    QuicClientSessionBase* session,
+    QuicStreamId id,
+    string url)
+    : QuicClientPromisedInfo(session, id, url) {}
+
+MockQuicClientPromisedInfo::~MockQuicClientPromisedInfo() {}
+
+}  // namespace test
+}  // namespace net
