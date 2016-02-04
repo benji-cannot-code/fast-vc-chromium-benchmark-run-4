a1c748d5dc99b9853d10263bfdc639867683ac0e
rdsmith <rdsmith@chromium.org>
2016-02-04 15:13:15 -0800

    Log what filter causes a decoding error.
    BUG=584326
    R=xunjieli@chromium.org
    
    Review URL: https://codereview.chromium.org/1670643003
    
    Cr-Commit-Position: refs/heads/master@{#373651}

--- net/filter/filter.h
+++ net/filter/filter.h
@@ -152,7 +152,8 @@ class NET_EXPORT_PRIVATE Filter {
     FILTER_ERROR
   };
 
-  // Specifies type of filters that can be created.
+  // Specifies type of filters that can be created.  Do not change the values
+  // of this enum; it is preserved in a histogram.
   enum FilterType {
     FILTER_TYPE_BROTLI,
     FILTER_TYPE_DEFLATE,
@@ -161,6 +162,8 @@ class NET_EXPORT_PRIVATE Filter {
     FILTER_TYPE_SDCH,
     FILTER_TYPE_SDCH_POSSIBLE,  // Sdch possible, but pass through allowed.
     FILTER_TYPE_UNSUPPORTED,
+
+    FILTER_TYPE_MAX
   };
 
   virtual ~Filter();
@@ -233,6 +236,8 @@ class NET_EXPORT_PRIVATE Filter {
 
   // Returns a string describing the FilterTypes implemented by this filter.
   std::string OrderedFilterList() const;
+
+  FilterType type() const { return type_id_; }
 
  protected:
   friend class BrotliUnitTest;
