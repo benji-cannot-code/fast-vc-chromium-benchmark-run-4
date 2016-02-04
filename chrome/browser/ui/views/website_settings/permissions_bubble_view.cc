dba50197cdf45283f1365c94476fa50bec0b3bbe
estade <estade@chromium.org>
2016-02-04 13:59:07 -0800

    Restore original compact margins to zoom bubble, tweak permissions bubble
    BUG=584187
    
    Review URL: https://codereview.chromium.org/1668463007
    
    Cr-Commit-Position: refs/heads/master@{#373631}

--- chrome/browser/ui/views/website_settings/permissions_bubble_view.cc
+++ chrome/browser/ui/views/website_settings/permissions_bubble_view.cc
@@ -48,10 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Spacing constant for outer margin. This is added to the
-// bubble margin itself to equalize the margins at 13px.
-const int kBubbleOuterMargin = 5;
-
 // Spacing between major items should be 9px.
 const int kItemMajorSpacing = 9;
 
@@ -212,13 +208,11 @@ PermissionsBubbleDelegateView::PermissionsBubbleDelegateView(
       deny_(nullptr) {
   DCHECK(!requests.empty());
 
-  RemoveAllChildViews(true);
-  customize_comboboxes_.clear();
   set_close_on_esc(true);
   set_close_on_deactivate(false);
 
-  SetLayoutManager(new views::BoxLayout(
-      views::BoxLayout::kVertical, kBubbleOuterMargin, 0, kItemMajorSpacing));
+  SetLayoutManager(new views::BoxLayout(views::BoxLayout::kVertical, 0, 0,
+                                        kItemMajorSpacing));
 
   display_origin_ = url_formatter::FormatUrlForSecurityDisplay(
       requests[0]->GetOrigin(), languages);
@@ -240,10 +234,8 @@ PermissionsBubbleDelegateView::PermissionsBubbleDelegateView(
     row_layout->StartRow(0, 0);
 
     views::View* label_container = new views::View();
-    label_container->SetLayoutManager(
-        new views::BoxLayout(views::BoxLayout::kHorizontal,
-                             kPermissionIndentSpacing,
-                             0, kBubbleOuterMargin));
+    label_container->SetLayoutManager(new views::BoxLayout(
+        views::BoxLayout::kHorizontal, kPermissionIndentSpacing, 0, 0));
     views::ImageView* icon = new views::ImageView();
     gfx::VectorIconId vector_id = requests[index]->GetVectorIconId();
     if (vector_id != gfx::VectorIconId::VECTOR_ICON_NONE) {
@@ -290,8 +282,6 @@ PermissionsBubbleDelegateView::PermissionsBubbleDelegateView(
     ok_button->SetStyle(views::Button::STYLE_BUTTON);
     button_layout->AddView(ok_button);
     allow_ = ok_button;
-
-    button_layout->AddPaddingRow(0, kBubbleOuterMargin);
     return;
   }
 
@@ -314,8 +304,6 @@ PermissionsBubbleDelegateView::PermissionsBubbleDelegateView(
   deny_button->SetStyle(views::Button::STYLE_BUTTON);
   button_layout->AddView(deny_button);
   deny_ = deny_button;
-
-  button_layout->AddPaddingRow(0, kBubbleOuterMargin);
 }
 
 PermissionsBubbleDelegateView::~PermissionsBubbleDelegateView() {
