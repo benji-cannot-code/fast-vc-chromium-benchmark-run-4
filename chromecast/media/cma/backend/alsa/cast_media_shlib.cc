7b8cb7f62448200b4225d5d0db865a5d54614d4f
cleichner <cleichner@chromium.org>
2016-02-03 18:07:32 -0800

    [Cast ALSA CMA] Clean up formatting, remove space
    BUG=NONE
    
    Review URL: https://codereview.chromium.org/1655403002
    
    Cr-Commit-Position: refs/heads/master@{#373424}

--- chromecast/media/cma/backend/alsa/cast_media_shlib.cc
+++ chromecast/media/cma/backend/alsa/cast_media_shlib.cc
@@ -34,7 +34,7 @@ namespace {
 
 const char kDefaultPcmDevice[] = "hw:0";
 const int kSoundControlBlockingMode = 0;
-const char kRateOffsetInterfaceName[] =  "PCM Playback Rate Offset";
+const char kRateOffsetInterfaceName[] = "PCM Playback Rate Offset";
 
 // 1 MHz reference allows easy translation between frequency and PPM.
 const double kOneMhzReference = 1e6;
