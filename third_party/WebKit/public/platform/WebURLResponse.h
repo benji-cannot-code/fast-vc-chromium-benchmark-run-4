5136c328596ec9ed591b125b758e14d4028889c7
lgarron <lgarron@chromium.org>
2016-02-03 17:56:55 -0800

    Surface SCT (Signed Certificate Timestamp) counts in the Security panel.
    BUG=551705
    TEST=Do the following:
    1) Visit mozilla.org, open the Security panel in DevTools, refresh, click on "https://www.mozilla.org" under "Main Origin", check that the "Certificate" section has an "SCTs" value of "3 valid SCTs".
    2) Do the same for sha1-2017.badssl.com and check that the value is "0 SCTs".
    3) Do the same for selecadm.name and check that the value is "2 valid SCTs". (This sanity checks that dynamically served SCTs are treated the same as embedded SCTs.)
    
    Review URL: https://codereview.chromium.org/1589703002
    
    Cr-Commit-Position: refs/heads/master@{#373420}

--- third_party/WebKit/public/platform/WebURLResponse.h
+++ third_party/WebKit/public/platform/WebURLResponse.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "public/platform/WebCommon.h"
 #include "public/platform/WebPrivateOwnPtr.h"
+#include "public/platform/WebString.h"
 #include "public/platform/modules/serviceworker/WebServiceWorkerResponseType.h"
 
 namespace blink {
@@ -60,6 +61,31 @@ public:
         SecurityStyleAuthenticationBroken,
         SecurityStyleWarning,
         SecurityStyleAuthenticated
+    };
+
+    struct WebSecurityDetails {
+        WebSecurityDetails(const WebString& protocol, const WebString& keyExchange, const WebString& cipher, const WebString& mac, int certId, size_t numUnknownScts, size_t numInvalidScts, size_t numValidScts)
+            : protocol(protocol)
+            , keyExchange(keyExchange)
+            , cipher(cipher)
+            , mac(mac)
+            , certId(certId)
+            , numUnknownScts(numUnknownScts)
+            , numInvalidScts(numInvalidScts)
+            , numValidScts(numValidScts)
+        {
+        }
+        // All strings are human-readable values.
+        WebString protocol;
+        WebString keyExchange;
+        WebString cipher;
+        // mac is the empty string when the connection cipher suite does not
+        // have a separate MAC value (i.e. if the cipher suite is AEAD).
+        WebString mac;
+        int certId;
+        size_t numUnknownScts;
+        size_t numInvalidScts;
+        size_t numValidScts;
     };
 
     class ExtraData {
@@ -152,7 +178,7 @@ public:
     BLINK_PLATFORM_EXPORT SecurityStyle securityStyle() const;
     BLINK_PLATFORM_EXPORT void setSecurityStyle(SecurityStyle);
 
-    BLINK_PLATFORM_EXPORT void setSecurityDetails(const WebString& protocol, const WebString& keyExchange, const WebString& cipher, const WebString& mac, int certId);
+    BLINK_PLATFORM_EXPORT void setSecurityDetails(const WebSecurityDetails&);
 
 #if INSIDE_BLINK
     BLINK_PLATFORM_EXPORT ResourceResponse& toMutableResourceResponse();
