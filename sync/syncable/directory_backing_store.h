3acdce52e3641212f46e47dd5f40d1c936d092ac
zea <zea@chromium.org>
2016-02-03 18:25:40 -0800

    [Sync] Fix sync directory initialization to better handle errors
    The following changes are made:
    - Handle failure to change the page size by failing early
    - Do not attempt to silently recreate the database on error loading.
    - Fix testing to verify that the page size is updated properly on update.
    - Add test for case where the database doesn't load properly on upgrade.
    
    BUG=583795
    
    Review URL: https://codereview.chromium.org/1661063004
    
    Cr-Commit-Position: refs/heads/master@{#373433}

--- sync/syncable/directory_backing_store.h
+++ sync/syncable/directory_backing_store.h
@@ -30,6 +30,7 @@ namespace syncer {
 namespace syncable {
 
 SYNC_EXPORT extern const int32_t kCurrentDBVersion;
+SYNC_EXPORT extern const int32_t kCurrentPageSizeKB;
 
 struct ColumnSpec;
 
@@ -97,6 +98,9 @@ class SYNC_EXPORT DirectoryBackingStore : public base::NonThreadSafe {
   // |catastrophic_error_handler|.
   virtual void SetCatastrophicErrorHandler(
       const base::Closure& catastrophic_error_handler);
+
+  // Returns true on success, false on error.
+  bool GetDatabasePageSize(int* page_size);
 
  protected:
   // For test classes.
@@ -186,8 +190,8 @@ class SYNC_EXPORT DirectoryBackingStore : public base::NonThreadSafe {
   void ResetAndCreateConnection();
 
  private:
-  friend class TestDirectoryBackingStore;
   friend class DirectoryBackingStoreTest;
+  friend class TestDirectoryBackingStore;
   FRIEND_TEST_ALL_PREFIXES(DirectoryBackingStoreTest,
                            IncreaseDatabasePageSizeFrom4KTo32K);
   FRIEND_TEST_ALL_PREFIXES(DirectoryBackingStoreTest,
@@ -197,6 +201,7 @@ class SYNC_EXPORT DirectoryBackingStore : public base::NonThreadSafe {
   FRIEND_TEST_ALL_PREFIXES(
       DirectoryBackingStoreTest,
       CatastrophicErrorHandler_InvocationDuringSaveChanges);
+  FRIEND_TEST_ALL_PREFIXES(MigrationTest, ToCurrentVersion);
 
   // Drop all tables in preparation for reinitialization.
   void DropAllTables();
@@ -236,10 +241,7 @@ class SYNC_EXPORT DirectoryBackingStore : public base::NonThreadSafe {
   bool Vacuum();
 
   // Returns true on success, false on error.
-  bool IncreasePageSizeTo32K();
-
-  // Returns true on success, false on error.
-  bool GetDatabasePageSize(int* page_size);
+  bool UpdatePageSizeIfNecessary();
 
   // Prepares |save_statement| for saving entries in |table|.
   void PrepareSaveEntryStatement(EntryTable table,
