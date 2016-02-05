f2f8c60b13d59bf95fe7be394befc19432c520de
bpastene <bpastene@chromium.org>
2016-02-05 11:37:07 -0800

    wait for device before restarting its adbd
    BUG=583614
    
    Review URL: https://codereview.chromium.org/1670833002
    
    Cr-Commit-Position: refs/heads/master@{#373873}

--- build/android/pylib/utils/test_environment.py
+++ build/android/pylib/utils/test_environment.py
@@ -41,6 +41,7 @@ def CleanupLeftoverProcesses(devices):
   device_utils.RestartServer()
 
   def cleanup_device(d):
+    d.WaitUntilFullyBooted()
     d.RestartAdbd()
     try:
       d.EnableRoot()
