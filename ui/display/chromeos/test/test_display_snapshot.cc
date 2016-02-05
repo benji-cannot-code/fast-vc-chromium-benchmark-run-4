efbfb6487e07169bfcb6117e5b5648a6928a5a9b
afakhry <afakhry@chromium.org>
2016-02-05 12:22:45 -0800

    Give user ability to file a feedback report from the display error notification.
    Make the display error notification clickable and starts the feedback app on click.
    We log the available displays IDs and EDIDs.
    
    BUG=570380
    TEST=manually
    
    Review URL: https://codereview.chromium.org/1636153002
    
    Cr-Commit-Position: refs/heads/master@{#373895}

--- ui/display/chromeos/test/test_display_snapshot.cc
+++ ui/display/chromeos/test/test_display_snapshot.cc
@@ -16,6 +16,7 @@ TestDisplaySnapshot::TestDisplaySnapshot()
                       std::string(),
                       base::FilePath(),
                       std::vector<const DisplayMode*>(),
+                      std::vector<uint8_t>(),
                       NULL,
                       NULL) {}
 
@@ -38,6 +39,7 @@ TestDisplaySnapshot::TestDisplaySnapshot(
                       std::string(),
                       base::FilePath(),
                       modes,
+                      std::vector<uint8_t>(),
                       current_mode,
                       native_mode) {
   product_id_ = product_id;
