71504a99c5530b97a1ba8ab69b987c2de27797d8
finnur <finnur@chromium.org>
2016-02-05 04:01:07 -0800

    Implement deletion of storage per site.
    BUG=543635
    
    Review URL: https://codereview.chromium.org/1661533002
    
    Cr-Commit-Position: refs/heads/master@{#373790}

--- chrome/browser/resources/settings/privacy_page/privacy_page.js
+++ chrome/browser/resources/settings/privacy_page/privacy_page.js
@@ -38,14 +38,6 @@ Polymer({
       type: Object,
       notify: true,
     },
-
-    /**
-     * The origin selected by the user.
-     */
-    originSelected: {
-      type: String,
-      observer: 'onSelectedOriginChanged_',
-    }
   },
 
   ready: function() {
@@ -65,10 +57,5 @@ Polymer({
   /** @private */
   onClearBrowsingDataTap_: function() {
     this.$.pages.setSubpageChain(['clear-browsing-data']);
-  },
-
-  onSelectedOriginChanged_: function() {
-    this.$.pages.setSubpageChain(
-        ['site-settings', 'site-settings-category', 'site-details']);
   },
 });
--- chrome/browser/storage/storage_info_fetcher.h
+++ chrome/browser/storage/storage_info_fetcher.h
@@ -19,6 +19,9 @@ class StorageInfoFetcher :
     // Called when the storage has been calculated.
     virtual void OnGetUsageInfo(const storage::UsageInfoEntries& entries) = 0;
 
+    // Called when the storage has been cleared.
+    virtual void OnUsageInfoCleared(storage::QuotaStatusCode code) = 0;
+
    protected:
     virtual ~Observer() {}
   };
@@ -26,7 +29,13 @@ class StorageInfoFetcher :
   explicit StorageInfoFetcher(storage::QuotaManager* quota_manager);
 
   // Asynchronously fetches the StorageInfo.
-  void Run();
+  void FetchStorageInfo();
+
+  // Asynchronously clears storage for the given host.
+  void ClearStorage(const std::string& host, storage::StorageType type);
+
+  // Called when usage has been cleared.
+  void OnUsageCleared(storage::QuotaStatusCode code);
 
   void AddObserver(Observer* observer);
   void RemoveObserver(Observer* observer);
