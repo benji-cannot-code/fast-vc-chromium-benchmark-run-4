0163306f80e3ed75d35a5c455cacaaf43ada0d36
hubbe <hubbe@chromium.org>
2016-02-04 16:50:44 -0800

    Add support for 9- and 10-bit h264 videos.
    Videos are uploaded to GPU using half-floats if supported, otherwise
    they are downshifted to regular 8-bit textures before uploading. No
    dithering is done, except for whatever GL_DITHER might do. (Which
    is probably nothing.)
    
    BUG=445071
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1599533002
    
    Cr-Commit-Position: refs/heads/master@{#373691}

--- media/base/video_frame.cc
+++ media/base/video_frame.cc
@@ -113,9 +113,13 @@ static gfx::Size SampleSize(VideoPixelFormat format, size_t plane) {
     case VideoFrame::kVPlane:
       switch (format) {
         case PIXEL_FORMAT_YV24:
+        case PIXEL_FORMAT_YUV444P9:
+        case PIXEL_FORMAT_YUV444P10:
           return gfx::Size(1, 1);
 
         case PIXEL_FORMAT_YV16:
+        case PIXEL_FORMAT_YUV422P9:
+        case PIXEL_FORMAT_YUV422P10:
           return gfx::Size(2, 1);
 
         case PIXEL_FORMAT_YV12:
@@ -124,6 +128,8 @@ static gfx::Size SampleSize(VideoPixelFormat format, size_t plane) {
         case PIXEL_FORMAT_NV12:
         case PIXEL_FORMAT_NV21:
         case PIXEL_FORMAT_MT21:
+        case PIXEL_FORMAT_YUV420P9:
+        case PIXEL_FORMAT_YUV420P10:
           return gfx::Size(2, 2);
 
         case PIXEL_FORMAT_UNKNOWN:
@@ -166,6 +172,12 @@ static int BytesPerElement(VideoPixelFormat format, size_t plane) {
       return 3;
     case PIXEL_FORMAT_UYVY:
     case PIXEL_FORMAT_YUY2:
+    case PIXEL_FORMAT_YUV420P9:
+    case PIXEL_FORMAT_YUV422P9:
+    case PIXEL_FORMAT_YUV444P9:
+    case PIXEL_FORMAT_YUV420P10:
+    case PIXEL_FORMAT_YUV422P10:
+    case PIXEL_FORMAT_YUV444P10:
       return 2;
     case PIXEL_FORMAT_NV12:
     case PIXEL_FORMAT_NV21:
@@ -215,7 +227,7 @@ bool VideoFrame::IsValidConfig(VideoPixelFormat format,
     return true;
 
   // Make sure new formats are properly accounted for in the method.
-  static_assert(PIXEL_FORMAT_MAX == 15,
+  static_assert(PIXEL_FORMAT_MAX == 21,
                 "Added pixel format, please review IsValidConfig()");
 
   if (format == PIXEL_FORMAT_UNKNOWN) {
@@ -618,6 +630,12 @@ size_t VideoFrame::NumPlanes(VideoPixelFormat format) {
     case PIXEL_FORMAT_YV12:
     case PIXEL_FORMAT_YV16:
     case PIXEL_FORMAT_YV24:
+    case PIXEL_FORMAT_YUV420P9:
+    case PIXEL_FORMAT_YUV422P9:
+    case PIXEL_FORMAT_YUV444P9:
+    case PIXEL_FORMAT_YUV420P10:
+    case PIXEL_FORMAT_YUV422P10:
+    case PIXEL_FORMAT_YUV444P10:
       return 3;
     case PIXEL_FORMAT_YV12A:
       return 4;
--- media/base/video_types.cc
+++ media/base/video_types.cc
@@ -43,6 +43,18 @@ std::string VideoPixelFormatToString(VideoPixelFormat format) {
       return "PIXEL_FORMAT_MJPEG";
     case PIXEL_FORMAT_MT21:
       return "PIXEL_FORMAT_MT21";
+    case PIXEL_FORMAT_YUV420P9:
+      return "PIXEL_FORMAT_YUV420P9";
+    case PIXEL_FORMAT_YUV420P10:
+      return "PIXEL_FORMAT_YUV420P10";
+    case PIXEL_FORMAT_YUV422P9:
+      return "PIXEL_FORMAT_YUV422P9";
+    case PIXEL_FORMAT_YUV422P10:
+      return "PIXEL_FORMAT_YUV422P10";
+    case PIXEL_FORMAT_YUV444P9:
+      return "PIXEL_FORMAT_YUV444P9";
+    case PIXEL_FORMAT_YUV444P10:
+      return "PIXEL_FORMAT_YUV444P10";
   }
   NOTREACHED() << "Invalid VideoPixelFormat provided: " << format;
   return "";
@@ -58,6 +70,12 @@ bool IsYuvPlanar(VideoPixelFormat format) {
     case PIXEL_FORMAT_NV12:
     case PIXEL_FORMAT_NV21:
     case PIXEL_FORMAT_MT21:
+    case PIXEL_FORMAT_YUV420P9:
+    case PIXEL_FORMAT_YUV420P10:
+    case PIXEL_FORMAT_YUV422P9:
+    case PIXEL_FORMAT_YUV422P10:
+    case PIXEL_FORMAT_YUV444P9:
+    case PIXEL_FORMAT_YUV444P10:
       return true;
 
     case PIXEL_FORMAT_UNKNOWN:
@@ -88,6 +106,12 @@ bool IsOpaque(VideoPixelFormat format) {
     case PIXEL_FORMAT_RGB24:
     case PIXEL_FORMAT_MJPEG:
     case PIXEL_FORMAT_MT21:
+    case PIXEL_FORMAT_YUV420P9:
+    case PIXEL_FORMAT_YUV420P10:
+    case PIXEL_FORMAT_YUV422P9:
+    case PIXEL_FORMAT_YUV422P10:
+    case PIXEL_FORMAT_YUV444P9:
+    case PIXEL_FORMAT_YUV444P10:
       return true;
     case PIXEL_FORMAT_YV12A:
     case PIXEL_FORMAT_ARGB:
