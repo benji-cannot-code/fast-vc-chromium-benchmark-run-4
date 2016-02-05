40f485f3db96ce8b0ab6f46788c8edda8f22b722
yoav <yoav@yoav.ws>
2016-02-04 23:03:40 -0800

    Link preload crossorigin attribute.
    This CL fixes an issue with <link rel=preload> where the crossorigin
    attribute was not properly enforced.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1667663003
    
    Cr-Commit-Position: refs/heads/master@{#373760}

--- third_party/WebKit/Source/core/fetch/RawResource.cpp
+++ third_party/WebKit/Source/core/fetch/RawResource.cpp
@@ -47,7 +47,6 @@ ResourcePtr<RawResource> RawResource::fetchImport(FetchRequest& request, Resourc
 {
     ASSERT(request.resourceRequest().frameType() == WebURLRequest::FrameTypeNone);
     request.mutableResourceRequest().setRequestContext(WebURLRequest::RequestContextImport);
-    RawResourceFactory factory(Resource::ImportResource);
     return toRawResource(fetcher->requestResource(request, RawResourceFactory(Resource::ImportResource)));
 }
 
