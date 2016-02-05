d023cd8269aff1c92eae0399396984d4a90929d1
mvanouwerkerk <mvanouwerkerk@chromium.org>
2016-02-05 10:35:57 -0800

    Disable testStopWhenVideoEnds for Jelly Bean.
    BUG=582067
    
    Review URL: https://codereview.chromium.org/1674743002
    
    Cr-Commit-Position: refs/heads/master@{#373855}

--- chrome/android/javatests/src/org/chromium/chrome/browser/media/remote/CastStartStopTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/media/remote/CastStartStopTest.java
@@ -9,6 +9,7 @@ import android.graphics.Rect;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.media.ui.MediaNotificationListener;
 import org.chromium.chrome.browser.tab.Tab;
@@ -70,6 +71,7 @@ public class CastStartStopTest extends CastTestBase {
     /*
      * Test that a cast session disconnects when the video ends
      */
+    @DisableIf.Build(sdk_is_less_than = 19, message = "crbug.com/582067")
     @Feature({"VideoFling"})
     @LargeTest
     public void testStopWhenVideoEnds() throws InterruptedException, TimeoutException {
