0097ff661bf0eee4729979cc5b91b8ba1b903bc8
majidvp <majidvp@chromium.org>
2016-02-05 08:56:30 -0800

    Add timestamp to fling's synthetic Wheel and GestureScrollUpate events
    Also fixed the type for wheel event.
    
    BUG=578243
    
    Review URL: https://codereview.chromium.org/1667293002
    
    Cr-Commit-Position: refs/heads/master@{#373830}

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -647,6 +647,8 @@ bool WebViewImpl::scrollBy(const WebFloatSize& delta, const WebFloatSize& veloci
         WebMouseWheelEvent syntheticWheel;
         const float tickDivisor = WheelEvent::TickMultiplier;
 
+        syntheticWheel.type = WebInputEvent::MouseWheel;
+        syntheticWheel.timeStampSeconds = WTF::monotonicallyIncreasingTime();
         syntheticWheel.deltaX = delta.width;
         syntheticWheel.deltaY = delta.height;
         syntheticWheel.wheelTicksX = delta.width / tickDivisor;
@@ -664,6 +666,7 @@ bool WebViewImpl::scrollBy(const WebFloatSize& delta, const WebFloatSize& veloci
         WebGestureEvent syntheticGestureEvent;
 
         syntheticGestureEvent.type = WebInputEvent::GestureScrollUpdate;
+        syntheticGestureEvent.timeStampSeconds = WTF::monotonicallyIncreasingTime();
         syntheticGestureEvent.data.scrollUpdate.preventPropagation = true;
         syntheticGestureEvent.data.scrollUpdate.deltaX = delta.width;
         syntheticGestureEvent.data.scrollUpdate.deltaY = delta.height;
