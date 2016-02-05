693cf4ca2dbcfa825d6b8da6141fca1600173299
shess <shess@chromium.org>
2016-02-05 10:25:29 -0800

    [sqlite] Fix bug in how recover virtual table handles empty.
    Previously the code assumed that the current cell was always less than
    the number of cells if it was a valid leaf page (see top of
    leafCursorCellDecode).  Empty tables have a single root page with no
    cells.
    
    No bugs have been seen which trace to this.  In testing what actually
    happens with a newly-created empty table, I found that
    leafCursorCellDecode exits with a ValidateError.  By inspection, I
    believe the most likely falsely valid result would be a single
    previously-valid row which has been deleted from the table.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1671773003
    
    Cr-Commit-Position: refs/heads/master@{#373853}

--- third_party/sqlite/amalgamation/sqlite3.c
+++ third_party/sqlite/amalgamation/sqlite3.c
@@ -137640,6 +137640,7 @@ struct RecoverLeafCursor {
  */
 static int leafCursorLoadPage(RecoverLeafCursor *pCursor, DbPage *pPage){
   const unsigned char *pPageHeader;  /* Header of *pPage */
+  unsigned nCells;                   /* Number of cells in the page */
 
   /* Release the current page. */
   if( pCursor->pPage ){
@@ -137669,10 +137670,17 @@ static int leafCursorLoadPage(RecoverLeafCursor *pCursor, DbPage *pPage){
     return SQLITE_OK;
   }
 
+  /* Leaf contains no data, skip it.  Empty tables, for instance. */
+  nCells = decodeUnsigned16(pPageHeader + kiPageCellCountOffset);;
+  if( nCells<1 ){
+    sqlite3PagerUnref(pPage);
+    return SQLITE_OK;
+  }
+
   /* Take ownership of the page and start decoding. */
   pCursor->pPage = pPage;
   pCursor->iCell = 0;
-  pCursor->nCells = decodeUnsigned16(pPageHeader + kiPageCellCountOffset);
+  pCursor->nCells = nCells;
   return SQLITE_OK;
 }
 
