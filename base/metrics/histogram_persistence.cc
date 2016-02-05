e0697420149a095ca208d1732c5590d30cf44478
bcwhite <bcwhite@chromium.org>
2016-02-05 13:55:41 -0800

    Recognize additional persistent-histogram failures.
    BUG=546019
    
    Review URL: https://codereview.chromium.org/1677643002
    
    Cr-Commit-Position: refs/heads/master@{#373920}

--- base/metrics/histogram_persistence.cc
+++ base/metrics/histogram_persistence.cc
@@ -44,6 +44,9 @@ enum CreateHistogramResultType {
   // Could not allocate histogram memory due to unknown error.
   CREATE_HISTOGRAM_ALLOCATOR_ERROR,
 
+  // Histogram was of unknown type.
+  CREATE_HISTOGRAM_UNKNOWN_TYPE,
+
   // Always keep this at the end.
   CREATE_HISTOGRAM_MAX
 };
@@ -237,6 +240,7 @@ HistogramBase* CreatePersistentHistogram(
           counts_data,
           histogram_data.bucket_count,
           &histogram_data_ptr->samples_metadata);
+      DCHECK(histogram);
       break;
     case LINEAR_HISTOGRAM:
       histogram = LinearHistogram::PersistentGet(
@@ -247,6 +251,7 @@ HistogramBase* CreatePersistentHistogram(
           counts_data,
           histogram_data.bucket_count,
           &histogram_data_ptr->samples_metadata);
+      DCHECK(histogram);
       break;
     case BOOLEAN_HISTOGRAM:
       histogram = BooleanHistogram::PersistentGet(
@@ -254,6 +259,7 @@ HistogramBase* CreatePersistentHistogram(
           ranges,
           counts_data,
           &histogram_data_ptr->samples_metadata);
+      DCHECK(histogram);
       break;
     case CUSTOM_HISTOGRAM:
       histogram = CustomHistogram::PersistentGet(
@@ -262,15 +268,20 @@ HistogramBase* CreatePersistentHistogram(
           counts_data,
           histogram_data.bucket_count,
           &histogram_data_ptr->samples_metadata);
+      DCHECK(histogram);
       break;
+    default:
+      NOTREACHED();
   }
 
   if (histogram) {
     DCHECK_EQ(histogram_data.histogram_type, histogram->GetHistogramType());
     histogram->SetFlags(histogram_data.flags);
+    RecordCreateHistogramResult(CREATE_HISTOGRAM_SUCCESS);
+  } else {
+    RecordCreateHistogramResult(CREATE_HISTOGRAM_UNKNOWN_TYPE);
   }
 
-  RecordCreateHistogramResult(CREATE_HISTOGRAM_SUCCESS);
   return histogram;
 }
 
@@ -396,6 +407,7 @@ HistogramBase* AllocatePersistentHistogram(
     result = CREATE_HISTOGRAM_ALLOCATOR_ERROR;
   }
   RecordCreateHistogramResult(result);
+  NOTREACHED() << "error=" << result;
 
   return nullptr;
 }
