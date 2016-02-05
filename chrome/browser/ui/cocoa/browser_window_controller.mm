3b2e0bfd9e9d9075b1e56bdc2a17bfcaf6dcbf48
spqchan <spqchan@chromium.org>
2016-02-04 16:59:18 -0800

    Preference for Presentation Mode/Fullscreen Toolbar
    Fixes the issue with the test on 10.10 for the commit: https://chromium.googlesource.com/chromium/src/+/048c293643242dc0357c8bc8651d1c251559a400
    
    BUG=544307
    
    Review URL: https://codereview.chromium.org/1650713003
    
    Cr-Commit-Position: refs/heads/master@{#373695}

--- chrome/browser/ui/cocoa/browser_window_controller.mm
+++ chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -82,6 +82,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/window_sizer/window_sizer.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/command.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/locale_settings.h"
@@ -435,6 +436,10 @@ void SetUpBrowserWindowCommandHandler(NSWindow* window) {
             [self window],
             extensions::ExtensionKeybindingRegistry::ALL_EXTENSIONS,
             windowShim_.get()));
+
+    PrefService* prefs = browser_->profile()->GetPrefs();
+    shouldHideFullscreenToolbar_ =
+        prefs->GetBoolean(prefs::kHideFullscreenToolbar);
 
     blockLayoutSubviews_ = NO;
 
@@ -1917,6 +1922,10 @@ willAnimateFromState:(BookmarkBar::State)oldState
   [sheet orderOut:self];
 }
 
+- (PresentationModeController*)presentationModeController {
+  return presentationModeController_.get();
+}
+
 - (void)executeExtensionCommand:(const std::string&)extension_id
                         command:(const extensions::Command&)command {
   // Global commands are handled by the ExtensionCommandsGlobalRegistry
@@ -1972,9 +1981,12 @@ willAnimateFromState:(BookmarkBar::State)oldState
   [self showFullscreenExitBubbleIfNecessary];
 }
 
-- (void)toggleFullscreenToolbar {
-  shouldHideFullscreenToolbar_ = !shouldHideFullscreenToolbar_;
+- (void)setFullscreenToolbarHidden:(BOOL)shouldHide {
+  if (shouldHideFullscreenToolbar_ == shouldHide)
+    return;
 
+  [presentationModeController_ setToolbarFraction:0.0];
+  shouldHideFullscreenToolbar_ = shouldHide;
   if ([self isInAppKitFullscreen])
     [self updateFullscreenWithToolbar:!shouldHideFullscreenToolbar_];
 }
--- chrome/browser/ui/cocoa/browser_window_controller_private.mm
+++ chrome/browser/ui/cocoa/browser_window_controller_private.mm
@@ -783,6 +783,7 @@ willPositionSheet:(NSWindow*)sheet
     [self deregisterForContentViewResizeNotifications];
 
   browser_->WindowFullscreenStateChanged();
+  [self.chromeContentView setAutoresizesSubviews:YES];
 
   [self resetCustomAppKitFullscreenVariables];
 
