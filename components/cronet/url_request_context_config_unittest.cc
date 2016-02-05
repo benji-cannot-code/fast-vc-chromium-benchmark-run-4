1b035a014f42d76cb3b852cd07d79c03dbc50cb7
sgurun <sgurun@chromium.org>
2016-02-04 17:37:26 -0800

    Disable failing unit test.
    BUG=584520
    
    TBR=pauljensen@chromium.org
    
    Review URL: https://codereview.chromium.org/1669033003
    
    Cr-Commit-Position: refs/heads/master@{#373709}

--- components/cronet/url_request_context_config_unittest.cc
+++ components/cronet/url_request_context_config_unittest.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cronet {
 
-TEST(URLRequestContextConfigTest, SetQuicExperimentalOptions) {
+TEST(URLRequestContextConfigTest, DISABLED_SetQuicExperimentalOptions) {
   URLRequestContextConfig config(
       // Enable QUIC.
       true,
