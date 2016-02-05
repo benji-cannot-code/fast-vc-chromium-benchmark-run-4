ce1a01073d824f0abac092320ae9ee5776d4bd05
ksakamoto <ksakamoto@chromium.org>
2016-02-04 20:06:12 -0800

    Cherry-pick woff2 revision d1efde9
    d1efde9: Update woff2_dec.cc range checking
    
    BUG=583563
    
    Review URL: https://codereview.chromium.org/1667413003
    
    Cr-Commit-Position: refs/heads/master@{#373741}

--- third_party/woff2/src/woff2_dec.cc
+++ third_party/woff2/src/woff2_dec.cc
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 
-#include "./buffer.h"
 #include "./decode.h"
+#include "./buffer.h"
 #include "./port.h"
 #include "./round.h"
 #include "./store_bytes.h"
@@ -884,7 +884,8 @@ bool ConvertWOFF2ToTTF(uint8_t* result, size_t result_length,
 
       for (uint32_t j = 0; j < num_tables; j++) {
         unsigned int table_idx;
-        if (PREDICT_FALSE(!Read255UShort(&file, &table_idx))) {
+        if (PREDICT_FALSE(!Read255UShort(&file, &table_idx)) ||
+            table_idx >= tables.size()) {
           return FONT_COMPRESSION_FAILURE();
         }
         ttc_font.table_indices[j] = table_idx;
