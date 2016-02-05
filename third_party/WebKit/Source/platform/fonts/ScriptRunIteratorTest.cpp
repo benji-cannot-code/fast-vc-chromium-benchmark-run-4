bc174fb7fcdaca9d15b181f283a63530bbb05f52
hiroshige <hiroshige@chromium.org>
2016-02-05 04:04:21 -0800

    Replace String::make16BitFrom8BitSource(0, 0) with emptyString16Bit()
    BUG=None
    
    Review URL: https://codereview.chromium.org/1667583002
    
    Cr-Commit-Position: refs/heads/master@{#373791}

--- third_party/WebKit/Source/platform/fonts/ScriptRunIteratorTest.cpp
+++ third_party/WebKit/Source/platform/fonts/ScriptRunIteratorTest.cpp
@@ -119,7 +119,7 @@ public:
 
     static String ToTestString(const std::string& input)
     {
-        String result(String::make16BitFrom8BitSource(0, 0));
+        String result(emptyString16Bit());
         bool inSet = false;
         int seen = 0;
         int code = 0;
@@ -288,7 +288,7 @@ class ScriptRunIteratorTest : public testing::Test {
 protected:
     void CheckRuns(const Vector<TestRun>& runs)
     {
-        String text(String::make16BitFrom8BitSource(0, 0));
+        String text(emptyString16Bit());
         Vector<ExpectedRun> expect;
         for (auto& run : runs) {
             text.append(String::fromUTF8(run.text.c_str()));
@@ -302,7 +302,7 @@ protected:
     // suitable equivalent real codepoint sequences instead.
     void CheckMockRuns(const Vector<TestRun>& runs)
     {
-        String text(String::make16BitFrom8BitSource(0, 0));
+        String text(emptyString16Bit());
         Vector<ExpectedRun> expect;
         for (const TestRun& run : runs) {
             text.append(MockScriptData::ToTestString(run.text));
@@ -333,7 +333,7 @@ protected:
 
 TEST_F(ScriptRunIteratorTest, Empty)
 {
-    String empty(String::make16BitFrom8BitSource(0, 0));
+    String empty(emptyString16Bit());
     ScriptRunIterator scriptRunIterator(empty.characters16(), empty.length());
     unsigned limit = 0;
     UScriptCode code = USCRIPT_INVALID_CODE;
