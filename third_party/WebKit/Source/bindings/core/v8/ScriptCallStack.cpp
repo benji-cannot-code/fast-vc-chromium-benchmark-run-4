2d7185ed8d03f1d2879607f7b587b11c42b3f8b7
kozyatinskiy <kozyatinskiy@chromium.org>
2016-02-05 13:30:59 -0800

    [DevTools] Fix crash in V8StackTraceImpl::capture
    Added missing ScriptForbiddenScope::AllowUserAgentScript.
    
    TBR=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/1664393003
    
    Cr-Commit-Position: refs/heads/master@{#373914}

--- third_party/WebKit/Source/bindings/core/v8/ScriptCallStack.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ScriptCallStack.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/ThreadDebugger.h"
 #include "core/inspector/v8/V8Debugger.h"
 #include "core/inspector/v8/V8StackTrace.h"
+#include "platform/ScriptForbiddenScope.h"
 #include "platform/TracedValue.h"
 
 namespace blink {
@@ -56,6 +57,7 @@ PassRefPtr<ScriptCallStack> ScriptCallStack::capture(size_t maxStackSize)
     V8PerIsolateData* data = V8PerIsolateData::from(isolate);
     if (!data->threadDebugger())
         return nullptr;
+    ScriptForbiddenScope::AllowUserAgentScript allowScripting;
     OwnPtr<V8StackTrace> stack = data->threadDebugger()->debugger()->captureStackTrace(maxStackSize);
     return stack ? adoptRef(new ScriptCallStack(stack.release())) : nullptr;
 }
