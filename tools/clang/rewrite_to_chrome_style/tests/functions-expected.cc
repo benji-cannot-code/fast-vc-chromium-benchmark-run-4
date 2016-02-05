6fce80cb9f54fd4a733f0ea2462588e2a076810f
danakj <danakj@chromium.org>
2016-02-05 13:10:39 -0800

    rewrite_to_chrome_style: Don't rename swap() functions.
    These are used for ADL tricks with std::swap, and need to stay
    lowercase.
    
    R=dcheng
    BUG=584526
    
    Review URL: https://codereview.chromium.org/1676463004
    
    Cr-Commit-Position: refs/heads/master@{#373911}

--- tools/clang/rewrite_to_chrome_style/tests/functions-expected.cc
+++ tools/clang/rewrite_to_chrome_style/tests/functions-expected.cc
@@ -26,6 +26,11 @@ void Begin() {}
 void Trace() {}
 void Lock() {}
 
+class SwapType {};
+
+// swap() functions are not renamed.
+void swap(SwapType& a, SwapType& b) {}
+
 // Note: F is already Google style and should not change.
 void F() {
   // Test referencing a function without calling it.
@@ -38,4 +43,7 @@ using blink::TestFunctionThatTakesTwoInts;
 
 void G() {
   TestFunctionThatTakesTwoInts(1, 2);
+
+  blink::SwapType a, b;
+  swap(a, b);
 }
