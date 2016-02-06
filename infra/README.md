9962e4b8f01ae73425a09ab617ec0a5ce77f4b73
luqui <luqui@chromium.org>
2016-02-05 17:41:59 -0800

    Made chromium into a recipe package, and added canary recipe.
    This is a small step on the path to src-side recipes in chromium.
    
    BUG=584079
    R=iannucci@chromium.org,martiniss@chromium.org
    CC=kbr@chromium.org,dpranke@chromium.org,phajdan.jr@chromium.org
    
    Review URL: https://codereview.chromium.org/1660853004
    
    Cr-Commit-Position: refs/heads/master@{#373988}

--- infra/README.md
+++ infra/README.md
@@ -1,2 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 This directory contains chrome-infra-specific files.
+
+This directory also contains some recipes, but don't get too excited, we're
+still getting support ready; for the time being chromium recipes should still
+live in tools/build.
---
+++ infra/recipes/canary.py
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A recipe that just checks out chromium, for canarying src-side recipes."""
+
+DEPS = [
+  'recipe_engine/properties',
+  'depot_tools/bot_update',
+  'depot_tools/gclient',
+]
+
+def RunSteps(api):
+  api.gclient.set_config('chromium')
+  api.bot_update.ensure_checkout(force=True)
+
+def GenTests(api):
+  yield api.test('basic') + api.properties.generic()
