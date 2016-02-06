2f9862b4e7a9bf523f5683383652c313b85072ba
skobes <skobes@chromium.org>
2016-02-05 18:42:23 -0800

    Implement findAnchor and computeRelativeOffset in ScrollAnchor.
    BUG=558575
    
    Review URL: https://codereview.chromium.org/1640973005
    
    Cr-Commit-Position: refs/heads/master@{#373998}

--- third_party/WebKit/Source/core/layout/ScrollAnchor.cpp
+++ third_party/WebKit/Source/core/layout/ScrollAnchor.cpp
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/layout/ScrollAnchor.h"
 
-#include "core/layout/LayoutObject.h"
-#include "platform/scroll/ScrollableArea.h"
+#include "core/frame/FrameView.h"
+#include "core/layout/LayoutView.h"
+#include "core/paint/PaintLayerScrollableArea.h"
 #include "wtf/Assertions.h"
 
 namespace blink {
@@ -23,26 +24,51 @@ ScrollAnchor::~ScrollAnchor()
 {
 }
 
+static LayoutBox* scrollerLayoutBox(const ScrollableArea* scroller)
+{
+    LayoutBox* box = scroller->isFrameView()
+        ? toFrameView(scroller)->layoutView()
+        : &toPaintLayerScrollableArea(scroller)->box();
+    ASSERT(box);
+    return box;
+}
+
 static LayoutObject* findAnchor(const ScrollableArea* scroller)
 {
-    // TODO(skobes): implement.
-    return nullptr;
+    LayoutBox* scrollerBox = scrollerLayoutBox(scroller);
+
+    FloatRect absoluteVisibleRect = scroller->isFrameView()
+        ? scroller->visibleContentRect()
+        : scrollerBox->localToAbsoluteQuad(
+            FloatQuad(FloatRect(scrollerBox->overflowClipRect(LayoutPoint())))).boundingBox();
+
+    LayoutObject* child = scrollerBox->nextInPreOrder(scrollerBox);
+    LayoutObject* candidate = nullptr;
+    while (child) {
+        // TODO(skobes): Compute scroller-relative bounds instead of absolute bounds.
+        FloatRect childRect = child->absoluteBoundingBoxFloatRect();
+        if (absoluteVisibleRect.intersects(childRect))
+            candidate = child;
+        if (absoluteVisibleRect.contains(childRect))
+            break;
+        child = child->nextInPreOrder(scrollerBox);
+    }
+    return candidate;
 }
 
 static DoublePoint computeRelativeOffset(const ScrollableArea* scroller, const LayoutObject* layoutObject)
 {
-    // TODO(skobes): implement.
-    return DoublePoint();
+    return DoublePoint(layoutObject->localToAbsolute() - scrollerLayoutBox(scroller)->localToAbsolute());
 }
 
 void ScrollAnchor::save()
 {
-    if (!m_anchorObject) {
-        m_anchorObject = findAnchor(m_scroller);
-        if (!m_anchorObject)
-            return;
-        m_anchorObject->setIsScrollAnchorObject();
-    }
+    if (m_anchorObject)
+        return;
+    m_anchorObject = findAnchor(m_scroller);
+    if (!m_anchorObject)
+        return;
+    m_anchorObject->setIsScrollAnchorObject();
     m_savedRelativeOffset = computeRelativeOffset(m_scroller, m_anchorObject);
 }
 
