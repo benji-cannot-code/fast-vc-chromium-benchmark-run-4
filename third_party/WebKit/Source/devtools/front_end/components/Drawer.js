0463d239bf2766cd8ab93c719846682dce6aa668
kozyatinskiy <kozyatinskiy@chromium.org>
2016-02-06 00:36:46 -0800

    Revert "Devtools: Add metrics for drawer views being shown"
    This reverts commit d4bebfb49bc2fad42b8b1a1bf92ade6a17377a25.
    
    This change broke old frontends with new backends in case of remote debugging.
    We've check of allowable histogram classes in DevToolsUIBindings::RecordEnumeratedHistogram and unfortunately can't add new classes.
    
    Original CL: https://codereview.chromium.org/1520403004
    > Devtools: Add metrics for drawer views being shown
    
    > Given that drawer views are taking an increasingly dominant UI position,
    > broad metric should be added like panels shown.
    
    > Committed: https://crrev.com/d4bebfb49bc2fad42b8b1a1bf92ade6a17377a25
    > Cr-Commit-Position: refs/heads/master@{#367455}
    
    TBR=holte@chromium.org, pfeldman@chromium.org
    BUG=584795
    
    Review URL: https://codereview.chromium.org/1672243003
    
    Cr-Commit-Position: refs/heads/master@{#374020}

--- third_party/WebKit/Source/devtools/front_end/components/Drawer.js
+++ third_party/WebKit/Source/devtools/front_end/components/Drawer.js
@@ -69,7 +69,6 @@ WebInspector.Drawer.prototype = {
     showView: function(id, immediate)
     {
         this._innerShow(immediate);
-        WebInspector.userMetrics.drawerShown(id);
         return this._extensibleTabbedPaneController.showTab(id);
     },
 
