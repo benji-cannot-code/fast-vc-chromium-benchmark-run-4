83bb7ddde1925579d75db2f480c05a3d8b21d6b1
jyasskin <jyasskin@chromium.org>
2016-02-05 20:53:22 -0800

    Revert of Fix static lock creation to be thread-safe. (patchset #1 id:1 of https://codereview.chromium.org/1670193002/ )
    Reason for revert:
    This still has a race on the construction of Iterator:
    
    https://build.chromium.org/p/chromium.memory.fyi/builders/Linux%20TSan%20Tests/builds/16709/steps/net_unittests/logs/stdio
    
    WARNING: ThreadSanitizer: data race (pid=10094)
      Read of size 1 at 0x000004878628 by main thread:
        #0 base::ImportPersistentHistograms() base/metrics/histogram_persistence.cc:402 (net_unittests+0x000001decfd5)
        #1 base::Histogram::Factory::Build() base/metrics/histogram.cc:154:3 (net_unittests+0x000001de8011)
    
      Previous write of size 1 at 0x000004878628 by thread T1:
        #0 Iterator base/metrics/persistent_memory_allocator.h:54:18 (net_unittests+0x000001ded001)
        #1 base::ImportPersistentHistograms() base/metrics/histogram_persistence.cc:406 (net_unittests+0x000001ded001)
        #2 base::Histogram::Factory::Build() base/metrics/histogram.cc:154:3 (net_unittests+0x000001de8011)
    
    Original issue's description:
    > Fix static lock creation to be thread-safe.
    >
    > BUG=546019
    >
    > Committed: https://crrev.com/7f2c259b80c17ea6bb1ba965471414ccc4002903
    > Cr-Commit-Position: refs/heads/master@{#373857}
    
    TBR=asvitkine@chromium.org,bcwhite@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    BUG=546019
    
    Review URL: https://codereview.chromium.org/1675913002
    
    Cr-Commit-Position: refs/heads/master@{#374007}

--- base/metrics/histogram_persistence.cc
+++ base/metrics/histogram_persistence.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_persistence.h"
 
-#include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/metrics/histogram.h"
@@ -415,12 +414,12 @@ HistogramBase* AllocatePersistentHistogram(
 void ImportPersistentHistograms() {
   // Each call resumes from where it last left off so need persistant iterator.
   // The lock protects against concurrent access to the iterator and is created
-  // in a thread-safe manner when needed.
+  // dynamically so as to not require destruction during program exit.
   static PersistentMemoryAllocator::Iterator iter;
-  static base::LazyInstance<base::Lock> lock = LAZY_INSTANCE_INITIALIZER;
+  static base::Lock* lock = new base::Lock();
 
   if (g_allocator) {
-    base::AutoLock auto_lock(lock.Get());
+    base::AutoLock auto_lock(*lock);
     if (iter.is_clear())
       g_allocator->CreateIterator(&iter);
 
