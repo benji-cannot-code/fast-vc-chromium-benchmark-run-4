a05cac3d60c9b375b71bbcc7f6ee2ddc9ed34185
kojii <kojii@chromium.org>
2016-02-06 01:34:38 -0800

    Import web-platform-tests@27e3d93f88a71a249d1df872a5d613b3243b9588
    Using update-w3c-deps in Blink cfa0c97600bb1dc697a8f2e854da0c971d3dd513.
    
    TBR=dpranke@chromium.org,jsbell@chromium.org,tkent@chromium.org,philipj@opera.com
    BUG=490511
    
    Review URL: https://codereview.chromium.org/1666363003
    
    Cr-Commit-Position: refs/heads/master@{#374024}

--- third_party/WebKit/LayoutTests/imported/web-platform-tests/html/dom/elements-obsolete.js
+++ third_party/WebKit/LayoutTests/imported/web-platform-tests/html/dom/elements-obsolete.js
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Up-to-date as of 2013-04-13.
 var obsoleteElements = {
+  // https://html.spec.whatwg.org/multipage/#the-applet-element
   applet: {
     align: "string",
     alt: "string",
@@ -14,6 +15,7 @@ var obsoleteElements = {
     vspace: "unsigned long",
     width: "string",
   },
+  // https://html.spec.whatwg.org/multipage/#the-marquee-element-2
   marquee: {
     behavior: "string",
     bgColor: "string",
@@ -26,10 +28,12 @@ var obsoleteElements = {
     vspace: "unsigned long",
     width: "string",
   },
+  // https://html.spec.whatwg.org/multipage/#frameset
   frameset: {
     cols: "string",
     rows: "string",
   },
+  // https://html.spec.whatwg.org/multipage/#frame
   frame: {
     name: "string",
     scrolling: "string",
@@ -40,9 +44,11 @@ var obsoleteElements = {
     marginHeight: {type: "string", treatNullAsEmptyString: true},
     marginWidth: {type: "string", treatNullAsEmptyString: true},
   },
+  // https://html.spec.whatwg.org/multipage/#htmldirectoryelement
   dir: {
     compact: "boolean",
   },
+  // https://html.spec.whatwg.org/multipage/#htmlfontelement
   font: {
     color: {type: "string", treatNullAsEmptyString: true},
     face: "string",
