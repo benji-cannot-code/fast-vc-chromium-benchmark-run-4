91fa9ea6a4d07424bbe1bb41f350794231ad9568
gayane <gayane@chromium.org>
2016-02-08 09:04:44 -0800

    When browser_options is gone don't execute the callback for updating UI.
    Otherwise it results in crash in that case.
    
    BUG=488815
    
    Review URL: https://codereview.chromium.org/1671543003
    
    Cr-Commit-Position: refs/heads/master@{#374138}

--- chrome/browser/ui/webui/options/browser_options_handler.cc
+++ chrome/browser/ui/webui/options/browser_options_handler.cc
@@ -2118,7 +2118,7 @@ void BrowserOptionsHandler::HandleMetricsReportingChange(
   InitiateMetricsReportingChange(
       enable,
       base::Bind(&BrowserOptionsHandler::NotifyUIOfMetricsReportingChange,
-                 base::Unretained(this)));
+                 weak_ptr_factory_.GetWeakPtr()));
 }
 
 void BrowserOptionsHandler::NotifyUIOfMetricsReportingChange(bool enabled) {
