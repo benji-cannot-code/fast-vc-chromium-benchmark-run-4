6ab1a40fd63d28061497111f294c24dffd6f2640
pauljensen <pauljensen@chromium.org>
2016-02-08 08:46:22 -0800

    [Cronet] Remove deprecated UrlRequest.read() API
    All embedders should be switched to readNew() at this point.
    A future CL will rename readNew() to read().
    
    BUG=531538
    
    Review URL: https://codereview.chromium.org/1670233002
    
    Cr-Commit-Position: refs/heads/master@{#374134}

--- components/cronet/android/sample/src/org/chromium/cronet_sample_apk/CronetSampleActivity.java
+++ components/cronet/android/sample/src/org/chromium/cronet_sample_apk/CronetSampleActivity.java
@@ -59,12 +59,13 @@ public class CronetSampleActivity extends Activity {
             Log.i(TAG, "****** Response Started ******");
             Log.i(TAG, "*** Headers Are *** %s", info.getAllHeaders());
 
-            request.read(ByteBuffer.allocateDirect(32 * 1024));
+            request.readNew(ByteBuffer.allocateDirect(32 * 1024));
         }
 
         @Override
         public void onReadCompleted(
                 UrlRequest request, UrlResponseInfo info, ByteBuffer byteBuffer) {
+            byteBuffer.flip();
             Log.i(TAG, "****** onReadCompleted ******%s", byteBuffer);
 
             try {
@@ -72,8 +73,8 @@ public class CronetSampleActivity extends Activity {
             } catch (IOException e) {
                 Log.i(TAG, "IOException during ByteBuffer read. Details: ", e);
             }
-            byteBuffer.position(0);
-            request.read(byteBuffer);
+            byteBuffer.clear();
+            request.readNew(byteBuffer);
         }
 
         @Override
