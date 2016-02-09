4ad87129d3ad93ec68788afbb24cdd055c03fe8f
iclelland <iclelland@chromium.org>
2016-02-08 20:26:09 -0800

    [Background Sync] Remove dead GetRegistration code
    BUG=583650
    
    Review URL: https://codereview.chromium.org/1661663003
    
    Cr-Commit-Position: refs/heads/master@{#374297}

--- content/child/background_sync/background_sync_provider.h
+++ content/child/background_sync/background_sync_provider.h
@@ -55,10 +55,6 @@ class BackgroundSyncProvider : public blink::WebSyncProvider,
       int64_t handle_id,
       blink::WebServiceWorkerRegistration* service_worker_registration,
       blink::WebSyncUnregistrationCallbacks* callbacks) override;
-  void getRegistration(
-      const blink::WebString& tag,
-      blink::WebServiceWorkerRegistration* service_worker_registration,
-      blink::WebSyncRegistrationCallbacks* callbacks) override;
   void getRegistrations(
       blink::WebServiceWorkerRegistration* service_worker_registration,
       blink::WebSyncGetRegistrationsCallbacks* callbacks) override;
@@ -82,10 +78,6 @@ class BackgroundSyncProvider : public blink::WebSyncProvider,
   void UnregisterCallback(
       scoped_ptr<blink::WebSyncUnregistrationCallbacks> callbacks,
       BackgroundSyncError error);
-  void GetRegistrationCallback(
-      scoped_ptr<blink::WebSyncRegistrationCallbacks> callbacks,
-      BackgroundSyncError error,
-      const SyncRegistrationPtr& options);
   void GetRegistrationsCallback(
       scoped_ptr<blink::WebSyncGetRegistrationsCallbacks> callbacks,
       BackgroundSyncError error,
