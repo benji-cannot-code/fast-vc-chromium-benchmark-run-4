d7d91f149a18119c3ab8a4708c2cc8002bf08e04
yosin <yosin@chromium.org>
2016-02-08 23:27:56 -0800

    Fix build error caused by core/editing/commands/EditingState.h
    This patch fixed following compilation error introduced by crrev.com/374111
    in  chromeos_x86-generic_chromium_compile_only_ng
    
    FAILED: /b/build/goma/gomacc i686-pc-linux-gnu-g++ -B/b/build/slave/chromeos_x86-generic/build/.cros_cache/chrome-sdk/tarballs/x86-generic+7904.0.0+target_toolchain/usr/x86_64-pc-linux-gnu/i686-pc-linux-gnu/binutils-bin/2.25.51-gold -MMD -MF obj/third_party/WebKit/Source/core/editing/commands/webcore_remaining.ApplyBlockElementCommand.o.d ... -fPIC -Wno-unused-local-typedefs -msse2 -mfpmath=sse -mmmx -m32 --sysroot=/b/build/slave/chromeos_x86-generic/build/.cros_cache/chrome-sdk/tarballs/x86-generic+7904.0.0+sysroot_chromeos-base_chromeos-chrome.tar.xz -O2 -fno-ident -fdata-sections -ffunction-sections -funwind-tables -march=i686 -pipe -march=i686 -pipe -pipe -march=i686 -mfpmath=sse -mmmx -msse -msse2 -msse3 -D__google_stl_debug_vector=1 -fno-exceptions -fno-rtti -fno-threadsafe-statics -fvisibility-inlines-hidden -std=gnu++11 -Wno-narrowing -Wno-literal-suffix  -c ../../third_party/WebKit/Source/core/editing/commands/ApplyBlockElementCommand.cpp -o obj/third_party/WebKit/Source/core/editing/commands/webcore_remaining.ApplyBlockElementCommand.o
    In file included from ../../third_party/WebKit/Source/core/editing/commands/CompositeEditCommand.h:31:0,
                     from ../../third_party/WebKit/Source/core/editing/commands/ApplyBlockElementCommand.h:35,
                     from ../../third_party/WebKit/Source/core/editing/commands/ApplyBlockElementCommand.cpp:27:
    ../../third_party/WebKit/Source/core/editing/commands/EditingState.h:45:19: error: 'blink::EditingState* blink::IgnorableEditingAbortState::editingState()' is private
         EditingState* editingState() { return &m_editingState; }
                       ^
    ../../third_party/WebKit/Source/core/editing/commands/EditingState.h:90:76: error: within this context
     #define ASSERT_NO_EDITING_ABORT (IgnorableEditingAbortState().editingState())
    
    TBR=yosin@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1678263003
    
    Cr-Commit-Position: refs/heads/master@{#374332}

--- third_party/WebKit/Source/core/editing/commands/EditingState.h
+++ third_party/WebKit/Source/core/editing/commands/EditingState.h
@@ -43,11 +43,11 @@ class IgnorableEditingAbortState final {
     STACK_ALLOCATED();
     WTF_MAKE_NONCOPYABLE(IgnorableEditingAbortState);
 
-    EditingState* editingState() { return &m_editingState; }
-
 public:
     IgnorableEditingAbortState();
     ~IgnorableEditingAbortState();
+
+    EditingState* editingState() { return &m_editingState; }
 
 private:
     EditingState m_editingState;
