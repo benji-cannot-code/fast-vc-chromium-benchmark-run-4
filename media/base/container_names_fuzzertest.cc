0d465a8fce74c6bb6a6683990a9dfb992d9af386
jrummell <jrummell@chromium.org>
2016-02-09 14:12:42 -0800

    Add fuzzer for container_names
    TEST=fuzzer test runs
    
    Review URL: https://codereview.chromium.org/1655423007
    
    Cr-Commit-Position: refs/heads/master@{#374494}

---
+++ media/base/container_names_fuzzertest.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+
+#include "media/base/container_names.h"
+
+// Entry point for LibFuzzer.
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  media::container_names::DetermineContainer(data, size);
+  return 0;
+}
