fb5ecedf3679f5f5d51dcc34fd8d1d715c3ce1a7
mikecase <mikecase@chromium.org>
2016-02-08 20:24:44 -0800

    [Android] Add --logcat-output-file option.
    This option will make it much easier to add logcats to
    individual steps in recipes.
    
    BUG=583798
    
    Review URL: https://codereview.chromium.org/1663933003
    
    Cr-Commit-Position: refs/heads/master@{#374295}

--- build/android/pylib/local/device/local_device_environment.py
+++ build/android/pylib/local/device/local_device_environment.py
@@ -6,12 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import datetime
 import logging
 import os
+import shutil
+import tempfile
 import threading
 
 from devil.android import device_blacklist
 from devil.android import device_errors
 from devil.android import device_utils
 from devil.android import logcat_monitor
+from devil.utils import file_utils
 from devil.utils import parallelizer
 from pylib import constants
 from pylib.base import environment
@@ -38,6 +41,7 @@ class LocalDeviceEnvironment(environment.Environment):
     self._incremental_install = args.incremental_install
     self._concurrent_adb = args.enable_concurrent_adb
     self._logcat_output_dir = args.logcat_output_dir
+    self._logcat_output_file = args.logcat_output_file
     self._logcat_monitors = []
 
   #override
@@ -63,6 +67,8 @@ class LocalDeviceEnvironment(environment.Environment):
           with open(cache_path) as f:
             d.LoadCacheData(f.read())
           os.unlink(cache_path)
+    if self._logcat_output_file:
+      self._logcat_output_dir = tempfile.mkdtemp()
     if self._logcat_output_dir:
       for d in self._devices:
         logcat_file = os.path.join(
@@ -113,6 +119,11 @@ class LocalDeviceEnvironment(environment.Environment):
     for m in self._logcat_monitors:
       m.Stop()
       m.Close()
+    if self._logcat_output_file:
+      file_utils.MergeFiles(
+          self._logcat_output_file,
+          [m.output_file for m in self._logcat_monitors])
+      shutil.rmtree(self._logcat_output_dir)
 
   def BlacklistDevice(self, device, reason='local_device_failure'):
     device_serial = device.adb.GetDeviceSerial()
