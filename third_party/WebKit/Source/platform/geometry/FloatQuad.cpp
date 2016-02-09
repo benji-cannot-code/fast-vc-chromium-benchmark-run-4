8969696da968de4f82ec602b4d2f8a7cc41200ff
jonross <jonross@chromium.org>
2016-02-09 10:28:30 -0800

    Fix FloatQuad compilation error Follow up to https://codereview.chromium.org/1678563002/ which breaks compilation when using GN build with the following settings: is_component_build = false is_asan = true is_debug = true use_goma = true enable_nacl = false
    BUG=583470
    
    Review URL: https://codereview.chromium.org/1678103005
    
    Cr-Commit-Position: refs/heads/master@{#374417}

--- third_party/WebKit/Source/platform/geometry/FloatQuad.cpp
+++ third_party/WebKit/Source/platform/geometry/FloatQuad.cpp
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cmath>
 #include <limits>
 
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
+
 namespace blink {
 
 static inline float min4(float a, float b, float c, float d)
