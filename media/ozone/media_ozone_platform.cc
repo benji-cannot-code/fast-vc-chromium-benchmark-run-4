9af04cd5d6f838463f0b39b2646fbde1bcfbeff3
kylechar <kylechar@chromium.org>
2016-02-09 12:45:33 -0800

    Implement core of Ozone X11 platform.
    This CL adds the boilerplate necessary for Ozone X11 platform. All the
    complex logic is skipped in favour of TODOs. This is to simplify the
    next CL containing PlatformWindow and PlatformEventSource changes.
    
    BUG=361137
    
    Review URL: https://codereview.chromium.org/1676933003
    
    Cr-Commit-Position: refs/heads/master@{#374461}

--- media/ozone/media_ozone_platform.cc
+++ media/ozone/media_ozone_platform.cc
@@ -62,6 +62,10 @@ MediaOzonePlatform* CreateMediaOzonePlatformWayland() {
   return new MediaOzonePlatformStub;
 }
 
+MediaOzonePlatform* CreateMediaOzonePlatformX11() {
+  return new MediaOzonePlatformStub;
+}
+
 MediaOzonePlatform::MediaOzonePlatform() {
   CHECK(!instance_) << "There should only be a single MediaOzonePlatform.";
   instance_ = this;
---
+++ ui/ozone/platform/x11/client_native_pixmap_factory_x11.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_OZONE_PLATFORM_X11_CLIENT_NATIVE_PIXMAP_FACTORY_X11_H_
+#define UI_OZONE_PLATFORM_X11_CLIENT_NATIVE_PIXMAP_FACTORY_X11_H_
+
+namespace ui {
+
+class ClientNativePixmapFactory;
+
+// Constructor hook for use in constructor_list.cc
+ClientNativePixmapFactory* CreateClientNativePixmapFactoryX11();
+
+}  // namespace ui
+
+#endif  // UI_OZONE_PLATFORM_X11_CLIENT_NATIVE_PIXMAP_FACTORY_X11_H_
