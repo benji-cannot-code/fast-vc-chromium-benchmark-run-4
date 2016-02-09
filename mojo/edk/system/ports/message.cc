e47fae0b56118f223c3b2051dcff16fd60c7b3fa
rockot <rockot@chromium.org>
2016-02-08 22:23:02 -0800

    [mojo-edk] Simplify multiprocess pipe bootstrap
    This introduces a new MergePort message at the Ports layer
    for joining two independent port cycles which each have
    an unused (i.e. unwritten, unread, unsent) receiving port.
    
    MergePort allows us to create a MessagePipeDispatcher which
    is immediately usable but which will eventually be linked to
    a MessagePipeDispatcher on another port cycle, potentially in
    another process.
    
    The basic idea is to create a fully functional port pair but
    only bind one port to an MPD. Do this on each end and
    merge the dangling ports asynchronously.
    
    The simplification here allows a lot of code to be deleted
    from NodeController, some of which is deleted in this CL.
    
    Future work will convert existing bootstrap sites back to
    using synchronous bootstrap, including the token-based APIs.
    
    BUG=584764
    TBR=ben@chromium.org for null check in mash shell
    
    Review URL: https://codereview.chromium.org/1675603002
    
    Cr-Commit-Position: refs/heads/master@{#374322}

--- mojo/edk/system/ports/message.cc
+++ mojo/edk/system/ports/message.cc
@@ -38,6 +38,9 @@ void Message::Parse(const void* bytes,
     case EventType::kObserveClosure:
       *num_header_bytes = sizeof(EventHeader) + sizeof(ObserveClosureEventData);
       break;
+    case EventType::kMergePort:
+      *num_header_bytes = sizeof(EventHeader) + sizeof(MergePortEventData);
+      break;
     default:
       CHECK(false) << "Bad event type";
       return;
--- mojo/edk/system/ports/name.h
+++ mojo/edk/system/ports/name.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <ostream>
+#include <tuple>
 
 namespace mojo {
 namespace edk {
@@ -22,9 +23,15 @@ struct Name {
 inline bool operator==(const Name& a, const Name& b) {
   return a.v1 == b.v1 && a.v2 == b.v2;
 }
+
 inline bool operator!=(const Name& a, const Name& b) {
   return !(a == b);
 }
+
+inline bool operator<(const Name& a, const Name& b) {
+  return std::tie(a.v1, a.v2) < std::tie(b.v1, b.v2);
+}
+
 std::ostream& operator<<(std::ostream& stream, const Name& name);
 
 struct PortName : Name {
