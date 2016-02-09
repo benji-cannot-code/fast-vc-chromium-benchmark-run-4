d863d1b9c67e7ab4ee4405d208b8885b0d32fda5
chrishtr <chrishtr@chromium.org>
2016-02-08 16:57:21 -0800

    Don't allow dirtying layout during paint of a plugin placeholder.
    This should not happen anyway, but adding defensive checks may uncover code
    which does it anyway.
    
    BUG=545039
    
    Review URL: https://codereview.chromium.org/1677063004
    
    Cr-Commit-Position: refs/heads/master@{#374246}

--- components/plugins/renderer/webview_plugin.cc
+++ components/plugins/renderer/webview_plugin.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include "base/auto_reset.h"
 #include "base/message_loop/message_loop.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/numerics/safe_conversions.h"
@@ -56,7 +57,8 @@ WebViewPlugin::WebViewPlugin(content::RenderView* render_view,
       container_(nullptr),
       web_view_(WebView::create(this)),
       finished_loading_(false),
-      focused_(false) {
+      focused_(false),
+      is_painting_(false) {
   // ApplyWebPreferences before making a WebLocalFrame so that the frame sees a
   // consistent view of our preferences.
   content::RenderView::ApplyWebPreferences(preferences, web_view_);
@@ -172,6 +174,9 @@ void WebViewPlugin::paint(WebCanvas* canvas, const WebRect& rect) {
   gfx::Rect paint_rect = gfx::IntersectRects(rect_, rect);
   if (paint_rect.IsEmpty())
     return;
+
+  base::AutoReset<bool> is_painting(
+        &is_painting_, true);
 
   paint_rect.Offset(-rect_.x(), -rect_.y());
 
@@ -289,8 +294,11 @@ void WebViewPlugin::didChangeCursor(const WebCursorInfo& cursor) {
 }
 
 void WebViewPlugin::scheduleAnimation() {
-  if (container_)
+  if (container_) {
+    // This should never happen; see also crbug.com/545039 for context.
+    CHECK(!is_painting_);
     container_->setNeedsLayout();
+  }
 }
 
 void WebViewPlugin::didClearWindowObject(WebLocalFrame* frame) {
