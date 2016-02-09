8a9c4ee1739ae0db0346c974bf6fa9a953c4fbe7
jww <jww@chromium.org>
2016-02-09 00:19:04 -0800

    Fix SRI bypass by loading same resource twice in same origin.
    This fixes a bug where the memory cache was bypassing subresource
    integrity checks when a resource is loaded for a second time in the same
    origin. The resource in the memory cache was correctly storing that an
    integrity check had already been done so whene it was retrieved later,
    it wouldn't need to be checked again, but it didn't store the fact that
    this was a *failure*, so when the load happened a second time, it
    assumed it was a good integrity.
    
    This modifies the resources to store a disposition for the integrity
    check, rather than just that the integrity check occurred. On a reload
    of the resource, if the integrity had failed the first time, the
    resource will fail to load.
    
    BUG=584155
    
    Review URL: https://codereview.chromium.org/1675183003
    
    Cr-Commit-Position: refs/heads/master@{#374336}

--- third_party/WebKit/Source/core/fetch/ScriptResource.h
+++ third_party/WebKit/Source/core/fetch/ScriptResource.h
@@ -35,6 +35,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+enum class ScriptIntegrityDisposition {
+    NotChecked = 0,
+    Failed,
+    Passed
+};
+
 class FetchRequest;
 class ScriptResource;
 
@@ -75,8 +81,9 @@ public:
 
     void setIntegrityMetadata(const IntegrityMetadataSet& metadata) { m_integrityMetadata = metadata; }
     const IntegrityMetadataSet& integrityMetadata() const { return m_integrityMetadata; }
-    void setIntegrityAlreadyChecked(bool checked) { m_integrityChecked = checked; }
-    bool integrityAlreadyChecked() { return m_integrityChecked; }
+    // The argument must never be |NotChecked|.
+    void setIntegrityDisposition(ScriptIntegrityDisposition);
+    ScriptIntegrityDisposition integrityDisposition() { return m_integrityDisposition; }
     bool mustRefetchDueToIntegrityMetadata(const FetchRequest&) const override;
 
 private:
@@ -93,7 +100,7 @@ private:
 
     ScriptResource(const ResourceRequest&, const String& charset);
 
-    bool m_integrityChecked;
+    ScriptIntegrityDisposition m_integrityDisposition;
     IntegrityMetadataSet m_integrityMetadata;
 
     CompressibleString m_script;
