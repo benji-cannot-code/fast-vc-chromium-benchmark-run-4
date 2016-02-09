81017dedf77707d75759f3c48a20566f0e55f128
aberent <aberent@chromium.org>
2016-02-09 04:01:47 -0800

    Clean up RemoteMediaPlayerBridge
    This removes the derivation from MediaPlayerBridge, and also removes
    the listeners. It also fixes a minor bug in the error handling.
    
    BUG=577110
    
    Review URL: https://codereview.chromium.org/1679863002
    
    Cr-Commit-Position: refs/heads/master@{#374352}

--- chrome/android/java/src/org/chromium/chrome/browser/media/remote/RemoteMediaPlayerBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/remote/RemoteMediaPlayerBridge.java
@@ -5,11 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.media.remote;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.media.MediaPlayer;
-import android.os.Build;
 
 import org.chromium.base.CommandLine;
 import org.chromium.base.Log;
@@ -17,26 +13,14 @@ import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.media.remote.RemoteVideoInfo.PlayerState;
-import org.chromium.media.MediaPlayerBridge;
 
 /**
  * Acts as a proxy between the remotely playing video and the HTMLMediaElement.
- *
- * Note that the only reason this derives from MediaPlayerBridge is that the
- * MediaPlayerListener takes a MediaPlayerBridge in its constructor.
- * TODO(aberent) fix this by creating a MediaPlayerBridgeInterface (or similar).
  */
 @JNINamespace("remote_media")
-public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
+public class RemoteMediaPlayerBridge {
     private long mStartPositionMillis;
     private long mNativeRemoteMediaPlayerBridge;
-
-    // TODO(dgn) We don't create MediaPlayerListener using a RemoteMediaPlayerBridge anymore so
-    //           the inheritance and the extra listeners can now go away. (https://crbug.com/577110)
-    private MediaPlayer.OnCompletionListener mOnCompletionListener;
-    private MediaPlayer.OnSeekCompleteListener mOnSeekCompleteListener;
-    private MediaPlayer.OnErrorListener mOnErrorListener;
-    private MediaPlayer.OnPreparedListener mOnPreparedListener;
 
     /**
      * The route controller for the video, null if no appropriate route controller.
@@ -72,12 +56,8 @@ public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
 
         @Override
         public void onError() {
-            if (mActive && mOnErrorListener != null) {
-                @SuppressLint("InlinedApi")
-                int errorExtra = Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1
-                        ? MediaPlayer.MEDIA_ERROR_TIMED_OUT
-                        : 0;
-                mOnErrorListener.onError(null, MediaPlayer.MEDIA_ERROR_UNKNOWN, errorExtra);
+            if (mActive && mNativeRemoteMediaPlayerBridge != 0) {
+                nativeOnError(mNativeRemoteMediaPlayerBridge);
             }
         }
 
@@ -98,7 +78,6 @@ public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
         public void onPlaybackStateChanged(PlayerState newState) {
             if (mNativeRemoteMediaPlayerBridge == 0) return;
             if (newState == PlayerState.FINISHED || newState == PlayerState.INVALIDATED) {
-                onCompleted();
                 nativeOnPlaybackFinished(mNativeRemoteMediaPlayerBridge);
             } else if (newState == PlayerState.PLAYING) {
                 nativeOnPlaying(mNativeRemoteMediaPlayerBridge);
@@ -265,28 +244,24 @@ public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
         mPosterBitmap = bitmap;
     }
 
-    @Override
     @CalledByNative
     protected boolean isPlaying() {
         if (mRouteController == null) return false;
         return mRouteController.isPlaying();
     }
 
-    @Override
     @CalledByNative
     protected int getCurrentPosition() {
         if (mRouteController == null) return 0;
         return (int) mRouteController.getPosition();
     }
 
-    @Override
     @CalledByNative
     protected int getDuration() {
         if (mRouteController == null) return 0;
         return (int) mRouteController.getDuration();
     }
 
-    @Override
     @CalledByNative
     protected void release() {
         // Remove the state change listeners. Release does mean that Chrome is no longer interested
@@ -295,26 +270,22 @@ public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
         mActive = false;
     }
 
-    @Override
     @CalledByNative
     protected void setVolume(double volume) {
     }
 
-    @Override
     @CalledByNative
     protected void start() throws IllegalStateException {
         mPauseRequested = false;
         if (mRouteController != null && mRouteController.isBeingCast()) mRouteController.resume();
     }
 
-    @Override
     @CalledByNative
     protected void pause() throws IllegalStateException {
         mPauseRequested = true;
         if (mRouteController != null && mRouteController.isBeingCast()) mRouteController.pause();
     }
 
-    @Override
     @CalledByNative
     protected void seekTo(int msec) throws IllegalStateException {
         mSeekRequested = true;
@@ -324,55 +295,12 @@ public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
         }
     }
 
-    @Override
-    protected boolean setDataSource(
-            Context context, String url, String cookies, String userAgent, boolean hideUrlLog) {
-        return true;
-    }
-
-    @Override
-    protected void setOnBufferingUpdateListener(MediaPlayer.OnBufferingUpdateListener listener) {
-    }
-
-    @Override
-    protected void setOnCompletionListener(MediaPlayer.OnCompletionListener listener) {
-        mOnCompletionListener = listener;
-    }
-
-    @Override
-    protected void setOnSeekCompleteListener(MediaPlayer.OnSeekCompleteListener listener) {
-        mOnSeekCompleteListener = listener;
-    }
-
-    @Override
-    protected void setOnErrorListener(MediaPlayer.OnErrorListener listener) {
-        mOnErrorListener = listener;
-    }
-
-    @Override
-    protected void setOnPreparedListener(MediaPlayer.OnPreparedListener listener) {
-    }
-
-    @Override
-    protected void setOnVideoSizeChangedListener(MediaPlayer.OnVideoSizeChangedListener listener) {
-    }
-
-    /**
-     * Called when the video finishes
-     */
-    public void onCompleted() {
-        if (mActive && mOnCompletionListener != null) {
-            mOnCompletionListener.onCompletion(null);
-        }
-    }
-
     private void onRouteAvailabilityChange() {
         if (mNativeRemoteMediaPlayerBridge == 0) return;
         boolean usable = mRouteIsAvailable && mIsPlayable;
         nativeOnRouteAvailabilityChanged(mNativeRemoteMediaPlayerBridge, usable);
     }
 
-    @Override
     @CalledByNative
     protected void destroy() {
         if (mDebug) Log.i(TAG, "destroy");
@@ -400,7 +328,6 @@ public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
                 });
     }
 
-    private native String nativeGetFrameUrl(long nativeRemoteMediaPlayerBridge);
     private native void nativeOnPlaying(long nativeRemoteMediaPlayerBridge);
     private native void nativeOnPaused(long nativeRemoteMediaPlayerBridge);
     private native void nativeOnRouteUnselected(long nativeRemoteMediaPlayerBridge);
@@ -413,5 +340,6 @@ public class RemoteMediaPlayerBridge extends MediaPlayerBridge {
     private native void nativeOnCastStarting(long nativeRemoteMediaPlayerBridge,
             String castingMessage);
     private native void nativeOnCastStopping(long nativeRemoteMediaPlayerBridge);
+    private native void nativeOnError(long nativeRemoteMediaPlayerBridge);
     private native void nativeOnSeekCompleted(long nativeRemoteMediaPlayerBridge);
 }
