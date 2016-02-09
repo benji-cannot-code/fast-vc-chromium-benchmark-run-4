758f6d229c6f56e2b11e25aadc01c2956407d232
jbudorick <jbudorick@chromium.org>
2016-02-09 10:10:40 -0800

    [Android] Make pylib.remote imports tolerate failure.
    BUG=580129
    
    Review URL: https://codereview.chromium.org/1673393003
    
    Cr-Commit-Position: refs/heads/master@{#374413}

--- build/android/pylib/base/environment_factory.py
+++ build/android/pylib/base/environment_factory.py
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from pylib import constants
 from pylib.local.device import local_device_environment
-from pylib.remote.device import remote_device_environment
+try:
+  from pylib.remote.device import remote_device_environment
+except ImportError:
+  remote_device_environment = None
 
 def CreateEnvironment(args, error_func):
 
@@ -13,7 +16,7 @@ def CreateEnvironment(args, error_func):
     if args.command not in constants.LOCAL_MACHINE_TESTS:
       return local_device_environment.LocalDeviceEnvironment(args, error_func)
     # TODO(jbudorick) Add local machine environment.
-  if args.environment == 'remote_device':
+  if args.environment == 'remote_device' and remote_device_environment:
     return remote_device_environment.RemoteDeviceEnvironment(args,
                                                              error_func)
   error_func('Unable to create %s environment.' % args.environment)
