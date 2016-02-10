5c1792ac336e6e88dbb1a5c6e1f09c5973fe1206
reillyg <reillyg@chromium.org>
2016-02-09 16:07:00 -0800

    Remove chromeos::PermissionBrokerClient::RequestPathAccess().
    This method has been completely replaced by OpenPath().
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1686823002
    
    Cr-Commit-Position: refs/heads/master@{#374540}

--- chromeos/dbus/permission_broker_client.cc
+++ chromeos/dbus/permission_broker_client.cc
@@ -23,7 +23,6 @@ using permission_broker::kPermissionBrokerServiceName;
 using permission_broker::kPermissionBrokerServicePath;
 using permission_broker::kReleaseTcpPort;
 using permission_broker::kReleaseUdpPort;
-using permission_broker::kRequestPathAccess;
 using permission_broker::kRequestTcpPortAccess;
 using permission_broker::kRequestUdpPortAccess;
 
@@ -39,20 +38,6 @@ class PermissionBrokerClientImpl : public PermissionBrokerClient {
     dbus::MessageWriter writer(&method_call);
     writer.AppendString(path);
     proxy_->CallMethod(&method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
-                       base::Bind(&PermissionBrokerClientImpl::OnResponse,
-                                  weak_ptr_factory_.GetWeakPtr(), callback));
-  }
-
-  void RequestPathAccess(const std::string& path,
-                         const int interface_id,
-                         const ResultCallback& callback) override {
-    dbus::MethodCall method_call(kPermissionBrokerInterface,
-                                 kRequestPathAccess);
-    dbus::MessageWriter writer(&method_call);
-    writer.AppendString(path);
-    writer.AppendInt32(interface_id);
-    proxy_->CallMethod(&method_call,
-                       dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
                        base::Bind(&PermissionBrokerClientImpl::OnResponse,
                                   weak_ptr_factory_.GetWeakPtr(), callback));
   }
