2e05ef2ff7ffe18fdb1a4a23b362e463ce04fca8
droger <droger@chromium.org>
2016-02-10 02:06:05 -0800

    Componentize login dialog strings
    The strings are componentized to be shared with the iOS build.
    
    TBR=pkasting
    
    Review URL: https://codereview.chromium.org/1682033002
    
    Cr-Commit-Position: refs/heads/master@{#374632}

--- chrome/browser/ui/login/login_prompt.cc
+++ chrome/browser/ui/login/login_prompt.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/log_manager.h"
 #include "components/password_manager/core/browser/password_manager.h"
 #include "components/prefs/pref_service.h"
+#include "components/strings/grit/components_strings.h"
 #include "components/url_formatter/elide_url.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_registrar.h"
