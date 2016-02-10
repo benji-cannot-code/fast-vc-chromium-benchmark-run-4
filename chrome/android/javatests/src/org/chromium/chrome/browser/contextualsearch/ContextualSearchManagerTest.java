562bb7e8fc64cc3461fc561e2414e8e6e4a62db4
blundell <blundell@chromium.org>
2016-02-10 03:53:34 -0800

    Revert of [Contextual Search] Re-landing the CL: https://codereview.chromium.org/1669983003/ (patchset #3 id:40001 of https://codereview.chromium.org/1685763003/ )
    Reason for revert:
    Test crashes flakily (e.g., https://build.chromium.org/p/chromium.linux/builders/Android%20Tests/builds/24765). I would attach a snippet of output from the crash, but there is no useful output that I can see.
    
    Original issue's description:
    > [Contextual Search] Re-landing the CL: https://codereview.chromium.org/1669983003/
    >
    > Fixes and re-enables
    > ContextualSearchManagerTest#testChainedSearchLoadsCorrectSearchTerm.
    >
    > Sometimes, the test tries to check the search term before
    > the URL has started loading, which ends up resulting in
    > failing to match the search term. The empty URL could also
    > cause a crash when running the test, so this CL fixes the
    > problem by waiting for the URL to match the search term,
    > and also checks if the URL is null to prevent crashes.
    >
    > BUG=551711
    >
    > Committed: https://crrev.com/9be717a273fb91fc5e2f1217fd42baabd92025fe
    > Cr-Commit-Position: refs/heads/master@{#374531}
    
    TBR=mdjones@chromium.org,pedrosimonetti@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=551711
    
    Review URL: https://codereview.chromium.org/1683083002
    
    Cr-Commit-Position: refs/heads/master@{#374642}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -26,6 +26,7 @@ import android.view.ViewConfiguration;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
@@ -201,21 +202,6 @@ public class ContextualSearchManagerTest extends ChromeActivityTestCaseBase<Chro
     }
 
     /**
-     * Waits for the requested search term to match, and asserts.
-     * @param searchTerm A given search term.
-     */
-    public void waitForSearchTermToMatch(
-            final String searchTerm) throws InterruptedException {
-        CriteriaHelper.pollForCriteria(new Criteria("Search term never matched.") {
-            @Override
-            public boolean isSatisfied() {
-                String url = mFakeServer.getLoadedUrl();
-                return !TextUtils.isEmpty(url) && url.contains("q=" + searchTerm);
-            }
-        }, TEST_TIMEOUT, DEFAULT_POLLING_INTERVAL);
-    }
-
-    /**
      * Runs the given Runnable in the main thread.
      * @param runnable The Runnable.
      */
@@ -257,7 +243,7 @@ public class ContextualSearchManagerTest extends ChromeActivityTestCaseBase<Chro
     private void simulateTapSearch(String nodeId) throws InterruptedException, TimeoutException {
         ContextualSearchFakeServer.FakeTapSearch search = mFakeServer.getFakeTapSearch(nodeId);
         search.simulate();
-        waitForSearchTermToMatch(search.getSearchTerm());
+        assertLoadedSearchTermMatches(search.getSearchTerm());
         waitForPanelToPeek();
     }
 
@@ -498,8 +484,7 @@ public class ContextualSearchManagerTest extends ChromeActivityTestCaseBase<Chro
         boolean doesMatch = false;
         String message = "but there was no loaded URL!";
         if (mFakeServer != null) {
-            String url = mFakeServer.getLoadedUrl();
-            doesMatch = !TextUtils.isEmpty(url) && url.contains("q=" + searchTerm);
+            doesMatch = mFakeServer.getLoadedUrl().contains("q=" + searchTerm);
             message = "in URL: " + mFakeServer.getLoadedUrl();
         }
         assertTrue("Expected to find searchTerm " + searchTerm + ", " + message, doesMatch);
@@ -2303,6 +2288,7 @@ public class ContextualSearchManagerTest extends ChromeActivityTestCaseBase<Chro
     /**
      * Tests that chained searches load correctly.
      */
+    @DisabledTest // https://crbug.com/551711
     @SmallTest
     @Feature({"ContextualSearch"})
     @Restriction({ChromeRestriction.RESTRICTION_TYPE_PHONE, RESTRICTION_TYPE_NON_LOW_END_DEVICE})
