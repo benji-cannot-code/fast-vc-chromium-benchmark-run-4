fc34b8ec6954b511b5e0cabea52b557f70b7d592
oysteine <oysteine@chromium.org>
2016-02-10 09:41:44 -0800

    Tracing: Removed monitoring mode
    This is not used by anyone it seems, and prevents ConvertableToTraceFormat from being used in a thread-safe manner.
    
    NOTE: Dependent on https://codereview.chromium.org/1673893002 landing in Catapult first.
    
    BUG=559117
    
    Review URL: https://codereview.chromium.org/1678503002
    
    Cr-Commit-Position: refs/heads/master@{#374678}

--- content/browser/tracing/tracing_controller_impl.h
+++ content/browser/tracing/tracing_controller_impl.h
@@ -42,16 +42,6 @@ class TracingControllerImpl
   bool StartTracing(const base::trace_event::TraceConfig& trace_config,
                     const StartTracingDoneCallback& callback) override;
   bool StopTracing(const scoped_refptr<TraceDataSink>& sink) override;
-  bool StartMonitoring(
-      const base::trace_event::TraceConfig& trace_config,
-      const StartMonitoringDoneCallback& callback) override;
-  bool StopMonitoring(
-      const StopMonitoringDoneCallback& callback) override;
-  void GetMonitoringStatus(
-      bool* out_enabled,
-      base::trace_event::TraceConfig* out_trace_config) override;
-  bool CaptureMonitoringSnapshot(
-      const scoped_refptr<TraceDataSink>& sink) override;
   bool GetTraceBufferUsage(
       const GetTraceBufferUsageCallback& callback) override;
   bool SetWatchEvent(const std::string& category_name,
@@ -125,15 +115,9 @@ class TracingControllerImpl
 
   void OnTraceDataCollected(
       const scoped_refptr<base::RefCountedString>& events_str_ptr);
-  void OnMonitoringTraceDataCollected(
-      const scoped_refptr<base::RefCountedString>& events_str_ptr);
 
   // Callback of TraceLog::Flush() for the local trace.
   void OnLocalTraceDataCollected(
-      const scoped_refptr<base::RefCountedString>& events_str_ptr,
-      bool has_more_events);
-  // Callback of TraceLog::FlushMonitoring() for the local trace.
-  void OnLocalMonitoringTraceDataCollected(
       const scoped_refptr<base::RefCountedString>& events_str_ptr,
       bool has_more_events);
 
@@ -151,9 +135,6 @@ class TracingControllerImpl
       const std::string& agent_name,
       const std::string& events_label,
       const scoped_refptr<base::RefCountedString>& events_str_ptr);
-
-  void OnCaptureMonitoringSnapshotAcked(
-      TraceMessageFilter* trace_message_filter);
 
   void OnTraceLogStatusReply(TraceMessageFilter* trace_message_filter,
                              const base::trace_event::TraceLogStatus& status);
@@ -176,12 +157,6 @@ class TracingControllerImpl
   void OnAllTracingAgentsStarted();
   void StopTracingAfterClockSync();
   void OnStopTracingDone();
-  void OnStartMonitoringDone(
-      const base::trace_event::TraceConfig& trace_config,
-      const StartMonitoringDoneCallback& callback);
-  void OnStopMonitoringDone(const StopMonitoringDoneCallback& callback);
-
-  void OnMonitoringStateChanged(bool is_monitoring);
 
   // Issue clock sync markers to the tracing agents.
   void IssueClockSyncMarker();
@@ -202,10 +177,6 @@ class TracingControllerImpl
   // Pending acks for StopTracing.
   int pending_stop_tracing_ack_count_;
   TraceMessageFilterSet pending_stop_tracing_filters_;
-
-  // Pending acks for CaptureMonitoringSnapshot.
-  int pending_capture_monitoring_snapshot_ack_count_;
-  TraceMessageFilterSet pending_capture_monitoring_filters_;
 
   // Pending acks for GetTraceLogStatus.
   int pending_trace_log_status_ack_count_;
