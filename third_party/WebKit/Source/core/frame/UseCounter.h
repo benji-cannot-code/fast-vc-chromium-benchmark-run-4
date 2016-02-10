c55cbf7f570a920c3ee2440c6ef0e06b8f34f76d
imcheng <imcheng@chromium.org>
2016-02-09 17:39:53 -0800

    [Presentation API] Add connect,close,terminate events.
    Add connect,close,terminate events to replace statechange event.
    
    Add custom PresentationConnectionCloseEvent for close event.
    
    TODOs in later patches:
    - plumb close event from content/ and MR.
    - deprecate statechange.
    
    BUG=574233,574234,579360
    
    Review URL: https://codereview.chromium.org/1651253002
    
    Cr-Commit-Position: refs/heads/master@{#374567}

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -1009,6 +1009,9 @@ public:
         NodeTreeRoot = 1154,
         BluetoothDeviceConnectGATT = 1155,
         CreateImageBitmap = 1156,
+        PresentationConnectionConnectEventListener = 1157,
+        PresentationConnectionCloseEventListener = 1158,
+        PresentationConnectionTerminateEventListener = 1159,
 
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
