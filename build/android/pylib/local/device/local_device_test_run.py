97ad5794cc0a64f9c8c9fabe21577b2e184bc9e7
agrieve <agrieve@chromium.org>
2016-02-10 11:25:26 -0800

    Android Add _incremental targets for instrumentation tests
    Switches gtests to explicitly passing path to install script rather than
    --incremental-install + heuristics.
    
    BUG=575975
    
    Review URL: https://codereview.chromium.org/1680233002
    
    Cr-Commit-Position: refs/heads/master@{#374699}

--- build/android/pylib/local/device/local_device_test_run.py
+++ build/android/pylib/local/device/local_device_test_run.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import fnmatch
 import functools
+import imp
 import logging
 
 from devil import base_error
@@ -13,6 +14,27 @@ from pylib import valgrind_tools
 from pylib.base import base_test_result
 from pylib.base import test_run
 from pylib.base import test_collection
+
+
+def IncrementalInstall(device, apk_helper, installer_script):
+  """Performs an incremental install.
+
+  Args:
+    device: Device to install on.
+    apk_helper: ApkHelper instance for the _incremental.apk.
+    installer_script: Path to the installer script for the incremental apk.
+  """
+  try:
+    install_wrapper = imp.load_source('install_wrapper', installer_script)
+  except IOError:
+    raise Exception('Incremental install script not found: %s\n' %
+                    installer_script)
+  params = install_wrapper.GetInstallParameters()
+
+  from incremental_install import installer
+  installer.Install(device, apk_helper, split_globs=params['splits'],
+                    native_libs=params['native_libs'],
+                    dex_files=params['dex_files'])
 
 
 def handle_shard_failures(f):
