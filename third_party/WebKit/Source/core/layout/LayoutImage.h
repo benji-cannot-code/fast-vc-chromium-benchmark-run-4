7eb42274071cc380aca665c8b1e37bd47e2a2a6f
davve <davve@opera.com>
2016-02-10 02:36:18 -0800

    Introduce IntrinsicSizingInfo
    Prepare for passing more information along, specifically whether
    intrinsic sizes are specified, when computing intrinsic sizing
    information. Do this by introducing a struct carrying the relevant
    information. No functional change expected.
    
    Factor out a part of computeAspectRatioInformationForLayoutBox (now
    named computeIntrinsicSizingInfoForLayoutBox) and put in a separate
    function constrainIntrinsicSizeToMinMax. In the process remove a FIXME
    about moving code back to some undefined previous state that's
    confusing now that the code has moved around even more.
    
    BUG=585467
    
    Review URL: https://codereview.chromium.org/1687503002
    
    Cr-Commit-Position: refs/heads/master@{#374635}

--- third_party/WebKit/Source/core/layout/LayoutImage.h
+++ third_party/WebKit/Source/core/layout/LayoutImage.h
@@ -82,7 +82,7 @@ public:
 protected:
     bool needsPreferredWidthsRecalculation() const final;
     LayoutBox* embeddedContentBox() const final;
-    void computeIntrinsicRatioInformation(FloatSize& intrinsicSize, double& intrinsicRatio) const final;
+    void computeIntrinsicSizingInfo(IntrinsicSizingInfo&) const final;
 
     void imageChanged(WrappedImagePtr, const IntRect* = nullptr) override;
 
