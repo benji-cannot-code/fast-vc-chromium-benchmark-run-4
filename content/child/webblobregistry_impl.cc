037d6f4f61a1bf2134bc5cf3fbf18fd42d4beed7
jam <jam@chromium.org>
2016-02-10 10:14:56 -0800

    Add a checked_cast for size_t to uint32_t conversion.
    This should never be hit since we don't allocate more than 2 GB, but it's best practice to ensure so.
    
    BUG=581409
    
    Review URL: https://codereview.chromium.org/1683033002
    
    Cr-Commit-Position: refs/heads/master@{#374688}

--- content/child/webblobregistry_impl.cc
+++ content/child/webblobregistry_impl.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/shared_memory.h"
 #include "base/message_loop/message_loop.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/trace_event/trace_event.h"
 #include "content/child/child_thread_impl.h"
 #include "content/child/thread_safe_sender.h"
@@ -155,7 +156,8 @@ void WebBlobRegistryImpl::addDataToStream(const WebURL& url,
       size_t chunk_size = std::min(remaining_bytes, shared_memory_size);
       memcpy(shared_memory->memory(), current_ptr, chunk_size);
       sender_->Send(new StreamHostMsg_SyncAppendSharedMemory(
-          url, shared_memory->handle(), chunk_size));
+          url, shared_memory->handle(),
+          base::checked_cast<uint32_t>(chunk_size)));
       remaining_bytes -= chunk_size;
       current_ptr += chunk_size;
     }
