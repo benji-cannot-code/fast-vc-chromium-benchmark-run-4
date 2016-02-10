ff9375a029e0123b4c2acfa18fc291f1a7ded7b8
reillyg <reillyg@chromium.org>
2016-02-09 17:53:06 -0800

    Add path open errors from the permission broker to the device log.
    This change adds an additional ErrorCallback parameter to the permission
    broker client so that the caller can get a better log message when its
    request to open a device node fails.
    
    This will hopefully help to determine the cause of the many "Did not get
    valid device handle from permission broker" errors that some users are
    seeing.
    
    BUG=570784
    
    Review URL: https://codereview.chromium.org/1681383002
    
    Cr-Commit-Position: refs/heads/master@{#374571}

--- chromeos/dbus/fake_permission_broker_client.cc
+++ chromeos/dbus/fake_permission_broker_client.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/posix/eintr_wrapper.h"
+#include "base/strings/stringprintf.h"
 #include "base/thread_task_runner_handle.h"
 #include "base/threading/worker_pool.h"
 #include "dbus/file_descriptor.h"
@@ -21,6 +22,8 @@ namespace chromeos {
 
 namespace {
 
+const char kOpenFailedError[] = "open_failed";
+
 // So that real devices can be accessed by tests and "Chromium OS on Linux" this
 // function implements a simplified version of the method implemented by the
 // permission broker by opening the path specified and returning the resulting
@@ -28,15 +31,23 @@ namespace {
 void OpenPathAndValidate(
     const std::string& path,
     const PermissionBrokerClient::OpenPathCallback& callback,
+    const PermissionBrokerClient::ErrorCallback& error_callback,
     scoped_refptr<base::TaskRunner> task_runner) {
-  dbus::FileDescriptor dbus_fd;
   int fd = HANDLE_EINTR(open(path.c_str(), O_RDWR));
   if (fd < 0) {
-    PLOG(WARNING) << "Failed to open '" << path << "'";
-  } else {
-    dbus_fd.PutValue(fd);
-    dbus_fd.CheckValidity();
+    int error_code = logging::GetLastSystemErrorCode();
+    task_runner->PostTask(
+        FROM_HERE,
+        base::Bind(error_callback, kOpenFailedError,
+                   base::StringPrintf(
+                       "Failed to open '%s': %s", path.c_str(),
+                       logging::SystemErrorCodeToString(error_code).c_str())));
+    return;
   }
+
+  dbus::FileDescriptor dbus_fd;
+  dbus_fd.PutValue(fd);
+  dbus_fd.CheckValidity();
   task_runner->PostTask(FROM_HERE,
                         base::Bind(callback, base::Passed(&dbus_fd)));
 }
@@ -56,11 +67,13 @@ void FakePermissionBrokerClient::CheckPathAccess(
 }
 
 void FakePermissionBrokerClient::OpenPath(const std::string& path,
-                                          const OpenPathCallback& callback) {
-  base::WorkerPool::PostTask(FROM_HERE,
-                             base::Bind(&OpenPathAndValidate, path, callback,
-                                        base::ThreadTaskRunnerHandle::Get()),
-                             false);
+                                          const OpenPathCallback& callback,
+                                          const ErrorCallback& error_callback) {
+  base::WorkerPool::PostTask(
+      FROM_HERE,
+      base::Bind(&OpenPathAndValidate, path, callback, error_callback,
+                 base::ThreadTaskRunnerHandle::Get()),
+      false);
 }
 
 void FakePermissionBrokerClient::RequestTcpPortAccess(
