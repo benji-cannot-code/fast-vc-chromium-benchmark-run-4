080189cda01a2dedac6d85a71ddb22ecac27a63f
pedrosimonetti <pedrosimonetti@chromium.org>
2016-02-09 16:51:43 -0800

    [Contextual Search] Fixes promo size upon rotation.
    The Opt Out Promo is not being properly resized after
    rotating the device with the promo visible. This issue
    was introduced by the CL:
    https://codereview.chromium.org/1536623003/
    
    This CL fixes the problem by re-creating the Promo View
    when the device orientation changes.
    
    BUG=571324
    
    Review URL: https://codereview.chromium.org/1683933002
    
    Cr-Commit-Position: refs/heads/master@{#374557}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/OverlayPanelBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/OverlayPanelBase.java
@@ -1222,6 +1222,8 @@ abstract class OverlayPanelBase implements ContextualSearchPromoHost {
      */
     private void updatePromoVisibility(float percentage) {
         if (isPromoVisible()) {
+            createPromoView();
+
             mPromoVisible = true;
 
             mPromoHeightPx = Math.round(MathUtils.clamp(percentage * mPromoContentHeightPx,
