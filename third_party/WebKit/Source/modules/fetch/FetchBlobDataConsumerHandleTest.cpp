34341b7385050d8aa3afa08cb1218a98c487e0bd
tyoshino <tyoshino@chromium.org>
2016-02-10 00:06:55 -0800

    Factor out MockThreadableLoader
    Split from https://codereview.chromium.org/1264453002
    
    BUG=none
    R=hiroshige,japhet
    
    Review URL: https://codereview.chromium.org/1656673003
    
    Cr-Commit-Position: refs/heads/master@{#374626}

--- third_party/WebKit/Source/modules/fetch/FetchBlobDataConsumerHandleTest.cpp
+++ third_party/WebKit/Source/modules/fetch/FetchBlobDataConsumerHandleTest.cpp
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/dom/ExecutionContext.h"
 #include "core/fetch/ResourceLoaderOptions.h"
-#include "core/loader/ThreadableLoader.h"
+#include "core/loader/MockThreadableLoader.h"
 #include "core/loader/ThreadableLoaderClient.h"
 #include "core/testing/DummyPageHolder.h"
 #include "modules/fetch/DataConsumerHandleTestUtil.h"
@@ -53,17 +53,6 @@ using Checkpoint = StrictMock<::testing::MockFunction<void(int)>>;
 class MockLoaderFactory : public FetchBlobDataConsumerHandle::LoaderFactory {
 public:
     MOCK_METHOD5(create, PassRefPtr<ThreadableLoader>(ExecutionContext&, ThreadableLoaderClient*, const ResourceRequest&, const ThreadableLoaderOptions&, const ResourceLoaderOptions&));
-};
-
-class MockThreadableLoader : public ThreadableLoader {
-public:
-    static PassRefPtr<MockThreadableLoader> create() { return adoptRef(new StrictMock<MockThreadableLoader>); }
-
-    MOCK_METHOD1(overrideTimeout, void(unsigned long));
-    MOCK_METHOD0(cancel, void());
-
-protected:
-    MockThreadableLoader() = default;
 };
 
 PassRefPtr<BlobDataHandle> createBlobDataHandle(const char* s)
