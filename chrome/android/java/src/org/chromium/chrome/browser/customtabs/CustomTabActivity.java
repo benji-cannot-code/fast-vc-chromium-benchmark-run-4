0247ad9c09bb8bba58766fb959c9279b81d3e5d5
dfalcantara <dfalcantara@chromium.org>
2016-02-10 16:11:20 -0800

    Herb: Show an icon allowing the user to open a tab in Chrome
    * Adds an icon to CCTs opened by Chrome that allows the user
      to open the URL in the full Chrome browser.
    
    * Allows CustomTabActivity to be finished() when it fires a
      PendingIntent that was created by Chrome itself.
    
    * Adds an icon for the "open in chrome" action.
    
    BUG=582539
    
    Review URL: https://codereview.chromium.org/1683533002
    
    Cr-Commit-Position: refs/heads/master@{#374797}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -40,6 +40,7 @@ import org.chromium.chrome.browser.appmenu.AppMenuPropertiesDelegate;
 import org.chromium.chrome.browser.compositor.bottombar.OverlayPanel.StateChangeReason;
 import org.chromium.chrome.browser.compositor.layouts.LayoutManagerDocument;
 import org.chromium.chrome.browser.datausage.DataUseTabUIManager;
+import org.chromium.chrome.browser.document.ChromeLauncherActivity;
 import org.chromium.chrome.browser.rappor.RapporServiceBridge;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabIdManager;
@@ -67,6 +68,7 @@ import java.util.List;
  */
 public class CustomTabActivity extends ChromeActivity {
     private static final String TAG = "CustomTabActivity";
+
     private static CustomTabContentHandler sActiveContentHandler;
 
     private FindToolbarManager mFindToolbarManager;
@@ -173,7 +175,6 @@ public class CustomTabActivity extends ChromeActivity {
     @Override
     public void preInflationStartup() {
         super.preInflationStartup();
-
         mIntentDataProvider = new CustomTabIntentDataProvider(getIntent(), this);
         supportRequestWindowFeature(Window.FEATURE_ACTION_MODE_OVERLAY);
     }
@@ -445,7 +446,7 @@ public class CustomTabActivity extends ChromeActivity {
      * Configures the custom button on toolbar. Does nothing if invalid data is provided by clients.
      */
     private void showCustomButtonOnToolbar() {
-        CustomButtonParams params = mIntentDataProvider.getCustomButtonOnToolbar();
+        final CustomButtonParams params = mIntentDataProvider.getCustomButtonOnToolbar();
         if (params == null) return;
         getToolbarManager().setCustomActionButton(
                 params.getIcon(getResources()),
@@ -453,9 +454,17 @@ public class CustomTabActivity extends ChromeActivity {
                 new OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        mIntentDataProvider.sendButtonPendingIntentWithUrl(
-                                getApplicationContext(), getActivityTab().getUrl());
-                        RecordUserAction.record("CustomTabsCustomActionButtonClick");
+                        String creatorPackage =
+                                ApiCompatibilityUtils.getCreatorPackage(params.getPendingIntent());
+                        if (mIntentDataProvider.finishAfterOpeningInBrowser()
+                                && TextUtils.equals(getPackageName(), creatorPackage)) {
+                            openCurrentUrlInBrowser();
+                            finish();
+                        } else {
+                            mIntentDataProvider.sendButtonPendingIntentWithUrl(
+                                    getApplicationContext(), getActivityTab().getUrl());
+                            RecordUserAction.record("CustomTabsCustomActionButtonClick");
+                        }
                     }
                 });
     }
@@ -562,22 +571,7 @@ public class CustomTabActivity extends ChromeActivity {
                 || id == R.id.new_tab_menu_id || id == R.id.open_history_menu_id) {
             return true;
         } else if (id == R.id.open_in_browser_id) {
-            String url = getTabModelSelector().getCurrentTab().getUrl();
-            if (DomDistillerUrlUtils.isDistilledPage(url)) {
-                url = DomDistillerUrlUtils.getOriginalUrlFromDistillerUrl(url);
-            }
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-            // Temporarily allowing disk access while fixing. TODO: http://crbug.com/581860
-            StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskReads();
-            StrictMode.allowThreadDiskWrites();
-            try {
-                startActivity(intent);
-            } finally {
-                StrictMode.setThreadPolicy(oldPolicy);
-            }
-
+            openCurrentUrlInBrowser();
             RecordUserAction.record("CustomTabsMenuOpenInChrome");
             return true;
         } else if (id == R.id.find_in_page_id) {
@@ -621,5 +615,27 @@ public class CustomTabActivity extends ChromeActivity {
     @VisibleForTesting
     CustomTabIntentDataProvider getIntentDataProvider() {
         return mIntentDataProvider;
+    }
+
+    /**
+     * Opens the URL currently being displayed in the Custom Tab in the regular browser.
+     */
+    void openCurrentUrlInBrowser() {
+        String url = getTabModelSelector().getCurrentTab().getUrl();
+        if (DomDistillerUrlUtils.isDistilledPage(url)) {
+            url = DomDistillerUrlUtils.getOriginalUrlFromDistillerUrl(url);
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(ChromeLauncherActivity.EXTRA_IS_ALLOWED_TO_RETURN_TO_PARENT, false);
+
+        // Temporarily allowing disk access while fixing. TODO: http://crbug.com/581860
+        StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskReads();
+        StrictMode.allowThreadDiskWrites();
+        try {
+            startActivity(intent);
+        } finally {
+            StrictMode.setThreadPolicy(oldPolicy);
+        }
     }
 }
