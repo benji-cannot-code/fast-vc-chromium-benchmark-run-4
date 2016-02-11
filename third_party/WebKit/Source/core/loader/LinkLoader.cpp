db23011ea2e4835b5977fdb7fb652c39f2167d1e
yoav <yoav@yoav.ws>
2016-02-11 03:52:00 -0800

    Link Header relative URLs are now based on the resource's URL
    Up until now, Link header based URLs were relative to the document. That's not
    spec compliant, can be confusing, and can result in extra downloaded bytes in
    common scenarios.
    This CL fixes that.
    
    BUG=584048
    
    Review URL: https://codereview.chromium.org/1672653002
    
    Cr-Commit-Position: refs/heads/master@{#374884}

--- third_party/WebKit/Source/core/loader/LinkLoader.cpp
+++ third_party/WebKit/Source/core/loader/LinkLoader.cpp
@@ -245,7 +245,7 @@ static Resource* preloadIfNeeded(const LinkRelAttribute& relAttribute, const KUR
     return document.loader()->startPreload(type, linkRequest);
 }
 
-bool LinkLoader::loadLinkFromHeader(const String& headerValue, Document* document, const NetworkHintsInterface& networkHintsInterface, CanLoadResources canLoadResources)
+bool LinkLoader::loadLinkFromHeader(const String& headerValue, const KURL& baseURL, Document* document, const NetworkHintsInterface& networkHintsInterface, CanLoadResources canLoadResources)
 {
     if (!document)
         return false;
@@ -255,7 +255,7 @@ bool LinkLoader::loadLinkFromHeader(const String& headerValue, Document* documen
             return false;
 
         LinkRelAttribute relAttribute(header.rel());
-        KURL url = document->completeURL(header.url());
+        KURL url(baseURL, header.url());
         if (canLoadResources != OnlyLoadResources) {
             if (RuntimeEnabledFeatures::linkHeaderEnabled())
                 dnsPrefetchIfNeeded(relAttribute, url, *document, networkHintsInterface, LinkCalledFromHeader);
