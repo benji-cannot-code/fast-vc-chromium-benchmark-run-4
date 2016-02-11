e4fedee91f231be26944effd8c0662b9ab26eb48
ortuno <ortuno@chromium.org>
2016-02-11 12:06:49 -0800

    bluetooth: Enable Web Bluetooth through experimental framework
    This enables Web Bluetooth through experimental frameworks in supported platforms.
    Intent to Experiment: https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/coyvHj1u2Z8
    
    Adds the APIExperimentEnabled extended binding so that the bindings are visible
    to domains specified by the Experimental Framework.
    
    Also adds WebBluetooth to frames and BluetoothDispatcherHost to processes' filters for supported platforms.
    
    TEST=Manual testing required:
    Launch chrome with the following command line flag:
    --enable-blink-features=ExperimentalFramework and perform the following tests:
    
    On a Chrome OS device and an Android device:
    1. Visit: https://g-ortuno.github.io/web-bluetooth-sandbox/allowed.html
    2. Press the "Run Test" button.
    3. "Web Bluetooth bindings" should be "Present" and "Use Web Bluetooth" should
       be "Bluetooth Works".
    
    1. Visit: https://g-ortuno.github.io/web-bluetooth-sandbox/not-allowed.html
    2. Press the "Run Test" button.
    3. "Web Bluetooth bindings" should be "Present" and "Use Web Bluetooth" should
       be "Bluetooth Unavailable".
    
    On a Linux, OSX and Windows devices:
    1. Visit: https://g-ortuno.github.io/web-bluetooth-sandbox/allowed.html
    2. Press the "Run Test" button.
    3. "Web Bluetooth bindings" should be "Present" and "Use Web Bluetooth" should
       be "Bluetooth blocked".
    
    1. Visit: https://g-ortuno.github.io/web-bluetooth-sandbox/not-allowed.html
    2. Press the "Run Test" button.
    3. "Web Bluetooth bindings" should be "Present" and "Use Web Bluetooth" should
       be "Bluetooth Unavailable".
    
    Launch Chrome with the following command line flags:
    --enable-blink-features=ExperimentalFramework --enable-web-bluetooth
    
    On a Chrome OS, Android, Linux, OSX, and Windows devices:
    1. Visit: https://g-ortuno.github.io/web-bluetooth-sandbox/allowed.html
    2. Press the "Run Test" button.
    3. "Web Bluetooth bindings" should be "Present" and "Use Web Bluetooth" should
       be "Bluetooth Works".
    
    On a Chrome OS, Android, Linux, OSX, and Windows devices:
    1. Visit: https://g-ortuno.github.io/web-bluetooth-sandbox/not-allowed.html
    2. Press the "Run Test" button.
    3. "Web Bluetooth bindings" should be "Present" and "Use Web Bluetooth" should
       be "Bluetooth Works".
    
    BUG=584113
    
    Review URL: https://codereview.chromium.org/1662183002
    
    Cr-Commit-Position: refs/heads/master@{#374954}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1058,7 +1058,14 @@ void RenderProcessHostImpl::CreateMessageFilters() {
 #endif
   AddFilter(new GeofencingDispatcherHost(
       storage_partition_impl_->GetGeofencingManager()));
-  if (browser_command_line.HasSwitch(switches::kEnableWebBluetooth)) {
+
+  bool enable_web_bluetooth =
+      browser_command_line.HasSwitch(switches::kEnableWebBluetooth);
+#if defined(OS_CHROMEOS) || defined(OS_ANDROID)
+  enable_web_bluetooth = true;
+#endif
+
+  if (enable_web_bluetooth) {
     bluetooth_dispatcher_host_ = new BluetoothDispatcherHost(GetID());
     AddFilter(bluetooth_dispatcher_host_.get());
   }
--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -4364,7 +4364,8 @@ void RenderFrameImpl::unregisterProtocolHandler(const WebString& scheme,
 }
 
 blink::WebBluetooth* RenderFrameImpl::bluetooth() {
-  if (!bluetooth_) {
+  // ChildThreadImpl::current() is null in some tests.
+  if (!bluetooth_ && ChildThreadImpl::current()) {
     bluetooth_.reset(new WebBluetoothImpl(
         ChildThreadImpl::current()->thread_safe_sender(), routing_id_));
   }
