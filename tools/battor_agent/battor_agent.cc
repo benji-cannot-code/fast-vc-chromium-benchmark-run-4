540cda4bc39efdfaecd07c45760e50c68384a91c
charliea <charliea@chromium.org>
2016-02-11 11:25:57 -0800

    tools/battor_agent: Implements RecordClockSyncMarker()
    This constitutes most of the Chrome-side work to sync timelines between
    Chrome traces BattOr traces.
    
    Trace viewer side work is also being done to be able to import Chrome
    and BattOr traces with these added sync markers.
    
    This changes the BattOr trace syntax by adding <sync_id> to the end of
    the sample line when a clock sync is present for that data point.
    
    I've verified that this works as expected on Mac, Linux, and Windows.
    
    BUG=542837
    
    Review URL: https://codereview.chromium.org/1647023002
    
    Cr-Commit-Position: refs/heads/master@{#374916}

--- tools/battor_agent/battor_agent.cc
+++ tools/battor_agent/battor_agent.cc
@@ -27,6 +27,10 @@ const uint8_t kMaxReadAttempts = 20;
 // The number of milliseconds to wait before trying to read a message again.
 const uint8_t kReadRetryDelayMilliseconds = 1;
 
+// The amount of time we need to wait after recording a clock sync marker in
+// order to ensure that the sample we synced to doesn't get thrown out.
+const uint8_t kStopTracingClockSyncDelayMilliseconds = 50;
+
 // Returns true if the specified vector of bytes decodes to a message that is an
 // ack for the specified control message type.
 bool IsAckOfControlCommand(BattOrMessageType message_type,
@@ -126,6 +130,10 @@ BattOrAgent::~BattOrAgent() {
 void BattOrAgent::StartTracing() {
   DCHECK(thread_checker_.CalledOnValidThread());
 
+  // When tracing is restarted, all previous clock sync markers are invalid.
+  clock_sync_markers_.clear();
+  last_clock_sync_time_ = base::TimeTicks();
+
   command_ = Command::START_TRACING;
   PerformAction(Action::REQUEST_CONNECTION);
 }
@@ -134,6 +142,14 @@ void BattOrAgent::StopTracing() {
   DCHECK(thread_checker_.CalledOnValidThread());
 
   command_ = Command::STOP_TRACING;
+  PerformAction(Action::REQUEST_CONNECTION);
+}
+
+void BattOrAgent::RecordClockSyncMarker(const std::string& marker) {
+  DCHECK(thread_checker_.CalledOnValidThread());
+
+  command_ = Command::RECORD_CLOCK_SYNC_MARKER;
+  pending_clock_sync_marker_ = marker;
   PerformAction(Action::REQUEST_CONNECTION);
 }
 
@@ -159,6 +175,9 @@ void BattOrAgent::OnConnectionOpened(bool success) {
       return;
     case Command::STOP_TRACING:
       PerformAction(Action::SEND_EEPROM_REQUEST);
+      return;
+    case Command::RECORD_CLOCK_SYNC_MARKER:
+      PerformAction(Action::SEND_CURRENT_SAMPLE_REQUEST);
       return;
     case Command::INVALID:
       NOTREACHED();
@@ -201,7 +220,10 @@ void BattOrAgent::OnBytesSent(bool success) {
       num_read_attempts_ = 1;
       PerformAction(Action::READ_CALIBRATION_FRAME);
       return;
-
+    case Action::SEND_CURRENT_SAMPLE_REQUEST:
+      num_read_attempts_ = 1;
+      PerformAction(Action::READ_CURRENT_SAMPLE);
+      return;
     default:
       CompleteCommand(BATTOR_ERROR_UNEXPECTED_MESSAGE);
   }
@@ -215,6 +237,7 @@ void BattOrAgent::OnMessageRead(bool success,
       case Action::READ_EEPROM:
       case Action::READ_CALIBRATION_FRAME:
       case Action::READ_DATA_FRAME:
+      case Action::READ_CURRENT_SAMPLE:
         if (++num_read_attempts_ > kMaxReadAttempts) {
           CompleteCommand(BATTOR_ERROR_RECEIVE_ERROR);
           return;
@@ -261,16 +284,25 @@ void BattOrAgent::OnMessageRead(bool success,
       CompleteCommand(BATTOR_ERROR_NONE);
       return;
 
-    case Action::READ_EEPROM:
+    case Action::READ_EEPROM: {
       battor_eeprom_ = ParseEEPROM(type, *bytes);
       if (!battor_eeprom_) {
         CompleteCommand(BATTOR_ERROR_UNEXPECTED_MESSAGE);
         return;
       }
 
-      PerformAction(Action::SEND_SAMPLES_REQUEST);
-      return;
+      // Make sure that we don't request samples until a safe amount of time has
+      // elapsed since recording the last clock sync marker: we need to ensure
+      // that the sample we synced to doesn't get thrown out.
+      base::TimeTicks min_request_samples_time =
+          last_clock_sync_time_ + base::TimeDelta::FromMilliseconds(
+                                      kStopTracingClockSyncDelayMilliseconds);
+      base::TimeDelta request_samples_delay = std::max(
+          min_request_samples_time - base::TimeTicks::Now(), base::TimeDelta());
 
+      PerformDelayedAction(Action::SEND_SAMPLES_REQUEST, request_samples_delay);
+      return;
+    }
     case Action::READ_CALIBRATION_FRAME: {
       BattOrFrameHeader frame_header;
       if (!ParseSampleFrame(type, *bytes, next_sequence_number_++,
@@ -312,6 +344,20 @@ void BattOrAgent::OnMessageRead(bool success,
       PerformAction(Action::READ_DATA_FRAME);
       return;
     }
+
+    case Action::READ_CURRENT_SAMPLE:
+      if (type != BATTOR_MESSAGE_TYPE_CONTROL_ACK ||
+          bytes->size() != sizeof(uint32_t)) {
+        CompleteCommand(BATTOR_ERROR_UNEXPECTED_MESSAGE);
+        return;
+      }
+
+      uint32_t sample_num;
+      memcpy(&sample_num, bytes->data(), sizeof(uint32_t));
+      clock_sync_markers_[sample_num] = pending_clock_sync_marker_;
+      last_clock_sync_time_ = base::TimeTicks::Now();
+      CompleteCommand(BATTOR_ERROR_NONE);
+      return;
 
     default:
       CompleteCommand(BATTOR_ERROR_UNEXPECTED_MESSAGE);
@@ -392,6 +438,14 @@ void BattOrAgent::PerformAction(Action action) {
       connection_->ReadMessage(BATTOR_MESSAGE_TYPE_SAMPLES);
       return;
 
+    // The following actions are required for RecordClockSyncMarker:
+    case Action::SEND_CURRENT_SAMPLE_REQUEST:
+      SendControlMessage(BATTOR_CONTROL_MESSAGE_TYPE_READ_SAMPLE_COUNT, 0, 0);
+      return;
+    case Action::READ_CURRENT_SAMPLE:
+      connection_->ReadMessage(BATTOR_MESSAGE_TYPE_CONTROL_ACK);
+      return;
+
     case Action::INVALID:
       NOTREACHED();
   }
@@ -419,19 +473,24 @@ void BattOrAgent::CompleteCommand(BattOrError error) {
           FROM_HERE, base::Bind(&Listener::OnStartTracingComplete,
                                 base::Unretained(listener_), error));
       break;
-    case Command::STOP_TRACING: {
+    case Command::STOP_TRACING:
       base::ThreadTaskRunnerHandle::Get()->PostTask(
           FROM_HERE,
           base::Bind(&Listener::OnStopTracingComplete,
                      base::Unretained(listener_), SamplesToString(), error));
       break;
-    }
+    case Command::RECORD_CLOCK_SYNC_MARKER:
+      base::ThreadTaskRunnerHandle::Get()->PostTask(
+          FROM_HERE, base::Bind(&Listener::OnRecordClockSyncMarkerComplete,
+                                base::Unretained(listener_), error));
+      break;
     case Command::INVALID:
       NOTREACHED();
   }
 
   last_action_ = Action::INVALID;
   command_ = Command::INVALID;
+  pending_clock_sync_marker_.clear();
   battor_eeprom_.reset();
   calibration_frame_.clear();
   samples_.clear();
@@ -451,9 +510,9 @@ std::string BattOrAgent::SamplesToString() {
   BattOrSample min_sample = converter.MinSample();
   BattOrSample max_sample = converter.MaxSample();
   trace_stream << "# BattOr" << std::endl
-               << std::setprecision(1)
-               << "# voltage_range [" <<  min_sample.voltage_mV << ", "
-               << max_sample.voltage_mV << "] mV" << std::endl
+               << std::setprecision(1) << "# voltage_range ["
+               << min_sample.voltage_mV << ", " << max_sample.voltage_mV
+               << "] mV" << std::endl
                << "# current_range [" << min_sample.current_mA << ", "
                << max_sample.current_mA << "] mA" << std::endl
                << "# sample_rate " << battor_eeprom_->sd_sample_rate << " Hz"
@@ -464,7 +523,15 @@ std::string BattOrAgent::SamplesToString() {
     BattOrSample sample = converter.ToSample(samples_[i], i);
     trace_stream << std::setprecision(2) << sample.time_ms << " "
                  << std::setprecision(1) << sample.current_mA << " "
-                 << sample.voltage_mV << std::endl;
+                 << sample.voltage_mV;
+
+    // If there's a clock sync marker for the current sample, print it.
+    auto clock_sync_marker = clock_sync_markers_.find(
+        static_cast<uint32_t>(calibration_frame_.size() + i));
+    if (clock_sync_marker != clock_sync_markers_.end())
+      trace_stream << " <" << clock_sync_marker->second << ">";
+
+    trace_stream << std::endl;
   }
 
   return trace_stream.str();
