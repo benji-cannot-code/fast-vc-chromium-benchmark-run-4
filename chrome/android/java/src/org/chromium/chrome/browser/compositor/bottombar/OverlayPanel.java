c9af7e3e4c68c802022758f31e9e038930e756bd
mdjones <mdjones@chromium.org>
2016-02-10 17:43:13 -0800

    Disable fullscreen video in OverlayPanels
    This change hides the fullscreen button for videos in Reader Mode
    and uses an empty implementation of ContentVideoViewEmbedder in case
    it is somehow triggered. This is a temporary measure until a proper
    fullscreen handler can be implemented.
    
    BUG=581943
    
    Review URL: https://codereview.chromium.org/1674753002
    
    Cr-Commit-Position: refs/heads/master@{#374830}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/OverlayPanel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/OverlayPanel.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.compositor.bottombar;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.View;
 import android.view.View.MeasureSpec;
 
 import org.chromium.base.ActivityState;
@@ -18,6 +19,7 @@ import org.chromium.chrome.browser.compositor.bottombar.OverlayPanelManager.Pane
 import org.chromium.chrome.browser.compositor.layouts.LayoutUpdateHost;
 import org.chromium.chrome.browser.compositor.scene_layer.SceneLayer;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.content.browser.ContentVideoViewEmbedder;
 import org.chromium.content.browser.ContentViewClient;
 import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content_public.common.TopControlsState;
@@ -334,6 +336,27 @@ public class OverlayPanel extends OverlayPanelAnimation implements ActivityState
                             getContentViewHeightPx(),
                             MeasureSpec.EXACTLY);
                 }
+            }
+
+            @Override
+            public ContentVideoViewEmbedder getContentVideoViewEmbedder() {
+                // TODO(mdjones): Possibly enable fullscreen video in overlay panels rather than
+                // passing an empty implementation.
+                return new ContentVideoViewEmbedder() {
+                    @Override
+                    public void enterFullscreenVideo(View view) {}
+
+                    @Override
+                    public void exitFullscreenVideo() {}
+
+                    @Override
+                    public View getVideoLoadingProgressView() {
+                        return null;
+                    }
+
+                    @Override
+                    public void setSystemUiVisibility(boolean enterFullscreen) {}
+                };
             }
         });
 
