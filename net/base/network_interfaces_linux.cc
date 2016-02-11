e9240af8ef8c67f7d9bc588c7323f1977282a90c
mdjones <mdjones@chromium.org>
2016-02-10 18:06:28 -0800

    Revert of Native SSID extraction moved to platform code on Android (patchset #8 id:140001 of https://codereview.chromium.org/1633733005/ )
    Reason for revert:
    Causes instant crash on Android KK, L, and M.
    
    Original issue's description:
    > On Android, move Wifi SSID extraction to a Java implementation
    >
    > On some Android devices, the device will restart right
    > after the browser is launched. This is due to a kernel bug on
    > these devices when using the Linux GetWifiSSID implementation,
    > which causes a kernel panic and device restart.
    >
    > To avoid this, a Java-based implementation is used for Android
    > devices, which avoids this. Examples of affected devices include:
    > HTC One S
    > Lenovo A820
    > Asus ZenFone 2
    > MediaPad 10 FHD
    >
    > R=bengr@chromium.org,rch@chromium.org
    > BUG=555067
    > TEST=Take the problem device, install browser and start it, wait for
    > 10 seconds. Make 10 experiments, cleaning data before each run.
    > Device should not reboot.
    
    TBR=bengr@chromium.org,rch@chromium.org,rsleevi@chromium.org,ripp@yandex-team.ru
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=555067
    
    Review URL: https://codereview.chromium.org/1688103002
    
    Cr-Commit-Position: refs/heads/master@{#374833}

--- net/base/network_interfaces_linux.cc
+++ net/base/network_interfaces_linux.cc
@@ -216,7 +216,6 @@ bool GetNetworkList(NetworkInterfaceList* networks, int policy) {
       &internal::AddressTrackerLinux::GetInterfaceName);
 }
 
-#if !defined(OS_ANDROID)
 std::string GetWifiSSID() {
   NetworkInterfaceList networks;
   if (GetNetworkList(&networks, INCLUDE_HOST_SCOPE_VIRTUAL_INTERFACES)) {
@@ -225,6 +224,5 @@ std::string GetWifiSSID() {
   }
   return "";
 }
-#endif  // !defined(OS_ANDROID)
 
 }  // namespace net
