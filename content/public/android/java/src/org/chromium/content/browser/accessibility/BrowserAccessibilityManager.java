163a2b33690858e806f0dd9b74fe8f963d15537d
dmazzoni <dmazzoni@chromium.org>
2016-02-11 15:55:36 -0800

    Include a role description string in node info for Android accessibility.
    BUG=583063
    
    Review URL: https://codereview.chromium.org/1654193002
    
    Cr-Commit-Position: refs/heads/master@{#375045}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/BrowserAccessibilityManager.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/BrowserAccessibilityManager.java
@@ -76,6 +76,9 @@ public class BrowserAccessibilityManager {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             return new LollipopBrowserAccessibilityManager(
                     nativeBrowserAccessibilityManagerAndroid, contentViewCore);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            return new KitKatBrowserAccessibilityManager(
+                    nativeBrowserAccessibilityManagerAndroid, contentViewCore);
         } else {
             return new BrowserAccessibilityManager(
                     nativeBrowserAccessibilityManagerAndroid, contentViewCore);
@@ -857,6 +860,12 @@ public class BrowserAccessibilityManager {
                 moveAccessibilityFocusToIdAndRefocusIfNeeded(virtualViewId);
             }
         }
+    }
+
+    @CalledByNative
+    protected void setAccessibilityNodeInfoKitKatAttributes(AccessibilityNodeInfo node,
+            String roleDescription) {
+        // Requires KitKat or higher.
     }
 
     @CalledByNative
