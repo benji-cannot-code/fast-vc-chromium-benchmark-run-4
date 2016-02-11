26259dad0b545c70aff2da3330639a769b7ad010
nick <nick@chromium.org>
2016-02-11 15:08:21 -0800

    Add a control group to the SiteIsolationExtensionsActive.
    "Control" corresponds to the Disabled group of the [non-synthetic]
    SiteIsolationExtensions field trial. In practice, our analysis
    has been skewed by the fact that SiteIsolationExtensionsActive/Default
    includes samples from populations where the SiteIsolationExtensions trial was
    not operative; these samples were a source of confounding variables.
    
    BUG=542921
    
    Review URL: https://codereview.chromium.org/1682253006
    
    Cr-Commit-Position: refs/heads/master@{#375027}

--- chrome/browser/site_details_browsertest.cc
+++ chrome/browser/site_details_browsertest.cc
@@ -1098,7 +1098,10 @@ IN_PROC_BROWSER_TEST_F(SiteDetailsBrowserTest, VerifyFieldTrialGroup) {
       group = "FieldTrial";
     }
   } else {
-    group = "Default";
+    if (base::FieldTrialList::FindFullName("SiteIsolationExtensions").empty())
+      group = "Default";
+    else
+      group = "Control";
   }
 
   EXPECT_TRUE(IsInTrialGroup("SiteIsolationExtensionsActive", group));
