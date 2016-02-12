1627f22d557867360327913072753957e9438de0
clamy <clamy@chromium.org>
2016-02-12 10:22:11 -0800

    Reland #1 Remove the is_loading_ field from WebContentsImpl
    The CL was reverted since it introduced flakyness on the
    SitePerProcessBrowserTest.NavigateRemoteFrameToBlankAndDataURLs test on the
    ChromiumOS dbg bot.
    
    Original commit message:
    This CL removes the is_loading_ field from WebContents, in favor of only
    tracking the loading state through the FrameTree. Currently the loading state
    is tracked in both, leading to more complexity in the code.
    
    BUG=571887, 298193
    TBR=rockot@chromium.org,nick@chromium.org,carlosk@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review URL: https://codereview.chromium.org/1690653003
    
    Cr-Commit-Position: refs/heads/master@{#375200}

--- content/browser/frame_host/navigation_controller_delegate.h
+++ content/browser/frame_host/navigation_controller_delegate.h
@@ -43,7 +43,6 @@ class NavigationControllerDelegate {
   virtual void Stop() = 0;
   virtual int32_t GetMaxPageID() = 0;
   virtual int32_t GetMaxPageIDForSiteInstance(SiteInstance* site_instance) = 0;
-  virtual bool IsLoading() const = 0;
   virtual bool IsBeingDestroyed() const = 0;
   virtual bool CanOverscrollContent() const = 0;
 
@@ -72,10 +71,8 @@ class NavigationControllerDelegate {
       RenderFrameHost* render_frame_host) = 0;
   virtual void AttachInterstitialPage(
       InterstitialPageImpl* interstitial_page) = 0;
+  virtual void DidProceedOnInterstitial() = 0;
   virtual void DetachInterstitialPage() = 0;
-  virtual void SetIsLoading(bool is_loading,
-                            bool to_different_document,
-                            LoadNotificationDetails* details) = 0;
 };
 
 }  // namespace content
