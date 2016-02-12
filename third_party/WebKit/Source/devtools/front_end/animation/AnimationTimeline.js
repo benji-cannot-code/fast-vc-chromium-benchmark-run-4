a5e9e44639a638008c782c9bddbc6b4e9d5913bd
samli <samli@chromium.org>
2016-02-11 17:10:29 -0800

    Devtools Animations: Remove cached left value when resizing
    BUG=585688
    
    Review URL: https://codereview.chromium.org/1689553002
    
    Cr-Commit-Position: refs/heads/master@{#375068}

--- third_party/WebKit/Source/devtools/front_end/animation/AnimationTimeline.js
+++ third_party/WebKit/Source/devtools/front_end/animation/AnimationTimeline.js
@@ -539,6 +539,7 @@ WebInspector.AnimationTimeline.prototype = {
         this.scheduleRedraw();
         if (this._scrubberPlayer)
             this._syncScrubber();
+        delete this._gridOffsetLeft;
     },
 
     /**
