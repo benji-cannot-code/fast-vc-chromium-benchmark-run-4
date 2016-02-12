6269e03c25a88cc6f820583609836c7545ce67b3
sadrul <sadrul@chromium.org>
2016-02-12 13:54:35 -0800

    mus/ozone: Avoid mus-apps from crashing when built with ozone.
    . mus-clients should not use a real OzonePlatform implementation.
      Instead, they should either not initialize ozone at all, or
      initialize an implementation that talks to mus instead of the
      native platform.
    
    . Avoid initializing cursor-manager in ash::Shell when running as
      a mus client.
    
    BUG=586512
    
    Review URL: https://codereview.chromium.org/1690333003
    
    Cr-Commit-Position: refs/heads/master@{#375266}

--- ash/shell.cc
+++ ash/shell.cc
@@ -643,9 +643,7 @@ Shell::Shell(ShellDelegate* delegate, base::SequencedWorkerPool* blocking_pool)
 #if defined(OS_CHROMEOS)
       display_configurator_(new ui::DisplayConfigurator()),
 #endif  // defined(OS_CHROMEOS)
-      native_cursor_manager_(new AshNativeCursorManager),
-      cursor_manager_(
-          scoped_ptr<::wm::NativeCursorManager>(native_cursor_manager_)),
+      native_cursor_manager_(nullptr),
       simulate_modal_window_open_for_testing_(false),
       is_touch_hud_projection_enabled_(false),
       blocking_pool_(blocking_pool) {
@@ -849,6 +847,16 @@ Shell::~Shell() {
 
 void Shell::Init(const ShellInitParams& init_params) {
   in_mus_ = init_params.in_mus;
+  if (!in_mus_) {
+    native_cursor_manager_ = new AshNativeCursorManager;
+#if defined(OS_CHROMEOS)
+    cursor_manager_.reset(
+        new CursorManager(make_scoped_ptr(native_cursor_manager_)));
+#else
+    cursor_manager_.reset(
+        new ::wm::CursorManager(make_scoped_ptr(native_cursor_manager_)));
+#endif
+  }
 
   delegate_->PreInit();
   bool display_initialized = display_manager_->InitFromCommandLine();
@@ -926,7 +934,8 @@ void Shell::Init(const ShellInitParams& init_params) {
       new ResolutionNotificationController);
 #endif
 
-  cursor_manager_.SetDisplay(gfx::Screen::GetScreen()->GetPrimaryDisplay());
+  if (cursor_manager_)
+    cursor_manager_->SetDisplay(gfx::Screen::GetScreen()->GetPrimaryDisplay());
 
   accelerator_controller_.reset(new AcceleratorController);
   maximize_mode_controller_.reset(new MaximizeModeController());
@@ -1073,9 +1082,11 @@ void Shell::Init(const ShellInitParams& init_params) {
   // the correct size.
   user_wallpaper_delegate_->InitializeWallpaper();
 
-  if (initially_hide_cursor_)
-    cursor_manager_.HideCursor();
-  cursor_manager_.SetCursor(ui::kCursorPointer);
+  if (cursor_manager_) {
+    if (initially_hide_cursor_)
+      cursor_manager_->HideCursor();
+    cursor_manager_->SetCursor(ui::kCursorPointer);
+  }
 
 #if defined(OS_CHROMEOS)
   // Set accelerator controller delegates.
@@ -1134,7 +1145,7 @@ void Shell::InitRootWindow(aura::Window* root_window) {
   aura::client::SetDragDropClient(root_window, drag_drop_controller_.get());
   aura::client::SetScreenPositionClient(root_window,
                                         screen_position_controller_.get());
-  aura::client::SetCursorClient(root_window, &cursor_manager_);
+  aura::client::SetCursorClient(root_window, cursor_manager_.get());
   aura::client::SetTooltipClient(root_window, tooltip_controller_.get());
   aura::client::SetEventClient(root_window, event_client_.get());
 
