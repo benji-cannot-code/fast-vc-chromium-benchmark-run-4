cc843d12a4325e69d6441c74a5ded571a6d98d15
aizatsky <aizatsky@chromium.org>
2016-02-11 17:40:14 -0800

    [libfuzzer] do not use weak attribute on windows.
    BUG=586030
    
    Review URL: https://codereview.chromium.org/1688113006
    
    Cr-Commit-Position: refs/heads/master@{#375082}

--- testing/libfuzzer/unittest_main.cc
+++ testing/libfuzzer/unittest_main.cc
@@ -11,12 +11,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iterator>
 #include <vector>
 
+#if defined(_MSC_VER)
+# define SANITIZER_WEAK_ATTRIBUTE
+#else
+# define SANITIZER_WEAK_ATTRIBUTE  __attribute__((weak))
+#endif
+
 // Libfuzzer API.
 extern "C" {
   // User function.
   int LLVMFuzzerTestOneInput(const unsigned char* data, size_t size);
   // Initialization function.
-  __attribute__((weak)) int LLVMFuzzerInitialize(int *argc, char ***argv);
+  SANITIZER_WEAK_ATTRIBUTE int LLVMFuzzerInitialize(int *argc, char ***argv);
 }
 
 std::vector<char> readFile(std::string path) {
