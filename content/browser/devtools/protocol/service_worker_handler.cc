2fef4761e1e94dff1d85b3ff86f275ca715b70ab
horo <horo@chromium.org>
2016-02-11 18:40:48 -0800

    [DevTools] Unset SW's "Force update on page load" flag when DevTools is closed.
    BUG=547666,500428
    
    Review URL: https://codereview.chromium.org/1682953003
    
    Cr-Commit-Position: refs/heads/master@{#375100}

--- content/browser/devtools/protocol/service_worker_handler.cc
+++ content/browser/devtools/protocol/service_worker_handler.cc
@@ -324,6 +324,8 @@ Response ServiceWorkerHandler::Disable() {
   enabled_ = false;
 
   ServiceWorkerDevToolsManager::GetInstance()->RemoveObserver(this);
+  for (const auto registration_id : force_update_enabled_registrations_)
+    context_->SetForceUpdateOnPageLoad(registration_id, false);
   for (const auto& pair : attached_hosts_)
     pair.second->DetachClient();
   attached_hosts_.clear();
@@ -424,6 +426,10 @@ Response ServiceWorkerHandler::SetForceUpdateOnPageLoad(
   int64_t id = kInvalidServiceWorkerRegistrationId;
   if (!base::StringToInt64(registration_id, &id))
     return CreateInvalidVersionIdErrorResponse();
+  if (force_update_on_page_load)
+    force_update_enabled_registrations_.insert(id);
+  else
+    force_update_enabled_registrations_.erase(id);
   context_->SetForceUpdateOnPageLoad(id, force_update_on_page_load);
   return Response::OK();
 }
