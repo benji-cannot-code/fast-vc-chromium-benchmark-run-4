04ed7e7167431198e6a0c586725653a4636701c4
reillyg <reillyg@chromium.org>
2016-02-12 17:54:22 -0800

    Parse USB interface association descriptors.
    USB interface association descriptors are used to combine multiple
    interfaces into a single functional group. This patch adds support for
    parsing them out of the |extra_data| field left by libusb's parsing of
    configuration, interface and endpoint descriptors. The resulting
    association is then represented by setting the |first_interface| field
    of each interface in a function to the |interface_number| of the first
    interface in the function.
    
    WebUSB will use these associations to set permissions for an entire
    function with a single descriptor.
    
    BUG=492204
    
    Review URL: https://codereview.chromium.org/1568673002
    
    Cr-Commit-Position: refs/heads/master@{#375324}

--- device/usb/usb_descriptors.h
+++ device/usb/usb_descriptors.h
@@ -86,6 +86,8 @@ struct UsbInterfaceDescriptor {
   uint8_t interface_protocol;
   std::vector<UsbEndpointDescriptor> endpoints;
   std::vector<uint8_t> extra_data;
+  // First interface of the function to which this interface belongs.
+  uint8_t first_interface;
 };
 
 struct UsbConfigDescriptor {
@@ -95,6 +97,10 @@ struct UsbConfigDescriptor {
                       uint16_t maximum_power);
   UsbConfigDescriptor() = delete;
   ~UsbConfigDescriptor();
+
+  // Scans through |extra_data| for interface association descriptors and
+  // populates |first_interface| for each interface in this configuration.
+  void AssignFirstInterfaceNumbers();
 
   uint8_t configuration_value;
   bool self_powered;
