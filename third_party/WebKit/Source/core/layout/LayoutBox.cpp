9fb58010169402cdfc0c50c0e6c4235e295dda58
robhogan <robhogan@gmail.com>
2016-02-13 01:47:59 -0800

    Don't collapse anonymous boxes in the left-side of a split box if they're not alone
    BUG=585704
    
    Review URL: https://codereview.chromium.org/1693473003
    
    Cr-Commit-Position: refs/heads/master@{#375358}

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -4467,7 +4467,7 @@ LayoutObject* LayoutBox::splitAnonymousBoxesAroundChild(LayoutObject* beforeChil
                 collapseLoneAnonymousBlockChild(child);
             child = boxToSplit->slowFirstChild();
             ASSERT(child);
-            if (child && !child->previousSibling())
+            if (child && !child->nextSibling())
                 collapseLoneAnonymousBlockChild(child);
 
             markBoxForRelayoutAfterSplit(boxToSplit);
