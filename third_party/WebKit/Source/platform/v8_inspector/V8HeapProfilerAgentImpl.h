2201d921418f01558c41898126c8f1d980d76d18
alph <alph@chromium.org>
2016-02-12 19:39:27 -0800

    DevTools: Add samling heap profiler support to BE and protocol
    BUG=586613
    
    Review URL: https://codereview.chromium.org/1699543002
    
    Cr-Commit-Position: refs/heads/master@{#375338}

--- third_party/WebKit/Source/platform/v8_inspector/V8HeapProfilerAgentImpl.h
+++ third_party/WebKit/Source/platform/v8_inspector/V8HeapProfilerAgentImpl.h
@@ -41,6 +41,9 @@ public:
     void addInspectedHeapObject(ErrorString*, const String& inspectedHeapObjectId) override;
     void getHeapObjectId(ErrorString*, const String& objectId, String* heapSnapshotObjectId) override;
 
+    void startSampling(ErrorString*) override;
+    void stopSampling(ErrorString*, RefPtr<protocol::TypeBuilder::HeapProfiler::SamplingHeapProfile>&) override;
+
     void requestHeapStatsUpdate() override;
 
 private:
