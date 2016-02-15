c777187204d6a7604149aa6f428580b7be875e6f
hajimehoshi <hajimehoshi@chromium.org>
2016-02-15 00:33:54 -0800

    Introduce HeapAllocHooks
    This CL introduces HeapAllocHooks like WTF::PartitionAllocHooks. This is
    preparation for Oilpan heap profiler on memory infra.
    
    Now hooks doesn't accept object type names. This would require a lot of
    code change, so I'll implement fundamental features for heap profiler
    first and add type names later.
    
    BUG=585063
    TEST=n/a
    
    Review URL: https://codereview.chromium.org/1676973002
    
    Cr-Commit-Position: refs/heads/master@{#375424}

--- third_party/WebKit/Source/platform/heap/Heap.cpp
+++ third_party/WebKit/Source/platform/heap/Heap.cpp
@@ -53,6 +53,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+HeapAllocHooks::AllocationHook* HeapAllocHooks::m_allocationHook = nullptr;
+HeapAllocHooks::FreeHook* HeapAllocHooks::m_freeHook = nullptr;
+
 class GCForbiddenScope final {
     DISALLOW_NEW();
 public:
