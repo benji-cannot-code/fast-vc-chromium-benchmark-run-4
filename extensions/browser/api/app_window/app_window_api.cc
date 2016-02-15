41c8f78dce19c10235b0a99ce45568b204c08f20
rob <rob@robwu.nl>
2016-02-15 03:06:26 -0800

    Remove "experimental-html" from chrome.app.window
    The "experimental-html" frame option is behind a flag, undocumented and
    seems not used by real app developers.
    
    Review URL: https://codereview.chromium.org/1690893003
    
    Cr-Commit-Position: refs/heads/master@{#375443}

--- extensions/browser/api/app_window/app_window_api.cc
+++ extensions/browser/api/app_window/app_window_api.cc
@@ -76,8 +76,6 @@ const char kImeWindowMustBeImeWindowOrPanel[] =
 }  // namespace app_window_constants
 
 const char kNoneFrameOption[] = "none";
-  // TODO(benwells): Remove HTML titlebar injection.
-const char kHtmlFrameOption[] = "experimental-html";
 
 namespace {
 
@@ -125,8 +123,7 @@ void CopyBoundsSpec(const app_window::BoundsSpecification* input_spec,
 
 }  // namespace
 
-AppWindowCreateFunction::AppWindowCreateFunction()
-    : inject_html_titlebar_(false) {}
+AppWindowCreateFunction::AppWindowCreateFunction() {}
 
 bool AppWindowCreateFunction::RunAsync() {
   // Don't create app window if the system is shutting down.
@@ -199,8 +196,6 @@ bool AppWindowCreateFunction::RunAsync() {
           result->Set("frameId", new base::FundamentalValue(frame_id));
           existing_window->GetSerializedState(result);
           result->SetBoolean("existingWindow", true);
-          // TODO(benwells): Remove HTML titlebar injection.
-          result->SetBoolean("injectTitlebar", false);
           SetResult(result);
           SendResponse(true);
           return true;
@@ -361,8 +356,6 @@ bool AppWindowCreateFunction::RunAsync() {
 
   base::DictionaryValue* result = new base::DictionaryValue;
   result->Set("frameId", new base::FundamentalValue(frame_id));
-  result->Set("injectTitlebar",
-      new base::FundamentalValue(inject_html_titlebar_));
   result->Set("id", new base::StringValue(app_window->window_key()));
   app_window->GetSerializedState(result);
   SetResult(result);
@@ -504,15 +497,6 @@ bool AppWindowCreateFunction::GetBoundsSpec(
 
 AppWindow::Frame AppWindowCreateFunction::GetFrameFromString(
     const std::string& frame_string) {
-  if (frame_string == kHtmlFrameOption &&
-      (extension()->permissions_data()->HasAPIPermission(
-           APIPermission::kExperimental) ||
-       base::CommandLine::ForCurrentProcess()->HasSwitch(
-           switches::kEnableExperimentalExtensionApis))) {
-     inject_html_titlebar_ = true;
-     return AppWindow::FRAME_NONE;
-  }
-
   if (frame_string == kNoneFrameOption)
     return AppWindow::FRAME_NONE;
 
