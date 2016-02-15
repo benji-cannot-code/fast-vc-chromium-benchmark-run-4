27bffa348aba2c29ee96f104dfd7f6ab6c491d3a
amistry <amistry@chromium.org>
2016-02-14 17:03:35 -0800

    Add mach port to PlatformHandle.
    BUG=582468
    
    Review URL: https://codereview.chromium.org/1689203004
    
    Cr-Commit-Position: refs/heads/master@{#375398}

--- mojo/edk/embedder/platform_handle.h
+++ mojo/edk/embedder/platform_handle.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include <windows.h>
+#elif defined(OS_MACOSX) && !defined(OS_IOS)
+#include <mach/mach.h>
 #endif
 
 namespace mojo {
@@ -18,14 +20,36 @@ namespace edk {
 
 #if defined(OS_POSIX)
 struct MOJO_SYSTEM_IMPL_EXPORT PlatformHandle {
-  PlatformHandle() : handle(-1) {}
+  PlatformHandle() {}
   explicit PlatformHandle(int handle) : handle(handle) {}
+#if defined(OS_MACOSX) && !defined(OS_IOS)
+  explicit PlatformHandle(mach_port_t port)
+      : type(Type::MACH), port(port) {}
+#endif
 
   void CloseIfNecessary();
 
-  bool is_valid() const { return handle != -1; }
+  bool is_valid() const {
+#if defined(OS_MACOSX) && !defined(OS_IOS)
+    if (type == Type::MACH)
+      return port != MACH_PORT_NULL;
+#endif
+    return handle != -1;
+  }
 
-  int handle;
+  enum class Type {
+    POSIX,
+#if defined(OS_MACOSX) && !defined(OS_IOS)
+    MACH,
+#endif
+  };
+  Type type = Type::POSIX;
+
+  int handle = -1;
+
+#if defined(OS_MACOSX) && !defined(OS_IOS)
+  mach_port_t port = MACH_PORT_NULL;
+#endif
 };
 #elif defined(OS_WIN)
 struct MOJO_SYSTEM_IMPL_EXPORT PlatformHandle {
