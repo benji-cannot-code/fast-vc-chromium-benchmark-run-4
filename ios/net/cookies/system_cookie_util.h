45306152ee0cd8d38a93d7b0a7d7e0e630cf14c6
sdefresne <sdefresne@chromium.org>
2016-02-16 04:44:42 -0800

    Clean forward-declaration of Objective-C classes.
    Remove hack to support forward-declaring Objective-C classes as C++
    classes since those headers are only included from Objective-C++
    files.
    
    BUG=298181
    
    Review URL: https://codereview.chromium.org/1699053002
    
    Cr-Commit-Position: refs/heads/master@{#375552}

--- ios/net/cookies/system_cookie_util.h
+++ ios/net/cookies/system_cookie_util.h
@@ -10,11 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/cookies/canonical_cookie.h"
 
-#if defined(__OBJC__)
 @class NSHTTPCookie;
-#else
-class NSHTTPCookie;
-#endif
 
 namespace base {
 class Time;
