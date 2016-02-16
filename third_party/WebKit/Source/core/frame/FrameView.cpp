8363d1c0d9fed09e12f184c3abc07a3fde6cdfee
leviw <leviw@chromium.org>
2016-02-16 14:42:05 -0800

    [Layout Object API] Create new LayoutItem API and use it in FrameView
    This API will be much like the current LineLayoutItem API, but its
    customers should be anything outside layout, paint, and editing,
    which is to say DOM, frame, html, outside core, etc.
    
    Eventually, it should cover the entire externally-facing API and
    will ideally aid the transition to a more sane API, and ease the
    pain of making larger structural changes inside the layout code.
    
    BUG=499321
    
    Review URL: https://codereview.chromium.org/1688403002
    
    Cr-Commit-Position: refs/heads/master@{#375688}

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/ScrollAlignment.h"
 #include "core/layout/TextAutosizer.h"
 #include "core/layout/TracedLayoutObject.h"
+#include "core/layout/api/LayoutItem.h"
 #include "core/layout/compositing/CompositedLayerMapping.h"
 #include "core/layout/compositing/CompositedSelection.h"
 #include "core/layout/compositing/PaintLayerCompositor.h"
@@ -870,8 +871,8 @@ void FrameView::performLayout(bool inSubtreeLayout)
             // We need to ensure that we mark up all layoutObjects up to the LayoutView
             // for paint invalidation. This simplifies our code as we just always
             // do a full tree walk.
-            if (LayoutObject* container = root->container())
-                container->setMayNeedPaintInvalidation();
+            if (LayoutItem container = LayoutItem(root->container()))
+                container.setMayNeedPaintInvalidation();
         }
         m_layoutSubtreeRootList.clear();
     } else {
@@ -957,7 +958,7 @@ void FrameView::layout()
     bool inSubtreeLayout = isSubtreeLayout();
 
     // FIXME: The notion of a single root for layout is no longer applicable. Remove or update this code. crbug.com/460596
-    LayoutObject* rootForThisLayout = inSubtreeLayout ? m_layoutSubtreeRootList.randomRoot() : layoutView();
+    LayoutItem rootForThisLayout(inSubtreeLayout ? m_layoutSubtreeRootList.randomRoot() : layoutView());
     if (!rootForThisLayout) {
         // FIXME: Do we need to set m_size here?
         ASSERT_NOT_REACHED();
