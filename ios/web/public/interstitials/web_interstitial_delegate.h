74f501b1e59131172354a96bdfc71bdc3a50b7c9
sdefresne <sdefresne@chromium.org>
2016-02-16 06:37:53 -0800

    Clean forward-declaration of Objective-C classes.
    Remove hack to support forward-declaring Objective-C classes as C++
    classes since those headers are only included from Objective-C++
    files.
    
    BUG=298181
    
    Review URL: https://codereview.chromium.org/1696343002
    
    Cr-Commit-Position: refs/heads/master@{#375568}

--- ios/web/public/interstitials/web_interstitial_delegate.h
+++ ios/web/public/interstitials/web_interstitial_delegate.h
@@ -8,13 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#if defined(__OBJC__)
 @class UIColor;
 @class UIView;
-#else
-class UIColor;
-class UIView;
-#endif  // __OBJC__
 
 namespace web {
 
