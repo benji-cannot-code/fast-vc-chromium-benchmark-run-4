0599097a773320773ad9b92b7eb4f02ab2521981
zqzhang <zqzhang@chromium.org>
2016-02-16 10:41:14 -0800

    [MediaSession] Record when media session ducks
    We now lower the volume instead of suspend the media session when we
    receiving AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK. However we still want to
    record the event in the UMA.
    
    BUG=580494
    
    Review URL: https://codereview.chromium.org/1685183002
    
    Cr-Commit-Position: refs/heads/master@{#375612}

--- content/public/android/java/src/org/chromium/content/browser/MediaSession.java
+++ content/public/android/java/src/org/chromium/content/browser/MediaSession.java
@@ -83,6 +83,7 @@ public class MediaSession implements AudioManager.OnAudioFocusChangeListener {
                 break;
             case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
                 mIsDucking = true;
+                nativeRecordSessionDuck(mNativeMediaSession);
                 nativeOnSetVolumeMultiplier(mNativeMediaSession, DUCKING_VOLUME_MULTIPLIER);
                 break;
             case AudioManager.AUDIOFOCUS_LOSS:
@@ -99,4 +100,5 @@ public class MediaSession implements AudioManager.OnAudioFocusChangeListener {
     private native void nativeOnResume(long nativeMediaSession);
     private native void nativeOnSetVolumeMultiplier(
             long nativeMediaSession, double volumeMultiplier);
+    private native void nativeRecordSessionDuck(long nativeMediaSession);
 }
