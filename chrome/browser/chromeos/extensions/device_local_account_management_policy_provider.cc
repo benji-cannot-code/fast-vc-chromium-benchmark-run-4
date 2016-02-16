e3d958491b150cce708b6ca5a873406c82fca9d1
bartfab <bartfab@chromium.org>
2016-02-16 06:19:35 -0800

    Whitelist smart card driver apps for public sessions
    This CL whitelists the two apps required to use smart cards in public
    sessions.
    
    BUG=584621
    
    Review URL: https://codereview.chromium.org/1700793003
    
    Cr-Commit-Position: refs/heads/master@{#375564}

--- chrome/browser/chromeos/extensions/device_local_account_management_policy_provider.cc
+++ chrome/browser/chromeos/extensions/device_local_account_management_policy_provider.cc
@@ -38,6 +38,8 @@ const char* const kPublicSessionWhitelist[] = {
     "gbchcmhmhahfdphkhkmpfmihenigjmpp",  // Chrome Remote Desktop
     "cjanmonomjogheabiocdamfpknlpdehm",  // HP printer driver
     "pmnllmkmjilbojkpgplbdmckghmaocjh",  // Scan app by François Beaufort
+    "khpfeaanjngmcnplbdlpegiifgpfgdco",  // SmartCard Manager App
+    "dojmlffgommefdofbfdajjpgjgjoffjo",  // Charismathics Smart Card Middleware
 
     // Libraries:
     "aclofikceldphonlfmghmimkodjdmhck",  // Ancoris login component
