3f3f3b6eee154386b53fbeb741809bc79a68f9f3
geofflang <geofflang@chromium.org>
2016-02-16 14:43:23 -0800

    Fix typo in the roll_angle.py script.
    TBR=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/1702773002
    
    Cr-Commit-Position: refs/heads/master@{#375690}

--- tools/roll_angle.py
+++ tools/roll_angle.py
@@ -21,7 +21,7 @@ extra_cq_trybots = [
   {
     "mastername": "tryserver.chromium.mac",
     "buildernames": ["mac_optional_gpu_tests_rel"]
-  }
+  },
   {
     "mastername": "tryserver.chromium.linux",
     "buildernames": ["linux_optional_gpu_tests_rel"]
