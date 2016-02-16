59dcdf55c31951b122cd53f653ca027520da7ce5
yosin <yosin@chromium.org>
2016-02-15 18:24:31 -0800

    Make ForwardDelete command abortable
    This patch makes "forwardDelete" command abortable to handle attempting
    to delete whole table in unedtiable element.
    
    Without CSS trick, we can't place selection in TABLE except for table cells and
    inside CAPTION. With CSS trick like below script, we can place selection in
    TABLE and make "ForwardDelete" command to attempt to remove TABLE.
    
    # Minimum script to reproduce crbug.com/539579
    <style>
    colgroup {
      display: block;
      float: right;
      height: 1px;
      width: 1px;
    }
    </style>
    <table contenteditable><colgroup id="sample"></colgroup></table>
    <script>
    getSelection().collapse(document.getElementById('sample'), 0);
    document.execCommand('ForwardDelete');
    </script>
    
    BUG=539579
    TEST=Cluster Fuzz will run its own script
    
    Review URL: https://codereview.chromium.org/1700703002
    
    Cr-Commit-Position: refs/heads/master@{#375503}

--- third_party/WebKit/Source/core/editing/Editor.cpp
+++ third_party/WebKit/Source/core/editing/Editor.cpp
@@ -292,6 +292,7 @@ bool Editor::deleteWithDirection(SelectionDirection direction, TextGranularity g
     if (!canEdit())
         return false;
 
+    EditingState editingState;
     if (frame().selection().isRange()) {
         if (isTypingAction) {
             ASSERT(frame().document());
@@ -313,7 +314,9 @@ bool Editor::deleteWithDirection(SelectionDirection direction, TextGranularity g
         case DirectionForward:
         case DirectionRight:
             ASSERT(frame().document());
-            TypingCommand::forwardDeleteKeyPressed(*frame().document(), options, granularity);
+            TypingCommand::forwardDeleteKeyPressed(*frame().document(), &editingState, options, granularity);
+            if (editingState.isAborted())
+                return false;
             break;
         case DirectionBackward:
         case DirectionLeft:
--- third_party/WebKit/Source/core/editing/commands/TypingCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/TypingCommand.cpp
@@ -137,7 +137,7 @@ void TypingCommand::deleteKeyPressed(Document& document, Options options, TextGr
     TypingCommand::create(document, DeleteKey, "", options, granularity)->apply();
 }
 
-void TypingCommand::forwardDeleteKeyPressed(Document& document, Options options, TextGranularity granularity)
+void TypingCommand::forwardDeleteKeyPressed(Document& document, EditingState* editingState, Options options, TextGranularity granularity)
 {
     // FIXME: Forward delete in TextEdit appears to open and close a new typing command.
     if (granularity == CharacterGranularity) {
@@ -145,7 +145,7 @@ void TypingCommand::forwardDeleteKeyPressed(Document& document, Options options,
         if (RefPtrWillBeRawPtr<TypingCommand> lastTypingCommand = lastTypingCommandIfStillOpenForTyping(frame)) {
             updateSelectionIfDifferentFromCurrentSelection(lastTypingCommand.get(), frame);
             lastTypingCommand->setShouldPreventSpellChecking(options & PreventSpellChecking);
-            lastTypingCommand->forwardDeleteKeyPressed(granularity, options & KillRing);
+            lastTypingCommand->forwardDeleteKeyPressed(granularity, options & KillRing, editingState);
             return;
         }
     }
@@ -274,7 +274,7 @@ void TypingCommand::doApply(EditingState* editingState)
         deleteKeyPressed(m_granularity, m_killRing, editingState);
         return;
     case ForwardDeleteKey:
-        forwardDeleteKeyPressed(m_granularity, m_killRing);
+        forwardDeleteKeyPressed(m_granularity, m_killRing, editingState);
         return;
     case InsertLineBreak:
         insertLineBreak();
@@ -537,7 +537,7 @@ void TypingCommand::deleteKeyPressed(TextGranularity granularity, bool killRing,
     typingAddedToOpenCommand(DeleteKey);
 }
 
-void TypingCommand::forwardDeleteKeyPressed(TextGranularity granularity, bool killRing)
+void TypingCommand::forwardDeleteKeyPressed(TextGranularity granularity, bool killRing, EditingState* editingState)
 {
     LocalFrame* frame = document().frame();
     if (!frame)
@@ -622,7 +622,9 @@ void TypingCommand::forwardDeleteKeyPressed(TextGranularity granularity, bool ki
     // Make undo select what was deleted on Mac alone
     if (frame->editor().behavior().shouldUndoOfDeleteSelectText())
         setStartingSelection(selectionAfterUndo);
-    CompositeEditCommand::deleteSelection(selectionToDelete, ASSERT_NO_EDITING_ABORT, m_smartDelete);
+    CompositeEditCommand::deleteSelection(selectionToDelete, editingState, m_smartDelete);
+    if (editingState->isAborted())
+        return;
     setSmartDelete(false);
     typingAddedToOpenCommand(ForwardDeleteKey);
 }
