662253325fdd14d8e8f3a8821e1c8810df0888d3
tzik <tzik@chromium.org>
2016-02-15 20:56:31 -0800

    Support smart pointers in base::DispatchToMethod
    * Make base::DispatchToMethod to accept smart pointers as the this pointer
    * Remove base::internal::UnwrapTraits from //storage code
    
    BUG=554299
    
    Review URL: https://codereview.chromium.org/1696093002
    
    Cr-Commit-Position: refs/heads/master@{#375516}

--- base/tuple.h
+++ base/tuple.h
@@ -197,7 +197,7 @@ inline Tuple<Ts&...> MakeRefTuple(Ts&... arg) {
 // Non-Static Dispatchers with no out params.
 
 template <typename ObjT, typename Method, typename... Ts, size_t... Ns>
-inline void DispatchToMethodImpl(ObjT* obj,
+inline void DispatchToMethodImpl(const ObjT& obj,
                                  Method method,
                                  const Tuple<Ts...>& arg,
                                  IndexSequence<Ns...>) {
@@ -205,7 +205,7 @@ inline void DispatchToMethodImpl(ObjT* obj,
 }
 
 template <typename ObjT, typename Method, typename... Ts>
-inline void DispatchToMethod(ObjT* obj,
+inline void DispatchToMethod(const ObjT& obj,
                              Method method,
                              const Tuple<Ts...>& arg) {
   DispatchToMethodImpl(obj, method, arg, MakeIndexSequence<sizeof...(Ts)>());
@@ -233,7 +233,7 @@ template <typename ObjT,
           typename... OutTs,
           size_t... InNs,
           size_t... OutNs>
-inline void DispatchToMethodImpl(ObjT* obj,
+inline void DispatchToMethodImpl(const ObjT& obj,
                                  Method method,
                                  const Tuple<InTs...>& in,
                                  Tuple<OutTs...>* out,
@@ -244,7 +244,7 @@ inline void DispatchToMethodImpl(ObjT* obj,
 }
 
 template <typename ObjT, typename Method, typename... InTs, typename... OutTs>
-inline void DispatchToMethod(ObjT* obj,
+inline void DispatchToMethod(const ObjT& obj,
                              Method method,
                              const Tuple<InTs...>& in,
                              Tuple<OutTs...>* out) {
