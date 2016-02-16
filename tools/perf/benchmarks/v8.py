e3342edccd36969d3b169aae3b849287743e7934
cbruni <cbruni@chromium.org>
2016-02-16 04:59:33 -0800

    [telemetry] adding more v8 metrics for parsing, optimization and execution - add Load / Wait interactions to infinite-scroll cases since v8 cares for startup
    BUG=
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_10_10_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1655223002
    
    Cr-Commit-Position: refs/heads/master@{#375555}

--- tools/perf/benchmarks/v8.py
+++ tools/perf/benchmarks/v8.py
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import shlex
 
 from core import perf_benchmark
 
@@ -12,6 +13,7 @@ from telemetry import benchmark
 from telemetry.timeline import tracing_category_filter
 from telemetry.web_perf import timeline_based_measurement
 from telemetry.web_perf.metrics import v8_gc_latency
+from telemetry.web_perf.metrics import v8_execution
 from telemetry.web_perf.metrics import smoothness
 from telemetry.web_perf.metrics import memory_timeline
 
@@ -61,23 +63,29 @@ class V8InfiniteScroll(perf_benchmark.PerfBenchmark):
   """Measures V8 GC metrics and memory usage while scrolling the top web pages.
   http://www.chromium.org/developers/design-documents/rendering-benchmarks"""
 
+  page_set = page_sets.InfiniteScrollPageSet
+
   def SetExtraBrowserOptions(self, options):
+    existing_js_flags = []
+    for extra_arg in options.extra_browser_args:
+      if extra_arg.startswith('--js-flags='):
+        existing_js_flags.extend(shlex.split(extra_arg[len('--js-flags='):]))
     options.AppendExtraBrowserArgs([
         # TODO(perezju): Temporary workaround to disable periodic memory dumps.
         # See: http://crbug.com/513692
         '--enable-memory-benchmarking',
-        '--js-flags=--heap-growing-percent=10'
+        # This overrides any existing --js-flags, hence we have to include the
+        # previous flags as well.
+        '--js-flags="--heap-growing-percent=10 %s"' %
+          (' '.join(existing_js_flags))
     ])
 
-  def CreateStorySet(self, options):
-    return page_sets.InfiniteScrollPageSet()
-
   def CreateTimelineBasedMeasurementOptions(self):
-    v8_gc_latency_categories = [
+    v8_categories = [
         'blink.console', 'renderer.scheduler', 'v8', 'webkit.console']
     smoothness_categories = [
         'webkit.console', 'blink.console', 'benchmark', 'trace_event_overhead']
-    categories = list(set(v8_gc_latency_categories + smoothness_categories))
+    categories = list(set(v8_categories + smoothness_categories))
     memory_categories = 'blink.console,disabled-by-default-memory-infra'
     category_filter = tracing_category_filter.TracingCategoryFilter(
         memory_categories)
@@ -85,6 +93,7 @@ class V8InfiniteScroll(perf_benchmark.PerfBenchmark):
       category_filter.AddIncludedCategory(category)
     options = timeline_based_measurement.Options(category_filter)
     options.SetLegacyTimelineBasedMetrics([v8_gc_latency.V8GCLatency(),
+                                     v8_execution.V8ExecutionMetric(),
                                      smoothness.SmoothnessMetric(),
                                      memory_timeline.MemoryTimelineMetric()])
     return options
@@ -95,8 +104,14 @@ class V8InfiniteScroll(perf_benchmark.PerfBenchmark):
 
   @classmethod
   def ValueCanBeAddedPredicate(cls, value, is_first_result):
+    if value.tir_label in ['Load', 'Wait']:
+      return (value.name.startswith('v8_') and not
+              value.name.startswith('v8_gc'))
     if value.tir_label in ['Begin', 'End']:
-      return value.name.startswith('memory_') and 'v8_renderer' in value.name
+      return (value.name.startswith('memory_') and
+              'v8_renderer' in value.name) or \
+             (value.name.startswith('v8_') and not
+              value.name.startswith('v8_gc'))
     else:
       return value.tir_label == 'Scrolling'
 
