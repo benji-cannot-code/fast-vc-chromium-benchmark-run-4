0429f1a58b4a2a5571bd12a3de61e55f7c47d45b
wnwen <wnwen@chromium.org>
2016-02-16 13:29:50 -0800

    Also adds debug build flag.
    BUG=583690
    
    Review URL: https://codereview.chromium.org/1597273005
    
    Cr-Commit-Position: refs/heads/master@{#375666}

--- base/android/java/src/org/chromium/base/multidex/ChromiumMultiDexInstaller.java
+++ base/android/java/src/org/chromium/base/multidex/ChromiumMultiDexInstaller.java
@@ -13,6 +13,7 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.multidex.MultiDex;
 
+import org.chromium.base.BuildConfig;
 import org.chromium.base.Log;
 import org.chromium.base.VisibleForTesting;
 
@@ -48,7 +49,7 @@ public class ChromiumMultiDexInstaller {
      */
     @VisibleForTesting
     public static void install(Context context) {
-        if (!ChromiumMultiDex.isMultidexEnabled()) return;
+        if (!BuildConfig.isMultidexEnabled()) return;
 
         // TODO(jbudorick): Back out this version check once support for K & below works.
         // http://crbug.com/512357
