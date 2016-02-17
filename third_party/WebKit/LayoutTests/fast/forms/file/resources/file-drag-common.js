927c9769842b30eee7dfb4ed42eb2cfcc739daab
oshima <oshima@chromium.org>
2016-02-16 23:46:21 -0800

    Do not scale the point to hitTest input element for file drop.
    BUG=268037
    
    Review URL: https://codereview.chromium.org/1690223004
    
    Cr-Commit-Position: refs/heads/master@{#375818}

--- third_party/WebKit/LayoutTests/fast/forms/file/resources/file-drag-common.js
+++ third_party/WebKit/LayoutTests/fast/forms/file/resources/file-drag-common.js
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function moveMouseToCenterOfElement(element) {
     var centerX = element.offsetLeft + element.offsetWidth / 2;
     var centerY = element.offsetTop + element.offsetHeight / 2;
-    eventSender.mouseMoveTo(centerX, centerY);
+    eventSender.mouseMoveTo(centerX * devicePixelRatio, centerY * devicePixelRatio);
 }
 
 function dragFilesOntoInput(input, files) {
