96dc63f94aa6d203e34cdc2855391acd19af676f
szager <szager@chromium.org>
2016-02-17 15:40:40 -0800

    Keep IntersectionObserver alive when it has no js references.
    Prior to this change, when the last js reference to an IntersectionObserver went away, and there were no js references to the callback function, the callback would disable itself.  The observer and its callback should stay alive as long as there are still target Elements being observed.
    
    BUG=586957,587299
    R=adamk@chromium.org,haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/1707913002
    
    Cr-Commit-Position: refs/heads/master@{#376033}

--- third_party/WebKit/Source/core/dom/IntersectionObserver.h
+++ third_party/WebKit/Source/core/dom/IntersectionObserver.h
@@ -52,6 +52,7 @@ public:
     void disconnect();
     void removeObservation(IntersectionObservation&);
     bool hasEntries() const { return m_entries.size(); }
+    const HeapHashSet<WeakMember<IntersectionObservation>>& observations() const { return m_observations; }
 
     DECLARE_TRACE();
 
