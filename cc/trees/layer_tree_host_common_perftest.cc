40f517c971d9e7d4fdeeb6da513c9af3bd05e8ee
weiliangc <weiliangc@chromium.org>
2016-02-17 13:28:23 -0800

    Use property trees for caculate draw properties perf test
    Set to use property trees for CalcDrawProps perf test. Other than
    command line option to turn off property trees, this should be the only
    place where we don't use property trees.
    
    R=enne
    BUG=581832
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Committed: https://crrev.com/87137a3dfba3e68b284335a26b9cabc687ac8b61
    Cr-Commit-Position: refs/heads/master@{#375687}
    
    Review URL: https://codereview.chromium.org/1702673003
    
    Cr-Commit-Position: refs/heads/master@{#375984}

--- cc/trees/layer_tree_host_common_perftest.cc
+++ cc/trees/layer_tree_host_common_perftest.cc
@@ -109,9 +109,8 @@ class CalcDrawPropsTest : public LayerTreeHostCommonPerfTest {
                                 LayerTreeImpl* active_tree,
                                 LayerTreeHostImpl* host_impl) {
     LayerImplList update_list;
-    PropertyTrees property_trees;
     bool verify_property_trees = false;
-    bool use_property_trees = false;
+    bool use_property_trees = true;
     active_tree->IncrementRenderSurfaceListIdForTesting();
     LayerTreeHostCommon::CalcDrawPropsImplInputs inputs(
         active_tree->root_layer(), active_tree->DrawViewportSize(),
@@ -127,7 +126,8 @@ class CalcDrawPropsTest : public LayerTreeHostCommonPerfTest {
         can_render_to_separate_surface,
         host_impl->settings().layer_transforms_should_scale_layer_contents,
         verify_property_trees, use_property_trees, &update_list,
-        active_tree->current_render_surface_list_id(), &property_trees);
+        active_tree->current_render_surface_list_id(),
+        active_tree->property_trees());
     LayerTreeHostCommon::CalculateDrawProperties(&inputs);
   }
 };
