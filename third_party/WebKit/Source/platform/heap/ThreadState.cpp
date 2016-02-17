108a9e80509cbf412c2401f83018e4c62c3947bb
haraken <haraken@chromium.org>
2016-02-17 06:55:46 -0800

    Oilpan: The relationship of GCScope : Visitor should be 1 : 1
    This CL makes the relationship of GCScope : Visitor be 1 : 1.
    This CL makes sure that all call sites that use a (Marking)Visitor
    create a VisitorScope, which wraps the Visitor.
    
    - Move GCScope::parkAllThreads to ResumeThreadScope.
    - Rename ResumeThreadScope to ParkThreadsScope.
    - Rename GCScope to VisitorScope.
    
    This CL shouldn't change any behavior.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1698433002
    
    Cr-Commit-Position: refs/heads/master@{#375882}

--- third_party/WebKit/Source/platform/heap/ThreadState.cpp
+++ third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -38,8 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/heap/CallbackStack.h"
 #include "platform/heap/Handle.h"
 #include "platform/heap/Heap.h"
-#include "platform/heap/MarkingVisitor.h"
 #include "platform/heap/SafePoint.h"
+#include "platform/heap/Visitor.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebMemoryAllocatorDump.h"
 #include "public/platform/WebProcessMemoryDump.h"
@@ -481,10 +481,10 @@ void ThreadState::threadLocalWeakProcessing()
     // Due to the complexity, we just forbid allocations.
     NoAllocationScope noAllocationScope(this);
 
-    MarkingVisitor<Visitor::WeakProcessing> weakProcessingVisitor;
+    VisitorScope visitorScope(this, BlinkGC::ThreadLocalWeakProcessing);
 
     // Perform thread-specific weak processing.
-    while (popAndInvokeThreadLocalWeakCallback(&weakProcessingVisitor)) { }
+    while (popAndInvokeThreadLocalWeakCallback(visitorScope.visitor())) { }
 
     if (isMainThread()) {
         double timeForThreadLocalWeakProcessing = WTF::currentTimeMS() - startTime;
