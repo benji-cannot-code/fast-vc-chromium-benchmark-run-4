b483ba16c1beacdab6d5bb4717cf9d4a5c4ac3af
mgiuca <mgiuca@chromium.org>
2016-02-16 17:45:30 -0800

    Increase the font size and related sizes for fullscreen bubble.
    Affects Windows, Linux and Chrome OS.
    
    BUG=585358
    
    Review URL: https://codereview.chromium.org/1683623002
    
    Cr-Commit-Position: refs/heads/master@{#375745}

--- chrome/browser/ui/views/exclusive_access_bubble_views.cc
+++ chrome/browser/ui/views/exclusive_access_bubble_views.cc
@@ -49,7 +49,7 @@ namespace {
 // Space between the site info label and the buttons / link.
 const int kMiddlePaddingPx = 30;
 
-const int kOuterPaddingHorizPx = 24;
+const int kOuterPaddingHorizPx = 40;
 const int kOuterPaddingVertPx = 8;
 
 // Partially-transparent background color. Only used with
@@ -261,11 +261,8 @@ ExclusiveAccessBubbleViews::ExclusiveAccessView::ExclusiveAccessView(
   SetFocusable(false);
 
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
-  ui::ResourceBundle::FontStyle font_style =
-      ExclusiveAccessManager::IsSimplifiedFullscreenUIEnabled()
-          ? ui::ResourceBundle::SmallFont
-          : ui::ResourceBundle::MediumFont;
-  const gfx::FontList& font_list = rb.GetFontList(font_style);
+  const gfx::FontList& font_list =
+      rb.GetFontList(ui::ResourceBundle::MediumFont);
 
   if (!ExclusiveAccessManager::IsSimplifiedFullscreenUIEnabled()) {
     message_label_ = new views::Label(base::string16(), font_list);
