92de64050abaf43c08f36b2c8afc9554a934089b
domlaskowski <domlaskowski@chromium.org>
2016-02-17 12:21:23 -0800

    ARC: Synchronize crash reporting consent
    This CL adds a settings observer for syncing crash reporting consent.
    
    BUG=586662
    TEST=Deploy both sides. Toggle "Automatically send usage statistics and
         crash reports to Google" in Chrome OS settings. Observe the change
         in Settings > Google > Overflow Icon > Usage & diagnostics.
    
    Review URL: https://codereview.chromium.org/1698333003
    
    Cr-Commit-Position: refs/heads/master@{#375966}

--- chrome/browser/chromeos/arc/settings_bridge.h
+++ chrome/browser/chromeos/arc/settings_bridge.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/values.h"
+#include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chromeos/settings/timezone_settings.h"
 #include "components/arc/arc_bridge_service.h"
 #include "components/prefs/pref_change_registrar.h"
@@ -69,6 +70,7 @@ class SettingsBridge : public chromeos::system::TimezoneSettings::Observer {
   void SyncAllPrefs() const;
   void SyncFontSize() const;
   void SyncLocale() const;
+  void SyncReportingConsent() const;
   void SyncSpokenFeedbackEnabled() const;
   void SyncTimeZone() const;
   void SyncUse24HourClock() const;
@@ -85,6 +87,9 @@ class SettingsBridge : public chromeos::system::TimezoneSettings::Observer {
 
   // Manages pref observation registration.
   PrefChangeRegistrar registrar_;
+
+  scoped_ptr<chromeos::CrosSettings::ObserverSubscription>
+      reporting_consent_subscription_;
 
   Delegate* const delegate_;
 
