979858337e9ffa1c39c5a2edc6f2b5c198185d05
youngmin.yoo <youngmin.yoo@samsung.com>
2016-02-16 22:10:14 -0800

    [tracing] Support shared and private permission. (missing perm flag)
    following /proc/[pid]/maps perm field:
    r = read
    w = write
    x = execute
    s = shared
    p = private (copy on write)
    
    BUG=584161
    
    Review URL: https://codereview.chromium.org/1663533005
    
    Cr-Commit-Position: refs/heads/master@{#375807}

--- base/trace_event/process_memory_maps.h
+++ base/trace_event/process_memory_maps.h
@@ -26,6 +26,7 @@ class BASE_EXPORT ProcessMemoryMaps {
     static const uint32_t kProtectionFlagsRead;
     static const uint32_t kProtectionFlagsWrite;
     static const uint32_t kProtectionFlagsExec;
+    static const uint32_t kProtectionFlagsMayshare;
 
     VMRegion();
 
