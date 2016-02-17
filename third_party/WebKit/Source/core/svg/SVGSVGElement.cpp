9cf079719d0991ede64d97a59ed2a0e72716c995
davve <davve@opera.com>
2016-02-16 18:56:34 -0800

    Deprecate SVGZoomEvent and SVGZoomEvents
    Deprecate the SVGZoomEvent interface and the corresponding onzoom
    attribute. Show deprecation message to the user when creating the
    event or when setting up an event handler using onzoom="..." on
    <svg:svg>.
    
    Intent to deprecate:
    
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/5PEUh2qHa7c/IUGlM4a9CQAJ
    
    BUG=367890
    
    Review URL: https://codereview.chromium.org/1691883003
    
    Cr-Commit-Position: refs/heads/master@{#375771}

--- third_party/WebKit/Source/core/svg/SVGSVGElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGSVGElement.cpp
@@ -32,14 +32,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/StaticNodeList.h"
 #include "core/editing/FrameSelection.h"
 #include "core/events/EventListener.h"
-#include "core/frame/LocalFrame.h"
-#include "core/page/FrameTree.h"
+#include "core/frame/Deprecation.h"
 #include "core/frame/FrameView.h"
-#include "core/frame/UseCounter.h"
+#include "core/frame/LocalFrame.h"
 #include "core/layout/LayoutObject.h"
 #include "core/layout/svg/LayoutSVGModelObject.h"
 #include "core/layout/svg/LayoutSVGRoot.h"
 #include "core/layout/svg/LayoutSVGViewportContainer.h"
+#include "core/page/FrameTree.h"
 #include "core/svg/SVGAngleTearOff.h"
 #include "core/svg/SVGDocumentExtensions.h"
 #include "core/svg/SVGNumberTearOff.h"
@@ -202,16 +202,18 @@ void SVGSVGElement::parseAttribute(const QualifiedName& name, const AtomicString
         bool setListener = true;
 
         // Only handle events if we're the outermost <svg> element
-        if (name == HTMLNames::onunloadAttr)
+        if (name == HTMLNames::onunloadAttr) {
             document().setWindowAttributeEventListener(EventTypeNames::unload, createAttributeEventListener(document().frame(), name, value, eventParameterName()));
-        else if (name == HTMLNames::onresizeAttr)
+        } else if (name == HTMLNames::onresizeAttr) {
             document().setWindowAttributeEventListener(EventTypeNames::resize, createAttributeEventListener(document().frame(), name, value, eventParameterName()));
-        else if (name == HTMLNames::onscrollAttr)
+        } else if (name == HTMLNames::onscrollAttr) {
             document().setWindowAttributeEventListener(EventTypeNames::scroll, createAttributeEventListener(document().frame(), name, value, eventParameterName()));
-        else if (name == SVGNames::onzoomAttr)
+        } else if (name == SVGNames::onzoomAttr) {
+            Deprecation::countDeprecation(document(), UseCounter::SVGZoomEvent);
             document().setWindowAttributeEventListener(EventTypeNames::zoom, createAttributeEventListener(document().frame(), name, value, eventParameterName()));
-        else
+        } else {
             setListener = false;
+        }
 
         if (setListener)
             return;
