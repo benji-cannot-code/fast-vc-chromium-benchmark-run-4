9f69abc27493a727092380c49222c9ee4b62f8b1
tedchoc <tedchoc@chromium.org>
2016-02-17 10:56:12 -0800

    Ensure the toolbar shadow is drawn for all texture captures.
    The underlying problem is that the shadow is hidden when entering
    the tab switcher.  The texture is often being recreated in this
    case, and that results in a texture w/o the shadow.
    
    BUG=583533
    
    Review URL: https://codereview.chromium.org/1703753003
    
    Cr-Commit-Position: refs/heads/master@{#375938}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarPhone.java
@@ -1373,10 +1373,12 @@ public class ToolbarPhone extends ToolbarLayout
         assert mTextureCaptureMode != textureMode;
         mTextureCaptureMode = textureMode;
         if (mTextureCaptureMode) {
+            mToolbarShadow.setVisibility(VISIBLE);
             mPreTextureCaptureAlpha = getAlpha();
             setAlpha(1);
         } else {
             setAlpha(mPreTextureCaptureAlpha);
+            updateShadowVisibility(mIsInTabSwitcherMode);
             mPreTextureCaptureAlpha = 1f;
         }
     }
