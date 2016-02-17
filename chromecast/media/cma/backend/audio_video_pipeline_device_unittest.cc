7495f34a80b607fd4fe3e48efddd49c528620911
kmackay <kmackay@chromium.org>
2016-02-17 11:39:14 -0800

    [Chromecast] Fix up media unit tests some more
     * kCodecPCM_S16BE is actually optional.
     * DTS will not be supported.
     * Fix up config conversions for AC3/EAC3.
    
    BUG= internal b/27218625
    
    Review URL: https://codereview.chromium.org/1710463002
    
    Cr-Commit-Position: refs/heads/master@{#375951}

--- chromecast/media/cma/backend/audio_video_pipeline_device_unittest.cc
+++ chromecast/media/cma/backend/audio_video_pipeline_device_unittest.cc
@@ -444,10 +444,6 @@ void BufferFeeder::TestAudioConfigs() {
   audio_decoder->SetConfig(config);
 
   // Next, test required sample formats.
-  config.codec = kCodecPCM_S16BE;
-  EXPECT_TRUE(audio_decoder->SetConfig(config))
-      << "Audio decoder does not accept kCodecPCM_S16BE";
-
   config.codec = kCodecPCM;
   EXPECT_TRUE(audio_decoder->SetConfig(config))
       << "Audio decoder does not accept kCodecPCM";
@@ -471,6 +467,9 @@ void BufferFeeder::TestAudioConfigs() {
 
   // Test optional codecs.
   // TODO(kmackay) Make sure other parts of config are correct for each codec.
+  config.codec = kCodecPCM_S16BE;
+  if (!audio_decoder->SetConfig(config))
+    LOG(INFO) << "Audio decoder does not accept kCodecPCM_S16BE";
   config.codec = kCodecOpus;
   if (!audio_decoder->SetConfig(config))
     LOG(INFO) << "Audio decoder does not accept kCodecOpus";
@@ -480,9 +479,6 @@ void BufferFeeder::TestAudioConfigs() {
   config.codec = kCodecAC3;
   if (!audio_decoder->SetConfig(config))
     LOG(INFO) << "Audio decoder does not accept kCodecAC3";
-  config.codec = kCodecDTS;
-  if (!audio_decoder->SetConfig(config))
-    LOG(INFO) << "Audio decoder does not accept kCodecDTS";
   config.codec = kCodecFLAC;
   if (!audio_decoder->SetConfig(config))
     LOG(INFO) << "Audio decoder does not accept kCodecFLAC";
@@ -923,16 +919,6 @@ TEST_F(AudioVideoPipelineDeviceTest, OpusPlayback_Optional) {
   message_loop->Run();
 }
 
-TEST_F(AudioVideoPipelineDeviceTest, DtsPlayback_Optional) {
-  scoped_ptr<base::MessageLoop> message_loop(new base::MessageLoop());
-
-  set_sync_type(MediaPipelineDeviceParams::kModeSyncPts);
-  ConfigureForAudioOnly("bear.adts");
-  PauseBeforeEos();
-  Start();
-  message_loop->Run();
-}
-
 TEST_F(AudioVideoPipelineDeviceTest, FlacPlayback_Optional) {
   scoped_ptr<base::MessageLoop> message_loop(new base::MessageLoop());
 
@@ -1107,17 +1093,6 @@ TEST_F(AudioVideoPipelineDeviceTest, OpusPlayback_WithEffectsStreams_Optional) {
 
   set_sync_type(MediaPipelineDeviceParams::kModeSyncPts);
   ConfigureForAudioOnly("bear-opus.ogg");
-  PauseBeforeEos();
-  AddEffectsStreams();
-  Start();
-  message_loop->Run();
-}
-
-TEST_F(AudioVideoPipelineDeviceTest, DtsPlayback_WithEffectsStreams_Optional) {
-  scoped_ptr<base::MessageLoop> message_loop(new base::MessageLoop());
-
-  set_sync_type(MediaPipelineDeviceParams::kModeSyncPts);
-  ConfigureForAudioOnly("bear.adts");
   PauseBeforeEos();
   AddEffectsStreams();
   Start();
