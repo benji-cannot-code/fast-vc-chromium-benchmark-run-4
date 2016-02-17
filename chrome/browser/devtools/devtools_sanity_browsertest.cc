08eee145c6643923466c55ad05ff75c1f0a4e0b9
henrika <henrika@chromium.org>
2016-02-17 02:41:26 -0800

    Revert of DevTools: add input event instrumentation test (patchset #2 id:40001 of https://codereview.chromium.org/1704723002/ )
    Reason for revert:
    Seems to break DevToolsPixelOutputTests.TestLatencyInfoInstrumentation on Mac.
    
    See https://build.chromium.org/p/chromium.mac/builders/Mac10.6%20Tests/builds/12163 for details.
    
    Original issue's description:
    > DevTools: add input event instrumentation test
    >
    > BUG=554215
    >
    > Committed: https://crrev.com/e99c42679051cd91ef92155bb7f190a9405acedc
    > Cr-Commit-Position: refs/heads/master@{#375802}
    
    TBR=dgozman@chromium.org,pfeldman@chromium.org,caseq@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=554215
    
    Review URL: https://codereview.chromium.org/1705533003
    
    Cr-Commit-Position: refs/heads/master@{#375847}

--- chrome/browser/devtools/devtools_sanity_browsertest.cc
+++ chrome/browser/devtools/devtools_sanity_browsertest.cc
@@ -50,8 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/render_view_host.h"
-#include "content/public/browser/render_widget_host.h"
-#include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/worker_service.h"
 #include "content/public/browser/worker_service_observer.h"
@@ -65,7 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/value_builder.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/spawned_test_server/spawned_test_server.h"
-#include "third_party/WebKit/public/web/WebInputEvent.h"
 #include "ui/compositor/compositor_switches.h"
 #include "ui/gl/gl_switches.h"
 
@@ -93,7 +90,6 @@ const char kPageWithContentScript[] =
 const char kNavigateBackTestPage[] =
     "files/devtools/navigate_back.html";
 const char kWindowOpenTestPage[] = "files/devtools/window_open.html";
-const char kLatencyInfoTestPage[] = "files/devtools/latency_info.html";
 const char kChunkedTestPage[] = "chunked";
 const char kSlowTestPage[] =
     "chunked?waitBeforeHeaders=100&waitBetweenChunks=100&chunksNumber=2";
@@ -105,25 +101,6 @@ const char kReloadSharedWorkerTestPage[] =
     "files/workers/debug_shared_worker_initialization.html";
 const char kReloadSharedWorkerTestWorker[] =
     "files/workers/debug_shared_worker_initialization.js";
-
-template <typename... T>
-void DispatchOnTestSuiteSkipCheck(DevToolsWindow* window,
-                                  const char* method,
-                                  T... args) {
-  RenderViewHost* rvh = DevToolsWindowTesting::Get(window)
-                            ->main_web_contents()
-                            ->GetRenderViewHost();
-  std::string result;
-  const char* args_array[] = {method, args...};
-  std::ostringstream script;
-  script << "uiTests.dispatchOnTestSuite([";
-  for (size_t i = 0; i < arraysize(args_array); ++i)
-    script << (i ? "," : "") << '\"' << args_array[i] << '\"';
-  script << "])";
-  ASSERT_TRUE(
-      content::ExecuteScriptAndExtractString(rvh, script.str(), &result));
-  EXPECT_EQ("[OK]", result);
-}
 
 template <typename... T>
 void DispatchOnTestSuite(DevToolsWindow* window,
@@ -143,7 +120,16 @@ void DispatchOnTestSuite(DevToolsWindow* window,
           "    '' + (window.uiTests && (typeof uiTests.dispatchOnTestSuite)));",
           &result));
   ASSERT_EQ("function", result) << "DevTools front-end is broken.";
-  DispatchOnTestSuiteSkipCheck(window, method, args...);
+
+  const char* args_array[] = {method, args...};
+  std::ostringstream script;
+  script << "uiTests.dispatchOnTestSuite([";
+  for (size_t i = 0; i < arraysize(args_array); ++i)
+    script << (i ? "," : "") << '\"' << args_array[i] << '\"';
+  script << "])";
+  ASSERT_TRUE(
+      content::ExecuteScriptAndExtractString(rvh, script.str(), &result));
+  EXPECT_EQ("[OK]", result);
 }
 
 void RunTestFunction(DevToolsWindow* window, const char* test_name) {
@@ -181,21 +167,6 @@ class DevToolsSanityTest : public InProcessBrowserTest {
     OpenDevToolsWindow(test_page, false);
     RunTestFunction(window_, test_name.c_str());
     CloseDevToolsWindow();
-  }
-
-  template <typename... T>
-  void RunTestMethod(const char* method, T... args) {
-    DispatchOnTestSuiteSkipCheck(window_, method, args...);
-  }
-
-  template <typename... T>
-  void DispatchAndWait(const char* method, T... args) {
-    DispatchOnTestSuiteSkipCheck(window_, "waitForAsync", method, args...);
-  }
-
-  template <typename... T>
-  void DispatchInPageAndWait(const char* method, T... args) {
-    DispatchAndWait("invokePageFunctionAsync", method, args...);
   }
 
   void LoadTestPage(const std::string& test_page) {
@@ -1175,44 +1146,8 @@ class DevToolsPixelOutputTests : public DevToolsSanityTest {
 #else
 #define MAYBE_TestScreenshotRecording TestScreenshotRecording
 #endif
+// Tests raw headers text.
 IN_PROC_BROWSER_TEST_F(DevToolsPixelOutputTests,
                        MAYBE_TestScreenshotRecording) {
   RunTest("testScreenshotRecording", std::string());
-}
-
-// This test enables switches::kUseGpuInTests which causes false positives
-// with MemorySanitizer.
-#if defined(MEMORY_SANITIZER) || defined(ADDRESS_SANITIZER)
-#define MAYBE_TestLatencyInfoInstrumentation \
-  DISABLED_TestLatencyInfoInstrumentation
-#else
-#define MAYBE_TestLatencyInfoInstrumentation TestLatencyInfoInstrumentation
-#endif
-IN_PROC_BROWSER_TEST_F(DevToolsPixelOutputTests,
-                       MAYBE_TestLatencyInfoInstrumentation) {
-  WebContents* web_contents = GetInspectedTab();
-  OpenDevToolsWindow(kLatencyInfoTestPage, false);
-  RunTestMethod("enableExperiment", "timelineLatencyInfo");
-  DispatchAndWait("startTimeline");
-
-  SimulateMouseEvent(web_contents, blink::WebInputEvent::MouseMove,
-                     gfx::Point(30, 60));
-  DispatchInPageAndWait("waitForEvent", "mousemove");
-
-  SimulateMouseClickAt(web_contents, 0, blink::WebPointerProperties::ButtonLeft,
-                       gfx::Point(30, 60));
-  DispatchInPageAndWait("waitForEvent", "click");
-
-  SimulateMouseWheelEvent(web_contents, gfx::Point(300, 100),
-                          gfx::Vector2d(0, 120));
-  DispatchInPageAndWait("waitForEvent", "wheel");
-
-  SimulateTapAt(web_contents, gfx::Point(30, 60));
-  DispatchInPageAndWait("waitForEvent", "gesturetap");
-
-  DispatchAndWait("stopTimeline");
-  RunTestMethod("checkInputEventsPresent", "MouseMove", "MouseDown",
-                "MouseWheel", "GestureTap");
-
-  CloseDevToolsWindow();
 }
