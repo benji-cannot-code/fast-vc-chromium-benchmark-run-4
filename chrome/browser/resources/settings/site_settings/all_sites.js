a828c8e7f8d86e2e675151e05b6c878beadf98eb
finnur <finnur@chromium.org>
2016-02-17 05:39:02 -0800

    Implement the All Sites category.
    BUG=543635
    
    Review URL: https://codereview.chromium.org/1688153003
    
    Cr-Commit-Position: refs/heads/master@{#375871}

---
+++ chrome/browser/resources/settings/site_settings/all_sites.js
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * 'all-sites' is the polymer element for showing the list of all sites under
+ * Site Settings.
+ *
+ * @group Chrome Settings Elements
+ * @element all-sites
+ */
+Polymer({
+  is: 'all-sites',
+
+  behaviors: [SiteSettingsBehavior],
+
+  properties: {
+    /**
+     * Preferences state.
+     */
+    prefs: {
+      type: Object,
+      notify: true,
+    },
+
+    /**
+     * The current active route.
+     */
+    currentRoute: {
+      type: Object,
+      notify: true,
+    },
+
+    /**
+     * The origin that was selected by the user in the dropdown list.
+     */
+    selectedOrigin: {
+      type: String,
+      notify: true,
+    },
+  },
+});
