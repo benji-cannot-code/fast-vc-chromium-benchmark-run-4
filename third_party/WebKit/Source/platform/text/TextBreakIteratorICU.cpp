8b2e7fd166e2c06d7082d841c8b6bf7e727f6b37
rob.buis <rob.buis@samsung.com>
2016-02-16 16:55:33 -0800

    Clean up header includes in TextBreakIteratorICU.cpp
    Based on work by a.bah@samsung.com.
    
    Review URL: https://codereview.chromium.org/1700213003
    
    Cr-Commit-Position: refs/heads/master@{#375733}

--- third_party/WebKit/Source/platform/text/TextBreakIteratorICU.cpp
+++ third_party/WebKit/Source/platform/text/TextBreakIteratorICU.cpp
@@ -28,8 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/PassOwnPtr.h"
 #include "wtf/ThreadSpecific.h"
 #include "wtf/ThreadingPrimitives.h"
-#include "wtf/text/AtomicString.h"
-#include "wtf/text/CString.h"
 #include "wtf/text/WTFString.h"
 #include <unicode/rbbi.h>
 #include <unicode/ubrk.h>
