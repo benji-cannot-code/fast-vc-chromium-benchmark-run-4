44d2eaba394bf8ba95bee39b18290dff43ab057b
falken <falken@chromium.org>
2016-02-17 15:40:20 -0800

    service worker: use 200 OK for update requests even in the no update case
    The main motivation is to stop network failure error spew in DevTools console.
    This patch reinstates the "pause on download" functionality we once had.
    
    BUG=541797
    
    Review URL: https://codereview.chromium.org/1675613002
    
    Cr-Commit-Position: refs/heads/master@{#376032}

--- content/browser/service_worker/service_worker_register_job.h
+++ content/browser/service_worker/service_worker_register_job.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_SERVICE_WORKER_SERVICE_WORKER_REGISTER_JOB_H_
 #define CONTENT_BROWSER_SERVICE_WORKER_SERVICE_WORKER_REGISTER_JOB_H_
 
+#include <string>
 #include <vector>
 
 #include "base/macros.h"
@@ -35,7 +36,8 @@ class ServiceWorkerStorage;
 //  - waiting for older ServiceWorkerVersions to deactivate
 //  - designating the new version to be the 'active' version
 //  - updating storage
-class ServiceWorkerRegisterJob : public ServiceWorkerRegisterJobBase {
+class ServiceWorkerRegisterJob : public ServiceWorkerRegisterJobBase,
+                                 public EmbeddedWorkerInstance::Listener {
  public:
   typedef base::Callback<void(ServiceWorkerStatusCode status,
                               const std::string& status_message,
@@ -134,6 +136,9 @@ class ServiceWorkerRegisterJob : public ServiceWorkerRegisterJobBase {
 
   void AddRegistrationToMatchingProviderHosts(
       ServiceWorkerRegistration* registration);
+
+  // EmbeddedWorkerInstance::Listener implementation:
+  void OnScriptLoaded() override;
 
   // The ServiceWorkerContextCore object should always outlive this.
   base::WeakPtr<ServiceWorkerContextCore> context_;
