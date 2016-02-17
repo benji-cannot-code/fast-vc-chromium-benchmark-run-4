9c7c7ddc7abbf13861c3d347516707ef304aeeb5
jbauman <jbauman@chromium.org>
2016-02-17 14:11:51 -0800

    Fix handling of failing GLSurface initialization.
    This was causing a crash later when the PassThroughImageTransportSurface is used.
    
    BUG=none
    TBR=piman@chromium.org
    
    Review URL: https://codereview.chromium.org/1700363004
    
    Cr-Commit-Position: refs/heads/master@{#375997}

--- content/common/gpu/image_transport_surface_win.cc
+++ content/common/gpu/image_transport_surface_win.cc
@@ -46,6 +46,8 @@ scoped_refptr<gfx::GLSurface> ImageTransportSurface::CreateNativeSurface(
       return nullptr;
   } else {
     surface = gfx::GLSurface::CreateViewGLSurface(handle.handle);
+    if (!surface)
+      return nullptr;
   }
 
   return scoped_refptr<gfx::GLSurface>(new PassThroughImageTransportSurface(
