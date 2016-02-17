13a8674f9d26fb4e72ca702cba00b1fa386e65dd
mattcary <mattcary@chromium.org>
2016-02-17 08:30:11 -0800

    Local webserver test harness plus 3 tests.
    This adds a trace_test directory to tools/android/loading that
    performs an integration test for our tracing, and also serves as a
    respository for initiator corner cases that we are investigating.
    
    Review URL: https://codereview.chromium.org/1696353002
    
    Cr-Commit-Position: refs/heads/master@{#375901}

---
+++ tools/android/loading/trace_test/tests/1a.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/* Copyright 2016 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+var scr = document.createElement('script');
+scr.setAttribute('type', 'text/javascript');
+scr.setAttribute('src', '1b.js');
+document.getElementsByTagName('head')[0].appendChild(scr)
