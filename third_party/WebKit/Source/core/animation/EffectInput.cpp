d3dbe66b31b6ebc034b1d9e9852230bbf3968c61
alancutter <alancutter@chromium.org>
2016-02-17 06:08:43 -0800

    Avoid style recalc during element.animate() when unnecessary
    BUG=587257
    
    Review URL: https://codereview.chromium.org/1698913003
    
    Cr-Commit-Position: refs/heads/master@{#375875}

--- third_party/WebKit/Source/core/animation/EffectInput.cpp
+++ third_party/WebKit/Source/core/animation/EffectInput.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/Dictionary.h"
 #include "bindings/core/v8/UnionTypesCore.h"
 #include "core/animation/AnimationInputHelpers.h"
+#include "core/animation/CompositorAnimations.h"
 #include "core/animation/KeyframeEffectModel.h"
 #include "core/animation/StringKeyframe.h"
 #include "core/css/CSSStyleSheet.h"
@@ -52,13 +53,10 @@ EffectModel* EffectInput::convert(Element* element, const Vector<Dictionary>& ke
     if (!element)
         return nullptr;
 
-    // TODO(alancutter): Remove this once composited animations no longer depend on AnimatableValues.
-    if (element->inActiveDocument())
-        element->document().updateLayoutTreeForNodeIfNeeded(element);
-
     StyleSheetContents* styleSheetContents = element->document().elementSheet().contents();
     StringKeyframeVector keyframes;
     double lastOffset = 0;
+    bool encounteredCompositableProperty = false;
 
     for (const auto& keyframeDictionary : keyframeDictionaryVector) {
         RefPtr<StringKeyframe> keyframe = StringKeyframe::create();
@@ -111,6 +109,9 @@ EffectModel* EffectInput::convert(Element* element, const Vector<Dictionary>& ke
 
             CSSPropertyID cssProperty = AnimationInputHelpers::keyframeAttributeToCSSProperty(property);
             if (cssProperty != CSSPropertyInvalid) {
+                if (!encounteredCompositableProperty && CompositorAnimations::isCompositableProperty(cssProperty))
+                    encounteredCompositableProperty = true;
+
                 keyframe->setCSSPropertyValue(cssProperty, value, element, styleSheetContents);
                 continue;
             }
@@ -132,6 +133,10 @@ EffectModel* EffectInput::convert(Element* element, const Vector<Dictionary>& ke
                 keyframe->setSVGAttributeValue(*svgAttribute, value);
         }
     }
+
+    // TODO(alancutter): Remove this once composited animations no longer depend on AnimatableValues.
+    if (encounteredCompositableProperty && element->inActiveDocument())
+        element->document().updateLayoutTreeForNodeIfNeeded(element);
 
     StringKeyframeEffectModel* keyframeEffectModel = StringKeyframeEffectModel::create(keyframes);
     if (!RuntimeEnabledFeatures::cssAdditiveAnimationsEnabled()) {
