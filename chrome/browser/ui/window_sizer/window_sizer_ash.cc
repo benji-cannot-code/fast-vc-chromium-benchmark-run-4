337e8f52b115c757b0b9d5497f39debc0d38b0ae
sadrul <sadrul@chromium.org>
2016-02-16 22:17:46 -0800

    chrome/chromeos: Fix launching chrome as a mus client.
    Avoid initializaing ash::Shell when running chrome as a mus client. This
    requires a number of components to be turned off that depend on a Shell
    instance.
    
    Notable changes:
    . Do not initialize ash::Shell when chrome is launched as a mus client.
      This is because the window-management and system-ui functionality are
      provided by separate mus clients (desktop_wm and ash_sysui).
    
    . Turn off AccessibilityManager (and MagnificationManager) when launching
      chrome as a mus client. Accessibility should be a separate service.
      That work is tracked in crbug.com/557401
    
    . Turn off PowerButtonObserver, DataPromoNotification, and EventRewriter.
      These too should be separate mus/mojo apps.
    
    BUG=585918
    
    Review URL: https://codereview.chromium.org/1706593002
    
    Cr-Commit-Position: refs/heads/master@{#375808}

--- chrome/browser/ui/window_sizer/window_sizer_ash.cc
+++ chrome/browser/ui/window_sizer/window_sizer_ash.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/wm/window_positioner.h"
 #include "ash/wm/window_state.h"
+#include "chrome/browser/ui/ash/ash_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "ui/aura/window.h"
@@ -16,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 bool WindowSizer::GetBrowserBoundsAsh(gfx::Rect* bounds,
                                       ui::WindowShowState* show_state) const {
+  if (!chrome::ShouldOpenAshOnStartup())
+    return false;
   if (!browser_ ||
       browser_->host_desktop_type() != chrome::HOST_DESKTOP_TYPE_ASH)
     return false;
