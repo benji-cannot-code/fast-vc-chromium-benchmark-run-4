c471463d76475710f3630ec279c701d657fae807
noel <noel@chromium.org>
2016-02-16 21:23:39 -0800

    BitmapImage: inline isBitmapImage
    Since BitmapImage is a final class, inline isBitmapImage is faster
    than when not inline. This patch is modestly progressive on render
    speed in cluster telemetry (see CT run #541 on https://ct.skia.org
    for reference).
    
    No change in behavior, no new tests.
    
    TBR=tomhudson@chromium.org
    BUG=587038
    
    Review URL: https://codereview.chromium.org/1695383002
    
    Cr-Commit-Position: refs/heads/master@{#375795}

--- third_party/WebKit/Source/platform/graphics/BitmapImage.cpp
+++ third_party/WebKit/Source/platform/graphics/BitmapImage.cpp
@@ -103,11 +103,6 @@ BitmapImage::~BitmapImage()
     stopAnimation();
 }
 
-bool BitmapImage::isBitmapImage() const
-{
-    return true;
-}
-
 bool BitmapImage::currentFrameHasSingleSecurityOrigin() const
 {
     return true;
