b7164e33038d25a652cea3029c457437d0d52183
jinsukkim <jinsukkim@chromium.org>
2016-02-16 18:14:07 -0800

    Set alpha attr only for low-end devices only
    Workaround to Android 4.2.2 + Ardeno 320 GPU combo that
    shows black noise on rendering result.
    
    BUG=584013
    
    Review URL: https://codereview.chromium.org/1674543002
    
    Cr-Commit-Position: refs/heads/master@{#375760}

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/single_thread_task_runner.h"
 #include "base/synchronization/lock.h"
+#include "base/sys_info.h"
 #include "base/thread_task_runner_handle.h"
 #include "base/threading/simple_thread.h"
 #include "base/threading/thread.h"
@@ -523,7 +524,8 @@ void CompositorImpl::CreateOutputSurface() {
   blink::WebGraphicsContext3D::Attributes attrs;
   attrs.shareResources = true;
   attrs.noAutomaticFlushes = true;
-  attrs.alpha = has_transparent_background_;
+  if (base::SysInfo::IsLowEndDevice())
+    attrs.alpha = has_transparent_background_;
 
   pending_swapbuffers_ = 0;
 
