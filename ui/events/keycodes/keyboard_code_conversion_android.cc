6f303b89c02935e1f4726c25aab3a7712c73fa50
kpschoedel <kpschoedel@chromium.org>
2016-02-17 07:35:13 -0800

    Update ui::DomKey and DomCode to match 2015/12/15 spec.
    Data in dom_key_data.inc and keycode_converter_data.inc are now in sync
    with the latest published W3C documents:
      https://www.w3.org/TR/2015/WD-uievents-key-20151215/
      https://www.w3.org/TR/2015/WD-uievents-code-20151215/
    
    The most notable change is the removal of 'OS' in favour of 'Meta'.
    
    Also revised unit tests for keycode_converter_data to make future work
    faster by checking all native columns regardless of the build target.
    
    Review URL: https://codereview.chromium.org/1668513003
    
    Cr-Commit-Position: refs/heads/master@{#375891}

--- ui/events/keycodes/keyboard_code_conversion_android.cc
+++ ui/events/keycodes/keyboard_code_conversion_android.cc
@@ -76,9 +76,9 @@ DomKey GetDomKeyFromAndroidKeycode(int keycode) {
     case AKEYCODE_DPAD_RIGHT:
       return DomKey::ARROW_RIGHT;
     case AKEYCODE_VOLUME_UP:
-      return DomKey::VOLUME_UP;
+      return DomKey::AUDIO_VOLUME_UP;
     case AKEYCODE_VOLUME_DOWN:
-      return DomKey::VOLUME_DOWN;
+      return DomKey::AUDIO_VOLUME_DOWN;
     case AKEYCODE_POWER:
       return DomKey::POWER;
     case AKEYCODE_CAMERA:
@@ -120,7 +120,7 @@ DomKey GetDomKeyFromAndroidKeycode(int keycode) {
     case AKEYCODE_MEDIA_FAST_FORWARD:
       return DomKey::MEDIA_FAST_FORWARD;
     case AKEYCODE_MUTE:
-      return DomKey::VOLUME_MUTE;
+      return DomKey::AUDIO_VOLUME_MUTE;
     case AKEYCODE_PAGE_UP:
       return DomKey::PAGE_UP;
     case AKEYCODE_PAGE_DOWN:
@@ -190,7 +190,7 @@ DomKey GetDomKeyFromAndroidKeycode(int keycode) {
     case AKEYCODE_NUM_LOCK:
       return DomKey::NUM_LOCK;
     case AKEYCODE_VOLUME_MUTE:
-      return DomKey::VOLUME_MUTE;
+      return DomKey::AUDIO_VOLUME_MUTE;
     case AKEYCODE_INFO:
       return DomKey::INFO;
     case AKEYCODE_CHANNEL_UP:
@@ -202,7 +202,7 @@ DomKey GetDomKeyFromAndroidKeycode(int keycode) {
     case AKEYCODE_ZOOM_OUT:
       return DomKey::ZOOM_OUT;
     case AKEYCODE_TV:
-      return DomKey::T_V;
+      return DomKey::TV;
     case AKEYCODE_GUIDE:
       return DomKey::GUIDE;
     case AKEYCODE_CAPTIONS:
