e152d091612dde075aa016477f1134cf52313a9a
dgn <dgn@chromium.org>
2016-02-18 12:43:48 -0800

    Reland of Allow delaying GoogleApiClientHelper disconnections (patchset #1 id:1 of https://codereview.chromium.org/1714553002/ )
    Reason for revert:
    Reland.Seems related to uncleared statics. Will protect the tests by resetting them
    state both before and after each test.
    
    Original issue's description:
    > Revert of Allow delaying GoogleApiClientHelper disconnections (patchset #7 id:120001 of https://codereview.chromium.org/1702373002/ )
    >
    > Reason for revert:
    > Speculatively reverting: see crbug.com/587938
    >
    > Original issue's description:
    > > Allow delaying GoogleApiClientHelper disconnections
    > >
    > > Adds a willUseConnection() method that allows to make sure a client is connected and pending disconnections are postponed.
    > >
    > > BUG=586396
    > >
    > > Committed: https://crrev.com/c2ca605c7c880869a55e51549ccc7b78e4a59441
    > > Cr-Commit-Position: refs/heads/master@{#376174}
    >
    > TBR=bauerb@chromium.org,dgn@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=586396
    >
    > Committed: https://crrev.com/3775a59123399eb6e9fc0f312f314676ef204d04
    > Cr-Commit-Position: refs/heads/master@{#376227}
    
    TBR=bauerb@chromium.org,fsamuel@chromium.org
    
    BUG=586396
    
    Review URL: https://codereview.chromium.org/1711933002
    
    Cr-Commit-Position: refs/heads/master@{#376251}

--- chrome/android/java/src/org/chromium/chrome/browser/gcore/LifecycleHook.java
+++ chrome/android/java/src/org/chromium/chrome/browser/gcore/LifecycleHook.java
@@ -85,7 +85,7 @@ class LifecycleHook implements ApplicationStateListener {
         synchronized (mClientHelpers) {
             for (GoogleApiClientHelper clientHelper : mClientHelpers) {
                 if (mIsApplicationVisible) clientHelper.restoreConnectedState();
-                else clientHelper.disconnectWithDelay();
+                else clientHelper.scheduleDisconnection();
             }
         }
     }
