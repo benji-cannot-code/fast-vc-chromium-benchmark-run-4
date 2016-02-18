7e4057d6cae9e53447fb44adfd5a05c34a09c3b9
skym <skym@chromium.org>
2016-02-18 09:52:48 -0800

    [Sync] DeviceInfoService implementation for GetData/GetAllData.
    BUG=543404
    
    Review URL: https://codereview.chromium.org/1690773002
    
    Cr-Commit-Position: refs/heads/master@{#376197}

--- sync/api/model_type_service.h
+++ sync/api/model_type_service.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace syncer_v2 {
 
 class DataBatch;
-class MetadataBatch;
 class MetadataChangeList;
 
 // Interface implemented by model types to receive updates from sync via the
