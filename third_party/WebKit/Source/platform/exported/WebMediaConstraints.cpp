0bbe61ea7e6b3ef93199ff40b4e813c803dc5b54
hta <hta@chromium.org>
2016-02-18 09:08:38 -0800

    Clean out vestiges of the googAecDump constraint
    The setting of the constraint was removed in
    https://codereview.chromium.org/1659803002.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1711653002
    
    Cr-Commit-Position: refs/heads/master@{#376183}

--- third_party/WebKit/Source/platform/exported/WebMediaConstraints.cpp
+++ third_party/WebKit/Source/platform/exported/WebMediaConstraints.cpp
@@ -455,7 +455,6 @@ WebMediaTrackConstraintSet::WebMediaTrackConstraintSet()
     , googArrayGeometry("googArrayGeometry")
     , googAudioMirroring("googAudioMirroring")
     , googDAEchoCancellation("googDAEchoCancellation")
-    , googAecDump("googAecDump")
     , googNoiseReduction("googNoiseReduction")
     , offerToReceiveAudio("offerToReceiveAudio")
     , offerToReceiveVideo("offerToReceiveVideo")
@@ -493,7 +492,7 @@ std::vector<const BaseConstraint*> WebMediaTrackConstraintSet::allConstraints() 
         &googHighpassFilter, &googTypingNoiseDetection,
         &googExperimentalNoiseSuppression, &googBeamforming,
         &googArrayGeometry, &googAudioMirroring, &googDAEchoCancellation,
-        &googAecDump, &googNoiseReduction, &offerToReceiveAudio,
+        &googNoiseReduction, &offerToReceiveAudio,
         &offerToReceiveVideo, &voiceActivityDetection, &iceRestart,
         &googUseRtpMux, &enableDtlsSrtp, &enableRtpDataChannels,
         &enableDscp, &enableIPv6, &googEnableVideoSuspendBelowMinBitrate,
--- third_party/WebKit/public/platform/WebMediaConstraints.h
+++ third_party/WebKit/public/platform/WebMediaConstraints.h
@@ -259,7 +259,6 @@ public:
     StringConstraint googArrayGeometry;
     BooleanConstraint googAudioMirroring;
     BooleanConstraint googDAEchoCancellation;
-    BooleanConstraint googAecDump;
     BooleanConstraint googNoiseReduction;
     StringConstraint offerToReceiveAudio;
     StringConstraint offerToReceiveVideo;
