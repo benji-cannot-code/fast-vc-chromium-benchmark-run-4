4bf08a936a63b022998f5467e0dc740a2741743c
vasilii <vasilii@chromium.org>
2016-02-18 11:18:05 -0800

    Don't disable the auto-signin flag for a credential from Incognito mode.
    BUG=585455
    
    Review URL: https://codereview.chromium.org/1713603002
    
    Cr-Commit-Position: refs/heads/master@{#376224}

--- components/password_manager/content/browser/credential_manager_dispatcher.h
+++ components/password_manager/content/browser/credential_manager_dispatcher.h
@@ -65,9 +65,6 @@ class CredentialManagerDispatcher
   // content::WebContentsObserver implementation.
   bool OnMessageReceived(const IPC::Message& message) override;
 
-  using CredentialCallback =
-      base::Callback<void(const autofill::PasswordForm&)>;
-
   // CredentialManagerPendingRequestTaskDelegate:
   bool IsZeroClickAllowed() const override;
   GURL GetOrigin() const override;
@@ -102,6 +99,9 @@ class CredentialManagerDispatcher
   void ScheduleRequireMediationTask(
       int request_id,
       const std::vector<std::string>& android_realms);
+
+  // Returns true iff it's OK to update credentials in the password store.
+  bool IsUpdatingCredentialAllowed() const;
 
   PasswordManagerClient* client_;
   scoped_ptr<CredentialManagerPasswordFormManager> form_manager_;
