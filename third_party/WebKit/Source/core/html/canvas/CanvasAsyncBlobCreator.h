0adc05e08095e4d5e5e32c271bbe56935823a616
xlai <xlai@chromium.org>
2016-02-18 10:46:22 -0800

    Transform JPEGImageEncoder to handle data race problem in jpeg initialization
    There's a data race problem in chromium_jpeg_make_c_derived_tbl (Issue 581542).
    Due to potential complexity of changing an upstream library, we enforce the
    execution of chromium_jpeg_make_c_derived_tbl to happen only on the main thread
    by splitting the JPEGImageEncoder::encode function into two parts: the first
    part deals with initialization of jpeg struct (that wraps the problematic
    chromium_jpeg_make_c_derived_tbl), the second part deals with the rest of
    encoding process. We post the first part as a task on main thread and leaves
    the second part on the Background thread pool.
    
    This transformation of JPEGImageEncoder happens to be part of the efforts of
    another Issue (Issue 571398: progressive image encoding for JPEG).
    
    BUG=581542,571398
    
    Review URL: https://codereview.chromium.org/1684943002
    
    Cr-Commit-Position: refs/heads/master@{#376211}

--- third_party/WebKit/Source/core/html/canvas/CanvasAsyncBlobCreator.h
+++ third_party/WebKit/Source/core/html/canvas/CanvasAsyncBlobCreator.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class PNGImageEncoderState;
+class JPEGImageEncoderState;
 
 class CORE_EXPORT CanvasAsyncBlobCreator
     : public RefCounted<CanvasAsyncBlobCreator> {
@@ -29,7 +30,8 @@ private:
     void scheduleCreateBlobAndCallOnMainThread();
     void scheduleCreateNullptrAndCallOnMainThread();
 
-    OwnPtr<PNGImageEncoderState> m_encoderState;
+    OwnPtr<PNGImageEncoderState> m_pngEncoderState;
+    OwnPtr<JPEGImageEncoderState> m_jpegEncoderState;
     RefPtr<DOMUint8ClampedArray> m_data;
     OwnPtr<Vector<unsigned char>> m_encodedImage;
     int m_numRowsCompleted;
@@ -45,6 +47,8 @@ private:
     void initiatePngEncoding(double deadlineSeconds);
     void scheduleIdleEncodeRowsPng();
     void idleEncodeRowsPng(double deadlineSeconds);
+
+    void initiateJpegEncoding(const double& quality);
 
     void createBlobAndCall();
 
