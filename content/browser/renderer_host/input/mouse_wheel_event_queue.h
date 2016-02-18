15fe783d27348c1718d9d53ac7cefb364b788d8a
dtapuska <dtapuska@chromium.org>
2016-02-18 13:12:25 -0800

    Add scrollDelta units to GestureScrollEnd.
    It is necessary to know if the scroll end gesture is from a
    high precision touchpad or not; add a field.
    
    BUG=568183
    
    Review URL: https://codereview.chromium.org/1710033002
    
    Cr-Commit-Position: refs/heads/master@{#376260}

--- content/browser/renderer_host/input/mouse_wheel_event_queue.h
+++ content/browser/renderer_host/input/mouse_wheel_event_queue.h
@@ -75,7 +75,7 @@ class CONTENT_EXPORT MouseWheelEventQueue {
 
  private:
   void TryForwardNextEventToRenderer();
-  void SendScrollEnd();
+  void SendScrollEnd(blink::WebGestureEvent::ScrollUnits units);
   void SendGesture(const GestureEventWithLatencyInfo& gesture);
 
   MouseWheelEventQueueClient* client_;
