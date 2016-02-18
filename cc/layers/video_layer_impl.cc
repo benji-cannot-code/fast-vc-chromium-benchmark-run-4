92ac9b00b819b45679b080d05a49c2caf98a3f7a
dyen <dyen@chromium.org>
2016-02-17 16:01:31 -0800

    Added methods to generate sync tokens for resource id(s).
    New functions have been added so a sync token can represent
    multiple resources at once. Adding a single sync token at
    the end of generating a multitude of resources can be a win.
    
    The most obvious use cases of this is the video layer which
    generates a resource for each layer. A single sync token is
    now generated at the end of the layer generation.
    
    The layer tree UI resource also generates its own sync token.
    
    BUG=584381
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1707143002
    
    Cr-Commit-Position: refs/heads/master@{#376039}

--- cc/layers/video_layer_impl.cc
+++ cc/layers/video_layer_impl.cc
@@ -125,6 +125,8 @@ bool VideoLayerImpl::WillDraw(DrawMode draw_mode,
 
   DCHECK_EQ(external_resources.mailboxes.size(),
             external_resources.release_callbacks.size());
+  ResourceProvider::ResourceIdArray resource_ids;
+  resource_ids.reserve(external_resources.mailboxes.size());
   for (size_t i = 0; i < external_resources.mailboxes.size(); ++i) {
     unsigned resource_id = resource_provider->CreateResourceFromTextureMailbox(
         external_resources.mailboxes[i],
@@ -134,7 +136,9 @@ bool VideoLayerImpl::WillDraw(DrawMode draw_mode,
     frame_resources_.push_back(FrameResource(
         resource_id, external_resources.mailboxes[i].size_in_pixels(),
         external_resources.mailboxes[i].is_overlay_candidate()));
+    resource_ids.push_back(resource_id);
   }
+  resource_provider->GenerateSyncTokenForResources(resource_ids);
 
   return true;
 }
