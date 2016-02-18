5b4ca598d0296cad45585fa0430251e531deabc3
sdefresne <sdefresne@chromium.org>
2016-02-18 05:46:05 -0800

    Componentize miscellaneous resources to share them with iOS.
    Chrome on iOS uses the resources IDS_OPTIONS_ADVANCED_SECTION_TITLE_PRIVACY
    and IDS_WEBSITE_SETTINGS_NON_SECURE_TRANSPORT so componentize them.
    
    TBR=felt@chromium.org
    BUG=568486
    
    Review URL: https://codereview.chromium.org/1709633002
    
    Cr-Commit-Position: refs/heads/master@{#376160}

--- chrome/browser/ui/website_settings/website_settings_ui.cc
+++ chrome/browser/ui/website_settings/website_settings_ui.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/content_settings/core/browser/plugins_field_trial.h"
+#include "components/strings/grit/components_strings.h"
 #include "grit/theme_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
