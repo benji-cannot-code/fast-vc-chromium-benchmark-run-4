78e901f77f782526899b631d5ea58bd66a08c641
dtseng <dtseng@chromium.org>
2016-02-17 21:21:10 -0800

    Make updates to ChromeVox KB Explorer for Next.
    - kbexplorer script now is blacklisted for classic
    - we now can actually handle the ctrl key in js (stop speech). Do that. Nicely, this let's us get the ctrl key and speak it in the kbexplorer (it probably just stopped speech in the past, so spoke nothing)
    - keys now just go through the background page
    - all keys speak
    
    TEST=ChromeVox+o, k. Press some f-keys (back, forward, etc) are read. Press modifiers ctrl, alt, search, shift; verify they are read (queued). Press letters, numbers, shifted keys.
    BUG=none
    
    Review URL: https://codereview.chromium.org/1702913002
    
    Cr-Commit-Position: refs/heads/master@{#376100}

--- ash/accelerators/accelerator_controller.cc
+++ ash/accelerators/accelerator_controller.cc
@@ -626,17 +626,6 @@ void HandleGetHelp() {
   Shell::GetInstance()->new_window_delegate()->OpenGetHelp();
 }
 
-bool CanHandleSilenceSpokenFeedback() {
-  AccessibilityDelegate* delegate =
-      Shell::GetInstance()->accessibility_delegate();
-  return delegate->IsSpokenFeedbackEnabled();
-}
-
-void HandleSilenceSpokenFeedback() {
-  base::RecordAction(UserMetricsAction("Accel_Silence_Spoken_Feedback"));
-  Shell::GetInstance()->accessibility_delegate()->SilenceSpokenFeedback();
-}
-
 void HandleSwapPrimaryDisplay() {
   base::RecordAction(UserMetricsAction("Accel_Swap_Primary_Display"));
   Shell::GetInstance()->display_configuration_controller()->SetPrimaryDisplayId(
@@ -1052,8 +1041,6 @@ bool AcceleratorController::CanPerformAction(
       return CanHandleDisableCapsLock(previous_accelerator);
     case LOCK_SCREEN:
       return CanHandleLock();
-    case SILENCE_SPOKEN_FEEDBACK:
-      return CanHandleSilenceSpokenFeedback();
     case SWITCH_TO_PREVIOUS_USER:
     case SWITCH_TO_NEXT_USER:
       return CanHandleCycleUser();
@@ -1351,9 +1338,6 @@ void AcceleratorController::PerformAction(AcceleratorAction action,
       // D-BUS), but we consume them to prevent them from getting
       // passed to apps -- see http://crbug.com/146609.
       break;
-    case SILENCE_SPOKEN_FEEDBACK:
-      HandleSilenceSpokenFeedback();
-      break;
     case SWAP_PRIMARY_DISPLAY:
       HandleSwapPrimaryDisplay();
       break;
@@ -1406,11 +1390,6 @@ void AcceleratorController::PerformAction(AcceleratorAction action,
 
 bool AcceleratorController::ShouldActionConsumeKeyEvent(
     AcceleratorAction action) {
-#if defined(OS_CHROMEOS)
-  if (action == SILENCE_SPOKEN_FEEDBACK)
-    return false;
-#endif
-
   // Adding new exceptions is *STRONGLY* discouraged.
   return true;
 }
--- chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
@@ -123,8 +123,8 @@ Background = function() {
 
   cvox.ExtensionBridge.addMessageListener(this.onMessage_);
 
-  document.addEventListener('keydown', this.onKeyDown.bind(this), true);
-  document.addEventListener('keyup', this.onKeyUp.bind(this), true);
+  document.addEventListener('keydown', this.onKeyDown.bind(this), false);
+  document.addEventListener('keyup', this.onKeyUp.bind(this), false);
   cvox.ChromeVoxKbHandler.commandHandler = this.onGotCommand.bind(this);
 
   // Classic keymap.
@@ -519,6 +519,10 @@ Background.prototype = {
       case 'openChromeVoxMenus':
         (new PanelCommand(PanelCommandType.OPEN_MENUS)).send();
         return false;
+      case 'showKbExplorerPage':
+        var explorerPage = {url: 'chromevox/background/kbexplorer.html'};
+        chrome.tabs.create(explorerPage);
+        break;
       case 'decreaseTtsRate':
         this.increaseOrDecreaseSpeechProperty_(cvox.AbstractTts.RATE, false);
         break;
@@ -537,6 +541,10 @@ Background.prototype = {
       case 'increaseTtsVolume':
         this.increaseOrDecreaseSpeechProperty_(cvox.AbstractTts.VOLUME, true);
         break;
+      case 'stopSpeech':
+        cvox.ChromeVox.tts.stop();
+        global.isReadingContinuously = false;
+        return false;
       default:
         return true;
     }
