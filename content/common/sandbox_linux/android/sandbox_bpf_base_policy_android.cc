5da70e6ec2a35bbf8a86b01225478576eddbf974
rsesek <rsesek@chromium.org>
2016-02-17 16:14:29 -0800

    [Android] Permit sched_getparam(), pwrite64(), and getsockopt() under Seccomp.
    BUG=586056
    R=mdempsky@chromium.org
    
    Review URL: https://codereview.chromium.org/1705243002
    
    Cr-Commit-Position: refs/heads/master@{#376045}

--- content/common/sandbox_linux/android/sandbox_bpf_base_policy_android.cc
+++ content/common/sandbox_linux/android/sandbox_bpf_base_policy_android.cc
@@ -87,7 +87,9 @@ ResultExpr SandboxBPFBasePolicyAndroid::EvaluateSyscall(int sysno) const {
 #endif
     case __NR_openat:
     case __NR_pread64:
+    case __NR_pwrite64:
     case __NR_rt_sigtimedwait:
+    case __NR_sched_getparam:
     case __NR_setpriority:
     case __NR_set_tid_address:
     case __NR_sigaltstack:
@@ -104,8 +106,9 @@ ResultExpr SandboxBPFBasePolicyAndroid::EvaluateSyscall(int sysno) const {
     // is demultiplexed below.
 #if defined(__x86_64__) || defined(__arm__) || defined(__aarch64__) || \
       defined(__mips__)
-    case __NR_socket:
+    case __NR_getsockopt:
     case __NR_connect:
+    case __NR_socket:
 #endif
 
     // Ptrace is allowed so the Breakpad Microdumper can fork in a renderer
@@ -134,6 +137,7 @@ ResultExpr SandboxBPFBasePolicyAndroid::EvaluateSyscall(int sysno) const {
            .ElseIf(AllOf(socketcall == SYS_SOCKET,
                          RestrictSocketArguments(domain, type, protocol)),
                    Allow())
+           .ElseIf(socketcall == SYS_GETSOCKOPT, Allow())
            .Else(Error(EPERM));
   }
 #endif
