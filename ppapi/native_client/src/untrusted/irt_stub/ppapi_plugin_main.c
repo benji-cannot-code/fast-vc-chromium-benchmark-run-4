3e6befdb3143536319316d16b1baa5a05f61cb2b
sbc <sbc@chromium.org>
2016-02-17 19:28:23 -0800

    Reland of [NaCl SDK] Add initial support for building the SDK with gn (patchset #1 id:1 of https://codereview.chromium.org/1693213002/ )
    Reason for revert:
    clang/win compile failures was fixed:
    https://codereview.chromium.org/1706613002
    
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_clang_x64_dbg
    
    Original issue's description:
    > Revert of [NaCl SDK] Add initial support for building the SDK with gn (patchset #6 id:100001 of https://codereview.chromium.org/1693053002/ )
    >
    > Reason for revert:
    > Doesn't build with clang/win and looks nontrivial to fix:
    >
    > https://build.chromium.org/p/chromium.fyi/builders/CrWinClang64%28dbg%29/builds/3655/steps/compile/logs/stdio
    >
    > FAILED: ninja -t msvc -e environment.x64 -- E:\b\build\goma/gomacc.exe ../../third_party/llvm-build/Release+Asserts/bin/clang-cl.exe /nologo /showIncludes /FC @obj/native_client/src/trusted/validator/driver/ncval_new/ncval.obj.rsp /c ../../native_client/src/trusted/validator/driver/ncval.cc /Foobj/native_client/src/trusted/validator/driver/ncval_new/ncval.obj /Fdobj/native_client/src/trusted/validator/driver/ncval_new_cc.pdb
    > ../../native_client/src/trusted/validator/driver/ncval.cc(359,36) :  error: ISO C++11 does not allow conversion from string literal to 'char *' [-Werror,-Wwritable-strings]
    >   while ((opt = getopt(argc, argv, "vd")) != -1) {
    >                                    ^
    >
    > (and i'm guessing several more)
    >
    > Original issue's description:
    > > [NaCl SDK] Add initial support for building the SDK with gn
    > >
    > > build_sdk.py still uses gyp.  I have a followup CL to
    > > convert that to gn.
    > >
    > > BUG=585999
    >
    > TBR=binji@chromium.org,dpranke@chromium.org,bradnelson@chromium.org,sbc@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=585999
    >
    > Committed: https://crrev.com/b78379a22336ce66dac20778db7964e40faa9180
    > Cr-Commit-Position: refs/heads/master@{#375349}
    
    TBR=binji@chromium.org,dpranke@chromium.org,bradnelson@chromium.org,thakis@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=585999
    
    Review URL: https://codereview.chromium.org/1705923002
    
    Cr-Commit-Position: refs/heads/master@{#376080}

--- ppapi/native_client/src/untrusted/irt_stub/ppapi_plugin_main.c
+++ ppapi/native_client/src/untrusted/irt_stub/ppapi_plugin_main.c
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
+#include "ppapi/native_client/src/shared/ppapi_proxy/ppruntime.h"
+
 #include "ppapi/native_client/src/untrusted/irt_stub/ppapi_start.h"
 
 /*
