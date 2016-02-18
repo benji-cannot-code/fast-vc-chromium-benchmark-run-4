7996694b2442bcb8223c0a3f00b62175103bd539
hayato <hayato@chromium.org>
2016-02-17 20:25:08 -0800

    Support slotchange events
    This is a proof of concept to support *slotchange* events at the end of a microtask.
    
    The spec discussion is here: https://github.com/w3c/webcomponents/issues/288
    
    Because slot elements are guarded by ShadowDOMV1 flag, this new events will be seen only when the flag is enabled.
    
    BUG=531990
    
    Review URL: https://codereview.chromium.org/1695163003
    
    Cr-Commit-Position: refs/heads/master@{#376090}

--- third_party/WebKit/Source/core/dom/Node.cpp
+++ third_party/WebKit/Source/core/dom/Node.cpp
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/ElementTraversal.h"
 #include "core/dom/ExceptionCode.h"
 #include "core/dom/LayoutTreeBuilderTraversal.h"
+#include "core/dom/Microtask.h"
 #include "core/dom/NodeRareData.h"
 #include "core/dom/NodeTraversal.h"
 #include "core/dom/ProcessingInstruction.h"
@@ -691,6 +692,11 @@ void Node::markAncestorsWithChildNeedsStyleInvalidation()
 void Node::markAncestorsWithChildNeedsDistributionRecalc()
 {
     ScriptForbiddenScope forbidScriptDuringRawIteration;
+    if (inDocument() && !document().childNeedsDistributionRecalc()) {
+        // TODO(hayato): Support a non-document composed tree.
+        // TODO(hayato): Enqueue a task only if a 'slotchange' event listner is registered in the document composed tree.
+        Microtask::enqueueMicrotask(WTF::bind(&Document::updateDistribution, PassRefPtrWillBeRawPtr<Document>(&document())));
+    }
     for (Node* node = this; node && !node->childNeedsDistributionRecalc(); node = node->parentOrShadowHostNode())
         node->setChildNeedsDistributionRecalc();
     document().scheduleLayoutTreeUpdateIfNeeded();
