205094538d29dbba18d10f7723cd7fe10b2c49c7
junov <junov@chromium.org>
2016-02-18 07:12:06 -0800

    Fixing a rare crash in 2D canvas text drawing with font in invalid state
    Bug repro is unknown. Fix based solely on crash report data.
    
    BUG=579846
    R=senorblanco@chromium.org
    
    Review URL: https://codereview.chromium.org/1706133002
    
    Cr-Commit-Position: refs/heads/master@{#376166}

--- third_party/WebKit/Source/modules/canvas2d/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/Source/modules/canvas2d/CanvasRenderingContext2D.cpp
@@ -1986,6 +1986,9 @@ void CanvasRenderingContext2D::drawTextInternal(const String& text, double x, do
         canvas()->disableDeferral(DisableDeferralReasonSubPixelTextAntiAliasingSupport);
 
     const Font& font = accessFont();
+    if (!font.primaryFont())
+        return;
+
     const FontMetrics& fontMetrics = font.fontMetrics();
 
     // FIXME: Need to turn off font smoothing.
