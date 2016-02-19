3553d8d9c053ae38acb7f6d9761d9ce1e2f99231
falken <falken@chromium.org>
2016-02-18 21:06:08 -0800

    Reland: service worker: use 200 OK for update requests even in the no update case
    The main motivation is to stop network failure error spew in DevTools console.
    This patch reinstates the "pause on download" functionality we once had.
    
    Relanding: forgot to add MAYBE_ for bug 586897.
    
    BUG=541797
    TBR=nhiroki,tkent,tsepez
    
    Review URL: https://codereview.chromium.org/1713863002
    
    Cr-Commit-Position: refs/heads/master@{#376390}

--- third_party/WebKit/public/web/WebEmbeddedWorkerStartData.h
+++ third_party/WebKit/public/web/WebEmbeddedWorkerStartData.h
@@ -40,6 +40,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 struct WebEmbeddedWorkerStartData {
+    enum PauseAfterDownloadMode {
+        DontPauseAfterDownload,
+        PauseAfterDownload,
+    };
     enum WaitForDebuggerMode {
         DontWaitForDebugger,
         WaitForDebugger
@@ -47,12 +51,16 @@ struct WebEmbeddedWorkerStartData {
 
     WebURL scriptURL;
     WebString userAgent;
+    PauseAfterDownloadMode pauseAfterDownloadMode;
     WaitForDebuggerMode waitForDebuggerMode;
     WebSettings::V8CacheOptions v8CacheOptions;
 
     WebEmbeddedWorkerStartData()
-        : waitForDebuggerMode(DontWaitForDebugger)
-        , v8CacheOptions(WebSettings::V8CacheOptionsDefault) { }
+        : pauseAfterDownloadMode(DontPauseAfterDownload)
+        , waitForDebuggerMode(DontWaitForDebugger)
+        , v8CacheOptions(WebSettings::V8CacheOptionsDefault)
+    {
+    }
 };
 
 } // namespace blink
