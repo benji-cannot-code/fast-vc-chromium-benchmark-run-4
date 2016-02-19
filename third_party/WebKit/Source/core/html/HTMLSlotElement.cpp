fb48f47b2c1d15d2762b40e4960180d19cec464d
rune <rune@opera.com>
2016-02-18 22:29:00 -0800

    Propagate inherited properties into slotted elements on recalc.
    When we reach a <slot> element during recalc, we need to trigger recalc
    of elements distributed to that slot for inheritance. This is similar
    to what is done for InsertionPoint. I have however not found a case
    where we need a bigger cannon than LocalStyleChange on the distributed
    elements.
    
    Made a few override methods final, expecting no inheritance of
    HTMLSlotElement.
    
    R=kochi@chromium.org,hayato@chromium.org
    BUG=587797
    
    Review URL: https://codereview.chromium.org/1708213002
    
    Cr-Commit-Position: refs/heads/master@{#376406}

--- third_party/WebKit/Source/core/html/HTMLSlotElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLSlotElement.cpp
@@ -46,6 +46,7 @@ using namespace HTMLNames;
 inline HTMLSlotElement::HTMLSlotElement(Document& document)
     : HTMLElement(slotTag, document)
 {
+    setHasCustomStyleCallbacks();
 }
 
 DEFINE_NODE_FACTORY(HTMLSlotElement);
@@ -213,6 +214,15 @@ void HTMLSlotElement::removedFrom(ContainerNode* insertionPoint)
         root->didRemoveSlot();
 
     HTMLElement::removedFrom(insertionPoint);
+}
+
+void HTMLSlotElement::willRecalcStyle(StyleRecalcChange change)
+{
+    if (change < Inherit && styleChangeType() < SubtreeStyleChange)
+        return;
+
+    for (Node* node : m_distributedNodes)
+        node->setNeedsStyleRecalc(LocalStyleChange, StyleChangeReasonForTracing::create(StyleChangeReason::PropagateInheritChangeToDistributedNodes));
 }
 
 void HTMLSlotElement::updateDistributedNodesWithFallback()
