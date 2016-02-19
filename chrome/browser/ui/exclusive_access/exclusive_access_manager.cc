9011c07bfdad2119e8fb9c79653be6e89707226b
mgiuca <mgiuca@chromium.org>
2016-02-18 19:11:29 -0800

    Fullscreen: Mouse events now count as input for activating the bubble.
    Only matters if the simplified-fullscreen-ui flag is enabled.
    
    After 15 minutes of no user input, the fullscreen / pointer lock bubble
    is re-shown on the next user input. Previously, this only considered
    mouse movement and key presses. Now all mouse events are also included
    for both extending the 15-minute timer, and re-showing the bubble after
    the timer has elapsed.
    
    Importantly, this works while mouselock is active (otherwise, you would
    not be reminded that you are in fullscreen if it also locks the mouse).
    The way mouse events are propagated from the RenderWidgetHostViewAura to
    the browser was changed to support this.
    
    Re-land (CL was committed in r369300, reverted in r369549).
    
    BUG=352425, 577983
    
    Review URL: https://codereview.chromium.org/1561963003
    
    Cr-Commit-Position: refs/heads/master@{#376357}

--- chrome/browser/ui/exclusive_access/exclusive_access_manager.cc
+++ chrome/browser/ui/exclusive_access/exclusive_access_manager.cc
@@ -122,7 +122,7 @@ void ExclusiveAccessManager::OnTabClosing(WebContents* web_contents) {
 bool ExclusiveAccessManager::HandleUserKeyPress(
     const content::NativeWebKeyboardEvent& event) {
   if (event.windowsKeyCode != ui::VKEY_ESCAPE) {
-    exclusive_access_context_->OnExclusiveAccessUserInput();
+    OnUserInput();
     return false;
   }
 
@@ -130,6 +130,10 @@ bool ExclusiveAccessManager::HandleUserKeyPress(
   handled = fullscreen_controller_.HandleUserPressedEscape();
   handled |= mouse_lock_controller_.HandleUserPressedEscape();
   return handled;
+}
+
+void ExclusiveAccessManager::OnUserInput() {
+  exclusive_access_context_->OnExclusiveAccessUserInput();
 }
 
 void ExclusiveAccessManager::OnAcceptExclusiveAccessPermission() {
