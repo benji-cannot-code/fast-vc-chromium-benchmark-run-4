1daf33a5cdc8d3d21650e3cc6298cfe454d21656
atwilson <atwilson@chromium.org>
2016-02-19 09:49:47 -0800

    No longer start up profile if there was an error fetching policy.
    Changed UserCloudPolicyManagerChromeOS to no longer complete profile
    initialization if there is an error fetching policy. Instead, we shutdown the
    user and force a policy load the next time we try to startup that user.
    
    BUG=532317
    TBR=vitalybuka
    
    Review URL: https://codereview.chromium.org/1608533002
    
    Cr-Commit-Position: refs/heads/master@{#376484}

--- chrome/browser/chromeos/login/users/avatar/user_image_manager_browsertest.cc
+++ chrome/browser/chromeos/login/users/avatar/user_image_manager_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -45,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chromeos/chromeos_paths.h"
+#include "chromeos/chromeos_switches.h"
 #include "chromeos/dbus/cryptohome_client.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/fake_session_manager_client.h"
@@ -116,6 +118,16 @@ class UserImageManagerTest : public LoginManagerTest,
 
     ASSERT_TRUE(PathService::Get(chrome::DIR_TEST_DATA, &test_data_dir_));
     ASSERT_TRUE(PathService::Get(chrome::DIR_USER_DATA, &user_data_dir_));
+  }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    LoginManagerTest::SetUpCommandLine(command_line);
+    // These tests create new users and then inject policy after the fact,
+    // to avoid having to set up a mock policy server. UserCloudPolicyManager
+    // will shut down the profile if there's an error loading the initial
+    // policy, so disable this behavior so we can inject policy directly.
+    command_line->AppendSwitch(
+        chromeos::switches::kAllowFailedPolicyFetchForTest);
   }
 
   void SetUpOnMainThread() override {
