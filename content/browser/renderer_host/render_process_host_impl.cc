70de9c14d7e5ef70f54b9ba480a3b0fcb6c45254
dtapuska <dtapuska@chromium.org>
2016-02-19 14:27:40 -0800

    Follow on for gesture events generated by wheel events.
    Provide mechanism to run with wheel events disabled.
    Implement overscroll handling for wheel event generation.
    Fix input router unit test when wheel events are generated.
    
    BUG=568183
    
    Review URL: https://codereview.chromium.org/1705323002
    
    Cr-Commit-Position: refs/heads/master@{#376552}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1427,6 +1427,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDisableTouchAdjustment,
     switches::kDisableTouchDragDrop,
     switches::kDisableV8IdleTasks,
+    switches::kDisableWheelGestures,
     switches::kDomAutomationController,
     switches::kEnableBlinkFeatures,
     switches::kEnableBrowserSideNavigation,
