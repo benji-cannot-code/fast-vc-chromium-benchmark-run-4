9467cd41ad027a2567b897aad14baef4ed2e70e8
eugenebut <eugenebut@chromium.org>
2016-02-19 10:56:32 -0800

    [ios] Removed extra include from crw_web_controller_unittest.
    BUG=579697
    
    Review URL: https://codereview.chromium.org/1716843002
    
    Cr-Commit-Position: refs/heads/master@{#376504}

--- ios/web/web_state/ui/crw_web_controller_unittest.mm
+++ ios/web/web_state/ui/crw_web_controller_unittest.mm
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/test/wk_web_view_crash_utils.h"
 #include "ios/web/web_state/blocked_popup_info.h"
 #import "ios/web/web_state/js/crw_js_invoke_parameter_queue.h"
-#import "ios/web/web_state/ui/crw_ui_web_view_web_controller.h"
 #import "ios/web/web_state/ui/crw_web_controller+protected.h"
 #import "ios/web/web_state/ui/crw_web_controller_container_view.h"
 #import "ios/web/web_state/web_state_impl.h"
