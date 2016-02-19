c8d29f53f2339354d1d7b531df0b5d6c447cb211
xdai <xdai@chromium.org>
2016-02-18 22:30:42 -0800

    Don't use the saved screen preference for Slave devices during OOBE.
    BUG=564370
    
    Review URL: https://codereview.chromium.org/1708943002
    
    Cr-Commit-Position: refs/heads/master@{#376407}

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -305,7 +305,8 @@ void WizardController::Init(const std::string& first_screen_name) {
   // Use the saved screen preference from Local State.
   const std::string screen_pref =
       GetLocalState()->GetString(prefs::kOobeScreenPending);
-  if (is_out_of_box_ && !screen_pref.empty() && !IsHostPairingOobe() &&
+  if (is_out_of_box_ && !screen_pref.empty() && !IsRemoraPairingOobe() &&
+      !IsBootstrappingSlave() &&
       (first_screen_name.empty() ||
        first_screen_name == WizardController::kTestNoScreenName)) {
     first_screen_name_ = screen_pref;
@@ -951,7 +952,7 @@ void WizardController::AdvanceToScreen(const std::string& screen_name) {
   } else if (screen_name != kTestNoScreenName) {
     if (is_out_of_box_) {
       time_oobe_started_ = base::Time::Now();
-      if (IsHostPairingOobe()) {
+      if (IsRemoraPairingOobe() || IsSlavePairingOobe()) {
         ShowHostPairingScreen();
       } else if (CanShowHIDDetectionScreen()) {
         hid_screen_ = GetScreen(kHIDDetectionScreenName);
@@ -1327,11 +1328,15 @@ bool WizardController::SetOnTimeZoneResolvedForTesting(
   return true;
 }
 
-bool WizardController::IsHostPairingOobe() const {
-  return (IsRemoraRequisition() || IsBootstrappingSlave()) &&
+bool WizardController::IsRemoraPairingOobe() const {
+  return IsRemoraRequisition() &&
          (base::CommandLine::ForCurrentProcess()->HasSwitch(
               switches::kHostPairingOobe) ||
           shark_controller_detected_);
+}
+
+bool WizardController::IsSlavePairingOobe() const {
+  return IsBootstrappingSlave() && shark_controller_detected_;
 }
 
 void WizardController::MaybeStartListeningForSharkConnection() {
@@ -1339,7 +1344,7 @@ void WizardController::MaybeStartListeningForSharkConnection() {
     return;
 
   // We shouldn't be here if we are running pairing OOBE already.
-  DCHECK(!IsHostPairingOobe());
+  DCHECK(!IsRemoraPairingOobe() && !IsSlavePairingOobe());
 
   if (!shark_connection_listener_) {
     shark_connection_listener_.reset(
