a376314055c069f561a9d82f75be83c89b569d68
knn <knn@chromium.org>
2016-02-19 11:06:27 -0800

    Ignore incorrectly persisted account ids without an email mapping.
    Current prognosis is this has persisted incorrectly from before account email
    to id migration.
    Hence we ignore the java side notification for account removal while ATS
    purges the account during migration.
    
    Also added 2 histograms to verify this prognosis. Added in the OAuth2Login namespace to minimize pollution due to debug histograms.
    
    BUG=582305
    
    Review URL: https://codereview.chromium.org/1706293002
    
    Cr-Commit-Position: refs/heads/master@{#376505}

--- components/signin/core/browser/account_tracker_service.h
+++ components/signin/core/browser/account_tracker_service.h
@@ -68,7 +68,9 @@ class AccountTrackerService : public KeyedService,
   enum AccountIdMigrationState {
     MIGRATION_NOT_STARTED,
     MIGRATION_IN_PROGRESS,
-    MIGRATION_DONE
+    MIGRATION_DONE,
+    // Keep in sync with OAuth2LoginAccountRevokedMigrationState histogram enum.
+    NUM_MIGRATION_STATES
   };
 
   AccountTrackerService();
