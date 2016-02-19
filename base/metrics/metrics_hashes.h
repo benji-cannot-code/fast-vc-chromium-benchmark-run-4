65e0dbf64ac7410c9c7802e535ae2dbfc9f5ae6d
bcwhite <bcwhite@chromium.org>
2016-02-19 09:12:37 -0800

    Use base::StringPiece where reasonable in metrics code.
    BUG=
    
    Review URL: https://codereview.chromium.org/1701403003
    
    Cr-Commit-Position: refs/heads/master@{#376470}

--- base/metrics/metrics_hashes.h
+++ base/metrics/metrics_hashes.h
@@ -7,15 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_METRICS_METRICS_HASHES_H_
 
 #include <stdint.h>
-#include <string>
 
 #include "base/base_export.h"
+#include "base/strings/string_piece.h"
 
 namespace base {
 
 // Computes a uint64_t hash of a given string based on its MD5 hash. Suitable
 // for metric names.
-BASE_EXPORT uint64_t HashMetricName(const std::string& name);
+BASE_EXPORT uint64_t HashMetricName(base::StringPiece name);
 
 }  // namespace metrics
 
