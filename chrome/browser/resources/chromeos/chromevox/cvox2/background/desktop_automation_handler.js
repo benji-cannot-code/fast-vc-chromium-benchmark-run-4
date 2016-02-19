f8abcf5b1c341af804c6fa0fb55a31153cb36d9a
dtseng <dtseng@chromium.org>
2016-02-18 16:24:11 -0800

    Support content editable.
    It turns out that content editable nodes contain all the info we need to compute diffs and read the text moved/change events (e.g. lineBreaks, textSelStart, textSelEnd).
    
    Flipping on this bit gives us most of what ChromeVox Classic reports. In addition, it is fair more performant and reliable.
    
    Unfortunately, on some complex examples, arrowing through text doesn't trigger all of the expected updates to textSelStart and textSelEnd that one would expect. This should be fixed in the underlying layers.
    
    Finally, flushing the next speech utterance should ensure that some text was actually spoken. If not, wait for the next actual spoken utterance to no longer flush. This was observed when tabbing among editable controls. It results from a caller constructing an Output object and calling go() with no speech actually buffered. This resulted in tabbing queueing up focus announcements.
    
    TEST=compose mail in Gmail's composition content editable. Reply to a complex html email. Open a Google Doc in braille mode.
    
    Review URL: https://codereview.chromium.org/1694243004
    
    Cr-Commit-Position: refs/heads/master@{#376300}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/desktop_automation_handler.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/desktop_automation_handler.js
@@ -141,8 +141,7 @@ DesktopAutomationHandler.prototype = {
     if (node.role == RoleType.embeddedObject || node.role == RoleType.client)
       return;
 
-    if (this.isEditable_(node))
-      this.createTextEditHandlerIfNeeded_(evt.target);
+    this.createTextEditHandlerIfNeeded_(evt.target);
 
     // Since we queue output mostly for live regions support and there isn't a
     // reliable way to know if this focus event resulted from a user's explicit
@@ -181,13 +180,13 @@ DesktopAutomationHandler.prototype = {
 
   /** @override */
   onTextChanged: function(evt) {
-    if (this.isEditable_(evt.target))
+    if (evt.target.state.editable)
       this.onEditableChanged_(evt);
   },
 
   /** @override */
   onTextSelectionChanged: function(evt) {
-    if (this.isEditable_(evt.target))
+    if (evt.target.state.editable)
       this.onEditableChanged_(evt);
   },
 
@@ -224,7 +223,7 @@ DesktopAutomationHandler.prototype = {
    */
   onValueChanged: function(evt) {
     // Delegate to the edit text handler if this is an editable.
-    if (this.isEditable_(evt.target)) {
+    if (evt.target.state.editable) {
       this.onEditableChanged_(evt);
       return;
     }
@@ -268,18 +267,6 @@ DesktopAutomationHandler.prototype = {
         this.textEditHandler_.node !== node) {
       this.textEditHandler_ = editing.TextEditHandler.createForNode(node);
     }
-  },
-
-  /**
-   * Returns true if |node| is editable.
-   * @param {AutomationNode} node
-   * @return {boolean}
-   * @private
-   */
-  isEditable_: function(node) {
-    // Remove the check for role after m47 whereafter the editable state can be
-    // used to know when to create an editable text handler.
-    return node.role == RoleType.textField || node.state.editable;
   }
 };
 
