444b411d76f41e896f8965ae882de43198f32661
bzanotti <bzanotti@chromium.org>
2016-02-19 03:19:21 -0800

    Release unused web views when under memory pressure.
    AccountConsistencyService and GaiaAuthFetcherIOS both use web views
    internally to manipulate cookies. Once allocated, those web views were
    never released to avoid having to re-allocate them later (which is
    expensive).
    
    Those web views are now released when they are unused and the app is
    under memory pressure.
    
    BUG=587487
    
    Review URL: https://codereview.chromium.org/1708643002
    
    Cr-Commit-Position: refs/heads/master@{#376431}

--- ios/chrome/browser/signin/gaia_auth_fetcher_ios.mm
+++ ios/chrome/browser/signin/gaia_auth_fetcher_ios.mm
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <WebKit/WebKit.h>
 
+#include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/json/string_escape.h"
 #include "base/logging.h"
 #import "base/mac/foundation_util.h"
@@ -211,6 +213,8 @@ GaiaAuthFetcherIOSBridge::GaiaAuthFetcherIOSBridge(
     web::BrowserState* browser_state)
     : browser_state_(browser_state), fetcher_(fetcher), request_() {
   web::BrowserState::GetActiveStateManager(browser_state_)->AddObserver(this);
+  memory_pressure_listener_.reset(new base::MemoryPressureListener(base::Bind(
+      &GaiaAuthFetcherIOSBridge::OnMemoryPressure, base::Unretained(this))));
 }
 
 GaiaAuthFetcherIOSBridge::~GaiaAuthFetcherIOSBridge() {
@@ -310,6 +314,16 @@ void GaiaAuthFetcherIOSBridge::OnInactive() {
   // |browser_state_| is now inactive. Stop using |web_view_| and don't create
   // a new one until it is active.
   ResetWKWebView();
+}
+
+void GaiaAuthFetcherIOSBridge::OnMemoryPressure(
+    base::MemoryPressureListener::MemoryPressureLevel level) {
+  bool is_memory_pressure_critical =
+      (level == base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_CRITICAL);
+  if (is_memory_pressure_critical && !request_.pending) {
+    // |web_view_| isn't being used, release it.
+    ResetWKWebView();
+  }
 }
 
 #pragma mark - GaiaAuthFetcherIOS definition
--- ios/chrome/browser/signin/gaia_auth_fetcher_ios_private.h
+++ ios/chrome/browser/signin/gaia_auth_fetcher_ios_private.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/mac/scoped_nsobject.h"
 #include "base/macros.h"
+#include "base/memory/memory_pressure_listener.h"
 #include "ios/web/public/active_state_manager.h"
 
 class GaiaAuthFetcherIOS;
@@ -93,6 +94,10 @@ class GaiaAuthFetcherIOSBridge : web::ActiveStateManager::Observer {
   void OnActive() override;
   void OnInactive() override;
 
+  // Memory pressure callback.
+  void OnMemoryPressure(
+      base::MemoryPressureListener::MemoryPressureLevel level);
+
   // Browser state associated with the bridge, used to create WKWebViews.
   web::BrowserState* browser_state_;
   // Fetcher owning this bridge.
@@ -104,6 +109,9 @@ class GaiaAuthFetcherIOSBridge : web::ActiveStateManager::Observer {
   base::scoped_nsobject<GaiaAuthFetcherNavigationDelegate> navigation_delegate_;
   // Web view used to do the network requests.
   base::scoped_nsobject<WKWebView> web_view_;
+
+  // Listens to memory pressure notifications.
+  scoped_ptr<base::MemoryPressureListener> memory_pressure_listener_;
 
   DISALLOW_COPY_AND_ASSIGN(GaiaAuthFetcherIOSBridge);
 };
