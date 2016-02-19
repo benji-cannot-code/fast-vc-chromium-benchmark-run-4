30a7b2ca277020086bfa27cdbc7b07bfc6a4e92a
dpapad <dpapad@chromium.org>
2016-02-18 19:38:23 -0800

    MD Settings: Manage search engines 3/3, hooking up UI.
    Specifically,
     - Migrating UI from chrome.searchEnginesPrivate API to use C++ handlers.
     - Implementing editing of existing search engine entries (and renamed
       add_search_engine_dialog to search_engine_dialog).
     - Hooking up validiation when editing/adding a search engine.
     - Some UI polish (show search engine icons when available, adjust column widths).
     - Adding lot of tests.
    
    BUG=479359
    
    Review URL: https://codereview.chromium.org/1666623006
    
    Cr-Commit-Position: refs/heads/master@{#376362}

--- chrome/browser/resources/settings/search_engines_page/search_engines_page.js
+++ chrome/browser/resources/settings/search_engines_page/search_engines_page.js
@@ -7,14 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview 'settings-search-engines-page' is the settings page
  * containing search engines settings.
  *
- * Example:
- *
- *    <core-animated-pages>
- *      <settings-search-engines-page prefs="{{prefs}}">
- *      </settings-search-engines-page>
- *      ... other pages ...
- *    </core-animated-pages>
- *
  * @group Chrome Settings Elements
  * @element settings-search-engines-page
  */
@@ -34,47 +26,62 @@ Polymer({
       value: function() { return []; }
     },
 
-    /** @type {boolean} */
-    showAddSearchEngineDialog_: {
+    /** @private {boolean} */
+    showAddSearchEngineDialog_: Boolean,
+
+    /** @private {boolean} */
+    otherSearchEnginesExpanded_: {
       type: Boolean,
-      value: false,
+      value: true,
     },
   },
 
+  /**
+   * Holds WebUI listeners that need to be removed when this element is
+   * destroyed.
+   * TODO(dpapad): Move listener tracking logic to a Polymer behavior class,
+   * such that it can be re-used.
+   * @private {!Array<!WebUIListener>}
+   */
+  webUIListeners_: [],
+
   /** @override */
   ready: function() {
-    chrome.searchEnginesPrivate.onSearchEnginesChanged.addListener(
-        this.enginesChanged_.bind(this));
-    this.enginesChanged_();
+    settings.SearchEnginesBrowserProxyImpl.getInstance().
+        getSearchEnginesList().then(this.enginesChanged_.bind(this));
+    this.webUIListeners_.push(cr.addWebUIListener(
+        'search-engines-changed', this.enginesChanged_.bind(this)));
   },
 
-  /** @private */
-  enginesChanged_: function() {
-    chrome.searchEnginesPrivate.getSearchEngines(function(engines) {
-      this.defaultEngines = engines.filter(function(engine) {
-        return engine.type ==
-            chrome.searchEnginesPrivate.SearchEngineType.DEFAULT;
-      }, this);
+  /** @override */
+  detached: function() {
+    this.webUIListeners_.forEach(function(listener) {
+      cr.removeWebUIListener(listener);
+    });
+  },
 
-      this.otherEngines = engines.filter(function(engine) {
-        return engine.type ==
-            chrome.searchEnginesPrivate.SearchEngineType.OTHER;
-      }, this);
-    }.bind(this));
+  /**
+   * @param {!SearchEnginesInfo} searchEnginesInfo
+   * @private
+   */
+  enginesChanged_: function(searchEnginesInfo) {
+    this.defaultEngines = searchEnginesInfo['defaults'];
+    this.otherEngines = searchEnginesInfo['others'];
+    // TODO(dpapad): Use searchEnginesInfo['extensions'] once UI mocks are
+    // provided.
   },
 
   /** @private */
   onAddSearchEngineTap_: function() {
     this.showAddSearchEngineDialog_ = true;
     this.async(function() {
-      var dialog = this.$$('settings-add-search-engine-dialog');
+      var dialog = this.$$('settings-search-engine-dialog');
       // Register listener to detect when the dialog is closed. Flip the boolean
       // once closed to force a restamp next time it is shown such that the
       // previous dialog's contents are cleared.
       dialog.addEventListener('iron-overlay-closed', function() {
         this.showAddSearchEngineDialog_ = false;
       }.bind(this));
-      dialog.open();
     }.bind(this));
   },
 });
