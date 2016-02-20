c4448ca108062949d51b87e091da11bcd56ac90a
tfarina <tfarina@chromium.org>
2016-02-20 07:44:56 -0800

    net: share SetDisableNagle() function with acceptor_thread.cc
    HandleConnection in acceptor_thread.cc had the same implementation of
    SetDisableNagle() that was in tcp_socket_util.cc. This patch exposes it
    in tcp_socket_util.h header, so it can be reused by acceptor_thread.
    
    BUG=None
    TEST=net_unittests
    R=eroman@chromium.org
    
    Review URL: https://codereview.chromium.org/1717643003
    
    Cr-Commit-Position: refs/heads/master@{#376649}

--- net/tools/flip_server/acceptor_thread.cc
+++ net/tools/flip_server/acceptor_thread.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <errno.h>
 #include <netinet/in.h>
-#include <netinet/tcp.h>  // For TCP_NODELAY
 #include <string.h>       // For strerror
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -18,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/tools/flip_server/flip_config.h"
 #include "net/tools/flip_server/sm_connection.h"
 #include "net/tools/flip_server/spdy_ssl.h"
+#include "net/tools/flip_server/tcp_socket_util.h"
 #include "openssl/err.h"
 #include "openssl/ssl.h"
 
@@ -83,17 +83,8 @@ void SMAcceptorThread::InitWorker() {
 
 void SMAcceptorThread::HandleConnection(int server_fd,
                                         struct sockaddr_in* remote_addr) {
-  int on = 1;
-  int rc;
   if (acceptor_->disable_nagle_) {
-    rc = setsockopt(server_fd,
-                    IPPROTO_TCP,
-                    TCP_NODELAY,
-                    reinterpret_cast<char*>(&on),
-                    sizeof(on));
-    if (rc < 0) {
-      close(server_fd);
-      LOG(ERROR) << "setsockopt() failed fd=" << server_fd;
+    if (!SetDisableNagle(server_fd)) {
       return;
     }
   }
