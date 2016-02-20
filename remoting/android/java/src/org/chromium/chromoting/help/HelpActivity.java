9eb1e7c6410c89365dab27b5ed84f5cf20d0ee47
lambroslambrou <lambroslambrou@chromium.org>
2016-02-19 17:38:44 -0800

    Android Chromoting: Pull Credits page out to a separate Activity.
    This makes the Credits page separate from the main Help screen, so that
    it can interoperate more easily with the Google-internal version of the
    Help system.
    
    BUG=548929
    
    Review URL: https://codereview.chromium.org/1672213003
    
    Cr-Commit-Position: refs/heads/master@{#376595}

--- remoting/android/java/src/org/chromium/chromoting/help/HelpActivity.java
+++ remoting/android/java/src/org/chromium/chromoting/help/HelpActivity.java
@@ -39,7 +39,6 @@ public class HelpActivity extends AppCompatActivity {
     private static final String TAG = "Chromoting";
 
     private static final String PLAY_STORE_URL = "market://details?id=";
-    private static final String CREDITS_URL = "file:///android_res/raw/credits.html";
 
     private static final String FEEDBACK_PACKAGE = "com.google.android.gms";
 
@@ -58,7 +57,7 @@ public class HelpActivity extends AppCompatActivity {
      */
     private static Bitmap sScreenshot;
 
-    /** WebView used to display help content and credits. */
+    /** WebView used to display help content. */
     private WebView mWebView;
 
     /** Constant used to send the feedback parcel to the system feedback service. */
@@ -174,7 +173,7 @@ public class HelpActivity extends AppCompatActivity {
             return true;
         }
         if (id == R.id.actionbar_credits) {
-            mWebView.loadUrl(CREDITS_URL);
+            startActivity(new Intent(this, CreditsActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
