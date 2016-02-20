36cd175231f92c2b4e11a5d662bc8340d595670d
ben <ben@chromium.org>
2016-02-19 19:59:58 -0800

    Remove standalone android shell, as it has bitrotted and is unused
    BUG=
    
    Review URL: https://codereview.chromium.org/1714593002
    
    Cr-Commit-Position: refs/heads/master@{#376623}

--- mojo/shell/standalone/desktop/launcher_process.cc
+++ mojo/shell/standalone/desktop/launcher_process.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 #include "base/threading/platform_thread.h"
 #include "mojo/shell/standalone/context.h"
-#include "mojo/shell/standalone/switches.h"
 #include "mojo/shell/switches.h"
 
 namespace mojo {
