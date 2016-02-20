5fe529f76ebb1f448f096c0fece0ec5153e38451
leviw <leviw@chromium.org>
2016-02-19 17:44:55 -0800

    Remove unused wtf/deque include in StyleSheetContents
    TBR=szager
    
    Review URL: https://codereview.chromium.org/1717883002
    
    Cr-Commit-Position: refs/heads/master@{#376602}

--- third_party/WebKit/Source/core/css/StyleSheetContents.cpp
+++ third_party/WebKit/Source/core/css/StyleSheetContents.cpp
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/InspectorTraceEvents.h"
 #include "platform/TraceEvent.h"
 #include "platform/weborigin/SecurityOrigin.h"
-#include "wtf/Deque.h"
 
 namespace blink {
 
