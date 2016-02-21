9616a2952cbf97ce0a993c7b8bb52c0c692cf775
sammc <sammc@chromium.org>
2016-02-21 15:30:19 -0800

    Fix some issues with ServiceRegistryJsWrapper service overrides.
    Currently, ServiceRegistryJsWrapper calls service override factories
    without creating V8 scopes; this allows it to work when the factory is
    called synchronously as the result of some JavaScript execution, but
    fail otherwise. This adds the appropriate V8 scopes when calling the
    factory function; it also uses
    WebFrame::callFunctionEvenIfScriptDisabled() as as
    WebScopedMicrotaskSuppression.h suggests.
    
    Additionally, the service override factories are only usable once. This
    changes them to be copyable persistents so they can be reused; this also
    happens to make them clean themselves up without an explicit Reset()
    call.
    
    Review URL: https://codereview.chromium.org/1702513002
    
    Cr-Commit-Position: refs/heads/master@{#376672}

--- content/renderer/mojo/service_registry_js_wrapper.h
+++ content/renderer/mojo/service_registry_js_wrapper.h
@@ -13,11 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gin/handle.h"
 #include "gin/object_template_builder.h"
 #include "gin/wrappable.h"
-#include "mojo/public/cpp/system/handle.h"
-
-namespace v8 {
-class Isolate;
-}
+#include "mojo/public/cpp/system/message_pipe.h"
+#include "v8/include/v8.h"
 
 namespace content {
 
@@ -31,6 +28,7 @@ class CONTENT_EXPORT ServiceRegistryJsWrapper
   ~ServiceRegistryJsWrapper() override;
   static gin::Handle<ServiceRegistryJsWrapper> Create(
       v8::Isolate* isolate,
+      v8::Handle<v8::Context> context,
       ServiceRegistry* service_registry);
 
   // gin::Wrappable<ServiceRegistryJsWrapper> overrides.
@@ -46,10 +44,24 @@ class CONTENT_EXPORT ServiceRegistryJsWrapper
   static const char kModuleName[];
 
  private:
-  explicit ServiceRegistryJsWrapper(
-      base::WeakPtr<ServiceRegistry> service_registry);
+  using ScopedJsFactory =
+      v8::Persistent<v8::Function, v8::CopyablePersistentTraits<v8::Function>>;
 
+  ServiceRegistryJsWrapper(v8::Isolate* isolate,
+                           v8::Handle<v8::Context> context,
+                           base::WeakPtr<ServiceRegistry> service_registry);
+
+  void CallJsFactory(const ScopedJsFactory& factory,
+                     mojo::ScopedMessagePipeHandle pipe);
+
+  static void ClearContext(
+      const v8::WeakCallbackInfo<ServiceRegistryJsWrapper>& data);
+
+  v8::Isolate* isolate_;
+  v8::Global<v8::Context> context_;
   base::WeakPtr<ServiceRegistry> service_registry_;
+
+  base::WeakPtrFactory<ServiceRegistryJsWrapper> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ServiceRegistryJsWrapper);
 };
--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -2383,9 +2383,9 @@ void RenderFrameImpl::EnsureMojoBuiltinsAreAvailable(
   registry->AddBuiltinModule(isolate, mojo::edk::js::Support::kModuleName,
                              mojo::edk::js::Support::GetModule(isolate));
   registry->AddBuiltinModule(
-      isolate,
-      ServiceRegistryJsWrapper::kModuleName,
-      ServiceRegistryJsWrapper::Create(isolate, &service_registry_).ToV8());
+      isolate, ServiceRegistryJsWrapper::kModuleName,
+      ServiceRegistryJsWrapper::Create(isolate, context, &service_registry_)
+          .ToV8());
 }
 
 void RenderFrameImpl::AddMessageToConsole(ConsoleMessageLevel level,
