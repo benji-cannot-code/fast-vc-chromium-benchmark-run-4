0d3c09270b42b78ddc5350b662bfbba34aee461e
hiroshige <hiroshige@chromium.org>
2016-02-21 12:00:45 -0800

    Clean up WebTaskRunner::Task subclasses in platform/heap/ and platform/testing/
    Also |file_path| is renamed to pass presubmit checks.
    
    BUG=478194
    
    Review URL: https://codereview.chromium.org/1718653003
    
    Cr-Commit-Position: refs/heads/master@{#376667}

--- third_party/WebKit/Source/platform/testing/UnitTestHelpers.cpp
+++ third_party/WebKit/Source/platform/testing/UnitTestHelpers.cpp
@@ -41,17 +41,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 namespace testing {
 
-class QuitTask : public WebTaskRunner::Task {
-public:
-    virtual void run()
-    {
-        exitRunLoop();
-    }
-};
-
 void runPendingTasks()
 {
-    Platform::current()->currentThread()->taskRunner()->postTask(BLINK_FROM_HERE, new QuitTask);
+    Platform::current()->currentThread()->taskRunner()->postTask(BLINK_FROM_HERE, bind(&exitRunLoop));
     enterRunLoop();
 }
 
@@ -67,10 +59,10 @@ String blinkRootDir()
 PassRefPtr<SharedBuffer> readFromFile(const String& path)
 {
     StringUTF8Adaptor utf8(path);
-    base::FilePath file_path = base::FilePath::FromUTF8Unsafe(
+    base::FilePath filePath = base::FilePath::FromUTF8Unsafe(
         std::string(utf8.data(), utf8.length()));
     std::string buffer;
-    base::ReadFileToString(file_path, &buffer);
+    base::ReadFileToString(filePath, &buffer);
     return SharedBuffer::create(buffer.data(), buffer.size());
 }
 
