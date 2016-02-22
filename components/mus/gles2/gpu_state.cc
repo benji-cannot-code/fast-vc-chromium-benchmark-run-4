84f6fe99adaed720e7101aea91eaf72e6f2a4fac
rjkroege <rjkroege@chromium.org>
2016-02-22 13:50:40 -0800

    Re-structure ozone initialization in mus
    Rearrange the ozone initialization code to better reflect the mus
    threading architecture.
    
    BUG=558602
    
    Review URL: https://codereview.chromium.org/1693033002
    
    Cr-Commit-Position: refs/heads/master@{#376818}

--- components/mus/gles2/gpu_state.cc
+++ components/mus/gles2/gpu_state.cc
@@ -13,12 +13,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_implementation.h"
 #include "ui/gl/gl_surface.h"
 
+#if defined(USE_OZONE)
+#include "ui/ozone/public/ozone_platform.h"
+#endif
+
 namespace mus {
 
-GpuState::GpuState(bool hardware_rendering_available)
+GpuState::GpuState()
     : gpu_thread_("gpu_thread"),
       control_thread_("gpu_command_buffer_control"),
-      hardware_rendering_available_(hardware_rendering_available) {
+      hardware_rendering_available_(false) {
   base::ThreadRestrictions::ScopedAllowWait allow_wait;
   gpu_thread_.Start();
   control_thread_.Start();
@@ -37,6 +41,7 @@ void GpuState::StopThreads() {
 }
 
 void GpuState::InitializeOnGpuThread(base::WaitableEvent* event) {
+  hardware_rendering_available_ = gfx::GLSurface::InitializeOneOff();
   command_buffer_task_runner_ = new CommandBufferTaskRunner;
   driver_manager_.reset(new CommandBufferDriverManager);
   sync_point_manager_.reset(new gpu::SyncPointManager(true));
@@ -59,6 +64,10 @@ void GpuState::InitializeOnGpuThread(base::WaitableEvent* event) {
         << "Collect context graphics info failed!";
   }
   event->Signal();
+
+#if defined(USE_OZONE)
+  ui::OzonePlatform::InitializeForGPU();
+#endif
 }
 
 }  // namespace mus
