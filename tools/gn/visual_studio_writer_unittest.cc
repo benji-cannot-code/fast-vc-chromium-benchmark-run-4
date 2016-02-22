eac3190c21bb1e6337a53fb7d9c72501990461eb
ngg <ngg@ngg.hu>
2016-02-22 15:18:48 -0800

    GN support for generating VS2013 project files
    Optional Visual Studio version can now be specified for "gn gen".
    The default remains Visual Studio 2015.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1713363002
    
    Cr-Commit-Position: refs/heads/master@{#376849}

--- tools/gn/visual_studio_writer_unittest.cc
+++ tools/gn/visual_studio_writer_unittest.cc
@@ -27,7 +27,8 @@ std::string MakeTestPath(const std::string& path) {
 }  // namespace
 
 TEST_F(VisualStudioWriterTest, ResolveSolutionFolders) {
-  VisualStudioWriter writer(setup_.build_settings());
+  VisualStudioWriter writer(setup_.build_settings(),
+                            VisualStudioWriter::Version::Vs2015);
 
   std::string path =
       MakeTestPath("/foo/chromium/src/out/Debug/obj/base/base.vcxproj");
@@ -80,7 +81,8 @@ TEST_F(VisualStudioWriterTest, ResolveSolutionFolders) {
 }
 
 TEST_F(VisualStudioWriterTest, ResolveSolutionFolders_AbsPath) {
-  VisualStudioWriter writer(setup_.build_settings());
+  VisualStudioWriter writer(setup_.build_settings(),
+                            VisualStudioWriter::Version::Vs2015);
 
   std::string path =
       MakeTestPath("/foo/chromium/src/out/Debug/obj/base/base.vcxproj");
