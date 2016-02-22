822d6cb7633d3a0a2329b77d53518b2cecfb9615
peter <peter@chromium.org>
2016-02-22 08:12:13 -0800

    Enable Push Messaging payloads by default
    Three LGTMs were received on the Intent to Ship thread:
        https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/Vga32co0YMQ
    
    This patch exposes the PushMessagingData object, as well as the
    PushSubscription.getKey() method and the PushEvent.data property.
    
    BUG=486040, 568106
    
    Review URL: https://codereview.chromium.org/1693373002
    
    Cr-Commit-Position: refs/heads/master@{#376739}

--- chrome/browser/push_messaging/push_messaging_service_unittest.cc
+++ chrome/browser/push_messaging/push_messaging_service_unittest.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/gcm_driver/crypto/gcm_crypto_test_helpers.h"
 #include "components/gcm_driver/fake_gcm_client_factory.h"
 #include "components/gcm_driver/gcm_profile_service.h"
-#include "content/public/common/content_switches.h"
 #include "content/public/common/push_event_payload.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -97,10 +96,6 @@ class PushMessagingServiceTest : public ::testing::Test {
     // Override the GCM Profile service so that we can send fake messages.
     gcm::GCMProfileServiceFactory::GetInstance()->SetTestingFactory(
         &profile_, &BuildFakeGCMProfileService);
-
-    // Force-enable encrypted payloads for incoming push messages.
-    base::CommandLine::ForCurrentProcess()->AppendSwitch(
-        switches::kEnableExperimentalWebPlatformFeatures);
   }
 
   ~PushMessagingServiceTest() override {}
