776cc7a3dcd8daf96d16615767169b84e1d4f6e1
yhirano <yhirano@chromium.org>
2016-02-21 23:05:36 -0800

    Fix a ReadableStreamDataConsumerHandle assertion failure
    This change fixes an assertion failure on ReadableStreamDataConsumerHandle
    which hits when an empty buffer is enqueued.
    
    BUG=588058
    R=tyoshino@chromium.org
    
    Review URL: https://codereview.chromium.org/1714103002
    
    Cr-Commit-Position: refs/heads/master@{#376697}

--- third_party/WebKit/Source/modules/fetch/ReadableStreamDataConsumerHandleTest.cpp
+++ third_party/WebKit/Source/modules/fetch/ReadableStreamDataConsumerHandleTest.cpp
@@ -186,6 +186,7 @@ TEST_F(ReadableStreamDataConsumerHandleTest, Read)
     ScriptValue stream(scriptState(), evalWithPrintingError(
         "var controller;"
         "var stream = new ReadableStream({start: c => controller = c});"
+        "controller.enqueue(new Uint8Array());"
         "controller.enqueue(new Uint8Array([0x43, 0x44, 0x45, 0x46]));"
         "controller.enqueue(new Uint8Array([0x47, 0x48, 0x49, 0x4a]));"
         "controller.close();"
@@ -206,6 +207,8 @@ TEST_F(ReadableStreamDataConsumerHandleTest, Read)
     EXPECT_CALL(checkpoint, Call(4));
     EXPECT_CALL(*client, didGetReadable());
     EXPECT_CALL(checkpoint, Call(5));
+    EXPECT_CALL(*client, didGetReadable());
+    EXPECT_CALL(checkpoint, Call(6));
 
     char buffer[3];
     size_t readBytes;
@@ -217,6 +220,9 @@ TEST_F(ReadableStreamDataConsumerHandleTest, Read)
     EXPECT_EQ(kShouldWait, reader->read(buffer, 3, kNone, &readBytes));
     testing::runPendingTasks();
     checkpoint.Call(3);
+    EXPECT_EQ(kShouldWait, reader->read(buffer, 3, kNone, &readBytes));
+    testing::runPendingTasks();
+    checkpoint.Call(4);
     EXPECT_EQ(kOk, reader->read(buffer, 3, kNone, &readBytes));
     EXPECT_EQ(3u, readBytes);
     EXPECT_EQ(0x43, buffer[0]);
@@ -227,7 +233,7 @@ TEST_F(ReadableStreamDataConsumerHandleTest, Read)
     EXPECT_EQ(0x46, buffer[0]);
     EXPECT_EQ(kShouldWait, reader->read(buffer, 3, kNone, &readBytes));
     testing::runPendingTasks();
-    checkpoint.Call(4);
+    checkpoint.Call(5);
     EXPECT_EQ(kOk, reader->read(buffer, 3, kNone, &readBytes));
     EXPECT_EQ(3u, readBytes);
     EXPECT_EQ(0x47, buffer[0]);
@@ -238,7 +244,7 @@ TEST_F(ReadableStreamDataConsumerHandleTest, Read)
     EXPECT_EQ(0x4a, buffer[0]);
     EXPECT_EQ(kShouldWait, reader->read(buffer, 3, kNone, &readBytes));
     testing::runPendingTasks();
-    checkpoint.Call(5);
+    checkpoint.Call(6);
     EXPECT_EQ(kDone, reader->read(buffer, 3, kNone, &readBytes));
 }
 
@@ -248,6 +254,7 @@ TEST_F(ReadableStreamDataConsumerHandleTest, TwoPhaseRead)
     ScriptValue stream(scriptState(), evalWithPrintingError(
         "var controller;"
         "var stream = new ReadableStream({start: c => controller = c});"
+        "controller.enqueue(new Uint8Array());"
         "controller.enqueue(new Uint8Array([0x43, 0x44, 0x45, 0x46]));"
         "controller.enqueue(new Uint8Array([0x47, 0x48, 0x49, 0x4a]));"
         "controller.close();"
@@ -268,6 +275,8 @@ TEST_F(ReadableStreamDataConsumerHandleTest, TwoPhaseRead)
     EXPECT_CALL(checkpoint, Call(4));
     EXPECT_CALL(*client, didGetReadable());
     EXPECT_CALL(checkpoint, Call(5));
+    EXPECT_CALL(*client, didGetReadable());
+    EXPECT_CALL(checkpoint, Call(6));
 
     const void* buffer;
     size_t available;
@@ -279,6 +288,9 @@ TEST_F(ReadableStreamDataConsumerHandleTest, TwoPhaseRead)
     EXPECT_EQ(kShouldWait, reader->beginRead(&buffer, kNone, &available));
     testing::runPendingTasks();
     checkpoint.Call(3);
+    EXPECT_EQ(kShouldWait, reader->beginRead(&buffer, kNone, &available));
+    testing::runPendingTasks();
+    checkpoint.Call(4);
     EXPECT_EQ(kOk, reader->beginRead(&buffer, kNone, &available));
     EXPECT_EQ(4u, available);
     EXPECT_EQ(0x43, static_cast<const char*>(buffer)[0]);
@@ -301,7 +313,7 @@ TEST_F(ReadableStreamDataConsumerHandleTest, TwoPhaseRead)
     EXPECT_EQ(kOk, reader->endRead(3));
     EXPECT_EQ(kShouldWait, reader->beginRead(&buffer, kNone, &available));
     testing::runPendingTasks();
-    checkpoint.Call(4);
+    checkpoint.Call(5);
     EXPECT_EQ(kOk, reader->beginRead(&buffer, kNone, &available));
     EXPECT_EQ(4u, available);
     EXPECT_EQ(0x47, static_cast<const char*>(buffer)[0]);
@@ -311,7 +323,7 @@ TEST_F(ReadableStreamDataConsumerHandleTest, TwoPhaseRead)
     EXPECT_EQ(kOk, reader->endRead(4));
     EXPECT_EQ(kShouldWait, reader->beginRead(&buffer, kNone, &available));
     testing::runPendingTasks();
-    checkpoint.Call(5);
+    checkpoint.Call(6);
     EXPECT_EQ(kDone, reader->beginRead(&buffer, kNone, &available));
 }
 
