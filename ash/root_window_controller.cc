95deb2276190b642dd5727288a3519bcd98d7cf4
kylechar <kylechar@chromium.org>
2016-02-22 07:50:03 -0800

    Add cursor support to Ozone X11.
    This CL adds a CursorFactoryOzone implementation for Ozone X11. A
    X11CursorOzone class is added along with X11WindowOzone. This class
    exists to own the X11 cursor id and own associated resources. It
    cleans up X resources on destruction.
    
    BUG=361137
    
    Review URL: https://codereview.chromium.org/1664373002
    
    Cr-Commit-Position: refs/heads/master@{#376735}

--- ash/root_window_controller.cc
+++ ash/root_window_controller.cc
@@ -706,7 +706,6 @@ void RootWindowController::Init(RootWindowType root_window_type,
   Shell* shell = Shell::GetInstance();
   shell->InitRootWindow(root_window);
 
-  ash_host_->AsWindowTreeHost()->SetCursor(ui::kCursorPointer);
   CreateContainersInRootWindow(root_window);
 
   CreateSystemBackground(first_run_after_boot);
