be412d3e2b3495118ced7352721adac8ba88017b
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-02-22 14:11:29 -0800

    [Extension UI Views] Properly order the browser actions container children
    The BrowserActionsContainer contains child views for each of the toolbar
    actions and lays them out according to its own internal vector with the right
    order. But we also need to update the views::View::children_ vector in order
    for in-menu navigation to work correctly. Update so that the children_ vector
    is kept in-sync after drag-and-drops.
    
    BUG=584183
    
    Review URL: https://codereview.chromium.org/1713833005
    
    Cr-Commit-Position: refs/heads/master@{#376825}

--- chrome/browser/ui/views/toolbar/browser_actions_container_browsertest.cc
+++ chrome/browser/ui/views/toolbar/browser_actions_container_browsertest.cc
@@ -48,6 +48,14 @@ IN_PROC_BROWSER_TEST_F(BrowserActionsBarBrowserTest, DragBrowserActions) {
       BrowserView::GetBrowserViewForBrowser(browser())
           ->toolbar()->browser_actions();
 
+  // The order of the child views should be the same.
+  EXPECT_EQ(container->GetViewForId(extension_a()->id()),
+            container->child_at(0));
+  EXPECT_EQ(container->GetViewForId(extension_b()->id()),
+            container->child_at(1));
+  EXPECT_EQ(container->GetViewForId(extension_c()->id()),
+            container->child_at(2));
+
   // Simulate a drag and drop to the right.
   ui::OSExchangeData drop_data;
   // Drag extension A from index 0...
@@ -67,6 +75,12 @@ IN_PROC_BROWSER_TEST_F(BrowserActionsBarBrowserTest, DragBrowserActions) {
   EXPECT_EQ(extension_b()->id(), browser_actions_bar()->GetExtensionId(0));
   EXPECT_EQ(extension_a()->id(), browser_actions_bar()->GetExtensionId(1));
   EXPECT_EQ(extension_c()->id(), browser_actions_bar()->GetExtensionId(2));
+  EXPECT_EQ(container->GetViewForId(extension_b()->id()),
+            container->child_at(0));
+  EXPECT_EQ(container->GetViewForId(extension_a()->id()),
+            container->child_at(1));
+  EXPECT_EQ(container->GetViewForId(extension_c()->id()),
+            container->child_at(2));
 
   const extensions::ExtensionSet& extension_set =
       extensions::ExtensionRegistry::Get(profile())->enabled_extensions();
@@ -96,6 +110,12 @@ IN_PROC_BROWSER_TEST_F(BrowserActionsBarBrowserTest, DragBrowserActions) {
   EXPECT_EQ(extension_a()->id(), browser_actions_bar()->GetExtensionId(0));
   EXPECT_EQ(extension_b()->id(), browser_actions_bar()->GetExtensionId(1));
   EXPECT_EQ(extension_c()->id(), browser_actions_bar()->GetExtensionId(2));
+  EXPECT_EQ(container->GetViewForId(extension_a()->id()),
+            container->child_at(0));
+  EXPECT_EQ(container->GetViewForId(extension_b()->id()),
+            container->child_at(1));
+  EXPECT_EQ(container->GetViewForId(extension_c()->id()),
+            container->child_at(2));
 
   // Shrink the size of the container so we have an overflow menu.
   toolbar_model()->SetVisibleIconCount(2u);
