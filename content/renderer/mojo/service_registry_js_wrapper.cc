5f14e0db1607f83699e5100edf5f974224054c31
sammc <sammc@chromium.org>
2016-02-22 15:56:09 -0800

    Clear overridden services when a mojo layout test ends.
    Now that service factory overrides can be called more than once, they
    need to be cleaned up to avoid being detected as leaks in layout tests.
    This adds a ClearServiceOverridesForTesting() to ServiceRegistryImpl
    and ServiceRegistryJsWrapper and adds a call at the end of a mojo_test
    layout test.
    
    Review URL: https://codereview.chromium.org/1716113002
    
    Cr-Commit-Position: refs/heads/master@{#376858}

--- content/renderer/mojo/service_registry_js_wrapper.cc
+++ content/renderer/mojo/service_registry_js_wrapper.cc
@@ -37,11 +37,13 @@ gin::Handle<ServiceRegistryJsWrapper> ServiceRegistryJsWrapper::Create(
 
 gin::ObjectTemplateBuilder ServiceRegistryJsWrapper::GetObjectTemplateBuilder(
     v8::Isolate* isolate) {
-  return Wrappable<ServiceRegistryJsWrapper>::GetObjectTemplateBuilder(isolate).
-      SetMethod("connectToService",
-                &ServiceRegistryJsWrapper::ConnectToService).
-      SetMethod("addServiceOverrideForTesting",
-                &ServiceRegistryJsWrapper::AddServiceOverrideForTesting);
+  return Wrappable<ServiceRegistryJsWrapper>::GetObjectTemplateBuilder(isolate)
+      .SetMethod("connectToService",
+                 &ServiceRegistryJsWrapper::ConnectToService)
+      .SetMethod("addServiceOverrideForTesting",
+                 &ServiceRegistryJsWrapper::AddServiceOverrideForTesting)
+      .SetMethod("clearServiceOverridesForTesting",
+                 &ServiceRegistryJsWrapper::ClearServiceOverridesForTesting);
 }
 
 mojo::Handle ServiceRegistryJsWrapper::ConnectToService(
@@ -64,6 +66,15 @@ void ServiceRegistryJsWrapper::AddServiceOverrideForTesting(
   registry->AddServiceOverrideForTesting(
       service_name, base::Bind(&ServiceRegistryJsWrapper::CallJsFactory,
                                weak_factory_.GetWeakPtr(), factory));
+}
+
+void ServiceRegistryJsWrapper::ClearServiceOverridesForTesting() {
+  ServiceRegistryImpl* registry =
+      static_cast<ServiceRegistryImpl*>(service_registry_.get());
+  if (!registry)
+    return;
+
+  registry->ClearServiceOverridesForTesting();
 }
 
 ServiceRegistryJsWrapper::ServiceRegistryJsWrapper(
