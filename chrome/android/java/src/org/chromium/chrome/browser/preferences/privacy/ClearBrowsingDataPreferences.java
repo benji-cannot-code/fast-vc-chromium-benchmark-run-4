17c2a989d944fe7cd8aa4be0a7e17ce802908c0f
msramek <msramek@chromium.org>
2016-02-23 04:46:03 -0800

    Enable browsing data counters on Android.
    And fix a crash that occurs immediately at the opening of the CBD dialog
    when the counter returns result before views are created.
    
    BUG=569870,586701
    
    Review URL: https://codereview.chromium.org/1709253002
    
    Cr-Commit-Position: refs/heads/master@{#376972}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/ClearBrowsingDataPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/ClearBrowsingDataPreferences.java
@@ -84,7 +84,7 @@ public class ClearBrowsingDataPreferences extends PreferenceFragment
 
         @Override
         public void onCounterFinished(String result) {
-            mCheckbox.setSummaryOn(result);
+            if (mCheckbox != null) mCheckbox.setSummaryOn(result);
         }
     }
 
