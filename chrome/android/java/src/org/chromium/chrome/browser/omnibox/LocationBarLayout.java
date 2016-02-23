fee373564adae28b4da32ada44aefb4d4ea66dc6
tedchoc <tedchoc@chromium.org>
2016-02-22 16:57:23 -0800

    [Android] Disable inline autocomplete for omnibox pasted text.
    BUG=588863
    
    Review URL: https://codereview.chromium.org/1720303002
    
    Cr-Commit-Position: refs/heads/master@{#376881}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -1766,6 +1766,7 @@ public class LocationBarLayout extends FrameLayout implements OnClickListener,
         Editable text = mUrlBar.getText();
 
         return mUrlBar.isCursorAtEndOfTypedText()
+                && !mUrlBar.isPastedText()
                 && !mUrlBar.isHandlingBatchInput()
                 && BaseInputConnection.getComposingSpanEnd(text)
                         == BaseInputConnection.getComposingSpanStart(text);
