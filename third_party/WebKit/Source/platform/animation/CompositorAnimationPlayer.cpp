a1c8273a7ef4319232fb516eb3e2224fabec9ef8
loyso <loyso@chromium.org>
2016-02-22 19:05:23 -0800

    Blink Compositor Animation: Initialize pointer to null in constructor.
    A code health fix.
    
    Not important since we don't use m_delegate unless you call set_animation_delegate. And if you call it, m_delegate becomes initialized.
    
    BUG=577016
    
    Review URL: https://codereview.chromium.org/1720313002
    
    Cr-Commit-Position: refs/heads/master@{#376911}

--- third_party/WebKit/Source/platform/animation/CompositorAnimationPlayer.cpp
+++ third_party/WebKit/Source/platform/animation/CompositorAnimationPlayer.cpp
@@ -14,6 +14,7 @@ namespace blink {
 
 CompositorAnimationPlayer::CompositorAnimationPlayer()
     : m_animationPlayer(cc::AnimationPlayer::Create(cc::AnimationIdProvider::NextPlayerId()))
+    , m_delegate()
 {
 }
 
