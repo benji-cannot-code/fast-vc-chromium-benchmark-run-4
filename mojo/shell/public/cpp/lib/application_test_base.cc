2708fd53cf2dae4f43c57a086cc926b05e50c3c2
xhwang <xhwang@chromium.org>
2016-02-23 11:56:27 -0800

    mojo: Delay ApplicationTestBase shell disconnection.
    BUG=587523
    TEST=Reenable tests.
    
    Review URL: https://codereview.chromium.org/1709173002
    
    Cr-Commit-Position: refs/heads/master@{#377068}

--- mojo/shell/public/cpp/lib/application_test_base.cc
+++ mojo/shell/public/cpp/lib/application_test_base.cc
@@ -136,9 +136,16 @@ TestHelper::~TestHelper() {
   shell_connection_.reset();
 }
 
-ApplicationTestBase::ApplicationTestBase() : test_helper_(nullptr) {}
+ApplicationTestBase::ApplicationTestBase() {}
 
 ApplicationTestBase::~ApplicationTestBase() {
+  CHECK(!g_shell_client_request.is_pending());
+  CHECK(!g_shell);
+
+  test_helper_.reset();
+
+  if (use_default_run_loop_)
+    Environment::DestroyDefaultRunLoop();
 }
 
 ShellClient* ApplicationTestBase::GetShellClient() {
@@ -148,8 +155,12 @@ ShellClient* ApplicationTestBase::GetShellClient() {
 void ApplicationTestBase::SetUp() {
   // A run loop is recommended for ShellConnection initialization and
   // communication.
-  if (ShouldCreateDefaultRunLoop())
+  // Check this in SetUp() instead of in the constructor because we cannot call
+  // virtual methods in the constructor.
+  if (ShouldCreateDefaultRunLoop()) {
+    use_default_run_loop_ = true;
     Environment::InstantiateDefaultRunLoop();
+  }
 
   CHECK(g_shell_client_request.is_pending());
   CHECK(g_shell);
@@ -159,13 +170,6 @@ void ApplicationTestBase::SetUp() {
 }
 
 void ApplicationTestBase::TearDown() {
-  CHECK(!g_shell_client_request.is_pending());
-  CHECK(!g_shell);
-
-  test_helper_.reset();
-
-  if (ShouldCreateDefaultRunLoop())
-    Environment::DestroyDefaultRunLoop();
 }
 
 bool ApplicationTestBase::ShouldCreateDefaultRunLoop() {
