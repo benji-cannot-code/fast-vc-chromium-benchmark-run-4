9ffcc61a38777a03e9944a87ee69cdd62c3c197b
alemate <alemate@chromium.org>
2016-02-23 15:25:15 -0800

    AccountId::GetGaiaIdKey should return filename-compatible string.
    This key sring should be safe to use in a file name.
    
    BUG=462823
    TEST=manual
    
    Review URL: https://codereview.chromium.org/1726543002
    
    Cr-Commit-Position: refs/heads/master@{#377120}

--- components/signin/core/account_id/account_id.cc
+++ components/signin/core/account_id/account_id.cc
@@ -23,7 +23,7 @@ const char kGaiaIdKey[] = "gaia_id";
 const char kEmailKey[] = "email";
 
 // Prefix for GetGaiaIdKey().
-const char kKeyGaiaIdPrefix[] = "g:";
+const char kKeyGaiaIdPrefix[] = "g-";
 
 struct GoogleStringSingleton {
   GoogleStringSingleton() : google(kGoogle) {}
