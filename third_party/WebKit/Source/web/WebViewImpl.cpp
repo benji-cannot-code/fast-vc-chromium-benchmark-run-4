bcf7a3b83ef8bb3fabdee3a79b09ee8a57ef2c42
skyostil <skyostil@chromium.org>
2016-02-23 13:20:04 -0800

    Only animate if we have a local frame
    Only call PageWidgetDelegate if we have a local main frame.
    
    BUG=587909
    
    Review URL: https://codereview.chromium.org/1714993002
    
    Cr-Commit-Position: refs/heads/master@{#377087}

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -1947,7 +1947,7 @@ void WebViewImpl::beginFrame(double lastFrameTimeMonotonic)
         }
     }
 
-    if (!m_page)
+    if (!mainFrameImpl())
         return;
 
     DocumentLifecycle::AllowThrottlingScope throttlingScope(mainFrameImpl()->frame()->document()->lifecycle());
