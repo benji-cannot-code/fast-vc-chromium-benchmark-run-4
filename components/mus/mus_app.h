2ee4dc498ec569b24b7ce18bf775286cbecd3c06
kylechar <kylechar@chromium.org>
2016-02-23 07:39:05 -0800

    Have mus initialize cursor resources.
    The mus app needs to initialize resources otherwise it crashes when
    trying to access cursor bitmaps. Initialize ResourceLoader when mus
    starts.
    
    BUG=586563
    
    Review URL: https://codereview.chromium.org/1705193002
    
    Cr-Commit-Position: refs/heads/master@{#376989}

--- components/mus/mus_app.h
+++ components/mus/mus_app.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <string>
 #include <vector>
 
 #include "base/macros.h"
@@ -59,6 +60,8 @@ class MandolineUIServicesApp
   // Holds InterfaceRequests received before the first WindowTreeHost Display
   // has been established.
   struct PendingRequest;
+
+  void InitializeResources(mojo::Shell* shell);
 
   // mojo::ShellClient:
   void Initialize(mojo::Shell* shell, const std::string& url,
