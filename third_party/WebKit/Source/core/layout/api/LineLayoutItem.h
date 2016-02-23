17c3ab8cee556ff54bbbb72cf63c9c9269e2afa4
dgrogan <dgrogan@chromium.org>
2016-02-23 13:43:26 -0800

    [Line Layout API] Remove LineLayoutRubyRun uses of LayoutObject* operator()
    The LayoutObject* operator() is a dirty escape hatch that hides gaps in
    the Line Layout API. We need to remove it before the API is done.
    
    This CL is trivial save for making LineLayoutRubyRun a friend of
    LineLayoutItem to get access to LineLayoutItem::layoutObject().
    
    BUG=499321
    
    Review URL: https://codereview.chromium.org/1725483002
    
    Cr-Commit-Position: refs/heads/master@{#377096}

--- third_party/WebKit/Source/core/layout/api/LineLayoutItem.h
+++ third_party/WebKit/Source/core/layout/api/LineLayoutItem.h
@@ -420,7 +420,8 @@ private:
     LayoutObject* m_layoutObject;
 
     friend class LineLayoutAPIShim;
-    friend class LineLayoutBlockFlow; // For layoutObject().
+    friend class LineLayoutBlockFlow;
+    friend class LineLayoutRubyRun;
 };
 
 } // namespace blink
