88f68e26b1efbe227ac15425006d24987bd82893
qiangchen <qiangchen@chromium.org>
2016-02-23 10:48:12 -0800

    Bug Fix: Show Desktop Share Security Message On All Virtual Desktops
    It was reported by an end user that our security message window for desktop capture was sticky to a specific workspace (Mac terminology: space, desktop), and thus becomes invisible when the user switch workspace.
    
    The fix will make the security message window visible on all workspaces for linux and mac.
    
    BUG=557293
    
    Review URL: https://codereview.chromium.org/1716563002
    
    Cr-Commit-Position: refs/heads/master@{#377025}

--- chrome/browser/ui/cocoa/screen_capture_notification_ui_cocoa.mm
+++ chrome/browser/ui/cocoa/screen_capture_notification_ui_cocoa.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/font_list.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/image/image_skia_util_mac.h"
+#include "ui/gfx/mac/nswindow_frame_controls.h"
 #include "ui/gfx/text_elider.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/native_theme/native_theme_mac.h"
@@ -89,6 +90,7 @@ scoped_ptr<ScreenCaptureNotificationUI> ScreenCaptureNotificationUI::Create(
   [window setLevel:NSStatusWindowLevel];
   [window setMovableByWindowBackground:YES];
   [window setDelegate:self];
+  gfx::SetNSWindowVisibleOnAllWorkspaces(window, true);
 
   self = [super initWithWindow:window];
   if (self) {
