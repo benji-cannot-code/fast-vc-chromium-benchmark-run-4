0d69750a238add2623f400e1071f02cb4b95ae00
mnaganov <mnaganov@chromium.org>
2016-02-23 20:12:31 -0800

    Pass ChildProcessCreationParams in ChildProcessLauncher.warmUp
    This fixes the use case when Chrome is started via Custom Tabs.
    
    BUG=588397
    TBR=dtrainor@chromium.org
    
    Review URL: https://codereview.chromium.org/1725643003
    
    Cr-Commit-Position: refs/heads/master@{#377217}

--- chrome/android/java/src/org/chromium/chrome/browser/init/NativeInitializationController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/NativeInitializationController.java
@@ -101,9 +101,7 @@ class NativeInitializationController {
                     return;
                 }
                 ChromeApplication chrome = (ChromeApplication) mContext;
-                ChildProcessLauncher.setChildProcessCreationParams(
-                        chrome.getChildProcessCreationParams());
-                ChildProcessLauncher.warmUp(mContext);
+                ChildProcessLauncher.warmUp(mContext, chrome.getChildProcessCreationParams());
                 ThreadUtils.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
