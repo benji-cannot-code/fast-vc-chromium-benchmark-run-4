f9d874b22ef9d29c94de9b6dd01875697b41e785
varkha <varkha@chromium.org>
2016-02-23 21:12:01 -0800

    Makes location bar IconLabelBubbleView use internal padding
    BUG=583234
    TEST=With chrome://flags/#top-chrome-md set to Non-Material, Material
         and Material-Hybrid:
         1. Confirm that focus rectangles of location bar icons have the same
         height.
         2. Navigate to www.microsoft.com and confirm that site settings
         bubble (left of URL) height has not changed from prior to this CL.
         3. Navigate to http://www.popuptest.com/popuptest1.html and confirm
         that popup blocker animated label bubble (right of URL) height
         has not changed from prior to this CL.
    
    Review URL: https://codereview.chromium.org/1651233003
    
    Cr-Commit-Position: refs/heads/master@{#377225}

--- chrome/browser/ui/views/location_bar/icon_label_bubble_view.cc
+++ chrome/browser/ui/views/location_bar/icon_label_bubble_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/native_theme/native_theme.h"
+#include "ui/views/border.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/painter.h"
 
@@ -54,6 +55,14 @@ IconLabelBubbleView::IconLabelBubbleView(int contained_image,
   if (elide_in_middle)
     label_->SetElideBehavior(gfx::ELIDE_MIDDLE);
   AddChildView(label_);
+
+  // Bubbles are given the full internal height of the location bar so that all
+  // child views in the location bar have the same height. The visible height of
+  // the bubble should be smaller, so use an empty border to shrink down the
+  // content bounds so the background gets painted correctly.
+  const int padding = GetLayoutConstant(LOCATION_BAR_BUBBLE_VERTICAL_PADDING);
+  SetBorder(
+      views::Border::CreateEmptyBorder(gfx::Insets(padding, 0, padding, 0)));
 }
 
 IconLabelBubbleView::~IconLabelBubbleView() {
@@ -200,8 +209,10 @@ const char* IconLabelBubbleView::GetClassName() const {
 void IconLabelBubbleView::OnPaint(gfx::Canvas* canvas) {
   if (!ShouldShowBackground())
     return;
-  if (background_painter_)
-    background_painter_->Paint(canvas, size());
+  if (background_painter_) {
+    views::Painter::PaintPainterAt(canvas, background_painter_.get(),
+                                   GetContentsBounds());
+  }
   if (background())
     background()->Paint(canvas, this);
 }
