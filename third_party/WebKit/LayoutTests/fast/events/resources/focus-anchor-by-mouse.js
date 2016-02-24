64b7ce5f9770d11398654ce7d1b8633618ecc62d
philipj <philipj@opera.com>
2016-02-24 02:40:29 -0800

    Update SVG tests ahead of offset* removal
    This converts some SVG tests to use getBoundingClientRect() instead of
    the deprecated offset* attributes, so that the removal CL is minimized
    and thus easier to revert in case of trouble.
    
    smil-scheduled-in-inactive-document-crash.html didn't actually run the
    problematic code because of a early finishJSTest(), which was moved.
    
    BUG=463116
    R=fs@opera.com
    
    Review URL: https://codereview.chromium.org/1729073002
    
    Cr-Commit-Position: refs/heads/master@{#377261}

--- third_party/WebKit/LayoutTests/fast/events/resources/focus-anchor-by-mouse.js
+++ third_party/WebKit/LayoutTests/fast/events/resources/focus-anchor-by-mouse.js
@@ -2,7 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 window.onload = function() {
     if (window.eventSender) {
         var aElement = document.getElementById('anchor');
-        eventSender.mouseMoveTo(aElement.offsetLeft + 2, aElement.offsetTop + 2);
+        var aRect = aElement.getBoundingClientRect();
+        eventSender.mouseMoveTo(aRect.left + 2, aRect.top + 2);
         eventSender.mouseDown();
     }
 };
