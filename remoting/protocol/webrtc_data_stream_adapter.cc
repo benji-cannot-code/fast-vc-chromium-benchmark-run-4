5aab19e206324c3b751238c18d4dfd7f64795958
sergeyu <sergeyu@chromium.org>
2016-02-24 11:44:22 -0800

    Fix WebrtcDataStreamAdapter to handle errors asychronously.
    Previously WebrtcDataStreamAdapter was handling errors synchronously
    and the host was crashing because the data channels were destroyed
    when they were on the stack.
    
    Review URL: https://codereview.chromium.org/1723423002
    
    Cr-Commit-Position: refs/heads/master@{#377350}

--- remoting/protocol/webrtc_data_stream_adapter.cc
+++ remoting/protocol/webrtc_data_stream_adapter.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include "base/bind.h"
 #include "base/callback.h"
 #include "base/callback_helpers.h"
 #include "base/location.h"
@@ -135,8 +136,11 @@ void WebrtcDataStreamAdapter::Channel::OnError() {
     return;
 
   state_ = State::CLOSED;
-  if (adapter_)
-    adapter_->OnChannelError(this);
+
+  // Notify the adapter about the error asychronously.
+  base::ThreadTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE,
+      base::Bind(&WebrtcDataStreamAdapter::OnChannelError, adapter_));
 }
 
 void WebrtcDataStreamAdapter::Channel::OnMessage(
@@ -227,7 +231,7 @@ void WebrtcDataStreamAdapter::OnChannelConnected(Channel* channel) {
   pending_channel.connected_callback.Run(std::move(pending_channel.channel));
 }
 
-void WebrtcDataStreamAdapter::OnChannelError(Channel* channel) {
+void WebrtcDataStreamAdapter::OnChannelError() {
   error_callback_.Run(CHANNEL_CONNECTION_ERROR);
 }
 
