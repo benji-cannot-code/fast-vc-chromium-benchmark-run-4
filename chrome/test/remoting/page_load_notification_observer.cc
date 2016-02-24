99151c125b71550dff19a12fad07b5aab995cc1b
jamiewalch <jamiewalch@chromium.org>
2016-02-24 12:31:54 -0800

    Fix MANUAL_Launch browser test.
    BUG=587704
    
    Review URL: https://codereview.chromium.org/1724613002
    
    Cr-Commit-Position: refs/heads/master@{#377369}

--- chrome/test/remoting/page_load_notification_observer.cc
+++ chrome/test/remoting/page_load_notification_observer.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/remoting/page_load_notification_observer.h"
 
 #include "content/public/browser/navigation_controller.h"
+#include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_types.h"
 #include "content/public/browser/web_contents.h"
 
@@ -17,7 +18,8 @@ PageLoadNotificationObserver::PageLoadNotificationObserver(const GURL& target)
           base::Bind(&PageLoadNotificationObserver::IsTargetLoaded,
                      base::Unretained(this))),
       target_(target),
-      ignore_url_parameters_(false) {
+      ignore_url_parameters_(false),
+      matched_source_(content::NotificationService::AllSources()) {
 }
 
 PageLoadNotificationObserver::~PageLoadNotificationObserver() {}
@@ -26,14 +28,19 @@ bool PageLoadNotificationObserver::IsTargetLoaded() {
   content::NavigationController* controller =
       content::Source<content::NavigationController>(source()).ptr();
   GURL current_url = controller->GetWebContents()->GetURL();
+  bool result = false;
   if (ignore_url_parameters_) {
     GURL::Replacements strip_query;
     strip_query.ClearQuery();
-    return current_url.ReplaceComponents(strip_query) ==
+    result = current_url.ReplaceComponents(strip_query) ==
         target_.ReplaceComponents(strip_query);
   } else {
-    return current_url == target_;
+    result = current_url == target_;
   }
+  if (result) {
+    matched_source_ = source();
+  }
+  return result;
 }
 
 }  // namespace remoting
