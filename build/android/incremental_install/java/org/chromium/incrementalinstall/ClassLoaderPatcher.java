53985b6dd8b897956943a14f812d89230c7128b9
agrieve <agrieve@chromium.org>
2016-02-23 17:20:19 -0800

    Revert of Add increment install native libraries first in lookup paths (patchset #1 id:1 of https://codereview.chromium.org/1726703002/ )
    Reason for revert:
    Breaks incremental install for M devices.
    
    Original issue's description:
    > Add increment install native libraries first in lookup paths
    >
    > This fixes a subtle bug where the wrong libchromium_android_linker.so is used
    > when it already exists in the system library path.
    >
    > BUG=587584
    >
    > Committed: https://crrev.com/86af989585dd6aac45d3087456ef7cafb37afa86
    > Cr-Commit-Position: refs/heads/master@{#377074}
    
    TBR=hanxi@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=587584
    
    Review URL: https://codereview.chromium.org/1731593003
    
    Cr-Commit-Position: refs/heads/master@{#377171}

--- build/android/incremental_install/java/org/chromium/incrementalinstall/ClassLoaderPatcher.java
+++ build/android/incremental_install/java/org/chromium/incrementalinstall/ClassLoaderPatcher.java
@@ -151,11 +151,11 @@ final class ClassLoaderPatcher {
         // Switched from an array to an ArrayList in Lollipop.
         if (currentDirs instanceof List) {
             List<File> dirsAsList = (List<File>) currentDirs;
-            dirsAsList.add(0, nativeLibDir);
+            dirsAsList.add(nativeLibDir);
         } else {
             File[] dirsAsArray = (File[]) currentDirs;
             Reflect.setField(dexPathList, "nativeLibraryDirectories",
-                    Reflect.concatArrays(newDirs, dirsAsArray));
+                    Reflect.concatArrays(dirsAsArray, newDirs));
         }
 
         Object[] nativeLibraryPathElements;
@@ -169,7 +169,7 @@ final class ClassLoaderPatcher {
         Object[] additionalElements = makeNativePathElements(newDirs);
         Reflect.setField(
                 dexPathList, "nativeLibraryPathElements",
-                Reflect.concatArrays(additionalElements, nativeLibraryPathElements));
+                Reflect.concatArrays(nativeLibraryPathElements, additionalElements));
     }
 
     private static void copyChangedFiles(File srcDir, File dstDir) throws IOException {
