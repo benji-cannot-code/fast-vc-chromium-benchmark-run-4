512413cf7ab48cbcf3d8093099f4edcc2da8260b
vasilii <vasilii@chromium.org>
2016-02-24 03:56:09 -0800

    Change the "Smart Lock" links to the help article.
    BUG=589171
    
    Review URL: https://codereview.chromium.org/1731183002
    
    Cr-Commit-Position: refs/heads/master@{#377268}

--- chrome/browser/ui/passwords/password_dialog_controller_impl.cc
+++ chrome/browser/ui/passwords/password_dialog_controller_impl.cc
@@ -101,7 +101,7 @@ PasswordDialogControllerImpl::GetAutoSigninText() const {
 }
 
 void PasswordDialogControllerImpl::OnSmartLockLinkClicked() {
-  delegate_->NavigateToExternalPasswordManager();
+  delegate_->NavigateToSmartLockHelpPage();
 }
 
 void PasswordDialogControllerImpl::OnChooseCredentials(
