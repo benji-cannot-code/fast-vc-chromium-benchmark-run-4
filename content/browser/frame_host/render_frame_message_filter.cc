d830568fe84c303adfc42c543ebe46e23f2afa7f
mkwst <mkwst@chromium.org>
2016-02-24 05:07:20 -0800

    Load cookies once per 'document.cookie' read
    This patch refactors RenderFrameMessageFilter to read cookies for a URL
    once when requested via 'document.cookie', rather than reading them once
    then checking cookie permissions, throwing the data away and requesting
    it again to build the cookie string.
    
    As a drive-by, it also fixes a bug in the logic which prevented
    same-site cookies from being returned, and fixes a bug in a related
    test for http-only cookies which turns out to have had no impact on
    the feature, but meant that it's been basically untested for years.
    
    BUG=581650
    R=mmenke@chromium.org,rdsmith@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review URL: https://codereview.chromium.org/1714013002
    
    Cr-Commit-Position: refs/heads/master@{#377278}

--- content/browser/frame_host/render_frame_message_filter.cc
+++ content/browser/frame_host/render_frame_message_filter.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_constants.h"
 #include "content/public/common/content_switches.h"
 #include "gpu/GLES2/gl2extchromium.h"
+#include "net/cookies/cookie_options.h"
 #include "net/cookies/cookie_store.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_getter.h"
@@ -392,10 +393,13 @@ void RenderFrameMessageFilter::OnGetCookies(int render_frame_id,
   base::debug::Alias(url_buf);
 
   net::URLRequestContext* context = GetRequestContextForURL(url);
-  context->cookie_store()->GetAllCookiesForURLAsync(
-      url, base::Bind(&RenderFrameMessageFilter::CheckPolicyForCookies, this,
-                      render_frame_id, url, first_party_for_cookies,
-                      reply_msg));
+
+  net::CookieOptions options;
+  options.set_include_same_site();
+  context->cookie_store()->GetCookieListWithOptionsAsync(
+      url, options,
+      base::Bind(&RenderFrameMessageFilter::CheckPolicyForCookies, this,
+                 render_frame_id, url, first_party_for_cookies, reply_msg));
 }
 
 void RenderFrameMessageFilter::OnCookiesEnabled(
@@ -424,11 +428,8 @@ void RenderFrameMessageFilter::CheckPolicyForCookies(
       GetContentClient()->browser()->AllowGetCookie(
           url, first_party_for_cookies, cookie_list, resource_context_,
           render_process_id_, render_frame_id)) {
-    // Gets the cookies from cookie store if allowed.
-    context->cookie_store()->GetCookiesWithOptionsAsync(
-        url, net::CookieOptions(),
-        base::Bind(&RenderFrameMessageFilter::SendGetCookiesResponse,
-                   this, reply_msg));
+    SendGetCookiesResponse(reply_msg,
+                           net::CookieStore::BuildCookieLine(cookie_list));
   } else {
     SendGetCookiesResponse(reply_msg, std::string());
   }
