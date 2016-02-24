05a46b15085db24b3312b9ca384976f8221539e1
landell <landell@opera.com>
2016-02-24 01:42:16 -0800

    Include errno.h
    BUG=
    
    Review URL: https://codereview.chromium.org/1729433002
    
    Cr-Commit-Position: refs/heads/master@{#377254}

--- components/leveldb/env_mojo.cc
+++ components/leveldb/env_mojo.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/leveldb/env_mojo.h"
 
+#include <errno.h>
+
 #include "base/trace_event/trace_event.h"
 #include "third_party/leveldatabase/chromium_logger.h"
 #include "third_party/leveldatabase/src/include/leveldb/status.h"
