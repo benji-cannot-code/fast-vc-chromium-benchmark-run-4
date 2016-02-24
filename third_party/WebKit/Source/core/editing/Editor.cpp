7191eb45921c0fdfa880e06b7b95ccd447298023
tkent <tkent@chromium.org>
2016-02-23 19:48:19 -0800

    Editing: Simplify arguments of TypingCommand.
    - Make Options argument of TypingCommand::deleteKeyPressed mandatory
     All of callsites specify it.
    
    - Remove Options argument of TypingCommand::insertLineBreak
      It's always 0.
    
    - Remove Options argument of TypingCommand::insertParagraphSeparator
      It's always 0.
    
    This CL has no behavior changes.
    
    Review URL: https://codereview.chromium.org/1714843002
    
    Cr-Commit-Position: refs/heads/master@{#377212}

--- third_party/WebKit/Source/core/editing/Editor.cpp
+++ third_party/WebKit/Source/core/editing/Editor.cpp
@@ -831,7 +831,7 @@ bool Editor::insertLineBreak()
     VisiblePosition caret = frame().selection().selection().visibleStart();
     bool alignToEdge = isEndOfEditableOrNonEditableContent(caret);
     ASSERT(frame().document());
-    if (!TypingCommand::insertLineBreak(*frame().document(), 0))
+    if (!TypingCommand::insertLineBreak(*frame().document()))
         return false;
     revealSelectionAfterEditingOperation(alignToEdge ? ScrollAlignment::alignToEdgeIfNeeded : ScrollAlignment::alignCenterIfNeeded);
 
@@ -850,7 +850,7 @@ bool Editor::insertParagraphSeparator()
     bool alignToEdge = isEndOfEditableOrNonEditableContent(caret);
     ASSERT(frame().document());
     EditingState editingState;
-    if (!TypingCommand::insertParagraphSeparator(*frame().document(), 0))
+    if (!TypingCommand::insertParagraphSeparator(*frame().document()))
         return false;
     revealSelectionAfterEditingOperation(alignToEdge ? ScrollAlignment::alignToEdgeIfNeeded : ScrollAlignment::alignCenterIfNeeded);
 
