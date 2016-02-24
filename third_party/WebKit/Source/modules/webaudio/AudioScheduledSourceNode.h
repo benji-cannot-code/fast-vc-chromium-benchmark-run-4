b98c0110f36b4cdc13f6e74baf167bf13c09373e
hongchan <hongchan@chromium.org>
2016-02-24 01:14:13 -0800

    Revert of Add ScriptWrappable::hasPendingActivity to AudioSourceNode (patchset #6 id:120001 of https://codereview.chromium.org/1593763002/ )
    Reason for revert:
    There is an ordering issue in the layout test. Will fix it and retry.
    
    Original issue's description:
    > Add ScriptWrappable to SchduledSourceNode and ScriptProcessorNode
    >
    > |onended| event in OscillatorNode, AudioBufferSourceNode and
    > |onaudioprocess| event in ScriptProcessor node are not fired once the JS
    > reference of the node goes out of local scope.
    >
    > This CL is to keep those nodes from being collected when they still has
    > a pending activity. This can be done by adding
    > ScriptWrappable.hasPendingActivity() to them.
    >
    > The change in ScriptWrappable is:
    > https://codereview.chromium.org/1609343002/
    >
    > BUG=577431,484176,559220
    > TEST=LayoutTests/webaudio/audiosource-premature-gc.html
    >
    > Committed: https://crrev.com/cfa51385f9a17df78576c607827910f91f4c6683
    > Cr-Commit-Position: refs/heads/master@{#377231}
    
    TBR=rtoy@chromium.org,haraken@chromium.org,yukishiino@chromium.org,sigbjornf@opera.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=577431,484176,559220
    
    Review URL: https://codereview.chromium.org/1727333002
    
    Cr-Commit-Position: refs/heads/master@{#377247}

--- third_party/WebKit/Source/modules/webaudio/AudioScheduledSourceNode.h
+++ third_party/WebKit/Source/modules/webaudio/AudioScheduledSourceNode.h
@@ -131,9 +131,6 @@ public:
     EventListener* onended();
     void setOnended(PassRefPtrWillBeRawPtr<EventListener>);
 
-    // ScriptWrappable
-    bool hasPendingActivity() const final;
-
 protected:
     explicit AudioScheduledSourceNode(AbstractAudioContext&);
     AudioScheduledSourceHandler& audioScheduledSourceHandler() const;
