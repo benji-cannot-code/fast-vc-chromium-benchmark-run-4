bd8301c32cb2c67b2e04c2405181f3b1bf1c7e2b
tasak <tasak@google.com>
2016-02-24 01:05:13 -0800

    Added errno (or GetLastError code) to crash dump when systemAllocPages fails.
    To understand WTF::initializeWithoutV8 crashes, we need more information
    about the crashes' reason. I guess, such crashes might occur because of
    systemAllocPages (i.e. mmap or VirtualAlloc) failures.
    So added errno (or GetLastError() code) to crash dumps when
    systemAllocPages fails.
    
    BUG=584628
    
    Review URL: https://codereview.chromium.org/1718123002
    
    Cr-Commit-Position: refs/heads/master@{#377245}

--- third_party/WebKit/Source/wtf/PageAllocator.cpp
+++ third_party/WebKit/Source/wtf/PageAllocator.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if OS(POSIX)
 
+#include <errno.h>
 #include <sys/mman.h>
 
 #ifndef MADV_FREE
@@ -50,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // On POSIX memmap uses a nearby address if the hint address is blocked.
 static const bool kHintIsAdvisory = true;
+static uint32_t allocPageErrorCode = 0;
 
 #elif OS(WIN)
 
@@ -57,6 +59,7 @@ static const bool kHintIsAdvisory = true;
 
 // VirtualAlloc will fail if allocation at the hint address is blocked.
 static const bool kHintIsAdvisory = false;
+static uint32_t allocPageErrorCode = ERROR_SUCCESS;
 
 #else
 #error Unknown OS
@@ -77,11 +80,16 @@ static void* systemAllocPages(void* hint, size_t len, PageAccessibilityConfigura
 #if OS(WIN)
     DWORD accessFlag = pageAccessibility == PageAccessible ? PAGE_READWRITE : PAGE_NOACCESS;
     ret = VirtualAlloc(hint, len, MEM_RESERVE | MEM_COMMIT, accessFlag);
+    allocPageErrorCode = !ret ? GetLastError() : ERROR_SUCCESS;
 #else
     int accessFlag = pageAccessibility == PageAccessible ? (PROT_READ | PROT_WRITE) : PROT_NONE;
     ret = mmap(hint, len, accessFlag, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-    if (ret == MAP_FAILED)
+    if (ret == MAP_FAILED) {
+        allocPageErrorCode = errno;
         ret = 0;
+    } else {
+        allocPageErrorCode = 0;
+    }
 #endif
     return ret;
 }
@@ -260,6 +268,11 @@ void discardSystemPages(void* addr, size_t len)
         RELEASE_ASSERT(ret);
     }
 #endif
+}
+
+uint32_t getAllocPageErrorCode()
+{
+    return allocPageErrorCode;
 }
 
 } // namespace WTF
