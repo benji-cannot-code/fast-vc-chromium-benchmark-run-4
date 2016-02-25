f92328ec11836345e544758dcd211da52114eb71
dpapad <dpapad@chromium.org>
2016-02-24 18:09:38 -0800

    MD Settings: Manage search engines, add omnibox extension strings.
    BUG=479359
    
    Review URL: https://codereview.chromium.org/1729723005
    
    Cr-Commit-Position: refs/heads/master@{#377465}

--- chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
@@ -42,6 +42,7 @@ const char kLocalizedStringsFile[] = "strings.js";
 void AddCommonStrings(content::WebUIDataSource* html_source) {
   html_source->AddLocalizedString("add", IDS_ADD);
   html_source->AddLocalizedString("cancel", IDS_CANCEL);
+  html_source->AddLocalizedString("disable", IDS_DISABLE);
   html_source->AddLocalizedString("learnMore", IDS_LEARN_MORE);
   html_source->AddLocalizedString("ok", IDS_OK);
   html_source->AddLocalizedString("save", IDS_SAVE);
@@ -713,6 +714,9 @@ void AddSearchEnginesStrings(content::WebUIDataSource* html_source) {
       "searchEnginesOther",
       IDS_SETTINGS_SEARCH_ENGINES_OTHER_ENGINES);
   html_source->AddLocalizedString(
+      "searchEnginesExtension",
+      IDS_SETTINGS_SEARCH_ENGINES_EXTENSION_ENGINES);
+  html_source->AddLocalizedString(
       "searchEnginesSearchEngine",
       IDS_SETTINGS_SEARCH_ENGINES_SEARCH_ENGINE);
   html_source->AddLocalizedString("searchEnginesKeyword",
@@ -729,6 +733,9 @@ void AddSearchEnginesStrings(content::WebUIDataSource* html_source) {
   html_source->AddLocalizedString(
       "searchEnginesRemoveFromList",
       IDS_SETTINGS_SEARCH_ENGINES_REMOVE_FROM_LIST);
+  html_source->AddLocalizedString(
+      "searchEnginesManageExtension",
+      IDS_SETTINGS_SEARCH_ENGINES_MANAGE_EXTENSION);
 }
 
 void AddSiteSettingsStrings(content::WebUIDataSource* html_source) {
