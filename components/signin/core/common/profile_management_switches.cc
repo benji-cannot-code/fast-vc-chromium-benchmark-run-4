b4473bf842db799043d36e79b18a70381cd01190
anthonyvd <anthonyvd@chromium.org>
2016-02-25 13:59:36 -0800

    Replace custom password-separated flow switch with features API.
    This allows the feature to be controlled via Finch.
    
    BUG=533004
    
    Review URL: https://codereview.chromium.org/1733373002
    
    Cr-Commit-Position: refs/heads/master@{#377681}

--- components/signin/core/common/profile_management_switches.cc
+++ components/signin/core/common/profile_management_switches.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/common/profile_management_switches.h"
 
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/metrics/field_trial.h"
 #include "build/build_config.h"
 #include "components/signin/core/common/signin_switches.h"
@@ -130,8 +131,8 @@ bool IsNewProfileManagementPreviewEnabled() {
 }
 
 bool UsePasswordSeparatedSigninFlow() {
-  return base::CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnablePasswordSeparatedSigninFlow);
+  return base::FeatureList::IsEnabled(
+      switches::kUsePasswordSeparatedSigninFlow);
 }
 
 void EnableNewProfileManagementForTesting(base::CommandLine* command_line) {
