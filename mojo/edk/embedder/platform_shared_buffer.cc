d88841445a7408f31409b55bc3008789c3421b3c
amistry <amistry@chromium.org>
2016-02-24 22:16:52 -0800

    [mojo-edk] Add some Mach shm support to PlatformSharedBuffer.
    These functions are only used internally by Mojo and still don't allow
    users to create/pass Mach shm objects.
    
    BUG=582468
    
    Review URL: https://codereview.chromium.org/1733903002
    
    Cr-Commit-Position: refs/heads/master@{#377522}

--- mojo/edk/embedder/platform_shared_buffer.cc
+++ mojo/edk/embedder/platform_shared_buffer.cc
@@ -28,9 +28,14 @@ ScopedPlatformHandle SharedMemoryToPlatformHandle(
 #elif defined(OS_WIN)
   return ScopedPlatformHandle(PlatformHandle(memory_handle.GetHandle()));
 #else
-  CHECK_EQ(memory_handle.GetType(), base::SharedMemoryHandle::POSIX);
-  return ScopedPlatformHandle(
-      PlatformHandle(memory_handle.GetFileDescriptor().fd));
+  if (memory_handle.GetType() == base::SharedMemoryHandle::MACH) {
+    return ScopedPlatformHandle(PlatformHandle(
+        memory_handle.GetMemoryObject()));
+  } else {
+    DCHECK(memory_handle.GetType() == base::SharedMemoryHandle::POSIX);
+    return ScopedPlatformHandle(PlatformHandle(
+        memory_handle.GetFileDescriptor().fd));
+  }
 #endif
 }
 
@@ -186,6 +191,14 @@ bool PlatformSharedBuffer::InitFromPlatformHandle(
 #if defined(OS_WIN)
   base::SharedMemoryHandle handle(platform_handle.release().handle,
                                   base::GetCurrentProcId());
+#elif defined(OS_MACOSX) && !defined(OS_IOS)
+  base::SharedMemoryHandle handle;
+  if (platform_handle.get().type == PlatformHandle::Type::MACH) {
+    handle = base::SharedMemoryHandle(
+        platform_handle.release().port, num_bytes_, base::GetCurrentProcId());
+  } else {
+    handle = base::SharedMemoryHandle(platform_handle.release().handle, false);
+  }
 #else
   base::SharedMemoryHandle handle(platform_handle.release().handle, false);
 #endif
