542d5a326de28324c614568bd7051c244656c99e
kojii <kojii@chromium.org>
2016-02-24 20:35:21 -0800

    Call propagateStyleToAnonymousChildren in LayoutInline::styleDidChange
    propagateStyleToAnonymousChildren() is called from styleDidChange() in
    LayoutBlock, LayoutTable, and a few other places, but not in
    LayoutInline. Since anonymous inline-table is created for inline-level
    parent (crrev.com/366564), LayoutInline not propagating to its
    anonymous children will leave the style of the anonymous inline-table
    not being updated, and styleDidChange() for the anonymous inline-table
    not being called.
    
    This patch adds a call to propagateStyleToAnonymousChildren() in
    LayoutInline in the same way as LayoutBlock does.
    
    One example case is when a vertical flow document is changed to
    horizontal flow, the anonymous inline-table is left in vertical flow
    and thus creates two orthogonal writing-mode roots without proper
    notifications.
    
    BUG=588711
    
    Review URL: https://codereview.chromium.org/1720193004
    
    Cr-Commit-Position: refs/heads/master@{#377506}

--- third_party/WebKit/Source/core/layout/LayoutInline.cpp
+++ third_party/WebKit/Source/core/layout/LayoutInline.cpp
@@ -195,6 +195,8 @@ void LayoutInline::styleDidChange(StyleDifference diff, const ComputedStyle* old
         }
         setAlwaysCreateLineBoxes(alwaysCreateLineBoxesNew);
     }
+
+    propagateStyleToAnonymousChildren(true);
 }
 
 void LayoutInline::updateAlwaysCreateLineBoxes(bool fullLayout)
