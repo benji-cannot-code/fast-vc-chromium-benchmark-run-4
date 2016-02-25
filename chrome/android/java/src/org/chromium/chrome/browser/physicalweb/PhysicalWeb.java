d17be2317e07629c8691ec7012423ae0ab3d3fcb
agrieve <agrieve@chromium.org>
2016-02-24 19:58:50 -0800

    Revert of Foreground subscribe to URLs in ListUrlsActivity (patchset #4 id:60001 of https://codereview.chromium.org/1636663002/ )
    Reason for revert:
    Broke org.chromium.chrome.browser.physicalweb.ListUrlsActivityTest#testTapEntryOpensUrl
    
    https://bugs.chromium.org/p/chromium/issues/detail?id=589612
    
    Original issue's description:
    > Foreground subscribe to URLs in ListUrlsActivity
    >
    > Instead of reperforming a background subscription, we start a
    > foreground subscription while the ListUrlsActivity is active.
    >
    > BUG=529962
    >
    > Committed: https://crrev.com/b0c3b08b1efcc91d1587f3533faec381d06caf9b
    > Cr-Commit-Position: refs/heads/master@{#377334}
    
    TBR=mattreynolds@chromium.org,nyquist@chromium.org,cco3@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=529962
    
    Review URL: https://codereview.chromium.org/1739463003
    
    Cr-Commit-Position: refs/heads/master@{#377497}

--- chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWeb.java
+++ chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWeb.java
@@ -74,7 +74,7 @@ public class PhysicalWeb {
      */
     public static void startPhysicalWeb(ChromeApplication application) {
         PhysicalWebBleClient physicalWebBleClient = PhysicalWebBleClient.getInstance(application);
-        physicalWebBleClient.backgroundSubscribe();
+        physicalWebBleClient.subscribe();
         clearUrlsAsync(application);
     }
 
@@ -85,7 +85,7 @@ public class PhysicalWeb {
      */
     public static void stopPhysicalWeb(ChromeApplication application) {
         PhysicalWebBleClient physicalWebBleClient = PhysicalWebBleClient.getInstance(application);
-        physicalWebBleClient.backgroundUnsubscribe();
+        physicalWebBleClient.unsubscribe();
         clearUrlsAsync(application);
     }
 
--- chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWebBleClient.java
+++ chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWebBleClient.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.physicalweb;
 
-import android.app.Activity;
-
 import org.chromium.base.Log;
 import org.chromium.chrome.browser.ChromeApplication;
 
@@ -35,35 +33,18 @@ public class PhysicalWebBleClient {
     }
 
     /**
-     * Begin a background subscription to URLs broadcasted from BLE beacons.
+     * Begin subscribing to URLs broadcasted from BLE beacons.
      * This currently does nothing and should be overridden by a subclass.
      */
-    void backgroundSubscribe() {
-        Log.d(TAG, "background subscribing in empty client");
+    void subscribe() {
+        Log.d(TAG, "subscribing in empty client");
     }
 
     /**
-     * Cancel a background subscription to URLs broadcasted from BLE beacons.
+     * Cancel subscription to URLs broadcasted from BLE beacons.
      * This currently does nothing and should be overridden by a subclass.
      */
-    void backgroundUnsubscribe() {
-        Log.d(TAG, "background unsubscribing in empty client");
-    }
-
-    /**
-     * Begin a foreground subscription to URLs broadcasted from BLE beacons.
-     * This currently does nothing and should be overridden by a subclass.
-     * @param activity The Activity that is performing the scan.
-     */
-    void foregroundSubscribe(Activity activity) {
-        Log.d(TAG, "foreground subscribing in empty client");
-    }
-
-    /**
-     * Cancel a foreground subscription to URLs broadcasted from BLE beacons.
-     * This currently does nothing and should be overridden by a subclass.
-     */
-    void foregroundUnsubscribe() {
-        Log.d(TAG, "foreground unsubscribing in empty client");
+    void unsubscribe() {
+        Log.d(TAG, "unsubscribing in empty client");
     }
 }
