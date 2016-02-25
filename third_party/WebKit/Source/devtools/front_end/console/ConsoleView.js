0c85eea45fe2b98deaed796fd2e63878e359b156
lushnikov <lushnikov@chromium.org>
2016-02-25 02:17:17 -0800

    DevTools: [Console] display control characters in console commands
    BUG=586799
    R=pfeldman, dgozman
    
    Review URL: https://codereview.chromium.org/1703393002
    
    Cr-Commit-Position: refs/heads/master@{#377545}

--- third_party/WebKit/Source/devtools/front_end/console/ConsoleView.js
+++ third_party/WebKit/Source/devtools/front_end/console/ConsoleView.js
@@ -770,7 +770,7 @@ WebInspector.ConsoleView.prototype = {
         var str = this._prompt.text();
         if (!str.length)
             return;
-        this._appendCommand(str, true);
+        this._appendCommand(str.replaceControlCharacters(), true);
     },
 
     /**
