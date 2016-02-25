3d365b89d248ffa88502cbca375c1eb2dcb3dd39
nick <nick@chromium.org>
2016-02-24 19:47:10 -0800

    Use DCHECK instead of <assert.h> in scoped_ptr.h.
    base/logging actually re-#defines assert(), so whether or not your translation
    unit winds up with the one from logging.h or <assert.h> depends on #include
    order.
    
    BUG=117803
    
    Review URL: https://codereview.chromium.org/1729643002
    
    Cr-Commit-Position: refs/heads/master@{#377493}

--- base/memory/scoped_ptr.h
+++ base/memory/scoped_ptr.h
@@ -91,7 +91,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This is an implementation designed to match the anticipated future TR2
 // implementation of the scoped_ptr class.
 
-#include <assert.h>
 #include <stddef.h>
 #include <stdlib.h>
 
@@ -101,6 +100,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/compiler_specific.h"
+#include "base/logging.h"
 #include "base/macros.h"
 #include "base/move.h"
 #include "base/template_util.h"
@@ -360,13 +360,13 @@ class scoped_ptr {
   void reset(element_type* p = nullptr) { impl_.reset(p); }
 
   // Accessors to get the owned object.
-  // operator* and operator-> will assert() if there is no current object.
+  // operator* and operator-> will DCHECK() if there is no current object.
   element_type& operator*() const {
-    assert(impl_.get() != nullptr);
+    DCHECK(impl_.get() != nullptr);
     return *impl_.get();
   }
   element_type* operator->() const  {
-    assert(impl_.get() != nullptr);
+    DCHECK(impl_.get() != nullptr);
     return impl_.get();
   }
   element_type* get() const { return impl_.get(); }
@@ -473,7 +473,7 @@ class scoped_ptr<T[], D> {
 
   // Accessors to get the owned array.
   element_type& operator[](size_t i) const {
-    assert(impl_.get() != nullptr);
+    DCHECK(impl_.get() != nullptr);
     return impl_.get()[i];
   }
   element_type* get() const { return impl_.get(); }
