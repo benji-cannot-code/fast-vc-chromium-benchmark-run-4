fe69af351f73c69d1a4d7a9a3b3f43f539e4ee15
shahriar.rostami <shahriar.rostami@gmail.com>
2016-02-25 14:44:28 -0800

    Make URLRequestFileDirJob::FillReadBuffer() return an int indicating the result
    BUG=586393
    
    URLRequestFileDirJob::FillReadBuffer() to return bytes reads if it really reads data.
    If list_complete, returns list_complete_result_, and otherwise ERR_IO_PENDING.
    
    Review URL: https://codereview.chromium.org/1702713004
    
    Cr-Commit-Position: refs/heads/master@{#377689}

--- chrome/browser/file_select_helper.cc
+++ chrome/browser/file_select_helper.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/threading/worker_pool.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/platform_util.h"
