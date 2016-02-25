08484e4e13c5b311b1fbdc6e41a619e033f889f4
miu <miu@chromium.org>
2016-02-24 17:39:19 -0800

    Introduce media::AudioPushFifo and a couple of use cases (and clean-ups).
    The media::AudioPushFifo is yet another FIFO for audio data.  It is the
    "push" version of the existing media::AudioPullFifo.
    
    Two audio-handling implementations are switched over to using the new
    AudioPushFifo in this change, with a few more to follow in later
    changes:
    
    1. Cast Receiver glue: While there was no FIFO before, one will be
    required due to future refactoring work in the MediaStream audio
    framework.  In addition, a time value calculation was fixed, to adhere
    to the media::AudioCapturerSource interface contract.
    
    2. content::WebAudioCapturerSource: The code using the AudioFifo
    implementation was simplified by using AudioPushFifo instead.
    
    BUG=577881, 577874
    
    Review URL: https://codereview.chromium.org/1714593003
    
    Cr-Commit-Position: refs/heads/master@{#377457}

--- chrome/renderer/media/cast_receiver_session.cc
+++ chrome/renderer/media/cast_receiver_session.cc
@@ -170,7 +170,7 @@ void CastReceiverSession::AudioCapturerSource::Initialize(
     callback->OnCaptureError(std::string());
     return;
   }
-  audio_valve_ = new CastReceiverAudioValve(callback);
+  audio_valve_ = new CastReceiverAudioValve(params, callback);
 }
 
 void CastReceiverSession::AudioCapturerSource::Start() {
--- chrome/renderer/media/cast_rtp_stream.cc
+++ chrome/renderer/media/cast_rtp_stream.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/audio_parameters.h"
 #include "media/base/audio_bus.h"
 #include "media/base/audio_converter.h"
-#include "media/base/audio_fifo.h"
 #include "media/base/bind_to_current_loop.h"
 #include "media/base/limits.h"
 #include "media/base/video_frame.h"
