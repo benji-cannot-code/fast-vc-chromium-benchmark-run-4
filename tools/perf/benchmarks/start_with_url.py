fff2edbdc83f080eafe7111adc2d5f40edd29688
fmeawad <fmeawad@chromium.org>
2016-02-25 10:14:58 -0800

    Disabled Android failing reference builds
    Reference builds on Android was introduced recently, and this is the first
    iteration attempt to remove the failing ones.
    
    BUG=588786
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:winx64_10_perf_cq;tryserver.chromium.perf:mac_retina_perf_cq;tryserver.chromium.perf:linux_perf_cq
    
    Review URL: https://codereview.chromium.org/1738533002
    
    Cr-Commit-Position: refs/heads/master@{#377616}

--- tools/perf/benchmarks/start_with_url.py
+++ tools/perf/benchmarks/start_with_url.py
@@ -35,6 +35,7 @@ class StartWithUrlColdTBM(startup2._StartupPerfBenchmark):
 
 @benchmark.Enabled('has tabs')
 @benchmark.Enabled('android')
+@benchmark.Disabled('android-reference')  # crbug.com/588786
 @benchmark.Disabled('chromeos', 'linux', 'mac', 'win')
 class StartWithUrlWarmTBM(startup2._StartupPerfBenchmark):
   """Measures stimetime to start Chrome warm with startup URLs."""
