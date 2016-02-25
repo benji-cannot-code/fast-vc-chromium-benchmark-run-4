04f1ee4a90dd664e89f849e2d23b1de6a992e04f
zhongyi <zhongyi@chromium.org>
2016-02-25 15:46:59 -0800

    Remove commented includes from reliable_quic_stream
    BUG=
    
    Review URL: https://codereview.chromium.org/1738683002
    
    Cr-Commit-Position: refs/heads/master@{#377714}

--- net/quic/reliable_quic_stream.h
+++ net/quic/reliable_quic_stream.h
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/quic/quic_protocol.h"
 #include "net/quic/quic_stream_sequencer.h"
 #include "net/quic/quic_types.h"
-//#include "std::strings/std::stringpiece.h"
-//#include "util/refcount/reffed_ptr.h"
 // TODO(alyssar) remove this after cleaning Priority logic from this class.
 #include "net/quic/quic_write_blocked_list.h"
 
