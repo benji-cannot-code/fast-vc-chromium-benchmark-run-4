1d15d4916e4d97ab42c8deb8acb45368813e08f0
chrishtr <chrishtr@chromium.org>
2016-02-24 16:38:58 -0800

    Revert code to heuristically adjust selection rects to avoid overlap.
    It is arguably bad in other ways, even in the example given (because the second and
    third line of text don't get a full-line-height-selection), and causes the
    referenced bug.
    
    The code was originally added here:
    http://trac.webkit.org/changeset/114784
    
    BUG=570491
    
    Review URL: https://codereview.chromium.org/1727113007
    
    Cr-Commit-Position: refs/heads/master@{#377437}

--- third_party/WebKit/Source/core/layout/line/RootInlineBox.h
+++ third_party/WebKit/Source/core/layout/line/RootInlineBox.h
@@ -65,9 +65,6 @@ public:
     LayoutUnit selectionBottom() const;
     LayoutUnit selectionHeight() const { return (selectionBottom() - selectionTop()).clampNegativeToZero(); }
 
-    LayoutUnit selectionTopAdjustedForPrecedingBlock() const;
-    LayoutUnit selectionHeightAdjustedForPrecedingBlock() const { return (selectionBottom() - selectionTopAdjustedForPrecedingBlock()).clampNegativeToZero(); }
-
     LayoutUnit blockDirectionPointInLine() const;
 
     LayoutUnit alignBoxesInBlockDirection(LayoutUnit heightOfBlock, GlyphOverflowAndFallbackFontsMap&, VerticalPositionCache&);
