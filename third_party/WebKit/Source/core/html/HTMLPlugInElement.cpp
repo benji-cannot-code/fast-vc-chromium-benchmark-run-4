22e6369857ea0efb5d42303d6bcbddfdba246003
fs <fs@opera.com>
2016-02-25 04:34:30 -0800

    Don't apply the SandboxPlugins flag until we know a plugin will be used
    Move the check of the SandboxPlugins flag out of the pluginIsLoadable
    function and to just before the actual load/instantiation of the plugin
    is initiated. This means the URL and MIME-type is still subject to
    SecurityOrigin, (some) CSP and Mixed-Content checks, but the flag will
    not block resources if they will not use a plugin.
    
    Split pluginIsLoadable into one part that checks if the URL/MIME-type is
    allowed, and one part that checks if the plugin itself can be
    loaded/instantiated. The former is allowedToLoadObject while the latter
    is allowedToLoadPlugin. Only call the latter if we determine that a
    plugin will be used to view the content (URL or not). Sink the
    allowedToLoadPlugin check into loadPlugin, which in turn means it will
    apply to the code-path through createPluginWithoutLayoutObject() as well,
    while adding a call to allowedToLoadObject there as well.
    
    Also make sure that shouldUsePlugin() sets the |useFallback| out-
    variable before all returns. (Could previously be used uninitialized.
    Found by code inspection.)
    
    BUG=578916
    
    Review URL: https://codereview.chromium.org/1645313002
    
    Cr-Commit-Position: refs/heads/master@{#377559}

--- third_party/WebKit/Source/core/html/HTMLPlugInElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLPlugInElement.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLImageLoader.h"
 #include "core/html/PluginDocument.h"
 #include "core/input/EventHandler.h"
+#include "core/inspector/ConsoleMessage.h"
 #include "core/layout/LayoutBlockFlow.h"
 #include "core/layout/LayoutEmbeddedObject.h"
 #include "core/layout/LayoutImage.h"
@@ -206,6 +207,10 @@ void HTMLPlugInElement::createPluginWithoutLayoutObject()
     ASSERT(document().frame()->loader().client()->canCreatePluginWithoutRenderer(m_serviceType));
 
     KURL url;
+    // CSP can block src-less objects.
+    if (!allowedToLoadObject(url, m_serviceType))
+        return;
+
     Vector<String> paramNames;
     Vector<String> paramValues;
 
@@ -475,24 +480,27 @@ bool HTMLPlugInElement::requestObject(const String& url, const String& mimeType,
         return false;
 
     KURL completedURL = url.isEmpty() ? KURL() : document().completeURL(url);
-    if (!pluginIsLoadable(completedURL, mimeType))
+    if (!allowedToLoadObject(completedURL, mimeType))
         return false;
 
     bool useFallback;
-    if (shouldUsePlugin(completedURL, mimeType, hasFallbackContent(), useFallback))
-        return loadPlugin(completedURL, mimeType, paramNames, paramValues, useFallback, true);
+    if (!shouldUsePlugin(completedURL, mimeType, hasFallbackContent(), useFallback)) {
+        // If the plugin element already contains a subframe,
+        // loadOrRedirectSubframe will re-use it. Otherwise, it will create a
+        // new frame and set it as the LayoutPart's widget, causing what was
+        // previously in the widget to be torn down.
+        return loadOrRedirectSubframe(completedURL, getNameAttribute(), true);
+    }
 
-    // If the plugin element already contains a subframe,
-    // loadOrRedirectSubframe will re-use it. Otherwise, it will create a new
-    // frame and set it as the LayoutPart's widget, causing what was previously
-    // in the widget to be torn down.
-    return loadOrRedirectSubframe(completedURL, getNameAttribute(), true);
+    return loadPlugin(completedURL, mimeType, paramNames, paramValues, useFallback, true);
 }
 
 bool HTMLPlugInElement::loadPlugin(const KURL& url, const String& mimeType, const Vector<String>& paramNames, const Vector<String>& paramValues, bool useFallback, bool requireLayoutObject)
 {
-    LocalFrame* frame = document().frame();
+    if (!allowedToLoadPlugin(url, mimeType))
+        return false;
 
+    LocalFrame* frame = document().frame();
     if (!frame->loader().allowPlugins(AboutToInstantiatePlugin))
         return false;
 
@@ -542,8 +550,10 @@ bool HTMLPlugInElement::shouldUsePlugin(const KURL& url, const String& mimeType,
     if (document().frame()->page() && (mimeType == "image/tiff" || mimeType == "image/tif" || mimeType == "image/x-tiff")) {
         const PluginData* pluginData = document().frame()->page()->pluginData();
         String pluginName = pluginData ? pluginData->pluginNameForMimeType(mimeType) : String();
-        if (!pluginName.isEmpty() && !pluginName.contains("QuickTime", TextCaseInsensitive))
+        if (!pluginName.isEmpty() && !pluginName.contains("QuickTime", TextCaseInsensitive)) {
+            useFallback = false;
             return true;
+        }
     }
 
     ObjectContentType objectType = document().frame()->loader().client()->objectContentType(url, mimeType, shouldPreferPlugInsForImages());
@@ -562,7 +572,7 @@ void HTMLPlugInElement::dispatchErrorEvent()
         dispatchEvent(Event::create(EventTypeNames::error));
 }
 
-bool HTMLPlugInElement::pluginIsLoadable(const KURL& url, const String& mimeType)
+bool HTMLPlugInElement::allowedToLoadObject(const KURL& url, const String& mimeType)
 {
     if (url.isEmpty() && mimeType.isEmpty())
         return false;
@@ -573,9 +583,6 @@ bool HTMLPlugInElement::pluginIsLoadable(const KURL& url, const String& mimeType
         return false;
 
     if (MIMETypeRegistry::isJavaAppletMIMEType(mimeType))
-        return false;
-
-    if (document().isSandboxed(SandboxPlugins))
         return false;
 
     if (!document().securityOrigin()->canDisplay(url)) {
@@ -591,8 +598,19 @@ bool HTMLPlugInElement::pluginIsLoadable(const KURL& url, const String& mimeType
         layoutEmbeddedObject()->setPluginUnavailabilityReason(LayoutEmbeddedObject::PluginBlockedByContentSecurityPolicy);
         return false;
     }
-
+    // If the URL is empty, a plugin could still be instantiated if a MIME-type
+    // is specified.
     return (!mimeType.isEmpty() && url.isEmpty()) || !MixedContentChecker::shouldBlockFetch(frame, WebURLRequest::RequestContextObject, WebURLRequest::FrameTypeNone, url);
+}
+
+bool HTMLPlugInElement::allowedToLoadPlugin(const KURL& url, const String& mimeType)
+{
+    if (document().isSandboxed(SandboxPlugins)) {
+        document().addConsoleMessage(ConsoleMessage::create(SecurityMessageSource, ErrorMessageLevel,
+            "Failed to load '" + url.elidedString() + "' as a plugin, because the frame into which the plugin is loading is sandboxed."));
+        return false;
+    }
+    return true;
 }
 
 void HTMLPlugInElement::didAddUserAgentShadowRoot(ShadowRoot&)
