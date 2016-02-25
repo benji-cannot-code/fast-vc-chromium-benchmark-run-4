37383971e37d572eb11a328047ea0b756e62eedd
haraken <haraken@chromium.org>
2016-02-25 06:48:57 -0800

    Oilpan: Decommit backing storage of CallbackStacks
    CallbackStacks are used only while Oilpan's GC is doing marking & weak processing.
    However, currently each CallbackStack continues retaining one Block forever.
    
    This wastes memory a lot. Oilpan has 4 global CallbackStacks and 1 CallbackStack per thread.
    Each Block consumes 8192 * sizeof(Item) = 128 KB. This means that Oilpan wastes 128 KB * (4 + # of threads).
    When I start Chrome's new tab page, it creates 18 CallbackStacks,
    meaning that it wastes 2.3 MB of memory.
    
    This CL removes the waste by discarding system pages of the Block after finishing every GC phase.
    
    BUG=
    
    Committed: https://crrev.com/34393519671e62e5a66602736899b94ad33dd45a
    Cr-Commit-Position: refs/heads/master@{#374674}
    
    Review URL: https://codereview.chromium.org/1686943002
    
    Cr-Commit-Position: refs/heads/master@{#377575}

--- third_party/WebKit/Source/platform/heap/Heap.cpp
+++ third_party/WebKit/Source/platform/heap/Heap.cpp
@@ -305,6 +305,14 @@ bool Heap::weakTableRegistered(const void* table)
 }
 #endif
 
+void Heap::decommitCallbackStacks()
+{
+    s_markingStack->decommit();
+    s_postMarkingCallbackStack->decommit();
+    s_globalWeakCallbackStack->decommit();
+    s_ephemeronStack->decommit();
+}
+
 void Heap::preGC()
 {
     ASSERT(!ThreadState::current()->isInGC());
@@ -421,6 +429,7 @@ void Heap::collectGarbage(BlinkGC::StackState stackState, BlinkGC::GCType gcType
     WTF::Partitions::reportMemoryUsageHistogram();
 
     postGC(gcType);
+    Heap::decommitCallbackStacks();
 
 #if ENABLE(ASSERT)
     // 0 is used to figure non-assigned area, so avoid to use 0 in s_gcGeneration.
@@ -463,6 +472,7 @@ void Heap::collectGarbageForTerminatingThread(ThreadState* state)
         globalWeakProcessing(visitorScope.visitor());
 
         state->postGC(BlinkGC::GCWithSweep);
+        Heap::decommitCallbackStacks();
     }
     state->preSweep();
 }
@@ -498,8 +508,6 @@ void Heap::postMarkingProcessing(Visitor* visitor)
     // 2. the markNoTracing callbacks on collection backings to mark them
     //    if they are only reachable from their front objects.
     while (popAndInvokePostMarkingCallback(visitor)) { }
-
-    s_ephemeronStack->clear();
 
     // Post-marking callbacks should not trace any objects and
     // therefore the marking stack should be empty after the
