cf7f1da4b3fbe162788f32d4897a4ca9d204064e
atwilson <atwilson@chromium.org>
2016-02-25 03:52:24 -0800

    Reland: No longer start up profile if there was an error fetching policy.
    Changed UserCloudPolicyManagerChromeOS to no longer complete profile
    initialization if there is an error fetching policy. Instead, we shutdown the
    user and force a policy load the next time we try to startup that user.
    
    BUG=532317
    TBR=tnagel@chromium.org
    
    Review URL: https://codereview.chromium.org/1735863002
    
    Cr-Commit-Position: refs/heads/master@{#377558}

--- chrome/browser/chromeos/policy/user_policy_test_helper.h
+++ chrome/browser/chromeos/policy/user_policy_test_helper.h
@@ -50,6 +50,8 @@ class UserPolicyTestHelper {
                     const base::DictionaryValue& recommended_policy,
                     Profile* profile);
 
+  void DeletePolicyFile();
+
  private:
   void WritePolicyFile(const base::DictionaryValue& mandatory,
                        const base::DictionaryValue& recommended);
