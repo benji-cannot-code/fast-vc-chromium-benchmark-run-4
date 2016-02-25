53398eb762dca8d37c2efb91b62acdf7bb103d35
nhiroki <nhiroki@chromium.org>
2016-02-24 20:57:35 -0800

    ServiceWorker: Move code for WindowClient.focus() into service_worker_client_utils for cleanup
    Code related to client operations (focus, openWindow, navigate, etc) should be
    located in service_worker_client_utils in terms of modularity.
    
    BUG=n/a
    
    Review URL: https://codereview.chromium.org/1691263002
    
    Cr-Commit-Position: refs/heads/master@{#377511}

--- content/browser/service_worker/service_worker_client_utils.h
+++ content/browser/service_worker/service_worker_client_utils.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/memory/weak_ptr.h"
-#include "content/browser/service_worker/service_worker_provider_host.h"
 #include "content/common/service_worker/service_worker_status_code.h"
 
 class GURL;
@@ -19,6 +18,7 @@ class GURL;
 namespace content {
 
 class ServiceWorkerContextCore;
+class ServiceWorkerProviderHost;
 class ServiceWorkerVersion;
 struct ServiceWorkerClientInfo;
 struct ServiceWorkerClientQueryOptions;
@@ -28,8 +28,15 @@ namespace service_worker_client_utils {
 using NavigationCallback =
     base::Callback<void(ServiceWorkerStatusCode status,
                         const ServiceWorkerClientInfo& client_info)>;
+using ClientCallback =
+    base::Callback<void(const ServiceWorkerClientInfo& client_info)>;
 using ServiceWorkerClients = std::vector<ServiceWorkerClientInfo>;
 using ClientsCallback = base::Callback<void(ServiceWorkerClients* clients)>;
+
+// Focuses the window client associated with |provider_host|. |callback| is
+// called with the client information on completion.
+void FocusWindowClient(ServiceWorkerProviderHost* provider_host,
+                       const ClientCallback& callback);
 
 // Opens a new window and navigates it to |url|. |callback| is called with the
 // window's client information on completion.
@@ -50,11 +57,10 @@ void NavigateClient(const GURL& url,
 
 // Gets a client matched by |client_uuid|. |callback| is called with the client
 // information on completion.
-void GetClient(
-    const base::WeakPtr<ServiceWorkerVersion>& controller,
-    const std::string& client_uuid,
-    const base::WeakPtr<ServiceWorkerContextCore>& context,
-    const ServiceWorkerProviderHost::GetClientInfoCallback& callback);
+void GetClient(const base::WeakPtr<ServiceWorkerVersion>& controller,
+               const std::string& client_uuid,
+               const base::WeakPtr<ServiceWorkerContextCore>& context,
+               const ClientCallback& callback);
 
 // Collects clients matched with |options|. |callback| is called with the client
 // information sorted in MRU order (most recently focused order) on completion.
