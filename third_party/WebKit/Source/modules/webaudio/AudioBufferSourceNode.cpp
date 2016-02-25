b630e2e1681c155d8bd741ffaf98ea795573c4ab
philipj <philipj@opera.com>
2016-02-24 20:35:42 -0800

    Remove dead code related to Web Audio doppler effects
    Doppler already has no effect on AudioBufferSourceNodes, as the only
    call to setPannerNode was removed in time for M41:
    https://codereview.chromium.org/783273002/
    
    PannerHandler::dopplerRate becomes unreachable next, and pulling at the
    thread of unreachable things ends up removing quite a lot.
    
    PannerNode's setVelocity is revealed to have no effect at all, although
    it remains in the spec. A spec issue was filed:
    https://github.com/WebAudio/web-audio-api/issues/730
    
    BUG=439644
    R=rtoy@chromium.org
    
    Review URL: https://codereview.chromium.org/1734483002
    
    Cr-Commit-Position: refs/heads/master@{#377507}

--- third_party/WebKit/Source/modules/webaudio/AudioBufferSourceNode.cpp
+++ third_party/WebKit/Source/modules/webaudio/AudioBufferSourceNode.cpp
@@ -80,7 +80,6 @@ PassRefPtr<AudioBufferSourceHandler> AudioBufferSourceHandler::create(AudioNode&
 
 AudioBufferSourceHandler::~AudioBufferSourceHandler()
 {
-    clearPannerNode();
     uninitialize();
 }
 
@@ -508,10 +507,6 @@ void AudioBufferSourceHandler::startSource(double when, double grainOffset, doub
 
 double AudioBufferSourceHandler::computePlaybackRate()
 {
-    double dopplerRate = 1;
-    if (m_pannerNode)
-        dopplerRate = m_pannerNode->dopplerRate();
-
     // Incorporate buffer's sample-rate versus AbstractAudioContext's sample-rate.
     // Normally it's not an issue because buffers are loaded at the
     // AbstractAudioContext's sample-rate, but we can handle it in any case.
@@ -525,7 +520,7 @@ double AudioBufferSourceHandler::computePlaybackRate()
     // AudioSummingJunction from m_playbackRate AudioParam.
     double basePlaybackRate = m_playbackRate->finalValue();
 
-    double finalPlaybackRate = dopplerRate * sampleRateFactor * basePlaybackRate;
+    double finalPlaybackRate = sampleRateFactor * basePlaybackRate;
 
     // Take the detune value into account for the final playback rate.
     finalPlaybackRate *= pow(2, m_detune->finalValue() / 1200);
@@ -549,26 +544,6 @@ double AudioBufferSourceHandler::computePlaybackRate()
 bool AudioBufferSourceHandler::propagatesSilence() const
 {
     return !isPlayingOrScheduled() || hasFinished() || !m_buffer;
-}
-
-void AudioBufferSourceHandler::setPannerNode(PannerHandler* pannerNode)
-{
-    if (m_pannerNode != pannerNode && !hasFinished()) {
-        RefPtr<PannerHandler> oldPannerNode(m_pannerNode.release());
-        m_pannerNode = pannerNode;
-        if (pannerNode)
-            pannerNode->makeConnection();
-        if (oldPannerNode)
-            oldPannerNode->breakConnection();
-    }
-}
-
-void AudioBufferSourceHandler::clearPannerNode()
-{
-    if (m_pannerNode) {
-        m_pannerNode->breakConnection();
-        m_pannerNode.clear();
-    }
 }
 
 void AudioBufferSourceHandler::handleStoppableSourceNode()
@@ -605,13 +580,6 @@ void AudioBufferSourceHandler::handleStoppableSourceNode()
             finishWithoutOnEnded();
         }
     }
-}
-
-void AudioBufferSourceHandler::finish()
-{
-    clearPannerNode();
-    ASSERT(!m_pannerNode);
-    AudioScheduledSourceHandler::finish();
 }
 
 // ----------------------------------------------------------------
@@ -711,4 +679,3 @@ void AudioBufferSourceNode::start(double when, double grainOffset, double grainD
 }
 
 } // namespace blink
-
