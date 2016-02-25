50deba362856edd90d9caa749f83eb6a04dad014
dcastagna <dcastagna@chromium.org>
2016-02-25 14:00:37 -0800

    Revert of gpu: Expose internal format R8 instead of RED. (patchset #8 id:140001 of https://codereview.chromium.org/1708263002/ )
    Reason for revert:
    This broke videos on windows: crbug.com/589775
    
    Original issue's description:
    > gpu: Expose internal format R8 instead of RED.
    >
    > As noted in crrev.com/1703153002 GL_R8 sized internalformat should be preferred to
    > GL_RED.
    >
    > This CL lets the users of the command buffer use GL_R8, and it takes care of
    > converting it to GL_RED when not supported by the underlying GL context.
    >
    > BUG=
    > CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel
    >
    > Committed: https://crrev.com/7b277c596c517ce4a5d0985ace0da1806f2470f4
    > Cr-Commit-Position: refs/heads/master@{#377222}
    
    TBR=dalecurtis@chromium.org,reveman@chromium.org,zmo@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=
    
    Review URL: https://codereview.chromium.org/1741513002
    
    Cr-Commit-Position: refs/heads/master@{#377682}

--- ui/gl/gl_image_io_surface.mm
+++ ui/gl/gl_image_io_surface.mm
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_context.h"
 #include "ui/gl/gl_helper.h"
-#include "ui/gl/gl_version_info.h"
 #include "ui/gl/scoped_binders.h"
 
 // Note that this must be included after gl_bindings.h to avoid conflicts.
@@ -70,7 +69,7 @@ STRINGIZE(
 
 bool ValidInternalFormat(unsigned internalformat) {
   switch (internalformat) {
-    case GL_R8:
+    case GL_RED:
     case GL_BGRA_EXT:
     case GL_RGB:
     case GL_RGB_YCBCR_420V_CHROMIUM:
@@ -109,9 +108,7 @@ bool ValidFormat(BufferFormat format) {
 GLenum TextureFormat(BufferFormat format) {
   switch (format) {
     case BufferFormat::R_8:
-      return gfx::GLContext::GetCurrent()->GetVersionInfo()->IsES3Capable()
-                 ? GL_R8
-                 : GL_RED;
+      return GL_RED;
     case BufferFormat::BGRA_8888:
     case BufferFormat::RGBA_8888:
       return GL_RGBA;
