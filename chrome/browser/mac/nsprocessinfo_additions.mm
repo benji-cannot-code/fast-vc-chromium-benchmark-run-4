fb7eba2242bfcd9ba46a41441bf4fa5656695cc5
shrike <shrike@chromium.org>
2016-02-25 09:46:22 -0800

    Filter redundant Safari Services from contextual menus.
    R=tapted@chromium.org
    BUG=531614
    
    Committed: https://crrev.com/1e0f05c282f4cc8702a66ca1f8b6e4e4595cb7bb
    Cr-Commit-Position: refs/heads/master@{#376258}
    
    Review URL: https://codereview.chromium.org/1688983002
    
    Cr-Commit-Position: refs/heads/master@{#377605}

---
+++ chrome/browser/mac/nsprocessinfo_additions.mm
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "chrome/browser/mac/nsprocessinfo_additions.h"
+
+#import "content/public/common/content_switches.h"
+
+@implementation NSProcessInfo(ChromeAdditions)
+
+- (BOOL)cr_isMainBrowserOrTestProcess {
+  NSString* processTypeString =
+      [NSString stringWithFormat:@"--%s=", switches::kProcessType];
+
+  for (NSString *argument in [self arguments]) {
+    if ([argument hasPrefix:processTypeString])
+      return NO;
+  }
+  return YES;
+}
+
+@end
