fdda036834dd056a9dd6f411bed78d8622a54a02
jrummell <jrummell@chromium.org>
2016-02-25 17:18:38 -0800

    Add checked_cast to avoid Windows compiler errors
    BUG=589852
    TEST=compiles
    
    Review URL: https://codereview.chromium.org/1725123006
    
    Cr-Commit-Position: refs/heads/master@{#377748}

--- media/base/container_names_fuzzertest.cc
+++ media/base/container_names_fuzzertest.cc
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include "base/numerics/safe_conversions.h"
 #include "media/base/container_names.h"
 
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  media::container_names::DetermineContainer(data, size);
+  media::container_names::DetermineContainer(data,
+                                             base::checked_cast<int>(size));
   return 0;
 }
