6fc27c0f3f07882c237122e360138bf7bededfbf
chrishtr <chrishtr@chromium.org>
2016-02-26 12:18:38 -0800

    Rename ValueToString<*>::string to ValueToType<*>::toString
    This should avoid a collision between Blink and Chromium.
    
    Review URL: https://codereview.chromium.org/1742653002
    
    Cr-Commit-Position: refs/heads/master@{#377958}

--- third_party/WebKit/Source/platform/PODIntervalTreeTest.cpp
+++ third_party/WebKit/Source/platform/PODIntervalTreeTest.cpp
@@ -42,12 +42,12 @@ using TreeTestHelpers::nextRandom;
 #ifndef NDEBUG
 template<>
 struct ValueToString<float> {
-    static String string(const float& value) { return String::number(value); }
+    static String toString(const float& value) { return String::number(value); }
 };
 
 template<>
 struct ValueToString<void*> {
-    static String string(void* const& value)
+    static String toString(void* const& value)
     {
         return String::format("0x%p", value);
     }
@@ -88,7 +88,7 @@ TEST(PODIntervalTreeTest, TestQueryAgainstZeroSizeInterval)
 #ifndef NDEBUG
 template<>
 struct ValueToString<int*> {
-    static String string(int* const& value)
+    static String toString(int* const& value)
     {
         return String::format("0x%p", value);
     }
@@ -131,7 +131,7 @@ public:
 #ifndef NDEBUG
 template<>
 struct ValueToString<UserData1> {
-    static String string(const UserData1& value)
+    static String toString(const UserData1& value)
     {
         return String("[UserData1 a=") + String::number(value.a) + " b=" + String::number(value.b) + "]";
     }
@@ -188,7 +188,7 @@ private:
 #ifndef NDEBUG
 template<>
 struct ValueToString<EndpointType1> {
-    static String string(const EndpointType1& value)
+    static String toString(const EndpointType1& value)
     {
         return String("[EndpointType1 value=") + String::number(value.value()) + "]";
     }
@@ -209,7 +209,7 @@ TEST(PODIntervalTreeTest, TestTreeDoesNotRequireMostOperators)
 #ifndef NDEBUG
 template<>
 struct ValueToString<int> {
-    static String string(const int& value) { return String::number(value); }
+    static String toString(const int& value) { return String::number(value); }
 };
 #endif
 
