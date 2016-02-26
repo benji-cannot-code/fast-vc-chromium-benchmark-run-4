78d291d8b7ba0e75ff97acefa0ef2eaa2c89e803
yzshen <yzshen@chromium.org>
2016-02-25 21:46:21 -0800

    Revert of Mojo bindings environment: remove usage in gles2. (patchset #1 id:1 of https://codereview.chromium.org/1725123002/ )
    Reason for revert:
    This causes crashes: https://bugs.chromium.org/p/chromium/issues/detail?id=589964
    
    The shell and the application belong to different modules, so they have different MessageLoop::current() / MessagePumpMojo::current() / etc. Although these values are in thread-local storage, we refer to them using global variables which are different in different modules.
    
    Original issue's description:
    > Mojo bindings environment: remove usage in gles2.
    >
    > BUG=585942
    >
    > Committed: https://crrev.com/67adb0d1a11573b9d0bd4fbe144f5e979327009d
    > Cr-Commit-Position: refs/heads/master@{#377314}
    
    TBR=sky@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=585942
    
    Review URL: https://codereview.chromium.org/1732373003
    
    Cr-Commit-Position: refs/heads/master@{#377822}

--- components/mus/public/cpp/lib/context_provider.cc
+++ components/mus/public/cpp/lib/context_provider.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "mojo/gles2/gles2_context.h"
 #include "mojo/gpu/mojo_gles2_impl_autogen.h"
+#include "mojo/public/cpp/environment/environment.h"
 
 namespace mus {
 
@@ -25,7 +26,8 @@ ContextProvider::ContextProvider(
 bool ContextProvider::BindToCurrentThread() {
   DCHECK(command_buffer_handle_.is_valid());
   context_ = MojoGLES2CreateContext(command_buffer_handle_.release().value(),
-                                    nullptr, &ContextLostThunk, this);
+                                    nullptr, &ContextLostThunk, this,
+                                    mojo::Environment::GetDefaultAsyncWaiter());
   context_gl_.reset(new mojo::MojoGLES2Impl(context_));
   return !!context_;
 }
