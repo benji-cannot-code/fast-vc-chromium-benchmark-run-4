2dcf26bae948252ade4f07e63edc6e886592afae
dkrahn <dkrahn@chromium.org>
2016-02-26 00:12:29 -0800

    Copy challenge*Key methods to enterprise.platformKeys.
    Up to this point the challengeMachineKey and challengeUserKey methods
    were only available through enterprise.platformKeysPrivate because the
    web APIs required to meaningfully use these methods had not been
    published (and now have).
    
    The methods will remain in enterprise.platformKeysPrivate until all
    users have been migrated to use enterprise.platformKeys.
    
    The implementation is identical and has been abstracted so it can be
    invoked from either entry point.
    
    BUG=chromium:329341
    TEST=unit tests extended to test both entry points
    
    Review URL: https://codereview.chromium.org/1685103003
    
    Cr-Commit-Position: refs/heads/master@{#377845}

--- chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api.h
+++ chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api.h
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
-#include "chrome/browser/extensions/chrome_extension_function.h"
+#include "chrome/browser/extensions/api/enterprise_platform_keys_private/enterprise_platform_keys_private_api.h"
+#include "extensions/browser/extension_function.h"
 
 namespace net {
 class X509Certificate;
@@ -21,7 +22,7 @@ typedef std::vector<scoped_refptr<X509Certificate> > CertificateList;
 namespace extensions {
 
 class EnterprisePlatformKeysInternalGenerateKeyFunction
-    : public ChromeUIThreadExtensionFunction {
+    : public UIThreadExtensionFunction {
  private:
   ~EnterprisePlatformKeysInternalGenerateKeyFunction() override;
   ResponseAction Run() override;
@@ -36,7 +37,7 @@ class EnterprisePlatformKeysInternalGenerateKeyFunction
 };
 
 class EnterprisePlatformKeysGetCertificatesFunction
-    : public ChromeUIThreadExtensionFunction {
+    : public UIThreadExtensionFunction {
  private:
   ~EnterprisePlatformKeysGetCertificatesFunction() override;
   ResponseAction Run() override;
@@ -51,7 +52,7 @@ class EnterprisePlatformKeysGetCertificatesFunction
 };
 
 class EnterprisePlatformKeysImportCertificateFunction
-    : public ChromeUIThreadExtensionFunction {
+    : public UIThreadExtensionFunction {
  private:
   ~EnterprisePlatformKeysImportCertificateFunction() override;
   ResponseAction Run() override;
@@ -65,7 +66,7 @@ class EnterprisePlatformKeysImportCertificateFunction
 };
 
 class EnterprisePlatformKeysRemoveCertificateFunction
-    : public ChromeUIThreadExtensionFunction {
+    : public UIThreadExtensionFunction {
  private:
   ~EnterprisePlatformKeysRemoveCertificateFunction() override;
   ResponseAction Run() override;
@@ -79,7 +80,7 @@ class EnterprisePlatformKeysRemoveCertificateFunction
 };
 
 class EnterprisePlatformKeysInternalGetTokensFunction
-    : public ChromeUIThreadExtensionFunction {
+    : public UIThreadExtensionFunction {
  private:
   ~EnterprisePlatformKeysInternalGetTokensFunction() override;
   ResponseAction Run() override;
@@ -91,6 +92,52 @@ class EnterprisePlatformKeysInternalGetTokensFunction
 
   DECLARE_EXTENSION_FUNCTION("enterprise.platformKeysInternal.getTokens",
                              ENTERPRISE_PLATFORMKEYSINTERNAL_GETTOKENS);
+};
+
+class EnterprisePlatformKeysChallengeMachineKeyFunction
+    : public UIThreadExtensionFunction {
+ public:
+  EnterprisePlatformKeysChallengeMachineKeyFunction();
+  explicit EnterprisePlatformKeysChallengeMachineKeyFunction(
+      EPKPChallengeMachineKey* impl_for_testing);
+
+ private:
+  ~EnterprisePlatformKeysChallengeMachineKeyFunction() override;
+  ResponseAction Run() override;
+
+  // Called when the challenge operation is complete. If the operation succeeded
+  // |success| will be true and |data| will contain the challenge response data.
+  // Otherwise |success| will be false and |data| is an error message.
+  void OnChallengedKey(bool success, const std::string& data);
+
+  scoped_ptr<EPKPChallengeMachineKey> default_impl_;
+  EPKPChallengeMachineKey* impl_;
+
+  DECLARE_EXTENSION_FUNCTION("enterprise.platformKeys.challengeMachineKey",
+                             ENTERPRISE_PLATFORMKEYS_CHALLENGEMACHINEKEY);
+};
+
+class EnterprisePlatformKeysChallengeUserKeyFunction
+    : public UIThreadExtensionFunction {
+ public:
+  EnterprisePlatformKeysChallengeUserKeyFunction();
+  explicit EnterprisePlatformKeysChallengeUserKeyFunction(
+      EPKPChallengeUserKey* impl_for_testing);
+
+ private:
+  ~EnterprisePlatformKeysChallengeUserKeyFunction() override;
+  ResponseAction Run() override;
+
+  // Called when the challenge operation is complete. If the operation succeeded
+  // |success| will be true and |data| will contain the challenge response data.
+  // Otherwise |success| will be false and |data| is an error message.
+  void OnChallengedKey(bool success, const std::string& data);
+
+  scoped_ptr<EPKPChallengeUserKey> default_impl_;
+  EPKPChallengeUserKey* impl_;
+
+  DECLARE_EXTENSION_FUNCTION("enterprise.platformKeys.challengeUserKey",
+                             ENTERPRISE_PLATFORMKEYS_CHALLENGEUSERKEY);
 };
 
 }  // namespace extensions
--- chrome/chrome_tests_unit.gypi
+++ chrome/chrome_tests_unit.gypi
@@ -1110,6 +1110,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/chromeos/ui/accessibility_focus_ring_controller_unittest.cc',
       'browser/chromeos/ui/idle_app_name_notification_view_unittest.cc',
       'browser/download/notification/download_item_notification_unittest.cc',
+      'browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api_unittest.cc',
       'browser/extensions/api/enterprise_platform_keys_private/enterprise_platform_keys_private_api_unittest.cc',
       'browser/extensions/api/log_private/syslog_parser_unittest.cc',
       'browser/extensions/updater/local_extension_cache_unittest.cc',
