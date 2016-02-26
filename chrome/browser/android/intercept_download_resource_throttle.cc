9df9c94ec070564c3ac06014cda9cc92c17d39c1
qinmin <qinmin@chromium.org>
2016-02-25 16:24:21 -0800

    add a flag to enable/disable download interception on android
    Currently some of the download are passed to Android DownloadManager.
    We might change that behavior in the future.
    This CL adds a flag to allow us to toggle that behavior.
    Chrome flag and finch flag "EnableDownloadInterception" are added to allow locally/remotely set/unset the flag
    
    BUG=545640
    
    Review URL: https://codereview.chromium.org/1692693003
    
    Cr-Commit-Position: refs/heads/master@{#377724}

--- chrome/browser/android/intercept_download_resource_throttle.cc
+++ chrome/browser/android/intercept_download_resource_throttle.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/android/intercept_download_resource_throttle.h"
 
+#include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
+#include "chrome/browser/android/chrome_feature_list.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_headers.h"
 #include "content/public/browser/android/download_controller_android.h"
 #include "content/public/browser/resource_controller.h"
@@ -42,6 +44,11 @@ void RecordInterceptFailureReasons(
 
 namespace chrome {
 
+// static
+bool InterceptDownloadResourceThrottle::IsDownloadInterceptionEnabled() {
+  return base::FeatureList::IsEnabled(chrome::android::kSystemDownloadManager);
+}
+
 InterceptDownloadResourceThrottle::InterceptDownloadResourceThrottle(
     net::URLRequest* request,
     int render_process_id,
@@ -65,6 +72,9 @@ const char* InterceptDownloadResourceThrottle::GetNameForLogging() const {
 }
 
 void InterceptDownloadResourceThrottle::ProcessDownloadRequest() {
+  if (!IsDownloadInterceptionEnabled())
+    return;
+
   if (request_->url_chain().empty()) {
     RecordInterceptFailureReasons(EMPTY_URL);
     return;
