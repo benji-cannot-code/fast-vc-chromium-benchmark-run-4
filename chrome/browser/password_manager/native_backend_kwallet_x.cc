467d891168e078cc0e34e1487bc2eaa3c2cd1ab7
mkwst <mkwst@chromium.org>
2016-02-26 06:56:37 -0800

    CREDENTIAL: Disable auto sign-in by default for existing credentials.
    The changes to libsecret and gnome backends are fairly hideous, but I am assured
    that this is How Things are Done. *sigh*
    
    BUG=587440
    R=vasilii@chromium.org
    
    Review URL: https://codereview.chromium.org/1734193003
    
    Cr-Commit-Position: refs/heads/master@{#377884}

--- chrome/browser/password_manager/native_backend_kwallet_x.cc
+++ chrome/browser/password_manager/native_backend_kwallet_x.cc
@@ -38,7 +38,7 @@ namespace {
 
 // In case the fields in the pickle ever change, version them so we can try to
 // read old pickles. (Note: do not eat old pickles past the expiration date.)
-const int kPickleVersion = 7;
+const int kPickleVersion = 8;
 
 // We could localize this string, but then changing your locale would cause
 // you to lose access to all your stored passwords. Maybe best not to do that.
@@ -222,6 +222,8 @@ bool DeserializeValueSize(const std::string& signon_realm,
         LogDeserializationWarning(version, signon_realm, false);
         return false;
       }
+      if (version <= 7)
+        form->skip_zero_click = true;
     }
 
     if (version > 4) {
--- components/autofill/core/common/password_form.cc
+++ components/autofill/core/common/password_form.cc
@@ -80,7 +80,7 @@ PasswordForm::PasswordForm()
       type(TYPE_MANUAL),
       times_used(0),
       generation_upload_status(NO_SIGNAL_SENT),
-      skip_zero_click(false),
+      skip_zero_click(true),
       layout(Layout::LAYOUT_OTHER),
       was_parsed_using_autofill_predictions(false),
       is_public_suffix_match(false),
