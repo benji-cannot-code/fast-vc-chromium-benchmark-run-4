9787e4e032cc8760846904df8e92bb8401382e27
bzanotti <bzanotti@chromium.org>
2016-02-26 01:34:01 -0800

    Change X-CHROME-CONNECTED cookie name to CHROME_CONNECTED.
    The new name follows cookie naming guideline. The old cookie will be
    deleted when a new cookie operation is applied on a domain.
    
    BUG=586113
    
    Review URL: https://codereview.chromium.org/1719153002
    
    Cr-Commit-Position: refs/heads/master@{#377859}

--- components/signin/ios/browser/account_consistency_service_unittest.mm
+++ components/signin/ios/browser/account_consistency_service_unittest.mm
@@ -28,11 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/ocmock/gtest_support.h"
 
 namespace {
-// URL of the Google domain where the X-CHROME-CONNECTED cookie is set/removed.
+// URL of the Google domain where the CHROME_CONNECTED cookie is set/removed.
 NSURL* const kGoogleUrl = [NSURL URLWithString:@"https://google.com/"];
-// URL of the Youtube domain where the X-CHROME-CONNECTED cookie is set/removed.
+// URL of the Youtube domain where the CHROME_CONNECTED cookie is set/removed.
 NSURL* const kYoutubeUrl = [NSURL URLWithString:@"https://youtube.com/"];
-// URL of a country Google domain where the X-CHROME-CONNECTED cookie is
+// URL of a country Google domain where the CHROME_CONNECTED cookie is
 // set/removed.
 NSURL* const kCountryGoogleUrl = [NSURL URLWithString:@"https://google.de/"];
 
@@ -186,9 +186,8 @@ class AccountConsistencyServiceTest : public PlatformTest {
 
   bool ShouldAddCookieToDomain(const std::string& domain,
                                bool should_check_last_update_time) {
-    return account_consistency_service_
-        ->ShouldAddXChromeConnectedCookieToDomain(
-            domain, should_check_last_update_time);
+    return account_consistency_service_->ShouldAddChromeConnectedCookieToDomain(
+        domain, should_check_last_update_time);
   }
 
   void CheckDomainHasCookie(const std::string& domain) {
