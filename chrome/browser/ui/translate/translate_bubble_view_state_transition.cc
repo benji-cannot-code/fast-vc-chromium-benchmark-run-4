c7316b5ec8051f835361d541ab462eef06ec1525
groby <groby@chromium.org>
2016-02-26 15:40:56 -0800

    [Translate] Add metrics for options page.
    Surface the number of times users select the options page/leave the options page in the translate bubble.
    BUG=none
    
    Review URL: https://codereview.chromium.org/1586233003
    
    Cr-Commit-Position: refs/heads/master@{#378024}

--- chrome/browser/ui/translate/translate_bubble_view_state_transition.cc
+++ chrome/browser/ui/translate/translate_bubble_view_state_transition.cc
@@ -6,6 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/translate/translate_bubble_view_state_transition.h"
 
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
+
+namespace {
+
+enum TranslateBubbleUiEvent {
+  // Bubble enters the options state.
+  SET_STATE_OPTIONS = 1,
+
+  // Bubble leaves the options state.
+  LEAVE_STATE_OPTIONS = 2,
+
+  TRANSLATE_BUBBLE_UI_EVENT_MAX
+};
+
+}  // namespace
 
 TranslateBubbleViewStateTransition::TranslateBubbleViewStateTransition(
     TranslateBubbleModel::ViewState view_state)
@@ -20,9 +35,14 @@ void TranslateBubbleViewStateTransition::SetViewState(
   view_state_ = view_state;
   if (view_state != TranslateBubbleModel::VIEW_STATE_ADVANCED)
     view_state_before_advanced_view_ = view_state;
+  else
+    UMA_HISTOGRAM_ENUMERATION("Translate.BubbleUiEvent", SET_STATE_OPTIONS,
+                              TRANSLATE_BUBBLE_UI_EVENT_MAX);
 }
 
 void TranslateBubbleViewStateTransition::GoBackFromAdvanced() {
   DCHECK(view_state_ == TranslateBubbleModel::VIEW_STATE_ADVANCED);
+  UMA_HISTOGRAM_ENUMERATION("Translate.BubbleUiEvent", LEAVE_STATE_OPTIONS,
+                            TRANSLATE_BUBBLE_UI_EVENT_MAX);
   SetViewState(view_state_before_advanced_view_);
 }
