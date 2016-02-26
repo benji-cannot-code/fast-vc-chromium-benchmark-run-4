7a311b0fd8b9669ce8bcf1f71c5a464490a23433
alexclarke <alexclarke@chromium.org>
2016-02-26 09:07:54 -0800

    Some TimeDomain tweaks for performance and clarity
    I've used the C++11 idiom for removing an element from a map by value.
    
    I realised registered_task_queues_ is only used for DCHECKS and added
    some #ifs.
    
    I changed the way we remove a queue from |newly_updatable_| so we
    no longer have to call MoveNewlyUpdatableQueuesIntoUpdatableQueueSet,
    which gave TimeDomain::UnregisterQueue and TimeDomain::MigrateQueue
    unexpected sideffects.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1742603003
    
    Cr-Commit-Position: refs/heads/master@{#377906}

--- components/scheduler/base/time_domain.h
+++ components/scheduler/base/time_domain.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "base/callback.h"
+#include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
@@ -151,7 +152,9 @@ class SCHEDULER_EXPORT TimeDomain {
   // only be accessed from the main thread.
   std::set<internal::TaskQueueImpl*> updatable_queue_set_;
 
+#if DCHECK_IS_ON()
   std::set<internal::TaskQueueImpl*> registered_task_queues_;
+#endif
 
   Observer* observer_;  // NOT OWNED.
 
