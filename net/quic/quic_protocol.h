7ca437695214d43b64b6c8ec2b8c3e19ea1cef43
ckrasic <ckrasic@chromium.org>
2016-02-25 17:20:16 -0800

    QUIC reject PUSH_PROMISE if method is not GET or HEAD.
    Merge internal change: 115506138
    
    R=rch@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1732193004
    
    Cr-Commit-Position: refs/heads/master@{#377751}

--- net/quic/quic_protocol.h
+++ net/quic/quic_protocol.h
@@ -484,6 +484,8 @@ enum QuicRstStreamErrorCode {
   QUIC_DUPLICATE_PROMISE_URL,
   // Vary check failed.
   QUIC_PROMISE_VARY_MISMATCH,
+  // Only GET and HEAD methods allowed.
+  QUIC_INVALID_PROMISE_METHOD,
   // No error. Used as bound while iterating.
   QUIC_STREAM_LAST_ERROR,
 };
