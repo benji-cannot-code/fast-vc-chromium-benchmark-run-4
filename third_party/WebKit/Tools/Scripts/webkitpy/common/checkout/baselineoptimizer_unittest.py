d8adb7180e41bab23f9cd665fc384166fa205644
dpranke <dpranke@chromium.org>
2016-02-25 18:58:38 -0800

    Remove Linux 32 support from webkit_tests.
    Since the official 32-bit x86 Linux Chromium build is no longer
    supported on trunk, I'm removing the baselines for the linux-x86
    port from the layout tests and removing the support for the port
    from webkitpy; the overhead of maintaining the pixel baselines for
    the port and the complexity introduced into the test code for
    x86 vs x86-64 isn't worth keeping around (I'm not if I ever
    actually saw a legit x86 layout test failure that we cared about).
    
    R=ojan@chromium.org
    BUG=564903
    
    Review URL: https://codereview.chromium.org/1731103006
    
    Cr-Commit-Position: refs/heads/master@{#377790}

--- third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/baselineoptimizer_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/baselineoptimizer_unittest.py
@@ -179,7 +179,6 @@ class BaselineOptimizerTest(unittest.TestCase):
         self._assertOptimization({
             'platform/mac': '1',
             'platform/linux': '1',
-            'platform/linux-x86': '1',
             'platform/linux-precise': '1',
         }, {
             'platform/mac': '1',
@@ -191,7 +190,6 @@ class BaselineOptimizerTest(unittest.TestCase):
             'platform/mac-snowleopard': '1',
             'platform/win': '1',
             'platform/linux': '1',
-            'platform/linux-x86': '1',
             'platform/linux-precise': '1',
         }, {
             'platform/mac-snowleopard': '1',
