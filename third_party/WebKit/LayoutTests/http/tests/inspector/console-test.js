97100c5a4bbaddc382c75a93deeee771fc05c344
adamk <adamk@chromium.org>
2016-02-25 21:34:49 -0800

    Prepare inspector layout tests for ES2015 Function.name changes
    This patch changes the regexps that inspector layout tests use to
    filter inspector-internal function names out of stack trace dumps.
    
    Under ES2015, the evaluate* methods will no longer include the
    "InjectedScript." prefix in stack traces, since they'll be given
    instance "name" properties that precisely match the property
    name to which they're assigned in the InjectedScript source.
    
    BUG=588803
    
    Review URL: https://codereview.chromium.org/1733073002
    
    Cr-Commit-Position: refs/heads/master@{#377818}

--- third_party/WebKit/LayoutTests/http/tests/inspector/console-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/console-test.js
@@ -34,7 +34,9 @@ InspectorTest.evaluateInConsoleAndDump = function(code, callback)
 
     function mycallback(text)
     {
-        InspectorTest.addResult(code + " = " + text.replace(/\bVM\d+/g, "VM").replace(/InjectedScript\.\w+ @ VM:\d+/g, ""));
+        text = text.replace(/\bVM\d+/g, "VM");
+        text = text.replace(/(?:InjectedScript\.)?_?evaluate\w* @ VM:\d+/g, "");
+        InspectorTest.addResult(code + " = " + text);
         callback(text);
     }
     InspectorTest.evaluateInConsole(code, mycallback);
@@ -46,7 +48,7 @@ InspectorTest.prepareConsoleMessageText = function(messageElement, consoleMessag
     // Replace scriptIds with generic scriptId string to avoid flakiness.
     messageText = messageText.replace(/VM\d+/g, "VM");
     // Strip out InjectedScript line numbers from stack traces to avoid rebaselining each time InjectedScriptSource is edited.
-    messageText = messageText.replace(/InjectedScript\.\w+ @ VM:\d+/g, "");
+    messageText = messageText.replace(/(?:InjectedScript\.)?_?evaluate\w* @ VM:\d+/g, "");
     // Strip out InjectedScript line numbers from console message anchor.
     var functionName = consoleMessage && consoleMessage.stackTrace && consoleMessage.stackTrace.callFrames[0] && consoleMessage.stackTrace.callFrames[0].functionName || "";
     if (functionName.indexOf("InjectedScript") !== -1)
@@ -132,7 +134,7 @@ InspectorTest.formatterIgnoreStackFrameUrls = function(messageFormatter, node)
     {
         var buffer = string.replace(/\u200b/g, "");
         buffer = buffer.replace(/VM\d+/g, "VM");
-        buffer = buffer.replace(/InjectedScript\.\w+ @ VM:\d+/g, "");
+        buffer = buffer.replace(/(?:InjectedScript\.)?_?evaluate\w* @ VM:\d+/g, "");
         return buffer.replace(/^\s+at [^\]]+(]?)$/, "$1");
     }
 
