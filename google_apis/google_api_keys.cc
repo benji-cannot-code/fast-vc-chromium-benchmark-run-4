92f5c882c0b33e5e3bf248e424547f48f78f9f1e
treib <treib@chromium.org>
2016-02-26 01:36:46 -0800

    Remove special API key for SafeSites
    We're using the regular Chrome key now
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1731313002
    
    Cr-Commit-Position: refs/heads/master@{#377860}

--- google_apis/google_api_keys.cc
+++ google_apis/google_api_keys.cc
@@ -30,10 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GOOGLE_API_KEY DUMMY_API_TOKEN
 #endif
 
-#if !defined(GOOGLE_API_KEY_SAFESITES)
-#define GOOGLE_API_KEY_SAFESITES DUMMY_API_TOKEN
-#endif
-
 #if !defined(GOOGLE_API_KEY_REMOTING)
 #define GOOGLE_API_KEY_REMOTING DUMMY_API_TOKEN
 #endif
@@ -100,14 +96,6 @@ class APIKeyCache {
                                  std::string(),
                                  environment.get(),
                                  command_line);
-
-    api_key_safesites_ =
-        CalculateKeyValue(GOOGLE_API_KEY_SAFESITES,
-                          STRINGIZE_NO_EXPANSION(GOOGLE_API_KEY_SAFESITES),
-                          NULL,
-                          std::string(),
-                          environment.get(),
-                          command_line);
 
     api_key_remoting_ =
         CalculateKeyValue(GOOGLE_API_KEY_REMOTING,
@@ -200,7 +188,6 @@ class APIKeyCache {
   }
 
   std::string api_key() const { return api_key_; }
-  std::string api_key_safesites() const { return api_key_safesites_; }
   std::string api_key_remoting() const { return api_key_remoting_; }
 
   std::string GetClientID(OAuth2Client client) const {
@@ -267,7 +254,6 @@ class APIKeyCache {
   }
 
   std::string api_key_;
-  std::string api_key_safesites_;
   std::string api_key_remoting_;
   std::string client_ids_[CLIENT_NUM_ITEMS];
   std::string client_secrets_[CLIENT_NUM_ITEMS];
@@ -293,10 +279,6 @@ bool HasKeysConfigured() {
 
 std::string GetAPIKey() {
   return g_api_key_cache.Get().api_key();
-}
-
-std::string GetSafeSitesAPIKey() {
-  return g_api_key_cache.Get().api_key_safesites();
 }
 
 std::string GetRemotingAPIKey() {
