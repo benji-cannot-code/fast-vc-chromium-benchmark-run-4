4a2f15fd6b9ef735ae009b99834ad5d2d5cc37c1
nainar <nainar@chromium.org>
2016-02-26 00:13:42 -0800

    Add internals.isCSSPropertyUseCounted functionality
    This patch adds the functionality of
    internals.isCSSPropertyUseCounted to test whether a particular CSSProperty
    is in use in a file. This function is being added to help improve testing
    for improvements made to UseCounter methods for counting CSSProperty.
    
    There is a currently a internals.isUseCounted that is used for
    testing whether a current feature is UseCounted.
    
    BUG=589343
    
    Review URL: https://codereview.chromium.org/1711703002
    
    Cr-Commit-Position: refs/heads/master@{#377846}

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -1118,6 +1118,10 @@ public:
     // Return whether the Feature was previously counted for this document.
     // NOTE: only for use in testing.
     static bool isCounted(Document&, Feature);
+    // Return whether the CSSPropertyID was previously counted for this document.
+    // NOTE: only for use in testing.
+    static bool isCounted(Document&, const String&);
+    bool isCounted(CSSPropertyID);
 
     void didCommitLoad();
 
