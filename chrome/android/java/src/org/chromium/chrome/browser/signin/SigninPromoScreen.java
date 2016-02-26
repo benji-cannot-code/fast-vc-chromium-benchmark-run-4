c489ad4f73b453e094bdd1278e6f5b582b8534c3
peconn <peconn@chromium.org>
2016-02-26 10:58:44 -0800

    Unified and improved the appearance of all elements of the First Run Experience.
    BUG=557781
    
    Review URL: https://codereview.chromium.org/1727363003
    
    Cr-Commit-Position: refs/heads/master@{#377935}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninPromoScreen.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninPromoScreen.java
@@ -11,7 +11,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.LinearLayout;
 
 import org.chromium.base.metrics.RecordUserAction;
@@ -76,15 +75,15 @@ public class SigninPromoScreen extends AlwaysDismissedDialog
         setOwnerActivity(activity);
 
         LayoutInflater inflater = LayoutInflater.from(activity);
-        View view = inflater.inflate(R.layout.account_signin_view, null);
+        mAccountFirstRunView = (AccountSigninView)
+                inflater.inflate(R.layout.account_signin_view, null);
         mProfileDataCache = new ProfileDataCache(activity, Profile.getLastUsedProfile());
-        mAccountFirstRunView = (AccountSigninView) view.findViewById(R.id.account_layout);
         mAccountFirstRunView.init(mProfileDataCache);
         mAccountFirstRunView.configureForAddAccountPromo();
         mAccountFirstRunView.setListener(this);
         mAccountFirstRunView.setDelegate(this);
 
-        setContentView(view, new LinearLayout.LayoutParams(
+        setContentView(mAccountFirstRunView, new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
     }
 
