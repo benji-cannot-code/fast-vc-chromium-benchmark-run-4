8c3776c3893fad0ba6d7cb4e71806d449901161e
gmanikpure <gmanikpure@chromium.org>
2016-02-25 16:01:57 -0800

    [Chromedriver] Replace dismiss alert with accept alert due to Issue26
    BUG=
    
    Review URL: https://codereview.chromium.org/1725303005
    
    Cr-Commit-Position: refs/heads/master@{#377719}

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -1230,7 +1230,7 @@ class ChromeDriverTest(ChromeDriverBaseTest):
     a = self._driver.FindElement('id', 'btn')
     a.Click()
     self.WaitForCondition(lambda: self._driver.IsAlertOpen())
-    self._driver.HandleAlert(False)
+    self._driver.HandleAlert(True)
 
 
 class ChromeDriverAndroidTest(ChromeDriverBaseTest):
