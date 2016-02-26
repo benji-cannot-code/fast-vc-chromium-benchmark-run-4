652e17166dd5f24f66b8546d9bef91abc6914f73
sorin <sorin@chromium.org>
2016-02-25 18:16:05 -0800

    Turn on CUP for extension update checks done through the update client.
    Write test that CPU is turned on in prod for the component updater.
    
    BUG=583027
    
    Review URL: https://codereview.chromium.org/1731023004
    
    Cr-Commit-Position: refs/heads/master@{#377773}

--- chrome/browser/component_updater/chrome_component_updater_configurator_unittest.cc
+++ chrome/browser/component_updater/chrome_component_updater_configurator_unittest.cc
@@ -17,20 +17,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace component_updater {
 
 TEST(ChromeComponentUpdaterConfiguratorTest, TestDisablePings) {
-  base::CommandLine* cmdline = base::CommandLine::ForCurrentProcess();
-  cmdline->AppendSwitchASCII(switches::kComponentUpdater, "disable-pings");
+  base::CommandLine cmdline(*base::CommandLine::ForCurrentProcess());
+  cmdline.AppendSwitchASCII(switches::kComponentUpdater, "disable-pings");
 
-  const auto config(MakeChromeComponentUpdaterConfigurator(cmdline, NULL));
+  const auto config(MakeChromeComponentUpdaterConfigurator(&cmdline, nullptr));
 
   const std::vector<GURL> pingUrls = config->PingUrl();
   EXPECT_TRUE(pingUrls.empty());
 }
 
 TEST(ChromeComponentUpdaterConfiguratorTest, TestFastUpdate) {
-  base::CommandLine* cmdline = base::CommandLine::ForCurrentProcess();
-  cmdline->AppendSwitchASCII(switches::kComponentUpdater, "fast-update");
+  base::CommandLine cmdline(*base::CommandLine::ForCurrentProcess());
+  cmdline.AppendSwitchASCII(switches::kComponentUpdater, "fast-update");
 
-  const auto config(MakeChromeComponentUpdaterConfigurator(cmdline, NULL));
+  const auto config(MakeChromeComponentUpdaterConfigurator(&cmdline, nullptr));
 
   ASSERT_EQ(10, config->InitialDelay());
 }
@@ -38,13 +38,14 @@ TEST(ChromeComponentUpdaterConfiguratorTest, TestFastUpdate) {
 TEST(ChromeComponentUpdaterConfiguratorTest, TestOverrideUrl) {
   const char overrideUrl[] = "http://0.0.0.0/";
 
-  base::CommandLine* cmdline = base::CommandLine::ForCurrentProcess();
+  base::CommandLine cmdline(*base::CommandLine::ForCurrentProcess());
+
   std::string val = "url-source";
   val.append("=");
   val.append(overrideUrl);
-  cmdline->AppendSwitchASCII(switches::kComponentUpdater, val.c_str());
+  cmdline.AppendSwitchASCII(switches::kComponentUpdater, val.c_str());
 
-  const auto config(MakeChromeComponentUpdaterConfigurator(cmdline, NULL));
+  const auto config(MakeChromeComponentUpdaterConfigurator(&cmdline, nullptr));
 
   const std::vector<GURL> urls = config->UpdateUrl();
 
@@ -53,12 +54,29 @@ TEST(ChromeComponentUpdaterConfiguratorTest, TestOverrideUrl) {
 }
 
 TEST(ChromeComponentUpdaterConfiguratorTest, TestSwitchRequestParam) {
-  base::CommandLine* cmdline = base::CommandLine::ForCurrentProcess();
-  cmdline->AppendSwitchASCII(switches::kComponentUpdater, "test-request");
+  base::CommandLine cmdline(*base::CommandLine::ForCurrentProcess());
+  cmdline.AppendSwitchASCII(switches::kComponentUpdater, "test-request");
 
-  const auto config(MakeChromeComponentUpdaterConfigurator(cmdline, NULL));
+  const auto config(MakeChromeComponentUpdaterConfigurator(&cmdline, nullptr));
 
   EXPECT_FALSE(config->ExtraRequestParams().empty());
+}
+
+TEST(ChromeComponentUpdaterConfiguratorTest, TestUpdaterDefaultUrl) {
+  base::CommandLine cmdline(*base::CommandLine::ForCurrentProcess());
+  const auto config(MakeChromeComponentUpdaterConfigurator(&cmdline, nullptr));
+  const auto urls = config->UpdateUrl();
+
+  // Expect the default url to be cryptographically secure.
+  EXPECT_GE(urls.size(), 1ul);
+  EXPECT_TRUE(urls.front().SchemeIsCryptographic());
+}
+
+TEST(ChromeComponentUpdaterConfiguratorTest, TestUseCupSigning) {
+  base::CommandLine cmdline(*base::CommandLine::ForCurrentProcess());
+  const auto config(MakeChromeComponentUpdaterConfigurator(&cmdline, nullptr));
+
+  EXPECT_TRUE(config->UseCupSigning());
 }
 
 }  // namespace component_updater
