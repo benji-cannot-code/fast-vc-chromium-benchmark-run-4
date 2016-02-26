b5dcca63aea4b3f8643528dc9b396affdda102e0
svaldez <svaldez@chromium.org>
2016-02-25 16:14:37 -0800

    Reduce Certificate Parsing Strictness
    In order to allow for compatibility with existing certificates/OCSP
    responders in the wild, the strictness on a few structures must be
    reduced:
    
    * Allow empty parameters in Signature Algorithms
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1690123002
    
    Cr-Commit-Position: refs/heads/master@{#377722}

--- net/cert/internal/signature_algorithm_unittest.cc
+++ net/cert/internal/signature_algorithm_unittest.cc
@@ -73,7 +73,10 @@ TEST(SignatureAlgorithmTest, ParseDerSha1WithRSAEncryptionNoParams) {
   };
   // clang-format on
   scoped_ptr<SignatureAlgorithm> algorithm;
-  ASSERT_FALSE(ParseDer(kData, &algorithm));
+  ASSERT_TRUE(ParseDer(kData, &algorithm));
+
+  EXPECT_EQ(SignatureAlgorithmId::RsaPkcs1, algorithm->algorithm());
+  EXPECT_EQ(DigestAlgorithm::Sha1, algorithm->digest());
 }
 
 // Parses a sha1WithRSAEncryption which contains an unexpected parameters
@@ -129,7 +132,10 @@ TEST(SignatureAlgorithmTest, ParseDerSha1WithRSASignatureNoParams) {
   };
   // clang-format on
   scoped_ptr<SignatureAlgorithm> algorithm;
-  ASSERT_FALSE(ParseDer(kData, &algorithm));
+  ASSERT_TRUE(ParseDer(kData, &algorithm));
+
+  EXPECT_EQ(SignatureAlgorithmId::RsaPkcs1, algorithm->algorithm());
+  EXPECT_EQ(DigestAlgorithm::Sha1, algorithm->digest());
 }
 
 // Parses a sha1WithRSAEncryption which contains values after the sequence.
@@ -241,7 +247,10 @@ TEST(SignatureAlgorithmTest, ParseDerSha256WithRSAEncryptionNoParams) {
   };
   // clang-format on
   scoped_ptr<SignatureAlgorithm> algorithm;
-  ASSERT_FALSE(ParseDer(kData, &algorithm));
+  ASSERT_TRUE(ParseDer(kData, &algorithm));
+
+  EXPECT_EQ(SignatureAlgorithmId::RsaPkcs1, algorithm->algorithm());
+  EXPECT_EQ(DigestAlgorithm::Sha256, algorithm->digest());
 }
 
 // Parses a sha384WithRSAEncryption which contains a NULL parameters field.
@@ -278,7 +287,10 @@ TEST(SignatureAlgorithmTest, ParseDerSha384WithRSAEncryptionNoParams) {
   };
   // clang-format on
   scoped_ptr<SignatureAlgorithm> algorithm;
-  ASSERT_FALSE(ParseDer(kData, &algorithm));
+  ASSERT_TRUE(ParseDer(kData, &algorithm));
+
+  EXPECT_EQ(SignatureAlgorithmId::RsaPkcs1, algorithm->algorithm());
+  EXPECT_EQ(DigestAlgorithm::Sha384, algorithm->digest());
 }
 
 // Parses a sha512WithRSAEncryption which contains a NULL parameters field.
@@ -315,7 +327,10 @@ TEST(SignatureAlgorithmTest, ParseDerSha512WithRSAEncryptionNoParams) {
   };
   // clang-format on
   scoped_ptr<SignatureAlgorithm> algorithm;
-  ASSERT_FALSE(ParseDer(kData, &algorithm));
+  ASSERT_TRUE(ParseDer(kData, &algorithm));
+
+  EXPECT_EQ(SignatureAlgorithmId::RsaPkcs1, algorithm->algorithm());
+  EXPECT_EQ(DigestAlgorithm::Sha512, algorithm->digest());
 }
 
 // Parses a sha224WithRSAEncryption which contains a NULL parameters field.
