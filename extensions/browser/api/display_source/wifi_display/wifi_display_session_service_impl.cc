f0201499ba3441f4556370f9c70759ef2f5cfcde
mikhail.pozdnyakov <mikhail.pozdnyakov@intel.com>
2016-02-26 02:48:10 -0800

    [chrome.displaySource] further implementation of call completion callbacks
    The API implementation determines when and with which
    arguments the 'startSession'/'terminateSession' completion
    callbacks are invoked.
    
    The 'exceeded_session_limit_error' session error type is
    eliminated and substituted with 'startSession' completion
    callback call (which is more natural since this problem is
    detected before a new session is started).
    
    JS bindings code is simplified.
    
    BUG=242107
    
    Review URL: https://codereview.chromium.org/1730583002
    
    Cr-Commit-Position: refs/heads/master@{#377864}

--- extensions/browser/api/display_source/wifi_display/wifi_display_session_service_impl.cc
+++ extensions/browser/api/display_source/wifi_display/wifi_display_session_service_impl.cc
@@ -63,8 +63,7 @@ void WiFiDisplaySessionServiceImpl::Connect(int32_t sink_id,
   DCHECK(client_);
   // We support only one Wi-Fi Display session at a time.
   if (delegate_->connection()) {
-    client_->OnError(ERROR_TYPE_SESSION_LIMIT_ERROR,
-                     kErrorCannotHaveMultipleSessions);
+    client_->OnConnectRequestHandled(false, kErrorCannotHaveMultipleSessions);
     return;
   }
 
@@ -73,7 +72,7 @@ void WiFiDisplaySessionServiceImpl::Connect(int32_t sink_id,
       sinks.begin(), sinks.end(),
       [sink_id](DisplaySourceSinkInfoPtr ptr) { return ptr->id == sink_id; });
   if (found == sinks.end() || (*found)->state != SINK_STATE_DISCONNECTED) {
-    client_->OnError(ERROR_TYPE_CONNECTION_ERROR, kErrorSinkNotAvailable);
+    client_->OnConnectRequestHandled(false, kErrorSinkNotAvailable);
     return;
   }
   AuthenticationInfo auth_info;
@@ -88,6 +87,7 @@ void WiFiDisplaySessionServiceImpl::Connect(int32_t sink_id,
   sink_id_ = sink_id;
   sink_state_ = (*found)->state;
   DCHECK(sink_state_ == SINK_STATE_CONNECTING);
+  client_->OnConnectRequestHandled(true, "");
 }
 
 void WiFiDisplaySessionServiceImpl::Disconnect() {
@@ -155,10 +155,11 @@ void WiFiDisplaySessionServiceImpl::OnSinksUpdated(
     auto on_message = base::Bind(&WiFiDisplaySessionServiceImpl::OnSinkMessage,
                                  weak_factory_.GetWeakPtr());
     connection->SetMessageReceivedCallback(on_message);
-    client_->OnEstablished(connection->GetLocalAddress());
+    client_->OnConnected(connection->GetLocalAddress());
   }
 
   if (actual_state == SINK_STATE_DISCONNECTED) {
+    client_->OnDisconnectRequestHandled(true, "");
     client_->OnTerminated();
     sink_id_ = DisplaySourceConnectionDelegate::kInvalidSinkId;
   }
@@ -191,7 +192,7 @@ void WiFiDisplaySessionServiceImpl::OnDisconnectFailed(
   if (sink_id != sink_id_)
     return;
   DCHECK(client_);
-  client_->OnError(ERROR_TYPE_CONNECTION_ERROR, message);
+  client_->OnDisconnectRequestHandled(false, message);
 }
 
 void WiFiDisplaySessionServiceImpl::OnClientConnectionError() {
--- extensions/renderer/resources/display_source_custom_bindings.js
+++ extensions/renderer/resources/display_source_custom_bindings.js
@@ -19,7 +19,7 @@ function callbackWrapper(callback, method, message) {
 
   try {
     if (message !== null)
-      lastError.set('displaySource.startSession', message, null, chrome);
+      lastError.set(method, message, null, chrome);
     callback();
   } finally {
     lastError.clear(chrome);
@@ -41,7 +41,7 @@ binding.registerCustomHook(function(bindingsAPI, extensionId) {
   apiFunctions.setHandleRequest(
       'startSession', function(sessionInfo, callback) {
         try {
-          var callId = natives.StartSession(sessionInfo, callbackWrapper);
+          var callId = natives.StartSession(sessionInfo);
           callbacksInfo[callId] = {
             callback: callback,
             method: 'displaySource.startSession'
@@ -53,7 +53,7 @@ binding.registerCustomHook(function(bindingsAPI, extensionId) {
   apiFunctions.setHandleRequest(
       'terminateSession', function(sink_id, callback) {
         try {
-          var callId = natives.TerminateSession(sink_id, callbackWrapper);
+          var callId = natives.TerminateSession(sink_id);
           callbacksInfo[callId] = {
             callback: callback,
             method: 'displaySource.terminateSession'
