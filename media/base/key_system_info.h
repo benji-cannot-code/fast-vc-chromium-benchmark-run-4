5d5b78db7903d0c2d4832516ea1e86337a2fcdec
ddorwin <ddorwin@chromium.org>
2016-02-25 17:25:01 -0800

    EME: Remove the concept of a parent key system.
    This was only used by prefixed EME.
    We still check whether a key system is a "subdomain" in a few specific cases.
    
    BUG=249976
    
    Review URL: https://codereview.chromium.org/1732423002
    
    Cr-Commit-Position: refs/heads/master@{#377755}

--- media/base/key_system_info.h
+++ media/base/key_system_info.h
@@ -22,12 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // * Abstract key system
 //    A key system string that cannot be instantiated like a concrete key system
 //    but is otherwise useful, such as in discovery using isTypeSupported().
-// TODO(ddorwin): Remove the concept of a parent key system now that prefixed
-// has been removed. https://crbug.com/249976
-// * Parent key system
-//    A key system string that is one level up from the child key system. It may
-//    be an abstract key system.
-//    As an example, "com.example" is the parent of "com.example.foo".
 
 namespace media {
 
@@ -52,11 +46,6 @@ struct MEDIA_EXPORT KeySystemInfo {
       EmeSessionTypeSupport::INVALID;
   EmeFeatureSupport persistent_state_support = EmeFeatureSupport::INVALID;
   EmeFeatureSupport distinctive_identifier_support = EmeFeatureSupport::INVALID;
-
-  // A hierarchical parent for |key_system|. This value can be used to check
-  // supported types but cannot be used to instantiate a MediaKeys object.
-  // Only one parent key system is currently supported per concrete key system.
-  std::string parent_key_system;
 
   // The following indicate how the corresponding CDM should be instantiated.
   bool use_aes_decryptor = false;
