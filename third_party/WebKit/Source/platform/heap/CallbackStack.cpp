438990126b08cc98524eb589e2d22cf51a9c7386
sigbjornf <sigbjornf@opera.com>
2016-02-26 02:52:42 -0800

    Parameterize CallbackStack over initial block size.
    CallbackStack keeps a chain of blocks, extending it as needed. So as to
    allow stacks with varying block sizes, have its constructor take the
    block size to use as argument.
    
    R=haraken
    BUG=420515
    
    Review URL: https://codereview.chromium.org/1737913002
    
    Cr-Commit-Position: refs/heads/master@{#377865}

--- third_party/WebKit/Source/platform/heap/CallbackStack.cpp
+++ third_party/WebKit/Source/platform/heap/CallbackStack.cpp
@@ -8,24 +8,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-CallbackStack::Block::Block(Block* next)
+CallbackStack::Block::Block(Block* next, size_t blockSize)
+    : m_blockSize(blockSize)
 {
-    static_assert((blockSize * sizeof(Item)) % WTF::kPageAllocationGranularity == 0, "CallbackStack::blockSize * sizeof(Item) must be a multiple of WTF::kPageAllocationGranularity");
-    m_buffer = static_cast<Item*>(WTF::allocPages(nullptr, blockSize * sizeof(Item), WTF::kPageAllocationGranularity, WTF::PageAccessible));
+    // Allocated block size must be a multiple of WTF::kPageAllocationGranularity.
+    ASSERT((m_blockSize * sizeof(Item)) % WTF::kPageAllocationGranularity == 0);
+    m_buffer = static_cast<Item*>(WTF::allocPages(nullptr, m_blockSize * sizeof(Item), WTF::kPageAllocationGranularity, WTF::PageAccessible));
     RELEASE_ASSERT(m_buffer);
 
 #if ENABLE(ASSERT)
     clear();
 #endif
 
-    m_limit = &(m_buffer[blockSize]);
+    m_limit = &(m_buffer[m_blockSize]);
     m_current = &(m_buffer[0]);
     m_next = next;
 }
 
 CallbackStack::Block::~Block()
 {
-    WTF::freePages(m_buffer, blockSize * sizeof(Item));
+    WTF::freePages(m_buffer, m_blockSize * sizeof(Item));
     m_buffer = nullptr;
     m_limit = nullptr;
     m_current = nullptr;
@@ -35,7 +37,7 @@ CallbackStack::Block::~Block()
 #if ENABLE(ASSERT)
 void CallbackStack::Block::clear()
 {
-    for (size_t i = 0; i < blockSize; i++)
+    for (size_t i = 0; i < m_blockSize; i++)
         m_buffer[i] = Item(0, 0);
 }
 #endif
@@ -45,7 +47,7 @@ void CallbackStack::Block::decommit()
 #if ENABLE(ASSERT)
     clear();
 #endif
-    WTF::discardSystemPages(m_buffer, blockSize * sizeof(Item));
+    WTF::discardSystemPages(m_buffer, m_blockSize * sizeof(Item));
 
     m_current = &m_buffer[0];
     m_next = nullptr;
@@ -73,8 +75,8 @@ bool CallbackStack::Block::hasCallbackForObject(const void* object)
 }
 #endif
 
-CallbackStack::CallbackStack()
-    : m_first(new Block(0))
+CallbackStack::CallbackStack(size_t blockSize)
+    : m_first(new Block(nullptr, blockSize))
     , m_last(m_first)
 {
 }
@@ -106,7 +108,7 @@ bool CallbackStack::isEmpty() const
 CallbackStack::Item* CallbackStack::allocateEntrySlow()
 {
     ASSERT(!m_first->allocateEntry());
-    m_first = new Block(m_first);
+    m_first = new Block(m_first, m_first->blockSize());
     return m_first->allocateEntry();
 }
 
@@ -152,7 +154,7 @@ void CallbackStack::invokeOldestCallbacks(Block* from, Block* upto, Visitor* vis
     if (from == upto)
         return;
     ASSERT(from);
-    // Recurse first (blockSize at a time) so we get to the newly added entries last.
+    // Recurse first so we get to the newly added entries last.
     invokeOldestCallbacks(from->next(), upto, visitor);
     from->invokeEphemeronCallbacks(visitor);
 }
--- third_party/WebKit/Source/platform/heap/CallbackStack.h
+++ third_party/WebKit/Source/platform/heap/CallbackStack.h
@@ -38,7 +38,7 @@ public:
         VisitorCallback m_callback;
     };
 
-    CallbackStack();
+    explicit CallbackStack(size_t blockSize = defaultBlockSize);
     ~CallbackStack();
 
     void clear();
@@ -56,12 +56,12 @@ public:
 #endif
 
 private:
-    static const size_t blockSize = (1 << 13);
+    static const size_t defaultBlockSize = (1 << 13);
 
     class Block {
         USING_FAST_MALLOC(Block);
     public:
-        explicit Block(Block* next);
+        Block(Block* next, size_t blockSize);
         ~Block();
 
 #if ENABLE(ASSERT)
@@ -76,6 +76,8 @@ private:
         {
             return m_current == &(m_buffer[0]);
         }
+
+        size_t blockSize() const { return m_blockSize; }
 
         Item* allocateEntry()
         {
@@ -92,11 +94,14 @@ private:
         }
 
         void invokeEphemeronCallbacks(Visitor*);
+
 #if ENABLE(ASSERT)
         bool hasCallbackForObject(const void*);
 #endif
 
     private:
+        size_t m_blockSize;
+
         Item* m_buffer;
         Item* m_limit;
         Item* m_current;
