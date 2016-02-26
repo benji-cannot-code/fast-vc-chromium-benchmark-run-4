031f8ed06b309f2f49161818efc0bbce84471f25
chrishtr <chrishtr@chromium.org>
2016-02-25 20:24:42 -0800

    Invalidate the new backing of newly unsquashed layers which are also not self-composited.
    BUG=587537
    
    Review URL: https://codereview.chromium.org/1737903003
    
    Cr-Commit-Position: refs/heads/master@{#377810}

--- third_party/WebKit/Source/core/layout/compositing/CompositedLayerMapping.h
+++ third_party/WebKit/Source/core/layout/compositing/CompositedLayerMapping.h
@@ -147,7 +147,7 @@ public:
     bool verifyLayerInSquashingVector(const PaintLayer*);
 #endif
 
-    void finishAccumulatingSquashingLayers(size_t nextSquashedLayerIndex);
+    void finishAccumulatingSquashingLayers(size_t nextSquashedLayerIndex, Vector<PaintLayer*>& layersNeedingPaintInvalidation);
     void updateRenderingContext();
     void updateShouldFlattenTransform();
     void updateElementIdAndCompositorMutableProperties();
--- third_party/WebKit/Source/core/layout/compositing/CompositingLayerAssigner.h
+++ third_party/WebKit/Source/core/layout/compositing/CompositingLayerAssigner.h
@@ -62,7 +62,7 @@ private:
             , nextSquashedLayerIndex(0)
             , totalAreaOfSquashedRects(0) { }
 
-        void updateSquashingStateForNewMapping(CompositedLayerMapping*, bool hasNewCompositedPaintLayerMapping);
+        void updateSquashingStateForNewMapping(CompositedLayerMapping*, bool hasNewCompositedPaintLayerMapping, Vector<PaintLayer*>& layersNeedingPaintInvalidation);
 
         // The most recent composited backing that the layer should squash onto if needed.
         CompositedLayerMapping* mostRecentMapping;
