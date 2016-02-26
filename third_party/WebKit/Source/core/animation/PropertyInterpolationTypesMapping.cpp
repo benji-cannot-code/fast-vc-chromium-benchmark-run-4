fd81040b3bdec9456af5782eec23718ff5715486
alancutter <alancutter@chromium.org>
2016-02-25 17:02:05 -0800

    Add additive animation support for border-radius CSS properties
    This patch adds support for composite modes on border-radius
    CSS animations via element.animate().
    
    BUG=437696
    
    Committed: https://crrev.com/bd831cabec2a2d24c0b635995df4677cb4b8f46d
    Cr-Commit-Position: refs/heads/master@{#376159}
    
    Review URL: https://codereview.chromium.org/1682843003
    
    Cr-Commit-Position: refs/heads/master@{#377742}

--- third_party/WebKit/Source/core/animation/PropertyInterpolationTypesMapping.cpp
+++ third_party/WebKit/Source/core/animation/PropertyInterpolationTypesMapping.cpp
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/animation/CSSImageListInterpolationType.h"
 #include "core/animation/CSSLengthInterpolationType.h"
 #include "core/animation/CSSLengthListInterpolationType.h"
+#include "core/animation/CSSLengthPairInterpolationType.h"
 #include "core/animation/CSSMotionRotationInterpolationType.h"
 #include "core/animation/CSSNumberInterpolationType.h"
 #include "core/animation/CSSPaintInterpolationType.h"
@@ -186,6 +187,12 @@ const InterpolationTypes* PropertyInterpolationTypesMapping::get(const PropertyH
         case CSSPropertyPerspectiveOrigin:
         case CSSPropertyObjectPosition:
             applicableTypes->append(adoptPtr(new CSSPositionInterpolationType(cssProperty)));
+            break;
+        case CSSPropertyBorderBottomLeftRadius:
+        case CSSPropertyBorderBottomRightRadius:
+        case CSSPropertyBorderTopLeftRadius:
+        case CSSPropertyBorderTopRightRadius:
+            applicableTypes->append(adoptPtr(new CSSLengthPairInterpolationType(cssProperty)));
             break;
         default:
             // TODO(alancutter): Support all interpolable CSS properties here so we can stop falling back to the old StyleInterpolation implementation.
