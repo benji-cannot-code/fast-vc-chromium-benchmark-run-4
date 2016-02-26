238a12bc21bf98bbbf7face08516dfdf8dceaa9d
khushalsagar <khushalsagar@chromium.org>
2016-02-26 11:08:00 -0800

    Blimp: Send the correct device scale factor to the BlimpView on android.
    This fixes the error in generating MotionEvents and WebGestureEvents on
    android which were causing incorrect resolution of touch targets on the
    client and the engine.
    
    Also increase the max payload size to 1MB since the compositor messages
    can be large right now.
    
    BUG=589323
    
    Review URL: https://codereview.chromium.org/1739563004
    
    Cr-Commit-Position: refs/heads/master@{#377938}

--- blimp/net/common.cc
+++ blimp/net/common.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blimp {
 
-const size_t kMaxPacketPayloadSizeBytes = 1 << 16;  // 64KB
+const size_t kMaxPacketPayloadSizeBytes = 1024 * 1024;  // 1MB
 const size_t kPacketHeaderSizeBytes = 4;
 
 std::ostream& operator<<(std::ostream& out, const BlimpMessage& message) {
