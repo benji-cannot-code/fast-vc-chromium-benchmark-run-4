9f6a80323ed9e0fb7d421f9cfe93d549b298acd5
mgiuca <mgiuca@chromium.org>
2016-02-25 22:43:31 -0800

    Enable the new-app-list-mixer flag on Chrome OS.
    This changes the way the Launcher ranks and mixes search results. For
    details on the change, see https://crrev.com/329601.
    
    It will still respect both the flag and the field trial, but as the
    trial is not currently active, the default should be respected for all
    users who haven't explicitly set the flag.
    
    BUG=487494,460451,422610,338418
    
    Review URL: https://codereview.chromium.org/1744493002
    
    Cr-Commit-Position: refs/heads/master@{#377833}

--- ui/app_list/search/mixer.cc
+++ ui/app_list/search/mixer.cc
@@ -34,6 +34,7 @@ const size_t kMinBlendedResults = 6;
 
 const char kAppListMixerFieldTrialName[] = "AppListMixer";
 const char kAppListMixerFieldTrialEnabled[] = "Blended";
+const char kAppListMixerFieldTrialDisabled[] = "Control";
 
 void UpdateResult(const SearchResult& source, SearchResult* target) {
   target->set_display_type(source.display_type());
@@ -52,6 +53,7 @@ bool IsBlendedMixerTrialEnabled() {
   const std::string group_name =
       base::FieldTrialList::FindFullName(kAppListMixerFieldTrialName);
 
+  // Respect command-line flags first.
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kDisableNewAppListMixer)) {
     return false;
@@ -62,7 +64,15 @@ bool IsBlendedMixerTrialEnabled() {
     return true;
   }
 
-  return group_name == kAppListMixerFieldTrialEnabled;
+  // Next, respect field-trial groups.
+  if (group_name == kAppListMixerFieldTrialEnabled)
+    return true;
+
+  if (group_name == kAppListMixerFieldTrialDisabled)
+    return false;
+
+  // By default, enable the new logic if the experimental app list is enabled.
+  return app_list::switches::IsExperimentalAppListEnabled();
 }
 
 }  // namespace
