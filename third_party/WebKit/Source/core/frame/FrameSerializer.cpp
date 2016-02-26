311cf3be4f125d77370a12b57795e238c27dbc82
lukasza <lukasza@chromium.org>
2016-02-26 10:52:43 -0800

    Only call shouldSkipResource if we are actually going to serialize a resource.
    A DCHECK in MHTMLPartsGenerationDelegate::shouldSkipResource verifies
    that it not called twice with the same URL - this is kind of important,
    because we care about not generating an MHTML file that contains 2 MHTML
    parts with the same URI (according to previous manual tests, such MHTML
    file would work ok in Chromium, but would not open in Internet
    Explorer).
    
    This CL makes sure that FrameSerializer::shouldSkipResource is only called
    if we are actually going to serialize a resource (and consequently put it
    into an MHTML part).  In other words - FrameSerializer should only defer
    to MHTMLPartsGenerationDelegate if all the internally done checks pass.
    This restores sanity to the DCHECK above.
    
    BUG=586680
    
    Review URL: https://codereview.chromium.org/1703273002
    
    Cr-Commit-Position: refs/heads/master@{#377933}

--- third_party/WebKit/Source/core/frame/FrameSerializer.cpp
+++ third_party/WebKit/Source/core/frame/FrameSerializer.cpp
@@ -404,10 +404,7 @@ void FrameSerializer::addToResources(Resource* resource, PassRefPtr<SharedBuffer
 
 void FrameSerializer::addImageToResources(ImageResource* image, const KURL& url)
 {
-    if (!shouldAddURL(url))
-        return;
-
-    if (!image || !image->hasImage() || image->errorOccurred())
+    if (!image || !image->hasImage() || image->errorOccurred() || !shouldAddURL(url))
         return;
 
     RefPtr<SharedBuffer> data = image->image()->data();
@@ -416,7 +413,7 @@ void FrameSerializer::addImageToResources(ImageResource* image, const KURL& url)
 
 void FrameSerializer::addFontToResources(FontResource* font)
 {
-    if (!font || !shouldAddURL(font->url()) || !font->isLoaded() || !font->resourceBuffer())
+    if (!font || !font->isLoaded() || !font->resourceBuffer() || !shouldAddURL(font->url()))
         return;
 
     RefPtr<SharedBuffer> data(font->resourceBuffer());
