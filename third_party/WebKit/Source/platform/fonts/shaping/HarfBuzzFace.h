b6455199d8d37e50921f6db8b3d0c784b13b8692
drott <drott@chromium.org>
2016-02-26 03:13:46 -0800

    Revert of Move glyph lookup to hb-ot-font and remove glyph cache in HarfBuzzFace (patchset #3 id:40001 of https://codereview.chromium.org/1733193002/ )
    Reason for revert:
    It seems to break chromeos=1, not only buildtype official. Reverting and will start discussing an agreeable solution on chrome-dev.
    
    Original issue's description:
    > Move glyph lookup to hb-ot-font and remove glyph cache in HarfBuzzFace
    >
    > Together with Behdad we identified several ways to optimize and cleanup
    > HarfBuzzFace:
    > * We moved the glyph lookup completely to the built-in HarfBuzz
    >   implementation in hb-ot-font.
    > * This allows us to remove the unbounded glyph cache we kept around in
    >   HarfBuzzFace.
    > * We also moved instantantion and ownership of HarfBuzzFontData to
    >   HarfBuzzFace. We only update the size and unicode-range on each call
    >   to getScaledFont(), saving us as the allocation and setup costs of
    >   this object for each shaping call.
    > * We removed the unneeded callback for retrieving the horizontal origin,
    >   which was implemented as a noop.
    >
    > In local measurements on Mac this leads to large improvements in the
    > blink_perf.layout tests, especially in
    > chapter-reflow-once-random (>130%), flexbox-lots-of-data (>58%) and
    > ArabicLineLayout (>26%) with only a few of the microbenchmarks being
    > slightly slower: So this is an improvement for layout cases that do not
    > profit much from the word cache.
    >
    > Note to ChromeOS developers: If this change breaks your workflow linking
    > release builds with system HarfBuzz because of an old version of
    > HarfBuzz on the host system, we recommend to install and apt-pin the
    > same version of the HarfBuzz library on your host system that ChromeOS
    > uses. Please see bug 589342.
    >
    > BUG=589340
    > R=eae,behdad,kojii
    >
    > Committed: https://crrev.com/85a2c1c0597a31b043e58fe4a4ce00a88ebc0684
    > Cr-Commit-Position: refs/heads/master@{#377849}
    
    TBR=kojii@chromium.org,behdad@chromium.org,eae@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=589340
    
    Review URL: https://codereview.chromium.org/1742593003
    
    Cr-Commit-Position: refs/heads/master@{#377867}

--- third_party/WebKit/Source/platform/fonts/shaping/HarfBuzzFace.h
+++ third_party/WebKit/Source/platform/fonts/shaping/HarfBuzzFace.h
@@ -45,11 +45,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class FontPlatformData;
-struct HarfBuzzFontData;
 
 class HarfBuzzFace : public RefCounted<HarfBuzzFace> {
     WTF_MAKE_NONCOPYABLE(HarfBuzzFace);
 public:
+    static const hb_tag_t vertTag;
+    static const hb_tag_t vrt2Tag;
 
     static PassRefPtr<HarfBuzzFace> create(FontPlatformData* platformData, uint64_t uniqueID)
     {
@@ -60,20 +61,18 @@ public:
     // In order to support the restricting effect of unicode-range optionally a
     // range restriction can be passed in, which will restrict which glyphs we
     // return in the harfBuzzGetGlyph function.
-    hb_font_t* getScaledFont(unsigned rangeFrom = 0, unsigned rangeTo = kMaxCodepoint);
+    hb_font_t* createFont(unsigned rangeFrom = 0, unsigned rangeTo = kMaxCodepoint) const;
     hb_face_t* face() const { return m_face; }
 
 private:
     HarfBuzzFace(FontPlatformData*, uint64_t);
 
     hb_face_t* createFace();
-    void prepareHarfBuzzFontData();
 
     FontPlatformData* m_platformData;
     uint64_t m_uniqueID;
     hb_face_t* m_face;
-    OwnPtr<hb_font_t> m_unscaledFont;
-    OwnPtr<HarfBuzzFontData> m_harfBuzzFontData;
+    WTF::HashMap<uint32_t, uint16_t>* m_glyphCacheForFaceCacheEntry;
 };
 
 } // namespace blink
