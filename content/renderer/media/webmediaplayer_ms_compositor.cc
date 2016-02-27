049f939b473b70f6bcbcc8346654746ca1387a8c
dalecurtis <dalecurtis@chromium.org>
2016-02-26 17:16:58 -0800

    Fix various issues with WebMediaPlayerMS and MediaSession support.
    Pre-MediaSession issues:
    [1] Rendering was allowed to start in hidden tabs.
    [2] VideoFrameMetadata was not copied to new frames, causing DCHECK
        failures when REFERENCE_TIME or END_OF_STREAM are used.
    
    Important detail:
    [3] Audio rendering is typically not stopped when a WMPMS instance is
    background; this is intentional for phone-like behavior.
    
    Post-MediaSession issues:
    [4] Due to [1] above, a hidden WebMediaPlayerMS player might steal
        focus from a foreground player.
    [5] MediaSession will suspend all other players when a new audio
        generating player starts up.  Even those that [3] initially allowed.
        When returning to one of these tabs they would be stuck in a paused
        state forever since the MediaSession instance is out of sync with
        the player.
    
    This change fixes issues [1], [2], and [5] and adds tests to cover this
    behavior.
    
    BUG=581983
    TEST=new unittests.
    
    Review URL: https://codereview.chromium.org/1743443002
    
    Cr-Commit-Position: refs/heads/master@{#378052}

--- content/renderer/media/webmediaplayer_ms_compositor.cc
+++ content/renderer/media/webmediaplayer_ms_compositor.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/hash.h"
 #include "base/single_thread_task_runner.h"
+#include "base/values.h"
 #include "cc/blink/context_provider_web_context.h"
 #include "content/renderer/media/webmediaplayer_ms.h"
 #include "content/renderer/render_thread_impl.h"
@@ -90,6 +91,11 @@ scoped_refptr<media::VideoFrame> CopyFrameToI420(
                      new_frame->stride(media::VideoFrame::kVPlane),
                      size.width(), size.height());
   }
+
+  // Transfer metadata keys.
+  base::DictionaryValue original_metadata;
+  frame->metadata()->MergeInternalValuesInto(&original_metadata);
+  new_frame->metadata()->MergeInternalValuesFrom(original_metadata);
   return new_frame;
 }
 
