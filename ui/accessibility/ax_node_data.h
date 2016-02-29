82fbe0fa4da8e719633a1b3252d4c51333b0928f
dmazzoni <dmazzoni@chromium.org>
2016-02-29 13:17:12 -0800

    Add explicit assignment operator and copy contructor to AXNodeData
    This will allow us to use a scoped_ptr as a member of AXNodeData in the
    future.
    
    BUG=551601
    
    Review URL: https://codereview.chromium.org/1749533003
    
    Cr-Commit-Position: refs/heads/master@{#378275}

--- ui/accessibility/ax_node_data.h
+++ ui/accessibility/ax_node_data.h
@@ -25,8 +25,10 @@ namespace ui {
 // one process to another.
 struct AX_EXPORT AXNodeData {
   AXNodeData();
-  AXNodeData(const AXNodeData& other);
   virtual ~AXNodeData();
+
+  AXNodeData(const AXNodeData& other);
+  AXNodeData& operator=(AXNodeData other);
 
   // Accessing accessibility attributes:
   //
@@ -96,8 +98,8 @@ struct AX_EXPORT AXNodeData {
   bool IsRoot() const;
   void SetRoot();
 
-  // This is a simple serializable struct. All member variables should be
-  // public and copyable.
+  // As much as possible this should behave as a simple, serializable,
+  // copyable struct.
   int32_t id;
   AXRole role;
   uint32_t state;
