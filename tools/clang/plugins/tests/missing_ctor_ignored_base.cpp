eb668dbefb216c8982aa3ed232fd27e89b95224d
vmpstr <vmpstr@chromium.org>
2016-02-29 12:03:17 -0800

    clang-plugin: Skip classes deriving from IPC::NoParams.
    This patch changes the check for complex implicit constructors to skip
    classes that derive (directly or indirectly) from IPC::NoParams.
    
    This avoids us having to modify IPC macros to sometimes include
    copy ctors.
    
    R=dcheng@chromium.org, thakis@chromium.org
    BUG=436357
    
    Review URL: https://codereview.chromium.org/1743833003
    
    Cr-Commit-Position: refs/heads/master@{#378250}

---
+++ tools/clang/plugins/tests/missing_ctor_ignored_base.cpp
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "missing_ctor_ignored_base.h"
+
+int main() {
+  MissingCtorsWithIgnoredBase one;
+  MissingCtorsWithIgnoredGrandBase two;
+  return 0;
+}
