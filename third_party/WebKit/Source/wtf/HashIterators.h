5c16a3b55c96c4ef99a21dd007cb6c53ecf265cf
danakj <danakj@chromium.org>
2016-02-29 15:18:09 -0800

    Rename enums/functions that collide in chromium style in WTF::HashMap
    R=esprehn@chromium.org, thakis@chromium.org
    BUG=582312
    
    Review URL: https://codereview.chromium.org/1746503002
    
    Cr-Commit-Position: refs/heads/master@{#378312}

--- third_party/WebKit/Source/wtf/HashIterators.h
+++ third_party/WebKit/Source/wtf/HashIterators.h
@@ -55,8 +55,8 @@ public:
     HashTableConstIteratorAdapter& operator++() { ++m_impl; return *this; }
     // postfix ++ intentionally omitted
 
-    Keys keys() { return Keys(*this); }
-    Values values() { return Values(*this); }
+    Keys getKeys() { return Keys(*this); }
+    Values getValues() { return Values(*this); }
 
     typename HashTableType::const_iterator m_impl;
 };
@@ -86,8 +86,8 @@ public:
         return i;
     }
 
-    Keys keys() { return Keys(*this); }
-    Values values() { return Values(*this); }
+    Keys getKeys() { return Keys(*this); }
+    Values getValues() { return Values(*this); }
 
     typename HashTableType::iterator m_impl;
 };
