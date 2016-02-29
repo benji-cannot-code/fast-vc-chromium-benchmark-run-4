f813c4ec383226a8f6662ba9f73ae774fb89b3e5
bmcquade <bmcquade@chromium.org>
2016-02-29 09:12:16 -0800

    Add additional auditing for PageLoadTiming (re-land after rollback).
    This patch was originally landed in
    https://codereview.chromium.org/1735363002, but rolled back as a result
    of its catching a bug elsewhere in our code (crbug.com/590454). That bug
    has been fixed, so this patch is ready to re-land.
    
    BUG=589944
    
    Review URL: https://codereview.chromium.org/1741263002
    
    Cr-Commit-Position: refs/heads/master@{#378222}

--- chrome/browser/page_load_metrics/observers/from_gws_page_load_metrics_observer_unittest.cc
+++ chrome/browser/page_load_metrics/observers/from_gws_page_load_metrics_observer_unittest.cc
@@ -54,6 +54,7 @@ TEST_F(FromGWSPageLoadMetricsObserverTest, NoReferral) {
   page_load_metrics::PageLoadTiming timing;
   timing.navigation_start = base::Time::FromDoubleT(1);
   timing.first_text_paint = base::TimeDelta::FromMilliseconds(1);
+  PopulateRequiredTimingFields(&timing);
   NavigateAndCommit(GURL("http://www.example.com"));
 
   SimulateTimingUpdate(timing);
@@ -67,6 +68,7 @@ TEST_F(FromGWSPageLoadMetricsObserverTest, ReferralsFromGWSHTTPToHTTPS) {
   page_load_metrics::PageLoadTiming timing;
   timing.navigation_start = base::Time::FromDoubleT(1);
   timing.first_text_paint = base::TimeDelta::FromMilliseconds(1);
+  PopulateRequiredTimingFields(&timing);
   // HTTPS google.com referral  to HTTP example.com.
   set_referrer(content::Referrer(GURL("https://www.google.com"),
                                  blink::WebReferrerPolicyOrigin));
@@ -86,6 +88,7 @@ TEST_F(FromGWSPageLoadMetricsObserverTest, ReferralFromGWS) {
   page_load_metrics::PageLoadTiming timing;
   timing.navigation_start = base::Time::FromDoubleT(1);
   timing.first_text_paint = base::TimeDelta::FromMilliseconds(1);
+  PopulateRequiredTimingFields(&timing);
 
   set_referrer(content::Referrer(GURL("https://www.google.com/url"),
                                  blink::WebReferrerPolicyDefault));
@@ -105,6 +108,7 @@ TEST_F(FromGWSPageLoadMetricsObserverTest, ReferralFromGWSBackgroundLater) {
   page_load_metrics::PageLoadTiming timing;
   timing.navigation_start = base::Time::FromDoubleT(1);
   timing.first_text_paint = base::TimeDelta::FromMicroseconds(1);
+  PopulateRequiredTimingFields(&timing);
 
   set_referrer(content::Referrer(GURL("https://www.google.com/url"),
                                  blink::WebReferrerPolicyDefault));
@@ -125,6 +129,7 @@ TEST_F(FromGWSPageLoadMetricsObserverTest, ReferralsFromCaseInsensitive) {
   page_load_metrics::PageLoadTiming timing;
   timing.navigation_start = base::Time::FromDoubleT(1);
   timing.first_text_paint = base::TimeDelta::FromMilliseconds(1);
+  PopulateRequiredTimingFields(&timing);
   // HTTPS google.com referral  to HTTP example.com.
   set_referrer(content::Referrer(GURL("https://wWw.GoOGlE.cOm/webhp"),
                                  blink::WebReferrerPolicyOrigin));
@@ -143,6 +148,7 @@ TEST_F(FromGWSPageLoadMetricsObserverTest, ReferralsFromGWSOrigin) {
   page_load_metrics::PageLoadTiming timing;
   timing.navigation_start = base::Time::FromDoubleT(1);
   timing.first_text_paint = base::TimeDelta::FromMilliseconds(1);
+  PopulateRequiredTimingFields(&timing);
   // HTTPS google.com referral  to HTTP example.com.
   set_referrer(content::Referrer(GURL("https://www.google.com"),
                                  blink::WebReferrerPolicyOrigin));
@@ -153,6 +159,7 @@ TEST_F(FromGWSPageLoadMetricsObserverTest, ReferralsFromGWSOrigin) {
   page_load_metrics::PageLoadTiming timing2;
   timing2.navigation_start = base::Time::FromDoubleT(10);
   timing2.first_text_paint = base::TimeDelta::FromMilliseconds(100);
+  PopulateRequiredTimingFields(&timing2);
   // HTTPS google.com referral  to HTTP example.com.
   set_referrer(content::Referrer(GURL("https://www.google.co.in"),
                                  blink::WebReferrerPolicyOrigin));
@@ -174,6 +181,7 @@ TEST_F(FromGWSPageLoadMetricsObserverTest, ReferralNotFromGWS) {
   page_load_metrics::PageLoadTiming timing;
   timing.navigation_start = base::Time::FromDoubleT(1);
   timing.first_text_paint = base::TimeDelta::FromMilliseconds(1);
+  PopulateRequiredTimingFields(&timing);
   set_referrer(content::Referrer(GURL("https://www.anothersite.com"),
                                  blink::WebReferrerPolicyDefault));
   NavigateAndCommit(GURL("https://www.example.com"));
