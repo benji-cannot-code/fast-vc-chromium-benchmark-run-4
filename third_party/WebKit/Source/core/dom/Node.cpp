478617fc91c420316cee19102101a2a663eb6701
rob.buis <rob.buis@samsung.com>
2016-02-29 05:30:47 -0800

    Fix isEqualNode behavior
    According to the steps listed in [1] isEqualNode should use
    namespace, namespace prefix and local name when comparing
    Elements, so we can just use tagQName. Note that nodeName
    differs from localName since it uppercases the tag name
    for HTML.
    
    This patch fixes dom/nodes/Node-isEqualNode.html.
    
    [1] https://dom.spec.whatwg.org/#dom-node-isequalnode
    
    BUG=590269
    
    Review URL: https://codereview.chromium.org/1741873002
    
    Cr-Commit-Position: refs/heads/master@{#378202}

--- third_party/WebKit/Source/core/dom/Node.cpp
+++ third_party/WebKit/Source/core/dom/Node.cpp
@@ -1122,9 +1122,6 @@ bool Node::isEqualNode(Node* other) const
     if (nodeType != other->getNodeType())
         return false;
 
-    if (nodeName() != other->nodeName())
-        return false;
-
     if (nodeValue() != other->nodeValue())
         return false;
 
@@ -1135,14 +1132,13 @@ bool Node::isEqualNode(Node* other) const
         if (toAttr(this)->namespaceURI() != toAttr(other)->namespaceURI())
             return false;
     } else if (isElementNode()) {
-        if (toElement(this)->localName() != toElement(other)->localName())
-            return false;
-
-        if (toElement(this)->namespaceURI() != toElement(other)->namespaceURI())
+        if (toElement(this)->tagQName() != toElement(other)->tagQName())
             return false;
 
         if (!toElement(this)->hasEquivalentAttributes(toElement(other)))
             return false;
+    } else if (nodeName() != other->nodeName()) {
+        return false;
     }
 
     Node* child = firstChild();
