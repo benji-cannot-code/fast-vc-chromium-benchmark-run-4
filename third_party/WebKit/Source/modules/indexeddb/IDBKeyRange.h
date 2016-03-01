10ea09b08b8e1d12d40ef628ecbb6dea8eb02803
jsbell <jsbell@chromium.org>
2016-03-01 10:18:31 -0800

    Add new experimental IDBKeyRange.includes(key) API
    Like IDBFactory.cmp, this simplifies the implementation of
    utilities and libraries for Indexed DB, but does not unlock
    any new database functionality itself.
    
    Spec: https://w3c.github.io/IndexedDB/#dom-idbkeyrange-includes
    
    Review URL: https://codereview.chromium.org/1392983003
    
    Cr-Commit-Position: refs/heads/master@{#378482}

--- third_party/WebKit/Source/modules/indexeddb/IDBKeyRange.h
+++ third_party/WebKit/Source/modules/indexeddb/IDBKeyRange.h
@@ -75,6 +75,8 @@ public:
 
     static IDBKeyRange* only(IDBKey* value, ExceptionState&);
 
+    bool includes(ExecutionContext*, const ScriptValue& key, ExceptionState&);
+
 private:
     IDBKeyRange(IDBKey* lower, IDBKey* upper, LowerBoundType lowerType, UpperBoundType upperType);
 
