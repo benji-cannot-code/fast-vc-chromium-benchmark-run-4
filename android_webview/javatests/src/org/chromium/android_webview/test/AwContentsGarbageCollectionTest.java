fa8e3cd01c4ec1ade546abeea12c8f637f455df4
boliu <boliu@chromium.org>
2016-03-01 11:19:54 -0800

    Do not queue sync IPC on start up
    It's illegal to queue a synchronous IPC, and then return true from send,
    since the reply fields of the IPC would not be correctly popuplated.
    Instead just return false in this case.
    
    BUG=590408
    
    Review URL: https://codereview.chromium.org/1746343002
    
    Cr-Commit-Position: refs/heads/master@{#378513}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsGarbageCollectionTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsGarbageCollectionTest.java
@@ -15,7 +15,6 @@ import static org.chromium.base.test.util.ScalableTimeout.scaleTimeout;
 import org.chromium.android_webview.AwContents;
 import org.chromium.base.annotations.SuppressFBWarnings;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.parameter.ParameterizedTest;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
 
@@ -105,7 +104,6 @@ public class AwContentsGarbageCollectionTest extends AwTestBase {
         gcAndCheckAllAwContentsDestroyed();
     }
 
-    @ParameterizedTest.Set  // crbug.com/590408
     @DisableHardwareAccelerationForTest
     @SmallTest
     @Feature({"AndroidWebView"})
@@ -127,7 +125,6 @@ public class AwContentsGarbageCollectionTest extends AwTestBase {
         gcAndCheckAllAwContentsDestroyed();
     }
 
-    @ParameterizedTest.Set  // crbug.com/590408
     @DisableHardwareAccelerationForTest
     @SmallTest
     @Feature({"AndroidWebView"})
--- android_webview/javatests/src/org/chromium/android_webview/test/ClientOnReceivedError2Test.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/ClientOnReceivedError2Test.java
@@ -15,7 +15,6 @@ import org.chromium.android_webview.ErrorCodeConversionHelper;
 import org.chromium.android_webview.test.util.AwTestTouchUtils;
 import org.chromium.android_webview.test.util.CommonResources;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.parameter.ParameterizedTest;
 import org.chromium.content.browser.test.util.TestCallbackHelperContainer;
 import org.chromium.net.test.util.TestWebServer;
 
@@ -113,7 +112,6 @@ public class ClientOnReceivedError2Test extends AwTestBase {
         assertNotNull(error.description);
     }
 
-    @ParameterizedTest.Set  // crbug.com/590408
     @SmallTest
     @Feature({"AndroidWebView"})
     public void testUserGesture() throws Throwable {
@@ -173,7 +171,6 @@ public class ClientOnReceivedError2Test extends AwTestBase {
         assertNotNull(error.description);
     }
 
-    @ParameterizedTest.Set  // crbug.com/590408
     @SmallTest
     @Feature({"AndroidWebView"})
     public void testUserGestureForIframeSubresource() throws Throwable {
