47de81477e0b506d8cd92dedd7559d9ddae62324
dgozman <dgozman@chromium.org>
2016-02-29 17:35:12 -0800

    Execute end of scope tasks in isolate's MicrotasksCompletedCallback.
    This removes additional responsibility from V8RecursionScope and Microtask
    and will allow to move microtasks management to V8.
    
    Attempt #2, previous one was reverted due to old v8 on some bots.
    
    BUG=585949
    TEST=all green
    
    Review URL: https://codereview.chromium.org/1733083002
    
    Cr-Commit-Position: refs/heads/master@{#378359}

--- third_party/WebKit/Source/bindings/core/v8/V8RecursionScope.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8RecursionScope.cpp
@@ -38,7 +38,6 @@ namespace blink {
 void V8RecursionScope::didLeaveScriptContext()
 {
     Microtask::performCheckpoint(m_isolate);
-    V8PerIsolateData::from(m_isolate)->runEndOfScopeTasks();
 }
 
 } // namespace blink
