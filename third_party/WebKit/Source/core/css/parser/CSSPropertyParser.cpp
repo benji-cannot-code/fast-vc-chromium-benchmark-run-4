dae18805d4818af8560d931f1e8ad153ed03b46d
timloh <timloh@chromium.org>
2016-02-29 22:23:15 -0800

    consumeFlex doesn't need to use ShorthandScope
    ShorthandScope only needs to be used in LegacyCSSPropertyParser since
    parseShorthand in CSSPropertyParser sets m_currentShorthand.
    
    BUG=499780
    
    Review URL: https://codereview.chromium.org/1747113002
    
    Cr-Commit-Position: refs/heads/master@{#378406}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -4292,7 +4292,6 @@ bool CSSPropertyParser::consumeShorthandGreedily(const StylePropertyShorthand& s
 
 bool CSSPropertyParser::consumeFlex(bool important)
 {
-    ShorthandScope scope(this, CSSPropertyFlex);
     static const double unsetValue = -1;
     double flexGrow = unsetValue;
     double flexShrink = unsetValue;
