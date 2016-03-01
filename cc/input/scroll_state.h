3a6c26341b995b25e38e5a10516b9e76ecb082d4
vollick <vollick@chromium.org>
2016-03-01 10:05:57 -0800

    Port ScrollState to LayerListImpl
    BUG=557194
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1747253002
    
    Cr-Commit-Position: refs/heads/master@{#378476}

--- cc/input/scroll_state.h
+++ cc/input/scroll_state.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
-class LayerTreeImpl;
+class LayerListImpl;
 
 // ScrollState is based on the proposal for scroll customization in blink, found
 // here: https://goo.gl/1ipTpP.
@@ -62,10 +62,10 @@ class CC_EXPORT ScrollState {
     data_.is_direct_manipulation = is_direct_manipulation;
   }
 
-  void set_scroll_chain_and_layer_tree(
+  void set_scroll_chain_and_layer_list(
       const std::list<const ScrollNode*>& scroll_chain,
-      LayerTreeImpl* layer_tree_impl) {
-    layer_tree_impl_ = layer_tree_impl;
+      LayerListImpl* layer_list_impl) {
+    layer_list_impl_ = layer_list_impl;
     scroll_chain_ = scroll_chain;
   }
 
@@ -98,7 +98,7 @@ class CC_EXPORT ScrollState {
 
  private:
   ScrollStateData data_;
-  LayerTreeImpl* layer_tree_impl_;
+  LayerListImpl* layer_list_impl_;
   std::list<const ScrollNode*> scroll_chain_;
 };
 
