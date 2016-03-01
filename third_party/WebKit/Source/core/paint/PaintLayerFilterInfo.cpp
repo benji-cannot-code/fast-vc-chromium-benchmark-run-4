4986bf1f1c595670a4f8f96f7af468f6ab3a6c22
schenney <schenney@chromium.org>
2016-03-01 14:07:28 -0800

    Fix a crash due to fullscreen with a CSS reference filter
    When a page is shifted to full screen, an anonymous layout object is
    created that inherits the entire style of the primary page. If that
    style includes a CSS reference filter, we hit code in
    PaintLayerFilterInfo that accesses the layout object's document.
    Previously, that document was accessed via the layout object's node,
    but anonymous objects don't have nodes. This patch switches to access
    directly through LayoutObject::document(), which works for anonymous
    objects.
    
    R=wangxianzhu@chromium.org
    BUG=590063
    
    Review URL: https://codereview.chromium.org/1751063002
    
    Cr-Commit-Position: refs/heads/master@{#378571}

--- third_party/WebKit/Source/core/paint/PaintLayerFilterInfo.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerFilterInfo.cpp
@@ -121,7 +121,7 @@ void PaintLayerFilterInfo::updateReferenceFilterClients(const FilterOperations& 
         } else {
             // Reference is internal; add layer as a client so we can trigger
             // filter paint invalidation on SVG attribute change.
-            Element* filter = m_layer->layoutObject()->node()->document().getElementById(referenceFilterOperation->fragment());
+            Element* filter = m_layer->layoutObject()->document().getElementById(referenceFilterOperation->fragment());
             if (!isSVGFilterElement(filter))
                 continue;
             addFilterReference(toSVGFilterElement(filter));
