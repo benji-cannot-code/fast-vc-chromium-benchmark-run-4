c5c22a27e4c30f9966daf2b44dbfdd03bc2ec95d
michaelpg <michaelpg@chromium.org>
2016-03-01 13:48:45 -0800

    Disable flaky GuestModeOptionsUIBrowserTest
    BUG=591154
    TBR=dschuyler@chromium.org
    
    Review URL: https://codereview.chromium.org/1751123003
    
    Cr-Commit-Position: refs/heads/master@{#378567}

--- chrome/browser/ui/webui/options/chromeos/guest_mode_options_browsertest.js
+++ chrome/browser/ui/webui/options/chromeos/guest_mode_options_browsertest.js
@@ -42,8 +42,9 @@ GuestModeOptionsUIBrowserTest.prototype = {
 
 /**
  * Test sections that should be hidden in guest mode.
+ * TODO(michaelpg): Debug flakiness: crbug.com/591154.
  */
-TEST_F('GuestModeOptionsUIBrowserTest', 'testSections', function() {
+TEST_F('GuestModeOptionsUIBrowserTest', 'DISABLED_testSections', function() {
   this.expectHidden($('startup-section'));
   this.expectHidden($('appearance-section'));
   this.expectHidden($('andorid-apps-section'));
@@ -54,8 +55,9 @@ TEST_F('GuestModeOptionsUIBrowserTest', 'testSections', function() {
 
 /**
  * Test controls that should be disabled in guest mode.
+ * TODO(michaelpg): Debug flakiness: crbug.com/591154.
  */
-TEST_F('GuestModeOptionsUIBrowserTest', 'testControls', function() {
+TEST_F('GuestModeOptionsUIBrowserTest', 'DISABLED_testControls', function() {
   // Appearance section.
   var setWallpaper = $('set-wallpaper');
   expectTrue(setWallpaper.disabled);
