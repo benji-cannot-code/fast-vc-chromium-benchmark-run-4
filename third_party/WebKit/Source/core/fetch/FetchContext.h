177e2ebc70742d83c269f823327ccc4a3edf6f3d
yhirano <yhirano@chromium.org>
2016-03-01 14:59:13 -0800

    Add Resource to MemoryCache only when needed.
    This change stops adding a resource to the blink memory cache if
     - The resource type is none of image / font / script / style sheet,
     - The resource is not for preloading, and
     - The inspector is not attached.
    
    This change is for an experiment to measure the impact and will
    be reverted soon.
    
    BUG=579496
    
    Review URL: https://codereview.chromium.org/1683773002
    
    Cr-Commit-Position: refs/heads/master@{#378589}

--- third_party/WebKit/Source/core/fetch/FetchContext.h
+++ third_party/WebKit/Source/core/fetch/FetchContext.h
@@ -113,6 +113,7 @@ public:
     virtual ResourceLoadPriority modifyPriorityForExperiments(ResourceLoadPriority priority, Resource::Type, const FetchRequest&, ResourcePriority::VisibilityStatus) { return priority; }
 
     virtual WebTaskRunner* loadingTaskRunner() const { return nullptr; }
+    virtual bool isInspectorAttached() const { return false; }
 
 protected:
     FetchContext() { }
--- third_party/WebKit/Source/core/loader/FrameFetchContext.h
+++ third_party/WebKit/Source/core/loader/FrameFetchContext.h
@@ -114,6 +114,7 @@ public:
     void countClientHintsViewportWidth() override;
 
     WebTaskRunner* loadingTaskRunner() const override;
+    bool isInspectorAttached() const override;
 
     DECLARE_VIRTUAL_TRACE();
 
