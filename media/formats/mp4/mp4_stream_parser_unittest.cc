fbf5449905e519ea0f58145e001f06cf335aba79
chcunningham <chcunningham@chromium.org>
2016-02-29 18:42:47 -0800

    Improve FourCCToString to print hex value for unprintable chars.
    If you observe the log "Unrecognized top-level box type"
    followed by some hex, chances are we are trying to
    parse a bogus FOURCC due to faulty mp4 atom framing.
    Check your file's framing using an mp4 parsing tool like
    hachoir-urwid or mp4box.js.
    
    Review URL: https://codereview.chromium.org/1749983002
    
    Cr-Commit-Position: refs/heads/master@{#378378}

--- media/formats/mp4/mp4_stream_parser_unittest.cc
+++ media/formats/mp4/mp4_stream_parser_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/text_track_config.h"
 #include "media/base/video_decoder_config.h"
 #include "media/formats/mp4/es_descriptor.h"
+#include "media/formats/mp4/fourccs.h"
 #include "media/formats/mp4/mp4_stream_parser.h"
 #include "media/media_features.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -388,6 +389,17 @@ TEST_F(MP4StreamParserTest, DemuxingEAC3) {
       ReadTestDataFile("bear-eac3-only-frag.mp4");
   EXPECT_EQ(expect_success,
             AppendDataInPieces(buffer->data(), buffer->data_size(), 512));
+}
+
+TEST_F(MP4StreamParserTest, FourCCToString) {
+  // A real FOURCC should print.
+  EXPECT_EQ("mvex", FourCCToString(FOURCC_MVEX));
+
+  // Invalid FOURCC should also print whenever ASCII values are printable.
+  EXPECT_EQ("fake", FourCCToString(static_cast<FourCC>(0x66616b65)));
+
+  // Invalid FORCC with non-printable values should not give error message.
+  EXPECT_EQ("0x66616b00", FourCCToString(static_cast<FourCC>(0x66616b00)));
 }
 
 }  // namespace mp4
