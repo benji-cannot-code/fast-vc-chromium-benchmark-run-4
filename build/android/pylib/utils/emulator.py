030aaa180944a24ada8d937854ed97f9e0ca19ce
yolandyan <yolandyan@google.com>
2016-02-29 16:54:21 -0800

    Fix a bug of not specifying android command path
    BUG=
    
    Review URL: https://codereview.chromium.org/1752493002
    
    Cr-Commit-Position: refs/heads/master@{#378347}

--- build/android/pylib/utils/emulator.py
+++ build/android/pylib/utils/emulator.py
@@ -28,12 +28,14 @@ DEFAULT_SDCARD_SIZE = '512M'
 # Default internal storage (MB) of emulator image
 DEFAULT_STORAGE_SIZE = '1024M'
 
-# Each emulator has 60 secs of wait time for launching
-BOOT_WAIT_INTERVALS = 6
-BOOT_WAIT_INTERVAL_TIME = 10
+# Each emulator has 30 secs of wait time for launching
+_BOOT_WAIT_INTERVALS = 3
+_BOOT_WAIT_INTERVAL_TIME = 10
 
 # Path for avd files and avd dir
-BASE_AVD_DIR = os.path.expanduser(os.path.join('~', '.android', 'avd'))
+_BASE_AVD_DIR = os.path.expanduser(os.path.join('~', '.android', 'avd'))
+_TOOLS_ANDROID_PATH = os.path.join(constants.ANDROID_SDK_ROOT,
+                                   'tools', 'android')
 
 # Template used to generate config.ini files for the emulator
 CONFIG_TEMPLATE = """avd.ini.encoding=ISO-8859-1
@@ -99,7 +101,7 @@ def WaitForEmulatorLaunch(num):
   Arg:
     num: the amount of emulators to wait.
   """
-  for _ in range(num*BOOT_WAIT_INTERVALS):
+  for _ in range(num*_BOOT_WAIT_INTERVALS):
     emulators = [device_utils.DeviceUtils(a)
                  for a in adb_wrapper.AdbWrapper.Devices()
                  if a.is_emulator]
@@ -107,9 +109,9 @@ def WaitForEmulatorLaunch(num):
       logging.info('All %d emulators launched', num)
       return
     logging.info(
-        'Waiting for %d emulators, %d of them launched: [%s], ',
-        num, len(emulators), ' '.join([str(x) for x in emulators]))
-    time.sleep(BOOT_WAIT_INTERVAL_TIME)
+        'Waiting for %d emulators, %d of them already launched', num,
+        len(emulators))
+    time.sleep(_BOOT_WAIT_INTERVAL_TIME)
   raise Exception("Expected %d emulators, %d launched within time limit" %
                   (num, len(emulators)))
 
@@ -147,7 +149,7 @@ def DeleteAllTempAVDs():
     return
   for avd_name in avds:
     if 'run_tests_avd' in avd_name:
-      cmd = ['android', '-s', 'delete', 'avd', '--name', avd_name]
+      cmd = [_TOOLS_ANDROID_PATH, '-s', 'delete', 'avd', '--name', avd_name]
       cmd_helper.RunCmd(cmd)
       logging.info('Delete AVD %s', avd_name)
 
@@ -286,7 +288,7 @@ class Emulator(object):
     """
     android_sdk_root = constants.ANDROID_SDK_ROOT
     self.emulator = os.path.join(android_sdk_root, 'tools', 'emulator')
-    self.android = os.path.join(android_sdk_root, 'tools', 'android')
+    self.android = _TOOLS_ANDROID_PATH
     self.popen = None
     self.device_serial = None
     self.abi = abi
@@ -340,8 +342,8 @@ class Emulator(object):
     avd_process.expect('Created AVD \'%s\'' % self.avd_name)
 
     # Replace current configuration with default Galaxy Nexus config.
-    ini_file = os.path.join(BASE_AVD_DIR, '%s.ini' % self.avd_name)
-    new_config_ini = os.path.join(BASE_AVD_DIR, '%s.avd' % self.avd_name,
+    ini_file = os.path.join(_BASE_AVD_DIR, '%s.ini' % self.avd_name)
+    new_config_ini = os.path.join(_BASE_AVD_DIR, '%s.avd' % self.avd_name,
                                   'config.ini')
 
     # Remove config files with defaults to replace with Google's GN settings.
@@ -352,7 +354,7 @@ class Emulator(object):
     with open(ini_file, 'w') as new_ini:
       new_ini.write('avd.ini.encoding=ISO-8859-1\n')
       new_ini.write('target=%s\n' % api_target)
-      new_ini.write('path=%s/%s.avd\n' % (BASE_AVD_DIR, self.avd_name))
+      new_ini.write('path=%s/%s.avd\n' % (_BASE_AVD_DIR, self.avd_name))
       new_ini.write('path.rel=avd/%s.avd\n' % self.avd_name)
 
     custom_config = CONFIG_TEMPLATE
@@ -386,9 +388,9 @@ class Emulator(object):
 
     This serves as a work around for '-partition-size' and '-wipe-data'
     """
-    userdata_img = os.path.join(BASE_AVD_DIR, '%s.avd' % self.avd_name,
+    userdata_img = os.path.join(_BASE_AVD_DIR, '%s.avd' % self.avd_name,
                                 'userdata.img')
-    userdata_qemu_img = os.path.join(BASE_AVD_DIR, '%s.avd' % self.avd_name,
+    userdata_qemu_img = os.path.join(_BASE_AVD_DIR, '%s.avd' % self.avd_name,
                                      'userdata-qemu.img')
     resize_cmd = ['resize2fs', userdata_img, '%s' % storage_size]
     logging.info('Resizing userdata.img to ideal size')
