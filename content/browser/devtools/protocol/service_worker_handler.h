97c63cffbdf5d8ff727e472ca018085a14dce64d
horo <horo@chromium.org>
2016-02-29 17:34:16 -0800

    [DevTools] Check the existence of |context_| in ServiceWorkerHandler
    When the user closes the tab, SetRenderFrameHost(nullptr) is called before
    Disable(). And if the user has checked the force-update flag, Chrome crashes.
    
    BUG=590050
    
    Review URL: https://codereview.chromium.org/1744943002
    
    Cr-Commit-Position: refs/heads/master@{#378358}

--- content/browser/devtools/protocol/service_worker_handler.h
+++ content/browser/devtools/protocol/service_worker_handler.h
@@ -93,6 +93,7 @@ class ServiceWorkerHandler : public DevToolsAgentHostClient,
                        const ServiceWorkerContextObserver::ErrorInfo& info);
 
   void OpenNewDevToolsWindow(int process_id, int devtools_agent_route_id);
+  void ClearForceUpdate();
 
   scoped_refptr<ServiceWorkerContextWrapper> context_;
   scoped_ptr<Client> client_;
