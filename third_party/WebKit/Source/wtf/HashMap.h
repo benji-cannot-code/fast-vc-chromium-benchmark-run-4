57dcc447c3121c8d42272ba3f314815fde9558aa
danakj <danakj@chromium.org>
2016-02-29 20:06:36 -0800

    Rename enums/functions that collide in chromium style in WTF::HashMap
    R=esprehn@chromium.org, thakis@chromium.org
    BUG=582312
    
    Committed: https://crrev.com/477aebd4f13056c90a257b78c1f572d910c85f19
    Cr-Commit-Position: refs/heads/master@{#378312}
    
    Review URL: https://codereview.chromium.org/1746503002
    
    Cr-Commit-Position: refs/heads/master@{#378391}

--- third_party/WebKit/Source/wtf/HashMap.h
+++ third_party/WebKit/Source/wtf/HashMap.h
@@ -178,8 +178,8 @@ class HashMap<KeyArg, MappedArg, HashArg, KeyTraitsArg, MappedTraitsArg, Allocat
     DISALLOW_NEW();
 public:
     typedef HashMap<KeyArg, MappedArg, HashArg, KeyTraitsArg, MappedTraitsArg, Allocator> HashMapType;
-    typedef typename HashMapType::iterator::Keys iterator;
-    typedef typename HashMapType::const_iterator::Keys const_iterator;
+    typedef typename HashMapType::iterator::KeysIterator iterator;
+    typedef typename HashMapType::const_iterator::KeysIterator const_iterator;
 
     iterator begin()
     {
@@ -217,8 +217,8 @@ class HashMap<KeyArg, MappedArg, HashArg, KeyTraitsArg, MappedTraitsArg, Allocat
     DISALLOW_NEW();
 public:
     typedef HashMap<KeyArg, MappedArg, HashArg, KeyTraitsArg, MappedTraitsArg, Allocator> HashMapType;
-    typedef typename HashMapType::iterator::Values iterator;
-    typedef typename HashMapType::const_iterator::Values const_iterator;
+    typedef typename HashMapType::iterator::ValuesIterator iterator;
+    typedef typename HashMapType::const_iterator::ValuesIterator const_iterator;
 
     iterator begin()
     {
@@ -488,7 +488,7 @@ inline bool operator!=(const HashMap<T, U, V, W, X, Y>& a, const HashMap<T, U, V
 template <typename T, typename U, typename V, typename W, typename X, typename Y, typename Z>
 inline void copyKeysToVector(const HashMap<T, U, V, W, X, Y>& collection, Z& vector)
 {
-    typedef typename HashMap<T, U, V, W, X, Y>::const_iterator::Keys iterator;
+    typedef typename HashMap<T, U, V, W, X, Y>::const_iterator::KeysIterator iterator;
 
     vector.resize(collection.size());
 
@@ -501,7 +501,7 @@ inline void copyKeysToVector(const HashMap<T, U, V, W, X, Y>& collection, Z& vec
 template <typename T, typename U, typename V, typename W, typename X, typename Y, typename Z>
 inline void copyValuesToVector(const HashMap<T, U, V, W, X, Y>& collection, Z& vector)
 {
-    typedef typename HashMap<T, U, V, W, X, Y>::const_iterator::Values iterator;
+    typedef typename HashMap<T, U, V, W, X, Y>::const_iterator::ValuesIterator iterator;
 
     vector.resize(collection.size());
 
