aac8a72b518dea5cea744ca829c0b3e069d6fc3f
caseq <caseq@chromium.org>
2016-03-02 14:07:56 -0800

    Timeline: fix timeline saving and make the test actually test it
    - defer writing temp file to output stream till all write operations are done (previously deferred just till underlying storage is initialized)
    - make test cases run asynchronously, as it's required by underlying stream functionality
    - rename writeToOutputSteam to writeToOutputStream
    
    BUG=547667
    
    Review URL: https://codereview.chromium.org/1756983002
    
    Cr-Commit-Position: refs/heads/master@{#378854}

--- third_party/WebKit/Source/devtools/front_end/bindings/TempFile.js
+++ third_party/WebKit/Source/devtools/front_end/bindings/TempFile.js
@@ -200,7 +200,7 @@ WebInspector.TempFile.prototype = {
      * @param {!WebInspector.OutputStream} outputStream
      * @param {!WebInspector.OutputStreamDelegate} delegate
      */
-    writeToOutputSteam: function(outputStream, delegate)
+    copyToOutputStream: function(outputStream, delegate)
     {
         /**
          * @param {!File} file
@@ -253,8 +253,6 @@ WebInspector.DeferredTempFile.prototype = {
      */
     write: function(strings, callback)
     {
-        if (!this._chunks)
-            return;
         if (this._finishCallback)
             throw new Error("No writes are allowed after close.");
         this._chunks.push({strings: strings, callback: callback});
@@ -368,14 +366,14 @@ WebInspector.DeferredTempFile.prototype = {
      * @param {!WebInspector.OutputStream} outputStream
      * @param {!WebInspector.OutputStreamDelegate} delegate
      */
-    writeToOutputStream: function(outputStream, delegate)
+    copyToOutputStream: function(outputStream, delegate)
     {
-        if (this._callsPendingOpen) {
-            this._callsPendingOpen.push(this.writeToOutputStream.bind(this, outputStream, delegate));
+        if (!this._finishedWriting) {
+            this._pendingReads.push(this.copyToOutputStream.bind(this, outputStream, delegate));
             return;
         }
         if (this._tempFile)
-            this._tempFile.writeToOutputSteam(outputStream, delegate);
+            this._tempFile.copyToOutputStream(outputStream, delegate);
     },
 
     remove: function()
@@ -591,6 +589,6 @@ WebInspector.TempFileBackingStorage.prototype = {
      */
     writeToStream: function(outputStream, delegate)
     {
-        this._file.writeToOutputStream(outputStream, delegate);
+        this._file.copyToOutputStream(outputStream, delegate);
     }
 }
