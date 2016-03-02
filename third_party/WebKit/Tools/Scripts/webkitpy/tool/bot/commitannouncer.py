c6fe6ac7fceec671ea15d559022bbded6c239371
alancutter <alancutter@chromium.org>
2016-03-02 02:34:32 -0800

    Add ping command to #blink's commit-bot
    This command allows people to ping commit-bot on
    irc.freenode.net/#blink to see if it's still online.
    
    Review URL: https://codereview.chromium.org/1747273003
    
    Cr-Commit-Position: refs/heads/master@{#378713}

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/bot/commitannouncer.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/bot/commitannouncer.py
@@ -47,6 +47,7 @@ class CommitAnnouncer(SingleServerIRCBot):
         self.git = Git(cwd=tool.scm().checkout_root, filesystem=tool.filesystem, executive=tool.executive)
         self.commands = {
             'help': self.help,
+            'ping': self.ping,
             'quit': self.stop,
         }
 
@@ -81,6 +82,9 @@ class CommitAnnouncer(SingleServerIRCBot):
 
     def help(self):
         self._post('Commands available: %s' % ' '.join(self.commands.keys()))
+
+    def ping(self):
+        self._post('Pong.')
 
     def stop(self, message=""):
         self.connection.execute_delayed(0, lambda: self.die(message))
