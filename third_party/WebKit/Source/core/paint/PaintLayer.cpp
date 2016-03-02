96decf51941d6712818b2be32994b653c4551cd0
wangxianzhu <wangxianzhu@chromium.org>
2016-03-02 13:53:33 -0800

    Partly revert of Conditionally create PaintLayer's scrollable area object (patchset #8 id:140001 of https://codereview.chromium.org/1643663002/ )
    Reason for revert:
    Caused bug 591045. CompositedLayerMapping and ScrollingCoordinator don't expect
    scrollableArea of a layer to disappear.
    
    Revert functional changes, but keep refactories.
    
    BUG=591045
    
    Original issue's description:
    > Conditionally create PaintLayer's scrollable area object
    >
    > BUG=467721
    >
    > Committed: https://crrev.com/e3e7bae15616191d743019b9a5b77ac201aec20e
    > Cr-Commit-Position: refs/heads/master@{#378247}
    
    TBR=chrishtr@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=467721
    
    Review URL: https://codereview.chromium.org/1755243002
    
    Cr-Commit-Position: refs/heads/master@{#378845}

--- third_party/WebKit/Source/core/paint/PaintLayer.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayer.cpp
@@ -1470,14 +1470,9 @@ void PaintLayer::updateStackingNode()
 
 void PaintLayer::updateScrollableArea()
 {
-    if (requiresScrollableArea()) {
-        if (!m_scrollableArea)
-            m_scrollableArea = PaintLayerScrollableArea::create(*this);
-    } else if (m_scrollableArea) {
-        if (m_scrollableArea)
-            m_scrollableArea->dispose();
-        m_scrollableArea.clear();
-    }
+    ASSERT(!m_scrollableArea);
+    if (requiresScrollableArea())
+        m_scrollableArea = PaintLayerScrollableArea::create(*this);
 }
 
 bool PaintLayer::hasOverflowControls() const
@@ -2583,7 +2578,6 @@ void PaintLayer::styleChanged(StyleDifference diff, const ComputedStyle* oldStyl
     m_stackingNode->updateIsTreatedAsStackingContext();
     m_stackingNode->updateStackingNodesAfterStyleChange(oldStyle);
 
-    updateScrollableArea();
     if (m_scrollableArea)
         m_scrollableArea->updateAfterStyleChange(oldStyle);
 
