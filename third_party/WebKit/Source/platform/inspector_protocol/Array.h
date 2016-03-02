fabb9412afd811742b1836c23cab888ed451bc34
pfeldman <pfeldman@chromium.org>
2016-03-02 14:00:40 -0800

    DevTools: simplify protocol Value interface: remove iterators.
    BUG=580337
    
    Review URL: https://codereview.chromium.org/1759633003
    
    Cr-Commit-Position: refs/heads/master@{#378849}

--- third_party/WebKit/Source/platform/inspector_protocol/Array.h
+++ third_party/WebKit/Source/platform/inspector_protocol/Array.h
@@ -33,9 +33,9 @@ public:
         }
         errors->push();
         OwnPtr<Array<T>> result = adoptPtr(new Array<T>());
-        for (size_t i = 0; i < array->length(); ++i) {
+        for (size_t i = 0; i < array->size(); ++i) {
             errors->setName("[" + String::number(i) + "]");
-            T item = FromValue<T>::parse(array->get(i), errors);
+            T item = FromValue<T>::parse(array->at(i), errors);
             result->m_vector.append(item);
         }
         errors->pop();
@@ -93,9 +93,9 @@ public:
         }
         OwnPtr<Array<T>> result = adoptPtr(new Array<T>());
         errors->push();
-        for (size_t i = 0; i < array->length(); ++i) {
+        for (size_t i = 0; i < array->size(); ++i) {
             errors->setName("[" + String::number(i) + "]");
-            OwnPtr<T> item = FromValue<T>::parse(array->get(i), errors);
+            OwnPtr<T> item = FromValue<T>::parse(array->at(i), errors);
             result->m_vector.append(item.release());
         }
         errors->pop();
--- third_party/WebKit/Source/platform/inspector_protocol/Values.h
+++ third_party/WebKit/Source/platform/inspector_protocol/Values.h
@@ -126,13 +126,8 @@ private:
 };
 
 class PLATFORM_EXPORT DictionaryValue : public Value {
-private:
-    typedef HashMap<String, OwnPtr<Value>> Dictionary;
-
 public:
-    typedef Dictionary::iterator iterator;
-    typedef Dictionary::const_iterator const_iterator;
-
+    using Entry = std::pair<String, Value*>;
     static PassOwnPtr<DictionaryValue> create()
     {
         return adoptPtr(new DictionaryValue());
@@ -153,7 +148,7 @@ public:
     void writeJSON(StringBuilder* output) const override;
     PassOwnPtr<Value> clone() const override;
 
-    int size() const { return m_data.size(); }
+    size_t size() const { return m_data.size(); }
 
     void setBoolean(const String& name, bool);
     void setNumber(const String& name, double);
@@ -175,20 +170,17 @@ public:
     DictionaryValue* getObject(const String& name) const;
     ListValue* getArray(const String& name) const;
     Value* get(const String& name) const;
+    Entry at(size_t index) const;
 
     bool booleanProperty(const String& name, bool defaultValue) const;
-
     void remove(const String& name);
 
-    iterator begin() { return m_data.begin(); }
-    iterator end() { return m_data.end(); }
-    const_iterator begin() const { return m_data.begin(); }
-    const_iterator end() const { return m_data.end(); }
     ~DictionaryValue() override;
 
 private:
     DictionaryValue();
 
+    using Dictionary = HashMap<String, OwnPtr<Value>>;
     Dictionary m_data;
     Vector<String> m_order;
 };
@@ -219,8 +211,8 @@ public:
 
     void pushValue(PassOwnPtr<Value>);
 
-    Value* get(size_t index);
-    unsigned length() const { return m_data.size(); }
+    Value* at(size_t index);
+    size_t size() const { return m_data.size(); }
 
 private:
     ListValue();
