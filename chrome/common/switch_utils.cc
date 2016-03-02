91d9ed800569712fde3e11776e600387707485d0
fdoray <fdoray@chromium.org>
2016-03-02 07:46:01 -0800

    Remove the current /prefetch switch when relaunching Chrome.
    Currently, when a browser process is relaunched, the /prefetch:#
    switch of the current process is reused, which is wrong.
    
    For example, when a process initially launched in background is
    relaunched in foreground, it shouldn't reuse the /prefetch:#
    switch dedicated to background browser processes.
    
    With this CL, RemoveSwitchesForAutostart() removes the
    /prefetch:# switch from the command line, if present.
    
    BUG=577698
    
    Review URL: https://codereview.chromium.org/1734053002
    
    Cr-Commit-Position: refs/heads/master@{#378751}

--- chrome/common/switch_utils.cc
+++ chrome/common/switch_utils.cc
@@ -8,9 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "base/macros.h"
+#include "build/build_config.h"
 #include "chrome/common/chrome_switches.h"
 
+#if defined(OS_WIN)
+#include "base/strings/string_util.h"
+#endif  // defined(OS_WIN)
+
 namespace switches {
+
+namespace {
 
 // Switches enumerated here will be removed when a background instance of
 // Chrome restarts itself. If your key is designed to only be used once,
@@ -27,10 +34,25 @@ const char* const kSwitchesToRemoveOnAutorestart[] = {
   switches::kWinJumplistAction
 };
 
+}  // namespace
+
 void RemoveSwitchesForAutostart(
     std::map<std::string, base::CommandLine::StringType>* switch_list) {
   for (size_t i = 0; i < arraysize(kSwitchesToRemoveOnAutorestart); ++i)
     switch_list->erase(kSwitchesToRemoveOnAutorestart[i]);
+
+#if defined(OS_WIN)
+  // The relaunched browser process shouldn't reuse the /prefetch:# switch of
+  // the current process because the process type can change (e.g. a process
+  // initially launched in background can be relaunched in foreground).
+  static const char kPrefetchSwitchPrefix[] = "prefetch:";
+  auto it = switch_list->lower_bound(kPrefetchSwitchPrefix);
+  if (it != switch_list->end() &&
+      base::StartsWith(it->first, kPrefetchSwitchPrefix,
+                       base::CompareCase::SENSITIVE)) {
+    switch_list->erase(it);
+  }
+#endif  // defined(OS_WIN)
 }
 
 }  // namespace switches
