b954765f5d40b8427485b9480dfb8f353315e397
mmoroz <mmoroz@chromium.org>
2016-03-02 08:57:17 -0800

    [libfuzzer] support of custom libfuzzer options via .options file.
    1) remove 'fuzzer_test_launcher', use 'fuzzer_test' for both fuzzers and tests
    2) copy dictionary with name like '<fuzzer_name>.dict' (otherwise there are
    warnings if one dictionary is used for mor than one fuzzer)
    3) remove .sh-warpper for fuzzers because it is platform-dependent
    4) copy .options file with name like '<fuzzer_name>.options' if no 'dict' used
    and update/generate .options file in the build root if 'dict' is used
    5) a couple of tests for updated build template and .options generation
    
    R=aizatsky@chromium.org, inferno@chromium.org, krasin@chromium.org
    BUG=539572
    
    Review URL: https://codereview.chromium.org/1703523002
    
    Cr-Commit-Position: refs/heads/master@{#378761}

---
+++ testing/libfuzzer/tests/check_fuzzer_config.py
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/python2
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Script that prints out "option=value" from the config file. Used for testing.
+
+import ConfigParser
+import os
+import sys
+
+OPTIONS_SECTION_LIBFUZZER = 'libfuzzer'
+
+config_path = os.path.join(os.path.dirname(sys.argv[0]), sys.argv[1])
+fuzzer_config = ConfigParser.ConfigParser()
+fuzzer_config.read(config_path)
+
+if not fuzzer_config.has_section(OPTIONS_SECTION_LIBFUZZER):
+  sys.exit(-1)
+
+for option_name, option_value in fuzzer_config.items(OPTIONS_SECTION_LIBFUZZER):
+  sys.stdout.write('%s=%s\n' % (option_name, option_value))
