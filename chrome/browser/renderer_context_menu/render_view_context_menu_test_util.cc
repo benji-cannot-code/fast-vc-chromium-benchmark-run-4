b2c3279b397b70a7a3ab446b66990792d4747933
lwchkg <lwchkg@gmail.com>
2016-03-03 03:54:14 -0800

    Fix current profile is unnecessary added to "Open link as" context menu.
    The CL which introduced the bug is https://crrev.com/378323
    
    This CL also updated a few tests for check for regressions of the same
    type.
    
    BUG=591331
    TEST=pass the browser tests in this CL
    
    Review URL: https://codereview.chromium.org/1754393002
    
    Cr-Commit-Position: refs/heads/master@{#378999}

--- chrome/browser/renderer_context_menu/render_view_context_menu_test_util.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu_test_util.cc
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <string>
+#include <vector>
 
+#include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/renderer_context_menu/render_view_context_menu_test_util.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/models/menu_model.h"
@@ -80,13 +82,13 @@ bool TestRenderViewContextMenu::GetMenuModelAndItemIndex(
   return false;
 }
 
-size_t TestRenderViewContextMenu::GetItemIndexByProfilePath(
+int TestRenderViewContextMenu::GetCommandIDByProfilePath(
     const base::FilePath& path) {
   size_t count = profile_link_paths_.size();
   for (size_t i = 0; i < count; ++i)
     if (profile_link_paths_[i] == path)
-      return i;
-  return std::string::npos;
+      return IDC_OPEN_LINK_IN_PROFILE_FIRST + static_cast<int>(i);
+  return -1;
 }
 
 void TestRenderViewContextMenu::Show() {
