136b434f18ad05eb56b323636c9a54651763aa53
dimich <dimich@chromium.org>
2016-03-03 11:46:21 -0800

    Revert of Use FontFallbackPriority Fonts in FontFallbackIterator (patchset #6 id:100001 of https://codereview.chromium.org/1672553004/ )
    Reason for revert:
    Broke layout tests: https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win10/builds/7227
    
    See http://crbug.com/591778
    
    Original issue's description:
    > Use FontFallbackPriority Fonts in FontFallbackIterator
    >
    > Now that the pieces are in place, use the emoji and symbol fonts in
    > FontFallbackIterator. Previously FontCache's character fallback
    > functions were extended so that they take FontFallbackPriority into
    > account. These functions can now be used in FontFallbackIterator as a
    > fallback priority stage where those fonts are used as the first fallback
    > font in order to correctly distinguish between emoji and text
    > presentation emoji.
    >
    > BUG=549571
    > R=eae, behdad
    >
    > Committed: https://crrev.com/978d4a1ba3d9654905378de192e64f9232e62798
    > Cr-Commit-Position: refs/heads/master@{#379036}
    
    TBR=behdad@chromium.org,eae@chromium.org,drott@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=549571
    
    Review URL: https://codereview.chromium.org/1758383002
    
    Cr-Commit-Position: refs/heads/master@{#379062}

--- third_party/WebKit/Source/platform/fonts/Font.h
+++ third_party/WebKit/Source/platform/fonts/Font.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/PlatformExport.h"
 #include "platform/fonts/FontDescription.h"
 #include "platform/fonts/FontFallbackList.h"
-#include "platform/fonts/FontFallbackPriority.h"
 #include "platform/fonts/SimpleFontData.h"
 #include "platform/text/TabSize.h"
 #include "platform/text/TextDirection.h"
@@ -141,8 +140,7 @@ private:
 
 public:
     FontSelector* fontSelector() const;
-    PassRefPtr<FontFallbackIterator> createFontFallbackIterator(
-        FontFallbackPriority) const;
+    PassRefPtr<FontFallbackIterator> createFontFallbackIterator() const;
 
     void willUseFontData(UChar32) const;
 
