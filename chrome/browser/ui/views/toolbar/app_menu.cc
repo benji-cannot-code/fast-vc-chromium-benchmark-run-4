ea4a2603c356e872d27f262177b623eab30067df
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-03-03 10:00:05 -0800

    [Extensions UI Views] Adjust the wrench menu actions for items added/removed
    Extension items can be added or removed to the wrench menu while it is open
    (if, for instance, a popup closes and slides the item back in, or, more rarely,
    the extension is only just added, crashes, etc). In rare cases, this can result
    in the container needing to add or remove a row, which means the rest of the
    menu's height needs to adjust.
    
    BUG=591028
    
    Review URL: https://codereview.chromium.org/1757993003
    
    Cr-Commit-Position: refs/heads/master@{#379035}

--- chrome/browser/ui/views/toolbar/app_menu.cc
+++ chrome/browser/ui/views/toolbar/app_menu.cc
@@ -1153,7 +1153,7 @@ void AppMenu::PopulateMenu(MenuItemView* parent, MenuModel* model) {
     switch (model->GetCommandIdAt(i)) {
       case IDC_EXTENSIONS_OVERFLOW_MENU: {
         scoped_ptr<ExtensionToolbarMenuView> extension_toolbar(
-            new ExtensionToolbarMenuView(browser_, this));
+            new ExtensionToolbarMenuView(browser_, this, item));
         if (!extension_toolbar->ShouldShow()) {
           item->SetVisible(false);
           extension_toolbar_ = nullptr;
