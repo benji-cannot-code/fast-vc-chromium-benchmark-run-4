c4caac2437e888d21e1eead087c697fbda07644e
haibinlu <haibinlu@chromium.org>
2016-03-03 14:21:54 -0800

    [Blimp Engine] Use URLRequestContextBuilder to properly create URLRequestContext.
    BUG=590251
    
    Review URL: https://codereview.chromium.org/1764493002
    
    Cr-Commit-Position: refs/heads/master@{#379112}

--- net/url_request/url_request_context_builder.h
+++ net/url_request/url_request_context_builder.h
@@ -16,9 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NET_URL_REQUEST_URL_REQUEST_CONTEXT_BUILDER_H_
 
 #include <stdint.h>
+#include <map>
 #include <string>
 #include <unordered_map>
 #include <utility>
+#include <vector>
 
 #include "base/files/file_path.h"
 #include "base/macros.h"
@@ -34,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/proxy/proxy_service.h"
 #include "net/quic/quic_protocol.h"
 #include "net/socket/next_proto.h"
+#include "net/url_request/url_request_job_factory.h"
 
 namespace base {
 class SingleThreadTaskRunner;
@@ -154,6 +157,12 @@ class NET_EXPORT URLRequestContextBuilder {
     ftp_enabled_ = enable;
   }
 #endif
+
+  // Sets a valid ProtocolHandler for a scheme.
+  // A ProtocolHandler already exists for |scheme| will be overwritten.
+  void SetProtocolHandler(
+      const std::string& scheme,
+      scoped_ptr<URLRequestJobFactory::ProtocolHandler> protocol_handler);
 
   // Unlike the other setters, the builder does not take ownership of the
   // NetLog.
@@ -346,6 +355,8 @@ class NET_EXPORT URLRequestContextBuilder {
   scoped_ptr<CertVerifier> cert_verifier_;
   std::vector<scoped_ptr<URLRequestInterceptor>> url_request_interceptors_;
   scoped_ptr<HttpServerProperties> http_server_properties_;
+  std::map<std::string, scoped_ptr<URLRequestJobFactory::ProtocolHandler>>
+      protocol_handlers_;
 
   DISALLOW_COPY_AND_ASSIGN(URLRequestContextBuilder);
 };
