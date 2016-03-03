053bd64afe1467f3f8b0fd77acd81995c9354368
mvanouwerkerk <mvanouwerkerk@chromium.org>
2016-03-03 11:01:02 -0800

    Geolocation: use the name enable_high_accuracy consistently.
    This is the name of the boolean in the JavaScript exposed API and the majority
    of the implementation.
    
    * In Java: rename gpsEnabled/isGpsEnabled to enableHighAccuracy
    * In C++: rename use_high_accuracy to enable_high_accuracy
    
    Review URL: https://codereview.chromium.org/1760883002
    
    Cr-Commit-Position: refs/heads/master@{#379049}

--- content/public/android/java/src/org/chromium/content/browser/LocationProviderFactory.java
+++ content/public/android/java/src/org/chromium/content/browser/LocationProviderFactory.java
@@ -29,7 +29,7 @@ public class LocationProviderFactory {
      * LocationProviderFactory.get() returns an instance of this interface.
      */
     public interface LocationProvider {
-        public void start(boolean gpsEnabled);
+        public void start(boolean enableHighAccuracy);
         public void stop();
         public boolean isRunning();
     }
@@ -71,12 +71,12 @@ public class LocationProviderFactory {
 
         /**
          * Start listening for location updates.
-         * @param gpsEnabled Whether or not we're interested in high accuracy GPS.
+         * @param enableHighAccuracy Whether or not to enable high accuracy location providers.
          */
         @Override
-        public void start(boolean gpsEnabled) {
+        public void start(boolean enableHighAccuracy) {
             unregisterFromLocationUpdates();
-            registerForLocationUpdates(gpsEnabled);
+            registerForLocationUpdates(enableHighAccuracy);
         }
 
         /**
@@ -139,7 +139,7 @@ public class LocationProviderFactory {
         /**
          * Registers this object with the location service.
          */
-        private void registerForLocationUpdates(boolean isGpsEnabled) {
+        private void registerForLocationUpdates(boolean enableHighAccuracy) {
             ensureLocationManagerCreated();
             if (usePassiveOneShotLocation()) return;
 
@@ -150,7 +150,7 @@ public class LocationProviderFactory {
             // bounce notifications to the Geolocation thread as they arrive in the mainLooper.
             try {
                 Criteria criteria = new Criteria();
-                if (isGpsEnabled) criteria.setAccuracy(Criteria.ACCURACY_FINE);
+                if (enableHighAccuracy) criteria.setAccuracy(Criteria.ACCURACY_FINE);
                 mLocationManager.requestLocationUpdates(0, 0, criteria, this,
                         ThreadUtils.getUiThreadLooper());
             } catch (SecurityException e) {
