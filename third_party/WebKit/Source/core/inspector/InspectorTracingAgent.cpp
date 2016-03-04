6c266723d50337ed1eec74bfc854458d6eca4437
dgozman <dgozman@chromium.org>
2016-03-03 17:53:03 -0800

    [DevTools] Fix crash in InspectorTracingAgent.
    |m_state| can be null at the time we call setLayerTreeId.
    
    BUG=591622
    
    Review URL: https://codereview.chromium.org/1761793004
    
    Cr-Commit-Position: refs/heads/master@{#379174}

--- third_party/WebKit/Source/core/inspector/InspectorTracingAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorTracingAgent.cpp
@@ -68,7 +68,8 @@ void InspectorTracingAgent::end(ErrorString* errorString, PassOwnPtr<EndCallback
 String InspectorTracingAgent::sessionId()
 {
     String result;
-    m_state->getString(TracingAgentState::sessionId, &result);
+    if (m_state)
+        m_state->getString(TracingAgentState::sessionId, &result);
     return result;
 }
 
