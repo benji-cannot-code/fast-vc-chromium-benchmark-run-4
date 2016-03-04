560882176027fca889b4ff8491f74ea293b38209
tyoshino <tyoshino@chromium.org>
2016-03-03 19:09:25 -0800

    Remove an unused forward declaration.
    WebWaitableEvent is gone.
    
    R=yoav@yoav.ws
    BUG=none
    
    Review URL: https://codereview.chromium.org/1759143002
    
    Cr-Commit-Position: refs/heads/master@{#379194}

--- third_party/WebKit/Source/core/html/parser/HTMLParserThread.h
+++ third_party/WebKit/Source/core/html/parser/HTMLParserThread.h
@@ -42,8 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class WebWaitableEvent;
-
 class CORE_EXPORT HTMLParserThread {
     USING_FAST_MALLOC(HTMLParserThread);
 public:
