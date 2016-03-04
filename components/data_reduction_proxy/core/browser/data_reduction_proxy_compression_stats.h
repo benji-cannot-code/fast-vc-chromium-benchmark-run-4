89d492222d77bad175d5047c2fe4cf1ae6245c70
kundaji <kundaji@chromium.org>
2016-03-04 11:05:41 -0800

    Remove data_reduction.update_daily_lengths preference.
    The Data Saver extension would set this preference to indicate that
    Chrome should update the data_reduction.daily_original_length and
    data_reduction.daily_received_length preferences with data usage
    and data savings information, which would then be read by the
    extension.
    
    These preferences are no longer used by the Data Saver extension and has
    been replaced by the dataReductionProxy.getDataUsage method in Chrome
    API, which is accessible only to the whitelisted Data Saver extension.
    
    BUG=442494
    
    Review URL: https://codereview.chromium.org/1762483002
    
    Cr-Commit-Position: refs/heads/master@{#379326}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_compression_stats.h
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_compression_stats.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/scoped_ptr_hash_map.h"
 #include "base/macros.h"
-#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
@@ -24,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_reduction_proxy/core/browser/db_data_owner.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_pref_names.h"
 #include "components/data_reduction_proxy/proto/data_store.pb.h"
-#include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_member.h"
 #include "net/base/network_change_notifier.h"
 
@@ -238,7 +236,6 @@ class DataReductionProxyCompressionStats
   const base::TimeDelta delay_;
   DataReductionProxyPrefMap pref_map_;
   DataReductionProxyListPrefMap list_pref_map_;
-  PrefChangeRegistrar pref_change_registrar_;
   BooleanPrefMember data_usage_reporting_enabled_;
   ConnectionType connection_type_;
 
