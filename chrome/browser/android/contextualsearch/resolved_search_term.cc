93c4e1b438e0b8ffbdf5d396c9f7456be90b970e
donnd <donnd@chromium.org>
2016-03-04 12:29:08 -0800

    [Contextual Search] Refactor native code to ease 1-Request.
    Refactor native code in contextual_search_delegate to make the transition to the single-request model easier.
    Add a method that uses gets the Resolved Search Term from a JSON string.  In two-request it comes from the response, in single-request comes from the header.
    Stop adding the base-page URL to the Resolve Request -- it's being added to the Discourse Context.
    
    BUG=526291
    
    Review URL: https://codereview.chromium.org/1726293002
    
    Cr-Commit-Position: refs/heads/master@{#379347}

--- chrome/browser/android/contextualsearch/resolved_search_term.cc
+++ chrome/browser/android/contextualsearch/resolved_search_term.cc
@@ -5,6 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/android/contextualsearch/resolved_search_term.h"
 
+#include "net/url_request/url_fetcher.h"
+
+ResolvedSearchTerm::ResolvedSearchTerm(int response_code)
+    : is_invalid(response_code == net::URLFetcher::RESPONSE_CODE_INVALID),
+      response_code(response_code),
+      search_term(""),
+      display_text(""),
+      alternate_term(""),
+      prevent_preload(false),
+      selection_start_adjust(0),
+      selection_end_adjust(0),
+      context_language("") {}
+
 ResolvedSearchTerm::ResolvedSearchTerm(bool is_invalid,
                                        int response_code,
                                        const std::string& search_term,
