f7f311af1c42a722d671a5903b67b8a5a6e06237
kozyatinskiy <kozyatinskiy@chromium.org>
2016-03-03 23:36:02 -0800

    [DevTools] Fix compiler errors in bindings module
    BUG=591865
    R=lushnikov@chromium.org
    
    Review URL: https://codereview.chromium.org/1763883003
    
    Cr-Commit-Position: refs/heads/master@{#379240}

--- third_party/WebKit/Source/devtools/front_end/bindings/StylesSourceMapping.js
+++ third_party/WebKit/Source/devtools/front_end/bindings/StylesSourceMapping.js
@@ -183,7 +183,7 @@ WebInspector.StylesSourceMapping.prototype = {
 
     _unbindAllUISourceCodes: function()
     {
-        for (var styleFile of this._styleFiles.keys())
+        for (var styleFile of this._styleFiles.values())
             styleFile.dispose();
         this._styleFiles.clear();
         this._urlToHeadersByFrameId = new Map();
