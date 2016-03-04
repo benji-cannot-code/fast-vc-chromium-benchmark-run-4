abf5afeed4e1a00a2dfcac74e4b1070c294f52f1
sergeyv <sergeyv@chromium.org>
2016-03-03 20:17:39 -0800

    Devtools: resolve parameters names via sourcemaps
    BUG=
    
    Review URL: https://codereview.chromium.org/1766513002
    
    Cr-Commit-Position: refs/heads/master@{#379206}

--- third_party/WebKit/LayoutTests/inspector/sources/debugger/resources/resolve-variable-names-compressed.js
+++ third_party/WebKit/LayoutTests/inspector/sources/debugger/resources/resolve-variable-names-compressed.js
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-function testFunction(){var e={};var n=new Map;debugger;return n.get(e)}
+function foo(o,t){try{throw"boom!"}catch(e){var n={};var r=new Map;r.set(o,t);debugger;return r.get(n)}}function testFunction(){foo(100,"hello")}
 //# sourceMappingURL=resolve-variable.js.map
--- third_party/WebKit/LayoutTests/inspector/sources/debugger/resources/resolve-variable-names-origin.js
+++ third_party/WebKit/LayoutTests/inspector/sources/debugger/resources/resolve-variable-names-origin.js
@@ -1,8 +1,18 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function foo(parameter1, parameter2)
+{
+    try {
+        throw "boom!";
+    } catch (error) {
+        var longObject = {};
+        var longMap = new Map();
+        longMap.set(parameter1, parameter2);
+        debugger;
+        return longMap.get(longObject);
+    }
+}
+
 function testFunction()
 {
-    var longObject = {};
-    var longMap = new Map();
-    debugger;
-    return longMap.get(longObject);
+    foo(100, "hello");
 }
