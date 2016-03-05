1ed02b55766d290e5c2a5445efd5f299ff45a5c4
hiroshige <hiroshige@chromium.org>
2016-03-04 18:17:18 -0800

    [MemoryCache] Exclude data URLs and SubstituteData from MemoryCache UMAs
    BUG=591079
    
    Review URL: https://codereview.chromium.org/1771473002
    
    Cr-Commit-Position: refs/heads/master@{#379435}

--- third_party/WebKit/Source/core/fetch/ResourceFetcher.h
+++ third_party/WebKit/Source/core/fetch/ResourceFetcher.h
@@ -182,6 +182,8 @@ private:
 
     void willTerminateResourceLoader(ResourceLoader*);
 
+    void updateMemoryCacheStats(Resource*, RevalidationPolicy, const FetchRequest&,  const ResourceFactory&, bool isStaticData) const;
+
     Member<FetchContext> m_context;
 
     HashSet<String> m_validatedURLs;
