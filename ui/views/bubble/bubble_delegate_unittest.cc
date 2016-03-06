0e42720030e7da0b6886508fd506ef3e1d75c5a7
patricialor <patricialor@chromium.org>
2016-03-06 15:48:57 -0800

    MacViews: Fix views_unittests BubbleDelegateTest.CloseReasons
    Test added in r360848, fails on Mac as the bubble's parent anchor is not being
    shown before the bubble is expected to appear - this causes the bubble to be
    treated as 'hidden' as its parent is not visible. This should actually be
    expected behaviour on all platforms, so the failure is actually indicating
    there's a bug on Aura (see http://crbug/590957).
    
    Workaround this for now by showing the bubble's anchor widget first to allow
    this to pass for all platforms.
    
    Also add ScopedFakeNSWindowFocus to all tests to simulate focus on the parent,
    as well as defend against test flakiness which might be caused by window
    activation issues.
    
    BUG=579380,590957
    
    Review URL: https://codereview.chromium.org/1637383003
    
    Cr-Commit-Position: refs/heads/master@{#379499}

--- ui/views/bubble/bubble_delegate_unittest.cc
+++ ui/views/bubble/bubble_delegate_unittest.cc
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_observer.h"
 
+#if defined(OS_MACOSX)
+#include "ui/base/test/scoped_fake_nswindow_focus.h"
+#endif
+
 namespace views {
 
 namespace {
@@ -67,6 +71,11 @@ class BubbleDelegateTest : public ViewsTestBase {
   }
 
  private:
+#if defined(OS_MACOSX)
+  // Ensure tests running in parallel don't steal focus from the Widget on Mac.
+  ui::test::ScopedFakeNSWindowFocus fake_focus_;
+#endif
+
   DISALLOW_COPY_AND_ASSIGN(BubbleDelegateTest);
 };
 
@@ -278,6 +287,7 @@ TEST_F(BubbleDelegateTest, CloseReasons) {
         anchor_widget->GetContentsView(), BubbleBorder::NONE);
     bubble_delegate->set_close_on_deactivate(true);
     Widget* bubble_widget = BubbleDelegateView::CreateBubble(bubble_delegate);
+    anchor_widget->Show();
     bubble_widget->Show();
     anchor_widget->Activate();
     EXPECT_TRUE(bubble_widget->IsClosed());
