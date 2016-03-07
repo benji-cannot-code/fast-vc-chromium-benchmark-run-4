cdcaed21fbd5f98579c83d34859b79004b829efd
tsergeant <tsergeant@chromium.org>
2016-03-06 17:02:33 -0800

    MD History: Implement search functionality.
    Search bar in the toolbar sends dynamic search requests as the user is typing
    and these results are displayed on one history-card. As you scroll, if there are
    more results these are loaded, and the number of results found is updated.
    
    Within each item: if the title contains the search term, this is bolded.
    
    Based off https://codereview.chromium.org/1643693003/ by hsampson@google.com
    
    BUG=425625
    
    Review URL: https://codereview.chromium.org/1722443002
    
    Cr-Commit-Position: refs/heads/master@{#379504}

--- chrome/test/data/webui/md_history/history_item_test.js
+++ chrome/test/data/webui/md_history/history_item_test.js
@@ -12,6 +12,12 @@ cr.define('md_history.history_item_test', function() {
     {"time": "1"}
   ];
 
+  var SEARCH_HISTORY_RESULTS = [
+    {"dateShort": "Feb 22, 2016"},
+    {"dateShort": "Feb 21, 2016"},
+    {"dateShort": "Feb 21, 2016"},
+  ];
+
   function registerTests() {
     suite('history-item', function() {
       var element;
@@ -21,12 +27,12 @@ cr.define('md_history.history_item_test', function() {
       });
 
       setup(function() {
-        element.addNewResults(TEST_HISTORY_RESULTS);
+        element.addNewResults(TEST_HISTORY_RESULTS, '');
       });
 
       test('basic separator insertion', function(done) {
         flush(function() {
-          // Check that the correct numbegitr of time gaps are inserted.
+          // Check that the correct number of time gaps are inserted.
           var items =
               Polymer.dom(element.root).querySelectorAll('history-item');
 
@@ -35,6 +41,20 @@ cr.define('md_history.history_item_test', function() {
           assertFalse(items[2].hasTimeGap);
           assertTrue(items[3].hasTimeGap);
           assertFalse(items[4].hasTimeGap);
+
+          done();
+        });
+      });
+
+      test('separator insertion for search', function(done) {
+        element.addNewResults(SEARCH_HISTORY_RESULTS, 'search');
+        flush(function() {
+          var items =
+              Polymer.dom(element.root).querySelectorAll('history-item');
+
+          assertTrue(items[0].hasTimeGap);
+          assertFalse(items[1].hasTimeGap);
+          assertFalse(items[2].hasTimeGap);
 
           done();
         });
