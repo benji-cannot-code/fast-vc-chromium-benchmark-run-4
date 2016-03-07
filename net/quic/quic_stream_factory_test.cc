03d4dcea45968a2912c1c21e0fd30494f9aa24e3
anthonyvd <anthonyvd@chromium.org>
2016-03-07 14:03:57 -0800

    Revert of Remove support for Alt-Svc/Alternate Protocol Probability (patchset #7 id:120001 of https://codereview.chromium.org/1699653002/ )
    Reason for revert:
    This appears to have broken the following bot: https://build.chromium.org/p/chromium.mac/builders/iOS_Device_%28ninja%29/builds/43364
    
    Original issue's description:
    > Remove support forAlt-Svc/Alternate Protocol Probability
    >
    > Committed: https://crrev.com/dced4c771d25317d26a0f6e258c7b7f64d4e8eea
    > Cr-Commit-Position: refs/heads/master@{#379652}
    
    TBR=bnc@chromium.org,eroman@chromium.org,mef@chromium.org,rch@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1773853003
    
    Cr-Commit-Position: refs/heads/master@{#379658}

--- net/quic/quic_stream_factory_test.cc
+++ net/quic/quic_stream_factory_test.cc
@@ -2326,7 +2326,7 @@ TEST_P(QuicStreamFactoryTest, RacingConnections) {
   AlternativeServiceInfoVector alternative_service_info_vector;
   base::Time expiration = base::Time::Now() + base::TimeDelta::FromDays(1);
   alternative_service_info_vector.push_back(
-      AlternativeServiceInfo(alternative_service1, expiration));
+      AlternativeServiceInfo(alternative_service1, 1.0, expiration));
 
   http_server_properties_.SetAlternativeServices(
       host_port_pair_, alternative_service_info_vector);
@@ -3399,7 +3399,7 @@ TEST_P(QuicStreamFactoryTest, MaybeInitialize) {
   AlternativeServiceInfoVector alternative_service_info_vector;
   base::Time expiration = base::Time::Now() + base::TimeDelta::FromDays(1);
   alternative_service_info_vector.push_back(
-      AlternativeServiceInfo(alternative_service1, expiration));
+      AlternativeServiceInfo(alternative_service1, 1.0, expiration));
 
   http_server_properties_.SetAlternativeServices(
       host_port_pair_, alternative_service_info_vector);
--- net/spdy/spdy_network_transaction_unittest.cc
+++ net/spdy/spdy_network_transaction_unittest.cc
@@ -113,7 +113,7 @@ void UpdateSpdySessionDependencies(SpdyNetworkTransactionTestParams test_params,
         HostPortPair("www.example.org", 80),
         AlternativeService(AlternateProtocolFromNextProto(test_params.protocol),
                            "www.example.org", 443),
-        expiration);
+        1.0, expiration);
   }
 }
 
