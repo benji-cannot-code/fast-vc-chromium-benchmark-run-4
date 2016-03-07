7c829a24947f7b4efc24bbc0eb5b651da2e61a78
timloh <timloh@chromium.org>
2016-03-06 18:08:18 -0800

    Allow setting custom properties from @apply rules
    This patch adds support for setting custom properties in @apply rules.
    The model implemented here is that custom properties get a first value,
    which can be used in @apply rules (not including those inside custom
    property values), and then may get a second (final) value after @apply
    rules are applied.
    
    BUG=586974
    
    Review URL: https://codereview.chromium.org/1724183007
    
    Cr-Commit-Position: refs/heads/master@{#379507}

--- third_party/WebKit/Source/core/css/CSSVariableData.cpp
+++ third_party/WebKit/Source/core/css/CSSVariableData.cpp
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-const StylePropertySet* CSSVariableData::propertySet()
+StylePropertySet* CSSVariableData::propertySet()
 {
     ASSERT(!m_needsVariableResolution);
     if (!m_cachedPropertySet) {
--- third_party/WebKit/Source/core/css/resolver/StyleResolverState.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleResolverState.cpp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/resolver/StyleResolverState.h"
 
 #include "core/animation/css/CSSAnimations.h"
+#include "core/css/StylePropertySet.h"
 #include "core/dom/Node.h"
 #include "core/dom/NodeComputedStyle.h"
 #include "core/frame/FrameHost.h"
@@ -71,6 +72,16 @@ CSSToLengthConversionData StyleResolverState::fontSizeConversionData() const
 void StyleResolverState::loadPendingResources()
 {
     m_elementStyleResources.loadPendingResources(style());
+}
+
+void StyleResolverState::setCustomPropertySetForApplyAtRule(const String& string, StylePropertySet* customPropertySet)
+{
+    m_customPropertySetsForApplyAtRule.set(string, customPropertySet);
+}
+
+StylePropertySet* StyleResolverState::customPropertySetForApplyAtRule(const String& string)
+{
+    return m_customPropertySetsForApplyAtRule.get(string);
 }
 
 } // namespace blink
