f37b17845fdac4cda1d028d5f6e6c1b656324024
atanasova <atanasova@chromium.org>
2016-03-07 02:49:46 -0800

    Start recording large icon path for whitelists.
    This is a first step in order to show the large icon associated with a
    whitelist on the NTP. This CL saves the large icon path in the
    SupervisedUserSiteList.
    
    BUG=586097
    
    Review URL: https://codereview.chromium.org/1758633003
    
    Cr-Commit-Position: refs/heads/master@{#379535}

--- chrome/browser/component_updater/supervised_user_whitelist_installer.cc
+++ chrome/browser/component_updater/supervised_user_whitelist_installer.cc
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequenced_task_runner.h"
 #include "base/strings/string_util.h"
 #include "base/thread_task_runner_handle.h"
+#include "base/value_conversions.h"
+#include "base/values.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
 #include "chrome/browser/supervised_user/supervised_user_whitelist_service.h"
 #include "chrome/common/chrome_paths.h"
@@ -55,6 +57,8 @@ const char kName[] = "name";
 // (since it's not built on Android).
 const char kExtensionName[] = "name";
 const char kExtensionShortName[] = "short_name";
+const char kExtensionIcons[] = "icons";
+const char kExtensionLargeIcon[] = "128";
 
 base::string16 GetWhitelistTitle(const base::DictionaryValue& manifest) {
   base::string16 title;
@@ -63,14 +67,33 @@ base::string16 GetWhitelistTitle(const base::DictionaryValue& manifest) {
   return title;
 }
 
+base::FilePath GetLargeIconPath(const base::DictionaryValue& manifest,
+                                const base::FilePath& install_dir) {
+  const base::DictionaryValue* icons = nullptr;
+  if (!manifest.GetDictionary(kExtensionIcons, &icons))
+    return base::FilePath();
+
+  base::FilePath path;
+  const base::Value* path_value = nullptr;
+  if (!icons->Get(kExtensionLargeIcon, &path_value))
+    return base::FilePath();
+  if (!base::GetValueAsFilePath(*path_value, &path))
+    return base::FilePath();
+
+  return install_dir.Append(path);
+}
+
 base::FilePath GetRawWhitelistPath(const base::DictionaryValue& manifest,
                                    const base::FilePath& install_dir) {
   const base::DictionaryValue* whitelist_dict = nullptr;
   if (!manifest.GetDictionary(kWhitelistedContent, &whitelist_dict))
     return base::FilePath();
 
-  base::FilePath::StringType whitelist_file;
-  if (!whitelist_dict->GetString(kSites, &whitelist_file))
+  base::FilePath whitelist_file;
+  const base::Value* whitelist_file_value = nullptr;
+  if (!whitelist_dict->Get(kSites, &whitelist_file_value))
+    return base::FilePath();
+  if (!base::GetValueAsFilePath(*whitelist_file_value, &whitelist_file))
     return base::FilePath();
 
   return install_dir.Append(whitelist_file);
@@ -208,7 +231,9 @@ class SupervisedUserWhitelistComponentInstallerTraits
     : public ComponentInstallerTraits {
  public:
   using RawWhitelistReadyCallback =
-      base::Callback<void(const base::string16&, const base::FilePath&)>;
+      base::Callback<void(const base::string16&, /* title */
+                          const base::FilePath&, /* icon_path */
+                          const base::FilePath& /* whitelist_path */)>;
 
   SupervisedUserWhitelistComponentInstallerTraits(
       const std::string& crx_id,
@@ -265,6 +290,7 @@ void SupervisedUserWhitelistComponentInstallerTraits::ComponentReady(
   // using extension_l10n_util::LocalizeExtension, but that doesn't exist on
   // Android. crbug.com/558387
   callback_.Run(GetWhitelistTitle(*manifest),
+                GetLargeIconPath(*manifest, install_dir),
                 GetRawWhitelistPath(*manifest, install_dir));
 }
 
@@ -305,9 +331,11 @@ class SupervisedUserWhitelistInstallerImpl
 
   void OnRawWhitelistReady(const std::string& crx_id,
                            const base::string16& title,
+                           const base::FilePath& large_icon_path,
                            const base::FilePath& whitelist_path);
   void OnSanitizedWhitelistReady(const std::string& crx_id,
-                                 const base::string16& title);
+                                 const base::string16& title,
+                                 const base::FilePath& large_icon_path);
 
   // SupervisedUserWhitelistInstaller overrides:
   void RegisterComponents() override;
@@ -400,6 +428,7 @@ bool SupervisedUserWhitelistInstallerImpl::UnregisterWhitelistInternal(
 void SupervisedUserWhitelistInstallerImpl::OnRawWhitelistReady(
     const std::string& crx_id,
     const base::string16& title,
+    const base::FilePath& large_icon_path,
     const base::FilePath& whitelist_path) {
   cus_->GetSequencedTaskRunner()->PostTask(
       FROM_HERE,
@@ -408,14 +437,16 @@ void SupervisedUserWhitelistInstallerImpl::OnRawWhitelistReady(
           base::ThreadTaskRunnerHandle::Get(),
           base::Bind(
               &SupervisedUserWhitelistInstallerImpl::OnSanitizedWhitelistReady,
-              weak_ptr_factory_.GetWeakPtr(), crx_id, title)));
+              weak_ptr_factory_.GetWeakPtr(), crx_id, title, large_icon_path)));
 }
 
 void SupervisedUserWhitelistInstallerImpl::OnSanitizedWhitelistReady(
     const std::string& crx_id,
-    const base::string16& title) {
+    const base::string16& title,
+    const base::FilePath& large_icon_path) {
   for (const WhitelistReadyCallback& callback : callbacks_)
-    callback.Run(crx_id, title, GetSanitizedWhitelistPath(crx_id));
+    callback.Run(crx_id, title, large_icon_path,
+                 GetSanitizedWhitelistPath(crx_id));
 }
 
 void SupervisedUserWhitelistInstallerImpl::RegisterComponents() {
--- chrome/browser/supervised_user/supervised_user_site_list.cc
+++ chrome/browser/supervised_user/supervised_user_site_list.cc
@@ -87,6 +87,7 @@ size_t SupervisedUserSiteList::HostnameHash::hash() const {
 
 void SupervisedUserSiteList::Load(const std::string& id,
                                   const base::string16& title,
+                                  const base::FilePath& large_icon_path,
                                   const base::FilePath& path,
                                   const LoadedCallback& callback) {
   base::PostTaskAndReplyWithResult(
@@ -95,19 +96,21 @@ void SupervisedUserSiteList::Load(const std::string& id,
               base::SequencedWorkerPool::CONTINUE_ON_SHUTDOWN)
           .get(),
       FROM_HERE, base::Bind(&ReadFileOnBlockingThread, path),
-      base::Bind(&SupervisedUserSiteList::OnJsonLoaded, id, title, path,
-                 base::TimeTicks::Now(), callback));
+      base::Bind(&SupervisedUserSiteList::OnJsonLoaded, id, title,
+                 large_icon_path, path, base::TimeTicks::Now(), callback));
 }
 
 SupervisedUserSiteList::SupervisedUserSiteList(
     const std::string& id,
     const base::string16& title,
     const GURL& entry_point,
+    const base::FilePath& large_icon_path,
     const base::ListValue* patterns,
     const base::ListValue* hostname_hashes)
     : SupervisedUserSiteList(id,
                              title,
                              entry_point,
+                             large_icon_path,
                              ConvertListValues(patterns),
                              ConvertListValues(hostname_hashes)) {}
 
@@ -115,9 +118,14 @@ SupervisedUserSiteList::SupervisedUserSiteList(
     const std::string& id,
     const base::string16& title,
     const GURL& entry_point,
+    const base::FilePath& large_icon_path,
     const std::vector<std::string>& patterns,
     const std::vector<std::string>& hostname_hashes)
-    : id_(id), title_(title), entry_point_(entry_point), patterns_(patterns) {
+    : id_(id),
+      title_(title),
+      entry_point_(entry_point),
+      large_icon_path_(large_icon_path),
+      patterns_(patterns) {
   for (const std::string& hostname_hash : hostname_hashes) {
     std::vector<uint8_t> hash_bytes;
     if (hostname_hash.size() != 2 * base::kSHA1Length ||
@@ -137,6 +145,7 @@ SupervisedUserSiteList::~SupervisedUserSiteList() {
 void SupervisedUserSiteList::OnJsonLoaded(
     const std::string& id,
     const base::string16& title,
+    const base::FilePath& large_icon_path,
     const base::FilePath& path,
     base::TimeTicks start_time,
     const SupervisedUserSiteList::LoadedCallback& callback,
@@ -184,6 +193,7 @@ void SupervisedUserSiteList::OnJsonLoaded(
   base::ListValue* hostname_hashes = nullptr;
   dict->GetList(kHostnameHashesKey, &hostname_hashes);
 
-  callback.Run(make_scoped_refptr(new SupervisedUserSiteList(
-      id, title, GURL(entry_point_url), patterns, hostname_hashes)));
+  callback.Run(make_scoped_refptr(
+      new SupervisedUserSiteList(id, title, GURL(entry_point_url),
+                                 large_icon_path, patterns, hostname_hashes)));
 }
