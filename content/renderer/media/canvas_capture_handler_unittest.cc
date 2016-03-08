f02f99241292891187e361382926408d52b4f8f8
emircan <emircan@chromium.org>
2016-03-08 14:13:50 -0800

    Handle Alpha channel in Canvas capture
    This CL changes the output frame format in canvas capture from
    I420 to YV12A so that alpha channel is preserved. The change
    discussion is here [0].
    
    As a result, each MediaStreamSink would be responsible for
    handling YV12A frames. For that purpose
    VideoUtil::WrapAsI420VideoFrame() is added to each appropriate
    sink.
    
    [0] https://github.com/w3c/mediacapture-fromelement/issues/30
    [1] https://code.google.com/p/chromium/codesearch#chromium/src/content/public/renderer/media_stream_sink.h&l=18&ct=xref_jump_to_def&cl=GROK&gsn=MediaStreamSink
    BUG=524218
    TEST= https://cdn.rawgit.com/uysalere/js-demos/master/canvascapture3.html
    TBR=bbudge@chromium.org
    
    Review URL: https://codereview.chromium.org/1737253002
    
    Cr-Commit-Position: refs/heads/master@{#379932}

--- content/renderer/media/canvas_capture_handler_unittest.cc
+++ content/renderer/media/canvas_capture_handler_unittest.cc
@@ -23,8 +23,11 @@ using ::testing::InSequence;
 using ::testing::Mock;
 using ::testing::SaveArg;
 using ::testing::Test;
+using ::testing::TestWithParam;
 
 namespace content {
+
+namespace {
 
 static const int kTestCanvasCaptureWidth = 320;
 static const int kTestCanvasCaptureHeight = 240;
@@ -33,12 +36,15 @@ static const double kTestCanvasCaptureFramesPerSecond = 55.5;
 static const int kTestCanvasCaptureFrameWidth = 2;
 static const int kTestCanvasCaptureFrameHeight = 2;
 static const int kTestCanvasCaptureFrameErrorTolerance = 2;
+static const int kTestAlphaValue = 175;
 
 ACTION_P(RunClosure, closure) {
   closure.Run();
 }
 
-class CanvasCaptureHandlerTest : public Test {
+}  // namespace
+
+class CanvasCaptureHandlerTest : public TestWithParam<bool> {
  public:
   CanvasCaptureHandlerTest() {}
 
@@ -77,27 +83,37 @@ class CanvasCaptureHandlerTest : public Test {
   void OnRunning(bool state) { DoOnRunning(state); }
 
   // Verify returned frames.
-  static skia::RefPtr<SkImage> GenerateTestImage() {
+  static skia::RefPtr<SkImage> GenerateTestImage(bool opaque) {
+
     SkBitmap testBitmap;
     testBitmap.allocN32Pixels(kTestCanvasCaptureFrameWidth,
-                              kTestCanvasCaptureFrameHeight);
-    testBitmap.eraseColor(SK_ColorBLUE);
+                              kTestCanvasCaptureFrameHeight, opaque);
+    testBitmap.eraseARGB(kTestAlphaValue, 30, 60, 200);
     return skia::AdoptRef(SkImage::NewFromBitmap(testBitmap));
   }
 
   void OnVerifyDeliveredFrame(
+      bool opaque,
       const scoped_refptr<media::VideoFrame>& video_frame,
       base::TimeTicks estimated_capture_time) {
-    EXPECT_EQ(media::PIXEL_FORMAT_I420, video_frame->format());
+    if (opaque)
+      EXPECT_EQ(media::PIXEL_FORMAT_I420, video_frame->format());
+    else
+      EXPECT_EQ(media::PIXEL_FORMAT_YV12A, video_frame->format());
+
     const gfx::Size& size = video_frame->coded_size();
     EXPECT_EQ(kTestCanvasCaptureFrameWidth, size.width());
     EXPECT_EQ(kTestCanvasCaptureFrameHeight, size.height());
-    const uint8_t* y_plane = video_frame->data(0);
-    EXPECT_NEAR(41, y_plane[0], kTestCanvasCaptureFrameErrorTolerance);
-    const uint8_t* u_plane = video_frame->data(1);
-    EXPECT_NEAR(239, u_plane[0], kTestCanvasCaptureFrameErrorTolerance);
-    const uint8_t* v_plane = video_frame->data(2);
-    EXPECT_NEAR(110, v_plane[0], kTestCanvasCaptureFrameErrorTolerance);
+    const uint8_t* y_plane = video_frame->data(media::VideoFrame::kYPlane);
+    EXPECT_NEAR(74, y_plane[0], kTestCanvasCaptureFrameErrorTolerance);
+    const uint8_t* u_plane = video_frame->data(media::VideoFrame::kUPlane);
+    EXPECT_NEAR(193, u_plane[0], kTestCanvasCaptureFrameErrorTolerance);
+    const uint8_t* v_plane = video_frame->data(media::VideoFrame::kVPlane);
+    EXPECT_NEAR(105, v_plane[0], kTestCanvasCaptureFrameErrorTolerance);
+    if (!opaque) {
+      const uint8_t* a_plane = video_frame->data(media::VideoFrame::kAPlane);
+      EXPECT_EQ(kTestAlphaValue, a_plane[0]);
+    }
   }
 
   blink::WebMediaStreamTrack track_;
@@ -126,7 +142,7 @@ TEST_F(CanvasCaptureHandlerTest, ConstructAndDestruct) {
 }
 
 // Checks that VideoCapturerSource call sequence works fine.
-TEST_F(CanvasCaptureHandlerTest, GetFormatsStartAndStop) {
+TEST_P(CanvasCaptureHandlerTest, GetFormatsStartAndStop) {
   InSequence s;
   const blink::WebMediaStreamSource& web_media_stream_source = track_.source();
   EXPECT_FALSE(web_media_stream_source.isNull());
@@ -147,7 +163,7 @@ TEST_F(CanvasCaptureHandlerTest, GetFormatsStartAndStop) {
       media::limits::kMaxFramesPerSecond /* max_requested_frame_rate */,
       base::Bind(&CanvasCaptureHandlerTest::OnVideoCaptureDeviceFormats,
                  base::Unretained(this)));
-  ASSERT_EQ(1u, formats.size());
+  ASSERT_EQ(2u, formats.size());
   EXPECT_EQ(kTestCanvasCaptureWidth, formats[0].frame_size.width());
   EXPECT_EQ(kTestCanvasCaptureHeight, formats[0].frame_size.height());
   media::VideoCaptureParams params;
@@ -163,14 +179,15 @@ TEST_F(CanvasCaptureHandlerTest, GetFormatsStartAndStop) {
       params, base::Bind(&CanvasCaptureHandlerTest::OnDeliverFrame,
                          base::Unretained(this)),
       base::Bind(&CanvasCaptureHandlerTest::OnRunning, base::Unretained(this)));
-  canvas_capture_handler_->sendNewFrame(GenerateTestImage().get());
+  canvas_capture_handler_->sendNewFrame(GenerateTestImage(GetParam()).get());
   run_loop.Run();
 
   source->StopCapture();
 }
 
 // Verifies that SkImage is processed and produces VideoFrame as expected.
-TEST_F(CanvasCaptureHandlerTest, VerifyFrame) {
+TEST_P(CanvasCaptureHandlerTest, VerifyOpaqueFrame) {
+  const bool isOpaque = GetParam();
   InSequence s;
   media::VideoCapturerSource* const source =
       GetVideoCapturerSource(static_cast<MediaStreamVideoCapturerSource*>(
@@ -182,9 +199,9 @@ TEST_F(CanvasCaptureHandlerTest, VerifyFrame) {
   media::VideoCaptureParams params;
   source->StartCapture(
       params, base::Bind(&CanvasCaptureHandlerTest::OnVerifyDeliveredFrame,
-                         base::Unretained(this)),
+                         base::Unretained(this), isOpaque),
       base::Bind(&CanvasCaptureHandlerTest::OnRunning, base::Unretained(this)));
-  canvas_capture_handler_->sendNewFrame(GenerateTestImage().get());
+  canvas_capture_handler_->sendNewFrame(GenerateTestImage(isOpaque).get());
   run_loop.RunUntilIdle();
 }
 
@@ -199,5 +216,7 @@ TEST_F(CanvasCaptureHandlerTest, CheckNeedsNewFrame) {
   source->StopCapture();
   EXPECT_FALSE(canvas_capture_handler_->needsNewFrame());
 }
+
+INSTANTIATE_TEST_CASE_P(, CanvasCaptureHandlerTest, ::testing::Bool());
 
 }  // namespace content
--- content/renderer/media/video_track_recorder.cc
+++ content/renderer/media/video_track_recorder.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "media/base/video_frame.h"
+#include "media/base/video_util.h"
 #include "ui/gfx/geometry/size.h"
 
 extern "C" {
@@ -187,11 +188,22 @@ void VideoTrackRecorder::VpxEncoder::StartFrameEncode(
 }
 
 void VideoTrackRecorder::VpxEncoder::EncodeOnEncodingThread(
-    const scoped_refptr<VideoFrame>& frame,
+    const scoped_refptr<VideoFrame>& video_frame,
     base::TimeTicks capture_timestamp) {
   TRACE_EVENT0("video",
                "VideoTrackRecorder::VpxEncoder::EncodeOnEncodingThread");
   DCHECK(encoding_thread_->task_runner()->BelongsToCurrentThread());
+
+  if (!(video_frame->format() == media::PIXEL_FORMAT_I420 ||
+        video_frame->format() == media::PIXEL_FORMAT_YV12 ||
+        video_frame->format() == media::PIXEL_FORMAT_YV12A)) {
+    NOTREACHED();
+    return;
+  }
+  scoped_refptr<media::VideoFrame> frame = video_frame;
+  // Drop alpha channel since we do not support it yet.
+  if (frame->format() == media::PIXEL_FORMAT_YV12A)
+    frame = media::WrapAsI420VideoFrame(video_frame);
 
   const gfx::Size frame_size = frame->visible_rect().size();
   if (!IsInitialized() ||
