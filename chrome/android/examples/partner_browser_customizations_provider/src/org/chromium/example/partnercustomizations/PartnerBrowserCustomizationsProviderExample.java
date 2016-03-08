145dbfdeef036215ebb9088940ce48d5ec04477a
newt <newt@chromium.org>
2016-03-07 18:59:14 -0800

    Add partner bookmarks to partner customizations sample app.
    This makes it easier to test partner bookmarks features: just install
    the partner customizations sample app and Chrome will show partner
    bookmarks.
    
    BUG=583477
    
    Review URL: https://codereview.chromium.org/1753583002
    
    Cr-Commit-Position: refs/heads/master@{#379708}

--- chrome/android/examples/partner_browser_customizations_provider/src/org/chromium/example/partnercustomizations/PartnerBrowserCustomizationsProviderExample.java
+++ chrome/android/examples/partner_browser_customizations_provider/src/org/chromium/example/partnercustomizations/PartnerBrowserCustomizationsProviderExample.java
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // Package path can be changed, but should match <manifest package="..."> in AndroidManifest.xml.
-package com.android.partnerbrowsercustomizations.example;
+package org.chromium.example.partnercustomizations;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
