bbe7fb3229185736a90e3ad3857a9286cb52ea6d
rockot <rockot@chromium.org>
2016-03-08 00:42:11 -0800

    [mojo-edk] Zero padding bytes in dispatcher headers.
    This change adds explicit padding to dispatcher header structs
    while enforcing that the structs remain 8-byte aligned.
    The padding is zeroed when writing headers into a buffer.
    
    BUG=585804
    R=amistry@chromium.org
    
    Review URL: https://codereview.chromium.org/1771273002
    
    Cr-Commit-Position: refs/heads/master@{#379790}

--- mojo/edk/system/shared_buffer_dispatcher.cc
+++ mojo/edk/system/shared_buffer_dispatcher.cc
@@ -17,16 +17,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/edk/system/configuration.h"
 #include "mojo/edk/system/node_controller.h"
 #include "mojo/edk/system/options_validation.h"
-#include "mojo/public/c/system/macros.h"
 
 namespace mojo {
 namespace edk {
 
 namespace {
 
-struct MOJO_ALIGNAS(8) SerializedSharedBufferDispatcher {
-  size_t num_bytes;
+#pragma pack(push, 1)
+
+struct SerializedState {
+  uint64_t num_bytes;
 };
+
+#pragma pack(pop)
+
+static_assert(sizeof(SerializedState) % 8 == 0,
+              "Invalid SerializedState size.");
 
 }  // namespace
 
@@ -109,13 +115,13 @@ scoped_refptr<SharedBufferDispatcher> SharedBufferDispatcher::Deserialize(
     size_t num_ports,
     PlatformHandle* platform_handles,
     size_t num_platform_handles) {
-  if (num_bytes != sizeof(SerializedSharedBufferDispatcher)) {
+  if (num_bytes != sizeof(SerializedState)) {
     LOG(ERROR) << "Invalid serialized shared buffer dispatcher (bad size)";
     return nullptr;
   }
 
-  const SerializedSharedBufferDispatcher* serialization =
-      static_cast<const SerializedSharedBufferDispatcher*>(bytes);
+  const SerializedState* serialization =
+      static_cast<const SerializedState*>(bytes);
   if (!serialization->num_bytes) {
     LOG(ERROR)
         << "Invalid serialized shared buffer dispatcher (invalid num_bytes)";
@@ -138,7 +144,8 @@ scoped_refptr<SharedBufferDispatcher> SharedBufferDispatcher::Deserialize(
   // closed even if creation fails.
   scoped_refptr<PlatformSharedBuffer> shared_buffer(
       PlatformSharedBuffer::CreateFromPlatformHandle(
-          serialization->num_bytes, ScopedPlatformHandle(platform_handle)));
+          static_cast<size_t>(serialization->num_bytes),
+          ScopedPlatformHandle(platform_handle)));
   if (!shared_buffer) {
     LOG(ERROR)
         << "Invalid serialized shared buffer dispatcher (invalid num_bytes?)";
@@ -218,7 +225,7 @@ MojoResult SharedBufferDispatcher::MapBuffer(
 void SharedBufferDispatcher::StartSerialize(uint32_t* num_bytes,
                                             uint32_t* num_ports,
                                             uint32_t* num_platform_handles) {
-  *num_bytes = sizeof(SerializedSharedBufferDispatcher);
+  *num_bytes = sizeof(SerializedState);
   *num_ports = 0;
   *num_platform_handles = 1;
 }
@@ -226,10 +233,11 @@ void SharedBufferDispatcher::StartSerialize(uint32_t* num_bytes,
 bool SharedBufferDispatcher::EndSerialize(void* destination,
                                           ports::PortName* ports,
                                           PlatformHandle* handles) {
-  SerializedSharedBufferDispatcher* serialization =
-      static_cast<SerializedSharedBufferDispatcher*>(destination);
+  SerializedState* serialization =
+      static_cast<SerializedState*>(destination);
   base::AutoLock lock(lock_);
-  serialization->num_bytes = shared_buffer_->GetNumBytes();
+  serialization->num_bytes =
+        static_cast<uint64_t>(shared_buffer_->GetNumBytes());
 
   handle_for_transit_ = shared_buffer_->DuplicatePlatformHandle();
   if (!handle_for_transit_.is_valid()) {
