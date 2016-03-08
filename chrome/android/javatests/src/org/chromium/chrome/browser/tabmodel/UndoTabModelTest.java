9631fce5464297370224b2637e43f0a0c7c114b0
mvanouwerkerk <mvanouwerkerk@chromium.org>
2016-03-08 04:02:04 -0800

    Mark UndoTabModelTest#testOutOfOrder2 as flaky.
    BUG=592969
    TBR=dfalcantara
    
    Review URL: https://codereview.chromium.org/1776473003
    
    Cr-Commit-Position: refs/heads/master@{#379811}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/UndoTabModelTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/UndoTabModelTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tabmodel;
 
+import android.test.FlakyTest;
 import android.test.suitebuilder.annotation.MediumTest;
 
 import org.chromium.base.ThreadUtils;
@@ -997,6 +998,7 @@ public class UndoTabModelTest extends ChromeTabbedActivityTestBase {
      * @throws InterruptedException
      */
     @MediumTest
+    @FlakyTest // crbug.com/592969
     public void testOutOfOrder2() throws InterruptedException {
         TabModel model = getActivity().getTabModelSelector().getModel(false);
         ChromeTabCreator tabCreator = getActivity().getTabCreator(false);
