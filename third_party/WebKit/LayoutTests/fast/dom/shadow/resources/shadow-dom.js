46c77f131bf81724481a40769e69344d3760714b
yuzus <yuzus@chromium.org>
2016-03-07 23:43:01 -0800

    Removing dependency on js-test.js from shadow-dom.js
    This CL removes dependency of shadow-dom.js on js-test.js, namely debug function.
    
    BUG=591946
    
    Review URL: https://codereview.chromium.org/1773553003
    
    Cr-Commit-Position: refs/heads/master@{#379777}

--- third_party/WebKit/LayoutTests/fast/dom/shadow/resources/shadow-dom.js
+++ third_party/WebKit/LayoutTests/fast/dom/shadow/resources/shadow-dom.js
@@ -1,4 +1,38 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// TODO(yuzus): These two functions below need cleaning up. They are currently
+// from js-test.js.
+function getOrCreateTestElement(id, tagName)
+{
+    var element = document.getElementById(id);
+    if (element)
+        return element;
+
+    element = document.createElement(tagName);
+    element.id = id;
+    var refNode;
+    var parent = document.body || document.documentElement;
+    if (id == "description")
+        refNode = getOrCreateTestElement("console", "div");
+    else
+        refNode = parent.firstChild;
+
+    parent.insertBefore(element, refNode);
+    return element;
+}
+
+function debug(msg)
+{
+    if (self._lazyTestResults) {
+        self._lazyTestResults.push(msg);
+    } else {
+        var span = document.createElement("span");
+        // insert it first so XHTML knows the namespace;
+        getOrCreateTestElement("console", "div").appendChild(span);
+        span.innerHTML = msg + '<br />';
+    };
+}
+
+
 function createShadowRoot()
 {
     var children = Array.prototype.slice.call(arguments);
@@ -228,6 +262,7 @@ function shouldNavigateFocus(from, to, direction)
         debug('PASS');
     else
         debug('FAIL');
+    return isInnermostActiveElement(to);
 }
 
 function navigateFocusForward()
