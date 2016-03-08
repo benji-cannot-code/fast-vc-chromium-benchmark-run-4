91f4cd70535193efa11178cbab7fe79fdbfa5f05
sdefresne <sdefresne@chromium.org>
2016-03-08 07:20:18 -0800

    Properly retain system_store_ in CookieStoreIOS.
    The NSHTTPCookieStorage referenced by CookieStoreIOS is not always
    the system singleton and need to be properly refcounted. Change the
    instance variable to base::scoped_nsobject<> to take care of this.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1771343002
    
    Cr-Commit-Position: refs/heads/master@{#379835}

--- ios/net/cookies/cookie_store_ios.h
+++ ios/net/cookies/cookie_store_ios.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/cancelable_callback.h"
+#include "base/mac/scoped_nsobject.h"
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -203,7 +204,7 @@ class CookieStoreIOS : public net::CookieStore,
                                const DeleteCallback& callback);
 
   scoped_ptr<net::CookieMonster> cookie_monster_;
-  NSHTTPCookieStorage* system_store_;
+  base::scoped_nsobject<NSHTTPCookieStorage> system_store_;
   scoped_ptr<CookieCreationTimeManager> creation_time_manager_;
   bool metrics_enabled_;
   base::TimeDelta flush_delay_;
