a3cc000d03fe5b27c0f10bcfd51ec86067a01528
rune <rune@opera.com>
2016-03-07 22:06:31 -0800

    No need to re-collect stylesheets for setting type StyleChange.
    Style recalc for all frames necessary, though.
    
    Review URL: https://codereview.chromium.org/1772513003
    
    Cr-Commit-Position: refs/heads/master@{#379763}

--- third_party/WebKit/Source/core/page/Page.cpp
+++ third_party/WebKit/Source/core/page/Page.cpp
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/css/resolver/ViewportStyleResolver.h"
 #include "core/dom/ClientRectList.h"
+#include "core/dom/StyleChangeReason.h"
 #include "core/dom/VisitedLinkState.h"
 #include "core/editing/DragCaretController.h"
 #include "core/editing/commands/UndoStack.h"
@@ -242,7 +243,7 @@ void Page::setNeedsRecalcStyleInAllFrames()
 {
     for (Frame* frame = mainFrame(); frame; frame = frame->tree().traverseNext()) {
         if (frame->isLocalFrame())
-            toLocalFrame(frame)->document()->styleEngine().resolverChanged(FullStyleUpdate);
+            toLocalFrame(frame)->document()->setNeedsStyleRecalc(SubtreeStyleChange, StyleChangeReasonForTracing::create(StyleChangeReason::Settings));
     }
 }
 
