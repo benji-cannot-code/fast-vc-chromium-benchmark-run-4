abacf2ce3f068252e94c9c221cf8871dff1cc17e
vogelheim <vogelheim@chromium.org>
2016-03-08 07:02:36 -0800

    Clear cached metadata when the resource data itself is invalidated.
    Follow-up to crrev.com/1412413003: That change updated blink::Resource to
    potentially update the Resource's data in-place when a revalidation
    fails. That also invalidate that Resource's in-memory cached
    meta data, which this change implements. This was previously
    unnecessary, since a new Resource object would not have had that
    cached data in the first place.
    
    For the full drama, please refer to the referenced bug.
    
    R=mkwst@chromium.org, jochen@chromium.org
    BUG=chromium:589158
    
    Review URL: https://codereview.chromium.org/1768143002
    
    Cr-Commit-Position: refs/heads/master@{#379828}

--- third_party/WebKit/Source/core/fetch/Resource.cpp
+++ third_party/WebKit/Source/core/fetch/Resource.cpp
@@ -837,6 +837,7 @@ void Resource::revalidationFailed()
     m_resourceRequest = m_revalidatingRequest;
     m_revalidatingRequest = ResourceRequest();
     m_data.clear();
+    m_cachedMetadata.clear();
     destroyDecodedDataForFailedRevalidation();
 }
 
