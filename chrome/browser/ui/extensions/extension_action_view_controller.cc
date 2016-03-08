0991fd99ddbf2d9d3169862b2d004639b6bcc6a0
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-03-08 14:21:42 -0800

    [Extensions UI Views] Generic-ify a toolbar actions bar bubble
    There's a generic ToolbarActionsBarBubbleDelegate, but the view for the bubble
    was linked to the ExtensionIconSurfacingBubble. There's no reason it has to be,
    and this can be re-used for other bubbles that dangle off the toolbar. Make the
    view more abstract (we already do this on mac), and add a few more tests for it.
    Next step will be to use this in click-to-script work.
    
    BUG=460306
    
    Review URL: https://codereview.chromium.org/1771033002
    
    Cr-Commit-Position: refs/heads/master@{#379936}

--- chrome/browser/ui/extensions/extension_action_view_controller.cc
+++ chrome/browser/ui/extensions/extension_action_view_controller.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/extensions/icon_with_badge_image_source.h"
 #include "chrome/browser/ui/toolbar/toolbar_action_view_delegate.h"
 #include "chrome/browser/ui/toolbar/toolbar_actions_bar.h"
-#include "chrome/browser/ui/toolbar/toolbar_actions_bar.h"
 #include "chrome/common/extensions/api/extension_action/action_info.h"
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/extension_registry.h"
