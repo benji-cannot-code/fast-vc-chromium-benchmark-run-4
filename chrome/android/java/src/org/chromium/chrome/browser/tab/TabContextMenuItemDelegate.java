9211776bf03c7184a507aaf9b3b71b19ca756a75
tedchoc <tedchoc@chromium.org>
2016-03-08 11:08:40 -0800

    Switch back to open image in new tab where applicable.
    open image will remain in CCT as that tries to prevent new
    tab creation
    
    BUG=589275
    
    Review URL: https://codereview.chromium.org/1741153002
    
    Cr-Commit-Position: refs/heads/master@{#379877}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabContextMenuItemDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabContextMenuItemDelegate.java
@@ -28,6 +28,9 @@ import java.util.Locale;
  * A default {@link ContextMenuItemDelegate} that supports the context menu functionality in Tab.
  */
 public class TabContextMenuItemDelegate implements ContextMenuItemDelegate {
+    public static final String PAGESPEED_PASSTHROUGH_HEADERS =
+            "Chrome-Proxy: pass-through\nCache-Control: no-cache";
+
     private final Clipboard mClipboard;
     private final Tab mTab;
 
@@ -106,6 +109,16 @@ public class TabContextMenuItemDelegate implements ContextMenuItemDelegate {
         loadUrlParams.setTransitionType(PageTransition.LINK);
         loadUrlParams.setReferrer(referrer);
         mTab.loadUrl(loadUrlParams);
+    }
+
+    @Override
+    public void onOpenImageInNewTab(String url, Referrer referrer) {
+        boolean useOriginal = isSpdyProxyEnabledForUrl(url);
+        LoadUrlParams loadUrlParams = new LoadUrlParams(url);
+        loadUrlParams.setVerbatimHeaders(useOriginal ? PAGESPEED_PASSTHROUGH_HEADERS : null);
+        loadUrlParams.setReferrer(referrer);
+        mTab.getActivity().getTabModelSelector().openNewTab(loadUrlParams,
+                TabLaunchType.FROM_LONGPRESS_BACKGROUND, mTab, isIncognito());
     }
 
     @Override
