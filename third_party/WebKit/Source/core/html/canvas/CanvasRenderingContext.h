c70e63f92fdc54529c0271da238df09e1e600f79
xlai <xlai@chromium.org>
2016-03-07 20:41:01 -0800

    Add rendering context and rendering 2D context to OffscreenCanvas
    This patch implements the getContext() function in OffscreenCanvas.
    It also implements the skeleton of  OffscreenCanvasRenderingContext2D, an object type
    returned by getContext("2d").
    
    BUG=563826
    
    Review URL: https://codereview.chromium.org/1748163003
    
    Cr-Commit-Position: refs/heads/master@{#379740}

--- third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext.h
+++ third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/CoreExport.h"
 #include "core/html/HTMLCanvasElement.h"
-#include "platform/heap/Handle.h"
 #include "wtf/HashSet.h"
 #include "wtf/Noncopyable.h"
 #include "wtf/text/StringHash.h"
@@ -127,7 +126,6 @@ public:
 protected:
     CanvasRenderingContext(HTMLCanvasElement*);
     DECLARE_VIRTUAL_TRACE();
-
     virtual void stop() = 0;
 
 private:
--- third_party/WebKit/Source/modules/canvas2d/BaseRenderingContext2D.h
+++ third_party/WebKit/Source/modules/canvas2d/BaseRenderingContext2D.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/core/v8/UnionTypesCore.h"
 #include "bindings/modules/v8/UnionTypesModules.h"
-#include "core/html/canvas/CanvasRenderingContext.h"
 #include "modules/ModulesExport.h"
 #include "modules/canvas2d/CanvasGradient.h"
 #include "modules/canvas2d/CanvasPathMethods.h"
