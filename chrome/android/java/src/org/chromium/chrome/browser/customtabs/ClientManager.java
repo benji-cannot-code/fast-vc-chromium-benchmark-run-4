477dae00dd870ea7d84799b24e516098dc99aec8
lizeb <lizeb@chromium.org>
2016-03-08 07:12:01 -0800

    customtabs: Don't drop a prerender when only the fragment differs.
    BUG=574864
    
    Review URL: https://codereview.chromium.org/1684513006
    
    Cr-Commit-Position: refs/heads/master@{#379833}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/ClientManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/ClientManager.java
@@ -53,6 +53,7 @@ class ClientManager {
         public final String packageName;
         public final ICustomTabsCallback callback;
         public final IBinder.DeathRecipient deathRecipient;
+        public boolean mIgnoreFragments;
         private boolean mShouldHideDomain;
         private ServiceConnection mKeepAliveConnection;
         private String mPredictedUrl;
@@ -257,6 +258,20 @@ class ClientManager {
     public synchronized void setHideDomainForSession(IBinder session, boolean hide) {
         SessionParams params = mSessionParams.get(session);
         if (params != null) params.mShouldHideDomain = hide;
+    }
+
+    /**
+     * @return Whether the fragment should be ignored for prerender matching.
+     */
+    public synchronized boolean getIgnoreFragmentsForSession(IBinder session) {
+        SessionParams params = mSessionParams.get(session);
+        return params == null ? false : params.mIgnoreFragments;
+    }
+
+    /** Sets whether the fragment should be ignored for prerender matching. */
+    public synchronized void setIgnoreFragmentsForSession(IBinder session, boolean value) {
+        SessionParams params = mSessionParams.get(session);
+        params.mIgnoreFragments = value;
     }
 
     /** Tries to bind to a client to keep it alive, and returns true for success. */
