02f3d26838491e2916c560c1f448a8455a8adc20
dmazzoni <dmazzoni@chromium.org>
2016-03-08 14:28:02 -0800

    Improve ChromeVox incremental search experience.
    * Add CSS so that search box is centered in the panel and styled to match
    * Hide caption panel while search box is open, restore it when it closes
    * Clear the previous search when the search box loses focus
    
    BUG=513714
    
    Review URL: https://codereview.chromium.org/1772293004
    
    Cr-Commit-Position: refs/heads/master@{#379941}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/panel.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/panel.js
@@ -38,6 +38,12 @@ Panel.init = function() {
   this.brailleContainer_ = $('braille-container');
 
   /** @type {Element} @private */
+  this.searchContainer_ = $('search-container');
+
+  /** @type {Element} @private */
+  this.searchInput_ = $('search');
+
+  /** @type {Element} @private */
   this.brailleTextElement_ = $('braille-text');
 
   /** @type {Element} @private */
@@ -74,6 +80,13 @@ Panel.init = function() {
    */
   this.pendingCallback_ = null;
 
+  /**
+   * True if we're currently in incremental search mode.
+   * @type {boolean}
+   * @private
+   */
+  this.searching_ = false;
+
   Panel.updateFromPrefs();
 
   Msgs.addTranslatedMessagesToDom(document);
@@ -95,12 +108,25 @@ Panel.init = function() {
 
   document.addEventListener('keydown', Panel.onKeyDown, false);
   document.addEventListener('mouseup', Panel.onMouseUp, false);
+
+  Panel.searchInput_.addEventListener('blur', Panel.onSearchInputBlur, false);
 };
 
 /**
  * Update the display based on prefs.
  */
 Panel.updateFromPrefs = function() {
+  if (Panel.searching_) {
+    this.speechContainer_.style.display = 'none';
+    this.brailleContainer_.style.display = 'none';
+    this.searchContainer_.style.display = 'block';
+    return;
+  }
+
+  this.speechContainer_.style.display = 'block';
+  this.brailleContainer_.style.display = 'block';
+  this.searchContainer_.style.display = 'none';
+
   if (localStorage['brailleCaptions'] === String(true)) {
     this.speechContainer_.style.visibility = 'hidden';
     this.brailleContainer_.style.visibility = 'visible';
@@ -309,10 +335,12 @@ Panel.onOpenMenus = function(opt_event) {
 Panel.onSearch = function() {
   Panel.clearMenus();
   Panel.pendingCallback_ = null;
+  Panel.searching_ = true;
+  Panel.updateFromPrefs();
 
   window.location = '#focus';
 
-  ISearchUI.get($('search'));
+  ISearchUI.get(Panel.searchInput_);
 };
 
 /**
@@ -461,6 +489,21 @@ Panel.onKeyDown = function(event) {
 
   event.preventDefault();
   event.stopPropagation();
+};
+
+/**
+ * Called when focus leaves the search input.
+ */
+Panel.onSearchInputBlur = function() {
+  if (Panel.searching_) {
+    if (document.activeElement != Panel.searchInput_ || !document.hasFocus()) {
+      Panel.searching_ = false;
+      if (window.location == '#focus')
+        window.location = '#';
+      Panel.updateFromPrefs();
+      Panel.searchInput_.value = '';
+    }
+  }
 };
 
 /**
