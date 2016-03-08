5e32683817f0cbb2da137b5e4f8ab558894abe19
mattcary <mattcary@chromium.org>
2016-03-08 03:28:32 -0800

    Device controller refactor.
    First step in refactoring connection setup and chrome launching in a way that's
    friendly to sandwich, clovis, pre* and any others.
    
    Review URL: https://codereview.chromium.org/1731973002
    
    Cr-Commit-Position: refs/heads/master@{#379807}

--- tools/android/loading/device_setup.py
+++ tools/android/loading/device_setup.py
@@ -98,6 +98,7 @@ def ForwardPort(device, local, remote):
     device.adb.ForwardRemove(local)
 
 
+# Deprecated
 def _SetUpDevice(device, package_info):
   """Enables root and closes Chrome on a device."""
   device.EnableRoot()
@@ -190,6 +191,7 @@ def WprHost(device, wpr_archive_path, record=False,
     shutil.rmtree(temp_certificate_dir)
 
 
+# Deprecated
 @contextlib.contextmanager
 def _DevToolsConnectionOnDevice(device, flags):
   """Returns a DevToolsConnection context manager for a given device.
@@ -216,6 +218,7 @@ def _DevToolsConnectionOnDevice(device, flags):
           OPTIONS.devtools_hostname, OPTIONS.devtools_port)
 
 
+# Deprecated, use *Controller.
 def DeviceConnection(device, additional_flags=None):
   """Context for starting recording on a device.
 
