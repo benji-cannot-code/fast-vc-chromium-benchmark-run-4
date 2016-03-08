29a6d69b7ed688b6ad6c524344a1dea9b12b38ed
ryanchung <ryanchung@chromium.org>
2016-03-07 21:08:10 -0800

    Support for server session cache.
    Allows SSL server sockets created through a SSLServerSocketContext
    to share a single session cache.
    OpenSSL only.
    
    BUG=568650
    
    Review URL: https://codereview.chromium.org/1518613002
    
    Cr-Commit-Position: refs/heads/master@{#379751}

--- net/test/embedded_test_server/embedded_test_server.h
+++ net/test/embedded_test_server/embedded_test_server.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/host_port_pair.h"
 #include "net/base/ip_endpoint.h"
 #include "net/cert/x509_certificate.h"
+#include "net/socket/ssl_server_socket.h"
 #include "net/socket/stream_socket.h"
 #include "net/socket/tcp_server_socket.h"
 #include "net/ssl/ssl_server_config.h"
@@ -244,6 +245,10 @@ class EmbeddedTestServer {
   void HandleRequest(HttpConnection* connection,
                      scoped_ptr<HttpRequest> request);
 
+  // Initializes the SSLServerContext so that SSLServerSocket connections may
+  // share the same cache
+  void InitializeSSLServerContext();
+
   HttpConnection* FindConnection(StreamSocket* socket);
 
   // Posts a task to the |io_thread_| and waits for a reply.
@@ -273,6 +278,7 @@ class EmbeddedTestServer {
 
   net::SSLServerConfig ssl_config_;
   ServerCertificate cert_;
+  scoped_ptr<SSLServerContext> context_;
 
   base::WeakPtrFactory<EmbeddedTestServer> weak_factory_;
 
