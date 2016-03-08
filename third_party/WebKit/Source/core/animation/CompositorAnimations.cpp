97f83b2c7d603bfaf27778a7a75459a82a67714c
dstockwell <dstockwell@chromium.org>
2016-03-07 20:47:34 -0800

    Web Animations: Dont suppress compositor transform for inline-block
    Transform is applied to display:inline-block/inline-transform so there's
    no need to suppress compositor animations here.
    
    BUG=592803
    
    Review URL: https://codereview.chromium.org/1775743002
    
    Cr-Commit-Position: refs/heads/master@{#379744}

--- third_party/WebKit/Source/core/animation/CompositorAnimations.cpp
+++ third_party/WebKit/Source/core/animation/CompositorAnimations.cpp
@@ -255,7 +255,7 @@ bool CompositorAnimations::isCandidateForAnimationOnCompositor(const Timing& tim
             return false;
 
         if (isTransformRelatedCSSProperty(property)) {
-            if (targetElement.layoutObject() && targetElement.layoutObject()->isInline()) {
+            if (targetElement.layoutObject() && targetElement.layoutObject()->isInline() && !targetElement.layoutObject()->isInlineBlockOrInlineTable()) {
                 return false;
             }
             transformPropertyCount++;
