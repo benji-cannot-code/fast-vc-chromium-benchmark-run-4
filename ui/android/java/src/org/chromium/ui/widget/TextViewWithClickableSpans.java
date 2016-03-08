2c3c0aaed35f2d7500903a4e97e942c9f9b72963
jyasskin <jyasskin@chromium.org>
2016-03-07 20:14:48 -0800

    Test that the BluetoothChooserDialog handles lacking the location permission.
    This also adds tests that the dialog shows the right strings and returns appropriate status codes.
    
    BUG=511805
    
    Review URL: https://codereview.chromium.org/1598553002
    
    Cr-Commit-Position: refs/heads/master@{#379732}

--- ui/android/java/src/org/chromium/ui/widget/TextViewWithClickableSpans.java
+++ ui/android/java/src/org/chromium/ui/widget/TextViewWithClickableSpans.java
@@ -20,6 +20,8 @@ import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
+import org.chromium.base.VisibleForTesting;
+
 /**
  * ClickableSpan isn't accessible by default, so we create a subclass
  * of TextView that tries to handle the case where a user clicks on a view
@@ -119,7 +121,11 @@ public class TextViewWithClickableSpans extends TextView {
         return clickableSpans.length > 0;
     }
 
-    private ClickableSpan[] getClickableSpans() {
+    /**
+     * Returns the ClickableSpans in this TextView's text.
+     */
+    @VisibleForTesting
+    public ClickableSpan[] getClickableSpans() {
         CharSequence text = getText();
         if (!(text instanceof SpannableString)) return null;
 
