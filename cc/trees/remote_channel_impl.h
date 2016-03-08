a0859e29fe9b669b01e5fb08d0c740d55bf154bf
khushalsagar <khushalsagar@chromium.org>
2016-03-08 14:46:15 -0800

    cc: Fix for releasing output surface during commit.
    In the remote compositor we can get into a state where the LTH on the
    client requests a BeginMainFrame and then releases the output surface.
    The server will still send a commit but we can't push this commit till
    the client initializes a new output surface.
    
    At the same time the scheduler will not request a new output surface
    till it clears the pipeline of the previous commit.
    
    This change adds a setting to the Scheduler to allow it to request a new
    output surface, while there is a commit pending. The RemoteChannelImpl
    queues any protos received if the output is released and process them
    when a new output surface is initialized.
    
    BUG=586210
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1691143002
    
    Cr-Commit-Position: refs/heads/master@{#379947}

--- cc/trees/remote_channel_impl.h
+++ cc/trees/remote_channel_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "cc/base/cc_export.h"
 #include "cc/base/completion_event.h"
+#include "cc/proto/compositor_message_to_impl.pb.h"
 #include "cc/trees/channel_impl.h"
 #include "cc/trees/proxy.h"
 #include "cc/trees/proxy_impl.h"
@@ -103,6 +104,14 @@ class CC_EXPORT RemoteChannelImpl : public ChannelImpl,
     RemoteProtoChannel* remote_proto_channel;
 
     bool started;
+
+    // This is set to true if we lost the output surface and can not push any
+    // commits to the impl thread.
+    bool waiting_for_output_surface_initialization;
+
+    // The queue of messages received from the server. The messages are added to
+    // this queue if we are waiting for a new output surface to be initialized.
+    std::queue<proto::CompositorMessageToImpl> pending_messages;
 
     RendererCapabilities renderer_capabilities;
 
