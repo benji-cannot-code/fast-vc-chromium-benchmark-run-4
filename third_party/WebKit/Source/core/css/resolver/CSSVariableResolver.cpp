1897e843a2f8d7b3c6619976823855b638c36460
timloh <timloh@chromium.org>
2016-03-07 20:55:18 -0800

    Check @apply syntax in custom properties declarations at parse time
    This patch moves syntax checking for @apply references inside custom
    property declarations from style computation to parse time. This makes
    us reject any custom property declarations which has an invalid @apply
    rule, such as "--foo: @apply bar".
    
    BUG=586974
    
    Review URL: https://codereview.chromium.org/1752773003
    
    Cr-Commit-Position: refs/heads/master@{#379747}

--- third_party/WebKit/Source/core/css/resolver/CSSVariableResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/CSSVariableResolver.cpp
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/parser/CSSParserTokenRange.h"
 #include "core/css/parser/CSSParserValues.h"
 #include "core/css/parser/CSSPropertyParser.h"
-#include "core/css/parser/CSSVariableParser.h"
 #include "core/css/resolver/StyleBuilder.h"
 #include "core/css/resolver/StyleResolverState.h"
 #include "core/style/StyleVariableData.h"
@@ -93,16 +92,9 @@ void CSSVariableResolver::resolveApplyAtRule(CSSParserTokenRange& range,
     Vector<CSSParserToken>& result)
 {
     ASSERT(range.peek().type() == AtKeywordToken && range.peek().valueEqualsIgnoringASCIICase("apply"));
-    CSSParserTokenRange originalRange = range;
-
     range.consumeIncludingWhitespace();
     const CSSParserToken& variableName = range.consumeIncludingWhitespace();
-    if (!CSSVariableParser::isValidVariableName(variableName)
-        || !(range.atEnd() || range.peek().type() == SemicolonToken || range.peek().type() == RightBraceToken)) {
-        range = originalRange;
-        result.append(range.consume());
-        return;
-    }
+    // TODO(timloh): Should we actually be consuming this?
     if (range.peek().type() == SemicolonToken)
         range.consume();
 
