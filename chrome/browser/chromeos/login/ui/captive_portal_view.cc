15c7fd52b75ef78af49491989bc0d511e1c598ad
dzhioev <dzhioev@chromium.org>
2016-03-09 13:22:01 -0800

    Temporarily change captive portal title bar to default.
    The toolbar that was used before has layout issues.
    
    BUG=588855
    TEST=connect to a captive portal network, check that the toolbar tittle has
         a single line of text.
    
    Review URL: https://codereview.chromium.org/1775073003
    
    Cr-Commit-Position: refs/heads/master@{#380224}

--- chrome/browser/chromeos/login/ui/captive_portal_view.cc
+++ chrome/browser/chromeos/login/ui/captive_portal_view.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/captive_portal/captive_portal_detector.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/views/window/dialog_delegate.h"
 #include "url/gurl.h"
 
 namespace {
@@ -68,11 +67,6 @@ base::string16 CaptivePortalView::GetWindowTitle() const {
 
 bool CaptivePortalView::ShouldShowWindowTitle() const {
   return true;
-}
-
-views::NonClientFrameView* CaptivePortalView::CreateNonClientFrameView(
-    views::Widget* widget) {
-  return views::DialogDelegate::CreateDialogFrameView(widget);
 }
 
 void CaptivePortalView::NavigationStateChanged(
