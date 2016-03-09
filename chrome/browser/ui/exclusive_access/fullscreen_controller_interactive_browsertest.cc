c7bf7d1544d84aa590483642aefd3ef48fd77694
lshang <lshang@chromium.org>
2016-03-08 19:58:35 -0800

    Use URLs instead of patterns in SetContentSetting in fullscreen and mouselock
    Migrate CONTENT_SETTINGS_TYPE_FULLSCREEN and CONTENT_SETTINGS_TYPE_MOUSELOCK to
    use SetContentSettingDefaultScope() which takes GURLs directly.
    
    BUG=551747
    
    Review URL: https://codereview.chromium.org/1700013002
    
    Cr-Commit-Position: refs/heads/master@{#380058}

--- chrome/browser/ui/exclusive_access/fullscreen_controller_interactive_browsertest.cc
+++ chrome/browser/ui/exclusive_access/fullscreen_controller_interactive_browsertest.cc
@@ -158,11 +158,8 @@ FullscreenControllerInteractiveTest::TestFullscreenMouseLockContentSettings() {
   // Add content setting to ALLOW fullscreen.
   HostContentSettingsMap* settings_map =
       HostContentSettingsMapFactory::GetForProfile(browser()->profile());
-  ContentSettingsPattern pattern =
-      ContentSettingsPattern::FromURL(url);
-  settings_map->SetContentSetting(
-      pattern, ContentSettingsPattern::Wildcard(),
-      CONTENT_SETTINGS_TYPE_FULLSCREEN, std::string(),
+  settings_map->SetContentSettingDefaultScope(
+      url, url, CONTENT_SETTINGS_TYPE_FULLSCREEN, std::string(),
       CONTENT_SETTING_ALLOW);
 
   // Now, fullscreen should not prompt for permission.
@@ -179,9 +176,8 @@ FullscreenControllerInteractiveTest::TestFullscreenMouseLockContentSettings() {
   LostMouseLock();
 
   // Add content setting to ALLOW mouse lock.
-  settings_map->SetContentSetting(
-      pattern, ContentSettingsPattern::Wildcard(),
-      CONTENT_SETTINGS_TYPE_MOUSELOCK, std::string(),
+  settings_map->SetContentSettingDefaultScope(
+      url, GURL(), CONTENT_SETTINGS_TYPE_MOUSELOCK, std::string(),
       CONTENT_SETTING_ALLOW);
 
   // Now, mouse lock should not prompt for permission.
@@ -193,9 +189,8 @@ FullscreenControllerInteractiveTest::TestFullscreenMouseLockContentSettings() {
   // Leaving tab in fullscreen, now test mouse lock BLOCK:
 
   // Add content setting to BLOCK mouse lock.
-  settings_map->SetContentSetting(
-      pattern, ContentSettingsPattern::Wildcard(),
-      CONTENT_SETTINGS_TYPE_MOUSELOCK, std::string(),
+  settings_map->SetContentSettingDefaultScope(
+      url, GURL(), CONTENT_SETTINGS_TYPE_MOUSELOCK, std::string(),
       CONTENT_SETTING_BLOCK);
 
   // Now, mouse lock should not be pending.
