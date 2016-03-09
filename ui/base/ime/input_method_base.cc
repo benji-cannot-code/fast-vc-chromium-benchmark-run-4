dbdd8527345fe4ae20df967386ac9a2f133d1158
shuchen <shuchen@chromium.org>
2016-03-08 21:56:44 -0800

    Set the current input context handler when the toplevel window is activated instead of when the input field is focused.
    When the input field is focused, the top level window may not be active.
    For example, the IME window inactivatable, however, the embedded web
    contents has its own window and tries to set the focused text input
    client when it recieves the mouse down events.
    
    BUG=517773,592976
    
    Review URL: https://codereview.chromium.org/1778693004
    
    Cr-Commit-Position: refs/heads/master@{#380081}

--- ui/base/ime/input_method_base.cc
+++ ui/base/ime/input_method_base.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 InputMethodBase::InputMethodBase()
-    : delegate_(NULL), text_input_client_(NULL) {}
+    : delegate_(nullptr), text_input_client_(nullptr) {}
 
 InputMethodBase::~InputMethodBase() {
   FOR_EACH_OBSERVER(InputMethodObserver,
@@ -33,20 +33,25 @@ void InputMethodBase::SetDelegate(internal::InputMethodDelegate* delegate) {
   delegate_ = delegate;
 }
 
-void InputMethodBase::OnFocus() {}
+void InputMethodBase::OnFocus() {
+  if (ui::IMEBridge::Get())
+    ui::IMEBridge::Get()->SetInputContextHandler(this);
+}
 
-void InputMethodBase::OnBlur() {}
+void InputMethodBase::OnBlur() {
+  if (ui::IMEBridge::Get() &&
+      ui::IMEBridge::Get()->GetInputContextHandler() == this)
+    ui::IMEBridge::Get()->SetInputContextHandler(nullptr);
+}
 
 void InputMethodBase::SetFocusedTextInputClient(TextInputClient* client) {
   SetFocusedTextInputClientInternal(client);
-  if (ui::IMEBridge::Get())
-    ui::IMEBridge::Get()->SetInputContextHandler(this);
 }
 
 void InputMethodBase::DetachTextInputClient(TextInputClient* client) {
   if (text_input_client_ != client)
     return;
-  SetFocusedTextInputClientInternal(NULL);
+  SetFocusedTextInputClientInternal(nullptr);
 }
 
 TextInputClient* InputMethodBase::GetTextInputClient() const {
@@ -132,7 +137,7 @@ void InputMethodBase::SetFocusedTextInputClientInternal(
   if (old == client)
     return;
   OnWillChangeFocusedClient(old, client);
-  text_input_client_ = client;  // NULL allowed.
+  text_input_client_ = client;  // nullptr allowed.
   OnDidChangeFocusedClient(old, client);
   NotifyTextInputStateChanged(text_input_client_);
 }
