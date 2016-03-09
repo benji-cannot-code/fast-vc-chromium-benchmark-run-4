2fd9e882914a407c0080e5a18c2ed5fb6b3fe753
rockot <rockot@chromium.org>
2016-03-08 17:03:40 -0800

    Fix IPC fuzzer ChannelMojo usage
    R=thakis@chromium.org,sammc@chromium.org
    BUG=
    TBR=jam@chromium.org for content
    
    Review URL: https://codereview.chromium.org/1773323002
    
    Cr-Commit-Position: refs/heads/master@{#380006}

--- tools/ipc_fuzzer/message_replay/replay_process.h
+++ tools/ipc_fuzzer/message_replay/replay_process.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_channel_proxy.h"
 #include "ipc/ipc_listener.h"
 #include "ipc/ipc_message.h"
+#include "ipc/mojo/scoped_ipc_support.h"
 #include "tools/ipc_fuzzer/message_lib/message_file.h"
 
 namespace ipc_fuzzer {
@@ -47,6 +48,7 @@ class ReplayProcess : public IPC::Listener {
  private:
   void SendNextMessage();
 
+  scoped_ptr<IPC::ScopedIPCSupport> mojo_ipc_support_;
   scoped_ptr<IPC::ChannelProxy> channel_;
   base::MessageLoop main_loop_;
   base::Thread io_thread_;
