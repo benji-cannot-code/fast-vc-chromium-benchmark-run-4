b7c8044a34f3ae94558e8fc14fe8c949f3fe9b0d
wfh <wfh@chromium.org>
2016-03-08 16:33:02 -0800

    Add creation tracking for ActiveVerifier.
    Sometimes the ActiveVerifier gets instantiated inside chrome_child's
    copy of base and sometimes inside chrome.exe's copy of base.
    
    The ActiveVerifier should only be running from chrome.exe's copy of base,
    but before enforcing this, am adding a check to determine what is causing
    this unpredictability.
    
    See bug for the gory details.
    
    BUG=592753
    
    Review URL: https://codereview.chromium.org/1767293005
    
    Cr-Commit-Position: refs/heads/master@{#379996}

--- base/win/scoped_handle.cc
+++ base/win/scoped_handle.cc
@@ -96,6 +96,7 @@ class ActiveVerifier {
 
   static void InstallVerifier();
 
+  base::debug::StackTrace creation_stack_;
   bool enabled_;
   bool closing_;
   NativeLock* lock_;
@@ -169,6 +170,7 @@ void ActiveVerifier::StartTracking(HANDLE handle, const void* owner,
   if (!result.second) {
     Info other = result.first->second;
     base::debug::Alias(&other);
+    base::debug::Alias(&creation_stack_);
     CHECK(false);  // Attempt to start tracking already tracked handle.
   }
 }
@@ -180,12 +182,15 @@ void ActiveVerifier::StopTracking(HANDLE handle, const void* owner,
 
   AutoNativeLock lock(*lock_);
   HandleMap::iterator i = map_.find(handle);
-  if (i == map_.end())
+  if (i == map_.end()) {
+    base::debug::Alias(&creation_stack_);
     CHECK(false);  // Attempting to close an untracked handle.
+  }
 
   Info other = i->second;
   if (other.owner != owner) {
     base::debug::Alias(&other);
+    base::debug::Alias(&creation_stack_);
     CHECK(false);  // Attempting to close a handle not owned by opener.
   }
 
@@ -210,6 +215,7 @@ void ActiveVerifier::OnHandleBeingClosed(HANDLE handle) {
 
   Info other = i->second;
   base::debug::Alias(&other);
+  base::debug::Alias(&creation_stack_);
   CHECK(false);  // CloseHandle called on tracked handle.
 }
 
