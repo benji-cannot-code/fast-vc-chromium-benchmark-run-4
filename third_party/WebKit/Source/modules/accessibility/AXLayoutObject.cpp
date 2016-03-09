14b70bb95b7da9b7e970d150ae18c59680ecdb2a
nektar <nektar@chromium.org>
2016-03-08 22:33:30 -0800

    Marks textbox role as containing only plain text.
    Fixes Google Docs and Sheets.
    This is according to the ARIA Spec.
    BUG=542998
    TESTED=Manually with NVDA, layout test
    R=dmazzoni@chromium.org
    
    Review URL: https://codereview.chromium.org/1767323002
    
    Cr-Commit-Position: refs/heads/master@{#380090}

--- third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
@@ -364,14 +364,14 @@ static bool isLinkable(const AXObject& object)
 // user-modify. Don't move this logic to AXNodeObject.
 bool AXLayoutObject::isEditable() const
 {
-    if (m_layoutObject && m_layoutObject->isTextControl())
+    if (layoutObject() && layoutObject()->isTextControl())
         return true;
 
     if (node() && node()->isContentEditable())
         return true;
 
     if (isWebArea()) {
-        Document& document = m_layoutObject->document();
+        Document& document = layoutObject()->document();
         HTMLElement* body = document.body();
         if (body && body->isContentEditable())
             return true;
@@ -386,6 +386,9 @@ bool AXLayoutObject::isEditable() const
 // user-modify. Don't move this logic to AXNodeObject.
 bool AXLayoutObject::isRichlyEditable() const
 {
+    if (isARIATextControl())
+        return false;
+
     if (node() && node()->isContentRichlyEditable())
         return true;
 
