f8a49347d11a3cf6654ad821de42d224c93fe820
zmo <zmo@chromium.org>
2016-03-09 15:47:36 -0800

    Handle UNPACK_SKIP_* and PACK_SKIP_* parameters on the client side.
    This is part of https://codereview.chromium.org/1750123002/
    Land this separately because this might have an unexpected perf impact
    (see crbug.com/571885)
    
    BUG=487841, 429053
    TEST=gpu_unittests,webgl_conformance,webgl2_conformance
    TBR=piman@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1785483002
    
    Cr-Commit-Position: refs/heads/master@{#380260}

--- gpu/command_buffer/service/gles2_cmd_decoder_unittest_base.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_unittest_base.cc
@@ -1977,15 +1977,6 @@ void GLES2DecoderTestBase::SetupInitStateManualExpectations(bool es3_capable) {
     EXPECT_CALL(*gl_, PixelStorei(GL_UNPACK_IMAGE_HEIGHT, 0))
         .Times(1)
         .RetiresOnSaturation();
-    EXPECT_CALL(*gl_, PixelStorei(GL_UNPACK_SKIP_PIXELS, 0))
-        .Times(1)
-        .RetiresOnSaturation();
-    EXPECT_CALL(*gl_, PixelStorei(GL_UNPACK_SKIP_ROWS, 0))
-        .Times(1)
-        .RetiresOnSaturation();
-    EXPECT_CALL(*gl_, PixelStorei(GL_UNPACK_SKIP_IMAGES, 0))
-        .Times(1)
-        .RetiresOnSaturation();
   }
 }
 
