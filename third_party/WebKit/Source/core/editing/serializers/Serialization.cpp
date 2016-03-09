08b6116cb77f5cf85971fb22664e7a6a0520fec3
tkent <tkent@chromium.org>
2016-03-09 00:57:40 -0800

    Do not produce BR elements on pasting plain text in TEXTAREA
    This CL makes already-broken TEXTAREA with white-space:normal/nowrap more broken.
    We'll fix it soon.  This CL has no behavior changes in other cases.
    
    BUG=593184
    
    Review URL: https://codereview.chromium.org/1776913002
    
    Cr-Commit-Position: refs/heads/master@{#380105}

--- third_party/WebKit/Source/core/editing/serializers/Serialization.cpp
+++ third_party/WebKit/Source/core/editing/serializers/Serialization.cpp
@@ -468,7 +468,7 @@ PassRefPtrWillBeRawPtr<DocumentFragment> createFragmentFromText(const EphemeralR
     string.replace("\r\n", "\n");
     string.replace('\r', '\n');
 
-    if (shouldPreserveNewline(context)) {
+    if (!isRichlyEditablePosition(context.startPosition()) || shouldPreserveNewline(context)) {
         fragment->appendChild(document.createTextNode(string));
         if (string.endsWith('\n')) {
             RefPtrWillBeRawPtr<HTMLBRElement> element = HTMLBRElement::create(document);
@@ -490,7 +490,6 @@ PassRefPtrWillBeRawPtr<DocumentFragment> createFragmentFromText(const EphemeralR
         && !isHTMLBodyElement(*block)
         && !isHTMLHtmlElement(*block)
         && block != rootEditableElementOf(context.startPosition());
-    bool useLineBreak = enclosingTextFormControl(context.startPosition());
 
     Vector<String> list;
     string.split('\n', true, list); // true gets us empty strings in the list
@@ -503,9 +502,6 @@ PassRefPtrWillBeRawPtr<DocumentFragment> createFragmentFromText(const EphemeralR
             // For last line, use the "magic BR" rather than a P.
             element = HTMLBRElement::create(document);
             element->setAttribute(classAttr, AppleInterchangeNewline);
-        } else if (useLineBreak) {
-            element = HTMLBRElement::create(document);
-            fillContainerFromString(fragment.get(), s);
         } else {
             if (useClonesOfEnclosingBlock)
                 element = block->cloneElementWithoutChildren();
