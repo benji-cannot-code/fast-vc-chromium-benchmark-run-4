4872fb266694ecd6993cb71f7fc623b0185bb401
rockot <rockot@chromium.org>
2016-03-09 09:14:59 -0800

    Convert mus apptests to shelltests
    This reworks some shelltest boilerplate to be more easily
    consumable and converts all mus apptests to shelltests.
    
    BUG=592313
    
    Review URL: https://codereview.chromium.org/1771393002
    
    Cr-Commit-Position: refs/heads/master@{#380148}

--- components/mus/ws/window_manager_client_unittest.cc
+++ components/mus/ws/window_manager_client_unittest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/mus/public/cpp/window_tree_connection_observer.h"
 #include "components/mus/public/cpp/window_tree_delegate.h"
 #include "mojo/converters/geometry/geometry_type_converters.h"
-#include "mojo/shell/public/cpp/application_test_base.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/mojo/geometry/geometry_util.h"
 
@@ -846,8 +845,7 @@ TEST_F(WindowServerTest, Activation) {
             ValidIndexOf(parent->children(), child1));
 }
 
-// Very flaky: http://crbug.com/592313.
-TEST_F(WindowServerTest, DISABLED_ActivationNext) {
+TEST_F(WindowServerTest, ActivationNext) {
   Window* parent = GetFirstWMRoot();
   Window* child1 = NewVisibleWindow(parent, window_manager());
   Window* child2 = NewVisibleWindow(parent, window_manager());
