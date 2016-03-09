4d3e262a8683f4cb8048f689ddc416b6737af529
eranm <eranm@chromium.org>
2016-03-09 11:28:46 -0800

    A second attempt at landing https://codereview.chromium.org/1758823002/, which "Broke sizes due to introducing new static initializers."
    The fix is to include <ostream> in signed_certificate_timestamp.cc rather than iostream.
    
    This reverts commit b9fa69cad6684fcc4d045dae9ba097ae5ff77773.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1774503002
    
    Cr-Commit-Position: refs/heads/master@{#380175}

--- net/cert/signed_tree_head.cc
+++ net/cert/signed_tree_head.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string.h>
 
+#include <ostream>
+
 #include "base/strings/string_number_conversions.h"
 
 namespace net {
@@ -30,17 +32,16 @@ SignedTreeHead::SignedTreeHead(Version version,
 
 SignedTreeHead::~SignedTreeHead() {}
 
-std::ostream& operator<<(std::ostream& stream, const SignedTreeHead& sth) {
-  return stream << "{\n"
-                << "\t\"version\": " << sth.version << ",\n"
-                << "\t\"timestamp\": " << sth.timestamp << ",\n"
-                << "\t\"tree_size\": " << sth.tree_size << ",\n"
-                << "\t\"sha256_root_hash\": \""
-                << base::HexEncode(sth.sha256_root_hash, kSthRootHashLength)
-                << "\",\n\t\"log_id\": \""
-                << base::HexEncode(sth.log_id.data(), sth.log_id.size())
-                << "\"\n"
-                << "}";
+void PrintTo(const SignedTreeHead& sth, std::ostream* os) {
+  (*os) << "{\n"
+        << "\t\"version\": " << sth.version << ",\n"
+        << "\t\"timestamp\": " << sth.timestamp << ",\n"
+        << "\t\"tree_size\": " << sth.tree_size << ",\n"
+        << "\t\"sha256_root_hash\": \""
+        << base::HexEncode(sth.sha256_root_hash, kSthRootHashLength)
+        << "\",\n\t\"log_id\": \""
+        << base::HexEncode(sth.log_id.data(), sth.log_id.size()) << "\"\n"
+        << "}";
 }
 
 }  // namespace ct
