9f1fdaa67927096c33260af1302d5683aa244e3f
tzik <tzik@chromium.org>
2016-03-09 01:47:03 -0800

    Extend base::Callback to have a move-only variant
    Add CopyMode flag to base::Callback to split it to the copyable variant
    and the move-only variant.
    
    BUG=554299
    
    Review URL: https://codereview.chromium.org/1699773002
    
    Cr-Commit-Position: refs/heads/master@{#380108}

--- base/callback_forward.h
+++ base/callback_forward.h
@@ -7,8 +7,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_CALLBACK_FORWARD_H_
 
 namespace base {
+namespace internal {
 
-template <typename Sig>
+// CopyMode is used to control the copyablity of a Callback.
+// MoveOnly indicates the Callback is not copyable but movable, and Copyable
+// indicates it is copyable and movable.
+enum class CopyMode {
+  MoveOnly, Copyable,
+};
+
+}  // namespace internal
+
+template <typename Signature,
+          internal::CopyMode copy_mode = internal::CopyMode::Copyable>
 class Callback;
 
 // Syntactic sugar to make Callback<void()> easier to declare since it
