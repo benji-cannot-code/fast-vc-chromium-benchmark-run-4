8d6f7253e1f4c0038ef62ce7d921e5105884199d
bokan <bokan@chromium.org>
2016-03-10 06:54:35 -0800

    Merged FrameView and LayoutBox scrolling in EventHandler.
    Scrolling a FrameView (window and iframes) has some small differences from
    scrolling a normal LayoutBox. Because of this, we've generally forked all our
    scrolling code to take two different paths based on whether a LayoutBox is a
    FrameView/LayoutView. This has caused pain in that much logic has been
    duplicated between the two.
    
    This patch removes the duplication in the calling code and makes the decision
    based on the virtual LayoutBox::scroll function.
    
    BUG=591124
    
    Review URL: https://codereview.chromium.org/1752043002
    
    Cr-Commit-Position: refs/heads/master@{#380403}

--- third_party/WebKit/Source/core/layout/LayoutView.cpp
+++ third_party/WebKit/Source/core/layout/LayoutView.cpp
@@ -1026,4 +1026,12 @@ bool LayoutView::allowsOverflowClip() const
     return document().settings() && document().settings()->rootLayerScrolls();
 }
 
+ScrollResult LayoutView::scroll(ScrollGranularity granularity, const FloatSize& delta)
+{
+    if (!frameView())
+        return ScrollResult();
+
+    return frame()->applyScrollDelta(granularity, delta, false);
+}
+
 } // namespace blink
