a647037bfb9c8da7cfac8b17cca529254a98a19b
michaelpg <michaelpg@chromium.org>
2016-03-10 15:56:34 -0800

    MD Settings: Fix SettingsSystemPageElement annotation
    BUG=546844
    R=dbeam@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1784933002
    
    Cr-Commit-Position: refs/heads/master@{#380495}

--- chrome/browser/resources/settings/advanced_page/advanced_page.js
+++ chrome/browser/resources/settings/advanced_page/advanced_page.js
@@ -44,7 +44,7 @@ Polymer({
 
   /** @private */
   onDomChange_: function() {
-    var systemPage = /** @type {?SystemPageElement} */(
+    var systemPage = /** @type {?SettingsSystemPageElement} */(
         this.$$('settings-system-page'));
     if (systemPage && !systemPage.delegate)
       systemPage.delegate = new settings.SystemPageDelegateImpl;
