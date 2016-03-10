8ea2e6d98f98ad710c7eb181d577370ab3846842
alexmos <alexmos@chromium.org>
2016-03-10 14:00:27 -0800

    Disable DumpAccessibilityTreeTest.AccessibilityModalDialogInIframeOpened on Windows
    This appears flaky just like AccessibilityModalDialogClosed which was
    disabled earlier.
    
    BUG=593846
    TBR=dmazzoni@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1783983002
    
    Cr-Commit-Position: refs/heads/master@{#380476}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -1090,6 +1090,7 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
   RunHtmlTest(FILE_PATH_LITERAL("modal-dialog-opened.html"));
 }
 
+// Flaky on Windows: crbug.com/593846
 #if defined(OS_WIN)
 #define MAYBE_AccessibilityModalDialogInIframeClosed \
     DISABLED_AccessibilityModalDialogInIframeClosed
@@ -1102,8 +1103,16 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
   RunHtmlTest(FILE_PATH_LITERAL("modal-dialog-in-iframe-closed.html"));
 }
 
+// Flaky on Windows: crbug.com/593846
+#if defined(OS_WIN)
+#define MAYBE_AccessibilityModalDialogInIframeOpened \
+    DISABLED_AccessibilityModalDialogInIframeOpened
+#else
+#define MAYBE_AccessibilityModalDialogInIframeOpened \
+    AccessibilityModalDialogInIframeOpened
+#endif
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
-                       AccessibilityModalDialogInIframeOpened) {
+                       MAYBE_AccessibilityModalDialogInIframeOpened) {
   RunHtmlTest(FILE_PATH_LITERAL("modal-dialog-in-iframe-opened.html"));
 }
 
