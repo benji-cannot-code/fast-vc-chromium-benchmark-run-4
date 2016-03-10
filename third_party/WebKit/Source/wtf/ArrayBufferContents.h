f52e95ae866c650613fe51817ec77d654be718ec
haraken <haraken@chromium.org>
2016-03-10 11:55:46 -0800

    Register ArrayBufferContents' adjustAmountOfMemory in V8Initializer::initialize
    Currently ArrayBufferContents' adjustAmountOfMemory is registered in initializeWithoutV8
    but this is not nice because adjustAmountOfMemory is a method used *with* V8.
    The registration should be moved to V8Initializer::initialize.
    
    BUG=593269
    
    Review URL: https://codereview.chromium.org/1776943002
    
    Cr-Commit-Position: refs/heads/master@{#380450}

--- third_party/WebKit/Source/wtf/ArrayBufferContents.h
+++ third_party/WebKit/Source/wtf/ArrayBufferContents.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "wtf/Allocator.h"
 #include "wtf/Assertions.h"
+#include "wtf/MainThread.h"
 #include "wtf/Noncopyable.h"
 #include "wtf/RefPtr.h"
 #include "wtf/ThreadSafeRefCounted.h"
@@ -37,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/WTFExport.h"
 
 namespace WTF {
+
+typedef void(*AdjustAmountOfExternalAllocatedMemoryFunction)(int size);
 
 class WTF_EXPORT ArrayBufferContents {
     WTF_MAKE_NONCOPYABLE(ArrayBufferContents);
@@ -77,8 +80,9 @@ public:
     static void allocateMemory(size_t, InitializationPolicy, void*&);
     static void allocateMemoryOrNull(size_t, InitializationPolicy, void*&);
     static void freeMemory(void*, size_t);
-    static void setAdjustAmoutOfExternalAllocatedMemoryFunction(AdjustAmountOfExternalAllocatedMemoryFunction function)
+    static void initialize(AdjustAmountOfExternalAllocatedMemoryFunction function)
     {
+        ASSERT(isMainThread());
         ASSERT(!s_adjustAmountOfExternalAllocatedMemoryFunction);
         s_adjustAmountOfExternalAllocatedMemoryFunction = function;
     }
