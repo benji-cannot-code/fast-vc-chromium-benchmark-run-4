60beef2a447a87e794186c6e0eb9915fb5b4abd2
tapted <tapted@chromium.org>
2016-03-09 22:29:25 -0800

    MacViews: Fix MenuRunnerTest.NonLatinMnemonic
    The test NSEvent synthesizer didn't handle unicode characters. Add
    support for this by feeding through the DomKey from "character"
    ui::KeyEvents.
    
    BUG=592890
    
    Review URL: https://codereview.chromium.org/1779743004
    
    Cr-Commit-Position: refs/heads/master@{#380361}

--- ui/events/test/cocoa_test_event_utils.h
+++ ui/events/test/cocoa_test_event_utils.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <objc/objc-class.h>
 
+#include "ui/events/keycodes/dom/dom_key.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 
 namespace cocoa_test_event_utils {
@@ -68,7 +69,8 @@ NSTimeInterval TimeIntervalSinceSystemStartup();
 NSEvent* SynthesizeKeyEvent(NSWindow* window,
                             bool keyDown,
                             ui::KeyboardCode keycode,
-                            NSUInteger flags);
+                            NSUInteger flags,
+                            ui::DomKey dom_key = ui::DomKey::NONE);
 
 }  // namespace cocoa_test_event_utils
 
