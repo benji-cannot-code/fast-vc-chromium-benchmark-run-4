afa9252e502a5c4d5a0ec28935709c8403a00a32
xiaochengh <xiaochengh@chromium.org>
2016-03-09 17:23:39 -0800

    Remove an unused parameter from canHaveWhitespaceChildren()
    The unused parameter |Text* text| of this function is removed.
    
    Consequently, |Text::textLayoutObjectIsNeeded()| can be declared |const|,
    and |shouldUpdateLayoutByReattaching()| can be passed with |const Text&|.
    
    BUG=n/a
    TEST=n/a; no behavior change
    
    Review URL: https://codereview.chromium.org/1773403003
    
    Cr-Commit-Position: refs/heads/master@{#380288}

--- third_party/WebKit/Source/core/dom/Text.h
+++ third_party/WebKit/Source/core/dom/Text.h
@@ -53,7 +53,7 @@ public:
     PassRefPtrWillBeRawPtr<Text> replaceWholeText(const String&);
 
     void recalcTextStyle(StyleRecalcChange, Text* nextTextSibling);
-    bool textLayoutObjectIsNeeded(const ComputedStyle&, const LayoutObject& parent);
+    bool textLayoutObjectIsNeeded(const ComputedStyle&, const LayoutObject& parent) const;
     LayoutText* createTextLayoutObject(const ComputedStyle&);
     void updateTextLayoutObject(unsigned offsetOfReplacedData, unsigned lengthOfReplacedData, RecalcStyleBehavior = DoNotRecalcStyle);
 
