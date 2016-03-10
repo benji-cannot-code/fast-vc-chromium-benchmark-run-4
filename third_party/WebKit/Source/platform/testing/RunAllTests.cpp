725c386754e82e96b0b90cae6a501fffa6b2ca77
haraken <haraken@chromium.org>
2016-03-09 23:26:46 -0800

    Introduce Platform::setCurrentPlatformForTesting
    In preparation for cleaning up the initialization/shutdown logic of Blink,
    I need to make Platfrom::initialize() and Platform::shutdown() are balanced
    (i.e., Platfrom::initialize() and Platform::shutdown() should be called only once
    respectively.)
    
    Currently TestingPlatformSupport violates the contract (i.e., it calls Platfrom::initialize()
    multiple times). This CL introduces Platform::setCurrentPlatformForTesting
    and makes sure that Platfrom::initialize() is called only once.
    
    BUG=593269
    
    Review URL: https://codereview.chromium.org/1780083002
    
    Cr-Commit-Position: refs/heads/master@{#380370}

--- third_party/WebKit/Source/platform/testing/RunAllTests.cpp
+++ third_party/WebKit/Source/platform/testing/RunAllTests.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/HTTPNames.h"
 #include "platform/heap/Heap.h"
 #include "platform/testing/TestingPlatformSupport.h"
+#include "public/platform/Platform.h"
 #include "wtf/CryptographicallyRandomNumber.h"
 #include "wtf/CurrentTime.h"
 #include "wtf/MainThread.h"
@@ -73,6 +74,7 @@ int main(int argc, char** argv)
     WTF::initialize(nullptr);
     WTF::initializeMainThread(0);
 
+    blink::Platform::initialize(nullptr);
     blink::TestingPlatformSupport::Config platformConfig;
     cc_blink::WebCompositorSupportImpl compositorSupport;
     platformConfig.compositorSupport = &compositorSupport;
@@ -96,6 +98,8 @@ int main(int argc, char** argv)
 
     blink::ThreadState::detachMainThread();
     blink::Heap::shutdown();
+
+    blink::Platform::shutdown();
 
     WTF::shutdown();
     WTF::Partitions::shutdown();
