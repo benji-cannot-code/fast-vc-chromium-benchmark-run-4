ee703688e45724409d9a177f48e820bca2edb9e0
thakis <thakis@chromium.org>
2016-03-10 07:12:06 -0800

    Revert of Fix tests for check-ipc Clang plugin to pass on Windows. (patchset #2 id:20001 of https://codereview.chromium.org/1785523002/ )
    Reason for revert:
    IPC plugin tests are failing on the clang/tot win bots.
    
    Original issue's description:
    > Fix tests for check-ipc Clang plugin to pass on Windows.
    >
    > Add -fno-delayed-template-parsing to disable MSVC-like behavior (check-ipc
    > will only be used on Android, the only platform where we need 32/64-bit
    > processes to talk to each other), remove test that relies on size_t being
    > unsigned long.
    >
    > BUG=581409
    >
    > Committed: https://crrev.com/b9d266457c4b0b619304c99a28fce037be7b0507
    > Cr-Commit-Position: refs/heads/master@{#380271}
    
    TBR=dcheng@chromium.org,hans@chromium.org,dskiba@google.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=581409
    
    Review URL: https://codereview.chromium.org/1778383002
    
    Cr-Commit-Position: refs/heads/master@{#380407}

--- tools/clang/plugins/tests/ipc.cpp
+++ tools/clang/plugins/tests/ipc.cpp
@@ -203,7 +203,7 @@ IPC_TEST_MESSAGE(__COUNTER__, (char, short, std::pair<size_t, bool>)) // ERROR
 IPC_TEST_MESSAGE(__COUNTER__, (std::vector<std::vector<long&>&>&)) // ERROR
 
 
-/* Check IPC::WriteParam() arguments. ERRORS: 30 */
+/* Check IPC::WriteParam() arguments. ERRORS: 31 */
 
 // ERRORS: 21
 void TestWriteParamArgument() {
@@ -306,7 +306,7 @@ struct Provider {
   std::vector<uint64_t> uint64s_data;
 };
 
-// ERRORS: 9
+// ERRORS: 10
 void TestWriteParamMemberArgument() {
   Provider p;
 
@@ -326,9 +326,7 @@ void TestWriteParamMemberArgument() {
   IPC::WriteParam(nullptr, p.get_long()); // ERROR
   IPC::WriteParam(nullptr, p.long_data); // ERROR
 
-  // This one is flaky and depends on whether size_t is typedefed to a
-  // blacklisted type (unsigned long).
-  //IPC::WriteParam(nullptr, p.get<size_t>()); // ERROR
+  IPC::WriteParam(nullptr, p.get<size_t>()); // ERROR
   IPC::WriteParam(nullptr, p.get_size()); // ERROR
   IPC::WriteParam(nullptr, p.size_data); // ERROR
 
@@ -351,4 +349,4 @@ void TestWriteParamMemberArgument() {
 }
 
 
-/* ERRORS: 41 */
+/* ERRORS: 42 */
