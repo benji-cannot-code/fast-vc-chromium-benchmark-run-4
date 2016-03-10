961cf7e0a949c1d1540e43b0f0bce8218ff967aa
sievers <sievers@chromium.org>
2016-03-09 17:39:04 -0800

    Defer WebGL lost context restore until visible.
    This doesn't affect the force-evict logic but only contexts
    in 'Auto' recovery mode. If the context is lost while hidden,
    defer restoring it until the canvas becomes visible again.
    
    The motivation is that this is more efficient wrt memory,
    and in particular on Android avoids webgl context restore
    to fight with the GPU oom killer for hidden webgl tabs.
    
    BUG=586905
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1756633002
    
    Cr-Commit-Position: refs/heads/master@{#380293}

--- third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.h
+++ third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.h
@@ -369,7 +369,8 @@ public:
         // Restore when resources are available.
         WhenAvailable,
 
-        // Restore as soon as possible.
+        // Restore as soon as possible, but only when
+        // the canvas is visible.
         Auto
     };
     void loseContext(LostContextMode) override;
@@ -494,6 +495,7 @@ protected:
 
     RefPtr<WebGLContextGroup> m_contextGroup;
 
+    bool m_isHidden;
     LostContextMode m_contextLostMode;
     AutoRecoveryMethod m_autoRecoveryMethod;
     // Dispatches a context lost event once it is determined that one is needed.
