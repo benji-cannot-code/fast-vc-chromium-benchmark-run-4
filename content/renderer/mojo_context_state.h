514412df42482dc4491e58c23d9918f5b88adc85
sammc <sammc@chromium.org>
2016-03-10 21:24:39 -0800

    Handle JS mojo module fetch failures gracefully.
    Currently, if the fetch of the source for a JS mojo module fails, the
    requesting renderer crashes. With this CL, it logs an error instead.
    
    BUG=592831
    
    Review URL: https://codereview.chromium.org/1776263002
    
    Cr-Commit-Position: refs/heads/master@{#380528}

--- content/renderer/mojo_context_state.h
+++ content/renderer/mojo_context_state.h
@@ -56,6 +56,7 @@ class MojoContextState : public gin::ModuleRegistryObserver {
 
   // Callback once a module has finished downloading. Passes data to |runner_|.
   void OnFetchModuleComplete(ResourceFetcher* fetcher,
+                             const std::string& id,
                              const blink::WebURLResponse& response,
                              const std::string& data);
 
