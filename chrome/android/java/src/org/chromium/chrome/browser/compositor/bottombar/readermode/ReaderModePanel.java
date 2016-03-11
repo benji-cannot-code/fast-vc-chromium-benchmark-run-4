c049311a4e69b38c0ddd133b00dbcfb11393206b
mdjones <mdjones@chromium.org>
2016-03-10 16:21:16 -0800

    Dismiss Reader Mode panel on user triggered close
    The Reader Mode panel only dismissed when the close button was used.
    This change listens to any user triggered close event and dismisses
    the panel accordingly.
    
    BUG=586061
    
    Review URL: https://codereview.chromium.org/1777373002
    
    Cr-Commit-Position: refs/heads/master@{#380499}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/readermode/ReaderModePanel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/readermode/ReaderModePanel.java
@@ -173,7 +173,6 @@ public class ReaderModePanel extends OverlayPanel {
         super.handleBarClick(time, x, y);
         if (isCoordinateInsideCloseButton(x)) {
             closePanel(StateChangeReason.CLOSE_BUTTON, true);
-            mManagerDelegate.onCloseButtonPressed();
         } else {
             maximizePanel(StateChangeReason.SEARCH_BAR_TAP);
         }
@@ -263,6 +262,13 @@ public class ReaderModePanel extends OverlayPanel {
         if (mTimerRunning) {
             onTimerEnded();
         }
+    }
+
+    @Override
+    protected void onClosed(StateChangeReason reason) {
+        super.onClosed(reason);
+        if (mManagerDelegate == null) return;
+        mManagerDelegate.onClosed(reason);
     }
 
     /**
