f6bc8f3ef89434d6d5eb9e55c46af273c420ca9d
tkent <tkent@chromium.org>
2016-03-11 02:34:06 -0800

    Improve paste performance in TEXTAREA.
    Skip unnecessary layout by removeUnrenderedTextNodesAtEnds.
    
    BUG=586772
    
    Review URL: https://codereview.chromium.org/1785003003
    
    Cr-Commit-Position: refs/heads/master@{#380593}

--- third_party/WebKit/Source/core/editing/commands/ReplaceSelectionCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/ReplaceSelectionCommand.cpp
@@ -1220,7 +1220,8 @@ void ReplaceSelectionCommand::doApply(EditingState* editingState)
         node = next;
     }
 
-    removeUnrenderedTextNodesAtEnds(insertedNodes);
+    if (isRichlyEditablePosition(insertionPos))
+        removeUnrenderedTextNodesAtEnds(insertedNodes);
 
     if (!handledStyleSpans) {
         handleStyleSpans(insertedNodes, editingState);
