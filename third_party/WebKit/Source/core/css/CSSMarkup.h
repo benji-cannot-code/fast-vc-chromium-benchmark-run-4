b77d83608c489c3fd3eaa61068e1f07c9f033dc8
rob.buis <rob.buis@samsung.com>
2016-03-10 21:16:35 -0800

    Use <string> serialization rules for computed value of reference filter
    According to [1] the filter reference is a <url> type. For <url> types
    the spec says to use <string>s [2]. And <string>s serialize with double
    quotes.
    
    Also remove the unused quoteCSSURLIfNeeded.
    
    Behavior matches Firefox.
    
    [1] https://drafts.fxtf.org/filters/#typedef-filter-function-list
    [2] https://www.w3.org/TR/css-values/#urls
    [3] https://drafts.csswg.org/cssom/#serialize-a-string
    
    Review URL: https://codereview.chromium.org/1783693006
    
    Cr-Commit-Position: refs/heads/master@{#380517}

--- third_party/WebKit/Source/core/css/CSSMarkup.h
+++ third_party/WebKit/Source/core/css/CSSMarkup.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 String quoteCSSStringIfNeeded(const String&);
-String quoteCSSURLIfNeeded(const String&);
 
 // Common serializing methods. See: http://dev.w3.org/csswg/cssom/#common-serializing-idioms
 void serializeIdentifier(const String& identifier, StringBuilder& appendTo);
