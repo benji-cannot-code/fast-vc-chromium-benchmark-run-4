2200209e773cbf88dfce3a3f4f32b9087b98c433
davidben <davidben@chromium.org>
2016-03-11 15:01:02 -0800

    Switch //net to the new SPKI and PKCS#8 APIs.
    This fixes the new verifier's SPKI parser and gets us closer to losing the
    legacy ASN.1 stack. This also rewrites the Android KeygenHandler
    implementation. This was tested manually, but to make sure it's good, make
    the tests actually parse out the SPKAC structure and verify the signature.
    (Everything that implements keygen may freely assume BoringSSL, so we have
    CBS and EVP available.)
    
    In doing so, this revealed a bug in our Mac SPKAC code. It was omitting the
    NULLs in both SPKI and signatureAlgorithm, and EVP_parse_public_key is
    strict about including them, per the MUST in the spec. (I've confirmed that
    Safari includes them, so it's just us that messed this up.)
    
    This resolves the last TODO about laxness in VerifySignedDataTest, so we
    should now be parsing SPKIs correctly. (Finally...)
    
    BUG=499653,522228
    
    Review URL: https://codereview.chromium.org/1742873002
    
    Cr-Commit-Position: refs/heads/master@{#380774}

--- net/android/keystore_unittest.cc
+++ net/android/keystore_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <openssl/evp.h>
 #include <openssl/pem.h>
 #include <openssl/rsa.h>
-#include <openssl/x509.h>
 
 #include "base/android/build_info.h"
 #include "base/android/jni_android.h"
@@ -24,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_file.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
+#include "crypto/auto_cbb.h"
 #include "crypto/openssl_util.h"
 #include "net/android/keystore.h"
 #include "net/android/keystore_openssl.h"
@@ -129,18 +129,16 @@ EVP_PKEY* ImportPrivateKeyFile(const char* filename) {
 // Returns true on success, false otherwise.
 bool GetPrivateKeyPkcs8Bytes(const crypto::ScopedEVP_PKEY& pkey,
                              std::string* pkcs8) {
-  // Convert to PKCS#8 object.
-  ScopedPKCS8_PRIV_KEY_INFO p8_info(EVP_PKEY2PKCS8(pkey.get()));
-  if (!p8_info.get()) {
-    LOG(ERROR) << "Can't get PKCS#8 private key from EVP_PKEY: "
-               << GetOpenSSLErrorString();
+  uint8_t* der;
+  size_t der_len;
+  crypto::AutoCBB cbb;
+  if (!CBB_init(cbb.get(), 0) ||
+      !EVP_marshal_private_key(cbb.get(), pkey.get()) ||
+      !CBB_finish(cbb.get(), &der, &der_len)) {
     return false;
   }
-
-  // Then convert it
-  int len = i2d_PKCS8_PRIV_KEY_INFO(p8_info.get(), NULL);
-  unsigned char* p = OpenSSLWriteInto(pkcs8, static_cast<size_t>(len));
-  i2d_PKCS8_PRIV_KEY_INFO(p8_info.get(), &p);
+  pkcs8->assign(reinterpret_cast<const char*>(der), der_len);
+  OPENSSL_free(der);
   return true;
 }
 
