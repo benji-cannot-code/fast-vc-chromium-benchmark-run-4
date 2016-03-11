b5b4385f1d8a7557ee8b157a12da4cd9ab386d57
tapted <tapted@chromium.org>
2016-03-10 16:22:33 -0800

    MacViews: Fix MenuControllerTest.SelectChildButtonView
    Above the declaration for MenuController::OnMouseMoved(..) it says
      // NOTE: the coordinates of the events are in that of the
      // MenuScrollViewContainer.
    But the test wasn't doing that.
    
    I think on Mac the menu Widget is shifted down by the height of the
    main menu bar, so the test couldn't assume it's at the top left of
    the screen.
    
    BUG=592890
    
    Review URL: https://codereview.chromium.org/1778243003
    
    Cr-Commit-Position: refs/heads/master@{#380500}

--- ui/views/controls/menu/menu_controller_unittest.cc
+++ ui/views/controls/menu/menu_controller_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/menu/menu_delegate.h"
 #include "ui/views/controls/menu/menu_item_view.h"
 #include "ui/views/controls/menu/menu_message_loop.h"
+#include "ui/views/controls/menu/menu_scroll_view_container.h"
 #include "ui/views/controls/menu/submenu_view.h"
 #include "ui/views/test/views_test_base.h"
 
@@ -408,8 +409,9 @@ class MenuControllerTest : public ViewsTestBase {
     menu_controller_->SetSelectionOnPointerDown(source, event);
   }
 
-  void ProcessMouseMoved(SubmenuView* source,
-                         const ui::MouseEvent& event) {
+  // Note that coordinates of events passed to MenuController must be in that of
+  // the MenuScrollViewContainer.
+  void ProcessMouseMoved(SubmenuView* source, const ui::MouseEvent& event) {
     menu_controller_->OnMouseMoved(source, event);
   }
 
@@ -765,10 +767,11 @@ TEST_F(MenuControllerTest, SelectChildButtonView) {
   EXPECT_TRUE(button3->IsHotTracked());
 
   // Move a mouse to hot track the |button1|.
+  SubmenuView* sub_menu = menu_item()->GetSubmenu();
   gfx::Point location(button1->GetBoundsInScreen().CenterPoint());
+  View::ConvertPointFromScreen(sub_menu->GetScrollViewContainer(), &location);
   ui::MouseEvent event(ui::ET_MOUSE_MOVED, location, location,
                        ui::EventTimeForNow(), 0, 0);
-  SubmenuView* sub_menu = menu_item()->GetSubmenu();
   ProcessMouseMoved(sub_menu, event);
 
   // Incrementing selection should move hot tracking to the second button (next
