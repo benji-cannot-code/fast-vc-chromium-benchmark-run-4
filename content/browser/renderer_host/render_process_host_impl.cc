5a72a5f30c7189ef6fd3d228830e44ad7270ac46
liushouqun <liushouqun@xiaomi.com>
2016-03-10 22:57:33 -0800

    Remove ICU header includes in content which are not necessary.
    BUG=
    
    Review URL: https://codereview.chromium.org/1777133003
    
    Cr-Commit-Position: refs/heads/master@{#380562}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -171,8 +171,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context_getter.h"
 #include "ppapi/shared_impl/ppapi_switches.h"
 #include "storage/browser/fileapi/sandbox_file_system_backend.h"
-#include "third_party/icu/source/common/unicode/unistr.h"
-#include "third_party/icu/source/i18n/unicode/timezone.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/base/ui_base_switches.h"
 #include "ui/events/event_switches.h"
