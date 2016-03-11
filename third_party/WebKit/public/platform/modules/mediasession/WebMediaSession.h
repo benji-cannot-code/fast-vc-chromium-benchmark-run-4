ebfa714f7bbaed275617b37f6ffdcb4d323b9989
davve <davve@opera.com>
2016-03-11 06:55:07 -0800

    Propagate media session id into MediaPlayerAndroid
    The media session id will be used to implicitly activate a user defined media session.
    
    BUG=497735, 581728
    
    Review URL: https://codereview.chromium.org/1640123004
    
    Cr-Commit-Position: refs/heads/master@{#380632}

--- third_party/WebKit/public/platform/modules/mediasession/WebMediaSession.h
+++ third_party/WebKit/public/platform/modules/mediasession/WebMediaSession.h
@@ -19,6 +19,8 @@ struct WebMediaMetadata;
 class WebMediaSession {
 public:
     enum {
+        // The media session for media elements that don't have an
+        // explicit user created media session set.
         DefaultID = 0
     };
 
