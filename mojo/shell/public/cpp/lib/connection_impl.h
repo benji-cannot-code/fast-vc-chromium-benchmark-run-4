58a8b60510a2a540b332c7cf37a9e0001cb9e21b
ben <ben@chromium.org>
2016-03-11 08:07:51 -0800

    Implement capability classes:
    - pass the union of the interfaces in the "interfaces" list of the capability request and the interfaces implied by all classes present in the request
    - pass the class names so they can be used for non-interface capability granting
    
    R=sky@chromium.org
    BUG=
    
    Committed: https://crrev.com/191b85068b4db3a79ff4853edef91857e98c68ea
    Cr-Commit-Position: refs/heads/master@{#380552}
    
    Review URL: https://codereview.chromium.org/1781913003
    
    Cr-Commit-Position: refs/heads/master@{#380644}

--- mojo/shell/public/cpp/lib/connection_impl.h
+++ mojo/shell/public/cpp/lib/connection_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/shell/public/cpp/capabilities.h"
 #include "mojo/shell/public/cpp/connection.h"
 #include "mojo/shell/public/cpp/identity.h"
 #include "mojo/shell/public/interfaces/connector.mojom.h"
@@ -34,7 +35,7 @@ class ConnectionImpl : public Connection {
                  uint32_t remote_id,
                  shell::mojom::InterfaceProviderPtr remote_interfaces,
                  shell::mojom::InterfaceProviderRequest local_interfaces,
-                 const std::set<std::string>& allowed_interfaces,
+                 const CapabilityRequest& capability_request,
                  State initial_state);
   ~ConnectionImpl() override;
 
@@ -42,6 +43,7 @@ class ConnectionImpl : public Connection {
 
  private:
   // Connection:
+  bool HasCapabilityClass(const std::string& class_name) const override;
   const std::string& GetConnectionName() override;
   const Identity& GetRemoteIdentity() const override;
   void SetConnectionLostClosure(const Closure& handler) override;
@@ -69,7 +71,7 @@ class ConnectionImpl : public Connection {
   InterfaceRegistry local_registry_;
   shell::mojom::InterfaceProviderPtr remote_interfaces_;
 
-  const std::set<std::string> allowed_interfaces_;
+  const CapabilityRequest capability_request_;
   const bool allow_all_interfaces_;
 
   base::WeakPtrFactory<ConnectionImpl> weak_factory_;
