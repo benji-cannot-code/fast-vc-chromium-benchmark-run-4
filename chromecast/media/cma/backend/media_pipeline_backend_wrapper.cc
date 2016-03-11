8ef9a387c77087df9db75bfed24ce68e039bffdf
derekjchow <derekjchow@chromium.org>
2016-03-10 21:20:12 -0800

    [Chromecast] Fix potential nullptr dereference.
    Guard call to SetStreamTypeVolume in MediaPipelineBackendWrapper
    with a check for audio_decoder_wrapper_. An audio decoder wrapper
    is not guarenteed to be initialized.
    
    Also adds DCHECKs to enforce correct API usage of Create*Decoder
    and Initialize.
    
    R=halliwell@chromium.org
    BUG=internal b/27596803
    TEST=Play video content with no audio track
    
    Review URL: https://codereview.chromium.org/1786563002
    
    Cr-Commit-Position: refs/heads/master@{#380522}

--- chromecast/media/cma/backend/media_pipeline_backend_wrapper.cc
+++ chromecast/media/cma/backend/media_pipeline_backend_wrapper.cc
@@ -29,6 +29,7 @@ MediaPipelineBackendWrapper::~MediaPipelineBackendWrapper() {
 
 MediaPipelineBackend::AudioDecoder*
 MediaPipelineBackendWrapper::CreateAudioDecoder() {
+  DCHECK(!is_initialized_);
   if (audio_decoder_wrapper_)
     return nullptr;
 
@@ -39,12 +40,14 @@ MediaPipelineBackendWrapper::CreateAudioDecoder() {
 
 MediaPipelineBackend::VideoDecoder*
 MediaPipelineBackendWrapper::CreateVideoDecoder() {
+  DCHECK(!is_initialized_);
   return backend_->CreateVideoDecoder();
 }
 
 bool MediaPipelineBackendWrapper::Initialize() {
+  DCHECK(!is_initialized_);
   is_initialized_ = backend_->Initialize();
-  if (is_initialized_)
+  if (is_initialized_ && audio_decoder_wrapper_)
     audio_decoder_wrapper_->SetStreamTypeVolume(stream_type_volume_);
 
   return is_initialized_;
