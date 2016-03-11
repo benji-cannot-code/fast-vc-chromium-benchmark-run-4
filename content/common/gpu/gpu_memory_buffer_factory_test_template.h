e7bc8c3547bd7682ca92ac6bed7bc2e8edb82e49
markdittmer <markdittmer@chromium.org>
2016-03-10 21:12:13 -0800

    Migrate gpu_memory_buffer_factory_test_template to content/common/gpu
    Context: As part of a plan to move content/common/gpu out of content, we are
    eliminating deps from content/common/gpu to elsewhere in content.
    
    BUG=592254
    
    Review URL: https://codereview.chromium.org/1784723003
    
    Cr-Commit-Position: refs/heads/master@{#380511}

--- content/common/gpu/gpu_memory_buffer_factory_test_template.h
+++ content/common/gpu/gpu_memory_buffer_factory_test_template.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This file defines tests that implementations of GpuMemoryBufferFactory should
 // pass in order to be conformant.
 
-#ifndef CONTENT_TEST_GPU_MEMORY_BUFFER_FACTORY_TEST_TEMPLATE_H_
-#define CONTENT_TEST_GPU_MEMORY_BUFFER_FACTORY_TEST_TEMPLATE_H_
+#ifndef CONTENT_COMMON_GPU_GPU_MEMORY_BUFFER_FACTORY_TEST_TEMPLATE_H_
+#define CONTENT_COMMON_GPU_GPU_MEMORY_BUFFER_FACTORY_TEST_TEMPLATE_H_
 
 #include "content/common/gpu/gpu_memory_buffer_factory.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -96,4 +96,4 @@ REGISTER_TYPED_TEST_CASE_P(GpuMemoryBufferFactoryImportTest,
 
 }  // namespace content
 
-#endif  // CONTENT_TEST_GPU_MEMORY_BUFFER_FACTORY_TEST_TEMPLATE_H_
+#endif  // CONTENT_COMMON_GPU_GPU_MEMORY_BUFFER_FACTORY_TEST_TEMPLATE_H_
