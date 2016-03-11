68217661382aff419d2d66218f2edc114bb43c74
dtseng <dtseng@chromium.org>
2016-03-11 14:43:49 -0800

    Do not output empty value changes.
    When arrowing around a spreadsheet in Sheets, ChromeVox sends empty strings to tts.
    
    A generic fix ignoring empty strings caused many regressions:
    https://codereview.chromium.org/1770943004/
    
    A closer investigation of the issue found that:
    cvox.ChromeVoxEventWatcher.handleEvent_ calls cvox.ChromeVoxEventWatcher.handleControlChanged which always announces value changes for Aria roles. Sheets uses role="textbox" on each textbox.
    
    The announcement itself is empty ('').
    
    A distinct / different codepath that also results in empty text is via EditableTextBase.
    TEST=move around in Sheets and observe all announcements work.
    
    Review URL: https://codereview.chromium.org/1791523002
    
    Cr-Commit-Position: refs/heads/master@{#380767}

--- chrome/browser/resources/chromeos/chromevox/common/editable_text_base.js
+++ chrome/browser/resources/chromeos/chromevox/common/editable_text_base.js
@@ -294,6 +294,9 @@ cvox.ChromeVoxEditableTextBase.prototype.shouldDescribeChange = function(evt) {
  */
 cvox.ChromeVoxEditableTextBase.prototype.speak =
     function(str, opt_triggeredByUser, opt_personality) {
+  if (!str) {
+    return;
+  }
   var queueMode = cvox.QueueMode.QUEUE;
   if (opt_triggeredByUser === true) {
     queueMode = cvox.QueueMode.FLUSH;
