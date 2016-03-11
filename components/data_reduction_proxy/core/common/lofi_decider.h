d2d984bacca8fc60293ad69f3628c9c98180dbb6
megjablon <megjablon@chromium.org>
2016-03-11 15:24:55 -0800

    Lo-Fi should check that the proxy is a Data Reduction Proxy
    Before adding the "Chrome-proxy: q=low" header, the LoFiDecider should
    check that the given proxy at the time of OnBeforeSendProxyHeadersInternal
    is a Data Reduction Proxy.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1779713004
    
    Cr-Commit-Position: refs/heads/master@{#380782}

--- components/data_reduction_proxy/core/common/lofi_decider.h
+++ components/data_reduction_proxy/core/common/lofi_decider.h
@@ -7,13 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_DATA_REDUCTION_PROXY_CORE_BROWSER_LOFI_DECIDER_H_
 
 #include "base/macros.h"
-#include "net/url_request/url_request.h"
 
 namespace net {
 class HttpRequestHeaders;
+class ProxyServer;
+class URLRequest;
 }
 
 namespace data_reduction_proxy {
+
+class DataReductionProxyConfig;
 
 // Interface to determine if a request should be made for a low fidelity version
 // of the resource.
@@ -29,10 +32,14 @@ class LoFiDecider {
   // |request| is using Lo-Fi mode, adds the "q=low" directive to the |headers|.
   // If the |request| is using Lo-Fi preview mode, and it is a main frame
   // request adds the "q=preview" and it is a main frame request directive to
-  // the |headers|.
+  // the |headers|. Only adds this header if the provided |proxy_server| is a
+  // data reduction proxy as identified by the given |config|.
+
   virtual bool MaybeAddLoFiDirectiveToHeaders(
       const net::URLRequest& request,
-      net::HttpRequestHeaders* headers) const = 0;
+      net::HttpRequestHeaders* headers,
+      const net::ProxyServer& proxy_server,
+      DataReductionProxyConfig* config) const = 0;
 };
 
 }  // namespace data_reduction_proxy
