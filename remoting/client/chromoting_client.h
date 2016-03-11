bde1734c58a6ea4187865eebbdcac75f3a7ddcb3
sergeyu <sergeyu@chromium.org>
2016-03-11 15:17:38 -0800

    Move NegotiatingClientAuthentication creation to ChromotingClient.
    For the new SPAKE2 authenticator we need to pass client_jid
    to the authenticator. This wasn't possible previously because
    NegotiatingClientAuthenticator was created before signaling is
    connected. Moved NegotiatingClientAuthentication creation to
    ChromotingClient.
    
    BUG=589698
    
    Review URL: https://codereview.chromium.org/1778023002
    
    Cr-Commit-Position: refs/heads/master@{#380779}

--- remoting/client/chromoting_client.h
+++ remoting/client/chromoting_client.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/protocol/clipboard_stub.h"
 #include "remoting/protocol/connection_to_host.h"
 #include "remoting/protocol/input_stub.h"
+#include "remoting/protocol/negotiating_client_authenticator.h"
 #include "remoting/protocol/performance_tracker.h"
 #include "remoting/protocol/session_config.h"
 #include "remoting/protocol/video_stub.h"
@@ -64,7 +65,7 @@ class ChromotingClient : public SignalStrategy::Listener,
   // Start the client. Must be called on the main thread. |signal_strategy|
   // must outlive the client.
   void Start(SignalStrategy* signal_strategy,
-             scoped_ptr<protocol::Authenticator> authenticator,
+             const protocol::ClientAuthenticationConfig& client_auth_config,
              scoped_refptr<protocol::TransportContext> transport_context,
              const std::string& host_jid,
              const std::string& capabilities);
@@ -122,7 +123,7 @@ class ChromotingClient : public SignalStrategy::Listener,
   SignalStrategy* signal_strategy_ = nullptr;
 
   std::string host_jid_;
-  scoped_ptr<protocol::Authenticator> authenticator_;
+  protocol::ClientAuthenticationConfig client_auth_config_;
   scoped_refptr<protocol::TransportContext> transport_context_;
 
   scoped_ptr<protocol::SessionManager> session_manager_;
