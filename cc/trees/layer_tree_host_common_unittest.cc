14486679d7637a316c2439b7463a9621670d44e6
weiliangc <weiliangc@chromium.org>
2016-03-11 20:53:56 -0800

    cc: Clips layer when render surface it owns has unclipped descendants
    For layers owning a render surface that has unclipped descendants, we
    used to not respect clip even when layer has mask to bounds.
    
    R=ajuma
    BUG=
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1790563004
    
    Cr-Commit-Position: refs/heads/master@{#380858}

--- cc/trees/layer_tree_host_common_unittest.cc
+++ cc/trees/layer_tree_host_common_unittest.cc
@@ -4025,7 +4025,7 @@ TEST_F(LayerTreeHostCommonTest, RootClipPropagationToClippedSurface) {
 
   ExecuteCalculateDrawProperties(root);
   EXPECT_EQ(gfx::Rect(50, 50), unclipped_surface->visible_layer_rect());
-  EXPECT_EQ(gfx::Rect(100, 100), unclipped_desc_surface->visible_layer_rect());
+  EXPECT_EQ(gfx::Rect(50, 50), unclipped_desc_surface->visible_layer_rect());
   EXPECT_EQ(gfx::Rect(10, 10), clipped_surface->visible_layer_rect());
 }
 
@@ -9423,8 +9423,8 @@ TEST_F(LayerTreeHostCommonTest,
                                false);
 
   ExecuteCalculateDrawProperties(root);
-  EXPECT_EQ(gfx::Rect(50, 50), render_surface->visible_layer_rect());
-  EXPECT_EQ(gfx::Rect(50, 50), test_layer1->visible_layer_rect());
+  EXPECT_EQ(gfx::Rect(30, 30), render_surface->visible_layer_rect());
+  EXPECT_EQ(gfx::Rect(30, 30), test_layer1->visible_layer_rect());
   EXPECT_EQ(gfx::Rect(30, 30), clip_child->visible_layer_rect());
   EXPECT_EQ(gfx::Rect(30, 30), test_layer2->visible_layer_rect());
 }
@@ -9519,7 +9519,7 @@ TEST_F(LayerTreeHostCommonTest, RenderSurfaceContentRectWithMultipleSurfaces) {
   ExecuteCalculateDrawProperties(root);
   EXPECT_EQ(gfx::Rect(50, 50),
             unclipped_surface->render_surface()->content_rect());
-  EXPECT_EQ(gfx::Rect(100, 100),
+  EXPECT_EQ(gfx::Rect(50, 50),
             unclipped_desc_surface->render_surface()->content_rect());
   EXPECT_EQ(gfx::Rect(50, 50),
             unclipped_desc_surface2->render_surface()->content_rect());
