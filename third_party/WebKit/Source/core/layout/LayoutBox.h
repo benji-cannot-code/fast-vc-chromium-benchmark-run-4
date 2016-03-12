42df061b900ff087eda7c2a8a2e61920a9997964
mstensho <mstensho@opera.com>
2016-03-11 16:11:38 -0800

    Only honor break-* values when appropriate.
    Only honor column-specific break property values if inside a multicol
    container, and only honor pagination-specific break property values if inside
    some kind of pagination container (printing or paged overflow).
    
    For breaking inside paged overflow containers, honor page values, not column
    values. The paged overflow implementation sits on top of the multicol
    implementation, which means that it's flowthread-based. But that's just an
    implementation detail. Paged overflow containers don't establish columns -- it
    establishes pages. Had to update one test, since it relied on
    -webkit-column-break-* working inside a paged overflow container, which no
    longer is the case. Some unit tests needed an update too.
    
    R=leviw@chromium.org
    BUG=223068
    
    Review URL: https://codereview.chromium.org/1762983002
    
    Cr-Commit-Position: refs/heads/master@{#380797}

--- third_party/WebKit/Source/core/layout/LayoutBox.h
+++ third_party/WebKit/Source/core/layout/LayoutBox.h
@@ -621,6 +621,20 @@ public:
             m_rareData->m_paginationStrut = LayoutUnit();
     }
 
+    // Is the specified break-before or break-after value supported on this object? It needs to be
+    // in-flow all the way up to a fragmentation context that supports the specified value.
+    bool isBreakBetweenControllable(EBreak) const;
+
+    // Is the specified break-inside value supported on this object? It needs to be contained by a
+    // fragmentation context that supports the specified value.
+    bool isBreakInsideControllable(EBreak) const;
+
+    EBreak breakAfter() const;
+    EBreak breakBefore() const;
+    EBreak breakInside() const;
+
+    static bool isForcedFragmentainerBreakValue(EBreak);
+
     bool hasForcedBreakBefore() const;
     bool hasForcedBreakAfter() const;
 
@@ -1135,6 +1149,16 @@ inline void LayoutBox::setInlineBoxWrapper(InlineBox* boxWrapper)
     }
 
     m_inlineBoxWrapper = boxWrapper;
+}
+
+inline bool LayoutBox::isForcedFragmentainerBreakValue(EBreak breakValue)
+{
+    return breakValue == BreakColumn
+        || breakValue == BreakLeft
+        || breakValue == BreakPage
+        || breakValue == BreakRecto
+        || breakValue == BreakRight
+        || breakValue == BreakVerso;
 }
 
 } // namespace blink
