23e788321370b557e1c87c8b544c92b8562d619f
sergeyu <sergeyu@chromium.org>
2016-03-11 17:58:58 -0800

    Move NegotiatingClientAuthentication creation to ChromotingClient.
    For the new SPAKE2 authenticator we need to pass client_jid
    to the authenticator. This wasn't possible previously because
    NegotiatingClientAuthenticator was created before signaling is
    connected. Moved NegotiatingClientAuthentication creation to
    ChromotingClient.
    
    BUG=589698
    
    Committed: https://crrev.com/279bf7c76a3e0bfbbd28748d0d02fcead88f4436
    Cr-Commit-Position: refs/heads/master@{#380779}
    
    Review URL: https://codereview.chromium.org/1778023002
    
    Cr-Commit-Position: refs/heads/master@{#380831}

--- remoting/protocol/negotiating_authenticator_base.h
+++ remoting/protocol/negotiating_authenticator_base.h
@@ -14,7 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "remoting/protocol/authenticator.h"
-#include "third_party/webrtc/libjingle/xmllite/xmlelement.h"
+
+namespace buzz {
+struct StaticQName;
+}  // namespace buzz
 
 namespace remoting {
 namespace protocol {
