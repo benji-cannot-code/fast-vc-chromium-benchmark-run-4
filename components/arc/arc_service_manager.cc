30fe728c18eea8969be245edf43d4c22f8f4935e
domlaskowski <domlaskowski@chromium.org>
2016-03-11 20:41:33 -0800

    ARC: Add crash collector bridge
    The crash collector bridge is responsible for relaying dumps for non-native ARC
    crashes to the crash_reporter in Chrome OS. This CL adds the Mojo host for
    accepting dumps collected in ARC and sent across the bridge. The dumps are
    discarded for now, but will be forwarded to crash_reporter in a subsequent CL.
    
    BUG=581551
    TEST=Deploy both sides. Trigger non-native ARC crash using dev tools.
         Verify that ~chronos/log/chrome contains the dump type and size.
    
    Review URL: https://codereview.chromium.org/1743423002
    
    Cr-Commit-Position: refs/heads/master@{#380842}

--- components/arc/arc_service_manager.cc
+++ components/arc/arc_service_manager.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/arc_bridge_bootstrap.h"
 #include "components/arc/arc_bridge_service_impl.h"
 #include "components/arc/clipboard/arc_clipboard_bridge.h"
+#include "components/arc/crash_collector/arc_crash_collector_bridge.h"
 #include "components/arc/ime/arc_ime_bridge.h"
 #include "components/arc/input/arc_input_bridge.h"
 #include "components/arc/net/arc_net_host_impl.h"
@@ -32,6 +33,8 @@ ArcServiceManager::ArcServiceManager()
   g_arc_service_manager = this;
 
   AddService(make_scoped_ptr(new ArcClipboardBridge(arc_bridge_service())));
+  AddService(
+      make_scoped_ptr(new ArcCrashCollectorBridge(arc_bridge_service())));
   AddService(make_scoped_ptr(new ArcImeBridge(arc_bridge_service())));
   AddService(make_scoped_ptr(new ArcInputBridge(arc_bridge_service())));
   AddService(make_scoped_ptr(new ArcNetHostImpl(arc_bridge_service())));
