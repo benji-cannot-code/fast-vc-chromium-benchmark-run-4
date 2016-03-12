5365f91f2f452f45fb31438ffe0c55809c27cdf0
khmel <khmel@chromium.org>
2016-03-11 16:12:37 -0800

    arc: Disable any UI for supervised profiles.
    We cannot fetch authorization code for supervised profiles.
    Therefore we cannot start Arc and should disable any Arc
    related UI.
    
    BUG=593542
    TEST=Sign-in with supervised profile. Arc is not started,
         there is no Arc settins in Chrome::Settings and there
         is no OptIn UI and Arc welcome notification. Signed in
         with normal profile and everything works as before.
    
    Review URL: https://codereview.chromium.org/1781933002
    
    Cr-Commit-Position: refs/heads/master@{#380798}

--- chrome/browser/ui/webui/options/browser_options_handler.cc
+++ chrome/browser/ui/webui/options/browser_options_handler.cc
@@ -1064,7 +1064,8 @@ void BrowserOptionsHandler::InitializePage() {
 
   if (!arc::ArcBridgeService::GetEnabled(
           base::CommandLine::ForCurrentProcess()) ||
-      arc::ArcAuthService::IsOptInVerificationDisabled()) {
+      arc::ArcAuthService::IsOptInVerificationDisabled() ||
+      profile->IsLegacySupervised()) {
     web_ui()->CallJavascriptFunction("BrowserOptions.hideAndroidAppsSection");
   }
 #endif
