953cb30ecda3f3c90930c54ff0c179c463d79f5a
xunjieli <xunjieli@chromium.org>
2016-03-11 16:17:25 -0800

    Only MarkTrailersConsumed when actually sending trailers notification
    MarkTrailersConsumed is currently done before the
    notification is posted asynchronously. However, there might
    be a pending read  before that notification is executed but
    after  MarkTrailersConsumed, which leads to delegate
    mistakenly to assume that the trailers are consumed.
    This CL changes so that we MarkTrailersConsumed when we
    actually will immediately notify delegate.
    
    This CL also patches some internal trailers changes in
    quic_spdy_stream.cc and quic_spdy_client_stream.cc.
    
    BUG=586207
    
    Review URL: https://codereview.chromium.org/1776423004
    
    Cr-Commit-Position: refs/heads/master@{#380799}

--- net/quic/quic_spdy_stream.h
+++ net/quic/quic_spdy_stream.h
@@ -142,6 +142,11 @@ class NET_EXPORT_PRIVATE QuicSpdyStream : public ReliableQuicStream {
     return decompressed_trailers_;
   }
 
+  // Returns whatever trailers have been received for this stream.
+  const SpdyHeaderBlock& response_trailers() const {
+    return response_trailers_;
+  }
+
   virtual SpdyPriority priority() const;
 
   // Sets priority_ to priority.  This should only be called before bytes are
@@ -183,6 +188,8 @@ class NET_EXPORT_PRIVATE QuicSpdyStream : public ReliableQuicStream {
   // Contains a copy of the decompressed trailers until they are consumed
   // via ProcessData or Readv.
   std::string decompressed_trailers_;
+  // The parsed trailers received from the peer.
+  SpdyHeaderBlock response_trailers_;
 
   DISALLOW_COPY_AND_ASSIGN(QuicSpdyStream);
 };
