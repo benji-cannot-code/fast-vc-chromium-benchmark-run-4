19f6c35effcc07e4e3c08ba6e463fa77de1f7cfa
wangxianzhu <wangxianzhu@chromium.org>
2016-03-11 20:40:54 -0800

    Skip PaintLayer empty paint phases if it previously painted nothing
    This avoids later painting of empty phases if the layer is marked to need
    a paint phase but the phase painted nothing in the following cases:
    - the object marking the layer needing the paint phase is not visible
      (e.g. because it is clipped)
    - the layer was marked needing the paint phase by some object that
      has been deleted or moved to another layer.
    
    Instead of directly clearing the needsPaintPhase flags, this CL adds
    a new set of flags to indicate if the previous paint of the phases painted
    nothing. This is because invalidations of the needsPaintPhase flags and the
    new flags need different conditions. The new flags are cleared when scroll
    offset, interest rect or clipping changes.
    
    Cluster telemetry runs show that this will improve non-cached painting
    by 1.7% and fully-cached painting (with subsequence-caching disabled) by
    45%.
    
    BUG=574938
    
    Review URL: https://codereview.chromium.org/1739893006
    
    Cr-Commit-Position: refs/heads/master@{#380841}

--- third_party/WebKit/Source/core/paint/PaintLayer.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayer.cpp
@@ -99,7 +99,7 @@ static CompositingQueryMode gCompositingQueryMode =
     CompositingQueriesAreOnlyAllowedInCertainDocumentLifecyclePhases;
 
 struct SameSizeAsPaintLayer : DisplayItemClient {
-    int bitFields;
+    uint32_t bitFields1, bitFields2;
     void* pointers[9];
     LayoutUnit layoutUnits[4];
     IntSize size;
@@ -161,8 +161,11 @@ PaintLayer::PaintLayer(LayoutBoxModelObject* layoutObject, PaintLayerType type)
     , m_needsRepaint(false)
     , m_previousPaintResult(PaintLayerPainter::FullyPainted)
     , m_needsPaintPhaseDescendantOutlines(false)
+    , m_previousPaintPhaseDescendantOutlinesWasEmpty(false)
     , m_needsPaintPhaseFloat(false)
+    , m_previousPaintPhaseFloatWasEmpty(false)
     , m_needsPaintPhaseDescendantBlockBackgrounds(false)
+    , m_previousPaintPhaseDescendantBlockBackgroundsWasEmpty(false)
     , m_hasDescendantWithClipPath(false)
     , m_hasNonIsolatedDescendantWithBlendMode(false)
     , m_hasAncestorWithClipPath(false)
