582b0d7ed50e4b9665038a38f1ed81e2811522ce
rob.buis <rob.buis@samsung.com>
2016-03-13 09:18:22 -0700

    Move the grid-gap shorthand into CSSPropertyParser
    Move the grid-gap shorthand from LegacyCSSPropertyParser
    into CSSPropertyParser.
    
    BUG=499780
    
    Review URL: https://codereview.chromium.org/1777483008
    
    Cr-Commit-Position: refs/heads/master@{#380897}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
@@ -127,7 +127,6 @@ private:
     bool parseGridTemplateShorthand(bool important);
     bool parseGridShorthand(bool important);
     bool parseGridAreaShorthand(bool important);
-    bool parseGridGapShorthand(bool important);
     bool parseSingleGridAreaLonghand(RefPtrWillBeRawPtr<CSSValue>&);
     PassRefPtrWillBeRawPtr<CSSValue> parseGridTrackList();
     bool parseGridTrackRepeatFunction(CSSValueList&, bool& isAutoRepeat);
