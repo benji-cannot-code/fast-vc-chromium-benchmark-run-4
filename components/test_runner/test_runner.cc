c8468457ace919bd55aa7a50f8dcab7ea94b99f2
lukasza <lukasza@chromium.org>
2016-03-14 09:44:15 -0700

    Remove TestRunner::disable_notify_done_ (field is never set).
    BUG=587175
    
    Review URL: https://codereview.chromium.org/1776483002
    
    Cr-Commit-Position: refs/heads/master@{#380990}

--- components/test_runner/test_runner.cc
+++ components/test_runner/test_runner.cc
@@ -336,7 +336,6 @@ class TestRunnerBindings : public gin::Wrappable<TestRunnerBindings> {
 
   std::string PlatformName();
   std::string TooltipText();
-  bool DisableNotifyDone();
   int WebHistoryItemCount();
   bool InterceptPostMessage();
   void SetInterceptPostMessage(bool value);
@@ -628,7 +627,6 @@ gin::ObjectTemplateBuilder TestRunnerBindings::GetObjectTemplateBuilder(
       // Properties.
       .SetProperty("platformName", &TestRunnerBindings::PlatformName)
       .SetProperty("tooltipText", &TestRunnerBindings::TooltipText)
-      .SetProperty("disableNotifyDone", &TestRunnerBindings::DisableNotifyDone)
       // webHistoryItemCount is used by tests in LayoutTests\http\tests\history
       .SetProperty("webHistoryItemCount",
                    &TestRunnerBindings::WebHistoryItemCount)
@@ -1569,12 +1567,6 @@ std::string TestRunnerBindings::TooltipText() {
   return std::string();
 }
 
-bool TestRunnerBindings::DisableNotifyDone() {
-  if (runner_)
-    return runner_->disable_notify_done_;
-  return false;
-}
-
 int TestRunnerBindings::WebHistoryItemCount() {
   if (runner_)
     return runner_->web_history_item_count_;
@@ -1665,7 +1657,6 @@ TestRunner::TestRunner(TestInterfaces* interfaces)
     : test_is_running_(false),
       close_remaining_windows_(false),
       work_queue_(this),
-      disable_notify_done_(false),
       web_history_item_count_(0),
       intercept_post_message_(false),
       test_interfaces_(interfaces),
@@ -1766,7 +1757,6 @@ void TestRunner::Reset() {
 
   platform_name_ = "chromium";
   tooltip_text_ = std::string();
-  disable_notify_done_ = false;
   web_history_item_count_ = 0;
   intercept_post_message_ = false;
 
@@ -2058,9 +2048,6 @@ class WorkItemBackForward : public TestRunner::WorkItem {
 };
 
 void TestRunner::NotifyDone() {
-  if (disable_notify_done_)
-    return;
-
   // Test didn't timeout. Kill the timeout timer.
   task_list_.RevokeAll();
 
