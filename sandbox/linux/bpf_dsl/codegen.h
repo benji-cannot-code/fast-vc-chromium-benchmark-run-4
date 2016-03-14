999c0c03f23641bd4f4474b2067ddd84eef0c7fb
tzik <tzik@chromium.org>
2016-03-13 23:07:31 -0700

    Replace base::Tuple in //sandbox with std::tuple
    BUG=554987
    
    Review URL: https://codereview.chromium.org/1769053002
    
    Cr-Commit-Position: refs/heads/master@{#380934}

--- sandbox/linux/bpf_dsl/codegen.h
+++ sandbox/linux/bpf_dsl/codegen.h
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <map>
+#include <tuple>
 #include <vector>
 
 #include "base/macros.h"
-#include "base/tuple.h"
 #include "sandbox/sandbox_export.h"
 
 struct sock_filter;
@@ -81,10 +81,7 @@ class SANDBOX_EXPORT CodeGen {
   Program Compile(Node head);
 
  private:
-  using MemoKey = base::Tuple<uint16_t, uint32_t, Node, Node>;
-  struct MemoKeyLess {
-    bool operator()(const MemoKey& lhs, const MemoKey& rhs) const;
-  };
+  using MemoKey = std::tuple<uint16_t, uint32_t, Node, Node>;
 
   // AppendInstruction adds a new instruction, ensuring that |jt| and
   // |jf| are within range as necessary for |code|.
@@ -113,7 +110,7 @@ class SANDBOX_EXPORT CodeGen {
   // if it's an unconditional jump to a node semantically-equivalent to N.
   std::vector<Node> equivalent_;
 
-  std::map<MemoKey, Node, MemoKeyLess> memos_;
+  std::map<MemoKey, Node> memos_;
 
   DISALLOW_COPY_AND_ASSIGN(CodeGen);
 };
