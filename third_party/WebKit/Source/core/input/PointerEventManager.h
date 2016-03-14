8797fce25e1235c6fb55118a4653c14c9784f18e
nzolghadr <nzolghadr@chromium.org>
2016-03-14 13:37:33 -0700

    Firing got/lost pointer capture events
    This CL is taking care of firing gotpointercapture and
    lostpointercapture events based on the status of the
    pointer capture and whether the element is still in
    the document or not.
    
    BUG=592280
    
    Review URL: https://codereview.chromium.org/1774913004
    
    Cr-Commit-Position: refs/heads/master@{#381061}

--- third_party/WebKit/Source/core/input/PointerEventManager.h
+++ third_party/WebKit/Source/core/input/PointerEventManager.h
@@ -71,7 +71,9 @@ private:
         }
         RefPtrWillBeMember<EventTarget> target;
         bool hasRecievedOverEvent;
-        EventTargetAttributes() {}
+        EventTargetAttributes()
+        : target(nullptr)
+        , hasRecievedOverEvent(false) {}
         EventTargetAttributes(PassRefPtrWillBeRawPtr<EventTarget> target,
             bool hasRecievedOverEvent)
         : target(target)
