70a43bc229dde05bbfb4ab0916f27e35633a0093
davidben <davidben@chromium.org>
2016-03-14 13:59:49 -0700

    Switch WebCrypto to new ASN.1 APIs.
    This makes the PKCS8 and SPKI importers parse strict DER and also avoids
    calling the clumsy i2d/d2i functions. Fix up some includes while I'm here.
    
    BUG=499653
    
    Review URL: https://codereview.chromium.org/1768833002
    
    Cr-Commit-Position: refs/heads/master@{#381070}

--- components/webcrypto/algorithms/ec.cc
+++ components/webcrypto/algorithms/ec.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <openssl/ec.h>
 #include <openssl/ec_key.h>
 #include <openssl/evp.h>
-#include <openssl/pkcs12.h>
 #include <stddef.h>
 #include <utility>
 
