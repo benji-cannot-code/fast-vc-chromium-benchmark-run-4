f6d5ac261723dcd91277971a98f771ff3c5849b0
huangs <huangs@chromium.org>
2016-03-14 09:35:39 -0700

    [Courgette] Clean up Disassembler; fix ELF Memory leaks.
    Cleaning up code surrounding Disassembler:
    - Extract AddressTranslator interface to be used across subclasses.
    - Use FileOffset = size_t by context.
    - Detailed comments & TODOs in DisassemblerElf32ARM.
    - Fix DisassemblerElf32ARM memory leaks.
    - Lots of superficial stylistic changes.
    
    Except for AddressTranslator routines and unit tests, shying away
    from control flow and logic changes.
    
    BUG=579206
    
    Committed: https://crrev.com/58b822d441f5c982e879e536fa3c1cbac8fd339a
    Cr-Commit-Position: refs/heads/master@{#380881}
    
    Review URL: https://codereview.chromium.org/1676683002
    
    Cr-Commit-Position: refs/heads/master@{#380987}

--- courgette/disassembler_win32_x86_unittest.cc
+++ courgette/disassembler_win32_x86_unittest.cc
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <string>
+#include <vector>
+
 #include "base/memory/scoped_ptr.h"
 #include "base/stl_util.h"
 #include "courgette/base_test_unittest.h"
@@ -38,16 +41,16 @@ void DisassemblerWin32X86Test::TestExe() const {
       disassembler->RVAToSection(0x00401234 - 0x00400000)),
       std::string(".text"));
 
-  EXPECT_EQ(0, disassembler->RVAToFileOffset(0));
-  EXPECT_EQ(1024, disassembler->RVAToFileOffset(4096));
-  EXPECT_EQ(46928, disassembler->RVAToFileOffset(50000));
+  EXPECT_EQ(0U, disassembler->RVAToFileOffset(0));
+  EXPECT_EQ(1024U, disassembler->RVAToFileOffset(4096));
+  EXPECT_EQ(46928U, disassembler->RVAToFileOffset(50000));
 
   std::vector<courgette::RVA> relocs;
   bool can_parse_relocs = disassembler->ParseRelocs(&relocs);
   EXPECT_TRUE(can_parse_relocs);
   EXPECT_TRUE(base::STLIsSorted(relocs));
 
-  const uint8_t* offset_p = disassembler->OffsetToPointer(0);
+  const uint8_t* offset_p = disassembler->FileOffsetToPointer(0);
   EXPECT_EQ(reinterpret_cast<const void*>(file1.c_str()),
             reinterpret_cast<const void*>(offset_p));
   EXPECT_EQ('M', offset_p[0]);
