a90c59a7fc2c6163b70017eec1314c60e5ce43fa
michaelpg <michaelpg@chromium.org>
2016-03-14 12:21:42 -0700

    Use polymer_config.html instead of polymer.html
    Polymer must be configured before loading. Several files imported polymer.html and not polymer_config.html. When these files are opened in a browser test, either:
    * polymer_config.html is never imported, so the test is run under shady DOM, or
    * the file indirectly imports polymer_config.html via its dependencies, causing the configuration to happen after Polymer is already loaded (an error)
    
    Using polymer_config.html in place of polymer.html will eliminate this problem, meaning one less error to debug when writing tests. Since every custom Polymer element should import polymer_config.html, we can just place the actual Polymer import in there.
    
    This CL applies this to MD Downloads, MD Settings, and cr_elements.
    
    Review URL: https://codereview.chromium.org/1767913002
    
    Cr-Commit-Position: refs/heads/master@{#381036}

--- ui/webui/resources/js/polymer_config.js
+++ ui/webui/resources/js/polymer_config.js
@@ -3,7 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-if (typeof Polymer == 'undefined')
+if (typeof Polymer == 'undefined') {
   Polymer = {dom: 'shadow'};
-else
-  console.error('Polymer is already defined.');
+} else {
+  // TODO(michaelpg): Change back to error once polymer_config.html is removed.
+  console.warn('Polymer is already defined.');
+}
