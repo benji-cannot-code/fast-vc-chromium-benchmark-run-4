7ce04c48b21e8b6760d5ab54ac552efaade6ccd7
dalecurtis <dalecurtis@chromium.org>
2016-03-15 14:32:06 -0700

    Remove Exynos from MediaCodec blacklist. Limit MediaTek blacklist.
    MediaCodec has a newer path for limiting the VP8 blacklist more
    precisely that should be used instead of a blanket Exynos blacklist.
    
    MediaTek nowadays also has a vp9 decoder, so restrict blacklist to
    only vp8 MediaTek devices. Per glaznev@ the MediaTek vp8 decoder is
    slower than software, so it kind of makes sense to include it on
    the "known unaccelerated" list.
    
    BUG=446974, 594814
    TEST=hardware vp9 on galaxy s6 works with <video>.
    
    Review URL: https://codereview.chromium.org/1796393002
    
    Cr-Commit-Position: refs/heads/master@{#381322}

--- content/common/gpu/media/android_video_encode_accelerator.cc
+++ content/common/gpu/media/android_video_encode_accelerator.cc
@@ -115,6 +115,11 @@ AndroidVideoEncodeAccelerator::GetSupportedProfiles() {
   };
 
   for (const auto& supported_codec : kSupportedCodecs) {
+    if (supported_codec.codec == media::kCodecVP8 &&
+        !media::MediaCodecUtil::IsVp8EncoderAvailable()) {
+      continue;
+    }
+
     if (VideoCodecBridge::IsKnownUnaccelerated(supported_codec.codec,
                                                media::MEDIA_CODEC_ENCODER)) {
       continue;
