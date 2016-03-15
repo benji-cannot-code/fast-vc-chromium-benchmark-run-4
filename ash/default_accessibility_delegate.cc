6df0ad9c3901b39a4641bc02cced713ddee6dd59
dmazzoni <dmazzoni@chromium.org>
2016-03-15 16:55:16 -0700

    Add 5 experimental accessibility features on Chrome OS.
    This just adds the boilerplate for five new accessibility features and hides
    them behind a flag. All five have been prototyped and are ready for
    implementation.
    
    The first three are related, they just provide visual highlighting to
    help low-vision users keep track of things on the screen:
    * Caret highlight
    * Cursor highlight
    * Focus highlight
    
    Hold down a key and click or drag to speak any visible text on screen
    * Select to speak
    
    Control the whole computer with a single switch (for motor impaired users)
    * Switch access
    
    For now, there's no need to put these in the tray. They can graduate to the
    tray when they launch.
    
    BUG=314889,593887,593885
    
    Review URL: https://codereview.chromium.org/1785833002
    
    Cr-Commit-Position: refs/heads/master@{#381353}

--- ash/default_accessibility_delegate.cc
+++ ash/default_accessibility_delegate.cc
@@ -9,16 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-DefaultAccessibilityDelegate::DefaultAccessibilityDelegate()
-    : spoken_feedback_enabled_(false),
-      high_contrast_enabled_(false),
-      screen_magnifier_enabled_(false),
-      screen_magnifier_type_(ui::kDefaultMagnifierType),
-      large_cursor_enabled_(false),
-      autoclick_enabled_(false),
-      virtual_keyboard_enabled_(false),
-      accessibility_alert_(ui::A11Y_ALERT_NONE) {
-}
+DefaultAccessibilityDelegate::DefaultAccessibilityDelegate() {}
 
 DefaultAccessibilityDelegate::~DefaultAccessibilityDelegate() {}
 
@@ -72,6 +63,46 @@ void DefaultAccessibilityDelegate::SetVirtualKeyboardEnabled(bool enabled) {
 
 bool DefaultAccessibilityDelegate::IsVirtualKeyboardEnabled() const {
   return virtual_keyboard_enabled_;
+}
+
+void DefaultAccessibilityDelegate::SetCaretHighlightEnabled(bool enabled) {
+  caret_highlight_enabled_ = enabled;
+}
+
+bool DefaultAccessibilityDelegate::IsCaretHighlightEnabled() const {
+  return caret_highlight_enabled_;
+}
+
+void DefaultAccessibilityDelegate::SetCursorHighlightEnabled(bool enabled) {
+  cursor_highlight_enabled_ = enabled;
+}
+
+bool DefaultAccessibilityDelegate::IsCursorHighlightEnabled() const {
+  return cursor_highlight_enabled_;
+}
+
+void DefaultAccessibilityDelegate::SetFocusHighlightEnabled(bool enabled) {
+  focus_highligh_enabled_ = enabled;
+}
+
+bool DefaultAccessibilityDelegate::IsFocusHighlightEnabled() const {
+  return focus_highligh_enabled_;
+}
+
+void DefaultAccessibilityDelegate::SetSelectToSpeakEnabled(bool enabled) {
+  select_to_speak_enabled_ = enabled;
+}
+
+bool DefaultAccessibilityDelegate::IsSelectToSpeakEnabled() const {
+  return select_to_speak_enabled_;
+}
+
+void DefaultAccessibilityDelegate::SetSwitchAccessEnabled(bool enabled) {
+  switch_access_enabled_ = enabled;
+}
+
+bool DefaultAccessibilityDelegate::IsSwitchAccessEnabled() const {
+  return switch_access_enabled_;
 }
 
 bool DefaultAccessibilityDelegate::ShouldShowAccessibilityMenu() const {
--- chrome/browser/resources/options/browser_options.js
+++ chrome/browser/resources/options/browser_options.js
@@ -729,6 +729,8 @@ cr.define('options', function() {
         Preferences.getInstance().addEventListener(
             $('accessibility-autoclick-check').getAttribute('pref'),
             updateDelayDropdown);
+        $('experimental-accessibility-features').hidden =
+            !loadTimeData.getBoolean('enableExperimentalAccessibilityFeatures');
       }
 
       // Display management section (CrOS only).
