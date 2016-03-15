b8128951e9badeb34cfdae6cb01371896a06a7d5
mikecase <mikecase@chromium.org>
2016-03-15 13:58:13 -0700

    [WebView] Add tests for navigator.requestMIDIAccess to LayoutTests.
    BUG=515926
    
    Review URL: https://codereview.chromium.org/1787833002
    
    Cr-Commit-Position: refs/heads/master@{#381308}

--- android_webview/tools/WebViewShell/src/org/chromium/webview_shell/WebViewLayoutTestActivity.java
+++ android_webview/tools/WebViewShell/src/org/chromium/webview_shell/WebViewLayoutTestActivity.java
@@ -36,9 +36,6 @@ public class WebViewLayoutTestActivity extends Activity {
     private boolean mFinished = false;
     private boolean mGrantPermission = false;
 
-    private static final String[] AUTOMATICALLY_GRANT =
-            { PermissionRequest.RESOURCE_VIDEO_CAPTURE, PermissionRequest.RESOURCE_AUDIO_CAPTURE };
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,8 +79,8 @@ public class WebViewLayoutTestActivity extends Activity {
                         + TextUtils.join(",", request.getResources()) + "\n");
                 if (mGrantPermission) {
                     mConsoleLog.append("request granted: "
-                            + TextUtils.join(",", AUTOMATICALLY_GRANT) + "\n");
-                    request.grant(AUTOMATICALLY_GRANT);
+                            + TextUtils.join(",", request.getResources()) + "\n");
+                    request.grant(request.getResources());
                 } else {
                     mConsoleLog.append("request denied" + "\n");
                     request.deny();
