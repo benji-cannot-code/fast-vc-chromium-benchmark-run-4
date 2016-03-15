c7cb3cf122dd41c71546330e2a8f87c8625a0800
clamy <clamy@chromium.org>
2016-03-15 09:43:30 -0700

    PlzNavigate: fix CrossSiteRedirectorBrowserTest.VerifyCrossSiteRedirectURL
    This CL fixes CrossSiteRedirectorBrowserTest.VerifyCrossSiteRedirectURL when
    PlzNavigate is enabled by having the WebContentsObserver in the test listen to
    DidRedirectNavigation instead of DidGetRedirectForResourceRequest.
    
    BUG=475027
    
    Review URL: https://codereview.chromium.org/1804033002
    
    Cr-Commit-Position: refs/heads/master@{#381234}

--- content/test/browser_test_utils_browsertest.cc
+++ content/test/browser_test_utils_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/macros.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/resource_request_details.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
@@ -27,10 +28,8 @@ class NavigationObserver: public WebContentsObserver {
     navigation_url_ = url;
   }
 
-  void DidGetRedirectForResourceRequest(
-      RenderFrameHost* render_frame_host,
-      const ResourceRedirectDetails& details) override {
-    redirect_url_ = details.new_url;
+  void DidRedirectNavigation(NavigationHandle* handle) override {
+    redirect_url_ = handle->GetURL();
   }
 
   const GURL& navigation_url() const {
