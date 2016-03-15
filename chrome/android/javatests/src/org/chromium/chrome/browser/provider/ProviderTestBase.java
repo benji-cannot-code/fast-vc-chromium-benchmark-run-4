59598d83d0235784b1f23a5d20a9f067cf865f0e
newt <newt@chromium.org>
2016-03-14 20:02:09 -0700

    Delete ChromeBrowserProviderClient.
    It's been superseded by BookmarkBridge and FaviconHelper. This also
    deletes the code from ChromeBrowserProvider that existed solely to
    support ChromeBrowserProviderClient.
    
    BUG=583477
    
    Review URL: https://codereview.chromium.org/1804813002
    
    Cr-Commit-Position: refs/heads/master@{#381163}

--- chrome/android/javatests/src/org/chromium/chrome/browser/provider/ProviderTestBase.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/provider/ProviderTestBase.java
@@ -51,7 +51,6 @@ public class ProviderTestBase extends ChromeActivityTestCaseBase<ChromeActivity>
 
         MockContentResolver resolver = new MockContentResolver();
         resolver.addProvider(ChromeBrowserProvider.getApiAuthority(activity), provider);
-        resolver.addProvider(ChromeBrowserProvider.getInternalAuthority(activity), provider);
 
         mContext = new IsolatedContext(resolver, activity);
         assertTrue(getContentResolver() instanceof MockContentResolver);
