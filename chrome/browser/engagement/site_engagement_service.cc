e59ed8b610afed7f7dd1e48c5296759b661b9402
calamity <calamity@chromium.org>
2016-03-14 20:38:12 -0700

    Fix crash in SiteEngagementMetrics::RecordEngagement.
    This CL fixes a crash in RecordEngagement where a value past the end of
    a map was being dereferenced.
    
    There are also some minor cleanup changes.
    
    BUG=594836
    
    Review URL: https://codereview.chromium.org/1803943002
    
    Cr-Commit-Position: refs/heads/master@{#381168}

--- chrome/browser/engagement/site_engagement_service.cc
+++ chrome/browser/engagement/site_engagement_service.cc
@@ -680,7 +680,7 @@ void SiteEngagementService::RecordMetrics() {
 }
 
 double SiteEngagementService::GetMedianEngagement(
-    std::map<GURL, double>& score_map) const {
+    const std::map<GURL, double>& score_map) const {
   if (score_map.size() == 0)
     return 0;
 
@@ -725,7 +725,7 @@ int SiteEngagementService::OriginsWithMaxDailyEngagement() const {
 }
 
 int SiteEngagementService::OriginsWithMaxEngagement(
-    std::map<GURL, double>& score_map) const {
+    const std::map<GURL, double>& score_map) const {
   int total_origins = 0;
 
   for (const auto& value : score_map)
