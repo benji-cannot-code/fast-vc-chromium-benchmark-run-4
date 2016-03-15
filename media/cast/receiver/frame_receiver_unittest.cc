6858981a81777a8c49ce4850c4095b3580d771e2
xjz <xjz@chromium.org>
2016-03-15 12:37:48 -0700

    Using multi-phase IPC messages to send RTCP from RTP receiver. The current interface of the RTCP session from RTP receicer with CastTransportSender is very confusing as one RTCP packet can include some optional RTCP blocks. This CL uses multi-phase IPC messages to solve this issue. The RTCP builder needs to be initialized first, optional RTCP messages are added, and the ending message finalizes the building of the packet and sends the built packet out.
    BUG=569259
    
    Review URL: https://codereview.chromium.org/1758363002
    
    Cr-Commit-Position: refs/heads/master@{#381285}

--- media/cast/receiver/frame_receiver_unittest.cc
+++ media/cast/receiver/frame_receiver_unittest.cc
@@ -153,6 +153,9 @@ class FrameReceiverTest : public ::testing::Test {
 };
 
 TEST_F(FrameReceiverTest, RejectsUnparsablePackets) {
+  EXPECT_CALL(mock_transport_, AddValidRtpReceiver(_, _))
+      .WillRepeatedly(testing::Return());
+
   CreateFrameReceiverOfVideo();
 
   SimpleEventSubscriber event_subscriber;
@@ -170,12 +173,21 @@ TEST_F(FrameReceiverTest, RejectsUnparsablePackets) {
 }
 
 TEST_F(FrameReceiverTest, ReceivesOneFrame) {
+  EXPECT_CALL(mock_transport_, AddValidRtpReceiver(_, _))
+      .WillRepeatedly(testing::Return());
+
   CreateFrameReceiverOfAudio();
 
   SimpleEventSubscriber event_subscriber;
   cast_environment_->logger()->Subscribe(&event_subscriber);
 
-  EXPECT_CALL(mock_transport_, SendRtcpFromRtpReceiver(_, _, _, _, _, _, _))
+  EXPECT_CALL(mock_transport_, InitializeRtpReceiverRtcpBuilder(_, _))
+      .WillRepeatedly(testing::Return());
+  EXPECT_CALL(mock_transport_, AddCastFeedback(_, _))
+      .WillRepeatedly(testing::Return());
+  EXPECT_CALL(mock_transport_, AddRtcpEvents(_))
+      .WillRepeatedly(testing::Return());
+  EXPECT_CALL(mock_transport_, SendRtcpFromRtpReceiver())
       .WillRepeatedly(testing::Return());
 
   FeedLipSyncInfoIntoReceiver();
@@ -211,12 +223,21 @@ TEST_F(FrameReceiverTest, ReceivesOneFrame) {
 }
 
 TEST_F(FrameReceiverTest, ReceivesFramesSkippingWhenAppropriate) {
+  EXPECT_CALL(mock_transport_, AddValidRtpReceiver(_, _))
+      .WillRepeatedly(testing::Return());
+
   CreateFrameReceiverOfAudio();
 
   SimpleEventSubscriber event_subscriber;
   cast_environment_->logger()->Subscribe(&event_subscriber);
 
-  EXPECT_CALL(mock_transport_, SendRtcpFromRtpReceiver(_, _, _, _, _, _, _))
+  EXPECT_CALL(mock_transport_, InitializeRtpReceiverRtcpBuilder(_, _))
+      .WillRepeatedly(testing::Return());
+  EXPECT_CALL(mock_transport_, AddCastFeedback(_, _))
+      .WillRepeatedly(testing::Return());
+  EXPECT_CALL(mock_transport_, AddRtcpEvents(_))
+      .WillRepeatedly(testing::Return());
+  EXPECT_CALL(mock_transport_, SendRtcpFromRtpReceiver())
       .WillRepeatedly(testing::Return());
 
   const base::TimeDelta time_advance_per_frame =
@@ -314,12 +335,21 @@ TEST_F(FrameReceiverTest, ReceivesFramesSkippingWhenAppropriate) {
 }
 
 TEST_F(FrameReceiverTest, ReceivesFramesRefusingToSkipAny) {
+  EXPECT_CALL(mock_transport_, AddValidRtpReceiver(_, _))
+      .WillRepeatedly(testing::Return());
+
   CreateFrameReceiverOfVideo();
 
   SimpleEventSubscriber event_subscriber;
   cast_environment_->logger()->Subscribe(&event_subscriber);
 
-  EXPECT_CALL(mock_transport_, SendRtcpFromRtpReceiver(_, _, _, _, _, _, _))
+  EXPECT_CALL(mock_transport_, InitializeRtpReceiverRtcpBuilder(_, _))
+      .WillRepeatedly(testing::Return());
+  EXPECT_CALL(mock_transport_, AddCastFeedback(_, _))
+      .WillRepeatedly(testing::Return());
+  EXPECT_CALL(mock_transport_, AddRtcpEvents(_))
+      .WillRepeatedly(testing::Return());
+  EXPECT_CALL(mock_transport_, SendRtcpFromRtpReceiver())
       .WillRepeatedly(testing::Return());
 
   const base::TimeDelta time_advance_per_frame =
