c02ac2e40c7d02de00fb26e7c4ebfe846b2c8f33
peconn <peconn@chromium.org>
2016-03-15 11:10:41 -0700

    Make ConfirmImportSyncDataDialog use android.support.v7.app.AlertDialog.
    It previously used android.app.AlertDialog which reverts to a Holo appearance on pre-L devices.
    
    BUG=594846
    
    Review URL: https://codereview.chromium.org/1807493002
    
    Cr-Commit-Position: refs/heads/master@{#381252}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/ui/ConfirmImportSyncDataDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/ui/ConfirmImportSyncDataDialog.java
@@ -5,12 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.sync.ui;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
