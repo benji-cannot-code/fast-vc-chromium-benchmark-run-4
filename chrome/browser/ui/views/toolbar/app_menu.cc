6f23cb82d441ad21edbfe69daa984921896623be
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-03-15 14:56:19 -0700

    [UI Views] Check menu() in ZoomView
    AppMenuView::menu() documents that it may be null during shutdown, but ZoomView
    doesn't check it. Add a check.
    
    BUG=594055
    
    Review URL: https://codereview.chromium.org/1803003003
    
    Cr-Commit-Position: refs/heads/master@{#381329}

--- chrome/browser/ui/views/toolbar/app_menu.cc
+++ chrome/browser/ui/views/toolbar/app_menu.cc
@@ -648,17 +648,19 @@ class AppMenu::ZoomView : public AppMenuView {
     }
   }
 
-  // Overridden from AppMenuObserver.
-  void AppMenuDestroyed() override { AppMenuView::AppMenuDestroyed(); }
-
  private:
+  content::WebContents* GetActiveWebContents() const {
+    return menu() ?
+        menu()->browser_->tab_strip_model()->GetActiveWebContents() :
+        nullptr;
+  }
+
   void OnZoomLevelChanged(const content::HostZoomMap::ZoomLevelChange& change) {
     UpdateZoomControls();
   }
 
   void UpdateZoomControls() {
-    WebContents* selected_tab =
-        menu()->browser_->tab_strip_model()->GetActiveWebContents();
+    WebContents* selected_tab = GetActiveWebContents();
     int zoom = 100;
     if (selected_tab) {
       auto zoom_controller =
@@ -686,8 +688,7 @@ class AppMenu::ZoomView : public AppMenuView {
 
       int max_w = 0;
 
-      WebContents* selected_tab =
-          menu()->browser_->tab_strip_model()->GetActiveWebContents();
+      WebContents* selected_tab = GetActiveWebContents();
       if (selected_tab) {
         auto zoom_controller =
             ui_zoom::ZoomController::FromWebContents(selected_tab);
