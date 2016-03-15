d4adfbfc7c3edf6734fcbc8d0d21ea69b6c73e94
yutak <yutak@chromium.org>
2016-03-15 02:03:46 -0700

    WTF: Implement move semantics for HashTable-based containers.
    This patch simply implements move constructors and move assignments for
    WTF's HashTable-based containers (HashSet, HashMap, ListHashSet, and
    LinkedHashSet). They basically just call swap().
    
    HashSet and HashMap makes use of implicit constructors and assignment
    operators, and compiler-generated versions of moving are just fine.
    
    A simple test is added for each container, in order to test we don't
    copy the content when moving is possible.
    
    BUG=567139,582349
    
    Review URL: https://codereview.chromium.org/1766303002
    
    Cr-Commit-Position: refs/heads/master@{#381195}

--- third_party/WebKit/Source/wtf/LinkedHashSet.h
+++ third_party/WebKit/Source/wtf/LinkedHashSet.h
@@ -199,7 +199,9 @@ public:
 
     LinkedHashSet();
     LinkedHashSet(const LinkedHashSet&);
+    LinkedHashSet(LinkedHashSet&&);
     LinkedHashSet& operator=(const LinkedHashSet&);
+    LinkedHashSet& operator=(LinkedHashSet&&);
 
     // Needs finalization. The anchor needs to unlink itself from the chain.
     ~LinkedHashSet();
@@ -515,10 +517,24 @@ inline LinkedHashSet<T, U, V, W>::LinkedHashSet(const LinkedHashSet& other)
 }
 
 template<typename T, typename U, typename V, typename W>
+inline LinkedHashSet<T, U, V, W>::LinkedHashSet(LinkedHashSet&& other)
+    : m_anchor()
+{
+    swap(other);
+}
+
+template<typename T, typename U, typename V, typename W>
 inline LinkedHashSet<T, U, V, W>& LinkedHashSet<T, U, V, W>::operator=(const LinkedHashSet& other)
 {
     LinkedHashSet tmp(other);
     swap(tmp);
+    return *this;
+}
+
+template<typename T, typename U, typename V, typename W>
+inline LinkedHashSet<T, U, V, W>& LinkedHashSet<T, U, V, W>::operator=(LinkedHashSet&& other)
+{
+    swap(other);
     return *this;
 }
 
