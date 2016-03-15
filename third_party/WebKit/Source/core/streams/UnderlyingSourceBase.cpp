23ce93163ee332edbe176fbc346d65b98c33cd89
guidou <guidou@chromium.org>
2016-03-15 04:20:17 -0700

    Add convenience method to return promises resolved with undefined.
    BUG=
    
    Review URL: https://codereview.chromium.org/1803553002
    
    Cr-Commit-Position: refs/heads/master@{#381207}

--- third_party/WebKit/Source/core/streams/UnderlyingSourceBase.cpp
+++ third_party/WebKit/Source/core/streams/UnderlyingSourceBase.cpp
@@ -28,12 +28,12 @@ ScriptPromise UnderlyingSourceBase::startWrapper(ScriptState* scriptState, Scrip
 
 ScriptPromise UnderlyingSourceBase::start(ScriptState* scriptState)
 {
-    return ScriptPromise::cast(scriptState, v8::Undefined(scriptState->isolate()));
+    return ScriptPromise::castUndefined(scriptState);
 }
 
 ScriptPromise UnderlyingSourceBase::pull(ScriptState* scriptState)
 {
-    return ScriptPromise::cast(scriptState, v8::Undefined(scriptState->isolate()));
+    return ScriptPromise::castUndefined(scriptState);
 }
 
 ScriptPromise UnderlyingSourceBase::cancelWrapper(ScriptState* scriptState, ScriptValue reason)
@@ -44,7 +44,7 @@ ScriptPromise UnderlyingSourceBase::cancelWrapper(ScriptState* scriptState, Scri
 
 ScriptPromise UnderlyingSourceBase::cancel(ScriptState* scriptState, ScriptValue reason)
 {
-    return ScriptPromise::cast(scriptState, v8::Undefined(scriptState->isolate()));
+    return ScriptPromise::castUndefined(scriptState);
 }
 
 bool UnderlyingSourceBase::hasPendingActivity() const
