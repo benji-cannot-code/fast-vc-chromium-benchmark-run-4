e684c664f51a7a73aed6804b1c75aa5f617b9fa5
estade <estade@chromium.org>
2016-03-15 18:07:17 -0700

    MD text buttons - implement focus ring.
    BUG=571500,593614
    
    Review URL: https://codereview.chromium.org/1800973002
    
    Cr-Commit-Position: refs/heads/master@{#381377}

--- ui/gfx/geometry/insets.cc
+++ ui/gfx/geometry/insets.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 
-Insets::Insets() : Insets(0, 0, 0, 0) {}
+Insets::Insets() : Insets(0) {}
 
 Insets::Insets(int all) : Insets(all, all, all, all) {}
 
--- ui/gfx/geometry/insets_f.cc
+++ ui/gfx/geometry/insets_f.cc
@@ -9,7 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 
-InsetsF::InsetsF() : InsetsF(0.f, 0.f, 0.f, 0.f) {}
+InsetsF::InsetsF() : InsetsF(0.f) {}
+
+InsetsF::InsetsF(float all) : InsetsF(all, all, all, all) {}
+
+InsetsF::InsetsF(float vertical, float horizontal)
+    : InsetsF(vertical, horizontal, vertical, horizontal) {}
 
 InsetsF::InsetsF(float top, float left, float bottom, float right)
     : top_(top), left_(left), bottom_(bottom), right_(right) {}
