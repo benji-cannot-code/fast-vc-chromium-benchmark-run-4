efd6373b22f8eb037b4089d8151be83158def4f7
bcwhite <bcwhite@chromium.org>
2016-03-16 10:25:41 -0700

    Report histogram creation results.
    Add a new histogram that reports information about all the
    histograms created in different procesesses, their types,
    and what flags were set.
    
    BUG=546019
    TBR=grt
    grt: single instantiating call in installer_metrics.cc
    
    Review URL: https://codereview.chromium.org/1726873002
    
    Cr-Commit-Position: refs/heads/master@{#381482}

--- content/app/content_main_runner.cc
+++ content/app/content_main_runner.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/scoped_vector.h"
+#include "base/metrics/histogram_base.h"
 #include "base/metrics/statistics_recorder.h"
 #include "base/path_service.h"
 #include "base/process/launch.h"
@@ -757,6 +758,8 @@ class ContentMainRunnerImpl : public ContentMainRunner {
         *base::CommandLine::ForCurrentProcess();
     std::string process_type =
         command_line.GetSwitchValueASCII(switches::kProcessType);
+
+    base::HistogramBase::EnableActivityReportHistogram(process_type);
 
     MainFunctionParams main_params(command_line);
     main_params.ui_task = ui_task_;
