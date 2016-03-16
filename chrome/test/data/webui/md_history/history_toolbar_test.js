9a4855c11bce7544bae41c301e30f4b33b18bf42
tsergeant <tsergeant@chromium.org>
2016-03-16 00:01:45 -0700

    MD History: Refactor how data is created in tests
    This CL adds a helper method, createHistoryEntry, which creates a mock
    history visit from a timestamp and URL. This makes creating test data
    simpler and more consistent.
    
    Adapted from https://codereview.chromium.org/1648803003/ by
    hsampson@google.com
    
    BUG=425625
    
    Review URL: https://codereview.chromium.org/1802013003
    
    Cr-Commit-Position: refs/heads/master@{#381413}

--- chrome/test/data/webui/md_history/history_toolbar_test.js
+++ chrome/test/data/webui/md_history/history_toolbar_test.js
@@ -4,23 +4,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 cr.define('md_history.history_toolbar_test', function() {
-  // Array of test history data.
-  var TEST_HISTORY_RESULTS = [
-    {
-      "dateRelativeDay": "Today - Wednesday, December 9, 2015",
-      "title": "Google",
-      "url": "https://www.google.com"
-    }
-  ];
-
   function registerTests() {
     suite('history-toolbar', function() {
       var element;
       var toolbar;
+      var TEST_HISTORY_RESULTS;
 
       suiteSetup(function() {
         element = $('history-list');
         toolbar = $('toolbar');
+        TEST_HISTORY_RESULTS =
+            [createHistoryEntry('2016-03-15', 'https://google.com')];
       });
 
       test('selecting checkbox causes toolbar to change', function(done) {
@@ -60,6 +54,7 @@ cr.define('md_history.history_toolbar_test', function() {
 
       teardown(function() {
         element.historyData = [];
+        registerMessageCallback('queryHistory', this, undefined);
         toolbar.count = 0;
       });
     });
