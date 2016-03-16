b036a069722085d526882c285cafe84b73d758a9
kozyatinskiy <kozyatinskiy@chromium.org>
2016-03-15 22:28:13 -0700

    [DevTools] Move restartFrame and setCallFrameVariableValue to V8DebuggerAgent
    Methods are moved from InjectedScriptSource.js to native implementation in V8DebuggerAgent.
    Removed setVariableValue by function object.
    
    R=dgozman@crhomium.org
    BUG=595206
    
    Review URL: https://codereview.chromium.org/1786243002
    
    Cr-Commit-Position: refs/heads/master@{#381400}

--- third_party/WebKit/Source/platform/v8_inspector/V8JavaScriptCallFrame.cpp
+++ third_party/WebKit/Source/platform/v8_inspector/V8JavaScriptCallFrame.cpp
@@ -100,29 +100,6 @@ void evaluateWithExceptionDetailsMethodCallback(const v8::FunctionCallbackInfo<v
     info.GetReturnValue().Set(impl->evaluateWithExceptionDetails(info[0], info[1]));
 }
 
-void restartMethodCallback(const v8::FunctionCallbackInfo<v8::Value>& info)
-{
-    JavaScriptCallFrame* impl = V8JavaScriptCallFrame::unwrap(info.GetIsolate()->GetCurrentContext(), info.Holder());
-    v8::MaybeLocal<v8::Value> result = impl->restart();
-    v8::Local<v8::Value> value;
-    if (result.ToLocal(&value))
-        info.GetReturnValue().Set(value);
-}
-
-void setVariableValueMethodCallback(const v8::FunctionCallbackInfo<v8::Value>& info)
-{
-    JavaScriptCallFrame* impl = V8JavaScriptCallFrame::unwrap(info.GetIsolate()->GetCurrentContext(), info.Holder());
-    v8::Maybe<int32_t> maybeScopeIndex = info[0]->Int32Value(info.GetIsolate()->GetCurrentContext());
-    if (maybeScopeIndex.IsNothing())
-        return;
-    int scopeIndex = maybeScopeIndex.FromJust();
-    v8::MaybeLocal<v8::Value> result = impl->setVariableValue(scopeIndex, info[1], info[2]);
-    v8::Local<v8::Value> value;
-    if (result.ToLocal(&value))
-        info.GetReturnValue().Set(value);
-
-}
-
 void scopeTypeMethodCallback(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     JavaScriptCallFrame* impl = V8JavaScriptCallFrame::unwrap(info.GetIsolate()->GetCurrentContext(), info.Holder());
@@ -185,8 +162,6 @@ const JavaScriptCallFrameWrapper::V8AttributeConfiguration V8JavaScriptCallFrame
 
 const JavaScriptCallFrameWrapper::V8MethodConfiguration V8JavaScriptCallFrameMethods[] = {
     {"evaluateWithExceptionDetails", evaluateWithExceptionDetailsMethodCallback},
-    {"restart", restartMethodCallback},
-    {"setVariableValue", setVariableValueMethodCallback},
     {"scopeType", scopeTypeMethodCallback},
     {"scopeName", scopeNameMethodCallback},
     {"scopeStartLocation", scopeStartLocationMethodCallback},
--- third_party/WebKit/Source/platform/v8_inspector/injected_script_externs.js
+++ third_party/WebKit/Source/platform/v8_inspector/injected_script_externs.js
@@ -129,15 +129,6 @@ InjectedScriptHostClass.prototype.suppressWarningsAndCallFunction = function(fn,
 InjectedScriptHostClass.prototype.setNonEnumProperty = function(obj, key, value) {}
 
 /**
- * @param {!Function} functionObject
- * @param {number} scopeIndex
- * @param {string} variableName
- * @param {*} newValue
- * @return {*}
- */
-InjectedScriptHostClass.prototype.setFunctionVariableValue = function(functionObject, scopeIndex, variableName, newValue) {}
-
-/**
  * @param {*} value
  * @param {string} groupName
  * @return {number}
@@ -194,19 +185,6 @@ function JavaScriptCallFrame()
  * @return {*}
  */
 JavaScriptCallFrame.prototype.evaluateWithExceptionDetails = function(script, scopeExtension) {}
-
-/**
- * @return {*}
- */
-JavaScriptCallFrame.prototype.restart = function() {}
-
-/**
- * @param {number=} scopeIndex
- * @param {?string=} variableName
- * @param {*=} newValue
- * @return {*}
- */
-JavaScriptCallFrame.prototype.setVariableValue = function(scopeIndex, variableName, newValue) {}
 
 /**
  * @param {number} scopeIndex
