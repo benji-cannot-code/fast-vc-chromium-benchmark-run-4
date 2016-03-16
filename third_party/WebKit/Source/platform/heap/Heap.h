fc38bf2bd7f7ab5e1bf78ea35fc101b45dcffb65
keishi <keishi@chromium.org>
2016-03-16 07:46:51 -0700

    Refactor RegionTree
    This creates a new class, RegionTree, so we can reference the binary search tree and the mutex as one thing.
    
    BUG=591606
    
    Review URL: https://codereview.chromium.org/1804863002
    
    Cr-Commit-Position: refs/heads/master@{#381451}

--- third_party/WebKit/Source/platform/heap/Heap.h
+++ third_party/WebKit/Source/platform/heap/Heap.h
@@ -260,8 +260,7 @@ public:
     // provide an efficient mapping from arbitrary addresses to the containing
     // heap-page if one exists.
     static BasePage* lookup(Address);
-    static void addPageMemoryRegion(PageMemoryRegion*);
-    static void removePageMemoryRegion(PageMemoryRegion*);
+    static RegionTree* getRegionTree();
 
     static const GCInfo* gcInfo(size_t gcInfoIndex)
     {
@@ -318,7 +317,6 @@ private:
     static HeapDoesNotContainCache* s_heapDoesNotContainCache;
     static FreePagePool* s_freePagePool;
     static OrphanedPagePool* s_orphanedPagePool;
-    static RegionTree* s_regionTree;
     static size_t s_allocatedSpace;
     static size_t s_allocatedObjectSize;
     static size_t s_objectSizeAtLastGC;
