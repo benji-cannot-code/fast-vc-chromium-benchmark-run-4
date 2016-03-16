ee0d5f4b6b946ffb7b70a656f32114bdb962188b
yyanagisawa <yyanagisawa@chromium.org>
2016-03-16 02:38:30 -0700

    off_the_record_chrome_browser_state_io_data.mm: fix case for UIKit.
    In SDK, it is UIKit.framework/Versions/C/Headers/UIKit.h.
    Use the same case, so that it would work on case sensitive filesystem.
    
    Review URL: https://codereview.chromium.org/1800423002
    
    Cr-Commit-Position: refs/heads/master@{#381424}

--- ios/chrome/browser/browser_state/off_the_record_chrome_browser_state_io_data.mm
+++ ios/chrome/browser/browser_state/off_the_record_chrome_browser_state_io_data.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/browser_state/off_the_record_chrome_browser_state_io_data.h"
 
-#import <UIKit/UIkit.h>
+#import <UIKit/UIKit.h>
 
 #include <utility>
 
