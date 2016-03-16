f3f233c8e528044a9b79e8932f74da79f10a7398
wjmaclean <wjmaclean@chromium.org>
2016-03-16 14:00:12 -0700

    Ignore gesture sequences that have no target.
    This CL implements a speculative fix for the associated bug, on the
    theory that sequences associated with context menus (triggered by
    GestureLongPress) may not be properly suppressed on Windows, leading
    to the observation of sequences without any target in
    RenderWidgetHostInputEventRouter. If this hypothesis is correct, then
    we should be correct in ignoring such sequences, and this CL implements
    that.
    
    This CL also removes the code for collecting debug data introduced in
    https://codereview.chromium.org/1783533002/.
    
    BUG=592319
    
    Review URL: https://codereview.chromium.org/1810693002
    
    Cr-Commit-Position: refs/heads/master@{#381540}

--- content/browser/renderer_host/render_widget_host_input_event_router.cc
+++ content/browser/renderer_host/render_widget_host_input_event_router.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/renderer_host/render_widget_host_input_event_router.h"
 
-#include "base/debug/dump_without_crashing.h"
 #include "cc/quads/surface_draw_quad.h"
 #include "cc/surfaces/surface_id_allocator.h"
 #include "cc/surfaces/surface_manager.h"
@@ -26,18 +25,6 @@ void TransformEventTouchPositions(blink::WebTouchEvent* event,
 }  // anonymous namespace
 
 namespace content {
-
-// Code to assist debugging https://crbug.com/592319.
-RenderWidgetHostInputEventRouter::GestureQueueDebugData::GestureQueueDebugData()
-    : number_of_gesture_targets_enqueued(0),
-      number_of_gesture_targets_dequeued(0),
-      next_gesture_event_index(0),
-      next_touch_event_index(0) {
-  for (unsigned i = 0; i < kNumLastEventTypes; ++i) {
-    last_gesture_event_types[i] = blink::WebInputEvent::Undefined;
-    last_touch_event_types[i] = blink::WebInputEvent::Undefined;
-  }
-}
 
 void RenderWidgetHostInputEventRouter::OnRenderWidgetHostViewBaseDestroyed(
     RenderWidgetHostViewBase* view) {
@@ -185,11 +172,11 @@ void RenderWidgetHostInputEventRouter::RouteGestureEvent(
   // fling.
   if (event->type == blink::WebInputEvent::GestureTapDown) {
     if (gesture_target_queue_.empty()) {
-      // Collect data to assist debugging https://crbug.com/592319.
-      // We'll assume for now if there's no target that we should just ignore
-      // the event.
       LOG(ERROR) << "Gesture sequence start detected with no target available.";
-      base::debug::DumpWithoutCrashing();
+      // Ignore this gesture sequence as no target is available.
+      // TODO(wjmaclean): this only happens on Windows, and should not happen.
+      // https://crbug.com/595422
+      gesture_target_ = nullptr;
       return;
     }
 
@@ -197,15 +184,7 @@ void RenderWidgetHostInputEventRouter::RouteGestureEvent(
     gesture_target_ = data.target;
     gesture_delta_ = data.delta;
     gesture_target_queue_.pop_front();
-    // Code to assist debugging https://crbug.com/592319.
-    debug_data_.number_of_gesture_targets_dequeued++;
   }
-
-  // Code to assist debugging https://crbug.com/592319.
-  debug_data_.last_gesture_event_types[debug_data_.next_gesture_event_index] =
-      event->type;
-  debug_data_.next_gesture_event_index =
-      (debug_data_.next_gesture_event_index + 1) % kNumLastEventTypes;
 
   if (!gesture_target_)
     return;
@@ -219,12 +198,6 @@ void RenderWidgetHostInputEventRouter::RouteTouchEvent(
     RenderWidgetHostViewBase* root_view,
     blink::WebTouchEvent* event,
     const ui::LatencyInfo& latency) {
-  // Code to assist debugging https://crbug.com/592319.
-  debug_data_.last_touch_event_types[debug_data_.next_touch_event_index] =
-      event->type;
-  debug_data_.next_touch_event_index =
-      (debug_data_.next_touch_event_index + 1) % kNumLastEventTypes;
-
   switch (event->type) {
     case blink::WebInputEvent::TouchStart: {
       if (!active_touches_) {
@@ -244,8 +217,6 @@ void RenderWidgetHostInputEventRouter::RouteTouchEvent(
         // for the duration of the sequence.
         touch_delta_ = transformed_point - original_point;
         gesture_target_queue_.emplace_back(touch_target_, touch_delta_);
-        // Code to assist debugging https://crbug.com/592319.
-        debug_data_.number_of_gesture_targets_enqueued++;
 
         if (!touch_target_)
           return;
