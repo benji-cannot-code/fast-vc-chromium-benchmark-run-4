547a5405f33a17284533f5f75a44d979693b5a33
tommi <tommi@chromium.org>
2016-03-15 19:31:56 -0700

    Revert of Update mojo Decryptor interface to support reusing shared memory (patchset #3 id:60001 of https://codereview.chromium.org/1766093004/ )
    Reason for revert:
    Broke the win build:
    
    https://build.chromium.org/p/chromium.win/builders/Win%20x64%20GN/builds/11754/steps/compile/logs/stdio
    
    FAILED: ninja -t msvc -e environment.x64 -- E:\b\build\goma/gomacc.exe "E:\b\depot_tools\win_toolchain\vs_files\a3796183a9fc4d22a687c5212b9c76dbd136d70d\VC\bin\amd64/cl.exe" /nologo /showIncludes /FC @obj/media/mojo/services/application/service_factory_impl.obj.rsp /c ../../media/mojo/services/service_factory_impl.cc /Foobj/media/mojo/services/application/service_factory_impl.obj /Fdobj/media/mojo/services/application_cc.pdb
    e:\b\build\slave\win_x64_gn\build\src\media\mojo\services\mojo_decryptor_service.h(99): error C2039: 'unordered_map': is not a member of 'std'
    e:\b\depot_tools\win_toolchain\vs_files\a3796183a9fc4d22a687c5212b9c76dbd136d70d\vc\include\list(15): note: see declaration of 'std'
    e:\b\build\slave\win_x64_gn\build\src\media\mojo\services\mojo_decryptor_service.h(99): error C2143: syntax error: missing ';' before '<'
    e:\b\build\slave\win_x64_gn\build\src\media\mojo\services\mojo_decryptor_service.h(99): error C4430: missing type specifier - int assumed. Note: C++ does not support default-int
    e:\b\build\slave\win_x64_gn\build\src\media\mojo\services\mojo_decryptor_service.h(100): error C2238: unexpected token(s) preceding ';'
    FAILED: ninja -t msvc -e environment.x64 -- E:\b\build\goma/gomacc.exe "E:\b\depot_tools\win_toolchain\vs_files\a3796183a9fc4d22a687c5212b9c76dbd136d70d\VC\bin\amd64/cl.exe" /nologo /showIncludes /FC @obj/media/mojo/services/cdm_service/mojo_cdm_service.obj.rsp /c ../../media/mojo/services/mojo_cdm_service.cc /Foobj/media/mojo/services/cdm_service/mojo_cdm_service.obj /Fdobj/media/mojo/services/cdm_service_cc.pdb
    e:\b\build\slave\win_x64_gn\build\src\media\mojo\services\mojo_decryptor_service.h(99): error C2039: 'unordered_map': is not a member of 'std'
    e:\b\depot_tools\win_toolchain\vs_files\a3796183a9fc4d22a687c5212b9c76dbd136d70d\vc\include\list(15): note: see declaration of 'std'
    ...
    
    Original issue's description:
    > Update mojo Decryptor interface to support reusing shared memory
    >
    > Now that VideoFrames are transported across mojo using shared memory,
    > the original needs to stay around until the other side is no longer
    > using it, after which the shared memory can be reused for a
    > subsequent VideoFrame.
    >
    > BUG=510153
    >
    > Committed: https://crrev.com/30752f9101640c873987758baa4b11407aaf3029
    > Cr-Commit-Position: refs/heads/master@{#381361}
    
    TBR=xhwang@chromium.org,rockot@chromium.org,jrummell@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=510153
    
    Review URL: https://codereview.chromium.org/1809573002
    
    Cr-Commit-Position: refs/heads/master@{#381383}

--- media/mojo/services/mojo_decryptor_service.cc
+++ media/mojo/services/mojo_decryptor_service.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_decoder_config.h"
 #include "media/base/video_frame.h"
 #include "media/mojo/common/media_type_converters.h"
-#include "media/mojo/common/mojo_shared_buffer_video_frame.h"
 #include "media/mojo/interfaces/demuxer_stream.mojom.h"
 
 namespace media {
@@ -114,12 +113,6 @@ void MojoDecryptorService::DeinitializeDecoder(
       static_cast<media::Decryptor::StreamType>(stream_type));
 }
 
-void MojoDecryptorService::ReleaseSharedBuffer(
-    mojo::ScopedSharedBufferHandle buffer,
-    uint64_t buffer_size) {
-  in_use_video_frames_.erase(buffer.get().value());
-}
-
 void MojoDecryptorService::OnDecryptDone(
     const DecryptCallback& callback,
     media::Decryptor::Status status,
@@ -180,15 +173,6 @@ void MojoDecryptorService::OnVideoDecoded(
     DCHECK_NE(status, media::Decryptor::kSuccess);
     callback.Run(static_cast<Decryptor::Status>(status), nullptr);
     return;
-  }
-
-  // If |frame| has shared memory that will be passed back, keep a reference
-  // to it until the other side is done with the memory.
-  if (frame->storage_type() == VideoFrame::STORAGE_MOJO_SHARED_BUFFER) {
-    MojoSharedBufferVideoFrame* mojo_frame =
-        static_cast<MojoSharedBufferVideoFrame*>(frame.get());
-    in_use_video_frames_.insert(
-        std::make_pair(mojo_frame->Handle().value(), frame));
   }
 
   callback.Run(static_cast<Decryptor::Status>(status),
