06528a45a77faa00b1717cbb3d90b985381951b5
peconn <peconn@chromium.org>
2016-03-16 05:33:03 -0700

    Sign the user out before signing the next user in when switching sync accounts.
    Additionally it prevents managed users from choosing to merge data.
    
    BUG=593637
    
    Review URL: https://codereview.chromium.org/1783393002
    
    Cr-Commit-Position: refs/heads/master@{#381438}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/SyncAccountSwitcher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/SyncAccountSwitcher.java
@@ -66,7 +66,16 @@ public class SyncAccountSwitcher
     @Override
     public void onConfirm() {
         assert mNewAccountName != null;
-        SigninManager.get(mActivity).signIn(mNewAccountName, mActivity, this);
+
+        final SigninManager.SignInCallback callback = this;
+
+        // Sign out first to get sync working correctly.
+        SigninManager.get(mActivity).signOut(new Runnable() {
+            @Override
+            public void run() {
+                SigninManager.get(mActivity).signIn(mNewAccountName, mActivity, callback);
+            }
+        });
     }
 
     @Override
