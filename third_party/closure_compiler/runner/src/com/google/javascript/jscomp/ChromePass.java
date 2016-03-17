07dbb875006ba2cf23acec2463d419f81ac0caba
dbeam <dbeam@chromium.org>
2016-03-17 16:21:49 -0700

    Fix cr.exportPath()'s @return signature so the result can be used
    R=dpapad@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/1801293005
    
    Cr-Commit-Position: refs/heads/master@{#381831}

--- third_party/closure_compiler/runner/src/com/google/javascript/jscomp/ChromePass.java
+++ third_party/closure_compiler/runner/src/com/google/javascript/jscomp/ChromePass.java
@@ -258,6 +258,7 @@ public class ChromePass extends AbstractPostOrderCallback implements CompilerPas
 
         Node pathArg = crExportPathNode.getChildAtIndex(1);
         if (pathArg.isString()) {
+            // TODO(dbeam): support cr.exportPath('ns').value.
             createAndInsertObjectsForQualifiedName(parent, pathArg.getString());
         }
     }
