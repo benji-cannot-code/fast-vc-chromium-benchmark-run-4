30eea84b6249068d5b5a65e070d41c535b7d72ef
nasko <nasko@chromium.org>
2016-03-16 17:22:22 -0700

    Remove swapped out state from RenderFrameHost.
    Since swapped out RenderFrameHosts are no longer used, its swapped out
    state is no longer necessary. This CL removes it along with a public
    API that exposed it outside of content/.
    
    BUG=357747
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review URL: https://codereview.chromium.org/1799163002
    
    Cr-Commit-Position: refs/heads/master@{#381601}

--- content/browser/renderer_host/render_view_host_impl.h
+++ content/browser/renderer_host/render_view_host_impl.h
@@ -333,6 +333,9 @@ class CONTENT_EXPORT RenderViewHostImpl : public RenderViewHost,
   FRIEND_TEST_ALL_PREFIXES(RenderViewHostTest, RoutingIdSane);
   FRIEND_TEST_ALL_PREFIXES(RenderFrameHostManagerTest,
                            CleanUpSwappedOutRVHOnProcessCrash);
+  FRIEND_TEST_ALL_PREFIXES(SitePerProcessBrowserTest,
+                           NavigateMainFrameToChildSite);
+
   // Send RenderViewReady to observers once the process is launched, but not
   // re-entrantly.
   void PostRenderViewReady();
