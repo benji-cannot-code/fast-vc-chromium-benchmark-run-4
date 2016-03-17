aee8c830d365dd64d9bea5ea8dbacb052ba87921
drott <drott@chromium.org>
2016-03-17 03:16:56 -0700

    Shape unicode-range: font faces in only one iteration
    Previously, we incorrectly split CSS composite faces into multiple faces
    for shaping, one for each comma-separated entry of unicode-range:. This
    breaks shaping of ligatures and other features when the characters that
    ought to be shaped combined were in different unicode-range entries. It
    is also inefficient for subsetted web fonts that use unicode-range:
    extensively, for example Google Fonts and Adobe TypeKit.
    
    The fix is to transfer the UnicodeRangeSet information from CSSFontFace
    to HarfBuzzFace and only restrict the glyph lookup function to the whole
    unicode-range information, instead of restricting it to a single entry
    and shaping multiple times with the same face. This should have a slight
    performance benefit as well.
    
    BUG=583450
    TEST=fast/css/font-face-unicode-range-ligatures.html
    R=eae, behdad
    
    Review URL: https://codereview.chromium.org/1806653002
    
    Cr-Commit-Position: refs/heads/master@{#381683}

--- third_party/WebKit/Source/platform/fonts/shaping/HarfBuzzFace.cpp
+++ third_party/WebKit/Source/platform/fonts/shaping/HarfBuzzFace.cpp
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/fonts/FontCache.h"
 #include "platform/fonts/FontPlatformData.h"
 #include "platform/fonts/SimpleFontData.h"
+#include "platform/fonts/UnicodeRangeSet.h"
 #include "platform/fonts/shaping/HarfBuzzShaper.h"
 #include "wtf/HashMap.h"
 #include "wtf/MathExtras.h"
@@ -115,13 +116,13 @@ struct HarfBuzzFontData {
     USING_FAST_MALLOC(HarfBuzzFontData);
     WTF_MAKE_NONCOPYABLE(HarfBuzzFontData);
 public:
-    HarfBuzzFontData(WTF::HashMap<uint32_t, uint16_t>* glyphCacheForFaceCacheEntry, hb_face_t* face, unsigned rangeFrom, unsigned rangeTo)
+    HarfBuzzFontData(WTF::HashMap<uint32_t, uint16_t>* glyphCacheForFaceCacheEntry, hb_face_t* face, PassRefPtr<UnicodeRangeSet> rangeSet)
         : m_glyphCacheForFaceCacheEntry(glyphCacheForFaceCacheEntry)
         , m_face(face)
         , m_hbOpenTypeFont(nullptr)
-        , m_rangeFrom(rangeFrom)
-        , m_rangeTo(rangeTo)
-    { }
+        , m_rangeSet(rangeSet)
+    {
+    }
 
     ~HarfBuzzFontData()
     {
@@ -134,8 +135,7 @@ public:
     WTF::HashMap<uint32_t, uint16_t>* m_glyphCacheForFaceCacheEntry;
     hb_face_t* m_face;
     hb_font_t* m_hbOpenTypeFont;
-    unsigned m_rangeFrom;
-    unsigned m_rangeTo;
+    RefPtr<UnicodeRangeSet> m_rangeSet;
 };
 
 static hb_position_t SkiaScalarToHarfBuzzPosition(SkScalar value)
@@ -185,7 +185,7 @@ static hb_bool_t harfBuzzGetGlyph(hb_font_t* hbFont, void* fontData, hb_codepoin
     HarfBuzzFontData* hbFontData = reinterpret_cast<HarfBuzzFontData*>(fontData);
 
     RELEASE_ASSERT(hbFontData);
-    if (unicode < hbFontData->m_rangeFrom || unicode > hbFontData->m_rangeTo)
+    if (hbFontData->m_rangeSet && !hbFontData->m_rangeSet->contains(unicode))
         return false;
 
     if (variationSelector) {
@@ -351,9 +351,9 @@ hb_face_t* HarfBuzzFace::createFace()
     return face;
 }
 
-hb_font_t* HarfBuzzFace::createFont(unsigned rangeFrom, unsigned rangeTo) const
+hb_font_t* HarfBuzzFace::createFont(PassRefPtr<UnicodeRangeSet> rangeSet) const
 {
-    HarfBuzzFontData* hbFontData = new HarfBuzzFontData(m_glyphCacheForFaceCacheEntry, m_face, rangeFrom, rangeTo);
+    HarfBuzzFontData* hbFontData = new HarfBuzzFontData(m_glyphCacheForFaceCacheEntry, m_face, rangeSet);
     m_platformData->setupPaint(&hbFontData->m_paint);
     hbFontData->m_simpleFontData = FontCache::fontCache()->fontDataFromFontPlatformData(m_platformData);
     ASSERT(hbFontData->m_simpleFontData);
--- third_party/WebKit/Source/platform/fonts/shaping/HarfBuzzShaper.h
+++ third_party/WebKit/Source/platform/fonts/shaping/HarfBuzzShaper.h
@@ -54,6 +54,7 @@ class Font;
 class GlyphBuffer;
 class SimpleFontData;
 class HarfBuzzShaper;
+class UnicodeRangeSet;
 
 // Shaping text runs is split into several stages: Run segmentation, shaping the
 // initial segment, identify shaped and non-shaped sequences of the shaping
@@ -163,8 +164,7 @@ private:
         unsigned startIndex,
         unsigned numCharacters,
         const SimpleFontData* currentFont,
-        unsigned currentFontRangeFrom,
-        unsigned currentFontRangeTo,
+        PassRefPtr<UnicodeRangeSet> currentFontRangeSet,
         UScriptCode currentRunScript,
         hb_language_t);
     bool extractShapeResults(hb_buffer_t* harfBuzzBuffer,
