44ae56303d7be8aea453e5df7d935f99189c73e3
brucedawson <brucedawson@chromium.org>
2016-03-16 19:03:53 -0700

    Fix VS 2015 Update 2 specific bug
    When building Chrome with VS 2015 Update 2 RC there is a compiler error:
    
    1>  texture_definition.cc
    1>base/memory/ref_counted.h(414): error C2027: use of undefined type 'gpu::gles2::GLStreamTextureImage'
    1>  gpu/command_buffer/service/texture_manager.h(31): note: see declaration of 'gpu::gles2::GLStreamTextureImage'
    1>  base/memory/ref_counted.h(413): note: while compiling class template member function 'void scoped_refptr<gpu::gles2::GLStreamTextureImage>::AddRef(T *)'
    
    This is caused by a compiler bug, together with a type trait used in
    std::vector:
    https://twitter.com/StephanTLavavej/status/710191543840219136
    
    A VS bug has been filed at:
    https://connect.microsoft.com/VisualStudio/feedback/details/2475971
    
    Bug 595189 has a more minimal repro.
    
    BUG=440500,595189
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1806743002
    
    Cr-Commit-Position: refs/heads/master@{#381640}

--- gpu/command_buffer/service/gl_stream_texture_image.h
+++ gpu/command_buffer/service/gl_stream_texture_image.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GPU_COMMAND_BUFFER_SERVICE_GL_STREAM_TEXTURE_IMAGE_H_
 #define GPU_COMMAND_BUFFER_SERVICE_GL_STREAM_TEXTURE_IMAGE_H_
 
+#include "gpu/gpu_export.h"
 #include "ui/gl/gl_image.h"
 
 namespace gpu {
