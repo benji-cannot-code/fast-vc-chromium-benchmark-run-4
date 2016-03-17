4c847d29c4d481dfe826dbcea492fd4f86bbc663
tedchoc <tedchoc@chromium.org>
2016-03-16 17:07:56 -0700

    Speculative fix for the NPE in dispatchGetDisplayList
    In https://codereview.chromium.org/1638303002, we began potentially
    removing the overscroll refresh layout at the end of the animations.
    
    According to this article:
    http://stackoverflow.com/questions/15636658/nullpointerexception-when-adding-removing-views-of-a-relativelayout-animations
    
    That could potentially be the source of the crashes we are seeing.
    
    Changing it to post the detach instead of calling it synchronously in
    the animation listener.
    
    BUG=590359
    
    Review URL: https://codereview.chromium.org/1800293004
    
    Cr-Commit-Position: refs/heads/master@{#381595}

--- chrome/android/java/src/org/chromium/chrome/browser/SwipeRefreshHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/SwipeRefreshHandler.java
@@ -43,6 +43,10 @@ public class SwipeRefreshHandler implements OverscrollRefreshHandler {
     // loads a frame. This is used to provide a reasonable minimum animation time.
     private Runnable mStopRefreshingRunnable;
 
+    // Handles removing the layout from the view hierarchy.  This is posted to ensure it does not
+    // conflict with pending Android draws.
+    private Runnable mDetachLayoutRunnable;
+
     // Accessibility utterance used to indicate refresh activation.
     private String mAccessibilityRefreshString;
 
@@ -101,7 +105,15 @@ public class SwipeRefreshHandler implements OverscrollRefreshHandler {
         mSwipeRefreshLayout.setOnResetListener(new SwipeRefreshLayout.OnResetListener() {
             @Override
             public void onReset() {
-                detachSwipeRefreshLayoutIfNecessary();
+                if (mDetachLayoutRunnable != null) return;
+                mDetachLayoutRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        mDetachLayoutRunnable = null;
+                        detachSwipeRefreshLayoutIfNecessary();
+                    }
+                };
+                mSwipeRefreshLayout.post(mDetachLayoutRunnable);
             }
         });
 
@@ -158,6 +170,13 @@ public class SwipeRefreshHandler implements OverscrollRefreshHandler {
         }
     }
 
+    private void cancelDetachLayoutRunnable() {
+        if (mDetachLayoutRunnable != null) {
+            mSwipeRefreshLayout.removeCallbacks(mDetachLayoutRunnable);
+            mDetachLayoutRunnable = null;
+        }
+    }
+
     private Runnable getStopRefreshingRunnable() {
         if (mStopRefreshingRunnable == null) {
             mStopRefreshingRunnable = new Runnable() {
@@ -173,6 +192,7 @@ public class SwipeRefreshHandler implements OverscrollRefreshHandler {
     // The animation view is attached/detached on-demand to minimize overlap
     // with composited SurfaceView content.
     private void attachSwipeRefreshLayoutIfNecessary() {
+        cancelDetachLayoutRunnable();
         if (mContentViewCore == null) return;
         if (mSwipeRefreshLayout.getParent() == null) {
             mContentViewCore.getContainerView().addView(mSwipeRefreshLayout);
@@ -180,6 +200,7 @@ public class SwipeRefreshHandler implements OverscrollRefreshHandler {
     }
 
     private void detachSwipeRefreshLayoutIfNecessary() {
+        cancelDetachLayoutRunnable();
         if (mContentViewCore == null) return;
         if (mSwipeRefreshLayout.getParent() != null) {
             mContentViewCore.getContainerView().removeView(mSwipeRefreshLayout);
