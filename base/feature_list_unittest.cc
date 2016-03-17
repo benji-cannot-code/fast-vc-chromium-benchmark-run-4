18ecf6a52c334b6580fda1d048fa2cf2dcd8202a
asvitkine <asvitkine@chromium.org>
2016-03-17 10:32:00 -0700

    Allow trials to associate without overriding a feature.
    This adds a heuristic such that if a study only references a single
    feature from any of its groups, its other groups will be associated
    with that feature as well, for reporting only. This allows a config like
    25% Enabled, 25% Disabled and 50% Default to still report the users
    in the default group to UMA, so that they show up in the data.
    
    More details in the code comments.
    
    BUG=587135
    
    Review URL: https://codereview.chromium.org/1809633003
    
    Cr-Commit-Position: refs/heads/master@{#381737}

--- base/feature_list_unittest.cc
+++ base/feature_list_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include <algorithm>
 #include <utility>
 
 #include "base/format_macros.h"
@@ -177,6 +178,35 @@ TEST_F(FeatureListTest, FieldTrialOverrides) {
     EXPECT_TRUE(FieldTrialList::IsTrialActive(trial1->trial_name()));
     EXPECT_TRUE(FieldTrialList::IsTrialActive(trial2->trial_name()));
   }
+}
+
+TEST_F(FeatureListTest, FieldTrialAssociateUseDefault) {
+  FieldTrialList field_trial_list(nullptr);
+  scoped_ptr<FeatureList> feature_list(new FeatureList);
+
+  FieldTrial* trial1 = FieldTrialList::CreateFieldTrial("TrialExample1", "A");
+  FieldTrial* trial2 = FieldTrialList::CreateFieldTrial("TrialExample2", "B");
+  feature_list->RegisterFieldTrialOverride(
+      kFeatureOnByDefaultName, FeatureList::OVERRIDE_USE_DEFAULT, trial1);
+  feature_list->RegisterFieldTrialOverride(
+      kFeatureOffByDefaultName, FeatureList::OVERRIDE_USE_DEFAULT, trial2);
+  RegisterFeatureListInstance(std::move(feature_list));
+
+  // Initially, neither trial should be active.
+  EXPECT_FALSE(FieldTrialList::IsTrialActive(trial1->trial_name()));
+  EXPECT_FALSE(FieldTrialList::IsTrialActive(trial2->trial_name()));
+
+  // Check the feature enabled state is its default.
+  EXPECT_TRUE(FeatureList::IsEnabled(kFeatureOnByDefault));
+  // The above should have activated |trial1|.
+  EXPECT_TRUE(FieldTrialList::IsTrialActive(trial1->trial_name()));
+  EXPECT_FALSE(FieldTrialList::IsTrialActive(trial2->trial_name()));
+
+  // Check the feature enabled state is its default.
+  EXPECT_FALSE(FeatureList::IsEnabled(kFeatureOffByDefault));
+  // The above should have activated |trial2|.
+  EXPECT_TRUE(FieldTrialList::IsTrialActive(trial1->trial_name()));
+  EXPECT_TRUE(FieldTrialList::IsTrialActive(trial2->trial_name()));
 }
 
 TEST_F(FeatureListTest, CommandLineTakesPrecedenceOverFieldTrial) {
