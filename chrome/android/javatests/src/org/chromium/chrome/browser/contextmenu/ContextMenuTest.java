b8c81f5c94ce6719a81cdbe7a2dbe37610f6e355
rnephew <rnephew@chromium.org>
2016-03-17 11:22:21 -0700

    [Android] Renable ContextMenuTest and SelectFileDialogTest disabled due to permissions issues.
    Should be fixed with https://codereview.chromium.org/1800283002/
    
    BUG=592594,592627,591224
    
    Review URL: https://codereview.chromium.org/1814593002
    
    Cr-Commit-Position: refs/heads/master@{#381750}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
@@ -15,7 +15,6 @@ import android.view.ContextMenu;
 import android.view.KeyEvent;
 
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -214,7 +213,6 @@ public class ContextMenuTest extends DownloadTestBase {
 
     @LargeTest
     @Feature({"Browser"})
-    @DisableIf.Build(sdk_is_greater_than = 22, message = "crbug.com/592594")
     public void testSaveDataUrl()
             throws InterruptedException, TimeoutException, SecurityException, IOException {
         saveMediaFromContextMenu("dataUrlIcon", R.id.contextmenu_save_image, "download.gif");
@@ -222,7 +220,6 @@ public class ContextMenuTest extends DownloadTestBase {
 
     @LargeTest
     @Feature({"Browser"})
-    @DisableIf.Build(sdk_is_greater_than = 22, message = "crbug.com/592594")
     public void testSaveImage()
             throws InterruptedException, TimeoutException, SecurityException, IOException {
         saveMediaFromContextMenu("testImage", R.id.contextmenu_save_image, "test_image.png");
@@ -230,7 +227,6 @@ public class ContextMenuTest extends DownloadTestBase {
 
     @LargeTest
     @Feature({"Browser"})
-    @DisableIf.Build(sdk_is_greater_than = 22, message = "crbug.com/592594")
     public void testSaveVideo()
             throws InterruptedException, TimeoutException, SecurityException, IOException {
         // Click the video to enable playback
