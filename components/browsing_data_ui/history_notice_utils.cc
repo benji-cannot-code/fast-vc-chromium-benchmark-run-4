d1dce21a623cc18f527d775b018be0659ca96a27
msramek <msramek@chromium.org>
2016-03-17 14:50:37 -0700

    Add an infrastructure to query history.google.com about other forms of browsing history.
    The Clear Browsing Data dialog UI will provide a notice informing the
    user that history.google.com stores other forms of browsing history
    (such as the search history).
    
    We will only show the notice to those users for whom it is valid. This
    will be determined by Sync status, and by a JSON response from
    history.google.com [not yet implemented].
    
    See https://docs.google.com/document/d/1ZMDSAd44KmzKhqXPjobZOf9rZezs6VqiBnqpDD0auCU/
    for the background.
    
    To communicate between backends (ProfileSyncService, HistoryService) and
    frontends on all platforms, we need to put this logic to a component
    (especially because of iOS). We create a new component "browsing_data_ui".
    
    BUG=595332
    
    Review URL: https://codereview.chromium.org/1806873002
    
    Cr-Commit-Position: refs/heads/master@{#381804}

---
+++ components/browsing_data_ui/history_notice_utils.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/browsing_data_ui/history_notice_utils.h"
+
+#include "components/browser_sync/browser/profile_sync_service.h"
+#include "components/history/core/browser/web_history_service.h"
+
+namespace browsing_data_ui {
+
+bool ShouldShowNoticeAboutOtherFormsOfBrowsingHistory(
+    const ProfileSyncService* sync_service,
+    const history::WebHistoryService* history_service) {
+  return sync_service &&
+         sync_service->IsUsingSecondaryPassphrase() &&
+         history_service &&
+         history_service->HasOtherFormsOfBrowsingHistory();
+}
+
+}  // namespace browsing_data_ui
--- components/components.gyp
+++ components/components.gyp
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     'autofill.gypi',
     'bookmarks.gypi',
     'browser_sync.gypi',
+    'browsing_data_ui.gypi',
     'bubble.gypi',
     'captive_portal.gypi',
     'certificate_reporting.gypi',
