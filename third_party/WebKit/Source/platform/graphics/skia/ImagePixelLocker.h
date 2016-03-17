4514d3f0ce6c9a90f84dd008ce6e3b5ab86ba871
robertphillips <robertphillips@google.com>
2016-03-17 06:22:16 -0700

    ImagePixelLocker now manually allocates SkPixmap
    Skia is retracting the SkAutoPixmapStorage class. These are the only two uses of it in Chrome.
    
    Review URL: https://codereview.chromium.org/1813483002
    
    Cr-Commit-Position: refs/heads/master@{#381700}

--- third_party/WebKit/Source/platform/graphics/skia/ImagePixelLocker.h
+++ third_party/WebKit/Source/platform/graphics/skia/ImagePixelLocker.h
@@ -29,7 +29,7 @@ public:
 private:
     const RefPtr<const SkImage> m_image;
     const void* m_pixels;
-    SkAutoPixmapStorage m_pixelStorage;
+    SkAutoMalloc m_pixelStorage;
 };
 
 } // namespace blink
