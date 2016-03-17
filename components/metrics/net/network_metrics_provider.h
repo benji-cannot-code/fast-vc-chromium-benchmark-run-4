3b5653eabeaf5ab7bae42dc6855973c10f261788
csharrison <csharrison@chromium.org>
2016-03-17 12:51:27 -0700

    Log aggregated abort metrics when histograms are uploaded
    This patch adds a method on NetworkMetricsProvider that logs aggregate ABORT
    metrics on the NetworkMetricsProvider::ProvideGeneralMetrics callback. This
    gives finer grained abort statistics.
    
    BUG=557430
    
    Review URL: https://codereview.chromium.org/1796983002
    
    Cr-Commit-Position: refs/heads/master@{#381780}

--- components/metrics/net/network_metrics_provider.h
+++ components/metrics/net/network_metrics_provider.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_base.h"
 #include "components/metrics/metrics_provider.h"
 #include "components/metrics/net/wifi_access_point_info_provider.h"
 #include "components/metrics/proto/system_profile.pb.h"
@@ -53,6 +54,9 @@ class NetworkMetricsProvider
       const WifiAccessPointInfoProvider::WifiAccessPointInfo& info,
       SystemProfileProto::Network* network_proto);
 
+  // Logs metrics that are functions of other metrics being uploaded.
+  void LogAggregatedMetrics();
+
   // Task runner used for blocking file I/O.
   base::TaskRunner* io_task_runner_;
 
@@ -69,6 +73,11 @@ class NetworkMetricsProvider
 
   // Helper object for retrieving connected wifi access point information.
   scoped_ptr<WifiAccessPointInfoProvider> wifi_access_point_info_provider_;
+
+  // These metrics track histogram totals for the Net.ErrorCodesForMainFrame3
+  // histogram. They are used to compute deltas at upload time.
+  base::HistogramBase::Count total_aborts_;
+  base::HistogramBase::Count total_codes_;
 
   base::WeakPtrFactory<NetworkMetricsProvider> weak_ptr_factory_;
 
