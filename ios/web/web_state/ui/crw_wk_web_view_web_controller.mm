189f189e032296764467b3c42bc55ff974f0b33f
eugenebut <eugenebut@chromium.org>
2016-03-17 11:44:43 -0700

    [ios] Refactored dialogs suppression.
    1.) Added |suppressDialogs| property to CRWWebController, which should
        be used instead of |setPageDialogOpenPolicy|.
    2.) Suppress all dialogs in native code instead of JavaScript (except
        navigation dialogs which can not be suppressed w/o JS override).
    3.) Suppress HTTP Auth dialog if |suppressDialogs| is YES.
    
    BUG=595463
    TEST=window.open, window.prompt, window.confirm, window.alert,
               geolocation and http auth dialogs should be blocked for preload
               pages. All these dialogs should work normally for regular pages.
    
    Review URL: https://codereview.chromium.org/1806043004
    
    Cr-Commit-Position: refs/heads/master@{#381759}

--- ios/web/web_state/ui/crw_wk_web_view_web_controller.mm
+++ ios/web/web_state/ui/crw_wk_web_view_web_controller.mm
@@ -458,12 +458,6 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
   [super terminateNetworkActivity];
 }
 
-- (void)setPageDialogOpenPolicy:(web::PageDialogOpenPolicy)policy {
-  // TODO(eugenebut): implement dialogs/windows suppression using
-  // WKNavigationDelegate methods where possible.
-  [super setPageDialogOpenPolicy:policy];
-}
-
 - (void)close {
   [_certVerificationController shutDown];
   [super close];
@@ -696,10 +690,6 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
 
 - (void)resetLoadState {
   // Nothing to do.
-}
-
-- (void)setSuppressDialogsWithHelperScript:(NSString*)script {
-  [self evaluateJavaScript:script stringResultHandler:nil];
 }
 
 - (void)applyWebViewScrollZoomScaleFromZoomState:
@@ -1215,6 +1205,12 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
       [space.authenticationMethod isEqual:NSURLAuthenticationMethodHTTPBasic] ||
       [space.authenticationMethod isEqual:NSURLAuthenticationMethodNTLM] ||
       [space.authenticationMethod isEqual:NSURLAuthenticationMethodHTTPDigest]);
+
+  if (self.suppressDialogs) {
+    [self didSuppressDialog];
+    completionHandler(NSURLSessionAuthChallengeRejectProtectionSpace, nil);
+    return;
+  }
 
   SEL selector = @selector(webController:
          runAuthDialogForProtectionSpace:
@@ -1982,6 +1978,11 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
     createWebViewWithConfiguration:(WKWebViewConfiguration*)configuration
                forNavigationAction:(WKNavigationAction*)navigationAction
                     windowFeatures:(WKWindowFeatures*)windowFeatures {
+  if (self.suppressDialogs) {
+    [self didSuppressDialog];
+    return nil;
+  }
+
   GURL requestURL = net::GURLWithNSURL(navigationAction.request.URL);
 
   if (![self userIsInteracting]) {
@@ -2017,6 +2018,12 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
     runJavaScriptAlertPanelWithMessage:(NSString*)message
                       initiatedByFrame:(WKFrameInfo*)frame
                      completionHandler:(void(^)())completionHandler {
+  if (self.suppressDialogs) {
+    [self didSuppressDialog];
+    completionHandler();
+    return;
+  }
+
   SEL alertSelector = @selector(webController:
            runJavaScriptAlertPanelWithMessage:
                                    requestURL:
@@ -2036,6 +2043,12 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
                         initiatedByFrame:(WKFrameInfo*)frame
                        completionHandler:
         (void (^)(BOOL result))completionHandler {
+  if (self.suppressDialogs) {
+    [self didSuppressDialog];
+    completionHandler(NO);
+    return;
+  }
+
   SEL confirmationSelector = @selector(webController:
                 runJavaScriptConfirmPanelWithMessage:
                                           requestURL:
@@ -2057,6 +2070,12 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
                          initiatedByFrame:(WKFrameInfo*)frame
                         completionHandler:
         (void (^)(NSString *result))completionHandler {
+  if (self.suppressDialogs) {
+    [self didSuppressDialog];
+    completionHandler(nil);
+    return;
+  }
+
   SEL textInputSelector = @selector(webController:
             runJavaScriptTextInputPanelWithPrompt:
                                       defaultText:
