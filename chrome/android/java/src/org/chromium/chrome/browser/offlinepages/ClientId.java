4ee51a5a3218bbc8dd1dfa6b6f3de1185e1470fd
fgorski <fgorski@chromium.org>
2016-03-16 17:11:39 -0700

    [Offline pages] Fixing client ID from bookmark generation
    Adding a check for a null bookmark ID and tests.
    
    BUG=594896
    TBR=nyquist@chromium.org
    
    Review URL: https://codereview.chromium.org/1804213002
    
    Cr-Commit-Position: refs/heads/master@{#381597}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/ClientId.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/ClientId.java
@@ -47,6 +47,7 @@ public class ClientId {
      * @return A {@link ClientId} that represents this BookmarkId.
      */
     public static ClientId createClientIdForBookmarkId(BookmarkId id) {
+        if (id == null) return null;
         return new ClientId(OfflinePageBridge.BOOKMARK_NAMESPACE, id.toString());
     }
 }
