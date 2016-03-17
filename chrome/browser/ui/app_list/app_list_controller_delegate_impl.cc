e0bde3e6520aecf3db568987be316d5cc33d78a4
scottmg <scottmg@chromium.org>
2016-03-17 12:22:43 -0700

    Remove HostDesktopType from AppLaunchParams
    This one is a bit subtle. The two removed "helper" AppLaunchParams
    constructors were using HostDesktopType as an unrelated boolean to
    choose one or the other of the two constructors (unrelated to the actual
    value of HostDesktopType).
    
    I pulled those two out and had them use the now-single constructor, and
    tried to give the two names that made some sense according to their
    comments.
    
    R=benwells@chromium.org, sky@chromium.org
    BUG=558054, 587387, 586862
    
    Review URL: https://codereview.chromium.org/1807743003
    
    Cr-Commit-Position: refs/heads/master@{#381770}

--- chrome/browser/ui/app_list/app_list_controller_delegate_impl.cc
+++ chrome/browser/ui/app_list/app_list_controller_delegate_impl.cc
@@ -112,8 +112,8 @@ void AppListControllerDelegateImpl::LaunchApp(
     int event_flags) {
   AppListServiceImpl::RecordAppListAppLaunch();
 
-  AppLaunchParams params(profile, extension, NEW_FOREGROUND_TAB,
-                         extensions::SOURCE_APP_LAUNCHER);
+  AppLaunchParams params = CreateAppLaunchParamsUserContainer(
+      profile, extension, NEW_FOREGROUND_TAB, extensions::SOURCE_APP_LAUNCHER);
 
   if (source != LAUNCH_FROM_UNKNOWN &&
       extension->id() == extensions::kWebStoreAppId) {
@@ -125,7 +125,6 @@ void AppListControllerDelegateImpl::LaunchApp(
         AppListSourceToString(source));
   }
 
-  FillLaunchParams(&params);
   OpenApplication(params);
 }
 
@@ -138,8 +137,6 @@ void AppListControllerDelegateImpl::ShowForProfileByPath(
 bool AppListControllerDelegateImpl::ShouldShowUserIcon() {
   return g_browser_process->profile_manager()->GetNumberOfProfiles() > 1;
 }
-
-void AppListControllerDelegateImpl::FillLaunchParams(AppLaunchParams* params) {}
 
 void AppListControllerDelegateImpl::OnCloseCreateShortcutsPrompt(
     bool created) {
