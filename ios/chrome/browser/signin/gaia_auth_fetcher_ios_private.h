20d24662b61e94ce74dc4746772d7d6d231f7ef7
yyanagisawa <yyanagisawa@chromium.org>
2016-03-17 03:19:38 -0700

    gaia_auth_fetcher_ios_private.h: fix case for WebKit.h.
    In SDK, it is WebKit.framework/Versions/A/Headers/WebKit.h.
    Use the same case, so that it would work on case sensitive filesystem.
    
    Review URL: https://codereview.chromium.org/1801363004
    
    Cr-Commit-Position: refs/heads/master@{#381684}

--- ios/chrome/browser/signin/gaia_auth_fetcher_ios_private.h
+++ ios/chrome/browser/signin/gaia_auth_fetcher_ios_private.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_SIGNIN_GAIA_AUTH_FETCHER_IOS_PRIVATE_H_
 #define IOS_CHROME_BROWSER_SIGNIN_GAIA_AUTH_FETCHER_IOS_PRIVATE_H_
 
-#import <Webkit/Webkit.h>
+#import <WebKit/WebKit.h>
 
 #import "base/mac/scoped_nsobject.h"
 #include "base/macros.h"
