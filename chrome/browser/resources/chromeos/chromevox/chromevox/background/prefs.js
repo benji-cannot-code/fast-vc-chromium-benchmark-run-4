56fff5fa52728760f36e2061a00b5bcae26934d4
dtseng <dtseng@chromium.org>
2016-03-18 11:33:15 -0700

    Make ancestry and role announcements at the end of output.
    This de-prioritizes secondary info like ancestry changes and puts the primary name (usually) at the beginning.
    
    TEST=interactive_ui_tests and chromevox_tests
    
    Review URL: https://codereview.chromium.org/1807843004
    
    Cr-Commit-Position: refs/heads/master@{#382022}

--- chrome/browser/resources/chromeos/chromevox/chromevox/background/prefs.js
+++ chrome/browser/resources/chromeos/chromevox/chromevox/background/prefs.js
@@ -70,6 +70,7 @@ cvox.ChromeVoxPrefs.DEFAULT_PREFS = {
   'earcons': true,
   'focusFollowsMouse': false,
   'granularity': undefined,
+  'outputContextFirst': false,
   'position': '{}',
   'siteSpecificScriptBase':
       'https://ssl.gstatic.com/accessibility/javascript/ext/',
--- chrome/browser/resources/chromeos/chromevox/cvox2/background/output.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/output.js
@@ -424,8 +424,8 @@ Output.RULES = {
     },
     heading: {
       enter: '@tag_h+$hierarchicalLevel',
-      speak: '@tag_h+$hierarchicalLevel !relativePitch(hierarchicalLevel)' +
-          ' $nameOrDescendants='
+      speak: '!relativePitch(hierarchicalLevel)' +
+          ' $nameOrDescendants= @tag_h+$hierarchicalLevel'
     },
     inlineTextBox: {
       speak: '$name='
@@ -1237,8 +1237,12 @@ Output.prototype = {
 
     var formatNodeAndAncestors = function(node, prevNode) {
       var buff = [];
-      this.ancestry_(node, prevNode, type, buff);
+      var outputContextFirst = localStorage['outputContextFirst'] == 'true';
+      if (outputContextFirst)
+        this.ancestry_(node, prevNode, type, buff);
       this.node_(node, prevNode, type, buff);
+      if (!outputContextFirst)
+        this.ancestry_(node, prevNode, type, buff);
       if (node.location)
         this.locations_.push(node.location);
       return buff;
@@ -1378,9 +1382,13 @@ Output.prototype = {
             startIndex));
       }
     }
-    this.ancestry_(node, prevNode, type, buff);
+    var outputContextFirst = localStorage['outputContextFirst'] == 'true';
+    if (outputContextFirst)
+      this.ancestry_(node, prevNode, type, buff);
     this.append_(buff, range.start.getText().substring(startIndex, endIndex),
         options);
+    if (!outputContextFirst)
+      this.ancestry_(node, prevNode, type, buff);
 
     var loc =
         range.start.node.boundsForRange(startIndex, endIndex);
