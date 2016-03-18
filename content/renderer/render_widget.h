ecc6923375fc843031717b4b04cbbcee63d46fb7
avi <avi@chromium.org>
2016-03-18 13:37:48 -0700

    Remove GetScrollOffset from RenderWidget/View.
    It hasn't been used since r292721.
    
    BUG=583347
    TEST=all green
    
    Review URL: https://codereview.chromium.org/1816663002
    
    Cr-Commit-Position: refs/heads/master@{#382073}

--- content/renderer/render_widget.h
+++ content/renderer/render_widget.h
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/text_input_type.h"
 #include "ui/base/ui_base_types.h"
 #include "ui/gfx/geometry/rect.h"
-#include "ui/gfx/geometry/vector2d.h"
 #include "ui/gfx/geometry/vector2d_f.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/gfx/range/range.h"
@@ -509,10 +508,6 @@ class CONTENT_EXPORT RenderWidget
   virtual void DidFlushPaint();
 
   virtual GURL GetURLForGraphicsContext3D();
-
-  // Gets the scroll offset of this widget, if this widget has a notion of
-  // scroll offset.
-  virtual gfx::Vector2d GetScrollOffset();
 
   // Sets the "hidden" state of this widget.  All accesses to is_hidden_ should
   // use this method so that we can properly inform the RenderThread of our
