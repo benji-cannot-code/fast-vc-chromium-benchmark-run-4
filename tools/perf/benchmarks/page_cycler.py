69ec475854de74ef5f69cc548b9b58bb2f00abda
nednguyen <nednguyen@google.com>
2016-03-18 13:19:19 -0700

    Disable page_cycler.basic_oopif on zenbook bot
    BUG=596067, 595744
    
    Review URL: https://codereview.chromium.org/1815243002
    
    Cr-Commit-Position: refs/heads/master@{#382065}

--- tools/perf/benchmarks/page_cycler.py
+++ tools/perf/benchmarks/page_cycler.py
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
+
 from core import perf_benchmark
 
 from measurements import page_cycler
@@ -250,6 +252,11 @@ class PageCyclerBasicOopifIsolated(_PageCycler):
 
   def SetExtraBrowserOptions(self, options):
     options.AppendExtraBrowserArgs(['--site-per-process'])
+
+  @classmethod
+  def ShouldDisable(cls, possible_browser):  # http://crbug.com/596067
+    # This test is flaky on Zenbook laptops in the Chromium perf lab.
+    return 'Zenbook' in os.getenv('BUILDBOT_BUILDERNAME', '')
 
 
 @benchmark.Disabled('reference')  # crbug.com/523346
