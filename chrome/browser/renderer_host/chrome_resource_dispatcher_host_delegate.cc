72307b8646ec46bf3d63f46e939d8d7b870cf607
pasko <pasko@chromium.org>
2016-03-18 04:12:52 -0700

    Simplify checking for prefetch field trial in RDH
    BUG=589369
    
    Review URL: https://codereview.chromium.org/1800723002
    
    Cr-Commit-Position: refs/heads/master@{#381936}

--- chrome/browser/renderer_host/chrome_resource_dispatcher_host_delegate.cc
+++ chrome/browser/renderer_host/chrome_resource_dispatcher_host_delegate.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/guid.h"
 #include "base/logging.h"
+#include "base/metrics/field_trial.h"
+#include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
@@ -22,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/mod_pagespeed/mod_pagespeed_metrics.h"
 #include "chrome/browser/net/resource_prefetch_predictor_observer.h"
 #include "chrome/browser/plugins/plugin_prefs.h"
-#include "chrome/browser/prefetch/prefetch.h"
 #include "chrome/browser/prerender/prerender_manager.h"
 #include "chrome/browser/prerender/prerender_manager_factory.h"
 #include "chrome/browser/prerender/prerender_resource_throttle.h"
@@ -298,8 +299,12 @@ bool ChromeResourceDispatcherHostDelegate::ShouldBeginRequest(
       return false;
 
     // If prefetch is disabled, kill the request.
-    if (!prefetch::IsPrefetchEnabled(resource_context))
+    std::string prefetch_experiment =
+        base::FieldTrialList::FindFullName("Prefetch");
+    if (base::StartsWith(prefetch_experiment, "ExperimentDisable",
+                         base::CompareCase::INSENSITIVE_ASCII)) {
       return false;
+    }
   }
 
   return true;
