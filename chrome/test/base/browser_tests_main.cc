03e1b6a604cf7a018b3dbca69b7bb6a29f45883f
sky <sky@chromium.org>
2016-03-18 11:38:16 -0700

    Minor changes to LaunchChromeTests
    Makes LaunchChromeTests take a content::TestLauncherDelegate. I'm
    going to need a custom one shortly for browser tests and this lets me
    reuse LaunchChromeTests().
    I also moved ChromeTestLauncherDelegate to the header and made
    ChromeTestSuiteRunner have the implementation all subclasses were
    using.
    
    BUG=581733
    TEST=test only changes
    R=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/1806853009
    
    Cr-Commit-Position: refs/heads/master@{#382025}

--- chrome/test/base/browser_tests_main.cc
+++ chrome/test/base/browser_tests_main.cc
@@ -9,15 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/chrome_test_launcher.h"
 #include "chrome/test/base/chrome_test_suite.h"
 
-class ChromeBrowserTestSuiteRunner : public ChromeTestSuiteRunner {
- public:
-  int RunTestSuite(int argc, char** argv) override {
-    return ChromeTestSuite(argc, argv).Run();
-  }
-};
-
 int main(int argc, char** argv) {
   int default_jobs = std::max(1, base::SysInfo::NumberOfProcessors() / 2);
-  ChromeBrowserTestSuiteRunner runner;
-  return LaunchChromeTests(default_jobs, &runner, argc, argv);
+  ChromeTestSuiteRunner runner;
+  ChromeTestLauncherDelegate delegate(&runner);
+  return LaunchChromeTests(default_jobs, &delegate, argc, argv);
 }
