40ecb093487d8a8b3757eaddf4b797c1f8d45d96
avayvod <avayvod@chromium.org>
2016-03-18 09:56:18 -0700

    [Blink,RemotePlaybackAPI] Added a runtime flag for the Remote Playback API with a stub IDL implementation.
    Adds the |remote| property on HTMLMediaElement which is an event target and has a |state| attribute.
    
    BUG=578833
    TEST=manual with experimental flag enabled (and RuntimeFeatures having the api as experimental)
    
    Review URL: https://codereview.chromium.org/1765423003
    
    Cr-Commit-Position: refs/heads/master@{#381994}

--- third_party/WebKit/Source/modules/remoteplayback/HTMLMediaElementRemotePlayback.h
+++ third_party/WebKit/Source/modules/remoteplayback/HTMLMediaElementRemotePlayback.h
@@ -6,17 +6,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef HTMLMediaElementRemotePlayback_h
 #define HTMLMediaElementRemotePlayback_h
 
+#include "modules/ModulesExport.h"
+#include "platform/Supplementable.h"
+#include "platform/heap/Handle.h"
+
 namespace blink {
 
 class HTMLMediaElement;
 class QualifiedName;
+class RemotePlayback;
 
-// Class used to implement the Remote Playback API. It will be a supplement to
-// HTMLMediaElement later.
-class HTMLMediaElementRemotePlayback final {
+// Class used to implement the Remote Playback API. It is a supplement to
+// HTMLMediaElement.
+class HTMLMediaElementRemotePlayback final : public NoBaseWillBeGarbageCollected<HTMLMediaElementRemotePlayback>, public WillBeHeapSupplement<HTMLMediaElement>  {
+    WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(HTMLMediaElementRemotePlayback);
+    USING_FAST_MALLOC_WILL_BE_REMOVED(HTMLMediaElementRemotePlayback);
 public:
     static bool fastHasAttribute(const QualifiedName&, const HTMLMediaElement&);
     static void setBooleanAttribute(const QualifiedName&, HTMLMediaElement&, bool);
+
+    static HTMLMediaElementRemotePlayback& from(HTMLMediaElement&);
+    static RemotePlayback* remote(HTMLMediaElement&);
+
+    DECLARE_VIRTUAL_TRACE();
+
+private:
+    static const char* supplementName();
+
+    PersistentWillBeMember<RemotePlayback> m_remote;
 };
 
 } // namespace blink
