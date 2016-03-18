3b6033bc8dbb21258cc25593fa7c8cf52dc85527
xunjieli <xunjieli@chromium.org>
2016-03-18 13:28:37 -0700

    Fix Cronet compile error
    BidirectionalStreamRequestInfo is a struct not a class.
    
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    TBR=mef@chromium.org
    
    Review URL: https://codereview.chromium.org/1815343002
    
    Cr-Commit-Position: refs/heads/master@{#382069}

--- components/cronet/android/cronet_bidirectional_stream_adapter.h
+++ components/cronet/android/cronet_bidirectional_stream_adapter.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/bidirectional_stream.h"
 
 namespace net {
-class BidirectionalStreamRequestInfo;
+struct BidirectionalStreamRequestInfo;
 class SpdyHeaderBlock;
 }  // namespace net
 
