f57f4aa921496006c4b78ad89ed0035ee703e33c
davidben <davidben@chromium.org>
2016-03-18 14:21:27 -0700

    Revert "Add histograms to track down the cause of ERR_SSL_PROTOCOL_ERROR."
    This reverts commit 6e50b9f54970b1527162fba5611b8d1b41a9d9ba. The cause of the
    bug is known, so the metrics are no longer needed.
    
    BUG=593963
    
    Review URL: https://codereview.chromium.org/1800883002
    
    Cr-Commit-Position: refs/heads/master@{#382091}

--- net/socket/ssl_client_socket_openssl.h
+++ net/socket/ssl_client_socket_openssl.h
@@ -127,8 +127,6 @@ class SSLClientSocketOpenSSL : public SSLClientSocket {
   void DoReadCallback(int result);
   void DoWriteCallback(int result);
 
-  uint16_t GetCipherSuite();
-
   bool DoTransportIO();
   int DoHandshake();
   int DoHandshakeComplete(int result);
