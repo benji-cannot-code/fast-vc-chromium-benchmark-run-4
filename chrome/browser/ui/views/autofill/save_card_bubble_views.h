41b3dae68314d0abe6d7eed51afc6c626c6b6fa0
jdonnelly <jdonnelly@chromium.org>
2016-03-18 14:41:28 -0700

    Add close method to SaveCardBubbleViews to prevent cancellation.
    BUG=535784
    
    Review URL: https://codereview.chromium.org/1813403002
    
    Cr-Commit-Position: refs/heads/master@{#382096}

--- chrome/browser/ui/views/autofill/save_card_bubble_views.h
+++ chrome/browser/ui/views/autofill/save_card_bubble_views.h
@@ -47,6 +47,7 @@ class SaveCardBubbleViews : public SaveCardBubbleView,
   views::View* CreateFootnoteView() override;
   bool Accept() override;
   bool Cancel() override;
+  bool Close() override;
   int GetDialogButtons() const override;
   base::string16 GetDialogButtonLabel(ui::DialogButton button) const override;
   bool ShouldDefaultButtonBeBlue() const override;
