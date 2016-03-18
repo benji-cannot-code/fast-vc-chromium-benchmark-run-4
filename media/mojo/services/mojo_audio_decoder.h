34dbfc716d0e02b6035a2b1c966dd2badfe311c0
timav <timav@chromium.org>
2016-03-18 13:34:44 -0700

    Implement MojoAudioDecoder
    Added MojoAudioDecoder implementation for Initialize(), Decode() and Reset(). It does not accept the decoded audio buffers yet.
    
    BUG=542910
    
    Review URL: https://codereview.chromium.org/1815553002
    
    Cr-Commit-Position: refs/heads/master@{#382070}

--- media/mojo/services/mojo_audio_decoder.h
+++ media/mojo/services/mojo_audio_decoder.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "media/base/audio_decoder.h"
 #include "media/mojo/interfaces/audio_decoder.mojom.h"
+#include "mojo/public/cpp/bindings/binding.h"
 
 namespace base {
 class SingleThreadTaskRunner;
@@ -18,7 +19,8 @@ class SingleThreadTaskRunner;
 namespace media {
 
 // An AudioDecoder that proxies to an interfaces::AudioDecoder.
-class MojoAudioDecoder : public AudioDecoder {
+class MojoAudioDecoder : public AudioDecoder,
+                         public interfaces::AudioDecoderClient {
  public:
   MojoAudioDecoder(scoped_refptr<base::SingleThreadTaskRunner> task_runner,
                    interfaces::AudioDecoderPtr remote_decoder);
@@ -35,9 +37,40 @@ class MojoAudioDecoder : public AudioDecoder {
   void Reset(const base::Closure& closure) final;
   bool NeedsBitstreamConversion() const final;
 
+  // AudioDecoderClient implementation.
+  void OnBufferDecoded(interfaces::AudioBufferPtr buffer) final;
+
  private:
+  // Callback for connection error on |remote_decoder_|.
+  void OnConnectionError();
+
+  // Called when |remote_decoder_| finished initialization.
+  void OnInitialized(bool status, bool needs_bitstream_conversion);
+
+  // Called when |remote_decoder_| accepted or rejected DecoderBuffer.
+  void OnDecodeStatus(interfaces::AudioDecoder::DecodeStatus decode_status);
+
+  // called when |remote_decoder_| finished Reset() sequence.
+  void OnResetDone();
+
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+
   interfaces::AudioDecoderPtr remote_decoder_;
+
+  // Binding for AudioDecoderClient, bound to the |task_runner_|.
+  mojo::Binding<AudioDecoderClient> binding_;
+
+  // We call these callbacks to pass the information to the pipeline.
+  InitCB init_cb_;
+  DecodeCB decode_cb_;
+  base::Closure reset_cb_;
+
+  // Flag that is set if we got connection error. Never cleared.
+  bool has_connection_error_;
+
+  // Flag telling whether this decoder requires bitstream conversion.
+  // Passed from |remote_decoder_| as a result of its initialization.
+  bool needs_bitstream_conversion_;
 
   DISALLOW_COPY_AND_ASSIGN(MojoAudioDecoder);
 };
