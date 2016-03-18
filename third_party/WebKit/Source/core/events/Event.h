f15d2780a7c71d76d6b1b8b2361fe9ddff51c410
yuzus <yuzus@chromium.org>
2016-03-18 03:54:54 -0700

    Implement Event.relatedTargetScoped
    This CL implements Event.relatedTargetScoped flag, which is set to false by default and should be set to true only if the event is trusted and has a related target or if the user sets it to true.
    See the spec here: https://w3c.github.io/webcomponents/spec/shadow/#dfn-relatedtargetscoped-flag
    
    BUG=589782
    TBR=holte@chromium.org
    
    Review URL: https://codereview.chromium.org/1817453002
    
    Cr-Commit-Position: refs/heads/master@{#381929}

--- third_party/WebKit/Source/core/events/Event.h
+++ third_party/WebKit/Source/core/events/Event.h
@@ -125,6 +125,7 @@ public:
     bool bubbles() const { return m_canBubble; }
     bool cancelable() const { return m_cancelable; }
     bool scoped() const { return m_scoped; }
+    bool relatedTargetScoped() const { return m_relatedTargetScoped; }
 
     // Event creation timestamp in milliseconds. If |HiResEventTimeStamp|
     // runtime feature is enabled it returns a DOMHighResTimeStamp using the
@@ -208,9 +209,11 @@ public:
 protected:
     Event();
     Event(const AtomicString& type, bool canBubble, bool cancelable);
+    Event(const AtomicString& type, bool canBubble, bool cancelable, EventTarget* relatedTarget);
     Event(const AtomicString& type, bool canBubble, bool cancelable, double platformTimeStamp);
+    Event(const AtomicString& type, bool canBubble, bool cancelable, EventTarget* relatedTarget, double platformTimeStamp);
     Event(const AtomicString& type, bool canBubble, bool cancelable, bool scoped);
-    Event(const AtomicString& type, bool canBubble, bool cancelable, bool scoped, double platformTimeStamp);
+    Event(const AtomicString& type, bool canBubble, bool cancelable, bool scoped, bool relatedTargetScoped, double platformTimeStamp);
     Event(const AtomicString& type, const EventInit&);
 
     virtual void receivedTarget();
@@ -230,6 +233,7 @@ private:
     unsigned m_canBubble:1;
     unsigned m_cancelable:1;
     unsigned m_scoped:1;
+    unsigned m_relatedTargetScoped:1;
 
     unsigned m_propagationStopped:1;
     unsigned m_immediatePropagationStopped:1;
