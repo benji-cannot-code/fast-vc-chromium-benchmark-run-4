2aed0f1a2aff4270d7684cee14683b88e72be597
mohsen <mohsen@chromium.org>
2016-03-17 19:12:50 -0700

    mus: Allow SetModal to fail gracefully
    Mus server might prevent some windows from becoming system modals. In
    this case, SetModal would fail and the client needs to handle the
    failure and revert the modality set locally for the window.
    
    BUG=548402
    
    Review URL: https://codereview.chromium.org/1811913004
    
    Cr-Commit-Position: refs/heads/master@{#381871}

--- components/mus/public/cpp/lib/window_private.h
+++ components/mus/public/cpp/lib/window_private.h
@@ -63,6 +63,7 @@ class WindowPrivate {
   void LocalRemoveTransientWindow(Window* child) {
     window_->LocalRemoveTransientWindow(child);
   }
+  void LocalUnsetModal() { window_->is_modal_ = false; }
   void LocalReorder(Window* relative, mojom::OrderDirection direction) {
     window_->LocalReorder(relative, direction);
   }
