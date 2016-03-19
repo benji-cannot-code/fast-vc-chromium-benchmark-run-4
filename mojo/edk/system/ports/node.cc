f6ffddf9e703eed66b7fad8b8ee5de8ad2b5ce38
rockot <rockot@chromium.org>
2016-03-18 18:31:09 -0700

    [mojo-edk] Remove debug aliasing from ports layer
    This was used when tracking down a thread-safety issue
    a while ago. No longer needed. Also CHECK -> DCHECK.
    
    BUG=None
    R=amistry@chromium.org
    
    Review URL: https://codereview.chromium.org/1815403002
    
    Cr-Commit-Position: refs/heads/master@{#382169}

--- mojo/edk/system/ports/node.cc
+++ mojo/edk/system/ports/node.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string.h>
 
-#include "base/debug/alias.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
 #include "base/synchronization/lock.h"
@@ -1044,10 +1043,8 @@ int Node::WillSendMessage_Locked(Port* port,
 
     {
       for (size_t i = 0; i < message->num_ports(); ++i) {
-        PortName port_name = message->ports()[i];
-        base::debug::Alias(&port_name);
-        ports[i] = GetPort_Locked(port_name);
-        CHECK(ports[i]);
+        ports[i] = GetPort_Locked(message->ports()[i]);
+        DCHECK(ports[i]);
 
         ports[i]->lock.Acquire();
         int error = OK;
