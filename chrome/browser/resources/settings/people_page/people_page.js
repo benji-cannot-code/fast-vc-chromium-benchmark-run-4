ecd35697ae2e08916fa69de1a94708163652e3f5
tommycli <tommycli@chromium.org>
2016-03-18 17:29:53 -0700

    Settings People Revamp: Easy Unlock: Add the Proximity Detection option.
    Adds a checkbox under the Easy Unlock section for proximity detection.
    
    BUG=563721
    
    Review URL: https://codereview.chromium.org/1813233003
    
    Cr-Commit-Position: refs/heads/master@{#382139}

--- chrome/browser/resources/settings/people_page/people_page.js
+++ chrome/browser/resources/settings/people_page/people_page.js
@@ -47,13 +47,11 @@ Polymer({
 
     /**
      * The currently selected profile icon URL. May be a data URL.
-     * @private {string}
      */
     profileIconUrl_: String,
 
     /**
      * The current profile name.
-     * @private {string}
      */
     profileName_: String,
 
@@ -68,7 +66,6 @@ Polymer({
 
     /**
      * True if Easy Unlock is allowed on this machine.
-     * @private {boolean}
      */
     easyUnlockAllowed_: {
       type: Boolean,
@@ -80,13 +77,24 @@ Polymer({
 
     /**
      * True if Easy Unlock is enabled.
-     * @private {boolean}
      */
     easyUnlockEnabled_: {
       type: Boolean,
       value: function() {
         return loadTimeData.getBoolean('easyUnlockEnabled');
       },
+    },
+
+    /**
+     * True if Easy Unlock's proximity detection feature is allowed.
+     */
+    easyUnlockProximityDetectionAllowed_: {
+      type: Boolean,
+      value: function() {
+        return loadTimeData.getBoolean('easyUnlockAllowed') &&
+            loadTimeData.getBoolean('easyUnlockProximityDetectionAllowed');
+      },
+      readOnly: true,
     },
 </if>
   },
