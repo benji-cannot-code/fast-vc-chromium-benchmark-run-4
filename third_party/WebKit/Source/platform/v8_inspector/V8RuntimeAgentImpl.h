498b211fb9045b6436a79d3a3a8f12592e1a184b
kozyatinskiy <kozyatinskiy@chromium.org>
2016-03-18 17:26:35 -0700

    [DevTools] Move callFunctionOn to native
    BUG=595206
    R=dgozman@chromium.org
    
    Review URL: https://codereview.chromium.org/1809073003
    
    Cr-Commit-Position: refs/heads/master@{#382134}

--- third_party/WebKit/Source/platform/v8_inspector/V8RuntimeAgentImpl.h
+++ third_party/WebKit/Source/platform/v8_inspector/V8RuntimeAgentImpl.h
@@ -40,6 +40,7 @@ namespace blink {
 
 class InjectedScript;
 class InjectedScriptManager;
+class RemoteObjectIdBase;
 class V8DebuggerImpl;
 
 namespace protocol {
@@ -130,6 +131,8 @@ public:
     void addInspectedObject(PassOwnPtr<Inspectable>) override;
     void clearInspectedObjects() override;
 private:
+    v8::MaybeLocal<v8::Value> evaluateInternal(InjectedScript*, bool doNotPauseOnExceptionsAndMuteConsole, const String& expression, v8::MaybeLocal<v8::Object> extension);
+
     int m_contextGroupId;
     protocol::DictionaryValue* m_state;
     protocol::Frontend::Runtime* m_frontend;
