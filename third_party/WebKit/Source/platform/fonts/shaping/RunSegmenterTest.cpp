9edb35feee0c6191d1fac647d580f8d5d0e6c08a
jshin <jshin@chromium.org>
2016-03-19 22:39:49 -0700

    Revert of Update ICU to 56.1 + local changes from 54.1 (patchset #8 id:140001 of https://codereview.chromium.org/1650073002/ )
    Reason for revert:
    ICU 56.1 introduced a new static initializer (RANGE_MAKER in range.cpp). A regression in si is not acceptable.
    
    It appears trivial to fix this issue (I don't see a reason to use a static UnicodeString for RANGE_MAKER). I'll reland after fixing it.
    
    When reverting, TestExpectation has to be kept because rebaseline bot already went through a cycle.
    
    Original issue's description:
    > Roll ICU to 56.1 + local changes from ICU 54.1
    >
    > https://chromium.googlesource.com/chromium/deps/icu.git/+log/8d342a4..8a6c927b
    >
    > ICU 56 brings us, among a number of changes,
    >
    > - Unicode 8.0 (from Unicode 7.0)
    > - CLDR 28 (from CLDR 26): better and up to date locale data
    > - Reduced data size thanks to improved string sharing: This helps Chrome's icu data file size under control despite a large increase in the number of locale strings.
    > - Formatter creation perf
    > - Better caching of formatting objects
    > - New line break iterator variants for CSS3
    > - Collator perf improvement
    > - A lot of bug fixes (security or others) some of which are from Chromium.
    >
    > For more details, see
    > http://site.icu-project.org/download/56 for 56.1 release
    > http://site.icu-project.org/download/55 for 55.1 release
    >
    > data size change from ICU 54.1
    >
    > - CrOS, Linux, Win, Mac: 10,207,936 => 10,124,096 (delta=-83,840)
    >
    > - Android: 6,268,240 => 6,560,080 (delta=291,840, 4.7%)
    >
    > Android data size gets larger because savings from string sharing didn't make up for the locale data size increase from CLDR update.
    >
    > In addition to rolling ICU to 0c9a780 (56.1 + local changes) from
    > ICU 54.1, ScriptRun test was adjusted as well as the expected result
    > for Russian short month names.
    >
    > Noto to Sheriff: The above data size changes (reduction or increase) may require the expected size adjustment. See https://codereview.chromium.org/403723003 for a similar change.
    >
    > CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    >
    > BUG=575007
    > TEST=Blink layout tests, {webkit,blink_*}_unittests
    > TEST=All bots can build successfully
    > TEST=net_unittests --gtest_filter="*ilenameUtil*"
    > TEST=net_unittests --gtest_filter="*IDN*" (pending bug 336973)
    > TEST=components_unittests --gtest_filter={*IDN*, *unyc*}
    > TEST=base_unittests --gtest_filter="*Conv*"
    > TEST=browser_tests --gtest_filter="*ncoding*"
    > TEST=base_unittests --gtest_filter="*essage*"
    > TEST=ui_base_unittests --gtest_filter="*ormat*"
    > TEST=ui_base_unittests --gtest_filter="L10n*"
    >
    > Committed: https://crrev.com/c49e2637e2e0e6a29968debcc92cc0fbe208e98d
    > Cr-Commit-Position: refs/heads/master@{#382201}
    
    TBR=drott@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=575007
    
    Review URL: https://codereview.chromium.org/1823483002
    
    Cr-Commit-Position: refs/heads/master@{#382216}

--- third_party/WebKit/Source/platform/fonts/shaping/RunSegmenterTest.cpp
+++ third_party/WebKit/Source/platform/fonts/shaping/RunSegmenterTest.cpp
@@ -213,7 +213,7 @@ TEST_F(RunSegmenterTest, JapaneseSmallCaps)
 
 TEST_F(RunSegmenterTest, DingbatsMiscSymbolsModifier)
 {
-    CHECK_RUNS_HORIZONTAL_NORMAL({ { "⛹🏻✍🏻✊🏼", USCRIPT_COMMON, OrientationIterator::OrientationKeep, SmallCapsIterator::SmallCapsSameCase, FontFallbackPriority::EmojiEmoji } });
+    CHECK_RUNS_HORIZONTAL_NORMAL({ { "⛹🏻✍🏻✊🏼", USCRIPT_UNKNOWN, OrientationIterator::OrientationKeep, SmallCapsIterator::SmallCapsSameCase, FontFallbackPriority::EmojiEmoji } });
 }
 
 
