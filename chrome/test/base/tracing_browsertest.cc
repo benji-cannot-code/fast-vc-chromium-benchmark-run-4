cb276d98432d0a1a7353fed361e2e411be91d37f
phoglund <phoglund@chromium.org>
2016-03-21 08:17:25 -0700

    Disabling flaky SingleProcessTracingBrowserTest.TestMemoryInfra
    TBR=hiroshige@chromium.org
    BUG=594884
    
    Review URL: https://codereview.chromium.org/1819883002
    
    Cr-Commit-Position: refs/heads/master@{#382289}

--- chrome/test/base/tracing_browsertest.cc
+++ chrome/test/base/tracing_browsertest.cc
@@ -165,7 +165,8 @@ IN_PROC_BROWSER_TEST_F(TracingBrowserTest, BeginTracingWithWatch) {
 }
 
 // Multi-process mode.
-IN_PROC_BROWSER_TEST_F(TracingBrowserTest, TestMemoryInfra) {
+// Flaky on win and Linux: https://crbug.com/594884.
+IN_PROC_BROWSER_TEST_F(TracingBrowserTest, DISABLED_TestMemoryInfra) {
   PerformDumpMemoryTestActions();
 }
 
