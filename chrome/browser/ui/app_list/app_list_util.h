bfabba74b0981baef4747736079e5e8731a50e02
tapted <tapted@chromium.org>
2016-03-20 18:34:14 -0700

    Disable the app launcher promo on chrome://apps
    BUG=576887
    
    Review URL: https://codereview.chromium.org/1811163003
    
    Cr-Commit-Position: refs/heads/master@{#382242}

--- chrome/browser/ui/app_list/app_list_util.h
+++ chrome/browser/ui/app_list/app_list_util.h
@@ -6,13 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_APP_LIST_APP_LIST_UTIL_H_
 #define CHROME_BROWSER_UI_APP_LIST_APP_LIST_UTIL_H_
 
-#include "base/callback_forward.h"
-
-class PrefService;
-
-// Does initializiation for the ShowAppLauncherPromo field trial.
-void SetupShowAppLauncherPromoFieldTrial(PrefService* local_state);
-
 // Returns whether the app launcher has been enabled.
 bool IsAppLauncherEnabled();
 
