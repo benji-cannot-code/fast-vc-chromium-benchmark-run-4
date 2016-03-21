80a8af0e989cf71e8ae982894630db5c8adf6c83
schenney <schenney@chromium.org>
2016-03-21 09:02:40 -0700

    Consolidate background fill layer opaqueness into one method
    Move all the logic for determining if a background layer is
    opaque into FillLayer. Also add repeat round as a opaque
    tiling mode.
    
    R=trchen@chromium.org
    BUG=593437
    
    Review URL: https://codereview.chromium.org/1787733003
    
    Cr-Commit-Position: refs/heads/master@{#382301}

--- third_party/WebKit/Source/core/style/FillLayer.h
+++ third_party/WebKit/Source/core/style/FillLayer.h
@@ -176,7 +176,7 @@ public:
         return m_next ? m_next->hasFixedImage() : false;
     }
 
-    bool hasOpaqueImage(const LayoutObject*) const;
+    bool imageOccludesNextLayers(const LayoutObject&) const;
     bool hasRepeatXY() const;
     bool clipOccludesNextLayers() const;
 
@@ -211,6 +211,9 @@ private:
     friend class ComputedStyle;
 
     FillLayer() { }
+
+    bool imageIsOpaque(const LayoutObject&) const;
+    bool imageTilesLayer() const;
 
     FillLayer* m_next;
 
