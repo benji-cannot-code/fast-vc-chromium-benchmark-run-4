42a2ece8a2e8d14caf434ff57c1d416133f6a318
charliea <charliea@chromium.org>
2016-03-21 11:12:11 -0700

    battor agent: Make timeout per-action rather than per-command
    Actions are internal implementation details of the BattOrAgent: reading
    a serial data frame is an action.
    
    Commands make up the interface of the BattOrAgent: StopTracing is a
    command.
    
    The problem with having a per-command timeout is that StopTracing can
    take a really long time to run if the trace has been collecting data
    for a while. StopTracing always takes 1/6 of the trace runtime to
    download, so any timeout we could add would be violated by running a
    trace for >6x that long.
    
    Now, as long as the BattOrAgent is making progress towards the
    completion of a command, it won't time out.
    
    BUG=585532,585533
    
    Review URL: https://codereview.chromium.org/1815323005
    
    Cr-Commit-Position: refs/heads/master@{#382328}

--- tools/battor_agent/battor_agent.h
+++ tools/battor_agent/battor_agent.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 
+#include "base/cancelable_callback.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
@@ -73,6 +74,12 @@ class BattOrAgent : public BattOrConnection::Listener,
   // fake in testing.
   scoped_ptr<BattOrConnection> connection_;
 
+  // Timeout for when an action isn't completed within the allotted time. This
+  // is virtual and protected so that timeouts can be disabled in testing. The
+  // testing task runner that runs delayed tasks immediately deals poorly with
+  // timeouts posted as future tasks.
+  virtual void OnActionTimeout();
+
  private:
   enum class Command {
     INVALID,
@@ -112,6 +119,8 @@ class BattOrAgent : public BattOrConnection::Listener,
   void PerformAction(Action action);
   // Performs an action after a delay.
   void PerformDelayedAction(Action action, base::TimeDelta delay);
+
+
 
   // Requests a connection to the BattOr.
   void BeginConnect();
@@ -167,6 +176,9 @@ class BattOrAgent : public BattOrConnection::Listener,
 
   // The number of times that we've attempted to read the last message.
   uint8_t num_read_attempts_;
+
+  // The timeout that's run when an action times out.
+  base::CancelableClosure timeout_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(BattOrAgent);
 };
