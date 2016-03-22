64f652004ef3be0182914ef2991435960b649c4d
fsamuel <fsamuel@chromium.org>
2016-03-22 12:41:58 -0700

    gpu_host_messages => content/common
    content/browser cannot depend on content/gpu because that pulls in blink.
    
    Thus, this CL moves gpu_host_messages.h and dependencies to content/common
    
    BUG=596935
    
    Review URL: https://codereview.chromium.org/1820793005
    
    Cr-Commit-Position: refs/heads/master@{#382648}

--- content/common/establish_channel_params.h
+++ content/common/establish_channel_params.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_GPU_ESTABLISH_CHANNEL_PARAMS_H_
-#define CONTENT_GPU_ESTABLISH_CHANNEL_PARAMS_H_
+#ifndef CONTENT_COMMON_ESTABLISH_CHANNEL_PARAMS_H_
+#define CONTENT_COMMON_ESTABLISH_CHANNEL_PARAMS_H_
 
 #include <stdint.h>
 
@@ -25,4 +25,4 @@ struct CONTENT_EXPORT EstablishChannelParams {
 
 }  // namespace content
 
-#endif  // CONTENT_GPU_ESTABLISH_CHANNEL_PARAMS_H_
+#endif  // CONTENT_COMMON_ESTABLISH_CHANNEL_PARAMS_H_
