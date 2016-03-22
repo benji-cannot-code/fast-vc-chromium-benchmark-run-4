bbb4e162e00fe1889a2b748ff1b37464cae9c15b
nhiroki <nhiroki@chromium.org>
2016-03-21 17:26:26 -0700

    ServiceWorker: Release a reference when it fails to dispatch ExtendableMessageEvent
    This CL makes sure to release a reference to ServiceWorkerHandle that was
    supposed to be sent to the destination.
    
    Note: ExtendableMessageEvent is still behind a flag and resource leaks due to
    this have not happened in release builds.
    
    BUG=543198
    
    Review URL: https://codereview.chromium.org/1799413002
    
    Cr-Commit-Position: refs/heads/master@{#382455}

--- content/browser/service_worker/embedded_worker_test_helper.h
+++ content/browser/service_worker/embedded_worker_test_helper.h
@@ -22,8 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
-struct EmbeddedWorkerMsg_StartWorker_Params;
 class GURL;
+struct EmbeddedWorkerMsg_StartWorker_Params;
+struct ServiceWorkerMsg_ExtendableMessageEvent_Params;
 
 namespace content {
 
@@ -31,11 +32,11 @@ class EmbeddedWorkerRegistry;
 class EmbeddedWorkerTestHelper;
 class MessagePortMessageFilter;
 class MockRenderProcessHost;
-struct PushEventPayload;
 class ServiceWorkerContextCore;
 class ServiceWorkerContextWrapper;
-struct ServiceWorkerFetchRequest;
 class TestBrowserContext;
+struct PushEventPayload;
+struct ServiceWorkerFetchRequest;
 
 // In-Process EmbeddedWorker test helper.
 //
@@ -81,7 +82,7 @@ class EmbeddedWorkerTestHelper : public IPC::Sender,
 
   int GetNextThreadId() { return next_thread_id_++; }
 
-  int mock_render_process_id() const { return mock_render_process_id_;}
+  int mock_render_process_id() const { return mock_render_process_id_; }
   MockRenderProcessHost* mock_render_process_host() {
     return render_process_host_.get();
   }
@@ -123,6 +124,7 @@ class EmbeddedWorkerTestHelper : public IPC::Sender,
   // worker. By default they just return success via
   // SimulateSendReplyToBrowser.
   virtual void OnActivateEvent(int embedded_worker_id, int request_id);
+  virtual void OnExtendableMessageEvent(int embedded_worker_id, int request_id);
   virtual void OnInstallEvent(int embedded_worker_id, int request_id);
   virtual void OnFetchEvent(int embedded_worker_id,
                             int request_id,
@@ -154,6 +156,9 @@ class EmbeddedWorkerTestHelper : public IPC::Sender,
                              int embedded_worker_id,
                              const IPC::Message& message);
   void OnActivateEventStub(int request_id);
+  void OnExtendableMessageEventStub(
+      int request_id,
+      const ServiceWorkerMsg_ExtendableMessageEvent_Params& params);
   void OnInstallEventStub(int request_id);
   void OnFetchEventStub(int request_id,
                         const ServiceWorkerFetchRequest& request);
