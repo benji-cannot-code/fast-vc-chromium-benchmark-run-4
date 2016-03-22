d28039497d5df487905b5dae610b496c00528151
jbudorick <jbudorick@chromium.org>
2016-03-22 10:07:31 -0700

    [Android] Fix various sign-in tests on M.
    This includes:
      - ParametersOnMultiTest#testActivityIsNotSignedInOnAppOrFakeOSorGoogleOS
      - ParametersOnMultiTest#testIsSignedInOnApp
      - ParametersOnMultiTest#testIsSignedInOnFakeOS
      - ParametersOnMultiTest#testIsSignedInOnFakeOSandApp
      - ChromeSigninUtilsTest#testActivityIsNotSignedInOnAppOrFakeOSorGoogleOS
      - ChromeSigninUtilsTest#testIsSignedInOnApp
      - ChromeSigninUtilsTest#testIsSignedInOnFakeOS
      - ChromeSigninUtilsTest#testIsSignedInOnFakeOSandApp
      - SigninParametersTest#testActivityIsNotSignedInOnAppOrFakeOSorGoogleOS
      - SigninParametersTest#testIsSignedInOnFakeOS
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1823433002
    
    Cr-Commit-Position: refs/heads/master@{#382590}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/ChromeSigninUtils.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/ChromeSigninUtils.java
@@ -48,7 +48,7 @@ public class ChromeSigninUtils {
     public ChromeSigninUtils(Instrumentation instrumentation) {
         mContext = instrumentation.getContext();
         mTargetContext = instrumentation.getTargetContext();
-        mAccountManager = AccountManager.get(mContext);
+        mAccountManager = AccountManager.get(mTargetContext);
         mMockAccountManager = new MockAccountManager(mContext, mTargetContext);
     }
 
