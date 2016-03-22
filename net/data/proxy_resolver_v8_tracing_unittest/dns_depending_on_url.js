f385b6cb0fb0086be9c89df882f260532fdc0d28
eroman <eroman@chromium.org>
2016-03-22 12:39:07 -0700

    Add a histogram (Net.PacResultForStrippedUrl) that estimates how often PAC scripts depend on the URL path.
    BUG=593759
    
    Review URL: https://codereview.chromium.org/1797313003
    
    Cr-Commit-Position: refs/heads/master@{#382644}

---
+++ net/data/proxy_resolver_v8_tracing_unittest/dns_depending_on_url.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This proxy script has different DNS dependencies based on whether the URL
+// contains "UseMyIpAddress". The final proxy list however is the same.
+function FindProxyForURL(url, host) {
+  if (url.indexOf("UseMyIpAddress") == -1) {
+    if (!myIpAddress())
+      return null;
+  } else {
+    if (!dnsResolve(host))
+      return null;
+  }
+
+  return "PROXY foopy:47";
+}
