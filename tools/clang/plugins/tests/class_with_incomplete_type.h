c7466a3f2771d25f9639efafa52f1b2bfe4a7e30
dcheng <dcheng@chromium.org>
2016-03-22 14:56:38 -0700

    chrome style plugin: Don't crash when checking class weight.
    It's impossible to determine if an incomplete type has a trivial
    destructor when counting the 'weight' of a class' fields, so check if
    the type has a definition first.
    
    BUG=595786
    
    Review URL: https://codereview.chromium.org/1816283003
    
    Cr-Commit-Position: refs/heads/master@{#382695}

---
+++ tools/clang/plugins/tests/class_with_incomplete_type.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Make sure that the plugin doesn't crash when trying to calculate the weight
+// of a class that has fields of incomplete type.
+
+#ifndef CLASS_WITH_INCOMPLETE_TYPE_H_
+#define CLASS_WITH_INCOMPLETE_TYPE_H_
+
+struct B;
+
+struct A {
+  ~A();
+  B incomplete;
+};
+
+#endif  // CLASS_WITH_INCOMPLETE_TYPE_H_
