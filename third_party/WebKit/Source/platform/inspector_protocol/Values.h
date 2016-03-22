752fca22ecf55194db891fbe0e7dd9a1c6e32aa1
blundell <blundell@chromium.org>
2016-03-22 09:19:46 -0700

    Reland of DevTools: Select max resource size in NetworkResourcesData depending on device capabilities. (patchset #1 id:1 of https://codereview.chromium.org/1825033002/ )
    Reason for revert:
    Reland the original CL as it's not the culprit ( https://build.chromium.org/p/chromium.webkit/buildstatus?builder=WebKit%20Win7%20%28dbg%29&number=5017, which includes the revert, still has the test failure)
    
    Original issue's description:
    > Revert of DevTools: Select max resource size in NetworkResourcesData depending on device capabilities. (patchset #1 id:1 of https://codereview.chromium.org/1817523003/ )
    >
    > Reason for revert:
    > Speculative revert to see if it fixes edit-dom-actions.html on Win7 webkit bots, which started timing out with no useful output on both Oilpan and non-Oilpan on runs including this CL (e.g., https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win7%20%28dbg%29/builds/5007).
    >
    > Original issue's description:
    > > DevTools: Select max resource size in NetworkResourcesData depending on device capabilities.
    > >
    > > BUG=587982
    > >
    > > Committed: https://crrev.com/ee50736c96ca97939d5b99543f3e0b297fd88029
    > > Cr-Commit-Position: refs/heads/master@{#382325}
    >
    > TBR=dgozman@chromium.org,pfeldman@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=587982
    >
    > Committed: https://crrev.com/6592b9dbd6050a2e7b9f9f0174dc133d4647309b
    > Cr-Commit-Position: refs/heads/master@{#382556}
    
    TBR=dgozman@chromium.org,pfeldman@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=587982
    
    Review URL: https://codereview.chromium.org/1821103003
    
    Cr-Commit-Position: refs/heads/master@{#382581}

--- third_party/WebKit/Source/platform/inspector_protocol/Values.h
+++ third_party/WebKit/Source/platform/inspector_protocol/Values.h
@@ -169,6 +169,7 @@ public:
     Entry at(size_t index) const;
 
     bool booleanProperty(const String16& name, bool defaultValue) const;
+    double numberProperty(const String16& name, double defaultValue) const;
     void remove(const String16& name);
 
     ~DictionaryValue() override;
