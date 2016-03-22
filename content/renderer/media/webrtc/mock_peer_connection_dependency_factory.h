87ad6eb99db06370609c14a6c6e461f4770a682f
nisse <nisse@chromium.org>
2016-03-22 07:27:52 -0700

    Drop include of webrtc/media/base/videorenderer.h.
    BUG=webrtc:5426
    
    Review URL: https://codereview.chromium.org/1824893003
    
    Cr-Commit-Position: refs/heads/master@{#382568}

--- content/renderer/media/webrtc/mock_peer_connection_dependency_factory.h
+++ content/renderer/media/webrtc/mock_peer_connection_dependency_factory.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "content/renderer/media/webrtc/peer_connection_dependency_factory.h"
 #include "third_party/webrtc/api/mediaconstraintsinterface.h"
-#include "third_party/webrtc/media/base/videorenderer.h"
 #include "third_party/webrtc/media/base/videosinkinterface.h"
 
 namespace content {
