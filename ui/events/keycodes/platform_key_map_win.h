7fab1bde810ba11d92be723656815a0cb3ab3cd7
chongz <chongz@chromium.org>
2016-03-22 08:33:12 -0700

    [Windows] Produce correct DomKey for NumPad when combined with Shift/NumLock
    The expected behavior for Shift, NumLock and NumPad should be (use NumPad4 as an example):
      NumPad4 => "ArrowLeft"
      Shift+NumPad4 => "ArrowLeft"
      NumLock+NumPad4 => "4"
      Shift+NumLock+NumPad4 => "ArrowLeft"
    
    However there is an implementation problem due to Windows' fake-shift-release behaviour, which means we cannot use DomCode+Flags to uniquely determine DomKey.
    (Cannot distinguish NumLock+NumPad4 and Shift+NumLock+NumPad4)
    
    The alternative approach is to use KeyboardCode instead DomCode because Shift modifier is not reliable
    when NumLock is on.
    
    /*
    Example for Windows' fake-shift-release behaviour:
      With NumLock on,
    	1. press Shift
    	2. press NumPad4
    	3. release NumPad4
    	4. release Shift
    will produce:
    -----
    EventType	DomCode		Shift	NumLock
    -----
    keydown 	ShiftLeft	true	true
    keyup		ShiftLeft	false	true
    keydown		Numpad4		false	true  <- Shift is false?!
    keyup		NumPad4		false	true
    keydown		ShiftLeft	true	true
    keyup		ShiftLeft	false	true
    */
    
    BUG=594552
    
    Review URL: https://codereview.chromium.org/1776673007
    
    Cr-Commit-Position: refs/heads/master@{#382574}

--- ui/events/keycodes/platform_key_map_win.h
+++ ui/events/keycodes/platform_key_map_win.h
@@ -10,8 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <unordered_map>
 
+#include "base/event_types.h"
 #include "base/hash.h"
-#include "ui/events/events_base_export.h"
+#include "ui/events/events_export.h"
 #include "ui/events/keycodes/dom/dom_key.h"
 #include "ui/events/keycodes/keyboard_codes_win.h"
 
@@ -19,26 +20,30 @@ namespace ui {
 
 enum class DomCode;
 
-class EVENTS_BASE_EXPORT PlatformKeyMap {
+class EVENTS_EXPORT PlatformKeyMap {
  public:
   // Create and load key map table with specified keyboard layout.
+  // Visible for testing.
   explicit PlatformKeyMap(HKL layout);
   ~PlatformKeyMap();
 
-  // Returns the DomKey 'meaning' of |code| in the context of specified
-  // |ui_event_flags| and stored keyboard layout.
-  DomKey DomCodeAndFlagsToDomKey(DomCode code,
-                                 int ui_event_flags) const;
-
-  // Returns the DomKey 'meaning' of |code| in the context of specified
-  // |ui_event_flags| and the keyboard layout of current thread.
+  // Returns the DOM KeyboardEvent key from a native event and the keyboard
+  // layout of current thread.
   // Updates a per-thread key map cache whenever the layout changes.
-  static DomKey DomCodeAndFlagsToDomKeyStatic(
-      DomCode code,
-      int ui_event_flags);
+  static DomKey DomKeyFromNative(const base::NativeEvent& native_event);
 
  private:
+  friend class PlatformKeyMapTest;
+
   PlatformKeyMap();
+
+  // TODO(chongz): Expose this function when we need to access separate layout.
+  // Returns the DomKey 'meaning' of |code| in the context of specified
+  // |ui_event_flags| and stored keyboard layout.
+  // |key_code| will only be used for NumPad.
+  DomKey DomKeyFromNativeImpl(DomCode code,
+                              KeyboardCode key_code,
+                              int ui_event_flags) const;
 
   // TODO(chongz): Expose this function in response to WM_INPUTLANGCHANGE.
   void UpdateLayout(HKL layout);
