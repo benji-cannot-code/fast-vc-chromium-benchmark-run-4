c8656120a190f66e236c522b82fb334976532935
fmalita <fmalita@chromium.org>
2016-03-22 14:16:43 -0700

    Propagate the decoder frame premul info to SkBitmap
    Currently ImageFrame always sets up the resulting SkBitmap as
    kPremul_SkAlphaType (or kOpaque_SkAlphaType in some cases), even
    when AlphaOption == AlphaNotPremultiplied.
    
    Update to set the correct bitmap alpha type.
    
    R=noel@chromium.org,reed@google.com,xidachen@chromium.org
    BUG=597035
    
    Review URL: https://codereview.chromium.org/1820733004
    
    Cr-Commit-Position: refs/heads/master@{#382680}

--- third_party/WebKit/Source/platform/image-decoders/ImageFrame.h
+++ third_party/WebKit/Source/platform/image-decoders/ImageFrame.h
@@ -200,6 +200,8 @@ private:
         return m_bitmap.height();
     }
 
+    SkAlphaType computeAlphaType() const;
+
     SkBitmap m_bitmap;
     SkBitmap::Allocator* m_allocator;
     bool m_hasAlpha;
