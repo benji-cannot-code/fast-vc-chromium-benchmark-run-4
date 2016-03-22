d0cffc6e7bd41bb58ec259d13852faced0a0c73d
hayato <hayato@chromium.org>
2016-03-21 22:38:56 -0700

    Introduce DocumentOrShadowRoot Mixin
    The spec is:
    - https://dom.spec.whatwg.org/#documentorshadowroot
    - http://w3c.github.io/webcomponents/spec/shadow/#extensions-to-the-documentorshadowroot-mixin
    
    BUG=595600
    
    Review URL: https://codereview.chromium.org/1815643002
    
    Cr-Commit-Position: refs/heads/master@{#382515}

---
+++ third_party/WebKit/Source/core/dom/DocumentOrShadowRoot.h
@@ -0,0 +1,54 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DocumentOrShadowRoot_h
+#define DocumentOrShadowRoot_h
+
+#include "core/dom/Document.h"
+#include "core/dom/shadow/ShadowRoot.h"
+
+namespace blink {
+
+class DocumentOrShadowRoot {
+public:
+    static Element* activeElement(Document& document)
+    {
+        return document.activeElement();
+    }
+
+    static Element* activeElement(ShadowRoot& shadowRoot)
+    {
+        return shadowRoot.activeElement();
+    }
+
+    static StyleSheetList* styleSheets(Document& document)
+    {
+        return document.styleSheets();
+    }
+
+    static StyleSheetList* styleSheets(ShadowRoot& shadowRoot)
+    {
+        return shadowRoot.styleSheets();
+    }
+
+    static DOMSelection* getSelection(TreeScope& treeScope)
+    {
+        return treeScope.getSelection();
+    }
+
+    static Element* elementFromPoint(TreeScope& treeScope, int x, int y)
+    {
+        return treeScope.elementFromPoint(x, y);
+    }
+
+    static WillBeHeapVector<RawPtrWillBeMember<Element>> elementsFromPoint(TreeScope& treeScope, int x, int y)
+    {
+        return treeScope.elementsFromPoint(x, y);
+    }
+};
+
+} // namespace blink
+
+#endif // DocumentOrShadowRoot_h
