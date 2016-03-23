6ef925f90ed528e4e9a832d2ebe32d5c3c8b9234
newt <newt@chromium.org>
2016-03-22 19:55:14 -0700

    Revert of Quick fix for broken JS notation (patchset #1 id:1 of https://codereview.chromium.org/1826603002/ )
    Reason for revert:
    Caused compile failure on 64-bit Android bots. See crbug.com/597155
    
    Original issue's description:
    > Quick fix for broken JS notation
    >
    > Fix for issue: https://codereview.chromium.org/1811833002
    >
    > TBR=sergeyu@chromium.org
    > BUG=
    >
    > Committed: https://crrev.com/bd12932cbd6656a2de7ff0a639b01364dddac013
    > Cr-Commit-Position: refs/heads/master@{#382714}
    
    TBR=sergeyu@chromium.org,lambroslambrou@chromium.org,yuweih@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review URL: https://codereview.chromium.org/1830483002
    
    Cr-Commit-Position: refs/heads/master@{#382776}

--- remoting/webapp/base/js/connection_stats.js
+++ remoting/webapp/base/js/connection_stats.js
@@ -124,7 +124,7 @@ remoting.ConnectionStats.prototype.update = function(stats) {
   /**
    * @param {number} value
    * @param {string} units
-   * @param {number} digits
+   * @param {number} number of digits after decimal.
    * @return {string} Formatted number.
    */
   function formatStatNumber(value, units, digits) {
