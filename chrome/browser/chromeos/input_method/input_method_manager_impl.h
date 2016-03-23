e363a95bd15adcbfeab021972112ede0d6fecee7
azurewei <azurewei@chromium.org>
2016-03-23 03:49:03 -0700

    The expanded IME menu is only supported under the 'normal' screen type. When switching into 'login' or 'lock' screen, we should always use the default IME menu in the status tray.
    BUG=570761
    TEST=None
    
    Review URL: https://codereview.chromium.org/1820223002
    
    Cr-Commit-Position: refs/heads/master@{#382824}

--- chrome/browser/chromeos/input_method/input_method_manager_impl.h
+++ chrome/browser/chromeos/input_method/input_method_manager_impl.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_checker.h"
 #include "chrome/browser/chromeos/input_method/candidate_window_controller.h"
 #include "chrome/browser/chromeos/input_method/input_method_util.h"
+#include "chrome/browser/chromeos/login/ui/user_adding_screen.h"
 #include "chrome/browser/profiles/profile.h"
 #include "ui/base/ime/chromeos/input_method_manager.h"
 #include "ui/base/ime/chromeos/input_method_whitelist.h"
@@ -37,7 +38,8 @@ class ImeKeyboard;
 
 // The implementation of InputMethodManager.
 class InputMethodManagerImpl : public InputMethodManager,
-                               public CandidateWindowController::Observer {
+                               public CandidateWindowController::Observer,
+                               public UserAddingScreen::Observer {
  public:
   class StateImpl : public InputMethodManager::State {
    public:
@@ -169,6 +171,10 @@ class InputMethodManagerImpl : public InputMethodManager,
       const std::string& engine_id,
       const std::vector<InputMethodManager::MenuItem>& items) override;
 
+  // chromeos::UserAddingScreen:
+  void OnUserAddingStarted() override;
+  void OnUserAddingFinished() override;
+
   ImeKeyboard* GetImeKeyboard() override;
   InputMethodUtil* GetInputMethodUtil() override;
   ComponentExtensionIMEManager* GetComponentExtensionIMEManager() override;
@@ -273,9 +279,11 @@ class InputMethodManagerImpl : public InputMethodManager,
   // auto-repeat interval.
   scoped_ptr<ImeKeyboard> keyboard_;
 
-
   // Whether load IME extensions.
   bool enable_extension_loading_;
+
+  // Whether the expanded IME menu is activated.
+  bool is_ime_menu_activated_;
 
   // The engine map from extension_id to an engine.
   typedef std::map<std::string, ui::IMEEngineHandlerInterface*> EngineMap;
