353329eea459f145ccf0f0b60c35360f03c5abe1
sebsg <sebsg@chromium.org>
2016-03-23 08:46:50 -0700

    Some forms have "Address Nickname" or "Address Label" fields that are used to identify an address. These fields were filled as address line 1.
    Added a regex to find those specific fields and not fill them.
    
    BUG=596913
    TEST=FormStructureBrowserTest
    
    Review URL: https://codereview.chromium.org/1825133002
    
    Cr-Commit-Position: refs/heads/master@{#382851}

--- components/autofill/core/browser/autofill_regex_constants.cc
+++ components/autofill/core/browser/autofill_regex_constants.cc
@@ -19,6 +19,7 @@ const char kRegionIgnoredRe[] =
     "province|region|other"
     "|provincia"  // es
     "|bairro|suburb";  // pt-BR, pt-PT
+const char kAddressNameIgnoredRe[] = "address.*nickname|address.*label";
 const char kCompanyRe[] =
     "company|business|organization|organisation"
     "|firma|firmenname"  // de-DE
