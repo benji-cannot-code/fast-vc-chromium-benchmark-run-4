e27bf623041113a71ab5a87b8aa79ecf1792d9ed
danakj <danakj@chromium.org>
2016-03-22 17:09:49 -0700

    Remove simple methods from WebGraphicsContext3D that do strings.
    Remove simple methods that return or take strings from
    WebGraphicsContext3D and just call the methods on GLES2Interface
    directly, converting to/from WTF::String and char*.
    
    R=chrishtr@chromium.org, kbr@chromium.org, pfeldman@chromium.org
    TBR=pfeldman
    BUG=584497
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1821903002
    
    Cr-Commit-Position: refs/heads/master@{#382741}

--- third_party/WebKit/Source/modules/webgl/WebGL2RenderingContextBase.cpp
+++ third_party/WebKit/Source/modules/webgl/WebGL2RenderingContextBase.cpp
@@ -31,6 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/WebGraphicsContext3DProvider.h"
 #include "wtf/OwnPtr.h"
 #include "wtf/PassOwnPtr.h"
+#include "wtf/text/WTFString.h"
+
+using WTF::String;
 
 namespace blink {
 
@@ -2781,10 +2784,11 @@ ScriptValue WebGL2RenderingContextBase::getParameter(ScriptState* scriptState, G
     if (isContextLost())
         return ScriptValue::createNull(scriptState);
     switch (pname) {
-    case GL_SHADING_LANGUAGE_VERSION:
-        return WebGLAny(scriptState, "WebGL GLSL ES 3.00 (" + String(webContext()->getString(GL_SHADING_LANGUAGE_VERSION)) + ")");
+    case GL_SHADING_LANGUAGE_VERSION: {
+        return WebGLAny(scriptState, "WebGL GLSL ES 3.00 (" + String(contextGL()->GetString(GL_SHADING_LANGUAGE_VERSION)) + ")");
+    }
     case GL_VERSION:
-        return WebGLAny(scriptState, "WebGL 2.0 (" + String(webContext()->getString(GL_VERSION)) + ")");
+        return WebGLAny(scriptState, "WebGL 2.0 (" + String(contextGL()->GetString(GL_VERSION)) + ")");
 
     case GL_COPY_READ_BUFFER_BINDING:
         return WebGLAny(scriptState, m_boundCopyReadBuffer.get());
