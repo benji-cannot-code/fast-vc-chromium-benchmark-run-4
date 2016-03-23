93fab90336f89d8025e99b8b7e5cc0883f00eef5
tnagel <tnagel@chromium.org>
2016-03-23 10:03:09 -0700

    Add const to some BrowserPolicyConnectorChromeOS methods.
    BUG=None
    
    Review URL: https://codereview.chromium.org/1826823002
    
    Cr-Commit-Position: refs/heads/master@{#382869}

--- chrome/browser/chromeos/policy/browser_policy_connector_chromeos.cc
+++ chrome/browser/chromeos/policy/browser_policy_connector_chromeos.cc
@@ -246,7 +246,7 @@ void BrowserPolicyConnectorChromeOS::Shutdown() {
   ChromeBrowserPolicyConnector::Shutdown();
 }
 
-bool BrowserPolicyConnectorChromeOS::IsEnterpriseManaged() {
+bool BrowserPolicyConnectorChromeOS::IsEnterpriseManaged() const {
   return install_attributes_ && install_attributes_->IsEnterpriseDevice();
 }
 
@@ -254,7 +254,7 @@ std::string BrowserPolicyConnectorChromeOS::GetEnterpriseDomain() const {
   return install_attributes_ ? install_attributes_->GetDomain() : std::string();
 }
 
-std::string BrowserPolicyConnectorChromeOS::GetDeviceAssetID() {
+std::string BrowserPolicyConnectorChromeOS::GetDeviceAssetID() const {
   if (device_cloud_policy_manager_) {
     const enterprise_management::PolicyData* policy =
         device_cloud_policy_manager_->device_store()->policy();
@@ -264,7 +264,7 @@ std::string BrowserPolicyConnectorChromeOS::GetDeviceAssetID() {
   return std::string();
 }
 
-std::string BrowserPolicyConnectorChromeOS::GetDirectoryApiID() {
+std::string BrowserPolicyConnectorChromeOS::GetDirectoryApiID() const {
   if (device_cloud_policy_manager_) {
     const enterprise_management::PolicyData* policy =
         device_cloud_policy_manager_->device_store()->policy();
@@ -274,7 +274,7 @@ std::string BrowserPolicyConnectorChromeOS::GetDirectoryApiID() {
   return std::string();
 }
 
-DeviceMode BrowserPolicyConnectorChromeOS::GetDeviceMode() {
+DeviceMode BrowserPolicyConnectorChromeOS::GetDeviceMode() const {
   return install_attributes_ ? install_attributes_->GetMode()
                              : DEVICE_MODE_NOT_SET;
 }
