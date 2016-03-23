0023a27fea5b70957527c39e4d2e72d381548445
gabadie <gabadie@chromium.org>
2016-03-23 08:40:08 -0700

    sandwich: Record a loading video on android and compute speed-index from it.
    Before, metrics in the CSV where fetched only from loading traces.
    This CL adds the hability to record a MP4 video of chrome loading
    a web page in sandwich, and then process that recorded video to
    output the loading speed-index into the metrics' CSV.
    
    BUG=582080
    
    Review URL: https://codereview.chromium.org/1782543002
    
    Cr-Commit-Position: refs/heads/master@{#382850}

---
+++ tools/android/loading/common_util.py
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import time
+
+
+def PollFor(condition, condition_name, interval=5):
+  """Polls for a function to return true.
+
+  Args:
+    condition: Function to wait its return to be True.
+    condition_name: The condition's name used for logging.
+    interval: Periods to wait between tries in seconds.
+
+  Returns:
+    What condition has returned to stop waiting.
+  """
+  while True:
+    result = condition()
+    logging.info('Polling condition %s is %s' % (
+        condition_name, 'met' if result else 'not met'))
+    if result:
+      return result
+    time.sleep(interval)
