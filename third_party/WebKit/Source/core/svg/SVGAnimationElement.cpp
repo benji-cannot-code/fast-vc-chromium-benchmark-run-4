4981b87b2181095202eae8b3fcf1a0a415c01d0d
loyso <loyso@chromium.org>
2016-03-22 18:17:50 -0700

    Blink Platform UnitBezier: Implement Slope and Range methods.
    We want to align blink::UnitBezier implementation with
    gfx::CubicBezier.
    
    Basically, blink::UnitBezier is a super set for gfx::CubicBezier.
    
    In next CLs:
    1) Merge blink::UnitBezier onto gfx::CubicBezier (including
    unit tests and rewriting to chromium style).
    
    gfx::CubicBezier is located at very low level and it is visible for all subsystems.
    
    2) Make blink::UnitBezier a thin wrapper around gfx::CubicBezier with all the calls inlined.
    
    Note, that we are allowed to include gfx/geometry in blink now (same as for cc/).
    
    Note, we will deal with std::numeric_limits<double>::epsilon separately here:
    http://crbug.com/458917
    
    Disclaimer:
    - Code is adapted from ui/gfx/geometry/cubic_bezier.cc
    - Tests are adapted from ui/gfx/geometry/cubic_bezier_unittest.cc
    
    BUG=577016
    
    Review URL: https://codereview.chromium.org/1819783002
    
    Cr-Commit-Position: refs/heads/master@{#382756}

--- third_party/WebKit/Source/core/svg/SVGAnimationElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGAnimationElement.cpp
@@ -448,7 +448,7 @@ float SVGAnimationElement::calculatePercentForSpline(float percent, unsigned spl
     SMILTime duration = simpleDuration();
     if (!duration.isFinite())
         duration = 100.0;
-    return narrowPrecisionToFloat(bezier.solve(percent, solveEpsilon(duration.value())));
+    return narrowPrecisionToFloat(bezier.solveWithEpsilon(percent, solveEpsilon(duration.value())));
 }
 
 float SVGAnimationElement::calculatePercentFromKeyPoints(float percent) const
--- third_party/WebKit/Source/platform/blink_platform.gypi
+++ third_party/WebKit/Source/platform/blink_platform.gypi
@@ -177,6 +177,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'animation/CubicBezierControlPoints.h',
       'animation/TimingFunction.cpp',
       'animation/TimingFunction.h',
+      'animation/UnitBezier.cpp',
       'animation/UnitBezier.h',
       'audio/AudioArray.h',
       'audio/AudioBus.cpp',
