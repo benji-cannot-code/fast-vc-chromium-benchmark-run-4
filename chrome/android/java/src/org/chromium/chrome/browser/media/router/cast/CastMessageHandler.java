5487126ff98609270bc8546ac90b657fc530fd1b
avayvod <avayvod@chromium.org>
2016-03-23 14:10:10 -0700

    [Android,Cast] Move session cleanup outside of the loop.
    BUG=597368
    
    Review URL: https://codereview.chromium.org/1831483003
    
    Cr-Commit-Position: refs/heads/master@{#382927}

--- chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/CastMessageHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/CastMessageHandler.java
@@ -418,8 +418,8 @@ public class CastMessageHandler {
                         clientId, "remove_session", mSession.getSessionId(), sequenceNumber);
             }
             mStopRequests.remove(clientId);
-            mSession = null;
         }
+        mSession = null;
     }
 
     /**
