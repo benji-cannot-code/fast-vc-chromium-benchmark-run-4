8f9c8d5e11ccf477b3faed48660c6f5fbc4637fa
alancutter <alancutter@chromium.org>
2016-03-22 20:33:37 -0700

    Support additive animations on CSS property shape-outside
    This change adds support for additive animations on CSS property
    shape-outside via element.animate().
    This change also refactors animations on -webkit-clip-path as it shares
    the same <basic-shape> interpolation logic as shape-outside.
    
    BUG=437696
    
    Review URL: https://codereview.chromium.org/1772783004
    
    Cr-Commit-Position: refs/heads/master@{#382783}

--- third_party/WebKit/Source/core/animation/PropertyInterpolationTypesMapping.cpp
+++ third_party/WebKit/Source/core/animation/PropertyInterpolationTypesMapping.cpp
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/animation/PropertyInterpolationTypesMapping.h"
 
 #include "core/HTMLNames.h"
+#include "core/animation/CSSBasicShapeInterpolationType.h"
 #include "core/animation/CSSBorderImageLengthBoxInterpolationType.h"
 #include "core/animation/CSSClipInterpolationType.h"
 #include "core/animation/CSSColorInterpolationType.h"
@@ -230,6 +231,10 @@ const InterpolationTypes* PropertyInterpolationTypesMapping::get(const PropertyH
         case CSSPropertyBorderImageSlice:
         case CSSPropertyWebkitMaskBoxImageSlice:
             applicableTypes->append(adoptPtr(new CSSImageSliceInterpolationType(cssProperty)));
+            break;
+        case CSSPropertyWebkitClipPath:
+        case CSSPropertyShapeOutside:
+            applicableTypes->append(adoptPtr(new CSSBasicShapeInterpolationType(cssProperty)));
             break;
         default:
             // TODO(alancutter): Support all interpolable CSS properties here so we can stop falling back to the old StyleInterpolation implementation.
