821a12b56f69ae7aa480b07126babe82ad29f82a
khushalsagar <khushalsagar@chromium.org>
2016-03-23 15:33:47 -0700

    cc: Fix LayerTree deserialization for commit.
    When we receive a commit protobuf from the engine, we collect all the
    existing Layers in a map and proceed with deserializing the new LayerTree
    in the commit protobuf, re-using any existing Layers if possible.
    
    When deserializing a Layer, Layer::FromLayerNodeProto creates a copy of
    the existing children, in order to clear the parent pointers for these
    layers, if they have now been added to the children of another layer.
    However, since we only traverse the layers from the updated LayerTree
    during deserialization, if a Layer has been destroyed on the engine, it will
    not be traversed during the derserialization step, and will still hold a
    reference to all its children. When this Layer is being destroyed on the
    client, it will call Layer::RemoveFromParent for all its children, which
    may now have been added to the children list of a different Layer.
    
    This patch clears all the properties that are sent with a Layer Hierarchy
    update in Layer::ClearLayerHierarchyPropertiesForDeserializationAndAddToMap,
    which is run for the existing LayerTree before parsing the new LayerTree from
    the commit protobuf.
    
    BUG=590102
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1804703004
    
    Cr-Commit-Position: refs/heads/master@{#382953}

--- cc/trees/layer_tree_host_unittest_serialization.cc
+++ cc/trees/layer_tree_host_unittest_serialization.cc
@@ -41,11 +41,17 @@ class LayerTreeHostSerializationTest : public testing::Test {
     layer_tree_host_src_->in_paint_layer_contents_ = false;
     layer_tree_host_dst_->in_paint_layer_contents_ = false;
 
-    // Need to reset root layer and LayerTreeHost pointers before tear down.
-    layer_tree_host_src_->SetRootLayer(nullptr);
+    // Need to reset LayerTreeHost pointers before tear down.
     layer_tree_host_src_ = nullptr;
-    layer_tree_host_dst_->SetRootLayer(nullptr);
     layer_tree_host_dst_ = nullptr;
+  }
+
+  void VerifyHostHasAllExpectedLayersInTree(Layer* root_layer) {
+    LayerTreeHostCommon::CallFunctionForSubtree(
+        root_layer, [root_layer](Layer* layer) {
+          DCHECK(layer->layer_tree_host());
+          EXPECT_EQ(layer, layer->layer_tree_host()->LayerById(layer->id()));
+        });
   }
 
   void VerifySerializationAndDeserialization() {
@@ -270,7 +276,7 @@ class LayerTreeHostSerializationTest : public testing::Test {
     VerifySerializationAndDeserialization();
   }
 
-  void LayersChangedMultipleSerializations() {
+  void RunLayersChangedMultipleSerializations() {
     // Just fake setup a layer for both source and dest.
     scoped_refptr<Layer> root_layer_src = Layer::Create();
     layer_tree_host_src_->SetRootLayer(root_layer_src);
@@ -308,6 +314,39 @@ class LayerTreeHostSerializationTest : public testing::Test {
     VerifySerializationAndDeserialization();
   }
 
+  void RunAddAndRemoveNodeFromLayerTree() {
+    /* Testing serialization when the tree hierarchy changes like this:
+         root                  root
+        /    \                /    \
+       a      b        =>    a      c
+               \                     \
+                c                     d
+    */
+    scoped_refptr<Layer> layer_src_root = Layer::Create();
+    layer_tree_host_src_->SetRootLayer(layer_src_root);
+
+    scoped_refptr<Layer> layer_src_a = Layer::Create();
+    scoped_refptr<Layer> layer_src_b = Layer::Create();
+    scoped_refptr<Layer> layer_src_c = Layer::Create();
+    scoped_refptr<Layer> layer_src_d = Layer::Create();
+
+    layer_src_root->AddChild(layer_src_a);
+    layer_src_root->AddChild(layer_src_b);
+    layer_src_b->AddChild(layer_src_c);
+
+    VerifySerializationAndDeserialization();
+    VerifyHostHasAllExpectedLayersInTree(layer_tree_host_dst_->root_layer());
+
+    // Now change the Layer Hierarchy
+    layer_src_c->RemoveFromParent();
+    layer_src_b->RemoveFromParent();
+    layer_src_root->AddChild(layer_src_c);
+    layer_src_c->AddChild(layer_src_d);
+
+    VerifySerializationAndDeserialization();
+    VerifyHostHasAllExpectedLayersInTree(layer_tree_host_dst_->root_layer());
+  }
+
  private:
   TestTaskGraphRunner task_graph_runner_src_;
   FakeLayerTreeHostClient client_src_;
@@ -327,7 +366,11 @@ TEST_F(LayerTreeHostSerializationTest, LayersChanged) {
 }
 
 TEST_F(LayerTreeHostSerializationTest, LayersChangedMultipleSerializations) {
-  LayersChangedMultipleSerializations();
+  RunLayersChangedMultipleSerializations();
+}
+
+TEST_F(LayerTreeHostSerializationTest, AddAndRemoveNodeFromLayerTree) {
+  RunAddAndRemoveNodeFromLayerTree();
 }
 
 }  // namespace cc
