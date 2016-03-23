1f1044458a2a63414fff82ba4bc3896c8ee49798
kenrb <kenrb@chromium.org>
2016-03-23 12:25:12 -0700

    Fix flakiness in PopupMenu tests
    SitePerProcessBrowserTest.PopupMenuTest has some observed flakiness,
    apparently due to raciness in element repositioning from the change of
    layout size caused by calling RenderWidgetHostView::SetBounds.
    
    The call isn't necessary for the test, so this CL removes it.
    
    BUG=597111
    
    Review URL: https://codereview.chromium.org/1823283003
    
    Cr-Commit-Position: refs/heads/master@{#382897}

--- content/browser/site_per_process_browsertest.cc
+++ content/browser/site_per_process_browsertest.cc
@@ -5191,7 +5191,6 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessBrowserTest, MAYBE_PopupMenuTest) {
   // Position main window to ensure consistent screen coordinates.
   RenderWidgetHostViewBase* rwhv_root = static_cast<RenderWidgetHostViewBase*>(
       root->current_frame_host()->GetRenderWidgetHost()->GetView());
-  rwhv_root->SetBounds(gfx::Rect(100, 100, 500, 500));
   static_cast<WebContentsImpl*>(shell()->web_contents())->SendScreenRects();
 
   content::TestNavigationObserver navigation_observer(shell()->web_contents());
@@ -5250,7 +5249,6 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessBrowserTest, MAYBE_NestedPopupMenuTest) {
   // Position main window to ensure consistent screen coordinates.
   RenderWidgetHostViewBase* rwhv_root = static_cast<RenderWidgetHostViewBase*>(
       root->current_frame_host()->GetRenderWidgetHost()->GetView());
-  rwhv_root->SetBounds(gfx::Rect(100, 100, 500, 500));
   static_cast<WebContentsImpl*>(shell()->web_contents())->SendScreenRects();
 
   // For clarity, we are labeling the frame tree nodes as:
