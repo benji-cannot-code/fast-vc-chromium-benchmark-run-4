8fe8dc29e34ec7a421a0aa92b154dc96f2939c26
pdr <pdr@chromium.org>
2016-03-23 13:06:56 -0700

    Remove extra per-character walk in SVGTextLayoutEngine
    This patch removes a per-character walk in SVGTextLayoutEngine which
    was unnecessary because the glyph computation has the data available
    already.
    
    BUG=597076
    
    Review URL: https://codereview.chromium.org/1824193002
    
    Cr-Commit-Position: refs/heads/master@{#382908}

--- third_party/WebKit/Source/core/layout/svg/SVGTextLayoutEngine.h
+++ third_party/WebKit/Source/core/layout/svg/SVGTextLayoutEngine.h
@@ -63,6 +63,7 @@ private:
     void updateCurrentTextPosition(float x, float y, float glyphAdvance);
     void updateRelativePositionAdjustmentsIfNeeded(float dx, float dy);
 
+    void computeCurrentFragmentMetrics(SVGInlineTextBox*);
     void recordTextFragment(SVGInlineTextBox*);
 
     void beginTextPathLayout(SVGInlineFlowBox*);
