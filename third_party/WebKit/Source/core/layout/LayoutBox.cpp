e67f9d9d000ff37c840dd103577cbd3d97cffc11
chrishtr <chrishtr@chromium.org>
2016-03-22 19:17:56 -0700

    Pass nullptr for paintInvalidationState if it can't be used to map geometry.
    If it can't be used for the current object, it can't be used for an ancestor.
    Therefore, after it returning false for canMapToAncestor(), pass nullptr fur
    further recursions up the layout tree.
    
    Furthermore, using it for an ancestor is invalid because a
    PaintInvalidationState object is bound to a single LayoutObject.
    
    Also adds PaintInvalidationState early-outing to LayoutView.
    
    BUG=594516
    
    Review URL: https://codereview.chromium.org/1818963003
    
    Cr-Commit-Position: refs/heads/master@{#382772}

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -2012,9 +2012,9 @@ void LayoutBox::mapToVisibleRectInAncestorSpace(const LayoutBoxModelObject* ance
     }
 
     if (container->isLayoutView())
-        toLayoutView(container)->mapToVisibleRectInAncestorSpace(ancestor, rect, LayoutView::toViewportConstrainedPosition(position), paintInvalidationState);
+        toLayoutView(container)->mapToVisibleRectInAncestorSpace(ancestor, rect, LayoutView::toViewportConstrainedPosition(position), nullptr);
     else
-        container->mapToVisibleRectInAncestorSpace(ancestor, rect, paintInvalidationState);
+        container->mapToVisibleRectInAncestorSpace(ancestor, rect, nullptr);
 }
 
 void LayoutBox::inflatePaintInvalidationRectForReflectionAndFilter(LayoutRect& paintInvalidationRect) const
--- third_party/WebKit/Source/core/layout/LayoutObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutObject.cpp
@@ -1659,7 +1659,7 @@ void LayoutObject::mapToVisibleRectInAncestorSpace(const LayoutBoxModelObject* a
                 return;
         }
 
-        parent->mapToVisibleRectInAncestorSpace(ancestor, rect, paintInvalidationState);
+        parent->mapToVisibleRectInAncestorSpace(ancestor, rect, nullptr);
     }
 }
 
