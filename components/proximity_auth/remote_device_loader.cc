2f499d328e7883d214ea2d50a11dca21ff4740a9
xyzzyz <xyzzyz@chromium.org>
2016-03-23 12:44:25 -0700

    Add <algorithm> header to remote_device_loader.cc
    The code is using std::find_if, but doesn't include the <algorithm> header. It
    works, because it is pulled as a dependency by something else. I am currently
    doing an update to third_party library that's currently pulling that header, but
    will no longer do after the update, breaking this code.
    
    BUG=597321
    
    Review URL: https://codereview.chromium.org/1825423002
    
    Cr-Commit-Position: refs/heads/master@{#382903}

--- components/proximity_auth/remote_device_loader.cc
+++ components/proximity_auth/remote_device_loader.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/proximity_auth/remote_device_loader.h"
 
+#include <algorithm>
 #include <utility>
 
 #include "base/base64url.h"
