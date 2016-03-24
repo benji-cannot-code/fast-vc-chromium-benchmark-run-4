8c90ad698b4859463098789ee72d2cc7d4f3f7ea
erikchen <erikchen@chromium.org>
2016-03-23 18:11:58 -0700

    Disable glReadPixels workaround.
    The original problem was seen on OS X 10.7. Now that Chrome no longer supports
    OS X 10.7, it may not be necessary anymore.
    
    BUG=581777
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1827983002
    
    Cr-Commit-Position: refs/heads/master@{#383001}

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -20,7 +20,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "8.53",
+  "version": "8.54",
   "entries": [
     {
       "id": 1,
@@ -1853,6 +1853,17 @@ LONG_STRING_CONST(
       "os": {
         "type": "macosx"
       },
+      "exceptions": [
+       {
+         "os": {
+           "type": "macosx",
+           "version": {
+             "op": ">=",
+             "value": "10.9"
+           }
+         }
+       }
+     ],
       "features": [
         "iosurface_readback_workaround"
       ]
