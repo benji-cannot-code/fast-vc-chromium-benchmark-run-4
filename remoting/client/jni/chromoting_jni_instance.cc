1f24235d28ddd2255bf08ef2104b5e9c321f23f1
aberent <aberent@chromium.org>
2016-03-24 07:48:55 -0700

    Revert of Move ServiceUrls to remoting/host (patchset #4 id:80001 of https://codereview.chromium.org/1817573003/ )
    Reason for revert:
    Breaks downstream Android ToT builds, and hence prevents Chromium changes rolling into downstream Android builds.
    
    BUG= 597559
    
    Original issue's description:
    > Move ServiceUrls to remoting/host
    >
    > ServiceUrls was used only in one place in android client and it's
    > not really useful there. Moved it to the host directory.
    >
    > TBR=rogerta@chromium.org
    >
    > Committed: https://crrev.com/2a2656a67bd2ce190df4d918f609bdb4449a64fe
    > Cr-Commit-Position: refs/heads/master@{#382939}
    
    TBR=lambroslambrou@chromium.org,rogerta@chromium.org,sergeyu@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1830993002
    
    Cr-Commit-Position: refs/heads/master@{#383060}

--- remoting/client/jni/chromoting_jni_instance.cc
+++ remoting/client/jni/chromoting_jni_instance.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "jingle/glue/thread_wrapper.h"
 #include "net/socket/client_socket_factory.h"
 #include "remoting/base/chromium_url_request.h"
+#include "remoting/base/service_urls.h"
 #include "remoting/client/audio_player.h"
 #include "remoting/client/client_status_logger.h"
 #include "remoting/client/jni/android_keymap.h"
@@ -41,8 +42,6 @@ namespace {
 const char* const kXmppServer = "talk.google.com";
 const int kXmppPort = 5222;
 const bool kXmppUseTls = true;
-
-const char kDirectoryBotJid[] = "remoting@bot.talk.google.com";
 
 // Interval at which to log performance statistics, if enabled.
 const int kPerfStatsIntervalMs = 60000;
@@ -397,8 +396,9 @@ void ChromotingJniInstance::ConnectToHostOnNetworkThread() {
       new XmppSignalStrategy(net::ClientSocketFactory::GetDefaultFactory(),
                              jni_runtime_->url_requester(), xmpp_config_));
 
-  client_status_logger_.reset(new ClientStatusLogger(
-      ServerLogEntry::ME2ME, signaling_.get(), kDirectoryBotJid));
+  client_status_logger_.reset(
+      new ClientStatusLogger(ServerLogEntry::ME2ME, signaling_.get(),
+                             ServiceUrls::GetInstance()->directory_bot_jid()));
 
   scoped_refptr<protocol::TransportContext> transport_context =
       new protocol::TransportContext(
