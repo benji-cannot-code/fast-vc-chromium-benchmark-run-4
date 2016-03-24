31163d16fc66d01d8ca0aaaa2a3bf27426d15553
ajuma <ajuma@chromium.org>
2016-03-24 10:42:59 -0700

    cc: Remove more code that was only used by CDP
    This removes a draw property and some setters/getters on Layer
    and LayerImpl that were only used by CDP.
    
    BUG=497821
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1829053002
    
    Cr-Commit-Position: refs/heads/master@{#383091}

--- cc/layers/layer_impl.h
+++ cc/layers/layer_impl.h
@@ -561,10 +561,6 @@ class CC_EXPORT LayerImpl {
 
   virtual gfx::Rect GetEnclosingRectInTargetSpace() const;
 
-  void set_visited(bool visited) { visited_ = visited; }
-
-  bool visited() { return visited_; }
-
   void set_layer_or_descendant_is_drawn(bool layer_or_descendant_is_drawn) {
     layer_or_descendant_is_drawn_ = layer_or_descendant_is_drawn;
   }
@@ -580,11 +576,6 @@ class CC_EXPORT LayerImpl {
   bool layer_or_descendant_has_touch_handler() {
     return layer_or_descendant_has_touch_handler_;
   }
-
-  void set_sorted_for_recursion(bool sorted_for_recursion) {
-    sorted_for_recursion_ = sorted_for_recursion;
-  }
-  bool sorted_for_recursion() { return sorted_for_recursion_; }
 
   int num_copy_requests_in_target_subtree();
 
@@ -771,11 +762,9 @@ class CC_EXPORT LayerImpl {
 
   std::vector<FrameTimingRequest> frame_timing_requests_;
   bool frame_timing_requests_dirty_;
-  bool visited_;
   bool layer_or_descendant_is_drawn_;
   // If true, the layer or one of its descendants has a touch handler.
   bool layer_or_descendant_has_touch_handler_;
-  bool sorted_for_recursion_;
 
   DISALLOW_COPY_AND_ASSIGN(LayerImpl);
 };
