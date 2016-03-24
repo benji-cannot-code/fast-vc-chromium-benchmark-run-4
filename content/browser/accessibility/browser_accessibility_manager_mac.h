dff3f12ce91c9ed3eb4d79db70a9aa327e86a5db
nektar <nektar@chromium.org>
2016-03-24 13:44:25 -0700

    Fixes typing and deleting text in simple text fields on Mac OS X 10.11.
    BUG=530826
    TESTED=VoiceOver on Mac OS X 10.11
    R=dmazzoni@chromium.org, avi@chromium.org, mark@chromium.org
    
    Review URL: https://codereview.chromium.org/1814163003
    
    Cr-Commit-Position: refs/heads/master@{#383141}

--- content/browser/accessibility/browser_accessibility_manager_mac.h
+++ content/browser/accessibility/browser_accessibility_manager_mac.h
@@ -7,9 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_ACCESSIBILITY_BROWSER_ACCESSIBILITY_MANAGER_MAC_H_
 
 #import <Cocoa/Cocoa.h>
+#include <stdint.h>
+
+#include <map>
+#include <vector>
 
 #include "base/macros.h"
+#include "base/strings/string16.h"
+#import "content/browser/accessibility/browser_accessibility_cocoa.h"
 #include "content/browser/accessibility/browser_accessibility_manager.h"
+#include "content/public/browser/ax_event_notification_details.h"
 
 namespace content {
 
@@ -22,6 +29,8 @@ class CONTENT_EXPORT BrowserAccessibilityManagerMac
       BrowserAccessibilityDelegate* delegate,
       BrowserAccessibilityFactory* factory = new BrowserAccessibilityFactory());
 
+  ~BrowserAccessibilityManagerMac() override;
+
   static ui::AXTreeUpdate GetEmptyDocument();
 
   BrowserAccessibility* GetFocus() override;
@@ -30,9 +39,16 @@ class CONTENT_EXPORT BrowserAccessibilityManagerMac
   void NotifyAccessibilityEvent(ui::AXEvent event_type,
                                 BrowserAccessibility* node) override;
 
+  void OnAccessibilityEvents(
+      const std::vector<AXEventNotificationDetails>& details) override;
+
   NSView* parent_view() { return parent_view_; }
 
  private:
+  // AXTreeDelegate methods.
+  void OnNodeDataWillChange(ui::AXTree* tree,
+                            const ui::AXNodeData& old_node_data,
+                            const ui::AXNodeData& new_node_data) override;
   void OnAtomicUpdateFinished(
       ui::AXTree* tree,
       bool root_changed,
@@ -41,11 +57,22 @@ class CONTENT_EXPORT BrowserAccessibilityManagerMac
   // Returns an autoreleased object.
   NSDictionary* GetUserInfoForSelectedTextChangedNotification();
 
+  // Returns an autoreleased object.
+  NSDictionary* GetUserInfoForValueChangedNotification(
+      const BrowserAccessibilityCocoa* native_node,
+      const base::string16& deleted_text,
+      const base::string16& inserted_text) const;
+
   // This gives BrowserAccessibilityManager::Create access to the class
   // constructor.
   friend class BrowserAccessibilityManager;
 
   NSView* parent_view_;
+
+  // Keeps track of any edits that have been made by the user during a tree
+  // update. Used by NSAccessibilityValueChangedNotification.
+  // Maps AXNode IDs to name or value attribute changes.
+  std::map<int32_t, base::string16> text_edits_;
 
   DISALLOW_COPY_AND_ASSIGN(BrowserAccessibilityManagerMac);
 };
