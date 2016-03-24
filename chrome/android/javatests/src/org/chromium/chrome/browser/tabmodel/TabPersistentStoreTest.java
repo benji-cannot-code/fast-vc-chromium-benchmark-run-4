67f3dad218e0b6a6d6568245e7b6632d9e37d926
dfalcantara <dfalcantara@chromium.org>
2016-03-24 14:11:50 -0700

    Add a class for killing document mode
    DocumentModeAssassin is the replacement for the DocumentMigrationHelper,
    and will be used to force migration from document mode to tabbed mode.
    The class works, but is not hooked into any particular pathway in this
    CL.
    
    Migration proceeds in two main phases, where items are copied over to
    the tabbed mode directories before document mode is turned off.  The
    second phase actually deletes the data.  Details of the implementation
    and the reasoning behind the decisions I made are put next to the code
    itself so that we don't lose track of them in the future.
    
    Only the first part of the path is tested right now because this CL
    is already relatively huge.
    
    BUG=582539
    TEST=DocumentModeAssassinTest
    
    Review URL: https://codereview.chromium.org/1797023002
    
    Cr-Commit-Position: refs/heads/master@{#383148}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabPersistentStoreTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabPersistentStoreTest.java
@@ -125,11 +125,11 @@ public class TabPersistentStoreTest extends NativeLibraryTestBase {
     /**
      * Used when testing interactions of TabPersistentStore with real {@link TabModelImpl}s.
      */
-    private static class TestTabModelSelector extends TabModelSelectorBase
+    static class TestTabModelSelector extends TabModelSelectorBase
             implements TabModelDelegate {
+        final TabPersistentStore mTabPersistentStore;
+        final MockTabPersistentStoreObserver mTabPersistentStoreObserver;
         private final MockTabCreatorManager mTabCreatorManager;
-        private final MockTabPersistentStoreObserver mTabPersistentStoreObserver;
-        private final TabPersistentStore mTabPersistentStore;
         private final TabModelOrderController mTabModelOrderController;
 
         public TestTabModelSelector(Context context) throws Exception {
@@ -157,6 +157,7 @@ public class TabPersistentStoreTest extends NativeLibraryTestBase {
             initialize(false, regularTabModel, incognitoTabModel);
         }
 
+        @Override
         public Tab openNewTab(
                 LoadUrlParams loadUrlParams, TabLaunchType type, Tab parent, boolean incognito) {
             return mTabCreatorManager.getTabCreator(incognito).createNewTab(
@@ -188,7 +189,7 @@ public class TabPersistentStoreTest extends NativeLibraryTestBase {
         }
     }
 
-    private static class MockTabPersistentStoreObserver implements TabPersistentStoreObserver {
+    static class MockTabPersistentStoreObserver implements TabPersistentStoreObserver {
         public final CallbackHelper initializedCallback = new CallbackHelper();
         public final CallbackHelper detailsReadCallback = new CallbackHelper();
         public final CallbackHelper stateLoadedCallback = new CallbackHelper();
