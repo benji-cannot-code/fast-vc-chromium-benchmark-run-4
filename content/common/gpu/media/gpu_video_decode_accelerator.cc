20fc45b00e02c0258c13a070150c956d7b32dfc7
mcasas <mcasas@chromium.org>
2016-03-24 12:29:59 -0700

    Smart revert of media: Add MediaOzonePlatform support
    Smart revert version of auto revert in https://crrev.com/1127343003/
    Original description -------------------------------------------------
    Revert of media: Add MediaOzonePlatform support (patchset #7 id:110001 of https://codereview.chromium.org/269673005/)
    
    Reason for revert:
    Not used at the moment.
    
    Agreed with kalyan.kondapally@intel.com,
    tiago.vignatti@intel.com and
    joone.hur@intel.com.
    
    Original issue's description:
    > media: Add MediaOzonePlatform support
    >
    > This CL adds MediaOzonePlatform, a new Ozone component for abstracting media
    > and related. In particular it adds CreateVideoDecodeAccelerator method so Ozone
    > implementations can use that for abstracting video decoding acceleration (i.e.
    > GPU based video decoding). Eventually MediaOzonePlatform will be used as well
    > for many windowing systems abstractions related to media, e.g. video encode,
    > audio, etc. We will add support for these others as needed.
    >
    > Media platform is called and created on demand whenever a video playback is set
    > to play. Different than ui::OzonePlatform methods, the one added in here is
    > non-pure virtual so the implementations can decide themselves whether a video
    > implementation is actually wanted, based on its hardware capabilities.
    >
    > This work is aimed at internal implementations like Ozone-DRI, Ozone-test but
    > also for externals like Ozone-Wayland. Different targets like Chromium Browser,
    > Chrome OS, ChromeCast and others can now take advantage of it.
    >
    > BUG=380884
    > TEST=manually on Ozone-Wayland, using:
    >
    >  $ export GYP_DEFINES='chromeos=0 use_ozone=1 proprietary_codecs=1 ffmpeg_branding=Chrome'
    >  $ ninja -j16 -C out/Release content
    >
    > and to run:
    >
    >  ~/git/chromium/src/out/Debug$ ./chrome --no-sandbox --user-data-dir=/tmp/chrome --ignore-gpu-blacklist ../../third_party/WebKit/PerformanceTests/resources/bear-1280x720.mp4  --vmodule=*/ozone/ui*=3 --v=0
    >
    > additionally, in Release builds you can start chrome with --disable-accelerated-video-decode to compare the results.
    >
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=276552
    
    TBR= rjkroege@chromium.org,dnicoara@chromium.org,spang@chromium.org,dongseong.hwang@intel.com,xhwang@chromium.org,tiago.vignatti@intel.com
    BUG=380884
    
    Review URL: https://codereview.chromium.org/1832743002
    
    Cr-Commit-Position: refs/heads/master@{#383120}

--- content/common/gpu/media/gpu_video_decode_accelerator.cc
+++ content/common/gpu/media/gpu_video_decode_accelerator.cc
@@ -45,8 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/gpu/media/vaapi_video_decode_accelerator.h"
 #include "ui/gl/gl_implementation.h"
 #endif
-#elif defined(USE_OZONE)
-#include "media/ozone/media_ozone_platform.h"
 #elif defined(OS_ANDROID)
 #include "content/common/gpu/media/android_video_decode_accelerator.h"
 #endif
@@ -368,9 +366,6 @@ bool GpuVideoDecodeAccelerator::Initialize(
 #if defined(OS_MACOSX)
     &GpuVideoDecodeAccelerator::CreateVTVDA,
 #endif
-#if !defined(OS_CHROMEOS) && defined(USE_OZONE)
-    &GpuVideoDecodeAccelerator::CreateOzoneVDA,
-#endif
 #if defined(OS_ANDROID)
     &GpuVideoDecodeAccelerator::CreateAndroidVDA,
 #endif
@@ -483,16 +478,6 @@ GpuVideoDecodeAccelerator::CreateVTVDA() {
           make_context_current_,
           base::Bind(&GpuVideoDecodeAccelerator::BindImage,
                      base::Unretained(this))));
-}
-#endif
-
-#if !defined(OS_CHROMEOS) && defined(USE_OZONE)
-scoped_ptr<media::VideoDecodeAccelerator>
-GpuVideoDecodeAccelerator::CreateOzoneVDA() {
-  media::MediaOzonePlatform* platform =
-      media::MediaOzonePlatform::GetInstance();
-  return make_scoped_ptr<media::VideoDecodeAccelerator>(
-      platform->CreateVideoDecodeAccelerator(make_context_current_));
 }
 #endif
 
