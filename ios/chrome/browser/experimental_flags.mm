9bb8c9b4bab45b510f6d2cb26e50f3ad04dea1fa
olivierrobin <olivierrobin@chromium.org>
2016-03-24 10:41:08 -0700

    Remove Keep Reading List Item Experimental flag.
    Reading list has now a persistent storage and this flag is useless.
    
    Review URL: https://codereview.chromium.org/1828983003
    
    Cr-Commit-Position: refs/heads/master@{#383090}

--- ios/chrome/browser/experimental_flags.mm
+++ ios/chrome/browser/experimental_flags.mm
@@ -28,7 +28,6 @@ NSString* const kEnableViewCopyPasswords = @"EnableViewCopyPasswords";
 NSString* const kHeuristicsForPasswordGeneration =
     @"HeuristicsForPasswordGeneration";
 NSString* const kEnableReadingList = @"EnableReadingList";
-NSString* const kEnableReadingListKeepItems = @"EnableReadingListKeepItems";
 }  // namespace
 
 namespace experimental_flags {
@@ -106,11 +105,6 @@ bool IsTabSwitcherEnabled() {
 
 bool IsReadingListEnabled() {
   return [[NSUserDefaults standardUserDefaults] boolForKey:kEnableReadingList];
-}
-
-bool IsReadingListKeepItemsEnabled() {
-  return [[NSUserDefaults standardUserDefaults]
-      boolForKey:kEnableReadingListKeepItems];
 }
 
 }  // namespace experimental_flags
