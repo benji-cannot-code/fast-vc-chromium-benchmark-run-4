02d359e9dca4c9249ab79aef3c82ebbbe3fa1a06
chrishtr <chrishtr@chromium.org>
2016-03-23 19:13:32 -0700

    Delete some dead code in LayoutObject.h.
    And some related cleanup.
    
    Review URL: https://codereview.chromium.org/1829633003
    
    Cr-Commit-Position: refs/heads/master@{#383016}

--- third_party/WebKit/Source/core/layout/LayoutBlock.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlock.cpp
@@ -1085,7 +1085,6 @@ bool LayoutBlock::simplifiedLayout()
     if (!posChildNeedsLayout() && !(needsSimplifiedNormalFlowLayout() || needsPositionedMovementLayout()))
         return false;
 
-
     {
         // LayoutState needs this deliberate scope to pop before paint invalidation.
         LayoutState state(*this, locationOffset());
--- third_party/WebKit/Source/core/layout/LayoutBlock.h
+++ third_party/WebKit/Source/core/layout/LayoutBlock.h
@@ -405,6 +405,7 @@ private:
 
     Node* nodeForHitTest() const;
 
+    // Returns true if the positioned movement-only layout succeeded.
     bool tryLayoutDoingPositionedMovementOnly();
 
     bool avoidsFloats() const override { return true; }
