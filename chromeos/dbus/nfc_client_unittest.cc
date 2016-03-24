8a30ad9502c8a3c2135eda3df4dfadd58e1cfc63
hashimoto <hashimoto@chromium.org>
2016-03-23 20:54:18 -0700

    Stop printing LOG(ERROR) when NFC service is unknown
    Add a new callback method to NfcPropertySet and let it own its WeakPtrFactory.
    Check the error name to reduce LOG(ERROR) noise.
    
    BUG=393311
    TEST=Boot a Chromebook (link) and "grep neard /var/log/ui/ui.LATEST" to
    see nothing.
    
    Review URL: https://codereview.chromium.org/1778273002
    
    Cr-Commit-Position: refs/heads/master@{#383025}

--- chromeos/dbus/nfc_client_unittest.cc
+++ chromeos/dbus/nfc_client_unittest.cc
@@ -421,7 +421,7 @@ TEST_F(NfcClientTest, AdaptersAddedAndRemoved) {
   EXPECT_CALL(*this,
               ErrorCallback(nfc_client_helpers::kUnknownObjectError, _));
   EXPECT_CALL(*mock_adapter1_proxy_, CallMethodWithErrorCallback(_, _, _, _))
-      .Times(0);
+      .Times(1);
   adapter_client_->StartPollLoop(
       dbus::ObjectPath(kTestAdapterPath1),
       nfc_adapter::kModeInitiator,
@@ -548,7 +548,7 @@ TEST_F(NfcClientTest, TagsAddedAndRemoved) {
   EXPECT_CALL(*this,
               ErrorCallback(nfc_client_helpers::kUnknownObjectError, _));
   EXPECT_CALL(*mock_tag1_proxy_, CallMethodWithErrorCallback(_, _, _, _))
-      .Times(0);
+      .Times(1);
   tag_client_->Write(dbus::ObjectPath(kTestTagPath1), write_data,
                      base::Bind(&NfcClientTest::SuccessCallback,
                                 base::Unretained(this)),
@@ -676,7 +676,7 @@ TEST_F(NfcClientTest, DevicesAddedAndRemoved) {
   EXPECT_CALL(*this,
               ErrorCallback(nfc_client_helpers::kUnknownObjectError, _));
   EXPECT_CALL(*mock_device1_proxy_, CallMethodWithErrorCallback(_, _, _, _))
-      .Times(0);
+      .Times(1);
   device_client_->Push(dbus::ObjectPath(kTestDevicePath1), write_data,
                        base::Bind(&NfcClientTest::SuccessCallback,
                                   base::Unretained(this)),
