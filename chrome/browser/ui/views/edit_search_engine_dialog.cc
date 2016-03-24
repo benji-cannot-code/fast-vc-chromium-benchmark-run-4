da62fc0795fdb99d13fbec826c2a34630b614eab
tfarina <tfarina@chromium.org>
2016-03-24 12:36:02 -0700

    views: remove unnecessary EditSearchEngine() function abstraction
    It is unlikely this function will be used to be called by cross-platform
    code, so it is not necessary to wrap EditSearchEngineDialog::Show()
    method through it. Just have BrowserView call it directly like it does
    with other views dialogs.
    
    BUG=None
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1833673002
    
    Cr-Commit-Position: refs/heads/master@{#383122}

--- chrome/browser/ui/views/edit_search_engine_dialog.cc
+++ chrome/browser/ui/views/edit_search_engine_dialog.cc
@@ -34,17 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using views::GridLayout;
 using views::Textfield;
 
-namespace chrome {
-
-void EditSearchEngine(gfx::NativeWindow parent,
-                      TemplateURL* template_url,
-                      EditSearchEngineControllerDelegate* delegate,
-                      Profile* profile) {
-  EditSearchEngineDialog::Show(parent, template_url, delegate, profile);
-}
-
-}  // namespace chrome
-
 EditSearchEngineDialog::EditSearchEngineDialog(
     TemplateURL* template_url,
     EditSearchEngineControllerDelegate* delegate,
