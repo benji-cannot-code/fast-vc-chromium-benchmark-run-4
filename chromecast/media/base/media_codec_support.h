20f4e5088887bee3a9361ab5df5cb52066e19f28
servolk <servolk@chromium.org>
2016-03-24 18:14:15 -0700

    [Chromecast] Cleanup in media_codec_support.*
    Replaced outdated dependency on net/base/mime_util.h, now we are using
    media/base/mime_util.h instead. Also updated AC3/EAC3 codec ids in
    chromecast::media::IsCodecSupported and removed outdated checks for
    codec ids supported by mainstream Chrome.
    
    Review URL: https://codereview.chromium.org/1827213002
    
    Cr-Commit-Position: refs/heads/master@{#383215}

--- chromecast/media/base/media_codec_support.h
+++ chromecast/media/base/media_codec_support.h
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback.h"
-#include "net/base/mime_util.h"
+#include "media/base/mime_util.h"
 
-// TODO(slan|servolk): remove when this definition exists upstream.
+// TODO(slan|servolk): remove when this definition exists in //media.
 namespace media {
 typedef base::Callback<bool(const std::string&)> IsCodecSupportedCB;
 }
