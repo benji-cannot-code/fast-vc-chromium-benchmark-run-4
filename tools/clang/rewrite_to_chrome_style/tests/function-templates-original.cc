c26633914d69b9efaa7f3a68deef9fd65fcdd3cf
dcheng <dcheng@chromium.org>
2016-03-24 23:14:08 -0700

    rewrite_to_chrome_style: handle simple UnresolvedLookupExprs
    If an UnresolvedLookupExpr only contains one candidate
    declaration in the lookup set, and the candidate is a
    function template, just base the rename on that function
    template.
    
    BUG=584408
    
    Review URL: https://codereview.chromium.org/1823963003
    
    Cr-Commit-Position: refs/heads/master@{#383257}

--- tools/clang/rewrite_to_chrome_style/tests/function-templates-original.cc
+++ tools/clang/rewrite_to_chrome_style/tests/function-templates-original.cc
@@ -12,8 +12,6 @@ bool isInBounds(From value) {
 
 template<typename To, typename From>
 To safeCast(From value) {
-  // Note: this (incorrectly) doesn't get rewritten, because this template is
-  // never instantiated, so the AST is just an UnresolvedLookupExpr node.
   if (!isInBounds<To>(value))
     return 0;
   return static_cast<To>(value);
@@ -24,8 +22,8 @@ class Checked {
  public:
   template<typename U, typename V>
   Checked(const Checked<U, V>& rhs){
-    // Similarly, this (incorrectly) also doesn't get rewritten, since it's not
-    // instantiated. In this case, the AST representation contains a bunch of
+    // This (incorrectly) doesn't get rewritten, since it's not instantiated. In
+    // this case, the AST representation contains a bunch of
     // CXXDependentScopeMemberExpr nodes.
     if (rhs.hasOverflowed())
       this->overflowed();
