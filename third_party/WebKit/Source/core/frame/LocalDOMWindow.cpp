8ae85eba79162ad01cbf39a22f5ad61269f53c03
tkent <tkent@chromium.org>
2016-03-24 20:16:30 -0700

    Remove ASSERT_ARG().
    ASSERT_ARG() is not much helpful than normal ASSERT() and DCHECK(). So this CL
    removes it, and replaces existing call sites with DCHECK().
    
    Also, this CL moves Node stream printers from testing code to production code
    because they are used in DCHECK().
    
    This relands crrev.com/383047 with just rebase.
    
    BUG=596760
    
    Review URL: https://codereview.chromium.org/1828163002
    
    Cr-Commit-Position: refs/heads/master@{#383236}

--- third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
+++ third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
@@ -266,7 +266,7 @@ static void removeAllBeforeUnloadEventListeners(LocalDOMWindow* domWindow)
 
 static bool allowsBeforeUnloadListeners(LocalDOMWindow* window)
 {
-    ASSERT_ARG(window, window);
+    DCHECK(window);
     LocalFrame* frame = window->frame();
     if (!frame)
         return false;
--- third_party/WebKit/Source/core/page/ContextMenuController.cpp
+++ third_party/WebKit/Source/core/page/ContextMenuController.cpp
@@ -48,7 +48,7 @@ using namespace HTMLNames;
 ContextMenuController::ContextMenuController(Page*, ContextMenuClient* client)
     : m_client(client)
 {
-    ASSERT_ARG(client, client);
+    DCHECK(client);
 }
 
 ContextMenuController::~ContextMenuController()
