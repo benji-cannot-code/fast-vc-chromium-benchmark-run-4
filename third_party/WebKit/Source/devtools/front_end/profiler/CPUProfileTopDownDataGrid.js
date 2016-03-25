dc2812520e5638f8135948edb4301425782f0a2b
alph <alph@chromium.org>
2016-03-24 21:48:14 -0700

    DevTools: Refactor CPU profiler: get rid of data object.
    Review URL: https://codereview.chromium.org/1833043002
    
    Cr-Commit-Position: refs/heads/master@{#383245}

--- third_party/WebKit/Source/devtools/front_end/profiler/CPUProfileTopDownDataGrid.js
+++ third_party/WebKit/Source/devtools/front_end/profiler/CPUProfileTopDownDataGrid.js
@@ -37,7 +37,6 @@ WebInspector.TopDownProfileDataGridNode = function(profileNode, owningTree)
     WebInspector.ProfileDataGridNode.call(this, profileNode, owningTree, hasChildren);
 
     this._remainingChildren = profileNode.children;
-    this.buildData();
 }
 
 WebInspector.TopDownProfileDataGridNode.prototype = {
