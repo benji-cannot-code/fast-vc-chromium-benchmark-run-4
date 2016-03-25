b9bff4ed3959dbece1c5ba20ee057017e917dd1d
allada <allada@chromium.org>
2016-03-25 02:32:59 -0700

    Added regex support in search for network tab
    Users may now filter with regular expression (optional checkbox)
    in the network tab
    
    http://imgur.com/a0ZKF4f
    
    BUG=576022
    R=lushnikov
    
    Review URL: https://codereview.chromium.org/1817193003
    
    Cr-Commit-Position: refs/heads/master@{#383266}

--- third_party/WebKit/Source/devtools/front_end/ui/FilterBar.js
+++ third_party/WebKit/Source/devtools/front_end/ui/FilterBar.js
@@ -266,6 +266,14 @@ WebInspector.TextFilterUI.prototype = {
     },
 
     /**
+     * @return {boolean}
+     */
+    isRegexChecked: function()
+    {
+        return this._supportRegex ? this._regexCheckBox.checked : false;
+    },
+
+    /**
      * @return {string}
      */
     value: function()
@@ -342,6 +350,11 @@ WebInspector.TextFilterUI.prototype = {
     {
         if (!this._suggestionBuilder)
             return;
+        if (this.isRegexChecked()) {
+            if (this._suggestBox.visible())
+                this._suggestBox.hide();
+            return;
+        }
         var suggestions = this._suggestionBuilder.buildSuggestions(this._filterInputElement);
         if (suggestions && suggestions.length) {
             if (this._suppressSuggestion)
@@ -370,7 +383,7 @@ WebInspector.TextFilterUI.prototype = {
         this._regex = null;
         this._filterInputElement.classList.remove("filter-text-invalid");
         if (filterQuery) {
-            if (this._supportRegex && this._regexCheckBox.checked) {
+            if (this.isRegexChecked()) {
                 try {
                     this._regex = new RegExp(filterQuery, "i");
                 } catch (e) {
