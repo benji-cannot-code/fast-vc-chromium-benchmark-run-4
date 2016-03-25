06d894ff077ad74f17fbf48e645e24cb86764fb9
dcheng <dcheng@chromium.org>
2016-03-24 17:15:56 -0700

    Remove more headers from base/memory/scoped_ptr.h
    BUG=554298
    
    Review URL: https://codereview.chromium.org/1830183004
    
    Cr-Commit-Position: refs/heads/master@{#383199}

--- base/test/test_discardable_memory_allocator.cc
+++ base/test/test_discardable_memory_allocator.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdint>
 #include <cstring>
 
+#include "base/logging.h"
 #include "base/memory/discardable_memory.h"
 
 namespace base {
