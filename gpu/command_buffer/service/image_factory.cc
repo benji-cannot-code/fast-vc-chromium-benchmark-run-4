8d0dd67fbcbe5504927982ed69f11a69d8c63c88
erikchen <erikchen@chromium.org>
2016-03-24 21:37:08 -0700

    Clean up calls to CreateGpuMemoryBufferImageCHROMIUM.
    This CL should not induce any behavior changes.
    
    IOSurfaces expect a format of GL_BGRA, and an internal format of GL_RGBA.
    Callers of CreateGpuMemoryBufferImageCHROMIUM were incorrectly passing in
    GL_BGRA as |internalformat|, and the implementation of
    CreateGpuMemoryBufferImageCHROMIUM was incorrectly allowing this. This CL
    updates callsites to pass in GL_RGBA, and updates the implementation of
    CreateGpuMemoryBufferImageCHROMIUM to not accept GL_BGRA.
    
    BUG=595948
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1832923002
    
    Cr-Commit-Position: refs/heads/master@{#383243}

--- gpu/command_buffer/service/image_factory.cc
+++ gpu/command_buffer/service/image_factory.cc
@@ -10,15 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 
-ImageFactory::ImageFactory() {
-}
-
-ImageFactory::~ImageFactory() {
-}
-
-// static
-gfx::BufferFormat ImageFactory::DefaultBufferFormatForImageFormat(
-    unsigned internalformat) {
+namespace {
+gfx::BufferFormat BufferFormatForInternalFormat(unsigned internalformat) {
   switch (internalformat) {
     case GL_RED:
       return gfx::BufferFormat::R_8;
@@ -50,6 +43,28 @@ gfx::BufferFormat ImageFactory::DefaultBufferFormatForImageFormat(
   }
 }
 
+}  // namespace
+
+ImageFactory::ImageFactory() {
+}
+
+ImageFactory::~ImageFactory() {
+}
+
+// static
+gfx::BufferFormat ImageFactory::DefaultBufferFormatForImageFormat(
+    unsigned internalformat) {
+  switch (internalformat) {
+    case GL_RGB:
+      return gfx::BufferFormat::BGRX_8888;
+    case GL_RGBA:
+      return gfx::BufferFormat::RGBA_8888;
+    default:
+      NOTREACHED();
+      return gfx::BufferFormat::RGBA_8888;
+  }
+}
+
 // static
 bool ImageFactory::IsImageFormatCompatibleWithGpuMemoryBufferFormat(
     unsigned internalformat,
@@ -68,7 +83,7 @@ bool ImageFactory::IsImageFormatCompatibleWithGpuMemoryBufferFormat(
     case gfx::BufferFormat::YUV_420:
     case gfx::BufferFormat::YUV_420_BIPLANAR:
     case gfx::BufferFormat::UYVY_422:
-      return format == DefaultBufferFormatForImageFormat(internalformat);
+      return format == BufferFormatForInternalFormat(internalformat);
     case gfx::BufferFormat::RGBA_4444:
       return internalformat == GL_RGBA;
   }
