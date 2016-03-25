351743920e74d093a92f4786237b518063c985fc
juncai <juncai@chromium.org>
2016-03-25 14:07:16 -0700

    Re-use NoUnderlineClickableSpan which shows a clickable link with underlines turned off
    A new class NoUnderlineClickableSpan which shows a
    clickable link with underlines turned off was added in
    patch:
    https://codereview.chromium.org/1739523002/
    
    This patch modified code which can re-use this class.
    
    BUG=593547
    
    Review URL: https://codereview.chromium.org/1774243003
    
    Cr-Commit-Position: refs/heads/master@{#383359}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchOptOutPromo.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/contextualsearch/ContextualSearchOptOutPromo.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.compositor.bottombar.contextualsearch;
 
 import android.content.Context;
 import android.text.method.LinkMovementMethod;
-import android.text.style.ClickableSpan;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +15,7 @@ import android.widget.TextView;
 
 import org.chromium.chrome.R;
 import org.chromium.ui.resources.dynamics.ViewResourceAdapter;
+import org.chromium.ui.text.NoUnderlineClickableSpan;
 import org.chromium.ui.text.SpanApplier;
 import org.chromium.ui.text.SpanApplier.SpanInfo;
 
@@ -86,17 +86,10 @@ public class ContextualSearchOptOutPromo extends RelativeLayout {
         // Fill in text with link to Settings.
         TextView optOutText = (TextView) findViewById(R.id.contextual_search_opt_out_text);
 
-        ClickableSpan settingsLink = new ClickableSpan() {
+        NoUnderlineClickableSpan settingsLink = new NoUnderlineClickableSpan() {
             @Override
             public void onClick(View view) {
                 mHost.onPromoPreferenceClick();
-            }
-
-            // Disable underline on the link text.
-            @Override
-            public void updateDrawState(android.text.TextPaint textPaint) {
-                super.updateDrawState(textPaint);
-                textPaint.setUnderlineText(false);
             }
         };
 
