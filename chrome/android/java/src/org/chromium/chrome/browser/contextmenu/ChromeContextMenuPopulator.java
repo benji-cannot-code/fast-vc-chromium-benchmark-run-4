84a7a705be44120379189515f04566dd7bcbb3ad
petewil <petewil@chromium.org>
2016-03-25 11:04:13 -0700

    Add menu item for testing to save pages offline.
    When we enable background loading of offline pages, we want a menu item
    to help us test the feature (and maybe this will be good for the product
    someday).
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1776813004
    
    Cr-Commit-Position: refs/heads/master@{#383303}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator.java
@@ -13,13 +13,17 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.webkit.MimeTypeMap;
 
+import org.chromium.base.Log;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.net.spdyproxy.DataReductionProxySettings;
+import org.chromium.chrome.browser.offlinepages.OfflinePageBridge;
 import org.chromium.chrome.browser.preferences.datareduction.DataReductionProxyUma;
 import org.chromium.chrome.browser.search_engines.TemplateUrlService;
 import org.chromium.chrome.browser.util.UrlUtilities;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 
 /**
@@ -29,6 +33,7 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
     public static final int NORMAL_MODE = 0;
     public static final int CUSTOM_TAB_MODE = 1;
     public static final int FULLSCREEN_TAB_MODE = 2;
+    private static final String TAG = "CCMenuPopulator";
 
     // Items that are included in all context menus.
     private static final int[] BASE_WHITELIST = {
@@ -50,6 +55,7 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
             R.id.contextmenu_load_original_image,
             R.id.contextmenu_open_image_in_new_tab,
             R.id.contextmenu_search_by_image,
+            R.id.contextmenu_save_offline,
     };
 
     // Additional items for custom tabs mode.
@@ -195,6 +201,14 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
         menu.findItem(R.id.contextmenu_save_link_as).setVisible(
                 UrlUtilities.isDownloadableScheme(params.getLinkUrl()));
 
+        // Only enable the save as offline feature if OfflinePagesBackgroundLoading is enabled, and
+        // it looks like a web page.
+        boolean showSaveOfflineMenuItem =
+                shouldShowBackgroundLoadingContextMenu(params.getLinkUrl());
+
+        menu.findItem(R.id.contextmenu_save_offline)
+                .setVisible(showSaveOfflineMenuItem);
+
         if (params.imageWasFetchedLoFi()
                 || !DataReductionProxySettings.getInstance().wasLoFiModeActiveOnMainFrame()
                 || !DataReductionProxySettings.getInstance().canUseDataReductionProxy(
@@ -329,6 +343,9 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
                 ContextMenuUma.recordSaveLinkTypes(url);
                 helper.startContextMenuDownload(true, false);
             }
+        } else if (itemId == R.id.contextmenu_save_offline) {
+            // TODO(petewil): Add code here to save the page offline.
+            Log.d(TAG, "Save an offline copy of the linked page");
         } else if (itemId == R.id.contextmenu_search_by_image) {
             ContextMenuUma.record(params, ContextMenuUma.ACTION_SEARCH_BY_IMAGE);
             helper.searchForImage();
@@ -342,6 +359,34 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
         }
 
         return true;
+    }
+
+    /**
+     * Return true if we should show a context menu for background loading.  Make sure the uri looks
+     * like a web page (scheme is http or https) and has mime type of unknown or text.
+     * @param uriString The uri that we are showing a context menu on.
+     */
+    private static boolean shouldShowBackgroundLoadingContextMenu(String uriString) {
+
+        if (!OfflinePageBridge.isBackgroundLoadingEnabled()) return false;
+
+        URI uri = null;
+        try {
+            uri = new URI(uriString);
+        } catch (URISyntaxException e) {
+            Log.e(TAG, "Trying to parse a link that is not a URI " + e);
+            return false;
+        }
+
+        String scheme = uri.getScheme();
+        String extension = MimeTypeMap.getFileExtensionFromUrl(uriString);
+        String type = null;
+        if (extension != null) {
+            type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+        }
+
+        return (scheme.equals("http") || scheme.equals("https"))
+                && (type == null || type.startsWith("text"));
     }
 
     private void setHeaderText(Context context, ContextMenu menu, String text) {
