4d10594680a8e6df4d85a368dbbfac5806e5a8f0
xhwang <xhwang@chromium.org>
2016-03-27 21:33:31 -0700

    media: Only provide necessary services in ServiceFactoryImpl
    This helps avoid including code that will never be used. On Chrome for
    Android, this reduces the final Chrome apk binary size by 5.2kB.
    
    BUG=521731
    TEST=No functionality change.
    
    Review URL: https://codereview.chromium.org/1840453003
    
    Cr-Commit-Position: refs/heads/master@{#383479}

--- media/mojo/services/service_factory_impl.cc
+++ media/mojo/services/service_factory_impl.cc
@@ -6,14 +6,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/mojo/services/service_factory_impl.h"
 
 #include "base/logging.h"
-#include "media/base/cdm_factory.h"
 #include "media/base/media_log.h"
-#include "media/base/renderer_factory.h"
-#include "media/mojo/services/mojo_audio_decoder_service.h"
-#include "media/mojo/services/mojo_cdm_service.h"
 #include "media/mojo/services/mojo_media_client.h"
-#include "media/mojo/services/mojo_renderer_service.h"
 #include "mojo/shell/public/interfaces/interface_provider.mojom.h"
+
+#if defined(ENABLE_MOJO_AUDIO_DECODER)
+#include "media/mojo/services/mojo_audio_decoder_service.h"
+#endif  // defined(ENABLE_MOJO_AUDIO_DECODER)
+
+#if defined(ENABLE_MOJO_RENDERER)
+#include "media/base/renderer_factory.h"
+#include "media/mojo/services/mojo_renderer_service.h"
+#endif  // defined(ENABLE_MOJO_RENDERER)
+
+#if defined(ENABLE_MOJO_CDM)
+#include "media/base/cdm_factory.h"
+#include "media/mojo/services/mojo_cdm_service.h"
+#endif  // defined(ENABLE_MOJO_CDM)
 
 namespace media {
 
@@ -39,6 +48,7 @@ ServiceFactoryImpl::~ServiceFactoryImpl() {
 
 void ServiceFactoryImpl::CreateAudioDecoder(
     mojo::InterfaceRequest<interfaces::AudioDecoder> request) {
+#if defined(ENABLE_MOJO_AUDIO_DECODER)
   scoped_refptr<base::SingleThreadTaskRunner> task_runner(
       base::MessageLoop::current()->task_runner());
 
@@ -51,10 +61,12 @@ void ServiceFactoryImpl::CreateAudioDecoder(
 
   new MojoAudioDecoderService(cdm_service_context_.GetWeakPtr(),
                               std::move(audio_decoder), std::move(request));
+#endif  // defined(ENABLE_MOJO_AUDIO_DECODER)
 }
 
 void ServiceFactoryImpl::CreateRenderer(
     mojo::InterfaceRequest<interfaces::Renderer> request) {
+#if defined(ENABLE_MOJO_RENDERER)
   // The created object is owned by the pipe.
   // The audio and video sinks are owned by the client.
   scoped_refptr<base::SingleThreadTaskRunner> task_runner(
@@ -78,10 +90,12 @@ void ServiceFactoryImpl::CreateRenderer(
 
   new MojoRendererService(cdm_service_context_.GetWeakPtr(),
                           std::move(renderer), std::move(request));
+#endif  // defined(ENABLE_MOJO_RENDERER)
 }
 
 void ServiceFactoryImpl::CreateCdm(
     mojo::InterfaceRequest<interfaces::ContentDecryptionModule> request) {
+#if defined(ENABLE_MOJO_CDM)
   CdmFactory* cdm_factory = GetCdmFactory();
   if (!cdm_factory)
     return;
@@ -89,8 +103,10 @@ void ServiceFactoryImpl::CreateCdm(
   // The created object is owned by the pipe.
   new MojoCdmService(cdm_service_context_.GetWeakPtr(), cdm_factory,
                      std::move(request));
+#endif  // defined(ENABLE_MOJO_CDM)
 }
 
+#if defined(ENABLE_MOJO_RENDERER)
 RendererFactory* ServiceFactoryImpl::GetRendererFactory() {
   if (!renderer_factory_) {
     renderer_factory_ = mojo_media_client_->CreateRendererFactory(media_log_);
@@ -98,7 +114,9 @@ RendererFactory* ServiceFactoryImpl::GetRendererFactory() {
   }
   return renderer_factory_.get();
 }
+#endif  // defined(ENABLE_MOJO_RENDERER)
 
+#if defined(ENABLE_MOJO_CDM)
 CdmFactory* ServiceFactoryImpl::GetCdmFactory() {
   if (!cdm_factory_) {
     cdm_factory_ = mojo_media_client_->CreateCdmFactory(interfaces_);
@@ -106,5 +124,6 @@ CdmFactory* ServiceFactoryImpl::GetCdmFactory() {
   }
   return cdm_factory_.get();
 }
+#endif  // defined(ENABLE_MOJO_CDM)
 
 }  // namespace media
