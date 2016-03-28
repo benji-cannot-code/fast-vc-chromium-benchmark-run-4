91801ff6688e45ca7fe8187cf683fe593a55c660
mmenke <mmenke@chromium.org>
2016-03-28 08:29:55 -0700

    Remove net::EscapeRules::URL_SPECIAL_CHARS
    We're removing it in favor of PATH_SEPARATORS and
    URL_SPECIAL_CHARS_EXCEPT_PATH_SEPARATORS, to reduce
    the chances of security issues with unescaping going
    forward.
    
    BUG=589257
    
    Review URL: https://codereview.chromium.org/1831123002
    
    Cr-Commit-Position: refs/heads/master@{#383494}

--- net/base/data_url.cc
+++ net/base/data_url.cc
@@ -100,9 +100,10 @@ bool DataURL::Parse(const GURL& url, std::string* mime_type,
   // of the data, and should be stripped. Otherwise, the escaped whitespace
   // could be part of the payload, so don't strip it.
   if (base64_encoded) {
-    temp_data = UnescapeURLComponent(temp_data,
-        UnescapeRule::SPACES | UnescapeRule::URL_SPECIAL_CHARS |
-        UnescapeRule::SPOOFING_AND_CONTROL_CHARS);
+    temp_data = UnescapeURLComponent(
+        temp_data, UnescapeRule::SPACES | UnescapeRule::PATH_SEPARATORS |
+                       UnescapeRule::URL_SPECIAL_CHARS_EXCEPT_PATH_SEPARATORS |
+                       UnescapeRule::SPOOFING_AND_CONTROL_CHARS);
   }
 
   // Strip whitespace.
@@ -114,9 +115,10 @@ bool DataURL::Parse(const GURL& url, std::string* mime_type,
   }
 
   if (!base64_encoded) {
-    temp_data = UnescapeURLComponent(temp_data,
-        UnescapeRule::SPACES | UnescapeRule::URL_SPECIAL_CHARS |
-        UnescapeRule::SPOOFING_AND_CONTROL_CHARS);
+    temp_data = UnescapeURLComponent(
+        temp_data, UnescapeRule::SPACES | UnescapeRule::PATH_SEPARATORS |
+                       UnescapeRule::URL_SPECIAL_CHARS_EXCEPT_PATH_SEPARATORS |
+                       UnescapeRule::SPOOFING_AND_CONTROL_CHARS);
   }
 
   if (base64_encoded) {
