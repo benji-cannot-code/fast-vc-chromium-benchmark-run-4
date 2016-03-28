23d9c5af9b5b24b50d16590f476c08b9d8c63f4d
newt <newt@chromium.org>
2016-03-28 11:21:26 -0700

    Fix navigation to History from Recent Tabs.
    Don't dismiss the Recent Tabs dialog under after navigation begins.
    Otherwise the RecentTabsManager will already be destroyed and the
    navigation will be ignored.
    
    BUG=598247
    
    Review URL: https://codereview.chromium.org/1835963002
    
    Cr-Commit-Position: refs/heads/master@{#383522}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/DocumentRecentTabsManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/DocumentRecentTabsManager.java
@@ -143,8 +143,8 @@ public class DocumentRecentTabsManager extends RecentTabsManager {
 
     @Override
     public void openHistoryPage() {
-        if (mDialog != null) mDialog.dismiss();
         super.openHistoryPage();
+        if (mDialog != null) mDialog.dismiss();
     }
 
     @Override
