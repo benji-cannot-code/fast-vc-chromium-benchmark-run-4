4236195b9b328f151463467db1bb591f80b9d144
mmenke <mmenke@chromium.org>
2016-03-28 14:34:53 -0700

    Prevent URLFetcher::AppendChunkedData from dereferencing NULL pointers.
    Previously, this would happen if it was called after a request failed,
    while a request was being throttled, or if a request was redirected and
    lost its body without having sent the entire upload body (Either
    redirected by stuff outside net with never uploading the body, or
    redirected by stuff outside net after a network error).
    
    BUG=568091
    
    Review URL: https://codereview.chromium.org/1732493002
    
    Cr-Commit-Position: refs/heads/master@{#383571}

--- net/url_request/url_request.cc
+++ net/url_request/url_request.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/values.h"
 #include "net/base/auth.h"
-#include "net/base/chunked_upload_data_stream.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/load_flags.h"
 #include "net/base/load_timing_info.h"
@@ -190,22 +189,6 @@ URLRequest::~URLRequest() {
   if (status_.status() == URLRequestStatus::FAILED)
     net_error = status_.error();
   net_log_.EndEventWithNetErrorCode(NetLog::TYPE_REQUEST_ALIVE, net_error);
-}
-
-void URLRequest::EnableChunkedUpload() {
-  DCHECK(!upload_data_stream_ || upload_data_stream_->is_chunked());
-  if (!upload_data_stream_) {
-    upload_chunked_data_stream_ = new ChunkedUploadDataStream(0);
-    upload_data_stream_.reset(upload_chunked_data_stream_);
-  }
-}
-
-void URLRequest::AppendChunkToUpload(const char* bytes,
-                                     int bytes_len,
-                                     bool is_last_chunk) {
-  DCHECK(upload_data_stream_);
-  DCHECK(upload_data_stream_->is_chunked());
-  upload_chunked_data_stream_->AppendData(bytes, bytes_len, is_last_chunk);
 }
 
 void URLRequest::set_upload(scoped_ptr<UploadDataStream> upload) {
