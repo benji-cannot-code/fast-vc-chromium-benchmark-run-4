68c90d57b0705ba7064577c394aa024ef7398ba8
justincohen <justincohen@chromium.org>
2016-03-28 08:48:09 -0700

    Followup to 'Scripts to upload and update the mac toolchain.'
    Incorrectly reverted this file before landing.  Make sure DEVELOPER_DIR
    is set before calling in to GYP.
    
    Original CL here: https://codereview.chromium.org/1759793003/
    
    BUG=474373
    TBR=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/1834183002
    
    Cr-Commit-Position: refs/heads/master@{#383497}

--- build/mac_toolchain.py
+++ build/mac_toolchain.py
@@ -41,6 +41,8 @@ TOOLCHAIN_URL = 'gs://chrome-mac-sdk/'
 def GetToolchainDirectory():
   if sys.platform == 'darwin' and not UseLocalMacSDK():
     return TOOLCHAIN_BUILD_DIR
+  else:
+    return None
 
 
 def ReadStampFile():
