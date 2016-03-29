bd9333ac48c34cf1205c0e607957b83933f0ce3b
erg <erg@chromium.org>
2016-03-29 13:09:34 -0700

    mojo leveldb: Remove the created file thread.
    This runs what was a background thread for file operations on the same
    thread as the mojo application, which is the db thread in chrome, and
    the main thread when run as an app.
    
    BUG=585587
    
    Review URL: https://codereview.chromium.org/1839823002
    
    Cr-Commit-Position: refs/heads/master@{#383799}

--- components/leveldb/leveldb_service_impl.cc
+++ components/leveldb/leveldb_service_impl.cc
@@ -17,7 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace leveldb {
 
-LevelDBServiceImpl::LevelDBServiceImpl() : thread_(new LevelDBFileThread) {}
+LevelDBServiceImpl::LevelDBServiceImpl(
+    scoped_refptr<base::SingleThreadTaskRunner> task_runner)
+    : thread_(new LevelDBMojoProxy(std::move(task_runner))) {}
 
 LevelDBServiceImpl::~LevelDBServiceImpl() {}
 
@@ -38,7 +40,7 @@ void LevelDBServiceImpl::Open(filesystem::DirectoryPtr directory,
   options.max_open_files = 80;
 
   // Register our directory with the file thread.
-  LevelDBFileThread::OpaqueDir* dir =
+  LevelDBMojoProxy::OpaqueDir* dir =
       thread_->RegisterDirectory(std::move(directory));
 
   scoped_ptr<MojoEnv> env_mojo(new MojoEnv(thread_, dir));
