2fe78adaf3662d8a63c9c3d0bd731c604ec530aa
msw <msw@chromium.org>
2016-03-29 13:11:01 -0700

    Refine ash shelf tooltip closing on non-mash ChromeOS.
    Fixes a regression from: https://codereview.chromium.org/1816753002
    Also reverse expected behavior for touching tooltips.
    (closes on stable, tests expected them to stay open...)
    
    Inline bubble function definitions.
    Use WindowObserver to remove the handler on window destruction.
    TODO: Encapsulate more ShelfLayoutManager code.
    
    BUG=595853
    TEST=ChromeOS tooltips stay open on hover; close for external touches.
    R=sky@chromium.org
    
    Committed: https://crrev.com/1a0d283fa81e19b47fb85523f28c4a03f7dc1d24
    Cr-Commit-Position: refs/heads/master@{#383404}
    
    Committed: https://crrev.com/c2aa7f5e8870c04c8aff0fd5da27820636e53e38
    Cr-Commit-Position: refs/heads/master@{#383579}
    
    Review URL: https://codereview.chromium.org/1828133004
    
    Cr-Commit-Position: refs/heads/master@{#383801}

--- ash/shelf/shelf_tooltip_manager.h
+++ ash/shelf/shelf_tooltip_manager.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
 #include "base/timer/timer.h"
+#include "ui/aura/window_observer.h"
 #include "ui/events/event_handler.h"
 
 namespace views {
@@ -30,6 +31,7 @@ class ShelfViewTest;
 
 // ShelfTooltipManager manages the tooltip bubble that appears for shelf items.
 class ASH_EXPORT ShelfTooltipManager : public ui::EventHandler,
+                                       public aura::WindowObserver,
                                        public ShelfLayoutManagerObserver {
  public:
   explicit ShelfTooltipManager(ShelfView* shelf_view);
@@ -58,6 +60,9 @@ class ASH_EXPORT ShelfTooltipManager : public ui::EventHandler,
   // ui::EventHandler overrides:
   void OnEvent(ui::Event* event) override;
 
+  // aura::WindowObserver overrides:
+  void OnWindowDestroying(aura::Window* window) override;
+
   // ShelfLayoutManagerObserver overrides:
   void WillDeleteShelf() override;
   void WillChangeVisibilityState(ShelfVisibilityState new_state) override;
@@ -72,6 +77,7 @@ class ASH_EXPORT ShelfTooltipManager : public ui::EventHandler,
   base::OneShotTimer timer_;
 
   ShelfView* shelf_view_;
+  aura::Window* root_window_;
   ShelfLayoutManager* shelf_layout_manager_;
   views::BubbleDelegateView* bubble_;
 
