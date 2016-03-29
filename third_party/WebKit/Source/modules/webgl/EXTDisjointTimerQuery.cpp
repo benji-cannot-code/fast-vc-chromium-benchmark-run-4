29db476e73c95ac71982e2c8834082fb2f2e1a77
ewell <ewell@google.com>
2016-03-29 14:05:50 -0700

    Disable timestamps in WebGL.
    The timestamp portion of the EXT_disjoint_timer_query does not have much
    support from GPU drivers and emulation of them by Chromium at a level
    that is accurate enough for users has been determined to be infeasible.
    Therefore timestamps are disabled at the WebGL/client side on all
    platforms as time elasped queries should be used by developers instead
    as they are more widely supported.
    
    BUG=595172
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1800383002
    
    Cr-Commit-Position: refs/heads/master@{#383822}

--- third_party/WebKit/Source/modules/webgl/EXTDisjointTimerQuery.cpp
+++ third_party/WebKit/Source/modules/webgl/EXTDisjointTimerQuery.cpp
@@ -142,7 +142,7 @@ void EXTDisjointTimerQuery::queryCounterEXT(WebGLTimerQueryEXT* query, GLenum ta
         return;
     }
 
-    scoped.context()->contextGL()->QueryCounterEXT(query->object(), target);
+    // Timestamps are disabled in WebGL due to lack of driver support on multiple platforms, so we don't actually perform a GL call
     query->setTarget(target);
     query->resetCachedResult();
 }
