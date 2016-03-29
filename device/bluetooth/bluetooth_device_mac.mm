21ee1372ee4049bcd9e7b1d8a932c8a3eb41b4a5
jlebel <jlebel@chromium.org>
2016-03-29 03:30:25 -0700

    Adding the last test related with connect/disconnect.
    This CL adds better support with error code and NSError conversion.
    
    BUG=520774
    
    Review URL: https://codereview.chromium.org/1685963003
    
    Cr-Commit-Position: refs/heads/master@{#383698}

--- device/bluetooth/bluetooth_device_mac.mm
+++ device/bluetooth/bluetooth_device_mac.mm
@@ -7,12 +7,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "device/bluetooth/bluetooth_adapter_mac.h"
 
+static NSString* const kErrorDomain = @"ConnectErrorCode";
+
 namespace device {
 
 BluetoothDeviceMac::BluetoothDeviceMac(BluetoothAdapterMac* adapter)
     : BluetoothDevice(adapter) {}
 
 BluetoothDeviceMac::~BluetoothDeviceMac() {
+}
+
+NSError* BluetoothDeviceMac::GetNSErrorFromConnectErrorCode(
+    BluetoothDevice::ConnectErrorCode error_code) {
+  // TODO(http://crbug.com/585894): Need to convert the error.
+  return [NSError errorWithDomain:kErrorDomain code:error_code userInfo:nil];
+}
+
+BluetoothDevice::ConnectErrorCode
+BluetoothDeviceMac::GetConnectErrorCodeFromNSError(NSError* error) {
+  if ([error.domain isEqualToString:kErrorDomain]) {
+    BluetoothDevice::ConnectErrorCode connect_error_code =
+        (BluetoothDevice::ConnectErrorCode)error.code;
+    if (connect_error_code >= 0 ||
+        connect_error_code < BluetoothDevice::NUM_CONNECT_ERROR_CODES) {
+      return connect_error_code;
+    }
+    DCHECK(false);
+    return BluetoothDevice::ERROR_FAILED;
+  }
+  // TODO(http://crbug.com/585894): Need to convert the error.
+  return BluetoothDevice::ERROR_FAILED;
 }
 
 }  // namespace device
