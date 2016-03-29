db7344b76056083efc6dce377889ab0a4a533de6
esum <esum@chromium.org>
2016-03-29 11:17:52 -0700

    [Chromecast] Add ctime include to DumpInfo.
    DumpInfo uses time_t, so it should directly include header that
    declares it.
    
    BUG=None
    TEST=Asan desktop build.
    
    Review URL: https://codereview.chromium.org/1843703002
    
    Cr-Commit-Position: refs/heads/master@{#383774}

--- chromecast/crash/linux/dump_info.h
+++ chromecast/crash/linux/dump_info.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMECAST_CRASH_LINUX_DUMP_INFO_H_
 #define CHROMECAST_CRASH_LINUX_DUMP_INFO_H_
 
+#include <ctime>
 #include <string>
 
 #include "base/macros.h"
