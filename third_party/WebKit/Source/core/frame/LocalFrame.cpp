690d653bf1209e6fd8bbd70ddde0f1e1d4e44819
pilgrim <pilgrim@chromium.org>
2016-03-29 11:52:15 -0700

    [Layout API] Add LocalFrame::contentLayoutItem and use in VisualViewport
    LocalFrame has a public contentLayoutObject() method that returns a
    LayoutView*. This needs to go away eventually. To ease migration, I've
    added a contentLayoutItem() method next to it that returns a
    LayoutViewItem. I fixed once reference in VisualViewport to call
    contentLayoutItem() instead of contentLayoutObject() (to access a method
    that already exists in LayoutViewItem, so no new API methods required).
    I also simplified some code in FrameView that had been working around
    the fact that LocalFrame didn't offer a method that returned a
    LayoutViewItem.
    
    There are no functional changes.
    
    BUG=499321
    
    Review URL: https://codereview.chromium.org/1843603002
    
    Cr-Commit-Position: refs/heads/master@{#383783}

--- third_party/WebKit/Source/core/frame/LocalFrame.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrame.cpp
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/InstrumentingAgents.h"
 #include "core/layout/HitTestResult.h"
 #include "core/layout/LayoutView.h"
+#include "core/layout/api/LayoutViewItem.h"
 #include "core/layout/compositing/PaintLayerCompositor.h"
 #include "core/loader/FrameLoadRequest.h"
 #include "core/loader/FrameLoaderClient.h"
@@ -450,6 +451,11 @@ void LocalFrame::setPagePopupOwner(Element& owner)
 LayoutView* LocalFrame::contentLayoutObject() const
 {
     return document() ? document()->layoutView() : nullptr;
+}
+
+LayoutViewItem LocalFrame::contentLayoutItem() const
+{
+    return LayoutViewItem(contentLayoutObject());
 }
 
 void LocalFrame::didChangeVisibilityState()
--- third_party/WebKit/Source/core/frame/LocalFrame.h
+++ third_party/WebKit/Source/core/frame/LocalFrame.h
@@ -63,6 +63,7 @@ class InputMethodController;
 class IntPoint;
 class IntSize;
 class InstrumentingAgents;
+class LayoutViewItem;
 class LocalDOMWindow;
 class NavigationScheduler;
 class Node;
@@ -113,6 +114,7 @@ public:
     Element* pagePopupOwner() const { return m_pagePopupOwner.get(); }
 
     LayoutView* contentLayoutObject() const; // Root of the layout tree for the document contained in this frame.
+    LayoutViewItem contentLayoutItem() const;
 
     Editor& editor() const;
     EventHandler& eventHandler() const;
