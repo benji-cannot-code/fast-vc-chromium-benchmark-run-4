aadc8ec72ea2cd89ebc5821369df8fb458f4e6be
wkorman <wkorman@chromium.org>
2016-03-28 22:31:54 -0700

    Pass graphics layer to GraphicsLayerClient::needsRepaint().
    Some clients, for example CompositedLayerMapping, manage multiple
    graphics layers and may need to make layer-specific decisions w.r.t.
    whether the layer needs repainting.
    
    Precursor for work in http://crrev.com/1830333002 to report visual
    rects for non-frame composited scrollbars properly.
    
    BUG=529938
    
    Review URL: https://codereview.chromium.org/1827083006
    
    Cr-Commit-Position: refs/heads/master@{#383671}

--- third_party/WebKit/Source/platform/graphics/GraphicsLayer.cpp
+++ third_party/WebKit/Source/platform/graphics/GraphicsLayer.cpp
@@ -335,7 +335,7 @@ bool GraphicsLayer::paintWithoutCommit(const IntRect* interestRect, GraphicsCont
     }
 
     if (!getPaintController().subsequenceCachingIsDisabled()
-        && !m_client->needsRepaint()
+        && !m_client->needsRepaint(*this)
         && !getPaintController().cacheIsEmpty()
         && m_previousInterestRect == *interestRect) {
         ASSERT(!getPaintController().hasInvalidations());
