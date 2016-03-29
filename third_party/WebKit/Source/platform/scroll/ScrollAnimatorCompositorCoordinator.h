39b52d364f0e6f389f3bfe49509b83593b3eed54
ymalik <ymalik@chromium.org>
2016-03-29 16:02:54 -0700

    Correctly reset ScrollAnimator animation state after cancelling animation
    We were calling willAnimaterToOffset with the updated target position before
    clearing the animation state when in PostAnimationCleanup. As a result, we
    didn't take into account the potentially modified scroll offset by a
    programmatic scroll.
    
    BUG=598548
    
    Review URL: https://codereview.chromium.org/1842623003
    
    Cr-Commit-Position: refs/heads/master@{#383854}

--- third_party/WebKit/Source/platform/scroll/ScrollAnimatorCompositorCoordinator.h
+++ third_party/WebKit/Source/platform/scroll/ScrollAnimatorCompositorCoordinator.h
@@ -82,6 +82,7 @@ protected:
     friend class Internals;
     FRIEND_TEST_ALL_PREFIXES(ScrollAnimatorTest, MainThreadStates);
     FRIEND_TEST_ALL_PREFIXES(ScrollAnimatorTest, AnimatedScrollTakeover);
+    FRIEND_TEST_ALL_PREFIXES(ScrollAnimatorTest, CancellingAnimationResetsState);
 
     enum class RunState {
         // No animation.
