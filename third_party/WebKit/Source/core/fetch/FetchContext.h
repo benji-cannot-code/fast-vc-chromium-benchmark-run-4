a123584e1557fda2600a0ea5bc6ef919b3f3bc1c
dproy <dproy@google.com>
2016-03-29 14:35:43 -0700

    Attach requestId to resource fetch trace
    We want to connect the source of a resource request with the resource fetch event. Adding the requestId to resource fetch is the first step.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1815383003
    
    Cr-Commit-Position: refs/heads/master@{#383834}

--- third_party/WebKit/Source/core/fetch/FetchContext.h
+++ third_party/WebKit/Source/core/fetch/FetchContext.h
@@ -85,7 +85,7 @@ public:
     virtual void dispatchDidFail(unsigned long identifier, const ResourceError&, bool isInternalRequest);
 
     virtual bool shouldLoadNewResource(Resource::Type) const { return false; }
-    virtual void willStartLoadingResource(ResourceRequest&);
+    virtual void willStartLoadingResource(Resource*, ResourceRequest&);
     virtual void didLoadResource(Resource*);
 
     virtual void addResourceTiming(const ResourceTimingInfo&);
