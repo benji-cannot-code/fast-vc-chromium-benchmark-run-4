15a7e584651c982ef727f65beee84733d3fb1b67
xidachen <xidachen@chromium.org>
2016-03-30 12:12:14 -0700

    Reject promise with InvalidStateError instead of null in createImageBitmap(Blob)
    Right now, the createImageBitmap(Blob) reject promise with null when
    the input image cannot be decoded, for example, the input image is a SVG
    image. A recently change is made to the specs here:
    https://github.com/whatwg/html/pull/884
    which states that we should reject the promise with InvalidStateError
    insetad of null.
    
    This CL changes it such that it will reject the promise with a
    InvalidStateError.
    
    BUG=596251
    
    Review URL: https://codereview.chromium.org/1841313003
    
    Cr-Commit-Position: refs/heads/master@{#384045}

--- third_party/WebKit/LayoutTests/fast/canvas/resources/canvas-createImageBitmap-invalid-blob-in-workers.js
+++ third_party/WebKit/LayoutTests/fast/canvas/resources/canvas-createImageBitmap-invalid-blob-in-workers.js
@@ -9,8 +9,8 @@ self.addEventListener('message', function(e) {
   createImageBitmap(e.data).then(function() {
     testFailed('Promise fulfuilled.');
     finishJSTest();
-  }, function() {
-    testPassed('Promise rejected.');
+  }, function(ex) {
+    testPassed('Promise rejected: ' + ex);
     finishJSTest();
   });
 });
