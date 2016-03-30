1e0a35790f47ef393752c88c63182c8e4de62cf2
dalecurtis <dalecurtis@chromium.org>
2016-03-29 18:06:58 -0700

    Allow play/pause on default controls to clear media errors.
    Simple change to the default controls that lets the user continue
    playback with subsequent play/pause button presses. Useful in cases
    where the error is transient (e.g., network related).
    
    In the event of an error the default controls will now pause playback.
    
    It'd be nice if we could restore current time in this case too, but
    it seems to be cleared in unpredictable ways upon decode error.
    
    BUG=none
    TEST=play after decode / network error restarts from beginning.
    
    Review URL: https://codereview.chromium.org/1827573004
    
    Cr-Commit-Position: refs/heads/master@{#383884}

--- third_party/WebKit/Source/core/html/shadow/MediaControls.cpp
+++ third_party/WebKit/Source/core/html/shadow/MediaControls.cpp
@@ -253,6 +253,10 @@ void MediaControls::reset()
         m_timeline->setIsWanted(true);
     }
 
+    // If the player has entered an error state, force it into the paused state.
+    if (mediaElement().error())
+        mediaElement().pause();
+
     updatePlayState();
 
     updateCurrentTimeDisplay();
