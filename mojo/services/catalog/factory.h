a0c834daf2e3b2deb650e2b565010d9eb40c08ba
ben <ben@chromium.org>
2016-03-30 13:44:54 -0700

    Implement a system-level entry cache so that packages loaded from the system dir can be visible to all user catalogs.
    This fixes the blank entries in the task viewer.
    
    R=sky@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1844973002
    
    Cr-Commit-Position: refs/heads/master@{#384083}

--- mojo/services/catalog/factory.h
+++ mojo/services/catalog/factory.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding.h"
 #include "mojo/services/catalog/public/interfaces/catalog.mojom.h"
 #include "mojo/services/catalog/public/interfaces/resolver.mojom.h"
+#include "mojo/services/catalog/types.h"
 #include "mojo/shell/public/cpp/shell_client.h"
 #include "mojo/shell/public/interfaces/shell_client.mojom.h"
 #include "mojo/shell/public/interfaces/shell_resolver.mojom.h"
@@ -70,6 +71,8 @@ class Factory
   scoped_ptr<mojo::ShellConnection> shell_connection_;
 
   std::map<std::string, scoped_ptr<Catalog>> catalogs_;
+
+  EntryCache system_catalog_;
 
   base::WeakPtrFactory<Factory> weak_factory_;
 
