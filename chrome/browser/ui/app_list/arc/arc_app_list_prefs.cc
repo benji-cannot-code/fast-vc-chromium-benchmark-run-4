919b1fd8a3bbd7f15bdb330c3856838700e43aae
cnwan <cnwan@chromium.org>
2016-03-30 00:40:17 -0700

    Add UMA histogram Arc.AppsInstalledAtStartup to measure the number of ARC apps installed.
    BUG=b:27344858
    TEST=Tested on minnie
    R=tapted@chromium.org,jwd@chromium.org
    
    Review URL: https://codereview.chromium.org/1818933002
    
    Cr-Commit-Position: refs/heads/master@{#383924}

--- chrome/browser/ui/app_list/arc/arc_app_list_prefs.cc
+++ chrome/browser/ui/app_list/arc/arc_app_list_prefs.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_util.h"
 #include "base/macros.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/task_runner_util.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_list_prefs_factory.h"
@@ -403,6 +404,11 @@ void ArcAppListPrefs::OnAppListRefreshed(mojo::Array<arc::AppInfoPtr> apps) {
   for (const auto& app_id : old_apps) {
     if (!ready_apps_.count(app_id))
       RemoveApp(app_id);
+  }
+
+  if (!is_initialized_) {
+    is_initialized_ = true;
+    UMA_HISTOGRAM_COUNTS_1000("Arc.AppsInstalledAtStartup", ready_apps_.size());
   }
 }
 
