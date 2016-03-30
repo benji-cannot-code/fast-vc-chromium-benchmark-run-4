de298cba27c0b679f6009f4b0fc0afef54b5dfb5
newt <newt@chromium.org>
2016-03-30 13:02:51 -0700

    Multi-instance Chrome.
    This adds a new activity (ChromeTabbedActivity2) that can run
    side-by-side with ChromeTabbedActivity in multi-window mode.
    
    This also implements the "Open in other window" context menu item, which
    is visible only in multi-window mode and does what you think it does.
    
    BUG=595889
    
    Review URL: https://codereview.chromium.org/1842533003
    
    Cr-Commit-Position: refs/heads/master@{#384066}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator.java
@@ -50,6 +50,7 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
     private static final int[] NORMAL_MODE_WHITELIST = {
             R.id.contextmenu_load_images,
             R.id.contextmenu_open_in_new_tab,
+            R.id.contextmenu_open_in_other_window,
             R.id.contextmenu_open_in_incognito_tab,
             R.id.contextmenu_save_link_as,
             R.id.contextmenu_load_original_image,
@@ -90,7 +91,8 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
         static final int ACTION_LOAD_ORIGINAL_IMAGE = 13;
         static final int ACTION_SAVE_VIDEO = 14;
         static final int ACTION_SHARE_IMAGE = 19;
-        static final int NUM_ACTIONS = 20;
+        static final int ACTION_OPEN_IN_OTHER_WINDOW = 20;
+        static final int NUM_ACTIONS = 21;
 
         // Note: these values must match the ContextMenuSaveLinkType enum in histograms.xml.
         // Only add new values at the end, right before NUM_TYPES. We depend on these specific
@@ -184,6 +186,10 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
         menu.setGroupVisible(R.id.contextmenu_group_image, params.isImage());
         menu.setGroupVisible(R.id.contextmenu_group_video, params.isVideo());
 
+        if (params.isAnchor() && !mDelegate.isOpenInOtherWindowSupported()) {
+            menu.findItem(R.id.contextmenu_open_in_other_window).setVisible(false);
+        }
+
         if (mDelegate.isIncognito() || !mDelegate.isIncognitoSupported()) {
             menu.findItem(R.id.contextmenu_open_in_incognito_tab).setVisible(false);
         }
@@ -206,8 +212,7 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
         boolean showSaveOfflineMenuItem =
                 shouldShowBackgroundLoadingContextMenu(params.getLinkUrl());
 
-        menu.findItem(R.id.contextmenu_save_offline)
-                .setVisible(showSaveOfflineMenuItem);
+        menu.findItem(R.id.contextmenu_save_offline).setVisible(showSaveOfflineMenuItem);
 
         if (params.imageWasFetchedLoFi()
                 || !DataReductionProxySettings.getInstance().wasLoFiModeActiveOnMainFrame()
@@ -286,7 +291,10 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
 
     @Override
     public boolean onItemSelected(ContextMenuHelper helper, ContextMenuParams params, int itemId) {
-        if (itemId == R.id.contextmenu_open_in_new_tab) {
+        if (itemId == R.id.contextmenu_open_in_other_window) {
+            ContextMenuUma.record(params, ContextMenuUma.ACTION_OPEN_IN_OTHER_WINDOW);
+            mDelegate.onOpenInOtherWindow(params.getLinkUrl(), params.getReferrer());
+        } else if (itemId == R.id.contextmenu_open_in_new_tab) {
             ContextMenuUma.record(params, ContextMenuUma.ACTION_OPEN_IN_NEW_TAB);
             mDelegate.onOpenInNewTab(params.getLinkUrl(), params.getReferrer());
         } else if (itemId == R.id.contextmenu_open_in_incognito_tab) {
--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabContextMenuItemDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabContextMenuItemDelegate.java
@@ -12,9 +12,11 @@ import android.net.Uri;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.contextmenu.ContextMenuItemDelegate;
+import org.chromium.chrome.browser.multiwindow.MultiWindowUtils;
 import org.chromium.chrome.browser.net.spdyproxy.DataReductionProxySettings;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.browser.tabmodel.TabModel.TabLaunchType;
+import org.chromium.chrome.browser.tabmodel.document.TabDelegate;
 import org.chromium.chrome.browser.util.UrlUtilities;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.common.Referrer;
@@ -53,6 +55,11 @@ public class TabContextMenuItemDelegate implements ContextMenuItemDelegate {
     }
 
     @Override
+    public boolean isOpenInOtherWindowSupported() {
+        return MultiWindowUtils.getInstance().isOpenInOtherWindowSupported(mTab.getActivity());
+    }
+
+    @Override
     public boolean isDataReductionProxyEnabledForURL(String url) {
         return isSpdyProxyEnabledForUrl(url);
     }
@@ -65,6 +72,14 @@ public class TabContextMenuItemDelegate implements ContextMenuItemDelegate {
     @Override
     public void onSaveToClipboard(String text, int clipboardType) {
         mClipboard.setText(text);
+    }
+
+    @Override
+    public void onOpenInOtherWindow(String url, Referrer referrer) {
+        TabDelegate tabDelegate = new TabDelegate(mTab.isIncognito());
+        LoadUrlParams loadUrlParams = new LoadUrlParams(url);
+        loadUrlParams.setReferrer(referrer);
+        tabDelegate.createTabInOtherWindow(loadUrlParams, mTab.getActivity(), mTab.getParentId());
     }
 
     @Override
