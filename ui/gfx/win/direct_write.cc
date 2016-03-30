c4cad56f79d558267f9fd24644a6e9d2f09d904a
kulshin <kulshin@chromium.org>
2016-03-30 11:41:31 -0700

    Move check for --disable-directwrite-for-ui switch
    This change will allow the DirectWriteFontProxyMessageFilter to bypass this
    switch. This is needed so that the message filter can still load fonts for
    renderers which might still be using DirectWrite for font rendering.
    
    BUG=594188
    
    Review URL: https://codereview.chromium.org/1839213002
    
    Cr-Commit-Position: refs/heads/master@{#384035}

--- ui/gfx/win/direct_write.cc
+++ ui/gfx/win/direct_write.cc
@@ -45,11 +45,8 @@ bool ShouldUseDirectWrite() {
 }
 
 void CreateDWriteFactory(IDWriteFactory** factory) {
-  if (!ShouldUseDirectWrite() ||
-      base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kDisableDirectWriteForUI)) {
+  if (!ShouldUseDirectWrite())
     return;
-  }
 
   using DWriteCreateFactoryProc = decltype(DWriteCreateFactory)*;
   HMODULE dwrite_dll = LoadLibraryW(L"dwrite.dll");
@@ -78,6 +75,11 @@ void MaybeInitializeDirectWrite() {
   if (tried_dwrite_initialize)
     return;
   tried_dwrite_initialize = true;
+
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kDisableDirectWriteForUI)) {
+    return;
+  }
 
   base::win::ScopedComPtr<IDWriteFactory> factory;
   CreateDWriteFactory(factory.Receive());
