20e382d1942f0ab7388226233b1eeff004129abf
ishell <ishell@chromium.org>
2016-03-30 01:25:19 -0700

    Control ES2015 'Tail call elimination' language feature via finch flag.
    BUG=chromium:596437
    LOG=N
    
    Review URL: https://codereview.chromium.org/1835553002
    
    Cr-Commit-Position: refs/heads/master@{#383930}

--- chrome/renderer/chrome_render_process_observer.cc
+++ chrome/renderer/chrome_render_process_observer.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_switches.h"
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/files/file_util.h"
 #include "base/location.h"
 #include "base/macros.h"
@@ -234,6 +235,10 @@ void CreateResourceUsageReporter(
   new ResourceUsageReporterImpl(observer, std::move(request));
 }
 
+const base::Feature kV8_ES2015_TailCalls_Feature {
+  "V8_ES2015_TailCalls", base::FEATURE_DISABLED_BY_DEFAULT
+};
+
 }  // namespace
 
 bool ChromeRenderProcessObserver::is_incognito_process_ = false;
@@ -246,6 +251,11 @@ ChromeRenderProcessObserver::ChromeRenderProcessObserver()
 #if defined(ENABLE_AUTOFILL_DIALOG)
   WebRuntimeFeatures::enableRequestAutocomplete(true);
 #endif
+
+  if (base::FeatureList::IsEnabled(kV8_ES2015_TailCalls_Feature)) {
+    std::string flag("--harmony-tailcalls");
+    v8::V8::SetFlagsFromString(flag.c_str(), static_cast<int>(flag.size()));
+  }
 
   if (command_line.HasSwitch(switches::kDisableJavaScriptHarmonyShipping)) {
     std::string flag("--noharmony-shipping");
