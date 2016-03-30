2c85efbba715be5209a5c07a6b0f60d2bf46867d
pkasting <pkasting@chromium.org>
2016-03-30 00:36:59 -0700

    When animating a content setting bubble smaller, hide the background later.
    We were hiding when the label size reached 0.  That winds up hiding the
    background when the icon has both the internal spacing and the trailing padding
    adjacent to it before the background ends.  Instead, wait to hide the background
    until the internal spacing portion of that is collapsed, so it's just
    <leading padding><icon><trailing padding>.
    
    This isn't very noticeable when the internal spacing is 2 px.  It will be more
    noticeable when I later increase the spacing (which is how I saw this).
    
    This allows moving label layout in IconLabelBubbleView() downward since we no
    longer care about sizing the label to 0 early.  I think the code reads more
    clearly that way.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1830343003
    
    Cr-Commit-Position: refs/heads/master@{#383922}

--- chrome/browser/ui/views/location_bar/content_setting_image_view.cc
+++ chrome/browser/ui/views/location_bar/content_setting_image_view.cc
@@ -177,7 +177,8 @@ SkColor ContentSettingImageView::GetBorderColor() const {
 }
 
 bool ContentSettingImageView::ShouldShowBackground() const {
-  return (!IsShrinking() || label()->width() > 0) &&
+  return (!IsShrinking() ||
+          (width() >= MinimumWidthForImageWithBackgroundShown())) &&
          (slide_animator_.is_animating() || pause_animation_);
 }
 
