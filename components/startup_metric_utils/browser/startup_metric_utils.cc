27ecd27cf691befdc17666d0050eaecd3e9ea6b3
erikchen <erikchen@chromium.org>
2016-03-30 15:10:35 -0700

    Revert changes to startup_metric_utils.cc.
    I recently enabled some code on OS X that lets startup_metric_utils.cc set the
    thread priority (https://codereview.chromium.org/1620673003/). This is most
    likely responsible for a performance regression.
    
    BUG=595043
    
    Review URL: https://codereview.chromium.org/1838173003
    
    Cr-Commit-Position: refs/heads/master@{#384113}

--- components/startup_metric_utils/browser/startup_metric_utils.cc
+++ components/startup_metric_utils/browser/startup_metric_utils.cc
@@ -272,6 +272,7 @@ base::TimeTicks StartupTimeToTimeTicks(const base::Time& time) {
 
 // platform_thread_mac.mm unfortunately doesn't properly support base's
 // thread priority APIs (crbug.com/554651).
+#if !defined(OS_MACOSX)
   static bool statics_initialized = false;
 
   base::ThreadPriority previous_priority = base::ThreadPriority::NORMAL;
@@ -280,15 +281,17 @@ base::TimeTicks StartupTimeToTimeTicks(const base::Time& time) {
     base::PlatformThread::SetCurrentThreadPriority(
         base::ThreadPriority::DISPLAY);
   }
+#endif
 
   static const base::Time time_base = base::Time::Now();
   static const base::TimeTicks trace_ticks_base = base::TimeTicks::Now();
 
+#if !defined(OS_MACOSX)
   if (!statics_initialized) {
     base::PlatformThread::SetCurrentThreadPriority(previous_priority);
   }
-
   statics_initialized = true;
+#endif
 
   // Then use the TimeDelta common ground between the two units to make the
   // conversion.
