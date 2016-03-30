9787829f0a8596366b6f6c66c3408f26c09e8201
yutak <yutak@chromium.org>
2016-03-29 18:32:33 -0700

    WTF: De-specialize PtrHash<T>.
    PtrHash<T> is specialized over various smart pointer types,
    e.g. PtrHash<RefPtr<T>>. However, this abstraction is not necessary;
    there's no code requiring the specialized versions, and they are nothing
    more than individual class declarations. This patch gets rid of that
    abstraction.
    
    Additionally, hash functions for a raw pointer T* are now defined in
    PtrHash<T>, instead of PtrHash<T*>, in order to align with other
    XXXPtrHash class templates: for example, hash functions for RefPtr<T>
    are in RefPtrHash<T>, not in RefPtrHash<RefPtr<T>>. I think my setup
    is more idiomatic C++.
    
    BUG=581524
    
    Review URL: https://codereview.chromium.org/1839003002
    
    Cr-Commit-Position: refs/heads/master@{#383897}

--- third_party/WebKit/Source/bindings/core/v8/RetainedDOMInfo.cpp
+++ third_party/WebKit/Source/bindings/core/v8/RetainedDOMInfo.cpp
@@ -74,7 +74,7 @@ bool RetainedDOMInfo::IsEquivalent(v8::RetainedObjectInfo* other)
 
 intptr_t RetainedDOMInfo::GetHash()
 {
-    return PtrHash<void*>::hash(m_root);
+    return PtrHash<void>::hash(m_root);
 }
 
 const char* RetainedDOMInfo::GetGroupLabel()
@@ -123,7 +123,7 @@ bool ActiveDOMObjectsInfo::IsEquivalent(v8::RetainedObjectInfo* other)
 
 intptr_t ActiveDOMObjectsInfo::GetHash()
 {
-    return PtrHash<void*>::hash(this);
+    return PtrHash<void>::hash(this);
 }
 
 const char* ActiveDOMObjectsInfo::GetGroupLabel()
