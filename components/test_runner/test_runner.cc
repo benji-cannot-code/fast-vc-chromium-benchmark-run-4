81deb9aa8701553210ad9d9109d326bccaa4b1ba
lukasza <lukasza@chromium.org>
2016-03-30 12:07:59 -0700

    Extract WebFrameClient implementation out of WebTestProxyBase.
    Overview
    ========
    
    This CL extracts WebFrameClient-specific parts of WebTestProxyBase into
    a new WebFrameTestClient class.
    
    This CL is part of a sequence of CLs that make progress toward:
    
    1) Splitting WebTestProxyBase into separate WebFrameClient /
       WebWidgetClient / WebViewClient focused classes
    
    2) Removing WebTestProxyBase from public API of components/test_runner
       (and having WebTestProxy and WebFrameTestProxy consume instead
       abstract WebFrameClient / WebWidgetClient / WebViewClient interfaces).
    
    Details
    =======
    
    Most of the change is mechanical move of code from WebTestProxyBase
    into the new WebFrameTestClient (slightly tweaking method signatures
    as needed to make them fit WebFrameClient interface - note camelCasing
    and extra parameters here and there).
    
    Additionally some helper methods (i.e. URLDescription and
    WebNavigationPolicyToString) are now needed by both WebTestProxyBase and
    by WebFrameTestClient.  Consequently, to facilitate code reuse, these
    helper methods were moved to test_common.cc.
    
    Finally, in order to decouple WebFrameTestClient from view-specific
    WebTestProxyBase the CL does the following:
    
    - It moves counting currently active color choosers from
      WebTestProxyBase into TestRunner (and adjusts MockColorChooser
      accordingly).
    
    - It moves ownership of MockWebUserMediaClient from
      WebTestProxyBase into TestRunner.  This is quite natural, as
      MockWebUserMediaClient depends only on WebTestDelegate::PostTask and
      WebTestDelegate::AddMediaStreamAudioSourceAndTrack (which are not
      view-specific).
    
    - Replacing WebFrameTestProxy::set_base_proxy with
      WebFrameTestProxy::set_test_client requires refactoring how higher
      layers provide the necessary object - in particular after the CL the
      WebFrame[Test]Client cannot be extracted from WebTestProxy/RenderView
      (because the whole point of this CL is to remove the coupling between
      view-specific WebTestProxy and frame-focused WebFrameTestProxy).  This
      part of the refactoring is accomplished by adding a frame-proxy
      creation callback that can be provided to EnableWebTestProxyCreation
      (similarily how it already was accepting equivalent view-proxy
      creation callback).  This refactoring also necessitates extracting a
      WebFrameTestProxyBase (to have a concrete/non-templatized type that
      content/shell/.../layout_tests layer can take a dependency on).
    
    BUG=595089
    
    Review URL: https://codereview.chromium.org/1821923003
    
    Cr-Commit-Position: refs/heads/master@{#384044}

--- components/test_runner/test_runner.cc
+++ components/test_runner/test_runner.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/test_runner/mock_credential_manager_client.h"
 #include "components/test_runner/mock_screen_orientation_client.h"
 #include "components/test_runner/mock_web_speech_recognizer.h"
+#include "components/test_runner/mock_web_user_media_client.h"
 #include "components/test_runner/test_interfaces.h"
 #include "components/test_runner/test_preferences.h"
 #include "components/test_runner/web_content_settings.h"
@@ -1669,6 +1670,7 @@ TestRunner::TestRunner(TestInterfaces* interfaces)
       web_view_(nullptr),
       web_content_settings_(new WebContentSettings()),
       mock_screen_orientation_client_(new MockScreenOrientationClient),
+      chooser_count_(0),
       weak_factory_(this) {}
 
 TestRunner::~TestRunner() {}
@@ -1760,6 +1762,7 @@ void TestRunner::Reset() {
 
   platform_name_ = "chromium";
   tooltip_text_ = std::string();
+  chooser_count_ = 0;
   web_history_item_count_ = 0;
   intercept_post_message_ = false;
 
@@ -2520,6 +2523,12 @@ MockScreenOrientationClient* TestRunner::getMockScreenOrientationClient() {
   return mock_screen_orientation_client_.get();
 }
 
+MockWebUserMediaClient* TestRunner::getMockWebUserMediaClient() {
+  if (!user_media_client_.get())
+    user_media_client_.reset(new MockWebUserMediaClient(delegate_));
+  return user_media_client_.get();
+}
+
 void TestRunner::SetMockScreenOrientation(const std::string& orientation_str) {
   blink::WebScreenOrientationType orientation;
 
@@ -2542,6 +2551,14 @@ void TestRunner::SetMockScreenOrientation(const std::string& orientation_str) {
 
 void TestRunner::DisableMockScreenOrientation() {
   mock_screen_orientation_client_->SetDisabled(true);
+}
+
+void TestRunner::DidOpenChooser() {
+  chooser_count_++;
+}
+
+void TestRunner::DidCloseChooser() {
+  chooser_count_--;
 }
 
 void TestRunner::DidAcquirePointerLock() {
@@ -2853,7 +2870,7 @@ void TestRunner::CloseWebInspector() {
 }
 
 bool TestRunner::IsChooserShown() {
-  return proxy_->IsChooserShown();
+  return 0 < chooser_count_;
 }
 
 void TestRunner::EvaluateInWebInspector(int call_id,
