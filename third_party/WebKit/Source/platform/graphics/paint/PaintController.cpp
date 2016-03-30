558a45047daf183e2f4b0ffb8b2b9b602fd7720b
wangxianzhu <wangxianzhu@chromium.org>
2016-03-29 17:54:58 -0700

    Avoid crash during PaintController::showDebugData()
    During merging, display items that have been moved to new places
    have null client. Avoid crash during PaintController::showDebugData()
    so that we can dump data during merge.
    
    Review URL: https://codereview.chromium.org/1843863002
    
    Cr-Commit-Position: refs/heads/master@{#383880}

--- third_party/WebKit/Source/platform/graphics/paint/PaintController.cpp
+++ third_party/WebKit/Source/platform/graphics/paint/PaintController.cpp
@@ -563,8 +563,10 @@ WTF::String PaintController::displayItemListAsDebugString(const DisplayItemList&
             stringBuilder.append(",\n");
         stringBuilder.append(String::format("{index: %d, ", (int)i));
         displayItem.dumpPropertiesAsDebugString(stringBuilder);
-        stringBuilder.append(", cacheIsValid: ");
-        stringBuilder.append(clientCacheIsValid(displayItem.client()) ? "true" : "false");
+        if (displayItem.hasValidClient()) {
+            stringBuilder.append(", cacheIsValid: ");
+            stringBuilder.append(clientCacheIsValid(displayItem.client()) ? "true" : "false");
+        }
         stringBuilder.append('}');
     }
     return stringBuilder.toString();
