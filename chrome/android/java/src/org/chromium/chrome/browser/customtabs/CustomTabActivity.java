59825a3716c6e90aa0a7475941da4b6e19efecf2
tedchoc <tedchoc@chromium.org>
2016-03-30 19:52:16 -0700

    Fix the Elderberrry experiment's handling of X and back.
    Only works on L plus right now.
    
    BUG=582539
    
    Review URL: https://codereview.chromium.org/1845683002
    
    Cr-Commit-Position: refs/heads/master@{#384190}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -283,7 +283,7 @@ public class CustomTabActivity extends ChromeActivity {
                             createHerbResultIntent(RESULT_CLOSED);
                         }
                         RecordUserAction.record("CustomTabs.CloseButtonClicked");
-                        CustomTabActivity.this.finish();
+                        finishAndClose();
                     }
                 });
 
@@ -542,9 +542,10 @@ public class CustomTabActivity extends ChromeActivity {
     }
 
     /**
-     * Finishes the activity after the tab has been reparented.
+     * Finishes the activity and removes the reference from the Android recents.
      */
-    public void finishForReparenting() {
+    public void finishAndClose() {
+        // When on top of another app, finish is all that is required.
         finish();
     }
 
@@ -563,7 +564,7 @@ public class CustomTabActivity extends ChromeActivity {
                 if (mIntentDataProvider.isOpenedByChrome() && isHerbResultNeeded()) {
                     createHerbResultIntent(RESULT_BACK_PRESSED);
                 }
-                finish();
+                finishAndClose();
             }
         }
         return true;
@@ -587,7 +588,7 @@ public class CustomTabActivity extends ChromeActivity {
                                 && TextUtils.equals(getPackageName(), creatorPackage)) {
                             RecordUserAction.record(
                                     "TaskManagement.OpenInChromeActionButtonClicked");
-                            if (openCurrentUrlInBrowser(false)) finishForReparenting();
+                            if (openCurrentUrlInBrowser(false)) finishAndClose();
                         } else {
                             mIntentDataProvider.sendButtonPendingIntentWithUrl(
                                     getApplicationContext(), getActivityTab().getUrl());
@@ -803,7 +804,7 @@ public class CustomTabActivity extends ChromeActivity {
                 Runnable finalizeCallback = new Runnable() {
                     @Override
                     public void run() {
-                        finishForReparenting();
+                        finishAndClose();
                     }
                 };
                 AsyncTabParamsManager.add(
