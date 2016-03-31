1197f51afe875e9c080a2066e71b9fc97aae4089
liberato <liberato@chromium.org>
2016-03-31 11:03:07 -0700

    Don't let AVDA copy pictures on PowerVR 544MP with KitKat.
    PowerVR 544MP on KitKat would crash when attaching an EGLImage to
    PictureBuffer textures when transitioning into full screen.  This CL
    turns off the copying on those devices to avoid it.  The result will
    be a slightly visually worse full screen transition, since the most
    recent frame won't be visible during the transition, or when the
    pipeline is suspended.
    
    BUG=598474
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1841973003
    
    Cr-Commit-Position: refs/heads/master@{#384327}

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/gpu_export.h"
 
 #define GPU_DRIVER_BUG_WORKAROUNDS(GPU_OP)                   \
+  GPU_OP(AVDA_DONT_COPY_PICTURES,                            \
+         avda_dont_copy_pictures)                            \
   GPU_OP(AVOID_EGL_IMAGE_TARGET_TEXTURE_REUSE,               \
          avoid_egl_image_target_texture_reuse)               \
   GPU_OP(BROKEN_EGL_IMAGE_REF_COUNTING,                      \
