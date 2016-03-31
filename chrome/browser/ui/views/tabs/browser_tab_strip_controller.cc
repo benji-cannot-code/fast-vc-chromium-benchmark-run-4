3c17a2c8f4bb24fe936a75ed3ac891ad7b18c977
pkasting <pkasting@chromium.org>
2016-03-31 00:28:35 -0700

    Suspected fix for a tab image caching bug.
    If we cache the same image for guest and incognito tabs, but they use different
    themes, we'll have drawing glitches.
    
    I can't reproduce the original problem, so I'm going to try landing this and see
    if the reporter sees things clear up.
    
    BUG=599077
    TEST=See bug
    TBR=estade
    
    Review URL: https://codereview.chromium.org/1845813002
    
    Cr-Commit-Position: refs/heads/master@{#384210}

--- chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
@@ -375,7 +375,7 @@ void BrowserTabStripController::CreateNewTabWithLocation(
 }
 
 bool BrowserTabStripController::IsIncognito() {
-  return browser_->profile()->IsOffTheRecord();
+  return browser_->profile()->GetProfileType() == Profile::INCOGNITO_PROFILE;
 }
 
 void BrowserTabStripController::StackedLayoutMaybeChanged() {
