1267eb6e096d9e244d86f5c1030d41f0322ccfc1
avallee <avallee@chromium.org>
2016-03-30 20:27:11 -0700

    Fix <webview> cursors with --site-per-process and --isolate-extensions.
    When running BrowserPlugin without either flag, the guest sets the
    cursor which is forwarded to the embedder. The embedder saves the
    current guest cursor and will set the cursor to the guest value when
    processing mouse events for the guest.
    
    When running with either --site-per-process or --isolate-extensions, the
    embedding renderer no longer receives the guest's mouse events which are
    how the cursor ends up being set. In this case, directly set the cursor
    with the owner view.
    
    BUG=595446
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review URL: https://codereview.chromium.org/1845083002
    
    Cr-Commit-Position: refs/heads/master@{#384193}

--- content/browser/frame_host/render_widget_host_view_guest.cc
+++ content/browser/frame_host/render_widget_host_view_guest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/browser_plugin/browser_plugin_messages.h"
 #include "content/common/frame_messages.h"
 #include "content/common/input/web_touch_event_traits.h"
+#include "content/common/site_isolation_policy.h"
 #include "content/common/view_messages.h"
 #include "content/common/webplugin_geometry.h"
 #include "content/public/common/content_switches.h"
@@ -335,10 +336,14 @@ void RenderWidgetHostViewGuest::UpdateCursor(const WebCursor& cursor) {
   // and so we will always hit this code path.
   if (!guest_)
     return;
-  guest_->SendMessageToEmbedder(
-      new BrowserPluginMsg_SetCursor(guest_->browser_plugin_instance_id(),
-                                     cursor));
-
+  if (SiteIsolationPolicy::AreCrossProcessFramesPossible()) {
+    RenderWidgetHostViewBase* rwhvb = GetOwnerRenderWidgetHostView();
+    if (rwhvb)
+      rwhvb->UpdateCursor(cursor);
+  } else {
+    guest_->SendMessageToEmbedder(new BrowserPluginMsg_SetCursor(
+        guest_->browser_plugin_instance_id(), cursor));
+  }
 }
 
 void RenderWidgetHostViewGuest::SetIsLoading(bool is_loading) {
