ed2aed89f32ba1028321b72e2b0a52679a080919
haraken <haraken@chromium.org>
2016-03-31 07:29:57 -0700

    Oilpan: Remove WillBe types (part 1)
    This CL fixes a manually-fixed part of https://codereview.chromium.org/1686483002/.
    
    BUG=585328
    TBR=tkent@chromium.org
    
    Review URL: https://codereview.chromium.org/1846893004
    
    Cr-Commit-Position: refs/heads/master@{#384270}

--- third_party/WebKit/Source/core/xmlhttprequest/XMLHttpRequest.cpp
+++ third_party/WebKit/Source/core/xmlhttprequest/XMLHttpRequest.cpp
@@ -668,7 +668,7 @@ void XMLHttpRequest::send(const ArrayBufferOrArrayBufferViewOrBlobOrDocumentOrSt
     }
 
     if (body.isDocument()) {
-        send(body.getAsDocument().get(), exceptionState);
+        send(body.getAsDocument(), exceptionState);
         return;
     }
 
