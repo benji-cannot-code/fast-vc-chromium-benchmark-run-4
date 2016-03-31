c368e375ddc2cbd8cb40a4b8f593c144e5f9c428
dschuyler <dschuyler@chromium.org>
2016-03-31 15:53:51 -0700

    [MD settings] theme name shown in sub-label, wallpaper chrome os only
    This CL causes the sub-label for the theme setting to
    show the current theme name and the wall paper app
    row only appears on chrome os. There is also a change
    to expand the font settings sub-page properly.
    
    BUG=598462, 531786, 559457
    
    Review URL: https://codereview.chromium.org/1841973004
    
    Cr-Commit-Position: refs/heads/master@{#384424}

--- chrome/browser/resources/settings/appearance_page/appearance_page.js
+++ chrome/browser/resources/settings/appearance_page/appearance_page.js
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'settings-appearance-page',
 
+  behaviors: [I18nBehavior],
+
   properties: {
     /**
      * The current active route.
@@ -100,13 +102,13 @@ Polymer({
         {value: 500, name: '500%'},
       ],
     },
+
+    /** @private */
+    themeSublabel_: String,
   },
 
-  behaviors: [
-    I18nBehavior,
-  ],
-
   observers: [
+    'themeChanged_(prefs.extensions.theme.id.value)',
     'zoomLevelChanged_(defaultZoomLevel_.value)',
   ],
 
@@ -167,6 +169,21 @@ Polymer({
   /** @private */
   showFontsPage_: function() {
     return this.currentRoute.subpage[0] == 'appearance-fonts';
+  },
+
+  /**
+   * @param {string} themeId The theme ID.
+   * @private
+   */
+  themeChanged_: function(themeId) {
+    if (themeId) {
+      chrome.management.get(themeId,
+          function(info) {
+            this.themeSublabel_ = info.name;
+          }.bind(this));
+    } else {
+      this.themeSublabel_ = this.i18n('chooseFromWebStore');
+    }
   },
 
   /**
