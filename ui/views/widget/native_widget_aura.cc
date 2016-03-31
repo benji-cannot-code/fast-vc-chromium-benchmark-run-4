954e42a68b17820f00bd6981aa3d237328900173
kinaba <kinaba@chromium.org>
2016-03-30 18:04:46 -0700

    NativeWidgetAura should not mark all key events as handled unconditionally.
    ...so that handlers added by EventTarget::AddPostEventHandler can have a chance.
    
    As far as I examined the history, the SetHandled originates in 'return true' of
      http://crrev.com/104486
    which meant to pass key events to IME first, always. (Normal key event handling
    comes later in another method called DispatchKeyEventPostIME.)
    
    The complex circulation of events was, however, later abstracted out in
      http://crrev.com/337157
    to higher layer. Thus today we don't have to care about grabbing events away
    at this point.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1832003002
    
    Cr-Commit-Position: refs/heads/master@{#384170}

--- ui/views/widget/native_widget_aura.cc
+++ ui/views/widget/native_widget_aura.cc
@@ -878,7 +878,6 @@ void NativeWidgetAura::OnKeyEvent(ui::KeyEvent* event) {
     return;
 
   delegate_->OnKeyEvent(event);
-  event->SetHandled();
 }
 
 void NativeWidgetAura::OnMouseEvent(ui::MouseEvent* event) {
