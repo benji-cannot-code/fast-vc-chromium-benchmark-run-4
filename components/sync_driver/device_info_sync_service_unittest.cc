ead1a6773dfcbe4df10695ec846001cd9b12a2be
skym <skym@chromium.org>
2016-03-31 15:20:00 -0700

    [Sync] Disable DeviceInfo pulsing when sync is disabled.
    BUG=598860
    
    Review URL: https://codereview.chromium.org/1849483002
    
    Cr-Commit-Position: refs/heads/master@{#384417}

--- components/sync_driver/device_info_sync_service_unittest.cc
+++ components/sync_driver/device_info_sync_service_unittest.cc
@@ -165,6 +165,7 @@ class DeviceInfoSyncServiceTest : public testing::Test,
                      const syncer::SyncData& sync_data) {
     sync_service_->StoreSyncData(client_id, sync_data);
   }
+  bool IsPulseTimerRunning() { return sync_service_->pulse_timer_.IsRunning(); }
 
   // Needs to be created for OneShotTimer to grab the current task runner.
   base::MessageLoop message_loop_;
@@ -211,9 +212,11 @@ TEST_F(DeviceInfoSyncServiceTest, StopSyncing) {
       CreateAndPassSyncErrorFactory());
   EXPECT_TRUE(sync_service_->IsSyncing());
   EXPECT_EQ(1, num_device_info_changed_callbacks_);
+  EXPECT_TRUE(IsPulseTimerRunning());
   sync_service_->StopSyncing(syncer::DEVICE_INFO);
   EXPECT_FALSE(sync_service_->IsSyncing());
   EXPECT_EQ(2, num_device_info_changed_callbacks_);
+  EXPECT_FALSE(IsPulseTimerRunning());
 }
 
 // Sync with initial data matching the local device data.
