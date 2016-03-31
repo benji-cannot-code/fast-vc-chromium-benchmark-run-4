6d686cd812321b7de30536081316af37ef5c11a3
skyostil <skyostil@chromium.org>
2016-03-31 06:10:59 -0700

    perf: Disable Oort Online as it is failing in most runs
    BUG=599416
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:android_s5_perf_cq;tryserver.chromium.perf:winx64_10_perf_cq;tryserver.chromium.perf:mac_retina_perf_cq;tryserver.chromium.perf:linux_perf_cq
    
    Review URL: https://codereview.chromium.org/1851453002
    
    Cr-Commit-Position: refs/heads/master@{#384258}

--- tools/perf/benchmarks/oortonline.py
+++ tools/perf/benchmarks/oortonline.py
@@ -53,6 +53,7 @@ class OortOnline(perf_benchmark.PerfBenchmark):
 # Tracing.requestMemoryDump DevTools API. See http://crbug.com/540022.
 @benchmark.Disabled('reference')
 @benchmark.Disabled('android')
+@benchmark.Disabled('mac', 'win')  # crbug.com/599416
 class OortOnlineTBM(perf_benchmark.PerfBenchmark):
   """OortOnline benchmark that measures WebGL and V8 performance.
   URL: http://oortonline.gl/#run
