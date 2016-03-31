3018fc5fd3d0f3a53d3b4e1f6fd4fdbcece467f7
twellington <twellington@chromium.org>
2016-03-30 20:48:02 -0700

    Update location bar button visibility when url bar text changed
    Only the delete button visibility was getting updated
    when the url bar text changed. All buttons should
    get updated visibility.
    
    Also update the location bar button visibility when
    the url bar text is cleared so the delete button is
    hidden and microphone button or bookmark button is shown.
    
    BUG=596868
    
    Review URL: https://codereview.chromium.org/1849643003
    
    Cr-Commit-Position: refs/heads/master@{#384194}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -985,7 +985,7 @@ public class LocationBarLayout extends FrameLayout implements OnClickListener,
     public void onTextChangedForAutocomplete(final boolean textDeleted) {
         cancelPendingAutocompleteStart();
 
-        updateDeleteButtonVisibility();
+        updateButtonVisibility();
         updateNavigationButton();
 
         if (!mHasStartedNewOmniboxEditSession && mNativeInitialized) {
@@ -1756,6 +1756,7 @@ public class LocationBarLayout extends FrameLayout implements OnClickListener,
             if (!TextUtils.isEmpty(mUrlBar.getQueryText())) {
                 setUrlBarText(null, null, "");
                 hideSuggestions();
+                updateButtonVisibility();
             }
 
             startZeroSuggest();
