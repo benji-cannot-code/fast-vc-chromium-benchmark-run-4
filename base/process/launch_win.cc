b18a32a509ef5477f29299c55c2bfec7efbced7a
jam <jam@chromium.org>
2016-03-31 09:43:44 -0700

    Remove "#pragma comment(lib" in base.
    BUG=597028
    
    Review URL: https://codereview.chromium.org/1842143002
    
    Cr-Commit-Position: refs/heads/master@{#384305}

--- base/process/launch_win.cc
+++ base/process/launch_win.cc
@@ -32,9 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/startup_information.h"
 #include "base/win/windows_version.h"
 
-// userenv.dll is required for CreateEnvironmentBlock().
-#pragma comment(lib, "userenv.lib")
-
 namespace base {
 
 namespace {
