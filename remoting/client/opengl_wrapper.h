bd7baf04994b3a5654f5ee7ffa5ba4a958c96324
nicholss <nicholss@chromium.org>
2016-03-31 11:20:07 -0700

    Staging an example iOS project to be the dummy target for CRD iOS App.
    Review URL: https://codereview.chromium.org/1763783002
    
    Cr-Commit-Position: refs/heads/master@{#384335}

---
+++ remoting/client/opengl_wrapper.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_CLIENT_OPENGL_WRAPPER_H_
+#define REMOTING_CLIENT_OPENGL_WRAPPER_H_
+
+#if defined(OS_IOS)
+#include <OpenGLES/ES2/gl.h>
+#endif  // defined(OS_IOS)
+
+#endif  // REMOTING_CLIENT_OPENGL_WRAPPER_H_
