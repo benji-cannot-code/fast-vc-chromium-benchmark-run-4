a82a8ad78f20347b50deaf02817d02dd6effd653
horo <horo@chromium.org>
2016-03-31 03:30:18 -0700

    Pipe the cache_name from CacheStorage to WebURLResponse via ServiceWorker.
    We are planning to support the V8 code caching in CacheStorage.
    To support this, we have to pipe the cache_name from CacheStorage to WebURLResponse.
    Design doc: https://docs.google.com/document/d/1dVlgbXls-G65koU2vlmAXy1EVsoY9Z55jaEX7cZgfDA
    
    BUG=581613
    
    Review URL: https://codereview.chromium.org/1731153005
    
    Cr-Commit-Position: refs/heads/master@{#384241}

--- third_party/WebKit/Source/platform/network/ResourceResponse.cpp
+++ third_party/WebKit/Source/platform/network/ResourceResponse.cpp
@@ -138,6 +138,7 @@ ResourceResponse::ResourceResponse(CrossThreadResourceResponseData* data)
     m_wasFallbackRequiredByServiceWorker = data->m_wasFallbackRequiredByServiceWorker;
     m_serviceWorkerResponseType = data->m_serviceWorkerResponseType;
     m_originalURLViaServiceWorker = data->m_originalURLViaServiceWorker;
+    m_cacheStorageCacheName = data->m_cacheStorageCacheName;
     m_responseTime = data->m_responseTime;
     m_remoteIPAddress = AtomicString(data->m_remoteIPAddress);
     m_remotePort = data->m_remotePort;
@@ -185,6 +186,7 @@ PassOwnPtr<CrossThreadResourceResponseData> ResourceResponse::copyData() const
     data->m_wasFallbackRequiredByServiceWorker = m_wasFallbackRequiredByServiceWorker;
     data->m_serviceWorkerResponseType = m_serviceWorkerResponseType;
     data->m_originalURLViaServiceWorker = m_originalURLViaServiceWorker.copy();
+    data->m_cacheStorageCacheName = cacheStorageCacheName().isolatedCopy();
     data->m_responseTime = m_responseTime;
     data->m_remoteIPAddress = m_remoteIPAddress.getString().isolatedCopy();
     data->m_remotePort = m_remotePort;
--- third_party/WebKit/public/platform/modules/serviceworker/WebServiceWorkerResponse.h
+++ third_party/WebKit/public/platform/modules/serviceworker/WebServiceWorkerResponse.h
@@ -80,6 +80,9 @@ public:
     void setResponseTime(int64_t);
     int64_t responseTime() const;
 
+    void setCacheStorageCacheName(const WebString&);
+    WebString cacheStorageCacheName() const;
+
 #if INSIDE_BLINK
     const HTTPHeaderMap& headers() const;
 
