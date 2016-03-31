cfe83931b4528e769841510b1cf239da6abe65bf
rob.buis <rob.buis@samsung.com>
2016-03-31 07:56:09 -0700

    Move grid-template-areas into CSSPropertyParser
    Move the grid-template-areas longhand from
    LegacyCSSPropertyParser into CSSPropertyParser.
    
    In order to reuse parseGridTemplateAreasRow make
    it work on a String parameter rather than the value
    list or range.
    
    BUG=499780
    
    Review URL: https://codereview.chromium.org/1829723002
    
    Cr-Commit-Position: refs/heads/master@{#384277}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
@@ -126,8 +126,6 @@ private:
     bool parseGridTrackRepeatFunction(CSSValueList&, bool& isAutoRepeat);
     PassRefPtrWillBeRawPtr<CSSValue> parseGridTrackSize(CSSParserValueList& inputList, TrackSizeRestriction = AllowAll);
     PassRefPtrWillBeRawPtr<CSSPrimitiveValue> parseGridBreadth(CSSParserValue*, TrackSizeRestriction = AllowAll);
-    bool parseGridTemplateAreasRow(NamedGridAreaMap&, const size_t, size_t&);
-    PassRefPtrWillBeRawPtr<CSSValue> parseGridTemplateAreas();
     bool parseGridLineNames(CSSParserValueList&, CSSValueList&, CSSGridLineNamesValue* = nullptr);
     PassRefPtrWillBeRawPtr<CSSValue> parseGridAutoFlow(CSSParserValueList&);
 
@@ -200,6 +198,7 @@ private:
 
 // TODO(rob.buis): should move to CSSPropertyParser after conversion.
 bool allTracksAreFixedSized(CSSValueList&);
+bool parseGridTemplateAreasRow(const String&, NamedGridAreaMap&, const size_t, size_t&);
 
 CSSPropertyID unresolvedCSSPropertyID(const CSSParserString&);
 CSSValueID cssValueKeywordID(const CSSParserString&);
