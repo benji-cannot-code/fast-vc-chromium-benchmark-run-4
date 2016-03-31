1c796fefdab4c282d51983012ea0b6feda89b0e4
jbudorick <jbudorick@chromium.org>
2016-03-31 12:56:25 -0700

    [Android] Fix lint error in EmbeddedTestServerImpl.
    Triggered by https://codereview.chromium.org/1848713002/
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1845223004
    
    Cr-Commit-Position: refs/heads/master@{#384366}

--- net/test/android/javatests/src/org/chromium/net/test/EmbeddedTestServerImpl.java
+++ net/test/android/javatests/src/org/chromium/net/test/EmbeddedTestServerImpl.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.net.test;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
 
@@ -175,7 +176,18 @@ public class EmbeddedTestServerImpl extends IEmbeddedTestServerImpl.Stub {
             }
         });
 
-        mHandlerThread.quitSafely();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            mHandlerThread.quitSafely();
+        } else {
+            runOnHandlerThread(new Callable<Void>() {
+                @Override
+                public Void call() {
+                    mHandlerThread.quit();
+                    return null;
+                }
+            });
+        }
+
         try {
             mHandlerThread.join();
         } catch (InterruptedException e) {
