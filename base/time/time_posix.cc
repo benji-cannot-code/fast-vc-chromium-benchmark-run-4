493acf57a730fbc56f47361b21bedfc903324be1
charliea <charliea@chromium.org>
2016-03-31 12:05:18 -0700

    time: Add a static TimeTicks method that returns the underlying clock
    In a follow-up CL, the return value of this method will be written into
    traces recorded by Chrome so that, when syncing Chrome traces with
    external traces, we have more information to determine whether the two
    traces use timestamps from the same underlying clock and can therefore
    be combined.
    
    BUG=597350
    
    Review URL: https://codereview.chromium.org/1824673002
    
    Cr-Commit-Position: refs/heads/master@{#384352}

--- base/time/time_posix.cc
+++ base/time/time_posix.cc
@@ -313,6 +313,11 @@ TimeTicks TimeTicks::Now() {
 }
 
 // static
+TimeTicks::Clock TimeTicks::GetClock() {
+  return Clock::LINUX_CLOCK_MONOTONIC;
+}
+
+// static
 bool TimeTicks::IsHighResolution() {
   return true;
 }
