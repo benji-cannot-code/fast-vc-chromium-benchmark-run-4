5893e82513cd27e82ed04ae81f5e14c37826a464
kylechar <kylechar@chromium.org>
2016-03-31 06:11:35 -0700

    Add mus_demo mojo app.
    Simple mojo app that connects to mus, creates a new window and draws
    into the window. Draws a spinning square.
    
    Provides a simple way to test mus graphics output without the complexity
    of starting mash.
    
    BUG=599162
    
    Review URL: https://codereview.chromium.org/1832943002
    
    Cr-Commit-Position: refs/heads/master@{#384259}

---
+++ components/mus/demo/main.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/mus/demo/mus_demo.h"
+#include "mojo/public/c/system/main.h"
+#include "mojo/shell/public/cpp/application_runner.h"
+
+MojoResult MojoMain(MojoHandle shell_handle) {
+  mojo::ApplicationRunner runner(new mus_demo::MusDemo);
+  runner.set_message_loop_type(base::MessageLoop::TYPE_UI);
+  return runner.Run(shell_handle);
+}
