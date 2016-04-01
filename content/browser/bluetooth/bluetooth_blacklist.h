dad3be750885d29de7bda80d4ef753719475292b
scheib <scheib@chromium.org>
2016-03-31 18:10:30 -0700

    bluetooth: Create BluetoothBlacklist::Add(string).
    This new overload of BluetoothBlacklist::Add accepts a string and parses
    it to add entries to the blacklist. This will be used to update the
    blacklist at runtime with strings obtained from a server.
    
    BUG=570121
    
    Review URL: https://codereview.chromium.org/1838853006
    
    Cr-Commit-Position: refs/heads/master@{#384459}

--- content/browser/bluetooth/bluetooth_blacklist.h
+++ content/browser/bluetooth/bluetooth_blacklist.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/lazy_instance.h"
 #include "base/macros.h"
+#include "base/strings/string_piece.h"
 #include "content/common/content_export.h"
 #include "device/bluetooth/bluetooth_uuid.h"
 
@@ -47,7 +48,26 @@ class CONTENT_EXPORT BluetoothBlacklist final {
   //   Add(uuid, EXCLUDE_READS);
   //   Add(uuid, EXCLUDE_WRITES);
   //   IsExcluded(uuid);  // true.
+  // Requires UUID to be valid.
   void Add(const device::BluetoothUUID&, Value);
+
+  // Adds UUIDs to the blacklist by parsing a blacklist string and calling
+  // Add(uuid, value).
+  //
+  // The blacklist string must be a comma-separated list of UUID:exclusion
+  // pairs. The pairs may be separated by whitespace. Pair components are
+  // colon-separated and must not have whitespace around the colon.
+  //
+  // UUIDs are a string that BluetoothUUID can parse (See BluetoothUUID
+  // constructor comment). Exclusion values are a single lower case character
+  // string "e", "r", or "w" for EXCLUDE, EXCLUDE_READS, or EXCLUDE_WRITES.
+  //
+  // Malformed pairs in the string are ignored, including invalid UUID or
+  // exclusion values. Duplicate UUIDs follow Add()'s merging rule.
+  //
+  // Example:
+  // "1812:e, 00001800-0000-1000-8000-00805f9b34fb:w, ignored:1, alsoignored."
+  void Add(base::StringPiece blacklist_string);
 
   // Returns if a UUID is excluded from all operations. UUID must be valid.
   bool IsExcluded(const device::BluetoothUUID&) const;
