bd9371d6b5eae1fc712fce618c282423cbf5c00b
robert.bradford <robert.bradford@intel.com>
2016-04-01 02:08:35 -0700

    Add support for more advanced color correction
    Rather than use just a gamma ramp (specified in a VCGT file) this change loads
    a correction matrix from the ICC file. As correction matrix needs to be applied
    in a linear colorspace it is also necesary to apply a "degamma" to adjust from
    rendered gamma (sRGB) to linear space. The display's gamma is then applied
    after the correction matrix.
    
    TEST=New unit tests in ash_unittests, interactive testing on Broadwell (like
    samus) device.
    BUG=495196
    
    Review URL: https://codereview.chromium.org/1182063002
    
    Cr-Commit-Position: refs/heads/master@{#384525}

--- ui/display/chromeos/test/test_display_snapshot.cc
+++ ui/display/chromeos/test/test_display_snapshot.cc
@@ -13,6 +13,7 @@ TestDisplaySnapshot::TestDisplaySnapshot()
                       DISPLAY_CONNECTION_TYPE_UNKNOWN,
                       false,
                       false,
+                      false,
                       std::string(),
                       base::FilePath(),
                       std::vector<const DisplayMode*>(),
@@ -27,6 +28,7 @@ TestDisplaySnapshot::TestDisplaySnapshot(
     DisplayConnectionType type,
     bool is_aspect_preserving_scaling,
     int64_t product_id,
+    bool has_color_correction_matrix,
     const std::vector<const DisplayMode*>& modes,
     const DisplayMode* current_mode,
     const DisplayMode* native_mode)
@@ -36,6 +38,7 @@ TestDisplaySnapshot::TestDisplaySnapshot(
                       type,
                       is_aspect_preserving_scaling,
                       false,
+                      has_color_correction_matrix,
                       std::string(),
                       base::FilePath(),
                       modes,
