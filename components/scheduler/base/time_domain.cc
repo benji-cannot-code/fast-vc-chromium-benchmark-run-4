5d478d293519c780e0cab886d22860839423b231
alexclarke <alexclarke@chromium.org>
2016-04-01 09:33:35 -0700

    Remove registered_task_queues_ and associated DCHECKS from TimeDomain
    TSAN is complaining about the usage of |registered_task_queues_| and
    since they where added purely to diagnose a UAF which we've fixed we
    might as well rip them out again.
    
    BUG=599535
    
    Review URL: https://codereview.chromium.org/1855453002
    
    Cr-Commit-Position: refs/heads/master@{#384603}

--- components/scheduler/base/time_domain.cc
+++ components/scheduler/base/time_domain.cc
@@ -18,26 +18,17 @@ TimeDomain::TimeDomain(Observer* observer) : observer_(observer) {}
 
 TimeDomain::~TimeDomain() {
   DCHECK(main_thread_checker_.CalledOnValidThread());
-#if DCHECK_IS_ON()
-  DCHECK(registered_task_queues_.empty());
-#endif
 }
 
 void TimeDomain::RegisterQueue(internal::TaskQueueImpl* queue) {
   DCHECK(main_thread_checker_.CalledOnValidThread());
   DCHECK_EQ(queue->GetTimeDomain(), this);
-#if DCHECK_IS_ON()
-  registered_task_queues_.insert(queue);
-#endif
 }
 
 void TimeDomain::UnregisterQueue(internal::TaskQueueImpl* queue) {
   DCHECK(main_thread_checker_.CalledOnValidThread());
   DCHECK_EQ(queue->GetTimeDomain(), this);
   UnregisterAsUpdatableTaskQueue(queue);
-#if DCHECK_IS_ON()
-  registered_task_queues_.erase(queue);
-#endif
 
   // We need to remove |task_queue| from delayed_wakeup_multimap_ which is a
   // little awkward since it's keyed by time. O(n) running time.
@@ -58,14 +49,6 @@ void TimeDomain::MigrateQueue(internal::TaskQueueImpl* queue,
   DCHECK_EQ(queue->GetTimeDomain(), this);
   DCHECK(destination_time_domain);
   UnregisterAsUpdatableTaskQueue(queue);
-#if DCHECK_IS_ON()
-  DCHECK(registered_task_queues_.find(queue) != registered_task_queues_.end());
-  registered_task_queues_.erase(queue);
-
-  // NOTE it's the responsibility of the caller to make sure
-  // |queue->GetTimeDomain()| is updated.
-  destination_time_domain->registered_task_queues_.insert(queue);
-#endif
 
   base::TimeTicks destination_now = destination_time_domain->Now();
   // We need to remove |task_queue| from delayed_wakeup_multimap_ which is a
@@ -87,9 +70,6 @@ void TimeDomain::ScheduleDelayedWork(internal::TaskQueueImpl* queue,
                                      base::TimeTicks delayed_run_time,
                                      base::TimeTicks now) {
   DCHECK(main_thread_checker_.CalledOnValidThread());
-#if DCHECK_IS_ON()
-  DCHECK(registered_task_queues_.find(queue) != registered_task_queues_.end());
-#endif
   if (delayed_wakeup_multimap_.empty() ||
       delayed_run_time < delayed_wakeup_multimap_.begin()->first) {
     base::TimeDelta delay = std::max(base::TimeDelta(), delayed_run_time - now);
@@ -102,9 +82,6 @@ void TimeDomain::ScheduleDelayedWork(internal::TaskQueueImpl* queue,
 }
 
 void TimeDomain::RegisterAsUpdatableTaskQueue(internal::TaskQueueImpl* queue) {
-#if DCHECK_IS_ON()
-  DCHECK(registered_task_queues_.find(queue) != registered_task_queues_.end());
-#endif
   {
     base::AutoLock lock(newly_updatable_lock_);
     newly_updatable_.push_back(queue);
@@ -115,9 +92,6 @@ void TimeDomain::RegisterAsUpdatableTaskQueue(internal::TaskQueueImpl* queue) {
 
 void TimeDomain::UnregisterAsUpdatableTaskQueue(
     internal::TaskQueueImpl* queue) {
-#if DCHECK_IS_ON()
-  DCHECK(registered_task_queues_.find(queue) != registered_task_queues_.end());
-#endif
   DCHECK(main_thread_checker_.CalledOnValidThread());
 
   updatable_queue_set_.erase(queue);
