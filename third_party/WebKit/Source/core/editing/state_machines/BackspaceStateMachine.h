0a07d54c75a1e5755b78474c23e2b0fb764be994
nona <nona@chromium.org>
2016-04-01 00:35:08 -0700

    Implement backspace state machine for complex emoji sequence.
    There are five types of complex emoji sequence as of Unicode 9.0
    - Emoji + Variatino selector
    - Emoji + Emoji modifier
    - Keycap base + Keycap
    - Regional indicator symbols
    - ZWJ sequence
    
    By this CL, BackspaceStateMachine can delete above emoji sequence
    correctly.
    
    BUG=594920
    
    Review URL: https://codereview.chromium.org/1844663002
    
    Cr-Commit-Position: refs/heads/master@{#384511}

--- third_party/WebKit/Source/core/editing/state_machines/BackspaceStateMachine.h
+++ third_party/WebKit/Source/core/editing/state_machines/BackspaceStateMachine.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Allocator.h"
 #include "wtf/Noncopyable.h"
 #include "wtf/text/Unicode.h"
+#include <iosfwd>
 
 namespace blink {
 
@@ -18,7 +19,7 @@ class CORE_EXPORT BackspaceStateMachine {
     STACK_ALLOCATED();
     WTF_MAKE_NONCOPYABLE(BackspaceStateMachine);
 public:
-    BackspaceStateMachine() = default;
+    BackspaceStateMachine();
 
     // Prepares by feeding preceding text.
     // This method must not be called after feedFollowingCodeUnit().
@@ -28,20 +29,40 @@ public:
     TextSegmentationMachineState tellEndOfPrecedingText();
 
     // Find boundary offset by feeding following text.
-    // This method must be called after feedPrecedingCodeUnit() returns NeedsFollowingCodeUnit.
+    // This method must be called after feedPrecedingCodeUnit() returns
+    // NeedsFollowingCodeUnit.
     TextSegmentationMachineState feedFollowingCodeUnit(UChar codeUnit);
 
-    // Returns the next boundary offset. This method finalizes the state machine if it is not finished.
+    // Returns the next boundary offset. This method finalizes the state machine
+    // if it is not finished.
     int finalizeAndGetBoundaryOffset();
 
     // Resets the internal state to the initial state.
     void reset();
 
 private:
-    int m_codeUnitsToBeDeleted = 0;
+    enum class BackspaceState;
+    friend std::ostream& operator<<(std::ostream&, BackspaceState);
+
+    // Updates the internal state to the |newState| then return
+    // InternalState::NeedMoreCodeUnit.
+    TextSegmentationMachineState moveToNextState(BackspaceState newState);
+
+    // Update the internal state to BackspaceState::Finished, then return
+    // MachineState::Finished.
+    TextSegmentationMachineState finish();
 
     // Used for composing supplementary code point with surrogate pairs.
     UChar m_trailSurrogate = 0;
+
+    // The number of code units to be deleted.
+    int m_codeUnitsToBeDeleted = 0;
+
+    // The length of the previously seen variation selector.
+    int m_lastSeenVSCodeUnits = 0;
+
+    // The internal state.
+    BackspaceState m_state;
 };
 
 } // namespace blink
