4002f77afc105438dab548b5b278e839e0f737b7
pnoland <pnoland@chromium.org>
2016-04-01 14:40:58 -0700

    [sync] fix StringValue constructor dcheck triggering
    ProgressMarkerMapToValue needs to Base64 encode the serialized proto
    string since SetString expects only utf-8 chars
    
    BUG=454461
    
    R=zea@chromium.org
    
    TEST=sync_session_snapshot_unittest in sync_unit_tests is updated to test for
    this problem
    
    Review URL: https://codereview.chromium.org/1846663003
    
    Cr-Commit-Position: refs/heads/master@{#384694}

--- sync/internal_api/public/base/progress_marker_map.cc
+++ sync/internal_api/public/base/progress_marker_map.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "sync/internal_api/public/base/progress_marker_map.h"
 
+#include "base/base64.h"
 #include "base/json/json_writer.h"
 #include "base/json/string_escape.h"
 #include "base/values.h"
@@ -19,6 +20,7 @@ scoped_ptr<base::DictionaryValue> ProgressMarkerMapToValue(
     std::string printable_payload;
     base::EscapeJSONString(
         it->second, false /* put_in_quotes */, &printable_payload);
+    base::Base64Encode(printable_payload, &printable_payload);
     value->SetString(ModelTypeToString(it->first), printable_payload);
   }
   return value;
