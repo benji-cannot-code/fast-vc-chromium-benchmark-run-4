1e57b8139ac904fde3dd49b50d1eda18233849b4
michaelbai <michaelbai@chromium.org>
2016-04-01 13:37:25 -0700

    Add NativeLibraryPreloader interface
    Add NativeLibraryPreloader interface, so we could share more of
    library in certain scenario. Refer bug for details
    
    BUG=579237
    
    TBR=mark
    
    Review URL: https://codereview.chromium.org/1607993003
    
    Cr-Commit-Position: refs/heads/master@{#384680}

---
+++ base/android/java/src/org/chromium/base/library_loader/NativeLibraryPreloader.java
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.base.library_loader;
+
+import android.content.Context;
+
+/**
+ * This is interface to preload the native library before calling System.loadLibrary.
+ */
+public abstract class NativeLibraryPreloader {
+    public abstract void loadLibrary(Context context);
+}
