b8a489f005fc3ec6a7593d7d94d938fc5220c7c6
charliea <charliea@chromium.org>
2016-04-01 04:57:04 -0700

    tracing: Add the TimeTicks clock source as trace metadata
    This is useful when syncing Chrome traces with external traces because
    we have more information to determine whether the two traces use
    timestamps from the same underlying clock and can therefore be combined.
    
    BUG=597350
    
    Review URL: https://codereview.chromium.org/1826973004
    
    Cr-Commit-Position: refs/heads/master@{#384555}

--- chrome/common/trace_event_args_whitelist.cc
+++ chrome/common/trace_event_args_whitelist.cc
@@ -28,6 +28,7 @@ const WhitelistEntry kEventArgsWhitelist[] = {
     {nullptr, nullptr, nullptr}};
 
 const char* kMetadataWhitelist[] = {
+  "clock-domain",
   "command_line",
   "config",
   "cpu-*",
