2fe54af21e072034ad392a2b9bb8b0e331b3391c
mmoroz <mmoroz@chromium.org>
2016-04-01 03:48:44 -0700

    [libfuzzer] disable noisy logging for gfx_png_image_fuzzer.
    Also I've merged existing corpus of this fuzzer with corpus from libpng fuzzer.
    Coverage increased twice (from ~551 to 1128).
    
    R=aizatsky@chromium.org, inferno@chromium.org, krasin@chromium.org, ochang@chromium.org
    BUG=584819
    
    Review URL: https://codereview.chromium.org/1845193003
    
    Cr-Commit-Position: refs/heads/master@{#384542}

--- testing/libfuzzer/fuzzers/gfx_png_image_fuzzer.cc
+++ testing/libfuzzer/fuzzers/gfx_png_image_fuzzer.cc
@@ -6,7 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include "base/logging.h"
 #include "ui/gfx/image/image.h"
+
+
+struct Environment {
+  Environment() {
+    // Disable noisy logging.
+    logging::SetMinLogLevel(logging::LOG_FATAL);
+  }
+};
+
+Environment* env = new Environment();
+
 
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
@@ -19,4 +31,3 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
   return 0;
 }
-
