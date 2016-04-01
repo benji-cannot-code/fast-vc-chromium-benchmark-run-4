cdf7d7fac32b2569d470c8cc66e842a521ba24e9
piman <piman@chromium.org>
2016-03-31 21:45:38 -0700

    Remove NPAPI support for form data
    Since PPAPI doesn't support it either, remove (some of) the corresponding blink
    code.
    
    BUG=493212
    
    Review URL: https://codereview.chromium.org/1844383003
    
    Cr-Commit-Position: refs/heads/master@{#384485}

--- content/plugin/webplugin_delegate_stub.cc
+++ content/plugin/webplugin_delegate_stub.cc
@@ -84,7 +84,6 @@ bool WebPluginDelegateStub::OnMessageReceived(const IPC::Message& msg) {
     IPC_MESSAGE_HANDLER(PluginMsg_HandleInputEvent, OnHandleInputEvent)
     IPC_MESSAGE_HANDLER(PluginMsg_Paint, OnPaint)
     IPC_MESSAGE_HANDLER(PluginMsg_DidPaint, OnDidPaint)
-    IPC_MESSAGE_HANDLER(PluginMsg_GetFormValue, OnGetFormValue)
     IPC_MESSAGE_HANDLER(PluginMsg_UpdateGeometry, OnUpdateGeometry)
     IPC_MESSAGE_HANDLER(PluginMsg_UpdateGeometrySync, OnUpdateGeometry)
     IPC_MESSAGE_HANDLER(PluginMsg_SetContentAreaFocus, OnSetContentAreaFocus)
@@ -178,14 +177,6 @@ void WebPluginDelegateStub::OnUpdateGeometry(
       param.window_rect, param.clip_rect,
       param.windowless_buffer0, param.windowless_buffer1,
       param.windowless_buffer_index);
-}
-
-void WebPluginDelegateStub::OnGetFormValue(base::string16* value,
-                                           bool* success) {
-  *success = false;
-  if (!delegate_)
-    return;
-  *success = delegate_->GetFormValue(value);
 }
 
 void WebPluginDelegateStub::OnSetContentAreaFocus(bool has_focus) {
