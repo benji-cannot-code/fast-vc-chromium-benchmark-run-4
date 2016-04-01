cc9eaa39193a5c73152afc67508809d31c707603
tapted <tapted@chromium.org>
2016-04-01 07:07:15 -0700

    Mac: Hide some unused code from third_party/mozilla that calls deprecated APIs
    ::Gestalt(..) and gestaltSystemVersion are deprecated in 10.8.
    
    In NSWorkspace+Utils.mm, it's only required to implement
    +[NSWorkspace(CaminoDefaultBrowserAdditions) isLeopardOrHigher] which
    isn't needed for Chrome.
    
    Comment it out.
    
    Presubmit also asked for a Security Critical field in the README.chromium.
    
    BUG=547071
    
    Review URL: https://codereview.chromium.org/1844113002
    
    Cr-Commit-Position: refs/heads/master@{#384577}

--- third_party/mozilla/NSWorkspace+Utils.m
+++ third_party/mozilla/NSWorkspace+Utils.m
@@ -157,6 +157,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return [versionInfo objectForKey:@"ProductVersion"];
 }
 
+// Begin Google Modified
+#if 0
 //
 // +systemVersion
 //
@@ -184,5 +186,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return [self systemVersion] >= 0x1050;
 #endif
 }
+#endif
+// End Google Modified
 
 @end
