3476fd5f8f4e51b9fad56609e575022053353b9c
sergiyb <sergiyb@chromium.org>
2016-04-01 04:47:12 -0700

    Revert of Move _CheckPydepsNeedsUpdating() out of _AndroidSpecificOnUploadChecks (patchset #1 id:1 of https://codereview.chromium.org/1842273002/ )
    Reason for revert:
    http://crbug.com/599692
    
    Original issue's description:
    > Move _CheckPydepsNeedsUpdating() out of _AndroidSpecificOnUploadChecks
    >
    > The check isn't currently required downstream, and the paths in
    > _ALL_PYDEPS_FILES were not being resolved correctly.
    >
    > BUG=589318
    >
    > Committed: https://crrev.com/3f73af6402a0b8a8e5dac0a8caea0b7b9e907d49
    > Cr-Commit-Position: refs/heads/master@{#384296}
    
    TBR=jochen@chromium.org,agrieve@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=589318
    
    Review URL: https://codereview.chromium.org/1852663003
    
    Cr-Commit-Position: refs/heads/master@{#384552}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1765,6 +1765,7 @@ def _AndroidSpecificOnUploadChecks(input_api, output_api):
   results.extend(_CheckAndroidCrLogUsage(input_api, output_api))
   results.extend(_CheckAndroidNewMdpiAssetLocation(input_api, output_api))
   results.extend(_CheckAndroidToastUsage(input_api, output_api))
+  results.extend(_CheckPydepsNeedsUpdating(input_api, output_api))
   return results
 
 
@@ -1815,7 +1816,6 @@ def _CommonChecks(input_api, output_api):
   results.extend(_CheckForWindowsLineEndings(input_api, output_api))
   results.extend(_CheckSingletonInHeaders(input_api, output_api))
   results.extend(_CheckNoDeprecatedCompiledResourcesGYP(input_api, output_api))
-  results.extend(_CheckPydepsNeedsUpdating(input_api, output_api))
 
   if any('PRESUBMIT.py' == f.LocalPath() for f in input_api.AffectedFiles()):
     results.extend(input_api.canned_checks.RunUnitTestsInDirectory(
