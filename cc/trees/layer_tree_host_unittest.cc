3a0275f05ee07bdf0ecc5bacbe51a6ff91cf17df
jaydasika <jaydasika@chromium.org>
2016-04-01 11:24:18 -0700

    Disable LayerTreeHostTestGpuRasterDeviceSizeChanged multi-thread case
    BUG=598491
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1851113002
    
    Cr-Commit-Position: refs/heads/master@{#384637}

--- cc/trees/layer_tree_host_unittest.cc
+++ cc/trees/layer_tree_host_unittest.cc
@@ -1000,7 +1000,8 @@ class LayerTreeHostTestGpuRasterDeviceSizeChanged : public LayerTreeHostTest {
 
 // As there's no pending tree in single-threaded case, this test should run
 // only for multi-threaded case.
-MULTI_THREAD_TEST_F(LayerTreeHostTestGpuRasterDeviceSizeChanged);
+// Disabled because its timing out flakily. crbug.com/598491
+// MULTI_THREAD_TEST_F(LayerTreeHostTestGpuRasterDeviceSizeChanged);
 
 class LayerTreeHostTestNoExtraCommitFromInvalidate : public LayerTreeHostTest {
  public:
