3fe629b898a12999582ba4681df17408fd4dc269
shanmuga.m <shanmuga.m@samsung.com>
2016-03-31 18:34:24 -0700

    Support currentScale for embedded SVG
    Added Support of SVGSVGElement.currentScale for embedded
    SVG Objects.
    Also this patch removing pageZoomFactor modification such that by
    applying currentScale will not affect the pageZoomFactor of document.
    
    BUG=174568
    
    Review URL: https://codereview.chromium.org/1827793003
    
    Cr-Commit-Position: refs/heads/master@{#384465}

--- third_party/WebKit/Source/core/svg/SVGSVGElement.h
+++ third_party/WebKit/Source/core/svg/SVGSVGElement.h
@@ -139,7 +139,7 @@ private:
 
     void inheritViewAttributes(SVGViewElement*);
 
-    void updateCurrentTranslate();
+    void updateUserTransform();
 
     void finishParsingChildren() override;
 
@@ -162,6 +162,7 @@ private:
     RefPtrWillBeMember<SMILTimeContainer> m_timeContainer;
     RefPtrWillBeMember<SVGPoint> m_translation;
     RefPtrWillBeMember<SVGViewSpec> m_viewSpec;
+    float m_currentScale;
 
     friend class SVGCurrentTranslateTearOff;
 };
