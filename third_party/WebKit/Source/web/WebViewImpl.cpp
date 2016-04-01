f886a60926fbffb005373d7d5cf24275159a5399
pilgrim <pilgrim@chromium.org>
2016-04-01 11:38:53 -0700

    [Layout API] Convert WebViewImpl to use new layout API
    There is only one reference to LocalFrame::contentLayoutObject(),
    in WebViewImpl::contentsSize(). I converted it to use contentLayoutItem()
    instead. The method it references is already part of the layout API,
    so no new API methods are required.
    
    There are no functional changes.
    
    BUG=499321
    
    Review URL: https://codereview.chromium.org/1851813002
    
    Cr-Commit-Position: refs/heads/master@{#384641}

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/LayoutPart.h"
 #include "core/layout/LayoutView.h"
 #include "core/layout/TextAutosizer.h"
+#include "core/layout/api/LayoutViewItem.h"
 #include "core/layout/compositing/PaintLayerCompositor.h"
 #include "core/loader/DocumentLoader.h"
 #include "core/loader/FrameLoadRequest.h"
@@ -3426,10 +3427,10 @@ IntSize WebViewImpl::contentsSize() const
 {
     if (!page()->mainFrame()->isLocalFrame())
         return IntSize();
-    LayoutView* root = page()->deprecatedLocalMainFrame()->contentLayoutObject();
-    if (!root)
+    LayoutViewItem root = page()->deprecatedLocalMainFrame()->contentLayoutItem();
+    if (root.isNull())
         return IntSize();
-    return root->documentRect().size();
+    return root.documentRect().size();
 }
 
 WebSize WebViewImpl::contentsPreferredMinimumSize()
