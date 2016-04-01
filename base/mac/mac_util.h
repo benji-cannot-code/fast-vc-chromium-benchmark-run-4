5dfff09ae14b8c250199b86b11f4686b9415c812
tapted <tapted@chromium.org>
2016-03-31 18:03:35 -0700

    Mac src/base nit: IWYU some ObjC stuff in mac_util.h
    NSImage* was once wanted here. It is no longer used, so zap some
    headers.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/1849913002
    
    Cr-Commit-Position: refs/heads/master@{#384456}

--- base/mac/mac_util.h
+++ base/mac/mac_util.h
@@ -12,13 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/base_export.h"
-#include "base/logging.h"
-
-#if defined(__OBJC__)
-#import <Foundation/Foundation.h>
-#else  // __OBJC__
-class NSImage;
-#endif  // __OBJC__
 
 namespace base {
 
--- sandbox/mac/os_compatibility.cc
+++ sandbox/mac/os_compatibility.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <unistd.h>
 
+#include "base/logging.h"
 #include "base/mac/mac_util.h"
 #include "sandbox/mac/xpc.h"
 
