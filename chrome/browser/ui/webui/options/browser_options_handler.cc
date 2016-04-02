e67643141ab18c6e3ef04c8d72e0d8584ddf77a9
alemate <alemate@chromium.org>
2016-04-01 18:17:52 -0700

    ChromeOS: Add SystemTimezoneAutomaticDetection policy.
    This CL adds SystemTimezoneAutomaticDetection device policy
    and modifies all necessary components to support it.
    
    BUG=596690
    TEST=unittest,browsertest
    
    Review URL: https://codereview.chromium.org/1843523002
    
    Cr-Commit-Position: refs/heads/master@{#384768}

--- chrome/browser/ui/webui/options/browser_options_handler.cc
+++ chrome/browser/ui/webui/options/browser_options_handler.cc
@@ -1014,6 +1014,13 @@ void BrowserOptionsHandler::InitializeHandler() {
       chromeos::kSystemTimezonePolicy,
       base::Bind(&BrowserOptionsHandler::OnSystemTimezonePolicyChanged,
                  weak_ptr_factory_.GetWeakPtr()));
+  local_state_pref_change_registrar_.Init(g_browser_process->local_state());
+  local_state_pref_change_registrar_.Add(
+      prefs::kSystemTimezoneAutomaticDetectionPolicy,
+      base::Bind(&BrowserOptionsHandler::
+                     OnSystemTimezoneAutomaticDetectionPolicyChanged,
+                 base::Unretained(this)));
+
 #else  // !defined(OS_CHROMEOS)
   profile_pref_registrar_.Add(
       proxy_config::prefs::kProxy,
@@ -1082,6 +1089,7 @@ void BrowserOptionsHandler::InitializePage() {
       profile->IsLegacySupervised()) {
     web_ui()->CallJavascriptFunction("BrowserOptions.hideAndroidAppsSection");
   }
+  OnSystemTimezoneAutomaticDetectionPolicyChanged();
 #endif
 }
 
@@ -1440,6 +1448,22 @@ void BrowserOptionsHandler::OnSystemTimezonePolicyChanged() {
   web_ui()->CallJavascriptFunction(
       "BrowserOptions.setSystemTimezoneManaged",
       base::FundamentalValue(chromeos::system::HasSystemTimezonePolicy()));
+}
+
+void BrowserOptionsHandler::OnSystemTimezoneAutomaticDetectionPolicyChanged() {
+  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
+          chromeos::switches::kEnableSystemTimezoneAutomaticDetectionPolicy)) {
+    return;
+  }
+
+  PrefService* prefs = g_browser_process->local_state();
+  const bool is_managed = prefs->IsManagedPreference(
+      prefs::kSystemTimezoneAutomaticDetectionPolicy);
+  const int value =
+      prefs->GetInteger(prefs::kSystemTimezoneAutomaticDetectionPolicy);
+  web_ui()->CallJavascriptFunction(
+      "BrowserOptions.setSystemTimezoneAutomaticDetectionManaged",
+      base::FundamentalValue(is_managed), base::FundamentalValue(value));
 }
 #endif
 
