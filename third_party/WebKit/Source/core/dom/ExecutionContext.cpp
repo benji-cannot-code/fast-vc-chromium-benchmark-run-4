299bca588ba15fd2d66284c79be3899b8175abb6
yhirano <yhirano@chromium.org>
2016-04-03 23:30:10 -0700

    Add ExecutionContext::outgoingReferrer.
    WorkerThreadableLoader and FetchManager uses url().strippedForUseAsReferrer()
    to obtain the referrer string. Let's call it outgoingReferrer, to be alined
    with Document.
    
    BUG=532385
    
    Review URL: https://codereview.chromium.org/1846523005
    
    Cr-Commit-Position: refs/heads/master@{#384855}

--- third_party/WebKit/Source/core/dom/ExecutionContext.cpp
+++ third_party/WebKit/Source/core/dom/ExecutionContext.cpp
@@ -255,6 +255,11 @@ bool ExecutionContext::isSecureContext(const SecureContextCheck privilegeContext
     return isSecureContext(unusedErrorMessage, privilegeContextCheck);
 }
 
+String ExecutionContext::outgoingReferrer() const
+{
+    return url().strippedForUseAsReferrer();
+}
+
 void ExecutionContext::setReferrerPolicy(ReferrerPolicy referrerPolicy)
 {
     // When a referrer policy has already been set, the latest value takes precedence.
