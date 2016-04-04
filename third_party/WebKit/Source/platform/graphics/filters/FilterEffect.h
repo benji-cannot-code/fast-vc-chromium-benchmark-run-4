139d77af61517310310c38ad06923fbcc7591fd0
senorblanco <senorblanco@chromium.org>
2016-04-04 15:32:56 -0700

    Use Blink's computed filterPrimitiveSubregion() as CropRect.
    For filters which affect transparent black, we were falling back to Skia's clip rect to know which pixels to fill. This works for SVG, where we set the clip rect to the filter region. However, this is not possible for CSS filters on HTML content, where there may be multiple reference filters with different filter regions, so we don't actually set a clip rect at all.
    
    The easiest fix is to set the crop rect unconditionally for all filters with a non-empty filter primitive subregion (aka all reference filters), which is what we do here. This also gets us closer to being able to removing the individual crop edges from Skia. This has minor pixel diffs in the affected tests.
    
    BUG=599933
    
    Review URL: https://codereview.chromium.org/1840303006
    
    Cr-Commit-Position: refs/heads/master@{#385031}

--- third_party/WebKit/Source/platform/graphics/filters/FilterEffect.h
+++ third_party/WebKit/Source/platform/graphics/filters/FilterEffect.h
@@ -167,8 +167,6 @@ protected:
     void addAbsolutePaintRect(const FloatRect& absolutePaintRect);
 
 private:
-    bool hasConnectedInput() const;
-
     FilterEffectVector m_inputEffects;
 
     IntRect m_absolutePaintRect;
