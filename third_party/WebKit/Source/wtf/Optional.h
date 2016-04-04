5ebc27cda31f4d968aee35f388a9d6497b72cac1
toyoshim <toyoshim@chromium.org>
2016-04-04 05:25:50 -0700

    Revert of Replace all occurrences of RELEASE_ASSERT in wtf with CHECK. (patchset #4 id:60001 of https://codereview.chromium.org/1840163002/ )
    Reason for revert:
    Caused performance regression.
    
    Original issue's description:
    > Replace all occurrences of RELEASE_ASSERT in wtf with CHECK.
    >
    > BUG=596760
    >
    > Committed: https://crrev.com/d181f524c11e0d456bc48261334beda61e6f852c
    > Cr-Commit-Position: refs/heads/master@{#384213}
    
    TBR=haraken@chromium.org,yutak@chromium.org,kotenkov@yandex-team.ru
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=596760, 599867
    
    Review URL: https://codereview.chromium.org/1859513002
    
    Cr-Commit-Position: refs/heads/master@{#384889}

--- third_party/WebKit/Source/wtf/Optional.h
+++ third_party/WebKit/Source/wtf/Optional.h
@@ -55,7 +55,7 @@ public:
     template <typename... Args>
     void emplace(Args&&... args)
     {
-        CHECK(!m_ptr);
+        RELEASE_ASSERT(!m_ptr);
         m_ptr = reinterpret_cast_ptr<T*>(&m_storage.buffer);
         new (m_ptr) T(std::forward<Args>(args)...);
     }
