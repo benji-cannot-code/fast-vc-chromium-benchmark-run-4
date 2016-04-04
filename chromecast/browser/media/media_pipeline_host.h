726c900849c05391ad95fe19343b065980594e2e
kmackay <kmackay@chromium.org>
2016-04-04 15:53:37 -0700

    Revert of [Chromecast] Always use "ignore PTS" mode for audio-only backends (patchset #4 id:60001 of https://codereview.chromium.org/1847533005/ )
    Reason for revert:
    Broke playback for apps that use the PTS to track media position.
    
    Original issue's description:
    > [Chromecast] Always use "ignore PTS" mode for audio-only backends
    >
    > BUG= internal b/27496334
    >
    > Committed: https://crrev.com/1b9bdfa54c637ee1f19a879db09868d3bd7889eb
    > Cr-Commit-Position: refs/heads/master@{#384796}
    
    TBR=dcheng@chromium.org,alokp@chromium.org,halliwell@chromium.org,lcwu@google.com,servolk@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG= internal b/27496334
    
    Review URL: https://codereview.chromium.org/1856083003
    
    Cr-Commit-Position: refs/heads/master@{#385039}

--- chromecast/browser/media/media_pipeline_host.h
+++ chromecast/browser/media/media_pipeline_host.h
@@ -46,7 +46,6 @@ class MediaPipelineHost {
   ~MediaPipelineHost();
 
   void Initialize(LoadType load_type,
-                  AvailableTracks available_tracks,
                   const MediaPipelineClient& client,
                   const CreateMediaPipelineBackendCB& create_backend_cb);
 
--- chromecast/renderer/media/cma_renderer.cc
+++ chromecast/renderer/media/cma_renderer.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/single_thread_task_runner.h"
 #include "base/thread_task_runner_handle.h"
-#include "chromecast/common/media/cma_ipc_common.h"
 #include "chromecast/media/cma/base/balanced_media_task_runner_factory.h"
 #include "chromecast/media/cma/base/cma_logging.h"
 #include "chromecast/media/cma/base/demuxer_stream_adapter.h"
@@ -117,16 +116,6 @@ void CmaRenderer::Initialize(
   ended_cb_ = ended_cb;
   error_cb_ = error_cb;
   waiting_for_decryption_key_cb_ = waiting_for_decryption_key_cb;
-
-  bool has_audio = (demuxer_stream_provider_->GetStream(
-                        ::media::DemuxerStream::AUDIO) != nullptr);
-  bool has_video = (demuxer_stream_provider_->GetStream(
-                        ::media::DemuxerStream::VIDEO) != nullptr);
-  DCHECK(has_audio || has_video);
-  AvailableTracks available_tracks =
-      (has_audio ? (has_video ? AUDIO_AND_VIDEO_TRACKS : AUDIO_TRACK_ONLY)
-                 : VIDEO_TRACK_ONLY);
-  media_pipeline_->Initialize(available_tracks);
 
   MediaPipelineClient media_pipeline_client;
   media_pipeline_client.error_cb = ::media::BindToCurrentLoop(error_cb_);
