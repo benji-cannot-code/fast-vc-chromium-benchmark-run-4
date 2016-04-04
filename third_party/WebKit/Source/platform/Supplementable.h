9c8959a2e2a08ac52eda0977eaf3f2b9843b122a
jbroman <jbroman@chromium.org>
2016-04-04 11:08:22 -0700

    Use static_cast, not reinterpret_cast, to cast Supplement pointers.
    This should always be a downcast, which doesn't need the stronger powers of
    reinterpret_cast. static_cast is already used in most instances of this, and
    it's safer.
    
    Review URL: https://codereview.chromium.org/1854163002
    
    Cr-Commit-Position: refs/heads/master@{#384954}

--- third_party/WebKit/Source/platform/Supplementable.h
+++ third_party/WebKit/Source/platform/Supplementable.h
@@ -70,7 +70,7 @@ namespace blink {
 //
 //     MyClass* MyClass::from(MySupplementable* host)
 //     {
-//         return reinterpret_cast<MyClass*>(Supplement<MySupplementable>::from(host, supplementName()));
+//         return static_cast<MyClass*>(Supplement<MySupplementable>::from(host, supplementName()));
 //     }
 //
 // What you should know about thread checks
