b1a75ea543391b03a7887a1f43fc685e29bb5124
reveman <reveman@chromium.org>
2016-04-04 03:19:10 -0700

    exo: Implement configure acks.
    Implement support for acknowledgement of configure requests. This
    allows clients to synchronize changes to surface origin with
    contents updates, which is required to properly resize a window
    in top-left direction.
    
    BUG=549781
    TEST=exo_unittests --gtest_filter=ShellSurfaceTest.AcknowledgeConfigure
    
    Review URL: https://codereview.chromium.org/1840593002
    
    Cr-Commit-Position: refs/heads/master@{#384876}

--- components/exo/shell_surface.h
+++ components/exo/shell_surface.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_EXO_SHELL_SURFACE_H_
 #define COMPONENTS_EXO_SHELL_SURFACE_H_
 
+#include <deque>
 #include <string>
 
 #include "ash/wm/window_state_observer.h"
@@ -15,6 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/surface_delegate.h"
 #include "components/exo/surface_observer.h"
 #include "ui/aura/window_observer.h"
+#include "ui/gfx/geometry/point.h"
+#include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/vector2d.h"
 #include "ui/views/widget/widget_delegate.h"
 #include "ui/wm/public/activation_change_observer.h"
 
@@ -66,13 +70,18 @@ class ShellSurface : public SurfaceDelegate,
   // it doesn't resize, pick a smaller size (to satisfy aspect ratio or resize
   // in steps of NxM pixels).
   using ConfigureCallback =
-      base::Callback<void(const gfx::Size& size,
-                          ash::wm::WindowStateType state_type,
-                          bool resizing,
-                          bool activated)>;
+      base::Callback<uint32_t(const gfx::Size& size,
+                              ash::wm::WindowStateType state_type,
+                              bool resizing,
+                              bool activated)>;
   void set_configure_callback(const ConfigureCallback& configure_callback) {
     configure_callback_ = configure_callback;
   }
+
+  // When the client is asked to configure the surface, it should acknowledge
+  // the configure request sometime before the commit. |serial| is the serial
+  // from the configure callback.
+  void AcknowledgeConfigure(uint32_t serial);
 
   // Set the "parent" of this surface. This window should be stacked above a
   // parent.
@@ -150,6 +159,9 @@ class ShellSurface : public SurfaceDelegate,
                                    ash::wm::WindowStateType old_type) override;
 
   // Overridden from aura::WindowObserver:
+  void OnWindowBoundsChanged(aura::Window* window,
+                             const gfx::Rect& old_bounds,
+                             const gfx::Rect& new_bounds) override;
   void OnWindowDestroying(aura::Window* window) override;
 
   // Overridden from aura::client::ActivationChangeObserver:
@@ -163,14 +175,18 @@ class ShellSurface : public SurfaceDelegate,
   void OnMouseEvent(ui::MouseEvent* event) override;
 
  private:
+  // Surface state associated with each configure request.
+  struct Config {
+    uint32_t serial;
+    gfx::Vector2d origin_offset;
+    int resize_component;
+  };
+
   // Creates the |widget_| for |surface_|.
   void CreateShellSurfaceWidget();
 
   // Asks the client to configure its surface.
   void Configure();
-
-  // Returns the "visible bounds" for the surface from the user's perspective.
-  gfx::Rect GetVisibleBounds() const;
 
   // Attempt to start a drag operation. The type of drag operation to start is
   // determined by |component|.
@@ -182,6 +198,16 @@ class ShellSurface : public SurfaceDelegate,
   // Returns true if surface is currently being resized.
   bool IsResizing() const;
 
+  // Returns the "visible bounds" for the surface from the user's perspective.
+  gfx::Rect GetVisibleBounds() const;
+
+  // Returns the origin for the surface taking visible bounds and current
+  // resize direction into account.
+  gfx::Point GetSurfaceOrigin() const;
+
+  // Update bounds of widget to match the current surface bounds.
+  void UpdateWidgetBounds();
+
   views::Widget* widget_;
   Surface* surface_;
   aura::Window* parent_;
@@ -190,9 +216,17 @@ class ShellSurface : public SurfaceDelegate,
   base::string16 title_;
   std::string application_id_;
   gfx::Rect geometry_;
+  gfx::Rect pending_geometry_;
   base::Closure close_callback_;
   base::Closure surface_destroyed_callback_;
   ConfigureCallback configure_callback_;
+  bool ignore_window_bounds_changes_;
+  gfx::Point origin_;
+  gfx::Vector2d pending_origin_offset_;
+  gfx::Vector2d pending_origin_config_offset_;
+  int resize_component_;  // HT constant (see ui/base/hit_test.h)
+  int pending_resize_component_;
+  std::deque<Config> pending_configs_;
   scoped_ptr<ash::WindowResizer> resizer_;
 
   DISALLOW_COPY_AND_ASSIGN(ShellSurface);
