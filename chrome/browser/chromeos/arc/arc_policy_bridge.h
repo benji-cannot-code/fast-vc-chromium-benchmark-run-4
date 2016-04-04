fc2efaaa461e829492dc0193af323ce536e191b1
phweiss <phweiss@chromium.org>
2016-04-04 10:55:05 -0700

    Implement ArcPolicyBridge::GetPolicies, add tests
    Small refactoring to make mocking of the policy service
    possible. For arc unit tests, the gn-build files were not adjusted
    yet. I needed to add one dependency that is needed through the
    following code path:
    ---
    FAILED: [...] gomacc [...]
    obj/chrome/browser/chromeos/unit_tests/settings_bridge_unittest.o
    In file included from
    ../../chrome/browser/chromeos/arc/settings_bridge_unittest.cc:5:
    In file included from
    ../../chrome/browser/chromeos/arc/settings_bridge.h:13:
    In file included from
    ../../chromeos/settings/timezone_settings.h:13:
    ../../third_party/icu/source/i18n/unicode/timezone.h:30:10:
    fatal error: 'unicode/utypes.h' file not found
    ---
    
    BUG=b/27807832
    
    Review URL: https://codereview.chromium.org/1819883003
    
    Cr-Commit-Position: refs/heads/master@{#384947}

--- chrome/browser/chromeos/arc/arc_policy_bridge.h
+++ chrome/browser/chromeos/arc/arc_policy_bridge.h
@@ -6,10 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_ARC_ARC_POLICY_BRIDGE_H_
 #define CHROME_BROWSER_CHROMEOS_ARC_ARC_POLICY_BRIDGE_H_
 
+#include <string>
+
+#include "base/macros.h"
 #include "components/arc/arc_bridge_service.h"
 #include "components/arc/arc_service.h"
 #include "components/policy/core/common/policy_service.h"
 #include "mojo/public/cpp/bindings/binding.h"
+
+namespace policy {
+class PolicyMap;
+}  // namespace policy
 
 namespace arc {
 
@@ -19,6 +26,8 @@ class ArcPolicyBridge : public ArcService,
                         public policy::PolicyService::Observer {
  public:
   explicit ArcPolicyBridge(ArcBridgeService* bridge_service);
+  ArcPolicyBridge(ArcBridgeService* bridge_service,
+                  policy::PolicyService* policy_service);
   ~ArcPolicyBridge() override;
 
   // ArcBridgeService::Observer overrides.
@@ -34,6 +43,9 @@ class ArcPolicyBridge : public ArcService,
                        const policy::PolicyMap& current) override;
 
  private:
+  void InitializePolicyService();
+  std::string GetFilteredJSONPolicies(const policy::PolicyMap& policy_map);
+
   mojo::Binding<PolicyHost> binding_;
   policy::PolicyService* policy_service_ = nullptr;
 
