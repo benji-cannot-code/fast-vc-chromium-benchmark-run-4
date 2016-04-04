cb456dafb9f87e942c534d108b114810175edc22
rune <rune@opera.com>
2016-04-04 01:34:50 -0700

    Fixed ::slotted performance in pure v1 shadow documents.
    Instead of traversing all m_treeBoundaryCrossingScopes matching rules
    from other scopes, just walk the assignedSlot() chain for resolvers.
    This makes rule matching a lot cheaper since quite a lot of components
    have tree boundary crossing rules in practice (polymer apps using v0
    typically have hundreds of such scopes).
    
    The assumption here is that the assignedSlot chain most of the time
    will be quite short and/or cheap to walk.
    
    Introducing a flag set in StyleEngine if there ever exists a v0 shadow
    tree to fall back to traversing m_treeBoundaryCrossingScopes when
    necessary.
    
    For the slotted.html demo in crbug.com/599833, the full recalc with
    ~4000 elements goes from ~800ms to ~40ms with this change when each
    shadow tree has a ::slotted rule. No substantial regression for the
    case without ::slotted rules.
    
    Added a test for /deep/ being used a descendant combinator in a
    document without shadow trees, as I feared I might have broken that and
    I couldn't find any existing tests for it.
    
    For later, we may choose to never collect m_treeBoundaryCrossingScopes
    for pure v1 documents, but that means we would have to recreate that
    collection once we add a v0 shadow to the document.
    
    R=kochi@chromium.org,hayato@chromium.org,dglazkov@chromium.org
    BUG=599833
    
    Review URL: https://codereview.chromium.org/1853713002
    
    Cr-Commit-Position: refs/heads/master@{#384867}

--- third_party/WebKit/Source/core/dom/StyleEngine.h
+++ third_party/WebKit/Source/core/dom/StyleEngine.h
@@ -186,6 +186,7 @@ public:
 
     ShadowCascadeOrder shadowCascadeOrder() const { return m_shadowCascadeOrder; }
     void setShadowCascadeOrder(ShadowCascadeOrder);
+    bool mayContainV0Shadow() const { return m_mayContainV0Shadow; }
 
     DECLARE_VIRTUAL_TRACE();
 
@@ -261,6 +262,7 @@ private:
 
     bool m_ignorePendingStylesheets = false;
     bool m_didCalculateResolver = false;
+    bool m_mayContainV0Shadow = false;
 
     ShadowCascadeOrder m_shadowCascadeOrder = ShadowCascadeNone;
 
