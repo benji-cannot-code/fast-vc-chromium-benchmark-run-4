6cd3e8bdea367c05156b22010b0d175f3bf7f7c9
keishi <keishi@chromium.org>
2016-04-05 00:27:37 -0700

    Fix incorrect blink_gc memory dump caused by r383965
    BUG=600574
    
    Review URL: https://codereview.chromium.org/1853423003
    
    Cr-Commit-Position: refs/heads/master@{#385120}

--- third_party/WebKit/Source/platform/heap/Heap.cpp
+++ third_party/WebKit/Source/platform/heap/Heap.cpp
@@ -113,6 +113,12 @@ void ProcessHeap::init()
     s_isLowEndDevice = base::SysInfo::IsLowEndDevice();
 }
 
+void ProcessHeap::resetHeapCounters()
+{
+    s_totalAllocatedObjectSize = 0;
+    s_totalMarkedObjectSize = 0;
+}
+
 void Heap::init()
 {
     ThreadState::init();
@@ -689,6 +695,7 @@ void Heap::resetHeapCounters()
 
     Heap::reportMemoryUsageForTracing();
 
+    ProcessHeap::resetHeapCounters();
     Heap::heapStats().reset();
     for (ThreadState* state : ThreadState::attachedThreads())
         state->resetHeapCounters();
