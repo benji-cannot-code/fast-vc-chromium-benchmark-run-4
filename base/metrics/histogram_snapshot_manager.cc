246805a16934851fbb400fe43aad98274af8ad1a
bcwhite <bcwhite@chromium.org>
2016-04-05 12:41:53 -0700

    Keep histogram and bucket-ranges info in minidumps.
    BUG=600717
    
    Review URL: https://codereview.chromium.org/1860033003
    
    Cr-Commit-Position: refs/heads/master@{#385259}

--- base/metrics/histogram_snapshot_manager.cc
+++ base/metrics/histogram_snapshot_manager.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/debug/alias.h"
 #include "base/metrics/histogram_flattener.h"
 #include "base/metrics/histogram_samples.h"
 #include "base/metrics/statistics_recorder.h"
@@ -119,6 +120,10 @@ void HistogramSnapshotManager::PrepareSamples(
     // The checksum should have caught this, so crash separately if it didn't.
     CHECK_NE(0U, HistogramBase::RANGE_CHECKSUM_ERROR & corruption);
     CHECK(false);  // Crash for the bucket order corruption.
+    // Ensure that compiler keeps around pointers to |histogram| and its
+    // internal |bucket_ranges_| for any minidumps.
+    base::debug::Alias(
+        static_cast<const Histogram*>(histogram)->bucket_ranges());
   }
   // Checksum corruption might not have caused order corruption.
   CHECK_EQ(0U, HistogramBase::RANGE_CHECKSUM_ERROR & corruption);
