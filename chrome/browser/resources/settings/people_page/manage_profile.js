fccec93c8b9b59e2ed7c97ef864f415745a6d900
tommycli <tommycli@chromium.org>
2016-04-05 15:25:04 -0700

    Settings People Revamp: Fix Mange Profile empty name crash.
    Prevents sending an empty string name to C++.
    
    BUG=597868,599842
    
    Review URL: https://codereview.chromium.org/1857203003
    
    Cr-Commit-Position: refs/heads/master@{#385305}

--- chrome/browser/resources/settings/people_page/manage_profile.js
+++ chrome/browser/resources/settings/people_page/manage_profile.js
@@ -53,6 +53,9 @@ Polymer({
    * @param {!Event} event
    */
   onProfileNameChanged_: function(event) {
+    if (event.target.invalid)
+      return;
+
     settings.SyncPrivateApi.setProfileIconAndName(this.profileIconUrl,
                                                   event.target.value);
   },
