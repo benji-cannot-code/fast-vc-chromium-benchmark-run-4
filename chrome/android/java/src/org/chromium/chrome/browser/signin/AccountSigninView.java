5669a68dc0c99897591b9835d76b85d6134df368
peconn <peconn@chromium.org>
2016-04-05 02:18:37 -0700

    Disable the signin buttons while waiting for async callbacks.
    BUG=597238,598001
    
    Review URL: https://codereview.chromium.org/1856013002
    
    Cr-Commit-Position: refs/heads/master@{#385139}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/AccountSigninView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/AccountSigninView.java
@@ -500,6 +500,7 @@ public class AccountSigninView extends FirstRunView
         updateProfileName();
 
         configureSpinner(false);
+        setButtonsEnabled(true);
         setUpConfirmButton();
         setUpUndoButton();
 
@@ -524,6 +525,9 @@ public class AccountSigninView extends FirstRunView
     }
 
     private void showConfirmSigninPageAccountTrackerServiceCheck() {
+        // Disable the buttons to prevent them being clicked again while waiting for the callbacks.
+        setButtonsEnabled(false);
+
         // Ensure that the AccountTrackerService has a fully up to date GAIA id <-> email mapping,
         // as this is needed for the previous account check.
         if (AccountTrackerService.get(getContext()).checkAndSeedSystemAccounts()) {
@@ -566,6 +570,11 @@ public class AccountSigninView extends FirstRunView
                             } else {
                                 showConfirmSigninPage();
                             }
+                        }
+
+                        @Override
+                        public void onCancel() {
+                            setButtonsEnabled(true);
                         }
                     });
         } else {
--- chrome/android/java/src/org/chromium/chrome/browser/sync/SyncAccountSwitcher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/SyncAccountSwitcher.java
@@ -88,7 +88,11 @@ public class SyncAccountSwitcher
                 }
             }
         });
+    }
 
+    @Override
+    public void onCancel() {
+        // The user aborted the 'merge data' dialog, there is nothing to do.
     }
 
     @Override
