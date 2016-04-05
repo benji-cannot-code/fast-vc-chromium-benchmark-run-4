c81c3e1df35f32b7c52bdaef470b8dad2ac36600
wolenetz <wolenetz@chromium.org>
2016-04-05 12:54:41 -0700

    Media: Report informational error messages to HTMLMediaElement
    Caches the most recent (if any) PIPELINE_ERROR and MEDIA_ERROR_LOG_ENTRY
    MediaLog events and serves their concatenation to HTMLMediaElement as an
    informative media error message.
    
    The next step will be for HTMLMediaElement to report this error message
    to the devtools console upon transition into error state. Other
    cumulative improvements to add such messages to MediaError for
    visibility to web apps, and to clarify the WebMediaPlayer and
    WebMediaPlayerClient error reporting channel will come later.
    
    BUG=472253
    
    Review URL: https://codereview.chromium.org/1850733003
    
    Cr-Commit-Position: refs/heads/master@{#385264}

--- content/renderer/media/html_video_element_capturer_source_unittest.cc
+++ content/renderer/media/html_video_element_capturer_source_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/WebKit/public/platform/WebMediaPlayer.h"
+#include "third_party/WebKit/public/platform/WebString.h"
 
 using ::testing::_;
 using ::testing::InSequence;
@@ -61,6 +62,7 @@ class MockWebMediaPlayer : public blink::WebMediaPlayer,
   ReadyState getReadyState() const override {
     return ReadyStateHaveNothing;
   }
+  blink::WebString getErrorMessage() override { return blink::WebString(); }
   bool didLoadingProgress() override { return true; }
   bool hasSingleSecurityOrigin() const override { return true; }
   bool didPassCORSAccessCheck() const override { return true; }
--- content/renderer/media/render_media_log.cc
+++ content/renderer/media/render_media_log.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/renderer/media/render_media_log.h"
 
+#include <sstream>
+
 #include "base/bind.h"
 #include "base/location.h"
 #include "base/logging.h"
@@ -52,12 +54,31 @@ void RenderMediaLog::AddEvent(scoped_ptr<media::MediaLogEvent> event) {
   {
     base::AutoLock auto_lock(lock_);
 
-    // Keep track of the latest buffered extents properties to avoid sending
-    // thousands of events over IPC. See http://crbug.com/352585 for details.
-    if (event->type == media::MediaLogEvent::BUFFERED_EXTENTS_CHANGED)
-      last_buffered_extents_changed_event_.swap(event);
-    else
-      queued_media_events_.push_back(*event);
+    switch (event->type) {
+      case media::MediaLogEvent::BUFFERED_EXTENTS_CHANGED:
+        // Keep track of the latest buffered extents properties to avoid sending
+        // thousands of events over IPC. See http://crbug.com/352585 for
+        // details.
+        last_buffered_extents_changed_event_.swap(event);
+        // SendQueuedMediaEvents() will enqueue the most recent event of this
+        // kind, if any, prior to sending the event batch.
+        break;
+
+      // Hold onto the most recent PIPELINE_ERROR and MEDIA_LOG_ERROR_ENTRY for
+      // use in GetLastErrorMessage().
+      case media::MediaLogEvent::PIPELINE_ERROR:
+        queued_media_events_.push_back(*event);
+        last_pipeline_error_.swap(event);
+        break;
+      case media::MediaLogEvent::MEDIA_ERROR_LOG_ENTRY:
+        queued_media_events_.push_back(*event);
+        last_media_error_log_entry_.swap(event);
+        break;
+
+      // Just enqueue all other event types for throttled transmission.
+      default:
+        queued_media_events_.push_back(*event);
+    }
 
     if (ipc_send_pending_)
       return;
@@ -81,6 +102,21 @@ void RenderMediaLog::AddEvent(scoped_ptr<media::MediaLogEvent> event) {
   }
   task_runner_->PostTask(
       FROM_HERE, base::Bind(&RenderMediaLog::SendQueuedMediaEvents, this));
+}
+
+std::string RenderMediaLog::GetLastErrorMessage() {
+  base::AutoLock auto_lock(lock_);
+
+  // Return the conditional concatenation of the last pipeline error and the
+  // last media error log.
+  std::stringstream result;
+  if (last_pipeline_error_) {
+    result << MediaEventToLogString(*last_pipeline_error_)
+           << (last_media_error_log_entry_ ? ", " : "");
+  }
+  if (last_media_error_log_entry_)
+    result << MediaEventToLogString(*last_media_error_log_entry_);
+  return result.str();
 }
 
 void RenderMediaLog::SendQueuedMediaEvents() {
