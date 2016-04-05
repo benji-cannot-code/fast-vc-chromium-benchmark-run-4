cb0388c84c1b0b23e3b36685f57f4024d0146b6e
dgn <dgn@chromium.org>
2016-04-05 07:07:41 -0700

    [tool] SpnegoAuth: add multi account, credential confirmation
    Add more features to the SpnegoAuthenticator:
    
     -  Set up up to 2 accounts.
     -  Account 1 will start authenticated.
     -  Account 2 will start unauthenticated. The first token request
        will require an additional confirmation step.
     -  Accounts can be added and removed from the Android account
        settings screen
    
    BUG=534293
    
    Review URL: https://codereview.chromium.org/1416443003
    
    Cr-Commit-Position: refs/heads/master@{#385165}

---
+++ tools/android/kerberos/SpnegoAuthenticator/src/org/chromium/tools/spnegoauthenticator/Constants.java
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.tools.spnegoauthenticator;
+
+class Constants {
+    static final String TAG = "tools_SpnegoAuth";
+    static final String AUTH_TOKEN = "DummyAuthToken";
+    static final int CONFIRM_CREDENTIAL_NOTIFICATION_ID = 42;
+    static final String ACCOUNT_1_NAME = "Dummy SpnegoAccount 1";
+    static final String ACCOUNT_2_NAME = "Dummy SpnegoAccount 2";
+}
