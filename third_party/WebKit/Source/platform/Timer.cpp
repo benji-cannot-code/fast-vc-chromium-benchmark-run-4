a37e5f1d79f66be651ee802aad1b94359c0d6c89
slan <slan@chromium.org>
2016-04-05 08:56:42 -0700

    [Build Fix] Initialize m_thread when IS_DCHECK_ON().
    This change removed use of ASSERT macros:
      https://codereview.chromium.org/1826733002
    
    However, a bug was introduced such that m_thread is only initialized
    when ENABLE(ASSERT), yet it is tested when DCHECK_IS_ON(). Chromecast
    builds disable ASSERT for size reasons, but enable DCHECK. Correct this
    such that m_thread is initialized and tested IFF DCHECK_IS_ON().
    
    Test: Chromecast boots and runs as expected
    
    BUG= internal b/28004974
    
    Review URL: https://codereview.chromium.org/1859763004
    
    Cr-Commit-Position: refs/heads/master@{#385178}

--- third_party/WebKit/Source/platform/Timer.cpp
+++ third_party/WebKit/Source/platform/Timer.cpp
@@ -48,7 +48,7 @@ TimerBase::TimerBase(WebTaskRunner* webTaskRunner)
     , m_repeatInterval(0)
     , m_cancellableTimerTask(nullptr)
     , m_webTaskRunner(webTaskRunner)
-#if ENABLE(ASSERT)
+#if DCHECK_IS_ON()
     , m_thread(currentThread())
 #endif
 {
