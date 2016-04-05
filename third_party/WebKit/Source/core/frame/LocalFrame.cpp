af7114697d56e085cc7e52eb9df0e6e84f25a08b
dcheng <dcheng@chromium.org>
2016-04-05 15:39:27 -0700

    Move isNavigationAllowed() check to main entry point for loads.
    Also document the difference between the two types of navigation
    disablers and how they should be used.
    
    BUG=600182
    
    Review URL: https://codereview.chromium.org/1858833003
    
    Cr-Commit-Position: refs/heads/master@{#385306}

--- third_party/WebKit/Source/core/frame/LocalFrame.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrame.cpp
@@ -293,8 +293,6 @@ void LocalFrame::navigate(Document& originDocument, const KURL& url, bool replac
 
 void LocalFrame::navigate(const FrameLoadRequest& request)
 {
-    if (!isNavigationAllowed())
-        return;
     m_loader.load(request);
 }
 
