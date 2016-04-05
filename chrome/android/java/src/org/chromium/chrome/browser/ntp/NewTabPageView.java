a9411da446edc2503294db6ecd1a917742e09440
mcwilliams <mcwilliams@chromium.org>
2016-04-05 03:47:53 -0700

    Add a snippet item decoration to recycler view for cards and make the card full bleed with a divider and padding
    BUG=584351
    
    Sreenshots:
    https://screenshot.googleplex.com/1D4fGYLxmk0
    https://screenshot.googleplex.com/m6rp2Cv5TKJ
    
    Review URL: https://codereview.chromium.org/1849173002
    
    Cr-Commit-Position: refs/heads/master@{#385146}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -45,6 +45,7 @@ import org.chromium.chrome.browser.ntp.MostVisitedItem.MostVisitedItemManager;
 import org.chromium.chrome.browser.ntp.NewTabPage.OnSearchBoxScrollListener;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageAdapter;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageRecyclerView;
+import org.chromium.chrome.browser.ntp.snippets.SnippetItemDecoration;
 import org.chromium.chrome.browser.ntp.snippets.SnippetsBridge.SnippetsObserver;
 import org.chromium.chrome.browser.profiles.MostVisitedSites.MostVisitedURLsObserver;
 import org.chromium.chrome.browser.profiles.MostVisitedSites.ThumbnailCallback;
@@ -386,6 +387,7 @@ public class NewTabPageView extends FrameLayout
                 }
             });
             initializeSearchBoxRecyclerViewScrollHandling();
+            mRecyclerView.addItemDecoration(new SnippetItemDecoration(getContext()));
         } else {
             initializeSearchBoxScrollHandling();
         }
