3f2c6f3782a12c5c99d4b56f64eed7f6bbd92318
perkj <perkj@chromium.org>
2016-04-05 07:35:03 -0700

    Prepare to remove webrtc::MediaStreamTrackInterface::set_state This removes the use of webrtc::MediaStreamTrackInterface::set_state and instead cleanup the Mock audio and video tracks and adds SetState on those. Only affects unittests.
    BUG=webrtc:5426
    
    Review URL: https://codereview.chromium.org/1853793002
    
    Cr-Commit-Position: refs/heads/master@{#385168}

--- content/renderer/media/webrtc/media_stream_remote_video_source_unittest.cc
+++ content/renderer/media/webrtc/media_stream_remote_video_source_unittest.cc
@@ -90,8 +90,7 @@ class MediaStreamRemoteVideoSourceTest
   }
 
   void StopWebRtcTrack() {
-    static_cast<MockWebRtcVideoTrack*>(webrtc_video_track_.get())->set_state(
-        webrtc::MediaStreamTrackInterface::kEnded);
+    static_cast<MockWebRtcVideoTrack*>(webrtc_video_track_.get())->SetEnded();
   }
 
   const blink::WebMediaStreamSource& webkit_source() const {
