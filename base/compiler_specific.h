3acc426b8ebe832c8d857842a6216268e9d3ee21
jfb <jfb@chromium.org>
2016-04-05 13:50:35 -0700

    bit_cast: check is_trivially_copyable
    Instead of using a misleading comment about POD-ness.
    
    R=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/1837563002
    
    Cr-Commit-Position: refs/heads/master@{#385281}

--- base/compiler_specific.h
+++ base/compiler_specific.h
@@ -188,4 +188,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // defined(COMPILER_GCC)
 #endif  // !defined(UNLIKELY)
 
+// Compiler feature-detection.
+// http://clang.llvm.org/docs/LanguageExtensions.html
+#if !defined(__has_feature)
+#define __has_feature(FEATURE) 0
+#endif
+
 #endif  // BASE_COMPILER_SPECIFIC_H_
