f7f2fc0d675d976edea107fe3a797e03c92daffe
nsatragno <nsatragno@chromium.org>
2016-04-05 11:35:05 -0700

    Added Android UI for background sync content setting
    This patch allows the user to toggle background sync on Chrome for
    Android.
    
    BUG=564052
    TEST=SiteSettingsPreferencesTest.{testAllowBackgroundSync, testBlockBackgroundSync}
    
    Review URL: https://codereview.chromium.org/1742433002
    
    Cr-Commit-Position: refs/heads/master@{#385233}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/WebsitePermissionsFetcher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/WebsitePermissionsFetcher.java
@@ -71,7 +71,7 @@ public class WebsitePermissionsFetcher {
         // Website storage is per-host.
         queue.add(new WebStorageInfoFetcher());
         // Popup exceptions are host-based patterns (unless we start
-        // synchronizing popup exceptions with desktop Chrome.)
+        // synchronizing popup exceptions with desktop Chrome).
         queue.add(new PopupExceptionInfoFetcher());
         // JavaScript exceptions are host-based patterns.
         queue.add(new JavaScriptExceptionInfoFetcher());
@@ -83,6 +83,8 @@ public class WebsitePermissionsFetcher {
         queue.add(new CameraCaptureInfoFetcher());
         // Micropohone capture permission is per-origin and per-embedder.
         queue.add(new MicrophoneCaptureInfoFetcher());
+        // Background sync permission is per-origin.
+        queue.add(new BackgroundSyncExceptionInfoFetcher());
         queue.add(new PermissionsAvailableCallbackRunner());
         queue.next();
     }
@@ -130,6 +132,9 @@ public class WebsitePermissionsFetcher {
         } else if (category.showNotificationsSites()) {
             // Push notification permission is per-origin.
             queue.add(new NotificationInfoFetcher());
+        } else if (category.showBackgroundSyncSites()) {
+            // Background sync info is per-origin.
+            queue.add(new BackgroundSyncExceptionInfoFetcher());
         } else if (category.showProtectedMediaSites()) {
             // Protected media identifier permission is per-origin and per-embedder.
             queue.add(new ProtectedMediaIdentifierInfoFetcher());
@@ -375,6 +380,24 @@ public class WebsitePermissionsFetcher {
                 WebsiteAddress address = WebsiteAddress.create(info.getOrigin());
                 if (address == null) continue;
                 createSiteByOriginAndHost(address).setMicrophoneInfo(info);
+            }
+        }
+    }
+
+    private class BackgroundSyncExceptionInfoFetcher extends Task {
+        @Override
+        public void run() {
+            for (ContentSettingException exception :
+                    WebsitePreferenceBridge.getContentSettingsExceptions(
+                            ContentSettingsType.CONTENT_SETTINGS_TYPE_BACKGROUND_SYNC)) {
+                // The pattern "*" represents the default setting, not a specific website.
+                if (exception.getPattern().equals("*")) continue;
+                WebsiteAddress address = WebsiteAddress.create(exception.getPattern());
+                if (address == null) continue;
+                Set<Website> sites = findOrCreateSitesByHost(address);
+                for (Website site : sites) {
+                    site.setBackgroundSyncException(exception);
+                }
             }
         }
     }
--- chrome/browser/android/preferences/website_preference_bridge.cc
+++ chrome/browser/android/preferences/website_preference_bridge.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/logging.h"
 #include "base/macros.h"
 #include "chrome/browser/browsing_data/browsing_data_local_storage_helper.h"
 #include "chrome/browser/browsing_data/browsing_data_quota_helper.h"
@@ -63,7 +64,7 @@ void GetOrigins(JNIEnv* env,
                 jboolean managedOnly) {
   ContentSettingsForOneType all_settings;
   HostContentSettingsMap* content_settings_map =
-      GetHostContentSettingsMap(false);
+      GetHostContentSettingsMap(false);  // is_incognito
   content_settings_map->GetSettingsForOneType(
       content_type, std::string(), &all_settings);
   ContentSetting default_content_setting = content_settings_map->
@@ -330,7 +331,7 @@ static void SetNotificationSettingForOrigin(
   // permission types. See https://crbug.com/416894.
   Profile* profile = GetActiveUserProfile(is_incognito);
   GURL url = GURL(ConvertJavaStringToUTF8(env, origin));
-  ContentSetting setting = (ContentSetting) value;
+  ContentSetting setting = static_cast<ContentSetting>(value);
   switch (setting) {
     case CONTENT_SETTING_DEFAULT:
       DesktopNotificationProfileUtil::ClearSetting(profile, url);
