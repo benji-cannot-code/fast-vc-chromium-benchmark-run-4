505189866a3f7264bd2083dd29877836f4ee279c
bnc <bnc@chromium.org>
2016-04-05 09:51:09 -0700

    Remove declaration of undefined method; add static comments.
    * Add static comment above function definition for three IOThread static
      methods.
    * Remove IOThread::InitializeNetworkSessionParamsFromGlobals() declaration: this
      method is undefined and unused.
    
    This is a follow-up to https://crrev.com/1687423002.
    
    BUG=586616
    
    Review URL: https://codereview.chromium.org/1858103003
    
    Cr-Commit-Position: refs/heads/master@{#385195}

--- chrome/browser/io_thread.cc
+++ chrome/browser/io_thread.cc
@@ -1068,6 +1068,7 @@ void IOThread::NetworkSessionConfigurator::ConfigurePriorityDependencies(
   }
 }
 
+// static
 void IOThread::RegisterPrefs(PrefRegistrySimple* registry) {
   registry->RegisterStringPref(prefs::kAuthSchemes,
                                "basic,digest,ntlm,negotiate");
@@ -1631,6 +1632,7 @@ net::QuicVersion IOThread::NetworkSessionConfigurator::ParseQuicVersion(
   return net::QUIC_VERSION_UNSUPPORTED;
 }
 
+// static
 net::URLRequestContext* IOThread::ConstructSystemRequestContext(
     IOThread::Globals* globals,
     const net::HttpNetworkSession::Params& params,
@@ -1679,6 +1681,7 @@ net::URLRequestContext* IOThread::ConstructSystemRequestContext(
   return context;
 }
 
+// static
 net::URLRequestContext* IOThread::ConstructProxyScriptFetcherContext(
     IOThread::Globals* globals,
     const net::HttpNetworkSession::Params& params,
