e113a7212704eedafdd19339d641bb775d005ce2
thestig <thestig@chromium.org>
2016-04-04 17:23:33 -0700

    Print Preview: Handle PDFs with reversed MediaBox / CropBox values.
    When left > right, or top < bottom, the values should be treated as
    though they are swapped.
    
    Based on https://codereview.chromium.org/1734393002 by krb@chromium.org.
    
    BUG=491160
    
    Review URL: https://codereview.chromium.org/1840203003
    
    Cr-Commit-Position: refs/heads/master@{#385058}

--- printing/pdf_transform.cc
+++ printing/pdf_transform.cc
@@ -12,6 +12,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace printing {
 
+namespace {
+
+// When a ClipBox has top < bottom, or right < left, the values should be
+// swapped.
+void SwapClipBoxValuesIfNeeded(ClipBox* clip_box) {
+  if (clip_box->top < clip_box->bottom)
+    std::swap(clip_box->top, clip_box->bottom);
+  if (clip_box->right < clip_box->left)
+    std::swap(clip_box->right, clip_box->left);
+}
+
+}  // namespace
+
 double CalculateScaleFactor(const gfx::Rect& content_rect,
                             double src_width,
                             double src_height,
@@ -43,6 +56,11 @@ void CalculateMediaBoxAndCropBox(bool rotated,
                                  bool has_crop_box,
                                  printing::ClipBox* media_box,
                                  printing::ClipBox* crop_box) {
+  if (has_media_box)
+    SwapClipBoxValuesIfNeeded(media_box);
+  if (has_crop_box)
+    SwapClipBoxValuesIfNeeded(crop_box);
+
   if (!has_media_box && !has_crop_box) {
     SetDefaultClipBox(rotated, crop_box);
     SetDefaultClipBox(rotated, media_box);
