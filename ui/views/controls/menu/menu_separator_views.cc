687fe9a3970371772bedfb2159074ab6b9beec9c
spqchan <spqchan@chromium.org>
2016-04-05 11:12:08 -0700

    MacViews: Fixed Menu Separator appearance
    Changed the menu separators colors on 10.10+ so that they are visible.
    Made the separators thicker on 10.10+
    
    BUG=566003
    
    Review URL: https://codereview.chromium.org/1807823003
    
    Cr-Commit-Position: refs/heads/master@{#385223}

--- ui/views/controls/menu/menu_separator_views.cc
+++ ui/views/controls/menu/menu_separator_views.cc
@@ -11,12 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/native_theme/native_theme.h"
 #include "ui/views/controls/menu/menu_config.h"
 
-namespace {
-
-const int kSeparatorHeight = 1;
-
-}  // namespace
-
 namespace views {
 
 #if !defined(OS_WIN)
@@ -48,9 +42,11 @@ gfx::Size MenuSeparator::GetPreferredSize() const {
 
 gfx::Rect MenuSeparator::GetPaintBounds() {
   int pos = 0;
+  const MenuConfig& menu_config = MenuConfig::instance();
+  int separator_thickness = menu_config.separator_thickness;
   switch (type_) {
     case ui::LOWER_SEPARATOR:
-      pos = height() - kSeparatorHeight;
+      pos = height() - separator_thickness;
       break;
     case ui::SPACING_SEPARATOR:
       return gfx::Rect();
@@ -61,8 +57,8 @@ gfx::Rect MenuSeparator::GetPaintBounds() {
       break;
   }
 
-  gfx::Rect paint_rect(0, pos, width(), kSeparatorHeight);
-  if (MenuConfig::instance().use_outer_border)
+  gfx::Rect paint_rect(0, pos, width(), separator_thickness);
+  if (menu_config.use_outer_border)
     paint_rect.Inset(1, 0);
   return paint_rect;
 }
