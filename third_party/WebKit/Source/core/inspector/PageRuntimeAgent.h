0060edb59db33826bd7944c47bd06686ecc502e1
dgozman <dgozman@chromium.org>
2016-04-06 00:23:56 -0700

    [DevTools] Remove InspectorRuntimeAgent::defaultScriptState.
    Introduced V8ContextInfo::isMainInGroup instead.
    Added test for this functionality.
    
    BUG=580337
    
    Review URL: https://codereview.chromium.org/1859223002
    
    Cr-Commit-Position: refs/heads/master@{#385394}

--- third_party/WebKit/Source/core/inspector/PageRuntimeAgent.h
+++ third_party/WebKit/Source/core/inspector/PageRuntimeAgent.h
@@ -55,8 +55,6 @@ public:
 private:
     PageRuntimeAgent(Client*, V8Debugger*, InspectedFrames*, int contextGroupId);
 
-    ScriptState* defaultScriptState() override;
-
     Member<InspectedFrames> m_inspectedFrames;
 };
 
--- third_party/WebKit/Source/core/inspector/WorkerRuntimeAgent.h
+++ third_party/WebKit/Source/core/inspector/WorkerRuntimeAgent.h
@@ -52,7 +52,7 @@ public:
 
 private:
     WorkerRuntimeAgent(V8Debugger*, WorkerGlobalScope*, InspectorRuntimeAgent::Client*, int contextGroupId);
-    ScriptState* defaultScriptState() override;
+
     Member<WorkerGlobalScope> m_workerGlobalScope;
 };
 
