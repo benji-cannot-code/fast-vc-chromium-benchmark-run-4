848f69d6da997246db5f53b03cc03ec52e45a340
hayato <hayato@chromium.org>
2016-04-06 00:45:29 -0700

    Do not leak a script element in a v1 shadow tree via document.currentScript
    BUG=394332
    
    Review URL: https://codereview.chromium.org/1842263005
    
    Cr-Commit-Position: refs/heads/master@{#385398}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -4651,6 +4651,13 @@ KURL Document::openSearchDescriptionURL()
     return KURL();
 }
 
+HTMLScriptElement* Document::currentScriptForBinding() const
+{
+    if (HTMLScriptElement* script = currentScript())
+        return script->isInV1ShadowTree() ? nullptr : script;
+    return nullptr;
+}
+
 void Document::pushCurrentScript(RawPtr<HTMLScriptElement> newCurrentScript)
 {
     ASSERT(newCurrentScript);
