aca57e1ec24c6adbfdbdc5d0fd968b8a3f2f9d1f
kraynov <kraynov@chromium.org>
2016-04-06 02:53:35 -0700

    Handle zero-size allocations properly in heap profiler
    Now zero-size allocations are ignored for heap profiling.
    Percentage calculation is originally changed to avoid div-by-zero,
    but it also solves integer overflow issue with >32768 Kb allocations
    on platforms with 32-bit size_t.
    
    Review URL: https://codereview.chromium.org/1859143003
    
    Cr-Commit-Position: refs/heads/master@{#385426}

--- base/trace_event/heap_profiler_allocation_register_unittest.cc
+++ base/trace_event/heap_profiler_allocation_register_unittest.cc
@@ -60,17 +60,20 @@ TEST_F(AllocationRegisterTest, InsertRemove) {
   AllocationRegister reg;
   AllocationContext ctx = AllocationContext::Empty();
 
+  // Zero-sized allocations should be discarded.
+  reg.Insert(reinterpret_cast<void*>(1), 0, ctx);
+
   EXPECT_EQ(0u, OrAllAddresses(reg));
 
-  reg.Insert(reinterpret_cast<void*>(1), 0, ctx);
+  reg.Insert(reinterpret_cast<void*>(1), 1, ctx);
 
   EXPECT_EQ(1u, OrAllAddresses(reg));
 
-  reg.Insert(reinterpret_cast<void*>(2), 0, ctx);
+  reg.Insert(reinterpret_cast<void*>(2), 1, ctx);
 
   EXPECT_EQ(3u, OrAllAddresses(reg));
 
-  reg.Insert(reinterpret_cast<void*>(4), 0, ctx);
+  reg.Insert(reinterpret_cast<void*>(4), 1, ctx);
 
   EXPECT_EQ(7u, OrAllAddresses(reg));
 
@@ -91,8 +94,8 @@ TEST_F(AllocationRegisterTest, DoubleFreeIsAllowed) {
   AllocationRegister reg;
   AllocationContext ctx = AllocationContext::Empty();
 
-  reg.Insert(reinterpret_cast<void*>(1), 0, ctx);
-  reg.Insert(reinterpret_cast<void*>(2), 0, ctx);
+  reg.Insert(reinterpret_cast<void*>(1), 1, ctx);
+  reg.Insert(reinterpret_cast<void*>(2), 1, ctx);
   reg.Remove(reinterpret_cast<void*>(1));
   reg.Remove(reinterpret_cast<void*>(1));  // Remove for the second time.
   reg.Remove(reinterpret_cast<void*>(4));  // Remove never inserted address.
@@ -180,7 +183,7 @@ TEST_F(AllocationRegisterTest, InsertRemoveRandomOrder) {
   uint32_t initial_water_mark = GetHighWaterMark(reg);
 
   for (uintptr_t i = 2; i < prime; i++) {
-    size_t size = i % 31;
+    size_t size = i % 31 + 1;
     expected_sum += size;
     reg.Insert(reinterpret_cast<void*>(i), size, ctx);
   }
@@ -190,7 +193,7 @@ TEST_F(AllocationRegisterTest, InsertRemoveRandomOrder) {
 
   // Iterate the numbers 2, 3, ..., prime - 1 in pseudorandom order.
   for (uintptr_t i = generator; i != 1; i = (i * generator) % prime) {
-    size_t size = i % 31;
+    size_t size = i % 31 + 1;
     expected_sum -= size;
     reg.Remove(reinterpret_cast<void*>(i));
     EXPECT_EQ(expected_sum, SumAllSizes(reg));
@@ -201,12 +204,12 @@ TEST_F(AllocationRegisterTest, InsertRemoveRandomOrder) {
   // Insert |prime - 2| entries again. This should use cells from the free list,
   // so the |next_unused_cell_| index should not change.
   for (uintptr_t i = 2; i < prime; i++)
-    reg.Insert(reinterpret_cast<void*>(i), 0, ctx);
+    reg.Insert(reinterpret_cast<void*>(i), 1, ctx);
 
   ASSERT_EQ(prime - 2, GetHighWaterMark(reg) - initial_water_mark);
 
   // Inserting one more entry should use a fresh cell again.
-  reg.Insert(reinterpret_cast<void*>(prime), 0, ctx);
+  reg.Insert(reinterpret_cast<void*>(prime), 1, ctx);
   ASSERT_EQ(prime - 1, GetHighWaterMark(reg) - initial_water_mark);
 }
 
@@ -269,7 +272,7 @@ TEST_F(AllocationRegisterTest, OverflowDeathTest) {
   // minus 1 elements are inserted, because cell 0 is unused, so this should
   // fill up the available cells exactly.
   for (i = 1; i < GetNumCells(reg); i++) {
-    reg.Insert(reinterpret_cast<void*>(i), 0, ctx);
+    reg.Insert(reinterpret_cast<void*>(i), 1, ctx);
   }
 
   // Adding just one extra element might still work because the allocated memory
@@ -278,7 +281,7 @@ TEST_F(AllocationRegisterTest, OverflowDeathTest) {
   const size_t cells_per_page = GetNumCellsPerPage();
 
   ASSERT_DEATH(for (size_t j = 0; j < cells_per_page; j++) {
-    reg.Insert(reinterpret_cast<void*>(i + j), 0, ctx);
+    reg.Insert(reinterpret_cast<void*>(i + j), 1, ctx);
   }, "");
 }
 #endif
