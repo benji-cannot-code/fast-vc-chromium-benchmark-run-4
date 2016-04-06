653879aebd3de7ada53fe84ae94b3f61da1abc58
pilgrim <pilgrim@chromium.org>
2016-04-05 17:25:34 -0700

    [Layout API] Convert ScrollingCoordinatorTest to use new LocalFrame::contentLayoutItem()
    There is only one reference to LocalFrame::contentLayoutObject(),
    in getRootScrollLayer(). I converted it to use contentLayoutItem()
    instead. The method it references (compositor()) is already part of the
    layout API, so no new API methods are required.
    
    There are no functional changes.
    
    BUG=499321
    
    Review URL: https://codereview.chromium.org/1862863002
    
    Cr-Commit-Position: refs/heads/master@{#385348}

--- third_party/WebKit/Source/web/tests/ScrollingCoordinatorTest.cpp
+++ third_party/WebKit/Source/web/tests/ScrollingCoordinatorTest.cpp
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/frame/FrameView.h"
 #include "core/layout/LayoutPart.h"
-#include "core/layout/LayoutView.h"
+#include "core/layout/api/LayoutViewItem.h"
 #include "core/layout/compositing/CompositedLayerMapping.h"
 #include "core/layout/compositing/PaintLayerCompositor.h"
 #include "core/page/Page.h"
@@ -84,7 +84,7 @@ public:
 
     WebLayer* getRootScrollLayer()
     {
-        PaintLayerCompositor* compositor = frame()->contentLayoutObject()->compositor();
+        PaintLayerCompositor* compositor = frame()->contentLayoutItem().compositor();
         DCHECK(compositor);
         DCHECK(compositor->scrollLayer());
 
