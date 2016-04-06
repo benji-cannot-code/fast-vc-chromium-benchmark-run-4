c39b3415efda2d667fd8218b10abd54e31f463ff
michaeln <michaeln@chromium.org>
2016-04-06 13:03:14 -0700

    Some fleshing out the mojo based localstorage implementation.
    Data is not yet stored on disk, but its functional from a pages point of view. The mojo based plumbing between the caching layers in the renderer and and browser process is working.
    
    The browser side now enforces a size constraint.
    
    The observer on the renderer side now longer lived so storage events are not dropped. Events have to be raised even if the local cache is not loaded.
    
    BUG=586194
    
    Review URL: https://codereview.chromium.org/1837883003
    
    Cr-Commit-Position: refs/heads/master@{#385533}

--- content/browser/dom_storage/dom_storage_context_wrapper.h
+++ content/browser/dom_storage/dom_storage_context_wrapper.h
@@ -67,6 +67,7 @@ class CONTENT_EXPORT DOMStorageContextWrapper :
 
   // See mojom::StoragePartitionService interface.
   void OpenLocalStorage(const url::Origin& origin,
+                        mojom::LevelDBObserverPtr observer,
                         mojom::LevelDBWrapperRequest request);
 
  private:
--- content/browser/storage_partition_impl.h
+++ content/browser/storage_partition_impl.h
@@ -66,6 +66,7 @@ class StoragePartitionImpl : public StoragePartition,
   // mojom::StoragePartitionService interface.
   void OpenLocalStorage(
       const url::Origin& origin,
+      mojom::LevelDBObserverPtr observer,
       mojo::InterfaceRequest<mojom::LevelDBWrapper> request) override;
 
   void ClearDataForOrigin(uint32_t remove_mask,
