93c1939f2f2dd2de813bc183f7b3e4c14a12eb34
mkwst <mkwst@chromium.org>
2016-04-06 05:02:43 -0700

    CREDENTIAL: Don't crash on navigation with open account picker.
    https://codereview.chromium.org/1861663002 introduced a security check
    that ensures we're only giving credentials back to a page if it is the
    top-level frame. It was a little too agressive, and caused crashes if
    the page navigated. This patch alters the check to crash iff there's a
    frame which could receive the data.
    
    BUG=600946
    
    Review URL: https://codereview.chromium.org/1861593004
    
    Cr-Commit-Position: refs/heads/master@{#385440}

--- third_party/WebKit/Source/modules/credentialmanager/CredentialsContainer.cpp
+++ third_party/WebKit/Source/modules/credentialmanager/CredentialsContainer.cpp
@@ -56,7 +56,7 @@ public:
     void onSuccess() override
     {
         Frame* frame = toDocument(m_resolver->getScriptState()->getExecutionContext())->frame();
-        SECURITY_CHECK(frame == frame->tree().top());
+        SECURITY_CHECK(!frame || frame == frame->tree().top());
 
         m_resolver->resolve();
     }
@@ -79,10 +79,10 @@ public:
     void onSuccess(WebPassOwnPtr<WebCredential> webCredential) override
     {
         Frame* frame = toDocument(m_resolver->getScriptState()->getExecutionContext())->frame();
-        SECURITY_CHECK(frame == frame->tree().top());
+        SECURITY_CHECK(!frame || frame == frame->tree().top());
 
         OwnPtr<WebCredential> credential = webCredential.release();
-        if (!credential) {
+        if (!credential || !frame) {
             m_resolver->resolve();
             return;
         }
