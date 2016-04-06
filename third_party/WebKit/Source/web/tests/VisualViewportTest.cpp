86854044a796dd8e243b38aaa057c99ce1fb2077
pilgrim <pilgrim@chromium.org>
2016-04-06 06:59:01 -0700

    [Layout API] Convert VisualViewportTest to use new LocalFrame::contentLayoutItem()
    There is only one reference to LocalFrame::contentLayoutObject(),
    in getRootScrollLayer(). I converted it to use contentLayoutItem()
    instead. The method it references (compositor()) is already part of the
    layout API, so no new API methods are required.
    
    There are no functional changes.
    
    BUG=499321
    
    Review URL: https://codereview.chromium.org/1863863002
    
    Cr-Commit-Position: refs/heads/master@{#385455}

--- third_party/WebKit/Source/web/tests/VisualViewportTest.cpp
+++ third_party/WebKit/Source/web/tests/VisualViewportTest.cpp
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLElement.h"
 #include "core/input/EventHandler.h"
 #include "core/layout/LayoutObject.h"
-#include "core/layout/LayoutView.h"
+#include "core/layout/api/LayoutViewItem.h"
 #include "core/layout/compositing/PaintLayerCompositor.h"
 #include "core/page/Page.h"
 #include "platform/PlatformGestureEvent.h"
@@ -153,7 +153,7 @@ public:
 
     WebLayer* getRootScrollLayer()
     {
-        PaintLayerCompositor* compositor = frame()->contentLayoutObject()->compositor();
+        PaintLayerCompositor* compositor = frame()->contentLayoutItem().compositor();
         DCHECK(compositor);
         DCHECK(compositor->scrollLayer());
 
