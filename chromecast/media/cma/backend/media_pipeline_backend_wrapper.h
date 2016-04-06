4653035f95bb9d8c862356a066ceda5fbb11d970
tianyuwang <tianyuwang@google.com>
2016-04-06 14:24:18 -0700

    [chromecast] Pass media task runner to MediaPipelineBackendManager.
    BUG=597450
    
    Review URL: https://codereview.chromium.org/1858803002
    
    Cr-Commit-Position: refs/heads/master@{#385556}

--- chromecast/media/cma/backend/media_pipeline_backend_wrapper.h
+++ chromecast/media/cma/backend/media_pipeline_backend_wrapper.h
@@ -16,18 +16,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/public/media/media_pipeline_backend.h"
 #include "chromecast/public/media/media_pipeline_device_params.h"
 
-namespace base {
-class SingleThreadTaskRunner;
-}  // namespace base
-
 namespace chromecast {
 namespace media {
 
+class MediaPipelineBackendManager;
+
 class MediaPipelineBackendWrapper : public MediaPipelineBackend {
  public:
-  MediaPipelineBackendWrapper(MediaPipelineBackend* backend,
+  MediaPipelineBackendWrapper(scoped_ptr<MediaPipelineBackend> backend,
                               int stream_type,
-                              float stream_type_volume);
+                              float stream_type_volume,
+                              MediaPipelineBackendManager* backend_manager);
   ~MediaPipelineBackendWrapper() override;
 
   // MediaPipelineBackend implementation:
@@ -50,6 +49,7 @@ class MediaPipelineBackendWrapper : public MediaPipelineBackend {
   scoped_ptr<AudioDecoderWrapper> audio_decoder_wrapper_;
   float stream_type_volume_;
   bool is_initialized_;
+  MediaPipelineBackendManager* const backend_manager_;
 
   DISALLOW_COPY_AND_ASSIGN(MediaPipelineBackendWrapper);
 };
