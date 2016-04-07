c9f481150017122ad370cefd251969365e678149
mstensho <mstensho@opera.com>
2016-04-07 14:40:19 -0700

    Make top-layer elements work also when the viewport is paginated.
    When the viewport is paginated (by overflow:-webkit-paged-* specified on HTML
    or BODY), top-layer elements are redirected to a flow thread, along with
    everything else. So we have to go through the children of the flow thread, not
    the children of the layout view, when looking for them.
    
    BUG=594306
    
    Review URL: https://codereview.chromium.org/1850153002
    
    Cr-Commit-Position: refs/heads/master@{#385883}

--- third_party/WebKit/Source/core/paint/PaintLayerStackingNode.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerStackingNode.cpp
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/paint/PaintLayerStackingNode.h"
 
+#include "core/layout/LayoutMultiColumnFlowThread.h"
 #include "core/layout/LayoutView.h"
 #include "core/layout/compositing/PaintLayerCompositor.h"
 #include "core/paint/PaintLayer.h"
@@ -137,8 +138,12 @@ void PaintLayerStackingNode::rebuildZOrderLists()
     // Append layers for top layer elements after normal layer collection, to ensure they are on top regardless of z-indexes.
     // The layoutObjects of top layer elements are children of the view, sorted in top layer stacking order.
     if (layer()->isRootLayer()) {
-        LayoutView* view = layoutObject()->view();
-        for (LayoutObject* child = view->firstChild(); child; child = child->nextSibling()) {
+        LayoutBlockFlow* rootBlock = layoutObject()->view();
+        // If the viewport is paginated, everything (including "top-layer" elements) gets
+        // redirected to the flow thread. So that's where we have to look, in that case.
+        if (LayoutBlockFlow* multiColumnFlowThread = rootBlock->multiColumnFlowThread())
+            rootBlock = multiColumnFlowThread;
+        for (LayoutObject* child = rootBlock->firstChild(); child; child = child->nextSibling()) {
             Element* childElement = (child->node() && child->node()->isElementNode()) ? toElement(child->node()) : 0;
             if (childElement && childElement->isInTopLayer()) {
                 PaintLayer* layer = toLayoutBoxModelObject(child)->layer();
