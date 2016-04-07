7f9a1ba54759aac83d8095cb12fe0b61db8960ac
pilgrim <pilgrim@chromium.org>
2016-04-07 15:34:40 -0700

    [Layout API] Convert PaintLayerCompositor to use new LocalFrame::contentLayoutItem()
    There are several references to LocalFrame::contentLayoutObject(), which
    is deprecated by the new layout API. I converted all of them to use
    contentLayoutItem() instead. The methods referenced are already part of
    the layout API, so no new API methods are required.
    
    There are no functional changes.
    
    BUG=499321
    
    Review URL: https://codereview.chromium.org/1866663003
    
    Cr-Commit-Position: refs/heads/master@{#385905}

--- third_party/WebKit/Source/core/layout/compositing/PaintLayerCompositor.cpp
+++ third_party/WebKit/Source/core/layout/compositing/PaintLayerCompositor.cpp
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/InspectorInstrumentation.h"
 #include "core/layout/LayoutPart.h"
 #include "core/layout/LayoutVideo.h"
-#include "core/layout/LayoutView.h"
+#include "core/layout/api/LayoutViewItem.h"
 #include "core/layout/compositing/CompositedLayerMapping.h"
 #include "core/layout/compositing/CompositingInputsUpdater.h"
 #include "core/layout/compositing/CompositingLayerAssigner.h"
@@ -214,8 +214,8 @@ void PaintLayerCompositor::updateIfNeededRecursiveInternal()
         // It's possible for trusted Pepper plugins to force hit testing in situations where
         // the frame tree is in an inconsistent state, such as in the middle of frame detach.
         // TODO(bbudge) Remove this check when trusted Pepper plugins are gone.
-        if (localFrame->document()->isActive() && localFrame->contentLayoutObject())
-            localFrame->contentLayoutObject()->compositor()->updateIfNeededRecursiveInternal();
+        if (localFrame->document()->isActive() && !localFrame->contentLayoutItem().isNull())
+            localFrame->contentLayoutItem().compositor()->updateIfNeededRecursiveInternal();
     }
 
     TRACE_EVENT0("blink", "PaintLayerCompositor::updateIfNeededRecursive");
@@ -254,9 +254,9 @@ void PaintLayerCompositor::updateIfNeededRecursiveInternal()
         if (!child->isLocalFrame())
             continue;
         LocalFrame* localFrame = toLocalFrame(child);
-        if (localFrame->shouldThrottleRendering() || !localFrame->contentLayoutObject())
+        if (localFrame->shouldThrottleRendering() || localFrame->contentLayoutItem().isNull())
             continue;
-        localFrame->contentLayoutObject()->compositor()->assertNoUnresolvedDirtyBits();
+        localFrame->contentLayoutItem().compositor()->assertNoUnresolvedDirtyBits();
     }
 #endif
 }
