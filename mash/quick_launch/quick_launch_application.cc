51060b26ab528bd4c489e6cd835458c41cea380b
sadrul <sadrul@chromium.org>
2016-04-07 14:05:47 -0700

    quick-launch: Fix continuous attempt to relaunch the app.
    BUG=none
    
    Review URL: https://codereview.chromium.org/1859333003
    
    Cr-Commit-Position: refs/heads/master@{#385847}

--- mash/quick_launch/quick_launch_application.cc
+++ mash/quick_launch/quick_launch_application.cc
@@ -159,5 +159,9 @@ void QuickLaunchApplication::Initialize(mojo::Connector* connector,
   window->Show();
 }
 
+bool QuickLaunchApplication::AcceptConnection(mojo::Connection* connection) {
+  return true;
+}
+
 }  // namespace quick_launch
 }  // namespace mash
