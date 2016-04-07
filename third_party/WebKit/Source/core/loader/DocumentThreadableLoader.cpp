d1becd96afc8c0d191d28c613c02fa438ea05acb
estark <estark@chromium.org>
2016-04-06 22:14:15 -0700

    Use RequestContext to apply CSP in FrameFetchContext
    Instead of a big switch statement on Resource::Type in
    FrameFetchContext::canRequest(), use
    ContentSecurityPolicy::allowRequest(), which decides which directives to
    apply based on the RequestContext.
    
    A nice-ish side effect of this is that DocumentThreadableLoader no
    longer needs to know about (much less be responsible for enforcing)
    CSP. Previously, DocumentThreadableLoader needed to enforce CSP because
    Resource::Type doesn't cover things like EventSource, so
    FrameFetchContext::canRequest() just let them pass through as raw
    resources. Now that FrameFetchContext::canRequest() is based on
    RequestContext, and RequestContext covers things like EventSource, the
    proper CSP directive can be applied, instead of DocumentThreadableLoader
    needing to do special CSP checks.
    
    I say "nice-ish" instead of "nice" because kicking CSP checks out of
    DocumentThreadableLoader introduces a slight complication: EventSource
    and friends want to know about redirects that are blocked, so that they
    can product an error for Javascript to consume. (We have layout tests
    that test for this error.) So I introduced a new set of hooks for
    RawResourceClients to get notified when ResourceFetcher declines to
    follow a redirect. (The previous flow for EventSource and friends was
    that ResourceFetcher would agree to follow the redirect, but
    DocumentThreadableLoader would block it and notify the
    RawResourceClient. Now that ResourceFetcher is deciding based on
    RequestContexts, ResourceFetcher will want to block the redirect.)
    
    BUG=474412
    
    Review URL: https://codereview.chromium.org/1866433002
    
    Cr-Commit-Position: refs/heads/master@{#385669}

--- third_party/WebKit/Source/core/loader/DocumentThreadableLoader.cpp
+++ third_party/WebKit/Source/core/loader/DocumentThreadableLoader.cpp
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/fetch/ResourceFetcher.h"
 #include "core/frame/FrameConsole.h"
 #include "core/frame/LocalFrame.h"
-#include "core/frame/csp/ContentSecurityPolicy.h"
 #include "core/inspector/InspectorInstrumentation.h"
 #include "core/inspector/InspectorTraceEvents.h"
 #include "core/loader/CrossOriginPreflightResultCache.h"
@@ -442,7 +441,7 @@ void DocumentThreadableLoader::redirectReceived(Resource* resource, ResourceRequ
         return;
     }
 
-    if (m_redirectMode == WebURLRequest::FetchRedirectModeError || !isAllowedByContentSecurityPolicy(request.url(), ContentSecurityPolicy::DidRedirect)) {
+    if (m_redirectMode == WebURLRequest::FetchRedirectModeError) {
         ThreadableLoaderClient* client = m_client;
         clear();
         client->didFailRedirectCheck();
@@ -528,6 +527,15 @@ void DocumentThreadableLoader::redirectReceived(Resource* resource, ResourceRequ
     }
 
     request = ResourceRequest();
+}
+
+void DocumentThreadableLoader::redirectBlocked()
+{
+    // Tells the client that a redirect was received but not followed (for an unknown reason).
+    ThreadableLoaderClient* client = m_client;
+    clear();
+    client->didFailRedirectCheck();
+    // |this| may be dead here
 }
 
 void DocumentThreadableLoader::dataSent(Resource* resource, unsigned long long bytesSent, unsigned long long totalBytesToBeSent)
@@ -882,7 +890,7 @@ void DocumentThreadableLoader::loadRequest(const ResourceRequest& request, Resou
     // FIXME: A synchronous request does not tell us whether a redirect happened or not, so we guess by comparing the
     // request and response URLs. This isn't a perfect test though, since a server can serve a redirect to the same URL that was
     // requested. Also comparing the request and response URLs as strings will fail if the requestURL still has its credentials.
-    if (requestURL != response.url() && (!isAllowedByContentSecurityPolicy(response.url(), ContentSecurityPolicy::DidRedirect) || !isAllowedRedirect(response.url()))) {
+    if (requestURL != response.url() && !isAllowedRedirect(response.url())) {
         m_client->didFailRedirectCheck();
         return;
     }
@@ -915,14 +923,6 @@ bool DocumentThreadableLoader::isAllowedRedirect(const KURL& url) const
         return true;
 
     return m_sameOriginRequest && getSecurityOrigin()->canRequest(url);
-}
-
-bool DocumentThreadableLoader::isAllowedByContentSecurityPolicy(const KURL& url, ContentSecurityPolicy::RedirectStatus redirectStatus) const
-{
-    if (m_options.contentSecurityPolicyEnforcement != EnforceContentSecurityPolicy)
-        return true;
-
-    return document().contentSecurityPolicy()->allowRequest(m_requestContext, url, redirectStatus);
 }
 
 StoredCredentials DocumentThreadableLoader::effectiveAllowCredentials() const
--- third_party/WebKit/Source/core/loader/DocumentThreadableLoader.h
+++ third_party/WebKit/Source/core/loader/DocumentThreadableLoader.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CoreExport.h"
 #include "core/fetch/RawResource.h"
 #include "core/fetch/ResourceOwner.h"
-#include "core/frame/csp/ContentSecurityPolicy.h"
 #include "core/loader/ThreadableLoader.h"
 #include "platform/Timer.h"
 #include "platform/heap/Handle.h"
@@ -77,13 +76,6 @@ class CORE_EXPORT DocumentThreadableLoader final : public ThreadableLoader, priv
             LoadAsynchronously
         };
 
-        enum EnforceContentSecurityPolicyDirective {
-            EnforceWorkerDirective,
-            EnforceConnectSrcDirective,
-            EnforceManifestSrcDirective,
-            EnforceMediaSrcDirective,
-        };
-
         DocumentThreadableLoader(Document&, ThreadableLoaderClient*, BlockingBehavior, const ThreadableLoaderOptions&, const ResourceLoaderOptions&);
 
         void clear();
@@ -103,6 +95,7 @@ class CORE_EXPORT DocumentThreadableLoader final : public ThreadableLoader, priv
         void setSerializedCachedMetadata(Resource*, const char*, size_t) override;
         void dataReceived(Resource*, const char* data, size_t dataLength) override;
         void redirectReceived(Resource*, ResourceRequest&, const ResourceResponse&) override;
+        void redirectBlocked() override;
         void dataDownloaded(Resource*, int) override;
         void didReceiveResourceTiming(Resource*, const ResourceTimingInfo&) override;
 
@@ -152,7 +145,6 @@ class CORE_EXPORT DocumentThreadableLoader final : public ThreadableLoader, priv
 
         void loadRequest(const ResourceRequest&, ResourceLoaderOptions);
         bool isAllowedRedirect(const KURL&) const;
-        bool isAllowedByContentSecurityPolicy(const KURL&, ContentSecurityPolicy::RedirectStatus) const;
         // Returns DoNotAllowStoredCredentials
         // if m_forceDoNotAllowStoredCredentials is set. Otherwise, just
         // returns allowCredentials value of m_resourceLoaderOptions.
@@ -209,7 +201,7 @@ class CORE_EXPORT DocumentThreadableLoader final : public ThreadableLoader, priv
 
         const bool m_async;
 
-        // Holds the original request context (used for sanity checks and Content Security Policy enforcement).
+        // Holds the original request context (used for sanity checks).
         WebURLRequest::RequestContext m_requestContext;
 
         // Holds the original request for fallback in case the Service Worker
