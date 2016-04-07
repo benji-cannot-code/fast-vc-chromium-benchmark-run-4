2753cc30f2be6876d7885d80794269c02ba57c5d
nona <nona@chromium.org>
2016-04-07 05:16:48 -0700

    Use our own grapheme boundary handling
    This CL replaces the grapheme boundary detection
    from ICU's implementation to our own.
    
    By replacing with own implementation, we can support
    latest Unicode spec without waiting ICU update.
    For example, grapheme boundary for the recent complex
    emoji sequence is now supported.
    
    There is another benefit of new implementation. The
    interface of new implementation does not require the
    full continuous text buffer. This is good if the target
    text crosses the Nodes.
    
    BUG=594923
    
    Review URL: https://codereview.chromium.org/1853033002
    
    Cr-Commit-Position: refs/heads/master@{#385720}

--- third_party/WebKit/Source/core/editing/EditingUtilities.cpp
+++ third_party/WebKit/Source/core/editing/EditingUtilities.cpp
@@ -43,6 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/iterators/TextIterator.h"
 #include "core/editing/serializers/HTMLInterchange.h"
 #include "core/editing/state_machines/BackspaceStateMachine.h"
+#include "core/editing/state_machines/BackwardGraphemeBoundaryStateMachine.h"
+#include "core/editing/state_machines/ForwardGraphemeBoundaryStateMachine.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/UseCounter.h"
 #include "core/html/HTMLBRElement.h"
@@ -556,7 +558,7 @@ int findNextBoundaryOffset(const String& str, int current)
         if (state != TextSegmentationMachineState::NeedMoreCodeUnit)
             break;
     }
-    if (state == TextSegmentationMachineState::NeedMoreCodeUnit)
+    if (current == 0 || state == TextSegmentationMachineState::NeedMoreCodeUnit)
         state = machine.tellEndOfPrecedingText();
     if (state == TextSegmentationMachineState::Finished)
         return current + machine.finalizeAndGetBoundaryOffset();
@@ -572,16 +574,15 @@ int findNextBoundaryOffset(const String& str, int current)
 
 int previousGraphemeBoundaryOf(const Node* node, int current)
 {
-    if (!node->isTextNode())
+    // TODO(yosin): Need to support grapheme crossing |Node| boundary.
+    DCHECK_GE(current, 0);
+    if (current <= 1 || !node->isTextNode())
         return current - 1;
     const String& text = toText(node)->data();
-    if (text.is8Bit())
-        return current - 1; // TODO(nona): Good to support CR x LF.
-    TextBreakIterator* iterator = cursorMovementIterator(text.characters16(), text.length());
-    if (!iterator)
+    // TODO(yosin): Replace with DCHECK for out-of-range request.
+    if (static_cast<unsigned>(current) > text.length())
         return current - 1;
-    const int result = iterator->preceding(current);
-    return result == TextBreakDone ? current - 1 : result;
+    return findNextBoundaryOffset<BackwardGraphemeBoundaryStateMachine>(text, current);
 }
 
 static int previousBackwardDeletionOffsetOf(const Node* node, int current)
@@ -599,16 +600,15 @@ static int previousBackwardDeletionOffsetOf(const Node* node, int current)
 
 int nextGraphemeBoundaryOf(const Node* node, int current)
 {
+    // TODO(yosin): Need to support grapheme crossing |Node| boundary.
     if (!node->isTextNode())
         return current + 1;
     const String& text = toText(node)->data();
-    if (text.is8Bit())
-        return current + 1; // TODO(nona): Good to support CR x LF.
-    TextBreakIterator* iterator = cursorMovementIterator(text.characters16(), text.length());
-    if (!iterator)
+    const int length = text.length();
+    DCHECK_LE(current, length);
+    if (current >= length - 1)
         return current + 1;
-    const int result = iterator->following(current);
-    return result == TextBreakDone ? current + 1 : result;
+    return findNextBoundaryOffset<ForwardGraphemeBoundaryStateMachine>(text, current);
 }
 
 template <typename Strategy>
