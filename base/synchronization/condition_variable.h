9c5d9bfcc3d1f07c2f33fa11ebd853e7b48e95a4
robliao <robliao@chromium.org>
2016-04-06 21:54:42 -0700

    Remove XP Condition Variable Affordances
    XP and Vista are no longer supported in Chromium.
    
    BUG=579196
    
    Review URL: https://codereview.chromium.org/1860353002
    
    Cr-Commit-Position: refs/heads/master@{#385665}

--- base/synchronization/condition_variable.h
+++ base/synchronization/condition_variable.h
@@ -76,9 +76,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <pthread.h>
 #endif
 
+#if defined(OS_WIN)
+#include <windows.h>
+#endif
+
 namespace base {
 
-class ConditionVarImpl;
 class TimeDelta;
 
 class BASE_EXPORT ConditionVariable {
@@ -101,14 +104,15 @@ class BASE_EXPORT ConditionVariable {
  private:
 
 #if defined(OS_WIN)
-  ConditionVarImpl* impl_;
+  CONDITION_VARIABLE cv_;
+  CRITICAL_SECTION* const crit_sec_;
 #elif defined(OS_POSIX)
   pthread_cond_t condition_;
   pthread_mutex_t* user_mutex_;
-#if DCHECK_IS_ON()
-  base::Lock* user_lock_;     // Needed to adjust shadow lock state on wait.
 #endif
 
+#if DCHECK_IS_ON() && (defined(OS_WIN) || defined(OS_POSIX))
+  base::Lock* const user_lock_;  // Needed to adjust shadow lock state on wait.
 #endif
 
   DISALLOW_COPY_AND_ASSIGN(ConditionVariable);
