b803e8cfc9c0fcf974ccdefacf7e8814eab0ccc6
lushnikov <lushnikov@chromium.org>
2016-04-07 14:28:44 -0700

    DevTools: [Console] fix console stick-to-bottom behavior in case of multiline messages
    The patch fixes the stick-to-bottom behavior of console in case of multiline
    messages.
    
    The patch does two things:
    1. ViewportControl starts to cache provider's itemCount in invalidate call.
       This ensures that the regular scrolling of viewport is not affected
       by provider's item changes unless an invalidate method was called.
    
    2. Stick to bottom only in case of non-user-generated gestures. This is
       done via transitioning to the event.isTrusted flag.
    
    BUG=594339
    R=dgozman, pfeldman
    
    Review URL: https://codereview.chromium.org/1863313002
    
    Cr-Commit-Position: refs/heads/master@{#385872}

--- third_party/WebKit/Source/devtools/front_end/ui/ViewportControl.js
+++ third_party/WebKit/Source/devtools/front_end/ui/ViewportControl.js
@@ -62,6 +62,7 @@ WebInspector.ViewportControl = function(provider)
     this._headSelection = null;
     this._stickToBottom = false;
     this._scrolledToBottom = true;
+    this._itemCount = 0;
 }
 
 /**
@@ -196,7 +197,8 @@ WebInspector.ViewportControl.prototype = {
     {
         delete this._cumulativeHeights;
         delete this._cachedProviderElements;
-        this.refresh();
+        this._itemCount = this._provider.itemCount();
+        this._innerRefresh(false);
     },
 
     /**
@@ -206,7 +208,7 @@ WebInspector.ViewportControl.prototype = {
     _providerElement: function(index)
     {
         if (!this._cachedProviderElements)
-            this._cachedProviderElements = new Array(this._provider.itemCount());
+            this._cachedProviderElements = new Array(this._itemCount);
         var element = this._cachedProviderElements[index];
         if (!element) {
             element = this._provider.itemElement(index);
@@ -219,14 +221,13 @@ WebInspector.ViewportControl.prototype = {
     {
         if (this._cumulativeHeights)
             return;
-        var itemCount = this._provider.itemCount();
-        if (!itemCount)
+        if (!this._itemCount)
             return;
         var firstVisibleIndex = this._firstVisibleIndex;
         var lastVisibleIndex = this._lastVisibleIndex;
         var height = 0;
-        this._cumulativeHeights = new Int32Array(itemCount);
-        for (var i = 0; i < itemCount; ++i) {
+        this._cumulativeHeights = new Int32Array(this._itemCount);
+        for (var i = 0; i < this._itemCount; ++i) {
             if (firstVisibleIndex <= i && i <= lastVisibleIndex)
                 height += this._renderedItems[i - firstVisibleIndex].element().offsetHeight;
             else
@@ -311,7 +312,7 @@ WebInspector.ViewportControl.prototype = {
 
         if (!this._anchorSelection || !this._headSelection) {
             this._anchorSelection = this._createSelectionModel(0, this.element, 0);
-            this._headSelection = this._createSelectionModel(this._provider.itemCount() - 1, this.element, this.element.children.length);
+            this._headSelection = this._createSelectionModel(this._itemCount - 1, this.element, this.element.children.length);
             this._selectionIsBackward = false;
         }
 
@@ -376,10 +377,18 @@ WebInspector.ViewportControl.prototype = {
 
     refresh: function()
     {
+        this._innerRefresh(false);
+    },
+
+    /**
+     * @param {boolean} isUserGesture
+     */
+    _innerRefresh: function(isUserGesture)
+    {
         if (!this._visibleHeight())
             return;  // Do nothing for invisible controls.
 
-        if (!this._provider.itemCount()) {
+        if (!this._itemCount) {
             for (var i = 0; i < this._renderedItems.length; ++i)
                 this._renderedItems[i].willHide();
             this._renderedItems = [];
@@ -405,20 +414,19 @@ WebInspector.ViewportControl.prototype = {
                 delete this._cumulativeHeights;
         }
         this._rebuildCumulativeHeightsIfNeeded();
-        var itemCount = this._cumulativeHeights.length;
         var oldFirstVisibleIndex = this._firstVisibleIndex;
         var oldLastVisibleIndex = this._lastVisibleIndex;
 
-        var shouldStickToBottom = isInvalidating && this._stickToBottom && this._scrolledToBottom;
+        var shouldStickToBottom = !isUserGesture && this._stickToBottom && this._scrolledToBottom;
 
         if (shouldStickToBottom) {
-            this._lastVisibleIndex = itemCount - 1;
-            this._firstVisibleIndex = Math.max(itemCount - Math.ceil(visibleHeight / this._provider.minimumRowHeight()), 0);
+            this._lastVisibleIndex = this._itemCount - 1;
+            this._firstVisibleIndex = Math.max(this._itemCount - Math.ceil(visibleHeight / this._provider.minimumRowHeight()), 0);
         } else {
             this._firstVisibleIndex = Math.max(Array.prototype.lowerBound.call(this._cumulativeHeights, visibleFrom + 1), 0);
             // Proactively render more rows in case some of them will be collapsed without triggering refresh. @see crbug.com/390169
             this._lastVisibleIndex = this._firstVisibleIndex + Math.ceil(visibleHeight / this._provider.minimumRowHeight()) - 1;
-            this._lastVisibleIndex = Math.min(this._lastVisibleIndex, itemCount - 1);
+            this._lastVisibleIndex = Math.min(this._lastVisibleIndex, this._itemCount - 1);
         }
         var topGapHeight = this._cumulativeHeights[this._firstVisibleIndex - 1] || 0;
         var bottomGapHeight = this._cumulativeHeights[this._cumulativeHeights.length - 1] - this._cumulativeHeights[this._lastVisibleIndex];
@@ -444,7 +452,7 @@ WebInspector.ViewportControl.prototype = {
         if (shouldRestoreSelection)
             this._restoreSelection(selection);
         if (shouldStickToBottom)
-            this.element.scrollTop = this.element.scrollHeight;
+            this.element.scrollTop = 10000000;
     },
 
     /**
@@ -561,7 +569,7 @@ WebInspector.ViewportControl.prototype = {
      */
     _onScroll: function(event)
     {
-        this.refresh();
+        this._innerRefresh(event.isTrusted);
     },
 
     /**
@@ -615,7 +623,7 @@ WebInspector.ViewportControl.prototype = {
     {
         this._rebuildCumulativeHeightsIfNeeded();
         this.element.scrollTop = index > 0 ? this._cumulativeHeights[index - 1] : 0;
-        this.refresh();
+        this._innerRefresh(false);
     },
 
     /**
@@ -625,7 +633,7 @@ WebInspector.ViewportControl.prototype = {
     {
         this._rebuildCumulativeHeightsIfNeeded();
         this.element.scrollTop = this._cumulativeHeights[index] - this._visibleHeight();
-        this.refresh();
+        this._innerRefresh(false);
     },
 
     /**
