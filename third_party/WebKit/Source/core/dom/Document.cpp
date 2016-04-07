b4dae2ba2484754631f0f950187f8753292c7992
deokjin81.kim <deokjin81.kim@samsung.com>
2016-04-07 05:51:53 -0700

    Rename Document::hasPendingStyleRecalc to hasPendingVisualUpdate.
    This CL is needed to better reflect the method's behavior.
    
    BUG=585653
    
    Review URL: https://codereview.chromium.org/1866303002
    
    Cr-Commit-Position: refs/heads/master@{#385731}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -1560,7 +1560,7 @@ bool Document::shouldScheduleLayoutTreeUpdate() const
 
 void Document::scheduleLayoutTreeUpdate()
 {
-    ASSERT(!hasPendingStyleRecalc());
+    ASSERT(!hasPendingVisualUpdate());
     ASSERT(shouldScheduleLayoutTreeUpdate());
     ASSERT(needsLayoutTreeUpdate());
 
@@ -1576,7 +1576,7 @@ void Document::scheduleLayoutTreeUpdate()
 
 bool Document::hasPendingForcedStyleRecalc() const
 {
-    return hasPendingStyleRecalc() && !inStyleRecalc() && getStyleChangeType() >= SubtreeStyleChange;
+    return hasPendingVisualUpdate() && !inStyleRecalc() && getStyleChangeType() >= SubtreeStyleChange;
 }
 
 void Document::updateStyleInvalidationIfNeeded()
