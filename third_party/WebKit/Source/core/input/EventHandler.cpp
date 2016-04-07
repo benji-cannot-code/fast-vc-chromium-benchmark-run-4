8815e693973255058de777a9f694e33ffca907bd
bokan <bokan@chromium.org>
2016-04-06 20:16:48 -0700

    Remove "reportWheelOverscroll" from Blink settings.
    This flag complicates Blink event handling needlessly. It's only needed on Mac
    where we use it to disable the elastic rubber banding effect on overscroll.
    
    This patch removes the setting and always reports overscroll for wheel from
    Blink. In the renderer content layer, we check if the ElasticOverscroll system
    setting is enabled on Mac and block the overscroll if not (there's no touch
    overscroll to worry about on Mac). Otherwise just allow the overscroll deltas to
    be sent to the browser since they have no effect on other platforms.
    
    BUG=505516
    
    Review URL: https://codereview.chromium.org/1861343002
    
    Cr-Commit-Position: refs/heads/master@{#385642}

--- third_party/WebKit/Source/core/input/EventHandler.cpp
+++ third_party/WebKit/Source/core/input/EventHandler.cpp
@@ -1907,7 +1907,7 @@ void EventHandler::defaultWheelEventHandler(Node* startNode, WheelEvent* wheelEv
     if (consumed)
         wheelEvent->setDefaultHandled();
 
-    if (m_frame->isMainFrame() && m_frame->settings() && m_frame->settings()->reportWheelOverscroll())
+    if (m_frame->isMainFrame())
         handleOverscroll(result);
 }
 
