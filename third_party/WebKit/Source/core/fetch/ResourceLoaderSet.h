ca3dcf885de6eb8e72173174ac1134fd3d9fef6c
japhet <japhet@chromium.org>
2016-04-07 14:13:20 -0700

    Speculative fix for crbug.com/583572
    Iterate over ResourceFetcher::m_documentResources to find images that need a priority update, rather than over ResourceFetcher::m_loaders. The former appears to be safe and reliable, based on the lack of crash reports.
    
    BUG=583572
    
    Review URL: https://codereview.chromium.org/1865963003
    
    Cr-Commit-Position: refs/heads/master@{#385852}

--- third_party/WebKit/Source/core/fetch/ResourceLoaderSet.h
+++ third_party/WebKit/Source/core/fetch/ResourceLoaderSet.h
@@ -51,7 +51,6 @@ public:
     void cancelAll();
     void setAllDefersLoading(bool);
     int size() const { return m_set.size(); }
-    const SetType& hashSet() const { return m_set; }
 
 private:
     SetType m_set;
