24dea026516a2492f5e102ff1df980fdc4603e43
mbarbella <mbarbella@chromium.org>
2016-04-06 23:09:08 -0700

    Remove remoting messages from the IPC fuzzer.
    R=inferno@chromium.org
    BUG=601230
    
    Review URL: https://codereview.chromium.org/1865163002
    
    Cr-Commit-Position: refs/heads/master@{#385676}

--- tools/ipc_fuzzer/message_lib/all_messages.h
+++ tools/ipc_fuzzer/message_lib/all_messages.h
@@ -27,4 +27,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/visitedlink/common/visitedlink_message_generator.h"
 #include "content/common/all_messages.h"
 #include "extensions/common/extension_message_generator.h"
-#include "remoting/host/chromoting_messages.h"
