5488a4a839cedd1ae23368c73fdcf27166fd7a3b
dgozman <dgozman@chromium.org>
2016-04-06 22:33:12 -0700

    [DevTools] Retrieve agents through session, do not store them in debugger.
    BUG=none
    
    Review URL: https://codereview.chromium.org/1859373003
    
    Cr-Commit-Position: refs/heads/master@{#385671}

--- third_party/WebKit/Source/platform/v8_inspector/V8RuntimeAgentImpl.cpp
+++ third_party/WebKit/Source/platform/v8_inspector/V8RuntimeAgentImpl.cpp
@@ -65,12 +65,10 @@ V8RuntimeAgentImpl::V8RuntimeAgentImpl(V8InspectorSessionImpl* session)
     , m_debugger(session->debugger())
     , m_enabled(false)
 {
-    m_debugger->addRuntimeAgent(m_session->contextGroupId(), this);
 }
 
 V8RuntimeAgentImpl::~V8RuntimeAgentImpl()
 {
-    m_debugger->removeRuntimeAgent(m_session->contextGroupId());
 }
 
 void V8RuntimeAgentImpl::evaluate(
@@ -456,6 +454,7 @@ void V8RuntimeAgentImpl::disable(ErrorString* errorString)
     if (!m_enabled)
         return;
     m_enabled = false;
+    m_session->discardInjectedScripts();
     reset();
 }
 
@@ -519,8 +518,6 @@ void V8RuntimeAgentImpl::addInspectedObject(PassOwnPtr<Inspectable> inspectable)
 void V8RuntimeAgentImpl::reset()
 {
     m_compiledScripts.clear();
-    // TODO(dgozman): reverse this call.
-    m_session->resetInjectedScripts();
     if (m_enabled) {
         if (const V8DebuggerImpl::ContextByIdMap* contexts = m_debugger->contextGroup(m_session->contextGroupId())) {
             for (auto& idContext : *contexts)
