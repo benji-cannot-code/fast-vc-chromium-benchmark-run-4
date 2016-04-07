5da6fa82237bd18dd14eb0c9bba3d6c91c219c21
phweiss <phweiss@chromium.org>
2016-04-07 04:35:50 -0700

    Fix syntax of JSON string, include cameraDisabled
    Instead of passing ArcApplicationPolicy as entry in the
    json dictionary, it is now parsed and used as the top level
    dictionary, with other policies added to it. The
    VideoCaptureAllowed policy is mapped to cameraDisabled.
    
    Bug: b/25985338
    
    Review URL: https://codereview.chromium.org/1864523002
    
    Cr-Commit-Position: refs/heads/master@{#385712}

--- chrome/browser/chromeos/arc/arc_policy_bridge.h
+++ chrome/browser/chromeos/arc/arc_policy_bridge.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_ARC_ARC_POLICY_BRIDGE_H_
 #define CHROME_BROWSER_CHROMEOS_ARC_ARC_POLICY_BRIDGE_H_
 
-#include <string>
-
 #include "base/macros.h"
 #include "components/arc/arc_bridge_service.h"
 #include "components/arc/arc_service.h"
@@ -44,7 +42,6 @@ class ArcPolicyBridge : public ArcService,
 
  private:
   void InitializePolicyService();
-  std::string GetFilteredJSONPolicies(const policy::PolicyMap& policy_map);
 
   mojo::Binding<PolicyHost> binding_;
   policy::PolicyService* policy_service_ = nullptr;
