d61ad7f3672fb091ab692c741264c5aab009060d
danakj <danakj@chromium.org>
2016-04-07 14:19:27 -0700

    Remove unused features for in-process GL contexts.
    These features are not used and need not be supported:
    - Global share group
    - Context lock
    
    Also cleans up GLInProcessContext to not require tracking lost
    context state, so that we can have users of GLES2Implementation
    get lost context notifications directly, going past the
    GLInProcessContext.
    
    R=piman@chomium.org, sievers@chromium.org
    BUG=584497
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1864373002
    
    Cr-Commit-Position: refs/heads/master@{#385859}

--- android_webview/browser/aw_render_thread_context_provider.h
+++ android_webview/browser/aw_render_thread_context_provider.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
-#include "base/synchronization/lock.h"
 #include "base/threading/thread_checker.h"
 #include "cc/output/context_provider.h"
 #include "gpu/command_buffer/service/in_process_command_buffer.h"
@@ -63,8 +62,6 @@ class AwRenderThreadContextProvider : public cc::ContextProvider {
   cc::ContextProvider::Capabilities capabilities_;
 
   LostContextCallback lost_context_callback_;
-
-  base::Lock context_lock_;
 
   DISALLOW_COPY_AND_ASSIGN(AwRenderThreadContextProvider);
 };
