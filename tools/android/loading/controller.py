d1ed50e3ceedaeb3f8d609be02c899739bd811f4
gabadie <gabadie@chromium.org>
2016-04-08 02:55:56 -0700

    tools/android/loading: Add WPR logging support in the ChromeController
    BUG=582080
    
    Review URL: https://codereview.chromium.org/1865803005
    
    Cr-Commit-Position: refs/heads/master@{#386030}

--- tools/android/loading/controller.py
+++ tools/android/loading/controller.py
@@ -153,7 +153,8 @@ class ChromeControllerBase(object):
   @contextlib.contextmanager
   def OpenWprHost(self, wpr_archive_path, record=False,
                   network_condition_name=None,
-                  disable_script_injection=False):
+                  disable_script_injection=False,
+                  out_log_path=None):
     """Opens a Web Page Replay host context.
 
     Args:
@@ -163,6 +164,7 @@ class ChromeControllerBase(object):
           emulation.NETWORK_CONDITIONS.
       disable_script_injection: Disable JavaScript file injections that is
         fighting against resources name entropy.
+      out_log_path: Path of the WPR host's log.
     """
     raise NotImplementedError
 
@@ -262,13 +264,15 @@ class RemoteChromeController(ChromeControllerBase):
   @contextlib.contextmanager
   def OpenWprHost(self, wpr_archive_path, record=False,
                   network_condition_name=None,
-                  disable_script_injection=False):
+                  disable_script_injection=False,
+                  out_log_path=None):
     """Starts a WPR host, overrides Chrome flags until contextmanager exit."""
     assert not self._chrome_wpr_specific_args, 'WPR is already running.'
     with device_setup.RemoteWprHost(self._device, wpr_archive_path,
         record=record,
         network_condition_name=network_condition_name,
-        disable_script_injection=disable_script_injection) as additional_flags:
+        disable_script_injection=disable_script_injection,
+        out_log_path=out_log_path) as additional_flags:
       self._chrome_wpr_specific_args = additional_flags
       yield
     self._chrome_wpr_specific_args = []
@@ -369,14 +373,15 @@ class LocalChromeController(ChromeControllerBase):
   @contextlib.contextmanager
   def OpenWprHost(self, wpr_archive_path, record=False,
                   network_condition_name=None,
-                  disable_script_injection=False):
+                  disable_script_injection=False,
+                  out_log_path=None):
     """Override for WPR context."""
     assert not self._chrome_wpr_specific_args, 'WPR is already running.'
     with device_setup.LocalWprHost(wpr_archive_path,
         record=record,
         network_condition_name=network_condition_name,
-        disable_script_injection=disable_script_injection
-        ) as additional_flags:
+        disable_script_injection=disable_script_injection,
+        out_log_path=out_log_path) as additional_flags:
       self._chrome_wpr_specific_args = additional_flags
       yield
     self._chrome_wpr_specific_args = []
