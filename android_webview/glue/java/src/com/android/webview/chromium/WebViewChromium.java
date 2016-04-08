b1d4f7b6168e07a7a9da2695dbc53477c1fa3109
boliu <boliu@chromium.org>
2016-04-08 10:23:47 -0700

    aw: Expose setDrawGLFunctionDetachedCallback to AwContents
    This is needed for generic Views to correctly support FullScreenView.
    This CL basically reverts r386036.
    
    BUG=597167
    
    Review URL: https://codereview.chromium.org/1870163002
    
    Cr-Commit-Position: refs/heads/master@{#386110}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromium.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromium.java
@@ -1890,9 +1890,6 @@ class WebViewChromium implements WebViewProvider, WebViewProvider.ScrollDelegate
             return;
         }
 
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M && !"N".equals(Build.VERSION.CODENAME)) {
-            mAwContents.onDrawGLFunctorDetached();
-        }
         mAwContents.onDetachedFromWindow();
     }
 
@@ -2248,6 +2245,11 @@ class WebViewChromium implements WebViewProvider, WebViewProvider.ScrollDelegate
 
     // AwContents.NativeGLDelegate implementation --------------------------------------
     private class WebViewNativeGLDelegate implements AwContents.NativeGLDelegate {
+        @Override
+        public boolean setDrawGLFunctionDetachedCallback(View view, Runnable callback) {
+            return false; // Not supported yet.
+        }
+
         @Override
         public boolean requestDrawGL(Canvas canvas, boolean waitForCompletion, View containerView) {
             if (mGLfunctor == null) {
