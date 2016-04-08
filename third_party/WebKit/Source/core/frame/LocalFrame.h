a4b6893a171c988b4143f78f3f8cdd22ada599ce
dglazkov <dglazkov@chromium.org>
2016-04-08 13:54:56 -0700

    Remove LocalFrame::updateSecurityOrigin.
    It's an unnecessary indirection that obscures the actor who actually
    does the work.
    
    R=esprehn
    BUG=
    
    Review URL: https://codereview.chromium.org/1872843002
    
    Cr-Commit-Position: refs/heads/master@{#386190}

--- third_party/WebKit/Source/core/frame/LocalFrame.h
+++ third_party/WebKit/Source/core/frame/LocalFrame.h
@@ -187,8 +187,6 @@ public:
     WebFrameScheduler* frameScheduler();
     void scheduleVisualUpdateUnlessThrottled();
 
-    void updateSecurityOrigin(SecurityOrigin*);
-
     bool isNavigationAllowed() const { return m_navigationDisableCount == 0; }
 
     ServiceRegistry* serviceRegistry() { return m_serviceRegistry; }
