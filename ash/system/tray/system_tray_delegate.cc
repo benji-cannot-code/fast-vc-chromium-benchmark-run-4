4e2098f1195cccfa7bcec2c8ed88f2094ef6b874
vmpstr <vmpstr@chromium.org>
2016-04-07 19:54:31 -0700

    ash: Add out-of-line copy ctors for complex classes.
    This patch adds out of line copy constructors for classes that our
    clang-plugin considers heavy. This is an effort to enable copy
    constructor checks by default.
    
    BUG=436357
    R=sky@chromium.org, dcheng@chromium.org, thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/1870843002
    
    Cr-Commit-Position: refs/heads/master@{#385948}

--- ash/system/tray/system_tray_delegate.cc
+++ ash/system/tray/system_tray_delegate.cc
@@ -27,6 +27,8 @@ BluetoothDeviceInfo::~BluetoothDeviceInfo() {
 IMEInfo::IMEInfo() : selected(false), third_party(false) {
 }
 
+IMEInfo::IMEInfo(const IMEInfo& other) = default;
+
 IMEInfo::~IMEInfo() {
 }
 
--- ash/system/tray/system_tray_delegate.h
+++ ash/system/tray/system_tray_delegate.h
@@ -73,6 +73,7 @@ using IMEPropertyInfoList = std::vector<IMEPropertyInfo>;
 
 struct ASH_EXPORT IMEInfo {
   IMEInfo();
+  IMEInfo(const IMEInfo& other);
   ~IMEInfo();
 
   bool selected;
