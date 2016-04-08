c8da0ed4c3864acbe22fe8825f2a63c4201eb0d5
rmcilroy <rmcilroy@chromium.org>
2016-04-08 10:59:10 -0700

    Add Ignition to about:flags.
    We are at the point where we would like to start experimenting with the Ignition
    interpreter on real web pages. Adding this to about:flags makes this easier.
    
    Design Doc: https://docs.google.com/document/d/11T2CRex9hXxoJwbYqVQ32yIPMh0uouUZLdyrtmMoL44
    
    BUG=594228, v8:4280, v8:4868
    
    Review URL: https://codereview.chromium.org/1820933002
    
    Cr-Commit-Position: refs/heads/master@{#386125}

--- gin/v8_initializer.cc
+++ gin/v8_initializer.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/debug/alias.h"
+#include "base/feature_list.h"
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/files/memory_mapped_file.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/platform_thread.h"
 #include "base/time/time.h"
 #include "crypto/sha2.h"
+#include "gin/public/gin_features.h"
 
 #if defined(V8_USE_EXTERNAL_STARTUP_DATA)
 #if defined(OS_ANDROID)
@@ -420,6 +422,11 @@ void V8Initializer::Initialize(IsolateHolder::ScriptMode mode,
   if (IsolateHolder::kStableAndExperimentalV8Extras == v8_extras_mode) {
     static const char flag[] = "--experimental_extras";
     v8::V8::SetFlagsFromString(flag, sizeof(flag) - 1);
+  }
+
+  if (base::FeatureList::IsEnabled(features::kV8Ignition)) {
+    std::string flag("--ignition");
+    v8::V8::SetFlagsFromString(flag.c_str(), static_cast<int>(flag.size()));
   }
 
 #if defined(V8_USE_EXTERNAL_STARTUP_DATA)
