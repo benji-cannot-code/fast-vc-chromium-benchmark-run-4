3c91f6c45390ffcfba5c0c24cbbd403c64dc6eb2
dpapad <dpapad@chromium.org>
2016-04-08 11:40:32 -0700

    MD Settings: Certificate manager, respect export/edit restrictions.
    BUG=601543
    
    Review URL: https://codereview.chromium.org/1867093003
    
    Cr-Commit-Position: refs/heads/master@{#386135}

--- chrome/browser/resources/settings/certificate_manager_page/certificates_browser_proxy.js
+++ chrome/browser/resources/settings/certificate_manager_page/certificates_browser_proxy.js
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *   policy: boolean,
  *   readonly: boolean,
  *   untrusted: boolean,
- *   urlLocked: boolean
  * }}
  * @see chrome/browser/ui/webui/settings/certificates_handler.cc
  */
