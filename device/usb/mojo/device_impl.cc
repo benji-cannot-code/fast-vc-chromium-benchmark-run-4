76267987ccc16c75e4eebb1c8285f2e1822e9125
reillyg <reillyg@chromium.org>
2016-04-08 11:52:46 -0700

    Implement tab indicator for WebUSB.
    When a frame within a tab has a USB device open a tab indicator is
    displayed to alert the user to the ongoing connection.
    
    BUG=592105
    
    Review URL: https://codereview.chromium.org/1857033003
    
    Cr-Commit-Position: refs/heads/master@{#386151}

--- device/usb/mojo/device_impl.cc
+++ device/usb/mojo/device_impl.cc
@@ -147,8 +147,11 @@ DeviceImpl::~DeviceImpl() {
 }
 
 void DeviceImpl::CloseHandle() {
-  if (device_handle_)
+  if (device_handle_) {
     device_handle_->Close();
+    if (permission_provider_)
+      permission_provider_->DecrementConnectionCount();
+  }
   device_handle_ = nullptr;
 }
 
@@ -195,6 +198,8 @@ bool DeviceImpl::HasControlTransferPermission(
 void DeviceImpl::OnOpen(const OpenCallback& callback,
                         scoped_refptr<UsbDeviceHandle> handle) {
   device_handle_ = handle;
+  if (device_handle_ && permission_provider_)
+    permission_provider_->IncrementConnectionCount();
   callback.Run(handle ? OpenDeviceError::OK : OpenDeviceError::ACCESS_DENIED);
 }
 
