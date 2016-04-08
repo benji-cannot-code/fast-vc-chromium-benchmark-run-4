3c3d2e20d0ecdf920812481f998c2671ac80a8a7
jessicag <jessicag@chromium.org>
2016-04-08 13:16:08 -0700

    Add new system url request context getter to the blimp engine. This new class and function provides context for system type calls (e.g. metrics uploads) that is independent of user context.
    BUG=601155
    
    Review URL: https://codereview.chromium.org/1864163003
    
    Cr-Commit-Position: refs/heads/master@{#386185}

--- blimp/engine/common/blimp_browser_context.h
+++ blimp/engine/common/blimp_browser_context.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/macros.h"
+#include "blimp/engine/app/blimp_system_url_request_context_getter.h"
 #include "blimp/engine/app/blimp_url_request_context_getter.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/content_browser_client.h"
@@ -61,12 +62,17 @@ class BlimpBrowserContext : public content::BrowserContext {
       content::ProtocolHandlerMap* protocol_handlers,
       content::URLRequestInterceptorScopedVector request_interceptors) override;
 
+  // Provides a URLRequestContextGetter for system requests (e.g. metrics
+  // uploads).
+  net::URLRequestContextGetter* GetSystemRequestContextGetter();
+
  private:
   // Performs initialization of the BlimpBrowserContext while IO is still
   // allowed on the current thread.
   void InitWhileIOAllowed();
 
   std::unique_ptr<BlimpResourceContext> resource_context_;
+  scoped_refptr<BlimpSystemURLRequestContextGetter> system_context_getter_;
   bool ignore_certificate_errors_;
   std::unique_ptr<content::PermissionManager> permission_manager_;
   bool off_the_record_;
