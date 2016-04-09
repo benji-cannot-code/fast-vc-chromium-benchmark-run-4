9bd682222a006c268bac849a9559b7ca5d8f0269
leon.han <leon.han@intel.com>
2016-04-08 23:46:30 -0700

    Reland: Switch components/password_manager code from IPC messages to Mojo.
    Original CL was found breaking android gn build after landed.. #strange
    Fix BUILD.gn and reland.
    
    The original CL:
    https://crrev.com/d20fb918841354a75546fa38b5307aaba117598b
    
    Original CL description follows:
    
    Replace credential_manager_messages.h IPC to Mojo service.
    
    BUG=582391
    
    Review URL: https://codereview.chromium.org/1866643002
    
    Cr-Commit-Position: refs/heads/master@{#386290}

--- chrome/browser/password_manager/chrome_password_manager_client.cc
+++ chrome/browser/password_manager/chrome_password_manager_client.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/browser_sync/browser/profile_sync_service.h"
 #include "components/password_manager/content/browser/content_password_manager_driver.h"
 #include "components/password_manager/content/browser/password_manager_internals_service_factory.h"
-#include "components/password_manager/content/common/credential_manager_messages.h"
 #include "components/password_manager/core/browser/browser_save_password_progress_logger.h"
 #include "components/password_manager/core/browser/log_manager.h"
 #include "components/password_manager/core/browser/log_receiver.h"
@@ -142,7 +141,7 @@ ChromePasswordManagerClient::ChromePasswordManagerClient(
       profile_(Profile::FromBrowserContext(web_contents->GetBrowserContext())),
       password_manager_(this),
       driver_factory_(nullptr),
-      credential_manager_dispatcher_(web_contents, this),
+      credential_manager_impl_(web_contents, this),
       observer_(nullptr),
       credentials_filter_(this,
                           base::Bind(&GetSyncService, profile_),
@@ -611,4 +610,18 @@ ChromePasswordManagerClient::GetStoreResultFilter() const {
 const password_manager::LogManager* ChromePasswordManagerClient::GetLogManager()
     const {
   return log_manager_.get();
+}
+
+// static
+void ChromePasswordManagerClient::BindCredentialManager(
+    content::RenderFrameHost* render_frame_host,
+    password_manager::mojom::CredentialManagerRequest request) {
+  content::WebContents* web_contents =
+      content::WebContents::FromRenderFrameHost(render_frame_host);
+  DCHECK(web_contents);
+
+  ChromePasswordManagerClient* instance =
+      ChromePasswordManagerClient::FromWebContents(web_contents);
+  DCHECK(instance);
+  instance->credential_manager_impl_.BindRequest(std::move(request));
 }
