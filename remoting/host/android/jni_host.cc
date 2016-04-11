d5e986a4c2474a6a623fbfdac828a484b79751d6
lambroslambrou <lambroslambrou@chromium.org>
2016-04-11 12:33:40 -0700

    [remoting android] Add native library and JNI glue for host.
    BUG=602355
    
    Review URL: https://codereview.chromium.org/1867433002
    
    Cr-Commit-Position: refs/heads/master@{#386439}

---
+++ remoting/host/android/jni_host.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/android/jni_host.h"
+
+#include "jni/Host_jni.h"
+
+namespace remoting {
+
+JniHost::JniHost() {}
+
+JniHost::~JniHost() {}
+
+// static
+bool JniHost::RegisterJni(JNIEnv* env) {
+  return RegisterNativesImpl(env);
+}
+
+void JniHost::Destroy(JNIEnv* env, const JavaParamRef<jobject>& caller) {
+  delete this;
+}
+
+static jlong Init(JNIEnv* env, const JavaParamRef<jobject>& caller) {
+  return reinterpret_cast<intptr_t>(new JniHost());
+}
+
+}  // namespace remoting
---
+++ remoting/host/android/jni_host.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_HOST_ANDROID_JNI_HOST_H_
+#define REMOTING_HOST_ANDROID_JNI_HOST_H_
+
+#include <jni.h>
+
+#include "base/android/scoped_java_ref.h"
+#include "base/macros.h"
+
+namespace remoting {
+
+class JniHost {
+ public:
+  JniHost();
+  ~JniHost();
+
+  // Register C++ methods exposed to Java using JNI.
+  static bool RegisterJni(JNIEnv* env);
+
+  void Destroy(JNIEnv* env, const base::android::JavaParamRef<jobject>& caller);
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(JniHost);
+};
+
+}  // namespace remoting
+
+#endif  // REMOTING_HOST_ANDROID_JNI_HOST_H_
