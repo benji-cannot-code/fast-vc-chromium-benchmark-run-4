a515c355288a99c17365f4e0bb7e358a5cbbf43e
shrike <shrike@chromium.org>
2016-04-11 09:53:57 -0700

    [Mac][Material Design] Change toolbar buttons to 24x24 from 28x28.
    The Material Design spec called for 28x28 toolbar buttons that touched
    each other. sgabriel@ revised the spec to specify 24x24 toolbar buttons
    with 4 pts of horizontal padding between them. This cl makes that
    change, and incidentally fixes a problem where it was no longer
    possible to resize the location bar by dragging it from its right edge.
    
    R=avi@chromium.org, sky@chromium.org
    BUG=601967
    
    Review URL: https://codereview.chromium.org/1870333002
    
    Cr-Commit-Position: refs/heads/master@{#386404}

--- chrome/browser/ui/cocoa/toolbar/toolbar_button_cocoa.mm
+++ chrome/browser/ui/cocoa/toolbar/toolbar_button_cocoa.mm
@@ -18,8 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// The bounds of toolbar buttons in Material Design.
-const NSRect kMDButtonBounds = NSMakeRect(0, 0, 28, 28);
+// Toolbar buttons are 24x24 in Material Design.
+const NSRect kMDButtonBounds = NSMakeRect(0, 0, 24, 24);
 
 // The size of a toolbar button icon in Material Design. A toolbar button image
 // consists of a border and background, with a centered icon.
@@ -80,9 +80,10 @@ const NSSize kMDButtonIconSize = NSMakeSize(16, 16);
 
 + (void)drawImage:(ToolbarButtonImageRep*)imageRep {
   // Create the path used for the background fill.
-  NSRect destRect = NSInsetRect(kMDButtonBounds, 2, 2);
   NSBezierPath* roundedRectPath =
-      [NSBezierPath bezierPathWithRoundedRect:destRect xRadius:2 yRadius:2];
+      [NSBezierPath bezierPathWithRoundedRect:kMDButtonBounds
+                                      xRadius:2
+                                      yRadius:2];
 
   // Determine the fill color.
   NSColor* fillColor = nil;
