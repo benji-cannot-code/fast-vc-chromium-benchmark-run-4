acbc660e6fa6a2c61f4310b18ea33c4b8b5cc509
malaykeshav <malaykeshav@chromium.org>
2016-04-11 14:15:53 -0700

    Display device specific up to date message on chromebook.
    BUG=585277
    COMPONENT=ChromeOS, WebUI->help
    
    Review URL: https://codereview.chromium.org/1861813003
    
    Cr-Commit-Position: refs/heads/master@{#386475}

--- chrome/browser/ui/webui/help/help_handler.cc
+++ chrome/browser/ui/webui/help/help_handler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "ash/system/chromeos/devicetype_utils.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/command_line.h"
@@ -237,7 +238,6 @@ void HelpHandler::GetLocalizedValues(base::DictionaryValue* localized_strings) {
 #endif
     {"productName", IDS_PRODUCT_NAME},
     {"updateCheckStarted", IDS_UPGRADE_CHECK_STARTED},
-    {"upToDate", IDS_UPGRADE_UP_TO_DATE},
     {"updating", IDS_UPGRADE_UPDATING},
 #if defined(OS_CHROMEOS) || defined(OS_WIN)
     {"updateDisabledByPolicy", IDS_UPGRADE_DISABLED_BY_POLICY},
@@ -301,6 +301,14 @@ void HelpHandler::GetLocalizedValues(base::DictionaryValue* localized_strings) {
     localized_strings->SetString(resources[i].name,
                                  l10n_util::GetStringUTF16(resources[i].ids));
   }
+
+#if defined(OS_CHROMEOS)
+  localized_strings->SetString("upToDate", ash::SubstituteChromeOSDeviceType(
+      IDS_UPGRADE_UP_TO_DATE));
+#else
+  localized_strings->SetString("upToDate", l10n_util::GetStringUTF16(
+      IDS_UPGRADE_UP_TO_DATE));
+#endif
 
   localized_strings->SetString("updateObsoleteSystem",
                                ObsoleteSystem::LocalizedObsoleteString());
