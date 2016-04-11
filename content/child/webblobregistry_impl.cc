7183d3f434b5e98e56fb65407f7d9ea83969467e
dcheng <dcheng@chromium.org>
2016-04-11 00:43:25 -0700

    Change WebBlobRegistry::createBuilder to return a std::unique_ptr.
    Enforce ownership transfer with langauge semantics rather than comments.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1878513002
    
    Cr-Commit-Position: refs/heads/master@{#386345}

--- content/child/webblobregistry_impl.cc
+++ content/child/webblobregistry_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/guid.h"
 #include "base/location.h"
+#include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/shared_memory.h"
 #include "base/message_loop/message_loop.h"
@@ -50,11 +51,11 @@ WebBlobRegistryImpl::WebBlobRegistryImpl(
 WebBlobRegistryImpl::~WebBlobRegistryImpl() {
 }
 
-blink::WebBlobRegistry::Builder* WebBlobRegistryImpl::createBuilder(
+std::unique_ptr<WebBlobRegistry::Builder> WebBlobRegistryImpl::createBuilder(
     const blink::WebString& uuid,
     const blink::WebString& content_type) {
-  return new BuilderImpl(uuid, content_type, sender_.get(), io_runner_,
-                         main_runner_);
+  return base::WrapUnique(new BuilderImpl(uuid, content_type, sender_.get(),
+                                          io_runner_, main_runner_));
 }
 
 void WebBlobRegistryImpl::registerBlobData(const blink::WebString& uuid,
--- third_party/WebKit/public/platform/WebBlobRegistry.h
+++ third_party/WebKit/public/platform/WebBlobRegistry.h
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebCommon.h"
 #include "WebThreadSafeData.h"
 
+#include <memory>
+
 namespace blink {
 
 class WebBlobData;
@@ -76,9 +78,8 @@ public:
     // TODO(dmurph): Deprecate and migrate to createBuilder
     virtual void registerBlobData(const WebString& uuid, const WebBlobData&) { }
 
-    // Caller takes ownership of the Builder. The blob is finalized (and sent to
-    // the browser) on calling build() on the Builder object.
-    virtual Builder* createBuilder(const WebString& uuid, const WebString& contentType) = 0;
+    // The blob is finalized (and sent to the browser) on calling build() on the Builder object.
+    virtual std::unique_ptr<Builder> createBuilder(const WebString& uuid, const WebString& contentType) = 0;
 
     virtual void addBlobDataRef(const WebString& uuid) { }
     virtual void removeBlobDataRef(const WebString& uuid) { }
