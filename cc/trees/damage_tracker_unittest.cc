f0b5b4c9d8aebc97ec677fd8ee65ee62fc182fbb
weiliangc <weiliangc@chromium.org>
2016-04-11 15:04:06 -0700

    cc: Clean up helper function from layer tree host common
    After removal of CDP, helper functions can be de-templated and cleaned
    up.
    
    R=ajuma, enne
    BUG=581832
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1869983003
    
    Cr-Commit-Position: refs/heads/master@{#386486}

--- cc/trees/damage_tracker_unittest.cc
+++ cc/trees/damage_tracker_unittest.cc
@@ -70,7 +70,7 @@ void EmulateDrawingOneFrame(LayerImpl* root) {
     RenderSurfaceImpl* target_surface =
         render_surface_layer_list[index]->render_surface();
     target_surface->damage_tracker()->UpdateDamageTrackingState(
-        target_surface->layer_list(), target_surface->OwningLayerId(),
+        target_surface->layer_list(), target_surface,
         target_surface->SurfacePropertyChangedOnlyFromDescendant(),
         target_surface->content_rect(),
         render_surface_layer_list[index]->mask_layer(),
@@ -1479,12 +1479,7 @@ TEST_F(DamageTrackerTest, VerifyDamageForEmptyLayerList) {
 
   LayerImplList empty_list;
   target_surface->damage_tracker()->UpdateDamageTrackingState(
-      empty_list,
-      target_surface->OwningLayerId(),
-      false,
-      gfx::Rect(),
-      NULL,
-      FilterOperations());
+      empty_list, target_surface, false, gfx::Rect(), NULL, FilterOperations());
 
   gfx::Rect damage_rect =
       target_surface->damage_tracker()->current_damage_rect();
