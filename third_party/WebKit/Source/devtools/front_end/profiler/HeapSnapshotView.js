f2d2bca139b68c3932c304b94669e1259b2c3f14
samli <samli@chromium.org>
2016-04-10 22:56:19 -0700

    Devtools: Simplify actions and add debugger commands to command menu
    Removes unused multi-state support in actions and moves titles to be
    declarative so action buttons can update themselves and allow them to be
    propogated to command menu.
    
    This adds several debugger specific commands like enabling async stacks,
    breakpoints and pausing script execution.
    
    BUG=589353
    
    Review URL: https://codereview.chromium.org/1841943002
    
    Cr-Commit-Position: refs/heads/master@{#386340}

--- third_party/WebKit/Source/devtools/front_end/profiler/HeapSnapshotView.js
+++ third_party/WebKit/Source/devtools/front_end/profiler/HeapSnapshotView.js
@@ -1062,7 +1062,7 @@ WebInspector.HeapSnapshotProfileType.prototype = {
 
     get buttonTooltip()
     {
-        return WebInspector.UIString("Take heap snapshot.");
+        return WebInspector.UIString("Take heap snapshot");
     },
 
     /**
@@ -1280,7 +1280,7 @@ WebInspector.TrackingHeapSnapshotProfileType.prototype = {
 
     get buttonTooltip()
     {
-        return this._recording ? WebInspector.UIString("Stop recording heap profile.") : WebInspector.UIString("Start recording heap profile.");
+        return this._recording ? WebInspector.UIString("Stop recording heap profile") : WebInspector.UIString("Start recording heap profile");
     },
 
     /**
--- third_party/WebKit/Source/devtools/front_end/ui/Toolbar.js
+++ third_party/WebKit/Source/devtools/front_end/ui/Toolbar.js
@@ -486,25 +486,18 @@ WebInspector.ToolbarToggle.prototype = {
  */
 WebInspector.Toolbar.createActionButton = function(action, toggledOptions, untoggledOptions)
 {
-    var button = new WebInspector.ToolbarButton(action.title(), action.icon());
+    var button = new WebInspector.ToolbarToggle(action.title(), action.icon());
     button.addEventListener("click", action.execute, action);
     action.addEventListener(WebInspector.Action.Events.Enabled, enabledChanged);
-    action.addEventListener(WebInspector.Action.Events.StateChanged, stateChanged);
-    action.addEventListener(WebInspector.Action.Events.TitleChanged, titleChanged);
+    action.addEventListener(WebInspector.Action.Events.Toggled, toggled);
     /** @type {?WebInspector.LongClickController} */
     var longClickController = null;
     /** @type {?Array<!WebInspector.ToolbarButton>} */
     var longClickButtons = null;
     /** @type {?Element} */
     var longClickGlyph = null;
-    titleChanged();
-    stateChanged();
+    toggled();
     return button;
-
-    function titleChanged()
-    {
-        WebInspector.Tooltip.install(button.element, action.title(), action.id());
-    }
 
     /**
      * @param {!WebInspector.Event} event
@@ -514,17 +507,16 @@ WebInspector.Toolbar.createActionButton = function(action, toggledOptions, untog
         button.setEnabled(/** @type {boolean} */ (event.data));
     }
 
-    function stateChanged()
+    function toggled()
     {
-        button.setState(action.state());
+        button.setToggled(action.toggled());
+        if (action.title())
+            WebInspector.Tooltip.install(button.element, action.title(), action.id());
         updateOptions();
     }
 
     function updateOptions()
     {
-        if (action.statesCount() !== 2)
-            return;
-
         var buttons = action.toggled() ? (toggledOptions || null) : (untoggledOptions || null);
 
         if (buttons && buttons.length) {
@@ -547,7 +539,7 @@ WebInspector.Toolbar.createActionButton = function(action, toggledOptions, untog
     function showOptions()
     {
         var buttons = longClickButtons.slice();
-        var mainButtonClone = new WebInspector.ToolbarButton(action.title(), action.icon());
+        var mainButtonClone = new WebInspector.ToolbarToggle(action.title(), action.icon());
         mainButtonClone.addEventListener("click", clicked);
 
         /**
@@ -558,7 +550,7 @@ WebInspector.Toolbar.createActionButton = function(action, toggledOptions, untog
             button._clicked(/** @type {!Event} */ (event.data));
         }
 
-        mainButtonClone.setState(action.state());
+        mainButtonClone.setToggled(action.toggled());
         buttons.push(mainButtonClone);
 
         var document = button.element.ownerDocument;
