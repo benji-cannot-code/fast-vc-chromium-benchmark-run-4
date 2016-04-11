7c18b764a9e786f2f4df97286d70020be0968822
isherman <isherman@chromium.org>
2016-04-10 23:15:01 -0700

    [Eraser] Remove the unowned histogram WebCore.PreloadDelayMs
    This histogram has remained unowned for several years, which means that likely
    nobody is using it.  So, let's prune it and save some resources.
    
    BUG=none
    TEST=none
    R=simonjam@chromium.org
    
    Review URL: https://codereview.chromium.org/1855393009
    
    Cr-Commit-Position: refs/heads/master@{#386341}

--- third_party/WebKit/Source/core/html/parser/HTMLResourcePreloader.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLResourcePreloader.cpp
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/fetch/FetchInitiatorInfo.h"
 #include "core/fetch/ResourceFetcher.h"
 #include "core/loader/DocumentLoader.h"
-#include "platform/Histogram.h"
 #include "public/platform/Platform.h"
 
 namespace blink {
@@ -77,9 +76,6 @@ void HTMLResourcePreloader::preload(PassOwnPtr<PreloadRequest> preload, const Ne
     if (preload->resourceType() == Resource::Script || preload->resourceType() == Resource::CSSStyleSheet || preload->resourceType() == Resource::ImportResource)
         request.setCharset(preload->charset().isEmpty() ? m_document->characterSet().getString() : preload->charset());
     request.setForPreload(true);
-    int duration = static_cast<int>(1000 * (monotonicallyIncreasingTime() - preload->discoveryTime()));
-    DEFINE_STATIC_LOCAL(CustomCountHistogram, preloadDelayHistogram, ("WebCore.PreloadDelayMs", 0, 2000, 20));
-    preloadDelayHistogram.count(duration);
     m_document->loader()->startPreload(preload->resourceType(), request);
 }
 
--- third_party/WebKit/Source/core/html/parser/HTMLResourcePreloader.h
+++ third_party/WebKit/Source/core/html/parser/HTMLResourcePreloader.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/parser/PreloadRequest.h"
 #include "core/html/parser/ResourcePreloader.h"
 #include "core/loader/NetworkHintsInterface.h"
-#include "wtf/CurrentTime.h"
 #include "wtf/text/TextPosition.h"
 
 namespace blink {
