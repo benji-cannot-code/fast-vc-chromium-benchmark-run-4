e97250ff542e6a8987f7ed5788b00b09e0949f8e
sigbjornf <sigbjornf@opera.com>
2016-04-11 09:23:46 -0700

    Abandon Prerender upon finalizing PrerenderHandle.
    Forcefully sever the connection to the embedder upon the PrerenderHandle
    becoming unreachable and unused. Otherwise we risk Prerender leaks
    and renderer shutdown crashes when the embedder tries to access Blink
    after it has already been shut down.
    
    R=haraken
    BUG=602227
    
    Review URL: https://codereview.chromium.org/1872383002
    
    Cr-Commit-Position: refs/heads/master@{#386401}

--- third_party/WebKit/Source/core/loader/PrerenderHandle.cpp
+++ third_party/WebKit/Source/core/loader/PrerenderHandle.cpp
@@ -67,8 +67,10 @@ PrerenderHandle::PrerenderHandle(Document& document, Prerender* prerender)
 
 PrerenderHandle::~PrerenderHandle()
 {
-    if (m_prerender)
+    if (m_prerender) {
+        m_prerender->abandon();
         detach();
+    }
 }
 
 void PrerenderHandle::cancel()
