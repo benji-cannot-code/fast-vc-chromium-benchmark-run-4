28f31f53ba6db65964f00e1c009a7c2036fbb4bc
zhongyi <zhongyi@chromium.org>
2016-04-11 15:19:53 -0700

    Remove unnecessary test cxtor. Minor correction to the landing CL.
    BUG=
    
    Review URL: https://codereview.chromium.org/1881643002
    
    Cr-Commit-Position: refs/heads/master@{#386495}

--- net/quic/quic_header_list_test.cc
+++ net/quic/quic_header_list_test.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
-class QuicHeaderListTest : public ::testing::Test {};
-
 // This test verifies that QuicHeaderList accumulates header pairs in order.
 TEST(QuicHeaderListTest, OnHeader) {
   QuicHeaderList headers;
