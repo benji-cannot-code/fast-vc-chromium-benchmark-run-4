8d362aa9d2833feca7aa544828ea5fa910815280
apacible <apacible@chromium.org>
2016-04-11 19:54:39 -0700

    [Media Router WebUI] Move localized strings into i18n_ property.
    Some clean up of the Media Router element JS files. All future strings should be added to |i18n_|.
    
    On a Mac Pro using a release build, the UI load time is:
    - before change: 1973.43ms (average 20 runs)
    - after change: 1897.82ms (average 20 runs)
    
    There is a 3.8% decrease in initial load time observed.
    
    BUG=549289
    
    Review URL: https://codereview.chromium.org/1871683003
    
    Cr-Commit-Position: refs/heads/master@{#386561}

--- chrome/browser/resources/media_router/elements/route_details/route_details.js
+++ chrome/browser/resources/media_router/elements/route_details/route_details.js
@@ -19,6 +19,27 @@ Polymer({
     },
 
     /**
+     * The localized strings used by |this|.
+     * @private {!Object}
+     */
+    i18n_: {
+      readOnly: true,
+      type: Object,
+      value: function() {
+        var strings = {};
+        [
+         'startCastingButtonText',
+         'stopCastingButtonText',
+         'dropDownButtonTitle',
+        ]
+        .forEach(function(s) {
+          strings[s] = loadTimeData.getString(s);
+        });
+        return strings;
+      },
+    },
+
+    /**
      * The route to show.
      * @type {?media_router.Route}
      */
@@ -26,26 +47,6 @@ Polymer({
       type: Object,
       value: null,
       observer: 'maybeLoadCustomController_',
-    },
-
-    /**
-     * The text for the start casting button.
-     * @private {string}
-     */
-    startCastingButtonText_: {
-      type: String,
-      readOnly: true,
-      value: loadTimeData.getString('startCastingButton'),
-    },
-
-    /**
-     * The text for the stop casting button.
-     * @private {string}
-     */
-    stopCastingButtonText_: {
-      type: String,
-      readOnly: true,
-      value: function() { return loadTimeData.getString('stopCastingButton'); },
     },
 
     /**
