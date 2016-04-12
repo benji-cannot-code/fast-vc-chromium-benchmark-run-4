6c8eca69e6d32a9f195c6900e225f190e0fb7b0a
bmcquade <bmcquade@chromium.org>
2016-04-12 12:41:23 -0700

    Add about:flags support for doc.write script blocking.
    This patch introduces an additional blink setting which allows us to enable
    blocking only on slow (2G) connections, or on all connections.
    
    This is consistent with the 2G web font intervention, which has a feature
    for enabling on 2G, and an additional feature to force the intervention on
    in all connection types.
    
    We then add support for enabling the setting that enables blocking on all
    connection types from about:flags.
    
    This change also removes about:flags support from the now discontinued low
    priority iframe experiment, since it's nontrivial to add support for
    multiple blink settings in about:flags.
    
    BUG=602137
    
    Review URL: https://codereview.chromium.org/1868413002
    
    Cr-Commit-Position: refs/heads/master@{#386769}

--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -940,7 +940,7 @@ public:
 
     void initDNSPrefetch();
 
-    bool isInDocumentWrite() { return m_writeRecursionDepth > 0; }
+    bool isInDocumentWrite() const { return m_writeRecursionDepth > 0; }
 
     TextAutosizer* textAutosizer();
 
