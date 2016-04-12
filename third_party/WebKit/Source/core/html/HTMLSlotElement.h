286ee3ccb90f6f01db9a00837eb390236e0f367a
hayato <hayato@chromium.org>
2016-04-12 00:13:22 -0700

    Simplify slot assignments
    Handle the default slot in the same way as other slots that have the name attribute.
    
    No behavior change.
    
    BUG=531990
    
    Review URL: https://codereview.chromium.org/1872303002
    
    Cr-Commit-Position: refs/heads/master@{#386604}

--- third_party/WebKit/Source/core/html/HTMLSlotElement.h
+++ third_party/WebKit/Source/core/html/HTMLSlotElement.h
@@ -70,6 +70,7 @@ public:
     void attributeChanged(const QualifiedName&, const AtomicString& oldValue, const AtomicString& newValue, AttributeModificationReason = ModifiedDirectly) final;
 
     short tabIndex() const override;
+    AtomicString name() const;
 
     DECLARE_VIRTUAL_TRACE();
 
