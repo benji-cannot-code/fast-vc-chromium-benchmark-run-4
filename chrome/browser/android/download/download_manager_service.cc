f19f2e7fb7b7fb7cec74f1cb502ac6817b72229c
qinmin <qinmin@chromium.org>
2016-04-12 10:37:47 -0700

    Fix a crash when calling PauseDownloadInternal()
    BUG=601378
    
    Review URL: https://codereview.chromium.org/1877423002
    
    Cr-Commit-Position: refs/heads/master@{#386734}

--- chrome/browser/android/download/download_manager_service.cc
+++ chrome/browser/android/download/download_manager_service.cc
@@ -155,9 +155,10 @@ void DownloadManagerService::PauseDownloadInternal(
   if (!manager)
     return;
   content::DownloadItem* item = manager->GetDownloadByGuid(download_guid);
-  if (item)
+  if (item) {
     item->Pause();
-  item->RemoveObserver(content::DownloadControllerAndroid::Get());
+    item->RemoveObserver(content::DownloadControllerAndroid::Get());
+  }
 }
 
 void DownloadManagerService::EnqueueDownloadAction(
