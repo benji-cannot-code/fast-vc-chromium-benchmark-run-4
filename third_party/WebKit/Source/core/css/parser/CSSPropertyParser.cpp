34ba8216520156562abc5c8c8139f1c0085aef13
rob.buis <rob.buis@samsung.com>
2016-04-12 06:30:51 -0700

    Move grid-auto-flow into CSSPropertyParser
    Move the grid-auto-flow longhand from LegacyCSSPropertyParser into
    CSSPropertyParser. Make the grid shorthand parsing code use this
    new code path (this means it does not use CSSParserValue anymore).
    
    BUG=499780
    
    Review URL: https://codereview.chromium.org/1787513004
    
    Cr-Commit-Position: refs/heads/master@{#386668}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -2924,6 +2924,23 @@ static CSSValue* consumeBackgroundSize(CSSPropertyID unresolvedProperty, CSSPars
     return CSSValuePair::create(horizontal, vertical, CSSValuePair::KeepIdenticalValues);
 }
 
+CSSValueList* consumeGridAutoFlow(CSSParserTokenRange& range)
+{
+    CSSPrimitiveValue* rowOrColumnValue = consumeIdent<CSSValueRow, CSSValueColumn>(range);
+    CSSPrimitiveValue* denseAlgorithm = consumeIdent<CSSValueDense>(range);
+    if (!rowOrColumnValue) {
+        rowOrColumnValue = consumeIdent<CSSValueRow, CSSValueColumn>(range);
+        if (!rowOrColumnValue && !denseAlgorithm)
+            return nullptr;
+    }
+    CSSValueList* parsedValues = CSSValueList::createSpaceSeparated();
+    if (rowOrColumnValue)
+        parsedValues->append(rowOrColumnValue);
+    if (denseAlgorithm)
+        parsedValues->append(denseAlgorithm);
+    return parsedValues;
+}
+
 static CSSValue* consumeBackgroundComponent(CSSPropertyID unresolvedProperty, CSSParserTokenRange& range, const CSSParserContext& context)
 {
     switch (unresolvedProperty) {
@@ -3102,7 +3119,7 @@ static CSSPrimitiveValue* consumeGridBreadth(CSSParserTokenRange& range, CSSPars
 }
 
 // TODO(rob.buis): This needs a bool parameter so we can disallow <auto-track-list> for the grid shorthand.
-static CSSValue* consumeGridTrackSize(CSSParserTokenRange& range, CSSParserMode cssParserMode, TrackSizeRestriction restriction = AllowAll)
+CSSValue* consumeGridTrackSize(CSSParserTokenRange& range, CSSParserMode cssParserMode, TrackSizeRestriction restriction)
 {
     const CSSParserToken& token = range.peek();
     if (restriction == AllowAll && identMatches<CSSValueAuto>(token.id()))
@@ -3637,16 +3654,10 @@ CSSValue* CSSPropertyParser::parseSingleValue(CSSPropertyID unresolvedProperty)
     case CSSPropertyGridTemplateAreas:
         ASSERT(RuntimeEnabledFeatures::cssGridLayoutEnabled());
         return consumeGridTemplateAreas(m_range);
+    case CSSPropertyGridAutoFlow:
+        ASSERT(RuntimeEnabledFeatures::cssGridLayoutEnabled());
+        return consumeGridAutoFlow(m_range);
     default:
-        CSSParserValueList valueList(m_range);
-        if (valueList.size()) {
-            m_valueList = &valueList;
-            if (CSSValue* result = legacyParseValue(unresolvedProperty)) {
-                while (!m_range.atEnd())
-                    m_range.consume();
-                return result;
-            }
-        }
         return nullptr;
     }
 }
