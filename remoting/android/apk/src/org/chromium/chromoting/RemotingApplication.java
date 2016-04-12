d0d02c625b86ca535020b1c10279ccde4811fb1c
lambroslambrou <lambroslambrou@chromium.org>
2016-04-11 20:03:05 -0700

    [remoting android] Fix lint and presubmit warnings
    Use AlertDialog class from support library.
    Use DrawerLayout.addDrawerListener() (the newer version of the support
    library added this and marked setDrawerListener() as deprecated).
    Add super.onCreate() in Application-derived class.
    
    Review URL: https://codereview.chromium.org/1878853002
    
    Cr-Commit-Position: refs/heads/master@{#386562}

--- remoting/android/apk/src/org/chromium/chromoting/RemotingApplication.java
+++ remoting/android/apk/src/org/chromium/chromoting/RemotingApplication.java
@@ -15,6 +15,7 @@ import org.chromium.chromoting.help.HelpSingleton;
 public class RemotingApplication extends Application {
     @Override
     public void onCreate() {
+        super.onCreate();
         AccountSwitcherFactory.setInstance(new AccountSwitcherFactory());
         HelpSingleton.setInstance(new HelpAndFeedbackBasic());
     }
