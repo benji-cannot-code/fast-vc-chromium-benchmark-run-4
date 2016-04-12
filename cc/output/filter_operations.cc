f32362ba236b218c85b6f81cbee8721dea26155f
jbroman <jbroman@chromium.org>
2016-04-12 13:47:22 -0700

    Implement cc::FilterOperations::MapRect.
    Unlike GetOutsets(), this is flexible enough to represent other kinds of
    mapping between filter source and destination rect, like scaling.
    
    BUG=600821
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1867913002
    
    Cr-Commit-Position: refs/heads/master@{#386801}

--- cc/output/filter_operations.cc
+++ cc/output/filter_operations.cc
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <cmath>
+#include <numeric>
 
 #include "base/trace_event/trace_event_argument.h"
 #include "base/values.h"
 #include "cc/output/filter_operation.h"
+#include "ui/gfx/geometry/rect.h"
 
 namespace cc {
 
@@ -55,6 +57,14 @@ static int SpreadForStdDeviation(float std_deviation) {
   // filter.
   float d = floorf(std_deviation * 3.f * sqrt(8.f * atan(1.f)) / 4.f + 0.5f);
   return static_cast<int>(ceilf(d * 3.f / 2.f));
+}
+
+gfx::Rect FilterOperations::MapRect(const gfx::Rect& rect) const {
+  auto accumulate_rect = [](const gfx::Rect& rect, const FilterOperation& op) {
+    return op.MapRect(rect);
+  };
+  return std::accumulate(operations_.begin(), operations_.end(), rect,
+                         accumulate_rect);
 }
 
 void FilterOperations::GetOutsets(int* top,
--- cc/output/filter_operations.h
+++ cc/output/filter_operations.h
@@ -21,6 +21,10 @@ class TracedValue;
 class Value;
 }
 
+namespace gfx {
+class Rect;
+}
+
 namespace cc {
 
 // An ordered list of filter operations.
@@ -46,6 +50,10 @@ class CC_EXPORT FilterOperations {
   void Clear();
 
   bool IsEmpty() const;
+
+  // Maps "forward" to determine which pixels in a destination rect are affected
+  // by pixels in the source rect.
+  gfx::Rect MapRect(const gfx::Rect& rect) const;
 
   void GetOutsets(int* top, int* right, int* bottom, int* left) const;
   bool HasFilterThatMovesPixels() const;
