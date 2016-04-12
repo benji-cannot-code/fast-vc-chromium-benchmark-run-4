8ddcf655f25e9d75f89e0309d754347c2f189700
haraken <haraken@chromium.org>
2016-04-12 00:57:49 -0700

    Oilpan: Remove Oilpan from core/svg/ (part 1)
    BUG=585328
    
    Review URL: https://codereview.chromium.org/1876423002
    
    Cr-Commit-Position: refs/heads/master@{#386615}

--- third_party/WebKit/Source/core/svg/SVGMPathElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGMPathElement.cpp
@@ -44,9 +44,6 @@ DEFINE_NODE_FACTORY(SVGMPathElement)
 
 SVGMPathElement::~SVGMPathElement()
 {
-#if !ENABLE(OILPAN)
-    clearResourceReferences();
-#endif
 }
 
 void SVGMPathElement::buildPendingResource()
