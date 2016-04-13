2ed64f5e2ba406d9dcc1040c5691e339f9f01237
cavalcantii <cavalcantii@chromium.org>
2016-04-12 17:16:02 -0700

    Remove the user counter for border-image elements with no border style defined (plus associated layout test) as also the console warning for such cases.
    BUG=602779
    
    Review URL: https://codereview.chromium.org/1885713002
    
    Cr-Commit-Position: refs/heads/master@{#386874}

--- third_party/WebKit/Source/core/paint/NinePieceImagePainter.cpp
+++ third_party/WebKit/Source/core/paint/NinePieceImagePainter.cpp
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/paint/NinePieceImagePainter.h"
 
-#include "core/frame/Deprecation.h"
 #include "core/layout/ImageQualityController.h"
 #include "core/layout/LayoutBoxModelObject.h"
 #include "core/paint/BoxPainter.h"
@@ -35,14 +34,6 @@ bool NinePieceImagePainter::paint(GraphicsContext& graphicsContext, const Layout
 
     if (!styleImage->canRender())
         return false;
-
-    // Find out if the hasImage() check in ComputedStyle::border*Width had any affect, i.e. if a border is non-zero while border-style is
-    // none or hidden.
-    if ((style.borderLeftWidth() && (style.borderLeft().style() == BorderStyleNone || style.borderLeft().style() == BorderStyleHidden))
-        || (style.borderRightWidth() && (style.borderRight().style() == BorderStyleNone || style.borderRight().style() == BorderStyleHidden))
-        || (style.borderTopWidth() && (style.borderTop().style() == BorderStyleNone || style.borderTop().style() == BorderStyleHidden))
-        || (style.borderBottomWidth() && (style.borderBottom().style() == BorderStyleNone || style.borderBottom().style() == BorderStyleHidden)))
-        Deprecation::countDeprecation(m_layoutObject.document(), UseCounter::BorderImageWithBorderStyleNone);
 
     // FIXME: border-image is broken with full page zooming when tiling has to happen, since the tiling function
     // doesn't have any understanding of the zoom that is in effect on the tile.
