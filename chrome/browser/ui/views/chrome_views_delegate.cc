914f101851c1c0e35296d509fd4ba9386ba3c217
dgn <dgn@chromium.org>
2016-04-13 12:10:04 -0700

    [bgmode] Enforce that ChromeViewsDelegate doesn't attempt multiple AddRef
    BUG=587926
    
    Review URL: https://codereview.chromium.org/1864543003
    
    Cr-Commit-Position: refs/heads/master@{#387060}

--- chrome/browser/ui/views/chrome_views_delegate.cc
+++ chrome/browser/ui/views/chrome_views_delegate.cc
@@ -186,6 +186,7 @@ ChromeViewsDelegate::ChromeViewsDelegate() {
 }
 
 ChromeViewsDelegate::~ChromeViewsDelegate() {
+  DCHECK_EQ(0u, ref_count_);
 }
 
 void ChromeViewsDelegate::SaveWindowPlacement(const views::Widget* window,
@@ -311,12 +312,20 @@ views::NonClientFrameView* ChromeViewsDelegate::CreateDefaultNonClientFrameView(
 #endif
 
 void ChromeViewsDelegate::AddRef() {
-  keep_alive_.reset(new ScopedKeepAlive(KeepAliveOrigin::CHROME_VIEWS_DELEGATE,
-                                        KeepAliveRestartOption::DISABLED));
+  if (ref_count_ == 0u) {
+    keep_alive_.reset(
+        new ScopedKeepAlive(KeepAliveOrigin::CHROME_VIEWS_DELEGATE,
+                            KeepAliveRestartOption::DISABLED));
+  }
+
+  ++ref_count_;
 }
 
 void ChromeViewsDelegate::ReleaseRef() {
-  keep_alive_.reset();
+  DCHECK_NE(0u, ref_count_);
+
+  if (--ref_count_ == 0u)
+    keep_alive_.reset();
 }
 
 void ChromeViewsDelegate::OnBeforeWidgetInit(
