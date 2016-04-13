b39a9987a8b569387d83ea9c881a214e8fa0a377
a-v-y <a-v-y@yandex-team.ru>
2016-04-13 00:46:48 -0700

    Remove unused history ui strings and variables
    This is cleanup of strings and variables used in history ui page.
    IDS_HISTORY_FILTER_ALLOW_ITEMS and IDS_HISTORY_FILTER_BLOCK_ITEMS are
    not used since https://chromiumcodereview.appspot.com/15415006.
    IDS_HISTORY_GROUP_BY_DOMAIN_LABEL, IDS_HISTORY_FILTER_ALLOWED,
    IDS_HISTORY_IN_CONTENT_PACK usage is removed in
    https://chromiumcodereview.appspot.com/15969014.
    isFullHistorySyncEnabled variable is not used since
    https://chromiumcodereview.appspot.com/14170006.
    
    R=dbeam@chromium.org, sdefresne@chromium.org
    
    Review URL: https://codereview.chromium.org/1836773002
    
    Cr-Commit-Position: refs/heads/master@{#386931}

--- ios/chrome/browser/ui/webui/history/history_ui.cc
+++ ios/chrome/browser/ui/webui/history/history_ui.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/chrome_switches.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
-#include "ios/chrome/browser/history/web_history_service_factory.h"
 #include "ios/chrome/browser/signin/signin_manager_factory.h"
 #include "ios/chrome/browser/ui/webui/history/browsing_history_handler.h"
 #include "ios/chrome/browser/ui/webui/history/metrics_handler.h"
@@ -89,8 +88,6 @@ web::WebUIIOSDataSource* CreateHistoryUIHTMLSource(
                              IDS_HISTORY_ACTION_MENU_DESCRIPTION);
   source->AddLocalizedString("removeFromHistory", IDS_HISTORY_REMOVE_PAGE);
   source->AddLocalizedString("moreFromSite", IDS_HISTORY_MORE_FROM_SITE);
-  source->AddLocalizedString("groupByDomainLabel",
-                             IDS_HISTORY_GROUP_BY_DOMAIN_LABEL);
   source->AddLocalizedString("rangeLabel", IDS_HISTORY_RANGE_LABEL);
   source->AddLocalizedString("rangeAllTime", IDS_HISTORY_RANGE_ALL_TIME);
   source->AddLocalizedString("rangeWeek", IDS_HISTORY_RANGE_WEEK);
@@ -99,11 +96,7 @@ web::WebUIIOSDataSource* CreateHistoryUIHTMLSource(
   source->AddLocalizedString("rangeNext", IDS_HISTORY_RANGE_NEXT);
   source->AddLocalizedString("rangePrevious", IDS_HISTORY_RANGE_PREVIOUS);
   source->AddLocalizedString("numberVisits", IDS_HISTORY_NUMBER_VISITS);
-  source->AddLocalizedString("filterAllowed", IDS_HISTORY_FILTER_ALLOWED);
   source->AddLocalizedString("filterBlocked", IDS_HISTORY_FILTER_BLOCKED);
-  source->AddLocalizedString("inContentPack", IDS_HISTORY_IN_CONTENT_PACK);
-  source->AddLocalizedString("allowItems", IDS_HISTORY_FILTER_ALLOW_ITEMS);
-  source->AddLocalizedString("blockItems", IDS_HISTORY_FILTER_BLOCK_ITEMS);
   source->AddLocalizedString("blockedVisitText",
                              IDS_HISTORY_BLOCKED_VISIT_TEXT);
   source->AddLocalizedString("hasSyncedResults",
@@ -116,9 +109,6 @@ web::WebUIIOSDataSource* CreateHistoryUIHTMLSource(
   source->AddLocalizedString("cancel", IDS_CANCEL);
   source->AddLocalizedString("deleteConfirm",
                              IDS_HISTORY_DELETE_PRIOR_VISITS_CONFIRM_BUTTON);
-  source->AddBoolean(
-      "isFullHistorySyncEnabled",
-      ios::WebHistoryServiceFactory::GetForBrowserState(browser_state) != NULL);
   bool group_by_domain = base::CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kHistoryEnableGroupByDomain);
   source->AddBoolean("groupByDomain", group_by_domain);
