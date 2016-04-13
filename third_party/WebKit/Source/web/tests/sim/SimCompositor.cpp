7590c2ce4c74ad900cf15ec1f52c6d40640b65a9
skyostil <skyostil@chromium.org>
2016-04-13 11:46:17 -0700

    Don't try to render composited selection in throttled frame
    If a frame is throttled (i.e., offscreen) and has focus, we should not
    try to render a composited selection for it because its layout
    information might be out of date.
    
    BUG=602334
    
    Review URL: https://codereview.chromium.org/1891453002
    
    Cr-Commit-Position: refs/heads/master@{#387047}

--- third_party/WebKit/Source/web/tests/sim/SimCompositor.cpp
+++ third_party/WebKit/Source/web/tests/sim/SimCompositor.cpp
@@ -46,6 +46,7 @@ static void paintFrames(LocalFrame& root, SimDisplayItemList& displayList)
 SimCompositor::SimCompositor()
     : m_needsAnimate(false)
     , m_deferCommits(true)
+    , m_hasSelection(false)
     , m_webViewImpl(0)
     , m_lastFrameTimeMonotonic(0)
 {
@@ -74,6 +75,16 @@ void SimCompositor::setNeedsAnimate()
 void SimCompositor::setDeferCommits(bool deferCommits)
 {
     m_deferCommits = deferCommits;
+}
+
+void SimCompositor::registerSelection(const WebSelection&)
+{
+    m_hasSelection = true;
+}
+
+void SimCompositor::clearSelection()
+{
+    m_hasSelection = false;
 }
 
 SimDisplayItemList SimCompositor::beginFrame()
