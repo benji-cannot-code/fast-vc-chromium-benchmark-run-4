f7864a5d2947bf8908f7583193c3dbd9dabb3d15
tkent <tkent@chromium.org>
2016-04-12 21:32:04 -0700

    Fix a regression of stream printer for Node.
    It showed a Node like:
    [...] ... "DIV"" ID=\"console-prompt\" CLASS=\"source-code text-prompt\""
    
    We don't need to quote the nodeName and the whole attributes.
    This is a regression by crrev.com/383236, which removed .utf8().data().
    This CL fixes it as follows:
    
    [...] ... DIV id="console-prompt" class="source-code text-prompt"
    
    Attribute names are lowercased because we use QualifiedName::toString().
    
    This CL doesn't affect production behavior.
    
    Review URL: https://codereview.chromium.org/1885753002
    
    Cr-Commit-Position: refs/heads/master@{#386913}

--- third_party/WebKit/Source/wtf/text/AtomicString.h
+++ third_party/WebKit/Source/wtf/text/AtomicString.h
@@ -240,7 +240,8 @@ template<> struct DefaultHash<AtomicString> {
     typedef AtomicStringHash Hash;
 };
 
-// Pretty printer for gtest and base/logging.*.
+// Pretty printer for gtest and base/logging.*.  It prepends and appends
+// double-quotes, and escapes chracters other than ASCII printables.
 WTF_EXPORT std::ostream& operator<<(std::ostream&, const AtomicString&);
 
 } // namespace WTF
