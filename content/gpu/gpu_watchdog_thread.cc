07b7ce11ee5ebbc7beeb265f30744cc7cab064af
stanisc <stanisc@chromium.org>
2016-04-13 16:00:58 -0700

    Add suspend and resume times to GpuWatchdogThread
    Adding these times for diagnostic purposes to help understand
    whether hangs are more likely to happen around suspend/resume.
    
    BUG=596190
    
    Review URL: https://codereview.chromium.org/1886073002
    
    Cr-Commit-Position: refs/heads/master@{#387139}

--- content/gpu/gpu_watchdog_thread.cc
+++ content/gpu/gpu_watchdog_thread.cc
@@ -404,6 +404,7 @@ void GpuWatchdogThread::OnAddPowerObserver() {
 
 void GpuWatchdogThread::OnSuspend() {
   suspended_ = true;
+  suspend_time_ = base::Time::Now();
 
   // When suspending force an acknowledgement to cancel any pending termination
   // tasks.
@@ -412,6 +413,7 @@ void GpuWatchdogThread::OnSuspend() {
 
 void GpuWatchdogThread::OnResume() {
   suspended_ = false;
+  resume_time_ = base::Time::Now();
 
   // After resuming jump-start the watchdog again.
   armed_ = false;
