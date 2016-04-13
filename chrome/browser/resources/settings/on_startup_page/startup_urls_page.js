c8dd69d711a6ca9c3c96f75a2ad5868098793cb7
dpapad <dpapad@chromium.org>
2016-04-13 16:53:09 -0700

    MD Settings: On Startup, moving remove URL option under a dots menu.
     - Extracting new element <settings-startup-url-entry>
     - Implementing dots menu, that currently only has the "Remove" option.
     - Adding test.
    
    This is in preparation of adding the "Edit" option under the same menu.
    
    BUG=601514
    
    Review URL: https://codereview.chromium.org/1876283002
    
    Cr-Commit-Position: refs/heads/master@{#387158}

--- chrome/browser/resources/settings/on_startup_page/startup_urls_page.js
+++ chrome/browser/resources/settings/on_startup_page/startup_urls_page.js
@@ -8,15 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * containing the urls that will be opened when chrome is started.
  */
 
-/**
- * @typedef {{
- *   title: string,
- *   tooltip: string,
- *   url: string
- * }}
- */
-var StartupPageInfo;
-
 Polymer({
   is: 'settings-startup-urls-page',
 
@@ -45,15 +36,6 @@ Polymer({
   },
 
   /**
-   * @param {string} url Location of an image to get a set of icons fors.
-   * @return {string} A set of icon URLs.
-   * @private
-   */
-  getIconSet_: function(url) {
-    return getFaviconImageSet(url);
-  },
-
-  /**
    * Opens the dialog and registers a listener for removing the dialog from the
    * DOM once is closed. The listener is destroyed when the dialog is removed
    * (because of 'restamp').
@@ -72,13 +54,5 @@ Polymer({
   /** @private */
   onUseCurrentPagesTap_: function() {
     this.browserProxy_.useCurrentPages();
-  },
-
-  /**
-   * @param {!{model: !{index: number}}} e
-   * @private
-   */
-  onRemoveUrlTap_: function(e) {
-    this.browserProxy_.removeStartupPage(e.model.index);
   },
 });
