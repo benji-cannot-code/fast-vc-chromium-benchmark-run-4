ed122ca9345a46bc065435fdbfc3691758a62115
rnephew <rnephew@chromium.org>
2016-04-13 13:18:34 -0700

    Revert of Move profile handling code to a child class of SharedPageState (patchset #2 id:20001 of https://codereview.chromium.org/1884493002/ )
    Reason for revert:
    Breaks startup tests.
    crbug.com/603121
    
    Original issue's description:
    > Move profile handling code to a child class of SharedPageState
    >
    > There will be a separate review for removing this logic from SharedPageState.
    > More info: https://codereview.chromium.org/1871033002/
    >
    > BUG=
    >
    > Committed: https://crrev.com/70390020b54d46b4d34f96429c1db62b1f0629c4
    > Cr-Commit-Position: refs/heads/master@{#386943}
    
    TBR=eakuefner@chromium.org,gurrrik@yandex-team.ru
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review URL: https://codereview.chromium.org/1879193005
    
    Cr-Commit-Position: refs/heads/master@{#387074}

--- tools/perf/page_sets/pregenerated_large_profile_shared_state.py
+++ tools/perf/page_sets/pregenerated_large_profile_shared_state.py
@@ -4,16 +4,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 import os
 
-from page_sets import pregenerated_profile_shared_state
+from telemetry.page import shared_page_state
 
 
-class PregeneratedLargeProfileSharedState(
-    pregenerated_profile_shared_state.PregeneratedProfileSharedState):
+class PregeneratedLargeProfileSharedState(shared_page_state.SharedPageState):
   def __init__(self, test, finder_options, story_set):
     super(PregeneratedLargeProfileSharedState, self).__init__(
         test, finder_options, story_set)
     perf_dir = os.path.normpath(
         os.path.join(os.path.dirname(__file__), os.path.pardir))
-    self._pregenerated_profile_archive_dir = os.path.join(
+    profile_archive_dir = os.path.join(
         perf_dir, 'generated_profiles', self._possible_browser.target_os,
         'large_profile.zip')
+
+    self.SetPregeneratedProfileArchiveDir(profile_archive_dir)
