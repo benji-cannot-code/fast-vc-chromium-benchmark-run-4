93aaf381642b0d508f81a6c7684118247027f355
michaeldo <michaeldo@chromium.org>
2016-04-14 12:47:02 -0700

    Revert of Move SSL handler methods to superclass. (patchset #3 id:40001 of https://codereview.chromium.org/1885173002/ )
    Reason for revert:
    Broke CRWWebControllerMock since init was marked as NS_UNAVAILABLE. Allowing init or updating the tests will be needed before re-landing.
    
    Original issue's description:
    > Move SSL handler methods to superclass.
    >
    > Move SSL handling methods from CRWWKWebViewWebController to
    > CRWWebController.
    >
    > BUG=579697
    >
    > Committed: https://crrev.com/3fb62d5e550e2953bc62e52c13e33b6bd1d9b962
    > Cr-Commit-Position: refs/heads/master@{#387371}
    
    TBR=eugenebut@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=579697
    
    Review URL: https://codereview.chromium.org/1884273002
    
    Cr-Commit-Position: refs/heads/master@{#387397}

--- ios/web/web_state/ui/crw_wk_web_view_web_controller.h
+++ ios/web/web_state/ui/crw_wk_web_view_web_controller.h
@@ -8,8 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web/web_state/ui/crw_web_controller.h"
 
+#include <memory>
+
 // A concrete implementation of CRWWebController based on WKWebView.
 @interface CRWWKWebViewWebController : CRWWebController
+
+// Designated initializer. Initializes web controller with |webState|. The
+// calling code must retain the ownership of |webState|.
+- (instancetype)initWithWebState:(web::WebStateImpl*)webState;
 
 @end
 
