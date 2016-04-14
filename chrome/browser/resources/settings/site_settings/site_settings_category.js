5cdb205fd17b6d799be96f4e3f7458dfab00eb34
finnur <finnur@chromium.org>
2016-04-14 04:39:54 -0700

    Site Settings: Specify categories on Site Settings Page in a more declarative way.
    BUG=543635
    
    Review URL: https://codereview.chromium.org/1889473002
    
    Cr-Commit-Position: refs/heads/master@{#387285}

--- chrome/browser/resources/settings/site_settings/site_settings_category.js
+++ chrome/browser/resources/settings/site_settings/site_settings_category.js
@@ -55,7 +55,6 @@ Polymer({
     this.$.blockList.categorySubtype = settings.PermissionValues.BLOCK;
     this.$.allowList.categorySubtype = settings.PermissionValues.ALLOW;
 
-    this.prefsProxy_ = settings.SiteSettingsPrefsBrowserProxyImpl.getInstance();
     this.addWebUIListener('contentSettingCategoryChanged',
         this.defaultValueForCategoryChanged_.bind(this));
   },
@@ -81,7 +80,7 @@ Polymer({
       case settings.ContentSettingsTypes.JAVASCRIPT:
       case settings.ContentSettingsTypes.POPUPS:
         // "Allowed" vs "Blocked".
-        this.prefsProxy_.setDefaultValueForContentType(
+        this.browserProxy.setDefaultValueForContentType(
             this.category,
             this.categoryEnabled ?
                 settings.PermissionValues.ALLOW :
@@ -92,7 +91,7 @@ Polymer({
       case settings.ContentSettingsTypes.CAMERA:
       case settings.ContentSettingsTypes.MIC:
         // "Ask" vs "Blocked".
-        this.prefsProxy_.setDefaultValueForContentType(
+        this.browserProxy.setDefaultValueForContentType(
             this.category,
             this.categoryEnabled ?
                 settings.PermissionValues.ASK :
@@ -100,7 +99,7 @@ Polymer({
         break;
       case settings.ContentSettingsTypes.FULLSCREEN:
         // "Allowed" vs. "Ask first".
-        this.prefsProxy_.setDefaultValueForContentType(
+        this.browserProxy.setDefaultValueForContentType(
             this.category,
             this.categoryEnabled ?
                 settings.PermissionValues.ALLOW :
