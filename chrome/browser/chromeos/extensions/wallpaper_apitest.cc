c38457da6babfc6cfd16900fbe8073cca2447579
ihf <ihf@chromium.org>
2016-04-14 15:15:22 -0700

    Revert of Remove WallpaperManager dependency on Cryptohome. (patchset #7 id:120001 of https://codereview.chromium.org/1886653002/ )
    Reason for revert:
    crbug.com/603655
    
    Original issue's description:
    > Remove WallpaperManager dependency on Cryptohome.
    >
    > This Cl adds wallpaper file names hashing to WallpaperManager itself,
    > removing dependency on cryptohome. No more asynchronous calls.
    >
    > BUG=462823
    > TEST=manual
    >
    > Committed: https://crrev.com/81e10078a524522e862dd33ebdceaf64f4ebeba1
    > Cr-Commit-Position: refs/heads/master@{#387235}
    
    TBR=dkrahn@chromium.org,bshe@chromium.org,alemate@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=462823
    
    Review URL: https://codereview.chromium.org/1887983005
    
    Cr-Commit-Position: refs/heads/master@{#387443}

--- chrome/browser/chromeos/extensions/wallpaper_apitest.cc
+++ chrome/browser/chromeos/extensions/wallpaper_apitest.cc
@@ -4,12 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/extensions/extension_apitest.h"
-#include "chromeos/cryptohome/system_salt_getter.h"
 #include "net/dns/mock_host_resolver.h"
 
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Wallpaper) {
-  chromeos::SystemSaltGetter::Get()->SetRawSaltForTesting(
-      chromeos::SystemSaltGetter::RawSalt({1, 2, 3, 4, 5, 6, 7, 8}));
   host_resolver()->AddRule("a.com", "127.0.0.1");
   ASSERT_TRUE(StartEmbeddedTestServer());
   ASSERT_TRUE(RunExtensionTest("wallpaper")) << message_;
