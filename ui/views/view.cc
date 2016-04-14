1e19bba3620d804f0e5ec2ebb159232baaca786a
bruthig <bruthig@chromium.org>
2016-04-14 16:30:06 -0700

    Set the name of the ui::Layer when a View creates it.
    Setting a default name to a View owned ui::Layer makes it easier to
    navigate and investigate the Layer trees.
    
    Review URL: https://codereview.chromium.org/1888873002
    
    Cr-Commit-Position: refs/heads/master@{#387471}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -2088,9 +2088,7 @@ void View::CreateLayer() {
 
   SetLayer(new ui::Layer());
   layer()->set_delegate(this);
-#if !defined(NDEBUG)
   layer()->set_name(GetClassName());
-#endif
 
   UpdateParentLayers();
   UpdateLayerVisibility();
