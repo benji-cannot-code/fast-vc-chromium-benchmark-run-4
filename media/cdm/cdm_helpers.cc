c29f97d30c5a366d675b041dab3428b5dbf4c0e1
jrummell <jrummell@chromium.org>
2016-04-13 19:26:53 -0700

    media: Fix gn deps on media/ targets.
    gn check out/GN //media/ passes.
    
    BUG=599578
    
    Review URL: https://codereview.chromium.org/1862543002
    
    Cr-Commit-Position: refs/heads/master@{#387212}

--- media/cdm/cdm_helpers.cc
+++ media/cdm/cdm_helpers.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(USE_PPAPI_CDM_ADAPTER)
 // When building the ppapi adapter do not include any non-trivial base/ headers.
-#include "ppapi/cpp/logging.h"
+#include "ppapi/cpp/logging.h"  // nogncheck
 #define PLATFORM_DCHECK PP_DCHECK
 #else
 #include "base/logging.h"
