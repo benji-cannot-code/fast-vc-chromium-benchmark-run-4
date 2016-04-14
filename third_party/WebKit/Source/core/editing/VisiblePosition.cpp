d67887744d9dbd0bfa9e4dd308c019ad95a47357
yosin <yosin@chromium.org>
2016-04-13 20:36:53 -0700

    Get rid of stdio.h dependency in "VisiblePosition.cpp"
    This patch gets rid of |#include <stdio.h>| from "VisiblePosition.cpp| by
    replacing |fprintf()| with |VLOG(0)|.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1887833003
    
    Cr-Commit-Position: refs/heads/master@{#387230}

--- third_party/WebKit/Source/core/editing/VisiblePosition.cpp
+++ third_party/WebKit/Source/core/editing/VisiblePosition.cpp
@@ -40,10 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/line/RootInlineBox.h"
 #include "platform/geometry/FloatQuad.h"
 #include "wtf/text/CString.h"
-
-#ifndef NDEBUG
-#include <stdio.h>
-#endif
+#include <ostream> // NOLINT
 
 namespace blink {
 
@@ -143,10 +140,11 @@ std::ostream& operator<<(std::ostream& ostream, const VisiblePositionInFlatTree&
 
 void showTree(const blink::VisiblePosition* vpos)
 {
-    if (vpos)
+    if (vpos) {
         vpos->showTreeForThis();
-    else
-        fprintf(stderr, "Cannot showTree for (nil) VisiblePosition.\n");
+        return;
+    }
+    DVLOG(0) << "Cannot showTree for (nil) VisiblePosition.";
 }
 
 void showTree(const blink::VisiblePosition& vpos)
