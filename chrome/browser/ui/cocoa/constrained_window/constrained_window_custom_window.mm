67c880335c74e76ad6ffe00dceed0c86fec06e63
erikchen <erikchen@chromium.org>
2016-04-13 17:47:58 -0700

    mac: Disable window shadows in tests.
    It causes Window Server crashes on VMs.
    
    BUG=515627
    
    Review URL: https://codereview.chromium.org/1888573002
    
    Cr-Commit-Position: refs/heads/master@{#387177}

--- chrome/browser/ui/cocoa/constrained_window/constrained_window_custom_window.mm
+++ chrome/browser/ui/cocoa/constrained_window/constrained_window_custom_window.mm
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "chrome/browser/ui/cocoa/constrained_window/constrained_window_custom_window.h"
 
+#include "base/command_line.h"
 #import "base/mac/scoped_nsobject.h"
 #import "chrome/browser/ui/chrome_style.h"
 #import "chrome/browser/ui/cocoa/constrained_window/constrained_window_sheet_controller.h"
+#include "content/public/common/content_switches.h"
 #include "skia/ext/skia_utils_mac.h"
 #include "ui/gfx/scoped_ns_graphics_context_save_gstate_mac.h"
 
@@ -40,7 +42,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                styleMask:windowStyle
                                  backing:bufferingType
                                    defer:NO])) {
-    [self setHasShadow:YES];
+    // Don't draw shadows in tests, as that causes Window Server crashes on VMs.
+    // https://crbug.com/515627
+    base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+    if (!command_line->HasSwitch(switches::kTestType))
+      [self setHasShadow:YES];
     [self setBackgroundColor:skia::SkColorToCalibratedNSColor(
         chrome_style::GetBackgroundColor())];
     [self setOpaque:NO];
