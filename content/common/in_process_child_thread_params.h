adb4e55070c1a41aca966d51843578b6dcd1d5ce
shrike <shrike@chromium.org>
2016-04-14 14:46:39 -0700

    Revert of Use a token to initialise ChannelMojo and MojoApplication everywhere. (patchset #5 id:80001 of https://codereview.chromium.org/1880343002/ )
    Reason for revert:
    Suspecting for https://crbug.com/603452. We need to see if reverting this patch fixes tomorrow's Mac Canary. If the problem persists then this cl is not to blame and can be relanded.
    
    Original issue's description:
    > Use a token to initialise ChannelMojo and MojoApplication everywhere.
    >
    > BUG=None
    >
    > Committed: https://crrev.com/babb3f34fe20effb72c81104b63b2974271ad88d
    > Cr-Commit-Position: refs/heads/master@{#387161}
    
    TBR=rockot@chromium.org,tsepez@chromium.org,avi@chromium.org,amistry@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=None
    
    Review URL: https://codereview.chromium.org/1891043002
    
    Cr-Commit-Position: refs/heads/master@{#387437}

--- content/common/in_process_child_thread_params.h
+++ content/common/in_process_child_thread_params.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/sequenced_task_runner.h"
 #include "content/common/content_export.h"
+#include "mojo/public/cpp/system/message_pipe.h"
 
 namespace content {
 
@@ -22,7 +23,7 @@ class CONTENT_EXPORT InProcessChildThreadParams {
   InProcessChildThreadParams(
       const std::string& channel_name,
       scoped_refptr<base::SequencedTaskRunner> io_runner,
-      const std::string& ipc_token = std::string(),
+      mojo::MessagePipeHandle handle = mojo::MessagePipeHandle(),
       const std::string& application_token = std::string());
   InProcessChildThreadParams(const InProcessChildThreadParams& other);
   ~InProcessChildThreadParams();
@@ -31,7 +32,7 @@ class CONTENT_EXPORT InProcessChildThreadParams {
   scoped_refptr<base::SequencedTaskRunner> io_runner() const {
     return io_runner_;
   }
-  const std::string& ipc_token() const { return ipc_token_; }
+  mojo::MessagePipeHandle handle() const { return handle_; }
   const std::string& application_token() const {
     return application_token_;
   }
@@ -39,7 +40,7 @@ class CONTENT_EXPORT InProcessChildThreadParams {
  private:
   std::string channel_name_;
   scoped_refptr<base::SequencedTaskRunner> io_runner_;
-  std::string ipc_token_;
+  mojo::MessagePipeHandle handle_;
   std::string application_token_;
 };
 
