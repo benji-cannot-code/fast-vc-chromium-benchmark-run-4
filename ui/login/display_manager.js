835cec48d3b14b4e24018534775be6a116c96860
jdufault <jdufault@chromium.org>
2016-04-14 11:38:14 -0700

    Add enterprise enrollment support for fake users.
    This is one CL in a set of three:
    - https://codereview.chromium.org/1767443002 (this one)
    - https://codereview.chromium.org/1765483004
    - https://chromium-review.googlesource.com/#/c/330272
    
    BUG=586195
    
    Review URL: https://codereview.chromium.org/1767443002
    
    Cr-Commit-Position: refs/heads/master@{#387375}

--- ui/login/display_manager.js
+++ ui/login/display_manager.js
@@ -573,6 +573,9 @@ cr.define('cr.ui.login', function() {
 
       $('step-logo').hidden = newStep.classList.contains('no-logo');
 
+      $('oobe').dispatchEvent(
+          new CustomEvent('screenchanged',
+                          {detail: this.currentScreen.id}));
       chrome.send('updateCurrentScreen', [this.currentScreen.id]);
     },
 
