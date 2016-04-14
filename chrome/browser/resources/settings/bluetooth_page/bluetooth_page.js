43590ad9e98d7cd2b749e12e16540f0950154d34
dbeam <dbeam@chromium.org>
2016-04-13 20:02:23 -0700

    MD Settings: polish top-level bluetooth page
    R=stevenjb@chromium.org
    BUG=602538
    
    Review URL: https://codereview.chromium.org/1875413002
    
    Cr-Commit-Position: refs/heads/master@{#387220}

--- chrome/browser/resources/settings/bluetooth_page/bluetooth_page.js
+++ chrome/browser/resources/settings/bluetooth_page/bluetooth_page.js
@@ -198,6 +198,16 @@ Polymer({
   },
 
   /**
+   * @param {boolean} bluetoothEnabled
+   * @param {boolean} deviceListExpanded
+   * @return {boolean} Whether the <iron-collapse> can be shown.
+   * @private
+   */
+  canShowDeviceList_: function(bluetoothEnabled, deviceListExpanded) {
+    return bluetoothEnabled && deviceListExpanded;
+  },
+
+  /**
    * If bluetooth is enabled, request the complete list of devices and update
    * |deviceList|.
    * @private
