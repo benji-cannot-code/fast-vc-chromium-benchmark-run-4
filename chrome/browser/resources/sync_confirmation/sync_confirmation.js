df1a06e4675684bf766f9292393c4dce8d27f6a8
anthonyvd <anthonyvd@chromium.org>
2016-04-14 13:35:31 -0700

    Switch button order in sync confirmation on Mac and Linux.
    This CL flips the order of the "OK, Got it" and "Undo" buttons in the
    sync confirmation dialog on platforms where that order is the platform-
    specific preferred order. It also makes "OK, Got it" the default
    focused element.
    
    BUG=601658,589506
    
    Review URL: https://codereview.chromium.org/1880003003
    
    Cr-Commit-Position: refs/heads/master@{#387410}

--- chrome/browser/resources/sync_confirmation/sync_confirmation.js
+++ chrome/browser/resources/sync_confirmation/sync_confirmation.js
@@ -19,6 +19,7 @@ cr.define('sync.confirmation', function() {
   }
 
   function initialize() {
+    document.addEventListener('keydown', onKeyDown);
     $('confirmButton').addEventListener('click', onConfirm);
     $('undoButton').addEventListener('click', onUndo);
     $('settingsLink').addEventListener('click', onGoToSettings);
@@ -32,6 +33,17 @@ cr.define('sync.confirmation', function() {
 
   function onPictureLoaded(e) {
     $('picture-container').classList.add('loaded');
+  }
+
+  function onKeyDown(e) {
+    // If the currently focused element isn't something that performs an action
+    // on "enter" being pressed and the user hits "enter", perform the default
+    // action of the dialog, which is "OK, Got It".
+    if (e.keyIdentifier == 'Enter' &&
+        !/^(A|PAPER-BUTTON)$/.test(document.activeElement.tagName)) {
+      $('confirmButton').click();
+      e.preventDefault();
+    }
   }
 
   return {
