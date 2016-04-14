6fd204ec9ade0953b1fc9903f0182cde50f638fa
xunjieli <xunjieli@chromium.org>
2016-04-14 14:15:29 -0700

    Log sent and received bytes in net::BidirectionalStream
    This CL adds logging of sent and received bytes in net::BidirectionalStream if
    capturing all bytes.
    
    BUG=602669
    
    Review URL: https://codereview.chromium.org/1883883002
    
    Cr-Commit-Position: refs/heads/master@{#387425}

--- net/http/bidirectional_stream.h
+++ net/http/bidirectional_stream.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/macros.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "net/http/bidirectional_stream_impl.h"
 #include "net/http/http_stream_factory.h"
@@ -202,6 +203,13 @@ class NET_EXPORT BidirectionalStream
   // The underlying BidirectioanlStreamImpl used for this stream. It is
   // non-NULL, if the |stream_request_| successfully finishes.
   scoped_ptr<BidirectionalStreamImpl> stream_impl_;
+
+  // Buffer used for reading.
+  scoped_refptr<IOBuffer> read_buffer_;
+  // Buffer used for writing.
+  scoped_refptr<IOBuffer> write_buffer_;
+  // Length of |write_buffer_|.
+  size_t write_buffer_len_;
 
   DISALLOW_COPY_AND_ASSIGN(BidirectionalStream);
 };
