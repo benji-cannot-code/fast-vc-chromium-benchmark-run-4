782c06d0c372cec25c9a28b5a6b13c449406f2d2
jinsukkim <jinsukkim@chromium.org>
2016-04-13 19:24:37 -0700

    UTF-8 detector for pages missing encoding info
    Experiment crbug.com/518968 shows that about 30% of the pages
    missing character encoding information are encoded in UTF-8.
    This CL runs a quick one-pass scan against them to check if
    they are encoded in UTF-8. It helps get the encoding right
    for 30% of the pages without having to turn on full auto-encoding
    detection logic which is disabled due to slow performance.
    
    BUG=583549
    TEST=Layout test: fast/encoding/unlabelled-non-ascii-utf8.html
    
    Review URL: https://codereview.chromium.org/1721373002
    
    Cr-Commit-Position: refs/heads/master@{#387209}

--- third_party/WebKit/Source/platform/text/TextEncodingDetector.cpp
+++ third_party/WebKit/Source/platform/text/TextEncodingDetector.cpp
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-bool detectTextEncoding(const char* data, size_t length,
+bool detectTextEncodingUniversal(const char* data, size_t length,
     const char* hintEncodingName, WTF::TextEncoding* detectedEncoding)
 {
     *detectedEncoding = WTF::TextEncoding();
