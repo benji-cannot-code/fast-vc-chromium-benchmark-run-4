31190258469e5f1767897d361ea52ec9818a1611
haraken <haraken@chromium.org>
2016-04-15 00:08:07 -0700

    Oilpan: Remove ENABLE(OILPAN) from core/dom/ (part 4)
    BUG=585328
    
    Review URL: https://codereview.chromium.org/1884363003
    
    Cr-Commit-Position: refs/heads/master@{#387544}

--- third_party/WebKit/Source/core/dom/ElementData.cpp
+++ third_party/WebKit/Source/core/dom/ElementData.cpp
@@ -80,7 +80,6 @@ ElementData::ElementData(const ElementData& other, bool isUnique)
     // NOTE: The inline style is copied by the subclass copy constructor since we don't know what to do with it here.
 }
 
-#if ENABLE(OILPAN)
 void ElementData::finalizeGarbageCollectedObject()
 {
     if (m_isUnique)
@@ -88,15 +87,6 @@ void ElementData::finalizeGarbageCollectedObject()
     else
         toShareableElementData(this)->~ShareableElementData();
 }
-#else
-void ElementData::destroy()
-{
-    if (m_isUnique)
-        delete toUniqueElementData(this);
-    else
-        delete toShareableElementData(this);
-}
-#endif
 
 UniqueElementData* ElementData::makeUniqueCopy() const
 {
