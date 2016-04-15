53c4dcb371764b0e8abb20ce34eab7c0a331d6ff
asvitkine <asvitkine@chromium.org>
2016-04-15 14:33:35 -0700

    Even more instrumentation to diagnose an Android render crash.
    We're seeing renderers receiving IPCs to activate a trial to
    a different state than what was specified on the command line.
    This CL expands previously added instrumentation to detect if
    this is happening via separate FieldTrialList instances by
    also keeping a copy of the seen states between instances.
    
    Note: While the locking here is done through the lock on the
    FieldTrialList instance, this is safe to do because FieldTrialList
    has a check to ensure there is only a single FieldTrialList
    instance at a given time, so the single lock is sufficient.
    
    BUG=359406
    
    Review URL: https://codereview.chromium.org/1897493002
    
    Cr-Commit-Position: refs/heads/master@{#387702}

--- base/metrics/field_trial.cc
+++ base/metrics/field_trial.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/build_time.h"
+#include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/rand_util.h"
 #include "base/strings/string_util.h"
@@ -115,6 +116,17 @@ void CheckTrialGroup(const std::string& trial_name,
     (*seen_states)[trial_name] = trial_group;
   }
 }
+
+// A second copy of FieldTrialList::seen_states_ that is meant to outlive the
+// FieldTrialList object to determine if the inconsistency happens because there
+// might be multiple FieldTrialList objects.
+// TODO(asvitkine): Remove when crbug.com/359406 is resolved.
+base::LazyInstance<std::map<std::string, std::string>>::Leaky g_seen_states =
+    LAZY_INSTANCE_INITIALIZER;
+
+// Tracks whether |g_seen_states| is used. Defaults to false, because unit tests
+// will create multiple FieldTrialList instances.
+bool g_use_global_check_states = false;
 
 }  // namespace
 
@@ -339,6 +351,12 @@ FieldTrialList::~FieldTrialList() {
 }
 
 // static
+void FieldTrialList::EnableGlobalStateChecks() {
+  CHECK(!g_use_global_check_states);
+  g_use_global_check_states = true;
+}
+
+// static
 FieldTrial* FieldTrialList::FactoryGetFieldTrial(
     const std::string& trial_name,
     FieldTrial::Probability total_probability,
@@ -490,8 +508,13 @@ void FieldTrialList::AllStatesToString(std::string* output) {
     trial.group_name.AppendToString(output);
     output->append(1, kPersistentStringSeparator);
 
+    // TODO(asvitkine): Remove these when http://crbug.com/359406 is fixed.
     CheckTrialGroup(trial.trial_name.as_string(), trial.group_name.as_string(),
                     &global_->seen_states_);
+    if (g_use_global_check_states) {
+      CheckTrialGroup(trial.trial_name.as_string(),
+                      trial.group_name.as_string(), &g_seen_states.Get());
+    }
   }
 }
 
@@ -616,10 +639,15 @@ void FieldTrialList::NotifyFieldTrialGroupSelection(FieldTrial* field_trial) {
   if (!field_trial->enable_field_trial_)
     return;
 
+  // TODO(asvitkine): Remove this block when http://crbug.com/359406 is fixed.
   {
     AutoLock auto_lock(global_->lock_);
     CheckTrialGroup(field_trial->trial_name(),
                     field_trial->group_name_internal(), &global_->seen_states_);
+    if (g_use_global_check_states) {
+      CheckTrialGroup(field_trial->trial_name(),
+                      field_trial->group_name_internal(), &g_seen_states.Get());
+    }
   }
   global_->observer_list_->Notify(
       FROM_HERE, &FieldTrialList::Observer::OnFieldTrialGroupFinalized,
