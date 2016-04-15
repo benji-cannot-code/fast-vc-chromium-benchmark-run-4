3a1cc7ada093286bc83951944e1d5a2651b90392
kraush <kraush@amazon.com>
2016-04-15 08:34:05 -0700

    Fix accessibility order of Android tabstrip
    This change alters the accessibility order of the tabstrip
    on Android devices. It contains two changes:
    - "Close tab" button will be selected after the tab itself
      during linear navigation
    - Tabs are selected from left to right when using linear
      navigation, rather than basing the order on which tab is
      currently active
    
    BUG=593111
    
    Review URL: https://codereview.chromium.org/1763713003
    
    Cr-Commit-Position: refs/heads/master@{#387604}

--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -3182,6 +3182,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'target_name': 'chrome_junit_tests',
           'type': 'none',
           'dependencies': [
+            # Allow unit-testing Chrome UI components
+            'android/chrome_apk.gyp:chrome_public_apk_java',
             'chrome_java',
             '../base/base.gyp:base',
             '../base/base.gyp:base_java_test_support',
--- ui/android/java/src/org/chromium/ui/base/LocalizationUtils.java
+++ ui/android/java/src/org/chromium/ui/base/LocalizationUtils.java
@@ -10,6 +10,7 @@ import android.view.View;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ApplicationStatus;
+import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 
@@ -59,6 +60,11 @@ public class LocalizationUtils {
         }
 
         return sIsLayoutRtl.booleanValue();
+    }
+
+    @VisibleForTesting
+    public static void setRtlForTesting(boolean shouldBeRtl) {
+        sIsLayoutRtl = shouldBeRtl;
     }
 
     /**
