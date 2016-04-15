a47955495bb7609bbc1fbaabdc78309f8addab47
ben <ben@chromium.org>
2016-04-15 16:13:43 -0700

    Add catalog viewer app
    R=sky@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1893763002
    
    Cr-Commit-Position: refs/heads/master@{#387740}

---
+++ mash/catalog_viewer/main.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mash/catalog_viewer/catalog_viewer.h"
+#include "mojo/public/c/system/main.h"
+#include "services/shell/public/cpp/application_runner.h"
+
+MojoResult MojoMain(MojoHandle shell_handle) {
+  shell::ApplicationRunner runner(new mash::catalog_viewer::CatalogViewer);
+  return runner.Run(shell_handle);
+}
