9f07cd4bb48ec117bf3261132f92eab494fb9cab
bashi <bashi@chromium.org>
2016-04-14 18:14:49 -0700

    Make code generator understand FrozenArray
    Currently FrozenArray is just an alias of Array. This means that:
    - Converted V8 arrays are not frozen.
    - Returned values are always newly created.
    
    The latter isn't actually a spec violence.
    
    BUG=515920
    
    Review URL: https://codereview.chromium.org/1890733002
    
    Cr-Commit-Position: refs/heads/master@{#387507}

--- third_party/WebKit/Source/bindings/scripts/idl_types.py
+++ third_party/WebKit/Source/bindings/scripts/idl_types.py
@@ -11,6 +11,7 @@ IdlTypeBase
  IdlArrayOrSequenceType
   IdlArrayType
   IdlSequenceType
+  IdlFrozenArrayType
  IdlNullableType
 
 IdlTypes are picklable because we store them in interfaces_info.
@@ -335,11 +336,12 @@ class IdlUnionType(IdlTypeBase):
 
 
 ################################################################################
-# IdlArrayOrSequenceType, IdlArrayType, IdlSequenceType
+# IdlArrayOrSequenceType, IdlArrayType, IdlSequenceType, IdlFrozenArrayType
 ################################################################################
 
+# TODO(bashi): Rename this like "IdlArrayTypeBase" or something.
 class IdlArrayOrSequenceType(IdlTypeBase):
-    """Base class for IdlArrayType and IdlSequenceType."""
+    """Base class for array-like types."""
 
     def __init__(self, element_type):
         super(IdlArrayOrSequenceType, self).__init__()
@@ -397,6 +399,19 @@ class IdlSequenceType(IdlArrayOrSequenceType):
     @property
     def name(self):
         return self.element_type.name + 'Sequence'
+
+
+class IdlFrozenArrayType(IdlArrayOrSequenceType):
+    def __init__(self, element_type):
+        super(IdlFrozenArrayType, self).__init__(element_type)
+
+    def __str__(self):
+        return 'FrozenArray<%s>' % self.element_type
+
+    @property
+    def name(self):
+        return self.element_type.name + 'Array'
+
 
 
 ################################################################################
