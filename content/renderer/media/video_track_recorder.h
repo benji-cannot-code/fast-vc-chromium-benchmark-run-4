9c520f99d6f3f980d6fc4b1d5e243889949d221f
mcasas <mcasas@chromium.org>
2016-04-14 19:29:01 -0700

    VideoTrackRecorder: extract base class Encoder out of VpxEncoder
    Some common functionality is moved to Encoder from
    VpxEncoder. Also VpxEncoder is moved to the anonymous
    namespace.
    
    No new code is added, this CL is pure refactoring.
    
    This is the 2nd step in a series of CLs to add support for
    H264 to MediaRecorder (see https://crrev.com/1886123002/).
    Next step is to add an H264Encoder (<is a> Encoder).
    
    BUG=601636
    
    Review URL: https://codereview.chromium.org/1893493003
    
    Cr-Commit-Position: refs/heads/master@{#387526}

--- content/renderer/media/video_track_recorder.h
+++ content/renderer/media/video_track_recorder.h
@@ -21,11 +21,11 @@ class VideoFrame;
 namespace content {
 
 // VideoTrackRecorder is a MediaStreamVideoSink that encodes the video frames
-// received from a Stream Video Track. The class is constructed and used on a
-// single thread, namely the main Render thread. It has an internal VpxEncoder
-// with its own threading subtleties, see the implementation file. This mirrors
-// the other MediaStreamVideo* classes that are constructed/configured on Main
-// Render thread but that pass frames on Render IO thread.
+// received from a Stream Video Track. This class is constructed and used on a
+// single thread, namely the main Render thread. This mirrors the other
+// MediaStreamVideo* classes that are constructed/configured on Main Render
+// thread but that pass frames on Render IO thread. It has an internal Encoder
+// with its own threading subtleties, see the implementation file.
 class CONTENT_EXPORT VideoTrackRecorder
     : NON_EXPORTED_BASE(public MediaStreamVideoSink) {
  public:
@@ -33,6 +33,7 @@ class CONTENT_EXPORT VideoTrackRecorder
     VP8,
     VP9,
   };
+  class Encoder;
 
   using OnEncodedVideoCB =
       base::Callback<void(const scoped_refptr<media::VideoFrame>& video_frame,
@@ -61,8 +62,7 @@ class CONTENT_EXPORT VideoTrackRecorder
   blink::WebMediaStreamTrack track_;
 
   // Inner class to encode using whichever codec is configured.
-  class VpxEncoder;
-  const scoped_refptr<VpxEncoder> encoder_;
+  const scoped_refptr<Encoder> encoder_;
 
   DISALLOW_COPY_AND_ASSIGN(VideoTrackRecorder);
 };
