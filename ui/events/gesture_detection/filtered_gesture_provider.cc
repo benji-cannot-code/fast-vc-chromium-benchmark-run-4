2fadf17b53f68bab4ebecec9e231107e24d86619
kylechar <kylechar@chromium.org>
2016-04-15 11:55:27 -0700

    Enable GN check for //ui/events/* plus more.
    Working on getting GN check enabled for all of //ui/*. Should just be
    missing //ui/arc/* and //ui/accelerated_widget_mac/* as the last
    unchecked directories.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1888403002
    
    Cr-Commit-Position: refs/heads/master@{#387661}

--- ui/events/gesture_detection/filtered_gesture_provider.cc
+++ ui/events/gesture_detection/filtered_gesture_provider.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "base/logging.h"
-#include "ui/events/blink/blink_event_util.h"
 #include "ui/events/gesture_detection/motion_event.h"
 
 namespace ui {
