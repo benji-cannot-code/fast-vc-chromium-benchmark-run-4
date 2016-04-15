43cb26149bd5b8ca820be8edbe8ab102abace33e
dtseng <dtseng@chromium.org>
2016-04-15 13:15:49 -0700

    Prune invisible aura Windows from the desktop automation tree.
    The specific bug involved the following events:
    - aura::Window notified observers OnWindowDestroying
    - in a separate thread, we get an accessibility event
    - AXWindowObjWrapper removes itself as an observer and deletes itself
    - the accessibility event comes through and we create a new AXWindowobjWrapper with the (about to be) destroyed window. But, we're already done notifying OnWindowDestroying...so this object sticks around.
    - the window gets to the end of its dctor and is done with dctor.
    -the serializer walks the tree and accesses the dealloced window.
    
    Solve this by:
    - listening to OnWindowDestroyed (which gets fired at the end of the aura::Window dctor)
    - ignore invisible Window's. This gets us away from accessing windows that are about to be destroyed or simply irrelevant for automation.
    
    BUG=576607
    TEST=manually verify repro in bug doesn't happen.
    
    Review URL: https://codereview.chromium.org/1888233002
    
    Cr-Commit-Position: refs/heads/master@{#387690}

--- ui/views/accessibility/ax_window_obj_wrapper.cc
+++ ui/views/accessibility/ax_window_obj_wrapper.cc
@@ -36,6 +36,8 @@ void AXWindowObjWrapper::GetChildren(
     std::vector<AXAuraObjWrapper*>* out_children) {
   aura::Window::Windows children = window_->children();
   for (size_t i = 0; i < children.size(); ++i) {
+    if (!children[i]->IsVisible())
+      continue;
     out_children->push_back(
         AXAuraObjCache::GetInstance()->GetOrCreate(children[i]));
   }
@@ -65,7 +67,7 @@ int32_t AXWindowObjWrapper::GetID() {
   return AXAuraObjCache::GetInstance()->GetID(window_);
 }
 
-void AXWindowObjWrapper::OnWindowDestroying(aura::Window* window) {
+void AXWindowObjWrapper::OnWindowDestroyed(aura::Window* window) {
   AXAuraObjCache::GetInstance()->Remove(window);
 }
 
