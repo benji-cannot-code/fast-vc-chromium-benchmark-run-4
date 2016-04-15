7b5d40a080c5192e8999c564ec7da1e95f02f286
shinyak <shinyak@google.com>
2016-04-14 20:16:12 -0700

    Move StringHash.h include to OriginTrialContext.h
    This is a follow-up patch for r386009.
    
    We added StringHash.h include in OriginTrial.h to fix build before.
    However, this is used for HashSet<String>, and actually
    OriginTrialContext.h has HashSet<String>.
    
    So, we should have added StirngHash.h in OriginTrialContext.h
    instead of OriginTrials.h.
    
    BUG=601282
    
    Review URL: https://codereview.chromium.org/1879993002
    
    Cr-Commit-Position: refs/heads/master@{#387535}

--- third_party/WebKit/Source/core/origin_trials/OriginTrialContext.h
+++ third_party/WebKit/Source/core/origin_trials/OriginTrialContext.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/Supplementable.h"
 #include "wtf/HashSet.h"
 #include "wtf/Vector.h"
+#include "wtf/text/StringHash.h"
 #include "wtf/text/WTFString.h"
 
 namespace blink {
