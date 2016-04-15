a2a7590e839b41615fddb5e3914d7fe3b33714dc
hta <hta@chromium.org>
2016-04-15 07:32:37 -0700

    Fix selection of "no audio processing" mode.
    It was only possible to turn it off with a basic (mandatory)
    constraint. It should also be possible to turn it off with an
    advanced (optional) constraint.
    
    BUG=603900
    
    Review URL: https://codereview.chromium.org/1891953003
    
    Cr-Commit-Position: refs/heads/master@{#387597}

--- content/renderer/media/media_stream_audio_processor_options.cc
+++ content/renderer/media/media_stream_audio_processor_options.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "content/common/media/media_stream_options.h"
+#include "content/renderer/media/media_stream_constraints_util.h"
 #include "content/renderer/media/media_stream_source.h"
 #include "media/audio/audio_parameters.h"
 #include "third_party/webrtc/modules/audio_processing/include/audio_processing.h"
@@ -172,9 +173,12 @@ MediaAudioConstraints::MediaAudioConstraints(
   // - gUM has a specific kMediaStreamSource, which is used by tab capture
   //   and screen capture.
   // - |kEchoCancellation| is explicitly set to false.
-  std::string value_str;
+  bool echo_constraint;
   if (!constraints.basic().mediaStreamSource.isEmpty() ||
-      !constraints.basic().echoCancellation.matches(true)) {
+      (GetConstraintValueAsBoolean(
+           constraints, &blink::WebMediaTrackConstraintSet::echoCancellation,
+           &echo_constraint) &&
+       echo_constraint == false)) {
     default_audio_processing_constraint_value_ = false;
   }
 }
