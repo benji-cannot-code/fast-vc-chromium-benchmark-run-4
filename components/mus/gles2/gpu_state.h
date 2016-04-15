02680b4cc22f8b6e9f6ac9a8184f90aa5c13ae40
cwallez <cwallez@chromium.org>
2016-04-15 06:29:34 -0700

    Revert of Compute GpuDriverBugWorkarounds only one time in the GPU process (patchset #16 id:300001 of https://codereview.chromium.org/1871613002/ )
    Reason for revert:
    This commit breaks the gles2_conform tests on all the GPU FYI Linux and Mac bots, see https://build.chromium.org/p/chromium.gpu.fyi/builders/Linux%20Release%20%28New%20Intel%29/builds/758/steps/gles2_conform_test/logs/stdio
    
    Original issue's description:
    > Compute GpuDriverBugWorkarounds only one time in the GPU process
    >
    > Previously the extraction of "--gpu-driver-bug-workarounds" from the
    > command-line was done in FeatureInfo's constructor. Since a new FeatureInfo
    > is created for each ContextGroup this extraction was done again whereas
    > the result is constant.
    >
    > This CL moved owner ship of Workarounds from FeatureInfo to
    > InProcessCommandBuffer when running in gpu process mode. But moved
    > to GpuChannelManager otherwise.
    >
    > class GpuDriverBugWorkarounds has been added within new files
    > gpu_driver_bug_workarounds.h/.cc.
    >
    > BUG=599964
    >
    > R=kbr@chromium.org, piman@chromium.org, rjkroege@chromium.org, zmo@chromium.org
    > CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    >
    > Committed: https://crrev.com/dab49af5278797db0a8374f2a7f06c6c33b8158b
    > Cr-Commit-Position: refs/heads/master@{#387569}
    
    TBR=kbr@chromium.org,piman@chromium.org,rjkroege@chromium.org,zmo@chromium.org,ben@chromium.org,cpu@chromium.org,j.isorce@samsung.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=599964
    
    Review URL: https://codereview.chromium.org/1892483004
    
    Cr-Commit-Position: refs/heads/master@{#387588}

--- components/mus/gles2/gpu_state.h
+++ components/mus/gles2/gpu_state.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/gpu_preferences.h"
 #include "gpu/command_buffer/service/mailbox_manager_impl.h"
 #include "gpu/command_buffer/service/sync_point_manager.h"
-#include "gpu/config/gpu_driver_bug_workarounds.h"
 #include "gpu/config/gpu_info.h"
 #include "ui/gl/gl_share_group.h"
 
@@ -42,10 +41,6 @@ class GpuState : public base::RefCountedThreadSafe<GpuState> {
 
   const gpu::GpuPreferences& gpu_preferences() const {
     return gpu_preferences_;
-  }
-
-  const gpu::GpuDriverBugWorkarounds& gpu_driver_bug_workarounds() const {
-    return gpu_driver_bug_workarounds_;
   }
 
   CommandBufferTaskRunner* command_buffer_task_runner() const {
@@ -92,7 +87,6 @@ class GpuState : public base::RefCountedThreadSafe<GpuState> {
   scoped_refptr<base::SingleThreadTaskRunner> control_thread_task_runner_;
 
   gpu::GpuPreferences gpu_preferences_;
-  const gpu::GpuDriverBugWorkarounds gpu_driver_bug_workarounds_;
   scoped_refptr<CommandBufferTaskRunner> command_buffer_task_runner_;
   scoped_ptr<CommandBufferDriverManager> driver_manager_;
   scoped_ptr<gpu::SyncPointManager> sync_point_manager_;
