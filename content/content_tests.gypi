ede648eeb9183809db0ba8b78d7faa748aef7452
guidou <guidou@chromium.org>
2016-04-15 12:00:59 -0700

    Prevent incorrect garbage collection of RTCDataChannel.
    BUG=405545
    
    Review URL: https://codereview.chromium.org/1880073005
    
    Cr-Commit-Position: refs/heads/master@{#387665}

--- content/content_tests.gypi
+++ content/content_tests.gypi
@@ -304,6 +304,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     'content_browsertests_webrtc_sources': [
       'browser/media/webrtc/webrtc_audio_debug_recordings_browsertest.cc',
       'browser/media/webrtc/webrtc_browsertest.cc',
+      'browser/media/webrtc/webrtc_datachannel_browsertest.cc',
       'browser/media/webrtc/webrtc_getusermedia_browsertest.cc',
       'browser/media/webrtc/webrtc_internals_browsertest.cc',
       'browser/media/webrtc/webrtc_ip_permissions_browsertest.cc',
--- third_party/WebKit/Source/modules/mediastream/RTCDataChannel.h
+++ third_party/WebKit/Source/modules/mediastream/RTCDataChannel.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define RTCDataChannel_h
 
 #include "base/gtest_prod_util.h"
+#include "bindings/core/v8/ActiveScriptWrappable.h"
+#include "core/dom/ActiveDOMObject.h"
 #include "modules/EventTargetModules.h"
 #include "platform/Timer.h"
 #include "platform/heap/Handle.h"
@@ -47,7 +49,10 @@ struct WebRTCDataChannelInit;
 
 class MODULES_EXPORT RTCDataChannel final
     : public EventTargetWithInlineData
-    , WTF_NON_EXPORTED_BASE(public WebRTCDataChannelHandlerClient) {
+    , WTF_NON_EXPORTED_BASE(public WebRTCDataChannelHandlerClient)
+    , public ActiveScriptWrappable
+    , public ActiveDOMObject {
+    USING_GARBAGE_COLLECTED_MIXIN(RTCDataChannel);
     DEFINE_WRAPPERTYPEINFO();
     USING_PRE_FINALIZER(RTCDataChannel, dispose);
 public:
@@ -94,6 +99,14 @@ public:
     const AtomicString& interfaceName() const override;
     ExecutionContext* getExecutionContext() const override;
 
+    // ActiveDOMObject
+    void suspend() override;
+    void resume() override;
+    void stop() override;
+
+    // ActiveScriptWrappable
+    bool hasPendingActivity() const override;
+
     DECLARE_VIRTUAL_TRACE();
 
     // WebRTCDataChannelHandlerClient
@@ -110,8 +123,6 @@ private:
     void scheduleDispatchEvent(Event*);
     void scheduledEventTimerFired(Timer<RTCDataChannel>*);
 
-    Member<ExecutionContext> m_executionContext;
-
     OwnPtr<WebRTCDataChannelHandler> m_handler;
 
     WebRTCDataChannelHandlerClient::ReadyState m_readyState;
@@ -126,6 +137,8 @@ private:
     HeapVector<Member<Event>> m_scheduledEvents;
 
     unsigned m_bufferedAmountLowThreshold;
+
+    bool m_stopped;
 
     FRIEND_TEST_ALL_PREFIXES(RTCDataChannelTest, BufferedAmountLow);
 };
