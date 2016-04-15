a6d7676430a88acdd16077ec1c72e7f174333838
twellington <twellington@chromium.org>
2016-04-15 10:46:51 -0700

    Remove experiment for disable auto-hiding toolbar
    This feature should be on by default. Removing the experiment code
    for disabling auuto-hiding the toolbar.
    
    BUG=594670
    
    Review URL: https://codereview.chromium.org/1889573002
    
    Cr-Commit-Position: refs/heads/master@{#387636}

--- chrome/android/java/src/org/chromium/chrome/browser/device/DeviceClassManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/device/DeviceClassManager.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.device;
 
 import android.content.Context;
-import android.text.TextUtils;
 import android.view.accessibility.AccessibilityManager;
 
 import org.chromium.base.ApplicationStatus;
@@ -14,7 +13,6 @@ import org.chromium.base.CommandLine;
 import org.chromium.base.SysUtils;
 import org.chromium.base.TraceEvent;
 import org.chromium.chrome.browser.ChromeSwitches;
-import org.chromium.components.variations.VariationsAssociatedData;
 import org.chromium.ui.base.DeviceFormFactor;
 
 /**
@@ -22,11 +20,7 @@ import org.chromium.ui.base.DeviceFormFactor;
  * devices.
  */
 public class DeviceClassManager {
-    private static final String DISABLE_AUTO_HIDING_FIELD_TRIAL_NAME = "DisableAutoHidingToolbar";
-    private static final String DISABLE_AUTO_HIDING_TOOLBAR_THRESHOLD_COMMAND =
-            "disable-auto-hiding-toolbar-threshold";
-    private static final String DISABLE_AUTO_HIDING_TOOLBAR_THRESHOLD_PARAM =
-            "disable_auto_hiding_toolbar_threshold";
+    private static final int DISABLE_AUTO_HIDING_TOOLBAR_THRESHOLD = 800;
 
     private static DeviceClassManager sInstance;
 
@@ -169,23 +163,9 @@ public class DeviceClassManager {
     public static boolean isAutoHidingToolbarDisabled(Context context) {
         if (getInstance().mDisableAutoHidingToolbar == null) {
             getInstance().mDisableAutoHidingToolbar = false;
-            String value = CommandLine.getInstance().getSwitchValue(
-                    DISABLE_AUTO_HIDING_TOOLBAR_THRESHOLD_COMMAND);
-            if (TextUtils.isEmpty(value)) {
-                value = VariationsAssociatedData.getVariationParamValue(
-                        DISABLE_AUTO_HIDING_FIELD_TRIAL_NAME,
-                        DISABLE_AUTO_HIDING_TOOLBAR_THRESHOLD_PARAM);
-            }
-            if (!TextUtils.isEmpty(value)) {
-                try {
-                    int threshold = Integer.parseInt(value);
-                    int smallestScreenDp =
-                            context.getResources().getConfiguration().smallestScreenWidthDp;
-                    if (smallestScreenDp >= threshold) {
-                        getInstance().mDisableAutoHidingToolbar = true;
-                    }
-                } catch (NumberFormatException e) {
-                }
+            int smallestScreenDp = context.getResources().getConfiguration().smallestScreenWidthDp;
+            if (smallestScreenDp >= DISABLE_AUTO_HIDING_TOOLBAR_THRESHOLD) {
+                getInstance().mDisableAutoHidingToolbar = true;
             }
         }
         return getInstance().mDisableAutoHidingToolbar;
