6458035fa95e5eabe8bcfe182eee2cf6553453c5
ellyjones <ellyjones@chromium.org>
2016-04-18 12:45:38 -0700

    views: support vertical-drag-to-end on textfields
    On Mac, dragging above the top of a textfield drags to the beginning of the
    textfield's contents, and dragging below the bottom drags to the end of the
    textfield's contents. This change implements that behavior, controlled by
    PlatformStyle::kTextfieldDragVerticallyDragsToEnd, which is true only on Mac.
    
    BUG=600166
    
    Review URL: https://codereview.chromium.org/1865063004
    
    Cr-Commit-Position: refs/heads/master@{#387988}

--- ui/views/controls/textfield/textfield.cc
+++ ui/views/controls/textfield/textfield.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/metrics.h"
 #include "ui/views/native_cursor.h"
 #include "ui/views/painter.h"
+#include "ui/views/style/platform_style.h"
 #include "ui/views/views_delegate.h"
 #include "ui/views/widget/widget.h"
 
@@ -1747,7 +1748,18 @@ void Textfield::MoveCursorTo(const gfx::Point& point, bool select) {
 
 void Textfield::SelectThroughLastDragLocation() {
   OnBeforeUserAction();
-  model_->MoveCursorTo(last_drag_location_, true);
+
+  const bool drags_to_end = PlatformStyle::kTextfieldDragVerticallyDragsToEnd;
+  if (drags_to_end && last_drag_location_.y() < 0) {
+    model_->MoveCursor(gfx::BreakType::LINE_BREAK,
+                       gfx::VisualCursorDirection::CURSOR_LEFT, true);
+  } else if (drags_to_end && last_drag_location_.y() > height()) {
+    model_->MoveCursor(gfx::BreakType::LINE_BREAK,
+                       gfx::VisualCursorDirection::CURSOR_RIGHT, true);
+  } else {
+    model_->MoveCursorTo(last_drag_location_, true);
+  }
+
   if (aggregated_clicks_ == 1) {
     model_->SelectWord();
     // Expand the selection so the initially selected word remains selected.
