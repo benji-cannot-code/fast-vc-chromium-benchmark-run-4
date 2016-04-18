8468abba3735e51253856a7f8a5c4a5a4a5dba2e
pilgrim <pilgrim@chromium.org>
2016-04-17 21:25:54 -0700

    [Layout API] Convert DragCaretController to use Document::layoutViewItem()
    There was one reference to Document::layoutView() (clearSelection() method)
    which is deprecated by the new layout API. I converted it to use
    layoutViewItem() instead. No new API methods were required.
    
    There are no functional changes.
    
    BUG=499321
    
    Review URL: https://codereview.chromium.org/1894583002
    
    Cr-Commit-Position: refs/heads/master@{#387857}

--- third_party/WebKit/Source/core/editing/DragCaretController.cpp
+++ third_party/WebKit/Source/core/editing/DragCaretController.cpp
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/DragCaretController.h"
 
 #include "core/editing/EditingUtilities.h"
-#include "core/layout/LayoutView.h"
+#include "core/layout/api/LayoutViewItem.h"
 #include "core/paint/PaintLayer.h"
 
 namespace blink {
@@ -93,7 +93,7 @@ void DragCaretController::nodeWillBeRemoved(Node& node)
     if (!removingNodeRemovesPosition(node, m_position.deepEquivalent()))
         return;
 
-    m_position.deepEquivalent().document()->layoutView()->clearSelection();
+    m_position.deepEquivalent().document()->layoutViewItem().clearSelection();
     clear();
 }
 
