b004d356bba97bc75e73c3f84f339ca04ffa498c
jbroman <jbroman@chromium.org>
2016-04-18 13:59:38 -0700

    GN: Use std::unique_ptr in Loader::ToolchainRecordMap.
    BUG=602726
    
    Review URL: https://codereview.chromium.org/1878353002
    
    Cr-Commit-Position: refs/heads/master@{#388015}

--- tools/gn/loader.h
+++ tools/gn/loader.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define TOOLS_GN_LOADER_H_
 
 #include <map>
+#include <memory>
 #include <set>
 
 #include "base/callback.h"
@@ -174,8 +175,7 @@ class LoaderImpl : public Loader {
   Label default_toolchain_label_;
 
   // Records for the build config file loads.
-  // Owning pointers.
-  typedef std::map<Label, ToolchainRecord*> ToolchainRecordMap;
+  typedef std::map<Label, std::unique_ptr<ToolchainRecord>> ToolchainRecordMap;
   ToolchainRecordMap toolchain_records_;
 };
 
