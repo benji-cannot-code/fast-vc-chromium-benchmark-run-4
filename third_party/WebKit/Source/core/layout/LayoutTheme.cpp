0247615dfb39ac3fe21f413734cf4b2d120858c6
tkent <tkent@chromium.org>
2016-04-18 03:41:37 -0700

    input[type=range]: box-shadow should not make -webkit-appearance none.
    Native rendering of input[type=range] with box-shadow looks bad, but web authors
    are responsible for this bad rendering.  we don't need to remove
    -webkit-appearance.
    
    BUG=442484
    
    Review URL: https://codereview.chromium.org/1895723002
    
    Cr-Commit-Position: refs/heads/master@{#387885}

--- third_party/WebKit/Source/core/layout/LayoutTheme.cpp
+++ third_party/WebKit/Source/core/layout/LayoutTheme.cpp
@@ -407,10 +407,6 @@ bool LayoutTheme::isControlStyled(const ComputedStyle& style) const
     case TextFieldPart:
         return style.hasAuthorBackground() || style.hasAuthorBorder() || style.boxShadow();
 
-    case SliderHorizontalPart:
-    case SliderVerticalPart:
-        return style.boxShadow();
-
     default:
         return false;
     }
