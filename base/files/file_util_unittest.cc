9802bd9250c66c05900bf57dc05362b5efd2305c
wfh <wfh@chromium.org>
2016-04-18 12:17:21 -0700

    Add base::IsOnNetworkDrive.
    BUG=572131
    TEST=base_unittests
    TEST=http://pastebin.com/raw/jNUNwiQe
    
    Review URL: https://codereview.chromium.org/1892153003
    
    Cr-Commit-Position: refs/heads/master@{#387981}

--- base/files/file_util_unittest.cc
+++ base/files/file_util_unittest.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <shlobj.h>
 #include <tchar.h>
 #include <winioctl.h>
+#include "base/environment.h"
 #include "base/win/scoped_handle.h"
 #include "base/win/windows_version.h"
 #endif
@@ -1663,6 +1664,63 @@ TEST_F(FileUtilTest, GetTempDirTest) {
     free(original_tmp);
   } else {
     ::_tputenv_s(kTmpKey, _T(""));
+  }
+}
+
+TEST_F(FileUtilTest, IsOnNetworkDrive) {
+  struct LocalTestData {
+    const FilePath::CharType* input;
+    bool expected;
+  };
+
+  const LocalTestData local_cases[] = {
+    { FPL(""),                               false },
+    { FPL("c:\\"),                           false },
+    { FPL("c:"),                             false },
+    { FPL("c:\\windows\\notepad.exe"),       false }
+  };
+
+  for (const auto& test_case : local_cases) {
+    FilePath input(test_case.input);
+    bool observed = IsOnNetworkDrive(input);
+    EXPECT_EQ(test_case.expected, observed) << " input: " << input.value();
+  }
+
+  Environment* env = Environment::Create();
+  ASSERT_TRUE(!!env);
+
+  // To test IsOnNetworkDrive() for remote cases, set up a file server
+  // and place a file called file.txt on the server e.g.
+  // \\DC01\TESTSHARE\file.txt
+  // then set the two environment variables:
+  // set BASE_TEST_FILE_SERVER=DC01
+  // set BASE_TEST_FILE_SHARE=TESTSHARE
+  if (!env->HasVar("BASE_TEST_FILE_SERVER") ||
+      !env->HasVar("BASE_TEST_FILE_SHARE")) {
+    return;
+  }
+
+  struct NetworkTestData {
+    const wchar_t* input;
+    bool expected;
+  };
+
+  const NetworkTestData network_cases[] = {
+    { L"\\\\%BASE_TEST_FILE_SERVER%",                                   false },
+    { L"\\\\%BASE_TEST_FILE_SERVER%\\",                                 false },
+    { L"\\\\%BASE_TEST_FILE_SERVER%\\file.txt",                         false },
+    { L"\\\\%BASE_TEST_FILE_SERVER%\\%BASE_TEST_FILE_SHARE%",           true },
+    { L"\\\\%BASE_TEST_FILE_SERVER%\\%BASE_TEST_FILE_SHARE%\\",         true },
+    { L"\\\\%BASE_TEST_FILE_SERVER%\\%BASE_TEST_FILE_SHARE%\\file.txt", true },
+    { L"\\\\%BASE_TEST_FILE_SERVER%\\%BASE_TEST_FILE_SHARE%\\no.txt",   false }
+  };
+
+  for (const auto& test_case : network_cases) {
+    wchar_t path[MAX_PATH] = {0};
+    ::ExpandEnvironmentStringsW(test_case.input, path, arraysize(path));
+    FilePath input(path);
+    EXPECT_EQ(test_case.expected, IsOnNetworkDrive(input)) << " input : "
+                                                           << input.value();
   }
 }
 #endif  // OS_WIN
