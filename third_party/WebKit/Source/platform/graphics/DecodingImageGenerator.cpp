1dd79a0750d457a0b67753cf01e4213892f48267
tomhudson <tomhudson@google.com>
2016-04-18 14:30:43 -0700

    Undefine SK_SUPPORT_LEGACY_REFENCODEDDATA_NOCTX
    Adds missing parameter to DecodingImageGenerator::onRefEncodedData().
    That's all we needed to do to get rid of this legacy flag in Chrome.
    
    Review URL: https://codereview.chromium.org/1892573002
    
    Cr-Commit-Position: refs/heads/master@{#388036}

--- third_party/WebKit/Source/platform/graphics/DecodingImageGenerator.cpp
+++ third_party/WebKit/Source/platform/graphics/DecodingImageGenerator.cpp
@@ -50,7 +50,7 @@ DecodingImageGenerator::~DecodingImageGenerator()
 {
 }
 
-SkData* DecodingImageGenerator::onRefEncodedData()
+SkData* DecodingImageGenerator::onRefEncodedData(GrContext* ctx)
 {
     TRACE_EVENT0("blink", "DecodingImageGenerator::refEncodedData");
 
