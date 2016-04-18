2bff1a542e043f58583e47adc5c7bab15846ff23
jbroman <jbroman@chromium.org>
2016-04-18 13:14:08 -0700

    GN: Use std::unique_ptr for owned pointers in ImportManager.
    BUG=602726
    
    Review URL: https://codereview.chromium.org/1886453002
    
    Cr-Commit-Position: refs/heads/master@{#387997}

--- tools/gn/import_manager.cc
+++ tools/gn/import_manager.cc
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "tools/gn/import_manager.h"
 
-#include <memory>
-
-#include "base/stl_util.h"
 #include "tools/gn/parse_tree.h"
 #include "tools/gn/scheduler.h"
 #include "tools/gn/scope_per_file_provider.h"
@@ -15,10 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Returns a newly-allocated scope on success, null on failure.
-Scope* UncachedImport(const Settings* settings,
-                      const SourceFile& file,
-                      const ParseNode* node_for_err,
-                      Err* err) {
+std::unique_ptr<Scope> UncachedImport(const Settings* settings,
+                                      const SourceFile& file,
+                                      const ParseNode* node_for_err,
+                                      Err* err) {
   const ParseNode* node = g_scheduler->input_file_manager()->SyncLoadFile(
       node_for_err->GetRange(), settings->build_settings(), file, err);
   if (!node)
@@ -38,7 +35,7 @@ Scope* UncachedImport(const Settings* settings,
     return nullptr;
   scope->ClearProcessingImport();
 
-  return scope.release();
+  return scope;
 }
 
 }  // namesapce
@@ -47,7 +44,6 @@ ImportManager::ImportManager() {
 }
 
 ImportManager::~ImportManager() {
-  STLDeleteContainerPairSecondPointers(imports_.begin(), imports_.end());
 }
 
 bool ImportManager::DoImport(const SourceFile& file,
@@ -61,14 +57,14 @@ bool ImportManager::DoImport(const SourceFile& file,
     base::AutoLock lock(lock_);
     ImportMap::const_iterator found = imports_.find(file);
     if (found != imports_.end())
-      imported_scope = found->second;
+      imported_scope = found->second.get();
   }
 
   if (!imported_scope) {
     // Do a new import of the file.
-    imported_scope = UncachedImport(scope->settings(), file,
-                                    node_for_err, err);
-    if (!imported_scope)
+    std::unique_ptr<Scope> new_imported_scope =
+        UncachedImport(scope->settings(), file, node_for_err, err);
+    if (!new_imported_scope)
       return false;
 
     // We loaded the file outside the lock. This means that there could be a
@@ -78,10 +74,10 @@ bool ImportManager::DoImport(const SourceFile& file,
       base::AutoLock lock(lock_);
       ImportMap::const_iterator found = imports_.find(file);
       if (found != imports_.end()) {
-        delete imported_scope;
-        imported_scope = found->second;
+        imported_scope = found->second.get();
       } else {
-        imports_[file] = imported_scope;
+        imported_scope = new_imported_scope.get();
+        imports_[file] = std::move(new_imported_scope);
       }
     }
   }
--- tools/gn/import_manager.h
+++ tools/gn/import_manager.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define TOOLS_GN_IMPORT_MANAGER_H_
 
 #include <map>
+#include <memory>
 
 #include "base/macros.h"
 #include "base/synchronization/lock.h"
@@ -34,7 +35,7 @@ class ImportManager {
   base::Lock lock_;
 
   // Owning pointers to the scopes.
-  typedef std::map<SourceFile, const Scope*> ImportMap;
+  typedef std::map<SourceFile, std::unique_ptr<const Scope>> ImportMap;
   ImportMap imports_;
 
   DISALLOW_COPY_AND_ASSIGN(ImportManager);
