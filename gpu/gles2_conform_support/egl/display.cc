3335c5c1f3c03384bac1bbf61c837b9139876e90
j.isorce <j.isorce@samsung.com>
2016-04-18 07:11:29 -0700

    Compute GpuDriverBugWorkarounds only one time in the GPU process
    Previously the extraction of "--gpu-driver-bug-workarounds" from the
    command-line was done in FeatureInfo's constructor. Since a new FeatureInfo
    is created for each ContextGroup this extraction was done again whereas
    the result is constant.
    
    This CL moved owner ship of Workarounds from FeatureInfo to
    InProcessCommandBuffer when running in gpu process mode. But moved
    to GpuChannelManager otherwise.
    
    class GpuDriverBugWorkarounds has been added within new files
    gpu_driver_bug_workarounds.h/.cc.
    
    BUG=599964
    
    R=kbr@chromium.org, piman@chromium.org, rjkroege@chromium.org, zmo@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Committed: https://crrev.com/dab49af5278797db0a8374f2a7f06c6c33b8158b
    Cr-Commit-Position: refs/heads/master@{#387569}
    
    Review URL: https://codereview.chromium.org/1871613002
    
    Cr-Commit-Position: refs/heads/master@{#387899}

--- gpu/gles2_conform_support/egl/display.cc
+++ gpu/gles2_conform_support/egl/display.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/gles2_conform_support/egl/display.h"
 
+#include "base/command_line.h"
 #include "gpu/gles2_conform_support/egl/config.h"
 #include "gpu/gles2_conform_support/egl/context.h"
 #include "gpu/gles2_conform_support/egl/surface.h"
@@ -16,7 +17,8 @@ Display::Display()
     : is_initialized_(false),
       next_create_window_surface_creates_pbuffer_(false),
       window_surface_pbuffer_width_(0),
-      window_surface_pbuffer_height_(0) {}
+      window_surface_pbuffer_height_(0),
+      gpu_driver_bug_workarounds_(base::CommandLine::ForCurrentProcess()) {}
 
 Display::~Display() {
   surfaces_.clear();
