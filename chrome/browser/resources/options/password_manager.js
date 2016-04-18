47ec423b703ddeab46f8bc1d9dbc8a31221bf610
Dan Beam <dbeam@chromium.org>
2016-04-18 16:25:02 -0700

    Fix closure compile
    R=xunlu@chromium.org
    TBR=xunlu@chromium.org
    
    Review URL: https://codereview.chromium.org/1895723003 .
    
    Cr-Commit-Position: refs/heads/master@{#388073}

--- chrome/browser/resources/options/password_manager.js
+++ chrome/browser/resources/options/password_manager.js
@@ -288,6 +288,7 @@ cr.define('options', function() {
       $('manage-passwords-span').hidden = !visible;
     },
 
+    /** @private */
     showImportExportButton_: function() {
       $('password-manager-import').hidden = false;
       $('password-manager-export').hidden = false;
