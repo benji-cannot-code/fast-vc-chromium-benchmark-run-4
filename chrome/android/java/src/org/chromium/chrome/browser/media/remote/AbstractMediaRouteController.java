f46dc16dde666dc11687d84f7c9f9c7a890cbf38
avayvod <avayvod@chromium.org>
2016-04-18 06:10:21 -0700

    [Cast, Android] Use elapsedRealtime vs uptimeMillis for end of playback UMA
    BUG=604345
    
    Review URL: https://codereview.chromium.org/1894013003
    
    Cr-Commit-Position: refs/heads/master@{#387896}

--- chrome/android/java/src/org/chromium/chrome/browser/media/remote/AbstractMediaRouteController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/remote/AbstractMediaRouteController.java
@@ -405,7 +405,7 @@ public abstract class AbstractMediaRouteController implements MediaRouteControll
     }
 
     private void recordEndOfSessionUMA() {
-        long remotePlaybackStoppedTimestampMs = SystemClock.uptimeMillis();
+        long remotePlaybackStoppedTimestampMs = SystemClock.elapsedRealtime();
 
         // There was no media element ever...
         if (mMediaElementAttachedTimestampMs == 0) return;
@@ -474,12 +474,12 @@ public abstract class AbstractMediaRouteController implements MediaRouteControll
     public void setMediaStateListener(MediaStateListener mediaStateListener) {
         if (mMediaStateListener != null && mediaStateListener == null
                     && mMediaElementAttachedTimestampMs != 0) {
-            mMediaElementDetachedTimestampMs = SystemClock.uptimeMillis();
+            mMediaElementDetachedTimestampMs = SystemClock.elapsedRealtime();
         } else if (mMediaStateListener == null && mediaStateListener != null) {
             // We're switching the videos so let's record the UMA for the previous one.
             if (mMediaElementDetachedTimestampMs != 0) recordEndOfSessionUMA();
 
-            mMediaElementAttachedTimestampMs = SystemClock.uptimeMillis();
+            mMediaElementAttachedTimestampMs = SystemClock.elapsedRealtime();
             mMediaElementDetachedTimestampMs = 0;
         }
 
