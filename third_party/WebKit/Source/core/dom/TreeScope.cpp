86f60f5eb29d28ec9a2defa3322d16e6db163524
pilgrim <pilgrim@chromium.org>
2016-04-17 19:43:55 -0700

    [Layout API] Convert TreeScope to use Document::layoutViewItem()
    There were several references to Document::layoutView()
    which is deprecated by the new layout API. I converted them to use
    layoutViewItem() instead. No new API methods were required.
    
    There are no functional changes.
    
    BUG=499321
    
    Review URL: https://codereview.chromium.org/1889813005
    
    Cr-Commit-Position: refs/heads/master@{#387846}

--- third_party/WebKit/Source/core/dom/TreeScope.cpp
+++ third_party/WebKit/Source/core/dom/TreeScope.cpp
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLLabelElement.h"
 #include "core/html/HTMLMapElement.h"
 #include "core/layout/HitTestResult.h"
-#include "core/layout/LayoutView.h"
+#include "core/layout/api/LayoutViewItem.h"
 #include "core/page/FocusController.h"
 #include "core/page/Page.h"
 #include "wtf/Vector.h"
@@ -231,7 +231,7 @@ HitTestResult hitTestInDocument(const Document* document, int x, int y, const Hi
         return HitTestResult();
 
     HitTestResult result(request, hitPoint);
-    document->layoutView()->hitTest(result);
+    document->layoutViewItem().hitTest(result);
     return result;
 }
 
@@ -299,7 +299,7 @@ HeapVector<Member<Element>> TreeScope::elementsFromPoint(int x, int y) const
 
     HitTestRequest request(HitTestRequest::ReadOnly | HitTestRequest::Active | HitTestRequest::ListBased | HitTestRequest::PenetratingList);
     HitTestResult result(request, hitPoint);
-    document.layoutView()->hitTest(result);
+    document.layoutViewItem().hitTest(result);
 
     return elementsFromHitTestResult(result);
 }
