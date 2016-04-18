365348dcad5982fddee617356b2637d082c8145a
hlundin <hlundin@chromium.org>
2016-04-18 16:39:44 -0700

    Add aec-refined-adaptive-filter command line switch
    The acoustic echo canceler (AEC) in WebRTC sometimes end up in bad
    states. A new tuning was added to WebRTC, and should be tested on
    selected platforms. This CL adds a command line switch to turn this
    new tuning on or off, facilitating the testing.
    
    Tuning added to WebRTC: https://chromium.googlesource.com/external/webrtc.git/+/0332c2db39d6f5c780ce9e92b850bcb57e24e7f8
    
    Tuning rolled into Chrome: https://crrev.com/8cf9068d949adc9e5e5f13346f776520cbb29c4d
    
    BUG=603411,webrtc:5777,webrtc:5778
    
    Review URL: https://codereview.chromium.org/1894963002
    
    Cr-Commit-Position: refs/heads/master@{#388078}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1390,6 +1390,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
   // with any associated values) if present in the browser command line.
   static const char* const kSwitchNames[] = {
     switches::kAgcStartupMinVolume,
+    switches::kAecRefinedAdaptiveFilter,
     switches::kAllowLoopbackInPeerConnection,
     switches::kAndroidFontsPath,
     switches::kAudioBufferSize,
