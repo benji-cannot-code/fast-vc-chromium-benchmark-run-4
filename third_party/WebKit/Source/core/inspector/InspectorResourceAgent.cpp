fecb150b6419d901d4d8446c2fd664499e547a97
pfeldman <pfeldman@chromium.org>
2016-04-19 13:22:40 -0700

    DevTools: introduce the bypass service worker checkbox.
    BUG=601263
    
    Review URL: https://codereview.chromium.org/1872673002
    
    Cr-Commit-Position: refs/heads/master@{#388296}

--- third_party/WebKit/Source/core/inspector/InspectorResourceAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorResourceAgent.cpp
@@ -86,6 +86,7 @@ namespace ResourceAgentState {
 static const char resourceAgentEnabled[] = "resourceAgentEnabled";
 static const char extraRequestHeaders[] = "extraRequestHeaders";
 static const char cacheDisabled[] = "cacheDisabled";
+static const char bypassServiceWorker[] = "bypassServiceWorker";
 static const char userAgentOverride[] = "userAgentOverride";
 static const char monitoringXHR[] = "monitoringXHR";
 static const char blockedURLs[] = "blockedURLs";
@@ -525,6 +526,8 @@ void InspectorResourceAgent::willSendRequest(LocalFrame* frame, unsigned long id
         request.setCachePolicy(WebCachePolicy::BypassingCache);
         request.setShouldResetAppCache(true);
     }
+    if (m_state->booleanProperty(ResourceAgentState::bypassServiceWorker, false))
+        request.setSkipServiceWorker(true);
 
     willSendRequestInternal(frame, identifier, loader, request, redirectResponse, initiatorInfo);
 
@@ -1056,6 +1059,11 @@ void InspectorResourceAgent::setCacheDisabled(ErrorString*, bool cacheDisabled)
     m_state->setBoolean(ResourceAgentState::cacheDisabled, cacheDisabled);
     if (cacheDisabled)
         memoryCache()->evictResources();
+}
+
+void InspectorResourceAgent::setBypassServiceWorker(ErrorString*, bool bypass)
+{
+    m_state->setBoolean(ResourceAgentState::bypassServiceWorker, bypass);
 }
 
 void InspectorResourceAgent::emulateNetworkConditions(ErrorString*, bool, double, double, double)
