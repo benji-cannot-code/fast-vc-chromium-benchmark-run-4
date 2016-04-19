f2c4eed7c7e35fd0da9c8650db123dd98c4d9d42
jbroman <jbroman@chromium.org>
2016-04-19 13:23:30 -0700

    WTF: Implement explicit RefPtr::operator bool.
    This is the modern alternative to the "safe bool" idiom. Some changes at
    call sites are required, primarily because returning a boolean does not
    cause an explicit conversion.
    
    Review URL: https://codereview.chromium.org/1891473002
    
    Cr-Commit-Position: refs/heads/master@{#388297}

--- third_party/WebKit/Source/platform/fonts/shaping/CachingWordShapeIterator.h
+++ third_party/WebKit/Source/platform/fonts/shaping/CachingWordShapeIterator.h
@@ -66,7 +66,7 @@ public:
                 return false;
             *wordResult = shapeWord(m_textRun, m_font);
             m_startIndex = 1;
-            return *wordResult;
+            return wordResult->get();
         }
 
         return nextWord(wordResult);
@@ -175,7 +175,7 @@ private:
             *result = shapeWord(subRun, m_font);
         }
         m_startIndex = endIndex;
-        return *result;
+        return result->get();
     }
 
     unsigned endIndexUntil(UChar ch)
--- third_party/WebKit/Source/wtf/typed_arrays/ArrayBufferView.h
+++ third_party/WebKit/Source/wtf/typed_arrays/ArrayBufferView.h
@@ -53,9 +53,9 @@ public:
     virtual ViewType type() const = 0;
     const char* typeName();
 
-    PassRefPtr<ArrayBuffer> buffer() const
+    ArrayBuffer* buffer() const
     {
-        return m_buffer;
+        return m_buffer.get();
     }
 
     void* baseAddress() const
