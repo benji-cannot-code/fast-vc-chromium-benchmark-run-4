bb3fc4c96e9c12de3996894d859bc8a0b65f8419
fs <fs@opera.com>
2016-04-19 15:48:05 -0700

    Pass first child to SVGLayoutSupport::layoutChildren
    Since everything depending on the "subtree root" has now been hoisted
    into the callers, we can pass firstChild() instead and avoid calling
    slowFirstChild().
    
    BUG=603956
    
    Review URL: https://codereview.chromium.org/1897273002
    
    Cr-Commit-Position: refs/heads/master@{#388337}

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGRoot.cpp
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGRoot.cpp
@@ -162,7 +162,7 @@ void LayoutSVGRoot::layout()
         || (normalChildNeedsLayout() && SVGLayoutSupport::hasFilterResource(*this));
 
     const bool transformToRootChanged = false;
-    SVGLayoutSupport::layoutChildren(this, forceLayoutOfChildren, transformToRootChanged, layoutSizeChanged);
+    SVGLayoutSupport::layoutChildren(firstChild(), forceLayoutOfChildren, transformToRootChanged, layoutSizeChanged);
 
     if (m_needsBoundariesOrTransformUpdate) {
         updateCachedBoundaries();
