5a7c86d4f583a9cd4e77097918fd1fc34d7f9d64
estade <estade@chromium.org>
2016-04-19 07:13:50 -0700

    Change autofill's InfoBubble to BubbleDialogDelegate
    There is some more cleanup possible here (such as using
    set_anchor_view_insets) but this bit of code is semi-dead and slated
    for removal so I won't bother.
    
    BUG=585312
    
    Review URL: https://codereview.chromium.org/1894153002
    
    Cr-Commit-Position: refs/heads/master@{#388203}

--- chrome/browser/ui/views/autofill/info_bubble.h
+++ chrome/browser/ui/views/autofill/info_bubble.h
@@ -10,14 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/string16.h"
 #include "ui/gfx/geometry/insets.h"
-#include "ui/views/bubble/bubble_delegate.h"
+#include "ui/views/bubble/bubble_dialog_delegate.h"
 
 namespace autofill {
 
 class InfoBubbleFrame;
 
 // Class to create and manage an information bubble for errors or tooltips.
-class InfoBubble : public views::BubbleDelegateView {
+class InfoBubble : public views::BubbleDialogDelegateView {
  public:
   InfoBubble(views::View* anchor, const base::string16& message);
   ~InfoBubble() override;
@@ -31,13 +31,14 @@ class InfoBubble : public views::BubbleDelegateView {
   // Updates the position of the bubble.
   void UpdatePosition();
 
-  // views::BubbleDelegateView:
+  // views::BubbleDialogDelegateView:
   views::NonClientFrameView* CreateNonClientFrameView(
       views::Widget* widget) override;
   gfx::Size GetPreferredSize() const override;
   void OnWidgetDestroyed(views::Widget* widget) override;
   void OnWidgetBoundsChanged(views::Widget* widget,
                              const gfx::Rect& new_bounds) override;
+  int GetDialogButtons() const override;
 
   views::View* anchor() { return anchor_; }
   const views::View* anchor() const { return anchor_; }
