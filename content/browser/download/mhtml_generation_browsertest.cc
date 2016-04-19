7c3130b2b624cf8087c7588ba08eee1b2e299875
lukasza <lukasza@chromium.org>
2016-04-19 15:37:40 -0700

    Enable MHTMLGenerationSitePerProcessTest.GenerateMHTML test.
    This test is passing now (and in fact, should
    be passing for quite a while - since Dec 2015).
    
    Note that majority of MHTML test verification
    (for various shapes of pages/framnes/content) is done via
    in chrome/browser/download/save_page_browsertest.cc, but
    mhtml_generation_browsertest.cc is still useful to keep for
    minimal sanity test from //content layer perspective).
    
    BUG=538766
    
    Review URL: https://codereview.chromium.org/1898903003
    
    Cr-Commit-Position: refs/heads/master@{#388334}

--- content/browser/download/mhtml_generation_browsertest.cc
+++ content/browser/download/mhtml_generation_browsertest.cc
@@ -115,7 +115,7 @@ class MHTMLGenerationSitePerProcessTest : public MHTMLGenerationTest {
     MHTMLGenerationTest::SetUpOnMainThread();
 
     host_resolver()->AddRule("*", "127.0.0.1");
-    ASSERT_TRUE(embedded_test_server()->Start());
+    ASSERT_TRUE(embedded_test_server()->Started());
     content::SetupCrossSiteRedirector(embedded_test_server());
   }
 
@@ -124,10 +124,7 @@ class MHTMLGenerationSitePerProcessTest : public MHTMLGenerationTest {
 };
 
 // Test for crbug.com/538766.
-// Disabled because the test will fail until the bug is fixed
-// (but note that the test only fails with --site-per-process flag).
-IN_PROC_BROWSER_TEST_F(MHTMLGenerationSitePerProcessTest,
-                       DISABLED_GenerateMHTML) {
+IN_PROC_BROWSER_TEST_F(MHTMLGenerationSitePerProcessTest, GenerateMHTML) {
   base::FilePath path(temp_dir_.path());
   path = path.Append(FILE_PATH_LITERAL("test.mht"));
 
