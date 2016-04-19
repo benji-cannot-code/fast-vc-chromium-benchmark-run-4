d0dc3fa159528fb85eb70a9caabce9e5f425920d
ccameron <ccameron@chromium.org>
2016-04-18 17:17:52 -0700

    cc: Clean up ResourceProvider C++ style
    Change ResourceProvider::Resource::gpu_memory_buffer to be a unique_ptr
    instead of raw pointer.
    
    Change NULL to nullptr.
    
    BUG=
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1886893007
    
    Cr-Commit-Position: refs/heads/master@{#388092}

--- cc/resources/resource_provider.h
+++ cc/resources/resource_provider.h
@@ -519,7 +519,7 @@ class CC_EXPORT ResourceProvider
              const gfx::Size& size,
              Origin origin,
              GLenum filter);
-    Resource(const Resource& other);
+    Resource(Resource&& other);
 
     bool needs_sync_token() const { return needs_sync_token_; }
 
@@ -571,12 +571,14 @@ class CC_EXPORT ResourceProvider
     ResourceFormat format;
     SharedBitmapId shared_bitmap_id;
     SharedBitmap* shared_bitmap;
-    gfx::GpuMemoryBuffer* gpu_memory_buffer;
+    std::unique_ptr<gfx::GpuMemoryBuffer> gpu_memory_buffer;
 
    private:
     SynchronizationState synchronization_state_ = SYNCHRONIZED;
     bool needs_sync_token_ = false;
     TextureMailbox mailbox_;
+
+    DISALLOW_COPY_AND_ASSIGN(Resource);
   };
   using ResourceMap = std::unordered_map<ResourceId, Resource>;
 
@@ -603,7 +605,7 @@ class CC_EXPORT ResourceProvider
                              ResourceType type,
                              ResourceFormat format);
   ResourceId CreateBitmap(const gfx::Size& size);
-  Resource* InsertResource(ResourceId id, const Resource& resource);
+  Resource* InsertResource(ResourceId id, Resource resource);
   Resource* GetResource(ResourceId id);
   const Resource* LockForRead(ResourceId id);
   void UnlockForRead(ResourceId id);
