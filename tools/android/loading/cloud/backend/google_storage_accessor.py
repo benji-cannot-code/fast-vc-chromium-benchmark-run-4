400fd4f755f64f933a0226d979c14ea1d2994a22
droger <droger@chromium.org>
2016-04-19 12:10:43 -0700

    tools/android/loading Switch the GCE worker to pull queues
    The GCE worker now uses pull queues instead of HTTP requests.
    
    Review URL: https://codereview.chromium.org/1895033002
    
    Cr-Commit-Position: refs/heads/master@{#388265}

--- tools/android/loading/cloud/backend/google_storage_accessor.py
+++ tools/android/loading/cloud/backend/google_storage_accessor.py
@@ -4,17 +4,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from gcloud import storage
-from oauth2client.client import GoogleCredentials
 
 
 class GoogleStorageAccessor(object):
   """Utility class providing helpers for Google Cloud Storage.
   """
-  def __init__(self, project_name, bucket_name):
+  def __init__(self, credentials, project_name, bucket_name):
     """project_name is the name of the Google Cloud project.
     bucket_name is the name of the bucket that is used for Cloud Storage calls.
     """
-    self._credentials = GoogleCredentials.get_application_default()
+    self._credentials = credentials
     self._project_name = project_name
     self._bucket_name = bucket_name
 
