05eb916d68aefcde0ae88652e42190b9e7e298f0
robert.bradford <robert.bradford@intel.com>
2016-04-19 05:00:04 -0700

    ozone: Propagate maximum cursor size information from HW
    This change extracts the maximum cursor size from DRM and stores it in
    the DisplaySnaphot. From there it is then propagated via
    ash::DisplayInfo to gfx::Display where it is used in the web cursor
    calculations
    
    BUG=570606
    TEST=With http://jsfiddle.net/s0754v0e/ observe that you can have larger
    cursors when running on samus.
    
    Review URL: https://codereview.chromium.org/1680243002
    
    Cr-Commit-Position: refs/heads/master@{#388185}

--- ui/ozone/platform/drm/common/drm_util.cc
+++ ui/ozone/platform/drm/common/drm_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <xf86drm.h>
 #include <xf86drmMode.h>
 #include <utility>
 
@@ -18,9 +19,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DRM_MODE_CONNECTOR_DSI 16
 #endif
 
+#if !defined(DRM_CAP_CURSOR_WIDTH)
+#define DRM_CAP_CURSOR_WIDTH 0x8
+#endif
+
+#if !defined(DRM_CAP_CURSOR_HEIGHT)
+#define DRM_CAP_CURSOR_HEIGHT 0x9
+#endif
+
 namespace ui {
 
 namespace {
+
+static const size_t kDefaultCursorWidth = 64;
+static const size_t kDefaultCursorHeight = 64;
 
 bool IsCrtcInUse(uint32_t crtc,
                  const ScopedVector<HardwareDisplayControllerInfo>& displays) {
@@ -173,6 +185,20 @@ bool HasColorCorrectionMatrix(int fd, drmModeCrtc* crtc) {
 
 }  // namespace
 
+gfx::Size GetMaximumCursorSize(int fd) {
+  uint64_t width = 0, height = 0;
+  if (drmGetCap(fd, DRM_CAP_CURSOR_WIDTH, &width)) {
+    PLOG(WARNING) << "Unable to get cursor width capability";
+    return gfx::Size(kDefaultCursorWidth, kDefaultCursorHeight);
+  }
+  if (drmGetCap(fd, DRM_CAP_CURSOR_HEIGHT, &height)) {
+    PLOG(WARNING) << "Unable to get cursor height capability";
+    return gfx::Size(kDefaultCursorWidth, kDefaultCursorHeight);
+  }
+
+  return gfx::Size(width, height);
+}
+
 HardwareDisplayControllerInfo::HardwareDisplayControllerInfo(
     ScopedDrmConnectorPtr connector,
     ScopedDrmCrtcPtr crtc,
@@ -245,6 +271,7 @@ DisplaySnapshot_Params CreateDisplaySnapshotParams(
       IsAspectPreserving(fd, info->connector());
   params.has_color_correction_matrix =
       HasColorCorrectionMatrix(fd, info->crtc());
+  params.maximum_cursor_size = GetMaximumCursorSize(fd);
 
   ScopedDrmPropertyBlobPtr edid_blob(
       GetDrmPropertyBlob(fd, info->connector(), "EDID"));
