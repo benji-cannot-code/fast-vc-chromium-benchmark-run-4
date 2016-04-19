71cb1caf439b3aa2872be0458f1cea164dbf1040
imcheng <imcheng@chromium.org>
2016-04-19 13:21:49 -0700

    [Test finch config] Update testing finch configs with Media Router bits
    Add EnableMediaRouter[WithCastExtension] => Enabled to
    fieldtrial_testing_configs for cros/win/linux/mac
    
    Tests that explicitly disable EAR were modified to also disable MR,
    since !EAR implies !MR. It is also done to ensure the correct
    FeatureSwitch is used for the entirety of the test.
    
    BUG=450767
    
    Review URL: https://codereview.chromium.org/1872283002
    
    Cr-Commit-Position: refs/heads/master@{#388295}

--- chrome/browser/ui/ash/system_tray_tray_cast_browsertest_chromeos.cc
+++ chrome/browser/ui/ash/system_tray_tray_cast_browsertest_chromeos.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/test/test_utils.h"
 #include "extensions/browser/process_manager.h"
+#include "extensions/common/feature_switch.h"
 
 namespace {
 
@@ -133,11 +134,21 @@ class SystemTrayTrayCastChromeOSTest : public ExtensionBrowserTest {
   SystemTrayTrayCastChromeOSTest() : ExtensionBrowserTest() {}
   ~SystemTrayTrayCastChromeOSTest() override {}
 
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    // SystemTrayTrayCastChromeOSTest tests the behavior of the system tray
+    // without Media Router, so we explicitly disable it.
+    ExtensionBrowserTest::SetUpCommandLine(command_line);
+    override_media_router_.reset(new extensions::FeatureSwitch::ScopedOverride(
+        extensions::FeatureSwitch::media_router(), false));
+  }
+
   const extensions::Extension* LoadCastTestExtension() {
     return LoadExtension(test_data_dir_.AppendASCII("tray_cast"));
   }
 
  private:
+  std::unique_ptr<extensions::FeatureSwitch::ScopedOverride>
+      override_media_router_;
   DISALLOW_COPY_AND_ASSIGN(SystemTrayTrayCastChromeOSTest);
 };
 
