eeaa333889f4ac28f1f123c6f94e2942956158b7
lazyboy <lazyboy@chromium.org>
2016-04-19 15:13:44 -0700

    Reset NavigationObserver's ExtensionInstallPrompt when extension is uninstalled.
    This fixes a crash:
    If an extension is uninstalled while it was showing an ExtensionInstallPrompt
    from NavigationObserver for permission increase, then acting on the prompt
    will:
    1) fail a CHECK in NavigationObserver::OnInstallPromptDone if the user accepts
      the permssion
    2) will try to nullptr deref if the user cancels or aborts the prompt. The
      nullptr deref will happen inside
      ExtensionService::RecordPermissionMessagesHistogram().
    
    BUG=604842
    Test=https://bugs.chromium.org/p/chromium/issues/detail?id=604842#c1
    
    Review URL: https://codereview.chromium.org/1898873004
    
    Cr-Commit-Position: refs/heads/master@{#388328}

--- chrome/browser/extensions/navigation_observer.h
+++ chrome/browser/extensions/navigation_observer.h
@@ -12,9 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observer.h"
 #include "chrome/browser/extensions/extension_install_prompt.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "extensions/browser/extension_registry_observer.h"
 
 class Profile;
 
@@ -28,7 +30,8 @@ namespace extensions {
 // navigates into an extension that has been disabled due to a permission
 // increase, it prompts the user to accept the new permissions and re-enables
 // the extension.
-class NavigationObserver : public content::NotificationObserver {
+class NavigationObserver : public content::NotificationObserver,
+                           public ExtensionRegistryObserver {
  public:
   explicit NavigationObserver(Profile* profile);
   ~NavigationObserver() override;
@@ -50,6 +53,11 @@ class NavigationObserver : public content::NotificationObserver {
 
   void OnInstallPromptDone(ExtensionInstallPrompt::Result result);
 
+  // extensions::ExtensionRegistryObserver:
+  void OnExtensionUninstalled(content::BrowserContext* browser_context,
+                              const Extension* extension,
+                              UninstallReason reason) override;
+
   content::NotificationRegistrar registrar_;
 
   Profile* profile_;
@@ -63,6 +71,9 @@ class NavigationObserver : public content::NotificationObserver {
 
   // The extension ids we've already prompted the user about.
   std::set<std::string> prompted_extensions_;
+
+  ScopedObserver<ExtensionRegistry, ExtensionRegistryObserver>
+      extension_registry_observer_;
 
   base::WeakPtrFactory<NavigationObserver> weak_factory_;
 
