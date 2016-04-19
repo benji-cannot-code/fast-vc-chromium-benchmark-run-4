d10f4a325083af7ed0e3fc3cf0f42836803cf3f4
mstensho <mstensho@opera.com>
2016-04-19 15:10:51 -0700

    Make MultiColumnFragmentainerGroup::m_columnSet const.
    Ideally, I'd like to get rid of the member altogether, but that would require a
    lot of refactoring.
    
    This is a preparatory patch for a fix for bug 604609.
    
    BUG=604609
    
    Review URL: https://codereview.chromium.org/1898293003
    
    Cr-Commit-Position: refs/heads/master@{#388326}

--- third_party/WebKit/Source/core/layout/MultiColumnFragmentainerGroup.cpp
+++ third_party/WebKit/Source/core/layout/MultiColumnFragmentainerGroup.cpp
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-MultiColumnFragmentainerGroup::MultiColumnFragmentainerGroup(LayoutMultiColumnSet& columnSet)
+MultiColumnFragmentainerGroup::MultiColumnFragmentainerGroup(const LayoutMultiColumnSet& columnSet)
     : m_columnSet(columnSet)
 {
 }
@@ -58,7 +58,7 @@ void MultiColumnFragmentainerGroup::resetColumnHeight()
     }
 }
 
-bool MultiColumnFragmentainerGroup::recalculateColumnHeight()
+bool MultiColumnFragmentainerGroup::recalculateColumnHeight(LayoutMultiColumnSet& columnSet)
 {
     LayoutUnit oldColumnHeight = m_columnHeight;
 
@@ -67,18 +67,18 @@ bool MultiColumnFragmentainerGroup::recalculateColumnHeight()
     // Only the last row may have auto height, and thus be balanced. There are no good reasons to
     // balance the preceding rows, and that could potentially lead to an insane number of layout
     // passes as well.
-    if (isLastGroup() && m_columnSet.heightIsAuto()) {
+    if (isLastGroup() && columnSet.heightIsAuto()) {
         LayoutUnit newColumnHeight;
-        if (!m_columnSet.isInitialHeightCalculated()) {
+        if (!columnSet.isInitialHeightCalculated()) {
             // Initial balancing: Start with the lowest imaginable column height. Also calculate the
             // height of the tallest piece of unbreakable content. Columns should never get any
             // shorter than that (unless constrained by max-height). Propagate this to our
             // containing column set, in case there is an outer multicol container that also needs
             // to balance. After having calculated the initial column height, the multicol container
             // needs another layout pass with the column height that we just calculated.
-            InitialColumnHeightFinder initialHeightFinder(columnSet(), logicalTopInFlowThread(), logicalBottomInFlowThread());
+            InitialColumnHeightFinder initialHeightFinder(columnSet, logicalTopInFlowThread(), logicalBottomInFlowThread());
             LayoutUnit tallestUnbreakableLogicalHeight = initialHeightFinder.tallestUnbreakableLogicalHeight();
-            m_columnSet.propagateTallestUnbreakableLogicalHeight(tallestUnbreakableLogicalHeight);
+            columnSet.propagateTallestUnbreakableLogicalHeight(tallestUnbreakableLogicalHeight);
             newColumnHeight = std::max(initialHeightFinder.initialMinimalBalancedHeight(), tallestUnbreakableLogicalHeight);
         } else {
             // Rebalancing: After having laid out again, we'll need to rebalance if the height
