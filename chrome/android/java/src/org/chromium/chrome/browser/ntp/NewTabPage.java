0f8781aa5913cc8b9136989146efffa6b4575625
dgn <dgn@chromium.org>
2016-04-19 06:00:06 -0700

    [NTP Snippets] Refresh the displayed snippets less frequently
    The fetch frequence of the snippet service does not change, but the
    snippets are now refreshed less frequently on the UI. They are pulled
    when the NTP is opened, or when the user switches back to it from
    another tab or another app.
    
    Additionally, we also change the way the list is updated, adding only
    new entries instead of clearing the old list and replacing it with
    the new one.
    
    BUG=601757
    
    Review URL: https://codereview.chromium.org/1888913002
    
    Cr-Commit-Position: refs/heads/master@{#388195}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
@@ -544,6 +544,12 @@ public class NewTabPage
             NewTabPageUma.recordSnippetAction(NewTabPageUma.SNIPPETS_ACTION_DISMISSED);
             mSnippetsBridge.discardSnippet(dismissedSnippet);
         }
+
+        @Override
+        public void addTabObserver(TabObserver tabObserver) {
+            if (mIsDestroyed) return;
+            mTab.addObserver(tabObserver);
+        }
     };
 
     /**
