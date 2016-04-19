6586b9949d04c7e98ee83df6c153057859992af1
atanasova <atanasova@chromium.org>
2016-04-19 07:44:13 -0700

    Adding AMP URL to snippets.
    Chrome Reader is passing AMP URL if it is available through source corpus info
    and on the client side we need to iterate over all the sourceCorpusInfos
    and save the AMP URL if it is available.
    
    BUG=603564
    
    Review URL: https://codereview.chromium.org/1893333002
    
    Cr-Commit-Position: refs/heads/master@{#388205}

--- components/ntp_snippets/ntp_snippets_service_unittest.cc
+++ components/ntp_snippets/ntp_snippets_service_unittest.cc
@@ -40,7 +40,10 @@ std::string GetTestJson(const std::string& content_creation_time_str,
       "\"snippet\" : \"Snippet\","
       "\"thumbnailUrl\" : \"http://localhost/salient_image\","
       "\"creationTimestampSec\" : \"%s\","
-      "\"expiryTimestampSec\" : \"%s\""
+      "\"expiryTimestampSec\" : \"%s\","
+      "\"sourceCorpusInfo\" : [ "
+      "{\"ampUrl\" : \"http://localhost/amp\"},"
+      "{\"corpusId\" : \"id\"}]"
       "}}"
       "]}";
 
@@ -182,6 +185,7 @@ TEST_F(NTPSnippetsServiceTest, Full) {
     EXPECT_EQ(snippet.salient_image_url(),
               GURL("http://localhost/salient_image"));
     EXPECT_EQ(GetDefaultCreationTime(), snippet.publish_date());
+    EXPECT_EQ(snippet.amp_url(), GURL("http://localhost/amp"));
   }
 }
 
