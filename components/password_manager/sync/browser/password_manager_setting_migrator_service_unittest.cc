8855066906471137f3c5bd2c6837d13b7f11b59d
melandory <melandory@chromium.org>
2016-04-20 02:06:51 -0700

    [Smart Lock] Fix settings reconciliation on ios.
    BUG=604721
    
    Review URL: https://codereview.chromium.org/1902113002
    
    Cr-Commit-Position: refs/heads/master@{#388452}

--- components/password_manager/sync/browser/password_manager_setting_migrator_service_unittest.cc
+++ components/password_manager/sync/browser/password_manager_setting_migrator_service_unittest.cc
@@ -287,7 +287,7 @@ TEST_F(PasswordManagerSettingMigratorServiceTest,
     PasswordManagerPreferencesInitialValues histogram_initial_value;
     PasswordManagerPreferencesInitialAndFinalValues histogram_initial_and_final;
   } kTestingTable[] = {
-#if defined(OS_ANDROID)
+#if defined(OS_ANDROID) || defined(OS_IOS)
     {ON, OFF, ON, EMPTY, false, N1L0, I10F00},
     {ON, OFF, OFF, EMPTY, false, N1L0, I10F00},
     {ON, OFF, EMPTY, EMPTY, false, N1L0, I10F00},
@@ -343,7 +343,7 @@ TEST_F(PasswordManagerSettingMigratorServiceTest,
     NotifyProfileAdded();
     StartSyncingPref(prefs(), prefs::kCredentialsEnableService,
                      test_case.new_pref_sync_value);
-#if !defined(OS_ANDROID)
+#if !defined(OS_ANDROID) && !defined(OS_IOS)
     StartSyncingPref(prefs(), prefs::kPasswordManagerSavingEnabled,
                      test_case.old_pref_sync_value);
 #endif
@@ -369,7 +369,7 @@ TEST_F(PasswordManagerSettingMigratorServiceTest,
     bool result_old_pref_value;
     PasswordManagerPreferencesInitialValues histogram_initial_value;
   } kTestingTable[] = {
-#if defined(OS_ANDROID)
+#if defined(OS_ANDROID) || defined(OS_IOS)
     {ON, OFF, ON, EMPTY, true, false, N1L0},
     {ON, OFF, OFF, EMPTY, false, false, N1L0},
     {ON, OFF, EMPTY, EMPTY, true, false, N1L0},
@@ -423,7 +423,7 @@ TEST_F(PasswordManagerSettingMigratorServiceTest,
     NotifyProfileAdded();
     StartSyncingPref(prefs(), prefs::kCredentialsEnableService,
                      test_case.new_pref_sync_value);
-#if !defined(OS_ANDROID)
+#if !defined(OS_ANDROID) && !defined(OS_IOS)
     StartSyncingPref(prefs(), prefs::kPasswordManagerSavingEnabled,
                      test_case.old_pref_sync_value);
 #endif
