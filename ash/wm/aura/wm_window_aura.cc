368af17369d9ef5da80e088a381c3a53479d5af4
sky <sky@chromium.org>
2016-04-20 13:08:50 -0700

    Removes most aura dependencies from DockedWindowResizer
    Remaining dependencies are necessitated by DockedWindowLayoutManager.
    
    BUG=603369
    TEST=covered by other tests
    R=jamescook@chromium.org
    
    Review URL: https://codereview.chromium.org/1901593004
    
    Cr-Commit-Position: refs/heads/master@{#388564}

--- ash/wm/aura/wm_window_aura.cc
+++ ash/wm/aura/wm_window_aura.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_state_aura.h"
 #include "ash/wm/window_util.h"
 #include "ui/aura/client/aura_constants.h"
+#include "ui/aura/client/window_tree_client.h"
 #include "ui/aura/layout_manager.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_delegate.h"
@@ -189,6 +190,12 @@ const WindowState* WmWindowAura::GetWindowState() const {
 
 WmWindow* WmWindowAura::GetToplevelWindow() {
   return Get(window_->GetToplevelWindow());
+}
+
+void WmWindowAura::SetParentUsingContext(WmWindow* context,
+                                         const gfx::Rect& screen_bounds) {
+  aura::client::ParentWindowWithContext(window_, GetAuraWindow(context),
+                                        screen_bounds);
 }
 
 void WmWindowAura::AddChild(WmWindow* window) {
--- ash/wm/dock/docked_window_resizer.h
+++ ash/wm/dock/docked_window_resizer.h
@@ -19,10 +19,6 @@ class Point;
 class Rect;
 }
 
-namespace aura {
-class RootWindow;
-}
-
 namespace ash {
 class DockedWindowLayoutManager;
 
