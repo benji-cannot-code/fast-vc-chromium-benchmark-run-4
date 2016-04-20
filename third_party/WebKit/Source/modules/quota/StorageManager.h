8e3b8f1feaa8a258782a55b277c45d08588bff15
jsbell <jsbell@chromium.org>
2016-04-20 14:05:49 -0700

    Implement quota estimation API from Storage spec
    https://storage.spec.whatwg.org/#api
    
    Adds navigator.storage.estimate() which returns a Promise
    with {usage, quota} values in bytes. This is behind
    the --enable-experimental-web-platform-features flag.
    
    I2I: https://groups.google.com/a/chromium.org/d/msg/blink-dev/P6eY26vB91c/Ri4ohXOPBQAJ
    
    R=kinuko@chromium.org
    
    Review URL: https://codereview.chromium.org/1855623002
    
    Cr-Commit-Position: refs/heads/master@{#388577}

--- third_party/WebKit/Source/modules/quota/StorageManager.h
+++ third_party/WebKit/Source/modules/quota/StorageManager.h
@@ -19,6 +19,7 @@ DEFINE_WRAPPERTYPEINFO();
 public:
     ScriptPromise requestPersistent(ScriptState*);
     ScriptPromise persistentPermission(ScriptState*);
+    ScriptPromise estimate(ScriptState*);
     DECLARE_TRACE();
 };
 
