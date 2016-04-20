57f2bed8a0843e473ec937614f3d88aa65b70ecf
skyostil <skyostil@chromium.org>
2016-04-20 06:12:47 -0700

    Make RenderFrameImpl own its frame blame context
    RenderFrameImpl creates the respective frame blame context, but was
    mistakenly not freeing it.
    
    BUG=546021
    
    Review URL: https://codereview.chromium.org/1907453002
    
    Cr-Commit-Position: refs/heads/master@{#388481}

--- content/renderer/render_frame_impl.h
+++ content/renderer/render_frame_impl.h
@@ -1221,7 +1221,7 @@ class CONTENT_EXPORT RenderFrameImpl
   std::unique_ptr<ExternalPopupMenu> external_popup_menu_;
 #endif
 
-  FrameBlameContext* blame_context_;  // Not owned.
+  std::unique_ptr<FrameBlameContext> blame_context_;
 
   base::WeakPtrFactory<RenderFrameImpl> weak_factory_;
 
