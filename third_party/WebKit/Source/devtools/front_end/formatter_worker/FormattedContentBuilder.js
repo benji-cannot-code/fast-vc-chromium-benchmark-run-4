5839abd750bb1bc2babca7663b08166f59d6a7f7
lushnikov <lushnikov@chromium.org>
2016-04-19 17:43:17 -0700

    DevTools: pretty-print HTML
    The patch implements beautiful HTML pretty-printing.
    
    Apart from HTML pretty-printing, the patch starts applying JavaScript
    pretty-printer to the elements with appropriate JavaScript mime types.
    
    DISCLAIMER: due to the nature of HTML documents, a pretty-printed HTML
    document could be evaluated differently comparing to the original HTML.
    However, the point of pretty-printing here is to make code more readable
    and analyzible.
    
    BUG=none
    R=dgozman, pfeldman
    
    Review URL: https://codereview.chromium.org/1894343002
    
    Cr-Commit-Position: refs/heads/master@{#388382}

--- third_party/WebKit/Source/devtools/front_end/formatter_worker/FormattedContentBuilder.js
+++ third_party/WebKit/Source/devtools/front_end/formatter_worker/FormattedContentBuilder.js
@@ -63,6 +63,9 @@ WebInspector.FormattedContentBuilder.prototype = {
      */
     addNewLine: function(noSquash)
     {
+        // Avoid leading newlines.
+        if (!this._formattedContentLength)
+            return;
         if (noSquash)
             ++this._newLines;
         else
