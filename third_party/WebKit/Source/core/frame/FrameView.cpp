64c8612504dddd4cbf550d495456f1ccd092494e
skyostil <skyostil@chromium.org>
2016-04-20 10:52:31 -0700

    Don't compute a composited selection for a detached frame
    BUG=604558
    
    Review URL: https://codereview.chromium.org/1903163002
    
    Cr-Commit-Position: refs/heads/master@{#388531}

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -1625,7 +1625,7 @@ void FrameView::updateLayersAndCompositingAfterScrollIfNeeded()
 
 bool FrameView::computeCompositedSelection(LocalFrame& frame, CompositedSelection& selection)
 {
-    if (frame.view()->shouldThrottleRendering())
+    if (!frame.view() || frame.view()->shouldThrottleRendering())
         return false;
 
     const VisibleSelection& visibleSelection = frame.selection().selection();
