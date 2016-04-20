abb2f888eb3ddf9bf40d1aa3ffe060326bdfdc31
nednguyen <nednguyen@google.com>
2016-04-19 20:34:34 -0700

    [gpu] Add run_gpu_integration_test.py
    After this patch, one can run webgl_conformance by:
    ./content/test/gpu/run_gpu_integration_test.py webgl_conformance --browser=...
    
    BUG=352807
    
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1898903002
    
    Cr-Commit-Position: refs/heads/master@{#388422}

---
+++ content/test/gpu/run_gpu_integration_test.py
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import sys
+
+from gpu_tests import path_util
+import gpu_project_config
+
+path_util.SetupTelemetryPaths()
+
+from telemetry.testing import browser_test_runner
+
+
+def main():
+  options = browser_test_runner.TestRunOptions()
+  return browser_test_runner.Run(
+      gpu_project_config.CONFIG, options, sys.argv[1:])
+
+
+if __name__ == '__main__':
+  sys.exit(main())
