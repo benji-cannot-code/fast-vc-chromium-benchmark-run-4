b7e5002e1d639436d1ea0097b012af68a47294bc
boliu <boliu@chromium.org>
2016-04-20 09:35:22 -0700

    aw: Fix AwGLFunctor lifetime object
    CleanupReference can't use the DestroyRunnable as the referent, since
    CleanupReference itself is a gc root that will keep DestroyRunnable
    alive, in which case the referent is never garbage collected.
    
    BUG=597167
    
    Review URL: https://codereview.chromium.org/1905623002
    
    Cr-Commit-Position: refs/heads/master@{#388518}

--- android_webview/java/src/org/chromium/android_webview/AwGLFunctor.java
+++ android_webview/java/src/org/chromium/android_webview/AwGLFunctor.java
@@ -34,7 +34,8 @@ class AwGLFunctor {
     }
 
     private final long mNativeAwGLFunctor;
-    private final DestroyRunnable mDestroyRunnable;
+    // Same gc-life time as this, but does not reference any members like |mContainerView|.
+    private final Object mLifetimeObject;
     private final CleanupReference mCleanupReference;
     private final AwContents.NativeGLDelegate mNativeGLDelegate;
     private final ViewGroup mContainerView;
@@ -42,8 +43,9 @@ class AwGLFunctor {
 
     public AwGLFunctor(AwContents.NativeGLDelegate nativeGLDelegate, ViewGroup containerView) {
         mNativeAwGLFunctor = nativeCreate(this);
-        mDestroyRunnable = new DestroyRunnable(mNativeAwGLFunctor);
-        mCleanupReference = new CleanupReference(mDestroyRunnable, mDestroyRunnable);
+        mLifetimeObject = new Object();
+        mCleanupReference =
+                new CleanupReference(mLifetimeObject, new DestroyRunnable(mNativeAwGLFunctor));
         mNativeGLDelegate = nativeGLDelegate;
         mContainerView = containerView;
         if (mNativeGLDelegate.supportsDrawGLFunctorReleasedCallback()) {
@@ -68,7 +70,7 @@ class AwGLFunctor {
     }
 
     public Object getNativeLifetimeObject() {
-        return mDestroyRunnable;
+        return mLifetimeObject;
     }
 
     public boolean requestDrawGLForCanvas(Canvas canvas) {
