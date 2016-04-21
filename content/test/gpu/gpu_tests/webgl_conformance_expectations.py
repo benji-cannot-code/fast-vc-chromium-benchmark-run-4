b3704609b528ad36a726c29209c9890ab54bfb5b
kjellander <kjellander@chromium.org>
2016-04-21 01:56:49 -0700

    Add WebglExtension.EXT_disjoint_timer_query expectation for Nexus 5.
    Most of the previous errors were fixed in
    https://codereview.chromium.org/1904843002 but one test still
    fails on Nexus 5:
    https://build.chromium.org/p/chromium.gpu/builders/Android%20Debug%20%28Nexus%205%29/builds/7310/steps/android_webview_webgl_conformance_tests/logs/stdio
    
    BUG=605255
    TBR=bajones@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1907913002
    
    Cr-Commit-Position: refs/heads/master@{#388729}

--- content/test/gpu/gpu_tests/webgl_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl_conformance_expectations.py
@@ -78,6 +78,8 @@ class WebGLConformanceExpectations(GpuTestExpectations):
         ['android'])
 
     # Nexus 5
+    self.Fail('WebglExtension.EXT_disjoint_timer_query',
+        ['android', ('qualcomm', 'Adreno (TM) 330')])
     self.Fail('WebglExtension.OES_texture_float_linear',
         ['android', ('qualcomm', 'Adreno (TM) 330')])
 
