5cd44b62c336fbc7840c78d9a6495f1f4902a4ea
emircan <emircan@chromium.org>
2016-04-20 17:06:53 -0700

    Reland: Tie kWebRtcH264WithOpenH264FFmpeg and kEnableWebRtcHWH264Encoding flags on Mac
    Additional to the original, this CL checks for >=OSX10.9
    in content/common/gpu/media/vt_video_encode_accelerator_mac.cc
    since VideoToolbox HW features are introduced there.
    The original CL got reverted because of a failure on a bot running 10.8.5.
    https://developer.apple.com/library/mac/releasenotes/General/APIDiffsMacOSX10_9/VideoToolbox.html
    
    Original CL description:
    https://codereview.chromium.org/1852523002/
    This CL enables using VideoToolbox encoder when
    kWebRtcH264WithOpenH264FFmpeg flag is on.
    
    BUG=597334
    TBR=mcasas@chromium.org, hbos@chromium.org
    
    Review URL: https://codereview.chromium.org/1886363002
    
    Cr-Commit-Position: refs/heads/master@{#388611}

--- content/common/gpu/media/vt_video_encode_accelerator_mac.cc
+++ content/common/gpu/media/vt_video_encode_accelerator_mac.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/mac/mac_util.h"
 #include "base/thread_task_runner_handle.h"
 #include "media/base/mac/coremedia_glue.h"
 #include "media/base/mac/corevideo_glue.h"
@@ -90,6 +91,11 @@ VTVideoEncodeAccelerator::GetSupportedProfiles() {
   videotoolbox_glue_ = VideoToolboxGlue::Get();
   if (!videotoolbox_glue_) {
     DLOG(ERROR) << "Failed creating VideoToolbox glue.";
+    return profiles;
+  }
+  if (!base::mac::IsOSMavericksOrLater()) {
+    DLOG(ERROR) << "VideoToolbox hardware encoder is supported on Mac OS 10.9 "
+                  "and later.";
     return profiles;
   }
   const bool rv = CreateCompressionSession(
