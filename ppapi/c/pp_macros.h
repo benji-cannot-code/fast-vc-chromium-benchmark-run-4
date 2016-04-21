02c54b137ae377b267eede5e4bbaaad8c84c8db5
alessandroa <alessandroa@google.com>
2016-04-21 12:35:25 -0700

    Added SetLayerTransform to PPAPI.
    SetLayerTransform() sets a transformation factor that will be applied for the current
    layer displayed on the output device.
    This function has no effect until you call Flush().
    @param[in] scale The scale to be applied.
    @param[in] origin The origin of the scale.
    @param[in] translate The translation to be applied.
    
    @return Returns true on success or false if the resource is invalid or the scale factor is 0 or less.
    
    BUG=605379
    NOPRESUBMIT=true
    
    Reason:
    ** Presubmit ERRORS **
    TODOs found in stable public PPAPI files:
    ppapi/cpp/graphics_2d.h
    
    Review URL: https://codereview.chromium.org/1881603002
    
    Cr-Commit-Position: refs/heads/master@{#388851}

--- ppapi/c/pp_macros.h
+++ ppapi/c/pp_macros.h
@@ -4,13 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From pp_macros.idl modified Fri Jun 13 10:40:42 2014. */
+/* From pp_macros.idl modified Mon Feb  1 13:37:12 2016. */
 
 #ifndef PPAPI_C_PP_MACROS_H_
 #define PPAPI_C_PP_MACROS_H_
 
 
-#define PPAPI_RELEASE 48
+#define PPAPI_RELEASE 52
 
 /**
  * @file
