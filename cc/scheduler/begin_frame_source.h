b6a4da896e41fa426162e8f1f79b955d1c7f7ade
enne <enne@chromium.org>
2016-04-21 15:54:11 -0700

    Revert of Remove BeginFrameObserver::AsValueInto (patchset #1 id:1 of https://codereview.chromium.org/1909683003/ )
    Reason for revert:
    Causing Windows compile failures
    
    Original issue's description:
    > Remove BeginFrameObserver::AsValueInto
    >
    > This doesn't seem to add much, and all BeginFrameSourceObservers have to
    > implement this function, which makes the interface more cumbersome.
    >
    > R=sunnyps@chromium.org
    > BUG=420607
    > CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    TBR=sunnyps@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=420607
    
    Review URL: https://codereview.chromium.org/1912913002
    
    Cr-Commit-Position: refs/heads/master@{#388936}

--- cc/scheduler/begin_frame_source.h
+++ cc/scheduler/begin_frame_source.h
@@ -62,6 +62,9 @@ class CC_EXPORT BeginFrameObserver {
   virtual const BeginFrameArgs& LastUsedBeginFrameArgs() const = 0;
 
   virtual void OnBeginFrameSourcePausedChanged(bool paused) = 0;
+
+  // Tracing support
+  virtual void AsValueInto(base::trace_event::TracedValue* dict) const = 0;
 };
 
 // Simple base class which implements a BeginFrameObserver which checks the
@@ -84,6 +87,9 @@ class CC_EXPORT BeginFrameObserverBase : public BeginFrameObserver {
   // true.
   void OnBeginFrame(const BeginFrameArgs& args) override;
   const BeginFrameArgs& LastUsedBeginFrameArgs() const override;
+
+  // Outputs last_begin_frame_args_
+  void AsValueInto(base::trace_event::TracedValue* dict) const override;
 
  protected:
   // Subclasses should override this method!
@@ -120,6 +126,10 @@ class CC_EXPORT BeginFrameSource {
   // should shut down its timers, disable vsync, etc.
   virtual void AddObserver(BeginFrameObserver* obs) = 0;
   virtual void RemoveObserver(BeginFrameObserver* obs) = 0;
+
+  // Tracing support - Recommend (but not required) to call this implementation
+  // in any override.
+  virtual void AsValueInto(base::trace_event::TracedValue* dict) const = 0;
 };
 
 // Simple base class which implements a BeginFrameSource.
@@ -127,6 +137,8 @@ class CC_EXPORT BeginFrameSource {
 //  - Implement the pure virtual (Set)NeedsBeginFrames methods from
 //    BeginFrameSource.
 //  - Use the CallOnBeginFrame method to call to the observer(s).
+//  - Recommended (but not required) to call BeginFrameSourceBase::AsValueInto
+//    in their own AsValueInto implementation.
 class CC_EXPORT BeginFrameSourceBase : public BeginFrameSource {
  public:
   ~BeginFrameSourceBase() override;
@@ -135,6 +147,10 @@ class CC_EXPORT BeginFrameSourceBase : public BeginFrameSource {
   void DidFinishFrame(size_t remaining_frames) override {}
   void AddObserver(BeginFrameObserver* obs) override;
   void RemoveObserver(BeginFrameObserver* obs) override;
+
+  // Tracing support - Recommend (but not required) to call this implementation
+  // in any override.
+  void AsValueInto(base::trace_event::TracedValue* dict) const override;
 
  protected:
   BeginFrameSourceBase();
@@ -174,6 +190,9 @@ class CC_EXPORT BackToBackBeginFrameSource : public BeginFrameSourceBase {
   void AddObserver(BeginFrameObserver* obs) override;
   void OnNeedsBeginFramesChanged(bool needs_begin_frames) override;
 
+  // Tracing
+  void AsValueInto(base::trace_event::TracedValue* dict) const override;
+
  protected:
   virtual base::TimeTicks Now();  // Now overridable for testing
 
@@ -206,6 +225,9 @@ class CC_EXPORT SyntheticBeginFrameSource : public BeginFrameSourceBase,
   // BeginFrameSourceBase
   void AddObserver(BeginFrameObserver* obs) override;
   void OnNeedsBeginFramesChanged(bool needs_begin_frames) override;
+
+  // Tracing
+  void AsValueInto(base::trace_event::TracedValue* dict) const override;
 
   // DelayBasedTimeSourceClient
   void OnTimerTick() override;
