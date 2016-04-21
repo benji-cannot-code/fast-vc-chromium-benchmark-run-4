a45451c00bbd8d788e6b727acce77861367ad743
lazyboy <lazyboy@chromium.org>
2016-04-21 13:04:03 -0700

    Remove an incorrect forward decl of ExtensionRegistry in management_api.h Also remove an unused fwd decl there.
    TBR=rdevlin.cronin@chromium.org
    BUG=None
    Test=None, internal only change.
    
    Review URL: https://codereview.chromium.org/1910003002
    
    Cr-Commit-Position: refs/heads/master@{#388870}

--- extensions/browser/api/management/management_api.h
+++ extensions/browser/api/management/management_api.h
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_function.h"
 #include "extensions/browser/extension_registry_observer.h"
 
-class ExtensionRegistry;
-class ExtensionUninstallDialog;
 struct WebApplicationInfo;
 
 namespace extensions {
