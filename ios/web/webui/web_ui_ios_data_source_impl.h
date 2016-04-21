ae17da5172306fe9bb780d5f61489e27d2d7dd48
dbeam <dbeam@chromium.org>
2016-04-21 12:33:22 -0700

    WebUI: make sure loadTimeData defaults can also be used from $i18n{}
    R=dschuyler@chromium.org
    BUG=506009
    
    Review URL: https://codereview.chromium.org/1908493003
    
    Cr-Commit-Position: refs/heads/master@{#388850}

--- ios/web/webui/web_ui_ios_data_source_impl.h
+++ ios/web/webui/web_ui_ios_data_source_impl.h
@@ -59,10 +59,6 @@ class WebUIIOSDataSourceImpl : public URLDataSourceIOSImpl,
   void StartDataRequest(const std::string& path,
                         const URLDataSourceIOS::GotDataCallback& callback);
 
-  void disable_set_font_strings_for_testing() {
-    disable_set_font_strings_ = true;
-  }
-
   // The name of this source.
   // E.g., for favicons, this could be "favicon", which results in paths for
   // specific resources like "favicon/34" getting sent to this source.
@@ -72,7 +68,6 @@ class WebUIIOSDataSourceImpl : public URLDataSourceIOSImpl,
   std::map<std::string, int> path_to_idr_map_;
   base::DictionaryValue localized_strings_;
   bool deny_xframe_options_;
-  bool disable_set_font_strings_;
   bool replace_existing_source_;
 
   DISALLOW_COPY_AND_ASSIGN(WebUIIOSDataSourceImpl);
