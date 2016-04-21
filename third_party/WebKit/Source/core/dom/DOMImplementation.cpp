3eb95a3c5c1d7e05b66f11bd37e299a5f05d1f5b
jochen <jochen@chromium.org>
2016-04-21 06:06:51 -0700

    Don't copy the security origin when creating a document
    The spec doesn't say we'd need to do this.
    
    BUG=605203
    R=dcheng@chromium.org
    
    Review URL: https://codereview.chromium.org/1911493004
    
    Cr-Commit-Position: refs/heads/master@{#388754}

--- third_party/WebKit/Source/core/dom/DOMImplementation.cpp
+++ third_party/WebKit/Source/core/dom/DOMImplementation.cpp
@@ -89,7 +89,7 @@ XMLDocument* DOMImplementation::createDocument(const AtomicString& namespaceURI,
         doc = XMLDocument::create(init);
     }
 
-    doc->setSecurityOrigin(document().getSecurityOrigin()->isolatedCopy());
+    doc->setSecurityOrigin(document().getSecurityOrigin());
     doc->setContextFeatures(document().contextFeatures());
 
     Node* documentElement = nullptr;
@@ -211,7 +211,7 @@ HTMLDocument* DOMImplementation::createHTMLDocument(const String& title)
         headElement->appendChild(titleElement);
         titleElement->appendChild(d->createTextNode(title), ASSERT_NO_EXCEPTION);
     }
-    d->setSecurityOrigin(document().getSecurityOrigin()->isolatedCopy());
+    d->setSecurityOrigin(document().getSecurityOrigin());
     d->setContextFeatures(document().contextFeatures());
     return d;
 }
