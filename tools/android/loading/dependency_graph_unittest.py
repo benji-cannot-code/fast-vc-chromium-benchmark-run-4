c71aeca3200dc755cb6fec093e03f0f34b991854
lizeb <lizeb@chromium.org>
2016-04-21 04:23:56 -0700

    clovis: Convert Timing to a proper class.
    This overdue change remove some headaches, and more importantly,
    preserves compatibility with past traces when the timing events list is
    enriched.
    
    Review URL: https://codereview.chromium.org/1909893002
    
    Cr-Commit-Position: refs/heads/master@{#388742}

--- tools/android/loading/dependency_graph_unittest.py
+++ tools/android/loading/dependency_graph_unittest.py
@@ -19,7 +19,7 @@ class RequestDependencyGraphTestCase(unittest.TestCase):
 
   def testUpdateRequestCost(self, serialize=False):
     requests = self.trace.request_track.GetEvents()
-    requests[0].timing = request_track.TimingFromDict(
+    requests[0].timing = request_track.Timing.FromDevToolsDict(
         {'requestTime': 12, 'loadingFinished': 10})
     dependencies_lens = request_dependencies_lens.RequestDependencyLens(
         self.trace)
@@ -36,7 +36,7 @@ class RequestDependencyGraphTestCase(unittest.TestCase):
   def testCost(self, serialize=False):
     requests = self.trace.request_track.GetEvents()
     for (index, request) in enumerate(requests):
-      request.timing = request_track.TimingFromDict(
+      request.timing = request_track.Timing.FromDevToolsDict(
           {'requestTime': index, 'receiveHeadersEnd': 10,
            'loadingFinished': 10})
     dependencies_lens = request_dependencies_lens.RequestDependencyLens(
