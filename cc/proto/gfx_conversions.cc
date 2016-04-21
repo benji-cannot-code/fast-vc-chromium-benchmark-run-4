c73b8bb569ab80d7eafd9361789ebb0cab680b86
khushalsagar <khushalsagar@chromium.org>
2016-04-21 12:55:19 -0700

    cc: Don't send the matrix in the transform proto if its identity.
    Sending this is unnecessary. The deserialization path already assumes
    an idenity matrix if the matrix is not set.
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1910733002
    
    Cr-Commit-Position: refs/heads/master@{#388864}

--- cc/proto/gfx_conversions.cc
+++ cc/proto/gfx_conversions.cc
@@ -101,6 +101,9 @@ gfx::SizeF ProtoToSizeF(const proto::SizeF& proto) {
 
 void TransformToProto(const gfx::Transform& transform,
                       proto::Transform* proto) {
+  if (transform.IsIdentity())
+    return;
+
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
       proto->add_matrix(transform.matrix().get(i, j));
@@ -111,6 +114,7 @@ void TransformToProto(const gfx::Transform& transform,
 gfx::Transform ProtoToTransform(const proto::Transform& proto) {
   if (proto.matrix_size() == 0)
     return gfx::Transform();
+
   gfx::Transform transform(gfx::Transform::kSkipInitialization);
   DCHECK_EQ(16, proto.matrix_size());
   for (int i = 0; i < 4; i++) {
