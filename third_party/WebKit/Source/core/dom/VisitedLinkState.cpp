c2f7c204ebf342066eed927824ea1b8051af5bb7
kochi <kochi@chromium.org>
2016-04-21 00:26:18 -0700

    Invalidate LinkHash state for links in shadow roots
    When link history state changed, :visited style can change, but
    it used to work only for links in document node tree.
    This CL fixes to make it work also for links in shadow node trees.
    
    Writing a layout test for this is harder than expected; tried using
    <iframe> to change history state, but it did not working as expected.
    
    Here's a manual test for this case.
    http://jsbin.com/yipozul/6/edit?html,output
    
    BUG=405707
    TEST=manual
    
    Review URL: https://codereview.chromium.org/1910433002
    
    Cr-Commit-Position: refs/heads/master@{#388710}

--- third_party/WebKit/Source/core/dom/VisitedLinkState.cpp
+++ third_party/WebKit/Source/core/dom/VisitedLinkState.cpp
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/HTMLNames.h"
 #include "core/dom/ElementTraversal.h"
+#include "core/dom/shadow/ElementShadow.h"
+#include "core/dom/shadow/ShadowRoot.h"
 #include "core/html/HTMLAnchorElement.h"
 #include "core/svg/SVGURIReference.h"
 #include "public/platform/Platform.h"
@@ -59,11 +61,9 @@ VisitedLinkState::VisitedLinkState(const Document& document)
 {
 }
 
-void VisitedLinkState::invalidateStyleForAllLinks(bool invalidateVisitedLinkHashes)
+static void invalidateStyleForAllLinksRecursively(Node& rootNode, bool invalidateVisitedLinkHashes)
 {
-    if (m_linksCheckedForVisitedState.isEmpty())
-        return;
-    for (Node& node : NodeTraversal::startsAt(document().firstChild())) {
+    for (Node& node : NodeTraversal::startsAt(&rootNode)) {
         if (node.isLink()) {
             if (invalidateVisitedLinkHashes && isHTMLAnchorElement(node))
                 toHTMLAnchorElement(node).invalidateCachedVisitedLinkHash();
@@ -71,20 +71,37 @@ void VisitedLinkState::invalidateStyleForAllLinks(bool invalidateVisitedLinkHash
             toElement(node).pseudoStateChanged(CSSSelector::PseudoVisited);
             toElement(node).pseudoStateChanged(CSSSelector::PseudoAnyLink);
         }
+        if (isShadowHost(&node)) {
+            for (ShadowRoot* root = node.youngestShadowRoot(); root; root = root->olderShadowRoot())
+                invalidateStyleForAllLinksRecursively(*root, invalidateVisitedLinkHashes);
+        }
     }
 }
 
-void VisitedLinkState::invalidateStyleForLink(LinkHash linkHash)
+void VisitedLinkState::invalidateStyleForAllLinks(bool invalidateVisitedLinkHashes)
 {
-    if (!m_linksCheckedForVisitedState.contains(linkHash))
-        return;
-    for (Node& node : NodeTraversal::startsAt(document().firstChild())) {
+    if (!m_linksCheckedForVisitedState.isEmpty())
+        invalidateStyleForAllLinksRecursively(*document().firstChild(), invalidateVisitedLinkHashes);
+}
+
+static void invalidateStyleForLinkRecursively(Node& rootNode, LinkHash linkHash)
+{
+    for (Node& node : NodeTraversal::startsAt(&rootNode)) {
         if (node.isLink() && linkHashForElement(toElement(node)) == linkHash) {
             toElement(node).pseudoStateChanged(CSSSelector::PseudoLink);
             toElement(node).pseudoStateChanged(CSSSelector::PseudoVisited);
             toElement(node).pseudoStateChanged(CSSSelector::PseudoAnyLink);
         }
+        if (isShadowHost(&node))
+            for (ShadowRoot* root = node.youngestShadowRoot(); root; root = root->olderShadowRoot())
+                invalidateStyleForLinkRecursively(*root, linkHash);
     }
+}
+
+void VisitedLinkState::invalidateStyleForLink(LinkHash linkHash)
+{
+    if (m_linksCheckedForVisitedState.contains(linkHash))
+        invalidateStyleForLinkRecursively(*document().firstChild(), linkHash);
 }
 
 EInsideLink VisitedLinkState::determineLinkStateSlowCase(const Element& element)
