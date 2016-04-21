7f41fcb56b94a09d136b17b01ccf50ecf2f45eeb
apacible <apacible@chromium.org>
2016-04-21 13:54:44 -0700

    Revert of Add code to call skia's matchFamilyStyleCharacter API, which uses the (patchset #7 id:150001 of https://codereview.chromium.org/1883483002/ )
    Reason for revert:
    WebKit Win10 bots failing.
    
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win10/builds/9115
    
    Original issue's description:
    > Add code to call skia's matchFamilyStyleCharacter API
    >
    > Add code to call skia's matchFamilyStyleCharacter API, which uses the
    > DirectWrite font fallback to find fonts for characters that do not exist
    > in the default font. The FontCache and FontDescription changes are
    > refactorings of shared code out of FontCacheAndroid and FontCacheSkiaWin.
    > The FontCacheSkiaWin change adds logic to call skia's
    > matchFamilyStyleCharacter API, which will ultimately call into
    > DirectWrite.
    >
    > This is part 3 of a three part change.
    > 1: https://codereview.chromium.org/1878843002/
    >    Adds support for specifying a font fallback in skia
    > 2: https://codereview.chromium.org/1846433005/
    >    Implements the fallback proxy in Chromium
    > 3: https://codereview.chromium.org/1883483002/
    >    Adds code to blink to call skia's fallback API
    >
    > BUG=459056
    >
    > patch from issue 1740593002 at patchset 140001 (http://crrev.com/1740593002#ps140001)
    
    TBR=jochen@chromium.org,bungeman@chromium.org,drott@chromium.org,eae@chromium.org,kulshin@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=459056
    
    Review URL: https://codereview.chromium.org/1911863002
    
    Cr-Commit-Position: refs/heads/master@{#388893}

--- third_party/WebKit/public/web/win/WebFontRendering.h
+++ third_party/WebKit/public/web/win/WebFontRendering.h
@@ -27,7 +27,6 @@ public:
     BLINK_EXPORT static void setLCDOrientation(SkFontHost::LCDOrientation);
     BLINK_EXPORT static void setAntialiasedTextEnabled(bool);
     BLINK_EXPORT static void setLCDTextEnabled(bool);
-    BLINK_EXPORT static void setUseSkiaFontFallback(bool);
 };
 
 } // namespace blink
