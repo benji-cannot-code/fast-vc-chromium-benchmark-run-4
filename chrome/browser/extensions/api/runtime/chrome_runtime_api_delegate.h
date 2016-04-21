5f720387ec4d56d2b8dbf6b81e27c1eeab17f7b0
asargent <asargent@chromium.org>
2016-04-20 18:17:16 -0700

    Rate limit programmatic update checks for extensions (reland)
    Previously extensions could call the runtime.requestUpdateCheck method
    as often as every 5 seconds. This CL introduces a more restrictive
    policy of around one extra check per autoupdate period, and changes the
    implementation location of the rate limiting from within the
    ExtensionUpdater code itself to the ChromeRuntimeAPIDelegate, and makes
    it more flexible by using net::BackoffEntry.
    
    This is a reland of https://codereview.chromium.org/1887253002, with
    the removal of a static initializer.
    
    BUG=599310
    
    Review URL: https://codereview.chromium.org/1897293005
    
    Cr-Commit-Position: refs/heads/master@{#388654}

--- chrome/browser/extensions/api/runtime/chrome_runtime_api_delegate.h
+++ chrome/browser/extensions/api/runtime/chrome_runtime_api_delegate.h
@@ -7,15 +7,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_EXTENSIONS_API_RUNTIME_CHROME_RUNTIME_API_DELEGATE_H_
 
 #include <map>
-#include <vector>
+#include <string>
+#include <utility>
 
 #include "base/macros.h"
+#include "base/scoped_observer.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "extensions/browser/api/runtime/runtime_api.h"
 #include "extensions/browser/api/runtime/runtime_api_delegate.h"
+#include "extensions/browser/extension_registry_observer.h"
 
 namespace base {
+class TickClock;
 class TimeTicks;
 }
 
@@ -26,15 +30,20 @@ class NotificationSource;
 }
 
 namespace extensions {
+class ExtensionRegistry;
 class RuntimeAPI;
 class UpdateObserver;
 }
 
 class ChromeRuntimeAPIDelegate : public extensions::RuntimeAPIDelegate,
-                                 public content::NotificationObserver {
+                                 public content::NotificationObserver,
+                                 public extensions::ExtensionRegistryObserver {
  public:
   explicit ChromeRuntimeAPIDelegate(content::BrowserContext* context);
   ~ChromeRuntimeAPIDelegate() override;
+
+  // Sets a custom TickClock to use in tests.
+  static void set_tick_clock_for_tests(base::TickClock* clock);
 
  private:
   friend class extensions::RuntimeAPI;
@@ -57,6 +66,11 @@ class ChromeRuntimeAPIDelegate : public extensions::RuntimeAPIDelegate,
                const content::NotificationSource& source,
                const content::NotificationDetails& details) override;
 
+  // ExtensionRegistryObserver implementation.
+  void OnExtensionInstalled(content::BrowserContext* browser_context,
+                            const extensions::Extension* extension,
+                            bool is_update) override;
+
   void UpdateCheckComplete(const std::string& extension_id);
   void CallUpdateCallbacks(const std::string& extension_id,
                            const UpdateCheckResult& result);
@@ -74,12 +88,14 @@ class ChromeRuntimeAPIDelegate : public extensions::RuntimeAPIDelegate,
   // it was reloaded with not enough time in between reloads.
   std::map<std::string, std::pair<base::TimeTicks, int> > last_reload_time_;
 
-  // Pending update checks.
-  typedef std::vector<UpdateCheckCallback> UpdateCallbackList;
-  typedef std::map<std::string, UpdateCallbackList> UpdateCallbackMap;
-  UpdateCallbackMap pending_update_checks_;
+  // Information about update checks, keyed by extension id.
+  struct UpdateCheckInfo;
+  std::map<std::string, UpdateCheckInfo> update_check_info_;
 
- private:
+  ScopedObserver<extensions::ExtensionRegistry,
+                 extensions::ExtensionRegistryObserver>
+      extension_registry_observer_;
+
   DISALLOW_COPY_AND_ASSIGN(ChromeRuntimeAPIDelegate);
 };
 
