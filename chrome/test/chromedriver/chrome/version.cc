8c0a8ecdd5dd66c8d87975397e723ea0b26802e2
gmanikpure <gmanikpure@chromium.org>
2016-04-21 14:44:30 -0700

    [chromedriver] Use snapshot instead of continuous archive, drop M48 and add M51.
    This is because the continuous archive is no longer being updated. Also delete
    the blink snapshot archive, since that is also no longer being updated.
    
    Review URL: https://codereview.chromium.org/1907993003
    
    Cr-Commit-Position: refs/heads/master@{#388911}

--- chrome/test/chromedriver/chrome/version.cc
+++ chrome/test/chromedriver/chrome/version.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // This variable must be able to be found and parsed by the upload script.
-const int kMinimumSupportedChromeVersion[] = {48, 0, 2564, 0};
+const int kMinimumSupportedChromeVersion[] = {49, 0, 2623, 0};
 
 }  // namespace
 
