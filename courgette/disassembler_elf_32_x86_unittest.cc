5fbc6cfa613cbbb03d4fe2d8c80bb77257186dd5
huangs <huangs@chromium.org>
2016-04-21 12:38:38 -0700

    [Courgette] Add parser for ELF section names.
    To reduce noise for ELF parsing, we plan to restrict ELF rel32
    parsing to ".text" section only. This CL adds ELF section name
    parsing, with tests.
    
    Review URL: https://codereview.chromium.org/1900043004
    
    Cr-Commit-Position: refs/heads/master@{#388854}

--- courgette/disassembler_elf_32_x86_unittest.cc
+++ courgette/disassembler_elf_32_x86_unittest.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "courgette/disassembler_elf_32_x86.h"
 
+#include <ctype.h>
 #include <stddef.h>
 #include <stdint.h>
 
 #include <algorithm>
 #include <memory>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -35,6 +37,24 @@ class TestDisassemblerElf32X86 : public DisassemblerElf32X86 {
     }
     EXPECT_EQ(static_cast<size_t>(SectionHeaderCount()), file_offsets.size());
     EXPECT_TRUE(std::is_sorted(file_offsets.begin(), file_offsets.end()));
+  }
+
+  void TestSectionName() {
+    std::set<std::string> name_set;
+    for (const Elf32_Shdr& section_header : section_header_table_) {
+      std::string name;
+      EXPECT_TRUE(SectionName(section_header, &name));
+      // Ensure |name| is unique and is printable (may be empty though).
+      EXPECT_EQ(0U, name_set.count(name));
+      EXPECT_TRUE(std::all_of(name.begin(), name.end(), ::isprint));
+      name_set.insert(name);
+    }
+    // Check for existence of a few common sections.
+    EXPECT_EQ(1U, name_set.count(".text"));
+    EXPECT_EQ(1U, name_set.count(".data"));
+    EXPECT_EQ(1U, name_set.count(".rodata"));
+    EXPECT_EQ(1U, name_set.count(".bss"));
+    EXPECT_EQ(1U, name_set.count(".shstrtab"));
   }
 };
 
@@ -105,6 +125,8 @@ void DisassemblerElf32X86Test::TestExe(const char* file_name,
   EXPECT_FALSE(found_match);
 
   disassembler->TestSectionHeaderFileOffsetOrder();
+
+  disassembler->TestSectionName();
 }
 
 }  // namespace
