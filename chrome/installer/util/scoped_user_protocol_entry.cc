37e3800a73d2a36e075defdd66b8e29aaafa22f1
pmonette <pmonette@chromium.org>
2016-04-20 17:05:16 -0700

    Fix registerProtocolHandler implementation on Windows 8 and 10.
    On Windows 8, now make sure the required registry key exist so that
    Chrome is always given as an option when poping the intent picker.
    
    On Windows 10, now shows the system settings to the user to set the
    default handler for a given protocol.
    
    Also fixed a small UI issue.
    
    BUG=562671
    
    Review URL: https://codereview.chromium.org/1896513002
    
    Cr-Commit-Position: refs/heads/master@{#388609}

--- chrome/installer/util/scoped_user_protocol_entry.cc
+++ chrome/installer/util/scoped_user_protocol_entry.cc
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/util/registry_entry.h"
 #include "chrome/installer/util/shell_util.h"
 
-ScopedUserProtocolEntry::ScopedUserProtocolEntry() {
+ScopedUserProtocolEntry::ScopedUserProtocolEntry(const wchar_t* protocol) {
   entries_.push_back(new RegistryEntry(
-      base::FilePath(ShellUtil::kRegClasses).Append(L"http").value(),
+      base::FilePath(ShellUtil::kRegClasses).Append(protocol).value(),
       ShellUtil::kRegUrlProtocol, base::string16()));
   if (!entries_.back()->KeyExistsInRegistry(RegistryEntry::LOOK_IN_HKCU) &&
       ShellUtil::AddRegistryEntries(HKEY_CURRENT_USER, entries_)) {
