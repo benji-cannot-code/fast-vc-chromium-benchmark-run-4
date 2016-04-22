480718694152fd413db6d74c30bbc295af4193c5
tkent <tkent@chromium.org>
2016-04-21 21:20:44 -0700

    Make blink::InputTypeView GarbageCollectedMixin.
    Also, all of child classes of InputTypeView should be
    GarbageCollectedFinalized<T>.
    
    This is a prepration to stop InputTypeView inheritance of
    InputType. InputTypeView should be GarbageCollectedMixin because some classes
    need to inherit from both of InputType and InputTypeView.
    
    This CL has no behavior changes.
    
    BUG=243714
    
    Review URL: https://codereview.chromium.org/1908873002
    
    Cr-Commit-Position: refs/heads/master@{#389031}

--- third_party/WebKit/Source/core/html/forms/ChooserOnlyTemporalInputTypeView.h
+++ third_party/WebKit/Source/core/html/forms/ChooserOnlyTemporalInputTypeView.h
@@ -35,7 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class ChooserOnlyTemporalInputTypeView final : public InputTypeView, public DateTimeChooserClient {
+class ChooserOnlyTemporalInputTypeView final
+    : public GarbageCollectedFinalized<ChooserOnlyTemporalInputTypeView>
+    , public InputTypeView
+    , public DateTimeChooserClient {
     USING_GARBAGE_COLLECTED_MIXIN(ChooserOnlyTemporalInputTypeView);
     USING_PRE_FINALIZER(ChooserOnlyTemporalInputTypeView, closeDateTimeChooser);
 public:
