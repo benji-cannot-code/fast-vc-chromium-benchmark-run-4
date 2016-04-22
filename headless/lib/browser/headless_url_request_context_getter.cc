8ca2fd21e8ee178ca855581a3137a2eef4706803
skyostil <skyostil@chromium.org>
2016-04-22 07:00:27 -0700

    headless: Add support for host resolver mapping
    BUG=598644
    
    Review URL: https://codereview.chromium.org/1912673002
    
    Cr-Commit-Position: refs/heads/master@{#389093}

--- headless/lib/browser/headless_url_request_context_getter.cc
+++ headless/lib/browser/headless_url_request_context_getter.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/command_line.h"
 #include "base/memory/ptr_util.h"
 #include "base/single_thread_task_runner.h"
 #include "base/threading/worker_pool.h"
@@ -106,9 +105,6 @@ HeadlessURLRequestContextGetter::GetURLRequestContext() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
 
   if (!url_request_context_) {
-    const base::CommandLine& command_line =
-        *base::CommandLine::ForCurrentProcess();
-
     url_request_context_.reset(new net::URLRequestContext());
     url_request_context_->set_net_log(net_log_);
     network_delegate_ = CreateNetworkDelegate();
@@ -163,11 +159,10 @@ HeadlessURLRequestContextGetter::GetURLRequestContext() {
     network_session_params.net_log = url_request_context_->net_log();
     network_session_params.ignore_certificate_errors =
         ignore_certificate_errors_;
-    if (command_line.HasSwitch(switches::kHostResolverRules)) {
+    if (!options_.host_resolver_rules.empty()) {
       std::unique_ptr<net::MappedHostResolver> mapped_host_resolver(
           new net::MappedHostResolver(std::move(host_resolver)));
-      mapped_host_resolver->SetRulesFromString(
-          command_line.GetSwitchValueASCII(switches::kHostResolverRules));
+      mapped_host_resolver->SetRulesFromString(options_.host_resolver_rules);
       host_resolver = std::move(mapped_host_resolver);
     }
 
