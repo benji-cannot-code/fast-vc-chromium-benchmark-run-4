d4b70a09bc640502a2d0fc682935543b2a3f1f09
skym <skym@chromium.org>
2016-04-22 12:49:08 -0700

    [Sync] Sessions now deletes incorrectly hashed data during merge.
    A previous change fixed the session's data type logic to be better at deleting old/stale foreign data. Previously various types of orphans were avoiding garbage collection. After this was fixed, we started seeing many errors on various clients, as it turns out some of that old orphaned data was invalid. When we wanted to delete this invalid data, the typical tag/hash lookup mechanism fails because the stored hashed client tag value is different from what we generate during the deletion. To fix this, we added some semi-sketchy plumbing that allows the sessions data type to be aware of the stored hashed client tag values during merge, and if it detects a mismatch hash value it can delete via the remote sync data, which uses a server id lookup mechanism instead.
    
    BUG=604657
    
    Review URL: https://codereview.chromium.org/1906943002
    
    Cr-Commit-Position: refs/heads/master@{#389214}

--- components/sync_sessions/sessions_sync_manager.h
+++ components/sync_sessions/sessions_sync_manager.h
@@ -48,6 +48,10 @@ class SessionWindow;
 class TabNavigation;
 }  // namespace sync_pb
 
+namespace extensions {
+class ExtensionSessionsTest;
+}  // namespace extensions
+
 namespace browser_sync {
 
 class DataTypeErrorHandler;
@@ -150,6 +154,8 @@ class SessionsSyncManager : public syncer::SyncableService,
   // Container for accessing local tab data by tab id.
   typedef std::map<SessionID::id_type, linked_ptr<TabLink>> TabLinksMap;
 
+  friend class extensions::ExtensionSessionsTest;
+  friend class SessionsSyncManagerTest;
   FRIEND_TEST_ALL_PREFIXES(SessionsSyncManagerTest, PopulateSessionHeader);
   FRIEND_TEST_ALL_PREFIXES(SessionsSyncManagerTest, PopulateSessionWindow);
   FRIEND_TEST_ALL_PREFIXES(SessionsSyncManagerTest, ValidTabs);
@@ -167,6 +173,7 @@ class SessionsSyncManager : public syncer::SyncableService,
   FRIEND_TEST_ALL_PREFIXES(SessionsSyncManagerTest,
                            SaveUnassociatedNodesForReassociation);
   FRIEND_TEST_ALL_PREFIXES(SessionsSyncManagerTest, MergeDeletesCorruptNode);
+  FRIEND_TEST_ALL_PREFIXES(SessionsSyncManagerTest, MergeDeletesBadHash);
   FRIEND_TEST_ALL_PREFIXES(SessionsSyncManagerTest,
                            MergeLocalSessionExistingTabs);
   FRIEND_TEST_ALL_PREFIXES(SessionsSyncManagerTest,
@@ -303,6 +310,16 @@ class SessionsSyncManager : public syncer::SyncableService,
   // Validates the content of a SessionHeader protobuf.
   // Returns false if validation fails.
   static bool IsValidSessionHeader(const sync_pb::SessionHeader& header);
+
+  // Calculates the tag hash from a specifics object. Calculating the hash is
+  // something we typically want to avoid doing in the model type like this.
+  // However, the only place that understands how to generate a tag from the
+  // specifics is the model type, ie us. We need to generate the tag because it
+  // is not passed over the wire for remote data. The use case this function was
+  // created for is detecting bad tag hashes from remote data, see
+  // crbug.com/604657.
+  static std::string TagHashFromSpecifics(
+      const sync_pb::SessionSpecifics& specifics);
 
   SyncedWindowDelegatesGetter* synced_window_delegates_getter() const;
 
