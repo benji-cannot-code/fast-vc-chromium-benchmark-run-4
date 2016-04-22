bc76cd8cfdee71d5089aa5aa6e0a0f0542dcfc7e
eero.hakkinen <eero.hakkinen@intel.com>
2016-04-21 23:38:07 -0700

    [chrome.displaySource] Consolidate media pipeline initialization.
    This consolidates orchestration of media pipeline initialization steps
    into one function. That makes it easier to add new steps and to change
    order of the steps. This does not change the functionality of the media
    pipeline.
    
    BUG=242107
    
    Review URL: https://codereview.chromium.org/1908563002
    
    Cr-Commit-Position: refs/heads/master@{#389045}

--- extensions/renderer/api/display_source/wifi_display/wifi_display_media_pipeline.h
+++ extensions/renderer/api/display_source/wifi_display/wifi_display_media_pipeline.h
@@ -48,8 +48,7 @@ class WiFiDisplayMediaPipeline {
       const ErrorCallback& error_callback);
   ~WiFiDisplayMediaPipeline();
   // Note: to be called only once.
-  void Initialize(
-      const InitCompletionCallback& callback);
+  void Initialize(const InitCompletionCallback& callback);
 
   void InsertRawVideoFrame(
       const scoped_refptr<media::VideoFrame>& video_frame,
@@ -58,6 +57,9 @@ class WiFiDisplayMediaPipeline {
   void RequestIDRPicture();
 
  private:
+  using InitStepCompletionCallback = InitCompletionCallback;
+  enum InitializationStep : unsigned;
+
   WiFiDisplayMediaPipeline(
       wds::SessionType type,
       const WiFiDisplayVideoEncoder::InitParameters& video_parameters,
@@ -67,11 +69,14 @@ class WiFiDisplayMediaPipeline {
       const RegisterMediaServiceCallback& service_callback,
       const ErrorCallback& error_callback);
 
-  void CreateVideoEncoder();
   void CreateMediaPacketizer();
+  void OnInitialize(const InitCompletionCallback& callback,
+                    InitializationStep current_step,
+                    bool success);
   void OnVideoEncoderCreated(
+      const InitStepCompletionCallback& callback,
       scoped_refptr<WiFiDisplayVideoEncoder> video_encoder);
-  void OnMediaServiceRegistered();
+  void OnMediaServiceRegistered(const InitCompletionCallback& callback);
 
   void OnEncodedVideoFrame(const WiFiDisplayEncodedFrame& frame);
 
@@ -89,7 +94,6 @@ class WiFiDisplayMediaPipeline {
 
   RegisterMediaServiceCallback service_callback_;
   ErrorCallback error_callback_;
-  InitCompletionCallback init_completion_callback_;
   WiFiDisplayMediaServicePtr media_service_;
 
   base::WeakPtrFactory<WiFiDisplayMediaPipeline> weak_factory_;
