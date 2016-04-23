94fb834b2fa9c7ff04cd26e1705125ec84043664
pdr <pdr@chromium.org>
2016-04-23 15:54:50 -0700

    Ensure display item ids are unique for image areas
    When painting the focus ring for an image map's area, we would use the
    same display item id (layout object + phase) for both the image's
    outline and the focus ring. This patch creates a separate display item
    type for image area focus rings.
    
    BUG=606113
    
    Review URL: https://codereview.chromium.org/1910233005
    
    Cr-Commit-Position: refs/heads/master@{#389392}

--- third_party/WebKit/Source/platform/graphics/paint/DisplayItem.h
+++ third_party/WebKit/Source/platform/graphics/paint/DisplayItem.h
@@ -69,6 +69,7 @@ public:
         DragImage,
         SVGImage,
         LinkHighlight,
+        ImageAreaFocusRing,
         PageOverlay,
         PageWidgetDelegateBackgroundFallback,
         PopupContainerBorder,
