26dbd7c6c7bf81478018aeab847b3f23ad87dd6d
michaelpg <michaelpg@chromium.org>
2016-04-22 19:42:35 -0700

    MD Settings: simplify language model and data binding
    Several data model changes that make things less confusing and
    data binding more predictable. Useful for the Input Methods CL.
    
    BUG=512479
    
    Review URL: https://codereview.chromium.org/1902893003
    
    Cr-Commit-Position: refs/heads/master@{#389348}

--- chrome/browser/resources/settings/languages_page/language_detail_page.js
+++ chrome/browser/resources/settings/languages_page/language_detail_page.js
@@ -22,13 +22,13 @@ Polymer({
     /**
      * Read-only reference to the languages model provided by the
      * 'settings-languages' instance.
-     * @type {LanguagesModel|undefined}
+     * @type {!LanguagesModel|undefined}
      */
     languages: Object,
 
     /**
      * The language to display the details for.
-     * @type {LanguageInfo|undefined}
+     * @type {!LanguageState|undefined}
      */
     detail: Object,
 
@@ -58,7 +58,6 @@ Polymer({
     }
   },
 
-<if expr="chromeos or is_win">
   /**
    * Checks whether the prospective UI language (the pref that indicates what
    * language to use in Chrome) matches the current language. This pref is only
@@ -70,6 +69,7 @@ Polymer({
    * @private
    */
   isProspectiveUILanguage_: function(languageCode, prospectiveUILanguage) {
+    assert(cr.isChromeOS || cr.isWindows);
     return languageCode == this.languageHelper_.getProspectiveUILanguage();
   },
 
@@ -81,10 +81,10 @@ Polymer({
    * @private
    */
   isCurrentUILanguage_: function(languageCode, prospectiveUILanguage) {
+    assert(cr.isChromeOS || cr.isWindows);
     return languageCode == prospectiveUILanguage &&
            languageCode == navigator.language;
   },
-</if>
 
    /**
    * @param {string} languageCode The language code identifying a language.
