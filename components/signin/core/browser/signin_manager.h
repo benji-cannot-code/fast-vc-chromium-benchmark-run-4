d897a0785577e41b3b58ae1a9cdd6255540625d6
danakj <danakj@chromium.org>
2016-04-22 17:03:32 -0700

    Remove unused memory include from singnin_manager.h
    TBR=brettw
    BUG=554298
    
    Review URL: https://codereview.chromium.org/1917633002
    
    Cr-Commit-Position: refs/heads/master@{#389316}

--- components/signin/core/browser/signin_manager.h
+++ components/signin/core/browser/signin_manager.h
@@ -17,9 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SIGNIN_CORE_BROWSER_SIGNIN_MANAGER_H_
 
 #if defined(OS_CHROMEOS)
-// On Chrome OS, SigninManagerBase is all that exists.
-#include <memory>
-
 #include "components/signin/core/browser/signin_manager_base.h"
 
 #else
