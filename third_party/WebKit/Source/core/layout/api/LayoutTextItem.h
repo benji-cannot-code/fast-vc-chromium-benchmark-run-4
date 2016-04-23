2daedcc249794bae5b37386b8447678d112b587f
eae <eae@chromium.org>
2016-04-22 17:00:26 -0700

    Revert "[Layout API] Create api/LayoutTextFragmentItem.h and use in FirstLetterPseudoElement::updateTextFragments"
    Revert r377142 (commit b14c80d94174aee9db41dcdf86b3f6168d9e3e6f) as the
    thread_times.key_silk_cases suite indicates a 4.4% regression in range.
    
    BUG=589723
    TBR=nednguyen@chromium.org
    
    Review URL: https://codereview.chromium.org/1913463003
    
    Cr-Commit-Position: refs/heads/master@{#389313}

--- third_party/WebKit/Source/core/layout/api/LayoutTextItem.h
+++ third_party/WebKit/Source/core/layout/api/LayoutTextItem.h
@@ -31,16 +31,6 @@ public:
 
     LayoutTextItem() { }
 
-    bool isTextFragment() const
-    {
-        return toText()->isTextFragment();
-    }
-
-    void dirtyLineBoxes()
-    {
-        toText()->dirtyLineBoxes();
-    }
-
     void setStyle(PassRefPtr<ComputedStyle> style)
     {
         toText()->setStyle(style);
