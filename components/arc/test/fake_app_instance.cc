203843ee75669b2f6dcbf9c08ace2f89ef4aaade
khmel <khmel@chromium.org>
2016-04-25 14:29:12 -0700

    arc: Add shelf support for running ARC apps.
    BUG=604773
    TEST=Manually on device. Tested different modes: app pinned/not-pinned,
         on/off pin status. New icon appears for each running ARC app.
         Active and running ARC app has visual feadback. Icon is removed
         once app is closed.
    TEST=unit_tests passes
    
    Review URL: https://codereview.chromium.org/1896223002
    
    Cr-Commit-Position: refs/heads/master@{#389563}

--- components/arc/test/fake_app_instance.cc
+++ components/arc/test/fake_app_instance.cc
@@ -154,4 +154,10 @@ void FakeAppInstance::GetTaskInfo(int32_t task_id,
     callback.Run(mojo::String(), mojo::String());
 }
 
+void FakeAppInstance::SetTaskActive(int32_t task_id) {
+}
+
+void FakeAppInstance::CloseTask(int32_t task_id) {
+}
+
 }  // namespace arc
