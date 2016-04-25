4766d41991bbead0f4325ac39e7933b14bf55b59
reillyg <reillyg@chromium.org>
2016-04-25 16:32:53 -0700

    Make new WebUSB implementation more resilient to garbage collection.
    This patch fixes a couple logic errors on USBDevice cleanup: assuming
    the contextDestroyed() was already called and not clearing the request
    list on connection failure.
    
    We then attempt to fix the failures reported in issue 605487 by using
    WeakPersistenThisPointer when registering the connection error
    callbacks.
    
    BUG=605487
    
    Review URL: https://codereview.chromium.org/1912223002
    
    Cr-Commit-Position: refs/heads/master@{#389610}

--- third_party/WebKit/Source/modules/webusb/USB.h
+++ third_party/WebKit/Source/modules/webusb/USB.h
@@ -53,6 +53,9 @@ public:
     void onGetPermission(ScriptPromiseResolver*, device::usb::wtf::DeviceInfoPtr);
     void onDeviceChanges(device::usb::wtf::DeviceChangeNotificationPtr);
 
+    void onDeviceManagerConnectionError();
+    void onChooserServiceConnectionError();
+
     DECLARE_VIRTUAL_TRACE();
 
 private:
