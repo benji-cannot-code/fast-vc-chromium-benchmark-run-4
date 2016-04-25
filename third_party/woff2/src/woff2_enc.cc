01b00d606c77b54a99b3c63d1bc1d1fcfff8cf04
toyoshim <toyoshim@chromium.org>
2016-04-25 03:21:00 -0700

    Update woff2 to 2bc6acf
    Roll the version that contains chromium local fixes.
    
    BUG=602975
    
    Review URL: https://codereview.chromium.org/1913773002
    
    Cr-Commit-Position: refs/heads/master@{#389439}

--- third_party/woff2/src/woff2_enc.cc
+++ third_party/woff2/src/woff2_enc.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "./encode.h"
+#include "./compressor.h"
 #include "./buffer.h"
 #include "./font.h"
 #include "./normalize.h"
@@ -146,14 +146,6 @@ size_t ComputeWoff2Length(const FontCollection& font_collection,
   size = Round4(size);
 
   size += extended_metadata_length;
-  return size;
-}
-
-size_t ComputeTTFLength(const std::vector<Table>& tables) {
-  size_t size = 12 + 16 * tables.size();  // sfnt header
-  for (const auto& table : tables) {
-    size += Round4(table.src_length);
-  }
   return size;
 }
 
--- third_party/woff2/woff2.gyp
+++ third_party/woff2/woff2.gyp
@@ -34,11 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'msvs_disabled_warnings': [
         4267,
       ],
-      'variables': {
-        'clang_warning_flags': [
-          '-Wno-unused-function',
-        ],
-      },
     },
   ],
 }
