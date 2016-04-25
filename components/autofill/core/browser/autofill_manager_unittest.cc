cc3ca41e1c764e5dfcc0bff526b5a4dbb091628b
sebsg <sebsg@chromium.org>
2016-04-25 12:50:09 -0700

    Set the use count to one and the use date and modification date to "now".
    Changed AutofillProfileSyncableService::MergeProfile to MergeSimilarProfiles. It now compared the merge_into before and after the merge to determine if the merge had any effect. It fixes bug 248440.
    
    Also changed AutofillProfile::PrimaryValue(). Its comment said it included the name but it did not. The name was added because a profile otherwise two people living at the same address could have the profile overwrite one another instead of creating two distinct profiles.
    
    BUG=603640, 248440
    TEST=PersonalDataManagerTest, AutofillProfileSyncableServiceTest, ProfileSyncServiceAutofillTest
    
    Review URL: https://codereview.chromium.org/1891903002
    
    Cr-Commit-Position: refs/heads/master@{#389545}

--- components/autofill/core/browser/autofill_manager_unittest.cc
+++ components/autofill/core/browser/autofill_manager_unittest.cc
@@ -1524,6 +1524,7 @@ TEST_F(AutofillManagerTest, GetCreditCardSuggestionsRepeatedObfuscatedNumber) {
                           "5231567890123456",  // Mastercard
                           "05", "2999");
   credit_card->set_guid("00000000-0000-0000-0000-000000000007");
+  credit_card->set_use_date(base::Time::Now() - base::TimeDelta::FromDays(15));
   autofill_manager_->AddCreditCard(credit_card);
 
   // Set up our form data.
@@ -1785,8 +1786,8 @@ TEST_F(AutofillManagerTest, FillAddressForm) {
   const char guid[] = "00000000-0000-0000-0000-000000000001";
   AutofillProfile* profile = autofill_manager_->GetProfileWithGUID(guid);
   ASSERT_TRUE(profile);
-  EXPECT_EQ(0U, profile->use_count());
-  EXPECT_EQ(base::Time(), profile->use_date());
+  EXPECT_EQ(1U, profile->use_count());
+  EXPECT_NE(base::Time(), profile->use_date());
 
   int response_page_id = 0;
   FormData response_data;
@@ -1796,7 +1797,7 @@ TEST_F(AutofillManagerTest, FillAddressForm) {
   ExpectFilledAddressFormElvis(response_page_id, response_data, kDefaultPageID,
                                false);
 
-  EXPECT_EQ(1U, profile->use_count());
+  EXPECT_EQ(2U, profile->use_count());
   EXPECT_NE(base::Time(), profile->use_date());
 }
 
@@ -4180,6 +4181,7 @@ TEST_F(AutofillManagerTest, FillInUpdatedExpirationDate) {
 }
 
 TEST_F(AutofillManagerTest, UploadCreditCard) {
+  personal_data_.ClearAutofillProfiles();
   autofill_manager_->set_credit_card_upload_enabled(true);
 
   // Create, fill and submit an address form in order to establish a recent
@@ -4213,6 +4215,7 @@ TEST_F(AutofillManagerTest, UploadCreditCard) {
 }
 
 TEST_F(AutofillManagerTest, UploadCreditCard_FeatureNotEnabled) {
+  personal_data_.ClearAutofillProfiles();
   autofill_manager_->set_credit_card_upload_enabled(false);
 
   // Create, fill and submit an address form in order to establish a recent
@@ -4247,6 +4250,7 @@ TEST_F(AutofillManagerTest, UploadCreditCard_FeatureNotEnabled) {
 }
 
 TEST_F(AutofillManagerTest, UploadCreditCard_CvcUnavailable) {
+  personal_data_.ClearAutofillProfiles();
   autofill_manager_->set_credit_card_upload_enabled(true);
 
   // Create, fill and submit an address form in order to establish a recent
@@ -4284,6 +4288,11 @@ TEST_F(AutofillManagerTest, UploadCreditCard_CvcUnavailable) {
 
 TEST_F(AutofillManagerTest, UploadCreditCard_MultipleCvcFields) {
   autofill_manager_->set_credit_card_upload_enabled(true);
+
+  // Remove the profiles that were created in the TestPersonalDataManager
+  // constructor because they would result in conflicting names that would
+  // prevent the upload.
+  personal_data_.ClearAutofillProfiles();
 
   // Create, fill and submit an address form in order to establish a recent
   // profile which can be selected for the upload request.
@@ -4337,6 +4346,7 @@ TEST_F(AutofillManagerTest, UploadCreditCard_MultipleCvcFields) {
 }
 
 TEST_F(AutofillManagerTest, UploadCreditCard_NoProfileAvailable) {
+  personal_data_.ClearAutofillProfiles();
   autofill_manager_->set_credit_card_upload_enabled(true);
 
   // Don't fill or submit an address form.
@@ -4367,6 +4377,7 @@ TEST_F(AutofillManagerTest, UploadCreditCard_NoProfileAvailable) {
 }
 
 TEST_F(AutofillManagerTest, UploadCreditCard_NoNameAvailable) {
+  personal_data_.ClearAutofillProfiles();
   autofill_manager_->set_credit_card_upload_enabled(true);
 
   // Create, fill and submit an address form in order to establish a recent
@@ -4403,6 +4414,7 @@ TEST_F(AutofillManagerTest, UploadCreditCard_NoNameAvailable) {
 }
 
 TEST_F(AutofillManagerTest, UploadCreditCard_ZipCodesConflict) {
+  personal_data_.ClearAutofillProfiles();
   autofill_manager_->set_credit_card_upload_enabled(true);
 
   // Create, fill and submit two address forms with different zip codes.
@@ -4447,6 +4459,7 @@ TEST_F(AutofillManagerTest, UploadCreditCard_ZipCodesConflict) {
 }
 
 TEST_F(AutofillManagerTest, UploadCreditCard_ZipCodesHavePrefixMatch) {
+  personal_data_.ClearAutofillProfiles();
   autofill_manager_->set_credit_card_upload_enabled(true);
 
   // Create, fill and submit two address forms with different zip codes.
@@ -4491,6 +4504,7 @@ TEST_F(AutofillManagerTest, UploadCreditCard_ZipCodesHavePrefixMatch) {
 }
 
 TEST_F(AutofillManagerTest, UploadCreditCard_NoZipCodeAvailable) {
+  personal_data_.ClearAutofillProfiles();
   autofill_manager_->set_credit_card_upload_enabled(true);
 
   // Create, fill and submit an address form in order to establish a recent
@@ -4534,6 +4548,7 @@ TEST_F(AutofillManagerTest, UploadCreditCard_NoZipCodeAvailable) {
 }
 
 TEST_F(AutofillManagerTest, UploadCreditCard_NamesMatchLoosely) {
+  personal_data_.ClearAutofillProfiles();
   autofill_manager_->set_credit_card_upload_enabled(true);
 
   // Create, fill and submit two address forms with different names.
@@ -4581,6 +4596,7 @@ TEST_F(AutofillManagerTest, UploadCreditCard_NamesMatchLoosely) {
 }
 
 TEST_F(AutofillManagerTest, UploadCreditCard_NamesHaveToMatch) {
+  personal_data_.ClearAutofillProfiles();
   autofill_manager_->set_credit_card_upload_enabled(true);
 
   // Create, fill and submit two address forms with different names.
@@ -4625,6 +4641,7 @@ TEST_F(AutofillManagerTest, UploadCreditCard_NamesHaveToMatch) {
 }
 
 TEST_F(AutofillManagerTest, UploadCreditCard_UploadDetailsFails) {
+  personal_data_.ClearAutofillProfiles();
   autofill_manager_->set_credit_card_upload_enabled(true);
 
   // Anything other than "en-US" will cause GetUploadDetails to return a failure
