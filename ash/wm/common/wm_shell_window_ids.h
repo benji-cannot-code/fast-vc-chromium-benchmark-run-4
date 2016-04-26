6bce60c7e50b8a6c5b62c362e5272ce2187b544d
sky <sky@chromium.org>
2016-04-26 14:13:25 -0700

    Starts splitting out shell window ids
    BUG=603369
    TEST=covered by tests
    R=jamescook@chromium.org
    
    Review URL: https://codereview.chromium.org/1920153003
    
    Cr-Commit-Position: refs/heads/master@{#389884}

---
+++ ash/wm/common/wm_shell_window_ids.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_WM_COMMON_WM_SHELL_WINDOW_IDS_H_
+#define ASH_WM_COMMON_WM_SHELL_WINDOW_IDS_H_
+
+#include "ash/ash_export.h"
+
+// Defines the ids assigned to known containers. The id for a WmWindow is
+// accessed via GetShellWindowId().
+
+// TODO(sky): move this into wm namespace.
+namespace ash {
+
+const int kShellWindowId_DockedContainer = 8;
+
+}  // namespace ash
+
+#endif  // ASH_WM_COMMON_WM_SHELL_WINDOW_IDS_H_
--- ash/wm/dock/docked_window_layout_manager.cc
+++ ash/wm/dock/docked_window_layout_manager.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/dock/docked_window_layout_manager.h"
 
-#include "ash/shell_window_ids.h"
 #include "ash/wm/common/shelf/wm_shelf.h"
 #include "ash/wm/common/shelf/wm_shelf_constants.h"
 #include "ash/wm/common/shelf/wm_shelf_observer.h"
@@ -13,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/common/window_parenting_utils.h"
 #include "ash/wm/common/wm_globals.h"
 #include "ash/wm/common/wm_root_window_controller.h"
+#include "ash/wm/common/wm_shell_window_ids.h"
 #include "ash/wm/common/wm_window.h"
 #include "ash/wm/window_resizer.h"
 #include "ash/wm/window_state.h"
