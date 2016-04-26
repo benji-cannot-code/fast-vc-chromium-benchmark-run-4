42ad2de5b905fdf45338c89c1dedb1175991bede
treib <treib@chromium.org>
2016-04-26 00:56:06 -0700

    Revert of [Media Router Tests] Add tests for MediaRouterFactory. (patchset #4 id:60001 of https://codereview.chromium.org/1877273002/ )
    Reason for revert:
    Breaks the build:
    https://build.chromium.org/p/chromium.linux/builders/Android%20Builder/builds/65239
    
    Original issue's description:
    > [Media Router Tests] Add tests for MediaRouterFactory.
    >
    > Committed: https://crrev.com/4f04fcb749ed895a6d5baa72a0ff164fa27589e6
    > Cr-Commit-Position: refs/heads/master@{#389721}
    
    TBR=mfoltz@chromium.org,imcheng@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1916973003
    
    Cr-Commit-Position: refs/heads/master@{#389722}

--- chrome/browser/media/router/media_router_factory.h
+++ chrome/browser/media/router/media_router_factory.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_MEDIA_ROUTER_MEDIA_ROUTER_FACTORY_H_
 #define CHROME_BROWSER_MEDIA_ROUTER_MEDIA_ROUTER_FACTORY_H_
 
-#include "base/gtest_prod_util.h"
 #include "base/lazy_instance.h"
 #include "base/macros.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
@@ -32,14 +31,9 @@ class MediaRouterFactory : public BrowserContextKeyedServiceFactory {
 
  private:
   friend struct base::DefaultLazyInstanceTraits<MediaRouterFactory>;
-  FRIEND_TEST_ALL_PREFIXES(MediaRouterFactoryTest,
-                           OffTheRecordBrowserContextShutdown);
 
   MediaRouterFactory();
   ~MediaRouterFactory() override;
-
-  // Exposed for testing.
-  static MediaRouterFactory* GetMediaRouterFactoryForTest();
 
   // BrowserContextKeyedServiceFactory interface.
   content::BrowserContext* GetBrowserContextToUse(
--- chrome/chrome_tests_unit.gypi
+++ chrome/chrome_tests_unit.gypi
@@ -1440,7 +1440,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/media/router/issue_manager_unittest.cc',
       'browser/media/router/issue_unittest.cc',
       'browser/media/router/media_route_unittest.cc',
-      'browser/media/router/media_router_factory_unittest.cc',
       'browser/media/router/media_sink_unittest.cc',
       'browser/media/router/media_sinks_observer_unittest.cc',
       'browser/media/router/media_source_helper_unittest.cc',
