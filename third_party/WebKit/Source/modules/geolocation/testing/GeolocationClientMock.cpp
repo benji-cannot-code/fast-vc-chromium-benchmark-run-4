1c2bea34571fa6516b3146fb1b89f5311da142f6
sigbjornf <sigbjornf@opera.com>
2016-04-26 07:46:31 -0700

    Tidy up WebGeolocationController.
    Get rid of impedance-matching wrapper now that Oilpan is enabled
    always.
    
    R=
    BUG=585328
    
    Review URL: https://codereview.chromium.org/1919153002
    
    Cr-Commit-Position: refs/heads/master@{#389777}

--- third_party/WebKit/Source/modules/geolocation/testing/GeolocationClientMock.cpp
+++ third_party/WebKit/Source/modules/geolocation/testing/GeolocationClientMock.cpp
@@ -148,7 +148,7 @@ void GeolocationClientMock::setEnableHighAccuracy(bool)
 
 GeolocationPosition* GeolocationClientMock::lastPosition()
 {
-    return m_lastPosition.get();
+    return m_lastPosition;
 }
 
 void GeolocationClientMock::asyncUpdateController()
@@ -163,10 +163,10 @@ void GeolocationClientMock::controllerTimerFired(Timer<GeolocationClientMock>* t
 
     // Make a copy of the set of controllers since it might be modified while iterating.
     GeolocationControllers controllers = m_controllers;
-    if (m_lastPosition.get()) {
+    if (m_lastPosition) {
         ASSERT(!m_hasError);
         for (GeolocationControllers::iterator it = controllers.begin(); it != controllers.end(); ++it)
-            (*it)->positionChanged(m_lastPosition.get());
+            (*it)->positionChanged(m_lastPosition);
     } else if (m_hasError) {
         for (GeolocationControllers::iterator it = controllers.begin(); it != controllers.end(); ++it)
             (*it)->errorOccurred(GeolocationError::create(GeolocationError::PositionUnavailable, m_errorMessage));
--- third_party/WebKit/public/web/WebGeolocationController.h
+++ third_party/WebKit/public/web/WebGeolocationController.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class GeolocationController;
-class GeolocationControllerPrivate;
 class WebGeolocationPosition;
 class WebGeolocationError;
 
@@ -56,7 +55,7 @@ private:
 
     BLINK_EXPORT void reset();
 
-    WebPrivatePtr<GeolocationControllerPrivate> m_private;
+    WebPrivatePtr<GeolocationController> m_private;
 };
 
 } // namespace blink
