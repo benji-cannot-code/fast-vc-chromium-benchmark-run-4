de631e3d8489157263b8bb452a5bfd66a211da41
avayvod <avayvod@chromium.org>
2016-04-26 02:48:23 -0700

    [Android,MediaFling] Fix the encoding issue with already encoded URLs for abc13.com/live
    Also some cleanup of error handling, url type conversions and media type detection,
    extend the UMA to cover more failure cases.
    
    BUG=605544
    TEST=unit tests + avayvod.github.io/mediaflingtest.html
    
    Review URL: https://codereview.chromium.org/1907413002
    
    Cr-Commit-Position: refs/heads/master@{#389735}

--- chrome/browser/media/android/remote/remote_media_player_bridge.cc
+++ chrome/browser/media/android/remote/remote_media_player_bridge.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/android/media_common_android.h"
 #include "media/base/android/media_resource_getter.h"
 #include "media/base/timestamp_constants.h"
+#include "net/base/escape.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/android/java_bitmap.h"
 
@@ -66,8 +67,11 @@ RemoteMediaPlayerBridge::RemoteMediaPlayerBridge(
   CHECK(env);
   ScopedJavaLocalRef<jstring> j_url_string;
   if (url_.is_valid()) {
+    // Escape the URL to make it safe to use. Don't escape existing escape
+    // sequences though.
+    std::string escaped_url = net::EscapeExternalHandlerValue(url_.spec());
     // Create a Java String for the URL.
-    j_url_string = ConvertUTF8ToJavaString(env, url_.spec());
+    j_url_string = ConvertUTF8ToJavaString(env, escaped_url);
   }
   ScopedJavaLocalRef<jstring> j_frame_url_string;
   GURL frameUrl = GetLocalPlayer()->frame_url();
