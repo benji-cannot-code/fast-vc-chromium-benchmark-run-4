f00fa4e37d0e817f6635e315d8bb62e709c45e78
mkwst <mkwst@chromium.org>
2016-04-26 12:51:52 -0700

    CREDENTIAL: Credentials should be submitted within a registrable domain.
    The current code checks for an exact origin match when creating a
    Request. That doesn't match the specification; see step 3.1 of
    https://w3c.github.io/webappsec-credential-management/#body-extraction.
    
    BUG=606788
    
    Review URL: https://codereview.chromium.org/1918253002
    
    Cr-Commit-Position: refs/heads/master@{#389858}

--- third_party/WebKit/LayoutTests/http/tests/credentialmanager/resources/echo-post.php
+++ third_party/WebKit/LayoutTests/http/tests/credentialmanager/resources/echo-post.php
@@ -1,4 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
+    $request_origin_value = $_SERVER["HTTP_ORIGIN"];
+    if (!is_null($request_origin_value)) {
+        header("Access-Control-Allow-Origin: $request_origin_value");
+        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Allow-Methods: GET,POST,OPTIONS");
+    }
     echo json_encode($_POST);
 ?>
--- third_party/WebKit/Source/modules/fetch/Request.cpp
+++ third_party/WebKit/Source/modules/fetch/Request.cpp
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/RuntimeEnabledFeatures.h"
 #include "platform/network/HTTPParsers.h"
 #include "platform/network/ResourceRequest.h"
+#include "platform/weborigin/OriginAccessEntry.h"
 #include "platform/weborigin/Referrer.h"
 #include "public/platform/WebURLRequest.h"
 #include "public/platform/modules/serviceworker/WebServiceWorkerRequest.h"
@@ -327,9 +328,9 @@ Request* Request::createRequestWithRequestOrString(ScriptState* scriptState, Req
     if (request->credentials() == WebURLRequest::FetchCredentialsModePassword) {
         r->getHeaders()->append(HTTPNames::Content_Type, init.contentType, exceptionState);
 
-        // TODO(mkwst): This should be a registrable-domain match.
-        if (!origin->canRequest(r->url())) {
-            exceptionState.throwTypeError("Credentials may only be submitted to same-origin endpoints.");
+        const OriginAccessEntry accessEntry = OriginAccessEntry(r->url().protocol(), r->url().host(), OriginAccessEntry::AllowRegisterableDomains);
+        if (accessEntry.matchesDomain(*origin) == OriginAccessEntry::DoesNotMatchOrigin) {
+            exceptionState.throwTypeError("Credentials may only be submitted to endpoints on the same registrable domain.");
             return nullptr;
         }
     }
