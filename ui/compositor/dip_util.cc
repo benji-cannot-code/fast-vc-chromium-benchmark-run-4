c1c8ff3d56074fed1921f8a5d1f9d10d19bfa373
oshima <oshima@chromium.org>
2016-04-25 19:40:14 -0700

    Remove unused include
    BUG=597761
    
    Review URL: https://codereview.chromium.org/1921933003
    
    Cr-Commit-Position: refs/heads/master@{#389674}

--- ui/compositor/dip_util.cc
+++ ui/compositor/dip_util.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/compositor.h"
 #include "ui/compositor/compositor_switches.h"
 #include "ui/compositor/layer.h"
-#include "ui/gfx/display.h"
 #include "ui/gfx/geometry/dip_util.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/point_conversions.h"
--- ui/compositor/layer.cc
+++ ui/compositor/layer.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/paint_context.h"
 #include "ui/gfx/animation/animation.h"
 #include "ui/gfx/canvas.h"
-#include "ui/gfx/display.h"
 #include "ui/gfx/geometry/point3_f.h"
 #include "ui/gfx/geometry/point_conversions.h"
 #include "ui/gfx/geometry/size_conversions.h"
