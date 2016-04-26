dd5847bf3ef7c1c6cca304d67c125ab8092e38b6
danakj <danakj@chromium.org>
2016-04-25 19:25:41 -0700

    Rename paintCompositedDeprecated to paintIgnoringCompositing.
    This method is not going away, delete TODOs saying it will, and give
    it a proper name (matching the one on ChromeClient) that describes
    what it does better.
    
    R=chrishtr
    BUG=367150
    
    Review URL: https://codereview.chromium.org/1922693002
    
    Cr-Commit-Position: refs/heads/master@{#389669}

--- third_party/WebKit/Source/web/WebViewImpl.h
+++ third_party/WebKit/Source/web/WebViewImpl.h
@@ -116,7 +116,7 @@ public:
     void updateAllLifecyclePhases() override;
     void paint(WebCanvas*, const WebRect&) override;
 #if OS(ANDROID)
-    void paintCompositedDeprecated(WebCanvas*, const WebRect&) override;
+    void paintIgnoringCompositing(WebCanvas*, const WebRect&) override;
 #endif
     void layoutAndPaintAsync(WebLayoutAndPaintAsyncCallback*) override;
     void compositeAndReadbackAsync(WebCompositeAndReadbackAsyncCallback*) override;
