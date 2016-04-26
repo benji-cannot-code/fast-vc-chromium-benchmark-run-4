8517059f898fa018583003b8a18b8a864926ad65
jsbell <jsbell@chromium.org>
2016-04-26 10:07:57 -0700

    Factor File API's 'partial interface URL' impl into separate files
    Preliminary CL for tackling changing the [Exposed] attributes for the
    partials, split the URL methods defined in FileAPI [1] out into their
    own implementation files. Also update bindings template to ensure the
    method supporting the partial interface / static method / conditional
    exposure combination was exported for use in modules.
    
    No behavior changes.
    
    [1] https://w3c.github.io/FileAPI/#creating-revoking
    
    BUG=604951
    R=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/1921063002
    
    Cr-Commit-Position: refs/heads/master@{#389811}

---
+++ third_party/WebKit/Source/core/fileapi/URLFileAPI.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef URLFileAPI_h
+#define URLFileAPI_h
+
+#include "wtf/Allocator.h"
+#include "wtf/Forward.h"
+
+namespace blink {
+
+class ExceptionState;
+class ExecutionContext;
+class Blob;
+
+class URLFileAPI {
+    STATIC_ONLY(URLFileAPI);
+public:
+    static String createObjectURL(ExecutionContext*, Blob*, ExceptionState&);
+    static void revokeObjectURL(ExecutionContext*, const String&);
+};
+
+} // namespace blink
+
+#endif
