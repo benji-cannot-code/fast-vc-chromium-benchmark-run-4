b9c7735d7bd86e49c28d1162ac78807564de1ec0
sadrul <sadrul@chromium.org>
2016-04-26 10:37:26 -0700

    mus/chrome: Always wait for the mojo shell in the browser process.
    When chrome is launched in mus, it always needs to block on Initialize
    to complete, regardless of whether run as 'chrome --mash' or as
    'mojo_runner mojo:mash_session'.
    
    BUG=594852
    
    Review URL: https://codereview.chromium.org/1915053004
    
    Cr-Commit-Position: refs/heads/master@{#389816}

--- content/common/mojo/mojo_shell_connection_impl.cc
+++ content/common/mojo/mojo_shell_connection_impl.cc
@@ -40,13 +40,6 @@ bool IsRunningInMojoShell() {
              mojo::edk::PlatformChannelPair::kMojoPlatformChannelHandleSwitch);
 }
 
-bool ShouldWaitForShell() {
-  return mojo_shell_connection_factory ||
-         (IsRunningInMojoShell() &&
-          base::CommandLine::ForCurrentProcess()->HasSwitch(
-              switches::kWaitForMojoShell));
-}
-
 // static
 bool MojoShellConnectionImpl::CreateUsingFactory() {
   if (mojo_shell_connection_factory) {
@@ -75,8 +68,6 @@ void MojoShellConnection::Create(shell::mojom::ShellClientRequest request,
   lazy_tls_ptr.Pointer()->Set(connection);
   connection->shell_connection_.reset(
       new shell::ShellConnection(connection, std::move(request)));
-  if (is_external)
-    connection->WaitForShellIfNecessary();
 }
 
 // static
@@ -96,7 +87,6 @@ void MojoShellConnectionImpl::BindToRequestFromCommandLine() {
   DCHECK(!shell_connection_);
   shell_connection_.reset(new shell::ShellConnection(
       this, shell::GetShellClientRequestFromCommandLine()));
-  WaitForShellIfNecessary();
 }
 
 MojoShellConnectionImpl::MojoShellConnectionImpl(bool external)
@@ -104,15 +94,6 @@ MojoShellConnectionImpl::MojoShellConnectionImpl(bool external)
 
 MojoShellConnectionImpl::~MojoShellConnectionImpl() {
   STLDeleteElements(&listeners_);
-}
-
-void MojoShellConnectionImpl::WaitForShellIfNecessary() {
-  // TODO(rockot): Remove this. http://crbug.com/594852.
-  if (ShouldWaitForShell()) {
-    base::RunLoop wait_loop;
-    shell_connection_->set_initialize_handler(wait_loop.QuitClosure());
-    wait_loop.Run();
-  }
 }
 
 void MojoShellConnectionImpl::Initialize(shell::Connector* connector,
