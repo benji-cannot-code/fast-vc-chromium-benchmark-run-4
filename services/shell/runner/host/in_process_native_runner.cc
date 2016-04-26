93489824bcb969c009e7bde6d0e203e15dfd56b0
ben <ben@chromium.org>
2016-04-26 12:12:55 -0700

    Fix crash with --mojo-local-storage
    null process id was being sent for threads created in the shell process. Use shell process id instead.
    
    R=jam@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1922043003
    
    Cr-Commit-Position: refs/heads/master@{#389841}

--- services/shell/runner/host/in_process_native_runner.cc
+++ services/shell/runner/host/in_process_native_runner.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
+#include "base/process/process.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task_runner.h"
 #include "base/thread_task_runner_handle.h"
@@ -59,7 +60,7 @@ mojom::ShellClientPtr InProcessNativeRunner::Start(
 #endif
   thread_.reset(new base::DelegateSimpleThread(this, thread_name));
   thread_->Start();
-  pid_available_callback.Run(base::kNullProcessId);
+  pid_available_callback.Run(base::Process::Current().Pid());
 
   return client;
 }
