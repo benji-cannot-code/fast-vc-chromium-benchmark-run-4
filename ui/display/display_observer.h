70291a05c1dfbdfde21e30a4af205e26c4228915
oshima <oshima@chromium.org>
2016-04-25 20:04:13 -0700

    Add display::Display forward decl in display_observer.h
    BUG=597761
    TBR=robliao@chromium.org
    
    Review URL: https://codereview.chromium.org/1914083002
    
    Cr-Commit-Position: refs/heads/master@{#389676}

--- ui/display/display_observer.h
+++ ui/display/display_observer.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/display_observer.h"
 
 namespace display {
+using Display = gfx::Display;
 
 // TODO(oshima): move the gfx::DisplayObserver to display::DisplayObserver.
 using DisplayObserver = gfx::DisplayObserver;
