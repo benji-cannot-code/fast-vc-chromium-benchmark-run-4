6308fc5b920728864c1b7dfa712bbb6950f4f482
twellington <twellington@chromium.org>
2016-04-27 08:46:48 -0700

    Use two padding layers for chrome::android::TabLayer
    Use two separate layers for the side padding and
    bottom padding in chrome::android::TabLayer. Previously
    one cc layer was used for both, which caused a hole
    in the padding if padding was needed on both the side
    and the bottom. Split into two different layers to
    avoid holes and overdrawing.
    
    BUG=604065
    
    Review-Url: https://codereview.chromium.org/1917043007
    Cr-Commit-Position: refs/heads/master@{#390084}

--- chrome/browser/android/compositor/layer/tab_layer.h
+++ chrome/browser/android/compositor/layer/tab_layer.h
@@ -102,6 +102,11 @@ class TabLayer : public Layer {
 
   scoped_refptr<cc::Layer> layer() override;
 
+  static void ComputePaddingPositions(const gfx::Size& content_size,
+                                      const gfx::Size& desired_size,
+                                      gfx::Rect* side_padding_rect,
+                                      gfx::Rect* bottom_padding_rect);
+
  protected:
   TabLayer(bool incognito,
            ui::ResourceManager* resource_manager,
@@ -131,7 +136,8 @@ class TabLayer : public Layer {
   scoped_refptr<ToolbarLayer> toolbar_layer_;
   scoped_refptr<cc::Layer> title_;
   scoped_refptr<ContentLayer> content_;
-  scoped_refptr<cc::SolidColorLayer> padding_;
+  scoped_refptr<cc::SolidColorLayer> side_padding_;
+  scoped_refptr<cc::SolidColorLayer> bottom_padding_;
   scoped_refptr<cc::UIResourceLayer> close_button_;
   scoped_refptr<cc::NinePatchLayer> front_border_;
   scoped_refptr<cc::NinePatchLayer> front_border_inner_shadow_;
