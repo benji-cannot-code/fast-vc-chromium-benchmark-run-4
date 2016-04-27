825609a5751768b39b63409e789b65ede0fa184f
tobiasjs <tobiasjs@chromium.org>
2016-04-27 02:40:13 -0700

    Test: deleting RTM before BVR does not leak resources.
    Deleting the RenderThreadManager before BrowserViewRenderer
    should not result in any resources being leaked.
    
    BUG=597167
    
    Review URL: https://codereview.chromium.org/1920843002
    
    Cr-Commit-Position: refs/heads/master@{#390032}

--- android_webview/browser/test/fake_window.cc
+++ android_webview/browser/test/fake_window.cc
@@ -42,7 +42,7 @@ class FakeWindow::ScopedMakeCurrent {
 };
 
 FakeWindow::FakeWindow(BrowserViewRenderer* view,
-                       RenderThreadManager* functor,
+                       const DrawGLCallback& draw_gl,
                        WindowHooks* hooks,
                        gfx::Rect location)
     : view_(view),
@@ -50,7 +50,7 @@ FakeWindow::FakeWindow(BrowserViewRenderer* view,
       surface_size_(100, 100),
       location_(location),
       on_draw_hardware_pending_(false),
-      functor_(functor),
+      draw_gl_(draw_gl),
       context_current_(false),
       weak_ptr_factory_(this) {
   CheckCurrentlyOnUIThread();
@@ -62,13 +62,6 @@ FakeWindow::FakeWindow(BrowserViewRenderer* view,
 
 FakeWindow::~FakeWindow() {
   CheckCurrentlyOnUIThread();
-}
-
-void FakeWindow::Detach() {
-  CheckCurrentlyOnUIThread();
-  functor_->DeleteHardwareRendererOnUI();
-  view_->OnDetachedFromWindow();
-
   if (render_thread_loop_) {
     base::WaitableEvent completion(true, false);
     render_thread_loop_->PostTask(
@@ -78,6 +71,11 @@ void FakeWindow::Detach() {
   }
 
   render_thread_.reset();
+}
+
+void FakeWindow::Detach() {
+  CheckCurrentlyOnUIThread();
+  view_->OnDetachedFromWindow();
 }
 
 void FakeWindow::RequestInvokeGL(bool wait_for_completion) {
@@ -97,12 +95,12 @@ void FakeWindow::ProcessFunctorOnRT(base::WaitableEvent* sync) {
   process_info.version = kAwDrawGLInfoVersion;
   process_info.mode = AwDrawGLInfo::kModeProcess;
 
-  hooks_->WillProcessOnRT(functor_);
+  hooks_->WillProcessOnRT();
   {
     ScopedMakeCurrent make_current(this);
-    functor_->DrawGL(&process_info);
+    draw_gl_.Run(&process_info);
   }
-  hooks_->DidProcessOnRT(functor_);
+  hooks_->DidProcessOnRT();
 
   if (sync)
     sync->Signal();
@@ -147,9 +145,9 @@ void FakeWindow::DrawFunctorOnRT(base::WaitableEvent* sync) {
     process_info.version = kAwDrawGLInfoVersion;
     process_info.mode = AwDrawGLInfo::kModeSync;
 
-    hooks_->WillSyncOnRT(functor_);
-    functor_->DrawGL(&process_info);
-    hooks_->DidSyncOnRT(functor_);
+    hooks_->WillSyncOnRT();
+    draw_gl_.Run(&process_info);
+    hooks_->DidSyncOnRT();
   }
   sync->Signal();
 
@@ -161,14 +159,14 @@ void FakeWindow::DrawFunctorOnRT(base::WaitableEvent* sync) {
   draw_info.clip_right = location.x() + location.width();
   draw_info.clip_bottom = location.y() + location.height();
 
-  if (!hooks_->WillDrawOnRT(functor_, &draw_info))
+  if (!hooks_->WillDrawOnRT(&draw_info))
     return;
 
   {
     ScopedMakeCurrent make_current(this);
-    functor_->DrawGL(&draw_info);
+    draw_gl_.Run(&draw_info);
   }
-  hooks_->DidDrawOnRT(functor_);
+  hooks_->DidDrawOnRT();
 }
 
 void FakeWindow::CheckCurrentlyOnUIThread() {
