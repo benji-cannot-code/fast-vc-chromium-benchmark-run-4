d0a28c02f8e5c871435939b14ded9478b1811b91
yusukes <yusukes@chromium.org>
2016-04-27 09:57:07 -0700

    Use base::ObserverList in LinkHandlerModelImpl
    BUG=591780
    TEST=manual+try
    
    Review-Url: https://codereview.chromium.org/1916913005
    Cr-Commit-Position: refs/heads/master@{#390107}

--- components/arc/intent_helper/link_handler_model_impl.h
+++ components/arc/intent_helper/link_handler_model_impl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/link_handler_model.h"
 #include "base/memory/weak_ptr.h"
+#include "base/observer_list.h"
 #include "components/arc/arc_service.h"
 #include "components/arc/common/intent_helper.mojom.h"
 
@@ -32,7 +33,7 @@ class LinkHandlerModelImpl : public ash::LinkHandlerModel {
   void OnUrlHandlerList(mojo::Array<mojom::UrlHandlerInfoPtr> handlers);
   void NotifyObserver();
 
-  Observer* observer_;
+  base::ObserverList<Observer> observer_list_;
   mojo::Array<mojom::UrlHandlerInfoPtr> handlers_;
 
   // Always keep this the last member of this class to make sure it's the
