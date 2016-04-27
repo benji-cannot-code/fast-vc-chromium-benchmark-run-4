c33500de67507c4c37b0030cd735f854b02ea1b8
ccameron <ccameron@chromium.org>
2016-04-27 14:13:57 -0700

    Mac: Add detection for low power video compatible frames
    Create a persistent AVSampleBufferDisplayLayer in CALayerTreeHost
    for fullscreen low power video.
    
    Add a function to CARendererLayerTree to check to see if the
    frame can be represented by a single video frame on a black
    background, and, if so, update the AVSampleBufferDisplayLayer
    to show this video frame.
    
    The AVSampleBufferDisplayLayer then doesn't go anywhere.
    The next patch will plumb it through to the
    AcceleratedWidgetMac.
    
    BUG=594460
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/1920723002
    Cr-Commit-Position: refs/heads/master@{#390187}

--- ui/accelerated_widget_mac/ca_layer_tree_coordinator.mm
+++ ui/accelerated_widget_mac/ca_layer_tree_coordinator.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/accelerated_widget_mac/ca_layer_tree_coordinator.h"
 
+#include <AVFoundation/AVFoundation.h>
+
 #include "base/trace_event/trace_event.h"
 #include "ui/base/cocoa/animation_utils.h"
 
@@ -16,6 +18,9 @@ CALayerTreeCoordinator::CALayerTreeCoordinator(bool allow_remote_layers)
     root_ca_layer_.reset([[CALayer alloc] init]);
     [root_ca_layer_ setGeometryFlipped:YES];
     [root_ca_layer_ setOpaque:YES];
+
+    fullscreen_low_power_layer_.reset(
+        [[AVSampleBufferDisplayLayer alloc] init]);
   }
 }
 
@@ -56,13 +61,19 @@ CARendererLayerTree* CALayerTreeCoordinator::GetPendingCARendererLayerTree() {
 }
 
 void CALayerTreeCoordinator::CommitPendingTreesToCA(
-    const gfx::Rect& pixel_damage_rect) {
+    const gfx::Rect& pixel_damage_rect,
+    bool* fullscreen_low_power_layer_valid) {
+  *fullscreen_low_power_layer_valid = false;
+
   // Update the CALayer hierarchy.
   ScopedCAActionDisabler disabler;
   if (pending_ca_renderer_layer_tree_) {
     pending_ca_renderer_layer_tree_->CommitScheduledCALayers(
         root_ca_layer_.get(), std::move(current_ca_renderer_layer_tree_),
         scale_factor_);
+    *fullscreen_low_power_layer_valid =
+        pending_ca_renderer_layer_tree_->CommitFullscreenLowPowerLayer(
+            fullscreen_low_power_layer_);
     current_ca_renderer_layer_tree_.swap(pending_ca_renderer_layer_tree_);
     current_gl_renderer_layer_tree_.reset();
   } else if (pending_gl_renderer_layer_tree_) {
@@ -78,6 +89,15 @@ void CALayerTreeCoordinator::CommitPendingTreesToCA(
     current_ca_renderer_layer_tree_.reset();
   }
 
+  // TODO(ccameron): It may be necessary to leave the last image up for a few
+  // extra frames to allow a smooth switch between the normal and low-power
+  // NSWindows.
+  if (current_fullscreen_low_power_layer_valid_ &&
+      !*fullscreen_low_power_layer_valid) {
+    [fullscreen_low_power_layer_ flushAndRemoveImage];
+  }
+  current_fullscreen_low_power_layer_valid_ = *fullscreen_low_power_layer_valid;
+
   // Reset all state for the next frame.
   pending_ca_renderer_layer_tree_.reset();
   pending_gl_renderer_layer_tree_.reset();
@@ -86,6 +106,10 @@ void CALayerTreeCoordinator::CommitPendingTreesToCA(
 CALayer* CALayerTreeCoordinator::GetCALayerForDisplay() const {
   DCHECK(allow_remote_layers_);
   return root_ca_layer_.get();
+}
+
+CALayer* CALayerTreeCoordinator::GetFullscreenLowPowerLayerForDisplay() const {
+  return fullscreen_low_power_layer_.get();
 }
 
 IOSurfaceRef CALayerTreeCoordinator::GetIOSurfaceForDisplay() {
