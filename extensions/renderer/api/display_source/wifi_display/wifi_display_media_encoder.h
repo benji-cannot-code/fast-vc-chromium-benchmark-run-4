5c68c30ef868b824963e2aed94ab449257dc94f8
eero.hakkinen <eero.hakkinen@intel.com>
2016-04-27 07:14:29 -0700

    [chrome.displaySource] Add client thread checks to encoders.
    Wi-Fi Display encoder client code should belong to a single thread. This
    CL adds DCHECKS to check that.
    
    BUG=242107
    
    Review URL: https://codereview.chromium.org/1925613002
    
    Cr-Commit-Position: refs/heads/master@{#390064}

--- extensions/renderer/api/display_source/wifi_display/wifi_display_media_encoder.h
+++ extensions/renderer/api/display_source/wifi_display/wifi_display_media_encoder.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/move.h"
 #include "base/single_thread_task_runner.h"
+#include "base/threading/thread_checker.h"
 
 namespace extensions {
 
@@ -63,6 +64,7 @@ class WiFiDisplayMediaEncoder
   WiFiDisplayMediaEncoder();
   virtual ~WiFiDisplayMediaEncoder();
 
+  base::ThreadChecker client_thread_checker_;
   EncodedUnitCallback encoded_callback_;
   base::Closure error_callback_;
 };
