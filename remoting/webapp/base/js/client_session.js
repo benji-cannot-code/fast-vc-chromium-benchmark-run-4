89e8e50c99143ebcfb951c0dafe1a4dc27e0a39c
yuweih <yuweih@chromium.org>
2016-04-27 11:33:28 -0700

    Report new session state for video-streaming started.
    From a user's perspective, a successful connection only begins when they are able to see frames from the host.
    So, we should add a session-state for video-streaming started.
    
    BUG=462725
    
    Review-Url: https://codereview.chromium.org/1924623003
    Cr-Commit-Position: refs/heads/master@{#390135}

--- remoting/webapp/base/js/client_session.js
+++ remoting/webapp/base/js/client_session.js
@@ -157,7 +157,8 @@ remoting.ClientSession.State = {
   CONNECTED: 4,
   CLOSED: 5,
   FAILED: 6,
-  MAX_STATE_ENUM: 6,
+  VIDEO_STREAM_STARTED: 7,
+  MAX_STATE_ENUM: 7
 };
 
 /**
@@ -382,6 +383,8 @@ remoting.ClientSession.prototype.dropSessionOnSuspend = function(
  */
 remoting.ClientSession.prototype.onFirstFrameReceived = function() {
   this.hasReceivedFrame_ = true;
+  this.logger_.logSessionStateChange(
+    toSessionState(remoting.ClientSession.State.VIDEO_STREAM_STARTED));
 };
 
 /**
@@ -689,6 +692,8 @@ function toSessionState(state) {
       return SessionState.CONNECTION_DROPPED;
     case remoting.ClientSession.State.CONNECTION_CANCELED:
       return SessionState.CONNECTION_CANCELED;
+    case remoting.ClientSession.State.VIDEO_STREAM_STARTED:
+      return SessionState.VIDEO_STREAM_STARTED;
     default:
       throw new Error('Unknown session state : ' + state);
   }
