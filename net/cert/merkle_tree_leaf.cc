a73d023a772a0ace1a9e7f83af388133423db2cd
eranm <eranm@chromium.org>
2016-04-27 15:07:08 -0700

    Certificate Transparency: Introducing MerkleTreeLeaf
    This structure contains everything in the MerkleTreeLeaf struct
    described in RFC6962 section 3.4.
    
    It is necessary for inclusion checking: The leaf hash used for checking
    inclusion is calculated by serializing the data and hashing it.
    
    BUG=506227
    
    Review-Url: https://codereview.chromium.org/1922173002
    Cr-Commit-Position: refs/heads/master@{#390208}

---
+++ net/cert/merkle_tree_leaf.cc
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/cert/merkle_tree_leaf.h"
+
+#include "net/cert/ct_objects_extractor.h"
+#include "net/cert/x509_certificate.h"
+
+namespace net {
+
+namespace ct {
+
+MerkleTreeLeaf::MerkleTreeLeaf() {}
+
+MerkleTreeLeaf::~MerkleTreeLeaf() {}
+
+bool GetMerkleTreeLeaf(const X509Certificate* cert,
+                       const SignedCertificateTimestamp* sct,
+                       MerkleTreeLeaf* merkle_tree_leaf) {
+  if (sct->origin == SignedCertificateTimestamp::SCT_EMBEDDED) {
+    if (cert->GetIntermediateCertificates().empty() ||
+        !GetPrecertLogEntry(cert->os_cert_handle(),
+                            cert->GetIntermediateCertificates().front(),
+                            &merkle_tree_leaf->log_entry)) {
+      return false;
+    }
+  } else {
+    if (!GetX509LogEntry(cert->os_cert_handle(),
+                         &merkle_tree_leaf->log_entry)) {
+      return false;
+    }
+  }
+
+  merkle_tree_leaf->log_id = sct->log_id;
+  merkle_tree_leaf->timestamp = sct->timestamp;
+  merkle_tree_leaf->extensions = sct->extensions;
+  return true;
+}
+
+}  // namespace ct
+
+}  // namespace net
