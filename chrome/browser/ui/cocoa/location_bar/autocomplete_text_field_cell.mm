a37ecaf2ce647bfdb64ef48bead8eb8c46fa6d69
shrike <shrike@chromium.org>
2016-04-27 10:40:38 -0700

    [Mac][Material Design] Rework how location bar shadow is drawn.
    The location bar in Material Design has a very faint line of shadow
    beneath it in Incognito mode. I originally accomplished this by drawing
    it within the bounds of the location bar but by borrowing a pixel row I
    changed the height of the interior of the location bar, causing items
    within the bar to no longer be vertically centered. This change moves
    the shadow into a separate view.
    
    R=avi@chromium.org
    BUG=594847
    
    Review-Url: https://codereview.chromium.org/1905163002
    Cr-Commit-Position: refs/heads/master@{#390115}

--- chrome/browser/ui/cocoa/location_bar/autocomplete_text_field_cell.mm
+++ chrome/browser/ui/cocoa/location_bar/autocomplete_text_field_cell.mm
@@ -318,10 +318,6 @@ size_t CalculatePositionsInFrame(
 
   // Compute the border's bezier path.
   NSRect pathRect = NSInsetRect(frame, insetSize, insetSize);
-  // In dark mode, make room for a shadow beneath the bottom edge.
-  if (inDarkMode && isModeMaterial) {
-    pathRect.size.height -= singlePixelLineWidth_;
-  }
   NSBezierPath* path =
       [NSBezierPath bezierPathWithRoundedRect:pathRect
                                       xRadius:kCornerRadius
@@ -370,7 +366,7 @@ size_t CalculatePositionsInFrame(
       }
 
       // Draw a highlight beneath the top edge, and a shadow beneath the bottom
-      // edge.
+      // edge when on a Retina screen.
       {
         gfx::ScopedNSGraphicsContextSaveGState saveState;
         [NSBezierPath setDefaultLineWidth:singlePixelLineWidth_];
@@ -384,10 +380,12 @@ size_t CalculatePositionsInFrame(
         [NSBezierPath strokeLineFromPoint:origin
                                   toPoint:destination];
 
-        origin.y = destination.y = NSMaxY(pathRect) + singlePixelLineWidth_;
-        [[NSColor colorWithCalibratedWhite:69 / 255. alpha:1] set];
-        [NSBezierPath strokeLineFromPoint:origin
-                                  toPoint:destination];
+        if (singlePixelLineWidth_ < 1) {
+          origin.y = destination.y = NSMaxY(pathRect) + singlePixelLineWidth_;
+          [[AutocompleteTextField shadowColor] set];
+          [NSBezierPath strokeLineFromPoint:origin
+                                    toPoint:destination];
+        }
       }
     }
   } else {
