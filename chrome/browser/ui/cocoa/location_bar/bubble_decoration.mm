6c6be128fa10c92c745cd1ffe79b0487194ed66c
shrike <shrike@chromium.org>
2016-04-27 10:46:42 -0700

    [Mac][Material Design] Adjust EV chip to match Material Design spec.
    Tweak the EV chip to match the spec, per sgabriel@.
    
    R=avi@chromium.org
    BUG=596594
    
    Review-Url: https://codereview.chromium.org/1909453004
    Cr-Commit-Position: refs/heads/master@{#390116}

--- chrome/browser/ui/cocoa/location_bar/bubble_decoration.mm
+++ chrome/browser/ui/cocoa/location_bar/bubble_decoration.mm
@@ -32,9 +32,10 @@ const CGFloat kBackgroundYInset = 4.0;
 
 }  // namespace
 
-BubbleDecoration::BubbleDecoration() {
+BubbleDecoration::BubbleDecoration() : baseline_offset_(0) {
   attributes_.reset([[NSMutableDictionary alloc] init]);
-  [attributes_ setObject:GetFont() forKey:NSFontAttributeName];
+  [attributes_ setObject:LocationBarDecoration::GetFont()
+                  forKey:NSFontAttributeName];
 }
 
 BubbleDecoration::~BubbleDecoration() {
@@ -103,6 +104,7 @@ void BubbleDecoration::DrawInFrame(NSRect frame, NSView* control_view) {
   if (label_) {
     NSRect textRect = frame;
     textRect.origin.x = textOffset;
+    textRect.origin.y += baseline_offset_;
     textRect.size.width = NSMaxX(decoration_frame) - NSMinX(textRect);
     DrawLabel(label_, attributes_, textRect);
   }
@@ -136,6 +138,10 @@ void BubbleDecoration::DrawWithBackgroundInFrame(NSRect background_frame,
   DrawInFrame(frame, control_view);
 }
 
+NSFont* BubbleDecoration::GetFont() const {
+  return [attributes_ objectForKey:NSFontAttributeName];
+}
+
 NSImage* BubbleDecoration::GetImage() {
   return image_;
 }
@@ -154,4 +160,12 @@ void BubbleDecoration::SetLabel(NSString* label) {
 
 void BubbleDecoration::SetTextColor(NSColor* text_color) {
   [attributes_ setObject:text_color forKey:NSForegroundColorAttributeName];
+}
+
+void BubbleDecoration::SetFont(NSFont* font) {
+  [attributes_ setObject:font forKey:NSFontAttributeName];
+}
+
+void BubbleDecoration::SetBaselineOffset(CGFloat offset) {
+  baseline_offset_ = offset;
 }
