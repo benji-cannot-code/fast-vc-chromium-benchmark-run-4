58b265053c35cf462664afca05827a556cb117b4
csharrison <csharrison@chromium.org>
2016-04-28 07:22:24 -0700

    [page_load_metrics] Eagerly send IPCs on navigation commit
    This starts the page_load_metrics IPC timer a bit earlier, so we know to start tracking a committed load in the browser process.
    
    BUG=607550
    
    Review-Url: https://codereview.chromium.org/1918393002
    Cr-Commit-Position: refs/heads/master@{#390372}

--- components/page_load_metrics/renderer/page_timing_metrics_sender.h
+++ components/page_load_metrics/renderer/page_timing_metrics_sender.h
@@ -29,7 +29,8 @@ class PageTimingMetricsSender {
  public:
   PageTimingMetricsSender(IPC::Sender* ipc_sender,
                           int routing_id,
-                          std::unique_ptr<base::Timer> timer);
+                          std::unique_ptr<base::Timer> timer,
+                          const PageLoadTiming& initial_timing);
   ~PageTimingMetricsSender();
 
   void DidObserveLoadingBehavior(blink::WebLoadingBehaviorFlag behavior);
@@ -39,7 +40,7 @@ class PageTimingMetricsSender {
   base::Timer* timer() const { return timer_.get(); }
 
  private:
-  void EnsureSendTimer();
+  void EnsureSendTimer(int delay);
   void SendNow();
 
   IPC::Sender* const ipc_sender_;
