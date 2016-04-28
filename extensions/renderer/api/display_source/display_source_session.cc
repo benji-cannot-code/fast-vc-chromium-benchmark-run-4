c581a01e2002c6f985b4cc6b2ad4fb1c3fa97ee0
eero.hakkinen <eero.hakkinen@intel.com>
2016-04-28 06:39:01 -0700

    [chrome.displaySource] Avoid compiler warning.
    Avoid warnings about unreachable codes, false warnings about potentially
    uninitialized variables, warnings about unused local objects related
    static functions and signedness warnings.
    
    BUG=242107
    
    Review-Url: https://codereview.chromium.org/1927923002
    Cr-Commit-Position: refs/heads/master@{#390363}

--- extensions/renderer/api/display_source/display_source_session.cc
+++ extensions/renderer/api/display_source/display_source_session.cc
@@ -41,8 +41,9 @@ DisplaySourceSessionFactory::CreateSession(
     const DisplaySourceSessionParams& params) {
 #if defined(ENABLE_WIFI_DISPLAY)
   return std::unique_ptr<DisplaySourceSession>(new WiFiDisplaySession(params));
-#endif
+#else
   return nullptr;
+#endif
 }
 
 }  // namespace extensions
