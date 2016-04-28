1cd7356757b640e82da2ff3a86947819082adc12
aleksandar.stojiljkovic <aleksandar.stojiljkovic@intel.com>
2016-04-28 01:24:44 -0700

    Remove unused BitmapImage::m_hasUniformFrameSize
    BUG=
    
    Review-Url: https://codereview.chromium.org/1918693007
    Cr-Commit-Position: refs/heads/master@{#390328}

--- third_party/WebKit/Source/platform/graphics/BitmapImage.cpp
+++ third_party/WebKit/Source/platform/graphics/BitmapImage.cpp
@@ -68,7 +68,6 @@ BitmapImage::BitmapImage(ImageObserver* observer)
     , m_allDataReceived(false)
     , m_haveSize(false)
     , m_sizeAvailable(false)
-    , m_hasUniformFrameSize(true)
     , m_haveFrameCount(false)
 {
 }
@@ -168,10 +167,6 @@ void BitmapImage::cacheFrame(size_t index)
     m_frames[index].m_hasAlpha = m_source.frameHasAlphaAtIndex(index);
     m_frames[index].m_frameBytes = m_source.frameBytesAtIndex(index);
 
-    const IntSize frameSize(index ? m_source.frameSizeAtIndex(index) : m_size);
-    if (frameSize != m_size)
-        m_hasUniformFrameSize = false;
-
     notifyMemoryChanged();
 }
 
@@ -237,7 +232,6 @@ bool BitmapImage::dataChanged(bool allDataReceived)
     m_source.setData(*data(), allDataReceived);
 
     m_haveFrameCount = false;
-    m_hasUniformFrameSize = true;
     return isSizeAvailable();
 }
 
