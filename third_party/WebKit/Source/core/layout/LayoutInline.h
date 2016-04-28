6ac9d9c0e0c47f0f97b3c8b73a83b13f67570a07
eae <eae@chromium.org>
2016-04-28 00:03:31 -0700

    Change linesBoundingBox to return LayoutRect
    Change the LayoutInline::linesBoundingBox method, and subclass overrides
    in LayoutText and LayoutSVGInlineText, to return a LayoutRect instead of
    an IntRect. Thereby exposing more correct and consistent offsetWidth and
    offsetHeight values through the DOM. Move enclosing int logic to callers
    as needed, particularly for PaintLayer, editing, and spatial navigation.
    
    TEST=fast/sub-pixel/width-of-inline-in-float.html
    BUG=414264
    R=drott@chromium.org
    
    Review-Url: https://codereview.chromium.org/1922223003
    Cr-Commit-Position: refs/heads/master@{#390316}

--- third_party/WebKit/Source/core/layout/LayoutInline.h
+++ third_party/WebKit/Source/core/layout/LayoutInline.h
@@ -143,7 +143,7 @@ public:
 
     LayoutSize offsetFromContainer(const LayoutObject*) const final;
 
-    IntRect linesBoundingBox() const;
+    LayoutRect linesBoundingBox() const;
     LayoutRect visualOverflowRect() const final;
 
     InlineFlowBox* createAndAppendInlineFlowBox();
@@ -233,8 +233,8 @@ private:
 
     LayoutUnit offsetLeft() const final;
     LayoutUnit offsetTop() const final;
-    LayoutUnit offsetWidth() const final { return LayoutUnit(linesBoundingBox().width()); }
-    LayoutUnit offsetHeight() const final { return LayoutUnit(linesBoundingBox().height()); }
+    LayoutUnit offsetWidth() const final { return linesBoundingBox().width(); }
+    LayoutUnit offsetHeight() const final { return linesBoundingBox().height(); }
 
     LayoutRect absoluteClippedOverflowRect() const override;
 
@@ -248,7 +248,7 @@ private:
 
     IntRect borderBoundingBox() const final
     {
-        IntRect boundingBox = linesBoundingBox();
+        IntRect boundingBox = enclosingIntRect(linesBoundingBox());
         return IntRect(0, 0, boundingBox.width(), boundingBox.height());
     }
 
