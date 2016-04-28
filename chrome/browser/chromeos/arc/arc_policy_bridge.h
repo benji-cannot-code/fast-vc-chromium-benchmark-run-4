2f0a70f0511e8ab597999d50b11d6a9e886b2940
phweiss <phweiss@chromium.org>
2016-04-28 01:36:18 -0700

    Make GetPolicies() return null for unmanaged users
    BUG=b/28338159
    
    Review-Url: https://codereview.chromium.org/1923363002
    Cr-Commit-Position: refs/heads/master@{#390329}

--- chrome/browser/chromeos/arc/arc_policy_bridge.h
+++ chrome/browser/chromeos/arc/arc_policy_bridge.h
@@ -28,6 +28,8 @@ class ArcPolicyBridge : public ArcService,
                   policy::PolicyService* policy_service);
   ~ArcPolicyBridge() override;
 
+  void OverrideIsManagedForTesting(bool is_managed);
+
   // ArcBridgeService::Observer overrides.
   void OnPolicyInstanceReady() override;
   void OnPolicyInstanceClosed() override;
@@ -45,6 +47,7 @@ class ArcPolicyBridge : public ArcService,
 
   mojo::Binding<PolicyHost> binding_;
   policy::PolicyService* policy_service_ = nullptr;
+  bool is_managed_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(ArcPolicyBridge);
 };
--- chrome/browser/chromeos/arc/arc_policy_bridge_unittest.cc
+++ chrome/browser/chromeos/arc/arc_policy_bridge_unittest.cc
@@ -31,6 +31,7 @@ class ArcPolicyBridgeTest : public testing::Test {
     bridge_service_.reset(new FakeArcBridgeService());
     policy_bridge_.reset(
         new ArcPolicyBridge(bridge_service_.get(), &policy_service_));
+    policy_bridge_->OverrideIsManagedForTesting(true);
 
     EXPECT_CALL(policy_service_,
                 GetPolicies(policy::PolicyNamespace(
@@ -70,6 +71,11 @@ class ArcPolicyBridgeTest : public testing::Test {
 
   DISALLOW_COPY_AND_ASSIGN(ArcPolicyBridgeTest);
 };
+
+TEST_F(ArcPolicyBridgeTest, UnmanagedTest) {
+  policy_bridge()->OverrideIsManagedForTesting(false);
+  policy_bridge()->GetPolicies(PolicyStringCallback(nullptr));
+}
 
 TEST_F(ArcPolicyBridgeTest, EmptyPolicyTest) {
   // No policy is set, result should be empty.
