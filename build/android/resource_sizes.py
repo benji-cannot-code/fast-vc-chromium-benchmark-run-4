899bb20e1fd07138976b4e346689e573a59175ac
rnephew <rnephew@chromium.org>
2016-04-28 14:37:05 -0700

    [Android] Add logging to resource_sizes.py
    BUG=605618, 605665
    
    Review-Url: https://codereview.chromium.org/1925063003
    Cr-Commit-Position: refs/heads/master@{#390492}

--- build/android/resource_sizes.py
+++ build/android/resource_sizes.py
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import collections
 import json
+import logging
 import operator
 import optparse
 import os
@@ -409,6 +410,7 @@ Pass any number of files to graph their sizes. Any files with the extension
 
   if chartjson:
     results_path = os.path.join(options.output_dir, 'results-chart.json')
+    logging.critical('Dumping json to %s', results_path)
     with open(results_path, 'w') as json_file:
       json.dump(chartjson, json_file)
 
