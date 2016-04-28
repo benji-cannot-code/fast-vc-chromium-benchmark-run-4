2ae901c76be049b87d18cbf0cf36150dc1e4dca5
liberato <liberato@chromium.org>
2016-04-28 15:05:25 -0700

    Add -ifsameorigin to autoplay experiment.
    If -ifsameorigin is set in the autoplay experiment config, then
    allow autoplay only for those things that are in the same origin.
    
    BUG=599690
    
    Review-Url: https://codereview.chromium.org/1853543002
    Cr-Commit-Position: refs/heads/master@{#390500}

--- third_party/WebKit/Source/core/html/AutoplayExperimentTest.cpp
+++ third_party/WebKit/Source/core/html/AutoplayExperimentTest.cpp
@@ -53,6 +53,8 @@ public:
             .WillByDefault(Return(false));
         ON_CALL(*this, pageVisibilityState())
             .WillByDefault(Return(PageVisibilityStateVisible));
+        ON_CALL(*this, isCrossOrigin())
+            .WillByDefault(Return(false));
         ON_CALL(*this, absoluteBoundingBoxRect())
             .WillByDefault(Return(
                 IntRect(10, 10, 100, 100)));
@@ -86,6 +88,7 @@ public:
     MOCK_METHOD0(isLegacyViewportType, bool());
     MOCK_CONST_METHOD0(pageVisibilityState, PageVisibilityState());
     MOCK_CONST_METHOD0(autoplayExperimentMode, String());
+    MOCK_CONST_METHOD0(isCrossOrigin, bool());
     MOCK_METHOD1(setRequestPositionUpdates, void(bool));
     MOCK_CONST_METHOD0(absoluteBoundingBoxRect, IntRect());
 
@@ -432,4 +435,23 @@ TEST_F(AutoplayExperimentTest, DeferPlaybackUntilInViewport)
         .Times(1);
     moveIntoViewport();
 }
+
+TEST_F(AutoplayExperimentTest, WithSameOriginTests)
+{
+    setInterface(new NiceMock<MockAutoplayClient>("enabled-forvideo-ifsameorigin", MockAutoplayClient::Video));
+    ON_CALL(*m_client, isCrossOrigin()).WillByDefault(Return(false));
+    EXPECT_TRUE(isEligible());
+    ON_CALL(*m_client, isCrossOrigin()).WillByDefault(Return(true));
+    EXPECT_FALSE(isEligible());
+}
+
+TEST_F(AutoplayExperimentTest, WithoutSameOriginTests)
+{
+    setInterface(new NiceMock<MockAutoplayClient>("enabled-forvideo", MockAutoplayClient::Video));
+    ON_CALL(*m_client, isCrossOrigin()).WillByDefault(Return(false));
+    EXPECT_TRUE(isEligible());
+    ON_CALL(*m_client, isCrossOrigin()).WillByDefault(Return(true));
+    EXPECT_TRUE(isEligible());
+}
+
 }
