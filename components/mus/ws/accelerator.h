9f7db446fb54d7f6ae622b792fe3d9d1fdfd0b53
jamescook <jamescook@chromium.org>
2016-04-28 10:08:41 -0700

    mus: Rename input_event_matcher.mojom to event_matcher.mojom
    It exists to define the struct EventMatcher (not InputEventMatcher) so make
    the filename match the struct name.
    
    BUG=none
    TEST=compiles
    
    Review-Url: https://codereview.chromium.org/1921553007
    Cr-Commit-Position: refs/heads/master@{#390410}

--- components/mus/ws/accelerator.h
+++ components/mus/ws/accelerator.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "components/mus/public/interfaces/input_event_matcher.mojom.h"
+#include "components/mus/public/interfaces/event_matcher.mojom.h"
 #include "components/mus/ws/event_matcher.h"
 
 namespace ui {
