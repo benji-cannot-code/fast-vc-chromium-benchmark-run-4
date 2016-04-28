d867313e0a26c8f40446ef5be0f843d9e11b162c
twellington <twellington@chromium.org>
2016-04-28 15:50:27 -0700

    Remove unneded lines in tab_layer_unittest.cc
    BUG=604065
    
    Review-Url: https://codereview.chromium.org/1925163002
    Cr-Commit-Position: refs/heads/master@{#390523}

--- chrome/browser/android/compositor/layer/tab_layer_unittest.cc
+++ chrome/browser/android/compositor/layer/tab_layer_unittest.cc
@@ -11,9 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using chrome::android::TabLayer;
 
-// A test class for TabLayer class.
-class TabLayerTest : public testing::Test {};
-
 TEST(ComputePaddingPositionsTest, NoSideOrBottomPadding) {
   gfx::Size content_size(100, 400);
   gfx::Size desired_size(100, 400);
