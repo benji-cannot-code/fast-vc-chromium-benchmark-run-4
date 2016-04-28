3d359d20d98a8bf6d097fb2a5fad5e386e38700a
enne <enne@chromium.org>
2016-04-28 15:54:43 -0700

    Remove BeginFrameObserver::AsValueInto
    This doesn't seem to add much, and all BeginFrameSourceObservers have to
    implement this function, which makes the interface more cumbersome.
    
    R=sunnyps@chromium.org
    BUG=420607
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review-Url: https://codereview.chromium.org/1909683003
    Cr-Commit-Position: refs/heads/master@{#390525}

--- cc/scheduler/begin_frame_source.cc
+++ cc/scheduler/begin_frame_source.cc
@@ -49,14 +49,6 @@ void BeginFrameObserverBase::OnBeginFrame(const BeginFrameArgs& args) {
   }
 }
 
-void BeginFrameObserverBase::AsValueInto(
-    base::trace_event::TracedValue* dict) const {
-  dict->BeginDictionary("last_begin_frame_args_");
-  last_begin_frame_args_.AsValueInto(dict);
-  dict->EndDictionary();
-  dict->SetInteger("dropped_begin_frame_args_", dropped_begin_frame_args_);
-}
-
 // BeginFrameSourceBase ------------------------------------------------------
 BeginFrameSourceBase::BeginFrameSourceBase()
     : paused_(false), inside_as_value_into_(false) {}
@@ -102,29 +94,6 @@ void BeginFrameSourceBase::SetBeginFrameSourcePaused(bool paused) {
   std::set<BeginFrameObserver*> observers(observers_);
   for (BeginFrameObserver* obs : observers)
     obs->OnBeginFrameSourcePausedChanged(paused_);
-}
-
-// Tracing support
-void BeginFrameSourceBase::AsValueInto(
-    base::trace_event::TracedValue* dict) const {
-  // As the observer might try to trace the source, prevent an infinte loop
-  // from occuring.
-  if (inside_as_value_into_) {
-    dict->SetString("observer", "<loop detected>");
-    return;
-  }
-
-  {
-    base::AutoReset<bool> prevent_loops(
-        const_cast<bool*>(&inside_as_value_into_), true);
-    dict->BeginArray("observers");
-    for (const BeginFrameObserver* obs : observers_) {
-      dict->BeginDictionary();
-      obs->AsValueInto(dict);
-      dict->EndDictionary();
-    }
-    dict->EndArray();
-  }
 }
 
 // BackToBackBeginFrameSource --------------------------------------------
@@ -191,13 +160,6 @@ void BackToBackBeginFrameSource::SendPendingBeginFrames() {
     obs->OnBeginFrame(args);
 }
 
-// Tracing support
-void BackToBackBeginFrameSource::AsValueInto(
-    base::trace_event::TracedValue* dict) const {
-  dict->SetString("type", "BackToBackBeginFrameSource");
-  BeginFrameSourceBase::AsValueInto(dict);
-}
-
 // SyntheticBeginFrameSource ---------------------------------------------
 SyntheticBeginFrameSource::SyntheticBeginFrameSource(
     base::SingleThreadTaskRunner* task_runner,
@@ -261,17 +223,6 @@ void SyntheticBeginFrameSource::OnTimerTick() {
       it->OnBeginFrame(args);
     }
   }
-}
-
-// Tracing support
-void SyntheticBeginFrameSource::AsValueInto(
-    base::trace_event::TracedValue* dict) const {
-  dict->SetString("type", "SyntheticBeginFrameSource");
-  BeginFrameSourceBase::AsValueInto(dict);
-
-  dict->BeginDictionary("time_source");
-  time_source_->AsValueInto(dict);
-  dict->EndDictionary();
 }
 
 }  // namespace cc
