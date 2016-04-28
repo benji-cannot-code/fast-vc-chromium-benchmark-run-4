56b703aac93909f633522a869c4ee6c242794455
xidachen <xidachen@chromium.org>
2016-04-28 11:49:19 -0700

    Remove the duplicate function body SerializedScriptValueForModulesFactory::create
    The class SerializedScriptValueFactory has exactly the same function create()
    with the almost identical function body as the
    SerializedScriptValueForModulesFactory::create(), the only difference
    is that the create() under core/ uses a SerializedScriptValueWriter
    while the create() under modules/ uses a SerializedScriptValueWriterForModules,
    which is a subclass of SerializedScriptValueWriter.
    This CL creates a common function create() which takes a SerializedScriptValueWriter
    as a parameter, and let both create() functions call the common create()
    function. In that case, we won't have duplicate function bodies.
    
    We should be able to rely on our bots, as long as there is no broken
    tests, it should be fine.
    
    Review-Url: https://codereview.chromium.org/1932563002
    Cr-Commit-Position: refs/heads/master@{#390435}

--- third_party/WebKit/Source/bindings/core/v8/SerializedScriptValueFactory.cpp
+++ third_party/WebKit/Source/bindings/core/v8/SerializedScriptValueFactory.cpp
@@ -18,10 +18,9 @@ namespace blink {
 
 SerializedScriptValueFactory* SerializedScriptValueFactory::m_instance = 0;
 
-PassRefPtr<SerializedScriptValue> SerializedScriptValueFactory::create(v8::Isolate* isolate, v8::Local<v8::Value> value, Transferables* transferables, WebBlobInfoArray* blobInfo, ExceptionState& exceptionState)
+PassRefPtr<SerializedScriptValue> SerializedScriptValueFactory::create(v8::Isolate* isolate, v8::Local<v8::Value> value, SerializedScriptValueWriter& writer, Transferables* transferables, WebBlobInfoArray* blobInfo, ExceptionState& exceptionState)
 {
     RefPtr<SerializedScriptValue> serializedValue = create();
-    SerializedScriptValueWriter writer;
     ScriptValueSerializer::Status status;
     String errorMessage;
     {
@@ -36,7 +35,7 @@ PassRefPtr<SerializedScriptValue> SerializedScriptValueFactory::create(v8::Isola
     switch (status) {
     case ScriptValueSerializer::InputError:
     case ScriptValueSerializer::DataCloneError:
-        exceptionState.throwDOMException(ScriptValueSerializer::DataCloneError, errorMessage);
+        exceptionState.throwDOMException(DataCloneError, errorMessage);
         return serializedValue.release();
     case ScriptValueSerializer::Success:
         transferData(serializedValue.get(), writer, transferables, exceptionState, isolate);
@@ -47,6 +46,12 @@ PassRefPtr<SerializedScriptValue> SerializedScriptValueFactory::create(v8::Isola
     }
     ASSERT_NOT_REACHED();
     return serializedValue.release();
+}
+
+PassRefPtr<SerializedScriptValue> SerializedScriptValueFactory::create(v8::Isolate* isolate, v8::Local<v8::Value> value, Transferables* transferables, WebBlobInfoArray* blobInfo, ExceptionState& exceptionState)
+{
+    SerializedScriptValueWriter writer;
+    return create(isolate, value, writer, transferables, blobInfo, exceptionState);
 }
 
 PassRefPtr<SerializedScriptValue> SerializedScriptValueFactory::create(v8::Isolate* isolate, v8::Local<v8::Value> value, Transferables* transferables, ExceptionState& exceptionState)
