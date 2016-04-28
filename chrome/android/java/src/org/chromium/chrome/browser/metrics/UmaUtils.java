583b843fd0ae55812279a7d15da880d4f7dadc2b
asvitkine <asvitkine@chromium.org>
2016-04-28 16:10:06 -0700

    Histogram to debug the timing window that caused a drop in UMA.
    Also switches UmaUtils.getMainEntryPointTime() to uptimeMillis() from
    currentTimeMillis(), so that it's not affected by system clock changes.
    
    BUG=607289
    
    Review-Url: https://codereview.chromium.org/1916713007
    Cr-Commit-Position: refs/heads/master@{#390531}

--- chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaUtils.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.metrics;
 
+import android.os.SystemClock;
+
 import org.chromium.base.annotations.CalledByNative;
 
 /**
@@ -25,7 +27,7 @@ public class UmaUtils {
         // isn't initialized until we start the native content browser component, and we
         // then need the start time in the C++ side before we return to Java. As such we
         // save it in a static that the C++ can fetch once it has initialized the JNI.
-        sApplicationStartWallClockMs = System.currentTimeMillis();
+        sApplicationStartWallClockMs = SystemClock.uptimeMillis();
     }
 
     /**
--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsObserver.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.tab;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.os.SystemClock;
 import android.support.annotation.IntDef;
 import android.view.View;
 
@@ -214,7 +215,7 @@ public class TabWebContentsObserver extends WebContentsObserver {
             //   s^b = 60000
             //   b = ln(60000) / ln(1.05) ~= 225
             RecordHistogram.recordCustomTimesHistogram("Startup.FirstCommitNavigationTime",
-                    System.currentTimeMillis() - UmaUtils.getMainEntryPointTime(),
+                    SystemClock.uptimeMillis() - UmaUtils.getMainEntryPointTime(),
                     1, 60000 /* 1 minute */, TimeUnit.MILLISECONDS, 225);
             UmaUtils.setRunningApplicationStart(false);
         }
