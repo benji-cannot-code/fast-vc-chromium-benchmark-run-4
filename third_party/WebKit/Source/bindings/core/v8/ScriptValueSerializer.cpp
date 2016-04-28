8ddeafd369638c091b0b13f8e63849ecb19c2af3
xidachen <xidachen@chromium.org>
2016-04-28 10:43:46 -0700

    Change word neutered to detached for ImageBitmap
    In the specification, when an ImageBitmap is transferred to worker, that
    ImageBitmap is detached instead of neutered. This CL changes the word
    neutered to detached.
    
    Notice that we only change the word when throws the exception, which is
    the place where users are able to see. The function names such as
    isNeutered is not changed.
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/1927033002
    Cr-Commit-Position: refs/heads/master@{#390414}

--- third_party/WebKit/Source/bindings/core/v8/ScriptValueSerializer.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ScriptValueSerializer.cpp
@@ -1054,7 +1054,7 @@ ScriptValueSerializer::StateBase* ScriptValueSerializer::writeImageBitmap(v8::Lo
     if (!imageBitmap)
         return nullptr;
     if (imageBitmap->isNeutered())
-        return handleError(DataCloneError, "An ImageBitmap is neutered and could not be cloned.", next);
+        return handleError(DataCloneError, "An ImageBitmap is detached and could not be cloned.", next);
     OwnPtr<uint8_t[]> pixelData = imageBitmap->copyBitmapData(PremultiplyAlpha);
     m_writer.writeImageBitmap(imageBitmap->width(), imageBitmap->height(), pixelData.get(), imageBitmap->width() * imageBitmap->height() * 4);
     return nullptr;
@@ -1124,7 +1124,7 @@ ScriptValueSerializer::StateBase* ScriptValueSerializer::writeTransferredImageBi
     if (!imageBitmap)
         return 0;
     if (imageBitmap->isNeutered())
-        return handleError(DataCloneError, "An ImageBitmap is neutered and could not be cloned.", next);
+        return handleError(DataCloneError, "An ImageBitmap is detached and could not be cloned.", next);
     m_writer.writeTransferredImageBitmap(index);
     return 0;
 }
