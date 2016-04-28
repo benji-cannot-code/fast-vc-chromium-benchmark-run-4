1eb8368ce7a2920f363d4a4801924ca7b609acbb
estark <estark@chromium.org>
2016-04-27 18:08:51 -0700

    Use document referrer policy when preloading
    Previously, preload requests used the referrer policy from meta tags
    encountered during scanning, but not from headers delivered with the
    page. This CL uses the document's current referrer policy when the
    preload scan starts.
    
    BUG=605451
    
    Review-Url: https://codereview.chromium.org/1913983002
    Cr-Commit-Position: refs/heads/master@{#390264}

---
+++ third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/referrer-from-document-on-preload.php
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header("Content-Security-Policy: referrer origin-when-cross-origin");
+?>
+<html>
+<head>
+</head>
+<img src="http://localhost:8000/security/resources/green-if-referrer-is-origin.php" />
+</html>
