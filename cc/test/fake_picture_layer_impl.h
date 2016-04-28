19e7187a11e84021be71b924b4ecd75d929adb22
vmpstr <vmpstr@chromium.org>
2016-04-27 17:52:11 -0700

    cc: Cleanup PictureLayerImpl friends/aliases.
    This patch removes a friend declaration for a non-existent class as well
    as moves the alias declaration to the test class where it is used.
    
    R=danakj
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review-Url: https://codereview.chromium.org/1917253005
    Cr-Commit-Position: refs/heads/master@{#390261}

--- cc/test/fake_picture_layer_impl.h
+++ cc/test/fake_picture_layer_impl.h
@@ -18,6 +18,8 @@ namespace cc {
 
 class FakePictureLayerImpl : public PictureLayerImpl {
  public:
+  using TileRequirementCheck = bool (PictureLayerTiling::*)(const Tile*) const;
+
   static std::unique_ptr<FakePictureLayerImpl> Create(LayerTreeImpl* tree_impl,
                                                       int id) {
     bool is_mask = false;
