352794e4c95a776aab283f693472af7d87f23d8d
fs <fs@opera.com>
2016-04-29 08:07:10 -0700

    Move isEmptyValue and emptyValue to SVGCharacterData
    These two helpers have a stronger tie to SVGCharacterData (on which they
    operate) than to SVGTextLayoutAttributes - where they are currently
    defined.
    Move them as described, and also add simple query helpers to make code
    using them simpler and more readable.
    
    BUG=607906
    
    Review-Url: https://codereview.chromium.org/1933183002
    Cr-Commit-Position: refs/heads/master@{#390649}

--- third_party/WebKit/Source/core/layout/svg/SVGTextLayoutAttributes.h
+++ third_party/WebKit/Source/core/layout/svg/SVGTextLayoutAttributes.h
@@ -34,6 +34,15 @@ struct SVGCharacterData {
     DISALLOW_NEW_EXCEPT_PLACEMENT_NEW();
     SVGCharacterData();
 
+    static float emptyValue() { return std::numeric_limits<float>::quiet_NaN(); }
+    static bool isEmptyValue(float value) { return std::isnan(value); }
+
+    bool hasX() const { return !isEmptyValue(x); }
+    bool hasY() const { return !isEmptyValue(y); }
+    bool hasDx() const { return !isEmptyValue(dx); }
+    bool hasDy() const { return !isEmptyValue(dy); }
+    bool hasRotate() const { return !isEmptyValue(rotate); }
+
     float x;
     float y;
     float dx;
@@ -64,11 +73,11 @@ private:
 };
 
 inline SVGCharacterData::SVGCharacterData()
-    : x(SVGTextLayoutAttributes::emptyValue())
-    , y(SVGTextLayoutAttributes::emptyValue())
-    , dx(SVGTextLayoutAttributes::emptyValue())
-    , dy(SVGTextLayoutAttributes::emptyValue())
-    , rotate(SVGTextLayoutAttributes::emptyValue())
+    : x(emptyValue())
+    , y(emptyValue())
+    , dx(emptyValue())
+    , dy(emptyValue())
+    , rotate(emptyValue())
 {
 }
 
