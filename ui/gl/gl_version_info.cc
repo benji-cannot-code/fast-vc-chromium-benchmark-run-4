6fbd85c76d99db8fc31e9f0a3ce60cb065daa3d3
pasko <pasko@chromium.org>
2016-04-29 10:27:04 -0700

    Revert of Fix ReadPixels from float fbo buffer in ES2/WebGL1. (patchset #2 id:20001 of https://codereview.chromium.org/1927593005/ )
    Reason for revert:
    Reason: crbug.com/607936
    
    (GLReadbackTest.ReadPixelsFloat fails on "Lollipop Tablet Tester")
    
    Original issue's description:
    > Fix ReadPixels from float fbo buffer in ES2/WebGL1.
    >
    > This CL change the default format/type from RGBA/UNSIGNED_BYTE to RGBA/FLOAT.
    >
    > Also, simplify querying IMPLMENTATION_READ_COLOR_FORMAT/TYPE by
    > always calling down to the driver (even if the two enums are not
    > supported) and fall back to internal decision if a GL error is
    > generated.
    >
    > BUG=607283
    > TEST=webgl_conformance,webgl2_conformance
    > R=piman@chromium.org,kbr@chromium.org,bajones@chromium.org
    > CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    TBR=bajones@chromium.org,kbr@chromium.org,piman@chromium.org,zmo@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=607283
    
    Review-Url: https://codereview.chromium.org/1929203003
    Cr-Commit-Position: refs/heads/master@{#390684}

--- ui/gl/gl_version_info.cc
+++ ui/gl/gl_version_info.cc
@@ -44,12 +44,11 @@ GLVersionInfo::GLVersionInfo(const char* version_str, const char* renderer_str)
       is_angle(false),
       major_version(0),
       minor_version(0),
-      is_es2(false),
       is_es3(false),
       is_desktop_core_profile(false) {
   if (version_str) {
     ParseVersionString(version_str, &major_version, &minor_version,
-                       &is_es, &is_es2, &is_es3);
+                       &is_es, &is_es3);
   }
   if (renderer_str) {
     is_angle = base::StartsWith(renderer_str, "ANGLE",
@@ -61,13 +60,11 @@ void GLVersionInfo::ParseVersionString(const char* version_str,
                                        unsigned* major_version,
                                        unsigned* minor_version,
                                        bool* is_es,
-                                       bool* is_es2,
                                        bool* is_es3) {
   // Make sure the outputs are always initialized.
   *major_version = 0;
   *minor_version = 0;
   *is_es = false;
-  *is_es2 = false;
   *is_es3 = false;
   if (!version_str)
     return;
@@ -85,8 +82,6 @@ void GLVersionInfo::ParseVersionString(const char* version_str,
       *minor_version = minor;
     }
   }
-  if (*is_es && *major_version == 2)
-    *is_es2 = true;
   if (*is_es && *major_version == 3)
     *is_es3 = true;
   DCHECK(major_version != 0);
