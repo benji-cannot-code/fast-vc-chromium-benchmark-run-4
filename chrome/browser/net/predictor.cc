932f87ac3e81f42ed7687b9e0579da9309e47811
r.karu <r.karu@samsung.com>
2016-04-29 01:17:44 -0700

    [CleanUp] Removed unused inclusions of 'base/profiler/scoped_tracker.h'.
    Removed unused inclusions of 'base/profiler/scoped_tracker.h' header
    file from 'chrome/browser'.
    
    BUG=580534
    
    Review-Url: https://codereview.chromium.org/1616073005
    Cr-Commit-Position: refs/heads/master@{#390603}

--- chrome/browser/net/predictor.cc
+++ chrome/browser/net/predictor.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/metrics/histogram.h"
-#include "base/profiler/scoped_tracker.h"
 #include "base/single_thread_task_runner.h"
 #include "base/stl_util.h"
 #include "base/strings/string_split.h"
