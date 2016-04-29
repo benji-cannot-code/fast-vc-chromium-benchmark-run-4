d6a3937443756a28ebb10ed3994cee735844b84c
fs <fs@opera.com>
2016-04-29 13:56:41 -0700

    Minor tweaks to m_needsReordering in LayoutSVGText::layout
    Nit fixes from https://codereview.chromium.org/1933193002/.
    
    BUG=607906
    
    Review-Url: https://codereview.chromium.org/1933413002
    Cr-Commit-Position: refs/heads/master@{#390753}

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGText.cpp
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGText.cpp
@@ -185,6 +185,8 @@ static inline void checkLayoutAttributesConsistency(LayoutSVGText* text, Vector<
 void LayoutSVGText::layout()
 {
     ASSERT(needsLayout());
+    // This flag is set and reset as needed only within this function.
+    ASSERT(!m_needsReordering);
     LayoutAnalyzer::Scope analyzer(*this);
 
     bool updateParentBoundaries = false;
@@ -193,9 +195,6 @@ void LayoutSVGText::layout()
         m_needsTransformUpdate = false;
         updateParentBoundaries = true;
     }
-
-    // This flag is set and reset as needed only within this function.
-    ASSERT(!m_needsReordering);
 
     // When laying out initially, build the character data map and propagate
     // resulting layout attributes to all LayoutSVGInlineText children in the
@@ -257,8 +256,7 @@ void LayoutSVGText::layout()
     LayoutUnit paintInvalidationLogicalBottom;
     layoutInlineChildren(true, paintInvalidationLogicalTop, paintInvalidationLogicalBottom, afterEdge);
 
-    if (m_needsReordering)
-        m_needsReordering = false;
+    m_needsReordering = false;
 
     // If we don't have any line boxes, then make sure the frame rect is still cleared.
     if (!firstLineBox())
