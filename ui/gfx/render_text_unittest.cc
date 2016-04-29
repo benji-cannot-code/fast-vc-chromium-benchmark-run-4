36dda52c989991139cbd0fbefc3f87a0471451c1
tapted <tapted@chromium.org>
2016-04-28 18:22:21 -0700

    RenderText: Don't default-construct gfx::Fonts unnecessarily
    The default constructor for gfx::Font is expensive. Don't use it when
    creating text runs. Instead, reference an existing font.
    
    RenderTextMac doesn't need the gfx::Font at all, so just retain the
    CTFont on the run, since constructing a gfx::Font from an NSFont is also
    expensive.
    
    BUG=605131
    
    Review-Url: https://codereview.chromium.org/1925033002
    Cr-Commit-Position: refs/heads/master@{#390556}

--- ui/gfx/render_text_unittest.cc
+++ ui/gfx/render_text_unittest.cc
@@ -2708,7 +2708,7 @@ TEST_F(RenderTextTest, HarfBuzz_Clusters) {
     },
   };
 
-  internal::TextRunHarfBuzz run;
+  internal::TextRunHarfBuzz run((Font()));
   run.range = Range(0, 4);
   run.glyph_count = 4;
   run.glyph_to_char.resize(4);
@@ -2791,7 +2791,7 @@ TEST_F(RenderTextTest, HarfBuzz_SubglyphGraphemePartition) {
     },
   };
 
-  internal::TextRunHarfBuzz run;
+  internal::TextRunHarfBuzz run((Font()));
   run.range = Range(0, 4);
   run.glyph_count = 2;
   run.glyph_to_char.resize(2);
@@ -2942,7 +2942,7 @@ TEST_F(RenderTextTest, HarfBuzz_NonExistentFont) {
 
 // Ensure an empty run returns sane values to queries.
 TEST_F(RenderTextTest, HarfBuzz_EmptyRun) {
-  internal::TextRunHarfBuzz run;
+  internal::TextRunHarfBuzz run((Font()));
   const base::string16 kString = ASCIIToUTF16("abcdefgh");
   std::unique_ptr<base::i18n::BreakIterator> iter(new base::i18n::BreakIterator(
       kString, base::i18n::BreakIterator::BREAK_CHARACTER));
