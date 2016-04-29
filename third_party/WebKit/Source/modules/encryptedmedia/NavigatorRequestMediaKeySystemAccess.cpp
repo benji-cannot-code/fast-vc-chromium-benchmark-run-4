f8ace342f530261bcb7c99b6ba10d0fb7678df3e
jrummell <jrummell@chromium.org>
2016-04-29 12:10:03 -0700

    EME: Correctly handle container-only contentType strings
    Container-only contentType strings are now only allowed if they
    imply a codec. Since none of the supported containers fall into
    this category, ignore the configuration if codecs= is not specified.
    
    For compatability with existing applications, allow (and add a
    deprecation message) if "audio/webm", "video/webm", "audio/mp4",
    or "video/mp4" are used without a codec string. Also add UMA
    statistics so we can tell when this exception can be removed.
    The UMA will log the the number of successful calls where
    audioCapabilities or videoCapabilities are specified, and whether
    the contentType string contains codecs= or not.
    
    For compatability with other browsers (which may not enforce this),
    the deprecation message is only generated on a successful
    requestMediaKeySystemAccess() call (provided the successful
    configuration specified a contentType without codecs=). This
    will allow clients to attempt requestMediaKeySystemAccess() with
    and without codecs= until all browsers enforce this requirement.
    
    BUG=605661
    TEST=updated EME layout tests passes
    
    Review-Url: https://codereview.chromium.org/1911953003
    Cr-Commit-Position: refs/heads/master@{#390711}

--- third_party/WebKit/Source/modules/encryptedmedia/NavigatorRequestMediaKeySystemAccess.cpp
+++ third_party/WebKit/Source/modules/encryptedmedia/NavigatorRequestMediaKeySystemAccess.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/WebVector.h"
 #include "wtf/Vector.h"
 #include "wtf/text/WTFString.h"
+#include <algorithm>
 
 namespace blink {
 
@@ -80,6 +81,11 @@ static WebVector<WebEncryptedMediaSessionType> convertSessionTypes(const Vector<
     return result;
 }
 
+static bool AreCodecsSpecified(const WebMediaKeySystemMediaCapability& capability)
+{
+    return !capability.codecs.isEmpty();
+}
+
 // This class allows capabilities to be checked and a MediaKeySystemAccess
 // object to be created asynchronously.
 class MediaKeySystemAccessInitializer final : public EncryptedMediaRequest {
@@ -111,6 +117,12 @@ private:
     // TODO(xhwang): Remove after we handle empty robustness correctly.
     // See http://crbug.com/482277
     void checkVideoCapabilityRobustness() const;
+
+    // Generate deprecation warning and log UseCounter if configuration
+    // contains only container-only contentType strings.
+    // TODO(jrummell): Remove once this is no longer allowed.
+    // See http://crbug.com/605661.
+    void checkEmptyCodecs(const WebMediaKeySystemConfiguration&);
 
     Member<ScriptPromiseResolver> m_resolver;
     const String m_keySystem;
@@ -155,6 +167,8 @@ MediaKeySystemAccessInitializer::MediaKeySystemAccessInitializer(ScriptState* sc
 
 void MediaKeySystemAccessInitializer::requestSucceeded(WebContentDecryptionModuleAccess* access)
 {
+    checkEmptyCodecs(access->getConfiguration());
+
     m_resolver->resolve(new MediaKeySystemAccess(m_keySystem, adoptPtr(access)));
     m_resolver.clear();
 }
@@ -200,6 +214,33 @@ void MediaKeySystemAccessInitializer::checkVideoCapabilityRobustness() const
         m_resolver->getExecutionContext()->addConsoleMessage(ConsoleMessage::create(JSMessageSource, WarningMessageLevel,
             "It is recommended that a robustness level be specified. Not specifying the robustness level could "
             "result in unexpected behavior in the future, potentially including failure to play."));
+    }
+}
+
+void MediaKeySystemAccessInitializer::checkEmptyCodecs(const WebMediaKeySystemConfiguration& config)
+{
+    // This is only checking for empty codecs in the selected configuration,
+    // as apps may pass container only contentType strings for compatibility
+    // with other implementations.
+    // This will only check that all returned capabilities do not contain
+    // codecs. This avoids alerting on configurations that will continue
+    // to succeed in the future once strict checking is enforced.
+    bool areAllAudioCodecsEmpty = false;
+    if (config.hasAudioCapabilities && !config.audioCapabilities.isEmpty()) {
+        areAllAudioCodecsEmpty = std::find_if(config.audioCapabilities.begin(), config.audioCapabilities.end(), AreCodecsSpecified)
+            == config.audioCapabilities.end();
+    }
+
+    bool areAllVideoCodecsEmpty = false;
+    if (config.hasVideoCapabilities && !config.videoCapabilities.isEmpty()) {
+        areAllVideoCodecsEmpty = std::find_if(config.videoCapabilities.begin(), config.videoCapabilities.end(), AreCodecsSpecified)
+            == config.videoCapabilities.end();
+    }
+
+    if (areAllAudioCodecsEmpty || areAllVideoCodecsEmpty) {
+        Deprecation::countDeprecation(m_resolver->getExecutionContext(), UseCounter::EncryptedMediaAllSelectedContentTypesMissingCodecs);
+    } else {
+        UseCounter::count(m_resolver->getExecutionContext(), UseCounter::EncryptedMediaAllSelectedContentTypesHaveCodecs);
     }
 }
 
