f4aacf0e0a94fdfdacf0fc56650dfe9e5efca721
dmazzoni <dmazzoni@chromium.org>
2016-04-29 11:08:08 -0700

    AccessibilityNodeInfo.setEditable requires a higher API level
    BUG=607832
    
    Review-Url: https://codereview.chromium.org/1931383002
    Cr-Commit-Position: refs/heads/master@{#390698}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/BrowserAccessibilityManager.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/BrowserAccessibilityManager.java
@@ -850,7 +850,6 @@ public class BrowserAccessibilityManager {
         }
         if (isEditableText) {
             node.setText(charSequence);
-            node.setEditable(true);
         } else {
             node.setContentDescription(charSequence);
         }
@@ -917,7 +916,7 @@ public class BrowserAccessibilityManager {
 
     @CalledByNative
     protected void setAccessibilityNodeInfoKitKatAttributes(AccessibilityNodeInfo node,
-            boolean isRoot, String roleDescription) {
+            boolean isRoot, boolean isEditableText, String roleDescription) {
         // Requires KitKat or higher.
     }
 
