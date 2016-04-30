82dd148f06fa90a7a0274b7bf0fb1d61c4a8f907
aizatsky <aizatsky@chromium.org>
2016-04-29 23:45:34 -0700

    [libfuzzer] woff2ttf fuzzer
    All corpus seed woff files were collect from Chromium source tree.
    
    BUG=539572
    
    Review-Url: https://codereview.chromium.org/1939573002
    Cr-Commit-Position: refs/heads/master@{#390846}

---
+++ testing/libfuzzer/fuzzers/convert_woff2ttf_fuzzer.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "third_party/woff2/src/woff2_dec.h"
+
+// Entry point for LibFuzzer.
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  std::string buf;
+  woff2::WOFF2StringOut out(&buf);
+  out.SetMaxSize(30 * 1024 * 1024);
+  woff2::ConvertWOFF2ToTTF(data, size, &out);
+  return 0;
+}
