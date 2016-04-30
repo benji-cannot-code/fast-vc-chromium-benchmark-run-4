b0fbdf6c49685731a6ca941438a3d3324203ebae
oshima <oshima@chromium.org>
2016-04-29 17:08:17 -0700

    Rename gfx::Display/Screen to display::Display/Screen in ui/views
    BUG=597761
    
    Review-Url: https://codereview.chromium.org/1939563002
    Cr-Commit-Position: refs/heads/master@{#390812}

--- ui/views/mus/screen_mus_unittest.cc
+++ ui/views/mus/screen_mus_unittest.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/mus/screen_mus.h"
 
 #include "base/command_line.h"
-#include "ui/gfx/screen.h"
+#include "ui/display/screen.h"
 #include "ui/gfx/switches.h"
 #include "ui/views/mus/window_manager_connection.h"
 #include "ui/views/test/scoped_views_test_helper.h"
@@ -20,10 +20,10 @@ TEST(ScreenMusTest, ConsistentDisplayInHighDPI) {
   base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
       switches::kForceDeviceScaleFactor, "2");
   ScopedViewsTestHelper test_helper;
-  gfx::Screen* screen = gfx::Screen::GetScreen();
-  std::vector<gfx::Display> displays = screen->GetAllDisplays();
+  display::Screen* screen = display::Screen::GetScreen();
+  std::vector<display::Display> displays = screen->GetAllDisplays();
   ASSERT_FALSE(displays.empty());
-  for (const gfx::Display& display : displays) {
+  for (const display::Display& display : displays) {
     EXPECT_EQ(2.f, display.device_scale_factor());
     EXPECT_EQ(display.work_area(), display.bounds());
   }
