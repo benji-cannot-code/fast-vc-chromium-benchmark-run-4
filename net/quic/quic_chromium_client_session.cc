ebd0813170424a0f81db3afc74e7d4db0f3b9295
krasin <krasin@google.com>
2016-04-30 11:57:51 -0700

    quic: check for stream_factory_ to be non-null before all accesses.
    One of the missing checks was the reason for net_unittests to fail
    under ubsan_vptr mode.
    
    BUG=607996
    
    Review-Url: https://codereview.chromium.org/1934043002
    Cr-Commit-Position: refs/heads/master@{#390859}

--- net/quic/quic_chromium_client_session.cc
+++ net/quic/quic_chromium_client_session.cc
@@ -706,7 +706,7 @@ void QuicChromiumClientSession::OnClosedStream() {
     request->OnRequestCompleteSuccess(CreateOutgoingReliableStreamImpl());
   }
 
-  if (GetNumOpenOutgoingStreams() == 0) {
+  if (GetNumOpenOutgoingStreams() == 0 && stream_factory_) {
     stream_factory_->OnIdleSession(this);
   }
 }
@@ -910,7 +910,9 @@ void QuicChromiumClientSession::OnSuccessfulVersionNegotiation(
 }
 
 void QuicChromiumClientSession::OnPathDegrading() {
-  stream_factory_->MaybeMigrateSessionEarly(this);
+  if (stream_factory_) {
+    stream_factory_->MaybeMigrateSessionEarly(this);
+  }
 }
 
 void QuicChromiumClientSession::OnProofValid(
