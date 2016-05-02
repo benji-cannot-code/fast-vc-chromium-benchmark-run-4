d089c1847e069a14526d44eafec16470a85796d5
weiliangc <weiliangc@chromium.org>
2016-05-02 11:10:26 -0700

    cc: Stop cache transform invertibility at Layer and LayerImpl
    Transform invertibility is cached just to be used at PreCalcMetaInfo.
    Even if this caching is needed, could do it on transform tree in the
    future.
    
    R=ajuma
    BUG=
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review-Url: https://codereview.chromium.org/1906003002
    Cr-Commit-Position: refs/heads/master@{#390992}

--- cc/trees/property_tree_builder.cc
+++ cc/trees/property_tree_builder.cc
@@ -107,16 +107,6 @@ static inline bool IsRootLayer(const Layer* layer) {
   return !layer->parent();
 }
 
-static bool HasInvertibleOrAnimatedTransform(Layer* layer) {
-  return layer->transform_is_invertible() ||
-         layer->HasPotentiallyRunningTransformAnimation();
-}
-
-static bool HasInvertibleOrAnimatedTransformForTesting(LayerImpl* layer) {
-  return layer->transform().IsInvertible() ||
-         layer->HasPotentiallyRunningTransformAnimation();
-}
-
 static bool IsMetaInformationRecomputationNeeded(Layer* layer) {
   return layer->layer_tree_host()->needs_meta_info_recomputation();
 }
@@ -133,12 +123,6 @@ static void PreCalculateMetaInformationInternal(
 
   if (layer->clip_parent())
     recursive_data->num_unclipped_descendants++;
-
-  if (!HasInvertibleOrAnimatedTransform(layer)) {
-    // Layers with singular transforms should not be drawn, the whole subtree
-    // can be skipped.
-    return;
-  }
 
   for (size_t i = 0; i < layer->children().size(); ++i) {
     Layer* child_layer = layer->child_at(i);
@@ -172,12 +156,6 @@ static void PreCalculateMetaInformationInternalForTesting(
     PreCalculateMetaInformationRecursiveData* recursive_data) {
   if (layer->test_properties()->clip_parent)
     recursive_data->num_unclipped_descendants++;
-
-  if (!HasInvertibleOrAnimatedTransformForTesting(layer)) {
-    // Layers with singular transforms should not be drawn, the whole subtree
-    // can be skipped.
-    return;
-  }
 
   for (size_t i = 0; i < layer->children().size(); ++i) {
     LayerImpl* child_layer = layer->child_at(i);
