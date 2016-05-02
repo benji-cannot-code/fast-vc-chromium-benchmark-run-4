312fd681ce22a0f047b3bb847dadb1e6f4376c7c
jsbell <jsbell@chromium.org>
2016-05-02 11:10:01 -0700

    Layout Tests: remove unneeded includes of testharness-helpers.js
    The bulk of the utilities in testharness-helpers have been upstreamed
    to testharness.js and most of the tests including the file don't need
    it any more.
    
    A few follow up CLs eliminate the remaining uses of helpers:
    
    https://codereview.chromium.org/1926813004 assert_promise_rejects
    https://codereview.chromium.org/1936533003 assert_will_be_idl_attribute
    https://codereview.chromium.org/1935703003 stringifyDOMObject
    
    Review-Url: https://codereview.chromium.org/1939553002
    Cr-Commit-Position: refs/heads/master@{#390991}

--- third_party/WebKit/LayoutTests/http/tests/fetch/resources/fetch-test-helpers.js
+++ third_party/WebKit/LayoutTests/http/tests/fetch/resources/fetch-test-helpers.js
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 if (self.importScripts) {
   importScripts('/resources/testharness.js');
-  importScripts('/resources/testharness-helpers.js');
   importScripts('/serviceworker/resources/test-helpers.js');
   importScripts('/fetch/resources/fetch-test-options.js');
 }
--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/interfaces-worker.js
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/interfaces-worker.js
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importScripts('interfaces.js');
 importScripts('worker-testharness.js');
-importScripts('/resources/testharness-helpers.js');
 
 var EVENT_HANDLER = 'object';
 
