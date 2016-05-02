40bc50379c1a02c330247819a000a96e132ba409
rouslan <rouslan@chromium.org>
2016-05-02 15:22:45 -0700

    Enable credit card scanning by default
    BUG=608388
    
    Review-Url: https://codereview.chromium.org/1939183003
    Cr-Commit-Position: refs/heads/master@{#391073}

--- components/autofill/core/browser/autofill_manager.cc
+++ components/autofill/core/browser/autofill_manager.cc
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/guid.h"
 #include "base/logging.h"
 #include "base/message_loop/message_loop.h"
-#include "base/metrics/field_trial.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/path_service.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_number_conversions.h"
@@ -250,12 +248,6 @@ bool AutofillManager::ShouldShowScanCreditCard(const FormData& form,
                                                const FormFieldData& field) {
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kDisableCreditCardScan)) {
-    return false;
-  }
-
-  if (base::FieldTrialList::FindFullName("CreditCardScan") == "Control" &&
-      !base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kEnableCreditCardScan)) {
     return false;
   }
 
