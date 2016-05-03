e2437862bf5f24553781a053c35f78ed36da1f00
yusufo <yusufo@chromium.org>
2016-05-03 16:52:11 -0700

    Fix issue with progress bar color
    Fix a regression introduced by
    https://codereview.chromium.org/1874953003/
    
    BUG=464377
    
    Review-Url: https://codereview.chromium.org/1947623002
    Cr-Commit-Position: refs/heads/master@{#391402}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/CustomTabToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/CustomTabToolbar.java
@@ -369,7 +369,7 @@ public class CustomTabToolbar extends ToolbarLayout implements LocationBar,
         if (getProgressBar() != null) {
             if (!ColorUtils.isUsingDefaultToolbarColor(getResources(),
                     getBackground().getColor())) {
-                getProgressBar().setThemeColor(getToolbarDataProvider().getPrimaryColor(), false);
+                getProgressBar().setThemeColor(getBackground().getColor(), false);
             } else {
                 getProgressBar().setBackgroundColor(ApiCompatibilityUtils.getColor(resources,
                         R.color.progress_bar_background));
