3334db024c05e5ed6f1dd740d859f9c580204b32
guidou <guidou@chromium.org>
2016-05-02 17:12:47 -0700

    Add support for device-change notifications to MediaStreamDispatcher and MediaStreamDispatcherHost
    This is a step towards supporting the "devicechange" event in Blink.
    
    BUG=388648
    
    Review-Url: https://codereview.chromium.org/1918173002
    Cr-Commit-Position: refs/heads/master@{#391114}

--- content/renderer/pepper/pepper_media_device_manager.h
+++ content/renderer/pepper/pepper_media_device_manager.h
@@ -73,6 +73,7 @@ class PepperMediaDeviceManager
                       const std::string& label,
                       const StreamDeviceInfo& device_info) override;
   void OnDeviceOpenFailed(int request_id) override;
+  void OnDevicesChanged() override;
 
   // Stream type conversion.
   static MediaStreamType FromPepperDeviceType(PP_DeviceType_Dev type);
