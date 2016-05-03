79edc9ddac7c8d5d0dae3d879d9b7a16b6ccbabc
torne <torne@chromium.org>
2016-05-03 05:18:18 -0700

    Try to diagnose blink reinitialisation in WebView.
    WebView somehow ends up shutting down and reinitialising blink in the
    same process sometimes, which isn't supported. We have no idea how to
    reproduce this, so this change adds assertions that verify that we never
    shut down InProcessRendererThread unless the browser is already being
    shut down. WebView only exits by the process being SIGKILLed, so the
    shutdown path is intended to be unreachable.
    
    This is a temporary change that will be reverted once we've found the
    stack that leads to the shutdown being initiated.
    
    BUG=514141
    
    Review-Url: https://codereview.chromium.org/1900513003
    Cr-Commit-Position: refs/heads/master@{#391207}

--- content/renderer/in_process_renderer_thread.cc
+++ content/renderer/in_process_renderer_thread.cc
@@ -16,12 +16,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+#if defined(OS_ANDROID)
+extern bool g_browser_main_loop_shutting_down;
+#endif
+
 InProcessRendererThread::InProcessRendererThread(
     const InProcessChildThreadParams& params)
     : Thread("Chrome_InProcRendererThread"), params_(params) {
 }
 
 InProcessRendererThread::~InProcessRendererThread() {
+#if defined(OS_ANDROID)
+  // Don't allow the render thread to be shut down in single process mode on
+  // Android unless the browser is shutting down.
+  // Temporary CHECK() to debug http://crbug.com/514141
+  CHECK(g_browser_main_loop_shutting_down);
+#endif
+
   Stop();
 }
 
@@ -32,12 +43,22 @@ void InProcessRendererThread::Init() {
   // will not change the thread name kept in Java VM.
 #if defined(OS_ANDROID)
   base::android::AttachCurrentThreadWithName(thread_name());
+  // Make sure we aren't somehow reinitialising the inprocess renderer thread on
+  // Android. Temporary CHECK() to debug http://crbug.com/514141
+  CHECK(!render_process_);
 #endif
   render_process_.reset(new RenderProcessImpl());
   RenderThreadImpl::Create(params_);
 }
 
 void InProcessRendererThread::CleanUp() {
+#if defined(OS_ANDROID)
+  // Don't allow the render thread to be shut down in single process mode on
+  // Android unless the browser is shutting down.
+  // Temporary CHECK() to debug http://crbug.com/514141
+  CHECK(g_browser_main_loop_shutting_down);
+#endif
+
   render_process_.reset();
 
   // It's a little lame to manually set this flag.  But the single process
