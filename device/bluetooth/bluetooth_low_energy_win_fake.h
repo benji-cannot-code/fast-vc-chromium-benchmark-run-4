461631ab72ad62858530a6886647ebecc320aea0
gogerald <gogerald@chromium.org>
2016-05-03 11:12:47 -0700

    Add BluetoothGattCharacteristicTest::StartNotifySession_Reentrant unit test
    This CL implements BluetoothGattCharacteristicTest::StartNotifySession_Reentrant unit test.
    It tests BluetoothGattCharacteristic::StartNotifySession in StartNotifySession error and success callback.
    
    BUG=579202
    
    Review-Url: https://codereview.chromium.org/1804093003
    Cr-Commit-Position: refs/heads/master@{#391299}

--- device/bluetooth/bluetooth_low_energy_win_fake.h
+++ device/bluetooth/bluetooth_low_energy_win_fake.h
@@ -66,6 +66,7 @@ struct GattCharacteristic {
   GattDescriptorsMap included_descriptors;
   std::vector<HRESULT> read_errors;
   std::vector<HRESULT> write_errors;
+  std::vector<HRESULT> notify_errors;
   std::vector<BLUETOOTH_GATT_EVENT_HANDLE> observers;
 };
 
@@ -178,6 +179,9 @@ class BluetoothLowEnergyWrapperFake : public BluetoothLowEnergyWrapper {
                                        const std::vector<uint8_t>& value);
   void SimulateCharacteristicValueChangeNotification(
       GattCharacteristic* characteristic);
+  void SimulateGattCharacteristicSetNotifyError(
+      GattCharacteristic* characteristic,
+      HRESULT error);
   void SimulateGattCharacteristicReadError(GattCharacteristic* characteristic,
                                            HRESULT error);
   void SimulateGattCharacteristicWriteError(GattCharacteristic* characteristic,
