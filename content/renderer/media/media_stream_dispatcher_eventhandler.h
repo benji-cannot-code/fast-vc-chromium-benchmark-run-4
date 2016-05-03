205d195e00d861eb99838e73e79df1b88315dae9
henrika <henrika@chromium.org>
2016-05-03 04:51:35 -0700

    Revert of Add support for device-change notifications to MediaStreamDispatcher (patchset #2 id:270001 of https://codereview.chromium.org/1943213002/ )
    Reason for revert:
    Still seems to break tests. See e.g.
    
    https://uberchromegw.corp.google.com/i/chromium.mac/builders/Mac10.9%20Tests/builds/20743/steps/content_unittests%20on%20Mac-10.9
    
    Original issue's description:
    > Reland of Add support for device-change notifications to MediaStreamDispatcher (patchset #1 id:1 of https://codereview.chromium.org/1939313003/ )
    >
    > Reason for revert:
    > This reland should fix the offending test.
    >
    > Original issue's description:
    > > Revert of Add support for device-change notifications to MediaStreamDispatcher and MediaStreamDispatcherHost (patchset #6 id:100001 of https://codereview.chromium.org/1918173002/ )
    > >
    > > Reason for revert:
    > > This has caused content_browsertests to fail on multiple builders, e.g.:
    > >
    > > https://build.chromium.org/p/chromium.linux/builders/Cast%20Linux/builds/23972
    > > https://build.chromium.org/p/chromium.linux/builders/Android%20GN/builds/34086
    > >
    > > Original issue's description:
    > > > Add support for device-change notifications to MediaStreamDispatcher and MediaStreamDispatcherHost
    > > >
    > > > This is a step towards supporting the "devicechange" event in Blink.
    > > >
    > > > BUG=388648
    > > >
    > > > Committed: https://crrev.com/ec334e1799ae25aaa1b0c66e25af641018a3a84a
    > > > Cr-Commit-Position: refs/heads/master@{#391114}
    > >
    > > TBR=tommi@chromium.org,palmer@chromium.org,bbudge@chromium.org,hta@chromium.org,guidou@chromium.org
    > > # Skipping CQ checks because original CL landed less than 1 days ago.
    > > NOPRESUBMIT=true
    > > NOTREECHECKS=true
    > > NOTRY=true
    > > BUG=388648
    > >
    > > Committed: https://crrev.com/2396227939dc83d23f2523a992b0e35da50d631e
    > > Cr-Commit-Position: refs/heads/master@{#391141}
    >
    > TBR=tommi@chromium.org,palmer@chromium.org,bbudge@chromium.org,hta@chromium.org,dpranke@chromium.org
    > # Not skipping CQ checks because original CL landed more than 1 days ago.
    > BUG=388648
    >
    > Committed: https://crrev.com/cb53c53e9816623ac51e9b151815897ba2a30b22
    > Cr-Commit-Position: refs/heads/master@{#391192}
    
    TBR=tommi@chromium.org,palmer@chromium.org,bbudge@chromium.org,hta@chromium.org,dpranke@chromium.org,perkj@chromium.org,guidou@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=388648
    
    Review-Url: https://codereview.chromium.org/1945603002
    Cr-Commit-Position: refs/heads/master@{#391202}

--- content/renderer/media/media_stream_dispatcher_eventhandler.h
+++ content/renderer/media/media_stream_dispatcher_eventhandler.h
@@ -47,8 +47,6 @@ class CONTENT_EXPORT MediaStreamDispatcherEventHandler {
   // Failed to open the device.
   virtual void OnDeviceOpenFailed(int request_id) = 0;
 
-  virtual void OnDevicesChanged() = 0;
-
  protected:
   virtual ~MediaStreamDispatcherEventHandler() {}
 };
