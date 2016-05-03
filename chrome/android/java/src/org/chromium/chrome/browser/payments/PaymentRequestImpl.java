fb60777fa9333b382ad387eef5c4f06ef13ced17
rouslan <rouslan@chromium.org>
2016-05-02 18:02:50 -0700

    Provide shipping in PaymentRequest result
    If the merchant provides non-NULL shipping options, the merchant does
    not need shipping address to calculate shipping costs. The UI selects
    the default shipping address, but does not send it to the merchant. This
    patch sends the shipping address to the merchant when the user clicks
    "Pay".
    
    BUG=608222
    
    Review-Url: https://codereview.chromium.org/1937783002
    Cr-Commit-Position: refs/heads/master@{#391133}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
@@ -506,6 +506,21 @@ public class PaymentRequestImpl implements PaymentRequest, PaymentRequestUI.Clie
         PaymentResponse response = new PaymentResponse();
         response.methodName = methodName;
         response.stringifiedDetails = stringifiedDetails;
+
+        PaymentOption selectedShippingAddress = mShippingAddresses.getSelectedItem();
+        if (selectedShippingAddress != null) {
+            // Shipping addresses are created in show(). The should all be instances of
+            // AutofillAddress.
+            assert selectedShippingAddress instanceof AutofillAddress;
+            response.shippingAddress =
+                    ((AutofillAddress) selectedShippingAddress).toShippingAddress();
+        }
+
+        PaymentOption selectedShippingOption = mShippingOptions.getSelectedItem();
+        if (selectedShippingOption != null && selectedShippingOption.getIdentifier() != null) {
+            response.shippingOptionId = selectedShippingOption.getIdentifier();
+        }
+
         mClient.onPaymentResponse(response);
     }
 
@@ -514,7 +529,7 @@ public class PaymentRequestImpl implements PaymentRequest, PaymentRequestUI.Clie
      */
     @Override
     public void onInstrumentDetailsError() {
-        disconnectFromClientWithDebugMessage("Fialed to retrieve payment instrument details");
+        disconnectFromClientWithDebugMessage("Failed to retrieve payment instrument details");
         closeUI(false);
     }
 
