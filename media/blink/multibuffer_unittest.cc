2c96604224b760307eccdff7dbf1d64447591223
hubbe <hubbe@chromium.org>
2016-05-04 16:35:19 -0700

    Tweak multibuffer buffering strategy.
    Separate buffer size from pin range.
    Buffering is now calculated as follows:
    
    target buffer size is max(25Mb, 12 seconds * bitrate)
    
    prefetch_low is max(20Mb, 10 seconds * bitrate)
    prefetch_high is prefetch_low + 1Mb
    
    pin_backwards is min(2 seconds * bitrate, 25Mb - prefetch_high)
    pin_forwards is 21Mb
    
    This means that the target buffer size is equivialent to BufferedDataSource.
    Note that we pin 21Mb in front of the current location no matter what, this
    means that if we happen to have that data in the cache we temporarily go over
    the target buffer size to avoid freeing something and then re-fetch it right
    afterwards.
    
    BUG=608974
    
    Review-Url: https://codereview.chromium.org/1954493002
    Cr-Commit-Position: refs/heads/master@{#391691}

--- media/blink/multibuffer_unittest.cc
+++ media/blink/multibuffer_unittest.cc
@@ -267,7 +267,7 @@ TEST_F(MultiBufferTest, ReadAll) {
   multibuffer_.SetMustReadWholeFile(true);
   MultiBufferReader reader(&multibuffer_, pos, end,
                            base::Callback<void(int64_t, int64_t)>());
-  reader.SetMaxBuffer(2000, 5000);
+  reader.SetPinRange(2000, 5000);
   reader.SetPreload(1000, 1000);
   while (pos < end) {
     unsigned char buffer[27];
@@ -295,7 +295,7 @@ TEST_F(MultiBufferTest, ReadAllAdvanceFirst) {
   multibuffer_.SetMustReadWholeFile(true);
   MultiBufferReader reader(&multibuffer_, pos, end,
                            base::Callback<void(int64_t, int64_t)>());
-  reader.SetMaxBuffer(2000, 5000);
+  reader.SetPinRange(2000, 5000);
   reader.SetPreload(1000, 1000);
   while (pos < end) {
     unsigned char buffer[27];
@@ -325,7 +325,7 @@ TEST_F(MultiBufferTest, ReadAllAdvanceFirst_NeverDefer) {
   multibuffer_.SetRangeSupported(true);
   MultiBufferReader reader(&multibuffer_, pos, end,
                            base::Callback<void(int64_t, int64_t)>());
-  reader.SetMaxBuffer(2000, 5000);
+  reader.SetPinRange(2000, 5000);
   reader.SetPreload(1000, 1000);
   while (pos < end) {
     unsigned char buffer[27];
@@ -356,7 +356,7 @@ TEST_F(MultiBufferTest, ReadAllAdvanceFirst_NeverDefer2) {
   multibuffer_.SetMaxBlocksAfterDefer(-10000);
   MultiBufferReader reader(&multibuffer_, pos, end,
                            base::Callback<void(int64_t, int64_t)>());
-  reader.SetMaxBuffer(2000, 5000);
+  reader.SetPinRange(2000, 5000);
   reader.SetPreload(1000, 1000);
   while (pos < end) {
     unsigned char buffer[27];
@@ -463,7 +463,7 @@ class ReadHelper {
                 pos_,
                 end_,
                 base::Callback<void(int64_t, int64_t)>()) {
-    reader_.SetMaxBuffer(2000, 5000);
+    reader_.SetPinRange(2000, 5000);
     reader_.SetPreload(1000, 1000);
   }
 
