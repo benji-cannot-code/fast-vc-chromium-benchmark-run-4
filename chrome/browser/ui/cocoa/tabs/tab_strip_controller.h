a92c408ed36657a589eaedf74d258b8de815336b
shrike <shrike@chromium.org>
2016-05-04 16:21:56 -0700

    [Mac][Material Design] Fix TabStripController test that fails under MD.
    This cl fixes a bug in one of the TabStripController unit tests. The
    test used hard-coded mouse coordinates which no longer work because of
    the new max tab width in Material Design.
    
    This cl also fixes a bug where the new Material Design max tab width
    was being used on non-MD.
    
    R=avi@chromium.org
    BUG=609254
    
    Review-Url: https://codereview.chromium.org/1946993004
    Cr-Commit-Position: refs/heads/master@{#391684}

--- chrome/browser/ui/cocoa/tabs/tab_strip_controller.h
+++ chrome/browser/ui/cocoa/tabs/tab_strip_controller.h
@@ -265,6 +265,9 @@ class WebContents;
 // Default indentation for tabs (see |leftIndentForControls_|).
 + (CGFloat)defaultLeftIndentForControls;
 
+// Returns the amount by which tabs overlap.
++ (CGFloat)tabOverlap;
+
 // Returns the currently active TabContentsController.
 - (TabContentsController*)activeTabContentsController;
 
