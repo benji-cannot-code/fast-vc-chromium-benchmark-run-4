e7b9067dbafe9e99fef9176055fe82fd3368c732
majidvp <majidvp@chromium.org>
2016-05-04 11:14:04 -0700

    Compute snap offsets according to CSS Scroll Snap Spec Level 1
    Add logic to compute snap offsets (those defined on snap container and snap
    areas) according to the published spec [1].
    
    SnapCoordinator maintains the mapping between snap containers and the snap
    areas. This mapping is stored in |LayoutBoxRareData| and updated on style
    change, attach, and detach events via hooks in |LayoutBox::styleWillChange| ,
    |LayoutBox::insertedIntoTree| and |LayoutBox::willBeDestoryed|.
    
    This implementation is the naive approach as described in [2]. It does not
    attempt to track which snap areas have moved in relation to their container to
    minimize  snap offsets re-computation. It simply re-computes all snap points
    every time they are needed.
    
    At the moment the computed snap offsets are not used for anything except to pass
    the tests. In future,  |SnapCoordinator| will compute them as part of the
    document lifecycle and send necessary updates to the Compositor Worker instance
    which is going to be responsible for snapping during scrolls.
    
    [1] https://www.w3.org/TR/2015/WD-css-snappoints-1-20150326/
    [2] https://docs.google.com/document/d/1BO0EgcMe3DTsO1uwM_psZeJ_Gz5OZR-oYrpJBKAJLoU/edit?pli=1
    
    Review-Url: https://codereview.chromium.org/1188563005
    Cr-Commit-Position: refs/heads/master@{#391562}

--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -161,6 +161,7 @@ class SegmentedString;
 class SelectorQueryCache;
 class SerializedScriptValue;
 class Settings;
+class SnapCoordinator;
 class StyleEngine;
 class StyleResolver;
 class StyleSheet;
@@ -1056,6 +1057,8 @@ public:
     }
     int nodeCount() const { return m_nodeCount; }
 
+    SnapCoordinator* snapCoordinator();
+
     using WeakDocumentSet = HeapHashSet<WeakMember<Document>>;
     static WeakDocumentSet& liveDocumentSet();
 
@@ -1395,6 +1398,8 @@ private:
     int m_nodeCount;
 
     bool m_mayContainV0Shadow = false;
+
+    Member<SnapCoordinator> m_snapCoordinator;
 };
 
 extern template class CORE_EXTERN_TEMPLATE_EXPORT Supplement<Document>;
