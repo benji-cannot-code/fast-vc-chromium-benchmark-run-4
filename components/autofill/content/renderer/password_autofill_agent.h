da1ff155feacac3c4f2d9ab3d881a9e5cf1a16a9
vabr <vabr@chromium.org>
2016-05-04 03:48:04 -0700

    Remove WeakPtrFactory from PasswordAutofillAgent
    Unlike in AutofillAgent, the factory is no longer used in PAA.
    
    R=dvadym@chromium.org
    BUG=609010,609007,608100,608101,433486
    
    Review-Url: https://codereview.chromium.org/1945723003
    Cr-Commit-Position: refs/heads/master@{#391475}

--- components/autofill/content/renderer/password_autofill_agent.h
+++ components/autofill/content/renderer/password_autofill_agent.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
-#include "base/memory/weak_ptr.h"
 #include "components/autofill/content/renderer/password_form_conversion_utils.h"
 #include "components/autofill/core/common/form_data_predictions.h"
 #include "components/autofill/core/common/password_form_field_prediction_map.h"
@@ -240,8 +239,6 @@ class PasswordAutofillAgent : public content::RenderFrameObserver {
   // Contains server predictions for username, password and/or new password
   // fields for individual forms.
   FormsPredictionsMap form_predictions_;
-
-  base::WeakPtrFactory<PasswordAutofillAgent> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(PasswordAutofillAgent);
 };
