ff4524f7e663ff0c260d9b243d4023237e111e98
msw <msw@chromium.org>
2016-05-04 14:28:37 -0700

    Close mash shelf tooltips on touches outside the shelf.
    Make ShelfTooltipManager a views::PointerWatcher.
    Close on mouse/touch press (including outside shelf root).
    
    BUG=595853
    TEST=Touches outside the shelf area close shelf tooltips.
    R=jamescook@chromium.org,sky@chromium.org
    
    Review-Url: https://codereview.chromium.org/1937673002
    Cr-Commit-Position: refs/heads/master@{#391647}

--- ash/shelf/shelf_tooltip_manager.h
+++ ash/shelf/shelf_tooltip_manager.h
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf_layout_manager_observer.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "base/strings/string16.h"
 #include "base/timer/timer.h"
 #include "ui/aura/window_observer.h"
 #include "ui/events/event_handler.h"
+#include "ui/views/pointer_watcher.h"
 
 namespace views {
 class BubbleDialogDelegateView;
@@ -32,6 +32,7 @@ class ShelfViewTest;
 // ShelfTooltipManager manages the tooltip bubble that appears for shelf items.
 class ASH_EXPORT ShelfTooltipManager : public ui::EventHandler,
                                        public aura::WindowObserver,
+                                       public views::PointerWatcher,
                                        public ShelfLayoutManagerObserver {
  public:
   explicit ShelfTooltipManager(ShelfView* shelf_view);
@@ -62,6 +63,12 @@ class ASH_EXPORT ShelfTooltipManager : public ui::EventHandler,
 
   // aura::WindowObserver overrides:
   void OnWindowDestroying(aura::Window* window) override;
+
+  // views::PointerWatcher overrides:
+  void OnMousePressed(const ui::MouseEvent& event,
+                      const gfx::Point& location_in_screen) override;
+  void OnTouchPressed(const ui::TouchEvent& event,
+                      const gfx::Point& location_in_screen) override;
 
   // ShelfLayoutManagerObserver overrides:
   void WillDeleteShelf() override;
