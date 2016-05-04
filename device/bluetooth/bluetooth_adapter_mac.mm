1f6d203f1fd7123f6c5cbd70111f7559e86571a9
jlebel <jlebel@chromium.org>
2016-05-04 11:15:27 -0700

    Cleaning BluetoothLowEnergyCentralManagerBridge.
    Removing virtual that are not useful, and set the delegate of CBCentralManager to nil, after deallocating BluetoothLowEnergyCentralManagerDelegate.
    
    Should have no side effect.
    
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/1949553002
    Cr-Commit-Position: refs/heads/master@{#391564}

--- device/bluetooth/bluetooth_adapter_mac.mm
+++ device/bluetooth/bluetooth_adapter_mac.mm
@@ -254,7 +254,7 @@ void BluetoothAdapterMac::SetCentralManagerForTesting(
       low_energy_central_manager_.get());
 }
 
-CBCentralManager* BluetoothAdapterMac::GetCentralManagerForTesting() {
+CBCentralManager* BluetoothAdapterMac::GetCentralManager() {
   return low_energy_central_manager_;
 }
 
