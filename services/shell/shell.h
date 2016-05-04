f562dfafc076020cd8e7be40f4e0fd4d6adae228
sky <sky@chromium.org>
2016-05-04 12:06:59 -0700

    Makes each Instance own the NativeRunner
    This makes lifetime easier to reason about.
    
    This gets rid of a feature, where it was possible for an app to exist
    once it closes its shell connection. We decided this feature is more
    confusing than useful, leading to it being hard to reason about
    shutdown.
    
    BUG=none
    TEST=covered by tests
    R=ben@chromium.org
    
    Review-Url: https://codereview.chromium.org/1953453002
    Cr-Commit-Position: refs/heads/master@{#391589}

--- services/shell/shell.h
+++ services/shell/shell.h
@@ -144,8 +144,6 @@ class Shell : public ShellClient {
 
   base::WeakPtr<Shell> GetWeakPtr();
 
-  void CleanupRunner(NativeRunner* runner);
-
   std::map<Identity, Instance*> identity_to_instance_;
 
   // Tracks the names of instances that are allowed to field connection requests
@@ -162,7 +160,6 @@ class Shell : public ShellClient {
 
   base::Callback<void(const Identity&)> instance_quit_callback_;
   std::unique_ptr<NativeRunnerFactory> native_runner_factory_;
-  std::vector<std::unique_ptr<NativeRunner>> native_runners_;
   std::unique_ptr<ShellConnection> shell_connection_;
   base::WeakPtrFactory<Shell> weak_ptr_factory_;
 
