3f10f99470a6e17b7d108599b3f3177c4f08d116
mahmadi <mahmadi@chromium.org>
2016-05-04 11:51:42 -0700

    MD User Manager: i18n-content -> $i18n{}
    BUG=563722
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/1943653002
    Cr-Commit-Position: refs/heads/master@{#391585}

--- chrome/browser/resources/md_user_manager/user_manager_tutorial.js
+++ chrome/browser/resources/md_user_manager/user_manager_tutorial.js
@@ -21,10 +21,6 @@ var TutorialSteps = {
 Polymer({
   is: 'user-manager-tutorial',
 
-  behaviors: [
-    I18nBehavior,
-  ],
-
   properties: {
     /**
      * True if the tutorial is currently hidden.
