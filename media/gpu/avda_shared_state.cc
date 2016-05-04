5dd6d14770ef7609a8cc37ba9329909b45d54d51
dalecurtis <dalecurtis@chromium.org>
2016-05-03 19:43:31 -0700

    Avoid waiting on the SurfaceTexture until we need to.
    We can let the SurfaceTexture operations complete in the background
    for back buffer rendering instead of waiting during frame delivery;
    this frees up the GPU thread for other tasks.
    
    Also reduces the wait time to be more inline with the UMA stats;
    some devices like the MotoX don't reliably deliver this event when
    the MediaCodec is underflowing. I've also seen the N7v2 drop this
    event every now and then.
    
    BUG=601066
    TEST=no playback regressions, less waiting.
    
    Review-Url: https://codereview.chromium.org/1924973004
    Cr-Commit-Position: refs/heads/master@{#391426}

--- media/gpu/avda_shared_state.cc
+++ media/gpu/avda_shared_state.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/gpu/avda_shared_state.h"
 
+#include "base/metrics/histogram_macros.h"
 #include "base/time/time.h"
 #include "media/gpu/avda_codec_image.h"
 #include "ui/gl/gl_bindings.h"
@@ -24,10 +25,33 @@ void AVDASharedState::SignalFrameAvailable() {
 }
 
 void AVDASharedState::WaitForFrameAvailable() {
-  // 10msec covers >99.9% of cases, so just wait for up to that much before
+  DCHECK(!release_time_.is_null());
+
+  // 5msec covers >99.9% of cases, so just wait for up to that much before
   // giving up.  If an error occurs, we might not ever get a notification.
-  const base::TimeDelta max_wait_time(base::TimeDelta::FromMilliseconds(10));
-  frame_available_event_.TimedWait(max_wait_time);
+  const base::TimeDelta max_wait = base::TimeDelta::FromMilliseconds(5);
+  const base::TimeTicks call_time = base::TimeTicks::Now();
+  const base::TimeDelta elapsed = call_time - release_time_;
+  const base::TimeDelta remaining = max_wait - elapsed;
+  release_time_ = base::TimeTicks();
+
+  if (remaining <= base::TimeDelta()) {
+    if (!frame_available_event_.IsSignaled()) {
+      DVLOG(1) << "Deferred WaitForFrameAvailable() timed out, elapsed: "
+               << elapsed.InMillisecondsF() << "ms";
+    }
+    return;
+  }
+
+  DCHECK_LE(remaining, max_wait);
+  SCOPED_UMA_HISTOGRAM_TIMER("Media.AvdaCodecImage.WaitTimeForFrame");
+  if (!frame_available_event_.TimedWait(remaining)) {
+    DVLOG(1) << "WaitForFrameAvailable() timed out, elapsed: "
+             << elapsed.InMillisecondsF()
+             << "ms, additionally waited: " << remaining.InMillisecondsF()
+             << "ms, total: " << (elapsed + remaining).InMillisecondsF()
+             << "ms";
+  }
 }
 
 void AVDASharedState::DidAttachSurfaceTexture() {
@@ -48,6 +72,7 @@ void AVDASharedState::DidDetachSurfaceTexture() {
 void AVDASharedState::CodecChanged(media::MediaCodecBridge* codec) {
   for (auto& image_kv : codec_images_)
     image_kv.second->CodecChanged(codec);
+  release_time_ = base::TimeTicks();
 }
 
 void AVDASharedState::SetImageForPicture(int picture_buffer_id,
@@ -66,6 +91,15 @@ AVDACodecImage* AVDASharedState::GetImageForPicture(
     int picture_buffer_id) const {
   auto it = codec_images_.find(picture_buffer_id);
   return it == codec_images_.end() ? nullptr : it->second;
+}
+
+void AVDASharedState::RenderCodecBufferToSurfaceTexture(
+    media::MediaCodecBridge* codec,
+    int codec_buffer_index) {
+  if (!release_time_.is_null())
+    WaitForFrameAvailable();
+  codec->ReleaseOutputBuffer(codec_buffer_index, true);
+  release_time_ = base::TimeTicks::Now();
 }
 
 }  // namespace media
