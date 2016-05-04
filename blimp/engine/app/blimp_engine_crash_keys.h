15fc89260f9257d5f0633768387eb8f6b8611ccb
marcinjb <marcinjb@chromium.org>
2016-05-04 11:35:27 -0700

    Initial addition of Blimp engine crash client code (take 2)
    This introduces a simple crash client for the engine, which includes a client
    that will always allow and always upload crash reports with the
    "Chrome_Blimp_Engine" name. Crash reporting is enabled for all processes.
    wget is added to the Docker container as it's necessary for crash uploads.
    
    This is based on http://crrev.com/1783053002, with the addition of
    missing //content crash keys (without which you'd check-fail), and a
    note to people who modify the list in //chrome/common/crash_keys.cc
    to also kindly update our list if they're changing things. This should
    get better when http://crbug.com/598854 is resolved.
    
    BUG=597454
    
    Review-Url: https://codereview.chromium.org/1925993004
    Cr-Commit-Position: refs/heads/master@{#391577}

---
+++ blimp/engine/app/blimp_engine_crash_keys.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BLIMP_ENGINE_APP_BLIMP_ENGINE_CRASH_KEYS_H_
+#define BLIMP_ENGINE_APP_BLIMP_ENGINE_CRASH_KEYS_H_
+
+#include <stddef.h>
+
+namespace blimp {
+namespace engine {
+
+// Registers the crash keys needed for the Blimp engine. Returns the size of the
+// union of all keys.
+size_t RegisterEngineCrashKeys();
+
+}  // namespace engine
+}  // namespace blimp
+
+#endif  // BLIMP_ENGINE_APP_BLIMP_ENGINE_CRASH_KEYS_H_
