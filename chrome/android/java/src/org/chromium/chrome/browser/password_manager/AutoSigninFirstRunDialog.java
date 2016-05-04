faebb2ed2917eb6823d2d1cccdc75b5dcd42985e
melandory <melandory@chromium.org>
2016-05-04 03:43:27 -0700

    [Smart Lock] Make Auto-signin first run experience dialog modal.
    BUG=609050
    
    Review-Url: https://codereview.chromium.org/1946163002
    Cr-Commit-Position: refs/heads/master@{#391473}

--- chrome/android/java/src/org/chromium/chrome/browser/password_manager/AutoSigninFirstRunDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/password_manager/AutoSigninFirstRunDialog.java
@@ -97,6 +97,7 @@ public class AutoSigninFirstRunDialog
         builder.setView(view);
 
         mDialog = builder.create();
+        mDialog.setCanceledOnTouchOutside(false);
         return mDialog;
     }
 
