30925c276f10cb01c69669c6c608bde833287bdc
erikchen <erikchen@chromium.org>
2016-05-05 15:49:01 -0700

    Pepper: Associate TextureMailboxes with the Graphics3D they were obtained from.
    This allows the TextureMailbox to be returned to the appropriate Graphics3D,
    which implicitly deals with trying to return a TextureMailbox to a null
    Graphics3D.
    
    BUG=602484
    
    Review-Url: https://codereview.chromium.org/1951263003
    Cr-Commit-Position: refs/heads/master@{#391934}

--- content/renderer/pepper/pepper_plugin_instance_impl.cc
+++ content/renderer/pepper/pepper_plugin_instance_impl.cc
@@ -779,12 +779,13 @@ void PepperPluginInstanceImpl::ScrollRect(int dx,
 void PepperPluginInstanceImpl::CommitTextureMailbox(
     const cc::TextureMailbox& texture_mailbox) {
   if (committed_texture_.IsValid() && !IsTextureInUse(committed_texture_)) {
-    bound_graphics_3d_->ReturnFrontBuffer(
+    committed_texture_graphics_3d_->ReturnFrontBuffer(
         committed_texture_.mailbox(), committed_texture_consumed_sync_token_,
         false);
   }
 
   committed_texture_ = texture_mailbox;
+  committed_texture_graphics_3d_ = bound_graphics_3d_;
   committed_texture_consumed_sync_token_ = gpu::SyncToken();
 
   if (!texture_layer_) {
@@ -805,7 +806,8 @@ void PepperPluginInstanceImpl::PassCommittedTextureToTextureLayer() {
   std::unique_ptr<cc::SingleReleaseCallback> callback(
       cc::SingleReleaseCallback::Create(base::Bind(
           &PepperPluginInstanceImpl::FinishedConsumingCommittedTexture,
-          weak_factory_.GetWeakPtr(), committed_texture_)));
+          weak_factory_.GetWeakPtr(), committed_texture_,
+          committed_texture_graphics_3d_)));
 
   IncrementTextureReferenceCount(committed_texture_);
   texture_layer_->SetTextureMailbox(committed_texture_, std::move(callback));
@@ -813,6 +815,7 @@ void PepperPluginInstanceImpl::PassCommittedTextureToTextureLayer() {
 
 void PepperPluginInstanceImpl::FinishedConsumingCommittedTexture(
     const cc::TextureMailbox& texture_mailbox,
+    scoped_refptr<PPB_Graphics3D_Impl> graphics_3d,
     const gpu::SyncToken& sync_token,
     bool is_lost) {
   bool removed = DecrementTextureReferenceCount(texture_mailbox);
@@ -825,8 +828,8 @@ void PepperPluginInstanceImpl::FinishedConsumingCommittedTexture(
   }
 
   if (removed && !is_committed_texture) {
-    bound_graphics_3d_->ReturnFrontBuffer(texture_mailbox.mailbox(), sync_token,
-                                          is_lost);
+    graphics_3d->ReturnFrontBuffer(texture_mailbox.mailbox(), sync_token,
+                                   is_lost);
   }
 }
 
