2a21dc2303a17f7fcbc5bf791b3a72ab3f7546e3
estade <estade@chromium.org>
2016-05-05 13:00:18 -0700

    Introduce new flag to control usage of MD in secondary (not top-chrome) UI.
    Example usage would be:
    
    --top-chrome-md="material-hybrid" --secondary-ui-md
    
    This is a separate flag instead of a new state for --top-chrome-md because it is orthogonal to the material/hybrid mode.
    
    For now, use this flag to control standard dialog/bubble buttons as well as the buttons in the
    bookmark bubble.
    
    BUG=512440
    
    Review-Url: https://codereview.chromium.org/1926943002
    Cr-Commit-Position: refs/heads/master@{#391882}

--- ui/views/window/dialog_client_view.cc
+++ ui/views/window/dialog_client_view.cc
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "build/build_config.h"
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/button/blue_button.h"
 #include "ui/views/controls/button/label_button.h"
+#include "ui/views/controls/button/md_text_button.h"
 #include "ui/views/layout/layout_constants.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/window/dialog_delegate.h"
@@ -309,10 +311,15 @@ LabelButton* DialogClientView::CreateDialogButton(ui::DialogButton type) {
   if (GetDialogDelegate()->UseNewStyleForThisDialog() &&
       GetDialogDelegate()->GetDefaultDialogButton() == type &&
       GetDialogDelegate()->ShouldDefaultButtonBeBlue()) {
-    button = new BlueButton(this, title);
+    if (ui::MaterialDesignController::IsSecondaryUiMaterial()) {
+      MdTextButton* md_button = MdTextButton::CreateMdButton(this, title);
+      md_button->SetCallToAction(MdTextButton::STRONG_CALL_TO_ACTION);
+      button = md_button;
+    } else {
+      button = new BlueButton(this, title);
+    }
   } else {
-    button = new LabelButton(this, title);
-    button->SetStyle(Button::STYLE_BUTTON);
+    button = MdTextButton::CreateSecondaryUiButton(this, title);
   }
   Button::ConfigureDefaultFocus(button);
 
