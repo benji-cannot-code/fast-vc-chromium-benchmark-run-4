35386a7f0814667dd292cd65147b5fe72d6f8eee
maxbogue <maxbogue@chromium.org>
2016-05-04 19:05:53 -0700

    [Sync] Clear LocalDeviceInfoProvider cache on sync DB destruction.
    Previously this cache would continue returning the old cache GUID and
    DeviceInfo, which are no longer valid after directory destruction.
    
    BUG=607287
    
    Review-Url: https://codereview.chromium.org/1951703002
    Cr-Commit-Position: refs/heads/master@{#391729}

--- components/sync_driver/local_device_info_provider_impl.h
+++ components/sync_driver/local_device_info_provider_impl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/threading/non_thread_safe.h"
 #include "components/sync_driver/device_info.h"
 #include "components/sync_driver/local_device_info_provider.h"
 #include "components/version_info/version_info.h"
@@ -15,7 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace browser_sync {
 
 class LocalDeviceInfoProviderImpl
-    : public sync_driver::LocalDeviceInfoProvider {
+    : public sync_driver::LocalDeviceInfoProvider,
+      public base::NonThreadSafe {
  public:
   LocalDeviceInfoProviderImpl(version_info::Channel channel,
                               const std::string& version,
@@ -32,6 +34,7 @@ class LocalDeviceInfoProviderImpl
       const scoped_refptr<base::TaskRunner>& blocking_task_runner) override;
   std::unique_ptr<Subscription> RegisterOnInitializedCallback(
       const base::Closure& callback) override;
+  void Clear() override;
 
  private:
   void InitializeContinuation(const std::string& guid,
