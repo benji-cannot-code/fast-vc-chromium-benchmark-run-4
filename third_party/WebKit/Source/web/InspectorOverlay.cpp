a6d333f74b02729702a83894a5a9e0a14648f13c
chrishtr <chrishtr@chromium.org>
2016-05-05 15:26:32 -0700

    Remove cache skipping for InspectorOverlay.
    It is not needed, since it does not use the same PaintController as the main
    document paint.
    
    Review-Url: https://codereview.chromium.org/1953083003
    Cr-Commit-Position: refs/heads/master@{#391927}

--- third_party/WebKit/Source/web/InspectorOverlay.cpp
+++ third_party/WebKit/Source/web/InspectorOverlay.cpp
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/ScriptForbiddenScope.h"
 #include "platform/graphics/GraphicsContext.h"
 #include "platform/graphics/paint/CullRect.h"
-#include "platform/graphics/paint/DisplayItemCacheSkipper.h"
 #include "platform/inspector_protocol/Values.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebData.h"
@@ -115,8 +114,6 @@ public:
         if (m_overlay->isEmpty())
             return;
 
-        // Skip cache because the following paint may conflict with the view's real painting.
-        DisplayItemCacheSkipper cacheSkipper(graphicsContext);
         FrameView* view = m_overlay->overlayMainFrame()->view();
         DCHECK(!view->needsLayout());
         view->paint(graphicsContext, CullRect(IntRect(0, 0, view->width(), view->height())));
