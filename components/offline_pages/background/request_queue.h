87d7f5a91d9ab3547f23d6d8495438ccfda71a07
fgorski <fgorski@chromium.org>
2016-05-05 14:37:58 -0700

    [Offline pages] Implementation of RequestQueue: Add, Remove, Get
    Basic implementation of request queue:
    * Add request
    * Remove request
    * Get requests
    * injecting store in ctor
    * Tests for above with exception of cases, where store issues an error.
    
    BUG=607221
    R=dougarnett@chromium.org,petewil@chromium.org
    
    Review-Url: https://codereview.chromium.org/1947253002
    Cr-Commit-Position: refs/heads/master@{#391910}

--- components/offline_pages/background/request_queue.h
+++ components/offline_pages/background/request_queue.h
@@ -7,12 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_OFFLINE_PAGES_BACKGROUND_REQUEST_QUEUE_H_
 
 #include <stdint.h>
+
+#include <memory>
 #include <vector>
 
 #include "base/callback.h"
+#include "base/macros.h"
 
 namespace offline_pages {
 
+class RequestQueueStore;
 class SavePageRequest;
 
 // Class responsible for managing save page requests.
@@ -49,6 +53,9 @@ class RequestQueue {
   // Callback used by |UdpateRequest| and |RemoveRequest|.
   typedef base::Callback<void(UpdateRequestResult)> UpdateRequestCallback;
 
+  explicit RequestQueue(std::unique_ptr<RequestQueueStore> store);
+  ~RequestQueue();
+
   // Gets all of the active requests from the store. Calling this method may
   // schedule purging of the request queue.
   void GetRequests(const GetRequestsCallback& callback);
@@ -79,6 +86,10 @@ class RequestQueue {
   // expired request. Result is returned through |callback| carries the number
   // of removed requests.
   void PurgeRequests(const PurgeRequestsCallback& callback);
+
+  std::unique_ptr<RequestQueueStore> store_;
+
+  DISALLOW_COPY_AND_ASSIGN(RequestQueue);
 };
 
 }  // namespace offline_pages
