41b538798ee1f194f7e6baa39dd9187787b2ba32
michaelpg <michaelpg@chromium.org>
2016-05-04 20:33:01 -0700

    Fail mocha browser_tests if no mocha tests run
    R=dbeam@chromium.org
    
    Review-Url: https://codereview.chromium.org/1949943002
    Cr-Commit-Position: refs/heads/master@{#391746}

--- chrome/test/data/webui/settings/device_page_tests.js
+++ chrome/test/data/webui/settings/device_page_tests.js
@@ -117,7 +117,7 @@ cr.define('settings_device_page', function() {
       };
 
       // First, wait for the initial call to getInfo.
-      var promise = fakeSystemDisplay.getInfoCalled.promise.then(function() {
+      return fakeSystemDisplay.getInfoCalled.promise.then(function() {
         expectTrue(!!displayPage.displays);
         expectEquals(0, displayPage.displays.length);
 
@@ -189,7 +189,7 @@ cr.define('settings_device_page', function() {
             });
           });
         });
-        return promise;
       });
     });
+  });
 });
