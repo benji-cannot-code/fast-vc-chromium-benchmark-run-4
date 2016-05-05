017c2dcd453da3d412fe825de90efd735b597927
shrike <shrike@chromium.org>
2016-05-05 13:16:29 -0700

    [Mac][Material Design] Fix BookmarkBubbleControllerTest.TestBubbleWindow
    This change adjusts the test for containment of the bubble window in the
    test browser window. Metrics have changed under Material Design, so the
    bokomarks window no longer fits completely within the test browser
    window.
    
    R=avi@chromium.org
    BUG=609346
    
    Review-Url: https://codereview.chromium.org/1952893002
    Cr-Commit-Position: refs/heads/master@{#391889}

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bubble_controller_unittest.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bubble_controller_unittest.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
 #include "testing/platform_test.h"
+#include "ui/base/material_design/material_design_controller.h"
 
 using base::ASCIIToUTF16;
 using bookmarks::BookmarkBubbleObserver;
@@ -120,16 +121,22 @@ class BookmarkBubbleControllerTest : public CocoaProfileTest {
 // static
 int BookmarkBubbleControllerTest::edits_;
 
-// Confirm basics about the bubble window (e.g. that it is inside the
-// parent window)
+// Confirm basics about the bubble window (e.g. that its frame fits inside, or
+// almost completely fits inside, its parent window's frame).
 TEST_F(BookmarkBubbleControllerTest, TestBubbleWindow) {
   const BookmarkNode* node = CreateTestBookmark();
   BookmarkBubbleController* controller = ControllerForNode(node);
   EXPECT_TRUE(controller);
   NSWindow* window = [controller window];
   EXPECT_TRUE(window);
-  EXPECT_TRUE(NSContainsRect([browser()->window()->GetNativeWindow() frame],
-                             [window frame]));
+  NSRect browser_window_frame = [browser()->window()->GetNativeWindow() frame];
+  // The metrics have changed slightly under Material Design, so that in this
+  // test case the bookmarks bubble's window frame extendeds slightly beyond its
+  // parent window's frame.
+  if (ui::MaterialDesignController::IsModeMaterial()) {
+    browser_window_frame.size.width += 1;
+  }
+  EXPECT_TRUE(NSContainsRect(browser_window_frame, [window frame]));
 }
 
 // Test that we can handle closing the parent window
--- chrome/browser/ui/cocoa/location_bar/autocomplete_text_field_cell.mm
+++ chrome/browser/ui/cocoa/location_bar/autocomplete_text_field_cell.mm
@@ -37,7 +37,13 @@ const CGFloat kRightDecorationXOffset = 5.0;
 
 // The amount of padding on either side reserved for drawing
 // decorations.  [Views has |kItemPadding| == 3.]
-const CGFloat kDecorationHorizontalPad = 3.0;
+CGFloat DecorationsHorizontalPad() {
+  const CGFloat kDecorationHorizontalPad = 3.0;
+  const CGFloat kMaterialDecorationHorizontalPad = 4.0;
+  return ui::MaterialDesignController::IsModeMaterial()
+      ? kMaterialDecorationHorizontalPad
+      : kDecorationHorizontalPad;
+}
 
 const ui::NinePartImageIds kPopupBorderImageIds =
     IMAGE_GRID(IDR_OMNIBOX_POPUP_BORDER_AND_SHADOW);
@@ -72,6 +78,7 @@ void CalculatePositionsHelper(
   // The initial padding depends on whether the first visible decoration is
   // a button or not.
   bool is_first_visible_decoration = true;
+  const CGFloat kDecorationHorizontalPad = DecorationsHorizontalPad();
 
   for (size_t i = 0; i < all_decorations.size(); ++i) {
     if (all_decorations[i]->IsVisible()) {
@@ -273,6 +280,7 @@ size_t CalculatePositionsInFrame(
 
   // Determine the left-most extent for the i-beam cursor.
   CGFloat minX = NSMinX(textFrame);
+  const CGFloat kDecorationHorizontalPad = DecorationsHorizontalPad();
   for (size_t index = left_count; index--; ) {
     if (decorations[index]->AcceptsMousePress())
       break;
@@ -431,6 +439,7 @@ size_t CalculatePositionsInFrame(
                             &decorations, &decorationFrames, &workingFrame);
 
   // Draw the decorations.
+  const CGFloat kDecorationHorizontalPad = DecorationsHorizontalPad();
   for (size_t i = 0; i < decorations.size(); ++i) {
     if (decorations[i]) {
       NSRect background_frame = NSInsetRect(
