9e2de8aabe6f8b11d698fead6d6ef7154cb42d28
jaydasika <jaydasika@chromium.org>
2016-05-05 08:43:35 -0700

    cc : Add subtree hidden bool to effect tree
    This is required for making the property tree sync at commit time
    independant of layer properties. Adding it to the effect tree also
    makes it possible to delete LayerImpl::hide_layer_and_subtree. I have
    deleted it and added it to LayerImplTestProperties for tests.
    
    BUG=609205
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review-Url: https://codereview.chromium.org/1947683007
    Cr-Commit-Position: refs/heads/master@{#391809}

--- cc/layers/layer_impl.h
+++ cc/layers/layer_impl.h
@@ -209,9 +209,6 @@ class CC_EXPORT LayerImpl {
   void SetDrawsContent(bool draws_content);
   bool DrawsContent() const { return draws_content_; }
 
-  void SetHideLayerAndSubtree(bool hide);
-  bool hide_layer_and_subtree() const { return hide_layer_and_subtree_; }
-
   LayerImplTestProperties* test_properties() {
     if (!test_properties_)
       test_properties_.reset(new LayerImplTestProperties());
@@ -244,7 +241,6 @@ class CC_EXPORT LayerImpl {
 
   void SetOpacity(float opacity);
   float opacity() const { return opacity_; }
-  float EffectiveOpacity() const;
   bool OpacityIsAnimating() const;
   bool HasPotentiallyRunningOpacityAnimation() const;
   bool OpacityIsAnimatingOnImplOnly() const;
@@ -603,7 +599,6 @@ class CC_EXPORT LayerImpl {
   bool should_check_backface_visibility_ : 1;
   bool draws_content_ : 1;
   bool is_drawn_render_surface_layer_list_member_ : 1;
-  bool hide_layer_and_subtree_ : 1;
 
   bool is_affected_by_page_scale_ : 1;
 
