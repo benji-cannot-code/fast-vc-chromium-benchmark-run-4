f5bc48879be1a4526ae13fb134d5cb972495446f
sigbjornf <sigbjornf@opera.com>
2016-05-05 11:25:27 -0700

    Initialize DeferredTaskHandler's audio thread ID once.
    Avoid repeatedly recording the audio thread's ID, along with assertedly
    verifying that this is done off the main thread.
    
    R=
    BUG=590108
    
    Review-Url: https://codereview.chromium.org/1954663002
    Cr-Commit-Position: refs/heads/master@{#391848}

--- third_party/WebKit/Source/modules/webaudio/DeferredTaskHandler.cpp
+++ third_party/WebKit/Source/modules/webaudio/DeferredTaskHandler.cpp
@@ -280,5 +280,16 @@ void DeferredTaskHandler::clearHandlersToBeDeleted()
     m_deletableOrphanHandlers.clear();
 }
 
+void DeferredTaskHandler::setAudioThreadToCurrentThread()
+{
+    ASSERT(!isMainThread());
+    if (m_audioThread) {
+        ASSERT(m_audioThread == currentThread());
+        return;
+    }
+    ThreadIdentifier thread = currentThread();
+    releaseStore(&m_audioThread, thread);
+}
+
 } // namespace blink
 
