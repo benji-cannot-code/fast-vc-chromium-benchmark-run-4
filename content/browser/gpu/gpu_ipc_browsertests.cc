f51bf56ee29ef2071d52b52ede9d3ad8d1d85e5f
danakj <danakj@chromium.org>
2016-05-04 17:46:07 -0700

    Remove WebGraphicsContext3D reason from the CauseForGpuLaunch enum
    Replace "WEBGRAPHICSCONTEXT3DCOMMANDBUFFERIMPL_INITIALIZE" with a bunch
    of more specific and real reasons.
    
    R=piman
    BUG=584497
    
    Review-Url: https://codereview.chromium.org/1950723002
    Cr-Commit-Position: refs/heads/master@{#391721}

--- content/browser/gpu/gpu_ipc_browsertests.cc
+++ content/browser/gpu/gpu_ipc_browsertests.cc
@@ -22,9 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const content::CauseForGpuLaunch kInitCause =
-    content::
-        CAUSE_FOR_GPU_LAUNCH_WEBGRAPHICSCONTEXT3DCOMMANDBUFFERIMPL_INITIALIZE;
+constexpr content::CauseForGpuLaunch kInitCause =
+    content::CAUSE_FOR_GPU_LAUNCH_BROWSER_STARTUP;
 
 scoped_refptr<content::ContextProviderCommandBuffer> CreateContext(
     scoped_refptr<gpu::GpuChannelHost> gpu_channel_host) {
