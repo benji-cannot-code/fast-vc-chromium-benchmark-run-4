4ae8838d3a50265ba8f98901d681423c5e48ec1f
jbauman <jbauman@chromium.org>
2016-05-05 14:39:23 -0700

    Make context current before ReusePictureBuffer.
    eglStreamConsumerReleaseKHR needs the context where the texture ids are
    to be current when it's called.
    
    BUG=574292
    
    Review-Url: https://codereview.chromium.org/1947343002
    Cr-Commit-Position: refs/heads/master@{#391912}

--- media/gpu/dxva_video_decode_accelerator_win.cc
+++ media/gpu/dxva_video_decode_accelerator_win.cc
@@ -968,6 +968,9 @@ void DXVAVideoDecodeAccelerator::ReusePictureBuffer(int32_t picture_buffer_id) {
   if (it->second->available() || it->second->waiting_to_reuse())
     return;
 
+  RETURN_AND_NOTIFY_ON_FAILURE(make_context_current_cb_.Run(),
+                               "Failed to make context current",
+                               PLATFORM_FAILURE, );
   if (use_keyed_mutex_ || using_angle_device_) {
     RETURN_AND_NOTIFY_ON_FAILURE(it->second->ReusePictureBuffer(),
                                  "Failed to reuse picture buffer",
@@ -980,9 +983,6 @@ void DXVAVideoDecodeAccelerator::ReusePictureBuffer(int32_t picture_buffer_id) {
                                 base::Unretained(this)));
     }
   } else {
-    RETURN_AND_NOTIFY_ON_FAILURE(make_context_current_cb_.Run(),
-                                 "Failed to make context current",
-                                 PLATFORM_FAILURE, );
     it->second->ResetReuseFence();
 
     WaitForOutputBuffer(picture_buffer_id, 0);
