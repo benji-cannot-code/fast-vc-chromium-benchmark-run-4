55df433dd8baac9bcee2bb8e91e93559704a73f1
afakhry <afakhry@chromium.org>
2016-05-06 13:01:29 -0700

    Make Chromevox speak the instructions text of the keyboard overlay
    This CL does the following:
    - Adds the instructions div at the top of the tree, so it's the first thing to be spoken.
    - Makes the instructions div hidden by default, until after a short delay after the widget
      is shown. After that it's focused so Chromevox reads all the instructions, not only the first
      paragraph.
    - Adds aria-live=polite to the instructions div.
    - Removes the keyboard_overlay_accessibility_helper, since the it's not working, and the
      cvox.Api will be deprecated anyway.
    
    BUG=463581
    
    Review-Url: https://codereview.chromium.org/1931293004
    Cr-Commit-Position: refs/heads/master@{#392131}

--- ash/content/keyboard_overlay/keyboard_overlay_delegate.cc
+++ ash/content/keyboard_overlay/keyboard_overlay_delegate.cc
@@ -54,6 +54,7 @@ void PaintMessageHandler::RegisterMessages() {
 void PaintMessageHandler::DidPaint(const base::ListValue* args) {
   // Show the widget after the web content has been painted.
   widget_->Show();
+  web_ui()->CallJavascriptFunction("onWidgetShown");
 }
 
 }  // namespace
