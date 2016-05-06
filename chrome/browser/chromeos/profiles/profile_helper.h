550f7e506cf864e7ff5816407993856618fa676a
xdai <xdai@chromium.org>
2016-05-06 13:07:20 -0700

    GetAccountIdFromProfile() should not return an empty account id if a valid profile is provided.
    On Chrome OS, we don't end the session immediately if a refresh token is revoked in the session. Thus it's possible that we get an empty account id for a profile from Profile::GetProfileUserName(). We should avoid this scenario on Chrome OS.
    
    BUG=602807, 532535
    
    Review-Url: https://codereview.chromium.org/1880923003
    Cr-Commit-Position: refs/heads/master@{#392133}

--- chrome/browser/chromeos/profiles/profile_helper.h
+++ chrome/browser/chromeos/profiles/profile_helper.h
@@ -34,6 +34,10 @@ class MultiUserWindowManagerChromeOSTest;
 }  // namespace test
 }  // namespace ash
 
+namespace test {
+class BrowserFinderChromeOSTest;
+}  // namespace test
+
 namespace chromeos {
 
 class FileFlusher;
@@ -156,6 +160,7 @@ class ProfileHelper
   friend class SessionStateDelegateChromeOSTest;
   friend class SystemTrayDelegateChromeOSTest;
   friend class ash::test::MultiUserWindowManagerChromeOSTest;
+  friend class ::test::BrowserFinderChromeOSTest;
 
   // Called when signin profile is cleared.
   void OnSigninProfileCleared();
