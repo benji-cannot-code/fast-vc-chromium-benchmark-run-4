36f93a03bf76c08e5ba109237d79f3d5835441b5
mek <mek@chromium.org>
2016-05-06 14:20:42 -0700

    Keep PaymentRequest alive when its promises are outstanding.
    The PaymentRequest is responsible for resolving the show and complete
    promises, so it needs to be kept alive as long as either of these
    promises haven't resolved yet. This makes sure of that.
    
    Also reverts the earlier change that "stopped" outstanding promises
    when the context is destroyed, as that doesn't actually do anything.
    
    BUG=606577
    
    Review-Url: https://codereview.chromium.org/1954973005
    Cr-Commit-Position: refs/heads/master@{#392157}

--- third_party/WebKit/Source/modules/payments/PaymentRequest.cpp
+++ third_party/WebKit/Source/modules/payments/PaymentRequest.cpp
@@ -249,14 +249,14 @@ void PaymentRequest::onUpdatePaymentDetails(const ScriptValue& detailsScriptValu
     V8PaymentDetails::toImpl(detailsScriptValue.isolate(), detailsScriptValue.v8Value(), details, exceptionState);
     if (exceptionState.hadException()) {
         m_showResolver->reject(DOMException::create(SyntaxError, exceptionState.message()));
-        stopResolversAndCloseMojoConnection();
+        clearResolversAndCloseMojoConnection();
         return;
     }
 
     validatePaymentDetails(details, exceptionState);
     if (exceptionState.hadException()) {
         m_showResolver->reject(DOMException::create(SyntaxError, exceptionState.message()));
-        stopResolversAndCloseMojoConnection();
+        clearResolversAndCloseMojoConnection();
         return;
     }
 
@@ -275,7 +275,7 @@ void PaymentRequest::onUpdatePaymentDetailsFailure(const ScriptValue& error)
         m_showResolver->reject(error);
     if (m_completeResolver)
         m_completeResolver->reject(error);
-    stopResolversAndCloseMojoConnection();
+    clearResolversAndCloseMojoConnection();
 }
 
 DEFINE_TRACE(PaymentRequest)
@@ -291,6 +291,7 @@ DEFINE_TRACE(PaymentRequest)
 
 PaymentRequest::PaymentRequest(ScriptState* scriptState, const Vector<String>& supportedMethods, const PaymentDetails& details, const PaymentOptions& options, const ScriptValue& data, ExceptionState& exceptionState)
     : ContextLifecycleObserver(scriptState->getExecutionContext())
+    , ActiveScriptWrappable(this)
     , m_options(options)
     , m_clientBinding(this)
 {
@@ -347,7 +348,12 @@ PaymentRequest::PaymentRequest(ScriptState* scriptState, const Vector<String>& s
 
 void PaymentRequest::contextDestroyed()
 {
-    stopResolversAndCloseMojoConnection();
+    clearResolversAndCloseMojoConnection();
+}
+
+bool PaymentRequest::hasPendingActivity() const
+{
+    return m_showResolver || m_completeResolver;
 }
 
 void PaymentRequest::OnShippingAddressChange(mojom::blink::ShippingAddressPtr address)
@@ -358,7 +364,7 @@ void PaymentRequest::OnShippingAddressChange(mojom::blink::ShippingAddressPtr ad
     String errorMessage;
     if (!PaymentsValidators::isValidShippingAddress(address, &errorMessage)) {
         m_showResolver->reject(DOMException::create(SyntaxError, errorMessage));
-        stopResolversAndCloseMojoConnection();
+        clearResolversAndCloseMojoConnection();
         return;
     }
 
@@ -393,7 +399,7 @@ void PaymentRequest::OnPaymentResponse(mojom::blink::PaymentResponsePtr response
         String errorMessage;
         if (!PaymentsValidators::isValidShippingAddress(response->shipping_address, &errorMessage)) {
             m_showResolver->reject(DOMException::create(SyntaxError, errorMessage));
-            stopResolversAndCloseMojoConnection();
+            clearResolversAndCloseMojoConnection();
             return;
         }
 
@@ -415,26 +421,20 @@ void PaymentRequest::OnError()
         m_completeResolver->reject(DOMException::create(SyntaxError, "Request cancelled"));
     if (m_showResolver)
         m_showResolver->reject(DOMException::create(SyntaxError, "Request cancelled"));
-    stopResolversAndCloseMojoConnection();
+    clearResolversAndCloseMojoConnection();
 }
 
 void PaymentRequest::OnComplete()
 {
     DCHECK(m_completeResolver);
     m_completeResolver->resolve();
-    stopResolversAndCloseMojoConnection();
+    clearResolversAndCloseMojoConnection();
 }
 
-void PaymentRequest::stopResolversAndCloseMojoConnection()
+void PaymentRequest::clearResolversAndCloseMojoConnection()
 {
-    if (m_completeResolver)
-        m_completeResolver->stop();
     m_completeResolver.clear();
-
-    if (m_showResolver)
-        m_showResolver->stop();
     m_showResolver.clear();
-
     if (m_clientBinding.is_bound())
         m_clientBinding.Close();
     m_paymentProvider.reset();
