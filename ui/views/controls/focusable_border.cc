bbc88ee08cb5fa49003ffcb866a2d9587f93a07a
estade <estade@chromium.org>
2016-05-06 11:02:05 -0700

    MD-ify textfields and combobox.
    The combobox dropdown itself needs some work on alignment. I'm not yet sure how that's going to work while still respecting platform specific dropdown styling, hence I'm punting it for later.
    
    BUG=571500
    
    Review-Url: https://codereview.chromium.org/1931013002
    Cr-Commit-Position: refs/heads/master@{#392093}

--- ui/views/controls/focusable_border.cc
+++ ui/views/controls/focusable_border.cc
@@ -7,10 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkPath.h"
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/gfx/canvas.h"
+#include "ui/gfx/color_palette.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/skia_util.h"
 #include "ui/native_theme/native_theme.h"
+#include "ui/views/controls/textfield/textfield.h"
 
 namespace {
 
@@ -22,7 +25,7 @@ namespace views {
 
 FocusableBorder::FocusableBorder()
     : insets_(kInsetSize, kInsetSize, kInsetSize, kInsetSize),
-      override_color_(SK_ColorWHITE),
+      override_color_(gfx::kPlaceholderColor),
       use_default_color_(true) {
 }
 
@@ -39,14 +42,21 @@ void FocusableBorder::UseDefaultColor() {
 }
 
 void FocusableBorder::Paint(const View& view, gfx::Canvas* canvas) {
-  SkPath path;
-  path.addRect(gfx::RectToSkRect(view.GetLocalBounds()), SkPath::kCW_Direction);
   SkPaint paint;
   paint.setStyle(SkPaint::kStroke_Style);
-
   paint.setColor(GetCurrentColor(view));
-  paint.setStrokeWidth(SkIntToScalar(2));
 
+  SkPath path;
+  if (ui::MaterialDesignController::IsSecondaryUiMaterial()) {
+    path.moveTo(Textfield::kTextPadding, view.height() - 1);
+    path.rLineTo(view.width() - Textfield::kTextPadding * 2, 0);
+    path.offset(0.5f, 0.5f);
+    paint.setStrokeWidth(SkIntToScalar(1));
+  } else {
+    path.addRect(gfx::RectToSkRect(view.GetLocalBounds()),
+                 SkPath::kCW_Direction);
+    paint.setStrokeWidth(SkIntToScalar(2));
+  }
   canvas->DrawPath(path, paint);
 }
 
