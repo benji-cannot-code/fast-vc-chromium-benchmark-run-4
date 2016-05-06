b8f2bb45b6d6c68c92ed21a049a627f09b9dfb1f
khmel <khmel@chromium.org>
2016-05-06 09:03:24 -0700

    arc: Store fullscreen state of Arc apps.
    This CL makes it possible to restore fullscreen mode for
    re-activated Arc apps and remove fullscreen mode for the
    widget when the System switches to non-Arc app.
    
    BUG=b/28380217
    TEST=Manually on device. Has different combination or Arc
         apps and legacy apps, different combination for
         fullscreen modes. When Arc app is non-active we have
         shelf always available.
    
    Review-Url: https://codereview.chromium.org/1954843002
    Cr-Commit-Position: refs/heads/master@{#392067}

--- chrome/browser/ui/ash/launcher/arc_app_window_launcher_controller.cc
+++ chrome/browser/ui/ash/launcher/arc_app_window_launcher_controller.cc
@@ -16,6 +16,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/base_window.h"
 #include "ui/views/widget/widget.h"
 
+namespace {
+
+enum class FullScreenMode {
+  NOT_DEFINED,  // Fullscreen mode was not defined.
+  ACTIVE,       // Fullscreen is activated for an app.
+  NON_ACTIVE,   // Fullscreen was not activated for an app.
+};
+}
+
 class ArcAppWindowLauncherController::AppWindow : public ui::BaseWindow {
  public:
   AppWindow(int task_id, ArcAppWindowLauncherController* owner)
@@ -26,6 +35,13 @@ class ArcAppWindowLauncherController::AppWindow : public ui::BaseWindow {
     DCHECK(!controller_ && controller);
     controller_ = controller;
   }
+
+  void SetFullscreenMode(FullScreenMode mode) {
+    DCHECK(mode != FullScreenMode::NOT_DEFINED);
+    fullscreen_mode_ = mode;
+  }
+
+  FullScreenMode fullscreen_mode() const { return fullscreen_mode_; }
 
   int task_id() const { return task_id_; }
 
@@ -138,6 +154,7 @@ class ArcAppWindowLauncherController::AppWindow : public ui::BaseWindow {
 
   int task_id_;
   ash::ShelfID shelf_id_ = 0;
+  FullScreenMode fullscreen_mode_ = FullScreenMode::NOT_DEFINED;
   // Unowned pointers
   ArcAppWindowLauncherController* owner_;
   ArcAppWindowLauncherItemController* controller_ = nullptr;
@@ -266,6 +283,8 @@ void ArcAppWindowLauncherController::OnTaskDestroyed(int task_id) {
   }
 
   task_id_to_app_window_.erase(it);
+  if (task_id_to_app_window_.empty() && root_widget_)
+    root_widget_->SetFullscreen(false);
 }
 
 void ArcAppWindowLauncherController::OnTaskSetActive(int32_t task_id) {
@@ -274,6 +293,10 @@ void ArcAppWindowLauncherController::OnTaskSetActive(int32_t task_id) {
   if (previous_active_app_it != task_id_to_app_window_.end()) {
     owner()->SetItemStatus(previous_active_app_it->second->shelf_id(),
                            ash::STATUS_RUNNING);
+    previous_active_app_it->second->SetFullscreenMode(
+        root_widget_ && root_widget_->IsFullscreen()
+            ? FullScreenMode::ACTIVE
+            : FullScreenMode::NON_ACTIVE);
   }
 
   active_task_id_ = task_id;
@@ -290,6 +313,14 @@ void ArcAppWindowLauncherController::OnTaskSetActive(int32_t task_id) {
                            root_widget_ && root_widget_->IsActive()
                                ? ash::STATUS_ACTIVE
                                : ash::STATUS_RUNNING);
+    if (root_widget_) {
+      root_widget_->SetFullscreen(
+          new_active_app_it->second->fullscreen_mode() ==
+          FullScreenMode::ACTIVE);
+    }
+  } else {
+    if (root_widget_)
+      root_widget_->SetFullscreen(false);
   }
 }
 
