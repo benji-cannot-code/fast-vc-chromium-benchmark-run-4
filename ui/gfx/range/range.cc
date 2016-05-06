1ecbce8f090a65defdf6704d984b7773728e1d27
karandeepb <karandeepb@chromium.org>
2016-05-05 18:56:47 -0700

    Fix gfx::Range::ToString() to use the correct format specifier.
    This bug seems to have been introduced in crrev.com/1671403002. Also added a
    test which fails on the current master.
    
    BUG=609359
    
    Review-Url: https://codereview.chromium.org/1948283002
    Cr-Commit-Position: refs/heads/master@{#391986}

--- ui/gfx/range/range.cc
+++ ui/gfx/range/range.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/range/range.h"
 
+#include <inttypes.h>
+
 #include <algorithm>
 #include <limits>
 
@@ -78,7 +80,7 @@ Range Range::Intersect(const Range& range) const {
 }
 
 std::string Range::ToString() const {
-  return base::StringPrintf("{%d,%d}", start(), end());
+  return base::StringPrintf("{%" PRIu32 ",%" PRIu32 "}", start(), end());
 }
 
 std::ostream& operator<<(std::ostream& os, const Range& range) {
