8f841147b3f9e2fd36e17276e17688ee6da68f76
tdanderson <tdanderson@chromium.org>
2016-05-06 15:40:08 -0700

    Permit MaterialDesignController to make IO call
    As a one-time call on startup, permit
    MaterialDesignController to check for
    available touch devices on Chrome OS.
    
    BUG=596294
    
    Review-Url: https://codereview.chromium.org/1844033002
    Cr-Commit-Position: refs/heads/master@{#392186}

--- ui/base/material_design/material_design_controller.cc
+++ ui/base/material_design/material_design_controller.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <fcntl.h>
 
 #include "base/files/file_enumerator.h"
+#include "base/threading/thread_restrictions.h"
 #include "ui/events/ozone/evdev/event_device_info.h"
 #endif  // defined(USE_OZONE)
 
@@ -83,6 +84,8 @@ bool MaterialDesignController::IsSecondaryUiMaterial() {
 // static
 MaterialDesignController::Mode MaterialDesignController::DefaultMode() {
 #if defined(OS_CHROMEOS)
+  // If a scan of available devices has already completed, use material-hybrid
+  // if a touchscreen is present.
   if (DeviceDataManager::HasInstance() &&
       DeviceDataManager::GetInstance()->device_lists_complete()) {
     return GetTouchScreensAvailability() == TouchScreensAvailability::ENABLED
@@ -91,6 +94,9 @@ MaterialDesignController::Mode MaterialDesignController::DefaultMode() {
   }
 
 #if defined(USE_OZONE)
+  // Otherwise perform our own scan to determine the presence of a touchscreen.
+  // Note this is a one-time call that occurs during device startup or restart.
+  base::ThreadRestrictions::ScopedAllowIO allow_io;
   base::FileEnumerator file_enum(
       base::FilePath(FILE_PATH_LITERAL("/dev/input")), false,
       base::FileEnumerator::FILES, FILE_PATH_LITERAL("event*[0-9]"));
--- ui/events/devices/device_util_linux.cc
+++ ui/events/devices/device_util_linux.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/message_loop/message_loop.h"
 #include "base/strings/string_util.h"
+#include "base/threading/thread_restrictions.h"
 
 namespace ui {
 
@@ -21,7 +22,7 @@ base::FilePath GetInputPathInSys(const base::FilePath& path) {
 }
 
 InputDeviceType GetInputDeviceTypeFromPath(const base::FilePath& path) {
-  DCHECK(!base::MessageLoopForUI::IsCurrent());
+  base::ThreadRestrictions::AssertIOAllowed();
   std::string event_node = path.BaseName().value();
   if (event_node.empty() ||
       !base::StartsWith(event_node, "event",
