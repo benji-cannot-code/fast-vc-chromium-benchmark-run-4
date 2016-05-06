ac1f8a1a4f3c6f0be9557453af237e55125af339
sammc <sammc@chromium.org>
2016-05-05 17:30:27 -0700

    Cancel permission requests when the client disconnects.
    Review-Url: https://codereview.chromium.org/1952953002
    Cr-Commit-Position: refs/heads/master@{#391966}

--- content/browser/permissions/permission_service_impl.cc
+++ content/browser/permissions/permission_service_impl.cc
@@ -108,6 +108,7 @@ PermissionServiceImpl::~PermissionServiceImpl() {
 }
 
 void PermissionServiceImpl::OnConnectionError() {
+  CancelPendingOperations();
   context_->ServiceHadConnectionError(this);
   // After that call, |this| will be deleted.
 }
