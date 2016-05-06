3ca3e321adf7bfefcfe7abe7c098d894753d4754
fdoray <fdoray@chromium.org>
2016-05-06 08:39:14 -0700

    TaskScheduler [13] TaskSchedulerImpl
    TaskSchedulerImpl is the default implementation of the TaskScheduler
    interface. It runs tasks in 4 thread pools:
    - Background with file i/o
    - Background without file i/o
    - Normal with file i/o
    - Normal without file i/o
    
    This change completes the necessary changes described in
    https://codereview.chromium.org/1698183005/
    
    BUG=553459
    
    Review-Url: https://codereview.chromium.org/1701343003
    Cr-Commit-Position: refs/heads/master@{#392063}

--- base/task_scheduler/task_scheduler.cc
+++ base/task_scheduler/task_scheduler.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_scheduler/task_scheduler.h"
 
 #include "base/logging.h"
+#include "base/task_scheduler/task_scheduler_impl.h"
 
 namespace base {
 
@@ -24,7 +25,7 @@ void TaskScheduler::SetInstance(std::unique_ptr<TaskScheduler> task_scheduler) {
 
 // static
 void TaskScheduler::InitializeDefaultTaskScheduler() {
-  NOTIMPLEMENTED();
+  SetInstance(internal::TaskSchedulerImpl::Create());
 }
 
 // static
