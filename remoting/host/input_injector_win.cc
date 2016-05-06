32d89751f18d5026e92b4241670bad25c8d67efb
joedow <joedow@chromium.org>
2016-05-06 12:19:37 -0700

    Fixing an It2Me host crash on debug builds on Windows
    While working on an different It2Me work item I noticed that every time I tried
    to connect the host would crash with an error: "Function marked as IO-only was
    called from a thread that disallows IO!...".
    
    After some investigation I found that the It2Me host attempts to create the
    TouchInputInjector (which loads a DLL) on its network thread causing the DCHECK
    crash.  The Me2Me host creates this object on its UI thread which is also where
    the injector is initialized so my fix is to move the creation of the
    TouchInputInjector to the UI thread as well so we can guarantee it does not
    occur on an invalid thread.
    
    BUG=609639
    
    Review-Url: https://codereview.chromium.org/1949113005
    Cr-Commit-Position: refs/heads/master@{#392117}

--- remoting/host/input_injector_win.cc
+++ remoting/host/input_injector_win.cc
@@ -202,7 +202,7 @@ class InputInjectorWin : public InputInjector {
     scoped_refptr<base::SingleThreadTaskRunner> main_task_runner_;
     scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner_;
     std::unique_ptr<Clipboard> clipboard_;
-    TouchInjectorWin touch_injector_;
+    std::unique_ptr<TouchInjectorWin> touch_injector_;
 
     DISALLOW_COPY_AND_ASSIGN(Core);
   };
@@ -316,7 +316,8 @@ void InputInjectorWin::Core::Start(
   }
 
   clipboard_->Start(std::move(client_clipboard));
-  touch_injector_.Init();
+  touch_injector_.reset(new TouchInjectorWin());
+  touch_injector_->Init();
 }
 
 void InputInjectorWin::Core::Stop() {
@@ -326,7 +327,7 @@ void InputInjectorWin::Core::Stop() {
   }
 
   clipboard_.reset();
-  touch_injector_.Deinitialize();
+  touch_injector_->Deinitialize();
 }
 
 InputInjectorWin::Core::~Core() {}
@@ -379,7 +380,8 @@ void InputInjectorWin::Core::HandleMouse(const MouseEvent& event) {
 }
 
 void InputInjectorWin::Core::HandleTouch(const TouchEvent& event) {
-  touch_injector_.InjectTouchEvent(event);
+  DCHECK(touch_injector_);
+  touch_injector_->InjectTouchEvent(event);
 }
 
 }  // namespace
