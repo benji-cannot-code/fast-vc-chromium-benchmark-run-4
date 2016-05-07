629f8840596ba3a2d8b610a998479944b95c1ec8
thakis <thakis@chromium.org>
2016-05-07 07:32:26 -0700

    Revert of Skeleton of the overall design for the database for Pver4 (patchset #5 id:80001 of https://codereview.chromium.org/1952843003/ )
    Reason for revert:
    Speculative; lots of browser tests started failing with leak reports, and all stacks contain "safe browsing". See https://build.chromium.org/p/chromium.linux/builders/Linux%20Tests%20%28dbg%29%281%29/builds/54004, e.g.
    
    [444:555:0506/185642:ERROR:leak_tracker.h(100)] #0 0x7fd9c5c00d0e base::debug::StackTrace::StackTrace()
    #1 0x00000186cf6f base::debug::LeakTracker<>::LeakTracker()
    #2 0x0000018635bb SystemURLRequestContextGetter::SystemURLRequestContextGetter()
    #3 0x000001865c21 IOThread::InitSystemRequestContext()
    #4 0x000001865af5 IOThread::system_url_request_context_getter()
    #5 0x000001a08b90 BrowserProcessImpl::system_request_context()
    #6 0x000001ef00c4 safe_browsing::SafeBrowsingService::Initialize()
    #7 0x000001a0c1b0 BrowserProcessImpl::CreateSafeBrowsingService()
    #8 0x000001a0c022 BrowserProcessImpl::safe_browsing_service()
    
    Original issue's description:
    > Skeleton of the overall design for the database for Pver4
    > Design doc: https://goto.google.com/chromium-pver4-design-doc
    >
    > BUG==543161
    >
    > Committed: https://crrev.com/8354386e0eb264960f0ccdaa9e3e003ad72f0bcd
    > Cr-Commit-Position: refs/heads/master@{#392211}
    
    TBR=nparker@chromium.org,vakh@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG==543161
    
    Review-Url: https://codereview.chromium.org/1960893002
    Cr-Commit-Position: refs/heads/master@{#392262}

--- components/safe_browsing_db/v4_local_database_manager.h
+++ components/safe_browsing_db/v4_local_database_manager.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/safe_browsing_db/database_manager.h"
 #include "components/safe_browsing_db/hit_report.h"
-#include "components/safe_browsing_db/v4_database.h"
 #include "components/safe_browsing_db/v4_protocol_manager_util.h"
 #include "components/safe_browsing_db/v4_update_protocol_manager.h"
 #include "url/gurl.h"
@@ -71,15 +70,10 @@ class V4LocalDatabaseManager : public SafeBrowsingDatabaseManager {
 
   // Stores the current status of the lists to download from the SafeBrowsing
   // servers.
-  // TODO(vakh): current_list_states_ doesn't really belong here.
-  // It should come through the database, from the various V4Stores.
   base::hash_map<UpdateListIdentifier, std::string> current_list_states_;
 
   // The protocol manager that downloads the hash prefix updates.
   std::unique_ptr<V4UpdateProtocolManager> v4_update_protocol_manager_;
-
-  // The database that manages the stores containing the hash prefix updates.
-  std::unique_ptr<V4Database> v4_database_;
 
   friend class base::RefCountedThreadSafe<V4LocalDatabaseManager>;
   DISALLOW_COPY_AND_ASSIGN(V4LocalDatabaseManager);
