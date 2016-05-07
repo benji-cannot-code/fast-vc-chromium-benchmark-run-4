bb62d4a6f0272e88b2275ed3a92208cbfc0db2b7
thakis <thakis@chromium.org>
2016-05-07 10:50:26 -0700

    Revert of network_time_tracker: add temporary time protocol. (patchset #27 id:520001 of https://codereview.chromium.org/1835823002/ )
    Reason for revert:
    Speculative; lots of browser tests started failing with leak reports, and all failing tests look network related. See
    https://build.chromium.org/p/chromium.linux/builders/Linux%20Tests%20%28dbg%29%281%29/builds/54004,
    e.g.
    
    [444:555:0506/185642:ERROR:leak_tracker.h(100)] #0 0x7fd9c5c00d0e
    base::debug::StackTrace::StackTrace()
    #1 0x00000186cf6f base::debug::LeakTracker<>::LeakTracker()
    #2 0x0000018635bb SystemURLRequestContextGetter::SystemURLRequestContextGetter()
    #3 0x000001865c21 IOThread::InitSystemRequestContext()
    #4 0x000001865af5 IOThread::system_url_request_context_getter()
    #5 0x000001a08b90 BrowserProcessImpl::system_request_context()
    #6 0x000001ef00c4 safe_browsing::SafeBrowsingService::Initialize()
    #7 0x000001a0c1b0 BrowserProcessImpl::CreateSafeBrowsingService()
    #8 0x000001a0c022 BrowserProcessImpl::safe_browsing_service()
    
    Original issue's description:
    > network_time_tracker: add temporary time protocol.
    >
    > This adds queries of the temporary secure time server, with signature
    > verification, and a very conservative scheduler.  (Checks once per hour,
    > queries only if sync believed to be lost.)
    >
    > BUG=589700
    >
    > Committed: https://crrev.com/ee2016a374b73ba35508ae26837755a23b3defa4
    > Cr-Commit-Position: refs/heads/master@{#392208}
    
    TBR=estark@chromium.org,waffles@chromium.org,zea@chromium.org,sky@chromium.org,rohitrao@chromium.org,rsleevi@chromium.org,mmenke@chromium.org,rsesek@chromium.org,mab@google.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=589700
    
    Review-Url: https://codereview.chromium.org/1956173002
    Cr-Commit-Position: refs/heads/master@{#392264}

--- ios/chrome/test/testing_application_context.mm
+++ ios/chrome/test/testing_application_context.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/default_tick_clock.h"
 #include "components/network_time/network_time_tracker.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
-#include "net/url_request/url_request_context_getter.h"
 
 TestingApplicationContext::TestingApplicationContext()
     : application_locale_("en"),
@@ -129,8 +128,7 @@ TestingApplicationContext::GetNetworkTimeTracker() {
     DCHECK(local_state_);
     network_time_tracker_.reset(new network_time::NetworkTimeTracker(
         base::WrapUnique(new base::DefaultClock),
-        base::WrapUnique(new base::DefaultTickClock), local_state_,
-        GetSystemURLRequestContext()));
+        base::WrapUnique(new base::DefaultTickClock), local_state_));
   }
   return network_time_tracker_.get();
 }
