16153ae95a5f0a7fa435c87f0d93faddfb8262a0
dpranke <dpranke@chromium.org>
2016-05-06 18:58:50 -0700

    Revert of Keep PaymentRequest alive when its promises are outstanding. (patchset #4 id:80001 of https://codereview.chromium.org/1954973005/ )
    Reason for revert:
    Looks like this new test is timing out or failing on a number of builders:
    
    https://test-results.appspot.com/dashboards/flakiness_dashboard.html#tests=payments%2Fpromises-keep-request-alive.html&testType=webkit_tests
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.10/builds/17016/steps/webkit_tests/logs/stdio
    
    Original issue's description:
    > Keep PaymentRequest alive when its promises are outstanding.
    >
    > The PaymentRequest is responsible for resolving the show and complete
    > promises, so it needs to be kept alive as long as either of these
    > promises haven't resolved yet. This makes sure of that.
    >
    > Also reverts the earlier change that "stopped" outstanding promises
    > when the context is destroyed, as that doesn't actually do anything.
    >
    > BUG=606577
    >
    > Committed: https://crrev.com/f2fc9122808b627241860773ec97c8d9120df7ee
    > Cr-Commit-Position: refs/heads/master@{#392157}
    
    TBR=esprehn@chromium.org,rouslan@chromium.org,mek@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=606577
    
    Review-Url: https://codereview.chromium.org/1954723005
    Cr-Commit-Position: refs/heads/master@{#392233}

--- third_party/WebKit/Source/modules/payments/PaymentRequest.cpp
+++ third_party/WebKit/Source/modules/payments/PaymentRequest.cpp
@@ -249,14 +249,14 @@ void PaymentRequest::onUpdatePaymentDetails(const ScriptValue& detailsScriptValu
     V8PaymentDetails::toImpl(detailsScriptValue.isolate(), detailsScriptValue.v8Value(), details, exceptionState);
     if (exceptionState.hadException()) {
         m_showResolver->reject(DOMException::create(SyntaxError, exceptionState.message()));
-        clearResolversAndCloseMojoConnection();
+        stopResolversAndCloseMojoConnection();
         return;
     }
 
     validatePaymentDetails(details, exceptionState);
     if (exceptionState.hadException()) {
         m_showResolver->reject(DOMException::create(SyntaxError, exceptionState.message()));
-        clearResolversAndCloseMojoConnection();
+        stopResolversAndCloseMojoConnection();
         return;
     }
 
@@ -275,7 +275,7 @@ void PaymentRequest::onUpdatePaymentDetailsFailure(const ScriptValue& error)
         m_showResolver->reject(error);
     if (m_completeResolver)
         m_completeResolver->reject(error);
-    clearResolversAndCloseMojoConnection();
+    stopResolversAndCloseMojoConnection();
 }
 
 DEFINE_TRACE(PaymentRequest)
@@ -291,7 +291,6 @@ DEFINE_TRACE(PaymentRequest)
 
 PaymentRequest::PaymentRequest(ScriptState* scriptState, const Vector<String>& supportedMethods, const PaymentDetails& details, const PaymentOptions& options, const ScriptValue& data, ExceptionState& exceptionState)
     : ContextLifecycleObserver(scriptState->getExecutionContext())
-    , ActiveScriptWrappable(this)
     , m_options(options)
     , m_clientBinding(this)
 {
@@ -348,12 +347,7 @@ PaymentRequest::PaymentRequest(ScriptState* scriptState, const Vector<String>& s
 
 void PaymentRequest::contextDestroyed()
 {
-    clearResolversAndCloseMojoConnection();
-}
-
-bool PaymentRequest::hasPendingActivity() const
-{
-    return m_showResolver || m_completeResolver;
+    stopResolversAndCloseMojoConnection();
 }
 
 void PaymentRequest::OnShippingAddressChange(mojom::blink::ShippingAddressPtr address)
@@ -364,7 +358,7 @@ void PaymentRequest::OnShippingAddressChange(mojom::blink::ShippingAddressPtr ad
     String errorMessage;
     if (!PaymentsValidators::isValidShippingAddress(address, &errorMessage)) {
         m_showResolver->reject(DOMException::create(SyntaxError, errorMessage));
-        clearResolversAndCloseMojoConnection();
+        stopResolversAndCloseMojoConnection();
         return;
     }
 
@@ -399,7 +393,7 @@ void PaymentRequest::OnPaymentResponse(mojom::blink::PaymentResponsePtr response
         String errorMessage;
         if (!PaymentsValidators::isValidShippingAddress(response->shipping_address, &errorMessage)) {
             m_showResolver->reject(DOMException::create(SyntaxError, errorMessage));
-            clearResolversAndCloseMojoConnection();
+            stopResolversAndCloseMojoConnection();
             return;
         }
 
@@ -421,20 +415,26 @@ void PaymentRequest::OnError()
         m_completeResolver->reject(DOMException::create(SyntaxError, "Request cancelled"));
     if (m_showResolver)
         m_showResolver->reject(DOMException::create(SyntaxError, "Request cancelled"));
-    clearResolversAndCloseMojoConnection();
+    stopResolversAndCloseMojoConnection();
 }
 
 void PaymentRequest::OnComplete()
 {
     DCHECK(m_completeResolver);
     m_completeResolver->resolve();
-    clearResolversAndCloseMojoConnection();
+    stopResolversAndCloseMojoConnection();
 }
 
-void PaymentRequest::clearResolversAndCloseMojoConnection()
+void PaymentRequest::stopResolversAndCloseMojoConnection()
 {
+    if (m_completeResolver)
+        m_completeResolver->stop();
     m_completeResolver.clear();
+
+    if (m_showResolver)
+        m_showResolver->stop();
     m_showResolver.clear();
+
     if (m_clientBinding.is_bound())
         m_clientBinding.Close();
     m_paymentProvider.reset();
