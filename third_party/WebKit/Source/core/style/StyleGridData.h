ae07792ce7151ce63cfef85a3ad864d69c74e48e
svillar <svillar@igalia.com>
2016-05-09 08:29:18 -0700

    [css-grid] Store auto repeat type for both rows and columns
    We're storing in style the the auto-repeat type (auto-fill or auto-fit) but
    using just one slot. That was wrong because rows and columns could both have
    auto-repeat tracks with different types (still just one auto-repeat per
    axis).
    
    This was not causing any issue because we're not using it yet until auto-fit
    is not implemented. That's the reason why we cannot create a test for it. In
    any case the future implementation of auto-fit won't ever work without this.
    
    BUG=589460
    
    Review-Url: https://codereview.chromium.org/1952923004
    Cr-Commit-Position: refs/heads/master@{#392321}

--- third_party/WebKit/Source/core/style/StyleGridData.h
+++ third_party/WebKit/Source/core/style/StyleGridData.h
@@ -58,7 +58,7 @@ public:
         && m_gridColumnGap == o.m_gridColumnGap && m_gridRowGap == o.m_gridRowGap
         && m_gridAutoRepeatColumns == o.m_gridAutoRepeatColumns && m_gridAutoRepeatRows == o.m_gridAutoRepeatRows
         && m_autoRepeatColumnsInsertionPoint == o.m_autoRepeatColumnsInsertionPoint && m_autoRepeatRowsInsertionPoint == o.m_autoRepeatRowsInsertionPoint
-        && m_autoRepeatType == o.m_autoRepeatType;
+        && m_autoRepeatColumnsType == o.m_autoRepeatColumnsType && m_autoRepeatRowsType == o.m_autoRepeatRowsType;
     }
 
     bool operator!=(const StyleGridData& o) const
@@ -102,7 +102,8 @@ public:
     size_t m_autoRepeatColumnsInsertionPoint;
     size_t m_autoRepeatRowsInsertionPoint;
 
-    AutoRepeatType m_autoRepeatType;
+    AutoRepeatType m_autoRepeatColumnsType;
+    AutoRepeatType m_autoRepeatRowsType;
 private:
     StyleGridData();
     StyleGridData(const StyleGridData&);
