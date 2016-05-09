3942935b41443609249a0198c61c58422a26b0f2
rouslan <rouslan@chromium.org>
2016-05-09 10:14:54 -0700

    Test dismissing CVC prompt for PaymentRequest
    This is an integration test for PaymentRequest that performs the
    following steps:
    1) Launch PaymentRequest UI.
    2) Click "Pay" in PaymentRequest UI.
    3) Click "Cancel" in CVC prompt.
    4) Click "OK" to dismiss the result dialog.
    
    BUG=608223
    
    Review-Url: https://codereview.chromium.org/1958873002
    Cr-Commit-Position: refs/heads/master@{#392347}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestUI.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestUI.java
@@ -130,6 +130,11 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
         void onPaymentRequestReadyToPay(PaymentRequestUI ui);
 
         /**
+         * Called when the result UI is showing.
+         */
+        void onPaymentRequestResultReady(PaymentRequestUI ui);
+
+        /**
          * Called when the UI is gone.
          */
         void onPaymentRequestDismiss();
@@ -343,6 +348,7 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
     public void close(boolean paymentSuccess, final Runnable callback) {
         mIsClientClosing = true;
         mResultUI.update(paymentSuccess, mDialog, callback);
+        if (mObserverForTest != null) mObserverForTest.onPaymentRequestResultReady(this);
     }
 
     /**
--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestTestBase.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestTestBase.java
@@ -37,6 +37,7 @@ abstract class PaymentRequestTestBase extends ChromeActivityTestCaseBase<ChromeA
     protected final PaymentsCallbackHelper<PaymentRequestUI> mReadyForInput;
     protected final PaymentsCallbackHelper<PaymentRequestUI> mReadyToPay;
     protected final PaymentsCallbackHelper<PaymentRequestUI> mReadyToClose;
+    protected final PaymentsCallbackHelper<PaymentRequestUI> mResultReady;
     protected final PaymentsCallbackHelper<CardUnmaskPrompt> mReadyToUnmask;
     protected final CallbackHelper mDismissed;
     private final AtomicReference<ContentViewCore> mViewCoreRef;
@@ -49,6 +50,7 @@ abstract class PaymentRequestTestBase extends ChromeActivityTestCaseBase<ChromeA
         mReadyForInput = new PaymentsCallbackHelper<>();
         mReadyToPay = new PaymentsCallbackHelper<>();
         mReadyToClose = new PaymentsCallbackHelper<>();
+        mResultReady = new PaymentsCallbackHelper<>();
         mReadyToUnmask = new PaymentsCallbackHelper<>();
         mDismissed = new CallbackHelper();
         mViewCoreRef = new AtomicReference<>();
@@ -154,6 +156,13 @@ abstract class PaymentRequestTestBase extends ChromeActivityTestCaseBase<ChromeA
         ThreadUtils.assertOnUiThread();
         mReadyToClose.notifyCalled(ui);
     }
+
+    @Override
+    public void onPaymentRequestResultReady(PaymentRequestUI ui) {
+        ThreadUtils.assertOnUiThread();
+        mResultReady.notifyCalled(ui);
+    }
+
 
     @Override
     public void onPaymentRequestDismiss() {
