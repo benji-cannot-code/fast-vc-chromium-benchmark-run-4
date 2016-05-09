3e57fbc2ed06f1bd6b859b4482a27f0463689663
nektar <nektar@chromium.org>
2016-05-09 14:12:24 -0700

    The bug fixed involves not recursing into inline text boxes in the case that a static text object covered more than one line.
    Also fixed a bug on Windows that didn't return the correct line boundaries if the start offset was equal to the text's length, something that is allowed by the IA2 Spec.
    BUG=605670
    R=dmazzoni@chromium.org
    TESTED=manual testing with Gmail > Compose and Docs, browser tests
    
    Review-Url: https://codereview.chromium.org/1951353002
    Cr-Commit-Position: refs/heads/master@{#392424}

--- content/browser/accessibility/accessibility_win_browsertest.cc
+++ content/browser/accessibility/accessibility_win_browsertest.cc
@@ -1623,7 +1623,9 @@ IN_PROC_BROWSER_TEST_F(AccessibilityWinBrowserTest,
         L"Game theory is \"the study of \xFFFC of conflict and\n");
   }
 
-  for (LONG i = newline_offset + 1; i < n_characters; ++i) {
+  // For line boundaries, IA2 Spec allows for the offset to be equal to the
+  // text's length.
+  for (LONG i = newline_offset + 1; i <= n_characters; ++i) {
     CheckTextAtOffset(
         paragraph_text, i, IA2_TEXT_BOUNDARY_LINE, newline_offset + 1,
         n_characters,
