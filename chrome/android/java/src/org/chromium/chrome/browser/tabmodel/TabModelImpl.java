afcfc55f7283189703dd9b936a62a8bd6a4d1bf7
timav <timav@chromium.org>
2016-05-09 15:54:42 -0700

    Do not suspend media when reparenting a tab
    When we do tab reparenting we assume that content will continue
    to function in an other tab. In this case it is better not to
    suspend the media playback, this allows for a smooth playback
    transition.
    
    Before this change we suspended the playback and muted audio
    in the beginning of the reparenting process, but did not resume
    and unmute at the end of it. Not suspening the playback in the
    first place fixes this issue.
    
    BUG=607752
    
    Review-Url: https://codereview.chromium.org/1956403003
    Cr-Commit-Position: refs/heads/master@{#392458}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelImpl.java
@@ -86,7 +86,7 @@ public class TabModelImpl extends TabModelJniBridge {
 
     @Override
     public void removeTab(Tab tab) {
-        removeTabAndSelectNext(tab, TabSelectionType.FROM_USER, true, true);
+        removeTabAndSelectNext(tab, TabSelectionType.FROM_USER, false, true);
 
         for (TabModelObserver obs : mObservers) obs.tabRemoved(tab);
     }
