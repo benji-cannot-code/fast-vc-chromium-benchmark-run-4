9d8ac532546f2fba36bb3e05860efa4be7c8b0f2
eero.hakkinen <eero.hakkinen@intel.com>
2016-05-09 01:42:07 -0700

    [chrome.displaySource] Use a scoped enumeration for initialization steps.
    Use scoped enumerations for media pipeline initialization steps for
    better compiler support.
    
    BUG=242107
    
    Review-Url: https://codereview.chromium.org/1930923002
    Cr-Commit-Position: refs/heads/master@{#392291}

--- extensions/renderer/api/display_source/wifi_display/wifi_display_media_pipeline.h
+++ extensions/renderer/api/display_source/wifi_display/wifi_display_media_pipeline.h
@@ -62,7 +62,7 @@ class WiFiDisplayMediaPipeline {
 
  private:
   using InitStepCompletionCallback = InitCompletionCallback;
-  enum InitializationStep : unsigned;
+  enum class InitializationStep : unsigned;
 
   WiFiDisplayMediaPipeline(
       wds::SessionType type,
