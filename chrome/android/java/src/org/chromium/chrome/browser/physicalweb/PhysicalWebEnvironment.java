a84b627ed492b7f8440a4d0d701f800f91bbd193
cco3 <cco3@chromium.org>
2016-05-09 16:28:38 -0700

    Test thread equality when initiating PWEnv class
    ThreadUtils.assertOnUiIThread doesn't work on newer versions of
    Android.  Instead, we should simply test thread equality to make
    sure we don't create a PhysicalWebEnvironment singleton twice.
    
    This change also renables Physical Web onboarding tests that had
    been disabled due to this issue.
    
    BUG=608872
    
    Review-Url: https://codereview.chromium.org/1955173003
    Cr-Commit-Position: refs/heads/master@{#392471}

--- chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWebEnvironment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWebEnvironment.java
@@ -5,13 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.physicalweb;
 
-import org.chromium.base.ThreadUtils;
 import org.chromium.chrome.browser.ChromeApplication;
 
 /**
  * Tool that reports information about conflicting clients.
  */
 public class PhysicalWebEnvironment {
+    private static final Object INSTANCE_LOCK = new Object();
     private static PhysicalWebEnvironment sInstance = null;
 
     /**
@@ -21,9 +21,10 @@ public class PhysicalWebEnvironment {
      * @return an instance of this class (or subclass) as decided by the application parameter
      */
     public static PhysicalWebEnvironment getInstance(ChromeApplication chromeApplication) {
-        ThreadUtils.assertOnUiThread();
-        if (sInstance == null) {
-            sInstance = chromeApplication.createPhysicalWebEnvironment();
+        synchronized (INSTANCE_LOCK) {
+            if (sInstance == null) {
+                sInstance = chromeApplication.createPhysicalWebEnvironment();
+            }
         }
         return sInstance;
     }
