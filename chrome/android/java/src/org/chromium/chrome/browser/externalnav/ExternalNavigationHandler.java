c98465f8ae57e8fb579985da84056992f5ebd08c
mariakhomenko <mariakhomenko@chromium.org>
2016-05-09 12:39:33 -0700

    Re-submit of AppLink implementation.
    The only difference is removed assert from AppLinkHandler getInstance()
    method because it gets created on Test thread within tests.
    
    BUG=609556
    TBR=asvitkine@chromium.org
    
    Review-Url: https://codereview.chromium.org/1956403002
    Cr-Commit-Position: refs/heads/master@{#392399}

--- chrome/android/java/src/org/chromium/chrome/browser/externalnav/ExternalNavigationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/externalnav/ExternalNavigationHandler.java
@@ -336,6 +336,10 @@ public class ExternalNavigationHandler {
             IntentHandler.setPendingReferrer(intent, params.getReferrerUrl());
         }
 
+        if (!params.isIncognito() && mDelegate.maybeDelegateToAppLink(intent)) {
+            return OverrideUrlLoadingResult.OVERRIDE_WITH_EXTERNAL_INTENT;
+        }
+
         // Make sure webkit can handle it internally before checking for specialized
         // handlers. If webkit can't handle it internally, we need to call
         // startActivityIfNeeded or startActivity.
@@ -384,6 +388,7 @@ public class ExternalNavigationHandler {
                         params.shouldCloseContentsOnOverrideUrlLoadingAndLaunchIntent());
                 return OverrideUrlLoadingResult.OVERRIDE_WITH_ASYNC_ACTION;
             } else {
+
                 // Some third-party app launched Chrome with an intent, and the URL got redirected.
                 // The user has explicitly chosen Chrome over other intent handlers, so stay in
                 // Chrome unless there was a new intent handler after redirection or Chrome cannot
@@ -399,6 +404,7 @@ public class ExternalNavigationHandler {
                 if (params.hasUserGesture()) {
                     IntentWithGesturesHandler.getInstance().onNewIntentWithGesture(intent);
                 }
+
                 if (mDelegate.startActivityIfNeeded(intent)) {
                     return OverrideUrlLoadingResult.OVERRIDE_WITH_EXTERNAL_INTENT;
                 } else {
