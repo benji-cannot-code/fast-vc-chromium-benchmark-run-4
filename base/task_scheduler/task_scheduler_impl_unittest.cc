de1cd120f6dc02dc25de6a196d920108235ee91b
gab <gab@chromium.org>
2016-05-09 12:28:49 -0700

    Name TaskScheduler's worker threads
    In conjunction with https://codereview.chromium.org/1937323002/ this
    makes TaskScheduler work with chrome://tracing.
    
    Thread name will be comprised of 3 parts:
     [1] Thread Pool name, e.g. "TaskSchedulerBackgroudFileIO"
     [2] "Worker"
     [3] Thread Index in thread pool
    
    This can make for long names, e.g.:
    "TaskSchedulerBackgroudFileIOWorker3"
    but I think a descriptive name is still preferable.
    
    The one caveat is that WinDBG's implementation of thread name
    currently truncates at 32 characters... I'm willing to live with
    that for now until (if ever) we realize it's too annoying.
    
    BUG=553459
    
    Committed: https://crrev.com/a0813e0c0286ee6790433164957141e9f299bff7
    Cr-Commit-Position: refs/heads/master@{#392351}
    
    Review-Url: https://codereview.chromium.org/1951453002
    Cr-Commit-Position: refs/heads/master@{#392395}

--- base/task_scheduler/task_scheduler_impl_unittest.cc
+++ base/task_scheduler/task_scheduler_impl_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include <string>
 #include <utility>
 #include <vector>
 
@@ -78,6 +79,16 @@ void VerifyTaskEnvironement(const TaskTraits& traits) {
   // !ENABLE_THREAD_RESTRICTIONS, even when |traits| don't allow file I/O.
   EXPECT_EQ(traits.with_file_io(), GetIOAllowed());
 #endif
+
+  // Verify that the thread the task is running on is named as expected.
+  const std::string current_thread_name(PlatformThread::GetName());
+  EXPECT_NE(std::string::npos, current_thread_name.find("TaskScheduler"));
+  EXPECT_NE(std::string::npos,
+            current_thread_name.find(
+                traits.priority() == TaskPriority::BACKGROUND ? "Background"
+                                                              : "Foreground"));
+  EXPECT_EQ(traits.with_file_io(),
+            current_thread_name.find("FileIO") != std::string::npos);
 }
 
 void VerifyTaskEnvironementAndSignalEvent(const TaskTraits& traits,
