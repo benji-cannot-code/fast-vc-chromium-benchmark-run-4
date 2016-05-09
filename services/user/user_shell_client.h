3db4183adf281f21d9dd2c52ed9832056b694435
rockot <rockot@chromium.org>
2016-05-09 11:36:03 -0700

    Disable tracing in user service
    Two problems:
    
     1. The tracing client library makes some now-invalid
        assumptions about its environment
     2. We don't have a tracing shell client setup properly
        for chrome or browser tests.
    
    The second problem in particular is causing bad behavior
    on OS X if anyone tries connecting to mojo:tracing.
    
    This CL removes tracing usage from UserShellClient for
    now.
    
    BUG=609632
    R=erg@chromium.org
    
    Review-Url: https://codereview.chromium.org/1963753002
    Cr-Commit-Position: refs/heads/master@{#392378}

--- services/user/user_shell_client.h
+++ services/user/user_shell_client.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "services/shell/public/cpp/interface_factory.h"
 #include "services/shell/public/cpp/shell_client.h"
-#include "services/tracing/public/cpp/tracing_impl.h"
 #include "services/user/public/interfaces/user_service.mojom.h"
 
 namespace user_service {
@@ -53,8 +52,6 @@ class UserShellClient
 
   scoped_refptr<base::SingleThreadTaskRunner> user_service_runner_;
   scoped_refptr<base::SingleThreadTaskRunner> leveldb_service_runner_;
-
-  mojo::TracingImpl tracing_;
 
   // We create these two objects so we can delete them on the correct task
   // runners.
