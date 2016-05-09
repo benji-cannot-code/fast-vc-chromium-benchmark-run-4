441bcd943753d3f62b3cad6e1528107eb0228310
yukishiino <yukishiino@chromium.org>
2016-05-09 07:53:54 -0700

    Revert of binding: Makes Window/Location's attributes accessor-type properties. (patchset #27 id:520001 of https://codereview.chromium.org/1380503002/ )
    Reason for revert:
    We hit a (huge) performance regression.  See http://crbug.com/608475 .
    
    Original issue's description:
    > binding: Makes Window/Location's attributes accessor-type properties.
    >
    > Makes almost all the attributes (except for cross-origin accessible
    > attributes) accessor-type properties.  The target attributes in this
    > CL are DOM attributes of Window, Location and workers not annotated
    > as [DoNotCheckSecurity], which are cross-origin accessible.
    >
    > BUG=43394, 516274
    >
    > Committed: https://crrev.com/9ac1750adf85027985c7af3468e0b972c2086235
    > Cr-Commit-Position: refs/heads/master@{#390893}
    
    TBR=tkent@chromium.org,haraken@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=43394, 516274
    
    Review-Url: https://codereview.chromium.org/1958883003
    Cr-Commit-Position: refs/heads/master@{#392315}

--- third_party/WebKit/Source/bindings/core/v8/custom/V8WindowCustom.cpp
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8WindowCustom.cpp
@@ -68,7 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void V8Window::eventAttributeGetterCustom(const v8::FunctionCallbackInfo<v8::Value>& info)
+void V8Window::eventAttributeGetterCustom(const v8::PropertyCallbackInfo<v8::Value>& info)
 {
     LocalDOMWindow* impl = toLocalDOMWindow(V8Window::toImpl(info.Holder()));
     ExceptionState exceptionState(ExceptionState::GetterContext, "event", "Window", info.Holder(), info.GetIsolate());
@@ -90,7 +90,7 @@ void V8Window::eventAttributeGetterCustom(const v8::FunctionCallbackInfo<v8::Val
     v8SetReturnValue(info, jsEvent);
 }
 
-void V8Window::eventAttributeSetterCustom(v8::Local<v8::Value> value, const v8::FunctionCallbackInfo<v8::Value>& info)
+void V8Window::eventAttributeSetterCustom(v8::Local<v8::Value> value, const v8::PropertyCallbackInfo<void>& info)
 {
     LocalDOMWindow* impl = toLocalDOMWindow(V8Window::toImpl(info.Holder()));
     ExceptionState exceptionState(ExceptionState::SetterContext, "event", "Window", info.Holder(), info.GetIsolate());
@@ -109,7 +109,7 @@ void V8Window::eventAttributeSetterCustom(v8::Local<v8::Value> value, const v8::
     V8HiddenValue::setHiddenValue(ScriptState::current(info.GetIsolate()), context->Global(), V8HiddenValue::event(info.GetIsolate()), value);
 }
 
-void V8Window::frameElementAttributeGetterCustom(const v8::FunctionCallbackInfo<v8::Value>& info)
+void V8Window::frameElementAttributeGetterCustom(const v8::PropertyCallbackInfo<v8::Value>& info)
 {
     LocalDOMWindow* impl = toLocalDOMWindow(V8Window::toImpl(info.Holder()));
 
--- third_party/WebKit/Source/bindings/templates/attributes.cpp
+++ third_party/WebKit/Source/bindings/templates/attributes.cpp
@@ -22,12 +22,10 @@ const v8::FunctionCallbackInfo<v8::Value>& info
     {% if not attribute.is_static %}
     {% if attribute.is_lenient_this %}
     {# Make sure that info.Holder() really points to an instance if [LenientThis]. #}
-    v8::Local<v8::Object> holder = {{v8_class}}::findInstanceInPrototypeChain(info.This(), info.GetIsolate());
-    if (holder.IsEmpty())
+    if (!{{v8_class}}::hasInstance(info.Holder(), info.GetIsolate()))
         return; // Return silently because of [LenientThis].
-    {% else %}
-    v8::Local<v8::Object> holder = info.Holder();
     {% endif %}
+    v8::Local<v8::Object> holder = info.Holder();
     {% endif %}
     {# impl #}
     {% if attribute.cached_attribute_validation_method %}
@@ -79,7 +77,7 @@ const v8::FunctionCallbackInfo<v8::Value>& info
     {% if not attribute.is_data_type_property %}
     {% if attribute.is_check_security_for_receiver %}
     if (!BindingSecurity::shouldAllowAccessTo(info.GetIsolate(), callingDOMWindow(info.GetIsolate()), impl, exceptionState)) {
-        {{attribute.v8_set_return_value_for_security_failure}};
+        v8SetReturnValueNull(info);
         exceptionState.throwIfNeeded();
         return;
     }
@@ -87,7 +85,7 @@ const v8::FunctionCallbackInfo<v8::Value>& info
     {% endif %}
     {% if attribute.is_check_security_for_return_value %}
     if (!BindingSecurity::shouldAllowAccessTo(info.GetIsolate(), callingDOMWindow(info.GetIsolate()), {{attribute.cpp_value}}, exceptionState)) {
-        {{attribute.v8_set_return_value_for_security_failure}};
+        v8SetReturnValueNull(info);
         exceptionState.throwIfNeeded();
         return;
     }
@@ -249,12 +247,10 @@ v8::Local<v8::Value> v8Value, const v8::FunctionCallbackInfo<v8::Value>& info
           raise_exception %}
     {% if attribute.is_lenient_this %}
     {# Make sure that info.Holder() really points to an instance if [LenientThis]. #}
-    v8::Local<v8::Object> holder = {{v8_class}}::findInstanceInPrototypeChain(info.This(), info.GetIsolate());
-    if (holder.IsEmpty())
+    if (!{{v8_class}}::hasInstance(info.Holder(), info.GetIsolate()))
         return; // Return silently because of [LenientThis].
-    {% else %}
-    v8::Local<v8::Object> holder = info.Holder();
     {% endif %}
+    v8::Local<v8::Object> holder = info.Holder();
     {% endif %}
     {% if raise_exception %}
     ExceptionState exceptionState(ExceptionState::SetterContext, "{{attribute.name}}", "{{interface_name}}", holder, info.GetIsolate());
