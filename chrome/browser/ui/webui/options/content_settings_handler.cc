120f15334aeb333a800f92c5fd7498d96817066c
huangs <huangs@chromium.org>
2016-05-09 15:42:33 -0700

    Revert of [Chrome Settings UI] If User Exceptions are not allowed, prevent editing / viewing. (patchset #9 id:160001 of https://chromiumcodereview.appspot.com/1855393006/ )
    Reason for revert:
    Reverting since this is causing http://crbug.com/609725 .  Problem is that we're hiding all "non-editable" entries in exception lists.  However, the list is sometimes used for read-only data, e.g., Zoom Level.  Will need to modify the logic in a follow-up.
    
    Original issue's description:
    > [Chrome Settings UI] If User Exceptions are not allowed, prevent editing / viewing.
    >
    > If user exceptions are rendered useless by policy defaults (or other default
    > providers with higher precedence), disabled them in exceptions dialog by:
    > - Hiding all editable rows.
    > - Removing the "Add New Exceptions" row.
    >
    > BUG=568031
    > CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    >
    > Committed: https://crrev.com/ff30d12673cee058e00d6d8da59ba971f24172bb
    > Cr-Commit-Position: refs/heads/master@{#391885}
    
    TBR=estade@chromium.org,dbeam@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=568031
    
    Review-Url: https://codereview.chromium.org/1962163002
    Cr-Commit-Position: refs/heads/master@{#392453}

--- chrome/browser/ui/webui/options/content_settings_handler.cc
+++ chrome/browser/ui/webui/options/content_settings_handler.cc
@@ -879,16 +879,6 @@ void ContentSettingsHandler::UpdateSettingDefaultFromModel(
 
   web_ui()->CallJavascriptFunction(
       "ContentSettings.setContentFilterSettingsValue", filter_settings);
-
-  // We assume that special cases covered above to get |provider_id| are not
-  // needed in AreUserExceptionsAllowedForType().
-  bool maybe_enable_exceptions =
-      GetContentSettingsMap()->AreUserExceptionsAllowedForType(type);
-
-  web_ui()->CallJavascriptFunction(
-      "ContentSettings.setUserExceptionsAllowed",
-      base::StringValue(ContentSettingsTypeToGroupName(type)),
-      base::FundamentalValue(maybe_enable_exceptions));
 }
 
 void ContentSettingsHandler::UpdateMediaSettingsFromPrefs(
@@ -974,7 +964,8 @@ void ContentSettingsHandler::UpdateGeolocationExceptionsView() {
     // Don't add default settings.
     if (i->primary_pattern == ContentSettingsPattern::Wildcard() &&
         i->secondary_pattern == ContentSettingsPattern::Wildcard() &&
-        i->source != site_settings::kPreferencesSource) {
+        i
+          ->source != site_settings::kPreferencesSource) {
       continue;
     }
     all_patterns_settings[std::make_pair(i->primary_pattern, i->source)]
@@ -1485,12 +1476,6 @@ void ContentSettingsHandler::RemoveException(const base::ListValue* args) {
   std::string type_string;
   CHECK(args->GetString(0, &type_string));
 
-  ContentSettingsType type = ContentSettingsTypeFromGroupName(type_string);
-
-  // If user exceptions are disabled for |type|, don't remove.
-  if (!GetContentSettingsMap()->AreUserExceptionsAllowedForType(type))
-    return;
-
   // Zoom levels are no actual content type so we need to handle them
   // separately. They would not be recognized by
   // ContentSettingsTypeFromGroupName.
@@ -1506,6 +1491,7 @@ void ContentSettingsHandler::RemoveException(const base::ListValue* args) {
     return;
   }
 
+  ContentSettingsType type = ContentSettingsTypeFromGroupName(type_string);
   RemoveExceptionFromHostContentSettingsMap(args, type);
 
   WebSiteSettingsUmaUtil::LogPermissionChange(
@@ -1529,10 +1515,6 @@ void ContentSettingsHandler::SetException(const base::ListValue* args) {
       type == CONTENT_SETTINGS_TYPE_MEDIASTREAM_CAMERA) {
     NOTREACHED();
   } else {
-    // If user exceptions are disabled for |type|, don't set.
-    if (!GetContentSettingsMap()->AreUserExceptionsAllowedForType(type))
-      return;
-
     HostContentSettingsMap* settings_map =
         mode == "normal" ? GetContentSettingsMap() :
                            GetOTRContentSettingsMap();
