abb7fc268e406fb6415e39d821329328ca58929a
nick <nick@chromium.org>
2016-05-10 11:18:50 -0700

    Switch ColumnSpecifier to enum class. This makes straddling namespaces easier.
    [splitting apart task_manager_browsertest_util, part 3 of 6]
    
    BUG=606963
    
    Review-Url: https://codereview.chromium.org/1960703002
    Cr-Commit-Position: refs/heads/master@{#392651}

--- chrome/browser/task_manager/task_manager_browsertest.cc
+++ chrome/browser/task_manager/task_manager_browsertest.cc
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/page_transition_types.h"
 
 using content::WebContents;
+using task_manager::browsertest_util::ColumnSpecifier;
 using task_manager::browsertest_util::MatchAboutBlankTab;
 using task_manager::browsertest_util::MatchAnyApp;
 using task_manager::browsertest_util::MatchAnyExtension;
@@ -825,7 +826,7 @@ IN_PROC_BROWSER_TEST_F(TaskManagerBrowserTest, ReloadExtension) {
 // http://crbug.com/241066
 IN_PROC_BROWSER_TEST_F(TaskManagerBrowserTest, WebWorkerJSHeapMemory) {
   ShowTaskManager();
-  model()->ToggleColumnVisibility(task_manager::browsertest_util::V8_MEMORY);
+  model()->ToggleColumnVisibility(ColumnSpecifier::V8_MEMORY);
   ui_test_utils::NavigateToURL(browser(), GetTestURL());
   size_t minimal_heap_size = 4 * 1024 * 1024 * sizeof(void*);
   std::string test_js = base::StringPrintf(
@@ -849,10 +850,9 @@ IN_PROC_BROWSER_TEST_F(TaskManagerBrowserTest, WebWorkerJSHeapMemory) {
   // The worker has allocated objects of at least |minimal_heap_size| bytes.
   // Wait for the heap stats to reflect this.
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerStatToExceed(
-      MatchTab("title1.html"), task_manager::browsertest_util::V8_MEMORY,
-      minimal_heap_size));
+      MatchTab("title1.html"), ColumnSpecifier::V8_MEMORY, minimal_heap_size));
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerStatToExceed(
-      MatchTab("title1.html"), task_manager::browsertest_util::V8_MEMORY_USED,
+      MatchTab("title1.html"), ColumnSpecifier::V8_MEMORY_USED,
       minimal_heap_size));
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerRows(1, MatchAnyTab()));
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerRows(1, MatchTab("title1.html")));
@@ -874,15 +874,14 @@ IN_PROC_BROWSER_TEST_F(TaskManagerBrowserTest, JSHeapMemory) {
       browser()->tab_strip_model()->GetActiveWebContents(), test_js, &ok));
   ASSERT_EQ("okay", ok);
 
-  model()->ToggleColumnVisibility(task_manager::browsertest_util::V8_MEMORY);
+  model()->ToggleColumnVisibility(ColumnSpecifier::V8_MEMORY);
 
   // The page's js has allocated objects of at least |minimal_heap_size| bytes.
   // Wait for the heap stats to reflect this.
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerStatToExceed(
-      MatchTab("title1.html"), task_manager::browsertest_util::V8_MEMORY,
-      minimal_heap_size));
+      MatchTab("title1.html"), ColumnSpecifier::V8_MEMORY, minimal_heap_size));
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerStatToExceed(
-      MatchTab("title1.html"), task_manager::browsertest_util::V8_MEMORY_USED,
+      MatchTab("title1.html"), ColumnSpecifier::V8_MEMORY_USED,
       minimal_heap_size));
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerRows(1, MatchAnyTab()));
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerRows(1, MatchTab("title1.html")));
@@ -892,7 +891,7 @@ IN_PROC_BROWSER_TEST_F(TaskManagerBrowserTest, JSHeapMemory) {
 IN_PROC_BROWSER_TEST_F(TaskManagerUtilityProcessBrowserTest,
                        UtilityJSHeapMemory) {
   ShowTaskManager();
-  model()->ToggleColumnVisibility(task_manager::browsertest_util::V8_MEMORY);
+  model()->ToggleColumnVisibility(ColumnSpecifier::V8_MEMORY);
 
   auto proxy_resolver_name =
       l10n_util::GetStringUTF16(IDS_UTILITY_PROCESS_PROXY_RESOLVER_NAME);
@@ -900,11 +899,11 @@ IN_PROC_BROWSER_TEST_F(TaskManagerUtilityProcessBrowserTest,
   // The PAC script is trivial, so don't expect a large heap.
   size_t minimal_heap_size = 1024;
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerStatToExceed(
-      MatchUtility(proxy_resolver_name),
-      task_manager::browsertest_util::V8_MEMORY, minimal_heap_size));
+      MatchUtility(proxy_resolver_name), ColumnSpecifier::V8_MEMORY,
+      minimal_heap_size));
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerStatToExceed(
-      MatchUtility(proxy_resolver_name),
-      task_manager::browsertest_util::V8_MEMORY_USED, minimal_heap_size));
+      MatchUtility(proxy_resolver_name), ColumnSpecifier::V8_MEMORY_USED,
+      minimal_heap_size));
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerRows(1, MatchAnyUtility()));
   ASSERT_NO_FATAL_FAILURE(
       WaitForTaskManagerRows(1, MatchUtility(proxy_resolver_name)));
