42ed841ac87530d853ab167552ad93353279a00c
vabr <vabr@chromium.org>
2016-05-10 09:24:04 -0700

    Add living frames to autofill driver factories
    ContentAutofillDriverFactory and ContentPasswordManagerDriverFactory both
    failed to notice live frames when DevTools got undocked.
    
    This CL makes sure that live frames are reported to the factories upon
    construction.
    
    BUG=595707
    
    Review-Url: https://codereview.chromium.org/1955413002
    Cr-Commit-Position: refs/heads/master@{#392623}

--- components/autofill/content/browser/content_autofill_driver_factory.cc
+++ components/autofill/content/browser/content_autofill_driver_factory.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/content/browser/content_autofill_driver_factory.h"
 
+#include <vector>
+
 #include "base/memory/ptr_util.h"
 #include "base/stl_util.h"
 #include "components/autofill/content/browser/content_autofill_driver.h"
@@ -23,6 +25,8 @@ const char ContentAutofillDriverFactory::
     kContentAutofillDriverFactoryWebContentsUserDataKey[] =
         "web_contents_autofill_driver_factory";
 
+ContentAutofillDriverFactory::~ContentAutofillDriverFactory() {}
+
 // static
 void ContentAutofillDriverFactory::CreateForWebContentsAndDelegate(
     content::WebContents* contents,
@@ -32,10 +36,17 @@ void ContentAutofillDriverFactory::CreateForWebContentsAndDelegate(
   if (FromWebContents(contents))
     return;
 
-  contents->SetUserData(
-      kContentAutofillDriverFactoryWebContentsUserDataKey,
-      new ContentAutofillDriverFactory(contents, client, app_locale,
-                                       enable_download_manager));
+  auto new_factory = base::WrapUnique(new ContentAutofillDriverFactory(
+      contents, client, app_locale, enable_download_manager));
+  const std::vector<content::RenderFrameHost*> frames =
+      contents->GetAllFrames();
+  for (content::RenderFrameHost* frame : frames) {
+    if (frame->IsRenderFrameLive())
+      new_factory->RenderFrameCreated(frame);
+  }
+
+  contents->SetUserData(kContentAutofillDriverFactoryWebContentsUserDataKey,
+                        new_factory.release());
 }
 
 // static
@@ -53,15 +64,7 @@ ContentAutofillDriverFactory::ContentAutofillDriverFactory(
     : content::WebContentsObserver(web_contents),
       client_(client),
       app_locale_(app_locale),
-      enable_download_manager_(enable_download_manager) {
-  content::RenderFrameHost* main_frame = web_contents->GetMainFrame();
-  if (main_frame->IsRenderFrameLive()) {
-    frame_driver_map_[main_frame] = base::WrapUnique(new ContentAutofillDriver(
-        main_frame, client_, app_locale_, enable_download_manager_));
-  }
-}
-
-ContentAutofillDriverFactory::~ContentAutofillDriverFactory() {}
+      enable_download_manager_(enable_download_manager) {}
 
 ContentAutofillDriver* ContentAutofillDriverFactory::DriverForFrame(
     content::RenderFrameHost* render_frame_host) {
