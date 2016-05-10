f4835a6bd484f2cec6ea09df6cb562378063417b
atanasova <atanasova@chromium.org>
2016-05-10 11:57:04 -0700

    Add a feature flag for enabling transparent doodle request.
    When the flag is enabled, use the transparent doodle request. This is disabled
    by default until the GWS changes are live.
    
    BUG=610319
    
    Review-Url: https://codereview.chromium.org/1956393002
    Cr-Commit-Position: refs/heads/master@{#392665}

--- chrome/browser/android/logo_service.cc
+++ chrome/browser/android/logo_service.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/android/logo_service.h"
 
+#include "base/feature_list.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/thread_task_runner_handle.h"
+#include "chrome/browser/android/chrome_feature_list.h"
 #include "chrome/browser/image_decoder.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
@@ -127,7 +129,7 @@ void LogoService::GetLogo(search_provider_logos::LogoObserver* observer) {
       base::Bind(&search_provider_logos::GoogleParseLogoResponse),
       base::Bind(&search_provider_logos::GoogleAppendQueryparamsToLogoURL),
       true, /* wants_cta */
-      false /* transparent */);
+      base::FeatureList::IsEnabled(chrome::android::kNTPTransparentDoodle));
   logo_tracker_->GetLogo(observer);
 }
 
