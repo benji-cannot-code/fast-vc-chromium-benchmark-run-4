e4f3dd50f226cc80ee2b98e7a6f55668b6823ef6
michaelpg <michaelpg@chromium.org>
2016-05-10 11:29:11 -0700

    MD Settings: Add remaining buttons to keyboard subpage
    BUG=546834
    R=stevenjb@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/1961133004
    Cr-Commit-Position: refs/heads/master@{#392658}

--- chrome/browser/resources/settings/device_page/keyboard.js
+++ chrome/browser/resources/settings/device_page/keyboard.js
@@ -54,6 +54,12 @@ Polymer({
       notify: true,
     },
 
+    /** The current active route. */
+    currentRoute: {
+      type: Object,
+      notify: true,
+    },
+
     /** @private Whether to show Caps Lock options. */
     showCapsLock_: Boolean,
 
@@ -80,7 +86,7 @@ Polymer({
   /** @override */
   ready: function() {
     cr.addWebUIListener('show-keys-changed', this.onShowKeysChange_.bind(this));
-    chrome.send('initializeKeyboardSettings');
+    settings.DevicePageBrowserProxyImpl.getInstance().initializeKeyboard();
     this.setUpKeyMapTargets_();
   },
 
@@ -114,6 +120,19 @@ Polymer({
   onShowKeysChange_: function(showCapsLock, showDiamondKey) {
     this.showCapsLock_ = showCapsLock;
     this.showDiamondKey_ = showDiamondKey;
+  },
+
+  onShowKeyboardShortcutsOverlayTap_: function() {
+    settings.DevicePageBrowserProxyImpl.getInstance()
+        .showKeyboardShortcutsOverlay();
+  },
+
+  onShowLanguageInputTap_: function() {
+    this.currentRoute = {
+      page: 'advanced',
+      section: 'languages',
+      subpage: [],
+    };
   },
 
   /** @private */
--- chrome/browser/ui/webui/settings/chromeos/device_keyboard_handler.cc
+++ chrome/browser/ui/webui/settings/chromeos/device_keyboard_handler.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/settings/chromeos/device_keyboard_handler.h"
 
+#include "ash/new_window_delegate.h"
+#include "ash/shell.h"
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/values.h"
@@ -45,6 +47,10 @@ void KeyboardHandler::RegisterMessages() {
       "initializeKeyboardSettings",
       base::Bind(&KeyboardHandler::HandleInitialize,
                  base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
+      "showKeyboardShortcutsOverlay",
+      base::Bind(&KeyboardHandler::HandleShowKeyboardShortcutsOverlay,
+                 base::Unretained(this)));
 }
 
 void KeyboardHandler::OnKeyboardDeviceConfigurationChanged() {
@@ -53,6 +59,11 @@ void KeyboardHandler::OnKeyboardDeviceConfigurationChanged() {
 
 void KeyboardHandler::HandleInitialize(const base::ListValue* args) {
   UpdateShowKeys();
+}
+
+void KeyboardHandler::HandleShowKeyboardShortcutsOverlay(
+    const base::ListValue* args) const {
+  ash::Shell::GetInstance()->new_window_delegate()->ShowKeyboardOverlay();
 }
 
 void KeyboardHandler::UpdateShowKeys() const {
