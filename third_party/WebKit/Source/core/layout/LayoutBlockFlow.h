ce9d7d990f0012cc28f23d3aa561074b28199ac9
mstensho <mstensho@opera.com>
2016-05-10 15:12:48 -0700

    Move nodeForHitTest() to LayoutBlockFlow.
    LayoutBlock shouldn't have to deal with continuations.
    
    BUG=302024
    
    Review-Url: https://codereview.chromium.org/1966713002
    Cr-Commit-Position: refs/heads/master@{#392742}

--- third_party/WebKit/Source/core/layout/LayoutBlockFlow.h
+++ third_party/WebKit/Source/core/layout/LayoutBlockFlow.h
@@ -336,6 +336,7 @@ protected:
 
     PaintInvalidationReason invalidatePaintIfNeeded(const PaintInvalidationState&) override;
 
+    Node* nodeForHitTest() const final;
     bool hitTestChildren(HitTestResult&, const HitTestLocation& locationInContainer, const LayoutPoint& accumulatedOffset, HitTestAction) override;
 
 private:
