dbcd72c69eb721c0e52b38e8258f1be03e588b5b
markdittmer <markdittmer@chromium.org>
2016-05-10 07:20:59 -0700

    Port PlatformWindowMus::SetBounds to NativeWidgetMus
    BUG=609555
    
    Review-Url: https://codereview.chromium.org/1953293004
    Cr-Commit-Position: refs/heads/master@{#392597}

--- ui/views/mus/platform_window_mus.cc
+++ ui/views/mus/platform_window_mus.cc
@@ -129,9 +129,7 @@ void PlatformWindowMus::Close() {
   NOTIMPLEMENTED();
 }
 
-void PlatformWindowMus::SetBounds(const gfx::Rect& bounds) {
-  mus_window_->SetBounds(bounds);
-}
+void PlatformWindowMus::SetBounds(const gfx::Rect& bounds) {}
 
 gfx::Rect PlatformWindowMus::GetBounds() {
   return mus_window_->bounds();
@@ -199,12 +197,6 @@ void PlatformWindowMus::OnWindowDestroyed(mus::Window* window) {
 #ifndef NDEBUG
   DCHECK(!weak_ptr);
 #endif
-}
-
-void PlatformWindowMus::OnWindowBoundsChanged(mus::Window* window,
-                                              const gfx::Rect& old_bounds,
-                                              const gfx::Rect& new_bounds) {
-  delegate_->OnBoundsChanged(new_bounds);
 }
 
 void PlatformWindowMus::OnWindowFocusChanged(mus::Window* gained_focus,
