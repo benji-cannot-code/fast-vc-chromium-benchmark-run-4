a18009364578251cab2103b4f6ca9e7f9908c9e8
xdai <xdai@chromium.org>
2016-05-10 16:28:23 -0700

    Clean up unused ErrorScreenActor and ErrorScreenDelegate.
    BUG=None
    
    Review-Url: https://codereview.chromium.org/1968753002
    Cr-Commit-Position: refs/heads/master@{#392769}

--- chrome/browser/ui/webui/chromeos/login/app_launch_splash_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/app_launch_splash_screen_handler.h
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-class ErrorScreenActor;
-
 // A class that handles the WebUI hooks for the app launch splash screen.
 class AppLaunchSplashScreenHandler
     : public BaseScreenHandler,
