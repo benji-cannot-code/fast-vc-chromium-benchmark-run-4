606b4e98f089b8c0dad85cea47e43e5451d1a3c4
wfh <wfh@chromium.org>
2016-05-09 19:39:50 -0700

    Add more sandbox error codes for interception manager.
    Also, cleanup some use of raw pointers and c++ificate the code a bit.
    
    BUG=610151
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win10_chromium_x64_rel_ng
    
    Review-Url: https://codereview.chromium.org/1966483002
    Cr-Commit-Position: refs/heads/master@{#392528}

--- sandbox/win/src/service_resolver_32.cc
+++ sandbox/win/src/service_resolver_32.cc
@@ -183,7 +183,7 @@ NTSTATUS ServiceResolverThunk::Setup(const void* target_module,
 
   if (!IsFunctionAService(&thunk->original) &&
       (!relaxed_ || !SaveOriginalFunction(&thunk->original, thunk_storage))) {
-    return STATUS_UNSUCCESSFUL;
+    return STATUS_OBJECT_NAME_COLLISION;
   }
 
   ret = PerformPatch(thunk, thunk_storage);
@@ -215,7 +215,7 @@ NTSTATUS ServiceResolverThunk::CopyThunk(const void* target_module,
 
   if (!IsFunctionAService(&thunk->original) &&
       (!relaxed_ || !SaveOriginalFunction(&thunk->original, thunk_storage))) {
-    return STATUS_UNSUCCESSFUL;
+    return STATUS_OBJECT_NAME_COLLISION;
   }
 
   if (NULL != storage_used)
