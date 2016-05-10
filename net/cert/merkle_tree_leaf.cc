862a190573ab9229811a59a32c54f2c9ed3203a5
robpercival <robpercival@chromium.org>
2016-05-10 13:41:52 -0700

    Adds function for generating MerkleTreeLeaf hash.
    This will be used when requesting audit proofs from Certificate Transparency
    logs.
    
    BUG=608868
    
    Review-Url: https://codereview.chromium.org/1945183005
    Cr-Commit-Position: refs/heads/master@{#392703}

--- net/cert/merkle_tree_leaf.cc
+++ net/cert/merkle_tree_leaf.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/cert/merkle_tree_leaf.h"
 
+#include "crypto/sha2.h"
 #include "net/cert/ct_objects_extractor.h"
+#include "net/cert/ct_serialization.h"
 #include "net/cert/x509_certificate.h"
 
 namespace net {
@@ -15,6 +17,16 @@ namespace ct {
 MerkleTreeLeaf::MerkleTreeLeaf() {}
 
 MerkleTreeLeaf::~MerkleTreeLeaf() {}
+
+bool Hash(const MerkleTreeLeaf& tree_leaf, std::string* out) {
+  // Prepend 0 byte as per RFC 6962, section-2.1
+  std::string leaf_in_tls_format("\x00", 1);
+  if (!EncodeTreeLeaf(tree_leaf, &leaf_in_tls_format))
+    return false;
+
+  *out = crypto::SHA256HashString(leaf_in_tls_format);
+  return true;
+}
 
 bool GetMerkleTreeLeaf(const X509Certificate* cert,
                        const SignedCertificateTimestamp* sct,
