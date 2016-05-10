ab959eb801a052e025f7b6b2a9b2692e1cc08717
sigbjornf <sigbjornf@opera.com>
2016-05-10 10:52:00 -0700

    Drop unnecessary uses of GarbageCollectedFinalized<>.
    Objects with Member<> fields can derive from the non-finalized
    GarbageCollected<> instead.
    
    R=
    BUG=389343
    
    Review-Url: https://codereview.chromium.org/1961173004
    Cr-Commit-Position: refs/heads/master@{#392641}

--- third_party/WebKit/Source/modules/vr/VRPose.h
+++ third_party/WebKit/Source/modules/vr/VRPose.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class VRPose final : public GarbageCollectedFinalized<VRPose>, public ScriptWrappable {
+class VRPose final : public GarbageCollected<VRPose>, public ScriptWrappable {
     DEFINE_WRAPPERTYPEINFO();
 public:
     static VRPose* create()
--- third_party/WebKit/Source/modules/vr/VRStageParameters.h
+++ third_party/WebKit/Source/modules/vr/VRStageParameters.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class VRStageParameters final : public GarbageCollectedFinalized<VRStageParameters>, public ScriptWrappable {
+class VRStageParameters final : public GarbageCollected<VRStageParameters>, public ScriptWrappable {
     DEFINE_WRAPPERTYPEINFO();
 public:
     VRStageParameters();
