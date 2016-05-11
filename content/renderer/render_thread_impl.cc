ed1902dc6fb03643775fa72fd5004a8ac4d22916
danakj <danakj@chromium.org>
2016-05-11 15:15:09 -0700

    Give the media context small memory limits (same as compositor).
    This makes a static gpu::SharedMemoryLimits::ForMailboxContext() method
    so that RenderThreadImpl and RenderWidget can share the same limits for
    the media context and the compositor context which both use the context
    for creating textures and mailboxing them.
    
    R=piman@chromium.org
    BUG=608946
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/1969163002
    Cr-Commit-Position: refs/heads/master@{#393086}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -407,6 +407,7 @@ void StringToUintVector(const std::string& str, std::vector<unsigned>* vector) {
 
 scoped_refptr<ContextProviderCommandBuffer> CreateOffscreenContext(
     scoped_refptr<gpu::GpuChannelHost> gpu_channel_host,
+    const gpu::SharedMemoryLimits& limits,
     command_buffer_metrics::ContextType type,
     int32_t stream_id,
     gpu::GpuStreamPriority stream_priority) {
@@ -429,8 +430,8 @@ scoped_refptr<ContextProviderCommandBuffer> CreateOffscreenContext(
       std::move(gpu_channel_host), stream_id, stream_priority,
       gpu::kNullSurfaceHandle,
       GURL("chrome://gpu/RenderThreadImpl::CreateOffscreenContext"),
-      gfx::PreferIntegratedGpu, automatic_flushes, gpu::SharedMemoryLimits(),
-      attributes, nullptr, type));
+      gfx::PreferIntegratedGpu, automatic_flushes, limits, attributes, nullptr,
+      type));
 }
 
 }  // namespace
@@ -1415,10 +1416,14 @@ media::GpuVideoAcceleratorFactories* RenderThreadImpl::GetGpuFactories() {
       EstablishGpuChannelSync(CAUSE_FOR_GPU_LAUNCH_MEDIA_CONTEXT);
   if (!gpu_channel_host)
     return nullptr;
+  // This context is only used to create textures and mailbox them, so
+  // use lower limits than the default.
+  gpu::SharedMemoryLimits limits = gpu::SharedMemoryLimits::ForMailboxContext();
   scoped_refptr<ContextProviderCommandBuffer> media_context_provider =
-      CreateOffscreenContext(
-          gpu_channel_host, command_buffer_metrics::RENDER_WORKER_CONTEXT,
-          gpu::GPU_STREAM_DEFAULT, gpu::GpuStreamPriority::NORMAL);
+      CreateOffscreenContext(gpu_channel_host, limits,
+                             command_buffer_metrics::RENDER_WORKER_CONTEXT,
+                             gpu::GPU_STREAM_DEFAULT,
+                             gpu::GpuStreamPriority::NORMAL);
   if (!media_context_provider->BindToCurrentThread())
     return nullptr;
   media_context_provider->SetupLock();
@@ -1463,7 +1468,7 @@ RenderThreadImpl::SharedMainThreadContextProvider() {
   }
 
   shared_main_thread_contexts_ = CreateOffscreenContext(
-      std::move(gpu_channel_host),
+      std::move(gpu_channel_host), gpu::SharedMemoryLimits(),
       command_buffer_metrics::RENDERER_MAINTHREAD_CONTEXT,
       gpu::GPU_STREAM_DEFAULT, gpu::GpuStreamPriority::NORMAL);
   if (!shared_main_thread_contexts_->BindToCurrentThread())
@@ -1969,10 +1974,10 @@ RenderThreadImpl::SharedCompositorWorkerContextProvider() {
     stream_priority = gpu::GpuStreamPriority::LOW;
   }
 
-  shared_worker_context_provider_ =
-      CreateOffscreenContext(std::move(gpu_channel_host),
-                             command_buffer_metrics::RENDER_WORKER_CONTEXT,
-                             stream_id, stream_priority);
+  shared_worker_context_provider_ = CreateOffscreenContext(
+      std::move(gpu_channel_host), gpu::SharedMemoryLimits(),
+      command_buffer_metrics::RENDER_WORKER_CONTEXT, stream_id,
+      stream_priority);
   if (!shared_worker_context_provider_->BindToCurrentThread())
     shared_worker_context_provider_ = nullptr;
   if (shared_worker_context_provider_)
