80f5f74421e1adc8d90c7020a78a8aeb99d5262d
gab <gab@chromium.org>
2016-05-11 11:09:39 -0700

    Fix include path for moved thread_task_runner_handle.h header in extensions/
    Changes made by tools/git/move_source_file.py
    
    BUG=610438
    TBR=mek
    
    Review-Url: https://codereview.chromium.org/1964333003
    Cr-Commit-Position: refs/heads/master@{#392986}

--- extensions/browser/value_store/leveldb_value_store.cc
+++ extensions/browser/value_store/leveldb_value_store.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/sys_string_conversions.h"
-#include "base/thread_task_runner_handle.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "base/trace_event/process_memory_dump.h"
 #include "content/public/browser/browser_thread.h"
