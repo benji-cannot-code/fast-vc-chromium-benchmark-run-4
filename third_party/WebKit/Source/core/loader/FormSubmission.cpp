26264cb57331028b58be285bf109652c823035ef
mkwst <mkwst@chromium.org>
2016-05-11 02:57:02 -0700

    UPGRADE: Correctly handle navigations.
    The behavior of 'upgrade-insecure-requests' has regressed for iframes and
    navigations due to some recent changes in those bits of the loader. Of
    course, I was an idiot, and didn't write layout tests for those pieces,
    but relied on unit tests. The unit tests kept working, because they hard
    coded the expected inputs! Hooray! The web, however, broke. Bleh.
    
    This patch adjusts things such that iframes and navigations are correctly
    upgraded in the presence of the directive.
    
    As a drive-by, it also disables the mixed content warning that we show for
    insecure form actions on secure pages if upgrading is active (as part of
    the UIR promise is that we won't break your security indicators).
    
    BUG=610441,610188
    
    Review-Url: https://codereview.chromium.org/1964303003
    Cr-Commit-Position: refs/heads/master@{#392890}

--- third_party/WebKit/Source/core/loader/FormSubmission.cpp
+++ third_party/WebKit/Source/core/loader/FormSubmission.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/InputTypeNames.h"
 #include "core/dom/Document.h"
 #include "core/events/Event.h"
+#include "core/frame/UseCounter.h"
 #include "core/html/FormData.h"
 #include "core/html/HTMLFormControlElement.h"
 #include "core/html/HTMLFormElement.h"
@@ -197,6 +198,14 @@ FormSubmission* FormSubmission::create(HTMLFormElement* form, const Attributes& 
 
     Document& document = form->document();
     KURL actionURL = document.completeURL(copiedAttributes.action().isEmpty() ? document.url().getString() : copiedAttributes.action());
+
+    if (document.getInsecureRequestsPolicy() == SecurityContext::InsecureRequestsUpgrade && actionURL.protocolIs("http")) {
+        UseCounter::count(document, UseCounter::UpgradeInsecureRequestsUpgradedRequest);
+        actionURL.setProtocol("https");
+        if (actionURL.port() == 80)
+            actionURL.setPort(443);
+    }
+
     bool isMailtoForm = actionURL.protocolIs("mailto");
     bool isMultiPartForm = false;
     AtomicString encodingType = copiedAttributes.encodingType();
