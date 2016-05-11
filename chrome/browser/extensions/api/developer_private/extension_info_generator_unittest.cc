c60491e51ab78d5cebfd98119f6c3a6dbdcf5d7d
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-05-11 10:25:34 -0700

    [MD Extensions] Add permissions to the detail view
    Add a permissions list to the detail view. Note, this does not account
    for:
    - retained file permissions
    - retained device permissions
    - revokable permissions
    
    BUG=529395
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/1944313004
    Cr-Commit-Position: refs/heads/master@{#392968}

--- chrome/browser/extensions/api/developer_private/extension_info_generator_unittest.cc
+++ chrome/browser/extensions/api/developer_private/extension_info_generator_unittest.cc
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_builder.h"
 #include "extensions/common/feature_switch.h"
+#include "extensions/common/permissions/permission_message.h"
+#include "extensions/common/permissions/permissions_data.h"
 #include "extensions/common/value_builder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -190,7 +192,8 @@ TEST_F(ExtensionInfoGeneratorUnitTest, BasicInfoTest) {
           .Set("version", kVersion)
           .Set("manifest_version", 2)
           .Set("description", "an extension")
-          .Set("permissions", ListBuilder().Append("file://*/*").Build())
+          .Set("permissions",
+               ListBuilder().Append("file://*/*").Append("tabs").Build())
           .Build();
   std::unique_ptr<base::DictionaryValue> manifest_copy(manifest->DeepCopy());
   scoped_refptr<const Extension> extension =
@@ -236,6 +239,14 @@ TEST_F(ExtensionInfoGeneratorUnitTest, BasicInfoTest) {
   EXPECT_FALSE(info->file_access.is_active);
   EXPECT_TRUE(info->incognito_access.is_enabled);
   EXPECT_FALSE(info->incognito_access.is_active);
+  PermissionMessages messages =
+      extension->permissions_data()->GetPermissionMessages();
+  ASSERT_EQ(messages.size(), info->permissions.size());
+  size_t i = 0;
+  for (const PermissionMessage& message : messages) {
+    EXPECT_EQ(message.message(), base::UTF8ToUTF16(info->permissions[i]));
+    ++i;
+  }
   ASSERT_EQ(2u, info->runtime_errors.size());
   const api::developer_private::RuntimeError& runtime_error =
       info->runtime_errors[0];
--- third_party/closure_compiler/externs/developer_private.js
+++ third_party/closure_compiler/externs/developer_private.js
@@ -301,6 +301,7 @@ chrome.developerPrivate.DependentExtension;
  *   offlineEnabled: boolean,
  *   optionsPage: (!chrome.developerPrivate.OptionsPage|undefined),
  *   path: (string|undefined),
+ *   permissions: !Array<string>,
  *   prettifiedPath: (string|undefined),
  *   runOnAllUrls: !chrome.developerPrivate.AccessModifier,
  *   runtimeErrors: !Array<!chrome.developerPrivate.RuntimeError>,
