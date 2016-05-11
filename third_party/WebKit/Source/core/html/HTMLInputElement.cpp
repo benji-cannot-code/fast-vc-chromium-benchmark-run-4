a5ea7d278c028507ece3c10c0171515c12ad2091
tkent <tkent@chromium.org>
2016-05-11 01:54:49 -0700

    Rename HTMLInputElement::m_reflectsCheckedAttribute to m_dirtyCheckedness
    and reverse its value.  'dirty checkedness flag' is the term used in the HTML
    specification.
    
    This CL has no behavior changes.
    
    Review-Url: https://codereview.chromium.org/1964333002
    Cr-Commit-Position: refs/heads/master@{#392881}

--- third_party/WebKit/Source/core/html/HTMLInputElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLInputElement.cpp
@@ -108,7 +108,7 @@ HTMLInputElement::HTMLInputElement(Document& document, HTMLFormElement* form, bo
     , m_minLength(-1)
     , m_maxResults(-1)
     , m_isChecked(false)
-    , m_reflectsCheckedAttribute(true)
+    , m_dirtyCheckedness(false)
     , m_isIndeterminate(false)
     , m_isActivatedSubmit(false)
     , m_autocomplete(Uninitialized)
@@ -700,9 +700,9 @@ void HTMLInputElement::parseAttribute(const QualifiedName& name, const AtomicStr
         // restore. We shouldn't call setChecked() even if this has the checked
         // attribute. So, delay the setChecked() call until
         // finishParsingChildren() is called if parsing is in progress.
-        if (!m_parsingInProgress && m_reflectsCheckedAttribute) {
+        if (!m_parsingInProgress && !m_dirtyCheckedness) {
             setChecked(!value.isNull());
-            m_reflectsCheckedAttribute = true;
+            m_dirtyCheckedness = false;
         }
         pseudoStateChanged(CSSSelector::PseudoDefault);
     } else if (name == maxlengthAttr) {
@@ -796,7 +796,7 @@ void HTMLInputElement::finishParsingChildren()
         bool checked = hasAttribute(checkedAttr);
         if (checked)
             setChecked(checked);
-        m_reflectsCheckedAttribute = true;
+        m_dirtyCheckedness = false;
     }
 }
 
@@ -878,7 +878,7 @@ void HTMLInputElement::resetImpl()
     }
 
     setChecked(hasAttribute(checkedAttr));
-    m_reflectsCheckedAttribute = true;
+    m_dirtyCheckedness = false;
 }
 
 bool HTMLInputElement::isTextField() const
@@ -900,7 +900,7 @@ bool HTMLInputElement::checked() const
 
 void HTMLInputElement::setChecked(bool nowChecked, TextFieldEventBehavior eventBehavior)
 {
-    m_reflectsCheckedAttribute = false;
+    m_dirtyCheckedness = true;
     if (checked() == nowChecked)
         return;
 
@@ -964,7 +964,7 @@ void HTMLInputElement::copyNonAttributePropertiesFromElement(const Element& sour
 
     m_valueIfDirty = sourceElement.m_valueIfDirty;
     setChecked(sourceElement.m_isChecked);
-    m_reflectsCheckedAttribute = sourceElement.m_reflectsCheckedAttribute;
+    m_dirtyCheckedness = sourceElement.m_dirtyCheckedness;
     m_isIndeterminate = sourceElement.m_isIndeterminate;
 
     HTMLTextFormControlElement::copyNonAttributePropertiesFromElement(source);
