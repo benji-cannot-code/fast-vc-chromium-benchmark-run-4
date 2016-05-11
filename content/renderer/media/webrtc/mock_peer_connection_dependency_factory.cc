cad872e230f133ce364e4dc5a7edb8234754d1d4
miu <miu@chromium.org>
2016-05-10 21:04:16 -0700

    MediaStream audio: Refactor 3 separate "glue" implementations into one.
    This is a refactoring of the MediaStreamAudioSource/Track object graph
    such that life-cycle and audio flow control are unified into a single
    architecture.  Previously, each of three implementations solved this
    same problem in three different ways; and this has made it difficult to
    maintain the code in src/content/renderer/media across all product
    features, WebRTC or otherwise.
    
    Diagram of post-refactoring class relationships: https://docs.google.com/drawings/d/1yTsXvRMIyMlXjIEeQOVqXPxMPWgekB5ePWUVSI5-zwo/edit?usp=sharing
    
    The new architeture is as follows:
    
    1. MediaStreamAudioSource becomes a base class implementation for
    creating MediaStreamAudioTracks and delivering audio to them from the
    source (i.e., an AudioInputDevice, a PeerConnection remote source, or a
    WebAudio source).  All of its methods are now assumed to be run on the
    main thread, while audio flow may optionally occur on a separate
    thread.
    
    2. MediaStreamAudioTrack becomes a base class implementation for
    connecting/disconnecting MediaStreamAudioSinks, and delivering audio to
    them from the source.
    
    3. Both MediaStreamAudioSource and MediaStreamAudioTrack are owned by
    their blink counterparts (WebMediaStreamSource and WebMediaStreamTrack),
    and their destruction may safely occur any time the blink implementation
    requires it.
    
    Following the new architeture, the refactoring is:
    
    1. WebRtcAudioCapturer becomes a sub-class of MediaStreamAudioSource and
    is renamed to ProcessedLocalAudioSource.  This new class owns and
    manages the WebRTC-specific audio processing on the source data.  Also:
    a) Significant implementation from PeerConnectionDependencyFactory was
    consolidated/moved to this class.  b) The "EnablePeerConnectionMode"
    functionality, which re-started the audio input with a different buffer
    size, has been removed to simplify the implementation.  Now, buffer size
    is determined BEFORE the first time the audio input device is started.
    c) Currently, all local audio sources (i.e., via AudioInputDevice) are
    routed through this pipeline, but a soon-upcoming change will split the
    WebRTC-specific cases from the ones that should not go through audio
    processing.
    
    2. MediaStreamRemoteAudioTrack becomes a sub-class of
    MediaStreamAudioSource and is renamed to
    PeerConnectionRemoteAudioSource.  This new class owns and manages the
    flow of audio data from a PeerConnection into the MediaStream
    framework.
    
    3. WebAudioCapturer becomes a sub-class of MediaStreamAudioSource and is
    renamed to WebAudioMediaStreamSource.  As a
    blink::WebAudioDestinationConsumer, it manages the flow of audio from a
    WebAudio destination node into the MediaStream framework.
    
    BUG=577881,577874
    
    Review-Url: https://codereview.chromium.org/1834323002
    Cr-Commit-Position: refs/heads/master@{#392845}

--- content/renderer/media/webrtc/mock_peer_connection_dependency_factory.cc
+++ content/renderer/media/webrtc/mock_peer_connection_dependency_factory.cc
@@ -10,11 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/strings/utf_string_conversions.h"
 #include "content/renderer/media/mock_peer_connection_impl.h"
-#include "content/renderer/media/webaudio_capturer_source.h"
-#include "content/renderer/media/webrtc/webrtc_local_audio_track_adapter.h"
 #include "content/renderer/media/webrtc/webrtc_video_capturer_adapter.h"
-#include "content/renderer/media/webrtc_audio_capturer.h"
-#include "content/renderer/media/webrtc_local_audio_track.h"
 #include "third_party/WebKit/public/platform/WebMediaStreamTrack.h"
 #include "third_party/webrtc/api/mediastreaminterface.h"
 #include "third_party/webrtc/base/scoped_ref_ptr.h"
@@ -381,7 +377,9 @@ class MockIceCandidate : public IceCandidateInterface {
 
 MockPeerConnectionDependencyFactory::MockPeerConnectionDependencyFactory()
     : PeerConnectionDependencyFactory(NULL),
-      fail_to_create_next_audio_capturer_(false) {
+      signaling_thread_("MockPCFactory WebRtc Signaling Thread") {
+  EnsureWebRtcAudioDeviceImpl();
+  CHECK(signaling_thread_.Start());
 }
 
 MockPeerConnectionDependencyFactory::~MockPeerConnectionDependencyFactory() {}
@@ -416,9 +414,6 @@ MockPeerConnectionDependencyFactory::CreateVideoSource(
   NOTIMPLEMENTED();
   return nullptr;
 }
-
-void MockPeerConnectionDependencyFactory::CreateWebAudioSource(
-    blink::WebMediaStreamSource* source) {}
 
 scoped_refptr<webrtc::MediaStreamInterface>
 MockPeerConnectionDependencyFactory::CreateLocalMediaStream(
@@ -459,19 +454,9 @@ MockPeerConnectionDependencyFactory::CreateIceCandidate(
   return new MockIceCandidate(sdp_mid, sdp_mline_index, sdp);
 }
 
-std::unique_ptr<WebRtcAudioCapturer>
-MockPeerConnectionDependencyFactory::CreateAudioCapturer(
-    int render_frame_id,
-    const StreamDeviceInfo& device_info,
-    const blink::WebMediaConstraints& constraints,
-    MediaStreamAudioSource* audio_source) {
-  if (fail_to_create_next_audio_capturer_) {
-    fail_to_create_next_audio_capturer_ = false;
-    return NULL;
-  }
-  DCHECK(audio_source);
-  return WebRtcAudioCapturer::CreateCapturer(-1, device_info, constraints, NULL,
-                                             audio_source);
+scoped_refptr<base::SingleThreadTaskRunner>
+MockPeerConnectionDependencyFactory::GetWebRtcSignalingThread() const {
+  return signaling_thread_.task_runner();
 }
 
 }  // namespace content
