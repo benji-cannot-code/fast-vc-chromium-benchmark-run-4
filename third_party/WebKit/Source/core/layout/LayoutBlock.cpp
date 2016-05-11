9f42c4dead09aa0e890f57d6da46b77a9a8c2cf9
mstensho <mstensho@opera.com>
2016-05-11 14:40:52 -0700

    Move updateDragState() override to LayoutBlockFlow.
    Only continuations stuff there.
    
    BUG=302024
    
    Review-Url: https://codereview.chromium.org/1969103002
    Cr-Commit-Position: refs/heads/master@{#393071}

--- third_party/WebKit/Source/core/layout/LayoutBlock.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlock.cpp
@@ -1948,13 +1948,6 @@ LayoutBlockFlow* LayoutBlock::nearestInnerBlockWithFirstLine()
     return nullptr;
 }
 
-void LayoutBlock::updateDragState(bool dragOn)
-{
-    LayoutBox::updateDragState(dragOn);
-    if (LayoutBoxModelObject* continuation = this->continuation())
-        continuation->updateDragState(dragOn);
-}
-
 void LayoutBlock::childBecameNonInline(LayoutObject*)
 {
     makeChildrenNonInline();
