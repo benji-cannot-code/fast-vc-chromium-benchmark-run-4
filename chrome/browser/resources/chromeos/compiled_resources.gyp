961b8252c463c070e206151280a09ab9134d8a2a
jdufault <jdufault@chromium.org>
2016-05-11 15:03:49 -0700

    Add a very basic PIN UI implementation that is shared between lock and settings.
    Most things are still in flux, but this will help provide some structure for the
    rest of the code.
    
    BUG=603217
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/1933913002
    Cr-Commit-Position: refs/heads/master@{#393081}

--- chrome/browser/resources/chromeos/compiled_resources.gyp
+++ chrome/browser/resources/chromeos/compiled_resources.gyp
@@ -23,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'externs': ['<(EXTERNS_DIR)/chrome_send.js'],
       },
       'includes': ['../../../../third_party/closure_compiler/compile_js.gypi'],
+    },
+    {
+      'target_name': 'quick_unlock/pin_keyboard',
+      'includes': ['../../../../third_party/closure_compiler/compile_js.gypi'],
     }
   ],
 }
--- ui/login/account_picker/user_pod_row.js
+++ ui/login/account_picker/user_pod_row.js
@@ -733,6 +733,13 @@ cr.define('login', function() {
             'keydown',
             this.handleRemoveUserConfirmationKeyDown_.bind(this));
 
+      var pinKeyboard = $('pin-keyboard');
+      // The pin keyboard is not present on the md user manager.
+      if (pinKeyboard) {
+        pinKeyboard.addEventListener('submit',
+            this.handlePinSubmitted_.bind(this));
+      }
+
       var customIcon = this.customIconElement;
       customIcon.parentNode.replaceChild(new UserPodCustomIcon(), customIcon);
     },
@@ -763,6 +770,16 @@ cr.define('login', function() {
       // Note: the |mainInput| can be the pod itself.
       this.mainInput.tabIndex = -1;
       this.tabIndex = UserPodTabOrder.POD_INPUT;
+    },
+
+    /**
+     * Handles the user hitting 'submit' on the PIN keyboard.
+     * @param {Event} e Submit event object.
+     * @private
+     */
+    handlePinSubmitted_: function(e) {
+      var pin = e.detail.pin;
+      chrome.send('authenticateUserWithPin', [this.user.username, pin]);
     },
 
     /**
