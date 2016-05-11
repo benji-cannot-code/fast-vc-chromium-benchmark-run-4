0dfd2f99b49c6624949125a07474d32499a73601
gab <gab@chromium.org>
2016-05-11 12:21:26 -0700

    Fix include path for moved thread_task_runner_handle.h header in win8/
    Changes made by tools/git/move_source_file.py
    
    BUG=610438
    TBR=robertshield
    
    Review-Url: https://codereview.chromium.org/1973453003
    Cr-Commit-Position: refs/heads/master@{#393019}

--- win8/test/open_with_dialog_async.cc
+++ win8/test/open_with_dialog_async.cc
@@ -16,9 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/memory/ref_counted.h"
 #include "base/single_thread_task_runner.h"
-#include "base/thread_task_runner_handle.h"
 #include "base/threading/platform_thread.h"
 #include "base/threading/thread.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/win/windows_version.h"
 
 namespace win8 {
