4e40d7fc4f1063f7ae82875b6c00681f9648cfd3
alph <alph@chromium.org>
2016-05-10 19:49:15 -0700

    DevTools: increase sampling heap profiler resolution.
    Increase both stack depth and sampling frequency.
    
    BUG=586613
    
    Review-Url: https://codereview.chromium.org/1966103002
    Cr-Commit-Position: refs/heads/master@{#392836}

--- third_party/WebKit/Source/devtools/front_end/sdk/HeapProfilerModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/HeapProfilerModel.js
@@ -32,7 +32,8 @@ WebInspector.HeapProfilerModel.prototype = {
 
     startSampling: function()
     {
-        this._heapProfilerAgent.startSampling();
+        var defaultSamplingIntervalInBytes = 16384;
+        this._heapProfilerAgent.startSampling(defaultSamplingIntervalInBytes);
     },
 
     /**
