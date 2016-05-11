6dc4352abf3685fb60bd9863aa82f77c222ee26e
shrike <shrike@chromium.org>
2016-05-11 10:12:31 -0700

    [Mac][Material Design] Fix ghosting of cast and other extension icons.
    Chrome Mac uses the ToolbarButton class to add extension buttons to the
    toolbar, and calls setImage: to set the button's default image. However
    under Material Design the ToolbarButton class sets images for several
    states (e.g. hover, pressed) - the result is double-drawing of the
    default icon. This cl changes ToolbarButton to store the setImage: icon
    in an ivar, which prevents the double-drawing but also makes the icon
    accessible for later use.
    
    This cl also fixes a problem where the default icon could be drawn in
    a different location for the default state than in the other states,
    causing the icon to appear to shift as you mouse over the button.
    
    R=avi@chromium.org
    BUG=593796
    
    Review-Url: https://codereview.chromium.org/1965193002
    Cr-Commit-Position: refs/heads/master@{#392964}

--- chrome/browser/ui/cocoa/toolbar/toolbar_button_cocoa.h
+++ chrome/browser/ui/cocoa/toolbar/toolbar_button_cocoa.h
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
+#import "base/mac/scoped_nsobject.h"
 #import "chrome/browser/ui/cocoa/themed_window.h"
 #import "ui/gfx/color_utils.h"
 #include "ui/gfx/vector_icons_public.h"
 
 enum class ToolbarButtonImageBackgroundStyle {
+  DEFAULT,
   HOVER,
   HOVER_THEMED,
   PRESSED,
@@ -22,6 +24,9 @@ enum class ToolbarButtonImageBackgroundStyle {
 // NSButton subclass which handles middle mouse clicking.
 @interface ToolbarButton : NSButton<ThemedWindowDrawing>  {
  @protected
+  // The toolbar button's image, as assigned by setImage:.
+  base::scoped_nsobject<NSImage> image_;
+
   // YES when middle mouse clicks should be handled.
   BOOL handleMiddleClick_;
 }
