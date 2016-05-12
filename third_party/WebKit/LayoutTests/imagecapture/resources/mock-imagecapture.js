82680860a1a773a688e1d6ef6e2ab627f7fb0382
mcasas <mcasas@chromium.org>
2016-05-12 10:36:08 -0700

    ImageCapture: takePhoto() mock LayoutTest
    ImageCapture takePhoto() implementation needs a
    meaningful MediaStream implementation in Browser
    side, but LayoutTests doesn't have it. Following the
    example of USB, 'battery-status' and 'payments'
    Layout Tests, this CL:
    - creates a LayoutTest entry for image capture
    - defines a MockImageCapture js class, that simply
    resolves successfully the takePhoto() Promise
    - adds a JS test that instantiates said Mock and
    exercises it.
    
    The previous LayoutTest/fast/imagecapture/...takePhoto
    is removed.
    
    BUG=610333
    
    Review-Url: https://codereview.chromium.org/1966083003
    Cr-Commit-Position: refs/heads/master@{#393290}

---
+++ third_party/WebKit/LayoutTests/imagecapture/resources/mock-imagecapture.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+"use strict";
+
+let mockImageCaptureReady = define(
+  'mockImageCapture',
+  ['third_party/WebKit/public/platform/modules/imagecapture/image_capture.mojom',
+   'mojo/public/js/bindings',
+   'mojo/public/js/connection',
+   'content/public/renderer/service_registry',
+  ], (imageCapture, bindings, connection, serviceRegistry) => {
+
+  class MockImageCapture {
+    constructor() {
+      serviceRegistry.addServiceOverrideForTesting(
+          imageCapture.ImageCapture.name,
+          pipe => this.bindToPipe(pipe));
+    }
+
+    bindToPipe(pipe) {
+      this.stub_ = connection.bindHandleToStub(
+          pipe, imageCapture.ImageCapture);
+      bindings.StubBindings(this.stub_).delegate = this;
+    }
+
+    takePhoto(sourceid) {
+      return Promise.resolve({ mime_type : 'image/cat',
+                               data : "(,,,)=(^.^)=(,,,)" });
+    }
+  }
+  return new MockImageCapture();
+});
