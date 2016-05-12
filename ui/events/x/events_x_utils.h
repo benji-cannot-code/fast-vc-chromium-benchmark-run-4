b6f1788b5f7216a8f156addccc34d6f3bf7dd95b
caseq <caseq@chromium.org>
2016-05-11 18:25:59 -0700

    Fix ui::Event timestamps in case x11 timestamps rolls over MAX_UINT32 ms
    This fixes the roll-over detection of x11 timestamps to work in case
    the roll-over in question happened before the start of chrome by ensuring
    the timestamps use the same base as TimeTicks.
    
    BUG=578243
    
    Committed: https://crrev.com/6b68f5780d585dffac4c6f8f70ce9be47e314c50
    Cr-Commit-Position: refs/heads/master@{#392996}
    
    Review-Url: https://codereview.chromium.org/1942083002
    Cr-Commit-Position: refs/heads/master@{#393133}

--- ui/events/x/events_x_utils.h
+++ ui/events/x/events_x_utils.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <memory>
+
+#include "base/time/tick_clock.h"
 #include "base/time/time.h"
 #include "ui/events/event_constants.h"
 #include "ui/events/x/events_x_export.h"
@@ -81,7 +84,8 @@ EVENTS_X_EXPORT bool GetFlingDataFromXEvent(const XEvent& xev,
                                             float* vy_ordinal,
                                             bool* is_cancel);
 
-EVENTS_X_EXPORT void ResetTimestampRolloverCountersForTesting();
+EVENTS_X_EXPORT void ResetTimestampRolloverCountersForTesting(
+    std::unique_ptr<base::TickClock> tick_clock = nullptr);
 
 }  // namespace ui
 
