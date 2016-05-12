f3a33314b0498537f9be4ab92d423a66dd722b46
estade <estade@chromium.org>
2016-05-11 18:32:09 -0700

    [MD] Use vector icon for omnibox "open in pdf" button.
    BUG=505953
    TBR=oshima@chromium.org
    
    Review-Url: https://codereview.chromium.org/1967203003
    Cr-Commit-Position: refs/heads/master@{#393137}

--- chrome/browser/ui/views/location_bar/open_pdf_in_reader_view.cc
+++ chrome/browser/ui/views/location_bar/open_pdf_in_reader_view.cc
@@ -12,13 +12,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/theme_resources.h"
 #include "ui/accessibility/ax_view_state.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/color_utils.h"
+#include "ui/gfx/paint_vector_icon.h"
+#include "ui/gfx/vector_icons_public.h"
+#include "ui/native_theme/native_theme.h"
 #include "ui/views/widget/widget.h"
 
 OpenPDFInReaderView::OpenPDFInReaderView() : bubble_(NULL), model_(NULL) {
   SetFocusBehavior(FocusBehavior::ACCESSIBLE_ONLY);
-  SetImage(ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
-      IDR_OMNIBOX_PDF_ICON));
+  if (!ui::MaterialDesignController::IsModeMaterial()) {
+    SetImage(ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
+        IDR_OMNIBOX_PDF_ICON));
+  }
   SetTooltipText(l10n_util::GetStringUTF16(IDS_PDF_BUBBLE_OPEN_IN_READER_LINK));
 }
 
@@ -77,6 +84,16 @@ bool OpenPDFInReaderView::OnKeyPressed(const ui::KeyEvent& event) {
 
   ShowBubble();
   return true;
+}
+
+void OpenPDFInReaderView::OnNativeThemeChanged(
+    const ui::NativeTheme* native_theme) {
+  if (ui::MaterialDesignController::IsModeMaterial()) {
+    SetImage(gfx::CreateVectorIcon(
+        gfx::VectorIconId::PDF,
+        color_utils::DeriveDefaultIconColor(native_theme->GetSystemColor(
+            ui::NativeTheme::kColorId_TextfieldDefaultColor))));
+  }
 }
 
 void OpenPDFInReaderView::OnWidgetDestroying(views::Widget* widget) {
