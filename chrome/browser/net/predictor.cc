87fbaf3c3ecc7c52ab911a4edd2e99b4995dde2e
csharrison <csharrison@chromium.org>
2016-05-11 18:39:49 -0700

    Effectively disable the predictor's DNS queue for an experiment
    This patch lets the predictor send as many requests as it needs to the
    host resolver. This is a test to see if the intermediate queues are
    really necessary for performance, as they add complexity to the codebase.
    
    BUG=610750
    
    Review-Url: https://codereview.chromium.org/1967663002
    Cr-Commit-Position: refs/heads/master@{#393141}

--- chrome/browser/net/predictor.cc
+++ chrome/browser/net/predictor.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/compiler_specific.h"
 #include "base/containers/mru_cache.h"
+#include "base/feature_list.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/macros.h"
@@ -56,6 +57,11 @@ using base::TimeDelta;
 using content::BrowserThread;
 
 namespace chrome_browser_net {
+
+namespace {
+const base::Feature kUsePredictorDNSQueue{"UsePredictorDNSQueue",
+                                          base::FEATURE_ENABLED_BY_DEFAULT};
+}
 
 // static
 const int Predictor::kPredictorReferrerVersion = 2;
@@ -427,7 +433,8 @@ void Predictor::DiscardAllResults() {
       assignees[url] = *info;
     }
   }
-  DCHECK_LE(assignees.size(), max_concurrent_dns_lookups_);
+  DCHECK(!base::FeatureList::IsEnabled(kUsePredictorDNSQueue) ||
+         assignees.size() <= max_concurrent_dns_lookups_);
   results_.clear();
   // Put back in the names being worked on.
   for (Results::iterator it = assignees.begin(); assignees.end() != it; ++it) {
@@ -1087,6 +1094,7 @@ UrlInfo* Predictor::AppendToResolutionQueue(
 
 bool Predictor::CongestionControlPerformed(UrlInfo* info) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  DCHECK(base::FeatureList::IsEnabled(kUsePredictorDNSQueue));
   // Note: queue_duration is ONLY valid after we go to assigned state.
   if (info->queue_duration() < max_dns_queue_delay_)
     return false;
@@ -1106,14 +1114,18 @@ bool Predictor::CongestionControlPerformed(UrlInfo* info) {
 void Predictor::StartSomeQueuedResolutions() {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
+  // If the queue is disabled, just make LookupRequests for all entries.
+  bool enable_queue = base::FeatureList::IsEnabled(kUsePredictorDNSQueue);
   while (!work_queue_.IsEmpty() &&
-         pending_lookups_.size() < max_concurrent_dns_lookups_) {
+         (!enable_queue ||
+          pending_lookups_.size() < max_concurrent_dns_lookups_)) {
     const GURL url(work_queue_.Pop());
     UrlInfo* info = &results_[url];
     DCHECK(info->HasUrl(url));
     info->SetAssignedState();
 
-    if (CongestionControlPerformed(info)) {
+    // Only perform congestion control if the queue is enabled.
+    if (enable_queue && CongestionControlPerformed(info)) {
       DCHECK(work_queue_.IsEmpty());
       return;
     }
