d9f79b7a9e30328770c5964392f138091132a3ce
jam <jam@chromium.org>
2016-05-11 20:57:55 -0700

    Move content/common/media/media_param_traits to media/base/ipc so that the mojo typemap for these types can be in media/.
    Review-Url: https://codereview.chromium.org/1968343002
    Cr-Commit-Position: refs/heads/master@{#393175}

--- media/base/ipc/media_param_traits.h
+++ media/base/ipc/media_param_traits.h
@@ -1,14 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_COMMON_MEDIA_MEDIA_PARAM_TRAITS_H_
-#define CONTENT_COMMON_MEDIA_MEDIA_PARAM_TRAITS_H_
+#ifndef MEDIA_BASE_IPC_COMMON_MEDIA_PARAM_TRAITS_H_
+#define MEDIA_BASE_IPC_COMMON_MEDIA_PARAM_TRAITS_H_
 
-#include "content/common/content_export.h"
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_param_traits.h"
+#include "media/base/ipc/media_param_traits_macros.h"
 
 namespace media {
 class AudioParameters;
@@ -18,7 +18,7 @@ struct VideoCaptureFormat;
 namespace IPC {
 
 template <>
-struct CONTENT_EXPORT ParamTraits<media::AudioParameters> {
+struct ParamTraits<media::AudioParameters> {
   typedef media::AudioParameters param_type;
   static void GetSize(base::PickleSizer* s, const param_type& p);
   static void Write(base::Pickle* m, const param_type& p);
@@ -29,7 +29,7 @@ struct CONTENT_EXPORT ParamTraits<media::AudioParameters> {
 };
 
 template <>
-struct CONTENT_EXPORT ParamTraits<media::VideoCaptureFormat> {
+struct ParamTraits<media::VideoCaptureFormat> {
   typedef media::VideoCaptureFormat param_type;
   static void GetSize(base::PickleSizer* s, const param_type& p);
   static void Write(base::Pickle* m, const param_type& p);
@@ -39,6 +39,6 @@ struct CONTENT_EXPORT ParamTraits<media::VideoCaptureFormat> {
   static void Log(const param_type& p, std::string* l);
 };
 
-} // namespace IPC
+}  // namespace IPC
 
-#endif  // CONTENT_COMMON_MEDIA_MEDIA_PARAM_TRAITS_H_
+#endif  // MEDIA_BASE_IPC_COMMON_MEDIA_PARAM_TRAITS_H_
---
+++ media/base/ipc/media_param_traits_macros.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_BASE_IPC_COMMON_MEDIA_PARAM_TRAITS_MACROS_H_
+#define MEDIA_BASE_IPC_COMMON_MEDIA_PARAM_TRAITS_MACROS_H_
+
+#include "ipc/ipc_message_macros.h"
+#include "media/base/audio_parameters.h"
+#include "media/base/video_capture_types.h"
+#include "media/base/video_types.h"
+
+IPC_ENUM_TRAITS_MAX_VALUE(media::AudioParameters::Format,
+                          media::AudioParameters::AUDIO_FORMAT_LAST)
+IPC_ENUM_TRAITS_MAX_VALUE(media::ChannelLayout, media::CHANNEL_LAYOUT_MAX)
+IPC_ENUM_TRAITS_MAX_VALUE(media::VideoPixelFormat, media::PIXEL_FORMAT_MAX)
+IPC_ENUM_TRAITS_MAX_VALUE(media::VideoPixelStorage, media::PIXEL_STORAGE_MAX)
+
+#endif  // MEDIA_BASE_IPC_COMMON_MEDIA_PARAM_TRAITS_MACROS_H_
