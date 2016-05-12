02e93f1d38132d0ed4d085a555222192f53214c1
haibinlu <haibinlu@chromium.org>
2016-05-11 18:49:42 -0700

    -- prefixes blimp test data file with the source root. -- adds blimp data deps for embedded test server. -- corrects test data file path for embedded test server.
    crbug.com/611241 tracks setting up new bot for blimp browser tests.
    
    Review-Url: https://codereview.chromium.org/1957403002
    Cr-Commit-Position: refs/heads/master@{#393143}

--- blimp/test/browser_tests/blimp_browser_test.cc
+++ blimp/test/browser_tests/blimp_browser_test.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/ptr_util.h"
 #include "base/message_loop/message_loop.h"
+#include "base/path_service.h"
 #include "base/run_loop.h"
 #include "blimp/client/app/blimp_client_switches.h"
 #include "blimp/client/session/assignment_source.h"
@@ -27,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blimp {
 namespace {
-const char kTestDataFilePath[] = "blimp/engine/test/data";
+const char kTestDataFilePath[] = "blimp/test/data";
 const char kClientTokenFilePath[] = "blimp/test/data/test_client_token";
 }  // namespace
 
@@ -85,8 +86,11 @@ void BlimpBrowserTest::SetUpCommandLine(base::CommandLine* command_line) {
 
   // Use dynamically allocated port for browser test.
   command_line->AppendSwitchASCII(blimp::engine::kEnginePort, "0");
-  command_line->AppendSwitchASCII(blimp::engine::kClientTokenPath,
-                                  kClientTokenFilePath);
+  base::FilePath src_root;
+  PathService::Get(base::DIR_SOURCE_ROOT, &src_root);
+  command_line->AppendSwitchASCII(
+      blimp::engine::kClientTokenPath,
+      src_root.Append(kClientTokenFilePath).value());
 }
 
 void BlimpBrowserTest::RunTestOnMainThreadLoop() {
