9b8c871f802c0e266fb8e5792af1bdd8c9c43abc
mikhail.pozdnyakov <mikhail.pozdnyakov@intel.com>
2016-05-12 03:52:52 -0700

    Add PassRefPtr constructor accepting RefPtr rvalue
    So that extra ref/derefing is avoided when RefPtr rvalue is passed to a function accepting PassRefPtr.
    
    This CL enables https://codereview.chromium.org/1974443002/
    
    BUG=567139
    
    Review-Url: https://codereview.chromium.org/1964093003
    Cr-Commit-Position: refs/heads/master@{#393223}

--- third_party/WebKit/Source/wtf/PassRefPtr.h
+++ third_party/WebKit/Source/wtf/PassRefPtr.h
@@ -73,6 +73,7 @@ public:
     ALWAYS_INLINE ~PassRefPtr() { derefIfNotNull(m_ptr); }
 
     template <typename U> PassRefPtr(const RefPtr<U>&, EnsurePtrConvertibleArgDecl(U, T));
+    template <typename U> PassRefPtr(RefPtr<U>&&, EnsurePtrConvertibleArgDecl(U, T));
 
     T* get() const { return m_ptr; }
 
@@ -109,6 +110,12 @@ template <typename U> inline PassRefPtr<T>::PassRefPtr(const RefPtr<U>& o, Ensur
 {
     T* ptr = m_ptr;
     refIfNotNull(ptr);
+}
+
+template <typename T>
+template <typename U> inline PassRefPtr<T>::PassRefPtr(RefPtr<U>&& o, EnsurePtrConvertibleArgDefn(U, T))
+    : m_ptr(o.release().leakRef())
+{
 }
 
 template <typename T> inline T* PassRefPtr<T>::leakRef() const
