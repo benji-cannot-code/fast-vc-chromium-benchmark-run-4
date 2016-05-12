cbaa72de423e3c2a0db32ae9f1a8e87f61fd34f3
mdjones <mdjones@chromium.org>
2016-05-12 11:41:23 -0700

    Add progress bar test
    Make sure that the progress bar only traverses the screen a single
    time per page navigation.
    
    BUG=573177
    
    Review-Url: https://codereview.chromium.org/1973833002
    Cr-Commit-Position: refs/heads/master@{#393315}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/ToolbarProgressBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/ToolbarProgressBar.java
@@ -72,6 +72,7 @@ public class ToolbarProgressBar extends ClipDrawableProgressBar {
     private boolean mAnimationInitialized;
     private int mMarginTop;
     private ViewGroup mControlContainer;
+    private int mProgressStartCount;
 
     private ToolbarProgressBarAnimatingView mAnimatingView;
 
@@ -202,6 +203,7 @@ public class ToolbarProgressBar extends ClipDrawableProgressBar {
      */
     public void start() {
         mIsStarted = true;
+        mProgressStartCount++;
 
         if (mAnimatingView != null) {
             removeCallbacks(mStartIndeterminate);
@@ -264,6 +266,22 @@ public class ToolbarProgressBar extends ClipDrawableProgressBar {
     @VisibleForTesting
     public void setHidingDelay(long hidngDelayMs) {
         mHidingDelayMs = hidngDelayMs;
+    }
+
+    /**
+     * @return The number of times the progress bar has been triggered.
+     */
+    @VisibleForTesting
+    public int getStartCountForTesting() {
+        return mProgressStartCount;
+    }
+
+    /**
+     * Reset the number of times the progress bar has been triggered.
+     */
+    @VisibleForTesting
+    public void resetStartCountForTesting() {
+        mProgressStartCount = 0;
     }
 
     private void animateAlphaTo(float targetAlpha) {
