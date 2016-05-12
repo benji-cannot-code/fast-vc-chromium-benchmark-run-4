e06cfa45233ff6249b8d42d46b98c60daa1bfcec
vkuzkokov <vkuzkokov@chromium.org>
2016-05-12 00:53:35 -0700

    Do nothing on lid-closed during sign-out.
    After sign-out is finished new instance of chrome
    switches lid-closed-action back to default.
    
    BUG=606105
    
    Review-Url: https://codereview.chromium.org/1954723003
    Cr-Commit-Position: refs/heads/master@{#393203}

--- chrome/browser/lifetime/application_lifetime.cc
+++ chrome/browser/lifetime/application_lifetime.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sys_info.h"
 #include "chrome/browser/chromeos/boot_times_recorder.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
+#include "chromeos/dbus/power_policy_controller.h"
 #include "chromeos/dbus/session_manager_client.h"
 #include "chromeos/dbus/update_engine_client.h"
 #endif
@@ -325,6 +326,9 @@ void NotifyAndTerminate(bool fast_path) {
     NotifyAppTerminating();
 
 #if defined(OS_CHROMEOS)
+  if (chromeos::PowerPolicyController::IsInitialized())
+    chromeos::PowerPolicyController::Get()->NotifyChromeIsExiting();
+
   if (base::SysInfo::IsRunningOnChromeOS()) {
     // If we're on a ChromeOS device, reboot if an update has been applied,
     // or else signal the session manager to log out.
