6bab24e4be7785fdd207ad5ab79085ec6a152aac
mariakhomenko <mariakhomenko@chromium.org>
2016-05-12 13:51:08 -0700

    Add a histogram for how often we intercept navigation.
    Review-Url: https://codereview.chromium.org/1973063002
    Cr-Commit-Position: refs/heads/master@{#393346}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/InterceptNavigationDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/InterceptNavigationDelegateImpl.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tab;
 
 import org.chromium.base.VisibleForTesting;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeApplication;
 import org.chromium.chrome.browser.datausage.DataUseTabUIManager;
@@ -126,6 +127,9 @@ public class InterceptNavigationDelegateImpl implements InterceptNavigationDeleg
                 .build();
         OverrideUrlLoadingResult result = mExternalNavHandler.shouldOverrideUrlLoading(params);
         mLastOverrideUrlLoadingResult = result;
+
+        RecordHistogram.recordEnumeratedHistogram("Android.TabNavigationInterceptResult",
+                result.ordinal(), OverrideUrlLoadingResult.values().length);
         switch (result) {
             case OVERRIDE_WITH_EXTERNAL_INTENT:
                 assert mExternalNavHandler.canExternalAppHandleUrl(url);
