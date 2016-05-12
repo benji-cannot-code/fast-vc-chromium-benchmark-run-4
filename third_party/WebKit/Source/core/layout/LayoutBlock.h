fd5ed09459adc66bb3c7b603a24beeb9561d3b8a
mstensho <mstensho@opera.com>
2016-05-12 09:24:21 -0700

    LayoutBlock::removeChild() override no longer needed.
    It was all about anonymous block merging and collapsing, which is a thing that
    only LayoutBlockFlow needs to do. So move everything there.
    
    BUG=302024
    
    Review-Url: https://codereview.chromium.org/1968403002
    Cr-Commit-Position: refs/heads/master@{#393267}

--- third_party/WebKit/Source/core/layout/LayoutBlock.h
+++ third_party/WebKit/Source/core/layout/LayoutBlock.h
@@ -158,7 +158,6 @@ public:
     virtual void deleteLineBoxTree();
 
     void addChild(LayoutObject* newChild, LayoutObject* beforeChild = nullptr) override;
-    void removeChild(LayoutObject*) override;
 
     virtual void layoutBlock(bool relayoutChildren);
 
