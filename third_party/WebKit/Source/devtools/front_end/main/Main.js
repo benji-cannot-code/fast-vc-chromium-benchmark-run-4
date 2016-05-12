fabd49acd7a45fa8a8f70fdbaa1b5d066ec57990
pfeldman <pfeldman@chromium.org>
2016-05-11 21:31:15 -0700

    DevTools: respect the "!condition" on the configuration and extension levels of the runtime.
    Review-Url: https://codereview.chromium.org/1973813003
    Cr-Commit-Position: refs/heads/master@{#393184}

--- third_party/WebKit/Source/devtools/front_end/main/Main.js
+++ third_party/WebKit/Source/devtools/front_end/main/Main.js
@@ -236,7 +236,9 @@ WebInspector.Main.prototype = {
         app.presentUI(document);
 
         var toggleSearchNodeAction = WebInspector.actionRegistry.action("elements.toggle-element-search");
-        InspectorFrontendHost.events.addEventListener(InspectorFrontendHostAPI.Events.EnterInspectElementMode, toggleSearchNodeAction.execute.bind(toggleSearchNodeAction), this);
+        // TODO: we should not access actions from other modules.
+        if (toggleSearchNodeAction)
+            InspectorFrontendHost.events.addEventListener(InspectorFrontendHostAPI.Events.EnterInspectElementMode, toggleSearchNodeAction.execute.bind(toggleSearchNodeAction), this);
         WebInspector.inspectorView.createToolbars();
         InspectorFrontendHost.loadCompleted();
 
@@ -448,7 +450,8 @@ WebInspector.Main.prototype = {
         section.addKey(advancedSearchShortcut, WebInspector.UIString("Search across all sources"));
 
         var inspectElementModeShortcuts = WebInspector.shortcutRegistry.shortcutDescriptorsForAction("elements.toggle-element-search");
-        section.addKey(inspectElementModeShortcuts[0], WebInspector.UIString("Select node to inspect"));
+        if (inspectElementModeShortcuts.length)
+            section.addKey(inspectElementModeShortcuts[0], WebInspector.UIString("Select node to inspect"));
 
         var openResourceShortcut = WebInspector.KeyboardShortcut.makeDescriptor("p", WebInspector.KeyboardShortcut.Modifiers.CtrlOrMeta);
         section.addKey(openResourceShortcut, WebInspector.UIString("Go to source"));
@@ -885,6 +888,9 @@ WebInspector.Main.MainMenuItem.prototype = {
  */
 WebInspector.NetworkPanelIndicator = function()
 {
+    // TODO: we should not access network from other modules.
+    if (!WebInspector.inspectorView.hasPanel("network"))
+        return;
     var manager = WebInspector.multitargetNetworkManager;
     manager.addEventListener(WebInspector.MultitargetNetworkManager.Events.ConditionsChanged, updateVisibility);
     var blockedURLsSetting = WebInspector.moduleSetting("blockedURLs");
