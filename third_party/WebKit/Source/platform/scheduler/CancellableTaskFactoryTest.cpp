699285f8de7e576ba88a8641937d5532a2779fdc
sigbjornf <sigbjornf@opera.com>
2016-05-12 01:49:05 -0700

    Remove dated NavigationScheduler TODO.
    Task closures created by CancellableTaskFactory keep a weak reference back
    to their owning GCed object, allowing the object to be garbage collected
    without the task keeping it alive.
    
    Retire the TODO in the NavigationScheduler suggesting otherwise along with
    adding a unit test to verify the weakness property.
    
    R=
    BUG=585328
    
    Review-Url: https://codereview.chromium.org/1976433003
    Cr-Commit-Position: refs/heads/master@{#393210}

--- third_party/WebKit/Source/platform/scheduler/CancellableTaskFactoryTest.cpp
+++ third_party/WebKit/Source/platform/scheduler/CancellableTaskFactoryTest.cpp
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/scheduler/CancellableTaskFactory.h"
 
+#include "platform/heap/Handle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace blink {
@@ -175,6 +176,53 @@ TEST_F(CancellableTaskFactoryTest, CreatingANewTaskCancelsPreviousOnes)
 
     taskB->run();
     EXPECT_EQ(1, executionCount);
+}
+
+namespace {
+
+class GCObject final : public GarbageCollectedFinalized<GCObject> {
+public:
+    GCObject()
+        : m_factory(CancellableTaskFactory::create(this, &GCObject::run))
+    {
+    }
+
+    ~GCObject()
+    {
+        s_destructed++;
+    }
+
+    void run()
+    {
+        s_invoked++;
+    }
+
+    DEFINE_INLINE_TRACE() { }
+
+    static int s_destructed;
+    static int s_invoked;
+
+    OwnPtr<CancellableTaskFactory> m_factory;
+};
+
+int GCObject::s_destructed = 0;
+int GCObject::s_invoked = 0;
+
+} // namespace
+
+TEST(CancellableTaskFactoryTest, GarbageCollectedWeak)
+{
+    GCObject* object = new GCObject();
+    OwnPtr<WebTaskRunner::Task> task = adoptPtr(object->m_factory->cancelAndCreate());
+    object = nullptr;
+    ThreadHeap::collectAllGarbage();
+    task->run();
+    // The owning object will have been GCed and the task will have
+    // lost its weak reference. Verify that it wasn't invoked.
+    EXPECT_EQ(0, GCObject::s_invoked);
+
+    // ..and just to make sure |object| was indeed destructed.
+    EXPECT_EQ(1, GCObject::s_destructed);
 }
 
 } // namespace blink
