8bde85884bd65d05e57d04d76d35d8d98c42d6b1
juliatuttle <juliatuttle@chromium.org>
2016-05-12 07:47:22 -0700

    DNS: Retain stale entries in HostCache and return when requested
    In preparation for future connection optimizations, retain stale
    entries (those with a response past the TTL or received on a different
    network) in the cache, and expose a way to look up and characterize the
    stale results.
    
    To do this, HostCache moves away from using ExpiringCache to a self-
    managed map of keys to entries, with extra metadata.
    
    Also, add a bunch of histograms measuring how stale updated entries are.
    
    BUG=605138
    
    Review-Url: https://codereview.chromium.org/1908543002
    Cr-Commit-Position: refs/heads/master@{#393246}

--- net/dns/host_resolver_mojo.cc
+++ net/dns/host_resolver_mojo.cc
@@ -120,8 +120,8 @@ int HostResolverMojo::ResolveFromCacheInternal(const RequestInfo& info,
   if (!entry)
     return ERR_DNS_CACHE_MISS;
 
-  *addresses = AddressList::CopyWithPort(entry->addrlist, info.port());
-  return entry->error;
+  *addresses = AddressList::CopyWithPort(entry->addresses(), info.port());
+  return entry->error();
 }
 
 HostResolverMojo::Job::Job(
