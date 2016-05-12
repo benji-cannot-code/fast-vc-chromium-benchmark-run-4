08d7a66cd87bb2e16630bd79914b6808d27e5222
sammc <sammc@chromium.org>
2016-05-12 00:54:05 -0700

    Run queued tasks before shutting down after running a layout test.
    Currently, when a layout test run finishes, the BlinkTestController
    falls out of scope, which triggers shutdown of the RenderProcessHostImpl
    used by the test. However, RenderProcessHostImpl posts a task to delete
    itself, which is currently never run; this results in a
    RenderProcessHostImpl outliving the UI MessageLoop and its
    BrowserContext, inviting use-after-frees. This CL fixes this issue by
    running queued tasks before shutting down the main runner.
    
    BUG=610989
    
    Review-Url: https://codereview.chromium.org/1975593003
    Cr-Commit-Position: refs/heads/master@{#393204}

--- content/shell/browser/layout_test/layout_test_browser_main.cc
+++ content/shell/browser/layout_test/layout_test_browser_main.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/location.h"
 #include "base/logging.h"
+#include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
@@ -143,6 +144,7 @@ int LayoutTestBrowserMain(
   }
 
   exit_code = RunTests(main_runner);
+  base::RunLoop().RunUntilIdle();
 
 #if !defined(OS_ANDROID)
   main_runner->Shutdown();
