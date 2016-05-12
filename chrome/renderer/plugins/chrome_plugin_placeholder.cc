58a5e88ccfec11cdcae948587b28c9bffc8ca9c9
groby <groby@chromium.org>
2016-05-11 20:24:25 -0700

    [PPS] Improve tiny content handling.
    BUG=608886
    
    Review-Url: https://codereview.chromium.org/1936283002
    Cr-Commit-Position: refs/heads/master@{#393168}

--- chrome/renderer/plugins/chrome_plugin_placeholder.cc
+++ chrome/renderer/plugins/chrome_plugin_placeholder.cc
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/prerender_messages.h"
 #include "chrome/common/render_messages.h"
 #include "chrome/grit/generated_resources.h"
@@ -66,7 +68,8 @@ ChromePluginPlaceholder::ChromePluginPlaceholder(
       placeholder_routing_id_(MSG_ROUTING_NONE),
 #endif
       has_host_(false),
-      context_menu_request_id_(0) {
+      context_menu_request_id_(0),
+      ignore_updates_(false) {
   RenderThread::Get()->AddObserver(this);
 }
 
@@ -84,6 +87,11 @@ ChromePluginPlaceholder::~ChromePluginPlaceholder() {
         routing_id(), placeholder_routing_id_));
   }
 #endif
+}
+
+// static
+bool ChromePluginPlaceholder::IsSmallContentFilterEnabled() {
+  return base::FeatureList::IsEnabled(features::kBlockSmallContent);
 }
 
 // static
@@ -164,6 +172,39 @@ ChromePluginPlaceholder* ChromePluginPlaceholder::CreateBlockedPlugin(
       power_saver_info.blocked_for_background_tab);
 
   return blocked_plugin;
+}
+
+// static
+ChromePluginPlaceholder* ChromePluginPlaceholder::CreateDelayedPlugin(
+    content::RenderFrame* render_frame,
+    blink::WebLocalFrame* frame,
+    const blink::WebPluginParams& params,
+    const content::WebPluginInfo& info,
+    const std::string& identifier,
+    const base::string16& name,
+    const PowerSaverInfo& power_saver_info) {
+  // Can never be called with an actual poster.
+  DCHECK(power_saver_info.poster_attribute.empty());
+
+  // Can never be called when blocked for background tab.
+  DCHECK(!power_saver_info.blocked_for_background_tab);
+
+  const base::StringPiece template_html(
+      ResourceBundle::GetSharedInstance().GetRawDataResource(
+          IDR_PLUGIN_DELAY_HTML));
+  base::DictionaryValue values;
+  std::string html_data = webui::GetI18nTemplateHtml(template_html, &values);
+
+  // |delay_plugin| will destroy itself when its WebViewPlugin is going away.
+  ChromePluginPlaceholder* delay_plugin =
+      new ChromePluginPlaceholder(render_frame, frame, params, html_data, name);
+
+  delay_plugin->set_delayed(true);
+  delay_plugin->SetPluginInfo(info);
+  delay_plugin->SetIdentifier(identifier);
+  delay_plugin->set_power_saver_enabled(power_saver_info.power_saver_enabled);
+
+  return delay_plugin;
 }
 
 void ChromePluginPlaceholder::SetStatus(
@@ -369,6 +410,31 @@ blink::WebPlugin* ChromePluginPlaceholder::CreatePlugin() {
   }
   return render_frame()->CreatePlugin(GetFrame(), GetPluginInfo(),
                                       GetPluginParams(), std::move(throttler));
+}
+
+void ChromePluginPlaceholder::OnLoadedRectUpdate(
+    const gfx::Rect& unobscured_rect,
+    content::RenderFrame::PeripheralContentStatus status) {
+  // If the placeholder is in the blocked state, do nothing.
+  if (ignore_updates_)
+    return;
+
+  // This should only be called once.
+  set_delayed(false);
+
+  // block tiny cross-origin - simply by not continuing the load chain.
+  if (status ==
+      content::RenderFrame::CONTENT_STATUS_ESSENTIAL_CROSS_ORIGIN_TINY) {
+    ignore_updates_ = true;
+    return;
+  }
+
+  // For essential content, powersaver can be turned off.
+  if (status != content::RenderFrame::CONTENT_STATUS_PERIPHERAL)
+    set_power_saver_enabled(false);
+
+  AllowLoading();
+  LoadPlugin();
 }
 
 gin::ObjectTemplateBuilder ChromePluginPlaceholder::GetObjectTemplateBuilder(
--- components/plugins/renderer/loadable_plugin_placeholder.h
+++ components/plugins/renderer/loadable_plugin_placeholder.h
@@ -6,12 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PLUGINS_RENDERER_LOADABLE_PLUGIN_PLACEHOLDER_H_
 #define COMPONENTS_PLUGINS_RENDERER_LOADABLE_PLUGIN_PLACEHOLDER_H_
 
+#include <memory>
+
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
 #include "components/plugins/renderer/plugin_placeholder.h"
 #include "content/public/common/webplugininfo.h"
 #include "content/public/renderer/plugin_instance_throttler.h"
+#include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_thread.h"
 
 namespace plugins {
@@ -19,6 +22,8 @@ namespace plugins {
 // (blocked or disabled).
 class LoadablePluginPlaceholder : public PluginPlaceholderBase {
  public:
+  void set_delayed(bool delayed) { is_delayed_placeholder_ = delayed; }
+
   void set_blocked_for_background_tab(bool blocked_for_background_tab) {
     is_blocked_for_background_tab_ = blocked_for_background_tab;
   }
@@ -48,6 +53,8 @@ class LoadablePluginPlaceholder : public PluginPlaceholderBase {
                             const std::string& html_data);
 
   ~LoadablePluginPlaceholder() override;
+
+  void SetDelegate(std::unique_ptr<Delegate> delegate);
 
   void MarkPluginEssential(
       content::PluginInstanceThrottler::PowerSaverUnthrottleMethod method);
@@ -93,9 +100,18 @@ class LoadablePluginPlaceholder : public PluginPlaceholderBase {
   // Plugin creation is embedder-specific.
   virtual blink::WebPlugin* CreatePlugin() = 0;
 
+  // Handling of delayed behavior is embedder-specific.
+  virtual void OnLoadedRectUpdate(
+      const gfx::Rect& unobscured_rect,
+      content::RenderFrame::PeripheralContentStatus status) = 0;
+
   content::WebPluginInfo plugin_info_;
 
   base::string16 message_;
+
+  // True if this is a "delayed" placeholder - one that will make a decision
+  // on placeholder vs. plugin behavior once initial size has been determined.
+  bool is_delayed_placeholder_;
 
   // True if the plugin load was deferred due to page being a background tab.
   // Plugin may be automatically loaded when the page is foregrounded.
@@ -124,6 +140,8 @@ class LoadablePluginPlaceholder : public PluginPlaceholderBase {
 
   // True if the power saver heuristic has already been run on this content.
   bool heuristic_run_before_;
+
+  std::unique_ptr<Delegate> delegate_;
 
   base::WeakPtrFactory<LoadablePluginPlaceholder> weak_factory_;
 
