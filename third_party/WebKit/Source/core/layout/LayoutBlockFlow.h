261a0a1a3f20efeace994e0c8fb79f2060e90007
ymalik <ymalik@chromium.org>
2016-05-12 14:35:40 -0700

    Descend into node with overflow and floating content
    BUG=609571
    
    Review-Url: https://codereview.chromium.org/1974953002
    Cr-Commit-Position: refs/heads/master@{#393361}

--- third_party/WebKit/Source/core/layout/LayoutBlockFlow.h
+++ third_party/WebKit/Source/core/layout/LayoutBlockFlow.h
@@ -295,6 +295,8 @@ public:
 
     PositionWithAffinity positionForPoint(const LayoutPoint&) override;
 
+    LayoutUnit lowestFloatLogicalBottom(FloatingObject::Type = FloatingObject::FloatLeftRight) const;
+
 #ifndef NDEBUG
     void showLineTreeAndMark(const InlineBox* = nullptr, const char* = nullptr, const InlineBox* = nullptr, const char* = nullptr, const LayoutObject* = nullptr) const;
 #endif
@@ -370,8 +372,6 @@ private:
     void addIntrudingFloats(LayoutBlockFlow* prev, LayoutUnit xoffset, LayoutUnit yoffset);
     void addOverhangingFloats(LayoutBlockFlow* child, bool makeChildPaintOtherFloats);
     bool isOverhangingFloat(const FloatingObject& floatObject) const { return logicalBottomForFloat(floatObject) > logicalHeight(); }
-
-    LayoutUnit lowestFloatLogicalBottom(FloatingObject::Type = FloatingObject::FloatLeftRight) const;
 
     bool hitTestFloats(HitTestResult&, const HitTestLocation& locationInContainer, const LayoutPoint& accumulatedOffset);
 
