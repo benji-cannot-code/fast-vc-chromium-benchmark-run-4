13985961b275e2c7936d51ffdbbe044c3b3aa2ef
hiroshige <hiroshige@chromium.org>
2016-05-13 13:46:38 -0700

    Encapsulate |MemoryCacheEntry::m_resource| and refactor pruneDead/LiveResources
    This CL
    - Make |MemoryCacheEntry::m_resource| private and always use MemoryCacheEntry::resource(),
    - Removes somes ASSERT() because Issue 594644 is now fixed, and
    - Continue pruning when we encounter entries evicted during destroyDecodedData().
    
    BUG=603462, 594644
    
    Review-Url: https://codereview.chromium.org/1918093003
    Cr-Commit-Position: refs/heads/master@{#393632}

--- third_party/WebKit/Source/core/fetch/MemoryCache.h
+++ third_party/WebKit/Source/core/fetch/MemoryCache.h
@@ -75,8 +75,8 @@ public:
     }
     DECLARE_TRACE();
     void dispose();
+    Resource* resource();
 
-    Member<Resource> m_resource;
     bool m_inLiveDecodedResourcesList;
     unsigned m_accessCount;
     double m_lastDecodedAccessTime; // Used as a thrash guard
@@ -88,16 +88,18 @@ public:
 
 private:
     explicit MemoryCacheEntry(Resource* resource)
-        : m_resource(resource)
-        , m_inLiveDecodedResourcesList(false)
+        : m_inLiveDecodedResourcesList(false)
         , m_accessCount(0)
         , m_lastDecodedAccessTime(0.0)
         , m_previousInLiveResourcesList(nullptr)
         , m_nextInLiveResourcesList(nullptr)
         , m_previousInAllResourcesList(nullptr)
         , m_nextInAllResourcesList(nullptr)
+        , m_resource(resource)
     {
     }
+
+    Member<Resource> m_resource;
 };
 
 WILL_NOT_BE_EAGERLY_TRACED_CLASS(MemoryCacheEntry);
