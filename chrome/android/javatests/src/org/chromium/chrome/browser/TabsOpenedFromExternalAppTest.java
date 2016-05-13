b65474193d345d0a5342494366a91f9fe3dd4e87
Vasilii Sukhanov <vasilii@chromium.org>
2016-05-13 13:42:34 +0200

    Disable two TabsOpenedFromExternalAppTest flaky tests.
    BUG=611701
    TBR=skyostil@chromium.org
    
    Review URL: https://codereview.chromium.org/1973763004 .
    
    Cr-Commit-Position: refs/heads/master@{#393501}

--- chrome/android/javatests/src/org/chromium/chrome/browser/TabsOpenedFromExternalAppTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/TabsOpenedFromExternalAppTest.java
@@ -21,6 +21,7 @@ import junit.framework.Assert;
 import org.chromium.base.BaseSwitches;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.R;
@@ -254,8 +255,9 @@ public class TabsOpenedFromExternalAppTest extends ChromeTabbedActivityTestBase 
      * Tests that URLs opened from the same external app don't create new tabs.
      * @throws InterruptedException
      */
-    @LargeTest
-    @Feature({"Navigation"})
+    // @LargeTest
+    // @Feature({"Navigation"})
+    @DisabledTest
     public void testNoNewTabForSameApp() throws InterruptedException {
         startMainActivityFromLauncher();
 
@@ -350,8 +352,9 @@ public class TabsOpenedFromExternalAppTest extends ChromeTabbedActivityTestBase 
      * the intent do create new tabs.
      * @throws InterruptedException
      */
-    @LargeTest
-    @Feature({"Navigation"})
+    // @LargeTest
+    // @Feature({"Navigation"})
+    @DisabledTest
     public void testNewTabWithNewTabExtra() throws InterruptedException {
         startMainActivityFromLauncher();
 
