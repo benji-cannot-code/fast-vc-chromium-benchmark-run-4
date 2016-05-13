fced85d1311f7d6fa21afec5377d8e3c4995cf50
juncai <juncai@chromium.org>
2016-05-13 08:35:29 -0700

    Clean up chooser_bubble_controller.h include files
    This patch removed unnecessary include file from chooser_bubble_controller.h
    and added necessary include files for usb_chooser_bubble_controller.h and
    bluetooth_chooser_bubble_controller.h
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/1978733002
    Cr-Commit-Position: refs/heads/master@{#393529}

--- chrome/browser/ui/bluetooth/bluetooth_chooser_bubble_controller.h
+++ chrome/browser/ui/bluetooth/bluetooth_chooser_bubble_controller.h
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include <string>
+#include <utility>
+#include <vector>
+
 #include "base/macros.h"
 #include "chrome/browser/ui/website_settings/chooser_bubble_controller.h"
 #include "components/bubble/bubble_reference.h"
