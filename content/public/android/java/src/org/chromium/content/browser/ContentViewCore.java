c23965536a0cc20a013d8c6b2e5696e8b96426e0
donnd <donnd@chromium.org>
2016-05-13 10:17:34 -0700

    Record user actions for long-press follow-on actions.
    This will be helpful for Contextual Search, and probably useful overall.
    Now all user actions in the Action Bar that follow a select action should record a user action on Android.
    This includes Share, Web Search, etc.  WebContents already records user actions for edits to text.
    
    BUG=606091
    
    Review-Url: https://codereview.chromium.org/1965013004
    Cr-Commit-Position: refs/heads/master@{#393561}

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -55,6 +55,7 @@ import org.chromium.base.TraceEvent;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.content.R;
 import org.chromium.content.browser.ScreenOrientationListener.ScreenOrientationObserver;
 import org.chromium.content.browser.accessibility.BrowserAccessibilityManager;
@@ -2013,9 +2014,17 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Screen
                 /** Google search doesn't support requests slightly larger than this. */
                 private static final int MAX_SEARCH_QUERY_LENGTH = 1000;
 
+                // All WebContents actions record a user action internally.
                 @Override
                 public void selectAll() {
                     mWebContents.selectAll();
+                    // Even though the above statement logged a SelectAll user action, we want to
+                    // track whether the focus was in an editable field, so log that too.
+                    if (isFocusedNodeEditable()) {
+                        RecordUserAction.record("MobileActionMode.SelectAllWasEditable");
+                    } else {
+                        RecordUserAction.record("MobileActionMode.SelectAllWasNonEditable");
+                    }
                 }
 
                 @Override
@@ -2035,6 +2044,7 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Screen
 
                 @Override
                 public void share() {
+                    RecordUserAction.record("MobileActionMode.Share");
                     final String query = sanitizeQuery(getSelectedText(), MAX_SHARE_QUERY_LENGTH);
                     if (TextUtils.isEmpty(query)) return;
 
@@ -2053,6 +2063,7 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Screen
 
                 @Override
                 public void processText(Intent intent) {
+                    RecordUserAction.record("MobileActionMode.ProcessTextIntent");
                     assert Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
 
                     final String query = sanitizeQuery(getSelectedText(), MAX_SEARCH_QUERY_LENGTH);
@@ -2080,6 +2091,7 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Screen
 
                 @Override
                 public void search() {
+                    RecordUserAction.record("MobileActionMode.WebSearch");
                     final String query = sanitizeQuery(getSelectedText(), MAX_SEARCH_QUERY_LENGTH);
                     if (TextUtils.isEmpty(query)) return;
 
--- ui/touch_selection/touch_selection_controller.cc
+++ ui/touch_selection/touch_selection_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/metrics/user_metrics.h"
 
 namespace ui {
 namespace {
@@ -385,6 +386,9 @@ void TouchSelectionController::OnDragBegin(
   if (anchor_drag_to_selection_start_)
     std::swap(base, extent);
 
+  // If this is the first drag, log an action to allow user action sequencing.
+  if (!selection_handle_dragged_)
+    base::RecordAction(base::UserMetricsAction("SelectionChanged"));
   selection_handle_dragged_ = true;
 
   // When moving the handle we want to move only the extent point. Before doing
