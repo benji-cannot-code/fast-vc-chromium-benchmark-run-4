16a93071f40d8d54bf4cc8bbcf14adae3160a587
penghuang <penghuang@chromium.org>
2016-05-13 08:37:46 -0700

    Add mojom files for GpuMemoryBufferHandle and GpuService
    This CL also implements type converters for GpuMemoryBufferHandle.
    
    This CL is for using Chrome IPC version GPU Command Buffer in
    Mus.
    
    BUG=586390
    
    Review-Url: https://codereview.chromium.org/1956743002
    Cr-Commit-Position: refs/heads/master@{#393530}

--- components/mus/gpu/gpu_type_converters.h
+++ components/mus/gpu/gpu_type_converters.h
@@ -6,8 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_MUS_GPU_GPU_TYPE_CONVERTERS_H_
 #define COMPONENTS_MUS_GPU_GPU_TYPE_CONVERTERS_H_
 
+#include "build/build_config.h"
 #include "components/mus/public/interfaces/channel_handle.mojom.h"
+#include "components/mus/public/interfaces/gpu_memory_buffer.mojom.h"
 #include "mojo/public/cpp/bindings/type_converter.h"
+
+namespace gfx {
+struct GpuMemoryBufferHandle;
+class GenericSharedMemoryId;
+using GpuMemoryBufferId = GenericSharedMemoryId;
+struct NativePixmapHandle;
+}
 
 namespace IPC {
 struct ChannelHandle;
@@ -23,6 +32,48 @@ struct TypeConverter<mus::mojom::ChannelHandlePtr, IPC::ChannelHandle> {
 template <>
 struct TypeConverter<IPC::ChannelHandle, mus::mojom::ChannelHandlePtr> {
   static IPC::ChannelHandle Convert(const mus::mojom::ChannelHandlePtr& handle);
+};
+
+#if defined(USE_OZONE)
+template <>
+struct TypeConverter<mus::mojom::NativePixmapHandlePtr,
+                     gfx::NativePixmapHandle> {
+  static mus::mojom::NativePixmapHandlePtr Convert(
+      const gfx::NativePixmapHandle& handle);
+};
+
+template <>
+struct TypeConverter<gfx::NativePixmapHandle,
+                     mus::mojom::NativePixmapHandlePtr> {
+  static gfx::NativePixmapHandle Convert(
+      const mus::mojom::NativePixmapHandlePtr& handle);
+};
+#endif
+
+template <>
+struct TypeConverter<mus::mojom::GpuMemoryBufferIdPtr, gfx::GpuMemoryBufferId> {
+  static mus::mojom::GpuMemoryBufferIdPtr Convert(
+      const gfx::GpuMemoryBufferId& id);
+};
+
+template <>
+struct TypeConverter<gfx::GpuMemoryBufferId, mus::mojom::GpuMemoryBufferIdPtr> {
+  static gfx::GpuMemoryBufferId Convert(
+      const mus::mojom::GpuMemoryBufferIdPtr& id);
+};
+
+template <>
+struct TypeConverter<mus::mojom::GpuMemoryBufferHandlePtr,
+                     gfx::GpuMemoryBufferHandle> {
+  static mus::mojom::GpuMemoryBufferHandlePtr Convert(
+      const gfx::GpuMemoryBufferHandle& handle);
+};
+
+template <>
+struct TypeConverter<gfx::GpuMemoryBufferHandle,
+                     mus::mojom::GpuMemoryBufferHandlePtr> {
+  static gfx::GpuMemoryBufferHandle Convert(
+      const mus::mojom::GpuMemoryBufferHandlePtr& handle);
 };
 
 }  // namespace mojo
