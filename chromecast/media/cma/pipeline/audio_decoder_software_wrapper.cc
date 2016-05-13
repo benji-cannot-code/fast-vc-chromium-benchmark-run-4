7aa446e1c7df0c89816b906197676e46a0326d8a
yucliu <yucliu@chromium.org>
2016-05-13 11:18:02 -0700

    [Chromecast] Always pass unencrypted AudioConfig to CastAudioDecoder
    None of the audio decoders support encrypted data. Report DecodeError if
    the pushed data is encrypted later when decoding.
    
    This will allow media pipeline decrypt the data before pushing to
    backend on audio pipeline.
    
    BUG=internal b/27883024
    TEST=play encrypted stream without failure
    
    Review-Url: https://codereview.chromium.org/1972043003
    Cr-Commit-Position: refs/heads/master@{#393586}

--- chromecast/media/cma/pipeline/audio_decoder_software_wrapper.cc
+++ chromecast/media/cma/pipeline/audio_decoder_software_wrapper.cc
@@ -60,7 +60,7 @@ bool AudioDecoderSoftwareWrapper::SetConfig(const AudioConfig& config) {
     return true;
   }
 
-  if (config.is_encrypted() || !CreateSoftwareDecoder(config))
+  if (!CreateSoftwareDecoder(config))
     return false;
 
   output_config_.codec = media::kCodecPCM;
