4dcf6353a919dc8edde627b79e5276c217f23fe7
rjwright <rjwright@chromium.org>
2016-05-12 21:15:16 -0700

    Typed CSSOM: Rename Rotation and RotationTransformComponent to CSSRotation
    Changes in line with changes to the spec: https://drafts.css-houdini.org/css-typed-om-1/#cssrotation
    
    Tracking bug: https://bugs.chromium.org/p/chromium/issues/detail?id=610159
    
    BUG=610169
    
    Review-Url: https://codereview.chromium.org/1976493002
    Cr-Commit-Position: refs/heads/master@{#393445}

--- third_party/WebKit/Source/core/css/cssom/CSSRotation.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSRotation.cpp
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "core/css/cssom/RotationTransformComponent.h"
+#include "core/css/cssom/CSSRotation.h"
 
 #include "core/css/CSSPrimitiveValue.h"
 #include "core/css/CSSValuePool.h"
 
 namespace blink {
 
-CSSFunctionValue* RotationTransformComponent::toCSSValue() const
+CSSFunctionValue* CSSRotation::toCSSValue() const
 {
     CSSFunctionValue* result = CSSFunctionValue::create(m_is2D ? CSSValueRotate : CSSValueRotate3d);
     if (!m_is2D) {
