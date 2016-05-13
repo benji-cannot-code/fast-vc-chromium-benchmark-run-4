32c4e2e4ad8b6c0a99a042ebe5914236f2c3b35f
samuong <samuong@chromium.org>
2016-05-12 21:38:07 -0700

    [chromedriver] Download all (not just HEAD) linux32 builds from chromium-linux-archive.
    BUG=
    
    Review-Url: https://codereview.chromium.org/1968423004
    Cr-Commit-Position: refs/heads/master@{#393447}

--- chrome/test/chromedriver/util.py
+++ chrome/test/chromedriver/util.py
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import atexit
 import httplib
 import os
+import platform
 import signal
 import stat
 import subprocess
@@ -38,6 +39,10 @@ def IsLinux():
 
 def IsMac():
   return sys.platform.startswith('darwin')
+
+
+def Is64Bit():
+  return platform.architecture()[0] == '64bit'
 
 
 def GetAbsolutePathOfUserPath(user_path):
