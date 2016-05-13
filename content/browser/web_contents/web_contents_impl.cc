aec6ce1e7bfbbc1d75599a99e98b527cef57c10e
wjmaclean <wjmaclean@chromium.org>
2016-05-13 16:34:44 -0700

    Audio notification plumbed from guest WebContents up to embedder.
    When a guest webcontents is playing audio, there is no way for an
    entity interacting with the embedder webcontents to determine this.
    This CL adds two things:
    i) The ability for WebContentsImpl::WasRecentlyAudible() to query any
    guest/child contents to determine if they were recently audible, and
    ii) the ability for a guest webcontents to pass any navigation state
    changes up to the embedder in order to make them visible at the top
    level.
    
    BUG=590656
    
    Review-Url: https://codereview.chromium.org/1970313002
    Cr-Commit-Position: refs/heads/master@{#393689}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -1291,6 +1291,9 @@ void WebContentsImpl::NotifyNavigationStateChanged(
 
   if (delegate_)
     delegate_->NavigationStateChanged(this, changed_flags);
+
+  if (GetOuterWebContents())
+    GetOuterWebContents()->NotifyNavigationStateChanged(changed_flags);
 }
 
 base::TimeTicks WebContentsImpl::GetLastActiveTime() const {
@@ -3045,7 +3048,9 @@ void WebContentsImpl::InsertCSS(const std::string& css) {
 }
 
 bool WebContentsImpl::WasRecentlyAudible() {
-  return audio_stream_monitor_.WasRecentlyAudible();
+  return audio_stream_monitor_.WasRecentlyAudible() ||
+         (browser_plugin_embedder_ &&
+          browser_plugin_embedder_->WereAnyGuestsRecentlyAudible());
 }
 
 void WebContentsImpl::GetManifest(const GetManifestCallback& callback) {
