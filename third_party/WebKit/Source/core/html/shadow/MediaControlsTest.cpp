02d5a6e9e97cde888891786b0d8a9d527bc8f538
liberato <liberato@chromium.org>
2016-05-13 10:08:08 -0700

    Enable material design media playback UI on all platforms.
    This CL switches the default chrome media player UI to be the
    material design UI.  It is enabled as the default in the finch
    experiment "NewMediaPlaybackUi", so that it can be disabled via
    finch if needed.
    
    This CL also makes some small test changes to make them compatible
    with the new UI, and rebaselines quite a few layout tests.
    
    BUG=487344
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/1811003002
    Cr-Commit-Position: refs/heads/master@{#393557}

--- third_party/WebKit/Source/core/html/shadow/MediaControlsTest.cpp
+++ third_party/WebKit/Source/core/html/shadow/MediaControlsTest.cpp
@@ -73,6 +73,13 @@ protected:
         m_mediaControls->mediaElement().remoteRouteAvailabilityChanged(true);
     }
 
+    void ensureLayout()
+    {
+        // Force a relayout, so that the controls know the width.  Otherwise,
+        // they don't know if, for example, the cast button will fit.
+        m_mediaControls->mediaElement().clientWidth();
+    }
+
     MediaControls& mediaControls() { return *m_mediaControls; }
     Document& document() { return m_pageHolder->document(); }
 
@@ -134,6 +141,7 @@ TEST_F(MediaControlsTest, ResetDoesNotTriggerInitialLayout)
 
 TEST_F(MediaControlsTest, CastButtonRequiresRoute)
 {
+    ensureLayout();
     mediaControls().mediaElement().setBooleanAttribute(HTMLNames::controlsAttr, true);
 
     Element* castButton = getElementByShadowPseudoId(mediaControls(), "-internal-media-controls-cast-button");
@@ -147,6 +155,7 @@ TEST_F(MediaControlsTest, CastButtonRequiresRoute)
 
 TEST_F(MediaControlsTest, CastButtonDisableRemotePlaybackAttr)
 {
+    ensureLayout();
     mediaControls().mediaElement().setBooleanAttribute(HTMLNames::controlsAttr, true);
 
     Element* castButton = getElementByShadowPseudoId(mediaControls(), "-internal-media-controls-cast-button");
