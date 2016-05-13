58fb118e6eae6c7f7282f20c25a7d9a3d5a686ec
harkness <harkness@chromium.org>
2016-05-13 09:15:41 -0700

    This code replaces the existing 1 in 10 grace period with an accumulating budget based on the site engagemene score for the origin.
    This also allowed extensive simplification in the
    PushMessagingNotificationManager, since we no longer need to update the
    stored information when a notification is displayed.
    
    This is the second part of a many-part project to add a framework for
    service workers to perform background processing on behalf of the user
    in response to a push message but without always showing a visible
    notification to the user.
    
    BUG=602244
    
    Review-Url: https://codereview.chromium.org/1887623002
    Cr-Commit-Position: refs/heads/master@{#393538}

--- chrome/browser/push_messaging/background_budget_service_factory.cc
+++ chrome/browser/push_messaging/background_budget_service_factory.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/push_messaging/background_budget_service_factory.h"
 
+#include "base/memory/ptr_util.h"
+#include "base/time/default_clock.h"
 #include "chrome/browser/engagement/site_engagement_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/push_messaging/background_budget_service.h"
