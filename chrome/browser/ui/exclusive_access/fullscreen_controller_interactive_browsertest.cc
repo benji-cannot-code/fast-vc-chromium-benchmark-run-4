fc4c4ed0a16eaaad1beaecfe851d095f92a38832
mgiuca <mgiuca@chromium.org>
2016-05-12 18:56:27 -0700

    Fullscreen / mouselock: Always auto-accept (even if flag disabled).
    This is the start of removing the old behaviour (when the
    simplified-fullscreen-ui flag disabled). Has no effect when the flag is
    enabled (the default).
    
    Lots of code can now be deleted, but saving that for a follow-up.
    
    BUG=610900
    
    Review-Url: https://codereview.chromium.org/1964383002
    Cr-Commit-Position: refs/heads/master@{#393427}

--- chrome/browser/ui/exclusive_access/fullscreen_controller_interactive_browsertest.cc
+++ chrome/browser/ui/exclusive_access/fullscreen_controller_interactive_browsertest.cc
@@ -104,9 +104,6 @@ class ParamaterizedFullscreenControllerInteractiveTest
     }
   }
 
-  // Whether the test should be prompting the user (i.e. non-simplified UI).
-  bool ShouldPrompt() { return GetParam() == PROMPTING; }
-
  private:
   DISALLOW_COPY_AND_ASSIGN(ParamaterizedFullscreenControllerInteractiveTest);
 };
@@ -729,13 +726,6 @@ IN_PROC_BROWSER_TEST_P(ParamaterizedFullscreenControllerInteractiveTest,
       chrome::NOTIFICATION_MOUSE_LOCK_CHANGED,
       content::NotificationService::AllSources()));
   ASSERT_TRUE(IsFullscreenBubbleDisplayed());
-  if (ShouldPrompt()) {
-    ASSERT_TRUE(IsMouseLockPermissionRequested());
-    ASSERT_FALSE(IsMouseLocked());
-
-    // Accept mouse lock.
-    AcceptCurrentFullscreenOrMouseLockRequest();
-  }
   ASSERT_TRUE(IsMouseLocked());
   ASSERT_TRUE(IsFullscreenBubbleDisplayed());
 
