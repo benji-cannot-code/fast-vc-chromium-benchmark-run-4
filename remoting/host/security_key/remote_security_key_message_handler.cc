a568c6b10d19f19bdc4965cde7c0f8a88f509588
joedow <joedow@chromium.org>
2016-05-12 19:36:47 -0700

    Fixing some bugs related to the remote_security_key tool
    I have found a couple of bugs which need to be addressed for this tool to work
    correctly with security key enabled tools.
    
    Now that all of the SK classes have been checked in, I need to enable the
    security key host extension.  I am using a Windows conditional now but I plan
    to remove it once we finish the mac work.
    
    The second is a cleanup change.  I was returning the char value of 0 and 1
    instead of numeric values which does not feel right when consuming the
    connect events.
    
    So far these are the only problems I have found while testing so I would like
    to get these changes in before the next release snap occurs.
    
    BUG=584463
    
    Review-Url: https://codereview.chromium.org/1978653002
    Cr-Commit-Position: refs/heads/master@{#393436}

--- remoting/host/security_key/remote_security_key_message_handler.cc
+++ remoting/host/security_key/remote_security_key_message_handler.cc
@@ -88,7 +88,8 @@ void RemoteSecurityKeyMessageHandler::HandleIpcConnectionChange(
     bool connection_established) {
   DCHECK(thread_checker_.CalledOnValidThread());
   if (connection_established) {
-    SendMessageWithPayload(RemoteSecurityKeyMessageType::CONNECT_RESPONSE, "1");
+    SendMessageWithPayload(RemoteSecurityKeyMessageType::CONNECT_RESPONSE,
+                           std::string(1, kConnectResponseActiveSession));
   } else {
     SendMessageWithPayload(
         RemoteSecurityKeyMessageType::CONNECT_ERROR,
@@ -131,7 +132,8 @@ void RemoteSecurityKeyMessageHandler::HandleConnectRequest(
         base::Bind(&RemoteSecurityKeyMessageHandler::HandleIpcConnectionChange,
                    base::Unretained(this), false));
   } else {
-    SendMessageWithPayload(RemoteSecurityKeyMessageType::CONNECT_RESPONSE, "0");
+    SendMessageWithPayload(RemoteSecurityKeyMessageType::CONNECT_RESPONSE,
+                           std::string(1, kConnectResponseNoSession));
   }
 }
 
