6bc9951b15d648ef7bf2a53fb75484f40f3b10df
kozyatinskiy <kozyatinskiy@chromium.org>
2016-05-12 23:40:40 -0700

    [DevTools] Partially revert of moving getProperties to native
    BUG=581495
    R=pfeldman@chromium.org
    
    Review-Url: https://codereview.chromium.org/1979443002
    Cr-Commit-Position: refs/heads/master@{#393459}

--- third_party/WebKit/Source/platform/v8_inspector/injected_script_externs.js
+++ third_party/WebKit/Source/platform/v8_inspector/injected_script_externs.js
@@ -33,6 +33,12 @@ InjectedScriptHostClass.prototype.formatAccessorsAsProperties = function(obj) {}
 InjectedScriptHostClass.prototype.subtype = function(obj) {}
 
 /**
+ * @param {*} obj
+ * @return {boolean}
+ */
+InjectedScriptHostClass.prototype.isTypedArray = function(obj) {}
+
+/**
  * @param {!Object} obj
  * @return {?Array.<*>}
  */
@@ -77,11 +83,6 @@ InjectedScriptHostClass.prototype.bind = function(value, groupName) {}
  * @return {!Object}
  */
 InjectedScriptHostClass.prototype.proxyTargetValue = function(object) {}
-
-/**
- * @return {!Array<string>}
- */
-InjectedScriptHostClass.prototype.ownPropertyNames = function(object) {}
 
 /**
  * @param {!Object} object
