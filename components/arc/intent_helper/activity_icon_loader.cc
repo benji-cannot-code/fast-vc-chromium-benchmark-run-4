23a12deb43e6a9165ce0a6434d9a2eb8ead10914
kinaba <kinaba@chromium.org>
2016-05-13 18:16:36 -0700

    ARC: Change ActivityIconLoader to the 'one shot' callback style.
    For bug 607059, I want to use the loader inside an async Chrome extension API,
    where it has to callback exactly once. The previous behavior of GetActivityIcons
    that may or may not callback multiple times does not suit well there.
    
    This CL changes the method to callback exactly once always.
    
    BUG=607059
    TEST=components_unittests --gtest_filter='ActivityIconLoaderTest.*'
    TEST=manually checked the Chrome context menu icon.
    
    Review-Url: https://codereview.chromium.org/1974833004
    Cr-Commit-Position: refs/heads/master@{#393721}

--- components/arc/intent_helper/activity_icon_loader.cc
+++ components/arc/intent_helper/activity_icon_loader.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 
 #include "base/bind.h"
+#include "base/memory/ptr_util.h"
 #include "base/task_runner_util.h"
 #include "components/arc/arc_bridge_service.h"
 #include "components/arc/arc_service_manager.h"
@@ -75,7 +76,7 @@ void ActivityIconLoader::InvalidateIcons(const std::string& package_name) {
   }
 }
 
-void ActivityIconLoader::GetActivityIcons(
+bool ActivityIconLoader::GetActivityIcons(
     const std::vector<ActivityName>& activities,
     const OnIconsReadyCallback& cb) {
   std::unique_ptr<ActivityToIconsMap> result(new ActivityToIconsMap);
@@ -93,22 +94,26 @@ void ActivityIconLoader::GetActivityIcons(
     }
   }
 
-  cb.Run(std::move(result));
-
   mojom::IntentHelperInstance* instance = GetIntentHelperInstance();
-  if (activities_to_fetch.empty() || !instance)
-    return;
+  if (activities_to_fetch.empty() || !instance) {
+    // If there's nothing to fetch or the mojo channel is not available, call
+    // back right now.
+    cb.Run(std::move(result));
+    return true;
+  }
 
   // Fetch icons from ARC.
   instance->RequestActivityIcons(
       std::move(activities_to_fetch), mojom::ScaleFactor(scale_factor_),
-      base::Bind(&ActivityIconLoader::OnIconsReady, this, cb));
+      base::Bind(&ActivityIconLoader::OnIconsReady, this, base::Passed(&result),
+                 cb));
+  return false;
 }
 
 void ActivityIconLoader::OnIconsResizedForTesting(
     const OnIconsReadyCallback& cb,
     std::unique_ptr<ActivityToIconsMap> result) {
-  OnIconsResized(cb, std::move(result));
+  OnIconsResized(base::MakeUnique<ActivityToIconsMap>(), cb, std::move(result));
 }
 
 void ActivityIconLoader::AddIconToCacheForTesting(const ActivityName& activity,
@@ -117,13 +122,15 @@ void ActivityIconLoader::AddIconToCacheForTesting(const ActivityName& activity,
 }
 
 void ActivityIconLoader::OnIconsReady(
+    std::unique_ptr<ActivityToIconsMap> cached_result,
     const OnIconsReadyCallback& cb,
     mojo::Array<mojom::ActivityIconPtr> icons) {
   ArcServiceManager* manager = ArcServiceManager::Get();
   base::PostTaskAndReplyWithResult(
       manager->blocking_task_runner().get(), FROM_HERE,
       base::Bind(&ActivityIconLoader::ResizeIcons, this, base::Passed(&icons)),
-      base::Bind(&ActivityIconLoader::OnIconsResized, this, cb));
+      base::Bind(&ActivityIconLoader::OnIconsResized, this,
+                 base::Passed(&cached_result), cb));
 }
 
 std::unique_ptr<ActivityIconLoader::ActivityToIconsMap>
@@ -166,6 +173,7 @@ ActivityIconLoader::ResizeIcons(mojo::Array<mojom::ActivityIconPtr> icons) {
 }
 
 void ActivityIconLoader::OnIconsResized(
+    std::unique_ptr<ActivityToIconsMap> cached_result,
     const OnIconsReadyCallback& cb,
     std::unique_ptr<ActivityToIconsMap> result) {
   // Update |cached_icons_|.
@@ -174,6 +182,8 @@ void ActivityIconLoader::OnIconsResized(
     cached_icons_.insert(std::make_pair(kv.first, kv.second));
   }
 
+  // Merge the results that were obtained from cache before doing IPC.
+  result->insert(cached_result->begin(), cached_result->end());
   cb.Run(std::move(result));
 }
 
