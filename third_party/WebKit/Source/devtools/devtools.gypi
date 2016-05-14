5145f7fd8806b7235a9e1a625683f7c9b255334e
lushnikov <lushnikov@chromium.org>
2016-05-14 13:32:00 -0700

    DevTools: SourcesPanel general improvements
    BUG=none
    TBR=pfeldman, dgozman
    
    Review-Url: https://codereview.chromium.org/1981673002
    Cr-Commit-Position: refs/heads/master@{#393745}

--- third_party/WebKit/Source/devtools/devtools.gypi
+++ third_party/WebKit/Source/devtools/devtools.gypi
@@ -694,6 +694,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'front_end/sources/ScriptFormatter.js',
             'front_end/sources/ScriptFormatterEditorAction.js',
             'front_end/sources/SimpleHistoryManager.js',
+            'front_end/sources/SourceCodeDiff.js',
             'front_end/sources/SourceMapNamesResolver.js',
             'front_end/sources/SourcesNavigator.js',
             'front_end/sources/SourcesPanel.js',
--- third_party/WebKit/Source/devtools/front_end/sources/UISourceCodeFrame.js
+++ third_party/WebKit/Source/devtools/front_end/sources/UISourceCodeFrame.js
@@ -37,7 +37,10 @@ WebInspector.UISourceCodeFrame = function(uiSourceCode)
     this._uiSourceCode = uiSourceCode;
     WebInspector.SourceFrame.call(this, uiSourceCode.contentURL(), workingCopy);
 
+    if (Runtime.experiments.isEnabled("sourceDiff"))
+        this._diff = new WebInspector.SourceCodeDiff(uiSourceCode, this.textEditor);
     this.textEditor.setAutocompleteDelegate(new WebInspector.SimpleAutocompleteDelegate());
+
     this._rowMessageBuckets = {};
     /** @type {!Set<string>} */
     this._typeDecorationsPending = new Set();
@@ -75,6 +78,8 @@ WebInspector.UISourceCodeFrame.prototype = {
     wasShown: function()
     {
         WebInspector.SourceFrame.prototype.wasShown.call(this);
+        if (this._diff)
+            this._diff.updateImmediately();
         this._boundWindowFocused = this._windowFocused.bind(this);
         this.element.ownerDocument.defaultView.addEventListener("focus", this._boundWindowFocused, false);
         this._checkContentUpdated();
@@ -161,6 +166,8 @@ WebInspector.UISourceCodeFrame.prototype = {
      */
     _onWorkingCopyChanged: function(event)
     {
+        if (this._diff)
+            this._diff.updateWhenPossible();
         if (this._muteSourceCodeEvents)
             return;
         this._innerSetContent(this._uiSourceCode.workingCopy());
@@ -178,6 +185,8 @@ WebInspector.UISourceCodeFrame.prototype = {
         }
         this._textEditor.markClean();
         this._updateStyle();
+        if (this._diff)
+            this._diff.updateWhenPossible();
     },
 
     _updateStyle: function()
