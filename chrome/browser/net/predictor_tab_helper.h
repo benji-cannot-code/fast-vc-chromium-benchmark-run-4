98a402cb7c4ce9f5940dfa7393f2984e97d3e1e2
csharrison <csharrison@chromium.org>
2016-05-14 13:54:20 -0700

    [reland] Change net's predictor_tab_helper to use the new content Navigation API
    The original CL had issues due to slow process spawn on
    Android, where the DidStartNavigation call could be delayed,
    stalling preconnects. This patch keeps the old code while adding
    a path for renderer initiated navigations to issue preconnects.
    
    Previous CL: https://codereview.chromium.org/1813553004/
    This change moves the trigger for preconnects/preresolves from
    DidStartNavigationToPendingEntry to DidStartNavigation, broadening the
    navigations that we can predict from.
    
    BUG=595730
    
    Review-Url: https://codereview.chromium.org/1857573002
    Cr-Commit-Position: refs/heads/master@{#393754}

--- chrome/browser/net/predictor_tab_helper.h
+++ chrome/browser/net/predictor_tab_helper.h
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
+namespace content {
+class NavigationHandle;
+}
+
 namespace chrome_browser_net {
 
 class PredictorTabHelper
@@ -20,7 +24,9 @@ class PredictorTabHelper
  public:
   ~PredictorTabHelper() override;
 
-  // content::WebContentsObserver implementation
+  // content::WebContentsObserver:
+  void DidStartNavigation(
+      content::NavigationHandle* navigation_handle) override;
   void DidStartNavigationToPendingEntry(
       const GURL& url,
       content::NavigationController::ReloadType reload_type) override;
@@ -28,6 +34,17 @@ class PredictorTabHelper
  private:
   explicit PredictorTabHelper(content::WebContents* web_contents);
   friend class content::WebContentsUserData<PredictorTabHelper>;
+
+  void PreconnectUrl(const GURL& url);
+
+  // This boolean is set to true after a call to
+  // DidStartNavigationToPendingEntry, which fires off predictive preconnects.
+  // This ensures that the resulting call to DidStartNavigation does not
+  // duplicate these preconnects. The tab helper spawns preconnects on the
+  // navigation to the pending entry because DidStartNavigation is called after
+  // render process spawn (pre-PlzNavigate), which can take substantial time
+  // especially on Android.
+  bool predicted_from_pending_entry_;
 
   DISALLOW_COPY_AND_ASSIGN(PredictorTabHelper);
 };
