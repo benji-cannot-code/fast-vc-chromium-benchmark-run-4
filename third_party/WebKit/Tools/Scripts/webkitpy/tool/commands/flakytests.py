b6ea20d17bac1aa4b3f80da763b869c2d32e8b46
qyearsley <qyearsley@chromium.org>
2016-05-15 16:56:37 -0700

    Split Command and HelpCommand into separate files.
    This is a refactoring CL.
    
    Reason: The base Command class can logically be considered and tested separately from MultiCommandTool.
    
    This is also somewhat related to clearing out the imports from commands/__init__.py -- in order to not do imports there, we could explicitly import and enumerate the Command classes to include, either in webkit_patch.py or multicommandtool.py, and so individual Command classes shouldn't depend on the multicommandtool module (because that leads to circular imports).
    
    Review-Url: https://codereview.chromium.org/1971593002
    Cr-Commit-Position: refs/heads/master@{#393777}

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/flakytests.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/flakytests.py
@@ -29,12 +29,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import logging
 
-from webkitpy.tool.multicommandtool import Command
+from webkitpy.common.net import sheriff_calendar
 from webkitpy.layout_tests.layout_package.bot_test_expectations import BotTestExpectationsFactory
 from webkitpy.layout_tests.models.test_expectations import TestExpectationParser
 from webkitpy.layout_tests.models.test_expectations import TestExpectations
 from webkitpy.layout_tests.models.test_expectations import TestExpectationsModel
-from webkitpy.common.net import sheriff_calendar
+from webkitpy.tool.commands.command import Command
 
 
 _log = logging.getLogger(__name__)
