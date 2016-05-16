0c91bb0ee4903acdd6d962e24459eb69c4cc4e0f
zmo <zmo@chromium.org>
2016-05-16 14:43:00 -0700

    Update WebGL2 conformance test expectations on Windows.
    BUG=
    TEST=win gpu bots
    TBR=kbr@chromium.org
    NOTRY=true
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/1986483002
    Cr-Commit-Position: refs/heads/master@{#393941}

--- content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
@@ -89,15 +89,6 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
     # Failing because the tests are using invalid combinations of source and
     # destination formats, see https://github.com/KhronosGroup/WebGL/issues/1628
     self.Fail('conformance2/textures/webgl_canvas/' +
-        'tex-2d-r8-red-unsigned_byte.html',
-        ['d3d9', 'd3d11', 'opengl'], bug=483282)
-    self.Fail('conformance2/textures/webgl_canvas/' +
-        'tex-2d-rg8-rg-unsigned_byte.html',
-        ['d3d9', 'd3d11', 'opengl'], bug=483282)
-    self.Fail('conformance2/textures/webgl_canvas/' +
-        'tex-2d-rgb8-rgb-unsigned_byte.html',
-        ['d3d9', 'd3d11', 'opengl'], bug=483282)
-    self.Fail('conformance2/textures/webgl_canvas/' +
         'tex-2d-rgb565-rgb-unsigned_byte.html',
         ['d3d9', 'd3d11', 'opengl'], bug=483282)
     self.Fail('conformance2/textures/webgl_canvas/' +
@@ -118,20 +109,9 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
 
     # Windows only.
 
-    self.Fail('deqp/functional/gles3/negativefragmentapi.html',
-        ['win'], bug=483282)
     self.Fail('deqp/functional/gles3/vertexarrays.html',
         ['win'], bug=483282)
 
-    self.Fail('conformance2/textures/canvas/' +
-        'tex-2d-r8-red-unsigned_byte.html',
-        ['win'], bug=483282)
-    self.Fail('conformance2/textures/canvas/' +
-        'tex-2d-rg8-rg-unsigned_byte.html',
-        ['win'], bug=483282)
-    self.Fail('conformance2/textures/canvas/' +
-        'tex-2d-rgb8-rgb-unsigned_byte.html',
-        ['win'], bug=483282)
     self.Fail('conformance2/textures/canvas/' +
         'tex-2d-rgb565-rgb-unsigned_byte.html',
         ['win'], bug=483282)
@@ -162,8 +142,6 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
         ['win'], bug=483282)
     self.Skip('conformance2/transform_feedback/transform_feedback.html',
         ['win'], bug=483282)
-    self.Fail('conformance2/glsl3/const-array-init.html',
-        ['win'], bug=1198) # angle bug ID
     self.Skip('conformance2/reading/read-pixels-into-pixel-pack-buffer.html',
         ['win'], bug=1266) # angle bug ID
     self.Fail('conformance2/state/gl-object-get-calls.html',
