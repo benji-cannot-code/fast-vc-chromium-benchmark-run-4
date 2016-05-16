2ebcba4372bb3d7ed312df190f07c78cafba1ef3
smaier <smaier@chromium.org>
2016-05-16 12:33:36 -0700

    Added brotli enc/ and tools/ directories.
    BUG=332521
    
    Review-Url: https://codereview.chromium.org/1956893002
    Cr-Commit-Position: refs/heads/master@{#393898}

---
+++ third_party/brotli/enc/literal_cost.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/* Copyright 2013 Google Inc. All Rights Reserved.
+
+   Distributed under MIT license.
+   See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+*/
+
+// Literal cost model to allow backward reference replacement to be efficient.
+
+#ifndef BROTLI_ENC_LITERAL_COST_H_
+#define BROTLI_ENC_LITERAL_COST_H_
+
+#include "./types.h"
+
+namespace brotli {
+
+// Estimates how many bits the literals in the interval [pos, pos + len) in the
+// ringbuffer (data, mask) will take entropy coded and writes these estimates
+// to the cost[0..len) array.
+void EstimateBitCostsForLiterals(size_t pos, size_t len, size_t mask,
+                                 const uint8_t *data, float *cost);
+
+}  // namespace brotli
+
+#endif  // BROTLI_ENC_LITERAL_COST_H_
