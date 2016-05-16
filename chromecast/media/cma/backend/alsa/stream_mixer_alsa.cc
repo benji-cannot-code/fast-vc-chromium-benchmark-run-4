b49bf43f78d6b86e93e1639b4b9cc944da5e6f0c
kmackay <kmackay@chromium.org>
2016-05-16 14:33:05 -0700

    [Chromecast] Make the ALSA mixer only write 20ms at a time.
    Without this limit, the mixer will write bursts of up to 100ms at
    a time, which causes the loopback data to be very bursty.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/1979193007
    Cr-Commit-Position: refs/heads/master@{#393936}

--- chromecast/media/cma/backend/alsa/stream_mixer_alsa.cc
+++ chromecast/media/cma/backend/alsa/stream_mixer_alsa.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <cmath>
-#include <limits>
 #include <utility>
 
 #include "base/bind_helpers.h"
@@ -73,6 +72,8 @@ const bool kPcmRecoverIsSilent = false;
 // are present.
 const int kPreventUnderrunChunkSize = 512;
 const int kDefaultCheckCloseTimeoutMs = 2000;
+
+const int kMaxWriteSizeMs = 20;
 
 // A list of supported sample rates.
 // TODO(jyw): move this up into chromecast/public for 1) documentation and
@@ -705,7 +706,7 @@ bool StreamMixerAlsa::TryWriteFrames() {
   DCHECK(mixer_task_runner_->BelongsToCurrentThread());
   if (state_ != kStateNormalPlayback)
     return false;
-  int chunk_size = std::numeric_limits<int>::max();
+  int chunk_size = output_samples_per_second_ * kMaxWriteSizeMs / 1000;
   std::vector<InputQueue*> active_inputs;
   for (auto&& input : inputs_) {
     int read_size = input->MaxReadSize();
