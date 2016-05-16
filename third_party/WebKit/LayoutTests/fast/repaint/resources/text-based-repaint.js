16d1d0812b56c5859e5fd8b962b14562b83a3d79
davve <davve@opera.com>
2016-05-16 04:29:34 -0700

    Remove workaround for multiple svg onload events
    The bug referred to has been fixed. Let's see if the flakiness stays
    away when removing the workaround.
    
    BUG=372946
    
    Review-Url: https://codereview.chromium.org/1983813002
    Cr-Commit-Position: refs/heads/master@{#393818}

--- third_party/WebKit/LayoutTests/fast/repaint/resources/text-based-repaint.js
+++ third_party/WebKit/LayoutTests/fast/repaint/resources/text-based-repaint.js
@@ -16,12 +16,6 @@ function runRepaintTest()
         return;
     }
 
-    // TODO(enne): this is a workaround for multiple svg onload events.
-    // See: http://crbug.com/372946
-    if (window.hasRunRepaintTest)
-        return;
-    window.hasRunRepaintTest = true;
-
     if (window.enablePixelTesting)
         testRunner.dumpAsTextWithPixelResults();
     else
