2b09e725af7c5332a9a25757e0a22446dc5f6cc7
erikchen <erikchen@chromium.org>
2016-05-16 14:07:16 -0700

    mac: Fix an attachment broker race condition.
    If a source or destination communication channel has not been established, the
    attachment broker will wait for the connection, rather than immediately failing
    to broker the attachment.
    
    BUG=609262
    
    Review-Url: https://codereview.chromium.org/1978353003
    Cr-Commit-Position: refs/heads/master@{#393915}

--- ipc/ipc_channel_posix.cc
+++ ipc/ipc_channel_posix.cc
@@ -1018,6 +1018,12 @@ void ChannelPosix::HandleInternalMessage(const Message& msg) {
 
       if (!FlushPrelimQueue())
         ClosePipeOnError();
+
+      if (IsAttachmentBrokerEndpoint() &&
+          AttachmentBroker::GetGlobal() &&
+          AttachmentBroker::GetGlobal()->IsPrivilegedBroker()) {
+        AttachmentBroker::GetGlobal()->ReceivedPeerPid(pid);
+      }
       break;
 
 #if defined(OS_MACOSX)
