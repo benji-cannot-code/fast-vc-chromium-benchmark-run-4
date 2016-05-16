b178e697c644d31fba92012de1216dc8c69ee1fe
sunnyps <sunnyps@chromium.org>
2016-05-16 14:50:35 -0700

    cc: Do not reset pending tree state incorrectly on aborted commits.
    When the --enable-main-frame-before-activation flag is passed aborted
    commits can cause the pending tree CHECK in LTHI::BeginCommit to fail.
    This happens because the aborted commit resets the scheduler's pending
    tree state and causes it to start a commit while there's a pending tree.
    
    BUG=606210
    R=enne@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review-Url: https://codereview.chromium.org/1984453003
    Cr-Commit-Position: refs/heads/master@{#393943}

--- cc/trees/proxy_impl.cc
+++ cc/trees/proxy_impl.cc
@@ -185,7 +185,6 @@ void ProxyImpl::BeginMainFrameAbortedOnImpl(
                CommitEarlyOutReasonToString(reason));
   DCHECK(IsImplThread());
   DCHECK(scheduler_->CommitPending());
-  DCHECK(!layer_tree_host_impl_->pending_tree());
 
   if (CommitEarlyOutHandledCommit(reason)) {
     SetInputThrottledUntilCommitOnImpl(false);
