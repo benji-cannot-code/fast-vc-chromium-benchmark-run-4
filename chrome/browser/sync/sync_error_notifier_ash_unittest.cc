699eec53266788d6a9980255ac8af189d6f01835
xdai <xdai@chromium.org>
2016-05-16 12:22:17 -0700

    [Reland] GetAccountIdFromProfile() should not return an empty account id if a valid profile is provided.
    This is a reland of https://codereview.chromium.org/1880923003/.
    
    On Chrome OS, we don't end the session immediately if a refresh token is revoked in the session. Thus it's possible that we get an empty account id for a profile from Profile::GetProfileUserName(). We should avoid this scenario on Chrome OS.
    
    BUG=602807
    TBR=thakis@chromium.org, rogerta@chromium.org, skuhne@chromium.org
    
    Committed: https://crrev.com/d2a0bb5307f8d0d09aff9d5d531dd9704fe85841
    Cr-Commit-Position: refs/heads/master@{#392133}
    
    Review-Url: https://codereview.chromium.org/1984733002
    Cr-Commit-Position: refs/heads/master@{#393895}

--- chrome/browser/sync/sync_error_notifier_ash_unittest.cc
+++ chrome/browser/sync/sync_error_notifier_ash_unittest.cc
@@ -35,6 +35,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/screen.h"
 #endif
 
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/login/users/mock_user_manager.h"
+#include "chrome/browser/chromeos/login/users/scoped_user_manager_enabler.h"
+#endif
+
 using ::testing::NiceMock;
 using ::testing::Return;
 using ::testing::ReturnRef;
@@ -185,6 +190,10 @@ class SyncErrorNotifierTest : public AshTestBase  {
 #define MAYBE_PassphraseNotification PassphraseNotification
 #endif
 TEST_F(SyncErrorNotifierTest, MAYBE_PassphraseNotification) {
+#if defined(OS_CHROMEOS)
+  chromeos::ScopedUserManagerEnabler scoped_enabler(
+      new chromeos::MockUserManager());
+#endif
   ASSERT_FALSE(notification_ui_manager_->FindById(
       kNotificationId, NotificationUIManager::GetProfileID(profile_)));
 
