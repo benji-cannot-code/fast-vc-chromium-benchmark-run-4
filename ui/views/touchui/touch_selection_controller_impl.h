f78fa12876e7dbea46b3e0df28c77fd1f0fbf92f
mfomitchev <mfomitchev@chromium.org>
2016-05-16 11:53:12 -0700

    Fixes a crash which occurs when a concealed selection handle in a textfield is exposed.
    The crash happened because the type of the SelectionBound within the handle
    wasn't getting initialized properly if the handle was invisible.
    
    The CL ensures that the correct SelectionBound type is maintained even for
    handles which are invisible.
    
    BUG=608687
    
    Review-Url: https://codereview.chromium.org/1947123003
    Cr-Commit-Position: refs/heads/master@{#393885}

--- ui/views/touchui/touch_selection_controller_impl.h
+++ ui/views/touchui/touch_selection_controller_impl.h
@@ -111,6 +111,7 @@ class VIEWS_EXPORT TouchSelectionControllerImpl
 
   // Convenience methods for testing.
   gfx::NativeView GetCursorHandleNativeView();
+  ui::SelectionBound::Type GetSelectionHandle1Type();
   gfx::Rect GetSelectionHandle1Bounds();
   gfx::Rect GetSelectionHandle2Bounds();
   gfx::Rect GetCursorHandleBounds();
