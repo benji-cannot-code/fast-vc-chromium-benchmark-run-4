ffa3ca664593fdb5b9c473c3db7a413a25339ebe
suzyh <suzyh@chromium.org>
2016-05-15 19:25:33 -0700

    Add unit test for TransformationMatrix::multiply.
    The comment on TransformationMatrix::multiply indicates that the function
    premultiplies 'this' by the argument. However, testing demonstrates that it
    actually postmultiplies. This change adds the unit test that verifies the
    behaviour and updates/elaborates on the function comment.
    
    BUG=477892
    
    Review-Url: https://codereview.chromium.org/1530293003
    Cr-Commit-Position: refs/heads/master@{#393783}

--- third_party/WebKit/Source/platform/transforms/TransformationMatrix.h
+++ third_party/WebKit/Source/platform/transforms/TransformationMatrix.h
@@ -264,7 +264,7 @@ public:
     double f() const { return m_matrix[3][1]; }
     void setF(double f) { m_matrix[3][1] = f; }
 
-    // this = mat * this.
+    // *this = *this * mat.
     TransformationMatrix& multiply(const TransformationMatrix&);
 
     TransformationMatrix& scale(double);
