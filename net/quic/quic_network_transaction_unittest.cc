3b23f92838faba32192cca707fa562dcb4c0a802
rtenneti <rtenneti@chromium.org>
2016-05-16 11:21:14 -0700

    QUIC - enable "delay_tcp_race" parameter by default. It was enabled before in M52 but was reverted in the following CL. Reenabling it by default.     https://codereview.chromium.org/1977303002/
    R=rch@chromium.org, xunjieli@chromium.org
    
    Review-Url: https://codereview.chromium.org/1979923002
    Cr-Commit-Position: refs/heads/master@{#393869}

--- net/quic/quic_network_transaction_unittest.cc
+++ net/quic/quic_network_transaction_unittest.cc
@@ -2171,8 +2171,6 @@ TEST_P(QuicNetworkTransactionTest, FailedZeroRttBrokenAlternateProtocol) {
                                      0);
   socket_factory_.AddSocketDataProvider(&quic_data);
 
-  AddHangingNonAlternateProtocolSocketData();
-
   // Second Alternate-protocol job which will race with the TCP job.
   StaticSocketDataProvider quic_data2(quic_reads, arraysize(quic_reads),
                                       nullptr, 0);
@@ -2189,6 +2187,7 @@ TEST_P(QuicNetworkTransactionTest, FailedZeroRttBrokenAlternateProtocol) {
   socket_factory_.AddSocketDataProvider(&http_data);
   socket_factory_.AddSSLSocketDataProvider(&ssl_data_);
 
+  AddHangingNonAlternateProtocolSocketData();
   CreateSession();
 
   AddQuicAlternateProtocolMapping(MockCryptoClientStream::ZERO_RTT);
