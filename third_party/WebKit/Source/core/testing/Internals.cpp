10c6c54571f67a3794aabe70aecd1f1a985c7bdf
pilgrim <pilgrim@chromium.org>
2016-05-16 11:40:56 -0700

    [Layout API] Use layoutViewItem() in Internals
    There are several references to FrameView::layoutView() which can be
    replaced 1-1 with layoutViewItem() instead. No new API methods are
    required.
    
    BUG=499321
    
    Review-Url: https://codereview.chromium.org/1978933002
    Cr-Commit-Position: refs/heads/master@{#393880}

--- third_party/WebKit/Source/core/testing/Internals.cpp
+++ third_party/WebKit/Source/core/testing/Internals.cpp
@@ -2008,7 +2008,7 @@ void Internals::forceFullRepaint(Document* document, ExceptionState& exceptionSt
 void Internals::startTrackingPaintInvalidationObjects()
 {
     ASSERT(RuntimeEnabledFeatures::slimmingPaintV2Enabled());
-    GraphicsLayer* graphicsLayer = toLocalFrame(frame()->page()->mainFrame())->view()->layoutView()->layer()->graphicsLayerBacking();
+    GraphicsLayer* graphicsLayer = toLocalFrame(frame()->page()->mainFrame())->view()->layoutViewItem().layer()->graphicsLayerBacking();
     if (graphicsLayer->drawsContent())
         graphicsLayer->getPaintController().startTrackingPaintInvalidationObjects();
 }
@@ -2016,7 +2016,7 @@ void Internals::startTrackingPaintInvalidationObjects()
 void Internals::stopTrackingPaintInvalidationObjects()
 {
     ASSERT(RuntimeEnabledFeatures::slimmingPaintV2Enabled());
-    GraphicsLayer* graphicsLayer = toLocalFrame(frame()->page()->mainFrame())->view()->layoutView()->layer()->graphicsLayerBacking();
+    GraphicsLayer* graphicsLayer = toLocalFrame(frame()->page()->mainFrame())->view()->layoutViewItem().layer()->graphicsLayerBacking();
     if (graphicsLayer->drawsContent())
         graphicsLayer->getPaintController().stopTrackingPaintInvalidationObjects();
 }
@@ -2024,7 +2024,7 @@ void Internals::stopTrackingPaintInvalidationObjects()
 Vector<String> Internals::trackedPaintInvalidationObjects()
 {
     ASSERT(RuntimeEnabledFeatures::slimmingPaintV2Enabled());
-    GraphicsLayer* graphicsLayer = toLocalFrame(frame()->page()->mainFrame())->view()->layoutView()->layer()->graphicsLayerBacking();
+    GraphicsLayer* graphicsLayer = toLocalFrame(frame()->page()->mainFrame())->view()->layoutViewItem().layer()->graphicsLayerBacking();
     if (!graphicsLayer->drawsContent())
         return Vector<String>();
     return graphicsLayer->getPaintController().trackedPaintInvalidationObjects();
