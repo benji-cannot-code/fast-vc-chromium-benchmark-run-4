06f92ecefc083c0071a254ba9275ba6051d68493
dgn <dgn@chromium.org>
2016-05-17 09:52:30 -0700

    [NTP Snippets] Fill space below the last snippet if necessary
    Add a filler item at the end of the recycler view to push the
    snippet items to the top of the view when there are not enough
    of them.
    
    Preview: https://goo.gl/photos/BDHFexhW8Cvo9Y4v5
    
    BUG=603308
    
    Review-Url: https://codereview.chromium.org/1928063002
    Cr-Commit-Position: refs/heads/master@{#394147}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -554,6 +554,9 @@ public class NewTabPageView extends FrameLayout
         snippetHeader.setAlpha(headerAlpha);
         params.height = headerHeight;
         snippetHeader.setLayoutParams(params);
+
+        // Update the space at the bottom, which needs to know about the height of the header.
+        mRecyclerView.refreshBottomSpacing();
     }
 
     /**
