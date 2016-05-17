03d3579a0cf1027ba0654cb39576ec6e6bcfa0c7
wjmaclean <wjmaclean@chromium.org>
2016-05-17 08:44:50 -0700

    Retest that we don't get empty gesture targets.
    Now that https://codereview.chromium.org/1907323003/ has landed, re-test
    that we aren't seeing any empty gesture sequences on Windows.
    
    BUG=592319, 595422
    
    Review-Url: https://codereview.chromium.org/1989433002
    Cr-Commit-Position: refs/heads/master@{#394134}

--- content/browser/renderer_host/render_widget_host_input_event_router.cc
+++ content/browser/renderer_host/render_widget_host_input_event_router.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/renderer_host/render_widget_host_input_event_router.h"
 
+#include "base/debug/dump_without_crashing.h"
 #include "cc/quads/surface_draw_quad.h"
 #include "cc/surfaces/surface_id_allocator.h"
 #include "cc/surfaces/surface_manager.h"
@@ -177,6 +178,7 @@ void RenderWidgetHostInputEventRouter::RouteGestureEvent(
       // TODO(wjmaclean): this only happens on Windows, and should not happen.
       // https://crbug.com/595422
       gesture_target_ = nullptr;
+      base::debug::DumpWithoutCrashing();
       return;
     }
 
