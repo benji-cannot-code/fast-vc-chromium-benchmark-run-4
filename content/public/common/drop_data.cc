9a4674cb034acbe54426f8024e7190aa41cc2c91
wjmaclean <wjmaclean@chromium.org>
2016-05-17 10:40:42 -0700

    Propagate drag-and-drop key modifiers to WebView's guest renderer.
    Currently BrowserPluginGuest strips drag event modifiers when forwarding
    the events to the guest renderer, which is wrong.
    
    This CL provides the necessary plumbing to propagate these modifiers.
    
    BUG=545315
    
    Review-Url: https://codereview.chromium.org/1411913010
    Cr-Commit-Position: refs/heads/master@{#394157}

--- content/public/common/drop_data.cc
+++ content/public/common/drop_data.cc
@@ -9,7 +9,8 @@ namespace content {
 
 DropData::DropData()
     : did_originate_from_renderer(false),
-      referrer_policy(blink::WebReferrerPolicyDefault) {}
+      referrer_policy(blink::WebReferrerPolicyDefault),
+      key_modifiers(0) {}
 
 DropData::DropData(const DropData& other) = default;
 
