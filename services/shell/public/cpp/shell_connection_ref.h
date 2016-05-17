5728fd1e5a336a3e54198e08522cca4f55e1424e
rockot <rockot@chromium.org>
2016-05-16 21:36:55 -0700

    Fix race in ShellConnectionRef
    When a ShellConnectionRef is Cloned it may post a task
    to the factory's thread to add a ref. This can race with
    the ref-count going to zero and the factory being deleted.
    
    This CL changes ShellConnectionRef to hold a WeakPtr
    to the factory to avoid the race.
    
    This means it's possible for a ShellConnectionRef to be
    acquired without keeping the ShellClient alive (because
    AddRef will never run), which is a problem. But it's
    less of a problem than crashing, for now.
    
    BUG=612332
    R=ben@chromium.org
    
    Review-Url: https://codereview.chromium.org/1987583002
    Cr-Commit-Position: refs/heads/master@{#394049}

--- services/shell/public/cpp/shell_connection_ref.h
+++ services/shell/public/cpp/shell_connection_ref.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 
 namespace shell {
 
@@ -49,6 +50,7 @@ class ShellConnectionRefFactory {
 
   const base::Closure quit_closure_;
   int ref_count_ = 0;
+  base::WeakPtrFactory<ShellConnectionRefFactory> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ShellConnectionRefFactory);
 };
