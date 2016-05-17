5ac211fe6b061132e8d0117e0670493ceaf44dd5
changwan <changwan@chromium.org>
2016-05-16 17:59:36 -0700

    Guard logging from InputMethodManagerWrapper
    BUG=610715
    
    Review-Url: https://codereview.chromium.org/1981863002
    Cr-Commit-Position: refs/heads/master@{#393998}

--- content/public/android/java/src/org/chromium/content/browser/input/InputMethodManagerWrapper.java
+++ content/public/android/java/src/org/chromium/content/browser/input/InputMethodManagerWrapper.java
@@ -23,12 +23,13 @@ import java.lang.reflect.Method;
  * Wrapper around Android's InputMethodManager
  */
 public class InputMethodManagerWrapper {
+    private static final boolean DEBUG_LOGS = false;
     private static final String TAG = "cr_Ime";
 
     private final Context mContext;
 
     public InputMethodManagerWrapper(Context context) {
-        Log.d(TAG, "Constructor");
+        if (DEBUG_LOGS) Log.d(TAG, "Constructor");
         mContext = context;
     }
 
@@ -40,7 +41,7 @@ public class InputMethodManagerWrapper {
      * @see android.view.inputmethod.InputMethodManager#restartInput(View)
      */
     public void restartInput(View view) {
-        Log.d(TAG, "restartInput");
+        if (DEBUG_LOGS) Log.d(TAG, "restartInput");
         getInputMethodManager().restartInput(view);
     }
 
@@ -48,7 +49,7 @@ public class InputMethodManagerWrapper {
      * @see android.view.inputmethod.InputMethodManager#showSoftInput(View, int, ResultReceiver)
      */
     public void showSoftInput(View view, int flags, ResultReceiver resultReceiver) {
-        Log.d(TAG, "showSoftInput");
+        if (DEBUG_LOGS) Log.d(TAG, "showSoftInput");
         getInputMethodManager().showSoftInput(view, flags, resultReceiver);
     }
 
@@ -57,7 +58,7 @@ public class InputMethodManagerWrapper {
      */
     public boolean isActive(View view) {
         final boolean active = getInputMethodManager().isActive(view);
-        Log.d(TAG, "isActive: " + active);
+        if (DEBUG_LOGS) Log.d(TAG, "isActive: " + active);
         return active;
     }
 
@@ -66,7 +67,7 @@ public class InputMethodManagerWrapper {
      */
     public boolean hideSoftInputFromWindow(IBinder windowToken, int flags,
             ResultReceiver resultReceiver) {
-        Log.d(TAG, "hideSoftInputFromWindow");
+        if (DEBUG_LOGS) Log.d(TAG, "hideSoftInputFromWindow");
         return getInputMethodManager().hideSoftInputFromWindow(windowToken, flags, resultReceiver);
     }
 
@@ -75,7 +76,7 @@ public class InputMethodManagerWrapper {
      */
     public void updateSelection(View view, int selStart, int selEnd,
             int candidatesStart, int candidatesEnd) {
-        Log.d(TAG, "updateSelection: SEL [%d, %d], COM [%d, %d]", selStart, selEnd,
+        if (DEBUG_LOGS) Log.d(TAG, "updateSelection: SEL [%d, %d], COM [%d, %d]", selStart, selEnd,
                 candidatesStart, candidatesEnd);
         getInputMethodManager().updateSelection(view, selStart, selEnd, candidatesStart,
                 candidatesEnd);
@@ -87,7 +88,7 @@ public class InputMethodManagerWrapper {
      */
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public void updateCursorAnchorInfo(View view, CursorAnchorInfo cursorAnchorInfo) {
-        Log.d(TAG, "updateCursorAnchorInfo");
+        if (DEBUG_LOGS) Log.d(TAG, "updateCursorAnchorInfo");
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getInputMethodManager().updateCursorAnchorInfo(view, cursorAnchorInfo);
         }
@@ -100,14 +101,14 @@ public class InputMethodManagerWrapper {
     public void notifyUserAction() {
         // On N and above, this is not needed.
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) return;
-        Log.d(TAG, "notifyUserAction");
+        if (DEBUG_LOGS) Log.d(TAG, "notifyUserAction");
         InputMethodManager manager = getInputMethodManager();
         try {
             Method method = InputMethodManager.class.getMethod("notifyUserAction");
             method.invoke(manager);
         } catch (NoSuchMethodException | IllegalAccessException | IllegalArgumentException
                 | InvocationTargetException e) {
-            Log.d(TAG, "notifyUserAction failed");
+            if (DEBUG_LOGS) Log.d(TAG, "notifyUserAction failed");
             return;
         }
     }
