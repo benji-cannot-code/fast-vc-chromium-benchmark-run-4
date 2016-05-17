deca3fb2fcb55fda5d2c5368436b2a801c6c343a
vakh <vakh@chromium.org>
2016-05-16 17:16:58 -0700

    Reland: Initialize and reset V4LocalDBManager. Instantiate V4Stores. A rough sketch of how this works is here: https://goto.google.com/pver4-LoadUpdateDatabase-doc
    This relands https://codereview.chromium.org/1954393002 which was reverted by https://codereview.chromium.org/1983603002
    
    Reason for failure: StopOnIOThread ran first and reset v4_update_protocol_manager_, which we were later tried to dereference in DatabaseReady()
    
    --
    Original commit: http://crrev.com/32b9468ab5177534cb155b2d222611a6bca16c98
    Which was reverted as: http://crrev.com/517133dfc95a9ca2b599bfe7452205e5b5d81fd4
    
    This CL reverts commit http://crrev.com/517133dfc95a9ca2b599bfe7452205e5b5d81fd4
    --
    
    BUG=543161
    
    Review-Url: https://codereview.chromium.org/1983723002
    Cr-Commit-Position: refs/heads/master@{#393989}

--- components/safe_browsing_db/v4_store.h
+++ components/safe_browsing_db/v4_store.h
@@ -6,9 +6,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SAFE_BROWSING_DB_V4_STORE_H_
 #define COMPONENTS_SAFE_BROWSING_DB_V4_STORE_H_
 
+#include "base/files/file_path.h"
+#include "base/memory/ref_counted.h"
+#include "base/sequenced_task_runner.h"
+
 namespace safe_browsing {
 
 class V4Store {
+ public:
+  // The |task_runner| is used to ensure that the operations in this file are
+  // performed on the correct thread. |store_path| specifies the location on
+  // disk for this file.
+  V4Store(const scoped_refptr<base::SequencedTaskRunner>& task_runner,
+          const base::FilePath& store_path);
+  virtual ~V4Store();
+
+  // Reset internal state and delete the backing file.
+  virtual bool Reset();
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(V4Store);
 };
 
 }  // namespace safe_browsing
