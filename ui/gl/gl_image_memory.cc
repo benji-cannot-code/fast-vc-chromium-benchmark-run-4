3cd6dfa752eee8d7a9f693bc2653a62c0b847ab7
reveman <reveman@chromium.org>
2016-05-17 09:43:48 -0700

    ui: Add RGB_565 buffer format.
    This adds a new 16-bit uncompressed RGB buffer format. This is useful to
    have when importing buffers.
    
    This also makes our shared memory implementation of GpuMemoryBuffers
    and GLImages support this format.
    
    BUG=538325
    TEST=gl_unittest --gtest_filter=GLImage*/GLImageCopyTest/*.CopyTexImage
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/1977363002
    Cr-Commit-Position: refs/heads/master@{#394145}

--- ui/gl/gl_image_memory.cc
+++ ui/gl/gl_image_memory.cc
@@ -45,6 +45,7 @@ bool ValidFormat(BufferFormat format) {
     case BufferFormat::DXT5:
     case BufferFormat::ETC1:
     case BufferFormat::R_8:
+    case BufferFormat::RGB_565:
     case BufferFormat::RGBA_4444:
     case BufferFormat::RGBX_8888:
     case BufferFormat::RGBA_8888:
@@ -70,6 +71,7 @@ bool IsCompressedFormat(BufferFormat format) {
     case BufferFormat::ETC1:
       return true;
     case BufferFormat::R_8:
+    case BufferFormat::RGB_565:
     case BufferFormat::RGBA_4444:
     case BufferFormat::RGBX_8888:
     case BufferFormat::RGBA_8888:
@@ -106,6 +108,7 @@ GLenum TextureFormat(BufferFormat format) {
       return GL_RGBA;
     case BufferFormat::BGRA_8888:
       return GL_BGRA_EXT;
+    case BufferFormat::RGB_565:
     case BufferFormat::RGBX_8888:
     case BufferFormat::BGRX_8888:
       return GL_RGB;
@@ -126,6 +129,7 @@ GLenum DataFormat(BufferFormat format) {
       return GL_RGBA;
     case BufferFormat::BGRX_8888:
       return GL_BGRA_EXT;
+    case BufferFormat::RGB_565:
     case BufferFormat::RGBA_4444:
     case BufferFormat::RGBA_8888:
     case BufferFormat::BGRA_8888:
@@ -149,6 +153,8 @@ GLenum DataFormat(BufferFormat format) {
 
 GLenum DataType(BufferFormat format) {
   switch (format) {
+    case BufferFormat::RGB_565:
+      return GL_UNSIGNED_SHORT_5_6_5;
     case BufferFormat::RGBA_4444:
       return GL_UNSIGNED_SHORT_4_4_4_4;
     case BufferFormat::RGBX_8888:
@@ -175,6 +181,7 @@ GLenum DataType(BufferFormat format) {
 
 GLint DataRowLength(size_t stride, BufferFormat format) {
   switch (format) {
+    case BufferFormat::RGB_565:
     case BufferFormat::RGBA_4444:
       return base::checked_cast<GLint>(stride) / 2;
     case BufferFormat::RGBX_8888:
@@ -256,6 +263,7 @@ std::unique_ptr<uint8_t[]> GLES2Data(const gfx::Size& size,
                             dst[1] = src[1];
                             dst[2] = src[0];
                           }, data_format, data_type, data_row_length);
+    case BufferFormat::RGB_565:
     case BufferFormat::RGBA_4444:
     case BufferFormat::RGBA_8888:
     case BufferFormat::BGRA_8888:
