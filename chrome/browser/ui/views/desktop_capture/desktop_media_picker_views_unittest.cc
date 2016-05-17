e64cb7cb27816d7603cfc97b2df2cfd97133549b
qiangchen <qiangchen@chromium.org>
2016-05-17 13:14:49 -0700

    Desktop Capture Picker New UI: Split File
    This CL is the preliminary change for detailed appearance
    change of desktop capture picker UI.
    
    We separate DesktopMediaListView and DesktopMediaSourceView
    into their own files, as we anticipated that after appearance
    change, the code size of those two classes will be too large
    to squeeze in DesktopMediaPickerDialogView's file.
    
    BUG=602478
    
    Review-Url: https://codereview.chromium.org/1978633002
    Cr-Commit-Position: refs/heads/master@{#394204}

--- chrome/browser/ui/views/desktop_capture/desktop_media_picker_views_unittest.cc
+++ chrome/browser/ui/views/desktop_capture/desktop_media_picker_views_unittest.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/media/fake_desktop_media_list.h"
+#include "chrome/browser/ui/views/desktop_capture/desktop_media_list_view.h"
+#include "chrome/browser/ui/views/desktop_capture/desktop_media_source_view.h"
 #include "components/web_modal/test_web_contents_modal_dialog_host.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gmock/include/gmock/gmock.h"
