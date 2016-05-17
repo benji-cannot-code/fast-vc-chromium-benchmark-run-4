7517bd85e0d0ad71f3b650bc638163ef9475ed13
brucedawson <brucedawson@chromium.org>
2016-05-16 19:44:04 -0700

    Allow building with VS 2015 Update 3 pre-release
    Add toolchain hash for Update 3 pre-release and fix declarations of
    delay-load hooks for VS 2015 Update 3. The new toolchain is not used
    unless you go "set DEPOT_TOOLS_WIN_TOOLCHAIN_PRERELEASE=1", run
    "gclient runhooks", and then regenerate ninja files (if using gn).
    Therefore this change is a NOP for the build machines and the code
    we ship.
    
    VS 2015 Update 3 changes the delay hook function pointers to be const
    because this is important for security. This matches that change.
    
    The new compiler is discussed here:
    https://blogs.msdn.microsoft.com/vcblog/2016/05/04/new-code-optimizer/
    
    The version packaged up for this change was installed May 16th.
    
    BUG=440500
    
    Review-Url: https://codereview.chromium.org/1959413002
    Cr-Commit-Position: refs/heads/master@{#394031}

--- chrome/app/delay_load_hook_win.cc
+++ chrome/app/delay_load_hook_win.cc
@@ -17,8 +17,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Alternatively referencing the ChromeDelayLoadHook function somehow will
 // cause this declaration of these variables to take preference to the delay
 // load runtime's defaults (in delayimp.lib).
+#if _MSC_FULL_VER >= 190024112
+// Prior to Visual Studio 2015 Update 3, these hooks were non-const.  They were
+// made const to improve security (global, writable function pointers are bad).
+// This #ifdef is needed for testing of VS 2015 Update 3 pre-release and can be
+// removed when we formally switch to Update 3 or higher.
+// TODO(612313): remove the #if when we update toolchains.
+const PfnDliHook __pfnDliNotifyHook2 = ChromeDelayLoadHook;
+const PfnDliHook __pfnDliFailureHook2 = ChromeDelayLoadHook;
+#else
 PfnDliHook __pfnDliNotifyHook2 = ChromeDelayLoadHook;
 PfnDliHook __pfnDliFailureHook2 = ChromeDelayLoadHook;
+#endif
 
 
 namespace {
