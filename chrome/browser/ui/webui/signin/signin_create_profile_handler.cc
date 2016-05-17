a813b1f3211015f8f0ece9c2dffd47f039c369da
mahmadi <mahmadi@chromium.org>
2016-05-17 07:18:05 -0700

    MD User Manager: Updated string for Supervised User confirmation screen.
    before: https://screenshot.googleplex.com/1t2UAqU0C1S
    after: https://screenshot.googleplex.com/4dC1XsQ8eDW
    
    BUG=563722, 610593, 610594
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/1967683004
    Cr-Commit-Position: refs/heads/master@{#394118}

--- chrome/browser/ui/webui/signin/signin_create_profile_handler.cc
+++ chrome/browser/ui/webui/signin/signin_create_profile_handler.cc
@@ -34,7 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/profile_helper.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
+#include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
+#include "chrome/grit/google_chrome_strings.h"
 #include "components/browser_sync/browser/profile_sync_service.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/signin_error_controller.h"
@@ -118,22 +120,16 @@ void SigninCreateProfileHandler::GetLocalizedValues(
   localized_strings->SetString(
       "supervisedUserCreatedTitle",
        l10n_util::GetStringUTF16(IDS_LEGACY_SUPERVISED_USER_CREATED_TITLE));
-  // The first substitution parameter remains to be filled by the page JS.
+  // The first two substitution parameters remain to be filled by the page JS.
   localized_strings->SetString(
-      "supervisedUserCreatedTextPart1",
+      "supervisedUserCreatedText",
       l10n_util::GetStringFUTF16(
-            IDS_SUPERVISED_USER_CREATED_TEXT_PART1,
+            IDS_SUPERVISED_USER_CREATED_TEXT,
             base::ASCIIToUTF16("$1"),
+            base::ASCIIToUTF16("$2"),
             base::ASCIIToUTF16(chrome::kLegacySupervisedUserManagementURL),
             base::ASCIIToUTF16(
                 chrome::kLegacySupervisedUserManagementDisplayURL)));
-  // The first two substitution parameters remain to be filled by the page JS.
-  localized_strings->SetString(
-      "supervisedUserCreatedTextPart2",
-      l10n_util::GetStringFUTF16(
-            IDS_SUPERVISED_USER_CREATED_TEXT_PART2,
-            base::ASCIIToUTF16("$1"),
-            base::ASCIIToUTF16("$2")));
   localized_strings->SetString(
       "exitAndChildlockLabel",
       l10n_util::GetStringUTF16(
