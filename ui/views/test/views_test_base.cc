0e403a429d0411311034ed90547680696f3a1f47
qyearsley <qyearsley@chromium.org>
2016-05-17 11:02:34 -0700

    Reland of land: mus: Add views_mus_interactive_ui_tests target (patchset #1 id:1 of https://codereview.chromium.org/1984403002/ )
    Reason for revert:
    Relanding (this doesn't appear to have caused the compile failure on Win)
    
    Original issue's description:
    > Revert of Reland: mus: Add views_mus_interactive_ui_tests target (patchset #2 id:20001 of https://codereview.chromium.org/1982963002/ )
    >
    > Reason for revert:
    > Speculative revert for compile failure on Win: https://uberchromegw.corp.google.com/i/chromium/builders/Win/builds/43482
    >
    > Original issue's description:
    > > Reland: mus: Add views_mus_interactive_ui_tests target
    > >
    > > Previous attempt had a size_t to int warning on Windows, which was suppressed in chrome's interactive_ui_tests but not this new target. See https://codereview.chromium.org/1972363002/
    > >
    > > * Run widget_interactive_uitests.cc in the new target.
    > > * Added a shared "test_support" target in ui/views/mus.
    > > * Pull ViewsMusTestSuite into its own file so I can share it.
    > > * Also make ViewsTestBase::IsMus static so I can call it before the
    > >   PlatformTestHelper is initialized.
    > > * Disable a bunch of tests that fail on mus (I filed bugs for them).
    > > * Fix an unnecessary size_t to int conversion
    > >
    > > BUG=602036
    > > TEST=views_mus_interactive_ui_tests on mus, interactive_ui_tests elsewhere
    > > TBR=sky@chromium.org, dpranke@chromium.org, sadrul@chromium.org
    > >
    > > Committed: https://crrev.com/ba13ca8e09763be3d4a93ef3d2d495aa806ddd77
    > > Cr-Commit-Position: refs/heads/master@{#394153}
    >
    > TBR=jamescook@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=602036
    >
    > Committed: https://crrev.com/78edbe6407945f0e2c8d1c7dfc873e398150ad45
    > Cr-Commit-Position: refs/heads/master@{#394158}
    
    TBR=jamescook@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=602036
    
    Review-Url: https://codereview.chromium.org/1990453003
    Cr-Commit-Position: refs/heads/master@{#394161}

--- ui/views/test/views_test_base.cc
+++ ui/views/test/views_test_base.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/clipboard.h"
 #include "ui/base/material_design/material_design_controller.h"
 #include "ui/base/test/material_design_controller_test_api.h"
+#include "ui/views/test/platform_test_helper.h"
 
 namespace views {
 
@@ -24,6 +25,11 @@ ViewsTestBase::~ViewsTestBase() {
       << "You have overridden SetUp but never called super class's SetUp";
   CHECK(teardown_called_)
       << "You have overridden TearDown but never called super class's TearDown";
+}
+
+// static
+bool ViewsTestBase::IsMus() {
+  return PlatformTestHelper::IsMus();
 }
 
 void ViewsTestBase::SetUp() {
@@ -70,10 +76,6 @@ void ViewsTestBase::DisableNativeWidgetMus() {
 
 gfx::NativeWindow ViewsTestBase::GetContext() {
   return test_helper_->GetContext();
-}
-
-bool ViewsTestBase::IsMus() const {
-  return test_helper_->IsMus();
 }
 
 }  // namespace views
