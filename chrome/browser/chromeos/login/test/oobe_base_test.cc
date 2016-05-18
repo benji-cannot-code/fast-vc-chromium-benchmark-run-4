3487a298a4aeef8809e0c432be504efdf2bb04f2
amistry <amistry@chromium.org>
2016-05-18 14:38:37 -0700

    Wait for OobeWebUI to finish loading before running tests.
    BUG=612688
    
    Review-Url: https://codereview.chromium.org/1987053003
    Cr-Commit-Position: refs/heads/master@{#394557}

--- chrome/browser/chromeos/login/test/oobe_base_test.cc
+++ chrome/browser/chromeos/login/test/oobe_base_test.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/message_loop/message_loop.h"
 #include "base/path_service.h"
+#include "base/run_loop.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chromeos/login/existing_user_controller.h"
 #include "chrome/browser/chromeos/login/session/user_session_manager.h"
@@ -111,6 +112,13 @@ void OobeBaseTest::SetUpOnMainThread() {
   session_manager_test_api.SetShouldObtainTokenHandleInTests(false);
 
   LoginDisplayHostImpl::DisableRestrictiveProxyCheckForTest();
+
+  // Wait for OobeUI to finish loading.
+  base::RunLoop run_loop;
+  if (!LoginDisplayHost::default_host()->GetOobeUI()->IsJSReady(
+          run_loop.QuitClosure())) {
+    run_loop.Run();
+  }
 
   ExtensionApiTest::SetUpOnMainThread();
 }
