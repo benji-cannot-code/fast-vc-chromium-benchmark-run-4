a5504fb2c13d5f6b05c68ba07d15e56e0e1f793d
yuweih <yuweih@chromium.org>
2016-05-18 15:22:23 -0700

    [Remoting Android Client] Fix issue with stale auth token
    The recent CL 1976853002 got rid of the logic of clearing the auth token cache when authorization fails. It turns out the auth token can still become stale if the user revokes the app authorization on Google security page. And the recent CL makes the app unable to handle this situation.
    
    This CL fixes this problem by clearing the token cache when an auth error occurs. The user will see the error message when they trigger the fetcher for the first time. For the second time, an activity will show up asking for permission.
    
    BUG=546735
    
    Review-Url: https://codereview.chromium.org/1991983002
    Cr-Commit-Position: refs/heads/master@{#394573}

--- remoting/android/java/src/org/chromium/chromoting/OAuthTokenConsumer.java
+++ remoting/android/java/src/org/chromium/chromoting/OAuthTokenConsumer.java
@@ -6,8 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chromoting;
 
 import android.content.Context;
+import android.os.AsyncTask;
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
 
 import org.chromium.chromoting.base.OAuthTokenFetcher;
+
+import java.io.IOException;
 
 /**
  * This helper guards same auth token requesting task shouldn't be run more than once at the same
@@ -38,6 +44,8 @@ public class OAuthTokenConsumer {
      * Each OAuthTokenConsumer is supposed to work for one specific task. It is the caller's
      * responsibility to supply equivalent callbacks (variables being captured can vary) for the
      * same consumer.
+     * If user recoverable exception occurs and |context| is an activity, |callback| will not be
+     * run and onActivityResult() of the activity will be called instead.
      * @param account User's account name (email).
      * @param callback the callback to be called
      * @return true if the consumer will run |callback| when the token is fetched and false
@@ -60,7 +68,9 @@ public class OAuthTokenConsumer {
             @Override
             public void onError(OAuthTokenFetcher.Error error) {
                 mWaitingForAuthToken = false;
-                callback.onError(error);
+                if (error != OAuthTokenFetcher.Error.INTERRUPTED) {
+                    callback.onError(error);
+                }
             }
         }).fetch();
         return true;
@@ -73,5 +83,35 @@ public class OAuthTokenConsumer {
      */
     public String getLatestToken() {
         return mLatestToken;
+    }
+
+    /**
+     * Revokes the latest token fetched by the consumer.
+     * @param callback onTokenFetch(null) will be called if the token is cleared successfully.
+     *                 onError(error) will be called if any error occurs. |callback| can be null,
+     *                 in which case token will be cleared without running the callback.
+     */
+    public void revokeLatestToken(final OAuthTokenFetcher.Callback callback) {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    GoogleAuthUtil.clearToken(mContext, mLatestToken);
+                    mLatestToken = null;
+                    if (callback != null) {
+                        callback.onTokenFetched(null);
+                    }
+                } catch (GoogleAuthException e) {
+                    if (callback != null) {
+                        callback.onError(OAuthTokenFetcher.Error.UNEXPECTED);
+                    }
+                } catch (IOException e) {
+                    if (callback != null) {
+                        callback.onError(OAuthTokenFetcher.Error.NETWORK);
+                    }
+                }
+                return null;
+            }
+        }.execute();
     }
 }
