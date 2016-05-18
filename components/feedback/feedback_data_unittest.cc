6ade90982b23028743cf80a94c04f73bd1f993a2
jyasskin <jyasskin@chromium.org>
2016-05-18 13:19:35 -0700

    Allow constexpr variables of TimeDelta type.
    This allows TimeDelta variables to be global per https://google.github.io/styleguide/cppguide.html#Static_and_Global_Variables since they don't have to run a dynamic initializer anymore.
    
    Review-Url: https://codereview.chromium.org/1976703005
    Cr-Commit-Position: refs/heads/master@{#394527}

--- components/feedback/feedback_data_unittest.cc
+++ components/feedback/feedback_data_unittest.cc
@@ -28,9 +28,6 @@ const char kHistograms[] = "";
 const char kImageData[] = "";
 const char kFileData[] = "";
 
-const base::TimeDelta kRetryDelayForTest =
-    base::TimeDelta::FromMilliseconds(100);
-
 using content::BrowserThread;
 
 class MockUploader : public feedback::FeedbackUploader, public KeyedService {
