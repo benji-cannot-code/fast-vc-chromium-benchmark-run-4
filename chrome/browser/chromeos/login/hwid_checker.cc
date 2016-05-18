4e88fb272ca8305344f1f3707c6845125091e50e
xdai <xdai@chromium.org>
2016-05-18 13:39:26 -0700

    Add log information to see what the system vendor is on the GCE VMs.
    This information will be used to filter out error messages on VMs later.
    
    BUG=585514, 585515
    
    Review-Url: https://codereview.chromium.org/1992733004
    Cr-Commit-Position: refs/heads/master@{#394536}

--- chrome/browser/chromeos/login/hwid_checker.cc
+++ chrome/browser/chromeos/login/hwid_checker.cc
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const char kVMSystemVendor[] = "QEMU";
-
 unsigned CalculateCRC32(const std::string& data) {
   return static_cast<unsigned>(crc32(
       0,
@@ -124,21 +122,20 @@ bool IsMachineHWIDCorrect() {
   chromeos::system::StatisticsProvider* stats =
       chromeos::system::StatisticsProvider::GetInstance();
 
-  std::string system_vendor;
-  if (stats->GetMachineStatistic(chromeos::system::kSystemVendorKey,
-                                 &system_vendor) &&
-      system_vendor == kVMSystemVendor) {
-    // We are running in a VM.
-    return true;
-  }
-
   std::string hwid;
   if (!stats->GetMachineStatistic(chromeos::system::kHardwareClassKey, &hwid)) {
     LOG(ERROR) << "Couldn't get machine statistic 'hardware_class'.";
     return false;
   }
   if (!chromeos::IsHWIDCorrect(hwid)) {
-    LOG(ERROR) << "Machine has malformed HWID '" << hwid << "'.";
+    // Log the system vendor info to see what the system vendor is on the GCE
+    // VMs. This info will be used to filter out error messages on VMs. See
+    // http://crbug.com/585514 and http://crbug.com/585515 for more info.
+    std::string system_vendor;
+    stats->GetMachineStatistic(chromeos::system::kSystemVendorKey,
+                               &system_vendor);
+    LOG(ERROR) << "Machine has malformed HWID '" << hwid << "'. "
+               << "The system vendor is '" << system_vendor << "'.";
     return false;
   }
   return true;
