2274c13c9830c25c00c680aaf83cccdf944d1327
cernekee <cernekee@chromium.org>
2016-05-18 13:03:11 -0700

    Add indication of camera use on SAML webcam logins
    SAML logins on Chrome OS currently display a banner showing the
    "authDomain" of the sign-in service.  If the sign-in service tries to
    enable the user's webcam, change this banner to warn the user by
    adding "is using your camera" verbiage and a red "recording" dot.
    
    BUG=606979
    TEST=manual
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/1966853002
    Cr-Commit-Position: refs/heads/master@{#394519}

--- chrome/browser/resources/gaia_auth_host/authenticator.js
+++ chrome/browser/resources/gaia_auth_host/authenticator.js
@@ -131,6 +131,7 @@ cr.define('cr.login', function() {
     this.skipForNow_ = false;
     this.authFlow = AuthFlow.DEFAULT;
     this.authDomain = '';
+    this.videoEnabled = false;
     this.idpOrigin_ = null;
     this.continueUrl_ = null;
     this.continueUrlWithoutParams_ = null;
@@ -159,6 +160,9 @@ cr.define('cr.login', function() {
     this.samlHandler_.addEventListener(
         'authPageLoaded',
         this.onAuthPageLoaded_.bind(this));
+    this.samlHandler_.addEventListener(
+        'videoEnabled',
+        this.onVideoEnabled_.bind(this));
 
     this.webview_.addEventListener('droplink', this.onDropLink_.bind(this));
     this.webview_.addEventListener(
@@ -209,6 +213,7 @@ cr.define('cr.login', function() {
     this.trusted_ = true;
     this.authFlow = AuthFlow.DEFAULT;
     this.samlHandler_.reset();
+    this.videoEnabled = false;
   };
 
   /**
@@ -734,6 +739,14 @@ cr.define('cr.login', function() {
   };
 
   /**
+   * Invoked when |samlHandler_| fires 'videoEnabled' event.
+   * @private
+   */
+  Authenticator.prototype.onVideoEnabled_ = function(e) {
+    this.videoEnabled = true;
+  };
+
+  /**
    * Invoked when a link is dropped on the webview.
    * @private
    */
@@ -840,6 +853,12 @@ cr.define('cr.login', function() {
    * @type {string}
    */
   cr.defineProperty(Authenticator, 'authDomain');
+
+  /**
+   * True if the page has requested media access.
+   * @type {boolean}
+   */
+  cr.defineProperty(Authenticator, 'videoEnabled');
 
   Authenticator.AuthFlow = AuthFlow;
   Authenticator.AuthMode = AuthMode;
