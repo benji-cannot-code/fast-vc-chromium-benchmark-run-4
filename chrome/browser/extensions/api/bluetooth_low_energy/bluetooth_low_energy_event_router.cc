137c92de58f386adef82aa5ee744f06ab632b866
rkc <rkc@chromium.org>
2016-05-17 17:28:21 -0700

    Add implementation for parsing device info in read/write attribute calls.
    This CL adds the actual DBus implementation for parsing the device information
    from the options dictionaries now passed up from BlueZ with the GATT
    attribute value read/write request calls. It also plumbs the device
    information up to the Chrome app getting these requests.
    
    R=xiyuan@chromium.org
    BUG=611862
    
    Review-Url: https://codereview.chromium.org/1984723006
    Cr-Commit-Position: refs/heads/master@{#394282}

--- chrome/browser/extensions/api/bluetooth_low_energy/bluetooth_low_energy_event_router.cc
+++ chrome/browser/extensions/api/bluetooth_low_energy/bluetooth_low_energy_event_router.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/api/bluetooth_low_energy/bluetooth_low_energy_event_router.h"
 
+#include <algorithm>
 #include <iterator>
 #include <utility>
 
@@ -13,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/hash_tables.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "base/strings/utf_string_conversions.h"
+#include "base/values.h"
 #include "chrome/browser/extensions/api/bluetooth_low_energy/bluetooth_low_energy_connection.h"
 #include "chrome/browser/extensions/api/bluetooth_low_energy/bluetooth_low_energy_notify_session.h"
 #include "chrome/browser/extensions/api/bluetooth_low_energy/utils.h"
@@ -28,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/api/api_resource_manager.h"
 #include "extensions/browser/event_listener_map.h"
 #include "extensions/browser/event_router.h"
+#include "extensions/browser/extension_registry.h"
 #include "extensions/common/api/bluetooth/bluetooth_manifest_data.h"
 #include "extensions/common/extension.h"
 
@@ -138,6 +142,16 @@ void PopulateDescriptor(const BluetoothRemoteGattDescriptor* descriptor,
     return;
 
   out->value.reset(new std::vector<char>(value.begin(), value.end()));
+}
+
+void PopulateDevice(const device::BluetoothDevice* device,
+                    apibtle::Request* request) {
+  if (!device)
+    return;
+  request->device.address = device->GetAddress();
+  request->device.name.reset(
+      new std::string(base::UTF16ToUTF8(device->GetName())));
+  request->device.device_class.reset(new int(device->GetBluetoothClass()));
 }
 
 typedef extensions::ApiResourceManager<extensions::BluetoothLowEnergyConnection>
@@ -1110,6 +1124,7 @@ void BluetoothLowEnergyEventRouter::OnCharacteristicReadRequest(
   request.request_id =
       StoreSentRequest(extension_id, base::WrapUnique(new AttributeValueRequest(
                                          value_callback, error_callback)));
+  PopulateDevice(device, &request);
   DispatchEventToExtension(
       extension_id, events::BLUETOOTH_LOW_ENERGY_ON_CHARACTERISTIC_READ_REQUEST,
       apibtle::OnCharacteristicReadRequest::kEventName,
@@ -1140,6 +1155,7 @@ void BluetoothLowEnergyEventRouter::OnCharacteristicWriteRequest(
       base::WrapUnique(new AttributeValueRequest(callback, error_callback)));
   request.value =
       base::WrapUnique(new std::vector<char>(value.begin(), value.end()));
+  PopulateDevice(device, &request);
   DispatchEventToExtension(
       extension_id,
       events::BLUETOOTH_LOW_ENERGY_ON_CHARACTERISTIC_WRITE_REQUEST,
@@ -1169,6 +1185,7 @@ void BluetoothLowEnergyEventRouter::OnDescriptorReadRequest(
   request.request_id =
       StoreSentRequest(extension_id, base::WrapUnique(new AttributeValueRequest(
                                          value_callback, error_callback)));
+  PopulateDevice(device, &request);
   DispatchEventToExtension(
       extension_id,
       events::BLUETOOTH_LOW_ENERGY_ON_CHARACTERISTIC_WRITE_REQUEST,
@@ -1201,6 +1218,7 @@ void BluetoothLowEnergyEventRouter::OnDescriptorWriteRequest(
       base::WrapUnique(new AttributeValueRequest(callback, error_callback)));
   request.value =
       base::WrapUnique(new std::vector<char>(value.begin(), value.end()));
+  PopulateDevice(device, &request);
   DispatchEventToExtension(
       extension_id,
       events::BLUETOOTH_LOW_ENERGY_ON_CHARACTERISTIC_WRITE_REQUEST,
