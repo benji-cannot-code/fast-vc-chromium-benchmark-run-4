bbfbefdecd195feeffcdc15599c43e03ab857ece
thakis <thakis@chromium.org>
2016-05-18 10:25:57 -0700

    Revert of [iOS/GN] Fix and enable "gn check" for "//ios/*" and "//ios_internal/*". (patchset #2 id:20001 of https://codereview.chromium.org/1984353002/ )
    Reason for revert:
    breaks the cq for everyone (e.g. https://build.chromium.org/p/tryserver.chromium.mac/builders/ios-simulator-gn/builds/7843/steps/generate_build_files%20%28without%20patch%29/logs/stdio)
    
    Original issue's description:
    > [iOS/GN] Fix and enable "gn check" for "//ios/*" and "//ios_internal/*".
    >
    > Add missing dependencies, add circular include exception for targets that
    > are conceptually a single target but split for practical reason, move some
    > files and remove obsolete includes to fix "gn check".
    >
    > Enable "gn check" for "//ios/*" and "//ios_internal/*".
    >
    > R=dpranke@chromium.org
    > TBR=brettw@chromium.org
    > BUG=None
    >
    > Committed: https://crrev.com/9852b77afa1a1b3875a73a091e94f88865c4e78e
    > Cr-Commit-Position: refs/heads/master@{#394428}
    
    TBR=dpranke@google.com,ben@chromium.org,brettw@chromium.org,dpranke@chromium.org,sdefresne@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=None
    
    Review-Url: https://codereview.chromium.org/1988203002
    Cr-Commit-Position: refs/heads/master@{#394458}

--- ios/chrome/browser/ios_chrome_main_parts.mm
+++ ios/chrome/browser/ios_chrome_main_parts.mm
@@ -52,8 +52,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 
 #if defined(ENABLE_RLZ)
-#include "components/rlz/rlz_tracker.h"                        // nogncheck
-#include "ios/chrome/browser/rlz/rlz_tracker_delegate_impl.h"  // nogncheck
+#include "components/rlz/rlz_tracker.h"
+#include "ios/chrome/browser/rlz/rlz_tracker_delegate_impl.h"
 #endif
 
 IOSChromeMainParts::IOSChromeMainParts(
--- ios/chrome/browser/search_engines/ui_thread_search_terms_data.cc
+++ ios/chrome/browser/search_engines/ui_thread_search_terms_data.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 #if defined(ENABLE_RLZ)
-#include "components/rlz/rlz_tracker.h"  // nogncheck
+#include "components/rlz/rlz_tracker.h"
 #endif
 
 namespace ios {
