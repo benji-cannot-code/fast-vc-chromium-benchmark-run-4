f43ba8fee8465677d88aea4c21fa0026f1f0e4a1
calamity <calamity@chromium.org>
2016-05-18 20:58:44 -0700

    [MD History] Make history-list:historyData private.
    BUG=589357
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/1972073002
    Cr-Commit-Position: refs/heads/master@{#394660}

--- chrome/test/data/webui/md_history/history_toolbar_test.js
+++ chrome/test/data/webui/md_history/history_toolbar_test.js
@@ -67,7 +67,7 @@ cr.define('md_history.history_toolbar_test', function() {
       });
 
       teardown(function() {
-        element.historyData = [];
+        element.historyData_ = [];
         element.searchedTerm = '';
         registerMessageCallback('queryHistory', this, undefined);
         toolbar.count = 0;
