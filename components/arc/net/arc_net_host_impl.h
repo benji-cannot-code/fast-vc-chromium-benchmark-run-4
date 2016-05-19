3411db497483ab1d71fa2706ce6c4214a1a85e57
cernekee <cernekee@chromium.org>
2016-05-19 12:38:52 -0700

    Notify ARC of default network changes
    Track the default logical service (i.e. VPN or physical interface) and
    notify ARC when it changes.  Allow ARC to query the default service
    at startup time.
    
    This reports IP information, MAC address, wifi parameters, etc.
    
    BUG=b/26495433
    
    Review-Url: https://codereview.chromium.org/1925083003
    Cr-Commit-Position: refs/heads/master@{#394836}

--- components/arc/net/arc_net_host_impl.h
+++ components/arc/net/arc_net_host_impl.h
@@ -12,12 +12,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/scoped_file.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
 #include "chromeos/network/network_state_handler_observer.h"
 #include "components/arc/arc_bridge_service.h"
 #include "components/arc/arc_service.h"
 #include "components/arc/common/net.mojom.h"
 #include "mojo/public/cpp/bindings/binding.h"
+
+namespace base {
+
+class DictionaryValue;
+
+}  // namespace base
 
 namespace arc {
 
@@ -67,13 +74,18 @@ class ArcNetHostImpl : public ArcService,
   // Overriden from chromeos::NetworkStateHandlerObserver.
   void ScanCompleted(const chromeos::DeviceState* /*unused*/) override;
   void OnShuttingDown() override;
+  void DefaultNetworkChanged(const chromeos::NetworkState* network) override;
+  void GetDefaultNetwork(const GetDefaultNetworkCallback& callback) override;
 
   // Overridden from ArcBridgeService::Observer:
   void OnNetInstanceReady() override;
 
  private:
+  void DefaultNetworkSuccessCallback(const std::string& service_path,
+                                     const base::DictionaryValue& dictionary);
   base::ThreadChecker thread_checker_;
   mojo::Binding<arc::mojom::NetHost> binding_;
+  base::WeakPtrFactory<ArcNetHostImpl> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ArcNetHostImpl);
 };
