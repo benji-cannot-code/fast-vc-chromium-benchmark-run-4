2e3a8d189b02d3659d7b8c99ab4da365c9b78401
dominickn <dominickn@chromium.org>
2016-05-19 01:53:33 -0700

    Persist prompt/block download limiter state on renderer-initiated loads.
    The download request limiter will reset its state to allow a download
    when it receives a notification of a pending navigation. This ensures
    that the newly loaded page is always allowed to download one file. The
    reset does not occur if the current state allows or blocks all downloads
    AND the navigation is to the same host.
    
    This means that a site can avoid prompting the user for multiple
    downloads as follows:
     1. trigger a download (moves state from ALLOW_ONE to PROMPT)
     2. change window.location.href, creating a navigation and
        resetting the limiter state to ALLOW_ONE
    
    Sites can repeatedly loop and trigger downloads in a new pop-up window,
    or use meta refresh tags to "ping-pong" between pages, downloading a
    new file on each page. Because the limiter is in the PROMPT state when
    the pending navigation arrives, the state is reset and the next download
    is always permitted.
    
    This CL closes the loophole by making the limiter maintain state
    following a renderer-initiated navigation, unless the state is allowing
    one or all downloads. This ensures that navigations which are not
    triggered by the user cannot be used to reset the limiter to allowing
    downloads if the state is either blocking downloads or prompting the
    user to permit downloads. Both carpet bomb attacks described above
    are mitigated in the CL.
    
    A new IsRendererInitiated method is added to
    content::NavigationHandle to enable this functionality. The limiter now
    overrides WebContentsObserver::DidStartNavigation so it can access
    the handle.
    
    BUG=559515
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/1964863002
    Cr-Commit-Position: refs/heads/master@{#394721}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/frame_host/frame_mojo_shell.h"
 #include "content/browser/frame_host/frame_tree.h"
 #include "content/browser/frame_host/frame_tree_node.h"
+#include "content/browser/frame_host/navigation_entry_impl.h"
 #include "content/browser/frame_host/navigation_handle_impl.h"
 #include "content/browser/frame_host/navigation_request.h"
 #include "content/browser/frame_host/navigator.h"
@@ -1094,12 +1095,14 @@ void RenderFrameHostImpl::OnDidCommitProvisionalLoad(const IPC::Message& msg) {
   // for these navigations should go away when this is properly handled. See
   // crbug.com/588317.
   int entry_id_for_data_nav = 0;
+  bool is_renderer_initiated = true;
   if (navigation_handle_ &&
       (navigation_handle_->GetURL() != validated_params.url)) {
     // Make sure that the pending entry was really loaded via
     // LoadDataWithBaseURL and that it matches this handle.
-    NavigationEntry* pending_entry =
-        frame_tree_node()->navigator()->GetController()->GetPendingEntry();
+    NavigationEntryImpl* pending_entry =
+        NavigationEntryImpl::FromNavigationEntry(
+            frame_tree_node()->navigator()->GetController()->GetPendingEntry());
     bool pending_entry_matches_handle =
         pending_entry &&
         pending_entry->GetUniqueID() ==
@@ -1111,6 +1114,7 @@ void RenderFrameHostImpl::OnDidCommitProvisionalLoad(const IPC::Message& msg) {
         pending_entry_matches_handle &&
         !pending_entry->GetBaseURLForDataURL().is_empty()) {
       entry_id_for_data_nav = navigation_handle_->pending_nav_entry_id();
+      is_renderer_initiated = pending_entry->is_renderer_initiated();
     }
     navigation_handle_.reset();
   }
@@ -1124,6 +1128,7 @@ void RenderFrameHostImpl::OnDidCommitProvisionalLoad(const IPC::Message& msg) {
     // loaded via LoadDataWithBaseURL, propogate the entry id.
     navigation_handle_ = NavigationHandleImpl::Create(
         validated_params.url, frame_tree_node_,
+        is_renderer_initiated,
         true,  // is_synchronous
         validated_params.is_srcdoc, base::TimeTicks::Now(),
         entry_id_for_data_nav);
