9468d1168e7926c3a2ad44e34b46e642fcfe80e4
srirama.m <srirama.m@samsung.com>
2016-05-19 09:25:44 -0700

    media: Replace wtf/Assertions.h macros in favor of base/logging.h macros
    Replace WTF_LOG macros with DVLOG in HTMLSourceElement.cpp.
    
    BUG=596522
    
    Review-Url: https://codereview.chromium.org/1991043003
    Cr-Commit-Position: refs/heads/master@{#394788}

--- third_party/WebKit/Source/core/html/HTMLSourceElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLSourceElement.cpp
@@ -37,6 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLPictureElement.h"
 #include "platform/Logging.h"
 
+#define SOURCE_LOG_LEVEL 3
+
 namespace blink {
 
 using namespace HTMLNames;
@@ -70,7 +72,7 @@ inline HTMLSourceElement::HTMLSourceElement(Document& document)
     : HTMLElement(sourceTag, document)
     , m_listener(new Listener(this))
 {
-    WTF_LOG(Media, "HTMLSourceElement::HTMLSourceElement - %p", this);
+    DVLOG(SOURCE_LOG_LEVEL) << "HTMLSourceElement - " << (void*)this;
 }
 
 DEFINE_NODE_FACTORY(HTMLSourceElement)
@@ -137,20 +139,20 @@ void HTMLSourceElement::setType(const AtomicString& type)
 
 void HTMLSourceElement::scheduleErrorEvent()
 {
-    WTF_LOG(Media, "HTMLSourceElement::scheduleErrorEvent - %p", this);
+    DVLOG(SOURCE_LOG_LEVEL) << "scheduleErrorEvent - " << (void*)this;
     sourceErrorEventSender().dispatchEventSoon(this);
 }
 
 void HTMLSourceElement::cancelPendingErrorEvent()
 {
-    WTF_LOG(Media, "HTMLSourceElement::cancelPendingErrorEvent - %p", this);
+    DVLOG(SOURCE_LOG_LEVEL) << "cancelPendingErrorEvent - " << (void*)this;
     sourceErrorEventSender().cancelEvent(this);
 }
 
 void HTMLSourceElement::dispatchPendingEvent(SourceEventSender* eventSender)
 {
     ASSERT_UNUSED(eventSender, eventSender == &sourceErrorEventSender());
-    WTF_LOG(Media, "HTMLSourceElement::dispatchPendingEvent - %p", this);
+    DVLOG(SOURCE_LOG_LEVEL) << "dispatchPendingEvent - " << (void*)this;
     dispatchEvent(Event::createCancelable(EventTypeNames::error));
 }
 
