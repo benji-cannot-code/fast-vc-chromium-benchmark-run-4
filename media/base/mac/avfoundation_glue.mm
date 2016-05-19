9210bde11c45335dd224cff7a0a417ccf9969a1c
pkasting <pkasting@chromium.org>
2016-05-19 11:17:23 -0700

    Fix "unused variable" warnings.
    These were exposed by changing DISALLOW_COPY_AND_ASSIGN to use "= delete", and
    must be fixed before that change can land.
    
    See https://codereview.chromium.org/1981053002/ for the full CL.
    
    BUG=447156
    TEST=none
    
    Review-Url: https://codereview.chromium.org/1993173002
    Cr-Commit-Position: refs/heads/master@{#394816}

--- media/base/mac/avfoundation_glue.mm
+++ media/base/mac/avfoundation_glue.mm
@@ -56,7 +56,6 @@ class AVFoundationInternal {
 
  private:
   NSBundle* bundle_;
-  void* library_handle_;
   // The following members are replicas of the respectives in AVFoundation.
   NSString* AVCaptureDeviceWasConnectedNotification_ =
       ::AVCaptureDeviceWasConnectedNotification;
