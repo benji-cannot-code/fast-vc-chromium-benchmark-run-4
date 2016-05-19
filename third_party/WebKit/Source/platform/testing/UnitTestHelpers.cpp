fd4c9198abc7beb815eb98e35dbf2e743eb36e5b
yhirano <yhirano@chromium.org>
2016-05-19 05:00:30 -0700

    Forbid Oilpan GC in blink::testing::runPendingTasks
    As webkit unittests are run outside of the message loop, tasks run in
    blink::testing::runPendingTasks are executed as non-nested tasks. That means
    the registered GCTaskObserver will run GC with NoHeapPointerOnStack. This is
    problematic because we often have heap pointers on stack.
    
    This change forbid Oilpan GC in runPendingTasks to avoid such a problem.
    
    BUG=613115
    
    Review-Url: https://codereview.chromium.org/1995733003
    Cr-Commit-Position: refs/heads/master@{#394743}

--- third_party/WebKit/Source/platform/testing/UnitTestHelpers.cpp
+++ third_party/WebKit/Source/platform/testing/UnitTestHelpers.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "platform/SharedBuffer.h"
 #include "platform/Timer.h"
+#include "platform/heap/Handle.h"
 #include "public/platform/FilePathConversion.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebString.h"
@@ -46,7 +47,12 @@ namespace testing {
 void runPendingTasks()
 {
     Platform::current()->currentThread()->getWebTaskRunner()->postTask(BLINK_FROM_HERE, bind(&exitRunLoop));
+
+    // We forbid GC in the tasks. Otherwise the registered GCTaskObserver tries
+    // to run GC with NoHeapPointerOnStack.
+    ThreadState::current()->enterGCForbiddenScope();
     enterRunLoop();
+    ThreadState::current()->leaveGCForbiddenScope();
 }
 
 void runDelayedTasks(double delayMs)
