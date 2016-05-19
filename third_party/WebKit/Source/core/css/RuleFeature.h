522eae950d390a8fb2b34105afeb202bd5ac5f7f
ericwilligers <ericwilligers@chromium.org>
2016-05-19 00:44:37 -0700

    CSS: Move forward VectorTraits specialization of RuleFeatureSet
    Declare the VectorTraits specialization for RuleFeature before
    any vectors of RuleFeature are declared.
    
    R=haraken@chromium.org
    BUG=595218
    
    Review-Url: https://codereview.chromium.org/1995943002
    Cr-Commit-Position: refs/heads/master@{#394708}

--- third_party/WebKit/Source/core/css/RuleFeature.h
+++ third_party/WebKit/Source/core/css/RuleFeature.h
@@ -51,6 +51,14 @@ public:
     bool hasDocumentSecurityOrigin;
 };
 
+} // namespace blink
+
+// Declare the VectorTraits specialization before RuleFeatureSet
+// declares its vector members below.
+WTF_ALLOW_MOVE_AND_INIT_WITH_MEM_FUNCTIONS(blink::RuleFeature);
+
+namespace blink {
+
 class CORE_EXPORT RuleFeatureSet {
     DISALLOW_NEW();
 public:
@@ -171,7 +179,5 @@ private:
 };
 
 } // namespace blink
-
-WTF_ALLOW_MOVE_AND_INIT_WITH_MEM_FUNCTIONS(blink::RuleFeature);
 
 #endif // RuleFeature_h
