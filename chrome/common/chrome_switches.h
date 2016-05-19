c6f4d36208653ed0c7771dc890d6f3691ea33533
peter <peter@chromium.org>
2016-05-19 09:25:21 -0700

    Add a command line to allow silent push for local usage
    BUG=
    
    Review-Url: https://codereview.chromium.org/1998663002
    Cr-Commit-Position: refs/heads/master@{#394787}

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -32,6 +32,7 @@ extern const char kAllowHttpScreenCapture[];
 extern const char kAllowInsecureLocalhost[];
 extern const char kAllowOutdatedPlugins[];
 extern const char kAllowRunningInsecureContent[];
+extern const char kAllowSilentPush[];
 extern const char kAlwaysAuthorizePlugins[];
 extern const char kAppId[];
 extern const char kApp[];
