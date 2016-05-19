82b25bd6f1afd76c0aa0a641b4aad93e8c576b3f
tzik <tzik@chromium.org>
2016-05-19 03:35:05 -0700

    Fix CDB search path in Windows LayoutTest tools
    WinSDK tools path has changed in depot_tools from depot_tools/win_toolchain/vs2013_files/win_sdk
    to depot_tools/win_toolchain/vs_files/<hash>/win_sdk.
    That causes local LayoutTest failure.
    
    This CL updates the path to support the latest toolchain setup.
    
    Review-Url: https://codereview.chromium.org/1990843003
    Cr-Commit-Position: refs/heads/master@{#394727}

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/breakpad/dump_reader_win.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/breakpad/dump_reader_win.py
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import errno
 import logging
 import os
 import shlex
@@ -116,11 +117,17 @@ class DumpReaderWin(DumpReader):
         # Look in depot_tools win_toolchain too.
         depot_tools = self._find_depot_tools_path()
         if depot_tools:
-            win_sdk = os.path.join(depot_tools, 'win_toolchain', 'vs2013_files', 'win_sdk')
-            possible_cdb_locations.extend([
-                '%s\\Debuggers\\x86' % win_sdk,
-                '%s\\Debuggers\\x64' % win_sdk,
-            ])
+            try:
+                vs_files = os.path.join(depot_tools, 'win_toolchain', 'vs_files')
+                for path in os.listdir(vs_files):
+                    win_sdk = os.path.join(vs_files, path, 'win_sdk')
+                    possible_cdb_locations.extend([
+                        '%s\\Debuggers\\x86' % win_sdk,
+                        '%s\\Debuggers\\x64' % win_sdk,
+                    ])
+            except OSError as e:
+                if e.errno != errno.ENOENT:
+                    raise
 
         for cdb_path in possible_cdb_locations:
             cdb = self._host.filesystem.join(cdb_path, 'cdb.exe')
