ac2a5b43163b2e795ab3922c556be2709679c4c6
wjmaclean <wjmaclean@chromium.org>
2016-05-19 09:40:37 -0700

    Remove unused forward declarations.
    TBR=creis@chromium.org
    BUG=none
    
    Review-Url: https://codereview.chromium.org/1991213002
    Cr-Commit-Position: refs/heads/master@{#394793}

--- content/browser/loader/resource_message_filter.h
+++ content/browser/loader/resource_message_filter.h
@@ -27,8 +27,6 @@ class URLRequestContext;
 namespace content {
 class ChromeAppCacheService;
 class ChromeBlobStorageContext;
-class HostZoomLevelContext;
-class HostZoomMap;
 class ResourceContext;
 class ServiceWorkerContextWrapper;
 
