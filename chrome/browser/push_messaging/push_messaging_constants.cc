206587ad7adebd92b7082a4d5f1513aba5a7c83b
peter <peter@chromium.org>
2016-05-19 05:28:43 -0700

    Ship Web Push subscription restrictions
    This exposes the "applicationServerKey" property when subscribing with
    the Push API, in which the developer can provide their application
    server's key per draft-ietf-webpush-vapid.
    
    https://tools.ietf.org/html/draft-ietf-webpush-vapid
    
    When doing so, Chrome will give them an endpoint for the subscription
    that supports the Web Push Protocol rather than the proprietary GCM
    protocol. The endpoint is still provided by GCM.
    
    Intent to Ship:
    
    https://groups.google.com/a/chromium.org/d/topic/blink-dev/Wo7fimlUUjc/discussion
    
    BUG=583753
    
    Review-Url: https://codereview.chromium.org/1931843002
    Cr-Commit-Position: refs/heads/master@{#394747}

--- chrome/browser/push_messaging/push_messaging_constants.cc
+++ chrome/browser/push_messaging/push_messaging_constants.cc
@@ -5,7 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/push_messaging/push_messaging_constants.h"
 
-const char kPushMessagingEndpoint[] = "https://android.googleapis.com/gcm/send";
+const char kPushMessagingGcmEndpoint[] =
+    "https://android.googleapis.com/gcm/send/";
+
+const char kPushMessagingPushProtocolEndpoint[] =
+    "https://fcm.googleapis.com/fcm/send/";
 
 const char kPushMessagingForcedNotificationTag[] =
     "user_visible_auto_notification";
--- chrome/browser/push_messaging/push_messaging_constants.h
+++ chrome/browser/push_messaging/push_messaging_constants.h
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PUSH_MESSAGING_PUSH_MESSAGING_CONSTANTS_H_
 #define CHROME_BROWSER_PUSH_MESSAGING_PUSH_MESSAGING_CONSTANTS_H_
 
-extern const char kPushMessagingEndpoint[];
+extern const char kPushMessagingGcmEndpoint[];
+extern const char kPushMessagingPushProtocolEndpoint[];
 
 // The tag of the notification that will be automatically shown if a webapp
 // receives a push message then fails to show a notification.
