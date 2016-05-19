d6f6018dec25e15ffd02b8cf665e825aa7015a28
wangxianzhu <wangxianzhu@chromium.org>
2016-05-18 22:54:47 -0700

    Optimize paint invalidation when table cell container's background changes
    Tracing showed that for PerformanceTests/Paint/large-table-background-change-with-invisible-collapsed-borders.html
    in each frame cycle update about 20% time is for paint invalidation in
    LayoutTable::invalidatePaintOfSubtreesIfNeeded() to check cell
    background.
    
    Optimize to reduce paint invalidation time by marking
    backgroundChangedSinceLastPaintInvalidation flag on table colgroups,
    cols, sections and rows.
    
    With this change, paint invalidation time can reduce to about 1% of
    a frame cycle update for the test.
    
    BUG=550044
    TEST=fast/repaint/table-col-background*.html
    TEST=fast/repaint/table-row-bg-change.html
    TEST=etc.
    
    Review-Url: https://codereview.chromium.org/1977733002
    Cr-Commit-Position: refs/heads/master@{#394691}

--- third_party/WebKit/Source/core/layout/LayoutTableBoxComponent.cpp
+++ third_party/WebKit/Source/core/layout/LayoutTableBoxComponent.cpp
@@ -9,9 +9,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void LayoutTableBoxComponent::imageChanged(WrappedImagePtr image, const IntRect* rect)
+void LayoutTableBoxComponent::styleDidChange(StyleDifference diff, const ComputedStyle* oldStyle)
+{
+    LayoutBox::styleDidChange(diff, oldStyle);
+
+    if (parent() && oldStyle) {
+        if (resolveColor(*oldStyle, CSSPropertyBackgroundColor) != resolveColor(CSSPropertyBackgroundColor)
+            || oldStyle->backgroundLayers() != styleRef().backgroundLayers())
+            m_backgroundChangedSinceLastPaintInvalidation = true;
+    }
+}
+
+void LayoutTableBoxComponent::imageChanged(WrappedImagePtr, const IntRect*)
 {
     setShouldDoFullPaintInvalidation();
+    m_backgroundChangedSinceLastPaintInvalidation = true;
 }
 
 } // namespace blink
