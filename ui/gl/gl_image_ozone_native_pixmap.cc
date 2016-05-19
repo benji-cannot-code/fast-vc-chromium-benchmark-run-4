3c8cb3e50bd3a5e95eb3bc01bc442e9b16f3a4e4
reveman <reveman@chromium.org>
2016-05-19 08:44:06 -0700

    ui: Native support for BGR_565 buffer format on Ozone.
    Adds native GPU_READ support for BGR_565 buffers on Ozone. Also
    removes incorrect SCANOUT support for non-BGRX_8888 formats.
    
    BUG=538325
    TEST=gl_unittest --gtest_filter=GLImage*
    
    Review-Url: https://codereview.chromium.org/1988553003
    Cr-Commit-Position: refs/heads/master@{#394779}

--- ui/gl/gl_image_ozone_native_pixmap.cc
+++ ui/gl/gl_image_ozone_native_pixmap.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
    (static_cast<uint32_t>(c) << 16) | (static_cast<uint32_t>(d) << 24))
 
 #define DRM_FORMAT_R8 FOURCC('R', '8', ' ', ' ')
+#define DRM_FORMAT_RGB565 FOURCC('R', 'G', '1', '6')
 #define DRM_FORMAT_ARGB8888 FOURCC('A', 'R', '2', '4')
 #define DRM_FORMAT_ABGR8888 FOURCC('A', 'B', '2', '4')
 #define DRM_FORMAT_XRGB8888 FOURCC('X', 'R', '2', '4')
@@ -33,6 +34,7 @@ bool ValidInternalFormat(unsigned internalformat) {
 bool ValidFormat(BufferFormat format) {
   switch (format) {
     case BufferFormat::R_8:
+    case BufferFormat::BGR_565:
     case BufferFormat::RGBA_8888:
     case BufferFormat::RGBX_8888:
     case BufferFormat::BGRA_8888:
@@ -43,7 +45,6 @@ bool ValidFormat(BufferFormat format) {
     case BufferFormat::DXT1:
     case BufferFormat::DXT5:
     case BufferFormat::ETC1:
-    case BufferFormat::BGR_565:
     case BufferFormat::RGBA_4444:
     case BufferFormat::YUV_420:
     case BufferFormat::YUV_420_BIPLANAR:
@@ -59,6 +60,8 @@ EGLint FourCC(BufferFormat format) {
   switch (format) {
     case BufferFormat::R_8:
       return DRM_FORMAT_R8;
+    case BufferFormat::BGR_565:
+      return DRM_FORMAT_RGB565;
     case BufferFormat::RGBA_8888:
       return DRM_FORMAT_ABGR8888;
     case BufferFormat::RGBX_8888:
@@ -72,7 +75,6 @@ EGLint FourCC(BufferFormat format) {
     case BufferFormat::DXT1:
     case BufferFormat::DXT5:
     case BufferFormat::ETC1:
-    case BufferFormat::BGR_565:
     case BufferFormat::RGBA_4444:
     case BufferFormat::YUV_420:
     case BufferFormat::YUV_420_BIPLANAR:
