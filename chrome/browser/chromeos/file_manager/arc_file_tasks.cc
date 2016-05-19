2097b4f4102407e0f620608c28f86a70449c1756
kinaba <kinaba@chromium.org>
2016-05-18 21:28:37 -0700

    Files app: Execution for ARC handler should pass full component name.
    Our previous IPC only passed the package name, but it is not sufficient
    to disambiguate for packages providing multiple handler activities.
    
    For not requiring user to do disambiguation both in and outside ARC,
    we need a new IPC to pass the full component name including activity name.
    
    BUG=607059
    BUG=b/28800370
    
    Review-Url: https://codereview.chromium.org/1991743002
    Cr-Commit-Position: refs/heads/master@{#394672}

--- chrome/browser/chromeos/file_manager/arc_file_tasks.cc
+++ chrome/browser/chromeos/file_manager/arc_file_tasks.cc
@@ -33,6 +33,8 @@ namespace file_tasks {
 namespace {
 
 constexpr int kArcInstanceHelperVersionWithUrlListSupport = 4;
+constexpr int kArcInstanceHelperVersionWithFullActivityName = 5;
+
 constexpr base::FilePath::CharType kArcDownloadPath[] =
     FILE_PATH_LITERAL("/sdcard/Download");
 constexpr char kAppIdSeparator = '/';
@@ -214,9 +216,16 @@ bool ExecuteArcTask(Profile* profile,
     urls.push_back(std::move(url_with_type));
   }
 
-  arc_intent_helper->HandleUrlList(
-      std::move(urls), AppIdToActivityName(task.app_id)->package_name,
-      StringToArcAction(task.action_id));
+  if (GetArcIntentHelper(profile,
+                         kArcInstanceHelperVersionWithFullActivityName)) {
+    arc_intent_helper->HandleUrlList(std::move(urls),
+                                     AppIdToActivityName(task.app_id),
+                                     StringToArcAction(task.action_id));
+  } else {
+    arc_intent_helper->HandleUrlListDeprecated(
+        std::move(urls), AppIdToActivityName(task.app_id)->package_name,
+        StringToArcAction(task.action_id));
+  }
   return true;
 }
 
