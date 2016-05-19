a0d3f96a24b7aa5e78a1b3d80d6d14868ced5775
kojii <kojii@chromium.org>
2016-05-19 09:30:04 -0700

    Compute correct min-content width when hyphenation is enabled
    This patch computes the correct min-content width[1] when hyphenation
    makes it shorter.
    
    The basic logic is as same as the one removed in [2], but:
    1. Updated to the current code base.
    2. Shape with context, the same way as BreakingContext in [3].
    3. Drop support for CSS Text Level 4 properties.
    
    Unlike the line breaker which needs to find one hyphen location,
    computing min-content needs to find all hyphen locations. A method to
    get all hyphen locations in a word is added for platforms that can get
    all hyphen locations at once.
    
    [1] https://www.w3.org/TR/css-sizing-3/#min-content-inline-size
    [2] https://crrev.com/20526006
    [3] https://crrev.com/1978683002
    
    BUG=605840
    
    Review-Url: https://codereview.chromium.org/1994483002
    Cr-Commit-Position: refs/heads/master@{#394791}

--- third_party/WebKit/Source/platform/text/Hyphenation.h
+++ third_party/WebKit/Source/platform/text/Hyphenation.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define Hyphenation_h
 
 #include "platform/PlatformExport.h"
+#include "platform/fonts/Font.h"
 #include "wtf/Forward.h"
 #include "wtf/HashMap.h"
 #include "wtf/RefCounted.h"
@@ -22,6 +23,11 @@ public:
     static Hyphenation* get(const AtomicString& locale);
 
     virtual size_t lastHyphenLocation(const StringView&, size_t beforeIndex) const = 0;
+    virtual Vector<size_t, 8> hyphenLocations(const StringView&) const;
+
+    static const unsigned minimumPrefixLength = 2;
+    static const unsigned minimumSuffixLength = 2;
+    static int minimumPrefixWidth(const Font&);
 
     static void setForTesting(const AtomicString& locale, PassRefPtr<Hyphenation>);
     static void clearForTesting();
@@ -32,6 +38,17 @@ private:
     static HyphenationMap& getHyphenationMap();
     static PassRefPtr<Hyphenation> platformGetHyphenation(const AtomicString& locale);
 };
+
+inline int Hyphenation::minimumPrefixWidth(const Font& font)
+{
+    // If the maximum width available for the prefix before the hyphen is small, then it is very unlikely
+    // that an hyphenation opportunity exists, so do not bother to look for it.
+    // These are heuristic numbers for performance added in http://wkb.ug/45606
+    const int minimumPrefixWidthNumerator = 5;
+    const int minimumPrefixWidthDenominator = 4;
+    return font.getFontDescription().computedPixelSize()
+        * minimumPrefixWidthNumerator / minimumPrefixWidthDenominator;
+}
 
 } // namespace blink
 
