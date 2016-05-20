a0993b417e2b04c73656201b7a2c67a284dc92ce
pfeldman <pfeldman@chromium.org>
2016-05-19 17:46:06 -0700

    DevTools: land V8Inspector stub upstream.
    Review-Url: https://codereview.chromium.org/1977683003
    Cr-Commit-Position: refs/heads/master@{#394937}

--- third_party/WebKit/Source/platform/v8_inspector/V8Console.cpp
+++ third_party/WebKit/Source/platform/v8_inspector/V8Console.cpp
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/inspector_protocol/String16.h"
 #include "platform/v8_inspector/InjectedScript.h"
 #include "platform/v8_inspector/InspectedContext.h"
+#include "platform/v8_inspector/V8Compat.h"
 #include "platform/v8_inspector/V8DebuggerAgentImpl.h"
 #include "platform/v8_inspector/V8DebuggerImpl.h"
 #include "platform/v8_inspector/V8InspectorSessionImpl.h"
