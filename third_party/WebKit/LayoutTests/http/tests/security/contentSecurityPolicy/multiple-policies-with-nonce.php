4ad807edb50ffaffc2c0f41adb5a070f7bb4420d
mkwst <mkwst@chromium.org>
2016-05-20 15:30:11 -0700

    CSP: Fix handling of nonces in report-only mode.
    Our current implementation will only allow a nonce to bypass whitelists if
    it is present in all active policies, not just those delivered in
    enforce-mode.
    
    This patch adjusts the logic in ContentSecurityPolicy to ensure that nonces
    bypass whitelists if and only if they are present in each enforced policy.
    In particular, note that policies which do not contain the relevant directive
    will no longer be considered as passing a nonce check (though they will, of
    course, continue to pass the whitelist check which happens during fetching).
    
    BUG=611652
    
    Review-Url: https://codereview.chromium.org/1980533002
    Cr-Commit-Position: refs/heads/master@{#395189}

---
+++ third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/multiple-policies-with-nonce.php
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header("Content-Security-Policy: script-src 'nonce-abcd1234'");
+header("Content-Security-Policy-Report-Only: script-src 'self'");
+?>
+<!DOCTYPE html>
+<script src="/resources/testharness.js" nonce="abcd1234"></script>
+<script src="/resources/testharnessreport.js" nonce="abcd1234"></script>
+<script>
+    assert_unreached("This script block has no nonce, and should not execute.");
+</script>
+<script nonce="abcd1234">
+    test(_ => {
+      assert_true(true);
+    }, "This script block has a matching nonce, and should execute.");  
+</script>
