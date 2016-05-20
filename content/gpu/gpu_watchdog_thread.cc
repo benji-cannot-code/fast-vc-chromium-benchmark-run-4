dc3e9f10cb9895fc7a99d22ea25abdaa49635f8e
thomasanderson <thomasanderson@google.com>
2016-05-19 18:57:02 -0700

    Don't exit the GPU process if on a non-host TTY
    Requests to the X11 server may be stalled if we switch away from the
    TTY that is hosting the X server.  In this case, don't restart the GPU
    process as it will become responsive again when we switch to its TTY.
    
    BUG=355720
    
    Review-Url: https://codereview.chromium.org/1991353002
    Cr-Commit-Position: refs/heads/master@{#394957}

--- content/gpu/gpu_watchdog_thread.cc
+++ content/gpu/gpu_watchdog_thread.cc
@@ -30,11 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 namespace {
-#if defined(OS_CHROMEOS)
+#if defined(USE_X11)
 const base::FilePath::CharType
     kTtyFilePath[] = FILE_PATH_LITERAL("/sys/class/tty/tty0/active");
-#endif
-#if defined(USE_X11)
 const unsigned char text[20] = "check";
 #endif
 }  // namespace
@@ -56,6 +54,7 @@ GpuWatchdogThread::GpuWatchdogThread(int timeout)
       display_(NULL),
       window_(0),
       atom_(None),
+      host_tty_(-1),
 #endif
       weak_factory_(this) {
   DCHECK(timeout >= 0);
@@ -74,10 +73,8 @@ GpuWatchdogThread::GpuWatchdogThread(int timeout)
   DCHECK(result);
 #endif
 
-#if defined(OS_CHROMEOS)
-  tty_file_ = base::OpenFile(base::FilePath(kTtyFilePath), "r");
-#endif
 #if defined(USE_X11)
+  tty_file_ = base::OpenFile(base::FilePath(kTtyFilePath), "r");
   SetupXServer();
 #endif
   watched_message_loop_->AddTaskObserver(&task_observer_);
@@ -137,12 +134,9 @@ GpuWatchdogThread::~GpuWatchdogThread() {
   if (power_monitor)
     power_monitor->RemoveObserver(this);
 
-#if defined(OS_CHROMEOS)
+#if defined(USE_X11)
   if (tty_file_)
     fclose(tty_file_);
-#endif
-
-#if defined(USE_X11)
   XDestroyWindow(display_, window_);
   XCloseDisplay(display_);
 #endif
@@ -326,17 +320,11 @@ void GpuWatchdogThread::DeliberatelyTerminateToRecoverFromHang() {
     return;
 #endif
 
-#if defined(OS_CHROMEOS)
-  // Don't crash if we're not on tty1. This avoids noise in the GPU process
-  // crashes caused by people who use VT2 but still enable crash reporting.
-  char tty_string[8] = {0};
-  if (tty_file_ &&
-      !fseek(tty_file_, 0, SEEK_SET) &&
-      fread(tty_string, 1, 7, tty_file_)) {
-    int tty_number = -1;
-    int num_res = sscanf(tty_string, "tty%d", &tty_number);
-    if (num_res == 1 && tty_number != 1)
-      return;
+#if defined(USE_X11)
+  // Don't crash if we're not on the TTY of our host X11 server.
+  int active_tty = GetActiveTTY();
+  if(host_tty_ != -1 && active_tty != -1 && host_tty_ != active_tty) {
+    return;
   }
 #endif
 
@@ -381,6 +369,7 @@ void GpuWatchdogThread::SetupXServer() {
   window_ = XCreateWindow(display_, DefaultRootWindow(display_), 0, 0, 1, 1, 0,
                           CopyFromParent, InputOutput, CopyFromParent, 0, NULL);
   atom_ = XInternAtom(display_, "CHECK", False);
+  host_tty_ = GetActiveTTY();
 }
 
 void GpuWatchdogThread::SetupXChangeProp() {
@@ -461,6 +450,20 @@ base::ThreadTicks GpuWatchdogThread::GetWatchedThreadTime() {
            base::TimeDelta::FromMilliseconds(static_cast<int64_t>(
                (user_time64.QuadPart + kernel_time64.QuadPart) / 10000));
   }
+}
+#endif
+
+#if defined(USE_X11)
+int GpuWatchdogThread::GetActiveTTY() const {
+  char tty_string[8] = {0};
+  if (tty_file_ && !fseek(tty_file_, 0, SEEK_SET) &&
+      fread(tty_string, 1, 7, tty_file_)) {
+    int tty_number;
+    size_t num_res = sscanf(tty_string, "tty%d\n", &tty_number);
+    if (num_res == 1)
+      return tty_number;
+  }
+  return -1;
 }
 #endif
 
