6fa026b15fc8a2c7d64fdf6299798fe8dd93276b
juliatuttle <juliatuttle@chromium.org>
2016-05-20 13:14:28 -0700

    net: Destroy TransportConnectJobHelper.
    TransportConnectJobHelper was a common helper class used by
    TransportConnectJob and WebSocketTransportConnectJob. Unfortunately,
    it added more complexity than it removed, and would make future
    experiments in speculative connection to stale DNS entries more
    difficult.
    
    Therefore, I've removed it, and moved the code in it into the existing
    ConnectJob classes or standalone functions.
    
    BUG=394462,496208
    
    Review-Url: https://codereview.chromium.org/1975853002
    Cr-Commit-Position: refs/heads/master@{#395151}

--- net/socket/websocket_transport_client_socket_pool_unittest.cc
+++ net/socket/websocket_transport_client_socket_pool_unittest.cc
@@ -568,7 +568,7 @@ TEST_F(WebSocketTransportClientSocketPoolTest,
 
   client_socket_factory_.set_client_socket_types(case_types, 2);
   client_socket_factory_.set_delay(base::TimeDelta::FromMilliseconds(
-      TransportConnectJobHelper::kIPv6FallbackTimerInMs + 50));
+      TransportConnectJob::kIPv6FallbackTimerInMs + 50));
 
   // Resolve an AddressList with an IPv6 address first and then an IPv4 address.
   host_resolver_->rules()->AddIPLiteralRule(
@@ -729,7 +729,7 @@ TEST_F(WebSocketTransportClientSocketPoolTest, IPv6RapidFail) {
   EXPECT_EQ(OK, callback.WaitForResult());
   EXPECT_LT(base::TimeTicks::Now() - start,
             base::TimeDelta::FromMilliseconds(
-                TransportConnectJobHelper::kIPv6FallbackTimerInMs));
+                TransportConnectJob::kIPv6FallbackTimerInMs));
   ASSERT_TRUE(handle.socket());
 
   IPEndPoint endpoint;
@@ -789,7 +789,7 @@ TEST_F(WebSocketTransportClientSocketPoolTest, LastFailureWins) {
   client_socket_factory_.set_default_client_socket_type(
       MockTransportClientSocketFactory::MOCK_DELAYED_FAILING_CLIENT_SOCKET);
   base::TimeDelta delay = base::TimeDelta::FromMilliseconds(
-      TransportConnectJobHelper::kIPv6FallbackTimerInMs / 3);
+      TransportConnectJob::kIPv6FallbackTimerInMs / 3);
   client_socket_factory_.set_delay(delay);
 
   // Resolve an AddressList with 4 IPv6 addresses and 2 IPv4 addresses.
@@ -1020,7 +1020,7 @@ TEST_F(WebSocketTransportClientSocketPoolTest,
   // Now we have |kMaxSockets| IPv6 sockets stalled in connect. No IPv4 sockets
   // are started yet.
   RunLoopForTimePeriod(base::TimeDelta::FromMilliseconds(
-      TransportConnectJobHelper::kIPv6FallbackTimerInMs));
+      TransportConnectJob::kIPv6FallbackTimerInMs));
   // Now we have |kMaxSockets| IPv6 sockets and one IPv4 socket stalled in
   // connect, and |kMaxSockets - 1| IPv4 sockets waiting for the endpoint lock.
   pool_.FlushWithError(ERR_FAILED);
