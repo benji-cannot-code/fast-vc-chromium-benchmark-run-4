44bb676a4e89f8dfd4a19c22717052fc14f44922
spqchan <spqchan@chromium.org>
2016-05-19 17:16:15 -0700

    Move the Omnibox left icons 1px to the right
    BUG=606204
    
    Review-Url: https://codereview.chromium.org/1997473006
    Cr-Commit-Position: refs/heads/master@{#394921}

--- chrome/browser/ui/cocoa/omnibox/omnibox_popup_cell.mm
+++ chrome/browser/ui/cocoa/omnibox/omnibox_popup_cell.mm
@@ -31,9 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// How far to offset image column from the left.
-const CGFloat kImageXOffset = 5.0;
-
 // How far to offset text.
 const CGFloat kVerticalTextPadding = 3.0;
 
@@ -41,7 +38,7 @@ const CGFloat kVerticalImagePadding = 3.0;
 const CGFloat kMaterialVerticalImagePadding = 5.0;
 
 const CGFloat kTextStartOffset = 28.0;
-const CGFloat kMaterialTextStartOffset = 26.0;
+const CGFloat kMaterialTextStartOffset = 27.0;
 
 // Rounding radius of selection and hover background on popup items.
 const CGFloat kCellRoundingRadius = 2.0;
@@ -52,6 +49,16 @@ CGFloat VerticalImagePadding() {
     return kVerticalImagePadding;
   }
   return kMaterialVerticalImagePadding;
+}
+
+// How far to offset the image column from the left.
+CGFloat ImageXOffset() {
+  const CGFloat kImageXOffset = 5.0;
+  const CGFloat kMaterialImageXOffset = 6.0;
+  if (!ui::MaterialDesignController::IsModeMaterial()) {
+    return kImageXOffset;
+  }
+  return kMaterialImageXOffset;
 }
 
 // How far to offset the text column from the left.
@@ -511,7 +518,7 @@ NSAttributedString* CreateClassifiedAttributedString(
   NSImage* theImage =
       isDarkTheme ? [cellData incognitoImage] : [cellData image];
   imageRect.size = [theImage size];
-  imageRect.origin.x += kImageXOffset + [tableView contentLeftPadding];
+  imageRect.origin.x += ImageXOffset() + [tableView contentLeftPadding];
   imageRect.origin.y += VerticalImagePadding();
   [theImage drawInRect:FlipIfRTL(imageRect, cellFrame)
               fromRect:NSZeroRect
