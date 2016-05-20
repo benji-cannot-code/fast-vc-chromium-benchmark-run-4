bf18132b8987ab98923764c78c17f4a2adb923cf
justincohen <justincohen@chromium.org>
2016-05-20 09:30:06 -0700

    Allow custom swipe thresholds for SideSwipeGestureRecognizers.
    The default swipe minimum threshold for iPad needs to be a bit bigger in some
    scenarios.
    
    BUG=532970
    
    Review-Url: https://codereview.chromium.org/1996363002
    Cr-Commit-Position: refs/heads/master@{#395092}

--- ios/chrome/browser/ui/side_swipe_gesture_recognizer.h
+++ ios/chrome/browser/ui/side_swipe_gesture_recognizer.h
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, assign) CGFloat swipeEdge;
 // The distance from the edge of the screen from which a gesture starts.
 @property(readonly, nonatomic) CGFloat swipeOffset;
+// The distance between touches for a swipe to begin.
+@property(readwrite, nonatomic) CGFloat swipeThreshold;
 // Starting point of swipe.
 @property(readonly, nonatomic) CGPoint startPoint;
 @property(nonatomic, assign) UISwipeGestureRecognizerDirection direction;
