0fdfc8f36fec7ea2eeb661ec39c2d2e0b8ac14fc
dalecurtis <dalecurtis@chromium.org>
2016-05-20 12:38:00 -0700

    Make painting a single frame a permanent API on VideoRendererSink.
    Having to call VRS::Start(), wait for VRS::RC::Render(), and then
    queuing a VRS::Stop() across multiple threads just to paint a
    single frame is not desirable... so remove a bunch of TODOs and
    rename PaintFrameUsingOldRenderingPath() to PaintSingleFrame().
    
    This allows a lot of simplification in VideoRendererImpl and avoids
    Chromecast from having to pick up this complexity. It also will
    likely resolve nullptr crashes due having two sink startup points.
    
    BUG=611714
    TEST=existing unittests pass.
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review-Url: https://codereview.chromium.org/1996763002
    Cr-Commit-Position: refs/heads/master@{#395138}

--- cc/layers/video_frame_provider.h
+++ cc/layers/video_frame_provider.h
@@ -44,8 +44,6 @@ class CC_EXPORT VideoFrameProvider {
     virtual void StopRendering() = 0;
 
     // Notifies the client that GetCurrentFrame() will return new data.
-    // TODO(dalecurtis): Nuke this once VideoFrameProviderClientImpl is using a
-    // BeginFrameObserver based approach. http://crbug.com/336733
     virtual void DidReceiveFrame() = 0;
 
    protected:
@@ -76,9 +74,6 @@ class CC_EXPORT VideoFrameProvider {
   //
   // Clients should call this in response to UpdateCurrentFrame() returning true
   // or in response to a DidReceiveFrame() call.
-  //
-  // TODO(dalecurtis): Remove text about DidReceiveFrame() once the old path
-  // has been removed. http://crbug.com/439548
   virtual scoped_refptr<media::VideoFrame> GetCurrentFrame() = 0;
 
   // Called in response to DidReceiveFrame() or a return value of true from
