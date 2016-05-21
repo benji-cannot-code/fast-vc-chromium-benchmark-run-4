dab6f332693092b18a6f375437dce3dd76c09e99
dtapuska <dtapuska@chromium.org>
2016-05-21 07:38:14 -0700

    Fix potential crash in FrameView::setScrollOffset
    There was a code path that was checking if layoutView was non-null yet
    there was a code path that used it unconditionally. The unconditional
    path seems to match a crash stack. Move the clearHitTestCache
    code path into the conditional.
    
    BUG=611683
    
    Review-Url: https://codereview.chromium.org/2004503003
    Cr-Commit-Position: refs/heads/master@{#395260}

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -3239,6 +3239,7 @@ void FrameView::setScrollOffset(const DoublePoint& offset, ScrollType scrollType
     if (LayoutViewItem layoutView = LayoutViewItem(document->layoutView())) {
         if (layoutView.usesCompositing())
             layoutView.compositor()->frameViewDidScroll();
+        layoutView.clearHitTestCache();
     }
 
     if (m_didScrollTimer.isActive())
@@ -3248,7 +3249,6 @@ void FrameView::setScrollOffset(const DoublePoint& offset, ScrollType scrollType
     if (AXObjectCache* cache = m_frame->document()->existingAXObjectCache())
         cache->handleScrollPositionChanged(this);
 
-    layoutView()->clearHitTestCache();
     frame().loader().saveScrollState();
     frame().loader().client()->didChangeScrollOffset();
 
