1b14d13e0c16c3f15fe34d108fd86cfb98573448
joone.hur <joone.hur@intel.com>
2016-05-23 01:28:56 -0700

    Use operator== instead of equalSelectionsInDOMTree() in editing
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2001883002
    Cr-Commit-Position: refs/heads/master@{#395294}

--- third_party/WebKit/Source/core/editing/commands/TypingCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/TypingCommand.cpp
@@ -127,7 +127,7 @@ void TypingCommand::updateSelectionIfDifferentFromCurrentSelection(TypingCommand
 {
     DCHECK(frame);
     VisibleSelection currentSelection = frame->selection().selection();
-    if (equalSelectionsInDOMTree(currentSelection, typingCommand->endingSelection()))
+    if (currentSelection == typingCommand->endingSelection())
         return;
 
     typingCommand->setStartingSelection(currentSelection);
@@ -176,7 +176,7 @@ void TypingCommand::insertText(Document& document, const String& text, const Vis
     // that is different from the current selection.  In the future, we should change EditCommand
     // to deal with custom selections in a general way that can be used by all of the commands.
     if (TypingCommand* lastTypingCommand = lastTypingCommandIfStillOpenForTyping(frame)) {
-        if (!equalSelectionsInDOMTree(lastTypingCommand->endingSelection(), selectionForInsertion)) {
+        if (lastTypingCommand->endingSelection() != selectionForInsertion) {
             lastTypingCommand->setStartingSelection(selectionForInsertion);
             lastTypingCommand->setEndingSelection(selectionForInsertion);
         }
@@ -191,7 +191,7 @@ void TypingCommand::insertText(Document& document, const String& text, const Vis
     }
 
     TypingCommand* command = TypingCommand::create(document, InsertText, newText, options, compositionType);
-    bool changeSelection = !equalSelectionsInDOMTree(selectionForInsertion, currentSelection);
+    bool changeSelection = selectionForInsertion != currentSelection;
     if (changeSelection) {
         command->setStartingSelection(selectionForInsertion);
         command->setEndingSelection(selectionForInsertion);
