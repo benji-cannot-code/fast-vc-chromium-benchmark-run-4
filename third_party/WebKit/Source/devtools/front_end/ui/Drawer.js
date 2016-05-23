7f4727ff1b2f968a398d0dfe1f9eed51f07ccffe
luoe <luoe@chromium.org>
2016-05-23 16:08:03 -0700

    DevTools: allow focus to be restored to the drawer after resize
    BUG=609013
    
    Review-Url: https://codereview.chromium.org/2002803002
    Cr-Commit-Position: refs/heads/master@{#395449}

--- third_party/WebKit/Source/devtools/front_end/ui/Drawer.js
+++ third_party/WebKit/Source/devtools/front_end/ui/Drawer.js
@@ -133,6 +133,15 @@ WebInspector.Drawer.prototype = {
     },
 
     /**
+     * @override
+     * @return {!Element}
+     */
+    defaultFocusedElement: function()
+    {
+        return this._tabbedPane.defaultFocusedElement();
+    },
+
+    /**
      * @return {?string}
      */
     selectedViewId: function()
