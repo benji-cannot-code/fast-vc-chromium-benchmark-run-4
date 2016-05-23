983f450b09595f142950e94ae13c438426c1ae46
pkotwicz <pkotwicz@chromium.org>
2016-05-23 16:56:19 -0700

    Always suppress DP_CREATE_CLASSLOADER_INSIDE_DO_PRIVILEGED
    According to the docs, using AccessController is discouraged:
    https://developer.android.com/reference/java/security/AccessController.html
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/1989403004
    Cr-Commit-Position: refs/heads/master@{#395469}

--- android_webview/java/src/org/chromium/android_webview/ResourcesContextWrapperFactory.java
+++ android_webview/java/src/org/chromium/android_webview/ResourcesContextWrapperFactory.java
@@ -11,8 +11,6 @@ import android.content.ContextWrapper;
 import android.content.Intent;
 import android.view.LayoutInflater;
 
-import org.chromium.base.annotations.SuppressFBWarnings;
-
 import java.lang.ref.WeakReference;
 import java.util.WeakHashMap;
 
@@ -55,7 +53,6 @@ public class ResourcesContextWrapperFactory {
             super(base);
         }
 
-        @SuppressFBWarnings("DP_CREATE_CLASSLOADER_INSIDE_DO_PRIVILEGED")
         @Override
         public ClassLoader getClassLoader() {
             final ClassLoader appCl = getBaseContext().getClassLoader();
