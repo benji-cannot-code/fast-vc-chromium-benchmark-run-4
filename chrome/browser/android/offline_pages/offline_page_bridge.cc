b3c8205238605e4cb24fcb01d7b4acaa102e180e
dewittj <dewittj@chromium.org>
2016-05-23 14:44:20 -0700

    Offline Pages: Add NULL-checks for when the model is NULL (incognito mode)
    Recently offline pages was changed to be completely disabled in
    incognito profiles, since it is no longer associated with bookmarks, so
    some consumers failed to check for null.
    
    BUG=613359
    
    Review-Url: https://codereview.chromium.org/2004473002
    Cr-Commit-Position: refs/heads/master@{#395422}

--- chrome/browser/android/offline_pages/offline_page_bridge.cc
+++ chrome/browser/android/offline_pages/offline_page_bridge.cc
@@ -162,8 +162,12 @@ static ScopedJavaLocalRef<jobject> GetOfflinePageBridgeForProfile(
     const JavaParamRef<jclass>& jcaller,
     const JavaParamRef<jobject>& j_profile) {
   Profile* profile = ProfileAndroid::FromProfileAndroid(j_profile);
+
   OfflinePageModel* offline_page_model =
       OfflinePageModelFactory::GetForBrowserContext(profile);
+
+  if (offline_page_model == nullptr)
+    return ScopedJavaLocalRef<jobject>();
 
   OfflinePageBridge* bridge = static_cast<OfflinePageBridge*>(
       offline_page_model->GetUserData(kOfflinePageBridgeKey));
@@ -171,6 +175,7 @@ static ScopedJavaLocalRef<jobject> GetOfflinePageBridgeForProfile(
     bridge = new OfflinePageBridge(env, profile, offline_page_model);
     offline_page_model->SetUserData(kOfflinePageBridgeKey, bridge);
   }
+
   return ScopedJavaLocalRef<jobject>(bridge->java_ref());
 }
 
