716cef7259ffbd2c34df3c574054af363dde9323
mstensho <mstensho@opera.com>
2016-05-23 10:34:15 -0700

    Move LineLayoutState forward declaration to LayoutBlockFlow.
    And the friend declaration was unnecessary.
    
    BUG=302024
    
    Review-Url: https://codereview.chromium.org/2007543002
    Cr-Commit-Position: refs/heads/master@{#395352}

--- third_party/WebKit/Source/core/layout/LayoutBlock.h
+++ third_party/WebKit/Source/core/layout/LayoutBlock.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class LineLayoutState;
 struct PaintInfo;
 class WordMeasurement;
 
@@ -103,9 +102,6 @@ enum ContainingBlockState { NewContainingBlock, SameContainingBlock };
 //     ...
 // }
 class CORE_EXPORT LayoutBlock : public LayoutBox {
-public:
-    friend class LineLayoutState;
-
 protected:
     explicit LayoutBlock(ContainerNode*);
     ~LayoutBlock() override;
--- third_party/WebKit/Source/core/layout/LayoutBlockFlow.h
+++ third_party/WebKit/Source/core/layout/LayoutBlockFlow.h
@@ -54,6 +54,7 @@ class MarginInfo;
 class LayoutInline;
 class LineBreaker;
 class LineInfo;
+class LineLayoutState;
 class LineWidth;
 class LayoutMultiColumnFlowThread;
 class LayoutMultiColumnSpannerPlaceholder;
