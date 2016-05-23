eefc2021773a13017c1d8e4eb117ee92900af586
yasong <yasong@chromium.org>
2016-05-23 10:21:01 -0700

    Change VLOGs to DVLOGs in spdy_header_block.cc and add move semantics to SpdyDataIR and SpdyGoAwayIR.
    This CL lands server change 122096521 by xunjieli@ and 122760675 and 122760675 by jamessynge@.
    
    BUG=488484
    
    Review-Url: https://codereview.chromium.org/1987123004
    Cr-Commit-Position: refs/heads/master@{#395346}

--- net/spdy/spdy_protocol.cc
+++ net/spdy/spdy_protocol.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/spdy/spdy_protocol.h"
 
+#include "base/memory/ptr_util.h"
 #include "net/spdy/spdy_bug_tracker.h"
 
 namespace net {
@@ -777,6 +778,16 @@ SpdyDataIR::SpdyDataIR(SpdyStreamId stream_id, base::StringPiece data)
   SetDataDeep(data);
 }
 
+SpdyDataIR::SpdyDataIR(SpdyStreamId stream_id, const char* data)
+    : SpdyDataIR(stream_id, base::StringPiece(data)) {}
+
+SpdyDataIR::SpdyDataIR(SpdyStreamId stream_id, std::string data)
+    : SpdyFrameWithFinIR(stream_id),
+      data_store_(base::MakeUnique<std::string>(std::move(data))),
+      data_(*data_store_),
+      padded_(false),
+      padding_payload_len_(0) {}
+
 SpdyDataIR::SpdyDataIR(SpdyStreamId stream_id)
     : SpdyFrameWithFinIR(stream_id), padded_(false), padding_payload_len_(0) {}
 
@@ -825,6 +836,22 @@ SpdyGoAwayIR::SpdyGoAwayIR(SpdyStreamId last_good_stream_id,
                            base::StringPiece description)
     : description_(description) {
       set_last_good_stream_id(last_good_stream_id);
+  set_status(status);
+}
+
+SpdyGoAwayIR::SpdyGoAwayIR(SpdyStreamId last_good_stream_id,
+                           SpdyGoAwayStatus status,
+                           const char* description)
+    : SpdyGoAwayIR(last_good_stream_id,
+                   status,
+                   base::StringPiece(description)) {}
+
+SpdyGoAwayIR::SpdyGoAwayIR(SpdyStreamId last_good_stream_id,
+                           SpdyGoAwayStatus status,
+                           std::string description)
+    : description_store_(std::move(description)),
+      description_(description_store_) {
+  set_last_good_stream_id(last_good_stream_id);
   set_status(status);
 }
 
