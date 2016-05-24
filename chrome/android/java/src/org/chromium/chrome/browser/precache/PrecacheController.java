caafcb9375335c9b253baa1ff5222c69b252df02
rajendrant <rajendrant@chromium.org>
2016-05-23 22:02:29 -0700

    Handle precache GCM task scheduling failures
    GCMNetworkManager schedule/cancel task calls can fail if
    ChromeBackgroundService has not registered as GcmTaskService yet.
    Handle these failures gracefully. Later these failures should be
    tracked via UMA.
    
    BUG=605384
    
    Review-Url: https://codereview.chromium.org/1958923002
    Cr-Commit-Position: refs/heads/master@{#395531}

--- chrome/android/java/src/org/chromium/chrome/browser/precache/PrecacheController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/precache/PrecacheController.java
@@ -166,11 +166,13 @@ public class PrecacheController {
                 .setTag(PERIODIC_TASK_TAG)
                 .build();
         sTaskScheduler.scheduleTask(context, task);
+        // TODO(rajenrant): Track any failure via UMA.
     }
 
     private static void cancelPeriodicPrecacheTask(Context context) {
         Log.v(TAG, "canceling a periodic precache task");
         sTaskScheduler.cancelTask(context, PERIODIC_TASK_TAG);
+        // TODO(rajenrant): Track any failure via UMA.
     }
 
     /**
@@ -194,11 +196,13 @@ public class PrecacheController {
                 .setUpdateCurrent(true)
                 .build();
         sTaskScheduler.scheduleTask(context, task);
+        // TODO(rajenrant): Track any failure via UMA.
     }
 
     private static void cancelPrecacheCompletionTask(Context context) {
         Log.v(TAG, "canceling a precache completion task");
         sTaskScheduler.cancelTask(context, CONTINUATION_TASK_TAG);
+        // TODO(rajenrant): Track any failure via UMA.
     }
 
     public static void rescheduleTasksOnUpgrade(Context context) {
