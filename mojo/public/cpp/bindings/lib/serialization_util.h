c2f70d0ac8eae2c4bb9a710a1269d4b8974a56b0
yzshen <yzshen@chromium.org>
2016-05-24 16:00:08 -0700

    Mojo C++ bindings: support attaching context object for StructTraits.
    This CL also updates the documentation of StructTraits.
    
    BUG=577686
    
    Review-Url: https://codereview.chromium.org/2007813004
    Cr-Commit-Position: refs/heads/master@{#395720}

--- mojo/public/cpp/bindings/lib/serialization_util.h
+++ mojo/public/cpp/bindings/lib/serialization_util.h
@@ -214,18 +214,18 @@ struct CustomContextHelper<Traits, false> {
   }
 };
 
-template <typename ReturnType, typename ParamType, typename MaybeConstUserType>
+template <typename ReturnType, typename ParamType, typename InputUserType>
 ReturnType CallWithContext(ReturnType (*f)(ParamType, void*),
-                           MaybeConstUserType& input,
+                           InputUserType&& input,
                            void* context) {
-  return f(input, context);
+  return f(std::forward<InputUserType>(input), context);
 }
 
-template <typename ReturnType, typename ParamType, typename MaybeConstUserType>
+template <typename ReturnType, typename ParamType, typename InputUserType>
 ReturnType CallWithContext(ReturnType (*f)(ParamType),
-                           MaybeConstUserType& input,
+                           InputUserType&& input,
                            void* context) {
-  return f(input);
+  return f(std::forward<InputUserType>(input));
 }
 
 }  // namespace internal
