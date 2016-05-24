c48e0c46eaf7e037caf341ac7f4ddbdcf8ad6d2e
hlopko <hlopko@chromium.org>
2016-05-24 06:13:08 -0700

    Implement Custom=VisitDOMWrapper idl attribute within traceWrappers for mutation observer
    Mutation observer wrapper lifetime is tested in fast/dom/MutationObserver/observer-wrapper-dropoff.html and fast/dom/MutationObserver/observer-wrapper-dropoff-transient.html layout tests.
    
    Follow status of tracing Custom=VisitDOMWrapper here:
    
    https://docs.google.com/a/google.com/spreadsheets/d/1RhLiHF9Pnw7Zx8EijuR0LruPkeiVJIXXi0eRkRDFwBc/edit?usp=sharing
    
    LOG=no
    BUG=468240
    
    Review-Url: https://codereview.chromium.org/2005793002
    Cr-Commit-Position: refs/heads/master@{#395583}

--- third_party/WebKit/Source/core/dom/MutationObserverRegistration.h
+++ third_party/WebKit/Source/core/dom/MutationObserverRegistration.h
@@ -64,6 +64,8 @@ public:
 
     DECLARE_TRACE();
 
+    DECLARE_TRACE_WRAPPERS();
+
     void dispose();
 
 private:
--- third_party/WebKit/Source/platform/heap/WrapperVisitor.h
+++ third_party/WebKit/Source/platform/heap/WrapperVisitor.h
@@ -21,8 +21,10 @@ class ScriptWrappable;
     V(ElementRareData);                                              \
     V(ElementShadow);                                                \
     V(HTMLImportsController)                                         \
+    V(MutationObserverRegistration);                                 \
     V(NodeIntersectionObserverData)                                  \
     V(NodeListsNodeData);                                            \
+    V(NodeMutationObserverData);                                     \
     V(NodeRareData);                                                 \
     V(StyleEngine);                                                  \
 
