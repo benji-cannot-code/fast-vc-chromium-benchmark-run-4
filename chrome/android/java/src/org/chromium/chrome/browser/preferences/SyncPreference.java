a11d3f987e5066bc293508b4bc8e62f4cd72fca9
maxbogue <maxbogue@chromium.org>
2016-05-23 18:48:30 -0700

    [Sync] Show "Setup in progress..." if sync is not yet active.
    BUG=614177
    
    Review-Url: https://codereview.chromium.org/2006143002
    Cr-Commit-Position: refs/heads/master@{#395512}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/SyncPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/SyncPreference.java
@@ -78,7 +78,7 @@ public class SyncPreference extends Preference {
         boolean syncEnabled = AndroidSyncSettings.isSyncEnabled(context);
 
         if (syncEnabled) {
-            if (!profileSyncService.isBackendInitialized()) {
+            if (!profileSyncService.isSyncActive()) {
                 return res.getString(R.string.sync_setup_progress);
             }
 
