a265e242f726481b8221a5d9599c1a1abc4d1b9f
peria <peria@chromium.org>
2016-05-24 04:39:50 -0700

    Add [OverrideBuiltins] label onto HTMLDocument interface.
    This CL also removes special handling of named properties
    using V8 script controller, which is no longer needed.
    
    Currently, we have no general routines to work for [Unforgeable]
    on [OverrideBuiltins] interface, and "location" in "HTMLDocument"
    is the only one which meets the situation.
    So now, we handle it as an edge case of named property.
    
    Spec: https://html.spec.whatwg.org/multipage/dom.html#dom-document-namedItem-which
    
    Updated a test expectation; Chrome has been FAILing it, while IE, FF, and Safari PASS it.
    
    BUG=611632
    
    Review-Url: https://codereview.chromium.org/1837823003
    Cr-Commit-Position: refs/heads/master@{#395570}

--- third_party/WebKit/Source/core/html/HTMLDocument.cpp
+++ third_party/WebKit/Source/core/html/HTMLDocument.cpp
@@ -171,8 +171,6 @@ void HTMLDocument::addItemToMap(HashCountedSet<AtomicString>& map, const AtomicS
     if (name.isEmpty())
         return;
     map.add(name);
-    if (LocalFrame* f = frame())
-        f->script().namedItemAdded(this, name);
 }
 
 void HTMLDocument::removeItemFromMap(HashCountedSet<AtomicString>& map, const AtomicString& name)
@@ -180,8 +178,6 @@ void HTMLDocument::removeItemFromMap(HashCountedSet<AtomicString>& map, const At
     if (name.isEmpty())
         return;
     map.remove(name);
-    if (LocalFrame* f = frame())
-        f->script().namedItemRemoved(this, name);
 }
 
 void HTMLDocument::addNamedItem(const AtomicString& name)
