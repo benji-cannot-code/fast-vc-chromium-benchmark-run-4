a97c01c73b891cfa53cdd069cc1dea27066006f4
ckulakowski <ckulakowski@opera.com>
2016-05-24 04:55:12 -0700

    Added missing include to profile_helper.h
    profile_helper.h uses ProfileMetrics::ProfileDelete defined in
    profile_metrics.h so it should be included. Lack of this include
    causes compilation error in our product.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2002073003
    Cr-Commit-Position: refs/heads/master@{#395573}

--- chrome/browser/ui/webui/profile_helper.h
+++ chrome/browser/ui/webui/profile_helper.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_metrics.h"
 
 namespace content {
 class WebUI;
