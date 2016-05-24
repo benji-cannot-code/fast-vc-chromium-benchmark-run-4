6f6c699e3d102b16b2a214a0671dca39a50f57e3
pauljensen <pauljensen@chromium.org>
2016-05-23 19:49:23 -0700

    Make LoggingNetworkChangeObserver log specific network changes
    BUG=610293
    
    Review-Url: https://codereview.chromium.org/1973743004
    Cr-Commit-Position: refs/heads/master@{#395520}

--- net/base/logging_network_change_observer.h
+++ net/base/logging_network_change_observer.h
@@ -19,7 +19,8 @@ class NetLog;
 class NET_EXPORT LoggingNetworkChangeObserver
     : public NetworkChangeNotifier::IPAddressObserver,
       public NetworkChangeNotifier::ConnectionTypeObserver,
-      public NetworkChangeNotifier::NetworkChangeObserver {
+      public NetworkChangeNotifier::NetworkChangeObserver,
+      public NetworkChangeNotifier::NetworkObserver {
  public:
   // Note: |net_log| must remain valid throughout the lifetime of this
   // LoggingNetworkChangeObserver.
@@ -37,7 +38,16 @@ class NET_EXPORT LoggingNetworkChangeObserver
   // NetworkChangeNotifier::NetworkChangeObserver implementation.
   void OnNetworkChanged(NetworkChangeNotifier::ConnectionType type) override;
 
- private:
+  // NetworkChangeNotifier::NetworkObserver implementation.
+  void OnNetworkConnected(
+      NetworkChangeNotifier::NetworkHandle network) override;
+  void OnNetworkDisconnected(
+      NetworkChangeNotifier::NetworkHandle network) override;
+  void OnNetworkSoonToDisconnect(
+      NetworkChangeNotifier::NetworkHandle network) override;
+  void OnNetworkMadeDefault(
+      NetworkChangeNotifier::NetworkHandle network) override;
+
   NetLog* net_log_;
 
   DISALLOW_COPY_AND_ASSIGN(LoggingNetworkChangeObserver);
