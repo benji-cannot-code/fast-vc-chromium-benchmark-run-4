503eddc699b9cce04e992591e7a877d84efc51c9
nzolghadr <nzolghadr@chromium.org>
2016-05-24 14:55:53 -0700

    Dispatch middle click manually by tracking mouse
    Tracking targets for middle button mousedown event
    and fire click event for it on mouse up if necessary.
    
    Note: The click is not finding the common parent of
    mousedown and up targets for simplicty and only sends
    the click to the target if both down and up happened
    on the same node.
    
    BUG=608697
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/1963823002
    Cr-Commit-Position: refs/heads/master@{#395707}

---
+++ chrome/browser/resources/ntp4/synthetic_middleclick.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(function() {
+
+var middleButtonMouseDownTarget = null;
+
+document.addEventListener('mousedown', function(e) {
+  if (e.button == 1)
+    middleButtonMouseDownTarget = e.path[0];
+}, true);
+
+document.addEventListener('mouseup', function(e) {
+  if (e.button != 1)
+    return;
+
+  if (e.path.length > 0 && e.path[0] == middleButtonMouseDownTarget)
+    middleButtonMouseDownTarget.dispatchEvent(new MouseEvent('click', e));
+
+  middleButtonMouseDownTarget = null;
+}, true);
+
+})();
