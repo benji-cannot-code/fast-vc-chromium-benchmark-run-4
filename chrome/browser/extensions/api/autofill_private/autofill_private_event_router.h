55320d59176789194eb7759621075198f3e4eb90
hajimehoshi <hajimehoshi@chromium.org>
2016-05-25 00:44:55 -0700

    Revert of Implement DISALLOW_COPY_AND_ASSIGN using = delete. (patchset #28 id:540001 of https://codereview.chromium.org/1981053002/ )
    Reason for revert:
    This causes compile errors on some bots (e.g. https://build.chromium.org/p/chromium.chromiumos/buildstatus?builder=Linux%20ChromiumOS%20Builder&number=40216)
    
    Original issue's description:
    > Implement DISALLOW_COPY_AND_ASSIGN using = delete.
    >
    > BUG=447156
    > TEST=none
    > CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    > TBR=rdevlin.cronin
    >
    > Committed: https://crrev.com/16afbf1fd62d6d4d4cf34c148333bd5b7aaf997f
    > Cr-Commit-Position: refs/heads/master@{#395814}
    
    TBR=thakis@chromium.org,hcarmona@chromium.org,lazyboy@chromium.org,rdevlin.cronin@chromium.org,pkasting@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=447156
    
    Review-Url: https://codereview.chromium.org/2009783002
    Cr-Commit-Position: refs/heads/master@{#395825}

--- chrome/browser/extensions/api/autofill_private/autofill_private_event_router.h
+++ chrome/browser/extensions/api/autofill_private/autofill_private_event_router.h
@@ -49,6 +49,9 @@ class AutofillPrivateEventRouter :
 
   autofill::PersonalDataManager* personal_data_;
 
+  // Whether this class is currently listening for changes to |personal_data_|.
+  bool listening_;
+
   DISALLOW_COPY_AND_ASSIGN(AutofillPrivateEventRouter);
 };
 
--- extensions/browser/extension_service_worker_message_filter.cc
+++ extensions/browser/extension_service_worker_message_filter.cc
@@ -15,6 +15,7 @@ ExtensionServiceWorkerMessageFilter::ExtensionServiceWorkerMessageFilter(
     content::BrowserContext* context)
     : content::BrowserMessageFilter(ExtensionWorkerMsgStart),
       render_process_id_(render_process_id),
+      browser_context_(context),
       dispatcher_(new ExtensionFunctionDispatcher(context)) {}
 
 ExtensionServiceWorkerMessageFilter::~ExtensionServiceWorkerMessageFilter() {
