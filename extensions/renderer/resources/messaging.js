ae72372434ce447389b7d49637cfde66cfbb751d
rob <rob@robwu.nl>
2016-05-25 04:03:10 -0700

    Remove port lifetime management from renderer
    - Remove port tracking logic from the renderer because it is handled by
      the browser since https://codereview.chromium.org/1413543005/.
    
    - Store the source tab id on the port in the browser to remove the need
      for tracking the tab ID in the renderer (originally introduced with
      https://codereview.chromium.org/145463002).
    
    - Remove redundant onDestroy_ from messaging.js (because the event
      listeners are already removed as a part of the event's destroy_()).
    
    - In the browser process, ask the renderer whether a newly created opener
      port exists because the originating frame might have navigated since the
      request to create a port.
    
    BUG=475536
    
    Review-Url: https://codereview.chromium.org/1966283002
    Cr-Commit-Position: refs/heads/master@{#395861}

--- extensions/renderer/resources/messaging.js
+++ extensions/renderer/resources/messaging.js
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var kRequestChannel = "chrome.extension.sendRequest";
   var kMessageChannel = "chrome.runtime.sendMessage";
   var kNativeMessageChannel = "chrome.runtime.sendNativeMessage";
+  var kPortClosedError = 'Attempting to use a disconnected port object';
 
   // Map of port IDs to port object.
   var ports = {__proto__: null};
@@ -54,13 +55,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     };
     this.onDisconnect = new Event(null, [portSchema], options);
     this.onMessage = new Event(null, [messageSchema, portSchema], options);
-    this.onDestroy_ = null;
   }
   $Object.setPrototypeOf(PortImpl.prototype, null);
 
   // Sends a message asynchronously to the context on the other end of this
   // port.
   PortImpl.prototype.postMessage = function(msg) {
+    if (!$Object.hasOwnProperty(ports, this.portId_))
+      throw new Error(kPortClosedError);
+
     // JSON.stringify doesn't support a root object which is undefined.
     if (msg === undefined)
       msg = null;
@@ -81,28 +84,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Disconnects the port from the other end.
   PortImpl.prototype.disconnect = function() {
+    if (!$Object.hasOwnProperty(ports, this.portId_))
+      return;  // disconnect() on an already-closed port is a no-op.
     messagingNatives.CloseChannel(this.portId_, true);
     this.destroy_();
   };
 
+  // Close this specific port without forcing the channel to close. The channel
+  // will close if this was the only port at this end of the channel.
+  PortImpl.prototype.disconnectSoftly = function() {
+    if (!$Object.hasOwnProperty(ports, this.portId_))
+      return;
+    messagingNatives.CloseChannel(this.portId_, false);
+    this.destroy_();
+  };
+
   PortImpl.prototype.destroy_ = function() {
-    if (this.onDestroy_) {
-      this.onDestroy_();
-      this.onDestroy_ = null;
-    }
     privates(this.onDisconnect).impl.destroy_();
     privates(this.onMessage).impl.destroy_();
-    // TODO(robwu): Remove port lifetime management because it is completely
-    // handled in the browser. The renderer's only roles are
-    // 1) rejecting ports so that the browser knows that the renderer is not
-    //    interested in the port (this is merely an optimization)
-    // 2) acknowledging port creations, so that the browser knows that the port
-    //    was successfully created (from the perspective of the extension), but
-    //    then closed for some non-fatal reason.
-    // 3) notifying the browser of explicit port closure via .disconnect().
-    // In other cases (navigations), the browser automatically cleans up the
-    //    port.
-    messagingNatives.PortRelease(this.portId_);
     delete ports[this.portId_];
   };
 
@@ -110,7 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // the C++ to avoid creating the javascript message for all the contexts that
   // don't care about a particular message.
   function hasPort(portId) {
-    return portId in ports;
+    return $Object.hasOwnProperty(ports, portId);
   };
 
   // Hidden port creation function.  We don't want to expose an API that lets
@@ -120,7 +119,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       throw new Error("Port '" + portId + "' already exists.");
     var port = new Port(portId, opt_name);
     ports[portId] = port;
-    messagingNatives.PortAddRef(portId);
     return port;
   };
 
@@ -178,7 +176,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       var responseCallback = function(response) {
         if (port) {
           port.postMessage(response);
-          privates(port).impl.destroy_();
+          // TODO(robwu): This can be changed to disconnect() because there is
+          // no point in allowing other receivers at this end of the port to
+          // keep the channel alive because the opener port can only receive one
+          // message.
+          privates(port).impl.disconnectSoftly();
           port = null;
         } else {
           // We nulled out port when sending the response, and now the page
@@ -199,7 +201,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       // the context has been destroyed, so there isn't any JS state to clear.
       messagingNatives.BindToGC(responseCallback, function() {
         if (port) {
-          privates(port).impl.destroy_();
+          privates(port).impl.disconnectSoftly();
           port = null;
         }
       }, portId);
@@ -210,15 +212,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         if (!responseCallbackPreserved && port) {
           // If they didn't access the response callback, they're not
           // going to send a response, so clean up the port immediately.
-          privates(port).impl.destroy_();
+          privates(port).impl.disconnectSoftly();
           port = null;
         }
       }
     }
 
-    privates(port).impl.onDestroy_ = function() {
-      port.onMessage.removeListener(messageListener);
-    };
     port.onMessage.addListener(messageListener);
 
     var eventName = isSendMessage ? "runtime.onMessage" : "extension.onRequest";
@@ -315,8 +314,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   function dispatchOnDisconnect(portId, errorMessage) {
     var port = ports[portId];
     if (port) {
-      // Update the renderer's port bookkeeping, without notifying the browser.
-      messagingNatives.CloseChannel(portId, false);
+      delete ports[portId];
       if (errorMessage)
         lastError.set('Port', errorMessage, null, chrome);
       try {
@@ -399,10 +397,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       }
     }
 
-    privates(port).impl.onDestroy_ = function() {
-      port.onDisconnect.removeListener(disconnectListener);
-      port.onMessage.removeListener(messageListener);
-    };
     port.onDisconnect.addListener(disconnectListener);
     port.onMessage.addListener(messageListener);
   };
