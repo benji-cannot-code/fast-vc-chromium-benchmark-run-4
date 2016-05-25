ff5a8b623f7e279e0c5cd9067f1fd390cca0482c
cco3 <cco3@chromium.org>
2016-05-25 16:06:37 -0700

    Record UMA for Physical Web refresh resolve time
    Physical Web Service resolve times are expected to differ based on
    whether we've recently sent out URLs for resolution, so it makes sense
    to distinguish between refreshes and non-refresh resolutions in our
    recorded UMA.
    
    BUG=609945
    
    Review-Url: https://codereview.chromium.org/2005313002
    Cr-Commit-Position: refs/heads/master@{#396029}

--- chrome/android/java/src/org/chromium/chrome/browser/physicalweb/ListUrlsActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/physicalweb/ListUrlsActivity.java
@@ -201,13 +201,17 @@ public class ListUrlsActivity extends AppCompatActivity implements AdapterView.O
         super.onStop();
     }
 
-    private void resolve(Collection<UrlInfo> urls) {
+    private void resolve(Collection<UrlInfo> urls, final boolean isUserInitiated) {
         final long timestamp = SystemClock.elapsedRealtime();
         mPwsClient.resolve(urls, new PwsClient.ResolveScanCallback() {
             @Override
             public void onPwsResults(Collection<PwsResult> pwsResults) {
                 long duration = SystemClock.elapsedRealtime() - timestamp;
-                PhysicalWebUma.onForegroundPwsResolution(ListUrlsActivity.this, duration);
+                if (isUserInitiated) {
+                    PhysicalWebUma.onRefreshPwsResolution(ListUrlsActivity.this, duration);
+                } else {
+                    PhysicalWebUma.onForegroundPwsResolution(ListUrlsActivity.this, duration);
+                }
 
                 // filter out duplicate site URLs.
                 for (PwsResult pwsResult : pwsResults) {
@@ -248,7 +252,7 @@ public class ListUrlsActivity extends AppCompatActivity implements AdapterView.O
      */
     @Override
     public void onDisplayableUrlsAdded(Collection<UrlInfo> urls) {
-        resolve(urls);
+        resolve(urls, false);
     }
 
     private void startRefresh(boolean isUserInitiated, boolean isSwipeInitiated) {
@@ -286,7 +290,7 @@ public class ListUrlsActivity extends AppCompatActivity implements AdapterView.O
                     (AnimationDrawable) mScanningImageView.getDrawable();
             animationDrawable.start();
 
-            resolve(urls);
+            resolve(urls, isUserInitiated);
         }
     }
 
