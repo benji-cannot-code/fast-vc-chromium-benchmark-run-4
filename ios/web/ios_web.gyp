9d1e0957c9502f071afd72eba63aa76afe0ffdf9
eugenebut <eugenebut@chromium.org>
2016-05-25 09:09:43 -0700

    [ios Mojo] Added ios-only built-in Mojo resources for web ui.
    Added JS files do not exist on other platforms because content//
    implements these funcitons natively.
    
    Design doc:
    https://docs.google.com/document/d/1iUJXX_zoY2BbnecpfxjznOeasf29KUzl_9S_LG08Bes/edit#heading=h.yjqp5garsqf9
    
    BUG=567809
    
    Review-Url: https://codereview.chromium.org/2013683003
    Cr-Commit-Position: refs/heads/master@{#395900}

--- ios/web/ios_web.gyp
+++ ios/web/ios_web.gyp
@@ -273,6 +273,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'webui/crw_web_ui_page_builder.mm',
         'webui/mojo_facade.h',
         'webui/mojo_facade.mm',
+        'webui/mojo_js_constants.cc',
+        'webui/mojo_js_constants.h',
         'webui/shared_resources_data_source_ios.h',
         'webui/shared_resources_data_source_ios.mm',
         'webui/url_data_manager_ios.cc',
---
+++ ios/web/webui/mojo_js_constants.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/web/webui/mojo_js_constants.h"
+
+namespace web {
+
+const char kSyncMessageChannelModuleName[] =
+    "ios/mojo/public/js/sync_message_channel";
+const char kHandleUtilModuleName[] = "ios/mojo/public/js/handle_util";
+const char kSupportModuleName[] = "mojo/public/js/support";
+const char kCoreModuleName[] = "mojo/public/js/core";
+const char kServiceProviderModuleName[] =
+    "content/public/renderer/service_provider";
+
+}  // namespace web
