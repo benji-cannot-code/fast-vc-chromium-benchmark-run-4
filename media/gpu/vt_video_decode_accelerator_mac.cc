3b2fcba94a0ef8b50a04eb045e65b6c5f9133a1d
sandersd <sandersd@chromium.org>
2016-05-24 18:09:00 -0700

    Switch VTVDA to use SharedMemoryRegion.
    Review-Url: https://codereview.chromium.org/2006303003
    Cr-Commit-Position: refs/heads/master@{#395763}

--- media/gpu/vt_video_decode_accelerator_mac.cc
+++ media/gpu/vt_video_decode_accelerator_mac.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/version.h"
 #include "media/base/limits.h"
+#include "media/gpu/shared_memory_region.h"
 #include "ui/gl/gl_context.h"
 #include "ui/gl/gl_image_io_surface.h"
 #include "ui/gl/gl_implementation.h"
@@ -467,9 +468,8 @@ void VTVideoDecodeAccelerator::DecodeTask(
   DCHECK(decoder_thread_.task_runner()->BelongsToCurrentThread());
 
   // Map the bitstream buffer.
-  base::SharedMemory memory(bitstream.handle(), true);
-  size_t size = bitstream.size();
-  if (!memory.Map(size)) {
+  SharedMemoryRegion memory(bitstream, true);
+  if (!memory.Map()) {
     DLOG(ERROR) << "Failed to map bitstream buffer";
     NotifyError(PLATFORM_FAILURE, SFT_PLATFORM_ERROR);
     return;
@@ -488,7 +488,7 @@ void VTVideoDecodeAccelerator::DecodeTask(
   bool has_slice = false;
   size_t data_size = 0;
   std::vector<media::H264NALU> nalus;
-  parser_.SetStream(buf, size);
+  parser_.SetStream(buf, memory.size());
   media::H264NALU nalu;
   while (true) {
     media::H264Parser::Result result = parser_.AdvanceToNextNALU(&nalu);
