7b5755ef78ddfc4ff4dc3081e573f94627900115
jyasskin <jyasskin@chromium.org>
2016-05-24 23:38:30 -0700

    Notify the BluetoothAdapter::Observer when a bluetooth peripheral disconnects on Android and Mac.
    BUG=582537
    TEST=The "Connecting and disconnecting" section of https://goo.gl/XwzFlR#heading=h.efzd105luqv2.
    
    Review-Url: https://codereview.chromium.org/2008113002
    Cr-Commit-Position: refs/heads/master@{#395815}

--- device/bluetooth/bluetooth_device.cc
+++ device/bluetooth/bluetooth_device.cc
@@ -27,7 +27,9 @@ BluetoothDevice::BluetoothDevice(BluetoothAdapter* adapter)
       services_data_(new base::DictionaryValue()) {}
 
 BluetoothDevice::~BluetoothDevice() {
-  DidDisconnectGatt();
+  for (BluetoothGattConnection* connection : gatt_connections_) {
+    connection->InvalidateConnectionReference();
+  }
 }
 
 BluetoothDevice::ConnectionInfo::ConnectionInfo()
@@ -336,6 +338,7 @@ void BluetoothDevice::DidConnectGatt() {
   }
   create_gatt_connection_success_callbacks_.clear();
   create_gatt_connection_error_callbacks_.clear();
+  GetAdapter()->NotifyDeviceChanged(this);
 }
 
 void BluetoothDevice::DidFailToConnectGatt(ConnectErrorCode error) {
@@ -359,6 +362,7 @@ void BluetoothDevice::DidDisconnectGatt() {
     connection->InvalidateConnectionReference();
   }
   gatt_connections_.clear();
+  GetAdapter()->NotifyDeviceChanged(this);
 }
 
 void BluetoothDevice::AddGattConnection(BluetoothGattConnection* connection) {
