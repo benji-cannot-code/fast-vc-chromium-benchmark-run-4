87d9e26d67e458b11188059c62dbd65956772f86
mstensho <mstensho@opera.com>
2016-05-25 05:43:24 -0700

    Remove unnecessary inclusions of LayoutObject-derived headers in core/animation/
    BUG=614015
    TBR=alancutter@chromium.org,eae@chromium.org
    
    Review-Url: https://codereview.chromium.org/2009583003
    Cr-Commit-Position: refs/heads/master@{#395871}

--- third_party/WebKit/Source/core/css/resolver/StyleAdjuster.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleAdjuster.cpp
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLPlugInElement.h"
 #include "core/html/HTMLTableCellElement.h"
 #include "core/html/HTMLTextAreaElement.h"
-#include "core/layout/LayoutReplaced.h"
 #include "core/layout/LayoutTheme.h"
 #include "core/style/ComputedStyle.h"
 #include "core/style/ComputedStyleConstants.h"
