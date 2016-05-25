93f6ccf56f9d43a1cf1b33e799c3d28868561531
cmihail <cmihail@chromium.org>
2016-05-24 19:30:38 -0700

    Add verbs API to file handlers. Modify the Chrome OS UI so that it displayes the internationalized verb together with the extension name in the file manager.
    BUG=578725
    R=fukino@chromium.org
    
    Review-Url: https://codereview.chromium.org/1872223002
    Cr-Commit-Position: refs/heads/master@{#395782}

--- ui/file_manager/file_manager/foreground/js/file_manager_commands.js
+++ ui/file_manager/file_manager/foreground/js/file_manager_commands.js
@@ -1023,7 +1023,7 @@ CommandHandler.COMMANDS_['default-task'] = /** @type {Command} */ ({
 });
 
 /**
- * Displays open with dialog for current selection.
+ * Displays "open with"/"more actions" dialog for current selection.
  * @type {Command}
  */
 CommandHandler.COMMANDS_['open-with'] = /** @type {Command} */ ({
@@ -1034,7 +1034,7 @@ CommandHandler.COMMANDS_['open-with'] = /** @type {Command} */ ({
   execute: function(event, fileManager) {
     fileManager.taskController.getFileTasks().then(function(tasks) {
       tasks.showTaskPicker(fileManager.ui.defaultTaskPicker,
-          str('OPEN_WITH_BUTTON_LABEL'),
+          str('MORE_ACTIONS_BUTTON_LABEL'),
           '',
           function(task) {
             tasks.execute(task.taskId);
@@ -1051,7 +1051,7 @@ CommandHandler.COMMANDS_['open-with'] = /** @type {Command} */ ({
    * @param {!FileManager} fileManager FileManager to use.
    */
   canExecute: function(event, fileManager) {
-    var canExecute = fileManager.taskController.canExecuteOpenWith();
+    var canExecute = fileManager.taskController.canExecuteMoreActions();
     event.canExecute = canExecute;
     event.command.setHidden(!canExecute);
   }
