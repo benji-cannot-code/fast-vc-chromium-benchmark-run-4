8e28e1408d59f4fc45211cde422b5792addb2b3e
jkarlin <jkarlin@chromium.org>
2016-05-25 07:05:38 -0700

    Record the number of freshness periods since a stale HttpCache entry was last used
    This metrics records the number of freshness periods that have elapsed since a stale entry was last used. This will help us to determine the right time to prefresh resources (e.g., prefresh on cache hit if the entry's age is > 50% of freshness).
    
    BUG=614355
    
    Review-Url: https://codereview.chromium.org/2005753004
    Cr-Commit-Position: refs/heads/master@{#395885}

--- net/http/mock_http_cache.cc
+++ net/http/mock_http_cache.cc
@@ -84,11 +84,11 @@ std::string MockDiskEntry::GetKey() const {
 }
 
 base::Time MockDiskEntry::GetLastUsed() const {
-  return base::Time::FromInternalValue(0);
+  return base::Time::Now();
 }
 
 base::Time MockDiskEntry::GetLastModified() const {
-  return base::Time::FromInternalValue(0);
+  return base::Time::Now();
 }
 
 int32_t MockDiskEntry::GetDataSize(int index) const {
