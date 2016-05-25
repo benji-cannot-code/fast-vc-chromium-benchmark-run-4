5c74d5cab582b9801005018067f76577e44cc973
mstensho <mstensho@opera.com>
2016-05-25 02:08:21 -0700

    Remove unnecessary inclusions of LayoutObject-derived headers in core/inspector/
    BUG=614015
    TBR=dgozman@chromium.org,eae@chromium.org
    
    Review-Url: https://codereview.chromium.org/2002153004
    Cr-Commit-Position: refs/heads/master@{#395838}

--- third_party/WebKit/Source/core/inspector/InspectorCSSAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorCSSAgent.cpp
@@ -69,7 +69,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/layout/LayoutObject.h"
 #include "core/layout/LayoutObjectInlines.h"
 #include "core/layout/LayoutText.h"
-#include "core/layout/LayoutTextFragment.h"
 #include "core/layout/api/LayoutViewItem.h"
 #include "core/layout/line/InlineTextBox.h"
 #include "core/loader/DocumentLoader.h"
