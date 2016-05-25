bc9403414a9e60357ecc13531e5748c4c209908a
ellyjones <ellyjones@chromium.org>
2016-05-25 10:22:51 -0700

    omnibox: announce inline autocompletes
    Announce via VoiceOver when autocompletes are available in the omnibox.
    
    BUG=580141
    
    Review-Url: https://codereview.chromium.org/1999823002
    Cr-Commit-Position: refs/heads/master@{#395921}

--- chrome/browser/ui/cocoa/omnibox/omnibox_view_mac.mm
+++ chrome/browser/ui/cocoa/omnibox/omnibox_view_mac.mm
@@ -40,12 +40,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/clipboard.h"
 #include "ui/base/clipboard/clipboard_util_mac.h"
 #import "ui/base/cocoa/cocoa_base_utils.h"
+#import "ui/base/l10n/l10n_util_mac.h"
 #include "ui/base/material_design/material_design_controller.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/font.h"
 #include "ui/gfx/font_list.h"
 #include "ui/gfx/geometry/rect.h"
+
+// TODO(ellyjones): Remove this when the deployment target is 10.9 or later.
+extern NSString* const NSAccessibilityPriorityKey;
 
 using content::WebContents;
 
@@ -658,6 +662,8 @@ bool OmniboxViewMac::OnInlineAutocompleteTextMaybeChanged(
   model()->OnChanged();
   [field_ clearUndoChain];
 
+  AnnounceAutocompleteForScreenReader(display_text);
+
   return true;
 }
 
@@ -1147,4 +1153,19 @@ NSUInteger OmniboxViewMac::GetTextLength() const {
 bool OmniboxViewMac::IsCaretAtEnd() const {
   const NSRange selection = GetSelectedRange();
   return NSMaxRange(selection) == GetTextLength();
+}
+
+void OmniboxViewMac::AnnounceAutocompleteForScreenReader(
+    const base::string16& display_text) {
+  NSString* announcement =
+      l10n_util::GetNSStringF(IDS_ANNOUNCEMENT_COMPLETION_AVAILABLE_MAC,
+                              display_text);
+  NSDictionary* notification_info = @{
+      NSAccessibilityAnnouncementKey : announcement,
+      NSAccessibilityPriorityKey :     @(NSAccessibilityPriorityHigh)
+  };
+  NSAccessibilityPostNotificationWithUserInfo(
+      [field_ window],
+      NSAccessibilityAnnouncementRequestedNotification,
+      notification_info);
 }
