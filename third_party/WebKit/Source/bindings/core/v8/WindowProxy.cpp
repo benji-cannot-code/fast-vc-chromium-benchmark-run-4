e954cca48b69a45007f463c9a5f6d94577d2c7a3
peria <peria@chromium.org>
2016-05-24 22:49:51 -0700

    Add a user metric to track time spent in WindowProxy::initialize to track how efficiently we will reduce it.
    BUG=325923,588893
    
    Review-Url: https://codereview.chromium.org/2003293003
    Cr-Commit-Position: refs/heads/master@{#395810}

--- third_party/WebKit/Source/bindings/core/v8/WindowProxy.cpp
+++ third_party/WebKit/Source/bindings/core/v8/WindowProxy.cpp
@@ -226,6 +226,7 @@ bool WindowProxy::initialize()
 {
     TRACE_EVENT0("v8", "WindowProxy::initialize");
     TRACE_EVENT_SCOPED_SAMPLING_STATE("blink", "InitializeWindow");
+    SCOPED_BLINK_UMA_HISTOGRAM_TIMER("Blink.Binding.InitializeWindowProxy");
 
     ScriptForbiddenScope::AllowUserAgentScript allowScript;
 
