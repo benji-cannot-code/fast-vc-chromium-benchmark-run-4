0326b36b9fead82c17661d380eba37edbd1811d9
nick <nick@chromium.org>
2016-05-25 14:29:42 -0700

    Task Manager: Make double-clicking on an extension do something useful.
    Extension background pages, etc, don't implement anything for
    WebContentsDelegate::ActivateContents; as a result, double-clicking
    on them does nothing. This change makes it so that we open
    the chrome-extensions:// page instead, highlighting the details
    for the particular extension.
    
    Make double-clicking on a subframe row have the same behavior as
    double-clicking on the main row for the tab.
    
    BUG=614529
    
    Review-Url: https://codereview.chromium.org/2006423002
    Cr-Commit-Position: refs/heads/master@{#395990}

--- chrome/browser/task_management/providers/web_contents/extension_task.cc
+++ chrome/browser/task_management/providers/web_contents/extension_task.cc
@@ -6,8 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/task_management/providers/web_contents/extension_task.h"
 
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/chrome_pages.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_contents.h"
+#include "extensions/browser/process_manager.h"
 #include "extensions/browser/view_type_utils.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
@@ -57,6 +62,32 @@ void ExtensionTask::UpdateTitle() {
 void ExtensionTask::UpdateFavicon() {
   // We don't care about the favicon of the WebContents but rather of the
   // extension.
+}
+
+void ExtensionTask::Activate() {
+  // This task represents the extension view of (for example) a background page
+  // or browser action button, so there is no top-level window to bring to the
+  // front. Instead, when this task is double-clicked, we bring up the
+  // chrome://extensions page in a tab, and highlight the details for this
+  // extension.
+  //
+  // TODO(nick): For extensions::VIEW_TYPE_APP_WINDOW, and maybe others, there
+  // may actually be a window we could focus. Special case those here as needed.
+  const extensions::Extension* extension =
+      extensions::ProcessManager::Get(web_contents()->GetBrowserContext())
+          ->GetExtensionForWebContents(web_contents());
+
+  if (!extension)
+    return;
+
+  Browser* browser = chrome::FindTabbedBrowser(
+      Profile::FromBrowserContext(web_contents()->GetBrowserContext()), true);
+
+  // If an existing browser isn't found, don't create a new one.
+  if (!browser)
+    return;
+
+  chrome::ShowExtensions(browser, extension->id());
 }
 
 Task::Type ExtensionTask::GetType() const {
