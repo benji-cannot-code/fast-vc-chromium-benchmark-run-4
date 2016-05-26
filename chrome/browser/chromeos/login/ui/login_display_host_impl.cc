0c67e0b606ebf662b6ee3ef1d398710bdc59235b
jonross <jonross@chromium.org>
2016-05-26 13:07:49 -0700

    Have Mash Login use the correct window container
    Update LoginDisplayHostImpl to set the Login container when chrome is running in Mash.
    Add the Login container to the mash activation containers so that keyboard focus can be gained.
    
    TEST=manual testing in mash shell
    BUG=557399
    
    Review-Url: https://codereview.chromium.org/2013653002
    Cr-Commit-Position: refs/heads/master@{#396265}

--- chrome/browser/chromeos/login/ui/login_display_host_impl.cc
+++ chrome/browser/chromeos/login/ui/login_display_host_impl.cc
@@ -105,6 +105,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget_delegate.h"
 #include "url/gurl.h"
 
+#if defined(MOJO_SHELL_CLIENT)
+#include "components/mus/public/cpp/property_type_converters.h"
+#include "mash/wm/public/interfaces/container.mojom.h"
+#endif
+
 namespace {
 
 // Maximum delay for startup sound after 'loginPromptVisible' signal.
@@ -1141,6 +1146,14 @@ void LoginDisplayHostImpl::InitLoginWindowAndView() {
     params.parent =
         ash::Shell::GetContainer(ash::Shell::GetPrimaryRootWindow(),
                                  ash::kShellWindowId_LockScreenContainer);
+  } else {
+#if defined(MOJO_SHELL_CLIENT)
+    params.mus_properties[mash::wm::mojom::kWindowContainer_Property] =
+        mojo::ConvertTo<std::vector<uint8_t>>(
+            static_cast<int32_t>(mash::wm::mojom::Container::LOGIN_WINDOWS));
+#else
+    NOTREACHED();
+#endif
   }
   login_window_ = new views::Widget;
   params.delegate = new LoginWidgetDelegate(login_window_);
