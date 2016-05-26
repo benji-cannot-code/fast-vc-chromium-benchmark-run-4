0d3b6b6edc9af316f7d87ad563999fdf7565288b
alemate <alemate@chromium.org>
2016-05-25 22:07:51 -0700

    ChromeOS: disable ToggleMaximize for login and lock screen.
    ChromeOS login and lock screens must be always maximized even on touch devices.
    
    BUG=600756
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2013963004
    Cr-Commit-Position: refs/heads/master@{#396123}

--- ash/wm/maximize_mode/maximize_mode_window_manager.cc
+++ ash/wm/maximize_mode/maximize_mode_window_manager.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_switches.h"
 #include "ash/root_window_controller.h"
+#include "ash/session/session_state_delegate.h"
 #include "ash/shell.h"
 #include "ash/shell_window_ids.h"
 #include "ash/wm/common/window_state.h"
@@ -165,6 +166,21 @@ void MaximizeModeWindowManager::OnDisplayMetricsChanged(const display::Display&,
 }
 
 void MaximizeModeWindowManager::OnTouchEvent(ui::TouchEvent* event) {
+  const SessionStateDelegate* delegate =
+      Shell::GetInstance()->session_state_delegate();
+
+  if (delegate->IsScreenLocked())
+    return;
+
+  switch (delegate->GetSessionState()) {
+    case SessionStateDelegate::SESSION_STATE_LOGIN_PRIMARY:
+      return;
+    case SessionStateDelegate::SESSION_STATE_ACTIVE:
+      break;
+    case SessionStateDelegate::SESSION_STATE_LOGIN_SECONDARY:
+      return;
+  }
+
   if (event->type() != ui::ET_TOUCH_PRESSED)
     return;
 
