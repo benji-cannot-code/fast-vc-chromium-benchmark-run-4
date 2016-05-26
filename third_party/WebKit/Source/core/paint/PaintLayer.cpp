fadbd66f68cb6960ce1bf67af2676c061e9a7990
chrishtr <chrishtr@chromium.org>
2016-05-25 20:21:01 -0700

    Refactor to simplify how layerType is stored on PaintLayers.
    Instead of caching it on the PaintLayer, delegate to the LayoutBoxModelObject
    to compute it on demand. Also remove a useless setter for it.
    
    BUG=610906
    
    Review-Url: https://codereview.chromium.org/2002153007
    Cr-Commit-Position: refs/heads/master@{#396096}

--- third_party/WebKit/Source/core/paint/PaintLayer.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayer.cpp
@@ -133,9 +133,8 @@ PaintLayerRareData::~PaintLayerRareData()
 {
 }
 
-PaintLayer::PaintLayer(LayoutBoxModelObject* layoutObject, PaintLayerType type)
-    : m_layerType(type)
-    , m_hasSelfPaintingLayerDescendant(false)
+PaintLayer::PaintLayer(LayoutBoxModelObject* layoutObject)
+    : m_hasSelfPaintingLayerDescendant(false)
     , m_hasSelfPaintingLayerDescendantDirty(false)
     , m_isRootLayer(layoutObject->isLayoutView())
     , m_visibleContentStatusDirty(true)
@@ -2445,7 +2444,7 @@ bool PaintLayer::shouldBeSelfPaintingLayer() const
 {
     if (layoutObject()->isLayoutPart() && toLayoutPart(layoutObject())->requiresAcceleratedCompositing())
         return true;
-    return m_layerType == NormalPaintLayer
+    return layoutObject()->layerTypeRequired() == NormalPaintLayer
         || (m_scrollableArea && m_scrollableArea->hasOverlayScrollbars())
         || needsCompositedScrolling();
 }
