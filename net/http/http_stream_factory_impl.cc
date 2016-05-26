b69dd14237abfe39f104ced4e9ebcba3e42348af
bnc <bnc@chromium.org>
2016-05-26 04:03:12 -0700

    Add Net.AlternativeServiceTypeForRequest histogram.
    Add histogram to record the type of AlternativeService
    HttpStreamFactory::GetAlternativeServiceFor() returns: none, QUIC, or other, and
    whether the destination host is different from the origin.
    
    BUG=501434
    
    Review-Url: https://codereview.chromium.org/2015623002
    Cr-Commit-Position: refs/heads/master@{#396158}

--- net/http/http_stream_factory_impl.cc
+++ net/http/http_stream_factory_impl.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
 #include "net/http/http_network_session.h"
@@ -182,6 +183,33 @@ const HostMappingRules* HttpStreamFactoryImpl::GetHostMappingRules() const {
 }
 
 AlternativeService HttpStreamFactoryImpl::GetAlternativeServiceFor(
+    const HttpRequestInfo& request_info,
+    HttpStreamRequest::Delegate* delegate,
+    HttpStreamRequest::StreamType stream_type) {
+  AlternativeService alternative_service =
+      GetAlternativeServiceForInternal(request_info, delegate, stream_type);
+  AlternativeServiceType type;
+  if (alternative_service.protocol == UNINITIALIZED_ALTERNATE_PROTOCOL) {
+    type = NO_ALTERNATIVE_SERVICE;
+  } else if (alternative_service.protocol == QUIC) {
+    if (request_info.url.host() == alternative_service.host) {
+      type = QUIC_SAME_DESTINATION;
+    } else {
+      type = QUIC_DIFFERENT_DESTINATION;
+    }
+  } else {
+    if (request_info.url.host() == alternative_service.host) {
+      type = NOT_QUIC_SAME_DESTINATION;
+    } else {
+      type = NOT_QUIC_DIFFERENT_DESTINATION;
+    }
+  }
+  UMA_HISTOGRAM_ENUMERATION("Net.AlternativeServiceTypeForRequest", type,
+                            MAX_ALTERNATIVE_SERVICE_TYPE);
+  return alternative_service;
+}
+
+AlternativeService HttpStreamFactoryImpl::GetAlternativeServiceForInternal(
     const HttpRequestInfo& request_info,
     HttpStreamRequest::Delegate* delegate,
     HttpStreamRequest::StreamType stream_type) {
