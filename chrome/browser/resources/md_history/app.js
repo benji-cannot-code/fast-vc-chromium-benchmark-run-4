79a84c38bdda1f7e169ec89c0f5d2f2a96a0a865
calamity <calamity@chromium.org>
2016-05-25 20:25:35 -0700

    [MD History] Lazily render synced devices.
    This CL puts the synced device manager behind a dom-if which shaves off
    some startup time for MD History.
    
    BUG=589355
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2003443002
    Cr-Commit-Position: refs/heads/master@{#396099}

--- chrome/browser/resources/md_history/app.js
+++ chrome/browser/resources/md_history/app.js
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *            results: ?Array<!HistoryEntry>,
  *            info: ?HistoryQuery,
  *            range: HistoryRange,
- *            groupedOffset: number}}
+ *            groupedOffset: number,
+ *            sessionList: ?Array<!ForeignSession>}}
  */
 var QueryState;
 
@@ -18,9 +19,7 @@ Polymer({
 
   properties: {
     // The id of the currently selected page.
-    selectedPage_: {
-      type: String,
-    },
+    selectedPage_: String,
 
     // Whether domain-grouped history is enabled.
     grouped_: Boolean,
@@ -46,6 +45,7 @@ Polymer({
           range: HistoryRange.ALL_TIME,
           // TODO(calamity): Make history toolbar buttons change the offset.
           groupedOffset: 0,
+          sessionList: null,
         };
       }
     },
@@ -202,12 +202,7 @@ Polymer({
     if (!isTabSyncEnabled)
       return;
 
-    // TODO(calamity): Add a 'no synced devices' message when sessions are
-    // empty.
-    var syncedDeviceElem = this.$['history-synced-device-manager'];
-    var syncedDeviceManager =
-        /** @type {HistorySyncedDeviceManagerElement} */(syncedDeviceElem);
-    syncedDeviceManager.setSyncedHistory(sessionList);
+    this.set('queryState_.sessionList', sessionList);
   },
 
   getSelectedPage: function(selectedPage, range) {
@@ -216,4 +211,8 @@ Polymer({
 
     return selectedPage;
   },
+
+  syncedTabsSelected_(selectedPage) {
+    return selectedPage == 'history-synced-device-manager';
+  }
 });
