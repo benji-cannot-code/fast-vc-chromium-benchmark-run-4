25f48e490ed7d6185147cf0864b51f81bf5d9e63
wangxianzhu <wangxianzhu@chromium.org>
2016-05-26 14:54:54 -0700

    Use original repaint tests for spv2
    We'll still need rect-based paint invalidation for spv2 at least for
    some time, and repaint tests for spv1 have been modified to be suitable
    for spv2, so remove paint/invalidation/spv2 (will be in a separate CL
    to reduce size of this one) and run the original repaint tests for
    spv2.
    
    BUG=524134
    
    Review-Url: https://codereview.chromium.org/2000323008
    Cr-Commit-Position: refs/heads/master@{#396291}

--- third_party/WebKit/Source/core/testing/Internals.h
+++ third_party/WebKit/Source/core/testing/Internals.h
@@ -291,10 +291,6 @@ public:
     void updateLayoutIgnorePendingStylesheetsAndRunPostLayoutTasks(Node*, ExceptionState&);
     void forceFullRepaint(Document*, ExceptionState&);
 
-    void startTrackingPaintInvalidationObjects();
-    void stopTrackingPaintInvalidationObjects();
-    Vector<String> trackedPaintInvalidationObjects();
-
     ClientRectList* draggableRegions(Document*, ExceptionState&);
     ClientRectList* nonDraggableRegions(Document*, ExceptionState&);
 
