919eeaf7dd165fb25e85104e28196d7bf1deedbd
tomhudson <tomhudson@google.com>
2016-05-26 10:07:46 -0700

    Rule out new setMatrixClip() overrides via presubmit
    Chrome is being migrated from SkDevice to new APIs; update the
    global presubmit script now that we've *removed* the dependency
    on onSetMatrixClip() so we don't have new instances added while
    we fix up other Skia clients.
    
    R=fmalita@chromium.org,cpu@chromium.org
    BUG=609894
    
    Review-Url: https://codereview.chromium.org/1996593002
    Cr-Commit-Position: refs/heads/master@{#396208}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -195,6 +195,15 @@ _BANNED_CPP_FUNCTIONS = (
       ),
     ),
     (
+      'setMatrixClip',
+      (
+        'Overriding setMatrixClip() is prohibited; ',
+        'the base function is deprecated. ',
+      ),
+      True,
+      (),
+    ),
+    (
       'skia::RefPtr',
       (
         'The use of skia::RefPtr is prohibited. ',
