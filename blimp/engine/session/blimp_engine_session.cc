7f857dc12ea9a03b91fbb93274b514e8849a696f
wez <wez@chromium.org>
2016-05-26 15:26:26 -0700

    Clean up ThreadPipeManager comments and constructor signature.
    The ThreadPipeManager class-level comments and naming no longer make
    assumptions about the layout of the calling code.
    
    BUG=574884
    
    Review-Url: https://codereview.chromium.org/2001363004
    Cr-Commit-Position: refs/heads/master@{#396301}

--- blimp/engine/session/blimp_engine_session.cc
+++ blimp/engine/session/blimp_engine_session.cc
@@ -291,8 +291,6 @@ void BlimpEngineSession::RegisterFeatures() {
   thread_pipe_manager_.reset(new ThreadPipeManager(
       content::BrowserThread::GetMessageLoopProxyForThread(
           content::BrowserThread::IO),
-      content::BrowserThread::GetMessageLoopProxyForThread(
-          content::BrowserThread::UI),
       net_components_->GetBrowserConnectionHandler()));
 
   // Register features' message senders and receivers.
