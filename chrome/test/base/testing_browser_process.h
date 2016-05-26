d6d574478cafba2da5440856e893a9f45f0277ff
dgn <dgn@chromium.org>
2016-05-26 13:02:36 -0700

    Avoid notifying about crashed extension during shutdown
    Some extension can crash during shutdown, which brings up a
    notification. Notifications now keep the browser running, and
    requesting to show one while we are shutting down is forbidden
    and causes browser crashes.
    
    BUG=612815
    
    Review-Url: https://codereview.chromium.org/2009343002
    Cr-Commit-Position: refs/heads/master@{#396262}

--- chrome/test/base/testing_browser_process.h
+++ chrome/test/base/testing_browser_process.h
@@ -141,6 +141,7 @@ class TestingBrowserProcess : public BrowserProcess {
   void SetNotificationPlatformBridge(
       std::unique_ptr<NotificationPlatformBridge> notification_platform_bridge);
   void SetRapporService(rappor::RapporService* rappor_service);
+  void SetShuttingDown(bool is_shutting_down);
   void ShutdownBrowserPolicyConnector();
 
  private:
@@ -150,6 +151,7 @@ class TestingBrowserProcess : public BrowserProcess {
 
   std::unique_ptr<content::NotificationService> notification_service_;
   std::string app_locale_;
+  bool is_shutting_down_;
 
   std::unique_ptr<policy::BrowserPolicyConnector> browser_policy_connector_;
   bool created_browser_policy_connector_ = false;
