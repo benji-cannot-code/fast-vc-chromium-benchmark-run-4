e3c5a5c7b496187d8fdd716c083e1e409aa97877
dgn <dgn@chromium.org>
2016-05-27 11:07:42 -0700

    Revert of Make push notifications register KeepAlives
    (patchset #15 id:360001 of
    https://codereview.chromium.org/1849153003/ )
    
    Reason for revert:
    Caused crashes: https://crbug.com/612815.
    
    Reopening the browser from a notification unregisters
    the notification's keepalive before creating the new
    browser window, so we get in situations where we try
    opening a new browser while the shutdown sequence is
    started.
    
    Original issue's description:
    > [bgmode] Make push notifications register KeepAlives
    >
    > A KeepAlive is now registered in the following cases:
    > a) There is an in-flight push message
    > b) A Notification is being shown by the message center
    >
    > That will happen on all platforms supporting Chrome's
    > background mode.
    >
    > BUG=585080
    >
    > Committed: https://crrev.com/f43094f56012798a8bb0144b0e17076abc8c8dd0
    > Cr-Commit-Position: refs/heads/master@{#394194}
    
    TBR=peter@chromium.org,sky@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=585080
    
    Review-Url: https://codereview.chromium.org/2020563003
    Cr-Commit-Position: refs/heads/master@{#396512}

--- chrome/browser/notifications/profile_notification.cc
+++ chrome/browser/notifications/profile_notification.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/stringprintf.h"
 #include "build/build_config.h"
-#include "chrome/browser/lifetime/keep_alive_types.h"
-#include "chrome/browser/lifetime/scoped_keep_alive.h"
 #include "chrome/browser/ui/ash/multi_user/multi_user_util.h"
 #include "components/signin/core/account_id/account_id.h"
 
@@ -32,9 +30,7 @@ ProfileNotification::ProfileNotification(Profile* profile,
           GetProfileNotificationId(
               notification.delegate_id(),
               NotificationUIManager::GetProfileID(profile)),
-          notification),
-      keep_alive_(new ScopedKeepAlive(KeepAliveOrigin::NOTIFICATION,
-                                      KeepAliveRestartOption::DISABLED)) {
+          notification) {
   DCHECK(profile);
 #if defined(OS_CHROMEOS)
   notification_.set_profile_id(
@@ -43,3 +39,4 @@ ProfileNotification::ProfileNotification(Profile* profile,
 }
 
 ProfileNotification::~ProfileNotification() {}
+
--- chrome/browser/notifications/profile_notification.h
+++ chrome/browser/notifications/profile_notification.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/notification.h"
 #include "chrome/browser/notifications/notification_ui_manager.h"
 
-class ScopedKeepAlive;
-
 // This class keeps a Notification objects and its corresponding Profile, so
 // that when the Notification UI manager needs to return or cancel all
 // notifications for a given Profile we have the ability to do this.
@@ -35,10 +33,6 @@ class ProfileNotification {
   ProfileID profile_id_;
 
   Notification notification_;
-
-  std::unique_ptr<ScopedKeepAlive> keep_alive_;
-
-  DISALLOW_COPY_AND_ASSIGN(ProfileNotification);
 };
 
 #endif  // CHROME_BROWSER_NOTIFICATIONS_PROFILE_NOTIFICATION_H_
