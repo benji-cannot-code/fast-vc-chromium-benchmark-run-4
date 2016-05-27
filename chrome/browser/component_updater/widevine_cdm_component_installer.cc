647090333cfc41aece6bf5868009ac26054b3b4a
xhwang <xhwang@chromium.org>
2016-05-27 14:47:49 -0700

    Add logging in WidevineCdmComponentInstaller to help investigate a bug.
    These logs will be removed after the investigation is finished.
    
    BUG=614745
    
    Review-Url: https://codereview.chromium.org/2013403003
    Cr-Commit-Position: refs/heads/master@{#396570}

--- chrome/browser/component_updater/widevine_cdm_component_installer.cc
+++ chrome/browser/component_updater/widevine_cdm_component_installer.cc
@@ -207,6 +207,8 @@ void RegisterWidevineCdmWithChrome(
     const base::Version& cdm_version,
     const base::FilePath& cdm_install_dir,
     std::unique_ptr<base::DictionaryValue> manifest) {
+  LOG(WARNING) << __FUNCTION__;
+
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   const std::string codecs = GetCodecs(*manifest);
 
@@ -318,6 +320,9 @@ bool WidevineCdmComponentInstallerTraits::VerifyInstallation(
 // <profile>\AppData\Local\Google\Chrome\User Data\WidevineCdm\.
 base::FilePath WidevineCdmComponentInstallerTraits::GetRelativeInstallDir()
     const {
+  // Add LOG(WARNING) here and below to help investigate http://crbug.com/614745
+  // TODO(xhwang): Remove LOG(WARNING)s in this file after investigation.
+  LOG(WARNING) << __FUNCTION__;
   return base::FilePath(FILE_PATH_LITERAL("WidevineCdm"));
 }
 
@@ -347,6 +352,8 @@ void WidevineCdmComponentInstallerTraits::UpdateCdmAdapter(
     const base::Version& cdm_version,
     const base::FilePath& cdm_install_dir,
     std::unique_ptr<base::DictionaryValue> manifest) {
+  LOG(WARNING) << __FUNCTION__;
+
   const base::FilePath adapter_version_path =
       GetPlatformDirectory(cdm_install_dir).AppendASCII(kCdmAdapterVersionName);
   const base::FilePath adapter_install_path =
