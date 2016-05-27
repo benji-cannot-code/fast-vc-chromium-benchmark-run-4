5425b2df02a948c51b67f41d2865cbf44766ef03
jamescook <jamescook@chromium.org>
2016-05-26 23:15:54 -0700

    mash: Convert shelf to wm::WmWindow types, part 1
    Shelf and its dependencies need to use wm::WmWindow so they can run both in
    classic ash and mash (with combined desktop_wm / ash_sysui).
    
    * Eliminate ash::RootWindowController use in ShelfWidget and StatusAreaWidget
    * Convert aura::Window use in shelf and status header files to wm::WmWindow
    * Eliminate unnecessary ShelfWidget::container_window_
    
    No functional changes.
    
    BUG=615155
    TEST=existing ash_unittests
    
    Review-Url: https://codereview.chromium.org/2015043002
    Cr-Commit-Position: refs/heads/master@{#396405}

--- ash/shell/window_watcher.cc
+++ ash/shell/window_watcher.cc
@@ -46,7 +46,7 @@ class WindowWatcher::WorkspaceWindowWatcher : public aura::WindowObserver {
     panel_container->AddObserver(watcher_);
 
     aura::Window* container =
-        Shelf::ForWindow(root)->shelf_widget()->window_container();
+        ash::Shell::GetContainer(root, kShellWindowId_ShelfContainer);
     container->AddObserver(this);
     for (size_t i = 0; i < container->children().size(); ++i)
       container->children()[i]->AddObserver(watcher_);
@@ -58,7 +58,7 @@ class WindowWatcher::WorkspaceWindowWatcher : public aura::WindowObserver {
     panel_container->RemoveObserver(watcher_);
 
     aura::Window* container =
-        Shelf::ForWindow(root)->shelf_widget()->window_container();
+        ash::Shell::GetContainer(root, kShellWindowId_ShelfContainer);
     container->RemoveObserver(this);
     for (size_t i = 0; i < container->children().size(); ++i)
       container->children()[i]->RemoveObserver(watcher_);
--- ash/system/status_area_widget.cc
+++ ash/system/status_area_widget.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/status_area_widget.h"
 
-#include "ash/root_window_controller.h"
 #include "ash/shelf/shelf_layout_manager.h"
 #include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
@@ -16,9 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/system_tray.h"
 #include "ash/system/tray/system_tray_delegate.h"
 #include "ash/system/web_notification/web_notification_tray.h"
+#include "ash/wm/common/wm_root_window_controller.h"
+#include "ash/wm/common/wm_window.h"
 #include "ash/wm/window_properties.h"
 #include "base/i18n/time_formatting.h"
-#include "ui/aura/window.h"
 
 #if defined(OS_CHROMEOS)
 #include "ash/system/chromeos/session/logout_button_tray.h"
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-StatusAreaWidget::StatusAreaWidget(aura::Window* status_container,
+StatusAreaWidget::StatusAreaWidget(wm::WmWindow* status_container,
                                    ShelfWidget* shelf_widget)
     : status_area_widget_delegate_(new StatusAreaWidgetDelegate),
       overview_button_tray_(NULL),
@@ -43,8 +43,10 @@ StatusAreaWidget::StatusAreaWidget(aura::Window* status_container,
       views::Widget::InitParams::TYPE_WINDOW_FRAMELESS);
   params.delegate = status_area_widget_delegate_;
   params.name = "StatusAreaWidget";
-  params.parent = status_container;
   params.opacity = views::Widget::InitParams::TRANSLUCENT_WINDOW;
+  status_container->GetRootWindowController()
+      ->ConfigureWidgetInitParamsForContainer(
+          this, status_container->GetShellWindowId(), &params);
   Init(params);
   set_focus_on_creation(false);
   SetContentsView(status_area_widget_delegate_);
