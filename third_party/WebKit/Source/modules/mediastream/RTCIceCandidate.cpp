adaef1e0002c554a9890edec46e191d5e96580f1
guidou <guidou@chromium.org>
2016-05-27 05:46:29 -0700

    Use Init dictionaries in RTCPeerConnection legacy methods.
    In addition, correctly handle the conversion to WebRTCIceCandidate when given a valid RTCIceCandidateInit with null sdpMLineIndex.
    
    BUG=612118
    
    Review-Url: https://codereview.chromium.org/2007923003
    Cr-Commit-Position: refs/heads/master@{#396449}

--- third_party/WebKit/Source/modules/mediastream/RTCIceCandidate.cpp
+++ third_party/WebKit/Source/modules/mediastream/RTCIceCandidate.cpp
@@ -36,11 +36,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptValue.h"
 #include "bindings/core/v8/V8ObjectBuilder.h"
 #include "core/dom/ExceptionCode.h"
+#include "core/dom/ExecutionContext.h"
+#include "core/frame/UseCounter.h"
 #include "modules/mediastream/RTCIceCandidateInit.h"
 
 namespace blink {
 
-RTCIceCandidate* RTCIceCandidate::create(const RTCIceCandidateInit& candidateInit, ExceptionState& exceptionState)
+RTCIceCandidate* RTCIceCandidate::create(ExecutionContext* context, const RTCIceCandidateInit& candidateInit, ExceptionState& exceptionState)
 {
     if (!candidateInit.hasCandidate() || !candidateInit.candidate().length()) {
         exceptionState.throwDOMException(TypeMismatchError, ExceptionMessages::incorrectPropertyType("candidate", "is not a string, or is empty."));
@@ -51,9 +53,12 @@ RTCIceCandidate* RTCIceCandidate::create(const RTCIceCandidateInit& candidateIni
     if (candidateInit.hasSdpMid())
         sdpMid = candidateInit.sdpMid();
 
+    // TODO(guidou): Change default value to -1. crbug.com/614958.
     unsigned short sdpMLineIndex = 0;
     if (candidateInit.hasSdpMLineIndex())
         sdpMLineIndex = candidateInit.sdpMLineIndex();
+    else
+        UseCounter::count(context, UseCounter::RTCIceCandidateDefaultSdpMLineIndex);
 
     return new RTCIceCandidate(WebRTCICECandidate(candidateInit.candidate(), sdpMid, sdpMLineIndex));
 }
