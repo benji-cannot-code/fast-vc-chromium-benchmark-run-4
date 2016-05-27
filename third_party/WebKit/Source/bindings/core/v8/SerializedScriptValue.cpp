32b92b91052fda8823bdd32cdfc36414a716e49b
peria <peria@chromium.org>
2016-05-27 04:15:16 -0700

    Use SerializedScriptValue class as external interface for (de)serialization.
    Before this CL, we use
    - SerializedScriptValueFactory::instance().create() to serialize values, and
    - serializedScriptValueInstance.deserialize() to deserialize it.
    
    and after this CL, we can use
    - SerializedScriptValue::serialize()  to serialize values, and
    - serializedScriptValueInstance.deserialize() to deserialize it.
    
    Now users don't have to know SerializedScriptFactory to serialize values.
    
    BUG=614919
    
    Review-Url: https://codereview.chromium.org/2017893002
    Cr-Commit-Position: refs/heads/master@{#396438}

--- third_party/WebKit/Source/bindings/core/v8/SerializedScriptValue.cpp
+++ third_party/WebKit/Source/bindings/core/v8/SerializedScriptValue.cpp
@@ -59,6 +59,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+PassRefPtr<SerializedScriptValue> SerializedScriptValue::serialize(v8::Isolate* isolate, v8::Local<v8::Value> value, Transferables* transferables, WebBlobInfoArray* blobInfo, ExceptionState& exception)
+{
+    return SerializedScriptValueFactory::instance().create(isolate, value, transferables, blobInfo, exception);
+}
+
+PassRefPtr<SerializedScriptValue> SerializedScriptValue::serialize(const String& str)
+{
+    return SerializedScriptValueFactory::instance().create(str);
+}
+
+PassRefPtr<SerializedScriptValue> SerializedScriptValue::serializeAndSwallowExceptions(v8::Isolate* isolate, v8::Local<v8::Value> value)
+{
+    TrackExceptionState exceptionState;
+    return serialize(isolate, value, nullptr, nullptr, exceptionState);
+}
+
 PassRefPtr<SerializedScriptValue> SerializedScriptValue::create()
 {
     return adoptRef(new SerializedScriptValue);
@@ -112,8 +128,7 @@ PassRefPtr<SerializedScriptValue> SerializedScriptValue::nullValue()
 {
     SerializedScriptValueWriter writer;
     writer.writeNull();
-    String wireData = writer.takeWireString();
-    return adoptRef(new SerializedScriptValue(wireData));
+    return create(writer.takeWireString());
 }
 
 // Convert serialized string to big endian wire data.
