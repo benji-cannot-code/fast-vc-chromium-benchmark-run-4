e90b3cfdd53440ea174bbf722e8f9aaeda552379
yoshiki <yoshiki@chromium.org>
2016-05-26 22:49:50 -0700

    Show message center on lock screen
    This patch does:
    - Show the message center button on tray on the lock screen
    - Show the message to encourage user to log in when user opens message center on lock screen
    - Add SetLockedState(bool) and IsLockedState() method into MessageCenter class.
    - Don't make notifications read when opening the message center on lock screen
    - Add tests
    
    And this patch also fixes the bug that this makes blocked notifications read wrongly.
    
    BUG=599891
    TEST=manually tested
    
    Review-Url: https://codereview.chromium.org/1986493002
    Cr-Commit-Position: refs/heads/master@{#396402}

--- ui/message_center/notification_list_unittest.cc
+++ ui/message_center/notification_list_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/message_center/fake_message_center.h"
 #include "ui/message_center/message_center_style.h"
 #include "ui/message_center/notification_blocker.h"
 #include "ui/message_center/notification_types.h"
@@ -30,7 +31,7 @@ class NotificationListTest : public testing::Test {
   ~NotificationListTest() override {}
 
   void SetUp() override {
-    notification_list_.reset(new NotificationList());
+    notification_list_.reset(new NotificationList(new FakeMessageCenter()));
     counter_ = 0;
   }
 
@@ -163,9 +164,8 @@ TEST_F(NotificationListTest, MessageCenterVisible) {
   ASSERT_EQ(1u, notification_list()->UnreadCount(blockers()));
   ASSERT_EQ(1u, GetPopupCounts());
 
-  // Make the message center visible. It resets the unread count and popup
-  // counts.
-  notification_list()->SetMessageCenterVisible(true, NULL);
+  // Resets the unread count and popup counts.
+  notification_list()->SetNotificationsShown(blockers(), NULL);
   ASSERT_EQ(0u, notification_list()->UnreadCount(blockers()));
   ASSERT_EQ(0u, GetPopupCounts());
 }
@@ -310,8 +310,7 @@ TEST_F(NotificationListTest, Priority) {
   EXPECT_EQ(kMaxVisiblePopupNotifications, GetPopupCounts());
 
   // Low priority: not visible to popups.
-  notification_list()->SetMessageCenterVisible(true, NULL);
-  notification_list()->SetMessageCenterVisible(false, NULL);
+  notification_list()->SetNotificationsShown(blockers(), NULL);
   EXPECT_EQ(0u, notification_list()->UnreadCount(blockers()));
   AddPriorityNotification(LOW_PRIORITY);
   EXPECT_EQ(kMaxVisiblePopupNotifications + 2,
@@ -369,8 +368,7 @@ TEST_F(NotificationListTest, HasPopupsWithSystemPriority) {
   notification_list()->MarkSinglePopupAsShown(normal_id, false);
   notification_list()->MarkSinglePopupAsShown(system_id, false);
 
-  notification_list()->SetMessageCenterVisible(true, NULL);
-  notification_list()->SetMessageCenterVisible(false, NULL);
+  notification_list()->SetNotificationsShown(blockers(), NULL);
   EXPECT_EQ(1u, GetPopupCounts());
 
   // Mark as read -- emulation of mouse click.
