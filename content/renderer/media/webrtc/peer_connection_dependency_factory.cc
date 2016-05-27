003769e047c331181d1c0dc45ee396d549adb9f4
hbos <hbos@chromium.org>
2016-05-27 08:05:19 -0700

    Remove usage of PeerConnectionIdentityStore, it is obsolete.
    By passing nullptr to CreatePeerConnection in
    peer_connection_dependency_factory.cc instead of Chromium's implementation of a
    store, WebRTC uses its own implementation.
    
    Because the store is used to generate the default certificate type, now ECDSA
    P-256, there is no benefit to using PeerConnectionIdentityStore which is only
    optimized for RSA-1024.
    
    BUG=webrtc:5707, webrtc:5708
    
    Review-Url: https://codereview.chromium.org/2019533003
    Cr-Commit-Position: refs/heads/master@{#396469}

--- content/renderer/media/webrtc/peer_connection_dependency_factory.cc
+++ content/renderer/media/webrtc/peer_connection_dependency_factory.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/media_stream.h"
 #include "content/renderer/media/media_stream_video_source.h"
 #include "content/renderer/media/media_stream_video_track.h"
-#include "content/renderer/media/peer_connection_identity_store.h"
 #include "content/renderer/media/rtc_peer_connection_handler.h"
 #include "content/renderer/media/rtc_video_decoder_factory.h"
 #include "content/renderer/media/rtc_video_encoder_factory.h"
@@ -290,12 +289,6 @@ PeerConnectionDependencyFactory::CreatePeerConnection(
   if (!GetPcFactory().get())
     return NULL;
 
-  std::unique_ptr<PeerConnectionIdentityStore> identity_store(
-      new PeerConnectionIdentityStore(
-          base::ThreadTaskRunnerHandle::Get(), GetWebRtcSignalingThread(),
-          GURL(web_frame->document().url()),
-          GURL(web_frame->document().firstPartyForCookies())));
-
   // Copy the flag from Preference associated with this WebFrame.
   P2PPortAllocator::Config port_config;
 
@@ -405,7 +398,7 @@ PeerConnectionDependencyFactory::CreatePeerConnection(
 
   return GetPcFactory()
       ->CreatePeerConnection(config, std::move(port_allocator),
-                             std::move(identity_store), observer)
+                             nullptr, observer)
       .get();
 }
 
