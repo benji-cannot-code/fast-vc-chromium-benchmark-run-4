2037825322a73b481a59a3252b53b22a044abfaf
hbos <hbos@chromium.org>
2016-05-27 07:13:47 -0700

    Remove rtc_peer_connection_handler.cc's OverrideDefaultCertificate which was used to override the default DTLS certificate to use in WebRTC to ECDSA. This is no longer needed since KT_DEFAULT == KT_ECDSA, meaning ECDSA is already the default certificate and calling this function does nothing.
    BUG=601850, 611698
    
    Review-Url: https://codereview.chromium.org/2016323002
    Cr-Commit-Position: refs/heads/master@{#396460}

--- content/renderer/media/webrtc/peer_connection_dependency_factory.cc
+++ content/renderer/media/webrtc/peer_connection_dependency_factory.cc
@@ -62,7 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/WebURL.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
 #include "third_party/WebKit/public/web/WebFrame.h"
-#include "third_party/webrtc/api/dtlsidentitystore.h"
 #include "third_party/webrtc/api/mediaconstraintsinterface.h"
 #include "third_party/webrtc/base/ssladapter.h"
 #include "third_party/webrtc/modules/video_coding/codecs/h264/include/h264.h"
@@ -408,14 +407,6 @@ PeerConnectionDependencyFactory::CreatePeerConnection(
       ->CreatePeerConnection(config, std::move(port_allocator),
                              std::move(identity_store), observer)
       .get();
-}
-
-// static
-rtc::scoped_refptr<rtc::RTCCertificate>
-PeerConnectionDependencyFactory::GenerateDefaultCertificate() {
-  std::unique_ptr<rtc::SSLIdentity> identity(rtc::SSLIdentity::Generate(
-      webrtc::kIdentityName, rtc::KeyParams::ECDSA(rtc::EC_NIST_P256)));
-  return rtc::RTCCertificate::Create(std::move(identity));
 }
 
 scoped_refptr<webrtc::MediaStreamInterface>
