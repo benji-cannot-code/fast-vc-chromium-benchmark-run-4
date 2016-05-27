affa4e3c4eeccf694bee5dc56fb36ae40f52fe2b
xidachen <xidachen@chromium.org>
2016-05-27 12:12:38 -0700

    Avoid copy pixel data in texImage2D(ImageBitmap)
    This is the first patch in order to avoid reading back GPU texture.
    Currently in texImage2D(ImageBitmap), it makes a copy of the pixel data
    stored inside the ImageBitmap (copyBitmapData). We should avoid that.
    To solve that, we use SkImage's peekPixels() API. peekPixels() returns
    true only when the ImageBitmap is stored in the CPU-side RAM. In the
    case when an ImageBitmap is texture backed, this approach will not work.
    
    I will have a follow up patch right after this, to handles the case where
    an ImageBitmap is texture backed.
    
    BUG=613411, 613409
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2016043002
    Cr-Commit-Position: refs/heads/master@{#396529}

--- third_party/WebKit/Source/platform/graphics/gpu/WebGLImageConversion.cpp
+++ third_party/WebKit/Source/platform/graphics/gpu/WebGLImageConversion.cpp
@@ -2314,6 +2314,7 @@ bool WebGLImageConversion::packImageData(
 
 bool WebGLImageConversion::extractImageData(
     const uint8_t* imageData,
+    DataFormat sourceDataFormat,
     const IntSize& imageDataSize,
     GLenum format,
     GLenum type,
@@ -2334,7 +2335,7 @@ bool WebGLImageConversion::extractImageData(
         return false;
     data.resize(packedSize);
 
-    if (!packPixels(imageData, DataFormatRGBA8, width, height, 0, format, type, premultiplyAlpha ? AlphaDoPremultiply : AlphaDoNothing, data.data(), flipY))
+    if (!packPixels(imageData, sourceDataFormat, width, height, 0, format, type, premultiplyAlpha ? AlphaDoPremultiply : AlphaDoNothing, data.data(), flipY))
         return false;
 
     return true;
