c52f7768bff85425bc002ab92f70c4848e85af3a
nparker <nparker@chromium.org>
2016-05-27 12:57:37 -0700

    Component updater for FileTypePolicies.
    BUG=596555
    
    Review-Url: https://codereview.chromium.org/1986403002
    Cr-Commit-Position: refs/heads/master@{#396542}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_browser_main_extra_parts.h"
 #include "chrome/browser/component_updater/cld_component_installer.h"
 #include "chrome/browser/component_updater/ev_whitelist_component_installer.h"
+#include "chrome/browser/component_updater/file_type_policies_component_installer.h"
 #include "chrome/browser/component_updater/origin_trials_component_installer.h"
 #include "chrome/browser/component_updater/pepper_flash_component_installer.h"
 #include "chrome/browser/component_updater/recovery_component_installer.h"
@@ -498,6 +499,8 @@ void RegisterComponentsForUpdate() {
     RegisterSTHSetComponent(cus, path);
 #endif  // defined(OS_ANDROID)
     RegisterOriginTrialsComponent(cus, path);
+
+    RegisterFileTypePoliciesComponent(cus, path);
   }
 
 #if defined(OS_WIN)
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -166,6 +166,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/component_updater/component_updater_resource_throttle.h',
       'browser/component_updater/ev_whitelist_component_installer.cc',
       'browser/component_updater/ev_whitelist_component_installer.h',
+      'browser/component_updater/file_type_policies_component_installer.cc',
+      'browser/component_updater/file_type_policies_component_installer.h',
       'browser/component_updater/origin_trials_component_installer.cc',
       'browser/component_updater/origin_trials_component_installer.h',
       'browser/component_updater/pnacl_component_installer.cc',
