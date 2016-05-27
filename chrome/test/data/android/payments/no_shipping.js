e482d4e8ff0a0cd429ab5056fbbb26fb49ca877a
rouslan <rouslan@chromium.org>
2016-05-27 14:55:28 -0700

    Clean up PaymentRequest integration tests.
    In this patch:
    - JavaScript and CSS is split from HTML.
    - License headers for the test files.
    - Linted JavaScript to follow Google style guide.
    
    BUG=608223
    
    Review-Url: https://codereview.chromium.org/2018133002
    Cr-Commit-Position: refs/heads/master@{#396576}

---
+++ chrome/test/data/android/payments/no_shipping.js
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*
+ * Copyright 2016 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/* global PaymentRequest:false */
+
+/**
+ * Launches the PaymentRequest UI that does not require a shipping address.
+ */
+function buy() {  // eslint-disable-line no-unused-vars
+  try {
+    new PaymentRequest(['visa'], {
+      items: [{
+        id: 'total',
+        label: 'Total',
+        amount: {currency: 'USD', value: '5.00'}
+      }]
+    })
+        .show()
+        .then(function(resp) {
+          resp.complete(true)
+              .then(function() {
+                print(
+                    resp.methodName + '<br>' +
+                    JSON.stringify(resp.details, undefined, 2));
+              })
+              .catch(function(error) {
+                print(error.message);
+              });
+        })
+        .catch(function(error) {
+          print(error.message);
+        });
+  } catch (error) {
+    print(error.message);
+  }
+}
---
+++ chrome/test/data/android/payments/util.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*
+ * Copyright 2016 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/**
+ * Prints the message.
+ * @param {String} msg - The message to print.
+ */
+function print(msg) {  // eslint-disable-line no-unused-vars
+  document.getElementById('result').innerHTML = msg;
+}
+
+/**
+ * Converts ShippingAddress into a dictionary.
+ * @param {ShippingAddress} addr - The object to convert.
+ * @return {object} The dictionary output.
+ */
+function toDictionary(addr) {  // eslint-disable-line no-unused-vars
+  var dict = {};
+  if (addr) {
+    dict.regionCode = addr.regionCode;
+    dict.administrativeArea = addr.administrativeArea;
+    dict.locality = addr.locality;
+    dict.dependentLocality = addr.dependentLocality;
+    dict.addressLine = addr.addressLine;
+    dict.postalCode = addr.postalCode;
+    dict.sortingCode = addr.sortingCode;
+    dict.languageCode = addr.languageCode;
+    dict.organization = addr.organization;
+    dict.recipient = addr.recipient;
+  }
+  return dict;
+}
