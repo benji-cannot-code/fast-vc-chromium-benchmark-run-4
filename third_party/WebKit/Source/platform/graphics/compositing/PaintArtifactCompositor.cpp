20a89bd4ea0007ec21bcf5cce3f0c40bc9f1800b
tomhudson <tomhudson@google.com>
2016-05-26 19:02:48 -0700

    Remove unused Skia headers from Blink
    skia/ext/refptr.h is being deprecated, and was no longer used anyway
    in the files in question.
    
    TBR=fmalita@chromium.org
    BUG=604716,skia:5077
    
    Review-Url: https://codereview.chromium.org/2018673002
    Cr-Commit-Position: refs/heads/master@{#396352}

--- third_party/WebKit/Source/platform/graphics/compositing/PaintArtifactCompositor.cpp
+++ third_party/WebKit/Source/platform/graphics/compositing/PaintArtifactCompositor.cpp
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/Platform.h"
 #include "public/platform/WebCompositorSupport.h"
 #include "public/platform/WebLayer.h"
-#include "skia/ext/refptr.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/point_f.h"
 #include "ui/gfx/geometry/rect.h"
