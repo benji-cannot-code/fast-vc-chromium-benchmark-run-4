33dffda9d4affc34e55c291d2f1b836a17e72635
mdjones <mdjones@chromium.org>
2016-05-26 19:32:29 -0700

    Restore infobars when Reader Mode peeks
    Previously infobars would only be restored if the Reader Mode panel
    was completely closed. This change allows infobars that tried to show
    while the panel was open to show when the panel is in the peeking
    state.
    
    BUG=613150
    
    Review-Url: https://codereview.chromium.org/2011113002
    Cr-Commit-Position: refs/heads/master@{#396360}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/readermode/ReaderModePanel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/readermode/ReaderModePanel.java
@@ -262,6 +262,10 @@ public class ReaderModePanel extends OverlayPanel {
         } else if (mTimerRunning && !animatingToOpenState) {
             onTimerEnded();
         }
+
+        if (getPanelState() == PanelState.PEEKED) {
+            mManagerDelegate.onPeek();
+        }
     }
 
     @Override
