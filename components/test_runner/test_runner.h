127c96442205e6ec0e289f0ca1297d020e42e115
tkent <tkent@chromium.org>
2016-05-26 21:58:25 -0700

    content_shell: Update absolute path rewriting logic again.
    With this CL, we rewrite absolute-looking paths only in
    web-platform-tests and csswg-test. RewriteAbsolutePathInWPT() doesn't affect
    non-W3C tests at all.
    
    BUG=614778
    
    Review-Url: https://codereview.chromium.org/2013203003
    Cr-Commit-Position: refs/heads/master@{#396396}

--- components/test_runner/test_runner.h
+++ components/test_runner/test_runner.h
@@ -133,8 +133,10 @@ class TestRunner : public WebTestRunner {
   bool shouldDumpSpellCheckCallbacks() const;
   bool shouldWaitUntilExternalURLLoad() const;
   const std::set<std::string>* httpHeadersToClear() const;
-  bool did_request_after_reset() const { return did_request_after_reset_; }
-  void set_did_request_after_reset() { did_request_after_reset_ = true; }
+  bool is_web_platform_tests_mode() const {
+    return is_web_platform_tests_mode_;
+  }
+  void set_is_web_platform_tests_mode() { is_web_platform_tests_mode_ = true; }
 
   // To be called when |frame| starts loading - TestRunner will check if
   // there is currently no top-loading-frame being tracked and if so, then it
@@ -640,8 +642,8 @@ class TestRunner : public WebTestRunner {
 
   std::set<blink::WebWidget*> widgets_with_scheduled_animations_;
 
-  // True if we requested any resource after Reset().
-  bool did_request_after_reset_;
+  // True if we run a test in LayoutTests/imported/{csswg-test,wpt}/.
+  bool is_web_platform_tests_mode_;
 
   base::WeakPtrFactory<TestRunner> weak_factory_;
 
