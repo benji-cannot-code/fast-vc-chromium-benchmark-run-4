96ea046114b4c4dd57bfe9d5dd886b926975e7e8
pdr <pdr@chromium.org>
2016-05-26 17:06:54 -0700

    Correctly position abspos content inside relpos inlines [spv2]
    This patch fixes a class of bugs where absolutely positioned content
    in a relative inline container would be positioned relative to the
    origin instead of relative to the inline. This largely mirrors the logic
    in PaintLayer::updateLayerPosition which used
    LayoutInline::offsetForInFlowPositionedInline to position PaintLayers.
    
    BUG=614257
    
    Review-Url: https://codereview.chromium.org/2000423009
    Cr-Commit-Position: refs/heads/master@{#396324}

--- third_party/WebKit/Source/core/paint/PaintPropertyTreeBuilder.cpp
+++ third_party/WebKit/Source/core/paint/PaintPropertyTreeBuilder.cpp
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/Settings.h"
+#include "core/layout/LayoutInline.h"
 #include "core/layout/LayoutPart.h"
 #include "core/paint/ObjectPaintProperties.h"
 #include "core/paint/PaintLayer.h"
@@ -26,6 +27,7 @@ struct PaintPropertyTreeBuilderContext {
         : currentTransform(nullptr)
         , currentClip(nullptr)
         , transformForAbsolutePosition(nullptr)
+        , containerForAbsolutePosition(nullptr)
         , clipForAbsolutePosition(nullptr)
         , transformForFixedPosition(nullptr)
         , clipForFixedPosition(nullptr)
@@ -52,6 +54,7 @@ struct PaintPropertyTreeBuilderContext {
     // are included in the additional context too.
     TransformPaintPropertyNode* transformForAbsolutePosition;
     LayoutPoint paintOffsetForAbsolutePosition;
+    LayoutObject* containerForAbsolutePosition;
     ClipPaintPropertyNode* clipForAbsolutePosition;
 
     TransformPaintPropertyNode* transformForFixedPosition;
@@ -114,7 +117,9 @@ void PaintPropertyTreeBuilder::walk(FrameView& frameView, const PaintPropertyTre
     frameScroll.translate(-scrollOffset.width(), -scrollOffset.height());
     RefPtr<TransformPaintPropertyNode> newTransformNodeForScrollTranslation = TransformPaintPropertyNode::create(frameScroll, FloatPoint3D(), newTransformNodeForPreTranslation);
     localContext.currentTransform = localContext.transformForAbsolutePosition = newTransformNodeForScrollTranslation.get();
-    localContext.paintOffset = localContext.paintOffsetForAbsolutePosition = LayoutPoint();
+    localContext.paintOffset = LayoutPoint();
+    localContext.paintOffsetForAbsolutePosition = LayoutPoint();
+    localContext.containerForAbsolutePosition = nullptr;
 
     frameView.setPreTranslation(newTransformNodeForPreTranslation.release());
     frameView.setScrollTranslation(newTransformNodeForScrollTranslation.release());
@@ -335,6 +340,7 @@ void PaintPropertyTreeBuilder::updateOutOfFlowContext(LayoutObject& object, Pain
         context.transformForAbsolutePosition = context.currentTransform;
         context.paintOffsetForAbsolutePosition = context.paintOffset;
         context.clipForAbsolutePosition = context.currentClip;
+        context.containerForAbsolutePosition = &object;
     }
 
     // TODO(pdr): Remove the !object.isLayoutView() condition when removing FrameView
@@ -380,11 +386,20 @@ static void deriveBorderBoxFromContainerContext(const LayoutObject& object, Pain
     case RelativePosition:
         context.paintOffset += boxModelObject.offsetForInFlowPosition();
         break;
-    case AbsolutePosition:
+    case AbsolutePosition: {
         context.currentTransform = context.transformForAbsolutePosition;
         context.paintOffset = context.paintOffsetForAbsolutePosition;
+
+        // Absolutely positioned content in an inline should be positioned relative to the inline.
+        LayoutObject* container = context.containerForAbsolutePosition;
+        if (container && container->isInFlowPositioned() && container->isLayoutInline()) {
+            DCHECK(object.isBox());
+            context.paintOffset += toLayoutInline(container)->offsetForInFlowPositionedInline(toLayoutBox(object));
+        }
+
         context.currentClip = context.clipForAbsolutePosition;
         break;
+    }
     case StickyPosition:
         context.paintOffset += boxModelObject.offsetForInFlowPosition();
         break;
