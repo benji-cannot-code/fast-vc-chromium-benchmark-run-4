2c346b1294241f90b489cbae9dfbdde1df08b190
thestig <thestig@chromium.org>
2016-05-27 11:53:39 -0700

    Fix a DCHECK failure in PrintJob.
    Review-Url: https://codereview.chromium.org/2015143002
    Cr-Commit-Position: refs/heads/master@{#396524}

--- chrome/browser/printing/print_job.cc
+++ chrome/browser/printing/print_job.cc
@@ -167,7 +167,8 @@ void PrintJob::Stop() {
     ControlledWorkerShutdown();
   } else {
     // Flush the cached document.
-    UpdatePrintedDocument(NULL);
+    is_job_pending_ = false;
+    UpdatePrintedDocument(nullptr);
   }
 }
 
@@ -436,7 +437,7 @@ void PrintJob::ControlledWorkerShutdown() {
 
   is_job_pending_ = false;
   registrar_.RemoveAll();
-  UpdatePrintedDocument(NULL);
+  UpdatePrintedDocument(nullptr);
 }
 
 void PrintJob::HoldUntilStopIsCalled() {
