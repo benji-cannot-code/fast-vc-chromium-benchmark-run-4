4618bf1a0d0b6583942c80dbb2dad65307a117e0
wangxianzhu <wangxianzhu@chromium.org>
2016-05-27 11:28:25 -0700

    Clear all should-keep-alive clients after PaintController commit
    Saw some crash reports at DisplayItemClient::~DisplayItemClient()
    whose call stack is not during painting. Suspect that there are
    some should-keep-alive entries not cleared after painting. Change
    to clear all should-keep-alive entries to ensure the register is
    cleared.
    
    BUG=609218
    
    Review-Url: https://codereview.chromium.org/2020433002
    Cr-Commit-Position: refs/heads/master@{#396517}

--- third_party/WebKit/Source/platform/graphics/paint/DisplayItemClient.cpp
+++ third_party/WebKit/Source/platform/graphics/paint/DisplayItemClient.cpp
@@ -54,10 +54,10 @@ void DisplayItemClient::beginShouldKeepAlive() const
 #endif
 }
 
-void DisplayItemClient::endShouldKeepAlive() const
+void DisplayItemClient::endShouldKeepAliveAllClients()
 {
-    CHECK(isAlive());
-    displayItemClientsShouldKeepAlive->remove(this);
+    delete displayItemClientsShouldKeepAlive;
+    displayItemClientsShouldKeepAlive = nullptr;
 }
 
 #endif // CHECK_DISPLAY_ITEM_CLIENT_ALIVENESS
