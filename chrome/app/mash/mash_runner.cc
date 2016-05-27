b6ae3d687f5c8cfb6a46bc5e6cd43372dcef4683
mfomitchev <mfomitchev@chromium.org>
2016-05-27 09:15:17 -0700

    Mustash: Fix startup for chrome --mash
    When Mustash was started via chrome --mash, some initialization code wasn't getting executed, and as a result stack traces dumping to stderr wasn't working among other things.
    
    - Renamed ChildProcessMain(RunCallback) to ChildProcessRun(RunCallback) to avoid confusion with ChildProcessMain()
    - Moved some init code from ChildProcessMain() to ChildProcessRun(Callback), which is getting called when Mustash is started via chrome --mash.
    
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2008933003
    Cr-Commit-Position: refs/heads/master@{#396484}

--- chrome/app/mash/mash_runner.cc
+++ chrome/app/mash/mash_runner.cc
@@ -197,7 +197,7 @@ void MashRunner::RunMain() {
 
 void MashRunner::RunChild() {
   base::i18n::InitializeICU();
-  shell::ChildProcessMain(
+  shell::ChildProcessMainWithCallback(
       base::Bind(&MashRunner::StartChildApp, base::Unretained(this)));
 }
 
