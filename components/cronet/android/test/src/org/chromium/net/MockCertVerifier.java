127cd5e966e29384c2d09e89e06d894c33f13587
agrieve <agrieve@chromium.org>
2016-05-27 08:57:57 -0700

    Reland of Move side-loaded test data /sdcard -> /sdcard/chromium_tests_root
    Reason for reland:
    Updated webview tests that broke (WebViewLayoutTest.java)
    
    And use delete_device_stale=True when pushing. This will prevent tests
    mistakenly passing when they depend on stale data files from a previous test.
    
    TBR=jbudorick@chromium.org,thakis@chromium.org,mef@chromium.org
    BUG=607169
    
    Review-Url: https://codereview.chromium.org/2018663002
    Cr-Commit-Position: refs/heads/master@{#396479}

--- components/cronet/android/test/src/org/chromium/net/MockCertVerifier.java
+++ components/cronet/android/test/src/org/chromium/net/MockCertVerifier.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.net;
 
 import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.test.util.UrlUtils;
 
 /**
  * A Java wrapper to supply a net::MockCertVerifier which can be then passed
@@ -23,8 +24,8 @@ public class MockCertVerifier {
      * @return a pointer to the newly created net::MockCertVerifier.
      */
     public static long createMockCertVerifier(String[] certs) {
-        return nativeCreateMockCertVerifier(certs);
+        return nativeCreateMockCertVerifier(certs, UrlUtils.getIsolatedTestRoot());
     }
 
-    private static native long nativeCreateMockCertVerifier(String[] certs);
+    private static native long nativeCreateMockCertVerifier(String[] certs, String testDataDir);
 }
