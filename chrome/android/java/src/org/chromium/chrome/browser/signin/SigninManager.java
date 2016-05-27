59cbf9ceb59193450adfe81075afe25aec9cb14d
gogerald <gogerald@chromium.org>
2016-05-27 06:55:49 -0700

    Abort sign in if the sign in flow activity was set but no longer visible to the user
    The root cause of the below bug: onSaveInstanceState is called when Activity is stopped, after that we can not attach Fragment to it.
    
    BUG=615129
    
    Review-Url: https://codereview.chromium.org/2013283002
    Cr-Commit-Position: refs/heads/master@{#396456}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManager.java
@@ -177,11 +177,13 @@ public class SigninManager implements AccountTrackerService.OnSystemAccountsSeed
         }
 
         /**
-         * Returns whether the sign-in flow activity was set but is no longer valid.
+         * Returns whether the sign-in flow activity was set but is no longer visible to the user.
          */
-        private boolean isActivityDestroyed() {
+        private boolean isActivityInvisible() {
             return activity != null
-                    && ApplicationStatus.getStateForActivity(activity) == ActivityState.DESTROYED;
+                    && (ApplicationStatus.getStateForActivity(activity) == ActivityState.STOPPED
+                               || ApplicationStatus.getStateForActivity(activity)
+                                       == ActivityState.DESTROYED);
         }
     }
 
@@ -391,7 +393,7 @@ public class SigninManager implements AccountTrackerService.OnSystemAccountsSeed
             return;
         }
 
-        if (mSignInState.isActivityDestroyed()) {
+        if (mSignInState.isActivityInvisible()) {
             abortSignIn();
             return;
         }
@@ -418,7 +420,7 @@ public class SigninManager implements AccountTrackerService.OnSystemAccountsSeed
             return;
         }
 
-        if (mSignInState.isActivityDestroyed()) {
+        if (mSignInState.isActivityInvisible()) {
             abortSignIn();
             return;
         }
