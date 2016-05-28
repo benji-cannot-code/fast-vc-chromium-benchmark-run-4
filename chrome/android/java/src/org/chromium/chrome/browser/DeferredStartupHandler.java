f0dc4b15526d63cbefd8eced7287352fa84dfde0
pkotwicz <pkotwicz@chromium.org>
2016-05-28 11:35:16 -0700

    Upstream: Extract "WebAPK service" implementation from Chrome APK
    Ideally as much WebAPK code as possible can be updated without a new WebAPK
    being downloaded from the Chrome server. This CL makes the WebAPK extract
    a dex with the "WebAPK service" implementation from the Chrome APK and use
    the extracted dex via reflection.
    
    BUG=609122
    
    Review-Url: https://codereview.chromium.org/2011613003
    Cr-Commit-Position: refs/heads/master@{#396645}

--- chrome/android/java/src/org/chromium/chrome/browser/DeferredStartupHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/DeferredStartupHandler.java
@@ -10,6 +10,7 @@ import android.os.AsyncTask;
 import android.os.SystemClock;
 import android.text.TextUtils;
 
+import org.chromium.base.CommandLine;
 import org.chromium.base.FieldTrialList;
 import org.chromium.base.PowerMonitor;
 import org.chromium.base.SysUtils;
@@ -28,6 +29,7 @@ import org.chromium.chrome.browser.precache.PrecacheLauncher;
 import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
 import org.chromium.chrome.browser.preferences.privacy.PrivacyPreferencesManager;
 import org.chromium.chrome.browser.share.ShareHelper;
+import org.chromium.chrome.browser.webapps.WebApkVersionManager;
 import org.chromium.content.browser.ChildProcessLauncher;
 
 import java.util.concurrent.TimeUnit;
@@ -97,6 +99,10 @@ public class DeferredStartupHandler {
 
                     // Initialize whether or not precaching is enabled.
                     PrecacheLauncher.updatePrecachingEnabled(application);
+
+                    if (CommandLine.getInstance().hasSwitch(ChromeSwitches.ENABLE_WEBAPK)) {
+                        WebApkVersionManager.updateWebApksIfNeeded();
+                    }
 
                     return null;
                 } finally {
