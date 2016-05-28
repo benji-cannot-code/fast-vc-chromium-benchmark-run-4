3d1d5314661e94ab93a5e7a2456a97ac05a6b6e9
jbauman <jbauman@chromium.org>
2016-05-27 21:10:06 -0700

    Plumb decoded video pixel format from GPU process to renderer.
    This allows the renderer to correctly interpret NV12 and other pixel formats.
    
    BUG=574292
    
    Review-Url: https://codereview.chromium.org/1942123002
    Cr-Commit-Position: refs/heads/master@{#396634}

--- media/gpu/ipc/client/gpu_video_decode_accelerator_host.cc
+++ media/gpu/ipc/client/gpu_video_decode_accelerator_host.cc
@@ -212,6 +212,7 @@ void GpuVideoDecodeAcceleratorHost::OnBitstreamBufferProcessed(
 
 void GpuVideoDecodeAcceleratorHost::OnProvidePictureBuffer(
     uint32_t num_requested_buffers,
+    VideoPixelFormat format,
     uint32_t textures_per_buffer,
     const gfx::Size& dimensions,
     uint32_t texture_target) {
@@ -225,8 +226,9 @@ void GpuVideoDecodeAcceleratorHost::OnProvidePictureBuffer(
   }
 
   if (client_) {
-    client_->ProvidePictureBuffers(num_requested_buffers, textures_per_buffer,
-                                   dimensions, texture_target);
+    client_->ProvidePictureBuffers(num_requested_buffers, format,
+                                   textures_per_buffer, dimensions,
+                                   texture_target);
   }
 }
 
