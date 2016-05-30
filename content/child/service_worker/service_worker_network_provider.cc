7e0765828e5e799f7a7e484dcf5c73d3214eb4f8
falken <falken@chromium.org>
2016-05-29 23:33:01 -0700

    service worker: Remove unused PROVIDER_FOR_SANDBOXED_IFRAME
    Clean-up only. This was added in https://codereview.chromium.org/1191293002/
    then became unused in https://codereview.chromium.org/1399363004. Originally it
    signaled to the ServiceWorkerNetworkProvider ctor that the provider id should
    be set to invalid; now the default ctor is used accomplish that.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2023733002
    Cr-Commit-Position: refs/heads/master@{#396685}

--- content/child/service_worker/service_worker_network_provider.cc
+++ content/child/service_worker/service_worker_network_provider.cc
@@ -25,15 +25,6 @@ int GetNextProviderId() {
   return sequence.GetNext();  // We start at zero.
 }
 
-// When the provider is for a sandboxed iframe we use
-// kInvalidServiceWorkerProviderId as the provider type and we don't create
-// ServiceWorkerProviderContext and ServiceWorkerProviderHost.
-int GenerateProviderIdForType(const ServiceWorkerProviderType provider_type) {
-  if (provider_type == SERVICE_WORKER_PROVIDER_FOR_SANDBOXED_FRAME)
-    return kInvalidServiceWorkerProviderId;
-  return GetNextProviderId();
-}
-
 }  // namespace
 
 void ServiceWorkerNetworkProvider::AttachToDocumentState(
@@ -122,7 +113,7 @@ ServiceWorkerNetworkProvider::ServiceWorkerNetworkProvider(
     ServiceWorkerProviderType provider_type)
     : ServiceWorkerNetworkProvider(route_id,
                                    provider_type,
-                                   GenerateProviderIdForType(provider_type)) {}
+                                   GetNextProviderId()) {}
 
 ServiceWorkerNetworkProvider::ServiceWorkerNetworkProvider()
     : provider_id_(kInvalidServiceWorkerProviderId) {}
