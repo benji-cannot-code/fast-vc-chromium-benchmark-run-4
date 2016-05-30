4a6a7d0b93b657434e1294456b832790d0a92787
asvitkine <asvitkine@chromium.org>
2016-05-30 15:01:02 -0700

    Remove debug instrumentation for crbug.com/359406.
    There are no more signs of the crash. This CL reverts the code
    to its original state to verify that the crash doesn't re-appear.
    If it doesn't re-appear, we can conclude that the issue is fixed,
    although it still bothers me that we don't know why.
    
    BUG=359406
    
    Review-Url: https://codereview.chromium.org/2024683002
    Cr-Commit-Position: refs/heads/master@{#396753}

--- chrome/renderer/chrome_render_thread_observer.h
+++ chrome/renderer/chrome_render_thread_observer.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/field_trial.h"
-#include "base/process/process.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "content/public/renderer/render_thread_observer.h"
 
@@ -63,9 +62,7 @@ class ChromeRenderThreadObserver : public content::RenderThreadObserver,
   void OnSetContentSettingRules(const RendererContentSettingRules& rules);
   void OnGetCacheResourceStats();
   void OnSetFieldTrialGroup(const std::string& fiel_trial_name,
-                            const std::string& group_name,
-                            base::ProcessId sender_pid,
-                            int32_t debug_token);
+                            const std::string& group_name);
 
   static bool is_incognito_process_;
   std::unique_ptr<content::ResourceDispatcherDelegate> resource_delegate_;
