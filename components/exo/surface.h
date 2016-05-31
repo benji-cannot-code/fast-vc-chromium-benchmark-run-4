f78c192b17d8fce934e82cb1b3e27cdb87228449
reveman <reveman@chromium.org>
2016-05-31 13:35:30 -0700

    exo: Implement lazy cursor changes.
    Pointer::SetCursor() is now required to be called in response to
    focus change events. This provides more seamless cursor changes
    as the cursor will remain the same until it has been re-defined.
    
    Simply clients that never define the cursor will now get the
    default cursor instead of no cursor at all. Clients that draw
    their own cursor now have a chance to provide a new frame before
    the cursor is changed.
    
    BUG=549781
    
    Review-Url: https://codereview.chromium.org/2028443002
    Cr-Commit-Position: refs/heads/master@{#396919}

--- components/exo/surface.h
+++ components/exo/surface.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <list>
 #include <memory>
+#include <set>
 #include <utility>
 
 #include "base/callback.h"
@@ -40,9 +41,14 @@ class Path;
 
 namespace exo {
 class Buffer;
+class Pointer;
 class SurfaceDelegate;
 class SurfaceObserver;
 class Surface;
+
+// The pointer class is currently the only cursor provider class but this can
+// change in the future when better hardware cursor support is added.
+using CursorProvider = Pointer;
 
 // This class owns the SurfaceFactory and keeps track of references to the
 // contents of Buffers. It's keeped alive by references from
@@ -167,6 +173,14 @@ class Surface : public aura::Window,
 
   // Returns the bounds of the surface area that is not know to be transparent.
   gfx::Rect GetNonTransparentBounds() const;
+
+  // Surface does not own cursor providers. It is the responsibility of the
+  // caller to remove the cursor provider before it is destroyed.
+  void RegisterCursorProvider(CursorProvider* provider);
+  void UnregisterCursorProvider(CursorProvider* provider);
+
+  // Returns true if surface has at least one cursor provider registered.
+  bool HasCursorProvider() const;
 
   // Set the surface delegate.
   void SetSurfaceDelegate(SurfaceDelegate* delegate);
@@ -305,6 +319,9 @@ class Surface : public aura::Window,
 
   // The compsitor being observer or null if not observing a compositor.
   ui::Compositor* compositor_;
+
+  // Cursor providers. Surface does not own the cursor providers.
+  std::set<CursorProvider*> cursor_providers_;
 
   // This can be set to have some functions delegated. E.g. ShellSurface class
   // can set this to handle Commit() and apply any double buffered state it
