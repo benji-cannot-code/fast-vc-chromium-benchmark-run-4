6de5214a26dd59dace82c477d993b6a23829643a
mdjones <mdjones@chromium.org>
2016-05-31 10:29:49 -0700

    Tab switcher uses theme colors
    This change allows the thumbnail decoration in the tab
    switcher to use the theme color of the web page. The
    feature can be enabled or disabled with the flag:
    tab-switcher-theme-colors
    
    BUG=606612
    
    Review-Url: https://codereview.chromium.org/2009713002
    Cr-Commit-Position: refs/heads/master@{#396852}

--- chrome/browser/android/compositor/layer/tab_layer.cc
+++ chrome/browser/android/compositor/layer/tab_layer.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/android/compositor/layer/tab_layer.h"
 
+#include "base/base_switches.h"
+#include "base/command_line.h"
 #include "base/i18n/rtl.h"
 #include "cc/layers/layer.h"
 #include "cc/layers/layer_collections.h"
@@ -17,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/compositor/layer/toolbar_layer.h"
 #include "chrome/browser/android/compositor/layer_title_cache.h"
 #include "chrome/browser/android/compositor/tab_content_manager.h"
+#include "chrome/common/chrome_switches.h"
 #include "content/public/browser/android/compositor.h"
 #include "ui/android/resources/resource_manager.h"
 #include "ui/base/l10n/l10n_util_android.h"
@@ -476,6 +479,21 @@ void TabLayer::SetProperties(int id,
     front_border_->SetBounds(border_size);
     front_border_->SetOpacity(border_alpha);
     front_border_->SetNearestNeighbor(toolbar_visible);
+
+    // Colorize the tab decoration.
+    if (tab_switcher_themes_enabled_ && !incognito_
+        && toolbar_background_color != toolbar_background_color_) {
+      toolbar_background_color_ = toolbar_background_color;
+      cc::FilterOperations filters;
+      SkScalar colorMatrix[] = {
+          SkColorGetR(toolbar_background_color) / 255.0f, 0, 0, 0, 0,
+          0, SkColorGetG(toolbar_background_color) / 255.0f, 0, 0, 0,
+          0, 0, SkColorGetB(toolbar_background_color) / 255.0f, 0, 0,
+          0, 0, 0, 1, 0,
+      };
+      filters.Append(cc::FilterOperation::CreateColorMatrixFilter(colorMatrix));
+      front_border_->SetFilters(filters);
+    }
   }
 
   front_border_inner_shadow_->SetHideLayerAndSubtree(
@@ -622,6 +640,8 @@ TabLayer::TabLayer(bool incognito,
                    LayerTitleCache* layer_title_cache,
                    TabContentManager* tab_content_manager)
     : incognito_(incognito),
+      toolbar_background_color_(0),
+      tab_switcher_themes_enabled_(false),
       resource_manager_(resource_manager),
       layer_title_cache_(layer_title_cache),
       layer_(cc::Layer::Create()),
@@ -659,6 +679,10 @@ TabLayer::TabLayer(bool incognito,
   back_logo_->SetIsDrawable(true);
 
   front_border_->SetFillCenter(false);
+
+  tab_switcher_themes_enabled_ =
+      base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableTabSwitcherThemeColors);
 }
 
 TabLayer::~TabLayer() {
