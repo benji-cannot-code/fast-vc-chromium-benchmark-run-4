60bbcb20e90bff3f6bb1d6cf1db00f47474caddb
nick <nick@chromium.org>
2016-05-31 14:36:42 -0700

    TableView: use a stable_sort instead of a sort, since the comparisons may not be totally ordered.
    BUG=606985
    
    Review-Url: https://codereview.chromium.org/2026793002
    Cr-Commit-Position: refs/heads/master@{#396937}

--- ui/views/controls/table/table_view.cc
+++ ui/views/controls/table/table_view.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include <algorithm>
 #include <map>
 #include <utility>
 
@@ -661,9 +662,11 @@ void TableView::SortItemsAndUpdateMapping() {
       GroupSortHelper sort_helper(this);
       GetModelIndexToRangeStart(grouper_, RowCount(),
                                 &sort_helper.model_index_to_range_start);
-      std::sort(view_to_model_.begin(), view_to_model_.end(), sort_helper);
+      std::stable_sort(view_to_model_.begin(), view_to_model_.end(),
+                       sort_helper);
     } else {
-      std::sort(view_to_model_.begin(), view_to_model_.end(), SortHelper(this));
+      std::stable_sort(view_to_model_.begin(), view_to_model_.end(),
+                       SortHelper(this));
     }
     for (int i = 0; i < row_count; ++i)
       model_to_view_[view_to_model_[i]] = i;
