bc0b2eda12ea3ac5086ace8918bc932aa36f3dc4
kojii <kojii@chromium.org>
2016-05-31 12:41:49 -0700

    Import wpt@d510ec1abc30eee4c855c13842bc2f0dfa791f8b
    Using update-w3c-deps in Chromium 3537e717b45ba20b348dc20c903ffa47d8a60303.
    
    No new failures but messages were updated by the changes to testharness.js.
    
    TBR=dpranke@chromium.org, jsbell@chromium.org, tkent@chromium.org, kochi@chromium.org
    
    Review-Url: https://codereview.chromium.org/2022203002
    Cr-Commit-Position: refs/heads/master@{#396897}

--- third_party/WebKit/LayoutTests/imported/wpt/common/form-submission.py
+++ third_party/WebKit/LayoutTests/imported/wpt/common/form-submission.py
@@ -5,6 +5,11 @@ def main(request, response):
             return 'OK'
         else:
             return 'FAIL'
+    elif request.headers.get('Content-Type') == 'text/plain':
+        if request.body == 'qux=baz\r\n':
+            return 'OK'
+        else:
+            return 'FAIL'
     else:
         if request.POST.first('foo') == 'bar':
             return 'OK'
