b1d014d190c68c25c700806bad7da6ff9dfd79a0
mastiz <mastiz@chromium.org>
2016-05-31 06:37:34 -0700

    [New Tab Page] Record snippet click duration
    The high-level idea is to approximate user engagement by measuring how
    much time the user has spent in the page linked by a snippet card.
    
    Various exit conditions are proposed:
    1. The tab is not visible anymore.
    2. User navigates back to the NTP.
    3. User enters a new address/term in the omnibox.
    4. Any page transition marked as navigation begin or end.
    
    BUG=605146
    
    Review-Url: https://codereview.chromium.org/2013653003
    Cr-Commit-Position: refs/heads/master@{#396823}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/MostVisitedItem.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/MostVisitedItem.java
@@ -29,7 +29,7 @@ public class MostVisitedItem implements OnCreateContextMenuListener,
          * Navigates to a most visited page in the existing tab.
          * @param item The most visited item to open.
          */
-        void open(MostVisitedItem item);
+        void openMostVisitedItem(MostVisitedItem item);
 
         /**
          * Allows the manager to add context menu items for a given MostVisitedItem.
@@ -167,6 +167,6 @@ public class MostVisitedItem implements OnCreateContextMenuListener,
 
     @Override
     public void onClick(View v) {
-        mManager.open(this);
+        mManager.openMostVisitedItem(this);
     }
 }
