41f061ba992a6e8caf9d09c52184a252b8877e22
chrishtr <chrishtr@chromium.org>
2016-05-31 14:35:13 -0700

    Clean up some dead code accidentally left around in cc DisplayItem subtypes.
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review-Url: https://codereview.chromium.org/2025893003
    Cr-Commit-Position: refs/heads/master@{#396935}

--- cc/playback/compositing_display_item.h
+++ cc/playback/compositing_display_item.h
@@ -83,7 +83,6 @@ class CC_EXPORT EndCompositingDisplayItem : public DisplayItem {
   size_t ExternalMemoryUsage() const override;
 
   int ApproximateOpCount() const { return 0; }
-  bool IsSuitableForGpuRasterization() const { return true; }
 };
 
 }  // namespace cc
