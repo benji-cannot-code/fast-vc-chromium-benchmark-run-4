accf120d6d9376153411d433f96e56288bdda91f
Dominic Cooney <dominicc@chromium.org>
2016-05-31 14:52:01 +0900

    Add a queue for custom element reactions.
    https://html.spec.whatwg.org/multipage/scripting.html#custom-element-reaction-queue
    
    BUG=594918
    R=kojii@chromium.org
    
    Review URL: https://codereview.chromium.org/2021763002 .
    
    Cr-Commit-Position: refs/heads/master@{#396790}

---
+++ third_party/WebKit/Source/core/dom/custom/CustomElementReaction.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CustomElementReaction_h
+#define CustomElementReaction_h
+
+#include "core/CoreExport.h"
+#include "platform/heap/Handle.h"
+#include "wtf/Noncopyable.h"
+
+namespace blink {
+
+class Element;
+
+class CORE_EXPORT CustomElementReaction
+    : public GarbageCollectedFinalized<CustomElementReaction> {
+    WTF_MAKE_NONCOPYABLE(CustomElementReaction);
+public:
+    CustomElementReaction() { }
+    virtual ~CustomElementReaction() { }
+    DEFINE_INLINE_VIRTUAL_TRACE() { }
+    virtual void invoke(Element*) = 0;
+};
+
+} // namespace blink
+
+#endif // CustomElementReaction_h
