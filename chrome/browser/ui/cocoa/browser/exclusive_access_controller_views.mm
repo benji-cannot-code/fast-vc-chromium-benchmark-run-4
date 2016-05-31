788e603b60ec063eabae4f2ede21bbe99b54ff07
mgiuca <mgiuca@chromium.org>
2016-05-30 17:22:42 -0700

    Added popup that shows the new Back shortcut on Mac.
    Implements BrowserWindowCocoa::ShowNewBackShortcutBubble to open the
    Views implementation of this bubble (similar to how the fullscreen
    bubble works on Mac).
    
    BUG=610039
    
    Review-Url: https://codereview.chromium.org/2017813004
    Cr-Commit-Position: refs/heads/master@{#396759}

--- chrome/browser/ui/cocoa/browser/exclusive_access_controller_views.mm
+++ chrome/browser/ui/cocoa/browser/exclusive_access_controller_views.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
 #include "chrome/browser/ui/status_bubble.h"
 #include "chrome/browser/ui/views/exclusive_access_bubble_views.h"
+#include "chrome/browser/ui/views/new_back_shortcut_bubble.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "ui/base/cocoa/cocoa_base_utils.h"
@@ -35,6 +36,13 @@ ExclusiveAccessController::~ExclusiveAccessController() {}
 
 void ExclusiveAccessController::Show() {
   views_bubble_.reset(new ExclusiveAccessBubbleViews(this, url_, bubble_type_));
+}
+
+void ExclusiveAccessController::ShowNewBackShortcutBubble(bool forward) {
+  if (new_back_shortcut_bubble_)
+    new_back_shortcut_bubble_->UpdateContent(forward);
+  else
+    new_back_shortcut_bubble_.reset(new NewBackShortcutBubble(this, forward));
 }
 
 void ExclusiveAccessController::Destroy() {
