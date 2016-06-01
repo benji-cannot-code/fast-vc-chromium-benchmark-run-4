533491013c7c5e4e4f1718d6558297f3d39e1d18
dougsteed <dougsteed@chromium.org>
2016-06-01 13:19:45 -0700

    Add declarations needed to build cast_shell_media with mojo.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2025623007
    Cr-Commit-Position: refs/heads/master@{#397227}

--- chromecast/browser/media/cast_mojo_media_client.cc
+++ chromecast/browser/media/cast_mojo_media_client.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "chromecast/browser/media/cast_renderer.h"
+#include "media/base/media_log.h"
+#include "media/base/renderer_factory.h"
 
 namespace {
 class CastRendererFactory : public media::RendererFactory {
