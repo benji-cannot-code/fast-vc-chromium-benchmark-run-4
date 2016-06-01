4ff9ebedb4b5c55cb4a5718bc6eecc851361480a
bnc <bnc@chromium.org>
2016-06-01 03:37:11 -0700

    Disable AltSvc from insecure origin.
    * Do not parse Alt-Svc response on insecure connections, add regression test.
    * Do not follow in-memory Alt-Svc on insecure connections, add regression test.
    
    This CL has a shortcut: |enable_alt_svc_for_insecure_origin|, which is false in
    production but true in tests (except for the two regression tests).  Adapting
    the existing tests is a lot of work, that will come in one or more follow-up
    CLs.
    
    BUG=615497
    
    Review-Url: https://codereview.chromium.org/2026863002
    Cr-Commit-Position: refs/heads/master@{#397097}

--- net/spdy/spdy_test_util_common.cc
+++ net/spdy/spdy_test_util_common.cc
@@ -344,6 +344,7 @@ SpdySessionDependencies::SpdySessionDependencies(NextProto protocol)
       enable_priority_dependencies(true),
       enable_spdy31(true),
       enable_quic(false),
+      enable_alternative_service_for_insecure_origins(true),
       protocol(protocol),
       session_max_recv_window_size(
           SpdySession::GetDefaultInitialWindowSize(protocol)),
@@ -382,6 +383,7 @@ SpdySessionDependencies::SpdySessionDependencies(
       enable_priority_dependencies(true),
       enable_spdy31(true),
       enable_quic(false),
+      enable_alternative_service_for_insecure_origins(true),
       protocol(protocol),
       session_max_recv_window_size(
           SpdySession::GetDefaultInitialWindowSize(protocol)),
@@ -433,6 +435,8 @@ HttpNetworkSession::Params SpdySessionDependencies::CreateSessionParams(
       session_deps->enable_priority_dependencies;
   params.enable_spdy31 = session_deps->enable_spdy31;
   params.enable_quic = session_deps->enable_quic;
+  params.enable_alternative_service_for_insecure_origins =
+      session_deps->enable_alternative_service_for_insecure_origins;
   params.spdy_default_protocol = session_deps->protocol;
   params.spdy_session_max_recv_window_size =
       session_deps->session_max_recv_window_size;
