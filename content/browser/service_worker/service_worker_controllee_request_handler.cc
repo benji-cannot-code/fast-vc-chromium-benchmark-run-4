29ab964457eb73ec47afdb50a9d478b49282807d
shimazu <shimazu@chromium.org>
2016-05-31 21:54:47 -0700

    ServiceWorker: Distinguish SW.PageLoad w/ and w/o fetch handler
    This patch separates ServiceWorker.PageLoad for serviceworker into the two: w/
    and w/o fetch handler in order to measure the impact of the no-fetch handler
    optimization.
    
    BUG=605844
    
    Review-Url: https://codereview.chromium.org/2025813002
    Cr-Commit-Position: refs/heads/master@{#397060}

--- content/browser/service_worker/service_worker_controllee_request_handler.cc
+++ content/browser/service_worker/service_worker_controllee_request_handler.cc
@@ -268,7 +268,8 @@ ServiceWorkerControlleeRequestHandler::DidLookupRegistrationForMainResource(
     return;
   }
 
-  ServiceWorkerMetrics::CountControlledPageLoad(stripped_url_);
+  ServiceWorkerMetrics::CountControlledPageLoad(
+      stripped_url_, active_version->has_fetch_handler());
   bool is_forwarded =
       MaybeForwardToServiceWorker(job_.get(), active_version.get());
 
@@ -296,7 +297,8 @@ void ServiceWorkerControlleeRequestHandler::OnVersionStatusChanged(
     return;
   }
 
-  ServiceWorkerMetrics::CountControlledPageLoad(stripped_url_);
+  ServiceWorkerMetrics::CountControlledPageLoad(stripped_url_,
+                                                version->has_fetch_handler());
 
   provider_host_->AssociateRegistration(registration,
                                         false /* notify_controllerchange */);
