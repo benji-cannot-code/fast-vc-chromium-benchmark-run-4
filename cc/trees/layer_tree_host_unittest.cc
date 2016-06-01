fb4fc4925287f267f8ba634a10d93755ab7b8ecb
gab <gab@chromium.org>
2016-06-01 13:09:39 -0700

    Migrate WaitableEvent to enum-based constructor in cc/
    Change automated with clang-tidy (details @ https://crbug.com/612843#c13)
    
    BUG=612843
    
    Review-Url: https://codereview.chromium.org/2031673002
    Cr-Commit-Position: refs/heads/master@{#397220}

--- cc/trees/layer_tree_host_unittest.cc
+++ cc/trees/layer_tree_host_unittest.cc
@@ -5346,7 +5346,8 @@ MULTI_THREAD_TEST_F(LayerTreeHostAcceptsDeltasFromImplWithoutRootLayer);
 class LayerTreeHostTestCrispUpAfterPinchEnds : public LayerTreeHostTest {
  protected:
   LayerTreeHostTestCrispUpAfterPinchEnds()
-      : playback_allowed_event_(true, true) {}
+      : playback_allowed_event_(base::WaitableEvent::ResetPolicy::MANUAL,
+                                base::WaitableEvent::InitialState::SIGNALED) {}
 
   void SetupTree() override {
     frame_ = 1;
@@ -5648,7 +5649,8 @@ class LayerTreeHostTestContinuousDrawWhenCreatingVisibleTiles
     : public LayerTreeHostTest {
  protected:
   LayerTreeHostTestContinuousDrawWhenCreatingVisibleTiles()
-      : playback_allowed_event_(true, true) {}
+      : playback_allowed_event_(base::WaitableEvent::ResetPolicy::MANUAL,
+                                base::WaitableEvent::InitialState::SIGNALED) {}
 
   void SetupTree() override {
     step_ = 1;
