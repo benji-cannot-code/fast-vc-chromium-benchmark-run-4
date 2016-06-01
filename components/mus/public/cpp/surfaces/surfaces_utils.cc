af792bb6e42a1da53534c29b1e95a0b95fc575fd
fsamuel <fsamuel@chromium.org>
2016-06-01 05:13:26 -0700

    Implement gfx::Transform StructTraits
    This has test coverage through surfaces unittests, but I will
    add a standalone test in a separate patch.
    
    BUG=611802
    
    Review-Url: https://codereview.chromium.org/2028573003
    Cr-Commit-Position: refs/heads/master@{#397110}

--- components/mus/public/cpp/surfaces/surfaces_utils.cc
+++ components/mus/public/cpp/surfaces/surfaces_utils.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
-#include "ui/gfx/mojo/transform_type_converters.h"
 #include "ui/gfx/transform.h"
 
 using mus::mojom::Pass;
@@ -19,7 +18,6 @@ namespace mojo {
 
 SharedQuadStatePtr CreateDefaultSQS(const gfx::Size& size) {
   SharedQuadStatePtr sqs = SharedQuadState::New();
-  sqs->quad_to_target_transform = Transform::From(gfx::Transform());
   sqs->quad_layer_bounds = size;
   sqs->visible_quad_layer_rect = gfx::Rect(size);
   sqs->clip_rect = gfx::Rect(size);
@@ -38,7 +36,6 @@ PassPtr CreateDefaultPass(int id, const gfx::Rect& rect) {
   pass->id = render_pass_id;
   pass->output_rect = rect;
   pass->damage_rect = rect;
-  pass->transform_to_root_target = Transform::From(gfx::Transform());
   pass->has_transparent_background = false;
   return pass;
 }
--- components/mus/ws/platform_display.cc
+++ components/mus/ws/platform_display.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_utils.h"
 #include "ui/events/mojo/input_events_type_converters.h"
 #include "ui/events/mojo/mojo_extended_key_event_data.h"
-#include "ui/gfx/mojo/transform_type_converters.h"
 #include "ui/platform_window/platform_ime_controller.h"
 #include "ui/platform_window/platform_window.h"
 
