94cdde0c060c09d2860e826e14aaa4053dc48a8e
anthonyvd <anthonyvd@chromium.org>
2016-06-01 15:49:54 -0700

    Properly handle dialogClose message in password separated sign in flow.
    This fixes an issue where the flow would stay open when breaking to a
    full tab during SAML sign in flows.
    
    BUG=615062
    TEST=
    1. Enable #enable-password-separated-signin-flow in chrome://flags
    2. Signin flow window properly closes after all sign ins
    3. Signin flow window properly closes after entering SAML email and
       opening SAML provider's page in full tab.
    
    Review-Url: https://codereview.chromium.org/2023763013
    Cr-Commit-Position: refs/heads/master@{#397276}

--- chrome/browser/ui/webui/signin/inline_login_handler.cc
+++ chrome/browser/ui/webui/signin/inline_login_handler.cc
@@ -61,6 +61,9 @@ void InlineLoginHandler::RegisterMessages() {
   web_ui()->RegisterMessageCallback("navigationButtonClicked",
       base::Bind(&InlineLoginHandler::HandleNavigationButtonClicked,
                  base::Unretained(this)));
+  web_ui()->RegisterMessageCallback("dialogClose",
+      base::Bind(&InlineLoginHandler::HandleDialogClose,
+                 base::Unretained(this)));
 }
 
 void InlineLoginHandler::HandleInitializeMessage(const base::ListValue* args) {
@@ -276,4 +279,11 @@ void InlineLoginHandler::HandleNavigationButtonClicked(
   DCHECK(browser);
 
   browser->signin_view_controller()->delegate()->PerformNavigation();
+}
+
+void InlineLoginHandler::HandleDialogClose(const base::ListValue* args) {
+  Browser* browser = GetDesktopBrowser(web_ui());
+  DCHECK(browser);
+
+  browser->CloseModalSigninWindow();
 }
