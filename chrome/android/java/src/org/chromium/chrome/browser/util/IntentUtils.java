35fbb0ddab0c0918994083adde7d1cab408243f4
pkotwicz <pkotwicz@chromium.org>
2016-06-01 11:38:01 -0700

    Mark IntentUtils#safeGetBinderExtra() as VisibleForTesting to unbreak downstream tests
    BUG=616509
    R=dfalcantara
    
    Review-Url: https://codereview.chromium.org/2024293005
    Cr-Commit-Position: refs/heads/master@{#397190}

--- chrome/android/java/src/org/chromium/chrome/browser/util/IntentUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/util/IntentUtils.java
@@ -244,6 +244,7 @@ public class IntentUtils {
      * Creates a temporary copy of the extra Bundle, which is required as
      * Intent#getBinderExtra() doesn't exist, but Bundle.getBinder() does.
      */
+    @VisibleForTesting
     public static IBinder safeGetBinderExtra(Intent intent, String name) {
         if (!intent.hasExtra(name)) return null;
         Bundle extras = intent.getExtras();
