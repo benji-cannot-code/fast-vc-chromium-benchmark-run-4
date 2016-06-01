6c47a2ffb92e7b412ccc3b41e177d96569d7d07e
tsergeant <tsergeant@chromium.org>
2016-05-31 21:04:31 -0700

    MD WebUI: Improve tab focus behavior for toolbar search field
    In wide mode, ensure the input field gets focus rather than the search
    icon when tabbing. The toolbar will only expand when the user starts
    typing.
    
    BUG=615318
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2025583002
    Cr-Commit-Position: refs/heads/master@{#397049}

--- chrome/test/data/webui/cr_elements_browsertest.js
+++ chrome/test/data/webui/cr_elements_browsertest.js
@@ -37,7 +37,7 @@ TEST_F('CrElementsBrowserTest', 'CrToolbarSearchFieldTest', function() {
     /** @param {string} term */
     function simulateSearch(term) {
       field.$.searchInput.bindValue = term;
-      field.onSearchTermSearch_();
+      field.onSearchTermSearch();
     }
 
     /**
