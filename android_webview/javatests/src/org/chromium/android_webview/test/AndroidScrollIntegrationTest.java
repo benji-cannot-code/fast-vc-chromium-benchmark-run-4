0330f8307c6c9b65be2cbf7ab86e4d91699fd5cf
boliu <boliu@chromium.org>
2016-06-01 10:57:24 -0700

    aw: Disable testTouchScrollCanBeAlteredByUi
    For multiprocess only. Test is flaky.
    
    BUG=616505
    TBR=sgurun
    
    Review-Url: https://codereview.chromium.org/2027243002
    Cr-Commit-Position: refs/heads/master@{#397179}

--- android_webview/javatests/src/org/chromium/android_webview/test/AndroidScrollIntegrationTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AndroidScrollIntegrationTest.java
@@ -17,6 +17,7 @@ import org.chromium.android_webview.test.util.JavascriptEventObserver;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.annotations.SuppressFBWarnings;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.parameter.ParameterizedTest;
 import org.chromium.content.browser.test.util.CallbackHelper;
 import org.chromium.content_public.browser.GestureStateListener;
 import org.chromium.net.test.util.TestWebServer;
@@ -439,6 +440,7 @@ public class AndroidScrollIntegrationTest extends AwTestBase {
 
     @SmallTest
     @Feature({"AndroidWebView"})
+    @ParameterizedTest.Set  // crbug.com/616505
     public void testTouchScrollCanBeAlteredByUi() throws Throwable {
         final TestAwContentsClient contentsClient = new TestAwContentsClient();
         final ScrollTestContainerView testContainerView =
