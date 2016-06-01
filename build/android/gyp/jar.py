e83fe81d544278e01b18f7c3e250afa22d5d8745
mikecase <mikecase@chromium.org>
2016-06-01 14:58:26 -0700

    [Android] Build changes needed for Robolectric 3.0.
    Robolectric uses javac annotation processors when building. This
    means it will need to pass some "-processor RobolectricProcessor" to
    javac as well as some "-Akey=value" args
    
    Additionally, since this processor is run by javac, Robolectric can't
    be compiled using the interface.jars. This is why I am adding an
    enable_interface_jars_javac option for GN.
    
    Finally, Robolectric uses the Java services functionality. I will
    need to add some configs to the META-INF/services/ dir in the
    Robolectric jars for this to work correctly. This is the purpose
    of the --provider-configurations option I am adding.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2014983003
    Cr-Commit-Position: refs/heads/master@{#397262}

--- build/android/gyp/jar.py
+++ build/android/gyp/jar.py
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import optparse
 import os
+import shutil
 import sys
 
 from util import build_utils
@@ -20,7 +21,8 @@ _RESOURCE_CLASSES = [
 ]
 
 
-def Jar(class_files, classes_dir, jar_path, manifest_file=None):
+def Jar(class_files, classes_dir, jar_path, manifest_file=None,
+        provider_configurations=None):
   jar_path = os.path.abspath(jar_path)
 
   # The paths of the files in the jar will be the same as they are passed in to
@@ -34,6 +36,15 @@ def Jar(class_files, classes_dir, jar_path, manifest_file=None):
     jar_cmd.append(os.path.abspath(manifest_file))
   jar_cmd.extend(class_files_rel)
 
+  if provider_configurations:
+    service_dir = os.path.join(jar_cwd, 'META-INF', 'services')
+    if not os.path.exists(service_dir):
+      os.makedirs(service_dir)
+    for config in provider_configurations:
+      config_jar_path = os.path.join(service_dir, os.path.basename(config))
+      shutil.copy(config, config_jar_path)
+      jar_cmd.append(os.path.relpath(config_jar_path, jar_cwd))
+
   if not class_files_rel:
     empty_file = os.path.join(classes_dir, '.empty')
     build_utils.Touch(empty_file)
@@ -42,12 +53,14 @@ def Jar(class_files, classes_dir, jar_path, manifest_file=None):
   build_utils.Touch(jar_path, fail_if_missing=True)
 
 
-def JarDirectory(classes_dir, jar_path, manifest_file=None, predicate=None):
+def JarDirectory(classes_dir, jar_path, manifest_file=None, predicate=None,
+                 provider_configurations=None):
   class_files = build_utils.FindInDirectory(classes_dir, '*.class')
   if predicate:
     class_files = [f for f in class_files if predicate(f)]
 
-  Jar(class_files, classes_dir, jar_path, manifest_file=manifest_file)
+  Jar(class_files, classes_dir, jar_path, manifest_file=manifest_file,
+      provider_configurations=provider_configurations)
 
 
 def main():
