076dd000eac2be3e28429769e773beaf5fd13bc2
shrike <shrike@chromium.org>
2016-06-02 11:21:07 -0700

    [Mac][Material Design] Fix regression with infobar border.
    This cl fixes a regression where infobars no longer displayed their
    border.
    
    R=avi@chromium.org
    BUG=615312
    
    Review-Url: https://codereview.chromium.org/2029893002
    Cr-Commit-Position: refs/heads/master@{#397473}

--- chrome/browser/ui/cocoa/infobars/infobar_gradient_view.mm
+++ chrome/browser/ui/cocoa/infobars/infobar_gradient_view.mm
@@ -53,14 +53,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (!themeProvider)
     return [NSColor blackColor];
 
-  if (!ui::MaterialDesignController::IsModeMaterial()) {
-    BOOL active = [[self window] isMainWindow];
-    return themeProvider->GetNSColor(
-        active ? ThemeProperties::COLOR_TOOLBAR_STROKE :
-                 ThemeProperties::COLOR_TOOLBAR_STROKE_INACTIVE);
-  }
+  BOOL active = [[self window] isMainWindow];
   return themeProvider->GetNSColor(
-             ThemeProperties::COLOR_DETACHED_BOOKMARK_BAR_SEPARATOR);
+      active ? ThemeProperties::COLOR_TOOLBAR_STROKE :
+               ThemeProperties::COLOR_TOOLBAR_STROKE_INACTIVE);
 }
 
 - (void)drawRect:(NSRect)rect {
