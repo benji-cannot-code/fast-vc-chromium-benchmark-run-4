afe2305f72a8694a0aa00a58e5e7b4274285cfa1
nhiroki <nhiroki@chromium.org>
2016-06-02 02:12:54 -0700

    Worker: Attempt to gracefully terminate WorkerThread as much as possible
    Before this CL, WorkerThread::terminate() called on the main thread may forcibly
    terminate V8 Isolate in order to ensure that the worker thread is not blocked by
    JS execution and a shutdown task starts soon. This is risky because, after the
    forced termination, V8 APIs suddenly start to return empty handles and it may
    cause crashes if there are no treatments for the case.
    
    After this CL, terminate() avoids the force termination as much as possible.
    Instead, it posts a delayed task to terminate the isolate in case that the
    shutdown sequence does not start on the worker thread in a certain time period.
    
    BUG=487050
    
    Review-Url: https://codereview.chromium.org/2011763002
    Cr-Commit-Position: refs/heads/master@{#397343}

--- third_party/WebKit/Source/core/workers/WorkerThread.h
+++ third_party/WebKit/Source/core/workers/WorkerThread.h
@@ -63,15 +63,22 @@ enum WorkerThreadStartMode {
 // an actual task is executed on the worker thread.
 class CORE_EXPORT WorkerThread {
 public:
+    // Represents how this thread is terminated.
+    enum class ExitCode {
+        NotTerminated,
+        GracefullyTerminated,
+        SyncForciblyTerminated,
+        AsyncForciblyTerminated,
+    };
+
     virtual ~WorkerThread();
 
     // Called on the main thread.
     void start(PassOwnPtr<WorkerThreadStartupData>);
     void terminate();
 
-    // Called in shutdown sequence on the main thread. Internally calls
-    // terminate() and wait (by *blocking* the calling thread) until the
-    // worker(s) is/are shut down.
+    // Called on the main thread. Internally calls terminateInternal() and wait
+    // (by *blocking* the calling thread) until the worker(s) is/are shut down.
     void terminateAndWait();
     static void terminateAndWaitForAllWorkers();
 
@@ -115,6 +122,8 @@ public:
 
     PlatformThreadId platformThreadId();
 
+    ExitCode getExitCode();
+
 protected:
     WorkerThread(PassRefPtr<WorkerLoaderProxy>, WorkerReportingProxy&);
 
@@ -126,9 +135,27 @@ protected:
     virtual void postInitialize() { }
 
 private:
+    friend class WorkerThreadTest;
+
+    class ForceTerminationTask;
     class WorkerMicrotaskRunner;
 
+    enum class TerminationMode {
+        // Synchronously terminate the worker execution. Please be careful to
+        // use this mode, because after the synchronous termination any V8 APIs
+        // may suddenly start to return empty handles and it may cause crashes.
+        Forcible,
+
+        // Don't synchronously terminate the worker execution. Instead, schedule
+        // a task to terminate it in case that the shutdown sequence does not
+        // start on the worker thread in a certain time period.
+        Graceful,
+    };
+
     std::unique_ptr<CrossThreadClosure> createWorkerThreadTask(std::unique_ptr<ExecutionContextTask>, bool isInstrumented);
+
+    void terminateInternal(TerminationMode);
+    void forciblyTerminateExecution();
 
     void initializeOnWorkerThread(PassOwnPtr<WorkerThreadStartupData>);
     void prepareForShutdownOnWorkerThread();
@@ -137,11 +164,16 @@ private:
     void runDebuggerTaskOnWorkerThread(std::unique_ptr<CrossThreadClosure>);
     void runDebuggerTaskDontWaitOnWorkerThread();
 
+    void setForceTerminationDelayInMsForTesting(long long forceTerminationDelayInMs) { m_forceTerminationDelayInMs = forceTerminationDelayInMs; }
+
     bool m_started = false;
     bool m_terminated = false;
     bool m_readyToShutdown = false;
     bool m_pausedInDebugger = false;
     bool m_runningDebuggerTask = false;
+    ExitCode m_exitCode = ExitCode::NotTerminated;
+
+    long long m_forceTerminationDelayInMs;
 
     OwnPtr<InspectorTaskRunner> m_inspectorTaskRunner;
     OwnPtr<WorkerMicrotaskRunner> m_microtaskRunner;
@@ -150,7 +182,8 @@ private:
     WorkerReportingProxy& m_workerReportingProxy;
 
     // This lock protects |m_workerGlobalScope|, |m_terminated|,
-    // |m_readyToShutdown|, |m_runningDebuggerTask| and |m_microtaskRunner|.
+    // |m_readyToShutdown|, |m_runningDebuggerTask|, |m_exitCode| and
+    // |m_microtaskRunner|.
     Mutex m_threadStateMutex;
 
     Persistent<WorkerGlobalScope> m_workerGlobalScope;
@@ -160,6 +193,10 @@ private:
 
     // Signaled when the thread completes termination on the worker thread.
     OwnPtr<WaitableEvent> m_shutdownEvent;
+
+    // Scheduled when termination starts with TerminationMode::Force, and
+    // cancelled when the worker thread is gracefully shut down.
+    OwnPtr<ForceTerminationTask> m_scheduledForceTerminationTask;
 };
 
 } // namespace blink
