e7171e26989cfe56bf86e490bd55db2e47a5f184
jasonroberts <jasonroberts@google.com>
2016-06-02 09:57:54 -0700

    [Chromecast] Fix lint issue with missing includes.
    cast_metrics_test_helper.cc was using string but not including the
    proper header.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/1777893002
    Cr-Commit-Position: refs/heads/master@{#397449}

--- chromecast/base/metrics/cast_metrics_test_helper.cc
+++ chromecast/base/metrics/cast_metrics_test_helper.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/base/metrics/cast_metrics_test_helper.h"
 
+#include <string>
+
 #include "base/logging.h"
 #include "base/macros.h"
 #include "chromecast/base/metrics/cast_metrics_helper.h"
