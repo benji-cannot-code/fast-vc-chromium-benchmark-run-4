ab2ae70be1ef4dad63052fb034dadc12ce3cb6e2
yosin <yosin@chromium.org>
2016-06-01 19:09:19 -0700

    Simplify logical Range management of selection
    "Logical Range" is a term of Selection API implementation in Blink. "Logical
    Range" is created by |Selection#addRange()| and retrieved by
    |Selection#getRangeAt()| to get a |Range| what the script set, aka
    non-canonicalized |Range|, until selection changed.
    
    Before this patch, "Logical Range" resetting is catched all modifier functions
    in |VisibleSelection| class, from this patch it is in |SelectionEditor|, since
    all changes of |DOMSelection| is made within |SelectionEditor| to simplify
    implementation and reducing code complexity for improving code health.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2024383002
    Cr-Commit-Position: refs/heads/master@{#397292}

--- third_party/WebKit/Source/core/editing/SelectionAdjuster.cpp
+++ third_party/WebKit/Source/core/editing/SelectionAdjuster.cpp
@@ -181,7 +181,6 @@ void SelectionAdjuster::adjustSelectionInFlatTree(VisibleSelectionInFlatTree* se
         selectionInFlatTree->m_end = position1;
     }
     selectionInFlatTree->updateSelectionType();
-    selectionInFlatTree->didChange();
 }
 
 static bool isCrossingShadowBoundaries(const VisibleSelectionInFlatTree& selection)
@@ -226,7 +225,6 @@ void SelectionAdjuster::adjustSelectionInDOMTree(VisibleSelection* selection, co
         selection->m_end = position1;
     }
     selection->updateSelectionType();
-    selection->didChange();
 }
 
 void SelectionAdjuster::adjustSelectionToAvoidCrossingShadowBoundaries(VisibleSelection* selection)
--- third_party/WebKit/Source/core/editing/SelectionAdjusterTest.cpp
+++ third_party/WebKit/Source/core/editing/SelectionAdjusterTest.cpp
@@ -9,37 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-namespace {
-
-class MockVisibleSelectionChangeObserver final :
-    public GarbageCollectedFinalized<MockVisibleSelectionChangeObserver>,
-    public VisibleSelectionChangeObserver {
-    WTF_MAKE_NONCOPYABLE(MockVisibleSelectionChangeObserver);
-    USING_GARBAGE_COLLECTED_MIXIN(MockVisibleSelectionChangeObserver);
-public:
-    MockVisibleSelectionChangeObserver() = default;
-    ~MockVisibleSelectionChangeObserver() final = default;
-
-    int callCounter() const { return m_callCounter; }
-
-private:
-    // VisibleSelectionChangeObserver interface.
-    void didChangeVisibleSelection() final { ++m_callCounter; }
-
-    int m_callCounter = 0;
-};
-
-} // namespace
-
 class SelectionAdjusterTest : public EditingTestBase  {
 };
 
 TEST_F(SelectionAdjusterTest, adjustSelectionInFlatTree)
 {
     setBodyContent("<div id=sample>foo</div>");
-    MockVisibleSelectionChangeObserver selectionObserver;
     VisibleSelectionInFlatTree selectionInFlatTree;
-    selectionInFlatTree.setChangeObserver(selectionObserver);
 
     Node* const sample = document().getElementById("sample");
     Node* const foo = sample->firstChild();
@@ -48,15 +24,12 @@ TEST_F(SelectionAdjusterTest, adjustSelectionInFlatTree)
     SelectionAdjuster::adjustSelectionInFlatTree(&selectionInFlatTree, selection);
     EXPECT_EQ(PositionInFlatTree(foo, 0), selectionInFlatTree.start());
     EXPECT_EQ(PositionInFlatTree(foo, 3), selectionInFlatTree.end());
-    EXPECT_EQ(1, selectionObserver.callCounter()) << "adjustSelectionInFlatTree() should call didChangeVisibleSelection()";
 }
 
 TEST_F(SelectionAdjusterTest, adjustSelectionInDOMTree)
 {
     setBodyContent("<div id=sample>foo</div>");
-    MockVisibleSelectionChangeObserver selectionObserver;
     VisibleSelection selection;
-    selection.setChangeObserver(selectionObserver);
 
     Node* const sample = document().getElementById("sample");
     Node* const foo = sample->firstChild();
@@ -67,7 +40,6 @@ TEST_F(SelectionAdjusterTest, adjustSelectionInDOMTree)
     SelectionAdjuster::adjustSelectionInDOMTree(&selection, selectionInFlatTree);
     EXPECT_EQ(Position(foo, 0), selection.start());
     EXPECT_EQ(Position(foo, 3), selection.end());
-    EXPECT_EQ(1, selectionObserver.callCounter()) << "adjustSelectionInDOMTree() should call didChangeVisibleSelection()";
 }
 
 } // namespace blink
