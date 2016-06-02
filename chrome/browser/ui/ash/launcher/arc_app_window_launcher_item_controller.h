be3647970cf2ec6624288d90c466e67932ad1406
khmel <khmel@chromium.org>
2016-06-02 10:18:13 -0700

    arc: Fix multiple Play Store icons in shelf.
    BUG=b/29068435
    TEST=Manually on device, duplicate Play Store icons no longer
         appear on app install from Play Store.
    
    Review-Url: https://codereview.chromium.org/2020273004
    Cr-Commit-Position: refs/heads/master@{#397454}

--- chrome/browser/ui/ash/launcher/arc_app_window_launcher_item_controller.h
+++ chrome/browser/ui/ash/launcher/arc_app_window_launcher_item_controller.h
@@ -16,8 +16,7 @@ class ChromeLauncherController;
 class ArcAppWindowLauncherItemController
     : public AppWindowLauncherItemController {
  public:
-  ArcAppWindowLauncherItemController(const std::string& shelf_app_id,
-                                     const std::string& arc_app_id,
+  ArcAppWindowLauncherItemController(const std::string& arc_app_id,
                                      ChromeLauncherController* controller);
 
   ~ArcAppWindowLauncherItemController() override;
