618c9d36351df29813ea78e020ca2ec71a82360e
wangxianzhu <wangxianzhu@chromium.org>
2016-06-02 10:56:56 -0700

    PrePaintTreeWalk should walk multicol spanner through the placeholder
    The LayoutObject of a multicol spanner is placed in the layout tree
    according to the DOM order, but it actually belongs to the multicol
    container, not its DOM hierarchy. During PrePaintTreeWalk we should
    follow the link from the placeholder to the spanner, instead of in
    layout tree order.
    
    Review-Url: https://codereview.chromium.org/2031923002
    Cr-Commit-Position: refs/heads/master@{#397463}

--- third_party/WebKit/Source/core/paint/PrePaintTreeWalk.cpp
+++ third_party/WebKit/Source/core/paint/PrePaintTreeWalk.cpp
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/DocumentLifecycle.h"
 #include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
+#include "core/layout/LayoutMultiColumnSpannerPlaceholder.h"
 #include "core/layout/LayoutPart.h"
 #include "core/layout/LayoutView.h"
 
@@ -41,9 +42,16 @@ void PrePaintTreeWalk::walk(const LayoutObject& object, const PrePaintTreeWalkCo
     m_propertyTreeBuilder.buildTreeNodes(object, localContext.treeBuilderContext);
 
     for (const LayoutObject* child = object.slowFirstChild(); child; child = child->nextSibling()) {
-        if (child->isBoxModelObject() || child->isSVG())
-            walk(*child, localContext);
+        if (!child->isBoxModelObject() && !child->isSVG())
+            continue;
+        // Column spanners are walked through their placeholders. See below.
+        if (child->isColumnSpanAll())
+            continue;
+        walk(*child, localContext);
     }
+
+    if (object.isLayoutMultiColumnSpannerPlaceholder())
+        walk(*toLayoutMultiColumnSpannerPlaceholder(object).layoutObjectInFlowThread(), localContext);
 
     if (object.isLayoutPart()) {
         Widget* widget = toLayoutPart(object).widget();
