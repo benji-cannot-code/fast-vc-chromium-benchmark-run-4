362d26b6d818000d283efc949a82daf80525cffd
braveyao <braveyao@chromium.org>
2016-06-02 14:49:55 -0700

    When Chrome app tries to open a camera/mic for the first time on Android, the pop-up runtime permission dialogue will cause activities onStart/Stop events. Chrome will release/resume camera device accordingly. But if activity onStart event comes after we start to open the camera and before we get onDeviceStarted callback, we will wrongly resume the same device which will fail with exception. In this cl, we will check if the resumed device is already in the start queue. Also move one function to public for unittests which can catch the scenario in this BUG.
    BUG=615557
    TESt=trybots and manual test by doing a apprtc test on a N
         device with building later than NRD45.
    
    Review-Url: https://codereview.chromium.org/2029933002
    Cr-Commit-Position: refs/heads/master@{#397518}

--- content/browser/renderer_host/media/video_capture_manager.h
+++ content/browser/renderer_host/media/video_capture_manager.h
@@ -163,6 +163,12 @@ class CONTENT_EXPORT VideoCaptureManager : public MediaStreamProvider {
   void TakePhoto(int session_id,
                  media::ScopedResultCallback<
                      VideoCaptureDevice::TakePhotoCallback> callback);
+#if defined(OS_ANDROID)
+  // Some devices had troubles when stopped and restarted quickly, so the device
+  // is only stopped when Chrome is sent to background and not when, e.g., a tab
+  // is hidden, see http://crbug.com/582295.
+  void OnApplicationStateChange(base::android::ApplicationState state);
+#endif
 
  private:
   class DeviceEntry;
@@ -276,10 +282,6 @@ class CONTENT_EXPORT VideoCaptureManager : public MediaStreamProvider {
 #endif
 
 #if defined(OS_ANDROID)
-  // Some devices had troubles when stopped and restarted quickly, so the device
-  // is only stopped when Chrome is sent to background and not when, e.g., a tab
-  // is hidden, see http://crbug.com/582295.
-  void OnApplicationStateChange(base::android::ApplicationState state);
   void ReleaseDevices();
   void ResumeDevices();
 
