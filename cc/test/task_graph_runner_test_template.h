b6cc6f128c304f89d429f624145494996345cdb8
prashant.n <prashant.n@samsung.com>
2016-06-01 22:57:58 -0700

    cc: Rename CompleteOnOriginThread() -> OnTaskCompleted() in tests.
    Rename to OnTaskCompleted() in task_graph_runner_test_template.h.
    
    BUG=599863
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review-Url: https://codereview.chromium.org/2021023002
    Cr-Commit-Position: refs/heads/master@{#397305}

--- cc/test/task_graph_runner_test_template.h
+++ cc/test/task_graph_runner_test_template.h
@@ -64,9 +64,7 @@ class TaskGraphRunnerTestBase {
     // Overridden from Task:
     void RunOnWorkerThread() override;
 
-    // TODO(prashant.n): Rename this function to OnTaskCompleted() to make it
-    // analogous with tile task. crbug.com/599863.
-    virtual void CompleteOnOriginThread();
+    virtual void OnTaskCompleted();
 
    protected:
     ~FakeTaskImpl() override {}
@@ -87,7 +85,7 @@ class TaskGraphRunnerTestBase {
         : FakeTaskImpl(test, namespace_index, id) {}
 
     // Overridden from FakeTaskImpl:
-    void CompleteOnOriginThread() override {}
+    void OnTaskCompleted() override {}
 
    private:
     ~FakeDependentTaskImpl() override {}
