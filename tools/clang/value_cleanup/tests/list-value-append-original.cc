7ad1ce449429f28e091a36c95bf69518a029d039
Daniel Cheng <dcheng@chromium.org>
2016-06-02 13:57:21 -0700

    base::Value cleanup: add simple Clang tool for structured cleanups
    Rewrites base::ListValue::Append(new base::StringValue(...)) and
    base::ListValue::Append(new base::FundamentalValue(...)) to use
    base::ListValue::Append{Boolean,Integer,Double,String} instead.
    
    BUG=581865
    R=danakj@chromium.org
    
    Review URL: https://codereview.chromium.org/2032983002 .
    
    Cr-Commit-Position: refs/heads/master@{#397506}

---
+++ tools/clang/value_cleanup/tests/list-value-append-original.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/values.h"
+
+void F() {
+  base::ListValue list;
+  list.Append(new base::FundamentalValue(1 == 0));
+  list.Append(new base::FundamentalValue(static_cast<unsigned char>(1.0)));
+  list.Append(new base::FundamentalValue(double{3}));
+  list.Append(new base::StringValue("abc"));
+}
