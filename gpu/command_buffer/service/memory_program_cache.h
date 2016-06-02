f38f8dc47a19561fa6e3172b78b39c34e3c378bc
xinghua.cao <xinghua.cao@intel.com>
2016-06-01 18:46:10 -0700

    Save interface block infomation to program cache
    If GPU supports program cache, and command buffer does not save interface
    block information, it cannot get correct uniform information when update
    uniforms.
    
    BUG=615794
    TestCase=deqp/functional/gles3/shaderstatequery.html
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2019293002
    Cr-Commit-Position: refs/heads/master@{#397291}

--- gpu/command_buffer/service/memory_program_cache.h
+++ gpu/command_buffer/service/memory_program_cache.h
@@ -65,11 +65,13 @@ class GPU_EXPORT MemoryProgramCache : public ProgramCache {
                       const UniformMap& uniform_map_0,
                       const VaryingMap& varying_map_0,
                       const OutputVariableList& output_variable_list_0,
+                      const InterfaceBlockMap& interface_block_map_0,
                       const char* shader_1_hash,
                       const AttributeMap& attrib_map_1,
                       const UniformMap& uniform_map_1,
                       const VaryingMap& varying_map_1,
                       const OutputVariableList& output_variable_list_1,
+                      const InterfaceBlockMap& interface_block_map_1,
                       MemoryProgramCache* program_cache);
 
     GLsizei length() const {
@@ -104,6 +106,10 @@ class GPU_EXPORT MemoryProgramCache : public ProgramCache {
       return output_variable_list_0_;
     }
 
+    const InterfaceBlockMap& interface_block_map_0() const {
+      return interface_block_map_0_;
+    }
+
     const std::string& shader_1_hash() const {
       return shader_1_hash_;
     }
@@ -124,6 +130,10 @@ class GPU_EXPORT MemoryProgramCache : public ProgramCache {
       return output_variable_list_1_;
     }
 
+    const InterfaceBlockMap& interface_block_map_1() const {
+      return interface_block_map_1_;
+    }
+
    private:
     friend class base::RefCounted<ProgramCacheValue>;
 
@@ -138,11 +148,13 @@ class GPU_EXPORT MemoryProgramCache : public ProgramCache {
     const UniformMap uniform_map_0_;
     const VaryingMap varying_map_0_;
     const OutputVariableList output_variable_list_0_;
+    const InterfaceBlockMap interface_block_map_0_;
     const std::string shader_1_hash_;
     const AttributeMap attrib_map_1_;
     const UniformMap uniform_map_1_;
     const VaryingMap varying_map_1_;
     const OutputVariableList output_variable_list_1_;
+    const InterfaceBlockMap interface_block_map_1_;
     MemoryProgramCache* const program_cache_;
 
     DISALLOW_COPY_AND_ASSIGN(ProgramCacheValue);
