1b3e6a218f9effd201f937209056059e20caa37a
dmazzoni <dmazzoni@chromium.org>
2016-06-03 12:52:32 -0700

    Fix the double-tap to click gesture in touch accessibility mode.
    Originally Chrome OS's touch accessibility support was implemented purely
    as an event rewriter. Tapping the screen generates a mouse move event,
    which announces the object under the finger without clicking it, and then
    double-tapping anywhere on the screen generates a click at the last point
    where the user tapped. It's also possible to trigger clicks and drags by
    more advanced means.
    
    This worked well for touch exploration alone, but it's also possible for
    the current accessibility object to move due to a gesture (to move to the
    next or previous object on the screen, for example) or due to input focus
    moving. In those cases, a double-tap was sending a simulated click to the
    wrong point - it was sending it to the last touch exploration point, rather
    than to the current accessibility object.
    
    This change fixes this behavior by changing how the touch accessibility
    support works.
    
    First, it introduces the concept of the anchor point, which is the point
    where the next simulated click will go. Initially this is set via touch
    exploration, however now as soon as ChromeVox identifies the object under
    the finger, it sends the center point of that object as the new explicit
    anchor point, and that point overrides the touch exploration point for the
    duration of this touch.
    
    Second, when the user does a discrete gesture to click on the current anchor
    point (double-tap, split-tap, or single-tap), it sends a gesture command
    to ChromeVox, allowing ChromeVox to directly activate the current object.
    However, if the user holds their finger down, it still passes through the
    touch events so dragging and long-pressing are possible.
    
    Third, it simplifies "split tap" support where you can tap a second finger
    as a shortcut to clicking. That support now only supports a basic click and
    not a long-press or drag.
    
    BUG=513713,613694
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2007863004
    Cr-Commit-Position: refs/heads/master@{#397781}

--- chrome/browser/chromeos/accessibility/accessibility_manager.cc
+++ chrome/browser/chromeos/accessibility/accessibility_manager.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/autoclick/autoclick_controller.h"
 #include "ash/high_contrast/high_contrast_controller.h"
 #include "ash/metrics/user_metrics_recorder.h"
+#include "ash/root_window_controller.h"
 #include "ash/session/session_state_delegate.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shelf/shelf_layout_manager.h"
@@ -795,6 +796,13 @@ void AccessibilityManager::HandleAccessibilityGesture(ui::AXGesture gesture) {
       std::move(event_args)));
   event_router->DispatchEventWithLazyListener(
       extension_misc::kChromeVoxExtensionId, std::move(event));
+}
+
+void AccessibilityManager::SetTouchAccessibilityAnchorPoint(
+    const gfx::Point& anchor_point) {
+  ash::RootWindowController* root_window_controller =
+      ash::RootWindowController::ForTargetRootWindow();
+  root_window_controller->SetTouchAccessibilityAnchorPoint(anchor_point);
 }
 
 bool AccessibilityManager::IsHighContrastEnabled() {
