5dd1369b1c1f642554f00436f52d1f48fdf91b04
dtseng <dtseng@chromium.org>
2016-06-03 10:17:54 -0700

    Refine ChromeVox's treatment of containers
    1. merge ignored containers and ignored leaf nodes into one predicate
    2. only visit containers that are also visited by the provided predicate. For example, an element will match against a focusable toolbar, but a line won't.
    3. scan the initial subtree of a node when moving to the next node if either of the following are true:
    the node is a container or
    the node doesn't match the predicate (e.g. element).
    This takes care of syncing when the current range is on an arbitrary position in the tree.
    When moving backwards, the leaf predicate restricts how deeply the backwards movement syncs downwards in the tree.
    
    BUG=613473
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2028173003
    Cr-Commit-Position: refs/heads/master@{#397737}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
@@ -773,7 +773,7 @@ Background.prototype = {
 
       if (node) {
         node = AutomationUtil.findNodePre(
-            node, dir, AutomationPredicate.element) || node;
+            node, dir, AutomationPredicate.object) || node;
       }
 
       if (node) {
