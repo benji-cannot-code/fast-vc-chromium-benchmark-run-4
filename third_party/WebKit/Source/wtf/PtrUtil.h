195d1d7831157fe3b2f2cae1dc1699b44fa2576a
yutak <yutak@chromium.org>
2016-06-03 04:48:24 -0700

    Add WTF::wrapArrayUnique().
    The work of converting existing usage of OwnPtr has turned out that we
    need to have an equivalent of adoptArrayPtr() for std::unique_ptr.
    
    BUG=582354
    
    Review-Url: https://codereview.chromium.org/2031253002
    Cr-Commit-Position: refs/heads/master@{#397685}

--- third_party/WebKit/Source/wtf/PtrUtil.h
+++ third_party/WebKit/Source/wtf/PtrUtil.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-template<typename T>
+template <typename T>
 std::unique_ptr<T> wrapUnique(T* ptr)
 {
     static_assert(
@@ -21,8 +21,18 @@ std::unique_ptr<T> wrapUnique(T* ptr)
     return std::unique_ptr<T>(ptr);
 }
 
+template <typename T>
+std::unique_ptr<T[]> wrapArrayUnique(T* ptr)
+{
+    static_assert(
+        !WTF::IsGarbageCollectedType<T>::value,
+        "Garbage collected types should not be stored in std::unique_ptr!");
+    return std::unique_ptr<T[]>(ptr);
+}
+
 } // namespace WTF
 
 using WTF::wrapUnique;
+using WTF::wrapArrayUnique;
 
 #endif // PtrUtil_h
