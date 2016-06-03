868358cda1d5fcedd38315a771c7b596f3744606
jamescook <jamescook@chromium.org>
2016-06-02 17:19:45 -0700

    mash: Remove aura references from ash/system/user (for status tray menu)
    This is part of the migration to ash/common types.
    
    No functional changes.
    
    BUG=existing ash_unittests, system shuts down cleanly even with add user menu
    open
    
    Review-Url: https://codereview.chromium.org/2034913003
    Cr-Commit-Position: refs/heads/master@{#397558}

--- ash/system/user/tray_user.cc
+++ ash/system/user/tray_user.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_switches.h"
 #include "ash/common/wm/shelf/wm_shelf_util.h"
-#include "ash/root_window_controller.h"
 #include "ash/session/session_state_delegate.h"
 #include "ash/shelf/shelf_layout_manager.h"
 #include "ash/shelf/shelf_util.h"
@@ -25,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/account_id/account_id.h"
 #include "components/user_manager/user_info.h"
 #include "grit/ash_strings.h"
-#include "ui/aura/window.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/image/image.h"
 #include "ui/views/border.h"
