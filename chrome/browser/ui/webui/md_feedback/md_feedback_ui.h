21847cf15393efd7181e39fcfe3177ab374bf4ae
apacible <apacible@chromium.org>
2016-06-02 17:17:44 -0700

    [MD Feedback] Add basic chrome://feedback dialog.
    This adds a basic dialog delegate and controller to house the feedback WebUI. The dialog is not anchored or constrained to any other browser window.
    
    Show() is only implemented for views. Mac will be done in a separate patch.
    
    BUG=615533
    
    Review-Url: https://codereview.chromium.org/2021753005
    Cr-Commit-Position: refs/heads/master@{#397556}

--- chrome/browser/ui/webui/md_feedback/md_feedback_ui.h
+++ chrome/browser/ui/webui/md_feedback/md_feedback_ui.h
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "content/public/browser/web_ui_controller.h"
 
+namespace content {
+class BrowserContext;
+}  // namespace content
+
 // The WebUI for chrome://feedback.
 class MdFeedbackUI : public content::WebUIController {
  public:
@@ -17,5 +21,10 @@ class MdFeedbackUI : public content::WebUIController {
  private:
   DISALLOW_COPY_AND_ASSIGN(MdFeedbackUI);
 };
+
+// Create and show a web dialog with the MD feedback UI.
+// |browser_context| Is used to constructed the HTML dialog's WebContents.
+void ShowFeedbackWebDialog(
+    content::BrowserContext* browser_context);
 
 #endif  // CHROME_BROWSER_UI_WEBUI_MD_FEEDBACK_MD_FEEDBACK_UI_H_
