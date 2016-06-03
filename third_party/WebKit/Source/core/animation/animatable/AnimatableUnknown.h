7128c2c28377358c188b9400f4376ab4cad7e599
alancutter <alancutter@chromium.org>
2016-06-03 01:12:06 -0700

    Allow transitions on background-size with positionally matched keywords
    This change allows CSS transitions to interpolate between lists with
    matching keywords.
    This affects interpolation for the following properties:
     - border-image-width
       This property is not animatable according to spec:
       https://drafts.csswg.org/css-backgrounds-3/#the-border-image-width
    
     - background-size
       Values are now interpolable if their keywords are positionally matched:
       https://github.com/w3c/csswg-drafts/commit/a5f9de921fa7834c79ee15660d9d86528bce1607
    
     - -webkit-background-size
     - -webkit-mask-size
       These mirror the animation behaviour of background-size.
    
    This change only affects CSS Transitions for the sake of a small mergable patch.
    CSS Animations behaviour will be brought in line in a separate patch.
    
    This change ensures we have compatible behaviour with other browsers for
    transitioning between single length background-size values e.g.
    "background-size: 50%" to "background-size: 100%"
    
    BUG=616072
    
    Review-Url: https://codereview.chromium.org/2027933002
    Cr-Commit-Position: refs/heads/master@{#397661}

--- third_party/WebKit/Source/core/animation/animatable/AnimatableUnknown.h
+++ third_party/WebKit/Source/core/animation/animatable/AnimatableUnknown.h
@@ -60,7 +60,7 @@ protected:
         return defaultInterpolateTo(this, value, fraction);
     }
 
-    bool usesDefaultInterpolationWith(const AnimatableValue*) const override { return true; }
+    bool usesDefaultInterpolationWith(const AnimatableValue*) const override;
 
 private:
     explicit AnimatableUnknown(CSSValue* value)
@@ -80,6 +80,12 @@ inline bool AnimatableUnknown::equalTo(const AnimatableValue* value) const
 {
     const AnimatableUnknown* unknown = toAnimatableUnknown(value);
     return m_value == unknown->m_value || m_value->equals(*unknown->m_value);
+}
+
+inline bool AnimatableUnknown::usesDefaultInterpolationWith(const AnimatableValue* value) const
+{
+    const AnimatableUnknown& unknown = toAnimatableUnknown(*value);
+    return !m_value->equals(*unknown.m_value);
 }
 
 } // namespace blink
