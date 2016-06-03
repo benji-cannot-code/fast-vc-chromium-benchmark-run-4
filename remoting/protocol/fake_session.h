e9aabaa17aada0177f09e8a035262d41206f960d
sergeyu <sergeyu@chromium.org>
2016-06-03 02:03:21 -0700

    Fix WebRtcFrameScheduler to wait for first key frame request.
    Previous the scheduler would start capturing before the stream is
    negotiated, which was causing first few frames to be dropped. Now it
    waits for the first key frame request.
    
    BUG=616637
    
    Review-Url: https://codereview.chromium.org/2035803002
    Cr-Commit-Position: refs/heads/master@{#397669}

--- remoting/protocol/fake_session.h
+++ remoting/protocol/fake_session.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/time/time.h"
 #include "remoting/protocol/fake_stream_socket.h"
 #include "remoting/protocol/session.h"
 #include "remoting/protocol/transport.h"
@@ -35,6 +36,11 @@ class FakeSession : public Session {
   void set_error(ErrorCode error) { error_ = error; }
   bool is_closed() const { return closed_; }
 
+  // Sets delay for signaling message deliver when connected to a peer.
+  void set_signaling_delay(base::TimeDelta signaling_delay) {
+    signaling_delay_ = signaling_delay;
+  }
+
   // Session interface.
   void SetEventHandler(EventHandler* event_handler) override;
   ErrorCode error() override;
@@ -46,6 +52,9 @@ class FakeSession : public Session {
  private:
   // Callback provided to the |transport_|.
   void SendTransportInfo(std::unique_ptr<buzz::XmlElement> transport_info);
+
+  // Called by the |peer_| to deliver incoming |transport_info|.
+  void ProcessTransportInfo(std::unique_ptr<buzz::XmlElement> transport_info);
 
   EventHandler* event_handler_ = nullptr;
   std::unique_ptr<SessionConfig> config_;
@@ -59,6 +68,7 @@ class FakeSession : public Session {
   bool closed_ = false;
 
   base::WeakPtr<FakeSession> peer_;
+  base::TimeDelta signaling_delay_;
 
   base::WeakPtrFactory<FakeSession> weak_factory_;
 
