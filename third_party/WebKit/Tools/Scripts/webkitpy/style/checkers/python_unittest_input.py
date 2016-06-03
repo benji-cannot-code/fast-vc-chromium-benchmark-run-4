d442c2f4a3e56f7ef241ceb412ea74227a79e43b
qyearsley <qyearsley@chromium.org>
2016-06-03 12:48:27 -0700

    Enable pylint warnings on presubmit checks.
    In http://crbug.com/493941, it was mentioned that we could fix all current pylint warnings before enabling pylint warnings in the presubmit -- but actually we could also enable warnings in the presubmit now without causing any problems; pylint will only print warnings about lines that are changed in a CL, and turning on warnings now could be potentially helpful in cleaning up the code.
    
    And turning on pylint warnings in check-webkit-style and the presubmit is just a one line change; any reason not to just do it now?
    
    NOPRESUBMIT=true
    BUG=493941
    
    Review-Url: https://codereview.chromium.org/2010173003
    Cr-Commit-Position: refs/heads/master@{#397780}

--- third_party/WebKit/Tools/Scripts/webkitpy/style/checkers/python_unittest_input.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/style/checkers/python_unittest_input.py
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# This file is sample input for python_unittest.py and includes two
-# problems, one that will generate a PEP-8 warning for trailing whitespace
-# and one that will generate a pylint error for an undefined variable.
-print error() 
+# This file is sample input for python_unittest.py and includes problems.
+print error()  
+import math#unused
