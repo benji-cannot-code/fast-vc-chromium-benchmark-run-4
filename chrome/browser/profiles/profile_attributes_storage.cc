69e7025c262adeb3553dc17cffc6b2d0616cca62
jshin <jshin@chromium.org>
2016-06-02 19:59:58 -0700

    Use plural formats and native digits
    With codesearch, look for '<ex>[0-9]+</ex> file:grd'. Review each of
    matches and change them to use plural formats(C++), toLocaleString(JavaScript),
    or other i18n-safe ways.
    
    Moreover, change GetStringFUTF16Int() to use 'FormatNumber' instead
    of Int*ToString(). Call sites of GetStringFUTF16Int() were reviewed
    Where native digits are not appropriate (non-UI), call sites were
    adjusted.
    
    Besides, add a warning about 'ASCII digits vs native digits' to
    base/strings/string_number_conversions.h and add more details
    on GetPlural* to ui/base/l10n/l10n_util.h.
    
    BUG=613331
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation;tryserver.blink:linux_blink_rel,mac_blink_rel
    
    Review-Url: https://codereview.chromium.org/2002783002
    Cr-Commit-Position: refs/heads/master@{#397608}

--- chrome/browser/profiles/profile_attributes_storage.cc
+++ chrome/browser/profiles/profile_attributes_storage.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/i18n/number_formatting.h"
 #include "base/i18n/string_compare.h"
 #include "base/rand_util.h"
 #include "base/strings/string_number_conversions.h"
@@ -121,17 +122,22 @@ base::string16 ProfileAttributesStorage::ChooseNameForNewProfile(
   base::string16 name;
   for (int name_index = 1; ; ++name_index) {
 #if !defined(OS_CHROMEOS) && !defined(OS_ANDROID)
-    name = l10n_util::GetStringFUTF16Int(IDS_NEW_NUMBERED_PROFILE_NAME,
-                                         name_index);
+    // Using native digits will break IsDefaultProfileName() below because
+    // it uses sscanf.
+    // TODO(jshin): fix IsDefaultProfileName to handle native digits.
+    name = l10n_util::GetStringFUTF16(IDS_NEW_NUMBERED_PROFILE_NAME,
+                                      base::IntToString16(name_index));
 #else
     if (icon_index < profiles::GetGenericAvatarIconCount()) {
       name = l10n_util::GetStringFUTF16Int(IDS_NUMBERED_PROFILE_NAME,
                                            name_index);
     } else {
+      // TODO(jshin): Check with UX if appending |name_index| to the default
+      // name without a space is intended.
       name = l10n_util::GetStringUTF16(
           kDefaultNames[icon_index - profiles::GetGenericAvatarIconCount()]);
       if (name_index > 1)
-        name.append(base::UTF8ToUTF16(base::IntToString(name_index)));
+        name.append(base::FormatNumber(name_index));
     }
 #endif
 
