edfb2d25f9a67b55d513b15395f50a75159806c9
ccameron <ccameron@chromium.org>
2016-06-02 23:13:50 -0700

    Mac: Move remote layer use to GpuOutputSurfaceMac
    Move the logic for managing remote layers from AcceleratedWidgetMac
    to GpuOutputSurfaceMac. Change AcceleratedWidgetMac's interface to take
    CALayers for new content, not remote handles (CAContextIDs).
    
    In future patches, GpuOutputSurfaceMac will instantiate a
    CALayerTreeCoordinatorMac locally (as opposed to in the GPU process),
    and will present the resulting CALayers directly.
    
    BUG=604052
    
    Review-Url: https://codereview.chromium.org/2018793002
    Cr-Commit-Position: refs/heads/master@{#397646}

--- content/browser/renderer_host/browser_compositor_view_mac.h
+++ content/browser/renderer_host/browser_compositor_view_mac.h
@@ -9,9 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
-#include "ui/accelerated_widget_mac/accelerated_widget_mac.h"
 #include "ui/compositor/compositor.h"
 #include "ui/compositor/compositor_observer.h"
+
+namespace ui {
+class AcceleratedWidgetMac;
+}
 
 namespace content {
 
