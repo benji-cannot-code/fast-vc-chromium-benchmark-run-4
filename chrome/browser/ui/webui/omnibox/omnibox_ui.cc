5bfec6801398f4ffcd67ea95e065fe7a08726303
dpapad <dpapad@chromium.org>
2016-06-02 19:43:29 -0700

    Mojo + WebUI: Cleanup chrome://omnibox.
     - Stop creating a new delegate on every startOmniboxQuery() call.
     - Reduce boilerplate setup code by using the new
       connection.bindStubDerivedImpl() method.
     - Rename interface name mojom::OmniboxUIHandlerMojo to
       mojom::OmniboxPageHandler
    
    This cleanup converts the chrome://omnibox WebUI to follow the pattern used in
    chrome://plugins to establish 2-way communication between JS and C++.
    
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2025143002
    Cr-Commit-Position: refs/heads/master@{#397597}

--- chrome/browser/ui/webui/omnibox/omnibox_ui.cc
+++ chrome/browser/ui/webui/omnibox/omnibox_ui.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/webui/omnibox/omnibox_ui_handler.h"
+#include "chrome/browser/ui/webui/omnibox/omnibox_page_handler.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_controller.h"
@@ -31,7 +31,7 @@ OmniboxUI::OmniboxUI(content::WebUI* web_ui) : MojoWebUIController(web_ui) {
 OmniboxUI::~OmniboxUI() {}
 
 void OmniboxUI::BindUIHandler(
-    mojo::InterfaceRequest<mojom::OmniboxUIHandlerMojo> request) {
-  omnibox_ui_handler_.reset(
-      new OmniboxUIHandler(Profile::FromWebUI(web_ui()), std::move(request)));
+    mojo::InterfaceRequest<mojom::OmniboxPageHandler> request) {
+  omnibox_handler_.reset(
+      new OmniboxPageHandler(Profile::FromWebUI(web_ui()), std::move(request)));
 }
--- chrome/browser/ui/webui/omnibox/omnibox_ui.h
+++ chrome/browser/ui/webui/omnibox/omnibox_ui.h
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/mojo_web_ui_controller.h"
 #include "chrome/browser/ui/webui/omnibox/omnibox.mojom.h"
 
-class OmniboxUIHandler;
+class OmniboxPageHandler;
 
 // The UI for chrome://omnibox/
-class OmniboxUI : public MojoWebUIController<mojom::OmniboxUIHandlerMojo> {
+class OmniboxUI : public MojoWebUIController<mojom::OmniboxPageHandler> {
  public:
   explicit OmniboxUI(content::WebUI* contents);
   ~OmniboxUI() override;
@@ -21,9 +21,9 @@ class OmniboxUI : public MojoWebUIController<mojom::OmniboxUIHandlerMojo> {
  private:
   // MojoWebUIController overrides:
   void BindUIHandler(
-      mojo::InterfaceRequest<mojom::OmniboxUIHandlerMojo> request) override;
+      mojo::InterfaceRequest<mojom::OmniboxPageHandler> request) override;
 
-  std::unique_ptr<OmniboxUIHandler> omnibox_ui_handler_;
+  std::unique_ptr<OmniboxPageHandler> omnibox_handler_;
 
   DISALLOW_COPY_AND_ASSIGN(OmniboxUI);
 };
