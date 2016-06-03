4de715d15d4a9e6cc040e499c4b8123af5ffe3a2
enne <enne@chromium.org>
2016-06-03 11:11:05 -0700

    mac: ensure ui::Compositor exists for visible RWHVs
    RenderWidgetHostViewMac is careful about creating ui::Compositor and so
    currently only does this in Show and SwapCompositorFrame.  However, if
    it the RenderWidgetHostView is created while the RenderWidgetHost is
    visible, then Show will early out and never create a ui::Compositor.
    
    This causes problems with begin frame scheduling, which wants to have
    the Display (which is created as part of creating the ui::Compositor's
    output surface currently) own the real begin frame source that drives
    everything.  In that world, if no ui::Compositor exists, no frames ever
    are sent or swapped.  So, the ui::Compositor needs to be created first.
    
    Also, if the ui::Compositor is correctly created during Show (or the
    constructor), then there's no need to ensure that it exists during
    swap compositor frame, and so that can be safely removed.
    
    R=asvitkine@chromium.org,ccameron@chromium.org
    
    Review-Url: https://codereview.chromium.org/2005013002
    Cr-Commit-Position: refs/heads/master@{#397755}

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -601,6 +601,9 @@ RenderWidgetHostViewMac::RenderWidgetHostViewMac(RenderWidgetHost* widget,
         ->GetInputEventRouter()
         ->AddSurfaceIdNamespaceOwner(GetSurfaceIdNamespace(), this);
   }
+
+  if (!render_widget_host_->is_hidden())
+    EnsureBrowserCompositorView();
 }
 
 RenderWidgetHostViewMac::~RenderWidgetHostViewMac() {
@@ -1498,8 +1501,7 @@ void RenderWidgetHostViewMac::OnSwapCompositorFrame(
     gfx::Size dip_size = gfx::ConvertSizeToDIP(scale_factor, pixel_size);
 
     root_layer_->SetBounds(gfx::Rect(dip_size));
-    if (!render_widget_host_->is_hidden()) {
-      EnsureBrowserCompositorView();
+    if (browser_compositor_ && browser_compositor_->compositor()) {
       browser_compositor_->compositor()->SetScaleAndSize(
           scale_factor, pixel_size);
     }
