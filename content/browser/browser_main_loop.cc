72fa595727547caa64c93a586eea31c04e85bcd7
jbroman <jbroman@chromium.org>
2016-06-02 17:59:26 -0700

    Print an error if browser start fails due to no X display.
    Review-Url: https://codereview.chromium.org/2035763002
    Cr-Commit-Position: refs/heads/master@{#397573}

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -1405,8 +1405,10 @@ bool BrowserMainLoop::InitializeToolkit() {
 #if defined(USE_AURA)
 
 #if defined(USE_X11)
-  if (!gfx::GetXDisplay())
+  if (!gfx::GetXDisplay()) {
+    LOG(ERROR) << "Unable to open X display.";
     return false;
+  }
 
 #if !defined(OS_CHROMEOS)
   // InitializeToolkit is called before CreateStartupTasks which one starts the
