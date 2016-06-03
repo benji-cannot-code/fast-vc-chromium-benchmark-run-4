9c2327032173a7706d09ce0b3265ec44ccec666c
pilgrim <pilgrim@chromium.org>
2016-06-03 13:19:27 -0700

    [Layout API] Introduce Layout API shim and use in LayerClipRecorderTest.cpp
    There are a few final obstacles to removing Document::layoutView() and
    FrameView::layoutView(), and they require the use of a shim to bust
    through the block layout API. In LayerClipRecorderTest, it gets a
    reference to the document's layout view and later passes it to
    LayoutObjectDrawingRecorder::useCachedDrawingIfPossible, which in turn
    passes it to DrawingRecorder::useCachedDrawingIfPossible, which is
    defined in platform/ and can not have a hard dependency on the block
    layout API.
    
    BUG=499321
    
    Review-Url: https://codereview.chromium.org/2036213002
    Cr-Commit-Position: refs/heads/master@{#397789}

--- third_party/WebKit/Source/core/layout/api/LayoutItem.h
+++ third_party/WebKit/Source/core/layout/api/LayoutItem.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class FrameView;
+class LayoutAPIShim;
 class Node;
 
 class LayoutItem {
@@ -258,6 +259,8 @@ protected:
 
 private:
     LayoutObject* m_layoutObject;
+
+    friend class LayoutAPIShim;
 };
 
 } // namespace blink
--- third_party/WebKit/Source/core/paint/LayerClipRecorderTest.cpp
+++ third_party/WebKit/Source/core/paint/LayerClipRecorderTest.cpp
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/layout/LayoutTestHelper.h"
 #include "core/layout/LayoutView.h"
+#include "core/layout/api/LayoutAPIShim.h"
 #include "core/layout/compositing/PaintLayerCompositor.h"
 #include "core/paint/LayoutObjectDrawingRecorder.h"
 #include "core/paint/PaintLayer.h"
@@ -33,7 +34,7 @@ private:
         RenderingTest::SetUp();
         enableCompositing();
 
-        m_layoutView = document().view()->layoutView();
+        m_layoutView = toLayoutView(LayoutAPIShim::layoutObjectFrom(document().view()->layoutViewItem()));
         ASSERT_TRUE(m_layoutView);
     }
 
