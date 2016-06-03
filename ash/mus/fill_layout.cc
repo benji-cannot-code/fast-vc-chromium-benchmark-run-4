b035523fb2c2cd5a970afc1bf19a5fba5de8fbba
sky <sky@chromium.org>
2016-06-02 17:56:11 -0700

    Moves mash/wm into ash/mus
    Namespaces have been updated as well. Unfortunately this necessitated
    ::mus in lots of places.
    
    BUG=616265
    TEST=none
    R=msw@chromium.org
    TBR=ben@chromium.org
    
    Review-Url: https://codereview.chromium.org/2029883002
    Cr-Commit-Position: refs/heads/master@{#397569}

---
+++ ash/mus/fill_layout.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/mus/fill_layout.h"
+
+#include "components/mus/public/cpp/window.h"
+
+namespace ash {
+namespace mus {
+
+FillLayout::FillLayout(::mus::Window* owner) : LayoutManager(owner) {}
+
+FillLayout::~FillLayout() {}
+
+void FillLayout::LayoutWindow(::mus::Window* window) {
+  window->SetBounds(gfx::Rect(owner()->bounds().size()));
+}
+
+}  // namespace mus
+}  // namespace ash
