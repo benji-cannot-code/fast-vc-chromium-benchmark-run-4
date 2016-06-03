9e46d614b484ee8715f9c00595b1a713a1a75f01
paulmeyer <paulmeyer@chromium.org>
2016-06-03 15:30:56 -0700

    Support tooltips in GuestViews with "--use-cross-process-frames-for-guests" flag.
    This was just a matter of plumbing the text to the right place.
    
    BUG=609932
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2041493002
    Cr-Commit-Position: refs/heads/master@{#397824}

--- content/browser/frame_host/render_widget_host_view_child_frame.cc
+++ content/browser/frame_host/render_widget_host_view_child_frame.cc
@@ -295,6 +295,7 @@ void RenderWidgetHostViewChildFrame::Destroy() {
 
 void RenderWidgetHostViewChildFrame::SetTooltipText(
     const base::string16& tooltip_text) {
+  frame_connector_->GetRootRenderWidgetHostView()->SetTooltipText(tooltip_text);
 }
 
 void RenderWidgetHostViewChildFrame::SelectionChanged(
