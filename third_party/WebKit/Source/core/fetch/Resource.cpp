dfd0382f7a8f7c3054eaf0fc5078082f88c18d42
hiroshige <hiroshige@chromium.org>
2016-06-03 04:28:20 -0700

    [Loader] Add asserts about revalidation and redirects
    After https://codereview.chromium.org/2011283002/ and
    https://codereview.chromium.org/2017883002/, redirect chain must be empty
    when revalidation starts or during revalidation is ongoing.
    This CL adds asserts to verify that.
    
    BUG=613971, 614989
    
    Review-Url: https://codereview.chromium.org/2020253002
    Cr-Commit-Position: refs/heads/master@{#397683}

--- third_party/WebKit/Source/core/fetch/Resource.cpp
+++ third_party/WebKit/Source/core/fetch/Resource.cpp
@@ -549,6 +549,7 @@ const ResourceRequest& Resource::lastResourceRequest() const
 
 void Resource::setRevalidatingRequest(const ResourceRequest& request)
 {
+    SECURITY_CHECK(m_redirectChain.isEmpty());
     m_revalidatingRequest = request;
     m_status = NotStarted;
 }
@@ -917,6 +918,8 @@ String Resource::getMemoryDumpName() const
 
 void Resource::revalidationSucceeded(const ResourceResponse& validatingResponse)
 {
+    SECURITY_CHECK(m_redirectChain.isEmpty());
+    SECURITY_CHECK(validatingResponse.url() == m_response.url());
     m_response.setResourceLoadTiming(validatingResponse.resourceLoadTiming());
 
     // RFC2616 10.3.5
@@ -941,7 +944,7 @@ void Resource::revalidationFailed()
 {
     m_resourceRequest = m_revalidatingRequest;
     m_revalidatingRequest = ResourceRequest();
-    m_redirectChain.clear();
+    SECURITY_CHECK(m_redirectChain.isEmpty());
     m_data.clear();
     m_cacheHandler.clear();
     destroyDecodedDataForFailedRevalidation();
