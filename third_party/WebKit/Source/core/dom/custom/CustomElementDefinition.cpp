06a028c13b7b66f46d1d586aeab675b3139d8c8c
dominicc <dominicc@chromium.org>
2016-06-03 03:38:55 -0700

    Implement the script parts of custom element upgrade steps.
    BUG=594918
    
    Review-Url: https://codereview.chromium.org/2035623002
    Cr-Commit-Position: refs/heads/master@{#397676}

--- third_party/WebKit/Source/core/dom/custom/CustomElementDefinition.cpp
+++ third_party/WebKit/Source/core/dom/custom/CustomElementDefinition.cpp
@@ -25,6 +25,11 @@ DEFINE_TRACE(CustomElementDefinition)
 // https://html.spec.whatwg.org/multipage/scripting.html#concept-upgrade-an-element
 void CustomElementDefinition::upgrade(Element* element)
 {
+    // TODO(dominicc): When the attributeChangedCallback is implemented,
+    // enqueue reactions for attributes here.
+    // TODO(dominicc): When the connectedCallback is implemented, enqueue
+    // reactions here, if applicable.
+
     m_constructionStack.append(element);
     size_t depth = m_constructionStack.size();
 
@@ -39,20 +44,7 @@ void CustomElementDefinition::upgrade(Element* element)
     if (!succeeded)
         return;
 
-    // TODO(dominicc): Turn this into an assertion when setting
-    // 'custom' moves to the HTMLElement constructor. We will need to
-    // add a bit for MARQUEE to be custom-gets-callbacks-yet-not-custom.
-    element->setCustomElementState(CustomElementState::Custom);
-
-    // TODO(dominicc): When the attributeChangedCallback is implemented,
-    // enqueue reactions for attributes here.
-    // TODO(dominicc): When the connectedCallback is implemented, enqueue
-    // reactions here, if applicable.
-}
-
-bool CustomElementDefinition::runConstructor(Element*)
-{
-    return true;
+    CHECK(element->getCustomElementState() == CustomElementState::Custom);
 }
 
 } // namespace blink
--- third_party/WebKit/Source/core/dom/custom/CustomElementsRegistryTest.cpp
+++ third_party/WebKit/Source/core/dom/custom/CustomElementsRegistryTest.cpp
@@ -244,6 +244,7 @@ public:
             || constructionStack().last() != element)
             return false;
         constructionStack().last().clear();
+        element->setCustomElementState(CustomElementState::Custom);
         return true;
     }
 };
