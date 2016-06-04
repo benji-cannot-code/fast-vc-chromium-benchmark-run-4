a0414a818f7d01bedd6cd3adff4e1ec4d1607ad6
eugenebut <eugenebut@chromium.org>
2016-06-03 18:24:24 -0700

    [ios] Cleanup resource bundle in WebTestSuite::Shutdown.
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2037763002
    Cr-Commit-Position: refs/heads/master@{#397881}

--- ios/web/test/web_test_suite.mm
+++ ios/web/test/web_test_suite.mm
@@ -59,4 +59,9 @@ void WebTestSuite::Initialize() {
       resources_pack_path, ui::SCALE_FACTOR_NONE);
 }
 
+void WebTestSuite::Shutdown() {
+  ResourceBundle::CleanupSharedInstance();
+  base::TestSuite::Shutdown();
+}
+
 }  // namespace web
