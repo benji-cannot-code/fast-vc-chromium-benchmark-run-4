4dd926902c48641e18e8a7fd2a7bf5912c8e512e
szager <szager@chromium.org>
2016-06-03 17:08:51 -0700

    Use hasRareData() rather than null-checking elementRareData().
    R=eae@chromium.org,atotic@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2010683002
    Cr-Commit-Position: refs/heads/master@{#397851}

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -2616,7 +2616,7 @@ Node* Element::insertAdjacent(const String& where, Node* newChild, ExceptionStat
 
 NodeIntersectionObserverData* Element::intersectionObserverData() const
 {
-    if (elementRareData())
+    if (hasRareData())
         return elementRareData()->intersectionObserverData();
     return nullptr;
 }
