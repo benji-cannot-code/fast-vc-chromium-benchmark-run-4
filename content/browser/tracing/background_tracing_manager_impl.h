4d232f6b61807e1618f9f2f6a0166cee6991a0e4
ssid <ssid@chromium.org>
2016-06-04 01:13:26 -0700

    Set up background memory light tracing field trials
    This CL introduces a BENCHMARK_MEMORY_LIGHT mode for background field
    trials to enable memory-infra category in BACKGROUND mode. The
    background tracing controller disables the periodic memory dumps and
    takes a single memory snapshot when a light preset is passed.
    Since the BACKGROUND mode in memory-infra is restricted only to
    whitelisted dump providers (currently empty), this tracing mode causes
    very low overhead (currently none) on the user experience.
    
    BUG=613198
    
    Review-Url: https://codereview.chromium.org/2034553002
    Cr-Commit-Position: refs/heads/master@{#397910}

--- content/browser/tracing/background_tracing_manager_impl.h
+++ content/browser/tracing/background_tracing_manager_impl.h
@@ -55,7 +55,8 @@ class BackgroundTracingManagerImpl : public BackgroundTracingManager {
   BackgroundTracingManagerImpl();
   ~BackgroundTracingManagerImpl() override;
 
-  void StartTracing(std::string, base::trace_event::TraceRecordMode);
+  void StartTracing(BackgroundTracingConfigImpl::CategoryPreset,
+                    base::trace_event::TraceRecordMode);
   void StartTracingIfConfigNeedsIt();
   void OnFinalizeStarted(std::unique_ptr<const base::DictionaryValue> metadata,
                          base::RefCountedString*);
