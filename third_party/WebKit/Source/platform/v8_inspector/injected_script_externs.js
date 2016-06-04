240e9a7122c54ca6856aa148881ee74bede96bd0
kozyatinskiy <kozyatinskiy@chromium.org>
2016-06-03 19:59:09 -0700

    [DevTools] Remove obsolete methods from V8InjectedScriptHost
    BUG=595206
    R=dgozman@chromium.org
    
    Review-Url: https://codereview.chromium.org/2043453002
    Cr-Commit-Position: refs/heads/master@{#397895}

--- third_party/WebKit/Source/platform/v8_inspector/injected_script_externs.js
+++ third_party/WebKit/Source/platform/v8_inspector/injected_script_externs.js
@@ -9,12 +9,6 @@ function InjectedScriptHostClass()
 }
 
 /**
- * @param {*} objectId
- * @param {!Object} hints
- */
-InjectedScriptHostClass.prototype.inspect = function(objectId, hints) {}
-
-/**
  * @param {*} obj
  * @return {string}
  */
@@ -58,13 +52,6 @@ InjectedScriptHostClass.prototype.getInternalProperties = function(obj) {}
  * @return {*}
  */
 InjectedScriptHostClass.prototype.suppressWarningsAndCallFunction = function(fn, receiver, argv) {}
-
-/**
- * @param {!Object} obj
- * @param {string} key
- * @param {*} value
- */
-InjectedScriptHostClass.prototype.setNonEnumProperty = function(obj, key, value) {}
 
 /**
  * @param {*} value
