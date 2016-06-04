a8291a911dc1a4bffe37c6b8fde1924489885599
kbr <kbr@chromium.org>
2016-06-03 17:52:04 -0700

    Prefer device type instead of machine name for scale factor overrides.
    This will make the Maps test work on Android bots on the commit queue.
    
    BUG=616297
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2037933003
    Cr-Commit-Position: refs/heads/master@{#397868}

--- content/test/gpu/gpu_tests/pixel.py
+++ content/test/gpu/gpu_tests/pixel.py
@@ -80,7 +80,7 @@ class PixelValidator(cloud_storage_test_base.ValidatorBase):
           os.path.dirname(__file__), page.expected_colors))
       expected_colors = self._ReadPixelExpectations(expected_colors_file)
       self._ValidateScreenshotSamples(
-          page.display_name, screenshot, expected_colors, dpr)
+          tab, page.display_name, screenshot, expected_colors, dpr)
       return
 
     image_name = self._UrlToImageName(page.display_name)
