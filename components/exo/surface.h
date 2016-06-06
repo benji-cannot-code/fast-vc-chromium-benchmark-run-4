b793f4e4661970aaa37b1e23e7d0cebab1622e5f
reveman <reveman@chromium.org>
2016-06-05 20:58:28 -0700

    exo: Handle cross-fade animations properly.
    Cross-fade animations used for example when maximizing a
    window will recreate the layer hierarchy and cross fade between
    the old layers and the new layers. This relies on the contents
    of the new layers being updated within some amount of time.
    
    Produce new texture mailboxes when a layer is recreated until
    we have an interface that provides some guarantee that the
    client will.
    
    BUG=549781
    TEST=exo_unittests
    
    Review-Url: https://codereview.chromium.org/2041663002
    Cr-Commit-Position: refs/heads/master@{#397968}

--- components/exo/surface.h
+++ components/exo/surface.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/aura/window_observer.h"
 #include "ui/compositor/compositor_observer.h"
+#include "ui/compositor/layer_owner_delegate.h"
 #include "ui/gfx/geometry/rect.h"
 
 namespace base {
@@ -82,6 +83,7 @@ class SurfaceFactoryOwner : public base::RefCounted<SurfaceFactoryOwner>,
 // It has a location, size and pixel contents.
 class Surface : public aura::Window,
                 public aura::WindowObserver,
+                public ui::LayerOwnerDelegate,
                 public ui::CompositorObserver {
  public:
   Surface();
@@ -203,6 +205,9 @@ class Surface : public aura::Window,
   void OnWindowRemovingFromRootWindow(aura::Window* window,
                                       aura::Window* new_root) override;
 
+  // Overridden from ui::LayerOwnerDelegate:
+  void OnLayerRecreated(ui::Layer* old_layer, ui::Layer* new_layer) override;
+
   // Overridden from ui::CompositorObserver:
   void OnCompositingDidCommit(ui::Compositor* compositor) override;
   void OnCompositingStarted(ui::Compositor* compositor,
@@ -220,10 +225,16 @@ class Surface : public aura::Window,
   }
 
   // Commit the current attached buffer to a TextureLayer.
-  void CommitLayerContents();
+  void CommitTextureContents();
 
   // Commit the current attached buffer to a SurfaceLayer.
   void CommitSurfaceContents();
+
+  // Set TextureLayer contents to the current buffer.
+  void SetTextureLayerContents(ui::Layer* layer);
+
+  // Set SurfaceLayer contents to the current buffer.
+  void SetSurfaceLayerContents(ui::Layer* layer);
 
   // This returns true when the surface has some contents assigned to it.
   bool has_contents() const { return !!current_buffer_; }
@@ -284,14 +295,23 @@ class Surface : public aura::Window,
   // The crop rectangle to take effect when Commit() is called.
   gfx::RectF pending_crop_;
 
+  // The active crop rectangle.
+  gfx::RectF crop_;
+
   // The secure output visibility state to take effect when Commit() is called.
   bool pending_only_visible_on_secure_output_;
+
+  // The active secure output visibility state.
+  bool only_visible_on_secure_output_;
 
   // The blend mode state to take effect when Commit() is called.
   SkXfermode::Mode pending_blend_mode_;
 
   // The alpha state to take effect when Commit() is called.
   float pending_alpha_;
+
+  // The active alpha state.
+  float alpha_;
 
   // The buffer that is currently set as content of surface.
   base::WeakPtr<Buffer> current_buffer_;
