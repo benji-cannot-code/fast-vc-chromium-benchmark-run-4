30f0345b56c9883959da98cb4de4e0e2b4e6b1d8
sigbjornf <sigbjornf@opera.com>
2016-06-06 00:13:44 -0700

    Add documentation of SafePointBarrier internal state.
    Renamed |m_canResume| to |m_parkingRequested| while doing so,
    its (inverted) meaning seems clearer imho in this context.
    
    R=
    BUG=
    
    Review-Url: https://codereview.chromium.org/2039793002
    Cr-Commit-Position: refs/heads/master@{#397988}

--- third_party/WebKit/Source/platform/heap/SafePoint.cpp
+++ third_party/WebKit/Source/platform/heap/SafePoint.cpp
@@ -21,8 +21,8 @@ static double lockingTimeout()
 }
 
 SafePointBarrier::SafePointBarrier()
-    : m_canResume(1)
-    , m_unparkedThreadCount(0)
+    : m_unparkedThreadCount(0)
+    , m_parkingRequested(0)
 {
 }
 
@@ -41,7 +41,7 @@ bool SafePointBarrier::parkOthers()
 
     MutexLocker locker(m_mutex);
     atomicAdd(&m_unparkedThreadCount, threads.size());
-    releaseStore(&m_canResume, 0);
+    releaseStore(&m_parkingRequested, 1);
 
     for (ThreadState* state : threads) {
         if (state == current)
@@ -69,7 +69,7 @@ void SafePointBarrier::resumeOthers(bool barrierLocked)
     ThreadState* current = ThreadState::current();
     const ThreadStateSet& threads = current->heap().threads();
     atomicSubtract(&m_unparkedThreadCount, threads.size());
-    releaseStore(&m_canResume, 1);
+    releaseStore(&m_parkingRequested, 0);
 
     if (UNLIKELY(barrierLocked)) {
         m_resume.broadcast();
@@ -87,7 +87,7 @@ void SafePointBarrier::resumeOthers(bool barrierLocked)
 void SafePointBarrier::checkAndPark(ThreadState* state, SafePointAwareMutexLocker* locker)
 {
     ASSERT(!state->sweepForbidden());
-    if (!acquireLoad(&m_canResume)) {
+    if (acquireLoad(&m_parkingRequested)) {
         // If we are leaving the safepoint from a SafePointAwareMutexLocker
         // call out to release the lock before going to sleep. This enables the
         // lock to be acquired in the sweep phase, e.g. during weak processing
@@ -117,7 +117,7 @@ void SafePointBarrier::doPark(ThreadState* state, intptr_t* stackEnd)
     MutexLocker locker(m_mutex);
     if (!atomicDecrement(&m_unparkedThreadCount))
         m_parked.signal();
-    while (!acquireLoad(&m_canResume))
+    while (acquireLoad(&m_parkingRequested))
         m_resume.wait(m_mutex);
     atomicIncrement(&m_unparkedThreadCount);
 }
@@ -126,14 +126,6 @@ void SafePointBarrier::doEnterSafePoint(ThreadState* state, intptr_t* stackEnd)
 {
     state->recordStackEnd(stackEnd);
     state->copyStackUntilSafePointScope();
-    // m_unparkedThreadCount tracks amount of unparked threads. It is
-    // positive if and only if we have requested other threads to park
-    // at safe-points in preparation for GC. The last thread to park
-    // itself will make the counter hit zero and should notify GC thread
-    // that it is safe to proceed.
-    // If no other thread is waiting for other threads to park then
-    // this counter can be negative: if N threads are at safe-points
-    // the counter will be -N.
     if (!atomicDecrement(&m_unparkedThreadCount)) {
         MutexLocker locker(m_mutex);
         m_parked.signal(); // Safe point reached.
