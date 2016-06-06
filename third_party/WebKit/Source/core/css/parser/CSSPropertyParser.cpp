247e84ce1e46d3bc34dafbc642aa93487ecae497
sashab <sashab@chromium.org>
2016-06-05 22:34:51 -0700

    Change CSSValueList::removeAll and hasValue to take const CSSValue&
    Change CSSValueList::removeAll and CSSValueList::hasValue to take const
    references instead of pointers. This is more semantically correct and
    safe, and is good pre-work for making CSSValueList::item() return const
    references.
    
    BUG=526586
    
    Review-Url: https://codereview.chromium.org/2033583006
    Cr-Commit-Position: refs/heads/master@{#397977}

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -1501,7 +1501,7 @@ static CSSValue* consumeTextDecorationLine(CSSParserTokenRange& range)
         CSSPrimitiveValue* ident = consumeIdent<CSSValueBlink, CSSValueUnderline, CSSValueOverline, CSSValueLineThrough>(range);
         if (!ident)
             break;
-        if (list->hasValue(ident))
+        if (list->hasValue(*ident))
             return nullptr;
         list->append(ident);
     }
@@ -1527,7 +1527,7 @@ static CSSValue* consumeContain(CSSParserTokenRange& range)
         CSSPrimitiveValue* ident = consumeIdent<CSSValuePaint, CSSValueLayout, CSSValueStyle, CSSValueSize>(range);
         if (!ident)
             break;
-        if (list->hasValue(ident))
+        if (list->hasValue(*ident))
             return nullptr;
         list->append(ident);
     }
