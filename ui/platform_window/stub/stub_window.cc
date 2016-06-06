83391b28e7658fad14de833c3b8659381e08150e
markdittmer <markdittmer@chromium.org>
2016-06-06 14:18:35 -0700

    Drop unnecessary override of OnWindowBoundsChanged in NativeWidgetMus::MusWindowObserver
    R=sky@chromium.org
    BUG=617315
    
    Review-Url: https://codereview.chromium.org/2040783002
    Cr-Commit-Position: refs/heads/master@{#398125}

--- ui/platform_window/stub/stub_window.cc
+++ ui/platform_window/stub/stub_window.cc
@@ -5,12 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/platform_window/stub/stub_window.h"
 
+#include "base/logging.h"
 #include "ui/platform_window/platform_window_delegate.h"
 
 namespace ui {
 
-StubWindow::StubWindow(PlatformWindowDelegate* delegate) : delegate_(delegate) {
-  if (delegate_)
+StubWindow::StubWindow(PlatformWindowDelegate* delegate,
+                       bool use_default_accelerated_widget)
+    : delegate_(delegate) {
+  DCHECK(delegate);
+  if (use_default_accelerated_widget)
     delegate_->OnAcceleratedWidgetAvailable(gfx::kNullAcceleratedWidget, 1.f);
 }
 
@@ -24,16 +28,14 @@ void StubWindow::Hide() {
 }
 
 void StubWindow::Close() {
-  if (delegate_)
-    delegate_->OnClosed();
+  delegate_->OnClosed();
 }
 
 void StubWindow::SetBounds(const gfx::Rect& bounds) {
   if (bounds_ == bounds)
     return;
   bounds_ = bounds;
-  if (delegate_)
-    delegate_->OnBoundsChanged(bounds);
+  delegate_->OnBoundsChanged(bounds);
 }
 
 gfx::Rect StubWindow::GetBounds() {
