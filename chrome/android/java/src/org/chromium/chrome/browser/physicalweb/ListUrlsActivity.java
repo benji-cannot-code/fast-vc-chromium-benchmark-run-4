9ccb1cb6684697679d84ce6a4bf6d215436f646c
mattreynolds <mattreynolds@chromium.org>
2016-06-06 14:00:18 -0700

    Set User-Agent and Accept-Language request headers for PWS requests
    The HTTP request headers normally used by Chrome for Android are
    generated in the native layer. We do not want to load native when the
    Physical Web is resolving URLs in the background, so we cannot access
    these values directly. Instead, they are reconstructed from values
    accessible from the Java layer.
    
    BUG=607660
    
    Review-Url: https://codereview.chromium.org/1989963003
    Cr-Commit-Position: refs/heads/master@{#398120}

--- chrome/android/java/src/org/chromium/chrome/browser/physicalweb/ListUrlsActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/physicalweb/ListUrlsActivity.java
@@ -117,7 +117,7 @@ public class ListUrlsActivity extends AppCompatActivity implements AdapterView.O
             }
         });
 
-        mPwsClient = new PwsClientImpl();
+        mPwsClient = new PwsClientImpl(this);
         int referer = getIntent().getIntExtra(REFERER_KEY, 0);
         if (savedInstanceState == null) {  // Ensure this is a newly-created activity.
             PhysicalWebUma.onActivityReferral(this, referer);
