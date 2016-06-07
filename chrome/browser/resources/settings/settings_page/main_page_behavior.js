27e9987c588ae44977782b3887e3e38af78395c1
michaelpg <michaelpg@chromium.org>
2016-06-06 20:43:53 -0700

    MD Settings: Responsive sub-page width
    The width only needs to be set explicitly during animations, when the card
    is position: fixed.
    
    BUG=608134
    R=dbeam@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2041073002
    Cr-Commit-Position: refs/heads/master@{#398217}

--- chrome/browser/resources/settings/settings_page/main_page_behavior.js
+++ chrome/browser/resources/settings/settings_page/main_page_behavior.js
@@ -252,6 +252,7 @@ var MainPageBehaviorImpl = {
       // Whether finished or canceled, clean up the animation.
       section.classList.remove('expanding');
       card.style.height = '';
+      card.style.width = '';
     });
 
     return promise;
@@ -272,6 +273,7 @@ var MainPageBehaviorImpl = {
     var startingTop = this.parentElement.getBoundingClientRect().top;
 
     var cardHeightStart = card.clientHeight;
+    var cardWidthStart = card.clientWidth;
 
     section.classList.add('collapsing');
     section.classList.remove('expanding', 'expanded');
@@ -310,7 +312,13 @@ var MainPageBehaviorImpl = {
     var options = /** @type {!KeyframeEffectOptions} */({
       duration: EXPAND_DURATION
     });
-    return this.animateElement('section', card, keyframes, options);
+
+    card.style.width = cardWidthStart + 'px';
+    var promise = this.animateElement('section', card, keyframes, options);
+    promise.then(function() {
+      card.style.width = '';
+    });
+    return promise;
   },
 };
 
