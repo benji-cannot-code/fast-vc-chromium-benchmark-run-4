1e6759c1a8f19e25d87bd5402bd564432b469041
sebmarchand <sebmarchand@chromium.org>
2016-06-07 12:04:58 -0700

    Add a buildflag to use the handle verifier in a per module mode.
    The HandleVerifier prevent you from using any ScopedHandle during a module initialization (i.e. if a DLL use one in DllMain this will call back into its host executable that hasn't been initialized yet and things will derail). In this CL I'm adding a build flag to make it possible to use it in a single module mode..
    
    Review-Url: https://codereview.chromium.org/1977833003
    Cr-Commit-Position: refs/heads/master@{#398361}

--- base/win/scoped_handle.cc
+++ base/win/scoped_handle.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/synchronization/lock_impl.h"
 #include "base/threading/thread_local.h"
+#include "base/win/base_features.h"
 #include "base/win/current_module.h"
 
 extern "C" {
@@ -132,7 +133,7 @@ void ThreadSafeAssignOrCreateActiveVerifier(ActiveVerifier* existing_verifier,
 
 // static
 void ActiveVerifier::InstallVerifier() {
-#if defined(COMPONENT_BUILD)
+#if BUILDFLAG(SINGLE_MODULE_MODE_HANDLE_VERIFIER)
   // Component build has one Active Verifier per module.
   ThreadSafeAssignOrCreateActiveVerifier(nullptr, true);
 #else
