910cefc4e66413a0a4f1b4e1ff77bda97bb47e0a
fsamuel <fsamuel@chromium.org>
2016-06-07 05:39:49 -0700

    Replace ui::LatencyInfo::InputCoordinate with gfx::PointF
    This reduces code duplication and makes the world a happier place.
    
    BUG=611802
    TBR=ben@chromium.org
    
    Review-Url: https://codereview.chromium.org/2039853004
    Cr-Commit-Position: refs/heads/master@{#398278}

--- ui/events/latency_info.h
+++ ui/events/latency_info.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "ui/events/events_base_export.h"
+#include "ui/gfx/geometry/point_f.h"
 
 #if !defined(OS_IOS)
 #include "ipc/ipc_param_traits.h"  // nogncheck
@@ -126,14 +127,6 @@ class EVENTS_BASE_EXPORT LatencyInfo {
     uint32_t event_count;
   };
 
-  struct EVENTS_BASE_EXPORT InputCoordinate {
-    InputCoordinate();
-    InputCoordinate(float x, float y);
-
-    float x;
-    float y;
-  };
-
   // Empirically determined constant based on a typical scroll sequence.
   enum { kTypicalMaxComponentsPerLatencyInfo = 10 };
 
@@ -201,12 +194,10 @@ class EVENTS_BASE_EXPORT LatencyInfo {
 
   // Returns true if there is still room for keeping the |input_coordinate|,
   // false otherwise.
-  bool AddInputCoordinate(const InputCoordinate& input_coordinate);
+  bool AddInputCoordinate(const gfx::PointF& input_coordinate);
 
   uint32_t input_coordinates_size() const { return input_coordinates_size_; }
-  const InputCoordinate* input_coordinates() const {
-    return input_coordinates_;
-  }
+  const gfx::PointF* input_coordinates() const { return input_coordinates_; }
 
   const LatencyMap& latency_components() const { return latency_components_; }
 
@@ -237,7 +228,7 @@ class EVENTS_BASE_EXPORT LatencyInfo {
 
   // These coordinates represent window coordinates of the original input event.
   uint32_t input_coordinates_size_;
-  InputCoordinate input_coordinates_[kMaxInputCoordinates];
+  gfx::PointF input_coordinates_[kMaxInputCoordinates];
 
   // The unique id for matching the ASYNC_BEGIN/END trace event.
   int64_t trace_id_;
