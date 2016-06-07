f9f3343827d785e7e3f907e0d02e1a2e2fb36972
mlliu <mlliu@chromium.org>
2016-06-07 11:22:01 -0700

    remove google account requirement on the client for local testing
    1) What used to happen before this CL?
    Even when the user provides the engine ip, port, transport protocol and
    token in the command line, he/she still needs to enter a google account
    although in the c++ code, it doesn't use the token generated by GAIA.
    
    2) Why was that bad? What kind of trouble did that get us into?
    It's not feasible to automate entering account from the bot. It'll
    keep us from doing UI-related automation testing.
    
    3) What does this CL do to make it better?
    It checks the command line flag from the java code, so that if engine ip
    etc are provided by the command line, a google account is not needed.
    
    BUG=602826
    
    Review-Url: https://codereview.chromium.org/2034953002
    Cr-Commit-Position: refs/heads/master@{#398347}

---
+++ blimp/client/app/android/java/src/org/chromium/blimp/BlimpClientSwitches.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.blimp;
+
+/**
+ * Contains all of the command line switches that are specific to the blimp/
+ * portion of Chromium on Android.
+ */
+public final class BlimpClientSwitches {
+
+    // Specifies the engine's IP address. Must be used in conjunction with
+    // --engine-port and --engine-transport.
+    // This is the same command line flag as kEngineIP in
+    // blimp/client/app/blimp_client_switches.cc
+    public static final String ENGINE_IP = "engine-ip";
+
+    // Prevent instantiation.
+    private BlimpClientSwitches() {}
+}
