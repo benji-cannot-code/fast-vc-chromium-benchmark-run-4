982fa569fcf966c3dda878ebae2335773a9dcdd3
hlopko <hlopko@chromium.org>
2016-06-07 05:02:38 -0700

    Maintain wrapper tracing deque in ScriptWrappableVisitor
    This cl uses updated V8 api which will be easier to work with incrementally
    (althrough it is not used incrementally at the moment). For that
    ScriptWrappableVisitor now has to maintain his marking deque.
    
    LOG=no
    BUG=468240
    
    Review-Url: https://codereview.chromium.org/2044023002
    Cr-Commit-Position: refs/heads/master@{#398273}

--- third_party/WebKit/Source/bindings/core/v8/ScriptWrappable.h
+++ third_party/WebKit/Source/bindings/core/v8/ScriptWrappable.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ScriptWrappable_h
 #define ScriptWrappable_h
 
-#include "bindings/core/v8/ScriptWrappableVisitor.h"
 #include "bindings/core/v8/WrapperTypeInfo.h"
 #include "core/CoreExport.h"
 #include "platform/heap/Handle.h"
