8216450928982dec53476b76446c84ff75787e07
caseq <caseq@chromium.org>
2016-06-07 01:07:56 -0700

    Prevent display going to sleep while taking a screenshot
    On MacOS, GrabViewSnapshot() call would block for the duration of
    display/GPU sleep which leads to protocol client to wait and tests
    to time out when power saving is engaged. This prevents power saving
    while there are pending screenshot requests.
    
    BUG=599776
    
    Review-Url: https://codereview.chromium.org/2044783002
    Cr-Commit-Position: refs/heads/master@{#398245}

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -83,6 +83,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/snapshot/snapshot.h"
 
 #if defined(OS_MACOSX)
+#include "content/public/browser/power_save_blocker.h"
+#include "content/public/browser/power_save_blocker_factory.h"
 #include "ui/accelerated_widget_mac/window_resize_helper_mac.h"
 #endif
 
@@ -1285,6 +1287,20 @@ void RenderWidgetHostImpl::NotifyScreenInfoChanged() {
 void RenderWidgetHostImpl::GetSnapshotFromBrowser(
     const GetSnapshotFromBrowserCallback& callback) {
   int id = next_browser_snapshot_id_++;
+
+#if defined(OS_MACOSX)
+  // MacOS version of underlying GrabViewSnapshot() blocks while
+  // display/GPU are in a power-saving mode, so make sure display
+  // does not go to sleep for the duration of reading a snapshot.
+  if (pending_browser_snapshots_.empty()) {
+    DCHECK(!power_save_blocker_);
+    power_save_blocker_.reset(
+        CreatePowerSaveBlocker(
+            PowerSaveBlocker::kPowerSaveBlockPreventDisplaySleep,
+            PowerSaveBlocker::kReasonOther, "GetSnapshot")
+            .release());
+  }
+#endif
   pending_browser_snapshots_.insert(std::make_pair(id, callback));
   Send(new ViewMsg_ForceRedraw(GetRoutingID(), id));
 }
@@ -2144,6 +2160,10 @@ void RenderWidgetHostImpl::OnSnapshotDataReceived(int snapshot_id,
         ++it;
       }
   }
+#if defined(OS_MACOSX)
+  if (pending_browser_snapshots_.empty())
+    power_save_blocker_.reset();
+#endif
 }
 
 void RenderWidgetHostImpl::OnSnapshotDataReceivedAsync(
