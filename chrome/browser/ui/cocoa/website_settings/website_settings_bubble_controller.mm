3b838011a0aced3cac9f7a2e60e53026e58c15f9
dominickn <dominickn@chromium.org>
2016-06-08 16:04:34 -0700

    [Mac] Dismiss an open page info dialog when the location icon is clicked.
    On views platforms, clicking the location (lock) icon opens the page
    info dialog. Clicking the icon again dismisses the dialog. Currently,
    the same actions on Mac "refreshes" the dialog in place, with a strange
    animation caused by the original dialog disappearing and a new one
    taking its place immediately.
    
    This CL makes the Cocoa and toolkit-views page info dialogs on Mac
    behave consistently with views platforms, so that clicking the location
    icon when the dialog is open dismisses the dialog.
    
    BUG=59289
    TEST=Open Chrome on Mac, and navigate to a page. Click on the lock
    icon to open the page info dialog. Click on the icon again and the
    dialog should dismiss, rather than refresh in place. The same
    behaviour should apply to the toolkit-views and MacViews-WebUI
    browsers.
    
    Review-Url: https://codereview.chromium.org/2041723002
    Cr-Commit-Position: refs/heads/master@{#398721}

--- chrome/browser/ui/cocoa/website_settings/website_settings_bubble_controller.mm
+++ chrome/browser/ui/cocoa/website_settings/website_settings_bubble_controller.mm
@@ -111,6 +111,11 @@ const CGFloat kPermissionPopUpXSpacing = 3;
 // |IDS_WEBSITE_SETTINGS_{FIRST,THIRD}_PARTY_SITE_DATA| next to each other.
 const CGFloat kTextLabelXPadding = 5;
 
+// NOTE: This assumes that there will never be more than one website settings
+// popup shown, and that the one that is shown is associated with the current
+// window. This matches the behaviour in views: see WebsiteSettingsPopupView.
+bool g_is_popup_showing = false;
+
 // Takes in the parent window, which should be a BrowserWindow, and gets the
 // proper anchor point for the bubble. The returned point is in screen
 // coordinates.
@@ -1097,9 +1102,14 @@ WebsiteSettingsUIBridge::WebsiteSettingsUIBridge(
     content::WebContents* web_contents)
     : content::WebContentsObserver(web_contents),
       web_contents_(web_contents),
-      bubble_controller_(nil) {}
+      bubble_controller_(nil) {
+  DCHECK(!g_is_popup_showing);
+  g_is_popup_showing = true;
+}
 
 WebsiteSettingsUIBridge::~WebsiteSettingsUIBridge() {
+  DCHECK(g_is_popup_showing);
+  g_is_popup_showing = false;
 }
 
 void WebsiteSettingsUIBridge::set_bubble_controller(
@@ -1120,6 +1130,12 @@ void WebsiteSettingsUIBridge::Show(
     return;
   }
 
+  // Don't show the popup if it's already being shown. Since this method is
+  // called each time the location icon is clicked, each click toggles the popup
+  // in and out.
+  if (g_is_popup_showing)
+    return;
+
   BubbleType bubble_type = WEB_PAGE;
   if (virtual_url.SchemeIs(content::kChromeUIScheme))
     bubble_type = INTERNAL_PAGE;
@@ -1132,7 +1148,8 @@ void WebsiteSettingsUIBridge::Show(
   bool is_devtools_disabled =
       profile->GetPrefs()->GetBoolean(prefs::kDevToolsDisabled);
 
-  // Create the bubble controller. It will dealloc itself when it closes.
+  // Create the bubble controller. It will dealloc itself when it closes,
+  // resetting |g_is_popup_showing|.
   WebsiteSettingsBubbleController* bubble_controller =
       [[WebsiteSettingsBubbleController alloc]
              initWithParentWindow:parent
