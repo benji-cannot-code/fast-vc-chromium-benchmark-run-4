d8d49a7e5cb971baa5e97eb4e2d48386d74902e5
sadrul <sadrul@chromium.org>
2016-06-08 10:37:08 -0700

    views/mus: Chaneg how Widget activation works in mus.
    Install an aura::client::FocusClient implementation on NativeWidgetMus's aura
    window-tree which focuses the mus::Window when the aura::Window is focused.
    
    This change allows running some tests in views/focus/focus_manager_unittest.cc
    under mus.
    
    BUG=577274
    
    Review-Url: https://codereview.chromium.org/2049723002
    Cr-Commit-Position: refs/heads/master@{#398610}

--- ui/wm/core/focus_controller.h
+++ ui/wm/core/focus_controller.h
@@ -50,7 +50,7 @@ class WM_EXPORT FocusController : public aura::client::ActivationClient,
   explicit FocusController(FocusRules* rules);
   ~FocusController() override;
 
- private:
+ protected:
   // Overridden from aura::client::ActivationClient:
   void AddObserver(aura::client::ActivationChangeObserver* observer) override;
   void RemoveObserver(
@@ -82,6 +82,7 @@ class WM_EXPORT FocusController : public aura::client::ActivationClient,
   void OnWindowHierarchyChanging(const HierarchyChangeParams& params) override;
   void OnWindowHierarchyChanged(const HierarchyChangeParams& params) override;
 
+ private:
   // Internal implementation that coordinates window focus and activation
   // changes.
   void FocusAndActivateWindow(
