ae56e27cb70ac99c9425b26c74e94711bea20ac3
timvolodine <timvolodine@chromium.org>
2016-06-08 09:08:39 -0700

    [Android] Disable flaky BattOrCases telemetry_perf_unittest
    Flakes on Android Tests bot.
    https://uberchromegw.corp.google.com/i/chromium.linux/builders/Android%20Tests
    
    BUG=618330
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:android_s5_perf_cq;tryserver.chromium.perf:winx64_10_perf_cq;tryserver.chromium.perf:mac_retina_perf_cq
    NOTRY=true
    TBR=nednguyen@google.com,boliu@chromium.org
    
    Review-Url: https://codereview.chromium.org/2049113002
    Cr-Commit-Position: refs/heads/master@{#398583}

--- tools/perf/benchmarks/battor.py
+++ tools/perf/benchmarks/battor.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from core import perf_benchmark
 from telemetry.web_perf import timeline_based_measurement
 import page_sets
+from telemetry import benchmark
 
 
 class _BattOrBenchmark(perf_benchmark.PerfBenchmark):
@@ -34,6 +35,7 @@ class _BattOrBenchmark(perf_benchmark.PerfBenchmark):
     return True
 
 
+@benchmark.Disabled('android')  # crbug.com/618330
 class BattOrPowerMobileSites(_BattOrBenchmark):
   page_set = page_sets.power_cases.PowerCasesPageSet
 
