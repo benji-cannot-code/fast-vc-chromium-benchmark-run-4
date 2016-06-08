0f1112a77c54016c723023148a11e3936617ad12
jinho.bang <jinho.bang@samsung.com>
2016-06-07 18:52:54 -0700

    PaymentRequest: Sync up PaymentAddress with the payment request spec.
    Some attributes in PaymentAddress was renamed and added. This CL syncs up
    the PaymentAddress with the spec[1].
    
    [1] https://w3c.github.io/browser-payment-api/specs/paymentrequest.html#paymentaddress-interface
    
    BUG=617190
    
    Review-Url: https://codereview.chromium.org/2039863002
    Cr-Commit-Position: refs/heads/master@{#398455}

--- third_party/WebKit/Source/modules/payments/PaymentAddress.cpp
+++ third_party/WebKit/Source/modules/payments/PaymentAddress.cpp
@@ -8,16 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 PaymentAddress::PaymentAddress(mojom::blink::PaymentAddressPtr address)
-    : m_regionCode(address->region_code)
+    : m_country(address->country)
     , m_addressLine(address->address_line.PassStorage())
-    , m_administrativeArea(address->administrative_area)
-    , m_locality(address->locality)
+    , m_region(address->region)
+    , m_city(address->city)
     , m_dependentLocality(address->dependent_locality)
     , m_postalCode(address->postal_code)
     , m_sortingCode(address->sorting_code)
     , m_languageCode(address->language_code)
     , m_organization(address->organization)
     , m_recipient(address->recipient)
+    , m_careOf(address->careOf)
+    , m_phone(address->phone)
 {
     if (!m_languageCode.isEmpty() && !address->script_code.isEmpty()) {
         m_languageCode.append("-");
