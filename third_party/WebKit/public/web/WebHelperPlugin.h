4e10e9b917ad16d4069e250864c39482feec0d00
yutak <yutak@chromium.org>
2016-06-08 02:36:09 -0700

    Remove the use of OwnedPtrDeleter in WebHelperPlugin.
    It's hard to mechanically replace specializations of OwnedPtrDeleter<T>
    to std::unique_ptr equivalents. Therefore, they are converted to
    std::unique_ptr manually.
    
    A new typedef WebHelperPluginUniquePtr is created so people can always
    use std::unique_ptr with a correct deleter.
    
    BUG=617504
    
    Review-Url: https://codereview.chromium.org/2041263002
    Cr-Commit-Position: refs/heads/master@{#398508}

--- third_party/WebKit/public/web/WebHelperPlugin.h
+++ third_party/WebKit/public/web/WebHelperPlugin.h
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "../platform/WebCommon.h"
 #include "WebFrame.h"
 
+#if INSIDE_BLINK
+#include <memory>
+#endif
+
 namespace blink {
 
 class WebPlugin;
@@ -57,19 +61,18 @@ protected:
     virtual ~WebHelperPlugin() { }
 };
 
-} // namespace blink
-
-namespace WTF {
-
-template<typename T> struct OwnedPtrDeleter;
-template<> struct OwnedPtrDeleter<blink::WebHelperPlugin> {
-    static void deletePtr(blink::WebHelperPlugin* plugin)
+#if INSIDE_BLINK
+struct WebHelperPluginDeleter {
+    void operator()(WebHelperPlugin* plugin)
     {
         if (plugin)
             plugin->destroy();
     }
 };
 
-} // namespace WTF
+using WebHelperPluginUniquePtr = std::unique_ptr<WebHelperPlugin, WebHelperPluginDeleter>;
+#endif
+
+} // namespace blink
 
 #endif
