f90cb17fa65af82b73346524fe79fe3539a31310
harkness <harkness@chromium.org>
2016-06-08 03:56:32 -0700

    Create new directory for budget_service.
    The budget service is going to be expanded in the future.
    Rather than pollute the push_messaging directory with the
    general purpose budget service, this separates it out into
    its own directory.
    
    BUG=617971
    
    Review-Url: https://codereview.chromium.org/2039283002
    Cr-Commit-Position: refs/heads/master@{#398518}

--- chrome/browser/budget_service/background_budget_service_unittest.cc
+++ chrome/browser/budget_service/background_budget_service_unittest.cc
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "base/test/simple_test_clock.h"
+#include "chrome/browser/budget_service/background_budget_service.h"
+#include "chrome/browser/budget_service/background_budget_service_factory.h"
 #include "chrome/browser/engagement/site_engagement_service.h"
-#include "chrome/browser/push_messaging/background_budget_service.h"
-#include "chrome/browser/push_messaging/background_budget_service_factory.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/prefs/pref_service.h"
--- chrome/browser/push_messaging/push_messaging_service_factory.cc
+++ chrome/browser/push_messaging/push_messaging_service_factory.cc
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "chrome/browser/budget_service/background_budget_service_factory.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/permissions/permission_manager_factory.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/push_messaging/background_budget_service_factory.h"
 #include "chrome/browser/push_messaging/push_messaging_service_impl.h"
 #include "chrome/browser/services/gcm/fake_gcm_profile_service.h"
 #include "chrome/browser/services/gcm/gcm_profile_service_factory.h"
