fda9f5fae59c9731126a02a2b064458571b4230d
tsergeant <tsergeant@chromium.org>
2016-06-07 21:08:15 -0700

    MD WebUI: Add loading spinner to cr-toolbar, hook into MD History toolbar
    Adds a processing spinner which is visible when the search bar is open,
    and hooks it into MD History so that it is shown when loading data from
    a search. It is not shown when incrementally loading new data in the
    infinite list.
    
    BUG=610609
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2032313002
    Cr-Commit-Position: refs/heads/master@{#398476}

--- chrome/browser/resources/md_history/app.js
+++ chrome/browser/resources/md_history/app.js
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *            searchTerm: string,
  *            results: ?Array<!HistoryEntry>,
  *            info: ?HistoryQuery,
+ *            incremental: boolean,
  *            range: HistoryRange,
  *            groupedOffset: number,
  *            sessionList: ?Array<!ForeignSession>}}
@@ -41,6 +42,8 @@ Polymer({
           querying: true,
           searchTerm: '',
           results: null,
+          // Whether the most recent query was incremental.
+          incremental: false,
           info: null,
           range: HistoryRange.ALL_TIME,
           // TODO(calamity): Make history toolbar buttons change the offset.
@@ -65,6 +68,7 @@ Polymer({
     'load-more-history': 'loadMoreHistory_',
   },
 
+  /** @override */
   ready: function() {
     this.grouped_ = loadTimeData.getBoolean('groupByDomain');
   },
@@ -176,6 +180,7 @@ Polymer({
       return;
 
     this.set('queryState_.querying', true);
+    this.set('queryState_.incremental', incremental);
 
     var queryState = this.queryState_;
 
@@ -205,6 +210,11 @@ Polymer({
     this.set('queryState_.sessionList', sessionList);
   },
 
+  /**
+   * @param {string} selectedPage
+   * @param {HistoryRange} range
+   * @return {string}
+   */
   getSelectedPage: function(selectedPage, range) {
     if (selectedPage == 'history-list' && range != HistoryRange.ALL_TIME)
       return 'history-grouped-list';
@@ -212,7 +222,24 @@ Polymer({
     return selectedPage;
   },
 
-  syncedTabsSelected_(selectedPage) {
+  /**
+   * @param {string} selectedPage
+   * @return {boolean}
+   * @private
+   */
+  syncedTabsSelected_: function(selectedPage) {
     return selectedPage == 'history-synced-device-manager';
+  },
+
+  /**
+   * @param {boolean} querying
+   * @param {boolean} incremental
+   * @param {string} searchTerm
+   * @return {boolean} Whether a loading spinner should be shown (implies the
+   *     backend is querying a new search term).
+   * @private
+   */
+  shouldShowSpinner_: function(querying, incremental, searchTerm) {
+    return querying && !incremental && searchTerm != '';
   }
 });
