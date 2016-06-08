9afe3b0084e4a8ad756ed9c0036dff1c87ec10c6
alemate <alemate@chromium.org>
2016-06-08 16:12:39 -0700

    ChromeOS: Update OOBE shelf on gaia screen reload.
    This CL fixes the problem when shelf state is not updated after
    OOBE gaia screen reload.
    
    BUG=588099
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2049633002
    Cr-Commit-Position: refs/heads/master@{#398725}

--- chrome/browser/chromeos/login/screens/core_oobe_actor.h
+++ chrome/browser/chromeos/login/screens/core_oobe_actor.h
@@ -46,7 +46,7 @@ class CoreOobeActor {
   virtual void ShowEnableDebuggingScreen() = 0;
   virtual void InitDemoModeDetection() = 0;
   virtual void StopDemoModeDetection() = 0;
-
+  virtual void UpdateKeyboardState() = 0;
 };
 
 }  // namespace chromeos
