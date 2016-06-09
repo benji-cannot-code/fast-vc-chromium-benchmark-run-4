71702efd42de437a53baaa04575a2cabb08aafc2
zqzhang <zqzhang@chromium.org>
2016-06-09 10:09:51 -0700

    Implement MediaMetadata artwork in Blink
    This CL adds artwork in MediaMetadata in Blink.
    
    The artwork info will be propagated to the browser through MediaMetadata, and we will finally fetch the image and display it in media notification.
    
    Discussion for the spec:
    https://github.com/whatwg/mediasession/pull/129
    
    Related CLs:
    https://codereview.chromium.org/2015433003/
    https://codereview.chromium.org/2014553002/
    https://codereview.chromium.org/2009243002/
    
    BUG=616411
    
    Review-Url: https://codereview.chromium.org/2013813002
    Cr-Commit-Position: refs/heads/master@{#398922}

--- third_party/WebKit/public/platform/modules/mediasession/WebMediaMetadata.h
+++ third_party/WebKit/public/platform/modules/mediasession/WebMediaMetadata.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WebMediaMetadata_h
 
 #include "public/platform/WebString.h"
+#include "public/platform/WebVector.h"
+#include "public/platform/modules/mediasession/WebMediaArtwork.h"
 
 namespace blink {
 
@@ -15,6 +17,7 @@ struct WebMediaMetadata {
     WebString title;
     WebString artist;
     WebString album;
+    WebVector<WebMediaArtwork> artwork;
 };
 
 } // namespace blink
