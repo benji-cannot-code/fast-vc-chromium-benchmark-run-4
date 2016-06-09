8401d625e212ebfd6b46e08c763cdcc87474b14b
skyostil <skyostil@chromium.org>
2016-06-09 10:11:17 -0700

    scheduler: Enable expensive task blocking by default
    BUG=598248,574343,581039
    
    Review-Url: https://codereview.chromium.org/2025863002
    Cr-Commit-Position: refs/heads/master@{#398924}

--- components/scheduler/renderer/renderer_scheduler.cc
+++ components/scheduler/renderer/renderer_scheduler.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace scheduler {
 namespace {
 const base::Feature kExpensiveTaskBlockingPolicyFeature{
-    "SchedulerExpensiveTaskBlocking", base::FEATURE_DISABLED_BY_DEFAULT};
+    "SchedulerExpensiveTaskBlocking", base::FEATURE_ENABLED_BY_DEFAULT};
 }
 
 RendererScheduler::RendererScheduler() {
