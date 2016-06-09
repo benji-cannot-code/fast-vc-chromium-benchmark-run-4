121cf6490a8e458aa44cd226ebd4dad8db638b6b
chongz <chongz@chromium.org>
2016-06-09 09:44:20 -0700

    Pressing ContextMenu key should scroll focused Element to view
    Focused Element should be scrolled to view when pressing ContextMenu key,
    otherwise ContextMenu will be displayed off-screen.
    
    Interop:
    When the focused Element is off-screen,
      * Edge display ContextMenu on the top-left corner;
      * Firefox scroll the Element to top or bottom edge.
    We match Firefox but scroll the Element to center, which matches our
    text insertion scroll.
    
    Also note that scroll was handled in 'keydown' event (match FF), so
      1. Mouse right click won't be affected;
      2. Preventing 'contextmenu' won't prevent scroll;
      3. Does not match our text insertion scroll, which was handled in 'textInput' event.
    
    BUG=323152
    
    Review-Url: https://codereview.chromium.org/2047493003
    Cr-Commit-Position: refs/heads/master@{#398916}

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -1541,6 +1541,9 @@ WebInputEventResult WebViewImpl::sendContextMenuEvent(const WebKeyboardEvent& ev
         Frame* focusedFrame = page()->focusController().focusedOrMainFrame();
         if (!focusedFrame->isLocalFrame())
             return WebInputEventResult::NotHandled;
+        // Firefox reveal focus based on "keydown" event but not "contextmenu" event, we match FF.
+        if (Element* focusedElement = toLocalFrame(focusedFrame)->document()->focusedElement())
+            focusedElement->scrollIntoViewIfNeeded();
         return toLocalFrame(focusedFrame)->eventHandler().sendContextMenuEventForKey(nullptr);
     }
 }
