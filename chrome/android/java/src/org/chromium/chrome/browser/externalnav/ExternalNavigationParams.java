ecacaa65508df55fa7a494145ce7de846c07b6e1
pkotwicz <pkotwicz@chromium.org>
2016-06-09 11:09:29 -0700

    Upstream: Launch WebAPK without showing intent picker when user taps link in WebAPK scope
    This CL surpresses the intent picker when:
    - User navigates via a link
    AND
    - The URL falls in the scope of a WebAPK
    AND
    - The WebAPK is the only specialized handler for the URL
    
    BUG=609122
    TEST=ExternalNavigationHandlerTest.*
    
    Review-Url: https://codereview.chromium.org/2035183002
    Cr-Commit-Position: refs/heads/master@{#398946}

--- chrome/android/java/src/org/chromium/chrome/browser/externalnav/ExternalNavigationParams.java
+++ chrome/android/java/src/org/chromium/chrome/browser/externalnav/ExternalNavigationParams.java
@@ -44,8 +44,11 @@ public class ExternalNavigationParams {
     /** Whether this navigation happens in main frame. */
     private final boolean mIsMainFrame;
 
-    /** Whether this navigation happens in a WebAPK. */
-    private final boolean mIsWebApk;
+    /**
+     * The package name of the WebAPK that the navigation happens in. Null if the navigation is not
+     * happening in a WebAPK.
+     */
+    private final String mWebApkPackageName;
 
     /** Whether this navigation is launched by user gesture. */
     private final boolean mHasUserGesture;
@@ -59,7 +62,7 @@ public class ExternalNavigationParams {
     private ExternalNavigationParams(String url, boolean isIncognito, String referrerUrl,
             int pageTransition, boolean isRedirect, boolean appMustBeInForeground,
             TabRedirectHandler redirectHandler, Tab tab, boolean openInNewTab,
-            boolean isBackgroundTabNavigation, boolean isMainFrame, boolean isWebApk,
+            boolean isBackgroundTabNavigation, boolean isMainFrame, String webApkPackageName,
             boolean hasUserGesture,
             boolean shouldCloseContentsOnOverrideUrlLoadingAndLaunchIntent) {
         mUrl = url;
@@ -73,7 +76,7 @@ public class ExternalNavigationParams {
         mOpenInNewTab = openInNewTab;
         mIsBackgroundTabNavigation = isBackgroundTabNavigation;
         mIsMainFrame = isMainFrame;
-        mIsWebApk = isWebApk;
+        mWebApkPackageName = webApkPackageName;
         mHasUserGesture = hasUserGesture;
         mShouldCloseContentsOnOverrideUrlLoadingAndLaunchIntent =
                 shouldCloseContentsOnOverrideUrlLoadingAndLaunchIntent;
@@ -137,9 +140,12 @@ public class ExternalNavigationParams {
         return mIsMainFrame;
     }
 
-    /** @return Whether this navigation happens in a WebAPK. */
-    public boolean isWebApk() {
-        return mIsWebApk;
+    /**
+     * @return Package name of the WebAPK that the navigation happens in. Null if the navigation is
+     *         not happening in a WebAPK.
+     */
+    public String webApkPackageName() {
+        return mWebApkPackageName;
     }
 
     /** @return Whether this navigation is launched by user gesture. */
@@ -189,8 +195,11 @@ public class ExternalNavigationParams {
         /** Whether this navigation happens in main frame. */
         private boolean mIsMainFrame;
 
-        /** Whether this navigation happens in a WebAPK. */
-        private boolean mIsWebApk;
+        /**
+         * The package name of the WebAPK that the navigation happens in. Null if the navigation is
+         * not happening in a WebAPK.
+         */
+        private String mWebApkPackageName;
 
         /** Whether this navigation is launched by user gesture. */
         private boolean mHasUserGesture;
@@ -251,9 +260,12 @@ public class ExternalNavigationParams {
             return this;
         }
 
-        /** Sets whether this navigation happens in a WebAPK. */
-        public Builder setIsWebApk(boolean v) {
-            mIsWebApk = v;
+        /**
+         * Sets the package name of the WebAPK that the navigation happens in. Null if the
+         * navigation is not happening in a WebAPK.
+         */
+        public Builder setWebApkPackageName(String v) {
+            mWebApkPackageName = v;
             return this;
         }
 
@@ -275,7 +287,7 @@ public class ExternalNavigationParams {
         public ExternalNavigationParams build() {
             return new ExternalNavigationParams(mUrl, mIsIncognito, mReferrerUrl, mPageTransition,
                     mIsRedirect, mApplicationMustBeInForeground, mRedirectHandler, mTab,
-                    mOpenInNewTab, mIsBackgroundTabNavigation, mIsMainFrame, mIsWebApk,
+                    mOpenInNewTab, mIsBackgroundTabNavigation, mIsMainFrame, mWebApkPackageName,
                     mHasUserGesture, mShouldCloseContentsOnOverrideUrlLoadingAndLaunchIntent);
         }
     }
--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/ExternalNavigationDelegateImplTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/ExternalNavigationDelegateImplTest.java
@@ -35,8 +35,8 @@ public class ExternalNavigationDelegateImplTest extends ChromeActivityTestCaseBa
     public void testIsPackageSpecializedHandler_NoResolveInfo() {
         String packageName = "";
         List<ResolveInfo> resolveInfos = new ArrayList<ResolveInfo>();
-        assertFalse(ExternalNavigationDelegateImpl
-                .isPackageSpecializedHandler(resolveInfos, packageName));
+        assertEquals(0, ExternalNavigationDelegateImpl.countSpecializedHandlersWithFilter(
+                                resolveInfos, packageName));
     }
 
     @SmallTest
@@ -45,8 +45,8 @@ public class ExternalNavigationDelegateImplTest extends ChromeActivityTestCaseBa
         ResolveInfo info = new ResolveInfo();
         info.filter = new IntentFilter();
         List<ResolveInfo> resolveInfos = makeResolveInfos(info);
-        assertFalse(ExternalNavigationDelegateImpl
-                .isPackageSpecializedHandler(resolveInfos, packageName));
+        assertEquals(0, ExternalNavigationDelegateImpl.countSpecializedHandlersWithFilter(
+                                resolveInfos, packageName));
     }
 
     @SmallTest
@@ -56,8 +56,8 @@ public class ExternalNavigationDelegateImplTest extends ChromeActivityTestCaseBa
         info.filter = new IntentFilter();
         info.filter.addDataPath("somepath", 2);
         List<ResolveInfo> resolveInfos = makeResolveInfos(info);
-        assertTrue(ExternalNavigationDelegateImpl
-                .isPackageSpecializedHandler(resolveInfos, packageName));
+        assertEquals(1, ExternalNavigationDelegateImpl.countSpecializedHandlersWithFilter(
+                                resolveInfos, packageName));
     }
 
     @SmallTest
@@ -67,8 +67,8 @@ public class ExternalNavigationDelegateImplTest extends ChromeActivityTestCaseBa
         info.filter = new IntentFilter();
         info.filter.addDataAuthority("http://www.google.com", "80");
         List<ResolveInfo> resolveInfos = makeResolveInfos(info);
-        assertTrue(ExternalNavigationDelegateImpl
-                .isPackageSpecializedHandler(resolveInfos, packageName));
+        assertEquals(1, ExternalNavigationDelegateImpl.countSpecializedHandlersWithFilter(
+                                resolveInfos, packageName));
     }
 
     @SmallTest
@@ -80,8 +80,8 @@ public class ExternalNavigationDelegateImplTest extends ChromeActivityTestCaseBa
         info.activityInfo = new ActivityInfo();
         info.activityInfo.packageName = packageName;
         List<ResolveInfo> resolveInfos = makeResolveInfos(info);
-        assertTrue(ExternalNavigationDelegateImpl
-                .isPackageSpecializedHandler(resolveInfos, packageName));
+        assertEquals(1, ExternalNavigationDelegateImpl.countSpecializedHandlersWithFilter(
+                                resolveInfos, packageName));
     }
 
     @SmallTest
@@ -93,8 +93,8 @@ public class ExternalNavigationDelegateImplTest extends ChromeActivityTestCaseBa
         info.activityInfo = new ActivityInfo();
         info.activityInfo.packageName = "com.foo.bar";
         List<ResolveInfo> resolveInfos = makeResolveInfos(info);
-        assertFalse(ExternalNavigationDelegateImpl
-                .isPackageSpecializedHandler(resolveInfos, packageName));
+        assertEquals(0, ExternalNavigationDelegateImpl.countSpecializedHandlersWithFilter(
+                                resolveInfos, packageName));
     }
 
     @SmallTest
