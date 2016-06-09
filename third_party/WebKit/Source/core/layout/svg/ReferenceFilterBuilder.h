2c107da890f1916820816fbce3422c32d2c8482b
fs <fs@opera.com>
2016-06-09 06:59:36 -0700

    Move ReferenceFilterBuilder::build to FilterEffectBuilder
    This function resolves a filter reference and then builds a filter
    (sub)DAG for painting. Split it into the two fairly distinct parts,
    leaving ReferenceFilterBuilder as a class that only does element
    lookup/resolution, while the DAG-building takes place in
    FilterEffectBuilder (with a little help from SVGFilterBuilder.)
    Use the new function in SVGFilterPainter.
    
    Also pass Element& to ReferenceFilterBuilder::build, and remove a
    redundant null-check of ComputedStyle in
    PaintLayer::updateOrRemoveFilterEffectBuilder (already checked by
    paintsWithFilters and assumed later in the function.)
    
    BUG=109224,533457
    
    Review-Url: https://codereview.chromium.org/2044153002
    Cr-Commit-Position: refs/heads/master@{#398866}

--- third_party/WebKit/Source/core/layout/svg/ReferenceFilterBuilder.h
+++ third_party/WebKit/Source/core/layout/svg/ReferenceFilterBuilder.h
@@ -34,26 +34,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/fetch/DocumentResourceReference.h"
 #include "wtf/Allocator.h"
-#include "wtf/HashMap.h"
-#include "wtf/PassRefPtr.h"
-
-class SkPaint;
 
 namespace blink {
 
 class Element;
-class Filter;
-class FilterEffect;
 class FilterOperation;
-class FloatSize;
 class ReferenceFilterOperation;
+class SVGFilterElement;
 
 class ReferenceFilterBuilder {
     STATIC_ONLY(ReferenceFilterBuilder);
 public:
     static DocumentResourceReference* documentResourceReference(const FilterOperation*);
     static void setDocumentResourceReference(const FilterOperation*, DocumentResourceReference*);
-    static Filter* build(float zoom, Element*, FilterEffect* previousEffect, const ReferenceFilterOperation&, const FloatSize* referenceBoxSize = nullptr, const SkPaint* fillPaint = nullptr, const SkPaint* strokePaint = nullptr);
+
+    static SVGFilterElement* resolveFilterReference(const ReferenceFilterOperation&, Element&);
 };
 
 } // namespace blink
