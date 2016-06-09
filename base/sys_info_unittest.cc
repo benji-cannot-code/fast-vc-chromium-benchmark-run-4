6b5e5330cf5e7dac728b2c6a77019e05eead0e5b
xyzzyz <xyzzyz@chromium.org>
2016-06-09 10:21:46 -0700

    Remove MaxSharedMemorySize() function from SysInfo
    It is not used anywhere, and is only supported on some platforms. In
    addition, it synchronously reads a file during Zygote startup.
    
    Last use was removed in 2014: 1cf03f7f0c2eff4e34b6d6e4a088dcbd762ee9b9
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2054593002
    Cr-Commit-Position: refs/heads/master@{#398926}

--- base/sys_info_unittest.cc
+++ base/sys_info_unittest.cc
@@ -17,13 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 typedef PlatformTest SysInfoTest;
 using base::FilePath;
 
-#if defined(OS_POSIX) && !defined(OS_MACOSX) && !defined(OS_ANDROID)
-TEST_F(SysInfoTest, MaxSharedMemorySize) {
-  // We aren't actually testing that it's correct, just that it's sane.
-  EXPECT_GT(base::SysInfo::MaxSharedMemorySize(), 0u);
-}
-#endif
-
 TEST_F(SysInfoTest, NumProcs) {
   // We aren't actually testing that it's correct, just that it's sane.
   EXPECT_GE(base::SysInfo::NumberOfProcessors(), 1);
