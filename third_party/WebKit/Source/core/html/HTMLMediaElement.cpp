e0afa8f7860072db6fd7700f37e45c7d91e1befd
avayvod <avayvod@chromium.org>
2016-06-09 08:19:06 -0700

    [Blink, HTMLMediaElement] Fix ASSERT in ScriptPromiseResolver dtor
    Don't get promise() from the resolver if it's not used.
    
    BUG=618429
    TEST=the test page from the bug.
    
    Review-Url: https://codereview.chromium.org/2047313004
    Cr-Commit-Position: refs/heads/master@{#398887}

--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -2070,7 +2070,8 @@ ScriptPromise HTMLMediaElement::playForBindings(ScriptState* scriptState)
         default:
             NOTREACHED();
         }
-        return ScriptPromise::rejectWithDOMException(scriptState, DOMException::create(code.get(), message));
+        resolver->reject(DOMException::create(code.get(), message));
+        return promise;
     }
 
     return promise;
