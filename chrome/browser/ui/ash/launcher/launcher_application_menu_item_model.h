1b22acf95fc9b36ad4084b8cb5f4f10e9cc72cb6
jamescook <jamescook@chromium.org>
2016-06-08 17:47:24 -0700

    mash: Move shelf menu code to //ash/common/shelf
    It no longer has non-common dependencies.
    
    I'll TBR since it's a purely mechanical file move.
    
    BUG=615155
    TEST=compiles
    TBR=sky@chromium.org
    
    Review-Url: https://codereview.chromium.org/2046563012
    Cr-Commit-Position: refs/heads/master@{#398750}

--- chrome/browser/ui/ash/launcher/launcher_application_menu_item_model.h
+++ chrome/browser/ui/ash/launcher/launcher_application_menu_item_model.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "ash/shelf/shelf_menu_model.h"
+#include "ash/common/shelf/shelf_menu_model.h"
 #include "base/macros.h"
 #include "base/memory/scoped_vector.h"
 
