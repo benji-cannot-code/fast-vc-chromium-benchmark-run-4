978bfc0bd42fe6087df39fad259691b7718c4d98
fdoray <fdoray@chromium.org>
2016-06-09 08:41:21 -0700

    Remove MessageLoop::current()->task_runner() in blimp.
    This CL replaces MessageLoop::current()->task_runner() with
    ThreadTaskRunnerHandle::Get() in blimp.
    
    In files where this replacement is made, it adds these include:
      #include "base/threading/thread_task_runner_handle.h"
      #include "base/location.h" (if required)
      #include "base/single_thread_task_runner.h" (if required)
    And removes this include if it is no longer required:
      #include "base/message_loop/message_loop.h"
    
    Why?
    The fact that there's a MessageLoop on the thread is an
    unnecessary implementation detail. When browser threads
    are migrated to base/task_scheduler, tasks will no longer
    have access to a MessageLoop but they will have access to
    a ThreadTaskRunnerHandle.
    
    Steps to generate this patch:
    1. Run message_loop_cleanup_2.py (see code on the bug).
    2. Run tools/sort-headers.py on modified files.
    3. Run git cl format.
    
    BUG=616447
    R=dtrainor@chromium.org
    
    Review-Url: https://codereview.chromium.org/2048213002
    Cr-Commit-Position: refs/heads/master@{#398892}

--- blimp/engine/common/blimp_browser_context_unittest.cc
+++ blimp/engine/common/blimp_browser_context_unittest.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "blimp/engine/common/blimp_browser_context.h"
 
-#include "base/message_loop/message_loop.h"
 #include "base/metrics/user_metrics.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "components/metrics/metrics_service.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "net/log/net_log.h"
@@ -21,8 +21,7 @@ class BlimpBrowserContextTest : public testing::Test {
 
  protected:
   void SetUp() override {
-    base::SetRecordActionTaskRunner(
-        base::MessageLoop::current()->task_runner());
+    base::SetRecordActionTaskRunner(base::ThreadTaskRunnerHandle::Get());
     // Create BlimpBrowserContext.
     browser_context_.reset(new BlimpBrowserContext(false /* off_the_record */,
                                                    &net_log_));
