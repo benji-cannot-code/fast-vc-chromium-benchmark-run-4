6379e7dced240fbd3a8b33e7269a2134d312646f
simonhatch <simonhatch@chromium.org>
2016-06-08 17:49:33 -0700

    Fix jetstream unreferenced tab variable.
    BUG=618513
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2051673003
    Cr-Commit-Position: refs/heads/master@{#398751}

--- tools/perf/benchmarks/jetstream.py
+++ tools/perf/benchmarks/jetstream.py
@@ -47,7 +47,7 @@ class _JetstreamMeasurement(legacy_page_test.LegacyPageTest):
         """
 
   def ValidateAndMeasurePage(self, page, tab, results):
-    del page, tab  # unused
+    del page  # unused
     get_results_js = """
         (function() {
           for (var i = 0; i < __results.length; i++) {
