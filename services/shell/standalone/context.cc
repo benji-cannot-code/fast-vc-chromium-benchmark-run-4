4ab2c8503c144daa9980305f01086d0ea7f041d9
fdoray <fdoray@chromium.org>
2016-06-09 09:39:45 -0700

    Remove MessageLoop::current()->task_runner() in services.
    This CL replaces MessageLoop::current()->task_runner() with
    ThreadTaskRunnerHandle::Get() in services.
    
    In files where this replacement is made, it adds these include:
      #include "base/threading/thread_task_runner_handle.h"
      #include "base/location.h" (if required)
      #include "base/single_thread_task_runner.h" (if required)
    And removes this include if it is no longer required:
      #include "base/message_loop/message_loop.h"
    
    Why?
    The fact that there's a MessageLoop on the thread is an
    unnecessary implementation detail. When browser threads
    are migrated to base/task_scheduler, tasks will no longer
    have access to a MessageLoop but they will have access to
    a ThreadTaskRunnerHandle.
    
    Steps to generate this patch:
    1. Run message_loop_cleanup_2.py (see code on the bug).
    2. Run tools/sort-headers.py on modified files.
    3. Run git cl format.
    
    BUG=616447
    R=jam@chromium.org
    
    Review-Url: https://codereview.chromium.org/2052653003
    Cr-Commit-Position: refs/heads/master@{#398913}

--- services/shell/standalone/context.cc
+++ services/shell/standalone/context.cc
@@ -15,15 +15,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/lazy_instance.h"
+#include "base/location.h"
 #include "base/macros.h"
 #include "base/path_service.h"
 #include "base/process/process_info.h"
 #include "base/run_loop.h"
+#include "base/single_thread_task_runner.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/sequenced_worker_pool.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "components/tracing/common/tracing_switches.h"
@@ -134,7 +137,7 @@ void Context::Init(std::unique_ptr<InitParams> init_params) {
   if (!init_params || init_params->init_edk)
     EnsureEmbedderIsInitialized();
 
-  shell_runner_ = base::MessageLoop::current()->task_runner();
+  shell_runner_ = base::ThreadTaskRunnerHandle::Get();
   blocking_pool_ =
       new base::SequencedWorkerPool(kMaxBlockingPoolThreads, "blocking_pool");
 
@@ -210,7 +213,7 @@ void Context::Shutdown() {
   // loop shutdown.
   shell_.reset();
 
-  DCHECK_EQ(base::MessageLoop::current()->task_runner(), shell_runner_);
+  DCHECK_EQ(base::ThreadTaskRunnerHandle::Get(), shell_runner_);
 
   // If we didn't initialize the edk we should not shut it down.
   if (!init_edk_)
@@ -225,7 +228,7 @@ void Context::Shutdown() {
 }
 
 void Context::OnShutdownComplete() {
-  DCHECK_EQ(base::MessageLoop::current()->task_runner(), shell_runner_);
+  DCHECK_EQ(base::ThreadTaskRunnerHandle::Get(), shell_runner_);
   base::MessageLoop::current()->QuitWhenIdle();
 }
 
