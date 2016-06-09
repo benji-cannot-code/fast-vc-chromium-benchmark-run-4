dffacb445570ed9b6c4db9cbcc65e5324ec45b70
jbroman <jbroman@chromium.org>
2016-06-09 13:53:25 -0700

    Attempt to deflake video-layer-crash.html.
    This makes the test wait for layout and paint, instead of finishing the test as
    soon as the seeked event is seen. Currently the Retina bot flakily produces the
    frame previous to the one that is expected.
    
    BUG=618693
    
    Review-Url: https://codereview.chromium.org/2053793002
    Cr-Commit-Position: refs/heads/master@{#399010}

--- third_party/WebKit/LayoutTests/media/video-paint-test.js
+++ third_party/WebKit/LayoutTests/media/video-paint-test.js
@@ -22,8 +22,11 @@ function init()
         }
 
         waitForMultipleEvents("seeked", videos.length, function() {
-            if (window.testRunner)
-                testRunner.notifyDone();
+            if (window.testRunner) {
+                testRunner.layoutAndPaintAsyncThen(function() {
+                    testRunner.notifyDone();
+                });
+            }
         });
     });
 }
