8609bc448e3f70c3719b44b6fc3fbe05755e8789
jamescook <jamescook@chromium.org>
2016-06-08 17:05:13 -0700

    ash: Remove broken OS update system tray "nagger" popup
    See bug for details. We used to show a "nag" popup if you had an update
    pending and your shelf had been hidden for > 24 hours. The feature has been
    broken for a while and after discussing with abodenha@ we've decided to
    remove it.
    
    This is a manual revert of http://crrev.com/134812
    
    BUG=122641
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2051643002
    Cr-Commit-Position: refs/heads/master@{#398740}

--- ash/system/tray_update.h
+++ ash/system/tray_update.h
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_SYSTEM_TRAY_UPDATE_H_
 #define ASH_SYSTEM_TRAY_UPDATE_H_
 
-#include <memory>
-
-#include "ash/system/tray/system_tray_delegate.h"
 #include "ash/system/tray/tray_image_item.h"
 #include "ash/system/user/update_observer.h"
 #include "base/macros.h"
@@ -18,10 +15,6 @@ class View;
 }
 
 namespace ash {
-
-namespace tray {
-class UpdateNagger;
-}
 
 class TrayUpdate : public TrayImageItem,
                    public UpdateObserver {
@@ -33,15 +26,9 @@ class TrayUpdate : public TrayImageItem,
   // Overridden from TrayImageItem.
   bool GetInitialVisibility() override;
   views::View* CreateDefaultView(LoginStatus status) override;
-  views::View* CreateDetailedView(LoginStatus status) override;
-  void DestroyDetailedView() override;
 
   // Overridden from UpdateObserver.
   void OnUpdateRecommended(const UpdateInfo& info) override;
-
-  // Used to nag the user in case the tray has been hidden too long with an
-  // unseen update notification.
-  std::unique_ptr<tray::UpdateNagger> nagger_;
 
   DISALLOW_COPY_AND_ASSIGN(TrayUpdate);
 };
