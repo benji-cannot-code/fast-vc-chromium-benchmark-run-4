a727d071f8d8a3fb23029daf69f750577f26b7c9
asvitkine <asvitkine@chromium.org>
2016-06-09 08:11:25 -0700

    Make Windows Chrome first run record default UMA checkbox state.
    This value is plumbed through the -statsdef:1 or -statsdef:0 tag in
    the "ap" param. This CL makes the value get picked up during
    Chrome first run on Windows.
    
    If not set in the "ap" tag, it means we don't know what the default
    state is, which is the default value in UMA and we do not need to
    set it.
    
    BUG=596185
    
    Review-Url: https://codereview.chromium.org/2041923002
    Cr-Commit-Position: refs/heads/master@{#398883}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -117,6 +117,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/google/core/browser/google_util.h"
 #include "components/language_usage_metrics/language_usage_metrics.h"
 #include "components/metrics/call_stack_profile_metrics_provider.h"
+#include "components/metrics/metrics_reporting_default_state.h"
 #include "components/metrics/metrics_service.h"
 #include "components/metrics/profiler/content/content_tracking_synchronizer_delegate.h"
 #include "components/metrics/profiler/tracking_synchronizer.h"
@@ -311,6 +312,13 @@ PrefService* InitializeLocalState(
     if (GoogleUpdateSettings::GetLanguage(&install_lang)) {
       local_state->SetString(prefs::kApplicationLocale,
                              base::UTF16ToASCII(install_lang));
+    }
+    bool stats_default;
+    if (GoogleUpdateSettings::GetCollectStatsConsentDefault(&stats_default)) {
+      // |stats_default| == true means that the default state of consent for the
+      // product at the time of install was to report usage statistics, meaning
+      // "opt-out".
+      metrics::RecordMetricsReportingDefaultOptIn(local_state, !stats_default);
     }
   }
 #endif  // defined(OS_WIN)
