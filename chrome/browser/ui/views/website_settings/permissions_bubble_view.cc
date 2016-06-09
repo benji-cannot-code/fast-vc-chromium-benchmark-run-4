a07a913ebee4e3a9371f9c5860f608c5897823e9
ellyjones <ellyjones@chromium.org>
2016-06-09 15:07:58 -0700

    PermissionSelectorView: use Combobox on MacViews builds.
    The Cocoa version of this dialog uses the cocoa equivalent of Comboboxes. Also,
    Comboboxes on MacViews have appropriate styling, whereas MenuButtons don't and
    changing them to have it seems quite involved.
    
    This CL involves adapting the permission selector's MenuModel to a
    ComboboxModel, then creating a Combobox instead of a MenuButton on Mac.
    
    BUG=605161
    
    Review-Url: https://codereview.chromium.org/2011963002
    Cr-Commit-Position: refs/heads/master@{#399034}

--- chrome/browser/ui/views/website_settings/permissions_bubble_view.cc
+++ chrome/browser/ui/views/website_settings/permissions_bubble_view.cc
@@ -133,6 +133,7 @@ void PermissionCombobox::OnMenuButtonClicked(views::MenuButton* source,
 
 void PermissionCombobox::PermissionChanged(
     const WebsiteSettingsUI::PermissionInfo& permission) {
+  LOG(ERROR) << "PermissionChanged!";
   SetText(model_->GetLabelAt(model_->GetIndexOfCommandId(permission.setting)));
   SizeToPreferredSize();
 
