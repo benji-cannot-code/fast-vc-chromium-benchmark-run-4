0a833d60d15723cf53379e8510b693f67e5aa3c1
srirama.m <srirama.m@samsung.com>
2016-06-09 05:05:46 -0700

    media: Remove logMedia from WTFLogChannel
    Removed logMedia from WTFLogChannel as all WTF_LOGs are replaced
    with DVLOGs in media modules. Few WTF_LOGs are still present in
    HTMLMediaElement.cpp and removed them as well in this CL.
    
    BUG=596522
    
    Review-Url: https://codereview.chromium.org/2046473002
    Cr-Commit-Position: refs/heads/master@{#398848}

--- third_party/WebKit/Source/platform/Logging.cpp
+++ third_party/WebKit/Source/platform/Logging.cpp
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-WTFLogChannel LogMedia =              { WTFLogChannelOff };
 WTFLogChannel LogNetwork =            { WTFLogChannelOff };
 WTFLogChannel LogResourceLoading =    { WTFLogChannelOff };
 WTFLogChannel LogSQLDatabase =        { WTFLogChannelOff };
@@ -43,9 +42,6 @@ WTFLogChannel* getChannelFromName(const String& channelName)
 {
     if (!(channelName.length() >= 2))
         return 0;
-
-    if (equalIgnoringCase(channelName, String("Media")))
-        return &LogMedia;
 
     if (equalIgnoringCase(channelName, String("Network")))
         return &LogNetwork;
