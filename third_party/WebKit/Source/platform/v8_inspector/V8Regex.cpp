c45075ea5fa040b249fd6cf6beeff2b92b2ef363
eostroukhov <eostroukhov@google.com>
2016-06-09 00:20:44 -0700

    Add missing include
    This include is required for INT_MAX constant used in this file.
    Currently this compiles without this statement because the header gets
    pulled through WTF dependencies.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2051653002
    Cr-Commit-Position: refs/heads/master@{#398803}

--- third_party/WebKit/Source/platform/v8_inspector/V8Regex.cpp
+++ third_party/WebKit/Source/platform/v8_inspector/V8Regex.cpp
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/v8_inspector/V8StringUtil.h"
 #include "platform/v8_inspector/public/V8DebuggerClient.h"
 
+#include <limits.h>
+
 namespace blink {
 
 V8Regex::V8Regex(V8DebuggerImpl* debugger, const String16& pattern, bool caseSensitive, bool multiline)
