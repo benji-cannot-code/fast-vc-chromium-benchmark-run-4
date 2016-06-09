dd7f81a9fb26204d409b235834a3ad2248629cd8
dougarnett <dougarnett@chromium.org>
2016-06-09 13:45:16 -0700

    For Offliner, pass in the URL from WebContents to SavePage request instead of the request URL since redirect link will fail a URL check in OfflinePageModelImpl. This is workaround until better understand how to deal with redirected URLs for async loading.
    BUG=618716
    
    Review-Url: https://codereview.chromium.org/2052913002
    Cr-Commit-Position: refs/heads/master@{#399001}

--- chrome/browser/android/offline_pages/prerendering_offliner.cc
+++ chrome/browser/android/offline_pages/prerendering_offliner.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/offline_pages/background/save_page_request.h"
 #include "components/offline_pages/offline_page_model.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/web_contents.h"
 
 namespace offline_pages {
 
@@ -53,7 +54,13 @@ void PrerenderingOffliner::OnLoadPageDone(
     DCHECK(web_contents);
     std::unique_ptr<OfflinePageArchiver> archiver(
         new OfflinePageMHTMLArchiver(web_contents));
-    SavePage(request.url(), request.client_id(), std::move(archiver),
+    // Pass in the URL from the WebContents in case it is redirected from
+    // the requested URL. This is to work around a check in the
+    // OfflinePageModel implementation that ensures URL passed in here is
+    // same as LastCommittedURL from the snapshot.
+    // TODO(dougarnett): Raise issue of how to better deal with redirects.
+    SavePage(web_contents->GetLastCommittedURL(), request.client_id(),
+             std::move(archiver),
              base::Bind(&PrerenderingOffliner::OnSavePageDone,
                         weak_ptr_factory_.GetWeakPtr(), request,
                         completion_callback));
