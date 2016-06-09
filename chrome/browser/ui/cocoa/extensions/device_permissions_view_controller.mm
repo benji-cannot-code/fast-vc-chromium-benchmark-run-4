e1f7e2abd81bf5c85d5e638052d1f093bb0cc7c2
juncai <juncai@chromium.org>
2016-06-09 11:40:45 -0700

    In DevicePermissionsViewController disable Connect button if no device selected on Mac
    When no device selected in the dialog, the "Connect" button needs to be
    disabled.
    
    BUG=613596
    
    Review-Url: https://codereview.chromium.org/2058433002
    Cr-Commit-Position: refs/heads/master@{#398958}

--- chrome/browser/ui/cocoa/extensions/device_permissions_view_controller.mm
+++ chrome/browser/ui/cocoa/extensions/device_permissions_view_controller.mm
@@ -47,6 +47,8 @@ using extensions::DevicePermissionsPrompt;
   [promptField_
       setStringValue:base::SysUTF16ToNSString(prompt_->GetPromptMessage())];
   [tableView_ setAllowsMultipleSelection:prompt_->multiple()];
+  [tableView_ setDelegate:self];
+  [tableView_ setDataSource:self];
   [[deviceNameColumn_ headerCell]
       setStringValue:l10n_util::GetNSString(
                          IDS_DEVICE_PERMISSIONS_DIALOG_DEVICE_NAME_COLUMN)];
@@ -55,6 +57,7 @@ using extensions::DevicePermissionsPrompt;
                          IDS_DEVICE_PERMISSIONS_DIALOG_SERIAL_NUMBER_COLUMN)];
   [okButton_
       setTitle:l10n_util::GetNSString(IDS_DEVICE_PERMISSIONS_DIALOG_SELECT)];
+  [okButton_ setEnabled:NO];
   [cancelButton_ setTitle:l10n_util::GetNSString(IDS_CANCEL)];
 }
 
@@ -74,6 +77,10 @@ using extensions::DevicePermissionsPrompt;
     NOTREACHED();
     return @"";
   }
+}
+
+- (void)tableViewSelectionDidChange:(NSNotification*)aNotification {
+  [okButton_ setEnabled:[tableView_ numberOfSelectedRows] > 0];
 }
 
 @end
