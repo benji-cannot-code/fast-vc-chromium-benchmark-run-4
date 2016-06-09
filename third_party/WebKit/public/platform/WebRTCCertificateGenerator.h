5f2e412524d5e0a8383e423682c56d55db5c3eff
hbos <hbos@chromium.org>
2016-06-09 06:12:21 -0700

    WebRTCCertificateGenerator dropping unused URL parameters.
    Non-functional clean-up. The generator previously needed "url" and
    "first party for cookies" because its implementation was using an identity store
    that had access to storage for caching. The implementation has since been
    replaced not to do this and the parameters are unused. So this CL removes them.
    
    BUG=webrtc:5708, webrtc:5707
    
    Review-Url: https://codereview.chromium.org/2030373002
    Cr-Commit-Position: refs/heads/master@{#398857}

--- third_party/WebKit/public/platform/WebRTCCertificateGenerator.h
+++ third_party/WebKit/public/platform/WebRTCCertificateGenerator.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/WebCallbacks.h"
 #include "public/platform/WebRTCCertificate.h"
 #include "public/platform/WebRTCKeyParams.h"
-#include "public/platform/WebURL.h"
 
 #include <memory>
 
@@ -52,17 +51,13 @@ public:
     // same thread that called generateCertificate when the operation is completed.
     virtual void generateCertificate(
         const WebRTCKeyParams&,
-        const WebURL&,
-        const WebURL& firstPartyForCookies,
         std::unique_ptr<WebRTCCertificateCallback> observer) = 0;
     virtual void generateCertificateWithExpiration(
         const WebRTCKeyParams&,
-        const WebURL&,
-        const WebURL& firstPartyForCookies,
         uint64_t expiresMs,
         std::unique_ptr<WebRTCCertificateCallback> observer) = 0;
 
-    // Determines if the parameters are supported by generateCertificate.
+    // Determines if the parameters are supported by |generateCertificate|.
     // For example, if the number of bits of some parameter is too small or too large we
     // may want to reject it for security or performance reasons.
     virtual bool isSupportedKeyParams(const WebRTCKeyParams&) = 0;
