fb05808a9f0a7d5c6412dc2b0efdf15b24bb8494
sigbjornf <sigbjornf@opera.com>
2016-06-10 07:51:50 -0700

    Drop unecessary use of CrossThreadPersistent by CanvasAsyncBlobCreator.
    The callback object that the CanvasAsyncBlobCreator passes along to a
    background thread can be kept as a simple Member<>; no need to
    involve CrossThreadPersistent<> and risk inadvertently introducing
    leaks.
    
    R=
    BUG=
    
    Review-Url: https://codereview.chromium.org/2051993002
    Cr-Commit-Position: refs/heads/master@{#399181}

--- third_party/WebKit/Source/core/html/canvas/CanvasAsyncBlobCreator.h
+++ third_party/WebKit/Source/core/html/canvas/CanvasAsyncBlobCreator.h
@@ -43,6 +43,7 @@ public:
     DEFINE_INLINE_VIRTUAL_TRACE()
     {
         visitor->trace(m_data);
+        visitor->trace(m_callback);
     }
 
 protected:
@@ -73,7 +74,7 @@ private:
     const IntSize m_size;
     size_t m_pixelRowStride;
     const MimeType m_mimeType;
-    CrossThreadPersistent<BlobCallback> m_callback;
+    Member<BlobCallback> m_callback;
 
     // PNG
     bool initializePngStruct();
