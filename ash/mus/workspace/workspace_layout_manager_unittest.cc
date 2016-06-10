8a5a4df58c7c7bb55d84c77fe557d9e1a28162de
fdoray <fdoray@chromium.org>
2016-06-10 08:53:00 -0700

    Remove MessageLoop::current()->RunUntilIdle() in ash.
    This CL replaces MessageLoop::current()->RunUntilIdle() with
    RunLoop().RunUntilIdle() in ash.
    
    In files where this replacement is made, it adds this include:
      #include "base/run_loop.h"
    And removes this include if it is no longer required:
      #include "base/message_loop/message_loop.h"
    
    Why?
    - The fact that there's a MessageLoop on the thread is an
      unnecessary implementation detail. When browser threads
      are migrated to base/task_scheduler, tasks will no longer
      have access to a MessageLoop but they will be able to use
      RunLoop.
    - MessageLoop::RunUntilIdle() is deprecated.
    
    Steps to generate this patch:
    1. Run message_loop_cleanup_3.py (see code on the bug).
    2. Run tools/sort-headers.py on modified files.
    3. Run git cl format.
    
    BUG=616447
    R=derat@chromium.org
    
    Review-Url: https://codereview.chromium.org/2051283002
    Cr-Commit-Position: refs/heads/master@{#399195}

--- ash/mus/workspace/workspace_layout_manager_unittest.cc
+++ ash/mus/workspace/workspace_layout_manager_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/mus/bridge/wm_root_window_controller_mus.h"
 #include "ash/mus/bridge/wm_window_mus.h"
 #include "ash/mus/test/wm_test_base.h"
+#include "base/run_loop.h"
 #include "components/mus/public/cpp/tests/test_window.h"
 #include "ui/display/display.h"
 
@@ -798,7 +799,7 @@ class WorkspaceLayoutManagerBackdropTest : public test::AshTestBase {
     GetWorkspaceLayoutManager(default_container_)
         ->SetMaximizeBackdropDelegate(std::move(backdrop));
     // Closing and / or opening can be a delayed operation.
-    base::MessageLoop::current()->RunUntilIdle();
+    base::RunLoop().RunUntilIdle();
   }
 
   // Return the default container.
--- ash/wm/workspace/workspace_layout_manager_unittest.cc
+++ ash/wm/workspace/workspace_layout_manager_unittest.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_state_aura.h"
 #include "ash/wm/window_util.h"
 #include "base/compiler_specific.h"
+#include "base/run_loop.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/test/test_windows.h"
 #include "ui/aura/window.h"
@@ -856,7 +857,7 @@ class WorkspaceLayoutManagerBackdropTest : public test::AshTestBase {
     GetWorkspaceLayoutManager(default_container_)
         ->SetMaximizeBackdropDelegate(std::move(backdrop));
     // Closing and / or opening can be a delayed operation.
-    base::MessageLoop::current()->RunUntilIdle();
+    base::RunLoop().RunUntilIdle();
   }
 
   // Return the default container.
