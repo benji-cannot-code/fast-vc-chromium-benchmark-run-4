bea0fc90e8d9c350309dc29d5ab35fb14f7f0115
sigbjornf <sigbjornf@opera.com>
2016-06-09 23:46:34 -0700

    Remove unnecessary use of CrossThreadPersistent<>.
    The CompositorMutatorImpl object that WebFrameWidgetImpl creates and
    controls the lifetime of, should be referenced as a normal Member<>
    
    R=
    BUG=
    
    Review-Url: https://codereview.chromium.org/2056833003
    Cr-Commit-Position: refs/heads/master@{#399125}

--- third_party/WebKit/Source/web/WebFrameWidgetImpl.cpp
+++ third_party/WebKit/Source/web/WebFrameWidgetImpl.cpp
@@ -122,6 +122,7 @@ DEFINE_TRACE(WebFrameWidgetImpl)
 {
     visitor->trace(m_localRoot);
     visitor->trace(m_mouseCaptureNode);
+    visitor->trace(m_mutator);
 }
 
 // WebWidget ------------------------------------------------------------------
