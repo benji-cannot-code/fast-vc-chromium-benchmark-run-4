33beeef9ae461ea4e748e987b048bc8f8c7215ff
alokp <alokp@chromium.org>
2016-06-09 21:29:45 -0700

    [chromecast] Moves fake key handling to DecoderBufferAdpter.
    The logic is being moved from DecoderBufferBaseMarshaller, IPC
    transport layer to DecoderBufferAdapter so that it is shared between
    the cma and mojo pipleine.
    
    BUG=571155
    TBR=yucliu1
    
    Review-Url: https://codereview.chromium.org/2054003002
    Cr-Commit-Position: refs/heads/master@{#399089}

--- chromecast/media/cma/base/cast_decrypt_config_impl.h
+++ chromecast/media/cma/base/cast_decrypt_config_impl.h
@@ -8,20 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/public/media/cast_decrypt_config.h"
 
-namespace media {
-class DecryptConfig;
-}
-
 namespace chromecast {
 namespace media {
 
 // Contains all information that a decryptor needs to decrypt a media sample.
 class CastDecryptConfigImpl : public CastDecryptConfig {
  public:
-  CastDecryptConfigImpl(const ::media::DecryptConfig& config);
-  CastDecryptConfigImpl(const std::string& key_id,
-                        const std::string& iv,
-                        const std::vector<SubsampleEntry>& subsamples);
+  CastDecryptConfigImpl(std::string key_id,
+                        std::string iv,
+                        std::vector<SubsampleEntry> subsamples);
   ~CastDecryptConfigImpl() override;
 
   const std::string& key_id() const override;
