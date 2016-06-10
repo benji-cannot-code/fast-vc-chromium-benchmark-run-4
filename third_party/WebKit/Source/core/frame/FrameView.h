3c092bb8c8c29d110dd9b03a0a911cb8c3453a4b
wangxianzhu <wangxianzhu@chromium.org>
2016-06-10 16:52:27 -0700

    More detailed checking of FrameView::needsLayout()
    BUG=590856
    
    Review-Url: https://codereview.chromium.org/2052263004
    Cr-Commit-Position: refs/heads/master@{#399325}

--- third_party/WebKit/Source/core/frame/FrameView.h
+++ third_party/WebKit/Source/core/frame/FrameView.h
@@ -139,6 +139,7 @@ public:
     void countObjectsNeedingLayout(unsigned& needsLayoutObjects, unsigned& totalObjects, bool& isPartial);
 
     bool needsLayout() const;
+    void checkDoesNotNeedLayout() const;
     void setNeedsLayout();
 
     void setNeedsUpdateWidgetGeometries() { m_needsUpdateWidgetGeometries = true; }
