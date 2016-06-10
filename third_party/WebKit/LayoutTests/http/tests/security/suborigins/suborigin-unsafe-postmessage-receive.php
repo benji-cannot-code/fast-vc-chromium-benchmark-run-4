a245b66b258f719a5c7fcfb14e5e7586ebd52274
jww <jww@chromium.org>
2016-06-09 18:00:10 -0700

    Update Suborigins grammar to match spec changes
    The spec took away semicolons from the grammar because they were
    unnecessary. This CL updates the implementation and tests accordingly.
    
    BUG=336894
    TBR=mkwst@chromium.org
    
    Review-Url: https://codereview.chromium.org/2052033002
    Cr-Commit-Position: refs/heads/master@{#399061}

--- third_party/WebKit/LayoutTests/http/tests/security/suborigins/suborigin-unsafe-postmessage-receive.php
+++ third_party/WebKit/LayoutTests/http/tests/security/suborigins/suborigin-unsafe-postmessage-receive.php
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-header("Suborigin: foobar 'unsafe-postmessage-receive';");
+header("Suborigin: foobar 'unsafe-postmessage-receive'");
 ?>
 <!DOCTYPE html>
 <html>
