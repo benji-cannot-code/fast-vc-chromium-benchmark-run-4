30df1afd95782335c5d392939a26c15ae4b0bad4
hta <hta@chromium.org>
2016-06-10 04:45:04 -0700

    Make ready to unprefix getUserMedia, and counters
    This CL adds an unprefixed getUserMedia on navigator,
    behind the same flag as the promise-based getUserMedia.
    It also counts all three variants.
    
    BUG=607439
    
    Review-Url: https://codereview.chromium.org/2045193003
    Cr-Commit-Position: refs/heads/master@{#399156}

--- third_party/WebKit/Source/modules/mediastream/NavigatorMediaStream.cpp
+++ third_party/WebKit/Source/modules/mediastream/NavigatorMediaStream.cpp
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void NavigatorMediaStream::webkitGetUserMedia(Navigator& navigator, const MediaStreamConstraints& options, NavigatorUserMediaSuccessCallback* successCallback, NavigatorUserMediaErrorCallback* errorCallback, ExceptionState& exceptionState)
+void NavigatorMediaStream::getUserMedia(Navigator& navigator, const MediaStreamConstraints& options, NavigatorUserMediaSuccessCallback* successCallback, NavigatorUserMediaErrorCallback* errorCallback, ExceptionState& exceptionState)
 {
     if (!successCallback)
         return;
