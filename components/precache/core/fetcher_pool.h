a670f7bc694db7025c08388e36762e5b17407c23
rajendrant <rajendrant@chromium.org>
2016-06-11 21:44:27 -0700

    Precache manifest should not be added to fetcher pool which is full
    Fetcher pool accepts limited number of requests which are fetched in
    parallel. If the pool is full, precache manifest resources should not be
    added to it.
    
    BUG=606231
    
    Review-Url: https://codereview.chromium.org/2037903002
    Cr-Commit-Position: refs/heads/master@{#399387}

--- components/precache/core/fetcher_pool.h
+++ components/precache/core/fetcher_pool.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <unordered_map>
 
+#include "base/gtest_prod_util.h"
 #include "base/logging.h"
 
 namespace precache {
@@ -71,6 +72,9 @@ class FetcherPool {
   const std::unordered_map<const T*, std::unique_ptr<T>>& elements() const {
     return elements_;
   }
+
+  // Returns the maximum size of the pool.
+  size_t max_size() const { return max_size_; }
 
  private:
   const size_t max_size_;
