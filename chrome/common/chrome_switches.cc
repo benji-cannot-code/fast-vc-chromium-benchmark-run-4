5a22199d26146b0e856bcbb5f18b98ee9e83e190
ccameron <ccameron@chromium.org>
2016-06-12 11:33:28 -0700

    Mac video: Enable fullscreen low power mode
    BUG=594460
    
    Review-Url: https://codereview.chromium.org/2059863004
    Cr-Commit-Position: refs/heads/master@{#399396}

--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -1141,6 +1141,10 @@ const char kDisableAppInfoDialogMac[] = "disable-app-info-dialog-mac";
 // Disables custom Cmd+` window cycling for platform apps and hosted apps.
 const char kDisableAppWindowCycling[] = "disable-app-window-cycling";
 
+// Disables fullscreen low power mode on Mac.
+const char kDisableFullscreenLowPowerMode[] =
+    "disable-fullscreen-low-power-mode";
+
 // Disables tab detaching in fullscreen mode on Mac.
 const char kDisableFullscreenTabDetaching[] =
     "disable-fullscreen-tab-detaching";
@@ -1164,9 +1168,6 @@ const char kEnableAppInfoDialogMac[] = "enable-app-info-dialog-mac";
 
 // Enables custom Cmd+` window cycling for platform apps and hosted apps.
 const char kEnableAppWindowCycling[] = "enable-app-window-cycling";
-
-// Enables fullscreen low power mode transitions on Mac.
-const char kEnableFullscreenLowPowerMode[] = "enable-fullscreen-low-power-mode";
 
 // Enables tab detaching in fullscreen mode on Mac.
 const char kEnableFullscreenTabDetaching[] = "enable-fullscreen-tab-detaching";
