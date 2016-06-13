af6cbcb8b1a31d1a9d2ea7d480a7d3554527f8c4
takaoka <takaoka@chromium.org>
2016-06-13 01:18:52 -0700

    Add storage_manager.DeleteApplicationsCache RPC
    BUG=29306776
    
    Review-Url: https://codereview.chromium.org/2058373002
    Cr-Commit-Position: refs/heads/master@{#399415}

--- components/arc/storage_manager/arc_storage_manager.h
+++ components/arc/storage_manager/arc_storage_manager.h
@@ -40,6 +40,9 @@ class ArcStorageManager : public ArcService,
       void(bool succeeded, arc::mojom::ApplicationsSizePtr)>;
   bool GetApplicationsSize(const GetApplicationsSizeCallback& callback);
 
+  // Deletes all applications' cache files.
+  bool DeleteApplicationsCache(const base::Callback<void()>& callback);
+
  private:
   mojom::StorageManagerInstance* GetStorageManagerInstance();
 
