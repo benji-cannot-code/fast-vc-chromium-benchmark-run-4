e2dea73214b0c14395f0a52771601ba82daf2667
mmenke <mmenke@chromium.org>
2016-06-13 12:04:57 -0700

    Fix generation of inner_url in GURL::Replacements.
    It was being created using a combination of the old and new URLs,
    instead of just using the new URL.
    
    BUG=615820
    
    Review-Url: https://codereview.chromium.org/2029213002
    Cr-Commit-Position: refs/heads/master@{#399501}

--- url/gurl.cc
+++ url/gurl.cc
@@ -279,7 +279,8 @@ GURL GURL::ReplaceComponents(
 
   output.Complete();
   if (result.is_valid_ && result.SchemeIsFileSystem()) {
-    result.inner_url_.reset(new GURL(spec_.data(), result.parsed_.Length(),
+    result.inner_url_.reset(new GURL(result.spec_.data(),
+                                     result.parsed_.Length(),
                                      *result.parsed_.inner_parsed(), true));
   }
   return result;
@@ -305,7 +306,8 @@ GURL GURL::ReplaceComponents(
 
   output.Complete();
   if (result.is_valid_ && result.SchemeIsFileSystem()) {
-    result.inner_url_.reset(new GURL(spec_.data(), result.parsed_.Length(),
+    result.inner_url_.reset(new GURL(result.spec_.data(),
+                                     result.parsed_.Length(),
                                      *result.parsed_.inner_parsed(), true));
   }
   return result;
