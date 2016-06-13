d7a51354d41aec0d45b6472ea6556df2a18650aa
gogerald <gogerald@chromium.org>
2016-06-13 08:25:13 -0700

    Record FRE events only once during FRE and remove unnecessary interface
    Record events in onStart() will cause the events been recorded multiple times.
    
    BUG=618834
    
    Review-Url: https://codereview.chromium.org/2047353005
    Cr-Commit-Position: refs/heads/master@{#399451}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
@@ -18,7 +18,6 @@ import org.chromium.base.Log;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.library_loader.ProcessInitException;
 import org.chromium.base.metrics.RecordHistogram;
-import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeApplication;
 import org.chromium.chrome.browser.ChromeVersionInfo;
@@ -29,8 +28,6 @@ import org.chromium.chrome.browser.net.spdyproxy.DataReductionProxySettings;
 import org.chromium.chrome.browser.preferences.datareduction.DataReductionPromoScreen;
 import org.chromium.chrome.browser.preferences.datareduction.DataReductionProxyUma;
 import org.chromium.chrome.browser.profiles.Profile;
-import org.chromium.chrome.browser.signin.SigninAccessPoint;
-import org.chromium.chrome.browser.signin.SigninManager;
 
 import java.lang.ref.WeakReference;
 import java.lang.reflect.Constructor;
@@ -320,13 +317,6 @@ public class FirstRunActivity extends AppCompatActivity implements FirstRunPageD
         Intent resultData = new Intent();
         resultData.putExtras(mFreProperties);
         finishAllFREActivities(Activity.RESULT_OK, resultData);
-    }
-
-    @Override
-    public void onSigninDialogShown() {
-        RecordUserAction.record("MobileFre.SignInShown");
-        RecordUserAction.record("Signin_Signin_FromStartPage");
-        SigninManager.logSigninStartAccessPoint(SigninAccessPoint.START_PAGE);
     }
 
     @Override
