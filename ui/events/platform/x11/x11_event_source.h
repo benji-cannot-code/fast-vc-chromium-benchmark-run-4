a740f34e2f2a77b352db643508a3687b83fdb159
thomasanderson <thomasanderson@google.com>
2016-06-13 11:11:36 -0700

    X11: Wait for UnmapNotify before trying to remap
    A speculative fix for a suspected race condition in which we wait for a
    MapNotify that will never come.
    
    BUG=381732
    
    Review-Url: https://codereview.chromium.org/2057333002
    Cr-Commit-Position: refs/heads/master@{#399484}

--- ui/events/platform/x11/x11_event_source.h
+++ ui/events/platform/x11/x11_event_source.h
@@ -63,6 +63,8 @@ class EVENTS_EXPORT X11EventSource {
   // functions which require a mapped window.
   void BlockUntilWindowMapped(XID window);
 
+  void BlockUntilWindowUnmapped(XID window);
+
   XDisplay* display() { return display_; }
   Time last_seen_server_time() const { return last_seen_server_time_; }
 
@@ -81,6 +83,10 @@ class EVENTS_EXPORT X11EventSource {
 
   // Handles updates after event has been dispatched.
   void PostDispatchEvent(XEvent* xevent);
+
+  // Block until receiving a structure notify event of |type| on |window|.
+  // Dispatch all encountered events prior to the one we're blocking on.
+  void BlockOnWindowStructureEvent(XID window, int type);
 
  private:
   static X11EventSource* instance_;
