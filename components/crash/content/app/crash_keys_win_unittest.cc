0209e7253a5d860affe86a7c8aab3bfdac13451f
thestig <thestig@chromium.org>
2016-06-13 11:35:53 -0700

    Cleanup: Remove static_casts of NULL in components/
    Review-Url: https://codereview.chromium.org/2044323004
    Cr-Commit-Position: refs/heads/master@{#399489}

--- components/crash/content/app/crash_keys_win_unittest.cc
+++ components/crash/content/app/crash_keys_win_unittest.cc
@@ -56,7 +56,6 @@ class MockCrashReporterClient : public crash_reporter::CrashReporterClient {
 
 class CrashKeysWinTest : public testing::Test {
  public:
-
   size_t CountKeyValueOccurences(
       const google_breakpad::CustomClientInfo* client_info,
       const wchar_t* key, const wchar_t* value);
@@ -80,7 +79,7 @@ size_t CrashKeysWinTest::CountKeyValueOccurences(
 }
 
 TEST_F(CrashKeysWinTest, RecordsSelf) {
-  ASSERT_EQ(static_cast<CrashKeysWin*>(NULL), CrashKeysWin::keeper());
+  ASSERT_FALSE(CrashKeysWin::keeper());
 
   {
     CrashKeysWin crash_keys;
@@ -88,7 +87,7 @@ TEST_F(CrashKeysWinTest, RecordsSelf) {
     ASSERT_EQ(&crash_keys, CrashKeysWin::keeper());
   }
 
-  ASSERT_EQ(static_cast<CrashKeysWin*>(NULL), CrashKeysWin::keeper());
+  ASSERT_FALSE(CrashKeysWin::keeper());
 }
 
 // Tests the crash keys set up for the most common official build consumer
