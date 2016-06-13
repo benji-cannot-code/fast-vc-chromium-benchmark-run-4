bf5afc7cd2ed84d03613f7ce839f4afbc5791010
mdjones <mdjones@chromium.org>
2016-06-13 12:00:51 -0700

    Use theme color when tab is restored
    BUG=606612
    
    Review-Url: https://codereview.chromium.org/2024333003
    Cr-Commit-Position: refs/heads/master@{#399499}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
@@ -563,12 +563,12 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
                     PolicyAuditor.nativeGetCertificateFailure(getWebContents()),
                     getApplicationContext());
             updateFullscreenEnabledState();
-            updateThemeColorIfNeeded();
+            updateThemeColorIfNeeded(false);
         }
 
         @Override
         public void onUrlUpdated(Tab tab) {
-            updateThemeColorIfNeeded();
+            updateThemeColorIfNeeded(false);
         }
 
         @Override
@@ -639,7 +639,7 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
             mDefaultThemeColor = mIncognito
                     ? ApiCompatibilityUtils.getColor(resources, R.color.incognito_primary_color)
                     : ApiCompatibilityUtils.getColor(resources, R.color.default_primary_color);
-            mThemeColor = calculateThemeColor();
+            mThemeColor = calculateThemeColor(false);
         } else {
             mIdealFaviconSize = 16;
             mDefaultThemeColor = 0;
@@ -707,7 +707,7 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
         mTimestampMillis = state.timestampMillis;
         mUrl = state.getVirtualUrlFromState();
 
-        mThemeColor = state.hasThemeColor() ? state.getThemeColor() : mDefaultThemeColor;
+        mThemeColor = state.hasThemeColor() ? state.getThemeColor() : getDefaultThemeColor();
 
         mTitle = state.getDisplayTitleFromState();
         mIsTitleDirectionRtl = mTitle != null
@@ -1068,14 +1068,27 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
         return mThemeColor;
     }
 
-    private int calculateThemeColor() {
+    /**
+     * Calculate the theme color based on if the page is native, the theme color changed, etc.
+     * @param didWebContentsThemeColorChange If the theme color of the web contents is known to have
+     *                                       changed.
+     * @return The theme color that should be used for this tab.
+     */
+    private int calculateThemeColor(boolean didWebContentsThemeColorChange) {
         // Theme color support is currently disabled for tablets.
         if (DeviceFormFactor.isTablet(getApplicationContext())) return getDefaultThemeColor();
 
         if (isNativePage()) return mNativePage.getThemeColor();
 
-        int themeColor = getDefaultThemeColor();
-        if (getWebContents() != null) themeColor = getWebContents().getThemeColor();
+        // Start by assuming the current theme color is that one that should be used. This will
+        // either be transparent, the last theme color, or the color restored from TabState.
+        int themeColor = mThemeColor;
+
+        // Only use the web contents for the theme color if it is known to have changed, This
+        // corresponds to the didChangeThemeColor in WebContentsObserver.
+        if (getWebContents() != null && didWebContentsThemeColorChange) {
+            themeColor = getWebContents().getThemeColor();
+        }
 
         // Do not apply the theme color if there are any security issues on the page.
         int securityLevel = getSecurityLevel();
@@ -1098,9 +1111,11 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
 
     /**
      * Determines if the theme color has changed and notifies the listeners if it has.
+     * @param didWebContentsThemeColorChange If the theme color of the web contents is known to have
+     *                                       changed.
      */
-    void updateThemeColorIfNeeded() {
-        int themeColor = calculateThemeColor();
+    void updateThemeColorIfNeeded(boolean didWebContentsThemeColorChange) {
+        int themeColor = calculateThemeColor(didWebContentsThemeColorChange);
         if (themeColor == mThemeColor) return;
         mThemeColor = themeColor;
         RewindableIterator<TabObserver> observers = getTabObservers();
@@ -1368,7 +1383,7 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
         // Notifying of theme color change before content change because some of
         // the observers depend on the theme information being correct in
         // onContentChanged().
-        updateThemeColorIfNeeded();
+        updateThemeColorIfNeeded(false);
         notifyContentChanged();
         destroyNativePageInternal(previousNativePage);
     }
@@ -1851,7 +1866,7 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
             mSwipeRefreshHandler = new SwipeRefreshHandler(mThemedApplicationContext);
             mSwipeRefreshHandler.setContentViewCore(mContentViewCore);
 
-            updateThemeColorIfNeeded();
+            updateThemeColorIfNeeded(false);
             notifyContentChanged();
 
             // For browser tabs, we want to set accessibility focus to the page
@@ -3116,7 +3131,8 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
     /**
      * @return The default theme color for this tab.
      */
-    int getDefaultThemeColor() {
+    @VisibleForTesting
+    public int getDefaultThemeColor() {
         return mDefaultThemeColor;
     }
 
--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsObserver.java
@@ -262,14 +262,14 @@ public class TabWebContentsObserver extends WebContentsObserver {
 
     @Override
     public void didChangeThemeColor(int color) {
-        mTab.updateThemeColorIfNeeded();
+        mTab.updateThemeColorIfNeeded(true);
     }
 
     @Override
     public void didAttachInterstitialPage() {
         mTab.getInfoBarContainer().setVisibility(View.INVISIBLE);
         mTab.showRenderedPage();
-        mTab.updateThemeColorIfNeeded();
+        mTab.updateThemeColorIfNeeded(false);
 
         RewindableIterator<TabObserver> observers = mTab.getTabObservers();
         while (observers.hasNext()) {
@@ -289,7 +289,7 @@ public class TabWebContentsObserver extends WebContentsObserver {
     @Override
     public void didDetachInterstitialPage() {
         mTab.getInfoBarContainer().setVisibility(View.VISIBLE);
-        mTab.updateThemeColorIfNeeded();
+        mTab.updateThemeColorIfNeeded(false);
 
         RewindableIterator<TabObserver> observers = mTab.getTabObservers();
         while (observers.hasNext()) {
