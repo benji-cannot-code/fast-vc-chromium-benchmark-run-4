0694d628c39da64427aad9cb2ee69807ff2d3c97
hidehiko <hidehiko@chromium.org>
2016-06-12 19:30:26 -0700

    exo: Implement version 3 of remote shell interface.
    Adds basic support for pin, as well as allowing the
    client to request that window is pinned.
    
    BUG=b/27995648
    TEST=exo_unittests
    
    Review-Url: https://codereview.chromium.org/2059943002
    Cr-Commit-Position: refs/heads/master@{#399403}

--- components/exo/shell_surface.h
+++ components/exo/shell_surface.h
@@ -109,6 +109,9 @@ class ShellSurface : public SurfaceDelegate,
   // Set fullscreen state for shell surface.
   void SetFullscreen(bool fullscreen);
 
+  // Pins the shell surface.
+  void SetPinned(bool pinned);
+
   // Set title for surface.
   void SetTitle(const base::string16& title);
 
