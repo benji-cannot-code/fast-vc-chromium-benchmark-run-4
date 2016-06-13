a4fd712e891373de4f7dd1e6ec926add836c6e30
varkha <varkha@chromium.org>
2016-06-13 15:24:06 -0700

    [ash-md] Makes overview window captions translucent
    Changes to update overview mode to MD spec.
    Captions are less dark.
    Close buttons are now part of a single caption widget.
    Close buttons use vector icons.
    Selector widget now blends with the caption label
    Label text is regular font
    Shield is darker
    Shelf is no longer put in overlap mode (stays transparent) in overview.
    
    BUG=604767
    
    Review-Url: https://codereview.chromium.org/2061533002
    Cr-Commit-Position: refs/heads/master@{#399576}

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accelerators/accelerator_commands.h"
 #include "ash/ash_switches.h"
 #include "ash/aura/wm_window_aura.h"
-#include "ash/common/material_design/material_design_controller.h"
 #include "ash/common/session/session_state_delegate.h"
 #include "ash/common/shelf/shelf_constants.h"
 #include "ash/common/shelf/wm_shelf_util.h"
@@ -562,16 +561,6 @@ void ShelfLayoutManager::OnShelfAutoHideBehaviorChanged(WmWindow* root_window) {
   UpdateVisibilityState();
 }
 
-void ShelfLayoutManager::OnOverviewModeStarting() {
-  state_.is_overview_mode = true;
-  UpdateShelfBackground(BACKGROUND_CHANGE_ANIMATE);
-}
-
-void ShelfLayoutManager::OnOverviewModeEnded() {
-  state_.is_overview_mode = false;
-  UpdateShelfBackground(BACKGROUND_CHANGE_ANIMATE);
-}
-
 void ShelfLayoutManager::OnWindowActivated(
     aura::client::ActivationChangeObserver::ActivationReason reason,
     aura::Window* gained_active,
@@ -609,7 +598,6 @@ void ShelfLayoutManager::SetState(ShelfVisibilityState visibility_state) {
   // Preserve the log in screen states.
   state.is_adding_user_screen = state_.is_adding_user_screen;
   state.is_screen_locked = state_.is_screen_locked;
-  state.is_overview_mode = state_.is_overview_mode;
 
   // Force an update because gesture drags affect the shelf bounds and we
   // should animate back to the normal bounds at the end of a gesture.
@@ -982,9 +970,7 @@ ShelfBackgroundType ShelfLayoutManager::GetShelfBackgroundType() const {
   if (gesture_drag_status_ == GESTURE_DRAG_IN_PROGRESS ||
       (!state_.is_screen_locked && !state_.is_adding_user_screen &&
        window_overlaps_shelf_) ||
-      (state_.visibility_state == SHELF_AUTO_HIDE) ||
-      (ash::MaterialDesignController::IsOverviewMaterial() &&
-       state_.is_overview_mode)) {
+      (state_.visibility_state == SHELF_AUTO_HIDE)) {
     return SHELF_BACKGROUND_OVERLAP;
   }
 
