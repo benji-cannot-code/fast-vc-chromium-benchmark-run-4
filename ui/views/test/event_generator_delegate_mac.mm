b0ef704e421b4abbdd25f70e2fd5e28f97e8f95f
tapted <tapted@chromium.org>
2016-06-12 23:29:02 -0700

    Support a ui::test::EventGenerator constructed with a single argument on Mac
    EventGeneratorDelegateMac::SetContext(..) ignores |root_window|. If that
    was all that's provided, assume it's the active window.
    
    This fixes all the Mac views_unittests regressions from r398705.
    
    BUG=618880
    
    Review-Url: https://codereview.chromium.org/2053253002
    Cr-Commit-Position: refs/heads/master@{#399409}

--- ui/views/test/event_generator_delegate_mac.mm
+++ ui/views/test/event_generator_delegate_mac.mm
@@ -383,6 +383,11 @@ void EventGeneratorDelegateMac::OnTouchEvent(ui::TouchEvent* event) {
 void EventGeneratorDelegateMac::SetContext(ui::test::EventGenerator* owner,
                                            gfx::NativeWindow root_window,
                                            gfx::NativeWindow window) {
+  // Mac doesn't use a |root_window|. Assume that if a single-argument
+  // constructor was used, it should be the actual |window|.
+  if (!window)
+    window = root_window;
+
   swizzle_pressed_.reset();
   swizzle_location_.reset();
   swizzle_current_event_.reset();
