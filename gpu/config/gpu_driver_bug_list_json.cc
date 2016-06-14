6d220a474524c607ba1dfe58dfd157acfd47ee87
j.isorce <j.isorce@samsung.com>
2016-06-14 03:21:39 -0700

    Do not apply NVIDIA driver workarounds to nouveau
    For each modifed entry I verified that the initial bug
    was reported against NVIDIA driver.
    
    Also I have tested the CL with nouveau driver.
    
    I have not changed entry 5 that adds workaround
    'use_current_program_after_successful_link' because it seems to
    be valid on old mesa drivers so it would require more investigation.
    
    I have not changed entry 54 that add workaround
    'clear_uniforms_before_first_program_use' because it seems to
    be general policy applied to all gpu except on some osx.
    
    BUG=505969
    
    R=kbr@chromium.org, piman@chromium.org, zmo@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2052243002
    Cr-Commit-Position: refs/heads/master@{#399676}

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -20,7 +20,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "8.69",
+  "version": "8.70",
   "entries": [
     {
       "id": 1,
@@ -312,6 +312,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
         "type": "linux"
       },
       "vendor_id": "0x10de",
+      "gl_vendor": "NVIDIA.*",
       "features": [
         "init_gl_position_in_vertex_shader"
       ]
@@ -539,6 +540,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
         "type": "linux"
       },
       "vendor_id": "0x10de",
+      "gl_vendor": "NVIDIA.*",
       "features": [
         "init_vertex_attributes"
       ]
@@ -1034,11 +1036,13 @@ LONG_STRING_CONST(
     },
     {
       "id": 109,
+      "cr_bugs": [449150, 514510],
       "description": "MakeCurrent is slow on Linux with NVIDIA drivers",
       "vendor_id": "0x10de",
       "os": {
         "type": "linux"
       },
+      "gl_vendor": "NVIDIA.*",
       "features": [
         "use_virtualized_gl_contexts"
       ]
@@ -1160,6 +1164,7 @@ LONG_STRING_CONST(
         "value": "331"
       },
       "vendor_id": "0x10de",
+      "gl_vendor": "NVIDIA.*",
       "features": [
         "remove_pow_with_constant_exponent"
       ]
@@ -1293,6 +1298,7 @@ LONG_STRING_CONST(
       "os": {
         "type": "linux"
       },
+      "gl_vendor": "NVIDIA.*",
       "features": [
         "force_cube_complete"
       ]
