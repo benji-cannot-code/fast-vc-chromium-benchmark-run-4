a5065f22e2463fadbed0da8f6841e38869614161
hcarmona <hcarmona@chromium.org>
2016-06-14 10:53:56 -0700

    Revert of Adding mac support for Remoting host GN builds. (patchset #6 id:100001 of https://codereview.chromium.org/2034513002/ )
    Reason for revert:
    Suspecting this CL for compile failure in Mac GN:
    
    https://build.chromium.org/p/chromium.mac/builders/Mac%20GN%20%28dbg%29/builds/34015
    
    Undefined symbols for architecture x86_64:
      "_GetApplicationEventTarget", referenced from:
          remoting::(anonymous namespace)::SessionWatcher::InstallEventHandler() in libhost.a(curtain_mode_mac.o)
      "_InstallEventHandler", referenced from:
          remoting::(anonymous namespace)::SessionWatcher::InstallEventHandler() in libhost.a(curtain_mode_mac.o)
      "_RemoveEventHandler", referenced from:
          remoting::(anonymous namespace)::SessionWatcher::RemoveEventHandler() in libhost.a(curtain_mode_mac.o)
    ld: symbol(s) not found for architecture x86_64
    clang: error: linker command failed with exit code 1 (use -v to see invocation)
    
    Original issue's description:
    > Adding mac support for Remoting host GN builds.
    >
    > BUG=611859
    >
    > R=sergeyu@chromium.org
    >
    > Committed: https://crrev.com/e55f21958be24503dd0390d93e999c0a23f724c5
    > Cr-Commit-Position: refs/heads/master@{#399731}
    
    TBR=sergeyu@chromium.org,nicholss@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=611859
    
    Review-Url: https://codereview.chromium.org/2068633003
    Cr-Commit-Position: refs/heads/master@{#399742}

--- remoting/host/constants_mac.cc
+++ remoting/host/constants_mac.cc
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/host/constants_mac.h"
 
-#if defined(GN_BUILD)
-#include "remoting/host/version.h"
-#endif
-
 namespace remoting {
 
 #define SERVICE_NAME "org.chromium.chromoting"
