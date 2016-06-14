832d0f920e557c0d6e95b06a155bda5e7d14a28a
xhwang <xhwang@chromium.org>
2016-06-13 17:33:26 -0700

    media: Cancel MediaCryptoReadyCB during AVDA destruction
    This is the AVDA version of
    https://codereview.chromium.org/1601743007
    
    We could be reusing the same MediaKeys object in JS app on a second
    media element. If the MediaCryptoReadyCB has not been returned by
    MediaDrmBridge for the first media element, and we attach the same
    MediaKeys on the second media element, we could hit the DCHECK in
    MediaDrmBridge that we only allow one MediaCryptoReadyCB at one
    time.
    
    BUG=b/26593586
    
    Review-Url: https://codereview.chromium.org/2059153003
    Cr-Commit-Position: refs/heads/master@{#399619}

--- media/gpu/android_video_decode_accelerator.cc
+++ media/gpu/android_video_decode_accelerator.cc
@@ -410,6 +410,11 @@ AndroidVideoDecodeAccelerator::~AndroidVideoDecodeAccelerator() {
     return;
 
   DCHECK(cdm_registration_id_);
+
+  // Cancel previously registered callback (if any).
+  media_drm_bridge_cdm_context_->SetMediaCryptoReadyCB(
+      MediaDrmBridgeCdmContext::MediaCryptoReadyCB());
+
   media_drm_bridge_cdm_context_->UnregisterPlayer(cdm_registration_id_);
 #endif  // defined(ENABLE_MOJO_MEDIA_IN_GPU_PROCESS)
 }
