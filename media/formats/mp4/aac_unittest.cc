1d2725f5f1ba0151c409e80b63eff07179d88729
chcunningham <chcunningham@chromium.org>
2016-06-13 19:58:20 -0700

    Simplify and document MP4 AAC parsing
    Minor cleanup of parser flow and comments. We implement the 2009 spec
    for base profiles 1-4 and extenions 5 (sbr) & 29 (ps). Parsing for other
    profiles is not implemented, so bail out and warn when found.
    
    BUG=526314,532281
    TEST=media_unittest updated
    
    Review-Url: https://codereview.chromium.org/2055663002
    Cr-Commit-Position: refs/heads/master@{#399647}

--- media/formats/mp4/aac_unittest.cc
+++ media/formats/mp4/aac_unittest.cc
@@ -12,9 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+using ::testing::AllOf;
+using ::testing::HasSubstr;
 using ::testing::InSequence;
 using ::testing::StrictMock;
-using ::testing::AllOf;
 
 namespace media {
 
@@ -187,6 +188,7 @@ TEST_F(AACTest, IncorrectProfileTest) {
   std::vector<uint8_t> data;
 
   data.assign(buffer, buffer + sizeof(buffer));
+  EXPECT_MEDIA_LOG(UnsupportedAudioProfileLog("mp4a.40.0"));
   EXPECT_FALSE(Parse(data));
 
   data[0] = 0x08;
