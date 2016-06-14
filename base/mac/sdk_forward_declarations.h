afa803b81cc005cb5c9032a9a815cdfa74b16f7b
rohitrao <rohitrao@chromium.org>
2016-06-14 11:38:38 -0700

    Include CoreBluetooth.h from files that need it.
    BUG=619813
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2068753002
    Cr-Commit-Position: refs/heads/master@{#399756}

--- base/mac/sdk_forward_declarations.h
+++ base/mac/sdk_forward_declarations.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_MAC_SDK_FORWARD_DECLARATIONS_H_
 
 #import <AppKit/AppKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
 #import <CoreWLAN/CoreWLAN.h>
 #import <ImageCaptureCore/ImageCaptureCore.h>
 #import <IOBluetooth/IOBluetooth.h>
