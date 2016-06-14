6180d16e273715b905c412eee71804d1db222022
bsep <bsep@chromium.org>
2016-06-13 17:51:51 -0700

    Draw nicer arrows when the scrollbar buttons are not square.
    Before they would look squished or messed up, but with this patch
    their bounding box is always square so we'll get the nice pixel pyramids
    that we want. This shouldn't change anything visually for the normal
    square scrollbar buttons.
    
    BUG=614297
    
    Review-Url: https://codereview.chromium.org/2009733002
    Cr-Commit-Position: refs/heads/master@{#399626}

--- ui/native_theme/native_theme_base.cc
+++ ui/native_theme/native_theme_base.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/skia_util.h"
@@ -369,12 +370,8 @@ void NativeThemeBase::PaintArrow(SkCanvas* gc,
 
 SkPath NativeThemeBase::PathForArrow(const gfx::Rect& rect,
                                      Part direction) const {
-  gfx::Rect bounding_rect(rect);
-  const int padding_width = ceil(rect.width() / 4.f);
-  const int padding_height = ceil(rect.height() / 4.f);
-  bounding_rect.Inset(padding_width, padding_height);
-  const gfx::Point center = bounding_rect.CenterPoint();
-
+  gfx::Rect bounding_rect = BoundingRectForArrow(rect);
+  const gfx::PointF center = gfx::RectF(bounding_rect).CenterPoint();
   SkPath path;
   SkMatrix transform;
   transform.setIdentity();
@@ -386,7 +383,6 @@ SkPath NativeThemeBase::PathForArrow(const gfx::Rect& rect,
     path.close();
     path.offset(0, -arrow_altitude / 2 + 1);
     if (direction == kScrollbarDownArrow) {
-      path.offset(0, -1);
       transform.setScale(1, -1, center.x(), center.y());
     }
   } else {
@@ -397,13 +393,24 @@ SkPath NativeThemeBase::PathForArrow(const gfx::Rect& rect,
     path.close();
     path.offset(arrow_altitude / 2, 0);
     if (direction == kScrollbarLeftArrow) {
-      path.offset(-1, 0);
       transform.setScale(-1, 1, center.x(), center.y());
     }
   }
   path.transform(transform);
 
   return path;
+}
+
+gfx::Rect NativeThemeBase::BoundingRectForArrow(const gfx::Rect& rect) const {
+  const std::pair<int, int> rect_sides =
+      std::minmax(rect.width(), rect.height());
+  const int side_length_inset = 2 * std::ceil(rect_sides.second / 4.f);
+  const int side_length =
+      std::min(rect_sides.first, rect_sides.second - side_length_inset);
+  // When there are an odd number of pixels, put the extra on the top/left.
+  return gfx::Rect(rect.x() + (rect.width() - side_length + 1) / 2,
+                   rect.y() + (rect.height() - side_length + 1) / 2,
+                   side_length, side_length);
 }
 
 void NativeThemeBase::PaintScrollbarTrack(SkCanvas* canvas,
