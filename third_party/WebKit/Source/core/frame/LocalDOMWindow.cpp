b896cb476cc76bb9f74fd38b888516cb3abf6111
lukasza <lukasza@chromium.org>
2016-06-15 12:31:13 -0700

    Returning early when setting a frame name to the same name.
    BUG=618813
    
    Review-Url: https://codereview.chromium.org/2053903002
    Cr-Commit-Position: refs/heads/master@{#399988}

--- third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
+++ third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
@@ -1057,6 +1057,9 @@ void LocalDOMWindow::setName(const AtomicString& name)
     if (!isCurrentlyDisplayedInFrame())
         return;
 
+    if (name == frame()->tree().name())
+        return;
+
     frame()->tree().setName(name);
     ASSERT(frame()->loader().client());
     frame()->loader().client()->didChangeName(name, frame()->tree().uniqueName());
