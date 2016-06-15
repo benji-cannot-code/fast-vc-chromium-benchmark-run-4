c6738f96ebda404ff6e7cb0fedc7221d06df999a
dmurph <dmurph@chromium.org>
2016-06-15 13:24:25 -0700

    Fix for clear all in SingleCategoryPreferences.
    BUG=560549,579763,619866
    
    Review-Url: https://codereview.chromium.org/2064143003
    Cr-Commit-Position: refs/heads/master@{#399999}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SingleCategoryPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SingleCategoryPreferences.java
@@ -462,7 +462,7 @@ public class SingleCategoryPreferences extends PreferenceFragment
             }
         }
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setPositiveButton(R.string.storage_clear_dialog_clear_storage_option,
                 new DialogInterface.OnClickListener() {
                     @Override
@@ -474,7 +474,7 @@ public class SingleCategoryPreferences extends PreferenceFragment
         builder.setTitle(R.string.storage_clear_site_storage_title);
         Resources res = getResources();
         String dialogFormattedText = res.getString(R.string.storage_clear_dialog_text,
-                Formatter.formatShortFileSize(getContext(), totalUsage));
+                Formatter.formatShortFileSize(getActivity(), totalUsage));
         builder.setMessage(dialogFormattedText);
         builder.create().show();
     }
