83da6029a8fa34d240b7ec5fb81aa2b4d6461e86
liberato <liberato@chromium.org>
2016-06-15 16:18:27 -0700

    Catch IllegalArgumentException during MediaCodec::start().
    MediaCodec::start sometimes throws IllegalArgumentException, though
    the documentation doesn't describe this.  Crash reports indicate
    nothing terribly useful, except that sometimes AVDA destruction is
    pending on another thread, indicating that mediaserver has stopped
    responding.  Many crash reports don't show this, however.
    
    Some others show messages about mediaserver crashing.
    
    This CL won't fix the underlying problem(s), but will prevent the
    browser from crashing.
    
    BUG=611502
    
    Review-Url: https://codereview.chromium.org/2067363002
    Cr-Commit-Position: refs/heads/master@{#400040}

--- media/base/android/java/src/org/chromium/media/MediaCodecBridge.java
+++ media/base/android/java/src/org/chromium/media/MediaCodecBridge.java
@@ -252,6 +252,9 @@ class MediaCodecBridge {
         } catch (IllegalStateException e) {
             Log.e(TAG, "Cannot start the media codec", e);
             return false;
+        } catch (IllegalArgumentException e) {
+            Log.e(TAG, "Cannot start the media codec", e);
+            return false;
         }
         return true;
     }
