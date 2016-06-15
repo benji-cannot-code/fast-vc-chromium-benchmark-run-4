c313679cddf05ab156e17a11f50ef6123ff5a4ea
jam <jam@chromium.org>
2016-06-15 10:25:18 -0700

    Remove debugging code for fixed bugs. Also clean up unused includes.
    BUG=90971,598073
    
    Review-Url: https://codereview.chromium.org/2062263002
    Cr-Commit-Position: refs/heads/master@{#399943}

--- content/browser/loader/async_resource_handler.cc
+++ content/browser/loader/async_resource_handler.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/loader/resource_dispatcher_host_impl.h"
 #include "content/browser/loader/resource_message_filter.h"
 #include "content/browser/loader/resource_request_info_impl.h"
-#include "content/browser/resource_context_impl.h"
 #include "content/common/resource_messages.h"
 #include "content/common/resource_request_completion_status.h"
 #include "content/common/view_messages.h"
--- content/browser/loader/resource_dispatcher_host_impl.h
+++ content/browser/loader/resource_dispatcher_host_impl.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
-#include <set>
 #include <string>
 #include <vector>
 
@@ -34,9 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/loader/resource_scheduler.h"
 #include "content/common/content_export.h"
 #include "content/common/resource_request_body.h"
-#include "content/public/browser/child_process_data.h"
-#include "content/public/browser/download_item.h"
-#include "content/public/browser/download_url_parameters.h"
 #include "content/public/browser/global_request_id.h"
 #include "content/public/browser/notification_types.h"
 #include "content/public/browser/resource_dispatcher_host.h"
@@ -135,12 +131,6 @@ class CONTENT_EXPORT ResourceDispatcherHostImpl
   // Puts the resource dispatcher host in an inactive state (unable to begin
   // new requests).  Cancels all pending requests.
   void Shutdown();
-
-  // Notify the ResourceDispatcherHostImpl of a new resource context.
-  void AddResourceContext(ResourceContext* context);
-
-  // Notify the ResourceDispatcherHostImpl of a resource context destruction.
-  void RemoveResourceContext(ResourceContext* context);
 
   // Force cancels any pending requests for the given |context|. This is
   // necessary to ensure that before |context| goes away, all requests
@@ -269,8 +259,7 @@ class CONTENT_EXPORT ResourceDispatcherHostImpl
 
   // Must be called after the ResourceRequestInfo has been created
   // and associated with the request.
-  // |id| should be |content::DownloadItem::kInvalidId| to request automatic
-  // assignment. This is marked virtual so it can be overriden in testing.
+  // This is marked virtual so it can be overriden in testing.
   virtual std::unique_ptr<ResourceHandler> CreateResourceHandlerForDownload(
       net::URLRequest* request,
       bool is_content_initiated,
@@ -649,10 +638,6 @@ class CONTENT_EXPORT ResourceDispatcherHostImpl
   // AsyncRevalidationManager is non-NULL if and only if
   // stale-while-revalidate is enabled.
   std::unique_ptr<AsyncRevalidationManager> async_revalidation_manager_;
-
-  // http://crbug.com/90971 - Assists in tracking down use-after-frees on
-  // shutdown.
-  std::set<const ResourceContext*> active_resource_contexts_;
 
   typedef std::map<GlobalRequestID,
                    base::ObserverList<ResourceMessageDelegate>*> DelegateMap;
