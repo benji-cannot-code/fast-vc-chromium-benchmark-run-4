99faf9e61cb43a1e2bae50b6204df9668e6825ef
thakis <thakis@chromium.org>
2016-06-15 06:51:23 -0700

    Gender-neutralize a few more comments / strings.
    BUG=none
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2065373002
    Cr-Commit-Position: refs/heads/master@{#399889}

--- components/password_manager/core/browser/login_database_unittest.cc
+++ components/password_manager/core/browser/login_database_unittest.cc
@@ -293,7 +293,7 @@ TEST_F(LoginDatabaseTest, Logins) {
   EXPECT_EQ(1U, result.size());
   result.clear();
 
-  // User changes his password.
+  // User changes their password.
   PasswordForm form6(form5);
   form6.password_value = ASCIIToUTF16("test6");
   form6.preferred = true;
