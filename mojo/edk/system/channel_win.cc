f13bfc82a8b8d641e3fe603a9519b2c2c6cb3b9f
amistry <amistry@chromium.org>
2016-06-15 10:57:27 -0700

    Always leak the Mojo parent pipe handle on child process shutdown.
    In legacy Chrome-IPC, the browser uses the disconnection on the IPC
    pipe as indication that the child process has died. It does this by
    intentionally holding on to the parent IPC pipe in the child as the
    child process exits. When the child exits, the OS closes the pipe
    and the browser receives a disconnection event and assumes the child
    process is dead.
    
    When using ChannelMojo, the IPC pipe is a Mojo message pipe. In the
    child, Mojo is is told to perform an orderly shutdown when the child
    process is shutting down. This results in the IPC pipe being closed
    before the child process dies. In this case, when the browser sees
    the pipe closure, the child process may not be dead, violating the
    assumption that the child is dead, which causes problems with child
    termination.
    
    To maintain existing semantics and avoid issues with child
    termination, the EDK always leaks the parent pipe in the child instead
    of closing it. The parent will then only see the pipe close when the
    child process exits. The EDK will only leak the parent-child OS pipe
    in the child process. All other pipes will be closed when Mojo is shut
    down.
    
    BUG=618121
    
    Review-Url: https://codereview.chromium.org/2065453004
    Cr-Commit-Position: refs/heads/master@{#399949}

--- mojo/edk/system/channel_win.cc
+++ mojo/edk/system/channel_win.cc
@@ -117,6 +117,11 @@ class ChannelWin : public Channel,
     }
   }
 
+  void LeakHandle() override {
+    DCHECK(io_task_runner_->RunsTasksOnCurrentThread());
+    leak_handle_ = true;
+  }
+
   bool GetReadPlatformHandles(
       size_t num_handles,
       const void* extra_header,
@@ -192,6 +197,8 @@ class ChannelWin : public Channel,
     // |handle_| should be valid at this point.
     CHECK(handle_.is_valid());
     CancelIo(handle_.get().handle);
+    if (leak_handle_)
+      ignore_result(handle_.release());
     handle_.reset();
 
     // May destroy the |this| if it was the last reference.
@@ -332,6 +339,8 @@ class ChannelWin : public Channel,
   std::deque<MessageView> outgoing_messages_;
 
   bool wait_for_connect_;
+
+  bool leak_handle_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(ChannelWin);
 };
