3b109d77d048acd2d1c9db2aa0635675bdde56cc
bruthig <bruthig@chromium.org>
2016-06-15 15:51:12 -0700

    Made AppListButton, OverflowButton, and Status area buttons use a white activated state.
    The previous blue color for that activated state was carried over from
    the non-material shelf and didn't look great.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2069203002
    Cr-Commit-Position: refs/heads/master@{#400034}

--- ash/common/shelf/shelf_constants.cc
+++ ash/common/shelf/shelf_constants.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/common/material_design/material_design_controller.h"
 #include "base/logging.h"
+#include "third_party/skia/include/core/SkColor.h"
 
 namespace ash {
 
@@ -17,6 +18,8 @@ const int kWorkspaceAreaAutoHideInset = 5;
 const int kShelfAutoHideSize = 3;
 const int kShelfItemInset = 3;
 const SkColor kShelfBaseColor = SK_ColorBLACK;
+const SkColor kShelfButtonActivatedHighlightColor =
+    SkColorSetA(SK_ColorWHITE, 100);
 const SkColor kShelfIconColor = SK_ColorWHITE;
 
 int GetShelfConstant(ShelfConstant shelf_constant) {
