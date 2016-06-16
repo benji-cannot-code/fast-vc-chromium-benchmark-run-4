4908ae8e74b4442be4b2a3ac5044ec367cb3589b
sanjoy.pal <sanjoy.pal@samsung.com>
2016-06-16 12:58:56 -0700

    Remove unused includes in c/b/prerender.
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2068463006
    Cr-Commit-Position: refs/heads/master@{#400239}

--- chrome/browser/prerender/prerender_link_manager.h
+++ chrome/browser/prerender/prerender_link_manager.h
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/keyed_service.h"
 #include "url/gurl.h"
 
-class Profile;
-
 namespace content {
 struct Referrer;
 }
--- chrome/browser/prerender/prerender_manager_factory.cc
+++ chrome/browser/prerender/prerender_manager_factory.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/sys_info.h"
 #include "base/trace_event/trace_event.h"
-#include "chrome/browser/browser_process.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/predictors/predictor_database_factory.h"
 #include "chrome/browser/prerender/prerender_manager.h"
