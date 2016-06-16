00553e15c23ee84c2f87fe238b91a984dbe5bdef
sdefresne <sdefresne@chromium.org>
2016-06-16 05:13:08 -0700

    Fix compilation with clang shipped with Xcode.
    After https://codereview.chromium.org/2051343002, the version of
    clang shipped with Xcode gets confused and think that gfx::RectF
    is a POD type and warns about constructing a const POD value.
    
    Workaround this by passing a temporary non-const value instead.
    
    BUG=620673
    
    Review-Url: https://codereview.chromium.org/2060943008
    Cr-Commit-Position: refs/heads/master@{#400135}

--- components/autofill/core/browser/autofill_external_delegate_unittest.cc
+++ components/autofill/core/browser/autofill_external_delegate_unittest.cc
@@ -137,9 +137,8 @@ class AutofillExternalDelegateUnitTest : public testing::Test {
     FormFieldData field;
     field.is_focusable = true;
     field.should_autocomplete = true;
-    const gfx::RectF element_bounds;
 
-    external_delegate_->OnQuery(query_id, form, field, element_bounds);
+    external_delegate_->OnQuery(query_id, form, field, gfx::RectF());
   }
 
   void IssueOnSuggestionsReturned() {
@@ -586,9 +585,8 @@ TEST_F(AutofillExternalDelegateUnitTest, IgnoreAutocompleteOffForAutofill) {
   FormFieldData field;
   field.is_focusable = true;
   field.should_autocomplete = false;
-  const gfx::RectF element_bounds;
 
-  external_delegate_->OnQuery(kQueryId, form, field, element_bounds);
+  external_delegate_->OnQuery(kQueryId, form, field, gfx::RectF());
 
   std::vector<Suggestion> autofill_items;
   autofill_items.push_back(Suggestion());
