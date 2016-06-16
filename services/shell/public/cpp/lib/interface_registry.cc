3b9194ca23d93ce66518f864f69aeddd0e425d02
ben <ben@chromium.org>
2016-06-16 06:41:18 -0700

    Allow InterfaceRegistry to wrap outbound InterfaceProviders also.
    R=rockot@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2070683002
    Cr-Commit-Position: refs/heads/master@{#400141}

--- services/shell/public/cpp/lib/interface_registry.cc
+++ services/shell/public/cpp/lib/interface_registry.cc
@@ -10,20 +10,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace shell {
 
 InterfaceRegistry::InterfaceRegistry(Connection* connection)
-    : InterfaceRegistry(nullptr, connection) {}
+    : InterfaceRegistry(nullptr, nullptr, connection) {}
 
-InterfaceRegistry::InterfaceRegistry(mojom::InterfaceProviderRequest request,
-                                     Connection* connection)
-    : binding_(this), connection_(connection) {
-  if (!request.is_pending())
-    request = GetProxy(&client_handle_);
-  binding_.Bind(std::move(request));
+InterfaceRegistry::InterfaceRegistry(
+    mojom::InterfaceProviderPtr remote_interfaces,
+    mojom::InterfaceProviderRequest local_interfaces_request,
+    Connection* connection)
+    : binding_(this),
+      connection_(connection),
+      remote_interfaces_(std::move(remote_interfaces)) {
+  if (!local_interfaces_request.is_pending())
+    local_interfaces_request = GetProxy(&client_handle_);
+  binding_.Bind(std::move(local_interfaces_request));
 }
 
 InterfaceRegistry::~InterfaceRegistry() {}
 
 mojom::InterfaceProviderPtr InterfaceRegistry::TakeClientHandle() {
   return std::move(client_handle_);
+}
+
+mojom::InterfaceProvider* InterfaceRegistry::GetRemoteInterfaces() {
+  return remote_interfaces_.get();
+}
+
+void InterfaceRegistry::SetRemoteInterfacesConnectionLostClosure(
+    const base::Closure& connection_lost_closure) {
+  remote_interfaces_.set_connection_error_handler(connection_lost_closure);
 }
 
 // mojom::InterfaceProvider:
