6b996064fe0be9401085f4eaf3c65544de1f6082
khmel <khmel@chromium.org>
2016-06-16 14:59:42 -0700

    arc: Restart OptIn flow from the begining on OptOut.
    BUG=b/29418860
    TEST=Manually on device. OptIn flow is restarted from Start page
         after disable/re-enabele Android apps and OptIn flow passes
         normally, no error is shown. Also Play Store is automatically
         started on first, second and next opt-ins.
    
    Review-Url: https://codereview.chromium.org/2073843002
    Cr-Commit-Position: refs/heads/master@{#400274}

--- chrome/browser/chromeos/arc/arc_auth_service.cc
+++ chrome/browser/chromeos/arc/arc_auth_service.cc
@@ -216,7 +216,7 @@ void ArcAuthService::OnSignInComplete() {
   DCHECK_EQ(state_, State::ACTIVE);
 
   if (!IsOptInVerificationDisabled() &&
-      !profile_->GetPrefs()->HasPrefPath(prefs::kArcSignedIn)) {
+      !profile_->GetPrefs()->GetBoolean(prefs::kArcSignedIn)) {
     playstore_launcher_.reset(
         new ArcAppLauncher(profile_, kPlayStoreAppId, true));
   }
@@ -447,6 +447,7 @@ void ArcAuthService::OnOptInPreferenceChanged() {
     if (state_ != State::STOPPED)
       UpdateEnabledStateUMA(false);
     ShutdownBridgeAndCloseUI();
+    profile_->GetPrefs()->SetBoolean(prefs::kArcSignedIn, false);
     return;
   }
 
