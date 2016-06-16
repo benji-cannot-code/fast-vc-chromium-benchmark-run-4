9d33343a7db533ce9deed73e491dc340847fa7af
joone.hur <joone.hur@intel.com>
2016-06-16 11:13:13 -0700

    Use enum instead of "bool preserveSelection and bool preserveStyle" of moveParagraph()
    This CL adds ShouldPreserveSelection and ShouldPreserveStyle enum types
    to CompositeEditCommand as follows:
    
    enum ShouldPreserveSelection { PreserveSelection, DoNotPreserveSelection };
    enum ShouldPreserveStyle { PreserveStyle, DoNotPreserveStyle };
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2073643002
    Cr-Commit-Position: refs/heads/master@{#400207}

--- third_party/WebKit/Source/core/editing/commands/InsertListCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/InsertListCommand.cpp
@@ -384,7 +384,7 @@ void InsertListCommand::unlistifyParagraph(const VisiblePosition& originalStart,
         return;
 
     VisiblePosition insertionPoint = createVisiblePosition(Position::beforeNode(placeholder));
-    moveParagraphs(start, end, insertionPoint, editingState, /* preserveSelection */ true, /* preserveStyle */ true, listChildNode);
+    moveParagraphs(start, end, insertionPoint, editingState, PreserveSelection, PreserveStyle, listChildNode);
 }
 
 static HTMLElement* adjacentEnclosingList(const VisiblePosition& pos, const VisiblePosition& adjacentPos, const HTMLQualifiedName& listTag)
@@ -496,7 +496,7 @@ void InsertListCommand::moveParagraphOverPositionIntoEmptyListItem(const Visible
     document().updateStyleAndLayoutIgnorePendingStylesheets();
     const VisiblePosition& start = startOfParagraph(pos, CanSkipOverEditingBoundary);
     const VisiblePosition& end = endOfParagraph(pos, CanSkipOverEditingBoundary);
-    moveParagraph(start, end, createVisiblePosition(Position::beforeNode(placeholder)), editingState, true);
+    moveParagraph(start, end, createVisiblePosition(Position::beforeNode(placeholder)), editingState, PreserveSelection);
 }
 
 DEFINE_TRACE(InsertListCommand)
