7dc269a87d72d33312674c73dbba67be99a03a4a
varkha <varkha@chromium.org>
2016-06-15 17:13:44 -0700

    [ash-md] Fixes regression that caused overview windows to accept input
    Regressed in https://codereview.chromium.org/1951183002/ with the label
    no longer covering the overview window.
    
    BUG=604767
    
    Review-Url: https://codereview.chromium.org/2068963003
    Cr-Commit-Position: refs/heads/master@{#400054}

--- ash/wm/overview/window_selector_item.h
+++ ash/wm/overview/window_selector_item.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/wm_window_observer.h"
 #include "ash/wm/overview/scoped_transform_overview_window.h"
 #include "base/macros.h"
+#include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/label_button.h"
@@ -40,15 +41,15 @@ class ASH_EXPORT WindowSelectorItem : public views::ButtonListener,
     // Makes sure that text is readable with |background_color|.
     void SetBackgroundColor(SkColor background_color);
 
-    void set_top_padding(int top_padding) { top_padding_ = top_padding; }
+    void set_padding(const gfx::Insets& padding) { padding_ = padding; }
 
    protected:
     // views::LabelButton:
     gfx::Rect GetChildAreaBounds() override;
 
    private:
-    // Padding on top of the button.
-    int top_padding_;
+    // Padding on all sides to correctly place the text inside the view.
+    gfx::Insets padding_;
 
     DISALLOW_COPY_AND_ASSIGN(OverviewLabelButton);
   };
