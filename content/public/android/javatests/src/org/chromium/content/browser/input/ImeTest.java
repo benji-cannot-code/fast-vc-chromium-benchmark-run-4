71eb6ea1d9f63dc91a0271769e5168b5fe85ea1c
grunell <grunell@chromium.org>
2016-06-16 07:21:14 -0700

    Revert of Remove  all the @FlakyTest annotations in ImeTest (patchset #2 id:20001 of https://codereview.chromium.org/2058633004/ )
    Reason for revert:
    Flaky failure seen in
    
    https://build.chromium.org/p/chromium.linux/builders/Android%20Tests%20%28dbg%29/builds/34878
    
    Original issue's description:
    > There are 6 flaky tests in ImeTest. They are flaky because when we call
    > InputConnection#setSelection and other methods together, the order of messages
    > sometimes does not get kept.
    >
    > We can solve this by enabling IME thread, because when IME thead is on,
    > the order can be kept.
    >
    > Another way is applying the following patch, which deals with the re-ordering
    > issue: https://codereview.chromium.org/1877073003/. Since the patch has been
    > landed, we can remove the @FlakyTest annotations.
    >
    > BUG=598482, 603991
    >
    > Committed: https://crrev.com/6f23392b1f1f2b91909f646bc5ddf54c2c4003a6
    > Cr-Commit-Position: refs/heads/master@{#400099}
    
    TBR=changwan@chromium.org,yabinh@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=598482, 603991
    
    Review-Url: https://codereview.chromium.org/2070083003
    Cr-Commit-Position: refs/heads/master@{#400151}

--- content/public/android/javatests/src/org/chromium/content/browser/input/ImeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/input/ImeTest.java
@@ -25,6 +25,7 @@ import android.view.inputmethod.InputConnection;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
@@ -110,6 +111,7 @@ public class ImeTest extends ContentShellTestBase {
 
     @MediumTest
     @Feature({"TextInput", "Main"})
+    @FlakyTest
     public void testDoesNotHang_getTextAfterKeyboardHides() throws Throwable {
         setComposingText("hello", 1);
         waitAndVerifyUpdateSelection(0, 5, 5, 0, 5);
@@ -237,6 +239,7 @@ public class ImeTest extends ContentShellTestBase {
 
     @SmallTest
     @Feature({"TextInput"})
+    @FlakyTest(message = "crbug.com/603991")
     public void testImeCopy() throws Exception {
         commitText("hello", 1);
         waitAndVerifyUpdateSelection(0, 5, 5, -1, -1);
@@ -577,6 +580,7 @@ public class ImeTest extends ContentShellTestBase {
 
     @SmallTest
     @Feature({"TextInput"})
+    @FlakyTest(message = "crbug.com/603991")
     public void testImeCut() throws Exception {
         commitText("snarful", 1);
         waitAndVerifyUpdateSelection(0, 7, 7, -1, -1);
@@ -668,6 +672,7 @@ public class ImeTest extends ContentShellTestBase {
 
     @SmallTest
     @Feature({"TextInput"})
+    @FlakyTest(message = "crbug.com/598482")
     public void testImeSelectAndUnSelectAll() throws Exception {
         commitText("hello", 1);
         waitAndVerifyUpdateSelection(0, 5, 5, -1, -1);
@@ -767,6 +772,7 @@ public class ImeTest extends ContentShellTestBase {
 
     @SmallTest
     @Feature({"TextInput", "Main"})
+    @FlakyTest
     public void testSwipingText() throws Throwable {
         focusElement("textarea");
 
@@ -1078,6 +1084,7 @@ public class ImeTest extends ContentShellTestBase {
 
     @SmallTest
     @Feature({"TextInput"})
+    @FlakyTest(message = "crbug.com/598482")
     public void testPastePopupShowAndHide() throws Throwable {
         commitText("hello", 1);
         waitAndVerifyUpdateSelection(0, 5, 5, -1, -1);
