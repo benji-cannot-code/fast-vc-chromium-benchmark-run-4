791c1308ff8717e3c2801c7ff9a0fc5cb23a5afb
zhongyi <zhongyi@chromium.org>
2016-06-17 15:24:54 -0700

    Revert of JobController 1: Remove cross reference between Request, Job, and Impl (patchset #23 id:660001 of https://codereview.chromium.org/1941083002/ )
    Reason for revert:
    This CL is top 1 causing crash.
    
    Revert this patch and re-land once we fixed the bug.
    
    Original issue's description:
    > JobController 1: Remove cross reference between Request, Job, and Impl.
    >
    > BUG=605398
    >
    > Committed: https://crrev.com/efbff200c26039dcd1ea0a4d585662629a8b9e69
    > Cr-Commit-Position: refs/heads/master@{#400086}
    
    TBR=rch@chromium.org,rdsmith@chromium.org,tbansal@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=605398
    
    Review-Url: https://codereview.chromium.org/2073293002
    Cr-Commit-Position: refs/heads/master@{#400525}

--- net/http/http_basic_stream.h
+++ net/http/http_basic_stream.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
-#include "net/base/net_export.h"
 #include "net/http/http_basic_state.h"
 #include "net/http/http_stream.h"
 
@@ -29,7 +28,7 @@ class HttpRequestHeaders;
 class HttpStreamParser;
 class IOBuffer;
 
-class NET_EXPORT_PRIVATE HttpBasicStream : public HttpStream {
+class HttpBasicStream : public HttpStream {
  public:
   // Constructs a new HttpBasicStream. InitializeStream must be called to
   // initialize it correctly.
