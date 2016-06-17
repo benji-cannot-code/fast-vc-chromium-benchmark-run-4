0592fc422a27bfa9ba8f53a96034419a36aa91f5
rouslan <rouslan@chromium.org>
2016-06-17 13:35:08 -0700

    Flag to disable Android Pay v1 integration.
    BUG=620173
    
    Review-Url: https://codereview.chromium.org/2074553002
    Cr-Commit-Position: refs/heads/master@{#400490}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -25,11 +25,12 @@ namespace android {
 namespace {
 
 // Array of features exposed through the Java ChromeFeatureList API. Entries in
-// this array may either refer to features defined in this file (above) or in
-// other locations in the code base (e.g. chrome/, components/, etc).
+// this array may either refer to features defined in the header of this file or
+// in other locations in the code base (e.g. chrome/, components/, etc).
 const base::Feature* kFeaturesExposedToJava[] = {
     &features::kCredentialManagementAPI,
     &features::kSimplifiedFullscreenUI,
+    &kAndroidPayIntegrationV1,
     &kImportantSitesInCBD,
     &kMediaStyleNotification,
     &kNTPFakeOmniboxTextFeature,
@@ -69,6 +70,10 @@ const base::Feature kNTPToolbarFeature {
 
 const base::Feature kNTPFakeOmniboxTextFeature {
   "NTPFakeOmniboxText", base::FEATURE_DISABLED_BY_DEFAULT
+};
+
+const base::Feature kAndroidPayIntegrationV1 {
+  "AndroidPayIntegrationV1", base::FEATURE_ENABLED_BY_DEFAULT
 };
 
 const base::Feature kPhysicalWebFeature {
