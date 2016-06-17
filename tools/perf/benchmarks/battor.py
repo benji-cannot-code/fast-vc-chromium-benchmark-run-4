085dc6452a3bdcaa4cb867df2078646930bd6c02
rnephew <rnephew@chromium.org>
2016-06-17 08:25:09 -0700

    [Telemetry] Rename battor benchmarks to make them consistent with other benchmarks.
    Review-Url: https://codereview.chromium.org/2073973002
    Cr-Commit-Position: refs/heads/master@{#400431}

--- tools/perf/benchmarks/battor.py
+++ tools/perf/benchmarks/battor.py
@@ -41,7 +41,7 @@ class BattOrPowerMobileSites(_BattOrBenchmark):
 
   @classmethod
   def Name(cls):
-    return 'BattOr.BattOrCases'
+    return 'battor.power_cases'
 
 @benchmark.Disabled('android') # crbug.com/618330
 class BattOrPowerMobileSitesNoChromeTrace(_BattOrBenchmark):
@@ -57,4 +57,4 @@ class BattOrPowerMobileSitesNoChromeTrace(_BattOrBenchmark):
 
   @classmethod
   def Name(cls):
-    return 'battor.battor_cases.no_chrome_trace'
+    return 'battor.power_cases.no_chrome_trace'
