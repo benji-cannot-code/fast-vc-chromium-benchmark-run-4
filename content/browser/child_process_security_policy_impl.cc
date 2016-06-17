3f7ebe41d5523d568a841b286d5856991f42065d
lukasza <lukasza@chromium.org>
2016-06-17 13:07:38 -0700

    Moving (Can|Grant)AccessFilesOfPageState into RenderFrameHostImpl.
    Moving CanAccessFilesOfPageState and CanAccessFilesOfPageState methods
    from RenderViewHostImpl into RenderFrameHostImpl (and refactoring
    and deduplicating some code along the way).
    
    BUG=613260
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2067493003
    Cr-Commit-Position: refs/heads/master@{#400479}

--- content/browser/child_process_security_policy_impl.cc
+++ content/browser/child_process_security_policy_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/child_process_security_policy_impl.h"
 
+#include <algorithm>
 #include <utility>
 
 #include "base/command_line.h"
@@ -627,6 +628,15 @@ bool ChildProcessSecurityPolicyImpl::CanCommitURL(int child_id,
 bool ChildProcessSecurityPolicyImpl::CanReadFile(int child_id,
                                                  const base::FilePath& file) {
   return HasPermissionsForFile(child_id, file, READ_FILE_GRANT);
+}
+
+bool ChildProcessSecurityPolicyImpl::CanReadAllFiles(
+    int child_id,
+    const std::vector<base::FilePath>& files) {
+  return std::all_of(files.begin(), files.end(),
+                     [this, child_id](const base::FilePath& file) {
+                       return CanReadFile(child_id, file);
+                     });
 }
 
 bool ChildProcessSecurityPolicyImpl::CanCreateReadWriteFile(
--- content/browser/child_process_security_policy_impl.h
+++ content/browser/child_process_security_policy_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <set>
 #include <string>
+#include <vector>
 
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
@@ -77,6 +78,9 @@ class CONTENT_EXPORT ChildProcessSecurityPolicyImpl
   bool HasWebUIBindings(int child_id) override;
   void GrantSendMidiSysExMessage(int child_id) override;
   bool CanAccessDataForOrigin(int child_id, const GURL& url) override;
+
+  // Returns if |child_id| can read all of the |files|.
+  bool CanReadAllFiles(int child_id, const std::vector<base::FilePath>& files);
 
   // Pseudo schemes are treated differently than other schemes because they
   // cannot be requested like normal URLs.  There is no mechanism for revoking
