fa8fdf2c85dad4690c6dd65cc5df4a955eb6b28f
qinmin <qinmin@chromium.org>
2016-06-16 22:45:01 -0700

    Format remaining download time in java code
    When displaying the remaining download time, ui::TimeFormat::Simple() is
    called.
    It calls into third_party/icu, and is quite expensive, takes 70kB from memory dump.
    This CL adds a simple java method to replace that call.
    The Java method is similar to
    android.java.text.Formatter.formatShortElapsedTime().
    And it should generate the same output string as
    ui::TimeFormat::Simple().
    
    BUG=620479
    
    Review-Url: https://codereview.chromium.org/2070713002
    Cr-Commit-Position: refs/heads/master@{#400370}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/MockDownloadNotificationService.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/MockDownloadNotificationService.java
@@ -59,10 +59,5 @@ public class MockDownloadNotificationService extends DownloadNotificationService
     public Context getApplicationContext() {
         return mContext == null ? super.getApplicationContext() : mContext;
     }
-
-    @Override
-    String getDurationString(long timeRemainingInMillis) {
-        return "";
-    }
 }
 
