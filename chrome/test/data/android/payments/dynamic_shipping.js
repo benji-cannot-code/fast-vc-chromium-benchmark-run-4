c4116bd497c75d2a022c54ca97ae85ec326cfc59
rouslan <rouslan@chromium.org>
2016-06-18 16:12:39 -0700

    Phone and email selection UI for PaymentRequest
    This patch adds the user interface for the user to select their
    preferred contact phone number and email address. An addition, this
    patch passes the request for these fields from Blink to the browser.
    
    BUG=617195
    
    Review-Url: https://codereview.chromium.org/2066483010
    Cr-Commit-Position: refs/heads/master@{#400603}

--- chrome/test/data/android/payments/dynamic_shipping.js
+++ chrome/test/data/android/payments/dynamic_shipping.js
@@ -31,7 +31,7 @@ function buy() {  // eslint-disable-line no-unused-vars
 
     request.show()
         .then(function(resp) {
-          resp.complete("success")
+          resp.complete('success')
               .then(function() {
                 print(
                     JSON.stringify(resp.totalAmount, undefined, 2) + '<br>' +
