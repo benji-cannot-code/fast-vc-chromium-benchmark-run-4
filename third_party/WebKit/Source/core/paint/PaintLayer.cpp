9c95d5997ade294f5cf73e86b2eefd00493b2b5b
wangxianzhu <wangxianzhu@chromium.org>
2016-06-18 20:11:36 -0700

    Check DisplayItemCient aliveness in cached subsequences
    BUG=609218
    
    Review-Url: https://codereview.chromium.org/2066603005
    Cr-Commit-Position: refs/heads/master@{#400607}

--- third_party/WebKit/Source/core/paint/PaintLayer.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayer.cpp
@@ -2889,8 +2889,10 @@ void PaintLayer::markCompositingContainerChainForNeedsRepaint()
                 break;
             container = owner->enclosingLayer();
         }
+
         if (container->m_needsRepaint)
             break;
+
         container->setNeedsRepaintInternal();
         layer = container;
     }
@@ -2908,6 +2910,15 @@ PaintTiming* PaintLayer::paintTiming()
     if (Node* node = layoutObject()->node())
         return &PaintTiming::from(node->document());
     return nullptr;
+}
+
+void PaintLayer::endShouldKeepAliveAllClientsRecursive()
+{
+    for (PaintLayer* child = firstChild(); child; child = child->nextSibling())
+        child->endShouldKeepAliveAllClientsRecursive();
+#if CHECK_DISPLAY_ITEM_CLIENT_ALIVENESS
+    DisplayItemClient::endShouldKeepAliveAllClients(this);
+#endif
 }
 
 DisableCompositingQueryAsserts::DisableCompositingQueryAsserts()
