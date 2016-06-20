016d1332acf8a9e488dda383d09324264331dd5a
jiadong.zhu <jiadong.zhu@linaro.org>
2016-06-20 04:09:43 -0700

    Update Zlib to version 1.2.8
    merge the latest open source zlib into chromium external projects
    
     Version 1.2.8 fixes a very rare bug in decompression. All users are encouraged to upgrade immediately. Version 1.2.8 also has these improvements:
        - Add new inflateGetDictionary() function
        - Fix bug where gzopen() immediately followed by gzclose() would write an empty file instead of an empty gzip stream.
        - Fix bug in gzclose() when gzwrite() runs out of memory
        Version 1.2.7 has many portability improvements over 1.2.6, and has these enhancements:
        - Fix bug in gzclose_w() when gzwrite() fails to allocate memory
        - Add "x" (O_EXCL) and "e" (O_CLOEXEC) modes support to gzopen()
        - Add gzopen_w() in Windows for wide character path names
        - Fix type mismatch between get_crc_table() and crc_table
        Version 1.2.6 has many changes over 1.2.5, including these improvements:
        - gzread() can now read a file that is being written concurrently
        - gzgetc() is now a macro for increased speed
        - Added a 'T' option to gzopen() for transparent writing (no compression)
        - Added deflatePending() to return the amount of pending output
        - Allow deflateSetDictionary() and inflateSetDictionary() at any time in raw mode
        - deflatePrime() can now insert bits in the middle of the stream
        - ./configure now creates a configure.log file with all of the results
        - Added a ./configure --solo option to compile zlib with no dependency on any libraries
        - Fixed a problem with large file support macros
        - Fixed a bug in contrib/puff
        - Many portability improvements
    
    BUG=610615
    R=agl@chromium.org
    R=gavinp@chromium.org
    
    Review-Url: https://codereview.chromium.org/1955383002
    Cr-Commit-Position: refs/heads/master@{#400647}

--- third_party/zlib/contrib/minizip/minizip.c
+++ third_party/zlib/contrib/minizip/minizip.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 */
 
 
-#ifndef _WIN32
+#if (!defined(_WIN32)) && (!defined(WIN32)) && (!defined(__APPLE__))
         #ifndef __USE_FILE_OFFSET64
                 #define __USE_FILE_OFFSET64
         #endif
@@ -29,6 +29,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         #endif
 #endif
 
+#ifdef __APPLE__
+// In darwin and perhaps other BSD variants off_t is a 64 bit value, hence no need for specific 64 bit functions
+#define FOPEN_FUNC(filename, mode) fopen(filename, mode)
+#define FTELLO_FUNC(stream) ftello(stream)
+#define FSEEKO_FUNC(stream, offset, origin) fseeko(stream, offset, origin)
+#else
+#define FOPEN_FUNC(filename, mode) fopen64(filename, mode)
+#define FTELLO_FUNC(stream) ftello64(stream)
+#define FSEEKO_FUNC(stream, offset, origin) fseeko64(stream, offset, origin)
+#endif
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -36,14 +49,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <errno.h>
 #include <fcntl.h>
 
-#ifdef unix
+#ifdef _WIN32
+# include <direct.h>
+# include <io.h>
+#else
 # include <unistd.h>
 # include <utime.h>
 # include <sys/types.h>
 # include <sys/stat.h>
-#else
-# include <direct.h>
-# include <io.h>
 #endif
 
 #include "zip.h"
@@ -82,7 +95,7 @@ uLong filetime(f, tmzip, dt)
   return ret;
 }
 #else
-#ifdef unix
+#ifdef unix || __APPLE__
 uLong filetime(f, tmzip, dt)
     char *f;               /* name of file to get info on */
     tm_zip *tmzip;         /* return value: access, modific. and creation times */
@@ -143,7 +156,7 @@ int check_exist_file(filename)
 {
     FILE* ftestexist;
     int ret = 1;
-    ftestexist = fopen64(filename,"rb");
+    ftestexist = FOPEN_FUNC(filename,"rb");
     if (ftestexist==NULL)
         ret = 0;
     else
@@ -174,7 +187,8 @@ int getFileCrc(const char* filenameinzip,void*buf,unsigned long size_buf,unsigne
 {
    unsigned long calculate_crc=0;
    int err=ZIP_OK;
-   FILE * fin = fopen64(filenameinzip,"rb");
+   FILE * fin = FOPEN_FUNC(filenameinzip,"rb");
+
    unsigned long size_read = 0;
    unsigned long total_read = 0;
    if (fin==NULL)
@@ -212,13 +226,12 @@ int isLargeFile(const char* filename)
 {
   int largeFile = 0;
   ZPOS64_T pos = 0;
-  FILE* pFile = fopen64(filename, "rb");
+  FILE* pFile = FOPEN_FUNC(filename, "rb");
 
   if(pFile != NULL)
   {
-    int n = fseeko64(pFile, 0, SEEK_END);
-
-    pos = ftello64(pFile);
+    int n = FSEEKO_FUNC(pFile, 0, SEEK_END);
+    pos = FTELLO_FUNC(pFile);
 
                 printf("File : %s is %lld bytes\n", filename, pos);
 
@@ -448,7 +461,7 @@ int main(argc,argv)
                     printf("error in opening %s in zipfile\n",filenameinzip);
                 else
                 {
-                    fin = fopen64(filenameinzip,"rb");
+                    fin = FOPEN_FUNC(filenameinzip,"rb");
                     if (fin==NULL)
                     {
                         err=ZIP_ERRNO;
--- third_party/zlib/inffixed.h
+++ third_party/zlib/inffixed.h
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
      * Generated automatically by makefixed().
      */
 
-    /* WARNING: this file should *not* be used by applications. It
-       is part of the implementation of the compression library and
-       is subject to change. Applications should only use zlib.h.
+    /* WARNING: this file should *not* be used by applications.
+       It is part of the implementation of this library and is
+       subject to change. Applications should only use zlib.h.
      */
 
     static const code lenfix[512] = {
