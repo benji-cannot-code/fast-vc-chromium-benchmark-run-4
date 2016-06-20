e10efb7cbcad9825c3364edea247535c7e6cfdc1
sky <sky@chromium.org>
2016-06-20 13:47:17 -0700

    Relaxes constraint that capture can only change with in flight event
    This is problematic for tab dragging. Also makes it so capture changes
    if in_nonclient_area changes.
    
    BUG=none
    TEST=covered by tests
    R=jonross@chromium.org
    
    Review-Url: https://codereview.chromium.org/2080333002
    Cr-Commit-Position: refs/heads/master@{#400774}

--- components/mus/ws/window_server.cc
+++ components/mus/ws/window_server.cc
@@ -333,9 +333,8 @@ void WindowServer::ProcessClientAreaChanged(
 }
 
 void WindowServer::ProcessLostCapture(const ServerWindow* window) {
-  for (auto& pair : tree_map_) {
+  for (auto& pair : tree_map_)
     pair.second->ProcessLostCapture(window, IsOperationSource(pair.first));
-  }
 }
 
 void WindowServer::ProcessWillChangeWindowHierarchy(
