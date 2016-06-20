7f15f55507aa14b70f1807e7b1f42740bea25a6a
paulmiller <paulmiller@chromium.org>
2016-06-20 13:03:11 -0700

    Revert of Roll internal Google Play Services (Chromium part) (patchset #3 id:40001 of https://codereview.chromium.org/2063703004/ )
    Reason for revert:
    downstream part broke
    
    Original issue's description:
    > Roll internal Google Play Services (Chromium part)
    >
    > BUG=613768
    >
    > Committed: https://crrev.com/27d98c86363d66c877e8ca29c1d34e3578eaa179
    > Cr-Commit-Position: refs/heads/master@{#400736}
    
    TBR=agrieve@chromium.org,mariakhomenko@chromium.org,jbudorick@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=613768
    
    Review-Url: https://codereview.chromium.org/2085723002
    Cr-Commit-Position: refs/heads/master@{#400754}

--- chrome/android/java/src/org/chromium/chrome/browser/externalauth/ExternalAuthUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/externalauth/ExternalAuthUtils.java
@@ -97,15 +97,16 @@ public class ExternalAuthUtils {
      * @return whether the currently running application is signed with Google keys.
      */
     public boolean isChromeGoogleSigned(Context context) {
-        return isGoogleSigned(context, context.getPackageName());
+        return isGoogleSigned(
+                context.getApplicationContext().getPackageManager(), context.getPackageName());
     }
 
     /**
      * Returns whether the call is originating from a Google-signed package.
-     * @param appContext the current context.
+     * @param pm Package manager to use for getting package related info.
      * @param packageName The package name to inquire about.
      */
-    public boolean isGoogleSigned(Context context, String packageName) {
+    public boolean isGoogleSigned(PackageManager pm, String packageName) {
         // This is overridden in a subclass.
         return false;
     }
@@ -129,7 +130,7 @@ public class ExternalAuthUtils {
         for (String packageName : callingPackages) {
             if (!TextUtils.isEmpty(packageToMatch) && !packageName.equals(packageToMatch)) continue;
             matchFound = true;
-            if ((shouldBeGoogleSigned && !isGoogleSigned(context, packageName))
+            if ((shouldBeGoogleSigned && !isGoogleSigned(pm, packageName))
                     || (shouldBeSystem && !isSystemBuild(pm, packageName))) {
                 return false;
             }
@@ -216,7 +217,7 @@ public class ExternalAuthUtils {
     public boolean canUseFirstPartyGooglePlayServices(
             Context context, UserRecoverableErrorHandler userRecoverableErrorHandler) {
         return canUseGooglePlayServices(context, userRecoverableErrorHandler)
-                && isChromeGoogleSigned(context);
+                && isGoogleSigned(context.getPackageManager(), context.getPackageName());
     }
 
     /**
