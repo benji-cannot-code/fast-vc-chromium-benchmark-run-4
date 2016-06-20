7aab1b9fd5c012786cdf56fc3fd4efffc27eea64
estade <estade@chromium.org>
2016-06-20 16:38:33 -0700

    Fix StyledLabel so it doesn't clip the bottom of Hindi text.
    A label can be taller than the calculated line height, so set the height
    according to the bottom of the lowest label (link) as we iterate through
    StyledLabel sub-views.
    
    Also, somewhat tangentially, reduce the amount of nesting
    in BubbleSyncPromoView by making it a StyledLabel rather
    than a view containing a single StyledLabel.
    
    BUG=618978
    
    Review-Url: https://codereview.chromium.org/2072513003
    Cr-Commit-Position: refs/heads/master@{#400833}

--- chrome/browser/ui/views/sync/bubble_sync_promo_view.h
+++ chrome/browser/ui/views/sync/bubble_sync_promo_view.h
@@ -8,14 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/macros.h"
+#include "ui/views/controls/styled_label.h"
 #include "ui/views/controls/styled_label_listener.h"
-#include "ui/views/view.h"
 
 class BubbleSyncPromoDelegate;
 
 // Bookmark sync promo displayed at the bottom of the bookmark bubble.
-class BubbleSyncPromoView : public views::StyledLabelListener,
-                            public views::View {
+class BubbleSyncPromoView : public views::StyledLabel,
+                            public views::StyledLabelListener {
  public:
   // |delegate| is not owned by BubbleSyncPromoView.
   BubbleSyncPromoView(BubbleSyncPromoDelegate* delegate,
@@ -24,13 +24,13 @@ class BubbleSyncPromoView : public views::StyledLabelListener,
   ~BubbleSyncPromoView() override;
 
  private:
+  // views::StyledLabel:
+  const char* GetClassName() const override;
+
   // views::StyledLabelListener:
   void StyledLabelLinkClicked(views::StyledLabel* label,
                               const gfx::Range& range,
                               int event_flags) override;
-
-  // views::View:
-  const char* GetClassName() const override;
 
   // Delegate, to handle clicks on the sign in link.
   BubbleSyncPromoDelegate* delegate_;
