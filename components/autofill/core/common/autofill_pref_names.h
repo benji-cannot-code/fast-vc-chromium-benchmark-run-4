7e025a349525f3c286327b6398e825d7a74f83c9
sebsg <sebsg@chromium.org>
2016-06-20 13:46:25 -0700

    Add a profile deduping routine to run on each major version.
    BUG=620422
    TEST=PersonalDataManagerTest
    
    TBR=thestig@chromium.org
    For the DEPS file where I added components/version_info. Thanks!
    
    Review-Url: https://codereview.chromium.org/2074253002
    Cr-Commit-Position: refs/heads/master@{#400773}

--- components/autofill/core/common/autofill_pref_names.h
+++ components/autofill/core/common/autofill_pref_names.h
@@ -14,6 +14,7 @@ namespace prefs {
 
 extern const char kAutofillEnabled[];
 extern const char kAutofillProfileUseDatesFixed[];
+extern const char kAutofillLastVersionDeduped[];
 extern const char kAutofillWalletImportEnabled[];
 extern const char kAutofillWalletImportStorageCheckboxState[];
 
