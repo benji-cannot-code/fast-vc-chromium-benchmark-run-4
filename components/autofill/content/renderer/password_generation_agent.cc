bee7a35fd686dcd8c06d4c349d38d2317eb0a66b
kolos <kolos@chromium.org>
2016-06-20 02:41:06 -0700

    [Password Generation] Sends form classifier vote to autofill server
    BUG=582434
    
    Review-Url: https://codereview.chromium.org/2055633003
    Cr-Commit-Position: refs/heads/master@{#400640}

--- components/autofill/content/renderer/password_generation_agent.cc
+++ components/autofill/content/renderer/password_generation_agent.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "components/autofill/content/common/autofill_messages.h"
 #include "components/autofill/content/renderer/form_autofill_util.h"
+#include "components/autofill/content/renderer/form_classifier.h"
 #include "components/autofill/content/renderer/password_autofill_agent.h"
 #include "components/autofill/content/renderer/password_form_conversion_utils.h"
 #include "components/autofill/core/common/autofill_switches.h"
@@ -195,6 +196,15 @@ void PasswordGenerationAgent::OnDynamicFormsSeen() {
   FindPossibleGenerationForm();
 }
 
+void PasswordGenerationAgent::RunFormClassifierAndSaveVote(
+    const blink::WebFormElement& web_form,
+    const PasswordForm& form) {
+  base::string16 generation_field;
+  ClassifyFormAndFindGenerationField(web_form, &generation_field);
+  Send(new AutofillHostMsg_SaveGenerationFieldDetectedByClassifier(
+      routing_id(), form, generation_field));
+}
+
 void PasswordGenerationAgent::FindPossibleGenerationForm() {
   if (!enabled_ || !render_frame())
     return;
@@ -233,6 +243,7 @@ void PasswordGenerationAgent::FindPossibleGenerationForm() {
     if (GetAccountCreationPasswordFields(
             form_util::ExtractAutofillableElementsInForm(forms[i]),
             &passwords)) {
+      RunFormClassifierAndSaveVote(forms[i], *password_form);
       AccountCreationFormData ac_form_data(
           make_linked_ptr(password_form.release()), passwords);
       possible_account_creation_forms_.push_back(ac_form_data);
