d430bfb2e745e46d1ceeec94fa10fe095d06be68
eostroukhov <eostroukhov@google.com>
2016-06-21 11:59:17 -0700

    Fix a V8 5.1 backwards compatibility
    R=dgozman@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2081233002
    Cr-Commit-Position: refs/heads/master@{#401061}

--- third_party/WebKit/Source/platform/v8_inspector/V8Compat.h
+++ third_party/WebKit/Source/platform/v8_inspector/V8Compat.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <v8.h>
 
-#if V8_MAJOR_VERSION < 5 || (V8_MAJOR_VERSION == 5 && V8_MINOR_VERSION < 2)
+#if V8_MAJOR_VERSION < 5 || (V8_MAJOR_VERSION == 5 && V8_MINOR_VERSION < 1)
 namespace v8 {
 
 // In standalone V8 inspector this is expected to be noop anyways...
