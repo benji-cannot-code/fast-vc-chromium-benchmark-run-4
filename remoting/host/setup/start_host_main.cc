62b294669ed6d487fcd8dddcb12ff7b735e52a2f
fdoray <fdoray@chromium.org>
2016-06-21 11:03:34 -0700

    Remove calls to MessageLoop::current() in remoting.
    Why?
    The fact that there's a MessageLoop on the thread is an
    unnecessary implementation detail. When browser threads
    are migrated to base/task_scheduler, tasks will no longer
    have access to a MessageLoop.
    
    These changes were generated manually.
    
    BUG=616447
    R=garykac@chromium.org
    
    Review-Url: https://codereview.chromium.org/2080343003
    Cr-Commit-Position: refs/heads/master@{#401042}

--- remoting/host/setup/start_host_main.cc
+++ remoting/host/setup/start_host_main.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/run_loop.h"
+#include "base/single_thread_task_runner.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread.h"
 #include "build/build_config.h"
@@ -87,7 +88,7 @@ std::string ReadString(bool no_echo) {
 
 // Called when the HostStarter has finished.
 void OnDone(HostStarter::Result result) {
-  if (base::MessageLoop::current() != g_message_loop) {
+  if (!g_message_loop->task_runner()->BelongsToCurrentThread()) {
     g_message_loop->PostTask(FROM_HERE, base::Bind(&OnDone, result));
     return;
   }
