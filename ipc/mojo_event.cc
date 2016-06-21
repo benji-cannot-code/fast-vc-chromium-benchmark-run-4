dae7ed5449f647c3fd1c1d5a42d2400414717b3c
rockot <rockot@chromium.org>
2016-06-21 13:17:49 -0700

    Implements Mojo-based waiting for IPC::SyncChannel
    Changes SyncChannel et al to use Mojo message pipe handles
    and mojo::SyncHandleRegistry for sync waiting in lieu of
    base::WaitableEvent.
    
    This is necessary to support sync Mojo interface messages
    without risk of deadlock.
    
    BUG=612500
    
    Review-Url: https://codereview.chromium.org/2072393003
    Cr-Commit-Position: refs/heads/master@{#401090}

--- ipc/mojo_event.cc
+++ ipc/mojo_event.cc
@@ -23,7 +23,7 @@ void MojoEvent::Signal() {
   MojoResult rv = mojo::WriteMessageRaw(
       signal_handle_.get(), nullptr, 0, nullptr, 0,
       MOJO_WRITE_MESSAGE_FLAG_NONE);
-  DCHECK_EQ(rv, MOJO_RESULT_OK);
+  CHECK_EQ(rv, MOJO_RESULT_OK);
 }
 
 void MojoEvent::Reset() {
@@ -34,7 +34,7 @@ void MojoEvent::Reset() {
   MojoResult rv = mojo::ReadMessageRaw(
       wait_handle_.get(), nullptr, nullptr, nullptr, nullptr,
       MOJO_READ_MESSAGE_FLAG_NONE);
-  DCHECK_EQ(rv, MOJO_RESULT_OK);
+  CHECK_EQ(rv, MOJO_RESULT_OK);
 }
 
 }  // namespace IPC
