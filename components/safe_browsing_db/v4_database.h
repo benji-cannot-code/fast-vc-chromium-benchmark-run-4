d8b5fbfb92d7de66f44f40fd3a5b86e9ba93b932
vakh <vakh@chromium.org>
2016-06-21 11:45:54 -0700

    Rename: s/ListInfoMap/StoreFileNameMap/g In components/safe_browsing_db/...
    TBR=nparker@chromium.org
    BUG=543161
    
    Review-Url: https://codereview.chromium.org/2080603005
    Cr-Commit-Position: refs/heads/master@{#401055}

--- components/safe_browsing_db/v4_database.h
+++ components/safe_browsing_db/v4_database.h
@@ -28,7 +28,7 @@ typedef base::Callback<void(std::unique_ptr<V4Database>)>
 // For instance, the UpdateListIdentifier could be for URL expressions for UwS
 // on Windows platform, and the corresponding file on disk could be named:
 // "uws_win_url.store"
-typedef base::hash_map<UpdateListIdentifier, std::string> ListInfoMap;
+typedef base::hash_map<UpdateListIdentifier, std::string> StoreFileNameMap;
 
 // This hash_map maps the UpdateListIdentifiers to their corresponding in-memory
 // stores, which contain the hash prefixes for that UpdateListIdentifier as well
@@ -43,7 +43,7 @@ class V4DatabaseFactory {
   virtual V4Database* CreateV4Database(
       const scoped_refptr<base::SequencedTaskRunner>& db_task_runner,
       const base::FilePath& base_dir_path,
-      const ListInfoMap& list_info_map) = 0;
+      const StoreFileNameMap& store_file_name_map) = 0;
 };
 
 // The on-disk databases are shared among all profiles, as it doesn't contain
@@ -61,7 +61,7 @@ class V4Database {
   static void Create(
       const scoped_refptr<base::SequencedTaskRunner>& db_task_runner,
       const base::FilePath& base_path,
-      const ListInfoMap& list_info_map,
+      const StoreFileNameMap& store_file_name_map,
       NewDatabaseReadyCallback callback);
 
   // Destroys the provided v4_database on its task_runner since this may be a
@@ -95,7 +95,7 @@ class V4Database {
   static void CreateOnTaskRunner(
       const scoped_refptr<base::SequencedTaskRunner>& db_task_runner,
       const base::FilePath& base_path,
-      const ListInfoMap& list_info_map,
+      const StoreFileNameMap& store_file_name_map,
       const scoped_refptr<base::SingleThreadTaskRunner>& callback_task_runner,
       NewDatabaseReadyCallback callback);
 
