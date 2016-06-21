d001f491c81845b3be84b4e6d86218a95d2e5a8c
penghuang <penghuang@chromium.org>
2016-06-21 09:38:25 -0700

    Use mojo SharedBuffer to replace base::SharedMemory in MojoGpuMemoryBuffer
    base::SharedMemory doesn't work in sandboxed process. So use mojo::SharedBuffer
    instead.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2081923003
    Cr-Commit-Position: refs/heads/master@{#401012}

--- components/mus/common/mojo_gpu_memory_buffer.cc
+++ components/mus/common/mojo_gpu_memory_buffer.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/shared_memory.h"
 #include "base/numerics/safe_conversions.h"
 #include "build/build_config.h"
+#include "mojo/public/cpp/system/buffer.h"
+#include "mojo/public/cpp/system/platform_handle.h"
 #include "ui/gfx/buffer_format_util.h"
 
 namespace mus {
@@ -31,11 +33,23 @@ std::unique_ptr<gfx::GpuMemoryBuffer> MojoGpuMemoryBufferImpl::Create(
     gfx::BufferFormat format,
     gfx::BufferUsage usage) {
   size_t bytes = gfx::BufferSizeForBufferFormat(size, format);
-  std::unique_ptr<base::SharedMemory> shared_memory(new base::SharedMemory);
 
-  if (!shared_memory->CreateAnonymous(bytes))
+  mojo::ScopedSharedBufferHandle handle =
+      mojo::SharedBufferHandle::Create(bytes);
+  if (!handle.is_valid())
     return nullptr;
 
+  base::SharedMemoryHandle platform_handle;
+  size_t shared_memory_size;
+  bool readonly;
+  MojoResult result = mojo::UnwrapSharedMemoryHandle(
+      std::move(handle), &platform_handle, &shared_memory_size, &readonly);
+  if (result != MOJO_RESULT_OK)
+    return nullptr;
+  DCHECK_EQ(shared_memory_size, bytes);
+
+  auto shared_memory =
+      base::MakeUnique<base::SharedMemory>(platform_handle, readonly);
   return base::WrapUnique<gfx::GpuMemoryBuffer>(
       new MojoGpuMemoryBufferImpl(size, format, std::move(shared_memory)));
 }
