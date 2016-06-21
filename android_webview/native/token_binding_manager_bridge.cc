058a432a12b932114b2d3212d0271ce7633ed1a7
davidben <davidben@chromium.org>
2016-06-21 12:53:42 -0700

    Switch AwTokenBindingManager's internals to PKCS#8.
    There's no need to do the silly encrypt with empty password and then decrypt in
    Java dance. Just serialize to a PKCS#8 blob and parse it back out normally.
    
    BUG=603319
    
    Review-Url: https://codereview.chromium.org/2047763002
    Cr-Commit-Position: refs/heads/master@{#401083}

--- android_webview/native/token_binding_manager_bridge.cc
+++ android_webview/native/token_binding_manager_bridge.cc
@@ -14,12 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/ec_private_key.h"
 #include "jni/AwTokenBindingManager_jni.h"
 #include "net/base/net_errors.h"
-#include "net/ssl/channel_id_service.h"
 
 using base::android::ConvertJavaStringToUTF8;
 using base::android::ScopedJavaGlobalRef;
 using content::BrowserThread;
-using net::ChannelIDService;
 
 namespace android_webview {
 
@@ -40,8 +38,7 @@ void OnKeyReady(const ScopedJavaGlobalRef<jobject>& callback,
   }
 
   std::vector<uint8_t> private_key;
-  key->ExportEncryptedPrivateKey(ChannelIDService::kEPKIPassword, 1,
-                                 &private_key);
+  key->ExportPrivateKey(&private_key);
   ScopedJavaLocalRef<jbyteArray> jprivate_key = base::android::ToJavaByteArray(
       env, private_key.data(), private_key.size());
 
