ae5186d77c507959a91ddcd550098d324b23151e
tedchoc <tedchoc@chromium.org>
2016-06-20 17:41:59 -0700

    Ensure WebContents are destroyed on the correct thread.
    BUG=619053
    
    Review-Url: https://codereview.chromium.org/2081853002
    Cr-Commit-Position: refs/heads/master@{#400853}

--- content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
@@ -13,6 +13,7 @@ import android.os.ParcelUuid;
 import android.os.Parcelable;
 
 import org.chromium.base.ObserverList;
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
@@ -136,6 +137,9 @@ import java.util.UUID;
 
     @Override
     public void destroy() {
+        if (!ThreadUtils.runningOnUiThread()) {
+            throw new IllegalStateException("Attempting to destroy WebContents on non-UI thread");
+        }
         if (mNativeWebContentsAndroid != 0) nativeDestroyWebContents(mNativeWebContentsAndroid);
     }
 
