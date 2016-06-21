a954681268af0a771a23b79c55c8e769832c9b30
pkasting <pkasting@chromium.org>
2016-06-21 14:17:38 -0700

    Fix tab coloring bugs with multiple open profiles which used different colors.
    Instead of caching based on incognito state, cache based on the actual colors
    used.
    
    This also fixes what I think was a real bug with not paying attention to a
    changing stroke color.
    
    BUG=616403
    TEST=Install theme from bug comment 0, hit ctrl-T several times, open a window from a second, unthemed profile, hit ctrl-T more times, and verify the tab background color in the second wnidow is the default one and not the themed one.
    
    Review-Url: https://codereview.chromium.org/2084583004
    Cr-Commit-Position: refs/heads/master@{#401109}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -1403,10 +1403,6 @@ bool TabStrip::CanPaintThrobberToLayer() const {
          !widget->IsFullscreen();
 }
 
-bool TabStrip::IsIncognito() const {
-  return controller_->IsIncognito();
-}
-
 bool TabStrip::IsImmersiveStyle() const {
   return immersive_style_;
 }
