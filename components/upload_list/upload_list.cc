11e4cc0c27828906c90542c869e3f4d2d1dea040
scottmg <scottmg@chromium.org>
2016-06-20 21:18:51 -0700

    List all crashes in chrome://crashes, including those not uploaded
    This will only affect Crashpad platforms (currently Mac and Windows).
    This is only a basic first step towards the linked bug, but might even
    be slightly useful already for users who are willing to manually attach
    a dump to a filed bug.
    
    Screenshot: http://imgur.com/C58HDi5.
    
    R=mark@chromium.org, caitkp@chromium.org
    BUG=620762
    
    Review-Url: https://codereview.chromium.org/2070993002
    Cr-Commit-Position: refs/heads/master@{#400892}

--- components/upload_list/upload_list.cc
+++ components/upload_list/upload_list.cc
@@ -21,13 +21,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 UploadList::UploadInfo::UploadInfo(const std::string& upload_id,
                                    const base::Time& upload_time,
                                    const std::string& local_id,
-                                   const base::Time& capture_time)
-    : upload_id(upload_id), upload_time(upload_time),
-      local_id(local_id), capture_time(capture_time) {}
+                                   const base::Time& capture_time,
+                                   State state)
+    : upload_id(upload_id),
+      upload_time(upload_time),
+      local_id(local_id),
+      capture_time(capture_time),
+      state(state) {}
 
 UploadList::UploadInfo::UploadInfo(const std::string& upload_id,
                                    const base::Time& upload_time)
-    : upload_id(upload_id), upload_time(upload_time) {}
+    : upload_id(upload_id), upload_time(upload_time), state(State::Uploaded) {}
 
 UploadList::UploadInfo::~UploadInfo() {}
 
@@ -83,7 +87,7 @@ void UploadList::ParseLogEntries(
     std::vector<std::string> components = base::SplitString(
         *i, ",", base::TRIM_WHITESPACE, base::SPLIT_WANT_ALL);
     // Skip any blank (or corrupted) lines.
-    if (components.size() < 2 || components.size() > 4)
+    if (components.size() < 2 || components.size() > 5)
       continue;
     base::Time upload_time;
     double seconds_since_epoch;
@@ -103,6 +107,13 @@ void UploadList::ParseLogEntries(
         !components[3].empty() &&
         base::StringToDouble(components[3], &seconds_since_epoch)) {
       info.capture_time = base::Time::FromDoubleT(seconds_since_epoch);
+    }
+
+    int state;
+    if (components.size() > 4 &&
+        !components[4].empty() &&
+        base::StringToInt(components[4], &state)) {
+      info.state = static_cast<UploadInfo::State>(state);
     }
 
     uploads->push_back(info);
