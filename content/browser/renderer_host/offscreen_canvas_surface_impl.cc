456044ab33016134ce1b501468d94d8524b7c71e
xlai <xlai@chromium.org>
2016-06-21 08:05:37 -0700

    Fix heap-use-after-free in cc::SurfaceManager::Destroy
    There is no need to call DestroyAll() on surface_factory_ inside
    OffscreenCanvasSurfaceImpl's destructor, because surface_factory_ is its unique
    pointer and SurfaceFactory's own destructor already contains DestroyAll().
    
    TBR=piman@chromium.org
    
    BUG=621849
    
    Review-Url: https://codereview.chromium.org/2088793003
    Cr-Commit-Position: refs/heads/master@{#400994}

--- content/browser/renderer_host/offscreen_canvas_surface_impl.cc
+++ content/browser/renderer_host/offscreen_canvas_surface_impl.cc
@@ -26,9 +26,6 @@ OffscreenCanvasSurfaceImpl::OffscreenCanvasSurfaceImpl(
       binding_(this, std::move(request)) {}
 
 OffscreenCanvasSurfaceImpl::~OffscreenCanvasSurfaceImpl() {
-  if (surface_factory_) {
-    surface_factory_->DestroyAll();
-  }
 }
 
 void OffscreenCanvasSurfaceImpl::GetSurfaceId(
