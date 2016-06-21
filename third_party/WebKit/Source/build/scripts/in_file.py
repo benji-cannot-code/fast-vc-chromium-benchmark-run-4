615d6ac2ed4870e27c78b9e3ed511054cd163725
iclelland <iclelland@chromium.org>
2016-06-21 12:33:53 -0700

    Remove old references to core/page/RuntimeEnabledFeatures.in
    This patch updates all of the references in code to the old location for
    RuntimeEnabledFeatures.in. It also removes the unused script
    audit_runtime_enabled_features.py, which has been broken for at least 18
    months.
    
    Review-Url: https://codereview.chromium.org/2080663006
    Cr-Commit-Position: refs/heads/master@{#401074}

--- third_party/WebKit/Source/build/scripts/in_file.py
+++ third_party/WebKit/Source/build/scripts/in_file.py
@@ -30,10 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import copy
 import os
 
-# NOTE: This has only been used to parse
-# core/page/RuntimeEnabledFeatures.in and may not be capable
-# of parsing other .in files correctly.
-
 # .in file format is:
 # // comment
 # name1 arg=value, arg2=value2, arg2=value3
