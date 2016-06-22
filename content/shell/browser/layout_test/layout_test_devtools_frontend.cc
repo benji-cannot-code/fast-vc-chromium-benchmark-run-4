be78e87e5f5ef91163ce39e91ed0ddad092ed52a
lukasza <lukasza@chromium.org>
2016-06-21 17:03:43 -0700

    Simplify detection of new, test-related RenderFrameHosts.
    Listening for WebContentsObserver::RenderFrameCreated is sufficient for
    detecting new test frames - there is no need to also listen for
    WebContentsObserver::RenderFrameHostChanged.
    
    BUG=583485
    
    Review-Url: https://codereview.chromium.org/2085023003
    Cr-Commit-Position: refs/heads/master@{#401143}

--- content/shell/browser/layout_test/layout_test_devtools_frontend.cc
+++ content/shell/browser/layout_test/layout_test_devtools_frontend.cc
@@ -139,10 +139,4 @@ void LayoutTestDevToolsFrontend::RenderFrameCreated(
   BlinkTestController::Get()->HandleNewRenderFrameHost(render_frame_host);
 }
 
-void LayoutTestDevToolsFrontend::RenderFrameHostChanged(
-    RenderFrameHost* old_host,
-    RenderFrameHost* new_host) {
-  BlinkTestController::Get()->HandleNewRenderFrameHost(new_host);
-}
-
 }  // namespace content
