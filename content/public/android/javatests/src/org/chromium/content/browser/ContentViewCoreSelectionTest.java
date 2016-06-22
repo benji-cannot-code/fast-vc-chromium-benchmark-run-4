ccd501dc99ff09e22dec8d1c614a4893412851a5
aelias <aelias@chromium.org>
2016-06-21 18:25:46 -0700

    Fix more corner cases for empty touch selection.
    1) In r373606 I fixed the case where selection would select whitespace,
    but that still left a broken case -- when the hit test intersects with
    the empty space at the end of a textbox (which doesn't have whitespace
    characters per se).  Add an explicit comparison to lastPositionInNode to
    cover this.
    
    2) The same empty selection conventions apply to double-tap as well as
    long-press, so extend the enum enabling this behavior to them.  (Double
    taps enter SelectionController as synthetic mouse events with
    clickCount=2 and fromTouch=true.)
    
    BUG=415933
    
    Review-Url: https://codereview.chromium.org/2074093003
    Cr-Commit-Position: refs/heads/master@{#401155}

--- content/public/android/javatests/src/org/chromium/content/browser/ContentViewCoreSelectionTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ContentViewCoreSelectionTest.java
@@ -36,7 +36,7 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
             + "<input id=\"empty_input_text\" type=\"text\" />"
             + "<br/><input id=\"input_text\" type=\"text\" value=\"SampleInputText\" />"
             + "<br/><textarea id=\"textarea\" rows=\"2\" cols=\"20\">SampleTextArea</textarea>"
-            + "<br/><input id=\"input_password\" type=\"password\" value=\"SamplePassword\" />"
+            + "<br/><input id=\"password\" type=\"password\" value=\"SamplePassword\" size=\"10\"/>"
             + "<br/><p><span id=\"plain_text_1\">SamplePlainTextOne</span></p>"
             + "<br/><p><span id=\"plain_text_2\">SamplePlainTextTwo</span></p>"
             + "<br/><input id=\"disabled_text\" type=\"text\" disabled value=\"Sample Text\" />"
@@ -242,7 +242,7 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
     @SmallTest
     @Feature({"TextInput"})
     public void testActionBarConfiguredCorrectlyForPassword() throws Throwable {
-        DOMUtils.longPressNode(this, mContentViewCore, "input_password");
+        DOMUtils.longPressNode(this, mContentViewCore, "password");
         waitForSelectActionBarVisible(true);
         assertTrue(mContentViewCore.hasSelection());
         assertNotNull(mContentViewCore.getSelectActionHandler());
@@ -303,7 +303,7 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
         assertNotNull(mContentViewCore.getSelectActionHandler());
         selectActionBarCopy();
         waitForClipboardContents(mContentViewCore.getContext(), "SamplePlainTextOne");
-        DOMUtils.longPressNode(this, mContentViewCore, "input_password");
+        DOMUtils.longPressNode(this, mContentViewCore, "password");
         waitForSelectActionBarVisible(true);
         assertTrue(mContentViewCore.hasSelection());
         assertNotNull(mContentViewCore.getSelectActionHandler());
@@ -360,7 +360,7 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
     @Feature({"TextInput"})
     public void testSelectActionBarPasswordCut() throws Exception {
         copyStringToClipboard("SampleTextToCopy");
-        DOMUtils.longPressNode(this, mContentViewCore, "input_password");
+        DOMUtils.longPressNode(this, mContentViewCore, "password");
         waitForSelectActionBarVisible(true);
         assertTrue(mContentViewCore.hasSelection());
         assertNotNull(mContentViewCore.getSelectActionHandler());
@@ -415,7 +415,7 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
     @SmallTest
     @Feature({"TextInput"})
     public void testSelectActionBarPasswordSelectAll() throws Exception {
-        DOMUtils.longPressNode(this, mContentViewCore, "input_password");
+        DOMUtils.longPressNode(this, mContentViewCore, "password");
         waitForSelectActionBarVisible(true);
         assertTrue(mContentViewCore.hasSelection());
         assertNotNull(mContentViewCore.getSelectActionHandler());
@@ -523,7 +523,7 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
         copyStringToClipboard("SamplePassword2");
 
         // Select the password field.
-        DOMUtils.longPressNode(this, mContentViewCore, "input_password");
+        DOMUtils.longPressNode(this, mContentViewCore, "password");
         waitForSelectActionBarVisible(true);
         assertTrue(mContentViewCore.hasSelection());
         assertEquals(mContentViewCore.getSelectedText().length(), "SamplePassword".length());
@@ -538,7 +538,7 @@ public class ContentViewCoreSelectionTest extends ContentShellTestBase {
         // Ensure the new text matches the pasted text. Note that we can't
         // actually compare strings as password field selections only provide
         // a placeholder with the correct length.
-        DOMUtils.longPressNode(this, mContentViewCore, "input_password");
+        DOMUtils.longPressNode(this, mContentViewCore, "password");
         waitForSelectActionBarVisible(true);
         assertTrue(mContentViewCore.hasSelection());
         assertEquals(mContentViewCore.getSelectedText().length(), "SamplePassword2".length());
