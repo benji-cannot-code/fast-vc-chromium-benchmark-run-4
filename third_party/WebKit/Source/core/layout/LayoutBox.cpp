ea5cffa17ed0f8866a3a196d0cc64c15cd274c5c
cbiesinger <cbiesinger@chromium.org>
2016-06-22 14:48:19 -0700

    Fix percentage handling for replaced elements
    - LayoutBox::computeReplacedLogicalHeightUsing needs to take into
      account the flexbox definite-height cases
    - LayoutBoxModelObject::hasAutoHeightOrContainingBlockWithAutoHeight
      needs to also call addPercentHeightDescendant so it can get
      relaid out appropriately
    
    BUG=499766
    TEST=css3/flexbox/percentage-height-replaced-element.html
    
    Review-Url: https://codereview.chromium.org/2086083002
    Cr-Commit-Position: refs/heads/master@{#401417}

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -2911,8 +2911,14 @@ LayoutUnit LayoutBox::computeReplacedLogicalHeightUsing(SizeType sizeType, const
         LayoutObject* cb = isOutOfFlowPositioned() ? container() : containingBlock();
         while (cb->isAnonymous())
             cb = cb->containingBlock();
-        if (cb->isLayoutBlock())
-            toLayoutBlock(cb)->addPercentHeightDescendant(const_cast<LayoutBox*>(this));
+        LayoutUnit stretchedFlexHeight(-1);
+        if (cb->isLayoutBlock()) {
+            LayoutBlock* block = toLayoutBlock(cb);
+            block->addPercentHeightDescendant(const_cast<LayoutBox*>(this));
+            if (block->isFlexItem())
+                stretchedFlexHeight = toLayoutFlexibleBox(block->parent())->childLogicalHeightForPercentageResolution(*block);
+
+        }
 
         if (cb->isOutOfFlowPositioned() && cb->style()->height().isAuto() && !(cb->style()->top().isAuto() || cb->style()->bottom().isAuto())) {
             ASSERT_WITH_SECURITY_IMPLICATION(cb->isLayoutBlock());
@@ -2930,6 +2936,8 @@ LayoutUnit LayoutBox::computeReplacedLogicalHeightUsing(SizeType sizeType, const
         LayoutUnit availableHeight;
         if (isOutOfFlowPositioned()) {
             availableHeight = containingBlockLogicalHeightForPositioned(toLayoutBoxModelObject(cb));
+        } else if (stretchedFlexHeight != -1) {
+            availableHeight = stretchedFlexHeight;
         } else {
             availableHeight = containingBlockLogicalHeightForContent(IncludeMarginBorderPadding);
             // It is necessary to use the border-box to match WinIE's broken
