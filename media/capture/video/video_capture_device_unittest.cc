7dadbc5367e548b33385767a3e845833fc94d6da
dalecurtis <dalecurtis@chromium.org>
2016-06-21 19:50:02 -0700

    Disable AllocateBadSize again on Linux.
    BUG=417824
    TBR=mcasas
    
    Review-Url: https://codereview.chromium.org/2082743002
    Cr-Commit-Position: refs/heads/master@{#401180}

--- media/capture/video/video_capture_device_unittest.cc
+++ media/capture/video/video_capture_device_unittest.cc
@@ -58,6 +58,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DeAllocateCameraWhileRunning DISABLED_DeAllocateCameraWhileRunning
 #define DeAllocateCameraWhileRunning DISABLED_DeAllocateCameraWhileRunning
 #define MAYBE_CaptureMjpeg DISABLED_CaptureMjpeg
+#elif defined(OS_LINUX)
+// AllocateBadSize will hang when a real camera is attached and if more than one
+// test is trying to use the camera (even across processes). Do NOT renable
+// this test without fixing the many bugs associated with it:
+// http://crbug.com/94134 http://crbug.com/137260 http://crbug.com/417824
+#define MAYBE_AllocateBadSize DISABLED_AllocateBadSize
+#define MAYBE_CaptureMjpeg CaptureMjpeg
 #else
 #define MAYBE_AllocateBadSize AllocateBadSize
 #define MAYBE_CaptureMjpeg CaptureMjpeg
