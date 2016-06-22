5a76c10034189a7b00981cd7420d5c6a430414b0
skyostil <skyostil@chromium.org>
2016-06-22 05:45:54 -0700

    headless: Add a dedicated child process entrypoint
    Add a dedicated entrypoint for transferring control to the headless
    library in a child process (e.g., a renderer). This avoids the need for
    the embedder to construct options for the browser, which would get
    ignored anyway.
    
    Embedders should now call headless::RunChildProcessIfNeeded() as soon as
    possible in their main entrypoint.
    
    BUG=595353
    
    Review-Url: https://codereview.chromium.org/2081423004
    Cr-Commit-Position: refs/heads/master@{#401260}

--- headless/lib/browser/headless_browser_impl.cc
+++ headless/lib/browser/headless_browser_impl.cc
@@ -7,11 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/command_line.h"
 #include "base/memory/ptr_util.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "content/public/app/content_main.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/content_switches.h"
 #include "headless/lib/browser/headless_browser_context_impl.h"
 #include "headless/lib/browser/headless_browser_main_parts.h"
 #include "headless/lib/browser/headless_web_contents_impl.h"
@@ -22,6 +24,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size.h"
 
 namespace headless {
+namespace {
+
+int RunContentMain(
+    HeadlessBrowser::Options options,
+    const base::Callback<void(HeadlessBrowser*)>& on_browser_start_callback) {
+  content::ContentMainParams params(nullptr);
+  params.argc = options.argc;
+  params.argv = options.argv;
+
+  // TODO(skyostil): Implement custom message pumps.
+  DCHECK(!options.message_pump);
+
+  std::unique_ptr<HeadlessBrowserImpl> browser(
+      new HeadlessBrowserImpl(on_browser_start_callback, std::move(options)));
+  headless::HeadlessContentMainDelegate delegate(std::move(browser));
+  params.delegate = &delegate;
+  return content::ContentMain(params);
+}
+
+}  // namespace
 
 HeadlessBrowserImpl::HeadlessBrowserImpl(
     const base::Callback<void(HeadlessBrowser*)>& on_start_callback,
@@ -29,7 +51,6 @@ HeadlessBrowserImpl::HeadlessBrowserImpl(
     : on_start_callback_(on_start_callback),
       options_(std::move(options)),
       browser_main_parts_(nullptr) {
-  DCHECK(!on_start_callback_.is_null());
 }
 
 HeadlessBrowserImpl::~HeadlessBrowserImpl() {}
@@ -142,21 +163,32 @@ void HeadlessBrowserImpl::SetOptionsForTesting(
       &options_);
 }
 
+void RunChildProcessIfNeeded(int argc, const char** argv) {
+  base::CommandLine command_line(argc, argv);
+  if (!command_line.HasSwitch(switches::kProcessType))
+    return;
+
+  HeadlessBrowser::Options::Builder builder(argc, argv);
+  exit(RunContentMain(builder.Build(),
+                      base::Callback<void(HeadlessBrowser*)>()));
+}
+
 int HeadlessBrowserMain(
     HeadlessBrowser::Options options,
     const base::Callback<void(HeadlessBrowser*)>& on_browser_start_callback) {
-  content::ContentMainParams params(nullptr);
-  params.argc = options.argc;
-  params.argv = options.argv;
+  DCHECK(!on_browser_start_callback.is_null());
+#if DCHECK_IS_ON()
+  // The browser can only be initialized once.
+  static bool browser_was_initialized;
+  DCHECK(!browser_was_initialized);
+  browser_was_initialized = true;
 
-  // TODO(skyostil): Implement custom message pumps.
-  DCHECK(!options.message_pump);
-
-  std::unique_ptr<HeadlessBrowserImpl> browser(
-      new HeadlessBrowserImpl(on_browser_start_callback, std::move(options)));
-  headless::HeadlessContentMainDelegate delegate(std::move(browser));
-  params.delegate = &delegate;
-  return content::ContentMain(params);
+  // Child processes should not end up here.
+  base::CommandLine command_line(options.argc, options.argv);
+  DCHECK(!command_line.HasSwitch(switches::kProcessType));
+#endif
+  return RunContentMain(std::move(options),
+                        std::move(on_browser_start_callback));
 }
 
 }  // namespace headless
