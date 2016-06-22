ba3e8a640fd2d2c5063c9c747d4de882a2793cae
sammc <sammc@chromium.org>
2016-06-21 21:12:39 -0700

    Change typemap options to use [].
    Currently, typemaps list options for type mappings in parenthesis.
    However, if the type is templated and passed by value, then the type
    mapping ends with >(pass_by_value), which gyp interprets as a variable
    expansion. This CL changes the options to use square brackets to avoid
    this problem.
    
    Review-Url: https://codereview.chromium.org/2085853002
    Cr-Commit-Position: refs/heads/master@{#401205}

--- mojo/public/tools/bindings/generate_type_mappings.py
+++ mojo/public/tools/bindings/generate_type_mappings.py
@@ -88,8 +88,8 @@ def ParseTypemap(typemap):
   mapping_pattern = \
       re.compile(r"""^([^=]+)           # mojom type
                      =
-                     ([^(]+)            # native type
-                     (?:\(([^)]+)\))?$  # optional attribute in parentheses
+                     ([^[]+)            # native type
+                     (?:\[([^]]+)\])?$  # optional attribute in square brackets
                  """, re.X)
   for typename in values['type_mappings']:
     match_result = mapping_pattern.match(typename)
