78d3dccb764fa145deeb811fa3066995d40159a0
fdoray <fdoray@chromium.org>
2016-06-22 10:33:03 -0700

    Remove calls to deprecated MessageLoop methods in rlz.
    This CL makes the following replacements in
    rlz:
    
    Before               After
    ----------------------------------------------------------
     x.PostTask()          x.task_runner()->PostTask()
       PostDelayedTask()                    PostDelayedTask()
       ReleaseSoon()                        ReleaseSoon()
       DeleteSoon()                         DeleteSoon()
    x->PostTask()         y->task_runner()->PostTask()
       PostDelayedTask()                    PostDelayedTask()
       ReleaseSoon()                        ReleaseSoon()
       DeleteSoon()                         DeleteSoon()
    
     x.Run()              RunLoop().Run()
     x.RunUntilIdle()     RunLoop().RunUntilIdle()
    
    x->Run()              RunLoop().Run()
    x->RunUntilIdle()     RunLoop().RunUntilIdle()
        If |y| isn't MessageLoopForUI::current() or
        MessageLoopForIO::current()
    
     y.message_loop()->task_runner()
                          y.task_runner()
    y->message_loop()->task_runner()
                          y->task_runner()
    ----------------------------------------------------------
    
    |x| is a base::MessageLoop(ForUI|ForIO) or a pointer to
    a base::MessageLoop(ForUI|ForIO). |y| is a base::Thread
    or a pointer to a base::Thread.
    
    This CL was generated using the MessageLoopDeprecatedMethods
    clang-tidy fix available on the associated bug. Only files
    that compile on Mac are affected. Follow-up CLs will make
    these replacements for other platforms.
    
    This CL doesn't change code behavior.
    
    BUG=616447
    R=rogerta@chromium.org
    
    Review-Url: https://codereview.chromium.org/2084913004
    Cr-Commit-Position: refs/heads/master@{#401330}

--- rlz/lib/rlz_lib_test.cc
+++ rlz/lib/rlz_lib_test.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/posix/eintr_wrapper.h"
+#include "base/single_thread_task_runner.h"
 #include "build/build_config.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -449,8 +450,7 @@ TEST_F(RlzLibTest, SendFinancialPing) {
   ASSERT_TRUE(io_thread.StartWithOptions(options));
 
   scoped_refptr<net::TestURLRequestContextGetter> context =
-      new net::TestURLRequestContextGetter(
-          io_thread.message_loop()->task_runner());
+      new net::TestURLRequestContextGetter(io_thread.task_runner());
   rlz_lib::SetURLRequestContext(context.get());
 
   URLRequestRAII set_context(context.get());
@@ -502,8 +502,7 @@ TEST_F(RlzLibTest, SendFinancialPingDuringShutdown) {
   ASSERT_TRUE(io_thread.StartWithOptions(options));
 
   scoped_refptr<net::TestURLRequestContextGetter> context =
-      new net::TestURLRequestContextGetter(
-          io_thread.message_loop()->task_runner());
+      new net::TestURLRequestContextGetter(io_thread.task_runner());
   rlz_lib::SetURLRequestContext(context.get());
 
   URLRequestRAII set_context(context.get());
@@ -515,7 +514,7 @@ TEST_F(RlzLibTest, SendFinancialPingDuringShutdown) {
   EXPECT_FALSE(rlz_lib::test::WasSendFinancialPingInterrupted());
 
   base::MessageLoop loop;
-  loop.PostTask(FROM_HERE, base::Bind(&ResetContext));
+  loop.task_runner()->PostTask(FROM_HERE, base::Bind(&ResetContext));
   std::string request;
   EXPECT_FALSE(rlz_lib::SendFinancialPing(rlz_lib::TOOLBAR_NOTIFIER, points,
       "swg", "GGLA", "SwgProductId1234", "en-UK", false,
