2ebf678fd82498d7f07765e61dfe064e5c6a605d
rockot <rockot@chromium.org>
2016-06-22 11:07:01 -0700

    Simplify SyncChannel message pumping event
    There's no need to use ref-counted or thread-local storage for
    this. It can be a single leaky MojoEvent instance.
    
    BUG=612500
    
    Review-Url: https://codereview.chromium.org/2081963003
    Cr-Commit-Position: refs/heads/master@{#401342}

--- ipc/ipc_sync_channel.h
+++ ipc/ipc_sync_channel.h
@@ -212,8 +212,6 @@ class IPC_EXPORT SyncChannel : public ChannelProxy {
   };
 
  private:
-  class PumpMessagesEvent;
-
   SyncChannel(
       Listener* listener,
       const scoped_refptr<base::SingleThreadTaskRunner>& ipc_task_runner,
@@ -229,7 +227,7 @@ class IPC_EXPORT SyncChannel : public ChannelProxy {
   // latter one also runs a nested message loop in the meantime.
   static void WaitForReply(mojo::SyncHandleRegistry* registry,
                            SyncContext* context,
-                           const MojoEvent* pump_messages_event);
+                           bool pump_messages);
 
   // Runs a nested message loop until a reply arrives, times out, or the process
   // shuts down.
@@ -241,7 +239,6 @@ class IPC_EXPORT SyncChannel : public ChannelProxy {
   // ChannelProxy overrides:
   void OnChannelInit() override;
 
-  scoped_refptr<PumpMessagesEvent> pump_messages_event_;
   scoped_refptr<mojo::SyncHandleRegistry> sync_handle_registry_;
 
   // Used to signal events between the IPC and listener threads.
