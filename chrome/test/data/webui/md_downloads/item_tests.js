ef3428829f7e7e114567fb2bef67e2f353fcdb5b
dbeam <dbeam@chromium.org>
2016-06-21 21:18:21 -0700

    MD Downloads: disallow linking to dangerous downloads
    R=asanka@chromium.org,michaelpg@chromium.org
    BUG=619834
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2081153004
    Cr-Commit-Position: refs/heads/master@{#401207}

---
+++ chrome/test/data/webui/md_downloads/item_tests.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+suite('item tests', function() {
+  /** @type {!downloads.Item} */
+  var item;
+
+  setup(function() {
+    item = document.createElement('downloads-item');
+    document.body.appendChild(item);
+  });
+
+  test("dangerous downloads aren't linkable", function() {
+    item.set('data', {
+      danger_type: downloads.DangerType.DANGEROUS_FILE,
+      file_externally_removed: false,
+      state: downloads.States.DANGEROUS,
+      url: 'http://evil.com'
+    });
+    Polymer.dom.flush();
+
+    assertTrue(item.$['file-link'].hidden);
+    assertFalse(item.$.url.hasAttribute('href'));
+  });
+});
