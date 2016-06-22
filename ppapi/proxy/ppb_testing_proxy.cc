5fa2fcf1cfc145999c8991f7de5178ed7c1736e1
fdoray <fdoray@chromium.org>
2016-06-22 06:20:43 -0700

    Remove calls to deprecated MessageLoop methods in ppapi.
    This CL makes the following replacements in
    ppapi:
    
    Before               After
    ----------------------------------------------------------
     x.PostTask()          x.task_runner()->PostTask()
       PostDelayedTask()                    PostDelayedTask()
       ReleaseSoon()                        ReleaseSoon()
       DeleteSoon()                         DeleteSoon()
    x->PostTask()         y->task_runner()->PostTask()
       PostDelayedTask()                    PostDelayedTask()
       ReleaseSoon()                        ReleaseSoon()
       DeleteSoon()                         DeleteSoon()
    
     x.Run()              RunLoop().Run()
     x.RunUntilIdle()     RunLoop().RunUntilIdle()
    
    x->Run()              RunLoop().Run()
    x->RunUntilIdle()     RunLoop().RunUntilIdle()
        If |y| isn't MessageLoopForUI::current() or
        MessageLoopForIO::current()
    
     y.message_loop()->task_runner()
                          y.task_runner()
    y->message_loop()->task_runner()
                          y->task_runner()
    ----------------------------------------------------------
    
    |x| is a base::MessageLoop(ForUI|ForIO) or a pointer to
    a base::MessageLoop(ForUI|ForIO). |y| is a base::Thread
    or a pointer to a base::Thread.
    
    This CL was generated using the MessageLoopDeprecatedMethods
    clang-tidy fix available on the associated bug. Only files
    that compile on Mac are affected. Follow-up CLs will make
    these replacements for other platforms.
    
    This CL doesn't change code behavior.
    
    BUG=616447
    R=bbudge@chromium.org
    
    Review-Url: https://codereview.chromium.org/2087923003
    Cr-Commit-Position: refs/heads/master@{#401268}

--- ppapi/proxy/ppb_testing_proxy.cc
+++ ppapi/proxy/ppb_testing_proxy.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
 #include "ppapi/c/private/ppb_testing_private.h"
 #include "ppapi/proxy/enter_proxy.h"
 #include "ppapi/proxy/plugin_dispatcher.h"
@@ -58,7 +59,7 @@ void RunMessageLoop(PP_Instance instance) {
       base::MessageLoop::current());
   CHECK(PpapiGlobals::Get()->GetMainThreadMessageLoop()->
       BelongsToCurrentThread());
-  base::MessageLoop::current()->Run();
+  base::RunLoop().Run();
 }
 
 void QuitMessageLoop(PP_Instance instance) {
