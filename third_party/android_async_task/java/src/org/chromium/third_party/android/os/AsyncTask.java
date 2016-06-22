876af7332c488c69390dc19c7931eb8800813f06
wnwen <wnwen@chromium.org>
2016-06-22 07:48:56 -0700

    Configure build rules for third_party/android_async_task.
    Original CL by hartmanng@: https://codereview.chromium.org/1702133003/
    
    Properly configuring our own AsyncTask to be used instead of the one
    provided by the os.
    
    BUG=542151
    
    Review-Url: https://codereview.chromium.org/2086093002
    Cr-Commit-Position: refs/heads/master@{#401279}

--- third_party/android_async_task/java/src/org/chromium/third_party/android/os/AsyncTask.java
+++ third_party/android_async_task/java/src/org/chromium/third_party/android/os/AsyncTask.java
@@ -15,10 +15,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * limitations under the License.
  */
 
-package android.os;
+package org.chromium.third_party.android.os;
 
-import android.annotation.MainThread;
-import android.annotation.WorkerThread;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Process;
+import android.support.annotation.MainThread;
+import android.support.annotation.WorkerThread;
 
 import java.util.ArrayDeque;
 import java.util.concurrent.BlockingQueue;
@@ -356,6 +361,7 @@ public abstract class AsyncTask<Params, Progress, Result> {
      * @see #onPostExecute
      * @see #publishProgress
      */
+    @SuppressWarnings("unchecked")
     @WorkerThread
     protected abstract Result doInBackground(Params... params);
 
@@ -395,7 +401,7 @@ public abstract class AsyncTask<Params, Progress, Result> {
      * @see #publishProgress
      * @see #doInBackground
      */
-    @SuppressWarnings({"UnusedDeclaration"})
+    @SuppressWarnings({"UnusedDeclaration", "unchecked"})
     @MainThread
     protected void onProgressUpdate(Progress... values) {
     }
@@ -547,6 +553,7 @@ public abstract class AsyncTask<Params, Progress, Result> {
      * @see #executeOnExecutor(java.util.concurrent.Executor, Object[])
      * @see #execute(Runnable)
      */
+    @SuppressWarnings("unchecked")
     @MainThread
     public final AsyncTask<Params, Progress, Result> execute(Params... params) {
         return executeOnExecutor(sDefaultExecutor, params);
@@ -585,6 +592,7 @@ public abstract class AsyncTask<Params, Progress, Result> {
      *
      * @see #execute(Object[])
      */
+    @SuppressWarnings("unchecked")
     @MainThread
     public final AsyncTask<Params, Progress, Result> executeOnExecutor(Executor exec,
             Params... params) {
@@ -637,6 +645,7 @@ public abstract class AsyncTask<Params, Progress, Result> {
      * @see #onProgressUpdate
      * @see #doInBackground
      */
+    @SuppressWarnings("unchecked")
     @WorkerThread
     protected final void publishProgress(Progress... values) {
         if (!isCancelled()) {
@@ -679,7 +688,7 @@ public abstract class AsyncTask<Params, Progress, Result> {
         Params[] mParams;
     }
 
-    @SuppressWarnings({"RawUseOfParameterizedType"})
+    @SuppressWarnings({"RawUseOfParameterizedType", "unchecked"})
     private static class AsyncTaskResult<Data> {
         final AsyncTask mTask;
         final Data[] mData;
