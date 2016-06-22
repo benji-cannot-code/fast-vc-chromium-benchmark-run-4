c20d012146d2787404017b4553e2cffe0b753164
ramya.v <ramya.v@samsung.com>
2016-06-22 06:16:32 -0700

    Shorthand serialization should check for var in longhands.
    If any subproperty of a shorthand has been set to a variable
    reference, shorthand is serialized as empty string.
    
    BUG=616341
    
    Review-Url: https://codereview.chromium.org/2083703005
    Cr-Commit-Position: refs/heads/master@{#401267}

--- third_party/WebKit/Source/core/css/StylePropertySerializer.cpp
+++ third_party/WebKit/Source/core/css/StylePropertySerializer.cpp
@@ -401,6 +401,8 @@ String StylePropertySerializer::commonShorthandChecks(const StylePropertyShortha
         // TODO(timloh): This should also check unset
         if (value.isInheritedValue())
             return emptyString();
+        if (value.isVariableReferenceValue())
+            return emptyString();
     }
 
     return String();
