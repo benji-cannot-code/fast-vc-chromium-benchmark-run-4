9a8fb980c69ff08c19c7c9bb4d64e06ca99ffb22
jamescook <jamescook@chromium.org>
2016-06-22 16:56:56 -0700

    mash: Optionally set bubble widget parent container via delegate
    Use the delegate mechanism on ash to support both aura and mus.
    
    This eliminates aura::Window usage in SystemTrayBubble and WebNotificationTray,
    which is step toward converting TrayBubbleView to wm common types.
    
    I didn't eliminate BubbleDialogDelegateView::parent_window_ because that
    mechanism is used in ~30 places inside //chrome.
    
    BUG=619636
    TEST=ash_unittests, views_unittests, manually check that system tray bubbles
    still open/close propertly
    
    Review-Url: https://codereview.chromium.org/2092473002
    Cr-Commit-Position: refs/heads/master@{#401469}

--- ash/system/tray/system_tray_bubble.cc
+++ ash/system/tray/system_tray_bubble.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/system_tray.h"
 #include "ash/system/tray/tray_bubble_wrapper.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "ui/aura/window.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_animation_observer.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
@@ -184,7 +183,8 @@ void SystemTrayBubble::UpdateView(
 void SystemTrayBubble::InitView(views::View* anchor,
                                 LoginStatus login_status,
                                 TrayBubbleView::InitParams* init_params) {
-  DCHECK(bubble_view_ == NULL);
+  DCHECK(anchor);
+  DCHECK(!bubble_view_);
 
   if (bubble_type_ == BUBBLE_TYPE_DETAILED &&
       init_params->max_height < kDetailedBubbleMaxHeight) {
@@ -192,8 +192,9 @@ void SystemTrayBubble::InitView(views::View* anchor,
   } else if (bubble_type_ == BUBBLE_TYPE_NOTIFICATION) {
     init_params->close_on_deactivate = false;
   }
-  bubble_view_ = TrayBubbleView::Create(
-      tray_->GetBubbleWindowContainer(), anchor, tray_, init_params);
+  // The TrayBubbleView will use |anchor| and |tray_| to determine the parent
+  // container for the bubble.
+  bubble_view_ = TrayBubbleView::Create(anchor, tray_, init_params);
   bubble_view_->set_adjust_if_offscreen(false);
   CreateItemViews(login_status);
 
