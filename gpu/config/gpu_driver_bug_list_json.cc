079121647fe528b58ef46e15f600371c21a33861
zmo <zmo@chromium.org>
2016-06-22 11:09:28 -0700

    The disable_seamless_cube_map workaround should only apply to primary Intel.
    On Mac retina where Intel is integrated and AMD/NVIDIA is discrete and is what
    is used for WebGL, this workaround should not apply.
    
    (This only affects WebGL2 on mac, so skip general try/cq bots)
    
    BUG=606074,597794
    TEST=mac gpu bots
    R=bajones@chromium.org
    NOTRY=true
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2081813002
    Cr-Commit-Position: refs/heads/master@{#401344}

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -20,7 +20,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "8.71",
+  "version": "8.72",
   "entries": [
     {
       "id": 1,
@@ -1622,6 +1622,7 @@ LONG_STRING_CONST(
         "type": "macosx"
       },
       "vendor_id": "0x8086",
+      "multi_gpu_category": "primary",
       "features": [
         "disable_texture_cube_map_seamless"
       ]
