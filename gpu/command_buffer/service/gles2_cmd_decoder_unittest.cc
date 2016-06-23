fddb8d084f53718cc69d918db5629a79adea441c
erikchen <erikchen@chromium.org>
2016-06-22 22:38:12 -0700

    Implement new behavior for DescheduleUntilFinishedCHROMIUM.
    Previously, the command immediately descheduled the command executor until all
    previously issued work had completed. This prevents pipelining of CPU-bound
    decoding and GPU-bound drawing.
    
    The new behavior deschedules the command executor until all work issued prior to
    the previous call to DescheduleUntilFinishedCHROMIUM has completed.
    
    BUG=611805
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2096503002
    Cr-Commit-Position: refs/heads/master@{#401544}

--- gpu/command_buffer/service/gles2_cmd_decoder_unittest.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_unittest.cc
@@ -1719,12 +1719,16 @@ class GLES2DecoderDescheduleUntilFinishedTest : public GLES2DecoderTest {
                    base::Unretained(this)));
 
     EXPECT_CALL(*gl_, FenceSync(GL_SYNC_GPU_COMMANDS_COMPLETE, 0))
-        .Times(1)
+        .Times(2)
         .WillOnce(Return(sync_service_id_))
+        .WillOnce(Return(sync_service_id2_))
         .RetiresOnSaturation();
     EXPECT_CALL(*gl_, IsSync(sync_service_id_)).WillRepeatedly(Return(GL_TRUE));
-    EXPECT_CALL(*gl_, Flush()).RetiresOnSaturation();
+    EXPECT_CALL(*gl_, Flush()).Times(2).RetiresOnSaturation();
     EXPECT_CALL(*gl_, DeleteSync(sync_service_id_))
+        .Times(1)
+        .RetiresOnSaturation();
+    EXPECT_CALL(*gl_, DeleteSync(sync_service_id2_))
         .Times(1)
         .RetiresOnSaturation();
   }
@@ -1740,6 +1744,7 @@ class GLES2DecoderDescheduleUntilFinishedTest : public GLES2DecoderTest {
   int deschedule_until_finished_callback_count_ = 0;
   int reschedule_after_finished_callback_count_ = 0;
   GLsync sync_service_id_ = reinterpret_cast<GLsync>(0x15);
+  GLsync sync_service_id2_ = reinterpret_cast<GLsync>(0x15);
 };
 
 TEST_P(GLES2DecoderDescheduleUntilFinishedTest, AlreadySignalled) {
@@ -1753,6 +1758,10 @@ TEST_P(GLES2DecoderDescheduleUntilFinishedTest, AlreadySignalled) {
   EXPECT_EQ(error::kNoError, ExecuteCmd(cmd));
   EXPECT_EQ(0, deschedule_until_finished_callback_count_);
   EXPECT_EQ(0, reschedule_after_finished_callback_count_);
+
+  EXPECT_EQ(error::kNoError, ExecuteCmd(cmd));
+  EXPECT_EQ(0, deschedule_until_finished_callback_count_);
+  EXPECT_EQ(0, reschedule_after_finished_callback_count_);
 }
 
 TEST_P(GLES2DecoderDescheduleUntilFinishedTest, NotYetSignalled) {
@@ -1763,6 +1772,10 @@ TEST_P(GLES2DecoderDescheduleUntilFinishedTest, NotYetSignalled) {
 
   cmds::DescheduleUntilFinishedCHROMIUM cmd;
   cmd.Init();
+  EXPECT_EQ(error::kNoError, ExecuteCmd(cmd));
+  EXPECT_EQ(0, deschedule_until_finished_callback_count_);
+  EXPECT_EQ(0, reschedule_after_finished_callback_count_);
+
   EXPECT_EQ(error::kDeferLaterCommands, ExecuteCmd(cmd));
   EXPECT_EQ(1, deschedule_until_finished_callback_count_);
   EXPECT_EQ(0, reschedule_after_finished_callback_count_);
