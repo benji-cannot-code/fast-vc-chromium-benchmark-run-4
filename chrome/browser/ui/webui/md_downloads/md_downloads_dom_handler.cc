aa5b1080b24d065f728134eaef58669d3b5ad6f0
dbeam <dbeam@chromium.org>
2016-06-23 09:58:02 -0700

    MD Downloads: "Clear All" should remove dangerous downloads for good
    R=asanka@chromium.org
    BUG=622362
    
    Review-Url: https://codereview.chromium.org/2087043004
    Cr-Commit-Position: refs/heads/master@{#401633}

--- chrome/browser/ui/webui/md_downloads/md_downloads_dom_handler.cc
+++ chrome/browser/ui/webui/md_downloads/md_downloads_dom_handler.cc
@@ -202,9 +202,7 @@ void MdDownloadsDOMHandler::HandleSaveDangerous(const base::ListValue* args) {
 void MdDownloadsDOMHandler::HandleDiscardDangerous(
     const base::ListValue* args) {
   CountDownloadsDOMEvents(DOWNLOADS_DOM_EVENT_DISCARD_DANGEROUS);
-  content::DownloadItem* file = GetDownloadByValue(args);
-  if (file)
-    file->Remove();
+  RemoveDownloadInArgs(args);
 }
 
 void MdDownloadsDOMHandler::HandleShow(const base::ListValue* args) {
@@ -233,13 +231,7 @@ void MdDownloadsDOMHandler::HandleRemove(const base::ListValue* args) {
     return;
 
   CountDownloadsDOMEvents(DOWNLOADS_DOM_EVENT_REMOVE);
-  content::DownloadItem* file = GetDownloadByValue(args);
-  if (!file)
-    return;
-
-  DownloadVector downloads;
-  downloads.push_back(file);
-  RemoveDownloads(downloads);
+  RemoveDownloadInArgs(args);
 }
 
 void MdDownloadsDOMHandler::HandleUndo(const base::ListValue* args) {
@@ -306,6 +298,12 @@ void MdDownloadsDOMHandler::RemoveDownloads(const DownloadVector& to_remove) {
   IdSet ids;
 
   for (auto* download : to_remove) {
+    if (download->IsDangerous()) {
+      // Don't allow users to revive dangerous downloads; just nuke 'em.
+      download->Remove();
+      continue;
+    }
+
     DownloadItemModel item_model(download);
     if (!item_model.ShouldShowInShelf() ||
         download->GetState() == content::DownloadItem::IN_PROGRESS) {
@@ -427,4 +425,14 @@ void MdDownloadsDOMHandler::CheckForRemovedFiles() {
     GetMainNotifierManager()->CheckForHistoryFilesRemoval();
   if (GetOriginalNotifierManager())
     GetOriginalNotifierManager()->CheckForHistoryFilesRemoval();
+}
+
+void MdDownloadsDOMHandler::RemoveDownloadInArgs(const base::ListValue* args) {
+  content::DownloadItem* file = GetDownloadByValue(args);
+  if (!file)
+    return;
+
+  DownloadVector downloads;
+  downloads.push_back(file);
+  RemoveDownloads(downloads);
 }
--- chrome/browser/ui/webui/md_downloads/md_downloads_dom_handler_unittest.cc
+++ chrome/browser/ui/webui/md_downloads/md_downloads_dom_handler_unittest.cc
@@ -5,7 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/md_downloads/md_downloads_dom_handler.h"
 
+#include <vector>
+
+#include "chrome/browser/download/download_item_model.h"
 #include "chrome/test/base/testing_profile.h"
+#include "content/public/test/mock_download_item.h"
 #include "content/public/test/mock_download_manager.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "content/public/test/test_web_ui.h"
@@ -21,6 +25,8 @@ class TestMdDownloadsDOMHandler : public MdDownloadsDOMHandler {
       : MdDownloadsDOMHandler(download_manager, web_ui) {}
 
   using MdDownloadsDOMHandler::set_web_ui;
+  using MdDownloadsDOMHandler::FinalizeRemovals;
+  using MdDownloadsDOMHandler::RemoveDownloads;
 };
 
 }  // namespace
@@ -67,4 +73,43 @@ TEST_F(MdDownloadsDOMHandlerTest, HandleGetDownloads) {
   EXPECT_EQ(1U, web_ui()->call_data().size());
   EXPECT_EQ("downloads.Manager.insertItems",
             web_ui()->call_data()[0]->function_name());
+}
+
+TEST_F(MdDownloadsDOMHandlerTest, ClearAll) {
+  std::vector<content::DownloadItem*> downloads;
+
+  // Safe, in-progress items should be passed over.
+  testing::StrictMock<content::MockDownloadItem> in_progress;
+  EXPECT_CALL(in_progress, IsDangerous()).WillOnce(testing::Return(false));
+  EXPECT_CALL(in_progress, GetState()).WillOnce(
+      testing::Return(content::DownloadItem::IN_PROGRESS));
+  downloads.push_back(&in_progress);
+
+  // Dangerous items should be removed (regardless of state).
+  testing::StrictMock<content::MockDownloadItem> dangerous;
+  EXPECT_CALL(dangerous, IsDangerous()).WillOnce(testing::Return(true));
+  EXPECT_CALL(dangerous, Remove());
+  downloads.push_back(&dangerous);
+
+  // Completed items should be marked as hidden from the shelf.
+  testing::StrictMock<content::MockDownloadItem> completed;
+  EXPECT_CALL(completed, IsDangerous()).WillOnce(testing::Return(false));
+  EXPECT_CALL(completed, GetState()).WillOnce(
+      testing::Return(content::DownloadItem::COMPLETE));
+  EXPECT_CALL(completed, GetId()).WillOnce(testing::Return(1));
+  EXPECT_CALL(completed, UpdateObservers());
+  downloads.push_back(&completed);
+
+  ASSERT_TRUE(DownloadItemModel(&completed).ShouldShowInShelf());
+
+  TestMdDownloadsDOMHandler handler(manager(), web_ui());
+  handler.RemoveDownloads(downloads);
+
+  // Ensure |completed| has been "soft removed" (i.e. can be revived).
+  EXPECT_FALSE(DownloadItemModel(&completed).ShouldShowInShelf());
+
+  // Make sure |completed| actually get removed when removals are "finalized".
+  EXPECT_CALL(*manager(), GetDownload(1)).WillOnce(testing::Return(&completed));
+  EXPECT_CALL(completed, Remove());
+  handler.FinalizeRemovals();
 }
