49d893ae4dc166af8d1ed3b04ccdf4b6bec9e8d8
asanka <asanka@chromium.org>
2016-06-23 14:33:40 -0700

    Neutralize dangerous subresource files during Save Page.
    Downloading a complete page using "Save as..." can result in downloading
    hundreds of subresources. The user often isn't interested in accessing
    individual resources directly while they are on disk. In addition,
    scanning hundreds of files during a single save page operation isn't
    currently practical.
    
    In order to mitigate the potential risk of leaving dangerous files
    around on the users' filesystem, this CL renames known dangerous files
    with an additional ".download" extension. I.e. A subresource named
    foo.exe would be saved as foo.exe.download.
    
    The code review includes lists of file types that are known to be
    affected by this change. Notable file types include .js, .swf, and
    .class. As a side-effect of the rename, they will not receive the
    correct MIME type when loaded via a file:// URL. The saved page should
    still function correctly even with the renamed resources.
    
    R=nparker@chromium.org, jam@chromium.org
    BUG=599224
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2060923002
    Cr-Commit-Position: refs/heads/master@{#401729}

--- chrome/browser/download/chrome_download_manager_delegate.cc
+++ chrome/browser/download/chrome_download_manager_delegate.cc
@@ -43,6 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/features.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/common/safe_browsing/file_type_policies.h"
+#include "chrome/grit/generated_resources.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_member.h"
 #include "components/prefs/pref_service.h"
@@ -52,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/page_navigator.h"
 #include "net/base/filename_util.h"
 #include "net/base/mime_util.h"
+#include "ui/base/l10n/l10n_util.h"
 
 #if BUILDFLAG(ANDROID_JAVA_UI)
 #include "chrome/browser/android/download/chrome_download_manager_overwrite_infobar_delegate.h"
@@ -431,6 +434,20 @@ void ChromeDownloadManagerDelegate::ChooseSavePath(
       can_save_as_complete,
       download_prefs_.get(),
       callback);
+}
+
+void ChromeDownloadManagerDelegate::SanitizeSavePackageResourceName(
+    base::FilePath* filename) {
+  safe_browsing::FileTypePolicies* file_type_policies =
+      safe_browsing::FileTypePolicies::GetInstance();
+
+  if (file_type_policies->GetFileDangerLevel(*filename) ==
+      safe_browsing::DownloadFileType::NOT_DANGEROUS)
+    return;
+
+  base::FilePath default_filename = base::FilePath::FromUTF8Unsafe(
+      l10n_util::GetStringUTF8(IDS_DEFAULT_DOWNLOAD_FILENAME));
+  *filename = filename->AddExtension(default_filename.BaseName().value());
 }
 
 void ChromeDownloadManagerDelegate::OpenDownloadUsingPlatformHandler(
