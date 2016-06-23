63a6898b3dad52edd123ca66c295b9b41e4888ba
tbansal <tbansal@chromium.org>
2016-06-23 12:49:38 -0700

    Use html page for Lo-Fi integration test
    Chrome no longer sends q=low for main frame requests.
    Instead, use an html page with an embedded image for testing.
    
    Tested locally and verified that the integration test now
    passes.
    
    BUG=622771
    
    Review-Url: https://codereview.chromium.org/2090413002
    Cr-Commit-Position: refs/heads/master@{#401689}

--- tools/chrome_proxy/integration_tests/chrome_proxy_pagesets/lo_fi.py
+++ tools/chrome_proxy/integration_tests/chrome_proxy_pagesets/lo_fi.py
@@ -24,7 +24,7 @@ class LoFiStorySet(story.StorySet):
     super(LoFiStorySet, self).__init__()
 
     urls_list = [
-      'http://check.googlezip.net/lofi.png',
+      'http://check.googlezip.net/cacheable/test.html',
     ]
 
     for url in urls_list:
