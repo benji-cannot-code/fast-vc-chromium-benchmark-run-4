13fb26068643a4ee0a2df59bb4e3cd76302c59ca
rjkroege <rjkroege@chromium.org>
2016-06-23 15:13:24 -0700

    Add StructTraits for AcceleratedWidget instances.
    Provide StructTraits for sending AcceleratedWidgets between
    processes.
    
    BUG=620927
    
    Review-Url: https://codereview.chromium.org/2071163002
    Cr-Commit-Position: refs/heads/master@{#401742}

--- ui/gfx/mojo/struct_traits_unittest.cc
+++ ui/gfx/mojo/struct_traits_unittest.cc
@@ -7,12 +7,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/mojo/traits_test_service.mojom.h"
+#include "ui/gfx/native_widget_types.h"
 #include "ui/gfx/selection_bound.h"
 #include "ui/gfx/transform.h"
 
 namespace gfx {
 
 namespace {
+
+gfx::AcceleratedWidget castToAcceleratedWidget(int i) {
+#if defined(USE_OZONE) || defined(USE_X11)
+  return static_cast<gfx::AcceleratedWidget>(i);
+#else
+  return reinterpret_cast<gfx::AcceleratedWidget>(i);
+#endif
+}
 
 class StructTraitsTest : public testing::Test, public mojom::TraitsTestService {
  public:
@@ -32,6 +41,12 @@ class StructTraitsTest : public testing::Test, public mojom::TraitsTestService {
 
   void EchoTransform(const Transform& t,
                      const EchoTransformCallback& callback) override {
+    callback.Run(t);
+  }
+
+  void EchoAcceleratedWidget(
+      const AcceleratedWidget& t,
+      const EchoAcceleratedWidgetCallback& callback) override {
     callback.Run(t);
   }
 
@@ -103,6 +118,21 @@ TEST_F(StructTraitsTest, Transform) {
   EXPECT_EQ(col2row4, output.matrix().get(3, 1));
   EXPECT_EQ(col3row4, output.matrix().get(3, 2));
   EXPECT_EQ(col4row4, output.matrix().get(3, 3));
+}
+
+// AcceleratedWidgets can only be sent between processes on X11, Ozone, Win
+#if defined(OS_WIN) || defined(USE_OZONE) || defined(USE_X11)
+#define MAYBE_AcceleratedWidget AcceleratedWidget
+#else
+#define MAYBE_AcceleratedWidget DISABLED_AcceleratedWidget
+#endif
+
+TEST_F(StructTraitsTest, MAYBE_AcceleratedWidget) {
+  gfx::AcceleratedWidget input(castToAcceleratedWidget(1001));
+  mojom::TraitsTestServicePtr proxy = GetTraitsTestProxy();
+  gfx::AcceleratedWidget output;
+  proxy->EchoAcceleratedWidget(input, &output);
+  EXPECT_EQ(input, output);
 }
 
 }  // namespace gfx
