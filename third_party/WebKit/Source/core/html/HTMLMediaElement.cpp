6edbe19476c41e071acbed634617f3ab4ea2b116
wolenetz <wolenetz@chromium.org>
2016-06-22 18:53:14 -0700

    MSE: Reset delaying-the-load-event-flag on attachment
    Currently, the earliest non-error case reset of media element's
    delaying-the-load-event-flag when MSE is attached occurs at either the
    'stalled' event scheduling or once first reaching >= HAVE_CURRENT_DATA.
    This change makes Blink compliant with recent MSE spec
    clarification [1] that resets this flag immediately upon MSE attachment.
    Non-MSE media element delaying-the-load-event-flag behavior remains
    unchanged.
    
    [1] https://github.com/w3c/media-source/pull/85
    
    TEST=http/tests/media/media-source/mediasource-attach-stops-delaying-load-event.html
    BUG=615481
    
    Review-Url: https://codereview.chromium.org/2021573002
    Cr-Commit-Position: refs/heads/master@{#401516}

--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -3622,6 +3622,7 @@ void HTMLMediaElement::setWebLayer(WebLayer* webLayer)
 
 void HTMLMediaElement::mediaSourceOpened(WebMediaSource* webMediaSource)
 {
+    setShouldDelayLoadEvent(false);
     m_mediaSource->setWebMediaSourceAndOpen(wrapUnique(webMediaSource));
 }
 
