efcc1cc73d8af0dc450dbf6528707616416568e5
donnd <donnd@chromium.org>
2016-06-23 14:54:23 -0700

    [TTS] Refactor Tap tracking and reset when switching Tabs.
    Updates the Selection Controller to move tracking the history of previous
    taps to a separate ContextualSearchTapState class. This will help keep the
    general Tap Suppression logic out of the Selection Controller.
    
    Clear the Tap State when we switch Tabs.
    
    BUG=609918
    
    Review-Url: https://codereview.chromium.org/2058643002
    Cr-Commit-Position: refs/heads/master@{#401737}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
@@ -191,6 +191,7 @@ public class ContextualSearchManager implements ContextualSearchManagementDelega
                 if (!mIsPromotingToTab && tab.getId() != lastId
                         || mActivity.getTabModelSelector().isIncognitoSelected()) {
                     hideContextualSearch(StateChangeReason.UNKNOWN);
+                    mSelectionController.onTabSelected();
                 }
             }
 
---
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchTapState.java
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.contextualsearch;
+
+/**
+ * Encapsulates the state of a recent Tap gesture; x, y position and if suppressed.
+ */
+class ContextualSearchTapState {
+    private final boolean mWasSuppressed;
+    private final float mX;
+    private final float mY;
+
+    /**
+     * Constructs a Tap at the given x,y position and indicates if the tap was suppressed or not.
+     */
+    ContextualSearchTapState(float x, float y, boolean wasSuppressed) {
+        mWasSuppressed = wasSuppressed;
+        mX = x;
+        mY = y;
+    }
+
+    /**
+     * @return The x coordinate of the Tap.
+     */
+    float x() {
+        return mX;
+    }
+
+    /**
+     * @return The y coordinate of the Tap.
+     */
+    float y() {
+        return mY;
+    }
+
+    /**
+     * @return Whether this Tap was suppressed.
+     */
+    boolean wasSuppressed() {
+        return mWasSuppressed;
+    }
+}
