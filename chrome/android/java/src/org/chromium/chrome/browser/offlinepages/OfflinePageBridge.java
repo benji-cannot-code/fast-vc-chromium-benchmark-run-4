deb169fb0c3a8b8c9fd1b938b87e7658b74a385e
fgorski <fgorski@chromium.org>
2016-06-23 09:52:59 -0700

    [Offline pages] Adding junit tests to OfflinePageUtils
    * Takes advantage of Tab.java no longer being final
    * Adds a test for OfflinePageUtils#saveBookmarkOffline
    
    BUG=594252
    
    Review-Url: https://codereview.chromium.org/2080013002
    Cr-Commit-Position: refs/heads/master@{#401631}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/OfflinePageBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/OfflinePageBridge.java
@@ -403,6 +403,11 @@ public class OfflinePageBridge {
         });
     }
 
+    @VisibleForTesting
+    static void setOfflineBookmarksEnabledForTesting(boolean enabled) {
+        sOfflineBookmarksEnabled = enabled;
+    }
+
     @CalledByNative
     void offlinePageModelLoaded() {
         mIsNativeOfflinePageModelLoaded = true;
