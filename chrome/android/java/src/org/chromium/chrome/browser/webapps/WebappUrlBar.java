af3dccd8baefb38b44c6980ae9330667167bd849
palmer <palmer@chromium.org>
2016-06-22 18:51:04 -0700

    [android] New origin security indicator icons.
    For the Omnibox.
    
    BUG=604520
    
    Review-Url: https://codereview.chromium.org/2063823005
    Cr-Commit-Position: refs/heads/master@{#401513}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappUrlBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappUrlBar.java
@@ -24,6 +24,7 @@ import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.omnibox.LocationBarLayout;
 import org.chromium.chrome.browser.util.UrlUtilities;
+import org.chromium.ui.base.DeviceFormFactor;
 
 import java.net.URI;
 
@@ -148,7 +149,9 @@ public class WebappUrlBar extends FrameLayout implements View.OnLayoutChangeList
     }
 
     private void updateSecurityIcon(int securityLevel) {
-        mCurrentIconResource = LocationBarLayout.getSecurityIconResource(securityLevel, false);
+        boolean isSmallDevice = !DeviceFormFactor.isTablet(getContext());
+        mCurrentIconResource =
+                LocationBarLayout.getSecurityIconResource(securityLevel, isSmallDevice);
 
         if (mCurrentIconResource != 0 && mIconResourceWidths.get(mCurrentIconResource, -1) == -1) {
             Drawable icon = ApiCompatibilityUtils.getDrawable(getResources(), mCurrentIconResource);
