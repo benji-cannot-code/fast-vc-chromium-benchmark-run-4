a89614cae5877d7280f263161c72b435fb3af1ee
dtseng <dtseng@chromium.org>
2016-06-23 14:25:07 -0700

    Reland: Make ChromeVox Next a setting in options page
    Original cl:
    https://codereview.chromium.org/2079073002/
    
    The flakeyness seen in the tests is fixed by the change to automation_custom_bindings.js.
    
    The child tree id callback can be called when the root automation node has already been attached. Natively, we send the child tree id whenever we see a child tree id within tree data which leads possibly to triggering the entire code path multiple times for the same tree.
    
    The reason why this flakeyness appears now is because
    Background depends on cvox.ChromeVoxBackground
    explicitly via goog.require. This is correct as ChromeVox Next (Background) needs some modules from Classic like prefs, keymaps, etc to be instantitated and ready.
    
    TBR=dmazzoni@chromium.org
    BUG=618094, 618092, 618090
    
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2093753002
    Cr-Commit-Position: refs/heads/master@{#401721}

--- chrome/renderer/resources/extensions/automation_custom_bindings.js
+++ chrome/renderer/resources/extensions/automation_custom_bindings.js
@@ -211,6 +211,10 @@ automationInternal.onChildTreeID.addListener(function(treeID,
   var subroot = AutomationRootNode.get(childTreeID);
   if (!subroot) {
     automationUtil.storeTreeCallback(childTreeID, function(root) {
+      // Return early if the root has already been attached.
+      if (root.parent)
+        return;
+
       privates(root).impl.setHostNode(node);
 
       if (root.docLoaded)
@@ -334,4 +338,3 @@ forEach(schema, function(k, v) {
 });
 
 exports.$set('binding', binding);
-
