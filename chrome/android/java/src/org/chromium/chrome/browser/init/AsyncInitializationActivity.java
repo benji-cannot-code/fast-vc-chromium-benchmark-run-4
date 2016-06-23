358c48b67f7d190c57dca93f6319f75144e792b2
lizeb <lizeb@chromium.org>
2016-06-23 08:20:26 -0700

    customtabs: Don't create an extra child process when one is already there.
    When a CustomTabActivity is created, the normal initialization sequence
    in AsyncInitializationActivity allocates a new child connection. This
    asks Android to start a new isolated process. When a spare renderer or a
    prerender is used, this is useless and wasteful. Even though this mostly
    does not happen on the main thread, child process binding usually takes
    >150ms on Android.
    Removing this overhead saves memory (a few MB of PSS) and CPU time, and
    should speed up Custom Tabs startup, especially on low-end devices.
    
    BUG=607560
    
    Review-Url: https://codereview.chromium.org/2087043003
    Cr-Commit-Position: refs/heads/master@{#401609}

--- chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
@@ -25,6 +25,7 @@ import android.view.WindowManager;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.TraceEvent;
+import org.chromium.base.VisibleForTesting;
 import org.chromium.base.library_loader.LoaderErrors;
 import org.chromium.base.library_loader.ProcessInitException;
 import org.chromium.chrome.browser.ChromeApplication;
@@ -107,7 +108,13 @@ public abstract class AsyncInitializationActivity extends AppCompatActivity impl
         // Kick off long running IO tasks that can be done in parallel.
         mNativeInitializationController = new NativeInitializationController(this, this);
         initializeChildProcessCreationParams();
-        mNativeInitializationController.startBackgroundTasks();
+        mNativeInitializationController.startBackgroundTasks(shouldAllocateChildConnection());
+    }
+
+    /** Controls the parameter of {@link NativeInitializationController#startBackgroundTasks()}.*/
+    @VisibleForTesting
+    public boolean shouldAllocateChildConnection() {
+        return true;
     }
 
     /**
