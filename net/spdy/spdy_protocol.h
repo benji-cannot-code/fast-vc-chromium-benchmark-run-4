6cd669143f1388d4cd10accd2654894a01833cef
bnc <bnc@chromium.org>
2016-06-23 16:28:04 -0700

    Remove SpdyFrameWithHeaderBlockIR::mutable_header_block().
    Also remove MaybeAddVersionHeader(SpdyFrameWithHeaderBlockIR*), use
    MaybeAddVersionHeader(SpdyHeaderBlock*) instead.
    
    Review-Url: https://codereview.chromium.org/2090403002
    Cr-Commit-Position: refs/heads/master@{#401754}

--- net/spdy/spdy_protocol.h
+++ net/spdy/spdy_protocol.h
@@ -726,7 +726,6 @@ class NET_EXPORT_PRIVATE SpdyFrameWithHeaderBlockIR
   void SetHeader(base::StringPiece name, base::StringPiece value) {
     header_block_[name] = value;
   }
-  SpdyHeaderBlock* mutable_header_block() { return &header_block_; }
 
  protected:
   SpdyFrameWithHeaderBlockIR(SpdyStreamId stream_id,
