06b2ecfd011866c2f1f6cbf18c27c3ab55e865f3
chrishtr <chrishtr@chromium.org>
2016-06-23 13:45:32 -0700

    Initial implementation of GeometryMapper
    Design document: https://docs.google.com/document/d/1WZKlOSUK4XI0Le0fgCsyUTVw0dTwutZXGWwzlHXewiU/edit
    
    This patch implements the GeometryMapper class, with these methods:
    
    LocalToVisibleRectInAncestorSpace: conceptually equivalent to LayoutObject::mapToVisualRectInAncestorSpace
    LocalToAncestorRect: conceptually equivalent to LayoutObject::mapLocalToAncestor
    AncestorToLocalRect: conceptually equivalent to LayoutObject::mapAncestorToLocal
    
    Over time, this class plus the property tree builder will replace most of the use cases
    of the above referenced methods.
    
    Review-Url: https://codereview.chromium.org/1973343003
    Cr-Commit-Position: refs/heads/master@{#401710}

---
+++ third_party/WebKit/Source/platform/graphics/paint/PropertyTreeState.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PropertyTreeState_h
+#define PropertyTreeState_h
+
+#include "wtf/HashFunctions.h"
+#include "wtf/HashTraits.h"
+
+namespace blink {
+
+class TransformPaintPropertyNode;
+class ClipPaintPropertyNode;
+class EffectPaintPropertyNode;
+
+// Represents the combination of transform, clip and effect nodes for a particular coordinate space.
+// See GeometryMapper.
+struct PropertyTreeState {
+    PropertyTreeState(
+        TransformPaintPropertyNode* transform,
+        ClipPaintPropertyNode* clip,
+        EffectPaintPropertyNode* effect)
+    : transform(transform), clip(clip), effect(effect) {}
+
+    TransformPaintPropertyNode* transform;
+    ClipPaintPropertyNode* clip;
+    EffectPaintPropertyNode* effect;
+};
+
+} // namespace blink
+
+#endif // PropertyTreeState_h
