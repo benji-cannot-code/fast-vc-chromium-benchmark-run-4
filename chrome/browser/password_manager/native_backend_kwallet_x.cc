178d230b888a42b4746f3a2110ed89c3b365193d
j.isorce <j.isorce@samsung.com>
2016-06-23 07:30:44 -0700

    Fix build error with gcc 4.9.3
    native_backend_kwallet_x.cc:391:1:
      error: control reaches end of non-void function [-Werror=return-type]
    
    Regression introduced by https://codereview.chromium.org/2057123002
    
    BUG=602624
    
    R=vabr@chromium.org
    
    Review-Url: https://codereview.chromium.org/2090343002
    Cr-Commit-Position: refs/heads/master@{#401601}

--- chrome/browser/password_manager/native_backend_kwallet_x.cc
+++ chrome/browser/password_manager/native_backend_kwallet_x.cc
@@ -389,6 +389,9 @@ NativeBackendKWallet::InitResult NativeBackendKWallet::InitWallet() {
     case KWalletDBus::Error::SUCCESS:
       return INIT_SUCCESS;
   }
+
+  NOTREACHED();
+  return PERMANENT_FAIL;
 }
 
 password_manager::PasswordStoreChangeList NativeBackendKWallet::AddLogin(
