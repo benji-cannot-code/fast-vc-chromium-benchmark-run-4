6ed7f3216283cbe8954179389f34475cd4a061d0
shrike <shrike@chromium.org>
2016-06-23 11:50:32 -0700

    [Mac][Material Design] Adjust (i) and lock Omnibox icons.
    This cl	brings the (i) icon into spec, and adjusts the position of the
    lock icon. The same lock icon is used in https-valid and ev modes but
    appears 1px too low in evcert mode on Retina machines. The issue is the
    text label that appears next to the lock is 1px	too high - this cl
    fixes the position of the text label as well.
    
    R=tapted@chromium.org
    BUG=621277
    
    Review-Url: https://codereview.chromium.org/2077743005
    Cr-Commit-Position: refs/heads/master@{#401666}

--- chrome/browser/ui/cocoa/location_bar/bubble_decoration.mm
+++ chrome/browser/ui/cocoa/location_bar/bubble_decoration.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/ext/skia_utils_mac.h"
 #import "ui/base/cocoa/nsview_additions.h"
 #include "ui/base/material_design/material_design_controller.h"
+#include "ui/gfx/scoped_ns_graphics_context_save_gstate_mac.h"
 
 namespace {
 
@@ -39,7 +40,7 @@ const CGFloat kBackgroundYInset = 4.0;
 
 }  // namespace
 
-BubbleDecoration::BubbleDecoration() : baseline_offset_(0) {
+BubbleDecoration::BubbleDecoration() : retina_baseline_offset_(0) {
   attributes_.reset([[NSMutableDictionary alloc] init]);
   [attributes_ setObject:LocationBarDecoration::GetFont()
                   forKey:NSFontAttributeName];
@@ -133,8 +134,16 @@ void BubbleDecoration::DrawInFrame(NSRect frame, NSView* control_view) {
   if (label_) {
     NSRect textRect = frame;
     textRect.origin.x = textOffset;
-    textRect.origin.y += baseline_offset_;
     textRect.size.width = NSMaxX(decoration_frame) - NSMinX(textRect);
+    // Transform the coordinate system to adjust the baseline on Retina. This is
+    // the only way to get fractional adjustments.
+    gfx::ScopedNSGraphicsContextSaveGState saveGraphicsState;
+    CGFloat lineWidth = [control_view cr_lineWidth];
+    if (lineWidth < 1) {
+      NSAffineTransform* transform = [NSAffineTransform transform];
+      [transform translateXBy:0 yBy:retina_baseline_offset_];
+      [transform concat];
+    }
     DrawLabel(label_, attributes_, textRect);
   }
 }
@@ -180,6 +189,6 @@ void BubbleDecoration::SetFont(NSFont* font) {
   [attributes_ setObject:font forKey:NSFontAttributeName];
 }
 
-void BubbleDecoration::SetBaselineOffset(CGFloat offset) {
-  baseline_offset_ = offset;
+void BubbleDecoration::SetRetinaBaselineOffset(CGFloat offset) {
+  retina_baseline_offset_ = offset;
 }
