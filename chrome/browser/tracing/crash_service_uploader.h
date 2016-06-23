b5a8ce3d82c4f87ca3b6a1df3e18c3c3cf651175
ssid <ssid@chromium.org>
2016-06-23 10:29:17 -0700

    Limit the background trials upload size to 100Kib when using mobile data
    The crash data is uploaded only if the file size was less than 10MB.
    But, this does not consider if device is running on wifi or mobile data.
    Uploading 10Mib crash files over data will be undesireable for android
    and ios users. To fix this issue, the limit is set to 100Kib if the
    connection is over data or connection type is unknown when tracing is
    running for background field trials.
    
    BUG=622047
    
    Review-Url: https://codereview.chromium.org/2085623005
    Cr-Commit-Position: refs/heads/master@{#401644}

--- chrome/browser/tracing/crash_service_uploader.h
+++ chrome/browser/tracing/crash_service_uploader.h
@@ -34,6 +34,8 @@ class TraceCrashServiceUploader : public content::TraceUploader,
 
   void SetUploadURL(const std::string& url);
 
+  void SetMaxUploadBytes(size_t max_upload_bytes);
+
   // net::URLFetcherDelegate implementation.
   void OnURLFetchComplete(const net::URLFetcher* source) override;
   void OnURLFetchUploadProgress(const net::URLFetcher* source,
@@ -82,6 +84,8 @@ class TraceCrashServiceUploader : public content::TraceUploader,
   net::URLRequestContextGetter* request_context_;
 
   std::string upload_url_;
+
+  size_t max_upload_bytes_;
 
   DISALLOW_COPY_AND_ASSIGN(TraceCrashServiceUploader);
 };
