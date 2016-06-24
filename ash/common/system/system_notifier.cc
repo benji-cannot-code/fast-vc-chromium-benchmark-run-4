ec500da68a51241611c44a0342f5efdf9892f351
dspaid <dspaid@chromium.org>
2016-06-23 23:08:52 -0700

    Add strings and notifier ID for low disk.
    Actual notifications using these strings will be added in a followup CL.
    
    BUG=622590
    
    Review-Url: https://codereview.chromium.org/2090913002
    Cr-Commit-Position: refs/heads/master@{#401825}

--- ash/common/system/system_notifier.cc
+++ ash/common/system/system_notifier.cc
@@ -40,6 +40,9 @@ const char* kAlwaysShownSystemNotifierIds[] = {
 const char* kAshSystemNotifiers[] = {
   kNotifierBluetooth,
   kNotifierDisplayResolutionChange,
+#if defined(OS_CHROMEOS)
+  kNotifierDisk,
+#endif
   kNotifierLocale,
   kNotifierMultiProfileFirstRun,
 #if defined(OS_CHROMEOS)
@@ -74,6 +77,7 @@ bool MatchSystemNotifierId(const message_center::NotifierId& notifier_id,
 const char kNotifierBattery[] = "ash.battery";
 const char kNotifierBluetooth[] = "ash.bluetooth";
 const char kNotifierDeprecatedAccelerator[] = "ash.accelerator-controller";
+const char kNotifierDisk[] = "ash.disk";
 const char kNotifierDisplay[] = "ash.display";
 const char kNotifierDisplayError[] = "ash.display.error";
 const char kNotifierDisplayResolutionChange[] = "ash.display.resolution-change";
