739d06e53f09471be112686ed9a0491bd2f4eea6
rouslan <rouslan@chromium.org>
2016-06-24 11:06:06 -0700

    Specify billing address for autofill cards in PaymentRequest.
    If a credit card has a billing address, then the billing address will
    also be passed to the merchant. The billing address specification
    redirects to the shipping address specification, so that's the format
    used in this patch.
    
    https://w3c.github.io/webpayments-methods-card/#billingaddress
    https://w3c.github.io/browser-payment-api/#paymentaddress-interface
    
    The credit card information is an "instrument detail" that's unique to
    each payment method. Therefore, it's passed through mojo as a JSON
    string.
    
    A credit card payment instrument detail now might look like this:
    
    {
      "cardholderName": "First Last",
      "cardNumber": "4111111111111111",
      "expiryMonth": "12",
      "expiryYear": "2020",
      "cardSecurityCode": "123",
      "billingAddress": {
        "country": "US",
        "region": "CA",
        "city": "Mountain View",
        "dependentLocality": "",
        "addressLine: [
          "1600 Amphitheatre Pkwy"
        ],
        "postalCode": "94043",
        "sortingCode": "",
        "languageCode": "en-US",
        "organization": "Google",
        "recipient": "First Last",
        "careOf": "",
        "phone": "(650) 253-0000"
      }
    }
    
    Additional changes in this patch:
     - Add billing addresses to several integration tests, because the plan
       is to require billing addresses until merchants have a way to specify
       that they are optional.
     - Remove total amounts from several integration tests, because total
       amounts are no longer passed to merchants in response.
    
    BUG=602663
    
    Review-Url: https://codereview.chromium.org/2097713002
    Cr-Commit-Position: refs/heads/master@{#401908}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/AutofillPaymentApp.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/AutofillPaymentApp.java
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.payments;
 
 import android.os.Handler;
+import android.text.TextUtils;
 
 import org.chromium.chrome.browser.autofill.PersonalDataManager;
+import org.chromium.chrome.browser.autofill.PersonalDataManager.AutofillProfile;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.CreditCard;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.mojom.payments.PaymentItem;
@@ -35,11 +37,17 @@ public class AutofillPaymentApp implements PaymentApp {
     @Override
     public void getInstruments(PaymentItem unusedTotal, List<PaymentItem> unusedCart,
             final InstrumentsCallback callback) {
-        List<CreditCard> cards = PersonalDataManager.getInstance().getCreditCardsToSuggest();
+        PersonalDataManager pdm = PersonalDataManager.getInstance();
+        List<CreditCard> cards = pdm.getCreditCardsToSuggest();
         final List<PaymentInstrument> instruments = new ArrayList<>(cards.size());
+
         for (int i = 0; i < cards.size(); i++) {
-            instruments.add(new AutofillPaymentInstrument(mWebContents, cards.get(i)));
+            CreditCard card = cards.get(i);
+            AutofillProfile billingAddress = TextUtils.isEmpty(card.getBillingAddressId())
+                    ? null : pdm.getProfile(card.getBillingAddressId());
+            instruments.add(new AutofillPaymentInstrument(mWebContents, card, billingAddress));
         }
+
         new Handler().post(new Runnable() {
             @Override
             public void run() {
@@ -50,7 +58,7 @@ public class AutofillPaymentApp implements PaymentApp {
 
     @Override
     public Set<String> getSupportedMethodNames() {
-        // https://w3c.github.io/browser-payment-api/specs/basic-card-payment.html#method-id
+        // https://w3c.github.io/webpayments-methods-card/#method-id
         // The spec also includes more detailed card types, e.g., "visa/credit" and "visa/debit".
         // Autofill does not distinguish between these types of cards, so they are not in the list
         // of supported method names.
--- chrome/android/java/src/org/chromium/chrome/browser/payments/AutofillPaymentInstrument.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/AutofillPaymentInstrument.java
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.payments;
 
+import android.text.TextUtils;
 import android.util.JsonWriter;
 
 import org.chromium.chrome.browser.autofill.PersonalDataManager;
+import org.chromium.chrome.browser.autofill.PersonalDataManager.AutofillProfile;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.CreditCard;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.FullCardRequestDelegate;
 import org.chromium.content_public.browser.WebContents;
@@ -19,6 +21,8 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 /**
  * The locally stored credit card payment instrument.
  */
@@ -26,19 +30,23 @@ public class AutofillPaymentInstrument
         extends PaymentInstrument implements FullCardRequestDelegate {
     private final WebContents mWebContents;
     private final CreditCard mCard;
-    private DetailsCallback mCallback;
+    @Nullable private AutofillProfile mBillingAddress;
+    @Nullable private DetailsCallback mCallback;
 
     /**
      * Builds a payment instrument for the given credit card.
      *
-     * @param webContents The web contents where PaymentRequest was invoked.
-     * @param card The autofill card that can be used for payment.
+     * @param webContents    The web contents where PaymentRequest was invoked.
+     * @param card           The autofill card that can be used for payment.
+     * @param billingAddress The optional billing address for the card.
      */
-    public AutofillPaymentInstrument(WebContents webContents, CreditCard card) {
+    public AutofillPaymentInstrument(
+            WebContents webContents, CreditCard card, @Nullable AutofillProfile billingAddress) {
         super(card.getGUID(), card.getObfuscatedNumber(), card.getName(),
                 card.getIssuerIconDrawableId());
         mWebContents = webContents;
         mCard = card;
+        mBillingAddress = billingAddress;
     }
 
     @Override
@@ -60,11 +68,43 @@ public class AutofillPaymentInstrument
         JsonWriter json = new JsonWriter(stringWriter);
         try {
             json.beginObject();
+
             json.name("cardholderName").value(card.getName());
             json.name("cardNumber").value(card.getNumber());
             json.name("expiryMonth").value(card.getMonth());
             json.name("expiryYear").value(card.getYear());
             json.name("cardSecurityCode").value(cvc);
+
+            if (mBillingAddress != null) {
+                json.name("billingAddress").beginObject();
+
+                json.name("country").value(ensureNotNull(mBillingAddress.getCountryCode()));
+                json.name("region").value(ensureNotNull(mBillingAddress.getRegion()));
+                json.name("city").value(ensureNotNull(mBillingAddress.getLocality()));
+                json.name("dependentLocality")
+                        .value(ensureNotNull(mBillingAddress.getDependentLocality()));
+
+                json.name("addressLine").beginArray();
+                String multipleLines = ensureNotNull(mBillingAddress.getStreetAddress());
+                if (!TextUtils.isEmpty(multipleLines)) {
+                    String[] lines = multipleLines.split("\n");
+                    for (int i = 0; i < lines.length; i++) {
+                        json.value(lines[i]);
+                    }
+                }
+                json.endArray();
+
+                json.name("postalCode").value(ensureNotNull(mBillingAddress.getPostalCode()));
+                json.name("sortingCode").value(ensureNotNull(mBillingAddress.getSortingCode()));
+                json.name("languageCode").value(ensureNotNull(mBillingAddress.getLanguageCode()));
+                json.name("organization").value(ensureNotNull(mBillingAddress.getCompanyName()));
+                json.name("recipient").value(ensureNotNull(mBillingAddress.getFullName()));
+                json.name("careOf").value("");
+                json.name("phone").value(ensureNotNull(mBillingAddress.getPhoneNumber()));
+
+                json.endObject();
+            }
+
             json.endObject();
         } catch (IOException e) {
             mCallback.onInstrumentDetailsError();
@@ -72,6 +112,10 @@ public class AutofillPaymentInstrument
         }
 
         mCallback.onInstrumentDetailsReady(card.getBasicCardPaymentType(), stringWriter.toString());
+    }
+
+    private static String ensureNotNull(@Nullable String value) {
+        return value == null ? "" : value;
     }
 
     @Override
