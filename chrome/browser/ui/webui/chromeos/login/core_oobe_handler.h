ace6b5b76a543174cbfdd477fd1d9682b144a764
warx <warx@chromium.org>
2016-06-23 22:50:11 -0700

    clean oobe mono audio setting as they are not used
    BUG=622959
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2096983002
    Cr-Commit-Position: refs/heads/master@{#401823}

--- chrome/browser/ui/webui/chromeos/login/core_oobe_handler.h
+++ chrome/browser/ui/webui/chromeos/login/core_oobe_handler.h
@@ -124,7 +124,6 @@ class CoreOobeHandler : public BaseScreenHandler,
   void HandleEnableVirtualKeyboard(bool enabled);
   void HandleEnableScreenMagnifier(bool enabled);
   void HandleEnableSpokenFeedback(bool /* enabled */);
-  void HandleEnableMonoAudio(bool enabled);
   void HandleInitialized();
   void HandleSkipUpdateEnrollAfterEula();
   void HandleUpdateCurrentScreen(const std::string& screen);
