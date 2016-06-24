1c0a05f08f921dee6321c6463475a81d36763080
dfalcantara <dfalcantara@chromium.org>
2016-06-24 13:51:09 -0700

    [Payments] Don't announce the radio button or credit card
    Marks the button and logo as unimportant for accessibility so that
    they're not read.  Ideally the row would be read out but because
    GridLayout has no notion of announcing rows, we either end up
    announcing the same text for all the views in the same row or just
    mark them as unimportant for accessibility.  We go with the latter
    because:
    
    * The radio button is just for the label next to it.
    
    * Short of having a way of determining what credit card is being
      displayed, we can't announce anything useful.
    
    BUG=619974
    
    Review-Url: https://codereview.chromium.org/2093623006
    Cr-Commit-Position: refs/heads/master@{#401961}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestSection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestSection.java
@@ -569,7 +569,7 @@ public abstract class PaymentRequestSection extends LinearLayout {
 
             /** Change the label for the row. */
             public void setLabel(int stringId) {
-                mLabel.setText(stringId);
+                setLabel(getContext().getString(stringId));
             }
 
             /** Change the label for the row. */
@@ -624,6 +624,7 @@ public abstract class PaymentRequestSection extends LinearLayout {
                 ApiCompatibilityUtils.setMarginEnd(buttonParams, mLargeSpacing);
                 parent.addView(view, buttonParams);
 
+                view.setImportantForAccessibility(IMPORTANT_FOR_ACCESSIBILITY_NO);
                 view.setOnClickListener(OptionSection.this);
                 return view;
             }
@@ -690,6 +691,7 @@ public abstract class PaymentRequestSection extends LinearLayout {
             private View createIcon(GridLayout parent, int rowIndex) {
                 // The icon has a pre-defined width.
                 ImageView icon = new ImageView(parent.getContext());
+                icon.setImportantForAccessibility(IMPORTANT_FOR_ACCESSIBILITY_NO);
                 icon.setBackgroundResource(R.drawable.payments_ui_logo_bg);
                 icon.setImageResource(mOption.getDrawableIconId());
                 icon.setMaxWidth(mIconMaxWidth);
