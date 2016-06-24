21e321b3511070bfee2db1e59067df48ce8cc0d8
tkent <tkent@chromium.org>
2016-06-24 02:18:57 -0700

    UseCounter: count Document::open().
    The function has very complex behavior and we have multiple bugs. We wonder how
    much it is important.
    
    TBR=isherman@chromium.org
    
    Review-Url: https://codereview.chromium.org/2098443005
    Cr-Commit-Position: refs/heads/master@{#401839}

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -1225,6 +1225,7 @@ public:
         AddEventListenerPassiveTrue = 1417,
         AddEventListenerPassiveFalse = 1418,
         CSPReferrerDirective = 1419,
+        DocumentOpen = 1420,
 
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
