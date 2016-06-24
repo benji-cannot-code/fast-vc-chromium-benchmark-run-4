77bfc8c76d886ed7a9b224981c4f13491c7333d5
thakis <thakis@chromium.org>
2016-06-24 13:41:27 -0700

    mac: Use GTM a little less.
    I've been looking at deprecation warnings in GTM, and noticed that GTM
    isn't really needed in these three files.
    
    No behavior change.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2097083003
    Cr-Commit-Position: refs/heads/master@{#401957}

--- chrome/browser/ui/cocoa/panels/panel_titlebar_view_cocoa.mm
+++ chrome/browser/ui/cocoa/panels/panel_titlebar_view_cocoa.mm
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/panels/panel_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "grit/theme_resources.h"
-#import "third_party/google_toolbox_for_mac/src/AppKit/GTMNSBezierPath+RoundRect.h"
-#import "third_party/google_toolbox_for_mac/src/AppKit/GTMNSColor+Luminance.h"
 #include "ui/base/cocoa/cocoa_base_utils.h"
 #import "ui/base/cocoa/hover_image_button.h"
 #import "ui/base/cocoa/nsview_additions.h"
--- ui/app_list/cocoa/apps_search_box_controller.mm
+++ ui/app_list/cocoa/apps_search_box_controller.mm
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/mac_util.h"
 #include "base/macros.h"
 #include "base/strings/sys_string_conversions.h"
-#import "third_party/google_toolbox_for_mac/src/AppKit/GTMNSBezierPath+RoundRect.h"
 #include "ui/app_list/app_list_menu.h"
 #include "ui/app_list/app_list_model.h"
 #include "ui/app_list/resources/grit/app_list_resources.h"
