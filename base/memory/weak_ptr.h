932f92096d0b553387eaebb9317fa5f89d0761cb
mek <mek@chromium.org>
2016-06-24 14:29:04 -0700

    Make base::WeakPtr moveable.
    Review-Url: https://codereview.chromium.org/2094873003
    Cr-Commit-Position: refs/heads/master@{#401977}

--- base/memory/weak_ptr.h
+++ base/memory/weak_ptr.h
@@ -110,9 +110,13 @@ class BASE_EXPORT WeakReference {
   };
 
   WeakReference();
-  WeakReference(const WeakReference& other);
   explicit WeakReference(const Flag* flag);
   ~WeakReference();
+
+  WeakReference(WeakReference&& other);
+  WeakReference(const WeakReference& other);
+  WeakReference& operator=(WeakReference&& other) = default;
+  WeakReference& operator=(const WeakReference& other) = default;
 
   bool is_valid() const;
 
@@ -145,6 +149,11 @@ class BASE_EXPORT WeakPtrBase {
  public:
   WeakPtrBase();
   ~WeakPtrBase();
+
+  WeakPtrBase(const WeakPtrBase& other) = default;
+  WeakPtrBase(WeakPtrBase&& other) = default;
+  WeakPtrBase& operator=(const WeakPtrBase& other) = default;
+  WeakPtrBase& operator=(WeakPtrBase&& other) = default;
 
  protected:
   explicit WeakPtrBase(const WeakReference& ref);
@@ -206,10 +215,13 @@ class WeakPtr : public internal::WeakPtrBase {
   WeakPtr(std::nullptr_t) : ptr_(nullptr) {}
 
   // Allow conversion from U to T provided U "is a" T. Note that this
-  // is separate from the (implicit) copy constructor.
+  // is separate from the (implicit) copy and move constructors.
   template <typename U>
   WeakPtr(const WeakPtr<U>& other) : WeakPtrBase(other), ptr_(other.ptr_) {
   }
+  template <typename U>
+  WeakPtr(WeakPtr<U>&& other)
+      : WeakPtrBase(std::move(other)), ptr_(other.ptr_) {}
 
   T* get() const { return ref_.is_valid() ? ptr_ : nullptr; }
 
