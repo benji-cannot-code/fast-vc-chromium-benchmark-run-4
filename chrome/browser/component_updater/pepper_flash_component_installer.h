c6edb569c6bcf27098d9666a61829d8b59003699
waffles <waffles@chromium.org>
2016-06-24 14:21:09 -0700

    Refactor flash component installer to use DefaultComponentInstaller. This enables it to use differential updates.
    BUG=601928
    
    Review-Url: https://codereview.chromium.org/2041573002
    Cr-Commit-Position: refs/heads/master@{#401973}

--- chrome/browser/component_updater/pepper_flash_component_installer.h
+++ chrome/browser/component_updater/pepper_flash_component_installer.h
@@ -6,11 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_COMPONENT_UPDATER_PEPPER_FLASH_COMPONENT_INSTALLER_H_
 #define CHROME_BROWSER_COMPONENT_UPDATER_PEPPER_FLASH_COMPONENT_INSTALLER_H_
 
-namespace base {
-class DictionaryValue;
-class Version;
-}
-
 namespace component_updater {
 
 class ComponentUpdateService;
