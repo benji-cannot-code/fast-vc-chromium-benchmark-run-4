3c844329209fec422acc69264fcb12c665503254
dfalcantara <dfalcantara@chromium.org>
2016-06-24 15:18:10 -0700

    [Payments] Refactor the autofill preferences
    * Pulls out code common to both editors into a new AutofillEditorBase.
    
    * AutofillEditorBase supports an ActionBar with a delete button and
      a pair of CANCEL/SAVE buttons that are permanently at the bottom
      of the screen.
    
    * Add a delete icon instead of the delete button for deleting entries.
    
    - Does not yet properly style the editor fields.
    
    See bug for screenshots:
    https://bugs.chromium.org/p/chromium/issues/detail?id=603635#c177
    
    BUG=603635
    
    Review-Url: https://codereview.chromium.org/2100483002
    Cr-Commit-Position: refs/heads/master@{#401995}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/EditorView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/EditorView.java
@@ -12,7 +12,6 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.design.widget.TextInputLayout;
-import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.Toolbar.OnMenuItemClickListener;
 import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.text.Editable;
@@ -197,6 +196,12 @@ public class EditorView extends AlwaysDismissedDialog
         };
     }
 
+    /** Launches the Autofill help page on top of the current Context. */
+    public static void launchAutofillHelpPage(Context context) {
+        EmbedContentViewActivity.show(
+                context, context.getString(R.string.help), HELP_URL);
+    }
+
     /**
      * Prepares the toolbar for use.
      *
@@ -204,16 +209,16 @@ public class EditorView extends AlwaysDismissedDialog
      * programmatically.  This is likely due to how we compile the support libraries.
      */
     private void prepareToolbar() {
-        Toolbar toolbar = (Toolbar) mLayout.findViewById(R.id.action_bar);
+        EditorDialogToolbar toolbar = (EditorDialogToolbar) mLayout.findViewById(R.id.action_bar);
         toolbar.setTitle(mEditorModel.getTitle());
         toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setShowDeleteMenuItem(false);
 
         // Show the help article when the user asks.
         toolbar.setOnMenuItemClickListener(new OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                EmbedContentViewActivity.show(
-                        mContext, mContext.getString(R.string.help), HELP_URL);
+                launchAutofillHelpPage(mContext);
                 return true;
             }
         });
