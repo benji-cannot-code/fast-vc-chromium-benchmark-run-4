540cdc8179820ed0e7579d887c58b69d8c0487a3
petrcermak <petrcermak@chromium.org>
2016-06-24 08:56:14 -0700

    [memory-infra] Add support for queueing memory dump requests in the browser process
    This patch changes TracingControllerImpl to queue global memory dump
    requests if another dump is in progress (instead of rejecting the
    requests). The requests are processed in a simple FIFO order.
    
    To avoid choking the controller, periodic dump requests are skipped if
    the queue already contains a (possibly non-periodic) dump request with
    the same level of detail.
    
    Rationale: This is necessary for Telemetry tests which combine periodic
    and triggered memory dumps.
    
    Note: Support for queueing global memory dump requests in child
    processes will be added in a separate patch.
    
    BUG=619935
    
    Review-Url: https://codereview.chromium.org/2067793004
    Cr-Commit-Position: refs/heads/master@{#401865}

--- content/browser/tracing/tracing_controller_impl.h
+++ content/browser/tracing/tracing_controller_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <list>
 #include <set>
 #include <string>
 #include <vector>
@@ -82,6 +83,16 @@ class TracingControllerImpl
   friend struct base::DefaultLazyInstanceTraits<TracingControllerImpl>;
   friend class TraceMessageFilter;
 
+  // The arguments and callback for an queued global memory dump request.
+  struct QueuedMemoryDumpRequest {
+    QueuedMemoryDumpRequest(
+        const base::trace_event::MemoryDumpRequestArgs& args,
+        const base::trace_event::MemoryDumpCallback& callback);
+    ~QueuedMemoryDumpRequest();
+    const base::trace_event::MemoryDumpRequestArgs args;
+    const base::trace_event::MemoryDumpCallback callback;
+  };
+
   TracingControllerImpl();
   ~TracingControllerImpl() override;
 
@@ -108,6 +119,8 @@ class TracingControllerImpl
   bool can_cancel_watch_event() const {
     return !watch_event_callback_.is_null();
   }
+
+  void PerformNextQueuedGlobalMemoryDump();
 
   // Methods for use by TraceMessageFilter.
   void AddTraceMessageFilter(TraceMessageFilter* trace_message_filter);
@@ -188,8 +201,7 @@ class TracingControllerImpl
   int pending_memory_dump_ack_count_;
   int failed_memory_dump_count_;
   TraceMessageFilterSet pending_memory_dump_filters_;
-  uint64_t pending_memory_dump_guid_;
-  base::trace_event::MemoryDumpCallback pending_memory_dump_callback_;
+  std::list<QueuedMemoryDumpRequest> queued_memory_dump_requests_;
 
   std::vector<base::trace_event::TracingAgent*> additional_tracing_agents_;
   int pending_clock_sync_ack_count_;
