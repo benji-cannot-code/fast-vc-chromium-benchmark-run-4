b2f22ab365683e3903f6bbbe316bffbac315d353
vkuzkokov <vkuzkokov@chromium.org>
2016-06-24 06:31:59 -0700

    arc: Remove android data if cloud provisioning failed.
    BUG=b/28186641
    
    Review-Url: https://codereview.chromium.org/2091483002
    Cr-Commit-Position: refs/heads/master@{#401856}

--- chrome/browser/chromeos/arc/arc_auth_service.cc
+++ chrome/browser/chromeos/arc/arc_auth_service.cc
@@ -31,11 +31,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_launcher.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_utils.h"
+#include "chrome/browser/ui/ash/multi_user/multi_user_util.h"
 #include "chrome/browser/ui/extensions/app_launch_params.h"
 #include "chrome/browser/ui/extensions/application_launch.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/chromeos_switches.h"
+#include "chromeos/cryptohome/cryptohome_parameters.h"
+#include "chromeos/dbus/dbus_thread_manager.h"
+#include "chromeos/dbus/session_manager_client.h"
 #include "components/arc/arc_bridge_service.h"
 #include "components/policy/core/browser/browser_policy_connector.h"
 #include "components/policy/core/common/cloud/device_management_service.h"
@@ -182,6 +186,15 @@ void ArcAuthService::OnAuthInstanceReady() {
       binding_.CreateInterfacePtrAndBind());
 }
 
+void ArcAuthService::OnBridgeStopped() {
+  if (!clear_required_)
+    return;
+  clear_required_ = false;
+  chromeos::DBusThreadManager::Get()->GetSessionManagerClient()->RemoveArcData(
+      cryptohome::Identification(
+          multi_user_util::GetAccountIdFromProfile(profile_)));
+}
+
 std::string ArcAuthService::GetAndResetAuthCode() {
   DCHECK(thread_checker.Get().CalledOnValidThread());
   std::string auth_code;
@@ -258,6 +271,8 @@ void ArcAuthService::OnSignInFailed(arc::mojom::ArcSignInFailureReason reason) {
 
   if (profile_->GetPrefs()->HasPrefPath(prefs::kArcSignedIn))
     profile_->GetPrefs()->SetBoolean(prefs::kArcSignedIn, false);
+  if (reason == arc::mojom::ArcSignInFailureReason::CLOUD_PROVISION_FLOW_FAIL)
+    clear_required_ = true;
   ShutdownBridgeAndShowUI(UIPage::ERROR,
                           l10n_util::GetStringUTF16(error_message_id));
 }
