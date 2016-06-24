799c2df4fd00a19d026cc01ff162c3bd1498f239
calamity <calamity@chromium.org>
2016-06-23 21:51:16 -0700

    Disable update on input field focus in chrome://site-engagement.
    This CL fixes an issue where the text fields in chrome://site-engagement
    would get defocused by an update. The update is now disabled when the
    input field is focused.
    
    BUG=524857
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2082243002
    Cr-Commit-Position: refs/heads/master@{#401816}

--- chrome/browser/resources/engagement/site_engagement.js
+++ chrome/browser/resources/engagement/site_engagement.js
@@ -57,6 +57,8 @@ define('main', [
       var scoreInput = createElementWithClassName('input', 'score-input');
       scoreInput.addEventListener(
           'change', handleScoreChange.bind(undefined, info.origin));
+      scoreInput.addEventListener('focus', disableAutoupdate);
+      scoreInput.addEventListener('blur', enableAutoupdate);
       scoreInput.value = info.score;
 
       var scoreCell = createElementWithClassName('td', 'score-cell');
@@ -76,6 +78,18 @@ define('main', [
       return row;
     }
 
+    function disableAutoupdate() {
+      if (updateInterval)
+        clearInterval(updateInterval);
+      updateInterval = null;
+    }
+
+    function enableAutoupdate() {
+      if (updateInterval)
+        clearInterval(updateInterval);
+      updateInterval = setInterval(updateEngagementTable, 5000);
+    }
+
     /**
      * Sets the engagement score when a score input is changed. Also resets the
      * update interval.
@@ -84,8 +98,7 @@ define('main', [
      */
     function handleScoreChange(origin, e) {
       uiHandler.setSiteEngagementScoreForOrigin(origin, e.target.value);
-      clearInterval(updateInterval);
-      updateInterval = setInterval(updateEngagementTable, 5000);
+      enableAutoupdate();
     }
 
     /**
@@ -151,6 +164,6 @@ define('main', [
     };
 
     updateEngagementTable();
-    updateInterval = setInterval(updateEngagementTable, 5000);
+    enableAutoupdate();
   };
 });
