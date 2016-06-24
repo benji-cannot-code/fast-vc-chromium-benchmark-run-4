b2b5f4d6a29415d8c8409169bb6b4eabc28b58aa
yiyix <yiyix@chromium.org>
2016-06-24 13:26:40 -0700

    For material design immersive mode (currently off by default, behind --ash-md): - remove immersive mode hints for both the tab indicator on the top and the   activity indicator on the shelf. - remove the activity indicator hints on shelf when it is hidden in the auto   hide mode.
    TEST=manual
    - Turn on immersive mode, check if the activity indicator is removed when shelf
      is positioned on right, left and bottom and the tab indicator is removed in
      all cases; and check if the tab and shelf reappear by mouse movements or by
      touch.
    - Autohide the shelf, check if the black bar and the activity indicator are
      both removed when the shelf is hidden.
    
    BUG=607029
    
    Review-Url: https://codereview.chromium.org/2069413004
    Cr-Commit-Position: refs/heads/master@{#401956}

--- ash/shelf/shelf_button.cc
+++ ash/shelf/shelf_button.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/shelf/shelf_constants.h"
 #include "ash/shelf/ink_drop_button_listener.h"
 #include "ash/shelf/shelf.h"
+#include "ash/shelf/shelf_layout_manager.h"
 #include "ash/shelf/shelf_view.h"
 #include "base/time/time.h"
 #include "grit/ash_resources.h"
@@ -554,11 +555,13 @@ void ShelfButton::UpdateBar() {
     Shelf* shelf = shelf_view_->shelf();
     gfx::ImageSkia image;
     if (ash::MaterialDesignController::IsShelfMaterial()) {
-      gfx::Size size(GetShelfConstant(SHELF_BUTTON_SIZE),
-                     GetShelfConstant(SHELF_SIZE));
-      gfx::Canvas canvas(size, kIndicatorCanvasScale, true /* is_opaque */);
-      PaintIndicatorOnCanvas(&canvas, size);
-      image = gfx::ImageSkia(canvas.ExtractImageRep());
+      if (shelf->shelf_widget()->shelf_layout_manager()->IsVisible()) {
+        gfx::Size size(GetShelfConstant(SHELF_BUTTON_SIZE),
+                       GetShelfConstant(SHELF_SIZE));
+        gfx::Canvas canvas(size, kIndicatorCanvasScale, true /* is_opaque */);
+        PaintIndicatorOnCanvas(&canvas, size);
+        image = gfx::ImageSkia(canvas.ExtractImageRep());
+      }
     } else {
       ResourceBundle* rb = &ResourceBundle::GetSharedInstance();
       image = *rb->GetImageNamed(bar_id).ToImageSkia();
