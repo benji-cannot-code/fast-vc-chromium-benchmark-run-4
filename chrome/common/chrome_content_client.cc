f61ce090e2f94be01d5acc1f1a9e438273752ea2
leon.han <leon.han@intel.com>
2016-06-24 09:34:08 -0700

    Add missing header file for chrome_content_client.cc.
    chrome/common/chrome_content_client.cc uses base::WrapUnique but does
    not include the header file "base/memory/ptr_util.h", this CL adds it
    explicitly.
    
    The implicit including will cause trybots failure just like what
    happened in https://codereview.chromium.org/2063683002/#ps140001.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2095473003
    Cr-Commit-Position: refs/heads/master@{#401879}

--- chrome/common/chrome_content_client.cc
+++ chrome/common/chrome_content_client.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/crash_logging.h"
 #include "base/files/file_util.h"
 #include "base/json/json_reader.h"
+#include "base/memory/ptr_util.h"
 #include "base/memory/scoped_vector.h"
 #include "base/native_library.h"
 #include "base/path_service.h"
