5fd4ceeefc7addd2a5928ed6102415d679334487
ramya.v <ramya.v@samsung.com>
2016-06-23 23:55:54 -0700

    CSS Variable #0000ee is read as #\30 000ee
    Handled HashTokenTypes seperately.
    
    BUG=617694
    
    Review-Url: https://codereview.chromium.org/2080723003
    Cr-Commit-Position: refs/heads/master@{#401828}

--- third_party/WebKit/Source/core/css/CSSMarkup.h
+++ third_party/WebKit/Source/core/css/CSSMarkup.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // Common serializing methods. See: http://dev.w3.org/csswg/cssom/#common-serializing-idioms
-void serializeIdentifier(const String& identifier, StringBuilder& appendTo);
+void serializeIdentifier(const String& identifier, StringBuilder& appendTo, bool skipStartChecks = false);
 void serializeString(const String&, StringBuilder& appendTo);
 String serializeString(const String&);
 String serializeURI(const String&);
