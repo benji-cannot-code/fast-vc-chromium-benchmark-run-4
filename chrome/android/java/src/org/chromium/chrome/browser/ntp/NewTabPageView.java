6ce9efc521d4c19e2d516ec9b4bc0624cd33edae
peconn <peconn@chromium.org>
2016-06-24 02:33:32 -0700

    Trigger a snap scroll on NTP rotation.
    This ensures that the device will stay fully scrolled to the omnibox if it had
    previously been snapped to the NTP Tiles in landscape and then rotated to
    portrait.
    
    BUG=612209
    
    Review-Url: https://codereview.chromium.org/2087443002
    Cr-Commit-Position: refs/heads/master@{#401842}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -462,7 +462,8 @@ public class NewTabPageView extends FrameLayout
                 assert mPendingSnapScroll;
                 mPendingSnapScroll = false;
                 NewTabPageUma.SnapState currentSnapState = CardsLayoutOperations.snapScroll(
-                        mRecyclerView, mNewTabPageLayout, mMostVisitedLayout, getVerticalScroll());
+                        mRecyclerView, mNewTabPageLayout, mMostVisitedLayout, getVerticalScroll(),
+                        false);
                 snapStateObserver.updateSnapState(NewTabPageView.this, currentSnapState);
             }
         };
@@ -815,6 +816,11 @@ public class NewTabPageView extends FrameLayout
 
         if (mUseCardsUi) {
             mRecyclerView.updatePeekingCard();
+            // The positioning of elements may have been changed (since the elements expand to fill
+            // the available vertical space), so adjust the scroll. We force snapping to Most Likely
+            // so the user's snap point doesn't change on rotation.
+            CardsLayoutOperations.snapScroll(mRecyclerView, mNewTabPageLayout, mMostVisitedLayout,
+                    getVerticalScroll(), true);
         }
     }
 
