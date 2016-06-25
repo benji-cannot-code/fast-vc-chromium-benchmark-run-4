68f90e5af31973c5f99209c1157b39f0cc2969f9
mbjorge <mbjorge@chromium.org>
2016-06-24 17:01:03 -0700

    [Chromecast] [LSAN] Wrap new in unique_ptr to avoid leak.
    Cleaning up LeakSanitizer errors.
    
    BUG= internal b/29642562
    TEST= ran cast_media_unittests with leak sanitizer
    
    Review-Url: https://codereview.chromium.org/2095153002
    Cr-Commit-Position: refs/heads/master@{#402019}

--- chromecast/media/audio/cast_audio_output_stream_unittest.cc
+++ chromecast/media/audio/cast_audio_output_stream_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/media/audio/cast_audio_output_stream.h"
 
+#include "base/memory/ptr_util.h"
 #include "base/run_loop.h"
 #include "chromecast/base/metrics/cast_metrics_test_helper.h"
 #include "chromecast/media/audio/cast_audio_manager.h"
@@ -105,8 +106,8 @@ class FakeMediaPipelineBackend : public MediaPipelineBackend {
   // MediaPipelineBackend implementation:
   AudioDecoder* CreateAudioDecoder() override {
     DCHECK(!audio_decoder_);
-    audio_decoder_ = new FakeAudioDecoder();
-    return audio_decoder_;
+    audio_decoder_ = base::MakeUnique<FakeAudioDecoder>();
+    return audio_decoder_.get();
   }
   VideoDecoder* CreateVideoDecoder() override {
     NOTREACHED();
@@ -138,11 +139,11 @@ class FakeMediaPipelineBackend : public MediaPipelineBackend {
   bool SetPlaybackRate(float rate) override { return true; }
 
   State state() const { return state_; }
-  FakeAudioDecoder* decoder() const { return audio_decoder_; }
+  FakeAudioDecoder* decoder() const { return audio_decoder_.get(); }
 
  private:
   State state_;
-  FakeAudioDecoder* audio_decoder_;
+  std::unique_ptr<FakeAudioDecoder> audio_decoder_;
 };
 
 class FakeAudioSourceCallback
