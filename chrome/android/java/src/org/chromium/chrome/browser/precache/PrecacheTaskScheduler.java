94317a1212a7df57eb107512d91362931196d48b
rajendrant <rajendrant@chromium.org>
2016-06-24 17:26:24 -0700

    Remove stable checks from precache code
    When Chrome is started by the user, periodic task will be scheduled in
    stable build, if user is part of field trial.
    
    Remove boot receiver intent to schedule periodic task. Since the GCM
    task is persisted, this is not needed.
    
    When Chrome apk upgrade completes, schedule the periodic task if
    precache has been enabled in shared preferences.
    
    BUG=619212
    
    Review-Url: https://codereview.chromium.org/2064313002
    Cr-Commit-Position: refs/heads/master@{#402025}

--- chrome/android/java/src/org/chromium/chrome/browser/precache/PrecacheTaskScheduler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/precache/PrecacheTaskScheduler.java
@@ -15,9 +15,16 @@ import com.google.android.gms.gcm.Task;
 import org.chromium.chrome.browser.ChromeBackgroundService;
 
 class PrecacheTaskScheduler {
-    boolean scheduleTask(Context context, Task task) {
+    boolean canScheduleTasks(Context context) {
         if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(context)
                 != ConnectionResult.SUCCESS) {
+            return false;
+        }
+        return true;
+    }
+
+    boolean scheduleTask(Context context, Task task) {
+        if (!canScheduleTasks(context)) {
             return false;
         }
         try {
@@ -29,8 +36,7 @@ class PrecacheTaskScheduler {
     }
 
     boolean cancelTask(Context context, String tag) {
-        if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(context)
-                != ConnectionResult.SUCCESS) {
+        if (!canScheduleTasks(context)) {
             return false;
         }
         try {
--- chrome/android/javatests/src/org/chromium/chrome/browser/precache/PrecacheControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/precache/PrecacheControllerTest.java
@@ -72,7 +72,13 @@ public class PrecacheControllerTest extends InstrumentationTestCase {
         public int cancelContinuationCnt = 0;
 
         @Override
+        boolean canScheduleTasks(Context context) {
+            return false;
+        }
+
+        @Override
         boolean scheduleTask(Context context, Task task) {
+            if (!canScheduleTasks(context)) return true;
             if (PrecacheController.PERIODIC_TASK_TAG.equals(task.getTag())) {
                 schedulePeriodicCnt++;
             } else if (PrecacheController.CONTINUATION_TASK_TAG.equals(task.getTag())) {
@@ -111,8 +117,7 @@ public class PrecacheControllerTest extends InstrumentationTestCase {
     protected void verifyScheduledAndCanceledCounts(
             int expectedPeriodicScheduled, int expectedContinuationScheduled,
             int expectedPeriodicCanceled, int expectedContinuationCanceled) {
-        // This experimental feature should not run on Chrome Stable.
-        if (!PrecacheController.canScheduleTasks(mContext)) {
+        if (!mPrecacheTaskScheduler.canScheduleTasks(mContext)) {
             expectedPeriodicScheduled = 0;
             expectedContinuationScheduled = 0;
         }
