0008cc67eeb97ca956813fa8a295a69e2653655c
tdanderson <tdanderson@chromium.org>
2016-06-27 10:59:02 -0700

    Modify PowerStatusViewTest for Ash material design
    Parameterize PowerStatusViewTest to run with all
    three values of the --ash-md flag, and add an
    early return in
    PowerStatusViewTest.AvoidNeedlessBatteryImageUpdates
    for material design.
    
    BUG=622902
    TEST=PowerStatusViewTest*
    
    Review-Url: https://codereview.chromium.org/2101493002
    Cr-Commit-Position: refs/heads/master@{#402222}

--- ash/common/system/chromeos/power/power_status_view_unittest.cc
+++ ash/common/system/chromeos/power/power_status_view_unittest.cc
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/common/system/chromeos/power/power_status_view.h"
 
+#include "ash/common/material_design/material_design_controller.h"
 #include "ash/common/system/chromeos/power/power_status.h"
-#include "ash/test/ash_test_base.h"
+#include "ash/test/ash_md_test_base.h"
 #include "chromeos/dbus/power_manager/power_supply_properties.pb.h"
 #include "grit/ash_strings.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -19,20 +20,20 @@ using power_manager::PowerSupplyProperties;
 
 namespace ash {
 
-class PowerStatusViewTest : public test::AshTestBase {
+class PowerStatusViewTest : public test::AshMDTestBase {
  public:
   PowerStatusViewTest() {}
   ~PowerStatusViewTest() override {}
 
   // Overridden from testing::Test:
   void SetUp() override {
-    test::AshTestBase::SetUp();
+    test::AshMDTestBase::SetUp();
     view_.reset(new PowerStatusView(false));
   }
 
   void TearDown() override {
     view_.reset();
-    test::AshTestBase::TearDown();
+    test::AshMDTestBase::TearDown();
   }
 
  protected:
@@ -61,7 +62,14 @@ class PowerStatusViewTest : public test::AshTestBase {
   DISALLOW_COPY_AND_ASSIGN(PowerStatusViewTest);
 };
 
-TEST_F(PowerStatusViewTest, Basic) {
+INSTANTIATE_TEST_CASE_P(
+    /* prefix intentionally left blank due to only one parameterization */,
+    PowerStatusViewTest,
+    testing::Values(MaterialDesignController::NON_MATERIAL,
+                    MaterialDesignController::MATERIAL_NORMAL,
+                    MaterialDesignController::MATERIAL_EXPERIMENTAL));
+
+TEST_P(PowerStatusViewTest, Basic) {
   EXPECT_FALSE(IsPercentageVisible());
   EXPECT_TRUE(IsTimeStatusVisible());
 
@@ -117,7 +125,11 @@ TEST_F(PowerStatusViewTest, Basic) {
   EXPECT_FALSE(IsTimeStatusVisible());
 }
 
-TEST_F(PowerStatusViewTest, AvoidNeedlessBatteryImageUpdates) {
+TEST_P(PowerStatusViewTest, AvoidNeedlessBatteryImageUpdates) {
+  // No battery icon is shown in the material design system menu.
+  if (ash::MaterialDesignController::UseMaterialDesignSystemIcons())
+    return;
+
   PowerSupplyProperties prop;
   prop.set_external_power(PowerSupplyProperties::AC);
   prop.set_battery_state(PowerSupplyProperties::CHARGING);
