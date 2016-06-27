fee1c14bcec88104404c6831746fda09fcc588ba
rbyers <rbyers@chromium.org>
2016-06-27 15:50:32 -0700

    Update web-platform-tests
    Import wpt@fed03d61ac548fb94838e0f868f75c619d9a8edb
    
    Using update-w3c-deps in Chromium 1353ef00266c2b94ab98b1e028296c273fa14a70.
    
    BUG=622853,622819
    
    Review-Url: https://codereview.chromium.org/2086283003
    Cr-Commit-Position: refs/heads/master@{#402318}

---
+++ third_party/WebKit/LayoutTests/imported/wpt/html/semantics/embedded-content/the-iframe-element/iframe_harness.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function get_test_results(id) {
+    async_test(function(test) {
+        var timer = window.setInterval(test.step_func(loop), 100);
+        function loop() {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'stash.py?id=' + id);
+            xhr.onreadystatechange = test.step_func(function() {
+                assert_equals(xhr.status, 200);
+                if (xhr.responseText) {
+                    assert_equals(xhr.responseText, "OK");
+                    test.done();
+                    window.clearTimeout(timer);
+                }
+            });
+            xhr.send();
+        }
+    });
+}
+
+function send_test_results(results) {
+    var ok = true;
+    for (result in results) { ok = ok && results[result]; }
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'stash.py?id=' + results.id);
+    xhr.send(ok ? "OK" : "FAIL: " + JSON.stringify(results));
+}
