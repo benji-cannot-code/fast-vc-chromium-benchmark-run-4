00775efc6300e3aada146a358ef0ccfd067fae4a
zhenw <zhenw@chromium.org>
2016-06-26 20:17:08 -0700

    Update benchmarks to use chrome_trace_config.MemoryDumpConfig
    TBR=nednguyen@google.com
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:android_s5_perf_cq
    
    Review-Url: https://codereview.chromium.org/2098813002
    Cr-Commit-Position: refs/heads/master@{#402104}

--- tools/perf/benchmarks/tracing.py
+++ tools/perf/benchmarks/tracing.py
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from core import perf_benchmark
 
 from telemetry import benchmark
+from telemetry.timeline import chrome_trace_config
 from telemetry.timeline import tracing_category_filter
-from telemetry.timeline import tracing_config
 from telemetry.web_perf import timeline_based_measurement
 
 import page_sets
@@ -41,7 +41,7 @@ class TracingWithBackgroundMemoryInfra(perf_benchmark.PerfBenchmark):
     trace_memory = tracing_category_filter.TracingCategoryFilter(
         filter_string='-*,blink.console,disabled-by-default-memory-infra')
     options = timeline_based_measurement.Options(overhead_level=trace_memory)
-    memory_dump_config = tracing_config.MemoryDumpConfig()
+    memory_dump_config = chrome_trace_config.MemoryDumpConfig()
     memory_dump_config.AddTrigger('background', 200)
     options.config.chrome_trace_config.SetMemoryDumpConfig(memory_dump_config)
     options.SetTimelineBasedMetric('tracingMetric')
