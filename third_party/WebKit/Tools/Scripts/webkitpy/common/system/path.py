812fd73d4237b50cba89b6082aef6ca0eb9fe5c5
qyearsley <qyearsley@chromium.org>
2016-06-27 09:18:48 -0700

    Remove unnecessary parentheses in if/while/assert statements.
    This changes all places where pylint would give the message "superfluous-parens".
    
    Review-Url: https://codereview.chromium.org/2097943003
    Cr-Commit-Position: refs/heads/master@{#402194}

--- third_party/WebKit/Tools/Scripts/webkitpy/common/system/path.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/system/path.py
@@ -78,7 +78,7 @@ class _CygPath(object):
         self._child_process = None
 
     def start(self):
-        assert(self._child_process is None)
+        assert self._child_process is None
         args = ['cygpath', '-f', '-', '-wa']
         self._child_process = subprocess.Popen(args,
                                                stdin=subprocess.PIPE,
