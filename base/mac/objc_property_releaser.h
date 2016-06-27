02592e154ca1f99a5fe551335c14fcd22632e782
stkhapugin <stkhapugin@chromium.org>
2016-06-27 09:16:32 -0700

    Prevents ObjCPropertyReleaser from compiling with ARC.
    Adds a compile guard to prevent ObjCPropertyReleaser header from being
    included in ARC code.
    
    BUG=None
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2096353002
    Cr-Commit-Position: refs/heads/master@{#402193}

--- base/mac/objc_property_releaser.h
+++ base/mac/objc_property_releaser.h
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 
+#if defined(__has_feature) && __has_feature(objc_arc)
+#error "In ARC code properties are release automatically. Don't use this class."
+#endif
+
 namespace base {
 namespace mac {
 
