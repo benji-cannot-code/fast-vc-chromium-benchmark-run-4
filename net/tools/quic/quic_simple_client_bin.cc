f56d1d69c6d52c4ee5191a18f1cfb8d4f9646c80
rch <rch@chromium.org>
2016-06-27 11:54:01 -0700

    Add a CTPolicyEnforce to the QUIC commandline client
    BUG=622965
    
    Review-Url: https://codereview.chromium.org/2094413002
    Cr-Commit-Position: refs/heads/master@{#402240}

--- net/tools/quic/quic_simple_client_bin.cc
+++ net/tools/quic/quic_simple_client_bin.cc
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::StringPiece;
 using net::CertVerifier;
+using net::CTPolicyEnforcer;
 using net::CTVerifier;
 using net::MultiLogCTVerifier;
 using net::ProofVerifierChromium;
@@ -253,9 +254,10 @@ int main(int argc, char* argv[]) {
   std::unique_ptr<TransportSecurityState> transport_security_state(
       new TransportSecurityState);
   std::unique_ptr<CTVerifier> ct_verifier(new MultiLogCTVerifier());
+  std::unique_ptr<CTPolicyEnforcer> ct_policy_enforcer(new CTPolicyEnforcer());
   ProofVerifierChromium* proof_verifier = new ProofVerifierChromium(
-      cert_verifier.get(), nullptr, transport_security_state.get(),
-      ct_verifier.get());
+      cert_verifier.get(), ct_policy_enforcer.get(),
+      transport_security_state.get(), ct_verifier.get());
   net::QuicSimpleClient client(net::IPEndPoint(ip_addr, port), server_id,
                                versions, proof_verifier);
   client.set_initial_max_packet_length(
