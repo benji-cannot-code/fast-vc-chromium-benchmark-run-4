cc45b45aa5b9122692e8928a06d899b50e7c0981
mathp <mathp@chromium.org>
2016-06-27 08:38:27 -0700

    [Autofill] Change color for sublabel and warning in Autofill popup.
    It's now a darker shade of gray (#646464).
    
    Note: warning stays black on Android.
    
    BUG=567755
    TEST=Visual
    
    Review-Url: https://codereview.chromium.org/2088173004
    Cr-Commit-Position: refs/heads/master@{#402186}

--- chrome/browser/ui/cocoa/autofill/autofill_popup_base_view_cocoa.mm
+++ chrome/browser/ui/cocoa/autofill/autofill_popup_base_view_cocoa.mm
@@ -35,11 +35,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (NSColor*)subtextColor {
-  return [NSColor grayColor];
+  // Represents #646464.
+  return [NSColor colorWithCalibratedRed:100.0 / 255.0
+                                   green:100.0 / 255.0
+                                    blue:100.0 / 255.0
+                                   alpha:1.0];
 }
 
 - (NSColor*)warningColor {
-  return [NSColor grayColor];
+  // Represents #646464.
+  return [NSColor colorWithCalibratedRed:100.0 / 255.0
+                                   green:100.0 / 255.0
+                                    blue:100.0 / 255.0
+                                   alpha:1.0];
 }
 
 #pragma mark -
