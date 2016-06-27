a468251aef9852d41d8edf84bf2c2d01ab166853
fsamuel <fsamuel@chromium.org>
2016-06-27 13:14:57 -0700

    Implement StreamVideoDrawQuad StructTraits
    This CL allows StreamVideoDrawQuad to be serialized and deserialized over mojo.
    
    BUG=611802
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review-Url: https://codereview.chromium.org/2101543002
    Cr-Commit-Position: refs/heads/master@{#402268}

--- cc/ipc/quads_struct_traits.cc
+++ cc/ipc/quads_struct_traits.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "cc/ipc/quads_struct_traits.h"
+#include "ui/gfx/mojo/transform_struct_traits.h"
 
 namespace mojo {
 
@@ -167,6 +168,20 @@ bool StructTraits<cc::mojom::SolidColorQuadState, cc::DrawQuad>::Read(
 }
 
 // static
+bool StructTraits<cc::mojom::StreamVideoQuadState, cc::DrawQuad>::Read(
+    cc::mojom::StreamVideoQuadStateDataView data,
+    cc::DrawQuad* out) {
+  cc::StreamVideoDrawQuad* quad = static_cast<cc::StreamVideoDrawQuad*>(out);
+  quad->resources.ids[cc::StreamVideoDrawQuad::kResourceIdIndex] =
+      data.resource_id();
+  quad->resources.count = 1;
+  return data.ReadResourceSizeInPixels(
+             &quad->overlay_resources
+                  .size_in_pixels[cc::StreamVideoDrawQuad::kResourceIdIndex]) &&
+         data.ReadMatrix(&quad->matrix);
+}
+
+// static
 bool StructTraits<cc::mojom::SurfaceQuadState, cc::DrawQuad>::Read(
     cc::mojom::SurfaceQuadStateDataView data,
     cc::DrawQuad* out) {
@@ -299,9 +314,7 @@ bool StructTraits<cc::mojom::DrawQuad, cc::DrawQuad>::Read(
     case cc::mojom::Material::SOLID_COLOR:
       return data.ReadSolidColorQuadState(out);
     case cc::mojom::Material::STREAM_VIDEO_CONTENT:
-      // TODO(fsamuel): Implement StreamVideoContentDrawQuad
-      // serialization/deserialization.
-      break;
+      return data.ReadStreamVideoQuadState(out);
     case cc::mojom::Material::SURFACE_CONTENT:
       return data.ReadSurfaceQuadState(out);
     case cc::mojom::Material::TEXTURE_CONTENT:
--- cc/quads/stream_video_draw_quad.h
+++ cc/quads/stream_video_draw_quad.h
@@ -18,6 +18,8 @@ namespace cc {
 
 class CC_EXPORT StreamVideoDrawQuad : public DrawQuad {
  public:
+  static const size_t kResourceIdIndex = 0;
+
   StreamVideoDrawQuad();
 
   void SetNew(const SharedQuadState* shared_quad_state,
@@ -53,8 +55,6 @@ class CC_EXPORT StreamVideoDrawQuad : public DrawQuad {
   }
 
  private:
-  static const size_t kResourceIdIndex = 0;
-
   void ExtendValue(base::trace_event::TracedValue* value) const override;
 };
 
