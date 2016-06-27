7f53de2f0c9cff35f64af1881aa2f594863586e0
meade <meade@chromium.org>
2016-06-27 13:14:21 -0700

    [Typed OM] Implement CSSValue->CSSRotation conversion
    The other TransformComponents
    (CSSMatrixTransformComponent, CSSPerspective, CSSScale, CSSSkew,
    CSSTranslation) just return nullptr for now.
    
    BUG=545318
    
    Review-Url: https://codereview.chromium.org/2039093003
    Cr-Commit-Position: refs/heads/master@{#402267}

--- third_party/WebKit/Source/core/css/cssom/CSSRotation.h
+++ third_party/WebKit/Source/core/css/cssom/CSSRotation.h
@@ -36,7 +36,7 @@ public:
         return new CSSRotation(x, y, z, angleValue->degrees());
     }
 
-    static CSSRotation* fromCSSValue(const CSSFunctionValue&) { return nullptr; }
+    static CSSRotation* fromCSSValue(const CSSFunctionValue&);
 
     double angle() const { return m_angle; }
     double x() const { return m_x; }
