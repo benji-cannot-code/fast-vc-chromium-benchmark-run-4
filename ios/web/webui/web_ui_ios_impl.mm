82a62f26bdeee2aeafc920c18651309bdd470e77
eugenebut <eugenebut@chromium.org>
2016-06-28 13:35:15 -0700

    [ios] Removed ExecuteJavaScriptAsync method as unused.
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2092633003
    Cr-Commit-Position: refs/heads/master@{#402535}

--- ios/web/webui/web_ui_ios_impl.mm
+++ ios/web/webui/web_ui_ios_impl.mm
@@ -148,7 +148,7 @@ void WebUIIOSImpl::AddMessageHandler(WebUIIOSMessageHandler* handler) {
 }
 
 void WebUIIOSImpl::ExecuteJavascript(const base::string16& javascript) {
-  web_state_->ExecuteJavaScriptAsync(javascript);
+  web_state_->ExecuteJavaScript(javascript);
 }
 
 }  // namespace web
