33b0870802fa4f984606cd5bee144c7f461cb43d
estade <estade@chromium.org>
2016-06-28 15:14:12 -0700

    Fix MD button hover shadow effect on odd-sized buttons.
    gfx::Point -> gfx::PointF to fix rounding errors.
    
    BUG=623776
    
    Review-Url: https://codereview.chromium.org/2094343005
    Cr-Commit-Position: refs/heads/master@{#402567}

--- ui/views/animation/test/test_ink_drop_host.cc
+++ ui/views/animation/test/test_ink_drop_host.cc
@@ -55,7 +55,7 @@ class TestInkDropHighlight : public InkDropHighlight {
  public:
   TestInkDropHighlight(const gfx::Size& size,
                        int corner_radius,
-                       const gfx::Point& center_point,
+                       const gfx::PointF& center_point,
                        SkColor color)
       : InkDropHighlight(size, corner_radius, center_point, color) {}
 
@@ -103,8 +103,8 @@ std::unique_ptr<InkDropHighlight> TestInkDropHost::CreateInkDropHighlight()
     const {
   std::unique_ptr<InkDropHighlight> highlight;
   if (should_show_highlight_) {
-    highlight.reset(new TestInkDropHighlight(gfx::Size(10, 10), 4, gfx::Point(),
-                                             SK_ColorBLACK));
+    highlight.reset(new TestInkDropHighlight(gfx::Size(10, 10), 4,
+                                             gfx::PointF(), SK_ColorBLACK));
     if (disable_timers_for_test_)
       highlight->GetTestApi()->SetDisableAnimationTimers(true);
   }
