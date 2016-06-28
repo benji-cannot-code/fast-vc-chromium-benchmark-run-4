ccdc0f78d22a2ae09ca92ac56d5f3d629dc56a9f
qinmin <qinmin@chromium.org>
2016-06-27 17:20:56 -0700

    capping how many times background download resumption can automatically start
    If download gets interrupted due to browser kill, we schedule a browser
    restart.
    And we could stuck in a cycle of kill->restart->kill.
    This CL caps the number of auto resumptions.
    And reset the cap when user relaunches chrome.
    
    BUG=623242
    
    Review-Url: https://codereview.chromium.org/2094033002
    Cr-Commit-Position: refs/heads/master@{#402340}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -71,6 +71,7 @@ import org.chromium.chrome.browser.datausage.DataUseTabUIManager;
 import org.chromium.chrome.browser.device.DeviceClassManager;
 import org.chromium.chrome.browser.dom_distiller.DistilledPagePrefsView;
 import org.chromium.chrome.browser.dom_distiller.ReaderModeManager;
+import org.chromium.chrome.browser.download.DownloadManagerService;
 import org.chromium.chrome.browser.fullscreen.ChromeFullscreenManager;
 import org.chromium.chrome.browser.gsa.ContextReporter;
 import org.chromium.chrome.browser.gsa.GSAServiceClient;
@@ -918,7 +919,8 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
         } else {
             removeWindowBackground();
         }
-
+        DownloadManagerService.getDownloadManagerService(
+                getApplicationContext()).onActivityLaunched();
         super.finishNativeInitialization();
     }
 
