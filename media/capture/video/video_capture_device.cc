dbe7ace106704a85ed73b376b20db084068b501e
mcasas <mcasas@chromium.org>
2016-06-28 14:14:13 -0700

    ImageCapture: Wire getCapabilities from ImageCaptureImpl to the device(s)
    This CL wires the getCapabilities query from image_capture.mojom
    to both VideoCaptureDeviceAndroid and FakeVideoCaptureDevice.
    
    BUG=518807
    
    TEST = Added unittests to FakeVCD and run demo html in
    https://rawgit.com/Miguelao/demos/master/imagecapture.html
    with flag --enable-blink-features=ImageCapture
    (click on "Open Camera 320x240", then on "Create
    Image Capturer", then click on GetCapabilities and
    verify the Zoom slider comes up with nonzero values.
    
    Review-Url: https://codereview.chromium.org/2077333003
    Cr-Commit-Position: refs/heads/master@{#402542}

--- media/capture/video/video_capture_device.cc
+++ media/capture/video/video_capture_device.cc
@@ -142,6 +142,9 @@ VideoCaptureDevice::Client::Buffer::~Buffer() {
 VideoCaptureDevice::~VideoCaptureDevice() {
 }
 
+void VideoCaptureDevice::GetPhotoCapabilities(
+    ScopedResultCallback<GetPhotoCapabilitiesCallback> callback) {}
+
 void VideoCaptureDevice::TakePhoto(
     ScopedResultCallback<TakePhotoCallback> callback) {}
 
