6ad28f8428d1ff80557cc3b0773d53b6f98a1ca7
edwardjung <edwardjung@chromium.org>
2016-06-28 08:38:50 -0700

    Allow page interactions whilst the offline easter egg is inactive.
    This fixes an issue where pull to refresh was not possible once the easter egg was activated.
    
    BUG=623589
    
    Review-Url: https://codereview.chromium.org/2107753002
    Cr-Commit-Position: refs/heads/master@{#402477}

--- components/neterror/resources/offline.js
+++ components/neterror/resources/offline.js
@@ -651,7 +651,7 @@ Runner.prototype = {
    */
   onKeyDown: function(e) {
     // Prevent native page scrolling whilst tapping on mobile.
-    if (IS_MOBILE) {
+    if (IS_MOBILE && this.activated) {
       e.preventDefault();
     }
 
