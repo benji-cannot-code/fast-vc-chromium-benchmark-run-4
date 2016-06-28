d43f58514809ee6a0c798607c2cc910827a33300
sky <sky@chromium.org>
2016-06-28 09:57:50 -0700

    Makes the pinned tab title change indicator hide correctly
    It was not properly hiding if the indicator was shown and the tab
    becomes active.
    
    BUG=623746
    TEST=covered by test
    R=estade@chromium.org
    
    Review-Url: https://codereview.chromium.org/2107603002
    Cr-Commit-Position: refs/heads/master@{#402492}

--- chrome/browser/ui/views/tabs/tab_strip_unittest.cc
+++ chrome/browser/ui/views/tabs/tab_strip_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.h"
 #include "chrome/browser/ui/views/tabs/tab.h"
+#include "chrome/browser/ui/views/tabs/tab_renderer_data.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_controller.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_observer.h"
@@ -128,6 +129,11 @@ class TabStripTest : public views::ViewsTestBase {
   }
 
  protected:
+  bool IsShowingPinnedTabTitleChangedIndicator(int model_index) {
+    return tab_strip_->tab_at(model_index)
+        ->showing_pinned_tab_title_changed_indicator_;
+  }
+
   // Returns the rectangular hit test region of |tab| in |tab|'s local
   // coordinate space.
   gfx::Rect GetTabHitTestMask(Tab* tab) {
@@ -635,4 +641,27 @@ TEST_F(TabStripTest, NewTabButtonStaysVisible) {
   DoLayout();
 
   EXPECT_LE(tab_strip_->GetNewTabButtonBounds().right(), kTabStripWidth);
+}
+
+TEST_F(TabStripTest, PinnedTabTitleChangedIndicatorHidesOnSelect) {
+  for (int i = 0; i < 2; ++i)
+    controller_->AddTab(i, (i == 0));
+
+  // Two tabs, both pinned.
+  TabRendererData pinned_data;
+  pinned_data.pinned = true;
+  tab_strip_->SetTabData(0, pinned_data);
+  tab_strip_->SetTabData(1, pinned_data);
+
+  EXPECT_FALSE(IsShowingPinnedTabTitleChangedIndicator(0));
+  EXPECT_FALSE(IsShowingPinnedTabTitleChangedIndicator(1));
+
+  // Change the title of the second tab (first tab is selected).
+  tab_strip_->TabTitleChangedNotLoading(1);
+  // Indicator should be shown.
+  EXPECT_TRUE(IsShowingPinnedTabTitleChangedIndicator(1));
+  // Select the second tab.
+  controller_->SelectTab(1);
+  // Indicator should hide.
+  EXPECT_FALSE(IsShowingPinnedTabTitleChangedIndicator(1));
 }
