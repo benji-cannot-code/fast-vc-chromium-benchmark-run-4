fd29fd5bc2b27ee247851410caa31fde17c73bb0
jlebel <jlebel@chromium.org>
2016-06-27 19:03:55 -0700

    Bluetooth: mac: unit tests
    Adding ReadRemoteCharacteristic_GattCharacteristicValueChanged unit tests for macOS.
    
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2102643002
    Cr-Commit-Position: refs/heads/master@{#402373}

--- device/bluetooth/bluetooth_remote_gatt_characteristic_unittest.cc
+++ device/bluetooth/bluetooth_remote_gatt_characteristic_unittest.cc
@@ -422,7 +422,7 @@ TEST_F(BluetoothRemoteGattCharacteristicTest, ReadRemoteCharacteristic) {
 }
 #endif  // defined(OS_ANDROID) || defined(OS_MACOSX) || defined(OS_WIN)
 
-#if defined(OS_ANDROID) || defined(OS_WIN)
+#if defined(OS_ANDROID) || defined(OS_MACOSX) || defined(OS_WIN)
 // Callback that make sure GattCharacteristicValueChanged has been called
 // before the callback runs.
 static void test_callback(
@@ -437,6 +437,10 @@ static void test_callback(
 // GattCharacteristicValueChanged call.
 TEST_F(BluetoothRemoteGattCharacteristicTest,
        ReadRemoteCharacteristic_GattCharacteristicValueChanged) {
+  if (!PlatformSupportsLowEnergy()) {
+    LOG(WARNING) << "Low Energy Bluetooth unavailable, skipping unit test.";
+    return;
+  }
   ASSERT_NO_FATAL_FAILURE(FakeCharacteristicBoilerplate(
       BluetoothRemoteGattCharacteristic::PROPERTY_READ));
 
@@ -457,7 +461,7 @@ TEST_F(BluetoothRemoteGattCharacteristicTest,
             observer.last_gatt_characteristic_uuid());
   EXPECT_EQ(test_vector, observer.last_changed_characteristic_value());
 }
-#endif  // defined(OS_ANDROID) || defined(OS_WIN)
+#endif  // defined(OS_ANDROID) || defined(OS_MACOSX) || defined(OS_WIN)
 
 #if defined(OS_ANDROID) || defined(OS_MACOSX) || defined(OS_WIN)
 // Tests WriteRemoteCharacteristic with non-empty value buffer.
