2d9e636b260bc3310730c50715e873f5f9ec92df
ymalik <ymalik@chromium.org>
2016-06-28 12:36:15 -0700

    Make visualViewport.scrollTop/Left readonly.
    BUG=595826
    
    Review-Url: https://codereview.chromium.org/2101393002
    Cr-Commit-Position: refs/heads/master@{#402513}

--- third_party/WebKit/Source/core/frame/VisualViewport.h
+++ third_party/WebKit/Source/core/frame/VisualViewport.h
@@ -204,8 +204,6 @@ public:
     // Visual Viewport API implementation.
     double scrollLeft();
     double scrollTop();
-    void setScrollLeft(double x);
-    void setScrollTop(double y);
     double clientWidth();
     double clientHeight();
     double pageScale();
