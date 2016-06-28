0c1a3a0f12ac6cfc36c5a6871fce4f95ab0fe0cd
rmcilroy <rmcilroy@chromium.org>
2016-06-28 08:10:14 -0700

    [Gin] Enable V8 Ignition interpreter for low end Android devices.
    Enable V8 Ignition interpreter for Ignition on low-end (Svelte) devices.
    This is still controlled by a field-trial flag and so can be turned off
    in the field if necessary.
    
    Perf Sheriffs: This CL is likely to cause regressions on some benchmarks
    This is expected, as we are trading off memory usage for benchmark
    performance on low-memory devices, but please file bugs against me
    for tracking.
    
    BUG=v8:4280,v8:4868,594228
    
    Review-Url: https://codereview.chromium.org/2101933003
    Cr-Commit-Position: refs/heads/master@{#402472}

--- gin/gin_features.cc
+++ gin/gin_features.cc
@@ -7,15 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace features {
 
-// Enables or disables the experimental V8 Ignition interpreter.
+// Enables or disables the V8 Ignition interpreter.
 const base::Feature kV8Ignition {
   "V8Ignition", base::FEATURE_DISABLED_BY_DEFAULT
 };
 
-// Enables or disables the experimental V8 Ignition interpreter on low end
+// Enables or disables the V8 Ignition interpreter on low end
 // Android devices.
 const base::Feature kV8IgnitionLowEnd {
-  "V8IgnitionLowEnd", base::FEATURE_DISABLED_BY_DEFAULT
+  "V8IgnitionLowEnd", base::FEATURE_ENABLED_BY_DEFAULT
 };
 
 // Enables lazy compilation for the V8 Ignition interpreter. Only
