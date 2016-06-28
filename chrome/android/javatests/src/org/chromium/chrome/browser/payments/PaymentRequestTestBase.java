c16ad756e0bf8dffa72713af9c6a40b0a054e920
rouslan <rouslan@chromium.org>
2016-06-27 17:59:12 -0700

    Refactor contact editor controller into its own class.
    BUG=603635
    
    Review-Url: https://codereview.chromium.org/2092083003
    Cr-Commit-Position: refs/heads/master@{#402352}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestTestBase.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestTestBase.java
@@ -141,7 +141,7 @@ abstract class PaymentRequestTestBase extends ChromeActivityTestCaseBase<ChromeA
         ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
-                mUI.getEditorViewForTest().findViewById(resourceId).performClick();
+                mUI.getEditorView().findViewById(resourceId).performClick();
             }
         });
         helper.waitForCallback(callCount);
@@ -177,7 +177,7 @@ abstract class PaymentRequestTestBase extends ChromeActivityTestCaseBase<ChromeA
         ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
-                ((EditText) mUI.getEditorViewForTest().findViewById(resourceId)).setText(input);
+                ((EditText) mUI.getEditorView().findViewById(resourceId)).setText(input);
             }
         });
     }
