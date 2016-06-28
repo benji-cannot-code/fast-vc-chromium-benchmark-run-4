fd783deb9765473ae6eb6d355420ee875f581efe
kmarshall <kmarshall@chromium.org>
2016-06-28 16:54:37 -0700

    Add certificate transparency enforcement to SSL client code.
    Recently-added policy rules in the SSL client socket require that
    we enable Certificate Transparency. This CL adds CT support by attaching
    the relevant CT objects to the SSL client context.
    
    R=khushalsagar@chromium.org
    BUG=623684
    
    Review-Url: https://codereview.chromium.org/2103143003
    Cr-Commit-Position: refs/heads/master@{#402603}

--- blimp/net/ssl_client_transport.h
+++ blimp/net/ssl_client_transport.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "blimp/net/tcp_client_transport.h"
 #include "net/base/address_list.h"
 #include "net/base/net_errors.h"
+#include "net/cert/ct_policy_enforcer.h"
+#include "net/cert/multi_log_ct_verifier.h"
 #include "net/http/transport_security_state.h"
 
 namespace net {
@@ -59,6 +61,8 @@ class BLIMP_NET_EXPORT SSLClientTransport : public TCPClientTransport {
   net::IPEndPoint ip_endpoint_;
   std::unique_ptr<ExactMatchCertVerifier> cert_verifier_;
   net::TransportSecurityState transport_security_state_;
+  net::MultiLogCTVerifier cert_transparency_verifier_;
+  net::CTPolicyEnforcer ct_policy_enforcer_;
 
   DISALLOW_COPY_AND_ASSIGN(SSLClientTransport);
 };
