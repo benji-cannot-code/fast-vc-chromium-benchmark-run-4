2bd9e8a5ae8503f51e84481aa214773e96dce5b3
sky <sky@chromium.org>
2016-06-28 09:29:38 -0700

    Fix bug in quicklaunch
    RemoveWindow can be called when the messageloop is already
    shutdown. If this happens we don't want to quit the messageloop as we
    hit a DCHECK.
    
    BUG=594600
    TEST=none
    R=ben@chromium.org
    
    Review-Url: https://codereview.chromium.org/2103863002
    Cr-Commit-Position: refs/heads/master@{#402486}

--- mash/quick_launch/quick_launch_application.cc
+++ mash/quick_launch/quick_launch_application.cc
@@ -160,7 +160,7 @@ void QuickLaunchApplication::RemoveWindow(views::Widget* window) {
   auto it = std::find(windows_.begin(), windows_.end(), window);
   DCHECK(it != windows_.end());
   windows_.erase(it);
-  if (windows_.empty())
+  if (windows_.empty() && base::MessageLoop::current()->is_running())
     base::MessageLoop::current()->QuitWhenIdle();
 }
 
