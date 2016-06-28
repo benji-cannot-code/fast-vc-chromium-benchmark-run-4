9e86bda30600dcf27d5d8339c78315d323fbea97
csharrison <csharrison@chromium.org>
2016-06-28 13:16:14 -0700

    Add UMA in preparation of running the ParserMainThread experiment
    This patch adds the following metrics:
    - queueing delay from bytes received on the main thread to bytes received
    on the BackgroundHTMLParser
    - preload delay measured from bytes received on the main thread to preloads
    sent to the main thread
    
    The patch also gets rid of the appendRawBytesFromParserThread method,
    which was dead code.
    
    BUG=623165,603274
    
    Review-Url: https://codereview.chromium.org/2097973002
    Cr-Commit-Position: refs/heads/master@{#402516}

--- third_party/WebKit/Source/core/html/parser/HTMLDocumentParser.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLDocumentParser.cpp
@@ -1025,6 +1025,7 @@ void HTMLDocumentParser::appendBytes(const char* data, size_t length)
         return;
 
     if (shouldUseThreading()) {
+        double bytesReceivedTime = monotonicallyIncreasingTimeMS();
         if (!m_haveBackgroundParser)
             startBackgroundParser();
 
@@ -1032,7 +1033,7 @@ void HTMLDocumentParser::appendBytes(const char* data, size_t length)
         memcpy(buffer->data(), data, length);
         TRACE_EVENT1(TRACE_DISABLED_BY_DEFAULT("blink.debug"), "HTMLDocumentParser::appendBytes", "size", (unsigned)length);
 
-        postTaskToLookaheadParser(threadSafeBind(&BackgroundHTMLParser::appendRawBytesFromMainThread, m_backgroundParser, passed(std::move(buffer))));
+        postTaskToLookaheadParser(threadSafeBind(&BackgroundHTMLParser::appendRawBytesFromMainThread, m_backgroundParser, passed(std::move(buffer)), bytesReceivedTime));
         return;
     }
 
