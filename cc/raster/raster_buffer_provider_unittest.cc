fd159aa7855d2cab53a6f6904a62fa9bc716786f
sunnyps <sunnyps@chromium.org>
2016-06-27 17:40:11 -0700

    Reland of cc: Add mailbox support to ResourceProvider write locks. (patchset #1 id:1 of https://codereview.chromium.org/2106503002/ )
    Original issue's description:
    > Revert of cc: Add mailbox support to ResourceProvider write locks. (patchset #5 id:70001 of https://codereview.chromium.org/2081883002/ )
    >
    > Reason for revert:
    > Causing canvas crashes - bug 623101
    >
    > Original issue's description:
    > > Reland of cc: Add mailbox support to ResourceProvider write locks. (patchset #24 id:460001 of https://codereview.chromium.org/1951193002/ )
    > >
    > > Original issue's description:
    > > > cc: Add mailbox support to ResourceProvider write locks.
    > > >
    > > > This adds support for mailboxes to ScopedWriteLockGL. Using the mailbox
    > > > requires using ScopedTextureProvider/ScopedSkSurfaceProvider which
    > > > ensures that the texture id for the mailbox is destroyed after use on
    > > > the worker context.
    > > >
    > > > This CL also includes the following cleanup:
    > > > 1. ResourceProvider locks don't keep resource pointers around.
    > > > 2. ScopedSamplerGL does not inherit from ScopedReadLockGL.
    > > > 3. GpuRasterizer is folded back into GpuRasterBufferProvider.
    > > > 4. TileTaskManager does not own RasterBufferProvider.
    > > >
    > > > BUG=525259
    > > > R=piman@chromium.org
    > > > CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    > > >
    > > > Committed: https://crrev.com/5fa5dbdf25bbec21b84f752d3f0642cd184467e2
    > > > Committed: https://crrev.com/3b0f0b8d3db0a9f66864d5b7da87c82f49e74a29
    > > > Cr-Original-Commit-Position: refs/heads/master@{#398204}
    > > > Cr-Commit-Position: refs/heads/master@{#399983}
    > >
    > > R=piman@chromium.org
    > > BUG=525259, 621422, 621130
    > > CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    > >
    > > Committed: https://crrev.com/b3071e4aabd0cb1f29cd624e77eb5f40722be965
    > > Cr-Commit-Position: refs/heads/master@{#401717}
    >
    > TBR=piman@chromium.org
    > # Not skipping CQ checks because original CL landed more than 1 days ago.
    > BUG=525259, 621422, 621130
    >
    > Committed: https://crrev.com/4d27a5a3c38c34bfa34e9937ef8e3ea3831d2194
    > Cr-Commit-Position: refs/heads/master@{#402290}
    
    TBR=piman@chromium.org
    BUG=525259, 621422, 621130
    
    Review-Url: https://codereview.chromium.org/2101043002
    Cr-Commit-Position: refs/heads/master@{#402345}

--- cc/raster/raster_buffer_provider_unittest.cc
+++ cc/raster/raster_buffer_provider_unittest.cc
@@ -21,10 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/base/unique_notifier.h"
 #include "cc/raster/bitmap_raster_buffer_provider.h"
 #include "cc/raster/gpu_raster_buffer_provider.h"
-#include "cc/raster/gpu_rasterizer.h"
 #include "cc/raster/one_copy_raster_buffer_provider.h"
 #include "cc/raster/synchronous_task_graph_runner.h"
 #include "cc/raster/zero_copy_raster_buffer_provider.h"
+#include "cc/resources/platform_color.h"
 #include "cc/resources/resource_pool.h"
 #include "cc/resources/resource_provider.h"
 #include "cc/resources/scoped_resource.h"
@@ -157,43 +157,43 @@ class RasterBufferProviderTest
 
   // Overridden from testing::Test:
   void SetUp() override {
-    std::unique_ptr<RasterBufferProvider> raster_buffer_provider;
     switch (GetParam()) {
       case RASTER_BUFFER_PROVIDER_TYPE_ZERO_COPY:
         Create3dOutputSurfaceAndResourceProvider();
-        raster_buffer_provider = ZeroCopyRasterBufferProvider::Create(
+        raster_buffer_provider_ = ZeroCopyRasterBufferProvider::Create(
             resource_provider_.get(), PlatformColor::BestTextureFormat());
         break;
       case RASTER_BUFFER_PROVIDER_TYPE_ONE_COPY:
         Create3dOutputSurfaceAndResourceProvider();
-        raster_buffer_provider = base::MakeUnique<OneCopyRasterBufferProvider>(
+        raster_buffer_provider_ = base::MakeUnique<OneCopyRasterBufferProvider>(
             base::ThreadTaskRunnerHandle::Get().get(), context_provider_.get(),
             worker_context_provider_.get(), resource_provider_.get(),
             kMaxBytesPerCopyOperation, false, kMaxStagingBuffers,
-            PlatformColor::BestTextureFormat());
+            PlatformColor::BestTextureFormat(), false);
         break;
       case RASTER_BUFFER_PROVIDER_TYPE_GPU:
         Create3dOutputSurfaceAndResourceProvider();
-        raster_buffer_provider = base::MakeUnique<GpuRasterBufferProvider>(
+        raster_buffer_provider_ = base::MakeUnique<GpuRasterBufferProvider>(
             context_provider_.get(), worker_context_provider_.get(),
-            resource_provider_.get(), false, 0);
+            resource_provider_.get(), false, 0, false);
         break;
       case RASTER_BUFFER_PROVIDER_TYPE_BITMAP:
         CreateSoftwareOutputSurfaceAndResourceProvider();
-        raster_buffer_provider =
+        raster_buffer_provider_ =
             BitmapRasterBufferProvider::Create(resource_provider_.get());
         break;
     }
 
-    DCHECK(raster_buffer_provider);
+    DCHECK(raster_buffer_provider_);
 
-    tile_task_manager_ = TileTaskManagerImpl::Create(
-        std::move(raster_buffer_provider), &task_graph_runner_);
+    tile_task_manager_ = TileTaskManagerImpl::Create(&task_graph_runner_);
   }
 
   void TearDown() override {
     tile_task_manager_->Shutdown();
     tile_task_manager_->CheckForCompletedTasks();
+
+    raster_buffer_provider_->Shutdown();
   }
 
   void AllTileTasksFinished() {
@@ -217,6 +217,7 @@ class RasterBufferProviderTest
                                 0 /* dependencies */);
     }
 
+    raster_buffer_provider_->OrderingBarrier();
     tile_task_manager_->ScheduleTasks(&graph_);
   }
 
@@ -229,8 +230,7 @@ class RasterBufferProviderTest
     // The raster buffer has no tile ids associated with it for partial update,
     // so doesn't need to provide a valid dirty rect.
     std::unique_ptr<RasterBuffer> raster_buffer =
-        tile_task_manager_->GetRasterBufferProvider()->AcquireBufferForRaster(
-            resource.get(), 0, 0);
+        raster_buffer_provider_->AcquireBufferForRaster(resource.get(), 0, 0);
     TileTask::Vector empty;
     tasks_.push_back(new TestRasterTaskImpl(this, std::move(resource), id,
                                             std::move(raster_buffer), &empty));
@@ -247,8 +247,7 @@ class RasterBufferProviderTest
                        RGBA_8888);
 
     std::unique_ptr<RasterBuffer> raster_buffer =
-        tile_task_manager_->GetRasterBufferProvider()->AcquireBufferForRaster(
-            resource.get(), 0, 0);
+        raster_buffer_provider_->AcquireBufferForRaster(resource.get(), 0, 0);
     TileTask::Vector empty;
     tasks_.push_back(new BlockingTestRasterTaskImpl(
         this, std::move(resource), id, std::move(raster_buffer), lock, &empty));
@@ -269,8 +268,7 @@ class RasterBufferProviderTest
   void OnRasterTaskCompleted(std::unique_ptr<RasterBuffer> raster_buffer,
                              unsigned id,
                              bool was_canceled) override {
-    tile_task_manager_->GetRasterBufferProvider()->ReleaseBufferForRaster(
-        std::move(raster_buffer));
+    raster_buffer_provider_->ReleaseBufferForRaster(std::move(raster_buffer));
     RasterTaskResult result;
     result.id = id;
     result.canceled = was_canceled;
@@ -308,6 +306,7 @@ class RasterBufferProviderTest
   std::unique_ptr<FakeOutputSurface> output_surface_;
   std::unique_ptr<ResourceProvider> resource_provider_;
   std::unique_ptr<TileTaskManager> tile_task_manager_;
+  std::unique_ptr<RasterBufferProvider> raster_buffer_provider_;
   TestGpuMemoryBufferManager gpu_memory_buffer_manager_;
   TestSharedBitmapManager shared_bitmap_manager_;
   SynchronousTaskGraphRunner task_graph_runner_;
