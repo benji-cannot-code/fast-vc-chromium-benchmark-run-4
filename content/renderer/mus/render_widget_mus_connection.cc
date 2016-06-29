86cec1ba915ef6a828a45babcafe53b934a9a9f4
dtapuska <dtapuska@chromium.org>
2016-06-28 20:35:07 -0700

    Ensure acks are sent for all blocking events.
    It was possible for two touch moves that had acks to get coalesced together
    and the main thread would only process one and deliver the ack for it. This
    caused a situation where the hung renderer timer was getting fired because
    there was an outstanding ack in the count. And this timer forced the
    omnibox to not go away.
    
    A large portion of this change (plumbing the ack_state will go away when
    I land my multi-thread main_thread_event_queue design); but since this
    needs to be merged back to M52 this is safest.
    
    BUG=616991
    
    Review-Url: https://codereview.chromium.org/2094323002
    Cr-Commit-Position: refs/heads/master@{#402703}

--- content/renderer/mus/render_widget_mus_connection.cc
+++ content/renderer/mus/render_widget_mus_connection.cc
@@ -121,7 +121,8 @@ void RenderWidgetMusConnection::OnInputEventAck(
 }
 
 void RenderWidgetMusConnection::NotifyInputEventHandled(
-    blink::WebInputEvent::Type handled_type) {
+    blink::WebInputEvent::Type handled_type,
+    InputEventAckState ack_result) {
   NOTIMPLEMENTED();
 }
 
