99a29b6224243d9f98ba82f931ab3dac992553e6
karandeepb <karandeepb@chromium.org>
2016-06-28 21:09:15 -0700

    MacViews: Modify cocoa_test_event_utils::SynthesizeKeyEvent to use the correct event timestamp value.
    This CL fixes the following views_unittests, currently disabled on MacViews-
      -TextfieldTest.DestroyingTextfieldFromOnKeyEvent
      -TextfieldTest.FocusTraversalTest
      -TextfieldTest.TextInputClientTest
    
    These regressed in crrev.com/2007083002, which added a DCHECK to validate that
    ui::Event::time_stamp comes from the same clock as TimeTicks::Now. These tests
    fail since we are using a mocked system clock in tests, set using
    SetEventTickClockForTesting in base_event_utils.h, against which the event
    timestamp is validated. However cocoa_test_event_utils::SynthesizeKeyEvent uses
    the actual/correct timestamp value, and not the mocked one.
    
    This CL fixes the tests by using a timestamp value obtained from
    ui::EventTimeForNow. This returns the mocked time (if applicable), or
    base::TimeTicks::Now(). base::TimeTicks::Now() internally uses the same logic as
    TimeIntervalSinceSystemStartup() did previously (i.e. mach_absolute_time()).
    
    BUG=623420, 607403
    
    Review-Url: https://codereview.chromium.org/2101903002
    Cr-Commit-Position: refs/heads/master@{#402708}

--- ui/events/test/cocoa_test_event_utils.h
+++ ui/events/test/cocoa_test_event_utils.h
@@ -6,9 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_EVENTS_TEST_COCOA_TEST_EVENT_UTILS_H_
 #define UI_EVENTS_TEST_COCOA_TEST_EVENT_UTILS_H_
 
-#include <utility>
-
+#import <Cocoa/Cocoa.h>
 #import <objc/objc-class.h>
+
+#include <utility>
 
 #include "ui/events/keycodes/dom/dom_key.h"
 #include "ui/events/keycodes/keyboard_codes.h"
