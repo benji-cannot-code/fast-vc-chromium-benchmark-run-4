4f71b28402e9af6eb6d4b048289100cd95ed0cbd
dtseng <dtseng@chromium.org>
2016-06-29 16:34:21 -0700

    Expose html attributes in automation tree.
    BUG=624124
    
    Review-Url: https://codereview.chromium.org/2059533003
    Cr-Commit-Position: refs/heads/master@{#402990}

--- chrome/renderer/resources/extensions/automation/automation_node.js
+++ chrome/renderer/resources/extensions/automation/automation_node.js
@@ -93,6 +93,13 @@ var GetChildIDAtIndex = requireNative('automationInternal').GetChildIDAtIndex;
 /**
  * @param {number} axTreeID The id of the accessibility tree.
  * @param {number} nodeID The id of a node.
+ * @return {?Object} An object mapping html attributes to values.
+ */
+var GetHtmlAttributes = requireNative('automationInternal').GetHtmlAttributes;
+
+/**
+ * @param {number} axTreeID The id of the accessibility tree.
+ * @param {number} nodeID The id of a node.
  * @return {?number} The index of this node in its parent, or undefined if
  *     the tree or node or node parent wasn't found.
  */
@@ -227,6 +234,10 @@ AutomationNodeImpl.prototype = {
       return this.hostNode_;
     var parentID = GetParentID(this.treeID, this.id);
     return this.rootImpl.get(parentID);
+  },
+
+  get htmlAttributes() {
+    return GetHtmlAttributes(this.treeID, this.id) || {};
   },
 
   get state() {
@@ -1012,6 +1023,7 @@ utils.expose(AutomationNode, AutomationNodeImpl, {
       'location',
       'indexInParent',
       'root',
+      'htmlAttributes',
   ]),
 });
 
