be4c06e86797686e8f1f5935b3b00d41cee78953
ianwen <ianwen@chromium.org>
2016-06-28 19:25:59 -0700

    CancelAwareAnimatorListener and Refactor Custom Tab Toolbar Animation
    This CL does two things.
    1. Introduce CancelAwareAnimatorListener to Chrome. For vanilla
    AnimationListener in Android, onAnimationCancel() and onAnimationEnd()
    will both be called if Animator is canclled. This is a confusing
    behavior and it caused problems in various places.
    2. Change the way security icon animation works in custom tab, making
    it able to refresh its theme when the color of the toolbar changes.
    
    Credit of this CL goes to tedchoc@.
    
    BUG=604520
    R=tedchoc@chromium.org
    
    Review-Url: https://codereview.chromium.org/2104053005
    Cr-Commit-Position: refs/heads/master@{#402672}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/SnippetHeaderViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/SnippetHeaderViewHolder.java
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ntp.snippets;
 
 import android.animation.Animator;
-import android.animation.Animator.AnimatorListener;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -93,17 +93,11 @@ public class SnippetHeaderViewHolder extends NewTabPageViewHolder {
 
         mAnimator = ObjectAnimator.ofFloat(itemView, View.ALPHA, currentAlpha, targetAlpha);
         mAnimator.setDuration(ANIMATION_DURATION_MS);
-        mAnimator.addListener(new AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animator) {}
-            @Override
-            public void onAnimationRepeat(Animator animator) {}
+        mAnimator.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animator) {
                 mAnimator = null;
             }
-            @Override
-            public void onAnimationCancel(Animator animator) {}
         });
         mAnimator.start();
     }
