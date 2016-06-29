f180403694ca59fad1401f8a2086fda4e357b5a0
vmpstr <vmpstr@chromium.org>
2016-06-28 19:22:28 -0700

    ash: Change auto to not deduce raw pointers.
    This patch updates the code to prevent auto deducing to a raw pointer.
    
    R=sky, danakj, dcheng
    BUG=554600
    
    Review-Url: https://codereview.chromium.org/2110643002
    Cr-Commit-Position: refs/heads/master@{#402668}

--- ash/mus/layout_manager.cc
+++ ash/mus/layout_manager.cc
@@ -27,7 +27,7 @@ void LayoutManager::Uninstall() {
   if (!owner_)
     return;
   owner_->RemoveObserver(this);
-  for (auto child : owner_->children())
+  for (auto* child : owner_->children())
     child->RemoveObserver(this);
   owner_ = nullptr;
 }
@@ -59,7 +59,7 @@ void LayoutManager::OnWindowBoundsChanged(::mus::Window* window,
     return;
 
   // Changes to the container's bounds require all windows to be laid out.
-  for (auto child : window->children())
+  for (auto* child : window->children())
     LayoutWindow(child);
 }
 
--- ash/mus/window_manager.cc
+++ ash/mus/window_manager.cc
@@ -221,7 +221,7 @@ bool WindowManager::OnWmSetProperty(
 void WindowManager::OnWmClientJankinessChanged(
     const std::set<::mus::Window*>& client_windows,
     bool janky) {
-  for (auto window : client_windows)
+  for (auto* window : client_windows)
     SetWindowIsJanky(window, janky);
 }
 
