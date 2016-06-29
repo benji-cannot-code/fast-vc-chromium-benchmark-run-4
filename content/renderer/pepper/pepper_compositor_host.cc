f4e9929bea68d64747b2b1420d08177327dd725c
dsinclair <dsinclair@chromium.org>
2016-06-29 05:58:20 -0700

    Correct old_layer checks in pepper compositor.
    The checks were incorrectly checking new_layer instead of old_layer.
    
    Review-Url: https://codereview.chromium.org/2101303003
    Cr-Commit-Position: refs/heads/master@{#402786}

--- content/renderer/pepper/pepper_compositor_host.cc
+++ content/renderer/pepper/pepper_compositor_host.cc
@@ -66,7 +66,7 @@ int32_t VerifyCommittedLayer(const ppapi::CompositorLayerData* old_layer,
   if (new_layer->texture) {
     if (old_layer) {
       // Make sure the old layer is a texture layer too.
-      if (!new_layer->texture)
+      if (!old_layer->texture)
         return PP_ERROR_BADARGUMENT;
       // The mailbox should be same, if the resource_id is not changed.
       if (new_layer->common.resource_id == old_layer->common.resource_id) {
@@ -88,7 +88,7 @@ int32_t VerifyCommittedLayer(const ppapi::CompositorLayerData* old_layer,
   if (new_layer->image) {
     if (old_layer) {
       // Make sure the old layer is an image layer too.
-      if (!new_layer->image)
+      if (!old_layer->image)
         return PP_ERROR_BADARGUMENT;
       // The image data resource should be same, if the resource_id is not
       // changed.
