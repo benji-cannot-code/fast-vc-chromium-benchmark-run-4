8d54d40b78ba2840ffe3b758645b58f23a4c95be
ekaramad <ekaramad@chromium.org>
2016-06-28 19:18:25 -0700

    Implement RenderWidgetHostViewBase::ImeCancelComposition for all Views (Aura Only)
    Currently, the RenderWidgetHostViewChildFrame's do not implement this method.
    This CL implements the method for RenderWidgetHostViewBase where the view will
    forward the call to the TextInputManager. The TextInputManager will then call
    its observers about the change. The tab's view in aura is an observer of the
    TextInputManager which will in turn cancel any ongoing composition.
    
    BUG=578168
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2098353005
    Cr-Commit-Position: refs/heads/master@{#402663}

--- content/browser/renderer_host/text_input_manager.cc
+++ content/browser/renderer_host/text_input_manager.cc
@@ -79,6 +79,12 @@ void TextInputManager::UpdateTextInputState(
   NotifyObserversAboutInputStateUpdate(view, changed);
 }
 
+void TextInputManager::ImeCancelComposition(RenderWidgetHostViewBase* view) {
+  DCHECK(IsRegistered(view));
+  FOR_EACH_OBSERVER(Observer, observer_list_,
+                    OnImeCancelComposition(this, view));
+}
+
 void TextInputManager::Register(RenderWidgetHostViewBase* view) {
   DCHECK(!IsRegistered(view));
 
