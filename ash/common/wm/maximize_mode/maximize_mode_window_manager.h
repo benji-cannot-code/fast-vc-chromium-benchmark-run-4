0904ee6e374b3c09eff93013b9691824d870ac79
sky <sky@chromium.org>
2016-06-29 12:07:47 -0700

    Moves handful of classes in ash/wm/maximize_mode to ash/common
    These classes now only depend upon ash/common.
    
    BUG=612629
    TEST=covered by tests
    R=jamescook@chromium.org
    TBR=jamescook@chromium.org
    
    Review-Url: https://codereview.chromium.org/2108223002
    Cr-Commit-Position: refs/heads/master@{#402888}

--- ash/common/wm/maximize_mode/maximize_mode_window_manager.h
+++ ash/common/wm/maximize_mode/maximize_mode_window_manager.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_WM_MAXIMIZE_MODE_MAXIMIZE_MODE_WINDOW_MANAGER_H_
-#define ASH_WM_MAXIMIZE_MODE_MAXIMIZE_MODE_WINDOW_MANAGER_H_
+#ifndef ASH_COMMON_WM_MAXIMIZE_MODE_MAXIMIZE_MODE_WINDOW_MANAGER_H_
+#define ASH_COMMON_WM_MAXIMIZE_MODE_MAXIMIZE_MODE_WINDOW_MANAGER_H_
 
 #include <stdint.h>
 
@@ -135,4 +135,4 @@ class ASH_EXPORT MaximizeModeWindowManager : public WmWindowObserver,
 
 }  // namespace ash
 
-#endif  // ASH_WM_MAXIMIZE_MODE_MAXIMIZE_MODE_WINDOW_MANAGER_H_
+#endif  // ASH_COMMON_WM_MAXIMIZE_MODE_MAXIMIZE_MODE_WINDOW_MANAGER_H_
--- ash/common/wm/maximize_mode/workspace_backdrop_delegate.h
+++ ash/common/wm/maximize_mode/workspace_backdrop_delegate.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_WM_MAXIMIZE_MODE_WORKSPACE_BACKDROP_DELEGATE_H_
-#define ASH_WM_MAXIMIZE_MODE_WORKSPACE_BACKDROP_DELEGATE_H_
+#ifndef ASH_COMMON_WM_MAXIMIZE_MODE_WORKSPACE_BACKDROP_DELEGATE_H_
+#define ASH_COMMON_WM_MAXIMIZE_MODE_WORKSPACE_BACKDROP_DELEGATE_H_
 
 #include <memory>
 
@@ -71,4 +71,4 @@ class ASH_EXPORT WorkspaceBackdropDelegate
 
 }  // namespace ash
 
-#endif  // ASH_WM_MAXIMIZE_MODE_WORKSPACE_BACKDROP_DELEGATE_H_
+#endif  // ASH_COMMON_WM_MAXIMIZE_MODE_WORKSPACE_BACKDROP_DELEGATE_H_
