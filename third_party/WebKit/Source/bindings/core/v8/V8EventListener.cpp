24fdb7716f9bb7a8cad728b1ea51d1f1cf6dd219
Yuki Shiino <yukishiino@chromium.org>
2016-06-29 09:09:45 +0900

    Adds a crash key to V8EventListener to help diagnose crbug.com/621730 .
    This change can be reverted after a day or two of data.
    
    BUG=621730
    R=haraken@chromium.org, rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/2107773002 .
    
    Cr-Commit-Position: refs/heads/master@{#402609}

--- third_party/WebKit/Source/bindings/core/v8/V8EventListener.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8EventListener.cpp
@@ -34,7 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptController.h"
 #include "bindings/core/v8/V8Binding.h"
 #include "core/dom/Document.h"
+#include "core/events/Event.h"
 #include "core/frame/LocalFrame.h"
+#include "wtf/debug/CrashLogging.h"
 
 namespace blink {
 
@@ -90,6 +92,28 @@ v8::Local<v8::Value> V8EventListener::callListenerFunction(ScriptState* scriptSt
     // TODO(jochen): Consider moving this check into canExecuteScripts. http://crbug.com/608641
     if (scriptState->world().isMainWorld() && !frame->script().canExecuteScripts(AboutToExecuteScript))
         return v8::Local<v8::Value>();
+
+    // Temporary instrumentation for http://crbug.com/621730.
+    String scriptInfo;
+    if (event->type() == EventTypeNames::message) {
+        v8::HandleScope handleScope(isolate());
+        StringBuilder scriptInfoBuilder;
+        V8StringResource<> functionName = handlerFunction->GetDebugName();
+        if (functionName.prepare()) {
+            scriptInfoBuilder.append(static_cast<String>(functionName));
+        }
+        scriptInfoBuilder.append(':');
+        scriptInfoBuilder.appendNumber(handlerFunction->GetScriptLineNumber());
+        scriptInfoBuilder.append(':');
+        scriptInfoBuilder.appendNumber(handlerFunction->GetScriptColumnNumber());
+        scriptInfoBuilder.append(':');
+        V8StringResource<> scriptName = handlerFunction->GetScriptOrigin().ResourceName();
+        if (scriptName.prepare()) {
+            scriptInfoBuilder.append(static_cast<String>(scriptName));
+        }
+        scriptInfo = scriptInfoBuilder.toString();
+    }
+    WTF::debug::ScopedCrashKey("postmessage_script_info", scriptInfo.utf8().data());
 
     v8::Local<v8::Value> parameters[1] = { jsEvent };
     v8::Local<v8::Value> result;
