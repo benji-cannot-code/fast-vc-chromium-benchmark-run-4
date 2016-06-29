2eeea80f402abdb3bf9febd973636b3bb57c3959
bzanotti <bzanotti@chromium.org>
2016-06-29 07:39:00 -0700

    Remove unused [ChromeIdentity isSignedIn].
    BUG=623928
    
    Review-Url: https://codereview.chromium.org/2103883003
    Cr-Commit-Position: refs/heads/master@{#402803}

--- ios/public/provider/chrome/browser/signin/chrome_identity.h
+++ ios/public/provider/chrome/browser/signin/chrome_identity.h
@@ -29,9 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // between apps.
 @property(nonatomic, readonly) NSString* hashedGaiaID;
 
-// Whether the ChromeIdentity object has been signed in.
-@property(nonatomic, readonly) BOOL isSignedIn;
-
 @end
 
 #endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_SIGNIN_CHROME_IDENTITY_H_
