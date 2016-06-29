fea4e3114c6a8e264fb47d9a3950eb18e5ba3191
jianli <jianli@chromium.org>
2016-06-29 12:18:59 -0700

    Fix the problem that argument is not provided in calling offline internal functions
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2111433002
    Cr-Commit-Position: refs/heads/master@{#402894}

--- chrome/browser/resources/offline_pages/offline_internals_browser_proxy.js
+++ chrome/browser/resources/offline_pages/offline_internals_browser_proxy.js
@@ -127,12 +127,12 @@ cr.define('offlineInternals', function() {
 
     /** @override */
     setRecordPageModel: function(shouldLog) {
-      chrome.send('setRecordPageModel');
+      chrome.send('setRecordPageModel', [shouldLog]);
     },
 
     /** @override */
     setRecordRequestQueue: function(shouldLog) {
-      chrome.send('setRecordRequestQueue');
+      chrome.send('setRecordRequestQueue', [shouldLog]);
     },
 
     /** @override */
