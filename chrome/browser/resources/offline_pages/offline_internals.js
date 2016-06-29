5b54afeb6dc7e9cba5375a997bb46ff65a27a7a5
dimich <dimich@chromium.org>
2016-06-28 19:53:10 -0700

    Fix chrome://offline-internal page.
    BUG=NONE
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2103383003
    Cr-Commit-Position: refs/heads/master@{#402692}

--- chrome/browser/resources/offline_pages/offline_internals.js
+++ chrome/browser/resources/offline_pages/offline_internals.js
@@ -12,8 +12,9 @@ cr.define('offlineInternals', function() {
   /** @type {!Array<SavePageRequest>} */
   var savePageRequests = [];
 
-  /** @type {offlineInternals.OfflineInternalsBrowserProxy} */
-  var browserProxy_;
+  /** @type {!offlineInternals.OfflineInternalsBrowserProxy} */
+  var browserProxy_ =
+      offlineInternals.OfflineInternalsBrowserProxyImpl.getInstance();
 
   /**
    * Fill stored pages table.
@@ -182,8 +183,6 @@ cr.define('offlineInternals', function() {
     $('log-request-off').onclick =
         browserProxy_.setRecordRequestQueue.bind(browserProxy_, false);
     $('refresh-logs').onclick = refreshLog;
-    browserProxy_ =
-        offlineInternals.OfflineInternalsBrowserProxyImpl.getInstance();
     refreshAll();
   }
 
