76fdae462fa8ff969072ec08f3791f063a2408a3
tkent <tkent@chromium.org>
2016-06-29 03:03:22 -0700

    Improve performance of HTMLSelectElement::setActiveSelectionAnchor().
    Do not update m_cachedStateForActiveSelection if it's a
    menulist. m_cachedStateForActiveSelection is used only in listboxes.
    
    Removing 20,000 OPTIONs: 4,787ms -> 3,772ms
    
    BUG=620142, 624220
    
    Review-Url: https://codereview.chromium.org/2104883004
    Cr-Commit-Position: refs/heads/master@{#402765}

--- third_party/WebKit/Source/core/html/HTMLSelectElement.h
+++ third_party/WebKit/Source/core/html/HTMLSelectElement.h
@@ -201,6 +201,7 @@ private:
     void resetToDefaultSelection(ResetReason = ResetReasonOthers);
     void typeAheadFind(KeyboardEvent*);
     void saveLastSelection();
+    void saveListboxActiveSelection();
     // Returns the first selected OPTION, or nullptr.
     HTMLOptionElement* selectedOption() const;
 
