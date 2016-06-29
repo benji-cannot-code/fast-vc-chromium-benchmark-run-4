c1f6d050ac86a7f8785f72305dc829a4a2c3c5bf
tkent <tkent@chromium.org>
2016-06-28 21:53:20 -0700

    Remove unnecessary code from HTMLOptGroupElement.
    * We don't need to call setRecalcListItems() in childrenChanged() because
      HTMLOptionElement::insertedInto() and removedFrom() do it.
    
    * We don't need to call setRecalcListItems() in parseAttribute(). OPTGROUP
      attributes don't affect HTMLSelectElement::m_listItems.
      The code exists since http://trac.webkit.org/changeset/4 .
    
    BUG=624220
    
    Review-Url: https://codereview.chromium.org/2105243002
    Cr-Commit-Position: refs/heads/master@{#402722}

--- third_party/WebKit/Source/core/html/HTMLOptGroupElement.h
+++ third_party/WebKit/Source/core/html/HTMLOptGroupElement.h
@@ -51,7 +51,6 @@ private:
 
     bool supportsFocus() const override;
     void parseAttribute(const QualifiedName&, const AtomicString&, const AtomicString&) override;
-    void childrenChanged(const ChildrenChange&) override;
     void accessKeyAction(bool sendMouseEvents) override;
     void didAddUserAgentShadowRoot(ShadowRoot&) override;
     void attach(const AttachContext& = AttachContext()) override;
@@ -64,7 +63,6 @@ private:
     PassRefPtr<ComputedStyle> customStyleForLayoutObject() override;
 
     void updateGroupLabel();
-    void recalcSelectOptions();
 
     RefPtr<ComputedStyle> m_style;
 };
