f58d47ada8e118581d48a92e66145da203f79f22
iclelland <iclelland@chromium.org>
2016-06-29 07:19:43 -0700

    Allow origin trials to be declared on IDL operations (methods)
    This change follows the changes to the V8 bindings generation code that allowed
    origin trials to be defined on attributes and constants. Similarly, it
    refactors the various lists of methods which were being passed into the template
    context into a single list and several filters which may be applied to it.
    
    This patch covers only the first stage of method support: Individual methods in
    full or partial interfaces, with no overloads. Attempting to define an origin
    trial on an overloaded method will result in an exception at compile time.
    
    R=haraken@chromium.org, yukishiino@chromium.org
    BUG=621641
    
    Review-Url: https://codereview.chromium.org/2106983002
    Cr-Commit-Position: refs/heads/master@{#402799}

--- third_party/WebKit/Source/bindings/scripts/code_generator_v8.py
+++ third_party/WebKit/Source/bindings/scripts/code_generator_v8.py
@@ -81,6 +81,7 @@ import v8_callback_interface
 import v8_dictionary
 from v8_globals import includes, interfaces
 import v8_interface
+from v8_methods import method_filters
 import v8_types
 import v8_union
 from v8_utilities import capitalize, cpp_name, for_origin_trial_feature, unique_by
@@ -432,6 +433,7 @@ def initialize_jinja_env(cache_dir):
         })
     jinja_env.filters.update(attribute_filters())
     jinja_env.filters.update(v8_interface.constant_filters())
+    jinja_env.filters.update(method_filters())
     return jinja_env
 
 
