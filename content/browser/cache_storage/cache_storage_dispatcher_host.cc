80dd571ef6274800a6b9b3271991c430c2a28576
jkarlin <jkarlin@chromium.org>
2016-06-29 07:17:25 -0700

    [CacheStorage] Temporarily preserved recently opened caches from CacheStorageDispatcherHost
    Currently when a cache is opened it is preserved for 30 seconds by
    content::CacheStorage so that, in case the cache is accessed again soon it
    doesn't have to be reopened. This happened in
    https://codereview.chromium.org/1470073003/.
    
    We don't want to preserve every opened cache. Particularly, we don't want to
    preserve caches opened by the QuotaManager when it's determining the size of
    all of the caches. We only want to preserve caches opened on behalf of documents.
    
    This CL moves the preservation logic to the CacheStorageDispatcherHost, where
    open calls from the document are made. That way open calls from other clients don't
    trigger preservation. The CL also lowers the time that the cache is held open to 5
    seconds from 30 as that should be more than enough time between cache actions on most
    pages.
    
    BUG=623159
    
    Review-Url: https://codereview.chromium.org/2100433003
    Cr-Commit-Position: refs/heads/master@{#402798}

--- content/browser/cache_storage/cache_storage_dispatcher_host.cc
+++ content/browser/cache_storage/cache_storage_dispatcher_host.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/threading/thread_task_runner_handle.h"
+#include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "content/browser/bad_message.h"
 #include "content/browser/cache_storage/cache_storage_cache.h"
@@ -32,6 +34,7 @@ namespace content {
 namespace {
 
 const uint32_t kFilteredMessageClasses[] = {CacheStorageMsgStart};
+const int32_t kCachePreservationSeconds = 5;
 
 blink::WebServiceWorkerCacheError ToWebServiceWorkerCacheError(
     CacheStorageError err) {
@@ -60,12 +63,14 @@ bool OriginCanAccessCacheStorage(const url::Origin& origin) {
   return !origin.unique() && IsOriginSecure(GURL(origin.Serialize()));
 }
 
+void StopPreservingCache(
+    std::unique_ptr<CacheStorageCacheHandle> cache_handle) {}
+
 }  // namespace
 
 CacheStorageDispatcherHost::CacheStorageDispatcherHost()
     : BrowserMessageFilter(kFilteredMessageClasses,
-                           arraysize(kFilteredMessageClasses)) {
-}
+                           arraysize(kFilteredMessageClasses)) {}
 
 CacheStorageDispatcherHost::~CacheStorageDispatcherHost() {
 }
@@ -349,6 +354,14 @@ void CacheStorageDispatcherHost::OnCacheStorageOpenCallback(
         thread_id, request_id, ToWebServiceWorkerCacheError(error)));
     return;
   }
+
+  // Hang on to the cache for a few seconds. This way if the user quickly closes
+  // and reopens it the cache backend won't have to be reinitialized.
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE,
+      base::Bind(&StopPreservingCache, base::Passed(cache_handle->Clone())),
+      base::TimeDelta::FromSeconds(kCachePreservationSeconds));
+
   CacheID cache_id = StoreCacheReference(std::move(cache_handle));
   Send(new CacheStorageMsg_CacheStorageOpenSuccess(thread_id, request_id,
                                                    cache_id));
