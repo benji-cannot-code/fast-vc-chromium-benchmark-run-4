1c32e74c117bfcbf7b564d6dee1cafd95a764746
mlamouri <mlamouri@chromium.org>
2016-06-30 05:22:28 -0700

    Set right Activity title when inside the Media sub-menu (Android).
    BUG=624687
    
    Review-Url: https://codereview.chromium.org/2117443002
    Cr-Commit-Position: refs/heads/master@{#403151}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SiteSettingsPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SiteSettingsPreferences.java
@@ -83,6 +83,7 @@ public class SiteSettingsPreferences extends PreferenceFragment
             category = getArguments().getString(SingleCategoryPreferences.EXTRA_CATEGORY, "");
             if (MEDIA_KEY.equals(category)) {
                 mMediaSubMenu = true;
+                getActivity().setTitle(findPreference(MEDIA_KEY).getTitle().toString());
             }
         }
 
