4c40c72f6b36293a101525fff77e8b55f06a3475
donnd <donnd@chromium.org>
2016-06-30 12:07:40 -0700

    [TTS] Basic Tap Suppression functionality.
    Adds a feature to suppress the Contextual Search UX when a Tap
    occurs and various signals indicate that it's unlikely the user
    will open the panel.
    
    For this CL, the only two signals are:
    1) Whether the user has Tapped N times without opening the panel.
    2) Whether the previous Tap was in about the same location recently.
    
    Signal #1 suppresses, signal #2 overrides the suppression by triggering.
    
    Also minor cleanup to TapFarFromPreviousSuppression and
    ContextualSearchTapState.
    
    BUG=609918
    
    Review-Url: https://codereview.chromium.org/2096203002
    Cr-Commit-Position: refs/heads/master@{#403239}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/BarOverlapTapSuppression.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/BarOverlapTapSuppression.java
@@ -34,7 +34,7 @@ public class BarOverlapTapSuppression extends ContextualSearchHeuristic {
         mActivity = selectionController.getActivity();
         mDoesBarOverlap = doesBarOverlap(x, y);
         mIsConditionSatisfied =
-                mDoesBarOverlap && ContextualSearchFieldTrial.isBarOverlapSupressionEnabled();
+                mDoesBarOverlap && ContextualSearchFieldTrial.isBarOverlapSuppressionEnabled();
     }
 
     @Override
@@ -44,7 +44,7 @@ public class BarOverlapTapSuppression extends ContextualSearchHeuristic {
 
     @Override
     protected void logConditionState() {
-        if (ContextualSearchFieldTrial.isBarOverlapSupressionEnabled()) {
+        if (ContextualSearchFieldTrial.isBarOverlapSuppressionEnabled()) {
             ContextualSearchUma.logBarOverlapSuppression(mIsConditionSatisfied);
         }
     }
--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/TapFarFromPreviousSuppression.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/TapFarFromPreviousSuppression.java
@@ -5,6 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.contextualsearch;
 
+/**
+ * Implements the policy that a Tap relatively far away from an existing Contextual Search
+ * selection should just dismiss our UX.  When a Tap is close by, we assume the user must have
+ * missed the original intended target so we reselect based on the new Tap location.
+ */
 class TapFarFromPreviousSuppression extends ContextualSearchHeuristic {
     private static final double RETAP_DISTANCE_SQUARED_DP = Math.pow(75, 2);
 
@@ -20,7 +25,7 @@ class TapFarFromPreviousSuppression extends ContextualSearchHeuristic {
      * @param y The y coordinate of the tap gesture.
      */
     TapFarFromPreviousSuppression(ContextualSearchSelectionController controller,
-                                  ContextualSearchTapState previousTapState, int x, int y) {
+            ContextualSearchTapState previousTapState, int x, int y) {
         mPxToDp = controller.getPxToDp();
         mPreviousTapState = previousTapState;
         mShouldHandleTap = shouldHandleTap(x, y);
@@ -45,8 +50,8 @@ class TapFarFromPreviousSuppression extends ContextualSearchHeuristic {
      * tap.
      */
     private boolean wasTapCloseToPreviousTap(int x, int y) {
-        float deltaXDp = (mPreviousTapState.x() - x) * mPxToDp;
-        float deltaYDp = (mPreviousTapState.y() - y) * mPxToDp;
+        float deltaXDp = (mPreviousTapState.getX() - x) * mPxToDp;
+        float deltaYDp = (mPreviousTapState.getY() - y) * mPxToDp;
         float distanceSquaredDp = deltaXDp * deltaXDp + deltaYDp * deltaYDp;
         return distanceSquaredDp <= RETAP_DISTANCE_SQUARED_DP;
     }
