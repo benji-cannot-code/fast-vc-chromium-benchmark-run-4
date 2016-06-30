0a30b68c5bac394f89935b4f91b6069f5d301525
pkotwicz <pkotwicz@chromium.org>
2016-06-30 16:18:08 -0700

    Move WebApkSandboxedProcessService to webapk/shell_apk
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2107573002
    Cr-Commit-Position: refs/heads/master@{#403330}

--- chrome/android/webapk/shell_apk/src/org/chromium/webapk/shell_apk/WebApkSandboxedProcessService.java
+++ chrome/android/webapk/shell_apk/src/org/chromium/webapk/shell_apk/WebApkSandboxedProcessService.java
@@ -3,13 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.webapk.lib.common;
+package org.chromium.webapk.shell_apk;
 
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
+
+import org.chromium.webapk.lib.common.WebApkUtils;
 
 import java.lang.reflect.Method;
 
--- content/public/android/java/src/org/chromium/content/browser/ChildProcessConnectionImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/ChildProcessConnectionImpl.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser;
 
-import android.app.Service;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -35,7 +34,6 @@ public class ChildProcessConnectionImpl implements ChildProcessConnection {
     private final int mServiceNumber;
     private final boolean mInSandbox;
     private final ChildProcessConnection.DeathCallback mDeathCallback;
-    private final Class<? extends Service> mServiceClass;
     private final ComponentName mServiceName;
 
     // Synchronization: While most internal flow occurs on the UI thread, the public API
@@ -212,7 +210,7 @@ public class ChildProcessConnectionImpl implements ChildProcessConnection {
 
     ChildProcessConnectionImpl(Context context, int number, boolean inSandbox,
             ChildProcessConnection.DeathCallback deathCallback,
-            Class<? extends Service> serviceClass,
+            String serviceClassName,
             ChromiumLinkerParams chromiumLinkerParams,
             boolean alwaysInForeground,
             ChildProcessCreationParams creationParams) {
@@ -220,10 +218,9 @@ public class ChildProcessConnectionImpl implements ChildProcessConnection {
         mServiceNumber = number;
         mInSandbox = inSandbox;
         mDeathCallback = deathCallback;
-        mServiceClass = serviceClass;
         String packageName =
                 creationParams != null ? creationParams.getPackageName() : context.getPackageName();
-        mServiceName = new ComponentName(packageName, mServiceClass.getName() + mServiceNumber);
+        mServiceName = new ComponentName(packageName, serviceClassName + mServiceNumber);
         mLinkerParams = chromiumLinkerParams;
         mAlwaysInForeground = alwaysInForeground;
         mCreationParams = creationParams;
