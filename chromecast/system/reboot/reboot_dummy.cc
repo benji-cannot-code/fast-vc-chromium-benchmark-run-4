2e7a9670cc2d50f975a9db1e7005bd2fe4318cca
ameyak <ameyak@chromium.org>
2016-06-29 17:11:43 -0700

    [Chromecast] Add device reboot utilities
    Provide various utilities for rebooting the device, including
    requesting reboots and querying for information about past
    reboots. On platforms which don't support this interface, a
    dummy implementation will be linked in.
    
    BUG=internal b/28817536
    TEST=pepperoni builds
    
    Review-Url: https://codereview.chromium.org/2003423006
    Cr-Commit-Position: refs/heads/master@{#403008}

---
+++ chromecast/system/reboot/reboot_dummy.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Dummy implementation of reboot.h
+
+#include "base/logging.h"
+#include "chromecast/system/reboot/reboot.h"
+
+namespace chromecast {
+
+RebootType GetLastRebootType() {
+  return RebootType::UNKNOWN;
+}
+
+base::Time GetLastRebootTime() {
+  return base::Time();
+}
+
+bool DoReboot(RebootType type) {
+  LOG(ERROR) << "Dummy DoReboot called, no action taken";
+  return false;
+}
+
+bool DoRebootApi(RebootCommand command) {
+  LOG(ERROR) << "Dummy DoRebootApi called, no action taken";
+  return false;
+}
+
+}  // namespace chromecast
