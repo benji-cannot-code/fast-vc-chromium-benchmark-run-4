f7e107c4a9ce1d38ac98eb071d47d3aded713444
kirr <kirr@yandex-team.ru>
2016-06-30 06:50:55 -0700

    MacViews: Ensure child windows are only managed with real changes to -[NSWindow isVisible].
    After restoring from a miniaturize, child windows are currently reattached too
    early. This is because AppKit draws the window before updating the result of
    -[NSWindow isVisible], and we need to participate in that draw. But updating
    child window relationships at this time causes layering issues when the window
    has finished restoring.
    
    BUG=620266
    
    Review-Url: https://codereview.chromium.org/2069103004
    Cr-Commit-Position: refs/heads/master@{#403166}

--- ui/views/cocoa/views_nswindow_delegate.mm
+++ ui/views/cocoa/views_nswindow_delegate.mm
@@ -36,16 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [parent_->ns_window() resetCursorRects];
 }
 
-- (void)onWindowOrderWillChange:(NSWindowOrderingMode)orderingMode {
-  parent_->OnVisibilityChangedTo(orderingMode != NSWindowOut);
-}
-
 - (void)onWindowOrderChanged:(NSNotification*)notification {
   parent_->OnVisibilityChanged();
-}
-
-- (void)onWindowWillDisplay {
-  parent_->OnVisibilityChangedTo(true);
 }
 
 - (void)sheetDidEnd:(NSWindow*)sheet
