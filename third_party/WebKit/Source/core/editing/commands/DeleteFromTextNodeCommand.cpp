866108779dbbea7e447b9babcfe4f4cdee3c387c
yosin <yosin@chromium.org>
2016-06-29 19:34:24 -0700

    Move |updateStyleAndLayout()| call from |updateTextLayoutObject()| to call sites
    This patch moves |updateStyleAndLayout()| call from |updateTextLayoutObject()|
    in |Text| to call sites for a preparation of getting rid of
    |DeprecatedRecalcStyleImmediatlelyForEditing|.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2102423002
    Cr-Commit-Position: refs/heads/master@{#403073}

--- third_party/WebKit/Source/core/editing/commands/DeleteFromTextNodeCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/DeleteFromTextNodeCommand.cpp
@@ -55,7 +55,8 @@ void DeleteFromTextNodeCommand::doApply(EditingState*)
     if (exceptionState.hadException())
         return;
 
-    m_node->deleteData(m_offset, m_count, exceptionState, CharacterData::DeprecatedRecalcStyleImmediatlelyForEditing);
+    m_node->deleteData(m_offset, m_count, exceptionState);
+    m_node->document().updateStyleAndLayout();
 }
 
 void DeleteFromTextNodeCommand::doUnapply()
@@ -65,7 +66,8 @@ void DeleteFromTextNodeCommand::doUnapply()
     if (!m_node->hasEditableStyle())
         return;
 
-    m_node->insertData(m_offset, m_text, IGNORE_EXCEPTION, CharacterData::DeprecatedRecalcStyleImmediatlelyForEditing);
+    m_node->insertData(m_offset, m_text, IGNORE_EXCEPTION);
+    m_node->document().updateStyleAndLayout();
 }
 
 DEFINE_TRACE(DeleteFromTextNodeCommand)
--- third_party/WebKit/Source/core/editing/commands/InsertIntoTextNodeCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/InsertIntoTextNodeCommand.cpp
@@ -60,7 +60,8 @@ void InsertIntoTextNodeCommand::doApply(EditingState*)
             layoutText->momentarilyRevealLastTypedCharacter(m_offset + m_text.length() - 1);
     }
 
-    m_node->insertData(m_offset, m_text, IGNORE_EXCEPTION, CharacterData::DeprecatedRecalcStyleImmediatlelyForEditing);
+    m_node->insertData(m_offset, m_text, IGNORE_EXCEPTION);
+    document().updateStyleAndLayout();
 }
 
 void InsertIntoTextNodeCommand::doUnapply()
@@ -68,7 +69,8 @@ void InsertIntoTextNodeCommand::doUnapply()
     if (!m_node->hasEditableStyle())
         return;
 
-    m_node->deleteData(m_offset, m_text.length(), IGNORE_EXCEPTION, CharacterData::DeprecatedRecalcStyleImmediatlelyForEditing);
+    m_node->deleteData(m_offset, m_text.length(), IGNORE_EXCEPTION);
+    document().updateStyleAndLayout();
 }
 
 DEFINE_TRACE(InsertIntoTextNodeCommand)
