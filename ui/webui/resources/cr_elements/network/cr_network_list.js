5ead692a3591353d39529acd411d2d5ce4c3aa3c
alemate <alemate@chromium.org>
2016-06-29 21:53:41 -0700

    Add support for custom entries in <cr_network_select/>.
    This CL adds support for custom menu entries in <cr_network_select/> .
    
    BUG=604119
    TEST=none
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2069323002
    Cr-Commit-Position: refs/heads/master@{#403090}

--- ui/webui/resources/cr_elements/network/cr_network_list.js
+++ ui/webui/resources/cr_elements/network/cr_network_list.js
@@ -39,10 +39,22 @@ Polymer({
 
     /**
      * The list of network state properties for the items to display.
+     * See <cr-network-list-network-item/> for details.
      *
      * @type {!Array<!CrOnc.NetworkStateProperties>}
      */
     networks: {
+      type: Array,
+      value: function() { return []; }
+    },
+
+    /**
+     * The list of custom state properties for the items to display.
+     * See <cr-network-list-custom-item/> for details.
+     *
+     * @type {!Array<Object>}
+     */
+    customItems: {
       type: Array,
       value: function() { return []; }
     },
@@ -53,7 +65,15 @@ Polymer({
     opened: {
       type: Boolean,
       value: true
-    }
+    },
+
+    /**
+     * Shows all buttons from the list items.
+     */
+    showButtons: {
+      type: Boolean,
+      value: false,
+    },
   },
 
   /**
---
+++ ui/webui/resources/cr_elements/network/cr_network_list_types.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ *
+ * This file contains typedefs <cr-network-list/> properties.
+ */
+
+var CrNetworkList = {};
+
+/**
+ * Generic managed property type. This should match any of the basic managed
+ * types in chrome.networkingPrivate, e.g. networkingPrivate.ManagedBoolean.
+ * @typedef {{
+ *   customItemName: (!String|undefined),
+ *   polymerIcon: (!String|undefined),
+ *   customData: (!Object|undefined),
+ * }}
+ */
+CrNetworkList.CustomItemState;
