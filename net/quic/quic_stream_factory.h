0b4589cb6f7c4b691c5862e424fb3463651c5f57
nharper <nharper@chromium.org>
2016-06-29 17:40:36 -0700

    Add QuicStreamFactory as an observer of an SSLConfigService
    QuicStreamFactory implements SSLConfigService::Observer, but it never
    was added as an observer to an SSLConfigService. Now
    QuicStreamFactory::OnSSLConfigChanged will actually get called, and QUIC
    streams will get torn down when the SSLConfig changes.
    
    BUG=548423
    
    Review-Url: https://codereview.chromium.org/2109303002
    Cr-Commit-Position: refs/heads/master@{#403038}

--- net/quic/quic_stream_factory.h
+++ net/quic/quic_stream_factory.h
@@ -151,6 +151,7 @@ class NET_EXPORT_PRIVATE QuicStreamFactory
   QuicStreamFactory(
       NetLog* net_log,
       HostResolver* host_resolver,
+      SSLConfigService* ssl_config_service,
       ClientSocketFactory* client_socket_factory,
       HttpServerProperties* http_server_properties,
       CertVerifier* cert_verifier,
@@ -584,6 +585,8 @@ class NET_EXPORT_PRIVATE QuicStreamFactory
   FactoryStatus status_;
 
   base::TaskRunner* task_runner_;
+
+  const scoped_refptr<SSLConfigService> ssl_config_service_;
 
   base::WeakPtrFactory<QuicStreamFactory> weak_factory_;
 
