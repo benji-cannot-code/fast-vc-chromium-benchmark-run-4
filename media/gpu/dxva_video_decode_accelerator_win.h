def0a26346bd9fd1e03efda47755968374d1030b
jbauman <jbauman@chromium.org>
2016-06-29 17:14:41 -0700

    Allow copying frames to NV12 DXGI picture buffers.
    Add support for creating NV12 pictures buffers, and using the video
    processor to copy frames from the decoder texture to them. Using NV12
    instead of ARGB reduces 1080p60 power consumption on my system from 4.8W
    to 4.4W.
    
    This is disabled by default, but can be enabled with the
    --enable-nv12-dxgi-video flag.
    
    BUG=574292
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2087113002
    Cr-Commit-Position: refs/heads/master@{#403011}

--- media/gpu/dxva_video_decode_accelerator_win.h
+++ media/gpu/dxva_video_decode_accelerator_win.h
@@ -55,6 +55,7 @@ typedef HRESULT(WINAPI* CreateDXGIDeviceManager)(
 
 namespace media {
 class DXVAPictureBuffer;
+class EGLStreamCopyPictureBuffer;
 class EGLStreamPictureBuffer;
 class PbufferPictureBuffer;
 
@@ -137,6 +138,7 @@ class MEDIA_GPU_EXPORT DXVAVideoDecodeAccelerator
 
  private:
   friend class DXVAPictureBuffer;
+  friend class EGLStreamCopyPictureBuffer;
   friend class EGLStreamPictureBuffer;
   friend class PbufferPictureBuffer;
   typedef void* EGLConfig;
@@ -362,6 +364,7 @@ class MEDIA_GPU_EXPORT DXVAVideoDecodeAccelerator
   base::win::ScopedComPtr<IDirect3DQuery9> query_;
 
   base::win::ScopedComPtr<ID3D11Device> d3d11_device_;
+  base::win::ScopedComPtr<ID3D11Device> angle_device_;
   base::win::ScopedComPtr<IMFDXGIDeviceManager> d3d11_device_manager_;
   base::win::ScopedComPtr<ID3D10Multithread> multi_threaded_;
   base::win::ScopedComPtr<ID3D11DeviceContext> d3d11_device_context_;
@@ -466,6 +469,9 @@ class MEDIA_GPU_EXPORT DXVAVideoDecodeAccelerator
   bool pending_flush_;
 
   bool share_nv12_textures_;
+
+  // Copy NV12 texture to another NV12 texture.
+  bool copy_nv12_textures_;
 
   // Defaults to false. Indicates if we should use D3D or DX11 interfaces for
   // H/W decoding.
