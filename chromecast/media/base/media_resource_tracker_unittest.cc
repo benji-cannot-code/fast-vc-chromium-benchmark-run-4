705bdae6b809e4b9b7545af7d026c3d03e36118a
halliwell <halliwell@chromium.org>
2016-06-29 17:13:38 -0700

    [Chromecast] Remove MediaResourceTracker::Delegate
    The only implementation of this delegate has been removed now.
    
    BUG=internal b/29204860
    
    Review-Url: https://codereview.chromium.org/2104253002
    Cr-Commit-Position: refs/heads/master@{#403010}

--- chromecast/media/base/media_resource_tracker_unittest.cc
+++ chromecast/media/base/media_resource_tracker_unittest.cc
@@ -31,16 +31,12 @@ void RunUntilIdle(base::TaskRunner* task_runner) {
 }
 
 // Collection of mocks to verify MediaResourceTracker takes the correct actions.
-class MediaResourceTrackerTestMocks : public MediaResourceTracker::Delegate {
+class MediaResourceTrackerTestMocks {
  public:
   MOCK_METHOD0(Initialize, void());  // CastMediaShlib::Initialize
   MOCK_METHOD0(Finalize, void());  // CastMediaShlib::Finalize
   MOCK_METHOD0(Destroyed, void());  // ~CastMediaResourceTracker
   MOCK_METHOD0(FinalizeCallback, void());  // callback to Finalize
-
-  // MediaResourceTracker::Delegate implementation:
-  MOCK_METHOD0(OnStartUsingMedia, void());
-  MOCK_METHOD0(OnStopUsingMedia, void());
 };
 
 class TestMediaResourceTracker : public MediaResourceTracker {
@@ -86,7 +82,6 @@ class MediaResourceTrackerTest : public ::testing::Test {
     resource_tracker_ = new TestMediaResourceTracker(
         test_mocks_.get(), message_loop_->task_runner(),
         media_task_runner_);
-    resource_tracker_->SetDelegate(test_mocks_.get());
   }
 
   void TearDown() override { media_thread_.reset(); }
@@ -176,7 +171,6 @@ TEST_F(MediaResourceTrackerTest, FinalizeResourceInUse) {
   resource_tracker_->InitializeMediaLib();
 
   IncrementMediaUsageCount();
-  EXPECT_CALL(*test_mocks_, OnStartUsingMedia()).Times(1);
   RunUntilIdle(media_task_runner_.get());
   base::RunLoop().RunUntilIdle();
 
@@ -191,7 +185,6 @@ TEST_F(MediaResourceTrackerTest, FinalizeResourceInUse) {
   DecrementMediaUsageCount();
 
   EXPECT_CALL(*test_mocks_, FinalizeCallback()).Times(1);
-  EXPECT_CALL(*test_mocks_, OnStopUsingMedia()).Times(1);
   RunUntilIdle(media_task_runner_.get());
   base::RunLoop().RunUntilIdle();
 
@@ -208,7 +201,6 @@ TEST_F(MediaResourceTrackerTest, DestroyWaitForNoUsers) {
   resource_tracker_->InitializeMediaLib();
 
   IncrementMediaUsageCount();
-  EXPECT_CALL(*test_mocks_, OnStartUsingMedia()).Times(1);
   RunUntilIdle(media_task_runner_.get());
   base::RunLoop().RunUntilIdle();
 
@@ -217,8 +209,6 @@ TEST_F(MediaResourceTrackerTest, DestroyWaitForNoUsers) {
   resource_tracker_->FinalizeAndDestroy();
   RunUntilIdle(media_task_runner_.get());
 
-  // Note, OnStop delegate call should not be made during shutdown
-  EXPECT_CALL(*test_mocks_, OnStopUsingMedia()).Times(0);
   EXPECT_CALL(*test_mocks_, Finalize()).Times(1);
   EXPECT_CALL(*test_mocks_, Destroyed()).Times(1);
   DecrementMediaUsageCount();
@@ -234,7 +224,6 @@ TEST_F(MediaResourceTrackerTest, DestroyWithPendingFinalize) {
   resource_tracker_->InitializeMediaLib();
 
   IncrementMediaUsageCount();
-  EXPECT_CALL(*test_mocks_, OnStartUsingMedia()).Times(1);
   RunUntilIdle(media_task_runner_.get());
   base::RunLoop().RunUntilIdle();
 
@@ -249,7 +238,6 @@ TEST_F(MediaResourceTrackerTest, DestroyWithPendingFinalize) {
   EXPECT_CALL(*test_mocks_, Finalize()).Times(1);
   EXPECT_CALL(*test_mocks_, Destroyed()).Times(1);
   EXPECT_CALL(*test_mocks_, FinalizeCallback()).Times(1);
-  EXPECT_CALL(*test_mocks_, OnStopUsingMedia()).Times(0);
 
   DecrementMediaUsageCount();
 
