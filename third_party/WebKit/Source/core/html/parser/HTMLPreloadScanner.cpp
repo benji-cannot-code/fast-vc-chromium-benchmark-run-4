80bafca6cb9bc6254fd3001756b6d903dd9f3a39
csharrison <csharrison@chromium.org>
2016-06-30 08:30:45 -0700

    Don't preload scripts with invalid type/language attributes
    This patch keeps track of a script tag's type and language attributes,
    and gates preloading if they are invalid. This aligns the preload scanner's
    policy with the ScriptLoader's policy.
    
    BUG=623109
    
    Review-Url: https://codereview.chromium.org/2099853002
    Cr-Commit-Position: refs/heads/master@{#403182}

--- third_party/WebKit/Source/core/html/parser/HTMLPreloadScanner.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLPreloadScanner.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/MediaValuesCached.h"
 #include "core/css/parser/SizesAttributeParser.h"
 #include "core/dom/Document.h"
+#include "core/dom/ScriptLoader.h"
 #include "core/fetch/IntegrityMetadata.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/Settings.h"
@@ -222,7 +223,6 @@ public:
             }
         }
 
-
         TextPosition position = TextPosition(source.currentLine(), source.currentColumn());
         FetchRequest::ResourceWidth resourceWidth;
         float sourceSize = m_sourceSize;
@@ -273,6 +273,10 @@ private:
         // explanation.
         else if (match(attributeName, integrityAttr))
             SubresourceIntegrity::parseIntegrityAttribute(attributeValue, m_integrityMetadata);
+        else if (match(attributeName, typeAttr))
+            m_typeAttributeValue = attributeValue;
+        else if (match(attributeName, languageAttr))
+            m_languageAttributeValue = attributeValue;
     }
 
     template<typename NameType>
@@ -441,6 +445,8 @@ private:
             return false;
         if (match(m_tagImpl, inputTag) && !m_inputIsImage)
             return false;
+        if (match(m_tagImpl, scriptTag) && !ScriptLoader::isValidScriptTypeAndLanguage(m_typeAttributeValue, m_languageAttributeValue, ScriptLoader::AllowLegacyTypeInTypeAttribute))
+            return false;
         return true;
     }
 
@@ -472,6 +478,8 @@ private:
     String m_imgSrcUrl;
     String m_srcsetAttributeValue;
     String m_asAttributeValue;
+    String m_typeAttributeValue;
+    String m_languageAttributeValue;
     float m_sourceSize;
     bool m_sourceSizeSet;
     FetchRequest::DeferOption m_defer;
