761c0f8e1d8110ead0ebace43133110b691a878d
tapted <tapted@chromium.org>
2016-06-29 17:36:12 -0700

    Mac: Use "esc" rather than "Esc" in "Press Esc to exit full screen".
    Mac keyboards use lowercase for everything except function keys, which
    are typically reserved for system use. Since |Esc| is placed in a box
    specifically to make it look like a keyboard key it looks weird to not
    follow suit.
    
    Use base::i18n::ToLower(..) rather than changing IDS_APP_ESC_KEY, since
    the latter is used by ui::Accelerator::GetShortcutText() for other
    things.
    
    This still isn't perfect. Some macbooks use the ISO symbol for escape,
    "⎋" (e.g. French Canadian). But we can't assume a user's locale setting
    always determines how the keyboard looks. Using lowercase gets us pretty
    close.
    
    BUG=623636
    
    Review-Url: https://codereview.chromium.org/2110743002
    Cr-Commit-Position: refs/heads/master@{#403035}

--- chrome/browser/ui/views/exclusive_access_bubble_views.cc
+++ chrome/browser/ui/views/exclusive_access_bubble_views.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/i18n/case_conversion.h"
 #include "base/location.h"
 #include "base/macros.h"
 #include "base/single_thread_task_runner.h"
@@ -176,6 +177,12 @@ void ExclusiveAccessBubbleViews::UpdateViewContent(
       link_visible = false;
     }
   }
+#if defined(OS_MACOSX)
+  // Mac keyboards use lowercase for everything except function keys, which are
+  // typically reserved for system use. Since |accelerator| is placed in a box
+  // to make it look like a keyboard key it looks weird to not follow suit.
+  accelerator = base::i18n::ToLower(accelerator);
+#endif
   base::string16 link_text;
   base::string16 exit_instruction_text;
   if (link_visible) {
