96a479a14004174ec5b640f41685e2063fe70718
jrummell <jrummell@chromium.org>
2016-06-29 18:18:57 -0700

    Improve data sanitization of initialization data
    Data sanitization of the initialization data now verifies data is
    provided and for WebM, that the size is not larger than a single
    key ID.
    
    BUG=622840
    TEST=EME tests pass
    
    checks for empty initData or WebM length
    
    Review-Url: https://codereview.chromium.org/2098143002
    Cr-Commit-Position: refs/heads/master@{#403053}

--- media/blink/webcontentdecryptionmodulesession_impl.cc
+++ media/blink/webcontentdecryptionmodulesession_impl.cc
@@ -105,6 +105,7 @@ static bool SanitizeInitData(EmeInitDataType init_data_type,
                              size_t init_data_length,
                              std::vector<uint8_t>* sanitized_init_data,
                              std::string* error_message) {
+  DCHECK_GT(init_data_length, 0u);
   if (init_data_length > limits::kMaxInitDataLength) {
     error_message->assign("Initialization data too long.");
     return false;
@@ -112,6 +113,11 @@ static bool SanitizeInitData(EmeInitDataType init_data_type,
 
   switch (init_data_type) {
     case EmeInitDataType::WEBM:
+      // |init_data| for WebM is a single key.
+      if (init_data_length > limits::kMaxKeyIdLength) {
+        error_message->assign("Initialization data for WebM is too long.");
+        return false;
+      }
       sanitized_init_data->assign(init_data, init_data + init_data_length);
       return true;
 
