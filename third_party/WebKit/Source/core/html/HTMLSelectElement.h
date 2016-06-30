a19026afe41e0dc292e08bf6797274276e8a3d7b
tkent <tkent@chromium.org>
2016-06-29 22:26:52 -0700

    Improve performance of HTMLSelectElement::m_listItems update.
    This CL introduces a fast path to update m_listItems directly instead of throwing
    it away and reconstructing it. The fast path supports:
     - Prepend an OPTION to a SELECT element
     - Append an OPTION to a SELECT element
     - Remove an OPTION child from a SELECT element or an OPTGROUP element.
    
    Removing 20,000 OPTIONs: 2,813ms -> 200ms
    
    BUG=620142, 624220
    
    Review-Url: https://codereview.chromium.org/2110303002
    Cr-Commit-Position: refs/heads/master@{#403093}

--- third_party/WebKit/Source/core/html/HTMLSelectElement.h
+++ third_party/WebKit/Source/core/html/HTMLSelectElement.h
@@ -91,7 +91,6 @@ public:
 
     void optionElementChildrenChanged(const HTMLOptionElement&);
 
-    void setRecalcListItems();
     void invalidateSelectedItems();
 
     using ListItems = HeapVector<Member<HTMLElement>>;
@@ -123,11 +122,11 @@ public:
     // For use in the implementation of HTMLOptionElement.
     void optionSelectionStateChanged(HTMLOptionElement*, bool optionIsSelected);
     void optionInserted(HTMLOptionElement&, bool optionIsSelected);
-    void optionRemoved(const HTMLOptionElement&);
+    void optionRemoved(HTMLOptionElement&);
     bool anonymousIndexedSetter(unsigned, HTMLOptionElement*, ExceptionState&);
 
-    void optGroupInsertedOrRemoved(const HTMLOptGroupElement&);
-    void hrInsertedOrRemoved(const HTMLHRElement&);
+    void optGroupInsertedOrRemoved(HTMLOptGroupElement&);
+    void hrInsertedOrRemoved(HTMLHRElement&);
 
     void updateListOnLayoutObject();
 
@@ -198,6 +197,8 @@ private:
 
     void dispatchInputAndChangeEventForMenuList();
 
+    // |subject| is an element which was inserted or removed.
+    void setRecalcListItems(HTMLElement& subject);
     void recalcListItems() const;
     enum ResetReason {
         ResetReasonSelectedOptionRemoved,
