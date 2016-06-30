e59ea2c557814e49136f01351674660c63ea0e85
pcc <pcc@chromium.org>
2016-06-30 16:34:04 -0700

    Move TestExtraData classes to anonymous namespace.
    We were previously declaring two different classes with the name
    blink::TestExtraData, which is an ODR violation. As it happens,
    their implementations compiled to the same machine code, which
    allowed the tests to pass in most normal circumstances. However,
    the ODR violation causes test failures in CFI mode (e.g. [1]) as a
    result of stricter type checking. The fix is to move both classes to
    an anonymous namespace which makes them internal to their TUs.
    
    [1] https://build.chromium.org/p/chromium.fyi/builders/CFI%20Linux/builds/5924/steps/webkit_unit_tests/logs/WebURLResponseTest.ExtraData
    
    BUG=464797
    R=thakis@chromium.org
    
    Review-Url: https://codereview.chromium.org/2119683002
    Cr-Commit-Position: refs/heads/master@{#403338}

--- third_party/WebKit/Source/web/tests/WebURLRequestTest.cpp
+++ third_party/WebKit/Source/web/tests/WebURLRequestTest.cpp
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+namespace {
+
 class TestExtraData : public WebURLRequest::ExtraData {
 public:
     explicit TestExtraData(bool* alive)
@@ -48,6 +50,8 @@ public:
 private:
     bool* m_alive;
 };
+
+} // anonymous namespace
 
 TEST(WebURLRequestTest, ExtraData)
 {
