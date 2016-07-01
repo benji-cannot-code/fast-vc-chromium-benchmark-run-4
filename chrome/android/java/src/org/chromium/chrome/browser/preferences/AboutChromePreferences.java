70944871158f287ff8eb920c0a95b58f02b522b2
tedchoc <tedchoc@chromium.org>
2016-07-01 15:31:09 -0700

    ManageSpace activity - Do not load browser on insta crash.
    Use a preference to track whether the managed space activity
    successfully completes before attempting to load the browser
    process on the next startup.
    
    This should ensure that native crashes in the renderer process
    do not prevent users from clearing their data.
    
    It uses the current product version to allow the browser process
    to be attempted after upgrades regardless of previous failures.
    
    BUG=571461
    
    Review-Url: https://codereview.chromium.org/2111813003
    Cr-Commit-Position: refs/heads/master@{#403553}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/AboutChromePreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/AboutChromePreferences.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.preferences;
 
+import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Build;
@@ -48,7 +49,7 @@ public class AboutChromePreferences extends PreferenceFragment {
         PrefServiceBridge prefServiceBridge = PrefServiceBridge.getInstance();
         AboutVersionStrings versionStrings = prefServiceBridge.getAboutVersionStrings();
         Preference p = findPreference(PREF_APPLICATION_VERSION);
-        p.setSummary(getApplicationVersion(versionStrings.getApplicationVersion()));
+        p.setSummary(getApplicationVersion(getActivity(), versionStrings.getApplicationVersion()));
         p = findPreference(PREF_OS_VERSION);
         p.setSummary(versionStrings.getOSVersion());
         p = findPreference(PREF_LEGAL_INFORMATION);
@@ -56,7 +57,11 @@ public class AboutChromePreferences extends PreferenceFragment {
         p.setSummary(getString(R.string.legal_information_summary, currentYear));
     }
 
-    private String getApplicationVersion(String version) {
+    /**
+     * Build the application version to be shown.  In particular, this ensures the debug build
+     * versions are more useful.
+     */
+    public static String getApplicationVersion(Context context, String version) {
         if (ChromeVersionInfo.isOfficialBuild()) {
             return version;
         }
@@ -64,14 +69,14 @@ public class AboutChromePreferences extends PreferenceFragment {
         // For developer builds, show how recently the app was installed/updated.
         PackageInfo info;
         try {
-            info = getActivity().getPackageManager().getPackageInfo(
-                    getActivity().getPackageName(), 0);
+            info = context.getPackageManager().getPackageInfo(
+                    context.getPackageName(), 0);
         } catch (NameNotFoundException e) {
             return version;
         }
         CharSequence updateTimeString = DateUtils.getRelativeTimeSpanString(
                 info.lastUpdateTime, System.currentTimeMillis(), 0);
-        return getActivity().getString(R.string.version_with_update_time, version,
+        return context.getString(R.string.version_with_update_time, version,
                 updateTimeString);
     }
 }
