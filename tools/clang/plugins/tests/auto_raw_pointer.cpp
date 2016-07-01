f9b22731f96cb3d75115afa7040d71263df7f7b7
Vladimir Levin <vmpstr@chromium.org>
2016-07-01 13:57:11 -0700

    clang-plugin: Remove system include from auto_raw_pointer test.
    This patch removes a system include from a test that is meant to be
    standalone.
    
    R=thakis@chromium.org, dcheng
    
    Review URL: https://codereview.chromium.org/2118653003 .
    
    Cr-Commit-Position: refs/heads/master@{#403527}

--- tools/clang/plugins/tests/auto_raw_pointer.cpp
+++ tools/clang/plugins/tests/auto_raw_pointer.cpp
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <utility>
-
 class Foo {
  public:
   void foo() {}
@@ -34,7 +32,7 @@ int main() {
   auto* raw_foo_ptr_valid = &foo;
   const auto* const_raw_foo_ptr_valid = &foo;
 
-  int *int_ptr;
+  int* int_ptr;
 
   auto double_ptr_auto = &int_ptr;
   auto* double_ptr_auto_ptr = &int_ptr;
@@ -43,11 +41,11 @@ int main() {
   auto function_ptr = &f;
   auto method_ptr = &Foo::foo;
 
-  int *const *const volatile **const *pointer_awesomeness;
+  int* const* const volatile** const* pointer_awesomeness;
   auto auto_awesome = pointer_awesomeness;
 
   auto& int_ptr_ref = int_ptr;
   const auto& const_int_ptr_ref = int_ptr;
-  auto&& int_ptr_rref = std::move(int_ptr);
-  const auto&& const_int_ptr_rref = std::move(int_ptr);
+  auto&& int_ptr_rref = static_cast<int*&&>(int_ptr);
+  const auto&& const_int_ptr_rref = static_cast<int*&&>(int_ptr);
 }
