cb62b5e8da7076a380c426ef8fbd11f474035df0
miu <miu@chromium.org>
2016-06-30 18:00:14 -0700

    Add experimental 'backlog' heuristic for measuring encoder utilization.
    The existing 'deadline' heuristic has proven to be very flawed on some
    systems, for reasons explained in the bug. This change adds a new
    experimental heuristic, activated via a new command-line switch, for lab
    testing: --cast-encoder-util-heuristic=backlog<N>
    
    If the new heuristic proves to work well across our lab's gamut of test
    systems, a future change will remove the command-line switch and make
    the new heuristic the default.
    
    BUG=chrome-os-partner:54806
    
    Review-Url: https://codereview.chromium.org/2108373002
    Cr-Commit-Position: refs/heads/master@{#403378}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1366,6 +1366,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kAudioBufferSize,
     switches::kBlinkPlatformLogChannels,
     switches::kBlinkSettings,
+    switches::kCastEncoderUtilHeuristic,
     switches::kDefaultTileWidth,
     switches::kDefaultTileHeight,
     switches::kDisable2dCanvasImageChromium,
