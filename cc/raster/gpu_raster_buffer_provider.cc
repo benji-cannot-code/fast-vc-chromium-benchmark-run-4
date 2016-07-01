265d492447ea745af45b19889028e675e0f17cd7
ericrk <ericrk@chromium.org>
2016-07-01 10:30:16 -0700

    Split partial raster histograms by Browser/Renderer
    Currently, the partial raster histograms combine data from Browser/
    Renderer processes. This leads to misleading comparisons between GPU/SW,
    as we always use SW in the browser process, which performs very
    different rendering.
    
    This change splits Renderer4.PArtialRasterPercentageSaved into
    Renderer4.Browser.Partial... and Renderer4.Renderer.Partial...
    
    R=enne@chromium.org,isherman@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review-Url: https://codereview.chromium.org/2114433003
    Cr-Commit-Position: refs/heads/master@{#403477}

--- cc/raster/gpu_raster_buffer_provider.cc
+++ cc/raster/gpu_raster_buffer_provider.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/trace_event/trace_event.h"
+#include "cc/base/histograms.h"
 #include "cc/playback/raster_source.h"
 #include "cc/raster/scoped_gpu_raster.h"
 #include "cc/resources/resource.h"
@@ -45,14 +46,16 @@ static sk_sp<SkPicture> PlaybackToPicture(
 
   // Log a histogram of the percentage of pixels that were saved due to
   // partial raster.
+  const char* client_name = GetClientNameForMetrics();
   float full_rect_size = raster_full_rect.size().GetArea();
-  if (full_rect_size > 0) {
+  if (full_rect_size > 0 && client_name) {
     float fraction_partial_rastered =
         static_cast<float>(playback_rect.size().GetArea()) / full_rect_size;
     float fraction_saved = 1.0f - fraction_partial_rastered;
-
-    UMA_HISTOGRAM_PERCENTAGE("Renderer4.PartialRasterPercentageSaved.Gpu",
-                             100.0f * fraction_saved);
+    UMA_HISTOGRAM_PERCENTAGE(
+        base::StringPrintf("Renderer4.%s.PartialRasterPercentageSaved.Gpu",
+                           client_name),
+        100.0f * fraction_saved);
   }
 
   // Play back raster_source into temp SkPicture.
