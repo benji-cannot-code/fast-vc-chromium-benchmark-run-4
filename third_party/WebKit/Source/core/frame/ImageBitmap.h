968a3c51d23ace51a24e474cb91fab977398d25d
xidachen <xidachen@chromium.org>
2016-07-01 10:10:14 -0700

    Change code path for structured cloning ImageBitmap
    This is how it works currently:
    When an ImageBitmap is un-premultiplied, we read bitmap's pixel data in
    premultiplied format. When de-serialize happens, we call
    ImageBitmap::create(ImageData*) and in that create() function, we create
    an SkImage that is in premultiplied format because the input data is
    already premultiplied. This is causing the problem because the SkImage
    should be in un-premultiplied format.
    
    We change the implementation like this:
    When we serialize the ImageBitmap into uint8_t*, we read the pixel
    data according to whether the ImageBitmap is premultiplied or not,
    also we store the information whether the bitmap is premultiplied or
    not. When de-serialization happens, we design a new ImageBitmap
    constructor specifically for it.
    
    Notice that after this change, the layout tests
    fast/canvas/webgl/texImage-imageBitmap-structured-clone.html
    passes.
    
    BUG=615172
    
    Review-Url: https://codereview.chromium.org/2039983002
    Cr-Commit-Position: refs/heads/master@{#403475}

--- third_party/WebKit/Source/core/frame/ImageBitmap.h
+++ third_party/WebKit/Source/core/frame/ImageBitmap.h
@@ -31,6 +31,10 @@ enum AlphaDisposition {
     PremultiplyAlpha,
     DontPremultiplyAlpha,
 };
+enum DataColorFormat {
+    RGBAColorType,
+    N32ColorType,
+};
 
 class CORE_EXPORT ImageBitmap final : public GarbageCollectedFinalized<ImageBitmap>, public ScriptWrappable, public CanvasImageSource, public ImageBitmapSource {
     DEFINE_WRAPPERTYPEINFO();
@@ -38,11 +42,15 @@ public:
     static ImageBitmap* create(HTMLImageElement*, const IntRect&, Document*, const ImageBitmapOptions& = ImageBitmapOptions());
     static ImageBitmap* create(HTMLVideoElement*, const IntRect&, Document*, const ImageBitmapOptions& = ImageBitmapOptions());
     static ImageBitmap* create(HTMLCanvasElement*, const IntRect&, const ImageBitmapOptions& = ImageBitmapOptions());
-    static ImageBitmap* create(ImageData*, const IntRect&, const ImageBitmapOptions& = ImageBitmapOptions(), const bool& isImageDataPremultiplied = false, const bool& isImageDataOriginClean = true);
+    static ImageBitmap* create(ImageData*, const IntRect&, const ImageBitmapOptions& = ImageBitmapOptions());
     static ImageBitmap* create(ImageBitmap*, const IntRect&, const ImageBitmapOptions& = ImageBitmapOptions());
     static ImageBitmap* create(PassRefPtr<StaticBitmapImage>);
     static ImageBitmap* create(PassRefPtr<StaticBitmapImage>, const IntRect&, const ImageBitmapOptions& = ImageBitmapOptions());
     static ImageBitmap* create(WebExternalTextureMailbox&);
+    // This function is called by structured-cloning an ImageBitmap.
+    // isImageBitmapPremultiplied indicates whether the original ImageBitmap is premultiplied or not.
+    // isImageBitmapOriginClean indicates whether the original ImageBitmap is origin clean or not.
+    static ImageBitmap* create(std::unique_ptr<uint8_t[]> data, uint32_t width, uint32_t height, bool isImageBitmapPremultiplied, bool isImageBitmapOriginClean);
     static PassRefPtr<SkImage> getSkImageFromDecoder(std::unique_ptr<ImageDecoder>);
 
     // Type and helper function required by CallbackPromiseAdapter:
@@ -50,7 +58,7 @@ public:
     static ImageBitmap* take(ScriptPromiseResolver*, sk_sp<SkImage>);
 
     StaticBitmapImage* bitmapImage() const { return (m_image) ? m_image.get() : nullptr; }
-    std::unique_ptr<uint8_t[]> copyBitmapData(AlphaDisposition alphaOp = DontPremultiplyAlpha);
+    std::unique_ptr<uint8_t[]> copyBitmapData(AlphaDisposition alphaOp = DontPremultiplyAlpha, DataColorFormat format = RGBAColorType);
     unsigned long width() const;
     unsigned long height() const;
     IntSize size() const;
@@ -83,11 +91,12 @@ private:
     ImageBitmap(HTMLImageElement*, const IntRect&, Document*, const ImageBitmapOptions&);
     ImageBitmap(HTMLVideoElement*, const IntRect&, Document*, const ImageBitmapOptions&);
     ImageBitmap(HTMLCanvasElement*, const IntRect&, const ImageBitmapOptions&);
-    ImageBitmap(ImageData*, const IntRect&, const ImageBitmapOptions&, const bool&, const bool&);
+    ImageBitmap(ImageData*, const IntRect&, const ImageBitmapOptions&);
     ImageBitmap(ImageBitmap*, const IntRect&, const ImageBitmapOptions&);
     ImageBitmap(PassRefPtr<StaticBitmapImage>);
     ImageBitmap(PassRefPtr<StaticBitmapImage>, const IntRect&, const ImageBitmapOptions&);
     ImageBitmap(WebExternalTextureMailbox&);
+    ImageBitmap(std::unique_ptr<uint8_t[]> data, uint32_t width, uint32_t height, bool isImageBitmapPremultiplied, bool isImageBitmapOriginClean);
 
     void parseOptions(const ImageBitmapOptions&, bool&, bool&);
 
