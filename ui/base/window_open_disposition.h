727a139e62a7c16f54f8316810abf80daf8c4213
pkasting <pkasting@chromium.org>
2016-07-01 13:45:42 -0700

    Remove SUPPRESS_OPEN disposition.
    This is never used and would be handled identically to IGNORE_ACTION anyway.
    
    This also adds a missing OWNERS file to silence a PRESUBMIT complaint.
    
    BUG=30431
    TEST=none
    TBR=jam,finnur
    
    Review-Url: https://codereview.chromium.org/2107323004
    Cr-Commit-Position: refs/heads/master@{#403518}

--- ui/base/window_open_disposition.h
+++ ui/base/window_open_disposition.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // GENERATED_JAVA_ENUM_PACKAGE: org.chromium.ui
 enum WindowOpenDisposition {
   UNKNOWN,
-  SUPPRESS_OPEN,
   CURRENT_TAB,
   // Indicates that only one tab with the url should exist in the same window.
   SINGLETON_TAB,
