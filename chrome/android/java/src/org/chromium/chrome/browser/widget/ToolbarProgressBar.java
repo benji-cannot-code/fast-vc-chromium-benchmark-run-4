28458d12f4c8aa549fabcaf11891ccc4e95998de
mdjones <mdjones@chromium.org>
2016-07-01 09:30:00 -0700

    Cancel indeterminate animation on progress hidden
    The progress bar does not necessarily hit 100% before being hidden
    any more. This change cancels the indeterminate progress bar animation
    if the progress bar is hidden.
    
    BUG=624917
    
    Review-Url: https://codereview.chromium.org/2110913003
    Cr-Commit-Position: refs/heads/master@{#403465}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/ToolbarProgressBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/ToolbarProgressBar.java
@@ -96,6 +96,7 @@ public class ToolbarProgressBar extends ClipDrawableProgressBar {
         public void run() {
             animateAlphaTo(0.0f);
             mIsRunningSmoothIndeterminate = false;
+            if (mAnimatingView != null) mAnimatingView.cancelAnimation();
         }
     };
 
