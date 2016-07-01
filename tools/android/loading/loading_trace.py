1a121031b8d3a6c4c07f3f20e8434816e129a369
gabadie <gabadie@chromium.org>
2016-07-01 06:08:44 -0700

    tools/android/loading: Use ujson to parse trace JSON files if available
    BUG=582080
    
    Review-Url: https://codereview.chromium.org/2109393004
    Cr-Commit-Position: refs/heads/master@{#403441}

--- tools/android/loading/loading_trace.py
+++ tools/android/loading/loading_trace.py
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Represents the trace of a page load."""
 
 import datetime
-import json
+try:
+  import ujson as json
+except ImportError:
+  import json
 import time
 
 import devtools_monitor
