d486be725072ad0eafea6170ee6c7a72bbed25a1
bsep <bsep@chromium.org>
2016-06-30 19:36:42 -0700

    Fix date picker datalist suggestion dropdown size with use-zoom-for-dsf.
    When a date picker has a suggestion list it's supposed to be the same
    width as the input element. With use-zoom-for-dsf on I discovered that
    it was too wide because the width that was being passed in was using
    "root frame" coords that were not scaled correctly.
    
    BUG=621221
    
    Review-Url: https://codereview.chromium.org/2095113002
    Cr-Commit-Position: refs/heads/master@{#403394}

--- third_party/WebKit/Source/core/html/HTMLInputElementTest.cpp
+++ third_party/WebKit/Source/core/html/HTMLInputElementTest.cpp
@@ -6,9 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLInputElement.h"
 
 #include "core/dom/Document.h"
+#include "core/frame/FrameHost.h"
+#include "core/frame/FrameView.h"
+#include "core/frame/VisualViewport.h"
 #include "core/html/HTMLBodyElement.h"
 #include "core/html/HTMLFormElement.h"
 #include "core/html/HTMLHtmlElement.h"
+#include "core/html/forms/DateTimeChooser.h"
 #include "core/testing/DummyPageHolder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include <memory>
@@ -83,6 +87,22 @@ TEST(HTMLInputElementTest, ImageTypeCrash)
     // |value| doesn't crash.
     input->ensurePrimaryContent();
     input->setAttribute(HTMLNames::valueAttr, "aaa");
+}
+
+TEST(HTMLInputElementTest, DateTimeChooserSizeParamRespectsScale)
+{
+    std::unique_ptr<DummyPageHolder> pageHolder = DummyPageHolder::create();
+    Document* document = &(pageHolder->document());
+    document->view()->frame().host()->visualViewport().setScale(2.f);
+    document->body()->setInnerHTML("<input type='date' style='width:200px;height:50px' />", ASSERT_NO_EXCEPTION);
+    document->view()->updateAllLifecyclePhases();
+    HTMLInputElement* input = toHTMLInputElement(document->body()->firstChild());
+
+    DateTimeChooserParameters params;
+    bool success = input->setupDateTimeChooserParameters(params);
+    EXPECT_TRUE(success);
+    EXPECT_EQ("date", params.type);
+    EXPECT_EQ(IntRect(16, 16, 400, 100), params.anchorRectInScreen);
 }
 
 } // namespace blink
