49d0b6236ca34a9876b8cfef45b730ea63245ccd
eae <eae@chromium.org>
2016-07-01 15:00:15 -0700

    Render Unicode control characters
    Change the shaping logic to no longer replace Unicode control characters
    with space or zero-width-space to better match the spec. Implement as an
    experimental web platform feature to ensure that it doesn't break sites.
    
    W3C: https://lists.w3.org/Archives/Public/www-style/2014Mar/0475.html
    Spec: https://drafts.csswg.org/css-text-3/#white-space-processing
    
    TEST=fast/text/zero-width-characters-complex-script.html
    BUG=530342
    R=kojii@chromium.org
    
    Review-Url: https://codereview.chromium.org/1964773002
    Cr-Commit-Position: refs/heads/master@{#403542}

--- third_party/WebKit/Source/platform/fonts/shaping/HarfBuzzShaper.cpp
+++ third_party/WebKit/Source/platform/fonts/shaping/HarfBuzzShaper.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/fonts/shaping/HarfBuzzShaper.h"
 
 #include "platform/Logging.h"
+#include "platform/RuntimeEnabledFeatures.h"
 #include "platform/fonts/Font.h"
 #include "platform/fonts/FontFallbackIterator.h"
 #include "platform/fonts/GlyphBuffer.h"
@@ -100,12 +101,16 @@ static void normalizeCharacters(const TextRun& run, unsigned length, UChar* dest
         UChar32 character;
         U16_NEXT(source, position, length, character);
         // Don't normalize tabs as they are not treated as spaces for word-end.
-        if (run.normalizeSpace() && Character::isNormalizedCanvasSpaceCharacter(character))
+        if (run.normalizeSpace() && Character::isNormalizedCanvasSpaceCharacter(character)) {
             character = spaceCharacter;
-        else if (Character::treatAsSpace(character) && character != noBreakSpaceCharacter)
+        } else if (Character::treatAsSpace(character) && character != noBreakSpaceCharacter) {
             character = spaceCharacter;
-        else if (Character::treatAsZeroWidthSpaceInComplexScript(character))
+        } else if (!RuntimeEnabledFeatures::renderUnicodeControlCharactersEnabled()
+            && Character::legacyTreatAsZeroWidthSpaceInComplexScript(character)) {
             character = zeroWidthSpaceCharacter;
+        } else if (Character::treatAsZeroWidthSpaceInComplexScript(character)) {
+            character = zeroWidthSpaceCharacter;
+        }
 
         U16_APPEND(destination, *destinationLength, length, character, error);
         ASSERT_UNUSED(error, !error);
