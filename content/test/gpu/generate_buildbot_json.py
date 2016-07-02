c8ee22a73c33f602a06be62553355e7b1955410a
kbr <kbr@chromium.org>
2016-07-01 21:37:48 -0700

    Revert of Switch WebGL 1.0 conformance tests to new GPU integration test harness. (patchset #1 id:1 of https://codereview.chromium.org/2110803004/ )
    Reason for revert:
    Broke Android bots on chromium.gpu.fyi per ynovikov@'s comment above; see http://crbug.com/622948 .
    
    Original issue's description:
    > Switch WebGL 1.0 conformance tests to new GPU integration test harness.
    >
    > This will allow these tests to be sharded and improve flaky test
    > handling.
    >
    > BUG=352807,622948
    > CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    >
    > Committed: https://crrev.com/6ab8b3bc2f12e0d4a2857f303bbbae1395c37fd2
    > Cr-Commit-Position: refs/heads/master@{#403240}
    
    TBR=zmo@chromium.org,jmadill@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=352807,622948
    
    Review-Url: https://codereview.chromium.org/2120043002
    Cr-Commit-Position: refs/heads/master@{#403604}

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -834,12 +834,6 @@ TELEMETRY_TESTS = {
       },
     ],
   },
-}
-
-# These tests use Telemetry's new, simpler, browser_test_runner.
-# Eventually all of the Telemetry based tests above will be ported to
-# this harness, and the old harness will be deleted.
-TELEMETRY_GPU_INTEGRATION_TESTS = {
   'webgl_conformance': {
     'tester_configs': [
       {
@@ -905,6 +899,12 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
       '--use-gl=angle',
     ],
   },
+}
+
+# These tests use Telemetry's new, simpler, browser_test_runner.
+# Eventually all of the Telemetry based tests above will be ported to
+# this harness, and the old harness will be deleted.
+TELEMETRY_GPU_INTEGRATION_TESTS = {
   'webgl2_conformance_tests': {
     'tester_configs': [
       {
