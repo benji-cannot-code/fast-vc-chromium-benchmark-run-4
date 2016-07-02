47e42ce9b57908609006118f16ac3343ac6bc91e
qyearsley <qyearsley@chromium.org>
2016-07-02 11:38:43 -0700

    Greatly simplify buildbot module and remove usage of master name.
    It looks like most of the functionality in the buildbot module was
    unused, dead code.
    
    This also means that master name is not required for anything, since
    actual fetching of layout test results don't use master name (because
    the Google Storage directory layout doesn't include master name).
    
    Use of master name was added in http://crrev.com/2009603004 because
    at that time, getting a Build object required making a request to
    build.chromium.org which required a master name. But now this is not
    the case.
    
    Review-Url: https://codereview.chromium.org/2112133002
    Cr-Commit-Position: refs/heads/master@{#403614}

--- third_party/WebKit/Tools/Scripts/webkitpy/common/net/rietveld_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/net/rietveld_unittest.py
@@ -27,13 +27,11 @@ class RietveldTest(unittest.TestCase):
                 'try_job_results': [
                     {
                         'builder': 'foo-builder',
-                        'master': 'master.tryserver.foo',
                         'buildnumber': 10,
                         'result': -1
                     },
                     {
                         'builder': 'bar-builder',
-                        'master': 'master.tryserver.bar',
                         'buildnumber': 50,
                         'results': 0
                     },
@@ -43,13 +41,11 @@ class RietveldTest(unittest.TestCase):
                 'try_job_results': [
                     {
                         'builder': 'foo-builder',
-                        'master': 'master.tryserver.foo',
                         'buildnumber': 20,
                         'result': 1
                     },
                     {
                         'builder': 'bar-builder',
-                        'master': 'master.tryserver.bar',
                         'buildnumber': 60,
                         'result': 0
                     },
@@ -61,7 +57,7 @@ class RietveldTest(unittest.TestCase):
     def test_latest_try_jobs(self):
         self.assertEqual(
             latest_try_jobs(11112222, ('bar-builder', 'other-builder'), self.web),
-            [TryJob('bar-builder', 'tryserver.bar', 60)])
+            [TryJob('bar-builder', 60)])
 
     def test_latest_try_jobs_http_error(self):
         def raise_error(_):
@@ -87,7 +83,7 @@ class RietveldTest(unittest.TestCase):
     def test_latest_try_jobs_with_patchset(self):
         self.assertEqual(
             latest_try_jobs(11112222, ('bar-builder', 'other-builder'), self.web, patchset_number=2),
-            [TryJob('bar-builder', 'tryserver.bar', 50)])
+            [TryJob('bar-builder', 50)])
 
     def test_latest_try_jobs_no_relevant_builders(self):
         self.assertEqual(latest_try_jobs(11112222, ('foo', 'bar'), self.web), [])
