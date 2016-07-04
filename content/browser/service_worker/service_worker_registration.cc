314f52dd59df649fcd5dc4f2c4ed878a44085472
falken <falken@chromium.org>
2016-07-04 01:48:36 -0700

    service worker: Refactor OnNoControllees
    OnNoControllees doesn't need to unset the flags.
    * If |is_uninstalling_| is set, the call to Clear() would unset it.
    * If |should_activate_when_ready_| is set, the call to ActivateWaitingVersion() would
    unset it. We also change Clear() to unset |should_activate_when_ready_| which
    makes sense for any callsite of Clear().
    
    Minor simplification in anticipation of changes for https://crbug.com/616331
    Refactoring only; no behavior change.
    
    BUG=616331
    
    Review-Url: https://codereview.chromium.org/2115393002
    Cr-Commit-Position: refs/heads/master@{#403650}

--- content/browser/service_worker/service_worker_registration.cc
+++ content/browser/service_worker/service_worker_registration.cc
@@ -256,8 +256,6 @@ void ServiceWorkerRegistration::OnNoControllees(ServiceWorkerVersion* version) {
     Clear();
   else if (should_activate_when_ready_)
     ActivateWaitingVersion(true);
-  is_uninstalling_ = false;
-  should_activate_when_ready_ = false;
 }
 
 void ServiceWorkerRegistration::ActivateWaitingVersion(bool delay) {
@@ -433,6 +431,7 @@ void ServiceWorkerRegistration::OnDeleteFinished(
 void ServiceWorkerRegistration::Clear() {
   is_uninstalling_ = false;
   is_uninstalled_ = true;
+  should_activate_when_ready_ = false;
   if (context_)
     context_->storage()->NotifyDoneUninstallingRegistration(this);
 
