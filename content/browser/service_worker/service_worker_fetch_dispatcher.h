ec9005859c34d66b11f3baa42fd323047e6baeb8
falken <falken@chromium.org>
2016-07-03 20:51:52 -0700

    service worker: Clean up the fetch event request if URLRequest gets cancelled.
    Before this patch, an inflight fetch event would never be
    finished, since the request being cancelled results in the
    URLRequestJob being destroyed, so the the response callback
    is never called. Eventually the event would timeout, but this
    is an error state that terminates the worker. It's better to
    listen for the response and finish the request cleanly.
    
    BUG=616331
    
    Review-Url: https://codereview.chromium.org/2114893002
    Cr-Commit-Position: refs/heads/master@{#403632}

--- content/browser/service_worker/service_worker_fetch_dispatcher.h
+++ content/browser/service_worker/service_worker_fetch_dispatcher.h
@@ -45,6 +45,8 @@ class CONTENT_EXPORT ServiceWorkerFetchDispatcher {
   void Run();
 
  private:
+  class ResponseCallback;
+
   void DidWaitForActivation();
   void StartWorker();
   void DidStartWorker();
