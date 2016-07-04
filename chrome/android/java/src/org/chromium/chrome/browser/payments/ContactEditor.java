f37d372b804e5fd613981fcdee5e8b878023f40f
sebsg <sebsg@chromium.org>
2016-07-04 01:54:29 -0700

    [Payments] Dedupe contact info, and phone and email editor suggestions.
    BUG=624869
    
    Review-Url: https://codereview.chromium.org/2122493003
    Cr-Commit-Position: refs/heads/master@{#403651}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ContactEditor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ContactEditor.java
@@ -16,8 +16,8 @@ import org.chromium.chrome.browser.payments.ui.EditorFieldModel;
 import org.chromium.chrome.browser.payments.ui.EditorFieldModel.EditorFieldValidator;
 import org.chromium.chrome.browser.payments.ui.EditorModel;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.annotation.Nullable;
 
@@ -27,8 +27,8 @@ import javax.annotation.Nullable;
 public class ContactEditor extends EditorBase<AutofillContact> {
     private final boolean mRequestPayerPhone;
     private final boolean mRequestPayerEmail;
-    private final List<CharSequence> mPhoneNumbers;
-    private final List<CharSequence> mEmailAddresses;
+    private final Set<CharSequence> mPhoneNumbers;
+    private final Set<CharSequence> mEmailAddresses;
     @Nullable private EditorFieldValidator mPhoneValidator;
     @Nullable private EditorFieldValidator mEmailValidator;
 
@@ -42,8 +42,8 @@ public class ContactEditor extends EditorBase<AutofillContact> {
         assert requestPayerPhone || requestPayerEmail;
         mRequestPayerPhone = requestPayerPhone;
         mRequestPayerEmail = requestPayerEmail;
-        mPhoneNumbers = new ArrayList<>();
-        mEmailAddresses = new ArrayList<>();
+        mPhoneNumbers = new HashSet<>();
+        mEmailAddresses = new HashSet<>();
     }
 
     /**
@@ -60,7 +60,7 @@ public class ContactEditor extends EditorBase<AutofillContact> {
     }
 
     /**
-     * Adds the given phone number to the autocomplete list, if it's valid.
+     * Adds the given phone number to the autocomplete set, if it's valid.
      *
      * @param phoneNumber The phone number to possibly add.
      */
@@ -69,7 +69,7 @@ public class ContactEditor extends EditorBase<AutofillContact> {
     }
 
     /**
-     * Adds the given email address to the autocomplete list, if it's valid.
+     * Adds the given email address to the autocomplete set, if it's valid.
      *
      * @param emailAddress The email address to possibly add.
      */
