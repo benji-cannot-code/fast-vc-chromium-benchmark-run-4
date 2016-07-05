86cafce52a479aa5e7cc944a4d3f8252dbcd03c3
calamity <calamity@chromium.org>
2016-07-05 01:42:18 -0700

    Add proportional decay support to the site engagement service.
    This CL allows the site engagement scores to decay to a fraction of
    their current scores. It also adds a threshold for scores to be cleaned
    up at since purely proportional decay will never reach zero.
    
    This CL also changes the way the experiment parameters are initialized
    in order to avoid mismatch problems arising.
    
    BUG=464234
    
    Review-Url: https://codereview.chromium.org/2110183004
    Cr-Commit-Position: refs/heads/master@{#403754}

--- chrome/browser/engagement/site_engagement_score.h
+++ chrome/browser/engagement/site_engagement_score.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ENGAGEMENT_SITE_ENGAGEMENT_SCORE_H_
 #define CHROME_BROWSER_ENGAGEMENT_SITE_ENGAGEMENT_SCORE_H_
 
+#include <array>
 #include <memory>
 
 #include "base/gtest_prod_util.h"
@@ -32,6 +33,14 @@ class SiteEngagementScore {
 
     // The number of points to decay per period.
     DECAY_POINTS,
+
+    // The proportion [0-1] which the current engagement value is multiplied by
+    // at each decay period, before subtracting DECAY_POINTS.
+    DECAY_PROPORTION,
+
+    // A score will be erased from the engagement system if it's less than this
+    // value.
+    SCORE_CLEANUP_THRESHOLD,
 
     // The number of points given for navigations.
     NAVIGATION_POINTS,
@@ -84,6 +93,8 @@ class SiteEngagementScore {
   static double GetMaxPointsPerDay();
   static double GetDecayPeriodInHours();
   static double GetDecayPoints();
+  static double GetDecayProportion();
+  static double GetScoreCleanupThreshold();
   static double GetNavigationPoints();
   static double GetUserInputPoints();
   static double GetVisibleMediaPoints();
@@ -152,8 +163,11 @@ class SiteEngagementScore {
   friend class SiteEngagementScoreTest;
   friend class SiteEngagementServiceTest;
 
+  using ParamValues = std::array<std::pair<std::string, double>, MAX_VARIATION>;
+
   // Array holding the values corresponding to each item in Variation array.
-  static double param_values[];
+  static ParamValues& GetParamValues();
+  static ParamValues BuildParamValues();
 
   // Keys used in the content settings dictionary.
   static const char* kRawScoreKey;
