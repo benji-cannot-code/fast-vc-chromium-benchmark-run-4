8e8eed2d91775064b8fe9bc42d63f1441657d34d
ksakamoto <ksakamoto@chromium.org>
2016-07-04 19:29:47 -0700

    Revert of Use metadata when launching WebAPKs. (patchset #11 id:480001 of https://codereview.chromium.org/2071213005/ )
    Reason for revert:
    Broke chrome_public_test_apk in Android Tests (dbg).
    https://build.chromium.org/p/chromium.linux/builders/Android%20Tests%20%28dbg%29/builds/35147
    
    Original issue's description:
    > Use metadata when launching WebAPKs.
    >
    > WebAPKs use WebappDataStorage to store the metadata too.
    > Some changes in the web app manifest, like themeColor,
    > orientation etc. don't request re-minting a WebAPK,
    > but will update the metadata in the SharedPreference
    > (WebappDataStorage). The metadata will be retrieved
    > and used for launching a WebAPK.
    >
    > Registering a WebAPK will be in a following up CL.
    >
    > BUG=624834
    >
    > Committed: https://crrev.com/a6fe748fda4067575facddcea0e99ac41139670a
    > Cr-Commit-Position: refs/heads/master@{#403677}
    
    TBR=pkotwicz@chromium.org,yfriedman@chromium.org,dominickn@chromium.org,dfalcantara@chromium.org,hanxi@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=624834
    
    Review-Url: https://codereview.chromium.org/2122813002
    Cr-Commit-Position: refs/heads/master@{#403733}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappInfo.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappInfo.java
@@ -221,10 +221,6 @@ public class WebappInfo {
         return mOrientation;
     }
 
-    public void updateOrientation(int orientation) {
-        mOrientation = orientation;
-    }
-
     public int source() {
         return mSource;
     }
@@ -236,10 +232,6 @@ public class WebappInfo {
      */
     public long themeColor() {
         return mThemeColor;
-    }
-
-    public void updateThemeColor(long themeColor) {
-        mThemeColor = themeColor;
     }
 
     /**
