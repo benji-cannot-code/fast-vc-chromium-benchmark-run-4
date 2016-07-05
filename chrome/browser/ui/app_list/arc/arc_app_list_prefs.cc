e3ce07cf6e74cf9bc1205caa6024068760868930
hirono <hirono@chromium.org>
2016-07-05 02:54:54 -0700

    Filter out Android system packages from Chrome OS notification settings.
    In Android Settings application, there are no UI to disable
    notifications for system packages.
    The CL filter out system packages from Chrome OS notification settings
    UI as well.
    
    BUG=623436
    TEST=manually tested on minnie
    
    Review-Url: https://codereview.chromium.org/2102433002
    Cr-Commit-Position: refs/heads/master@{#403761}

--- chrome/browser/ui/app_list/arc/arc_app_list_prefs.cc
+++ chrome/browser/ui/app_list/arc/arc_app_list_prefs.cc
@@ -38,6 +38,7 @@ const char kLastBackupAndroidId[] = "last_backup_android_id";
 const char kLastBackupTime[] = "last_backup_time";
 const char kLastLaunchTime[] = "lastlaunchtime";
 const char kShouldSync[] = "should_sync";
+const char kSystem[] = "system";
 constexpr int kSetNotificationsEnabledMinVersion = 6;
 
 // Provider of write access to a dictionary storing ARC prefs.
@@ -175,6 +176,7 @@ void AddOrUpdatePackagePrefs(PrefService* prefs,
   package_dict->SetInteger(kPackageVersion, package.package_version);
   package_dict->SetString(kLastBackupAndroidId, id_str);
   package_dict->SetString(kLastBackupTime, time_str);
+  package_dict->SetBoolean(kSystem, package.system);
 }
 
 // Remove given package from local pref.
@@ -397,15 +399,17 @@ std::unique_ptr<ArcAppListPrefs::PackageInfo> ArcAppListPrefs::GetPackage(
   int64_t last_backup_android_id = 0;
   int64_t last_backup_time = 0;
   bool should_sync = false;
+  bool system = false;
 
   GetInt64FromPref(package, kLastBackupAndroidId, &last_backup_android_id);
   GetInt64FromPref(package, kLastBackupTime, &last_backup_time);
   package->GetInteger(kPackageVersion, &package_version);
   package->GetBoolean(kShouldSync, &should_sync);
+  package->GetBoolean(kSystem, &system);
 
   return base::MakeUnique<PackageInfo>(package_name, package_version,
                                        last_backup_android_id, last_backup_time,
-                                       should_sync);
+                                       should_sync, system);
 }
 
 std::vector<std::string> ArcAppListPrefs::GetAppIds() const {
@@ -934,9 +938,11 @@ ArcAppListPrefs::PackageInfo::PackageInfo(const std::string& package_name,
                                           int32_t package_version,
                                           int64_t last_backup_android_id,
                                           int64_t last_backup_time,
-                                          bool should_sync)
+                                          bool should_sync,
+                                          bool system)
     : package_name(package_name),
       package_version(package_version),
       last_backup_android_id(last_backup_android_id),
       last_backup_time(last_backup_time),
-      should_sync(should_sync) {}
+      should_sync(should_sync),
+      system(system) {}
