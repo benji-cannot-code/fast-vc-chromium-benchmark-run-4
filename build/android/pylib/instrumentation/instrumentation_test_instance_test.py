6bf82c8efe0795eb0325b2db1dc509702d92b3b7
jbudorick <jbudorick@chromium.org>
2016-07-06 12:41:34 -0700

    [Android] Fix support for multiple instances of value annotation filters.
    BUG=626028
    
    Review-Url: https://codereview.chromium.org/2124043002
    Cr-Commit-Position: refs/heads/master@{#403942}

--- build/android/pylib/instrumentation/instrumentation_test_instance_test.py
+++ build/android/pylib/instrumentation/instrumentation_test_instance_test.py
@@ -288,7 +288,7 @@ class InstrumentationTestInstanceTest(unittest.TestCase):
       },
     ]
 
-    o._annotations = {'SmallTest': None}
+    o._annotations = [('SmallTest', None)]
     with mock.patch(_INSTRUMENTATION_TEST_INSTANCE_PATH % '_GetTestsFromPickle',
                     return_value=raw_tests):
       actual_tests = o.GetTests()
@@ -335,7 +335,7 @@ class InstrumentationTestInstanceTest(unittest.TestCase):
       },
     ]
 
-    o._excluded_annotations = {'SmallTest': None}
+    o._excluded_annotations = [('SmallTest', None)]
     with mock.patch(_INSTRUMENTATION_TEST_INSTANCE_PATH % '_GetTestsFromPickle',
                     return_value=raw_tests):
       actual_tests = o.GetTests()
@@ -392,7 +392,7 @@ class InstrumentationTestInstanceTest(unittest.TestCase):
       },
     ]
 
-    o._annotations = {'TestValue': '1'}
+    o._annotations = [('TestValue', '1')]
     with mock.patch(_INSTRUMENTATION_TEST_INSTANCE_PATH % '_GetTestsFromPickle',
                     return_value=raw_tests):
       actual_tests = o.GetTests()
@@ -439,7 +439,65 @@ class InstrumentationTestInstanceTest(unittest.TestCase):
       },
     ]
 
-    o._annotations = {'Feature': 'Bar'}
+    o._annotations = [('Feature', 'Bar')]
+    with mock.patch(_INSTRUMENTATION_TEST_INSTANCE_PATH % '_GetTestsFromPickle',
+                    return_value=raw_tests):
+      actual_tests = o.GetTests()
+
+    self.assertEquals(actual_tests, expected_tests)
+
+  def testGetTests_multipleAnnotationValuesRequested(self):
+    o = self.createTestInstance()
+    raw_tests = [
+      {
+        'annotations': {'Feature': {'value': ['Foo']}},
+        'class': 'org.chromium.test.SampleTest',
+        'methods': [
+          {
+            'annotations': {'SmallTest': None},
+            'method': 'testMethod1',
+          },
+          {
+            'annotations': {
+              'Feature': {'value': ['Baz']},
+              'MediumTest': None,
+            },
+            'method': 'testMethod2',
+          },
+        ],
+      },
+      {
+        'annotations': {'Feature': {'value': ['Bar']}},
+        'class': 'org.chromium.test.SampleTest2',
+        'methods': [
+          {
+            'annotations': {'SmallTest': None},
+            'method': 'testMethod1',
+          },
+        ],
+      }
+    ]
+
+    expected_tests = [
+      {
+        'annotations': {
+          'Feature': {'value': ['Baz']},
+          'MediumTest': None,
+        },
+        'class': 'org.chromium.test.SampleTest',
+        'method': 'testMethod2',
+      },
+      {
+        'annotations': {
+          'Feature': {'value': ['Bar']},
+          'SmallTest': None,
+        },
+        'class': 'org.chromium.test.SampleTest2',
+        'method': 'testMethod1',
+      },
+    ]
+
+    o._annotations = [('Feature', 'Bar'), ('Feature', 'Baz')]
     with mock.patch(_INSTRUMENTATION_TEST_INSTANCE_PATH % '_GetTestsFromPickle',
                     return_value=raw_tests):
       actual_tests = o.GetTests()
