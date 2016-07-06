3b3004b0410cacb9921bae082a4c42c6e8498786
kulshin <kulshin@chromium.org>
2016-07-06 14:56:27 -0700

    Add a check and tests for fallback text being shorter than claimed.
    DirectWrite crashes if it is asked to find fallback fonts for more text
    than is contained in the TextAnalysisSource than was passed in. This
    suppresses the crash by adding a check to cap the text length to what
    actually passed in.
    
    BUG=624905
    
    Review-Url: https://codereview.chromium.org/2114823002
    Cr-Commit-Position: refs/heads/master@{#403959}

--- ui/gfx/font_fallback_win.cc
+++ ui/gfx/font_fallback_win.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <usp10.h>
 #include <wrl.h>
 
+#include <algorithm>
 #include <map>
 
 #include "base/i18n/rtl.h"
@@ -350,6 +351,12 @@ bool GetFallbackFont(const Font& font,
   // renderer this can cause hangs. Code that needs font fallback in the
   // renderer should instead use the font proxy.
   DCHECK(base::MessageLoopForUI::IsCurrent());
+
+  // Check that we have at least as much text as was claimed. If we have less
+  // text than expected then DirectWrite will become confused and crash. This
+  // shouldn't happen, but crbug.com/624905 shows that it happens sometimes.
+  DCHECK_GE(wcslen(text), static_cast<size_t>(text_length));
+  text_length = std::min(wcslen(text), static_cast<size_t>(text_length));
 
   if (g_factory == nullptr) {
     gfx::win::CreateDWriteFactory(&g_factory);
