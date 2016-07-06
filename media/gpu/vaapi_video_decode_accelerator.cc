38c737c36473c682db4899be04f4de1deae1b463
posciak <posciak@chromium.org>
2016-07-06 16:58:01 -0700

    Add support for multi-plane YUV format import into ArcGVDA and VAVDA.
    Add support for passing multiple strides and offsets along with a dmabuf
    to the ArcVideoAccelerator interface via AVA::BindDmabuf().
    
    Add support for importing YVU420 format to VAVDA and multi-plane pixmap
    import support to VaapiWrapper.
    
    TEST=video playback, crosvideo.appspot, vdatest
    BUG=b/29335168
    
    Review-Url: https://codereview.chromium.org/2115453003
    Cr-Commit-Position: refs/heads/master@{#403991}

--- media/gpu/vaapi_video_decode_accelerator.cc
+++ media/gpu/vaapi_video_decode_accelerator.cc
@@ -42,7 +42,8 @@ enum VAVDADecoderFailure {
 
 // Buffer format to use for output buffers backing PictureBuffers. This is the
 // format decoded frames in VASurfaces are converted into.
-const gfx::BufferFormat kOutputPictureFormat = gfx::BufferFormat::BGRA_8888;
+const gfx::BufferFormat kAllocatePictureFormat = gfx::BufferFormat::BGRA_8888;
+const gfx::BufferFormat kImportPictureFormat = gfx::BufferFormat::YVU_420;
 }
 
 static void ReportToUMA(VAVDADecoderFailure failure) {
@@ -311,6 +312,7 @@ VaapiVideoDecodeAccelerator::VaapiVideoDecodeAccelerator(
       finish_flush_pending_(false),
       awaiting_va_surfaces_recycle_(false),
       requested_num_pics_(0),
+      output_format_(kAllocatePictureFormat),
       make_context_current_cb_(make_context_current_cb),
       bind_image_cb_(bind_image_cb),
       weak_this_factory_(this) {
@@ -332,9 +334,18 @@ bool VaapiVideoDecodeAccelerator::Initialize(const Config& config,
     return false;
   }
 
-  if (config.output_mode != Config::OutputMode::ALLOCATE &&
-      config.output_mode != Config::OutputMode::IMPORT) {
-    NOTREACHED() << "Only ALLOCATE and IMPORT OutputModes are supported";
+  switch (config.output_mode) {
+    case Config::OutputMode::ALLOCATE:
+      output_format_ = kAllocatePictureFormat;
+      break;
+
+    case Config::OutputMode::IMPORT:
+      output_format_ = kImportPictureFormat;
+      break;
+
+    default:
+      NOTREACHED() << "Only ALLOCATE and IMPORT OutputModes are supported";
+      return false;
   }
 
   client_ptr_factory_.reset(new base::WeakPtrFactory<Client>(client));
@@ -643,6 +654,9 @@ static VideoPixelFormat BufferFormatToVideoPixelFormat(
     case gfx::BufferFormat::BGRA_8888:
       return PIXEL_FORMAT_ARGB;
 
+    case gfx::BufferFormat::YVU_420:
+      return PIXEL_FORMAT_YV12;
+
     default:
       LOG(FATAL) << "Add more cases as needed";
       return PIXEL_FORMAT_UNKNOWN;
@@ -688,8 +702,7 @@ void VaapiVideoDecodeAccelerator::TryFinishSurfaceSetChange() {
   DVLOG(1) << "Requesting " << requested_num_pics_
            << " pictures of size: " << requested_pic_size_.ToString();
 
-  VideoPixelFormat format =
-      BufferFormatToVideoPixelFormat(kOutputPictureFormat);
+  VideoPixelFormat format = BufferFormatToVideoPixelFormat(output_format_);
   task_runner_->PostTask(
       FROM_HERE, base::Bind(&Client::ProvidePictureBuffers, client_,
                             requested_num_pics_, format, 1, requested_pic_size_,
@@ -790,7 +803,7 @@ void VaapiVideoDecodeAccelerator::AssignPictureBuffers(
 
     if (output_mode_ == Config::OutputMode::ALLOCATE) {
       RETURN_AND_NOTIFY_ON_FAILURE(
-          picture->Allocate(kOutputPictureFormat),
+          picture->Allocate(output_format_),
           "Failed to allocate memory for a VaapiPicture", PLATFORM_FAILURE, );
       output_buffers_.push(buffers[i].id());
     }
@@ -828,13 +841,6 @@ void VaapiVideoDecodeAccelerator::ImportBufferForPicture(
     return;
   }
 
-  if (gpu_memory_buffer_handle.native_pixmap_handle.fds.size() != 1) {
-    CloseGpuMemoryBufferHandle(gpu_memory_buffer_handle);
-    LOG(ERROR) << "Handles backed by multple fds unsupported";
-    NotifyError(INVALID_ARGUMENT);
-    return;
-  }
-
   VaapiPicture* picture = PictureById(picture_buffer_id);
   if (!picture) {
     CloseGpuMemoryBufferHandle(gpu_memory_buffer_handle);
@@ -848,7 +854,7 @@ void VaapiVideoDecodeAccelerator::ImportBufferForPicture(
     return;
   }
 
-  if (!picture->ImportGpuMemoryBufferHandle(kOutputPictureFormat,
+  if (!picture->ImportGpuMemoryBufferHandle(output_format_,
                                             gpu_memory_buffer_handle)) {
     // ImportGpuMemoryBufferHandle will close the handles even on failure, so
     // we don't need to do this ourselves.
