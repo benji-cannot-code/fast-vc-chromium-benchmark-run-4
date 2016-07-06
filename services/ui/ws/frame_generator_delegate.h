bbb71d51d14a77db59ad8d8764b026df0d5dce3e
mfomitchev <mfomitchev@chromium.org>
2016-07-06 16:18:42 -0700

    Factor draw functionality out of DefaultPlatformDisplay into FrameGenerator.
    Introducing a new class FrameGenerator, which is responsible for redrawing the
    display by submitting CompositorFrames to the owned DisplayCompositor.
    FrameGenerator is owned by PlatformDisplay.
    
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2112993002
    Cr-Commit-Position: refs/heads/master@{#403969}

---
+++ services/ui/ws/frame_generator_delegate.h
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_UI_WS_FRAME_GENERATOR_DELEGATE_H_
+#define SERVICES_UI_WS_FRAME_GENERATOR_DELEGATE_H_
+
+#include "base/macros.h"
+
+namespace ui {
+namespace ws {
+
+class ServerWindow;
+
+struct ViewportMetrics {
+  gfx::Size size_in_pixels;
+  float device_scale_factor = 0.f;
+};
+
+class FrameGeneratorDelegate {
+ public:
+  // Returns the root window of the display.
+  virtual ServerWindow* GetRootWindow() = 0;
+
+  // Called when a compositor frame is finished drawing.
+  virtual void OnCompositorFrameDrawn() = 0;
+
+  virtual const ViewportMetrics& GetViewportMetrics() = 0;
+
+ protected:
+  virtual ~FrameGeneratorDelegate() {}
+};
+
+}  // namespace ws
+}  // namespace ui
+
+#endif  // SERVICES_UI_WS_FRAME_GENERATOR_DELEGATE_H_
