54ff748dad81d508929c3ab32689038ecbfa80fa
nyquist <nyquist@chromium.org>
2016-07-06 16:06:58 -0700

    Fix blimp native Java tests on platforms with assert.
    The BlimpNativeInstrumentationTestCase wrapper for blimp tests that need
    to load native code to run was not using the UI thread for loading the
    library. There was an assert checking for this, but this doesn't execute
    on newer platforms, so was not caught during development or in the CQ.
    
    This CL fixes the issue by ensuring that the library is loaded on the
    correct thread.
    
    BUG=622236
    
    Review-Url: https://codereview.chromium.org/2124143002
    Cr-Commit-Position: refs/heads/master@{#403965}

--- blimp/client/app/android/javatests/src/org/chromium/blimp/BlimpNativeInstrumentationTestCase.java
+++ blimp/client/app/android/javatests/src/org/chromium/blimp/BlimpNativeInstrumentationTestCase.java
@@ -7,6 +7,7 @@ package org.chromium.blimp;
 
 import android.test.InstrumentationTestCase;
 
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.library_loader.ProcessInitException;
 
 import java.util.concurrent.Semaphore;
@@ -21,13 +22,22 @@ public class BlimpNativeInstrumentationTestCase extends InstrumentationTestCase 
 
     @Override
     public void setUp() throws ProcessInitException {
-        BlimpLibraryLoader.startAsync(
-                getInstrumentation().getTargetContext(), new BlimpLibraryLoader.Callback() {
-                    public void onStartupComplete(boolean success) {
-                        mSuccess = success;
-                        mNativeReadySemaphore.release();
-                    }
-                });
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    BlimpLibraryLoader.startAsync(getInstrumentation().getTargetContext(),
+                            new BlimpLibraryLoader.Callback() {
+                                public void onStartupComplete(boolean success) {
+                                    mSuccess = success;
+                                    mNativeReadySemaphore.release();
+                                }
+                            });
+                } catch (ProcessInitException e) {
+                    throw new RuntimeException("Failed to initialize process.");
+                }
+            }
+        });
     }
 
     /**
