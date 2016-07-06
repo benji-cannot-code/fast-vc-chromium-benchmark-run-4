714a7fd544e0c981e372ca7270573b0d043e9677
hiroshige <hiroshige@chromium.org>
2016-07-06 02:42:32 -0700

    Remove tuple-related includes and comments in Functional.h
    After https://codereview.chromium.org/2092093002, tuples are no longer used
    directly in Functional.h.
    
    BUG=597856
    
    Review-Url: https://codereview.chromium.org/2123883002
    Cr-Commit-Position: refs/heads/master@{#403875}

--- third_party/WebKit/Source/wtf/Functional.h
+++ third_party/WebKit/Source/wtf/Functional.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/threading/thread_checker.h"
-#include "base/tuple.h"
 #include "wtf/Allocator.h"
 #include "wtf/Assertions.h"
 #include "wtf/PassRefPtr.h"
@@ -38,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/ThreadSafeRefCounted.h"
 #include "wtf/TypeTraits.h"
 #include "wtf/WeakPtr.h"
-#include <tuple>
 #include <utility>
 
 namespace blink {
@@ -249,10 +247,6 @@ private:
 template <FunctionThreadAffinity threadAffinity, typename FunctionType, typename... BoundParameters>
 std::unique_ptr<Function<base::MakeUnboundRunType<FunctionType, BoundParameters...>, threadAffinity>> bindInternal(FunctionType function, BoundParameters&&... boundParameters)
 {
-    // Bound parameters' types are wrapped with std::tuple so we can pass two template parameter packs (bound
-    // parameters and unbound) to PartBoundFunctionImpl. Note that a tuple of this type isn't actually created;
-    // std::tuple<> is just for carrying the bound parameters' types. Any other class template taking a type parameter
-    // pack can be used instead of std::tuple. std::tuple is used just because it's most convenient for this purpose.
     using UnboundRunType = base::MakeUnboundRunType<FunctionType, BoundParameters...>;
     return wrapUnique(new Function<UnboundRunType, threadAffinity>(base::Bind(function, typename ParamStorageTraits<typename std::decay<BoundParameters>::type>::StorageType(std::forward<BoundParameters>(boundParameters))...)));
 }
