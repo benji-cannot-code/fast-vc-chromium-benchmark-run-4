ddcbaa852bb0920716b87e152537f162d230bd30
kapishnikov <kapishnikov@chromium.org>
2016-07-06 10:05:48 -0700

    Fixed CronetHttpURLConnectionTest failure on Android N
    + use Build.VERSION_CODES instead of integers.
    
    BUG=625188
    
    Review-Url: https://codereview.chromium.org/2123993002
    Cr-Commit-Position: refs/heads/master@{#403921}

--- components/cronet/android/java/src/org/chromium/net/CronetUrlRequestContext.java
+++ components/cronet/android/java/src/org/chromium/net/CronetUrlRequestContext.java
@@ -164,7 +164,7 @@ class CronetUrlRequestContext extends CronetEngine {
 
     @Override
     public boolean isEnabled() {
-        return Build.VERSION.SDK_INT >= 14;
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
     }
 
     @Override
