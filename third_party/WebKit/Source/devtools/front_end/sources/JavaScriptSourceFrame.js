942ff8fd90996543486e648e7fcc07cf821eca6b
lushnikov <lushnikov@chromium.org>
2016-07-06 14:00:24 -0700

    DevTools: gray out linenumbers for the unmapped source code lines
    This patch:
    - grays out linenumbers for the unmapped source code lines
    - disabled setting breakpoint on these lines
    
    These are cosmetic changes - currently, breakpoints will
    never break if set on the line without mapping.
    
    BUG=479733
    R=paulirish
    
    Review-Url: https://codereview.chromium.org/2120603002
    Cr-Commit-Position: refs/heads/master@{#403950}

--- third_party/WebKit/Source/devtools/front_end/sources/JavaScriptSourceFrame.js
+++ third_party/WebKit/Source/devtools/front_end/sources/JavaScriptSourceFrame.js
@@ -1119,6 +1119,9 @@ WebInspector.JavaScriptSourceFrame.prototype = {
      */
     _setBreakpoint: function(lineNumber, columnNumber, condition, enabled)
     {
+        if (!WebInspector.debuggerWorkspaceBinding.uiLineHasMapping(this.uiSourceCode(), lineNumber))
+            return;
+
         this._breakpointManager.setBreakpoint(this.uiSourceCode(), lineNumber, columnNumber, condition, enabled);
     },
 
