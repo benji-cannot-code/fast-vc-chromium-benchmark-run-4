61a59ae876a146052ca6037914f8faaa61960c4e
henrika <henrika@chromium.org>
2016-07-06 00:31:31 -0700

    Reland of Prevents crash in media::AudioInputController::OnData in Mac at shutdown (patchset #1 id:1 of https://codereview.chromium.org/2125623002/ )
    Reason for revert:
    I am landing the original fix and will then add a minor change directly after. The new change (https://codereview.chromium.org/2126603002/) will ensure that all tests runs as expected.
    
    Original issue's description:
    > Revert of Prevents crash in media::AudioInputController::OnData in Mac at shutdown (patchset #3 id:40001 of https://codereview.chromium.org/2110133002/ )
    >
    > Reason for revert:
    > Causes some WebRTC content_browsertests to fail on Mac https://build.chromium.org/p/chromium.webrtc/builders/Mac%20Tester/builds/56577
    >
    > 17 tests failed:
    >     WebRtcAudioDebugRecordingsBrowserTest.CallWithAudioDebugRecordings (../../content/browser/media/webrtc/webrtc_audio_debug_recordings_browsertest.cc:98)
    >     WebRtcAudioDebugRecordingsBrowserTest.CallWithAudioDebugRecordingsEnabledThenDisabled (../../content/browser/media/webrtc/webrtc_audio_debug_recordings_browsertest.cc:164)
    >     WebRtcBrowserTest.CallAndVerifyVideoMutingWorks (../../content/browser/media/webrtc/webrtc_browsertest.cc:318)
    >     WebRtcBrowserTest.CallWithDataAndMedia (../../content/browser/media/webrtc/webrtc_browsertest.cc:225)
    >     WebRtcBrowserTest.CallWithSctpDataAndMedia (../../content/browser/media/webrtc/webrtc_browsertest.cc:237)
    >     WebRtcBrowserTest.CanSetupAudioAndVideoCall (../../content/browser/media/webrtc/webrtc_browsertest.cc:113)
    >     WebRtcBrowserTest.CanSetupAudioAndVideoCallWithoutMsidAndBundle (../../content/browser/media/webrtc/webrtc_browsertest.cc:178)
    >     WebRtcBrowserTest.CanSetupCallAndSendDtmf (../../content/browser/media/webrtc/webrtc_browsertest.cc:126)
    >     WebRtcBrowserTest.CanSetupLegacyCall (../../content/browser/media/webrtc/webrtc_browsertest.cc:201)
    >     WebRtcBrowserTest.EnsureLocalVideoMuteDoesntMuteAudio (../../content/browser/media/webrtc/webrtc_browsertest.cc:301)
    >     WebRtcBrowserTest.EnsureRemoteVideoMuteDoesntMuteAudio (../../content/browser/media/webrtc/webrtc_browsertest.cc:307)
    >     WebRtcBrowserTest.EstablishAudioVideoCallAndEnsureAudioIsPlaying (../../content/browser/media/webrtc/webrtc_browsertest.cc:271)
    >     WebRtcBrowserTest.EstablishAudioVideoCallAndVerifyLocalMutingWorks (../../content/browser/media/webrtc/webrtc_browsertest.cc:295)
    >     WebRtcBrowserTest.EstablishAudioVideoCallAndVerifyRemoteMutingWorks (../../content/browser/media/webrtc/webrtc_browsertest.cc:289)
    >     WebRtcBrowserTest.EstablishAudioVideoCallAndVerifyUnmutingWorks (../../content/browser/media/webrtc/webrtc_browsertest.cc:313)
    >     WebRtcBrowserTest.NegotiateOfferWithBLine (../../content/browser/media/webrtc/webrtc_browsertest.cc:197)
    >     WebRtcBrowserTest.NoCrashWhenConnectChromiumSinkToRemoteTrack (../../content/browser/media/webrtc/webrtc_browsertest.cc:169)
    >
    > Original issue's description:
    > > Prevents crash in media::AudioInputController::OnData in Mac at shutdown
    > >
    > > BUG=623703
    > >
    > > Committed: https://crrev.com/46d57073d145e2af834ae93aad15cc3a138a6cc4
    > > Cr-Commit-Position: refs/heads/master@{#403664}
    >
    > TBR=dalecurtis@chromium.org,alokp@chromium.org,primiano@chromium.org,tommi@chromium.org,phoglund@chromium.org,henrika@chromium.org
    > # Not skipping CQ checks because original CL landed more than 1 days ago.
    > BUG=623703
    >
    > Committed: https://crrev.com/a52511fc4d907663193d7ba18928e7edd0890ce2
    > Cr-Commit-Position: refs/heads/master@{#403783}
    
    TBR=dalecurtis@chromium.org,alokp@chromium.org,primiano@chromium.org,tommi@chromium.org,phoglund@chromium.org,magjed@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=623703
    
    Review-Url: https://codereview.chromium.org/2123103002
    Cr-Commit-Position: refs/heads/master@{#403863}

--- media/audio/audio_manager.cc
+++ media/audio/audio_manager.cc
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/fake_audio_log_factory.h"
 #include "media/base/media_switches.h"
 
+#if defined(OS_MACOSX)
+#include "media/audio/mac/audio_manager_mac.h"
+#endif
+
 #if defined(OS_WIN)
 #include "base/win/scoped_com_initializer.h"
 #include "media/audio/win/core_audio_util_win.h"
@@ -272,10 +276,23 @@ void AudioManagerDeleter::operator()(const AudioManager* instance) const {
     LOG(WARNING) << "Multiple instances of AudioManager detected";
   }
 
+#if defined(OS_MACOSX)
+  // If we are on Mac, tasks after this point are not executed, hence this is
+  // the only chance to delete the audio manager (which on Mac lives on the
+  // main browser thread instead of a dedicated audio thread). If we don't
+  // delete here, the CoreAudio thread can keep providing callbacks, which
+  // uses a state that is destroyed in ~BrowserMainLoop().
+  // See http://crbug.com/623703 for more details.
+  DCHECK(instance->GetTaskRunner()->BelongsToCurrentThread());
+  AudioManagerMac* mac_instance =
+      static_cast<AudioManagerMac*>(const_cast<AudioManager*>(instance));
+  delete mac_instance;
+#else
   // AudioManager must be destroyed on the audio thread.
   if (!instance->GetTaskRunner()->DeleteSoon(FROM_HERE, instance)) {
     LOG(WARNING) << "Failed to delete AudioManager instance.";
   }
+#endif
 }
 
 // Forward declaration of the platform specific AudioManager factory function.
