be7bd32c43a7390162d9114b93e441b1db2d34e1
xidachen <xidachen@chromium.org>
2016-07-06 19:42:27 -0700

    Check display list canvas fall back reason unknown
    The FallbackReasonUnknown bin is still non-zero. This CL changes DCHECK
    to CHECK, such that when Unknown happens, browser crashes. This will hit
    canary first, once we have crash report with stack trace, we can fix it
    right away.
    
    BUG=585578
    
    Review-Url: https://codereview.chromium.org/2125153002
    Cr-Commit-Position: refs/heads/master@{#404040}

--- third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.cpp
+++ third_party/WebKit/Source/platform/graphics/RecordingImageBufferSurface.cpp
@@ -73,7 +73,7 @@ bool RecordingImageBufferSurface::writePixels(const SkImageInfo& origInfo, const
 void RecordingImageBufferSurface::fallBackToRasterCanvas(FallbackReason reason)
 {
     ASSERT(m_fallbackFactory);
-    DCHECK(reason != FallbackReasonUnknown);
+    CHECK(reason != FallbackReasonUnknown);
 
     if (m_fallbackSurface) {
         ASSERT(!m_currentFrame);
