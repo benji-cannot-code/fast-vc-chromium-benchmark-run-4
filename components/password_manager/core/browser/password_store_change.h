c08a1dbb6590bd6aa948a78a57ad7f3ac25708ae
vabr <vabr@chromium.org>
2016-07-07 00:58:16 -0700

    Fix logging for PasswordStoreChange
    It failed to separate the two fields of PasswordStoreChange with a comma,
    leading to confusing output.
    
    The only association with the bug below is that by working on that bug the
    fixed log was frequently seen. :)
    
    BUG=413020
    
    Review-Url: https://codereview.chromium.org/2124923003
    Cr-Commit-Position: refs/heads/master@{#404098}

--- components/password_manager/core/browser/password_store_change.h
+++ components/password_manager/core/browser/password_store_change.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_STORE_CHANGE_H__
 #define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_STORE_CHANGE_H__
 
+#include <ostream>
 #include <vector>
 
 #include "components/autofill/core/common/password_form.h"
