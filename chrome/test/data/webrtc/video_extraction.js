7cb396410ca3c9188881ef068b2cf459d8f8fd08
phoglund <phoglund@chromium.org>
2016-07-07 06:29:29 -0700

    Make video quality test fail cleaner if video hasn't started yet.
    Video must be playing when the onplay handler gets called, and
    the test detects that. This patch makes the test fail faster in
    that situation though; it used to return still-capturing up to
    timeout when capturing had already failed. This change will
    make it immediately return failure.
    
    BUG=625943
    
    Review-Url: https://codereview.chromium.org/2123133003
    Cr-Commit-Position: refs/heads/master@{#404145}

--- chrome/test/data/webrtc/video_extraction.js
+++ chrome/test/data/webrtc/video_extraction.js
@@ -37,10 +37,9 @@ var gFrames = [];
 var gHasThrownAwayFirstTwoFrames = false;
 
 /**
- * We need this global variable to synchronize with the test how long to run the
- * call between the two peers.
+ * A string to be returned to the test about the current status of capture.
  */
-var gDoneFrameCapturing = false;
+var gCapturingStatus = 'capturing-not-started';
 
 /**
  * Starts the frame capturing.
@@ -56,6 +55,7 @@ function startFrameCapture(videoTag, frameRate, duration) {
   inputElement = document.getElementById("local-view");
   var width = inputElement.videoWidth;
   var height = inputElement.videoHeight;
+
   // The WebRTC code is free to start in VGA, so make sure that the output video
   // tag scales up to whatever the input size is (otherwise the video quality
   // comparison will go poorly.
@@ -63,13 +63,16 @@ function startFrameCapture(videoTag, frameRate, duration) {
   videoTag.height = height;
 
   if (width == 0 || height == 0) {
-    throw failTest('Trying to capture from ' + videoTag.id +
-                   ' but it is not playing any video.');
+    // Video must be playing at this point since this function is invoked from
+    // onplay on the <video> tag. See http://crbug.com/625943.
+    gCapturingStatus = 'failed-video-was-0x0-after-onplay'
+    return;
   }
 
   console.log('Received width is: ' + width + ', received height is: ' + height
               + ', capture interval is: ' + gFrameCaptureInterval +
               ', duration is: ' + gCaptureDuration);
+  gCapturingStatus = 'still-capturing';
 
   var remoteCanvas = document.createElement('canvas');
   remoteCanvas.width = width;
@@ -86,11 +89,7 @@ function startFrameCapture(videoTag, frameRate, duration) {
  * Queries if we're done with the frame capturing yet.
  */
 function doneFrameCapturing() {
-  if (gDoneFrameCapturing) {
-    returnToTest('done-capturing');
-  } else {
-    returnToTest('still-capturing');
-  }
+  returnToTest(gCapturingStatus);
 }
 
 /**
@@ -170,7 +169,7 @@ function shoot_(video, canvas, width, height) {
                gFrameCaptureInterval - diff);
   } else {
     // Done capturing!
-    gDoneFrameCapturing = true;
+    gCapturingStatus = 'done-capturing';
     prepareProgressBar_();
   }
 }
