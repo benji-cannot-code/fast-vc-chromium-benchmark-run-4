2d0fd8922b8d6a22503ec682de4344ae95c555fb
mdjones <mdjones@chromium.org>
2016-07-07 13:23:05 -0700

    Cancel indeterminate animation on progress hidden
    The progress bar does not necessarily hit 100% before being hidden
    any more. This change cancels the indeterminate progress bar animation
    if the progress bar is hidden.
    
    BUG=624917
    
    Committed: https://crrev.com/59f868324bb77028013b875a992fdf7de787ac7c
    Review-Url: https://codereview.chromium.org/2110913003
    Cr-Original-Commit-Position: refs/heads/master@{#403465}
    Cr-Commit-Position: refs/heads/master@{#404230}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/ProgressAnimationSmooth.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/ProgressAnimationSmooth.java
@@ -34,8 +34,6 @@ class ProgressAnimationSmooth implements ToolbarProgressBar.AnimationLogic {
 
     @Override
     public float updateProgress(float targetProgress, float frameTimeSec, int resolution) {
-        assert mProgress <= targetProgress;
-
         final float acceleratingDuration = computeAcceleratingDuration(
                 targetProgress, frameTimeSec);
         final float deceleratingDuration = frameTimeSec - acceleratingDuration;
