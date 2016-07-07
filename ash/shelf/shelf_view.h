f2fee039fb06ee1d6ef042bcf5af4d7f5cc1a5e4
jonross <jonross@chromium.org>
2016-07-07 06:15:39 -0700

    Turn ShelfView Menu Async
    Switch ShelfView's menus to use ASYNC. Adjusting their shutdown via MenuModelAdapter::OnMenuClosed callback.
    
    Deletion pattern updated to no longer delete itself while triggering a menu item.
    
    Move InkDropAnimation updates to OnMenuClosed as menu launching is no longer blocking.
    
    I've also updated ChromeLauncherAppMenuItemBrowser::Observe has there is a race during browser shutdown. It is possible that the same browser notifies of closing twice. This observer would then fails its DCHECKs. Since the observing was only to turn off its own behaviour, I've updated it to stop listening as well.
    
    TEST=ShelfViewTest, manual testing on device
    BUG=557130
    
    Review-Url: https://codereview.chromium.org/2090893004
    Cr-Commit-Position: refs/heads/master@{#404144}

--- ash/shelf/shelf_view.h
+++ ash/shelf/shelf_view.h
@@ -34,6 +34,7 @@ class MenuModel;
 
 namespace views {
 class BoundsAnimator;
+class MenuModelAdapter;
 class MenuRunner;
 }
 
@@ -42,6 +43,7 @@ class AppListButton;
 class DragImageView;
 class OverflowBubble;
 class OverflowButton;
+class ScopedTargetRootWindow;
 class Shelf;
 class ShelfButton;
 class ShelfDelegate;
@@ -312,11 +314,15 @@ class ASH_EXPORT ShelfView : public views::View,
   // If |context_menu| is set, the displayed menu is a context menu and not
   // a menu listing one or more running applications.
   // The |click_point| is only used for |context_menu|'s.
-  void ShowMenu(ui::MenuModel* menu_model,
+  void ShowMenu(std::unique_ptr<ui::MenuModel> menu_model,
                 views::View* source,
                 const gfx::Point& click_point,
                 bool context_menu,
-                ui::MenuSourceType source_type);
+                ui::MenuSourceType source_type,
+                views::InkDrop* ink_drop);
+
+  // Callback for MenuModelAdapter.
+  void OnMenuClosed(views::InkDrop* ink_drop);
 
   // Overridden from views::BoundsAnimatorObserver:
   void OnBoundsAnimatorProgressed(views::BoundsAnimator* animator) override;
@@ -386,7 +392,11 @@ class ASH_EXPORT ShelfView : public views::View,
 
   std::unique_ptr<views::FocusSearch> focus_search_;
 
+  // Manages the context menu, and the list menu.
+  std::unique_ptr<ui::MenuModel> menu_model_;
+  std::unique_ptr<views::MenuModelAdapter> menu_model_adapter_;
   std::unique_ptr<views::MenuRunner> launcher_menu_runner_;
+  std::unique_ptr<ScopedTargetRootWindow> scoped_target_root_window_;
 
   base::ObserverList<ShelfIconObserver> observers_;
 
@@ -405,10 +415,6 @@ class ASH_EXPORT ShelfView : public views::View,
 
   // The timestamp of the event which closed the last menu - or 0.
   base::TimeTicks closing_event_time_;
-
-  // When this object gets deleted while a menu is shown, this pointed
-  // element will be set to false.
-  bool* got_deleted_;
 
   // True if a drag and drop operation created/pinned the item in the launcher
   // and it needs to be deleted/unpinned again if the operation gets cancelled.
