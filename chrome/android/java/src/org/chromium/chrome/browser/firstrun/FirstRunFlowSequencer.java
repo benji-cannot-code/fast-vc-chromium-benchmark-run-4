b2d7e488414387dc5f9787f74ea92dc9940c99df
twellington <twellington@chromium.org>
2016-07-06 17:15:09 -0700

    Disable Android FRE in demo mode
    BUG=625032
    
    Review-Url: https://codereview.chromium.org/2113243004
    Cr-Commit-Position: refs/heads/master@{#404002}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencer.java
@@ -66,7 +66,8 @@ public abstract class FirstRunFlowSequencer  {
      * Once finished, calls onFlowIsKnown().
      */
     public void start() {
-        if (CommandLine.getInstance().hasSwitch(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)) {
+        if (CommandLine.getInstance().hasSwitch(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)
+                || ApiCompatibilityUtils.isDemoUser(mActivity)) {
             onFlowIsKnown(null);
             return;
         }
@@ -224,7 +225,8 @@ public abstract class FirstRunFlowSequencer  {
      */
     public static Intent checkIfFirstRunIsNecessary(Context context, Intent fromIntent) {
         // If FRE is disabled (e.g. in tests), proceed directly to the intent handling.
-        if (CommandLine.getInstance().hasSwitch(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)) {
+        if (CommandLine.getInstance().hasSwitch(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)
+                || ApiCompatibilityUtils.isDemoUser(context)) {
             return null;
         }
 
