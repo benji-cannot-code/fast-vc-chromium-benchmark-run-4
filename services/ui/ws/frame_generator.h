81e3e81c23a709141048c406bc1a39c7bb2dbecf
mfomitchev <mfomitchev@chromium.org>
2016-07-07 10:41:58 -0700

    Mus: Fixes rendering of transparent containers.
    Pre-fix, setting opacity on the parent window did not affect how the child
    windows were rendered.
    Effective opacity of window contents when rendered needs to be a product of the
    opacities of the window and all its ancestors.
    This CL fixes the issue and adds a unit test.
    
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2099893002
    Cr-Commit-Position: refs/heads/master@{#404171}

--- services/ui/ws/frame_generator.h
+++ services/ui/ws/frame_generator.h
@@ -28,6 +28,10 @@ class SurfacesState;
 
 namespace ws {
 
+namespace test {
+class FrameGeneratorTest;
+}
+
 class FrameGeneratorDelegate;
 class ServerWindow;
 
@@ -49,6 +53,8 @@ class FrameGenerator {
   bool is_frame_pending() { return frame_pending_; }
 
  private:
+  friend class ui::ws::test::FrameGeneratorTest;
+
   void WantToDraw();
 
   // This method initiates a top level redraw of the display.
