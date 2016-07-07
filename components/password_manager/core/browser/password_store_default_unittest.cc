4aefd1ec6aa5961e97c778c2484837066a519def
vabr <vabr@chromium.org>
2016-07-07 04:42:35 -0700

    Reformat some password_manager tests
    The password_store_mac_unittest.cc had a lot of clang-format off blocks for no good reason. Keeping
    them would make the upcoming https://codereview.chromium.org/2127533003/ rather
    noisy, so this CL pre-formats the file. It also removes clutter by using
    SCOPED_TRACE.
    
    This CL also ran clang-format on a number of other tests, for the same reason -- to make upcoming changes easier to review.
    
    BUG=413020
    
    Review-Url: https://codereview.chromium.org/2127063002
    Cr-Commit-Position: refs/heads/master@{#404130}

--- components/password_manager/core/browser/password_store_default_unittest.cc
+++ components/password_manager/core/browser/password_store_default_unittest.cc
@@ -62,20 +62,18 @@ class BadLoginDatabase : public LoginDatabase {
 };
 
 PasswordFormData CreateTestPasswordFormData() {
-  PasswordFormData data = {
-    PasswordForm::SCHEME_HTML,
-    "http://bar.example.com",
-    "http://bar.example.com/origin",
-    "http://bar.example.com/action",
-    L"submit_element",
-    L"username_element",
-    L"password_element",
-    L"username_value",
-    L"password_value",
-    true,
-    false,
-    1
-  };
+  PasswordFormData data = {PasswordForm::SCHEME_HTML,
+                           "http://bar.example.com",
+                           "http://bar.example.com/origin",
+                           "http://bar.example.com/action",
+                           L"submit_element",
+                           L"username_element",
+                           L"password_element",
+                           L"username_value",
+                           L"password_value",
+                           true,
+                           false,
+                           1};
   return data;
 }
 
@@ -169,16 +167,10 @@ TEST(PasswordStoreDefaultTest, NonASCIIData) {
 
   // Some non-ASCII password form data.
   static const PasswordFormData form_data[] = {
-    { PasswordForm::SCHEME_HTML,
-      "http://foo.example.com",
-      "http://foo.example.com/origin",
-      "http://foo.example.com/action",
-      L"มีสีสัน",
-      L"お元気ですか?",
-      L"盆栽",
-      L"أحب كرة",
-      L"£éä국수çà",
-      true, false, 1 },
+      {PasswordForm::SCHEME_HTML, "http://foo.example.com",
+       "http://foo.example.com/origin", "http://foo.example.com/action",
+       L"มีสีสัน", L"お元気ですか?", L"盆栽", L"أحب كرة", L"£éä국수çà", true,
+       false, 1},
   };
 
   // Build the expected forms vector and add the forms to the store.
@@ -213,7 +205,7 @@ TEST(PasswordStoreDefaultTest, Notifications) {
   store->AddObserver(&observer);
 
   const PasswordStoreChange expected_add_changes[] = {
-    PasswordStoreChange(PasswordStoreChange::ADD, *form),
+      PasswordStoreChange(PasswordStoreChange::ADD, *form),
   };
 
   EXPECT_CALL(observer,
@@ -227,7 +219,7 @@ TEST(PasswordStoreDefaultTest, Notifications) {
   form->password_value = base::ASCIIToUTF16("a different password");
 
   const PasswordStoreChange expected_update_changes[] = {
-    PasswordStoreChange(PasswordStoreChange::UPDATE, *form),
+      PasswordStoreChange(PasswordStoreChange::UPDATE, *form),
   };
 
   EXPECT_CALL(observer,
@@ -238,7 +230,7 @@ TEST(PasswordStoreDefaultTest, Notifications) {
   base::RunLoop().RunUntilIdle();
 
   const PasswordStoreChange expected_delete_changes[] = {
-    PasswordStoreChange(PasswordStoreChange::REMOVE, *form),
+      PasswordStoreChange(PasswordStoreChange::REMOVE, *form),
   };
 
   EXPECT_CALL(observer,
