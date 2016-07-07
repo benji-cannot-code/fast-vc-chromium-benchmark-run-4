72f7df0d002ad348d88815ace19a6b70bfdede4c
yosin <yosin@chromium.org>
2016-07-06 21:41:48 -0700

    Make ReplaceSelectionCommand to handle empty SPAN replacement
    This patch makes |ReplaceSelectionCommand| to handle empty SPAN replacement.
    
    Before this patch, it crashes at |SimplifyMarkupCommand::doApply()| with
    null pointer reference passed by |ReplaceSelectionCommand| with
    null |insertNodes.firstNodeInserted()|.
    
    BUG=619131
    TEST=run_webkit_unit_tests --gtest_filter=ReplaceSelectionCommandTest.pastingEmptySpan
    
    Review-Url: https://codereview.chromium.org/2122003003
    Cr-Commit-Position: refs/heads/master@{#404075}

--- third_party/WebKit/Source/core/editing/commands/ReplaceSelectionCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/ReplaceSelectionCommand.cpp
@@ -1314,7 +1314,7 @@ void ReplaceSelectionCommand::doApply(EditingState* editingState)
     if (editingState->isAborted())
         return;
 
-    if (m_sanitizeFragment) {
+    if (m_sanitizeFragment && insertedNodes.firstNodeInserted()) {
         applyCommandToComposite(SimplifyMarkupCommand::create(document(), insertedNodes.firstNodeInserted(), insertedNodes.pastLastLeaf()), editingState);
         if (editingState->isAborted())
             return;
