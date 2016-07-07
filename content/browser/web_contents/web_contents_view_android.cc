9c7c2242a4cb06ac951930a3e94507843ef54f26
hush <hush@chromium.org>
2016-07-07 15:12:52 -0700

    Support dragging texts into Chrome on Android.
    To do so, we just need to plumb the DragEvent from ContentView.
    This CL also corrects the event coordinates passed to blink, which expects CSS
    pixels.
    
    BUG=584789
    
    Review-Url: https://codereview.chromium.org/2113183003
    Cr-Commit-Position: refs/heads/master@{#404248}

--- content/browser/web_contents/web_contents_view_android.cc
+++ content/browser/web_contents/web_contents_view_android.cc
@@ -199,7 +199,7 @@ void WebContentsViewAndroid::StartDragging(
 }
 
 void WebContentsViewAndroid::UpdateDragCursor(blink::WebDragOperation op) {
-  NOTIMPLEMENTED();
+  // Intentional no-op because Android does not have cursor.
 }
 
 void WebContentsViewAndroid::OnDragEntered(
