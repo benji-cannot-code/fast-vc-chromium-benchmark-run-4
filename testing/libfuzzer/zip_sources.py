88e166c0e571f5692a5cef2e16aac0371f826ff8
aizatsky <aizatsky@chromium.org>
2016-07-07 15:12:16 -0700

    [libfuzzer] zip_sources: using system dwarfdump instead of llvm.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2125393002
    Cr-Commit-Position: refs/heads/master@{#404247}

--- testing/libfuzzer/zip_sources.py
+++ testing/libfuzzer/zip_sources.py
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Archive all source files that are references in binary debug info.
 
-Invoked by libfuzzer buildbots. Executes llvm-dwarfdump to parse debug info.
+Invoked by libfuzzer buildbots. Executes dwarfdump to parse debug info.
 """
 
 from __future__ import print_function
@@ -19,7 +19,7 @@ import subprocess
 import zipfile
 
 compile_unit_re = re.compile('.*DW_TAG_compile_unit.*')
-at_name_re = re.compile('.*DW_AT_name.*debug_str\[.*\] = "(.*)".*')
+at_name_re = re.compile('.*DW_AT_name.*"(.*)".*')
 
 
 def main():
@@ -28,15 +28,17 @@ def main():
           help='binary file to read')
   parser.add_argument('--workdir', required=True,
           help='working directory to use to resolve relative paths')
+  parser.add_argument('--srcdir', required=True,
+          help='sources root directory to calculate zip entry names')
   parser.add_argument('--output', required=True,
           help='output zip file name')
-  parser.add_argument('--llvm-dwarfdump', required=False,
-          default='llvm-dwarfdump', help='path to llvm-dwarfdump utility')
+  parser.add_argument('--dwarfdump', required=False,
+          default='dwarfdump', help='path to dwarfdump utility')
   args = parser.parse_args()
 
   # Dump .debug_info section.
   out = subprocess.check_output(
-          [args.llvm_dwarfdump, '-debug-dump=info', args.binary])
+          [args.dwarfdump, '-i', args.binary])
 
   looking_for_unit = True
   compile_units = set()
@@ -56,7 +58,7 @@ def main():
     for compile_unit in sorted(compile_units):
       src_file = os.path.abspath(os.path.join(args.workdir, compile_unit))
       print(src_file)
-      z.write(src_file, compile_unit)
+      z.write(src_file, os.path.relpath(src_file, args.srcdir))
 
 
 if __name__ == '__main__':
