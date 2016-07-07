45e5f9fc32dcf6dad069ad66190df899b4fc1584
kylechar <kylechar@chromium.org>
2016-07-07 06:54:07 -0700

    Make PlatformScreenImplOzone use DisplayConfigurator.
    Currently the PlatformScreen implementation for Ozone uses
    NativeDisplayDelegate directly. DisplayConfigurator is already designed
    to use it and is accessible from services/ui. Change the implementation
    to use DisplayConfigurator instead.
    
    This provides PlatformScreen with much of the necessary display
    configuration code it will need for multiple monitor support. For now
    PlatformScreenImplOzone just forces the native resolution on 1 display.
    
    BUG=625876
    
    Review-Url: https://codereview.chromium.org/2121053002
    Cr-Commit-Position: refs/heads/master@{#404151}

--- services/ui/ws/platform_screen.h
+++ services/ui/ws/platform_screen.h
@@ -21,6 +21,9 @@ namespace ws {
 // attached physical displays.
 class PlatformScreen {
  public:
+  using ConfiguredDisplayCallback =
+      base::Callback<void(int64_t, const gfx::Rect&)>;
+
   virtual ~PlatformScreen() {}
 
   // Creates a PlatformScreen instance.
@@ -28,9 +31,6 @@ class PlatformScreen {
 
   // Initializes platform specific screen resources.
   virtual void Init() = 0;
-
-  using ConfiguredDisplayCallback =
-      base::Callback<void(int64_t, const gfx::Rect&)>;
 
   // ConfigurePhysicalDisplay() configures a single physical display and returns
   // its id and bounds for it via |callback|.
