a01d1050d15d7f889e767e671ae22a22ff3d54aa
yinjie <yinjie@chromium.org>
2016-07-07 10:40:25 -0700

    Add const modifier to HpackDecoderInterface::decoded_block(). This eliminates the need for access via the HpackDecoderPeer. This is in aid of a coming CL adding an implementation of HpackDecoderInterface.
    This CL lands server change 125948766 by jamessynge.
    
    BUG=488484
    
    Review-Url: https://codereview.chromium.org/2122393002
    Cr-Commit-Position: refs/heads/master@{#404169}

--- net/spdy/hpack/hpack_decoder_test.cc
+++ net/spdy/hpack/hpack_decoder_test.cc
@@ -36,9 +36,6 @@ class HpackDecoderPeer {
     return decoder_->DecodeNextName(in, out);
   }
   HpackHeaderTable* header_table() { return &decoder_->header_table_; }
-  const SpdyHeaderBlock& decoded_block() const {
-    return decoder_->decoded_block_;
-  }
 
   bool DecodeNextStringLiteral(HpackInputStream* in,
                                bool is_header_key,
@@ -89,7 +86,7 @@ class HpackDecoderTest : public ::testing::TestWithParam<bool> {
     if (handler_exists_) {
       return handler_.decoded_block();
     } else {
-      return decoder_peer_.decoded_block();
+      return decoder_.decoded_block();
     }
   }
 
