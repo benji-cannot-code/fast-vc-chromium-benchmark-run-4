48bb6b6b1758c8f6f780bd0c63e56e6f0f768a03
kochi <kochi@chromium.org>
2016-07-07 02:21:39 -0700

    Add a comment about why HTMLObjectElement inherits FormAssociatedElement
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2123963003
    Cr-Commit-Position: refs/heads/master@{#404110}

--- third_party/WebKit/Source/core/html/HTMLObjectElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLObjectElement.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/shadow/ShadowRoot.h"
 #include "core/fetch/ImageResource.h"
 #include "core/frame/Settings.h"
-#include "core/html/FormData.h"
 #include "core/html/HTMLDocument.h"
 #include "core/html/HTMLImageLoader.h"
 #include "core/html/HTMLMetaElement.h"
