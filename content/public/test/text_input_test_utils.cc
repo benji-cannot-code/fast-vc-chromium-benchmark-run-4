054f6ca2da1b8a5cd6393367dd731845200c6911
ekaramad <ekaramad@chromium.org>
2016-07-06 19:44:51 -0700

    Tracking SelectionBounds for all RenderWidgets on the browser side.
    This patch implements the RenderWidgetHostViewBase::SelectionBoundsChanged as well as
    RenderWidgetHostViewChildFrame::SelectionBoundsChanged and moves the logic inside the
    RenderWidgetHostViewAura::SelectionBoundsChanged to the TextInputManager owned by the
    WebContentsImpl. This patch also adds a test to verify that TextInputManager tracks
    all the RWHVs selection bounds.
    
    The same implementation will be required for Mac; but perhaps it requires slight changes
    on the logic to store the state from ViewHostMsg_SelectionBounds_Params.
    
    BUG=578168, 602723
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2057803002
    Cr-Commit-Position: refs/heads/master@{#404043}

--- content/public/test/text_input_test_utils.cc
+++ content/public/test/text_input_test_utils.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/text_input_manager.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/common/text_input_state.h"
+#include "content/common/view_messages.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -36,7 +37,6 @@ class TextInputManagerTester::InternalObserver
  public:
   InternalObserver(WebContents* web_contents, TextInputManagerTester* tester)
       : WebContentsObserver(web_contents),
-        tester_(tester),
         updated_view_(nullptr),
         text_input_state_changed_(false) {
     text_input_manager_ =
@@ -51,8 +51,12 @@ class TextInputManagerTester::InternalObserver
   }
 
   void set_update_text_input_state_called_callback(
-      const TextInputManagerTester::Callback& callback) {
+      const base::Closure& callback) {
     update_text_input_state_callback_ = callback;
+  }
+
+  void set_on_selection_bounds_changed_callback(const base::Closure& callback) {
+    on_selection_bounds_changed_callback_ = callback;
   }
 
   const RenderWidgetHostView* GetUpdatedView() const { return updated_view_; }
@@ -69,18 +73,27 @@ class TextInputManagerTester::InternalObserver
       return;
     text_input_state_changed_ = did_change_state;
     updated_view_ = updated_view;
-    update_text_input_state_callback_.Run(tester_);
+    if (!update_text_input_state_callback_.is_null())
+      update_text_input_state_callback_.Run();
+  }
+
+  void OnSelectionBoundsChanged(
+      TextInputManager* text_input_manager_,
+      RenderWidgetHostViewBase* updated_view) override {
+    updated_view_ = updated_view;
+    if (!on_selection_bounds_changed_callback_.is_null())
+      on_selection_bounds_changed_callback_.Run();
   }
 
   // WebContentsObserver implementation.
   void WebContentsDestroyed() override { text_input_manager_ = nullptr; }
 
  private:
-  TextInputManagerTester* tester_;
   TextInputManager* text_input_manager_;
   RenderWidgetHostViewBase* updated_view_;
   bool text_input_state_changed_;
-  TextInputManagerTester::Callback update_text_input_state_callback_;
+  base::Closure update_text_input_state_callback_;
+  base::Closure on_selection_bounds_changed_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(InternalObserver);
 };
@@ -223,8 +236,13 @@ TextInputManagerTester::TextInputManagerTester(WebContents* web_contents)
 TextInputManagerTester::~TextInputManagerTester() {}
 
 void TextInputManagerTester::SetUpdateTextInputStateCalledCallback(
-    const Callback& callback) {
+    const base::Closure& callback) {
   observer_->set_update_text_input_state_called_callback(callback);
+}
+
+void TextInputManagerTester::SetOnSelectionBoundsChangedCallback(
+    const base::Closure& callback) {
+  observer_->set_on_selection_bounds_changed_callback(callback);
 }
 
 bool TextInputManagerTester::GetTextInputType(ui::TextInputType* type) {
