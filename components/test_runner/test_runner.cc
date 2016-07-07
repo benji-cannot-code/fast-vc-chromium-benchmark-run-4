eafbb9f9ccf1d23c2932c6ad7f3c8d99b1d097bf
wjmaclean <wjmaclean@chromium.org>
2016-07-06 17:35:50 -0700

    Remove duplicated function in RenderView.
    TBR=brettw@chromium.org
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2123833002
    Cr-Commit-Position: refs/heads/master@{#404016}

--- components/test_runner/test_runner.cc
+++ components/test_runner/test_runner.cc
@@ -1724,8 +1724,7 @@ void TestRunner::DumpPixelsAsync(
   }
 
   test_runner::DumpPixelsAsync(web_view, layout_test_runtime_flags_,
-                               delegate_->GetDeviceScaleFactorForTest(),
-                               callback);
+                               delegate_->GetDeviceScaleFactor(), callback);
 }
 
 void TestRunner::ReplicateLayoutTestRuntimeFlagsChanges(
