50eb304a0ba33f7aceddf8c7fc1aa2780f405a7c
dmurph <dmurph@chromium.org>
2016-07-07 12:06:19 -0700

    Remove redundant uint64_t typedef
    This was created by this refactoring patch:
    https://codereview.chromium.org/1538743002
    
    R=avi@chromium.org, mark@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2123973003
    Cr-Commit-Position: refs/heads/master@{#404201}

--- base/third_party/symbolize/utilities.h
+++ base/third_party/symbolize/utilities.h
@@ -7,6 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef uint64_t uint64_t;
+
 #define HAVE_SYMBOLIZE 1
 #define ATTRIBUTE_NOINLINE __attribute__ ((noinline))
