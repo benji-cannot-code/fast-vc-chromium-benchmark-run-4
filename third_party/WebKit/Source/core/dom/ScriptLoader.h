393da87e88bdccadf1510cc650ae88b38519b512
csharrison <csharrison@chromium.org>
2016-07-07 06:58:40 -0700

    Revert of Don't preload scripts with invalid type/language attributes (patchset #5 id:80001 of https://codereview.chromium.org/2099853002/ )
    Reason for revert:
    Some compat risk for this change was brought up. Going to send a note to blink-dev before re-landing.
    
    Original issue's description:
    > Don't preload scripts with invalid type/language attributes
    >
    > This patch keeps track of a script tag's type and language attributes,
    > and gates preloading if they are invalid. This aligns the preload scanner's
    > policy with the ScriptLoader's policy.
    >
    > BUG=623109
    >
    > Committed: https://crrev.com/23f4df7587f338837a709afb192900102584fc0a
    > Cr-Commit-Position: refs/heads/master@{#403182}
    
    TBR=yoav@yoav.ws
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=623109
    
    Review-Url: https://codereview.chromium.org/2130773002
    Cr-Commit-Position: refs/heads/master@{#404153}

--- third_party/WebKit/Source/core/dom/ScriptLoader.h
+++ third_party/WebKit/Source/core/dom/ScriptLoader.h
@@ -51,8 +51,6 @@ public:
     Element* element() const { return m_element; }
 
     enum LegacyTypeSupport { DisallowLegacyTypeInTypeAttribute, AllowLegacyTypeInTypeAttribute };
-    static bool isValidScriptTypeAndLanguage(const String& typeAttributeValue, const String& languageAttributeValue, LegacyTypeSupport supportLegacyTypes);
-
     bool prepareScript(const TextPosition& scriptStartPosition = TextPosition::minimumPosition(), LegacyTypeSupport = DisallowLegacyTypeInTypeAttribute);
 
     String scriptCharset() const { return m_characterEncoding; }
--- third_party/WebKit/Source/core/html/parser/HTMLPreloadScanner.cpp
+++ third_party/WebKit/Source/core/html/parser/HTMLPreloadScanner.cpp
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/MediaValuesCached.h"
 #include "core/css/parser/SizesAttributeParser.h"
 #include "core/dom/Document.h"
-#include "core/dom/ScriptLoader.h"
 #include "core/fetch/IntegrityMetadata.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/Settings.h"
@@ -223,6 +222,7 @@ public:
             }
         }
 
+
         TextPosition position = TextPosition(source.currentLine(), source.currentColumn());
         FetchRequest::ResourceWidth resourceWidth;
         float sourceSize = m_sourceSize;
@@ -273,10 +273,6 @@ private:
         // explanation.
         else if (match(attributeName, integrityAttr))
             SubresourceIntegrity::parseIntegrityAttribute(attributeValue, m_integrityMetadata);
-        else if (match(attributeName, typeAttr))
-            m_typeAttributeValue = attributeValue;
-        else if (match(attributeName, languageAttr))
-            m_languageAttributeValue = attributeValue;
     }
 
     template<typename NameType>
@@ -445,8 +441,6 @@ private:
             return false;
         if (match(m_tagImpl, inputTag) && !m_inputIsImage)
             return false;
-        if (match(m_tagImpl, scriptTag) && !ScriptLoader::isValidScriptTypeAndLanguage(m_typeAttributeValue, m_languageAttributeValue, ScriptLoader::AllowLegacyTypeInTypeAttribute))
-            return false;
         return true;
     }
 
@@ -478,8 +472,6 @@ private:
     String m_imgSrcUrl;
     String m_srcsetAttributeValue;
     String m_asAttributeValue;
-    String m_typeAttributeValue;
-    String m_languageAttributeValue;
     float m_sourceSize;
     bool m_sourceSizeSet;
     FetchRequest::DeferOption m_defer;
