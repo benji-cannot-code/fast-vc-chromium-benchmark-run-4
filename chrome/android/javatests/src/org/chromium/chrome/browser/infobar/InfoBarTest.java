f027f0134fe033a93122aa6cd1109627e7d9471f
megjablon <megjablon@chromium.org>
2016-07-08 15:07:01 -0700

    Re-enable DataReductionPromoInfoBar tests and fix flakiness
    StrictMode violation was from Calendar.getInstance() reading
    the /etc/timezone file. Whitelist the StrictMode violation
    to be fixed.
    
    Tests were flaky because the UI thread would not always launch
    the InfoBar before calling addInfoBarAnimationFinished(). Run
    the UI thread blocking so that the InfoBar has always been
    launched.
    
    BUG=625038, 577185
    
    Review-Url: https://codereview.chromium.org/2130083002
    Cr-Commit-Position: refs/heads/master@{#404506}

--- chrome/android/javatests/src/org/chromium/chrome/browser/infobar/InfoBarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/infobar/InfoBarTest.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.infobar;
 
 import static org.chromium.base.test.util.ScalableTimeout.scaleTimeout;
 
+import android.content.Context;
 import android.os.Environment;
 import android.test.UiThreadTest;
 import android.test.suitebuilder.annotation.MediumTest;
@@ -15,6 +16,7 @@ import android.test.suitebuilder.annotation.Smoke;
 import org.chromium.base.CommandLine;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.AdvancedMockContext;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
@@ -83,6 +85,11 @@ public class InfoBarTest extends ChromeActivityTestCaseBase<ChromeActivity> {
 
         mTestServer = EmbeddedTestServer.createAndStartFileServer(
                 getInstrumentation().getContext(), Environment.getExternalStorageDirectory());
+
+        // Using an AdvancedMockContext allows us to use a fresh in-memory SharedPreference.
+        Context context = new AdvancedMockContext(
+                getInstrumentation().getTargetContext().getApplicationContext());
+        ContextUtils.initApplicationContextForTests(context);
     }
 
     @Override
@@ -176,9 +183,8 @@ public class InfoBarTest extends ChromeActivityTestCaseBase<ChromeActivity> {
      */
     @MediumTest
     @Feature({"Browser", "Main"})
-    @DisabledTest // crbug.com/625038
     public void testDataReductionPromoInfoBar() throws InterruptedException {
-        ThreadUtils.runOnUiThread(new Runnable() {
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
                 assertFalse("Data Reduction Proxy enabled",
@@ -204,7 +210,7 @@ public class InfoBarTest extends ChromeActivityTestCaseBase<ChromeActivity> {
         assertTrue("InfoBar does not have secondary button",
                 InfoBarUtil.hasSecondaryButton(infoBars.get(0)));
 
-        ThreadUtils.runOnUiThread(new Runnable() {
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
                 InfoBarUtil.clickPrimaryButton(infoBars.get(0));
@@ -215,7 +221,7 @@ public class InfoBarTest extends ChromeActivityTestCaseBase<ChromeActivity> {
         assertTrue("InfoBar not removed.", mListener.removeInfoBarAnimationFinished());
         assertTrue("Wrong infobar count", getInfoBars().isEmpty());
 
-        ThreadUtils.runOnUiThread(new Runnable() {
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
                 assertTrue("Data Reduction Proxy not enabled",
@@ -238,9 +244,8 @@ public class InfoBarTest extends ChromeActivityTestCaseBase<ChromeActivity> {
      */
     @MediumTest
     @Feature({"Browser", "Main"})
-    @DisabledTest // crbug.com/625038
     public void testDataReductionPromoInfoBarDismissed() throws InterruptedException {
-        ThreadUtils.runOnUiThread(new Runnable() {
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
                 assertFalse("Data Reduction Proxy enabled",
@@ -266,7 +271,7 @@ public class InfoBarTest extends ChromeActivityTestCaseBase<ChromeActivity> {
         assertTrue("InfoBar does not have secondary button",
                 InfoBarUtil.hasSecondaryButton(infoBars.get(0)));
 
-        ThreadUtils.runOnUiThread(new Runnable() {
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
                 InfoBarUtil.clickSecondaryButton(infoBars.get(0));
@@ -277,7 +282,7 @@ public class InfoBarTest extends ChromeActivityTestCaseBase<ChromeActivity> {
         assertTrue("InfoBar not removed.", mListener.removeInfoBarAnimationFinished());
         assertTrue("Wrong infobar count", getInfoBars().isEmpty());
 
-        ThreadUtils.runOnUiThread(new Runnable() {
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
                 assertFalse("Data Reduction Proxy enabled",
@@ -298,7 +303,6 @@ public class InfoBarTest extends ChromeActivityTestCaseBase<ChromeActivity> {
     @UiThreadTest
     @MediumTest
     @Feature({"Browser", "Main"})
-    @DisabledTest // crbug.com/625038
     public void testDataReductionPromoInfoBarPostM48Install() {
         assertFalse("Data Reduction Proxy enabled",
                 DataReductionProxySettings.getInstance().isDataReductionProxyEnabled());
@@ -323,7 +327,6 @@ public class InfoBarTest extends ChromeActivityTestCaseBase<ChromeActivity> {
     @UiThreadTest
     @MediumTest
     @Feature({"Browser", "Main"})
-    @DisabledTest // crbug.com/625038
     public void testDataReductionPromoInfoBarFreOptOut() {
         // Try to add an infobar. Infobar should not be added since the first run experience or
         // second run promo hasn't been shown.
