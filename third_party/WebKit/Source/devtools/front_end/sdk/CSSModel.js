170b8df3e71f1008ac2fbd37a307005195e52045
lushnikov <lushnikov@chromium.org>
2016-07-08 02:25:23 -0700

    DevTools: do not detach sourcemap in case of style sheet editing
    Also the sourcemapping does not make sense for the edited style sheet,
    it still seems to be helpful for the web developers as it
    gives a brief idea of source file name / approximate line
    number.
    
    This was regressed at crrev.com/1806233002
    
    BUG=257778
    R=dgozman
    
    Review-Url: https://codereview.chromium.org/2116903002
    Cr-Commit-Position: refs/heads/master@{#404316}

--- third_party/WebKit/Source/devtools/front_end/sdk/CSSModel.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/CSSModel.js
@@ -261,7 +261,7 @@ WebInspector.CSSModel.prototype = {
         var originalAndDetach = originalAndDetachIfSuccess.bind(this, header);
 
         if (!sourceMap.editable())
-            return originalAndDetach();
+            return original();
 
         return /** @type {!Promise<boolean>} */(sourceMap.editCompiled([range], [text])
             .then(onEditingDone.bind(this))
@@ -419,7 +419,7 @@ WebInspector.CSSModel.prototype = {
                 return false;
             this._domModel.markUndoableState();
             var edit = new WebInspector.CSSModel.Edit(styleSheetId, range, text, selectorPayload);
-            this._fireStyleSheetChangedAndDetach(styleSheetId, edit);
+            this._fireStyleSheetChanged(styleSheetId, edit);
             return true;
         }
 
@@ -449,7 +449,7 @@ WebInspector.CSSModel.prototype = {
                 return false;
             this._domModel.markUndoableState();
             var edit = new WebInspector.CSSModel.Edit(styleSheetId, range, text, payload);
-            this._fireStyleSheetChangedAndDetach(styleSheetId, edit);
+            this._fireStyleSheetChanged(styleSheetId, edit);
             return true;
         }
 
@@ -683,7 +683,7 @@ WebInspector.CSSModel.prototype = {
                 return false;
             this._domModel.markUndoableState();
             var edit = new WebInspector.CSSModel.Edit(styleSheetId, range, newMediaText, mediaPayload);
-            this._fireStyleSheetChangedAndDetach(styleSheetId, edit);
+            this._fireStyleSheetChanged(styleSheetId, edit);
             return true;
         }
 
@@ -717,7 +717,7 @@ WebInspector.CSSModel.prototype = {
                 return null;
             this._domModel.markUndoableState();
             var edit = new WebInspector.CSSModel.Edit(styleSheetId, ruleLocation, ruleText, rulePayload);
-            this._fireStyleSheetChangedAndDetach(styleSheetId, edit);
+            this._fireStyleSheetChanged(styleSheetId, edit);
             return new WebInspector.CSSStyleRule(this, rulePayload);
         }
     },
@@ -783,18 +783,6 @@ WebInspector.CSSModel.prototype = {
     _fireStyleSheetChanged: function(styleSheetId, edit)
     {
         this.dispatchEventToListeners(WebInspector.CSSModel.Events.StyleSheetChanged, { styleSheetId: styleSheetId, edit: edit });
-    },
-
-    /**
-     * @param {!CSSAgent.StyleSheetId} styleSheetId
-     * @param {!WebInspector.CSSModel.Edit=} edit
-     */
-    _fireStyleSheetChangedAndDetach: function(styleSheetId, edit)
-    {
-        this.dispatchEventToListeners(WebInspector.CSSModel.Events.StyleSheetChanged, { styleSheetId: styleSheetId, edit: edit });
-        var header = this.styleSheetHeaderForId(styleSheetId);
-        if (header)
-            this._detachSourceMap(header);
     },
 
     /**
@@ -1097,7 +1085,7 @@ WebInspector.CSSDispatcher.prototype = {
      */
     styleSheetChanged: function(styleSheetId)
     {
-        this._cssModel._fireStyleSheetChangedAndDetach(styleSheetId);
+        this._cssModel._fireStyleSheetChanged(styleSheetId);
     },
 
     /**
