0091cd4b1afa2d202b0794a1c927e9a9c311e8ee
spqchan <spqchan@chromium.org>
2016-07-08 11:06:25 -0700

    [Material][Mac] Fix the indicator and close button in a dragged tab
    Override hasDarkTheme: in TabWindowOverlayWindow so that we
    return the correct value. Make sure that the indicator icon updates its
    color when it moves to a new window.
    
    BUG=621348
    
    Review-Url: https://codereview.chromium.org/2126953002
    Cr-Commit-Position: refs/heads/master@{#404438}

--- chrome/browser/ui/cocoa/tabs/tab_window_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_window_controller.mm
@@ -46,6 +46,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return [[self parentWindow] themeImagePositionForAlignment:alignment];
 }
 
+- (BOOL)hasDarkTheme {
+  return [[self parentWindow] hasDarkTheme];
+}
+
 @end
 
 @implementation TabWindowController
