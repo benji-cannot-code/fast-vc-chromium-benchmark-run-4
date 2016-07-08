e030624a867f1c4e75c75fd81662907906983b24
hush <hush@chromium.org>
2016-07-07 19:23:37 -0700

    Revert of QUIC - Race Cert Verification with host resolution if certs are (patchset #4 id:100001 of https://codereview.chromium.org/2120703003/ )
    Reason for revert:
    Failed cronet unit test and net unit test
    https://build.chromium.org/p/chromium.android/builders/Android%20Cronet%20Data%20Reduction%20Proxy%20Builder/builds/2590/steps/cronet_unittests/logs/stdio
    https://build.chromium.org/p/chromium.android/builders/Android%20Cronet%20Data%20Reduction%20Proxy%20Builder/builds/2590/steps/net_unittests/logs/stdio
    
    Original issue's description:
    > QUIC - Race Cert Verification with host resolution if certs are
    > availiable when race_cert_verification network session param is enabled.
    >
    > This experiment is not enabled in chromium.
    > CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    >
    > R=eroman@chromium.org, mef@chromium.org, rch@chromium.org,
    > TBR=eroman@chromium.org (for net-internals changes)
    >
    > Committed: https://crrev.com/61f8d17a6a2ee893f42f62164a81026c56186c1d
    > Cr-Commit-Position: refs/heads/master@{#404296}
    
    TBR=eroman@chromium.org,mef@chromium.org,rch@chromium.org,rtenneti@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2131813002
    Cr-Commit-Position: refs/heads/master@{#404311}

--- net/quic/test_tools/quic_stream_factory_peer.cc
+++ net/quic/test_tools/quic_stream_factory_peer.cc
@@ -8,14 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "net/cert/x509_certificate.h"
 #include "net/quic/crypto/quic_crypto_client_config.h"
 #include "net/quic/quic_chromium_client_session.h"
 #include "net/quic/quic_clock.h"
 #include "net/quic/quic_http_stream.h"
 #include "net/quic/quic_stream_factory.h"
-#include "net/test/cert_test_util.h"
-#include "net/test/test_data_directory.h"
 
 using std::string;
 using std::vector;
@@ -35,12 +32,6 @@ QuicCryptoClientConfig* QuicStreamFactoryPeer::GetCryptoConfig(
 bool QuicStreamFactoryPeer::HasActiveSession(QuicStreamFactory* factory,
                                              const QuicServerId& server_id) {
   return factory->HasActiveSession(server_id);
-}
-
-bool QuicStreamFactoryPeer::HasActiveCertVerifierJob(
-    QuicStreamFactory* factory,
-    const QuicServerId& server_id) {
-  return factory->HasActiveCertVerifierJob(server_id);
 }
 
 QuicChromiumClientSession* QuicStreamFactoryPeer::GetActiveSession(
@@ -90,24 +81,6 @@ bool QuicStreamFactoryPeer::GetDelayTcpRace(QuicStreamFactory* factory) {
 void QuicStreamFactoryPeer::SetDelayTcpRace(QuicStreamFactory* factory,
                                             bool delay_tcp_race) {
   factory->delay_tcp_race_ = delay_tcp_race;
-}
-
-bool QuicStreamFactoryPeer::GetRaceCertVerification(
-    QuicStreamFactory* factory) {
-  return factory->race_cert_verification_;
-}
-
-void QuicStreamFactoryPeer::SetRaceCertVerification(
-    QuicStreamFactory* factory,
-    bool race_cert_verification) {
-  factory->race_cert_verification_ = race_cert_verification;
-}
-
-void QuicStreamFactoryPeer::StartCertVerifyJob(QuicStreamFactory* factory,
-                                               const QuicServerId& server_id,
-                                               int cert_verify_flags,
-                                               const BoundNetLog& net_log) {
-  factory->StartCertVerifyJob(server_id, cert_verify_flags, net_log);
 }
 
 void QuicStreamFactoryPeer::SetYieldAfterPackets(QuicStreamFactory* factory,
@@ -177,15 +150,9 @@ void QuicStreamFactoryPeer::CacheDummyServerConfig(
   string server_config(reinterpret_cast<const char*>(&scfg), sizeof(scfg));
   string source_address_token("test_source_address_token");
   string signature("test_signature");
-
+  string test_cert("test_cert");
   vector<string> certs;
-  // Load a certificate that is valid for *.example.org
-  scoped_refptr<X509Certificate> cert(
-      ImportCertFromFile(GetTestCertsDirectory(), "wildcard.pem"));
-  DCHECK(cert);
-  std::string der_bytes;
-  DCHECK(X509Certificate::GetDEREncoded(cert->os_cert_handle(), &der_bytes));
-  certs.push_back(der_bytes);
+  certs.push_back(test_cert);
 
   QuicCryptoClientConfig* crypto_config = &factory->crypto_config_;
   QuicCryptoClientConfig::CachedState* cached =
