0c29cc6d96a46588742755c4489ca138b51559bd
kouhei <kouhei@chromium.org>
2016-07-08 07:18:44 -0700

    Use DCHECK_NE in HTMLToken.h
    Simple cosmetic change / no behavioral change.
    
    BUG=None
    TBR=csharrison@chromium.org
    
    Review-Url: https://codereview.chromium.org/2134543002
    Cr-Commit-Position: refs/heads/master@{#404366}

--- third_party/WebKit/Source/core/html/parser/HTMLToken.h
+++ third_party/WebKit/Source/core/html/parser/HTMLToken.h
@@ -95,7 +95,7 @@ public:
             // Check Range instance that is actively being parsed.
             inline void checkValidStart() const
             {
-                DCHECK(start != kInvalidOffset);
+                DCHECK_NE(start, kInvalidOffset);
                 DCHECK_GE(start, 0);
             }
 
@@ -103,7 +103,7 @@ public:
             inline void checkValid() const
             {
                 checkValidStart();
-                DCHECK(end != kInvalidOffset);
+                DCHECK_NE(end, kInvalidOffset);
                 DCHECK_GE(end, 0);
                 DCHECK_LE(start, end);
             }
