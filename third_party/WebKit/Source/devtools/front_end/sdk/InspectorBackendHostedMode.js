89ffdaecba9d3d5412d28aebdd5fd6003c6a9b91
lushnikov <lushnikov@chromium.org>
2016-07-08 11:28:12 -0700

    DevTools: copy protocol files to front-end to fix hosted mode
    DevTools "hosted mode" is broken in a multiple ways:
    - it uses sync XHR from inside the microtask (this was recentrly forbidden)
    - as the protocol was split and moved around, the InspectorBackendHostedMode.js
      is not able to generate proper inspector backend commands
    
    The patch fixes both problems. Here we:
    - manually copy browser_protocol.json and js_protocol.json to the front-end
      folder so that hosted mode can rely on it
    - preload protocol files into Runtime.cachedResources in hosted mode for future
      use of InspectorBackendHostedMode.js
    
    BUG=623602
    R=dgozman, pfeldman
    
    Review-Url: https://codereview.chromium.org/2124203002
    Cr-Commit-Position: refs/heads/master@{#404447}

--- third_party/WebKit/Source/devtools/front_end/sdk/InspectorBackendHostedMode.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/InspectorBackendHostedMode.js
@@ -7,21 +7,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 WebInspector.InspectorBackendHostedMode = {};
 
-/**
- * @param {string} jsonUrl
- */
-WebInspector.InspectorBackendHostedMode.loadFromJSONIfNeeded = function(jsonUrl)
+WebInspector.InspectorBackendHostedMode.loadFromJSONIfNeeded = function()
 {
     if (InspectorBackend.isInitialized())
         return;
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", jsonUrl, false);
-    xhr.send(null);
-
-    var schema = JSON.parse(xhr.responseText);
-    var code = WebInspector.InspectorBackendHostedMode.generateCommands(schema);
-    eval(code);
+    for (var url of Object.keys(Runtime.cachedResources)) {
+        if (url.indexOf("protocol.json") !== -1) {
+            var protocol = Runtime.cachedResources[url];
+            var code = WebInspector.InspectorBackendHostedMode.generateCommands(JSON.parse(protocol));
+            eval(code);
+        }
+    }
 }
 
 /**
@@ -123,4 +120,4 @@ WebInspector.InspectorBackendHostedMode.generateCommands = function(schema)
     return result.join("\n");
 }
 
-WebInspector.InspectorBackendHostedMode.loadFromJSONIfNeeded("../inspector.json");
+WebInspector.InspectorBackendHostedMode.loadFromJSONIfNeeded();
