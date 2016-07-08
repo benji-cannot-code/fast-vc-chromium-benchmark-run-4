be0645085f08cd887364af8b97112f50ac074317
fsamuel <fsamuel@chromium.org>
2016-07-08 16:02:44 -0700

    Make all tests profilable
    We did not run profiling on unit and perf tests even when the
    enable_profiling build flag was present. In order to dump profiling data,
    base::debug::StartProfiling(..) must be called somewhere. I've moved that
    code to base::TestSuite. Note that this code is a no-op unless the profiling
    build flag is present.
    
    BUG=624459
    TBR=sky@chromium.org
    
    Review-Url: https://codereview.chromium.org/2131293002
    Cr-Commit-Position: refs/heads/master@{#404522}

--- base/base_switches.h
+++ base/base_switches.h
@@ -23,6 +23,7 @@ extern const char kFullMemoryCrashReport[];
 extern const char kNoErrorDialogs[];
 extern const char kProfilerTiming[];
 extern const char kProfilerTimingDisabledValue[];
+extern const char kProfilingFile[];
 extern const char kTestChildProcess[];
 extern const char kTestDoNotInitializeIcu[];
 extern const char kTraceToFile[];
--- chrome/common/profiling.cc
+++ chrome/common/profiling.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/profiling.h"
 
 #include "base/at_exit.h"
+#include "base/base_switches.h"
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/debug/profiler.h"
