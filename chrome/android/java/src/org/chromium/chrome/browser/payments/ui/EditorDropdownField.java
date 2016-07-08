ea00763c127722baf32e7f0d12f06e1e79ab2595
gsennton <gsennton@chromium.org>
2016-07-08 02:39:54 -0700

    Revert of [Payments] Update autofill/payments spacings (patchset #9 id:160001 of https://codereview.chromium.org/2114493002/ )
    Reason for revert:
    This patch breaks the Android Tests (dbg) bot, I've filed bug crbug.com/626606 for this issue. It breaks in
    
    C  857.741s Main  [FAIL] org.chromium.chrome.browser.payments.PaymentRequestFreeShippingTest#testChangeCountryAddAddressAndPay:
    C  857.741s Main  java.lang.AssertionError
    C  857.741s Main  	at org.chromium.chrome.browser.payments.ui.EditorView.addFieldViewToEditor(EditorView.java:293)
    
    Original issue's description:
    > [Payments] Update autofill/payments spacings
    >
    > * The fields felt a little squished.  Try to space them out a little better.
    >
    > * Update the underline colors to match the TextInputLayers.
    >
    > * Add support for half width controls in the display.
    >
    > Screenshots here:
    > https://bugs.chromium.org/p/chromium/issues/detail?id=603635#c190
    >
    > BUG=603635,626451
    >
    > Committed: https://crrev.com/975b62f4fe7cfd7f3de7ab6fc49a3cc20e7f49ce
    > Cr-Commit-Position: refs/heads/master@{#404241}
    
    TBR=rouslan@chromium.org,ianwen@chromium.org,dfalcantara@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=603635,626451
    
    Review-Url: https://codereview.chromium.org/2137443002
    Cr-Commit-Position: refs/heads/master@{#404331}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/EditorDropdownField.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/EditorDropdownField.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.payments.ui;
 
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
@@ -14,7 +13,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import org.chromium.chrome.R;
 import org.chromium.chrome.browser.preferences.autofill.AutofillProfileBridge.DropdownKeyValue;
 
 import java.util.List;
@@ -24,7 +22,6 @@ import java.util.List;
  */
 class EditorDropdownField {
     private final EditorFieldModel mFieldModel;
-    private final View mLayout;
     private final TextView mLabel;
     private final Spinner mDropdown;
     private int mSelectedIndex;
@@ -42,10 +39,7 @@ class EditorDropdownField {
         assert fieldModel.getInputTypeHint() == EditorFieldModel.INPUT_TYPE_HINT_DROPDOWN;
         mFieldModel = fieldModel;
 
-        mLayout = LayoutInflater.from(context).inflate(
-                R.layout.payment_request_editor_dropdown, null, false);
-
-        mLabel = (TextView) mLayout.findViewById(R.id.spinner_label);
+        mLabel = new TextView(context);
         mLabel.setText(mFieldModel.getLabel());
 
         final List<DropdownKeyValue> dropdownKeyValues = mFieldModel.getDropdownKeyValues();
@@ -56,12 +50,11 @@ class EditorDropdownField {
             }
         }
 
-        ArrayAdapter<DropdownKeyValue> adapter = new ArrayAdapter<DropdownKeyValue>(
-                context, android.R.layout.simple_spinner_item, dropdownKeyValues);
+        ArrayAdapter<DropdownKeyValue> adapter = new ArrayAdapter<DropdownKeyValue>(context,
+                android.R.layout.simple_spinner_item, dropdownKeyValues);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        mDropdown = (Spinner) mLayout.findViewById(R.id.spinner);
-        mDropdown.setContentDescription(mFieldModel.getLabel());
+        mDropdown = new Spinner(context);
         mDropdown.setAdapter(adapter);
         mDropdown.setSelection(mSelectedIndex);
         mDropdown.setOnItemSelectedListener(new OnItemSelectedListener() {
@@ -77,11 +70,6 @@ class EditorDropdownField {
             @Override
             public void onNothingSelected(AdapterView<?> parent) {}
         });
-    }
-
-    /** @return The View containing everything. */
-    public View getLayout() {
-        return mLayout;
     }
 
     /** @return The EditorFieldModel that the EditorDropdownField represents. */
--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestTestBase.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestTestBase.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.payments;
 
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -29,7 +31,6 @@ import org.chromium.content.browser.test.util.CriteriaHelper;
 import org.chromium.content.browser.test.util.DOMUtils;
 import org.chromium.content_public.browser.WebContents;
 
-import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
@@ -213,7 +214,15 @@ abstract class PaymentRequestTestBase extends ChromeActivityTestCaseBase<ChromeA
         ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
-                ((Spinner) mUI.getEditorView().findViewById(R.id.spinner)).setSelection(selection);
+                ViewGroup contents = (ViewGroup) mUI.getEditorView().findViewById(R.id.contents);
+                assertNotNull(contents);
+                for (int i = 0; i < contents.getChildCount(); i++) {
+                    View view = contents.getChildAt(i);
+                    if (view instanceof Spinner) {
+                        ((Spinner) view).setSelection(selection);
+                        return;
+                    }
+                }
             }
         });
         helper.waitForCallback(callCount);
@@ -226,9 +235,13 @@ abstract class PaymentRequestTestBase extends ChromeActivityTestCaseBase<ChromeA
         ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
-                List<EditorTextField> fields = mUI.getEditorView().getEditorTextFields();
-                for (int i = 0; i < values.length; i++) {
-                    fields.get(i).getEditText().setText(values[i]);
+                ViewGroup contents = (ViewGroup) mUI.getEditorView().findViewById(R.id.contents);
+                assertNotNull(contents);
+                for (int i = 0, j = 0; i < contents.getChildCount() && j < values.length; i++) {
+                    View view = contents.getChildAt(i);
+                    if (view instanceof EditorTextField) {
+                        ((EditorTextField) view).getEditText().setText(values[j++]);
+                    }
                 }
             }
         });
