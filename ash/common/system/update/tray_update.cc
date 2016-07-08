57bcd546165a8fac1cd6cd7716a99de4ac809960
bruthig <bruthig@chromium.org>
2016-07-08 15:06:28 -0700

    Added UMA metrics for the system update UI in the status area.
    Record user action when:
    - User clicks the update menu item in the system menu
    
    BUG=620508
    TEST=manual
    
    Review-Url: https://codereview.chromium.org/2125403002
    Cr-Commit-Position: refs/heads/master@{#404505}

--- ash/common/system/update/tray_update.cc
+++ ash/common/system/update/tray_update.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/common/system/update/tray_update.h"
 
+#include "ash/common/metrics/user_metrics_action.h"
 #include "ash/common/system/tray/fixed_sized_image_view.h"
 #include "ash/common/system/tray/system_tray_delegate.h"
 #include "ash/common/system/tray/system_tray_notifier.h"
@@ -75,6 +76,8 @@ class UpdateView : public ActionableView {
   // Overridden from ActionableView.
   bool PerformAction(const ui::Event& event) override {
     WmShell::Get()->system_tray_delegate()->RequestRestartForUpdate();
+    WmShell::Get()->RecordUserMetricsAction(
+        UMA_STATUS_AREA_OS_UPDATE_DEFAULT_SELECTED);
     return true;
   }
 
