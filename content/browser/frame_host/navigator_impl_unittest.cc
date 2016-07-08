26d6fbacf784549d78cc86ada6d23c82a6d2ed7b
toyoshim <toyoshim@chromium.org>
2016-07-08 02:29:50 -0700

    Enable NonValidatingReloadOnNormalReload on variations testing
    To expand the field study to Beta channel, enable the feature
    on variations testing waterfall.
    
    BUG=612701
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2128783002
    Cr-Commit-Position: refs/heads/master@{#404321}

--- content/browser/frame_host/navigator_impl_unittest.cc
+++ content/browser/frame_host/navigator_impl_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include "base/feature_list.h"
 #include "base/macros.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/site_isolation_policy.h"
 #include "content/public/browser/navigation_data.h"
 #include "content/public/browser/stream_handle.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/url_constants.h"
 #include "content/public/common/url_utils.h"
 #include "content/public/test/mock_render_process_host.h"
@@ -833,8 +835,15 @@ TEST_F(NavigatorTestWithBrowserSideNavigation, Reload) {
   // A NavigationRequest should have been generated.
   NavigationRequest* main_request = node->navigation_request();
   ASSERT_TRUE(main_request != NULL);
-  EXPECT_EQ(FrameMsg_Navigate_Type::RELOAD,
-            main_request->common_params().navigation_type);
+  // TODO(toyoshim): Modify following checks once the feature is enabled.
+  if (base::FeatureList::IsEnabled(
+          features::kNonValidatingReloadOnNormalReload)) {
+    EXPECT_EQ(FrameMsg_Navigate_Type::RELOAD_MAIN_RESOURCE,
+              main_request->common_params().navigation_type);
+  } else {
+    EXPECT_EQ(FrameMsg_Navigate_Type::RELOAD,
+              main_request->common_params().navigation_type);
+  }
   main_test_rfh()->PrepareForCommit();
   EXPECT_FALSE(GetSpeculativeRenderFrameHost(node));
 
