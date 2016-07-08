1071ce25343439b679d669bd2cb9ff5e78943aca
malaykeshav <malaykeshav@chromium.org>
2016-07-08 12:35:42 -0700

    Checks the age of device before initializing HaTS Notification
    - Checks the age of device by comparing the Oobe flag file creation
      date.
    
    Design Doc=go/cros-hats-eng
    BUG=611781
    COMPONENT=Chrome OS, HaTS
    
    Review-Url: https://codereview.chromium.org/2103293003
    Cr-Commit-Position: refs/heads/master@{#404464}

--- chrome/browser/chromeos/hats/hats_notification_controller.h
+++ chrome/browser/chromeos/hats/hats_notification_controller.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_HATS_HATS_NOTIFICATION_CONTROLLER_H_
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "chrome/browser/notifications/notification.h"
 #include "chrome/browser/notifications/notification_delegate.h"
@@ -24,7 +25,10 @@ class HatsNotificationController : public NotificationDelegate,
  public:
   // Minimum amount of time before the notification is displayed again after a
   // user has interacted with it.
-  static const base::TimeDelta kHatsThresholdTime;
+  static const int kHatsThresholdDays;
+  // Minimum amount of time after initial login or oobe after which we can show
+  // the HaTS notification.
+  static const int kHatsNewDeviceThresholdDays;
   static const char kDelegateId[];
   static const char kNotificationId[];
 
@@ -37,6 +41,7 @@ class HatsNotificationController : public NotificationDelegate,
   ~HatsNotificationController() override;
 
   // NotificationDelegate overrides:
+  void Initialize(bool is_new_device);
   void ButtonClick(int button_index) override;
   void Close(bool by_user) override;
   std::string id() const override;
@@ -50,6 +55,7 @@ class HatsNotificationController : public NotificationDelegate,
   void UpdateLastInteractionTime();
 
   Profile* profile_;
+  base::WeakPtrFactory<HatsNotificationController> weak_pointer_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(HatsNotificationController);
 };
