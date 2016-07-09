fe54e50ba70915860a0c9f5643ecb968c0ae00d5
khmel <khmel@chromium.org>
2016-07-08 17:43:53 -0700

    arc: Provide progress to load TOS content for OptIn.
    This implements progress for loading TOS content for OptIn
    workflow with error/retry support. Also fixes few UI bugs
    in OptIn layout windows.
    
    BUG=b/29419423
    TEST=Manually on device. Emulate error conditions on different
         stages of OptIn workflow. OptIn is correctly recoverable
         in each case.
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2137713002
    Cr-Commit-Position: refs/heads/master@{#404550}

--- chrome/browser/resources/chromeos/arc_support/background.js
+++ chrome/browser/resources/chromeos/arc_support/background.js
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @type {Array<string>}
  */
 var UI_PAGES = ['none',
-                'start',
+                'terms-loading',
+                'terms',
                 'lso-loading',
                 'lso',
                 'arc-loading',
@@ -60,6 +61,12 @@ var termsReloadTimeout = null;
 var currentDeviceId = null;
 
 /**
+ * Indicates that terms were accepted by user.
+ * @type {boolean}
+ */
+var termsAccepted = false;
+
+/**
  * Closes current window in response to request from native code. This does not
  * issue 'cancelAuthCode' message to native code.
  */
@@ -106,9 +113,6 @@ function initialize(data, deviceId) {
         js: { files: ['playstore.js'] },
         run_at: 'document_end'
       }]);
-
-  // Applying localization changes page layout, update terms height.
-  updateTermsHeight();
 }
 
 /**
@@ -172,6 +176,9 @@ function updateTermsHeight() {
   var setTermsHeight = function() {
     var doc = appWindow.contentWindow.document;
     var termsContainer = doc.getElementById('terms-container');
+    // Reset terms-view height in order to stabilize style computation. For
+    // some reason, child webview affects final result.
+    termsView.style.height = '0px';
     var style = window.getComputedStyle(termsContainer, null);
     var height = style.getPropertyValue('height');
     termsView.style.height = height;
@@ -278,10 +285,17 @@ function showPageWithStatus(pageId, status) {
     return;
   }
 
-  if (UI_PAGES[pageId] == 'start') {
+  if (UI_PAGES[pageId] == 'terms-loading') {
+    termsAccepted = false;
     loadInitialTerms();
-  } else if (UI_PAGES[pageId] == 'error' ||
-             UI_PAGES[pageId] == 'error-with-feedback') {
+  } else {
+    // Explicit request to start not from start page. Assume terms are
+    // accepted in this case.
+    termsAccepted = true;
+  }
+
+  if (UI_PAGES[pageId] == 'error' ||
+      UI_PAGES[pageId] == 'error-with-feedback') {
     setErrorMessage(status);
   }
   showPage(UI_PAGES[pageId]);
@@ -298,25 +312,32 @@ chrome.app.runtime.onLaunched.addListener(function() {
   var onAppContentLoad = function() {
     var doc = appWindow.contentWindow.document;
     lsoView = doc.getElementById('arc-support');
-    // Apply absolute dimension to webview tag in order to avoid UI glitch
-    // when embedded content layout is visible for user, even if 100% width and
-    // height are set in css file.
-    // TODO(khmel): Investigate why relative layout is not enough.
-    lsoView.style.width = appWindow.innerBounds.width + 'px';
-    lsoView.style.height = appWindow.innerBounds.height + 'px';
 
     var isApprovalResponse = function(url) {
       var resultUrlPrefix = 'https://accounts.google.com/o/oauth2/approval?';
       return url.substring(0, resultUrlPrefix.length) == resultUrlPrefix;
     };
 
+    var lsoError = false;
     var onLsoViewRequestResponseStarted = function(details) {
       if (isApprovalResponse(details.url)) {
         showPage('arc-loading');
       }
+      lsoError = false;
+    };
+
+    var onLsoViewErrorOccurred = function(details) {
+      setErrorMessage(appWindow.contentWindow.loadTimeData.getString(
+          'serverError'));
+      showPage('error');
+      lsoError = true;
     };
 
     var onLsoViewContentLoad = function() {
+      if (lsoError) {
+        return;
+      }
+
       if (!isApprovalResponse(lsoView.src)) {
         // Show LSO page when its content is ready.
         showPage('lso');
@@ -325,8 +346,8 @@ chrome.app.runtime.onLaunched.addListener(function() {
 
       lsoView.executeScript({code: 'document.title;'}, function(results) {
         var authCodePrefix = 'Success code=';
-        if (results[0].substring(0, authCodePrefix.length) ==
-            authCodePrefix) {
+        if (results && results.length == 1 && typeof results[0] == 'string' &&
+            results[0].substring(0, authCodePrefix.length) == authCodePrefix) {
           var authCode = results[0].substring(authCodePrefix.length);
           sendNativeMessage('setAuthCode', {code: authCode});
         } else {
@@ -344,31 +365,40 @@ chrome.app.runtime.onLaunched.addListener(function() {
 
     lsoView.request.onResponseStarted.addListener(
         onLsoViewRequestResponseStarted, requestFilter);
+    lsoView.request.onErrorOccurred.addListener(
+        onLsoViewErrorOccurred, requestFilter);
     lsoView.addEventListener('contentload', onLsoViewContentLoad);
 
-    termsView = doc.getElementById('terms');
+    termsView = doc.getElementById('terms-view');
 
-    // Handle terms view completed event. Enable button 'Agree' in case terms
-    // were loaded successfully and try to reload its content on error.
-    var termsReloadRetryTimeMs = 1000;  // 1 second
-    function onTermsViewRequestCompleted(details) {
-      if (termsReloadTimeout) {
-        clearTimeout(termsReloadTimeout);
-        termsReloadTimeout = null;
+    var termsError = false;
+    var onTermsViewBeforeRequest = function(details) {
+      showPage('terms-loading');
+      termsError = false;
+    };
+
+    var onTermsViewErrorOccurred = function(details) {
+      termsAccepted = false;
+      setErrorMessage(appWindow.contentWindow.loadTimeData.getString(
+          'serverError'));
+      showPage('error');
+      termsError = true;
+    };
+
+    var onTermsViewContentLoad = function() {
+      if (termsError) {
+        return;
       }
-      if (details.statusCode == 200) {
-        doc.getElementById('button-agree').disabled = false;
-      } else {
-        termsReloadTimeout = setTimeout(loadInitialTerms,
-                                        termsReloadRetryTimeMs);
-        termsReloadRetryTimeMs = termsReloadRetryTimeMs * 2;
-        if (termsReloadRetryTimeMs > 30000) {
-          termsReloadRetryTimeMs = 30000;
-        }
-      }
-    }
-    termsView.request.onCompleted.addListener(onTermsViewRequestCompleted,
-                                              requestFilter);
+      showPage('terms');
+      updateTermsHeight();
+    };
+
+    termsView.request.onBeforeRequest.addListener(onTermsViewBeforeRequest,
+                                                  requestFilter);
+    termsView.request.onErrorOccurred.addListener(onTermsViewErrorOccurred,
+                                                  requestFilter);
+    termsView.addEventListener('contentload', onTermsViewContentLoad);
+
 
     // webview is not allowed to open links in the new window. Hook these events
     // and open links in context of main page.
@@ -378,6 +408,8 @@ chrome.app.runtime.onLaunched.addListener(function() {
     });
 
     var onAgree = function() {
+      termsAccepted = true;
+
       var enableMetrics = doc.getElementById('enable-metrics');
       if (!enableMetrics.hidden) {
         sendNativeMessage('enableMetrics', {
@@ -402,7 +434,11 @@ chrome.app.runtime.onLaunched.addListener(function() {
     };
 
     var onRetry = function() {
-      sendNativeMessage('startLso');
+      if (termsAccepted) {
+        sendNativeMessage('startLso');
+      } else {
+        loadInitialTerms();
+      }
     };
 
     var onSendFeedback = function() {
