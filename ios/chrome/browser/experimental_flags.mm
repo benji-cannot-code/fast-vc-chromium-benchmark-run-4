100bd98a3263705f29b42123157df72bbf5772bf
sklencarova <sklencarova@google.com>
2016-07-11 09:09:46 -0700

    Add experimental flag for the QR Code Reader feature.
    The switch for the experiment in settings will be added in a separate CL.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2116773002
    Cr-Commit-Position: refs/heads/master@{#404666}

--- ios/chrome/browser/experimental_flags.mm
+++ ios/chrome/browser/experimental_flags.mm
@@ -29,6 +29,7 @@ NSString* const kHeuristicsForPasswordGeneration =
     @"HeuristicsForPasswordGeneration";
 NSString* const kEnableReadingList = @"EnableReadingList";
 NSString* const kUpdatePasswordUIEnabled = @"UpdatePasswordUIEnabled";
+NSString* const kEnableQRCodeReader = @"EnableQRCodeReader";
 }  // namespace
 
 namespace experimental_flags {
@@ -141,6 +142,11 @@ bool IsPhysicalWebEnabled() {
 bool IsUpdatePasswordUIEnabled() {
   return [[NSUserDefaults standardUserDefaults]
       boolForKey:kUpdatePasswordUIEnabled];
+}
+
+bool IsQRCodeReaderEnabled() {
+  return [[NSUserDefaults standardUserDefaults]
+      boolForKey:kEnableQRCodeReader];
 }
 
 }  // namespace experimental_flags
