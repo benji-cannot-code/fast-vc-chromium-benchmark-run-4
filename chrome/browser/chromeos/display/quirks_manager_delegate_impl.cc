efe650696980b71c20b0e3ba01a5bb746ad147bb
glevin <glevin@chromium.org>
2016-07-10 18:15:02 -0700

    Remove first check random delay on older devices
    BUG=598841
    TEST=Not easily testable, as this CL removes code that is no longer
    active after a first Quirks server check; we expect nearly all devices
    should have made a server check as of M-54, if they're ever going to.
    
    Review-Url: https://codereview.chromium.org/2006143011
    Cr-Commit-Position: refs/heads/master@{#404587}

--- chrome/browser/chromeos/display/quirks_manager_delegate_impl.cc
+++ chrome/browser/chromeos/display/quirks_manager_delegate_impl.cc
@@ -7,22 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/path_service.h"
 #include "base/sys_info.h"
-#include "base/task_runner_util.h"
-#include "base/threading/thread_restrictions.h"
-#include "chrome/browser/chromeos/login/startup_utils.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/common/chrome_paths.h"
 #include "chromeos/chromeos_paths.h"
-#include "content/public/browser/browser_thread.h"
 #include "google_apis/google_api_keys.h"
 
 namespace {
 
 const char kUserDataDisplayProfilesDirectory[] = "display_profiles";
-
-int GetDaysSinceOobeOnBlockingPool() {
-  return chromeos::StartupUtils::GetTimeSinceOobeFlagFileCreation().InDays();
-}
 
 }  // namespace
 
@@ -59,13 +51,6 @@ bool QuirksManagerDelegateImpl::DevicePolicyEnabled() const {
   chromeos::CrosSettings::Get()->GetBoolean(
       chromeos::kDeviceQuirksDownloadEnabled, &quirks_enabled);
   return quirks_enabled;
-}
-
-void QuirksManagerDelegateImpl::GetDaysSinceOobe(
-    QuirksManager::DaysSinceOobeCallback callback) const {
-  base::PostTaskAndReplyWithResult(
-      content::BrowserThread::GetBlockingPool(), FROM_HERE,
-      base::Bind(&GetDaysSinceOobeOnBlockingPool), callback);
 }
 
 }  // namespace quirks
