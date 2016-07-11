8927ec3b9f38f9a9201f91e74eac87157737fe48
mlliu <mlliu@chromium.org>
2016-07-11 12:15:26 -0700

    cc: Clean up PictureLayer API.
    To create a distinction between the PictureLayer data that is received
    the embedder and values that are internally cached or set. Move the
    embedder and interfaces into a separate struct.
    
    BUG=625284
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review-Url: https://codereview.chromium.org/2131233003
    Cr-Commit-Position: refs/heads/master@{#404705}

--- cc/layers/picture_layer.h
+++ cc/layers/picture_layer.h
@@ -38,7 +38,7 @@ class CC_EXPORT PictureLayer : public Layer {
 
   void RunMicroBenchmark(MicroBenchmark* benchmark) override;
 
-  ContentLayerClient* client() { return client_; }
+  ContentLayerClient* client() { return inputs_.client; }
 
   RecordingSource* GetRecordingSourceForTesting() {
     return recording_source_.get();
@@ -64,7 +64,6 @@ class CC_EXPORT PictureLayer : public Layer {
 
   void DropRecordingSourceContentIfInvalid();
 
-  ContentLayerClient* client_;
   std::unique_ptr<RecordingSource> recording_source_;
   devtools_instrumentation::
       ScopedLayerObjectTracker instrumentation_object_tracker_;
@@ -73,7 +72,14 @@ class CC_EXPORT PictureLayer : public Layer {
 
   int update_source_frame_number_;
   bool is_mask_;
-  bool nearest_neighbor_;
+
+  // Encapsulates all data, callbacks or interfaces received from the embedder.
+  struct Inputs {
+    ContentLayerClient* client = nullptr;
+    bool nearest_neighbor = false;
+  };
+
+  Inputs inputs_;
 
   DISALLOW_COPY_AND_ASSIGN(PictureLayer);
 };
