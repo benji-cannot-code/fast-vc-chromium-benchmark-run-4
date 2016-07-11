012bdc2b3d51ff6b7ff51b1aa76d925025941301
kouhei <kouhei@chromium.org>
2016-07-11 00:37:05 -0700

    Remove unused enum DisallowScriptingContent
    Follow-up to https://chromium.googlesource.com/chromium/src/+/3854a2102cfd8a10f28cf5123f593b87aaac3af0
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2134043002
    Cr-Commit-Position: refs/heads/master@{#404612}

--- third_party/WebKit/Source/core/dom/ParserContentPolicy.h
+++ third_party/WebKit/Source/core/dom/ParserContentPolicy.h
@@ -31,7 +31,6 @@ namespace blink {
 
 enum ParserContentPolicy {
     DisallowScriptingAndPluginContent,
-    DisallowScriptingContent,
     AllowScriptingContent,
     AllowScriptingContentAndDoNotMarkAlreadyStarted,
 };
