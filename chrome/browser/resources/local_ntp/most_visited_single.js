e63bfc0f672d0aec4a5f191d45bf08631630c743
treib <treib@chromium.org>
2016-07-11 06:41:50 -0700

    Remove a bunch of broken/unused/non-useful NTP UMA histograms
    BUG=625154
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2124953004
    Cr-Commit-Position: refs/heads/master@{#404649}

--- chrome/browser/resources/local_ntp/most_visited_single.js
+++ chrome/browser/resources/local_ntp/most_visited_single.js
@@ -24,11 +24,6 @@ var LOG_TYPE = {
   // Indicates a tile was rendered, no matter if it's a thumbnail, a gray tile
   // or an external tile.
   NTP_TILE: 2,
-  // The tile uses a local thumbnail image.
-  NTP_THUMBNAIL_TILE: 3,
-  // There was an error in loading both the thumbnail image and the fallback
-  // (if it was provided), resulting in a gray tile.
-  NTP_THUMBNAIL_ERROR: 6,
   // A NTP Tile has finished loading (successfully or failing).
   NTP_TILE_LOADED: 10,
 };
@@ -471,10 +466,8 @@ var renderTile = function(data) {
       img.addEventListener('error', function(ev) {
         thumb.classList.add('failed-img');
         thumb.removeChild(img);
-        logEvent(LOG_TYPE.NTP_THUMBNAIL_ERROR);
       });
       thumb.appendChild(img);
-      logEvent(LOG_TYPE.NTP_THUMBNAIL_TILE);
     } else {
       thumb.classList.add('failed-img');
     }
@@ -507,7 +500,6 @@ var renderTile = function(data) {
       }
       thumb.classList.add('failed-img');
       thumb.removeChild(img);
-      logEvent(LOG_TYPE.NTP_THUMBNAIL_ERROR);
       countLoad();
     };
 
@@ -533,10 +525,8 @@ var renderTile = function(data) {
     img.addEventListener('error', function(ev) {
       thumb.classList.add('failed-img');
       thumb.removeChild(img);
-      logEvent(LOG_TYPE.NTP_THUMBNAIL_ERROR);
     });
     thumb.appendChild(img);
-    logEvent(LOG_TYPE.NTP_THUMBNAIL_TILE);
 
     if (data.thumbnailUrl) {
       img.src = data.thumbnailUrl;
