cafd103020136778505e162af5061ce1849ca0f4
nektar <nektar@chromium.org>
2016-07-12 15:26:04 -0700

    Revert of DevTools: automatically populate 'More tools' submenu with the drawer views. (patchset #4 id:60001 of https://codereview.chromium.org/2137763002/ )
    Reason for revert:
    Possible compile failure on Linux ChromiumOS GN
    
    Original issue's description:
    > DevTools: automatically populate 'More tools' submenu with the drawer views.
    >
    > NOTRY=true
    > NOPRESUBMIT=true
    >
    > Committed: https://crrev.com/51a628c40b3f8e25e35bb45f94f9417ff8b7fc37
    > Cr-Commit-Position: refs/heads/master@{#404838}
    
    TBR=dgozman@chromium.org,pfeldman@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2142303002
    Cr-Commit-Position: refs/heads/master@{#404845}

--- third_party/WebKit/Source/devtools/front_end/ui/ContextMenu.js
+++ third_party/WebKit/Source/devtools/front_end/ui/ContextMenu.js
@@ -172,14 +172,11 @@ WebInspector.ContextSubMenuItem.prototype = {
     /**
      * @param {string} label
      * @param {boolean=} disabled
-     * @param {string=} subMenuId
      * @return {!WebInspector.ContextSubMenuItem}
      */
-    appendSubMenuItem: function(label, disabled, subMenuId)
+    appendSubMenuItem: function(label, disabled)
     {
         var item = new WebInspector.ContextSubMenuItem(this._contextMenu, label, disabled);
-        if (subMenuId)
-            this._contextMenu._namedSubMenus.set(subMenuId, item);
         this._pushItem(item);
         return item;
     },
@@ -250,7 +247,7 @@ WebInspector.ContextSubMenuItem.prototype = {
         {
             var subMenuId = extension.descriptor()["subMenuId"];
             if (subMenuId) {
-                var subMenuItem = menu.appendSubMenuItem(extension.title(), false, subMenuId);
+                var subMenuItem = menu.appendSubMenuItem(extension.title(WebInspector.platform()));
                 subMenuItem.appendItemsAtLocation(subMenuId);
             } else {
                 menu.appendAction(extension.descriptor()["actionId"]);
@@ -313,8 +310,6 @@ WebInspector.ContextMenu = function(event, useSoftMenu, x, y)
     this._y = y === undefined ? event.y : y;
     this._handlers = {};
     this._id = 0;
-    /** @type {!Map<string, !WebInspector.ContextSubMenuItem>} */
-    this._namedSubMenus = new Map();
 }
 
 WebInspector.ContextMenu.initialize = function()
@@ -487,15 +482,6 @@ WebInspector.ContextMenu.prototype = {
     {
         this._pendingPromises.push(self.runtime.instancesPromise(WebInspector.ContextMenu.Provider, target));
         this._pendingTargets.push(target);
-    },
-
-    /**
-     * @param {string} name
-     * @return {?WebInspector.ContextSubMenuItem}
-     */
-    namedSubMenu: function(name)
-    {
-        return this._namedSubMenus.get(name) || null;
     },
 
     __proto__: WebInspector.ContextSubMenuItem.prototype
