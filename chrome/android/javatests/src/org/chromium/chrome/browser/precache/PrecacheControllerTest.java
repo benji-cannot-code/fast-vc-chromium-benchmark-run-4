67875ce05951ee94ffbfea7944705585c7ccff47
picksi <picksi@chromium.org>
2016-07-12 07:34:27 -0700

    Revert of PrecacheFetcher::CancelPrecaching should run in the main UI thread (patchset #4 id:60001 of https://codereview.chromium.org/2137593002/ )
    Reason for revert:
    Android Test (dbg) has turned red, with failures:
    C  860.211s Main  [  FAILED  ] org.chromium.chrome.browser.precache.PrecacheControllerTest#testDeviceStateChangeCancels
    C  860.211s Main  [  FAILED  ] org.chromium.chrome.browser.precache.PrecacheControllerTest#testTimeoutCancelsPrecaching
    C  860.211s Main  [  FAILED  ] org.chromium.chrome.browser.precache.PrecacheControllerTest#testTimeoutDoesNotCancelIfNotPrecaching (UNKNOWN)
    C  860.211s Main  [  FAILED  ] org.chromium.chrome.browser.precache.PrecacheLauncherTest#testUpdateEnabled_Disabled_ThenEnabled (UNKNOWN)
    C  860.211s Main  [  FAILED  ] org.chromium.chrome.browser.precache.PrecacheLauncherTest#testUpdateEnabled_SyncNotReady_ThenEnabled (UNKNOWN)
    
     Link to bot:
    
    https://uberchromegw.corp.google.com/i/chromium.linux/builders/Android%20Tests%20(dbg)
    
    Original issue's description:
    > PrecacheFetcher::CancelPrecaching should run in the main UI thread
    >
    > Reducing the possibilities of crash in CancelPrecaching.
    >
    > BUG=619111
    >
    > Committed: https://crrev.com/28e7a0cc33dafc8719c4b9c740d79a47ff90f067
    > Cr-Commit-Position: refs/heads/master@{#404792}
    
    TBR=sclittle@chromium.org,rajendrant@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=619111
    
    Review-Url: https://codereview.chromium.org/2147543002
    Cr-Commit-Position: refs/heads/master@{#404810}

--- chrome/android/javatests/src/org/chromium/chrome/browser/precache/PrecacheControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/precache/PrecacheControllerTest.java
@@ -9,7 +9,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences.Editor;
 import android.test.InstrumentationTestCase;
-import android.test.UiThreadTest;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import com.google.android.gms.gcm.GcmNetworkManager;
@@ -196,7 +195,6 @@ public class PrecacheControllerTest extends InstrumentationTestCase {
     }
 
     @SmallTest
-    @UiThreadTest
     @Feature({"Precache"})
     public void testDeviceStateChangeCancels() {
         verifyBeginPrecaching();
@@ -236,7 +234,6 @@ public class PrecacheControllerTest extends InstrumentationTestCase {
     }
 
     @SmallTest
-    @UiThreadTest
     @Feature({"Precache"})
     public void testTimeoutCancelsPrecaching() {
         verifyBeginPrecaching();
