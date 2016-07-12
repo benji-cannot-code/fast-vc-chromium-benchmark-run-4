a9c372c1fd8503e0f05ffea0053b866d8fae1e3b
alexandermont <alexandermont@chromium.org>
2016-07-11 17:22:10 -0700

    Add clock sync latency metric to BattOr test cases.
    BUG=catapult:#2459
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:android_s5_perf_cq;tryserver.chromium.perf:mac_retina_perf_cq
    
    Review-Url: https://codereview.chromium.org/2123393003
    Cr-Commit-Position: refs/heads/master@{#404749}

--- tools/perf/benchmarks/battor.py
+++ tools/perf/benchmarks/battor.py
@@ -18,7 +18,7 @@ class _BattOrBenchmark(perf_benchmark.PerfBenchmark):
     options.config.enable_battor_trace = True
     options.config.enable_chrome_trace = True
     options.config.chrome_trace_config.SetDefaultOverheadFilter()
-    options.SetTimelineBasedMetrics(['powerMetric'])
+    options.SetTimelineBasedMetrics(['powerMetric', 'clockSyncLatencyMetric'])
     return options
 
   @classmethod
@@ -106,7 +106,7 @@ class BattOrPowerCasesNoChromeTrace(_BattOrBenchmark):
     options.config.enable_battor_trace = True
     options.config.enable_chrome_trace = False
     options.config.chrome_trace_config.SetDefaultOverheadFilter()
-    options.SetTimelineBasedMetrics(['powerMetric'])
+    options.SetTimelineBasedMetrics(['powerMetric', 'clockSyncLatencyMetric'])
     return options
 
   @classmethod
