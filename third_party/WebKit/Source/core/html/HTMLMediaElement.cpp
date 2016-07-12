46d998287d412426aed2e51e45b2cbf52a1af719
dbeam <dbeam@chromium.org>
2016-07-12 14:30:56 -0700

    Revert "Measure whether muted videos that started playing with play() become visible at some point"
    This reverts commit 9d961d0a083fe4ebd8c200c31b87d75e4727958b.
    
    Seems to be leaking, broke WebKit Leak bots here:
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Leak/builds/20963
    
    BUG=627539,622720
    
    TBR=zqzhang@chromium.org
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    
    Review-Url: https://codereview.chromium.org/2141093003
    Cr-Commit-Position: refs/heads/master@{#404836}

--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/Settings.h"
 #include "core/frame/UseCounter.h"
 #include "core/frame/csp/ContentSecurityPolicy.h"
-#include "core/html/AutoplayUmaHelper.h"
 #include "core/html/HTMLMediaSource.h"
 #include "core/html/HTMLSourceElement.h"
 #include "core/html/HTMLTrackElement.h"
@@ -282,6 +281,16 @@ String preloadTypeToString(WebMediaPlayer::Preload preloadType)
     return String();
 }
 
+// These values are used for histograms. Do not reorder.
+enum AutoplaySource {
+    // Autoplay comes from HTMLMediaElement `autoplay` attribute.
+    AutoplaySourceAttribute = 0,
+    // Autoplay comes from `play()` method.
+    AutoplaySourceMethod = 1,
+    // This enum value must be last.
+    NumberOfAutoplaySources = 2,
+};
+
 } // anonymous namespace
 
 class HTMLMediaElement::AutoplayHelperClientImpl :
@@ -439,7 +448,6 @@ HTMLMediaElement::HTMLMediaElement(const QualifiedName& tagName, Document& docum
     , m_audioSourceNode(nullptr)
     , m_autoplayHelperClient(AutoplayHelperClientImpl::create(this))
     , m_autoplayHelper(AutoplayExperimentHelper::create(m_autoplayHelperClient.get()))
-    , m_autoplayUmaHelper(AutoplayUmaHelper::create(this))
     , m_remotePlaybackClient(nullptr)
     , m_autoplayVisibilityObserver(nullptr)
 {
@@ -483,8 +491,6 @@ void HTMLMediaElement::dispose()
     // See Document::isDelayingLoadEvent().
     // Also see http://crbug.com/275223 for more details.
     clearMediaPlayerAndAudioSourceProviderClientWithoutLocking();
-
-    m_autoplayUmaHelper->onElementDestroyed();
 }
 
 void HTMLMediaElement::didMoveToNewDocument(Document& oldDocument)
@@ -1621,7 +1627,7 @@ void HTMLMediaElement::setReadyState(ReadyState state)
 
         // Check for autoplay, and record metrics about it if needed.
         if (shouldAutoplay(RecordMetricsBehavior::DoRecord)) {
-            m_autoplayUmaHelper->onAutoplayInitiated(AutoplaySource::Attribute);
+            recordAutoplaySourceMetric(AutoplaySourceAttribute);
 
             // If the autoplay experiment says that it's okay to play now,
             // then don't require a user gesture.
@@ -2076,7 +2082,7 @@ Nullable<ExceptionCode> HTMLMediaElement::play()
     m_autoplayHelper->playMethodCalled();
 
     if (!UserGestureIndicator::processingUserGesture()) {
-        m_autoplayUmaHelper->onAutoplayInitiated(AutoplaySource::Method);
+        recordAutoplaySourceMetric(AutoplaySourceMethod);
         if (isGestureNeededForPlayback()) {
             // If playback is deferred, then don't start playback but don't
             // fail yet either.
@@ -2301,9 +2307,9 @@ void HTMLMediaElement::setMuted(bool muted)
     if (wasAutoplayingMuted) {
         if (isGestureNeededForPlayback()) {
             pause();
-            m_autoplayUmaHelper->recordAutoplayUnmuteStatus(AutoplayUnmuteActionStatus::Failure);
+            recordAutoplayUnmuteStatus(AutoplayUnmuteActionFailure);
         } else {
-            m_autoplayUmaHelper->recordAutoplayUnmuteStatus(AutoplayUnmuteActionStatus::Success);
+            recordAutoplayUnmuteStatus(AutoplayUnmuteActionSuccess);
         }
     }
 }
@@ -3661,7 +3667,6 @@ DEFINE_TRACE(HTMLMediaElement)
     visitor->trace(m_audioSourceProvider);
     visitor->trace(m_autoplayHelperClient);
     visitor->trace(m_autoplayHelper);
-    visitor->trace(m_autoplayUmaHelper);
     visitor->trace(m_srcObject);
     visitor->trace(m_autoplayVisibilityObserver);
     visitor->template registerWeakMembers<HTMLMediaElement, &HTMLMediaElement::clearWeakMembers>(this);
@@ -3880,6 +3885,28 @@ EnumerationHistogram& HTMLMediaElement::showControlsHistogram() const
 
     DEFINE_STATIC_LOCAL(EnumerationHistogram, histogram, ("Media.Controls.Show.Audio", MediaControlsShowMax));
     return histogram;
+}
+
+void HTMLMediaElement::recordAutoplaySourceMetric(int source)
+{
+    DEFINE_STATIC_LOCAL(EnumerationHistogram, videoHistogram, ("Media.Video.Autoplay", NumberOfAutoplaySources));
+    DEFINE_STATIC_LOCAL(EnumerationHistogram, mutedVideoHistogram, ("Media.Video.Autoplay.Muted", NumberOfAutoplaySources));
+    DEFINE_STATIC_LOCAL(EnumerationHistogram, audioHistogram, ("Media.Audio.Autoplay", NumberOfAutoplaySources));
+
+    if (isHTMLVideoElement()) {
+        videoHistogram.count(source);
+        if (muted())
+            mutedVideoHistogram.count(source);
+    } else {
+        audioHistogram.count(source);
+    }
+}
+
+void HTMLMediaElement::recordAutoplayUnmuteStatus(AutoplayUnmuteActionStatus status)
+{
+    DEFINE_STATIC_LOCAL(EnumerationHistogram, autoplayUnmuteHistogram, ("Media.Video.Autoplay.Muted.UnmuteAction", AutoplayUnmuteActionMax));
+
+    autoplayUnmuteHistogram.count(status);
 }
 
 void HTMLMediaElement::onVisibilityChangedForAutoplay(bool isVisible)
--- third_party/WebKit/Source/core/html/HTMLMediaElement.h
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.h
@@ -47,7 +47,6 @@ namespace blink {
 
 class AudioSourceProviderClient;
 class AudioTrackList;
-class AutoplayUmaHelper;
 class ContentType;
 class CueTimeline;
 class ElementVisibilityObserver;
@@ -300,6 +299,13 @@ protected:
     void recordAutoplayMetric(AutoplayMetrics);
 
 private:
+    // These values are used for histograms. Do not reorder.
+    enum AutoplayUnmuteActionStatus {
+        AutoplayUnmuteActionFailure = 0,
+        AutoplayUnmuteActionSuccess = 1,
+        AutoplayUnmuteActionMax
+    };
+
     void resetMediaPlayerAndMediaSource();
 
     bool alwaysCreateUserAgentShadowRoot() const final { return true; }
@@ -494,6 +500,9 @@ private:
 
     EnumerationHistogram& showControlsHistogram() const;
 
+    void recordAutoplaySourceMetric(int source);
+    void recordAutoplayUnmuteStatus(AutoplayUnmuteActionStatus);
+
     void onVisibilityChangedForAutoplay(bool isVisible);
 
     UnthrottledTimer<HTMLMediaElement> m_loadTimer;
@@ -670,7 +679,6 @@ private:
 
     Member<AutoplayExperimentHelper::Client> m_autoplayHelperClient;
     Member<AutoplayExperimentHelper> m_autoplayHelper;
-    Member<AutoplayUmaHelper> m_autoplayUmaHelper;
 
     WebRemotePlaybackClient* m_remotePlaybackClient;
 
