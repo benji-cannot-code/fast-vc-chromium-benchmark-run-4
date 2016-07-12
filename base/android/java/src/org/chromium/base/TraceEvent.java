d12f41ec7dd4530e69e972b31b85bf42e3675df7
wychen <wychen@chromium.org>
2016-07-11 17:35:00 -0700

    Revert of Android: Add support for TraceEvent before the native library is loaded. (patchset #12 id:220001 of https://codereview.chromium.org/1486053003/ )
    Reason for revert:
    https://crbug.com/627142
    
    Original issue's description:
    > Android: Add support for TraceEvent before the native library is loaded.
    >
    > Currently TraceEvents recorded before the native library load are
    > silently discarded. This CL adds support for such "early" events.
    >
    > The events are buffered in Java, and then sent to the native side once
    > it is available. This support is intentionally simple now. The aim is to
    > ease performance investigation of early startup, and to ease tracking of
    > early startup performance regression, since timings are then accessible
    > to TBM tests.
    >
    > Also use the newly-introduced events in a couple places, and fix a bug
    > in the TraceEvent we use to record the launcher activity lifetime (when
    > an Activity calls finish from onCreate(), onPause() is not called).
    >
    > BUG=564041
    >
    > Committed: https://crrev.com/fb0784294541c53f12e283124672750ab2e291f1
    > Cr-Commit-Position: refs/heads/master@{#404635}
    
    TBR=pasko@chromium.org,yfriedman@chromium.org,jam@chromium.org,lizeb@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=564041
    
    Review-Url: https://codereview.chromium.org/2145443002
    Cr-Commit-Position: refs/heads/master@{#404752}

--- base/android/java/src/org/chromium/base/TraceEvent.java
+++ base/android/java/src/org/chromium/base/TraceEvent.java
@@ -17,8 +17,8 @@ import org.chromium.base.annotations.JNINamespace;
  * Java mirror of Chrome trace event API. See base/trace_event/trace_event.h. Unlike the native
  * version, Java does not have stack objects, so a TRACE_EVENT() which does both TRACE_EVENT_BEGIN()
  * and TRACE_EVENT_END() in ctor/dtor is not possible.
- * It is OK to use tracing before the native library has loaded, in a slightly restricted fashion.
- * @see EarlyTraceEvent for details.
+ * It is OK to use tracing before the native library has loaded, but such traces will
+ * be ignored. (Perhaps we could devise to buffer them up in future?).
  */
 @JNINamespace("base::android")
 public class TraceEvent {
@@ -180,21 +180,11 @@ public class TraceEvent {
      */
     @CalledByNative
     public static void setEnabled(boolean enabled) {
-        if (enabled) EarlyTraceEvent.disable();
         sEnabled = enabled;
         // Android M+ systrace logs this on its own. Only log it if not writing to Android systrace.
         if (sATraceEnabled) return;
         ThreadUtils.getUiThreadLooper().setMessageLogging(
                 enabled ? LooperMonitorHolder.sInstance : null);
-    }
-
-    /**
-     * May enable early tracing depending on the environment.
-     *
-     * Must be called after the command-line has been read.
-     */
-    public static void maybeEnableEarlyTracing() {
-        EarlyTraceEvent.maybeEnable();
     }
 
     /**
@@ -265,7 +255,7 @@ public class TraceEvent {
      * @param name The name of the event.
      */
     public static void begin(String name) {
-        begin(name, null);
+        if (sEnabled) nativeBegin(name, null);
     }
 
     /**
@@ -274,7 +264,6 @@ public class TraceEvent {
      * @param arg  The arguments of the event.
      */
     public static void begin(String name, String arg) {
-        EarlyTraceEvent.begin(name);
         if (sEnabled) nativeBegin(name, arg);
     }
 
@@ -283,7 +272,7 @@ public class TraceEvent {
      * @param name The name of the event.
      */
     public static void end(String name) {
-        end(name, null);
+        if (sEnabled) nativeEnd(name, null);
     }
 
     /**
@@ -292,7 +281,6 @@ public class TraceEvent {
      * @param arg  The arguments of the event.
      */
     public static void end(String name, String arg) {
-        EarlyTraceEvent.end(name);
         if (sEnabled) nativeEnd(name, arg);
     }
 
