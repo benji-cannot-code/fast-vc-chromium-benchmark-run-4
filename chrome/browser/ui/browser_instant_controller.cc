3ac6d58087862b5c21018ab21aa26775d69c66a3
sfiera <sfiera@chromium.org>
2016-07-13 01:44:00 -0700

    Split NTP_TILE_LOADED event.
    Multi-iframe NTP continues to use it, but no longer logs statistics for
    load time and number of tiles. This happens only for external NTPs,
    where the load time and number of tiles are not fully under our control
    anyway.
    
    Single-iframe NTP now uses NTP_ALL_TILES_LOADED. When this is logged, we
    emit statistics, ensuring that we log a load time and number of tiles
    exactly once per NTP (well, unless the user closes the page or navigates
    away without the page having finished loading).
    
    BUG=625990
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2141933002
    Cr-Commit-Position: refs/heads/master@{#405087}

--- chrome/browser/ui/browser_instant_controller.cc
+++ chrome/browser/ui/browser_instant_controller.cc
@@ -155,8 +155,6 @@ void BrowserInstantController::ActiveTabChanged() {
 }
 
 void BrowserInstantController::TabDeactivated(content::WebContents* contents) {
-  instant_.TabDeactivated(contents);
-
   InstantSearchPrerenderer* prerenderer =
       GetInstantSearchPrerenderer(profile());
   if (prerenderer)
