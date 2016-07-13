76b55315db11ae939457bdfdd06d035925f875e9
wangxianzhu <wangxianzhu@chromium.org>
2016-07-13 15:57:50 -0700

    Early out for inactive documents in FrameView lifecycle updating functions
    We may trigger lifecycle update while some documents in the frame tree
    are inactive. This looks valid and the functions should early out.
    
    BUG=626675
    
    Review-Url: https://codereview.chromium.org/2140983003
    Cr-Commit-Position: refs/heads/master@{#405334}

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -2520,6 +2520,9 @@ void FrameView::updateLifecyclePhasesInternal(DocumentLifecycle::LifecycleState 
         || targetState == DocumentLifecycle::PrePaintClean
         || targetState == DocumentLifecycle::PaintClean);
 
+    if (!m_frame->document()->isActive())
+        return;
+
     TemporaryChange<DocumentLifecycle::LifecycleState> targetStateScope(m_currentUpdateLifecyclePhasesTargetState, targetState);
 
     if (shouldThrottleRendering()) {
@@ -2699,7 +2702,7 @@ void FrameView::updateStyleAndLayoutIfNeededRecursive()
 
 void FrameView::updateStyleAndLayoutIfNeededRecursiveInternal()
 {
-    if (shouldThrottleRendering())
+    if (shouldThrottleRendering() || !m_frame->document()->isActive())
         return;
 
     ScopedFrameBlamer frameBlamer(m_frame);
