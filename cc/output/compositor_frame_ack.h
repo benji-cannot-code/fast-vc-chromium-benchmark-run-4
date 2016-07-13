e56667a3b13ceba07675b4901ec6b84b931da077
fsamuel <fsamuel@chromium.org>
2016-07-12 21:23:36 -0700

    cc::CompositorFrameAck gl_frame_data is dead code
    This CL deletes some dead code.
    
    BUG=none
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review-Url: https://codereview.chromium.org/2134333002
    Cr-Commit-Position: refs/heads/master@{#405017}

--- cc/output/compositor_frame_ack.h
+++ cc/output/compositor_frame_ack.h
@@ -21,7 +21,6 @@ class CC_EXPORT CompositorFrameAck {
   ~CompositorFrameAck();
 
   ReturnedResourceArray resources;
-  std::unique_ptr<GLFrameData> gl_frame_data;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(CompositorFrameAck);
