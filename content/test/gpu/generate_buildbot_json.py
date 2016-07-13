c0c120d73034f61f11db3e1a1e549c0e2a32c619
kbr <kbr@chromium.org>
2016-07-13 12:58:22 -0700

    Run WebGL 1.0 tests as part of the WebGL 2.0 conformance suite.
    Now that the shards are more evenly distributed, this is feasible.
    Increase the number of shards to handle the additional ~15 minutes of
    run time.
    
    BUG=586033
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2133673003
    Cr-Commit-Position: refs/heads/master@{#405246}

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -927,7 +927,6 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
     'target_name': 'webgl_conformance',
     'args': [
       '--webgl-conformance-version=2.0.0',
-      '--webgl2-only=true',
       # The current working directory when run via isolate is
       # out/Debug or out/Release. Reference this file relatively to
       # it.
@@ -935,9 +934,9 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
       '../../content/test/data/gpu/webgl2_conformance_tests_output.json',
     ],
     'swarming': {
-      # These tests currently take about an hour to run. Split them
-      # into roughly 5-minute shards.
-      'shards': 12,
+      # These tests currently take about an hour and fifteen minutes
+      # to run. Split them into roughly 5-minute shards.
+      'shards': 15,
     },
   },
   'webgl2_conformance_angle_tests': {
@@ -967,7 +966,6 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
     ],
     'args': [
       '--webgl-conformance-version=2.0.0',
-      '--webgl2-only=true',
       # The current working directory when run via isolate is
       # out/Debug or out/Release. Reference this file relatively to
       # it.
@@ -975,9 +973,9 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
       '../../content/test/data/gpu/webgl2_conformance_tests_output.json',
     ],
     'swarming': {
-      # These tests currently take about an hour to run. Split them
-      # into roughly 5-minute shards.
-      'shards': 12,
+      # These tests currently take about an hour and fifteen minutes
+      # to run. Split them into roughly 5-minute shards.
+      'shards': 15,
     },
   },
 }
