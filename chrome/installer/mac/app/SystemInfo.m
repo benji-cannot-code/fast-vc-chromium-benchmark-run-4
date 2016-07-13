743beb02e71ab33ac162452c8c16ac36f94115ae
zengster <zengster@google.com>
2016-07-13 07:18:00 -0700

    Initial commit for Chrome metainstaller on Mac.
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2094583004
    Cr-Commit-Position: refs/heads/master@{#405140}

---
+++ chrome/installer/mac/app/SystemInfo.m
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <mach-o/arch.h>
+
+#import "SystemInfo.h"
+
+@implementation SystemInfo
+
++ (NSString*)getArch {
+  const NXArchInfo* arch = NXGetLocalArchInfo();
+  NSString* archName = [NSString stringWithUTF8String:arch->name];
+  return archName;
+}
+
++ (NSString*)getOSVersion {
+  NSDictionary* systemVersion =
+      [NSDictionary dictionaryWithContentsOfFile:
+                        @"/System/Library/CoreServices/SystemVersion.plist"];
+  NSString* versionNumber = [systemVersion objectForKey:@"ProductVersion"];
+
+  return versionNumber;
+}
+
+@end
