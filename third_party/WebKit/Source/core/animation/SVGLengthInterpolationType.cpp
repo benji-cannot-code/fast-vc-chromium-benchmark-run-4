e9119a568e6ddca0def2a43fc1b2f252a0900e9c
shanmuga.m <shanmuga.m@samsung.com>
2016-07-12 22:07:29 -0700

    Support viewport units for SVG Length animations
    Add support of viewport units such as vm,vw, vmin, vmax for animations on SVG Length.
    Chrome already supporting all viewport units for SVGLength.
    https://bugs.chromium.org/p/chromium/issues/detail?id=368598
    In that case, supporting those units for SVG animation will make sense.
    
    BUG=626297
    
    Review-Url: https://codereview.chromium.org/2130753003
    Cr-Commit-Position: refs/heads/master@{#405038}

--- third_party/WebKit/Source/core/animation/SVGLengthInterpolationType.cpp
+++ third_party/WebKit/Source/core/animation/SVGLengthInterpolationType.cpp
@@ -24,6 +24,10 @@ enum LengthInterpolatedUnit {
     LengthInterpolatedEXS,
     LengthInterpolatedREMS,
     LengthInterpolatedCHS,
+    LengthInterpolatedViewportWidth,
+    LengthInterpolatedViewportHeight,
+    LengthInterpolatedViewportMin,
+    LengthInterpolatedViewportMax,
 };
 
 static const CSSPrimitiveValue::UnitType unitTypes[] = {
@@ -32,7 +36,11 @@ static const CSSPrimitiveValue::UnitType unitTypes[] = {
     CSSPrimitiveValue::UnitType::Ems,
     CSSPrimitiveValue::UnitType::Exs,
     CSSPrimitiveValue::UnitType::Rems,
-    CSSPrimitiveValue::UnitType::Chs
+    CSSPrimitiveValue::UnitType::Chs,
+    CSSPrimitiveValue::UnitType::ViewportWidth,
+    CSSPrimitiveValue::UnitType::ViewportHeight,
+    CSSPrimitiveValue::UnitType::ViewportMin,
+    CSSPrimitiveValue::UnitType::ViewportMax,
 };
 
 const size_t numLengthInterpolatedUnits = WTF_ARRAY_LENGTH(unitTypes);
@@ -72,6 +80,14 @@ LengthInterpolatedUnit convertToInterpolatedUnit(CSSPrimitiveValue::UnitType uni
         return LengthInterpolatedREMS;
     case CSSPrimitiveValue::UnitType::Chs:
         return LengthInterpolatedCHS;
+    case CSSPrimitiveValue::UnitType::ViewportWidth:
+        return LengthInterpolatedViewportWidth;
+    case CSSPrimitiveValue::UnitType::ViewportHeight:
+        return LengthInterpolatedViewportHeight;
+    case CSSPrimitiveValue::UnitType::ViewportMin:
+        return LengthInterpolatedViewportMin;
+    case CSSPrimitiveValue::UnitType::ViewportMax:
+        return LengthInterpolatedViewportMax;
     }
 }
 
