e81d692ed84b4307826d28b2d94245464d8481c1
stevenjb <stevenjb@chromium.org>
2016-07-12 18:42:29 -0700

    MD Settings: Display: Add mirroring and other fixes
    This adds proper mirroring support and includes some styling fixes.
    
    BUG=547080
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2128773004
    Cr-Commit-Position: refs/heads/master@{#404883}

--- chrome/browser/resources/settings/device_page/layout_behavior.js
+++ chrome/browser/resources/settings/device_page/layout_behavior.js
@@ -16,6 +16,12 @@ var LayoutBehavior = {
      * @type {!Array<!chrome.system.display.DisplayLayout>}
      */
     layouts: Array,
+
+    /**
+     * Whether or not mirroring is enabled.
+     * @type {boolean}
+     */
+    mirroring: false,
   },
 
   /** @private {!Map<string, chrome.system.display.Bounds>} */
@@ -49,6 +55,8 @@ var LayoutBehavior = {
   initializeDisplayLayout: function(displays, layouts) {
     this.dragLayoutId = '';
     this.dragParentId_ = '';
+
+    this.mirroring = displays.length > 0 && !!displays[0].mirroringSourceId;
 
     this.displayBoundsMap_.clear();
     for (let display of displays)
@@ -183,11 +191,12 @@ var LayoutBehavior = {
 
   /**
    * @param {string} displayId
+   * @param {boolean=} opt_notest Set to true if bounds may not be set.
    * @return {!chrome.system.display.Bounds} bounds
    */
-  getCalculatedDisplayBounds: function(displayId) {
+  getCalculatedDisplayBounds: function(displayId, opt_notest) {
     var bounds = this.calculatedBoundsMap_.get(displayId);
-    assert(bounds);
+    assert(opt_notest || bounds);
     return bounds;
   },
 
@@ -309,7 +318,7 @@ var LayoutBehavior = {
   calculateBounds_: function(id, width, height) {
     var left, top;
     var layout = this.displayLayoutMap_.get(id);
-    if (!layout || !layout.parentId) {
+    if (this.mirroring || !layout || !layout.parentId) {
       left = -width / 2;
       top = -height / 2;
     } else {
