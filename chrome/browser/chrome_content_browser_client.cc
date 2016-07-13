42183244c614cd1edfbb4b0926838ac3364fc835
csharrison <csharrison@chromium.org>
2016-07-12 22:03:39 -0700

    Add an experiment group via Settings to tokenize synchronously
    This patch adds an experiment group that tokenizes synchronously
    on the main thread in response to a recieve data IPC.
    
    If this regresses performance, we can augment the CL slightly
    to only synchronously tokenize when the main parser has no
    work.
    
    BUG=627161
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2139703003
    Cr-Commit-Position: refs/heads/master@{#405035}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1386,7 +1386,10 @@ void MaybeAppendBlinkSettingsSwitchForFieldTrial(
     "LowPriorityIFrames",
 
     // Keys: disallowFetchForDocWrittenScriptsInMainFrame
-    "DisallowFetchForDocWrittenScriptsInMainFrame"
+    "DisallowFetchForDocWrittenScriptsInMainFrame",
+
+    // Keys: parseHTMLOnMainThreadSyncTokenize
+    "ParseHTMLOnMainThread",
   };
 
   std::vector<std::string> blink_settings;
