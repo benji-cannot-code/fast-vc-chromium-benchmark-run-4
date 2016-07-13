f59461f734878e665d3356ad5f24aafe5401947a
treib <treib@chromium.org>
2016-07-13 07:28:55 -0700

    Add NewTabPage.MostVisited and .SuggestionsImpression on Android
    The same histograms already exist on desktop, see ntp_user_data_logger.cc
    
    BUG=625153
    
    Review-Url: https://codereview.chromium.org/2126263002
    Cr-Commit-Position: refs/heads/master@{#405142}

--- components/ntp_tiles/most_visited_sites.h
+++ components/ntp_tiles/most_visited_sites.h
@@ -230,9 +230,6 @@ class MostVisitedSites : public history::TopSitesObserver,
 
   void OnPopularSitesAvailable(bool success);
 
-  // Records thumbnail-related UMA histogram metrics.
-  void RecordThumbnailUMAMetrics();
-
   // Records UMA histogram metrics related to the number of impressions.
   void RecordImpressionUMAMetrics();
 
