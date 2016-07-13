6a1723c01a57c0a6fdaf28f3e9e5449d25a16815
forshaw <forshaw@chromium.org>
2016-07-13 09:07:22 -0700

    Made setting lowbox token a warning. This patch makes setting of the lowbox toke a warning rather than a hard failure. Some configurations cause the setting of the token to fail, this is most likely issues with third party software. Worst case this will mean that the lowbox token is not set, but we'll continue with the original restricted token sandbox.
    BUG=501975
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win10_chromium_x64_rel_ng
    
    Review-Url: https://codereview.chromium.org/2130753002
    Cr-Commit-Position: refs/heads/master@{#405173}

--- sandbox/win/tests/common/controller.cc
+++ sandbox/win/tests/common/controller.cc
@@ -223,6 +223,8 @@ int TestRunner::InternalRunTest(const wchar_t* command) {
 
   // Launch the sandboxed process.
   ResultCode result = SBOX_ALL_OK;
+  ResultCode warning_result = SBOX_ALL_OK;
+  DWORD last_error = ERROR_SUCCESS;
   PROCESS_INFORMATION target = {0};
 
   base::string16 arguments(L"\"");
@@ -239,7 +241,7 @@ int TestRunner::InternalRunTest(const wchar_t* command) {
     }
   } else {
     result = broker_->SpawnTarget(prog_name, arguments.c_str(), policy_,
-                                  &target);
+                                  &warning_result, &last_error, &target);
   }
 
   if (SBOX_ALL_OK != result)
