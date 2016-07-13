240be6b8e59b59500406d7bca278cb5caf476e71
gogerald <gogerald@chromium.org>
2016-07-12 19:22:59 -0700

    Add Signin_Impression_FromPasswordBubble user action for Desktops
    BUG=615825
    
    Review-Url: https://codereview.chromium.org/2138203002
    Cr-Commit-Position: refs/heads/master@{#404907}

--- chrome/browser/ui/cocoa/passwords/signin_promo_view_controller.mm
+++ chrome/browser/ui/cocoa/passwords/signin_promo_view_controller.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/hover_close_button.h"
 #include "chrome/browser/ui/cocoa/passwords/passwords_bubble_utils.h"
 #include "chrome/browser/ui/passwords/manage_passwords_bubble_model.h"
+#include "content/public/browser/user_metrics.h"
 #include "grit/generated_resources.h"
 #import "ui/base/cocoa/controls/hyperlink_text_view.h"
 #include "ui/base/l10n/l10n_util_mac.h"
@@ -100,6 +101,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [_closeButton setFrameOrigin:closeButtonOrigin];
   [view setFrame:NSMakeRect(0, 0, kDesiredBubbleWidth, height)];
   [self setView:view];
+  content::RecordAction(
+      base::UserMetricsAction("Signin_Impression_FromPasswordBubble"));
 }
 
 - (void)onSignInClicked:(id)sender {
--- chrome/browser/ui/views/passwords/manage_passwords_bubble_view.cc
+++ chrome/browser/ui/views/passwords/manage_passwords_bubble_view.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/passwords/manage_password_items_view.h"
 #include "chrome/browser/ui/views/passwords/manage_passwords_icon_views.h"
 #include "chrome/grit/generated_resources.h"
+#include "content/public/browser/user_metrics.h"
 #include "grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/material_design/material_design_controller.h"
@@ -594,6 +595,8 @@ ManagePasswordsBubbleView::SignInPromoView::SignInPromoView(
   layout->AddView(no_button_);
 
   parent_->set_initially_focused_view(signin_button_);
+  content::RecordAction(
+      base::UserMetricsAction("Signin_Impression_FromPasswordBubble"));
 }
 
 void ManagePasswordsBubbleView::SignInPromoView::ButtonPressed(
