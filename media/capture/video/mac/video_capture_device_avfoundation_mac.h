c84ff593135207955b312faaf23f8655a02d55a2
mcasas <mcasas@chromium.org>
2016-07-13 14:34:25 -0700

    Revert of RELAND: ImageCapture: Implement takePhoto() for Mac AVFoundation (patchset #2 id:20001 of https://codereview.chromium.org/2146973002/ )
    Reason for revert:
    It caused timeout in
    WebRtcWebcamBrowserTests/WebRtcWebcamBrowserTest.MANUAL_TestAcquiringAndReacquiringWebcam/0
    
    see
    https://build.chromium.org/p/chromium.webrtc/builders/Mac%20Tester/builds/56929
    
    Original issue's description:
    > RELAND: ImageCapture: Implement takePhoto() for Mac AVFoundation
    >
    > Got reverted due to breaking
    > WebRtcWebcamBrowserTests/WebRtcWebcamBrowserTest.MANUAL_TestAcquiringAndReacquiringWebcam/0
    > in some WebRtc Mac bot, possibly because it has no real WebCam
    > or it has one without  AVCaptureStillImageOutputClass support.
    >
    > This CL adds a guard  [captureSession_ canAddOutput:...]
    > before [captureSession_ addOutput:...]
    >
    > Original description ----------------------------------------------
    > ImageCapture: Implement takePhoto() for Mac AVFoundation
    >
    > By pulling the necessary symbols out of AVFoundation
    > and using them; most notably (Cr)AVCaptureStillImageOutput.
    >
    > TakePhoto is already implemented for Android (both APIs).
    >
    > Also adding VideoCaptureDeviceTest::MAYBE_TakePhoto
    > test case, enabled only for Mac.
    >
    > BUG=518807
    > TEST=Run build with flag --enable-blink-features=ImageCapture,
    > navigate to [1] and push buttons
    > - Open Camera ...
    > - Create ImageCapturer
    > - takePhoto()  (N times!) --> profit
    >
    > [1] https://rawgit.com/Miguelao/demos/master/imagecapture.html
    >
    > TBR=rsesek@chromium.org since the change is trivial.
    >
    > Committed: https://crrev.com/d4179235b928634aeadb0c3379d385caf0ad3a07
    > Cr-Commit-Position: refs/heads/master@{#405243}
    
    TBR=
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=518807
    
    Review-Url: https://codereview.chromium.org/2151443003
    Cr-Commit-Position: refs/heads/master@{#405294}

--- media/capture/video/mac/video_capture_device_avfoundation_mac.h
+++ media/capture/video/mac/video_capture_device_avfoundation_mac.h
@@ -22,9 +22,8 @@ class VideoCaptureDeviceMac;
 @class CrAVCaptureDevice;
 @class CrAVCaptureSession;
 @class CrAVCaptureVideoDataOutput;
-@class CrAVCaptureStillImageOutput;
 
-// Class used by VideoCaptureDeviceMac (VCDM) for video and image capture using
+// Class used by VideoCaptureDeviceMac (VCDM) for video capture using
 // AVFoundation API. This class lives inside the thread created by its owner
 // VCDM.
 //
@@ -64,7 +63,7 @@ class VideoCaptureDeviceMac;
   int frameHeight_;
   float frameRate_;
 
-  base::Lock lock_;  // Protects concurrent setting and using |frameReceiver_|.
+  base::Lock lock_;  // Protects concurrent setting and using of frameReceiver_.
   media::VideoCaptureDeviceMac* frameReceiver_;  // weak.
 
   base::scoped_nsobject<CrAVCaptureSession> captureSession_;
@@ -75,9 +74,6 @@ class VideoCaptureDeviceMac;
   // |captureDeviceInput_| is owned by |captureSession_|.
   CrAVCaptureDeviceInput* captureDeviceInput_;
   base::scoped_nsobject<CrAVCaptureVideoDataOutput> captureVideoDataOutput_;
-
-  // An AVDataOutput specialized for taking pictures out of |captureSession_|.
-  base::scoped_nsobject<CrAVCaptureStillImageOutput> stillImageOutput_;
 
   base::ThreadChecker main_thread_checker_;
 }
@@ -118,10 +114,6 @@ class VideoCaptureDeviceMac;
 
 // Stops video capturing and stops listening to notifications.
 - (void)stopCapture;
-
-// Takes a photo. This method should only be called between -startCapture and
-// -stopCapture.
-- (void)takePhoto;
 
 @end
 
