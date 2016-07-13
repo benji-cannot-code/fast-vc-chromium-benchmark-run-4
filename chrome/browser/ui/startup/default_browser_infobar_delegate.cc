53dcac1df962cff8acadd622f3726b52473ae6af
pmonette <pmonette@chromium.org>
2016-07-12 19:29:04 -0700

    Now correctly tracks the case where the default browser prompt is ignored.
    Turns out IGNORE_INFO_BAR was misleading (Meant dismissed) and there was no
    metric for no interaction with the infobar.
    
    Review-Url: https://codereview.chromium.org/2139753002
    Cr-Commit-Position: refs/heads/master@{#404914}

--- chrome/browser/ui/startup/default_browser_infobar_delegate.cc
+++ chrome/browser/ui/startup/default_browser_infobar_delegate.cc
@@ -58,6 +58,7 @@ DefaultBrowserInfoBarDelegate::DefaultBrowserInfoBarDelegate(Profile* profile)
     : ConfirmInfoBarDelegate(),
       profile_(profile),
       should_expire_(false),
+      action_taken_(false),
       weak_factory_(this) {
   // We want the info-bar to stick-around for few seconds and then be hidden
   // on the next navigation after that.
@@ -67,7 +68,13 @@ DefaultBrowserInfoBarDelegate::DefaultBrowserInfoBarDelegate(Profile* profile)
       base::TimeDelta::FromSeconds(8));
 }
 
-DefaultBrowserInfoBarDelegate::~DefaultBrowserInfoBarDelegate() = default;
+DefaultBrowserInfoBarDelegate::~DefaultBrowserInfoBarDelegate() {
+  if (!action_taken_) {
+    UMA_HISTOGRAM_ENUMERATION("DefaultBrowser.InfoBar.UserInteraction",
+                              IGNORE_INFO_BAR,
+                              NUM_INFO_BAR_USER_INTERACTION_TYPES);
+  }
+}
 
 void DefaultBrowserInfoBarDelegate::AllowExpiry() {
   should_expire_ = true;
@@ -128,10 +135,11 @@ bool DefaultBrowserInfoBarDelegate::ShouldExpire(
 }
 
 void DefaultBrowserInfoBarDelegate::InfoBarDismissed() {
+  action_taken_ = true;
   content::RecordAction(
       base::UserMetricsAction("DefaultBrowserInfoBar_Dismiss"));
   UMA_HISTOGRAM_ENUMERATION("DefaultBrowser.InfoBar.UserInteraction",
-                            IGNORE_INFO_BAR,
+                            DISMISS_INFO_BAR,
                             NUM_INFO_BAR_USER_INTERACTION_TYPES);
 }
 
@@ -165,6 +173,7 @@ bool DefaultBrowserInfoBarDelegate::OKButtonTriggersUACPrompt() const {
 }
 
 bool DefaultBrowserInfoBarDelegate::Accept() {
+  action_taken_ = true;
   content::RecordAction(
       base::UserMetricsAction("DefaultBrowserInfoBar_Accept"));
   UMA_HISTOGRAM_ENUMERATION("DefaultBrowser.InfoBar.UserInteraction",
@@ -191,6 +200,7 @@ bool DefaultBrowserInfoBarDelegate::Accept() {
 }
 
 bool DefaultBrowserInfoBarDelegate::Cancel() {
+  action_taken_ = true;
   // This can get reached in tests where profile_ is null.
   if (profile_)
     chrome::DefaultBrowserPromptDeclined(profile_);
