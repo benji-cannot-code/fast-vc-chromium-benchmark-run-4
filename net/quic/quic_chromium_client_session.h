9714d1eec3e14d2b23d89f2d2029e80f15e3851b
jri <jri@chromium.org>
2016-07-13 13:15:09 -0700

    Implements migration of a QUIC connection to a different destination address if specified by the server in an ASAD tag in the Server Hello. Quietly falls back to original socket if creation and connecting a new socket fails.
    R=rch@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2124753005
    Cr-Commit-Position: refs/heads/master@{#405262}

--- net/quic/quic_chromium_client_session.h
+++ net/quic/quic_chromium_client_session.h
@@ -184,6 +184,7 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
   void OnRstStream(const QuicRstStreamFrame& frame) override;
 
   // QuicClientSessionBase methods:
+  void OnConfigNegotiated() override;
   void OnProofValid(const QuicCryptoClientConfig::CachedState& cached) override;
   void OnProofVerifyDetailsAvailable(
       const ProofVerifyDetails& verify_details) override;
--- net/quic/quic_client_session_base.h
+++ net/quic/quic_client_session_base.h
@@ -43,6 +43,8 @@ class NET_EXPORT_PRIVATE QuicClientSessionBase
 
   ~QuicClientSessionBase() override;
 
+  void OnConfigNegotiated() override;
+
   // Override base class to set FEC policy before any data is sent by client.
   void OnCryptoHandshakeEvent(CryptoHandshakeEvent event) override;
 
