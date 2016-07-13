19084b676156bfe556a9b975293c636e0c3e4845
pke <pke@google.com>
2016-07-13 02:20:36 -0700

    Change NTPSnippetsService to implement ContentSuggestionsProvider
    The NTPSnippetsService currently delivers snippets to the UI via the
    Android bridge. Change it to additionally serve as a
    ContentSuggestionsProvider implementation and deliver
    ContentSuggestions for the ARTICLES category. These ContentSuggestions
    are currently only delivered to the ContentSuggestionsService and not
    served to the UI, yet.
    
    Adjust NTPSnippetsServiceTest, SnippetsBridge and SnippetsInternals
    because of a few renamed functions.
    
    Change NTPSnippetsServiceFactory to register the NTPSnippetsService
    as a provder with the ContentSuggestionsService.
    
    BUG=619560
    
    Review-Url: https://codereview.chromium.org/2131943002
    Cr-Commit-Position: refs/heads/master@{#405103}

--- chrome/browser/ntp_snippets/ntp_snippets_service_factory.cc
+++ chrome/browser/ntp_snippets/ntp_snippets_service_factory.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/memory/singleton.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/ntp_snippets/content_suggestions_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/suggestions/image_decoder_impl.h"
 #include "chrome/browser/search/suggestions/suggestions_service_factory.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/image_fetcher/image_fetcher.h"
 #include "components/image_fetcher/image_fetcher_impl.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "components/ntp_snippets/content_suggestions_service.h"
 #include "components/ntp_snippets/ntp_snippets_constants.h"
 #include "components/ntp_snippets/ntp_snippets_database.h"
 #include "components/ntp_snippets/ntp_snippets_fetcher.h"
@@ -49,6 +51,7 @@ using image_fetcher::ImageFetcherImpl;
 using suggestions::ImageDecoderImpl;
 using suggestions::SuggestionsService;
 using suggestions::SuggestionsServiceFactory;
+using ntp_snippets::ContentSuggestionsService;
 
 // static
 NTPSnippetsServiceFactory* NTPSnippetsServiceFactory::GetInstance() {
@@ -71,6 +74,7 @@ NTPSnippetsServiceFactory::NTPSnippetsServiceFactory()
   DependsOn(ProfileSyncServiceFactory::GetInstance());
   DependsOn(SigninManagerFactory::GetInstance());
   DependsOn(SuggestionsServiceFactory::GetInstance());
+  DependsOn(ContentSuggestionsServiceFactory::GetInstance());
 }
 
 NTPSnippetsServiceFactory::~NTPSnippetsServiceFactory() {}
@@ -78,6 +82,13 @@ NTPSnippetsServiceFactory::~NTPSnippetsServiceFactory() {}
 KeyedService* NTPSnippetsServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
   Profile* profile = Profile::FromBrowserContext(context);
+
+  ContentSuggestionsService* content_suggestions_service =
+      ContentSuggestionsServiceFactory::GetForProfile(profile);
+  // TODO(pke): When the AndroidBridge does not access the NTPSnippetsService
+  // directly anymore (for retrieving content), the NTPSnippetsService does not
+  // need to be created if content_suggestions_service->state() == DISABLED,
+  // just return nullptr then and remove the other "if" below.
 
   // TODO(mvanouwerkerk): Move the enable logic into the service once we start
   // observing pref changes.
@@ -112,18 +123,25 @@ KeyedService* NTPSnippetsServiceFactory::BuildServiceInstanceFor(
               base::SequencedWorkerPool::GetSequenceToken(),
               base::SequencedWorkerPool::CONTINUE_ON_SHUTDOWN);
 
-  return new ntp_snippets::NTPSnippetsService(
-      enabled, profile->GetPrefs(), suggestions_service,
-      g_browser_process->GetApplicationLocale(), scheduler,
-      base::MakeUnique<ntp_snippets::NTPSnippetsFetcher>(
-          signin_manager, token_service, request_context,
-          base::Bind(&safe_json::SafeJsonParser::Parse),
-          chrome::GetChannel() == version_info::Channel::STABLE),
-      base::MakeUnique<ImageFetcherImpl>(
-          base::MakeUnique<ImageDecoderImpl>(), request_context.get()),
-      base::MakeUnique<ImageDecoderImpl>(),
-      base::MakeUnique<ntp_snippets::NTPSnippetsDatabase>(database_dir,
-                                                          task_runner),
-      base::MakeUnique<ntp_snippets::NTPSnippetsStatusService>(signin_manager,
-                                                               sync_service));
+  ntp_snippets::NTPSnippetsService* ntp_snippets_service =
+      new ntp_snippets::NTPSnippetsService(
+          enabled, profile->GetPrefs(), suggestions_service,
+          g_browser_process->GetApplicationLocale(), scheduler,
+          base::MakeUnique<ntp_snippets::NTPSnippetsFetcher>(
+              signin_manager, token_service, request_context,
+              base::Bind(&safe_json::SafeJsonParser::Parse),
+              chrome::GetChannel() == version_info::Channel::STABLE),
+          base::MakeUnique<ImageFetcherImpl>(
+              base::MakeUnique<ImageDecoderImpl>(), request_context.get()),
+          base::MakeUnique<ImageDecoderImpl>(),
+          base::MakeUnique<ntp_snippets::NTPSnippetsDatabase>(database_dir,
+                                                              task_runner),
+          base::MakeUnique<ntp_snippets::NTPSnippetsStatusService>(
+              signin_manager, sync_service));
+
+  if (content_suggestions_service->state() ==
+      ContentSuggestionsService::State::ENABLED) {
+    content_suggestions_service->RegisterProvider(ntp_snippets_service);
+  }
+  return ntp_snippets_service;
 }
