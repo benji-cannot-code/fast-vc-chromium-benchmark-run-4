fa8669617adb06969256b7f50b5421b97f090695
Yuli Huang <yuli@chromium.org>
2016-07-13 19:37:06 +0800

    Support both KeyboardEvent keyIdentifier and key.
    TEST=Tested manually on peppy.
    BUG=chromium:627029
    
    Change-Id: I97a8c1a20a36ecfba2a45eaa57f8dfdade737c80
    Reviewed-on: https://chromium-review.googlesource.com/360292
    Reviewed-by: Tomasz Mikolajewski <mtomasz@chromium.org>
    Tested-by: Yuli Huang <yuli@chromium.org>

--- src/js/views/dialog.js
+++ src/js/views/dialog.js
@@ -149,7 +149,7 @@ camera.views.Dialog.prototype.onKeyPressed = function(event) {
       this.router.back({isPositive: true});
       event.preventDefault();
       break;
-    case 'U+001B':  // Escape.
+    case 'Escape':
       this.router.back({isPositive: false});
       event.preventDefault();
       break;
