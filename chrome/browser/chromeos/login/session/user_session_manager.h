0146a39237fe0d71d4fde9bfdefb0a816131d3d3
malaykeshav <malaykeshav@chromium.org>
2016-07-13 02:25:54 -0700

    Implements the feature notification for Quick Unlock
    Displays a feature notification informing the user about quick unlock.
    Upon clicking the notification the user is taken to the settings page.
    
    The UI strings used in the notification are still TODO and not final.
    
    BUG=626079
    COMPONENT=Chrome OS, Quick Unlock, Preferences, Strings
    
    Review-Url: https://codereview.chromium.org/2128053002
    Cr-Commit-Position: refs/heads/master@{#405105}

--- chrome/browser/chromeos/login/session/user_session_manager.h
+++ chrome/browser/chromeos/login/session/user_session_manager.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/base/locale_util.h"
 #include "chrome/browser/chromeos/eol_notification.h"
 #include "chrome/browser/chromeos/hats/hats_notification_controller.h"
+#include "chrome/browser/chromeos/login/quick_unlock/quick_unlock_notification_controller.h"
 #include "chrome/browser/chromeos/login/signin/oauth2_login_manager.h"
 #include "chrome/browser/chromeos/login/signin/token_handle_util.h"
 #include "chromeos/dbus/session_manager_client.h"
@@ -475,6 +476,12 @@ class UserSessionManager
   // Per-user-session EndofLife Notification
   std::map<Profile*, std::unique_ptr<EolNotification>, ProfileCompare>
       eol_notification_handler_;
+
+  // Per-user-session Quick Unlock Feature Notification
+  std::map<Profile*,
+           scoped_refptr<QuickUnlockNotificationController>,
+           ProfileCompare>
+      quick_unlock_notification_handler_;
 
   // Manages Easy unlock cryptohome keys.
   std::unique_ptr<EasyUnlockKeyManager> easy_unlock_key_manager_;
