bb262223af162ddad5e2d827ff9fdf9ea28e6cff
lfg <lfg@chromium.org>
2016-07-13 10:08:44 -0700

    Move didChangeCursor calls from WebViewClient to WebWidgetClient.
    This is part of a refactor to split WebView/WebWidget.
    
    BUG=419087
    
    Review-Url: https://codereview.chromium.org/2043903002
    Cr-Commit-Position: refs/heads/master@{#405202}

--- third_party/WebKit/Source/web/ChromeClientImpl.h
+++ third_party/WebKit/Source/web/ChromeClientImpl.h
@@ -118,7 +118,7 @@ public:
     DateTimeChooser* openDateTimeChooser(DateTimeChooserClient*, const DateTimeChooserParameters&) override;
     void openFileChooser(LocalFrame*, PassRefPtr<FileChooser>) override;
     void enumerateChosenDirectory(FileChooser*) override;
-    void setCursor(const Cursor&, LocalFrame* localRoot) override;
+    void setCursor(const Cursor&, LocalFrame*) override;
     Cursor lastSetCursorForTesting() const override;
     void setEventListenerProperties(WebEventListenerClass, WebEventListenerProperties) override;
     WebEventListenerProperties eventListenerProperties(WebEventListenerClass) const override;
@@ -145,7 +145,7 @@ public:
     String acceptLanguages() override;
 
     // ChromeClientImpl:
-    void setCursorForPlugin(const WebCursorInfo&, LocalFrame* localRoot);
+    void setCursorForPlugin(const WebCursorInfo&, LocalFrame*);
     void setNewWindowNavigationPolicy(WebNavigationPolicy);
     void setCursorOverridden(bool);
 
@@ -198,7 +198,7 @@ private:
     void registerPopupOpeningObserver(PopupOpeningObserver*) override;
     void unregisterPopupOpeningObserver(PopupOpeningObserver*) override;
 
-    void setCursor(const WebCursorInfo&, LocalFrame* localRoot);
+    void setCursor(const WebCursorInfo&, LocalFrame*);
 
     WebViewImpl* m_webView; // Weak pointer.
     WindowFeatures m_windowFeatures;
