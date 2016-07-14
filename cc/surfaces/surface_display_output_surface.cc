069f2f0cf32ab42c07fc88a69b7d07b0a4b22c0d
danakj <danakj@chromium.org>
2016-07-14 16:24:05 -0700

    cc: Allow TestDelegatingOutputSurface to be used for non-pixel tests.
    This renames the PixelTestDelegatingOutputSurface to drop the "Pixel",
    it's cleaner.
    
    And the caller creates the Display and its OutputSurface, etc. So it
    has control over the types and can use a PixelTestOutputSurface or a
    FakeOutputSurface for non-pixel unit tests.
    
    R=enne
    BUG=606056
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_blink_rel
    
    Review-Url: https://codereview.chromium.org/2144393003
    Cr-Commit-Position: refs/heads/master@{#405623}

--- cc/surfaces/surface_display_output_surface.cc
+++ cc/surfaces/surface_display_output_surface.cc
@@ -98,7 +98,8 @@ bool SurfaceDisplayOutputSurface::BindToClient(OutputSurfaceClient* client) {
 
   // Avoid initializing GL context here, as this should be sharing the
   // Display's context.
-  display_->Initialize(this);
+  display_->Initialize(this, surface_manager_,
+                       surface_id_allocator_->client_id());
   return true;
 }
 
