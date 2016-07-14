d377e8fac4aa1caf344e983044ba5809213aded9
eostroukhov <eostroukhov@google.com>
2016-07-14 15:17:33 -0700

    [DevTools] Support for the inspector target type
    No longer try to set overlay message and no longer send
    "Inspecotr.enable" message to the targets of JS inspector type.
    
    BUG=624494
    
    Review-Url: https://codereview.chromium.org/2122113004
    Cr-Commit-Position: refs/heads/master@{#405601}

--- third_party/WebKit/Source/devtools/front_end/main/OverlayController.js
+++ third_party/WebKit/Source/devtools/front_end/main/OverlayController.js
@@ -8,20 +8,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 WebInspector.OverlayController = function()
 {
-    WebInspector.moduleSetting("disablePausedStateOverlay").addChangeListener(this._updateOverlayMessage, this);
+    WebInspector.moduleSetting("disablePausedStateOverlay").addChangeListener(this._updateAllOverlayMessages, this);
     WebInspector.targetManager.addModelListener(WebInspector.DebuggerModel, WebInspector.DebuggerModel.Events.DebuggerPaused, this._updateOverlayMessage, this);
     WebInspector.targetManager.addModelListener(WebInspector.DebuggerModel, WebInspector.DebuggerModel.Events.DebuggerResumed, this._updateOverlayMessage, this);
     WebInspector.targetManager.addModelListener(WebInspector.DebuggerModel, WebInspector.DebuggerModel.Events.GlobalObjectCleared, this._updateOverlayMessage, this);
 }
 
 WebInspector.OverlayController.prototype = {
+    _updateAllOverlayMessages: function()
+    {
+        for (var target of WebInspector.targetManager.targets(WebInspector.Target.Capability.Browser))
+            this._updateTargetOverlayMessage(/** @type {!WebInspector.DebuggerModel} */ (WebInspector.DebuggerModel.fromTarget(target)));
+    },
 
     /**
      * @param {!WebInspector.Event} event
      */
     _updateOverlayMessage: function(event)
     {
-        var debuggerModel = /** @type {!WebInspector.DebuggerModel} */ (event.target);
+        this._updateTargetOverlayMessage(/** @type {!WebInspector.DebuggerModel} */ (event.target));
+    },
+
+    /**
+     * @param {!WebInspector.DebuggerModel} debuggerModel
+     */
+    _updateTargetOverlayMessage: function(debuggerModel)
+    {
+        if (!debuggerModel.target().hasBrowserCapability())
+            return;
         var message = debuggerModel.isPaused() && !WebInspector.moduleSetting("disablePausedStateOverlay").get() ? WebInspector.UIString("Paused in debugger") : undefined;
         debuggerModel.target().pageAgent().setOverlayMessage(message);
     }
