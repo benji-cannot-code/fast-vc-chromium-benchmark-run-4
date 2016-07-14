208821a771ca37557d6ab8aa1a5885bed1a59b57
tobiasjs <tobiasjs@chromium.org>
2016-07-14 10:07:47 -0700

    Always apply UniformMatrix4fvStreamTextureMatrixCHROMIUM matrix argument.
    Regardless of whether we have a stream texture matrix, we should apply
    the transformation matrix held by the StreamTextureDrawQuad instance.
    
    Change the implementation of the extension to always premultiply either
    the stream texture matrix or the identity matrix by the passed
    transformation matrix.
    
    BUG=597681
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/1818073002
    Cr-Commit-Position: refs/heads/master@{#405507}

--- gpu/ipc/service/stream_texture_android.cc
+++ gpu/ipc/service/stream_texture_android.cc
@@ -97,6 +97,7 @@ void StreamTexture::GetTextureMatrix(float xform[16]) {
     surface_texture_->GetTransformMatrix(current_matrix_);
   }
   memcpy(xform, current_matrix_, sizeof(current_matrix_));
+  YInvertMatrix(xform);
 }
 
 void StreamTexture::OnWillDestroyStub() {
