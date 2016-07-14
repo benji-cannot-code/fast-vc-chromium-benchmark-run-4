fe286da901ff0215510975022a910824d5d3f44e
juncai <juncai@chromium.org>
2016-07-13 17:46:54 -0700

    This is part 1 of matching WebBluetooth chooser views to mocks.
    This CL changes the WebBluetooth chooser title from "connect to"
    to "pair with".
    
    BUG=583452
    
    Review-Url: https://codereview.chromium.org/2126873003
    Cr-Commit-Position: refs/heads/master@{#405370}

--- chrome/browser/chooser_controller/chooser_controller.cc
+++ chrome/browser/chooser_controller/chooser_controller.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chooser_controller/chooser_controller.h"
 
 #include "base/strings/utf_string_conversions.h"
-#include "chrome/grit/generated_resources.h"
 #include "components/url_formatter/elide_url.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
@@ -15,8 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "url/origin.h"
 
-ChooserController::ChooserController(content::RenderFrameHost* owner)
-    : owning_frame_(owner) {}
+ChooserController::ChooserController(content::RenderFrameHost* owner,
+                                     int title_string_id_origin,
+                                     int title_string_id_extension)
+    : owning_frame_(owner),
+      title_string_id_origin_(title_string_id_origin),
+      title_string_id_extension_(title_string_id_extension) {}
 
 ChooserController::~ChooserController() {}
 
@@ -37,15 +40,14 @@ base::string16 ChooserController::GetTitle() const {
       const extensions::Extension* extension =
           extension_registry->enabled_extensions().GetByID(origin.host());
       if (extension) {
-        return l10n_util::GetStringFUTF16(
-            IDS_DEVICE_CHOOSER_PROMPT_EXTENSION_NAME,
-            base::UTF8ToUTF16(extension->name()));
+        return l10n_util::GetStringFUTF16(title_string_id_extension_,
+                                          base::UTF8ToUTF16(extension->name()));
       }
     }
   }
 
   return l10n_util::GetStringFUTF16(
-      IDS_DEVICE_CHOOSER_PROMPT_ORIGIN,
+      title_string_id_origin_,
       url_formatter::FormatOriginForSecurityDisplay(
           origin, url_formatter::SchemeDisplay::OMIT_CRYPTOGRAPHIC));
 }
--- chrome/browser/chooser_controller/mock_chooser_controller.cc
+++ chrome/browser/chooser_controller/mock_chooser_controller.cc
@@ -4,9 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/chooser_controller/mock_chooser_controller.h"
+#include "chrome/grit/generated_resources.h"
 
 MockChooserController::MockChooserController(content::RenderFrameHost* owner)
-    : ChooserController(owner) {}
+    : ChooserController(owner,
+                        IDS_USB_DEVICE_CHOOSER_PROMPT_ORIGIN,
+                        IDS_USB_DEVICE_CHOOSER_PROMPT_EXTENSION_NAME) {}
 
 MockChooserController::~MockChooserController() {}
 
