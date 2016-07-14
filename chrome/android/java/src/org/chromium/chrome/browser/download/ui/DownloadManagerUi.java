04fa65ad277274465a1be0cde9bd604aa3081d42
dfalcantara <dfalcantara@chromium.org>
2016-07-14 15:34:25 -0700

    [Downloads] Expose basic download history to UI
    * Adds hooks in DownloadManagerService for calling GetAllDownloads()
      and sending the information back to Java.  This creates a
      List of DownloadItems that contain the basic information needed
      for the UI.
    
    * Adds a basic RecyclerView to display the files in the user's
      download history.
    
    * Adds a DownloadHistoryAdapter that is used to track information
      about the download history.  This *should* be reusable among
      multiple RecyclerViews in case the user opens up multi-window
      mode.
    
    - This currently doesn't monitor any changes to the history and is
      used solely for getting a skeleton working.
    
    BUG=616324
    
    Review-Url: https://codereview.chromium.org/2143303002
    Cr-Commit-Position: refs/heads/master@{#405607}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadManagerUi.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadManagerUi.java
@@ -14,6 +14,8 @@ import android.os.Environment;
 import android.os.StatFs;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.Toolbar.OnMenuItemClickListener;
 import android.text.format.Formatter;
@@ -30,8 +32,10 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.chromium.base.ApiCompatibilityUtils;
+import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.download.DownloadManagerService;
 import org.chromium.chrome.browser.widget.FadingShadow;
 import org.chromium.chrome.browser.widget.FadingShadowView;
 import org.chromium.chrome.browser.widget.TintedDrawable;
@@ -192,6 +196,7 @@ public class DownloadManagerUi extends DrawerLayout implements OnMenuItemClickLi
     private DownloadManagerToolbar mToolbar;
     private SpaceDisplay mSpaceDisplay;
     private ListView mFilterView;
+    private RecyclerView mRecyclerView;
 
     public DownloadManagerUi(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -209,6 +214,12 @@ public class DownloadManagerUi extends DrawerLayout implements OnMenuItemClickLi
         mFilterView = (ListView) findViewById(R.id.section_list);
         mFilterView.setAdapter(mFilterAdapter);
         mFilterView.setOnItemClickListener(mFilterAdapter);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setAdapter(getDownloadManagerService().getDownloadHistoryAdapter());
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        getDownloadManagerService().getAllDownloads();
     }
 
     /**
@@ -259,6 +270,11 @@ public class DownloadManagerUi extends DrawerLayout implements OnMenuItemClickLi
             return true;
         }
         return false;
+    }
+
+    private DownloadManagerService getDownloadManagerService() {
+        return DownloadManagerService.getDownloadManagerService(
+                ContextUtils.getApplicationContext());
     }
 
 }
