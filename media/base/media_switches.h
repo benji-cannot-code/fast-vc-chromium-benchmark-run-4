e7b22fcb57dc9ef70615220cc3e6349d5deb5286
xjz <xjz@chromium.org>
2016-07-14 15:16:35 -0700

    Remove command-line switch: cast-encoder-util-heuristic=backlog<N> This command-line switch was added for testing on an experimental 'backlog' heuristic for measuring encoder utilization in: https://codereview.chromium.org/2108373002/
    From lab testing, the new heuristic works better and more stable than
    original deadline heuristic. This CL removes the command-line switch
    and makes the new heuristic default.
    
    BUG=chrome-os-partner:55236,chrome-os-partner:54806
    
    Review-Url: https://codereview.chromium.org/2149843002
    Cr-Commit-Position: refs/heads/master@{#405600}

--- media/base/media_switches.h
+++ media/base/media_switches.h
@@ -66,8 +66,6 @@ MEDIA_EXPORT extern const char kDisableRTCSmoothnessAlgorithm[];
 
 MEDIA_EXPORT extern const char kEnableVp9InMp4[];
 
-MEDIA_EXPORT extern const char kCastEncoderUtilHeuristic[];
-
 }  // namespace switches
 
 namespace media {
