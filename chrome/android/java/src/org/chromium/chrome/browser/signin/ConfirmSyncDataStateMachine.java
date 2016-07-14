7167f1e54a326fb2a9b688a7d4d50b1006aee3dc
peconn <peconn@chromium.org>
2016-07-14 08:11:37 -0700

    Dismiss Sync Dialogs if Account has been deleted.
    BUG=627079
    
    Review-Url: https://codereview.chromium.org/2145983002
    Cr-Commit-Position: refs/heads/master@{#405497}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/ConfirmSyncDataStateMachine.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/ConfirmSyncDataStateMachine.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.signin;
 
+import android.app.DialogFragment;
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
 import android.support.annotation.IntDef;
@@ -85,6 +87,28 @@ public class ConfirmSyncDataStateMachine
         ConfirmSyncDataStateMachine stateMachine = new ConfirmSyncDataStateMachine(oldAccountName,
                 newAccountName, importSyncType, fragmentManager, context, callback);
         stateMachine.progress();
+    }
+
+    /**
+     * If any of the dialogs used by this state machine are shown, cancel them. If this state
+     * machine is running and a dialog is being shown, the given
+     * {@link ConfirmImportSyncDataDialog.Listener#onCancel())} is called.
+     */
+    public static void cancelAllDialogs(FragmentManager fragmentManager) {
+        cancelDialog(fragmentManager,
+                ConfirmImportSyncDataDialog.CONFIRM_IMPORT_SYNC_DATA_DIALOG_TAG);
+        cancelDialog(fragmentManager,
+                ConfirmManagedSyncDataDialog.CONFIRM_IMPORT_SYNC_DATA_DIALOG_TAG);
+    }
+
+    private static void cancelDialog(FragmentManager fragmentManager, String tag) {
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+
+        if (fragment == null) return;
+        DialogFragment dialogFragment = (DialogFragment) fragment;
+
+        if (dialogFragment.getDialog() == null) return;
+        dialogFragment.getDialog().cancel();
     }
 
     private ConfirmSyncDataStateMachine(String oldAccountName, String newAccountName,
