ab65f842060984f45e2230686c72c32b2e6a3131
mlamouri <mlamouri@chromium.org>
2016-07-14 03:13:09 -0700

    Mark PaymentRequestNoShippingTest#testCancelUnmaskAndRetry as flaky.
    BUG=628143
    TBR=dfalcantara@chromium.org
    
    Review-Url: https://codereview.chromium.org/2149913002
    Cr-Commit-Position: refs/heads/master@{#405462}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestNoShippingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestNoShippingTest.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.payments;
 import android.content.DialogInterface;
 import android.test.suitebuilder.annotation.MediumTest;
 
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.autofill.AutofillTestHelper;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.AutofillProfile;
@@ -72,6 +73,7 @@ public class PaymentRequestNoShippingTest extends PaymentRequestTestBase {
                 "123"});
     }
 
+    @FlakyTest(message = "crbug.com/628143")
     @MediumTest
     public void testCancelUnmaskAndRetry()
             throws InterruptedException, ExecutionException, TimeoutException {
