834ad34fa8e27ed6c0021effd985b2b5a87f188f
lfg <lfg@chromium.org>
2016-07-14 07:16:33 -0700

    Remove didAutoResize from WebWidgetClient and move it to WebViewClient.
    This is part of a refactor to split WebView/WebWidget.
    
    BUG=419087
    
    Review-Url: https://codereview.chromium.org/2143643002
    Cr-Commit-Position: refs/heads/master@{#405489}

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -2018,7 +2018,7 @@ void RenderViewImpl::convertWindowToViewport(blink::WebFloatRect* rect) {
 }
 
 void RenderViewImpl::didAutoResize(const blink::WebSize& newSize) {
-  RenderWidget::didAutoResize(newSize);
+  RenderWidget::DidAutoResize(newSize);
 }
 
 void RenderViewImpl::didInvalidateRect(const blink::WebRect& rect) {
