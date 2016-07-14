e797fcd0ed8101faad9afd34901a90ca66b2cf0b
robert.bradford <robert.bradford@intel.com>
2016-07-14 14:05:23 -0700

    ui: Pass display size by const reference in AddVirtualDisplay
    BUG=425060
    TEST=Build test.
    
    Review-Url: https://codereview.chromium.org/2152883002
    Cr-Commit-Position: refs/heads/master@{#405568}

--- ui/display/chromeos/display_configurator.cc
+++ ui/display/chromeos/display_configurator.cc
@@ -1117,7 +1117,7 @@ void DisplayConfigurator::NotifyPowerStateObservers() {
       Observer, observers_, OnPowerStateChanged(current_power_state_));
 }
 
-int64_t DisplayConfigurator::AddVirtualDisplay(gfx::Size display_size) {
+int64_t DisplayConfigurator::AddVirtualDisplay(const gfx::Size& display_size) {
   if (last_virtual_display_id_ == 0xff) {
     LOG(WARNING) << "Exceeded virtual display id limit";
     return display::Display::kInvalidDisplayID;
