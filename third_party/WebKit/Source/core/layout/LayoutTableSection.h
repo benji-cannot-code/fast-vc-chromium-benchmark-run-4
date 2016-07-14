0b2a412001c2485f473fff42f110488c0e9ecff3
dgrogan <dgrogan@chromium.org>
2016-07-14 13:36:39 -0700

    [css-tables] Don't use internal layout structure when iterating all rows
    Instead use basic firstChild and nextSibling. This avoids a crash when
    the internal layout structure is dirty.
    
    Add DCHECK to discourage future mistakes.
    
    BUG=627839
    
    Review-Url: https://codereview.chromium.org/2146163005
    Cr-Commit-Position: refs/heads/master@{#405555}

--- third_party/WebKit/Source/core/layout/LayoutTableSection.h
+++ third_party/WebKit/Source/core/layout/LayoutTableSection.h
@@ -247,7 +247,11 @@ public:
     int outerBorderStart() const { return m_outerBorderStart; }
     int outerBorderEnd() const { return m_outerBorderEnd; }
 
-    unsigned numRows() const { return m_grid.size(); }
+    unsigned numRows() const
+    {
+        DCHECK(!needsCellRecalc());
+        return m_grid.size();
+    }
     unsigned numEffectiveColumns() const;
 
     // recalcCells() is used when we are not sure about the section's structure
