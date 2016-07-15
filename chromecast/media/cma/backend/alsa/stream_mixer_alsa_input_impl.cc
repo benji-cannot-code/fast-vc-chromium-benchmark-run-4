1120eb7f5c0ee92b56df509db592518254d62cbf
kmackay <kmackay@chromium.org>
2016-07-15 16:11:57 -0700

    [Chromecast] Make ALSA rendering delay either accurate or kNoTimestamp
    Update the ALSA backend so that the calculated rendering delay is either
    accurate, or kNoTimestamp. We no longer try to guess the correct delay
    timestamp. Note that this requires callers of GetRenderingDelay() to
    correctly handle the kNoTimestamp case.
    
    BUG= internal b/30034408
    
    Review-Url: https://codereview.chromium.org/2133293003
    Cr-Commit-Position: refs/heads/master@{#405893}

--- chromecast/media/cma/backend/alsa/stream_mixer_alsa_input_impl.cc
+++ chromecast/media/cma/backend/alsa/stream_mixer_alsa_input_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/media/cma/backend/alsa/stream_mixer_alsa_input_impl.h"
 
 #include <algorithm>
+#include <limits>
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
@@ -54,6 +55,7 @@ const int64_t kFadeMs = 15;
 // fill the frames with silence.
 const int kPausedReadSamples = 512;
 const int kDefaultReadSize = ::media::SincResampler::kDefaultRequestSize;
+const int64_t kNoTimestamp = std::numeric_limits<int64_t>::min();
 
 }  // namespace
 
@@ -201,9 +203,11 @@ MediaPipelineBackendAlsa::RenderingDelay StreamMixerAlsaInputImpl::QueueData(
   }
 
   MediaPipelineBackendAlsa::RenderingDelay delay = mixer_rendering_delay_;
-  delay.delay_microseconds += static_cast<int64_t>(
-      queued_frames_including_resampler_ * base::Time::kMicrosecondsPerSecond /
-      input_samples_per_second_);
+  if (delay.timestamp_microseconds != kNoTimestamp) {
+    delay.delay_microseconds += static_cast<int64_t>(
+        queued_frames_including_resampler_ *
+        base::Time::kMicrosecondsPerSecond / input_samples_per_second_);
+  }
   return delay;
 }
 
