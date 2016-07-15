d8d3fdc824772c1aae5116a162de27b55b9b02b8
thestig <thestig@chromium.org>
2016-07-15 12:26:44 -0700

    Clean up printing::StickySetting.
    Review-Url: https://codereview.chromium.org/2148303004
    Cr-Commit-Position: refs/heads/master@{#405819}

--- chrome/browser/ui/webui/print_preview/sticky_settings.cc
+++ chrome/browser/ui/webui/print_preview/sticky_settings.cc
@@ -5,18 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/print_preview/sticky_settings.h"
 
-#include "base/command_line.h"
 #include "base/values.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
-#include "printing/page_size_margins.h"
 
 namespace printing {
 
+namespace {
+
 const char kSettingAppState[] = "appState";
+
+}  // namespace
 
 StickySettings::StickySettings() {}
 
@@ -27,26 +28,18 @@ void StickySettings::StoreAppState(const std::string& data) {
 }
 
 void StickySettings::SaveInPrefs(PrefService* prefs) {
-  DCHECK(prefs);
-  if (prefs) {
-    std::unique_ptr<base::DictionaryValue> value(new base::DictionaryValue);
-    if (printer_app_state_.get())
-      value->SetString(printing::kSettingAppState,
-          *printer_app_state_);
-    prefs->Set(prefs::kPrintPreviewStickySettings, *value);
-  }
+  std::unique_ptr<base::DictionaryValue> value(new base::DictionaryValue);
+  if (printer_app_state_)
+    value->SetString(kSettingAppState, *printer_app_state_);
+  prefs->Set(prefs::kPrintPreviewStickySettings, *value);
 }
 
 void StickySettings::RestoreFromPrefs(PrefService* prefs) {
-  DCHECK(prefs);
-  if (prefs) {
-    const base::DictionaryValue* value =
-        prefs->GetDictionary(prefs::kPrintPreviewStickySettings);
-
-    std::string buffer;
-    if (value->GetString(printing::kSettingAppState, &buffer))
-      printer_app_state_.reset(new std::string(buffer));
-  }
+  const base::DictionaryValue* value =
+      prefs->GetDictionary(prefs::kPrintPreviewStickySettings);
+  std::string buffer;
+  if (value->GetString(kSettingAppState, &buffer))
+    StoreAppState(buffer);
 }
 
 void StickySettings::RegisterProfilePrefs(
@@ -58,4 +51,4 @@ std::string* StickySettings::printer_app_state() {
   return printer_app_state_.get();
 }
 
-} // namespace printing
+}  // namespace printing
