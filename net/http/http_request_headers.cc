85320946a40029c0d3553667cb1c9ba37ad81cad
ricea <ricea@chromium.org>
2016-07-14 22:26:18 -0700

    Make calling SetHeader() with invalid value fatal
    crrev.com/2134083003 made net::HttpUtil::IsValidHeaderValue() reject
    individual CR and NL as well as CRNL.
    
    I believe that all callers of net::HttpRequestHeaders::SetHeader() and
    SetHeaderIfMissing() which use user-supplied values already verify the
    value with IsValidHeaderValue() first. However, to be sure, temporarily
    make it a fatal error to call SetHeader() with an invalid value.
    
    If you see a crash attributed to this CL:
    
    1. Associate it with the bug.
    2. Follow the stack flow to work out how untrusted data ended up
    being passed to SetHeader().
    3. Add a call to IsValidHeaderValue() at the point where the untrusted
    data was introduced. A tighter check such as IsToken() may be appopriate.
    
    BUG=627398
    
    Review-Url: https://codereview.chromium.org/2143903002
    Cr-Commit-Position: refs/heads/master@{#405702}

--- net/http/http_request_headers.cc
+++ net/http/http_request_headers.cc
@@ -91,7 +91,8 @@ void HttpRequestHeaders::Clear() {
 void HttpRequestHeaders::SetHeader(const base::StringPiece& key,
                                    const base::StringPiece& value) {
   DCHECK(HttpUtil::IsValidHeaderName(key.as_string()));
-  DCHECK(HttpUtil::IsValidHeaderValue(value.as_string()));
+  // TODO(ricea): Revert this. See crbug.com/627398.
+  CHECK(HttpUtil::IsValidHeaderValue(value.as_string()));
   HeaderVector::iterator it = FindHeader(key);
   if (it != headers_.end())
     it->value.assign(value.data(), value.size());
@@ -102,7 +103,8 @@ void HttpRequestHeaders::SetHeader(const base::StringPiece& key,
 void HttpRequestHeaders::SetHeaderIfMissing(const base::StringPiece& key,
                                             const base::StringPiece& value) {
   DCHECK(HttpUtil::IsValidHeaderName(key.as_string()));
-  DCHECK(HttpUtil::IsValidHeaderValue(value.as_string()));
+  // TODO(ricea): Revert this. See crbug.com/627398.
+  CHECK(HttpUtil::IsValidHeaderValue(value.as_string()));
   HeaderVector::iterator it = FindHeader(key);
   if (it == headers_.end())
     headers_.push_back(HeaderKeyValuePair(key, value));
