7cd2bf38cb5c1488f6323eaa1da0faca4e96d3eb
dfalcantara <dfalcantara@chromium.org>
2016-07-15 13:05:30 -0700

    [Payments] API Level 16 fixes
    * Try to address the background of PaymentRequest being gray.
    
    * Address crashes from calling methods that exist only on
      API level 17+.
    
    - This is a slight step backward for accessibility pre-JBMR1,
      but the parent layout already has an accessibility hint set,
      so the child view just ends up saying something less useful
      during TalkBack.
    
    BUG=628587
    
    Review-Url: https://codereview.chromium.org/2150403002
    Cr-Commit-Position: refs/heads/master@{#405838}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/EditorView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/EditorView.java
@@ -257,7 +257,7 @@ public class EditorView extends AlwaysDismissedDialog
 
                 firstParams.width = 0;
                 firstParams.weight = 1;
-                firstParams.setMarginEnd(mHalfRowMargin);
+                ApiCompatibilityUtils.setMarginEnd(firstParams, mHalfRowMargin);
                 lastParams.width = 0;
                 lastParams.weight = 1;
                 i = i + 1;
