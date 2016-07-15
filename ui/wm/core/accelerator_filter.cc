2891cdb12402a81192946f77440358d1bd6b146d
oshima <oshima@chromium.org>
2016-07-15 12:50:54 -0700

    Mark POWER key as system key so that it won't be passed to regular apps.
    BUG=628240,b/30105805
    
    Review-Url: https://codereview.chromium.org/2147013003
    Cr-Commit-Position: refs/heads/master@{#405830}

--- ui/wm/core/accelerator_filter.cc
+++ ui/wm/core/accelerator_filter.cc
@@ -14,30 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/wm/core/accelerator_delegate.h"
 
 namespace wm {
-namespace {
-
-// Returns true if |key_code| is a key usually handled directly by the shell.
-bool IsSystemKey(ui::KeyboardCode key_code) {
-#if defined(OS_CHROMEOS)
-  switch (key_code) {
-    case ui::VKEY_MEDIA_LAUNCH_APP2:  // Fullscreen button.
-    case ui::VKEY_MEDIA_LAUNCH_APP1:  // Overview button.
-    case ui::VKEY_BRIGHTNESS_DOWN:
-    case ui::VKEY_BRIGHTNESS_UP:
-    case ui::VKEY_KBD_BRIGHTNESS_DOWN:
-    case ui::VKEY_KBD_BRIGHTNESS_UP:
-    case ui::VKEY_VOLUME_MUTE:
-    case ui::VKEY_VOLUME_DOWN:
-    case ui::VKEY_VOLUME_UP:
-      return true;
-    default:
-      return false;
-  }
-#endif  // defined(OS_CHROMEOS)
-  return false;
-}
-
-}  // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
 // AcceleratorFilter, public:
@@ -67,11 +43,7 @@ void AcceleratorFilter::OnKeyEvent(ui::KeyEvent* event) {
   ui::Accelerator accelerator(*event);
   accelerator_history_->StoreCurrentAccelerator(accelerator);
 
-  AcceleratorDelegate::KeyType key_type =
-      IsSystemKey(event->key_code()) ? AcceleratorDelegate::KEY_TYPE_SYSTEM
-                                     : AcceleratorDelegate::KEY_TYPE_OTHER;
-
-  if (delegate_->ProcessAccelerator(*event, accelerator, key_type))
+  if (delegate_->ProcessAccelerator(*event, accelerator))
     event->StopPropagation();
 }
 
