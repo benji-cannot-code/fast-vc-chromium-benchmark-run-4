287ffcae70fee934a7f4cf9f8ee523eece12c4e5
estade <estade@chromium.org>
2016-07-15 15:04:35 -0700

    Remove icon bg color for message center notifications.
    Also change notification bg from 0xFEFEFE to 0xFFFFFF
    
    BUG=624968
    TBR=rdsmith@chromium.org
    
    Review-Url: https://codereview.chromium.org/2143653007
    Cr-Commit-Position: refs/heads/master@{#405874}

--- ui/message_center/cocoa/notification_controller.mm
+++ ui/message_center/cocoa/notification_controller.mm
@@ -737,8 +737,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   base::scoped_nsobject<NSBox> imageBox(
       [[AccessibilityIgnoredBox alloc] initWithFrame:imageFrame]);
   [self configureCustomBox:imageBox];
-  [imageBox setFillColor:skia::SkColorToCalibratedNSColor(
-      message_center::kIconBackgroundColor)];
   [imageBox setAutoresizingMask:NSViewMinYMargin];
 
   // Inside the image box put the actual icon view.
