1a2431f78b8e4c7f00223151a89a75c254ea82c6
ekaramad <ekaramad@chromium.org>
2016-07-15 14:59:00 -0700

    Removed const from RenderWidgetHostView::GetSelectedText()
    This method is only used by a test (WebViewInteractiveTest.TextSelection) and
    may eventually be removed. In the meantime, comply with the content API
    guidelines by removing const from the public interface. This is also useful for
    a followup CL (https://codereview.chromium.org/2130133004).
    
    BUG=578168
    
    Review-Url: https://codereview.chromium.org/2152073002
    Cr-Commit-Position: refs/heads/master@{#405870}

--- content/browser/frame_host/render_widget_host_view_guest.cc
+++ content/browser/frame_host/render_widget_host_view_guest.cc
@@ -247,7 +247,7 @@ gfx::Size RenderWidgetHostViewGuest::GetPhysicalBackingSize() const {
   return RenderWidgetHostViewBase::GetPhysicalBackingSize();
 }
 
-base::string16 RenderWidgetHostViewGuest::GetSelectedText() const {
+base::string16 RenderWidgetHostViewGuest::GetSelectedText() {
   return platform_view_->GetSelectedText();
 }
 
