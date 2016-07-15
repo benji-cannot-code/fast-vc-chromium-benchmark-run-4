7b90a2d360b7107c6ac02e09964b881c205b467e
varkha <varkha@chromium.org>
2016-07-15 12:58:42 -0700

    [ash-md] Allows docked windows to be selected in overview mode
    BUG=351329
    
    Review-Url: https://codereview.chromium.org/2133333002
    Cr-Commit-Position: refs/heads/master@{#405834}

--- ash/common/wm/dock/docked_window_layout_manager.h
+++ ash/common/wm/dock/docked_window_layout_manager.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/ash_export.h"
+#include "ash/common/shell_observer.h"
 #include "ash/common/wm/dock/dock_types.h"
 #include "ash/common/wm/dock/docked_window_layout_manager_observer.h"
 #include "ash/common/wm/window_state_observer.h"
@@ -48,6 +49,7 @@ class ASH_EXPORT DockedWindowLayoutManager
       public WmRootWindowControllerObserver,
       public WmWindowObserver,
       public WmActivationObserver,
+      public ShellObserver,
       public keyboard::KeyboardControllerObserver,
       public wm::WindowStateObserver {
  public:
@@ -154,6 +156,13 @@ class ASH_EXPORT DockedWindowLayoutManager
   // WmActivationObserver:
   void OnWindowActivated(WmWindow* gained_active,
                          WmWindow* lost_active) override;
+
+  // ShellObserver:
+  void OnShelfAlignmentChanged(WmWindow* root_window) override;
+  void OnFullscreenStateChanged(bool is_fullscreen,
+                                WmWindow* root_window) override;
+  void OnOverviewModeStarting() override;
+  void OnOverviewModeEnded() override;
 
  private:
   struct CompareMinimumHeight;
@@ -277,6 +286,9 @@ class ASH_EXPORT DockedWindowLayoutManager
 
   // Target bounds of a docked window being dragged.
   gfx::Rect dragged_bounds_;
+
+  // True while in overview mode.
+  bool in_overview_;
 
   // Side of the screen that the dock is positioned at.
   DockedAlignment alignment_;
