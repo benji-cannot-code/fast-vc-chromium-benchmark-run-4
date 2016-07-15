1bd35019a415bdb68f2350df5d81bdb2d3815ed9
vasilii <vasilii@chromium.org>
2016-07-15 04:09:42 -0700

    Add instrumentation for password autofill and Credential Manager API user flows.
    The instrumentation is implemented via user actions rather than histograms so
    that events can be sequenced, based on their timestamps. This will allow us to
    better understand how the Credential Manager API and password autofill interact.
    For example, we currently observe that users dismiss the account chooser more
    frequently than we expected. We'd like to estimate whether users had the
    intention of logging in by measuring whether users log in after dismissing the
    account chooser; and if so, whether they used the autofill password manager.
    
    In addition the CL marks kManagerActionBlacklisted as deprecated since it's unused.
    
    BUG=400674
    
    Review-Url: https://codereview.chromium.org/2113743002
    Cr-Commit-Position: refs/heads/master@{#405741}

--- components/password_manager/content/browser/credential_manager_impl.cc
+++ components/password_manager/content/browser/credential_manager_impl.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/metrics/user_metrics.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/autofill/core/common/password_form.h"
@@ -251,6 +252,11 @@ void CredentialManagerImpl::SendPasswordForm(
         store->UpdateLogin(update_form);
       }
     }
+    base::RecordAction(
+        base::UserMetricsAction("CredentialManager_AccountChooser_Accepted"));
+  } else {
+    base::RecordAction(
+        base::UserMetricsAction("CredentialManager_AccountChooser_Dismissed"));
   }
   SendCredential(send_callback, info);
 }
--- components/password_manager/core/browser/credential_manager_pending_request_task.cc
+++ components/password_manager/core/browser/credential_manager_pending_request_task.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <utility>
 
+#include "base/metrics/user_metrics.h"
 #include "components/autofill/core/common/password_form.h"
 #include "components/password_manager/core/browser/affiliated_match_helper.h"
 #include "components/password_manager/core/browser/password_bubble_experiment.h"
@@ -146,6 +147,7 @@ void CredentialManagerPendingRequestTask::OnGetPasswordStoreResults(
                             : CredentialType::CREDENTIAL_TYPE_FEDERATED);
     delegate_->client()->NotifyUserAutoSignin(std::move(local_results),
                                               origin_);
+    base::RecordAction(base::UserMetricsAction("CredentialManager_Autosignin"));
     delegate_->SendCredential(send_callback_, info);
     return;
   }
