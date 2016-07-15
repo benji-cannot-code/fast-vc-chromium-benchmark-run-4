354f40ee0d424fdefffb464dabba1559f2bfca16
johnme <johnme@chromium.org>
2016-07-15 11:00:00 -0700

    Push: Add applicationServerKey to sender ID missing error messages
    The PUSH_REGISTRATION_STATUS_NO_SENDER_ID and
    PUSH_REGISTRATION_STATUS_MANIFEST_EMPTY_OR_MISSING error messages
    are misleading now that a key or sender ID can be provided via
    applicationServerKey instead of the gcm_sender_id manifest
    property. Updated error messages accordingly.
    
    BUG=463113,583753
    
    Review-Url: https://codereview.chromium.org/2152823003
    Cr-Commit-Position: refs/heads/master@{#405788}

--- chrome/browser/push_messaging/push_messaging_browsertest.cc
+++ chrome/browser/push_messaging/push_messaging_browsertest.cc
@@ -437,8 +437,10 @@ IN_PROC_BROWSER_TEST_F(PushMessagingBrowserTest, SubscribeFailureNoManifest) {
   ASSERT_EQ("manifest removed", script_result);
 
   ASSERT_TRUE(RunScript("documentSubscribePushWithoutKey()", &script_result));
-  EXPECT_EQ("AbortError - Registration failed - manifest empty or missing",
-            script_result);
+  EXPECT_EQ(
+      "AbortError - Registration failed - missing applicationServerKey, and "
+      "manifest empty or missing",
+      script_result);
 }
 
 IN_PROC_BROWSER_TEST_F(PushMessagingBrowserTest, SubscribeFailureNoSenderId) {
@@ -454,7 +456,8 @@ IN_PROC_BROWSER_TEST_F(PushMessagingBrowserTest, SubscribeFailureNoSenderId) {
 
   ASSERT_TRUE(RunScript("documentSubscribePushWithoutKey()", &script_result));
   EXPECT_EQ(
-      "AbortError - Registration failed - gcm_sender_id not found in manifest",
+      "AbortError - Registration failed - missing applicationServerKey, and "
+      "gcm_sender_id not found in manifest",
       script_result);
 }
 
@@ -488,7 +491,8 @@ IN_PROC_BROWSER_TEST_F(PushMessagingBrowserTest, SubscribeWorker) {
   // Try to subscribe from a worker without a key. This should fail.
   ASSERT_TRUE(RunScript("workerSubscribePushNoKey()", &script_result));
   EXPECT_EQ(
-      "AbortError - Registration failed - gcm_sender_id not found in manifest",
+      "AbortError - Registration failed - missing applicationServerKey, and "
+      "gcm_sender_id not found in manifest",
       script_result);
   // Now run the subscribe from the service worker with a key. This
   // should succeed, and write the key to the datastore.
@@ -525,7 +529,8 @@ IN_PROC_BROWSER_TEST_F(PushMessagingBrowserTest, SubscribeWorkerUsingManifest) {
   // Try to subscribe from a worker without a key. This should fail.
   ASSERT_TRUE(RunScript("workerSubscribePushNoKey()", &script_result));
   EXPECT_EQ(
-      "AbortError - Registration failed - gcm_sender_id not found in manifest",
+      "AbortError - Registration failed - missing applicationServerKey, and "
+      "gcm_sender_id not found in manifest",
       script_result);
   EXPECT_NE(push_service(), GetAppHandler());
 
