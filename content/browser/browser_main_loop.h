b17d2f9f32edc2a917e9ba77baa78c1ec011053c
bashi <bashi@chromium.org>
2016-07-14 17:34:53 -0700

    Re-reland: Add MemoryCoordinator
    This is third attempt. `git cl try` doesn't help me :(
    
    Original description:
    
    This CL adds MemoryCoordinator mojo interface and
    its implementation. When --enable-features=MemoryCoordinator
    is specified the browser and renderers create their
    MemoryCoordinators. MemoryCoordinatorImpl (which lives in
    the browser process) creates a mojo message pipe when a
    renderer is launched.
    
    This CL doesn't add actual handling of memory events, which
    will be implemented in follow-up CLs.
    
    BUG=617492
    TBR=chrisha@chromium.org,haraken@chromium.org,dcheng@chromium.org,jam@chromium.org
    
    Review-Url: https://codereview.chromium.org/2147333002
    Cr-Commit-Position: refs/heads/master@{#405646}

--- content/browser/browser_main_loop.h
+++ content/browser/browser_main_loop.h
@@ -51,6 +51,10 @@ class MidiManager;
 }  // namespace midi
 }  // namespace media
 
+namespace memory_coordinator {
+class MemoryCoordinator;
+}  // namespace memory_coordinator
+
 namespace mojo {
 namespace edk {
 class ScopedIPCSupport;
@@ -143,6 +147,10 @@ class CONTENT_EXPORT BrowserMainLoop {
   }
 
   void StopStartupTracingTimer();
+
+  memory_coordinator::MemoryCoordinator* memory_coordinator() const {
+    return memory_coordinator_.get();
+  }
 
 #if defined(OS_MACOSX) && !defined(OS_IOS)
   media::DeviceMonitorMac* device_monitor_mac() const {
@@ -250,6 +258,7 @@ class CONTENT_EXPORT BrowserMainLoop {
   // Members initialized in |PreCreateThreads()| -------------------------------
   // Torn down in ShutdownThreadsAndCleanUp.
   std::unique_ptr<base::MemoryPressureMonitor> memory_pressure_monitor_;
+  std::unique_ptr<memory_coordinator::MemoryCoordinator> memory_coordinator_;
 
   // Members initialized in |CreateThreads()| ----------------------------------
   std::unique_ptr<BrowserProcessSubThread> db_thread_;
