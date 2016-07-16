de950ed9b643b01db4a9012015f2bb1810de19a6
varkha <varkha@chromium.org>
2016-07-15 21:23:58 -0700

    [ash-md] Implements a shadow over overview items and selector
    This is non-MD 9-patch shadow just to test drive the looks.
    Effectively this patch preserves the regular ash window shadows
    and applies them in overview.
    
    BUG=608852
    
    Review-Url: https://codereview.chromium.org/2150823002
    Cr-Commit-Position: refs/heads/master@{#405932}

--- ash/common/wm/overview/window_selector_item.h
+++ ash/common/wm/overview/window_selector_item.h
@@ -22,6 +22,10 @@ namespace views {
 class ImageButton;
 }
 
+namespace wm {
+class Shadow;
+}
+
 namespace ash {
 
 class WindowSelector;
@@ -174,6 +178,9 @@ class ASH_EXPORT WindowSelectorItem : public views::ButtonListener,
   // shown above the |transform_window_|.
   // Otherwise it is shown under the window.
   std::unique_ptr<views::Widget> window_label_;
+
+  // Shadow around the item in overview.
+  std::unique_ptr<::wm::Shadow> shadow_;
 
   // Label background widget used to fade in opacity when moving selection.
   std::unique_ptr<views::Widget> window_label_selector_;
