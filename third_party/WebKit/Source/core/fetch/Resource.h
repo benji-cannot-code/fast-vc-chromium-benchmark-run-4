ddafe8a27417b12c90ddaeda652dd546b243784c
hiroshige <hiroshige@chromium.org>
2016-07-17 08:09:38 -0700

    Add UMAs for SharedBuffer::lock()/unlock()
    If SharedBuffer is rarely lock()ed successfully after its unlock(), we can
    remove SharedBuffer::unlock() without significant performance regressions.
    To evaluate this assumption, this CL adds UMAs for SharedBuffer::lock() and
    unlock() counts.
    
    This CL also fixes ResourceType enum names in histograms.xml.
    ResourceType enum is introduced in 2013 and left unmodified since then, but
    it is now inconsistent with C++'s Resource::Type values.
    Because ResourceType enum has been unused recently (it is referenced in
    histograms.xml only from WebCore.ResourceFetcher.ResourceTypeUponCacheHit,
    which doesn't have C++ counterpart nor UMA reports at least since 2016/01),
    updating the ResourceType enum values in histograms.xml looks safe.
    
    BUG=603791
    
    Review-Url: https://codereview.chromium.org/2140513002
    Cr-Commit-Position: refs/heads/master@{#405956}

--- third_party/WebKit/Source/core/fetch/Resource.h
+++ third_party/WebKit/Source/core/fetch/Resource.h
@@ -58,6 +58,9 @@ class SharedBuffer;
 class CORE_EXPORT Resource : public GarbageCollectedFinalized<Resource> {
     WTF_MAKE_NONCOPYABLE(Resource);
 public:
+    // |Type| enum values are used in UMAs, so do not change the values of
+    // existing |Type|. When adding a new |Type|, append it at the end and
+    // update |kLastResourceType|.
     enum Type {
         MainResource,
         Image,
@@ -74,6 +77,7 @@ public:
         Media, // Audio or video file requested by a HTML5 media element
         Manifest
     };
+    static const int kLastResourceType = Manifest + 1;
 
     enum Status {
         NotStarted,
