43b74c64093aaa4255003e15becf21ec497affb9
dfalcantara <dfalcantara@chromium.org>
2016-07-18 12:29:15 -0700

    [Payments] Add ability to bold parts of addresses
    * Add the ability to bold parts of a label.
    
    * Add the ability to show a tertiary label in the PaymentOption, which
      will later be used for phone number display.
    
    * The phone number is passed in to the PaymentOption using the new constructor
      via the AutofillAddress class.
    
    See crbug.com/626833 for screenshots.
    
    BUG=603635,626833
    
    Committed: https://crrev.com/92f4c6ac21b2578ab3938c5bd3eb0f793621164f
    Review-Url: https://codereview.chromium.org/2135573004
    Cr-Original-Commit-Position: refs/heads/master@{#405900}
    Cr-Commit-Position: refs/heads/master@{#406062}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/AutofillPaymentInstrument.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/AutofillPaymentInstrument.java
@@ -161,7 +161,7 @@ public class AutofillPaymentInstrument
         mBillingAddress = billingAddress;
         mIsComplete = true;
         updateIdentifierLabelsAndIcon(card.getGUID(), card.getObfuscatedNumber(), card.getName(),
-                card.getIssuerIconDrawableId());
+                null, card.getIssuerIconDrawableId());
     }
 
     /** @return The credit card represented by this payment instrument. */
