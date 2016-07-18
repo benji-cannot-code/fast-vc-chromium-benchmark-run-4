dbb80bb21277b049b31678503fb04ab409870841
lizeb <lizeb@chromium.org>
2016-07-18 03:14:29 -0700

    Revert of Credit card editor for PaymentRequest UI. (patchset #5 id:490001 of https://codereview.chromium.org/2116583002/ )
    Reason for revert:
    Broke Android Tests (dbg).
    
    BUG=629011,624924
    
    Original issue's description:
    > Credit card editor for PaymentRequest UI.
    >
    > When user taps on an incomplete autofill credit card or [+ ADD CARD]
    > button, the card editor launches.
    >
    > For server cards, the editor shows uneditable card information and lets
    > the user set the billing address.
    >
    > For local cards, the editor shows the following fields.
    >
    > 1) A list of icons for supported card types.
    >
    > 2) The card number input with Luhn validation and formatting. Required
    >    field.
    >
    > 3) The name on card input. Required field.
    >
    > 4) The month and year dropdowns. January of this year by default for new
    >    cards.
    >
    > 5) The billing address dropdown with only complete addresses on disk and
    >    an option to add a new address. Required field.
    >
    > 6) A checkbox for saving the new card on disk. Checked by default,
    >    so it behaves similar to autofill settings.
    >
    > BUG=624924
    >
    > Committed: https://crrev.com/6c0897f84b3907e48ea75c1a8df6558134f28bea
    > Cr-Commit-Position: refs/heads/master@{#405876}
    
    TBR=mathp@chromium.org,dfalcantara@chromium.org,estade@chromium.org,rouslan@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    
    Review-Url: https://codereview.chromium.org/2158783002
    Cr-Commit-Position: refs/heads/master@{#405971}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestUI.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestUI.java
@@ -223,7 +223,6 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
 
     private final Dialog mDialog;
     private final EditorView mEditorView;
-    private final EditorView mCardEditorView;
     private final ViewGroup mFullContainer;
     private final ViewGroup mRequestView;
     private final PaymentRequestUiErrorView mErrorView;
@@ -333,7 +332,6 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
         mFullContainer.addView(mRequestView, bottomSheetParams);
 
         mEditorView = new EditorView(activity, sObserverForTest);
-        mCardEditorView = new EditorView(activity, sObserverForTest);
 
         // Set up the dialog.
         mDialog = new AlwaysDismissedDialog(activity, R.style.DialogWhenLarge);
@@ -449,8 +447,7 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
                 activity.getString(R.string.payments_contact_details_label),
                 activity.getString(R.string.payments_select_contact_details_prompt), this);
         mPaymentMethodSection = new OptionSection(activity,
-                activity.getString(R.string.payments_method_of_payment_label),
-                activity.getString(R.string.payments_select_method_of_payment_prompt), this);
+                activity.getString(R.string.payments_method_of_payment_label), null, this);
 
         // Add the necessary sections to the layout.
         mPaymentContainerLayout.addView(mOrderSummarySection, new LinearLayout.LayoutParams(
@@ -621,15 +618,9 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
         updatePayButtonEnabled();
     }
 
-    /** @return The common editor user interface. */
+    /** @return The editor user interface. */
     public EditorView getEditorView() {
         return mEditorView;
-    }
-
-    /** @return The card editor user interface. Distinct from the common editor user interface,
-     * because the credit card editor can launch the address editor. */
-    public EditorView getCardEditorView() {
-        return mCardEditorView;
     }
 
     /**
@@ -808,7 +799,7 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
             mPaymentContainerLayout.requestLayout();
 
             // Switch the 'edit' button to a 'cancel' button.
-            mEditButton.setText(mContext.getString(R.string.cancel));
+            mEditButton.setText(mContext.getString(R.string.payments_cancel_button));
 
             // Make the dialog take the whole screen.
             mDialog.getWindow().setLayout(
@@ -1107,11 +1098,6 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
     @VisibleForTesting
     public ViewGroup getShippingAddressSectionForTest() {
         return mShippingAddressSection;
-    }
-
-    @VisibleForTesting
-    public ViewGroup getPaymentMethodSectionForTest() {
-        return mPaymentMethodSection;
     }
 
     @VisibleForTesting
--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestIncompletePhoneTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestIncompletePhoneTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.payments;
 
+import android.content.DialogInterface;
 import android.test.suitebuilder.annotation.MediumTest;
 
 import org.chromium.chrome.R;
@@ -18,8 +19,6 @@ import java.util.concurrent.TimeoutException;
 /**
  * A payment integration test for a merchant that requests phone number from a user that has
  * incomplete phone number stored on disk.
- *
- * TODO(rouslan): Add a test to fill in the valid phone number and submit it to the merchant.
  */
 public class PaymentRequestIncompletePhoneTest extends PaymentRequestTestBase {
     public PaymentRequestIncompletePhoneTest() {
@@ -52,5 +51,20 @@ public class PaymentRequestIncompletePhoneTest extends PaymentRequestTestBase {
         clickInEditorAndWait(R.id.payments_edit_cancel_button, mReadyToClose);
         clickAndWait(R.id.close_button, mDismissed);
         expectResultContains(new String[] {"Request cancelled"});
+    }
+
+    /** Update the phone number with valid data and provide that to the merchant. */
+    @MediumTest
+    public void testEditIncompletePhoneAndPay()
+            throws InterruptedException, ExecutionException, TimeoutException {
+        triggerUIAndWait(mReadyForInput);
+        clickInContactInfoAndWait(R.id.payments_section, mReadyForInput);
+        clickInContactInfoAndWait(R.id.payments_first_radio_button, mReadyToEdit);
+        setTextInEditorAndWait(new String[] {"555-555-5555"}, mEditorTextUpdate);
+        clickInEditorAndWait(R.id.payments_edit_done_button, mReadyToPay);
+        clickAndWait(R.id.button_primary, mReadyForUnmaskInput);
+        setTextInCardUnmaskDialogAndWait(R.id.card_unmask_input, "123", mReadyToUnmask);
+        clickCardUnmaskButtonAndWait(DialogInterface.BUTTON_POSITIVE, mDismissed);
+        expectResultContains(new String[] {"555-555-5555"});
     }
 }
