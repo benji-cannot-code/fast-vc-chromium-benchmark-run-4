82b1472fe2d53ffe99e64510510bdf14ac6d2d0c
halliwell <halliwell@chromium.org>
2016-07-18 10:25:28 -0700

    Set default window title for Ozone X11 windows
    The old "egltest" platform set window title to "EGL test" by default.
    Would like to have the equivalent for X11.
    
    BUG=internal b/29226425
    
    Review-Url: https://codereview.chromium.org/2156743002
    Cr-Commit-Position: refs/heads/master@{#406023}

--- ui/ozone/platform/x11/ozone_platform_x11.cc
+++ ui/ozone/platform/x11/ozone_platform_x11.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/memory/ptr_util.h"
+#include "base/strings/utf_string_conversions.h"
 #include "ui/events/platform/x11/x11_event_source_libevent.h"
 #include "ui/ozone/common/stub_overlay_manager.h"
 #include "ui/ozone/platform/x11/native_display_delegate_ozone_x11.h"
@@ -86,6 +87,7 @@ class OzonePlatformX11 : public OzonePlatform {
                                             window_manager_.get(), delegate));
     window->SetBounds(bounds);
     window->Create();
+    window->SetTitle(base::ASCIIToUTF16("Ozone X11"));
     return std::move(window);
   }
 
