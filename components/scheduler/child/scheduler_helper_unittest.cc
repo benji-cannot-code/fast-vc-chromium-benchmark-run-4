b053801afa6e13afacdb10d464241ce449f2dbc3
alexclarke <alexclarke@chromium.org>
2016-07-18 09:56:48 -0700

    Fix a bug that could occasionaly cause setInterval to stop
    There was a bad interaction between throttled task queues and
    logic in TimerBase::setNextFireTime that could cause setInterval
    to stop firing.
    
    If a timer was posted to a throttled task queue after running on
    a normal task queue, it was possible for NOW to be in the past.
    
    Logic existed in TimeDomain::ComputeDelayedRunTime to compensate
    for this. Usually that worked fine, however the anti-drift logic
    in TimerBase for repeating tasks could cause the scheduled run
    time (as visible to TimerBase) to fall on the exact same time as
    the previous run.  This would cause TimerBase::setNextFireTime
    to nop out, which would cause the setInterval to stop firing.
    
    This patch prevents that by changing ThrottledTimeDomain to be based
    on RealTimeDomain.  This means Now() is the real time rather than a
    periodically updated virtual time.  I.e. it's monotonically increasing.
    
    At the same time I've remove the now useless
    TimeDomain::ComputeDelayedRunTime
    
    BUG=625041
    
    Review-Url: https://codereview.chromium.org/2155143002
    Cr-Commit-Position: refs/heads/master@{#406017}

--- components/scheduler/child/scheduler_helper_unittest.cc
+++ components/scheduler/child/scheduler_helper_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/test/simple_test_tick_clock.h"
 #include "cc/test/ordered_simple_task_runner.h"
+#include "components/scheduler/base/lazy_now.h"
 #include "components/scheduler/base/task_queue.h"
 #include "components/scheduler/base/test_time_source.h"
 #include "components/scheduler/child/scheduler_tqm_delegate_for_test.h"
@@ -180,7 +181,8 @@ TEST_F(SchedulerHelperTest,
 
   EXPECT_CALL(observer, WillProcessTask(_)).Times(0);
   EXPECT_CALL(observer, DidProcessTask(_)).Times(0);
-  scheduler_helper_->ControlAfterWakeUpTaskRunner()->PumpQueue(true);
+  LazyNow lazy_now(clock_.get());
+  scheduler_helper_->ControlAfterWakeUpTaskRunner()->PumpQueue(&lazy_now, true);
   RunUntilIdle();
 }
 
