a82889f48fc934cb5c92db2e827646054ef40a6f
tfarina <tfarina@chromium.org>
2016-07-18 14:15:05 -0700

    components/signin: convert GaiaCookieManagerService to use CanonicalCookie's Create() method.
    This patch converts the usage of CanonicalCookie's constructor in
    GaiaCookieManagerService::ForceOnCookieChangedProcessing() to use
    CanonicalCookie::Create() method.
    
    We are doing this because the Create() method validates the parameters
    and we thus we can test the return value to see if it has passed or not.
    Whereas we can not do this when using the constructor.
    
    BUG=57061
    TEST=components_unittests
    R=mmenke@chromium.org,rogerta@chromium.org
    
    Review-Url: https://codereview.chromium.org/2113863002
    Cr-Commit-Position: refs/heads/master@{#406097}

--- google_apis/gaia/gaia_urls.cc
+++ google_apis/gaia/gaia_urls.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Gaia service constants
-const char kDefaultGoogleUrl[] = "http://.google.com";
+const char kDefaultGoogleUrl[] = "http://google.com";
 const char kDefaultGaiaUrl[] = "https://accounts.google.com";
 const char kDefaultGoogleApisBaseUrl[] = "https://www.googleapis.com";
 
