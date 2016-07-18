c3738b9f0a600de5d643d44431dc2e4142051cf5
eranm <eranm@chromium.org>
2016-07-18 09:12:38 -0700

    STH Set: Delay loading until after start-up.
    When loading the STHs, the STHSet Component will invoke the
    SafeJsonParser up to half a dozen times (one for each STH), which
    can have performance implications.
    
    Also update the PostAfterStartupTask documentation to indicate the
    task will be posted immediately if called after start-up.
    
    BUG=607946
    
    Review-Url: https://codereview.chromium.org/2140093002
    Cr-Commit-Position: refs/heads/master@{#406009}

--- chrome/browser/component_updater/sth_set_component_installer.h
+++ chrome/browser/component_updater/sth_set_component_installer.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/gtest_prod_util.h"
+#include "base/memory/weak_ptr.h"
 #include "components/component_updater/default_component_installer.h"
 
 namespace base {
@@ -77,6 +78,8 @@ class STHSetComponentInstallerTraits : public ComponentInstallerTraits {
   // of this class is expected to ensure the STHObserver lives as long as
   // this class does. Typically the observer provided will be a global.
   net::ct::STHObserver* sth_observer_;
+
+  base::WeakPtrFactory<STHSetComponentInstallerTraits> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(STHSetComponentInstallerTraits);
 };
