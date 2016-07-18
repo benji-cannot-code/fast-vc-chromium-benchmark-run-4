fa3b4dae74dd0efc55bb104169ac7d23837b9bd5
thestig <thestig@chromium.org>
2016-07-18 12:36:50 -0700

    Modify grit unit tests to test for missing resources.
    This is a follow up to the fix in r405892. Also cleanup some nits.
    
    BUG=628086
    
    Review-Url: https://codereview.chromium.org/2153763005
    Cr-Commit-Position: refs/heads/master@{#406066}

--- tools/grit/grit/format/resource_map_unittest.py
+++ tools/grit/grit/format/resource_map_unittest.py
@@ -117,6 +117,28 @@ const size_t kTheRcHeaderSize = arraysize(kTheRcHeader);''', output)
               <structure type="chrome_scaled_image" name="IDR_MISSING"
                          file="bar.png" />
             </if>
+            <if expr="True">
+              <structure type="chrome_scaled_image" name="IDR_BLOB"
+                         file="blob.png" />
+            </if>
+            <if expr="True">
+              <then>
+                <structure type="chrome_scaled_image" name="IDR_METEOR"
+                           file="meteor.png" />
+              </then>
+              <else>
+                <structure type="chrome_scaled_image" name="IDR_METEOR"
+                           file="roetem.png" />
+              </else>
+            </if>
+            <if expr="False">
+              <structure type="chrome_scaled_image" name="IDR_LAST"
+                         file="zyx.png" />
+            </if>
+            <if expr="True">
+              <structure type="chrome_scaled_image" name="IDR_LAST"
+                         file="xyz.png" />
+            </if>
          </structures>
         </release>
       </grit>'''), util.PathFromRoot('.'))
@@ -144,6 +166,9 @@ extern const size_t kTheRcHeaderSize;''', output)
 #include "the_rc_header.h"
 const GritResourceMap kTheRcHeader[] = {
   {"IDR_KLONKMENU", IDR_KLONKMENU},
+  {"IDR_BLOB", IDR_BLOB},
+  {"IDR_METEOR", IDR_METEOR},
+  {"IDR_LAST", IDR_LAST},
 };
 const size_t kTheRcHeaderSize = arraysize(kTheRcHeader);''', output)
     output = util.StripBlankLinesAndComments(''.join(
@@ -155,6 +180,9 @@ const size_t kTheRcHeaderSize = arraysize(kTheRcHeader);''', output)
 #include "the_rc_header.h"
 const GritResourceMap kTheRcHeader[] = {
   {"IDR_KLONKMENU", IDR_KLONKMENU},
+  {"IDR_BLOB", IDR_BLOB},
+  {"IDR_METEOR", IDR_METEOR},
+  {"IDR_LAST", IDR_LAST},
 };
 const size_t kTheRcHeaderSize = arraysize(kTheRcHeader);''', output)
 
@@ -179,6 +207,23 @@ const size_t kTheRcHeaderSize = arraysize(kTheRcHeader);''', output)
               <include type="foo" file="def" name="IDS_MISSING" />
             </if>
             <include type="foo" file="mno" name="IDS_THIRDPRESENT" />
+            <if expr="True">
+              <include type="foo" file="blob" name="IDS_BLOB" />
+            </if>
+            <if expr="True">
+              <then>
+                <include type="foo" file="meteor" name="IDS_METEOR" />
+              </then>
+              <else>
+                <include type="foo" file="roetem" name="IDS_METEOR" />
+              </else>
+            </if>
+            <if expr="False">
+              <include type="foo" file="zyx" name="IDS_LAST" />
+            </if>
+            <if expr="True">
+              <include type="foo" file="xyz" name="IDS_LAST" />
+            </if>
          </includes>
         </release>
       </grit>'''), util.PathFromRoot('.'))
@@ -208,6 +253,9 @@ const GritResourceMap kTheRcHeader[] = {
   {"IDC_KLONKMENU", IDC_KLONKMENU},
   {"IDS_FIRSTPRESENT", IDS_FIRSTPRESENT},
   {"IDS_THIRDPRESENT", IDS_THIRDPRESENT},
+  {"IDS_BLOB", IDS_BLOB},
+  {"IDS_METEOR", IDS_METEOR},
+  {"IDS_LAST", IDS_LAST},
 };
 const size_t kTheRcHeaderSize = arraysize(kTheRcHeader);''', output)
     output = util.StripBlankLinesAndComments(''.join(
@@ -221,6 +269,9 @@ const GritResourceMap kTheRcHeader[] = {
   {"grit/testdata/klonk.rc", IDC_KLONKMENU},
   {"abc", IDS_FIRSTPRESENT},
   {"mno", IDS_THIRDPRESENT},
+  {"blob", IDS_BLOB},
+  {"meteor", IDS_METEOR},
+  {"xyz", IDS_LAST},
 };
 const size_t kTheRcHeaderSize = arraysize(kTheRcHeader);''', output)
 
