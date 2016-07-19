e4926564b4383789e45b8b0252c789275d67aadd
yuweih <yuweih@chromium.org>
2016-07-18 18:54:57 -0700

    [Remoting Android] OpenGL Desktop View Implementation
    This is part of the project of using OpenGL to render remote host on Android.
    This CL includes the OpenGL AbstractDesktopView implementation and refactors
    some common code into AbstractDesktopView.
    
    BUG=385924
    
    Review-Url: https://codereview.chromium.org/2152563002
    Cr-Commit-Position: refs/heads/master@{#406175}

--- remoting/android/java/src/org/chromium/chromoting/FeedbackAnimator.java
+++ remoting/android/java/src/org/chromium/chromoting/FeedbackAnimator.java
@@ -35,7 +35,7 @@ public final class FeedbackAnimator
     }
 
     /** Begins a new animation sequence at position (|pos|). */
-    public static void startAnimation(AbstractDesktopView view,
+    public static void startAnimation(DesktopView view,
                                       Point pos,
                                       DesktopView.InputFeedbackType feedbackType) {
         if (feedbackType == DesktopView.InputFeedbackType.NONE) {
--- remoting/android/java/src/org/chromium/chromoting/jni/GlDisplay.java
+++ remoting/android/java/src/org/chromium/chromoting/jni/GlDisplay.java
@@ -13,6 +13,7 @@ import org.chromium.chromoting.AbstractDesktopView;
 import org.chromium.chromoting.Desktop;
 import org.chromium.chromoting.DesktopViewFactory;
 import org.chromium.chromoting.Event;
+import org.chromium.chromoting.GlDesktopView;
 import org.chromium.chromoting.SizeChangedEventParameter;
 
 /**
@@ -164,8 +165,7 @@ public class GlDisplay {
         client.setDesktopViewFactory(new DesktopViewFactory() {
             @Override
             public AbstractDesktopView createDesktopView(Desktop desktop, Client client) {
-                // UNIMPLEMENTED.
-                return null;
+                return new GlDesktopView(GlDisplay.this, desktop, client);
             }
         });
     }
