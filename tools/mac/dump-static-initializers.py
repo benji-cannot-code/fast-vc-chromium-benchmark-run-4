3917e4330e13f12bf5980da196f74b1543f32cc0
Robert Sesek <rsesek@chromium.org>
2016-07-19 14:24:51 -0400

    Add tools/mac/show_mod_init_func.py to symbolize static initializers without a dSYM.
    BUG=628052
    R=mark@chromium.org
    
    Review URL: https://codereview.chromium.org/2161093002 .
    
    Cr-Commit-Position: refs/heads/master@{#406325}

--- tools/mac/dump-static-initializers.py
+++ tools/mac/dump-static-initializers.py
@@ -8,10 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Dumps a list of files with static initializers. Use with release builds.
 
 Usage:
-  tools/mac/dump-static-initializers.py out/Release/Chromium\ Framework.framework.dSYM/Contents/Resources/DWARF/Chromium\ Framework 
+  tools/mac/dump-static-initializers.py out/Release/Chromium\ Framework.framework.dSYM/Contents/Resources/DWARF/Chromium\ Framework
 
 Do NOT use mac_strip_release=0 or component=shared_library if you want to use
 this script.
+
+This is meant to be used on a dSYM file. If only an unstripped executable is
+present, use show_mod_init_func.py.
 """
 
 import optparse
