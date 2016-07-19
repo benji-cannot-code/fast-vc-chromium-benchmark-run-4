fec764d8dc9bd2be1892b122d0269dfc6e7402d2
yosin <yosin@chromium.org>
2016-07-19 04:17:20 -0700

    Update layout at required place in InputMethodController::setComposition()
    This patch moves update layout call before |mostForwardCaretPosition()|
    which requires clean layout tree as a preparation of introducing clean layout
    tree assertion to |mostForwardCaretPosition()|.
    
    BUG=590369
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2147303003
    Cr-Commit-Position: refs/heads/master@{#406246}

--- third_party/WebKit/Source/core/editing/InputMethodController.cpp
+++ third_party/WebKit/Source/core/editing/InputMethodController.cpp
@@ -365,6 +365,10 @@ void InputMethodController::setComposition(const String& text, const Vector<Comp
     if (!frame().document())
         return;
 
+    // TODO(yosin): The use of updateStyleAndLayoutIgnorePendingStylesheets
+    // needs to be audited. see http://crbug.com/590369 for more details.
+    frame().document()->updateStyleAndLayoutIgnorePendingStylesheets();
+
     // Find out what node has the composition now.
     Position base = mostForwardCaretPosition(frame().selection().base());
     Node* baseNode = base.anchorNode();
@@ -399,10 +403,6 @@ void InputMethodController::setComposition(const String& text, const Vector<Comp
     Element* rootEditableElement = frame().selection().rootEditableElement();
     if (!rootEditableElement)
         return;
-
-    // TODO(dglazkov): The use of updateStyleAndLayoutIgnorePendingStylesheets needs to be audited.
-    // see http://crbug.com/590369 for more details.
-    rootEditableElement->document().updateStyleAndLayoutIgnorePendingStylesheets();
 
     // In case of exceeding the right boundary.
     // If both |value1| and |value2| exceed right boundary,
