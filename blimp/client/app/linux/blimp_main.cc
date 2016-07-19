f3eaff49e005299e10b884173609561b40c255fa
nyquist <nyquist@chromium.org>
2016-07-19 15:29:18 -0700

    Move blimp AssignmentSource to core
    The assignment source is an internal detail of Blimp and does not have
    to have a public API. It is used by the BlimpClientSession, and in the
    process of converting that to have a public API, moving the source
    to the internal core folder is one step on the way towards that goal.
    
    This temporarily splits up the session-target in blimp, as the
    BlimpClientSession continues to stay in //blimp/client/session.
    
    BUG=611097
    
    Review-Url: https://codereview.chromium.org/2157133003
    Cr-Commit-Position: refs/heads/master@{#406399}

--- blimp/client/app/linux/blimp_main.cc
+++ blimp/client/app/linux/blimp_main.cc
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "blimp/client/app/blimp_startup.h"
 #include "blimp/client/app/linux/blimp_client_session_linux.h"
+#include "blimp/client/core/session/assignment_source.h"
 #include "blimp/client/feature/navigation_feature.h"
 #include "blimp/client/feature/tab_control_feature.h"
-#include "blimp/client/session/assignment_source.h"
 #include "ui/gfx/x/x11_connection.h"
 
 namespace {
--- blimp/client/core/session/assignment_source.h
+++ blimp/client/core/session/assignment_source.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef BLIMP_CLIENT_SESSION_ASSIGNMENT_SOURCE_H_
-#define BLIMP_CLIENT_SESSION_ASSIGNMENT_SOURCE_H_
+#ifndef BLIMP_CLIENT_CORE_SESSION_ASSIGNMENT_SOURCE_H_
+#define BLIMP_CLIENT_CORE_SESSION_ASSIGNMENT_SOURCE_H_
 
 #include <string>
 
@@ -64,7 +64,7 @@ struct Assignment {
 class AssignmentSource : public net::URLFetcherDelegate {
  public:
   // A Java counterpart will be generated for this enum.
-  // GENERATED_JAVA_ENUM_PACKAGE: org.chromium.blimp.assignment
+  // GENERATED_JAVA_ENUM_PACKAGE: org.chromium.blimp.core.session.assignment
   enum Result {
     RESULT_UNKNOWN = 0,
     RESULT_OK = 1,
@@ -130,4 +130,4 @@ class AssignmentSource : public net::URLFetcherDelegate {
 }  // namespace client
 }  // namespace blimp
 
-#endif  // BLIMP_CLIENT_SESSION_ASSIGNMENT_SOURCE_H_
+#endif  // BLIMP_CLIENT_CORE_SESSION_ASSIGNMENT_SOURCE_H_
