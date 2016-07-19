4287846d20cffb2dbb2f9ae1a168e30a85b4eaa0
hubbe <hubbe@chromium.org>
2016-07-18 18:59:57 -0700

    Revert of Support for retreiving the monitor color space on X11. (patchset #2 id:20001 of https://codereview.chromium.org/2115103002/ )
    Reason for revert:
    Temporary revert as it breaks layout tests for many people.
    
    Original issue's description:
    > Support for retreiving the monitor color space on X11.
    >
    > BUG=622133
    >
    > Committed: https://crrev.com/b0df9e3fcbca19d2a625fa274172bd3be4116261
    > Cr-Commit-Position: refs/heads/master@{#403958}
    
    TBR=ccameron@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=622133
    
    Review-Url: https://codereview.chromium.org/2160133002
    Cr-Commit-Position: refs/heads/master@{#406181}

--- ui/gfx/color_space.cc
+++ ui/gfx/color_space.cc
@@ -123,7 +123,7 @@ ColorSpace ColorSpace::FromICCProfile(const std::vector<char>& icc_profile) {
   return color_space;
 }
 
-#if !defined(OS_WIN) && !defined(OS_MACOSX) && !defined(USE_X11)
+#if !defined(OS_WIN) && !defined(OS_MACOSX)
 // static
 ColorSpace ColorSpace::FromBestMonitor() {
   return ColorSpace();
--- ui/gfx/gfx.gyp
+++ ui/gfx/gfx.gyp
@@ -152,7 +152,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'color_space.h',
         'color_space_mac.mm',
         'color_space_win.cc',
-        'color_space_x11.cc',
         'color_utils.cc',
         'color_utils.h',
         'favicon_size.cc',
