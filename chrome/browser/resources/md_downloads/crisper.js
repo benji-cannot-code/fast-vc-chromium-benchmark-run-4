e66f9eb0e5764376ea6612c608deecb5a3f6bfab
dbeam <dbeam@chromium.org>
2016-07-18 21:03:17 -0700

    MD Downloads: fix focus issue on downloads page by combining pause/play
    R=tsergeant@chromium.org
    BUG=605093
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    NOPRESUBMIT=true
    
    Review-Url: https://codereview.chromium.org/2154033002
    Cr-Commit-Position: refs/heads/master@{#406209}

--- chrome/browser/resources/md_downloads/crisper.js
+++ chrome/browser/resources/md_downloads/crisper.js
@@ -6323,10 +6323,21 @@ cr.define('downloads', function() {
         value: false,
       },
 
+      isMalware_: {
+        computed: 'computeIsMalware_(isDangerous_, data.danger_type)',
+        type: Boolean,
+        value: false,
+      },
+
       isInProgress_: {
         computed: 'computeIsInProgress_(data.state)',
         type: Boolean,
         value: false,
+      },
+
+      pauseOrResumeText_: {
+        computed: 'computePauseOrResumeText_(isInProgress_, data.resume)',
+        type: String,
       },
 
       showCancel_: {
@@ -6337,12 +6348,6 @@ cr.define('downloads', function() {
 
       showProgress_: {
         computed: 'computeShowProgress_(showCancel_, data.percent)',
-        type: Boolean,
-        value: false,
-      },
-
-      isMalware_: {
-        computed: 'computeIsMalware_(isDangerous_, data.danger_type)',
         type: Boolean,
         value: false,
       },
@@ -6473,6 +6478,15 @@ cr.define('downloads', function() {
     },
 
     /** @private */
+    computePauseOrResumeText_: function() {
+      if (this.isInProgress_)
+        return loadTimeData.getString('controlPause');
+      if (this.data.resume)
+        return loadTimeData.getString('controlResume');
+      return '';
+    },
+
+    /** @private */
     computeRemoveStyle_: function() {
       var canDelete = loadTimeData.getBoolean('allowDeletingHistory');
       var hideRemove = this.isDangerous_ || this.showCancel_ || !canDelete;
@@ -6561,18 +6575,16 @@ cr.define('downloads', function() {
     },
 
     /** @private */
-    onPauseTap_: function() {
-      downloads.ActionService.getInstance().pause(this.data.id);
+    onPauseOrResumeTap_: function() {
+      if (this.isInProgress_)
+        downloads.ActionService.getInstance().pause(this.data.id);
+      else
+        downloads.ActionService.getInstance().resume(this.data.id);
     },
 
     /** @private */
     onRemoveTap_: function() {
       downloads.ActionService.getInstance().remove(this.data.id);
-    },
-
-    /** @private */
-    onResumeTap_: function() {
-      downloads.ActionService.getInstance().resume(this.data.id);
     },
 
     /** @private */
@@ -11378,7 +11390,7 @@ cr.define('downloads', function() {
     },
 
     /** @private */
-    closeMoreActions_: function(e) {
+    closeMoreActions_: function() {
       this.$.more.close();
     },
 
@@ -11403,7 +11415,8 @@ cr.define('downloads', function() {
      * @private
      */
     onItemBlur_: function(e) {
-      if (this.$$('paper-menu').items.indexOf(e.relatedTarget) >= 0)
+      var menu = /** @type {PaperMenuElement} */(this.$$('paper-menu'));
+      if (menu.items.indexOf(e.relatedTarget) >= 0)
         return;
 
       // This can be this.$.more.restoreFocusOnClose = false when this lands:
--- chrome/browser/resources/md_downloads/item.js
+++ chrome/browser/resources/md_downloads/item.js
@@ -38,10 +38,21 @@ cr.define('downloads', function() {
         value: false,
       },
 
+      isMalware_: {
+        computed: 'computeIsMalware_(isDangerous_, data.danger_type)',
+        type: Boolean,
+        value: false,
+      },
+
       isInProgress_: {
         computed: 'computeIsInProgress_(data.state)',
         type: Boolean,
         value: false,
+      },
+
+      pauseOrResumeText_: {
+        computed: 'computePauseOrResumeText_(isInProgress_, data.resume)',
+        type: String,
       },
 
       showCancel_: {
@@ -52,12 +63,6 @@ cr.define('downloads', function() {
 
       showProgress_: {
         computed: 'computeShowProgress_(showCancel_, data.percent)',
-        type: Boolean,
-        value: false,
-      },
-
-      isMalware_: {
-        computed: 'computeIsMalware_(isDangerous_, data.danger_type)',
         type: Boolean,
         value: false,
       },
@@ -188,6 +193,15 @@ cr.define('downloads', function() {
     },
 
     /** @private */
+    computePauseOrResumeText_: function() {
+      if (this.isInProgress_)
+        return loadTimeData.getString('controlPause');
+      if (this.data.resume)
+        return loadTimeData.getString('controlResume');
+      return '';
+    },
+
+    /** @private */
     computeRemoveStyle_: function() {
       var canDelete = loadTimeData.getBoolean('allowDeletingHistory');
       var hideRemove = this.isDangerous_ || this.showCancel_ || !canDelete;
@@ -276,18 +290,16 @@ cr.define('downloads', function() {
     },
 
     /** @private */
-    onPauseTap_: function() {
-      downloads.ActionService.getInstance().pause(this.data.id);
+    onPauseOrResumeTap_: function() {
+      if (this.isInProgress_)
+        downloads.ActionService.getInstance().pause(this.data.id);
+      else
+        downloads.ActionService.getInstance().resume(this.data.id);
     },
 
     /** @private */
     onRemoveTap_: function() {
       downloads.ActionService.getInstance().remove(this.data.id);
-    },
-
-    /** @private */
-    onResumeTap_: function() {
-      downloads.ActionService.getInstance().resume(this.data.id);
     },
 
     /** @private */
