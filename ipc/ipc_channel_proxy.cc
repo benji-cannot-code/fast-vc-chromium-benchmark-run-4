5e2476eaf7b230a809588a272383b57059448b04
amistry <amistry@chromium.org>
2016-07-18 19:52:31 -0700

    Fix data race in ChannelProxy::GetPeerPID().
    In some cases, GetPeerPID() can be accessed on either the listener
    or IPC thread.
    
    BUG=616980
    
    Review-Url: https://codereview.chromium.org/2147933005
    Cr-Commit-Position: refs/heads/master@{#406196}

--- ipc/ipc_channel_proxy.cc
+++ ipc/ipc_channel_proxy.cc
@@ -121,7 +121,10 @@ bool ChannelProxy::Context::OnMessageReceivedNoFilter(const Message& message) {
 // Called on the IPC::Channel thread
 void ChannelProxy::Context::OnChannelConnected(int32_t peer_pid) {
   // We cache off the peer_pid so it can be safely accessed from both threads.
-  peer_pid_ = channel_->GetPeerPID();
+  {
+    base::AutoLock l(peer_pid_lock_);
+    peer_pid_ = channel_->GetPeerPID();
+  }
 
   // Add any pending filters.  This avoids a race condition where someone
   // creates a ChannelProxy, calls AddFilter, and then right after starts the
@@ -233,6 +236,8 @@ void ChannelProxy::Context::OnAddFilter() {
   // sure that channel_ is valid yet. When OnChannelConnected *is* called,
   // it invokes OnAddFilter, so any pending filter(s) will be added at that
   // time.
+  // No lock necessary for |peer_pid_| because it is only modified on this
+  // thread.
   if (peer_pid_ == base::kNullProcessId)
     return;
 
@@ -256,6 +261,8 @@ void ChannelProxy::Context::OnAddFilter() {
 
 // Called on the IPC::Channel thread
 void ChannelProxy::Context::OnRemoveFilter(MessageFilter* filter) {
+  // No lock necessary for |peer_pid_| because it is only modified on this
+  // thread.
   if (peer_pid_ == base::kNullProcessId) {
     // The channel is not yet connected, so any filters are still pending.
     base::AutoLock auto_lock(pending_filters_lock_);
@@ -337,9 +344,14 @@ void ChannelProxy::Context::OnDispatchConnected() {
     }
   }
 
+  base::ProcessId peer_pid;
+  {
+    base::AutoLock l(peer_pid_lock_);
+    peer_pid = peer_pid_;
+  }
   channel_connected_called_ = true;
   if (listener_)
-    listener_->OnChannelConnected(peer_pid_);
+    listener_->OnChannelConnected(peer_pid);
 }
 
 // Called on the listener's thread
@@ -568,6 +580,7 @@ void ChannelProxy::ClearIPCTaskRunner() {
 }
 
 base::ProcessId ChannelProxy::GetPeerPID() const {
+  base::AutoLock l(context_->peer_pid_lock_);
   return context_->peer_pid_;
 }
 
