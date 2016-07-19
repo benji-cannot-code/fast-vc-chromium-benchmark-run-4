004137ee1785b005a08451ea522d67ede173511d
pke <pke@google.com>
2016-07-19 07:23:54 -0700

    Use pipe as ID separator and add inverse functions for MakeUniqueID
    A ContentSuggestionsProvider needs to be able to split up the
    suggestion_id it retrieves in DiscardSuggestion and FetchSuggestionImage
    into the category and the within_category_id that it provided to
    MakeUniqueID when creating the ContentSuggestion.
    Add two static helper functions to ContentSuggestionsProvider that
    do the splitting.
    
    Use the pipe "|" character instead of a colon to separate the category
    from the within_category_id to prevent mistakes when implementing a
    ContentSuggestionsProvider: Accidentally splitting a URL (which might
    already be the within_category_id) instead of the unique_id fails
    silently because URLs contain a colon. Using a different separator
    prevents this.
    
    BUG=619560
    
    Review-Url: https://codereview.chromium.org/2155243003
    Cr-Commit-Position: refs/heads/master@{#406268}

--- components/ntp_snippets/content_suggestions_provider.cc
+++ components/ntp_snippets/content_suggestions_provider.cc
@@ -5,13 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/ntp_snippets/content_suggestions_provider.h"
 
+#include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 
 namespace ntp_snippets {
 
 namespace {
 
-const char kCombinedIDFormat[] = "%d:%s";
+const char kCombinedIDFormat[] = "%d|%s";
+const char kSeparator = '|';
 
 }  // namespace
 
@@ -21,12 +23,36 @@ ContentSuggestionsProvider::ContentSuggestionsProvider(
 
 ContentSuggestionsProvider::~ContentSuggestionsProvider() {}
 
+// static
 std::string ContentSuggestionsProvider::MakeUniqueID(
     ContentSuggestionsCategory category,
     const std::string& within_category_id) {
   return base::StringPrintf(kCombinedIDFormat,
                             static_cast<int>(category),
                             within_category_id.c_str());
+}
+
+// static
+ContentSuggestionsCategory ContentSuggestionsProvider::GetCategoryFromUniqueID(
+    const std::string& unique_id) {
+  size_t colon_index = unique_id.find(kSeparator);
+  DCHECK_NE(std::string::npos, colon_index) << "Not a valid unique_id: "
+                                            << unique_id;
+  int category = -1;
+  DCHECK(base::StringToInt(unique_id.substr(0, colon_index), &category))
+      << "Non-numeric category part in unique_id: " << unique_id;
+  DCHECK(0 <= category && category < int(ContentSuggestionsCategory::COUNT))
+      << "Category does not exist: " << category;
+  return ContentSuggestionsCategory(category);
+}
+
+// static
+std::string ContentSuggestionsProvider::GetWithinCategoryIDFromUniqueID(
+    const std::string& unique_id) {
+  size_t colon_index = unique_id.find(kSeparator);
+  DCHECK_NE(std::string::npos, colon_index) << "Not a valid unique_id: "
+                                            << unique_id;
+  return unique_id.substr(colon_index + 1);
 }
 
 }  // namespace ntp_snippets
