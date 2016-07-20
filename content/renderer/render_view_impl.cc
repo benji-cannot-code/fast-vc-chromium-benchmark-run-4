a16404f1b619572396abb0bafa64259f14e08c02
lfg <lfg@chromium.org>
2016-07-19 21:27:14 -0700

    Move allowsBrokenNullLayerTreeView calls from WebViewClient to WebWidgetClient.
    This is part of a refactor to split WebView/WebWidget.
    
    BUG=419087
    
    Review-Url: https://codereview.chromium.org/2121373002
    Cr-Commit-Position: refs/heads/master@{#406485}

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -2002,10 +2002,6 @@ void RenderViewImpl::initializeLayerTreeView() {
   }
 }
 
-bool RenderViewImpl::allowsBrokenNullLayerTreeView() const {
-  return RenderWidget::allowsBrokenNullLayerTreeView();
-}
-
 void RenderViewImpl::closeWidgetSoon() {
   RenderWidget::closeWidgetSoon();
 }
@@ -2094,6 +2090,10 @@ blink::WebRect RenderViewImpl::windowRect() {
 
 blink::WebRect RenderViewImpl::windowResizerRect() {
   return RenderWidget::windowResizerRect();
+}
+
+blink::WebWidgetClient* RenderViewImpl::widgetClient() {
+  return static_cast<RenderWidget*>(this);
 }
 
 // blink::WebFrameClient -----------------------------------------------------
