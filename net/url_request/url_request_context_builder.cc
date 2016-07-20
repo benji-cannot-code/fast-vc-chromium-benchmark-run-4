70d383fb3a32af0e5b317cca759cc9e495dfe4f4
rsleevi <rsleevi@chromium.org>
2016-07-19 19:03:36 -0700

    URLRequestContextBuilder should initialize a default CT state
    When using a URLRequestContextBuilder, the URLRequestContext
    it built would not be initialized with any Certificate
    Transparency logs. This is because CT logs are a policy of the
    embedder. Without any CT logs, all certificates naturally fail
    any CT policies the embedder has set.
    
    With no way to override this on the URLRequestContextBuilder,
    any CA which has CT compliance enforced on it will be rejected.
    This is because per-CA policies are enforced at the //net layer
    as part of a 'secure by default' policy, and thus fail closed.
    
    The result is that embedders that used URLRequestContextBuilder,
    such as Android WebView, rather than building URLRequestContexts
    themselves, such as Chrome does, would reject certs from some
    CAs. While this could be overridden by manipulating the
    TransportSecurityState of the URLRequestContext that was built,
    doing so would result in a less-secure solution.
    
    This CL mitigates it two-fold, by allowing the embedder to specify
    their own CTVerifier to handle evaluating Certificate Transparency
    information, and if they fail to, by initializing with the set
    of logs known to //net and trusted by Chrome according to the
    Chrome CT policy. This gives embedders a sane default, while still
    allowing them to override with their own application policies.
    
    This doesn't fully bootstrap a CT enforcement for embedders, as
    things like consistency proof fetching are concepts that extend
    beyond the URLRequestContext (and handled by the IOThread in
    Chrome), but offers a reasonable baseline. This also helps ensure
    things like Expect-CT work consistently for embedders.
    
    BUG=628421
    
    Review-Url: https://codereview.chromium.org/2161983003
    Cr-Commit-Position: refs/heads/master@{#406455}

--- net/url_request/url_request_context_builder.cc
+++ net/url_request/url_request_context_builder.cc
@@ -22,7 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/network_delegate_impl.h"
 #include "net/base/sdch_manager.h"
 #include "net/cert/cert_verifier.h"
+#include "net/cert/ct_known_logs.h"
+#include "net/cert/ct_log_verifier.h"
 #include "net/cert/ct_policy_enforcer.h"
+#include "net/cert/ct_verifier.h"
 #include "net/cert/multi_log_ct_verifier.h"
 #include "net/cookies/cookie_monster.h"
 #include "net/dns/host_resolver.h"
@@ -250,6 +253,11 @@ void URLRequestContextBuilder::SetSpdyAndQuicEnabled(bool spdy_enabled,
   http_network_session_params_.enable_quic = quic_enabled;
 }
 
+void URLRequestContextBuilder::set_ct_verifier(
+    std::unique_ptr<CTVerifier> ct_verifier) {
+  ct_verifier_ = std::move(ct_verifier);
+}
+
 void URLRequestContextBuilder::SetCertVerifier(
     std::unique_ptr<CertVerifier> cert_verifier) {
   cert_verifier_ = std::move(cert_verifier);
@@ -388,8 +396,14 @@ std::unique_ptr<URLRequestContext> URLRequestContextBuilder::Build() {
     storage->set_cert_verifier(CertVerifier::CreateDefault());
   }
 
-  storage->set_cert_transparency_verifier(
-      base::MakeUnique<MultiLogCTVerifier>());
+  if (ct_verifier_) {
+    storage->set_cert_transparency_verifier(std::move(ct_verifier_));
+  } else {
+    std::unique_ptr<MultiLogCTVerifier> ct_verifier =
+        base::MakeUnique<MultiLogCTVerifier>();
+    ct_verifier->AddLogs(ct::CreateLogVerifiersForKnownLogs());
+    storage->set_cert_transparency_verifier(std::move(ct_verifier));
+  }
   storage->set_ct_policy_enforcer(base::MakeUnique<CTPolicyEnforcer>());
 
   if (throttling_enabled_) {
