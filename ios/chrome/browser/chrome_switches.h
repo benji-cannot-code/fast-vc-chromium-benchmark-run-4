e5c9a605a71ff20db6be3d1019f95e5a3b3f9c75
gchatz <gchatz@chromium.org>
2016-07-20 09:22:05 -0700

    Remove flags for Tab Eviction experiment.
    Remove the flags for the Tab Eviction experiment, since they are now
    unused.
    
    BUG=629644
    
    Review-Url: https://codereview.chromium.org/2165883002
    Cr-Commit-Position: refs/heads/master@{#406576}

--- ios/chrome/browser/chrome_switches.h
+++ ios/chrome/browser/chrome_switches.h
@@ -20,7 +20,6 @@ extern const char kDisableIOSWKWebView[];
 extern const char kDisableLRUSnapshotCache[];
 extern const char kDisableNTPFavicons[];
 extern const char kDisableOfflineAutoReload[];
-extern const char kDisableTabEviction[];
 extern const char kDisableTabSwitcher[];
 extern const char kDisableIOSPhysicalWeb[];
 
@@ -37,7 +36,6 @@ extern const char kEnableNTPFavicons[];
 extern const char kEnableOfflineAutoReload[];
 extern const char kEnableQRScanner[];
 extern const char kEnableReaderModeToolbarIcon[];
-extern const char kEnableTabEviction[];
 extern const char kEnableTabSwitcher[];
 extern const char kEnableIOSPhysicalWeb[];
 
