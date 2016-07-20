fad6e05593d0738fde5873ab5b3ce5acd3703c35
baxley <baxley@chromium.org>
2016-07-20 14:37:31 -0700

    Use include instead of import for <map> in http_server_test_util.h
    map should be included, rather than imported.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2163373003
    Cr-Commit-Position: refs/heads/master@{#406675}

--- ios/web/public/test/http_server_util.h
+++ ios/web/public/test/http_server_util.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_WEB_PUBLIC_TEST_HTTP_SERVER_UTIL_H_
 #define IOS_WEB_PUBLIC_TEST_HTTP_SERVER_UTIL_H_
 
-#import <map>
+#include <map>
 
 #include "url/gurl.h"
 
