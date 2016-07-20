5d096ac2acd2466c5beb9dec3a6f2ef297f640d9
achuith <achuith@chromium.org>
2016-07-20 00:27:17 -0700

    Revert of Mojo interface/service for Leak Detector on remote process (patchset #20 id:400001 of https://codereview.chromium.org/2064463002/ )
    Reason for revert:
    crbug.com/629760
    
    Original issue's description:
    > Mojo interface/service for Leak Detector on remote process
    >
    > This interface allows an instance of the Runtime Memory Leak Detector
    > (components/metrics/leak_detector) to run on a non-browser process, such
    > as a tab (renderer) process. The interface provides two function APIs:
    >
    > - GetParams(): Returns the profiling parameters used for initializing
    >   LeakDetector.
    > - SendLeakReports(): Passes an array of leak reports to the browser process,
    >   where they can be attached to the UMA protobuf.
    >
    > Also added an implementation of the Mojo interface,
    > LeakDetectorRemoteController, that implements the Mojo interface
    > LeakDetectorRemote. This class provides a bridge between the browser and
    > renderer processes. It also allows a single instance of
    > LeakDetectorController to provide leak detector params to the renderer
    > process and receive leak reports from the renderer process.
    >
    > BUG=chromium:615223
    > TEST=Build successfully, unit tests pass
    > R=wfh@chromium.org
    >
    > Committed: https://crrev.com/c21a6e92a02f064431b22aa52b136f4682778c06
    > Cr-Commit-Position: refs/heads/master@{#406466}
    
    TBR=jochen@chromium.org,ben@chromium.org,dcheng@chromium.org,isherman@chromium.org,wfh@chromium.org,sque@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=chromium:615223
    
    Review-Url: https://codereview.chromium.org/2166553003
    Cr-Commit-Position: refs/heads/master@{#406511}

--- chrome/browser/metrics/leak_detector/leak_detector_controller.h
+++ chrome/browser/metrics/leak_detector/leak_detector_controller.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/threading/thread_checker.h"
-#include "chrome/browser/metrics/leak_detector/leak_detector_remote_controller.h"
 #include "components/metrics/leak_detector/leak_detector.h"
 #include "components/metrics/proto/memory_leak_report.pb.h"
 
@@ -18,9 +17,7 @@ namespace metrics {
 
 // This class initializes the LeakDetector on the browser process and registers
 // itself to be notified of leak reports.
-class LeakDetectorController
-    : public LeakDetector::Observer,
-      public LeakDetectorRemoteController::LocalController {
+class LeakDetectorController : public LeakDetector::Observer {
  public:
   LeakDetectorController();
   ~LeakDetectorController() override;
@@ -33,19 +30,7 @@ class LeakDetectorController
   // LeakDetector::Observer:
   void OnLeaksFound(const std::vector<MemoryLeakReportProto>& reports) override;
 
-  // LeakDetectorRemoteController::LocalController:
-  MemoryLeakReportProto::Params GetParams() const override;
-  void SendLeakReports(
-      const std::vector<MemoryLeakReportProto>& reports) override;
-  void OnRemoteProcessShutdown() override;
-
  private:
-  // Stores a given array of leak reports in |stored_reports_|. |process_type|
-  // is the type of process that generated these reports. The reports must all
-  // come from the same process type.
-  void StoreLeakReports(const std::vector<MemoryLeakReportProto>& reports,
-                        MemoryLeakReportProto::ProcessType process_type);
-
   // All leak reports received through OnLeakFound() are stored in protobuf
   // format.
   std::vector<MemoryLeakReportProto> stored_reports_;
