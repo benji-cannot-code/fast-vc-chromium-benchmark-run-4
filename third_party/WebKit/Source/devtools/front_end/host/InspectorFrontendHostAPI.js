b59d893e4cd100ce8b4306198d3df8e2a0dbf93b
einbinder <einbinder@chromium.org>
2016-07-20 16:34:49 -0700

    DevTools: Fix presubmits failing on InspectorFrontendHostAPI
    Review-Url: https://codereview.chromium.org/2161033005
    Cr-Commit-Position: refs/heads/master@{#406709}

--- third_party/WebKit/Source/devtools/front_end/host/InspectorFrontendHostAPI.js
+++ third_party/WebKit/Source/devtools/front_end/host/InspectorFrontendHostAPI.js
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 /** @interface */
-window.InspectorFrontendHostAPI = function()
+function InspectorFrontendHostAPI()
 {
 }
-
+window.InspectorFrontendHostAPI = InspectorFrontendHostAPI;
 /** @typedef
 {{
     type: string,
