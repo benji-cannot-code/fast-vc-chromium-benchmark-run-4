1d86efec2c892e7a23f041f4de9ae2f94d0eff97
jamescook <jamescook@chromium.org>
2016-07-20 15:45:29 -0700

    mash: Move shelf_navigator.* to //ash/common/shelf
    Mechanical move, no functional changes
    
    BUG=615155
    TEST=compiles
    
    Review-Url: https://codereview.chromium.org/2169653002
    Cr-Commit-Position: refs/heads/master@{#406700}

--- ash/shelf/shelf_widget.cc
+++ ash/shelf/shelf_widget.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/wm_shell.h"
 #include "ash/shelf/shelf_delegate.h"
 #include "ash/shelf/shelf_layout_manager.h"
-#include "ash/shelf/shelf_navigator.h"
 #include "ash/shelf/shelf_util.h"
 #include "ash/shelf/shelf_view.h"
 #include "ash/shell.h"
