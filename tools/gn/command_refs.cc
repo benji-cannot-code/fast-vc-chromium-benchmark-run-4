bea281737c2d1de60edfcfc69fba6010e20fe27c
dpranke <dpranke@chromium.org>
2016-07-20 15:21:54 -0700

    Make `gn refs` handle files in data directories properly.
    If a target specified `data = [ "test/" ]` and you ran
    `gn refs out/Default //foo //foo/test/bar.cc`, gn refs would
    incorrectly think that nothing referenced bar.cc. This patch
    fixes that.
    
    R=brettw@chromium.org
    BUG=628783
    
    Review-Url: https://codereview.chromium.org/2160383002
    Cr-Commit-Position: refs/heads/master@{#406689}

--- tools/gn/command_refs.cc
+++ tools/gn/command_refs.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/file_util.h"
 #include "base/strings/string_split.h"
+#include "base/strings/string_util.h"
 #include "tools/gn/commands.h"
 #include "tools/gn/deps_iterator.h"
 #include "tools/gn/filesystem_utils.h"
@@ -143,6 +144,9 @@ bool TargetContainsFile(const Target* target, const SourceFile& file) {
   }
   for (const auto& cur_file : target->data()) {
     if (cur_file == file.value())
+      return true;
+    if (cur_file.back() == '/' &&
+        base::StartsWith(file.value(), cur_file, base::CompareCase::SENSITIVE))
       return true;
   }
 
