300821e1f308e9ca393cb69b00e695d241ecc023
ssid <ssid@chromium.org>
2016-07-20 10:26:23 -0700

    [telemetry] Trace config should be used to disable periodic dumps
    Telemetry previously used the "--enable-memory-benchmarking" flag to
    tell memory dump manager to disable periodic dumps. That is cleaned up
    in this CL and the trace config is used to achieve this behavior.
    
    Note to perf sheriff: Memory benchmarks could regress if the config
    did not propagate correctly.
    
    BUG=513692
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.perf:android_s5_perf_cq;master.tryserver.chromium.perf:mac_retina_perf_cq
    
    Review-Url: https://codereview.chromium.org/2162283002
    Cr-Commit-Position: refs/heads/master@{#406598}

--- tools/perf/benchmarks/oortonline.py
+++ tools/perf/benchmarks/oortonline.py
@@ -11,6 +11,7 @@ from telemetry.page import legacy_page_test
 from telemetry.value import scalar
 from telemetry.value import improvement_direction
 from telemetry.timeline import chrome_trace_category_filter
+from telemetry.timeline import chrome_trace_config
 from telemetry.web_perf import timeline_based_measurement
 from telemetry.web_perf.metrics import v8_gc_latency
 from telemetry.web_perf.metrics import smoothness
@@ -60,13 +61,6 @@ class OortOnlineTBM(perf_benchmark.PerfBenchmark):
   Info: http://v8project.blogspot.de/2015/10/jank-busters-part-one.html
   """
 
-  def SetExtraBrowserOptions(self, options):
-    options.AppendExtraBrowserArgs([
-        # TODO(perezju): Temporary workaround to disable periodic memory dumps.
-        # See: http://crbug.com/513692
-        '--enable-memory-benchmarking',
-    ])
-
   def CreateStorySet(self, options):
     return page_sets.OortOnlineTBMPageSet()
 
@@ -85,6 +79,9 @@ class OortOnlineTBM(perf_benchmark.PerfBenchmark):
     options.SetLegacyTimelineBasedMetrics([v8_gc_latency.V8GCLatency(),
                                      smoothness.SmoothnessMetric(),
                                      memory_timeline.MemoryTimelineMetric()])
+    # Setting an empty memory dump config disables periodic dumps.
+    options.config.chrome_trace_config.SetMemoryDumpConfig(
+        chrome_trace_config.MemoryDumpConfig())
     return options
 
   @classmethod
