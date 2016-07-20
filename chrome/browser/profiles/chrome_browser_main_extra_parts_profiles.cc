5e353a56c945d90f4957778af4d4100d205a2379
ryansturm <ryansturm@chromium.org>
2016-07-20 11:56:45 -0700

    Exposing NQE on the Browser UI thread
    This involves an IO base observer which passes messages to the UI thread
    using a KeyedService. Consumers should be able to consume this similarly
    to the way the NQE is already consumed.
    
    BUG=615563
    
    Review-Url: https://codereview.chromium.org/2103323007
    Cr-Commit-Position: refs/heads/master@{#406630}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/invalidation/profile_invalidation_provider_factory.h"
 #include "chrome/browser/media_galleries/media_galleries_preferences_factory.h"
+#include "chrome/browser/net/nqe/ui_network_quality_estimator_service_factory.h"
 #include "chrome/browser/notifications/extension_welcome_notification_factory.h"
 #include "chrome/browser/notifications/notifier_state_tracker_factory.h"
 #include "chrome/browser/ntp_snippets/ntp_snippets_service_factory.h"
@@ -194,6 +195,7 @@ EnsureBrowserContextKeyedServiceFactoriesBuilt() {
 #if defined(OS_ANDROID)
   chrome::android::DataUseUITabModelFactory::GetInstance();
 #endif
+  UINetworkQualityEstimatorServiceFactory::GetInstance();
 #if defined(ENABLE_PRINT_PREVIEW)
   CloudPrintProxyServiceFactory::GetInstance();
 #endif
