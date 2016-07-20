0cdaee674a9576ddfc8302520e207504776fe779
mostynb <mostynb@opera.com>
2016-07-20 13:34:38 -0700

    Remove WebContents::InsertCSS since it is unused
    This looks like it hasn't been used since CL923463003 landed
    over a year ago.
    
    BUG=331654
    
    Review-Url: https://codereview.chromium.org/2157153002
    Cr-Commit-Position: refs/heads/master@{#406656}

--- content/public/android/java/src/org/chromium/content_public/browser/WebContents.java
+++ content/public/android/java/src/org/chromium/content_public/browser/WebContents.java
@@ -110,11 +110,6 @@ public interface WebContents extends Parcelable {
     void unselect();
 
     /**
-     * Inserts css into main frame's document.
-     */
-    void insertCSS(String css);
-
-    /**
      * To be called when the ContentView is hidden.
      */
     void onHide();
