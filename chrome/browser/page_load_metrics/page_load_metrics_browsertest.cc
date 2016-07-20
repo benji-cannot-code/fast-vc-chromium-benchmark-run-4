cbecda3e01ab177ddb2fb4aa65c2cf4250ccfb3f
bmcquade <bmcquade@chromium.org>
2016-07-19 18:56:17 -0700

    Refactor PageLoadMetricsObserver completion callback policy
    This patch modifies PageLoadMetricsObserver to invoke OnComplete for tracked
    loads that committed, and OnFailedProvisionalLoad for tracked loads that did
    not commit.
    
    This makes it more straightforward for implementers to reason about which method
    to override, and encourages separation of code for the different states into
    methods which makes observer code more maintainable.
    
    This patch also refactors the various observers to implement these methods as
    needed.
    
    BUG=627585
    
    Review-Url: https://codereview.chromium.org/2152683004
    Cr-Commit-Position: refs/heads/master@{#406454}

--- chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/histogram_tester.h"
 #include "chrome/browser/page_load_metrics/observers/core_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/document_write_page_load_metrics_observer.h"
+#include "chrome/common/url_constants.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
@@ -25,6 +26,7 @@ class MetricsWebContentsObserverBrowserTest : public InProcessBrowserTest {
 IN_PROC_BROWSER_TEST_F(MetricsWebContentsObserverBrowserTest, NoNavigation) {
   ASSERT_TRUE(embedded_test_server()->Start());
 
+  histogram_tester_.ExpectTotalCount(internal::kHistogramCommit, 0);
   histogram_tester_.ExpectTotalCount(internal::kHistogramDomContentLoaded, 0);
   histogram_tester_.ExpectTotalCount(internal::kHistogramLoad, 0);
   histogram_tester_.ExpectTotalCount(internal::kHistogramFirstLayout, 0);
@@ -60,6 +62,47 @@ IN_PROC_BROWSER_TEST_F(MetricsWebContentsObserverBrowserTest,
   histogram_tester_.ExpectTotalCount(internal::kHistogramDomContentLoaded, 1);
   histogram_tester_.ExpectTotalCount(internal::kHistogramLoad, 1);
   histogram_tester_.ExpectTotalCount(internal::kHistogramFirstLayout, 1);
+}
+
+IN_PROC_BROWSER_TEST_F(MetricsWebContentsObserverBrowserTest,
+                       SameUrlNavigation) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+
+  ui_test_utils::NavigateToURL(browser(),
+                               embedded_test_server()->GetURL("/title1.html"));
+  ui_test_utils::NavigateToURL(browser(),
+                               embedded_test_server()->GetURL("/title1.html"));
+  ui_test_utils::NavigateToURL(browser(),
+                               embedded_test_server()->GetURL("/title2.html"));
+
+  // We expect one histogram sample for each navigation to title1.html.
+  histogram_tester_.ExpectTotalCount(internal::kHistogramCommit, 2);
+  histogram_tester_.ExpectTotalCount(internal::kHistogramDomContentLoaded, 2);
+  histogram_tester_.ExpectTotalCount(internal::kHistogramLoad, 2);
+  histogram_tester_.ExpectTotalCount(internal::kHistogramFirstLayout, 2);
+}
+
+IN_PROC_BROWSER_TEST_F(MetricsWebContentsObserverBrowserTest,
+                       NonHtmlMainResource) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+
+  ui_test_utils::NavigateToURL(browser(),
+                               embedded_test_server()->GetURL("/simple.svg"));
+  ui_test_utils::NavigateToURL(browser(),
+                               embedded_test_server()->GetURL("/title1.html"));
+
+  histogram_tester_.ExpectTotalCount(internal::kHistogramCommit, 0);
+}
+
+IN_PROC_BROWSER_TEST_F(MetricsWebContentsObserverBrowserTest,
+                       NonHttpOrHttpsUrl) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+
+  ui_test_utils::NavigateToURL(browser(), GURL(chrome::kChromeUIVersionURL));
+  ui_test_utils::NavigateToURL(browser(),
+                               embedded_test_server()->GetURL("/title1.html"));
+
+  histogram_tester_.ExpectTotalCount(internal::kHistogramCommit, 0);
 }
 
 IN_PROC_BROWSER_TEST_F(MetricsWebContentsObserverBrowserTest,
@@ -149,7 +192,7 @@ IN_PROC_BROWSER_TEST_F(MetricsWebContentsObserverBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(MetricsWebContentsObserverBrowserTest,
-                       DocumentWriteASync) {
+                       DocumentWriteAsync) {
   ASSERT_TRUE(embedded_test_server()->Start());
 
   ui_test_utils::NavigateToURL(
