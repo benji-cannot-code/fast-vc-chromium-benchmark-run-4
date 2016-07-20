41fb491a91c76e0cb3800b3c5ea8d0377d21a332
lshang <lshang@chromium.org>
2016-07-20 16:49:44 -0700

    MD History: Add shortcuts for 'Delete' and 'Backspace' to delete selected items
    Press 'Delete' or 'Backspace' key from keyboard will delete selected history items
    on MD History.
    
    BUG=627408
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2159773003
    Cr-Commit-Position: refs/heads/master@{#406715}

--- chrome/browser/resources/md_history/app.js
+++ chrome/browser/resources/md_history/app.js
@@ -160,6 +160,9 @@ Polymer({
             !(this.$.toolbar.searchBar.showingSearch &&
               this.$.toolbar.searchBar.isSearchFocused());
         break;
+      case 'delete-command':
+        e.canExecute = this.$.toolbar.count > 0;
+        break;
     }
   },
 
@@ -187,6 +190,8 @@ Polymer({
   onCommand_: function(e) {
     if (e.command.id == 'find-command' || e.command.id == 'slash-command')
       this.$.toolbar.showSearchField();
+    if (e.command.id == 'delete-command')
+      this.deleteSelected();
   },
 
   /**
