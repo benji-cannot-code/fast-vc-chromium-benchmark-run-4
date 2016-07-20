fd70e2c352cc28802e550ef1715a2268543d1a72
hayato <hayato@chromium.org>
2016-07-19 18:35:33 -0700

    Revert of Renaming Gnubby and RemoteSecurityKey files/classes/members (patchset #3 id:40001 of https://codereview.chromium.org/2162083003/ )
    Reason for revert:
    Build break:
    https://build.chromium.org/p/chromium.mac/builders/Mac%20GYP%20%28dbg%29/builds/1194
    
    Original issue's description:
    > Renaming Gnubby and RemoteSecurityKey files/classes/members
    >
    > This change is a simple refactoring to rename the 'Gnubby' and
    > 'RemoteSecurityKey' file/class names/comments/etc to be consistent.
    > I've chosen to use 'SecurityKey' as it is shorter that RemoteSecurityKey
    > (which was causing too many 80 char limit issues).  No functional
    > changes are expected as a result of this change.
    >
    > Note: Not every instance of Gnubby or RemoteSecurityKey has been
    > changed.  The registry key used for toggling this functionality still
    > uses 'gnubby'.  I don't want to change that here.  Also, the process
    > used on the host machine is still called remote_security_key so classes
    > associated with that process were not renamed (neither were the IPC
    > messages used by it).
    >
    > BUG=593736
    > TBR=jochen@chromium.org
    >
    > Committed: https://crrev.com/281516541a3aa129b5a23c29cdfb926c99f3d516
    > Cr-Commit-Position: refs/heads/master@{#406435}
    
    TBR=sergeyu@chromium.org,joedow@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=593736
    
    Review-Url: https://codereview.chromium.org/2167473003
    Cr-Commit-Position: refs/heads/master@{#406440}

--- remoting/host/remoting_me2me_host.cc
+++ remoting/host/remoting_me2me_host.cc
@@ -73,8 +73,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/pairing_registry_delegate.h"
 #include "remoting/host/pin_hash.h"
 #include "remoting/host/policy_watcher.h"
-#include "remoting/host/security_key/security_key_auth_handler.h"
-#include "remoting/host/security_key/security_key_extension.h"
+#include "remoting/host/security_key/gnubby_auth_handler.h"
+#include "remoting/host/security_key/gnubby_extension.h"
 #include "remoting/host/service_urls.h"
 #include "remoting/host/shutdown_watchdog.h"
 #include "remoting/host/signaling_connector.h"
@@ -152,7 +152,7 @@ const char kStdinConfigPath[] = "-";
 const char kAudioPipeSwitchName[] = "audio-pipe-name";
 
 // The command line switch used to pass name of the unix domain socket used to
-// listen for security key requests.
+// listen for gnubby requests.
 const char kAuthSocknameSwitchName[] = "ssh-auth-sockname";
 #endif  // defined(OS_LINUX)
 
@@ -375,8 +375,8 @@ class HostProcess : public ConfigWatcher::Delegate,
 
   bool curtain_required_ = false;
   ThirdPartyAuthConfig third_party_auth_config_;
-  bool security_key_auth_policy_enabled_ = false;
-  bool security_key_extension_supported_ = false;
+  bool gnubby_auth_policy_enabled_ = false;
+  bool gnubby_extension_supported_ = false;
 
   // Boolean to change flow, where necessary, if we're
   // capturing a window instead of the entire desktop.
@@ -817,17 +817,15 @@ void HostProcess::StartOnUiThread() {
         context_->audio_task_runner(), audio_pipe_name);
   }
 
-  base::FilePath security_key_socket_name =
-      base::CommandLine::ForCurrentProcess()->GetSwitchValuePath(
-          kAuthSocknameSwitchName);
-  if (!security_key_socket_name.empty()) {
-    remoting::SecurityKeyAuthHandler::SetSecurityKeySocketName(
-        security_key_socket_name);
-    security_key_extension_supported_ = true;
+  base::FilePath gnubby_socket_name = base::CommandLine::ForCurrentProcess()->
+      GetSwitchValuePath(kAuthSocknameSwitchName);
+  if (!gnubby_socket_name.empty()) {
+    remoting::GnubbyAuthHandler::SetGnubbySocketName(gnubby_socket_name);
+    gnubby_extension_supported_ = true;
   }
 #elif defined(OS_WIN)
   // TODO(joedow): Remove the conditional once this is supported on OSX.
-  security_key_extension_supported_ = true;
+  gnubby_extension_supported_ = true;
 #endif  // defined(OS_WIN)
 
   // Create a desktop environment factory appropriate to the build type &
@@ -1320,14 +1318,14 @@ bool HostProcess::OnGnubbyAuthPolicyUpdate(base::DictionaryValue* policies) {
   DCHECK(context_->network_task_runner()->BelongsToCurrentThread());
 
   if (!policies->GetBoolean(policy::key::kRemoteAccessHostAllowGnubbyAuth,
-                            &security_key_auth_policy_enabled_)) {
+                            &gnubby_auth_policy_enabled_)) {
     return false;
   }
 
-  if (security_key_auth_policy_enabled_) {
-    HOST_LOG << "Policy enables security key auth.";
+  if (gnubby_auth_policy_enabled_) {
+    HOST_LOG << "Policy enables gnubby auth.";
   } else {
-    HOST_LOG << "Policy disables security key auth.";
+    HOST_LOG << "Policy disables gnubby auth.";
   }
 
   return true;
@@ -1461,8 +1459,8 @@ void HostProcess::StartHost() {
                                  context_->audio_task_runner(),
                                  context_->video_encode_task_runner()));
 
-  if (security_key_auth_policy_enabled_ && security_key_extension_supported_) {
-    host_->AddExtension(base::WrapUnique(new SecurityKeyExtension()));
+  if (gnubby_auth_policy_enabled_ && gnubby_extension_supported_) {
+    host_->AddExtension(base::WrapUnique(new GnubbyExtension()));
   }
 
   // TODO(simonmorris): Get the maximum session duration from a policy.
