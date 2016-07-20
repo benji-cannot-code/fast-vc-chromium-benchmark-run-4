e1504a078dcd0659d01d38fac0eedfaef8cf6228
paulirish <paulirish@chromium.org>
2016-07-20 12:09:19 -0700

    DevTools: Change a few implicit globals to explicit globals.
    Review-Url: https://codereview.chromium.org/2156663002
    Cr-Commit-Position: refs/heads/master@{#406633}

--- third_party/WebKit/Source/devtools/front_end/host/InspectorFrontendHost.js
+++ third_party/WebKit/Source/devtools/front_end/host/InspectorFrontendHost.js
@@ -450,7 +450,7 @@ WebInspector.InspectorFrontendHostStub.prototype = {
 /**
  * @type {!InspectorFrontendHostAPI}
  */
-var InspectorFrontendHost = window.InspectorFrontendHost || null;
+window.InspectorFrontendHost = window.InspectorFrontendHost || null;
 
 (function(){
 
