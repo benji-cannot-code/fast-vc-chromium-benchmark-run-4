09d70dac256735dec03f439fa372ab9cd915a68c
kolos <kolos@chromium.org>
2016-07-20 07:37:49 -0700

    [Password Generation] Fix bug in sending the outcome of form classifier to the server
    The form classifier outcome was saved to AutofillField, but not copied to AutofillUploadContents. The bug was made in this CL (https://codereview.chromium.org/2055633003/)
    
    Also added an initialization of AutofillField.form_classifier_outcome_ to NO_OUTCOME in the constructors.
    
    BUG=582434
    
    Review-Url: https://codereview.chromium.org/2165893003
    Cr-Commit-Position: refs/heads/master@{#406552}

--- components/autofill/core/browser/autofill_field.cc
+++ components/autofill/core/browser/autofill_field.cc
@@ -490,7 +490,8 @@ AutofillField::AutofillField()
       phone_part_(IGNORED),
       credit_card_number_offset_(0),
       previously_autofilled_(false),
-      generation_type_(AutofillUploadContents::Field::NO_GENERATION) {}
+      generation_type_(AutofillUploadContents::Field::NO_GENERATION),
+      form_classifier_outcome_(AutofillUploadContents::Field::NO_OUTCOME) {}
 
 AutofillField::AutofillField(const FormFieldData& field,
                              const base::string16& unique_name)
@@ -504,7 +505,8 @@ AutofillField::AutofillField(const FormFieldData& field,
       credit_card_number_offset_(0),
       previously_autofilled_(false),
       parseable_name_(field.name),
-      generation_type_(AutofillUploadContents::Field::NO_GENERATION) {}
+      generation_type_(AutofillUploadContents::Field::NO_GENERATION),
+      form_classifier_outcome_(AutofillUploadContents::Field::NO_OUTCOME) {}
 
 AutofillField::~AutofillField() {}
 
