0cc8858766e2bfed24e9801d38e0f50a5753aef1
sashab <sashab@chromium.org>
2016-07-19 19:07:40 -0700

    Make CSSValuePair store const CSSValues
    Make CSSValuePair store const CSSValues, which involves making the
    constructor take and getter methods return const CSSValue*s. This is an
    entirely mechanical change that is more semantically correct and safe,
    and is pre-work for making CSSStyleValue::toCSSValue() return a const
    CSSValue.
    
    BUG=526586
    
    Review-Url: https://codereview.chromium.org/2158723002
    Cr-Commit-Position: refs/heads/master@{#406459}

--- third_party/WebKit/Source/core/css/CSSBasicShapeValues.cpp
+++ third_party/WebKit/Source/core/css/CSSBasicShapeValues.cpp
@@ -72,7 +72,7 @@ static String serializePositionOffset(const CSSValuePair& offset, const CSSValue
 static CSSValuePair* buildSerializablePositionOffset(CSSValue* offset, CSSValueID defaultSide)
 {
     CSSValueID side = defaultSide;
-    CSSPrimitiveValue* amount = nullptr;
+    const CSSPrimitiveValue* amount = nullptr;
 
     if (!offset) {
         side = CSSValueCenter;
