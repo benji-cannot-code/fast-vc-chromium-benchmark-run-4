3228348d42ee013828a47b74b8656bf86f507fe1
dgn <dgn@chromium.org>
2016-07-21 11:54:51 -0700

    [NTP Snippets] Adjust the card display depending on the screen width.
    Changes the lines to go from always 2 to at most 2 by default, so that
    we don't show empty lines on very large screens.
    For smaller screens, the title can go up to 4 lines, and we then hide
    the description.
    On bigger screens, we add space on the side of the cards
    
    Measures used:
    < 360dp: Narrow -> 4 lines title
    >= 360dp: Regular -> 2 + 2 lines
    >= 600dp: Wide -> 2 + 2 lines, 48dp gutters around the cards
    
    Preview: https://goo.gl/photos/prJ42tvP4jzwiCn3A
    BUG=625628, 624333
    
    Review-Url: https://codereview.chromium.org/2149333003
    Cr-Commit-Position: refs/heads/master@{#406923}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/DisplayStyleObserver.java
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ntp;
+
+/**
+ * Gets notified of changes in the display style.
+ *
+ * @see UiConfig.DisplayStyle
+ * @see UiConfig#getDisplayStyle()
+ * @see org.chromium.chrome.browser.ntp.cards.DisplayStyleObserverAdapter
+ */
+public interface DisplayStyleObserver {
+    void onDisplayStyleChanged(@UiConfig.DisplayStyle int newDisplayStyle);
+}
