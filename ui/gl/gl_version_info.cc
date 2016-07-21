115e206e28a6b83114a42d9014487298083279bc
junov <junov@chromium.org>
2016-07-21 11:56:02 -0700

    Fix gpu mipmap support on ChromeOS
    Fix gl version info check used to control texture internal
    format substitutions so that it does the right thing on
    ChromeOS.  Without this substitution we'd hit a Mesa bug
    that prevents mipmapping from working when the internal
    format is GL_BGRA
    
    BUG=540761
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2164723003
    Cr-Commit-Position: refs/heads/master@{#406924}

--- ui/gl/gl_version_info.cc
+++ ui/gl/gl_version_info.cc
@@ -46,6 +46,7 @@ GLVersionInfo::GLVersionInfo(const char* version_str,
 GLVersionInfo::GLVersionInfo()
     : is_es(false),
       is_angle(false),
+      is_mesa(false),
       major_version(0),
       minor_version(0),
       is_es2(false),
@@ -63,6 +64,8 @@ void GLVersionInfo::Initialize(const char* version_str,
   if (renderer_str) {
     is_angle = base::StartsWith(renderer_str, "ANGLE",
                                 base::CompareCase::SENSITIVE);
+    is_mesa = base::StartsWith(renderer_str, "Mesa",
+                               base::CompareCase::SENSITIVE);
   }
   is_desktop_core_profile =
       DesktopCoreCommonCheck(is_es, major_version, minor_version) &&
