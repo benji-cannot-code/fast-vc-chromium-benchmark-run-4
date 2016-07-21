8034504a0f0efb2fb96a1dbb699063df02da4907
piman <piman@chromium.org>
2016-07-20 17:33:23 -0700

    remove linked_ptr from gpu/
    Because unique_ptr and move semantics are more awesome.
    
    BUG=None
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2163093002
    Cr-Commit-Position: refs/heads/master@{#406728}

--- gpu/command_buffer/service/common_decoder.h
+++ gpu/command_buffer/service/common_decoder.h
@@ -11,11 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
-#include <stack>
 #include <string>
 
 #include "base/macros.h"
-#include "base/memory/linked_ptr.h"
 #include "gpu/command_buffer/common/buffer.h"
 #include "gpu/command_buffer/service/cmd_parser.h"
 #include "gpu/gpu_export.h"
@@ -199,7 +197,7 @@ class GPU_EXPORT CommonDecoder : NON_EXPORTED_BASE(public AsyncAPIInterface) {
   CommandBufferEngine* engine_;
   size_t max_bucket_size_;
 
-  typedef std::map<uint32_t, linked_ptr<Bucket>> BucketMap;
+  typedef std::map<uint32_t, std::unique_ptr<Bucket>> BucketMap;
   BucketMap buckets_;
 
   typedef Error (CommonDecoder::*CmdHandler)(uint32_t immediate_data_size,
@@ -216,6 +214,7 @@ class GPU_EXPORT CommonDecoder : NON_EXPORTED_BASE(public AsyncAPIInterface) {
   // A table of CommandInfo for all the commands.
   static const CommandInfo command_info[];
 
+  DISALLOW_COPY_AND_ASSIGN(CommonDecoder);
 };
 
 }  // namespace gpu
--- gpu/command_buffer/service/mailbox_manager_impl.h
+++ gpu/command_buffer/service/mailbox_manager_impl.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/macros.h"
-#include "base/memory/linked_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "gpu/command_buffer/common/constants.h"
 #include "gpu/command_buffer/common/mailbox.h"
