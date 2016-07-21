e336cba90102f27016dc463b1d90e089c785f2b7
riajiang <riajiang@chromium.org>
2016-07-21 16:22:09 -0700

    mash: Add touch-hud app.
    BUG=588311
    TEST=mash_unittests + manual
    
    Review-Url: https://codereview.chromium.org/2150933003
    Cr-Commit-Position: refs/heads/master@{#406986}

---
+++ ash/mus/root_window_controller_unittest.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/mus/bridge/wm_window_mus.h"
+#include "ash/mus/test/wm_test_base.h"
+
+namespace ash {
+
+class RootWindowControllerTest : public mus::WmTestBase {
+ public:
+  RootWindowControllerTest() {}
+  ~RootWindowControllerTest() override {}
+
+  WmWindow* CreateFullscreenTestWindow() {
+    return mus::WmWindowMus::Get(mus::WmTestBase::CreateFullscreenTestWindow());
+  }
+
+  WmWindow* GetPrimaryRootWindow() {
+    return mus::WmWindowMus::Get(mus::WmTestBase::GetPrimaryRootWindow());
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(RootWindowControllerTest);
+};
+
+TEST_F(RootWindowControllerTest, CreateFullscreenWindow) {
+  WmWindow* window = CreateFullscreenTestWindow();
+  WmWindow* root_window = GetPrimaryRootWindow();
+  EXPECT_EQ(root_window->GetBounds(), window->GetBounds());
+}
+
+}  // namespace ash
---
+++ ash/touch_hud/mus/main.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/touch_hud/mus/touch_hud_application.h"
+#include "mojo/public/c/system/main.h"
+#include "services/shell/public/cpp/service_runner.h"
+
+MojoResult MojoMain(MojoHandle shell_handle) {
+  shell::ServiceRunner runner(new ash::touch_hud::TouchHudApplication);
+  runner.set_message_loop_type(base::MessageLoop::TYPE_UI);
+  return runner.Run(shell_handle);
+}
