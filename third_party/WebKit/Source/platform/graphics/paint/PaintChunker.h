0cf9b0b87f1069981a64e59dd8c792ee4e1fdd3f
wangxianzhu <wangxianzhu@chromium.org>
2016-07-21 00:08:36 -0700

    Reland PaintChunk::id
    The id will be used in rasterization paint invalidation to match old
    and new paint chunks. It can be also used in incremental layerization.
    
    Original review url: https://codereview.chromium.org/2116693002
    
    BUG=510908,629946
    
    Review-Url: https://codereview.chromium.org/2116693002
    Review-Url: https://codereview.chromium.org/2168513005
    Cr-Original-Commit-Position: refs/heads/master@{#406461}
    Cr-Commit-Position: refs/heads/master@{#406793}

--- third_party/WebKit/Source/platform/graphics/paint/PaintChunker.h
+++ third_party/WebKit/Source/platform/graphics/paint/PaintChunker.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PaintChunker_h
 
 #include "platform/PlatformExport.h"
+#include "platform/graphics/paint/DisplayItem.h"
 #include "platform/graphics/paint/PaintChunk.h"
 #include "platform/graphics/paint/PaintChunkProperties.h"
 #include "wtf/Allocator.h"
@@ -22,23 +23,15 @@ class PLATFORM_EXPORT PaintChunker final {
     DISALLOW_NEW();
     WTF_MAKE_NONCOPYABLE(PaintChunker);
 public:
-    enum ItemBehavior {
-        // Can be combined with adjacent items when building chunks.
-        DefaultBehavior = 0,
-
-        // Item requires its own paint chunk.
-        RequiresSeparateChunk,
-    };
-
     PaintChunker();
     ~PaintChunker();
 
     bool isInInitialState() const { return m_chunks.isEmpty() && m_currentProperties == PaintChunkProperties(); }
 
     const PaintChunkProperties& currentPaintChunkProperties() const { return m_currentProperties; }
-    void updateCurrentPaintChunkProperties(const PaintChunkProperties&);
+    void updateCurrentPaintChunkProperties(const PaintChunk::Id*, const PaintChunkProperties&);
 
-    void incrementDisplayItemIndex(ItemBehavior);
+    void incrementDisplayItemIndex(const DisplayItem&);
     void decrementDisplayItemIndex();
 
     void clear();
@@ -48,8 +41,17 @@ public:
     Vector<PaintChunk> releasePaintChunks();
 
 private:
+    enum ItemBehavior {
+        // Can be combined with adjacent items when building chunks.
+        DefaultBehavior = 0,
+
+        // Item requires its own paint chunk.
+        RequiresSeparateChunk,
+    };
+
     Vector<PaintChunk> m_chunks;
     Vector<ItemBehavior> m_chunkBehavior;
+    Optional<PaintChunk::Id> m_currentChunkId;
     PaintChunkProperties m_currentProperties;
 };
 
