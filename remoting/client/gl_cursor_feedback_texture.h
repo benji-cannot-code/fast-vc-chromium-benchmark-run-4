df41c223a481347f531cde8e95066157536bdb57
yuweih <yuweih@chromium.org>
2016-07-21 16:38:03 -0700

    [Remoting Android] Cursor & Cursor Feedback for OpenGL Renderer
    This CL implements classes for drawing cursor and cursor feedback on the
    OpenGL canvas. This is part of the project of using OpenGL to render
    remoting host on Android.
    
    BUG=385924
    
    Review-Url: https://codereview.chromium.org/2148743005
    Cr-Commit-Position: refs/heads/master@{#406991}

---
+++ remoting/client/gl_cursor_feedback_texture.h
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_CLIENT_GL_CURSOR_FEEDBACK_TEXTURE_H_
+#define REMOTING_CLIENT_GL_CURSOR_FEEDBACK_TEXTURE_H_
+
+#include <vector>
+
+#include "base/macros.h"
+#include "base/memory/singleton.h"
+
+namespace remoting {
+
+// This is a singleton for generating and holding the the cursor feedback
+// texture.
+class GlCursorFeedbackTexture {
+ public:
+  static const int kTextureWidth;
+
+  static GlCursorFeedbackTexture* GetInstance();
+
+  const std::vector<uint8_t>& GetTexture() const;
+
+ private:
+  GlCursorFeedbackTexture();
+  ~GlCursorFeedbackTexture();
+
+  friend struct base::DefaultSingletonTraits<GlCursorFeedbackTexture>;
+
+  std::vector<uint8_t> texture_;
+
+  DISALLOW_COPY_AND_ASSIGN(GlCursorFeedbackTexture);
+};
+
+}  // namespace remoting
+#endif  // REMOTING_CLIENT_GL_CURSOR_FEEDBACK_TEXTURE_H_
--- remoting/remoting_client.gypi
+++ remoting/remoting_client.gypi
@@ -199,6 +199,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           ],
           'dependencies': [
             '../third_party/khronos/khronos.gyp:khronos_headers',
+            '../third_party/libyuv/libyuv.gyp:libyuv',
             '../third_party/webrtc/webrtc.gyp:webrtc'
           ]
         }  # end of target 'remoting_opengl_renderer'
