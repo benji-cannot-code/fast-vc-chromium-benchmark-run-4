6768c9b61c3be3a53b7d9ea988e34232c00d75be
avi <avi@chromium.org>
2016-07-21 11:47:28 -0700

    Rename the chrome/browser/ui/app_modal directory to be javascript_dialogs.
    It will be the future home of the JavaScript dialog tab helper code, which will not be app-modal, so the name is no longer appropriate for use.
    
    In addition, I'm the owner of the existing dialog code (components/app_modal) so I'm going to claim ownership of the new code.
    
    BUG=629964
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2169593003
    Cr-Commit-Position: refs/heads/master@{#406920}

--- chrome/browser/ui/android/javascript_app_modal_dialog_android.cc
+++ chrome/browser/ui/android/javascript_app_modal_dialog_android.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
-#include "chrome/browser/ui/app_modal/chrome_javascript_native_dialog_factory.h"
+#include "chrome/browser/ui/javascript_dialogs/chrome_javascript_native_dialog_factory.h"
 #include "components/app_modal/app_modal_dialog_queue.h"
 #include "components/app_modal/javascript_app_modal_dialog.h"
 #include "components/app_modal/javascript_dialog_manager.h"
--- chrome/browser/ui/views/chrome_javascript_native_dialog_factory_views.cc
+++ chrome/browser/ui/views/chrome_javascript_native_dialog_factory_views.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/app_modal/chrome_javascript_native_dialog_factory.h"
+#include "chrome/browser/ui/javascript_dialogs/chrome_javascript_native_dialog_factory.h"
 
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
