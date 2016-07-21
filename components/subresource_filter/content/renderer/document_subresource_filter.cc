5204833a67f90ab5d50ea58eef5a8246218b5cd1
engedy <engedy@chromium.org>
2016-07-21 07:02:18 -0700

    Add tracing for DocumentSubresourceFilter::allowLoad.
    BUG=609747
    
    Review-Url: https://codereview.chromium.org/2160323002
    Cr-Commit-Position: refs/heads/master@{#406836}

--- components/subresource_filter/content/renderer/document_subresource_filter.cc
+++ components/subresource_filter/content/renderer/document_subresource_filter.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
+#include "base/trace_event/trace_event.h"
 #include "components/subresource_filter/core/common/memory_mapped_ruleset.h"
 #include "third_party/WebKit/public/platform/WebURL.h"
 #include "url/gurl.h"
@@ -30,6 +31,8 @@ DocumentSubresourceFilter::~DocumentSubresourceFilter() = default;
 bool DocumentSubresourceFilter::allowLoad(
     const blink::WebURL& resourceUrl,
     blink::WebURLRequest::RequestContext /* ignored */) {
+  TRACE_EVENT1("loader", "DocumentSubresourceFilter::allowLoad", "url",
+               resourceUrl.string().utf8());
   ++num_loads_evaluated_;
   if (DoesLoadMatchFilteringRules(GURL(resourceUrl))) {
     ++num_loads_matching_rules_;
