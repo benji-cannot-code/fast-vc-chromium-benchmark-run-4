259b7f7be0eb45a2027492059fe30e8311281431
ajith.v <ajith.v@chromium.org>
2016-07-21 03:23:49 -0700

    Remove ContentViewCore::RequestTextSurroundingSelection
    This change takes care of scaling down the scope of CVC by
    removing RequestTextSurroundingSelection() method. Logic has been
    moved to RenderFrameHost. Additionally added the support of responding
    with empty text when a new request comes during processing of a pending
    request.
    
    BUG=626770
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2157793002
    Cr-Commit-Position: refs/heads/master@{#406812}

--- chrome/browser/android/contextualsearch/contextual_search_delegate.cc
+++ chrome/browser/android/contextualsearch/contextual_search_delegate.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/net/variations_http_headers.h"
 #include "components/variations/variations_associated_data.h"
 #include "content/public/browser/android/content_view_core.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "net/base/escape.h"
 #include "net/http/http_status_code.h"
@@ -34,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 using content::ContentViewCore;
+using content::RenderFrameHost;
 
 namespace {
 
@@ -235,13 +237,23 @@ void ContextualSearchDelegate::GatherSurroundingTextWithCallback(
     content::ContentViewCore* content_view_core,
     bool may_send_base_page_url,
     HandleSurroundingsCallback callback) {
+  DCHECK(content_view_core);
+  DCHECK(content_view_core->GetWebContents());
+  DCHECK(!callback.is_null());
+  DCHECK(!selection.empty());
+  RenderFrameHost* focused_frame =
+      content_view_core->GetWebContents()->GetFocusedFrame();
+  if (!focused_frame) {
+    callback.Run(base::string16(), 0, 0);
+    return;
+  }
   // Immediately cancel any request that's in flight, since we're building a new
   // context (and the response disposes of any existing context).
   search_term_fetcher_.reset();
   BuildContext(selection, use_resolved_search_term, content_view_core,
                may_send_base_page_url);
-  content_view_core->RequestTextSurroundingSelection(
-      field_trial_->GetSurroundingSize(), callback);
+  focused_frame->RequestTextSurroundingSelection(
+      callback, field_trial_->GetSurroundingSize());
 }
 
 void ContextualSearchDelegate::BuildContext(
--- content/browser/renderer_host/render_widget_host_view_android.h
+++ content/browser/renderer_host/render_widget_host_view_android.h
@@ -162,9 +162,6 @@ class CONTENT_EXPORT RenderWidgetHostViewAndroid
       override;
   void LockCompositingSurface() override;
   void UnlockCompositingSurface() override;
-  void OnTextSurroundingSelectionResponse(const base::string16& content,
-                                          size_t start_offset,
-                                          size_t end_offset) override;
   void OnDidNavigateMainFrameToNewPage() override;
 
   // cc::SurfaceFactoryClient implementation.
@@ -249,12 +246,6 @@ class CONTENT_EXPORT RenderWidgetHostViewAndroid
   void SynchronousFrameMetadata(cc::CompositorFrameMetadata frame_metadata);
 
   void SetOverlayVideoMode(bool enabled);
-
-  typedef base::Callback<
-      void(const base::string16& content, int start_offset, int end_offset)>
-      TextSurroundingSelectionCallback;
-  void SetTextSurroundingSelectionCallback(
-      const TextSurroundingSelectionCallback& callback);
 
   static void OnContextLost();
 
@@ -394,8 +385,6 @@ class CONTENT_EXPORT RenderWidgetHostViewAndroid
   };
 
   std::unique_ptr<LastFrameInfo> last_frame_info_;
-
-  TextSurroundingSelectionCallback text_surrounding_selection_callback_;
 
   // The last scroll offset of the view.
   gfx::Vector2dF last_scroll_offset_;
