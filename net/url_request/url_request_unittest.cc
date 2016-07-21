e46f15c37ac7e6e7c05c24d21bdc908544cbdbee
timvolodine <timvolodine@chromium.org>
2016-07-21 08:23:32 -0700

    Disable flaky HTTPSOCSPVerifyTest on Android.
    This test has been added in https://codereview.chromium.org/2100303002
    and appears to be flaky on the Android Tests (dbg) bot, see
    crbug.com/630286 for details.
    
    BUG=630286
    TBR=rsleevi@chromium.org,tedchoc@chromium.org,dadrian@google.com
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2164303002
    Cr-Commit-Position: refs/heads/master@{#406854}

--- net/url_request/url_request_unittest.cc
+++ net/url_request/url_request_unittest.cc
@@ -9464,6 +9464,8 @@ static const struct OCSPVerifyTestData {
 
 };
 
+// Flaky on Android Tests (dbg) bot, crbug.com/630286.
+#if !defined(OS_ANDROID)
 class HTTPSOCSPVerifyTest
     : public HTTPSOCSPTest,
       public testing::WithParamInterface<OCSPVerifyTestData> {};
@@ -9498,6 +9500,7 @@ TEST_P(HTTPSOCSPVerifyTest, VerifyResult) {
 INSTANTIATE_TEST_CASE_P(OCSPVerify,
                         HTTPSOCSPVerifyTest,
                         testing::ValuesIn(kOCSPVerifyData));
+#endif
 
 class HTTPSHardFailTest : public HTTPSOCSPTest {
  protected:
