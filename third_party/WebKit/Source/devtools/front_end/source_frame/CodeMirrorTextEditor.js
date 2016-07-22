670f19c35b664cb6aa3f3a95e207186c1e512013
flandy <flandy@google.com>
2016-07-21 20:44:26 -0700

    DevTools: Immediately hide on scroll for source color picker
    Move the timeout for handling scroll from CodeMirrorTextEditor to SourceFrame.
    
    R=lushnikov
    
    Review-Url: https://codereview.chromium.org/2173553002
    Cr-Commit-Position: refs/heads/master@{#407040}

--- third_party/WebKit/Source/devtools/front_end/source_frame/CodeMirrorTextEditor.js
+++ third_party/WebKit/Source/devtools/front_end/source_frame/CodeMirrorTextEditor.js
@@ -1439,10 +1439,8 @@ WebInspector.CodeMirrorTextEditor.prototype = {
 
     _scroll: function()
     {
-        if (this._scrollTimer)
-            clearTimeout(this._scrollTimer);
         var topmostLineNumber = this._codeMirror.lineAtHeight(this._codeMirror.getScrollInfo().top, "local");
-        this._scrollTimer = setTimeout(this._delegate.scrollChanged.bind(this._delegate, topmostLineNumber), 100);
+        this._delegate.scrollChanged(topmostLineNumber);
     },
 
     _focus: function()
