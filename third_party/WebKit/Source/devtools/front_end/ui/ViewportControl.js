b62042b00faf60c08bd02acf2ad77aed07bf0440
luoe <luoe@chromium.org>
2016-07-21 21:43:07 -0700

    DevTools: remove isTrusted check and isUserGesture from ViewportControl
    Previously, the ViewportControl never stuck to the bottom after a scroll event.
    The event.isTrusted check was always true, so it was not correctly
    distinguishing user-generated scroll events from script-generated ones.  This
    CL removes isUserGesture accordingly.
    
    BUG=613879
    
    Review-Url: https://codereview.chromium.org/2154893002
    Cr-Commit-Position: refs/heads/master@{#407062}

--- third_party/WebKit/Source/devtools/front_end/ui/ViewportControl.js
+++ third_party/WebKit/Source/devtools/front_end/ui/ViewportControl.js
@@ -198,7 +198,7 @@ WebInspector.ViewportControl.prototype = {
         delete this._cumulativeHeights;
         delete this._cachedProviderElements;
         this._itemCount = this._provider.itemCount();
-        this._innerRefresh(false);
+        this.refresh();
     },
 
     /**
@@ -377,14 +377,6 @@ WebInspector.ViewportControl.prototype = {
 
     refresh: function()
     {
-        this._innerRefresh(false);
-    },
-
-    /**
-     * @param {boolean} isUserGesture
-     */
-    _innerRefresh: function(isUserGesture)
-    {
         if (!this._visibleHeight())
             return;  // Do nothing for invisible controls.
 
@@ -417,7 +409,7 @@ WebInspector.ViewportControl.prototype = {
         var oldFirstVisibleIndex = this._firstVisibleIndex;
         var oldLastVisibleIndex = this._lastVisibleIndex;
 
-        var shouldStickToBottom = !isUserGesture && this._stickToBottom && this._scrolledToBottom;
+        var shouldStickToBottom = this._stickToBottom && this._scrolledToBottom;
 
         if (shouldStickToBottom) {
             this._lastVisibleIndex = this._itemCount - 1;
@@ -569,7 +561,7 @@ WebInspector.ViewportControl.prototype = {
      */
     _onScroll: function(event)
     {
-        this._innerRefresh(event.isTrusted);
+        this.refresh();
     },
 
     /**
@@ -623,7 +615,7 @@ WebInspector.ViewportControl.prototype = {
     {
         this._rebuildCumulativeHeightsIfNeeded();
         this.element.scrollTop = index > 0 ? this._cumulativeHeights[index - 1] : 0;
-        this._innerRefresh(false);
+        this.refresh();
     },
 
     /**
@@ -633,7 +625,7 @@ WebInspector.ViewportControl.prototype = {
     {
         this._rebuildCumulativeHeightsIfNeeded();
         this.element.scrollTop = this._cumulativeHeights[index] - this._visibleHeight();
-        this._innerRefresh(false);
+        this.refresh();
     },
 
     /**
