dcc92c28ced20f4869aed20d29773bcf3eb22aba
csharrison <csharrison@chromium.org>
2016-07-22 12:15:58 -0700

    Don't wait for AppCache for link rel preloads
    The preload scanner currently waits for the Application Cache to be
    initialized before sending out any preloads. This is less important for link
    rel preloads, which are directives to send a fetch as soon as possible.
    
    This patch bypasses the fetch queuing for these preloads.
    
    For link preloads in headers, we can issue these as early as the commit
    point (for those which don't have a media attribute).
    
    BUG=629420
    
    Review-Url: https://codereview.chromium.org/2165653004
    Cr-Commit-Position: refs/heads/master@{#407227}

--- third_party/WebKit/Source/core/loader/DocumentLoader.h
+++ third_party/WebKit/Source/core/loader/DocumentLoader.h
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/loader/DocumentLoadTiming.h"
 #include "core/loader/DocumentWriter.h"
 #include "core/loader/FrameLoaderTypes.h"
+#include "core/loader/LinkLoader.h"
 #include "core/loader/NavigationPolicy.h"
 #include "platform/SharedBuffer.h"
 #include "platform/network/ResourceError.h"
@@ -61,6 +62,7 @@ class LocalFrame;
 class FrameLoader;
 class ResourceLoader;
 class WebDocumentSubresourceFilter;
+struct ViewportDescriptionWrapper;
 
 class CORE_EXPORT DocumentLoader : public GarbageCollectedFinalized<DocumentLoader>, private RawResourceClient {
 public:
@@ -146,6 +148,8 @@ public:
 
     void setWasBlockedAfterXFrameOptionsOrCSP() { m_wasBlockedAfterXFrameOptionsOrCSP = true; }
     bool wasBlockedAfterXFrameOptionsOrCSP() { return m_wasBlockedAfterXFrameOptionsOrCSP; }
+
+    void dispatchLinkHeaderPreloads(ViewportDescriptionWrapper*, LinkLoader::MediaPreloadPolicy);
 
     Resource* startPreload(Resource::Type, FetchRequest&);
 
