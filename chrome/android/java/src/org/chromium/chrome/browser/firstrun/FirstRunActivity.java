55e53fc2adb09c7cb3e92bdcef858c51c0e4697a
gogerald <gogerald@chromium.org>
2016-07-22 07:00:13 -0700

    Disable swipe gesture and smoothly scroll
    Smoothly scroll effect also triggers TalkBack read previous screen after navigate to new screen.
    
    BUG=602373, 628255
    
    Review-Url: https://codereview.chromium.org/2166383002
    Cr-Commit-Position: refs/heads/master@{#407149}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
@@ -9,7 +9,6 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 
@@ -85,7 +84,7 @@ public class FirstRunActivity extends AppCompatActivity implements FirstRunPageD
     private boolean mNativeSideIsInitialized;
 
     private ProfileDataCache mProfileDataCache;
-    private ViewPager mPager;
+    private FirstRunViewPager mPager;
 
     private Bundle mFreProperties;
 
@@ -148,7 +147,7 @@ public class FirstRunActivity extends AppCompatActivity implements FirstRunPageD
             return;
         }
 
-        mPager = new ViewPager(this);
+        mPager = new FirstRunViewPager(this);
         mPager.setId(R.id.fre_pager);
         setContentView(mPager);
 
@@ -235,7 +234,7 @@ public class FirstRunActivity extends AppCompatActivity implements FirstRunPageD
         if (mPager.getCurrentItem() == 0) {
             abortFirstRunExperience();
         } else {
-            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1, false);
         }
     }
 
@@ -248,7 +247,7 @@ public class FirstRunActivity extends AppCompatActivity implements FirstRunPageD
 
     @Override
     public void advanceToNextPage() {
-        jumpToPage(mPager.getCurrentItem() + 1, true);
+        jumpToPage(mPager.getCurrentItem() + 1);
     }
 
     @Override
@@ -357,7 +356,7 @@ public class FirstRunActivity extends AppCompatActivity implements FirstRunPageD
         FirstRunStatus.setSkipWelcomePage(FirstRunActivity.this, true);
         flushPersistentData();
         stopProgressionIfNotAcceptedTermsOfService();
-        jumpToPage(mPager.getCurrentItem() + 1, true);
+        jumpToPage(mPager.getCurrentItem() + 1);
     }
 
     @Override
@@ -390,9 +389,8 @@ public class FirstRunActivity extends AppCompatActivity implements FirstRunPageD
      * Transitions to a given page.
      * @return Whether the transition to a given page was allowed.
      * @param position A page index to transition to.
-     * @param smooth   Whether the transition should be smooth.
      */
-    private boolean jumpToPage(int position, boolean smooth) {
+    private boolean jumpToPage(int position) {
         if (mShowWelcomePage && !didAcceptTermsOfService()) {
             return position == 0;
         }
@@ -400,7 +398,7 @@ public class FirstRunActivity extends AppCompatActivity implements FirstRunPageD
             completeFirstRunExperience();
             return false;
         }
-        mPager.setCurrentItem(position, smooth);
+        mPager.setCurrentItem(position, false);
         return true;
     }
 
@@ -416,7 +414,7 @@ public class FirstRunActivity extends AppCompatActivity implements FirstRunPageD
         while (currentPageIndex < mPagerAdapter.getCount()) {
             FirstRunPage currentPage = (FirstRunPage) mPagerAdapter.getItem(currentPageIndex);
             if (!currentPage.shouldSkipPageOnCreate(getApplicationContext())) return;
-            if (!jumpToPage(currentPageIndex + 1, false)) return;
+            if (!jumpToPage(currentPageIndex + 1)) return;
             currentPageIndex = mPager.getCurrentItem();
         }
     }
