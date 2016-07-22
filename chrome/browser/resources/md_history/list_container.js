4f33524312bc9248ed0f59af8ed747cef34ff4d8
tsergeant <tsergeant@chromium.org>
2016-07-21 20:22:21 -0700

    MD History: Close the item overflow menu when new results load in
    This fixes an issue where it was possible to have the menu open
    for a history item which is no longer visible.
    
    BUG=626231
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2165473002
    Cr-Commit-Position: refs/heads/master@{#407017}

--- chrome/browser/resources/md_history/list_container.js
+++ chrome/browser/resources/md_history/list_container.js
@@ -38,6 +38,7 @@ Polymer({
    */
   historyResult: function(info, results) {
     this.initializeResults_(info, results);
+    this.closeMenu_();
 
     if (this.selectedPage_ == 'grouped-list') {
       this.$$('#grouped-list').historyData = results;
