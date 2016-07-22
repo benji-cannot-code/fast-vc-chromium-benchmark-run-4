503595ec2db1b00b94a885633b9eb3ae66efe00c
jmadill <jmadill@chromium.org>
2016-07-21 20:49:58 -0700

    Revert of Add new gpu driver bug workaround DISABLE_TRANSPARENT_VISUALS (patchset #17 id:310001 of https://codereview.chromium.org/2124633002/ )
    Reason for revert:
    Seems to break the Android and Linux GPU fyi bots:
    https://build.chromium.org/p/chromium.gpu.fyi/builders/Android%20Release%20%28Nexus%205X%29/builds/1098
    https://build.chromium.org/p/chromium.gpu.fyi/builders/Linux%20Release%20%28ATI%29/builds/48671
    https://build.chromium.org/p/chromium.gpu.fyi/builders/Android%20Release%20%28Nexus%206P%29/builds/1004
    <couple others>
    
    GpuProcess.no_transparent_visuals_gpu_process
    Failure: disable_transparent_visuals is missing in Browser process workarounds: [u'broken_egl_image_ref_counting', u'clear_uniforms_before_first_program_use', u'disable_blend_equation_advanced', u'disable_chromium_framebuffer_multisample', u'disable_depth_texture', u'disable_discard_framebuffer', u'disable_framebuffer_cmaa', u'disable_program_cache', u'force_cube_map_positive_x_allocation', u'max_copy_texture_chromium_size_1048576', u'max_texture_size_limit_4096', u'scalarize_vec_and_mat_constructor_args', u'unbind_attachments_on_bound_render_fbo_delete', u'unbind_egl_context_to_flush_driver_caches', u'use_virtualized_gl_contexts', u'wake_up_gpu_before_drawing']
    
    [  FAILED  ] GpuProcess.no_transparent_visuals_gpu_process (5860 ms)
    
    Original issue's description:
    > Add new gpu driver bug workaround DISABLE_TRANSPARENT_VISUALS
    >
    > This is for better polish in the UI but it fixes a drag
    > image issue on Linux crbug.com/593256 .
    >
    > Also add a kGpuDriverBugListJson entry to automatically
    > disable transparent visuals on all drivers (especially
    > the proprietary NVIDIA driver) except for opensource
    > drivers (i.e. Mesa based drivers).
    >
    > BUG=369209
    >
    > R=kbr@chromium.org, sadrul@chromium.org, tapted@chromium.org
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    >
    > Committed: https://crrev.com/71b2517dd4cd620a56ca6f13571129c6aee30b42
    > Cr-Commit-Position: refs/heads/master@{#406505}
    
    TBR=kbr@chromium.org,sadrul@chromium.org,tapted@chromium.org,piman@chromium.org,j.isorce@samsung.com
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=369209
    
    Review-Url: https://codereview.chromium.org/2171023002
    Cr-Commit-Position: refs/heads/master@{#407047}

--- ui/views/widget/widget_unittest.cc
+++ ui/views/widget/widget_unittest.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 
 #include "base/bind.h"
-#include "base/environment.h"
+#include "base/command_line.h"
 #include "base/macros.h"
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
@@ -3741,14 +3741,13 @@ void InitializeWidgetForOpacity(
     Widget::InitParams init_params,
     const Widget::InitParams::WindowOpacity opacity) {
 #if defined(USE_X11)
-  // testing/xvfb.py runs xvfb and xcompmgr.
-  std::unique_ptr<base::Environment> env(base::Environment::Create());
-  bool has_compositing_manager = env->HasVar("_CHROMIUM_INSIDE_XVFB");
-  int depth = 0;
-  ui::ChooseVisualForWindow(has_compositing_manager, NULL, &depth);
+  // On Linux, transparent visuals is currently not activated by default.
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  command_line->AppendSwitch(switches::kEnableTransparentVisuals);
 
-  if (has_compositing_manager)
-    EXPECT_EQ(depth, 32);
+  int depth = 0;
+  ui::ChooseVisualForWindow(NULL, &depth);
+  EXPECT_EQ(depth, 32);
 #endif
 
   init_params.opacity = opacity;
@@ -3760,8 +3759,7 @@ void InitializeWidgetForOpacity(
   widget.Init(init_params);
 
 #if defined(USE_X11)
-  if (has_compositing_manager)
-    EXPECT_TRUE(widget.IsTranslucentWindowOpacitySupported());
+  EXPECT_TRUE(widget.IsTranslucentWindowOpacitySupported());
 #endif
 }
 
