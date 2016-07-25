0a7ec070a65aae6bf4b18391c45dd1f90db73d54
jif <jif@chromium.org>
2016-07-25 12:01:07 -0700

    Add missing NSString forward declaration.
    BUG=623016
    TBR=rohitrao
    
    Review-Url: https://codereview.chromium.org/2180943002
    Cr-Commit-Position: refs/heads/master@{#407535}

--- ios/chrome/browser/storekit_launcher.h
+++ ios/chrome/browser/storekit_launcher.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_STOREKIT_LAUNCHER_H_
 #define IOS_CHROME_BROWSER_STOREKIT_LAUNCHER_H_
 
+@class NSString;
+
 // Protocol to be implemented by a class that provides an access to the app
 // store with StoreKit.
 @protocol StoreKitLauncher
