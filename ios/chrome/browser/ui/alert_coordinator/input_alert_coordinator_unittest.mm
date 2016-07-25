2169db947e6e5e56cd4e39e2f683cd9ac3e27dfc
gambard <gambard@chromium.org>
2016-07-25 02:29:19 -0700

    Creates coordinator for ActionSheet and TextInput
    This CL creates subclasses of AlertCoordinator for moving the use of
    ActionSheet to another class and enabling the use of text inputs in the alert.
    
    BUG=629515
    
    Review-Url: https://codereview.chromium.org/2167683002
    Cr-Commit-Position: refs/heads/master@{#407437}

---
+++ ios/chrome/browser/ui/alert_coordinator/input_alert_coordinator_unittest.mm
@@ -0,0 +1,54 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/alert_coordinator/input_alert_coordinator.h"
+
+#include "testing/platform_test.h"
+#import "third_party/ocmock/OCMock/OCMock.h"
+#include "third_party/ocmock/gtest_support.h"
+
+TEST(InputAlertCoordinatorTest, AddTextField) {
+  // Setup.
+  UIViewController* viewController =
+      [[[UIViewController alloc] init] autorelease];
+  InputAlertCoordinator* alertCoordinator = [[[InputAlertCoordinator alloc]
+      initWithBaseViewController:viewController
+                           title:@"Test"
+                         message:nil] autorelease];
+
+  void (^emptyHandler)(UITextField* textField) = ^(UITextField* textField) {
+  };
+  id alert =
+      [OCMockObject partialMockForObject:alertCoordinator.alertController];
+  [[alert expect] addTextFieldWithConfigurationHandler:emptyHandler];
+
+  // Action.
+  [alertCoordinator addTextFieldWithConfigurationHandler:emptyHandler];
+
+  // Test.
+  EXPECT_OCMOCK_VERIFY(alert);
+}
+
+TEST(InputAlertCoordinatorTest, GetTextFields) {
+  // Setup.
+  UIViewController* viewController =
+      [[[UIViewController alloc] init] autorelease];
+  InputAlertCoordinator* alertCoordinator = [[[InputAlertCoordinator alloc]
+      initWithBaseViewController:viewController
+                           title:@"Test"
+                         message:nil] autorelease];
+
+  NSArray<UITextField*>* array = [NSArray array];
+  id alert =
+      [OCMockObject partialMockForObject:alertCoordinator.alertController];
+  [[[alert expect] andReturn:array] textFields];
+
+  // Action.
+  NSArray<UITextField*>* resultArray = alertCoordinator.textFields;
+
+  // Test.
+  EXPECT_EQ(array, resultArray);
+  EXPECT_OCMOCK_VERIFY(alert);
+}
