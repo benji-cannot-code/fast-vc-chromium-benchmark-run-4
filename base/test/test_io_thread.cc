83d41fdb83c3eb5d0407e805decf0fd077d5659f
gab <gab@chromium.org>
2016-07-25 14:21:23 -0700

    Fix MemoryDumpManagerTest's thread unsafe usage of the base::TestIOThread API
    Also move TestIOThread::PostTaskAndWait to those tests as they were the
    only user.
    
    BUG=629139
    
    Review-Url: https://codereview.chromium.org/2170953002
    Cr-Commit-Position: refs/heads/master@{#407577}

--- base/test/test_io_thread.cc
+++ base/test/test_io_thread.cc
@@ -5,19 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/test_io_thread.h"
 
-#include "base/bind.h"
-#include "base/callback.h"
-#include "base/synchronization/waitable_event.h"
-
-namespace {
-
-void PostTaskAndWaitHelper(base::WaitableEvent* event,
-                           const base::Closure& task) {
-  task.Run();
-  event->Signal();
-}
-
-}  // namespace
+#include "base/logging.h"
 
 namespace base {
 
@@ -53,15 +41,6 @@ void TestIOThread::Stop() {
 void TestIOThread::PostTask(const tracked_objects::Location& from_here,
                             const base::Closure& task) {
   task_runner()->PostTask(from_here, task);
-}
-
-void TestIOThread::PostTaskAndWait(const tracked_objects::Location& from_here,
-                                   const base::Closure& task) {
-  base::WaitableEvent event(WaitableEvent::ResetPolicy::AUTOMATIC,
-                            WaitableEvent::InitialState::NOT_SIGNALED);
-  task_runner()->PostTask(from_here,
-                          base::Bind(&PostTaskAndWaitHelper, &event, task));
-  event.Wait();
 }
 
 }  // namespace base
