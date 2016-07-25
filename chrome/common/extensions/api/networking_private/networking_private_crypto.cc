21418fd528f6acc40f2eee62ee003469356c7256
mpearson <mpearson@chromium.org>
2016-07-25 12:06:20 -0700

    Revert of Cast device revocation checking. (patchset #26 id:500001 of https://codereview.chromium.org/2050983002/ )
    Reason for revert:
    Causes failure on
    https://build.chromium.org/p/chromium.linux/builders/Linux%20Tests%20%28dbg%29%281%29%2832%29/builds/31399
    in
    components_unittests
    
    failures
    VerifyCastDeviceCertTest.ChromecastGen2InvalidTime
    
    VerifyCastDeviceCertTest.ChromecastGen2InvalidTime (run #1):
    [ RUN      ] VerifyCastDeviceCertTest.ChromecastGen2InvalidTime
    ../../components/cast_certificate/cast_cert_validator_unittest.cc:99: Failure
    Value of: base::Time::FromUTCExploded(time, &result)
      Actual: false
    Expected: true
    [  FAILED  ] VerifyCastDeviceCertTest.ChromecastGen2InvalidTime (13 ms)
    
    Original issue's description:
    > Cast device revocation checking.
    >
    > Cast device certificates may be revoked through two ways:
    > 1. The hash of the public key.
    > 2. A serial number range for an issuer identified by the hash
    >    of its public key.
    >
    > A customized proto is used as the medium for this information.
    >
    > This change contains the implementation for verifying the custom CRL
    > and verifying a certificate's revocation status based on that CRL.
    >
    > BUG=618463
    >
    > Committed: https://crrev.com/698608f28ed2df276f920c9691cbfcb8f9069337
    > Cr-Commit-Position: refs/heads/master@{#407492}
    
    TBR=sheretov@chromium.org,eroman@chromium.org,asargent@chromium.org,davidben@chromium.org,ryanchung@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=618463
    
    Review-Url: https://codereview.chromium.org/2181013002
    Cr-Commit-Position: refs/heads/master@{#407537}

--- chrome/common/extensions/api/networking_private/networking_private_crypto.cc
+++ chrome/common/extensions/api/networking_private/networking_private_crypto.cc
@@ -52,7 +52,8 @@ bool VerifyCredentials(
     const std::string& signature,
     const std::string& data,
     const std::string& connected_mac) {
-  base::Time now = base::Time::Now();
+  base::Time::Exploded now;
+  base::Time::Now().UTCExplode(&now);
   return VerifyCredentialsAtTime(certificate, intermediate_certificates,
                                  signature, data, connected_mac, now);
 }
@@ -63,7 +64,7 @@ bool VerifyCredentialsAtTime(
     const std::string& signature,
     const std::string& data,
     const std::string& connected_mac,
-    const base::Time& time) {
+    const base::Time::Exploded& time) {
   static const char kErrorPrefix[] = "Device verification failed. ";
 
   std::vector<std::string> headers;
@@ -97,8 +98,7 @@ bool VerifyCredentialsAtTime(
 
   std::unique_ptr<cast_crypto::CertVerificationContext> verification_context;
   if (!cast_crypto::VerifyDeviceCert(certs, time, &verification_context,
-                                     &unused_policy, nullptr,
-                                     cast_crypto::CRLPolicy::CRL_OPTIONAL)) {
+                                     &unused_policy)) {
     LOG(ERROR) << kErrorPrefix << "Failed verifying cast device cert";
     return false;
   }
