44a7ac85c84ebc94c3113bbd089a6c82b31ab75c
ryanchung <ryanchung@chromium.org>
2016-07-25 09:44:02 -0700

    Cast device revocation checking.
    Cast device certificates may be revoked through two ways:
    1. The hash of the public key.
    2. A serial number range for an issuer identified by the hash
       of its public key.
    
    A customized proto is used as the medium for this information.
    
    This change contains the implementation for verifying the custom CRL
    and verifying a certificate's revocation status based on that CRL.
    
    BUG=618463
    
    Review-Url: https://codereview.chromium.org/2050983002
    Cr-Commit-Position: refs/heads/master@{#407492}

--- components/cast_certificate/cast_cert_validator_test_helpers.cc
+++ components/cast_certificate/cast_cert_validator_test_helpers.cc
@@ -14,10 +14,6 @@ namespace cast_certificate {
 
 namespace testing {
 
-namespace {
-
-// Reads a file from the test data directory
-// (//src/components/test/data/cast_certificate)
 std::string ReadTestFileToString(const base::StringPiece& file_name) {
   base::FilePath filepath;
   PathService::Get(base::DIR_SOURCE_ROOT, &filepath);
@@ -36,8 +32,6 @@ std::string ReadTestFileToString(const base::StringPiece& file_name) {
 
   return file_data;
 }
-
-}  // namespace
 
 std::vector<std::string> ReadCertificateChainFromFile(
     const base::StringPiece& file_name) {
