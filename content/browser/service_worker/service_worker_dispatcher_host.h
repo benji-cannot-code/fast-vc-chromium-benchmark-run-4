f4498189bb22e829361de2ed0a4379345751b4fa
horo <horo@chromium.org>
2016-07-25 09:00:19 -0700

    Stop passing const-reference of SWRegistration in all GetRegistrationsCallbacks.
    BUG=616502
    
    Review-Url: https://codereview.chromium.org/2181553003
    Cr-Commit-Position: refs/heads/master@{#407487}

--- content/browser/service_worker/service_worker_dispatcher_host.h
+++ content/browser/service_worker/service_worker_dispatcher_host.h
@@ -214,7 +214,7 @@ class CONTENT_EXPORT ServiceWorkerDispatcherHost : public BrowserMessageFilter {
       int provider_id,
       int request_id,
       ServiceWorkerStatusCode status,
-      const scoped_refptr<ServiceWorkerRegistration>& registration);
+      scoped_refptr<ServiceWorkerRegistration> registration);
   void GetRegistrationsComplete(
       int thread_id,
       int provider_id,
