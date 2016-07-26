74df4c49bd8bd77d7405a241e1ed4168f4369313
sorin <sorin@chromium.org>
2016-07-25 18:02:40 -0700

    fast-update flag only accelerates the first component update check.
    BUG=630732
    
    Review-Url: https://codereview.chromium.org/2179593003
    Cr-Commit-Position: refs/heads/master@{#407664}

--- components/component_updater/configurator_impl_unittest.cc
+++ components/component_updater/configurator_impl_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2016 The Chromium Authors. All rights reserved.
+// Copyright (c) 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -45,7 +45,7 @@ TEST_F(ComponentUpdaterConfiguratorImplTest, FastUpdate) {
   cmdline.AppendSwitchASCII("--component-updater", "fast-update");
   config.reset(new ConfiguratorImpl(&cmdline, nullptr, false));
   CHECK_EQ(10, config->InitialDelay());
-  CHECK_EQ(kDelayOneMinute, config->NextCheckDelay());
+  CHECK_EQ(6 * kDelayOneHour, config->NextCheckDelay());
   CHECK_EQ(1, config->StepDelay());
   CHECK_EQ(2, config->OnDemandDelay());
   CHECK_EQ(10, config->UpdateDelay());
