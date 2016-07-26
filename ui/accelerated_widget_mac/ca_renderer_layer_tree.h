c1f4bf82047064c62013f7b9c814a70ba40c45b2
erikchen <erikchen@chromium.org>
2016-07-26 16:02:57 -0700

    Implement filter effects in Core Animation using CAFilters.
    Filter effects are converted into CAFilters, which are applied to the CALayer.
    
    BUG=581526
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2186693002
    Cr-Commit-Position: refs/heads/master@{#407951}

--- ui/accelerated_widget_mac/ca_renderer_layer_tree.h
+++ ui/accelerated_widget_mac/ca_renderer_layer_tree.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/mac/io_surface.h"
 #include "ui/gfx/transform.h"
+#include "ui/gl/ca_renderer_layer_params.h"
 
 @class AVSampleBufferDisplayLayer;
 
@@ -142,7 +143,8 @@ class ACCELERATED_WIDGET_MAC_EXPORT CARendererLayerTree {
                  unsigned background_color,
                  unsigned edge_aa_mask,
                  float opacity,
-                 unsigned filter);
+                 unsigned filter,
+                 const CARendererLayerParams::FilterEffects& filter_effects);
     ContentLayer(ContentLayer&& layer);
 
     // See the behavior of RootLayer for the effects of these functions on the
@@ -171,6 +173,9 @@ class ACCELERATED_WIDGET_MAC_EXPORT CARendererLayerTree {
     // AVSampleBufferDisplayLayer, then |ca_layer| will point to |av_layer|.
     base::scoped_nsobject<AVSampleBufferDisplayLayer> av_layer;
     bool use_av_layer = false;
+
+    // Filter effects to apply to this layer.
+    CARendererLayerParams::FilterEffects filter_effects;
 
    private:
     DISALLOW_COPY_AND_ASSIGN(ContentLayer);
