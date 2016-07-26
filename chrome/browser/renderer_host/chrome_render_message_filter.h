e63463206f2337189b47413a3a48a43114fc8c7e
dominickn <dominickn@chromium.org>
2016-07-25 22:32:01 -0700

    Revert "Speculatively launch Service Workers on mouse/touch events. [3/5]"
    This reverts commit 2cba6671596841c77457423e70e9d12070425dd1.
    
    Reason: Speculative revert so that crrev.com/2181553003 can be reverted.
    This may have caused crbug.com/631190.
    
    This CL will be relanded if reverting the dependent CL and re-enabling
    the disabled app banner data fetcher tests still causes failures on
    https://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%28dbg%29%281%29
    
    TBR=horo@chromium.org,jochen@chromium.org,nhiroki@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=631190
    
    Review-Url: https://codereview.chromium.org/2181983002
    Cr-Commit-Position: refs/heads/master@{#407714}

--- chrome/browser/renderer_host/chrome_render_message_filter.h
+++ chrome/browser/renderer_host/chrome_render_message_filter.h
@@ -25,10 +25,6 @@ namespace chrome_browser_net {
 class Predictor;
 }
 
-namespace content {
-class ServiceWorkerContext;
-}
-
 namespace content_settings {
 class CookieSettings;
 }
@@ -45,10 +41,7 @@ class InfoMap;
 // process on the IPC thread.
 class ChromeRenderMessageFilter : public content::BrowserMessageFilter {
  public:
-  ChromeRenderMessageFilter(
-      int render_process_id,
-      Profile* profile,
-      content::ServiceWorkerContext* service_worker_context);
+  ChromeRenderMessageFilter(int render_process_id, Profile* profile);
 
   // content::BrowserMessageFilter methods:
   bool OnMessageReceived(const IPC::Message& message) override;
@@ -144,9 +137,6 @@ class ChromeRenderMessageFilter : public content::BrowserMessageFilter {
 
   // Used to look up permissions at database creation time.
   scoped_refptr<content_settings::CookieSettings> cookie_settings_;
-
-  // Used to start Service Workers for navigation hints.
-  content::ServiceWorkerContext* service_worker_context_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeRenderMessageFilter);
 };
--- content/browser/service_worker/service_worker_context_wrapper.h
+++ content/browser/service_worker/service_worker_context_wrapper.h
@@ -26,10 +26,6 @@ class SequencedTaskRunner;
 class SingleThreadTaskRunner;
 }
 
-namespace blink {
-enum class WebNavigationHintType;
-}
-
 namespace storage {
 class QuotaManagerProxy;
 class SpecialStoragePolicy;
@@ -115,11 +111,6 @@ class CONTENT_EXPORT ServiceWorkerContextWrapper
       const CheckHasServiceWorkerCallback& callback) override;
   void StopAllServiceWorkersForOrigin(const GURL& origin) override;
   void ClearAllServiceWorkersForTest(const base::Closure& callback) override;
-  void StartServiceWorkerForNavigationHint(
-      const GURL& document_url,
-      blink::WebNavigationHintType type,
-      int render_process_id,
-      const ResultCallback& callback) override;
 
   // These methods must only be called from the IO thread.
   ServiceWorkerRegistration* GetLiveRegistration(int64_t registration_id);
@@ -240,23 +231,6 @@ class CONTENT_EXPORT ServiceWorkerContextWrapper
   void DidFindRegistrationForUpdate(
       ServiceWorkerStatusCode status,
       scoped_refptr<content::ServiceWorkerRegistration> registration);
-
-  void DidCheckRenderProcessForNavigationHint(const GURL& document_url,
-                                              blink::WebNavigationHintType type,
-                                              int render_process_id,
-                                              const ResultCallback& callback);
-
-  void DidFindRegistrationForNavigationHint(
-      blink::WebNavigationHintType type,
-      int render_process_id,
-      const ResultCallback& callback,
-      ServiceWorkerStatusCode status,
-      scoped_refptr<ServiceWorkerRegistration> registration);
-
-  void DidStartServiceWorkerForNavigationHint(const GURL& pattern,
-                                              int render_process_id,
-                                              const ResultCallback& callback,
-                                              ServiceWorkerStatusCode code);
 
   // The core context is only for use on the IO thread.
   // Can be null before/during init, during/after shutdown, and after
