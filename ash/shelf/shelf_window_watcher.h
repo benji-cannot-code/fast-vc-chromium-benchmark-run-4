79c72059db57a55b16a1c3a992b717a81c4b3fc3
jamescook <jamescook@chromium.org>
2016-07-26 14:25:12 -0700

    Remove ash::ScopedObserverWithDuplicatedSources
    It was added in http://crrev.com/21185004 to allow observation of multiple
    ActivationClients in ash. However, ash only has a single ActivationClient, so
    just observe that directly.
    
    This eliminates ash::WindowTreeHostManager access in ShelfWindowWatcher, which
    will simplify its conversion to wm types. It also simplifies
    BrowserStatusMonitor, which no longer needs to observe root windows or displays
    just to add activation client observers.
    
    BUG=629257
    TEST=ash_unittests, chrome unit_tests, manually activate windows and tabs in
    ash and verify shelf light-bar appearance changes correctly
    
    Review-Url: https://codereview.chromium.org/2185743002
    Cr-Commit-Position: refs/heads/master@{#407919}

--- ash/shelf/shelf_window_watcher.h
+++ ash/shelf/shelf_window_watcher.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include "ash/shelf/scoped_observer_with_duplicated_sources.h"
-#include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/scoped_observer.h"
 #include "ui/aura/window_observer.h"
@@ -139,11 +137,6 @@ class ShelfWindowWatcher : public aura::client::ActivationChangeObserver,
 
   // Holds removed windows that has an item from default container.
   ScopedObserver<aura::Window, aura::WindowObserver> observed_removed_windows_;
-
-  // Holds all observed activation clients.
-  ScopedObserverWithDuplicatedSources<aura::client::ActivationClient,
-                                      aura::client::ActivationChangeObserver>
-      observed_activation_clients_;
 
   DISALLOW_COPY_AND_ASSIGN(ShelfWindowWatcher);
 };
