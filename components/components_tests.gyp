93fdb032d01ea2f3ebfa373385af79f2ad6dab3e
dimich <dimich@chromium.org>
2016-07-26 14:44:11 -0700

    Add support for OfflinePagesDownloadBridge.
    This adds initial DownloadUIAdapter and DownloadUIItem.
    Adapter is a class that caches filtered subset of OfflinePages that should be
    shown in DownloadsUI. DownloadUIItem mimics DownloadItem and is a collection of fields
    needed for UI.
    
    BUG=630817
    
    Review-Url: https://codereview.chromium.org/2176013002
    Cr-Commit-Position: refs/heads/master@{#407923}

--- components/components_tests.gyp
+++ components/components_tests.gyp
@@ -443,6 +443,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'offline_pages/background/request_queue_unittest.cc',
       'offline_pages/background/save_page_request_unittest.cc',
     ],
+    'offline_pages_downloads_unittest_sources': [
+      'offline_pages/downloads/download_ui_adapter_unittest.cc',
+    ],
     'offline_pages_unittest_sources': [
       'offline_pages/archive_manager_unittest.cc',
       'offline_pages/client_policy_controller_unittest.cc',
---
+++ components/offline_pages/downloads/download_ui_item.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/offline_pages/downloads/download_ui_item.h"
+
+namespace offline_pages {
+
+DownloadUIItem::DownloadUIItem()
+    : total_bytes(0) {
+}
+
+DownloadUIItem::DownloadUIItem(const OfflinePageItem& page)
+    : guid(page.client_id.id),
+      url(page.url),
+      target_path(page.file_path),
+      start_time(page.creation_time),
+      total_bytes(page.file_size) {}
+
+DownloadUIItem::~DownloadUIItem() {
+}
+
+}  // namespace offline_pages
