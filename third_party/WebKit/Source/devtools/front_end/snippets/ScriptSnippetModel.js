57363aa4eb9cae4f66c40489de80b7a3f2544d2f
kozyatinskiy <kozyatinskiy@chromium.org>
2016-07-25 21:18:41 -0700

    [DevTools] Better locations for snippets
    - highlight errors in snippet source;
    - fix location of Syntax Errors in console messge;
    - fix location of Snippet result in console message
    
    BUG=479712
    R=lushnikov@chromium.org
    
    Review-Url: https://codereview.chromium.org/2173233002
    Cr-Commit-Position: refs/heads/master@{#407706}

--- third_party/WebKit/Source/devtools/front_end/snippets/ScriptSnippetModel.js
+++ third_party/WebKit/Source/devtools/front_end/snippets/ScriptSnippetModel.js
@@ -226,9 +226,17 @@ WebInspector.ScriptSnippetModel.prototype = {
         var mapping = this._mappingForTarget.get(target);
         mapping._setEvaluationIndex(evaluationIndex, uiSourceCode);
         var evaluationUrl = mapping._evaluationSourceURL(uiSourceCode);
-        var expression = uiSourceCode.workingCopy();
-        WebInspector.console.show();
-        runtimeModel.compileScript(expression, "", true, executionContext.id, compileCallback.bind(this));
+        uiSourceCode.requestContent().then(compileSnippet.bind(this));
+
+        /**
+         * @this {WebInspector.ScriptSnippetModel}
+         */
+        function compileSnippet()
+        {
+            var expression = uiSourceCode.workingCopy();
+            WebInspector.console.show();
+            runtimeModel.compileScript(expression, "", true, executionContext.id, compileCallback.bind(this));
+        }
 
         /**
          * @param {!RuntimeAgent.ScriptId=} scriptId
@@ -241,12 +249,12 @@ WebInspector.ScriptSnippetModel.prototype = {
             if (mapping.evaluationIndex(uiSourceCode) !== evaluationIndex)
                 return;
 
+            mapping._addScript(executionContext.debuggerModel.scriptForId(scriptId || exceptionDetails.scriptId), uiSourceCode);
             if (!scriptId) {
                 this._printRunOrCompileScriptResultFailure(target, exceptionDetails, evaluationUrl);
                 return;
             }
 
-            mapping._addScript(executionContext.debuggerModel.scriptForId(scriptId), uiSourceCode);
             var breakpointLocations = this._removeBreakpoints(uiSourceCode);
             this._restoreBreakpoints(uiSourceCode, breakpointLocations);
 
@@ -273,7 +281,7 @@ WebInspector.ScriptSnippetModel.prototype = {
         function runCallback(target, result, exceptionDetails)
         {
             if (!exceptionDetails)
-                this._printRunScriptResult(target, result, sourceURL);
+                this._printRunScriptResult(target, result, scriptId, sourceURL);
             else
                 this._printRunOrCompileScriptResultFailure(target, exceptionDetails, sourceURL);
         }
@@ -282,9 +290,10 @@ WebInspector.ScriptSnippetModel.prototype = {
     /**
      * @param {!WebInspector.Target} target
      * @param {?RuntimeAgent.RemoteObject} result
+     * @param {!RuntimeAgent.ScriptId} scriptId
      * @param {?string=} sourceURL
      */
-    _printRunScriptResult: function(target, result, sourceURL)
+    _printRunScriptResult: function(target, result, scriptId, sourceURL)
     {
         var consoleMessage = new WebInspector.ConsoleMessage(
             target,
@@ -297,7 +306,10 @@ WebInspector.ScriptSnippetModel.prototype = {
             undefined,
             undefined,
             [result],
-            undefined);
+            undefined,
+            undefined,
+            undefined,
+            scriptId);
         target.consoleModel.addMessage(consoleMessage);
     },
 
@@ -319,7 +331,10 @@ WebInspector.ScriptSnippetModel.prototype = {
             exceptionDetails.columnNumber,
             undefined,
             undefined,
-            exceptionDetails.stackTrace);
+            exceptionDetails.stackTrace,
+            undefined,
+            undefined,
+            exceptionDetails.stackTrace ? undefined : exceptionDetails.scriptId);
         target.consoleModel.addMessage(consoleMessage);
     },
 
