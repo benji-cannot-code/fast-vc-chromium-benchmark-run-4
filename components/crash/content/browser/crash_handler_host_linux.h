dff5c84f6c4138fa97ac4315c7324fea669ca614
wzhong <wzhong@chromium.org>
2016-07-25 17:55:44 -0700

    Add retry logic for FindThreadIDWithSyscall()
    Crashing thread might be in "running" state, i.e. after sys_sendmsg()
    and before sys_read().
    
    Wait for a short period in browser process, allow the crashing thread to
    catch up and retry.
    
    BUG=internal b/30003601
    TEST=Manually crash the main thread in utility process and make sure
         thread ID is translated.
    
    Review-Url: https://codereview.chromium.org/2169063002
    Cr-Commit-Position: refs/heads/master@{#407662}

--- components/crash/content/browser/crash_handler_host_linux.h
+++ components/crash/content/browser/crash_handler_host_linux.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/message_loop/message_loop.h"
 #include "base/threading/sequenced_worker_pool.h"
+#include "components/crash/content/app/breakpad_linux_impl.h"
 
 namespace base {
 class Thread;
@@ -71,6 +72,19 @@ class CrashHandlerHostLinux : public base::MessageLoopForIO::Watcher,
 
   // Continue OnFileCanReadWithoutBlocking()'s work on the IO thread.
   void QueueCrashDumpTask(std::unique_ptr<BreakpadInfo> info, int signal_fd);
+
+  // Find crashing thread (may delay and retry) and dump on IPC thread.
+  void FindCrashingThreadAndDump(pid_t crashing_pid,
+                                 const std::string& expected_syscall_data,
+                                 std::unique_ptr<char[]> crash_context,
+                                 std::unique_ptr<CrashKeyStorage> crash_keys,
+#if defined(ADDRESS_SANITIZER)
+                                 std::unique_ptr<char[]> asan_report,
+#endif
+                                 uint64_t uptime,
+                                 size_t oom_size,
+                                 int signal_fd,
+                                 int attempt);
 
   std::string process_type_;
   base::FilePath dumps_path_;
