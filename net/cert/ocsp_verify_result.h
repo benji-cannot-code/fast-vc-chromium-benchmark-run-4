936da2b616e2decb60a2fd14bdd7d6999363c7a1
dadrian <dadrian@google.com>
2016-07-26 14:33:24 -0700

    Enable Expect-Staple in SSLClientSocket.
    In TransportSecurityState, set |enable_static_expect_staple_|
    to true by default. Update SSLClientSocket to call
    TransportSecurityState::ProcessExpectStaple. Implements
    operator== for OCSPVerifyResult, to make sure OCSP data
    is not accidentally lost.
    
    In ssl_client_socket_impl.cc, this also removes the if
    (|signed_certificate_timestamps_enabled_) check around
    extracting the OCSP response and setting the
    UMA_HISTOGRAM_BOOLEAN("Net.OCSPResponseStapled"). Since
    SCTs are always enabled, this if statement was a noop.
    
    This does not enable Expect-Staple for QUIC. See
    https://crbug.com/631101
    
    BUG=598021
    
    Review-Url: https://codereview.chromium.org/2176413002
    Cr-Commit-Position: refs/heads/master@{#407921}

--- net/cert/ocsp_verify_result.h
+++ net/cert/ocsp_verify_result.h
@@ -25,6 +25,8 @@ struct NET_EXPORT OCSPVerifyResult {
   OCSPVerifyResult(const OCSPVerifyResult&);
   ~OCSPVerifyResult();
 
+  bool operator==(const OCSPVerifyResult& other) const;
+
   enum ResponseStatus {
     // No OCSPResponse was stapled.
     MISSING,
