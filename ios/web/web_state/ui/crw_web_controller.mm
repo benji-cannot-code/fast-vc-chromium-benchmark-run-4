e098540787a409f0641a7d8a58614fcd24d55cc0
eugenebut <eugenebut@chromium.org>
2016-07-25 17:44:28 -0700

    Removed _lastSeenWindowID ivar from CRWWebController as unused.
    BUG=628832
    
    Review-Url: https://codereview.chromium.org/2158363006
    Cr-Commit-Position: refs/heads/master@{#407657}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -326,8 +326,6 @@ NSError* WKWebViewErrorWithSource(NSError* error, WKWebViewErrorSource source) {
   // TODO(droger): Remove |_observerBridges| when all CRWWebControllerObservers
   // are converted to WebStateObservers.
   ScopedVector<web::WebControllerObserverBridge> _observerBridges;
-  // |windowId| that is saved when a page changes. Used to detect refreshes.
-  base::scoped_nsobject<NSString> _lastSeenWindowID;
   // YES if a user interaction has been registered at any time once the page has
   // loaded.
   BOOL _userInteractionRegistered;
@@ -1041,7 +1039,6 @@ const NSTimeInterval kSnapshotOverlayTransition = 0.5;
         [[CRWJSInjectionReceiver alloc] initWithEvaluator:self]);
     _windowIDJSManager.reset([(CRWJSWindowIdManager*)[_jsInjectionReceiver
         instanceOfClass:[CRWJSWindowIdManager class]] retain]);
-    _lastSeenWindowID.reset();
     _webViewProxy.reset(
         [[CRWWebViewProxyImpl alloc] initWithWebController:self]);
     [[_webViewProxy scrollViewProxy] addObserver:self];
@@ -3205,7 +3202,6 @@ const NSTimeInterval kSnapshotOverlayTransition = 0.5;
 - (void)resetDocumentSpecificState {
   _lastUserInteraction.reset();
   _clickInProgress = NO;
-  _lastSeenWindowID.reset([[_windowIDJSManager windowId] copy]);
 }
 
 - (void)didStartLoadingURL:(const GURL&)url updateHistory:(BOOL)updateHistory {
