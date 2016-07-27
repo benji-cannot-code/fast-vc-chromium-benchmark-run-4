21a1e8174197962cc14c495dd0a32d7ea94ce633
dgozman <dgozman@chromium.org>
2016-07-26 19:53:19 -0700

    [DevTools] Do not expose findSource{Map}URL from v8_inspector.
    BUG=631162
    
    Review-Url: https://codereview.chromium.org/2178153004
    Cr-Commit-Position: refs/heads/master@{#408025}

--- third_party/WebKit/Source/platform/v8_inspector/V8DebuggerAgentImpl.cpp
+++ third_party/WebKit/Source/platform/v8_inspector/V8DebuggerAgentImpl.cpp
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/v8_inspector/V8RuntimeAgentImpl.h"
 #include "platform/v8_inspector/V8StackTraceImpl.h"
 #include "platform/v8_inspector/V8StringUtil.h"
-#include "platform/v8_inspector/public/V8ContentSearchUtil.h"
 #include "platform/v8_inspector/public/V8Debugger.h"
 #include "platform/v8_inspector/public/V8DebuggerClient.h"
 
@@ -996,15 +995,15 @@ void V8DebuggerAgentImpl::didParseSource(std::unique_ptr<V8DebuggerScript> scrip
     String16 scriptSource = toProtocolString(script->source(m_isolate));
     bool isDeprecatedSourceURL = false;
     if (!success)
-        script->setSourceURL(V8ContentSearchUtil::findSourceURL(scriptSource, false, &isDeprecatedSourceURL));
+        script->setSourceURL(findSourceURL(scriptSource, false, &isDeprecatedSourceURL));
     else if (script->hasSourceURL())
-        V8ContentSearchUtil::findSourceURL(scriptSource, false, &isDeprecatedSourceURL);
+        findSourceURL(scriptSource, false, &isDeprecatedSourceURL);
 
     bool isDeprecatedSourceMappingURL = false;
     if (!success)
-        script->setSourceMappingURL(V8ContentSearchUtil::findSourceMapURL(scriptSource, false, &isDeprecatedSourceMappingURL));
+        script->setSourceMappingURL(findSourceMapURL(scriptSource, false, &isDeprecatedSourceMappingURL));
     else if (!script->sourceMappingURL().isEmpty())
-        V8ContentSearchUtil::findSourceMapURL(scriptSource, false, &isDeprecatedSourceMappingURL);
+        findSourceMapURL(scriptSource, false, &isDeprecatedSourceMappingURL);
 
     bool isContentScript = script->isContentScript();
     bool isInternalScript = script->isInternalScript();
--- third_party/WebKit/Source/platform/v8_inspector/V8StringUtil.cpp
+++ third_party/WebKit/Source/platform/v8_inspector/V8StringUtil.cpp
@@ -194,8 +194,6 @@ std::vector<std::unique_ptr<protocol::Debugger::SearchMatch>> searchInTextByLine
     return result;
 }
 
-namespace V8ContentSearchUtil {
-
 String16 findSourceURL(const String16& content, bool multiline, bool* deprecated)
 {
     return findMagicComment(content, "sourceURL", multiline, deprecated);
@@ -205,6 +203,8 @@ String16 findSourceMapURL(const String16& content, bool multiline, bool* depreca
 {
     return findMagicComment(content, "sourceMappingURL", multiline, deprecated);
 }
+
+namespace V8ContentSearchUtil {
 
 std::unique_ptr<protocol::Array<protocol::Debugger::API::SearchMatch>> searchInTextByLines(V8InspectorSession* session, const String16& text, const String16& query, const bool caseSensitive, const bool isRegex)
 {
