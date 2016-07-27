873adad87b3497f88a730c187c6c965988fd2d69
michaelpg <michaelpg@chromium.org>
2016-07-27 00:55:50 -0700

    MD Settings: Flush templates in settings-main before checking
    An odd interleaving of async calls can cause dom-ifs not to be updated yet
    in a function scheduled by async(). This causes expected elements to not
    exist at the moment we look for them, even though the properties are correct.
    
    TBR=tommycli@chromium.org
    BUG=631891,593989
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2182283003
    Cr-Commit-Position: refs/heads/master@{#408074}

--- chrome/browser/resources/settings/settings_main/settings_main.js
+++ chrome/browser/resources/settings/settings_main/settings_main.js
@@ -131,7 +131,7 @@ Polymer({
         this.advancedToggleExpanded_ = true;
     }
 
-    // Wait for the dom-if changes prior to calculating the overflow padding.
+    // Wait for any other changes prior to calculating the overflow padding.
     this.async(function() {
       this.$.overscroll.style.paddingBottom = this.overscrollHeight_() + 'px';
     });
@@ -150,6 +150,9 @@ Polymer({
         this.showPages_.about) {
       return 0;
     }
+
+    // Ensure any dom-if reflects the current properties.
+    Polymer.dom.flush();
 
     /**
      * @param {!Element} element
