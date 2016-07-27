1cd0869573026cbaefe7c7859cd3d5453c77b62b
bnc <bnc@chromium.org>
2016-07-27 12:07:57 -0700

    Remove unused NetPrefObserver forward declaration.
    This CL removes the last reference of NetPrefObserver from the codebase.  This
    is a follow-up to https://crrev.com/2172543003.
    
    Review-Url: https://codereview.chromium.org/2179403003
    Cr-Commit-Position: refs/heads/master@{#408208}

--- chrome/browser/profiles/profile_impl.h
+++ chrome/browser/profiles/profile_impl.h
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/host_zoom_map.h"
 
-class NetPrefObserver;
 class PrefService;
 
 class TrackedPreferenceValidationDelegate;
