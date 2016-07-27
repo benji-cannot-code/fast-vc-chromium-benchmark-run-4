3b293e81ce8774ae7ada2adda7fce960cb0282ff
pennymac <pennymac@chromium.org>
2016-07-27 16:24:35 -0700

    [chrome_elf] Removing blacklist finch for dynamic dll changes.
    Part of chrome_elf cleanup.  This was never used in the wild, and
    we won't need it in the foreseeable future.
    
    Leaving beacon and emergency disable finch switch for safety.
    
    Tests:
    1) chrome_elf_unittests, chrome_elf_util_unittest.cc: BlacklistTest*
    2) unit_tests, chrome_elf_init_unittest_win.cc: ChromeBlacklistTrialTest*
    R=robertshield@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    BUG=631771
    
    Review-Url: https://codereview.chromium.org/2163803003
    Cr-Commit-Position: refs/heads/master@{#408269}

--- chrome_elf/chrome_elf_constants.cc
+++ chrome_elf/chrome_elf_constants.cc
@@ -20,10 +20,6 @@ namespace blacklist {
 
 const wchar_t kRegistryBeaconPath[] =
     L"SOFTWARE\\" PRODUCT_STRING_PATH L"\\BLBeacon";
-const wchar_t kRegistryFinchListPath[] =
-    L"SOFTWARE\\" PRODUCT_STRING_PATH L"\\BLFinchList";
-const char kRegistryFinchListValueNameStr[] = "BLDlls";
-const wchar_t kRegistryFinchListValueName[] = L"BLDlls";
 const wchar_t kBeaconVersion[] = L"version";
 const wchar_t kBeaconState[] = L"state";
 const wchar_t kBeaconAttemptCount[] = L"failed_count";
@@ -36,7 +32,6 @@ namespace elf_sec {
 
 const wchar_t kRegSecurityFinchPath[] =
     L"SOFTWARE\\" PRODUCT_STRING_PATH L"\\BrowserSboxFinch";
-
 const wchar_t kRegSecurityPath[] =
     L"SOFTWARE\\" PRODUCT_STRING_PATH L"\\BrowserSec";
 
