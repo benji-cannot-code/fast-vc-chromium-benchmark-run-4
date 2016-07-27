c24a4e5c2c746fe5dcce40f637a483a073913f4a
hubbe <hubbe@chromium.org>
2016-07-27 12:14:08 -0700

    Add some actual data to gfx::ColorSpace
    To be followed up with gfx::ColorTransform which will actually do something with the data.
    
    BUG=622133
    
    Review-Url: https://codereview.chromium.org/2183953002
    Cr-Commit-Position: refs/heads/master@{#408209}

--- ui/gfx/ipc/color/gfx_param_traits_macros.h
+++ ui/gfx/ipc/color/gfx_param_traits_macros.h
@@ -16,6 +16,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #undef IPC_MESSAGE_EXPORT
 #define IPC_MESSAGE_EXPORT GFX_IPC_COLOR_EXPORT
 
+IPC_ENUM_TRAITS_MAX_VALUE(gfx::ColorSpace::PrimaryID,
+                          gfx::ColorSpace::PrimaryID::LAST);
+IPC_ENUM_TRAITS_MAX_VALUE(gfx::ColorSpace::TransferID,
+                          gfx::ColorSpace::TransferID::LAST);
+IPC_ENUM_TRAITS_MAX_VALUE(gfx::ColorSpace::MatrixID,
+                          gfx::ColorSpace::MatrixID::LAST);
+IPC_ENUM_TRAITS_MAX_VALUE(gfx::ColorSpace::RangeID,
+                          gfx::ColorSpace::RangeID::LAST);
+
 IPC_STRUCT_TRAITS_BEGIN(gfx::ICCProfile)
   IPC_STRUCT_TRAITS_MEMBER(valid_)
   IPC_STRUCT_TRAITS_MEMBER(data_)
