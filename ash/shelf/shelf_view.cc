58a4f20aa0c9c66f31e03cc33a0a6eac57f5c431
jamescook <jamescook@chromium.org>
2016-07-27 16:05:56 -0700

    Eliminate ash::Shell access in ShelfView
    This is a small step toward conversion it to ash common types.
    
    BUG=615155
    TEST=manual, show context menu and change auto-hide and alignment options,
    plus ash_unittests
    
    Review-Url: https://codereview.chromium.org/2186913003
    Cr-Commit-Position: refs/heads/master@{#408261}

--- ash/shelf/shelf_view.cc
+++ ash/shelf/shelf_view.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/shelf/shelf_delegate.h"
 #include "ash/common/shelf/shelf_menu_model.h"
 #include "ash/common/shelf/shelf_model.h"
+#include "ash/common/shelf/wm_shelf.h"
 #include "ash/common/shell_delegate.h"
 #include "ash/common/wm/root_window_finder.h"
 #include "ash/common/wm_shell.h"
@@ -28,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf.h"
 #include "ash/shelf/shelf_icon_observer.h"
 #include "ash/shelf/shelf_widget.h"
-#include "ash/shell.h"
 #include "base/auto_reset.h"
 #include "base/metrics/histogram.h"
 #include "grit/ash_strings.h"
@@ -1381,7 +1381,7 @@ void ShelfView::ToggleOverflowBubble() {
 
   overflow_bubble_->Show(overflow_button_, overflow_view);
 
-  Shell::GetInstance()->UpdateShelfVisibility();
+  wm_shelf_->UpdateVisibilityState();
 }
 
 void ShelfView::OnFadeOutAnimationEnded() {
@@ -1782,7 +1782,7 @@ void ShelfView::ShowContextMenuForView(views::View* source,
 
   const ShelfItem* item = ShelfItemForView(source);
   if (!item) {
-    Shell::GetInstance()->ShowContextMenu(point, source_type);
+    WmShell::Get()->ShowContextMenu(point, source_type);
     return;
   }
 
@@ -1862,7 +1862,8 @@ void ShelfView::OnMenuClosed(views::InkDrop* ink_drop) {
   menu_model_.reset();
   scoped_target_root_window_.reset();
 
-  Shell::GetInstance()->UpdateShelfVisibility();
+  // Auto-hide or alignment might have changed, but only for this shelf.
+  wm_shelf_->UpdateVisibilityState();
 }
 
 void ShelfView::OnBoundsAnimatorProgressed(views::BoundsAnimator* animator) {
