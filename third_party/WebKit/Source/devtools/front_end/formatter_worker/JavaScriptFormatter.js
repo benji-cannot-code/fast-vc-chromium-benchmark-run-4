6eb28be03324a567d26cd23f382941e6e3f8bb8f
lushnikov <lushnikov@chromium.org>
2016-07-27 12:02:59 -0700

    DevTools: fix formatting of continue statement
    BUG=630718
    R=kozyatinskiy
    
    Review-Url: https://codereview.chromium.org/2184193003
    Cr-Commit-Position: refs/heads/master@{#408206}

--- third_party/WebKit/Source/devtools/front_end/formatter_worker/JavaScriptFormatter.js
+++ third_party/WebKit/Source/devtools/front_end/formatter_worker/JavaScriptFormatter.js
@@ -300,7 +300,7 @@ WebInspector.JavaScriptFormatter.prototype = {
                 if (node.consequent.type !== "BlockStatement")
                     return "<";
             }
-        } else if (node.type === "BreakStatement" || node.type === "ThrowStatement" || node.type === "ReturnStatement" || node.type === "ExpressionStatement") {
+        } else if (node.type === "BreakStatement" || node.type === "ContinueStatement" || node.type === "ThrowStatement" || node.type === "ReturnStatement" || node.type === "ExpressionStatement") {
             return "n";
         }
         return "";
