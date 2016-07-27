a8260fcf01d28fd785ed486e0f8cb556224a2204
flandy <flandy@google.com>
2016-07-26 19:47:03 -0700

    DevTools: Optimize source color picker to analyze changed lines
    -Analyze the new lines from onTextChanged when updating the color swatches.
    (CodeMirror handles the bookmarks for any old lines that have been deleted)
    -Use bookmark find() method to keep track of swatch positions.
    -Remove the timeout so that swatches are updated immediately.
    -Only analyze a set number of columns (currently 300) to improve loading
    of minified style sheets.
    
    R=lushnikov
    
    Review-Url: https://codereview.chromium.org/2173913002
    Cr-Commit-Position: refs/heads/master@{#408022}

--- third_party/WebKit/Source/devtools/front_end/externs.js
+++ third_party/WebKit/Source/devtools/front_end/externs.js
@@ -409,6 +409,7 @@ CodeMirror.prototype = {
     eachLine: function(from, to, op) { },
     execCommand: function(cmd) { },
     extendSelection: function(from, to) { },
+    findMarks: function(from, to) { },
     findMarksAt: function(pos) { },
     /**
      * @param {!CodeMirror.Pos} from
@@ -589,7 +590,8 @@ CodeMirror.StringStream.prototype = {
 /** @constructor */
 CodeMirror.TextMarker = function(doc, type) { }
 CodeMirror.TextMarker.prototype = {
-    clear: function() { }
+    clear: function() { },
+    find: function() { }
 }
 
 /** @type {Object.<string, !Object.<string, string>>} */
