6b784fa8634187fcdaad74af9b4f956bede78d85
flackr <flackr@chromium.org>
2016-07-26 18:11:42 -0700

    Ensure that we consistently check contains: paint for fixed position containment.
    contains: paint; should contain fixed position descendants, however we had only
    updated this check in one place while we have many other places in the code
    which simply checked for having transform related properties. This patch
    attempts to unify all of these code paths to call through
    ComputedStyle::canContainFixedPositionObjects.
    
    TEST=fast/css/containment/paint-containment-with-fixed-position-scrolled.html, LayoutGeometryMapTest.ContainsFixedPositionTest, MapCoordinatesTest.FixedPosInTransform, MapCoordinatesTest.FixedPosInContainPaint
    BUG=619999
    
    Review-Url: https://codereview.chromium.org/2173963002
    Cr-Commit-Position: refs/heads/master@{#408002}

--- third_party/WebKit/Source/core/layout/LayoutBoxModelObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBoxModelObject.cpp
@@ -1025,7 +1025,7 @@ const LayoutObject* LayoutBoxModelObject::pushMappingToContainer(const LayoutBox
 
     bool isInline = isLayoutInline();
     bool isFixedPos = !isInline && style()->position() == FixedPosition;
-    bool hasTransform = !isInline && hasLayer() && layer()->transform();
+    bool containsFixedPosition = canContainFixedPositionObjects();
 
     LayoutSize adjustmentForSkippedAncestor;
     if (ancestorSkipped) {
@@ -1051,8 +1051,8 @@ const LayoutObject* LayoutBoxModelObject::pushMappingToContainer(const LayoutBox
         flags |= IsNonUniform;
     if (isFixedPos)
         flags |= IsFixedPosition;
-    if (hasTransform)
-        flags |= HasTransform;
+    if (containsFixedPosition)
+        flags |= ContainsFixedPosition;
     if (shouldUseTransformFromContainer(container)) {
         TransformationMatrix t;
         getTransformFromContainer(container, containerOffset, t);
