ee970bd7dc025f0b72b3b32dc19497e218f1aac9
sigbjornf <sigbjornf@opera.com>
2016-07-28 10:28:47 -0700

    Remove unnecessary eager finalization of PingLoaders.
    A PingLoader instance controls its own lifetime, releasing its
    self-keepalive reference upon completion of the load request. At that
    time it also lets go of its loader object (and stops the timer), hence
    there's no need to eagerly finalize this object -- it serves no real
    purpose.
    
    R=
    BUG=
    
    Review-Url: https://codereview.chromium.org/2180423005
    Cr-Commit-Position: refs/heads/master@{#408417}

--- third_party/WebKit/Source/core/loader/PingLoader.h
+++ third_party/WebKit/Source/core/loader/PingLoader.h
@@ -77,9 +77,6 @@ public:
 
     DECLARE_VIRTUAL_TRACE();
 
-    // Promptly finalize m_loader.
-    EAGERLY_FINALIZE();
-
 protected:
     PingLoader(LocalFrame*, ResourceRequest&, const FetchInitiatorInfo&, StoredCredentials);
 
