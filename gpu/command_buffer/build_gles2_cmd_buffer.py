435dfc0aa383bd2e847410fab2b61524186ecc3d
yunchao.he <yunchao.he@intel.com>
2016-07-27 19:16:15 -0700

    WebGL 2: Fix bugs in negativetextureapi.html for Mac
    BUG=631934
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2182443003
    Cr-Commit-Position: refs/heads/master@{#408313}

--- gpu/command_buffer/build_gles2_cmd_buffer.py
+++ gpu/command_buffer/build_gles2_cmd_buffer.py
@@ -2648,6 +2648,8 @@ _FUNCTION_INFO = {
     'trace_level': 1,
   },
   'CopyTexSubImage3D': {
+    'decoder_func': 'DoCopyTexSubImage3D',
+    'unit_test': False,
     'defer_reads': True,
     'unsafe': True,
     'trace_level': 1,
