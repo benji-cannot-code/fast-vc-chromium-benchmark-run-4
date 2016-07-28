7d246780c87867a1c09b3546b28d0b4430a21d60
jbauman <jbauman@chromium.org>
2016-07-28 14:21:44 -0700

    Enable hardware VPx decode by default.
    BUG=616318
    
    Review-Url: https://codereview.chromium.org/2183303002
    Cr-Commit-Position: refs/heads/master@{#408475}

--- content/browser/gpu/compositor_util.cc
+++ content/browser/gpu/compositor_util.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/base/switches.h"
 #include "content/browser/gpu/browser_gpu_memory_buffer_manager.h"
 #include "content/browser/gpu/gpu_data_manager_impl.h"
+#include "content/public/browser/gpu_utils.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "gpu/config/gpu_feature_type.h"
@@ -59,6 +60,13 @@ const GpuFeatureInfo GetGpuFeatureInfo(size_t index, bool* eof) {
   const base::CommandLine& command_line =
       *base::CommandLine::ForCurrentProcess();
   GpuDataManagerImpl* manager = GpuDataManagerImpl::GetInstance();
+  gpu::GpuPreferences gpu_preferences = GetGpuPreferencesFromCommandLine();
+
+  bool accelerated_vpx_disabled =
+      command_line.HasSwitch(switches::kDisableAcceleratedVideoDecode);
+#if defined(OS_WIN)
+  accelerated_vpx_disabled |= !gpu_preferences.enable_accelerated_vpx_decode;
+#endif
 
   const GpuFeatureInfo kGpuFeatureInfo[] = {
     {"2d_canvas",
@@ -142,9 +150,9 @@ const GpuFeatureInfo GetGpuFeatureInfo(size_t index, bool* eof) {
                        gpu::GPU_FEATURE_TYPE_ACCELERATED_VPX_DECODE) ||
                        manager->IsFeatureBlacklisted(
                            gpu::GPU_FEATURE_TYPE_ACCELERATED_VIDEO_DECODE),
-     command_line.HasSwitch(switches::kDisableAcceleratedVideoDecode),
-     "Accelerated VPx video decode has been disabled, either via blacklist,"
-     " about:flags or the command line.",
+     accelerated_vpx_disabled,
+     "Accelerated VPx video decode has been disabled, either via blacklist"
+     " or the command line.",
      true},
   };
   DCHECK(index < arraysize(kGpuFeatureInfo));
