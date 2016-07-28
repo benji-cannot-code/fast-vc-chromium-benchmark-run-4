ade90722323e68c70efa4849d51ad013053ff6d3
yoichio <yoichio@chromium.org>
2016-07-28 15:40:45 -0700

    [Editing][CodeHealth] Replace isContent/RichEditable with hasRichly/EditableStyle
    That's because isContentEditable does same as hasEditableStyle except calling
    document::updateStyleAndLayoutTree.
    
    This CL replaces isContentEditable(node) to
    node.document().updateStyleAndLayoutTree(); hasEditableStyle(node)
     and ditto with isRichEditable.
    
    TEST=No change in behavior
    
    Review-Url: https://codereview.chromium.org/2185343002
    Cr-Commit-Position: refs/heads/master@{#408502}

--- third_party/WebKit/Source/core/dom/Node.cpp
+++ third_party/WebKit/Source/core/dom/Node.cpp
@@ -2124,7 +2124,8 @@ bool Node::willRespondToMouseClickEvents()
 {
     if (isDisabledFormControl(this))
         return false;
-    return isContentEditable(*this) || hasEventListeners(EventTypeNames::mouseup) || hasEventListeners(EventTypeNames::mousedown) || hasEventListeners(EventTypeNames::click) || hasEventListeners(EventTypeNames::DOMActivate);
+    document().updateStyleAndLayoutTree();
+    return hasEditableStyle(*this) || hasEventListeners(EventTypeNames::mouseup) || hasEventListeners(EventTypeNames::mousedown) || hasEventListeners(EventTypeNames::click) || hasEventListeners(EventTypeNames::DOMActivate);
 }
 
 bool Node::willRespondToTouchEvents()
