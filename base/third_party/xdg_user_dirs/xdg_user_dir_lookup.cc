dcba59a41c7ce550138702a161f9d698809b5914
altimin <altimin@chromium.org>
2016-07-28 04:24:07 -0700

    Added include from xdg_user_dir_lookup.cc to xdg_user_dir_lookup.h
    Review-Url: https://codereview.chromium.org/2191513002
    Cr-Commit-Position: refs/heads/master@{#408372}

--- base/third_party/xdg_user_dirs/xdg_user_dir_lookup.cc
+++ base/third_party/xdg_user_dirs/xdg_user_dir_lookup.cc
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   SOFTWARE.
 */
 
+#include "base/third_party/xdg_user_dirs/xdg_user_dir_lookup.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
