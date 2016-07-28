b0d62f1dcd0b068c2698de5ad1d5b05194f3397a
gab <gab@chromium.org>
2016-07-28 12:05:07 -0700

    Revert of Revert "Allow SequencedTaskRunnerHandle::Get() while running unsequenced tasks." (patchset #2 id:40001 of https://codereview.chromium.org/2177373005/ )
    Reason for revert:
    Actually oops forgot this wasn't supposed to land until this blocker is resolved : https://bugs.chromium.org/p/chromium/issues/detail?id=618043#c5
    
    Original issue's description:
    > Revert "Allow SequencedTaskRunnerHandle::Get() while running unsequenced tasks."
    >
    > This is effectively a manual revert of 5d315e4b4a7cdef34b0c1e863353d03a30781217.
    >
    > Per the outcome of the discussion on http://crbug.com/618043 and associated doc/chromium-dev discussion.
    >
    > BUG=618043
    >
    > Committed: https://crrev.com/41339c7ae8458dd500d1be0e9d4b8da6d8c4c33b
    > Cr-Commit-Position: refs/heads/master@{#408440}
    
    TBR=danakj@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=618043
    
    Review-Url: https://codereview.chromium.org/2190073002
    Cr-Commit-Position: refs/heads/master@{#408448}

--- base/threading/sequenced_task_runner_handle_unittest.cc
+++ base/threading/sequenced_task_runner_handle_unittest.cc
@@ -28,9 +28,6 @@ namespace {
 
 class SequencedTaskRunnerHandleTest : public ::testing::Test {
  protected:
-  // Verifies that the context it runs on has a SequencedTaskRunnerHandle
-  // and that posting to it results in the posted task running in that same
-  // context (sequence). Runs |callback| on sequence when done.
   static void VerifyCurrentSequencedTaskRunner(const Closure& callback) {
     ASSERT_TRUE(SequencedTaskRunnerHandle::IsSet());
     scoped_refptr<SequencedTaskRunner> task_runner =
@@ -44,13 +41,6 @@ class SequencedTaskRunnerHandleTest : public ::testing::Test {
         FROM_HERE,
         base::Bind(&SequencedTaskRunnerHandleTest::CheckValidSequence,
                    base::Passed(&sequence_checker), callback));
-  }
-
-  // Verifies that there is no SequencedTaskRunnerHandle in the context it runs.
-  // Runs |callback| when done.
-  static void VerifyNoSequencedTaskRunner(const Closure& callback) {
-    ASSERT_FALSE(SequencedTaskRunnerHandle::IsSet());
-    callback.Run();
   }
 
  private:
@@ -82,9 +72,10 @@ TEST_F(SequencedTaskRunnerHandleTest, FromSequencedWorkerPoolTask) {
           &SequencedTaskRunnerHandleTest::VerifyCurrentSequencedTaskRunner,
           base::Bind(&WaitableEvent::Signal, base::Unretained(&event))));
   event.Wait();
+  owner.pool()->Shutdown();
 }
 
-TEST_F(SequencedTaskRunnerHandleTest, NoHandleFromUnsequencedTask) {
+TEST_F(SequencedTaskRunnerHandleTest, FromUnsequencedTask) {
   // Wrap the SequencedWorkerPool to avoid leaks due to its asynchronous
   // destruction.
   SequencedWorkerPoolOwner owner(3, "Test");
@@ -93,7 +84,7 @@ TEST_F(SequencedTaskRunnerHandleTest, NoHandleFromUnsequencedTask) {
   owner.pool()->PostWorkerTask(
       FROM_HERE,
       base::Bind(
-          &SequencedTaskRunnerHandleTest::VerifyNoSequencedTaskRunner,
+          &SequencedTaskRunnerHandleTest::VerifyCurrentSequencedTaskRunner,
           base::Bind(&WaitableEvent::Signal, base::Unretained(&event))));
   event.Wait();
 }
