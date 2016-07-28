56ae66a91563952b20218623ee5a27af3696580f
karandeepb <karandeepb@chromium.org>
2016-07-27 20:47:35 -0700

    MacViews: Fix setMarkedText in BridgedContentView to take replacementRange into account.
    This CL changes setMarkedText:selectedRange:replacementRange: method implemented
    by BridgedContentView to take replacementRange parameter into account. This
    prevents the insertion of redudant characters by the IME, when accented
    characters are input. Also, a test is added which fails on the current master.
    
    BUG=624657
    
    Review-Url: https://codereview.chromium.org/2177853002
    Cr-Commit-Position: refs/heads/master@{#408331}

--- ui/views/cocoa/bridged_content_view.mm
+++ ui/views/cocoa/bridged_content_view.mm
@@ -1222,6 +1222,8 @@ ui::KeyEvent GetCharacterEventFromNSEvent(NSEvent* event) {
 
   if ([text isKindOfClass:[NSAttributedString class]])
     text = [text string];
+
+  textInputClient_->DeleteRange(gfx::Range(replacementRange));
   ui::CompositionText composition;
   composition.text = base::SysNSStringToUTF16(text);
   composition.selection = gfx::Range(selectedRange);
