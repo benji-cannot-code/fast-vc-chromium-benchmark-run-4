72da5e8b0850d92add9f2c6220deadf890709184
lizeb <lizeb@chromium.org>
2016-07-28 07:59:15 -0700

    customtabs: Send time to first contentful paint to the caller.
    Time to First Contentful Paint is a very useful metric to assess page load
    performance. It is collected on the native side. This patch forwards it to the
    Java side on Android, and makes it available to the embedding app for Custom
    Tabs.
    
    BUG=624909
    
    Review-Url: https://codereview.chromium.org/2139243003
    Cr-Commit-Position: refs/heads/master@{#408391}

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -117,6 +117,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/spdyproxy/data_reduction_promo_infobar_delegate_android.h"
 #include "chrome/browser/net/spdyproxy/data_reduction_proxy_settings_android.h"
 #include "chrome/browser/notifications/notification_platform_bridge_android.h"
+#include "chrome/browser/page_load_metrics/observers/android_page_load_metrics_observer.h"
 #include "chrome/browser/password_manager/account_chooser_dialog_android.h"
 #include "chrome/browser/password_manager/auto_signin_first_run_dialog_android.h"
 #include "chrome/browser/password_manager/credential_android.h"
@@ -337,6 +338,7 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
     {"OmniboxViewUtil", OmniboxViewUtil::RegisterOmniboxViewUtil},
     {"OverlayPanelContent", RegisterOverlayPanelContent},
     {"PlatformUtil", platform_util::RegisterPlatformUtil},
+    {"PageLoadMetrics", RegisterPageLoadMetricsBindings},
     {"PartnerBookmarksReader",
      PartnerBookmarksReader::RegisterPartnerBookmarksReader},
     {"PasswordGenerationPopup",
