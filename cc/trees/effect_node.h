487b15135d3c01c431d92655e80eb8880f2acd4d
ajuma <ajuma@chromium.org>
2016-07-28 10:42:35 -0700

    cc: Add mask target size to the effect tree
    This removes another dependency of RenderSurfaceImpl on its
    owning layer.
    
    BUG=611883
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2188873002
    Cr-Commit-Position: refs/heads/master@{#408428}

--- cc/trees/effect_node.h
+++ cc/trees/effect_node.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/base/cc_export.h"
 #include "cc/output/filter_operations.h"
 #include "third_party/skia/include/core/SkXfermode.h"
+#include "ui/gfx/geometry/size_f.h"
 
 namespace base {
 namespace trace_event {
@@ -41,6 +42,8 @@ struct CC_EXPORT EffectNode {
   SkXfermode::Mode blend_mode;
 
   gfx::Vector2dF surface_contents_scale;
+
+  gfx::Size unscaled_mask_target_size;
 
   bool has_render_surface;
   RenderSurfaceImpl* render_surface;
