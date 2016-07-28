4aaff0f963a76ae183b968f7b0d1eb16ceb63fc8
sfiera <sfiera@chromium.org>
2016-07-28 03:20:49 -0700

    Start PopularSites fetch from separate function.
    Then, inject a PopularSites into MostVisited, instead of injecting all
    of its arguments and creating one locally.
    
    BUG=617966
    
    Review-Url: https://codereview.chromium.org/2179233003
    Cr-Commit-Position: refs/heads/master@{#408369}

--- chrome/browser/ui/webui/popular_sites_internals_message_handler.cc
+++ chrome/browser/ui/webui/popular_sites_internals_message_handler.cc
@@ -67,9 +67,11 @@ void PopularSitesInternalsMessageHandler::HandleRegisterForEvents(
       content::BrowserThread::GetBlockingPool(), profile->GetPrefs(),
       TemplateURLServiceFactory::GetForProfile(profile),
       g_browser_process->variations_service(), profile->GetRequestContext(),
-      ChromePopularSites::GetDirectory(), false,
+      ChromePopularSites::GetDirectory()));
+  popular_sites_->StartFetch(
+      false,
       base::Bind(&PopularSitesInternalsMessageHandler::OnPopularSitesAvailable,
-                 base::Unretained(this), false)));
+                 base::Unretained(this), false));
 }
 
 void PopularSitesInternalsMessageHandler::HandleUpdate(
@@ -108,7 +110,8 @@ void PopularSitesInternalsMessageHandler::HandleUpdate(
       content::BrowserThread::GetBlockingPool(), prefs,
       TemplateURLServiceFactory::GetForProfile(profile),
       g_browser_process->variations_service(), profile->GetRequestContext(),
-      ChromePopularSites::GetDirectory(), true, callback));
+      ChromePopularSites::GetDirectory()));
+  popular_sites_->StartFetch(true, callback);
 }
 
 void PopularSitesInternalsMessageHandler::HandleViewJson(
