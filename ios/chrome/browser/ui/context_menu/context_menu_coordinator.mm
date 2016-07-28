904b2fddc37e1e997062e9560d57842f64f3622e
gambard <gambard@chromium.org>
2016-07-28 10:19:34 -0700

    Remove code used for downstream compatibility
    This CL removes ContextMenuCoordinator code used for downstream compatibility
    during refactoring.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2156953002
    Cr-Commit-Position: refs/heads/master@{#408406}

--- ios/chrome/browser/ui/context_menu/context_menu_coordinator.mm
+++ ios/chrome/browser/ui/context_menu/context_menu_coordinator.mm
@@ -37,11 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return self;
 }
 
-- (instancetype)initWithViewController:(UIViewController*)viewController
-                                params:(const web::ContextMenuParams&)params {
-  return [self initWithBaseViewController:viewController params:params];
-}
-
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController {
   NOTREACHED();
   return nil;
