9c97e738fc11ad5447ffdf6b6bef5d3c43a3d1ec
hajimehoshi <hajimehoshi@chromium.org>
2016-07-28 08:41:26 -0700

    Make Resource::m_response private
    This CL makes |Resource::m_response| private and replaces the usage of
    this with |response()| calls.
    
    There was only one place where |m_response| was updated in the
    subclasses of Resource. This was in
    |ImageResource::onePartInMultipartReceived| and replaced with
    |setResponse()|. |setResponse()| updates not only |m_response| but
    also |m_cacheHandler|. Calling |setReponse()| is more appropriate
    for caching of a response which is multipart and fetched from
    ServiceWorker.
    
    BUG=n/a
    TEST=webkit_unit_tests --gtest_filter=ImageResourceTest.*
    
    Review-Url: https://codereview.chromium.org/2183323005
    Cr-Commit-Position: refs/heads/master@{#408399}

--- third_party/WebKit/Source/core/fetch/ImageResource.cpp
+++ third_party/WebKit/Source/core/fetch/ImageResource.cpp
@@ -359,7 +359,7 @@ inline void ImageResource::createImage()
     if (m_image)
         return;
 
-    if (m_response.mimeType() == "image/svg+xml") {
+    if (response().mimeType() == "image/svg+xml") {
         m_image = SVGImage::create(this);
     } else {
         m_image = BitmapImage::create(this);
@@ -457,7 +457,7 @@ void ImageResource::responseReceived(const ResourceResponse& response, std::uniq
         m_multipartParser = new MultipartImageResourceParser(response, response.multipartBoundary(), this);
     Resource::responseReceived(response, std::move(handle));
     if (RuntimeEnabledFeatures::clientHintsEnabled()) {
-        m_devicePixelRatioHeaderValue = m_response.httpHeaderField(HTTPNames::Content_DPR).toFloat(&m_hasDevicePixelRatioHeaderValue);
+        m_devicePixelRatioHeaderValue = this->response().httpHeaderField(HTTPNames::Content_DPR).toFloat(&m_hasDevicePixelRatioHeaderValue);
         if (!m_hasDevicePixelRatioHeaderValue || m_devicePixelRatioHeaderValue <= 0.0) {
             m_devicePixelRatioHeaderValue = 1.0;
             m_hasDevicePixelRatioHeaderValue = false;
@@ -541,7 +541,7 @@ void ImageResource::reloadIfLoFi(ResourceFetcher* fetcher)
 {
     if (m_resourceRequest.loFiState() != WebURLRequest::LoFiOn)
         return;
-    if (isLoaded() && !m_response.httpHeaderField("chrome-proxy").contains("q=low"))
+    if (isLoaded() && !response().httpHeaderField("chrome-proxy").contains("q=low"))
         return;
     m_resourceRequest.setCachePolicy(WebCachePolicy::BypassingCache);
     m_resourceRequest.setLoFiState(WebURLRequest::LoFiOff);
@@ -565,7 +565,7 @@ void ImageResource::onePartInMultipartReceived(const ResourceResponse& response)
 {
     ASSERT(m_multipartParser);
 
-    m_response = response;
+    setResponse(response);
     if (m_multipartParsingState == MultipartParsingState::WaitingForFirstPart) {
         // We have nothing to do because we don't have any data.
         m_multipartParsingState = MultipartParsingState::ParsingFirstPart;
--- third_party/WebKit/Source/core/fetch/ScriptResource.cpp
+++ third_party/WebKit/Source/core/fetch/ScriptResource.cpp
@@ -106,7 +106,7 @@ void ScriptResource::destroyDecodedDataForFailedRevalidation()
 
 bool ScriptResource::mimeTypeAllowedByNosniff() const
 {
-    return parseContentTypeOptionsHeader(m_response.httpHeaderField(HTTPNames::X_Content_Type_Options)) != ContentTypeOptionsNosniff || MIMETypeRegistry::isSupportedJavaScriptMIMEType(httpContentType());
+    return parseContentTypeOptionsHeader(response().httpHeaderField(HTTPNames::X_Content_Type_Options)) != ContentTypeOptionsNosniff || MIMETypeRegistry::isSupportedJavaScriptMIMEType(httpContentType());
 }
 
 void ScriptResource::setIntegrityDisposition(ScriptIntegrityDisposition disposition)
