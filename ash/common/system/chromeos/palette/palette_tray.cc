d2d010ab172461d468690f1f21128326c93142b9
jdufault <jdufault@chromium.org>
2016-07-28 11:57:07 -0700

    Add switch to enable palette tray UI.
    BUG=625251
    
    Review-Url: https://codereview.chromium.org/2170173002
    Cr-Commit-Position: refs/heads/master@{#408447}

--- ash/common/system/chromeos/palette/palette_tray.cc
+++ ash/common/system/chromeos/palette/palette_tray.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/common/system/chromeos/palette/palette_tray.h"
 
+#include "ash/common/ash_switches.h"
 #include "ash/common/material_design/material_design_controller.h"
 #include "ash/common/shelf/shelf_constants.h"
 #include "ash/common/shelf/wm_shelf.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/wm_root_window_controller.h"
 #include "ash/common/wm_shell.h"
 #include "ash/common/wm_window.h"
+#include "base/command_line.h"
 #include "base/sys_info.h"
 #include "grit/ash_resources.h"
 #include "grit/ash_strings.h"
@@ -48,8 +50,8 @@ const int kPaletteWidth = 360;
 
 // Returns true if the command line flag is present that enables the palette.
 bool IsPaletteEnabled() {
-  // TODO(jdufault): Hookup this function when the flag is in Chrome.
-  return false;
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+      ash::switches::kAshEnablePalette);
 }
 
 // Creates a separator.
