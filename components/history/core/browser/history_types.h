4e8d6e45a0588a7c8bfd035f208773cd93a52128
jyquinn <jyquinn@chromium.org>
2016-07-28 10:44:27 -0700

    Add matching algorithm to history query options
    Add text matching algorithm to history query options
    to allow history queries to match against text regardless
    of length. Previously, local queries would only perform
    text matching for queries greater than 2 characters long.
    
    BUG=613591
    
    Review-Url: https://codereview.chromium.org/2187013002
    Cr-Commit-Position: refs/heads/master@{#408429}

--- components/history/core/browser/history_types.h
+++ components/history/core/browser/history_types.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/history_context.h"
 #include "components/history/core/browser/url_row.h"
 #include "components/history/core/common/thumbnail_score.h"
+#include "components/query_parser/query_parser.h"
 #include "ui/base/page_transition_types.h"
 #include "ui/gfx/geometry/size.h"
 #include "url/gurl.h"
@@ -272,6 +273,9 @@ struct QueryOptions {
   // Allows the caller to specify how duplicate URLs in the result set should
   // be handled. The default is REMOVE_DUPLICATES.
   DuplicateHandling duplicate_policy;
+
+  // Allows the caller to specify the matching algorithm for text queries.
+  query_parser::MatchingAlgorithm matching_algorithm;
 
   // Helpers to get the effective parameters values, since a value of 0 means
   // "unspecified".
