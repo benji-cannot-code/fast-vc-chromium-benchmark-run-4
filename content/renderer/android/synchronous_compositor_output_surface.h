2f023d664bcafdd0bb94316df54415ed9570ca68
boliu <boliu@chromium.org>
2016-07-27 17:09:45 -0700

    sync compositor: Set display viewport for software draw
    After switching to using cc::Display for software draw, this fixes the
    case where the viewport is not at the origin of the surface.
    
    BUG=631276
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2187563006
    Cr-Commit-Position: refs/heads/master@{#408288}

--- content/renderer/android/synchronous_compositor_output_surface.h
+++ content/renderer/android/synchronous_compositor_output_surface.h
@@ -105,6 +105,8 @@ class SynchronousCompositorOutputSurface
   void SetBeginFrameSource(cc::BeginFrameSource* begin_frame_source) override;
 
  private:
+  class SoftwareOutputSurface;
+
   void InvokeComposite(const gfx::Transform& transform,
                        const gfx::Rect& viewport,
                        const gfx::Rect& clip);
@@ -158,6 +160,8 @@ class SynchronousCompositorOutputSurface
   StubDisplayClient display_client_;
   // Uses surface_manager_.
   std::unique_ptr<cc::Display> display_;
+  // Owned by |display_|.
+  SoftwareOutputSurface* software_output_surface_ = nullptr;
 
   base::ThreadChecker thread_checker_;
 
