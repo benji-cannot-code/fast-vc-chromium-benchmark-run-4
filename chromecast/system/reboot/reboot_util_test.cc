f49eabaf0af8af7829eceeabe1c026da399d75ed
mbjorge <mbjorge@chromium.org>
2016-07-28 13:03:24 -0700

    [Chromecast] Fix reboot_util_test to use renamed api.
    RebootType was renamed to RebootSource, but the change was missed
    in this file. Update accordingly.
    
    BUG= internal b/27494734
    TEST= build all unittests
    
    Review-Url: https://codereview.chromium.org/2193493002
    Cr-Commit-Position: refs/heads/master@{#408457}

--- chromecast/system/reboot/reboot_util_test.cc
+++ chromecast/system/reboot/reboot_util_test.cc
@@ -9,25 +9,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromecast {
 
-TEST(RebootUtil, SingleSetGetLastRebootType) {
-  if (RebootUtil::SetLastRebootType(RebootShlib::RebootSource::FORCED)) {
-    EXPECT_EQ(RebootUtil::GetLastRebootType(),
+TEST(RebootUtil, SingleSetGetLastRebootSource) {
+  if (RebootUtil::SetLastRebootSource(RebootShlib::RebootSource::FORCED)) {
+    EXPECT_EQ(RebootUtil::GetLastRebootSource(),
               RebootShlib::RebootSource::FORCED);
   }
 }
 
-TEST(RebootUtil, MultipleSetGetLastRebootType) {
-  if (RebootUtil::SetLastRebootType(RebootShlib::RebootSource::FORCED)) {
-    EXPECT_EQ(RebootUtil::GetLastRebootType(),
+TEST(RebootUtil, MultipleSetGetLastRebootSource) {
+  if (RebootUtil::SetLastRebootSource(RebootShlib::RebootSource::FORCED)) {
+    EXPECT_EQ(RebootUtil::GetLastRebootSource(),
               RebootShlib::RebootSource::FORCED);
   }
 
-  if (RebootUtil::SetLastRebootType(RebootShlib::RebootSource::OTA)) {
-    EXPECT_EQ(RebootUtil::GetLastRebootType(), RebootShlib::RebootSource::OTA);
+  if (RebootUtil::SetLastRebootSource(RebootShlib::RebootSource::OTA)) {
+    EXPECT_EQ(RebootUtil::GetLastRebootSource(),
+              RebootShlib::RebootSource::OTA);
   }
 
-  if (RebootUtil::SetLastRebootType(RebootShlib::RebootSource::FDR)) {
-    EXPECT_EQ(RebootUtil::GetLastRebootType(), RebootShlib::RebootSource::FDR);
+  if (RebootUtil::SetLastRebootSource(RebootShlib::RebootSource::FDR)) {
+    EXPECT_EQ(RebootUtil::GetLastRebootSource(),
+              RebootShlib::RebootSource::FDR);
   }
 }
 
