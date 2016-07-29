57904fed14fb30e340b0a0c8cbf20af8332e61a2
hiroshige <hiroshige@chromium.org>
2016-07-28 23:03:10 -0700

    Call FetchContext::dispatchWillSendRequest() only if an actual load is started
    Fixing a regression since [1].
    [1] https://codereview.chromium.org/1889973002/
    
    Since [1], FetchContext::dispatchWillSendRequest() was called from
    ResourceFetcher::willSendRequest() in the middle of
    ResourceFetcher::requestResource(), and might have modified
    Resource::m_resourceRequest.
    However, if the actual loading is deferred, e.g. when FontResource is not
    used, FetchContext::dispatchWillSendRequest() was called without actual
    loading, causing stale pending load reports in DevTools.
    
    This CL
    - Moves the ResourceFetcher::willSendRequest() call in requestResource() to
      ResourceFetcher::startLoad().
      Now FetchContext::dispatchWillSendRequest() (called from startLoad())
      modifies the local ResourceRequest in startLoad(), not modifying
      ResourceRequest::m_resourceRequest (which is the behavior before [1]).
    - Moves setAllowStoredCredentials() call in ResourceFetcher::willSendRequest()
      to its callsites, because this should be done even when the actual loading
      is deferred and should modify Resource::m_resourceRequest.
    - Adds a layout test.
    
    BUG=623616, 632580
    
    Review-Url: https://codereview.chromium.org/2184823006
    Cr-Commit-Position: refs/heads/master@{#408588}

--- third_party/WebKit/Source/core/fetch/ResourceFetcher.cpp
+++ third_party/WebKit/Source/core/fetch/ResourceFetcher.cpp
@@ -467,8 +467,7 @@ Resource* ResourceFetcher::requestResource(FetchRequest& request, const Resource
 
     updateMemoryCacheStats(resource, policy, request, factory, isStaticData);
 
-    if (policy != Use)
-        willSendRequest(identifier, request.mutableResourceRequest(), ResourceResponse(), request.options());
+    request.mutableResourceRequest().setAllowStoredCredentials(request.options().allowCredentials == AllowStoredCredentials);
 
     switch (policy) {
     case Reload:
@@ -993,6 +992,9 @@ bool ResourceFetcher::startLoad(Resource* resource)
         return false;
     }
 
+    ResourceRequest request(resource->resourceRequest());
+    willSendRequest(resource->identifier(), request, ResourceResponse(), resource->options());
+
     ResourceLoader* loader = ResourceLoader::create(this, resource);
     if (resource->shouldBlockLoadEvent())
         m_loaders.add(loader);
@@ -1001,7 +1003,7 @@ bool ResourceFetcher::startLoad(Resource* resource)
 
     storeResourceTimingInitiatorInformation(resource);
     resource->setFetcherSecurityOrigin(context().getSecurityOrigin());
-    loader->start(resource->resourceRequest(), context().loadingTaskRunner(), context().defersLoading());
+    loader->start(request, context().loadingTaskRunner(), context().defersLoading());
     return true;
 }
 
@@ -1071,13 +1073,13 @@ bool ResourceFetcher::willFollowRedirect(Resource* resource, ResourceRequest& ne
         bool crossOrigin = !redirectedSecurityOrigin->isSameSchemeHostPort(originalSecurityOrigin.get());
         it->value->addRedirect(redirectResponse, encodedDataLength, crossOrigin);
     }
+    newRequest.setAllowStoredCredentials(resource->options().allowCredentials == AllowStoredCredentials);
     willSendRequest(resource->identifier(), newRequest, redirectResponse, resource->options());
     return true;
 }
 
 void ResourceFetcher::willSendRequest(unsigned long identifier, ResourceRequest& newRequest, const ResourceResponse& redirectResponse, const ResourceLoaderOptions& options)
 {
-    newRequest.setAllowStoredCredentials(options.allowCredentials == AllowStoredCredentials);
     context().dispatchWillSendRequest(identifier, newRequest, redirectResponse, options.initiatorInfo);
 }
 
