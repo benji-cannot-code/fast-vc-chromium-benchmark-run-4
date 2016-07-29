68e379fb8a1f161ab160a11aed90dbfebfa396e9
csharrison <csharrison@chromium.org>
2016-07-28 18:00:24 -0700

    ParseHTMLOnMainThread: notify main thread of chunks less frequently
    This patch adds an experiment variant to the
    ParseHTMLOnMainThread experiment threaded group. It adds
    a group that uses the threaded parser, but coalesces token
    chunks in the background parser, notifying the main thread
    less often. This can often result in issuing preloads faster
    due to not eagerly parsing/scripting when tokens are about to
    come in.
    
    Note: this experiment group actually aligns the threaded behavior
    closer to the non-threaded behavior. The non-threaded experiment
    group also runs the entirety of pumpTokenizer when it is tokenizing,
    whereas the threaded parser usually starts parsing at the first call to
    notifyPendingTokenizedChunks (if it is idle). This is usually very early
    on in pumpTokenizer, because we split up chunks very fine grained.
    This experiment group tries not to start the main thread parsing until
    we've tokenized a bit more.
    
    TBR=kinuko@chromium.org
    
    BUG=632394
    
    Review-Url: https://codereview.chromium.org/2065163005
    Cr-Commit-Position: refs/heads/master@{#408537}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1380,6 +1380,7 @@ void MaybeAppendBlinkSettingsSwitchForFieldTrial(
     "DisallowFetchForDocWrittenScriptsInMainFrame",
 
     // Keys: parseHTMLOnMainThreadSyncTokenize
+    //       parseHTMLOnMainThreadCoalesceChunks
     "ParseHTMLOnMainThread",
   };
 
--- third_party/WebKit/Source/core/html/parser/BackgroundHTMLParser.h
+++ third_party/WebKit/Source/core/html/parser/BackgroundHTMLParser.h
@@ -63,6 +63,7 @@ public:
         // pendingTokenLimit
         size_t outstandingTokenLimit;
         size_t pendingTokenLimit;
+        bool shouldCoalesceChunks;
     };
 
     static void start(PassRefPtr<WeakReference<BackgroundHTMLParser>>, std::unique_ptr<Configuration>, const KURL& documentURL, std::unique_ptr<CachedDocumentParameters>, const MediaValuesCached::MediaValuesCachedData&, std::unique_ptr<WebTaskRunner>);
@@ -96,7 +97,11 @@ private:
     void appendDecodedBytes(const String&);
     void markEndOfFile();
     void pumpTokenizer();
-    void sendTokensToMainThread();
+
+    // Returns whether or not the HTMLDocumentParser should be notified of
+    // pending chunks.
+    bool queueChunkForMainThread();
+    void notifyMainThreadOfNewChunks();
     void updateDocument(const String& decodedData);
 
     template <typename FunctionType, typename... Ps>
@@ -129,6 +134,7 @@ private:
 
     bool m_startingScript;
     double m_lastBytesReceivedTime;
+    bool m_shouldCoalesceChunks;
 };
 
 } // namespace blink
