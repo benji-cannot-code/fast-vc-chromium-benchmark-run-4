9bcaf650099b13cefc9f6672d8cc3321240131bd
agrieve <agrieve@chromium.org>
2016-07-28 20:17:56 -0700

    Tidy up of TabPersistentStore's clean-up logic
    * Clean files on every activity start rather than just the first
    * Don't delete files owned by other TabModels
    * Don't delete icons used by other TabModels
    * Adds test for incognito tab files being removed appropraitely
    
    BUG=626629
    
    Review-Url: https://codereview.chromium.org/2143203002
    Cr-Commit-Position: refs/heads/master@{#408566}

--- chrome/android/javatests/src/org/chromium/chrome/browser/TabsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/TabsTest.java
@@ -52,8 +52,10 @@ import org.chromium.chrome.browser.tabmodel.TabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorImpl;
 import org.chromium.chrome.browser.tabmodel.TabModelUtils;
+import org.chromium.chrome.browser.tabmodel.TabPersistentStore;
 import org.chromium.chrome.browser.toolbar.ToolbarPhone;
 import org.chromium.chrome.test.ChromeTabbedActivityTestBase;
+import org.chromium.chrome.test.util.ApplicationTestUtils;
 import org.chromium.chrome.test.util.ChromeRestriction;
 import org.chromium.chrome.test.util.ChromeTabUtils;
 import org.chromium.chrome.test.util.MenuUtils;
@@ -70,6 +72,7 @@ import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.WebContentsObserver;
 import org.chromium.net.test.EmbeddedTestServer;
 
+import java.io.File;
 import java.util.Locale;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
@@ -1651,6 +1654,33 @@ public class TabsTest extends ChromeTabbedActivityTestBase {
         });
 
         assertTrue("WebContentsObserver was never destroyed", webContentsDestroyCalled.get());
+    }
+
+    @MediumTest
+    @Feature({"Android-TabSwitcher"})
+    public void testIncognitoTabsNotRestoredAfterSwipe() throws Exception {
+        newIncognitoTabFromMenu();
+
+        File tabStateDir = TabPersistentStore.getOrCreateStateDirectory();
+        TabModel normalModel = getActivity().getTabModelSelector().getModel(false);
+        TabModel incognitoModel = getActivity().getTabModelSelector().getModel(true);
+        File normalTabFile = new File(tabStateDir,
+                TabState.getTabStateFilename(
+                        normalModel.getTabAt(normalModel.getCount() - 1).getId(), false));
+        File incognitoTabFile = new File(tabStateDir,
+                TabState.getTabStateFilename(incognitoModel.getTabAt(0).getId(), true));
+
+        assertTrue(normalTabFile.getAbsolutePath(), normalTabFile.exists());
+        assertTrue(incognitoTabFile.getAbsolutePath(), incognitoTabFile.exists());
+
+        // Although we're destroying the activity, the Application will still live on since its in
+        // the same process as this test.
+        ApplicationTestUtils.finishActivity(getActivity());
+
+        // Activity will be started without a savedInstanceState.
+        startMainActivityOnBlankPage();
+        assertTrue(normalTabFile.exists());
+        assertFalse(incognitoTabFile.exists());
     }
 
     @Override
