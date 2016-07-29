fa5682359ec8f963f1b232e5c43eabdac2bc348c
boliu <boliu@chromium.org>
2016-07-29 08:39:21 -0700

    aw: Fix FactoryProvider threading
    Access thread-unsafe chromium classes on the UI thread only
    
    BUG=630709
    
    Review-Url: https://codereview.chromium.org/2180423003
    Cr-Commit-Position: refs/heads/master@{#408643}

--- android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewThreadTest.java
+++ android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewThreadTest.java
@@ -13,7 +13,6 @@ import android.webkit.WebStorage;
 
 import static org.chromium.base.test.util.ScalableTimeout.scaleTimeout;
 
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.webview_shell.WebViewThreadTestActivity;
 
 /**
@@ -85,7 +84,6 @@ public class WebViewThreadTest extends ActivityInstrumentationTestCase2<WebViewT
         });
     }
 
-    @DisabledTest
     @SmallTest
     public void testWebViewInitByWebStorage() throws InterruptedException {
         initThenCreateWebViewOnUiThread(new Runnable() {
