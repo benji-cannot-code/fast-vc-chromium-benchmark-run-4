4aa90b82d6dd0ee20bf003c26e7098ba738f2130
nzolghadr <nzolghadr@chromium.org>
2016-07-29 12:19:31 -0700

    Revert "Send got/lostpointercapture immediately if possible"
    This reverts commit 7d314cf3ad6a9f0fe163228f7a0efc5a72adc560 as a follow up to the decision about delayed capturing
    https://github.com/w3c/pointerevents/issues/32
    
    This also gets rid of the need to make PointerEventManager a GC object.
    
    BUG=627192, 629935
    
    Review-Url: https://codereview.chromium.org/2193473002
    Cr-Commit-Position: refs/heads/master@{#408714}

--- third_party/WebKit/LayoutTests/imported/wpt_automation/pointerevents/pointerevent_lostpointercapture_is_first-manual-automation.js
+++ third_party/WebKit/LayoutTests/imported/wpt_automation/pointerevents/pointerevent_lostpointercapture_is_first-manual-automation.js
@@ -3,5 +3,8 @@ importAutomationScript('/pointerevents/pointerevent_common_input.js');
 
 function inject_input() {
   mouseClickInTarget('btnCapture');
+
+  // To handle delayed capturing
+  mouseMoveIntoTarget('btnCapture');
 }
 
--- third_party/WebKit/LayoutTests/imported/wpt_automation/pointerevents/pointerevent_releasepointercapture_onpointerup_mouse-manual-automation.js
+++ third_party/WebKit/LayoutTests/imported/wpt_automation/pointerevents/pointerevent_releasepointercapture_onpointerup_mouse-manual-automation.js
@@ -3,5 +3,8 @@ importAutomationScript('/pointerevents/pointerevent_common_input.js');
 
 function inject_input() {
   mouseClickInTarget('btnCapture');
+
+  // To Handle delayed capturing
+  mouseMoveIntoTarget('btnCapture');
 }
 
