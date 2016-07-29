eb4e30b6d39c60244e127312e10078cd57b78387
varkha <varkha@chromium.org>
2016-07-29 14:23:27 -0700

    [ash-md] Properly scales windows with transient parents in overview
    Previously to this CL the code was only considering the focused window
    when calculating scale for the overview mode transform. It needs to
    consider the bounding rectangle of the whole transient parent's tree.
    
    BUG=632694
    
    Review-Url: https://codereview.chromium.org/2197773002
    Cr-Commit-Position: refs/heads/master@{#408763}

--- ash/common/wm/overview/window_grid.cc
+++ ash/common/wm/overview/window_grid.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/common/wm/overview/cleanup_animation_observer.h"
 #include "ash/common/wm/overview/scoped_overview_animation_settings.h"
 #include "ash/common/wm/overview/scoped_overview_animation_settings_factory.h"
-#include "ash/common/wm/overview/scoped_transform_overview_window.h"
 #include "ash/common/wm/overview/window_selector.h"
 #include "ash/common/wm/overview/window_selector_delegate.h"
 #include "ash/common/wm/overview/window_selector_item.h"
@@ -1015,7 +1014,7 @@ bool WindowGrid::FitWindowRectsInBounds(const gfx::Rect& bounds,
   const gfx::Size item_size(0, height);
   size_t i = 0;
   for (auto* window : window_list_) {
-    const gfx::Rect target_bounds = window->GetWindow()->GetTargetBounds();
+    const gfx::Rect target_bounds = window->GetTargetBoundsInScreen();
     const int width =
         std::max(1, gfx::ToFlooredInt(target_bounds.width() *
                                       window->GetItemScale(item_size)) +
