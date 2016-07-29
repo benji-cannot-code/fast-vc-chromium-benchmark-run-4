ff3af10c7c92d58edd8747bdc5065a904ab6636d
domlaskowski <domlaskowski@chromium.org>
2016-07-28 17:46:33 -0700

    exo: Convert initial bounds to screen coordinates
    Since the initial bounds of a ShellSurface were relative to the root
    window, pop-up windows (e.g. context menus in weston-terminal) whose
    parents are on a secondary display would appear on the primary display.
    
    BUG=631136
    TEST=Drag weston-terminal to secondary display and right-click.
    TEST=ARC apps can still be dragged and maximized.
    
    Review-Url: https://codereview.chromium.org/2175373003
    Cr-Commit-Position: refs/heads/master@{#408535}

--- components/exo/shell_surface.cc
+++ components/exo/shell_surface.cc
@@ -1101,7 +1101,7 @@ gfx::Rect ShellSurface::GetVisibleBounds() const {
 }
 
 gfx::Point ShellSurface::GetSurfaceOrigin() const {
-  gfx::Rect window_bounds = widget_->GetNativeWindow()->bounds();
+  gfx::Rect window_bounds = widget_->GetWindowBoundsInScreen();
 
   // If initial bounds were specified then surface origin is always relative
   // to those bounds.
