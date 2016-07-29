ba68eef360ccd701ad1d8c9f5a2dc1a804b03d6f
mgersh <mgersh@chromium.org>
2016-07-29 14:16:01 -0700

    Delete old NetworkQualityEstimator Cronet API
    Now that RequestFinishedListener doesn't depend on NQE's executor, the
    rest of this API can go away.
    
    BUG=618034
    
    Review-Url: https://codereview.chromium.org/2188553002
    Cr-Commit-Position: refs/heads/master@{#408754}

--- components/cronet/android/api/src/org/chromium/net/JavaCronetEngine.java
+++ components/cronet/android/api/src/org/chromium/net/JavaCronetEngine.java
@@ -105,12 +105,6 @@ final class JavaCronetEngine extends CronetEngine {
     }
 
     @Override
-    public void setRequestFinishedListenerExecutor(Executor executor) {}
-
-    @Override
-    public void enableNetworkQualityEstimator(Executor executor) {}
-
-    @Override
     public void configureNetworkQualityEstimatorForTesting(
             boolean useLocalHostRequests, boolean useSmallerResponses) {}
 
