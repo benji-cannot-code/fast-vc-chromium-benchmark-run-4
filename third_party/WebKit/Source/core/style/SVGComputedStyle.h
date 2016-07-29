7cb18ce2995576922991403c9fd7349b3e0dd348
sashab <sashab@chromium.org>
2016-07-29 01:06:57 -0700

    Organize public ComputedStyle methods by property
    Organized ComputedStyle public methods by property. This is part of a
    larger effort to clean up ComputedStyle and ultimately generate it. Also
    added a comment to the top of ComputedStyle explaining the new style, as
    well as TODOs to classes used by ComputedStyle to merge their logic into
    ComputedStyle.
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2174703002
    Cr-Commit-Position: refs/heads/master@{#408603}

--- third_party/WebKit/Source/core/style/SVGComputedStyle.h
+++ third_party/WebKit/Source/core/style/SVGComputedStyle.h
@@ -36,6 +36,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+// TODO(sashab): Move this into a private class on ComputedStyle, and remove
+// all methods on it, merging them into copy/creation methods on ComputedStyle
+// instead. Keep the allocation logic, only allocating a new object if needed.
 class CORE_EXPORT SVGComputedStyle : public RefCounted<SVGComputedStyle> {
 public:
     static PassRefPtr<SVGComputedStyle> create() { return adoptRef(new SVGComputedStyle); }
