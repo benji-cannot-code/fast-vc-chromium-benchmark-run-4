bddf147ed789b4f003629c46d5e5840d119dd30e
rouslan <rouslan@chromium.org>
2016-07-29 12:26:47 -0700

    Don't crash when closing UI while waiting for apps to respond.
    BUG=632468
    
    Review-Url: https://codereview.chromium.org/2193293003
    Cr-Commit-Position: refs/heads/master@{#408722}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
@@ -826,7 +826,7 @@ public class PaymentRequestImpl implements PaymentRequest, PaymentRequestUI.Clie
 
     private void disconnectFromClientWithDebugMessage(String debugMessage, int reason) {
         Log.d(TAG, debugMessage);
-        mClient.onError(reason);
+        if (mClient != null) mClient.onError(reason);
         closeClient();
     }
 
@@ -885,6 +885,7 @@ public class PaymentRequestImpl implements PaymentRequest, PaymentRequestUI.Clie
      */
     @Override
     public void onInstrumentsReady(PaymentApp app, List<PaymentInstrument> instruments) {
+        if (mClient == null) return;
         mPendingApps.remove(app);
 
         // Place the instruments into either "autofill" or "non-autofill" list to be displayed when
