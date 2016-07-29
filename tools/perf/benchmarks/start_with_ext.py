7daa0575a52301109334ffa5cdb6bfd4b967e637
erikchen <erikchen@chromium.org>
2016-07-29 10:11:30 -0700

    Revert of Reenable start_with_ext tests. (patchset #1 id:1 of https://codereview.chromium.org/2188273002/ )
    Reason for revert:
    Errors on waterfall:
    
    https://bugs.chromium.org/p/chromium/issues/detail?id=563424#c25
    
    Original issue's description:
    > Reenable start_with_ext tests.
    >
    > BUG=563424
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.perf:android_s5_perf_cq;master.tryserver.chromium.perf:mac_retina_perf_cq
    >
    > Committed: https://crrev.com/c51f7349b05bfaccbff4ac41d6897f84d9c9d64b
    > Cr-Commit-Position: refs/heads/master@{#408523}
    
    TBR=nednguyen@google.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=563424
    
    Review-Url: https://codereview.chromium.org/2195763002
    Cr-Commit-Position: refs/heads/master@{#408669}

--- tools/perf/benchmarks/start_with_ext.py
+++ tools/perf/benchmarks/start_with_ext.py
@@ -31,6 +31,7 @@ class _StartWithExt(perf_benchmark.PerfBenchmark):
 
 
 @benchmark.Enabled('has tabs')
+@benchmark.Disabled('mac')  # crbug.com/563424
 @benchmark.Disabled('win', 'linux', 'reference', 'android')
 class StartWithExtCold(_StartWithExt):
   """Measure time to start Chrome cold with extensions."""
@@ -43,6 +44,7 @@ class StartWithExtCold(_StartWithExt):
 
 
 @benchmark.Enabled('has tabs')
+@benchmark.Disabled('mac')  # crbug.com/563424
 @benchmark.Disabled('win', 'linux', 'reference', 'android')
 class StartWithExtWarm(_StartWithExt):
   """Measure time to start Chrome warm with extensions."""
