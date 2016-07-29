b9c274e32a74fae930bc35b66addf7f06367481c
tedchoc <tedchoc@chromium.org>
2016-07-29 14:21:00 -0700

    Remove herb restriction for tablets and stable/beta builds.
    BUG=630475
    
    Review-Url: https://codereview.chromium.org/2193933002
    Cr-Commit-Position: refs/heads/master@{#408760}

--- chrome/android/java/src/org/chromium/chrome/browser/util/FeatureUtilities.java
+++ chrome/android/java/src/org/chromium/chrome/browser/util/FeatureUtilities.java
@@ -26,7 +26,6 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.browser.ChromeApplication;
 import org.chromium.chrome.browser.ChromeSwitches;
-import org.chromium.chrome.browser.ChromeVersionInfo;
 import org.chromium.chrome.browser.InstantAppsHandler;
 import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
 import org.chromium.chrome.browser.tabmodel.DocumentModeAssassin;
@@ -161,8 +160,7 @@ public class FeatureUtilities {
     }
 
     private static boolean isHerbDisallowed(Context context) {
-        return isDocumentMode(context) || ChromeVersionInfo.isStableBuild()
-                || ChromeVersionInfo.isBetaBuild() || DeviceFormFactor.isTablet(context);
+        return isDocumentMode(context);
     }
 
     /**
