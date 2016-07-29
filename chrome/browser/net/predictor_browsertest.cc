ec82dc1586e467e9d41a610f50e477cdd58e9dab
csharrison <csharrison@chromium.org>
2016-07-28 17:54:55 -0700

    Remove old LinkPreconnect flag from predictor browsertests
    BUG=
    
    Review-Url: https://codereview.chromium.org/2189783006
    Cr-Commit-Position: refs/heads/master@{#408536}

--- chrome/browser/net/predictor_browsertest.cc
+++ chrome/browser/net/predictor_browsertest.cc
@@ -102,7 +102,6 @@ std::unique_ptr<net::test_server::HttpResponse> RedirectForPathHandler(
   return std::move(response);
 }
 
-const char kBlinkPreconnectFeature[] = "LinkPreconnect";
 const char kChromiumUrl[] = "http://chromium.org";
 const char kInvalidLongUrl[] =
     "http://"
@@ -513,8 +512,6 @@ class PredictorBrowserTest : public InProcessBrowserTest {
   void SetUpCommandLine(base::CommandLine* command_line) override {
     command_line->AppendSwitch(
         switches::kEnableExperimentalWebPlatformFeatures);
-    command_line->AppendSwitchASCII(
-        switches::kEnableBlinkFeatures, kBlinkPreconnectFeature);
     command_line->AppendSwitchASCII(switches::kEnableFeatures,
                                     "PreconnectMore");
   }
