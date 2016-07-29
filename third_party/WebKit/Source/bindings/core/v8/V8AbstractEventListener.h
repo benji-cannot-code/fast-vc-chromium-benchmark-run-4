18fa3883243a9ad51cd7f1c746a95334b90d3944
yukishiino <yukishiino@chromium.org>
2016-07-29 08:21:27 -0700

    binding: Compiles event handlers only once if not compilable.
    Blink no longer compiles a script for an event handler twice
    or more if the script is not compilable.
    
    BUG=597900
    
    Review-Url: https://codereview.chromium.org/2190553003
    Cr-Commit-Position: refs/heads/master@{#408638}

--- third_party/WebKit/Source/bindings/core/v8/V8AbstractEventListener.h
+++ third_party/WebKit/Source/bindings/core/v8/V8AbstractEventListener.h
@@ -86,8 +86,7 @@ public:
     // even if the user script is not compilable.
     v8::Local<v8::Object> getListenerObject(ExecutionContext* executionContext)
     {
-        prepareListenerObject(executionContext);
-        return m_listener.newLocal(m_isolate);
+        return getListenerObjectInternal(executionContext);
     }
 
     v8::Local<v8::Object> getExistingListenerObject()
@@ -120,7 +119,10 @@ public:
 protected:
     V8AbstractEventListener(bool isAttribute, DOMWrapperWorld&, v8::Isolate*);
 
-    virtual void prepareListenerObject(ExecutionContext*) { }
+    virtual v8::Local<v8::Object> getListenerObjectInternal(ExecutionContext* executionContext)
+    {
+        return getExistingListenerObject();
+    }
 
     void setListenerObject(v8::Local<v8::Object>);
 
