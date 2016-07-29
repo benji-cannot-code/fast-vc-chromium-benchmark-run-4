e6c9e0e59da2fb618055a4c99cbce84ae17b02e8
lizeb <lizeb@chromium.org>
2016-07-29 03:52:19 -0700

    predictors: Support fonts in resource_prefetch_predictors.
    BUG=631966
    
    Review-Url: https://codereview.chromium.org/2184323003
    Cr-Commit-Position: refs/heads/master@{#408613}

--- tools/resource_prefetch_predictor/prefetch_predictor_tool.py
+++ tools/resource_prefetch_predictor/prefetch_predictor_tool.py
@@ -18,8 +18,10 @@ import sqlite3
 
 
 class ResourceType(object):
+  """Partially mirrors content::ResourceType."""
   STYLESHEET = 2
   SCRIPT = 3
+  FONT_RESOURCE = 5
 
 
 class Entry(object):
@@ -45,7 +47,8 @@ class Entry(object):
   def _Score(self):
     """Mirrors ResourcePrefetchPredictorTables::ResourceRow::UpdateScore."""
     multiplier = 1
-    if self.resource_type in (ResourceType.STYLESHEET, ResourceType.SCRIPT):
+    if self.resource_type in (ResourceType.STYLESHEET, ResourceType.SCRIPT,
+                              ResourceType.FONT_RESOURCE):
       multiplier = 2
     return multiplier * 100 - self.average_position
 
