eb5f29126c7c9a0010b55de8c15c7cd417129dc1
sunxd <sunxd@chromium.org>
2016-07-29 11:46:56 -0700

    Undo scroll snaps in to_parent when NeedsSourceToParentUpdate.
    When we scroll on impl thread, we do scroll snapping in transform trees
    if the transform node has a non-integer screen space transform. However,
    the main thread logic also do snapping. When updating source to parent
    transforms on main thread, it is possible that the scroll snaps are used
    to compute the to_parent transform.
    
    It becomes a problem if the node is created due to a fixed pos layer,
    the snapping will add a small translation to the to_parent transform and
    result in the unstable position. This CL undoes the snapping in this
    case. It is notable that scroll_parent might also be influenced by this
    change.
    
    BUG=584598
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2183923002
    Cr-Commit-Position: refs/heads/master@{#408699}

--- cc/trees/property_tree_builder.cc
+++ cc/trees/property_tree_builder.cc
@@ -59,7 +59,6 @@ struct DataForRecursion {
   uint32_t main_thread_scrolling_reasons;
   bool scroll_tree_parent_created_by_uninheritable_criteria;
   const gfx::Transform* device_transform;
-  gfx::Vector2dF scroll_snap;
   gfx::Transform compound_transform_since_render_target;
   bool axis_align_since_render_target;
   SkColor safe_opaque_background_color;
@@ -541,7 +540,6 @@ bool AddTransformNodeIfNeeded(
                           ->offset_to_transform_parent();
       source_index =
           data_from_ancestor.transform_tree_parent->transform_tree_index();
-      source_offset -= data_from_ancestor.scroll_snap;
     }
   }
 
@@ -559,9 +557,6 @@ bool AddTransformNodeIfNeeded(
     }
   }
   data_for_children->transform_tree_parent = layer;
-
-  if (IsContainerForFixedPositionLayers(layer) || is_fixed)
-    data_for_children->scroll_snap = gfx::Vector2dF();
 
   if (!requires_node) {
     data_for_children->should_flatten |= ShouldFlattenTransform(layer);
@@ -694,8 +689,6 @@ bool AddTransformNodeIfNeeded(
 
   // Flattening (if needed) will be handled by |node|.
   layer->set_should_flatten_transform_from_property_tree(false);
-
-  data_for_children->scroll_snap += node->scroll_snap;
 
   node->owner_id = layer->id();
 
