5c33448a096fae71422d7f8bc3987dbe9fb0bd1f
jbauman <jbauman@chromium.org>
2016-07-28 18:51:29 -0700

    Use enum to track state of DXVAPictureBuffers.
    DXVAPictureBuffers currently use multiple booleans to specify what
    state they're in. Switch to using a single enum to simplify the
    logic and add DCHECKs to ensure we don't have invalid state
    transitions.
    
    Review-Url: https://codereview.chromium.org/2135773002
    Cr-Commit-Position: refs/heads/master@{#408552}

--- media/gpu/dxva_video_decode_accelerator_win.cc
+++ media/gpu/dxva_video_decode_accelerator_win.cc
@@ -1669,7 +1669,7 @@ void DXVAVideoDecodeAccelerator::ProcessPendingSamples() {
       }
 
       pending_sample->picture_buffer_id = index->second->id();
-      index->second->set_available(false);
+      index->second->set_bound();
       if (share_nv12_textures_) {
         main_thread_task_runner_->PostTask(
             FROM_HERE,
