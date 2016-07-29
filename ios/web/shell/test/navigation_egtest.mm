331a5f249c2f049afbd882574004187c043c6957
baxley <baxley@chromium.org>
2016-07-29 12:11:48 -0700

    Rename EarlGrey test classes with TestCase suffix.
    They inherit from XCTestCase, so TestCase suffix makes
    it a bit more clear.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2195773002
    Cr-Commit-Position: refs/heads/master@{#408708}

--- ios/web/shell/test/navigation_egtest.mm
+++ ios/web/shell/test/navigation_egtest.mm
@@ -23,10 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Navigation test cases for the web shell. These are Earl Grey integration
 // tests, which are based on XCTest.
-@interface NavigationTest : ShellBaseTestCase
+@interface NavigationTestCase : ShellBaseTestCase
 @end
 
-@implementation NavigationTest
+@implementation NavigationTestCase
 
 // Tests clicking a link to about:blank.
 - (void)testNavigationLinkToAboutBlank {
