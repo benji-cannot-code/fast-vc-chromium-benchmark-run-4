bed5b8189a9d2d849e2a12a0c1c08a1c0eefe42f
bmcquade <bmcquade@chromium.org>
2016-07-29 14:07:37 -0700

    Notify page load metrics when the app enters the background.
    This change adds a PageLoadMetricsProvider, which implements the new
    OnAppEnterBackground hook, and informs all MetricsWebContentsObservers
    and in turn all PageLoadTrackers when the app enters the background.
    
    On Android (and iOS), when an app enters the background, it may be
    killed without further notifications. Thus, having this callback
    will allow us to persist metrics before the app may be killed.
    Our page load metrics subsystem isn't used on iOS, so we only target
    Android in this change.
    
    Unfortunately, we can't use the existing WebContents hidden callback,
    as this gets invoked after the UMA subsystem has already flushed
    metrics to disk. Thus, we need to add this explicit callback which
    allows us to persist metrics as part of the UMA subsystem handling
    the backgrounding callback.
    
    For now, we log a debugging histogram in PageLoadTracker to help
    understand how often trackers enter the background, and how often
    those trackers are destroyed without running their destructors. In a
    subsequent change, we'll add support for informing PLMObservers that
    the app entered the background. This will allow any PLMObsevers that
    implement OnComplete or OnFailedProvisionalLoad to also get called back
    in cases where the tracker may be destroyed before its destructor runs,
    due to the application being killed.
    
    BUG=608360
    
    Review-Url: https://codereview.chromium.org/2189543002
    Cr-Commit-Position: refs/heads/master@{#408744}

--- chrome/browser/page_load_metrics/metrics_web_contents_observer.cc
+++ chrome/browser/page_load_metrics/metrics_web_contents_observer.cc
@@ -54,6 +54,8 @@ const char kClientRedirectWithoutPaint[] =
     "PageLoad.Internal.ClientRedirect.NavigationWithoutPaint";
 const char kCommitToCompleteNoTimingIPCs[] =
     "PageLoad.Internal.CommitToComplete.NoTimingIPCs";
+const char kPageLoadCompletedAfterAppBackground[] =
+    "PageLoad.Internal.PageLoadCompleted.AfterAppBackground";
 
 }  // namespace internal
 
@@ -188,6 +190,11 @@ void RecordInternalError(InternalErrorLoadEvent event) {
   UMA_HISTOGRAM_ENUMERATION(internal::kErrorEvents, event, ERR_LAST_ENTRY);
 }
 
+void RecordAppBackgroundPageLoadCompleted(bool completed_after_background) {
+  UMA_HISTOGRAM_BOOLEAN(internal::kPageLoadCompletedAfterAppBackground,
+                        completed_after_background);
+}
+
 UserAbortType AbortTypeForPageTransition(ui::PageTransition transition) {
   if (ui::PageTransitionCoreTypeIs(transition, ui::PAGE_TRANSITION_RELOAD))
     return ABORT_RELOAD;
@@ -248,6 +255,7 @@ PageLoadTracker::PageLoadTracker(
     int aborted_chain_size,
     int aborted_chain_size_same_url)
     : did_stop_tracking_(false),
+      app_entered_background_(false),
       navigation_start_(navigation_handle->NavigationStart()),
       url_(navigation_handle->GetURL()),
       abort_type_(ABORT_NONE),
@@ -264,6 +272,10 @@ PageLoadTracker::PageLoadTracker(
 }
 
 PageLoadTracker::~PageLoadTracker() {
+  if (app_entered_background_) {
+    RecordAppBackgroundPageLoadCompleted(true);
+  }
+
   if (did_stop_tracking_)
     return;
 
@@ -396,6 +408,13 @@ void PageLoadTracker::Redirect(content::NavigationHandle* navigation_handle) {
 void PageLoadTracker::OnInputEvent(const blink::WebInputEvent& event) {
   for (const auto& observer : observers_) {
     observer->OnUserInput(event);
+  }
+}
+
+void PageLoadTracker::FlushMetricsOnAppEnterBackground() {
+  if (!app_entered_background_) {
+    RecordAppBackgroundPageLoadCompleted(false);
+    app_entered_background_ = true;
   }
 }
 
@@ -804,6 +823,17 @@ void MetricsWebContentsObserver::OnInputEvent(
 
   if (committed_load_)
     committed_load_->OnInputEvent(event);
+}
+
+void MetricsWebContentsObserver::FlushMetricsOnAppEnterBackground() {
+  if (committed_load_)
+    committed_load_->FlushMetricsOnAppEnterBackground();
+  for (const auto& kv : provisional_loads_) {
+    kv.second->FlushMetricsOnAppEnterBackground();
+  }
+  for (const auto& tracker : aborted_provisional_loads_) {
+    tracker->FlushMetricsOnAppEnterBackground();
+  }
 }
 
 void MetricsWebContentsObserver::DidRedirectNavigation(
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -1041,6 +1041,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'browser/media/protected_media_identifier_infobar_delegate_android.h',
       'browser/metrics/android_metrics_provider.cc',
       'browser/metrics/android_metrics_provider.h',
+      'browser/metrics/page_load_metrics_provider.cc',
+      'browser/metrics/page_load_metrics_provider.h',
       'browser/net/spdyproxy/data_reduction_promo_infobar_delegate_android.cc',
       'browser/net/spdyproxy/data_reduction_promo_infobar_delegate_android.h',
       'browser/net/spdyproxy/data_reduction_proxy_settings_android.cc',
