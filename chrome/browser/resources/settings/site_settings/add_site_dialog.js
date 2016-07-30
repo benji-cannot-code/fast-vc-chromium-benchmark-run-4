b860f87f122ce2010080f83ac61e9a7319d3f9ca
dpapad <dpapad@chromium.org>
2016-07-29 18:10:50 -0700

    MD Settings: Fix canceling of "Add site exception" dialog.
    BUG=625332
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2197723003
    Cr-Commit-Position: refs/heads/master@{#408829}

--- chrome/browser/resources/settings/site_settings/add_site_dialog.js
+++ chrome/browser/resources/settings/site_settings/add_site_dialog.js
@@ -47,6 +47,11 @@ Polymer({
     }.bind(this));
   },
 
+  /** @private */
+  onCancelTap_: function() {
+    this.$.dialog.cancel();
+  },
+
   /**
    * The tap handler for the Add [Site] button (adds the pattern and closes
    * the dialog).
