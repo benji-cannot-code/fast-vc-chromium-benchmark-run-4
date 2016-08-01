4b1b2eadbdd3c3e10b2dce07f1120e0b0f4dc3d7
cwallez <cwallez@chromium.org>
2016-08-01 13:49:46 -0700

    Revert of On Linux rework driver_version/vendor extraction from gl version (patchset #11 id:200001 of https://codereview.chromium.org/2153373002/ )
    Reason for revert:
    https://bugs.chromium.org/p/chromium/issues/detail?id=633286
    
    Original issue's description:
    > On Linux rework driver_version/vendor extraction from gl version
    >
    > Previously it could not extract them from the gl version
    > "3.3 (Core Profile) Mesa 10.5.9" because of the presence
    > of '(Core Profile)'.
    >
    > BUG=628929
    >
    > R=kbr@chromium.org, piman@chromium.org, zmo@chromium.org
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    >
    > TEST=gpu_unittests --gtest_filter=*CollectDriverInfoGL*
    >
    > Committed: https://crrev.com/e5fdc90d17a40a9166a335a0d7c8fd8b2ccf40cd
    > Committed: https://crrev.com/f65e8f9730d970724d533a27b96d7c6388c84358
    > Cr-Original-Commit-Position: refs/heads/master@{#407804}
    > Cr-Commit-Position: refs/heads/master@{#408604}
    
    TBR=kbr@chromium.org,piman@chromium.org,zmo@chromium.org,patyag0814@gmail.com,j.isorce@samsung.com
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=628929
    
    Review-Url: https://codereview.chromium.org/2198243002
    Cr-Commit-Position: refs/heads/master@{#409049}

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -20,7 +20,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "8.81",
+  "version": "8.80",
   "entries": [
     {
       "id": 1,
@@ -1843,7 +1843,6 @@ LONG_STRING_CONST(
       "os": {
         "type": "linux"
       },
-      "driver_vendor": ".*",
       "exceptions" : [
         {
           "driver_vendor": "Mesa"
