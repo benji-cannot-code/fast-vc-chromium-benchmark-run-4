8220edc88c8c0a6b3f92b916d913a05c84ee4e42
ianwen <ianwen@chromium.org>
2016-08-01 15:27:08 -0700

    Make "All Bookmarks" to be controlled by feature
    This is used for controlling this feature in field trial, which targets
    at M52.
    
    BUG=605614
    
    Review-Url: https://codereview.chromium.org/2199033002
    Cr-Commit-Position: refs/heads/master@{#409069}

--- chrome/browser/android/chrome_feature_list.h
+++ chrome/browser/android/chrome_feature_list.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chrome {
 namespace android {
 
+extern const base::Feature kAllBookmarksFeature;
 extern const base::Feature kAndroidPayIntegrationV1;
 extern const base::Feature kImportantSitesInCBD;
 extern const base::Feature kNTPMaterialDesign;
