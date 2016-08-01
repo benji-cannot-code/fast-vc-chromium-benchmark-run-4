ecb88787dd39983520c15506f0ff97f4406eb677
mathp <mathp@chromium.org>
2016-08-01 15:21:46 -0700

    Revert of [Sync] Merge sync_unit_tests into components_unit_tests. (patchset #4 id:60001 of https://codereview.chromium.org/2186733002/ )
    Reason for revert:
    Seems to be causing compile failure on Linux https://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Full/builds/49248
    
    Original issue's description:
    > [Sync] Merge sync_unit_tests into components_unit_tests.
    >
    > BUG=626114
    >
    > Committed: https://crrev.com/9f7c262325b965be2a51475650d542c9791eee9d
    > Cr-Commit-Position: refs/heads/master@{#409060}
    
    TBR=dpranke@chromium.org,jam@chromium.org,slan@chromium.org,maxbogue@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=626114
    
    Review-Url: https://codereview.chromium.org/2205613002
    Cr-Commit-Position: refs/heads/master@{#409065}

--- build/android/pylib/gtest/gtest_config.py
+++ build/android/pylib/gtest/gtest_config.py
@@ -38,6 +38,7 @@ STABLE_TEST_SUITES = [
     'sandbox_linux_unittests',
     'skia_unittests',
     'sql_unittests',
+    'sync_unit_tests',
     'ui_android_unittests',
     'ui_base_unittests',
     'ui_touch_selection_unittests',
