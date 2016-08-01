1e93df6cb35ac6ee23e05521e49393219b171f05
hajimehoshi <hajimehoshi@chromium.org>
2016-08-01 06:14:52 -0700

    Refactoring: Remove unused member functions in MemoryCache
    This CL removes unused member functions in MemoryCache for code health.
    
    BUG=n/a
    TEST=n/a
    
    Review-Url: https://codereview.chromium.org/2192803002
    Cr-Commit-Position: refs/heads/master@{#408951}

--- third_party/WebKit/Source/core/fetch/MemoryCache.h
+++ third_party/WebKit/Source/core/fetch/MemoryCache.h
@@ -200,11 +200,6 @@ public:
     void makeLive(Resource*);
     void makeDead(Resource*);
 
-    // This should be called when a Resource object is created.
-    void registerLiveResource(Resource&);
-    // This should be called when a Resource object becomes unnecesarry.
-    void unregisterLiveResource(Resource&);
-
     void removeURLFromCache(const KURL&);
 
     Statistics getStatistics();
