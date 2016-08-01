c557a2865132585a6f070f136c8084d9da01a8f4
jmadill <jmadill@chromium.org>
2016-08-01 12:48:59 -0700

    WebGL 2: Mark Windows multisample blit tests as flaky.
    Intel and AMD already have blanket flaky suppressions.
    
    BUG=631317
    TBR=zmo@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2197293002
    Cr-Commit-Position: refs/heads/master@{#409033}

--- content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
@@ -55,7 +55,8 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
     self.Flaky('deqp/functional/gles3/buffercopy.html', ['win8'], bug=587601)
 
     # Win / NVidia
-    # La la la la la la la la la la ...
+    self.Flaky('deqp/functional/gles3/fbomultisample*',
+        ['win', 'nvidia'], bug=631317)
 
     # Win / AMD
     self.Fail('deqp/functional/gles3/texturespecification/' +
