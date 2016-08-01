8eec065531501463b3488def756ea6e45afdcf81
horo <horo@chromium.org>
2016-08-01 05:42:54 -0700

    [ServiceWorker] Don't check the origin equality when disable-web-security flag is set.
    When "disable-web-security" flag is set, we don't check the origin equality in Blink.
    So we shouldn't check it in content side.
    
    BUG=619294,630495,630496
    
    Review-Url: https://codereview.chromium.org/2196633002
    Cr-Commit-Position: refs/heads/master@{#408948}

--- content/common/service_worker/service_worker_utils.h
+++ content/common/service_worker/service_worker_utils.h
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_COMMON_SERVICE_WORKER_SERVICE_WORKER_UTILS_H_
 #define CONTENT_COMMON_SERVICE_WORKER_SERVICE_WORKER_UTILS_H_
 
+#include "base/command_line.h"
 #include "base/macros.h"
 #include "content/common/content_export.h"
 #include "content/common/service_worker/service_worker_status_code.h"
 #include "content/common/service_worker/service_worker_types.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/common/resource_type.h"
 #include "url/gurl.h"
 
@@ -44,6 +46,21 @@ class ServiceWorkerUtils {
   static bool CanRegisterServiceWorker(const GURL& context_url,
                                        const GURL& pattern,
                                        const GURL& script_url);
+
+  // Returns true when '--disable-web-security' flag is set. Otherwise returns
+  // whether the all origins of |urls| are same as the origin of |url|.
+  template <typename... Args>
+  static bool PassOriginEqualitySecurityCheck(const GURL& url,
+                                              const Args&... urls) {
+    if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+            switches::kDisableWebSecurity))
+      return true;
+    for (const GURL& u : {urls...}) {
+      if (url.GetOrigin() != u.GetOrigin())
+        return false;
+    }
+    return true;
+  }
 
   // PlzNavigate
   // Returns true if the |provider_id| was assigned by the browser process.
