b0065e0d80b98be21604121f8191a35b9761e332
j.isorce <j.isorce@samsung.com>
2016-08-02 08:35:14 -0700

    Fix ineffective --disable-gpu-driver-bug-workarounds
    It was hidden to the user since chrome://gpu was not showing
    any workarounds, but they were still applied in the gpu process.
    
    Also add a gpu test that sets
    --disable-gpu-driver-bug-workarounds --use_gpu_driver_workaround_for_testing
    and verifies that the only active workaround is USE_TESTING_GPU_DRIVER_WORKAROUND
    in both browser and gpu processes.
    
    BUG=359367
    
    R=kbr@chromium.org, zmo@chromium.org
    
    TEST=./content/test/gpu/run_gpu_test.py gpu_process --show-stdout --browser=exact --extra-browser-args="--no-sandbox" --browser-executable=./out/build/chrome
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2195743005
    Cr-Commit-Position: refs/heads/master@{#409197}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -162,6 +162,7 @@ static const char* const kSwitchNames[] = {
     switches::kGpuTestingGLVendor,
     switches::kGpuTestingGLRenderer,
     switches::kGpuTestingGLVersion,
+    switches::kDisableGpuDriverBugWorkarounds
 };
 
 enum GPUProcessLifetimeEvent {
