315301260aa6624931701dd62147df94fc4c5f22
robliao <robliao@chromium.org>
2016-08-02 13:51:49 -0700

    Add TaskScheduler::CreateAndSetDefaultTaskScheduler
    BUG=553459
    
    Review-Url: https://codereview.chromium.org/2199943003
    Cr-Commit-Position: refs/heads/master@{#409319}

--- base/task_scheduler/task_scheduler.cc
+++ base/task_scheduler/task_scheduler.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_scheduler/task_scheduler.h"
 
 #include "base/logging.h"
+#include "base/task_scheduler/task_scheduler_impl.h"
 
 namespace base {
 
@@ -15,6 +16,15 @@ namespace {
 TaskScheduler* g_task_scheduler = nullptr;
 
 }  // namespace
+
+// static
+void TaskScheduler::CreateAndSetDefaultTaskScheduler(
+    const std::vector<SchedulerWorkerPoolParams>& worker_pool_params_vector,
+    const WorkerPoolIndexForTraitsCallback&
+        worker_pool_index_for_traits_callback) {
+  SetInstance(internal::TaskSchedulerImpl::Create(
+      worker_pool_params_vector, worker_pool_index_for_traits_callback));
+}
 
 // static
 void TaskScheduler::SetInstance(std::unique_ptr<TaskScheduler> task_scheduler) {
