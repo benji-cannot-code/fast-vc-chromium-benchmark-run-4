05b02743ea45f7e3fa647e3bf8bd704f6c6967bb
wnwen <wnwen@chromium.org>
2016-08-02 11:26:02 -0700

    Separate deferred startup into tasks.
    Correct UMA timing to be based on foreground start rather than
    application initialization, as the application is often initialized in
    the background, causing misleading stats.
    
    By separating one single task into multiple tasks queued by the idle
    handler, we don't block UI thread for a long time. We used to do
    ~100ms of blocking work for deferred startup, now it is reduced to
    around ~20ms for the longest task. Tested on an N6, so on slower
    devices this takes much longer.
    
    BUG=614452
    
    Review-Url: https://codereview.chromium.org/2121863002
    Cr-Commit-Position: refs/heads/master@{#409251}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/ChromeActivityTestCaseBase.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/ChromeActivityTestCaseBase.java
@@ -465,7 +465,7 @@ public abstract class ChromeActivityTestCaseBase<T extends ChromeActivity>
         CriteriaHelper.pollUiThread(new Criteria("Deferred startup never completed") {
             @Override
             public boolean isSatisfied() {
-                return DeferredStartupHandler.getInstance().isDeferredStartupComplete();
+                return DeferredStartupHandler.getInstance().isDeferredStartupCompleteForApp();
             }
         });
 
@@ -827,7 +827,7 @@ public abstract class ChromeActivityTestCaseBase<T extends ChromeActivity>
                 final int maxIndex = Math.min(annotation.traceNames().length, Math.min(
                         annotation.graphNames().length, annotation.seriesNames().length));
 
-                List<String> allNames = new LinkedList<String>();
+                List<String> allNames = new LinkedList<>();
                 for (int i = 0; i < maxIndex; ++i) {
                     // Prune out all of ',' and ';' from the strings.  Replace them with '-'.
                     String name = annotation.traceNames()[i].replaceAll("[,;]", "-");
