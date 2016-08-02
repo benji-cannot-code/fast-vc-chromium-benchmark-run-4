21c5cb39d2430df72a8f44148fd5c0c8cf551f18
mgiuca <mgiuca@chromium.org>
2016-08-01 19:11:47 -0700

    Added experimental Web Share API on Android behind flag.
    On Android, navigator.share now sends a system intent. Requires
    the enable-experimental-web-platform-features flag.
    
    BUG=595608
    
    Review-Url: https://codereview.chromium.org/1814133002
    Cr-Commit-Position: refs/heads/master@{#409121}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/webshare/ShareServiceImplementationFactory.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.webshare;
+
+import org.chromium.content.browser.InterfaceRegistry.ImplementationFactory;
+import org.chromium.content_public.browser.WebContents;
+import org.chromium.mojom.webshare.ShareService;
+
+/**
+ * Factory that creates instances of ShareService.
+ */
+public class ShareServiceImplementationFactory implements ImplementationFactory<ShareService> {
+    private final WebContents mWebContents;
+
+    public ShareServiceImplementationFactory(WebContents webContents) {
+        mWebContents = webContents;
+    }
+
+    @Override
+    public ShareService createImpl() {
+        return new ShareServiceImpl(mWebContents);
+    }
+}
