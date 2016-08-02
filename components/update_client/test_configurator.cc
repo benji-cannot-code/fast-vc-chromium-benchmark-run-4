d106050ea02629edb7882e47a28f19e44932ed01
sorin <sorin@chromium.org>
2016-08-02 14:48:40 -0700

    Mechanical change in the component updater Configurator.
    This is the first CL in a series of CLs to implement component updater
    group policies
    
    The CL renames a few functions in the Configurator, for consistency.
    It introduces Configurator::EnabledComponentUpdates and
    CrxComponent::supports_group_policy_enable_component_updates.
    
    These members are placeholders and have no effect on the current
    code behavior.
    
    BUG=624128
    
    Review-Url: https://codereview.chromium.org/2199423002
    Cr-Commit-Position: refs/heads/master@{#409337}

--- components/update_client/test_configurator.cc
+++ components/update_client/test_configurator.cc
@@ -108,15 +108,19 @@ scoped_refptr<OutOfProcessPatcher> TestConfigurator::CreateOutOfProcessPatcher()
   return NULL;
 }
 
-bool TestConfigurator::DeltasEnabled() const {
+bool TestConfigurator::EnabledDeltas() const {
   return true;
 }
 
-bool TestConfigurator::UseBackgroundDownloader() const {
+bool TestConfigurator::EnabledComponentUpdates() const {
+  return true;
+}
+
+bool TestConfigurator::EnabledBackgroundDownloader() const {
   return false;
 }
 
-bool TestConfigurator::UseCupSigning() const {
+bool TestConfigurator::EnabledCupSigning() const {
   return use_cup_signing_;
 }
 
@@ -132,7 +136,7 @@ void TestConfigurator::SetInitialDelay(int seconds) {
   initial_time_ = seconds;
 }
 
-void TestConfigurator::SetUseCupSigning(bool use_cup_signing) {
+void TestConfigurator::SetEnabledCupSigning(bool use_cup_signing) {
   use_cup_signing_ = use_cup_signing;
 }
 
