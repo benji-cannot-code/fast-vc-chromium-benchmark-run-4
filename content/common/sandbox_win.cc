dc85377d8f51e3f2c2edf18ad274d786d8bab76a
pennymac <pennymac@chromium.org>
2016-08-02 12:12:26 -0700

    [Windows Sandbox] Turn on MITIGATION_EXTENSION_POINT_DISABLE.
    This CL is part of a chain of CLs:
    1) "MITIGATION_EXTENSION_POINT_DISABLE support for children" (https://codereview.chromium.org/1835003003)
    2) "MITIGATION_EXTENSION_POINT_DISABLE emergency off finch" (https://codereview.chromium.org/1836523004/)
    3) "New NT registry API" (https://codereview.chromium.org/1841573002)
    4) "Early browser security support" (https://codereview.chromium.org/1656453002)
    -> THIS
    
    This CL only turns on the new mitigation (in all child processes, and early for the browser process).
    
    BUG=557798
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    
    Review-Url: https://codereview.chromium.org/1854323002
    Cr-Commit-Position: refs/heads/master@{#409264}

--- content/common/sandbox_win.cc
+++ content/common/sandbox_win.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_version.h"
 #include "content/common/content_switches_internal.h"
 #include "content/public/common/content_client.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/sandbox_init.h"
 #include "content/public/common/sandboxed_process_launcher_delegate.h"
@@ -725,8 +726,10 @@ sandbox::ResultCode StartSandboxedProcess(
       sandbox::MITIGATION_IMAGE_LOAD_NO_REMOTE |
       sandbox::MITIGATION_IMAGE_LOAD_NO_LOW_LABEL;
 
-  sandbox::ResultCode result = sandbox::SBOX_ERROR_GENERIC;
+  if (base::FeatureList::IsEnabled(features::kWinSboxDisableExtensionPoints))
+    mitigations |= sandbox::MITIGATION_EXTENSION_POINT_DISABLE;
 
+  sandbox::ResultCode result = sandbox::SBOX_ERROR_GENERIC;
   result = policy->SetProcessMitigations(mitigations);
 
   if (result != sandbox::SBOX_ALL_OK)
