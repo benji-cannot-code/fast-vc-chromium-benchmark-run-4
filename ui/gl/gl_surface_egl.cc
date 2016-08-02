19db985ab4d41ed32516490cff2354e2db761f18
jbauman <jbauman@chromium.org>
2016-08-02 16:19:09 -0700

    Update vsync override in PostSubBuffers.
    This was only happening in SwapBuffers, so the vsync override could
    become stale if only PostSubBuffers were happening.
    
    BUG=632785
    
    Review-Url: https://codereview.chromium.org/2188063002
    Cr-Commit-Position: refs/heads/master@{#409376}

--- ui/gl/gl_surface_egl.cc
+++ ui/gl/gl_surface_egl.cc
@@ -747,11 +747,7 @@ bool NativeViewGLSurfaceEGL::IsOffscreen() {
   return false;
 }
 
-gfx::SwapResult NativeViewGLSurfaceEGL::SwapBuffers() {
-  TRACE_EVENT2("gpu", "NativeViewGLSurfaceEGL:RealSwapBuffers",
-      "width", GetSize().width(),
-      "height", GetSize().height());
-
+void NativeViewGLSurfaceEGL::UpdateSwapInterval() {
 #if defined(OS_WIN)
   if (swap_interval_ != 0) {
     // This code is a simple way of enforcing that we only vsync if one surface
@@ -786,6 +782,14 @@ gfx::SwapResult NativeViewGLSurfaceEGL::SwapBuffers() {
     swaps_this_generation_++;
   }
 #endif
+}
+
+gfx::SwapResult NativeViewGLSurfaceEGL::SwapBuffers() {
+  TRACE_EVENT2("gpu", "NativeViewGLSurfaceEGL:RealSwapBuffers",
+      "width", GetSize().width(),
+      "height", GetSize().height());
+
+  UpdateSwapInterval();
 
   if (!CommitAndClearPendingOverlays()) {
     DVLOG(1) << "Failed to commit pending overlay planes.";
@@ -872,6 +876,7 @@ gfx::SwapResult NativeViewGLSurfaceEGL::PostSubBuffer(int x,
                                                       int width,
                                                       int height) {
   DCHECK(supports_post_sub_buffer_);
+  UpdateSwapInterval();
   if (!CommitAndClearPendingOverlays()) {
     DVLOG(1) << "Failed to commit pending overlay planes.";
     return gfx::SwapResult::SWAP_FAILED;
