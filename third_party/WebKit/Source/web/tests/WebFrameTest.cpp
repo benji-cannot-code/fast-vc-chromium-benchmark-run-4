14f4be4c478dc3966d74e005189b5804919b12b2
xiaochengh <xiaochengh@chromium.org>
2016-08-01 18:31:51 -0700

    Remove grammar marker inspection from WebFrameTest
    This patch is part of the refactoring of removing grammar
    checking code, because neither Chromium nor Blink supports
    supports grammar checking.
    
    BUG=619452
    TEST=n/a; this patch changes a test file
    
    Review-Url: https://codereview.chromium.org/2201783002
    Cr-Commit-Position: refs/heads/master@{#409115}

--- third_party/WebKit/Source/web/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/web/tests/WebFrameTest.cpp
@@ -5290,11 +5290,6 @@ public:
         kick(1, 8, WebTextDecorationTypeSpelling);
     }
 
-    void kickGrammar()
-    {
-        kick(1, 8, WebTextDecorationTypeGrammar);
-    }
-
     void kickInvisibleSpellcheck()
     {
         kick(1, 8, WebTextDecorationTypeInvisibleSpellcheck);
@@ -5391,9 +5386,8 @@ TEST_P(ParameterizedWebFrameTest, SpellcheckResultErasesMarkers)
     EXPECT_FALSE(exceptionState.hadException());
     auto range = EphemeralRange::rangeOfContents(*element);
     document->markers().addMarker(range.startPosition(), range.endPosition(), DocumentMarker::Spelling);
-    document->markers().addMarker(range.startPosition(), range.endPosition(), DocumentMarker::Grammar);
     document->markers().addMarker(range.startPosition(), range.endPosition(), DocumentMarker::InvisibleSpellcheck);
-    EXPECT_EQ(3U, document->markers().markers().size());
+    EXPECT_EQ(2U, document->markers().markers().size());
 
     spellcheck.kickNoResults();
     EXPECT_EQ(0U, document->markers().markers().size());
@@ -5424,14 +5418,6 @@ TEST_P(ParameterizedWebFrameTest, SpellcheckResultsSavedInDocument)
     ASSERT_EQ(1U, document->markers().markers().size());
     ASSERT_NE(static_cast<DocumentMarker*>(0), document->markers().markers()[0]);
     EXPECT_EQ(DocumentMarker::Spelling, document->markers().markers()[0]->type());
-
-    document->execCommand("InsertText", false, "wellcome ", exceptionState);
-    EXPECT_FALSE(exceptionState.hadException());
-
-    spellcheck.kickGrammar();
-    ASSERT_EQ(1U, document->markers().markers().size());
-    ASSERT_NE(static_cast<DocumentMarker*>(0), document->markers().markers()[0]);
-    EXPECT_EQ(DocumentMarker::Grammar, document->markers().markers()[0]->type());
 
     document->execCommand("InsertText", false, "wellcome ", exceptionState);
     EXPECT_FALSE(exceptionState.hadException());
