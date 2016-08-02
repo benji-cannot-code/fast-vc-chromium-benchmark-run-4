3fa6af29c7dda8f11dd70ff126f61452faa31f44
xiaochengh <xiaochengh@chromium.org>
2016-08-02 03:53:41 -0700

    Cleanup function names in SpellChecker and TextCheckingHelper
    The above classes no longer performs any grammar checking. This patch
    cleans up their member function names so that one can be clear that the
    two classes only performs spell checking.
    
    BUG=619452
    TEST=n/a; no behavior change
    
    Review-Url: https://codereview.chromium.org/2205823002
    Cr-Commit-Position: refs/heads/master@{#409168}

--- third_party/WebKit/Source/core/editing/spellcheck/TextCheckingHelper.h
+++ third_party/WebKit/Source/core/editing/spellcheck/TextCheckingHelper.h
@@ -90,10 +90,7 @@ public:
     ~TextCheckingHelper();
 
     String findFirstMisspelling(int& firstMisspellingOffset, bool markAll);
-    String findFirstMisspellingOrBadGrammar(bool& outIsSpelling, int& outFirstFoundOffset, GrammarDetail& outGrammarDetail);
-    String findFirstBadGrammar(GrammarDetail& outGrammarDetail, int& outGrammarPhraseOffset, bool markAll);
     bool markAllMisspellings();
-    void markAllBadGrammar();
 
 private:
     SpellCheckerClient* m_client;
