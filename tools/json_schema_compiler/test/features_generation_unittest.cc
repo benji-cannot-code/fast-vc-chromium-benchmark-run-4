5d328d17e388604071b45465ffb40080feb6008b
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-08-02 10:53:38 -0700

    [Extensions] Add FeatureCompiler validation
    Add validation steps to the feature compiler.
    
    BUG=280286
    
    Review-Url: https://codereview.chromium.org/2199063002
    Cr-Commit-Position: refs/heads/master@{#409233}

--- tools/json_schema_compiler/test/features_generation_unittest.cc
+++ tools/json_schema_compiler/test/features_generation_unittest.cc
@@ -112,6 +112,9 @@ TEST(FeaturesGenerationTest, FeaturesTest) {
   {
     APIFeature* feature = GetAPIFeature("beta");
     FeatureComparator comparator("beta");
+    comparator.contexts = {Feature::BLESSED_EXTENSION_CONTEXT};
+    comparator.channel.reset(
+        new version_info::Channel(version_info::Channel::DEV));
     comparator.extension_types = {Manifest::TYPE_EXTENSION,
                                   Manifest::TYPE_PLATFORM_APP};
     comparator.whitelist = {"aaa", "bbb"};
@@ -147,6 +150,8 @@ TEST(FeaturesGenerationTest, FeaturesTest) {
     FeatureComparator comparator("gamma.unparented");
     comparator.blacklist = {"ddd"};
     comparator.contexts = {Feature::UNBLESSED_EXTENSION_CONTEXT};
+    comparator.channel.reset(
+        new version_info::Channel(version_info::Channel::DEV));
     comparator.CompareFeature(feature);
   }
   {
@@ -166,6 +171,8 @@ TEST(FeaturesGenerationTest, FeaturesTest) {
     FeatureComparator comparator("delta");
     comparator.contexts = {Feature::BLESSED_EXTENSION_CONTEXT,
                            Feature::WEBUI_CONTEXT};
+    comparator.channel.reset(
+        new version_info::Channel(version_info::Channel::DEV));
     comparator.matches.AddPattern(
         URLPattern(URLPattern::SCHEME_ALL, "*://example.com/*"));
     comparator.CompareFeature(feature);
@@ -175,6 +182,8 @@ TEST(FeaturesGenerationTest, FeaturesTest) {
     APIFeature* feature = GetAPIFeature("omega");
     FeatureComparator comparator("omega");
     comparator.contexts = {Feature::WEB_PAGE_CONTEXT};
+    comparator.channel.reset(
+        new version_info::Channel(version_info::Channel::DEV));
     comparator.min_manifest_version = 2;
     comparator.CompareFeature(feature);
   }
