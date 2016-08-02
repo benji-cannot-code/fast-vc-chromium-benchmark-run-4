4f897cc3770f5560201177c4b2ca3906057fe680
nick <nick@chromium.org>
2016-08-02 13:28:06 -0700

    TaskManager: unique_ptr<> ownership of TaskGroups
    BUG=None
    TEST=tryjobs
    
    Review-Url: https://codereview.chromium.org/2183023005
    Cr-Commit-Position: refs/heads/master@{#409308}

--- chrome/browser/task_management/sampling/task_group.cc
+++ chrome/browser/task_management/sampling/task_group.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
-#include "base/stl_util.h"
 #include "build/build_config.h"
 #include "chrome/browser/task_management/task_manager_observer.h"
 #include "components/nacl/browser/nacl_browser.h"
