0afe37b45b1d5cccc44f7f2dd33c04e7495258d0
metzman <metzman@google.com>
2016-08-03 15:33:27 -0700

    [libFuzzer] Exercise more code from target and add dictionary.
    1. Add call to ParseFromStringUsingSuffixMatching from target function.
    2. Add dictionary and let gn know about it.
    Both of these changes were found to improve coverage for libFuzzer when
    tested locally.
    
    BUG=611337
    
    Review-Url: https://codereview.chromium.org/2200053006
    Cr-Commit-Position: refs/heads/master@{#409642}

--- net/proxy/parse_proxy_bypass_rules_fuzzer.cc
+++ net/proxy/parse_proxy_bypass_rules_fuzzer.cc
@@ -13,5 +13,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   net::ProxyBypassRules rules;
   std::string input(data, data + size);
   rules.ParseFromString(input);
+  rules.ParseFromStringUsingSuffixMatching(input);
   return 0;
 }
