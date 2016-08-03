24b5467f3980ef8e95423e9da081938b4fa6b616
pkalinnikov <pkalinnikov@chromium.org>
2016-08-03 01:42:19 -0700

    Implement deactivating SubresourceFilter for document.
    BUG=609747
    
    Review-Url: https://codereview.chromium.org/2183883002
    Cr-Commit-Position: refs/heads/master@{#409476}

--- components/subresource_filter/content/renderer/subresource_filter_agent.cc
+++ components/subresource_filter/content/renderer/subresource_filter_agent.cc
@@ -67,6 +67,9 @@ void SubresourceFilterAgent::RecordHistogramsOnLoadCommitted() {
 void SubresourceFilterAgent::RecordHistogramsOnLoadFinished() {
   DCHECK(filter_for_last_committed_load_);
   UMA_HISTOGRAM_COUNTS_1000(
+      "SubresourceFilter.DocumentLoad.NumSubresourceLoads.Total",
+      filter_for_last_committed_load_->num_loads_total());
+  UMA_HISTOGRAM_COUNTS_1000(
       "SubresourceFilter.DocumentLoad.NumSubresourceLoads.Evaluated",
       filter_for_last_committed_load_->num_loads_evaluated());
   UMA_HISTOGRAM_COUNTS_1000(
@@ -96,7 +99,7 @@ void SubresourceFilterAgent::DidCommitProvisionalLoad(
     std::unique_ptr<DocumentSubresourceFilter> filter(
         new DocumentSubresourceFilter(activation_state_for_provisional_load_,
                                       ruleset_dealer_->ruleset(),
-                                      std::move(ancestor_document_urls)));
+                                      ancestor_document_urls));
     filter_for_last_committed_load_ = filter->AsWeakPtr();
     SetSubresourceFilterForCommittedLoad(std::move(filter));
   }
