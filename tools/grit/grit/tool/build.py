02027fdd78c3a03f036f18708a9e3eff635e6711
aberent <aberent@chromium.org>
2016-08-03 05:43:34 -0700

    Strip comments and whitespace from javascript resources
    On Android strip comments and whitespace from javascript resources.
    Add an option to grit build that provides a command that strips
    comments and whitespace, and modify grit so that this is applied
    to all javascript resource and scripts within HTML resources.
    Add a python script that wraps the closure compiler to implement
    this command.
    Pull it all together with GN.
    
    BUG=619091
    
    Review-Url: https://codereview.chromium.org/2179033002
    Cr-Commit-Position: refs/heads/master@{#409498}

--- tools/grit/grit/tool/build.py
+++ tools/grit/grit/tool/build.py
@@ -18,6 +18,7 @@ import sys
 from grit import grd_reader
 from grit import shortcuts
 from grit import util
+from grit.format import minifier
 from grit.node import include
 from grit.node import message
 from grit.node import structure
@@ -127,6 +128,12 @@ Options:
                     generated will depend on a stampfile instead of the first
                     output in the input .grd file.
 
+  --js-minifier     A command to run the Javascript minifier. If not set then
+                    Javascript won't be minified. The command should read the
+                    original Javascript from standard input, and output the
+                    minified Javascript to standard output. A non-zero exit
+                    status will be taken as indicating failure.
+
 Conditional inclusion of resources only affects the output of files which
 control which resources get linked into a binary, e.g. it affects .rc files
 meant for compilation but it does not affect resource header files (that define
@@ -149,6 +156,7 @@ are exported to translation interchange files (e.g. XMB files), etc.
     output_all_resource_defines = None
     write_only_new = False
     depend_on_stamp = False
+    js_minifier = None
     replace_ellipsis = True
     (own_opts, args) = getopt.getopt(args, 'a:o:D:E:f:w:t:h:',
         ('depdir=','depfile=','assert-file-list=',
@@ -156,6 +164,7 @@ are exported to translation interchange files (e.g. XMB files), etc.
          'no-output-all-resource-defines',
          'no-replace-ellipsis',
          'depend-on-stamp',
+         'js-minifier=',
          'write-only-new='))
     for (key, val) in own_opts:
       if key == '-a':
@@ -196,6 +205,8 @@ are exported to translation interchange files (e.g. XMB files), etc.
         write_only_new = val != '0'
       elif key == '--depend-on-stamp':
         depend_on_stamp = True
+      elif key == '--js-minifier':
+        js_minifier = val
 
     if len(args):
       print 'This tool takes no tool-specific arguments.'
@@ -214,6 +225,9 @@ are exported to translation interchange files (e.g. XMB files), etc.
         self.VerboseOut('Using whitelist: %s\n' % whitelist_filename);
         whitelist_contents = util.ReadFile(whitelist_filename, util.RAW_TEXT)
         self.whitelist_names.update(whitelist_contents.strip().split('\n'))
+
+    if js_minifier:
+      minifier.SetJsMinifier(js_minifier)
 
     self.write_only_new = write_only_new
 
