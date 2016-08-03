6140dae6b5590076ff676116d99b6c6b37b8d9b9
pilgrim <pilgrim@chromium.org>
2016-08-03 09:33:29 -0700

    [Layout API] Use ownerLayoutItem() in PaintLayer
    Direct access to LayoutPart is deprecated and being removed soon. The
    replacement is LayoutPartItem, which routes methods through the new
    block layout API. (See linked crbug for details.) This means the
    ownerLayoutObject() method is also deprecated, to be replaced with
    ownerLayoutItem().
    
    There are no functional changes.
    
    BUG=499321
    
    Review-Url: https://codereview.chromium.org/2207433004
    Cr-Commit-Position: refs/heads/master@{#409532}

--- third_party/WebKit/Source/core/paint/PaintLayer.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayer.cpp
@@ -931,10 +931,10 @@ PaintLayer* PaintLayer::enclosingLayerForPaintInvalidationCrossingFrameBoundarie
         compositedLayer = layer->enclosingLayerForPaintInvalidation();
         if (!compositedLayer) {
             RELEASE_ASSERT(layer->layoutObject()->frame());
-            LayoutObject* owner = layer->layoutObject()->frame()->ownerLayoutObject();
-            if (!owner)
+            LayoutItem owner = layer->layoutObject()->frame()->ownerLayoutItem();
+            if (owner.isNull())
                 break;
-            layer = owner->enclosingLayer();
+            layer = owner.enclosingLayer();
         }
     }
     return compositedLayer;
@@ -2866,10 +2866,10 @@ void PaintLayer::markCompositingContainerChainForNeedsRepaint()
 
         PaintLayer* container = layer->compositingContainer();
         if (!container) {
-            LayoutObject* owner = layer->layoutObject()->frame()->ownerLayoutObject();
-            if (!owner)
+            LayoutItem owner = layer->layoutObject()->frame()->ownerLayoutItem();
+            if (owner.isNull())
                 break;
-            container = owner->enclosingLayer();
+            container = owner.enclosingLayer();
         }
 
         if (container->m_needsRepaint)
