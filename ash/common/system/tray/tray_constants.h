698df2e09aca557a9551a82cbcd53f2cbc879f0b
bruthig <bruthig@chromium.org>
2016-08-03 08:12:23 -0700

    Updated System tray menu row height to 48 for Material Design.
    BUG=632478
    
    Review-Url: https://codereview.chromium.org/2186393002
    Cr-Commit-Position: refs/heads/master@{#409516}

--- ash/common/system/tray/tray_constants.h
+++ ash/common/system/tray/tray_constants.h
@@ -48,7 +48,6 @@ extern const int kTrayPopupPaddingBetweenItems;
 extern const int kTrayPopupTextSpacingVertical;
 extern const int kTrayPopupUserCardVerticalPadding;
 
-extern const int kTrayPopupItemHeight;
 extern const int kTrayPopupDetailsIconWidth;
 extern const int kTrayPopupDetailsLabelExtraLeftMargin;
 extern const SkColor kTrayPopupHoverBackgroundColor;
@@ -98,6 +97,9 @@ enum TrayConstant {
 
   // Padding between the edge of shelf and the item in status tray area.
   TRAY_PADDING_FROM_EDGE_OF_SHELF,
+
+  // The height of the rows in the system tray menu.
+  TRAY_POPUP_ITEM_HEIGHT,
 
   // The width and height of the virtual keyboard button in the status tray
   // area. For non-MD, adjustments are made to the button dimensions based on
