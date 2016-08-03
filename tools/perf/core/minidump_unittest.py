24a5d3227de18941424cb64de0c22591582fcd92
nednguyen <nednguyen@google.com>
2016-08-03 16:14:27 -0700

    Disable testMultipleCrashMinidumps on Mac
    BUG=634156
    
    Review-Url: https://codereview.chromium.org/2208653003
    Cr-Commit-Position: refs/heads/master@{#409655}

--- tools/perf/core/minidump_unittest.py
+++ tools/perf/core/minidump_unittest.py
@@ -42,6 +42,7 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
 
   @decorators.Isolated
   @decorators.Enabled('mac')
+  @decorators.Disabled('mac')  # crbug.com/634156
   def testMultipleCrashMinidumps(self):
     # Wait for the browser to restart fully before crashing
     self._LoadPageThenWait('var cat = "dog";', 'cat')
