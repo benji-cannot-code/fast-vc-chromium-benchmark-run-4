3cdb9dcdaccf2ce0232c2fdc8fb0923eae2cc13c
treib <treib@chromium.org>
2016-08-03 04:05:20 -0700

    Remove search::ShouldPrefetchSearchResultsOnSRP
    It's always false in practice.
    
    BUG=627483
    
    Review-Url: https://codereview.chromium.org/2143953002
    Cr-Commit-Position: refs/heads/master@{#409485}

--- chrome/browser/search/search.cc
+++ chrome/browser/search/search.cc
@@ -53,7 +53,6 @@ namespace search {
 
 namespace {
 
-const char kPrefetchSearchResultsOnSRP[] = "prefetch_results_srp";
 const char kPrerenderInstantUrlOnOmniboxFocus[] =
     "prerender_instant_url_on_omnibox_focus";
 
@@ -610,12 +609,6 @@ InstantSupportState GetInstantSupportStateFromNavigationEntry(
     return INSTANT_SUPPORT_UNKNOWN;
 
   return StringToInstantSupportState(value);
-}
-
-bool ShouldPrefetchSearchResultsOnSRP() {
-  FieldTrialFlags flags;
-  return GetFieldTrialInfo(&flags) && GetBoolValueForFlagWithDefault(
-      kPrefetchSearchResultsOnSRP, false, flags);
 }
 
 bool ShouldUseAltInstantURL() {
