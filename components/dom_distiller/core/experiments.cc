8381503ccc09faea12d95b5221aef5dd29965df9
wychen <wychen@chromium.org>
2016-08-04 13:04:35 -0700

    Do prefix matching for ReaderModeUI finch group
    This allows multiple experimental groups that behave the same.
    
    BUG=634162
    
    Review-Url: https://codereview.chromium.org/2210683002
    Cr-Commit-Position: refs/heads/master@{#409880}

--- components/dom_distiller/core/experiments.cc
+++ components/dom_distiller/core/experiments.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/metrics/field_trial.h"
+#include "base/strings/string_util.h"
 #include "components/dom_distiller/core/dom_distiller_switches.h"
 
 namespace dom_distiller {
@@ -32,10 +33,12 @@ DistillerHeuristicsType GetDistillerHeuristicsType() {
     }
     NOTREACHED() << "Invalid value for " << switches::kReaderModeHeuristics;
   } else {
-    if (group_name == "AdaBoost") {
+    if (base::StartsWith(group_name, "AdaBoost",
+                         base::CompareCase::INSENSITIVE_ASCII)) {
       return DistillerHeuristicsType::ADABOOST_MODEL;
     }
-    if (group_name == "OGArticle") {
+    if (base::StartsWith(group_name, "OGArticle",
+                         base::CompareCase::INSENSITIVE_ASCII)) {
       return DistillerHeuristicsType::OG_ARTICLE;
     }
   }
