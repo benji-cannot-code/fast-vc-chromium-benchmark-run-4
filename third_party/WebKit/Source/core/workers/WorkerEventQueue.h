f88b991edec37408cf084984e13d102661287518
tzik <tzik@chromium.org>
2016-08-04 00:41:56 -0700

    Remove WorkerEventQueue::EventDispatcherTask
    EventDispatcherTask is the last blocker of ExecutionContextTask removal.
    It holds an Event and WorkerEventQueue instance, and makes the Event
    cancellable.
    
    This CL reimplements the cancellation mechanism without the custom
    ExecutionContextTask and removes the custom one.
    
    BUG=630139
    
    Review-Url: https://codereview.chromium.org/2209613002
    Cr-Commit-Position: refs/heads/master@{#409732}

--- third_party/WebKit/Source/core/workers/WorkerEventQueue.h
+++ third_party/WebKit/Source/core/workers/WorkerEventQueue.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WorkerEventQueue_h
 
 #include "core/events/EventQueue.h"
-#include "wtf/HashMap.h"
 #include "wtf/HashSet.h"
 
 namespace blink {
@@ -51,13 +50,13 @@ public:
 
 private:
     explicit WorkerEventQueue(ExecutionContext*);
+    bool removeEvent(Event*);
+    void dispatchEvent(Event*, ExecutionContext*);
 
     Member<ExecutionContext> m_executionContext;
     bool m_isClosed;
 
-    class EventDispatcherTask;
-    using EventTaskMap = HeapHashMap<Member<Event>, EventDispatcherTask*>;
-    EventTaskMap m_eventTaskMap;
+    HeapHashSet<Member<Event>> m_pendingEvents;
 };
 
 } // namespace blink
