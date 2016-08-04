44535d6ea53a05441188dc1c6ed0bac7338b0091
aschulman <aschulman@chromium.org>
2016-08-04 15:29:58 -0700

    [serial] Only add a watcher if if the immeidate read attempt fails.
    BUG=634278
    
    Review-Url: https://codereview.chromium.org/2201893008
    Cr-Commit-Position: refs/heads/master@{#409917}

--- device/serial/serial_io_handler_posix.cc
+++ device/serial/serial_io_handler_posix.cc
@@ -124,8 +124,6 @@ void SerialIoHandlerPosix::ReadImpl() {
   DCHECK(pending_read_buffer());
   DCHECK(file().IsValid());
 
-  EnsureWatchingReads();
-
   // Try to read immediately. This is needed because on some platforms
   // (e.g., OSX) there may not be a notification from the message loop
   // when the fd is ready to read immediately after it is opened. There
@@ -304,7 +302,7 @@ void SerialIoHandlerPosix::OnFileCanReadWithoutBlocking(int fd) {
   AttemptRead(false);
 }
 
-void SerialIoHandlerPosix::AttemptRead(bool within_read) {
+bool SerialIoHandlerPosix::AttemptRead(bool within_read) {
   if (pending_read_buffer()) {
     int bytes_read = HANDLE_EINTR(read(file().GetPlatformFile(),
                                        pending_read_buffer(),
@@ -312,7 +310,7 @@ void SerialIoHandlerPosix::AttemptRead(bool within_read) {
     if (bytes_read < 0) {
       if (errno == EAGAIN) {
         // The fd does not have data to read yet so continue waiting.
-        return;
+        EnsureWatchingReads();
       } else if (errno == ENXIO) {
         RunReadCompleted(within_read, 0, serial::ReceiveError::DEVICE_LOST);
       } else {
@@ -344,6 +342,8 @@ void SerialIoHandlerPosix::AttemptRead(bool within_read) {
     is_watching_reads_ = false;
     file_read_watcher_.StopWatchingFileDescriptor();
   }
+
+  return true;
 }
 
 void SerialIoHandlerPosix::RunReadCompleted(bool within_read,
