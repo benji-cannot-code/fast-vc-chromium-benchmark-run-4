222fbd436e70a05dc936902cf9d54e6b9723298e
dougarnett <dougarnett@chromium.org>
2016-08-04 13:12:01 -0700

    [Offline Pages] Moves Coordinator to using MarkAttemptStarted/MarkAttemptCompleted API. This does fix the basic issue with the internals page showing STARTED status incorrectly although we will be removing this Status separately.
    Also adds new MarkAttemptAborted method for dealing with attempts we cancel because
    chrome transitions to the foreground. [I imaging adding Pause support might also be reason to MarkAttemptAborted.]
    BUG=632143
    
    Review-Url: https://codereview.chromium.org/2209813002
    Cr-Commit-Position: refs/heads/master@{#409881}

--- chrome/browser/android/offline_pages/prerendering_offliner.cc
+++ chrome/browser/android/offline_pages/prerendering_offliner.cc
@@ -108,6 +108,8 @@ void PrerenderingOffliner::OnSavePageDone(
 
 bool PrerenderingOffliner::LoadAndSave(const SavePageRequest& request,
                                        const CompletionCallback& callback) {
+  DCHECK(!pending_request_.get());
+
   if (pending_request_) {
     DVLOG(1) << "Already have pending request";
     return false;
--- components/offline_pages/background/offliner.h
+++ components/offline_pages/background/offliner.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OFFLINE_PAGES_BACKGROUND_OFFLINER_H_
 #define COMPONENTS_OFFLINE_PAGES_BACKGROUND_OFFLINER_H_
 
+#include <string>
+
 #include "base/callback.h"
 
 namespace offline_pages {
