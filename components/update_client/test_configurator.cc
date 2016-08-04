6c3d81bc7c143b61e278d39b68d559acb47d694b
sorin <sorin@chromium.org>
2016-08-03 18:22:02 -0700

    Handle the case when the updates are disabled for a component.
    This change does not include the group policy changes.
    
    BUG=624128
    
    Review-Url: https://codereview.chromium.org/2206583007
    Cr-Commit-Position: refs/heads/master@{#409693}

--- components/update_client/test_configurator.cc
+++ components/update_client/test_configurator.cc
@@ -30,7 +30,8 @@ TestConfigurator::TestConfigurator(
       brand_("TEST"),
       initial_time_(0),
       ondemand_time_(0),
-      use_cup_signing_(false),
+      enabled_cup_signing_(false),
+      enabled_component_updates_(false),
       context_(new net::TestURLRequestContextGetter(network_task_runner)) {}
 
 TestConfigurator::~TestConfigurator() {
@@ -113,7 +114,7 @@ bool TestConfigurator::EnabledDeltas() const {
 }
 
 bool TestConfigurator::EnabledComponentUpdates() const {
-  return true;
+  return enabled_component_updates_;
 }
 
 bool TestConfigurator::EnabledBackgroundDownloader() const {
@@ -121,7 +122,7 @@ bool TestConfigurator::EnabledBackgroundDownloader() const {
 }
 
 bool TestConfigurator::EnabledCupSigning() const {
-  return use_cup_signing_;
+  return enabled_cup_signing_;
 }
 
 void TestConfigurator::SetBrand(const std::string& brand) {
@@ -136,8 +137,13 @@ void TestConfigurator::SetInitialDelay(int seconds) {
   initial_time_ = seconds;
 }
 
-void TestConfigurator::SetEnabledCupSigning(bool use_cup_signing) {
-  use_cup_signing_ = use_cup_signing;
+void TestConfigurator::SetEnabledCupSigning(bool enabled_cup_signing) {
+  enabled_cup_signing_ = enabled_cup_signing;
+}
+
+void TestConfigurator::SetEnabledComponentUpdates(
+    bool enabled_component_updates) {
+  enabled_component_updates_ = enabled_component_updates;
 }
 
 void TestConfigurator::SetDownloadPreference(
