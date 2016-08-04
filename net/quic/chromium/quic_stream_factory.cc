17e1d1d1f0a36e3c8b12928a33806b06d56f91cb
jri <jri@chromium.org>
2016-08-03 17:07:52 -0700

    Adds histogram element for connection migration.
    R=rch@chromium.org, asvitkine@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2206643002
    Cr-Commit-Position: refs/heads/master@{#409676}

--- net/quic/chromium/quic_stream_factory.cc
+++ net/quic/chromium/quic_stream_factory.cc
@@ -77,6 +77,7 @@ enum QuicConnectionMigrationStatus {
   MIGRATION_STATUS_SUCCESS,
   MIGRATION_STATUS_NON_MIGRATABLE_STREAM,
   MIGRATION_STATUS_DISABLED,
+  MIGRATION_STATUS_NO_ALTERNATE_NETWORK,
   MIGRATION_STATUS_MAX
 };
 
@@ -1473,11 +1474,9 @@ void QuicStreamFactory::MaybeMigrateSingleSession(
       FindAlternateNetwork(session->GetDefaultSocket()->GetBoundNetwork());
   if (new_network == NetworkChangeNotifier::kInvalidNetworkHandle) {
     // No alternate network found.
-    // TODO (jri): Add histogram for this failure case.
-    scoped_event_log.net_log().AddEvent(
-        NetLog::TYPE_QUIC_CONNECTION_MIGRATION_FAILURE,
-        base::Bind(&NetLogQuicConnectionMigrationFailureCallback,
-                   session->connection_id(), "No new network"));
+    HistogramAndLogMigrationFailure(
+        scoped_event_log.net_log(), MIGRATION_STATUS_NO_ALTERNATE_NETWORK,
+        session->connection_id(), "No alternate network found");
     return;
   }
   OnSessionGoingAway(session);
