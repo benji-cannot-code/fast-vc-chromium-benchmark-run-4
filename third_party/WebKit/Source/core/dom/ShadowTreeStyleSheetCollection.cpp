5ba9b17af27ec19a856b3d103183949d28ade37c
hayato <hayato@chromium.org>
2016-08-03 23:34:32 -0700

    Allow <link rel=stylesheet> in a connected shadow tree
    See https://github.com/w3c/webcomponents/issues/530. We have relaxed the condition.
    
    "Web-Facing Change PSA" mail in blink-dev is:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/b7g62d2yqWo/2itpAFafBgAJ
    
    Regarding a browsing context, this CL does not change any behavior. That should be
    another concern and should be investigated later.
    
    See also https://github.com/w3c/webcomponents/issues/535. "title= attribute" is
    ignored completely for <link rel=stylesheet>.
    
    BUG=630141
    
    Review-Url: https://codereview.chromium.org/2177163002
    Cr-Commit-Position: refs/heads/master@{#409728}

--- third_party/WebKit/Source/core/dom/ShadowTreeStyleSheetCollection.cpp
+++ third_party/WebKit/Source/core/dom/ShadowTreeStyleSheetCollection.cpp
@@ -52,9 +52,6 @@ void ShadowTreeStyleSheetCollection::collectStyleSheets(StyleEngine& engine, Sty
         StyleSheetCandidate candidate(*n);
         DCHECK(!candidate.isXSL());
 
-        if (!candidate.isCSSStyle())
-            continue;
-
         StyleSheet* sheet = candidate.sheet();
         if (!sheet)
             continue;
