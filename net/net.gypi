ad97f08a9a37ecfc50458cc4cc8301424fc1be10
diannahu <diannahu@chromium.org>
2016-08-04 11:08:57 -0700

    Add SpdyNoOpVisitor, which ignores calls.
    This CL lands server change 127075410 by jamessynge.
    
    BUG=488484
    
    Review-Url: https://codereview.chromium.org/2206943003
    Cr-Commit-Position: refs/heads/master@{#409834}

--- net/net.gypi
+++ net/net.gypi
@@ -1804,6 +1804,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'spdy/spdy_http_stream_unittest.cc',
       'spdy/spdy_http_utils_unittest.cc',
       'spdy/spdy_network_transaction_unittest.cc',
+      'spdy/spdy_no_op_visitor.h',
+      'spdy/spdy_no_op_visitor.cc',
       'spdy/spdy_pinnable_buffer_piece_test.cc',
       'spdy/spdy_prefixed_buffer_reader_test.cc',
       'spdy/spdy_protocol_test.cc',
---
+++ net/spdy/spdy_no_op_visitor.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/spdy/spdy_no_op_visitor.h"
+
+#include <type_traits>
+
+namespace net {
+namespace test {
+
+SpdyNoOpVisitor::SpdyNoOpVisitor() {
+  static_assert(std::is_abstract<SpdyNoOpVisitor>::value == false,
+                "Need to update SpdyNoOpVisitor.");
+}
+SpdyNoOpVisitor::~SpdyNoOpVisitor() {}
+
+net::SpdyHeadersHandlerInterface* SpdyNoOpVisitor::OnHeaderFrameStart(
+    SpdyStreamId stream_id) {
+  return this;
+}
+
+bool SpdyNoOpVisitor::OnControlFrameHeaderData(SpdyStreamId stream_id,
+                                               const char* header_data,
+                                               size_t header_data_len) {
+  return true;
+}
+
+bool SpdyNoOpVisitor::OnUnknownFrame(SpdyStreamId stream_id, int frame_type) {
+  return true;
+}
+
+}  // namespace test
+}  // namespace net
