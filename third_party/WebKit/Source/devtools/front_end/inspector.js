dbeaf8cf71d64ff7aa893c9a9e9bcf66c50a76c1
chenwilliam <chenwilliam@chromium.org>
2016-08-04 12:56:57 -0700

    DevTools: improve UX for hosted mode + fix bug
    This CL includes several quick improvements for hosted mode:
    - Properly serves InspectorBackendCommands.js. It turns out the metadata from
    the file is necessary for certain operations (e.g. $0 in the console).
    - Can now customize remote debugging port in addition to server port
    (e.g. REMOTE_DEBUGGING_PORT=9333 PORT=1234 npm run server)
    - Provides helpful info if you go to server root path: (e.g. http://localhost:8090/).
    As Paul pointed out, users may go here to check if the server is working.
    “Please go to http://localhost:9222#http://localhost:8090/front_end/inspector.html?experiments=true”
    (Note: it accounts for custom ports passed in)
    - Add chrome favicon (same favicon as https://developer.chrome.com/devtools)
    
    I'll update the docs with the RDP info once this CL has landed.
    
    BUG=629914
    
    Review-Url: https://codereview.chromium.org/2195113002
    Cr-Commit-Position: refs/heads/master@{#409878}

--- third_party/WebKit/Source/devtools/front_end/inspector.js
+++ third_party/WebKit/Source/devtools/front_end/inspector.js
@@ -3,13 +3,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Preload protocol resources for hosted mode.
-if (!/** @type {?Object} */(window.InspectorFrontendHost)) {
-    Promise.all([
-        Runtime.loadResourceIntoCache("./sdk/protocol/browser_protocol.json", false /* appendSourceURL */),
-        Runtime.loadResourceIntoCache("./sdk/protocol/js_protocol.json", false /* appendSourceURL */)
-    ]).then(() => Runtime.startApplication("inspector"));
-} else {
-    Runtime.startApplication("inspector");
-}
-
+Runtime.startApplication("inspector");
