8ce15b122010fa9d15441583f031d4e93961dcf6
vakh <vakh@chromium.org>
2016-08-05 14:17:18 -0700

    Move code for PVer4 from util.* to v4_protocol_manager_util.*
    BUG=543161
    
    Review-Url: https://codereview.chromium.org/2218723002
    Cr-Commit-Position: refs/heads/master@{#410164}

--- chrome/browser/safe_browsing/local_database_manager.cc
+++ chrome/browser/safe_browsing/local_database_manager.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing_db/util.h"
+#include "components/safe_browsing_db/v4_protocol_manager_util.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_service.h"
 #include "net/url_request/url_request_context_getter.h"
@@ -137,7 +138,7 @@ ListType GetUrlSeverestThreatListType(
     return INVALID;
 
   std::vector<std::string> patterns;
-  GeneratePatternsToCheck(url, &patterns);
+  V4ProtocolManagerUtil::GeneratePatternsToCheck(url, &patterns);
 
   ListType pending_threat = INVALID;
   int pending_threat_severity = GetThreatSeverity(INVALID);
