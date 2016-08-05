e9c7aa3495459eeb43c9d1ff0cff63661dfddd0a
yosin <yosin@chromium.org>
2016-08-04 22:34:03 -0700

    Change return value of FrameSelection::modify() to prevent unnecessary window scrolling
    This patch changes return value of |FrameSelection::modify()| to prevent
    unnecessary window scrolling by returning true, indicating user action is
    processed. Note: for spacial navigation case, |FrameSelection::modify()| returns
    false to allow default key event handler of spacial navigation to move
    focus to another element.
    
    Before this patch, hitting right arrow key at end of text field causes
    window scrolling by WebViewImpl::keyEventDefault() due by
    |FrameSelection::modify()| returns false since selection isn't changed.
    
    BUG=633376
    TEST=run_webkit_unit_tests --gtest_filter=FrameSelectionTest.ModifyWithUserTriggered
    
    Review-Url: https://codereview.chromium.org/2200833007
    Cr-Commit-Position: refs/heads/master@{#409995}

--- third_party/WebKit/Source/core/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/core/editing/FrameSelection.cpp
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/page/FocusController.h"
 #include "core/page/FrameTree.h"
 #include "core/page/Page.h"
+#include "core/page/SpatialNavigation.h"
 #include "core/paint/PaintLayer.h"
 #include "platform/SecureTextInput.h"
 #include "platform/geometry/FloatQuad.h"
@@ -611,6 +612,12 @@ static DispatchEventResult dispatchSelectStart(const VisibleSelection& selection
     return selectStartTarget->dispatchEvent(Event::createCancelableBubble(EventTypeNames::selectstart));
 }
 
+// The return value of |FrameSelection::modify()| is different based on
+// value of |userTriggered| parameter.
+// When |userTriggered| is |userTriggered|, |modify()| returns false if
+// "selectstart" event is dispatched and canceled, otherwise returns true.
+// When |userTriggered| is |NotUserTrigged|, return value specifies whether
+// selection is modified or not.
 bool FrameSelection::modify(EAlteration alter, SelectionDirection direction, TextGranularity granularity, EUserTriggered userTriggered)
 {
     SelectionModifier selectionModifier(*frame(), selection(), m_xPosForVerticalArrowNavigation);
@@ -621,8 +628,17 @@ bool FrameSelection::modify(EAlteration alter, SelectionDirection direction, Tex
         && dispatchSelectStart(selection()) != DispatchEventResult::NotCanceled) {
         return false;
     }
-    if (!modified)
-        return false;
+    if (!modified) {
+        if (userTriggered == NotUserTriggered)
+            return false;
+        // If spatial navigation enabled, focus navigator will move focus to
+        // another element. See snav-input.html and snav-textarea.html
+        if (isSpatialNavigationEnabled(m_frame))
+            return false;
+        // Even if selection isn't changed, we prevent to default action, e.g.
+        // scroll window when caret is at end of content editable.
+        return true;
+    }
 
     const SetSelectionOptions options = CloseTyping | ClearTypingStyle | userTriggered;
     setSelection(selectionModifier.selection(), options);
