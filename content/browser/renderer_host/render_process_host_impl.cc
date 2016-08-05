ba362373d5840219c80bb873eba7d5e630a5db8a
eseckler <eseckler@chromium.org>
2016-08-05 02:16:12 -0700

    Add flag to hide (native composited) scrollbars completely.
    For headless chrome, we're interested in controlling the visibility of scrollbars in screenshots. As a first step, we would like to hide scrollbars completely. This patch adds a --enable-hidden-scrollbar flag, which utilizes non-animated overlay scrollbars. When enabled, the scrollbars never appear and are effectively hidden completely. This affects only native composited scrollbars (a future patch will add support for hiding native non-composited scrollbars in Blink).
    
    BUG=617618
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2175163005
    Cr-Commit-Position: refs/heads/master@{#410018}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1537,6 +1537,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kEnableCanvas2dDynamicRenderingModeSwitching,
     switches::kForceOverlayFullscreenVideo,
     switches::kFullMemoryCrashReport,
+    switches::kHideScrollbars,
     switches::kInertVisualViewport,
     switches::kIPCConnectionTimeout,
     switches::kIsRunningInMash,
