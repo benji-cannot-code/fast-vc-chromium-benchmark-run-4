392c04bebef0f7742c29cdfd0bb653a418d539e5
justincohen <justincohen@chromium.org>
2016-08-05 14:43:45 -0700

    Revert of Add joinable option to SimpleThread::Options as was just done for Thread.
    (patchset #2 id:20001 of https://codereview.chromium.org/2204333003/ )
    
    Reason for revert:
    Reverting this since it is breaking the iOS base_unittests.
    
    gba@ is working on a fix in https://codereview.chromium.org/2218983003/
    
    Original issue's description:
    > Add joinable option to SimpleThread::Options as was just done for Thread::Options.
    >
    > SimpleThread will be more suitable than Thread for
    > https://codereview.chromium.org/2197753002/ per it not needing a MessageLoop.
    >
    > Also, cleanup the SimpleThread::Options API, making it more like Thread::Options.
    >
    > BUG=629716
    > TBR=avi@chromium.org
    >
    > Committed: https://crrev.com/8b35b0572443f4a84377a1fbd199a1edabe3d7d6
    > Cr-Commit-Position: refs/heads/master@{#409830}
    
    TBR=thestig@chromium.org,avi@chromium.org,gab@chromium.org
    BUG=629716
    
    Review-Url: https://codereview.chromium.org/2219663006
    Cr-Commit-Position: refs/heads/master@{#410169}

--- base/threading/simple_thread_unittest.cc
+++ base/threading/simple_thread_unittest.cc
@@ -3,28 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <memory>
-
 #include "base/atomic_sequence_num.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/synchronization/waitable_event.h"
-#include "base/test/gtest_util.h"
-#include "base/threading/platform_thread.h"
 #include "base/threading/simple_thread.h"
-#include "base/time/time.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
 
 namespace {
-
-// Sleeps a tiny amount to augment chances of tests using it running in races
-// should there be any while the tested thread is alive.
-class SleepRunner : public DelegateSimpleThread::Delegate {
-  void Run() override {
-    PlatformThread::Sleep(TimeDelta::FromMilliseconds(20));
-  }
-};
 
 class SetIntRunner : public DelegateSimpleThread::Delegate {
  public:
@@ -145,25 +132,6 @@ TEST(SimpleThreadTest, NamedWithOptions) {
   EXPECT_EQ(thread.name_prefix(), "event_waiter");
   EXPECT_EQ(thread.name(),
             std::string("event_waiter/") + IntToString(thread.tid()));
-}
-
-TEST(SimpleThreadTest, JoinNonJoinableThreadDeath) {
-  SleepRunner runner;
-  SimpleThread::Options options;
-  options.joinable = false;
-  DelegateSimpleThread thread(&runner, "noop_runner", options);
-
-  thread.Start();
-  EXPECT_DCHECK_DEATH(thread.Join());
-}
-
-TEST(SimpleThreadTest, DeleteNonJoinableWithoutJoinIsSafe) {
-  SleepRunner runner;
-  SimpleThread::Options options;
-  options.joinable = false;
-  DelegateSimpleThread thread(&runner, "noop_runner", options);
-  thread.Start();
-  // Nothing blows up when |thread| goes out of scope.
 }
 
 TEST(SimpleThreadTest, ThreadPool) {
