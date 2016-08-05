96a4c6629c67bd222582802eb25cf58f11e823fb
staraz <staraz@chromium.org>
2016-08-05 07:10:34 -0700

    Struct traits used by GpuInfo
    This CL contains struct traits of the structs used by gpu::GPUInfo
    
    BUG=622707
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2147693002
    Cr-Commit-Position: refs/heads/master@{#410053}

---
+++ mojo/common/common_custom_types_struct_traits.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MOJO_COMMON_COMMON_CUSTOM_TYPES_STRUCT_TRAITS_H_
+#define MOJO_COMMON_COMMON_CUSTOM_TYPES_STRUCT_TRAITS_H_
+
+#include "mojo/common/common_custom_types.mojom.h"
+
+namespace base {
+class Version;
+}
+
+namespace mojo {
+
+template <>
+struct StructTraits<mojo::common::mojom::Version, base::Version> {
+  static const std::vector<uint32_t>& components(const base::Version& version);
+  static bool Read(mojo::common::mojom::VersionDataView data,
+                   base::Version* out);
+};
+
+}  // namespace mojo
+
+#endif  // MOJO_COMMON_COMMON_CUSTOM_TYPES_STRUCT_TRAITS_H_
