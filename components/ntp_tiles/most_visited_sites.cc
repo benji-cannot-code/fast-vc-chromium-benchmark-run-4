6710541c59c0fe116c666399556288caa79de348
sfiera <sfiera@chromium.org>
2016-08-05 03:46:42 -0700

    Make supervisor arg to MostVisitedSites optional.
    On iOS, we're not going to have a SupervisedUserService, so don't
    require one. Without it, we assume a fully-privileged user: nothing
    blocked, nothing whitelisted.
    
    (also, add DCHECKs for the args that are required)
    
    BUG=631990
    
    Review-Url: https://codereview.chromium.org/2214873002
    Cr-Commit-Position: refs/heads/master@{#410024}

--- components/ntp_tiles/most_visited_sites.cc
+++ components/ntp_tiles/most_visited_sites.cc
@@ -158,12 +158,16 @@ MostVisitedSites::MostVisitedSites(PrefService* prefs,
       top_sites_observer_(this),
       mv_source_(NTPTileSource::SUGGESTIONS_SERVICE),
       weak_ptr_factory_(this) {
+  DCHECK(prefs_);
+  DCHECK(top_sites_);
   DCHECK(suggestions_service_);
-  supervisor_->SetObserver(this);
+  if (supervisor_)
+    supervisor_->SetObserver(this);
 }
 
 MostVisitedSites::~MostVisitedSites() {
-  supervisor_->SetObserver(nullptr);
+  if (supervisor_)
+    supervisor_->SetObserver(nullptr);
 }
 
 void MostVisitedSites::SetMostVisitedURLsObserver(Observer* observer,
@@ -292,9 +296,11 @@ void MostVisitedSites::InitiateTopSitesQuery() {
 }
 
 base::FilePath MostVisitedSites::GetWhitelistLargeIconPath(const GURL& url) {
-  for (const auto& whitelist : supervisor_->whitelists()) {
-    if (AreURLsEquivalent(whitelist.entry_point, url))
-      return whitelist.large_icon_path;
+  if (supervisor_) {
+    for (const auto& whitelist : supervisor_->whitelists()) {
+      if (AreURLsEquivalent(whitelist.entry_point, url))
+        return whitelist.large_icon_path;
+    }
   }
   return base::FilePath();
 }
@@ -310,7 +316,7 @@ void MostVisitedSites::OnMostVisitedURLsAvailable(
       num_tiles = i;
       break;  // This is the signal that there are no more real visited sites.
     }
-    if (supervisor_->IsBlocked(visited.url))
+    if (supervisor_ && supervisor_->IsBlocked(visited.url))
       continue;
 
     NTPTile tile;
@@ -344,7 +350,7 @@ void MostVisitedSites::OnSuggestionsProfileAvailable(
   for (int i = 0; i < num_tiles; ++i) {
     const ChromeSuggestion& suggestion_pb = suggestions_profile.suggestions(i);
     GURL url(suggestion_pb.url());
-    if (supervisor_->IsBlocked(url))
+    if (supervisor_ && supervisor_->IsBlocked(url))
       continue;
 
     NTPTile tile;
@@ -364,6 +370,10 @@ void MostVisitedSites::OnSuggestionsProfileAvailable(
 
 NTPTilesVector MostVisitedSites::CreateWhitelistEntryPointTiles(
     const NTPTilesVector& personal_tiles) {
+  if (!supervisor_) {
+    return NTPTilesVector();
+  }
+
   size_t num_personal_tiles = personal_tiles.size();
   DCHECK_LE(num_personal_tiles, static_cast<size_t>(num_sites_));
 
@@ -406,7 +416,7 @@ NTPTilesVector MostVisitedSites::CreatePopularSitesTiles(
     const NTPTilesVector& personal_tiles,
     const NTPTilesVector& whitelist_tiles) {
   // For child accounts popular sites tiles will not be added.
-  if (supervisor_->IsChildProfile())
+  if (supervisor_ && supervisor_->IsChildProfile())
     return NTPTilesVector();
 
   size_t num_tiles = personal_tiles.size() + whitelist_tiles.size();
