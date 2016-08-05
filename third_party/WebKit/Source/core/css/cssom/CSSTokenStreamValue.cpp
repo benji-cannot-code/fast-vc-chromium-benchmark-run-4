d42a3071068b494367bce8ddb349cb34569ab1f8
anthonyhkf <anthonyhkf@google.com>
2016-08-04 20:02:14 -0700

    [Typed-OM] Add compound type of CSSVariableReferenceValue and String with some tests
    Spec: https://drafts.css-houdini.org/css-typed-om/#tokenstreamvalue-objects
    
    BUG=545318
    
    Review-Url: https://codereview.chromium.org/2140073002
    Cr-Commit-Position: refs/heads/master@{#409975}

--- third_party/WebKit/Source/core/css/cssom/CSSTokenStreamValue.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSTokenStreamValue.cpp
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/cssom/CSSTokenStreamValue.h"
 
 #include "core/css/CSSVariableReferenceValue.h"
+#include "core/css/cssom/CSSStyleVariableReferenceValue.h"
 #include "core/css/parser/CSSTokenizer.h"
 #include "wtf/text/StringBuilder.h"
 
@@ -24,7 +25,7 @@ public:
     {
         if (m_index >= m_tokenStreamValue->size())
             return false;
-        value.setString(m_tokenStreamValue->fragmentAtIndex(m_index));
+        value = m_tokenStreamValue->fragmentAtIndex(m_index);
         return true;
     }
 
@@ -52,7 +53,12 @@ CSSValue* CSSTokenStreamValue::toCSSValue() const
     for (unsigned i = 0; i < m_fragments.size(); i++) {
         if (i)
             tokens.append("/**/");
-        tokens.append(m_fragments[i]);
+        if (m_fragments[i].isString())
+            tokens.append(m_fragments[i].getAsString());
+        else if (m_fragments[i].isCSSVariableReferenceValue())
+            tokens.append(m_fragments[i].getAsCSSVariableReferenceValue()->variable());
+        else
+            NOTREACHED();
     }
 
     CSSTokenizer::Scope scope(tokens.toString());
--- third_party/WebKit/Source/core/css/cssom/CSSTokenStreamValue.h
+++ third_party/WebKit/Source/core/css/cssom/CSSTokenStreamValue.h
@@ -17,7 +17,7 @@ class CORE_EXPORT CSSTokenStreamValue final : public CSSStyleValue, public Value
     WTF_MAKE_NONCOPYABLE(CSSTokenStreamValue);
     DEFINE_WRAPPERTYPEINFO();
 public:
-    static CSSTokenStreamValue* create(const Vector<String>& fragments)
+    static CSSTokenStreamValue* create(const HeapVector<StringOrCSSVariableReferenceValue>& fragments)
     {
         return new CSSTokenStreamValue(fragments);
     }
@@ -26,12 +26,18 @@ public:
 
     StyleValueType type() const override { return TokenStreamType; }
 
-    String fragmentAtIndex(int index) const { return m_fragments.at(index); }
+    StringOrCSSVariableReferenceValue fragmentAtIndex(int index) const { return m_fragments.at(index); }
 
     size_t size() const { return m_fragments.size(); }
 
+    DEFINE_INLINE_VIRTUAL_TRACE()
+    {
+        visitor->trace(m_fragments);
+        CSSStyleValue::trace(visitor);
+    }
+
 protected:
-    CSSTokenStreamValue(const Vector<String>& fragments)
+    CSSTokenStreamValue(const HeapVector<StringOrCSSVariableReferenceValue>& fragments)
         : CSSStyleValue()
         , m_fragments(fragments)
     {
@@ -40,7 +46,7 @@ protected:
 private:
     IterationSource* startIteration(ScriptState*, ExceptionState&) override;
 
-    Vector<String> m_fragments;
+    HeapVector<StringOrCSSVariableReferenceValue> m_fragments;
 };
 
 } // namespace blink
