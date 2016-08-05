c4821a2be1258e10f8ca3ab31114c91118945434
cbruni <cbruni@chromium.org>
2016-08-05 16:36:06 -0700

    Make ScriptForbiddenScope inlineable
    Stack allocated scopes work considerably faster if they have inlineable
    constructors and destructors. This CL moves ScriptForbiddenScope to it's header
    file.
    
    BUG=chromium:630217
    
    Review-Url: https://codereview.chromium.org/2213903002
    Cr-Commit-Position: refs/heads/master@{#410209}

--- third_party/WebKit/Source/platform/ScriptForbiddenScope.cpp
+++ third_party/WebKit/Source/platform/ScriptForbiddenScope.cpp
@@ -9,56 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-static unsigned s_scriptForbiddenCount = 0;
+unsigned ScriptForbiddenScope::s_scriptForbiddenCount = 0;
 
-ScriptForbiddenScope::ScriptForbiddenScope()
-{
-    enter();
-}
-
-ScriptForbiddenScope::~ScriptForbiddenScope()
-{
-    exit();
-}
-
-void ScriptForbiddenScope::enter()
-{
-    ASSERT(isMainThread());
-    ++s_scriptForbiddenCount;
-}
-
-void ScriptForbiddenScope::exit()
-{
-    ASSERT(isMainThread());
-    ASSERT(s_scriptForbiddenCount);
-    --s_scriptForbiddenCount;
-}
-
-bool ScriptForbiddenScope::isScriptForbidden()
-{
-    return isMainThread() && s_scriptForbiddenCount;
-}
-
-ScriptForbiddenScope::AllowUserAgentScript::AllowUserAgentScript()
-{
-    if (isMainThread())
-        m_change.emplace(&s_scriptForbiddenCount, 0);
-}
-
-ScriptForbiddenScope::AllowUserAgentScript::~AllowUserAgentScript()
-{
-    ASSERT(!isMainThread() || !s_scriptForbiddenCount);
-}
-
-ScriptForbiddenIfMainThreadScope::ScriptForbiddenIfMainThreadScope()
-{
-    if (isMainThread())
-        ScriptForbiddenScope::enter();
-}
-
-ScriptForbiddenIfMainThreadScope::~ScriptForbiddenIfMainThreadScope()
-{
-    if (isMainThread())
-        ScriptForbiddenScope::exit();
-}
 } // namespace blink
