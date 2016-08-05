3c47bd2ed78afac8581e3ad0d06720c7ba9b564c
lushnikov <lushnikov@chromium.org>
2016-08-05 11:25:14 -0700

    DevTools: do not use CSSMetadata instances for autocompletion
    The WI.CSSMetadata should serve a single purpose: hold meta-information about CSS properties.
    
    However, today WI.CSSMetadata also provides an API for CSS property autocompletion in
    Styles sidebar and Source frame. There's no good reason for that.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2215253002
    Cr-Commit-Position: refs/heads/master@{#410117}

--- third_party/WebKit/Source/devtools/front_end/sources/JavaScriptSourceFrame.js
+++ third_party/WebKit/Source/devtools/front_end/sources/JavaScriptSourceFrame.js
@@ -150,7 +150,6 @@ WebInspector.JavaScriptSourceFrame.prototype = {
             return;
         var networkURL = WebInspector.networkMapping.networkURL(uiSourceCode);
         var url = projectType === WebInspector.projectTypes.Formatter ? uiSourceCode.url() : networkURL;
-        var isContentScript = projectType === WebInspector.projectTypes.ContentScripts;
         if (!WebInspector.blackboxManager.isBlackboxedUISourceCode(uiSourceCode)) {
             this._hideBlackboxInfobar();
             return;
