bd55c1492159a707e0562333067ddfa38a935594
dschuyler <dschuyler@chromium.org>
2016-08-05 09:30:58 -0700

    [MD settings] policy controlled site settings
    This CL adds policy control icons to site setting URLs that are
    controlled by (admin) policy.
    
    There is a small extra in this CL which allows for the URLs to be selected (so the text can be copied).
    
    BUG=588721
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2218693002
    Cr-Commit-Position: refs/heads/master@{#410076}

--- chrome/browser/resources/settings/site_settings/site_list.js
+++ chrome/browser/resources/settings/site_settings/site_list.js
@@ -170,6 +170,24 @@ Polymer({
   },
 
   /**
+   * @param {string} source Where the setting came from.
+   * @return {boolean}
+   * @private
+   */
+  isPolicyControlled_: function(source) {
+    return source == 'policy';
+  },
+
+  /**
+   * @param {string} source Where the setting came from.
+   * @return {boolean}
+   * @private
+   */
+  shouldShowMenu_: function(source) {
+    return !(this.isPolicyControlled_(source) || this.allSites);
+  },
+
+  /**
    * Makes sure the visibility is correct for this widget.
    * @private
    */
@@ -300,7 +318,7 @@ Polymer({
   /**
    * Converts an unordered site list to an ordered array, sorted by site name
    * then protocol and de-duped (by origin).
-   * @param {!Array<SiteException>} sites A list of sites to sort and de-dup.
+   * @param {!Array<SiteException>} sites A list of sites to sort and de-dupe.
    * @return {!Array<SiteException>} Sorted and de-duped list.
    * @private
    */
@@ -355,6 +373,7 @@ Polymer({
          originForDisplay: originForDisplay,
          embeddingOrigin: embeddingOrigin,
          embeddingOriginForDisplay: embeddingOriginForDisplay,
+         source: sites[i].source,
       });
 
       lastOrigin = originForDisplay;
@@ -383,6 +402,9 @@ Polymer({
    */
   onOriginTap_: function(event) {
     this.selectedSite = event.model.item;
+    if (this.isPolicyControlled_(this.selectedSite.source))
+      return;
+
     if (this.allSites)
       settings.navigateTo(settings.Route.SITE_SETTINGS_ALL_DETAILS);
     else
