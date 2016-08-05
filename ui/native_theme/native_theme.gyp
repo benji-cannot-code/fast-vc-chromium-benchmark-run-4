e5d0eba9224c42dc287f1bed7506fce59bc4c1fb
ckulakowski <ckulakowski@opera.com>
2016-08-05 11:03:43 -0700

    Initialize MaterialDesignController in NativeThemeMacTest
    This is fix for crash (CHECK) in NativeThemeMacTest.SystemColorsExist:
    FATAL:material_design_controller.cc(66)] Check failed: is_mode_initialized_
    
    BUG=625642
    
    Review-Url: https://codereview.chromium.org/2120273002
    Cr-Commit-Position: refs/heads/master@{#410108}

--- ui/native_theme/native_theme.gyp
+++ ui/native_theme/native_theme.gyp
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '../../base/base.gyp:test_support_base',
         '../../skia/skia.gyp:skia',
         '../../testing/gtest.gyp:gtest',
+        '../base/ui_base.gyp:ui_base',
         '../gfx/gfx.gyp:gfx_geometry',
         'native_theme',
       ],
--- ui/native_theme/native_theme_mac_unittest.cc
+++ ui/native_theme/native_theme_mac_unittest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/native_theme/native_theme_mac.h"
 
 #include "base/mac/mac_util.h"
@@ -10,10 +11,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
+class NativeThemeMacTest : public testing::Test {
+ public:
+  static void SetUpTestCase() {
+    MaterialDesignController::Initialize();
+  }
+};
+
 // Test to ensure any system colors that are looked up by name exist on all Mac
 // platforms Chrome supports, and that their colorspace and component count is
 // sane.
-TEST(NativeThemeMacTest, SystemColorsExist) {
+TEST_F(NativeThemeMacTest, SystemColorsExist) {
   NativeTheme* native_theme = NativeThemeMac::instance();
   ASSERT_TRUE(native_theme);
   for (int i = 0; i < NativeTheme::kColorId_NumColors; ++i) {
@@ -28,7 +36,7 @@ TEST(NativeThemeMacTest, SystemColorsExist) {
 
 // Spot-check some system colours that can't be changed through System
 // Preferences.
-TEST(NativeThemeMacTest, SystemColorSpotChecks) {
+TEST_F(NativeThemeMacTest, SystemColorSpotChecks) {
   NativeTheme* native_theme = NativeThemeMac::instance();
   const SkColor kWindowColorCatsMavericks = SkColorSetARGB(255, 232, 232, 232);
   const SkColor kWindowColorYosemite = SkColorSetARGB(255, 236, 236, 236);
