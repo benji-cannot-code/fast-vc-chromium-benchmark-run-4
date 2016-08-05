77588aa0fdd74d453f78e96fedcc44c43981df49
servolk <servolk@chromium.org>
2016-08-04 17:18:39 -0700

    Notify demux stream clients when a stream is disabled.
    Currently DemuxerStream client are notified about the stream being
    disabled by an EOS buffer being returned from the Read call. But that
    is problematic on media pipelines with deep buffering (like Chromecast)
    where the client would have to read and process all previous buffers
    before getting the EOS buffer. In order to ensure that disabling a
    demuxer stream happens instantly, we need to notify clients via the
    same mechanism that is currently being used for notifying them about
    stream being re-enabled. This change will make it so that the
    corresponding renderer gets flushed as soon as the DemuxerStream is
    disabled. And then the renderer is going to be restarted, will read
    the EOS buffer from the stream and will go into the 'ended' state just
    like before.
    
    BUG=633299
    
    Review-Url: https://codereview.chromium.org/2199913002
    Cr-Commit-Position: refs/heads/master@{#409941}

--- media/base/fake_text_track_stream.cc
+++ media/base/fake_text_track_stream.cc
@@ -55,7 +55,8 @@ void FakeTextTrackStream::set_enabled(bool enabled, base::TimeDelta timestamp) {
   NOTIMPLEMENTED();
 }
 
-void FakeTextTrackStream::SetStreamRestartedCB(const StreamRestartedCB& cb) {
+void FakeTextTrackStream::SetStreamStatusChangeCB(
+    const StreamStatusChangeCB& cb) {
   NOTIMPLEMENTED();
 }
 
