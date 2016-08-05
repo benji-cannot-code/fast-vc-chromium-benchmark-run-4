b48b77c67721bae66b57a29ad286d074515acb80
takumif <takumif@chromium.org>
2016-08-05 12:38:58 -0700

    Revert of Disable Print in the context menu when a modal dialog is shown (patchset #3 id:40001 of https://codereview.chromium.org/2215123002/ )
    Reason for revert:
    chrome::FindBrowserWithWebContents(source_web_contents_) returns a nullptr for the PDF viewer, causing the Print option to be disabled for PDFs. We need to replace chrome::FindBrowserWithWebContents(source_web_contents_).
    
    Original issue's description:
    > Disable Print in the context menu when a modal dialog is shown
    >
    > Disable the "Print..." option in the page context menu when chrome::CanPrint
    > is false. This disables printing when a modal dialog, such as the Media Router
    > dialog, is being shown, just like the Print option in the hotdog menu.
    >
    > BUG=590650
    >
    > Committed: https://crrev.com/10d41a7cb5416afdb434e98c3b411284c0ab0be8
    > Cr-Commit-Position: refs/heads/master@{#410138}
    
    TBR=thestig@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=590650
    
    Review-Url: https://codereview.chromium.org/2214323003
    Cr-Commit-Position: refs/heads/master@{#410146}

--- chrome/browser/renderer_context_menu/render_view_context_menu.h
+++ chrome/browser/renderer_context_menu/render_view_context_menu.h
@@ -165,7 +165,6 @@ class RenderViewContextMenu : public RenderViewContextMenuBase {
   bool IsSavePageEnabled() const;
   bool IsPasteEnabled() const;
   bool IsPasteAndMatchStyleEnabled() const;
-  bool IsPrintPreviewEnabled() const;
   bool IsRouteMediaEnabled() const;
 
   // Command execution functions.
