937c4aa4fcf3e0b60e8f4709f643d6538ed708df
rsesek <rsesek@chromium.org>
2016-08-08 11:08:41 -0700

    Revert of Simple: Move PVer4 related code from util.* to v4_protocol_manager_util.* (patchset #3 id:80001 of https://codereview.chromium.org/2218723002/ )
    Reason for revert:
    Causing static initializers:
    
    https://build.chromium.org/p/chromium/builders/Mac/builds/18359
    
    # Static initializers in /b/c/b/mac_ng/src/out/Release/Chromium Framework.framework/Chromium Framework:
    # HINT: To get this list, run tools/mac/show_mod_init_func.py
    # HINT: diff against the log from the last run to see what changed
    /b/c/b/mac_ng/src/out/Release/Chromium Framework.unstripped
    0x00000000013489f0 @ _GLOBAL__sub_I_v4_store.cc (in Chromium Framework.unstripped) (v4_store.cc:0)
    0x000000000168c3f0 @ _GLOBAL__sub_I_safe_browsing_service.cc (in Chromium Framework.unstripped) (safe_browsing_service.cc:0)
    0x00000000016bbc30 @ _GLOBAL__sub_I_local_database_manager.cc (in Chromium Framework.unstripped) (local_database_manager.cc:0)
    0x00000000016e6300 @ _GLOBAL__sub_I_services_delegate_impl.cc (in Chromium Framework.unstripped) (services_delegate_impl.cc:0)
    0x000000000391ed70 @ _GLOBAL__sub_I_database_manager.cc (in Chromium Framework.unstripped) (database_manager.cc:0)
    0x000000000391fd00 @ _GLOBAL__sub_I_util.cc (in Chromium Framework.unstripped) (util.cc:0)
    0x0000000003934180 @ _GLOBAL__sub_I_v4_get_hash_protocol_manager.cc (in Chromium Framework.unstripped) (v4_get_hash_protocol_manager.cc:0)
    0x0000000003936ef0 @ _GLOBAL__sub_I_v4_local_database_manager.cc (in Chromium Framework.unstripped) (v4_local_database_manager.cc:0)
    0x0000000003938990 @ _GLOBAL__sub_I_v4_database.cc (in Chromium Framework.unstripped) (v4_database.cc:0)
    0x000000000393a580 @ _GLOBAL__sub_I_v4_update_protocol_manager.cc (in Chromium Framework.unstripped) (v4_update_protocol_manager.cc:0)
    
    Original issue's description:
    > Move code for PVer4 from util.* to v4_protocol_manager_util.*
    >
    > BUG=543161
    >
    > Committed: https://crrev.com/4424f4abf1e9b4ad4b4a52371e2646921e6872b5
    > Cr-Commit-Position: refs/heads/master@{#410164}
    
    TBR=nparker@chromium.org,vakh@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=543161
    
    Review-Url: https://codereview.chromium.org/2229533002
    Cr-Commit-Position: refs/heads/master@{#410397}

--- chrome/browser/safe_browsing/safe_browsing_service_browsertest.cc
+++ chrome/browser/safe_browsing/safe_browsing_service_browsertest.cc
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing_db/metadata.pb.h"
 #include "components/safe_browsing_db/test_database_manager.h"
 #include "components/safe_browsing_db/util.h"
-#include "components/safe_browsing_db/v4_protocol_manager_util.h"
 #include "components/subresource_filter/content/browser/content_subresource_filter_driver_factory.h"
 #include "content/public/browser/interstitial_page.h"
 #include "content/public/browser/navigation_entry.h"
@@ -486,7 +485,7 @@ class SafeBrowsingServiceTest : public InProcessBrowserTest {
                                    SBFullHashResult* full_hash) {
     std::string host;
     std::string path;
-    V4ProtocolManagerUtil::CanonicalizeUrl(url, &host, &path, nullptr);
+    CanonicalizeUrl(url, &host, &path, nullptr);
     full_hash->hash = SBFullHashForString(host + path);
     full_hash->list_id = list_id;
   }
