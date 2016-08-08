0611adf7d0475f92c09e10be6c8593473edc1fc3
gayane <gayane@chromium.org>
2016-08-08 09:25:49 -0700

    Allow changing metrics reporting state without callback.
    Allow changing metrics reporting state without providing
    callback in cases when the caller doesn't care about the
    success of the change.
    
    Review-Url: https://codereview.chromium.org/2215993002
    Cr-Commit-Position: refs/heads/master@{#410375}

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -633,9 +633,9 @@ void WizardController::OnUpdateCompleted() {
 void WizardController::OnEulaAccepted() {
   time_eula_accepted_ = base::Time::Now();
   StartupUtils::MarkEulaAccepted();
-  InitiateMetricsReportingChange(
+  ChangeMetricsReportingStateWithReply(
       usage_statistics_reporting_,
-      base::Bind(&WizardController::InitiateMetricsReportingChangeCallback,
+      base::Bind(&WizardController::OnChangedMetricsReportingState,
                  weak_factory_.GetWeakPtr()));
   PerformPostEulaActions();
 
@@ -646,7 +646,7 @@ void WizardController::OnEulaAccepted() {
   }
 }
 
-void WizardController::InitiateMetricsReportingChangeCallback(bool enabled) {
+void WizardController::OnChangedMetricsReportingState(bool enabled) {
   CrosSettings::Get()->SetBoolean(kStatsReportingPref, enabled);
   if (!enabled)
     return;
--- chrome/browser/ui/views/session_crashed_bubble_view.cc
+++ chrome/browser/ui/views/session_crashed_bubble_view.cc
@@ -92,9 +92,9 @@ void RecordBubbleHistogramValue(SessionCrashedBubbleHistogramValue value) {
 
 // Whether or not the bubble UI should be used.
 bool IsBubbleUIEnabled() {
-  // Function InitiateMetricsReportingChange (called when the user chooses to
-  // opt-in to UMA) does not support Chrome OS yet, so don't show the bubble on
-  // Chrome OS.
+// Function ChangeMetricsReportingState (called when the user chooses to
+// opt-in to UMA) does not support Chrome OS yet, so don't show the bubble on
+// Chrome OS.
 #if defined(OS_CHROMEOS)
   return false;
 #else
@@ -411,7 +411,7 @@ void SessionCrashedBubbleView::RestorePreviousSession() {
   // Record user's choice for opt-in in to UMA.
   // There's no opt-out choice in the crash restore bubble.
   if (uma_option_ && uma_option_->checked()) {
-    InitiateMetricsReportingChange(true, OnMetricsReportingCallbackType());
+    ChangeMetricsReportingState(true);
     RecordBubbleHistogramValue(SESSION_CRASHED_BUBBLE_UMA_OPTIN);
   }
   CloseBubble();
