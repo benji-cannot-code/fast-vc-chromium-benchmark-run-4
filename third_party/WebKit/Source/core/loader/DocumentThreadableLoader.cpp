da8f2304a7ad0a2bb8b66588a7bd5d1671ba95c3
yhirano <yhirano@chromium.org>
2016-08-08 00:17:50 -0700

    Revert of Move ThreadableLoader to Oilpan heap (3/3) (patchset #5 id:80001 of https://codereview.chromium.org/2193683004/ )
    Reason for revert:
    To investigate crashes reported at https://crbug.com/634546.
    
    Original issue's description:
    > Move ThreadableLoader to Oilpan heap (3/3)
    >
    > This CL is a refactoring and does not change the behavior.
    >
    > BUG=587663
    >
    > Committed: https://crrev.com/2ec047e0061d6c07339e404f918f34459ab6a7b8
    > Cr-Commit-Position: refs/heads/master@{#409961}
    
    TBR=haraken@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=587663
    
    Review-Url: https://codereview.chromium.org/2225733002
    Cr-Commit-Position: refs/heads/master@{#410312}

--- third_party/WebKit/Source/core/loader/DocumentThreadableLoader.cpp
+++ third_party/WebKit/Source/core/loader/DocumentThreadableLoader.cpp
@@ -342,7 +342,7 @@ void DocumentThreadableLoader::makeCrossOriginAccessRequest(const ResourceReques
 DocumentThreadableLoader::~DocumentThreadableLoader()
 {
     CHECK(!m_client);
-    DCHECK(!resource());
+    DCHECK(!m_resource);
 }
 
 void DocumentThreadableLoader::overrideTimeout(unsigned long timeoutMilliseconds)
@@ -972,9 +972,9 @@ Document& DocumentThreadableLoader::document() const
 
 DEFINE_TRACE(DocumentThreadableLoader)
 {
+    visitor->trace(m_resource);
     visitor->trace(m_document);
     ThreadableLoader::trace(visitor);
-    ResourceOwner<RawResource>::trace(visitor);
 }
 
 } // namespace blink
