b325653e79f36ab54bacd205d988f13392bb848b
ikilpatrick <ikilpatrick@chromium.org>
2016-08-08 13:40:32 -0700

    [LayoutNG] Add NGFragment class definition.
    Define initial fragment placeholder. Initially just contains the
    inline/block size, and inline/block offset.
    
    BUG=591099
    
    Review-Url: https://codereview.chromium.org/2229583002
    Cr-Commit-Position: refs/heads/master@{#410446}

---
+++ third_party/WebKit/Source/core/layout/ng/NGFragment.cpp
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/layout/ng/NGFragment.h"
+
+namespace blink {
+
+NGFragment::NGFragment(LayoutUnit inlineSize, LayoutUnit blockSize)
+    : m_inlineSize(inlineSize)
+    , m_blockSize(blockSize)
+{
+}
+
+void NGFragment::setOffset(LayoutUnit inlineOffset, LayoutUnit blockOffset)
+{
+    m_inlineOffset = inlineOffset;
+    m_blockOffset = blockOffset;
+}
+
+} // namespace blink
---
+++ third_party/WebKit/Source/core/layout/ng/NGFragment.h
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NGFragment_h
+#define NGFragment_h
+
+#include "core/CoreExport.h"
+#include "platform/LayoutUnit.h"
+
+namespace blink {
+
+class CORE_EXPORT NGFragment final {
+public:
+    NGFragment(LayoutUnit inlineSize, LayoutUnit blockSize);
+    ~NGFragment() { }
+
+    LayoutUnit inlineSize() const { return m_inlineSize; }
+    LayoutUnit blockSize() const { return m_blockSize; }
+
+    LayoutUnit inlineOffset() const { return m_inlineOffset; }
+    LayoutUnit blockOffset() const { return m_blockOffset; }
+
+    void setOffset(LayoutUnit inlineOffset, LayoutUnit blockOffset);
+
+private:
+    LayoutUnit m_inlineSize;
+    LayoutUnit m_blockSize;
+
+    LayoutUnit m_inlineOffset;
+    LayoutUnit m_blockOffset;
+};
+
+} // namespace blink
+
+#endif // NGFragment_h
