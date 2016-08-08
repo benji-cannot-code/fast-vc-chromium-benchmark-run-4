f2389feba98291f312ceee25454c9bbd404ee4da
jbudorick <jbudorick@chromium.org>
2016-08-08 08:49:14 -0700

    Revert of Add per-section clearing and dismissed suggestions to snippets-internals (patchset #3 id:40001 of https://codereview.chromium.org/2222853004/ )
    Reason for revert:
    Appears to have broken compilation on Android Builder: https://build.chromium.org/p/chromium.linux/builders/Android%20Builder/builds/70928
    
    Original issue's description:
    > Add per-section clearing and dismissed suggestions to snippets-internals
    >
    > Add new ClearCachedSuggestionsForDebugging,
    > GetDismissedSuggestionsForDebugging and
    > ClearDismissedSuggestionsForDebugging methods to the
    > ContentSuggestionsProvider. Implement these methods in all existing
    > providers. Adjust the unit tests.
    >
    > Call those methods from snippets-internals, build new internals UI for
    > viewing dismissed suggestions and clearing suggestions per category.
    > Remove now unused NTPSnippetsService-related things.
    >
    > BUG=619560
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    >
    > Committed: https://crrev.com/17361590f84745a69e0373e6482184e3bada6c3b
    > Cr-Commit-Position: refs/heads/master@{#410360}
    
    TBR=treib@chromium.org,bauerb@chromium.org,pke@google.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=619560
    
    Review-Url: https://codereview.chromium.org/2223963002
    Cr-Commit-Position: refs/heads/master@{#410365}

--- components/ntp_snippets/offline_pages/offline_page_suggestions_provider.cc
+++ components/ntp_snippets/offline_pages/offline_page_suggestions_provider.cc
@@ -50,7 +50,6 @@ std::vector<Category> OfflinePageSuggestionsProvider::GetProvidedCategories() {
 
 CategoryStatus OfflinePageSuggestionsProvider::GetCategoryStatus(
     Category category) {
-  DCHECK_EQ(category, provided_category_);
   return category_status_;
 }
 
@@ -69,23 +68,11 @@ void OfflinePageSuggestionsProvider::FetchSuggestionImage(
       FROM_HERE, base::Bind(callback, suggestion_id, gfx::Image()));
 }
 
-void OfflinePageSuggestionsProvider::ClearCachedSuggestionsForDebugging(
-    Category category) {
-  DCHECK_EQ(category, provided_category_);
+void OfflinePageSuggestionsProvider::ClearCachedSuggestionsForDebugging() {
   // Ignored.
 }
 
-std::vector<ContentSuggestion>
-OfflinePageSuggestionsProvider::GetDismissedSuggestionsForDebugging(
-    Category category) {
-  DCHECK_EQ(category, provided_category_);
-  // TODO(pke): Implement when dismissed suggestions are supported.
-  return std::vector<ContentSuggestion>();
-}
-
-void OfflinePageSuggestionsProvider::ClearDismissedSuggestionsForDebugging(
-    Category category) {
-  DCHECK_EQ(category, provided_category_);
+void OfflinePageSuggestionsProvider::ClearDismissedSuggestionsForDebugging() {
   // TODO(pke): Implement when dismissed suggestions are supported.
 }
 
