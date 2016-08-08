4e905e099c556678208a22f9ed21f3882ce56e1e
cbiesinger <cbiesinger@chromium.org>
2016-08-08 13:52:13 -0700

    [layoutng] Rename exclusion types to avoid conflicts with ComputedStyleConstants.h
    R=ikilpatrick@chromium.org
    BUG=591099
    
    Review-Url: https://codereview.chromium.org/2223163002
    Cr-Commit-Position: refs/heads/master@{#410453}

--- third_party/WebKit/Source/core/layout/ng/NGConstraintSpace.h
+++ third_party/WebKit/Source/core/layout/ng/NGConstraintSpace.h
@@ -18,10 +18,10 @@ class NGFragment;
 class NGLayoutOpportunityIterator;
 
 enum NGExclusionType {
-    ClearNone = 0,
-    ClearFloatLeft = 1,
-    ClearFloatRight = 2,
-    ClearFragment = 4
+    NGClearNone = 0,
+    NGClearFloatLeft = 1,
+    NGClearFloatRight = 2,
+    NGClearFragment = 4
 };
 
 enum NGFragmentationType {
@@ -103,7 +103,7 @@ public:
     DoublyLinkedList<const NGExclusion> exclusions(unsigned options = 0) const;
 
     NGLayoutOpportunityIterator layoutOpportunities(
-        unsigned clear = ClearNone,
+        unsigned clear = NGClearNone,
         NGExclusionFlowType avoid = ExcludeNone) const;
 
     // Modifies constraint space to account for a placed fragment. Depending on
