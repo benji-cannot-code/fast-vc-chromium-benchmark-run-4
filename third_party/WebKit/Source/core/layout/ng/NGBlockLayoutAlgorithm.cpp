627f6b8546c10d370836f6c450ae61f95b6161a1
cbiesinger <cbiesinger@chromium.org>
2016-08-09 13:54:44 -0700

    [LayoutNG] Very simple layout() implementation
    Just resolved width and height, and returns a fragment with that.
    
    R=eae@chromium.org,ikilpatrick@chromium.org,glebl@chromium.org
    BUG=635619
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2230563002
    Cr-Commit-Position: refs/heads/master@{#410814}

--- third_party/WebKit/Source/core/layout/ng/NGBlockLayoutAlgorithm.cpp
+++ third_party/WebKit/Source/core/layout/ng/NGBlockLayoutAlgorithm.cpp
@@ -5,10 +5,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/layout/ng/NGBlockLayoutAlgorithm.h"
 
+#include "core/layout/LayoutBox.h"
+#include "core/layout/ng/NGConstraintSpace.h"
+#include "core/layout/ng/NGFragment.h"
+#include "core/style/ComputedStyle.h"
+#include "platform/LengthFunctions.h"
+
 namespace blink {
 
-NGBlockLayoutAlgorithm::NGBlockLayoutAlgorithm()
+NGBlockLayoutAlgorithm::NGBlockLayoutAlgorithm(const ComputedStyle* style)
+    : m_style(style)
 {
+}
+
+NGFragment* NGBlockLayoutAlgorithm::layout(
+    const NGConstraintSpace& constraintSpace)
+{
+    LayoutUnit inlineSize = valueForLength(m_style->logicalWidth(),
+        constraintSpace.inlineContainerSize());
+    LayoutUnit blockSize = valueForLength(m_style->logicalHeight(),
+        constraintSpace.blockContainerSize());
+    return new NGFragment(inlineSize, blockSize, inlineSize, blockSize);
 }
 
 } // namespace blink
