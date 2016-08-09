80f8bd87e696235b0bc7a03ee6b4698daae108dd
sammc <sammc@chromium.org>
2016-08-08 20:04:43 -0700

    Require a user gesture to use navigator.share.
    BUG=635387
    
    Review-Url: https://codereview.chromium.org/2219383002
    Cr-Commit-Position: refs/heads/master@{#410556}

--- third_party/WebKit/Source/modules/webshare/NavigatorShare.cpp
+++ third_party/WebKit/Source/modules/webshare/NavigatorShare.cpp
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/frame/LocalFrame.h"
 #include "core/frame/Navigator.h"
 #include "modules/webshare/ShareData.h"
+#include "platform/UserGestureIndicator.h"
 #include "platform/mojo/MojoHelper.h"
 #include "public/platform/InterfaceProvider.h"
 #include "public/platform/Platform.h"
@@ -82,6 +83,11 @@ const char* NavigatorShare::supplementName()
 
 ScriptPromise NavigatorShare::share(ScriptState* scriptState, const ShareData& shareData)
 {
+    if (!UserGestureIndicator::utilizeUserGesture()) {
+        DOMException* error = DOMException::create(SecurityError, "Must be handling a user gesture to perform a share request.");
+        return ScriptPromise::rejectWithDOMException(scriptState, error);
+    }
+
     if (!m_service) {
         Document* doc = toDocument(scriptState->getExecutionContext());
         DCHECK(doc);
