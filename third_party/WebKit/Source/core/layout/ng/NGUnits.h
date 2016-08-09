0aab0e2418c6b13014e5bafe2ca62ee77c43260d
cbiesinger <cbiesinger@chromium.org>
2016-08-09 10:46:02 -0700

    [LayoutNG] Add units structures
    R=eae@chromium.org,ikilpatrick@chromium.org,glebl@chromium.org
    BUG=635619
    
    Review-Url: https://codereview.chromium.org/2229523004
    Cr-Commit-Position: refs/heads/master@{#410732}

---
+++ third_party/WebKit/Source/core/layout/ng/NGUnits.h
@@ -0,0 +1,47 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NGUnits_h
+#define NGUnits_h
+
+namespace blink {
+
+class LayoutUnit;
+
+struct NGLogicalSize {
+    LayoutUnit inlineSize;
+    LayoutUnit blockSize;
+};
+
+struct NGLogicalOffset {
+    LayoutUnit inlineOffset;
+    LayoutUnit blockOffset;
+};
+
+struct NGPhysicalSize {
+    LayoutUnit width;
+    LayoutUnit height;
+};
+
+struct NGPhysicalLocation {
+    LayoutUnit top;
+    LayoutUnit left;
+};
+
+struct NGPhysicalRect {
+    NGPhysicalSize size;
+    NGPhysicalLocation location;
+};
+
+struct NGPixelSnappedPhysicalRect {
+    int top;
+    int left;
+    int width;
+    int height;
+};
+
+} // namespace blink
+
+#endif // NGUnits_h
