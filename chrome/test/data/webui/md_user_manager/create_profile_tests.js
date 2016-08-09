35931410ac37c0daf20efd952067bc6b638feaff
mahmadi <mahmadi@chromium.org>
2016-08-09 13:00:45 -0700

    MD User Manager: Changes to 'import supervised users' dialog.
    1. Shows the 'no supervised users to import' message in the error banner instead.
    2. Adds a shadow to the banner for better separation from the content area.
    3. Changes paper-listbox's selected item style to Polymer's default.
    
    BUG=635043, 635048
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2223743003
    Cr-Commit-Position: refs/heads/master@{#410784}

--- chrome/test/data/webui/md_user_manager/create_profile_tests.js
+++ chrome/test/data/webui/md_user_manager/create_profile_tests.js
@@ -119,7 +119,9 @@ cr.define('user_manager.create_profile_tests', function() {
         // Create is not in progress.
         assertFalse(createProfileElement.createInProgress_);
         // Message container is visible.
-        assertFalse(createProfileElement.$$('#message-container').hidden);
+        var messageContainer =
+            createProfileElement.$$('#message-container');
+        assertTrue(messageContainer.clientHeight > 0);
         // Error message is set.
         assertEquals(
             loadTimeData.getString('custodianAccountNotSelectedError'),
@@ -147,7 +149,9 @@ cr.define('user_manager.create_profile_tests', function() {
               // Create is not in progress.
               assertFalse(createProfileElement.createInProgress_);
               // Message container is visible.
-              assertFalse(createProfileElement.$$('#message-container').hidden);
+              var messageContainer =
+                  createProfileElement.$$('#message-container');
+              assertTrue(messageContainer.clientHeight > 0);
               // Error message is set.
               var message = loadTimeData.getString(
                   'managedProfilesExistingLocalSupervisedUser');
@@ -176,11 +180,43 @@ cr.define('user_manager.create_profile_tests', function() {
               // Create is not in progress.
               assertFalse(createProfileElement.createInProgress_);
               // Message container is visible.
-              assertFalse(createProfileElement.$$('#message-container').hidden);
+              var messageContainer =
+                  createProfileElement.$$('#message-container');
+              assertTrue(messageContainer.clientHeight > 0);
               // Error message contains a link to import the supervised user.
               var message = createProfileElement.$.message;
               assertTrue(
                   !!message.querySelector('#supervised-user-import-existing'));
+            });
+      });
+
+      test('Displays error if custodian has no supervised users', function() {
+        browserProxy.setExistingSupervisedUsers([]);
+
+        // Simulate checking the checkbox.
+        MockInteractions.tap(createProfileElement.$$('paper-checkbox'));
+        Polymer.dom.flush();
+
+        // Select the first signed in user.
+        var dropdownMenu = createProfileElement.$$('paper-dropdown-menu');
+        var selector = dropdownMenu.querySelector('paper-listbox');
+        selector.selected = 0;
+
+        // Simulate clicking 'Import supervised user'.
+        MockInteractions.tap(createProfileElement.$$('#import-user'));
+
+        return browserProxy.whenCalled('getExistingSupervisedUsers').then(
+            function(args) {
+              // Create is not in progress.
+              assertFalse(createProfileElement.createInProgress_);
+              // Message container is visible.
+              var messageContainer =
+                  createProfileElement.$$('#message-container');
+              assertTrue(messageContainer.clientHeight > 0);
+              // Error message is set.
+              var message = loadTimeData.getString(
+                  'noSupervisedUserImportText');
+              assertEquals(message, createProfileElement.$.message.innerHTML);
             });
       });
 
