e15ac7d9226fff70843c1c602b2d5edad484053c
dpapad <dpapad@chromium.org>
2016-08-09 16:18:34 -0700

    MD Settings: Fix missing yellow rectangle highlighting.
    BUG=635409
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2226883002
    Cr-Commit-Position: refs/heads/master@{#410864}

--- chrome/browser/resources/settings/search_settings.js
+++ chrome/browser/resources/settings/search_settings.js
@@ -103,6 +103,7 @@ cr.define('settings', function() {
       } else {
         var span = document.createElement('span');
         span.classList.add(HIT_CSS_CLASS);
+        span.style.backgroundColor = '#ffeb3b'; // --var(--paper-yellow-500)
         span.textContent = tokens[i];
         wrapper.appendChild(span);
       }
