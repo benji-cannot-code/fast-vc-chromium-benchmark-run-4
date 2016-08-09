f98e0e9b7e903bc660ec24706498584e12fd7e6c
vmpstr <vmpstr@chromium.org>
2016-08-09 14:27:02 -0700

    cc: Remove unused parameter from GetOriginalImageDecode.
    The key parameter isn't used, so we can remove it from
    SoftwareImageDecodeController::GetOriginalImageDecode.
    
    R=ericrk@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2219673006
    Cr-Commit-Position: refs/heads/master@{#410826}

--- cc/tiles/software_image_decode_controller.cc
+++ cc/tiles/software_image_decode_controller.cc
@@ -370,7 +370,7 @@ SoftwareImageDecodeController::DecodeImageInternal(
   switch (key.filter_quality()) {
     case kNone_SkFilterQuality:
     case kLow_SkFilterQuality:
-      return GetOriginalImageDecode(key, std::move(image));
+      return GetOriginalImageDecode(std::move(image));
     case kMedium_SkFilterQuality:
     case kHigh_SkFilterQuality:
       return GetScaledImageDecode(key, std::move(image));
@@ -491,7 +491,6 @@ DecodedDrawImage SoftwareImageDecodeController::GetDecodedImageForDrawInternal(
 
 std::unique_ptr<SoftwareImageDecodeController::DecodedImage>
 SoftwareImageDecodeController::GetOriginalImageDecode(
-    const ImageKey& key,
     sk_sp<const SkImage> image) {
   SkImageInfo decoded_info =
       CreateImageInfo(image->width(), image->height(), format_);
