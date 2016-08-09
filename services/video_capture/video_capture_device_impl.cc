eff2990092dcfc81c91e8f6f685fcdd5bbb33342
chfremer <chfremer@chromium.org>
2016-08-09 10:57:55 -0700

    Video Capture Mojo Component: Skeleton
    For the bigger picture, please refer to the design document at [1].
    
    * Create Mojo interfaces in src/services/video_capture/public/interfaces.
    * Replicate simplified versions of the C++ interfaces of VideoCaptureDeviceFactory [2] and its dependencies.
      Keep interfaces close to originals and avoid name changes for now.
      Keep *.mojom files clear of comments for now. Intend to move over existing comments from the corresponding
      *.h at a later time.
      When the Mojo component has proven stable and all usages have been redirected, the originals are to be removed.
    * Create empty implementations.
    * Add a build target that generates the Mojo bindings.
    * Do not yet hook up the build target to anything.
    * Use a follow-up CL to package the component into a Mojo Shell Service and hook up the build targets.
    
    BUG=584797
    TEST=No new behavior added. Nothing new added to the build.
    
    [1] https://docs.google.com/a/chromium.org/document/d/1Qw7rw1AJy0QHXjha36jZNiEuxsxWslJ_X-zpOhijvI8/edit?usp=sharing
    [2] https://cs.chromium.org/chromium/src/media/capture/video/video_capture_device_factory.h?rcl=1468855046&l=18
    
    Review-Url: https://codereview.chromium.org/2155303002
    Cr-Commit-Position: refs/heads/master@{#410738}

---
+++ services/video_capture/video_capture_device_impl.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/logging.h"
+#include "services/video_capture/video_capture_device_impl.h"
+
+namespace video_capture {
+
+void VideoCaptureDeviceImpl::Start(
+    mojom::VideoCaptureFormatPtr requested_format,
+    mojom::ResolutionChangePolicy resolution_change_policy,
+    mojom::PowerLineFrequency power_line_frequency,
+    mojom::VideoCaptureDeviceClientPtr client) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace video_capture
---
+++ services/video_capture/video_capture_device_impl.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_VIDEO_CAPTURE_VIDEO_CAPTURE_DEVICE_IMPL_H_
+#define SERVICES_VIDEO_CAPTURE_VIDEO_CAPTURE_DEVICE_IMPL_H_
+
+#include "services/video_capture/public/interfaces/video_capture_device.mojom.h"
+
+namespace video_capture {
+
+// Implementation of the VideoCaptureDevice Mojo interface.
+class VideoCaptureDeviceImpl : public mojom::VideoCaptureDevice {
+ public:
+  // mojom::VideoCaptureDevice:
+  void Start(mojom::VideoCaptureFormatPtr requested_format,
+             mojom::ResolutionChangePolicy resolution_change_policy,
+             mojom::PowerLineFrequency power_line_frequency,
+             mojom::VideoCaptureDeviceClientPtr client) override;
+};
+
+}  // namespace video_capture
+
+#endif  // SERVICES_VIDEO_CAPTURE_VIDEO_CAPTURE_DEVICE_IMPL_H_
