c817c463f01932608e1184523e8b469b7bb9b385
btolsch <btolsch@chromium.org>
2016-08-09 00:35:35 -0700

    [Media Router WebUI] Move replace route responsibility to extension
    Currently the MR dialog controls the replace route functionality by
    stopping the current route and starting a new one when that completes.
    This doesn't allow the extension to optimize the change of source. This
    change replaces the current UI behavior by simply calling CreateRoute
    with the same sink and new source. This requires an extension change to
    support accepting CreateRoute calls to a sink with an existing route.
    
    BUG=614144
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation;master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2142613002
    Cr-Commit-Position: refs/heads/master@{#410607}

--- chrome/browser/resources/media_router/media_router_ui_interface.js
+++ chrome/browser/resources/media_router/media_router_ui_interface.js
@@ -204,6 +204,19 @@ cr.define('media_router.browserApi', function() {
   }
 
   /**
+   * Modifies |route| by changing its source to the one identified by
+   * |selectedCastMode|.
+   *
+   * @param {!media_router.Route} route The route being modified.
+   * @param {number} selectedCastMode The value of the cast mode the user
+   *   selected.
+   */
+  function changeRouteSource(route, selectedCastMode) {
+    chrome.send('requestRoute',
+                [{sinkId: route.sinkId, selectedCastMode: selectedCastMode}]);
+  }
+
+  /**
    * Closes the dialog.
    *
    * @param {boolean} pressEscToClose Whether the user pressed ESC to close the
@@ -388,6 +401,7 @@ cr.define('media_router.browserApi', function() {
   return {
     acknowledgeFirstRunFlow: acknowledgeFirstRunFlow,
     actOnIssue: actOnIssue,
+    changeRouteSource: changeRouteSource,
     closeDialog: closeDialog,
     closeRoute: closeRoute,
     joinRoute: joinRoute,
