4a0cf6f9df637fbb3f8920fab65cbae0cbf4176b
ymalik <ymalik@chromium.org>
2016-08-09 07:35:42 -0700

    Null check for inset observer view in tab
    In some cases a custom tab may be created even before setContentView
    is called and before InsetObserverView is created. Null check in tab
    before calling getInsetObserverView()
    
    This is a follow-up CL to https://codereview.chromium.org/1848343003.
    
    BUG=598182,598865
    
    Review-Url: https://codereview.chromium.org/2225923002
    Cr-Commit-Position: refs/heads/master@{#410664}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
@@ -467,7 +467,7 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
         @Override
         public int getSystemWindowInsetLeft() {
             ChromeActivity activity = getActivity();
-            if (activity != null) {
+            if (activity != null && activity.getInsetObserverView() != null) {
                 return activity.getInsetObserverView().getSystemWindowInsetsLeft();
             }
             return 0;
@@ -476,7 +476,7 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
         @Override
         public int getSystemWindowInsetTop() {
             ChromeActivity activity = getActivity();
-            if (activity != null) {
+            if (activity != null && activity.getInsetObserverView() != null) {
                 return activity.getInsetObserverView().getSystemWindowInsetsTop();
             }
             return 0;
@@ -485,7 +485,7 @@ public class Tab implements ViewGroup.OnHierarchyChangeListener,
         @Override
         public int getSystemWindowInsetRight() {
             ChromeActivity activity = getActivity();
-            if (activity != null) {
+            if (activity != null && activity.getInsetObserverView() != null) {
                 return activity.getInsetObserverView().getSystemWindowInsetsRight();
             }
             return 0;
