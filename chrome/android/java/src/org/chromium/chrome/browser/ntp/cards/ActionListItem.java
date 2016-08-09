cb5d98bb15f2d1c1df7393bf68f17b0a0673367a
dgn <dgn@chromium.org>
2016-08-09 09:47:52 -0700

    [NTP Client] Add an action button that can be used in sections
    Adds the button to the sections, that can be enabled when they are
    initialised.
    The current implementation of the tap handler just opens the
    bookmarks manager
    
    Preview: https://goo.gl/photos/Z6A3toNYpV649Yjx8
    
    BUG=610648
    
    Review-Url: https://codereview.chromium.org/2223743004
    Cr-Commit-Position: refs/heads/master@{#410706}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/ActionListItem.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ntp.cards;
+
+import android.view.View;
+
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ntp.NewTabPageView.NewTabPageManager;
+import org.chromium.chrome.browser.ntp.UiConfig;
+
+/**
+ * Item that allows the user to perform an action on the NTP.
+ */
+class ActionListItem implements NewTabPageListItem {
+    @Override
+    public int getType() {
+        return NewTabPageListItem.VIEW_TYPE_ACTION;
+    }
+
+    public static class ViewHolder extends CardViewHolder {
+        public ViewHolder(NewTabPageRecyclerView recyclerView, final NewTabPageManager manager,
+                UiConfig uiConfig) {
+            super(R.layout.new_tab_page_action_card, recyclerView, uiConfig);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // TODO(dgn): Implement other behaviours.
+                    manager.navigateToBookmarks();
+                }
+            });
+        }
+    }
+}
--- chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/SuggestionsSection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/SuggestionsSection.java
@@ -22,9 +22,12 @@ public class SuggestionsSection implements ItemGroup {
     private final SnippetHeaderListItem mHeader = new SnippetHeaderListItem();
     private StatusListItem mStatus;
     private final ProgressListItem mProgressIndicator = new ProgressListItem();
+    private final ActionListItem mMoreButton;
 
     public SuggestionsSection(List<SnippetArticleListItem> suggestions,
-            @CategoryStatusEnum int status, NewTabPageAdapter adapter) {
+            @CategoryStatusEnum int status, NewTabPageAdapter adapter, boolean showMoreButton) {
+        mMoreButton = showMoreButton ? new ActionListItem() : null;
+
         // TODO(mvanouwerkerk): Pass in the header text.
         setSuggestions(suggestions, status, adapter);
     }
@@ -37,6 +40,8 @@ public class SuggestionsSection implements ItemGroup {
         if (mSuggestions.isEmpty()) {
             items.add(mStatus);
             items.add(mProgressIndicator);
+        } else if (mMoreButton != null) {
+            items.add(mMoreButton);
         }
         return Collections.unmodifiableList(items);
     }
