3398cabae11487a02a331817c717bc264d653331
csharrison <csharrison@chromium.org>
2016-08-09 16:57:28 -0700

    [page_load_metrics] Log cache warmth ratios at parse stop
    This patch instruments ChromeResourceDispatcherHostDelegate to forward
    request information upon request completion to page_load_metrics. This
    data is then associated with the current committed navigation, and used
    to generate histograms of % cache warmth in terms of total requests. This is
    only an estimate, as the actual event in browser process where we log parse
    stop can be up to a second after the true parse stop. In any case, it is still
    useful data.
    
    If we think it is necessary, we can eagerly send the parse stop IPC to
    the browser process in a follow up.
    
    Note: This CL also coalesces short events posted to the UI thread to avoid
    spamming the task queue on request complete.
    
    BUG=634120
    
    Review-Url: https://codereview.chromium.org/2218583002
    Cr-Commit-Position: refs/heads/master@{#410881}

--- chrome/browser/page_load_metrics/page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/page_load_metrics_observer.h
@@ -70,6 +70,8 @@ struct PageLoadExtraInfo {
       const base::Optional<base::TimeDelta>& time_to_commit,
       UserAbortType abort_type,
       const base::Optional<base::TimeDelta>& time_to_abort,
+      int num_cache_requests,
+      int num_network_requests,
       const PageLoadMetadata& metadata);
 
   PageLoadExtraInfo(const PageLoadExtraInfo& other);
@@ -97,6 +99,12 @@ struct PageLoadExtraInfo {
   const UserAbortType abort_type;
 
   const base::Optional<base::TimeDelta> time_to_abort;
+
+  // Note: these are only approximations, based on WebContents attribution from
+  // ResourceRequestInfo objects while this is the currently committed load in
+  // the WebContents.
+  int num_cache_requests;
+  int num_network_requests;
 
   // Extra information supplied to the page load metrics system from the
   // renderer.
