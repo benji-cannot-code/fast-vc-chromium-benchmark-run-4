ff400b818ca0200d233e39b68325b6e55de86a96
ben <ben@chromium.org>
2016-08-09 13:39:44 -0700

    Fix a UAF with ConnectionFilterImpl where the weakptr used to bind a method wasn't being invalidated until after the bound object was destructed
    R=rockot@chromium.org
    http://crbug.com/635602
    
    Review-Url: https://codereview.chromium.org/2224013003
    Cr-Commit-Position: refs/heads/master@{#410803}

--- content/browser/renderer_host/render_process_host_impl.h
+++ content/browser/renderer_host/render_process_host_impl.h
@@ -367,6 +367,8 @@ class CONTENT_EXPORT RenderProcessHostImpl
   base::FilePath GetAecDumpFilePathWithExtensions(const base::FilePath& file);
 #endif
 
+  void DestroyConnectionFilter();
+
   static void OnMojoError(
       base::WeakPtr<RenderProcessHostImpl> process,
       scoped_refptr<base::SingleThreadTaskRunner> task_runner,
