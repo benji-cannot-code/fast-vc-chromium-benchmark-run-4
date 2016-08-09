2c0a26d83e14bd94cfcf090128b98bbba7748bc2
qyearsley <qyearsley@chromium.org>
2016-08-08 20:27:00 -0700

    In test_expectations_unittest, unconditionally import collections.OrderedDict.
    Reason: At this point, we can assume Python 2.7,
    and we can assume collections.OrderedDict is available.
    
    Review-Url: https://codereview.chromium.org/2194093002
    Cr-Commit-Position: refs/heads/master@{#410563}

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/models/test_expectations_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/models/test_expectations_unittest.py
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from collections import OrderedDict
 import unittest
 
 from webkitpy.common.host_mock import MockHost
@@ -34,12 +35,6 @@ from webkitpy.common.system.outputcapture import OutputCapture
 
 from webkitpy.layout_tests.models.test_configuration import *
 from webkitpy.layout_tests.models.test_expectations import *
-
-try:
-    from collections import OrderedDict
-except ImportError:
-    # Needed for Python < 2.7
-    from webkitpy.thirdparty.ordered_dict import OrderedDict
 
 
 class Base(unittest.TestCase):
