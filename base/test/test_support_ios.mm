e0a7738f6299093c9558b5b574b1798b849a443b
rohitrao <rohitrao@chromium.org>
2016-08-09 09:26:50 -0700

    Adds support for running unittests using XCTest on iOS.
    Modifies the base TestSuite to check if running under XCTest and return early
    without running tests if so.  This allows XCTest to drive running the individual
    tests, while still allowing the TestSuite to perform setup tasks.
    
    BUG=635509
    
    Review-Url: https://codereview.chromium.org/2226623003
    Cr-Commit-Position: refs/heads/master@{#410698}

--- base/test/test_support_ios.mm
+++ base/test/test_support_ios.mm
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/test/test_support_ios.h"
+
 #import <UIKit/UIKit.h>
 
 #include "base/debug/debugger.h"
@@ -156,6 +158,12 @@ static char** g_argv;
 - (void)runTests {
   int exitStatus = g_test_suite->Run();
 
+  if (base::IsRunningWithXCTest()) {
+    // Do not kill the app when running in XCTest mode.  XCTest will shut down
+    // the app itself.
+    return;
+  }
+
   if ([self shouldRedirectOutputToFile])
     [self writeOutputToNSLog];
 
@@ -216,6 +224,12 @@ void RunTestsFromIOSApp() {
                                         @"ChromeUnitTestDelegate");
     exit(exit_status);
   }
+}
+
+bool IsRunningWithXCTest() {
+  // Under Xcode 7.3 and 8.0, the "XCTestConfigurationFilePath" environment
+  // variable is set when running XCTests.
+  return getenv("XCTestConfigurationFilePath") != nullptr;
 }
 
 }  // namespace base
