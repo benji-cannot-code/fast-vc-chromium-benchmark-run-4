7a39675c95efe208bb35d8d7f06d83675c2ae194
nektar <nektar@chromium.org>
2016-08-08 17:22:05 -0700

    Exposed hierarchical level in IA2 level_in_group and fixed minor other issues.
    1. S_FALSE needs to be returned when all groupping IA2 arguments are not set.
    2. ARIA 1.1 user agent implementation guide says that if aria-posinset, aria-setsize and aria-level have a 0 or negative value, 1 should be returned instead.
    3. Heading level should not be exposed on headings when the value is somehow 0.
    4. Computed or manually set hierarchical level via aria-level should be exposed in IA2 level_in_group.
    The user visible change is that Jaws no longer announces "-1" and "0" for the tree items in Bookmark Manager.
    5. IA2 groupping information should not be printed in chrome://accessibility when not present.
    R=dmazzoni@chromium.org
    TESTED=Partly manually with Jaws
    
    Review-Url: https://codereview.chromium.org/2199113004
    Cr-Commit-Position: refs/heads/master@{#410513}

--- third_party/WebKit/Source/modules/accessibility/AXObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXObject.cpp
@@ -1071,8 +1071,10 @@ bool AXObject::supportsSetSizeAndPosInSet() const
         || (role == MenuItemRole && parentRole == MenuRole)
         || (role == RadioButtonRole)
         || (role == TabRole && parentRole == TabListRole)
-        || (role == TreeItemRole && parentRole == TreeRole))
+        || (role == TreeItemRole && parentRole == TreeRole)
+        || (role == TreeItemRole && parentRole == GroupRole)) {
         return true;
+    }
 
     return false;
 }
