3b79d8fe10b40a2c6ab6c6fc360c176dffe31b2e
estade <estade@chromium.org>
2016-08-09 11:31:21 -0700

    Implement Harmony textfield border
    In case it's unfamiliar, "Harmony" is the name of the version of MD we're using for secondary UI in chrome (dialogs, bubbles, etc.).
    
    BUG=635164,635160
    
    Review-Url: https://codereview.chromium.org/2228593002
    Cr-Commit-Position: refs/heads/master@{#410754}

--- ui/views/cocoa/bridged_native_widget_unittest.mm
+++ ui/views/cocoa/bridged_native_widget_unittest.mm
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "testing/gtest_mac.h"
 #import "ui/base/cocoa/window_size_constants.h"
 #include "ui/base/ime/input_method.h"
+#include "ui/base/material_design/material_design_controller.h"
+#include "ui/base/test/material_design_controller_test_api.h"
 #include "ui/events/test/cocoa_test_event_utils.h"
 #import "ui/gfx/mac/coordinate_conversion.h"
 #import "ui/gfx/test/ui_cocoa_test_helper.h"
@@ -240,6 +242,7 @@ class BridgedNativeWidgetTestBase : public ui::CocoaTest {
   // Overridden from testing::Test:
   void SetUp() override {
     ui::CocoaTest::SetUp();
+    ui::MaterialDesignController::Initialize();
 
     init_params_.native_widget = native_widget_mac_;
 
@@ -257,6 +260,11 @@ class BridgedNativeWidgetTestBase : public ui::CocoaTest {
     init_params_.bounds = gfx::Rect(100, 100, 100, 100);
 
     native_widget_mac_->GetWidget()->Init(init_params_);
+  }
+
+  void TearDown() override {
+    ui::test::MaterialDesignControllerTestAPI::Uninitialize();
+    ui::CocoaTest::TearDown();
   }
 
  protected:
