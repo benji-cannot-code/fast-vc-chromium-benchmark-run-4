7c91fba1c8a53092e84f7d4c810fa2fba89d6906
derat <derat@chromium.org>
2016-08-09 08:12:30 -0700

    chromeos: Add annotate to download and screenshot notifications.
    Add "Annotate in Google Keep" buttons to the image-download
    and screenshot notifications.
    
    BUG=605805
    TBR=oshima@chromium.org
    
    Review-Url: https://codereview.chromium.org/2225733003
    Cr-Commit-Position: refs/heads/master@{#410676}

--- chrome/browser/download/notification/download_item_notification.cc
+++ chrome/browser/download/notification/download_item_notification.cc
@@ -46,6 +46,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/message_center_style.h"
 
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/note_taking_app_utils.h"
+#endif  // defined(OS_CHROMEOS)
+
 using base::UserMetricsAction;
 
 namespace {
@@ -164,6 +168,10 @@ void RecordButtonClickAction(DownloadCommands::Command command) {
     case DownloadCommands::COPY_TO_CLIPBOARD:
       content::RecordAction(
           UserMetricsAction("DownloadNotification.Button_CopyToClipboard"));
+      break;
+    case DownloadCommands::ANNOTATE:
+      content::RecordAction(
+          UserMetricsAction("DownloadNotification.Button_Annotate"));
       break;
   }
 }
@@ -644,8 +652,13 @@ DownloadItemNotification::GetExtraActions() const {
       break;
     case content::DownloadItem::COMPLETE:
       actions->push_back(DownloadCommands::SHOW_IN_FOLDER);
-      if (!notification_->image().IsEmpty())
+      if (!notification_->image().IsEmpty()) {
         actions->push_back(DownloadCommands::COPY_TO_CLIPBOARD);
+#if defined(OS_CHROMEOS)
+        if (chromeos::IsNoteTakingAppAvailable(profile()))
+          actions->push_back(DownloadCommands::ANNOTATE);
+#endif  // defined(OS_CHROMEOS)
+      }
       break;
     case content::DownloadItem::MAX_DOWNLOAD_STATE:
       NOTREACHED();
@@ -732,6 +745,9 @@ base::string16 DownloadItemNotification::GetCommandLabel(
       break;
     case DownloadCommands::COPY_TO_CLIPBOARD:
       id = IDS_DOWNLOAD_NOTIFICATION_COPY_TO_CLIPBOARD;
+      break;
+    case DownloadCommands::ANNOTATE:
+      id = IDS_DOWNLOAD_NOTIFICATION_ANNOTATE;
       break;
     case DownloadCommands::ALWAYS_OPEN_TYPE:
     case DownloadCommands::PLATFORM_OPEN:
