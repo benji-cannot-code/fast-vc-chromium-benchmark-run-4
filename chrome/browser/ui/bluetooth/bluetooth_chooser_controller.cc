f841d3f27548d0c75d2e704ce64b90b63f2d36c6
juncai <juncai@chromium.org>
2016-08-09 08:58:58 -0700

    Reset Bluetooth EventHandler when it is invalid
    When BluetoothChooser object is destroyed, its associated Bluetooth
    EventHandler becomes invalid too and should not be used any more.
    This CL resets the Bluetooth EventHandler when it becomes invalid
    so that it can't be used any more.
    
    BUG=629298
    
    Review-Url: https://codereview.chromium.org/2203103003
    Cr-Commit-Position: refs/heads/master@{#410690}

--- chrome/browser/ui/bluetooth/bluetooth_chooser_controller.cc
+++ chrome/browser/ui/bluetooth/bluetooth_chooser_controller.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/scoped_tabbed_browser_displayer.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/generated_resources.h"
+#include "content/browser/bluetooth/bluetooth_metrics.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
@@ -66,6 +67,8 @@ base::string16 BluetoothChooserController::GetOption(size_t index) const {
 }
 
 void BluetoothChooserController::RefreshOptions() {
+  if (event_handler_.is_null())
+    return;
   ClearAllDevices();
   event_handler_.Run(content::BluetoothChooser::Event::RESCAN, std::string());
 }
@@ -75,17 +78,27 @@ base::string16 BluetoothChooserController::GetStatus() const {
 }
 
 void BluetoothChooserController::Select(size_t index) {
+  if (event_handler_.is_null()) {
+    content::RecordRequestDeviceOutcome(
+        content::UMARequestDeviceOutcome::
+            BLUETOOTH_CHOOSER_EVENT_HANDLER_INVALID);
+    return;
+  }
   DCHECK_LT(index, device_names_and_ids_.size());
   event_handler_.Run(content::BluetoothChooser::Event::SELECTED,
                      device_names_and_ids_[index].second);
 }
 
 void BluetoothChooserController::Cancel() {
+  if (event_handler_.is_null())
+    return;
   event_handler_.Run(content::BluetoothChooser::Event::CANCELLED,
                      std::string());
 }
 
 void BluetoothChooserController::Close() {
+  if (event_handler_.is_null())
+    return;
   event_handler_.Run(content::BluetoothChooser::Event::CANCELLED,
                      std::string());
 }
@@ -171,6 +184,10 @@ void BluetoothChooserController::RemoveDevice(const std::string& device_id) {
       return;
     }
   }
+}
+
+void BluetoothChooserController::ResetEventHandler() {
+  event_handler_.Reset();
 }
 
 void BluetoothChooserController::ClearAllDevices() {
