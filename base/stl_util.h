b7d280e3064a1e7075fbfdfbf6af0f459e3f2c1a
skyostil <skyostil@chromium.org>
2016-08-09 15:18:00 -0700

    base: Move all stl utilities to the base namespace
    Previously only some of the utility functions in base/stl_util.h were in
    the base namespace. This patch moves all of them into that namespace.
    
    Since there are many call sites for these functions expecting them in
    the global namespace, this patch also adds transitionary aliases which
    will be removed once the call sites have been updated.
    
    Review-Url: https://codereview.chromium.org/2221403002
    Cr-Commit-Position: refs/heads/master@{#410851}

--- base/stl_util.h
+++ base/stl_util.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 
+namespace base {
+
 // Clears internal memory of an STL object.
 // STL clear()/reserve(0) does not always free internal memory allocated
 // This function uses swap/destructor to ensure the internal memory is freed.
@@ -199,8 +201,6 @@ bool ContainsValue(const Collection& collection, const Value& value) {
       collection.end();
 }
 
-namespace base {
-
 // Returns true if the container is sorted.
 template <typename Container>
 bool STLIsSorted(const Container& cont) {
@@ -259,5 +259,21 @@ bool STLIncludes(const Arg1& a1, const Arg2& a2) {
 }
 
 }  // namespace base
+
+// TODO(skyostil): Remove these global aliases once all call sites have been
+// fixed.
+using base::ContainsKey;
+using base::ContainsValue;
+using base::STLClearObject;
+using base::STLCount;
+using base::STLDeleteContainerPairFirstPointers;
+using base::STLDeleteContainerPairPointers;
+using base::STLDeleteContainerPairSecondPointers;
+using base::STLDeleteContainerPointers;
+using base::STLDeleteElements;
+using base::STLDeleteValues;
+using base::STLElementDeleter;
+using base::STLValueDeleter;
+using base::string_as_array;
 
 #endif  // BASE_STL_UTIL_H_
