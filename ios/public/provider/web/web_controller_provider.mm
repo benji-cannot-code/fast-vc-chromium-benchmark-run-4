bc68262337c997a7a8b3b04a95314774efa69b5e
eugenebut <eugenebut@chromium.org>
2016-08-10 15:18:01 -0700

    [ios] Deprecated WCProvider::InjectScript which returns string result.
    Script execution code used to return string because this was the only
    option with UIWebView. WKWebView returns id which should be used as
    script execution result.
    
    BUG=595761
    
    Review-Url: https://codereview.chromium.org/2231073003
    Cr-Commit-Position: refs/heads/master@{#411161}

--- ios/public/provider/web/web_controller_provider.mm
+++ ios/public/provider/web/web_controller_provider.mm
@@ -38,6 +38,13 @@ void WebControllerProvider::InjectScript(const std::string& script,
     completion(nil, nil);
 }
 
+void WebControllerProvider::InjectScript(
+    const std::string& script,
+    web::JavaScriptResultBlock completion) {
+  if (completion)
+    completion(nil, nil);
+}
+
 WebControllerProviderFactory::WebControllerProviderFactory() {
 }
 
