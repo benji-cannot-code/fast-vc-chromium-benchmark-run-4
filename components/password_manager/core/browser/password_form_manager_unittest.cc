017b44b3f65e5d97e38c46673ad6ba7cfc220943
vabr <vabr@chromium.org>
2016-08-10 01:06:16 -0700

    Fix sync-credential-related metrics
    This CL makes PasswordManager_SyncCredentialUsed obsolete, beacuse it does not
    meaure what it should (see the associated bug).
    
    The CL also introduces a new user action,
    PasswordManager_SyncCredentialFilledAndLoginSuccessfull, to actually measure
    what the above action was supposed to measure.
    
    The CL also introduces another user action, PasswordManager_ProcessingUpdate,
    which keeps measuring what PasswordManager_SyncCredentialUsed measured, because
    that data was also useful.
    
    BUG=635428
    
    Review-Url: https://codereview.chromium.org/2220423002
    Cr-Commit-Position: refs/heads/master@{#410999}

--- components/password_manager/core/browser/password_form_manager_unittest.cc
+++ components/password_manager/core/browser/password_form_manager_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/user_action_tester.h"
 #include "build/build_config.h"
 #include "components/autofill/core/browser/autofill_manager.h"
 #include "components/autofill/core/browser/proto/server.pb.h"
@@ -3021,6 +3022,25 @@ TEST_F(PasswordFormManagerFillOnAccountSelectTest, ProcessFrame) {
   EXPECT_CALL(*client()->mock_driver(),
               ShowInitialPasswordAccountSuggestions(_));
   SimulateMatchingPhase(form_manager(), RESULT_SAVED_MATCH);
+}
+
+// Check that PasswordFormManager records
+// PasswordManager_LoginFollowingAutofill as part of processing a credential
+// update.
+TEST_F(PasswordFormManagerTest, ReportProcessingUpdate) {
+  SimulateMatchingPhase(form_manager(), RESULT_SAVED_MATCH);
+  PasswordForm pending = *observed_form();
+  pending.username_value = saved_match()->username_value;
+  pending.password_value = saved_match()->password_value;
+  form_manager()->ProvisionallySave(
+      pending, PasswordFormManager::IGNORE_OTHER_POSSIBLE_USERNAMES);
+
+  EXPECT_FALSE(form_manager()->IsNewLogin());
+
+  base::UserActionTester tester;
+  EXPECT_EQ(0, tester.GetActionCount("PasswordManager_LoginFollowingAutofill"));
+  form_manager()->Update(*saved_match());
+  EXPECT_EQ(1, tester.GetActionCount("PasswordManager_LoginFollowingAutofill"));
 }
 
 }  // namespace password_manager
