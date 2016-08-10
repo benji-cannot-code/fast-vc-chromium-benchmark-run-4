1734b9ece77e40c246de2c68f2a2cd066d391e6f
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-08-10 13:50:07 -0700

    [Extensions] Kill remaining uses of ExtensionFunction::render_view_host
    RenderViewHost is deprecated. Kill off the remaining extension function
    uses.
    
    BUG=498017
    
    Review-Url: https://codereview.chromium.org/2206863003
    Cr-Commit-Position: refs/heads/master@{#411138}

--- extensions/browser/guest_view/web_view/web_ui/web_ui_url_fetcher.cc
+++ extensions/browser/guest_view/web_view/web_ui/web_ui_url_fetcher.cc
@@ -13,15 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 WebUIURLFetcher::WebUIURLFetcher(content::BrowserContext* context,
                                  int render_process_id,
-                                 int render_view_id,
+                                 int render_frame_id,
                                  const GURL& url,
                                  const WebUILoadFileCallback& callback)
     : context_(context),
       render_process_id_(render_process_id),
-      render_view_id_(render_view_id),
+      render_frame_id_(render_frame_id),
       url_(url),
-      callback_(callback) {
-}
+      callback_(callback) {}
 
 WebUIURLFetcher::~WebUIURLFetcher() {
 }
@@ -34,7 +33,7 @@ void WebUIURLFetcher::Start() {
   fetcher_->SetLoadFlags(net::LOAD_DO_NOT_SAVE_COOKIES);
 
   content::AssociateURLFetcherWithRenderFrame(
-      fetcher_.get(), url_, render_process_id_, render_view_id_);
+      fetcher_.get(), url_, render_process_id_, render_frame_id_);
   fetcher_->Start();
 }
 
