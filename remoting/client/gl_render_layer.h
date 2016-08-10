53ca84fc531c7b6e83633cb59d39ae7c91ef226c
yuweih <yuweih@chromium.org>
2016-08-10 14:41:04 -0700

    [Chromoting] Unit Tests for GlRenderer
    This CL separates the OpenGL drawing logic from GlRenderer and adds unit tests
    for delegate callback and redraw scheduling logic.
    
    BUG=385924
    
    Review-Url: https://codereview.chromium.org/2196493002
    Cr-Commit-Position: refs/heads/master@{#411152}

--- remoting/client/gl_render_layer.h
+++ remoting/client/gl_render_layer.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_CLIENT_OPENGL_GL_RENDER_LAYER_H_
 #define REMOTING_CLIENT_OPENGL_GL_RENDER_LAYER_H_
 
+#include <array>
+#include <memory>
+
 #include "base/macros.h"
 #include "base/threading/thread_checker.h"
 #include "remoting/client/sys_opengl.h"
--- remoting/client/sys_opengl.h
+++ remoting/client/sys_opengl.h
@@ -10,10 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_IOS)
 #include <OpenGLES/ES3/gl.h>
-#elif defined(OS_ANDROID)
-#include <GLES3/gl3.h>
+#elif defined(OS_LINUX)
+#define GL_GLEXT_PROTOTYPES
+#include <GL/gl.h>
+#include <GL/glext.h>
+#elif defined(OS_MACOSX)
+#define GL_GLEXT_PROTOTYPES
+#include <OpenGL/gl.h>
+#include <OpenGL/glext.h>
 #else
-#error OpenGL renderer not supported on this platform.
-#endif
+#include <GLES3/gl3.h>
+#endif  // defined(OS_IOS)
 
 #endif  // REMOTING_CLIENT_SYS_OPENGL_H_
