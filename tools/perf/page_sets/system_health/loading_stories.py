86f7b44f97d24d333b0377ebf3137aa1659961e4
petrcermak <petrcermak@chromium.org>
2016-08-10 10:40:01 -0700

    [system-health] Rename browse:media:facebook to browse:media:facebook_photos
    As part of the renaming, the patch changes the name of the classes
    defining the stories ("Facebook(Mobile|Desktop)Story" to
    "FacebookPhotos(Mobile|Desktop)Story" to avoid the clash with
    browse:social:facebook (see the bug below).
    
    The patch also adds a corresponding loading story
    (load:media:facebook_photos) and removes the initial navigation to
    facebook.com/rihanna from browse:media:facebook_photos. As a result,
    there will be 4 Facebook stories on each platform:
    
      load:social:facebook
      browse:social:facebook
      load:media:facebook_photos
      browse:media:facebook_photos
    
    Finally, the patch fixes the SystemHealthStory metaclass logic.
    
    BUG=636401
    
    Review-Url: https://codereview.chromium.org/2235803002
    Cr-Commit-Position: refs/heads/master@{#411072}

--- tools/perf/page_sets/system_health/loading_stories.py
+++ tools/perf/page_sets/system_health/loading_stories.py
@@ -233,7 +233,7 @@ class Load9GagStory(_LoadingStory):
   URL = 'https://www.9gag.com/'
 
 
-class LoadFlickr(_LoadingStory):
+class LoadFlickrStory(_LoadingStory):
   NAME = 'load:media:flickr'
   URL = 'https://www.flickr.com/photos/tags/farm'
 
@@ -245,9 +245,25 @@ class LoadFlickr(_LoadingStory):
                 !== null''')
 
 
-class LoadImgur(_LoadingStory):
+class LoadImgurStory(_LoadingStory):
   NAME = 'load:media:imgur'
   URL = 'http://imgur.com/gallery/5UlBN'
+
+
+class LoadFacebookPhotosMobileStory(_LoadingStory):
+  NAME = 'load:media:facebook_photos'
+  URL = (
+      'https://m.facebook.com/rihanna/photos/a.207477806675.138795.10092511675/10153911739606676/?type=3&source=54&ref=page_internal')
+  SUPPORTED_PLATFORMS = platforms.MOBILE_ONLY
+
+
+class LoadFacebookPhotosDesktopStory(_LoadingStory):
+  NAME = 'load:media:facebook_photos'
+  URL = (
+      'https://www.facebook.com/rihanna/photos/a.207477806675.138795.10092511675/10153911739606676/?type=3&theater')
+  # Recording currently does not work. The page gets stuck in the
+  # theater viewer.
+  SUPPORTED_PLATFORMS = platforms.NO_PLATFORMS
 
 
 ################################################################################
@@ -264,7 +280,7 @@ class LoadDocsStory(_LoadingStory):
 class _LoadGmailBaseStory(_LoadingStory):
   NAME = 'load:tools:gmail'
   URL = 'https://mail.google.com/mail/'
-  SUPPORTED_PLATFORMS = platforms.MOBILE_ONLY
+  ABSTRACT_STORY = True
 
   def _Login(self, action_runner):
     google_login.LoginGoogleAccount(action_runner, 'googletest',
