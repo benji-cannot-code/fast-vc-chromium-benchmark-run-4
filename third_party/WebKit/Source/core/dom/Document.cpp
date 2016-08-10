24654675a3fcfec8587b75252e729822588768d7
yosin <yosin@chromium.org>
2016-08-09 23:11:30 -0700

    Introduce FrameSelection::nodeChildrenWillBeRemoved()
    This patch introduces |FrameSelection::nodeChildrenWillBeRemoved()| to
    process removing children once for container node rather than calling
    |FrameSelection::nodeWillBeRemoved()| for each descendant nodes in container
    to improve execution speed of callers of |ContainerNode::removeChildren()|, e.g.
    |innserHTML| setter, |innerText| setter, etc.
    
    After this patch execution speeof is O(1) instead of O(n), where n is number of
    descendant nodes in container.
    
    Updating test expectations of "svg/custom/use-clipped-hit.svg" is caused by
    difference of selection paint invalidation. From this patch, selection paint
    invalidation is done by children removal rather than caused by selected nodes.
    
    BUG=139552, 632333
    TEST=PerformanceTests/DOM/inner_html_with_selection.html
    
    Review-Url: https://codereview.chromium.org/2221783003
    Cr-Commit-Position: refs/heads/master@{#410976}

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -3796,9 +3796,9 @@ void Document::nodeChildrenWillBeRemoved(ContainerNode& container)
     }
 
     if (LocalFrame* frame = this->frame()) {
+        frame->selection().nodeChildrenWillBeRemoved(container);
         for (Node& n : NodeTraversal::childrenOf(container)) {
             frame->eventHandler().nodeWillBeRemoved(n);
-            frame->selection().nodeWillBeRemoved(n);
             frame->page()->dragCaretController().nodeWillBeRemoved(n);
         }
     }
