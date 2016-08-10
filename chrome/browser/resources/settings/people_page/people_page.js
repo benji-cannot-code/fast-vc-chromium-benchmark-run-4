dabbca0205223aa2c83c71b9078a67710b687dde
jdufault <jdufault@chromium.org>
2016-08-10 14:03:40 -0700

    Rework quick unlock settings to follow new specs.
    BUG=
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2208473007
    Cr-Commit-Position: refs/heads/master@{#411142}

--- chrome/browser/resources/settings/people_page/people_page.js
+++ chrome/browser/resources/settings/people_page/people_page.js
@@ -11,7 +11,11 @@ Polymer({
   is: 'settings-people-page',
 
   behaviors: [
+    I18nBehavior,
     WebUIListenerBehavior,
+<if expr="chromeos">
+    LockStateBehavior,
+</if>
   ],
 
   properties: {
@@ -136,6 +140,17 @@ Polymer({
 </if>
   },
 
+<if expr="chromeos">
+  /** @private */
+  getPasswordState_: function(hasPin, enableScreenLock) {
+    if (!enableScreenLock)
+      return this.i18n('lockScreenNone');
+    if (hasPin)
+      return this.i18n('lockScreenPinOrPassword');
+    return this.i18n('lockScreenPasswordOnly');
+  },
+</if>
+
   /**
    * Handler for when the profile's icon and name is updated.
    * @private
@@ -233,8 +248,8 @@ Polymer({
 
 <if expr="chromeos">
   /** @private */
-  onQuickUnlockTap_: function() {
-    settings.navigateTo(settings.Route.QUICK_UNLOCK_AUTHENTICATE);
+  onConfigureLockTap_: function() {
+    settings.navigateTo(settings.Route.LOCK_SCREEN);
   },
 
   /** @private */
--- chrome/browser/resources/settings/people_page/setup_pin_dialog.js
+++ chrome/browser/resources/settings/people_page/setup_pin_dialog.js
@@ -5,37 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * @fileoverview
- * 'settings-quick-unlock-setup-pin' is the settings page for choosing a PIN.
+ * 'settings-setup-pin-dialog' is the settings page for choosing a PIN.
  *
  * Example:
  *
- * <settings-quick-unlock-setup-pin set-modes="[[quickUnlockSetModes]]">
- * </settings-quick-unlock-setup-pin>
+ * <settings-setup-pin-dialog set-modes="[[quickUnlockSetModes]]">
+ * </settings-setup-pin-dialog>
  */
 
 (function() {
 'use strict';
-
-/**
- * Metainformation about a problem message to pass to showProblem. |class| is
- * the css class to show the problem message with. |messageId| is the i18n
- * string id to use.
- * @const
- */
-var ProblemInfo = {
-  TOO_SHORT: {
-    messageId: 'quickUnlockConfigurePinChoosePinTooShort',
-    class: 'error'
-  },
-  WEAK: {
-    messageId: 'quickUnlockConfigurePinChoosePinWeakPinWarning',
-    class: 'warning'
-  },
-  MISMATCHED: {
-    messageId: 'quickUnlockConfigurePinMismatchedPins',
-    class: 'error'
-  }
-};
 
 /**
  * A list of the top-10 most commmonly used PINs. This list is taken from
@@ -48,13 +27,9 @@ var WEAK_PINS = [
 ];
 
 Polymer({
-  is: 'settings-quick-unlock-setup-pin',
+  is: 'settings-setup-pin-dialog',
 
-  behaviors: [
-    I18nBehavior,
-    QuickUnlockPasswordDetectBehavior,
-    settings.RouteObserverBehavior
-  ],
+  behaviors: [I18nBehavior],
 
   properties: {
     /**
@@ -96,31 +71,21 @@ Polymer({
     },
   },
 
-  observers: ['onSetModesChanged_(setModes)'],
-
   /** @override */
   attached: function() {
     this.resetState_();
-
-    if (settings.getCurrentRoute() == settings.Route.QUICK_UNLOCK_SETUP_PIN)
-      this.askForPasswordIfUnset();
   },
 
-  /** @protected */
-  currentRouteChanged: function() {
-    if (settings.getCurrentRoute() == settings.Route.QUICK_UNLOCK_SETUP_PIN) {
-      this.askForPasswordIfUnset();
-    } else {
-      // If the user hits the back button, they can leave the element
-      // half-completed; therefore, reset state if the element is not active.
-      this.resetState_();
-    }
+  open: function() {
+    this.$.dialog.showModal();
+    this.$.pinKeyboard.focus();
   },
 
-  /** @private */
-  onSetModesChanged_: function() {
-    if (settings.getCurrentRoute() == settings.Route.QUICK_UNLOCK_SETUP_PIN)
-      this.askForPasswordIfUnset();
+  close: function() {
+    if (this.$.dialog.open)
+      this.$.dialog.close();
+
+    this.resetState_();
   },
 
   /**
@@ -133,6 +98,12 @@ Polymer({
     this.enableSubmit_ = false;
     this.isConfirmStep_ = false;
     this.onPinChange_();
+  },
+
+  /** @private */
+  onCancelTap_: function() {
+    this.resetState_();
+    this.$.dialog.cancel();
   },
 
   /**
@@ -183,11 +154,12 @@ Polymer({
   /**
    * Notify the user about a problem.
    * @private
-   * @param {!{messageId: string, class: string}} problemInfo
+   * @param {string} messageId
+   * @param {string} problemClass
    */
-  showProblem_: function(problemInfo) {
-    this.problemMessage_ = this.i18n(problemInfo.messageId);
-    this.problemClass_ = problemInfo.class;
+  showProblem_: function(messageId, problemClass) {
+    this.problemMessage_ = this.i18n(messageId);
+    this.problemClass_ = problemClass;
     this.updateStyles();
   },
 
@@ -204,9 +176,9 @@ Polymer({
       var isWeak = isPinLongEnough && this.isPinWeak_(this.pinKeyboardValue_);
 
       if (!isPinLongEnough && this.pinKeyboardValue_)
-        this.showProblem_(ProblemInfo.TOO_SHORT);
+        this.showProblem_('configurePinTooShort', 'error');
       else if (isWeak)
-        this.showProblem_(ProblemInfo.WEAK);
+        this.showProblem_('configurePinWeakPin', 'warning');
       else
         this.hideProblem_();
 
@@ -216,7 +188,7 @@ Polymer({
       var canSubmit = this.canSubmit_();
 
       if (!canSubmit && this.pinKeyboardValue_)
-        this.showProblem_(ProblemInfo.MISMATCHED);
+        this.showProblem_('configurePinMismatched', 'error');
       else
         this.hideProblem_();
 
@@ -246,7 +218,7 @@ Polymer({
         }
 
         this.resetState_();
-        settings.navigateTo(settings.Route.QUICK_UNLOCK_CHOOSE_METHOD);
+        this.fire('done');
       }
 
       this.setModes.call(
@@ -272,9 +244,8 @@ Polymer({
    * @return {string}
    */
   getTitleMessage_: function(isConfirmStep) {
-    if (!isConfirmStep)
-      return this.i18n('quickUnlockConfigurePinChoosePinTitle');
-    return this.i18n('quickUnlockConfigurePinConfirmPinTitle');
+    return this.i18n(isConfirmStep ? 'configurePinConfirmPinTitle' :
+                                     'configurePinChoosePinTitle');
   },
 
   /**
@@ -283,9 +254,7 @@ Polymer({
    * @return {string}
    */
   getContinueMessage_: function(isConfirmStep) {
-    if (!isConfirmStep)
-      return this.i18n('quickUnlockConfigurePinContinueButton');
-    return this.i18n('save');
+    return this.i18n(isConfirmStep ? 'confirm' : 'configurePinContinueButton');
   },
 });
 
