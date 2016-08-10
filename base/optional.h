2b10280cdf0555ba77d093a88712be869bcdb69e
mlamouri <mlamouri@chromium.org>
2016-08-10 05:24:15 -0700

    Implement Optional::has_value() and Optional::reset().
    BUG=632970
    
    Review-Url: https://codereview.chromium.org/2198673002
    Cr-Commit-Position: refs/heads/master@{#411026}

--- base/optional.h
+++ base/optional.h
@@ -164,6 +164,8 @@ class Optional {
 
   constexpr explicit operator bool() const { return !storage_.is_null_; }
 
+  constexpr bool has_value() const { return !storage_.is_null_; }
+
   // TODO(mlamouri): using 'constexpr' here breaks compiler that assume it was
   // meant to be 'constexpr const'.
   T& value() & {
@@ -230,6 +232,10 @@ class Optional {
     DCHECK(!storage_.is_null_ && !other.storage_.is_null_);
     using std::swap;
     swap(**this, *other);
+  }
+
+  void reset() {
+    FreeIfNeeded();
   }
 
   template <class... Args>
