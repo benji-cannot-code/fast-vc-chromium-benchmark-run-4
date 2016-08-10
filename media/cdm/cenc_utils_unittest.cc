c1f610a73d92dc2e98510cf5db280bf9b6562073
ddorwin <ddorwin@chromium.org>
2016-08-10 16:35:52 -0700

    Specify the correct size for PSSH boxes in tests
    Size 0 is unrealistic.
    
    See https://github.com/w3c/web-platform-tests/issues/3430.
    
    Review-Url: https://codereview.chromium.org/2228213002
    Cr-Commit-Position: refs/heads/master@{#411184}

--- media/cdm/cenc_utils_unittest.cc
+++ media/cdm/cenc_utils_unittest.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <limits>
+
 #include "base/logging.h"
 #include "base/macros.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -55,6 +57,7 @@ class CencUtilsTest : public testing::Test {
 
     box->reserve(size);
     // Add size.
+    DCHECK(size < std::numeric_limits<uint8_t>::max());
     box->push_back(0);
     box->push_back(0);
     box->push_back(0);
