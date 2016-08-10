b3fa0d747043c5a173a96d15dcf510959ff3cf9a
oka <oka@chromium.org>
2016-08-10 01:27:16 -0700

    Improved security of Quick View by rendering videos and audios inside webview.
    Added files-safe-media tag which generalizes and replaces files-safe-img, and used them in Quick View.
    This addresses security concern raised on
    https://bugs.chromium.org/p/chromium/issues/detail?id=614228#c21.
    
    BUG=614228,627698
    TEST=manually
    third_party/closure_compiler/run_compiler
    
    Review-Url: https://codereview.chromium.org/2181953003
    Cr-Commit-Position: refs/heads/master@{#411004}

--- ui/file_manager/file_manager/foreground/elements/files_quick_view.js
+++ ui/file_manager/file_manager/foreground/elements/files_quick_view.js
@@ -27,7 +27,7 @@ var FilesQuickView = Polymer({
 
   listeners: {
     'iron-overlay-closed': 'clear',
-    'files-safe-img-tap-outside': 'close',
+    'files-safe-media-tap-outside': 'close',
   },
 
   // Clears fields.
@@ -116,6 +116,17 @@ var FilesQuickView = Polymer({
   },
 
   /**
+   * @param {string} contentUrl
+   * @param {string} type
+   * @return {string}
+   *
+   * @private
+   */
+  videoUrl_: function(contentUrl, type) {
+    return this.isVideo_(type) ? contentUrl : "";
+  },
+
+  /**
    * @param {string} type
    * @return {boolean}
    *
@@ -123,6 +134,17 @@ var FilesQuickView = Polymer({
    */
   isAudio_: function(type) {
     return type === 'audio';
+  },
+
+  /**
+   * @param {string} contentUrl
+   * @param {string} type
+   * @return {string}
+   *
+   * @private
+   */
+  audioUrl_: function(contentUrl, type) {
+    return this.isAudio_(type) ? contentUrl : "";
   },
 
   /**
