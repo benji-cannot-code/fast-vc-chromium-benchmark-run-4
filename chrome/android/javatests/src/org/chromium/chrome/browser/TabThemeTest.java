618563130ee81f41060e28dec0f4f951adef3528
treib <treib@chromium.org>
2016-08-10 10:08:33 -0700

    Add fieldtrial testing config for NTPSnippets
    Also fix a test-only threading issue that was uncovered by this.
    
    BUG=636307
    
    Review-Url: https://codereview.chromium.org/2226263002
    Cr-Commit-Position: refs/heads/master@{#411063}

--- chrome/android/javatests/src/org/chromium/chrome/browser/TabThemeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/TabThemeTest.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser;
 import android.os.Environment;
 import android.test.suitebuilder.annotation.MediumTest;
 
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.tab.Tab;
@@ -18,6 +19,8 @@ import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.WebContentsObserver;
 import org.chromium.net.test.EmbeddedTestServer;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -76,7 +79,7 @@ public class TabThemeTest extends ChromeActivityTestCaseBase<ChromeTabbedActivit
     @MediumTest
     @Restriction(ChromeRestriction.RESTRICTION_TYPE_PHONE)
     public void testThemeColorIsCorrect()
-            throws InterruptedException, TimeoutException {
+            throws ExecutionException, InterruptedException, TimeoutException {
 
         EmbeddedTestServer testServer = EmbeddedTestServer.createAndStartFileServer(
                 getInstrumentation().getContext(), Environment.getExternalStorageDirectory());
@@ -96,7 +99,13 @@ public class TabThemeTest extends ChromeActivityTestCaseBase<ChromeTabbedActivit
         // Navigate to a native page from a themed page.
         loadUrl("chrome://newtab");
         // WebContents does not set theme color for native pages, so don't wait for the call.
-        assertColorsEqual(tab.getNativePage().getThemeColor(), tab.getThemeColor());
+        int nativePageThemeColor = ThreadUtils.runOnUiThreadBlocking(new Callable<Integer>() {
+            @Override
+            public Integer call() {
+                return tab.getNativePage().getThemeColor();
+            }
+        });
+        assertColorsEqual(nativePageThemeColor, tab.getThemeColor());
 
         // Navigate to a themed page from a native page.
         curCallCount = themeColorHelper.getCallCount();
