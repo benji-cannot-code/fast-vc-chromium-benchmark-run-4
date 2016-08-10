23e57273c747f9be4e2b927f94114eaac94036dc
calamity <calamity@chromium.org>
2016-08-09 20:34:03 -0700

    Fix broken installer tests.
    This CL fixes installer tests that were broken by r409132 which removed
    the app launcher from Windows but didn't update the installer tests
    that checked app launcher registry values.
    
    BUG=633490
    
    Review-Url: https://codereview.chromium.org/2224843002
    Cr-Commit-Position: refs/heads/master@{#410945}

--- chrome/installer/setup/uninstall.cc
+++ chrome/installer/setup/uninstall.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_result_codes.h"
-#include "chrome/installer/setup/app_launcher_installer.h"
 #include "chrome/installer/setup/install.h"
 #include "chrome/installer/setup/install_worker.h"
 #include "chrome/installer/setup/setup_constants.h"
