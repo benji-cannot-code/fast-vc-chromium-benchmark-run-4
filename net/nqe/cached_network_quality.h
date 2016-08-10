96427de19255890bef93a09e64e8b3f08170e7d5
tbansal <tbansal@chromium.org>
2016-08-09 22:40:23 -0700

    NQE: Add ECT as a field to cached network quality.
    EffectiveConnectionType (ECT) needs to be persistently stored
    on disk by the network quality store. This CL adds ECT to the
    cached network quality struct so that it is available to the
    network quality store. This would be used by the network
    quality store in the next few CLs where it will be written to
    the disk.
    
    BUG=490870
    
    Review-Url: https://codereview.chromium.org/2201223003
    Cr-Commit-Position: refs/heads/master@{#410967}

--- net/nqe/cached_network_quality.h
+++ net/nqe/cached_network_quality.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_NQE_CACHED_NETWORK_QUALITY_H_
 #define NET_NQE_CACHED_NETWORK_QUALITY_H_
 
-#include "base/macros.h"
 #include "base/time/time.h"
 #include "net/base/net_export.h"
+#include "net/nqe/effective_connection_type.h"
 #include "net/nqe/network_quality.h"
 
 namespace net {
@@ -24,7 +24,8 @@ class NET_EXPORT_PRIVATE CachedNetworkQuality {
 
   // |last_update_time| is the time when the |network_quality| was computed.
   CachedNetworkQuality(base::TimeTicks last_update_time,
-                       const NetworkQuality& network_quality);
+                       const NetworkQuality& network_quality,
+                       EffectiveConnectionType effective_connection_type);
   CachedNetworkQuality(const CachedNetworkQuality& other);
   ~CachedNetworkQuality();
 
@@ -41,12 +42,19 @@ class NET_EXPORT_PRIVATE CachedNetworkQuality {
 
   const NetworkQuality& network_quality() { return network_quality_; }
 
+  EffectiveConnectionType effective_connection_type() const {
+    return effective_connection_type_;
+  }
+
  private:
   // Time when this cache entry was last updated.
   base::TimeTicks last_update_time_;
 
   // Quality of this cached network.
   NetworkQuality network_quality_;
+
+  // Effective connection type of the cached network.
+  EffectiveConnectionType effective_connection_type_;
 };
 
 }  // namespace internal
