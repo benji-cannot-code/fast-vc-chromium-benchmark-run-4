abb2078cb5c32e8a691ae2af8fb77bd8682fed6a
eostroukhov <eostroukhov@chromium.org>
2016-08-11 09:00:41 -0700

    [DevTools] Recognize node remote target type
    I redid the patch to rely on target type attribute (Node.js specifies "node")
    
    BUG=636539
    R=dgozman@chromium.org, pfeldman@chromium.org
    
    Review-Url: https://codereview.chromium.org/2230993004
    Cr-Commit-Position: refs/heads/master@{#411341}

--- chrome/browser/devtools/devtools_target_impl.cc
+++ chrome/browser/devtools/devtools_target_impl.cc
@@ -35,6 +35,7 @@ const char DevToolsTargetImpl::kTargetTypePage[] = "page";
 const char DevToolsTargetImpl::kTargetTypeWorker[] = "worker";
 const char DevToolsTargetImpl::kTargetTypeWebView[] = "webview";
 const char DevToolsTargetImpl::kTargetTypeIFrame[] = "iframe";
+const char DevToolsTargetImpl::kTargetTypeNode[] = "node";
 const char DevToolsTargetImpl::kTargetTypeOther[] = "other";
 const char DevToolsTargetImpl::kTargetTypeServiceWorker[] = "service_worker";
 
