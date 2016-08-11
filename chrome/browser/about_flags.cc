74c093927a2412b2a03eabf2ebe63f8723708c63
robliao <robliao@chromium.org>
2016-08-10 17:24:14 -0700

    Add Browser Task Scheduler Flags
    Adds the browser scheduler command line flags as well as exposing it
    via chrome://flags.
    
    Enabling the feature will place the browser in a forced variations
    group, allowing it to use task scheduler parameters from there. If the
    variations group is unavailable, then enabling this will still not
    enable the browser task scheduler
    
    BUG=553459,633389
    
    Review-Url: https://codereview.chromium.org/2227343002
    Cr-Commit-Position: refs/heads/master@{#411199}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/task_scheduler/switches.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "cc/base/switches.h"
@@ -2045,6 +2046,10 @@ const FeatureEntry kFeatureEntries[] = {
      IDS_FLAGS_QUICK_UNLOCK_PIN_DESCRIPTION, kOsCrOS,
      FEATURE_VALUE_TYPE(features::kQuickUnlockPin)},
 #endif  // defined(OS_CHROMEOS)
+    {"browser-task-scheduler", IDS_FLAGS_BROWSER_TASK_SCHEDULER_NAME,
+     IDS_FLAGS_BROWSER_TASK_SCHEDULER_DESCRIPTION, kOsAll,
+     ENABLE_DISABLE_VALUE_TYPE(switches::kEnableBrowserTaskScheduler,
+                               switches::kDisableBrowserTaskScheduler)},
     // NOTE: Adding new command-line switches requires adding corresponding
     // entries to enum "LoginCustomFlags" in histograms.xml. See note in
     // histograms.xml and don't forget to run AboutFlagsHistogramTest unit test.
--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/sys_info.h"
 #include "base/task_scheduler/scheduler_worker_pool_params.h"
+#include "base/task_scheduler/switches.h"
 #include "base/task_scheduler/task_scheduler.h"
 #include "base/task_scheduler/task_traits.h"
 #include "base/threading/platform_thread.h"
@@ -396,8 +397,15 @@ size_t WorkerPoolIndexForTraits(const base::TaskTraits& traits) {
 void MaybeInitializeTaskScheduler() {
   static constexpr char kFieldTrialName[] = "BrowserScheduler";
   std::map<std::string, std::string> variation_params;
-  if (!variations::GetVariationParams(kFieldTrialName, &variation_params))
+  if (!variations::GetVariationParams(kFieldTrialName, &variation_params)) {
+    DCHECK(!base::CommandLine::ForCurrentProcess()->HasSwitch(
+        switches::kEnableBrowserTaskScheduler))
+        << "The Browser Task Scheduler remains disabled with "
+        << switches::kEnableBrowserTaskScheduler
+        << " because there is no available variation param for this build or "
+           " the task scheduler is disabled in chrome://flags.";
     return;
+  }
 
   using ThreadPriority = base::ThreadPriority;
   using IORestriction = base::SchedulerWorkerPoolParams::IORestriction;
