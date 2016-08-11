8d7006a9037fdfcf420458b5b893d68fe28cd4e3
yabinh <yabinh@chromium.org>
2016-08-10 20:28:07 -0700

    Remove unnecessary page loading in ContentShellActivity
    In ContentShellActivity#onCreate, we load the page twice: one is the
    default page and the other is the target page.  The second loading can
    be avoided if we load the target page in the first loading. Thus, we can
    be sure that the target page will be ready once the page stops loading.
    
    assertWaitForPageScaleFactorMatch() used to be a workaround for the
    flaky bug crbug.com/179511 . The tests are not flaky anymore after
    applying this patch. Thus, we can remove it.
    
    BUG=580419,179511
    
    Committed: https://crrev.com/797581eba513a69c80fbcbff43817b6fc6c6a9c0
    Review-Url: https://codereview.chromium.org/2199223002
    Cr-Original-Commit-Position: refs/heads/master@{#410519}
    Cr-Commit-Position: refs/heads/master@{#411250}

--- content/shell/android/shell_apk/src/org/chromium/content_shell_apk/ContentShellActivity.java
+++ content/shell/android/shell_apk/src/org/chromium/content_shell_apk/ContentShellActivity.java
@@ -43,6 +43,7 @@ public class ContentShellActivity extends Activity {
     private ShellManager mShellManager;
     private ActivityWindowAndroid mWindowAndroid;
     private Intent mLastSentIntent;
+    private String mStartupUrl;
 
     @Override
     @SuppressFBWarnings("DM_EXIT")
@@ -82,9 +83,9 @@ public class ContentShellActivity extends Activity {
         mWindowAndroid.setAnimationPlaceholderView(
                 mShellManager.getContentViewRenderView().getSurfaceView());
 
-        String startupUrl = getUrlFromIntent(getIntent());
-        if (!TextUtils.isEmpty(startupUrl)) {
-            mShellManager.setStartupUrl(Shell.sanitizeUrl(startupUrl));
+        mStartupUrl = getUrlFromIntent(getIntent());
+        if (!TextUtils.isEmpty(mStartupUrl)) {
+            mShellManager.setStartupUrl(Shell.sanitizeUrl(mStartupUrl));
         }
 
         if (CommandLine.getInstance().hasSwitch(ContentSwitches.RUN_LAYOUT_TEST)) {
@@ -119,7 +120,13 @@ public class ContentShellActivity extends Activity {
     }
 
     private void finishInitialization(Bundle savedInstanceState) {
-        String shellUrl = ShellManager.DEFAULT_SHELL_URL;
+        String shellUrl;
+        if (!TextUtils.isEmpty(mStartupUrl)) {
+            shellUrl = mStartupUrl;
+        } else {
+            shellUrl = ShellManager.DEFAULT_SHELL_URL;
+        }
+
         if (savedInstanceState != null
                 && savedInstanceState.containsKey(ACTIVE_SHELL_URL_KEY)) {
             shellUrl = savedInstanceState.getString(ACTIVE_SHELL_URL_KEY);
