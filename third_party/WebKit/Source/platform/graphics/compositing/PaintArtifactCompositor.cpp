d20c3cd43b9c9c730c4316b9feda921fe5f39392
pdr <pdr@chromium.org>
2016-08-10 20:20:06 -0700

    [spv2] Do not manage effect node render surfaces from blink
    Render surface determination will happen on the compositor thread so we
    will not need to make that decision in blink/PaintArtifactCompositor.
    This patch switches PaintArtifactCompositor to not set the render
    surface bit on effect nodes.
    
    With this patch, test failures with spv2 and layer lists [1] are reduced
    from 1103 to 16. No new tests are included in this patch but this brings
    us very close to being able to turn on --enable-layer-lists by default
    for spv2.
    
    [1] --additional-drt-flag=--enable-slimming-paint-v2 --additional-drt-flag=--enable-layer-lists
    
    BUG=635691
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2230723004
    Cr-Commit-Position: refs/heads/master@{#411248}

--- third_party/WebKit/Source/platform/graphics/compositing/PaintArtifactCompositor.cpp
+++ third_party/WebKit/Source/platform/graphics/compositing/PaintArtifactCompositor.cpp
@@ -543,7 +543,6 @@ void PropertyTreeManager::buildEffectNodesRecursively(const EffectPaintPropertyN
     cc::EffectNode& effectNode = *effectTree().Node(effectTree().Insert(cc::EffectNode(), compositorIdForCurrentEffectNode()));
     effectNode.owner_id = dummyLayer->id();
     effectNode.clip_id = dummyClip.id;
-    effectNode.has_render_surface = true;
     effectNode.opacity = nextEffect->opacity();
     m_effectStack.append(BlinkEffectAndCcIdPair{nextEffect, effectNode.id});
 
