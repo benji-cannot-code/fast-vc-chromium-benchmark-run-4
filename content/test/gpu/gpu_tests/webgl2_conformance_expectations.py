994316532066d1b25b6ae5ef9ee213d042f9a5e9
kbr <kbr@chromium.org>
2016-08-11 11:16:10 -0700

    Suppress three WebGL 2.0 conformance failures on Mac/NVIDIA with 10.11.
    BUG=483282, 635212
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    TBR=zmo@chromium.org
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2241523002
    Cr-Commit-Position: refs/heads/master@{#411380}

--- content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
@@ -240,6 +240,8 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
         ['mac', ('nvidia', 0xfe9)], bug=483282)
     self.Fail('conformance/programs/gl-bind-attrib-location-test.html',
         ['mac', ('nvidia', 0xfe9)], bug=483282)
+    self.Fail('conformance2/glsl3/loops-with-side-effects.html',
+        ['mac', ('nvidia', 0xfe9)], bug=483282)
     self.Fail('conformance2/textures/misc/tex-input-validation.html',
         ['mac', ('nvidia', 0xfe9), 'no_angle'], bug=483282)
     self.Fail('conformance2/textures/misc/tex-mipmap-levels.html',
@@ -252,7 +254,10 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
         ['mac', ('nvidia', 0xfe9)], bug=483282)
     self.Fail('deqp/functional/gles3/negativevertexarrayapi.html',
         ['mac', ('nvidia', 0xfe9)], bug=483282)
-
+    self.Fail('deqp/functional/gles3/shaderindexing/varying.html',
+        ['mac', ('nvidia', 0xfe9)], bug=483282)
+    self.Fail('deqp/functional/gles3/shadertexturefunction/texturesize.html',
+        ['mac', ('nvidia', 0xfe9)], bug=483282)
     self.Fail('deqp/functional/gles3/texturespecification/' +
         'basic_copyteximage2d.html',
         ['mac', ('nvidia', 0xfe9)], bug=620067)
