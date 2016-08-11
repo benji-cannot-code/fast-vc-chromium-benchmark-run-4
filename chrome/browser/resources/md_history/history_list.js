eba61b40d249a2c2073b81102a7c3d4382777fbc
calamity <calamity@chromium.org>
2016-08-10 20:36:27 -0700

    [MD History] Factor out a common HistoryListBehavior.
    This CL extracts a HistoryListBehavior that is used by the history-list
    and the history-grouped-list. The selection system has been refactored
    to accommodate different list structures.
    
    Using this behavior in the grouped history list means that it now
    implements selection and deletion.
    
    BUG=589357,589363
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2207323002
    Cr-Commit-Position: refs/heads/master@{#411253}

--- chrome/browser/resources/md_history/history_list.js
+++ chrome/browser/resources/md_history/history_list.js
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'history-list',
 
+  behaviors: [HistoryListBehavior],
+
   properties: {
     // The search term for the current query. Set when the query returns.
     searchedTerm: {
@@ -94,75 +96,6 @@ Polymer({
   },
 
   /**
-   * Cycle through each entry in historyData_ and set all items to be
-   * unselected.
-   * @param {number} overallItemCount The number of checkboxes selected.
-   */
-  unselectAllItems: function(overallItemCount) {
-    if (this.historyData_ === undefined)
-      return;
-
-    for (var i = 0; i < this.historyData_.length; i++) {
-      if (this.historyData_[i].selected) {
-        this.set('historyData_.' + i + '.selected', false);
-        overallItemCount--;
-        if (overallItemCount == 0)
-          break;
-      }
-    }
-  },
-
-  /**
-   * Remove the given |items| from the list. Expected to be called after the
-   * items are removed from the backend.
-   * @param {!Array<!HistoryEntry>} removalList
-   * @private
-   */
-  removeDeletedHistory_: function(removalList) {
-    // This set is only for speed. Note that set inclusion for objects is by
-    // reference, so this relies on the HistoryEntry objects never being copied.
-    var deletedItems = new Set(removalList);
-    var splices = [];
-
-    for (var i = this.historyData_.length - 1; i >= 0; i--) {
-      var item = this.historyData_[i];
-      if (deletedItems.has(item)) {
-        // Removes the selected item from historyData_. Use unshift so
-        // |splices| ends up in index order.
-        splices.unshift({
-          index: i,
-          removed: [item],
-          addedCount: 0,
-          object: this.historyData_,
-          type: 'splice'
-        });
-        this.historyData_.splice(i, 1);
-      }
-    }
-    // notifySplices gives better performance than individually splicing as it
-    // batches all of the updates together.
-    this.notifySplices('historyData_', splices);
-  },
-
-  /**
-   * Performs a request to the backend to delete all selected items. If
-   * successful, removes them from the view. Does not prompt the user before
-   * deleting -- see <history-list-container> for a version of this method which
-   * does prompt.
-   */
-  deleteSelected: function() {
-    var toBeRemoved = this.historyData_.filter(function(item) {
-      return item.selected;
-    });
-    md_history.BrowserService.getInstance()
-        .deleteItems(toBeRemoved)
-        .then(function(items) {
-          this.removeDeletedHistory_(items);
-          this.fire('unselect-all');
-        }.bind(this));
-  },
-
-  /**
    * Called when the page is scrolled to near the bottom of the list.
    * @private
    */
@@ -185,17 +118,6 @@ Polymer({
   needsTimeGap_: function(item, index, length) {
     return md_history.HistoryItem.needsTimeGap(
         this.historyData_, index, this.searchedTerm);
-  },
-
-  hasResults: function(historyDataLength) {
-    return historyDataLength > 0;
-  },
-
-  noResultsMessage_: function(searchedTerm, isLoading) {
-    if (isLoading)
-      return '';
-    var messageId = searchedTerm !== '' ? 'noSearchResults' : 'noResults';
-    return loadTimeData.getString(messageId);
   },
 
   /**
@@ -244,5 +166,14 @@ Polymer({
    */
   notifyListScroll_: function() {
     this.fire('history-list-scrolled');
+  },
+
+  /**
+   * @param {number} index
+   * @return {string}
+   * @private
+   */
+  pathForItem_: function(index) {
+    return 'historyData_.' + index;
   },
 });
