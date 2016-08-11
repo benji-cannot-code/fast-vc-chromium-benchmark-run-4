21329e1558db87b1743d9ee882720cc853607524
yoshiki <yoshiki@chromium.org>
2016-08-11 13:59:36 -0700

    Show small notification icons in notification tray (reland)
    This patch is a reland of crrev.com/411019 with fixing the memory leak by moving the place of AddChild(count_label_view).
    
    Original Description:
    
    This patch shows small notification icons (up to 3) of the
    current notifications in the notification tray.
    
    This also makes the counter show the count of all the
    notification excluding the visible small icons (previously
    it was the unread count).
    
    See the issue for screenshots.
    
    BUG=633399
    TEST=manual test (with and without MD mode)
    R=xiyuan@chromuim.org
    TBR=oshima@chromium.org
    
    Review-Url: https://codereview.chromium.org/2243563002
    Cr-Commit-Position: refs/heads/master@{#411418}

--- ash/common/system/web_notification/web_notification_tray.h
+++ ash/common/system/web_notification/web_notification_tray.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "ui/base/models/simple_menu_model.h"
+#include "ui/gfx/animation/animation_container.h"
 #include "ui/message_center/message_center_tray.h"
 #include "ui/message_center/message_center_tray_delegate.h"
 #include "ui/views/bubble/tray_bubble_view.h"
@@ -42,7 +43,9 @@ namespace ash {
 class AshPopupAlignmentDelegate;
 class SystemTray;
 class WebNotificationBubbleWrapper;
-class WebNotificationIcon;
+class WebNotificationButton;
+class WebNotificationImage;
+class WebNotificationLabel;
 class WmWindow;
 
 class ASH_EXPORT WebNotificationTray
@@ -56,6 +59,8 @@ class ASH_EXPORT WebNotificationTray
                       WmWindow* status_area_window,
                       SystemTray* system_tray);
   ~WebNotificationTray() override;
+
+  static void DisableAnimationsForTest(bool disable);
 
   // Sets the height of the system tray bubble (or legacy notification bubble)
   // from the edge of the work area so that the web notification popups don't
@@ -114,7 +119,7 @@ class ASH_EXPORT WebNotificationTray
   bool IsContextMenuEnabled() const override;
   message_center::MessageCenterTray* GetMessageCenterTray() override;
 
-  // Overridden from SimpleMenuModel::Delegate.
+  // Overridden from ui::SimpleMenuModel::Delegate.
   bool IsCommandIdChecked(int command_id) const override;
   bool IsCommandIdEnabled(int command_id) const override;
   void ExecuteCommand(int command_id, int event_flags) override;
@@ -170,7 +175,13 @@ class ASH_EXPORT WebNotificationTray
   std::unique_ptr<message_center::MessageCenterTray> message_center_tray_;
   std::unique_ptr<WebNotificationBubbleWrapper> message_center_bubble_;
   std::unique_ptr<message_center::MessagePopupCollection> popup_collection_;
-  WebNotificationIcon* icon_;
+  std::unique_ptr<WebNotificationImage> bell_icon_;
+  std::unique_ptr<WebNotificationLabel> counter_;
+
+  scoped_refptr<gfx::AnimationContainer> animation_container_ =
+      new gfx::AnimationContainer();
+
+  std::unordered_map<std::string, WebNotificationImage*> visible_small_icons_;
 
   bool show_message_center_on_unlock_;
 
