e9a88884ff9b2a6b5c2cc2d580284f132516c42e
reillyg <reillyg@chromium.org>
2016-08-10 20:58:12 -0700

    Mojo C++ bindings: make permissions service use STL string/vector types
    BUG=624136
    
    Review-Url: https://codereview.chromium.org/2228663002
    Cr-Commit-Position: refs/heads/master@{#411255}

--- third_party/WebKit/Source/modules/permissions/Permissions.cpp
+++ third_party/WebKit/Source/modules/permissions/Permissions.cpp
@@ -214,9 +214,8 @@ ScriptPromise Permissions::requestAll(ScriptState* scriptState, const Vector<Dic
     ScriptPromiseResolver* resolver = ScriptPromiseResolver::create(scriptState);
     ScriptPromise promise = resolver->promise();
 
-    Vector<PermissionName> internalPermissionsCopy(internalPermissions);
-    service->RequestPermissions(std::move(internalPermissions), scriptState->getExecutionContext()->getSecurityOrigin()->toString(), UserGestureIndicator::processingUserGesture(),
-        convertToBaseCallback(WTF::bind(&Permissions::batchTaskComplete, wrapPersistent(this), wrapPersistent(resolver), std::move(internalPermissionsCopy), std::move(callerIndexToInternalIndex))));
+    service->RequestPermissions(internalPermissions, scriptState->getExecutionContext()->getSecurityOrigin()->toString(), UserGestureIndicator::processingUserGesture(),
+        convertToBaseCallback(WTF::bind(&Permissions::batchTaskComplete, wrapPersistent(this), wrapPersistent(resolver), internalPermissions, callerIndexToInternalIndex)));
     return promise;
 }
 
@@ -239,7 +238,7 @@ void Permissions::taskComplete(ScriptPromiseResolver* resolver, PermissionName n
     resolver->resolve(PermissionStatus::take(resolver, result, name));
 }
 
-void Permissions::batchTaskComplete(ScriptPromiseResolver* resolver, Vector<PermissionName> names, Vector<int> callerIndexToInternalIndex, mojo::WTFArray<mojom::blink::PermissionStatus> results)
+void Permissions::batchTaskComplete(ScriptPromiseResolver* resolver, Vector<PermissionName> names, Vector<int> callerIndexToInternalIndex, const Vector<mojom::blink::PermissionStatus>& results)
 {
     if (!resolver->getExecutionContext() || resolver->getExecutionContext()->activeDOMObjectsAreStopped())
         return;
