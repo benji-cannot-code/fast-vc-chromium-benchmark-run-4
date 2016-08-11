e58f697185670397ab07b413a49bd69a4044f2f7
xidachen <xidachen@chromium.org>
2016-08-11 10:37:54 -0700

    Revert of Optimize posting a WTF::Closure on WebTaskRunner (patchset #2 id:20001 of https://codereview.chromium.org/2232253002/ )
    Reason for revert:
    Causing test failure here:
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win%20x64%20Builder%20%28dbg%29/builds/91156
    
    Original issue's description:
    > Optimize posting a WTF::Closure on WebTaskRunner
    >
    > Previously the WTF:Closure (which contains a base::Closure) was wrapped
    > in a WebTaskRunner::Task which was then wrapped in a base::Closure!
    >
    > Thanks to moving the scheduler into blink we no longer have to live with
    > this absurdity :)
    >
    > Also rename WebTaskRunner::taskRunner() to
    > WebTaskRunner::toSingleThreadTaskRunner().
    >
    > BUG=
    >
    > Committed: https://crrev.com/1fa70eceee90f421cdeaa27da46ef386cb9a04ca
    > Cr-Commit-Position: refs/heads/master@{#411353}
    
    TBR=haraken@chromium.org,skyostil@chromium.org,sievers@chromium.org,kinuko@chromium.org,alexclarke@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=
    
    Review-Url: https://codereview.chromium.org/2237103002
    Cr-Commit-Position: refs/heads/master@{#411370}

--- third_party/WebKit/Source/platform/testing/TestingPlatformSupport.h
+++ third_party/WebKit/Source/platform/testing/TestingPlatformSupport.h
@@ -59,9 +59,6 @@ public:
     void runSingleTask();
     void runAllTasks();
 
-    void addTaskObserver(WebThread::TaskObserver*);
-    void removeTaskObserver(WebThread::TaskObserver*);
-
     // WebScheduler implementation:
     WebTaskRunner* loadingTaskRunner() override;
     WebTaskRunner* timerTaskRunner() override;
@@ -81,7 +78,6 @@ public:
 private:
     WTF::Deque<std::unique_ptr<WebTaskRunner::Task>> m_tasks;
     std::unique_ptr<TestingPlatformMockWebTaskRunner> m_mockWebTaskRunner;
-    WebThread::TaskObserver* m_taskObserver;
 };
 
 class TestingPlatformSupport : public Platform {
