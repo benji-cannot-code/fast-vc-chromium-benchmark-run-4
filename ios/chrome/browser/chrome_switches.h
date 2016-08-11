14abec024e4a644985c754531ad5a4e6f6fb5454
jdonnelly <jdonnelly@chromium.org>
2016-08-10 19:29:12 -0700

    Add data types and experimental flag for the Payment Request API.
    BUG=602666
    
    Review-Url: https://codereview.chromium.org/1876463002
    Cr-Commit-Position: refs/heads/master@{#411229}

--- ios/chrome/browser/chrome_switches.h
+++ ios/chrome/browser/chrome_switches.h
@@ -19,6 +19,7 @@ extern const char kDisableIOSPasswordSuggestions[];
 extern const char kDisableLRUSnapshotCache[];
 extern const char kDisableNTPFavicons[];
 extern const char kDisableOfflineAutoReload[];
+extern const char kDisablePaymentRequest[];
 extern const char kDisableTabSwitcher[];
 extern const char kDisableIOSPhysicalWeb[];
 
@@ -32,6 +33,7 @@ extern const char kEnableIOSPasswordGeneration[];
 extern const char kEnableLRUSnapshotCache[];
 extern const char kEnableNTPFavicons[];
 extern const char kEnableOfflineAutoReload[];
+extern const char kEnablePaymentRequest[];
 extern const char kEnableQRScanner[];
 extern const char kEnableReaderModeToolbarIcon[];
 extern const char kEnableTabSwitcher[];
