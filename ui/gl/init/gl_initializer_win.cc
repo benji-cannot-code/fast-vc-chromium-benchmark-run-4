10fb392c5381ed350a8ee9d92a52d2ee0a632eb0
capn <capn@chromium.org>
2016-08-11 11:06:38 -0700

    Register SwiftShader key without using chrome-internal header.
    //third_party/swiftshader/include/software_renderer.h from chrome-internal is
    used to register a secret license key to disable SwiftShader's legacy logo
    watermark in official Chrome builds. We're going to populate that directory
    with the full SwiftShader source code instead, since it is now open-source, but
    it adds Khronos headers which ozone is not compatible with (Issue 635702).
    Hence we need to avoid having anything depend on the old include directory
    content. The open-sourcing also means the the old license key does not need to
    be kept secret, so we can use it directly in chromium during the transition to
    logo-free builds.
    
    BUG=630728
    
    Change-Id: Ica40e98a276fc5e1a48178cecb0d908d14482622
    Review-Url: https://codereview.chromium.org/2231913002
    Cr-Commit-Position: refs/heads/master@{#411372}

--- ui/gl/init/gl_initializer_win.cc
+++ ui/gl/init/gl_initializer_win.cc
@@ -31,10 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_wgl_api_implementation.h"
 #include "ui/gl/vsync_provider_win.h"
 
-#if defined(ENABLE_SWIFTSHADER)
-#include "software_renderer.h"
-#endif
-
 namespace gl {
 namespace init {
 
@@ -146,7 +142,13 @@ bool InitializeStaticEGLInternal() {
 
 #if defined(ENABLE_SWIFTSHADER)
   if (using_swift_shader) {
-    SetupSoftwareRenderer(gles_library);
+    // Register key so that SwiftShader doesn't display watermark logo.
+    typedef void (__stdcall *RegisterFunc)(const char* key);
+    RegisterFunc reg = reinterpret_cast<RegisterFunc>(
+      base::GetFunctionPointerFromNativeLibrary(gles_library, "Register"));
+    if (reg) {
+      reg("SS3GCKK6B448CF63");
+    }
   }
 #endif
 
