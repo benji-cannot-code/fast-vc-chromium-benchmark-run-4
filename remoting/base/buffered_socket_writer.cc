5cea76f667bf7db771d0b9fcf035af81d8bc20fb
skyostil <skyostil@chromium.org>
2016-08-12 05:07:54 -0700

    remoting: Use stl utilities from the base namespace
    The utilities in base/stl_util.h have been moved from the global
    into the base namespace. This patch updates the call sites accordingly.
    
    No functional changes.
    
    BUG=636301
    
    Review-Url: https://codereview.chromium.org/2230193002
    Cr-Commit-Position: refs/heads/master@{#411604}

--- remoting/base/buffered_socket_writer.cc
+++ remoting/base/buffered_socket_writer.cc
@@ -48,7 +48,7 @@ std::unique_ptr<BufferedSocketWriter> BufferedSocketWriter::CreateForSocket(
 BufferedSocketWriter::BufferedSocketWriter() : weak_factory_(this) {}
 
 BufferedSocketWriter::~BufferedSocketWriter() {
-  STLDeleteElements(&queue_);
+  base::STLDeleteElements(&queue_);
 }
 
 void BufferedSocketWriter::Start(
