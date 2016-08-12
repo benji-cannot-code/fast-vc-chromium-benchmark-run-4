2d7ae96ab0b2fd21b89bc5511364cfbbe41850e6
jonross <jonross@chromium.org>
2016-08-12 07:09:32 -0700

    Turn Native Window Context Menus Async
    Update ChromeNativeAppWindowViewsAuraAsh to use ASYNC menus.
    
    TEST=manual testing of Chrome OS Files app context menu
    BUG=557136
    
    Review-Url: https://codereview.chromium.org/2195033002
    Cr-Commit-Position: refs/heads/master@{#411640}

--- chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.cc
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/hit_test.h"
 #include "ui/base/models/simple_menu_model.h"
 #include "ui/gfx/skia_util.h"
+#include "ui/views/controls/menu/menu_model_adapter.h"
 #include "ui/views/controls/menu/menu_runner.h"
 #include "ui/views/widget/widget.h"
 
@@ -237,9 +238,8 @@ void ChromeNativeAppWindowViewsAuraAsh::ShowContextMenuForView(
     views::View* source,
     const gfx::Point& p,
     ui::MenuSourceType source_type) {
-  std::unique_ptr<ui::MenuModel> model =
-      CreateMultiUserContextMenu(app_window()->GetNativeWindow());
-  if (!model.get())
+  menu_model_ = CreateMultiUserContextMenu(app_window()->GetNativeWindow());
+  if (!menu_model_.get())
     return;
 
   // Only show context menu if point is in caption.
@@ -249,15 +249,19 @@ void ChromeNativeAppWindowViewsAuraAsh::ShowContextMenuForView(
   int hit_test =
       widget()->non_client_view()->NonClientHitTest(point_in_view_coords);
   if (hit_test == HTCAPTION) {
+    menu_model_adapter_.reset(new views::MenuModelAdapter(
+        menu_model_.get(),
+        base::Bind(&ChromeNativeAppWindowViewsAuraAsh::OnMenuClosed,
+                   base::Unretained(this))));
     menu_runner_.reset(new views::MenuRunner(
-        model.get(),
-        views::MenuRunner::HAS_MNEMONICS | views::MenuRunner::CONTEXT_MENU));
-    if (menu_runner_->RunMenuAt(source->GetWidget(), NULL,
-                                gfx::Rect(p, gfx::Size(0, 0)),
-                                views::MENU_ANCHOR_TOPLEFT, source_type) ==
-        views::MenuRunner::MENU_DELETED) {
-      return;
-    }
+        menu_model_adapter_->CreateMenu(), views::MenuRunner::HAS_MNEMONICS |
+                                               views::MenuRunner::CONTEXT_MENU |
+                                               views::MenuRunner::ASYNC));
+    menu_runner_->RunMenuAt(source->GetWidget(), NULL,
+                            gfx::Rect(p, gfx::Size(0, 0)),
+                            views::MENU_ANCHOR_TOPLEFT, source_type);
+  } else {
+    menu_model_.reset();
   }
 }
 
@@ -344,4 +348,10 @@ void ChromeNativeAppWindowViewsAuraAsh::UpdateDraggableRegions(
     GetMusWindow(widget()->GetNativeWindow())
         ->SetClientArea(insets, std::move(additional_client_regions));
   }
+}
+
+void ChromeNativeAppWindowViewsAuraAsh::OnMenuClosed() {
+  menu_runner_.reset();
+  menu_model_adapter_.reset();
+  menu_model_.reset();
 }
