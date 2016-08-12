9c3e9a01a51f4fba0efef932e41172402ac2d8b6
danakj <danakj@chromium.org>
2016-08-11 19:59:22 -0700

    blimp: Make Blimp client use a Display for compositing.
    Gives Blimp client a delegatig output surface that owns a Display,
    based on the TestDelegatingOutputSurface.
    
    BUG=606056
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2242653002
    Cr-Commit-Position: refs/heads/master@{#411532}

--- cc/test/test_delegating_output_surface.cc
+++ cc/test/test_delegating_output_surface.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/output/begin_frame_args.h"
 #include "cc/output/copy_output_request.h"
 #include "cc/output/texture_mailbox_deleter.h"
-#include "cc/test/begin_frame_args_test.h"
 
 static constexpr uint32_t kCompositorClientId = 1;
 
