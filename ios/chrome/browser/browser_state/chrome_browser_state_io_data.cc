cefa6b3418fcd75f7c4fcf6265c24849fa861348
skyostil <skyostil@chromium.org>
2016-08-12 05:38:31 -0700

    ios: Use stl utilities from the base namespace
    The utilities in base/stl_util.h have been moved from the global
    into the base namespace. This patch updates the call sites accordingly.
    
    No functional changes.
    
    BUG=636301
    TBR=noyau@chromium.org
    
    Review-Url: https://codereview.chromium.org/2230123003
    Cr-Commit-Position: refs/heads/master@{#411619}

--- ios/chrome/browser/browser_state/chrome_browser_state_io_data.cc
+++ ios/chrome/browser/browser_state/chrome_browser_state_io_data.cc
@@ -269,7 +269,7 @@ net::URLRequestContext* ChromeBrowserStateIOData::GetIsolatedAppRequestContext(
     const base::FilePath& partition_path) const {
   DCHECK(initialized_);
   AppRequestContext* context = nullptr;
-  if (ContainsKey(app_request_context_map_, partition_path)) {
+  if (base::ContainsKey(app_request_context_map_, partition_path)) {
     context = app_request_context_map_[partition_path];
   } else {
     context = AcquireIsolatedAppRequestContext(main_context);
@@ -282,7 +282,7 @@ net::URLRequestContext* ChromeBrowserStateIOData::GetIsolatedAppRequestContext(
 void ChromeBrowserStateIOData::SetCookieStoreForPartitionPath(
     std::unique_ptr<net::CookieStore> cookie_store,
     const base::FilePath& partition_path) {
-  DCHECK(ContainsKey(app_request_context_map_, partition_path));
+  DCHECK(base::ContainsKey(app_request_context_map_, partition_path));
   app_request_context_map_[partition_path]->SetCookieStore(
       std::move(cookie_store));
 }
