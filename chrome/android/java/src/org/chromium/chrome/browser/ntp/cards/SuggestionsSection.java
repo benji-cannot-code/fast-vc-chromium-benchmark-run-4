217e9414389639025bfa4153444316c6cfa9fa9c
peconn <peconn@chromium.org>
2016-08-12 09:29:32 -0700

    Change Snippet layout based on Category.
    BUG=634275, 631479
    
    Review-Url: https://codereview.chromium.org/2235463002
    Cr-Commit-Position: refs/heads/master@{#411671}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/SuggestionsSection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/SuggestionsSection.java
@@ -21,7 +21,6 @@ import java.util.List;
  */
 public class SuggestionsSection implements ItemGroup {
     private final List<SnippetArticleListItem> mSuggestions = new ArrayList<>();
-    private final SuggestionsCategoryInfo mInfo;
     private final SnippetHeaderListItem mHeader;
     private StatusListItem mStatus;
     private final ProgressListItem mProgressIndicator = new ProgressListItem();
@@ -30,8 +29,8 @@ public class SuggestionsSection implements ItemGroup {
     public SuggestionsSection(int category, List<SnippetArticleListItem> suggestions,
             @CategoryStatusEnum int status, SuggestionsCategoryInfo info,
             NewTabPageAdapter adapter) {
-        mInfo = info;
-        mHeader = new SnippetHeaderListItem(mInfo.getTitle());
+
+        mHeader = new SnippetHeaderListItem(info.getTitle());
         // TODO(pke): Replace the condition with "info.hasMoreButton()" once all other categories
         // are supported by the C++ backend, too.
         // Right now, we hard-code all the sections that are handled in ActionListItem.
