bbf12b2dd3a2dfaec792cfa56a81f2ae153d0137
ben <ben@chromium.org>
2016-08-11 17:19:20 -0700

    Convert Identity and CapabilitySpec to use typemaps instead of TypeConverters
    R=rockot@chromium.org,tsepez@chromium.org
    
    Review-Url: https://codereview.chromium.org/2222403003
    Cr-Commit-Position: refs/heads/master@{#411470}

--- services/shell/public/cpp/connection.h
+++ services/shell/public/cpp/connection.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "services/shell/public/cpp/identity.h"
 #include "services/shell/public/cpp/interface_provider.h"
+#include "services/shell/public/interfaces/connector.mojom.h"
 
 namespace shell {
 
