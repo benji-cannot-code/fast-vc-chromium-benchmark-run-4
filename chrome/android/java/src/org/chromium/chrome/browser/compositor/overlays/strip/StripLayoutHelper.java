6410f0876060aab1b43bb07cbe999d175b799fc6
twellington <twellington@chromium.org>
2016-08-12 11:49:35 -0700

    Use CallbackHelpers in TabStripTest
    Also, run more methods that obtain StripLayoutTab state on the UI
    thread rather than the test thread and disable animations for
    testScrollingStripStackerTabOffsets.
    
    BUG=632843
    
    Review-Url: https://codereview.chromium.org/2238083003
    Cr-Commit-Position: refs/heads/master@{#411722}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
@@ -55,6 +55,7 @@ import java.util.List;
  * The stacking and visual behavior is driven by setting a {@link StripStacker}.
  */
 public class StripLayoutHelper {
+
     // Drag Constants
     private static final int REORDER_SCROLL_NONE = 0;
     private static final int REORDER_SCROLL_LEFT = 1;
@@ -148,6 +149,7 @@ public class StripLayoutHelper {
     // Whether the CascadingStripStacker should be used.
     private boolean mShouldCascadeTabs;
     private boolean mIsFirstLayoutPass;
+    private boolean mAnimationsDisabledForTesting;
 
     // Tab menu item IDs
     public static final int ID_CLOSE_ALL_TABS = 0;
@@ -560,7 +562,8 @@ public class StripLayoutHelper {
                 // If the ScrollingStripStacker is being used and the new tab button is visible, go
                 // directly to the new scroll offset rather than animating. Animating the scroll
                 // causes the new tab button to disappear for a frame.
-                boolean shouldAnimate = !mNewTabButton.isVisible();
+                boolean shouldAnimate = !mNewTabButton.isVisible()
+                        && !mAnimationsDisabledForTesting;
                 setScrollForScrollingTabStacker(delta, shouldAnimate, time);
             } else if (delta != 0.f) {
                 mScroller.startScroll(mScrollOffset, 0, (int) delta, 0, time, EXPAND_DURATION_MS);
@@ -1110,7 +1113,7 @@ public class StripLayoutHelper {
             // 5.a. Cancel any outstanding tab width animations.
             cancelAnimation(mStripTabs[i], StripLayoutTab.Property.WIDTH);
 
-            if (animate) {
+            if (animate && !mAnimationsDisabledForTesting) {
                 startAnimation(buildTabResizeAnimation(tab, mCachedTabWidth), false);
             } else {
                 mStripTabs[i].setWidth(mCachedTabWidth);
@@ -1437,7 +1440,7 @@ public class StripLayoutHelper {
         if (index < newIndex) newIndex--;
 
         // 5. Animate if necessary.
-        if (animate) {
+        if (animate && !mAnimationsDisabledForTesting) {
             final float flipWidth = mCachedTabWidth - mTabOverlapWidth;
             final int direction = oldIndex <= newIndex ? 1 : -1;
             final float animationLength =
@@ -1631,7 +1634,7 @@ public class StripLayoutHelper {
     private void setScrollForScrollingTabStacker(float delta, boolean shouldAnimate, long time) {
         if (delta == 0.f) return;
 
-        if (shouldAnimate) {
+        if (shouldAnimate && !mAnimationsDisabledForTesting) {
             mScroller.startScroll(mScrollOffset, 0, (int) delta, 0, time, EXPAND_DURATION_MS);
         } else {
             mScrollOffset += delta;
@@ -1732,6 +1735,14 @@ public class StripLayoutHelper {
     @VisibleForTesting
     float getTabOverlapWidth() {
         return mTabOverlapWidth;
+    }
+
+    /**
+     * Disables animations for testing purposes.
+     */
+    @VisibleForTesting
+    public void disableAnimationsForTesting() {
+        mAnimationsDisabledForTesting = true;
     }
 
     private void setAccessibilityDescription(StripLayoutTab stripTab, Tab tab) {
--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutTab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutTab.java
@@ -10,6 +10,7 @@ import static org.chromium.chrome.browser.compositor.layouts.ChromeAnimation.Ani
 import android.content.Context;
 import android.graphics.RectF;
 
+import org.chromium.base.ObserverList;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.compositor.layouts.ChromeAnimation;
@@ -34,6 +35,13 @@ import java.util.List;
  */
 public class StripLayoutTab
         implements ChromeAnimation.Animatable<StripLayoutTab.Property>, VirtualView {
+
+    /** An observer interface for StripLayoutTab. */
+    public interface Observer {
+        /** @param visible Whether the StripLayoutTab is visible. */
+        public void onVisibilityChanged(boolean visible);
+    }
+
     /**
      * Animatable properties that can be used with a {@link ChromeAnimation.Animatable} on a
      * {@link StripLayoutTab}.
@@ -90,6 +98,8 @@ public class StripLayoutTab
     // Preallocated
     private final RectF mClosePlacement = new RectF();
 
+    private ObserverList<Observer> mObservers = new ObserverList<>();
+
     /**
      * Create a {@link StripLayoutTab} that represents the {@link Tab} with an id of
      * {@code id}.
@@ -99,7 +109,7 @@ public class StripLayoutTab
      * @param loadTrackerCallback The {@link TabLoadTrackerCallback} to be notified of loading state
      *                            changes.
      * @param renderHost The {@link LayoutRenderHost}.
-     * @param incogntio Whether or not this layout tab is icognito.
+     * @param incognito Whether or not this layout tab is incognito.
      */
     public StripLayoutTab(Context context, int id, TabLoadTrackerCallback loadTrackerCallback,
             LayoutRenderHost renderHost, boolean incognito) {
@@ -116,6 +126,17 @@ public class StripLayoutTab
         String description =
                 context.getResources().getString(R.string.accessibility_tabstrip_btn_close_tab);
         mCloseButton.setAccessibilityDescription(description, description);
+    }
+
+    /** @param observer The observer to add. */
+    @VisibleForTesting
+    public void addObserver(Observer observer) {
+        mObservers.addObserver(observer);
+    }
+
+    /** @param observer The observer to remove. */
+    public void removeObserver(Observer observer) {
+        mObservers.removeObserver(observer);
     }
 
     /**
@@ -174,6 +195,9 @@ public class StripLayoutTab
      */
     public void setVisible(boolean visible) {
         mVisible = visible;
+        for (Observer observer : mObservers) {
+            observer.onVisibilityChanged(mVisible);
+        }
     }
 
     /**
