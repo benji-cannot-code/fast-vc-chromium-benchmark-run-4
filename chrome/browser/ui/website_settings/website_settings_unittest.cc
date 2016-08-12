6268e5e8c9c80e48c10cd0b35fca15eb291b32f5
estark <estark@chromium.org>
2016-08-11 17:27:32 -0700

    Rename SecurityStateModel::MIXED_CONTENT_STATUS enum values
    I'm planning to add information about subresources with certificate
    errors to SecurityStateModel. This CL renames the MixedContentStatus
    enum values to be more generic, so that we can use the same
    passive/displayed, active/ran distinction for subresources with cert
    errors that we use for mixed content.
    
    #1: https://codereview.chromium.org/2224193003/
      (this CL)
    #2: https://codereview.chromium.org/2224023003/
      Teach SecurityStateModel about subresources with cert errors
    #3: https://codereview.chromium.org/2225213004/
      Teach SSLHostStateDelegate about subresources with cert errors
    #4: https://codereview.chromium.org/2226363002/
      Track subresources with cert errors separately from mixed content
    
    BUG=634171,636986
    
    Review-Url: https://codereview.chromium.org/2224193003
    Cr-Commit-Position: refs/heads/master@{#411476}

--- chrome/browser/ui/website_settings/website_settings_unittest.cc
+++ chrome/browser/ui/website_settings/website_settings_unittest.cc
@@ -386,7 +386,7 @@ TEST_F(WebsiteSettingsTest, HTTPSPassiveMixedContent) {
   security_info_.cert_status = 0;
   security_info_.security_bits = 81;  // No error if > 80.
   security_info_.mixed_content_status =
-      SecurityStateModel::DISPLAYED_MIXED_CONTENT;
+      SecurityStateModel::CONTENT_STATUS_DISPLAYED;
   int status = 0;
   status = SetSSLVersion(status, net::SSL_CONNECTION_VERSION_TLS1);
   status = SetSSLCipherSuite(status, CR_TLS_RSA_WITH_AES_256_CBC_SHA256);
@@ -412,7 +412,7 @@ TEST_F(WebsiteSettingsTest, HTTPSActiveMixedContent) {
   security_info_.cert_status = 0;
   security_info_.security_bits = 81;  // No error if > 80.
   security_info_.mixed_content_status =
-      SecurityStateModel::RAN_AND_DISPLAYED_MIXED_CONTENT;
+      SecurityStateModel::CONTENT_STATUS_DISPLAYED_AND_RAN;
   int status = 0;
   status = SetSSLVersion(status, net::SSL_CONNECTION_VERSION_TLS1);
   status = SetSSLCipherSuite(status, CR_TLS_RSA_WITH_AES_256_CBC_SHA256);
@@ -446,7 +446,7 @@ TEST_F(WebsiteSettingsTest, HTTPSEVCert) {
   security_info_.cert_status = net::CERT_STATUS_IS_EV;
   security_info_.security_bits = 81;  // No error if > 80.
   security_info_.mixed_content_status =
-      SecurityStateModel::DISPLAYED_MIXED_CONTENT;
+      SecurityStateModel::CONTENT_STATUS_DISPLAYED;
   int status = 0;
   status = SetSSLVersion(status, net::SSL_CONNECTION_VERSION_TLS1);
   status = SetSSLCipherSuite(status, CR_TLS_RSA_WITH_AES_256_CBC_SHA256);
