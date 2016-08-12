c0aa3250a71a4819023032ccdea5a885502fc854
skym <skym@chromium.org>
2016-08-12 12:51:12 -0700

    [Sync] Adding string and constants for preference data type on ios.
    BUG=374865
    
    Review-Url: https://codereview.chromium.org/2245493002
    Cr-Commit-Position: refs/heads/master@{#411742}

--- ios/chrome/browser/sync/sync_setup_service.h
+++ ios/chrome/browser/sync/sync_setup_service.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_SYNC_SYNC_SETUP_SERVICE_H_
 
 #include <map>
+#include <memory>
 
 #include "base/macros.h"
 #include "base/strings/string16.h"
@@ -43,7 +44,10 @@ class SyncSetupService : public KeyedService {
     kSyncPasswords,
     kSyncOpenTabs,
     kSyncAutofill,
-    kNumberOfSyncableDatatypes
+    kNumberOfSyncableDatatypes,
+    // TODO(skym): This should be moved above kNumberOfSyncableDatatypes after
+    // downstream handling is in place, tracking bug crbug.com/374865.
+    kSyncPreferences
   } SyncableDatatype;
 
   SyncSetupService(sync_driver::SyncService* sync_service, PrefService* prefs);
