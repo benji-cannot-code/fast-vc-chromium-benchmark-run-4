4b444a414820755f11ef4f70ddab4426ad81d722
bmcquade <bmcquade@chromium.org>
2016-08-12 12:59:04 -0700

    Track parse time blocked on script in xml documents.
    This adds the existing equivalent logic from HTMLDocumentParser to
    XMLDocumentParser.
    
    BUG=632343
    
    Review-Url: https://codereview.chromium.org/2235733004
    Cr-Commit-Position: refs/heads/master@{#411745}

--- third_party/WebKit/Source/core/xml/parser/XMLDocumentParser.cpp
+++ third_party/WebKit/Source/core/xml/parser/XMLDocumentParser.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Comment.h"
 #include "core/dom/Document.h"
 #include "core/dom/DocumentFragment.h"
+#include "core/dom/DocumentParserTiming.h"
 #include "core/dom/DocumentType.h"
 #include "core/dom/ProcessingInstruction.h"
 #include "core/dom/ScriptLoader.h"
@@ -400,6 +401,7 @@ void XMLDocumentParser::detach()
     if (m_pendingScript) {
         m_pendingScript->removeClient(this);
         m_pendingScript = nullptr;
+        m_parserBlockingPendingScriptLoadStartTime = 0.0;
     }
     clearCurrentNodeStack();
     ScriptableDocumentParser::detach();
@@ -463,6 +465,8 @@ void XMLDocumentParser::notifyFinished(Resource* unusedResource)
     ScriptSourceCode sourceCode(m_pendingScript.get());
     bool errorOccurred = m_pendingScript->errorOccurred();
     bool wasCanceled = m_pendingScript->wasCanceled();
+    double scriptParserBlockingTime = m_parserBlockingPendingScriptLoadStartTime;
+    m_parserBlockingPendingScriptLoadStartTime = 0.0;
 
     m_pendingScript->removeClient(this);
     m_pendingScript = nullptr;
@@ -476,6 +480,10 @@ void XMLDocumentParser::notifyFinished(Resource* unusedResource)
     if (errorOccurred) {
         scriptLoader->dispatchErrorEvent();
     } else if (!wasCanceled) {
+        if (scriptParserBlockingTime > 0.0) {
+            DocumentParserTiming::from(*document()).recordParserBlockedOnScriptLoadDuration(monotonicallyIncreasingTime() - scriptParserBlockingTime, scriptLoader->wasCreatedDuringDocumentWrite());
+        }
+
         if (!scriptLoader->executeScript(sourceCode))
             scriptLoader->dispatchErrorEvent();
         else
@@ -778,6 +786,7 @@ XMLDocumentParser::XMLDocumentParser(Document& document, FrameView* frameView)
     , m_finishCalled(false)
     , m_xmlErrors(&document)
     , m_scriptStartPosition(TextPosition::belowRangePosition())
+    , m_parserBlockingPendingScriptLoadStartTime(0.0)
     , m_parsingFragment(false)
 {
     // This is XML being used as a document resource.
@@ -1094,6 +1103,8 @@ void XMLDocumentParser::endElementNs()
             }
         } else if (scriptLoader->willBeParserExecuted()) {
             m_pendingScript = scriptLoader->resource();
+            DCHECK_EQ(m_parserBlockingPendingScriptLoadStartTime, 0.0);
+            m_parserBlockingPendingScriptLoadStartTime = monotonicallyIncreasingTime();
             m_scriptElement = element;
             m_pendingScript->addClient(this);
             // m_pendingScript will be 0 if script was already loaded and
--- third_party/WebKit/Source/core/xml/parser/XMLDocumentParser.h
+++ third_party/WebKit/Source/core/xml/parser/XMLDocumentParser.h
@@ -189,6 +189,7 @@ private:
     Member<ScriptResource> m_pendingScript;
     Member<Element> m_scriptElement;
     TextPosition m_scriptStartPosition;
+    double m_parserBlockingPendingScriptLoadStartTime;
 
     bool m_parsingFragment;
     AtomicString m_defaultNamespaceURI;
