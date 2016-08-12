f5a7acd87f4255f6dd073ad47f01da1d673acd58
eugenebut <eugenebut@chromium.org>
2016-08-11 17:42:10 -0700

    Use new JS execution API for translate feature.
    Use methods which returns id instead of deprecated methods which return
    NSString. JS execution API returned NSString because that was the only
    supported option from UIWebView. Now WKWebView returns id which should
    be used everywhere.
    
    BUG=595761
    
    Review-Url: https://codereview.chromium.org/2232033002
    Cr-Commit-Position: refs/heads/master@{#411486}

--- components/translate/ios/browser/js_translate_manager.mm
+++ components/translate/ios/browser/js_translate_manager.mm
@@ -48,27 +48,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)injectWaitUntilTranslateReadyScript {
-  [self.receiver evaluateJavaScript:@"__gCrWeb.translate.checkTranslateReady()"
-                stringResultHandler:nil];
+  [self.receiver executeJavaScript:@"__gCrWeb.translate.checkTranslateReady()"
+                 completionHandler:nil];
 }
 
 - (void)injectTranslateStatusScript {
-  [self.receiver evaluateJavaScript:@"__gCrWeb.translate.checkTranslateStatus()"
-                stringResultHandler:nil];
+  [self.receiver executeJavaScript:@"__gCrWeb.translate.checkTranslateStatus()"
+                 completionHandler:nil];
 }
 
 - (void)startTranslationFrom:(const std::string&)source
                           to:(const std::string&)target {
-  NSString* js =
+  NSString* script =
       [NSString stringWithFormat:@"cr.googleTranslate.translate('%s','%s')",
                                  source.c_str(), target.c_str()];
-  [self.receiver evaluateJavaScript:js stringResultHandler:nil];
+  [self.receiver executeJavaScript:script completionHandler:nil];
 }
 
 - (void)revertTranslation {
   DCHECK([self hasBeenInjected]);
-  [self.receiver evaluateJavaScript:@"cr.googleTranslate.revert()"
-                stringResultHandler:nil];
+  [self.receiver executeJavaScript:@"cr.googleTranslate.revert()"
+                 completionHandler:nil];
 }
 
 #pragma mark -
