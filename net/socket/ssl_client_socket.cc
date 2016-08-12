b3e3433e2597ef5af00f4c934ab0b9acc8010a5e
mab <mab@google.com>
2016-08-11 19:06:40 -0700

    Metric & meta-metric for CECPQ1 handshake latency.
    To fairly measure the latency impact of CECPQ1, we need to measure only
    hosts that will negotiate it.  Define a small group of Google hosts that
    we believe ought to be negotiating CECPQ1, and a new histogram that
    measures latency when connecting to these hosts.
    
    In addition, add a meta-metric that (in the experiment group only)
    allows us to verify that CECPQ1 is being negotiated in cases where we
    expect it to be.  This will serve as a check on the quality of the first
    metric.
    
    BUG=626363
    
    Review-Url: https://codereview.chromium.org/2192053002
    Cr-Commit-Position: refs/heads/master@{#411518}

--- net/socket/ssl_client_socket.cc
+++ net/socket/ssl_client_socket.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/socket/ssl_client_socket.h"
 
+#include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/sparse_histogram.h"
 #include "base/strings/string_util.h"
+#include "build/build_config.h"
 #include "crypto/ec_private_key.h"
 #include "net/base/net_errors.h"
 #include "net/socket/ssl_client_socket_impl.h"
@@ -15,6 +17,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/ssl/ssl_config_service.h"
 
 namespace net {
+
+namespace {
+#if !defined(OS_NACL)
+const base::Feature kPostQuantumExperiment{"SSLPostQuantumExperiment",
+                                           base::FEATURE_DISABLED_BY_DEFAULT};
+#endif
+}  // namespace
 
 SSLClientSocket::SSLClientSocket()
     : signed_cert_timestamps_received_(false),
@@ -78,6 +87,15 @@ bool SSLClientSocket::IgnoreCertError(int error, int load_flags) {
     return true;
   return (load_flags & LOAD_IGNORE_ALL_CERT_ERRORS) &&
          IsCertificateError(error);
+}
+
+// static
+bool SSLClientSocket::IsPostQuantumExperimentEnabled() {
+#if !defined(OS_NACL)
+  return base::FeatureList::IsEnabled(kPostQuantumExperiment);
+#else
+  return false;
+#endif
 }
 
 // static
