a6fb7f46ab1c55b6fbf7c8cc1e9a70822052bd71
erikchen <erikchen@chromium.org>
2016-08-12 16:15:32 -0700

    Fix a bug that prevents all reuse of ResourcePool resources.
    The ColorSpace parameter was being lost when creating a ResourcePool resource.
    
    BUG=44872
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2240873003
    Cr-Commit-Position: refs/heads/master@{#411812}

--- cc/resources/resource.h
+++ cc/resources/resource.h
@@ -35,6 +35,9 @@ class CC_EXPORT Resource {
     size_ = size;
     format_ = format;
   }
+  void set_color_space(const gfx::ColorSpace& color_space) {
+    color_space_ = color_space;
+  }
 
  private:
   ResourceId id_;
