7e8e9d51cb6563ba189887aab93036fdeefc126f
sammc <sammc@chromium.org>
2016-08-11 21:21:29 -0700

    Add a Java InterfaceRegistry.
    This also removes the old Java mojo application support, which has
    rotted.
    
    BUG=634568,637174
    
    Review-Url: https://codereview.chromium.org/2214323002
    Cr-Commit-Position: refs/heads/master@{#411550}

---
+++ services/shell/public/java/src/org/chromium/services/shell/InterfaceFactory.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.services.shell;
+
+import org.chromium.mojo.bindings.Interface;
+
+/**
+ * A factory that creates implementations of a mojo interface.
+ *
+ * @param <I> the mojo interface
+ */
+public interface InterfaceFactory<I extends Interface> {
+    /**
+     * Returns an implementation of the mojo interface.
+     */
+    I createImpl();
+}
