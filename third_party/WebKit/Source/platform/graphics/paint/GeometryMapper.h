b50f7d7fbdef0ce73f25599537c1689141c61137
wangxianzhu <wangxianzhu@chromium.org>
2016-08-11 20:29:15 -0700

    Add GeometryMapper::mapRectToDestinationSpace()
    This is like mapToVisualRectInDestinationSpace() but no clip is applied.
    Will be used in PaintInvalidator.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2243703002
    Cr-Commit-Position: refs/heads/master@{#411542}

--- third_party/WebKit/Source/platform/graphics/paint/GeometryMapper.h
+++ third_party/WebKit/Source/platform/graphics/paint/GeometryMapper.h
@@ -43,7 +43,7 @@ struct PrecomputedDataForAncestor {
 class PLATFORM_EXPORT GeometryMapper {
 public:
     GeometryMapper() {}
-    // The runtime of m calls among LocalToVisualRectInAncestorSpace, LocalToAncestorRect or AncestorToLocalRect
+    // The runtime of m calls among localToVisualRectInAncestorSpace, localToAncestorRect or ancestorToLocalRect
     // with the same |ancestorState| parameter is guaranteed to be O(n + m),  where n is the number of transform and clip
     // nodes in their respective property trees.
 
@@ -55,6 +55,12 @@ public:
     // If that inverse transform is not invertible, sets |success| to false and returns the input rect. Otherwise, sets
     // |success| to true.
     FloatRect mapToVisualRectInDestinationSpace(const FloatRect&,
+        const PropertyTreeState& sourceState,
+        const PropertyTreeState& destinationState,
+        bool& success);
+
+    // Same as mapToVisualRectInDestinationSpace() except that *no* clip is applied.
+    FloatRect mapRectToDestinationSpace(const FloatRect&,
         const PropertyTreeState& sourceState,
         const PropertyTreeState& destinationState,
         bool& success);
@@ -100,6 +106,13 @@ public:
         const PropertyTreeState& ancestorState, bool& success);
 
 private:
+    // Used by mapToVisualRectInDestinationSpace() and mapRectToDestinationSpace() after fast mapping
+    // (assuming destination is an ancestor of source) failed.
+    FloatRect slowMapRectToDestinationSpace(const FloatRect&,
+        const PropertyTreeState& sourceState,
+        const PropertyTreeState& destinationState,
+        bool& success);
+
     // Returns the matrix used in |LocalToAncestorRect|. Sets |success| to failse iff |localTransformNode| is not
     // equal to or a descendant of |ancestorState.transform|.
     const TransformationMatrix& localToAncestorMatrix(
