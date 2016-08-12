c113eeed0a0bd657b8ded57ebe32ee816870c801
senorblanco <senorblanco@chromium.org>
2016-08-12 12:34:17 -0700

    cc: fix damage tracker rect with hidpi filters.
    BUG=624027
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2241763002
    Cr-Commit-Position: refs/heads/master@{#411734}

--- cc/layers/render_surface_impl.h
+++ cc/layers/render_surface_impl.h
@@ -157,6 +157,7 @@ class CC_EXPORT RenderSurfaceImpl {
 
   const FilterOperations& Filters() const;
   const FilterOperations& BackgroundFilters() const;
+  gfx::Transform FiltersTransform() const;
 
   bool HasCopyRequest() const;
 
