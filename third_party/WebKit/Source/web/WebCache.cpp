14082a0adcda330ebad7fa0de18da9f535d2a1dc
ssid <ssid@chromium.org>
2016-08-11 22:07:34 -0700

    Fix error in purgeable_size reported by WebCache.
    Review-Url: https://codereview.chromium.org/2244493002
    Cr-Commit-Position: refs/heads/master@{#411554}

--- third_party/WebKit/Source/web/WebCache.cpp
+++ third_party/WebKit/Source/web/WebCache.cpp
@@ -44,7 +44,7 @@ static void ToResourceTypeStat(const MemoryCache::TypeStatistic& from,
     to.size = from.size;
     to.liveSize = from.liveSize;
     to.decodedSize = from.decodedSize;
-    to.purgeableSize = from.purgedSize;
+    to.purgeableSize = from.purgeableSize;
     to.purgedSize = from.purgedSize;
 }
 
