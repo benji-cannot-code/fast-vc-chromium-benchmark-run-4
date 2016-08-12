b8d5ad86c01c45aa5e585bc379465c15c01ce28c
qyearsley <qyearsley@chromium.org>
2016-08-12 15:17:49 -0700

    In rebaseline-cl: add option to only rebaseline tests changed in the CL.
    In this CL:
     - Add a flag --only-changed-tests to rebaseline-cl, to specify that only baselines for tests changed in the CL should be downloaded, rather than all baselines for any failed tests.
     - Add a helper function changed_files to rietveld to get the list of changed files for a CL.
     - Add a helper method layout_test_name to WebKitFinder to return the test name given the path from the chromium repo base.
    
    Purpose: There are sometimes flaky test failures when running the try jobs, and sometimes users will only want baselines for tests modified in the CL. In some cases they'll change source files and want baselines for tests not changed in the current CL though.
    
    So, I believe we may want an option to choose between downloading all new baselines, and downloading just those for changed tests.
    
    BUG=474273
    
    Review-Url: https://codereview.chromium.org/2237083002
    Cr-Commit-Position: refs/heads/master@{#411794}

--- third_party/WebKit/Tools/Scripts/webkitpy/common/net/rietveld_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/net/rietveld_unittest.py
@@ -11,6 +11,7 @@ import urllib2
 from webkitpy.common.net.rietveld import filter_latest_jobs
 from webkitpy.common.net.rietveld import get_latest_try_job_results
 from webkitpy.common.net.rietveld import latest_try_jobs
+from webkitpy.common.net.rietveld import changed_files
 from webkitpy.common.net.buildbot import Build
 from webkitpy.common.net.web_mock import MockWeb
 from webkitpy.common.system.outputcapture import OutputCapture
@@ -53,6 +54,10 @@ class RietveldTest(unittest.TestCase):
                         'result': 0
                     },
                 ],
+                'files': {
+                    'some/path/foo.cc': {'status': 'M'},
+                    'some/path/bar.html': {'status': 'M'},
+                }
             }),
             'https://codereview.chromium.org/api/11113333': 'my non-JSON contents',
         })
@@ -111,3 +116,11 @@ class RietveldTest(unittest.TestCase):
         self.assertEqual(
             filter_latest_jobs([Build('foo', 3), Build('bar')]),
             [Build('foo', 3)])
+
+    def test_changed_files(self):
+        self.assertEqual(
+            changed_files(11112222, self.web),
+            ['some/path/bar.html', 'some/path/foo.cc'])
+
+    def test_changed_files_no_results(self):
+        self.assertIsNone(changed_files(11113333, self.web))
