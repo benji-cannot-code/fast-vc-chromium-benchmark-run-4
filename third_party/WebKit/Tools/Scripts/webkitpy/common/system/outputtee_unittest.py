aebd8d346d8a1d857fd2f9ca4c7975b6bf7e38f7
qyearsley <qyearsley@chromium.org>
2016-08-11 18:29:03 -0700

    Remove unused class OutputTee.
    Review-Url: https://codereview.chromium.org/2237283002
    Cr-Commit-Position: refs/heads/master@{#411506}

--- third_party/WebKit/Tools/Scripts/webkitpy/common/system/outputtee_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/system/outputtee_unittest.py
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import StringIO
 import unittest
 
-from webkitpy.common.system.outputtee import OutputTee
 from webkitpy.common.system.outputtee import Tee
 
 
