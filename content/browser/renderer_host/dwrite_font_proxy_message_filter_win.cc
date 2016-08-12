06d67863810d1e02f0dec34802bbc576088b3207
kulshin <kulshin@chromium.org>
2016-08-11 19:04:51 -0700

    Add telemetry to track HRESULTS from failing calls
    We've identified some points of failure in font loading. Adding these
    metrics will allow us to determine in what way it is failing.
    
    BUG=561873
    
    Review-Url: https://codereview.chromium.org/2238093002
    Cr-Commit-Position: refs/heads/master@{#411517}

--- content/browser/renderer_host/dwrite_font_proxy_message_filter_win.cc
+++ content/browser/renderer_host/dwrite_font_proxy_message_filter_win.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/metrics/sparse_histogram.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -433,6 +434,8 @@ void DWriteFontProxyMessageFilter::InitializeDirectWrite() {
   DCHECK(SUCCEEDED(hr));
 
   if (!collection_) {
+    UMA_HISTOGRAM_SPARSE_SLOWLY(
+        "DirectWrite.Fonts.Proxy.GetSystemFontCollectionResult", hr);
     LogMessageFilterError(ERROR_NO_COLLECTION);
     return;
   }
@@ -463,6 +466,8 @@ bool DWriteFontProxyMessageFilter::AddFilesForFont(
   HRESULT hr;
   hr = font->CreateFontFace(&font_face);
   if (FAILED(hr)) {
+    UMA_HISTOGRAM_SPARSE_SLOWLY("DirectWrite.Fonts.Proxy.CreateFontFaceResult",
+                                hr);
     LogMessageFilterError(ADD_FILES_FOR_FONT_CREATE_FACE_FAILED);
     return false;
   }
