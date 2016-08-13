f919dc43b7d8a683bd224b63915a1375034b62bf
dimich <dimich@chromium.org>
2016-08-12 17:32:39 -0700

    Hook up the OfflinePageDownloadBridge to UI actions open/delete/download
    BUG=630817
    
    Review-Url: https://codereview.chromium.org/2243063002
    Cr-Commit-Position: refs/heads/master@{#411832}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/downloads/OfflinePageDownloadBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/downloads/OfflinePageDownloadBridge.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.offlinepages.downloads;
 
-import android.app.Activity;
-
 import org.chromium.base.ObserverList;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
@@ -125,9 +123,8 @@ public class OfflinePageDownloadBridge {
      * No automatic redirection is happening based on the connection status.
      * If the item with specified GUID is not found or can't be opened, nothing happens.
      * @param guid a GUID of the item to open.
-     * @param activity Activity requesting to open the item (offline page).
      */
-    public void openItem(String guid, Activity activity) {
+    public void openItem(String guid) {
         String url = nativeGetOfflineUrlByGuid(mNativeOfflinePageDownloadBridge, guid);
         if (url == null) return;
 
