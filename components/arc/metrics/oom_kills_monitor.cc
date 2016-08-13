77049044260eac95d0f6d3347b75b452a73d4708
gab <gab@chromium.org>
2016-08-13 11:38:29 -0700

    Move OomKillsMonitor from a single-threaded SequencedWorkerPool to a non-joinable thread.
    The semantics are equivalent after this CL:
     - SequencedWorkerPool::CONTINUE_ON_SHUTDOWN => non-joinable thread + leaky instance.
       - Actually, it's better than before because with lack of a Leaky lazy instance,
         there was a join despite the CONTINUE_ON_SHUTDOWN request [1].
     - Ramping worker thread down on destruction through OomKillsMonitorHandle.
    
    [1] https://cs.chromium.org/chromium/src/base/threading/sequenced_worker_pool.cc?rcl=1469791693&l=601
    
    BUG=622400
    
    Review-Url: https://codereview.chromium.org/2197753002
    Cr-Commit-Position: refs/heads/master@{#411899}

--- components/arc/metrics/oom_kills_monitor.cc
+++ components/arc/metrics/oom_kills_monitor.cc
@@ -12,10 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
+#include "base/debug/leak_annotations.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_file.h"
 #include "base/location.h"
 #include "base/logging.h"
+#include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/posix/safe_strerror.h"
 #include "base/sequenced_task_runner.h"
@@ -33,25 +35,6 @@ using base::StringPiece;
 
 using base::SequencedWorkerPool;
 using base::TimeDelta;
-
-OomKillsMonitor::OomKillsMonitor()
-    : worker_pool_(
-          new SequencedWorkerPool(1, "oom_kills_monitor")) {}
-
-OomKillsMonitor::~OomKillsMonitor() {
-  Stop();
-}
-
-void OomKillsMonitor::Start() {
-  auto task_runner = worker_pool_->GetTaskRunnerWithShutdownBehavior(
-      SequencedWorkerPool::CONTINUE_ON_SHUTDOWN);
-  task_runner->PostTask(
-      FROM_HERE, base::Bind(&OomKillsMonitor::Run, worker_pool_));
-}
-
-void OomKillsMonitor::Stop() {
-  worker_pool_->Shutdown();
-}
 
 namespace {
 
@@ -155,9 +138,48 @@ void LogLowMemoryKill(const StringPiece& line) {
 
 }  // namespace
 
+OomKillsMonitor::Handle::Handle(OomKillsMonitor* outer) : outer_(outer) {
+  DCHECK(outer_);
+}
+
+OomKillsMonitor::Handle::~Handle() {
+  outer_->is_shutting_down_.Set();
+}
+
+OomKillsMonitor::OomKillsMonitor() {
+  base::SimpleThread::Options non_joinable_options;
+  non_joinable_options.joinable = false;
+  non_joinable_worker_thread_ = base::MakeUnique<base::DelegateSimpleThread>(
+      this, "oom_kills_monitor", non_joinable_options);
+  non_joinable_worker_thread_->Start();
+}
+
+OomKillsMonitor::~OomKillsMonitor() {
+  // The instance has to be leaked on shutdown as it is referred to by a
+  // non-joinable thread but ~OomKillsMonitor() can't be explicitly deleted as
+  // it overrides ~SimpleThread(), it should nevertheless never be invoked.
+  NOTREACHED();
+}
+
 // static
-void OomKillsMonitor::Run(
-    scoped_refptr<base::SequencedWorkerPool> worker_pool) {
+OomKillsMonitor::Handle OomKillsMonitor::StartMonitoring() {
+#if DCHECK_IS_ON()
+  static volatile bool monitoring_active = false;
+  DCHECK(!monitoring_active);
+  monitoring_active = true;
+#endif
+
+  // Instantiate the OomKillsMonitor and its underlying thread. The
+  // OomKillsMonitor itself has to be leaked on shutdown per having a
+  // non-joinable thread associated to its state. The OomKillsMonitor::Handle
+  // will notify the OomKillsMonitor when it is destroyed so that the underlying
+  // thread can at a minimum not do extra work during shutdown.
+  OomKillsMonitor* instance = new OomKillsMonitor;
+  ANNOTATE_LEAKING_OBJECT_PTR(instance);
+  return Handle(instance);
+}
+
+void OomKillsMonitor::Run() {
   base::ScopedFILE kmsg_handle(
       base::OpenFile(base::FilePath("/dev/kmsg"), "r"));
   if (!kmsg_handle) {
@@ -172,7 +194,7 @@ void OomKillsMonitor::Run(
   char buf[kMaxBufSize];
 
   while (fgets(buf, kMaxBufSize, kmsg_handle.get())) {
-    if (worker_pool->IsShutdownInProgress()) {
+    if (is_shutting_down_.IsSet()) {
       DVLOG(1) << "Chrome is shutting down, exit now.";
       break;
     }
