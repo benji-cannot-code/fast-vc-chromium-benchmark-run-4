0696a47961e698965262743e7ff785caacb127ad
qinmin <qinmin@chromium.org>
2016-08-15 13:09:54 -0700

    Fix an issue that "save video" context menu appears for MSE video
    We should use media_flags to determine if a video can be saved.
    
    BUG=637010
    
    Committed: https://crrev.com/16006f8e310a6b0da0988b558f661003a8bd262e
    Review-Url: https://codereview.chromium.org/2239063004
    Cr-Original-Commit-Position: refs/heads/master@{#411807}
    Cr-Commit-Position: refs/heads/master@{#412027}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuParams.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuParams.java
@@ -40,6 +40,7 @@ public class ContextMenuParams {
     private final boolean mIsAnchor;
     private final boolean mIsImage;
     private final boolean mIsVideo;
+    private final boolean mCanSavemedia;
 
     /**
      * @return The URL associated with the main frame of the page that triggered the context menu.
@@ -118,9 +119,13 @@ public class ContextMenuParams {
         return mIsVideo;
     }
 
+    public boolean canSaveMedia() {
+        return mCanSavemedia;
+    }
+
     private ContextMenuParams(int mediaType, String pageUrl, String linkUrl, String linkText,
             String unfilteredLinkUrl, String srcUrl, String titleText, boolean imageWasFetchedLoFi,
-            Referrer referrer) {
+            Referrer referrer, boolean canSavemedia) {
         mPageUrl = pageUrl;
         mLinkUrl = linkUrl;
         mLinkText = linkText;
@@ -133,15 +138,17 @@ public class ContextMenuParams {
         mIsAnchor = !TextUtils.isEmpty(linkUrl);
         mIsImage = mediaType == MediaType.MEDIA_TYPE_IMAGE;
         mIsVideo = mediaType == MediaType.MEDIA_TYPE_VIDEO;
+        mCanSavemedia = canSavemedia;
     }
 
     @CalledByNative
     private static ContextMenuParams create(int mediaType, String pageUrl, String linkUrl,
             String linkText, String unfilteredLinkUrl, String srcUrl, String titleText,
-            boolean imageWasFetchedLoFi, String sanitizedReferrer, int referrerPolicy) {
+            boolean imageWasFetchedLoFi, String sanitizedReferrer, int referrerPolicy,
+            boolean canSavemedia) {
         Referrer referrer = TextUtils.isEmpty(sanitizedReferrer)
                 ? null : new Referrer(sanitizedReferrer, referrerPolicy);
         return new ContextMenuParams(mediaType, pageUrl, linkUrl, linkText, unfilteredLinkUrl,
-                srcUrl, titleText, imageWasFetchedLoFi, referrer);
+                srcUrl, titleText, imageWasFetchedLoFi, referrer, canSavemedia);
     }
 }
--- chrome/browser/ui/android/context_menu_helper.cc
+++ chrome/browser/ui/android/context_menu_helper.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/context_menu_params.h"
 #include "jni/ContextMenuHelper_jni.h"
 #include "jni/ContextMenuParams_jni.h"
+#include "third_party/WebKit/public/web/WebContextMenuData.h"
 #include "ui/android/window_android.h"
 #include "ui/gfx/geometry/point.h"
 
@@ -90,7 +91,7 @@ ContextMenuHelper::CreateJavaContextMenuParams(
   bool image_was_fetched_lo_fi =
       it != params.properties.end() &&
       it->second == data_reduction_proxy::chrome_proxy_lo_fi_directive();
-
+  bool can_save = params.media_flags & blink::WebContextMenuData::MediaCanSave;
   JNIEnv* env = base::android::AttachCurrentThread();
   base::android::ScopedJavaLocalRef<jobject> jmenu_info =
       ContextMenuParamsAndroid::Java_ContextMenuParams_create(
@@ -104,7 +105,8 @@ ContextMenuHelper::CreateJavaContextMenuParams(
           ConvertUTF16ToJavaString(env, params.title_text).obj(),
           image_was_fetched_lo_fi,
           ConvertUTF8ToJavaString(env, sanitizedReferrer.spec()).obj(),
-          params.referrer_policy);
+          params.referrer_policy,
+          can_save);
 
   return jmenu_info;
 }
