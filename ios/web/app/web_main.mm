4bc246e430cf4ccfe58aad0f2fdb95472b4a905b
eugenebut <eugenebut@chromium.org>
2016-08-15 11:16:52 -0700

    [ios] Made web_main_runner.h header public.
    content_main_runner.h is public, so web_main_runner.h should be public
    as well.
    
    BUG=616247
    
    Review-Url: https://codereview.chromium.org/2246673002
    Cr-Commit-Position: refs/heads/master@{#411989}

--- ios/web/app/web_main.mm
+++ ios/web/app/web_main.mm
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ios/web/app/web_main_runner.h"
 #include "ios/web/public/app/web_main.h"
+#include "ios/web/public/app/web_main_runner.h"
 
 namespace web {
 
