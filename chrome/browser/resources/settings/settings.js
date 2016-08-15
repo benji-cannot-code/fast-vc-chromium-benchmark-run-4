cd5a464740f40f25da55e1e07dd43d58e05982de
tommycli <tommycli@chromium.org>
2016-08-15 11:31:48 -0700

    Settings Router Refactor: Normalize URL only for main settings page.
    Normalize URL in cr-settings.created only, not when child elements are
    directly loaded.
    
    i.e., URL normalized for chrome://md-settings/foo/bar but not for
    chrome://md-settings/people_page/people_page.html
    
    Also fixes a bug where the URL state is pushed rather than replaced.
    
    BUG=632382
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2231203002
    Cr-Commit-Position: refs/heads/master@{#411993}

--- chrome/browser/resources/settings/settings.js
+++ chrome/browser/resources/settings/settings.js
@@ -20,6 +20,12 @@ settings.defaultResourceLoaded = true;
 Polymer({
   is: 'cr-settings',
 
+  /** @override */
+  created: function() {
+    settings.initializeRouteFromUrl();
+  },
+
+  /** @override */
   ready: function() {
     this.$.ui.directionDelegate = new settings.DirectionDelegateImpl;
   },
