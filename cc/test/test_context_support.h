a9b0adc2952bef5fa75a8d7735af2a64a445df84
ericrk <ericrk@chromium.org>
2016-08-15 12:29:55 -0700

    Fix shared context cleanup logic
    When a client of a context (either LayerTreeHostImpl (LTHI) or
    GLRenderer) becomes invisible, it currently causes the context to clean
    up cached resources and enter a low-memory/low-efficiency mode. This
    logic is incorrect in cases where more than one client shares a context.
    Instead, we should only clean up and enter low-memory mode when *all*
    clients are invisible.
    
    This CL adds tracking logic so that we ensure all clients are visible
    before purging caches and entering low-memory mode. It also
    consolidates cleanup logic to two locations: GLRenderer and LTHI.
    
    R=danakj@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2243933002
    Cr-Commit-Position: refs/heads/master@{#412019}

--- cc/test/test_context_support.h
+++ cc/test/test_context_support.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <set>
 #include <vector>
 
 #include "base/macros.h"
@@ -42,6 +43,8 @@ class TestContextSupport : public gpu::ContextSupport {
   uint64_t ShareGroupTracingGUID() const override;
   void SetErrorMessageCallback(
       const base::Callback<void(const char*, int32_t)>& callback) override;
+  void SetClientVisible(int client_id, bool is_visible) override;
+  bool AnyClientsVisible() const override;
 
   void CallAllSyncPointCallbacks();
 
@@ -64,6 +67,7 @@ class TestContextSupport : public gpu::ContextSupport {
   std::vector<base::Closure> sync_point_callbacks_;
   ScheduleOverlayPlaneCallback schedule_overlay_plane_callback_;
   bool out_of_order_callbacks_;
+  std::set<int> visible_clients_;
 
   base::WeakPtrFactory<TestContextSupport> weak_ptr_factory_;
 
