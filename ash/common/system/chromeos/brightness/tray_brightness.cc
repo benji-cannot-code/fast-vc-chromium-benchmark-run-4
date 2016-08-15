93a3e05f69701700f634fee522438495de2eaa9c
tdanderson <tdanderson@chromium.org>
2016-08-15 08:37:12 -0700

    Add icons for Ash material design system menu rows
    Add the following icons to be used in the Ash
    material design system menu default views:
    
    * Enterprise,
    * IME (note this icon will also be used in the detailed
      views for accessibility and Bluetooth),
    * Brightness,
    * Child user,
    * Supervised user, and
    * Session length limit.
    
    BUG=632040,632022,631809,630642
    TEST=manual
    
    Review-Url: https://codereview.chromium.org/2243913002
    Cr-Commit-Position: refs/heads/master@{#411970}

--- ash/common/system/chromeos/brightness/tray_brightness.cc
+++ ash/common/system/chromeos/brightness/tray_brightness.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "ash/common/ash_constants.h"
+#include "ash/common/material_design/material_design_controller.h"
 #include "ash/common/shell_observer.h"
 #include "ash/common/system/brightness_control_delegate.h"
 #include "ash/common/system/tray/fixed_sized_image_view.h"
@@ -25,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/display/display.h"
 #include "ui/gfx/image/image.h"
+#include "ui/gfx/paint_vector_icon.h"
+#include "ui/gfx/vector_icons_public.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
@@ -97,19 +100,23 @@ BrightnessView::BrightnessView(bool default_view, double initial_percent)
                                         kTrayPopupPaddingHorizontal, 0,
                                         kTrayPopupPaddingBetweenItems));
 
+  ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
   views::ImageView* icon =
       new FixedSizedImageView(0, GetTrayConstant(TRAY_POPUP_ITEM_HEIGHT));
-  gfx::Image image = ui::ResourceBundle::GetSharedInstance().GetImageNamed(
-      IDR_AURA_UBER_TRAY_BRIGHTNESS);
-  icon->SetImage(image.ToImageSkia());
+  if (MaterialDesignController::IsSystemTrayMenuMaterial()) {
+    icon->SetImage(gfx::CreateVectorIcon(
+        gfx::VectorIconId::SYSTEM_MENU_BRIGHTNESS, kMenuIconColor));
+  } else {
+    icon->SetImage(
+        rb.GetImageNamed(IDR_AURA_UBER_TRAY_BRIGHTNESS).ToImageSkia());
+  }
   AddChildView(icon);
 
   slider_ = new views::Slider(this, views::Slider::HORIZONTAL);
   slider_->set_focus_border_color(kFocusBorderColor);
   slider_->SetValue(static_cast<float>(initial_percent / 100.0));
   slider_->SetAccessibleName(
-      ui::ResourceBundle::GetSharedInstance().GetLocalizedString(
-          IDS_ASH_STATUS_TRAY_BRIGHTNESS));
+      rb.GetLocalizedString(IDS_ASH_STATUS_TRAY_BRIGHTNESS));
   AddChildView(slider_);
 
   if (is_default_view_) {
