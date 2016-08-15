3592e093d904bedef66125f78c16345194d5b2d8
sadrul <sadrul@chromium.org>
2016-08-15 15:16:46 -0700

    mus: Always use the GPU compositor for mus.
    BUG=635613
    
    Review-Url: https://codereview.chromium.org/2237053002
    Cr-Commit-Position: refs/heads/master@{#412069}

--- content/browser/compositor/gpu_process_transport_factory.cc
+++ content/browser/compositor/gpu_process_transport_factory.cc
@@ -269,6 +269,8 @@ static bool ShouldCreateGpuOutputSurface(ui::Compositor* compositor) {
   // Software fallback does not happen on Chrome OS.
   return true;
 #endif
+  if (IsUsingMus())
+    return true;
 
 #if defined(OS_WIN)
   if (::GetProp(compositor->widget(), kForceSoftwareCompositor) &&
@@ -330,10 +332,12 @@ void GpuProcessTransportFactory::EstablishedGpuChannel(
   DCHECK(data);
 
   if (num_attempts > kNumRetriesBeforeSoftwareFallback) {
+    bool fatal = IsUsingMus();
 #if defined(OS_CHROMEOS)
-    LOG(FATAL) << "Unable to create a UI graphics context, and cannot use "
-               << "software compositing on ChromeOS.";
+    fatal = true;
 #endif
+    LOG_IF(FATAL, fatal) << "Unable to create a UI graphics context, and "
+                         << "cannot use software compositing on ChromeOS.";
     create_gpu_output_surface = false;
   }
 
