ac09db54aaf35fa37dddea98a92418db0a93f6a7
mariakhomenko <mariakhomenko@chromium.org>
2016-08-15 16:33:55 -0700

    Rework some incognito click fixes.
    Removed the EXTRA in favor of keeping a pending URL in memory.
    
    BUG=633040
    
    Review-Url: https://codereview.chromium.org/2242643002
    Cr-Commit-Position: refs/heads/master@{#412095}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
@@ -308,7 +308,7 @@ public class ChromeApplication extends ContentApplication {
 
         ChildProcessLauncher.onSentToBackground();
         IntentHandler.clearPendingReferrer();
-        IntentHandler.clearPendingIncognitoIntent();
+        IntentHandler.clearPendingIncognitoUrl();
 
         if (FeatureUtilities.isDocumentMode(this)) {
             if (sDocumentTabModelSelector != null) {
