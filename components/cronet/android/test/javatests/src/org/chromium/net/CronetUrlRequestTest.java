fa810c0149487ed9749906aa60206b4097884e48
xunjieli <xunjieli@chromium.org>
2016-08-15 11:39:15 -0700

    [Cronet] Fix testDestroyUploadDataStreamAdapterOnSucceededCallback flake.
    This CL makes testDestroyUploadDataStreamAdapterOnSucceededCallback to
    wait until the request finishes, so tearDown() can safely shut down the
    CronetEngine.
    
    BUG=635618
    
    Review-Url: https://codereview.chromium.org/2247773002
    Cr-Commit-Position: refs/heads/master@{#411999}

--- components/cronet/android/test/javatests/src/org/chromium/net/CronetUrlRequestTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/CronetUrlRequestTest.java
@@ -1725,6 +1725,7 @@ public class CronetUrlRequestTest extends CronetTestBase {
 
         request.start();
         uploadDataStreamAdapterDestroyed.block();
+        callback.blockForDone();
 
         assertEquals(200, callback.mResponseInfo.getHttpStatusCode());
         assertEquals("", callback.mResponseAsString);
