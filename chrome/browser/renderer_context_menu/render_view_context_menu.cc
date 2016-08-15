d3fbc8442c18110c320052f033192db0c8442a79
takumif <takumif@chromium.org>
2016-08-15 16:49:15 -0700

    Call GetBrowser() from IsRouteMediaEnabled()
    This fixes the issue of the "Cast..." context menu option being disabled in the PDF viewer.
    
    BUG=602177
    
    Review-Url: https://codereview.chromium.org/2232353002
    Cr-Commit-Position: refs/heads/master@{#412101}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -2099,7 +2099,7 @@ bool RenderViewContextMenu::IsRouteMediaEnabled() const {
   if (!media_router::MediaRouterEnabled(browser_context_))
     return false;
 
-  Browser* browser = chrome::FindBrowserWithWebContents(source_web_contents_);
+  Browser* browser = GetBrowser();
   if (!browser)
     return false;
 
@@ -2378,13 +2378,9 @@ void RenderViewContextMenu::ExecRouteMedia() {
   if (!media_router::MediaRouterEnabled(browser_context_))
     return;
 
-  Browser* browser =
-      chrome::FindBrowserWithWebContents(source_web_contents_);
-  DCHECK(browser);
-
   media_router::MediaRouterDialogController* dialog_controller =
       media_router::MediaRouterDialogController::GetOrCreateForWebContents(
-          source_web_contents_);
+          embedder_web_contents_);
   if (!dialog_controller)
     return;
 
