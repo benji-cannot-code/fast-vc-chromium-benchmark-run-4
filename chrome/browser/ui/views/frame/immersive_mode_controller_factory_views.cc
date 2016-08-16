7e00d0ff896f7375d2fdaa314ff5ab0b374a4fd3
sky <sky@chromium.org>
2016-08-16 16:51:07 -0700

    Separates out focus and gesture handling for ImmersiveFullscreenController
    These need to be different for mus.
    
    BUG=548435
    TEST=covered by tests
    R=jamescook@chromium.org
    
    Review-Url: https://codereview.chromium.org/2247343002
    Cr-Commit-Position: refs/heads/master@{#412381}

--- chrome/browser/ui/views/frame/immersive_mode_controller_factory_views.cc
+++ chrome/browser/ui/views/frame/immersive_mode_controller_factory_views.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/immersive_mode_controller_stub.h"
 
 #if defined(USE_ASH)
+#include "chrome/browser/ui/ash/ash_util.h"
 #include "chrome/browser/ui/views/frame/immersive_mode_controller_ash.h"
 #endif
 
@@ -13,10 +14,13 @@ namespace chrome {
 
 ImmersiveModeController* CreateImmersiveModeController() {
 #if defined(USE_ASH)
-  return new ImmersiveModeControllerAsh();
-#else
-  return new ImmersiveModeControllerStub();
+  if (!IsRunningInMash())
+    return new ImmersiveModeControllerAsh();
+
+  // TODO: http://crbug.com/548435
+  NOTIMPLEMENTED();
 #endif  // USE_ASH
+  return new ImmersiveModeControllerStub();
 }
 
 }  // namespace chrome
