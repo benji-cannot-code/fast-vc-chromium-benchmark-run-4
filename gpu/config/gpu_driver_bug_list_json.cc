d00ac91944a0ff10de26d0810b35bde54605c051
zmo <zmo@chromium.org>
2016-08-16 15:16:15 -0700

    Work around MacOSX driver bug related with glGetFragDataLocation.
    BUG=638340
    TEST=gl_tests
    R=piman@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2250803002
    Cr-Commit-Position: refs/heads/master@{#412355}

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -20,7 +20,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "8.82",
+  "version": "8.83",
   "entries": [
     {
       "id": 1,
@@ -1899,6 +1899,17 @@ LONG_STRING_CONST(
       "gl_renderer": ".*Atom.*x5/x7.*",
       "features": [
         "gl_clear_broken"
+      ]
+    },
+    {
+      "id": 177,
+      "description": "glGetFragData{Location|Index} works incorrectly on Max",
+      "cr_bugs": [638340],
+      "os": {
+        "type": "macosx"
+      },
+      "features": [
+        "get_frag_data_info_bug"
       ]
     }
   ]
