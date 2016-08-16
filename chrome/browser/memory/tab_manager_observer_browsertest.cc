51010c745827a7fad715480302fd8cf8a9e56059
andersoncss <andersoncss@google.com>
2016-08-16 13:23:02 -0700

    TabManager should be defined in all cases, so we no longer need to defensively check whether or not it is null.
    BUG=630645
    
    Review-Url: https://codereview.chromium.org/2245203002
    Cr-Commit-Position: refs/heads/master@{#412323}

--- chrome/browser/memory/tab_manager_observer_browsertest.cc
+++ chrome/browser/memory/tab_manager_observer_browsertest.cc
@@ -84,7 +84,6 @@ class MockTabManagerObserver : public TabManagerObserver {
 
 IN_PROC_BROWSER_TEST_F(TabManagerObserverTest, OnDiscardStateChange) {
   TabManager* tab_manager = g_browser_process->GetTabManager();
-  ASSERT_TRUE(tab_manager);
   auto* tsm = browser()->tab_strip_model();
   set_tab_strip_model(tsm);
 
@@ -147,7 +146,6 @@ IN_PROC_BROWSER_TEST_F(TabManagerObserverTest, OnDiscardStateChange) {
 
 IN_PROC_BROWSER_TEST_F(TabManagerObserverTest, OnAutoDiscardableStateChange) {
   TabManager* tab_manager = g_browser_process->GetTabManager();
-  ASSERT_TRUE(tab_manager);
   auto tsm = browser()->tab_strip_model();
   set_tab_strip_model(tsm);
 
