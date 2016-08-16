3a53123028be42c1037bd8cf200dae3cb3608f6b
sadrul <sadrul@chromium.org>
2016-08-16 13:11:21 -0700

    gpu: Use GpuMemoryBufferManager from GpuService when possible.
    ui::GpuService creates a GpuMemoryBufferManager instance that communicates with
    the gpu process over mojo (MojoGpuMemoryBufferManager). When running chrome as a
    muc client, use this version of the GpuMemoryBufferManager, instead of the one
    created in content. Add GpuChannelEstablishFactory::GetGpuMemoryBufferManager()
    for this.
    
    This makes it unnecessary to create the BrowserGpuChannelHostFactory or the
    BrowserGpuMemoryBufferManager singleton intances when running chrome as a mus
    client.
    
    BUG=637920
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2247173002
    Cr-Commit-Position: refs/heads/master@{#412316}

--- gpu/ipc/client/gpu_channel_host.h
+++ gpu/ipc/client/gpu_channel_host.h
@@ -55,6 +55,7 @@ class GPU_EXPORT GpuChannelEstablishFactory {
   virtual void EstablishGpuChannel(
       const GpuChannelEstablishedCallback& callback) = 0;
   virtual scoped_refptr<GpuChannelHost> EstablishGpuChannelSync() = 0;
+  virtual GpuMemoryBufferManager* GetGpuMemoryBufferManager() = 0;
 };
 
 class GPU_EXPORT GpuChannelHostFactory {
