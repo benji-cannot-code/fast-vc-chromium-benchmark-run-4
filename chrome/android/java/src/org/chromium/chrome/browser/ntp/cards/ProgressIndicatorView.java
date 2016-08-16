789e3f6220d9a217e5f0f662ed6591e2db9a08ac
dgn <dgn@chromium.org>
2016-08-16 06:17:22 -0700

    [NTP Client] Make the spinner take no space when invisible
    BUG=636376
    
    Review-Url: https://codereview.chromium.org/2247213004
    Cr-Commit-Position: refs/heads/master@{#412225}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/ProgressIndicatorView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/ProgressIndicatorView.java
@@ -76,7 +76,7 @@ public class ProgressIndicatorView extends ImageView {
         mProgressDrawable.stop();
         removeCallbacks(mShowSpinnerRunnable);
         mPostedCallback = false;
-        setVisibility(View.INVISIBLE);
+        setVisibility(View.GONE);
     }
 
     public void showDelayed() {
