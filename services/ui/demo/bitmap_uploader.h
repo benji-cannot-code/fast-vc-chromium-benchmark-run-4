f45cfdbfdaf800940d185b304b67cef1f2c12346
sadrul <sadrul@chromium.org>
2016-08-15 17:19:45 -0700

    services/ui: Move BitmapUploader into mus_demo.
    Only mus_demo uses BitmapUploader. So move it out of the client-lib into
    mus_demo.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2247023002
    Cr-Commit-Position: refs/heads/master@{#412109}

--- services/ui/demo/bitmap_uploader.h
+++ services/ui/demo/bitmap_uploader.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef SERVICES_UI_PUBLIC_CPP_BITMAP_UPLOADER_H_
-#define SERVICES_UI_PUBLIC_CPP_BITMAP_UPLOADER_H_
+#ifndef SERVICES_UI_DEMO_BITMAP_UPLOADER_H_
+#define SERVICES_UI_DEMO_BITMAP_UPLOADER_H_
 
 #include <stdint.h>
 
@@ -84,4 +84,4 @@ class BitmapUploader : public WindowSurfaceClient {
 
 }  // namespace ui
 
-#endif  // SERVICES_UI_PUBLIC_CPP_BITMAP_UPLOADER_H_
+#endif  // SERVICES_UI_DEMO_BITMAP_UPLOADER_H_
