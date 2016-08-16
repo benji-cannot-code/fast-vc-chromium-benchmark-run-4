9d9306948155583af07db5e84599cf67a928ae0c
pkotwicz <pkotwicz@chromium.org>
2016-08-16 10:57:01 -0700

    Fix "Add" button in add-to-homescreen dialog never getting enabled
    This is a regression due to https://codereview.chromium.org/2247633002/
    
    BUG=638095
    
    Review-Url: https://codereview.chromium.org/2250113002
    Cr-Commit-Position: refs/heads/master@{#412288}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/AddToHomescreenDialogHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/AddToHomescreenDialogHelper.java
@@ -74,8 +74,8 @@ public class AddToHomescreenDialogHelper {
 
     @CalledByNative
     private void onIconAvailable(Bitmap icon) {
-        mObserver.onIconAvailable(icon);
         mIsInitialized = true;
+        mObserver.onIconAvailable(icon);
     }
 
     /**
