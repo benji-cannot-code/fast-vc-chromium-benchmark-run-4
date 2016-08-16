db4812bde9c10946102714e56aaf2dfb7904cc07
chfremer <chfremer@chromium.org>
2016-08-16 10:55:16 -0700

    This is a follow-up to the skeleton CL [1] which is currently under review. For the bigger picture, please refer to the design document at [2].
    * Added BUILD.gn for building the video_capture bindings, skeleton implementation, and package it as a Mojo Shell Service.
    * Added a simple Shell Service Test to confirm that the service can be reached and responds with a callback.
    * Hooked the build into gn_only section of the main BUILD.gn file
    
    BUG=584797
    TEST=Build of gn_only succeeds. Test in video_capture_unittests passes.
    
    [1] https://codereview.chromium.org/2155303002/
    [2] https://docs.google.com/a/chromium.org/document/d/1Qw7rw1AJy0QHXjha36jZNiEuxsxWslJ_X-zpOhijvI8/edit?usp=sharing
    
    Review-Url: https://codereview.chromium.org/2224103002
    Cr-Commit-Position: refs/heads/master@{#412286}

---
+++ services/video_capture/service_main.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/shell/public/c/main.h"
+#include "services/shell/public/cpp/service_runner.h"
+#include "services/video_capture/video_capture_service.h"
+
+MojoResult ServiceMain(MojoHandle service_request_handle) {
+  return shell::ServiceRunner(new video_capture::VideoCaptureService)
+      .Run(service_request_handle);
+}
