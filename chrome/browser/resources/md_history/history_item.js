a30508cb9d9d3cef32ed850c0bf7b1e29ce70d6f
calamity <calamity@chromium.org>
2016-08-15 18:50:57 -0700

    [MD History] Fix the 'Remove from history' button.
    This CL fixes the 'Remove from history' button which was broken by
    r411253 and adds a test.
    
    BUG=637199
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2246033002
    Cr-Commit-Position: refs/heads/master@{#412129}

--- chrome/browser/resources/md_history/history_item.js
+++ chrome/browser/resources/md_history/history_item.js
@@ -72,6 +72,7 @@ cr.define('md_history', function() {
       this.fire('toggle-menu', {
         target: Polymer.dom(e).localTarget,
         item: this.item,
+        path: this.path,
       });
 
       // Stops the 'tap' event from closing the menu when it opens.
