f8f606100a006df1906b6f4596c484f868bad7ab
sadrul <sadrul@chromium.org>
2016-08-15 20:16:32 -0700

    views/mus: Use the GpuMemoryBufferManager from GpuService.
    Instead of creating a new MojoGpuMemoryBufferManager in SurfaceContextFactory,
    use the one created by ui::GpuService.
    
    BUG=637920
    
    Review-Url: https://codereview.chromium.org/2247123002
    Cr-Commit-Position: refs/heads/master@{#412155}

--- ui/views/mus/surface_context_factory.cc
+++ ui/views/mus/surface_context_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/output/output_surface.h"
 #include "cc/resources/shared_bitmap_manager.h"
 #include "cc/surfaces/surface_id_allocator.h"
+#include "services/ui/public/cpp/gpu_service.h"
 #include "services/ui/public/cpp/output_surface.h"
 #include "services/ui/public/cpp/window.h"
 #include "ui/compositor/reflector.h"
@@ -82,7 +83,7 @@ cc::SharedBitmapManager* SurfaceContextFactory::GetSharedBitmapManager() {
 
 gpu::GpuMemoryBufferManager*
 SurfaceContextFactory::GetGpuMemoryBufferManager() {
-  return &gpu_memory_buffer_manager_;
+  return gpu_service_->gpu_memory_buffer_manager();
 }
 
 cc::TaskGraphRunner* SurfaceContextFactory::GetTaskGraphRunner() {
--- ui/views/mus/surface_context_factory.h
+++ ui/views/mus/surface_context_factory.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/macros.h"
-#include "services/ui/public/cpp/mojo_gpu_memory_buffer_manager.h"
 #include "services/ui/public/cpp/raster_thread_helper.h"
 #include "services/ui/public/interfaces/window_tree.mojom.h"
 #include "ui/compositor/compositor.h"
@@ -58,7 +57,6 @@ class VIEWS_MUS_EXPORT SurfaceContextFactory : public ui::ContextFactory {
 
   uint32_t next_surface_id_namespace_;
   ui::RasterThreadHelper raster_thread_helper_;
-  ui::MojoGpuMemoryBufferManager gpu_memory_buffer_manager_;
   ui::GpuService* gpu_service_;
 
   DISALLOW_COPY_AND_ASSIGN(SurfaceContextFactory);
