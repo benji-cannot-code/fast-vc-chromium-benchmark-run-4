3b3d4ff6b54a5f90f193939d3a602ebf25637a90
eugenebut <eugenebut@chromium.org>
2016-08-15 18:44:42 -0700

    [ios] Added comments for JavaScript execution API.
    Comments explain the result type of JS execution.
    
    BUG=595761
    
    Review-Url: https://codereview.chromium.org/2241013002
    Cr-Commit-Position: refs/heads/master@{#412125}

--- ios/web/public/test/js_test_util.h
+++ ios/web/public/test/js_test_util.h
@@ -14,6 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web {
 
+// These functions synchronously execute JavaScript and return result as id.
+// id will be backed up by different classes depending on resulting JS type:
+// NSString (string), NSNumber (number or boolean), NSDictionary (object),
+// NSArray (array), NSNull (null), NSDate (Date), nil (undefined or execution
+// exception).
+
 // Executes JavaScript on the |manager| and returns the result as an id.
 id ExecuteJavaScript(CRWJSInjectionManager* manager, NSString* script);
 
