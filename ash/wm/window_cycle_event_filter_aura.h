c277bd788e0fdf19e6a7227bd6ce0ab1efc173bf
estade <estade@chromium.org>
2016-08-16 08:32:03 -0700

    Ash - Make Alt+Tab repeatable
    Continuously cycle through windows while Alt+Tab is held.
    
    BUG=626111
    
    Review-Url: https://codereview.chromium.org/2252503002
    Cr-Commit-Position: refs/heads/master@{#412248}

--- ash/wm/window_cycle_event_filter_aura.h
+++ ash/wm/window_cycle_event_filter_aura.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/common/wm/window_cycle_event_filter.h"
 #include "base/macros.h"
+#include "base/timer/timer.h"
 #include "ui/events/event_handler.h"
 
 namespace ash {
@@ -22,6 +23,12 @@ class WindowCycleEventFilterAura : public ui::EventHandler,
   void OnKeyEvent(ui::KeyEvent* event) override;
 
  private:
+  // When the user holds Alt+Tab, this timer is used to send repeated
+  // cycle commands to WindowCycleController. Note this is not accomplished
+  // by marking the Alt+Tab accelerator as "repeatable" in the accelerator
+  // table because we wish to control the repeat interval.
+  base::RepeatingTimer repeat_timer_;
+
   DISALLOW_COPY_AND_ASSIGN(WindowCycleEventFilterAura);
 };
 
