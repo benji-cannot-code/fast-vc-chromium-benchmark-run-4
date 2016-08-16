a69c4be0a85ca52fee9e22ec3d1a7301f2eb1558
twellington <twellington@chromium.org>
2016-08-16 14:25:44 -0700

    [Downloads] Add save offline button to tablet toolbar
    When the downloads UI is enabled, a button to save the current
    web page offline takes the place of the microphone button in the
    unfocused omnibox (on non-incognito, non-native tabs).
    When the omnibox is focused, if voice search is enabled, the mic
    button is shown.
    
    This change also only shows the bookmark button when the omnibox
    is unfocused (if the downloads UI is enabled).
    
    BUG=616324
    
    Review-Url: https://codereview.chromium.org/2249773003
    Cr-Commit-Position: refs/heads/master@{#412340}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarTablet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarTablet.java
@@ -23,6 +23,7 @@ import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.NavigationPopup;
 import org.chromium.chrome.browser.device.DeviceClassManager;
 import org.chromium.chrome.browser.ntp.NewTabPage;
+import org.chromium.chrome.browser.offlinepages.downloads.OfflinePageDownloadBridge;
 import org.chromium.chrome.browser.omnibox.LocationBar;
 import org.chromium.chrome.browser.omnibox.LocationBarTablet;
 import org.chromium.chrome.browser.partnercustomizations.HomepageManager;
@@ -47,6 +48,7 @@ public class ToolbarTablet extends ToolbarLayout implements OnClickListener {
     private TintedImageButton mForwardButton;
     private TintedImageButton mReloadButton;
     private TintedImageButton mBookmarkButton;
+    private TintedImageButton mSaveOfflineButton;
     private ImageButton mAccessibilitySwitcherButton;
 
     private OnClickListener mBookmarkListener;
@@ -72,6 +74,10 @@ public class ToolbarTablet extends ToolbarLayout implements OnClickListener {
     private AnimatorSet mButtonVisibilityAnimators;
 
     private NewTabPage mVisibleNtp;
+
+    // TODO(twellington): Once incognito is supported, we will need one OfflinePageDownloadBridges
+    // for each profile (normal and incognito).
+    private OfflinePageDownloadBridge mOfflinePageDownloadBridge;
 
     /**
      * Constructs a ToolbarTablet object.
@@ -117,6 +123,8 @@ public class ToolbarTablet extends ToolbarLayout implements OnClickListener {
             ApiCompatibilityUtils.setPaddingRelative((View) mMenuButtonWrapper.getParent(), 0, 0,
                     getResources().getDimensionPixelSize(R.dimen.tablet_toolbar_end_padding), 0);
         }
+
+        mSaveOfflineButton = (TintedImageButton) findViewById(R.id.save_offline_button);
 
         // Initialize values needed for showing/hiding toolbar buttons when the activity size
         // changes.
@@ -237,6 +245,8 @@ public class ToolbarTablet extends ToolbarLayout implements OnClickListener {
         if (HomepageManager.isHomepageEnabled(getContext())) {
             mHomeButton.setVisibility(VISIBLE);
         }
+
+        mSaveOfflineButton.setOnClickListener(this);
     }
 
     @Override
@@ -301,6 +311,13 @@ public class ToolbarTablet extends ToolbarLayout implements OnClickListener {
                 cancelAppMenuUpdateBadgeAnimation();
                 mTabSwitcherListener.onClick(mAccessibilitySwitcherButton);
             }
+        } else if (mSaveOfflineButton == v) {
+            // This is a temporary hookup point to save a page later.
+            Tab tab = getToolbarDataProvider().getTab();
+            if (mOfflinePageDownloadBridge == null) {
+                mOfflinePageDownloadBridge = new OfflinePageDownloadBridge(tab.getProfile());
+            }
+            mOfflinePageDownloadBridge.startDownload(tab);
         }
     }
 
@@ -326,6 +343,7 @@ public class ToolbarTablet extends ToolbarLayout implements OnClickListener {
             mHomeButton.setTint(incognito ? mLightModeTint : mDarkModeTint);
             mBackButton.setTint(incognito ? mLightModeTint : mDarkModeTint);
             mForwardButton.setTint(incognito ? mLightModeTint : mDarkModeTint);
+            mSaveOfflineButton.setTint(incognito ? mLightModeTint : mDarkModeTint);
             if (incognito) {
                 mLocationBar.getContainerView().getBackground().setAlpha(
                         ToolbarPhone.LOCATION_BAR_TRANSPARENT_BACKGROUND_ALPHA);
@@ -372,6 +390,11 @@ public class ToolbarTablet extends ToolbarLayout implements OnClickListener {
     protected void onTabContentViewChanged() {
         super.onTabContentViewChanged();
         updateNtp();
+    }
+
+    @Override
+    public void updateButtonVisibility() {
+        mLocationBar.updateButtonVisibility();
     }
 
     @Override
