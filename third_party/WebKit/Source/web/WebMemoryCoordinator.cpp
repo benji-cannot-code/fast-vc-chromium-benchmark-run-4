9349470e5bdcfa81cd08a12924c91bd1540e6137
boliu <boliu@chromium.org>
2016-08-16 10:30:52 -0700

    Revert of Add MemoryCoordinatorClient (patchset #3 id:40001 of https://codereview.chromium.org/2230433002/ )
    Reason for revert:
    Null pointer crash. See crbug.com/638274
    
    BUG=638274
    
    Original issue's description:
    > Add MemoryCoordinatorClient
    >
    > Also move MemoryCoordinator from core/dom to platform/. This is a preparation for experimenting purge and suspend [1,2].
    >
    > [1] https://docs.google.com/document/d/1EgLimgxWK5DGhptnNVbEGSvVn6Q609ZJaBkLjEPRJvI/edit
    > [2] https://codereview.chromium.org/2130683002/
    >
    > Committed: https://crrev.com/75ce9c3e2c15f2df215184026162dc505df37e4b
    > Cr-Commit-Position: refs/heads/master@{#412171}
    
    TBR=esprehn@chromium.org,haraken@chromium.org,tasak@google.com,bashi@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2252743002
    Cr-Commit-Position: refs/heads/master@{#412276}

--- third_party/WebKit/Source/web/WebMemoryCoordinator.cpp
+++ third_party/WebKit/Source/web/WebMemoryCoordinator.cpp
@@ -3,20 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "public/platform/WebMemoryCoordinator.h"
+#include "public/web/WebMemoryCoordinator.h"
 
-#include "platform/MemoryCoordinator.h"
+#include "core/dom/MemoryCoordinator.h"
+#include "core/page/Page.h"
 
 namespace blink {
 
 void WebMemoryCoordinator::onMemoryPressure(WebMemoryPressureLevel pressureLevel)
 {
     MemoryCoordinator::instance().onMemoryPressure(pressureLevel);
-}
-
-void WebMemoryCoordinator::purgeMemory()
-{
-    MemoryCoordinator::instance().purgeMemory();
 }
 
 } // namespace blink
