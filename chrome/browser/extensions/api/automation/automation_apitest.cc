f4e90d9729672a8eec3aed07528e74dd0fe190b8
sashab <sashab@chromium.org>
2016-08-15 18:48:00 -0700

    Disable AutomationApiTest.Desktop (flaky)
    Disable AutomationApiTest.Desktop since it's flaky.
    
    TBR=dtseng@chromium.org,vabr@chromium.org
    BUG=637525
    
    Review-Url: https://codereview.chromium.org/2247143002
    Cr-Commit-Position: refs/heads/master@{#412127}

--- chrome/browser/extensions/api/automation/automation_apitest.cc
+++ chrome/browser/extensions/api/automation/automation_apitest.cc
@@ -155,7 +155,8 @@ IN_PROC_BROWSER_TEST_F(AutomationApiTest, TabsAutomationHostsPermissions) {
 }
 
 #if defined(USE_AURA)
-IN_PROC_BROWSER_TEST_F(AutomationApiTest, Desktop) {
+// Flaky, see http://crbug.com/637525
+IN_PROC_BROWSER_TEST_F(AutomationApiTest, DISABLED_Desktop) {
   ASSERT_TRUE(RunExtensionSubtest("automation/tests/desktop", "desktop.html"))
       << message_;
 }
