14a1b7ec3ac5758b6e6e7ee5663948fa07aa0ad6
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-08-17 16:23:58 -0700

    [Extensions] Make ExtensionAPI use unique_ptrs instead of linked_ptrs
    For the SchemaMap, use unique_ptrs instead of linked_ptrs. Also clean
    up the implementation a little.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2255953002
    Cr-Commit-Position: refs/heads/master@{#412688}

--- extensions/common/extension_api.h
+++ extensions/common/extension_api.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
-#include "base/memory/linked_ptr.h"
 #include "base/memory/singleton.h"
 #include "base/strings/string_piece.h"
 #include "base/values.h"
@@ -146,8 +145,8 @@ class ExtensionAPI {
   UnloadedSchemaMap unloaded_schemas_;
 
   // Schemas for each namespace.
-  typedef std::map<std::string, linked_ptr<const base::DictionaryValue> >
-        SchemaMap;
+  using SchemaMap =
+      std::map<std::string, std::unique_ptr<const base::DictionaryValue>>;
   SchemaMap schemas_;
 
   // FeatureProviders used for resolving dependencies.
