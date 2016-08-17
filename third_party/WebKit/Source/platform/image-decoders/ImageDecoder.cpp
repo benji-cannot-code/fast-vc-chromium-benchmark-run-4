99f3452ed8decce8f167fca42bea88dcb0eef0c8
fmalita <fmalita@chromium.org>
2016-08-17 13:56:39 -0700

    Fix fragmented image signature handling
    ImageDecoder::determineImageType() is currently only examining the first
    buffer segment (getSomeData).  But the signature is not guaranteed to be contiguous,
    nor do we ever re-consolidate the buffer after receiving more data.
    
    As a consequence, when the signature is fragmented (e.g. due to a slow
    image load), we cannot detect the image type - even after all the data is
    later received.
    
    Refactor determineImageType() to consolidate the signature when needed.
    
    BUG=637556
    R=pkasting@chromium.org,scroggo@chromium.org,schenney@chromium.org
    
    Review-Url: https://codereview.chromium.org/2252723003
    Cr-Commit-Position: refs/heads/master@{#412631}

--- third_party/WebKit/Source/platform/image-decoders/ImageDecoder.cpp
+++ third_party/WebKit/Source/platform/image-decoders/ImageDecoder.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/PlatformInstrumentation.h"
 #include "platform/RuntimeEnabledFeatures.h"
 #include "platform/graphics/BitmapImageMetrics.h"
+#include "platform/image-decoders/FastSharedBufferReader.h"
 #include "platform/image-decoders/bmp/BMPImageDecoder.h"
 #include "platform/image-decoders/gif/GIFImageDecoder.h"
 #include "platform/image-decoders/ico/ICOImageDecoder.h"
@@ -108,12 +109,16 @@ std::unique_ptr<ImageDecoder> ImageDecoder::create(SniffResult sniffResult, Alph
     return nullptr;
 }
 
+namespace {
+
+// This needs to be updated if we ever add a matches*Signature() which requires more characters.
+constexpr size_t kLongestSignatureLength = sizeof("RIFF????WEBPVP") - 1;
+
+} // anonymous ns
+
 ImageDecoder::SniffResult ImageDecoder::determineImageType(const char* contents, size_t length)
 {
-    const size_t longestSignatureLength = sizeof("RIFF????WEBPVP") - 1;
-    DCHECK_EQ(14u, longestSignatureLength);
-
-    if (length < longestSignatureLength)
+    if (length < kLongestSignatureLength)
         return SniffResult::InsufficientData;
     if (matchesJPEGSignature(contents))
         return SniffResult::JPEG;
@@ -132,16 +137,20 @@ ImageDecoder::SniffResult ImageDecoder::determineImageType(const char* contents,
 
 ImageDecoder::SniffResult ImageDecoder::determineImageType(const SharedBuffer& data)
 {
-    const char* contents;
-    const size_t length = data.getSomeData<size_t>(contents);
-    return determineImageType(contents, length);
+    // TODO(fmalita): refactor the method signature to avoid casting.
+    RefPtr<SegmentReader> reader = SegmentReader::createFromSharedBuffer(const_cast<SharedBuffer*>(&data));
+
+    return determineImageType(*reader);
 }
 
 ImageDecoder::SniffResult ImageDecoder::determineImageType(const SegmentReader& data)
 {
-    const char* contents;
-    const size_t length = data.getSomeData(contents, 0);
-    return determineImageType(contents, length);
+    // TODO(fmalita): refactor the method signature to avoid casting.
+    const FastSharedBufferReader fastReader(const_cast<SegmentReader*>(&data));
+    char buffer[kLongestSignatureLength];
+    const size_t len = std::min(kLongestSignatureLength, data.size());
+
+    return determineImageType(fastReader.getConsecutiveData(0, len, buffer), len);
 }
 
 size_t ImageDecoder::frameCount()
