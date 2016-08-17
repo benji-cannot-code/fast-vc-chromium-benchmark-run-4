febf912f5936c73c1375919fd7217c619213b5a9
zhongyi <zhongyi@chromium.org>
2016-08-17 14:57:48 -0700

    Remove unused histogram: Net.QuicSession.HostResolutionTime
    BUG=629384
    
    Review-Url: https://codereview.chromium.org/2256803002
    Cr-Commit-Position: refs/heads/master@{#412659}

--- net/quic/chromium/quic_stream_factory.cc
+++ net/quic/chromium/quic_stream_factory.cc
@@ -349,7 +349,6 @@ class QuicStreamFactory::Job {
   QuicChromiumClientSession* session_;
   CompletionCallback callback_;
   AddressList address_list_;
-  base::TimeTicks dns_resolution_start_time_;
   base::TimeTicks dns_resolution_end_time_;
   base::WeakPtrFactory<Job> weak_factory_;
   DISALLOW_COPY_AND_ASSIGN(Job);
@@ -481,7 +480,6 @@ int QuicStreamFactory::Job::DoResolveHost() {
     server_info_->Start();
 
   io_state_ = STATE_RESOLVE_HOST_COMPLETE;
-  dns_resolution_start_time_ = base::TimeTicks::Now();
   return host_resolver_->Resolve(
       HostResolver::RequestInfo(key_.destination()), DEFAULT_PRIORITY,
       &address_list_,
@@ -490,9 +488,6 @@ int QuicStreamFactory::Job::DoResolveHost() {
 }
 
 int QuicStreamFactory::Job::DoResolveHostComplete(int rv) {
-  dns_resolution_end_time_ = base::TimeTicks::Now();
-  UMA_HISTOGRAM_TIMES("Net.QuicSession.HostResolutionTime",
-                      dns_resolution_end_time_ - dns_resolution_start_time_);
   if (rv != OK)
     return rv;
 
