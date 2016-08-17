c6a0c2d6ccc6a8e84e7707a1a2e8cee9593373dd
kojii <kojii@chromium.org>
2016-08-17 01:56:33 -0700

    Remove unused Font argument from InlineTextBox::constructTextRun
    The "const Font& font" argument is not used in
    InlineTextBox::constructTextRun and all its variants.
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2246383002
    Cr-Commit-Position: refs/heads/master@{#412478}

--- third_party/WebKit/Source/core/inspector/InspectorCSSAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorCSSAgent.cpp
@@ -1031,7 +1031,7 @@ void InspectorCSSAgent::collectPlatformFontsForLayoutObject(LayoutObject* layout
     for (InlineTextBox* box = layoutText->firstTextBox(); box; box = box->nextTextBox()) {
         const ComputedStyle& style = layoutText->styleRef(box->isFirstLineStyle());
         const Font& font = style.font();
-        TextRun run = box->constructTextRunForInspector(style, font);
+        TextRun run = box->constructTextRunForInspector(style);
         TextRunPaintInfo paintInfo(run);
         GlyphBuffer glyphBuffer;
         font.buildGlyphBuffer(paintInfo, glyphBuffer);
