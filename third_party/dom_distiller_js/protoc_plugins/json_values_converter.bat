6ba67bf66d3a10bfe275ee8c92480de89c12a0b4
wychen <wychen@chromium.org>
2016-08-16 20:54:11 -0700

    Generate the proto JSON converter for DOM distiller
    The protoc_plugins files are from:
    https://github.com/chromium/dom-distiller/tree/master/protoc_plugins
    
    This goes along with https://codereview.chromium.org/2039043003/
    
    Roll DOM Distiller JavaScript distribution package
    
        Diff since last roll:
        https://github.com/chromium/dom-distiller/compare/91f9f016e0...b21e6a334a
    
        Picked up changes:
        b21e6a3 Remove //base dependency
    
    BUG=572585
    
    Review-Url: https://codereview.chromium.org/2034373002
    Cr-Commit-Position: refs/heads/master@{#412445}

---
+++ third_party/dom_distiller_js/protoc_plugins/json_values_converter.bat
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+@echo off
+
+set PYTHONPATH=..\..\third_party\protobuf\python;..\..\third_party\protobuf\third_party\six
+call python %~dp0\json_values_converter.py
