2d561df76de0904990d13cf87a1eb2187ba5b442
peconn <peconn@chromium.org>
2016-08-17 09:27:33 -0700

    Add UMA for signin through NTP content suggestions.
    BUG=636523
    
    Review-Url: https://codereview.chromium.org/2230183005
    Cr-Commit-Position: refs/heads/master@{#412554}

--- chrome/browser/ui/webui/signin/inline_login_handler.cc
+++ chrome/browser/ui/webui/signin/inline_login_handler.cc
@@ -178,6 +178,10 @@ void InlineLoginHandler::RecordSigninUserActionForAccessPoint(
       content::RecordAction(
           base::UserMetricsAction("Signin_Signin_FromAutofillDropdown"));
       break;
+    case signin_metrics::AccessPoint::ACCESS_POINT_NTP_CONTENT_SUGGESTIONS:
+      content::RecordAction(
+          base::UserMetricsAction("Signin_Signin_FromNTPContentSuggestions"));
+      break;
     case signin_metrics::AccessPoint::ACCESS_POINT_MAX:
       NOTREACHED();
       break;
