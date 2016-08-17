9858a43d416ed9289744a391c3d500c341a31a60
dalecurtis <dalecurtis@chromium.org>
2016-08-17 15:15:23 -0700

    Add watch time metrics for HTML5 media playback.
    Watch time for our purposes is defined as the amount of elapsed media time
    for audio+video media. A minimum of 7 seconds of unmuted, foreground media
    must be watched to start watch time monitoring. Watch time is checked every
    5 seconds from then on and reported to multiple buckets: All, MSE, SRC, EME,
    AC, and battery.
    
    Any one of paused, hidden, or muted is sufficient to stop watch time metric
    reports. Each of these has a hysteresis where if the state change is undone
    within 5 seconds, the watch time will be counted as uninterrupted.
    
    Power events (on/off battery power) have a similar hysteresis, but unlike
    the aforementioned properties, will not stop metric collection.
    
    Each seek event will result in a new watch time metric being started and the
    old metric finalized as accurately as possible.
    
    BUG=633743
    TEST=new unittest.
    
    Review-Url: https://codereview.chromium.org/2160963002
    Cr-Commit-Position: refs/heads/master@{#412664}

--- media/blink/webmediaplayer_impl.h
+++ media/blink/webmediaplayer_impl.h
@@ -73,6 +73,7 @@ class GpuVideoAcceleratorFactories;
 class MediaLog;
 class UrlIndex;
 class VideoFrameCompositor;
+class WatchTimeReporter;
 class WebAudioSourceProviderImpl;
 class WebMediaPlayerDelegate;
 class WebTextTrackImpl;
@@ -350,6 +351,8 @@ class MEDIA_BLINK_EXPORT WebMediaPlayerImpl
   // paused state after some idle timeout.
   void ScheduleIdlePauseTimer();
 
+  void CreateWatchTimeReporter();
+
   blink::WebLocalFrame* frame_;
 
   // The playback state last reported to |delegate_|, to avoid setting duplicate
@@ -535,6 +538,10 @@ class MEDIA_BLINK_EXPORT WebMediaPlayerImpl
   // Called some-time after OnHidden() if the media was suspended in a playing
   // state as part of the call to OnHidden().
   base::OneShotTimer background_pause_timer_;
+
+  // Monitors the watch time of the played content.
+  std::unique_ptr<WatchTimeReporter> watch_time_reporter_;
+  bool is_encrypted_;
 
   DISALLOW_COPY_AND_ASSIGN(WebMediaPlayerImpl);
 };
