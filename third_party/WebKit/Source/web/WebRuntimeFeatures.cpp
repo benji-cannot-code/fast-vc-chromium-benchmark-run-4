f4e5e6815aa6b47829d8497f0f34b4a4f5ac0a72
iclelland <iclelland@chromium.org>
2016-08-17 08:25:07 -0700

    [Feature Policy] Add command-line flag and about://flags interface for Feature Policy
    BUG=638238
    
    Review-Url: https://codereview.chromium.org/2248083002
    Cr-Commit-Position: refs/heads/master@{#412534}

--- third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
@@ -141,6 +141,11 @@ void WebRuntimeFeatures::enableFastMobileScrolling(bool enable)
     RuntimeEnabledFeatures::setFastMobileScrollingEnabled(enable);
 }
 
+void WebRuntimeFeatures::enableFeaturePolicy(bool enable)
+{
+    RuntimeEnabledFeatures::setFeaturePolicyEnabled(enable);
+}
+
 void WebRuntimeFeatures::enableFileSystem(bool enable)
 {
     RuntimeEnabledFeatures::setFileSystemEnabled(enable);
