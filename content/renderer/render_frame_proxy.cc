8d201c767f60d947b8122ef5c7a13d399e3d1794
lfg <lfg@chromium.org>
2016-08-17 14:18:42 -0700

    Use the correct scale factor when rendering RemoteFrames with UseZoomForDSF.
    The view bounds reported by RenderWidgetHostViewChildFrame must return the frame size without the device scale factor applied.
    
    BUG=621020
    
    Review-Url: https://codereview.chromium.org/2248823003
    Cr-Commit-Position: refs/heads/master@{#412639}

--- content/renderer/render_frame_proxy.cc
+++ content/renderer/render_frame_proxy.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/child/web_url_request_util.h"
 #include "content/child/webmessageportchannel_impl.h"
 #include "content/common/content_security_policy_header.h"
+#include "content/common/content_switches_internal.h"
 #include "content/common/frame_messages.h"
 #include "content/common/frame_owner_properties.h"
 #include "content/common/frame_replication_state.h"
@@ -439,10 +440,9 @@ void RenderFrameProxy::forwardPostMessage(
   Send(new FrameHostMsg_RouteMessageEvent(routing_id_, params));
 }
 
-void RenderFrameProxy::initializeChildFrame(
-    float scale_factor) {
+void RenderFrameProxy::initializeChildFrame() {
   Send(new FrameHostMsg_InitializeChildFrame(
-      routing_id_, scale_factor));
+      routing_id_, render_widget_->GetOriginalDeviceScaleFactor()));
 }
 
 void RenderFrameProxy::navigate(const blink::WebURLRequest& request,
@@ -468,7 +468,12 @@ void RenderFrameProxy::forwardInputEvent(const blink::WebInputEvent* event) {
 }
 
 void RenderFrameProxy::frameRectsChanged(const blink::WebRect& frame_rect) {
-  Send(new FrameHostMsg_FrameRectChanged(routing_id_, frame_rect));
+  gfx::Rect rect = frame_rect;
+  if (IsUseZoomForDSFEnabled()) {
+    rect = gfx::ScaleToEnclosingRect(
+        rect, 1.f / render_widget_->GetOriginalDeviceScaleFactor());
+  }
+  Send(new FrameHostMsg_FrameRectChanged(routing_id_, rect));
 }
 
 void RenderFrameProxy::visibilityChanged(bool visible) {
