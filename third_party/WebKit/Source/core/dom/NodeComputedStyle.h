682c2da3968302e4fabc3bed709e9973cf8a24cb
bugsnash <bugsnash@chromium.org>
2016-08-17 01:04:02 -0700

    Fixed crash by replacing DCHECK with returning nullptr.
    Fixed crash  by replacing DCHECK in Node::mutableComputedStyle with returning nullptr if the NodeRareData is not an ElementRareData.
    
    Crash introcuded in https://codereview.chromium.org/1962953002.
    
    BUG=638102
    
    Review-Url: https://codereview.chromium.org/2241193005
    Cr-Commit-Position: refs/heads/master@{#412472}

--- third_party/WebKit/Source/core/dom/NodeComputedStyle.h
+++ third_party/WebKit/Source/core/dom/NodeComputedStyle.h
@@ -53,7 +53,8 @@ inline ComputedStyle* Node::mutableComputedStyle() const
         return nonLayoutObjectComputedStyle();
     if (hasRareData()) {
         NodeRareData* rareData = this->rareData();
-        DCHECK(rareData->isElementRareData());
+        if (!rareData->isElementRareData())
+            return nullptr;
         return static_cast<ElementRareData*>(rareData)->computedStyle();
     }
     return m_data.m_computedStyle;
