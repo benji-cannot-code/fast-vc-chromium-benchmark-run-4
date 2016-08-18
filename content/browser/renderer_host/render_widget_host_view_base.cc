6421ddaff96129386c4ceda4b6066e10577d0a11
ekaramad <ekaramad@chromium.org>
2016-08-18 14:44:53 -0700

    Track text selection on the browser side (Mac)
    This CL will mimic the code for aura in using TextInputManager for tracking the
    text selection information on the browser side. The CL also enables the corresponding
    ui test in Mac.
    
    BUG=578168,602723
    
    Review-Url: https://codereview.chromium.org/2240553003
    Cr-Commit-Position: refs/heads/master@{#412938}

--- content/browser/renderer_host/render_widget_host_view_base.cc
+++ content/browser/renderer_host/render_widget_host_view_base.cc
@@ -65,6 +65,19 @@ RenderWidgetHostViewBase::~RenderWidgetHostViewBase() {
     text_input_manager_->Unregister(this);
 }
 
+RenderWidgetHostImpl* RenderWidgetHostViewBase::GetFocusedWidget() const {
+  RenderWidgetHostImpl* host =
+      RenderWidgetHostImpl::From(GetRenderWidgetHost());
+
+  return host && host->delegate()
+             ? host->delegate()->GetFocusedRenderWidgetHost(host)
+             : nullptr;
+}
+
+RenderWidgetHost* RenderWidgetHostViewBase::GetRenderWidgetHost() const {
+  return nullptr;
+}
+
 void RenderWidgetHostViewBase::NotifyObserversAboutShutdown() {
   // Note: RenderWidgetHostInputEventRouter is an observer, and uses the
   // following notification to remove this view from its surface owners map.
@@ -122,7 +135,7 @@ void RenderWidgetHostViewBase::SelectionChanged(const base::string16& text,
 // TODO(ekaramad): Use TextInputManager code paths for IME on other platforms.
 // Also, remove the following local variables when that happens
 // (https://crbug.com/578168 and https://crbug.com/602427).
-#if defined(USE_AURA)
+#if !defined(OS_ANDROID)
   if (GetTextInputManager())
     GetTextInputManager()->SelectionChanged(this, text, offset, range);
 #else
@@ -155,7 +168,7 @@ base::string16 RenderWidgetHostViewBase::GetSelectedText() {
 // TODO(ekaramad): Use TextInputManager code paths for IME on other platforms.
 // Also, remove the following local variables when that happens
 // (https://crbug.com/578168 and https://crbug.com/602427).
-#if defined(USE_AURA)
+#if !defined(OS_ANDROID)
   if (!GetTextInputManager())
     return base::string16();
 
