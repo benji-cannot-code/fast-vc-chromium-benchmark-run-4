e006b5706f2977bcb50aa6012650057f428ff73b
peria <peria@chromium.org>
2016-08-18 08:36:38 -0700

    Add necessary includes
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2258703003
    Cr-Commit-Position: refs/heads/master@{#412838}

--- third_party/WebKit/Source/modules/csspaint/PaintSize.h
+++ third_party/WebKit/Source/modules/csspaint/PaintSize.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PaintSize_h
 #define PaintSize_h
 
+#include "bindings/core/v8/ScriptWrappable.h"
+#include "platform/geometry/IntSize.h"
+#include "platform/heap/Handle.h"
+
 namespace blink {
 
 class PaintSize : public GarbageCollectedFinalized<PaintSize>, public ScriptWrappable {
