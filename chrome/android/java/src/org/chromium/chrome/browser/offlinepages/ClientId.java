ee4041ff8a3c4befb124dd6ee511bb7cc66a2e61
weiran <weiran@google.com>
2016-08-18 16:31:21 -0700

    [Offline Page]Enable share offline page from online tab
    The primary implementation in Issue 2081153005 only trigger offline page sharing if user is currently on an offline page. This branch enables sharing offline copy of the page if user is on an online webpage.
    
    BUG=622139
    
    Review-Url: https://codereview.chromium.org/2202423004
    Cr-Commit-Position: refs/heads/master@{#412971}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/ClientId.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/ClientId.java
@@ -7,6 +7,8 @@ package org.chromium.chrome.browser.offlinepages;
 
 import org.chromium.components.bookmarks.BookmarkId;
 
+import java.util.UUID;
+
 /**
  * Object to hold a client identifier for an offline page.
  */
@@ -49,5 +51,15 @@ public class ClientId {
     public static ClientId createClientIdForBookmarkId(BookmarkId id) {
         if (id == null) return null;
         return new ClientId(OfflinePageBridge.BOOKMARK_NAMESPACE, id.toString());
+    }
+
+    /**
+     * Create a client id for a tab when its contents need to be saved for sharing purposes.
+     * @param id The tab id to wrap.
+     * @return A {@link ClientId} that represents this TabId.
+     */
+    public static ClientId createClientIdForTabSharing() {
+        String uuid = UUID.randomUUID().toString();
+        return new ClientId(OfflinePageBridge.SHARE_NAMESPACE, uuid);
     }
 }
