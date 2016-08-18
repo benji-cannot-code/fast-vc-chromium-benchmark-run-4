9dcbe30ea2ab96f18cef09836491e99a69fcb916
peconn <peconn@chromium.org>
2016-08-18 12:03:35 -0700

    Create Java infrastructure for UI Render Tests.
    Creates the first part of the infrastructure that will allow us to test for
    any regressions to the Chrome on Android UI. This includes utilities to save
    Views to file, an annotation (RenderTest) to identify the instrumentation
    tests that use this and two examples of their use.
    
    The aim is to follow this up with host side code that grabs the images after
    the tests have been run and performs pixel comparisons on them against golden
    images stored in the repository.
    
    BUG=614305
    
    Review-Url: https://codereview.chromium.org/2114963002
    Cr-Commit-Position: refs/heads/master@{#412905}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/LogoBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/LogoBridge.java
@@ -15,12 +15,12 @@ import jp.tomorrowkey.android.gifplayer.BaseGifImage;
 /**
  * Provides access to the search provider's logo via the C++ LogoService.
  */
-class LogoBridge {
+public class LogoBridge {
 
     /**
      * A logo for a search provider (e.g. the Yahoo! logo or Google doodle).
      */
-    static class Logo {
+    public static class Logo {
         /**
          * The logo image. Non-null.
          */
@@ -52,7 +52,7 @@ class LogoBridge {
     /**
      * Observer for receiving the logo when it's available.
      */
-    interface LogoObserver {
+    public interface LogoObserver {
         /**
          * Called when the cached or fresh logo is available. This may be called up to two times,
          * once with the cached logo and once with a freshly downloaded logo.
@@ -67,7 +67,7 @@ class LogoBridge {
     /**
      * A callback that is called when the animated logo is successfully downloaded.
      */
-    interface AnimatedLogoCallback {
+    public interface AnimatedLogoCallback {
 
         /**
          * Called when the animated GIF logo is successfully downloaded.
@@ -85,7 +85,7 @@ class LogoBridge {
      *
      * @param profile Profile of the tab that will show the logo.
      */
-    LogoBridge(Profile profile) {
+    public LogoBridge(Profile profile) {
         mNativeLogoBridge = nativeInit(profile);
     }
 
