26550bb987f1637f91533d6b4b1bb12babc555d7
dfalcantara <dfalcantara@chromium.org>
2016-08-18 17:30:56 -0700

    [Downloads] Add basic DownloadHistoryAdapterTests
    * Stub out calls from the DownloadHistoryAdapter to the
      backends.
    
    * Add a DownloadHistoryAdapterTest that tests basic things
      about how the Adapter is expected to work, including
      initialization, dealing with Incognito tabs, alerts
      about new downloads, and alerts about removed downloads.
    
    BUG=616324
    
    Review-Url: https://codereview.chromium.org/2247923003
    Cr-Commit-Position: refs/heads/master@{#412990}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryAdapter.java
@@ -32,6 +32,7 @@ import org.chromium.chrome.browser.offlinepages.downloads.OfflinePageDownloadIte
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.util.UrlUtilities;
 import org.chromium.chrome.browser.widget.DateDividedAdapter;
+import org.chromium.chrome.browser.widget.selection.SelectionDelegate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -71,7 +72,7 @@ public class DownloadHistoryAdapter extends DateDividedAdapter implements Downlo
     private final boolean mShowOffTheRecord;
 
     private int mFilter = DownloadFilter.FILTER_ALL;
-    private DownloadManagerUi mManager;
+    private SelectionDelegate<DownloadHistoryItemWrapper> mSelectionDelegate;
     private OfflinePageDownloadBridge mOfflinePageBridge;
     private int mFilenameViewTextColor;
 
@@ -82,14 +83,9 @@ public class DownloadHistoryAdapter extends DateDividedAdapter implements Downlo
 
     @Override
     public void initialize(DownloadManagerUi manager) {
-        manager.addObserver(this);
-        mManager = manager;
-
-        // Get all regular and (if necessary) off the record downloads.
-        getDownloadManagerService().addDownloadHistoryAdapter(this);
-        getDownloadManagerService().getAllDownloads(false);
-        if (mShowOffTheRecord) getDownloadManagerService().getAllDownloads(true);
-
+        if (manager != null) manager.addObserver(this);
+        mSelectionDelegate = getSelectionDelegate(manager);
+        initializeDownloadBridge();
         initializeOfflinePageBridge();
     }
 
@@ -139,7 +135,7 @@ public class DownloadHistoryAdapter extends DateDividedAdapter implements Downlo
     public ViewHolder createViewHolder(ViewGroup parent) {
         View v = LayoutInflater.from(parent.getContext()).inflate(
                 R.layout.download_item_view, parent, false);
-        ((DownloadItemView) v).setSelectionDelegate(mManager.getSelectionDelegate());
+        ((DownloadItemView) v).setSelectionDelegate(mSelectionDelegate);
         return new ItemViewHolder(v);
     }
 
@@ -219,8 +215,8 @@ public class DownloadHistoryAdapter extends DateDividedAdapter implements Downlo
         int index = findItemIndex(list, guid);
         if (index != INVALID_INDEX) {
             DownloadItemWrapper wrapper = list.remove(index);
-            if (mManager.getSelectionDelegate().isItemSelected(wrapper)) {
-                mManager.getSelectionDelegate().toggleSelectionForItem(wrapper);
+            if (mSelectionDelegate != null && mSelectionDelegate.isItemSelected(wrapper)) {
+                mSelectionDelegate.toggleSelectionForItem(wrapper);
             }
             filter(mFilter);
         }
@@ -239,8 +235,12 @@ public class DownloadHistoryAdapter extends DateDividedAdapter implements Downlo
             mOfflinePageBridge.destroy();
             mOfflinePageBridge = null;
         }
+    }
 
-        mManager = null;
+    /** Returns the SelectionDelegate to use for each ViewHolder. */
+    protected SelectionDelegate<DownloadHistoryItemWrapper> getSelectionDelegate(
+            DownloadManagerUi manager) {
+        return manager == null ? null : manager.getSelectionDelegate();
     }
 
     /** Filters the list of downloads to show only files of a specific type. */
@@ -268,7 +268,14 @@ public class DownloadHistoryAdapter extends DateDividedAdapter implements Downlo
         loadItems(mFilteredItems);
     }
 
-    private void initializeOfflinePageBridge() {
+    protected void initializeDownloadBridge() {
+        // Get all regular and (if necessary) off the record downloads.
+        getDownloadManagerService().addDownloadHistoryAdapter(this);
+        getDownloadManagerService().getAllDownloads(false);
+        if (mShowOffTheRecord) getDownloadManagerService().getAllDownloads(true);
+    }
+
+    protected void initializeOfflinePageBridge() {
         mOfflinePageBridge = new OfflinePageDownloadBridge(
                 Profile.getLastUsedProfile().getOriginalProfile());
 
@@ -290,8 +297,8 @@ public class DownloadHistoryAdapter extends DateDividedAdapter implements Downlo
                 int index = findItemIndex(mOfflinePageItems, guid);
                 if (index != INVALID_INDEX) {
                     DownloadHistoryItemWrapper wrapper = mOfflinePageItems.remove(index);
-                    if (mManager.getSelectionDelegate().isItemSelected(wrapper)) {
-                        mManager.getSelectionDelegate().toggleSelectionForItem(wrapper);
+                    if (mSelectionDelegate.isItemSelected(wrapper)) {
+                        mSelectionDelegate.toggleSelectionForItem(wrapper);
                     }
                     updateFilter();
                 }
@@ -333,11 +340,6 @@ public class DownloadHistoryAdapter extends DateDividedAdapter implements Downlo
         return INVALID_INDEX;
     }
 
-    private static DownloadManagerService getDownloadManagerService() {
-        return DownloadManagerService.getDownloadManagerService(
-                ContextUtils.getApplicationContext());
-    }
-
     private void setItemViewStyle(ItemViewHolder holder, DownloadHistoryItemWrapper item) {
         if (mFilenameViewTextColor == 0) {
             // The color is not explicitly set in the XML. Programmatically retrieve the original
@@ -368,4 +370,8 @@ public class DownloadHistoryAdapter extends DateDividedAdapter implements Downlo
         }
     }
 
+    private static DownloadManagerService getDownloadManagerService() {
+        return DownloadManagerService.getDownloadManagerService(
+                ContextUtils.getApplicationContext());
+    }
 }
