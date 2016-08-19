706c086e6233a16e7dc8f2886feee084914439ae
xdai <xdai@chromium.org>
2016-08-19 16:00:35 -0700

    [CUPS] WebUI handler for CUPS printing: Retrieve the printer list from user preference.
    BUG=626752
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2250843002
    Cr-Commit-Position: refs/heads/master@{#413275}

--- chrome/browser/resources/settings/printing_page/cups_printers_browser_proxy.js
+++ chrome/browser/resources/settings/printing_page/cups_printers_browser_proxy.js
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *   printerStatus: string,
  *   printerAddress: string,
  *   printerProtocol: string,
+ *   printerQueue: string
  * }}
  */
 var CupsPrinterInfo;
