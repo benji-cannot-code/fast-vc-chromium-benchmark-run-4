89dab802d489b7cb98c4cdfd7c71df144b023db3
rajendrant <rajendrant@chromium.org>
2016-08-19 14:41:16 -0700

    Send GWS ID to google domains when matching rule is available
    When valid matching rules are available, a synthetic field trial is
    created and GWS ID is set for it. When the control app is
    uninstalled or the matching rules are removed, the synthetic field trial
    is set to disabled group.
    
    BUG=615170
    
    Review-Url: https://codereview.chromium.org/2241133002
    Cr-Commit-Position: refs/heads/master@{#413251}

--- chrome/browser/metrics/chrome_metrics_service_accessor.h
+++ chrome/browser/metrics/chrome_metrics_service_accessor.h
@@ -26,6 +26,9 @@ class FlashDOMHandler;
 
 namespace chrome {
 void AttemptRestart();
+namespace android {
+class ExternalDataUseObserverBridge;
+}
 }
 
 namespace chrome_browser {
@@ -86,6 +89,7 @@ class ChromeMetricsServiceAccessor : public metrics::MetricsServiceAccessor {
   friend class BrowserProcessImpl;
   friend void chrome::AttemptRestart();
   friend void chrome_browser::SetupPreReadFieldTrial();
+  friend class chrome::android::ExternalDataUseObserverBridge;
   friend class ChromeMetricsServicesManagerClient;
   friend class ChromeRenderMessageFilter;
   friend class DataReductionProxyChromeSettings;
