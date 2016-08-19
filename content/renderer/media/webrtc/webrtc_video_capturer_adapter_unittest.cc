a272c883813839de68342f4b20688a03ae7d734d
nisse <nisse@chromium.org>
2016-08-19 02:37:20 -0700

    Delete the class WebRtcVideoCapturerAdapter::MediaVideoFrameFactory
    Instead, use the AdaptFrame and OnFrame methods of the VideoCapturer
    base class.
    
    This cl also applies the new timestamp aligner logic, to translate
    Chrome's camera timestamps into the timescale of rtc::TimeMicros(). No
    other change in behavior is intended.
    
    BUG=webrtc:5682, webrtc:5740, 516700
    
    Review-Url: https://codereview.chromium.org/2244213002
    Cr-Commit-Position: refs/heads/master@{#413093}

--- content/renderer/media/webrtc/webrtc_video_capturer_adapter_unittest.cc
+++ content/renderer/media/webrtc/webrtc_video_capturer_adapter_unittest.cc
@@ -12,17 +12,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 class WebRtcVideoCapturerAdapterTest
-    : public sigslot::has_slots<>,
+    : public rtc::VideoSinkInterface<cricket::VideoFrame>,
       public ::testing::Test {
  public:
   WebRtcVideoCapturerAdapterTest()
       : adapter_(false),
         output_frame_width_(0),
         output_frame_height_(0) {
-    adapter_.SignalFrameCaptured.connect(
-        this, &WebRtcVideoCapturerAdapterTest::OnFrameCaptured);
+    adapter_.AddOrUpdateSink(this, rtc::VideoSinkWants());
   }
-  ~WebRtcVideoCapturerAdapterTest() override {}
+  ~WebRtcVideoCapturerAdapterTest() override {
+    adapter_.RemoveSink(this);
+  }
 
   void TestSourceCropFrame(int capture_width,
                            int capture_height,
@@ -43,11 +44,11 @@ class WebRtcVideoCapturerAdapterTest
     EXPECT_EQ(natural_width, output_frame_width_);
     EXPECT_EQ(natural_height, output_frame_height_);
   }
- protected:
-  void OnFrameCaptured(cricket::VideoCapturer* capturer,
-                       const cricket::CapturedFrame* frame) {
-    output_frame_width_ = frame->width;
-    output_frame_height_ = frame->height;
+
+  // rtc::VideoSinkInterface
+  void OnFrame(const cricket::VideoFrame& frame) override {
+    output_frame_width_ = frame.width();
+    output_frame_height_ = frame.height();
   }
 
  private:
