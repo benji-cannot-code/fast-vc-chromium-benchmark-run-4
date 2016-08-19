36aa5eb61d5718086b5167810ce7ac4646a24197
erikchen <erikchen@chromium.org>
2016-08-19 14:25:51 -0700

    mac: Fix IOSurface clearing for RPDQ CALayer promotion.
    The scissor rect should never affect clearing of the IOSurface.
    
    BUG=638526
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2250883004
    Cr-Commit-Position: refs/heads/master@{#413243}

--- cc/output/gl_renderer.cc
+++ cc/output/gl_renderer.cc
@@ -3968,7 +3968,11 @@ void GLRenderer::CopyRenderPassDrawQuadToOverlayResource(
 
   // Clear to 0 to ensure the background is transparent.
   gl_->ClearColor(0, 0, 0, 0);
+  if (is_scissor_enabled_)
+    gl_->Disable(GL_SCISSOR_TEST);
   gl_->Clear(GL_COLOR_BUFFER_BIT);
+  if (is_scissor_enabled_)
+    gl_->Enable(GL_SCISSOR_TEST);
 
   UpdateRPDQTexturesForSampling(&params);
   UpdateRPDQBlendMode(&params);
