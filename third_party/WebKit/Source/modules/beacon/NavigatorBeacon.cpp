954f6906faae447d9ff044a664d3733797747b2c
tyoshino <tyoshino@chromium.org>
2016-08-18 23:26:38 -0700

    Simplify sendBeacon's allowance enforcement logic
    The CL http://crrev.com/408380 removed the heavy serialization
    (FormData::encodeMultiPartFormData()) from BeaconFormData::serialize().
    Now, it's fine that we call m_entityBody->sizeInBytes() in
    BeaconFormData::size() and finish allowance checking in
    sendBeaconCommon().
    
    This CL:
    - simplifies the serialize() method by removing allowance check
      from serialize().
    - simplifies NavigatorBeacon by moving logic not accessing any
      NavigatorBeacon instance variable to sendBeacon().
    
    Note: I moved the position of mixed content check from above
    serialize() to below it for simplification in http://crrev.com/411630.
    The original motivation of this CL was to investigate if I can preserve
    the old behavior by refactoring the Beacon class. But it turned out
    that serialize() is no longer heavy logic. So, I'd like to keep the
    simplified code. Also, I want to note that the change hasn't impacted
    the allowance enforcement logic. payloadLength is now set even when
    mixed content check fails, but in NavigatorBeacon::beaconResult, the
    result is applied to m_transmittedBytes only when allowed is true.
    
    BUG=490015
    R=japhet,haraken
    
    Review-Url: https://codereview.chromium.org/2251133003
    Cr-Commit-Position: refs/heads/master@{#413068}

--- third_party/WebKit/Source/modules/beacon/NavigatorBeacon.cpp
+++ third_party/WebKit/Source/modules/beacon/NavigatorBeacon.cpp
@@ -79,7 +79,7 @@ bool NavigatorBeacon::canSendBeacon(ExecutionContext* context, const KURL& url, 
 
 int NavigatorBeacon::maxAllowance() const
 {
-    ASSERT(frame());
+    DCHECK(frame());
     const Settings* settings = frame()->settings();
     if (settings) {
         int maxAllowed = settings->maxBeaconTransmission();
@@ -90,15 +90,10 @@ int NavigatorBeacon::maxAllowance() const
     return m_transmittedBytes;
 }
 
-bool NavigatorBeacon::beaconResult(ExecutionContext* context, bool allowed, int sentBytes)
+void NavigatorBeacon::addTransmittedBytes(int sentBytes)
 {
-    if (allowed) {
-        ASSERT(sentBytes >= 0);
-        m_transmittedBytes += sentBytes;
-    } else {
-        UseCounter::count(context, UseCounter::SendBeaconQuotaExceeded);
-    }
-    return allowed;
+    DCHECK_GE(sentBytes, 0);
+    m_transmittedBytes += sentBytes;
 }
 
 bool NavigatorBeacon::sendBeacon(ExecutionContext* context, Navigator& navigator, const String& urlstring, const ArrayBufferViewOrBlobOrStringOrFormData& data, ExceptionState& exceptionState)
@@ -133,7 +128,13 @@ bool NavigatorBeacon::sendBeacon(ExecutionContext* context, Navigator& navigator
         allowed = PingLoader::sendBeacon(impl.frame(), allowance, url, String(), bytes);
     }
 
-    return impl.beaconResult(context, allowed, bytes);
+    if (allowed) {
+        impl.addTransmittedBytes(bytes);
+        return true;
+    }
+
+    UseCounter::count(context, UseCounter::SendBeaconQuotaExceeded);
+    return false;
 }
 
 } // namespace blink
--- third_party/WebKit/Source/modules/beacon/NavigatorBeacon.h
+++ third_party/WebKit/Source/modules/beacon/NavigatorBeacon.h
@@ -36,7 +36,7 @@ private:
 
     bool canSendBeacon(ExecutionContext*, const KURL&, ExceptionState&);
     int maxAllowance() const;
-    bool beaconResult(ExecutionContext*, bool allowed, int sentBytes);
+    void addTransmittedBytes(int sentBytes);
 
     int m_transmittedBytes;
 };
