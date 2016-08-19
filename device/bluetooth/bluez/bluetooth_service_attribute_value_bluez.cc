6c3873139ef25cd8e6ab8c1033537c9d041a95af
mcchou <mcchou@chromium.org>
2016-08-19 12:37:29 -0700

    device/bluetooth: Add a constructor to BluetoothServiceAttributeValueBlueZ
    This adds a default constructor to bluez::BluetoothServiceAttrivuteValueBlueZ.
    
    BUG=b:29314637
    
    Review-Url: https://codereview.chromium.org/2254973006
    Cr-Commit-Position: refs/heads/master@{#413212}

--- device/bluetooth/bluez/bluetooth_service_attribute_value_bluez.cc
+++ device/bluetooth/bluez/bluetooth_service_attribute_value_bluez.cc
@@ -5,10 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "device/bluetooth/bluez/bluetooth_service_attribute_value_bluez.h"
 
+#include <utility>
+
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 
 namespace bluez {
+
+BluetoothServiceAttributeValueBlueZ::BluetoothServiceAttributeValueBlueZ()
+    : type_(NULLTYPE), size_(0) {}
 
 BluetoothServiceAttributeValueBlueZ::BluetoothServiceAttributeValueBlueZ(
     Type type,
