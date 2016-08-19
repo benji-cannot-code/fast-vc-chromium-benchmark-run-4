741e3a444e94106094185eeeff7c25f30c5ca151
kochi <kochi@chromium.org>
2016-08-19 03:00:10 -0700

    Remove first argument for shoudCreateCustomElement()
    After https://codereview.chromium.org/2252003002
    (Change custom element state in documents without browsing context),
    the first argument (document) is not necessary for
    CustomElement::shouldCreateCustomElement().
    
    This is clean-up only and no functional change.
    
    BUG=594918
    
    Review-Url: https://codereview.chromium.org/2258023003
    Cr-Commit-Position: refs/heads/master@{#413096}

--- third_party/WebKit/Source/core/dom/custom/CustomElement.cpp
+++ third_party/WebKit/Source/core/dom/custom/CustomElement.cpp
@@ -79,15 +79,15 @@ bool CustomElement::isValidName(const AtomicString& name)
     return !hyphenContainingElementNames.contains(name);
 }
 
-bool CustomElement::shouldCreateCustomElement(Document& document, const AtomicString& localName)
+bool CustomElement::shouldCreateCustomElement(const AtomicString& localName)
 {
     return RuntimeEnabledFeatures::customElementsV1Enabled()
         && isValidName(localName);
 }
 
-bool CustomElement::shouldCreateCustomElement(Document& document, const QualifiedName& tagName)
+bool CustomElement::shouldCreateCustomElement(const QualifiedName& tagName)
 {
-    return shouldCreateCustomElement(document, tagName.localName())
+    return shouldCreateCustomElement(tagName.localName())
         && tagName.namespaceURI() == HTMLNames::xhtmlNamespaceURI;
 }
 
@@ -107,7 +107,7 @@ HTMLElement* CustomElement::createCustomElementSync(Document& document, const At
 
 HTMLElement* CustomElement::createCustomElementSync(Document& document, const QualifiedName& tagName, ExceptionState& exceptionState)
 {
-    CHECK(shouldCreateCustomElement(document, tagName));
+    CHECK(shouldCreateCustomElement(tagName));
 
     // To create an element:
     // https://dom.spec.whatwg.org/#concept-create-element
@@ -121,7 +121,7 @@ HTMLElement* CustomElement::createCustomElementSync(Document& document, const Qu
 
 HTMLElement* CustomElement::createCustomElementSync(Document& document, const QualifiedName& tagName)
 {
-    CHECK(shouldCreateCustomElement(document, tagName));
+    CHECK(shouldCreateCustomElement(tagName));
 
     // When invoked from "create an element for a token":
     // https://html.spec.whatwg.org/multipage/syntax.html#create-an-element-for-the-token
@@ -136,7 +136,7 @@ HTMLElement* CustomElement::createCustomElementSync(Document& document, const Qu
 
 HTMLElement* CustomElement::createCustomElementAsync(Document& document, const QualifiedName& tagName)
 {
-    CHECK(shouldCreateCustomElement(document, tagName));
+    CHECK(shouldCreateCustomElement(tagName));
 
     // To create an element:
     // https://dom.spec.whatwg.org/#concept-create-element
@@ -150,7 +150,7 @@ HTMLElement* CustomElement::createCustomElementAsync(Document& document, const Q
 
 HTMLElement* CustomElement::createUndefinedElement(Document& document, const QualifiedName& tagName)
 {
-    DCHECK(shouldCreateCustomElement(document, tagName));
+    DCHECK(shouldCreateCustomElement(tagName));
 
     HTMLElement* element;
     if (V0CustomElement::isValidName(tagName.localName()) && document.registrationContext()) {
@@ -168,7 +168,7 @@ HTMLElement* CustomElement::createUndefinedElement(Document& document, const Qua
 
 HTMLElement* CustomElement::createFailedElement(Document& document, const QualifiedName& tagName)
 {
-    DCHECK(shouldCreateCustomElement(document, tagName));
+    DCHECK(shouldCreateCustomElement(tagName));
 
     // "create an element for a token":
     // https://html.spec.whatwg.org/multipage/syntax.html#create-an-element-for-the-token
