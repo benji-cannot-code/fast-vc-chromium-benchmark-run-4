17928b538f1537ac9ca44f377451e23e94d8613a
vabr <vabr@chromium.org>
2016-08-19 09:37:30 -0700

    Simplify signature of OnGetSiteStatistics
    This addresses a TODO: PasswordStoreConsumer::OnGetSiteStatistics currently
    takes a unique_ptr of a vector, while it should have just needed the vector
    itself. At the time this was introduced, it was not possible to pass movable
    types to callbacks in general, just the smart pointer. Now it is possible for
    all movable types.
    
    Therefore this CL gets rid of the now unnecessary unique_ptr in the argument.
    
    BUG=562554
    
    Review-Url: https://codereview.chromium.org/2265523002
    Cr-Commit-Position: refs/heads/master@{#413165}

--- components/password_manager/core/browser/password_store_consumer.cc
+++ components/password_manager/core/browser/password_store_consumer.cc
@@ -16,6 +16,6 @@ PasswordStoreConsumer::~PasswordStoreConsumer() {
 }
 
 void PasswordStoreConsumer::OnGetSiteStatistics(
-    std::unique_ptr<std::vector<std::unique_ptr<InteractionsStats>>> stats) {}
+    std::vector<std::unique_ptr<InteractionsStats>> stats) {}
 
 }  // namespace password_manager
