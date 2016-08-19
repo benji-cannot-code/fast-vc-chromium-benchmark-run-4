95df2586e0371705d6aa87cf8e30e7f4b8316070
tsergeant <tsergeant@chromium.org>
2016-08-18 23:03:11 -0700

    MD History: Synced Tabs fixes
    * Correctly update device list when an item is deleted
    * Make collapsed state a property of the model rather than element,
      fixing issues when the list shrinks or reorders.
    * Tweak margin for long titles
    * Close menu on scroll
    
    BUG=635753
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2251323002
    Cr-Commit-Position: refs/heads/master@{#413061}

--- chrome/browser/resources/md_history/app.crisper.js
+++ chrome/browser/resources/md_history/app.crisper.js
@@ -14612,7 +14612,7 @@ Polymer({
     separatorIndexes: Array,
 
     // Whether the card is open.
-    cardOpen_: {type: Boolean, value: true},
+    opened: Boolean,
 
     searchTerm: String,
 
@@ -14664,12 +14664,22 @@ Polymer({
   },
 
   /**
-   * @param {boolean} cardOpen
+   * @param {boolean} opened
    * @return {string}
+   * @private
    */
-  getCollapseTitle_: function(cardOpen) {
-    return cardOpen ? loadTimeData.getString('collapseSessionButton') :
-                      loadTimeData.getString('expandSessionButton');
+  getCollapseIcon_: function(opened) {
+    return opened ? 'cr:expand-less' : 'cr:expand-more';
+  },
+
+  /**
+   * @param {boolean} opened
+   * @return {string}
+   * @private
+   */
+  getCollapseTitle_: function(opened) {
+    return opened ? loadTimeData.getString('collapseSessionButton') :
+                    loadTimeData.getString('expandSessionButton');
   },
 
   /**
@@ -14691,6 +14701,7 @@ Polymer({
 /**
  * @typedef {{device: string,
  *           lastUpdateTime: string,
+ *           opened: boolean,
  *           separatorIndexes: !Array<number>,
  *           timestamp: number,
  *           tabs: !Array<!ForeignSessionTab>,
@@ -14745,6 +14756,7 @@ Polymer({
 
   listeners: {
     'toggle-menu': 'onToggleMenu_',
+    'scroll': 'onListScroll_'
   },
 
   /** @override */
@@ -14791,6 +14803,7 @@ Polymer({
     return {
       device: session.name,
       lastUpdateTime: '– ' + session.modifiedTime,
+      opened: true,
       separatorIndexes: separatorIndexes,
       timestamp: session.timestamp,
       tabs: tabs,
@@ -14800,6 +14813,12 @@ Polymer({
 
   onSignInTap_: function() {
     chrome.send('startSignInFlow');
+  },
+
+  onListScroll_: function() {
+    var menu = this.$.menu.getIfExists();
+    if (menu)
+      menu.closeMenu();
   },
 
   onToggleMenu_: function(e) {
@@ -14895,9 +14914,16 @@ Polymer({
       }
     }
 
-    // Then, append any new devices.
-    for (var i = updateCount; i < sessionList.length; i++) {
-      this.push('syncedDevices_', this.createInternalDevice_(sessionList[i]));
+    if (sessionList.length >= this.syncedDevices_.length) {
+      // The list grew; append new items.
+      for (var i = updateCount; i < sessionList.length; i++) {
+        this.push('syncedDevices_', this.createInternalDevice_(sessionList[i]));
+      }
+    } else {
+      // The list shrank; remove deleted items.
+      this.splice(
+          'syncedDevices_', updateCount,
+          this.syncedDevices_.length - updateCount);
     }
   },
 
--- chrome/browser/resources/md_history/synced_device_manager.js
+++ chrome/browser/resources/md_history/synced_device_manager.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @typedef {{device: string,
  *           lastUpdateTime: string,
+ *           opened: boolean,
  *           separatorIndexes: !Array<number>,
  *           timestamp: number,
  *           tabs: !Array<!ForeignSessionTab>,
@@ -60,6 +61,7 @@ Polymer({
 
   listeners: {
     'toggle-menu': 'onToggleMenu_',
+    'scroll': 'onListScroll_'
   },
 
   /** @override */
@@ -106,6 +108,7 @@ Polymer({
     return {
       device: session.name,
       lastUpdateTime: '– ' + session.modifiedTime,
+      opened: true,
       separatorIndexes: separatorIndexes,
       timestamp: session.timestamp,
       tabs: tabs,
@@ -115,6 +118,12 @@ Polymer({
 
   onSignInTap_: function() {
     chrome.send('startSignInFlow');
+  },
+
+  onListScroll_: function() {
+    var menu = this.$.menu.getIfExists();
+    if (menu)
+      menu.closeMenu();
   },
 
   onToggleMenu_: function(e) {
@@ -210,9 +219,16 @@ Polymer({
       }
     }
 
-    // Then, append any new devices.
-    for (var i = updateCount; i < sessionList.length; i++) {
-      this.push('syncedDevices_', this.createInternalDevice_(sessionList[i]));
+    if (sessionList.length >= this.syncedDevices_.length) {
+      // The list grew; append new items.
+      for (var i = updateCount; i < sessionList.length; i++) {
+        this.push('syncedDevices_', this.createInternalDevice_(sessionList[i]));
+      }
+    } else {
+      // The list shrank; remove deleted items.
+      this.splice(
+          'syncedDevices_', updateCount,
+          this.syncedDevices_.length - updateCount);
     }
   },
 
