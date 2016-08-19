f24618c16f1110fa3e4f4c7b2a332e5badf25960
hayato <hayato@chromium.org>
2016-08-19 03:34:59 -0700

    Rename Node::isUnclosedNodeOf() to Node::isClosedShadowHiddenFrom()
    Rename Node::isUnclosedNodeOf() to Node::isClosedShadowHiddenFrom() (with a negative meaning)
    to honer the recent change of DOM Standard.
    
    See the commit for whatwg/dom: https://github.com/whatwg/dom/commit/ca0f853a5ea377817883196aa37b73812ed32c52.
    
    Review-Url: https://codereview.chromium.org/2254103005
    Cr-Commit-Position: refs/heads/master@{#413100}

--- third_party/WebKit/Source/core/dom/Node.cpp
+++ third_party/WebKit/Source/core/dom/Node.cpp
@@ -589,10 +589,10 @@ Node& Node::shadowIncludingRoot() const
     return *root;
 }
 
-bool Node::isUnclosedNodeOf(const Node& other) const
+bool Node::isClosedShadowHiddenFrom(const Node& other) const
 {
     if (!isInShadowTree() || treeScope() == other.treeScope())
-        return true;
+        return false;
 
     const TreeScope* scope = &treeScope();
     for (; scope->parentTreeScope(); scope = scope->parentTreeScope()) {
@@ -603,9 +603,9 @@ bool Node::isUnclosedNodeOf(const Node& other) const
 
     for (TreeScope* otherScope = &other.treeScope(); otherScope; otherScope = otherScope->parentTreeScope()) {
         if (otherScope == scope)
-            return true;
+            return false;
     }
-    return false;
+    return true;
 }
 
 bool Node::needsDistributionRecalc() const
