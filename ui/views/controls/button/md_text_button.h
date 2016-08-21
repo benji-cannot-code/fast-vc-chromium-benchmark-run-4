179430754572db6a23170d8ddebf3d9772fd874a
estade <estade@chromium.org>
2016-08-20 18:05:34 -0700

    Adjust MdTextButton border calculation.
    Buttons should normally be 28dp tall. Other scenarios with bigger than
    usual text require adjustment to that value.
    
    BUG=635151
    
    Review-Url: https://codereview.chromium.org/2256403002
    Cr-Commit-Position: refs/heads/master@{#413357}

--- ui/views/controls/button/md_text_button.h
+++ ui/views/controls/button/md_text_button.h
@@ -66,7 +66,7 @@ class VIEWS_EXPORT MdTextButton : public LabelButton {
   MdTextButton(ButtonListener* listener);
   ~MdTextButton() override;
 
-  void UpdatePaddingForFont();
+  void UpdatePadding();
   void UpdateColors();
 
   // The MD-style focus ring. This is not done via a FocusPainter
