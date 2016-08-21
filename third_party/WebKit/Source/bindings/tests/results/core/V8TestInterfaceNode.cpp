89124301b4c5453a6742980fa9212a5a9461be11
yukishiino <yukishiino@chromium.org>
2016-08-21 06:12:26 -0700

    binding: Removes unused code in templates/attributes.cpp.
    Faking {{cpp_class}} and {{c8_class}} doesn't make sense.
    Probably it made sense before the introduction of virtual
    ScriptWrappable::wrap().
    
    Checking the existence of window->document() doesn't seem
    making sense to me, and CQ tests seem passing without the
    check.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2268433002
    Cr-Commit-Position: refs/heads/master@{#413375}

--- third_party/WebKit/Source/bindings/tests/results/core/V8TestInterfaceNode.cpp
+++ third_party/WebKit/Source/bindings/tests/results/core/V8TestInterfaceNode.cpp
@@ -166,7 +166,7 @@ static void perWorldBindingsReadonlyTestInterfaceEmptyAttributeAttributeGetterCa
 static void reflectStringAttributeAttributeGetter(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     v8::Local<v8::Object> holder = info.Holder();
-    Element* impl = V8Element::toImpl(holder);
+    TestInterfaceNode* impl = V8TestInterfaceNode::toImpl(holder);
     v8SetReturnValueString(info, impl->fastGetAttribute(HTMLNames::reflectstringattributeAttr), info.GetIsolate());
 }
 
@@ -178,7 +178,7 @@ static void reflectStringAttributeAttributeGetterCallback(const v8::FunctionCall
 static void reflectStringAttributeAttributeSetter(v8::Local<v8::Value> v8Value, const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     v8::Local<v8::Object> holder = info.Holder();
-    Element* impl = V8Element::toImpl(holder);
+    TestInterfaceNode* impl = V8TestInterfaceNode::toImpl(holder);
     V8StringResource<> cppValue = v8Value;
     if (!cppValue.prepare())
         return;
@@ -207,7 +207,7 @@ static void reflectUrlStringAttributeAttributeGetterCallback(const v8::FunctionC
 static void reflectUrlStringAttributeAttributeSetter(v8::Local<v8::Value> v8Value, const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     v8::Local<v8::Object> holder = info.Holder();
-    Element* impl = V8Element::toImpl(holder);
+    TestInterfaceNode* impl = V8TestInterfaceNode::toImpl(holder);
     V8StringResource<> cppValue = v8Value;
     if (!cppValue.prepare())
         return;
