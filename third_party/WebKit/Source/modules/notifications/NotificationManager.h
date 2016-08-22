e52fc8b9ee119bc44abc4af30dfe211dc3382a04
yzshen <yzshen@chromium.org>
2016-08-22 00:47:52 -0700

    Mojo C++ bindings: extract code shared by different variants.
    - enums, including those defined inside structs and interfaces.
    - struct and union internal data types (i.e., the wire format definition).
    
    BUG=632061
    
    Review-Url: https://codereview.chromium.org/2247083003
    Cr-Commit-Position: refs/heads/master@{#413415}

--- third_party/WebKit/Source/modules/notifications/NotificationManager.h
+++ third_party/WebKit/Source/modules/notifications/NotificationManager.h
@@ -14,12 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-namespace mojom {
-namespace blink {
-enum class PermissionStatus;
-}
-}
-
 // The notification manager, unique to the execution context, is responsible for
 // connecting and communicating with the Mojo notification service.
 //
