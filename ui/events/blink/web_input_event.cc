f84fb770ff9f639805994154d47b45bb748d24a7
jonross <jonross@chromium.org>
2016-08-22 16:12:21 -0700

    Remove unused gesture event builder on windows
    Remove a method that was TODO implement me, as we do not receive these events on window anyways.
    
    TEST=events_unittests content_unittests
    BUG=616673
    
    Review-Url: https://codereview.chromium.org/2265133002
    Cr-Commit-Position: refs/heads/master@{#413570}

--- ui/events/blink/web_input_event.cc
+++ ui/events/blink/web_input_event.cc
@@ -90,15 +90,6 @@ blink::WebKeyboardEvent MakeWebKeyboardEventFromNativeEvent(
       native_event.hwnd, native_event.message, native_event.wParam,
       native_event.lParam, ui::EventTimeStampToSeconds(time_stamp));
 }
-
-blink::WebGestureEvent MakeWebGestureEventFromNativeEvent(
-    const base::NativeEvent& native_event,
-    const base::TimeTicks& time_stamp) {
-  // TODO: Create gestures from native event.
-  NOTIMPLEMENTED();
-  return blink::WebGestureEvent();
-}
-
 #endif  // defined(OS_WIN)
 
 blink::WebKeyboardEvent MakeWebKeyboardEventFromUiEvent(const KeyEvent& event) {
@@ -351,16 +342,7 @@ blink::WebGestureEvent MakeWebGestureEvent(
     const GestureEvent& event,
     const base::Callback<gfx::Point(const ui::LocatedEvent& event)>&
         screen_location_callback) {
-  blink::WebGestureEvent gesture_event;
-#if defined(OS_WIN)
-  if (event.HasNativeEvent())
-    gesture_event = MakeWebGestureEventFromNativeEvent(event.native_event(),
-                                                       event.time_stamp());
-  else
-    gesture_event = MakeWebGestureEventFromUIEvent(event);
-#else
-  gesture_event = MakeWebGestureEventFromUIEvent(event);
-#endif
+  blink::WebGestureEvent gesture_event = MakeWebGestureEventFromUIEvent(event);
 
   gesture_event.x = event.x();
   gesture_event.y = event.y();
@@ -377,15 +359,7 @@ blink::WebGestureEvent MakeWebGestureEvent(
     const ScrollEvent& event,
     const base::Callback<gfx::Point(const ui::LocatedEvent& event)>&
         screen_location_callback) {
-  blink::WebGestureEvent gesture_event;
-
-#if defined(OS_WIN)
-  gesture_event = MakeWebGestureEventFromNativeEvent(event.native_event(),
-                                                     event.time_stamp());
-#else
-  gesture_event = MakeWebGestureEventFromUiEvent(event);
-#endif
-
+  blink::WebGestureEvent gesture_event = MakeWebGestureEventFromUiEvent(event);
   gesture_event.x = event.x();
   gesture_event.y = event.y();
 
