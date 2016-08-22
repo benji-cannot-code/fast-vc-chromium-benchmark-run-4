933c178429bd6ae89fe17edc4fc8ecf2da18de9b
jamescook <jamescook@chromium.org>
2016-08-22 15:31:10 -0700

    ash: Remove unnecessary WmShelf::SchedulePaint() method
    This method existed because the overflow bubble used it to cause a repaint of
    the overflow button in the parent shelf view. Instead of repainting all the
    buttons, just repaint the anchor directly.
    
    (I am uncertain this code path ever gets called in production. It would only
    run if some code directly destroyed the bubble widget instead of calling into
    the shelf code to do so. This does not seem to happen at shutdown or when
    disconnecting a display, even with the bubble visible. However, failure to
    cleanup the cached pointers in OverflowBubble on widget destruction would
    cause crashes, so I'm keeping the OnWidgetDestroying() method.)
    
    BUG=615502
    TEST=ash_browsertests, manual tests of opening and closing the overflow bubble
    on multiple displays
    
    Review-Url: https://codereview.chromium.org/2267803003
    Cr-Commit-Position: refs/heads/master@{#413556}

--- ash/common/shelf/overflow_bubble.cc
+++ ash/common/shelf/overflow_bubble.cc
@@ -30,6 +30,9 @@ OverflowBubble::~OverflowBubble() {
 }
 
 void OverflowBubble::Show(views::View* anchor, ShelfView* shelf_view) {
+  DCHECK(anchor);
+  DCHECK(shelf_view);
+
   Hide();
 
   bubble_ = new OverflowBubbleView(wm_shelf_);
@@ -83,10 +86,11 @@ void OverflowBubble::OnPointerEventObserved(
 
 void OverflowBubble::OnWidgetDestroying(views::Widget* widget) {
   DCHECK(widget == bubble_->GetWidget());
-  bubble_ = NULL;
-  anchor_ = NULL;
-  wm_shelf_->SchedulePaint();
-  shelf_view_ = NULL;
+  // Update the overflow button in the parent ShelfView.
+  anchor_->SchedulePaint();
+  bubble_ = nullptr;
+  anchor_ = nullptr;
+  shelf_view_ = nullptr;
 }
 
 }  // namespace ash
