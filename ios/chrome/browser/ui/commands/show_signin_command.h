719570b8c62a56501feb7b01368bd95be142eeb8
bzanotti <bzanotti@chromium.org>
2016-08-22 10:01:22 -0700

    Remove SignInSource from ShowSigninCommand.
    Also remove it altogether as it isn't used anymore.
    
    BUG=612908
    
    Review-Url: https://codereview.chromium.org/2254813002
    Cr-Commit-Position: refs/heads/master@{#413458}

--- ios/chrome/browser/ui/commands/show_signin_command.h
+++ ios/chrome/browser/ui/commands/show_signin_command.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 
 #include "components/signin/core/browser/signin_metrics.h"
-#include "ios/chrome/browser/signin/constants.h"
 #include "ios/chrome/browser/ui/commands/generic_chrome_command.h"
 
 typedef void (^ShowSigninCommandCompletionCallback)(BOOL succeeded);
@@ -37,19 +36,9 @@ enum AuthenticationOperation {
 // Initializes a command to perform the specified operation with a
 // SigninInteractionController and invoke a possibly-nil callback when finished.
 - (instancetype)initWithOperation:(AuthenticationOperation)operation
-                     signInSource:(SignInSource)signInSource
+                signInAccessPoint:(signin_metrics::AccessPoint)signInAccessPoint
                          callback:(ShowSigninCommandCompletionCallback)callback
     NS_DESIGNATED_INITIALIZER;
-
-// Initializes a ShowSigninCommand with a nil callback.
-- (instancetype)initWithOperation:(AuthenticationOperation)operation
-                     signInSource:(SignInSource)signInSource;
-
-// Initializes a command to perform the specified operation with a
-// SigninInteractionController and invoke a possibly-nil callback when finished.
-- (instancetype)initWithOperation:(AuthenticationOperation)operation
-                signInAccessPoint:(signin_metrics::AccessPoint)signInAccessPoint
-                         callback:(ShowSigninCommandCompletionCallback)callback;
 
 // Initializes a ShowSigninCommand with a nil callback.
 - (instancetype)initWithOperation:(AuthenticationOperation)operation
@@ -61,9 +50,6 @@ enum AuthenticationOperation {
 
 // The operation to perform during the sign-in flow.
 @property(nonatomic, readonly) AuthenticationOperation operation;
-
-// The source of this authentication operation.
-@property(nonatomic, readonly) SignInSource signInSource;
 
 // The access point of this authentication operation.
 @property(nonatomic, readonly) signin_metrics::AccessPoint signInAccessPoint;
