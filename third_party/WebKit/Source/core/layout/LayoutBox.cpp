f6b01e9b93608307278425ffa461a2b028d2756c
cbiesinger <cbiesinger@chromium.org>
2016-08-22 08:39:18 -0700

    Allow using overrideContainingBlockContentLogical{Width,Height} outside of grid
    I am using it for the LayoutNG transition; there does not seem to be a need
    to limit this to grid items as nothing else currently sets this.
    
    This change also makes LayoutNG use this new function.
    
    R=eae@chromium.org,ikilpatrick@chromium.org,glebl@chromium.org
    BUG=635619
    
    Review-Url: https://codereview.chromium.org/2257223002
    Cr-Commit-Position: refs/heads/master@{#413445}

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -3017,7 +3017,7 @@ LayoutUnit LayoutBox::computeReplacedLogicalHeightUsing(SizeType sizeType, const
             availableHeight = containingBlockLogicalHeightForPositioned(toLayoutBoxModelObject(cb));
         } else if (stretchedHeight != -1) {
             availableHeight = stretchedHeight;
-        } else if (isGridItem() && hasOverrideContainingBlockLogicalHeight()) {
+        } else if (hasOverrideContainingBlockLogicalHeight()) {
             availableHeight = overrideContainingBlockContentLogicalHeight();
         } else {
             availableHeight = containingBlockLogicalHeightForContent(IncludeMarginBorderPadding);
