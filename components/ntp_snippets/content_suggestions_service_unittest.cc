82b726460f1a3acc623ac1b8d2f592cee5b0daba
vitaliii <vitaliii@chromium.org>
2016-08-22 03:08:40 -0700

    Remove ContentSuggestionsProvider::GetProvidedCategories().
    Since there is ContentSuggestionsProvider::Observer::OnCategoryStatusChanged(),
    there is no need for ContentSuggestionsProvider::GetProvidedCategories().
    
    BUG=639282
    
    Review-Url: https://codereview.chromium.org/2260703004
    Cr-Commit-Position: refs/heads/master@{#413428}

--- components/ntp_snippets/content_suggestions_service_unittest.cc
+++ components/ntp_snippets/content_suggestions_service_unittest.cc
@@ -72,10 +72,6 @@ class MockProvider : public ContentSuggestionsProvider {
     }
   }
 
-  std::vector<Category> GetProvidedCategories() override {
-    return provided_categories_;
-  }
-
   CategoryStatus GetCategoryStatus(Category category) {
     return statuses_[category.id()];
   }
@@ -93,6 +89,11 @@ class MockProvider : public ContentSuggestionsProvider {
   void FireCategoryStatusChanged(Category category, CategoryStatus new_status) {
     statuses_[category.id()] = new_status;
     observer()->OnCategoryStatusChanged(this, category, new_status);
+  }
+
+  void FireCategoryStatusChangedWithCurrentStatus(Category category) {
+    observer()->OnCategoryStatusChanged(this, category,
+                                        statuses_[category.id()]);
   }
 
   void FireSuggestionInvalidated(Category category,
@@ -230,7 +231,9 @@ TEST_F(ContentSuggestionsServiceTest, ShouldRegisterProviders) {
               Eq(CategoryStatus::NOT_PROVIDED));
 
   MockProvider* provider1 = RegisterProvider(articles_category);
+  provider1->FireCategoryStatusChangedWithCurrentStatus(articles_category);
   EXPECT_THAT(providers().count(offline_pages_category), Eq(0ul));
+  ASSERT_THAT(providers().count(articles_category), Eq(1ul));
   EXPECT_THAT(providers().at(articles_category), Eq(provider1));
   EXPECT_THAT(providers().size(), Eq(1ul));
   EXPECT_THAT(service()->GetCategories(), ElementsAre(articles_category));
@@ -240,7 +243,10 @@ TEST_F(ContentSuggestionsServiceTest, ShouldRegisterProviders) {
               Eq(CategoryStatus::NOT_PROVIDED));
 
   MockProvider* provider2 = RegisterProvider(offline_pages_category);
+  provider2->FireCategoryStatusChangedWithCurrentStatus(offline_pages_category);
+  ASSERT_THAT(providers().count(offline_pages_category), Eq(1ul));
   EXPECT_THAT(providers().at(articles_category), Eq(provider1));
+  ASSERT_THAT(providers().count(articles_category), Eq(1ul));
   EXPECT_THAT(providers().at(offline_pages_category), Eq(provider2));
   EXPECT_THAT(providers().size(), Eq(2ul));
   EXPECT_THAT(service()->GetCategories(),
@@ -351,8 +357,12 @@ TEST_F(ContentSuggestionsServiceTest, ShouldForwardSuggestions) {
 
   // Create and register providers
   MockProvider* provider1 = RegisterProvider(articles_category);
+  provider1->FireCategoryStatusChangedWithCurrentStatus(articles_category);
   MockProvider* provider2 = RegisterProvider(offline_pages_category);
+  provider2->FireCategoryStatusChangedWithCurrentStatus(offline_pages_category);
+  ASSERT_THAT(providers().count(articles_category), Eq(1ul));
   EXPECT_THAT(providers().at(articles_category), Eq(provider1));
+  ASSERT_THAT(providers().count(offline_pages_category), Eq(1ul));
   EXPECT_THAT(providers().at(offline_pages_category), Eq(provider2));
 
   // Create and register observer
@@ -417,6 +427,7 @@ TEST_F(ContentSuggestionsServiceTest,
 TEST_F(ContentSuggestionsServiceTest, ShouldReturnCategoryInfo) {
   Category category = FromKnownCategory(KnownCategories::BOOKMARKS);
   MockProvider* provider = RegisterProvider(category);
+  provider->FireCategoryStatusChangedWithCurrentStatus(category);
   base::Optional<CategoryInfo> result = service()->GetCategoryInfo(category);
   ASSERT_TRUE(result.has_value());
   CategoryInfo expected = provider->GetCategoryInfo(category);
@@ -430,6 +441,7 @@ TEST_F(ContentSuggestionsServiceTest,
        ShouldRegisterNewCategoryOnNewSuggestions) {
   Category category = FromKnownCategory(KnownCategories::BOOKMARKS);
   MockProvider* provider = RegisterProvider(category);
+  provider->FireCategoryStatusChangedWithCurrentStatus(category);
   MockServiceObserver observer;
   service()->AddObserver(&observer);
 
@@ -446,9 +458,11 @@ TEST_F(ContentSuggestionsServiceTest,
   provider->FireSuggestionsChanged(new_category, {1, 2});
 
   ExpectThatSuggestionsAre(new_category, {1, 2});
+  ASSERT_THAT(providers().count(category), Eq(1ul));
   EXPECT_THAT(providers().at(category), Eq(provider));
   EXPECT_THAT(service()->GetCategoryStatus(category),
               Eq(CategoryStatus::AVAILABLE));
+  ASSERT_THAT(providers().count(new_category), Eq(1ul));
   EXPECT_THAT(providers().at(new_category), Eq(provider));
   EXPECT_THAT(service()->GetCategoryStatus(new_category),
               Eq(CategoryStatus::AVAILABLE));
@@ -460,6 +474,7 @@ TEST_F(ContentSuggestionsServiceTest,
        ShouldRegisterNewCategoryOnCategoryStatusChanged) {
   Category category = FromKnownCategory(KnownCategories::BOOKMARKS);
   MockProvider* provider = RegisterProvider(category);
+  provider->FireCategoryStatusChangedWithCurrentStatus(category);
   MockServiceObserver observer;
   service()->AddObserver(&observer);
 
@@ -473,6 +488,7 @@ TEST_F(ContentSuggestionsServiceTest,
   provider->FireCategoryStatusChanged(new_category,
                                       CategoryStatus::INITIALIZING);
 
+  ASSERT_THAT(providers().count(new_category), Eq(1ul));
   EXPECT_THAT(providers().at(new_category), Eq(provider));
   ExpectThatSuggestionsAre(new_category, std::vector<int>());
   EXPECT_THAT(service()->GetCategoryStatus(new_category),
--- components/ntp_snippets/physical_web_pages/physical_web_page_suggestions_provider.h
+++ components/ntp_snippets/physical_web_pages/physical_web_page_suggestions_provider.h
@@ -44,7 +44,6 @@ class PhysicalWebPageSuggestionsProvider : public ContentSuggestionsProvider {
   void OnDisplayableUrlsChanged(const std::vector<UrlInfo>& urls);
 
   // ContentSuggestionsProvider implementation.
-  std::vector<Category> GetProvidedCategories() override;
   CategoryStatus GetCategoryStatus(Category category) override;
   CategoryInfo GetCategoryInfo(Category category) override;
   void DismissSuggestion(const std::string& suggestion_id) override;
