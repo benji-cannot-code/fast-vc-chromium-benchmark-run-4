309118b3a7de6a83b3a959eb0241495c39fe85cd
thestig <thestig@chromium.org>
2016-08-22 11:10:35 -0700

    Fix branding/official build confusion in os_crypt.
    TBR=cfroussios@chromium.org
    
    Review-Url: https://codereview.chromium.org/2264163002
    Cr-Commit-Position: refs/heads/master@{#413477}

--- components/os_crypt/key_storage_linux.cc
+++ components/os_crypt/key_storage_linux.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/os_crypt/key_storage_kwallet.h"
 #endif
 
-#if defined(OFFICIAL_BUILD)
+#if defined(GOOGLE_CHROME_BUILD)
 const char KeyStorageLinux::kFolderName[] = "Chrome Keys";
 const char KeyStorageLinux::kKey[] = "Chrome Safe Storage";
 #else
