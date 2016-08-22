751c4bffeec0c3b6f1eefba1fb87bc678091b9b9
landell <landell@opera.com>
2016-08-22 12:44:31 -0700

    Use base::SizeTToString instead of std::to_string
    std::to_string is disallowed by the chromium code style and we get into trouble with a custom mips toolchain that lacks support.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2264993002
    Cr-Commit-Position: refs/heads/master@{#413513}

--- net/log/bounded_file_net_log_observer.cc
+++ net/log/bounded_file_net_log_observer.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/json/json_writer.h"
 #include "base/logging.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/synchronization/lock.h"
 #include "base/threading/thread.h"
 #include "base/values.h"
@@ -355,8 +356,8 @@ void BoundedFileNetLogObserver::FileWriter::IncrementCurrentFile() {
   current_file_idx_ %= total_num_files_;
   event_files_[current_file_idx_].reset();
   event_files_[current_file_idx_] = base::ScopedFILE(base::OpenFile(
-      directory_.AppendASCII("event_file_" + std::to_string(current_file_idx_) +
-                             ".json"),
+      directory_.AppendASCII("event_file_" +
+                             base::SizeTToString(current_file_idx_) + ".json"),
       "w"));
 }
 
@@ -395,9 +396,9 @@ void BoundedFileNetLogObserver::FileWriter::DeleteAllFiles() {
   base::DeleteFile(directory_.AppendASCII("constants.json"), false);
   base::DeleteFile(directory_.AppendASCII("end_netlog.json"), false);
   for (size_t i = 0; i < total_num_files_; i++) {
-    base::DeleteFile(
-        directory_.AppendASCII("event_file_" + std::to_string(i) + ".json"),
-        false);
+    base::DeleteFile(directory_.AppendASCII("event_file_" +
+                                            base::SizeTToString(i) + ".json"),
+                     false);
   }
 }
 
