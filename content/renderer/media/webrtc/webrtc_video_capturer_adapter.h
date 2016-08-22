c59b01d553e4ead0f331f69f3f3bd2a5d40562f1
nisse <nisse@chromium.org>
2016-08-22 08:15:28 -0700

    Revert of Delete the class WebRtcVideoCapturerAdapter::MediaVideoFrameFactory (patchset #5 id:80001 of https://codereview.chromium.org/2244213002/ )
    Reason for revert:
    Somehow breaks timestamping. Reverting for investigation. To reproduce, run
    
    ./browser_tests --gtest_filter='WebRtcVideoQualityBrowserTests/WebRtcVideoQualityBrowserTest*' --run-manual --ui-test-action-max-timeout=200000
    
    This produces lots of warnings like
    
    [1:14:0822/153830:WARNING:video_capture_input.cc(79)] Same/old NTP timestamp (3680861906110 <= 3680861906110) for incoming frame. Dropping.
    [1:14:0822/153830:WARNING:video_capture_input.cc(79)] Same/old NTP timestamp (3680861906042 <= 3680861906042) for incoming frame. Dropping.
    
    Original issue's description:
    > Delete the class WebRtcVideoCapturerAdapter::MediaVideoFrameFactory
    >
    > Instead, use the AdaptFrame and OnFrame methods of the VideoCapturer
    > base class.
    >
    > This cl also applies the new timestamp aligner logic, to translate
    > Chrome's camera timestamps into the timescale of rtc::TimeMicros(). No
    > other change in behavior is intended.
    >
    > BUG=webrtc:5682, webrtc:5740, 516700
    >
    > Committed: https://crrev.com/5b9a4b339447b033ca52244a02c0bef17067385f
    > Cr-Commit-Position: refs/heads/master@{#413093}
    
    TBR=perkj@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=webrtc:5682, webrtc:5740, 516700
    
    Review-Url: https://codereview.chromium.org/2263183002
    Cr-Commit-Position: refs/heads/master@{#413444}

--- content/renderer/media/webrtc/webrtc_video_capturer_adapter.h
+++ content/renderer/media/webrtc/webrtc_video_capturer_adapter.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "media/base/video_capture_types.h"
 #include "media/base/video_frame.h"
-#include "media/base/video_frame_pool.h"
 #include "third_party/webrtc/media/base/videocapturer.h"
 
 namespace content {
@@ -58,7 +57,7 @@ class CONTENT_EXPORT WebRtcVideoCapturerAdapter
   const bool is_screencast_;
   bool running_;
 
-  media::VideoFramePool scaled_frame_pool_;
+  class MediaVideoFrameFactory;
 
   DISALLOW_COPY_AND_ASSIGN(WebRtcVideoCapturerAdapter);
 };
