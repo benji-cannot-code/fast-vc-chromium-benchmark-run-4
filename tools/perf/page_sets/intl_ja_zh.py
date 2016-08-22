aa28bcc1f16c45d3ae0edcc8d1873352160d20e4
kouhei <kouhei@chromium.org>
2016-08-22 00:09:25 -0700

    Introduce PageCyclerStory and let the stories wait for onload+TTI
    BUG=626274
    
    Review-Url: https://codereview.chromium.org/2244943002
    Cr-Commit-Position: refs/heads/master@{#413414}

--- tools/perf/page_sets/intl_ja_zh.py
+++ tools/perf/page_sets/intl_ja_zh.py
@@ -2,13 +2,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+from page_sets import page_cycler_story
 from telemetry.page import cache_temperature as cache_temperature_module
-from telemetry.page import page as page_module
 from telemetry.page import shared_page_state
 from telemetry import story
 
 
-class IntlJaZhPage(page_module.Page):
+class IntlJaZhPage(page_cycler_story.PageCyclerStory):
 
   def __init__(self, url, page_set, cache_temperature=None):
     super(IntlJaZhPage, self).__init__(
