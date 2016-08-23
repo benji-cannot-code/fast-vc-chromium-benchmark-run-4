e4fb3de83ad87d09a69bbe91b60d47251c9b1f73
melandory <melandory@chromium.org>
2016-08-22 18:54:58 -0700

    Simplify triggering logic for subresource filter prompt.
    Currently UI triggering logic is spread all over SubresourceFIlterFactory. This CL simplifies logic used and makes it less error prone.
    
    BUG=609747
    
    Review-Url: https://codereview.chromium.org/2250923002
    Cr-Commit-Position: refs/heads/master@{#413626}

--- components/subresource_filter/content/browser/subresource_filter_navigation_throttle.cc
+++ components/subresource_filter/content/browser/subresource_filter_navigation_throttle.cc
@@ -53,8 +53,8 @@ SubresourceFilterNavigationThrottle::WillProcessResponse() {
 
   ContentSubresourceFilterDriverFactory::FromWebContents(
       navigation_handle()->GetWebContents())
-      ->ActivateForFrameHostIfNeeded(navigation_handle()->GetRenderFrameHost(),
-                                     initial_url_);
+      ->ReadyToCommitMainFrameNavigation(
+          navigation_handle()->GetRenderFrameHost(), initial_url_);
 
   return NavigationThrottle::PROCEED;
 }
