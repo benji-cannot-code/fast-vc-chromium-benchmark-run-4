64b5d1db81e291892253bf715edb83628366fb89
tomhudson <tomhudson@google.com>
2016-08-23 10:43:54 -0700

    Cons up an SkBitmap on demand for PPAPI
    ImageData's GetMappedBitmap() returned a const pointer to an SkBitmap;
    some backends implemented this using a deprecated API which was
    exposing transient internal Skia state.
    
    Instead we create a lightweight SkBitmap and return it. The pixel
    memory may still be ephemeral, but this lets us get rid of the last
    user of the bad API.
    
    When we returned a pointer to a locked SkBitmap, the pointer shared
    the lock; since new SkBitmaps are always unlocked, clients of
    GetMappedBitmap() will need to explicitly use SkAutoLockPixels if
    they want to access the pixel memory directly.
    
    R=fmalita@chromium.org,reed@chromium.org,mcasas@chromium.org,bbudge@chromium.org
    BUG=543755
    
    Review-Url: https://codereview.chromium.org/2149453003
    Cr-Commit-Position: refs/heads/master@{#413779}

--- content/renderer/pepper/plugin_instance_throttler_impl_unittest.cc
+++ content/renderer/pepper/plugin_instance_throttler_impl_unittest.cc
@@ -106,12 +106,12 @@ TEST_F(PluginInstanceThrottlerImplTest, ThrottleByKeyframe) {
   SkBitmap interesting_bitmap = skia::ReadPixels(canvas.sk_canvas());
 
   // Don't throttle for a boring frame.
-  throttler()->OnImageFlush(&boring_bitmap);
+  throttler()->OnImageFlush(boring_bitmap);
   EXPECT_FALSE(throttler()->IsThrottled());
   EXPECT_EQ(0, change_callback_calls());
 
   // Throttle after an interesting frame.
-  throttler()->OnImageFlush(&interesting_bitmap);
+  throttler()->OnImageFlush(interesting_bitmap);
   EXPECT_TRUE(throttler()->IsThrottled());
   EXPECT_EQ(1, change_callback_calls());
 }
@@ -124,7 +124,7 @@ TEST_F(PluginInstanceThrottlerImplTest, MaximumKeyframesAnalyzed) {
 
   // Throttle after tons of boring bitmaps.
   for (int i = 0; i < kMaximumFramesToExamine; ++i) {
-    throttler()->OnImageFlush(&boring_bitmap);
+    throttler()->OnImageFlush(boring_bitmap);
   }
   EXPECT_TRUE(throttler()->IsThrottled());
   EXPECT_EQ(1, change_callback_calls());
