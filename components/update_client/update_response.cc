f3531cb7e61dabe4436cf58368dd3aae78fed915
waffles <waffles@chromium.org>
2016-08-23 12:15:29 -0700

    Add support for Omaha cohorts to the component updater.
    BUG=638633,639569
    
    Review-Url: https://codereview.chromium.org/2252093002
    Cr-Commit-Position: refs/heads/master@{#413809}

--- components/update_client/update_response.cc
+++ components/update_client/update_response.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <memory>
 
+#include "base/memory/ptr_util.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
@@ -21,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace update_client {
 
 static const char* kExpectedResponseProtocol = "3.0";
+const char UpdateResponse::Result::kCohort[] = "cohort";
+const char UpdateResponse::Result::kCohortHint[] = "cohorthint";
+const char UpdateResponse::Result::kCohortName[] = "cohortname";
 
 UpdateResponse::UpdateResponse() {
 }
@@ -33,8 +37,7 @@ UpdateResponse::Results::Results(const Results& other) = default;
 UpdateResponse::Results::~Results() {
 }
 
-UpdateResponse::Result::Result() {
-}
+UpdateResponse::Result::Result() {}
 UpdateResponse::Result::Result(const Result& other) = default;
 UpdateResponse::Result::~Result() {
 }
@@ -92,6 +95,21 @@ static std::string GetAttribute(xmlNode* node, const char* attribute_name) {
     }
   }
   return std::string();
+}
+
+// Returns the value of a named attribute, or nullptr .
+static std::unique_ptr<std::string> GetAttributePtr(
+    xmlNode* node,
+    const char* attribute_name) {
+  const xmlChar* name = reinterpret_cast<const xmlChar*>(attribute_name);
+  for (xmlAttr* attr = node->properties; attr != NULL; attr = attr->next) {
+    if (!xmlStrcmp(attr->name, name) && attr->children &&
+        attr->children->content) {
+      return base::MakeUnique<std::string>(
+          reinterpret_cast<const char*>(attr->children->content));
+    }
+  }
+  return nullptr;
 }
 
 // This is used for the xml parser to report errors. This assumes the context
@@ -269,6 +287,17 @@ bool ParseUpdateCheckTag(xmlNode* updatecheck,
 bool ParseAppTag(xmlNode* app,
                  UpdateResponse::Result* result,
                  std::string* error) {
+  // Read cohort information.
+  auto cohort = GetAttributePtr(app, "cohort");
+  static const char* attrs[] = {UpdateResponse::Result::kCohort,
+                                UpdateResponse::Result::kCohortHint,
+                                UpdateResponse::Result::kCohortName};
+  for (const auto& attr : attrs) {
+    auto value = GetAttributePtr(app, attr);
+    if (value)
+      result->cohort_attrs.insert({attr, *value});
+  }
+
   // Read the crx id.
   result->extension_id = GetAttribute(app, "appid");
   if (result->extension_id.empty()) {
--- components/update_client/update_response.h
+++ components/update_client/update_response.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_UPDATE_CLIENT_UPDATE_RESPONSE_H_
 #define COMPONENTS_UPDATE_CLIENT_UPDATE_RESPONSE_H_
 
+#include <map>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -100,6 +102,15 @@ class UpdateResponse {
     std::vector<GURL> crx_diffurls;
 
     Manifest manifest;
+
+    // The server has instructed the client to set its [key] to [value] for each
+    // key-value pair in this string.
+    std::map<std::string, std::string> cohort_attrs;
+
+    // The following are the only allowed keys in |cohort_attrs|.
+    static const char kCohort[];
+    static const char kCohortHint[];
+    static const char kCohortName[];
   };
 
   static const int kNoDaystart = -1;
