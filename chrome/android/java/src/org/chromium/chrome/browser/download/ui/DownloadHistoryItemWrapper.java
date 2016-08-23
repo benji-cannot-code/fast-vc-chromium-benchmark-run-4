ceaebb8ee16e4f1277f99a6c1c82e0fd56e08c00
twellington <twellington@chromium.org>
2016-08-23 16:43:57 -0700

    [Downloads] Fix crash on opening items
    Histogram boundary was set incorrectly.
    
    BUG=616324
    
    Review-Url: https://codereview.chromium.org/2272853002
    Cr-Commit-Position: refs/heads/master@{#413899}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryItemWrapper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryItemWrapper.java
@@ -88,12 +88,12 @@ abstract class DownloadHistoryItemWrapper implements TimedItem {
 
     protected void recordOpenSuccess() {
         RecordHistogram.recordEnumeratedHistogram("Android.DownloadManager.Item.OpenSucceeded",
-                getFilterType(), DownloadFilter.FILTER_ALL);
+                getFilterType(), DownloadFilter.FILTER_BOUNDARY);
     }
 
     protected void recordOpenFailure() {
         RecordHistogram.recordEnumeratedHistogram("Android.DownloadManager.Item.OpenFailed",
-                getFilterType(), DownloadFilter.FILTER_ALL);
+                getFilterType(), DownloadFilter.FILTER_BOUNDARY);
     }
 
     /** Wraps a {@link DownloadItem}. */
