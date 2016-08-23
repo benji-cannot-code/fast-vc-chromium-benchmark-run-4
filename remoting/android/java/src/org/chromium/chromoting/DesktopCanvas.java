3c03d01bd46b070ea9bebebf9ff6fb1893a0ef24
yuweih <yuweih@chromium.org>
2016-08-22 19:04:30 -0700

    [Remoting Android] Rename AbstractDesktopView to DesktopView
    Previously the old bitmap renderer has been removed from the codebase. Per
    sergeyu@'s comment, we are now able to drop the Abstract* prefix in
    AbstractDesktopView since the old DesktopView has been deleted. This CL renames
    AbstractDesktopView to DesktopView.
    
    Review-Url: https://codereview.chromium.org/2267213002
    Cr-Commit-Position: refs/heads/master@{#413631}

--- remoting/android/java/src/org/chromium/chromoting/DesktopCanvas.java
+++ remoting/android/java/src/org/chromium/chromoting/DesktopCanvas.java
@@ -19,7 +19,7 @@ public class DesktopCanvas {
      */
     private static final float MAX_ZOOM_FACTOR = 100.0f;
 
-    private final AbstractDesktopView mViewer;
+    private final DesktopView mViewer;
     private final RenderData mRenderData;
 
     /**
@@ -35,7 +35,7 @@ public class DesktopCanvas {
      */
     private Rect mSystemUiOffsetPixels = new Rect();
 
-    public DesktopCanvas(AbstractDesktopView viewer, RenderData renderData) {
+    public DesktopCanvas(DesktopView viewer, RenderData renderData) {
         mViewer = viewer;
         mRenderData = renderData;
     }
