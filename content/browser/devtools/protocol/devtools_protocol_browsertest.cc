4e6cb94545afd12acf85174161a7194068e543e7
eseckler <eseckler@chromium.org>
2016-08-23 06:37:08 -0700

    Adds commands to get and set the frame size of a DevTools target.
    This is a first step towards more flexible screenshots
    (bit.ly/sized-screenshots): The size of the CompositorFrame sent from the
    renderer and the screenshot size are determined by the size of the RWHV.
    Combined with renderer-side Emulation overrides, this will allow us to obtain
    screenshots of a configurable size.
    
    We are planning on adding an additional Emulation override command that
    allows us to scale and position the page content, which will enable
    screenshots of a custom area of the page. Work-in-progress patch:
    https://codereview.chromium.org/2237433004/
    
    BUG=625577
    
    Review-Url: https://codereview.chromium.org/2226323002
    Cr-Commit-Position: refs/heads/master@{#413721}

--- content/browser/devtools/protocol/devtools_protocol_browsertest.cc
+++ content/browser/devtools/protocol/devtools_protocol_browsertest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_view_host.h"
+#include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/url_constants.h"
 #include "content/public/test/browser_test_utils.h"
@@ -33,6 +34,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/compositor/compositor_switches.h"
 #include "ui/gfx/codec/png_codec.h"
+
+#define EXPECT_SIZE_EQ(expected, actual)               \
+  do {                                                 \
+    EXPECT_EQ((expected).width(), (actual).width());   \
+    EXPECT_EQ((expected).height(), (actual).height()); \
+  } while (false)
 
 using testing::ElementsAre;
 
@@ -867,6 +874,27 @@ IN_PROC_BROWSER_TEST_F(IsolatedDevToolsProtocolTest,
                   "iframe_navigation.html",
                   "http://a.com/devtools/navigation.html",
                   "http://b.com/devtools/control_navigations/meta_tag.html"));
+}
+
+// Setting RWHV size is not supported on Android.
+#if defined(OS_ANDROID)
+#define MAYBE_EmulationSetVisibleSize DISABLED_EmulationSetVisibleSize
+#else
+#define MAYBE_EmulationSetVisibleSize EmulationSetVisibleSize
+#endif
+IN_PROC_BROWSER_TEST_F(DevToolsProtocolTest,
+                       MAYBE_EmulationSetVisibleSize) {
+  NavigateToURLBlockUntilNavigationsComplete(shell(), GURL("about:blank"), 1);
+  Attach();
+  gfx::Size new_size(200, 400);
+  std::unique_ptr<base::DictionaryValue> params(new base::DictionaryValue());
+  params->SetInteger("width", new_size.width());
+  params->SetInteger("height", new_size.height());
+  SendCommand("Emulation.setVisibleSize", std::move(params), true);
+  EXPECT_SIZE_EQ(new_size, (shell()->web_contents())
+                               ->GetRenderWidgetHostView()
+                               ->GetViewBounds()
+                               .size());
 }
 
 IN_PROC_BROWSER_TEST_F(DevToolsProtocolTest, VirtualTimeTest) {
