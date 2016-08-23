a0d2ec23fdb7237c1bbc8467985fc2f0ba069c91
nedenwang <nedenwang@tencent.com>
2016-08-23 00:34:05 -0700

    [Android] Fix icu_use_data_file = false.
    There are compiler errors while building Android WebView Shell with icu_use_data_file = false.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2241753002
    Cr-Commit-Position: refs/heads/master@{#413686}

--- content/app/content_main_runner.cc
+++ content/app/content_main_runner.cc
@@ -472,6 +472,7 @@ class ContentMainRunnerImpl : public ContentMainRunner {
 #endif  // OS_ANDROID
 
     base::GlobalDescriptors* g_fds = base::GlobalDescriptors::GetInstance();
+    ALLOW_UNUSED_LOCAL(g_fds);
 
     // On Android,
     // - setlocale() is not supported.
@@ -679,7 +680,7 @@ class ContentMainRunnerImpl : public ContentMainRunner {
     RegisterPathProvider();
     RegisterContentSchemes(true);
 
-#if defined(OS_ANDROID)
+#if defined(OS_ANDROID) && (ICU_UTIL_DATA_IMPL == ICU_UTIL_DATA_FILE)
     int icudata_fd = g_fds->MaybeGet(kAndroidICUDataDescriptor);
     if (icudata_fd != -1) {
       auto icudata_region = g_fds->GetRegion(kAndroidICUDataDescriptor);
@@ -690,7 +691,7 @@ class ContentMainRunnerImpl : public ContentMainRunner {
     }
 #else
     CHECK(base::i18n::InitializeICU());
-#endif  // OS_ANDROID
+#endif  // OS_ANDROID && (ICU_UTIL_DATA_IMPL == ICU_UTIL_DATA_FILE)
 
     base::StatisticsRecorder::Initialize();
 
