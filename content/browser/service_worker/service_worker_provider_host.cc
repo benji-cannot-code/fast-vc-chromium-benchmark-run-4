953123c8dc6c419807de23e47bbd0d214816a337
shimazu <shimazu@chromium.org>
2016-08-23 00:03:39 -0700

    ServiceWorker: Call SyncMatchingRegistration when document_url is changed
    For details: https://crbug.com/634222
    
    BUG=634222,619294,454250
    
    Review-Url: https://codereview.chromium.org/2245063003
    Cr-Commit-Position: refs/heads/master@{#413682}

--- content/browser/service_worker/service_worker_provider_host.cc
+++ content/browser/service_worker/service_worker_provider_host.cc
@@ -104,10 +104,7 @@ ServiceWorkerProviderHost::~ServiceWorkerProviderHost() {
   if (controlling_version_.get())
     controlling_version_->RemoveControllee(this);
 
-  for (auto& key_registration : matching_registrations_) {
-    DecreaseProcessReference(key_registration.second->pattern());
-    key_registration.second->RemoveListener(this);
-  }
+  RemoveAllMatchingRegistrations();
 
   for (const GURL& pattern : associated_patterns_)
     DecreaseProcessReference(pattern);
@@ -183,6 +180,8 @@ void ServiceWorkerProviderHost::OnSkippedWaiting(
 void ServiceWorkerProviderHost::SetDocumentUrl(const GURL& url) {
   DCHECK(!url.has_ref());
   document_url_ = url;
+  if (IsProviderForClient())
+    SyncMatchingRegistrations();
 }
 
 void ServiceWorkerProviderHost::SetTopmostFrameUrl(const GURL& url) {
@@ -303,19 +302,6 @@ void ServiceWorkerProviderHost::RemoveMatchingRegistration(
   DecreaseProcessReference(registration->pattern());
   registration->RemoveListener(this);
   matching_registrations_.erase(key);
-}
-
-void ServiceWorkerProviderHost::AddAllMatchingRegistrations() {
-  DCHECK(context_);
-  const std::map<int64_t, ServiceWorkerRegistration*>& registrations =
-      context_->GetLiveRegistrations();
-  for (const auto& key_registration : registrations) {
-    ServiceWorkerRegistration* registration = key_registration.second;
-    if (!registration->is_uninstalled() &&
-        ServiceWorkerUtils::ScopeMatches(registration->pattern(),
-                                         document_url_))
-      AddMatchingRegistration(registration);
-  }
 }
 
 ServiceWorkerRegistration*
@@ -598,6 +584,28 @@ void ServiceWorkerProviderHost::SendAssociateRegistrationMessage() {
   DCHECK(IsProviderForClient());
   dispatcher_host_->Send(new ServiceWorkerMsg_AssociateRegistration(
       render_thread_id_, provider_id(), handle->GetObjectInfo(), attrs));
+}
+
+void ServiceWorkerProviderHost::SyncMatchingRegistrations() {
+  DCHECK(context_);
+  RemoveAllMatchingRegistrations();
+  const auto& registrations = context_->GetLiveRegistrations();
+  for (const auto& key_registration : registrations) {
+    ServiceWorkerRegistration* registration = key_registration.second;
+    if (!registration->is_uninstalled() &&
+        ServiceWorkerUtils::ScopeMatches(registration->pattern(),
+                                         document_url_))
+      AddMatchingRegistration(registration);
+  }
+}
+
+void ServiceWorkerProviderHost::RemoveAllMatchingRegistrations() {
+  for (const auto& it : matching_registrations_) {
+    ServiceWorkerRegistration* registration = it.second.get();
+    DecreaseProcessReference(registration->pattern());
+    registration->RemoveListener(this);
+  }
+  matching_registrations_.clear();
 }
 
 void ServiceWorkerProviderHost::IncreaseProcessReference(
