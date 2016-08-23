ccf0243d54ea75b7a3c815e3b3f8e45c6fa3713d
yzshen <yzshen@chromium.org>
2016-08-23 16:19:54 -0700

    Mojo C++ bindings: clean up pending callbacks when connection error occurs.
    The callbacks may hold on to resources. There is no need to keep them after
    connection error occurs. They won't be called.
    
    BUG=640293
    
    Review-Url: https://codereview.chromium.org/2274743002
    Cr-Commit-Position: refs/heads/master@{#413890}

--- mojo/public/cpp/bindings/lib/router.cc
+++ mojo/public/cpp/bindings/lib/router.cc
@@ -247,6 +247,8 @@ void Router::HandleQueuedMessages() {
 }
 
 bool Router::HandleMessageInternal(Message* message) {
+  DCHECK(!encountered_error_);
+
   if (message->has_flag(Message::kFlagExpectsResponse)) {
     if (!incoming_receiver_)
       return false;
@@ -309,6 +311,11 @@ void Router::OnConnectionError() {
   }
 
   encountered_error_ = true;
+
+  // The callbacks may hold on to resources. There is no need to keep them any
+  // longer.
+  async_responders_.clear();
+
   if (!error_handler_.is_null())
     error_handler_.Run();
 }
