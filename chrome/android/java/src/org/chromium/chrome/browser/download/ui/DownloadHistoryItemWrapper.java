cc8d7a7ade17f79d9029e7c8431fbf0de3cf617a
dfalcantara <dfalcantara@chromium.org>
2016-08-23 19:53:07 -0700

    [Download Home] Add basic tests for the DownloadManagerUi
    Add tests that fire up the DownloadActivity on phones so that we can
    test the DownloadManagerUi.  The tests added check for:
    * Selecting/deleting multiple files
    * Filtering items via the sidebar
    * The space display being updated.
    
    Also changes the DownloadHistoryAdapter to ignore deleted files
    when computing the amount of space used.
    
    BUG=616324
    
    Review-Url: https://codereview.chromium.org/2276633002
    Cr-Commit-Position: refs/heads/master@{#413957}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryItemWrapper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryItemWrapper.java
@@ -19,8 +19,6 @@ import org.chromium.base.Log;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.download.DownloadItem;
-import org.chromium.chrome.browser.download.DownloadManagerService;
-import org.chromium.chrome.browser.download.ui.BackendProvider.OfflinePageDelegate;
 import org.chromium.chrome.browser.offlinepages.downloads.OfflinePageDownloadItem;
 import org.chromium.chrome.browser.widget.DateDividedAdapter.TimedItem;
 import org.chromium.ui.widget.Toast;
@@ -29,10 +27,15 @@ import java.io.File;
 import java.util.Locale;
 
 /** Wraps different classes that contain information about downloads. */
-abstract class DownloadHistoryItemWrapper implements TimedItem {
+public abstract class DownloadHistoryItemWrapper implements TimedItem {
     private static final String TAG = "download_ui";
 
+    protected final BackendProvider mBackendProvider;
     private Long mStableId;
+
+    private DownloadHistoryItemWrapper(BackendProvider provider) {
+        mBackendProvider = provider;
+    }
 
     @Override
     public long getStableId() {
@@ -104,10 +107,11 @@ abstract class DownloadHistoryItemWrapper implements TimedItem {
         private static final String MIMETYPE_DOCUMENT = "text";
 
         private final DownloadItem mItem;
-        private boolean mIsOffTheRecord;
+        private final boolean mIsOffTheRecord;
         private File mFile;
 
-        DownloadItemWrapper(DownloadItem item, boolean isOffTheRecord) {
+        DownloadItemWrapper(DownloadItem item, boolean isOffTheRecord, BackendProvider provider) {
+            super(provider);
             mItem = item;
             mIsOffTheRecord = isOffTheRecord;
         }
@@ -200,9 +204,7 @@ abstract class DownloadHistoryItemWrapper implements TimedItem {
         @Override
         public void delete(final Callback<Void> callback) {
             // Tell the DownloadManager to remove the file from history.
-            DownloadManagerService service = DownloadManagerService.getDownloadManagerService(
-                    ContextUtils.getApplicationContext());
-            service.removeDownload(getId(), mIsOffTheRecord);
+            mBackendProvider.getDownloadDelegate().removeDownload(getId(), mIsOffTheRecord);
 
             // Delete the file from storage.
             new AsyncTask<Void, Void, Void>() {
@@ -251,14 +253,13 @@ abstract class DownloadHistoryItemWrapper implements TimedItem {
     /** Wraps a {@link OfflinePageDownloadItem}. */
     static class OfflinePageItemWrapper extends DownloadHistoryItemWrapper {
         private final OfflinePageDownloadItem mItem;
-        private final OfflinePageDelegate mBridge;
         private final ComponentName mComponent;
         private File mFile;
 
-        OfflinePageItemWrapper(OfflinePageDownloadItem item, OfflinePageDelegate bridge,
+        OfflinePageItemWrapper(OfflinePageDownloadItem item, BackendProvider provider,
                 ComponentName component) {
+            super(provider);
             mItem = item;
-            mBridge = bridge;
             mComponent = component;
         }
 
@@ -321,13 +322,13 @@ abstract class DownloadHistoryItemWrapper implements TimedItem {
 
         @Override
         public void open() {
-            mBridge.openItem(getId(), mComponent);
+            mBackendProvider.getOfflinePageBridge().openItem(getId(), mComponent);
             recordOpenSuccess();
         }
 
         @Override
         public void delete(Callback<Void> callback) {
-            mBridge.deleteItem(getId());
+            mBackendProvider.getOfflinePageBridge().deleteItem(getId());
             callback.onResult(null);
         }
 
