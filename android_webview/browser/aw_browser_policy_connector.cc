7d3614ea6b1812e3d9aea2036c9dc9a0f4a6ac3b
ricea <ricea@chromium.org>
2016-08-23 19:43:17 -0700

    Re-write many calls to WrapUnique() with MakeUnique()
    A mostly-automated change to convert instances of WrapUnique(new Foo(...)) to
    MakeUnique<Foo>(...). See the thread at
    https://groups.google.com/a/chromium.org/forum/#!topic/chromium-dev/iQgMedVA8-k
    for background.
    
    To avoid requiring too many manual fixups, the change skips some cases that are
    frequently problematic. In particular, in methods named Foo::Method() it will
    not try to change WrapUnique(new Foo()) to MakeUnique<Foo>(). This is because
    Foo::Method() may be accessing an internal constructor of Foo.
    
    Cases where MakeUnique<NestedClass>(...) is called within a method of
    OuterClass are common but hard to detect automatically, so have been fixed-up
    manually.
    
    The only types of manual fix ups applied are:
    1) Revert MakeUnique back to WrapUnique
    2) Change NULL to nullptr in argument list (MakeUnique cannot forward NULL
       correctly)
    3) Add base:: namespace qualifier where missing.
    
    WrapUnique(new Foo) has not been converted to MakeUnique<Foo>() as this might
    change behaviour if Foo does not have a user-defined constructor. For example,
    WrapUnique(new int) creates an unitialised integer, but MakeUnique<int>()
    creates an integer initialised to 0.
    
    git cl format has been been run over the CL. Spot-checking has uncovered no
    cases of mis-formatting.
    
      BUG=637812
    
    Review-Url: https://codereview.chromium.org/2258823002
    Cr-Commit-Position: refs/heads/master@{#413953}

--- android_webview/browser/aw_browser_policy_connector.cc
+++ android_webview/browser/aw_browser_policy_connector.cc
@@ -38,19 +38,18 @@ std::unique_ptr<policy::ConfigurationPolicyHandlerList> BuildHandlerList(
           base::Bind(&GetChromePolicyDetails)));
 
   // URL Filtering
-  handlers->AddHandler(base::WrapUnique(new policy::SimplePolicyHandler(
+  handlers->AddHandler(base::MakeUnique<policy::SimplePolicyHandler>(
       policy::key::kURLWhitelist, policy::policy_prefs::kUrlWhitelist,
-      base::Value::TYPE_LIST)));
-  handlers->AddHandler(
-      base::WrapUnique(new policy::URLBlacklistPolicyHandler()));
+      base::Value::TYPE_LIST));
+  handlers->AddHandler(base::MakeUnique<policy::URLBlacklistPolicyHandler>());
 
   // HTTP Negotiate authentication
-  handlers->AddHandler(base::WrapUnique(new policy::SimplePolicyHandler(
+  handlers->AddHandler(base::MakeUnique<policy::SimplePolicyHandler>(
       policy::key::kAuthServerWhitelist, prefs::kAuthServerWhitelist,
-      base::Value::TYPE_STRING)));
-  handlers->AddHandler(base::WrapUnique(new policy::SimplePolicyHandler(
+      base::Value::TYPE_STRING));
+  handlers->AddHandler(base::MakeUnique<policy::SimplePolicyHandler>(
       policy::key::kAuthAndroidNegotiateAccountType,
-      prefs::kAuthAndroidNegotiateAccountType, base::Value::TYPE_STRING)));
+      prefs::kAuthAndroidNegotiateAccountType, base::Value::TYPE_STRING));
 
   // Web restrictions
   handlers->AddHandler(base::WrapUnique(new policy::SimplePolicyHandler(
@@ -64,8 +63,9 @@ std::unique_ptr<policy::ConfigurationPolicyHandlerList> BuildHandlerList(
 
 AwBrowserPolicyConnector::AwBrowserPolicyConnector()
    : BrowserPolicyConnectorBase(base::Bind(&BuildHandlerList)) {
-  SetPlatformPolicyProvider(base::WrapUnique(
-      new policy::android::AndroidCombinedPolicyProvider(GetSchemaRegistry())));
+  SetPlatformPolicyProvider(
+      base::MakeUnique<policy::android::AndroidCombinedPolicyProvider>(
+          GetSchemaRegistry()));
   InitPolicyProviders();
 }
 
--- android_webview/native/android_protocol_handler.cc
+++ android_webview/native/android_protocol_handler.cc
@@ -142,7 +142,7 @@ AndroidStreamReaderURLRequestJobDelegateImpl::OpenInputStream(JNIEnv* env,
     DLOG(ERROR) << "Unable to open input stream for Android URL";
     return std::unique_ptr<InputStream>();
   }
-  return base::WrapUnique(new InputStreamImpl(stream));
+  return base::MakeUnique<InputStreamImpl>(stream);
 }
 
 void AndroidStreamReaderURLRequestJobDelegateImpl::OnInputStreamOpenFailed(
@@ -252,7 +252,7 @@ bool RegisterAndroidProtocolHandler(JNIEnv* env) {
 // static
 std::unique_ptr<net::URLRequestInterceptor>
 CreateContentSchemeRequestInterceptor() {
-  return base::WrapUnique(new ContentSchemeRequestInterceptor());
+  return base::MakeUnique<ContentSchemeRequestInterceptor>();
 }
 
 // static
