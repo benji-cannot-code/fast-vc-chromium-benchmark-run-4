9da3a1d2e71487c1a2873c298670da682db42149
jkarlin <jkarlin@chromium.org>
2016-08-24 14:15:38 -0700

    [CacheStorage] Don't store open caches in Blink
    Blink objects were storing cache objects so that they could be returned quickly
    on subsequent open calls. This is problematic as there can be many contexts
    (different windows, workers, service workers, and dev tools) opening/deleting
    caches. If one of them deletes the cache then all of the others need to be
    invalidated. Rather than synchronize the various contexts, for now just remove
    the stored caches from Blink.
    
    BUG=639034
    
    Review-Url: https://codereview.chromium.org/2270413003
    Cr-Commit-Position: refs/heads/master@{#414147}

--- third_party/WebKit/Source/modules/cachestorage/CacheStorage.h
+++ third_party/WebKit/Source/modules/cachestorage/CacheStorage.h
@@ -54,7 +54,6 @@ private:
 
     Member<GlobalFetch::ScopedFetcher> m_scopedFetcher;
     std::unique_ptr<WebServiceWorkerCacheStorage> m_webCacheStorage;
-    HeapHashMap<String, Member<Cache>> m_nameToCacheMap;
 };
 
 } // namespace blink
