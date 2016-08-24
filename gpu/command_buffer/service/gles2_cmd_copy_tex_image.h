8c1f9e77e01b28e265fc489f2bf33cc011c7674e
yunchao.he <yunchao.he@intel.com>
2016-08-24 05:08:58 -0700

    [Command buffer] CopyTexSubImage3D: emulate unsized format in desktop core profile
    BUG=631934, 638470
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2241593003
    Cr-Commit-Position: refs/heads/master@{#414052}

--- gpu/command_buffer/service/gles2_cmd_copy_tex_image.h
+++ gpu/command_buffer/service/gles2_cmd_copy_tex_image.h
@@ -48,7 +48,7 @@ class GPU_EXPORT CopyTexImageResourceManager {
       GLuint source_framebuffer,
       GLenum source_framebuffer_internal_format);
 
-  void DoCopyTexSubImage2DToLUMACompatibilityTexture(
+  void DoCopyTexSubImageToLUMACompatibilityTexture(
       const gles2::GLES2Decoder* decoder,
       GLuint dest_texture,
       GLenum dest_texture_target,
@@ -58,6 +58,7 @@ class GPU_EXPORT CopyTexImageResourceManager {
       GLint level,
       GLint xoffset,
       GLint yoffset,
+      GLint zoffset,
       GLint x,
       GLint y,
       GLsizei width,
