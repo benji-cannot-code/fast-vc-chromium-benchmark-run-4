c216ad2144001d90b8d59e2ebbca1f015827d099
jdufault <jdufault@chromium.org>
2016-08-23 17:46:35 -0700

    Add screenshot related palette tools.
    BUG=625251
    
    Review-Url: https://codereview.chromium.org/2176273005
    Cr-Commit-Position: refs/heads/master@{#413910}

---
+++ ash/common/system/chromeos/palette/tools/capture_region_action.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_COMMON_SYSTEM_CHROMEOS_PALETTE_CAPTURE_REGION_ACTION_H_
+#define ASH_COMMON_SYSTEM_CHROMEOS_PALETTE_CAPTURE_REGION_ACTION_H_
+
+#include "ash/ash_export.h"
+#include "ash/common/system/chromeos/palette/common_palette_tool.h"
+
+namespace ash {
+
+class ASH_EXPORT CaptureRegionAction : public CommonPaletteTool {
+ public:
+  explicit CaptureRegionAction(Delegate* delegate);
+  ~CaptureRegionAction() override;
+
+ private:
+  // PaletteTool:
+  PaletteGroup GetGroup() const override;
+  PaletteToolId GetToolId() const override;
+  void OnEnable() override;
+  views::View* CreateView() override;
+
+  // CommonPaletteTool:
+  gfx::VectorIconId GetPaletteIconId() override;
+
+  DISALLOW_COPY_AND_ASSIGN(CaptureRegionAction);
+};
+
+}  // namespace ash
+
+#endif  // ASH_COMMON_SYSTEM_CHROMEOS_PALETTE_CAPTURE_REGION_ACTION_H_
--- chrome/browser/ui/ash/palette_delegate_chromeos.cc
+++ chrome/browser/ui/ash/palette_delegate_chromeos.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/ash/palette_delegate_chromeos.h"
 
+#include "ash/accelerators/accelerator_controller_delegate_aura.h"
+#include "ash/screenshot_delegate.h"
+#include "ash/shell.h"
+#include "ash/utility/screenshot_controller.h"
 #include "chrome/browser/chromeos/note_taking_app_utils.h"
 #include "chrome/browser/profiles/profile_manager.h"
 
@@ -27,6 +31,25 @@ void PaletteDelegateChromeOS::CreateNote() {
 
 bool PaletteDelegateChromeOS::HasNoteApp() {
   return chromeos::IsNoteTakingAppAvailable(GetProfile());
+}
+
+void PaletteDelegateChromeOS::TakeScreenshot() {
+  auto* screenshot_delegate = ash::Shell::GetInstance()
+                                  ->accelerator_controller_delegate()
+                                  ->screenshot_delegate();
+  screenshot_delegate->HandleTakeScreenshotForAllRootWindows();
+}
+
+void PaletteDelegateChromeOS::TakePartialScreenshot() {
+  auto* screenshot_controller =
+      ash::Shell::GetInstance()->screenshot_controller();
+  auto* screenshot_delegate = ash::Shell::GetInstance()
+                                  ->accelerator_controller_delegate()
+                                  ->screenshot_delegate();
+
+  screenshot_controller->set_pen_events_only(true);
+  screenshot_controller->StartPartialScreenshotSession(
+      screenshot_delegate, false /* draw_overlay_immediately */);
 }
 
 }  // namespace chromeos
