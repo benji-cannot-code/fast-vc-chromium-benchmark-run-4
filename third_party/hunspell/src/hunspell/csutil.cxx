8d66db2e997584798f29e54ec95c1a0396c9d0de
krb <krb@chromium.org>
2016-08-24 09:57:21 -0700

    Changed signed char* to unsigned char*, fuzzer fix
    Fuzzer reports undefined behavior precisely at the locations where
    it performs a left shift on a signed char. Changing it to a regular
    char makes the errors go away.
    
    Since the function is doing lots of bit manipulation, a signed char
    seems inappropriate anyways. All spellcheck tests pass with regular
    char.
    
    The upstream github history (that I could find) shows that it has
    always been signed, so I couldn't find a reason why it changed, if
    it ever did.
    
    BUG=620659,624348
    
    Review-Url: https://codereview.chromium.org/2263263003
    Cr-Commit-Position: refs/heads/master@{#414095}

--- third_party/hunspell/src/hunspell/csutil.cxx
+++ third_party/hunspell/src/hunspell/csutil.cxx
@@ -97,10 +97,10 @@ char * u16_u8(char * dest, int size, const w_char * src, int srclen) {
 
 /* only UTF-16 (BMP) implementation */
 int u8_u16(w_char * dest, int size, const char * src) {
-    const signed char * u8 = (const signed char *)src;
+    const unsigned char * u8 = (const unsigned char*)src;
     w_char * u2 = dest;
     w_char * u2_max = u2 + size;
-    
+
     while ((u2 < u2_max) && *u8) {
     switch ((*u8) & 0xf0) {
         case 0x00:
@@ -119,7 +119,7 @@ int u8_u16(w_char * dest, int size, const char * src) {
         case 0x90:
         case 0xa0:
         case 0xb0: {
-            HUNSPELL_WARNING(stderr, "UTF-8 encoding error. Unexpected continuation bytes in %ld. character position\n%s\n", static_cast<long>(u8 - (signed char *)src), src);    
+            HUNSPELL_WARNING(stderr, "UTF-8 encoding error. Unexpected continuation bytes in %ld. character position\n%s\n", static_cast<long>(u8 - (const unsigned char*)src), src);
             u2->h = 0xff;
             u2->l = 0xfd;
             break;
@@ -131,7 +131,7 @@ int u8_u16(w_char * dest, int size, const char * src) {
                 u2->l = (*u8 << 6) + (*(u8+1) & 0x3f);
                 u8++;
             } else {
-                HUNSPELL_WARNING(stderr, "UTF-8 encoding error. Missing continuation byte in %ld. character position:\n%s\n", static_cast<long>(u8 - (signed char *)src), src);
+                HUNSPELL_WARNING(stderr, "UTF-8 encoding error. Missing continuation byte in %ld. character position:\n%s\n", static_cast<long>(u8 - (const unsigned char*)src), src);
                 u2->h = 0xff;
                 u2->l = 0xfd;
             }
@@ -145,12 +145,12 @@ int u8_u16(w_char * dest, int size, const char * src) {
                     u2->l = (*u8 << 6) + (*(u8+1) & 0x3f);
                     u8++;
                 } else {
-                    HUNSPELL_WARNING(stderr, "UTF-8 encoding error. Missing continuation byte in %ld. character position:\n%s\n", static_cast<long>(u8 - (signed char *)src), src);
+                    HUNSPELL_WARNING(stderr, "UTF-8 encoding error. Missing continuation byte in %ld. character position:\n%s\n", static_cast<long>(u8 - (const unsigned char*)src), src);
                     u2->h = 0xff;
                     u2->l = 0xfd;
                 }
             } else {
-                HUNSPELL_WARNING(stderr, "UTF-8 encoding error. Missing continuation byte in %ld. character position:\n%s\n", static_cast<long>(u8 - (signed char *)src), src);
+                HUNSPELL_WARNING(stderr, "UTF-8 encoding error. Missing continuation byte in %ld. character position:\n%s\n", static_cast<long>(u8 - (const unsigned char*)src), src);
                 u2->h = 0xff;
                 u2->l = 0xfd;
             }
