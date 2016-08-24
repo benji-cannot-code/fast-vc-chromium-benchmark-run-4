199ed555e2ca855360c77a609b0dd89952d2898f
kylechar <kylechar@chromium.org>
2016-08-24 14:09:10 -0700

    Move GLImageOzoneNativePixmap tests to src/ui/ozone.
    GLImageOzoneNativePixmap was previously moved from //ui/gl to
    //ui/ozone/gl. The related tests were not moved at the same time. This
    CL moves the tests to a new target //ui/ozone/gl:ozone_gl_unittests.
    
    The exists ozone_unittests target does not have an appropriate TestSuite
    for running GL tests, so a different target is required. The existing
    TestSuite for GL tests is placed in a source_set so it can be used by
    both gl_unittests and ozone_gl_unittests.
    
    Add the new ozone_gl_unittests target to the Ozone Chrome OS trybot.
    Finally, remove some unneeded DEPS rules now that the tests have been
    moved.
    
    BUG=611142
    
    Review-Url: https://codereview.chromium.org/2254303002
    Cr-Commit-Position: refs/heads/master@{#414145}

--- ui/gl/test/run_all_unittests.cc
+++ ui/gl/test/run_all_unittests.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
 #include "build/build_config.h"
-#include "ui/gl/test/gl_image_test_support.h"
 
 #if defined(OS_MACOSX) && !defined(OS_IOS)
 #include "base/test/mock_chrome_application_mac.h"
--- ui/ozone/gl/gl_image_ozone_native_pixmap_unittest.cc
+++ ui/ozone/gl/gl_image_ozone_native_pixmap_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <memory>
+
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/buffer_types.h"
 #include "ui/gl/test/gl_image_test_template.h"
