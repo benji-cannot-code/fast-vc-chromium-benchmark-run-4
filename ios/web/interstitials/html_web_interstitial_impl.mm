287131cce5402e231c491c01ab13cf6a5786d993
eugenebut <eugenebut@chromium.org>
2016-08-24 11:44:38 -0700

    [ios] New API to execute JS on Interstitials.
    Added method which returns id instead of deprecated method which returns
    NSString. JS execution API returned NSString because that was the only
    supported option from UIWebView. Now WKWebView returns id which should
    be used everywhere.
    
    BUG=595761
    
    Review-Url: https://codereview.chromium.org/2269193003
    Cr-Commit-Position: refs/heads/master@{#414113}

--- ios/web/interstitials/html_web_interstitial_impl.mm
+++ ios/web/interstitials/html_web_interstitial_impl.mm
@@ -136,4 +136,10 @@ void HtmlWebInterstitialImpl::EvaluateJavaScript(
   web::EvaluateJavaScript(web_view_, script, completionHandler);
 }
 
+void HtmlWebInterstitialImpl::ExecuteJavaScript(
+    NSString* script,
+    JavaScriptResultBlock completion_handler) {
+  web::ExecuteJavaScript(web_view_, script, completion_handler);
+}
+
 }  // namespace web
