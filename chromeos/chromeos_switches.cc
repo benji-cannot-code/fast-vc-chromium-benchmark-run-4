8a76968481e9b17fcacef846fac96a02210d6c1f
lhchavez <lhchavez@chromium.org>
2016-08-23 17:35:56 -0700

    arc: Add the --arc-available switch
    A previous change added a Chrome Variation to allow enabling ARC.
    This change adds another flag to ensure that the board Chrome is running
    on actually has the bits to enable it and avoid showing people the UI
    when it is not actually supported.
    
    BUG=633704
    TEST=ARC still works
    
    Review-Url: https://codereview.chromium.org/2266123002
    Cr-Commit-Position: refs/heads/master@{#413908}

--- chromeos/chromeos_switches.cc
+++ chromeos/chromeos_switches.cc
@@ -59,6 +59,9 @@ const char kAllowRAInDevMode[] = "allow-ra-in-dev-mode";
 // Path for app's OEM manifest file.
 const char kAppOemManifestFile[] = "app-mode-oem-manifest";
 
+// Signals the availability of the ARC instance on this device.
+const char kArcAvailable[] = "arc-available";
+
 // Screenshot testing: specifies the directoru where artifacts will be stored.
 const char kArtifactsDir[] = "artifacts-dir";
 
