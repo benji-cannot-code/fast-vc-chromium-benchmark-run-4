0b65505cf7a9461b55dab83f38db93330dab6524
ericwilligers <ericwilligers@chromium.org>
2016-08-24 01:35:03 -0700

    CSS: Reliable style invalidation for custom pseudo-elements
    If two or more descendant invalidation sets were scheduled for an
    element, we have only been using the last one to decide if
    custom pseudo elements' styles should be invalidated.
    
    StyleInvalidator::RecursionData::pushInvalidationSet
    now treats customPseudoInvalid() in the same manner
    as other invalidation flags.
    
    Review-Url: https://codereview.chromium.org/2274783003
    Cr-Commit-Position: refs/heads/master@{#414021}

--- third_party/WebKit/Source/core/css/invalidation/StyleInvalidator.cpp
+++ third_party/WebKit/Source/core/css/invalidation/StyleInvalidator.cpp
@@ -151,6 +151,8 @@ void StyleInvalidator::RecursionData::pushInvalidationSet(const InvalidationSet&
     DCHECK(!m_wholeSubtreeInvalid);
     DCHECK(!invalidationSet.wholeSubtreeInvalid());
     DCHECK(!invalidationSet.isEmpty());
+    if (invalidationSet.customPseudoInvalid())
+        m_invalidateCustomPseudo = true;
     if (invalidationSet.treeBoundaryCrossing())
         m_treeBoundaryCrossing = true;
     if (invalidationSet.insertionPointCrossing())
@@ -158,7 +160,6 @@ void StyleInvalidator::RecursionData::pushInvalidationSet(const InvalidationSet&
     if (invalidationSet.invalidatesSlotted())
         m_invalidatesSlotted = true;
     m_invalidationSets.append(&invalidationSet);
-    m_invalidateCustomPseudo = invalidationSet.customPseudoInvalid();
 }
 
 ALWAYS_INLINE bool StyleInvalidator::RecursionData::matchesCurrentInvalidationSets(Element& element) const
