6f5186429310180315be1dca1591263ebb9963d5
fdoray <fdoray@chromium.org>
2016-08-25 08:04:24 -0700

    Return version of the generated file in GenerateAlternatePEFileVersion.
    BUG=451546
    
    Review-Url: https://codereview.chromium.org/2273103002
    Cr-Commit-Position: refs/heads/master@{#414437}

--- chrome/installer/test/alternate_version_generator.h
+++ chrome/installer/test/alternate_version_generator.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_INSTALLER_TEST_ALTERNATE_VERSION_GENERATOR_H_
 #define CHROME_INSTALLER_TEST_ALTERNATE_VERSION_GENERATOR_H_
 
-#include <string>
+#include "base/strings/string16.h"
 
 namespace base {
 class FilePath;
@@ -31,16 +31,18 @@ enum Direction {
 bool GenerateAlternateVersion(const base::FilePath& original_installer_path,
                               const base::FilePath& target_path,
                               Direction direction,
-                              std::wstring* original_version,
-                              std::wstring* new_version);
+                              base::string16* original_version,
+                              base::string16* new_version);
 
 // Given a path to a PEImage in |original_file|, copy that file to
 // |target_file|, modifying the version of the copy according to |direction|.
-// Any previous file at |target_file| is clobbered. Returns true on success.
-// Note that |target_file| may still be mutated on failure.
-bool GenerateAlternatePEFileVersion(const base::FilePath& original_file,
-                                    const base::FilePath& target_file,
-                                    Direction direction);
+// Any previous file at |target_file| is clobbered. On success, returns the
+// version of the generated file. Otherwise, returns an empty string. Note that
+// |target_file| may still be mutated on failure.
+base::string16 GenerateAlternatePEFileVersion(
+    const base::FilePath& original_file,
+    const base::FilePath& target_file,
+    Direction direction);
 
 // Given a path to a PEImage in |original_file|, copy that file to
 // |target_file|, modifying the version of the copy according to |version|.
