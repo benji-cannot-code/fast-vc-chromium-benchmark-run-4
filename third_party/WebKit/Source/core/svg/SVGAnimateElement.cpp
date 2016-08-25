9d4a10b2a085e0b0c35929d2fa2de858445749ba
fs <fs@opera.com>
2016-08-25 01:17:31 -0700

    Simplify SMIL animation CSS property updates
    After https://codereview.chromium.org/2251073002 we should no longer
    need to walk any <use> instances "manually", since setNeedsStyleRecalc
    does that already. This should also avoid the potential double-work
    because of this.
    
    TEST=svg/W3C-SVG-1.1/animate-elem-30-t.svg
    TEST=svg/W3C-SVG-1.1/animate-elem-40-t.svg
    
    BUG=166438,355359,484552,637310
    
    Review-Url: https://codereview.chromium.org/2270363004
    Cr-Commit-Position: refs/heads/master@{#414358}

--- third_party/WebKit/Source/core/svg/SVGAnimateElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGAnimateElement.cpp
@@ -209,21 +209,6 @@ void SVGAnimateElement::resetAnimatedType()
     m_animatedProperty = m_animator.constructFromString(baseValue);
 }
 
-static inline void applyCSSPropertyToTarget(SVGElement* targetElement, CSSPropertyID id, const String& value)
-{
-    MutableStylePropertySet* propertySet = targetElement->ensureAnimatedSMILStyleProperties();
-    if (!propertySet->setProperty(id, value, false, 0))
-        return;
-
-    targetElement->setNeedsStyleRecalc(LocalStyleChange, StyleChangeReasonForTracing::create(StyleChangeReason::Animation));
-}
-
-static inline void removeCSSPropertyFromTarget(SVGElement* targetElement, CSSPropertyID id)
-{
-    targetElement->ensureAnimatedSMILStyleProperties()->removeProperty(id);
-    targetElement->setNeedsStyleRecalc(LocalStyleChange, StyleChangeReasonForTracing::create(StyleChangeReason::Animation));
-}
-
 static inline void applyCSSPropertyToTargetAndInstances(SVGElement* targetElement, const QualifiedName& attributeName, const String& valueAsString)
 {
     ASSERT(targetElement);
@@ -231,16 +216,11 @@ static inline void applyCSSPropertyToTargetAndInstances(SVGElement* targetElemen
         return;
 
     CSSPropertyID id = cssPropertyID(attributeName.localName());
+    MutableStylePropertySet* propertySet = targetElement->ensureAnimatedSMILStyleProperties();
+    if (!propertySet->setProperty(id, valueAsString, false, 0))
+        return;
 
-    SVGElement::InstanceUpdateBlocker blocker(targetElement);
-    applyCSSPropertyToTarget(targetElement, id, valueAsString);
-
-    // If the target element has instances, update them as well, w/o requiring the <use> tree to be rebuilt.
-    const HeapHashSet<WeakMember<SVGElement>>& instances = targetElement->instancesForElement();
-    for (SVGElement* shadowTreeElement : instances) {
-        if (shadowTreeElement)
-            applyCSSPropertyToTarget(shadowTreeElement, id, valueAsString);
-    }
+    targetElement->setNeedsStyleRecalc(LocalStyleChange, StyleChangeReasonForTracing::create(StyleChangeReason::Animation));
 }
 
 static inline void removeCSSPropertyFromTargetAndInstances(SVGElement* targetElement, const QualifiedName& attributeName)
@@ -250,16 +230,8 @@ static inline void removeCSSPropertyFromTargetAndInstances(SVGElement* targetEle
         return;
 
     CSSPropertyID id = cssPropertyID(attributeName.localName());
-
-    SVGElement::InstanceUpdateBlocker blocker(targetElement);
-    removeCSSPropertyFromTarget(targetElement, id);
-
-    // If the target element has instances, update them as well, w/o requiring the <use> tree to be rebuilt.
-    const HeapHashSet<WeakMember<SVGElement>>& instances = targetElement->instancesForElement();
-    for (SVGElement* shadowTreeElement : instances) {
-        if (shadowTreeElement)
-            removeCSSPropertyFromTarget(shadowTreeElement, id);
-    }
+    targetElement->ensureAnimatedSMILStyleProperties()->removeProperty(id);
+    targetElement->setNeedsStyleRecalc(LocalStyleChange, StyleChangeReasonForTracing::create(StyleChangeReason::Animation));
 }
 
 static inline void notifyTargetAboutAnimValChange(SVGElement* targetElement, const QualifiedName& attributeName)
