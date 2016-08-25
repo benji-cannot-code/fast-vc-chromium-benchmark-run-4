52ea8c8d5192cdfb922fc57ba0777e21416582cd
flandy <flandy@google.com>
2016-08-25 16:07:47 -0700

    DevTools: Box-shadow editor initial implementation
    This is dependent on the model in https://codereview.chromium.org/2230183004/
    and will be changed accordingly.
    
    BUG=639095
    
    Review-Url: https://codereview.chromium.org/2252913002
    Cr-Commit-Position: refs/heads/master@{#414575}

--- third_party/WebKit/Source/devtools/front_end/common/CSSShadowModel.js
+++ third_party/WebKit/Source/devtools/front_end/common/CSSShadowModel.js
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * @constructor
+ * @param {boolean} isBoxShadow
  */
-WebInspector.CSSShadowModel = function()
+WebInspector.CSSShadowModel = function(isBoxShadow)
 {
+    this._isBoxShadow = isBoxShadow;
     this._inset = false;
     this._offsetX = WebInspector.CSSLength.zero();
     this._offsetY = WebInspector.CSSLength.zero();
@@ -60,11 +62,17 @@ WebInspector.CSSShadowModel.prototype = {
 
     /**
      * @param {!WebInspector.CSSLength} offsetX
-     * @param {!WebInspector.CSSLength} offsetY
      */
-    setOffset: function(offsetX, offsetY)
+    setOffsetX: function(offsetX)
     {
         this._offsetX = offsetX;
+    },
+
+    /**
+     * @param {!WebInspector.CSSLength} offsetY
+     */
+    setOffsetY: function(offsetY)
+    {
         this._offsetY = offsetY;
     },
 
@@ -101,6 +109,14 @@ WebInspector.CSSShadowModel.prototype = {
         this._color = color;
         if (this._format.indexOf(WebInspector.CSSShadowModel._Part.Color) === -1)
             this._format.push(WebInspector.CSSShadowModel._Part.Color);
+    },
+
+    /**
+     * @return {boolean}
+     */
+    isBoxShadow: function()
+    {
+        return this._isBoxShadow;
     },
 
     /**
@@ -198,7 +214,7 @@ WebInspector.CSSShadowModel._parseShadow = function(text, isBoxShadow)
 
     var shadows = [];
     for (var i = 0; i < shadowTexts.length; i++) {
-        var shadow = new WebInspector.CSSShadowModel();
+        var shadow = new WebInspector.CSSShadowModel(isBoxShadow);
         shadow._format = [];
         var nextPartAllowed = true;
         var regexes = [/inset/gi, WebInspector.Color.Regex, WebInspector.CSSLength.Regex];
