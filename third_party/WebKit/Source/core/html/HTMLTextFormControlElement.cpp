1be207cfdaeb14065ab76b6e86af140b86caace4
tkent <tkent@chromium.org>
2016-08-25 09:22:12 -0700

    Fix selectionDirection after setting value of TEXTAREA/INPUT.
    slectionDirection wrongly returned "none" on non-Mac platforms in such
    case. This CL
    
    - makes sure that selection operations are done by setSelectionRange().
    - moves direction normalization code from setSelectionRangeForBinding() to
      setSelectionRange().
    
    Also, this makes cacheSelection() private. Only HTMLTextFormControlElement calls
    it.
    
    BUG=640861
    
    Review-Url: https://codereview.chromium.org/2277903003
    Cr-Commit-Position: refs/heads/master@{#414451}

--- third_party/WebKit/Source/core/html/HTMLTextFormControlElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLTextFormControlElement.cpp
@@ -271,11 +271,7 @@ void HTMLTextFormControlElement::setSelectionRangeForBinding(int start, int end,
         direction = SelectionHasForwardDirection;
     else if (directionString == "backward")
         direction = SelectionHasBackwardDirection;
-
-    if (direction == SelectionHasNoDirection && document().frame() && document().frame()->editor().behavior().shouldConsiderSelectionAsDirectional())
-        direction = SelectionHasForwardDirection;
-
-    return setSelectionRange(start, end, direction);
+    setSelectionRange(start, end, direction);
 }
 
 static Position positionForIndex(HTMLElement* innerEditor, int index)
@@ -352,6 +348,9 @@ void HTMLTextFormControlElement::setSelectionRange(int start, int end, TextField
     DCHECK_GE(editorValueLength, 0);
     end = std::max(std::min(end, editorValueLength), 0);
     start = std::min(std::max(start, 0), end);
+    LocalFrame* frame = document().frame();
+    if (direction == SelectionHasNoDirection && frame && frame->editor().behavior().shouldConsiderSelectionAsDirectional())
+        direction = SelectionHasForwardDirection;
     cacheSelection(start, end, direction);
 
     if (document().focusedElement() != this) {
@@ -360,7 +359,6 @@ void HTMLTextFormControlElement::setSelectionRange(int start, int end, TextField
         return;
     }
 
-    LocalFrame* frame = document().frame();
     HTMLElement* innerEditor = innerEditorElement();
     if (!frame || !innerEditor)
         return;
@@ -472,11 +470,10 @@ static const AtomicString& directionString(TextFieldSelectionDirection direction
 
 const AtomicString& HTMLTextFormControlElement::selectionDirection() const
 {
-    if (!isTextFormControl())
-        return directionString(SelectionHasNoDirection);
+    // Ensured by HTMLInputElement::selectionDirectionForBinding().
+    DCHECK(isTextFormControl());
     if (document().focusedElement() != this)
         return directionString(m_cachedSelectionDirection);
-
     return directionString(computeSelectionDirection());
 }
 
