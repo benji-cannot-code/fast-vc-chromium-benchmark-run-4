1addcc5ded41432c596a3e5f7e0217a86ffb1cb7
chenwilliam <chenwilliam@chromium.org>
2016-08-24 17:29:44 -0700

    DevTools: Simplify DOMBreakpointsSidebarPane inspected URL
    Previously the restoreBreakpoints method implictly relied on the inspected URL being set
    to the current inspected URL. This CL simplifies the logic for setting the inspected URL
    and ensures that the inspected URL is always accurate in the process of restoring breakpoints.
    
    BUG=627296
    
    Review-Url: https://codereview.chromium.org/2277543002
    Cr-Commit-Position: refs/heads/master@{#414219}

--- third_party/WebKit/Source/devtools/front_end/components/DOMBreakpointsSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/components/DOMBreakpointsSidebarPane.js
@@ -58,21 +58,12 @@ WebInspector.DOMBreakpointsSidebarPane = function()
     this._contextMenuLabels[this._breakpointTypes.NodeRemoved] = WebInspector.UIString.capitalize("Node ^removal");
 
     WebInspector.targetManager.addModelListener(WebInspector.DOMModel, WebInspector.DOMModel.Events.NodeRemoved, this._nodeRemoved, this);
-    WebInspector.targetManager.addEventListener(WebInspector.TargetManager.Events.InspectedURLChanged, this._inspectedURLChanged, this);
-    this._inspectedURL = WebInspector.targetManager.inspectedURL();
     this._update();
 }
 
 WebInspector.DOMBreakpointsSidebarPane.Marker = "breakpoint-marker";
 
 WebInspector.DOMBreakpointsSidebarPane.prototype = {
-    _inspectedURLChanged: function()
-    {
-        this._breakpointElements = {};
-        this.reset();
-        this._inspectedURL = WebInspector.targetManager.inspectedURL();
-    },
-
     /**
      * @param {!WebInspector.DOMNode} node
      * @param {!WebInspector.ContextMenu} contextMenu
@@ -376,10 +367,14 @@ WebInspector.DOMBreakpointsSidebarPane.prototype = {
     },
 
     /**
-     * @param {!WebInspector.DOMModel} domModel
+     * @param {!WebInspector.DOMDocument} domDocument
      */
-    restoreBreakpoints: function(domModel)
+    restoreBreakpoints: function(domDocument)
     {
+        this._breakpointElements = {};
+        this.reset();
+        this._inspectedURL = domDocument.documentURL;
+        var domModel = domDocument.domModel();
         var pathToBreakpoints = {};
 
         /**
--- third_party/WebKit/Source/devtools/front_end/elements/ElementsPanel.js
+++ third_party/WebKit/Source/devtools/front_end/elements/ElementsPanel.js
@@ -383,7 +383,7 @@ WebInspector.ElementsPanel.prototype = {
             return;
         }
 
-        WebInspector.domBreakpointsSidebarPane.restoreBreakpoints(domModel);
+        WebInspector.domBreakpointsSidebarPane.restoreBreakpoints(inspectedRootDocument);
 
         /**
          * @this {WebInspector.ElementsPanel}
