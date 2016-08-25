6e6566dc430a4a83a3213493d3514c6b351f1d77
lgarron <lgarron@chromium.org>
2016-08-24 23:58:38 -0700

    Fix a comment in the GridLayout explanation (refer to column set rather than colum).
    BUG=
    
    Review-Url: https://codereview.chromium.org/2140103002
    Cr-Commit-Position: refs/heads/master@{#414347}

--- ui/views/layout/grid_layout.h
+++ ui/views/layout/grid_layout.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // define the structure of the Grid first, then add the Views.
 // The following creates a trivial grid with two columns separated by
 // a column with padding:
-// ColumnSet* columns = layout->AddColumnSet(0); // Give this column an
+// ColumnSet* columns = layout->AddColumnSet(0); // Give this column set an
 //                                               // identifier of 0.
 // columns->AddColumn(FILL, // Views are horizontally resized to fill column.
 //                    FILL, // Views starting in this column are vertically
