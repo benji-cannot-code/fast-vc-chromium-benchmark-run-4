24469bfafa1578b41363560a81f8af093c4df4fe
dvadym <dvadym@chromium.org>
2016-08-25 03:59:20 -0700

    Improvement of password manager logging.
    It's nice to have more information in password manager logging in order to debug password generation in Chrome. This CL updates FormStructure presentation.
    
    For example Linkedin SignUp form has the following presentation in logs:
    
    Adding manager for form: {
    Signature of form: 5301209056338178556
    Signon realm: https://www.linkedin.com/
    Origin: https://www.linkedin.com/
    Action: https://www.linkedin.com/
    Form name:
    Form fields:
    firstname: 1855613035, text
    lastname: 4163345999, text
    emailaddress: 4103469401, email
    password: 2051817934, password, off
    }
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2268313003
    Cr-Commit-Position: refs/heads/master@{#414387}

--- components/password_manager/core/browser/browser_save_password_progress_logger.cc
+++ components/password_manager/core/browser/browser_save_password_progress_logger.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/browser_save_password_progress_logger.h"
 
 #include "base/strings/string_util.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/common/password_form.h"
@@ -35,18 +36,32 @@ void BrowserSavePasswordProgressLogger::LogFormSignatures(
     SavePasswordProgressLogger::StringID label,
     const autofill::PasswordForm& form) {
   autofill::FormStructure form_structure(form.form_data);
-  base::DictionaryValue log;
-  log.SetString(GetStringFromID(STRING_FORM_SIGNATURE),
-                ScrubNonDigit(form_structure.FormSignature()));
+  std::string message = GetStringFromID(label) + ": {\n";
+  message += GetStringFromID(STRING_FORM_SIGNATURE) + ": " +
+             ScrubNonDigit(form_structure.FormSignature()) + "\n";
+  message += GetStringFromID(STRING_SIGNON_REALM) + ": " +
+             ScrubURL(GURL(form.signon_realm)) + "\n";
+  message +=
+      GetStringFromID(STRING_ORIGIN) + ": " + ScrubURL(form.origin) + "\n";
+  message +=
+      GetStringFromID(STRING_ACTION) + ": " + ScrubURL(form.action) + "\n";
+  message += GetStringFromID(STRING_FORM_NAME) + ": " +
+             ScrubElementID(form.form_data.name) + "\n";
+  message += GetStringFromID(STRING_FIELDS) + ": " + "\n";
+
   for (const autofill::AutofillField* field : form_structure) {
-    log.SetString(ScrubElementID(field->name),
-                  ScrubNonDigit(field->FieldSignature()));
+    std::string field_info = ScrubElementID(field->name) + ": " +
+                             ScrubNonDigit(field->FieldSignature()) + ", " +
+                             ScrubElementID(field->form_control_type);
+    if (!field->autocomplete_attribute.empty())
+      field_info += ", " + ScrubElementID(field->autocomplete_attribute);
+    message += field_info + "\n";
   }
-  LogValue(label, log);
+  message += "}";
+  SendLog(message);
 }
 
-BrowserSavePasswordProgressLogger::~BrowserSavePasswordProgressLogger() {
-}
+BrowserSavePasswordProgressLogger::~BrowserSavePasswordProgressLogger() {}
 
 void BrowserSavePasswordProgressLogger::SendLog(const std::string& log) {
   log_manager_->LogSavePasswordProgress(log);
