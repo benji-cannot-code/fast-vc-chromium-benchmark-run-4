dc81baa6e51d3aa9b550189cacd33d44c49ca822
brianderson <brianderson@chromium.org>
2016-08-24 23:35:19 -0700

    gl: Disable SGI_video_sync by default.
    Disables the SGI_video_sync extension and assumes a
    vsync interval of 59.9Hz on Linux.
    
    Also adds an --enable-sgi-video-sync command line flag to
    re-enable the extension.
    
    Use of the extension on a thread other than the main GPU
    thread is encountering issues with Chrome's sandbox for
    certain combinations of Linux window managers and NVIDIA
    drivers.
    
    Before this patch, we were assuming a frequency of 60Hz
    on Linux and re-aligning the phase every 5 seconds.
    Since we can no longer align the phase, we assume a
    frequency of 59.9 Hz. On a 60Hz monitor, this avoids
    driver backpressure on the GPU thread at the cost of
    skipping a frame every 10 seconds.
    
    BUG=636644
    
    Review-Url: https://codereview.chromium.org/2277883003
    Cr-Commit-Position: refs/heads/master@{#414342}

---
+++ ui/gfx/vsync_provider.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/gfx/vsync_provider.h"
+
+namespace gfx {
+
+void FixedVSyncProvider::GetVSyncParameters(
+    const UpdateVSyncCallback& callback) {
+  callback.Run(timebase_, interval_);
+}
+
+}  // namespace gfx
