0fe802a80bdda3413716e3c2b229d9ef1d7a4181
cbiesinger <cbiesinger@chromium.org>
2016-08-25 11:28:28 -0700

    [layoutng] Rename struct members to match style guide
    This was focused on ng_units and ng_margin_strut, though I also updated
    some more variables in ng_block_layout_algorithm.
    
    https://google.github.io/styleguide/cppguide.html#Variable_Names
    
    R=ikilpatrick@chromium.org,eae@chromium.org
    BUG=635619
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2272313004
    Cr-Commit-Position: refs/heads/master@{#414483}

--- third_party/WebKit/Source/core/layout/ng/ng_length_utils.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_length_utils.cc
@@ -22,7 +22,7 @@ LayoutUnit resolveInlineLength(const NGConstraintSpace& constraintSpace,
                                LengthResolveType type) {
   // TODO(layout-ng): Handle min/max/fit-content
   DCHECK(!length.isMaxSizeNone());
-  DCHECK_GE(constraintSpace.ContainerSize().inlineSize, LayoutUnit());
+  DCHECK_GE(constraintSpace.ContainerSize().inline_size, LayoutUnit());
 
   if (type == LengthResolveType::MinSize && length.isAuto())
     return LayoutUnit();
@@ -30,7 +30,7 @@ LayoutUnit resolveInlineLength(const NGConstraintSpace& constraintSpace,
   if (type == LengthResolveType::MarginSize && length.isAuto())
     return LayoutUnit();
 
-  return valueForLength(length, constraintSpace.ContainerSize().inlineSize);
+  return valueForLength(length, constraintSpace.ContainerSize().inline_size);
 }
 
 LayoutUnit resolveBlockLength(const NGConstraintSpace& constraintSpace,
@@ -52,17 +52,17 @@ LayoutUnit resolveBlockLength(const NGConstraintSpace& constraintSpace,
   // Make sure that indefinite percentages resolve to NGSizeIndefinite, not to
   // a random negative number.
   if (length.hasPercent() &&
-      constraintSpace.ContainerSize().blockSize == NGSizeIndefinite)
+      constraintSpace.ContainerSize().block_size == NGSizeIndefinite)
     return contentSize;
 
-  return valueForLength(length, constraintSpace.ContainerSize().blockSize);
+  return valueForLength(length, constraintSpace.ContainerSize().block_size);
 }
 
 LayoutUnit computeInlineSizeForFragment(
     const NGConstraintSpace& constraintSpace,
     const ComputedStyle& style) {
   if (constraintSpace.fixedInlineSize())
-    return constraintSpace.ContainerSize().inlineSize;
+    return constraintSpace.ContainerSize().inline_size;
 
   LayoutUnit extent = resolveInlineLength(constraintSpace, style.logicalWidth(),
                                           LengthResolveType::ContentSize);
@@ -89,7 +89,7 @@ LayoutUnit computeBlockSizeForFragment(const NGConstraintSpace& constraintSpace,
                                        const ComputedStyle& style,
                                        LayoutUnit contentSize) {
   if (constraintSpace.fixedBlockSize())
-    return constraintSpace.ContainerSize().blockSize;
+    return constraintSpace.ContainerSize().block_size;
 
   LayoutUnit extent =
       resolveBlockLength(constraintSpace, style.logicalHeight(), contentSize,
@@ -122,14 +122,14 @@ NGBoxMargins computeMargins(const NGConstraintSpace& constraintSpace,
   // Margins always get computed relative to the inline size:
   // https://www.w3.org/TR/CSS2/box.html#value-def-margin-width
   NGBoxMargins margins;
-  margins.inlineStart = resolveInlineLength(
+  margins.inline_start = resolveInlineLength(
       constraintSpace, style.marginStart(), LengthResolveType::MarginSize);
-  margins.inlineEnd = resolveInlineLength(constraintSpace, style.marginEnd(),
-                                          LengthResolveType::MarginSize);
-  margins.blockStart = resolveInlineLength(
+  margins.inline_end = resolveInlineLength(constraintSpace, style.marginEnd(),
+                                           LengthResolveType::MarginSize);
+  margins.block_start = resolveInlineLength(
       constraintSpace, style.marginBefore(), LengthResolveType::MarginSize);
-  margins.blockEnd = resolveInlineLength(constraintSpace, style.marginAfter(),
-                                         LengthResolveType::MarginSize);
+  margins.block_end = resolveInlineLength(constraintSpace, style.marginAfter(),
+                                          LengthResolveType::MarginSize);
   return margins;
 }
 
