cf13996d4311c1cb9994b9c25a2c6b2ec71f1528
waffles <waffles@chromium.org>
2016-08-25 13:01:40 -0700

    Change pepper flash name.
    ("pepper_flash" is displaying in plugin placeholders; we prefer
    "Adobe Flash Player").
    
    BUG=626107
    
    Review-Url: https://codereview.chromium.org/2284443002
    Cr-Commit-Position: refs/heads/master@{#414516}

--- chrome/browser/component_updater/pepper_flash_component_installer.cc
+++ chrome/browser/component_updater/pepper_flash_component_installer.cc
@@ -248,7 +248,7 @@ void FlashComponentInstallerTraits::GetHash(std::vector<uint8_t>* hash) const {
 }
 
 std::string FlashComponentInstallerTraits::GetName() const {
-  return "pepper_flash";
+  return "Adobe Flash Player";
 }
 
 update_client::InstallerAttributes
