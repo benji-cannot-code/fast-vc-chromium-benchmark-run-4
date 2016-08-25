b80e27fa957d168050828f6a3ae7ad91562872f9
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2016-08-25 10:40:50 +0200

    chromium: sync DEPS with the build copy
    This will ensure we don't run into missing dependencies.
    
    BUG=584079
    R=emso@chromium.org
    
    Review URL: https://codereview.chromium.org/2278893002 .
    
    Cr-Commit-Position: refs/heads/master@{#414364}

--- infra/recipes/chromium.py
+++ infra/recipes/chromium.py
@@ -4,13 +4,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 DEPS = [
+  'build/adb',
+  'build/bisect_tester',
   'build/chromium',
+  'build/chromium_android',
   'build/chromium_swarming',
   'build/chromium_tests',
+  'build/commit_position',
+  'build/file',
+  'build/gsutil',
+  'build/isolate',
+  'build/swarming',
   'build/test_results',
   'build/test_utils',
+  'depot_tools/bot_update',
+  'recipe_engine/json',
+  'recipe_engine/path',
+  'recipe_engine/platform',
   'recipe_engine/properties',
+  'recipe_engine/python',
+  'recipe_engine/raw_io',
   'recipe_engine/step',
+  'recipe_engine/tempfile',
 ]
 
 
