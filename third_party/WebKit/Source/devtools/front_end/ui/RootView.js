e4d81aafccaf5850e51cd76470a0a54276d7ccff
dgozman <dgozman@chromium.org>
2016-08-25 12:42:11 -0700

    [DevTools] Remove a small part of inspectorStyle.css.
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2276163002
    Cr-Commit-Position: refs/heads/master@{#414513}

--- third_party/WebKit/Source/devtools/front_end/ui/RootView.js
+++ third_party/WebKit/Source/devtools/front_end/ui/RootView.js
@@ -12,6 +12,7 @@ WebInspector.RootView = function()
     WebInspector.VBox.call(this);
     this.markAsRoot();
     this.element.classList.add("root-view");
+    this.registerRequiredCSS("ui/rootView.css");
     this.element.setAttribute("spellcheck", false);
 }
 
