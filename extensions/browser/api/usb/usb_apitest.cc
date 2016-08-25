6577bcbdb329251a432e83e55985cc912ad0d9ea
rkc <rkc@chromium.org>
2016-08-25 15:49:12 -0700

    Remove unused include.
    Creates a fake dependency on //chrome.
    
    R=reillyg@chromium.org
    BUG=None.
    
    Review-Url: https://codereview.chromium.org/2279083002
    Cr-Commit-Position: refs/heads/master@{#414567}

--- extensions/browser/api/usb/usb_apitest.cc
+++ extensions/browser/api/usb/usb_apitest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <numeric>
 
 #include "base/memory/ptr_util.h"
-#include "chrome/browser/extensions/extension_apitest.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/test_utils.h"
 #include "device/core/mock_device_client.h"
