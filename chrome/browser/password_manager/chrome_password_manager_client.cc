988a8363b79d84d4ea1f1dbd5cd41869896589ae
dvadym <dvadym@chromium.org>
2016-08-25 00:36:23 -0700

    Turning on change password form support in Password Manageron Android by default.
    On https://codereview.chromium.org/2256653002/ it was implemented last fix to UI of change passowrd forms on Android. Since it seems no other significant problems are present from launching change password support on Android, this CL turns on change password support by default, without flag. The flag is not removed yet, just in case, if some problems will be found. It's going to be quite big CL for removing a flag and cleaning all places which depend on it, it will be done in a month, when it will be clear that everything is ok.
    
    BUG=564674
    
    Review-Url: https://codereview.chromium.org/2274913002
    Cr-Commit-Position: refs/heads/master@{#414350}

--- chrome/browser/password_manager/chrome_password_manager_client.cc
+++ chrome/browser/password_manager/chrome_password_manager_client.cc
@@ -556,12 +556,7 @@ void ChromePasswordManagerClient::GenerationAvailableForForm(
 }
 
 bool ChromePasswordManagerClient::IsUpdatePasswordUIEnabled() const {
-#if BUILDFLAG(ANDROID_JAVA_UI)
-  return base::FeatureList::IsEnabled(
-      password_manager::features::kEnablePasswordChangeSupport);
-#else
   return true;
-#endif
 }
 
 const GURL& ChromePasswordManagerClient::GetMainFrameURL() const {
