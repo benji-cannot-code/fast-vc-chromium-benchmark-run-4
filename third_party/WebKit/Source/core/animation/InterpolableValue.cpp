e8b8a6f640702fb325b07136e025c40050477593
alancutter <alancutter@chromium.org>
2016-08-24 22:41:13 -0700

    Remove redundant size member from InterpolableList
    The size of the list can be read from the m_values vector, no need to
    store the size separately.
    
    Review-Url: https://codereview.chromium.org/2273233002
    Cr-Commit-Position: refs/heads/master@{#414330}

--- third_party/WebKit/Source/core/animation/InterpolableValue.cpp
+++ third_party/WebKit/Source/core/animation/InterpolableValue.cpp
@@ -17,9 +17,9 @@ bool InterpolableNumber::equals(const InterpolableValue& other) const
 bool InterpolableList::equals(const InterpolableValue& other) const
 {
     const InterpolableList& otherList = toInterpolableList(other);
-    if (m_size != otherList.m_size)
+    if (length() != otherList.length())
         return false;
-    for (size_t i = 0; i < m_size; i++) {
+    for (size_t i = 0; i < length(); i++) {
         if (!m_values[i]->equals(*otherList.m_values[i]))
             return false;
     }
@@ -55,10 +55,10 @@ void InterpolableList::interpolate(const InterpolableValue& to, const double pro
     const InterpolableList& toList = toInterpolableList(to);
     InterpolableList& resultList = toInterpolableList(result);
 
-    DCHECK_EQ(toList.m_size, m_size);
-    DCHECK_EQ(resultList.m_size, m_size);
+    DCHECK_EQ(toList.length(), length());
+    DCHECK_EQ(resultList.length(), length());
 
-    for (size_t i = 0; i < m_size; i++) {
+    for (size_t i = 0; i < length(); i++) {
         DCHECK(m_values[i]);
         DCHECK(toList.m_values[i]);
         m_values[i]->interpolate(*(toList.m_values[i]), progress, *(resultList.m_values[i]));
@@ -67,8 +67,8 @@ void InterpolableList::interpolate(const InterpolableValue& to, const double pro
 
 std::unique_ptr<InterpolableValue> InterpolableList::cloneAndZero() const
 {
-    std::unique_ptr<InterpolableList> result = InterpolableList::create(m_size);
-    for (size_t i = 0; i < m_size; i++)
+    std::unique_ptr<InterpolableList> result = InterpolableList::create(length());
+    for (size_t i = 0; i < length(); i++)
         result->set(i, m_values[i]->cloneAndZero());
     return std::move(result);
 }
@@ -80,7 +80,7 @@ void InterpolableNumber::scale(double scale)
 
 void InterpolableList::scale(double scale)
 {
-    for (size_t i = 0; i < m_size; i++)
+    for (size_t i = 0; i < length(); i++)
         m_values[i]->scale(scale);
 }
 
@@ -92,8 +92,8 @@ void InterpolableNumber::scaleAndAdd(double scale, const InterpolableValue& othe
 void InterpolableList::scaleAndAdd(double scale, const InterpolableValue& other)
 {
     const InterpolableList& otherList = toInterpolableList(other);
-    DCHECK_EQ(otherList.m_size, m_size);
-    for (size_t i = 0; i < m_size; i++)
+    DCHECK_EQ(otherList.length(), length());
+    for (size_t i = 0; i < length(); i++)
         m_values[i]->scaleAndAdd(scale, *otherList.m_values[i]);
 }
 
