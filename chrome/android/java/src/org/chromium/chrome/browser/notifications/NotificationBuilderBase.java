db562e527cb59fbfc10a285a6b086f7c06c5ce4a
johnme <johnme@chromium.org>
2016-08-25 10:34:18 -0700

    Add Notification images (Android without custom layouts)
    Behind --enable-experimental-web-platform-features flag.
    Not yet hooked up on Android Marshmallow or below, which
    use custom layouts for notifications.
    
    Intent to implement:
    https://groups.google.com/a/chromium.org/d/topic/blink-dev/FQxPB5GEQjo/discussion
    
    Screenshot: https://imgur.com/a/SM3tq
    
    Depends on https://codereview.chromium.org/2261283002
    
    BUG=614456
    
    Review-Url: https://codereview.chromium.org/2273033002
    Cr-Commit-Position: refs/heads/master@{#414466}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationBuilderBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationBuilderBase.java
@@ -66,6 +66,7 @@ public abstract class NotificationBuilderBase {
     protected CharSequence mBody;
     protected CharSequence mOrigin;
     protected CharSequence mTickerText;
+    protected Bitmap mImage;
     protected Bitmap mLargeIcon;
     protected int mSmallIconId;
     protected Bitmap mSmallIconBitmap;
@@ -112,6 +113,14 @@ public abstract class NotificationBuilderBase {
      */
     public NotificationBuilderBase setTicker(@Nullable CharSequence tickerText) {
         mTickerText = limitLength(tickerText);
+        return this;
+    }
+
+    /**
+     * Sets the content image to be prominently displayed when the notification is expanded.
+     */
+    public NotificationBuilderBase setImage(@Nullable Bitmap image) {
+        mImage = image;
         return this;
     }
 
