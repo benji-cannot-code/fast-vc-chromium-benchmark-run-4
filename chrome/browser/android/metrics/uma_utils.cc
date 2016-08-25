c0a5c5d47d97ac200f3a4a4d077371224e826810
jwd <jwd@chromium.org>
2016-08-25 11:23:44 -0700

    Enabling sampling of UMA and crash reports on Android.
    This has a side effect of causing metrics reporting to be enabled during the session that the pref is changed.
    
    BUG=609987
    
    Review-Url: https://codereview.chromium.org/2248243002
    Cr-Commit-Position: refs/heads/master@{#414482}

--- chrome/browser/android/metrics/uma_utils.cc
+++ chrome/browser/android/metrics/uma_utils.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/metrics/chrome_metrics_services_manager_client.h"
 #include "components/metrics/metrics_reporting_default_state.h"
 #include "jni/UmaUtils_jni.h"
 
@@ -23,6 +24,12 @@ base::Time GetMainEntryPointTime() {
   int64_t startTimeUnixMs = Java_UmaUtils_getMainEntryPointWallTime(env);
   return base::Time::UnixEpoch() +
          base::TimeDelta::FromMilliseconds(startTimeUnixMs);
+}
+
+static jboolean IsClientInMetricsReportingSample(
+    JNIEnv* env,
+    const JavaParamRef<jclass>& obj) {
+  return ChromeMetricsServicesManagerClient::IsClientInSample();
 }
 
 static void RecordMetricsReportingDefaultOptIn(JNIEnv* env,
