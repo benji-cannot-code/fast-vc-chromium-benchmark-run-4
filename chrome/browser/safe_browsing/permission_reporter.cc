8af230751e11a8f67b079b459f8574df32efa05f
haraken <haraken@chromium.org>
2016-08-25 21:39:33 -0700

    Revert of Permission Action Reporting: Fix upload url. (patchset #1 id:1 of https://codereview.chromium.org/2272793009/ )
    Reason for revert:
    PermissionReporterTest.SendReport started failing on many bots after this CL:
    
    https://build.chromium.org/p/chromium.mac/builders/Mac10.9%20Tests%20%28dbg%29/builds/29240
    
    Original issue's description:
    > Permission Action Reporting: Fix upload url.
    >
    > BUG=641220,613883
    >
    > TBR=nparker@chromium.org
    > NOPRESUBMIT=true
    > NOTRY=true
    >
    > Committed: https://crrev.com/ade822e48b63a78dbe5b812702f74fe831bd6a5c
    > Cr-Commit-Position: refs/heads/master@{#414637}
    
    TBR=nparker@chromium.org,kcarattini@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=641220,613883
    
    Review-Url: https://codereview.chromium.org/2279153002
    Cr-Commit-Position: refs/heads/master@{#414651}

--- chrome/browser/safe_browsing/permission_reporter.cc
+++ chrome/browser/safe_browsing/permission_reporter.cc
@@ -23,8 +23,8 @@ namespace safe_browsing {
 namespace {
 // URL to upload permission action reports.
 const char kPermissionActionReportingUploadUrl[] =
-    "https://safebrowsing.googleusercontent.com/safebrowsing/clientreport/"
-    "chrome-permissions";
+    "http://safebrowsing.googleusercontent.com/safebrowsing/clientreport/"
+    "permission-action";
 
 const int kMaximumReportsPerOriginPerPermissionPerMinute = 5;
 
