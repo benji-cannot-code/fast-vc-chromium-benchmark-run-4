8add4aa0b37f69cae9550a2e03e70bf367e91ba8
mustaq <mustaq@chromium.org>
2016-08-26 09:25:33 -0700

    Added counters for pointer boundary events fired.
    Covered both unconditional firing and firing for captured pointer ids.
    
    BUG=640637
    TBR=isherman@chromium.org
    
    Review-Url: https://codereview.chromium.org/2274253002
    Cr-Commit-Position: refs/heads/master@{#414728}

--- third_party/WebKit/Source/core/input/EventHandler.h
+++ third_party/WebKit/Source/core/input/EventHandler.h
@@ -187,7 +187,8 @@ public:
 
     void setPointerCapture(int, EventTarget*);
     void releasePointerCapture(int, EventTarget*);
-    bool hasPointerCapture(int, EventTarget*);
+    bool hasPointerCapture(int, const EventTarget*) const;
+    bool hasProcessedPointerCapture(int, const EventTarget*) const;
 
     void elementRemoved(EventTarget*);
 
