b946a15556fc430297fdeda16951b07fa974e5d2
felt <felt@chromium.org>
2016-08-26 06:46:08 -0700

    Downgrade security state while displaying an SB interstitial
    BUG=615123
    TBR=jialiul@chromium.org
    TEST=
    1. Go to the following URLs in different tabs:
       http://ianfette.org
       http://parkerly.com/sb-tests/bad-subresources.html
       https://parkerly.com/sb-tests/bad-subresources.html
    2. Verify that the omnibox security indicator shows a red triangle for
       all three warnings
    
    Review-Url: https://codereview.chromium.org/2275123004
    Cr-Commit-Position: refs/heads/master@{#414700}

--- chrome/browser/safe_browsing/ui_manager_unittest.cc
+++ chrome/browser/safe_browsing/ui_manager_unittest.cc
@@ -80,7 +80,7 @@ class SafeBrowsingUIManagerTest : public ChromeRenderViewHostTestHarness {
   }
 
   void AddToWhitelist(SafeBrowsingUIManager::UnsafeResource resource) {
-    ui_manager_->AddToWhitelist(resource);
+    ui_manager_->AddToWhitelistUrlSet(resource, false);
   }
 
   SafeBrowsingUIManager::UnsafeResource MakeUnsafeResource(
