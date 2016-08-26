38cb8eba4062ec8d5f46c505093ef775e730570c
oka <oka@chromium.org>
2016-08-25 19:22:59 -0700

    Re-focus file list when quick view is closed.
    BUG=640133
    TEST=manually
    
    Review-Url: https://codereview.chromium.org/2285533002
    Cr-Commit-Position: refs/heads/master@{#414630}

--- ui/file_manager/file_manager/foreground/js/quick_view_controller.js
+++ ui/file_manager/file_manager/foreground/js/quick_view_controller.js
@@ -76,6 +76,9 @@ function QuickViewController(
   listContainer.element.addEventListener(
       'keydown', this.onKeyDownToOpen_.bind(this));
   quickView.addEventListener('keydown', this.onQuickViewKeyDown_.bind(this));
+  quickView.addEventListener('iron-overlay-closed', function() {
+    this.listContainer_.focus();
+  }.bind(this));
   quickView.onOpenInNewButtonTap = this.onOpenInNewButtonTap_.bind(this);
 
   var toolTip = this.quickView_.$$('files-tooltip');
@@ -121,7 +124,6 @@ QuickViewController.prototype.onQuickViewKeyDown_ = function(event) {
     case 'Escape':
       event.preventDefault();
       this.quickView_.close();
-      this.listContainer_.focus();
       break;
     case 'ArrowRight':
       var index = this.fileListSelectionModel_.selectedIndex + 1;
