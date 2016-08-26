5a94c3b207e85371b5937daa178146dd7e207bcc
fs <fs@opera.com>
2016-08-26 12:26:32 -0700

    Move onclick handler in svg/animations/use-animate-width-and-height.html
    We can no longer listen after events on something that is an instance in
    a <use>. Move the executeTest() call to the <use> element instead.
    
    Since the mentioned file is almost a stub, the actual change is in
    svg/animations/script-tests/use-animate-width-and-height.js.
    
    BUG=641398
    
    Review-Url: https://codereview.chromium.org/2283893002
    Cr-Commit-Position: refs/heads/master@{#414776}

--- third_party/WebKit/LayoutTests/svg/animations/script-tests/use-animate-width-and-height.js
+++ third_party/WebKit/LayoutTests/svg/animations/script-tests/use-animate-width-and-height.js
@@ -13,7 +13,6 @@ rect.setAttribute("y", "10");
 rect.setAttribute("width", "100%");
 rect.setAttribute("height", "100%");
 rect.setAttribute("fill", "green");
-rect.setAttribute("onclick", "executeTest()");
 symbol.appendChild(rect);
 rootSVGElement.appendChild(symbol);
 
@@ -24,6 +23,7 @@ use.setAttribute("x", "0");
 use.setAttribute("y", "0");
 use.setAttribute("width", "100");
 use.setAttribute("height", "100");
+use.setAttribute("onclick", "executeTest()");
 rootSVGElement.appendChild(use);
 
 var animate = createSVGElement("animate");
