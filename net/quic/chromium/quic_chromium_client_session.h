9f5e4829bf69b6d6a762fc2f1d9184bc35e5e2e0
jri <jri@chromium.org>
2016-08-25 17:37:21 -0700

    Removes unused code in QuicStreamFactory.
    R=rch@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2277633006
    Cr-Commit-Position: refs/heads/master@{#414601}

--- net/quic/chromium/quic_chromium_client_session.h
+++ net/quic/chromium/quic_chromium_client_session.h
@@ -333,8 +333,6 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
   // delete |this|.
   void NotifyFactoryOfSessionClosed();
 
-  void OnConnectTimeout();
-
   QuicServerId server_id_;
   bool require_confirmation_;
   std::unique_ptr<QuicCryptoClientStream> crypto_stream_;
