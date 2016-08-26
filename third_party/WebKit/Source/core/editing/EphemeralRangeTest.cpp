4d639bcd74798d26bb09eab65f395c696746fb91
tkent <tkent@chromium.org>
2016-08-26 09:12:14 -0700

    Use Node stream printer in Node::showNode().
    This is a preparation to remove WTFLogAlwasy() from Node.cpp.
    This CL has no behavior changes.
    
    BUG=638849
    
    Review-Url: https://codereview.chromium.org/2282683002
    Cr-Commit-Position: refs/heads/master@{#414726}

--- third_party/WebKit/Source/core/editing/EphemeralRangeTest.cpp
+++ third_party/WebKit/Source/core/editing/EphemeralRangeTest.cpp
@@ -84,7 +84,7 @@ TEST_F(EphemeralRangeTest, rangeShadowTraversal)
 
     // In this case FlatTree traverse should differs from DOM tree traverse.
     EXPECT_EQ(
-        "[BODY][P id=\"host\"][B id=\"one\"][INPUT][DIV id=\"inner-editor\"][#text \"some\"]",
+        "[BODY][P id=\"host\"][B id=\"one\"][INPUT][DIV id=\"inner-editor\" (editable)][#text \"some\"]",
         traverseRange<FlatTreeTraversal>(getBodyRange()));
     EXPECT_EQ(
         traverseRange<FlatTreeTraversal>(getBodyRange()),
@@ -124,7 +124,7 @@ TEST_F(EphemeralRangeTest, rangeTraversalLimited)
     EXPECT_EQ(traverseRange<>(fromBToSpan), traverseRange(EphemeralRange(fromBToSpan)));
 
     EXPECT_EQ(
-        "[B id=\"one\"][INPUT][DIV id=\"inner-editor\"][#text \"some\"][SPAN id=\"two\"]",
+        "[B id=\"one\"][INPUT][DIV id=\"inner-editor\" (editable)][#text \"some\"][SPAN id=\"two\"]",
         traverseRange<FlatTreeTraversal>(fromBToSpan));
     EXPECT_EQ(
         traverseRange<FlatTreeTraversal>(fromBToSpan),
