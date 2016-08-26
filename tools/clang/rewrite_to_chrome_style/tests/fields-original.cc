35171d9ebe9436a4413de61c8cba19370ae9c8b7
lukasza <lukasza@chromium.org>
2016-08-26 11:25:44 -0700

    Extend |is_type_trait_value| matcher to cover |blink| namespace.
    Also tighten up the matcher to also require that |value| field:
    - is public,
    - is inside a struct-or-class that doesn't have any methods.
    
    BUG=640749
    
    Review-Url: https://codereview.chromium.org/2283553002
    Cr-Commit-Position: refs/heads/master@{#414751}

--- tools/clang/rewrite_to_chrome_style/tests/fields-original.cc
+++ tools/clang/rewrite_to_chrome_style/tests/fields-original.cc
@@ -63,6 +63,11 @@ union U {
   int m_hasPrefix;
 };
 
+// https://crbug.com/640749#c1: Some type traits are inside blink namespace.
+struct IsGarbageCollectedMixin {
+  static const bool value = true;
+};
+
 }  // namespace blink
 
 namespace WTF {
@@ -73,7 +78,7 @@ struct TypeTrait1 {
   static const bool value = true;
 };
 
-// Some type traits  are implemented as classes, not structs
+// Some type traits are implemented as classes, not structs
 // (e.g. WTF::IsGarbageCollectedType or WTF::IsAssignable).
 template <typename T>
 class TypeTrait2 {
