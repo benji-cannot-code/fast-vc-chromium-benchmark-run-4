4a92a64799813c881e64a2dfaf03ccc01848d5ae
tkent <tkent@chromium.org>
2016-08-26 02:33:31 -0700

    Fix selectionDirection attribute value of INPUT/TEXTAREA just after creation.
    selectionDirection incorrectly returned "none" in such case even on non-Mac
    platforms.
    
    BUG=640861
    
    Review-Url: https://codereview.chromium.org/2285613002
    Cr-Commit-Position: refs/heads/master@{#414678}

--- third_party/WebKit/Source/core/html/HTMLTextFormControlElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLTextFormControlElement.cpp
@@ -63,8 +63,8 @@ HTMLTextFormControlElement::HTMLTextFormControlElement(const QualifiedName& tagN
     , m_lastChangeWasUserEdit(false)
     , m_cachedSelectionStart(0)
     , m_cachedSelectionEnd(0)
-    , m_cachedSelectionDirection(SelectionHasNoDirection)
 {
+    m_cachedSelectionDirection = doc.frame() && doc.frame()->editor().behavior().shouldConsiderSelectionAsDirectional() ? SelectionHasForwardDirection : SelectionHasNoDirection;
 }
 
 HTMLTextFormControlElement::~HTMLTextFormControlElement()
