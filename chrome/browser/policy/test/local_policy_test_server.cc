a306d9be42b61521f7f6a5e81a324845b7864609
brettw <brettw@chromium.org>
2016-08-25 19:16:41 -0700

    Update policy proto path for browser tests
    This moved from policy/proto to components/policy/proto in https://codereview.chromium.org/2273783002 and this updates the Python server code to unbreak the test.
    
    TBR=jianli@chromium.org
    
    Review-Url: https://codereview.chromium.org/2286473003
    Cr-Commit-Position: refs/heads/master@{#414628}

--- chrome/browser/policy/test/local_policy_test_server.cc
+++ chrome/browser/policy/test/local_policy_test_server.cc
@@ -198,6 +198,7 @@ bool LocalPolicyTestServer::SetPythonPath() const {
   }
 
   AppendToPythonPath(pyproto_dir
+                     .AppendASCII("components")
                      .AppendASCII("policy")
                      .AppendASCII("proto"));
 #if defined(OS_CHROMEOS)
