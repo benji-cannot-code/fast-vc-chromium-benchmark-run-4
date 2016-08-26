8bbd6656f556d14fb564b51c5bea46ccd5d25acb
justincohen <justincohen@chromium.org>
2016-08-26 11:51:33 -0700

    Add GetCurrentNativeController to webstate.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2277103003
    Cr-Commit-Position: refs/heads/master@{#414764}

---
+++ ios/web/public/test/native_controller_test_util.mm
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/web/public/test/native_controller_test_util.h"
+
+#import "ios/web/web_state/ui/crw_web_controller.h"
+#import "ios/web/web_state/web_state_impl.h"
+
+namespace web {
+namespace test {
+
+id<CRWNativeContent> GetCurrentNativeController(WebState* web_state) {
+  web::WebStateImpl* web_state_impl =
+      static_cast<web::WebStateImpl*>(web_state);
+  return [web_state_impl->GetWebController() nativeController];
+}
+
+}  // namespace test
+}  // namespace web
