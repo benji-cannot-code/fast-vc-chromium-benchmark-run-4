8044a827d9d4be9bc80766292811826aab2d1d48
fdoray <fdoray@chromium.org>
2016-08-26 15:44:29 -0700

    Delete old files after an update.
    This is a variation of r378802 which was reverted because of
    a Dr Memory error (solved by using GenerateAlternatePEFileVersion
    instead of GenerateSpecificPEFileVersion). r378802 also caused
    problems with shortcuts crbug.com/592040 (solved by not letting
    the deletion of old files cause a rollback).
    
    With this CL, the installer deletes all files that belong to old
    versions of Chrome that are not in use after an update.
    
    Eventually, the DeleteOldVersions() will be invoked again to delete
    old versions that were in use at time of the update.
    
    BUG=451546
    TEST=
    -- Not-in-use --
    1. Run mini_installer.exe --chrome --multi-install --verbose-logging --do-not-launch-chrome
    - The install directory should contain chrome.exe and a matching version directory.
    - There should be no other executable or version directory in the install directory.
    2. Run next_version_mini_installer.exe --multi-install --verbose-logging --do-not-launch-chrome
    - The install directory should contain chrome.exe and a matching version directory. The version should be higher than at the previous step.
    - There should be no other executable or version directory in the install directory.
    
    -- In-use --
    1. Run mini_installer.exe --chrome --multi-install --verbose-logging --do-not-launch-chrome
    - The install directory should contain chrome.exe and a matching version directory.
    - There should be no other executable or version directory in the install directory.
    2. Launch Chrome.
    3. Run next_version_mini_installer.exe --multi-install --verbose-logging --do-not-launch-chrome.
    - The install directory should contain chrome.exe and a matching version directory + new_chrome.exe and a matching version directory.
    - There should be no other executable or version directory in the install directory.
    
    Review-Url: https://codereview.chromium.org/2273113002
    Cr-Commit-Position: refs/heads/master@{#414842}

---
+++ chrome/installer/util/delete_old_versions.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_INSTALLER_UTIL_DELETE_OLD_VERSIONS_H_
+#define CHROME_INSTALLER_UTIL_DELETE_OLD_VERSIONS_H_
+
+namespace base {
+class FilePath;
+}
+
+namespace installer {
+
+// Deletes files that belong to old versions of Chrome. chrome.exe,
+// new_chrome.exe and their associated version directories are never deleted.
+// Also, no file is deleted for a given version if a .exe or .dll file for that
+// version is in use. Returns true if no files that belong to an old version of
+// Chrome remain.
+bool DeleteOldVersions(const base::FilePath& install_dir);
+
+}  // namespace installer
+
+#endif  // CHROME_INSTALLER_UTIL_DELETE_OLD_VERSIONS_H_
