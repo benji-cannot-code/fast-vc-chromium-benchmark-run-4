50a444b3af450399ae89406753ce0addc4078ba7
mcasas <mcasas@chromium.org>
2016-08-27 04:07:01 -0700

    ImageCapture: wire exposureCompensation retrieval/setting from Blink to Android
    This CL adds exposureCompensation to idl/mojom/java
    interface definitions and classes. When enumerated,
    it's a Range type (min-current-max), so the # new locs
    is larger than expected.
    
    Also, MediaSettingsRange(.idl and mojom's Range) are
    changed to signed int32s to accommodate integer ranges
    (https://github.com/w3c/mediacapture-image/pull/52).
    
    Also three's some tiny cleanups in the java files, and
    the unittests and LayoutTests are updated.
    
    BUG=368381, 581807
    TEST=by hand on a N7 with old android.hardware.Camera API,
    on a N5X with Camera2 API -- note that N6 does not have
    exposure compensation for some reason :?
    
    TBR=miu@chromium.org for
    content/browser/media/capture/image_capture_impl.cc
    since it's just a one liner.
    
    Review-Url: https://codereview.chromium.org/2281993002
    Cr-Commit-Position: refs/heads/master@{#414922}

--- third_party/WebKit/Source/modules/imagecapture/PhotoCapabilities.h
+++ third_party/WebKit/Source/modules/imagecapture/PhotoCapabilities.h
@@ -39,6 +39,9 @@ public:
     String exposureMode() const;
     void setExposureMode(media::mojom::blink::MeteringMode exposureMode) { m_exposureMode = exposureMode; }
 
+    MediaSettingsRange* exposureCompensation() const { return m_exposureCompensation; }
+    void setExposureCompensation(MediaSettingsRange* value) { m_exposureCompensation = value; }
+
     DECLARE_VIRTUAL_TRACE();
 
 private:
@@ -50,6 +53,7 @@ private:
     Member<MediaSettingsRange> m_zoom;
     media::mojom::blink::MeteringMode m_focusMode = media::mojom::blink::MeteringMode::UNAVAILABLE;
     media::mojom::blink::MeteringMode m_exposureMode = media::mojom::blink::MeteringMode::UNAVAILABLE;
+    Member<MediaSettingsRange> m_exposureCompensation;
 };
 
 } // namespace blink
