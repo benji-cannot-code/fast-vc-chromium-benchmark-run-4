3dfc7ef4a4b7d19c5af4ef2ef75a44b17c7dbc72
estark <estark@chromium.org>
2016-08-27 03:49:49 -0700

    Stop supporting legacy keywords in Referrer-Policy header
    https://github.com/w3c/webappsec-referrer-policy/pull/66 removed support
    for legacy keywords in the Referrer-Policy header. There is no need to
    support these legacy keywords because the Referrer-Policy header is
    brand new (not shipped yet) and there is no legacy usage.
    
    In the course of implementing this, I realized that we do in fact
    support legacy keywords for 'referrerpolicy' content attributes. This is
    not necessary per the spec, but I left it as-is since we should measure
    usage before removing support for legacy keywords in 'referrerpolicy'
    attributes.
    
    BUG=641128
    
    Review-Url: https://codereview.chromium.org/2278823004
    Cr-Commit-Position: refs/heads/master@{#414903}

---
+++ third_party/WebKit/LayoutTests/http/tests/security/referrerPolicyHeader/legacy-origin-when-crossorigin.php
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header("Referrer-Policy: no-referrer,origin-when-crossorigin");
+?>
+<!DOCTYPE html>
+<head>
+  <script src="/resources/testharness.js"></script>
+  <script src="/resources/testharnessreport.js"></script>
+  <script src="/resources/get-host-info.js"></script>
+</head>
+<body>
+</body>
+<script>
+    var policy = "no-referrer,origin-when-crossorigin";
+    var expectedReferrer = "";
+    var navigateTo = "cross-origin-no-downgrade";
+</script>
+<script src="resources/header-test.js"></script>
+</html>
--- third_party/WebKit/Source/platform/weborigin/SecurityPolicy.h
+++ third_party/WebKit/Source/platform/weborigin/SecurityPolicy.h
@@ -69,6 +69,7 @@ public:
     static bool isOriginWhiteListedTrustworthy(const SecurityOrigin&);
 
     static bool referrerPolicyFromString(const String& policy, ReferrerPolicy* result);
+    static bool referrerPolicyFromStringWithLegacyKeywords(const String& policy, ReferrerPolicy* result);
 };
 
 } // namespace blink
