e56bc730490c704a4d48636197914583c3762de1
spqchan <spqchan@chromium.org>
2016-08-27 03:29:39 -0700

    [Mac] Fix for fullscreen toolbar
    Added back mouse tracking to fix an edge case where the menu
    bar disappears when but the mouse is still hovering the toolbar.
    
    The tracking area is only used to keep the toolbar active when this
    happens. The toolbar will hide when the mouse leaves the tracking
    area.
    
    BUG=639562
    
    Review-Url: https://codereview.chromium.org/2272783002
    Cr-Commit-Position: refs/heads/master@{#414888}

--- chrome/browser/ui/cocoa/browser_window_controller.mm
+++ chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -1980,9 +1980,9 @@ willAnimateFromState:(BookmarkBar::State)oldState
 }
 
 - (BOOL)isBarVisibilityLockedForOwner:(id)owner {
-  DCHECK(owner);
   DCHECK(barVisibilityLocks_);
-  return [barVisibilityLocks_ containsObject:owner];
+  return owner ? [barVisibilityLocks_ containsObject:owner]
+               : [barVisibilityLocks_ count];
 }
 
 - (void)lockBarVisibilityForOwner:(id)owner withAnimation:(BOOL)animate {
