f2abcc42763043bbad1b4575c909742ba847b2fa
shess <shess@chromium.org>
2016-08-27 03:26:23 -0700

    [sql] Track SQLite error codes from sql::Recovery ATTACH.
    The major reason for sql::Recover::Begin() to fail is in the statement
    attaching the original database to the recovery database.  I believe
    this is because of SQLITE_NOTADB.  This is to verify that hypothesis.
    If the hypothesis is true, then the best course of action is to Raze()
    the database, as SQLITE_NOTADB means that some or all of the SQLite
    header data appears to be garbage.
    
    BUG=597785
    R=michaeln@chromium.org
    
    Review-Url: https://codereview.chromium.org/2274883003
    Cr-Commit-Position: refs/heads/master@{#414885}

--- sql/recovery_unittest.cc
+++ sql/recovery_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/test/histogram_tester.h"
 #include "sql/connection.h"
 #include "sql/meta_table.h"
 #include "sql/statement.h"
@@ -831,6 +832,40 @@ TEST_F(SQLRecoveryTest, RecoverDatabase) {
   EXPECT_EQ("dean|trailer\njim|telephone",
             ExecuteWithResults(&db(), kYSql, "|", "\n"));
   EXPECT_EQ("trailer", ExecuteWithResults(&db(), kVSql, "|", "\n"));
+}
+
+// Test histograms recorded when the invalid database cannot be attached.
+TEST_F(SQLRecoveryTest, AttachFailure) {
+  // Create a valid database, then write junk over the header.  This should lead
+  // to SQLITE_NOTADB, which will cause ATTACH to fail.
+  ASSERT_TRUE(db().Execute("CREATE TABLE x (t TEXT)"));
+  ASSERT_TRUE(db().Execute("INSERT INTO x VALUES ('This is a test')"));
+  db().Close();
+  WriteJunkToDatabase(SQLTestBase::TYPE_OVERWRITE);
+
+  const char kEventHistogramName[] = "Sqlite.RecoveryEvents";
+  const int kEventEnum = 5;  // RECOVERY_FAILED_ATTACH
+  const char kErrorHistogramName[] = "Sqlite.RecoveryAttachError";
+  base::HistogramTester tester;
+
+  {
+    sql::test::ScopedErrorExpecter expecter;
+    expecter.ExpectError(SQLITE_NOTADB);
+
+    // Reopen() here because it will see SQLITE_NOTADB.
+    ASSERT_TRUE(Reopen());
+
+    // Begin() should fail.
+    std::unique_ptr<sql::Recovery>
+        recovery = sql::Recovery::Begin(&db(), db_path());
+    ASSERT_FALSE(recovery.get());
+
+    ASSERT_TRUE(expecter.SawExpectedErrors());
+  }
+
+  // Verify that the failure was in the right place with the expected code.
+  tester.ExpectBucketCount(kEventHistogramName, kEventEnum, 1);
+  tester.ExpectBucketCount(kErrorHistogramName, SQLITE_NOTADB, 1);
 }
 
 }  // namespace
