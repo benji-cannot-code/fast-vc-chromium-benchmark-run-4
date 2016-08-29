e5c34eda6af502fe4f9e78ee39ca3f0d1fd70b57
bartfab <bartfab@chromium.org>
2016-08-29 03:42:19 -0700

    Whitelist Service NSW Kiosk Utility for Public Sessions
    This CL whitelists the Service NSW Kiosk Utility for use in
    Public Sessions.
    
    TEST=None
    BUG=626850
    
    Review-Url: https://codereview.chromium.org/2287493002
    Cr-Commit-Position: refs/heads/master@{#414992}

--- chrome/browser/chromeos/extensions/device_local_account_management_policy_provider.cc
+++ chrome/browser/chromeos/extensions/device_local_account_management_policy_provider.cc
@@ -41,6 +41,7 @@ const char* const kPublicSessionWhitelist[] = {
     "pmnllmkmjilbojkpgplbdmckghmaocjh",  // Scan app by François Beaufort
     "khpfeaanjngmcnplbdlpegiifgpfgdco",  // Smart Card Connector App
     "haeblkpifdemlfnkogkipmghfcbonief",  // Charismathics Smart Card Middleware
+    "mpnkhdpphjiihmlmkcamhpogecnnfffa",  // Service NSW Kiosk Utility
 
     // Libraries:
     "aclofikceldphonlfmghmimkodjdmhck",  // Ancoris login component
