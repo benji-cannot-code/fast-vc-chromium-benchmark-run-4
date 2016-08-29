edfd8c365e17e7c134963116b4df07c940017207
Ben Chan <benchan@chromium.org>
2016-08-29 13:27:06 -0700

    Replace deprecated ScopedVector<T> in chromeos::PolicyApplicator
    This CL changes chromeos::PolicyApplicator to use vector<unique_ptr<T>>
    instead of the deprecated ScopedVector<T>.
    
    BUG=554289
    R=stevenjb@chromium.org
    
    Review URL: https://codereview.chromium.org/2281403002 .
    
    Cr-Commit-Position: refs/heads/master@{#415031}

--- chromeos/network/policy_applicator.h
+++ chromeos/network/policy_applicator.h
@@ -7,11 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMEOS_NETWORK_POLICY_APPLICATOR_H_
 
 #include <map>
+#include <memory>
 #include <set>
 #include <string>
+#include <vector>
 
 #include "base/macros.h"
-#include "base/memory/scoped_vector.h"
 #include "base/memory/weak_ptr.h"
 #include "base/values.h"
 #include "chromeos/network/network_profile.h"
@@ -107,7 +108,7 @@ class PolicyApplicator {
   NetworkProfile profile_;
   GuidToPolicyMap all_policies_;
   base::DictionaryValue global_network_config_;
-  ScopedVector<base::DictionaryValue> new_shill_configurations_;
+  std::vector<std::unique_ptr<base::DictionaryValue>> new_shill_configurations_;
   base::WeakPtrFactory<PolicyApplicator> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(PolicyApplicator);
