783c85aff63a82b0c12ef57631aec554f50be5b5
Pavel Feldman <pfeldman@chromium.org>
2016-08-29 16:11:08 -0700

    DevTools: return page type for render frames by default (should fix webview telemetry tests).
    BUG=641390
    TBR=dgozman
    
    Review URL: https://codereview.chromium.org/2285403002 .
    
    Cr-Commit-Position: refs/heads/master@{#415036}

--- content/browser/devtools/render_frame_devtools_agent_host.cc
+++ content/browser/devtools/render_frame_devtools_agent_host.cc
@@ -886,7 +886,9 @@ std::string RenderFrameDevToolsAgentHost::GetType() {
   DevToolsManager* manager = DevToolsManager::GetInstance();
   if (manager->delegate())
     return manager->delegate()->GetTargetType(current_->host());
-  return kTypeOther;
+  if (IsChildFrame())
+    return kTypeFrame;
+  return kTypePage;
 }
 
 std::string RenderFrameDevToolsAgentHost::GetTitle() {
