ee25ab14cc7045a3117ee7cafd71b875c1f4b946
vabr <vabr@chromium.org>
2016-08-29 09:53:09 -0700

    Remove STLDeleteContainerPairFirstPointers from AutofillDownloadManager
    BUG=555865
    
    Review-Url: https://codereview.chromium.org/2288173002
    Cr-Commit-Position: refs/heads/master@{#415019}

--- components/autofill/core/browser/autofill_download_manager.h
+++ components/autofill/core/browser/autofill_download_manager.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <list>
 #include <map>
+#include <memory>
 #include <string>
 #include <utility>
 #include <vector>
@@ -136,8 +137,11 @@ class AutofillDownloadManager : public net::URLFetcherDelegate {
 
   // For each requested form for both query and upload we create a separate
   // request and save its info. As url fetcher is identified by its address
-  // we use a map between fetchers and info.
-  std::map<net::URLFetcher*, FormRequestData> url_fetchers_;
+  // we use a map between fetchers and info. The value type is a pair of an
+  // owning pointer to the key and the actual FormRequestData.
+  std::map<net::URLFetcher*,
+           std::pair<std::unique_ptr<net::URLFetcher>, FormRequestData>>
+      url_fetchers_;
 
   // Cached QUERY requests.
   QueryRequestCache cached_forms_;
