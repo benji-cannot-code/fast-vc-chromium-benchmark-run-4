9b08e39ee73a71ccd160280c453a790dde53b8ce
tyoshino <tyoshino@chromium.org>
2016-08-30 00:28:41 -0700

    Use window.testRunner in if-clause
    "if (testRunner)" throws if testRunner is undefined.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2295653002
    Cr-Commit-Position: refs/heads/master@{#415232}

--- third_party/WebKit/LayoutTests/fast/js/script-tests/cross-frame-really-bad-time.js
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/cross-frame-really-bad-time.js
@@ -42,7 +42,7 @@ function done() {
     else
         testFailed("Did not get 5 ouches. Got " + ouches + " + instead.");
     
-    if (testRunner)
+    if (window.testRunner)
         testRunner.notifyDone();
 }
 
