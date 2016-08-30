afad11a1184ba204073c9d8492e633e20763b73a
brettw <brettw@chromium.org>
2016-08-30 13:52:13 -0700

    Remove extensions_tests.gyp[i]
    Inlines sources into the GN build files. This creates smaller helper "unit_tests" and "browser_tests" targets for the various subdirectories to keep the file lists closer to the code.
    
    Adds the wifi display unit tests which were missing from the GN build (these are not compiled by default, only when you explicitly pass a flag).
    
    Adds the new targets to GN check and adds additional dependencies to make this check pass.
    
    Review-Url: https://codereview.chromium.org/2281053004
    Cr-Commit-Position: refs/heads/master@{#415419}

--- extensions/shell/browser/shell_browsertest.cc
+++ extensions/shell/browser/shell_browsertest.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/shell/test/shell_apitest.h"
 
 #if defined(USE_AURA)
-#include "ui/aura/window.h"
+#include "ui/aura/window.h"  // nogncheck
 #endif
 
 namespace extensions {
