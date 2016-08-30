0b9b08f3bcd0cd21a99e8228a6d77604fffd8366
yukishiino <yukishiino@chromium.org>
2016-08-30 03:25:06 -0700

    binding: Reduces the binary size of binding's generated code.
    Retires installConditionallyEnabledProperties.  This function has been
    unused for months at least, and it's now unlikely we'll need this
    mechanism again.
    
    Stops generating unused functions of preparePrototypeAndInterfaceObject.
    Even when we don't use them, each function requires an unique address,
    i.e. each function consumes 8 bytes.  Uses nullptr instead of generating
    unused functions.
    
    This CL is expected to reduce the size by 400k+ bytes.
    
    BUG=621802
    
    Review-Url: https://codereview.chromium.org/2296573003
    Cr-Commit-Position: refs/heads/master@{#415263}

--- third_party/WebKit/Source/bindings/templates/partial_interface.cpp
+++ third_party/WebKit/Source/bindings/templates/partial_interface.cpp
@@ -1,6 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {% extends 'interface_base.cpp' %}
 
+{% set has_prepare_prototype_and_interface_object =
+    unscopeables or has_conditional_attributes_on_prototype or
+    methods | conditionally_exposed(is_partial) %}
+{% set prepare_prototype_and_interface_object_func =
+    '%s::preparePrototypeAndInterfaceObject' % v8_class_or_partial
+    if has_prepare_prototype_and_interface_object
+    else 'nullptr' %}
+
 
 {##############################################################################}
 {% block prepare_prototype_and_interface_object %}
@@ -8,14 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 {% from 'interface.cpp' import install_conditionally_enabled_attributes_on_prototype with context %}
 {% from 'methods.cpp' import install_conditionally_enabled_methods
         with context %}
+{% if has_prepare_prototype_and_interface_object %}
 void {{v8_class_or_partial}}::preparePrototypeAndInterfaceObject(v8::Local<v8::Context> context, const DOMWrapperWorld& world, v8::Local<v8::Object> prototypeObject, v8::Local<v8::Function> interfaceObject, v8::Local<v8::FunctionTemplate> interfaceTemplate)
 {
+#error No one is currently using a partial interface with context-dependent properties.  If you\'re planning to use it, please consult with the binding team: <blink-reviews-bindings@chromium.org>
     {{v8_class}}::preparePrototypeAndInterfaceObject(context, world, prototypeObject, interfaceObject, interfaceTemplate);
-{% if unscopeables or
-      has_conditional_attributes_on_prototype or
-      methods | conditionally_exposed(is_partial) %}
     v8::Isolate* isolate = context->GetIsolate();
-{% endif %}
 {% if unscopeables %}
     {{install_unscopeables() | indent}}
 {% endif %}
@@ -26,6 +32,7 @@ void {{v8_class_or_partial}}::preparePrototypeAndInterfaceObject(v8::Local<v8::C
     {{install_conditionally_enabled_methods() | indent}}
 {% endif %}
 }
+{% endif %}
 
 {% endblock %}
 
@@ -37,7 +44,7 @@ void {{v8_class_or_partial}}::initialize()
     // Should be invoked from ModulesInitializer.
     {{v8_class}}::updateWrapperTypeInfo(
         &{{v8_class_or_partial}}::install{{v8_class}}Template,
-        &{{v8_class_or_partial}}::preparePrototypeAndInterfaceObject);
+        {{prepare_prototype_and_interface_object_func}});
     {% for method in methods %}
     {% if method.overloads and method.overloads.has_partial_overloads %}
     {{v8_class}}::register{{method.name | blink_capitalize}}MethodForPartialInterface(&{{cpp_class_or_partial}}V8Internal::{{method.name}}Method);
--- third_party/WebKit/Source/core/dom/Node.cpp
+++ third_party/WebKit/Source/core/dom/Node.cpp
@@ -2369,7 +2369,6 @@ v8::Local<v8::Object> Node::wrap(v8::Isolate* isolate, v8::Local<v8::Object> cre
 
     v8::Local<v8::Object> wrapper = V8DOMWrapper::createWrapper(isolate, creationContext, wrapperType);
     DCHECK(!wrapper.IsEmpty());
-    wrapperType->installConditionallyEnabledProperties(wrapper, isolate);
     return associateWithWrapper(isolate, wrapperType, wrapper);
 }
 
