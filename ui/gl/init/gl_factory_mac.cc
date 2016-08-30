9853a2f5aafdface6d32aace52826decde432196
zmo <zmo@chromium.org>
2016-08-30 11:00:49 -0700

    Enable core profile GL on Mac OSX
    BUG=635081
    TEST=mac bots
    NOTRY=true
    R=kbr@chromium.org,erikchen@chromium.org
    
    Review-Url: https://codereview.chromium.org/2251263002
    Cr-Commit-Position: refs/heads/master@{#415350}

--- ui/gl/init/gl_factory_mac.cc
+++ ui/gl/init/gl_factory_mac.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace features {
 const base::Feature kDesktopCoreProfileGLOnMac{
-    "DesktopCoreProfileGLOnMac", base::FEATURE_DISABLED_BY_DEFAULT};
+    "DesktopCoreProfileGLOnMac", base::FEATURE_ENABLED_BY_DEFAULT};
 }
 
 namespace gl {
