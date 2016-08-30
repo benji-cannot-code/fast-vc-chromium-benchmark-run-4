d52e836abd8490c96d963262680a0de869b0d6ed
lukasza <lukasza@chromium.org>
2016-08-29 20:48:48 -0700

    s/s_fontManager/s_staticFontManager/ to help rewrite_to_chrome_style tool.
    This CL helps rewrite_to_chrome_style tool, by preventing the tool for
    suggesting the following conflicting renames:
    - |s_field| -> |field_|
    - |m_field| -> |field_|
    
    BUG=641004
    
    Review-Url: https://codereview.chromium.org/2294433002
    Cr-Commit-Position: refs/heads/master@{#415058}

--- third_party/WebKit/Source/platform/fonts/FontCache.h
+++ third_party/WebKit/Source/platform/fonts/FontCache.h
@@ -191,7 +191,7 @@ private:
 
     RefPtr<SkFontMgr> m_fontManager;
 
-    static SkFontMgr* s_fontManager;
+    static SkFontMgr* s_staticFontManager;
 
 #if OS(WIN)
     static bool s_antialiasedTextEnabled;
