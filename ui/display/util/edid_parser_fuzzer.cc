a22f1f65c2cd3c2ca58b44e083be94f13e213a57
mmoroz <mmoroz@chromium.org>
2016-08-30 06:22:36 -0700

    Set log level to logging::LOG_FATAL for edid_parser_fuzzer.
    TBR=aizatsky@chromium.org, inferno@chromium.org, oshima@chromium.org, robert.bradford@intel.com
    BUG=584819
    
    Review-Url: https://codereview.chromium.org/2294733002
    Cr-Commit-Position: refs/heads/master@{#415282}

--- ui/display/util/edid_parser_fuzzer.cc
+++ ui/display/util/edid_parser_fuzzer.cc
@@ -11,6 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/util/edid_parser.h"
 #include "ui/gfx/geometry/size.h"
 
+struct Environment {
+  Environment() {
+    logging::SetMinLogLevel(logging::LOG_FATAL);
+  }
+};
+
+Environment* env = new Environment();
+
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   std::vector<uint8_t> edid;
