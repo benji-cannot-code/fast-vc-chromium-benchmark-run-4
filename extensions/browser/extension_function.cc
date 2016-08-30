2208be10f4af75ca7c5bd13d4f1e747de1762f58
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-08-30 15:55:07 -0700

    [Extensions] Remove SyncExtensionFunction
    SyncExtensionFunctions, AsyncExtensionFunctions,
    ChromeSyncExtensionFunctions, and ChromeAsyncExtensionFunctions are
    deprecated.
    
    Remove SyncExtensionFunction.
    
    BUG=634140
    
    Review-Url: https://codereview.chromium.org/2294943002
    Cr-Commit-Position: refs/heads/master@{#415475}

--- extensions/browser/extension_function.cc
+++ extensions/browser/extension_function.cc
@@ -621,19 +621,3 @@ bool AsyncExtensionFunction::ValidationFailure(
     AsyncExtensionFunction* function) {
   return false;
 }
-
-SyncExtensionFunction::SyncExtensionFunction() {
-}
-
-SyncExtensionFunction::~SyncExtensionFunction() {
-}
-
-ExtensionFunction::ResponseAction SyncExtensionFunction::Run() {
-  return RespondNow(RunSync() ? ArgumentList(std::move(results_))
-                              : Error(error_));
-}
-
-// static
-bool SyncExtensionFunction::ValidationFailure(SyncExtensionFunction* function) {
-  return false;
-}
