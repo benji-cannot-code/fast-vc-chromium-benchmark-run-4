a73f86973bca3a1549cda92d4ccaf1a0cb3ab57c
estade <estade@chromium.org>
2016-08-29 21:30:03 -0700

    Redirect all mouse input to Alt+Tab window when it's visible, effectively disabling the mouse. Capture is reset when the widget is destroyed.
    BUG=641171
    
    Review-Url: https://codereview.chromium.org/2284763002
    Cr-Commit-Position: refs/heads/master@{#415096}

--- ash/common/wm/window_cycle_list.cc
+++ ash/common/wm/window_cycle_list.cc
@@ -391,6 +391,10 @@ class WindowCycleView : public views::WidgetDelegateView {
     }
   }
 
+  void OnMouseCaptureLost() override {
+    WmShell::Get()->window_cycle_controller()->StopCycling();
+  }
+
   View* GetContentsView() override { return this; }
 
   View* GetInitiallyFocusedView() override {
@@ -617,6 +621,8 @@ void WindowCycleList::InitWindowCycleView() {
   widget_rect.set_height(widget_height);
   widget->SetBounds(widget_rect);
   widget->Show();
+  widget->SetCapture(cycle_view_);
+  widget->set_auto_release_capture(false);
   cycle_ui_widget_.reset(widget);
 }
 
