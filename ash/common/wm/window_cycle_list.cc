77336aa1febbcc400d745e05e87b24b7aec0c7db
perkj <perkj@chromium.org>
2016-08-30 03:48:22 -0700

    Revert of Redirect all mouse input to Alt+Tab window when it's visible, (patchset #3 id:40001 of https://codereview.chromium.org/2284763002/ )
    Reason for revert:
    WindowCycleControllerTest.MouseCaptureLost fails on Chrome os and Asan.
    
    https://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20(dbg)(1)
    
    Original issue's description:
    > Redirect all mouse input to Alt+Tab window when it's visible,
    > effectively disabling the mouse. Capture is reset when the widget is
    > destroyed.
    >
    > BUG=641171
    >
    > Committed: https://crrev.com/c7e292ea58a76fe27c6c247bc640054bcf4cb6aa
    > Cr-Commit-Position: refs/heads/master@{#415096}
    
    TBR=sky@chromium.org,estade@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=641171
    
    Review-Url: https://codereview.chromium.org/2294723002
    Cr-Commit-Position: refs/heads/master@{#415267}

--- ash/common/wm/window_cycle_list.cc
+++ ash/common/wm/window_cycle_list.cc
@@ -391,10 +391,6 @@ class WindowCycleView : public views::WidgetDelegateView {
     }
   }
 
-  void OnMouseCaptureLost() override {
-    WmShell::Get()->window_cycle_controller()->StopCycling();
-  }
-
   View* GetContentsView() override { return this; }
 
   View* GetInitiallyFocusedView() override {
@@ -621,8 +617,6 @@ void WindowCycleList::InitWindowCycleView() {
   widget_rect.set_height(widget_height);
   widget->SetBounds(widget_rect);
   widget->Show();
-  widget->SetCapture(cycle_view_);
-  widget->set_auto_release_capture(false);
   cycle_ui_widget_.reset(widget);
 }
 
