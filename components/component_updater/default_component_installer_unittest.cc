b8a0e6e0a799fe86b8357fa431bffd481ef2ccce
sorin <sorin@chromium.org>
2016-08-29 21:43:24 -0700

    Fix DefaultComponentInstaller::FinishRegistration and group policy.
    The code in DefaultComponentInstaller::FinishRegistration is not updating
    the value of CrxComponent::supports_group_policy_enable_component_updates
    with the value provided by the specific installer traits.
    
    Therefore, the components using the DefaultComponentInstaller for their
    installer ignore the group policy settings for controlling the update behavior.
    
    BUG=642148
    
    Review-Url: https://codereview.chromium.org/2292463004
    Cr-Commit-Position: refs/heads/master@{#415110}

--- components/component_updater/default_component_installer_unittest.cc
+++ components/component_updater/default_component_installer_unittest.cc
@@ -77,7 +77,7 @@ class FakeInstallerTraits : public ComponentInstallerTraits {
   }
 
   bool SupportsGroupPolicyEnabledComponentUpdates() const override {
-    return false;
+    return true;
   }
 
   bool RequiresNetworkEncryption() const override { return true; }
@@ -230,6 +230,7 @@ TEST_F(DefaultComponentInstallerTest, RegisterComponent) {
   EXPECT_STREQ("fake name", component.name.c_str());
   EXPECT_EQ(expected_attrs, component.installer_attributes);
   EXPECT_TRUE(component.requires_network_encryption);
+  EXPECT_TRUE(component.supports_group_policy_enable_component_updates);
 }
 
 }  // namespace component_updater
