a45da4ddf1cf4094bd4782cabaf54adb881ee9b2
mlippautz <mlippautz@chromium.org>
2016-08-30 06:10:29 -0700

    [v8] Allow querying the number of wrappers that still need to be traced
    BUG=chromium:468240
    
    Review-Url: https://codereview.chromium.org/2288253003
    Cr-Commit-Position: refs/heads/master@{#415281}

--- third_party/WebKit/Source/bindings/core/v8/ScriptWrappableVisitor.h
+++ third_party/WebKit/Source/bindings/core/v8/ScriptWrappableVisitor.h
@@ -102,6 +102,7 @@ public:
     void TraceEpilogue() override;
     void AbortTracing() override;
     void EnterFinalPause() override;
+    size_t NumberOfWrappersToTrace() override;
 
     void dispatchTraceWrappers(const ScriptWrappable*) const override;
 #define DECLARE_DISPATCH_TRACE_WRAPPERS(className)                   \
