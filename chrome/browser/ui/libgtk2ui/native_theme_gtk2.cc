76ad066548140de6823d6c049c676346900f158a
estade <estade@chromium.org>
2016-08-30 08:21:37 -0700

    Handle alert severity colors in NativeThemeGtk2.
    BUG=642163
    
    Review-Url: https://codereview.chromium.org/2297453003
    Cr-Commit-Position: refs/heads/master@{#415299}

--- chrome/browser/ui/libgtk2ui/native_theme_gtk2.cc
+++ chrome/browser/ui/libgtk2ui/native_theme_gtk2.cc
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/path.h"
 #include "ui/gfx/skia_util.h"
 #include "ui/native_theme/common_theme.h"
+#include "ui/native_theme/native_theme_aura.h"
+#include "ui/native_theme/native_theme_dark_aura.h"
 
 namespace libgtk2ui {
 
@@ -456,11 +458,17 @@ SkColor NativeThemeGtk2::GetSystemColor(ColorId color_id) const {
           0x80);
 
     // Alert icons
-    // These aren't yet used in any GTK NativeTheme context, so they don't have
-    // definitions.
+    // Just fall back to the same colors as Aura.
     case kColorId_AlertSeverityLow:
     case kColorId_AlertSeverityMedium:
-    case kColorId_AlertSeverityHigh:
+    case kColorId_AlertSeverityHigh: {
+      ui::NativeTheme* fallback_theme =
+          color_utils::IsDark(GetTextColor(GetEntry(), NORMAL))
+              ? ui::NativeThemeAura::instance()
+              : ui::NativeThemeDarkAura::instance();
+      return fallback_theme->GetSystemColor(color_id);
+    }
+
     case kColorId_NumColors:
       NOTREACHED();
       break;
