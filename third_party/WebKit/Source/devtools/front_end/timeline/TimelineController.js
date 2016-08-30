d947eb9ada599f84cad0194776f380fd8a7f2f9b
alph <alph@chromium.org>
2016-08-30 15:15:01 -0700

    DevTools: Do not run CPU profiler on targets with no JS capability.
    BUG=642432
    
    Review-Url: https://codereview.chromium.org/2297813002
    Cr-Commit-Position: refs/heads/master@{#415454}

--- third_party/WebKit/Source/devtools/front_end/timeline/TimelineController.js
+++ third_party/WebKit/Source/devtools/front_end/timeline/TimelineController.js
@@ -110,7 +110,7 @@ WebInspector.TimelineController.prototype = {
      */
     _startProfilingOnTarget: function(target)
     {
-        return target.profilerAgent().start();
+        return target.hasJSCapability() ? target.profilerAgent().start() : Promise.resolve();
     },
 
     /**
@@ -130,7 +130,7 @@ WebInspector.TimelineController.prototype = {
      */
     _stopProfilingOnTarget: function(target)
     {
-        return target.profilerAgent().stop(this._addCpuProfile.bind(this, target.id()));
+        return target.hasJSCapability() ? target.profilerAgent().stop(this._addCpuProfile.bind(this, target.id())) : Promise.resolve();
     },
 
     /**
