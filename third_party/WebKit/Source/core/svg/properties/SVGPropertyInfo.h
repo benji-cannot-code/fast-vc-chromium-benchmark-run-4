b84dcaeccabb585ae002dd0172ca8ab1d0872894
fs <fs@opera.com>
2016-08-29 21:03:04 -0700

    Reorder the AnimatedPropertyType enumeration
    By moving AnimatedUnknown first (giving it the ordinal value 0), we can
    simplify SVGElement::animatedPropertyTypeForCSSAttribute by removing
    the contains() call (avoiding a double hash/lookup for the common case.)
    
    Review-Url: https://codereview.chromium.org/2288583002
    Cr-Commit-Position: refs/heads/master@{#415069}

--- third_party/WebKit/Source/core/svg/properties/SVGPropertyInfo.h
+++ third_party/WebKit/Source/core/svg/properties/SVGPropertyInfo.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 enum AnimatedPropertyType {
+    AnimatedUnknown = 0,
     AnimatedAngle,
     AnimatedBoolean,
     AnimatedColor,
@@ -44,7 +45,6 @@ enum AnimatedPropertyType {
     AnimatedStringList,
     AnimatedTransform,
     AnimatedTransformList,
-    AnimatedUnknown
 };
 
 } // namespace blink
