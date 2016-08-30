5a98afa3f8e5e37a989de8401ee616f6ac578759
fs <fs@opera.com>
2016-08-30 04:30:49 -0700

    Simplify SVGAnimatedTypeAnimator interface
    The two methods startAnimValAnimation and resetAnimValToBaseVal share
    the same implementation, but are called in two different branches of the
    same if-statement. Fold the two and add a new method
    createAnimatedValue() to provide the animated value, to make this appear
    as straight-forward as it is. Also fold the stopAnimValAnimation()
    method since it's only used once, and because this makes the start/stop
    action somewhat symmetric.
    Rename the constructFromString(...) method to
    createAnimatedValueFromString() to illustrate its relation to the newly
    minted method and make it a bit more clear what it does.
    
    BUG=640676
    
    Review-Url: https://codereview.chromium.org/2284263002
    Cr-Commit-Position: refs/heads/master@{#415271}

--- third_party/WebKit/Source/core/svg/SVGAnimateElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGAnimateElement.cpp
@@ -123,7 +123,7 @@ bool SVGAnimateElement::calculateToAtEndOfDurationValue(const String& toAtEndOfD
 {
     if (toAtEndOfDurationString.isEmpty())
         return false;
-    m_toAtEndOfDurationProperty = m_animator.constructFromString(toAtEndOfDurationString);
+    m_toAtEndOfDurationProperty = m_animator.createAnimatedValueFromString(toAtEndOfDurationString);
     return true;
 }
 
@@ -189,11 +189,8 @@ void SVGAnimateElement::resetAnimatedType()
         for (SVGElement* element : animatedElements)
             addReferenceTo(element);
 
-        if (!m_animatedProperty)
-            m_animatedProperty = m_animator.startAnimValAnimation();
-        else
-            m_animatedProperty = m_animator.resetAnimValToBaseVal();
-
+        m_animatedProperty = m_animator.createAnimatedValue();
+        targetElement->setAnimatedAttribute(attributeName, m_animatedProperty);
         return;
     }
     DCHECK_EQ(shouldApply, ApplyCSSAnimation);
@@ -203,7 +200,7 @@ void SVGAnimateElement::resetAnimatedType()
     DCHECK(isTargetAttributeCSSProperty(targetElement, attributeName));
     computeCSSPropertyValue(targetElement, cssPropertyID(attributeName.localName()), baseValue);
 
-    m_animatedProperty = m_animator.constructFromString(baseValue);
+    m_animatedProperty = m_animator.createAnimatedValueFromString(baseValue);
 }
 
 void SVGAnimateElement::clearAnimatedType()
@@ -233,7 +230,7 @@ void SVGAnimateElement::clearAnimatedType()
     }
     if (shouldApply == ApplyXMLandCSSAnimation || m_animator.isAnimatingSVGDom()) {
         // SVG DOM animVal animation code-path.
-        m_animator.stopAnimValAnimation();
+        targetElement->clearAnimatedAttribute(attributeName());
         if (shouldApply != DontApplyAnimation)
             targetElement->invalidateAnimatedAttribute(attributeName());
     }
