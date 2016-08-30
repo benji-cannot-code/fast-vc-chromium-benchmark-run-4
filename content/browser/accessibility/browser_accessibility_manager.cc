17c08cfbea5100f31197046cf8602e905365f7bb
nektar <nektar@chromium.org>
2016-08-30 13:36:53 -0700

    On the Mac, AX_EVENT_LOAD_COMPLETE should only be fired on the top document.
    In Apple's WebKit, AXLayoutComplete, and not AXLoadComplete, fires when an iframe loads. I am not sure if it also fires when an iframe finishes layout, so we should leave the |ui::AX_EVENT_LAYOUT_COMPLETE| case in for now.
    BUG=637895
    TESTED=Voiceover
    R=dmazzoni@chromium.org
    
    Review-Url: https://codereview.chromium.org/2297433002
    Cr-Commit-Position: refs/heads/master@{#415413}

--- content/browser/accessibility/browser_accessibility_manager.cc
+++ content/browser/accessibility/browser_accessibility_manager.cc
@@ -1033,6 +1033,10 @@ BrowserAccessibilityDelegate*
   return nullptr;
 }
 
+bool BrowserAccessibilityManager::IsRootTree() {
+  return delegate() && delegate()->AccessibilityGetAcceleratedWidget();
+}
+
 ui::AXTreeUpdate
 BrowserAccessibilityManager::SnapshotAXTreeForTesting() {
   std::unique_ptr<
--- content/browser/renderer_host/render_widget_host_view_mac.h
+++ content/browser/renderer_host/render_widget_host_view_mac.h
@@ -309,6 +309,7 @@ class CONTENT_EXPORT RenderWidgetHostViewMac
   BrowserAccessibilityManager* CreateBrowserAccessibilityManager(
       BrowserAccessibilityDelegate* delegate, bool for_root_frame) override;
   gfx::Point AccessibilityOriginInScreen(const gfx::Rect& bounds) override;
+  gfx::AcceleratedWidget AccessibilityGetAcceleratedWidget() override;
 
   bool HasAcceleratedSurface(const gfx::Size& desired_size) override;
   gfx::Rect GetBoundsInRootWindow() override;
