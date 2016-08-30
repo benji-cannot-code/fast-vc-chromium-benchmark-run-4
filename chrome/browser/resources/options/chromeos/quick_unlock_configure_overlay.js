106a6bd212a5acaccefe30b6be9f023d67f55cbe
xiaoyinh <xiaoyinh@chromium.org>
2016-08-29 21:13:53 -0700

    A11y improvements for quick unlock settings
    1. Make screen lock element tab reachable
    2. Add a button on the overlay
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    BUG=641591
    
    Review-Url: https://codereview.chromium.org/2280253002
    Cr-Commit-Position: refs/heads/master@{#415080}

--- chrome/browser/resources/options/chromeos/quick_unlock_configure_overlay.js
+++ chrome/browser/resources/options/chromeos/quick_unlock_configure_overlay.js
@@ -28,6 +28,9 @@ cr.define('options', function() {
     /** @override */
     initializePage: function() {
       Page.prototype.initializePage.call(this);
+      $('screen-lock-done').onclick = function() {
+        QuickUnlockConfigureOverlay.dismiss();
+      };
     },
 
     /** @override */
@@ -40,7 +43,8 @@ cr.define('options', function() {
       settings.navigateTo(settings.Route.LOCK_SCREEN);
       var lockScreen = document.querySelector('settings-lock-screen');
 
-      var checkbox = lockScreen.root.querySelector('settings-checkbox');
+      var checkbox =
+          lockScreen.root.querySelector('div.settings-box.single-column');
       checkbox.hidden = true;
 
       var passwordPrompt = lockScreen.root.
