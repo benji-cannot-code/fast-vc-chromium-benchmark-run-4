ac561cd6134a5073b4ec4beb13092eb75d22724f
mdjones <mdjones@chromium.org>
2016-08-30 14:39:33 -0700

    Revert of Add support for method selection in the Payment Request UI on iOS. (patchset #4 id:80001 of https://codereview.chromium.org/2285523002/ )
    Reason for revert:
    Breaking testMultilineStreetAddress on KitKat Tablet Tester
    
    Original issue's description:
    > Add support for method selection in the Payment Request UI on iOS.
    >
    > This includes:
    > - Moving the mapping between autofill credit card types and types
    >   defined in the spec to components/autofill.
    > - Adding the basic credit card response type from the spec.
    >
    > BUG=602666
    >
    > Committed: https://crrev.com/c3d5660f6d497ea5dffc885f198cefdc7aabad83
    > Cr-Commit-Position: refs/heads/master@{#415351}
    
    TBR=marq@chromium.org,eugenebut@chromium.org,estade@chromium.org,jdonnelly@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=602666
    
    Review-Url: https://codereview.chromium.org/2289263003
    Cr-Commit-Position: refs/heads/master@{#415436}

--- chrome/browser/autofill/android/personal_data_manager_android.cc
+++ chrome/browser/autofill/android/personal_data_manager_android.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/content/browser/content_autofill_driver.h"
 #include "components/autofill/content/browser/content_autofill_driver_factory.h"
 #include "components/autofill/core/browser/autofill_country.h"
-#include "components/autofill/core/browser/autofill_data_util.h"
 #include "components/autofill/core/browser/autofill_type.h"
 #include "components/autofill/core/browser/country_names.h"
 #include "components/autofill/core/browser/field_types.h"
@@ -133,11 +132,41 @@ void PopulateNativeProfileFromJava(
       Java_AutofillProfile_getLanguageCode(env, jprofile)));
 }
 
+// Mapping from Chrome card types to PaymentRequest basic card payment spec and
+// icons. Note that "generic" is not in the spec.
+// https://w3c.github.io/webpayments-methods-card/#method-id
+const struct PaymentRequestData {
+  const char* card_type;
+  const char* basic_card_payment_type;
+  const int icon_resource_id;
+} kPaymentRequestData[] {
+  {"genericCC", "generic", IDR_AUTOFILL_PR_GENERIC},
+
+  {"americanExpressCC", "amex", IDR_AUTOFILL_PR_AMEX},
+  {"dinersCC", "diners", IDR_AUTOFILL_PR_DINERS},
+  {"discoverCC", "discover", IDR_AUTOFILL_PR_DISCOVER},
+  {"jcbCC", "jcb", IDR_AUTOFILL_PR_JCB},
+  {"masterCardCC", "mastercard", IDR_AUTOFILL_PR_MASTERCARD},
+  {"unionPayCC", "unionpay", IDR_AUTOFILL_PR_UNIONPAY},
+  {"visaCC", "visa", IDR_AUTOFILL_PR_VISA},
+};
+
+// Converts the card type into PaymentRequest type according to the basic card
+// payment spec and an icon. Will set the type and the icon to "generic" for
+// unrecognized card type.
+const PaymentRequestData& GetPaymentRequestData(const std::string& type) {
+  for (size_t i = 0; i < arraysize(kPaymentRequestData); ++i) {
+    if (type == kPaymentRequestData[i].card_type)
+      return kPaymentRequestData[i];
+  }
+  return kPaymentRequestData[0];
+}
+
 ScopedJavaLocalRef<jobject> CreateJavaCreditCardFromNative(
     JNIEnv* env,
     const CreditCard& card) {
-  const data_util::PaymentRequestData& payment_request_data =
-      data_util::GetPaymentRequestData(card.type());
+  const PaymentRequestData& payment_request_data =
+      GetPaymentRequestData(card.type());
   return Java_CreditCard_create(
       env, ConvertUTF8ToJavaString(env, card.guid()),
       ConvertUTF8ToJavaString(env, card.origin()),
@@ -154,6 +183,16 @@ ScopedJavaLocalRef<jobject> CreateJavaCreditCardFromNative(
       ResourceMapper::MapFromChromiumId(payment_request_data.icon_resource_id),
       ConvertUTF8ToJavaString(env, card.billing_address_id()),
       ConvertUTF8ToJavaString(env, card.server_id()));
+}
+
+const char* GetCardTypeForBasicCardPaymentType(
+    const std::string& basic_card_payment_type) {
+  for (size_t i = 0; i < arraysize(kPaymentRequestData); ++i) {
+    if (basic_card_payment_type ==
+        kPaymentRequestData[i].basic_card_payment_type)
+      return kPaymentRequestData[i].card_type;
+  }
+  return kPaymentRequestData[0].card_type;
 }
 
 void PopulateNativeCreditCardFromJava(
@@ -187,8 +226,9 @@ void PopulateNativeCreditCardFromJava(
     } else {
       card->set_record_type(CreditCard::MASKED_SERVER_CARD);
       card->SetTypeForMaskedCard(
-          data_util::GetCardTypeForBasicCardPaymentType(ConvertJavaStringToUTF8(
-              env, Java_CreditCard_getBasicCardPaymentType(env, jcard))));
+          GetCardTypeForBasicCardPaymentType(
+              ConvertJavaStringToUTF8(
+                  env, Java_CreditCard_getBasicCardPaymentType(env, jcard))));
     }
   }
 }
@@ -444,11 +484,11 @@ PersonalDataManagerAndroid::GetBasicCardPaymentTypeIfValid(
     const JavaParamRef<jstring>& jcard_number) {
   base::string16 card_number = ConvertJavaStringToUTF16(env, jcard_number);
   return ConvertUTF8ToJavaString(
-      env, IsValidCreditCardNumber(card_number)
-               ? data_util::GetPaymentRequestData(
-                     CreditCard::GetCreditCardType(card_number))
-                     .basic_card_payment_type
-               : "");
+      env,
+      IsValidCreditCardNumber(card_number)
+          ? GetPaymentRequestData(CreditCard::GetCreditCardType(card_number))
+                .basic_card_payment_type
+          : "");
 }
 
 void PersonalDataManagerAndroid::AddServerCreditCardForTest(
