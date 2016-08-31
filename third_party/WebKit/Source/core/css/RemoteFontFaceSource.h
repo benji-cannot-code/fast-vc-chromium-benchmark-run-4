d7f75ee8154bd5ec7a658f942a34a74481b187e9
toyoshim <toyoshim@chromium.org>
2016-08-31 15:30:03 -0700

    WebFonts: classify memory cache hit case in WebFont.CacheHit metric
    wasCached() does not contain memory cache hit case. But WebFonts
    intervention study's results imply memory cache hit case is not
    minor group, but probably bigger one than Miss group.
    
    This patch adds one additional group MemoryHit to WebFoint.CacheHit
    metric to ensure memory cache hit ratio.
    
    Review-Url: https://codereview.chromium.org/2289113002
    Cr-Commit-Position: refs/heads/master@{#415783}

--- third_party/WebKit/Source/core/css/RemoteFontFaceSource.h
+++ third_party/WebKit/Source/core/css/RemoteFontFaceSource.h
@@ -68,7 +68,7 @@ private:
         void fontLoaded(bool isInterventionTriggered, bool isLoadedFromNetwork);
         void longLimitExceeded(bool isInterventionTriggered);
         void recordFallbackTime(const FontResource*);
-        void recordRemoteFont(const FontResource*);
+        void recordRemoteFont(const FontResource*, bool isLoadedFromMemoryCache);
         bool hadBlankText() { return m_blankPaintTime; }
     private:
         void recordLoadTimeHistogram(const FontResource*, int duration);
