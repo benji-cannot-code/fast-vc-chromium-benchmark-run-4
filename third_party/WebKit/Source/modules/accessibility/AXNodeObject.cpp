a6628ec1b67d46d82b3eeb1b2a7346bbc54119d0
glebl <glebl@chromium.org>
2016-08-31 00:22:26 -0700

    Add grid/flex layout support for <fieldset>
    This adds grid/flex layout support for <fieldset> by
    introducing an anonymous inner block that is used to
    layout fieldset's underlying blocks without interfering
    with the special paint flow used to render <legend>
    element.
    
    This is the updated version of http://crrev.com/2150003005 that passes failed ClusterFuzz tests.
    
    W3C/CSS WG discussion:
    https://github.com/w3c/csswg-drafts/issues/321
    
    Testing:
    - All affected tests' results were verified to make sure
    that there are no differences in pixel test results between
    old and new version of LayoutFieldSet.
    - 2 more tests were added
    (fieldset-display-{flex|grid}.html)
    - verified interoperability of fieldset(display: flex)
    with Firefox(one of 2 browsers that support fieldset's flex
    layout)
    
    BUG=262679,375693
    TEST=fast/forms/fieldset/fieldset-display-flex.html;
    fast/forms/fieldset/fieldset-display-grid.html
    
    Committed: https://crrev.com/c6d69f896f406c9a7801b29cb8c02a88e5b01770
    Cr-Commit-Position: refs/heads/master@{#409303}
    
    patch from issue 2150003005 at patchset 290001 (http://crrev.com/2150003005#ps290001)
    
    Review-Url: https://codereview.chromium.org/2215133005
    Cr-Commit-Position: refs/heads/master@{#415577}

--- third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
@@ -538,6 +538,10 @@ AccessibilityRole AXNodeObject::nativeAccessibilityRoleIgnoringAria() const
     if (isHTMLHRElement(*getNode()))
         return SplitterRole;
 
+    if (isFieldset()) {
+        return GroupRole;
+    }
+
     return UnknownRole;
 }
 
