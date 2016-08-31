84e61d7f4fbb10a31c2ac9d9f1ca433139b829c8
lushnikov <lushnikov@chromium.org>
2016-08-30 19:47:25 -0700

    DevTools: do not leak memory through event listeners
    EventListeners array hold references to the objects which
    they were listening to, preventing them from being
    cleaned up.
    
    BUG=none
    R=dgozman
    
    Review-Url: https://codereview.chromium.org/2298473003
    Cr-Commit-Position: refs/heads/master@{#415545}

--- third_party/WebKit/Source/devtools/front_end/bindings/NetworkProject.js
+++ third_party/WebKit/Source/devtools/front_end/bindings/NetworkProject.js
@@ -401,7 +401,6 @@ WebInspector.NetworkProject.prototype = {
     {
         this._reset();
         WebInspector.EventTarget.removeEventListeners(this._eventListeners);
-        this._eventListeners = [];
         delete this.target()[WebInspector.NetworkProject._networkProjectSymbol];
     },
 
