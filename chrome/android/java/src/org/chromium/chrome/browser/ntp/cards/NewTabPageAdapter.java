8590a85c28a0d97fbf56e8173bb47e6273ca64d8
pke <pke@google.com>
2016-08-31 02:57:41 -0700

    Fix suggestion dismissing when underlying data changes
    When a suggestion is dismissed from the NewTabPage using the 'Remove'
    context menu item, the content that it refers to cannot be looked up
    from the corresponding ViewHolder, as onBindViewHolder may be called
    while the context menu is still open (which would result in the wrong
    suggestion being dismissed).
    Instead, store the referenced suggestion in the listener for the
    context menu items. This requires some additional refactorings.
    
    When the 'Remove' item is clicked for a suggestion that has been
    removed for other reasons (section became unavailable, suggestion was
    invalidated) in the meantime, ignore the call. When it is clicked for
    a suggestion that as moved out of the visible view, skip the
    animation and just dismiss it from the underlying data model directly.
    
    BUG=641313
    
    Review-Url: https://codereview.chromium.org/2285833002
    Cr-Commit-Position: refs/heads/master@{#415597}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/NewTabPageAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/NewTabPageAdapter.java
@@ -70,8 +70,7 @@ public class NewTabPageAdapter extends Adapter<NewTabPageViewHolder>
         @Override
         public void onSwiped(ViewHolder viewHolder, int direction) {
             mRecyclerView.onItemDismissStarted(viewHolder.itemView);
-
-            NewTabPageAdapter.this.dismissItem(viewHolder);
+            NewTabPageAdapter.this.dismissItem(viewHolder.getAdapterPosition());
         }
 
         @Override
@@ -109,7 +108,7 @@ public class NewTabPageAdapter extends Adapter<NewTabPageViewHolder>
                 float dX, float dY, int actionState, boolean isCurrentlyActive) {
             assert viewHolder instanceof NewTabPageViewHolder;
 
-            ((NewTabPageViewHolder) viewHolder).updateViewStateForDismiss(dX);
+            mRecyclerView.updateViewStateForDismiss(dX, viewHolder);
 
             // The super implementation performs animation and elevation, but only the animation is
             // needed.
@@ -312,6 +311,18 @@ public class NewTabPageAdapter extends Adapter<NewTabPageViewHolder>
         return getGroupPositionOffset(mBottomSpacer);
     }
 
+    public int getSuggestionPosition(String suggestionId) {
+        List<NewTabPageItem> items = getItems();
+        for (int i = 0; i < items.size(); i++) {
+            NewTabPageItem item = items.get(i);
+            if (item instanceof SnippetArticle
+                    && ((SnippetArticle) item).mId.equals(suggestionId)) {
+                return i;
+            }
+        }
+        return RecyclerView.NO_POSITION;
+    }
+
     /** Start a request for new snippets. */
     public void reloadSnippets() {
         SnippetsBridge.fetchSnippets(/*forceRequest=*/true);
@@ -361,10 +372,8 @@ public class NewTabPageAdapter extends Adapter<NewTabPageViewHolder>
         mRecyclerView = (NewTabPageRecyclerView) recyclerView;
     }
 
-    public void dismissItem(ViewHolder itemViewHolder) {
-        int position = itemViewHolder.getAdapterPosition();
+    public void dismissItem(int position) {
         SnippetArticle suggestion = (SnippetArticle) getItems().get(position);
-
         mSuggestionsSource.getSuggestionVisited(suggestion, new Callback<Boolean>() {
             @Override
             public void onResult(Boolean result) {
@@ -374,9 +383,8 @@ public class NewTabPageAdapter extends Adapter<NewTabPageViewHolder>
             }
         });
 
-        itemViewHolder.itemView.announceForAccessibility(
-                itemViewHolder.itemView.getResources().getString(
-                        R.string.ntp_accessibility_item_removed, suggestion.mTitle));
+        mRecyclerView.announceForAccessibility(mRecyclerView.getResources().getString(
+                R.string.ntp_accessibility_item_removed, suggestion.mTitle));
 
         mSuggestionsSource.dismissSuggestion(suggestion);
         SuggestionsSection section = (SuggestionsSection) getGroup(position);
