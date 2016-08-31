9a92079daf3c05b8ebf57e5e0064c693945d29f5
steimel <steimel@chromium.org>
2016-08-31 11:29:33 -0700

    Add command line flag to allow testing clients not connected to localhost. Otherwise, restrict connections to localhost
    BUG=582102
    TEST=Run the engine without the "listen-outside" flag and make sure incoming connections are limited to localhost. Run with --listen-outside=1 and make sure it allows incomiong connections from outside localhost
    
    Review-Url: https://codereview.chromium.org/2292563003
    Cr-Commit-Position: refs/heads/master@{#415687}

--- blimp/engine/app/switches.cc
+++ blimp/engine/app/switches.cc
@@ -9,6 +9,7 @@ namespace blimp {
 namespace engine {
 
 const char kEnginePort[] = "engine-port";
+const char kAllowNonLocalhost[] = "allow-non-localhost";
 
 }  // namespace engine
 }  // namespace blimp
