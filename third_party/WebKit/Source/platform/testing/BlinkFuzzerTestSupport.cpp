d8b3cdc5993a3d141d78aaaa5f4e5ef761780afb
csharrison <csharrison@chromium.org>
2016-08-31 08:10:43 -0700

    Add fuzzer infra to blink and fuzz CSS parser
    This patch adds initialization for blink fuzzers that need the
    full unit test environment, as well as a simple fuzzer for the
    CSS tokenizer + parser.
    
    BUG=642570,642572
    
    Review-Url: https://codereview.chromium.org/2274563002
    Cr-Commit-Position: refs/heads/master@{#415634}

---
+++ third_party/WebKit/Source/platform/testing/BlinkFuzzerTestSupport.cpp
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/testing/BlinkFuzzerTestSupport.h"
+
+#include "base/at_exit.h"
+#include "base/command_line.h"
+#include "mojo/edk/embedder/embedder.h"
+#include "platform/weborigin/SchemeRegistry.h"
+#include <content/test/blink_test_environment.h>
+
+namespace blink {
+
+void InitializeBlinkFuzzTest(int* argc, char ***argv)
+{
+    // Note: we don't tear anything down here after an iteration of the fuzzer
+    // is complete, this is for efficiency. We rerun the fuzzer with the same
+    // environment as the previous iteration.
+    base::AtExitManager atExit;
+
+    mojo::edk::Init();
+    base::CommandLine::Init(*argc, *argv);
+
+    content::SetUpBlinkTestEnvironment();
+
+    blink::SchemeRegistry::initialize();
+}
+
+} // namespace blink
