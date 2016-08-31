b487e2e36f99f5ca9c57947c99eeef7e6373bdd4
stanisc <stanisc@chromium.org>
2016-08-31 13:49:19 -0700

    Revert of Disable flaky TaskManagerBrowserTest.DevToolsNew{Und,D}ockedWindow tests (patchset #1 id:1 of https://codereview.chromium.org/2301493002/ )
    Reason for revert:
    The bug is now fixed so disabling these tests is no longer needed.
    See https://codereview.chromium.org/2293293002/
    
    Original issue's description:
    > Disable flaky TaskManagerBrowserTest.DevToolsNew{Und,D}ockedWindow tests
    >
    > BUG=642482
    > TBR=stanisc@chromium.org,nick@chromium.org:
    >
    > Committed: https://crrev.com/a01d97dff593c530fe6b204421bdaa39252801e9
    > Cr-Commit-Position: refs/heads/master@{#415595}
    
    TBR=nick@chromium.org,battre@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=642482
    
    Review-Url: https://codereview.chromium.org/2298213003
    Cr-Commit-Position: refs/heads/master@{#415747}

--- chrome/browser/task_manager/task_manager_browsertest.cc
+++ chrome/browser/task_manager/task_manager_browsertest.cc
@@ -701,9 +701,7 @@ IN_PROC_BROWSER_TEST_F(TaskManagerUtilityProcessBrowserTest,
       WaitForTaskManagerRows(1, MatchUtility(proxy_resolver_name)));
 }
 
-// TODO(crbug.com/642482): Disabled flaky test.
-IN_PROC_BROWSER_TEST_F(TaskManagerBrowserTest,
-                       DISABLED_DevToolsNewDockedWindow) {
+IN_PROC_BROWSER_TEST_F(TaskManagerBrowserTest, DevToolsNewDockedWindow) {
   ShowTaskManager();  // Task manager shown BEFORE dev tools window.
 
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerRows(1, MatchAnyTab()));
@@ -714,9 +712,7 @@ IN_PROC_BROWSER_TEST_F(TaskManagerBrowserTest,
   DevToolsWindowTesting::CloseDevToolsWindowSync(devtools);
 }
 
-// TODO(crbug.com/642482): Disabled flaky test.
-IN_PROC_BROWSER_TEST_F(TaskManagerBrowserTest,
-                       DISABLED_DevToolsNewUndockedWindow) {
+IN_PROC_BROWSER_TEST_F(TaskManagerBrowserTest, DevToolsNewUndockedWindow) {
   ShowTaskManager();  // Task manager shown BEFORE dev tools window.
   ASSERT_NO_FATAL_FAILURE(WaitForTaskManagerRows(1, MatchAnyTab()));
   DevToolsWindow* devtools =
