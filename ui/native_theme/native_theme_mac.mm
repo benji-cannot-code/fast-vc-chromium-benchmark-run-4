6a28591f819bd3dcec9e3ebab3c3348b8bf851ca
ellyjones <ellyjones@chromium.org>
2016-08-31 03:52:27 -0700

    macviews: apply graphite tint to button colors when needed
    BUG=603176
    
    Review-Url: https://codereview.chromium.org/2292233002
    Cr-Commit-Position: refs/heads/master@{#415605}

--- ui/native_theme/native_theme_mac.mm
+++ ui/native_theme/native_theme_mac.mm
@@ -134,20 +134,20 @@ SkColor NativeThemeMac::GetSystemColor(ColorId color_id) const {
       return NSSystemColorToSkColor([NSColor windowBackgroundColor]);
     case kColorId_DialogBackground:
       return ui::MaterialDesignController::IsSecondaryUiMaterial()
-                ? GetAuraColor(color_id, this)
+                ? GetAuraColorWithSystemTint(color_id, this)
                 : kDialogBackgroundColor;
     case kColorId_BubbleBackground:
       return SK_ColorWHITE;
 
     case kColorId_FocusedBorderColor:
       return ui::MaterialDesignController::IsSecondaryUiMaterial()
-                ? GetAuraColor(color_id, this)
+                ? GetAuraColorWithSystemTint(color_id, this)
                 : NSSystemColorToSkColor([NSColor keyboardFocusIndicatorColor]);
     case kColorId_FocusedMenuButtonBorderColor:
       return NSSystemColorToSkColor([NSColor keyboardFocusIndicatorColor]);
     case kColorId_UnfocusedBorderColor:
       return ui::MaterialDesignController::IsSecondaryUiMaterial()
-                ? GetAuraColor(color_id, this)
+                ? GetAuraColorWithSystemTint(color_id, this)
                 : NSSystemColorToSkColor([NSColor controlColor]);
 
     // Buttons and labels.
@@ -162,7 +162,7 @@ SkColor NativeThemeMac::GetSystemColor(ColorId color_id) const {
       return NSSystemColorToSkColor([NSColor controlTextColor]);
     case kColorId_CallToActionColor:
       return ui::MaterialDesignController::IsSecondaryUiMaterial()
-                ? GetAuraColor(color_id, this)
+                ? GetAuraColorWithSystemTint(color_id, this)
                 : NSSystemColorToSkColor([NSColor controlTextColor]);
     case kColorId_ButtonDisabledColor:
     case kColorId_LabelDisabledColor:
@@ -426,6 +426,12 @@ void NativeThemeMac::PaintStyledGradientButton(SkCanvas* canvas,
   paint.setShader(nullptr);
   paint.setColor(kFocusRingColor);
   canvas->drawDRRect(outer_shape, shape, paint);
+}
+
+SkColor NativeThemeMac::GetAuraColorWithSystemTint(
+    NativeTheme::ColorId color_id,
+    const NativeTheme* base_theme) const {
+  return ApplySystemControlTint(GetAuraColor(color_id, base_theme));
 }
 
 NativeThemeMac::NativeThemeMac() {
