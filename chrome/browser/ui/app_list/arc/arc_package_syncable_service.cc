48047a6097967449c0fe8463ed6f314bc8e11ee6
lgcheng <lgcheng@google.com>
2016-08-31 13:50:14 -0700

    arc: Add arc enable sync pref initial set up.
    Arc enable sync pref is false by default if user never modify sync settings. This
    patch enables arc sync service to start when user never manually modify sync
    settings.
    
    BUG= http://b/31156650
    TEST=Manual test on a machine that sync settings is never modified.
    
    Review-Url: https://codereview.chromium.org/2289163003
    Cr-Commit-Position: refs/heads/master@{#415748}

--- chrome/browser/ui/app_list/arc/arc_package_syncable_service.cc
+++ chrome/browser/ui/app_list/arc/arc_package_syncable_service.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/api/sync_change_processor.h"
 #include "components/sync/api/sync_data.h"
 #include "components/sync/api/sync_merge_result.h"
+#include "components/sync/driver/pref_names.h"
 #include "components/sync/driver/sync_prefs.h"
 #include "components/sync/driver/sync_service.h"
 #include "components/sync/protocol/sync.pb.h"
@@ -71,6 +72,26 @@ std::unique_ptr<ArcSyncItem> CreateSyncItemFromPrefs(
   return base::MakeUnique<ArcSyncItem>(
       package_info->package_name, package_info->package_version,
       package_info->last_backup_android_id, package_info->last_backup_time);
+}
+
+bool ValidateEnableArcPackageSyncPref(Profile* profile) {
+  PrefService* pref_service = profile->GetPrefs();
+  // If device is set to sync everything, Arc package should be synced.
+  if (pref_service->GetBoolean(sync_driver::prefs::kSyncKeepEverythingSynced))
+    return true;
+
+  bool apps_sync_enable = pref_service->GetBoolean(
+      sync_driver::SyncPrefs::GetPrefNameForDataType(syncer::APPS));
+  // ArcPackage sync service is controlled by apps checkbox in sync settings.
+  // Update ArcPackage sync setting pref if it is different from apps sync
+  // setting pref.
+  const char* arc_sync_path =
+      sync_driver::SyncPrefs::GetPrefNameForDataType(syncer::ARC_PACKAGE);
+  if (apps_sync_enable != pref_service->GetBoolean(arc_sync_path)) {
+    pref_service->SetBoolean(arc_sync_path, apps_sync_enable);
+  }
+
+  return apps_sync_enable;
 }
 
 }  // namespace
@@ -232,8 +253,8 @@ bool ArcPackageSyncableService::SyncStarted() {
   sync_driver::SyncService* sync_service =
       ProfileSyncServiceFactory::GetSyncServiceForBrowserContext(profile_);
   // ArcPackage sync service is controlled by apps checkbox in sync settings.
-  bool arc_package_sync_should_enable = profile_->GetPrefs()->GetBoolean(
-      sync_driver::SyncPrefs::GetPrefNameForDataType(syncer::ARC_PACKAGE));
+  bool arc_package_sync_should_enable =
+      ValidateEnableArcPackageSyncPref(profile_);
 
   if (!sync_service || !arc_package_sync_should_enable)
     return false;
