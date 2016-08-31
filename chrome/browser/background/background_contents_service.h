b38cc2d02a87ea21cd3d88407cc9de426c26508b
apacible <apacible@chromium.org>
2016-08-30 21:28:15 -0700

    Use exponential backoff for component extension reloads.
    Currently, any force installed extension attempts to reload after 3s. We're currently seeing an uptick in extension process crashes -- please see BUG for details.
    
    This is a short term fix, which will mitigate crash loops for component extensions. Any extensions that are policy-enabled will still be reloaded after 3s. We avoid using exponential backoff for policy-installed extensions since this can allow users to kill the extension multiple times, effectively disabling it.
    
    This patch adds tracking for BackoffEntry objects corresponding to each component extensions that attempt to reload. The backoff policy values are similar to the extension check update's backoff policy.
    
    BUG=639104
    
    Review-Url: https://codereview.chromium.org/2281323002
    Cr-Commit-Position: refs/heads/master@{#415555}

--- chrome/browser/background/background_contents_service.h
+++ chrome/browser/background/background_contents_service.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/common/window_container_type.h"
 #include "extensions/browser/extension_registry_observer.h"
+#include "net/base/backoff_entry.h"
 #include "ui/base/window_open_disposition.h"
 #include "url/gurl.h"
 
@@ -239,6 +240,12 @@ class BackgroundContentsService : private content::NotificationObserver,
   typedef std::map<base::string16, BackgroundContentsInfo>
       BackgroundContentsMap;
   BackgroundContentsMap contents_map_;
+
+  // Map associating component extensions that have attempted to reload with a
+  // BackoffEntry keeping track of retry timing.
+  typedef std::map<extensions::ExtensionId, std::unique_ptr<net::BackoffEntry>>
+      ExtensionBackoffEntryMap;
+  ExtensionBackoffEntryMap backoff_map_;
 
   ScopedObserver<extensions::ExtensionRegistry,
                  extensions::ExtensionRegistryObserver>
