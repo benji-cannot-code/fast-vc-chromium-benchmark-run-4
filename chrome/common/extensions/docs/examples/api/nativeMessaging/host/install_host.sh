1418aa41f6c47845be1b4db5402861efaa05cd07
krytarowski <krytarowski@gmail.com>
2016-09-01 04:35:13 -0700

    Fix unportable shell construct (detected with pkgsrc)
    BUG=
    
    Review-Url: https://codereview.chromium.org/2216023004
    Cr-Commit-Position: refs/heads/master@{#415930}

--- chrome/common/extensions/docs/examples/api/nativeMessaging/host/install_host.sh
+++ chrome/common/extensions/docs/examples/api/nativeMessaging/host/install_host.sh
@@ -7,14 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 set -e
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-if [ "$(uname -s)" == "Darwin" ]; then
-  if [ "$(whoami)" == "root" ]; then
+if [ "$(uname -s)" = "Darwin" ]; then
+  if [ "$(whoami)" = "root" ]; then
     TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
   else
     TARGET_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
   fi
 else
-  if [ "$(whoami)" == "root" ]; then
+  if [ "$(whoami)" = "root" ]; then
     TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
   else
     TARGET_DIR="$HOME/.config/google-chrome/NativeMessagingHosts"
--- chrome/common/extensions/docs/examples/api/nativeMessaging/host/uninstall_host.sh
+++ chrome/common/extensions/docs/examples/api/nativeMessaging/host/uninstall_host.sh
@@ -6,14 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 set -e
 
-if [ "$(uname -s)" == "Darwin" ]; then
-  if [ "$(whoami)" == "root" ]; then
+if [ "$(uname -s)" = "Darwin" ]; then
+  if [ "$(whoami)" = "root" ]; then
     TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
   else
     TARGET_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
   fi
 else
-  if [ "$(whoami)" == "root" ]; then
+  if [ "$(whoami)" = "root" ]; then
     TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
   else
     TARGET_DIR="$HOME/.config/google-chrome/NativeMessagingHosts"
