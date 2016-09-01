c40bdc277c371636a7a8b3c61442341b58cf8430
rtoy <rtoy@chromium.org>
2016-09-01 15:05:52 -0700

    Return the correct channelCountMode and channelInterpretation
    Instead of returning the current mode or interpretation, return the
    value used in the setter.  Eventually, the real mode will get set to
    the desired value.
    
    The members m_channelCountMode and m_channelInterpretation are also
    made private to force use of the setters and getters.  A setter is
    added too so that the internal state is maintained.
    
    BUG=636576
    TEST=channel-mode-interp-basic.html
    
    Review-Url: https://codereview.chromium.org/2282483002
    Cr-Commit-Position: refs/heads/master@{#416078}

--- third_party/WebKit/Source/modules/webaudio/ConvolverNode.cpp
+++ third_party/WebKit/Source/modules/webaudio/ConvolverNode.cpp
@@ -52,8 +52,8 @@ ConvolverHandler::ConvolverHandler(AudioNode& node, float sampleRate)
 
     // Node-specific default mixing rules.
     m_channelCount = 2;
-    m_channelCountMode = ClampedMax;
-    m_channelInterpretation = AudioBus::Speakers;
+    setInternalChannelCountMode(ClampedMax);
+    setInternalChannelInterpretation(AudioBus::Speakers);
 
     initialize();
 }
--- third_party/WebKit/Source/modules/webaudio/ScriptProcessorNode.cpp
+++ third_party/WebKit/Source/modules/webaudio/ScriptProcessorNode.cpp
@@ -57,7 +57,7 @@ ScriptProcessorHandler::ScriptProcessorHandler(AudioNode& node, float sampleRate
     addOutput(numberOfOutputChannels);
 
     m_channelCount = numberOfInputChannels;
-    m_channelCountMode = Explicit;
+    setInternalChannelCountMode(Explicit);
 
     initialize();
 }
