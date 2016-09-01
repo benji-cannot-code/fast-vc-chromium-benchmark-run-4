28d3ea2b497566eb5b6be84cad445eb40184cbfa
fs <fs@opera.com>
2016-09-01 01:27:54 -0700

    Resolve percentage in <use> against the instance's viewport element
    In LayoutSVGTransformableContainer::calculateLocalTransform, 'x' and 'y'
    were resolved against the original (corresponding) element. For a <use>
    nested within a <symbol>, this would mean that when were going to look
    up the viewport element, we'd return the outer <symbol> element and thus
    fail to get a viewport.
    Use the instance element to setup the SVGLengthContext instead.
    
    BUG=642524
    
    Review-Url: https://codereview.chromium.org/2298783002
    Cr-Commit-Position: refs/heads/master@{#415910}

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGTransformableContainer.cpp
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGTransformableContainer.cpp
@@ -91,7 +91,7 @@ SVGTransformChange LayoutSVGTransformableContainer::calculateLocalTransform()
     }
 
     if (useElement) {
-        SVGLengthContext lengthContext(useElement);
+        SVGLengthContext lengthContext(element);
         FloatSize translation(
             useElement->x()->currentValue()->value(lengthContext),
             useElement->y()->currentValue()->value(lengthContext));
