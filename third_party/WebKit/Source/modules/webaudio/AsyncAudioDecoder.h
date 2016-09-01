3653b37e4ea168acd33d6e9efe0f861476a5296e
hongchan <hongchan@chromium.org>
2016-09-01 15:54:11 -0700

    Use background thread for AsyncAudioDecoder
    With the creation of BaseAudioContext, an additional thread dedicated to
    AsyncAudioDecoder is also instantiated. This design is sub-optimal and we
    can use the background thread (i.e. WorkerPool) for this purpose.
    
    BUG=640793
    TEST=NONE (all the decoder-related layout test passes)
    
    Review-Url: https://codereview.chromium.org/2268403006
    Cr-Commit-Position: refs/heads/master@{#416100}

--- third_party/WebKit/Source/modules/webaudio/AsyncAudioDecoder.h
+++ third_party/WebKit/Source/modules/webaudio/AsyncAudioDecoder.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define AsyncAudioDecoder_h
 
 #include "platform/heap/Handle.h"
-#include "public/platform/WebThread.h"
 #include "wtf/build_config.h"
 #include <memory>
 
@@ -40,15 +39,17 @@ class AudioBus;
 class DOMArrayBuffer;
 class ScriptPromiseResolver;
 
-// AsyncAudioDecoder asynchronously decodes audio file data from a DOMArrayBuffer in a worker thread.
-// Upon successful decoding, a completion callback will be invoked with the decoded PCM data in an AudioBuffer.
+// AsyncAudioDecoder asynchronously decodes audio file data from a
+// DOMArrayBuffer in the background thread. Upon successful decoding, a
+// completion callback will be invoked with the decoded PCM data in an
+// AudioBuffer.
 
 class AsyncAudioDecoder {
     DISALLOW_NEW();
     WTF_MAKE_NONCOPYABLE(AsyncAudioDecoder);
 public:
-    AsyncAudioDecoder();
-    ~AsyncAudioDecoder();
+    AsyncAudioDecoder() { };
+    ~AsyncAudioDecoder() { };
 
     // Must be called on the main thread.  |decodeAsync| and callees must not modify any of the
     // parameters except |audioData|.  They are used to associate this decoding instance with the
@@ -57,10 +58,8 @@ public:
 
 private:
     AudioBuffer* createAudioBufferFromAudioBus(AudioBus*);
-    static void decode(DOMArrayBuffer* audioData, float sampleRate, AudioBufferCallback* successCallback, AudioBufferCallback* errorCallback, ScriptPromiseResolver*, BaseAudioContext*);
+    static void decodeOnBackgroundThread(DOMArrayBuffer* audioData, float sampleRate, AudioBufferCallback* successCallback, AudioBufferCallback* errorCallback, ScriptPromiseResolver*, BaseAudioContext*);
     static void notifyComplete(DOMArrayBuffer* audioData, AudioBufferCallback* successCallback, AudioBufferCallback* errorCallback, AudioBus*, ScriptPromiseResolver*, BaseAudioContext*);
-
-    std::unique_ptr<WebThread> m_thread;
 };
 
 } // namespace blink
