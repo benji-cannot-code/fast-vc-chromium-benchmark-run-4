468664eb412feaa23dcdac8317bc58c2acdd5f9a
trizzofo <trizzofo@google.com>
2016-09-01 16:18:27 -0700

    [HBD] Intercept navigation to Flash download page
    Intercepts navigation to get.adobe.com/flashplayer
    
    Here is more information about HTML5 by Default: go/hbd-design-doc
    
    BUG=626728
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2272123002
    Cr-Commit-Position: refs/heads/master@{#416113}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -303,6 +303,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(ENABLE_PLUGINS)
 #include "chrome/browser/plugins/chrome_content_browser_client_plugins_part.h"
+#include "chrome/browser/plugins/flash_download_interception.h"
 #endif
 
 #if defined(ENABLE_SPELLCHECK)
@@ -3004,7 +3005,15 @@ ScopedVector<content::NavigationThrottle>
 ChromeContentBrowserClient::CreateThrottlesForNavigation(
     content::NavigationHandle* handle) {
   ScopedVector<content::NavigationThrottle> throttles;
+
   if (handle->IsInMainFrame()) {
+#if defined(ENABLE_PLUGINS)
+    std::unique_ptr<content::NavigationThrottle> flash_url_throttle =
+        FlashDownloadInterception::MaybeCreateThrottleFor(handle);
+    if (flash_url_throttle)
+      throttles.push_back(std::move(flash_url_throttle));
+#endif
+
     throttles.push_back(
         page_load_metrics::MetricsNavigationThrottle::Create(handle));
   }
