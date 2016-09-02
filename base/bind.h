1c21695e70c0da8b6b4c50aced80f92539e888ce
tzik <tzik@chromium.org>
2016-09-02 00:36:55 -0700

    Move documentation of base::Callback from base/callback.h to docs/callback.md
    BUG=554299
    
    Review-Url: https://codereview.chromium.org/2300983004
    Cr-Commit-Position: refs/heads/master@{#416208}

--- base/bind.h
+++ base/bind.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Usage documentation
 // -----------------------------------------------------------------------------
 //
-// See base/callback.h for documentation.
+// See //docs/callback.md for documentation.
 //
 //
 // -----------------------------------------------------------------------------
