8859e6611dfcff893b23c1e7d9d761be7a2cfe20
Andrey Lushnikov <lushnikov@chromium.org>
2016-09-02 09:06:19 -0700

    DevTools: fix hosted mode
    This was regressed in crrev.com/2296983003
    
    BUG=none
    TBR=dgozman
    
    Review URL: https://codereview.chromium.org/2303663003 .
    
    Cr-Commit-Position: refs/heads/master@{#416277}

--- third_party/WebKit/Source/devtools/front_end/workspace/IsolatedFileSystemManager.js
+++ third_party/WebKit/Source/devtools/front_end/workspace/IsolatedFileSystemManager.js
@@ -93,8 +93,8 @@ WebInspector.IsolatedFileSystemManager.prototype = {
     {
         var fulfill;
         var promise = new Promise(f => fulfill = f);
-        InspectorFrontendHost.requestFileSystems();
         InspectorFrontendHost.events.addEventListener(InspectorFrontendHostAPI.Events.FileSystemsLoaded, onFileSystemsLoaded, this);
+        InspectorFrontendHost.requestFileSystems();
         return promise;
 
         /**
