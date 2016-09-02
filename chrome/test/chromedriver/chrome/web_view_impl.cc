d79ae4e4af799b4699a2c7ef41a2bc7efc1756c6
samuong <samuong@chromium.org>
2016-09-01 17:58:25 -0700

    [chromedriver] Set |key| when calling Input.dispatchKeyEvent.
    BUG=chromedriver:1484,chromedriver:1411
    
    Review-Url: https://codereview.chromium.org/2302803004
    Cr-Commit-Position: refs/heads/master@{#416149}

--- chrome/test/chromedriver/chrome/web_view_impl.cc
+++ chrome/test/chromedriver/chrome/web_view_impl.cc
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/chromedriver/chrome/status.h"
 #include "chrome/test/chromedriver/chrome/ui_events.h"
 #include "chrome/test/chromedriver/net/timeout.h"
+#include "ui/events/keycodes/dom/keycode_converter.h"
+#include "ui/events/keycodes/keyboard_code_conversion.h"
 
 namespace {
 
@@ -400,6 +402,10 @@ Status WebViewImpl::DispatchKeyEvents(const std::list<KeyEvent>& events) {
     params.SetString("unmodifiedText", it->unmodified_text);
     params.SetInteger("nativeVirtualKeyCode", it->key_code);
     params.SetInteger("windowsVirtualKeyCode", it->key_code);
+    ui::DomCode dom_code = ui::UsLayoutKeyboardCodeToDomCode(it->key_code);
+    std::string key = ui::KeycodeConverter::DomCodeToCodeString(dom_code);
+    if (!key.empty())
+      params.SetString("key", key);
     Status status = client_->SendCommand("Input.dispatchKeyEvent", params);
     if (status.IsError())
       return status;
