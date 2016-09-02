92ac6d4b9770e019dc8ba086c82e4fea5bb78c62
jbroman <jbroman@chromium.org>
2016-09-02 08:08:35 -0700

    Remove style checks which require the use of Pass*Ptr in return and parameter types.
    BUG=494719
    
    Review-Url: https://codereview.chromium.org/2307613002
    Cr-Commit-Position: refs/heads/master@{#416265}

--- third_party/WebKit/Tools/Scripts/webkitpy/style/checkers/cpp_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/style/checkers/cpp_unittest.py
@@ -3634,26 +3634,6 @@ class PassPtrTest(CppStyleTestBase):
         self.assert_pass_ptr_check(
             'OwnRefPtr<Type1> myFunction();\n',
             '')
-        self.assert_pass_ptr_check(
-            'RefPtr<Type1> myFunction(int)\n'
-            '{\n'
-            '}',
-            'The return type should use PassRefPtr instead of RefPtr.  [readability/pass_ptr] [5]')
-        self.assert_pass_ptr_check(
-            'OwnPtr<Type1> myFunction(int)\n'
-            '{\n'
-            '}',
-            'The return type should use PassOwnPtr instead of OwnPtr.  [readability/pass_ptr] [5]')
-        self.assert_pass_ptr_check(
-            'RefPtrWillBeRawPtr<Type1> myFunction(int)\n'
-            '{\n'
-            '}',
-            'The return type should use PassRefPtrWillBeRawPtr instead of RefPtrWillBeRawPtr.  [readability/pass_ptr] [5]')
-        self.assert_pass_ptr_check(
-            'OwnPtrWillBeRawPtr<Type1> myFunction(int)\n'
-            '{\n'
-            '}',
-            'The return type should use PassOwnPtrWillBeRawPtr instead of OwnPtrWillBeRawPtr.  [readability/pass_ptr] [5]')
 
     def test_ref_ptr_parameter_value(self):
         self.assert_pass_ptr_check(
@@ -3661,11 +3641,6 @@ class PassPtrTest(CppStyleTestBase):
             '{\n'
             '}',
             '')
-        self.assert_pass_ptr_check(
-            'int myFunction(RefPtr<Type1>)\n'
-            '{\n'
-            '}',
-            'The parameter type should use PassRefPtr instead of RefPtr.  [readability/pass_ptr] [5]')
         self.assert_pass_ptr_check(
             'int myFunction(RefPtr<Type1>&)\n'
             '{\n'
@@ -3703,11 +3678,6 @@ class PassPtrTest(CppStyleTestBase):
             '{\n'
             '}',
             '')
-        self.assert_pass_ptr_check(
-            'int myFunction(OwnPtr<Type1>)\n'
-            '{\n'
-            '}',
-            'The parameter type should use PassOwnPtr instead of OwnPtr.  [readability/pass_ptr] [5]')
         self.assert_pass_ptr_check(
             'int myFunction(OwnPtr<Type1>& simple)\n'
             '{\n'
