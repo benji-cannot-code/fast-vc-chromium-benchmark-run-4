239ab669291c26a0e6fdc2c9d450b83e95795f2b
sandersd <sandersd@chromium.org>
2016-09-02 20:04:14 -0700

    PipelineController: Fix definition of IsSuspended().
    BUG=643383
    
    Review-Url: https://codereview.chromium.org/2308893002
    Cr-Commit-Position: refs/heads/master@{#416432}

--- media/filters/pipeline_controller_unittest.cc
+++ media/filters/pipeline_controller_unittest.cc
@@ -81,6 +81,7 @@ class PipelineControllerTest : public ::testing::Test, public Pipeline::Client {
     EXPECT_CALL(pipeline_, Suspend(_)).WillOnce(SaveArg<0>(&suspend_cb));
     pipeline_controller_.Suspend();
     Mock::VerifyAndClear(&pipeline_);
+    EXPECT_TRUE(pipeline_controller_.IsSuspended());
     EXPECT_FALSE(pipeline_controller_.IsStable());
     EXPECT_FALSE(pipeline_controller_.IsPipelineSuspended());
     return suspend_cb;
@@ -96,6 +97,7 @@ class PipelineControllerTest : public ::testing::Test, public Pipeline::Client {
         .WillRepeatedly(Return(base::TimeDelta()));
     pipeline_controller_.Resume();
     Mock::VerifyAndClear(&pipeline_);
+    EXPECT_FALSE(pipeline_controller_.IsSuspended());
     EXPECT_FALSE(pipeline_controller_.IsStable());
     EXPECT_FALSE(pipeline_controller_.IsPipelineSuspended());
     return resume_cb;
