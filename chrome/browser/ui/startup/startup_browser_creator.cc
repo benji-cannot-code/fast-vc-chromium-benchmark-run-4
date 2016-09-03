d46aa70f74a31aa60958fb9f428b9abf7e4cabc5
thomasanderson <thomasanderson@google.com>
2016-09-02 17:31:36 -0700

    Linux: Remove --wm-user-time-ms command line argument
    CL ca3656d added the --wm-user-time-ms command line argument so that
    window activation would be more reliable.  However, its implementation
    was awkward and complex, and is no longer needed now that
    X11EventSource::GetTimestamp() provides the correct functionality.
    
    Explanation: X11EventSource::GetTimestamp() gives the timestamp of the
    event currently being dispatched.  If there's no event being
    dispatched, or the event does not have a timestamp, we fall back on
    making a round trip.  Simply using last_seen_server_time like we did
    with activation before ca3656d meant that the timestamp we used could
    be out of date if we were activating in response to anything other
    than a user input event.  This is what led to the --wm-user-time-ms
    hack.
    
    Review-Url: https://codereview.chromium.org/2287583003
    Cr-Commit-Position: refs/heads/master@{#416411}

--- chrome/browser/ui/startup/startup_browser_creator.cc
+++ chrome/browser/ui/startup/startup_browser_creator.cc
@@ -103,10 +103,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/jumplist_metrics_win.h"
 #endif
 
-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
-#include "ui/events/platform/x11/x11_event_source.h"
-#endif
-
 #if defined(ENABLE_PRINT_PREVIEW)
 #include "chrome/browser/printing/cloud_print/cloud_print_proxy_service.h"
 #include "chrome/browser/printing/cloud_print/cloud_print_proxy_service_factory.h"
@@ -703,22 +699,6 @@ bool StartupBrowserCreator::ProcessCmdLineImpl(
         command_line.GetSwitchValueASCII(switches::kWinJumplistAction));
   }
 #endif  // defined(OS_WIN)
-
-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
-  // Our request to Activate may be discarded on some linux window
-  // managers unless given a recent timestamp, so update the timestamp if
-  // we were given one.
-  if (command_line.HasSwitch(switches::kWmUserTimeMs)) {
-    uint64_t time;
-    std::string switch_value =
-        command_line.GetSwitchValueASCII(switches::kWmUserTimeMs);
-    if (base::StringToUint64(switch_value, &time) &&
-        ui::X11EventSource::HasInstance()) {
-      ui::X11EventSource::GetInstance()->SetLastSeenServerTime(
-          static_cast<Time>(time));
-    }
-  }
-#endif
 
   chrome::startup::IsProcessStartup is_process_startup = process_startup ?
       chrome::startup::IS_PROCESS_STARTUP :
