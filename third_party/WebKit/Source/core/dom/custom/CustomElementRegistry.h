afee913b2abb64fa8f506f4a5bd0026a58c2ca19
dominicc <dominicc@chromium.org>
2016-09-03 18:00:58 -0700

    Prevent recursion in critical part of CustomElementRegistry::define.
    Custom element registry used to allow recursion, but this complicated
    the check for whether a name was in use. Now define [1] has an simpler
    explicit recursion guard called the "element definition is running
    flag." [2]
    
    [1] https://html.spec.whatwg.org/#dom-customelementregistry-define
    [2] https://html.spec.whatwg.org/#element-definition-is-running
    
    BUG=643051
    
    Review-Url: https://codereview.chromium.org/2306923002
    Cr-Commit-Position: refs/heads/master@{#416460}

--- third_party/WebKit/Source/core/dom/custom/CustomElementRegistry.h
+++ third_party/WebKit/Source/core/dom/custom/CustomElementRegistry.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/ScriptWrappable.h"
 #include "core/CoreExport.h"
 #include "platform/heap/Handle.h"
-#include "wtf/HashSet.h"
 #include "wtf/Noncopyable.h"
 #include "wtf/text/AtomicString.h"
 #include "wtf/text/AtomicStringHash.h"
@@ -84,9 +83,9 @@ private:
         const CustomElementDescriptor&,
         HeapVector<Member<Element>>*);
 
-    class NameIsBeingDefined;
+    class ElementDefinitionIsRunning;
+    bool m_elementDefinitionIsRunning;
 
-    HashSet<AtomicString> m_namesBeingDefined;
     using DefinitionMap =
         HeapHashMap<AtomicString, Member<CustomElementDefinition>>;
     DefinitionMap m_definitions;
