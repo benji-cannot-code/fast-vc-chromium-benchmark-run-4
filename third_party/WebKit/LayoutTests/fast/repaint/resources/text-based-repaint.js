f7829642eb1a45537e03c1ac5d5902a25794a293
mgiuca <mgiuca@chromium.org>
2016-09-05 01:31:07 -0700

    Revert of Reland Compile under-invalidation checking in all builds (patchset #4 id:60001 of https://codereview.chromium.org/2301303002/ )
    Reason for revert:
    Causing failures on WebKit Mac 10.9:
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.9
    
    updating-scrolling-container-and-content.html is hitting the new CHECK on
    PaintController.cpp:662. It's not clear why this passed on build #36527; all
    other builds since this landed failed.
    
    Original issue's description:
    > Reland Compile under-invalidation checking in all builds
    >
    > This relands https://codereview.chromium.org/2299223002 which was
    > reverted because of random pixel under-invalidations.
    >
    > Now clear the bitmaps before drawing the pictures.
    >
    > BTW changed the error indication color from solid red to translucent
    > magenta to distinguish from the normal red pixels in test results.
    >
    > > This will let the tests having under-invalidation issues produce the
    > > same result on all builds, so enable us to rebaseline the tests with
    > > failure results. In this way we can track either regressions and
    > > progressions instead of ignoring the tests.
    > >
    > > BUG=619103
    > > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    > >
    > > Review-Url: https://codereview.chromium.org/2299223002
    > > Cr-Commit-Position: refs/heads/master@{#416060}
    >
    > BUG=619103
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    > R=pdr@chromium.org
    
    TBR=pdr@chromium.org,wangxianzhu@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=619103
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2309193002
    Cr-Commit-Position: refs/heads/master@{#416516}

--- third_party/WebKit/LayoutTests/fast/repaint/resources/text-based-repaint.js
+++ third_party/WebKit/LayoutTests/fast/repaint/resources/text-based-repaint.js
@@ -10,7 +10,7 @@ if (window.testRunner)
 
 if (window.internals) {
     internals.settings.setUseDefaultImageInterpolationQuality(true);
-    internals.runtimeFlags.paintUnderInvalidationCheckingEnabled = true;
+    internals.runtimeFlags.slimmingPaintUnderInvalidationCheckingEnabled = true;
 }
 
 function runRepaintTest()
--- third_party/WebKit/Source/platform/graphics/paint/DrawingDisplayItem.cpp
+++ third_party/WebKit/Source/platform/graphics/paint/DrawingDisplayItem.cpp
@@ -7,11 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "platform/graphics/GraphicsContext.h"
 #include "public/platform/WebDisplayItemList.h"
+#include "third_party/skia/include/core/SkPictureAnalyzer.h"
+
+#if ENABLE(ASSERT)
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkData.h"
-#include "third_party/skia/include/core/SkPictureAnalyzer.h"
 #include "third_party/skia/include/core/SkStream.h"
+#endif
 
 namespace blink {
 
@@ -49,6 +52,7 @@ void DrawingDisplayItem::dumpPropertiesAsDebugString(StringBuilder& stringBuilde
 }
 #endif
 
+#if ENABLE(ASSERT)
 static bool picturesEqual(const SkPicture* picture1, const SkPicture* picture2)
 {
     if (picture1->approximateOpCount() != picture2->approximateOpCount())
@@ -124,5 +128,6 @@ bool DrawingDisplayItem::equals(const DisplayItem& other) const
     // which should be treated as equal.
     return bitmapsEqual(picture, otherPicture);
 }
+#endif // ENABLE(ASSERT)
 
 } // namespace blink
