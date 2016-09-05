492c9507059eba033a824b2c37d04bc79c646fa4
haraken <haraken@chromium.org>
2016-09-04 21:19:50 -0700

    Revert of Stop calling blink::shutdown (patchset #9 id:160001 of https://codereview.chromium.org/2249353002/ )
    Reason for revert:
    I'll revert r413430 and its dependent CLs because r413430 caused issue 642072.
    
    The problem is that we cannot simply remove blink::shutdown because the following scenario can happen:
    
    1) blink::shutdown is not called. Workers are still running.
    2) RenderThreadImpl gets destructed. MessageLoop gets destructed.
    3) The workers may access the RenderThreadImpl and MessageLoop.
    
    To fix the problem, we need to call ProcessDied() and forcibly kill the renderer process at the end of RenderThreadImpl::Shutdown().
    
    Original issue's description:
    > Stop calling blink::shutdown
    >
    > RenderThreadImpl::Shutdown has been trying to shut down Blink and V8 gracefully,
    > but the graceful shutdown has caused tons of use-after-free bugs
    > (and many engineers has spent lots of time fixing ordering issues around the shutdown).
    >
    > As discussed in blink-dev@ (https://groups.google.com/a/chromium.org/d/topic/blink-dev/kk4VX0xRB7I/discussion)
    > and platform-architecture-dev@ (https://groups.google.com/a/chromium.org/d/topic/platform-architecture-dev/Zc12k91NTFk/discussion),
    > there is no reason we have to shut down the renderer gracefully.
    > It's just causing use-after-free bugs and wasting performance.
    > Hence, this CL stops calling blink::shutdown, which had been
    > shutting down *some things* in Blink and V8 gracefully.
    > (Remember that blink::shutdown hadn't been shutting down everything;
    > a lot of objects in Blink and V8 had already been left as is without getting destructed.)
    >
    > Ideally we should just call ProcessDied() at an earlier stage of
    > RenderThreadImpl::Shutdown(), but I'd like to defer the change to a separate CL.
    >
    > BUG=639244
    >
    > Committed: https://crrev.com/01cb51d26b17923ed2b5c3b59566f0fc9aed74ae
    > Cr-Commit-Position: refs/heads/master@{#413430}
    
    TBR=jochen@chromium.org,esprehn@chromium.org,torne@chromium.org,tzik@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=639244
    
    Review-Url: https://codereview.chromium.org/2312593002
    Cr-Commit-Position: refs/heads/master@{#416494}

--- third_party/WebKit/Source/wtf/ThreadSpecific.h
+++ third_party/WebKit/Source/wtf/ThreadSpecific.h
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WTF_ThreadSpecific_h
 #define WTF_ThreadSpecific_h
 
-#include "wtf/AddressSanitizer.h"
 #include "wtf/Allocator.h"
 #include "wtf/Noncopyable.h"
 #include "wtf/StdLibExtras.h"
@@ -157,7 +156,6 @@ inline void ThreadSpecific<T>::set(T* ptr)
 {
     ASSERT(!get());
     pthread_setspecific(m_key, new Data(ptr, this));
-    __lsan_register_root_region(ptr, sizeof(Data));
 }
 
 #elif OS(WIN)
@@ -216,7 +214,6 @@ inline void ThreadSpecific<T>::set(T* ptr)
 {
     ASSERT(!get());
     Data* data = new Data(ptr, this);
-    __lsan_register_root_region(ptr, sizeof(Data));
     data->destructor = &ThreadSpecific<T>::destroy;
     TlsSetValue(tlsKeys()[m_index], data);
 }
@@ -228,9 +225,7 @@ inline void ThreadSpecific<T>::set(T* ptr)
 template<typename T>
 inline void ThreadSpecific<T>::destroy(void* ptr)
 {
-    // Never call destructors on the main thread.
-    // This is fine because Blink no longer has a graceful shutdown sequence.
-    if (isMainThread())
+    if (isShutdown())
         return;
 
     Data* data = static_cast<Data*>(ptr);
@@ -271,7 +266,6 @@ inline ThreadSpecific<T>::operator T*()
         ptr = static_cast<T*>(Partitions::fastZeroedMalloc(sizeof(T), WTF_HEAP_PROFILER_TYPE_NAME(T)));
         set(ptr);
         new (NotNull, ptr) T;
-        __lsan_register_root_region(ptr, sizeof(T));
     }
     return ptr;
 }
