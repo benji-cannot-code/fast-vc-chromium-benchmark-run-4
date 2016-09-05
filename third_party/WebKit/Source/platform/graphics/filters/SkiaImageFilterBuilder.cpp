c7d5b02202ec72d4c47f9c567992f4b0fa6a1f29
fs <fs@opera.com>
2016-09-05 05:49:38 -0700

    Reland of Revamp filter primitive region calculations for Filter Effects
    This moves the filter primitive region calculation to
    SVGFilterPrimitiveStandardAttributes::setStandardAttributes, folding in
    FilterEffect::applyEffectBoundaries and getting rid of
    FilterEffect::m_effectBoundaries and related flags.
    What's left of FilterEffect::determineFilterPrimitiveSubregion() is
    renamed to determineMaximumEffectRect(), and callsites updated.
    
    BUG=642035
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2303703002
    Cr-Commit-Position: refs/heads/master@{#416537}

--- third_party/WebKit/Source/platform/graphics/filters/SkiaImageFilterBuilder.cpp
+++ third_party/WebKit/Source/platform/graphics/filters/SkiaImageFilterBuilder.cpp
@@ -115,7 +115,7 @@ void buildFilterOperations(const FilterOperations& operations, CompositorFilterO
 
                 FilterEffect* filterEffect = referenceFilter->lastEffect();
                 currentColorSpace = filterEffect->operatingColorSpace();
-                filterEffect->determineFilterPrimitiveSubregion(MapRectForward);
+                filterEffect->determineMaximumEffectRect(MapRectForward);
                 filters->appendReferenceFilter(SkiaImageFilterBuilder::build(filterEffect, currentColorSpace));
             }
             break;
