f297d8b27226ae4e077750d580558152e27d6851
pdr <pdr@chromium.org>
2016-09-04 19:57:39 -0700

    Remove non-layer-list codepath from PaintArtifactCompositor
    Slimming paint v2 only uses layer lists now and the old non-layer-list
    codepath can be removed.
    
    BUG=630691
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2307143002
    Cr-Commit-Position: refs/heads/master@{#416489}

--- third_party/WebKit/Source/platform/testing/WebLayerTreeViewImplForTesting.h
+++ third_party/WebKit/Source/platform/testing/WebLayerTreeViewImplForTesting.h
@@ -29,8 +29,6 @@ class WebLayerTreeViewImplForTesting : public blink::WebLayerTreeView,
     WTF_MAKE_NONCOPYABLE(WebLayerTreeViewImplForTesting);
 public:
     WebLayerTreeViewImplForTesting();
-    enum LayerListPolicy { DontUseLayerLists, UseLayerLists };
-    explicit WebLayerTreeViewImplForTesting(LayerListPolicy);
     explicit WebLayerTreeViewImplForTesting(const cc::LayerTreeSettings&);
     ~WebLayerTreeViewImplForTesting() override;
 
