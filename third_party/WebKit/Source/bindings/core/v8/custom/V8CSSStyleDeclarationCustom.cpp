b0eefe2555b083c321aa98cf6e8dd11ce4af495d
timloh <timloh@chromium.org>
2016-09-04 23:06:17 -0700

    Fix assertion when trying to set custom properties as named properties
    This patch fixes a bug where trying to access a custom property as a
    named property causes an assertion (e.g. e.style['--foo'] = 'a').
    None of the named-property handling code on CSSStyleDeclaration should
    support custom properties (we should actually use attributes instead of
    named properties, crbug.com/628785, but that's another issue).
    
    The added test already passes in Firefox.
    
    BUG=643194
    
    Review-Url: https://codereview.chromium.org/2308373002
    Cr-Commit-Position: refs/heads/master@{#416502}

--- third_party/WebKit/Source/bindings/core/v8/custom/V8CSSStyleDeclarationCustom.cpp
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8CSSStyleDeclarationCustom.cpp
@@ -142,6 +142,8 @@ static CSSPropertyID cssPropertyInfo(v8::Local<v8::String> v8PropertyName)
         return iter->value;
 
     CSSPropertyID unresolvedProperty = parseCSSPropertyID(propertyName);
+    if (unresolvedProperty == CSSPropertyVariable)
+        unresolvedProperty = CSSPropertyInvalid;
     map.add(propertyName, unresolvedProperty);
     ASSERT(!unresolvedProperty || CSSPropertyMetadata::isEnabledProperty(unresolvedProperty));
     return unresolvedProperty;
@@ -196,7 +198,6 @@ void V8CSSStyleDeclaration::namedPropertyGetterCustom(v8::Local<v8::Name> name, 
     CSSPropertyID resolvedProperty = resolveCSSPropertyID(unresolvedProperty);
 
     CSSStyleDeclaration* impl = V8CSSStyleDeclaration::toImpl(info.Holder());
-    // TODO(leviw): This API doesn't support custom properties.
     const CSSValue* cssValue = impl->getPropertyCSSValueInternal(resolvedProperty);
     if (cssValue) {
         v8SetReturnValueStringOrNull(info, cssValue->cssText(), info.GetIsolate());
@@ -218,7 +219,6 @@ void V8CSSStyleDeclaration::namedPropertySetterCustom(v8::Local<v8::Name> name, 
 
     TOSTRING_VOID(V8StringResource<TreatNullAsNullString>, propertyValue, value);
     ExceptionState exceptionState(ExceptionState::SetterContext, getPropertyName(resolveCSSPropertyID(unresolvedProperty)), "CSSStyleDeclaration", info.Holder(), info.GetIsolate());
-    // TODO(leviw): This API doesn't support custom properties.
     impl->setPropertyInternal(unresolvedProperty, String(), propertyValue, false, exceptionState);
 
     v8SetReturnValue(info, value);
