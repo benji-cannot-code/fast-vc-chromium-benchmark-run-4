d0d98d8e45fca13985c0d537dbc3cdb14f68029b
estade <estade@chromium.org>
2016-09-06 08:47:20 -0700

    Update Harmony textfield and combobox default border color.
    BUG=635160,635164
    
    Review-Url: https://codereview.chromium.org/2302173002
    Cr-Commit-Position: refs/heads/master@{#416629}

--- ui/native_theme/common_theme.cc
+++ ui/native_theme/common_theme.cc
@@ -22,6 +22,16 @@ namespace ui {
 
 SkColor GetAuraColor(NativeTheme::ColorId color_id,
                      const NativeTheme* base_theme) {
+  // Second wave of MD colors (colors that only appear in secondary UI).
+  if (ui::MaterialDesignController::IsSecondaryUiMaterial()) {
+    static const SkColor kPrimaryTextColor = SkColorSetRGB(0x33, 0x33, 0x33);
+
+    if (color_id == NativeTheme::kColorId_LabelEnabledColor)
+      return kPrimaryTextColor;
+    if (color_id == NativeTheme::kColorId_UnfocusedBorderColor)
+      return SkColorSetA(SK_ColorBLACK, 0x24);
+  }
+
   // MD colors.
   if (ui::MaterialDesignController::IsModeMaterial()) {
     // Dialogs:
@@ -69,6 +79,8 @@ SkColor GetAuraColor(NativeTheme::ColorId color_id,
       // FocusableBorder
       case NativeTheme::kColorId_FocusedBorderColor:
         return gfx::kGoogleBlue500;
+      // TODO(estade): I'm not sure why this one is here but it should be
+      // removed in favor of the value in the SecondaryUiMaterial block.
       case NativeTheme::kColorId_UnfocusedBorderColor:
         return SkColorSetA(SK_ColorBLACK, 0x66);
 
@@ -103,14 +115,6 @@ SkColor GetAuraColor(NativeTheme::ColorId color_id,
       default:
         break;
     }
-  }
-
-  // Second wave of MD colors (colors that only appear in secondary UI).
-  if (ui::MaterialDesignController::IsSecondaryUiMaterial()) {
-    static const SkColor kPrimaryTextColor = SkColorSetRGB(0x33, 0x33, 0x33);
-
-    if (color_id == NativeTheme::kColorId_LabelEnabledColor)
-      return kPrimaryTextColor;
   }
 
   // Pre-MD colors.
