ad800fee01034347126ad4aaeb85afd20eeb9db5
petewil <petewil@chromium.org>
2016-09-06 11:50:36 -0700

    Rename an Enum to a slightly better name.
    Resolves an outstanding TODO to choose a better name for this enum.
    
    BUG=610521
    
    Review-Url: https://codereview.chromium.org/2303953002
    Cr-Commit-Position: refs/heads/master@{#416684}

--- components/offline_pages/background/request_notifier.h
+++ components/offline_pages/background/request_notifier.h
@@ -13,8 +13,7 @@ class SavePageRequest;
 class RequestNotifier {
  public:
   // Status to return for failed notifications.
-  // TODO(petewil): Can we find a better name for this enum?
-  enum class SavePageStatus {
+  enum class BackgroundSavePageResult {
     SUCCESS,
     PRERENDER_FAILURE,
     PRERENDER_CANCELED,
@@ -33,7 +32,7 @@ class RequestNotifier {
 
   // Notifies observers that |request| has been completed with |status|.
   virtual void NotifyCompleted(const SavePageRequest& request,
-                               SavePageStatus status) = 0;
+                               BackgroundSavePageResult status) = 0;
 
   // Notifies observers that |request| has been changed.
   virtual void NotifyChanged(const SavePageRequest& request) = 0;
