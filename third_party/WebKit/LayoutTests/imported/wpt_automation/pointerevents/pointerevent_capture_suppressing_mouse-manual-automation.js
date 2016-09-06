ff15ae6666e8d9585dd885aa800e6e0c83c4ad1b
nzolghadr <nzolghadr@chromium.org>
2016-09-06 10:58:36 -0700

    Use querySelector instead of getElementById
    Using querySelector to get the target to do
    actions on makes the need for ids go away.
    
    BUG=633672
    
    Review-Url: https://codereview.chromium.org/2306103003
    Cr-Commit-Position: refs/heads/master@{#416664}

--- third_party/WebKit/LayoutTests/imported/wpt_automation/pointerevents/pointerevent_capture_suppressing_mouse-manual-automation.js
+++ third_party/WebKit/LayoutTests/imported/wpt_automation/pointerevents/pointerevent_capture_suppressing_mouse-manual-automation.js
@@ -2,13 +2,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importAutomationScript('/pointerevents/pointerevent_common_input.js');
 
 function inject_input() {
-  return mouseMoveIntoTarget('target0').then(function() {
-    return mouseMoveIntoTarget('target1');
+  return mouseMoveIntoTarget('#target0').then(function() {
+    return mouseMoveIntoTarget('#target1');
   }).then(function() {
-    return mouseDragInTargets(['btnCapture', 'target1', 'target0', 'target1']);
+    return mouseDragInTargets(['#btnCapture', '#target1', '#target0', '#target1']);
   }).then(function() {
     // To handle delayed capturing.
-    mouseMoveIntoTarget('target0');
+    mouseMoveIntoTarget('#target0');
   });
 
 }
--- third_party/WebKit/LayoutTests/imported/wpt_automation/pointerevents/pointerevent_touch-action-inherit_parent-none_touch-manual-automation.js
+++ third_party/WebKit/LayoutTests/imported/wpt_automation/pointerevents/pointerevent_touch-action-inherit_parent-none_touch-manual-automation.js
@@ -2,8 +2,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importAutomationScript('/pointerevents/pointerevent_common_input.js');
 
 function inject_input() {
-  touchScrollUpInTarget('target0');
-  touchScrollLeftInTarget('target0');
-  touchTapInTarget('btnComplete');
+  return touchScrollInTarget('#target0 > div > p', 'down').then(function() {
+    return touchScrollInTarget('#target0 > div > p', 'right');
+  }).then(function() {
+    return touchTapInTarget('#btnComplete');
+  });
 }
 
