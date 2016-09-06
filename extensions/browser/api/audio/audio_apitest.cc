42fc2420d5e779af1478eee37ec6d61840dd781c
fdoray <fdoray@chromium.org>
2016-09-06 16:12:59 -0700

    Remove calls to deprecated MessageLoop methods in extensions.
    This CL (hopefully) removes all calls to these methods in extensions:
    - MessageLoop::PostTask
    - MessageLoop::PostDelayedTask
    - MessageLoop::DeleteSoon
    - MessageLoop::ReleaseSoon
    - MessageLoop::Run
    - MessageLoop::RunUntilIdle
    
    BUG=616447
    R=reillyg@chromium.org
    
    Review-Url: https://codereview.chromium.org/2314983002
    Cr-Commit-Position: refs/heads/master@{#416758}

--- extensions/browser/api/audio/audio_apitest.cc
+++ extensions/browser/api/audio/audio_apitest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
 #include "build/build_config.h"
 #include "extensions/shell/test/shell_apitest.h"
 #if defined(OS_CHROMEOS)
@@ -107,14 +108,14 @@ class AudioApiTest : public ShellApiTest {
         audio_nodes);
     cras_audio_handler_ = chromeos::CrasAudioHandler::Get();
     DCHECK(cras_audio_handler_);
-    message_loop_.RunUntilIdle();
+    base::RunLoop().RunUntilIdle();
   }
 
   void ChangeAudioNodes(const AudioNodeList& audio_nodes) {
     DCHECK(fake_cras_audio_client_);
     fake_cras_audio_client_->SetAudioNodesAndNotifyObserversForTesting(
         audio_nodes);
-    message_loop_.RunUntilIdle();
+    base::RunLoop().RunUntilIdle();
   }
 
  protected:
