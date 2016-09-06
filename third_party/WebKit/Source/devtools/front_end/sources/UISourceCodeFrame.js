c0c05ef0423a2d8d689227a0ab8f9167de4a6743
pfeldman <pfeldman@chromium.org>
2016-09-06 16:07:22 -0700

    Revert of DevTools: remove UISourceCodeFrame from context when switching panels. (patchset #2 id:20001 of https://codereview.chromium.org/2287713002/ )
    Reason for revert:
    I'll have a better one for merge.
    
    Original issue's description:
    > DevTools: remove UISourceCodeFrame from context when switching panels.
    >
    > BUG=640375
    >
    > Committed: https://crrev.com/0cfac8a68fbabad4515c0a9f58c9e1dffd8c4a74
    > Cr-Commit-Position: refs/heads/master@{#415105}
    
    TBR=alph@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=640375
    
    Review-Url: https://codereview.chromium.org/2311303002
    Cr-Commit-Position: refs/heads/master@{#416753}

--- third_party/WebKit/Source/devtools/front_end/sources/UISourceCodeFrame.js
+++ third_party/WebKit/Source/devtools/front_end/sources/UISourceCodeFrame.js
@@ -82,7 +82,6 @@ WebInspector.UISourceCodeFrame.prototype = {
     wasShown: function()
     {
         WebInspector.SourceFrame.prototype.wasShown.call(this);
-        WebInspector.context.setFlavor(WebInspector.UISourceCodeFrame, this);
         this._boundWindowFocused = this._windowFocused.bind(this);
         this.element.ownerDocument.defaultView.addEventListener("focus", this._boundWindowFocused, false);
         this._checkContentUpdated();
@@ -93,7 +92,6 @@ WebInspector.UISourceCodeFrame.prototype = {
     willHide: function()
     {
         WebInspector.SourceFrame.prototype.willHide.call(this);
-        WebInspector.context.setFlavor(WebInspector.UISourceCodeFrame, null);
         this.element.ownerDocument.defaultView.removeEventListener("focus", this._boundWindowFocused, false);
         delete this._boundWindowFocused;
         this._uiSourceCode.removeWorkingCopyGetter();
