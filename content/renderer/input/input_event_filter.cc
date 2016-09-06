2eb027319deef6f6b7438aad4130accbf0931245
majidvp <majidvp@chromium.org>
2016-09-06 15:03:59 -0700

    Use IO thread time for event timestamp if TimeTicks isn't consistent x-process.
    On some platforms (a subset of Windows) we don't have a monotonic clock that
    is consistent across processes. On such platforms we cannot reliably compare
    timeticks that are from different processes.
    
    This is particularly problematic for WebInputEvent whose timestamp comes
    from UI process but is consumed by the renderer process and eventually exposed
    to the web as high-resolution timestamp.
    
    This patch introduces a fallback where we use the message received time on the
    renderer IO thread (i.e., the earliest time we get the message on the renderer)
    as the event timestamp and sidestep the inconsistency.
    
    BUG=642714
    
    Review-Url: https://codereview.chromium.org/2289173003
    Cr-Commit-Position: refs/heads/master@{#416737}

--- content/renderer/input/input_event_filter.cc
+++ content/renderer/input/input_event_filter.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/render_thread_impl.h"
 #include "ipc/ipc_listener.h"
 #include "ipc/ipc_sender.h"
+#include "ui/events/base_event_utils.h"
 #include "ui/events/blink/did_overscroll_params.h"
 #include "ui/events/blink/web_input_event_traits.h"
 #include "ui/gfx/geometry/vector2d_f.h"
@@ -170,6 +171,13 @@ bool InputEventFilter::OnMessageReceived(const IPC::Message& message) {
   if (!RequiresThreadBounce(message))
     return false;
 
+  // If TimeTicks is not consistent across processes we cannot use the event's
+  // platform timestamp in this process. Instead the time that the event is
+  // received on the IO thread is used as the event's timestamp.
+  base::TimeTicks received_time;
+  if (!base::TimeTicks::IsConsistentAcrossProcesses())
+    received_time = base::TimeTicks::Now();
+
   TRACE_EVENT0("input", "InputEventFilter::OnMessageReceived::InputMessage");
 
   {
@@ -179,8 +187,8 @@ bool InputEventFilter::OnMessageReceived(const IPC::Message& message) {
   }
 
   target_task_runner_->PostTask(
-      FROM_HERE,
-      base::Bind(&InputEventFilter::ForwardToHandler, this, message));
+      FROM_HERE, base::Bind(&InputEventFilter::ForwardToHandler, this, message,
+                            received_time));
   return true;
 }
 
@@ -188,7 +196,8 @@ InputEventFilter::~InputEventFilter() {
   DCHECK(!current_overscroll_params_);
 }
 
-void InputEventFilter::ForwardToHandler(const IPC::Message& message) {
+void InputEventFilter::ForwardToHandler(const IPC::Message& message,
+                                        base::TimeTicks received_time) {
   DCHECK(input_handler_manager_);
   DCHECK(target_task_runner_->BelongsToCurrentThread());
   TRACE_EVENT1("input", "InputEventFilter::ForwardToHandler",
@@ -214,6 +223,9 @@ void InputEventFilter::ForwardToHandler(const IPC::Message& message) {
   DCHECK(event);
   DCHECK(dispatch_type == DISPATCH_TYPE_BLOCKING ||
          dispatch_type == DISPATCH_TYPE_NON_BLOCKING);
+
+  if (!received_time.is_null())
+    event->timeStampSeconds = ui::EventTimeStampToSeconds(received_time);
 
   bool send_ack = dispatch_type == DISPATCH_TYPE_BLOCKING;
 
