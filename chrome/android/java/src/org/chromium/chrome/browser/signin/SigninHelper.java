c9cf856fe630fad02fa4e617bedd731c6b4a6327
gogerald <gogerald@chromium.org>
2016-09-06 13:38:36 -0700

    Verify accounts in AccountTrackerService up-to-date with the accounts in Android.
    Accounts in Android may changed when Chrome resume, but Chrome has not received account changed broadcast yet because of latency.
    
    BUG=640708
    
    Review-Url: https://codereview.chromium.org/2289133004
    Cr-Commit-Position: refs/heads/master@{#416711}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninHelper.java
@@ -135,6 +135,9 @@ public class SigninHelper {
     public void validateAccountSettings(boolean accountsChanged) {
         // Ensure System accounts have been seeded.
         mAccountTrackerService.checkAndSeedSystemAccounts();
+        if (!accountsChanged) {
+            mAccountTrackerService.validateSystemAccounts();
+        }
 
         Account syncAccount = mChromeSigninController.getSignedInUser();
         if (syncAccount == null) {
