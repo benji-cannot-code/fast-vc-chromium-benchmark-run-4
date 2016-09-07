8620c56d027d430790f2aa7880927147a9fefd34
mcasas <mcasas@chromium.org>
2016-09-06 17:13:04 -0700

    Image Capture: wire RedEyeReduction getter
    This CL lands the red-eye-reduction state getter
    in idl/mojom/java and updates LayoutTests
    accordingly.
    
    BUG=518807
    
    Review-Url: https://codereview.chromium.org/2301043006
    Cr-Commit-Position: refs/heads/master@{#416788}

--- media/capture/video/android/java/src/org/chromium/media/PhotoCapabilities.java
+++ media/capture/video/android/java/src/org/chromium/media/PhotoCapabilities.java
@@ -32,12 +32,14 @@ class PhotoCapabilities {
     public final int currentExposureCompensation;
     public final int whiteBalanceMode;
     public final int fillLightMode;
+    public final boolean redEyeReduction;
 
     PhotoCapabilities(int maxIso, int minIso, int currentIso, int maxHeight, int minHeight,
             int currentHeight, int maxWidth, int minWidth, int currentWidth, int maxZoom,
             int minZoom, int currentZoom, int focusMode, int exposureMode,
             int maxExposureCompensation, int minExposureCompensation,
-            int currentExposureCompensation, int whiteBalanceMode, int fillLightMode) {
+            int currentExposureCompensation, int whiteBalanceMode, int fillLightMode,
+            boolean redEyeReduction) {
         this.maxIso = maxIso;
         this.minIso = minIso;
         this.currentIso = currentIso;
@@ -57,6 +59,7 @@ class PhotoCapabilities {
         this.currentExposureCompensation = currentExposureCompensation;
         this.whiteBalanceMode = whiteBalanceMode;
         this.fillLightMode = fillLightMode;
+        this.redEyeReduction = redEyeReduction;
     }
 
     @CalledByNative
@@ -154,6 +157,11 @@ class PhotoCapabilities {
         return fillLightMode;
     }
 
+    @CalledByNative
+    public boolean getRedEyeReduction() {
+        return redEyeReduction;
+    }
+
     public static class Builder {
         public int maxIso;
         public int minIso;
@@ -174,6 +182,7 @@ class PhotoCapabilities {
         public int currentExposureCompensation;
         public int whiteBalanceMode;
         public int fillLightMode;
+        public boolean redEyeReduction;
 
         public Builder() {}
 
@@ -272,11 +281,16 @@ class PhotoCapabilities {
             return this;
         }
 
+        public Builder setRedEyeReduction(boolean redEyeReduction) {
+            this.redEyeReduction = redEyeReduction;
+            return this;
+        }
+
         public PhotoCapabilities build() {
             return new PhotoCapabilities(maxIso, minIso, currentIso, maxHeight, minHeight,
                     currentHeight, maxWidth, minWidth, currentWidth, maxZoom, minZoom, currentZoom,
                     focusMode, exposureMode, maxExposureCompensation, minExposureCompensation,
-                    currentExposureCompensation, whiteBalanceMode, fillLightMode);
+                    currentExposureCompensation, whiteBalanceMode, fillLightMode, redEyeReduction);
         }
     }
 }
