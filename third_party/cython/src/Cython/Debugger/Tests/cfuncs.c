af22e5bdfcce169cb0c8876481ee223a185d7376
brettw <brettw@chromium.org>
2016-09-06 23:05:22 -0700

    Remove third_party/cython from the source tree.
    This is currently unused. It was originally intended for Mojo Python integration.
    
    NOPRESUBMIT=true
    (because apparently checklicenses runs on all "changed" files, including deleted ones).
    
    Review-Url: https://codereview.chromium.org/2317453002
    Cr-Commit-Position: refs/heads/master@{#416857}

--- third_party/cython/src/Cython/Debugger/Tests/cfuncs.c
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-void
-some_c_function(void)
-{
-    int a, b, c;
-
-    a = 1;
-    b = 2;
-}
--- third_party/cython/src/Cython/Distutils/__init__.py
+++
@@ -1,3 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-from Cython.Distutils.build_ext import build_ext
-from Cython.Distutils.extension import Extension
