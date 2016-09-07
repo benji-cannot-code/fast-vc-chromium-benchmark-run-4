c705990feb4e1bbbe102caacd7984b2ffb9e7489
flandy <flandy@google.com>
2016-09-07 16:04:26 -0700

    DevTools: Allow clicks to register in SSP when swatch popover is open
    Applying the style text to the tree element when the popover is hidden
    causes the section to update, not allowing clicks to register.
    
    It is unnecessary to apply the style text when the popover is hidden
    (unless reverting back to original property text) because the change
    has already been applied.
    
    BUG=644779
    
    Review-Url: https://codereview.chromium.org/2307463004
    Cr-Commit-Position: refs/heads/master@{#417109}

--- third_party/WebKit/Source/devtools/front_end/elements/ColorSwatchPopoverIcon.js
+++ third_party/WebKit/Source/devtools/front_end/elements/ColorSwatchPopoverIcon.js
@@ -80,8 +80,9 @@ WebInspector.BezierPopoverIcon.prototype = {
         this._bezierEditor.removeEventListener(WebInspector.BezierEditor.Events.BezierChanged, this._boundBezierChanged);
         delete this._bezierEditor;
 
-        var propertyText = commitEdit ? this._treeElement.renderedPropertyText() : this._originalPropertyText;
-        this._treeElement.applyStyleText(propertyText, true);
+        // Clicking between swatches in the same section should open each swatch popover. crbug.com/644779
+        if (!commitEdit)
+            this._treeElement.applyStyleText(this._originalPropertyText, true);
         this._treeElement.parentPane().setEditingStyle(false);
         delete this._originalPropertyText;
     }
@@ -207,8 +208,9 @@ WebInspector.ColorSwatchPopoverIcon.prototype = {
         this._spectrum.removeEventListener(WebInspector.Spectrum.Events.ColorChanged, this._boundSpectrumChanged);
         delete this._spectrum;
 
-        var propertyText = commitEdit ? this._treeElement.renderedPropertyText() : this._originalPropertyText;
-        this._treeElement.applyStyleText(propertyText, true);
+        // Clicking between swatches in the same section should open each swatch popover. crbug.com/644779
+        if (!commitEdit)
+            this._treeElement.applyStyleText(this._originalPropertyText, true);
         this._treeElement.parentPane().setEditingStyle(false);
         delete this._originalPropertyText;
     }
@@ -289,8 +291,9 @@ WebInspector.ShadowSwatchPopoverHelper.prototype = {
         this._cssShadowEditor.removeEventListener(WebInspector.CSSShadowEditor.Events.ShadowChanged, this._boundShadowChanged);
         delete this._cssShadowEditor;
 
-        var propertyText = commitEdit ? this._treeElement.renderedPropertyText() : this._originalPropertyText;
-        this._treeElement.applyStyleText(propertyText, true);
+        // Clicking between swatches in the same section should open each swatch popover. crbug.com/644779
+        if (!commitEdit)
+            this._treeElement.applyStyleText(this._originalPropertyText, true);
         this._treeElement.parentPane().setEditingStyle(false);
         delete this._originalPropertyText;
     }
