7d43bd23364a03eaa756faff0f36ccfca6289cb4
pkalinnikov <pkalinnikov@chromium.org>
2016-09-07 06:14:48 -0700

    Use Origin::DomainIs without converting to GURL.
    BUG=609747
    
    Review-Url: https://codereview.chromium.org/2282773002
    Cr-Commit-Position: refs/heads/master@{#416924}

--- components/subresource_filter/core/common/indexed_ruleset.cc
+++ components/subresource_filter/core/common/indexed_ruleset.cc
@@ -329,10 +329,6 @@ bool DoesInitiatorMatchDomainList(
     return true;
   }
 
-  // TODO(pkalinnikov): Switch back to const url::Origin& once |DomainIs|
-  // becomes implemented for url::Origin.
-  GURL initiator_as_url(initiator.Serialize());
-
   size_t max_domain_length = 0;
   bool is_positive = true;
   bool negatives_only = true;
@@ -353,7 +349,7 @@ bool DoesInitiatorMatchDomainList(
       negatives_only = false;
     }
 
-    if (!initiator_as_url.DomainIs(filter_piece))
+    if (!initiator.DomainIs(filter_piece))
       continue;
     max_domain_length = filter_piece.length();
     is_positive = !is_negative;
