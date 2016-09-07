86f083f524b22f2f3a8a38e12305229bee9d6fe3
shaktisahu <shaktisahu@chromium.org>
2016-09-06 23:15:49 -0700

    blimp: Fix for multiple show IME messages
    Some websites trigger multiple Show IME messages for the same textbox
    which generates multiple popups superimposed one over another. This CL
    avoids creating multiple popups.
    
    BUG=611111
    
    Review-Url: https://codereview.chromium.org/2303263003
    Cr-Commit-Position: refs/heads/master@{#416860}

--- blimp/client/core/contents/android/java/src/org/chromium/blimp/core/contents/input/ImeHelperDialog.java
+++ blimp/client/core/contents/android/java/src/org/chromium/blimp/core/contents/input/ImeHelperDialog.java
@@ -69,6 +69,10 @@ public class ImeHelperDialog {
 
     @CalledByNative
     private void onShowImeRequested(int inputType, String text) {
+        if (mAlertDialog != null && mAlertDialog.isShowing()) {
+            mAlertDialog.dismiss();
+        }
+
         createTextInputPopup(inputType, text);
     }
 
