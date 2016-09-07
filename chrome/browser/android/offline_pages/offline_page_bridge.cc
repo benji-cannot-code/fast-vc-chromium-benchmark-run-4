ecd2587f1c1103bd40f5c14278410e447413970c
dougarnett <dougarnett@chromium.org>
2016-09-07 09:51:08 -0700

    [Offline Pages] Removes the deprecated IsOfflinePages synthetic flag since no uses of it remain.
    BUG=639354
    
    Review-Url: https://codereview.chromium.org/2317723003
    Cr-Commit-Position: refs/heads/master@{#416967}

--- chrome/browser/android/offline_pages/offline_page_bridge.cc
+++ chrome/browser/android/offline_pages/offline_page_bridge.cc
@@ -199,11 +199,6 @@ void OnRemoveRequestsDone(
 
 }  // namespace
 
-static jboolean IsOfflinePagesEnabled(JNIEnv* env,
-                                          const JavaParamRef<jclass>& clazz) {
-  return offline_pages::IsOfflinePagesEnabled();
-}
-
 static jboolean IsOfflineBookmarksEnabled(JNIEnv* env,
                                           const JavaParamRef<jclass>& clazz) {
   return offline_pages::IsOfflineBookmarksEnabled();
