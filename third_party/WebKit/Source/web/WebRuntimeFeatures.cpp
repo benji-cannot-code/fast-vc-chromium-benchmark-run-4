bb7f9315f93917122a09069945a6276bacaa956f
toyoshim <toyoshim@chromium.org>
2016-09-06 22:52:08 -0700

    Restore user state on ReloadwithoutSubResourceCacheRevalidation
    Currently, Chrome does not restore user state, scroll position
    and scale factor for same page navigation regardless of
    NonValidatingReloadOnNormalReload content feature flag.
    
    This patch connects NonValidatingReloadOnNormalReload flag
    to ReloadwithoutSubResourceCacheRevalidation blink feature flag,
    and changes the behavior to restore user state even for the same
    page navigation if ReloadwithoutSubResourceCacheRevalidation
    flag is enabled.
    
    BUG=632358
    
    Review-Url: https://codereview.chromium.org/2257743002
    Cr-Commit-Position: refs/heads/master@{#416855}

--- third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
@@ -321,6 +321,11 @@ void WebRuntimeFeatures::enableReducedReferrerGranularity(bool enable)
     RuntimeEnabledFeatures::setReducedReferrerGranularityEnabled(enable);
 }
 
+void WebRuntimeFeatures::enableReloadwithoutSubResourceCacheRevalidation(bool enable)
+{
+    RuntimeEnabledFeatures::setReloadwithoutSubResourceCacheRevalidationEnabled(enable);
+}
+
 void WebRuntimeFeatures::enablePushMessaging(bool enable)
 {
     RuntimeEnabledFeatures::setPushMessagingEnabled(enable);
