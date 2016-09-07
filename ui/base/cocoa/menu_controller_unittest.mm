4a832002211e6a030f1dc133181f2a2171e3a678
fdoray <fdoray@chromium.org>
2016-09-07 14:50:21 -0700

    Remove calls to deprecated MessageLoop methods in ui.
    This CL (hopefully) removes all calls to these methods in ui:
    - MessageLoop::PostTask
    - MessageLoop::PostDelayedTask
    - MessageLoop::DeleteSoon
    - MessageLoop::ReleaseSoon
    - MessageLoop::Run
    - MessageLoop::RunUntilIdle
    
    BUG=616447
    R=sky@chromium.org
    
    Review-Url: https://codereview.chromium.org/2315003002
    Cr-Commit-Position: refs/heads/master@{#417059}

--- ui/base/cocoa/menu_controller_unittest.mm
+++ ui/base/cocoa/menu_controller_unittest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 
 #include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -387,7 +388,7 @@ TEST_F(MenuControllerTest, OpenClose) {
   EXPECT_FALSE(delegate.did_close_);
 
   // Pump the task that notifies the delegate.
-  message_loop.RunUntilIdle();
+  base::RunLoop().RunUntilIdle();
 
   // Expect that the delegate got notified properly.
   EXPECT_TRUE(delegate.did_close_);
