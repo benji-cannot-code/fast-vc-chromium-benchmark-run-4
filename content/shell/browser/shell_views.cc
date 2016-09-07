ded3a422800a786f02b36a518965d50609a63dd6
estade <estade@chromium.org>
2016-09-07 15:06:51 -0700

    Rename views::MdTextButton::CreateMdButton to just Create
    BUG=none
    TBR=peter@chromium.org
    
    Review-Url: https://codereview.chromium.org/2313253003
    Cr-Commit-Position: refs/heads/master@{#417075}

--- content/shell/browser/shell_views.cc
+++ content/shell/browser/shell_views.cc
@@ -133,7 +133,7 @@ class ShellWindowDelegateView : public views::WidgetDelegateView,
           toolbar_layout->AddColumnSet(0);
       // Back button
       back_button_ =
-          views::MdTextButton::CreateMdButton(this, base::ASCIIToUTF16("Back"));
+          views::MdTextButton::Create(this, base::ASCIIToUTF16("Back"));
       gfx::Size back_button_size = back_button_->GetPreferredSize();
       toolbar_column_set->AddColumn(views::GridLayout::CENTER,
                                     views::GridLayout::CENTER, 0,
@@ -141,8 +141,8 @@ class ShellWindowDelegateView : public views::WidgetDelegateView,
                                     back_button_size.width(),
                                     back_button_size.width() / 2);
       // Forward button
-      forward_button_ = views::MdTextButton::CreateMdButton(
-          this, base::ASCIIToUTF16("Forward"));
+      forward_button_ =
+          views::MdTextButton::Create(this, base::ASCIIToUTF16("Forward"));
       gfx::Size forward_button_size = forward_button_->GetPreferredSize();
       toolbar_column_set->AddColumn(views::GridLayout::CENTER,
                                     views::GridLayout::CENTER, 0,
@@ -150,8 +150,8 @@ class ShellWindowDelegateView : public views::WidgetDelegateView,
                                     forward_button_size.width(),
                                     forward_button_size.width() / 2);
       // Refresh button
-      refresh_button_ = views::MdTextButton::CreateMdButton(
-          this, base::ASCIIToUTF16("Refresh"));
+      refresh_button_ =
+          views::MdTextButton::Create(this, base::ASCIIToUTF16("Refresh"));
       gfx::Size refresh_button_size = refresh_button_->GetPreferredSize();
       toolbar_column_set->AddColumn(views::GridLayout::CENTER,
                                     views::GridLayout::CENTER, 0,
@@ -160,7 +160,7 @@ class ShellWindowDelegateView : public views::WidgetDelegateView,
                                     refresh_button_size.width() / 2);
       // Stop button
       stop_button_ =
-          views::MdTextButton::CreateMdButton(this, base::ASCIIToUTF16("Stop"));
+          views::MdTextButton::Create(this, base::ASCIIToUTF16("Stop"));
       gfx::Size stop_button_size = stop_button_->GetPreferredSize();
       toolbar_column_set->AddColumn(views::GridLayout::CENTER,
                                     views::GridLayout::CENTER, 0,
