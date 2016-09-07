00e4be4c2a5559a4f0dc703b490e739631a18f99
dgrogan <dgrogan@chromium.org>
2016-09-07 14:54:50 -0700

    Log UMA when x-origin documents (1) are created and (2) become visible
    This will give us an upper bound on the % of iframes for which we can
    defer loading.
    
    BUG=635105
    
    Review-Url: https://codereview.chromium.org/2212183004
    Cr-Commit-Position: refs/heads/master@{#417064}

--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -1095,6 +1095,8 @@ public:
 
     bool isInMainFrame() const;
 
+    void onVisibilityMaybeChanged(bool visible);
+
 protected:
     Document(const DocumentInit&, DocumentClassFlags = DefaultDocumentClass);
 
@@ -1417,6 +1419,8 @@ private:
     bool m_mayContainV0Shadow = false;
 
     Member<SnapCoordinator> m_snapCoordinator;
+
+    bool m_visibilityWasLogged;
 };
 
 extern template class CORE_EXTERN_TEMPLATE_EXPORT Supplement<Document>;
