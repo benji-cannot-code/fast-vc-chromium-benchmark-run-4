10997fec7803554f7866dee5dcb9b93b63c56270
sprang <sprang@chromium.org>
2016-09-07 09:11:33 -0700

    Remove mock for deprecated webrtc::PeerConnectionInterface::ice_state()
    BUG=
    
    Review-Url: https://codereview.chromium.org/2318163002
    Cr-Commit-Position: refs/heads/master@{#416949}

--- content/renderer/media/mock_peer_connection_impl.h
+++ content/renderer/media/mock_peer_connection_impl.h
@@ -50,10 +50,6 @@ class MockPeerConnectionImpl : public webrtc::PeerConnectionInterface {
     NOTIMPLEMENTED();
     return PeerConnectionInterface::kStable;
   }
-  IceState ice_state() override {
-    NOTIMPLEMENTED();
-    return PeerConnectionInterface::kIceNew;
-  }
   IceConnectionState ice_connection_state() override {
     NOTIMPLEMENTED();
     return PeerConnectionInterface::kIceConnectionNew;
