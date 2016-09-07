8ad9baf3148b7bf1442f68b601679cbc796a7265
agrieve <agrieve@chromium.org>
2016-09-07 07:10:02 -0700

    Reland of GN(Android): Stop specifying depfile under outputs
    Previous attempt:
    https://codereview.chromium.org/2318673003/ )
    
    Reason for reland:
    Now creates depfile parent directory
    
    Doing so is unnecessary (GN already knows that depfiles are generated by
    their target), and causes the unwanted side-effect of changes to .d
    files triggering rebuilds of dependent targets.
    
    TBR=michaelbai@chromium.org,dpranke@chromium.org,lushnikov@chromium.org
    BUG=639042
    
    Review-Url: https://codereview.chromium.org/2315993003
    Cr-Commit-Position: refs/heads/master@{#416930}

--- build/android/gyp/util/build_utils.py
+++ build/android/gyp/util/build_utils.py
@@ -425,6 +425,7 @@ def AddDepfileOption(parser):
 
 
 def WriteDepfile(path, dependencies):
+  MakeDirectory(os.path.dirname(path))
   with open(path, 'w') as depfile:
     depfile.write(path)
     depfile.write(': ')
