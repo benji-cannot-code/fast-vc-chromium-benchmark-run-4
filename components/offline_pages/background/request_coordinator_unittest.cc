9ba557413191ee9192d6be69dbb37eaa44334486
dougarnett <dougarnett@chromium.org>
2016-09-07 09:32:51 -0700

    [Offline Pages] Close race condition of multiple StartProcessing callers. Introduces new bool to track the starting/picking phase so that we avoid concurrent pickers (and not overwrite the schedule_callback) in order to also avoid the DCHECK that guards concurrent SendRequestToOffliner calls.
    BUG=643455
    
    Review-Url: https://codereview.chromium.org/2317893002
    Cr-Commit-Position: refs/heads/master@{#416957}

--- components/offline_pages/background/request_coordinator_unittest.cc
+++ components/offline_pages/background/request_coordinator_unittest.cc
@@ -194,6 +194,8 @@ class RequestCoordinatorTest
     return coordinator_->is_busy();
   }
 
+  bool is_starting() { return coordinator_->is_starting(); }
+
   // Empty callback function.
   void EmptyCallbackFunction(bool result) {
   }
@@ -601,11 +603,14 @@ TEST_F(RequestCoordinatorTest, RequestNotPickedNonUserRequestedItemsRemain) {
   base::Callback<void(bool)> callback = base::Bind(
       &RequestCoordinatorTest::EmptyCallbackFunction, base::Unretained(this));
   coordinator()->StartProcessing(device_conditions, callback);
+  EXPECT_TRUE(is_starting());
 
   // Call RequestNotPicked, and make sure we pick schedule a task for non user
   // requested conditions.
   CallRequestNotPicked(true);
   PumpLoop();
+
+  EXPECT_FALSE(is_starting());
 
   // The scheduler should have been called to schedule the non-user requested
   // task.
@@ -675,12 +680,15 @@ TEST_F(RequestCoordinatorTest, StartProcessingThenStopProcessingImmediately) {
           &RequestCoordinatorTest::EmptyCallbackFunction,
           base::Unretained(this));
   EXPECT_TRUE(coordinator()->StartProcessing(device_conditions, callback));
+  EXPECT_TRUE(is_starting());
 
   // Now, quick, before it can do much (we haven't called PumpLoop), cancel it.
   coordinator()->StopProcessing();
 
   // Let the async callbacks in the request coordinator run.
   PumpLoop();
+
+  EXPECT_FALSE(is_starting());
 
   // OfflinerDoneCallback will not end up getting called with status SAVED,
   // since we cancelled the event before it called offliner_->LoadAndSave().
@@ -712,12 +720,14 @@ TEST_F(RequestCoordinatorTest, StartProcessingThenStopProcessingLater) {
           &RequestCoordinatorTest::EmptyCallbackFunction,
           base::Unretained(this));
   EXPECT_TRUE(coordinator()->StartProcessing(device_conditions, callback));
+  EXPECT_TRUE(is_starting());
 
   // Let all the async parts of the start processing pipeline run to completion.
   PumpLoop();
 
   // Coordinator should now be busy.
   EXPECT_TRUE(is_busy());
+  EXPECT_FALSE(is_starting());
 
   // Now we cancel it while the prerenderer is busy.
   coordinator()->StopProcessing();
@@ -812,6 +822,7 @@ TEST_F(RequestCoordinatorTest, WatchdogTimeout) {
   WaitForCallback();
   PumpLoop();
 
+  EXPECT_FALSE(is_starting());
   EXPECT_TRUE(OfflinerWasCanceled());
   EXPECT_EQ(Offliner::RequestStatus::REQUEST_COORDINATOR_CANCELED,
             last_offlining_status());
