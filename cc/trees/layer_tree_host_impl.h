632cb38c70c348c938ded9f0ca6cb57df5296fa1
danakj <danakj@chromium.org>
2016-09-07 18:33:53 -0700

    cc: Remove DelegatingRenderer.
    Inline the tiny code left there into LayerTreeHostImpl, delete the
    |renderer_| member and change references to the |output_surface_|
    instead.
    
    R=enne
    BUG=641613
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2323583002
    Cr-Commit-Position: refs/heads/master@{#417150}

--- cc/trees/layer_tree_host_impl.h
+++ cc/trees/layer_tree_host_impl.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/layers/render_pass_sink.h"
 #include "cc/output/begin_frame_args.h"
 #include "cc/output/context_cache_controller.h"
-#include "cc/output/delegating_renderer.h"
 #include "cc/output/managed_memory_policy.h"
 #include "cc/output/output_surface_client.h"
 #include "cc/quads/render_pass.h"
@@ -399,6 +398,7 @@ class CC_EXPORT LayerTreeHostImpl
 
   int RequestedMSAASampleCount() const;
 
+  // TODO(danakj): Rename this, there is no renderer.
   virtual bool InitializeRenderer(OutputSurface* output_surface);
   TileManager* tile_manager() { return &tile_manager_; }
 
@@ -416,7 +416,6 @@ class CC_EXPORT LayerTreeHostImpl
     return settings_.create_low_res_tiling && !use_gpu_rasterization_;
   }
   ResourcePool* resource_pool() { return resource_pool_.get(); }
-  DelegatingRenderer* renderer() { return renderer_.get(); }
   ImageDecodeController* image_decode_controller() {
     return image_decode_controller_.get();
   }
@@ -548,9 +547,8 @@ class CC_EXPORT LayerTreeHostImpl
   void ScheduleMicroBenchmark(std::unique_ptr<MicroBenchmarkImpl> benchmark);
 
   CompositorFrameMetadata MakeCompositorFrameMetadata() const;
-  // Viewport rectangle and clip in nonflipped window space.  These rects
-  // should only be used by Renderer subclasses to populate glViewport/glClip
-  // and their software-mode equivalents.
+  // Viewport rectangle and clip in device space.  These rects are used to
+  // prioritize raster and determine what is submitted in a CompositorFrame.
   gfx::Rect DeviceViewport() const;
 
   // When a SwapPromiseMonitor is created on the impl thread, it calls
@@ -643,7 +641,6 @@ class CC_EXPORT LayerTreeHostImpl
       const gfx::Vector2dF& viewport_delta,
       ScrollTree* scroll_tree);
 
-  void CreateAndSetRenderer();
   void CleanUpTileManagerAndUIResources();
   void CreateTileManagerResources();
   void ReleaseTreeResources();
@@ -744,7 +741,6 @@ class CC_EXPORT LayerTreeHostImpl
   std::unique_ptr<RasterBufferProvider> raster_buffer_provider_;
   std::unique_ptr<TileTaskManager> tile_task_manager_;
   std::unique_ptr<ResourcePool> resource_pool_;
-  std::unique_ptr<DelegatingRenderer> renderer_;
   std::unique_ptr<ImageDecodeController> image_decode_controller_;
 
   GlobalStateThatImpactsTilePriority global_tile_state_;
