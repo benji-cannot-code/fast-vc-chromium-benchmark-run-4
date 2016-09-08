b44585e8039750c1717d4a7865f97bbe3ef7f847
brucedawson <brucedawson@chromium.org>
2016-09-07 18:53:00 -0700

    Switch to VS 2015 Update 3
    VS 2015 Update 3 offers numerous improvements such as improved
    language conformance, fixes to avoid linker crashes, and
    improved code-gen for faster performance. Update 3 also resolves
    some problems with incremental linking silently falling back to
    a full link.
    
    It also fixes an internal compiler error in /analyze builds.
    
    The landmines change is needed because otherwise things like the
    out\Release\cdb directory do not get regenerated.
    
    The process for creating this package is:
    - Create a clean Windows VM running Windows Server 2012 R2 (en_windows_server_2012_r2_vl_with_update_x64_dvd_6052766.iso, SHA1: 247EAEE5628850A41F0C51471656AAFB2492EA08, Standard Edition)
    - Install depot tools, run ‘gclient’, and add depot_tools to the path
    - Install VS 2015 Professional Update 3 - must have been installed after July 12th to get the latest fixes - with these settings and nothing else:
     * Visual C++ and make sure the three nodes underneath are also selected
     * Under Universal Windows App Development Tools make sure the "Tools (1.4.1) and Windows 10 SDK (10.0.14393)" and the "Windows 10 SDK (10.0.10586)" nodes are selected and nothing else
    - Copy the *Debugger and Tools*.msi installers from another machine and run them. The 10.0.14393 versions (Anniversary Edition) should be used.
    - Then run:
        python depot_tools\win_toolchain\package_from_installed.py 2015
    
    It is also possible to package the 14393 SDK by running this command,
    but that change is being saved for later:
        python depot_tools\win_toolchain\package_from_installed.py 2015 -w 10.0.14393.0
    
    BUG=627216,636468,427616,637456
    
    Review-Url: https://codereview.chromium.org/2106203002
    Cr-Commit-Position: refs/heads/master@{#417157}

--- build/win/copy_cdb_to_output.py
+++ build/win/copy_cdb_to_output.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import glob
 import hashlib
 import os
 import shutil
@@ -78,6 +79,7 @@ def _CopyCDBToOutput(output_dir, target_arch):
   dst_winext_dir = os.path.join(output_dir, 'winext')
   src_winxp_dir = os.path.join(src_dir, 'winxp')
   dst_winxp_dir = os.path.join(output_dir, 'winxp')
+  src_crt_dir = os.path.join(win_sdk_dir, r'Redist\ucrt\DLLs', src_arch)
   _ConditionalMkdir(dst_winext_dir)
   _ConditionalMkdir(dst_winxp_dir)
   # Note that the outputs from the "copy_cdb_to_output" target need to
@@ -90,6 +92,9 @@ def _CopyCDBToOutput(output_dir, target_arch):
   _CopyImpl('uext.dll', dst_winext_dir, src_winext_dir)
   _CopyImpl('exts.dll', dst_winxp_dir, src_winxp_dir)
   _CopyImpl('ntsdexts.dll', dst_winxp_dir, src_winxp_dir)
+  for dll_path in glob.glob(os.path.join(src_crt_dir, 'api-ms-win-*.dll')):
+    _CopyImpl(os.path.split(dll_path)[1], output_dir, src_crt_dir)
+  _CopyImpl('ucrtbase.dll', output_dir, src_crt_dir)
   return 0
 
 
