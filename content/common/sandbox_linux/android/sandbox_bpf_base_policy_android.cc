8bf9d062b0b0e9bf4099c09af9bbb0863a44dcae
rsesek <rsesek@chromium.org>
2016-09-08 15:33:38 -0700

    [Android] Allow __NR_rt_tgsigqueueinfo under seccomp.
    BUG=644759
    
    Review-Url: https://codereview.chromium.org/2313393003
    Cr-Commit-Position: refs/heads/master@{#417419}

--- content/common/sandbox_linux/android/sandbox_bpf_base_policy_android.cc
+++ content/common/sandbox_linux/android/sandbox_bpf_base_policy_android.cc
@@ -51,7 +51,8 @@ BoolExpr RestrictSocketArguments(const Arg<int>& domain,
 }  // namespace
 
 SandboxBPFBasePolicyAndroid::SandboxBPFBasePolicyAndroid()
-    : SandboxBPFBasePolicy() {}
+    : SandboxBPFBasePolicy(),
+      pid_(getpid()) {}
 
 SandboxBPFBasePolicyAndroid::~SandboxBPFBasePolicyAndroid() {}
 
@@ -121,6 +122,13 @@ ResultExpr SandboxBPFBasePolicyAndroid::EvaluateSyscall(int sysno) const {
     case __NR_ptrace:
       override_and_allow = true;
       break;
+  }
+
+  // https://crbug.com/644759
+  if (sysno == __NR_rt_tgsigqueueinfo) {
+    const Arg<pid_t> tgid(0);
+    return If(tgid == pid_, Allow())
+           .Else(Error(EPERM));
   }
 
 #if defined(__x86_64__) || defined(__arm__) || defined(__aarch64__) || \
--- content/common/sandbox_linux/android/sandbox_bpf_base_policy_android.h
+++ content/common/sandbox_linux/android/sandbox_bpf_base_policy_android.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_COMMON_SANDBOX_LINUX_ANDROID_SANDBOX_BPF_BASE_POLICY_ANDROID_H_
 #define CONTENT_COMMON_SANDBOX_LINUX_ANDROID_SANDBOX_BPF_BASE_POLICY_ANDROID_H_
 
+#include <sys/types.h>
+
 #include "base/macros.h"
 #include "content/common/sandbox_linux/sandbox_bpf_base_policy_linux.h"
 
@@ -24,6 +26,8 @@ class SandboxBPFBasePolicyAndroid : public SandboxBPFBasePolicy {
       int system_call_number) const override;
 
  private:
+  const pid_t pid_;
+
   DISALLOW_COPY_AND_ASSIGN(SandboxBPFBasePolicyAndroid);
 };
 
