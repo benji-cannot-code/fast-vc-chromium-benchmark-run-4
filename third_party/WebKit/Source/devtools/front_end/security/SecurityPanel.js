754248a69380437e981c97ad9d6502c7dc3d5a80
chenwilliam <chenwilliam@chromium.org>
2016-09-07 17:03:46 -0700

    DevTools: Fix eslint errors
    Found these eslint errors on the new devtools buildbot I setup!
    
    Right now, the buildbot is configured to be non-blocking (i.e. the errors
    don't prevent commits from landing), but eventually it will be.
    
    Eslint errors from buildbot:
    https://luci-logdog.appspot.com/v/?s=chromium%2Fbb%2Fchromium.fyi%2FChromium_DevTools_Linux%2F475%2F%2B%2Frecipes%2Fsteps%2Frun_eslint%2F0%2Fstdout
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2319823002
    Cr-Commit-Position: refs/heads/master@{#417125}

--- third_party/WebKit/Source/devtools/front_end/security/SecurityPanel.js
+++ third_party/WebKit/Source/devtools/front_end/security/SecurityPanel.js
@@ -806,7 +806,7 @@ WebInspector.SecurityMainView.prototype = {
                 "description": WebInspector.UIString("This site includes resources that were loaded with certificate errors.")
             }));
         }
-      },
+    },
 
 
     __proto__: WebInspector.VBox.prototype
