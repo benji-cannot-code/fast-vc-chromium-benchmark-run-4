71559034642c93537845b2f6f06feeeac255173f
iannucci <iannucci@chromium.org>
2016-09-08 04:07:10 -0700

    Remove ignored "force" parameter from bot_update.
    It's already ignored by the bot_update implementation, so this is an
    expectations-only change.
    
    R=agable@chromium.org, estaab@chromium.org, hinoka@chromium.org, martiniss@chromium.org
    BUG=643885
    
    Review-Url: https://codereview.chromium.org/2318353002
    Cr-Commit-Position: refs/heads/master@{#417251}

--- infra/recipes/canary.py
+++ infra/recipes/canary.py
@@ -13,7 +13,7 @@ DEPS = [
 
 def RunSteps(api):
   api.gclient.set_config('chromium')
-  api.bot_update.ensure_checkout(force=True)
+  api.bot_update.ensure_checkout()
 
 def GenTests(api):
   yield api.test('basic') + api.properties.generic()
