7a71173b2b9cd52187151fdb6b155902013545b1
tedchoc <tedchoc@chromium.org>
2016-09-07 18:27:06 -0700

    Add workaround to remove elderberry tabs from recents (pre-L).
    BUG=644953
    
    Review-Url: https://codereview.chromium.org/2319963002
    Cr-Commit-Position: refs/heads/master@{#417146}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/SeparateTaskManagedCustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/SeparateTaskManagedCustomTabActivity.java
@@ -23,6 +23,8 @@ import org.chromium.chrome.browser.webapps.ActivityAssigner;
  * limited number of activities that we will cycle through.
  */
 public class SeparateTaskManagedCustomTabActivity extends SeparateTaskCustomTabActivity {
+    private static final String FORCE_FINISH = "CCT.ForceFinish";
+
     // Time at which an intent was received and handled.
     private long mIntentHandlingTimeMs = 0;
 
@@ -39,6 +41,10 @@ public class SeparateTaskManagedCustomTabActivity extends SeparateTaskCustomTabA
     @Override
     public void onNewIntent(Intent intent) {
         mIntentHandlingTimeMs = SystemClock.uptimeMillis();
+        if (intent != null && intent.getBooleanExtra(FORCE_FINISH, false)) {
+            finish();
+            return;
+        }
         super.onNewIntent(intent);
     }
 
@@ -80,5 +86,14 @@ public class SeparateTaskManagedCustomTabActivity extends SeparateTaskCustomTabA
         assert className.matches("^" + baseClassName + "[0-9]+$");
         String indexString = className.substring(baseClassName.length());
         return Integer.parseInt(indexString);
+    }
+
+    @Override
+    public void finishAndClose() {
+        Intent intent = new Intent(getIntent());
+        intent.setFlags(intent.getFlags() & ~Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        intent.putExtra(FORCE_FINISH, true);
+        startActivity(intent);
     }
 }
