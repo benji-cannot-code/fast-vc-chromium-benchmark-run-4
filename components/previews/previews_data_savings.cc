4557b9184b5cc000fee4c97922db92ce3e791ee6
ryansturm <ryansturm@chromium.org>
2016-09-08 12:12:57 -0700

    Adding a previews object, PreviewsDataSavings
    This object hooks into existing data saver code to track savings based
    on host, data used, and original size of the data.
    
    BUG=615565
    
    Review-Url: https://codereview.chromium.org/2257533003
    Cr-Commit-Position: refs/heads/master@{#417359}

---
+++ components/previews/previews_data_savings.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/previews/previews_data_savings.h"
+
+#include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
+#include "components/data_reduction_proxy/core/common/data_savings_recorder.h"
+
+namespace previews {
+
+PreviewsDataSavings::PreviewsDataSavings(
+    data_reduction_proxy::DataSavingsRecorder* data_savings_recorder)
+    : data_savings_recorder_(data_savings_recorder) {
+  DCHECK(data_savings_recorder);
+}
+
+PreviewsDataSavings::~PreviewsDataSavings() {
+  DCHECK(thread_checker_.CalledOnValidThread());
+}
+
+void PreviewsDataSavings::RecordDataSavings(
+    const std::string& fully_qualified_domain_name,
+    int64_t data_used,
+    int64_t original_size) {
+  DCHECK(thread_checker_.CalledOnValidThread());
+
+  data_savings_recorder_->UpdateDataSavings(fully_qualified_domain_name,
+                                            data_used, original_size);
+}
+
+}  // namespace previews
