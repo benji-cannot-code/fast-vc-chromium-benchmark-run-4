bc5043f24d0a7e3deb02bf12a55a110b00212740
haraken <haraken@chromium.org>
2016-09-07 19:56:22 -0700

    Rename Document::attach/detachLayoutTree to Document::initialize/shutdown
    Document::attach/detachLayoutTree is confusing. It's indeed attaching/detaching
    layout trees but doing more things than that. In particular, Document::detachLayoutTree
    does a bunch of disruptive operations unlike other Node::detachLayoutTree.
    
    It's better to rename Document::attach/detachLayoutTree to Document::initialize/shutdown
    and make it clear that they can be called only once when the Document gets
    initialized/shut down.
    
    BUG=610176
    
    Review-Url: https://codereview.chromium.org/2318913002
    Cr-Commit-Position: refs/heads/master@{#417175}

--- third_party/WebKit/Source/core/html/PluginDocument.h
+++ third_party/WebKit/Source/core/html/PluginDocument.h
@@ -46,7 +46,7 @@ public:
     Widget* pluginWidget();
     Node* pluginNode();
 
-    void detachLayoutTree(const AttachContext& = AttachContext()) override;
+    void shutdown() override;
 
     DECLARE_VIRTUAL_TRACE();
 
