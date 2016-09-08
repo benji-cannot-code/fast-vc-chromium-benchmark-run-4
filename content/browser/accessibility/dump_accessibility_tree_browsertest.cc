bf340dbb27e28e8abec424f4e2227bd0f028b664
dmazzoni <dmazzoni@chromium.org>
2016-09-07 22:40:37 -0700

    Re-land (2): Fix loading accessibility tree for child frame that's already loaded.
    Original issue: http://crrev.com/2299673002
    Second attempt: http://crrev.com/2299283002
    
    Fix the flakiness by not trying to toggle accessibility off and on between the
    two runs of the same page within each test.
    
    Code leftover from the pre-OOPIF days was causing us to exit early from
    the RenderAccessibilityImpl constructor for some child frames that were
    already loaded. Everything worked fine if accessibility was already enabled
    when loading the frame, but if the frame was already loaded and then
    accessibility was enabled, this could cause it to fail to create an
    accessibility tree.
    
    The code in RenderAccessibilityImpl is no longer needed because now we
    have exactly one accessibility tree per frame.
    
    This wasn't caught by tests because we didn't cover both scenarios, we
    always enabled accessibility first.
    
    Added two variants of existing tests that load the page first and then
    enable accessibility.
    
    BUG=640231
    TBR=dtseng@chromium.org
    
    Review-Url: https://codereview.chromium.org/2317323002
    Cr-Commit-Position: refs/heads/master@{#417194}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -849,6 +849,12 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityFrameset) {
   RunHtmlTest(FILE_PATH_LITERAL("frameset.html"));
 }
 
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
+                       AccessibilityFramesetPostEnable) {
+  enable_accessibility_after_navigating_ = true;
+  RunHtmlTest(FILE_PATH_LITERAL("frameset.html"));
+}
+
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityHead) {
   RunHtmlTest(FILE_PATH_LITERAL("head.html"));
 }
@@ -879,6 +885,12 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityI) {
 }
 
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityIframe) {
+  RunHtmlTest(FILE_PATH_LITERAL("iframe.html"));
+}
+
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
+                       AccessibilityIframePostEnable) {
+  enable_accessibility_after_navigating_ = true;
   RunHtmlTest(FILE_PATH_LITERAL("iframe.html"));
 }
 
