df806790106b6f49725b68629fb7605ee0197d30
agrieve <agrieve@chromium.org>
2016-09-08 08:00:56 -0700

    Allow test_runner.py to run host perf tests without devices attached
    BUG=644970
    
    Review-Url: https://codereview.chromium.org/2320903002
    Cr-Commit-Position: refs/heads/master@{#417295}

--- build/android/pylib/local/device/local_device_environment.py
+++ build/android/pylib/local/device/local_device_environment.py
@@ -81,7 +81,7 @@ class LocalDeviceEnvironment(environment.Environment):
                        else None)
     self._device_serial = args.test_device
     self._devices_lock = threading.Lock()
-    self._devices = []
+    self._devices = None
     self._concurrent_adb = args.enable_concurrent_adb
     self._enable_device_cache = args.enable_device_cache
     self._logcat_monitors = []
@@ -94,6 +94,9 @@ class LocalDeviceEnvironment(environment.Environment):
 
   #override
   def SetUp(self):
+    pass
+
+  def _InitDevices(self):
     device_arg = 'default'
     if self._target_devices_file:
       device_arg = device_list.GetPersistentDeviceList(
@@ -150,6 +153,10 @@ class LocalDeviceEnvironment(environment.Environment):
 
   @property
   def devices(self):
+    # Initialize lazily so that host-only tests do not fail when no devices are
+    # attached.
+    if self._devices is None:
+      self._InitDevices()
     if not self._devices:
       raise device_errors.NoDevicesError()
     return self._devices
@@ -172,6 +179,8 @@ class LocalDeviceEnvironment(environment.Environment):
 
   #override
   def TearDown(self):
+    if self._devices is None:
+      return
     @handle_shard_failures_with(on_failure=self.BlacklistDevice)
     def tear_down_device(d):
       # Write the cache even when not using it so that it will be ready the
