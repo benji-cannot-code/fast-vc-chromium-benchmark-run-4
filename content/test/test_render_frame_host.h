92c690008392f766b25d5059667e7bde3594aee3
scottmg <scottmg@chromium.org>
2016-09-08 12:56:14 -0700

    PlzNavigate: Fix DownloadRequestLimiterTest.DownloadRequestLimiter_RendererInitiated
    Expose NavigateAndCommitRendererInitiated in TestRendererHost, use it to
    fix download request limiter test for renderer-initiated case in
    PlzNavigate.
    
    R=clamy@chromium.org, dominickn@chromium.org
    BUG=510836
    
    Review-Url: https://codereview.chromium.org/2319733002
    Cr-Commit-Position: refs/heads/master@{#417373}

--- content/test/test_render_frame_host.h
+++ content/test/test_render_frame_host.h
@@ -80,6 +80,9 @@ class TestRenderFrameHost : public RenderFrameHostImpl,
   void SetContentsMimeType(const std::string& mime_type) override;
   void SendBeforeUnloadACK(bool proceed) override;
   void SimulateSwapOutACK() override;
+  void NavigateAndCommitRendererInitiated(int page_id,
+                                          bool did_create_new_entry,
+                                          const GURL& url) override;
 
   void SendNavigateWithReplacement(int page_id,
                                    int nav_entry_id,
@@ -97,11 +100,6 @@ class TestRenderFrameHost : public RenderFrameHostImpl,
       const ModificationCallback& callback);
   void SendNavigateWithParams(
       FrameHostMsg_DidCommitProvisionalLoad_Params* params);
-
-  // Simulate a renderer-initiated navigation up until commit.
-  void NavigateAndCommitRendererInitiated(int page_id,
-                                          bool did_create_new_entry,
-                                          const GURL& url);
 
   // With the current navigation logic this method is a no-op.
   // PlzNavigate: this method simulates receiving a BeginNavigation IPC.
