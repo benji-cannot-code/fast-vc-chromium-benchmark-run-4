a5349aa1f233ed3153128cdb4128c3e62ca2681f
jbauman <jbauman@chromium.org>
2016-09-07 19:20:33 -0700

    Validate all flush IDs if the unvalidated list gets too long.
    The browser process normally never needs to validate sync tokens unless
    it's returning resources to a renderer, so it's possible the highest
    verified flush ID will almost never be updated and the list of
    unvalidated flush IDs will grow indefinitely. Force a validation if that
    happens to avoid wasting memory.
    
    BUG=635051
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2318933005
    Cr-Commit-Position: refs/heads/master@{#417168}

--- gpu/ipc/client/command_buffer_proxy_impl.cc
+++ gpu/ipc/client/command_buffer_proxy_impl.cc
@@ -267,7 +267,6 @@ void CommandBufferProxyImpl::Flush(int32_t put_offset) {
     const uint32_t flush_id = channel_->OrderingBarrier(
         route_id_, stream_id_, put_offset, ++flush_count_, latency_info_,
         put_offset_changed, true, &highest_verified_flush_id);
-    UpdateVerifiedReleases(highest_verified_flush_id);
     if (put_offset_changed) {
       DCHECK(flush_id);
       const uint64_t fence_sync_release = next_fence_sync_release_ - 1;
@@ -277,6 +276,7 @@ void CommandBufferProxyImpl::Flush(int32_t put_offset) {
             std::make_pair(fence_sync_release, flush_id));
       }
     }
+    CleanupFlushedReleases(highest_verified_flush_id);
   }
 
   if (put_offset_changed)
@@ -299,7 +299,6 @@ void CommandBufferProxyImpl::OrderingBarrier(int32_t put_offset) {
     const uint32_t flush_id = channel_->OrderingBarrier(
         route_id_, stream_id_, put_offset, ++flush_count_, latency_info_,
         put_offset_changed, false, &highest_verified_flush_id);
-    UpdateVerifiedReleases(highest_verified_flush_id);
 
     if (put_offset_changed) {
       DCHECK(flush_id);
@@ -310,6 +309,7 @@ void CommandBufferProxyImpl::OrderingBarrier(int32_t put_offset) {
             std::make_pair(fence_sync_release, flush_id));
       }
     }
+    CleanupFlushedReleases(highest_verified_flush_id);
   }
 
   if (put_offset_changed)
@@ -745,6 +745,18 @@ void CommandBufferProxyImpl::UpdateVerifiedReleases(uint32_t verified_flush) {
     verified_fence_sync_release_ = front_item.first;
     flushed_release_flush_id_.pop();
   }
+}
+
+void CommandBufferProxyImpl::CleanupFlushedReleases(
+    uint32_t highest_verified_flush_id) {
+  DCHECK(channel_);
+  static const uint32_t kMaxUnverifiedFlushes = 1000;
+  if (flushed_release_flush_id_.size() > kMaxUnverifiedFlushes) {
+    // Prevent list of unverified flushes from growing indefinitely.
+    highest_verified_flush_id =
+        channel_->ValidateFlushIDReachedServer(stream_id_, false);
+  }
+  UpdateVerifiedReleases(highest_verified_flush_id);
 }
 
 gpu::CommandBufferSharedState* CommandBufferProxyImpl::shared_state() const {
