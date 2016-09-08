c04aa0c9d06beaba8a28261e5fab5583bc7828ef
peria <peria@chromium.org>
2016-09-08 03:54:32 -0700

    Remove unnecessary include
    Password manager does not use clients, while it includes
    chrome_password_manager_client.h.
    
    It caused flaky build failures on some trybots.
    e.g. https://build.chromium.org/p/tryserver.chromium.win/builders/win_clang/builds/82274
    https://build.chromium.org/p/tryserver.chromium.win/builders/win_clang/builds/82281
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2319763003
    Cr-Commit-Position: refs/heads/master@{#417243}

--- chrome/browser/password_manager/password_manager_test_base.cc
+++ chrome/browser/password_manager/password_manager_test_base.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/run_loop.h"
 #include "base/strings/stringprintf.h"
-#include "chrome/browser/password_manager/chrome_password_manager_client.h"
 #include "chrome/browser/password_manager/password_store_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
