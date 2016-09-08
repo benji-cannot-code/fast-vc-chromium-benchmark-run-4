b1c5e7f501efa08738f343b8638013adadf70417
cbiesinger <cbiesinger@chromium.org>
2016-09-08 15:57:43 -0700

    [layout-ng] Use correct height for ContainerBlockSize
    https://codereview.chromium.org/2154593003 added
    availableLogicalHeightForPercentageComputation, shared with the regular
    percentage computation code, so this is definitely correct.
    
    R=ikilpatrick@chromium.org,eae@chromium.org,glebl@chromium.org
    BUG=635619
    
    Review-Url: https://codereview.chromium.org/2323953002
    Cr-Commit-Position: refs/heads/master@{#417427}

--- third_party/WebKit/Source/core/layout/ng/ng_constraint_space.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_constraint_space.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/layout/ng/ng_constraint_space.h"
 
+#include "core/layout/LayoutBlock.h"
+#include "core/layout/LayoutView.h"
 #include "core/layout/ng/ng_units.h"
 
 namespace blink {
@@ -55,9 +57,13 @@ NGConstraintSpace* NGConstraintSpace::CreateFromLayoutObject(
   // XXX for orthogonal writing mode this is not right
   LayoutUnit container_logical_width =
       std::max(LayoutUnit(), box.containingBlockLogicalWidthForContent());
-  // XXX Make sure this height is correct
-  LayoutUnit container_logical_height =
-      box.containingBlockLogicalHeightForContent(ExcludeMarginBorderPadding);
+  LayoutUnit container_logical_height;
+  if (!box.parent()) {
+    container_logical_height = box.view()->viewLogicalHeightForPercentages();
+  } else if (box.containingBlock()) {
+    container_logical_height =
+        box.containingBlock()->availableLogicalHeightForPercentageComputation();
+  }
   if (box.hasOverrideLogicalContentWidth()) {
     container_logical_width = box.overrideLogicalContentWidth();
     fixed_inline = true;
