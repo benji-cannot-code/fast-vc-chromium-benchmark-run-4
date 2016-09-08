f8eb1a6869a2e1c07a1369c7dabc5db3049a8344
fdoray <fdoray@chromium.org>
2016-09-08 09:13:45 -0700

    Remove calls to deprecated MessageLoop methods in ios.
    This CL (hopefully) removes all calls to these methods in ios:
    - MessageLoop::PostTask
    - MessageLoop::PostDelayedTask
    - MessageLoop::DeleteSoon
    - MessageLoop::ReleaseSoon
    - MessageLoop::Run
    - MessageLoop::RunUntilIdle
    
    BUG=616447
    R=sdefresne@chromium.org
    
    Review-Url: https://codereview.chromium.org/2314243002
    Cr-Commit-Position: refs/heads/master@{#417305}

--- ios/web/webui/url_data_manager_ios_backend.mm
+++ ios/web/webui/url_data_manager_ios_backend.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted_memory.h"
 #include "base/memory/weak_ptr.h"
 #include "base/message_loop/message_loop.h"
+#include "base/single_thread_task_runner.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/trace_event/trace_event.h"
@@ -462,11 +463,11 @@ bool URLDataManagerIOSBackend::StartRequest(const net::URLRequest* request,
   // replies are put on the IO thread in the same order.
   base::MessageLoop* target_message_loop =
       web::WebThread::UnsafeGetMessageLoopForThread(web::WebThread::UI);
-  target_message_loop->PostTask(
+  target_message_loop->task_runner()->PostTask(
       FROM_HERE, base::Bind(&GetMimeTypeOnUI, base::RetainedRef(source), path,
                             job->weak_factory_.GetWeakPtr()));
 
-  target_message_loop->PostTask(
+  target_message_loop->task_runner()->PostTask(
       FROM_HERE, base::Bind(&URLDataManagerIOSBackend::CallStartRequest,
                             make_scoped_refptr(source), path, request_id));
   return true;
