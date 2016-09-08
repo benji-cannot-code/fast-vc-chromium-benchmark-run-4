55bbd949ad486f229985f1207fdca1f2e8f11997
dfalcantara <dfalcantara@chromium.org>
2016-09-08 16:45:20 -0700

    [Download Home] Expose Java side to DownloadItem states
    * Expose DownloadItem::DownloadState to Java as an enum.
    
    * Pass all DownloadItem updates to Java so that the DownloadHistoryAdapter
      can process them.  We're still ignoring everything but the COMPLETE state.
    
    BUG=616324
    
    Review-Url: https://codereview.chromium.org/2320693003
    Cr-Commit-Position: refs/heads/master@{#417439}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadActivityTest.java
@@ -32,6 +32,7 @@ import org.chromium.chrome.test.util.ChromeRestriction;
 import org.chromium.content.browser.test.util.CallbackHelper;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
+import org.chromium.content_public.browser.DownloadState;
 
 import java.util.List;
 
@@ -141,7 +142,7 @@ public class DownloadActivityTest extends BaseActivityInstrumentationTestCase<Do
         ThreadUtils.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                mAdapter.onDownloadItemUpdated(updateItem, false);
+                mAdapter.onDownloadItemUpdated(updateItem, false, DownloadState.COMPLETE);
             }
         });
         mAdapterObserver.onChangedCallback.waitForCallback(callCount);
@@ -154,7 +155,7 @@ public class DownloadActivityTest extends BaseActivityInstrumentationTestCase<Do
         ThreadUtils.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                mAdapter.onDownloadItemUpdated(deletedItem, false);
+                mAdapter.onDownloadItemUpdated(deletedItem, false, DownloadState.COMPLETE);
             }
         });
         mAdapterObserver.onChangedCallback.waitForCallback(callCount);
