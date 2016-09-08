04544413aae9e06bdc458889b1b65a3adc3d41c0
xunjieli <xunjieli@chromium.org>
2016-09-08 12:00:21 -0700

    Add back initialization of |dns_resolution_end_time_| in QuicStreamFactory::Job
    |dns_resolution_end_time_| is still used in other histograms
    (e.g. Net.QuicServerInfo.DiskCacheWaitForDataReadyTime and
    Net.QuicSession.HostResolution.HandshakeConfirmedTime).
    
    The initialization is removed in crrev.com/2256803002. This CL adds it back
    to maintain correctness of the remaining histograms which are still using this
    field.
    
    R=zhongyi@chromium.org
    
    BUG=629384, 637051
    
    Review-Url: https://codereview.chromium.org/2314403006
    Cr-Commit-Position: refs/heads/master@{#417355}

--- net/quic/chromium/quic_stream_factory.cc
+++ net/quic/chromium/quic_stream_factory.cc
@@ -489,6 +489,7 @@ int QuicStreamFactory::Job::DoResolveHost() {
 }
 
 int QuicStreamFactory::Job::DoResolveHostComplete(int rv) {
+  dns_resolution_end_time_ = base::TimeTicks::Now();
   if (rv != OK)
     return rv;
 
