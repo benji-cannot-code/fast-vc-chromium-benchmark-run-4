e1f1914c89f38d7153d99a997dfd71fc6fb1bbf8
Daniel Murphy <dmurph@chromium.org>
2016-09-08 13:49:11 -0700

    Revert "Import wpt@65954a35b4e0a5cee1f4839271ba0e44f187c0a6"
    This reverts commit 9cdba1441437ab946977a224cc45971cfadd8abb.
    https://codereview.chromium.org/2308153002/
    
    BUG=645177,645181
    TBR=qyearsley
    
    Previous commit message:
    Import wpt@65954a35b4e0a5cee1f4839271ba0e44f187c0a6
    
    Using update-w3c-deps in Chromium c7eb73acf26f4e00ad724d8d2efde5dc63c07e66.
    
    Review URL: https://codereview.chromium.org/2323113002 .
    
    Committed: https://crrev.com/9cdba1441437ab946977a224cc45971cfadd8abb
    Cr-Original-Commit-Position: refs/heads/master@{#417302}
    Cr-Commit-Position: refs/heads/master@{#417393}

--- third_party/WebKit/LayoutTests/resources/idlharness.js
+++ third_party/WebKit/LayoutTests/resources/idlharness.js
@@ -1395,9 +1395,6 @@ IdlInterface.prototype.test_members = function()
             {
                 this.test_member_attribute(member);
             }
-            if (member.stringifier) {
-                this.test_member_stringifier(member);
-            }
             break;
 
         case "operation":
