0f05f577257e0a76c2e5dbc227d4b5f9f178a537
svillar <svillar@igalia.com>
2016-09-08 09:37:47 -0700

    [css-grid] Fix a dangling reference
    The code was making a reference to an attribute of a temporary object created by gridTrackSize().
    
    BUG=641982
    
    Review-Url: https://codereview.chromium.org/2325603003
    Cr-Commit-Position: refs/heads/master@{#417309}

--- third_party/WebKit/Source/core/layout/LayoutGrid.cpp
+++ third_party/WebKit/Source/core/layout/LayoutGrid.cpp
@@ -2081,7 +2081,7 @@ LayoutUnit LayoutGrid::assumedRowsSizeForOrthogonalChild(const LayoutBox& child,
     bool gridAreaIsIndefinite = false;
     LayoutUnit containingBlockAvailableSize = containingBlockLogicalHeightForContent(ExcludeMarginBorderPadding);
     for (auto trackPosition : span) {
-        const GridLength& maxTrackSize = gridTrackSize(ForRows, trackPosition, sizingOperation).maxTrackBreadth();
+        GridLength maxTrackSize = gridTrackSize(ForRows, trackPosition, sizingOperation).maxTrackBreadth();
         if (maxTrackSize.isContentSized() || maxTrackSize.isFlex())
             gridAreaIsIndefinite = true;
         else
