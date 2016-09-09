e8c388e42f338d6c48865112ab83b98a1bff2e68
loyso <loyso@chromium.org>
2016-09-09 00:54:25 -0700

    CC Animation: Rename has_any_animation and make implementattion out-of-line
    This is a preparation to erase animations_ data stored in ElementAnimation.
    
    BUG=592873
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2322693003
    Cr-Commit-Position: refs/heads/master@{#417531}

--- cc/animation/element_animations.cc
+++ cc/animation/element_animations.cc
@@ -136,7 +136,7 @@ void ElementAnimations::PushPropertiesTo(
     return;
   needs_push_properties_ = false;
 
-  if (!has_any_animation() && !element_animations_impl->has_any_animation())
+  if (!HasAnyAnimation() && !element_animations_impl->HasAnyAnimation())
     return;
   MarkAbortedAnimationsForDeletion(element_animations_impl.get());
   PurgeAnimationsMarkedForDeletion();
@@ -1209,6 +1209,10 @@ bool ElementAnimations::HasActiveAnimation() const {
       return true;
   }
   return false;
+}
+
+bool ElementAnimations::HasAnyAnimation() const {
+  return !animations_.empty();
 }
 
 bool ElementAnimations::IsPotentiallyAnimatingProperty(
