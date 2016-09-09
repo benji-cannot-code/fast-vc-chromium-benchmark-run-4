9757588a99e8b5da05184eb899a0044a28c49fbd
mmenke <mmenke@chromium.org>
2016-09-09 14:55:44 -0700

    MockHostResolver:  Don't resolve some types of invalid DNS addresses.
    The MockHostResolver didn't have the DNS validity check that
    HostResolverImpl had. This was causing a fuzzer to send invalid domains
    to the CookieStore code, causing DCHECKs.
    
    Also prevent invalid DNS addresses from being put in the HOSTS list,
    when using the internal resolver, as that list is checked before
    checking if DNS addresses are valid.
    
    BUG=632606
    
    Review-Url: https://codereview.chromium.org/2294343003
    Cr-Commit-Position: refs/heads/master@{#417723}

--- net/dns/dns_hosts.cc
+++ net/dns/dns_hosts.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_util.h"
+#include "net/dns/dns_util.h"
 
 using base::StringPiece;
 
@@ -156,6 +157,8 @@ void ParseHostsWithCommaMode(const std::string& contents,
       }
     } else {
       DnsHostsKey key(parser.token().as_string(), family);
+      if (!IsValidDNSDomain(key.first))
+        continue;
       key.first = base::ToLowerASCII(key.first);
       IPAddress* mapped_ip = &(*dns_hosts)[key];
       if (mapped_ip->empty())
