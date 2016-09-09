2b2fb43ee55726a3fc9e2eabf0ad3fc22940c563
rjkroege <rjkroege@chromium.org>
2016-09-08 18:27:59 -0700

    Cleanup Ash Metro mode.
    Remove an unecessary switch exposed during ash::DisplayManager
    refactoring.
    
    BUG=581462
    
    Review-Url: https://codereview.chromium.org/2325723003
    Cr-Commit-Position: refs/heads/master@{#417474}

--- ash/test/test_suite.cc
+++ ash/test/test_suite.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/test/gl_surface_test_support.h"
 
 #if defined(OS_WIN)
-#include "base/win/windows_version.h"
+#include "base/win/scoped_com_initializer.h"
 #include "ui/base/win/atl_module.h"
 #endif
 
@@ -37,15 +37,8 @@ void AuraShellTestSuite::Initialize() {
   gl::GLSurfaceTestSupport::InitializeOneOff();
 
 #if defined(OS_WIN)
-  base::win::Version version = base::win::GetVersion();
-  // Although Ash officially is only supported for users on Win7+, we still run
-  // ash_unittests on Vista builders, so we still need to initialize COM.
-  if (version >= base::win::VERSION_VISTA &&
-      !base::CommandLine::ForCurrentProcess()->HasSwitch(
-          ash::switches::kForceAshToDesktop)) {
-    com_initializer_.reset(new base::win::ScopedCOMInitializer());
-    ui::win::CreateATLModuleIfNeeded();
-  }
+  com_initializer_.reset(new base::win::ScopedCOMInitializer());
+  ui::win::CreateATLModuleIfNeeded();
 #endif
 
   gfx::RegisterPathProvider();
