dc8fcfc9b1ee3003d14766fd83ee786602d70fc3
lhchavez <lhchavez@chromium.org>
2016-09-09 16:51:41 -0700

    arc: Add a global 5 minute timeout for ARC opt-in
    This should be a safety net for *all* boot failures we have (or might
    have). This change replaces the flag |waiting_for_reply_| with a
    base::OneShotTimeout that will log the situation and report a timeout
    error to the user.
    
    BUG=645233
    TEST=Intentionally make ARC not send the reply, see the UI, ARC is still
         running.
    
    Review-Url: https://codereview.chromium.org/2323043003
    Cr-Commit-Position: refs/heads/master@{#417770}

--- chrome/browser/chromeos/arc/arc_auth_service.h
+++ chrome/browser/chromeos/arc/arc_auth_service.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
+#include "base/timer/timer.h"
 #include "chrome/browser/chromeos/arc/arc_android_management_checker_delegate.h"
 #include "chrome/browser/chromeos/arc/arc_auth_code_fetcher_delegate.h"
 #include "chrome/browser/chromeos/arc/arc_auth_context_delegate.h"
@@ -220,6 +221,7 @@ class ArcAuthService : public ArcService,
   void CheckAndroidManagement(bool background_mode);
   void StartArcIfSignedIn();
   void OnArcDataRemoved(bool success);
+  void OnArcSignInTimeout();
 
   // Unowned pointer. Keeps current profile.
   Profile* profile_ = nullptr;
@@ -239,7 +241,7 @@ class ArcAuthService : public ArcService,
   base::string16 ui_page_status_;
   bool clear_required_ = false;
   bool reenable_arc_ = false;
-  bool waiting_for_reply_ = false;
+  base::OneShotTimer arc_sign_in_timer_;
 
   std::unique_ptr<ArcAuthContext> context_;
   std::unique_ptr<ArcAuthCodeFetcher> auth_code_fetcher_;
