dc8ed124058b0d2c95f42c053c159ecbe9f2bfac
ryansturm <ryansturm@chromium.org>
2016-09-09 12:34:08 -0700

    Adding an IO Data and keyed service to previews/
    Due to the need to have inter-thread communication for black listing and
    other features of previews/, an IO Data object and KeyedService are
    introduced. ProfileIOData owns the PreviewsIOData and a new
    KeyedService, PreviewsService owns a PreviewsUIService object. The major
    implementation remains in previews/, but the KeyedService is exposed to
    TabHelpers (chrome/ based WebContentsObservers) and any other UI objects
    that might need to access previews/. The IOData object is similarly
    accessible on the IO thread, typically by embedding a raw pointer into
    objects that need it (e.g., OfflinePageRequestInterceptor,
    PreviewsNetworkDelegate).
    
    The inter-thread communication model will be consumed by the previews/
    opt-out black list, which will need to access the black list on the IO
    thread for synchronous calls and access previews/ on the UI thread with
    asynchronous calls.
    
    BUG=639087
    
    Review-Url: https://codereview.chromium.org/2300213002
    Cr-Commit-Position: refs/heads/master@{#417668}

--- chrome/browser/profiles/profile_impl_io_data.cc
+++ chrome/browser/profiles/profile_impl_io_data.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/memory/ptr_util.h"
 #include "base/metrics/field_trial.h"
 #include "base/sequenced_task_runner.h"
 #include "base/stl_util.h"
@@ -34,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/spdyproxy/data_reduction_proxy_chrome_io_data.h"
 #include "chrome/browser/net/spdyproxy/data_reduction_proxy_chrome_settings.h"
 #include "chrome/browser/net/spdyproxy/data_reduction_proxy_chrome_settings_factory.h"
+#include "chrome/browser/previews/previews_service.h"
+#include "chrome/browser/previews/previews_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
@@ -49,6 +52,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_filter.h"
 #include "components/prefs/pref_member.h"
 #include "components/prefs/pref_service.h"
+#include "components/previews/previews_io_data.h"
+#include "components/previews/previews_ui_service.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/cookie_store_factory.h"
 #include "content/public/browser/notification_service.h"
@@ -201,6 +206,14 @@ void ProfileImplIOData::Handle::Init(
           profile_->GetRequestContext(), std::move(store),
           BrowserThread::GetTaskRunnerForThread(BrowserThread::UI),
           db_task_runner);
+
+  io_data_->set_previews_io_data(base::MakeUnique<previews::PreviewsIOData>(
+      BrowserThread::GetTaskRunnerForThread(BrowserThread::UI),
+      BrowserThread::GetTaskRunnerForThread(BrowserThread::IO)));
+  PreviewsServiceFactory::GetForProfile(profile_)->set_previews_ui_service(
+      base::MakeUnique<previews::PreviewsUIService>(
+          io_data_->previews_io_data(),
+          BrowserThread::GetTaskRunnerForThread(BrowserThread::IO)));
 }
 
 content::ResourceContext*
