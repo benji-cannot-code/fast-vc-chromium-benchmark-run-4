fbee735d4a87513024d70157ebbe8429aa723681
xunjieli <xunjieli@chromium.org>
2016-09-09 14:32:48 -0700

    Add net::SourceStream and net::FilterSourceStream
    This CL adds two new classes net::SourceStream and
    net::FilterSourceStream. This is a part of the effort to
    refactor net::Filter into a pull-based interface. The
    complete CL is at crrev.com/1662763002.
    
    BUG=474859
    
    Review-Url: https://codereview.chromium.org/2251853002
    Cr-Commit-Position: refs/heads/master@{#417716}

---
+++ net/filter/source_stream_type_list.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+SOURCE_STREAM_TYPE(BROTLI)
+SOURCE_STREAM_TYPE(DEFLATE)
+SOURCE_STREAM_TYPE(GZIP)
+SOURCE_STREAM_TYPE(SDCH)
+SOURCE_STREAM_TYPE(GZIP_FALLBACK)
+SOURCE_STREAM_TYPE(INVALID)
+SOURCE_STREAM_TYPE(NONE)
