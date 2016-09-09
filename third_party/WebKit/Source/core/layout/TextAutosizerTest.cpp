f7b9f09a6e91bc2ac75a92c8085896332e71c1a8
pdr <pdr@chromium.org>
2016-09-09 15:38:41 -0700

    Temporarily stop honoring text-size-adjust as it breaks accessibility
    This patch temporarily diables text-size-adjust from affecting the
    text autosizer because the accessibility font size adjustment also
    uses the text autosizer and sites should not be able to disable the
    accessibility settings.
    
    This is a temporary change. A proper fix will be coming in a followup.
    
    Text-size-adjust support was added in:
    https://crrev.com/5365b8ad85c2f5cee6034b8baea3c764ed78e1ea
    
    BUG=645269
    
    Review-Url: https://codereview.chromium.org/2330513002
    Cr-Commit-Position: refs/heads/master@{#417738}

--- third_party/WebKit/Source/core/layout/TextAutosizerTest.cpp
+++ third_party/WebKit/Source/core/layout/TextAutosizerTest.cpp
@@ -40,7 +40,7 @@ TEST_F(TextAutosizerTest, SimpleParagraph)
     EXPECT_FLOAT_EQ(40.f, autosized->layoutObject()->style()->computedFontSize());
 }
 
-TEST_F(TextAutosizerTest, TextSizeAdjustDisablesAutosizing)
+TEST_F(TextAutosizerTest, DISABLED_TextSizeAdjustDisablesAutosizing)
 {
     setBodyInnerHTML(
         "<meta name='viewport' content='width=800'>"
@@ -83,7 +83,7 @@ TEST_F(TextAutosizerTest, TextSizeAdjustDisablesAutosizing)
     EXPECT_FLOAT_EQ(16.f, textSizeAdjust100->style()->computedFontSize());
 }
 
-TEST_F(TextAutosizerTest, ParagraphWithChangingTextSizeAdjustment)
+TEST_F(TextAutosizerTest, DISABLED_ParagraphWithChangingTextSizeAdjustment)
 {
     setBodyInnerHTML(
         "<meta name='viewport' content='width=800'>"
@@ -127,7 +127,7 @@ TEST_F(TextAutosizerTest, ParagraphWithChangingTextSizeAdjustment)
     EXPECT_FLOAT_EQ(40.f, autosizedDiv->layoutObject()->style()->computedFontSize());
 }
 
-TEST_F(TextAutosizerTest, ZeroTextSizeAdjustment)
+TEST_F(TextAutosizerTest, DISABLED_ZeroTextSizeAdjustment)
 {
     setBodyInnerHTML(
         "<meta name='viewport' content='width=800'>"
@@ -192,7 +192,7 @@ TEST_F(TextAutosizerTest, TextSizeAdjustmentPixelUnits)
     EXPECT_FLOAT_EQ(40.f, textSizeAdjustPixels->style()->computedFontSize());
 }
 
-TEST_F(TextAutosizerTest, NestedTextSizeAdjust)
+TEST_F(TextAutosizerTest, DISABLED_NestedTextSizeAdjust)
 {
     setBodyInnerHTML(
         "<meta name='viewport' content='width=800'>"
@@ -226,7 +226,7 @@ TEST_F(TextAutosizerTest, NestedTextSizeAdjust)
     EXPECT_FLOAT_EQ(8.48f, textSizeAdjustB->style()->computedFontSize());
 }
 
-TEST_F(TextAutosizerTest, PrefixedTextSizeAdjustIsAlias)
+TEST_F(TextAutosizerTest, DISABLED_PrefixedTextSizeAdjustIsAlias)
 {
     setBodyInnerHTML(
         "<meta name='viewport' content='width=800'>"
