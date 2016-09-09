4f1ac5348c251069342ac30894530cd7ee985e90
dmurph <dmurph@chromium.org>
2016-09-09 13:39:09 -0700

    Revert of Remove obsolete QUIC disabling code. (patchset #4 id:60001 of https://codereview.chromium.org/2318053004/ )
    Reason for revert:
    test build broken on Android
    BUG=645623
    
    Original issue's description:
    > Remove obsolete QUIC disabling code.
    >
    > No longer disable QUIC based on public resets post handshake or packet loss during the handshake. Also remove the "k of n" tracking and per-port disablement.
    >
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    >
    > Committed: https://crrev.com/1c1aa05eaa6b2a04c51e175ed76b5c6f810ff0aa
    > Cr-Commit-Position: refs/heads/master@{#417677}
    
    TBR=eroman@chromium.org,ckrasic@chromium.org,ianswett@chromium.org,rch@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2327923002
    Cr-Commit-Position: refs/heads/master@{#417694}

--- net/quic/chromium/quic_chromium_client_session.cc
+++ net/quic/chromium/quic_chromium_client_session.cc
@@ -238,6 +238,7 @@ QuicChromiumClientSession::QuicChromiumClientSession(
                                        net_log_)),
       going_away_(false),
       port_migration_detected_(false),
+      disabled_reason_(QUIC_DISABLED_NOT),
       token_binding_signatures_(kTokenBindingSignatureMapSize),
       streams_pushed_count_(0),
       streams_pushed_and_claimed_count_(0),
@@ -774,7 +775,8 @@ void QuicChromiumClientSession::OnConfigNegotiated() {
 void QuicChromiumClientSession::OnCryptoHandshakeEvent(
     CryptoHandshakeEvent event) {
   if (stream_factory_ && event == HANDSHAKE_CONFIRMED &&
-      stream_factory_->OnHandshakeConfirmed(this)) {
+      (stream_factory_->OnHandshakeConfirmed(
+          this, logger_->ReceivedPacketLossRate()))) {
     return;
   }
 
@@ -899,6 +901,7 @@ void QuicChromiumClientSession::OnConnectionClosed(
     }
     if (IsCryptoHandshakeConfirmed()) {
       if (GetNumOpenOutgoingStreams() > 0) {
+        disabled_reason_ = QUIC_DISABLED_TIMEOUT_WITH_OPEN_STREAMS;
         UMA_HISTOGRAM_BOOLEAN(
             "Net.QuicSession.TimedOutWithOpenStreams.HasUnackedPackets",
             connection()->sent_packet_manager().HasUnackedPackets());
@@ -936,6 +939,8 @@ void QuicChromiumClientSession::OnConnectionClosed(
           "Net.QuicSession.ConnectionClose.HandshakeFailureUnknown.QuicError",
           error);
     }
+  } else if (error == QUIC_PUBLIC_RESET) {
+    disabled_reason_ = QUIC_DISABLED_PUBLIC_RESET_POST_HANDSHAKE;
   }
 
   UMA_HISTOGRAM_SPARSE_SLOWLY("Net.QuicSession.QuicVersion",
