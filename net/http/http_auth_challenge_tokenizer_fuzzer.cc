afa0ca0ca493c5d6b7f1e6ef2fc2b35c969d13e2
csharrison <csharrison@chromium.org>
2016-09-09 09:58:54 -0700

    Add simple fuzzer for HttpAuthChallengeTokenizer
    BUG=599584
    
    Review-Url: https://codereview.chromium.org/2323913003
    Cr-Commit-Position: refs/heads/master@{#417609}

---
+++ net/http/http_auth_challenge_tokenizer_fuzzer.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include "net/http/http_auth_challenge_tokenizer.h"
+#include "net/http/http_util.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  std::string input(reinterpret_cast<const char*>(data), size);
+  net::HttpAuthChallengeTokenizer tokenizer(input.begin(), input.end());
+  net::HttpUtil::NameValuePairsIterator parameters = tokenizer.param_pairs();
+  while (parameters.GetNext()) {
+  }
+  tokenizer.base64_param();
+  return 0;
+}
