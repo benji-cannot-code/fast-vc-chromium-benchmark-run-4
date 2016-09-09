e9bce180ab9dc8646d193b0b862c31383301c004
dominickn <dominickn@chromium.org>
2016-09-08 20:35:51 -0700

    Add a metric measuring how many times an infobar hid other infobars on Android.
    This CL adds a new UMA metric to record the identifier of an infobar
    which was visible to the user and hid another infobar behind it. This CL
    also updates the visible/hidden check to simply query whether or not
    another infobar exists. This is to correct an overcounting issue that
    recorded infobars which appeared after another infobar was dismissed
    as being hidden.
    
    BUG=589163
    
    Review-Url: https://codereview.chromium.org/2321923002
    Cr-Commit-Position: refs/heads/master@{#417496}

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBar.java
@@ -137,6 +137,10 @@ public abstract class InfoBar implements InfoBarView {
         return false;
     }
 
+    long getNativeInfoBarPtr() {
+        return mNativeInfoBarPtr;
+    }
+
     void setInfoBarContainer(InfoBarContainer container) {
         mContainer = container;
     }
