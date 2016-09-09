1840ac921b231f6353dc43fa10dae811251b2b8d
sashab <sashab@chromium.org>
2016-09-09 02:12:54 -0700

    Added ASSERT_SIZE macro for checking size of size-sensitive classes
    Added ASSERT_SIZE macro for checking size of size-sensitive classes,
    which can be used with the SameSizeAs structs to give better compile
    errors with both the expected and actual size values. Also added the
    assert to the check for SameSizeAsCSSValue.
    
    Patch written with help by timloh@.
    
    BUG=486252
    
    Review-Url: https://codereview.chromium.org/2231953003
    Cr-Commit-Position: refs/heads/master@{#417540}

--- third_party/WebKit/Source/core/css/CSSValue.cpp
+++ third_party/WebKit/Source/core/css/CSSValue.cpp
@@ -63,14 +63,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/CSSValueList.h"
 #include "core/css/CSSValuePair.h"
 #include "core/css/CSSVariableReferenceValue.h"
+#include "wtf/SizeAssertions.h"
 
 namespace blink {
 
 struct SameSizeAsCSSValue : public GarbageCollectedFinalized<SameSizeAsCSSValue> {
     uint32_t bitfields;
 };
-
-static_assert(sizeof(CSSValue) <= sizeof(SameSizeAsCSSValue), "CSSValue should stay small");
+ASSERT_SIZE(CSSValue, SameSizeAsCSSValue);
 
 bool CSSValue::isImplicitInitialValue() const
 {
