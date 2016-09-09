92d94909584b46026ea5217f290842b72b2d86af
eroman <eroman@chromium.org>
2016-09-08 21:01:16 -0700

    Introduce CertErrors::ToDebugString().
    This removes some duplicated code.
    
    BUG=634443
    
    Review-Url: https://codereview.chromium.org/2326923002
    Cr-Commit-Position: refs/heads/master@{#417498}

--- net/cert/internal/cert_errors.h
+++ net/cert/internal/cert_errors.h
@@ -116,6 +116,9 @@ class NET_EXPORT CertErrors {
 
   const std::vector<CertError>& errors() const { return errors_; }
 
+  // Dumps a textual representation of the errors for debugging purposes.
+  std::string ToDebugString() const;
+
  private:
   std::vector<CertError> errors_;
 
--- net/tools/cert_verify_tool/verify_using_path_builder.cc
+++ net/tools/cert_verify_tool/verify_using_path_builder.cc
@@ -120,15 +120,6 @@ std::string SubjectFromTrustAnchor(const net::TrustAnchor* trust_anchor) {
   return SubjectToString(parsed_subject);
 }
 
-void PrintCertErrors(const net::CertErrors& errors) {
-  // TODO(crbug.com/634443): Include more detailed error information. Also this
-  // should likely be extracted to a common location and used by unit-tests and
-  // other debugging needs.
-  for (const auto& error : errors.errors()) {
-    std::cout << " " << error.type << "\n";
-  }
-}
-
 // Dumps a ResultPath to std::cout.
 void PrintResultPath(const net::CertPathBuilder::ResultPath* result_path,
                      size_t index,
@@ -158,7 +149,7 @@ void PrintResultPath(const net::CertPathBuilder::ResultPath* result_path,
   // Print the errors.
   if (!result_path->errors.errors().empty()) {
     std::cout << "Errors:\n";
-    PrintCertErrors(result_path->errors);
+    std::cout << result_path->errors.ToDebugString() << "\n";
   }
 }
 
