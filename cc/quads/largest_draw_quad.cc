f42a394cff37ce1aca42c5e3b3deaf22cc9d7674
dmurph <dmurph@chromium.org>
2016-09-09 14:02:01 -0700

    Revert of Fix kLargestDrawQuadSize (patchset #1 id:1 of https://codereview.chromium.org/2328433005/ )
    Reason for revert:
    Reverting, I think this caused buffer size issues
    https://bugs.chromium.org/p/chromium/issues/detail?id=645634
    BUG=645634
    
    Original issue's description:
    > Fix kLargestDrawQuadSize
    >
    > This should work, but triggered a bug in the past.
    > Trying again, if nothing else I'll find out what problem was triggered.
    >
    > BUG=644443
    > CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    >
    > Committed: https://crrev.com/e099ceca4f5145bf650a2293807c585cc717121f
    > Cr-Commit-Position: refs/heads/master@{#417646}
    
    TBR=danakj@chromium.org,hubbe@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=644443
    
    Review-Url: https://codereview.chromium.org/2326153002
    Cr-Commit-Position: refs/heads/master@{#417708}

--- cc/quads/largest_draw_quad.cc
+++ cc/quads/largest_draw_quad.cc
@@ -20,7 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/quads/yuv_video_draw_quad.h"
 
 namespace {
-const size_t kLargestDrawQuadSize = sizeof(cc::YUVVideoDrawQuad);
+const size_t kLargestDrawQuadSize =
+    sizeof(cc::YUVVideoDrawQuad) + sizeof(gfx::ColorSpace);
 }  // namespace
 
 namespace cc {
