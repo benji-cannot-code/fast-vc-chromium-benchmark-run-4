ad3119e2442c0be95e69120836507b37093c79b2
kbr <kbr@chromium.org>
2016-09-09 15:47:40 -0700

    Deploy Mac ASAN bot on chromium.gpu.fyi waterfall.
    Refactored mb's mixins for the GPU FYI tests and bots to make this
    simpler.
    
    Removed the Mac-10.10 specialization for jobs launched on the MacBook
    Pros with AMD GPUs, to allow them to be upgraded to 10.11.
    
    BUG=641066, 644920
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    TBR=dpranke@chromium.org,zmo@chromium.org
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2320023002
    Cr-Commit-Position: refs/heads/master@{#417744}

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -89,7 +89,7 @@ WATERFALL = {
         {
           'gpu': '1002:6821',
           'hidpi': '1',
-          'os': 'Mac-10.10'
+          'os': 'Mac'
         },
       ],
       'build_config': 'Release',
@@ -101,7 +101,7 @@ WATERFALL = {
         {
           'gpu': '1002:6821',
           'hidpi': '1',
-          'os': 'Mac-10.10'
+          'os': 'Mac'
         },
       ],
       'build_config': 'Debug',
@@ -377,7 +377,7 @@ FYI_WATERFALL = {
         {
           'gpu': '1002:6821',
           'hidpi': '1',
-          'os': 'Mac-10.10'
+          'os': 'Mac'
         },
       ],
       'build_config': 'Release',
@@ -389,7 +389,7 @@ FYI_WATERFALL = {
         {
           'gpu': '1002:6821',
           'hidpi': '1',
-          'os': 'Mac-10.10'
+          'os': 'Mac'
         },
       ],
       'build_config': 'Debug',
@@ -401,12 +401,29 @@ FYI_WATERFALL = {
         {
           'gpu': '1002:6821',
           'hidpi': '1',
-          'os': 'Mac-10.11'
+          'os': 'Mac'
         },
       ],
       'build_config': 'Release',
       # This bot is a one-off for testing purposes.
       'swarming': False,
+      'os_type': 'mac',
+    },
+    'Mac GPU ASAN Release': {
+      # This bot spawns jobs on multiple GPU types.
+      'swarming_dimensions': [
+        {
+          'gpu': '8086:0a2e',
+          'os': 'Mac-10.10'
+        },
+        {
+          'gpu': '1002:6821',
+          'hidpi': '1',
+          'os': 'Mac'
+        },
+      ],
+      'build_config': 'Release',
+      'swarming': True,
       'os_type': 'mac',
     },
     'Linux Release (NVIDIA)': {
@@ -652,7 +669,7 @@ FYI_WATERFALL = {
         {
           'gpu': '1002:6821',
           'hidpi': '1',
-          'os': 'Mac-10.10'
+          'os': 'Mac'
         },
       ],
       'build_config': 'Release',
