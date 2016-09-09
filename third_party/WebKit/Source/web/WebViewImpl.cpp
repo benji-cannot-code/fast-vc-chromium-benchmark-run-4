f8c9f4af052a959e8c2c51ec17010676b0201a54
dglazkov <dglazkov@chromium.org>
2016-09-08 20:14:49 -0700

    Introduce WebRange::isEmpty.
    It's more idiomatic in Blink land.
    
    R=esprehn
    BUG=
    
    Review-Url: https://codereview.chromium.org/2316423002
    Cr-Commit-Position: refs/heads/master@{#417491}

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -2830,10 +2830,7 @@ void WebViewImpl::didChangeWindowResizerRect()
 bool WebViewImpl::getCompositionCharacterBounds(WebVector<WebRect>& bounds)
 {
     WebRange range = compositionRange();
-    if (range.isNull())
-        return false;
-
-    if (range.length() == 0)
+    if (range.isEmpty())
         return false;
 
     WebLocalFrame* frame = focusedFrame();
