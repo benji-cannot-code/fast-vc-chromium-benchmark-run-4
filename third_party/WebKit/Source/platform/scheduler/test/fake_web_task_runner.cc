748f568c6c99930102a20b3fb2d6b6849291ba8c
fs <fs@opera.com>
2016-09-10 14:57:34 -0700

    Revert of Make canceling Timers fast. (patchset #10 id:180001 of https://codereview.chromium.org/2319053004/ )
    Reason for revert:
    Wreaks havoc on the ASAN bots:
    
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20ASAN
    
    STDERR: =================================================================
    STDERR: ==4==ERROR: AddressSanitizer: use-after-poison on address 0x7ed9a5616190 at pc 0x00000768b0da bp 0x7fff4bbca630 sp 0x7fff4bbca628
    STDERR: READ of size 8 at 0x7ed9a5616190 thread T0 (content_shell)
    STDERR:     #0 0x768b0d9 in operator-> third_party/WebKit/Source/wtf/RefPtr.h:68:50
    STDERR:     #1 0x768b0d9 in revokeAll third_party/WebKit/Source/wtf/WeakPtr.h:146:0
    STDERR:     #2 0x768b344 in ?? third_party/WebKit/Source/platform/Timer.cpp:124:22
    STDERR:     #3 0x47a4461 in Run base/callback.h:56:12
    STDERR:     #4 0x47a4461 in RunTask base/debug/task_annotator.cc:54:0
    STDERR:     #5 0x79e2381 in ProcessTaskFromWorkQueue third_party/WebKit/Source/platform/scheduler/base/task_queue_manager.cc:316:19
    ...
    STDERR: AddressSanitizer can not describe address in more detail (wild memory access suspected).
    STDERR: SUMMARY: AddressSanitizer: use-after-poison
    
    Appears to be accessing a "user-poison" area, so maybe a timer in something that was swept? (Wild guess.)
    
    Original issue's description:
    > Make canceling Timers fast.
    >
    > base::Closure recently got an IsCancelled method. Taking advantage of
    > that the scheduler can short circuit a bunch of logic for cancelled
    > tasks and avoid running them and the rest of the task selection
    > machinery.
    >
    > On the new TimerPerfTest benchmark this makes running 10000 cancelled
    > tasks aprox 50x - 60x faster (measured on Android and Linux).
    >
    > Note this patch reverts many of the changes made in
    > https://codereview.chromium.org/2258713004 in favor of
    > WeakPtr based cancellation as favored by the base owners.
    >
    > BUG=605718, 638542
    >
    > Committed: https://crrev.com/e4e5868c5f32b015bf0d07a6eeace892d6a789a1
    > Cr-Commit-Position: refs/heads/master@{#417621}
    
    TBR=jochen@chromium.org,haraken@chromium.org,skyostil@chromium.org,alexclarke@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=605718, 638542
    
    Review-Url: https://codereview.chromium.org/2326313003
    Cr-Commit-Position: refs/heads/master@{#417846}

--- third_party/WebKit/Source/platform/scheduler/test/fake_web_task_runner.cc
+++ third_party/WebKit/Source/platform/scheduler/test/fake_web_task_runner.cc
@@ -16,7 +16,6 @@ class FakeWebTaskRunner::Data : public WTF::ThreadSafeRefCounted<Data> {
   Data() : time_(0.0) {}
 
   std::unique_ptr<Task> task_;
-  base::Closure closure_;
   double time_;
 
  private:
@@ -47,12 +46,6 @@ void FakeWebTaskRunner::postDelayedTask(const WebTraceLocation&,
                                         Task* task,
                                         double) {
   data_->task_.reset(task);
-}
-
-void FakeWebTaskRunner::postDelayedTask(const WebTraceLocation&,
-                                        const base::Closure& closure,
-                                        double) {
-  data_->closure_ = closure;
 }
 
 bool FakeWebTaskRunner::runsTasksOnCurrentThread() {
