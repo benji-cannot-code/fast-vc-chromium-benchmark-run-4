ad71db3f7c17bcb33114883fb8cd464ab68d7c8a
rjkroege <rjkroege@chromium.org>
2016-09-09 19:38:57 -0700

    Remove ash::DisplayManager dependency on ash::Shell
    Continue to sever ash::DisplayManager from its ash dependencies by
    injecting its ash::Shell dependency.
    
    BUG=581462
    
    Review-Url: https://codereview.chromium.org/2320043003
    Cr-Commit-Position: refs/heads/master@{#417806}

--- ash/display/display_manager.h
+++ ash/display/display_manager.h
@@ -29,10 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/chromeos/display_configurator.h"
 #endif
 
-namespace chromeos {
-class DisplayNotificationsTest;
-}
-
 namespace display {
 class DisplayLayoutStore;
 class DisplayObserver;
@@ -45,14 +41,11 @@ class Rect;
 }
 
 namespace ash {
-class AcceleratorControllerTest;
-
 using DisplayInfoList = std::vector<display::ManagedDisplayInfo>;
 
 namespace test {
 class AshTestBase;
 class DisplayManagerTestApi;
-class SystemGestureEventFilterTest;
 }
 
 // DisplayManager maintains the current display configurations,
@@ -81,6 +74,11 @@ class ASH_EXPORT DisplayManager
     // deactivate the active window and set the focus window to NULL.
     virtual void PreDisplayConfigurationChange(bool clear_focus) = 0;
     virtual void PostDisplayConfigurationChange() = 0;
+
+#if defined(OS_CHROMEOS)
+    // Get the ui::DisplayConfigurator.
+    virtual ui::DisplayConfigurator* display_configurator() = 0;
+#endif
   };
 
   // How the second display will be used.
@@ -354,12 +352,9 @@ class ASH_EXPORT DisplayManager
                            NativeDisplaysChangedAfterPrimaryChange);
   FRIEND_TEST_ALL_PREFIXES(DisplayManagerTest, AutomaticOverscanInsets);
   FRIEND_TEST_ALL_PREFIXES(DisplayManagerTest, Rotate);
-  friend class AcceleratorControllerTest;
   friend class DisplayManagerTest;
-  friend class chromeos::DisplayNotificationsTest;
   friend class test::AshTestBase;
   friend class test::DisplayManagerTestApi;
-  friend class test::SystemGestureEventFilterTest;
 
   bool software_mirroring_enabled() const {
     return multi_display_mode_ == MIRRORING;
