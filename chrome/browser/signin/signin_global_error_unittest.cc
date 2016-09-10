2398137c752fff0b054153a4a2a834ae297983a7
glevin <glevin@chromium.org>
2016-09-10 15:37:11 -0700

    Deprecate HOSTED_NOT_ALLOWED auth error
    BUG=640958
    TEST=Just a code cleanup, shouldn't change any behavior.
    
    Review-Url: https://codereview.chromium.org/2314913003
    Cr-Commit-Position: refs/heads/master@{#417847}

--- chrome/browser/signin/signin_global_error_unittest.cc
+++ chrome/browser/signin/signin_global_error_unittest.cc
@@ -140,7 +140,7 @@ TEST_F(SigninGlobalErrorTest, AuthStatusEnumerateAllErrors) {
     { GoogleServiceAuthError::SERVICE_UNAVAILABLE, true },
     { GoogleServiceAuthError::TWO_FACTOR, true },
     { GoogleServiceAuthError::REQUEST_CANCELED, true },
-    { GoogleServiceAuthError::HOSTED_NOT_ALLOWED, true },
+    { GoogleServiceAuthError::HOSTED_NOT_ALLOWED_DEPRECATED, false },
     { GoogleServiceAuthError::UNEXPECTED_SERVICE_RESPONSE, true },
     { GoogleServiceAuthError::SERVICE_ERROR, true },
     { GoogleServiceAuthError::WEB_LOGIN_REQUIRED, true },
@@ -152,6 +152,8 @@ TEST_F(SigninGlobalErrorTest, AuthStatusEnumerateAllErrors) {
   testing_profile_manager()->UpdateLastUser(profile());
 
   for (size_t i = 0; i < arraysize(table); ++i) {
+    if (GoogleServiceAuthError::IsDeprecated(table[i].error_state))
+      continue;
     base::HistogramTester histogram_tester;
     FakeAuthStatusProvider provider(error_controller());
     provider.SetAuthError(kTestAccountId,
