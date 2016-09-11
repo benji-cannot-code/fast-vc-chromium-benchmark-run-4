851804d2464ac3e3a60cca4fecea4d1b98aa31e6
flackr <flackr@chromium.org>
2016-09-11 05:03:12 -0700

    Reland Paint solid color backgrounds which are equivalent to locally attached into scrolling contents layer (patchset #9 id:160001 of https://codereview.chromium.org/2264663002/ )
    If we have a solid color background whose bounds / visible rect is equivalent to the padding-box we can treat it as locally attached and paint it into the scrolling contents layer. This enables composited scrolling with sub-pixel text anti-aliasing.
    
    TBR=chrishtr
    BUG=381840
    TEST=CompositedLayerMappingTest.ShouldPaintBackgroundOntoScrollingContentsLayer
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2302743004
    Cr-Commit-Position: refs/heads/master@{#417858}

--- third_party/WebKit/Source/core/paint/PaintLayer.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayer.cpp
@@ -2361,7 +2361,7 @@ bool PaintLayer::shouldPaintBackgroundOntoScrollingContentsLayer() const
 {
     return !isRootLayer()
         && scrollsOverflow()
-        && layoutObject()->style()->hasEntirelyLocalBackground()
+        && layoutObject()->hasLocalEquivalentBackground()
         && !stackingNode()->hasNegativeZOrderList();
 }
 
