58b65ec1edadc98af02adafb066fcdc3b4666dc3
zmo <zmo@chromium.org>
2016-09-11 15:13:52 -0700

    Roll WebGL afd388c..c9d4d25
    https://chromium.googlesource.com/external/khronosgroup/webgl.git/+log/afd388c..c9d4d25
    
    BUG=
    NOTRY=true
    TEST=bots
    TBR=kbr@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2331823002
    Cr-Commit-Position: refs/heads/master@{#417862}

--- content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
@@ -44,12 +44,10 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
     self.Fail('conformance2/rendering/blitframebuffer-outside-readbuffer.html',
         bug=664740)
     self.Flaky('conformance2/query/occlusion-query.html', bug=603168)
-    self.Fail('conformance2/misc/expando-loss-2.html', bug=483282)
     self.Fail('conformance2/rendering/blitframebuffer-test.html', bug=634525)
 
     # Windows only.
-    self.Fail('conformance2/textures/misc/copy-texture-image-luma-format.html',
-        ['win'], bug=638470)
+    # We are awesome!
 
     # Win / NVidia
     self.Flaky('deqp/functional/gles3/fbomultisample*',
@@ -70,8 +68,6 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
         ['win', ('amd', 0x6779)], bug=626068)
 
     self.Fail('deqp/functional/gles3/shadercommonfunction.html',
-        ['win', ('amd', 0x6779)], bug=483282)
-    self.Fail('deqp/functional/gles3/fbomultisample.8_samples.html',
         ['win', ('amd', 0x6779)], bug=483282)
     self.Fail('deqp/functional/gles3/multisample.html',
         ['win', ('amd', 0x6779)], bug=617290)
@@ -127,8 +123,6 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
         ['win', 'intel'], bug=614418)
     self.Flaky('deqp/functional/gles3/textureformat/unsized_3d.html',
         ['win', 'intel'], bug=614418)
-    self.Fail('deqp/functional/gles3/shadertexturefunction/texture.html',
-        ['win', 'intel'], bug=483282)
     self.Fail('deqp/functional/gles3/shadertexturefunction/' +
         'texelfetchoffset.html',
         ['win', 'intel'], bug=483282)
@@ -155,6 +149,8 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
     # Fixed on OSX 10.11
     self.Fail('deqp/functional/gles3/shaderloop_do_while.html',
         ['mac'], bug=617820)
+    self.Fail('deqp/functional/gles3/shaderoperator/common_functions.html',
+        ['mac', 'amd'], bug=483282)
 
     self.Fail('conformance2/rendering/' +
         'framebuffer-completeness-unaffected.html',
@@ -371,14 +367,10 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
     self.Fail('deqp/functional/gles3/shaderoperator/' +
         'angle_and_trigonometry_03.html',
         ['mac', 'amd'], bug=483282)
-    self.Fail('deqp/functional/gles3/shaderoperator/common_functions.html',
-        ['mac', 'amd'], bug=483282)
     self.Fail('deqp/functional/gles3/shaderoperator/geometric.html',
         ['mac', 'amd'], bug=483282)
 
     self.Flaky('deqp/functional/gles3/shaderindexing/mat_02.html',
-        ['mac', 'amd'], bug=644360)
-    self.Fail('conformance2/textures/misc/tex-mipmap-levels.html',
         ['mac', 'amd'], bug=644360)
 
     # Mac Pro with AMD GPU
@@ -417,9 +409,6 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
         'texsubimage3d_pbo_params.html',
         ['mac', 'intel'], bug=483282)
 
-    self.Fail('deqp/functional/gles3/shadertexturefunction/' +
-        'texture.html',
-        ['mac', 'intel'], bug=483282)
     self.Fail('deqp/functional/gles3/shadertexturefunction/' +
         'texturelod.html',
         ['mac', 'intel'], bug=483282)
