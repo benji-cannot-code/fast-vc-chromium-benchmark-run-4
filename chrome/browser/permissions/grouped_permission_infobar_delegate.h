ce480171369d42ef7137f9a61d70b6dd71b49434
dominickn <dominickn@chromium.org>
2016-09-12 00:00:46 -0700

    Revert of Enable the optional permission prompt persistence toggle on grouped infobars (patchset #13 id:240001 of https://codereview.chromium.org/2254763002/ )
    Reason for revert:
    This still breaks the Android Tests bot.
    
    https://uberchromegw.corp.google.com/i/chromium.linux/builders/Android%20Tests/builds/31799
    
    Original issue's description:
    > Enable the optional permission prompt persistence toggle on grouped infobars
    >
    > This CL adds support for the feature enabling a persistence toggle on
    > permission prompts to grouped permission infobars. The toggle is only
    > shown if an enabling feature is active. Grouped bars are used exclusively
    > for media permissions, so this change allows the toggle to be shown
    > when a single and multiple media permissions are requested.
    >
    > New Android UI tests for responding to the permission prompts triggered
    > by getUserMedia are added to verify the toggle behaviour. The persistence
    > boolean in media_stream_devices_controller is renamed in preparation for
    > upstreaming it into PermissionRequest.
    >
    > BUG=632269
    >
    > Committed: https://crrev.com/fea9c39c10d391e8b03b77b39b889c54a96a9b5c
    > Committed: https://crrev.com/c23b2a5192ee77dd842b3e79575d586743364b32
    > Cr-Original-Commit-Position: refs/heads/master@{#413970}
    > Cr-Commit-Position: refs/heads/master@{#417870}
    
    TBR=dfalcantara@chromium.org,raymes@chromium.org,kcarattini@chromium.org,xhwang@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=632269
    
    Review-Url: https://codereview.chromium.org/2332663002
    Cr-Commit-Position: refs/heads/master@{#417881}

--- chrome/browser/permissions/grouped_permission_infobar_delegate.h
+++ chrome/browser/permissions/grouped_permission_infobar_delegate.h
@@ -34,10 +34,6 @@ class GroupedPermissionInfoBarDelegate : public ConfirmInfoBarDelegate {
       const std::vector<ContentSettingsType>& types);
   ~GroupedPermissionInfoBarDelegate() override;
 
-  bool ShouldShowPersistenceToggle() const;
-  bool persist() const { return persist_; }
-  void set_persist(bool persist) { persist_ = persist; }
-
   base::string16 GetMessageText() const override;
 
   int GetPermissionCount() const;
@@ -62,7 +58,6 @@ class GroupedPermissionInfoBarDelegate : public ConfirmInfoBarDelegate {
   const GURL requesting_origin_;
   const std::vector<ContentSettingsType> types_;
   std::vector<bool> accept_states_;
-  bool persist_;
 
   DISALLOW_COPY_AND_ASSIGN(GroupedPermissionInfoBarDelegate);
 };
