46745883171f4aa619ea1ce73d5ba9fc57abc832
sadrul <sadrul@chromium.org>
2016-09-12 14:48:05 -0700

    services/ui: Use new gpu::GpuInit to do the GPU initialization.
    BUG=643746
    
    Review-Url: https://codereview.chromium.org/2329543002
    Cr-Commit-Position: refs/heads/master@{#418062}

--- services/ui/gpu/gpu_service_internal.h
+++ services/ui/gpu/gpu_service_internal.h
@@ -23,11 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/ui/gpu/interfaces/gpu_service_internal.mojom.h"
 #include "ui/gfx/native_widget_types.h"
 
-namespace base {
-template <typename T>
-struct DefaultSingletonTraits;
-}
-
 namespace gpu {
 class GpuChannelHost;
 class GpuMemoryBufferFactory;
@@ -41,6 +36,8 @@ class MediaService;
 
 namespace ui {
 
+class GpuMain;
+
 // This runs in the GPU process, and communicates with the gpu host (which is
 // the window server) over the mojom APIs. This is responsible for setting up
 // the connection to clients, allocating/free'ing gpu memory etc.
@@ -48,17 +45,16 @@ class GpuServiceInternal : public gpu::GpuChannelManagerDelegate,
                            public mojom::GpuServiceInternal,
                            public base::NonThreadSafe {
  public:
-  void Add(mojom::GpuServiceInternalRequest request);
+  ~GpuServiceInternal() override;
 
-  // TODO(sad): This should not be a singleton.
-  static GpuServiceInternal* GetInstance();
+  void Add(mojom::GpuServiceInternalRequest request);
 
   gpu::GpuChannelManager* gpu_channel_manager() const {
     return gpu_channel_manager_.get();
   }
 
   gpu::GpuMemoryBufferFactory* gpu_memory_buffer_factory() const {
-    return gpu_memory_buffer_factory_.get();
+    return gpu_memory_buffer_factory_;
   }
 
   // TODO(sad): These should be mojom API.
@@ -74,10 +70,11 @@ class GpuServiceInternal : public gpu::GpuChannelManagerDelegate,
                               const gpu::SyncToken& sync_token);
 
  private:
-  friend struct base::DefaultSingletonTraits<GpuServiceInternal>;
+  friend class GpuMain;
 
-  GpuServiceInternal();
-  ~GpuServiceInternal() override;
+  GpuServiceInternal(const gpu::GPUInfo& gpu_info,
+                     gpu::GpuWatchdogThread* watchdog,
+                     gpu::GpuMemoryBufferFactory* memory_buffer_factory);
 
   void EstablishGpuChannelInternal(int32_t client_id,
                                    uint64_t client_tracing_id,
@@ -138,7 +135,7 @@ class GpuServiceInternal : public gpu::GpuChannelManagerDelegate,
   // The thread that handles IO events for GpuService.
   base::Thread io_thread_;
 
-  scoped_refptr<gpu::GpuWatchdogThread> watchdog_thread_;
+  gpu::GpuWatchdogThread* watchdog_thread_;
 
   std::unique_ptr<gpu::SyncPointManager> owned_sync_point_manager_;
 
@@ -146,7 +143,7 @@ class GpuServiceInternal : public gpu::GpuChannelManagerDelegate,
 
   std::unique_ptr<media::MediaService> media_service_;
 
-  std::unique_ptr<gpu::GpuMemoryBufferFactory> gpu_memory_buffer_factory_;
+  gpu::GpuMemoryBufferFactory* gpu_memory_buffer_factory_;
 
   gpu::GpuPreferences gpu_preferences_;
 
