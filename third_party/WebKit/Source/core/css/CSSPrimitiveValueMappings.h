ca53d4ff3b84507aeb5671260de21415560d80b0
kdsilva <kdsilva@google.com>
2016-09-12 12:20:36 -0700

    Adding a download button to the media player controls.
    This CL contains the logic behind a runtime enabled flag to display a download button for downloadable files. It does not contain the full implementation to download the media.
    
    BUG=601247
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2293273002
    Cr-Commit-Position: refs/heads/master@{#417996}

--- third_party/WebKit/Source/core/css/CSSPrimitiveValueMappings.h
+++ third_party/WebKit/Source/core/css/CSSPrimitiveValueMappings.h
@@ -469,6 +469,9 @@ template<> inline CSSPrimitiveValue::CSSPrimitiveValue(ControlPart e)
     case MediaOverflowMenuButtonPart:
         m_value.valueID = CSSValueInternalOverflowMenuButton;
         break;
+    case MediaDownloadIconPart:
+        m_value.valueID = CSSValueInternalMediaDownloadIcon;
+        break;
     case MenulistPart:
         m_value.valueID = CSSValueMenulist;
         break;
--- third_party/WebKit/Source/core/html/shadow/MediaControlElementTypes.h
+++ third_party/WebKit/Source/core/html/shadow/MediaControlElementTypes.h
@@ -71,6 +71,7 @@ enum MediaControlElementType {
     MediaOverlayCastOnButton,
     MediaOverflowButton,
     MediaOverflowList,
+    MediaDownloadButton,
 };
 
 CORE_EXPORT const HTMLMediaElement* toParentMediaElement(const Node*);
