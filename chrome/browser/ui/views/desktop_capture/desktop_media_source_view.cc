680573d4acebdd17b871644a53baf13d8502c0ce
qiangchen <qiangchen@chromium.org>
2016-09-12 10:21:41 -0700

    Desktop Capture Picker: More Clear Selection
    We set border color for source item to indicate it is selected.
    But the color is just light grey on Windows and ChromeOS, and thus
    hardly visible.
    
    We changed to use kColorId_FocusedBorderColor, which looks good
    on all platforms.
    
    BUG=643292
    
    Review-Url: https://codereview.chromium.org/2323913002
    Cr-Commit-Position: refs/heads/master@{#417964}

--- chrome/browser/ui/views/desktop_capture/desktop_media_source_view.cc
+++ chrome/browser/ui/views/desktop_capture/desktop_media_source_view.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_view_state.h"
 #include "ui/gfx/canvas.h"
 #include "ui/native_theme/native_theme.h"
-#include "ui/views/background.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
@@ -96,7 +95,7 @@ void DesktopMediaSourceView::SetSelected(bool selected) {
     }
 
     const SkColor border_color = GetNativeTheme()->GetSystemColor(
-        ui::NativeTheme::kColorId_FocusedMenuItemBackgroundColor);
+        ui::NativeTheme::kColorId_FocusedBorderColor);
     image_view_->SetBorder(views::Border::CreateSolidBorder(
         style_.selection_border_thickness, border_color));
     label_->SetFontList(label_->font_list().Derive(0, gfx::Font::NORMAL,
@@ -111,19 +110,6 @@ void DesktopMediaSourceView::SetSelected(bool selected) {
   SchedulePaint();
 }
 
-void DesktopMediaSourceView::SetHovered(bool hovered) {
-  if (hovered) {
-    // Use background color to show mouse hover.
-    const SkColor bg_color = GetNativeTheme()->GetSystemColor(
-        ui::NativeTheme::kColorId_HoverMenuItemBackgroundColor);
-    set_background(views::Background::CreateSolidBackground(bg_color));
-  } else {
-    set_background(nullptr);
-  }
-
-  SchedulePaint();
-}
-
 const char* DesktopMediaSourceView::GetClassName() const {
   return DesktopMediaSourceView::kDesktopMediaSourceViewClassName;
 }
@@ -133,7 +119,7 @@ void DesktopMediaSourceView::SetStyle(DesktopMediaSourceViewStyle style) {
   image_view_->SetBoundsRect(style_.image_rect);
   if (selected_) {
     const SkColor border_color = GetNativeTheme()->GetSystemColor(
-        ui::NativeTheme::kColorId_FocusedMenuItemBackgroundColor);
+        ui::NativeTheme::kColorId_FocusedBorderColor);
     image_view_->SetBorder(views::Border::CreateSolidBorder(
         style_.selection_border_thickness, border_color));
   }
@@ -195,14 +181,6 @@ bool DesktopMediaSourceView::OnMousePressed(const ui::MouseEvent& event) {
     parent_->OnDoubleClick();
   }
   return true;
-}
-
-void DesktopMediaSourceView::OnMouseEntered(const ui::MouseEvent& event) {
-  SetHovered(true);
-}
-
-void DesktopMediaSourceView::OnMouseExited(const ui::MouseEvent& event) {
-  SetHovered(false);
 }
 
 void DesktopMediaSourceView::OnGestureEvent(ui::GestureEvent* event) {
