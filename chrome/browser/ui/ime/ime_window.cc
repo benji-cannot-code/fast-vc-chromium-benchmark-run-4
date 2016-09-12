57e22091db668f357fcb92e1d4925108139b24b9
shuchen <shuchen@chromium.org>
2016-09-12 03:34:07 -0700

    Finds the nearest display to show the follow-cursor IME window, instead of  using the primary one.
    BUG=645842
    
    Review-Url: https://codereview.chromium.org/2335503002
    Cr-Commit-Position: refs/heads/master@{#417912}

--- chrome/browser/ui/ime/ime_window.cc
+++ chrome/browser/ui/ime/ime_window.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 #include "extensions/common/manifest_handlers/icons_handler.h"
 #include "ui/display/display.h"
+#include "ui/display/display_finder.h"
 #include "ui/display/screen.h"
 #include "ui/gfx/image/image.h"
 
@@ -102,10 +103,12 @@ void ImeWindow::FollowCursor(const gfx::Rect& cursor_bounds) {
     return;
 
   gfx::Rect screen_bounds =
-      display::Screen::GetScreen()->GetPrimaryDisplay().bounds();
+      display::FindDisplayNearestPoint(
+          display::Screen::GetScreen()->GetAllDisplays(),
+          gfx::Point(cursor_bounds.x(), cursor_bounds.y()))->bounds();
   gfx::Rect window_bounds = native_window_->GetBounds();
-  int screen_width = screen_bounds.width();
-  int screen_height = screen_bounds.height();
+  int screen_width = screen_bounds.x() + screen_bounds.width();
+  int screen_height = screen_bounds.y() + screen_bounds.height();
   int width = window_bounds.width();
   int height = window_bounds.height();
   // By default, aligns the left of the window client area to the left of the
