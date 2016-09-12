e47207cff515d45612739c4ab49927c2239f85b7
xlai <xlai@chromium.org>
2016-09-12 09:17:32 -0700

    Disallow resizing on OffscreenCanvas that has been transferred control from canvas
    In my previous patch (https://codereview.chromium.org/2294963002/)
    I had some primitive ideas about disallowing resizing on
    OffscreenCanvas. This CL makes it more assertive by throwing
    exception (not just causing no effect) to alert users.
    
    BUG=563852
    
    Review-Url: https://codereview.chromium.org/2330553002
    Cr-Commit-Position: refs/heads/master@{#417951}

--- third_party/WebKit/Source/core/offscreencanvas/OffscreenCanvas.h
+++ third_party/WebKit/Source/core/offscreencanvas/OffscreenCanvas.h
@@ -31,8 +31,8 @@ public:
     // IDL attributes
     unsigned width() const { return m_size.width(); }
     unsigned height() const { return m_size.height(); }
-    void setWidth(unsigned);
-    void setHeight(unsigned);
+    void setWidth(unsigned, ExceptionState&);
+    void setHeight(unsigned, ExceptionState&);
 
     // API Methods
     ImageBitmap* transferToImageBitmap(ExceptionState&);
