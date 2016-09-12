f70b80f40da2f721931dd2681076b4c3c38b19bb
thomasanderson <thomasanderson@google.com>
2016-09-12 12:37:49 -0700

    This CL renames XForeignWindowManager to XWindowEventManager since it will be used on local windows now.  Get rid of OnWindowDestroyed since this will be used by the XWindowCache for windows other than top-level windows as well. Finally, refactor the old Request implementation to use a Mask instead.
    This is needed by the XWindowCache which will select events on all windows, and
    therefore needs to know which masks we've already selected on Chrome windows.
    
    This will be the first in the line of 3 CLs.
    1. Refactor X11ForeignWindowManager
    2. Update Chrome code to use the XWindowManager instead of calling XSelectInput
    manually
    3. Add a PRESUBMIT check to make sure CWEventMask, XSelectInput, or
    XCB_CW_EVENT_MASK is not added to new code.
    
    BUG=634084
    
    Committed: https://crrev.com/5c04e8aa6732cca81e518dacf06c48398426b415
    Review-Url: https://codereview.chromium.org/2313033002
    Cr-Original-Commit-Position: refs/heads/master@{#417793}
    Cr-Commit-Position: refs/heads/master@{#418005}

--- ui/base/x/selection_owner.cc
+++ ui/base/x/selection_owner.cc
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "ui/base/x/selection_utils.h"
-#include "ui/base/x/x11_foreign_window_manager.h"
 #include "ui/base/x/x11_util.h"
+#include "ui/base/x/x11_window_event_manager.h"
 #include "ui/events/platform/x11/x11_event_source.h"
 
 namespace ui {
@@ -270,17 +270,10 @@ bool SelectionOwner::ProcessTarget(XAtom target,
       base::TimeTicks timeout =
           base::TimeTicks::Now() +
           base::TimeDelta::FromMilliseconds(kIncrementalTransferTimeoutMs);
-      int foreign_window_manager_id =
-          ui::XForeignWindowManager::GetInstance()->RequestEvents(
-              requestor, PropertyChangeMask);
-      incremental_transfers_.push_back(
-          IncrementalTransfer(requestor,
-                              target,
-                              property,
-                              it->second,
-                              0,
-                              timeout,
-                              foreign_window_manager_id));
+      requestor_events_.reset(
+          new ui::XScopedEventSelector(requestor, PropertyChangeMask));
+      incremental_transfers_.push_back(IncrementalTransfer(
+          requestor, target, property, it->second, 0, timeout));
 
       // Start a timer to abort the data transfer in case that the selection
       // requestor does not support the INCR property or gets destroyed during
@@ -346,8 +339,8 @@ void SelectionOwner::AbortStaleIncrementalTransfers() {
 
 void SelectionOwner::CompleteIncrementalTransfer(
     std::vector<IncrementalTransfer>::iterator it) {
-  ui::XForeignWindowManager::GetInstance()->CancelRequest(
-      it->foreign_window_manager_id);
+  requestor_events_.reset();
+
   incremental_transfers_.erase(it);
 
   if (incremental_transfers_.empty())
@@ -374,16 +367,13 @@ SelectionOwner::IncrementalTransfer::IncrementalTransfer(
     XAtom property,
     const scoped_refptr<base::RefCountedMemory>& data,
     int offset,
-    base::TimeTicks timeout,
-    int foreign_window_manager_id)
+    base::TimeTicks timeout)
     : window(window),
       target(target),
       property(property),
       data(data),
       offset(offset),
-      timeout(timeout),
-      foreign_window_manager_id(foreign_window_manager_id) {
-}
+      timeout(timeout) {}
 
 SelectionOwner::IncrementalTransfer::IncrementalTransfer(
     const IncrementalTransfer& other) = default;
