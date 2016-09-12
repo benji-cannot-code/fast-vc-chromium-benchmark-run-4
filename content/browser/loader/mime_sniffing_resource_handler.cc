f5c342aa141ea14363890ec11cb59a18dab5577e
mkwst <mkwst@chromium.org>
2016-09-12 03:32:36 -0700

    Send an image-like 'Accept' header for favicons.
    Requests for images send an `Accept` header of `image/webp,image/*,*/*;q=0.8`.
    Favicon requests currently send `*/*`, which seems like an oversight. This
    patch aligns the latter with the former.
    
    BUG=645901
    R=jochen@chromium.org, japhet@chromium.org
    
    Review-Url: https://codereview.chromium.org/2303443005
    Cr-Commit-Position: refs/heads/master@{#417911}

--- content/browser/loader/mime_sniffing_resource_handler.cc
+++ content/browser/loader/mime_sniffing_resource_handler.cc
@@ -107,6 +107,7 @@ bool MimeSniffingResourceHandler::OnWillStart(const GURL& url, bool* defer) {
     case RESOURCE_TYPE_STYLESHEET:
       accept_value = kStylesheetAcceptHeader;
       break;
+    case RESOURCE_TYPE_FAVICON:
     case RESOURCE_TYPE_IMAGE:
       accept_value = kImageAcceptHeader;
       break;
@@ -118,7 +119,6 @@ bool MimeSniffingResourceHandler::OnWillStart(const GURL& url, bool* defer) {
     case RESOURCE_TYPE_WORKER:
     case RESOURCE_TYPE_SHARED_WORKER:
     case RESOURCE_TYPE_PREFETCH:
-    case RESOURCE_TYPE_FAVICON:
     case RESOURCE_TYPE_XHR:
     case RESOURCE_TYPE_PING:
     case RESOURCE_TYPE_SERVICE_WORKER:
