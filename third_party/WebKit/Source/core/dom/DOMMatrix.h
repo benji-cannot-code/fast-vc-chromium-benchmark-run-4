297559505c13ffe72d05ca3488d1c29f19402c3e
hs1217.lee <hs1217.lee@samsung.com>
2016-09-12 08:27:09 -0700

    [GeometryInterface] Add fromFloat32Array & fromFloat64Array function
    Return the result of invoking create a matrix of type
    DOMMatrixReadOnly or DOMMatrix as appropriate, with a sequence of
    numbers taking the values from Float32Array or Float64Array in the provided order.
    If array has 6 elements, Return a 2d matrix.
    If array has 16 elements, Return a 3d matrix.
    Otherwise, Throw a TypeError exception.
    
    spec list:
    https://drafts.fxtf.org/geometry/#dom-dommatrixreadonly-fromfloat32array
    https://drafts.fxtf.org/geometry/#dom-dommatrixreadonly-fromfloat64array
    https://drafts.fxtf.org/geometry/#dom-dommatrix-fromfloat32array
    https://drafts.fxtf.org/geometry/#dom-dommatrix-fromfloat64array
    
    BUG=388780
    
    Review-Url: https://codereview.chromium.org/2309013002
    Cr-Commit-Position: refs/heads/master@{#417943}

--- third_party/WebKit/Source/core/dom/DOMMatrix.h
+++ third_party/WebKit/Source/core/dom/DOMMatrix.h
@@ -16,6 +16,8 @@ public:
     static DOMMatrix* create();
     static DOMMatrix* create(DOMMatrixReadOnly*);
     static DOMMatrix* create(const SkMatrix44&);
+    static DOMMatrix* fromFloat32Array(DOMFloat32Array*, ExceptionState&);
+    static DOMMatrix* fromFloat64Array(DOMFloat64Array*, ExceptionState&);
 
     void setA(double value) { m_matrix->setM11(value); }
     void setB(double value) { m_matrix->setM12(value); }
@@ -54,6 +56,8 @@ public:
 
 private:
     DOMMatrix(const TransformationMatrix&, bool is2D = true);
+    template <typename T>
+    DOMMatrix(T sequence, int size);
 
     void setIs2D(bool value);
 };
