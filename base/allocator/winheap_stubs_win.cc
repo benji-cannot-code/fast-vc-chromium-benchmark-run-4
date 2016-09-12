aee5035d7bc0868ddfa227cb936ef5aa0c34e6a5
siggi <siggi@chromium.org>
2016-09-12 11:41:49 -0700

    Fix SyzyASAN build, by removing base/bits.h include.
    This include would otherwise introduce a circular build file dependency
    under the is_syzyasan configuration.
    
    BUG=645819
    
    Review-Url: https://codereview.chromium.org/2331973002
    Cr-Commit-Position: refs/heads/master@{#417982}

--- base/allocator/winheap_stubs_win.cc
+++ base/allocator/winheap_stubs_win.cc
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <new.h>
 #include <windows.h>
 
-#include "base/bits.h"
-
 namespace base {
 namespace allocator {
 
@@ -72,7 +70,7 @@ size_t WinHeapGetSizeEstimate(void* ptr) {
   const size_t kAllocationGranularity = 8;
 #endif
 
-  return base::bits::Align(size, kAllocationGranularity);
+  return (size + kAllocationGranularity - 1) & ~(kAllocationGranularity - 1);
 }
 
 // Call the new handler, if one has been set.
