9c7aced097e27160c4932d6976def4f3d79eee37
sklencarova <sklencarova@google.com>
2016-09-12 03:20:34 -0700

    Change QR Scanner experimental setting to be three-valued.
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2322953003
    Cr-Commit-Position: refs/heads/master@{#417906}

--- ios/chrome/browser/experimental_flags.mm
+++ ios/chrome/browser/experimental_flags.mm
@@ -29,7 +29,6 @@ NSString* const kHeuristicsForPasswordGeneration =
     @"HeuristicsForPasswordGeneration";
 NSString* const kEnableReadingList = @"EnableReadingList";
 NSString* const kUpdatePasswordUIDisabled = @"UpdatePasswordUIDisabled";
-NSString* const kEnableQRCodeReader = @"EnableQRCodeReader";
 NSString* const kEnableNewClearBrowsingDataUI = @"EnableNewClearBrowsingDataUI";
 }  // namespace
 
@@ -147,12 +146,17 @@ bool IsUpdatePasswordUIEnabled() {
 
 bool IsQRCodeReaderEnabled() {
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-  if (command_line->HasSwitch(switches::kEnableQRScanner))
+  if (command_line->HasSwitch(switches::kEnableQRScanner)) {
     return true;
+  } else if (command_line->HasSwitch(switches::kDisableQRScanner)) {
+    return false;
+  }
 
   // Check if the finch experiment is turned on.
-  return [[NSUserDefaults standardUserDefaults]
-      boolForKey:kEnableQRCodeReader];
+  std::string group_name =
+      base::FieldTrialList::FindFullName("QRScannerEnabled");
+  return base::StartsWith(group_name, "Enabled",
+                          base::CompareCase::INSENSITIVE_ASCII);
 }
 
 bool IsNewClearBrowsingDataUIEnabled() {
