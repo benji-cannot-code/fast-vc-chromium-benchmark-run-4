74516d33e64c27e8b2c78c636050ff18fb78179c
nyquist <nyquist@chromium.org>
2016-09-12 15:52:16 -0700

    Update blimp documentation to refer to Chrome
    Up until now the blimp client code has only been living in the
    //blimp directory, but now there is support for using the embedder
    from //chrome.
    
    This CL updates the documentation to refer to using the
    Chrome Public APK instead of Blimp APK in most places, and also updates
    the client-engine integration test script.
    
    It also updates some outdated documentation about markdown and testing
    in general.
    
    BUG=597756
    
    Review-Url: https://codereview.chromium.org/2323993003
    Cr-Commit-Position: refs/heads/master@{#418087}

--- blimp/tools/client_engine_integration.py
+++ blimp/tools/client_engine_integration.py
@@ -38,7 +38,7 @@ from devil.android.sdk import version_codes
 from devil.utils import cmd_helper
 
 _CLIENT_TOKEN_PATH = posixpath.join('/', 'data', 'data',
-                                    'org.chromium.blimp',
+                                    'org.chromium.chrome',
                                     'blimp_client_token')
 _TOKEN_FILE_PATH = os.path.join(SRC_PATH, 'blimp', 'test', 'data',
                                 'test_client_token')
@@ -70,8 +70,8 @@ def RunClient(device, optional_url):
   """
   run_client_intent = intent.Intent(
       action='android.intent.action.VIEW',
-      package='org.chromium.blimp',
-      activity='org.chromium.blimp.BlimpRendererActivity',
+      package='org.chromium.chrome',
+      activity='com.google.android.apps.chrome.Main',
       data=optional_url)
   device.StartActivity(run_client_intent, blocking=True)
 
@@ -121,7 +121,7 @@ def RunEngine(output_linux_directory, token_file_path, device):
 
 
 def SetCommandFlag(device, engine_ip, engine_port):
-  """Set up adb Blimp command line flags
+  """Set up adb Chrome command line flags
 
   Args:
     device: (str) Serial number of device we should use.
@@ -130,8 +130,9 @@ def SetCommandFlag(device, engine_ip, engine_port):
   """
   cmd_helper.GetCmdStatusAndOutput([
       os.path.join(SRC_PATH, 'build', 'android',
-                   'adb_blimp_command_line'),
+                   'adb_chrome_command_line'),
       '--device=' + str(device),
+      '--enable-blimp',
       '--engine-ip=' + engine_ip,
       '--engine-port=' + engine_port,
       '--engine-transport=tcp',
@@ -237,8 +238,8 @@ def _Load(args, json_file_path, device): # pylint: disable=unused-argument
                  reinstall=True)
   run_client_intent = intent.Intent(
       action='android.intent.action.VIEW',
-      package='org.chromium.blimp',
-      activity='org.chromium.blimp.BlimpRendererActivity',
+      package='org.chromium.chrome',
+      activity='com.google.android.apps.chrome.Main',
       data=args.optional_url)
   device.StartActivity(run_client_intent, blocking=True)
 
@@ -329,4 +330,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-
