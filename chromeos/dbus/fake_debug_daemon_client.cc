78d60c7ec2e511f1d3474bd2b5d905501e0dfb65
skau <skau@chromium.org>
2016-09-12 11:43:59 -0700

    Add CupsAdd and CupsRemove operations to debug daemon client.
    lpadmin is being invoked over dbus using debugd on CrOS.  This will facilitate Chrome calling lpadmin to administer printers.
    
    BUG=616855
    TEST=Compiles.  Passes CQ.  Verified with test integration w/ UI.
    
    Review-Url: https://codereview.chromium.org/2232203003
    Cr-Commit-Position: refs/heads/master@{#417983}

--- chromeos/dbus/fake_debug_daemon_client.cc
+++ chromeos/dbus/fake_debug_daemon_client.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/location.h"
 #include "base/single_thread_task_runner.h"
+#include "base/stl_util.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chromeos/chromeos_switches.h"
 
@@ -216,6 +217,30 @@ void FakeDebugDaemonClient::SetServiceIsAvailable(bool is_available) {
   callbacks.swap(pending_wait_for_service_to_be_available_callbacks_);
   for (size_t i = 0; i < callbacks.size(); ++i)
     callbacks[i].Run(is_available);
+}
+
+void FakeDebugDaemonClient::CupsAddPrinter(
+    const std::string& name,
+    const std::string& uri,
+    const std::string& ppd_path,
+    bool ipp_everywhere,
+    const DebugDaemonClient::CupsAddPrinterCallback& callback,
+    const base::Closure& error_callback) {
+  printers_.insert(name);
+  base::ThreadTaskRunnerHandle::Get()->PostTask(FROM_HERE,
+                                                base::Bind(callback, true));
+}
+
+void FakeDebugDaemonClient::CupsRemovePrinter(
+    const std::string& name,
+    const DebugDaemonClient::CupsRemovePrinterCallback& callback,
+    const base::Closure& error_callback) {
+  const bool has_printer = base::ContainsKey(printers_, name);
+  if (has_printer)
+    printers_.erase(name);
+
+  base::ThreadTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::Bind(callback, has_printer));
 }
 
 }  // namespace chromeos
