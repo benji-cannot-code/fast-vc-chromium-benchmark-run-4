86e4fa847fedf6787d01568e5b61057fa2bd1245
harkness <harkness@chromium.org>
2016-09-13 07:43:12 -0700

    Connect Mojo budget_service to BudgetManager implementation of Reserve.
    This connects the Mojo service to the pre-existing implementation of
    Reserve. It also updates the Reserve implementation to track both whether
    the origin had enough budget to spend as well as whether there was an
    error in reading or writing the database. Tests were updated to reflect
    the new parameters, but no new tests were needed.
    
    BUG=617971
    
    Review-Url: https://codereview.chromium.org/2326523003
    Cr-Commit-Position: refs/heads/master@{#418244}

--- chrome/browser/budget_service/budget_manager.h
+++ chrome/browser/budget_service/budget_manager.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_BUDGET_SERVICE_BUDGET_MANAGER_H_
 #define CHROME_BROWSER_BUDGET_SERVICE_BUDGET_MANAGER_H_
 
+#include <map>
 #include <memory>
-#include <string>
 
 #include "base/callback_forward.h"
 #include "base/gtest_prod_util.h"
@@ -40,7 +40,7 @@ class BudgetManager : public KeyedService {
   static double GetCost(blink::mojom::BudgetOperationType type);
 
   using GetBudgetCallback = blink::mojom::BudgetService::GetBudgetCallback;
-  using ReserveCallback = base::Callback<void(bool success)>;
+  using ReserveCallback = blink::mojom::BudgetService::ReserveCallback;
   using ConsumeCallback = base::Callback<void(bool success)>;
 
   // Get the budget associated with the origin. This is passed to the
@@ -64,17 +64,23 @@ class BudgetManager : public KeyedService {
  private:
   friend class BudgetManagerTest;
 
+  // Called as a callback from BudgetDatabase after it made a SpendBudget
+  // decision.
+  void DidConsume(const ConsumeCallback& callback,
+                  blink::mojom::BudgetServiceErrorType error,
+                  bool success);
+
   // Called as a callback from BudgetDatabase after it has made a reserve
   // decision.
   void DidReserve(const url::Origin& origin,
-                  blink::mojom::BudgetOperationType type,
                   const ReserveCallback& callback,
+                  blink::mojom::BudgetServiceErrorType error,
                   bool success);
 
   Profile* profile_;
   BudgetDatabase db_;
 
-  std::unordered_map<std::string, int> reservation_map_;
+  std::map<url::Origin, int> reservation_map_;
   base::WeakPtrFactory<BudgetManager> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(BudgetManager);
