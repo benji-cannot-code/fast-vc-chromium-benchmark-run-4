c3e13dca17451843c6ad055fc2a7433f2243291c
lgarron <lgarron@chromium.org>
2016-09-13 07:16:25 -0700

    Remove buttons from internal page info bubble.
    BUG=645855
    
    Review-Url: https://codereview.chromium.org/2330223002
    Cr-Commit-Position: refs/heads/master@{#418236}

--- chrome/browser/ui/views/website_settings/website_settings_popup_view.cc
+++ chrome/browser/ui/views/website_settings/website_settings_popup_view.cc
@@ -168,6 +168,7 @@ class InternalPageInfoPopupView : public views::BubbleDialogDelegateView {
   views::NonClientFrameView* CreateNonClientFrameView(
       views::Widget* widget) override;
   void OnWidgetDestroying(views::Widget* widget) override;
+  int GetDialogButtons() const override;
 
  private:
   friend class WebsiteSettingsPopupView;
@@ -395,6 +396,10 @@ views::NonClientFrameView* InternalPageInfoPopupView::CreateNonClientFrameView(
 
 void InternalPageInfoPopupView::OnWidgetDestroying(views::Widget* widget) {
   is_popup_showing = false;
+}
+
+int InternalPageInfoPopupView::GetDialogButtons() const {
+  return ui::DIALOG_BUTTON_NONE;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
