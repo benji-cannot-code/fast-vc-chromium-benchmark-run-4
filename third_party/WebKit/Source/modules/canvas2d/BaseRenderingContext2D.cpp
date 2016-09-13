e5f69d9d0cdfbb3f065a32f3f5065d90a640cb20
xidachen <xidachen@chromium.org>
2016-09-13 06:21:54 -0700

    Using SKComposeFilter instead of doing saveLayer
    Currently in BaseRenderingContext2D::compositedDraw, we do saveLayer when
    there is a SKImageFilter involved. We should be able to achieve the same
    functionality by combining those filters, and assign the combined filter
    to the foregroundPaint which is passed to drawFunc(). The drawFunc()
    internally will optimize the cases where there are filters.
    
    BUG=502921
    
    Review-Url: https://codereview.chromium.org/2324803004
    Cr-Commit-Position: refs/heads/master@{#418227}

--- third_party/WebKit/Source/modules/canvas2d/BaseRenderingContext2D.cpp
+++ third_party/WebKit/Source/modules/canvas2d/BaseRenderingContext2D.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/graphics/ImageBuffer.h"
 #include "platform/graphics/StrokeData.h"
 #include "platform/graphics/skia/SkiaUtils.h"
-#include "third_party/skia/include/core/SkImageFilter.h"
 
 namespace blink {
 
