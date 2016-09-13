7cb3eef2b807aa98c6782bd7fa9206a609afafdf
xlai <xlai@chromium.org>
2016-09-13 13:43:55 -0700

    Disallow users modify canvas after it transfers control to offscreen
    After a canvas has transferred its control to Offscreen, we should not allow
    users to perform resizing, toDataURL, toBlob and getContext.
    
    This is a reland of
    https://crrev.com/85264448db0298f836e6d7bf95ad11134637e4ed
    at refs/heads/master@{#418114}.
    
    BUG=563852
    
    Review-Url: https://codereview.chromium.org/2323933004
    Cr-Commit-Position: refs/heads/master@{#418357}

--- third_party/WebKit/Source/modules/canvas/HTMLCanvasElementModule.cpp
+++ third_party/WebKit/Source/modules/canvas/HTMLCanvasElementModule.cpp
@@ -12,8 +12,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void HTMLCanvasElementModule::getContext(HTMLCanvasElement& canvas, const String& type, const CanvasContextCreationAttributes& attributes, RenderingContext& result)
+void HTMLCanvasElementModule::getContext(HTMLCanvasElement& canvas, const String& type, const CanvasContextCreationAttributes& attributes, ExceptionState& exceptionState, RenderingContext& result)
 {
+    if (canvas.surfaceLayerBridge()) {
+        // The existence of canvas surfaceLayerBridge indicates that HTMLCanvasElement.transferControlToOffscreen() has been called.
+        exceptionState.throwDOMException(InvalidStateError, "Cannot get context from a canvas that has transferred its control to offscreen.");
+        return;
+    }
+
     CanvasRenderingContext* context = canvas.getCanvasRenderingContext(type, attributes);
     if (context) {
         context->setCanvasGetContextResult(result);
