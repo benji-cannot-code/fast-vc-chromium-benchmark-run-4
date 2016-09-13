7ba7f22891dd2a850ac4b80ebe91ebb65933f195
cbiesinger <cbiesinger@chromium.org>
2016-09-13 16:16:04 -0700

    [layoutng] Handle box-sizing and correct margin handling for width: auto
    Makes sure to only add border/padding for specified values, not keywords,
    and makes sure to subtract margins when computing the size of width: auto/
    fill-available or of height: fill-available.
    
    BUG=635619
    
    Review-Url: https://codereview.chromium.org/2335323002
    Cr-Commit-Position: refs/heads/master@{#418405}

--- third_party/WebKit/Source/core/layout/ng/ng_length_utils_test.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_length_utils_test.cc
@@ -37,7 +37,8 @@ class NGLengthUtilsTest : public ::testing::Test {
       const Length& length,
       LengthResolveType type = LengthResolveType::ContentSize) {
     NGConstraintSpace* constraintSpace = ConstructConstraintSpace(200, 300);
-    return ::blink::resolveInlineLength(*constraintSpace, length, type);
+    return ::blink::resolveInlineLength(*constraintSpace, *style_, length,
+                                        type);
   }
 
   LayoutUnit resolveBlockLength(
@@ -45,8 +46,8 @@ class NGLengthUtilsTest : public ::testing::Test {
       LengthResolveType type = LengthResolveType::ContentSize,
       LayoutUnit contentSize = LayoutUnit()) {
     NGConstraintSpace* constraintSpace = ConstructConstraintSpace(200, 300);
-    return ::blink::resolveBlockLength(*constraintSpace, length, contentSize,
-                                       type);
+    return ::blink::resolveBlockLength(*constraintSpace, *style_, length,
+                                       contentSize, type);
   }
 
   LayoutUnit computeInlineSizeForFragment(
@@ -123,6 +124,17 @@ TEST_F(NGLengthUtilsTest, testComputeInlineSizeForFragment) {
   style_->setMinWidth(Length(80, Percent));
   EXPECT_EQ(LayoutUnit(160), computeInlineSizeForFragment());
 
+  style_ = ComputedStyle::create();
+  style_->setMarginRight(Length(20, Fixed));
+  EXPECT_EQ(LayoutUnit(180), computeInlineSizeForFragment());
+
+  style_->setLogicalWidth(Length(100, Fixed));
+  style_->setPaddingLeft(Length(50, Fixed));
+  EXPECT_EQ(LayoutUnit(150), computeInlineSizeForFragment());
+
+  style_->setBoxSizing(BoxSizingBorderBox);
+  EXPECT_EQ(LayoutUnit(100), computeInlineSizeForFragment());
+
   // TODO(layout-ng): test {min,max}-content on max-width.
 }
 
@@ -160,6 +172,18 @@ TEST_F(NGLengthUtilsTest, testComputeBlockSizeForFragment) {
   style_->setLogicalHeight(Length(100, Fixed));
   style_->setMinHeight(Length(80, Percent));
   EXPECT_EQ(LayoutUnit(240), computeBlockSizeForFragment());
+
+  style_ = ComputedStyle::create();
+  style_->setMarginTop(Length(20, Fixed));
+  style_->setLogicalHeight(Length(FillAvailable));
+  EXPECT_EQ(LayoutUnit(280), computeBlockSizeForFragment());
+
+  style_->setLogicalHeight(Length(100, Fixed));
+  style_->setPaddingBottom(Length(50, Fixed));
+  EXPECT_EQ(LayoutUnit(150), computeBlockSizeForFragment());
+
+  style_->setBoxSizing(BoxSizingBorderBox);
+  EXPECT_EQ(LayoutUnit(100), computeBlockSizeForFragment());
 
   // TODO(layout-ng): test {min,max}-content on max-height.
 }
