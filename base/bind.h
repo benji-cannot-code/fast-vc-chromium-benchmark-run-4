3f39f198a71e8a409985baf42c97d3fe7e4c036d
tzik <tzik@chromium.org>
2016-09-12 22:28:59 -0700

    Introduce OnceClosure and BindOnce
    This CL adds another flag, RepeatMode, to base::Callback to make a Callback
    variant that can be run only once, and exposes it as OnceCallback.
    This also adds a base:::Bind variant to create an OneOnce, and
    adds provides conversions and constraints between Callbacks.
    
    BUG=554299
    
    Review-Url: https://codereview.chromium.org/2042223002
    Cr-Commit-Position: refs/heads/master@{#418177}

--- base/bind.h
+++ base/bind.h
@@ -24,15 +24,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // terms and concepts.
 
 namespace base {
+namespace internal {
 
+// Bind as OnceCallback.
 template <typename Functor, typename... Args>
-inline base::Callback<MakeUnboundRunType<Functor, Args...>> Bind(
-    Functor&& functor,
-    Args&&... args) {
+inline OnceCallback<MakeUnboundRunType<Functor, Args...>>
+BindOnce(Functor&& functor, Args&&... args) {
   using BindState = internal::MakeBindStateType<Functor, Args...>;
   using UnboundRunType = MakeUnboundRunType<Functor, Args...>;
   using Invoker = internal::Invoker<BindState, UnboundRunType>;
-  using CallbackType = Callback<UnboundRunType>;
+  using CallbackType = OnceCallback<UnboundRunType>;
+
+  // Store the invoke func into PolymorphicInvoke before casting it to
+  // InvokeFuncStorage, so that we can ensure its type matches to
+  // PolymorphicInvoke, to which CallbackType will cast back.
+  using PolymorphicInvoke = typename CallbackType::PolymorphicInvoke;
+  PolymorphicInvoke invoke_func = &Invoker::RunOnce;
+
+  using InvokeFuncStorage = internal::BindStateBase::InvokeFuncStorage;
+  return CallbackType(new BindState(
+      reinterpret_cast<InvokeFuncStorage>(invoke_func),
+      std::forward<Functor>(functor),
+      std::forward<Args>(args)...));
+}
+
+// Bind as RepeatingCallback.
+template <typename Functor, typename... Args>
+inline RepeatingCallback<MakeUnboundRunType<Functor, Args...>>
+BindRepeating(Functor&& functor, Args&&... args) {
+  using BindState = internal::MakeBindStateType<Functor, Args...>;
+  using UnboundRunType = MakeUnboundRunType<Functor, Args...>;
+  using Invoker = internal::Invoker<BindState, UnboundRunType>;
+  using CallbackType = RepeatingCallback<UnboundRunType>;
 
   // Store the invoke func into PolymorphicInvoke before casting it to
   // InvokeFuncStorage, so that we can ensure its type matches to
@@ -45,6 +68,18 @@ inline base::Callback<MakeUnboundRunType<Functor, Args...>> Bind(
       reinterpret_cast<InvokeFuncStorage>(invoke_func),
       std::forward<Functor>(functor),
       std::forward<Args>(args)...));
+}
+
+}  // namespace internal
+
+// Unannotated Bind.
+// TODO(tzik): Deprecate this and migrate to OnceCallback and
+// RepeatingCallback, once they get ready.
+template <typename Functor, typename... Args>
+inline Callback<MakeUnboundRunType<Functor, Args...>>
+Bind(Functor&& functor, Args&&... args) {
+  return internal::BindRepeating(std::forward<Functor>(functor),
+                                 std::forward<Args>(args)...);
 }
 
 }  // namespace base
