dd1ee81331d513a0c31df49d679eb7265e8796fb
perezju <perezju@chromium.org>
2016-09-13 09:33:48 -0700

    [tools/perf] tab_switching benchmarks should not tear down state
    The tab_switching benchmarks should not tear down state between each
    story, as this is used to keep several tabs open and switch among them.
    
    BUG=646011
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.perf:android_s5_perf_cq;master.tryserver.chromium.perf:linux_perf_cq;master.tryserver.chromium.perf:mac_retina_perf_cq;master.tryserver.chromium.perf:winx64_10_perf_cq
    
    Review-Url: https://codereview.chromium.org/2336153002
    Cr-Commit-Position: refs/heads/master@{#418272}

--- tools/perf/benchmarks/tab_switching.py
+++ tools/perf/benchmarks/tab_switching.py
@@ -28,6 +28,10 @@ class TabSwitchingTop10(perf_benchmark.PerfBenchmark):
   def Name(cls):
     return 'tab_switching.top_10'
 
+  @classmethod
+  def ShouldTearDownStateAfterEachStoryRun(cls):
+    return False
+
 
 @benchmark.Enabled('has tabs')
 @benchmark.Disabled('mac-reference')  # http://crbug.com/612774
@@ -49,6 +53,10 @@ class TabSwitchingTypical25(perf_benchmark.PerfBenchmark):
   def Name(cls):
     return 'tab_switching.typical_25'
 
+  @classmethod
+  def ShouldTearDownStateAfterEachStoryRun(cls):
+    return False
+
 
 @benchmark.Disabled('android')  # http://crbug.com/460084
 @benchmark.Disabled('mac-reference')  # http://crbug.com/634360
@@ -68,6 +76,10 @@ class TabSwitchingFiveBlankTabs(perf_benchmark.PerfBenchmark):
   @classmethod
   def Name(cls):
     return 'tab_switching.five_blank_pages'
+
+  @classmethod
+  def ShouldTearDownStateAfterEachStoryRun(cls):
+    return False
 
 
 @benchmark.Enabled('has tabs')
@@ -89,6 +101,10 @@ class TabSwitchingToughEnergyCases(perf_benchmark.PerfBenchmark):
   def Name(cls):
     return 'tab_switching.tough_energy_cases'
 
+  @classmethod
+  def ShouldTearDownStateAfterEachStoryRun(cls):
+    return False
+
 
 @benchmark.Enabled('has tabs')
 @benchmark.Disabled('android')  # http://crbug.com/460084
@@ -106,3 +122,7 @@ class TabSwitchingToughImageCases(perf_benchmark.PerfBenchmark):
   @classmethod
   def Name(cls):
     return 'tab_switching.tough_image_cases'
+
+  @classmethod
+  def ShouldTearDownStateAfterEachStoryRun(cls):
+    return False
