022f192009513ac1d450f868a2827a33fdfe774e
zqzhang <zqzhang@chromium.org>
2016-09-13 08:13:57 -0700

    Disabling MediaSessionVisibilityBrowserTests for WMPA
    Disabling the WMPA tests since they are flaky on Marshmallow Tablet tester.
    
    BUG=646312
    TBR=avayvod@chromium.org
    
    Review-Url: https://codereview.chromium.org/2337133002
    Cr-Commit-Position: refs/heads/master@{#418250}

--- content/browser/media/session/media_session_visibility_browsertest.cc
+++ content/browser/media/session/media_session_visibility_browsertest.cc
@@ -288,7 +288,8 @@ VisibilityTestData kTestParams[] = {
 Pipeline kPipelines[] = {
     Pipeline::WMPI,
 #if defined(OS_ANDROID)
-    Pipeline::WMPA,
+    // Disabling WMPA tests because of https://crbug.com/646312
+    // Pipeline::WMPA,
 #endif  // defined(OS_ANDROID)
 };
 
