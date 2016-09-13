62c9385b918a05e3da949be2d3463129905560b0
afakhry <afakhry@chromium.org>
2016-09-13 13:34:34 -0700

    Add the most recent crash report IDs to feedback reports
    Adds the IDs of crashes that occurred within the last hour before sending the
    feedback report, making sure that these IDs don't end up in the system_logs.txt
    file for privacy reasons.
    
    BUG=613606
    
    Review-Url: https://codereview.chromium.org/2242833003
    Cr-Commit-Position: refs/heads/master@{#418351}

--- chrome/browser/feedback/system_logs/scrubbed_system_logs_fetcher.cc
+++ chrome/browser/feedback/system_logs/scrubbed_system_logs_fetcher.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "build/build_config.h"
 #include "chrome/browser/feedback/system_logs/log_sources/chrome_internal_log_source.h"
+#include "chrome/browser/feedback/system_logs/log_sources/crash_ids_source.h"
 #include "chrome/browser/feedback/system_logs/log_sources/memory_details_log_source.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -27,6 +28,7 @@ namespace system_logs {
 
 ScrubbedSystemLogsFetcher::ScrubbedSystemLogsFetcher() {
   data_sources_.push_back(new ChromeInternalLogSource());
+  data_sources_.push_back(new CrashIdsSource());
   data_sources_.push_back(new MemoryDetailsLogSource());
 
 #if defined(OS_CHROMEOS)
