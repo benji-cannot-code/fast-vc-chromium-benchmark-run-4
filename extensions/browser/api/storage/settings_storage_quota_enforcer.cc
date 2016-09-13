c6d9e643cb0c0bb775a13edd148a7a332d45152a
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-09-13 13:36:22 -0700

    Remove unused histograms
    The histograms in settings_storage_quota_enforcer were never used, and
    weren't very interesting (they only logged a count of one whenever
    encountered, but didn't offer any kind of scale, making the numbers
    all but meaningless). Remove them.
    
    BUG=643559
    
    Review-Url: https://codereview.chromium.org/2329973004
    Cr-Commit-Position: refs/heads/master@{#418353}

--- extensions/browser/api/storage/settings_storage_quota_enforcer.cc
+++ extensions/browser/api/storage/settings_storage_quota_enforcer.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/json/json_writer.h"
 #include "base/message_loop/message_loop.h"
-#include "base/metrics/histogram.h"
 #include "base/stl_util.h"
 #include "base/strings/stringprintf.h"
 #include "extensions/common/extension_api.h"
@@ -47,22 +46,15 @@ void Allocate(
 
 ValueStore::Status QuotaExceededError(Resource resource) {
   const char* name = NULL;
-  // TODO(kalman): These hisograms are both silly and untracked. Fix.
   switch (resource) {
     case QUOTA_BYTES:
       name = "QUOTA_BYTES";
-      UMA_HISTOGRAM_COUNTS_100(
-          "Extensions.SettingsQuotaExceeded.TotalBytes", 1);
       break;
     case QUOTA_BYTES_PER_ITEM:
       name = "QUOTA_BYTES_PER_ITEM";
-      UMA_HISTOGRAM_COUNTS_100(
-          "Extensions.SettingsQuotaExceeded.BytesPerSetting", 1);
       break;
     case MAX_ITEMS:
       name = "MAX_ITEMS";
-      UMA_HISTOGRAM_COUNTS_100(
-          "Extensions.SettingsQuotaExceeded.KeyCount", 1);
       break;
   }
   CHECK(name);
