91bfa41967ae02f943ad9091e08a999300aa803d
dcheng <dcheng@chromium.org>
2016-09-13 17:47:00 -0700

    Implement WTF::WeakPtr in terms of base::WeakPtr
    BUG=645774
    
    Review-Url: https://codereview.chromium.org/2329243002
    Cr-Commit-Position: refs/heads/master@{#418441}

--- third_party/WebKit/Source/core/html/parser/BackgroundHTMLParser.cpp
+++ third_party/WebKit/Source/core/html/parser/BackgroundHTMLParser.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "public/platform/Platform.h"
 #include "public/platform/WebTaskRunner.h"
 #include "wtf/CurrentTime.h"
+#include "wtf/Functional.h"
 #include "wtf/PtrUtil.h"
 #include "wtf/text/TextPosition.h"
 #include <memory>
@@ -85,11 +86,17 @@ static void checkThatXSSInfosAreSafeToSendToAnotherThread(const XSSInfoStream& i
 
 #endif
 
-void BackgroundHTMLParser::start(PassRefPtr<WeakReference<BackgroundHTMLParser>> reference, std::unique_ptr<Configuration> config, const KURL& documentURL, std::unique_ptr<CachedDocumentParameters> cachedDocumentParameters, const MediaValuesCached::MediaValuesCachedData& mediaValuesCachedData, std::unique_ptr<WebTaskRunner> loadingTaskRunner)
+WeakPtr<BackgroundHTMLParser> BackgroundHTMLParser::create(std::unique_ptr<Configuration> config, std::unique_ptr<WebTaskRunner> loadingTaskRunner)
 {
-    new BackgroundHTMLParser(reference, std::move(config), documentURL, std::move(cachedDocumentParameters), mediaValuesCachedData, std::move(loadingTaskRunner));
-    // Caller must free by calling stop().
+    auto* backgroundParser = new BackgroundHTMLParser(std::move(config), std::move(loadingTaskRunner));
+    return backgroundParser->m_weakFactory.createWeakPtr();
 }
+
+void BackgroundHTMLParser::init(const KURL& documentURL, std::unique_ptr<CachedDocumentParameters> cachedDocumentParameters, const MediaValuesCached::MediaValuesCachedData& mediaValuesCachedData)
+{
+    m_preloadScanner.reset(new TokenPreloadScanner(documentURL, std::move(cachedDocumentParameters), mediaValuesCachedData));
+}
+
 
 BackgroundHTMLParser::Configuration::Configuration()
     : outstandingTokenLimit(defaultOutstandingTokenLimit)
@@ -98,8 +105,8 @@ BackgroundHTMLParser::Configuration::Configuration()
 {
 }
 
-BackgroundHTMLParser::BackgroundHTMLParser(PassRefPtr<WeakReference<BackgroundHTMLParser>> reference, std::unique_ptr<Configuration> config, const KURL& documentURL, std::unique_ptr<CachedDocumentParameters> cachedDocumentParameters, const MediaValuesCached::MediaValuesCachedData& mediaValuesCachedData, std::unique_ptr<WebTaskRunner> loadingTaskRunner)
-    : m_weakFactory(reference, this)
+BackgroundHTMLParser::BackgroundHTMLParser(std::unique_ptr<Configuration> config, std::unique_ptr<WebTaskRunner> loadingTaskRunner)
+    : m_weakFactory(this)
     , m_token(wrapUnique(new HTMLToken))
     , m_tokenizer(HTMLTokenizer::create(config->options))
     , m_treeBuilderSimulator(config->options)
@@ -109,7 +116,6 @@ BackgroundHTMLParser::BackgroundHTMLParser(PassRefPtr<WeakReference<BackgroundHT
     , m_pendingTokens(wrapUnique(new CompactHTMLTokenStream))
     , m_pendingTokenLimit(config->pendingTokenLimit)
     , m_xssAuditor(std::move(config->xssAuditor))
-    , m_preloadScanner(wrapUnique(new TokenPreloadScanner(documentURL, std::move(cachedDocumentParameters), mediaValuesCachedData)))
     , m_decoder(std::move(config->decoder))
     , m_loadingTaskRunner(std::move(loadingTaskRunner))
     , m_tokenizedChunkQueue(config->tokenizedChunkQueue.release())
--- third_party/WebKit/Source/platform/CrossThreadCopier.h
+++ third_party/WebKit/Source/platform/CrossThreadCopier.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/RefPtr.h"
 #include "wtf/ThreadSafeRefCounted.h"
 #include "wtf/TypeTraits.h"
+#include "wtf/WeakPtr.h"
 #include <memory>
 
 class SkRefCnt;
