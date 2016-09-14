de02785409656dc65b563414a9e0485dcb823f59
junov <junov@chromium.org>
2016-09-14 07:59:02 -0700

    Fix crash and potential crashes caused by ImageBuffer allocation failure.
    This CL add null ptr checks in a few places that need them.
    
    BUG=644644
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2335263002
    Cr-Commit-Position: refs/heads/master@{#418563}

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
@@ -327,7 +327,7 @@ void HTMLCanvasElement::didFinalizeFrame()
     m_numFramesSinceLastRenderingModeSwitch++;
     if (RuntimeEnabledFeatures::enableCanvas2dDynamicRenderingModeSwitchingEnabled()
         && !RuntimeEnabledFeatures::canvas2dFixedRenderingModeEnabled()) {
-        if (m_context->is2d() && buffer()->isAccelerated()
+        if (m_context->is2d() && buffer() && buffer()->isAccelerated()
             && m_numFramesSinceLastRenderingModeSwitch >= ExpensiveCanvasHeuristicParameters::MinFramesBeforeSwitch
             && !m_pendingRenderingModeSwitch) {
             if (!m_context->isAccelerationOptimalForCanvasContent()) {
@@ -346,7 +346,7 @@ void HTMLCanvasElement::didFinalizeFrame()
         }
     }
 
-    if (m_pendingRenderingModeSwitch && !buffer()->isAccelerated()) {
+    if (m_pendingRenderingModeSwitch && buffer() && !buffer()->isAccelerated()) {
         m_pendingRenderingModeSwitch = false;
     }
 
@@ -626,10 +626,12 @@ ImageData* HTMLCanvasElement::toImageData(SourceDrawingBuffer sourceBuffer, Snap
 
         m_context->paintRenderingResultsToCanvas(sourceBuffer);
         imageData = ImageData::create(m_size);
-        sk_sp<SkImage> snapshot = buffer()->newSkImageSnapshot(PreferNoAcceleration, reason);
-        if (snapshot) {
-            SkImageInfo imageInfo = SkImageInfo::Make(width(), height(), kRGBA_8888_SkColorType, kUnpremul_SkAlphaType);
-            snapshot->readPixels(imageInfo, imageData->data()->data(), imageInfo.minRowBytes(), 0, 0);
+        if (hasImageBuffer()) {
+            sk_sp<SkImage> snapshot = buffer()->newSkImageSnapshot(PreferNoAcceleration, reason);
+            if (snapshot) {
+                SkImageInfo imageInfo = SkImageInfo::Make(width(), height(), kRGBA_8888_SkColorType, kUnpremul_SkAlphaType);
+                snapshot->readPixels(imageInfo, imageData->data()->data(), imageInfo.minRowBytes(), 0, 0);
+            }
         }
         return imageData;
     }
@@ -640,10 +642,12 @@ ImageData* HTMLCanvasElement::toImageData(SourceDrawingBuffer sourceBuffer, Snap
         return imageData;
 
     DCHECK(m_context->is2d());
-    sk_sp<SkImage> snapshot = buffer()->newSkImageSnapshot(PreferNoAcceleration, reason);
-    if (snapshot) {
-        SkImageInfo imageInfo = SkImageInfo::Make(width(), height(), kRGBA_8888_SkColorType, kUnpremul_SkAlphaType);
-        snapshot->readPixels(imageInfo, imageData->data()->data(), imageInfo.minRowBytes(), 0, 0);
+    if (hasImageBuffer()) {
+        sk_sp<SkImage> snapshot = buffer()->newSkImageSnapshot(PreferNoAcceleration, reason);
+        if (snapshot) {
+            SkImageInfo imageInfo = SkImageInfo::Make(width(), height(), kRGBA_8888_SkColorType, kUnpremul_SkAlphaType);
+            snapshot->readPixels(imageInfo, imageData->data()->data(), imageInfo.minRowBytes(), 0, 0);
+        }
     }
 
     return imageData;
