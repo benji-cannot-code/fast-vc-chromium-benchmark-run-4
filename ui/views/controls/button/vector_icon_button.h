5b2df61944057201388052c5e5cf6f04089de6f9
estade <estade@chromium.org>
2016-09-13 17:15:04 -0700

    Fix appearance of disabled VectorIconButtons.
    Ironically, removing OnEnabledChanged() fixes this bug. Not calling
    through to the parent class's implementation was problematic, but it
    turns out that handling changes to the enable state isn't necessary
    anyway since that's done with the state parameter of
    ImageButton::SetImage().
    
    BUG=645353
    
    Review-Url: https://codereview.chromium.org/2340503002
    Cr-Commit-Position: refs/heads/master@{#418431}

--- ui/views/controls/button/vector_icon_button.h
+++ ui/views/controls/button/vector_icon_button.h
@@ -29,7 +29,6 @@ class VIEWS_EXPORT VectorIconButton : public views::ImageButton {
   void SetIcon(gfx::VectorIconId id);
 
   // views::ImageButton:
-  void OnEnabledChanged() override;
   void OnThemeChanged() override;
   void OnNativeThemeChanged(const ui::NativeTheme* theme) override;
 
