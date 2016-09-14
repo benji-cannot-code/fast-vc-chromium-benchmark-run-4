29cae09596848543803603becb94bfe256bb9da7
skobes <skobes@chromium.org>
2016-09-14 14:29:41 -0700

    Workaround for scroll anchoring issue with AutoScrollExtensionSettingsWebUITest.
    BUG=646977
    
    Review-Url: https://codereview.chromium.org/2343683002
    Cr-Commit-Position: refs/heads/master@{#418680}

--- chrome/browser/ui/webui/extensions/extension_settings_browsertest.js
+++ chrome/browser/ui/webui/extensions/extension_settings_browsertest.js
@@ -324,8 +324,11 @@ TEST_F('AutoScrollExtensionSettingsWebUITest', 'testAutoScroll', function() {
     assertEquals(0, document.body.scrollTop);
     this.nextStep();
   };
-  var scrollToTop = function() {
+  var scrollToOrigin = function() {
+    // Reset both top and left to avoid a scroll anchoring adjustment when the
+    // #dev-controls element is inserted (crbug.com/646977).
     document.body.scrollTop = 0;
+    document.body.scrollLeft = 0;
     this.nextStep();
   };
   // Test that a) autoscroll works on first page load and b) updating the
@@ -333,7 +336,7 @@ TEST_F('AutoScrollExtensionSettingsWebUITest', 'testAutoScroll', function() {
   this.steps = [this.waitForPageLoad,
                 checkHasScrollbar,
                 checkIsScrolled,
-                scrollToTop,
+                scrollToOrigin,
                 this.enableDeveloperMode,
                 checkScrolledToTop,
                 testDone];
