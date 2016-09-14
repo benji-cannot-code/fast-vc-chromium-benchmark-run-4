1635c13f88d5425c6386a2eb3862f56a16bf0b7b
robhogan <robhogan@gmail.com>
2016-09-13 17:16:02 -0700

    Revert of Apply first-line transform-text style (patchset #3 id:40001 of https://codereview.chromium.org/2331793002/ )
    Reason for revert:
    Still hitting clusterfuzz.
    
    Original issue's description:
    > Apply first-line transform-text style
    >
    > A second go at https://crrev.com/3c64df1fc98aa06eabfc18d1f5c2f2b0aec1a658
    >
    > BUG=129669,644733
    >
    > Committed: https://crrev.com/25c2cadce2d4a92f4f0f8b669fed2d097a6b7afa
    > Cr-Commit-Position: refs/heads/master@{#418032}
    
    TBR=eae@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=129669,644733
    
    Review-Url: https://codereview.chromium.org/2337133004
    Cr-Commit-Position: refs/heads/master@{#418432}

--- third_party/WebKit/Source/core/layout/LayoutText.h
+++ third_party/WebKit/Source/core/layout/LayoutText.h
@@ -182,7 +182,6 @@ public:
 
     float hyphenWidth(const Font&, TextDirection);
 
-    void applyTextTransformFromTo(int from, int len, const ComputedStyle*);
 protected:
     void willBeDestroyed() override;
 
