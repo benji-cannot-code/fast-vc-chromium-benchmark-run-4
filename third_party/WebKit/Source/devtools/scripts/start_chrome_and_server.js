668acb8dd95307d6f5ce05c4c7a15f1b2bab05a6
chenwilliam <chenwilliam@chromium.org>
2016-09-13 20:08:09 -0700

    DevTools: find and launch chrome in debug mode
    Cross-platform node.js script to automatically find Chrome Canary (in Mac and Windows)
    and launch it with debug flags.
    
    I've updated README which explains the workflow for users.
    
    Workflow commands (from /devtools):
    * npm start - starts Chrome and development server
    * npm run chrome - starts Chrome browser
    * npm run server - starts development server
    
    BUG=629914
    
    Review-Url: https://codereview.chromium.org/2337713002
    Cr-Commit-Position: refs/heads/master@{#418469}

---
+++ third_party/WebKit/Source/devtools/scripts/start_chrome_and_server.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var childProcess = require("child_process");
+var path = require("path");
+
+childProcess.fork(path.join(__dirname, "chrome_debug_launcher/launch_chrome.js"));
+childProcess.fork(path.join(__dirname, "hosted_mode/server.js"));
