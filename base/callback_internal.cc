0c0b2abb5002c7c1765ceb8c64d11109778c92b3
tzik <tzik@chromium.org>
2016-09-14 00:15:00 -0700

    Split BindStateBase ctor for non-cancellable Bind
    http://crrev.com/59aa6bb1162b31d2 added a parameter of BindStateBase,
    and that causes 0.8% (40kB) binary size bloat on cronet, by adding
    a new instruction to the constructor invocation at BindState ctor.
    
    To reduce the binary size, this CL splits BindStateBase into two: one
    for cancellable bind and another for non-cancellable one, so that
    non-cancellable bind doesn't need to pass an extra argument to
    BindStateBase.
    
    This reduces the stripped binary size of chrome on Linux by 56kB.
    
    Review-Url: https://codereview.chromium.org/2322313002
    Cr-Commit-Position: refs/heads/master@{#418502}

--- base/callback_internal.cc
+++ base/callback_internal.cc
@@ -10,13 +10,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace internal {
 
+namespace {
+
+bool ReturnFalse(const BindStateBase*) {
+  return false;
+}
+
+}  // namespace
+
+BindStateBase::BindStateBase(InvokeFuncStorage polymorphic_invoke,
+                             void (*destructor)(BindStateBase*))
+    : BindStateBase(polymorphic_invoke, destructor, &ReturnFalse) {
+}
+
 BindStateBase::BindStateBase(InvokeFuncStorage polymorphic_invoke,
                              void (*destructor)(BindStateBase*),
                              bool (*is_cancelled)(const BindStateBase*))
-      : polymorphic_invoke_(polymorphic_invoke),
-        ref_count_(0),
-        destructor_(destructor),
-        is_cancelled_(is_cancelled) {}
+    : polymorphic_invoke_(polymorphic_invoke),
+      ref_count_(0),
+      destructor_(destructor),
+      is_cancelled_(is_cancelled) {}
 
 void BindStateBase::AddRef() {
   AtomicRefCountInc(&ref_count_);
