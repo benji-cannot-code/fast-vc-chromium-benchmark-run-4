2164e6d3486bf4b786a7ba76557985c36e77490e
pauljensen <pauljensen@chromium.org>
2016-09-13 18:46:28 -0700

    [Cronet] Strip prefix of EFFECTIVE_CONNECTION_TYPE to just TYPE_blah
    The prefix is redundant with the class name. TYPE_ cannot be stripped
    because the members like "2G" wouldn't be a proper identifier.
    
    BUG=629299
    R=xunjieli
    
    Review-Url: https://codereview.chromium.org/2341443002
    Cr-Commit-Position: refs/heads/master@{#418452}

--- components/cronet/android/test/javatests/src/org/chromium/net/CronetUrlRequestContextTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/CronetUrlRequestContextTest.java
@@ -348,7 +348,7 @@ public class CronetUrlRequestContextTest extends CronetTestBase {
         // Verify that effective connection type callback is received and
         // effective connection type is correctly set.
         assertTrue(testFramework.mCronetEngine.getEffectiveConnectionType()
-                != EffectiveConnectionType.EFFECTIVE_CONNECTION_TYPE_UNKNOWN);
+                != EffectiveConnectionType.TYPE_UNKNOWN);
 
         testFramework.mCronetEngine.shutdown();
     }
