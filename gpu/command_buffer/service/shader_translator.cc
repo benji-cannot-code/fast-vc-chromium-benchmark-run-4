faf8b5a2b2708d2ffb2df5e2cbe68a30fbe4cda0
qiankun.miao <qiankun.miao@intel.com>
2016-09-14 02:25:30 -0700

    Use 64-bits shader compile options
    BUG=645071
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2321953002
    Cr-Commit-Position: refs/heads/master@{#418518}

--- gpu/command_buffer/service/shader_translator.cc
+++ gpu/command_buffer/service/shader_translator.cc
@@ -206,7 +206,7 @@ bool ShaderTranslator::Init(GLenum shader_type,
   return compiler_ != NULL;
 }
 
-int ShaderTranslator::GetCompileOptions() const {
+ShCompileOptions ShaderTranslator::GetCompileOptions() const {
   return compile_options_;
 }
 
@@ -262,7 +262,7 @@ std::string ShaderTranslator::GetStringForOptionsThatWouldAffectCompilation()
     const {
   DCHECK(compiler_ != NULL);
   return std::string(":CompileOptions:" +
-         base::IntToString(GetCompileOptions())) +
+         base::Uint64ToString(GetCompileOptions())) +
          ShGetBuiltInResourcesString(compiler_);
 }
 
--- gpu/command_buffer/service/shader_translator.h
+++ gpu/command_buffer/service/shader_translator.h
@@ -124,10 +124,10 @@ class GPU_EXPORT ShaderTranslator
  private:
   ~ShaderTranslator() override;
 
-  int GetCompileOptions() const;
+  ShCompileOptions GetCompileOptions() const;
 
   ShHandle compiler_;
-  int compile_options_;
+  ShCompileOptions compile_options_;
   base::ObserverList<DestructionObserver> destruction_observers_;
 };
 
