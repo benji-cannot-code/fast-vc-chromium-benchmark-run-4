f6248e39b366b0c9856860ba506474e389f288b9
horo <horo@chromium.org>
2016-09-13 20:04:08 -0700

    Use mojo.common.mojom.Time for dispatch_event_time in background_sync.mojom
    BUG=561209
    
    Review-Url: https://codereview.chromium.org/2274283003
    Cr-Commit-Position: refs/heads/master@{#418467}

--- content/renderer/background_sync/background_sync_client_impl.cc
+++ content/renderer/background_sync/background_sync_client_impl.cc
@@ -41,7 +41,7 @@ void BackgroundSyncClientImpl::Sync(
       ServiceWorkerContextClient::ThreadSpecificInstance();
   if (!client) {
     callback.Run(blink::mojom::ServiceWorkerEventStatus::ABORTED,
-                 base::Time::Now().ToDoubleT());
+                 base::Time::Now());
     return;
   }
 
