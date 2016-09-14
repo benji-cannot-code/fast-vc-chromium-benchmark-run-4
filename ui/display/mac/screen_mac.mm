0c8427829e478164d31d5f4e485291f45331b387
ccameron <ccameron@chromium.org>
2016-09-14 09:54:42 -0700

    cc: Plumb the monitor color profile to renderer for rasterization
    This adds the output device color profile to display::Display, and
    populates it correctly on Mac. We will want to do this for all
    platforms.
    
    The color profile is then plumbed through the same IPCs that take
    the device scale factor, to get to the renderer process'
    RenderWidgetCompositor.
    
    Note that we are sending the full ICCProfile this far. This is
    important, because the renderer process will be setting the ICCProfile
    of its rasterized IOSurfaces, and there is a power impact of even slight
    differences between the monitor profile and the IOSurface profile.
    
    The ICCProfile is then sent as a gfx::ColorProfile (which internally
    references the ICCProfile, for the above purpose) to the
    RenderWidgetCompositor, from where it will be pushed to cc.
    
    In the next step, we will (under a flag) specify the color space
    for rasterization.
    
    BUG=44872
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Committed: https://crrev.com/f1fef7489c29deeee2a29c41ce4851e9ad1bd67b
    Review-Url: https://codereview.chromium.org/2325773003
    Cr-Original-Commit-Position: refs/heads/master@{#418422}
    Cr-Commit-Position: refs/heads/master@{#418591}

--- ui/display/mac/screen_mac.mm
+++ ui/display/mac/screen_mac.mm
@@ -20,6 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/display_change_notifier.h"
 #include "ui/gfx/mac/coordinate_conversion.h"
 
+extern "C" {
+Boolean CGDisplayUsesForceToGray(void);
+}
+
 namespace display {
 namespace {
 
@@ -72,6 +76,13 @@ Display GetDisplayForScreen(NSScreen* screen) {
     scale = Display::GetForcedDeviceScaleFactor();
 
   display.set_device_scale_factor(scale);
+
+  display.set_icc_profile(
+      gfx::ICCProfile::FromCGColorSpace([[screen colorSpace] CGColorSpace]));
+  display.set_color_depth(NSBitsPerPixelFromDepth([screen depth]));
+  display.set_depth_per_component(NSBitsPerSampleFromDepth([screen depth]));
+  display.set_is_monochrome(CGDisplayUsesForceToGray());
+
   // CGDisplayRotation returns a double. Display::SetRotationAsDegree will
   // handle the unexpected situations were the angle is not a multiple of 90.
   display.SetRotationAsDegree(static_cast<int>(CGDisplayRotation(display_id)));
