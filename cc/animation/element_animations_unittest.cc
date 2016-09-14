3886657f0f54e8dcff016f9ca53ab08428d32034
jaydasika <jaydasika@chromium.org>
2016-09-14 15:36:09 -0700

    cc : Fix the bug in tracking currently running animations for an element
    We are updating this value based on the status of the lst animation of
    the element which is wrong.
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2346583002
    Cr-Commit-Position: refs/heads/master@{#418701}

--- cc/animation/element_animations_unittest.cc
+++ cc/animation/element_animations_unittest.cc
@@ -2889,6 +2889,12 @@ TEST_F(ElementAnimationsTest, ObserverNotifiedWhenTransformAnimationChanges) {
   // Case 2: An animation that's removed before it finishes.
   int animation_id =
       AddAnimatedTransformToElementAnimations(animations.get(), 10.0, 2, 2);
+  int animation2_id =
+      AddAnimatedTransformToElementAnimations(animations.get(), 10.0, 2, 1);
+  animations->GetAnimationById(animation2_id)
+      ->set_time_offset(base::TimeDelta::FromMilliseconds(-10000));
+  animations->GetAnimationById(animation2_id)
+      ->set_fill_mode(Animation::FillMode::NONE);
   EXPECT_TRUE(client_.GetHasPotentialTransformAnimation(
       element_id_, ElementListType::ACTIVE));
   EXPECT_TRUE(client_.GetTransformIsCurrentlyAnimating(
@@ -2907,6 +2913,9 @@ TEST_F(ElementAnimationsTest, ObserverNotifiedWhenTransformAnimationChanges) {
   animations_impl->ActivateAnimations();
   EXPECT_TRUE(client_impl_.GetHasPotentialTransformAnimation(
       element_id_, ElementListType::ACTIVE));
+  // animation1 is in effect currently and animation2 isn't. As the element has
+  // atleast one animation that's in effect currently, client should be notified
+  // that the transform is currently animating.
   EXPECT_TRUE(client_impl_.GetTransformIsCurrentlyAnimating(
       element_id_, ElementListType::ACTIVE));
 
@@ -2918,6 +2927,7 @@ TEST_F(ElementAnimationsTest, ObserverNotifiedWhenTransformAnimationChanges) {
   events->events_.clear();
 
   animations->RemoveAnimation(animation_id);
+  animations->RemoveAnimation(animation2_id);
   EXPECT_FALSE(client_.GetHasPotentialTransformAnimation(
       element_id_, ElementListType::ACTIVE));
   EXPECT_FALSE(client_.GetTransformIsCurrentlyAnimating(
