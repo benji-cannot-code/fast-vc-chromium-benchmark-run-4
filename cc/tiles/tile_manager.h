d27e1465bf84d31287f5005568dce90585c7c66a
vmpstr <vmpstr@chromium.org>
2016-09-15 13:47:05 -0700

    cc: Add ImageManager to keep all the image locking in the same place.
    This patch refactors some tile manager code into ImageManager with
    an intent of moving all image related stuff (pre-decode images /
    checker images) into that class.
    
    R=enne
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2339703005
    Cr-Commit-Position: refs/heads/master@{#418963}

--- cc/tiles/tile_manager.h
+++ cc/tiles/tile_manager.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/resources/memory_history.h"
 #include "cc/resources/resource_pool.h"
 #include "cc/tiles/eviction_tile_priority_queue.h"
-#include "cc/tiles/image_decode_controller.h"
+#include "cc/tiles/image_manager.h"
 #include "cc/tiles/raster_tile_priority_queue.h"
 #include "cc/tiles/tile.h"
 #include "cc/tiles/tile_draw_info.h"
@@ -39,6 +39,7 @@ class TracedValue;
 namespace cc {
 class PictureLayerImpl;
 class ResourceProvider;
+class ImageDecodeController;
 
 class CC_EXPORT TileManagerClient {
  public:
@@ -313,7 +314,7 @@ class CC_EXPORT TileManager {
   bool did_check_for_completed_tasks_since_last_schedule_tasks_;
   bool did_oom_on_last_assign_;
 
-  ImageDecodeController* image_decode_controller_;
+  ImageManager image_manager_;
 
   RasterTaskCompletionStats flush_stats_;
 
@@ -338,7 +339,8 @@ class CC_EXPORT TileManager {
   uint64_t next_tile_id_;
 
   std::unordered_map<Tile::Id, std::vector<DrawImage>> scheduled_draw_images_;
-  std::vector<std::pair<DrawImage, scoped_refptr<TileTask>>> locked_images_;
+  std::vector<DrawImage> locked_images_;
+  std::vector<scoped_refptr<TileTask>> locked_image_tasks_;
 
   base::WeakPtrFactory<TileManager> task_set_finished_weak_ptr_factory_;
 
