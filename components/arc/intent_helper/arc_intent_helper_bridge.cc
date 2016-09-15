4a2971f65212ff29c94c3016fa2e6e37eb02800d
muyuanli <muyuanli@chromium.org>
2016-09-15 15:26:45 -0700

    cheets: deprecate setWallpaper from ArcIntentHelper.
    This CL deprecates setWallpaper call in intent_helper.mojom.
    
    It also removes set_wallpaper_delegate.h since it's no
    longer needed.
    
    This is going to be committed only after ag/1432663/ enters
    canary.
    
    Test=Manually test setting wallpaper with OGQ Backgrounds
      app in Arc++.
    
    Bug=642465
    
    Review-Url: https://codereview.chromium.org/2332063004
    Cr-Commit-Position: refs/heads/master@{#419015}

--- components/arc/intent_helper/arc_intent_helper_bridge.cc
+++ components/arc/intent_helper/arc_intent_helper_bridge.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/intent_helper/activity_icon_loader.h"
 #include "components/arc/intent_helper/link_handler_model_impl.h"
 #include "components/arc/intent_helper/local_activity_resolver.h"
-#include "components/arc/set_wallpaper_delegate.h"
 #include "ui/base/layout.h"
 #include "url/gurl.h"
 
@@ -29,10 +28,6 @@ namespace {
 constexpr char kArcIntentHelperPackageName[] = "org.chromium.arc.intent_helper";
 
 }  // namespace
-
-// TODO(muyuanli): This will be removed once SetWallpaperDelegate class is
-// removed.
-SetWallpaperDelegate* SetWallpaperDelegate::instance_ = nullptr;
 
 ArcIntentHelperBridge::ArcIntentHelperBridge(
     ArcBridgeService* bridge_service,
@@ -89,14 +84,10 @@ void ArcIntentHelperBridge::OpenWallpaperPicker() {
   ash::WmShell::Get()->wallpaper_delegate()->OpenSetWallpaperPage();
 }
 
-void ArcIntentHelperBridge::SetWallpaper(mojo::Array<uint8_t> jpeg_data) {
+void ArcIntentHelperBridge::SetWallpaperDeprecated(
+    mojo::Array<uint8_t> jpeg_data) {
   DCHECK(thread_checker_.CalledOnValidThread());
-  SetWallpaperDelegate* delegate = SetWallpaperDelegate::instance();
-  if (delegate == nullptr) {
-    LOG(ERROR) << "SetWallpaperDelegate is not available.";
-    return;
-  }
-  delegate->SetWallpaperJpeg(jpeg_data.PassStorage());
+  LOG(ERROR) << "IntentHelper.SetWallpaper is deprecated";
 }
 
 std::unique_ptr<ash::LinkHandlerModel> ArcIntentHelperBridge::CreateModel(
