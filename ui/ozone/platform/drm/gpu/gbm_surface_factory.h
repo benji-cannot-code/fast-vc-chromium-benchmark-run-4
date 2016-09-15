84b6e83cc4d3b00fdba8280f0cd55f1305f0b72e
kylechar <kylechar@chromium.org>
2016-09-15 06:44:50 -0700

    Convert Ozone GBM to use GLOzone.
    Add GLOzoneEGLGbm and remove old API calls.
    
    BUG=643368
    
    Review-Url: https://codereview.chromium.org/2327813002
    Cr-Commit-Position: refs/heads/master@{#418857}

--- ui/ozone/platform/drm/gpu/gbm_surface_factory.h
+++ ui/ozone/platform/drm/gpu/gbm_surface_factory.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_checker.h"
 #include "ui/gl/gl_implementation.h"
 #include "ui/gl/gl_surface.h"
+#include "ui/ozone/common/gl_ozone_egl.h"
 #include "ui/ozone/public/surface_factory_ozone.h"
 
 namespace ui {
@@ -34,20 +35,11 @@ class GbmSurfaceFactory : public SurfaceFactoryOzone {
   GbmSurfaceless* GetSurface(gfx::AcceleratedWidget widget) const;
 
   // SurfaceFactoryOzone:
-  scoped_refptr<gl::GLSurface> CreateViewGLSurface(
-      gl::GLImplementation implementation,
-      gfx::AcceleratedWidget widget) override;
-  scoped_refptr<gl::GLSurface> CreateSurfacelessViewGLSurface(
-      gl::GLImplementation implementation,
-      gfx::AcceleratedWidget widget) override;
-  scoped_refptr<gl::GLSurface> CreateOffscreenGLSurface(
-      gl::GLImplementation implementation,
-      const gfx::Size& size) override;
+  std::vector<gl::GLImplementation> GetAllowedGLImplementations() override;
+  GLOzone* GetGLOzone(gl::GLImplementation implementation) override;
 
-  intptr_t GetNativeDisplay() override;
   std::vector<gfx::BufferFormat> GetScanoutFormats(
       gfx::AcceleratedWidget widget) override;
-  bool LoadEGLGLES2Bindings() override;
   std::unique_ptr<SurfaceOzoneCanvas> CreateCanvasForWidget(
       gfx::AcceleratedWidget widget) override;
   scoped_refptr<ui::NativePixmap> CreateNativePixmap(
@@ -62,6 +54,8 @@ class GbmSurfaceFactory : public SurfaceFactoryOzone {
       const gfx::NativePixmapHandle& handle) override;
 
  private:
+  std::unique_ptr<GLOzone> egl_implementation_;
+
   base::ThreadChecker thread_checker_;
 
   DrmThreadProxy* drm_thread_;
