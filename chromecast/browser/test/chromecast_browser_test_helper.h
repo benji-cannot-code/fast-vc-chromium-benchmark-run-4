70fae06a0db529d7991c270ca5f57b666d5c304f
jbroman <jbroman@chromium.org>
2016-09-15 13:17:40 -0700

    Revert of Add CastMediaBlocker and BrowserTest (patchset #5 id:80001 of https://codereview.chromium.org/2330243002/ )
    Reason for revert:
    Test is failing on Cast Linux:
    https://build.chromium.org/p/chromium.linux/builders/Cast%20Linux/builds/31563
    
    Original issue's description:
    > Add CastMediaBlocker and BrowserTest
    >
    > CastMediaBlocker is a class which can prevent media from being played
    > on a web page.
    >
    > BUG=NONE
    > TEST=ChromecastShellMediaBlockingBrowserTest
    >
    > Committed: https://crrev.com/502bcc946bd12e907572f8b22ef4a0abbc8190d0
    > Cr-Commit-Position: refs/heads/master@{#418897}
    
    TBR=halliwell@chromium.org,byungchul@chromium.org,derekjchow@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=NONE
    
    Review-Url: https://codereview.chromium.org/2345053002
    Cr-Commit-Position: refs/heads/master@{#418950}

--- chromecast/browser/test/chromecast_browser_test_helper.h
+++ chromecast/browser/test/chromecast_browser_test_helper.h
@@ -25,7 +25,6 @@ class ChromecastBrowserTestHelper {
 
   virtual ~ChromecastBrowserTestHelper() {}
   virtual content::WebContents* NavigateToURL(const GURL& url) = 0;
-  virtual void BlockMediaLoading(bool block) = 0;
 };
 
 }  // namespace shell
