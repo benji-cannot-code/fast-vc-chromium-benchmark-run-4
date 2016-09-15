3d0eabc4319bfa8f2f33f39395fd55f91afddc0a
ricea <ricea@chromium.org>
2016-09-14 22:39:58 -0700

    Revert of Temporary: Detect WebSocket destruction order problem (patchset #2 id:20001 of https://codereview.chromium.org/2315213002/ )
    Reason for revert:
    We have learnt everything we are going to learn
    from this CHECK. Reverting as intended.
    
    This does not need to be relanded.
    
    Original issue's description:
    > Temporary: Detect WebSocket destruction order problem
    >
    > Add a CHECK() to crash the browser if a destruction order problem is
    > encountered.
    >
    > This will be reverted once the information is collected.
    >
    > BUG=641013
    >
    > Committed: https://crrev.com/23169a0f5421e6759d89b1feaa83b06b197a7039
    > Cr-Commit-Position: refs/heads/master@{#417522}
    
    TBR=yhirano@chromium.org,mmenke@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=641013
    
    Review-Url: https://codereview.chromium.org/2340253002
    Cr-Commit-Position: refs/heads/master@{#418791}

--- net/http/http_network_session.h
+++ net/http/http_network_session.h
@@ -274,10 +274,6 @@ class NET_EXPORT HttpNetworkSession
                     SSLConfig* server_config,
                     SSLConfig* proxy_config) const;
 
-  // TODO(ricea): Remove this by October 2016.
-  void IncrementActiveWebSockets() { ++active_websockets_; }
-  void DecrementActiveWebSockets() { --active_websockets_; }
-
  private:
   friend class HttpNetworkSessionPeer;
 
@@ -312,9 +308,6 @@ class NET_EXPORT HttpNetworkSession
   Params params_;
 
   std::unique_ptr<base::MemoryPressureListener> memory_pressure_listener_;
-
-  // TODO(ricea): Remove this by October 2016.
-  int active_websockets_ = 0;
 };
 
 }  // namespace net
