3c9fceb8c36f54be55ae01f7a863d3687a2dd23f
mcasas <mcasas@chromium.org>
2016-09-15 16:48:43 -0700

    Reland: ImageCapture: Connect takePhoto() VideoCaptureDeviceTest
    The original CL got reverted due to failing on a Lollipop bot. This
    CL avoids Lollipops while investigations continue.
    
    Original CL description ------------------------------------------------
    
    This CL enables VideoCaptureDeviceTest.TakePhoto for
    Android. (Note that, due to other circumstances, it'll
    only be enabled for devices implementing the Camera2
    API in non-legacy mode).
    
    The only modification is that Android creates JPEGs
    with APP1 JFIF header, because it includes EXIF information.
    
    BUG=518807, 626857
    
    TBR=emircan@chromium.org since the addec change is trivial.
    
    Review-Url: https://codereview.chromium.org/2344683003
    Cr-Commit-Position: refs/heads/master@{#419039}

--- media/capture/video/video_capture_device_unittest.cc
+++ media/capture/video/video_capture_device_unittest.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include "base/android/jni_android.h"
+#include "base/android/build_info.h"
 #include "media/capture/video/android/video_capture_device_android.h"
 #include "media/capture/video/android/video_capture_device_factory_android.h"
 #endif
@@ -56,7 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #elif defined(OS_ANDROID)
 #define MAYBE_AllocateBadSize AllocateBadSize
 #define MAYBE_CaptureMjpeg CaptureMjpeg
-#define MAYBE_TakePhoto DISABLED_TakePhoto
+#define MAYBE_TakePhoto TakePhoto
 #elif defined(OS_LINUX)
 // AllocateBadSize will hang when a real camera is attached and if more than one
 // test is trying to use the camera (even across processes). Do NOT renable
@@ -149,7 +150,7 @@ class MockImageCaptureClient : public base::RefCounted<MockImageCaptureClient> {
     EXPECT_EQ(0xFF, blob->data[0]);  // First SOI byte
     EXPECT_EQ(0xD8, blob->data[1]);  // Second SOI byte
     EXPECT_EQ(0xFF, blob->data[2]);  // First JFIF-APP0 byte
-    EXPECT_EQ(0xE0, blob->data[3]);  // Second JFIF-APP0 byte
+    EXPECT_EQ(0xE0, blob->data[3] & 0xF0);  // Second JFIF-APP0/APP1 byte
     OnCorrectPhotoTaken();
   }
   MOCK_METHOD0(OnCorrectPhotoTaken, void(void));
@@ -509,6 +510,14 @@ TEST_F(VideoCaptureDeviceTest, NoCameraSupportsPixelFormatMax) {
 TEST_F(VideoCaptureDeviceTest, MAYBE_TakePhoto) {
   if (!EnumerateAndFindUsableDevices())
     return;
+
+#if defined(OS_ANDROID)
+  // TODO(mcasas): fails on Lollipop devices, reconnect https://crbug.com/646840
+  if (base::android::BuildInfo::GetInstance()->sdk_int() <
+      base::android::SDK_VERSION_MARSHMALLOW) {
+    return;
+  }
+#endif
 
   std::unique_ptr<VideoCaptureDevice> device(
       video_capture_device_factory_->CreateDevice(
