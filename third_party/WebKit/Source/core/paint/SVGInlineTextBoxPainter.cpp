81e04ff008ae2cd1aa6b5451ae8aaa61b687d20f
wkorman <wkorman@chromium.org>
2016-09-15 13:10:27 -0700

    Update composition underline start/end constraints and add tests.
    - Constrain paint start/end to be bounded by actual text size
      and leave space for at least one character.
    - Include start() in paintLength for RTL in paintCompositionUnderline().
      It's included in paintEnd, and I believe its absence was a bug.
    - Early-out when painting SVG inline text if text length is zero,
      as we do in for non-SVG inline text.
    - Add some DCHECKs to reflect my understanding of key preconditions.
    - Add basic LTR/RTL composition underline layout tests.
    - Add a 'split' test case wherein InlineTextBox has start = 1.
    
    BUG=641261
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2337373002
    Cr-Commit-Position: refs/heads/master@{#418947}

--- third_party/WebKit/Source/core/paint/SVGInlineTextBoxPainter.cpp
+++ third_party/WebKit/Source/core/paint/SVGInlineTextBoxPainter.cpp
@@ -84,7 +84,8 @@ void SVGInlineTextBoxPainter::paint(const PaintInfo& paintInfo, const LayoutPoin
     ASSERT(paintInfo.phase == PaintPhaseForeground || paintInfo.phase == PaintPhaseSelection);
     ASSERT(m_svgInlineTextBox.truncation() == cNoTruncation);
 
-    if (m_svgInlineTextBox.getLineLayoutItem().style()->visibility() != EVisibility::Visible)
+    if (m_svgInlineTextBox.getLineLayoutItem().style()->visibility() != EVisibility::Visible
+        || !m_svgInlineTextBox.len())
         return;
 
     // We're explicitly not supporting composition & custom underlines and custom highlighters -- unlike InlineTextBox.
@@ -107,15 +108,14 @@ void SVGInlineTextBoxPainter::paint(const PaintInfo& paintInfo, const LayoutPoin
             && (haveSelection || InlineTextBoxPainter::paintsMarkerHighlights(textLayoutObject));
         DrawingRecorder recorder(paintInfo.context, m_svgInlineTextBox, displayItemType,
             boundsForDrawingRecorder(paintInfo, style, paintOffset, includeSelectionRect));
-        InlineTextBoxPainter(m_svgInlineTextBox).paintDocumentMarkers(
-            paintInfo, paintOffset, style,
+        InlineTextBoxPainter textPainter(m_svgInlineTextBox);
+        textPainter.paintDocumentMarkers(paintInfo, paintOffset, style,
             textLayoutObject.scaledFont(), DocumentMarkerPaintPhase::Background);
 
         if (!m_svgInlineTextBox.textFragments().isEmpty())
             paintTextFragments(paintInfo, parentLayoutObject);
 
-        InlineTextBoxPainter(m_svgInlineTextBox).paintDocumentMarkers(
-            paintInfo, paintOffset, style,
+        textPainter.paintDocumentMarkers(paintInfo, paintOffset, style,
             textLayoutObject.scaledFont(), DocumentMarkerPaintPhase::Foreground);
     }
 }
