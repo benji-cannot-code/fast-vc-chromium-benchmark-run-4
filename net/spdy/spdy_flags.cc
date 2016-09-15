cdff52f7ac866b7b586aca32f56658a7152e21fb
bnc <bnc@chromium.org>
2016-09-14 18:49:23 -0700

    Sort SPDY flags alphabetically and move to net namespace.
    Review-Url: https://codereview.chromium.org/2339473005
    Cr-Commit-Position: refs/heads/master@{#418751}

--- net/spdy/spdy_flags.cc
+++ net/spdy/spdy_flags.cc
@@ -5,8 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/spdy/spdy_flags.h"
 
-// Use NestedSpdyFramerDecoder.
-bool FLAGS_use_nested_spdy_framer_decoder = false;
+namespace net {
 
 // If true, SpdyFramer uses the new visitor methods OnHeaderFrameStart and
 // OnHeaderFrameEnd.  Fourth attempt.
@@ -14,3 +13,8 @@ bool FLAGS_chromium_http2_flag_spdy_framer_use_new_methods4 = true;
 
 // Use SpdyHeaderBlock::AppendValueOrAddHeader when adding to headers.
 bool FLAGS_chromium_http2_flag_use_new_spdy_header_block_header_joining = true;
+
+// Use NestedSpdyFramerDecoder.
+bool FLAGS_use_nested_spdy_framer_decoder = false;
+
+}  // namespace net
--- net/spdy/spdy_flags.h
+++ net/spdy/spdy_flags.h
@@ -8,10 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/base/net_export.h"
 
-NET_EXPORT_PRIVATE extern bool FLAGS_use_nested_spdy_framer_decoder;
+namespace net {
+
 NET_EXPORT_PRIVATE extern bool
     FLAGS_chromium_http2_flag_spdy_framer_use_new_methods4;
 NET_EXPORT_PRIVATE extern bool
     FLAGS_chromium_http2_flag_use_new_spdy_header_block_header_joining;
+NET_EXPORT_PRIVATE extern bool FLAGS_use_nested_spdy_framer_decoder;
+
+}  // namespace net
 
 #endif  // NET_SPDY_SPDY_FLAGS_H_
