5c99ae06deb62416ef44698607143abb39ca2e39
yzshen <yzshen@chromium.org>
2016-09-15 15:10:23 -0700

    Mojo C++ bindings: add more perf tests for Router/MultiplexRouter.
    These new tests measure performance of dispatching incoming messages on the same
    thread.
    
    Currently on Linux:
    RouterDispatchCost  3.93483e+06 times/second
    MultiplexRouterDispatchCost 3.39754e+06 times/second
    
    BUG=594244
    
    Review-Url: https://codereview.chromium.org/2340363002
    Cr-Commit-Position: refs/heads/master@{#419007}

--- mojo/public/cpp/bindings/lib/multiplex_router.h
+++ mojo/public/cpp/bindings/lib/multiplex_router.h
@@ -130,6 +130,10 @@ class MultiplexRouter
     return connector_.handle();
   }
 
+  bool SimulateReceivingMessageForTesting(Message* message) {
+    return filters_.Accept(message);
+  }
+
  private:
   class InterfaceEndpoint;
   struct Task;
