c125631567a3e21e9c4ead197037c34ee2f5c165
vakh <vakh@chromium.org>
2016-09-15 15:23:05 -0700

    Each DatabaseManager gets to decide which stores to track. V4LocalDatabaseManager now tracks the same lists for updates as well as full hashes. SafeBrowsingDatabaseManager tracks only API requests.
    Also, since hash_tables.h is deprecated, moved all code from base::hash_set to std::unordered_set using sed:
    $ sed -i 's/base::hash_set/std::unordered_set/g' *
    
    BUG=543161, 576864
    
    Review-Url: https://codereview.chromium.org/2345573002
    Cr-Commit-Position: refs/heads/master@{#419014}

--- components/safe_browsing_db/database_manager.h
+++ components/safe_browsing_db/database_manager.h
@@ -9,10 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SAFE_BROWSING_DB_DATABASE_MANAGER_H_
 #define COMPONENTS_SAFE_BROWSING_DB_DATABASE_MANAGER_H_
 
-#include <deque>
-#include <map>
 #include <set>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
 #include "base/gtest_prod_util.h"
@@ -29,6 +28,7 @@ class URLRequestContextGetter;
 
 namespace safe_browsing {
 
+struct UpdateListIdentifier;
 struct V4ProtocolConfig;
 class V4GetHashProtocolManager;
 
@@ -221,6 +221,10 @@ class SafeBrowsingDatabaseManager
                            CachedResultsAreEvicted);
 
   typedef std::set<SafeBrowsingApiCheck*> ApiCheckSet;
+
+  // Returns the lists that this DatabaseManager should get full hashes for.
+  virtual std::unordered_set<UpdateListIdentifier>
+  GetStoresForFullHashRequests();
 
   // Called on the IO thread when the SafeBrowsingProtocolManager has received
   // the full hash and api results for prefixes of the |url| argument in
