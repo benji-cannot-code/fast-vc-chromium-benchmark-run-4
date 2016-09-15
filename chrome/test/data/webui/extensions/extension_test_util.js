19cccd7e99c2f9ea60fc203259f6a236f12f2655
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-09-15 15:07:19 -0700

    [MD Extensions] Implement opening tab-style options pages
    Add logic to open options pages that open in tabs (embedded options
    pages still to come). Add a test for the same.
    
    BUG=529395
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2329443004
    Cr-Commit-Position: refs/heads/master@{#419004}

--- chrome/test/data/webui/extensions/extension_test_util.js
+++ chrome/test/data/webui/extensions/extension_test_util.js
@@ -120,6 +120,9 @@ cr.define('extension_test_util', function() {
 
     /** @override */
     repairItem: function(id) {},
+
+    /** @override */
+    showItemOptionsPage: function(id) {},
   };
 
   /**
