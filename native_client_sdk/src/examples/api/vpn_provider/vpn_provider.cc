39ce7a063e073a413b706a7e305e2900e8014faf
bradnelson <bradnelson@chromium.org>
2016-09-15 10:26:11 -0700

    Fix ppapi sample when built on windows.
    BUG=None
    TEST=None
    R=bbudge@chromium.org,sbc@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_nacl_sdk;master.tryserver.chromium.mac:mac_nacl_sdk
    
    Review-Url: https://codereview.chromium.org/2337973003
    Cr-Commit-Position: refs/heads/master@{#418895}

--- native_client_sdk/src/examples/api/vpn_provider/vpn_provider.cc
+++ native_client_sdk/src/examples/api/vpn_provider/vpn_provider.cc
@@ -14,6 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "vpn_provider_helper.h"
 
+#ifdef WIN32
+# undef PostMessage
+#endif
+
 class VpnProviderInstance : public pp::Instance {
  public:
   explicit VpnProviderInstance(PP_Instance instance)
