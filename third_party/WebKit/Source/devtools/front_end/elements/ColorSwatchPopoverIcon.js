40c47d3ab603d9897617fa0e2e64c5ef739e42f3
flandy <flandy@google.com>
2016-09-14 19:15:57 -0700

    DevTools: Add buttons to add box-shadow and text-shadow properties in SSP
    These buttons will appear in same toolbar as "Add background-color" and
    "Add color."
    
    BUG=639095
    
    Review-Url: https://codereview.chromium.org/2324313002
    Cr-Commit-Position: refs/heads/master@{#418759}

--- third_party/WebKit/Source/devtools/front_end/elements/ColorSwatchPopoverIcon.js
+++ third_party/WebKit/Source/devtools/front_end/elements/ColorSwatchPopoverIcon.js
@@ -225,6 +225,7 @@ WebInspector.ColorSwatchPopoverIcon.prototype = {
 WebInspector.ShadowSwatchPopoverHelper = function(treeElement, swatchPopoverHelper, shadowSwatch)
 {
     this._treeElement = treeElement;
+    this._treeElement[WebInspector.ShadowSwatchPopoverHelper._treeElementSymbol] = this;
     this._swatchPopoverHelper = swatchPopoverHelper;
     this._shadowSwatch = shadowSwatch;
     this._iconElement = shadowSwatch.iconElement();
@@ -236,6 +237,17 @@ WebInspector.ShadowSwatchPopoverHelper = function(treeElement, swatchPopoverHelp
     this._boundOnScroll = this._onScroll.bind(this);
 }
 
+WebInspector.ShadowSwatchPopoverHelper._treeElementSymbol = Symbol("WebInspector.ShadowSwatchPopoverHelper._treeElementSymbol");
+
+/**
+ * @param {!WebInspector.StylePropertyTreeElement} treeElement
+ * @return {?WebInspector.ShadowSwatchPopoverHelper}
+ */
+WebInspector.ShadowSwatchPopoverHelper.forTreeElement = function(treeElement)
+{
+    return treeElement[WebInspector.ShadowSwatchPopoverHelper._treeElementSymbol] || null;
+}
+
 WebInspector.ShadowSwatchPopoverHelper.prototype = {
     /**
      * @param {!Event} event
@@ -243,6 +255,11 @@ WebInspector.ShadowSwatchPopoverHelper.prototype = {
     _iconClick: function(event)
     {
         event.consume(true);
+        this.showPopover();
+    },
+
+    showPopover: function()
+    {
         if (this._swatchPopoverHelper.isShowing()) {
             this._swatchPopoverHelper.hide(true);
             return;
