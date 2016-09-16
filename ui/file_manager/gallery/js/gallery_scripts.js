2b75a028543a07cf3da589199eb18086662f1c6d
harukam <harukam@google.com>
2016-09-16 05:55:47 -0700

    Add an ability for resize in gallery.
    BUG=554440
    TEST=browser_tests --gtest_filter=GalleryBrowserTestInGuestMode.ResizeImageOnDownloads
    
    Review-Url: https://codereview.chromium.org/2299493002
    Cr-Commit-Position: refs/heads/master@{#419151}

--- ui/file_manager/gallery/js/gallery_scripts.js
+++ ui/file_manager/gallery/js/gallery_scripts.js
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="../../file_manager/foreground/js/share_client.js">
 //<include src="../../file_manager/foreground/js/thumbnail_loader.js">
 //<include src="../../file_manager/foreground/js/ui/file_manager_dialog_base.js">
+//<include src="../../file_manager/foreground/js/ui/files_alert_dialog.js">
 //<include src="../../file_manager/foreground/js/ui/files_confirm_dialog.js">
 //<include src="../../file_manager/foreground/js/ui/share_dialog.js">
 //<include src="../../file_manager/foreground/js/volume_manager_wrapper.js">
@@ -70,6 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="image_editor/image_editor.js">
 //<include src="image_editor/image_transform.js">
 //<include src="image_editor/image_adjust.js">
+//<include src="image_editor/image_resize.js">
 //<include src="image_editor/filter.js">
 //<include src="image_editor/image_encoder.js">
 //<include src="image_editor/exif_encoder.js">
