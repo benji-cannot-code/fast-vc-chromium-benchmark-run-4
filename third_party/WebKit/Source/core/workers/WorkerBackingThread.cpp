060a16d3df58c37c2e3c6bbaa6f86b3168e95c7f
keishi <keishi@chromium.org>
2016-09-15 21:22:20 -0700

    Enable Oilpan per thread heap on CompositorWorker
    Design doc: https://docs.google.com/document/d/14MzxMuUV9e5ZgxbZL3-jCMKa3S-UJGsbYSmRodLh9uc/edit#
    PSA email: https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/wROZOp-YKSc
    
    BUG=591606
    
    Review-Url: https://codereview.chromium.org/2344443003
    Cr-Commit-Position: refs/heads/master@{#419098}

--- third_party/WebKit/Source/core/workers/WorkerBackingThread.cpp
+++ third_party/WebKit/Source/core/workers/WorkerBackingThread.cpp
@@ -56,7 +56,7 @@ WorkerBackingThread::WorkerBackingThread(const char* name, bool shouldCallGCOnSh
 }
 
 WorkerBackingThread::WorkerBackingThread(WebThread* thread, bool shouldCallGCOnShutdown)
-    : m_backingThread(WebThreadSupportingGC::createForThread(thread))
+    : m_backingThread(WebThreadSupportingGC::createForThread(thread, true))
     , m_isOwningThread(false)
     , m_shouldCallGCOnShutdown(shouldCallGCOnShutdown)
 {
