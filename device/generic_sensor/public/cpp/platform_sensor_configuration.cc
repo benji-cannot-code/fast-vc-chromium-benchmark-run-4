80cba117c37d97dc0f92112aa03e00d3969955d7
alexander.shalamov <alexander.shalamov@intel.com>
2016-09-15 23:59:14 -0700

    [sensors] Android platform adaptation for Generic Sensor API
    Authors: Alexander Shalamov, Mikhail Pozdnyakov
    
    This patch introduces implementation of generic sensor interfaces
    for Android platform. Support for following sensors is added:
    ambient light, gyroscope, accelerometer, linear acceleration and magnetometer.
    
    BUG=606766
    
    Review-Url: https://codereview.chromium.org/2284613002
    Cr-Commit-Position: refs/heads/master@{#419119}

--- device/generic_sensor/public/cpp/platform_sensor_configuration.cc
+++ device/generic_sensor/public/cpp/platform_sensor_configuration.cc
@@ -20,4 +20,9 @@ bool PlatformSensorConfiguration::operator==(
   return frequency_ == other.frequency();
 }
 
+bool PlatformSensorConfiguration::operator>(
+    const PlatformSensorConfiguration& other) const {
+  return frequency() > other.frequency();
+}
+
 }  // namespace device
