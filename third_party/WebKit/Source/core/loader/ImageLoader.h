912e6e314b8a36ac1649212752443091fe40f15d
yoav <yoav@yoav.ws>
2016-09-16 14:41:41 -0700

    Turn off isLinkPreload() when used
    Make sure that isLinkPreload flag is turned off when the resource is used,
    making sure that it can be cleared earlier.
    
    BUG=635483
    
    Review-Url: https://codereview.chromium.org/2319483002
    Cr-Commit-Position: refs/heads/master@{#419283}

--- third_party/WebKit/Source/core/loader/ImageLoader.h
+++ third_party/WebKit/Source/core/loader/ImageLoader.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/CoreExport.h"
 #include "core/fetch/ImageResource.h"
 #include "core/fetch/ImageResourceObserver.h"
-#include "core/fetch/ResourceClient.h"
 #include "platform/heap/Handle.h"
 #include "wtf/HashSet.h"
 #include "wtf/WeakPtr.h"
