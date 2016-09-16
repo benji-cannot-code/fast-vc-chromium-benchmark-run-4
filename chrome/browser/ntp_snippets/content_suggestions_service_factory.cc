2c32b2ed4f4d1b3f3660e3ba5391c66f32e4c2f4
jkrcal <jkrcal@chromium.org>
2016-09-16 04:02:02 -0700

    Clear browsing data from ContentSuggestionService only if exists.
    Currently, we instantiate the service in BrowsingDataRemover even when
    it does not exist, yet. This means that we instantiate it on non-android
    platforms, in unit-tests, etc.
    
    This CL changes the logic so that the service is accessed only if it has
    been created previously. This way, the BrowsingDataRemover respects the
    start-up logic that decides whether to create the service or not.
    
    BUG=647222
    
    Review-Url: https://codereview.chromium.org/2342673004
    Cr-Commit-Position: refs/heads/master@{#419141}

--- chrome/browser/ntp_snippets/content_suggestions_service_factory.cc
+++ chrome/browser/ntp_snippets/content_suggestions_service_factory.cc
@@ -185,6 +185,13 @@ ContentSuggestionsService* ContentSuggestionsServiceFactory::GetForProfile(
       GetInstance()->GetServiceForBrowserContext(profile, true));
 }
 
+// static
+ContentSuggestionsService*
+ContentSuggestionsServiceFactory::GetForProfileIfExists(Profile* profile) {
+  return static_cast<ContentSuggestionsService*>(
+      GetInstance()->GetServiceForBrowserContext(profile, false));
+}
+
 ContentSuggestionsServiceFactory::ContentSuggestionsServiceFactory()
     : BrowserContextKeyedServiceFactory(
           "ContentSuggestionsService",
