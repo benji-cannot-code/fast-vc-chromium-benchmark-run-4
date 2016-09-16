4570368774074498ffc758235d5bd9f1c4d898c5
stevenjb <stevenjb@chromium.org>
2016-09-15 21:55:42 -0700

    MD Settings: Third party VPN support
    BUG=643729
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2311473002
    Cr-Commit-Position: refs/heads/master@{#419104}

--- chrome/browser/resources/settings/internet_page/internet_detail_page.js
+++ chrome/browser/resources/settings/internet_page/internet_detail_page.js
@@ -165,7 +165,7 @@ Polymer({
 
     // Update the detail page title.
     this.parentNode.pageTitle =
-        CrOnc.getNetworkName(this.networkProperties, this.i18n);
+        CrOnc.getNetworkName(this.networkProperties, this);
   },
 
   /** @private */
@@ -622,13 +622,16 @@ Polymer({
           'RestrictedConnectivity', 'Cellular.ServingOperator.Name');
     }
     if (this.networkProperties.Type == CrOnc.Type.VPN) {
-      fields.push('VPN.Host', 'VPN.Type');
-      if (this.networkProperties.VPN.Type == 'OpenVPN')
-        fields.push('VPN.OpenVPN.Username');
-      else if (this.networkProperties.VPN.Type == 'L2TP-IPsec')
-        fields.push('VPN.L2TP.Username');
-      else if (this.networkProperties.VPN.Type == 'ThirdPartyVPN')
+      let vpnType = CrOnc.getActiveValue(this.networkProperties.VPN.Type);
+      if (vpnType == 'ThirdPartyVPN') {
         fields.push('VPN.ThirdPartyVPN.ProviderName');
+      } else {
+        fields.push('VPN.Host', 'VPN.Type');
+        if (vpnType == 'OpenVPN')
+          fields.push('VPN.OpenVPN.Username');
+        else if (vpnType == 'L2TP-IPsec')
+          fields.push('VPN.L2TP.Username');
+      }
     }
     if (this.networkProperties.Type == CrOnc.Type.WI_FI)
       fields.push('RestrictedConnectivity');
@@ -701,6 +704,14 @@ Polymer({
    */
   hasDeviceFields_: function() {
     return this.hasVisibleFields_(this.getDeviceFields_());
+  },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  hasAdvancedOrDeviceFields_: function() {
+    return this.hasAdvancedFields_() || this.hasDeviceFields_();
   },
 
   /**
--- ui/webui/resources/cr_elements/network/compiled_resources2.gyp
+++ ui/webui/resources/cr_elements/network/compiled_resources2.gyp
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     {
       'target_name': 'cr_onc_types',
       'dependencies': [
+        '<(DEPTH)/ui/webui/resources/js/compiled_resources2.gyp:i18n_behavior',
         '<(EXTERNS_GYP):networking_private',
       ],
       'includes': ['../../../../../third_party/closure_compiler/compile_js2.gypi'],
