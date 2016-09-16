4c75bed5b18b0d5b7368295982aac3c7cf71cc36
cwallez <cwallez@chromium.org>
2016-09-16 08:10:41 -0700

    gpu/config: Add workaround rewrite do-while on Mac < 10.11
    BUG=644669
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2343863003
    Cr-Commit-Position: refs/heads/master@{#419171}

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // throughout the table.
 // clang-format off
 #define GPU_DRIVER_BUG_WORKAROUNDS(GPU_OP)                   \
+  GPU_OP(ADD_AND_TRUE_TO_LOOP_CONDITION,                     \
+         add_and_true_to_loop_condition)                     \
   GPU_OP(AVDA_DONT_COPY_PICTURES,                            \
          avda_dont_copy_pictures)                            \
   GPU_OP(AVOID_EGL_IMAGE_TARGET_TEXTURE_REUSE,               \
@@ -132,8 +134,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          restore_scissor_on_fbo_change)                      \
   GPU_OP(REVERSE_POINT_SPRITE_COORD_ORIGIN,                  \
          reverse_point_sprite_coord_origin)                  \
-  GPU_OP(ADD_AND_TRUE_TO_LOOP_CONDITION,                     \
-         add_and_true_to_loop_condition)                     \
+  GPU_OP(REWRITE_DO_WHILE_LOOPS,                             \
+         rewrite_do_while_loops)                             \
   GPU_OP(REWRITE_TEXELFETCHOFFSET_TO_TEXELFETCH,             \
          rewrite_texelfetchoffset_to_texelfetch)             \
   GPU_OP(SCALARIZE_VEC_AND_MAT_CONSTRUCTOR_ARGS,             \
