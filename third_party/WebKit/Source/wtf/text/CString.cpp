2d9c2f312f92ea88a8562f5563e076ebaad6d7ff
esprehn <esprehn@chromium.org>
2016-09-15 23:47:25 -0700

    Make CString more similar to WTF::String.
    Nothing uses the copy on write feature, and it's implemented in a
    somewhat confusing way since mutableData() would create a copy of the
    underlying buffer, but any instances created from the CString after
    calling mutableData() would still share with the mutable buffer allowing
    modification of all of the CString instances. Instead lets just remove
    this feature.
    
    Doing this makes CString and CStringBuffer immutable and makes their
    API look very similar to String and StringImpl. A future patch will
    rename CStringBuffer to CStringImpl to match.
    
    I also threw in a bunch of documentation comments. :)
    
    Review-Url: https://codereview.chromium.org/2345893002
    Cr-Commit-Position: refs/heads/master@{#419115}

--- third_party/WebKit/Source/wtf/text/CString.cpp
+++ third_party/WebKit/Source/wtf/text/CString.cpp
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-
 #include "wtf/text/CString.h"
 
 #include "wtf/ASCIICType.h"
@@ -36,14 +35,17 @@ using namespace std;
 
 namespace WTF {
 
-PassRefPtr<CStringBuffer> CStringBuffer::createUninitialized(size_t length)
+PassRefPtr<CStringBuffer> CStringBuffer::createUninitialized(size_t length, char*& data)
 {
+    // TODO(esprehn): This doesn't account for the NUL.
     RELEASE_ASSERT(length < (numeric_limits<unsigned>::max() - sizeof(CStringBuffer)));
 
     // The +1 is for the terminating NUL character.
     size_t size = sizeof(CStringBuffer) + length + 1;
-    CStringBuffer* stringBuffer = static_cast<CStringBuffer*>(Partitions::bufferMalloc(size, WTF_HEAP_PROFILER_TYPE_NAME(CStringBuffer)));
-    return adoptRef(new (stringBuffer) CStringBuffer(length));
+    CStringBuffer* buffer = static_cast<CStringBuffer*>(Partitions::bufferMalloc(size, WTF_HEAP_PROFILER_TYPE_NAME(CStringBuffer)));
+    data = reinterpret_cast<char*>(buffer + 1);
+    data[length] = '\0';
+    return adoptRef(new (buffer) CStringBuffer(length));
 }
 
 void CStringBuffer::operator delete(void* ptr)
@@ -51,60 +53,15 @@ void CStringBuffer::operator delete(void* ptr)
     Partitions::bufferFree(ptr);
 }
 
-CString::CString(const char* str)
+CString::CString(const char* chars, size_t length)
 {
-    if (!str)
-        return;
-
-    init(str, strlen(str));
-}
-
-CString::CString(const char* str, size_t length)
-{
-    if (!str) {
-        ASSERT(!length);
+    if (!chars) {
+        DCHECK_EQ(length, 0u);
         return;
     }
-
-    init(str, length);
-}
-
-void CString::init(const char* str, size_t length)
-{
-    ASSERT(str);
-
-    m_buffer = CStringBuffer::createUninitialized(length);
-    memcpy(m_buffer->mutableData(), str, length);
-    m_buffer->mutableData()[length] = '\0';
-}
-
-char* CString::mutableData()
-{
-    copyBufferIfNeeded();
-    if (!m_buffer)
-        return 0;
-    return m_buffer->mutableData();
-}
-
-CString CString::newUninitialized(size_t length, char*& characterBuffer)
-{
-    CString result;
-    result.m_buffer = CStringBuffer::createUninitialized(length);
-    char* bytes = result.m_buffer->mutableData();
-    bytes[length] = '\0';
-    characterBuffer = bytes;
-    return result;
-}
-
-void CString::copyBufferIfNeeded()
-{
-    if (!m_buffer || m_buffer->hasOneRef())
-        return;
-
-    RefPtr<CStringBuffer> buffer = m_buffer.release();
-    size_t length = buffer->length();
-    m_buffer = CStringBuffer::createUninitialized(length);
-    memcpy(m_buffer->mutableData(), buffer->data(), length + 1);
+    char* data;
+    m_buffer = CStringBuffer::createUninitialized(length, data);
+    memcpy(data, chars, length);
 }
 
 bool CString::isSafeToSendToAnotherThread() const
