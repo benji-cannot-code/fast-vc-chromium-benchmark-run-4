152eb8a6e33db3318e839eda360b7996a4a20f14
sky <sky@chromium.org>
2016-09-15 17:02:46 -0700

    Adds bug ids to some NOTIMPLEMENTEDs
    For better tracking.
    
    BUG=647433,647412,647416,647409,647421,647417
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2345613003
    Cr-Commit-Position: refs/heads/master@{#419044}

--- ash/mus/new_window_delegate_mus.cc
+++ ash/mus/new_window_delegate_mus.cc
@@ -46,7 +46,7 @@ void NewWindowDelegateMus::RestoreTab() {
 }
 
 void NewWindowDelegateMus::ShowKeyboardOverlay() {
-  // TODO: http://crbug.com/631836.
+  // TODO: http://crbug.com/647433.
   NOTIMPLEMENTED();
 }
 
--- ash/mus/shell_delegate_mus.cc
+++ ash/mus/shell_delegate_mus.cc
@@ -171,6 +171,7 @@ ShelfDelegate* ShellDelegateMus::CreateShelfDelegate(ShelfModel* model) {
 }
 
 SystemTrayDelegate* ShellDelegateMus::CreateSystemTrayDelegate() {
+  // TODO: http://crbug.com/647412.
   NOTIMPLEMENTED() << " Using the default SystemTrayDelegate implementation";
   return new DefaultSystemTrayDelegate;
 }
@@ -180,6 +181,7 @@ std::unique_ptr<WallpaperDelegate> ShellDelegateMus::CreateWallpaperDelegate() {
 }
 
 SessionStateDelegate* ShellDelegateMus::CreateSessionStateDelegate() {
+  // TODO: http://crbug.com/647416.
   NOTIMPLEMENTED() << " Using a stub SessionStateDeleagte implementation";
   return new SessionStateDelegateStub;
 }
@@ -193,11 +195,13 @@ NewWindowDelegate* ShellDelegateMus::CreateNewWindowDelegate() {
 }
 
 MediaDelegate* ShellDelegateMus::CreateMediaDelegate() {
+  // TODO: http://crbug.com/647409.
   NOTIMPLEMENTED() << " Using a stub MediaDelegate implementation";
   return new MediaDelegateStub;
 }
 
 std::unique_ptr<PaletteDelegate> ShellDelegateMus::CreatePaletteDelegate() {
+  // TODO: http://crbug.com/647417.
   NOTIMPLEMENTED();
   return nullptr;
 }
@@ -208,6 +212,7 @@ ui::MenuModel* ShellDelegateMus::CreateContextMenu(WmShelf* wm_shelf,
 }
 
 GPUSupport* ShellDelegateMus::CreateGPUSupport() {
+  // TODO: http://crbug.com/647421.
   NOTIMPLEMENTED() << " Using a stub GPUSupport implementation";
   return new GPUSupportStub();
 }
