61de36e1ff695e6a56ee2697dd2afb4a72e52a8c
bradnelson <bradnelson@chromium.org>
2016-09-16 16:52:35 -0700

    [nacl-sdk] Fix nacl sdk example on windows.
    BUG=None
    TEST=None
    R=sbc@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_nacl_sdk;master.tryserver.chromium.mac:mac_nacl_sdk;master.tryserver.chromium.win:win_nacl_sdk
    BUG=646185
    
    Review-Url: https://codereview.chromium.org/2342353003
    Cr-Commit-Position: refs/heads/master@{#419332}

--- native_client_sdk/src/examples/api/vpn_provider/vpn_provider_helper.cc
+++ native_client_sdk/src/examples/api/vpn_provider/vpn_provider_helper.cc
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ppapi/cpp/var_dictionary.h"
 
+#ifdef WIN32
+#  undef PostMessage
+#endif
+
 VpnProviderHelper::VpnProviderHelper(pp::Instance* instance)
     : instance_(instance),
       thread_(pp::InstanceHandle(instance)),
