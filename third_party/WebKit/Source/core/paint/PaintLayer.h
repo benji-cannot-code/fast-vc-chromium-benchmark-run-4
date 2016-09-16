ae0454bcf88a398daf9456ec5b692aec141f7885
fs <fs@opera.com>
2016-09-16 15:03:47 -0700

    Let clients of FilterEffectBuilder compute/provide the reference-box
    Rather than having code to compute the reference box for the various
    clients that use FilterEffectBuilder in the class itself, let clients
    compute the desired reference box themselves and pass it along.
    This separates concerns, and makes code for computing the reference box
    less defensive. It also eliminates the need to add even more cases in
    the future (like for SVG shorthand support.)
    
    Also push calls to resolveReferenceFilters() in PaintLayer closer to the
    filter-building calls, and rename computeFilterOperations to
    addReflectionToFilterOperations. This allows us to eliminate a redundant
    call to resolveReferenceFilters() in the mapRectForFilter() code-path.
    
    BUG=439970
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    
    Review-Url: https://codereview.chromium.org/2343173002
    Cr-Commit-Position: refs/heads/master@{#419290}

--- third_party/WebKit/Source/core/paint/PaintLayer.h
+++ third_party/WebKit/Source/core/paint/PaintLayer.h
@@ -374,6 +374,7 @@ public:
     LayoutRect physicalBoundingBoxIncludingReflectionAndStackingChildren(const LayoutPoint& offsetFromRoot, CalculateBoundsOptions = MaybeIncludeTransformForAncestorLayer) const;
     LayoutRect fragmentsBoundingBox(const PaintLayer* ancestorLayer) const;
 
+    FloatRect boxForFilter() const;
     LayoutRect boxForClipPath() const;
 
     LayoutRect boundingBoxForCompositingOverlapTest() const;
@@ -789,7 +790,7 @@ private:
     bool requiresStackingNode() const { return true; }
     void updateStackingNode();
 
-    FilterOperations computeFilterOperations(const ComputedStyle&) const;
+    FilterOperations addReflectionToFilterOperations(const ComputedStyle&) const;
     void updateReflectionInfo(const ComputedStyle*);
     FilterEffectBuilder* updateFilterEffectBuilder() const;
 
