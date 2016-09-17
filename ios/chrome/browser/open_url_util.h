eea9702c7707ffb0fda0cb5a7ac272f065802589
sczs <sczs@google.com>
2016-09-16 17:10:05 -0700

    Creates a file that contains a function for openingURL's that uses UIApplication OpenURL(). The function will use the non deprecated method if its available (iOS10+)
    BUG=622735
    
    Review-Url: https://codereview.chromium.org/2340873002
    Cr-Commit-Position: refs/heads/master@{#419336}

---
+++ ios/chrome/browser/open_url_util.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_OPEN_URL_UTIL_H_
+#define IOS_CHROME_BROWSER_OPEN_URL_UTIL_H_
+
+#import <Foundation/Foundation.h>
+
+// Wrapper method for UIApplication openURL() that uses
+// the non-deprecated method when it is available (iOS10+).
+void OpenUrlWithCompletionHandler(NSURL* url,
+                                  void (^completion_handler)(BOOL success));
+
+#endif  // IOS_CHROME_BROWSER_OPEN_URL_UTIL_H_
---
+++ ios/chrome/browser/open_url_util.mm
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/open_url_util.h"
+
+#import <UIKit/UIKit.h>
+
+#include "base/ios/ios_util.h"
+
+void OpenUrlWithCompletionHandler(NSURL* url,
+                                  void (^completion_handler)(BOOL success)) {
+  if (base::ios::IsRunningOnIOS10OrLater()) {
+    [[UIApplication sharedApplication] openURL:url
+                                       options:@{}
+                             completionHandler:completion_handler];
+  } else {
+    BOOL result = [[UIApplication sharedApplication] openURL:url];
+    if (!completion_handler)
+      return;
+    dispatch_async(dispatch_get_main_queue(), ^{
+      completion_handler(result);
+    });
+  }
+}
