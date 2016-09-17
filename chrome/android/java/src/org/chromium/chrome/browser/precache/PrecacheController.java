3280a0aaaacf4113733033724069ffc62b6c5ead
rajendrant <rajendrant@chromium.org>
2016-09-16 17:12:13 -0700

    Record UMA for battery level when precache starts and ends
    BUG=646184
    
    Review-Url: https://codereview.chromium.org/2333063002
    Cr-Commit-Position: refs/heads/master@{#419337}

--- chrome/android/java/src/org/chromium/chrome/browser/precache/PrecacheController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/precache/PrecacheController.java
@@ -370,6 +370,7 @@ public class PrecacheController {
                 recordPeriodicTaskIntervalHistogram();
                 cancelPrecacheCompletionTask(mAppContext);
             }
+            recordBatteryLevelAtStart();
             registerDeviceStateReceiver();
             acquirePrecachingWakeLock();
             startPrecachingAfterSyncInit();
@@ -440,6 +441,7 @@ public class PrecacheController {
         if (precachingIncomplete) {
             schedulePrecacheCompletionTask(mAppContext);
         }
+        recordBatteryLevelAtEnd();
         mHandler.removeCallbacks(mTimeoutRunnable);
         mAppContext.unregisterReceiver(mDeviceStateReceiver);
         releasePrecachingWakeLock();
@@ -543,5 +545,22 @@ public class PrecacheController {
         prefs.edit()
                 .putLong(PREF_PRECACHE_PERIODIC_TASK_START_TIME_MS, current_start_time_ms)
                 .apply();
+    }
+
+    private void recordBatteryLevelAtStart() {
+        mDeviceState.saveCurrentBatteryPercentage(mAppContext);
+
+        // Report battery percentage.
+        RecordHistogram.recordPercentageHistogram(
+                "Precache.BatteryPercentage.Start", mDeviceState.getSavedBatteryPercentage());
+    }
+
+    private void recordBatteryLevelAtEnd() {
+        int delta_percentage = mDeviceState.getCurrentBatteryPercentage(mAppContext)
+                - mDeviceState.getSavedBatteryPercentage();
+        if (delta_percentage >= 0) {
+            RecordHistogram.recordPercentageHistogram(
+                    "Precache.BatteryPercentageDiff.End", delta_percentage);
+        }
     }
 }
