977568ae7a1d4c5c0130c9a3bd27ed5fe7aa2552
aboxhall <aboxhall@chromium.org>
2016-09-16 20:32:27 -0700

    Show ancestor hierarchy in accessibility panel
    BUG=560525
    
    Review-Url: https://codereview.chromium.org/2322413003
    Cr-Commit-Position: refs/heads/master@{#419369}

--- third_party/WebKit/Source/modules/accessibility/InspectorAccessibilityAgent.h
+++ third_party/WebKit/Source/modules/accessibility/InspectorAccessibilityAgent.h
@@ -12,7 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class AXObjectCacheImpl;
 class InspectorDOMAgent;
+class LocalFrame;
 class Page;
 
 class MODULES_EXPORT InspectorAccessibilityAgent : public InspectorBaseAgent<protocol::Accessibility::Metainfo> {
@@ -24,7 +26,7 @@ public:
     DECLARE_VIRTUAL_TRACE();
 
     // Protocol methods.
-    void getAXNode(ErrorString*, int nodeId, Maybe<protocol::Accessibility::AXNode>* accessibilityNode) override;
+    void getAXNodeChain(ErrorString*, int domNodeId, bool fetchAncestors, std::unique_ptr<protocol::Array<protocol::Accessibility::AXNode>>*) override;
 
 private:
     Member<Page> m_page;
