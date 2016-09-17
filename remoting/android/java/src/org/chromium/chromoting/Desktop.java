e90c2ab79b70a4b470084fe60dd0162f3a3010aa
yuweih <yuweih@chromium.org>
2016-09-16 17:23:35 -0700

    [Remoting Android] Fix split-screen crash
    Previously we assumed that DestopView.onAttachedToWindow() will only be called
    after DesktopView.init() is called but this is not true when we enter split
    screen mode. Since the lifecycle of View.onAttached... and View.onDetached in
    relate to Activity.onCreate() and Activity.onDestroy() is not well documented
    and behaves quite surprisingly, it may be better to just rely on the lifecycle
    of the activity itself.
    
    This CL removes DesktopView's onAttachedToWindow() and onDetachedFromWindow()
    overrides and handles initialization and invalidation by Desktop's onCreate()
    and onDestroy().
    
    BUG=647823
    
    Review-Url: https://codereview.chromium.org/2348563004
    Cr-Commit-Position: refs/heads/master@{#419343}

--- remoting/android/java/src/org/chromium/chromoting/Desktop.java
+++ remoting/android/java/src/org/chromium/chromoting/Desktop.java
@@ -178,6 +178,12 @@ public class Desktop
         mClient.enableVideoChannel(false);
     }
 
+    @Override
+    protected void onDestroy() {
+        mRemoteHostDesktop.destroy();
+        super.onDestroy();
+    }
+
     /** Called to initialize the action bar. */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
