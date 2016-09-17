22eafc43d2c357feebed4cfce167917109794517
djacobo <djacobo@google.com>
2016-09-16 17:07:27 -0700

    Closing unused new tabs for ARC
    Some links open a new tab while navigating, given that we redirect some
    of the traffic to ARC, it can be necessary to close any unused tab.
    
    Since we are closing the contained WebContents on a tab, we need to make
    sure the callback within the UI doesn't get called twice, since intent
    picker has an observer for the WebContents.
    
    BUG=647806
    TEST=manual test
    
    Review-Url: https://codereview.chromium.org/2347273002
    Cr-Commit-Position: refs/heads/master@{#419335}

--- chrome/browser/chromeos/arc/arc_navigation_throttle.cc
+++ chrome/browser/chromeos/arc/arc_navigation_throttle.cc
@@ -17,7 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/intent_helper/arc_intent_helper_bridge.h"
 #include "components/arc/intent_helper/local_activity_resolver.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_handle.h"
+#include "content/public/browser/web_contents.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "ui/base/page_transition_types.h"
 
@@ -232,6 +234,8 @@ void ArcNavigationThrottle::OnIntentPickerClosed(
                           handlers[selected_app_index]->package_name);
         handle->CancelDeferredNavigation(
             content::NavigationThrottle::CANCEL_AND_IGNORE);
+        if (handle->GetWebContents()->GetController().IsInitialNavigation())
+          handle->GetWebContents()->Close();
       }
       break;
     }
