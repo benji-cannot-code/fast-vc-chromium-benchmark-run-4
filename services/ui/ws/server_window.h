9f3adb475cb726bfeee43d457540a316087c2a10
sky <sky@chromium.org>
2016-09-18 10:27:23 -0700

    Makes WindowTree::DeleteWindow() unembed for roots
    Currently DeleteWindow() is (mostly) ignored if the client doesn't own
    the root. This change allows the client to call delete on a root and
    it's treated as unembedded the client from that root.
    
    BUG=647509
    TEST=covered by tests
    R=sadrul@chromium.org
    
    Review-Url: https://codereview.chromium.org/2349003002
    Cr-Commit-Position: refs/heads/master@{#419401}

--- services/ui/ws/server_window.h
+++ services/ui/ws/server_window.h
@@ -186,6 +186,9 @@ class ServerWindow {
 
   ServerWindowDelegate* delegate() { return delegate_; }
 
+  // Called when the window is no longer an embed root.
+  void OnEmbeddedAppDisconnected();
+
 #if !defined(NDEBUG) || defined(DCHECK_ALWAYS_ON)
   std::string GetDebugWindowHierarchy() const;
   std::string GetDebugWindowInfo() const;
--- services/ui/ws/window_tree.cc
+++ services/ui/ws/window_tree.cc
@@ -203,7 +203,7 @@ void WindowTree::OnWindowDestroyingTreeImpl(WindowTree* tree) {
   if (event_source_wms_ && event_source_wms_->window_tree() == tree)
     event_source_wms_ = nullptr;
 
-  // Notify our client if |tree| was embedded in any of our views.
+  // Notify our client if |tree| was embedded in any of our windows.
   for (const auto* tree_root : tree->roots_) {
     const bool owns_tree_root = tree_root->id().client_id == id_;
     if (owns_tree_root) {
@@ -606,7 +606,7 @@ void WindowTree::ProcessWindowReorder(const ServerWindow* window,
   window_server_->OnTreeMessagedClient(id_);
 }
 
-void WindowTree::ProcessWindowDeleted(const ServerWindow* window,
+void WindowTree::ProcessWindowDeleted(ServerWindow* window,
                                       bool originated_change) {
   if (window->id().client_id == id_)
     created_window_map_.erase(window->id());
@@ -888,8 +888,7 @@ void WindowTree::RemoveFromKnown(const ServerWindow* window,
     RemoveFromKnown(child, local_windows);
 }
 
-void WindowTree::RemoveRoot(const ServerWindow* window,
-                            RemoveRootReason reason) {
+void WindowTree::RemoveRoot(ServerWindow* window, RemoveRootReason reason) {
   DCHECK(roots_.count(window) > 0);
   roots_.erase(window);
 
@@ -911,6 +910,17 @@ void WindowTree::RemoveRoot(const ServerWindow* window,
   RemoveFromKnown(window, &local_windows);
   for (size_t i = 0; i < local_windows.size(); ++i)
     local_windows[i]->parent()->Remove(local_windows[i]);
+
+  if (reason == RemoveRootReason::UNEMBED) {
+    window->OnEmbeddedAppDisconnected();
+
+    // Notify the owner of the window it no longer has a client embedded in it.
+    WindowTree* owning_tree =
+        window_server_->GetTreeWithId(window->id().client_id);
+    DCHECK(owning_tree && owning_tree != this);
+    owning_tree->client()->OnEmbeddedAppDisconnected(
+        owning_tree->ClientWindowIdForWindow(window).id);
+  }
 }
 
 Array<mojom::WindowDataPtr> WindowTree::WindowsToWindowDatas(
@@ -1114,12 +1124,17 @@ void WindowTree::DeleteWindow(uint32_t change_id, Id transport_window_id) {
   ServerWindow* window =
       GetWindowByClientId(ClientWindowId(transport_window_id));
   bool success = false;
-  bool should_close = window && (access_policy_->CanDeleteWindow(window) ||
-                                 ShouldRouteToWindowManager(window));
-  if (should_close) {
-    WindowTree* tree =
-        window_server_->GetTreeWithId(window->id().client_id);
-    success = tree && tree->DeleteWindowImpl(this, window);
+  if (window && roots_.count(window) > 0) {
+    success = true;
+    window_server_->OnTreeMessagedClient(id_);
+    RemoveRoot(window, RemoveRootReason::UNEMBED);
+  } else {
+    bool should_close = window && (access_policy_->CanDeleteWindow(window) ||
+                                   ShouldRouteToWindowManager(window));
+    if (should_close) {
+      WindowTree* tree = window_server_->GetTreeWithId(window->id().client_id);
+      success = tree && tree->DeleteWindowImpl(this, window);
+    }
   }
   client()->OnChangeCompleted(change_id, success);
 }
