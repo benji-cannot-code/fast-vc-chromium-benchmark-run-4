d504154913ebc474041c6449768383431a7a76b2
alexander.shalamov <alexander.shalamov@intel.com>
2016-09-19 05:32:48 -0700

    [sensors] Ambient light sensor bindings implementation
    This patch implements AmbientLightSensor [1] blink bindings and adds
    LayoutTest helpers for testing sensors that are based on Generic Sensor API.
    
    Following layout tests added to test new functionality:
    IDL tests.
     - third_party/WebKit/LayoutTests/sensor/idl-AmbientLightSensor.html
     - third_party/WebKit/LayoutTests/sensor/idl-AmbientLightSensorReading.html
    AmbientLightSensor tests.
     - third_party/WebKit/LayoutTests/sensor/ambient-light-sensor.html
    
    Intent to Implement:
    https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/TkfdVqYAYiE/xLGN2b1-AAAJ
    
    [1] ED specification for Ambient Light Sensor http://w3c.github.io/ambient-light/
    
    BUG=606766
    
    Review-Url: https://codereview.chromium.org/2332323002
    Cr-Commit-Position: refs/heads/master@{#419438}

--- third_party/WebKit/Source/modules/sensor/SensorReading.h
+++ third_party/WebKit/Source/modules/sensor/SensorReading.h
@@ -17,7 +17,7 @@ class ExecutionContext;
 class ScriptState;
 
 class SensorReading
-    : public GarbageCollected<SensorReading>
+    : public GarbageCollectedMixin
     , public ScriptWrappable {
     DEFINE_WRAPPERTYPEINFO();
 public:
