f89d61f0cd1fa3b09d0acba7564c79219b29fabe
tguilbert <tguilbert@chromium.org>
2016-09-19 14:11:25 -0700

    Add base::UnguessableToken
    cc::SurfaceId, gpu::Mailbox and ScopedSurfaceRequestManager need an
    unguessable identifier. Security recommends using 128 bits to make sure
    an ID is unguessable. However, there is no conveniently serializable
    way to represent 128 bits.
    
    This change introduces base::UnguessableToken, a 128 bit class with a
    cryptographically strong Create() function. UnguessableToken can be
    used by themselves, or as part of an aggregate ID.
    
    An empty UnguessableToken is a valid value. It is however illegal to
    send empty UnguessableToken across processes (because the resource that
    is supposed to be protected by the token would now be guessable).
    Sending empty tokens across processes is a security issue, and should
    be handled as such.
    
    This change also introduces the appropriate code to send tokens
    over IPC and Mojo. base::Optional should be used in cases where it may
    be valid to send no token (rather than sending an empty token).
    
    TEST=Added unittests. Also tested in a prototype that uses IPC and Mojo.
    BUG=643857
    
    Review-Url: https://codereview.chromium.org/2333443002
    Cr-Commit-Position: refs/heads/master@{#419550}

--- ipc/ipc_message_utils_unittest.cc
+++ ipc/ipc_message_utils_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/json/json_reader.h"
+#include "base/unguessable_token.h"
 #include "ipc/ipc_channel_handle.h"
 #include "ipc/ipc_message.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -198,6 +199,26 @@ TEST(IPCMessageUtilsTest, OptionalSet) {
   EXPECT_TRUE(IPC::ReadParam(&pickle, &iter, &unserialized_opt));
   EXPECT_TRUE(unserialized_opt);
   EXPECT_EQ(opt.value(), unserialized_opt.value());
+}
+
+TEST(IPCMessageUtilsTest, UnguessableTokenTest) {
+  base::UnguessableToken token = base::UnguessableToken::Create();
+  base::Pickle pickle;
+  IPC::WriteParam(&pickle, token);
+
+  base::PickleSizer sizer;
+  IPC::GetParamSize(&sizer, token);
+
+  EXPECT_EQ(sizer.payload_size(), pickle.payload_size());
+
+  std::string log;
+  IPC::LogParam(token, &log);
+  EXPECT_EQ(token.ToString(), log);
+
+  base::UnguessableToken deserialized_token;
+  base::PickleIterator iter(pickle);
+  EXPECT_TRUE(IPC::ReadParam(&pickle, &iter, &deserialized_token));
+  EXPECT_EQ(token, deserialized_token);
 }
 
 }  // namespace
--- mojo/common/common_custom_types_struct_traits.h
+++ mojo/common/common_custom_types_struct_traits.h
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MOJO_COMMON_COMMON_CUSTOM_TYPES_STRUCT_TRAITS_H_
 #define MOJO_COMMON_COMMON_CUSTOM_TYPES_STRUCT_TRAITS_H_
 
+#include "base/unguessable_token.h"
 #include "base/version.h"
 #include "mojo/common/common_custom_types.mojom-shared.h"
+#include "mojo/common/mojo_common_export.h"
 
 namespace mojo {
 
@@ -22,6 +24,26 @@ struct StructTraits<mojo::common::mojom::VersionDataView, base::Version> {
   static const std::vector<uint32_t>& components(const base::Version& version);
   static bool Read(mojo::common::mojom::VersionDataView data,
                    base::Version* out);
+};
+
+// If base::UnguessableToken is no longer 128 bits, the logic below and the
+// mojom::UnguessableToken type should be updated.
+static_assert(sizeof(base::UnguessableToken) == 2 * sizeof(uint64_t),
+              "base::UnguessableToken should be of size 2 * sizeof(uint64_t).");
+
+template <>
+struct StructTraits<mojo::common::mojom::UnguessableTokenDataView,
+                 base::UnguessableToken> {
+  static uint64_t high(const base::UnguessableToken& token) {
+    return token.GetHighForSerialization();
+  }
+
+  static uint64_t low(const base::UnguessableToken& token) {
+    return token.GetLowForSerialization();
+  }
+
+  static bool Read(mojo::common::mojom::UnguessableTokenDataView data,
+                   base::UnguessableToken* out);
 };
 
 template <>
