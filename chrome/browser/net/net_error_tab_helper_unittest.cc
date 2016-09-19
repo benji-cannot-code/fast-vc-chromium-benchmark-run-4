ce53e7e15824d48b4bbccb39c4ccb2aae4b391c0
jam <jam@chromium.org>
2016-09-19 10:54:44 -0700

    Update PlzNavigate test filter after NetErrorTabHelper fixes in r419483.
    BUG=504347
    
    Review-Url: https://codereview.chromium.org/2355533002
    Cr-Commit-Position: refs/heads/master@{#419500}

--- chrome/browser/net/net_error_tab_helper_unittest.cc
+++ chrome/browser/net/net_error_tab_helper_unittest.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/page_transition_types.h"
 
-#undef NO_ERROR
+#undef NO_ERROR  // Defined in winerror.h.
 
 using chrome_browser_net::NetErrorTabHelper;
 using error_page::DnsProbeStatus;
