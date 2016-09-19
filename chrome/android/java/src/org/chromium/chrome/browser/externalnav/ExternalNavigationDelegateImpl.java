a0711c83c92ad70eb0c35997baeb0cd0c2d416f9
dominickn <dominickn@chromium.org>
2016-09-18 18:31:52 -0700

    Prevent navigations within fullscreen webapps from being intercepted.
    If a native app and a PWA are installed which handle the same URLs,
    a navigation within the PWA may open the intent picker to ask the user
    if they wish to browse in the native app.
    
    This CL stops the picker from appearing if the user is browsing within
    the fullscreen PWA. It does not do so if the user is browsing the PWA
    outside of the fullscreen standalone (i.e. launched from homescreen)
    mode.
    
    BUG=647569
    
    Review-Url: https://codereview.chromium.org/2348853002
    Cr-Commit-Position: refs/heads/master@{#419406}

--- chrome/android/java/src/org/chromium/chrome/browser/externalnav/ExternalNavigationDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/externalnav/ExternalNavigationDelegateImpl.java
@@ -42,6 +42,7 @@ import org.chromium.chrome.browser.document.ChromeLauncherActivity;
 import org.chromium.chrome.browser.externalnav.ExternalNavigationHandler.OverrideUrlLoadingResult;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.util.UrlUtilities;
+import org.chromium.chrome.browser.webapps.WebappActivity;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.common.Referrer;
 import org.chromium.ui.base.PageTransition;
@@ -238,6 +239,16 @@ public class ExternalNavigationDelegateImpl implements ExternalNavigationDelegat
     @Override
     public boolean isSpecializedHandlerAvailable(List<ResolveInfo> infos) {
         return countSpecializedHandlers(infos) > 0;
+    }
+
+    @Override
+    public boolean isWithinCurrentWebappScope(String url) {
+        Context context = getAvailableContext();
+        if (context instanceof WebappActivity) {
+            String scope = ((WebappActivity) context).getWebappScope();
+            return url.startsWith(scope);
+        }
+        return false;
     }
 
     @Override
