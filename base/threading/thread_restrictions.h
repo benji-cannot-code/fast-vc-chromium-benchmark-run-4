ec814f03565aded166f61668b4e3f1324a95d019
sadrul <sadrul@chromium.org>
2016-09-19 10:06:30 -0700

    mojo: Use TYPE_DEFAULT message-loop for browser-tests.
    Use TYPE_DEFAULT message-loop for browser-tests, since that's what
    mojo_runner uses by default. We would normally use TYPE_DEFAULT
    message-loops for chrome --mash too, but keep using TYPE_UI for now,
    since mojo:ui needs that, and it is not known whether mojo:ui will be
    run at the type of the message-loop creation.
    
    Also, remove references to MessagePumpMojo since that has been removed.
    
    BUG=none
    TBR=jam@ for base/threading/
    
    Review-Url: https://codereview.chromium.org/2351603002
    Cr-Commit-Position: refs/heads/master@{#419486}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -55,9 +55,6 @@ namespace gpu {
 class GpuChannelHost;
 }
 namespace mojo {
-namespace common {
-class MessagePumpMojo;
-}
 class SyncCallRestrictions;
 }
 namespace ui {
@@ -209,7 +206,6 @@ class BASE_EXPORT ThreadRestrictions {
   friend class ThreadTestHelper;
   friend class PlatformThread;
   friend class android::JavaHandlerThread;
-  friend class mojo::common::MessagePumpMojo;
   friend class mojo::SyncCallRestrictions;
   friend class ui::CommandBufferClientImpl;
   friend class ui::CommandBufferLocal;
