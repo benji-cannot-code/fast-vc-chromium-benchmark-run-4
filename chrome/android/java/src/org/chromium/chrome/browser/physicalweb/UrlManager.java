2dc6305e70e1626e2ee8e1ab6bccbe11f8be8725
hayesjordan <hayesjordan@google.com>
2016-09-19 16:42:39 -0700

    Add access to Physical Web Service results
    To allow access to Physical Web data, the URL info and Physical Web
    Service data needs to be paired together. The pairing gives all relevant
    information for a scanned URL.
    
    BUG=636490
    
    Committed: https://crrev.com/576c1dde62f1bc26e4366d50b32f76a636a1dbbd
    Review-Url: https://codereview.chromium.org/2330253002
    Cr-Original-Commit-Position: refs/heads/master@{#419531}
    Cr-Commit-Position: refs/heads/master@{#419603}

--- chrome/android/java/src/org/chromium/chrome/browser/physicalweb/UrlManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/physicalweb/UrlManager.java
@@ -253,6 +253,17 @@ class UrlManager {
     }
 
     /**
+     * Gets all UrlInfos and PwsResults for resolved URLs.
+     */
+    public PwCollection getPwCollection() {
+        List<PwsResult> nearbyPwsResults = new ArrayList<>();
+        for (String url : mNearbyUrls) {
+            nearbyPwsResults.add(mPwsResultMap.get(url));
+        }
+        return new PwCollection(getUrlInfoList(mNearbyUrls), nearbyPwsResults);
+    }
+
+    /**
      * Forget all stored URLs and clear the notification.
      */
     public void clearAllUrls() {
