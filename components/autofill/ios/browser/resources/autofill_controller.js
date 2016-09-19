7327c0b6ad0f30f08e16d2e9814afc0f823fd3fc
eugenebut <eugenebut@chromium.org>
2016-09-19 03:02:20 -0700

    [ios] Fixed some checkTypes errors in autofill_controller.js.
    Moved typedefs out from anonymous function because typedefs can
    only be defined at the top level, not inside functions.
    
    R=thestig@chromium.org
    BUG=487804
    
    Review-Url: https://codereview.chromium.org/2341963006
    Cr-Commit-Position: refs/heads/master@{#419423}

--- components/autofill/ios/browser/resources/autofill_controller.js
+++ components/autofill/ios/browser/resources/autofill_controller.js
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // (chromium/src/components/autofill/core/common/form_data.h) for further
 // processing.
 
-/* Beginning of anonymous object. */
-(function() {
-
 /** @typedef {HTMLInputElement|HTMLTextAreaElement|HTMLSelectElement} */
 var FormControlElement;
 
@@ -46,6 +43,9 @@ var AutofillFormFieldData;
   * }}
   */
 var AutofillFormData;
+
+/* Beginning of anonymous object. */
+(function() {
 
 /**
  * Namespace for this file. It depends on |__gCrWeb| having already been
