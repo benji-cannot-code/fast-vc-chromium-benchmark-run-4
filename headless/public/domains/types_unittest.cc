5770561ccfbc827bff10e24ac41fe9f1456fbf6b
aboxhall <aboxhall@chromium.org>
2016-09-19 06:38:34 -0700

    Rework types_unittest.cc not to rely on the GetAXNode method.
    Should fix build breakage (https://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20GN/builds/45952/steps/compile/logs/stdio) caused by modifying the Accessibility model in https://codereview.chromium.org/2322413003/
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2351693002
    Cr-Commit-Position: refs/heads/master@{#419445}

--- headless/public/domains/types_unittest.cc
+++ headless/public/domains/types_unittest.cc
@@ -10,23 +10,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace headless {
 
 TEST(TypesTest, IntegerProperty) {
-  std::unique_ptr<accessibility::GetAXNodeParams> object(
-      accessibility::GetAXNodeParams::Builder().SetNodeId(123).Build());
+  std::unique_ptr<page::NavigateToHistoryEntryParams> object(
+      page::NavigateToHistoryEntryParams::Builder().SetEntryId(123).Build());
   EXPECT_TRUE(object);
-  EXPECT_EQ(123, object->GetNodeId());
+  EXPECT_EQ(123, object->GetEntryId());
 
-  std::unique_ptr<accessibility::GetAXNodeParams> clone(object->Clone());
+  std::unique_ptr<page::NavigateToHistoryEntryParams> clone(object->Clone());
   EXPECT_TRUE(clone);
-  EXPECT_EQ(123, clone->GetNodeId());
+  EXPECT_EQ(123, clone->GetEntryId());
 }
 
 TEST(TypesTest, IntegerPropertyParseError) {
-  const char* json = "{\"nodeId\": \"foo\"}";
+  const char* json = "{\"entryId\": \"foo\"}";
   std::unique_ptr<base::Value> object = base::JSONReader::Read(json);
   EXPECT_TRUE(object);
 
   ErrorReporter errors;
-  EXPECT_FALSE(accessibility::GetAXNodeParams::Parse(*object, &errors));
+  EXPECT_FALSE(page::NavigateToHistoryEntryParams::Parse(*object, &errors));
   EXPECT_TRUE(errors.HasErrors());
 }
 
