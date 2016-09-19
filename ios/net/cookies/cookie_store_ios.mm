7ac453f8e2a87fb9c20d17d2063f22a955b95a53
avi <avi@chromium.org>
2016-09-19 06:52:27 -0700

    Remove stl_util's STLDeleteContainerPairSecondPointers from ios/.
    BUG=555865
    
    Review-Url: https://codereview.chromium.org/2348963002
    Cr-Commit-Position: refs/heads/master@{#419447}

--- ios/net/cookies/cookie_store_ios.mm
+++ ios/net/cookies/cookie_store_ios.mm
@@ -17,11 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/foundation_util.h"
 #include "base/mac/scoped_nsobject.h"
 #include "base/macros.h"
+#include "base/memory/ptr_util.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram.h"
 #include "base/observer_list.h"
 #include "base/sequenced_task_runner.h"
-#include "base/stl_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/task_runner_util.h"
 #include "base/threading/thread_restrictions.h"
@@ -314,8 +314,6 @@ CookieStoreIOS::CookieStoreIOS(
 
 CookieStoreIOS::~CookieStoreIOS() {
   NotificationTrampoline::GetInstance()->RemoveObserver(this);
-  base::STLDeleteContainerPairSecondPointers(hook_map_.begin(),
-                                             hook_map_.end());
 }
 
 // static
@@ -996,7 +994,7 @@ CookieStoreIOS::AddCallbackForCookie(const GURL& gurl,
   if (hook_map_.count(key) == 0) {
     UpdateCacheForCookieFromSystem(gurl, name, nullptr, nullptr);
     if (hook_map_.count(key) == 0)
-      hook_map_[key] = new CookieChangedCallbackList;
+      hook_map_[key] = base::MakeUnique<CookieChangedCallbackList>();
   }
 
   DCHECK(hook_map_.find(key) != hook_map_.end());
@@ -1029,7 +1027,7 @@ void CookieStoreIOS::RunCallbacksForCookies(
     return;
 
   std::pair<GURL, std::string> key(url, name);
-  CookieChangedCallbackList* callbacks = hook_map_[key];
+  CookieChangedCallbackList* callbacks = hook_map_[key].get();
   for (const auto& cookie : cookies) {
     DCHECK_EQ(name, cookie.Name());
     callbacks->Notify(cookie, removed);
