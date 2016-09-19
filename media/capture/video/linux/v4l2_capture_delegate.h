81f4f5ffa2ca7246b93b9549cd6e4c738b734958
xianglu <xianglu@chromium.org>
2016-09-19 09:58:07 -0700

    ImageCapture: Implement TakePhoto() for Linux/CrOs
    This CL implements ImageCapture's takePhoto() method by enqueueing a
    callback every time the method is called. Check the queue when a new
    frame is captured in DoCapture(), and execute callbacks with a blob
    argument.
    
    Add a new method GetPhotoBlob() to construct blob. If the source image
    is originally in JPEG format, it's copied to blob directly. Otherwise
    convert the source image to RGB format and encode it to PNG.
    
    R=mcasas@chromium.org
    BUG=646430
    TEST=All tests in VideoCaptureDeviceTests passed.
    
    Review-Url: https://codereview.chromium.org/2344123002
    Cr-Commit-Position: refs/heads/master@{#419484}

--- media/capture/video/linux/v4l2_capture_delegate.h
+++ media/capture/video/linux/v4l2_capture_delegate.h
@@ -56,6 +56,8 @@ class V4L2CaptureDelegate final
                         std::unique_ptr<VideoCaptureDevice::Client> client);
   void StopAndDeAllocate();
 
+  void TakePhoto(VideoCaptureDevice::TakePhotoCallback callback);
+
   void SetRotation(int rotation);
 
  private:
@@ -67,6 +69,11 @@ class V4L2CaptureDelegate final
   bool MapAndQueueBuffer(int index);
 
   void DoCapture();
+
+  class BufferTracker;
+  mojom::BlobPtr GetPhotoBlob(
+      const scoped_refptr<BufferTracker>& buffer_tracker,
+      const uint32_t bytesused);
 
   void SetErrorState(const tracked_objects::Location& from_here,
                      const std::string& reason);
@@ -81,8 +88,9 @@ class V4L2CaptureDelegate final
   std::unique_ptr<VideoCaptureDevice::Client> client_;
   base::ScopedFD device_fd_;
 
+  std::queue<VideoCaptureDevice::TakePhotoCallback> take_photo_callbacks_;
+
   // Vector of BufferTracker to keep track of mmap()ed pointers and their use.
-  class BufferTracker;
   std::vector<scoped_refptr<BufferTracker>> buffer_tracker_pool_;
 
   bool is_capturing_;
