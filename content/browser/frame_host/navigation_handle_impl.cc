d2cc5dc01fcb4ea208770465958b6c5b2ce1acdd
John Abd-El-Malek <jam@chromium.org>
2016-09-19 09:45:39 -0700

    Fix NetErrorTabHelper with PlzNavigate.
    This includes:
    -switching NetErrorTabHelper to use the new WebContentsObserver navigation callbacks that work with PlzNavigate
    -fixing NavigationHandle::IsErrorPage to return true for reloads of error pages
    
    This fixes
    DnsProbeBrowserTest.CorrectionsDisabled
    DnsProbeBrowserTest.CorrectionsLoadStopped
    DnsProbeBrowserTest.CorrectionsLoadStoppedSlowProbe
    DnsProbeBrowserTest.Incognito
    DnsProbeBrowserTest.NoInternetProbeResultWithBrokenCorrections
    DnsProbeBrowserTest.NoInternetProbeResultWithSlowBrokenCorrections
    DnsProbeBrowserTest.NxdomainProbeResultWithWorkingCorrections
    DnsProbeBrowserTest.NxdomainProbeResultWithWorkingSlowCorrections
    DnsProbeBrowserTest.ProbesDisabled
    DnsProbeBrowserTest.SyncFailureWithBrokenCorrections
    ErrorPageNavigationCorrectionsFailTest.FetchCorrectionsFails
    
    BUG=504347,647859
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    R=clamy@chromium.org, juliatuttle@chromium.org
    
    Review URL: https://codereview.chromium.org/2345913002 .
    
    Cr-Commit-Position: refs/heads/master@{#419483}

--- content/browser/frame_host/navigation_handle_impl.cc
+++ content/browser/frame_host/navigation_handle_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/common/browser_side_navigation_policy.h"
 #include "content/public/common/content_client.h"
+#include "content/public/common/url_constants.h"
 #include "net/url_request/redirect_info.h"
 #include "url/gurl.h"
 #include "url/url_constants.h"
@@ -462,7 +463,14 @@ void NavigationHandleImpl::DidCommitNavigation(
   render_frame_host_ = render_frame_host;
   is_same_page_ = same_page;
 
-  state_ = net_error_code_ == net::OK ? DID_COMMIT : DID_COMMIT_ERROR_PAGE;
+  // If an error page reloads, net_error_code might be 200 but we still want to
+  // count it as an error page.
+  if (params.base_url.spec() == kUnreachableWebDataURL ||
+      net_error_code_ != net::OK) {
+    state_ = DID_COMMIT_ERROR_PAGE;
+  } else {
+    state_ = DID_COMMIT;
+  }
 }
 
 NavigationThrottle::ThrottleCheckResult
