15ecdfe1147eeced81a40d7f0b7e476d6d52332e
reveman <reveman@chromium.org>
2016-09-19 00:59:14 -0700

    third_party: Switch to zcr prefix for downstream wayland interfaces.
    Add existing interfaces with zcr prefix and mark old zwp interfaces
    as deprecated.
    
    No change in behavior.
    
    BUG=631960
    TBR=piman@chromium.org
    
    Review-Url: https://codereview.chromium.org/2350633002
    Cr-Commit-Position: refs/heads/master@{#419417}

--- third_party/wayland-protocols/protocol/stylus-protocol.c
+++ third_party/wayland-protocols/protocol/stylus-protocol.c
@@ -29,18 +29,47 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wayland-util.h"
 
 extern const struct wl_interface wl_pointer_interface;
+extern const struct wl_interface zcr_pointer_stylus_v1_interface;
 extern const struct wl_interface zwp_pointer_stylus_v1_interface;
 
 static const struct wl_interface *types[] = {
 	NULL,
 	NULL,
 	NULL,
+	&zcr_pointer_stylus_v1_interface,
+	&wl_pointer_interface,
 	&zwp_pointer_stylus_v1_interface,
 	&wl_pointer_interface,
 };
 
-static const struct wl_message zwp_stylus_v1_requests[] = {
+static const struct wl_message zcr_stylus_v1_requests[] = {
 	{ "get_pointer_stylus", "no", types + 3 },
+};
+
+WL_EXPORT const struct wl_interface zcr_stylus_v1_interface = {
+	"zcr_stylus_v1", 1,
+	1, zcr_stylus_v1_requests,
+	0, NULL,
+};
+
+static const struct wl_message zcr_pointer_stylus_v1_requests[] = {
+	{ "destroy", "", types + 0 },
+};
+
+static const struct wl_message zcr_pointer_stylus_v1_events[] = {
+	{ "tool_change", "u", types + 0 },
+	{ "force", "uf", types + 0 },
+	{ "tilt", "uff", types + 0 },
+};
+
+WL_EXPORT const struct wl_interface zcr_pointer_stylus_v1_interface = {
+	"zcr_pointer_stylus_v1", 1,
+	1, zcr_pointer_stylus_v1_requests,
+	3, zcr_pointer_stylus_v1_events,
+};
+
+static const struct wl_message zwp_stylus_v1_requests[] = {
+	{ "get_pointer_stylus", "no", types + 5 },
 };
 
 WL_EXPORT const struct wl_interface zwp_stylus_v1_interface = {
--- third_party/wayland-protocols/protocol/vsync-feedback-protocol.c
+++ third_party/wayland-protocols/protocol/vsync-feedback-protocol.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wayland-util.h"
 
 extern const struct wl_interface wl_output_interface;
+extern const struct wl_interface zcr_vsync_timing_v1_interface;
 extern const struct wl_interface zwp_vsync_timing_v1_interface;
 
 static const struct wl_interface *types[] = {
@@ -36,13 +37,40 @@ static const struct wl_interface *types[] = {
 	NULL,
 	NULL,
 	NULL,
+	&zcr_vsync_timing_v1_interface,
+	&wl_output_interface,
 	&zwp_vsync_timing_v1_interface,
 	&wl_output_interface,
 };
 
-static const struct wl_message zwp_vsync_feedback_v1_requests[] = {
+static const struct wl_message zcr_vsync_feedback_v1_requests[] = {
 	{ "destroy", "", types + 0 },
 	{ "get_vsync_timing", "n?o", types + 4 },
+};
+
+WL_EXPORT const struct wl_interface zcr_vsync_feedback_v1_interface = {
+	"zcr_vsync_feedback_v1", 1,
+	2, zcr_vsync_feedback_v1_requests,
+	0, NULL,
+};
+
+static const struct wl_message zcr_vsync_timing_v1_requests[] = {
+	{ "destroy", "", types + 0 },
+};
+
+static const struct wl_message zcr_vsync_timing_v1_events[] = {
+	{ "update", "uuuu", types + 0 },
+};
+
+WL_EXPORT const struct wl_interface zcr_vsync_timing_v1_interface = {
+	"zcr_vsync_timing_v1", 1,
+	1, zcr_vsync_timing_v1_requests,
+	1, zcr_vsync_timing_v1_events,
+};
+
+static const struct wl_message zwp_vsync_feedback_v1_requests[] = {
+	{ "destroy", "", types + 0 },
+	{ "get_vsync_timing", "n?o", types + 6 },
 };
 
 WL_EXPORT const struct wl_interface zwp_vsync_feedback_v1_interface = {
