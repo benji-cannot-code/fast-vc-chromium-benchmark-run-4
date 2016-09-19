bda9fb26799fb829ce839f36122067604437999b
fmalita <fmalita@chromium.org>
2016-09-19 13:06:44 -0700

    Avoid kStrokeAndFill_Style in BorderShadowLayerDelegate::OnPaintLayer
    We're looking at deprecating kStrokeAndFill_Style in Skia, so this CL's
    main focus is removing the use of the flag.
    
    While in this area, I've also updated the impl to draw the shadow using
    Canvas::DrawRoundRect instead of going through a path.
    
    R=reed@google.com,bruthig@chromium.org
    
    Review-Url: https://codereview.chromium.org/2349273002
    Cr-Commit-Position: refs/heads/master@{#419541}

--- ui/views/animation/ink_drop_painted_layer_delegates.cc
+++ ui/views/animation/ink_drop_painted_layer_delegates.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/skia/include/core/SkDrawLooper.h"
 #include "third_party/skia/include/core/SkPaint.h"
-#include "third_party/skia/include/core/SkPath.h"
 #include "ui/compositor/paint_recorder.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_palette.h"
@@ -155,24 +154,21 @@ gfx::Vector2dF BorderShadowLayerDelegate::GetCenteringOffset() const {
 void BorderShadowLayerDelegate::OnPaintLayer(const ui::PaintContext& context) {
   SkPaint paint;
   paint.setLooper(gfx::CreateShadowDrawLooperCorrectBlur(shadows_));
-  paint.setStyle(SkPaint::kStrokeAndFill_Style);
+  paint.setStyle(SkPaint::kFill_Style);
   paint.setAntiAlias(true);
   paint.setColor(SK_ColorTRANSPARENT);
-  paint.setStrokeJoin(SkPaint::kRound_Join);
-  SkRect path_bounds = gfx::RectFToSkRect(
-      gfx::RectF(bounds_ - GetPaintedBounds().OffsetFromOrigin()));
+  gfx::RectF rrect_bounds =
+      gfx::RectF(bounds_ - GetPaintedBounds().OffsetFromOrigin());
 
   ui::PaintRecorder recorder(context, GetPaintedBounds().size());
-  const SkScalar corner = SkFloatToScalar(corner_radius_);
-  SkPath path;
-  path.addRoundRect(path_bounds, corner, corner, SkPath::kCCW_Direction);
-  recorder.canvas()->DrawPath(path, paint);
+  recorder.canvas()->DrawRoundRect(rrect_bounds, corner_radius_, paint);
 
   SkPaint clear_paint;
   clear_paint.setAntiAlias(true);
   clear_paint.setXfermodeMode(SkXfermode::kClear_Mode);
-  recorder.canvas()->sk_canvas()->drawRoundRect(path_bounds, corner, corner,
-                                                clear_paint);
+  // If we don't care about actually clearing preexisting buffer content,
+  // this could be replaced with a kDifference clip for the draw above.
+  recorder.canvas()->DrawRoundRect(rrect_bounds, corner_radius_, clear_paint);
 }
 
 }  // namespace views
