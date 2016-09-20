db3e65a1fab99d8299b1800fae9894746a57f090
boliu <boliu@chromium.org>
2016-09-20 15:49:17 -0700

    Disable PrecacheLauncherTest#testUpdateEnabledNullProfileSyncService
    BUG=648749
    TBR=bengr@chromium.org
    
    Review-Url: https://codereview.chromium.org/2354103002
    Cr-Commit-Position: refs/heads/master@{#419879}

--- chrome/android/javatests/src/org/chromium/chrome/browser/precache/PrecacheLauncherTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/precache/PrecacheLauncherTest.java
@@ -11,6 +11,7 @@ import android.test.suitebuilder.annotation.SmallTest;
 import com.google.android.gms.gcm.Task;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.browser.sync.ProfileSyncService;
@@ -191,6 +192,7 @@ public class PrecacheLauncherTest extends NativeLibraryTestBase {
     @SmallTest
     @Feature({"Precache"})
     @RetryOnFailure
+    @DisabledTest(message = "crbug.com/648749")
     public void testUpdateEnabledNullProfileSyncService() {
         ProfileSyncService.overrideForTests(null);
 
