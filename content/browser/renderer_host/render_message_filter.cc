bf131e84d7220056acdc43b08e01bc5ced10e3a0
reveman <reveman@chromium.org>
2016-09-19 19:10:58 -0700

    base: Allow renderer thread priorities to be changed.
    This provides a mechanism for a renderer to change the priority of
    its threads and as a result also the cpuset they are assigned to.
    
    A ChildProcessHost IPC message from the renderer is used to request
    a thread priority change and have the browser process identify the
    renderer thread that is requesting a priority change by checking all
    its threads to find a thread with NSpid field in
    /proc/[pid]/task/[thread_id]/status that matches the namespace tid
    from the renderer.
    
    This is currently limited to Linux and ChromeOS but follow up work
    will investigate the possibility and benefits of doing the same on
    other platforms.
    
    Note: Thread priorities in the renderer are already adjusted in a
    similar way on Android as the sandbox is not strict enough to
    prevent this on Android today.
    
    BUG=chrome-os-partner:56550
    TEST=
    
    Review-Url: https://codereview.chromium.org/2334533002
    Cr-Commit-Position: refs/heads/master@{#419649}

--- content/browser/renderer_host/render_message_filter.cc
+++ content/browser/renderer_host/render_message_filter.cc
@@ -90,6 +90,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accelerated_widget_mac/window_resize_helper_mac.h"
 #endif
 
+#if defined(OS_LINUX)
+#include "base/linux_util.h"
+#include "base/threading/platform_thread.h"
+#endif
+
 namespace content {
 namespace {
 
@@ -205,6 +210,10 @@ bool RenderMessageFilter::OnMessageReceived(const IPC::Message& message) {
         OnAllocateLockedDiscardableSharedMemory)
     IPC_MESSAGE_HANDLER(ChildProcessHostMsg_DeletedDiscardableSharedMemory,
                         OnDeletedDiscardableSharedMemory)
+#if defined(OS_LINUX)
+    IPC_MESSAGE_HANDLER(ChildProcessHostMsg_SetThreadPriority,
+                        OnSetThreadPriority)
+#endif
     IPC_MESSAGE_HANDLER_DELAY_REPLY(RenderProcessHostMsg_Keygen, OnKeygen)
     IPC_MESSAGE_HANDLER(RenderProcessHostMsg_DidGenerateCacheableMetadata,
                         OnCacheableMetadataAvailable)
@@ -414,6 +423,35 @@ void RenderMessageFilter::OnDeletedDiscardableSharedMemory(
           &RenderMessageFilter::DeletedDiscardableSharedMemoryOnFileThread,
           this, id));
 }
+
+#if defined(OS_LINUX)
+void RenderMessageFilter::SetThreadPriorityOnFileThread(
+    base::PlatformThreadId ns_tid,
+    base::ThreadPriority priority) {
+  bool ns_pid_supported = false;
+  pid_t peer_tid = base::FindThreadID(peer_pid(), ns_tid, &ns_pid_supported);
+  if (peer_tid == -1) {
+    if (ns_pid_supported)
+      DLOG(WARNING) << "Could not find tid";
+    return;
+  }
+
+  if (peer_tid == peer_pid()) {
+    DLOG(WARNING) << "Changing priority of main thread is not allowed";
+    return;
+  }
+
+  base::PlatformThread::SetThreadPriority(peer_tid, priority);
+}
+
+void RenderMessageFilter::OnSetThreadPriority(base::PlatformThreadId ns_tid,
+                                              base::ThreadPriority priority) {
+  BrowserThread::PostTask(
+      BrowserThread::FILE_USER_BLOCKING, FROM_HERE,
+      base::Bind(&RenderMessageFilter::SetThreadPriorityOnFileThread, this,
+                 ns_tid, priority));
+}
+#endif
 
 void RenderMessageFilter::OnCacheableMetadataAvailable(
     const GURL& url,
--- content/common/child_process_messages.h
+++ content/common/child_process_messages.h
@@ -28,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/ipc/gfx_param_traits.h"
 #include "ui/gfx/ipc/skia/gfx_skia_param_traits.h"
 
+#if defined(OS_LINUX)
+#include "base/threading/platform_thread.h"
+#endif
+
 IPC_ENUM_TRAITS_MAX_VALUE(tracked_objects::ThreadData::Status,
                           tracked_objects::ThreadData::STATUS_LAST)
 
@@ -66,6 +70,11 @@ IPC_STRUCT_TRAITS_BEGIN(tracked_objects::ProcessDataSnapshot)
   IPC_STRUCT_TRAITS_MEMBER(phased_snapshots)
   IPC_STRUCT_TRAITS_MEMBER(process_id)
 IPC_STRUCT_TRAITS_END()
+
+#if defined(OS_LINUX)
+IPC_ENUM_TRAITS_MAX_VALUE(base::ThreadPriority,
+                          base::ThreadPriority::REALTIME_AUDIO)
+#endif
 
 #undef IPC_MESSAGE_EXPORT
 #define IPC_MESSAGE_EXPORT CONTENT_EXPORT
@@ -212,3 +221,10 @@ IPC_SYNC_MESSAGE_CONTROL2_1(
 // memory.
 IPC_MESSAGE_CONTROL1(ChildProcessHostMsg_DeletedDiscardableSharedMemory,
                      content::DiscardableSharedMemoryId)
+
+#if defined(OS_LINUX)
+// Asks the browser to change the priority of thread.
+IPC_MESSAGE_CONTROL2(ChildProcessHostMsg_SetThreadPriority,
+                     base::PlatformThreadId,
+                     base::ThreadPriority)
+#endif
