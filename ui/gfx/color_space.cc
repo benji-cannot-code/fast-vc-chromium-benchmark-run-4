e67311338fff9424bacae81d3ac7d117b7c7a24a
ccameron <ccameron@chromium.org>
2016-09-19 19:29:01 -0700

    cc: Add conversion between gfx::ColorSpace and SkColorSpace
    Use the ICC profile to convert between the two for now. This is not
    robust or efficient, but it is behind a development flag.
    
    BUG=44872
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2351823002
    Cr-Commit-Position: refs/heads/master@{#419653}

--- ui/gfx/color_space.cc
+++ ui/gfx/color_space.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/lazy_instance.h"
 #include "base/synchronization/lock.h"
+#include "third_party/skia/include/core/SkColorSpace.h"
 #include "ui/gfx/icc_profile.h"
 
 namespace gfx {
@@ -96,8 +97,14 @@ ColorSpace ColorSpace::CreateREC709() {
 }
 
 bool ColorSpace::operator==(const ColorSpace& other) const {
-  return primaries_ == other.primaries_ && transfer_ == other.transfer_ &&
-         matrix_ == other.matrix_ && range_ == other.range_;
+  if (primaries_ != other.primaries_ || transfer_ != other.transfer_ ||
+      matrix_ != other.matrix_ || range_ != other.range_)
+    return false;
+  if (primaries_ == PrimaryID::CUSTOM &&
+      memcmp(custom_primary_matrix_, other.custom_primary_matrix_,
+             sizeof(custom_primary_matrix_)))
+    return false;
+  return true;
 }
 
 bool ColorSpace::operator!=(const ColorSpace& other) const {
@@ -120,12 +127,32 @@ bool ColorSpace::operator<(const ColorSpace& other) const {
   if (range_ < other.range_)
     return true;
   if (range_ > other.range_)
-    return true;
-
-  // TODO(hubbe): For "CUSTOM" primaries or tranfer functions, compare their
-  // coefficients here
-
+    return false;
+  if (primaries_ == PrimaryID::CUSTOM) {
+    int primary_result =
+        memcmp(custom_primary_matrix_, other.custom_primary_matrix_,
+               sizeof(custom_primary_matrix_));
+    if (primary_result < 0)
+      return true;
+    if (primary_result > 0)
+      return false;
+  }
   return false;
+}
+
+sk_sp<SkColorSpace> ColorSpace::ToSkColorSpace() const {
+  // Unspecified color spaces are represented as nullptr SkColorSpaces.
+  if (*this == gfx::ColorSpace())
+    return nullptr;
+
+  // TODO(crbug.com/634102): Support more than just ICC profile based color
+  // spaces. The DCHECK here is to ensure that callers that expect a valid
+  // result are notified of this incomplete functionality.
+  std::vector<char> icc_data = gfx::ICCProfile::FromColorSpace(*this).GetData();
+  sk_sp<SkColorSpace> result =
+      SkColorSpace::NewICC(icc_data.data(), icc_data.size());
+  DCHECK(result);
+  return result;
 }
 
 }  // namespace gfx
--- ui/gfx/color_space.h
+++ ui/gfx/color_space.h
@@ -18,6 +18,10 @@ template <class P>
 struct ParamTraits;
 }  // namespace IPC
 
+template <typename T>
+class sk_sp;
+class SkColorSpace;
+
 namespace gfx {
 
 class ICCProfile;
@@ -146,6 +150,9 @@ class GFX_EXPORT ColorSpace {
   bool operator==(const ColorSpace& other) const;
   bool operator!=(const ColorSpace& other) const;
   bool operator<(const ColorSpace& other) const;
+
+  // Note that this may return nullptr.
+  sk_sp<SkColorSpace> ToSkColorSpace() const;
 
  private:
   PrimaryID primaries_ = PrimaryID::UNSPECIFIED;
