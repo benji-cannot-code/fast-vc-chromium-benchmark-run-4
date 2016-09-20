5cac703e418e4a1a7da6160f3a5af8c78440c961
avi <avi@chromium.org>
2016-09-19 18:52:43 -0700

    Remove stl_util's STLDeleteContainerPointers from error_map.
    BUG=555865
    
    Review-Url: https://codereview.chromium.org/2349333002
    Cr-Commit-Position: refs/heads/master@{#419641}

--- extensions/browser/error_map.h
+++ extensions/browser/error_map.h
@@ -19,11 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
-typedef std::deque<const ExtensionError*> ErrorList;
+using ErrorList = std::deque<std::unique_ptr<ExtensionError>>;
 
 // An ErrorMap is responsible for storing Extension-related errors, keyed by
-// Extension ID. The errors are owned by the ErrorMap, and are deleted upon
-// destruction.
+// Extension ID.
 class ErrorMap {
  public:
   ErrorMap();
@@ -78,10 +77,9 @@ class ErrorMap {
   // An Entry is created for each Extension ID, and stores the errors related to
   // that Extension.
   class ExtensionEntry;
-  using EntryMap = std::map<std::string, ExtensionEntry*>;
 
   // The mapping between Extension IDs and their corresponding Entries.
-  EntryMap map_;
+  std::map<std::string, std::unique_ptr<ExtensionEntry>> map_;
 
   DISALLOW_COPY_AND_ASSIGN(ErrorMap);
 };
