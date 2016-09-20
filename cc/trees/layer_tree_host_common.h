d0d7bc7689172afb80dcba0d41f525f5568c550d
jaydasika <jaydasika@chromium.org>
2016-09-19 18:34:30 -0700

    cc : Compute visible rects dynamically (1)
    This CL computes visible rects dynamically for all cases we don't early
    out in draw_property_utils::CalculateVisibleRects (we early out for
    copy requests, singular transforms etc) and compares it with the
    current computation. This dynamic computation is behind the
    'verify_visible_rectcalculations' flag. This CL also skips
    verify_visible_rect_calculations for some test where we expect the
    visible rects computed by the two methods to be different
    
    BUG=642054
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2334003003
    Cr-Commit-Position: refs/heads/master@{#419628}

--- cc/trees/layer_tree_host_common.h
+++ cc/trees/layer_tree_host_common.h
@@ -81,6 +81,7 @@ class CC_EXPORT LayerTreeHostCommon {
         bool can_render_to_separate_surface,
         bool can_adjust_raster_scales,
         bool verify_clip_tree_calculations,
+        bool verify_visible_rect_calculations,
         bool verify_transform_tree_calculations,
         LayerImplList* render_surface_layer_list,
         PropertyTrees* property_trees);
@@ -99,6 +100,7 @@ class CC_EXPORT LayerTreeHostCommon {
     bool can_render_to_separate_surface;
     bool can_adjust_raster_scales;
     bool verify_clip_tree_calculations;
+    bool verify_visible_rect_calculations;
     bool verify_transform_tree_calculations;
     LayerImplList* render_surface_layer_list;
     PropertyTrees* property_trees;
