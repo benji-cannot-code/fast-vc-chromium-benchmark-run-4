5368ec4e9bc046caef2b0e516b79d156ae11eceb
charliea <charliea@chromium.org>
2016-09-20 08:03:50 -0700

    Disable v8.todomvc on Samsung Galaxy S5s
    The benchmark is failing about 1/3 of the time on the S5s.
    
    TBR=nednguyen@google.com
    BUG=644826
    
    Review-Url: https://codereview.chromium.org/2349283003
    Cr-Commit-Position: refs/heads/master@{#419763}

--- tools/perf/benchmarks/v8.py
+++ tools/perf/benchmarks/v8.py
@@ -128,6 +128,12 @@ class V8TodoMVC(perf_benchmark.PerfBenchmark):
     return 'v8.todomvc'
 
   @classmethod
+  def ShouldDisable(cls, possible_browser):
+    # This benchmark is flaky on Samsung Galaxy S5s.
+    # http://crbug.com/644826
+    return possible_browser.platform.GetDeviceTypeName() == 'SM-G900H'
+
+  @classmethod
   def ShouldTearDownStateAfterEachStoryRun(cls):
     return True
 
