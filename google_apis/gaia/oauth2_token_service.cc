b73907498a9b43b5d091ef854fbc748593f0ef81
avi <avi@chromium.org>
2016-09-20 09:50:15 -0700

    Remove stl_util's STLDeleteContainerPairSecondPointers from google_apis.
    BUG=555865
    
    Review-Url: https://codereview.chromium.org/2347853003
    Cr-Commit-Position: refs/heads/master@{#419790}

--- google_apis/gaia/oauth2_token_service.cc
+++ google_apis/gaia/oauth2_token_service.cc
@@ -12,12 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/location.h"
 #include "base/macros.h"
+#include "base/memory/ptr_util.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/profiler/scoped_tracker.h"
 #include "base/rand_util.h"
 #include "base/single_thread_task_runner.h"
-#include "base/stl_util.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
@@ -122,13 +122,14 @@ class OAuth2TokenService::Fetcher : public OAuth2AccessTokenConsumer {
   // Creates a Fetcher and starts fetching an OAuth2 access token for
   // |account_id| and |scopes| in the request context obtained by |getter|.
   // The given |oauth2_token_service| will be informed when fetching is done.
-  static Fetcher* CreateAndStart(OAuth2TokenService* oauth2_token_service,
-                                 const std::string& account_id,
-                                 net::URLRequestContextGetter* getter,
-                                 const std::string& client_id,
-                                 const std::string& client_secret,
-                                 const ScopeSet& scopes,
-                                 base::WeakPtr<RequestImpl> waiting_request);
+  static std::unique_ptr<OAuth2TokenService::Fetcher> CreateAndStart(
+      OAuth2TokenService* oauth2_token_service,
+      const std::string& account_id,
+      net::URLRequestContextGetter* getter,
+      const std::string& client_id,
+      const std::string& client_secret,
+      const ScopeSet& scopes,
+      base::WeakPtr<RequestImpl> waiting_request);
   ~Fetcher() override;
 
   // Add a request that is waiting for the result of this Fetcher.
@@ -199,7 +200,8 @@ class OAuth2TokenService::Fetcher : public OAuth2AccessTokenConsumer {
 };
 
 // static
-OAuth2TokenService::Fetcher* OAuth2TokenService::Fetcher::CreateAndStart(
+std::unique_ptr<OAuth2TokenService::Fetcher>
+OAuth2TokenService::Fetcher::CreateAndStart(
     OAuth2TokenService* oauth2_token_service,
     const std::string& account_id,
     net::URLRequestContextGetter* getter,
@@ -207,14 +209,9 @@ OAuth2TokenService::Fetcher* OAuth2TokenService::Fetcher::CreateAndStart(
     const std::string& client_secret,
     const OAuth2TokenService::ScopeSet& scopes,
     base::WeakPtr<RequestImpl> waiting_request) {
-  OAuth2TokenService::Fetcher* fetcher = new Fetcher(
-      oauth2_token_service,
-      account_id,
-      getter,
-      client_id,
-      client_secret,
-      scopes,
-      waiting_request);
+  std::unique_ptr<OAuth2TokenService::Fetcher> fetcher = base::WrapUnique(
+      new Fetcher(oauth2_token_service, account_id, getter, client_id,
+                  client_secret, scopes, waiting_request));
 
   // TODO(robliao): Remove ScopedTracker below once https://crbug.com/422460 is
   // fixed.
@@ -399,8 +396,7 @@ OAuth2TokenService::OAuth2TokenService(OAuth2TokenServiceDelegate* delegate)
 
 OAuth2TokenService::~OAuth2TokenService() {
   // Release all the pending fetchers.
-  base::STLDeleteContainerPairSecondPointers(pending_fetchers_.begin(),
-                                             pending_fetchers_.end());
+  pending_fetchers_.clear();
 }
 
 OAuth2TokenServiceDelegate* OAuth2TokenService::GetDelegate() {
@@ -554,8 +550,7 @@ void OAuth2TokenService::FetchOAuth2Token(RequestImpl* request,
   RequestParameters request_parameters = RequestParameters(client_id,
                                                            account_id,
                                                            scopes);
-  std::map<RequestParameters, Fetcher*>::iterator iter =
-      pending_fetchers_.find(request_parameters);
+  auto iter = pending_fetchers_.find(request_parameters);
   if (iter != pending_fetchers_.end()) {
     iter->second->AddWaitingRequest(request->AsWeakPtr());
     return;
@@ -695,10 +690,12 @@ void OAuth2TokenService::OnFetchComplete(Fetcher* fetcher) {
     }
   }
 
-  std::map<RequestParameters, Fetcher*>::iterator iter =
-    pending_fetchers_.find(request_param);
+  auto iter = pending_fetchers_.find(request_param);
   DCHECK(iter != pending_fetchers_.end());
-  DCHECK_EQ(fetcher, iter->second);
+  DCHECK_EQ(fetcher, iter->second.get());
+
+  // The Fetcher deletes itself.
+  iter->second.release();
   pending_fetchers_.erase(iter);
 }
 
@@ -785,11 +782,9 @@ void OAuth2TokenService::ClearCacheForAccount(const std::string& account_id) {
 
 void OAuth2TokenService::CancelAllRequests() {
   std::vector<Fetcher*> fetchers_to_cancel;
-  for (std::map<RequestParameters, Fetcher*>::iterator iter =
-           pending_fetchers_.begin();
-       iter != pending_fetchers_.end();
+  for (auto iter = pending_fetchers_.begin(); iter != pending_fetchers_.end();
        ++iter) {
-    fetchers_to_cancel.push_back(iter->second);
+    fetchers_to_cancel.push_back(iter->second.get());
   }
   CancelFetchers(fetchers_to_cancel);
 }
@@ -797,21 +792,17 @@ void OAuth2TokenService::CancelAllRequests() {
 void OAuth2TokenService::CancelRequestsForAccount(
     const std::string& account_id) {
   std::vector<Fetcher*> fetchers_to_cancel;
-  for (std::map<RequestParameters, Fetcher*>::iterator iter =
-           pending_fetchers_.begin();
-       iter != pending_fetchers_.end();
+  for (auto iter = pending_fetchers_.begin(); iter != pending_fetchers_.end();
        ++iter) {
     if (iter->first.account_id == account_id)
-      fetchers_to_cancel.push_back(iter->second);
+      fetchers_to_cancel.push_back(iter->second.get());
   }
   CancelFetchers(fetchers_to_cancel);
 }
 
 void OAuth2TokenService::CancelFetchers(
     std::vector<Fetcher*> fetchers_to_cancel) {
-  for (std::vector<OAuth2TokenService::Fetcher*>::iterator iter =
-           fetchers_to_cancel.begin();
-       iter != fetchers_to_cancel.end();
+  for (auto iter = fetchers_to_cancel.begin(); iter != fetchers_to_cancel.end();
        ++iter) {
     (*iter)->Cancel();
   }
@@ -827,11 +818,8 @@ size_t OAuth2TokenService::GetNumPendingRequestsForTesting(
     const std::string& client_id,
     const std::string& account_id,
     const ScopeSet& scopes) const {
-  PendingFetcherMap::const_iterator iter = pending_fetchers_.find(
-      OAuth2TokenService::RequestParameters(
-          client_id,
-          account_id,
-          scopes));
+  auto iter = pending_fetchers_.find(
+      OAuth2TokenService::RequestParameters(client_id, account_id, scopes));
   return iter == pending_fetchers_.end() ?
              0 : iter->second->GetWaitingRequestCount();
 }
