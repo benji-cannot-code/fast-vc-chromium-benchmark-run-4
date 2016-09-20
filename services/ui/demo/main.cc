38dda69cd5334204c04107c844fa0b6660daa0f9
sadrul <sadrul@chromium.org>
2016-09-20 06:06:01 -0700

    mojo: mus clients do not need UI message loops.
    Only the window server (mojo:ui) needs the UI message loop. The mus
    clients do not need to use the UI message loop.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2351963002
    Cr-Commit-Position: refs/heads/master@{#419734}

--- services/ui/demo/main.cc
+++ services/ui/demo/main.cc
@@ -9,6 +9,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 MojoResult ServiceMain(MojoHandle service_request_handle) {
   shell::ServiceRunner runner(new ui::demo::MusDemo);
-  runner.set_message_loop_type(base::MessageLoop::TYPE_UI);
   return runner.Run(service_request_handle);
 }
