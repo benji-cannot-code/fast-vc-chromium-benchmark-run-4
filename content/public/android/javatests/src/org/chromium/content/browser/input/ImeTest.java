98a74981447d35eb5b41bd8afba8780ef09f611f
yabinh <yabinh@chromium.org>
2016-09-20 03:12:29 -0700

    Fix flaky ImeTest#testImeStaysOnLongPressingDifferentNonEmptyInputs
    commitText() runs on ImeThread and focusNode() runs on UIThread, which
    causes a race condition. We should make sure that focusNode() runs after
    commitText() finished.
    
    BUG=628964, 646231
    
    Review-Url: https://codereview.chromium.org/2356723002
    Cr-Commit-Position: refs/heads/master@{#419714}

--- content/public/android/javatests/src/org/chromium/content/browser/input/ImeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/input/ImeTest.java
@@ -24,7 +24,6 @@ import android.view.inputmethod.InputConnection;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.content.browser.ContentViewCore;
@@ -660,20 +659,26 @@ public class ImeTest extends ContentShellTestBase {
         assertWaitForKeyboardStatus(false);
     }
 
-    //@SmallTest
-    //@Feature({"TextInput"})
-    //@RetryOnFailure
-    // Disabled, see crbug.com/646231.
-    @DisabledTest
+    @SmallTest
+    @Feature({"TextInput"})
     public void testImeStaysOnLongPressingDifferentNonEmptyInputs() throws Exception {
         DOMUtils.focusNode(mWebContents, "input_text");
         assertWaitForKeyboardStatus(true);
+
         commitText("Sample Text", 1);
+        // We should wait to avoid race condition.
+        waitAndVerifyUpdateSelection(0, 11, 11, -1, -1);
+
         DOMUtils.focusNode(mWebContents, "textarea");
+        waitAndVerifyUpdateSelection(1, 0, 0, -1, -1);
+
         commitText("Sample Text", 1);
+        waitAndVerifyUpdateSelection(2, 11, 11, -1, -1);
+
         DOMUtils.longPressNode(this, mContentViewCore, "input_text");
         assertWaitForKeyboardStatus(true);
         assertWaitForSelectActionBarStatus(true);
+
         DOMUtils.longPressNode(this, mContentViewCore, "textarea");
         assertWaitForKeyboardStatus(true);
     }
