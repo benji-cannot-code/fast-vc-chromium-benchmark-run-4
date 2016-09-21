94329613552c4b8c5f350544008fd64fd27accc4
fs <fs@opera.com>
2016-09-21 13:21:59 -0700

    Drop the SVGLengthType enumeration
    After the transition to using CSSPrimitiveValue, this enumeration is
    not used by anything but the DOM-side of SVGLength (SVGLengthTearOff),
    and for that we might as well use the interface constants directly.
    
    The LengthTypeREMS and LengthTypeCHS values are dropped entirely because
    they are not referenced by anything.
    
    Review-Url: https://codereview.chromium.org/2354283002
    Cr-Commit-Position: refs/heads/master@{#420150}

--- third_party/WebKit/Source/core/svg/SVGLengthTearOff.h
+++ third_party/WebKit/Source/core/svg/SVGLengthTearOff.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SVGLengthTearOff_h
 
 #include "bindings/core/v8/ScriptWrappable.h"
-#include "core/css/CSSPrimitiveValue.h"
 #include "core/svg/SVGLength.h"
 #include "core/svg/properties/SVGPropertyTearOff.h"
 
@@ -44,17 +43,17 @@ class SVGLengthTearOff final : public SVGPropertyTearOff<SVGLength>, public Scri
 public:
     // Forward declare these enums in the w3c naming scheme, for IDL generation
     enum {
-        kSvgLengthtypeUnknown = LengthTypeUnknown,
-        kSvgLengthtypeNumber = LengthTypeNumber,
-        kSvgLengthtypePercentage = LengthTypePercentage,
-        kSvgLengthtypeEms = LengthTypeEMS,
-        kSvgLengthtypeExs = LengthTypeEXS,
-        kSvgLengthtypePx = LengthTypePX,
-        kSvgLengthtypeCm = LengthTypeCM,
-        kSvgLengthtypeMm = LengthTypeMM,
-        kSvgLengthtypeIn = LengthTypeIN,
-        kSvgLengthtypePt = LengthTypePT,
-        kSvgLengthtypePc = LengthTypePC
+        kSvgLengthtypeUnknown = 0,
+        kSvgLengthtypeNumber = 1,
+        kSvgLengthtypePercentage = 2,
+        kSvgLengthtypeEms = 3,
+        kSvgLengthtypeExs = 4,
+        kSvgLengthtypePx = 5,
+        kSvgLengthtypeCm = 6,
+        kSvgLengthtypeMm = 7,
+        kSvgLengthtypeIn = 8,
+        kSvgLengthtypePt = 9,
+        kSvgLengthtypePc = 10
     };
 
     static SVGLengthTearOff* create(SVGLength* target, SVGElement* contextElement, PropertyIsAnimValType propertyIsAnimVal, const QualifiedName& attributeName = QualifiedName::null())
@@ -62,7 +61,7 @@ public:
         return new SVGLengthTearOff(target, contextElement, propertyIsAnimVal, attributeName);
     }
 
-    SVGLengthType unitType();
+    unsigned short unitType();
     SVGLengthMode unitMode();
     float value(ExceptionState&);
     void setValue(float value, ExceptionState&);
