9f9b4ceb6f63fdcc41a9e6fb231ce7dfcb317879
shaochuan <shaochuan@chromium.org>
2016-09-21 14:25:51 -0700

    MidiManagerWinrt as base::FeatureList feature
    Now MidiManagerWinrt is available as a base::FeatureList feature instead of command-line switch.
    
    BUG=645403
    
    Review-Url: https://codereview.chromium.org/2352013002
    Cr-Commit-Position: refs/heads/master@{#420165}

--- media/midi/midi_switches.h
+++ media/midi/midi_switches.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_MIDI_MIDI_SWITCHES_H_
 #define MEDIA_MIDI_MIDI_SWITCHES_H_
 
+#include "base/feature_list.h"
 #include "build/build_config.h"
 #include "media/midi/midi_export.h"
 
@@ -17,10 +18,18 @@ namespace switches {
 MIDI_EXPORT extern const char kUseAndroidMidiApi[];
 #endif
 
+}  // namespace switches
+
+namespace media {
+namespace midi {
+namespace features {
+
 #if defined(OS_WIN)
-MIDI_EXPORT extern const char kUseWinrtMidiApi[];
+MIDI_EXPORT extern const base::Feature kMidiManagerWinrt;
 #endif
 
-}  // namespace switches
+}  // namespace features
+}  // namespace midi
+}  // namespace media
 
 #endif  // MEDIA_MIDI_MIDI_SWITCHES_H_
