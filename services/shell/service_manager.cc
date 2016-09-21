1bd188fd0d8bb7dffa4d70754e8009f64581df58
loyso <loyso@chromium.org>
2016-09-20 18:08:02 -0700

    Revert of services/shell: Turn on sandboxing for mojo apps. (patchset #9 id:180001 of https://codereview.chromium.org/2338793003/ )
    Reason for revert:
    Causes a timeout on mash_browser_tests.
    
    Original issue's description:
    > services/shell: Turn on sandboxing for mojo apps.
    >
    > Turn on sandbox for mojo apps by default. Apps that need to run
    > without a sandbox need to include 'shell:unsandboxed' in their
    > capabilities in manifest for mojo:shell.
    >
    > Update manifests for mojo:ui, mojo:font_service, mojo:file_system,
    > and mojo:leveldb services to include 'shell:unsandboxed'.
    >
    > BUG=645602, 646376
    >
    > Committed: https://crrev.com/43aa4f706d6020c93f11fa952d7f1d7a7337c570
    > Cr-Commit-Position: refs/heads/master@{#419891}
    
    TBR=rockot@chromium.org,ben@chromium.org,tsepez@chromium.org,sadrul@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=645602, 646376
    
    Review-Url: https://codereview.chromium.org/2353783004
    Cr-Commit-Position: refs/heads/master@{#419928}

--- services/shell/service_manager.cc
+++ services/shell/service_manager.cc
@@ -40,7 +40,6 @@ const char kCapabilityClass_ClientProcess[] = "shell:client_process";
 const char kCapabilityClass_InstanceName[] = "shell:instance_name";
 const char kCapabilityClass_AllUsers[] = "shell:all_users";
 const char kCapabilityClass_ExplicitClass[] = "shell:explicit_class";
-const char kCapabilityClass_Unsandboxed[] = "shell:unsandboxed";
 
 }  // namespace
 
@@ -218,10 +217,10 @@ class ServiceManager::Instance
     StartWithService(std::move(service));
   }
 
-  void StartWithFilePath(const base::FilePath& path, bool unsandboxed) {
+  void StartWithFilePath(const base::FilePath& path) {
     CHECK(!service_);
     runner_ = service_manager_->native_runner_factory_->Create(path);
-    bool start_sandboxed = !unsandboxed;
+    bool start_sandboxed = false;
     mojom::ServicePtr service = runner_->Start(
         path, identity_, start_sandboxed,
         base::Bind(&Instance::PIDAvailable, weak_factory_.GetWeakPtr()),
@@ -823,8 +822,7 @@ void ServiceManager::OnGotResolvedName(std::unique_ptr<ConnectParams> params,
                        instance_name);
       CreateServiceWithFactory(factory, target.name(), std::move(request));
     } else {
-      bool unsandboxed = HasClass(capabilities, kCapabilityClass_Unsandboxed);
-      instance->StartWithFilePath(result->package_path, unsandboxed);
+      instance->StartWithFilePath(result->package_path);
     }
   }
 
