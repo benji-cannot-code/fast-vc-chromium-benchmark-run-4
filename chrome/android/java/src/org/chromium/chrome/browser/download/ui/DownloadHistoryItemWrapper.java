d7a822bf86021434a62b04e68973d178a72304d7
twellington <twellington@chromium.org>
2016-09-21 11:16:59 -0700

    [Downloads UI] Add an undo deletion snackbar
    When items are deleted, show a snackbar that gives the user the option
    of undoing item deletion. If the file path for one of the items being
    deleted matches other download items, those items are removed as well.
    
    BUG=646065
    
    Review-Url: https://codereview.chromium.org/2345713004
    Cr-Commit-Position: refs/heads/master@{#420105}

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryItemWrapper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/DownloadHistoryItemWrapper.java
@@ -102,6 +102,11 @@ public abstract class DownloadHistoryItemWrapper implements TimedItem {
      */
     abstract boolean hasBeenExternallyRemoved();
 
+    /**
+     * @return Whether this download is associated with the off the record profile.
+     */
+    abstract boolean isOffTheRecord();
+
     protected void recordOpenSuccess() {
         RecordHistogram.recordEnumeratedHistogram("Android.DownloadManager.Item.OpenSucceeded",
                 getFilterType(), DownloadFilter.FILTER_BOUNDARY);
@@ -252,6 +257,11 @@ public abstract class DownloadHistoryItemWrapper implements TimedItem {
             return mItem.hasBeenExternallyRemoved();
         }
 
+        @Override
+        boolean isOffTheRecord() {
+            return mIsOffTheRecord;
+        }
+
         /** Identifies the type of file represented by the given MIME type string. */
         private static int convertMimeTypeToFilterType(String mimeType) {
             if (TextUtils.isEmpty(mimeType)) return DownloadFilter.FILTER_OTHER;
@@ -356,6 +366,11 @@ public abstract class DownloadHistoryItemWrapper implements TimedItem {
         @Override
         boolean hasBeenExternallyRemoved() {
             // We don't currently detect when offline pages have been removed externally.
+            return false;
+        }
+
+        @Override
+        boolean isOffTheRecord() {
             return false;
         }
     }
