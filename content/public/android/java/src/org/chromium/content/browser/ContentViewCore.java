da72b6b3544563197c8dd0d4201e7c3bec59b0f7
timav <timav@chromium.org>
2016-09-20 17:43:39 -0700

    Keep ContentViewCore selection rectangle in DIP
    ContentViewCore holds selection rectangle in physical pixels,
    thus making it indirectly dependent on DIP scale.
    
    This CL changes it to DIP and does the necessary conversions
    in Java layer. As a consequence the position of the paste popup
    dialog is also passed from native layer in DIP.
    
    BUG=620929
    
    Review-Url: https://codereview.chromium.org/2352143002
    Cr-Commit-Position: refs/heads/master@{#419922}

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -376,6 +376,8 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Screen
     private boolean mUnselectAllOnActionModeDismiss;
     private boolean mPreserveSelectionOnNextLossOfFocus;
     private WebActionModeCallback.ActionHandler mActionHandler;
+
+    // Selection rectangle in DIP.
     private final Rect mSelectionRect = new Rect();
 
     // Whether native accessibility, i.e. without any script injection, is allowed.
@@ -1966,11 +1968,16 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Screen
                 }
 
                 @Override
-                public void onGetContentRect(Rect outRect) {
+                public void onGetContentRect(Rect outRectPix) {
+                    final float deviceScale = mRenderCoordinates.getDeviceScaleFactor();
+                    outRectPix.set((int) (mSelectionRect.left * deviceScale),
+                            (int) (mSelectionRect.top * deviceScale),
+                            (int) (mSelectionRect.right * deviceScale),
+                            (int) (mSelectionRect.bottom * deviceScale));
+
                     // The selection coordinates are relative to the content viewport, but we need
                     // coordinates relative to the containing View.
-                    outRect.set(mSelectionRect);
-                    outRect.offset(0, (int) mRenderCoordinates.getContentOffsetYPix());
+                    outRectPix.offset(0, (int) mRenderCoordinates.getContentOffsetYPix());
                 }
 
                 @Override
@@ -2110,6 +2117,7 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Screen
         return mHasInsertion;
     }
 
+    // All coordinates are in DIP.
     @CalledByNative
     private void onSelectionEvent(
             int eventType, int xAnchor, int yAnchor, int left, int top, int right, int bottom) {
@@ -2196,7 +2204,10 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Screen
                 assert false : "Invalid selection event type.";
         }
         if (mContextualSearchClient != null) {
-            mContextualSearchClient.onSelectionEvent(eventType, xAnchor, yAnchor);
+            final float deviceScale = mRenderCoordinates.getDeviceScaleFactor();
+            int xAnchorPix = (int) (xAnchor * deviceScale);
+            int yAnchorPix = (int) (yAnchor * deviceScale);
+            mContextualSearchClient.onSelectionEvent(eventType, xAnchorPix, yAnchorPix);
         }
     }
 
@@ -2496,17 +2507,23 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Screen
         return false;
     }
 
+    // Coordinates are in DIP.
     private boolean showPastePopup(int x, int y) {
         if (mContainerView.getParent() == null || mContainerView.getVisibility() != View.VISIBLE) {
             return false;
         }
 
         if (!mHasInsertion || !canPaste()) return false;
-        final float topControlsShown = mRenderCoordinates.getContentOffsetYPix();
+
         PastePopupMenu pastePopupMenu = getPastePopup();
         if (pastePopupMenu == null) return false;
+
+        final float deviceScale = mRenderCoordinates.getDeviceScaleFactor();
+        final int xPix = (int) (x * deviceScale);
+        final int yPix = (int) (y * deviceScale);
+        final float topControlsShownPix = mRenderCoordinates.getContentOffsetYPix();
         try {
-            pastePopupMenu.show(x, (int) (y + topControlsShown));
+            pastePopupMenu.show(xPix, (int) (yPix + topControlsShownPix));
         } catch (WindowManager.BadTokenException e) {
             return false;
         }
