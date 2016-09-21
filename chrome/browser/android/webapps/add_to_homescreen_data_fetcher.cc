68b2cee0a927c23705c3b18ea92b2e2d15f7710a
pkotwicz <pkotwicz@chromium.org>
2016-09-20 19:17:41 -0700

    Do not create WebAPK via add-to-homescreen menu item if manifest is not WebAPK-compatible
    This CL prevents creating WebAPKs via the add-to-homescreen menu item:
    - If the Web Manifest does not contain any icons
    - If any URL in the Web Manifest contains a username and password
    
    This makes the logic in add_to_homescreen_data_fetcher.cc match the logic in
    app_banner_manager_android.cc
    
    BUG=648491
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2349263003
    Cr-Commit-Position: refs/heads/master@{#419949}

--- chrome/browser/android/webapps/add_to_homescreen_data_fetcher.cc
+++ chrome/browser/android/webapps/add_to_homescreen_data_fetcher.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "chrome/browser/android/offline_pages/offline_page_utils.h"
 #include "chrome/browser/android/shortcut_helper.h"
+#include "chrome/browser/android/webapk/webapk_web_manifest_checker.h"
 #include "chrome/browser/favicon/favicon_service_factory.h"
 #include "chrome/browser/installable/installable_manager.h"
 #include "chrome/browser/manifest/manifest_icon_selector.h"
@@ -206,8 +207,12 @@ void AddToHomescreenDataFetcher::OnDidPerformInstallableCheck(
 
   is_installable_check_complete_ = true;
 
-  if (check_webapk_compatibility_)
-    weak_observer_->OnDidDetermineWebApkCompatibility(data.is_installable);
+  if (check_webapk_compatibility_) {
+    bool webapk_compatible =
+        (data.error_code == NO_ERROR_DETECTED &&
+         AreWebManifestUrlsWebApkCompatible(data.manifest));
+    weak_observer_->OnDidDetermineWebApkCompatibility(webapk_compatible);
+  }
 
   if (!data.manifest.IsEmpty()) {
     content::RecordAction(
