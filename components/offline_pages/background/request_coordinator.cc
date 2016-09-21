a93fdf8d58e8eb2f24fe280b06aff358071eec89
dougarnett <dougarnett@chromium.org>
2016-09-21 13:35:42 -0700

    [Offline Pages] Breaks out new TIMED_OUT Offliner status for RequestCoordinator watchdog timeout in UMA and event log.
    BUG=624452
    
    Review-Url: https://codereview.chromium.org/2355163002
    Cr-Commit-Position: refs/heads/master@{#420153}

--- components/offline_pages/background/request_coordinator.cc
+++ components/offline_pages/background/request_coordinator.cc
@@ -142,18 +142,20 @@ void RequestCoordinator::GetQueuedRequestsCallback(
   callback.Run(std::move(requests));
 }
 
-void RequestCoordinator::StopPrerendering() {
+void RequestCoordinator::StopPrerendering(Offliner::RequestStatus stop_status) {
   if (offliner_ && is_busy_) {
     DCHECK(active_request_.get());
     offliner_->Cancel();
     AbortRequestAttempt(active_request_.get());
   }
 
-  // Stopping offliner means it will not call callback.
-  last_offlining_status_ =
-      Offliner::RequestStatus::REQUEST_COORDINATOR_CANCELED;
+  // Stopping offliner means it will not call callback so set last status.
+  last_offlining_status_ = stop_status;
 
   if (active_request_) {
+    event_logger_.RecordOfflinerResult(active_request_->client_id().name_space,
+                                       last_offlining_status_,
+                                       active_request_->request_id());
     RecordOfflinerResultUMA(active_request_->client_id(),
                             last_offlining_status_);
     is_busy_ = false;
@@ -190,7 +192,7 @@ bool RequestCoordinator::CancelActiveRequestIfItMatches(
   if (active_request_ != nullptr) {
     if (request_ids.end() != std::find(request_ids.begin(), request_ids.end(),
                                        active_request_->request_id())) {
-      StopPrerendering();
+      StopPrerendering(Offliner::RequestStatus::REQUEST_COORDINATOR_CANCELED);
       return true;
     }
   }
@@ -345,8 +347,17 @@ void RequestCoordinator::ScheduleAsNeeded() {
 }
 
 void RequestCoordinator::StopProcessing() {
+  StopProcessingWithStatus(Offliner::REQUEST_COORDINATOR_CANCELED);
+}
+
+void RequestCoordinator::HandleWatchdogTimeout() {
+  StopProcessingWithStatus(Offliner::REQUEST_COORDINATOR_TIMED_OUT);
+}
+
+void RequestCoordinator::StopProcessingWithStatus(
+    Offliner::RequestStatus stop_status) {
   is_stopped_ = true;
-  StopPrerendering();
+  StopPrerendering(stop_status);
 
   // Let the scheduler know we are done processing.
   scheduler_callback_.Run(true);
@@ -483,7 +494,7 @@ void RequestCoordinator::SendRequestToOffliner(const SavePageRequest& request) {
 
     // Start a watchdog timer to catch pre-renders running too long
     watchdog_timer_.Start(FROM_HERE, offliner_timeout_, this,
-                          &RequestCoordinator::StopProcessing);
+                          &RequestCoordinator::HandleWatchdogTimeout);
   } else {
     is_busy_ = false;
     DVLOG(0) << "Unable to start LoadAndSave";
