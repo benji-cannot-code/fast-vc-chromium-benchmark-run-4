9e6938fb7df5e900f7054e200832772db40462f2
dgrogan <dgrogan@chromium.org>
2016-09-21 16:17:41 -0700

    [css-tables] Set needsLayout on cells when table border width changes
    When borders collapse, table borders affect cell widths but we weren't
    marking cells properly. As a result cells and tables were using their
    old widths.
    
    BUG=613728
    
    Review-Url: https://codereview.chromium.org/2358663003
    Cr-Commit-Position: refs/heads/master@{#420193}

--- third_party/WebKit/Source/core/layout/LayoutTableSection.h
+++ third_party/WebKit/Source/core/layout/LayoutTableSection.h
@@ -116,8 +116,8 @@ public:
     void layoutRows();
     void computeOverflowFromCells();
     bool recalcChildOverflowAfterStyleChange();
-    enum WhatToMarkAllCells { MarkDirtyOnly, MarkDirtyAndNeedsLayout };
-    void markAllCellsWidthsDirtyAndOrNeedsLayout(WhatToMarkAllCells);
+
+    void markAllCellsWidthsDirtyAndOrNeedsLayout(LayoutTable::WhatToMarkAllCells);
 
     LayoutTable* table() const { return toLayoutTable(parent()); }
 
