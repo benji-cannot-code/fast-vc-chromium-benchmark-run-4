a34e0bd1fe485c475b15c6a561615696662505f8
xiaochengh <xiaochengh@chromium.org>
2016-09-21 03:45:32 -0700

    Introduce VisiblePosition::isValid
    In general, functions taking VisiblePosition parameters must be called
    in clean layout and before any DOM mutation. This patch introduces
    VisiblePosition::isValid to check this.
    
    This patch is a preparation for pruning createVisiblePositionDeprecated.
    
    BUG=647219
    
    Review-Url: https://codereview.chromium.org/2354893002
    Cr-Commit-Position: refs/heads/master@{#420024}

--- third_party/WebKit/Source/core/editing/VisibleUnits.cpp
+++ third_party/WebKit/Source/core/editing/VisibleUnits.cpp
@@ -1338,13 +1338,13 @@ static inline LayoutPoint absoluteLineDirectionPointToLocalPointInBlock(RootInli
 
 VisiblePosition previousLinePosition(const VisiblePosition& visiblePosition, LayoutUnit lineDirectionPoint, EditableType editableType)
 {
+    DCHECK(visiblePosition.isValid());
+
     Position p = visiblePosition.deepEquivalent();
     Node* node = p.anchorNode();
 
     if (!node)
         return VisiblePosition();
-
-    DCHECK(!node->document().needsLayoutTreeUpdate());
 
     LayoutObject* layoutObject = node->layoutObject();
     if (!layoutObject)
@@ -1391,13 +1391,13 @@ VisiblePosition previousLinePosition(const VisiblePosition& visiblePosition, Lay
 
 VisiblePosition nextLinePosition(const VisiblePosition& visiblePosition, LayoutUnit lineDirectionPoint, EditableType editableType)
 {
+    DCHECK(visiblePosition.isValid());
+
     Position p = visiblePosition.deepEquivalent();
     Node* node = p.anchorNode();
 
     if (!node)
         return VisiblePosition();
-
-    DCHECK(!node->document().needsLayoutTreeUpdate());
 
     LayoutObject* layoutObject = node->layoutObject();
     if (!layoutObject)
