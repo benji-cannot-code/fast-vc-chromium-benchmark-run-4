ddd9fea8355fdf0122f947a60b863656deec7d61
mahmadi <mahmadi@chromium.org>
2016-09-22 08:46:23 -0700

    Profile Avatar Selector: Allow arrow keys to be used for moving between avatars.
    BUG=639843
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2269743002
    Cr-Commit-Position: refs/heads/master@{#420355}

--- chrome/test/data/webui/settings/people_page_manage_profile_test.js
+++ chrome/test/data/webui/settings/people_page_manage_profile_test.js
@@ -54,7 +54,7 @@ cr.define('settings_people_page_manage_profile', function() {
       //  - has the correct icon selected
       //  - can select a new icon
       test('ManageProfileChangeIcon', function() {
-        var selector = manageProfile.$.selector.$.selector;
+        var selector = manageProfile.$.selector.$['avatar-grid'];
         assertTrue(!!selector);
 
         return browserProxy.whenCalled('getAvailableIcons').then(function() {
@@ -76,7 +76,7 @@ cr.define('settings_people_page_manage_profile', function() {
 
       // Tests profile icon updates pushed from the browser.
       test('ManageProfileIconUpdated', function() {
-        var selector = manageProfile.$.selector.$.selector;
+        var selector = manageProfile.$.selector.$['avatar-grid'];
         assertTrue(!!selector);
 
         return browserProxy.whenCalled('getAvailableIcons').then(function() {
