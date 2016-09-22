c0169bf610a2dbee159266e16d03f058181c0df7
estade <estade@chromium.org>
2016-09-22 08:33:24 -0700

    Use vector icons for MacOS infobars.
    Remove png asset for app banner infobar.
    
    BUG=505953
    
    Review-Url: https://codereview.chromium.org/2336283002
    Cr-Commit-Position: refs/heads/master@{#420349}

--- components/infobars/core/infobar_delegate.cc
+++ components/infobars/core/infobar_delegate.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/vector_icons_public.h"
 
-#if !defined(OS_MACOSX) && !defined(OS_IOS) && !defined(OS_ANDROID)
+#if !defined(OS_IOS) && !defined(OS_ANDROID)
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/paint_vector_icon.h"
 #endif
@@ -43,7 +43,7 @@ gfx::VectorIconId InfoBarDelegate::GetVectorIconId() const {
 }
 
 gfx::Image InfoBarDelegate::GetIcon() const {
-#if !defined(OS_MACOSX) && !defined(OS_IOS) && !defined(OS_ANDROID)
+#if !defined(OS_IOS) && !defined(OS_ANDROID)
   if (ui::MaterialDesignController::IsModeMaterial()) {
     gfx::VectorIconId vector_id = GetVectorIconId();
     if (vector_id != gfx::VectorIconId::VECTOR_ICON_NONE) {
