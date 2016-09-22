5b2cc97e033b967fe0a8a2b18480781030e98c1f
tsergeant <tsergeant@chromium.org>
2016-09-21 18:00:19 -0700

    History: Fix flicker of sync notices when loading new data
    The history page shows a notice when showing synced results from other
    devices. This notice would frequently flicker when loading in new
    results (eg, from a search), which was particularly noticable on the MD
    History page, where this can cause toolbar elements to move around.
    
    This fixes the issue by only resetting notice status back to false
    when there is no web history service. In all other cases, we can
    rely on the call to web history to reset the sync notices to their
    correct values.
    
    BUG=628555
    
    Review-Url: https://codereview.chromium.org/2354053003
    Cr-Commit-Position: refs/heads/master@{#420220}

--- chrome/browser/ui/webui/browsing_history_handler.cc
+++ chrome/browser/ui/webui/browsing_history_handler.cc
@@ -419,6 +419,7 @@ void BrowsingHistoryHandler::OnStateChanged() {
 }
 
 void BrowsingHistoryHandler::WebHistoryTimeout() {
+  has_synced_results_ = false;
   // TODO(dubroy): Communicate the failure to the front end.
   if (!query_task_tracker_.HasTrackedTasks())
     ReturnResultsToFrontEnd();
@@ -439,8 +440,6 @@ void BrowsingHistoryHandler::QueryHistory(
 
   query_results_.clear();
   results_info_value_.Clear();
-  has_synced_results_ = false;
-  has_other_forms_of_browsing_history_ = false;
 
   history::HistoryService* hs = HistoryServiceFactory::GetForProfile(
       profile, ServiceAccessType::EXPLICIT_ACCESS);
@@ -482,6 +481,8 @@ void BrowsingHistoryHandler::QueryHistory(
   } else {
     // The notice could not have been shown, because there is no web history.
     RecordMetricsForNoticeAboutOtherFormsOfBrowsingHistory(false);
+    has_synced_results_ = false;
+    has_other_forms_of_browsing_history_ = false;
   }
 }
 
