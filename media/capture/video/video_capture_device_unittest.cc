efdad649c15c5032751b33e63620eae0e9c68989
mcasas <mcasas@chromium.org>
2016-09-22 12:27:02 -0700

    VideoCaptureDeviceTest: improve error logging
    Associated bug's #3 is a revert and got me thinking that we don't log
    the error received in the VCDTest's MockVideoCaptureDeviceClient's, so
    this CL adds a small ON_CALL for it.  The difference is that, e.g. in
    the OpenInvalidDevice test case, we get an extra line with the error
    origin, message and the OS/System error message, if any.
    
    [ RUN      ] VideoCaptureDeviceTest.OpenInvalidDevice
    [...]
    [58186:1295:0922/102314:1087119696054855:ERROR:video_capture_device_unittest.cc(84)] AllocateAndStart@../../media/capture/video/mac/video_capture_device_mac.mm:326 Could not open capture device.: No such file or directory
    
    BUG=647352
    
    Review-Url: https://codereview.chromium.org/2359243002
    Cr-Commit-Position: refs/heads/master@{#420423}

--- media/capture/video/video_capture_device_unittest.cc
+++ media/capture/video/video_capture_device_unittest.cc
@@ -73,10 +73,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using ::testing::_;
+using ::testing::Invoke;
 using ::testing::SaveArg;
 
 namespace media {
 namespace {
+
+void DumpError(const tracked_objects::Location& location,
+               const std::string& message) {
+  DPLOG(ERROR) << location.ToString() << " " << message;
+}
 
 class MockVideoCaptureClient : public VideoCaptureDevice::Client {
  public:
@@ -91,8 +97,9 @@ class MockVideoCaptureClient : public VideoCaptureDevice::Client {
 
   explicit MockVideoCaptureClient(
       base::Callback<void(const VideoCaptureFormat&)> frame_cb)
-      : main_thread_(base::ThreadTaskRunnerHandle::Get()),
-        frame_cb_(frame_cb) {}
+      : main_thread_(base::ThreadTaskRunnerHandle::Get()), frame_cb_(frame_cb) {
+    ON_CALL(*this, OnError(_, _)).WillByDefault(Invoke(DumpError));
+  }
 
   void OnIncomingCapturedData(const uint8_t* data,
                               int length,
