368cc3c309f484573945e28cf5fb803910a96720
boliu <boliu@chromium.org>
2016-09-21 23:55:44 -0700

    Remove VIDEO_HOLE implementation
    Remove components/external_video_surface.
    Remove everything behind VIDEO_HOLE.
    Remove use_view_overlay_for_all_video.
    
    use_video_overlay_for_embedded_encrypted_video is still used for
    media encryption look up, so not removed.
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2353823002
    Cr-Commit-Position: refs/heads/master@{#420273}

--- content/public/browser/content_browser_client.h
+++ content/public/browser/content_browser_client.h
@@ -787,13 +787,6 @@ class CONTENT_EXPORT ContentBrowserClient {
   virtual bool IsWin32kLockdownEnabledForMimeType(
       const std::string& mime_type) const;
 #endif
-
-#if defined(VIDEO_HOLE)
-  // Allows an embedder to provide its own ExternalVideoSurfaceContainer
-  // implementation.  Return nullptr to disable external surface video.
-  virtual ExternalVideoSurfaceContainer*
-  OverrideCreateExternalVideoSurfaceContainer(WebContents* web_contents);
-#endif
 };
 
 }  // namespace content
