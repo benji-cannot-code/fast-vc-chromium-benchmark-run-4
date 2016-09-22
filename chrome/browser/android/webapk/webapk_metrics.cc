1e7831ecb1454b49d4f5cfe9d6f264ac6e397e95
hanxi <hanxi@chromium.org>
2016-09-22 12:53:42 -0700

    Add WebAPK installation metrics.
    Records metrics when WebAPK is installed via banner or add-to-homescreen menu,
    including: install events, installation start types, and user actions.
    
    Design doc: https://docs.google.com/document/d/1pQPrHeI4L_jUZ8B8Sh4N0z_yiXwiFuoEhLl3AMXvt-I/edit#heading=h.1ctovw0c1tx
    
    BUG=626510
    
    Review-Url: https://codereview.chromium.org/2301263004
    Cr-Commit-Position: refs/heads/master@{#420433}

---
+++ chrome/browser/android/webapk/webapk_metrics.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/android/webapk/webapk_metrics.h"
+
+#include "base/metrics/histogram_macros.h"
+
+namespace webapk {
+
+const char kInstallEventHistogram[] = "WebApk.Install.InstallEvent";
+const char kInstallSourceHistogram[] = "WebApk.Install.InstallSource";
+const char kInfoBarShownHistogram[] = "WebApk.Install.InfoBarShown";
+const char kUserActionHistogram[] = "WebApk.Install.UserAction";
+
+void TrackInstallEvent(InstallEvent event) {
+  UMA_HISTOGRAM_ENUMERATION(kInstallEventHistogram, event, INSTALL_EVENT_MAX);
+}
+
+void TrackInstallSource(InstallSource event) {
+  UMA_HISTOGRAM_ENUMERATION(kInstallSourceHistogram, event, INSTALL_SOURCE_MAX);
+}
+
+void TrackInstallInfoBarShown(InfoBarShown event) {
+  UMA_HISTOGRAM_ENUMERATION(kInfoBarShownHistogram, event,
+                            WEBAPK_INFOBAR_SHOWN_MAX);
+}
+
+void TrackUserAction(UserAction event) {
+  UMA_HISTOGRAM_ENUMERATION(kUserActionHistogram, event, USER_ACTION_MAX);
+}
+
+}  // namespace webapk
