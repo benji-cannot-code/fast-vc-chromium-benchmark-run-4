2899194a4b28c3bef26eca969a3dfb3a017d9e40
eugenebut <eugenebut@chromium.org>
2016-09-21 19:40:48 -0700

    [ios] Removed __gCrWeb.windowIdObject JS object.
    windowIdObject was used by UIWebView as a presence beacon and not
    required anymore.
    
    BUG=579697
    
    Review-Url: https://codereview.chromium.org/2358103003
    Cr-Commit-Position: refs/heads/master@{#420244}

--- ios/web/web_state/js/resources/window_id.js
+++ ios/web/web_state/js/resources/window_id.js
@@ -7,11 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // compilation with ADVANCED_OPTIMIZATIONS. See http://goo.gl/FwOgy
 
 // Script to set windowId.
-
-
-// Namespace for module, used as presence beacon for injection checks.
-__gCrWeb['windowIdObject'] = {};
-
 (function() {
   // CRWJSWindowIDManager replaces $(WINDOW_ID) with appropriate string upon
   // injection.
