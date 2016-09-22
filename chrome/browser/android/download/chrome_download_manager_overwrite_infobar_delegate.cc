60509b88484814b75932337f4d27c20a0efd192e
qinmin <qinmin@chromium.org>
2016-09-22 10:29:37 -0700

    Ask download manager to remove a download if it is overwritten by another
    When a downloaded file is overwritten by another,
    we should ask DownloadManager to check for externally removed files.
    
    BUG=647042
    
    Review-Url: https://codereview.chromium.org/2344483003
    Cr-Commit-Position: refs/heads/master@{#420381}

--- chrome/browser/android/download/chrome_download_manager_overwrite_infobar_delegate.cc
+++ chrome/browser/android/download/chrome_download_manager_overwrite_infobar_delegate.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/android/download/download_controller.h"
+#include "chrome/browser/android/download/download_manager_service.h"
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/ui/android/infobars/download_overwrite_infobar.h"
 #include "components/infobars/core/infobar.h"
@@ -21,6 +22,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 
 namespace {
+
+void DeleteExistingDownloadFileDone(
+    const base::FilePath& download_path,
+    const DownloadTargetDeterminerDelegate::FileSelectedCallback& callback) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  DownloadManagerService::GetInstance()->RemoveDownloadsForPath(
+      download_path);
+  callback.Run(download_path);
+}
 
 void DeleteExistingDownloadFile(
     const base::FilePath& download_path,
@@ -30,8 +40,9 @@ void DeleteExistingDownloadFile(
   if (GetFileInfo(download_path, &info) && !info.is_directory)
     base::DeleteFile(download_path, false);
 
-  content::BrowserThread::PostTask(content::BrowserThread::UI, FROM_HERE,
-                                   base::Bind(callback, download_path));
+  content::BrowserThread::PostTask(
+      content::BrowserThread::UI, FROM_HERE,
+      base::Bind(&DeleteExistingDownloadFileDone, download_path, callback));
 }
 
 void CreateNewFileDone(
@@ -90,6 +101,9 @@ ChromeDownloadManagerOverwriteInfoBarDelegate::GetIdentifier() const {
 }
 
 bool ChromeDownloadManagerOverwriteInfoBarDelegate::OverwriteExistingFile() {
+  if (!download_item_)
+    return true;
+
   content::BrowserThread::PostTask(
       content::BrowserThread::FILE, FROM_HERE,
       base::Bind(&DeleteExistingDownloadFile, suggested_download_path_,
