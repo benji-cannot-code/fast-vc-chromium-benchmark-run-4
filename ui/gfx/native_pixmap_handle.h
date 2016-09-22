fe82e773794670392d6f3060718c1fe5f3248df2
dcastagna <dcastagna@chromium.org>
2016-09-21 20:24:19 -0700

    Add ClientNativePixmap multi-planar support.
    ClientNativePixmap, that lets clients map pixmaps in user space,
    always assumed a pixmap contained only one plane.
    
    This CL adds support for multiple-planes.
    
    BUG=618516
    TBR=posciak
    TEST=gl_unittests on samus.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2272153002
    Cr-Commit-Position: refs/heads/master@{#420251}

--- ui/gfx/native_pixmap_handle.h
+++ ui/gfx/native_pixmap_handle.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GFX_NATIVE_PIXMAP_HANDLE_H_
 #define UI_GFX_NATIVE_PIXMAP_HANDLE_H_
 
-#include <stdint.h>
+#include <stddef.h>
 #include <vector>
 
 #include "ui/gfx/gfx_export.h"
@@ -21,7 +21,7 @@ namespace gfx {
 // buffer. More fields can be added if they are plane specific.
 struct GFX_EXPORT NativePixmapPlane {
   NativePixmapPlane();
-  NativePixmapPlane(int stride, int offset, uint64_t modifier);
+  NativePixmapPlane(int stride, int offset, size_t size, uint64_t modifier);
   NativePixmapPlane(const NativePixmapPlane& other);
   ~NativePixmapPlane();
 
@@ -29,6 +29,9 @@ struct GFX_EXPORT NativePixmapPlane {
   // a memory mapping. One per plane per entry.
   int stride;
   int offset;
+  // Size in bytes of the plane.
+  // This is necessary to map the buffers.
+  size_t size;
   // The modifier is retrieved from GBM library and passed to EGL driver.
   // Generally it's platform specific, and we don't need to modify it in
   // Chromium code. Also one per plane per entry.
