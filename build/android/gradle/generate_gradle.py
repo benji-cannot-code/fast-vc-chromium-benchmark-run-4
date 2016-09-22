d779703acb3d9bb1c7780c32f810aedf30a2211e
agrieve <agrieve@chromium.org>
2016-09-21 18:03:34 -0700

    Change gradle project names to use > rather than $ as separators
    The $ was sometimes causing Android Studio to show warnings that the
    names were interpreted as variables. / and : are not allowed at all.
    
    BUG=620034
    TBR=jbudorick
    
    Review-Url: https://codereview.chromium.org/2354073002
    Cr-Commit-Position: refs/heads/master@{#420222}

--- build/android/gradle/generate_gradle.py
+++ build/android/gradle/generate_gradle.py
@@ -116,7 +116,7 @@ class _ProjectEntry(object):
 
   def ProjectName(self):
     """Returns the Gradle project name."""
-    return self.GradleSubdir().replace(os.path.sep, '\\$')
+    return self.GradleSubdir().replace(os.path.sep, '>')
 
   def BuildConfig(self):
     """Reads and returns the project's .build_config JSON."""
