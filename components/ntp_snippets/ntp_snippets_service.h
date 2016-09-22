aa679fdd52029ccdfb9fa4fad2c355a5fa351ab5
treib <treib@chromium.org>
2016-09-22 05:44:01 -0700

    [NTP Snippets] Don't unschedule background fetching when NOT_INITED
    BUG=646842
    
    Review-Url: https://codereview.chromium.org/2360613002
    Cr-Commit-Position: refs/heads/master@{#420312}

--- components/ntp_snippets/ntp_snippets_service.h
+++ components/ntp_snippets/ntp_snippets_service.h
@@ -147,6 +147,7 @@ class NTPSnippetsService : public ContentSuggestionsProvider,
 
  private:
   friend class NTPSnippetsServiceTest;
+  FRIEND_TEST_ALL_PREFIXES(NTPSnippetsServiceTest, RescheduleOnStateChange);
   FRIEND_TEST_ALL_PREFIXES(NTPSnippetsServiceTest, StatusChanges);
 
   // Possible state transitions:
