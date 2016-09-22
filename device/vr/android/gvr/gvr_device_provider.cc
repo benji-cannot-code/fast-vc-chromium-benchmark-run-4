052ce537857f542430d37632ab00fb1da8b246f1
bajones <bajones@chromium.org>
2016-09-21 19:21:51 -0700

    Added support for GVR controllers
    Currently uses a different instance of GvrApi than the VrShell, but it's not
    clear if that's something that's going to cause a problem. Works well enough for
    the time being.
    
    BUG=389343
    
    Review-Url: https://codereview.chromium.org/2351573002
    Cr-Commit-Position: refs/heads/master@{#420240}

--- device/vr/android/gvr/gvr_device_provider.cc
+++ device/vr/android/gvr/gvr_device_provider.cc
@@ -13,9 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "device/vr/android/gvr/gvr_delegate.h"
 #include "device/vr/android/gvr/gvr_device.h"
+#include "device/vr/android/gvr/gvr_gamepad_data_fetcher.h"
 #include "device/vr/vr_device_manager.h"
 #include "jni/GvrDeviceProvider_jni.h"
 #include "third_party/gvr-android-sdk/src/ndk-beta/include/vr/gvr/capi/include/gvr.h"
+#include "third_party/gvr-android-sdk/src/ndk-beta/include/vr/gvr/capi/include/gvr_controller.h"
+#include "third_party/gvr-android-sdk/src/ndk-beta/include/vr/gvr/capi/include/gvr_types.h"
 
 using base::android::AttachCurrentThread;
 using base::android::GetApplicationContext;
@@ -75,6 +78,9 @@ GvrDeviceProvider::GvrDeviceProvider()
       main_thread_task_runner_(base::ThreadTaskRunnerHandle::Get()) {}
 
 GvrDeviceProvider::~GvrDeviceProvider() {
+  GamepadDataFetcherManager::GetInstance()->RemoveSourceFactory(
+      GAMEPAD_SOURCE_GVR);
+
   ExitPresent();
 }
 
@@ -96,6 +102,10 @@ void GvrDeviceProvider::Initialize() {
     if (non_presenting_delegate_->gvr_api()) {
       vr_device_.reset(new GvrDevice(this, non_presenting_delegate_.get()));
       client_->OnDeviceConnectionStatusChanged(vr_device_.get(), true);
+
+      GamepadDataFetcherManager::GetInstance()->AddFactory(
+          new GvrGamepadDataFetcher::Factory(non_presenting_delegate_.get(),
+                                             vr_device_->id()));
     }
   }
 }
