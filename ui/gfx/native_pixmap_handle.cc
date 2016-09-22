4fe4cd30d873b18b3510929a55923f90f285dd2b
dcastagna <dcastagna@chromium.org>
2016-09-22 16:36:57 -0700

    Revert of Add ClientNativePixmap multi-planar support. (patchset #8 id:160001 of https://codereview.chromium.org/2272153002/ )
    Reason for revert:
    This makes samus stuck at the Chrome logo.
    
    Original issue's description:
    > Add ClientNativePixmap multi-planar support.
    >
    > ClientNativePixmap, that lets clients map pixmaps in user space,
    > always assumed a pixmap contained only one plane.
    >
    > This CL adds support for multiple-planes.
    >
    > BUG=618516
    > TBR=posciak
    > TEST=gl_unittests on samus.
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    >
    > Committed: https://crrev.com/cdb357b81306f5e8170027d13d931b8e26140405
    > Cr-Commit-Position: refs/heads/master@{#420251}
    
    TBR=ccameron@chromium.org,dnicoara@chromium.org,posciak@chromium.org,reveman@chromium.org,spang@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=618516
    
    Review-Url: https://codereview.chromium.org/2361023003
    Cr-Commit-Position: refs/heads/master@{#420501}

--- ui/gfx/native_pixmap_handle.cc
+++ ui/gfx/native_pixmap_handle.cc
@@ -3,20 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <stdint.h>
-
 #include "ui/gfx/native_pixmap_handle.h"
 
 namespace gfx {
 
-NativePixmapPlane::NativePixmapPlane()
-    : stride(0), offset(0), size(0), modifier(0) {}
+NativePixmapPlane::NativePixmapPlane() : stride(0), offset(0), modifier(0) {}
 
-NativePixmapPlane::NativePixmapPlane(int stride,
-                                     int offset,
-                                     size_t size,
-                                     uint64_t modifier)
-    : stride(stride), offset(offset), size(size), modifier(modifier) {}
+NativePixmapPlane::NativePixmapPlane(int stride, int offset, uint64_t modifier)
+    : stride(stride), offset(offset), modifier(modifier) {}
 
 NativePixmapPlane::NativePixmapPlane(const NativePixmapPlane& other) = default;
 
