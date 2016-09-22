c0fb63a04fcff35a695fa189a860ab482e6a4149
rockot <rockot@chromium.org>
2016-09-21 17:25:53 -0700

    Add mojom for WindowOpenDisposition
    Adds ui::mojom::WindowOpenDisposition and a typemap which maps it
    to ::WindowOpenDisposition. This will be useful when converting
    existing IPCs to mojom.
    
    C++ references to WindowOpenDisposition are left alone for now and
    we'll rely on the typemap to do its thing for Mojo messages.
    
    Java references have been updated to use the mojom generated type
    directly, removing window_open_disposition.h from the set of headers
    processed by a java_cpp_enum rule.
    
    BUG=612500
    
    Review-Url: https://codereview.chromium.org/2363533002
    Cr-Commit-Position: refs/heads/master@{#420211}

--- ui/base/window_open_disposition.h
+++ ui/base/window_open_disposition.h
@@ -8,8 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/ui_base_export.h"
 
-// A Java counterpart will be generated for this enum.
-// GENERATED_JAVA_ENUM_PACKAGE: org.chromium.ui
+// DEPRECATED: Instead of introducing new references to this enum, use
+// the generated ui::mojom::WindowOpenDisposition in
+// ui/base/mojo/window_open_disposition.mojom.h.
 enum class WindowOpenDisposition {
   UNKNOWN,
   CURRENT_TAB,
