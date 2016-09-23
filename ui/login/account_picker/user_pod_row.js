13060687dc76b6a2061bcf041247c088e1095f4f
sammiequon <sammiequon@chromium.org>
2016-09-23 11:16:03 -0700

    cros: Minor issues regarding submit button and tabbing on user pod fixed.
    Previously, when multiple user pods are present, tabbing out of oneresets the password, but the submit button is not disabled. Also the submit button was being tabbed into after the header bar; removed tabbing for submit button since enter key does the same thing.
    
    BUG=649370
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2365943002
    Cr-Commit-Position: refs/heads/master@{#420665}

--- ui/login/account_picker/user_pod_row.js
+++ ui/login/account_picker/user_pod_row.js
@@ -1443,6 +1443,7 @@ cr.define('login', function() {
       this.passwordElement.value = '';
       if (this.pinKeyboard)
         this.pinKeyboard.value = '';
+      this.updateInput_();
       this.classList.toggle('signing-in', false);
       if (takeFocus) {
         if (!this.multiProfilesPolicyApplied)
