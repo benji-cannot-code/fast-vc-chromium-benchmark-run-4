e467967dfc9889777481704ba7ae0b344df2d6c4
ftirelo <ftirelo@chromium.org>
2016-09-22 17:58:13 -0700

    Adds histograms for tracking Software Reporter logs uploads in SRT Fetcher.
    BUG=634434
    
    Review-Url: https://codereview.chromium.org/2347753002
    Cr-Commit-Position: refs/heads/master@{#420516}

--- chrome/browser/component_updater/sw_reporter_installer_win_unittest.cc
+++ chrome/browser/component_updater/sw_reporter_installer_win_unittest.cc
@@ -82,11 +82,11 @@ class SwReporterInstallerTest : public ::testing::Test {
     EXPECT_TRUE(invocation.command_line.GetSwitches().empty());
     EXPECT_TRUE(invocation.command_line.GetArgs().empty());
     EXPECT_TRUE(invocation.suffix.empty());
-    EXPECT_EQ(SwReporterInvocation::FLAG_LOG_TO_RAPPOR |
-                  SwReporterInvocation::FLAG_LOG_EXIT_CODE_TO_PREFS |
-                  SwReporterInvocation::FLAG_TRIGGER_PROMPT |
-                  SwReporterInvocation::FLAG_SEND_REPORTER_LOGS,
-              invocation.flags);
+    EXPECT_EQ(SwReporterInvocation::BEHAVIOUR_LOG_TO_RAPPOR |
+                  SwReporterInvocation::BEHAVIOUR_LOG_EXIT_CODE_TO_PREFS |
+                  SwReporterInvocation::BEHAVIOUR_TRIGGER_PROMPT |
+                  SwReporterInvocation::BEHAVIOUR_ALLOW_SEND_REPORTER_LOGS,
+              invocation.supported_behaviours);
   }
 
   // |ComponentReady| asserts that it is run on the UI thread, so we must
@@ -189,7 +189,7 @@ class ExperimentalSwReporterInstallerTest : public SwReporterInstallerTest {
                 invocation.command_line.GetArgs()[0]);
     }
 
-    EXPECT_EQ(0U, invocation.flags);
+    EXPECT_EQ(0U, invocation.supported_behaviours);
     histograms_.ExpectTotalCount(kErrorHistogramName, 0);
   }
 
@@ -302,7 +302,7 @@ TEST_F(ExperimentalSwReporterInstallerTest, SingleInvocation) {
   ASSERT_EQ(1U, invocation.command_line.GetArgs().size());
   EXPECT_EQ(L"random argument", invocation.command_line.GetArgs()[0]);
   EXPECT_EQ("TestSuffix", invocation.suffix);
-  EXPECT_EQ(0U, invocation.flags);
+  EXPECT_EQ(0U, invocation.supported_behaviours);
   histograms_.ExpectTotalCount(kErrorHistogramName, 0);
 }
 
@@ -349,7 +349,7 @@ TEST_F(ExperimentalSwReporterInstallerTest, MultipleInvocations) {
     ASSERT_EQ(1U, invocation.command_line.GetArgs().size());
     EXPECT_EQ(L"random argument", invocation.command_line.GetArgs()[0]);
     EXPECT_EQ("TestSuffix", invocation.suffix);
-    EXPECT_EQ(0U, invocation.flags);
+    EXPECT_EQ(0U, invocation.supported_behaviours);
   }
 
   {
@@ -363,7 +363,8 @@ TEST_F(ExperimentalSwReporterInstallerTest, MultipleInvocations) {
                                   kRegistrySuffixSwitch));
     ASSERT_TRUE(invocation.command_line.GetArgs().empty());
     EXPECT_EQ("SecondSuffix", invocation.suffix);
-    EXPECT_EQ(SwReporterInvocation::FLAG_TRIGGER_PROMPT, invocation.flags);
+    EXPECT_EQ(SwReporterInvocation::BEHAVIOUR_TRIGGER_PROMPT,
+              invocation.supported_behaviours);
   }
 
   {
@@ -378,7 +379,7 @@ TEST_F(ExperimentalSwReporterInstallerTest, MultipleInvocations) {
     ASSERT_TRUE(invocation.command_line.GetArgs().empty());
     EXPECT_EQ("ThirdSuffix", invocation.suffix);
     // A missing "prompt" key means "false".
-    EXPECT_EQ(0U, invocation.flags);
+    EXPECT_EQ(0U, invocation.supported_behaviours);
   }
 
   histograms_.ExpectTotalCount(kErrorHistogramName, 0);
