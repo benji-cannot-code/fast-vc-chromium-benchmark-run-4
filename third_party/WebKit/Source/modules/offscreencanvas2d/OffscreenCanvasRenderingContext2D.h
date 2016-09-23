e5b55e734c0ff7795000e40ad7d9b3678ad63d4b
xlai <xlai@chromium.org>
2016-09-23 16:33:21 -0700

    Implement OffscreenCanvas Accelerated 2D commit()
    This CL makes commit() on accelerated 2d context on OffscreenCanvas work.
    
    BUG=563852
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2359723003
    Cr-Commit-Position: refs/heads/master@{#420785}

--- third_party/WebKit/Source/modules/offscreencanvas2d/OffscreenCanvasRenderingContext2D.h
+++ third_party/WebKit/Source/modules/offscreencanvas2d/OffscreenCanvasRenderingContext2D.h
@@ -87,6 +87,8 @@ private:
     std::unique_ptr<ImageBuffer> m_imageBuffer;
 
     bool isPaintable() const final;
+
+    RefPtr<StaticBitmapImage> transferToStaticBitmapImage();
 };
 
 DEFINE_TYPE_CASTS(OffscreenCanvasRenderingContext2D, CanvasRenderingContext, context,
