c9da1ae66588b0f9d44abd6eec6e9bab3d38bdf0
khushalsagar <khushalsagar@chromium.org>
2016-09-23 16:53:55 -0700

    Revert of Move language pak files to assets. (patchset #13 id:240001 of https://codereview.chromium.org/2345143002/ )
    Reason for revert:
    Broke the internal build, https://uberchromegw.corp.google.com/i/internal.client.clank_tot/builders/instrumentation-mako-clankium-tot/builds/10008
    
    Original issue's description:
    > Move language pak files to assets.
    >
    > Language pak files were moved to res/raw when split apks was being implemented.
    > Moving them to assets with a java constant keeping track of pak file locations
    > will improve lookup time and save some space (no more xml files and
    > resources.arsc).
    >
    > BUG=529604
    >
    > Committed: https://crrev.com/34ae27889dd8c8ffd5f5ca8c7bc10c17e57c12e2
    > Cr-Commit-Position: refs/heads/master@{#420641}
    
    TBR=agrieve@chromium.org,michaelbai@chromium.org,nyquist@chromium.org,tedchoc@chromium.org,estevenson@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=529604
    
    Review-Url: https://codereview.chromium.org/2369683002
    Cr-Commit-Position: refs/heads/master@{#420791}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
@@ -49,6 +49,7 @@ import org.chromium.android_webview.AwResource;
 import org.chromium.android_webview.AwServiceWorkerController;
 import org.chromium.android_webview.AwSettings;
 import org.chromium.android_webview.HttpAuthDatabase;
+import org.chromium.android_webview.R;
 import org.chromium.android_webview.ResourcesContextWrapperFactory;
 import org.chromium.base.BuildConfig;
 import org.chromium.base.CommandLine;
@@ -64,7 +65,6 @@ import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.base.library_loader.ProcessInitException;
 import org.chromium.content.browser.ContentViewStatics;
 import org.chromium.net.NetworkChangeNotifier;
-import org.chromium.ui.LocalePakFiles;
 import org.chromium.ui.base.ResourceBundle;
 
 import java.io.File;
@@ -394,7 +394,7 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
         // Make sure that ResourceProvider is initialized before starting the browser process.
         final String webViewPackageName = WebViewFactory.getLoadedPackageInfo().packageName;
         setUpResources(webViewPackageName, context);
-        ResourceBundle.initializeLocalePaks(LocalePakFiles.getFiles());
+        ResourceBundle.initializeLocalePaks(context, R.array.locale_paks);
         initPlatSupportLibrary();
         initNetworkChangeNotifier(context);
         final int extraBindFlags = Context.BIND_EXTERNAL_SERVICE;
