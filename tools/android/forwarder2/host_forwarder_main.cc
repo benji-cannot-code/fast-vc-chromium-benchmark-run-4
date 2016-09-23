110d53e2195dca915df940355eba70c9e34c11f3
jbudorick <jbudorick@chromium.org>
2016-09-22 19:39:22 -0700

    [Android] Switch adb_reverse_forwarder to argparse.
    Also, add newlines to host forwarder error logging.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2364733003
    Cr-Commit-Position: refs/heads/master@{#420552}

--- tools/android/forwarder2/host_forwarder_main.cc
+++ tools/android/forwarder2/host_forwarder_main.cc
@@ -162,7 +162,7 @@ class HostControllersManager {
     if (adb_port < 0) {
       SendMessage(
           "ERROR: could not get adb port for device. You might need to add "
-          "'adb' to your PATH or provide the device serial id.",
+          "'adb' to your PATH or provide the device serial id.\n",
           client_socket.get());
       return;
     }
@@ -173,7 +173,7 @@ class HostControllersManager {
       const bool controller_did_exist = DeleteRefCountedValueInMap(
           controller_key, controllers_.get());
       if (!controller_did_exist) {
-        SendMessage("ERROR: could not unmap port.", client_socket.get());
+        SendMessage("ERROR: could not unmap port.\n", client_socket.get());
         LogExistingControllers(client_socket);
       } else {
         SendMessage("OK", client_socket.get());
@@ -183,7 +183,7 @@ class HostControllersManager {
       return;
     }
     if (host_port < 0) {
-      SendMessage("ERROR: missing host port", client_socket.get());
+      SendMessage("ERROR: missing host port\n", client_socket.get());
       return;
     }
     const bool use_dynamic_port_allocation = device_port == 0;
@@ -205,7 +205,7 @@ class HostControllersManager {
                    weak_ptr_factory_.GetWeakPtr())));
     if (!host_controller.get()) {
       has_failed_ = true;
-      SendMessage("ERROR: Connection to device failed.", client_socket.get());
+      SendMessage("ERROR: Connection to device failed.\n", client_socket.get());
       LogExistingControllers(client_socket);
       return;
     }
@@ -223,9 +223,9 @@ class HostControllersManager {
   }
 
   void LogExistingControllers(const std::unique_ptr<Socket>& client_socket) {
-    SendMessage("ERROR: Existing controllers:", client_socket.get());
+    SendMessage("ERROR: Existing controllers:\n", client_socket.get());
     for (const auto& controller : *controllers_) {
-      SendMessage(base::StringPrintf("ERROR:   %s", controller.first.c_str()),
+      SendMessage(base::StringPrintf("ERROR: %s\n", controller.first.c_str()),
           client_socket.get());
     }
   }
@@ -351,7 +351,7 @@ class ServerDelegate : public Daemon::ServerDelegate {
     CHECK(pickle_it.ReadString(&device_serial));
     int device_port;
     if (!pickle_it.ReadInt(&device_port)) {
-      client_socket->WriteString("ERROR: missing device port");
+      client_socket->WriteString("ERROR: missing device port\n");
       return;
     }
     int host_port;
