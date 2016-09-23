14b551eb241596bb443d484bb001ea4ff1013797
ellyjones <ellyjones@chromium.org>
2016-09-23 12:01:28 -0700

    cocoa browser: remove non-material omnibox support
    This is part 3 of removing non-material top chrome support.
    
    BUG=649368
    
    Review-Url: https://codereview.chromium.org/2364933002
    Cr-Commit-Position: refs/heads/master@{#420684}

--- chrome/browser/ui/cocoa/omnibox/omnibox_view_mac.mm
+++ chrome/browser/ui/cocoa/omnibox/omnibox_view_mac.mm
@@ -81,7 +81,6 @@ using content::WebContents;
 namespace {
 const int kOmniboxLargeFontSizeDelta = 9;
 const int kOmniboxNormalFontSizeDelta = 1;
-const int kOmniboxSmallFontSizeDelta = 0;
 const int kOmniboxSmallMaterialFontSizeDelta = -1;
 
 // TODO(shess): This is ugly, find a better way.  Using it right now
@@ -98,15 +97,9 @@ NSColor* ColorWithRGBBytes(int rr, int gg, int bb) {
 }
 
 NSColor* HostTextColor(bool in_dark_mode) {
-  if (!ui::MaterialDesignController::IsModeMaterial()) {
-    return [NSColor blackColor];
-  }
   return in_dark_mode ? [NSColor whiteColor] : [NSColor blackColor];
 }
 NSColor* SecureSchemeColor(bool in_dark_mode) {
-  if (!ui::MaterialDesignController::IsModeMaterial()) {
-    return ColorWithRGBBytes(0x07, 0x95, 0x00);
-  }
   return in_dark_mode ? skia::SkColorToSRGBNSColor(SK_ColorWHITE)
                       : skia::SkColorToSRGBNSColor(gfx::kGoogleGreen700);
 }
@@ -116,9 +109,6 @@ NSColor* SecurityWarningSchemeColor(bool in_dark_mode) {
       : skia::SkColorToSRGBNSColor(gfx::kGoogleYellow700);
 }
 NSColor* SecurityErrorSchemeColor(bool in_dark_mode) {
-  if (!ui::MaterialDesignController::IsModeMaterial()) {
-    return ColorWithRGBBytes(0xa2, 0x00, 0x00);
-  }
   return in_dark_mode
       ? skia::SkColorToSRGBNSColor(SkColorSetA(SK_ColorWHITE, 0x7F))
       : skia::SkColorToSRGBNSColor(gfx::kGoogleRed700);
@@ -181,9 +171,6 @@ SkColor OmniboxViewMac::BaseTextColorSkia(bool in_dark_mode) {
 
 // static
 NSColor* OmniboxViewMac::BaseTextColor(bool in_dark_mode) {
-  if (!ui::MaterialDesignController::IsModeMaterial()) {
-    return [NSColor darkGrayColor];
-  }
   return skia::SkColorToSRGBNSColor(BaseTextColorSkia(in_dark_mode));
 }
 
@@ -586,11 +573,9 @@ void OmniboxViewMac::ApplyTextAttributes(
                            value:@"en_US_POSIX"
                            range:as_entire_string];
 
-  if (ui::MaterialDesignController::IsModeMaterial()) {
-    [attributedString addAttribute:NSForegroundColorAttributeName
-                             value:HostTextColor(in_dark_mode)
-                             range:as_entire_string];
-  }
+  [attributedString addAttribute:NSForegroundColorAttributeName
+                           value:HostTextColor(in_dark_mode)
+                           range:as_entire_string];
 
   url::Component scheme, host;
   AutocompleteInput::ParseForEmphasizeComponents(
@@ -1101,15 +1086,7 @@ NSFont* OmniboxViewMac::GetLargeFont() {
 }
 
 NSFont* OmniboxViewMac::GetSmallFont() {
-  ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
-  if (!ui::MaterialDesignController::IsModeMaterial()) {
-    return rb
-        .GetFontWithDelta(kOmniboxSmallFontSizeDelta, gfx::Font::NORMAL,
-                          gfx::Font::Weight::NORMAL)
-        .GetNativeFont();
-  }
-
-  return rb
+  return ui::ResourceBundle::GetSharedInstance()
       .GetFontWithDelta(kOmniboxSmallMaterialFontSizeDelta, gfx::Font::NORMAL,
                         gfx::Font::Weight::NORMAL)
       .GetNativeFont();
