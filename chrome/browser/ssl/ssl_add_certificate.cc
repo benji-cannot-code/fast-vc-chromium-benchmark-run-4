d8e48436a8a9fc82979f4e673ee404655213f8d0
estade <estade@chromium.org>
2016-09-23 15:39:05 -0700

    Always use vector icons for desktop infobars (when one is defined), rather than caring about the (deprecated) material design mode.
    Make IDR_INFOBAR_SAVE_PASSWORD android-only.
    
    BUG=648281
    TBR=oshima@chromium.org
    
    Review-Url: https://codereview.chromium.org/2361843003
    Cr-Commit-Position: refs/heads/master@{#420748}

--- chrome/browser/ssl/ssl_add_certificate.cc
+++ chrome/browser/ssl/ssl_add_certificate.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/certificate_viewer.h"
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/grit/generated_resources.h"
-#include "chrome/grit/theme_resources.h"
 #include "components/infobars/core/confirm_infobar_delegate.h"
 #include "components/infobars/core/infobar.h"
 #include "components/infobars/core/infobar_delegate.h"
@@ -48,7 +47,6 @@ class SSLAddCertificateInfoBarDelegate : public ConfirmInfoBarDelegate {
   // ConfirmInfoBarDelegate:
   Type GetInfoBarType() const override;
   infobars::InfoBarDelegate::InfoBarIdentifier GetIdentifier() const override;
-  int GetIconId() const override;
   gfx::VectorIconId GetVectorIconId() const override;
   base::string16 GetMessageText() const override;
   int GetButtons() const override;
@@ -87,17 +85,9 @@ SSLAddCertificateInfoBarDelegate::GetIdentifier() const {
   return SSL_ADD_CERTIFICATE_INFOBAR_DELEGATE;
 }
 
-int SSLAddCertificateInfoBarDelegate::GetIconId() const {
-  // TODO(davidben): Use a more appropriate icon.
-  return IDR_INFOBAR_SAVE_PASSWORD;
-}
-
 gfx::VectorIconId SSLAddCertificateInfoBarDelegate::GetVectorIconId() const {
-#if !defined(OS_MACOSX)
+  // TODO(davidben): Use a more appropriate icon.
   return gfx::VectorIconId::AUTOLOGIN;
-#else
-  return gfx::VectorIconId::VECTOR_ICON_NONE;
-#endif
 }
 
 base::string16 SSLAddCertificateInfoBarDelegate::GetMessageText() const {
@@ -141,12 +131,8 @@ void ShowErrorInfoBar(int message_id,
   SimpleAlertInfoBarDelegate::Create(
       InfoBarService::FromWebContents(web_contents),
       infobars::InfoBarDelegate::SSL_ADD_CERTIFICATE,
-      IDR_INFOBAR_SAVE_PASSWORD,
-#if !defined(OS_MACOSX)
+      0,
       gfx::VectorIconId::AUTOLOGIN,
-#else
-      gfx::VectorIconId::VECTOR_ICON_NONE,
-#endif
       l10n_util::GetStringFUTF16(
           IDS_ADD_CERT_ERR_INVALID_CERT, base::IntToString16(-cert_error),
           base::ASCIIToUTF16(net::ErrorToString(cert_error))),
