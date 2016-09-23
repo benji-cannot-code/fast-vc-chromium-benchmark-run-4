aff115e06912c8b458c8c241a7db64e854281240
jbudorick <jbudorick@chromium.org>
2016-09-23 12:43:03 -0700

    [Android] Add device serial to all host_controller log messages.
    BUG=634052
    
    Review-Url: https://codereview.chromium.org/2366763003
    Cr-Commit-Position: refs/heads/master@{#420699}

--- tools/android/forwarder2/host_controller.h
+++ tools/android/forwarder2/host_controller.h
@@ -40,6 +40,7 @@ class HostController {
   // If |device_port| is zero then a dynamic port is allocated (and retrievable
   // through device_port() below).
   static std::unique_ptr<HostController> Create(
+      const std::string& device_serial,
       int device_port,
       int host_port,
       int adb_port,
@@ -56,7 +57,8 @@ class HostController {
   int device_port() const { return device_port_; }
 
  private:
-  HostController(int device_port,
+  HostController(const std::string& device_serial,
+                 int device_port,
                  int host_port,
                  int adb_port,
                  const ErrorCallback& error_callback,
@@ -74,6 +76,7 @@ class HostController {
   void UnmapPortOnDevice();
 
   SelfDeleterHelper<HostController> self_deleter_helper_;
+  const std::string device_serial_;
   const int device_port_;
   const int host_port_;
   const int adb_port_;
