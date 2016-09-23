8159225a5e37f29fd6ad9374056b2c60d989ca75
gcasto <gcasto@chromium.org>
2016-09-23 16:16:05 -0700

    Revert of Handling new frames and frame navigations with find-in-page during a find session. (patchset #2 id:120001 of https://codereview.chromium.org/2236403004/ )
    Reason for revert:
    This seems to be failing pretty consistently on some bots (e.g. https://uberchromegw.corp.google.com/i/chromium.win/builders/Win7%20Tests%20%28dbg%29%281%29)
    
    Original issue's description:
    > Handling new frames and frame navigations with find-in-page during a find session.
    >
    > This patch implements new find-in-page functionality that allows for frames that are either newly added or navigated during a find session to be automatically searched so that matches in their new content are automatically highlighted and included in the results shown in the find bar.
    >
    > Also, there is a fix included in this patch to prevent the find-in-page bar from closing when a subframe navigates (the find session should only end if the main frame navigates).
    >
    > Design doc: https://docs.google.com/document/d/1r4F19FIKg4zPJCaSyJ9-T0sgFbxlCGKL3FjqQEAKegg/view
    >
    > BUG=2220,621660
    >
    > Committed: https://crrev.com/ec9c7f30eb10ab3fc9ab6707f7b736446ab89aa1
    > Cr-Commit-Position: refs/heads/master@{#420715}
    
    TBR=dcheng@chromium.org,pkasting@chromium.org,kenrb@chromium.org,paulmeyer@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=2220,621660
    
    Review-Url: https://codereview.chromium.org/2363993003
    Cr-Commit-Position: refs/heads/master@{#420774}

--- third_party/WebKit/Source/web/TextFinder.cpp
+++ third_party/WebKit/Source/web/TextFinder.cpp
@@ -201,6 +201,7 @@ bool TextFinder::find(int identifier, const WebString& searchText, const WebFind
         reportFindInPageSelection(selectionRect, m_activeMatchIndex + 1, identifier);
     }
 
+    m_lastFindRequestCompletedWithNoMatches = false;
     return true;
 }
 
@@ -208,7 +209,6 @@ void TextFinder::clearActiveFindMatch()
 {
     m_currentActiveMatchFrame = false;
     setMarkerActive(m_activeMatch.get(), false);
-    resetActiveMatch();
 }
 
 void TextFinder::stopFindingAndClearSelection()
@@ -286,7 +286,7 @@ void TextFinder::scopeStringMatches(int identifier, const WebString& searchText,
         return;
     }
 
-    if (!shouldScopeMatches(searchText, options)) {
+    if (!shouldScopeMatches(searchText)) {
         finishCurrentScopingEffort(identifier);
         return;
     }
@@ -674,22 +674,16 @@ void TextFinder::unmarkAllTextMatches()
         frame->document()->markers().removeMarkers(DocumentMarker::TextMatch);
 }
 
-bool TextFinder::shouldScopeMatches(const String& searchText, const WebFindOptions& options)
+bool TextFinder::shouldScopeMatches(const String& searchText)
 {
     // Don't scope if we can't find a frame or a view.
     // The user may have closed the tab/application, so abort.
     LocalFrame* frame = ownerFrame().frame();
-    if (!frame || !frame->view() || !frame->page())
+    if (!frame || !frame->view() || !frame->page() || !ownerFrame().hasVisibleContent())
         return false;
 
     DCHECK(frame->document());
     DCHECK(frame->view());
-
-    if (options.force)
-        return true;
-
-    if (!ownerFrame().hasVisibleContent())
-        return false;
 
     // If the frame completed the scoping operation and found 0 matches the last
     // time it was searched, then we don't have to search it again if the user is
