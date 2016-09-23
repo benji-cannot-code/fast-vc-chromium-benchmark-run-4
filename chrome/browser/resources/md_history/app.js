daa403bf56c50417d53f98b8befa38d2efe3a5cb
tsergeant <tsergeant@chromium.org>
2016-09-22 19:35:48 -0700

    MD History: Lazily load element files which are not needed for first paint
    This splits out many invisible or heavy elements into a separate
    lazy_load.html file, which is only imported after the main app has
    rendered. Where necessary, simple CSS shims are added to prevent visible
    changes to the page.
    
    In effect, this splits apart the first meaningful paint from the page
    being fully interactive, and in the process improves first meaningful
    paint by 10-15%.
    
    BUG=629710
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2264983002
    Cr-Commit-Position: refs/heads/master@{#420550}

--- chrome/browser/resources/md_history/app.js
+++ chrome/browser/resources/md_history/app.js
@@ -3,6 +3,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+cr.define('md_history', function() {
+  var lazyLoadPromise = null;
+  function ensureLazyLoaded() {
+    if (!lazyLoadPromise) {
+      lazyLoadPromise = new Promise(function(resolve, reject) {
+        Polymer.Base.importHref(
+            'chrome://history/lazy_load.html', resolve, reject, true);
+      });
+    }
+    return lazyLoadPromise;
+  }
+
+  return {
+    ensureLazyLoaded: ensureLazyLoaded,
+  };
+});
+
 Polymer({
   is: 'history-app',
 
@@ -125,6 +142,9 @@ Polymer({
     if (!this.hasDrawer_) {
       this.focusToolbarSearchField();
     }
+
+    // Lazily load the remainder of the UI.
+    md_history.ensureLazyLoaded();
   },
 
   /** Overridden from IronScrollTargetBehavior */
