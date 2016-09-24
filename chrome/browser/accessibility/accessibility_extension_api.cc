59f1ec5caa226d643905888185bd0880f246e61f
nektar <nektar@chromium.org>
2016-09-23 18:24:59 -0700

    Get rid of AX_LINE_BREAKS attribute to improve performance and clarity.
    1. Attribute renamed to line_start_offsets.
    2. Now computed on browser side and cached.
    3. Attribute should not be used directly but line start offsets should be retrieved via |AXNode::GetOrComputeLineStartOffsets|.
    4. Should work on all kinds of text fields and both in automation and other platforms.
    TESTED=Manually with Voiceover ChromeVox Jaws and NVDA, existing Windows browser tests, new Automation API tests, existing ChromeVox tests
    R=dmazzoni@chromium.org, dtseng@chromium.org
    BUG=642337
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2301833005
    Cr-Commit-Position: refs/heads/master@{#420807}

--- chrome/browser/accessibility/accessibility_extension_api.cc
+++ chrome/browser/accessibility/accessibility_extension_api.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
 #include "build/build_config.h"
-#include "chrome/browser/accessibility/accessibility_extension_api.h"
 #include "chrome/browser/extensions/api/tabs/tabs_constants.h"
 #include "chrome/browser/extensions/chrome_extension_function_details.h"
 #include "chrome/browser/extensions/extension_service.h"
--- content/browser/accessibility/browser_accessibility.cc
+++ content/browser/accessibility/browser_accessibility.cc
@@ -230,7 +230,7 @@ bool BrowserAccessibility::IsNextSiblingOnSameLine() const {
     const BrowserAccessibility* next_on_line =
         manager()->GetFromID(next_on_line_id);
     // In the case of a static text sibling, the object designated to be the
-    // next object on this line might be one of its children, i.e. the last
+    // next object on this line might be one of its children, i.e. the first
     // inline text box.
     return next_on_line && next_on_line->IsDescendantOf(next_sibling);
   }
@@ -534,9 +534,7 @@ int BrowserAccessibility::GetLineStartBoundary(
   DCHECK_LE(start, static_cast<int>(GetText().length()));
 
   if (IsSimpleTextControl()) {
-    const std::vector<int32_t>& line_breaks =
-        GetIntListAttribute(ui::AX_ATTR_LINE_BREAKS);
-    return ui::FindAccessibleTextBoundary(GetText(), line_breaks,
+    return ui::FindAccessibleTextBoundary(GetText(), GetLineStartOffsets(),
                                           ui::LINE_BOUNDARY, start, direction,
                                           affinity);
   }
@@ -1141,6 +1139,12 @@ std::string BrowserAccessibility::ComputeAccessibleNameFromDescendants() {
   }
 
   return name;
+}
+
+std::vector<int> BrowserAccessibility::GetLineStartOffsets() const {
+  if (!instance_active())
+    return std::vector<int>();
+  return node()->GetOrComputeLineStartOffsets();
 }
 
 base::string16 BrowserAccessibility::GetInnerText() const {
