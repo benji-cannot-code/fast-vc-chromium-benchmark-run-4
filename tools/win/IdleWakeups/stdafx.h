61b24cbb9f89375e064d9e40682bef7982d6877f
stanisc <stanisc@chromium.org>
2016-09-23 18:37:55 -0700

    IdleWakeups tool
    IdleWakeups is a Windows tool that is designed to measure and
    aggregate a few key performance metrics for all specified
    browser processes over time. The list of metrics includes:
    - Number of running processes
    - CPU usage (normalized to one core - 100% meaning one core being
      fully utilized)
    - Number of context switches (idle wakeups)
    - Private Working Set
    - System wide CPU power usage (as reported by Intel Power driver)
    
    The tool supports other browsers (such as Edge or Firefox) based
    on partial image name filter (chrome.exe is default)
    
    This was used for browsers efficiency comparison - chrome vs. Edge and
    Firefox.
    
    Also the tool was used as a basis for prototyping and implementing new
    OS_WIN features in Chrome Task Manager including:
    - Idle Wakeups column.
    - More power/memory efficient implementation of Memory column.
    
    Sample usage:
    - "IdleWakeups.exe" to match all Chrome.exe processes.
    - "IdleWakeups.exe" firefox to match Firefox process.
    - "IdleWakeups.exe MicrosoftEdge" to match all Edge processes.
    
    When the tool starts it begins gathering and aggregating all metrics
    for all matching processes and print results every 2 seconds.
    
    Hit Ctrl+C to stop the measurements and print average and median
    values over the entire measurement interval.
    
    Review-Url: https://codereview.chromium.org/2356753004
    Cr-Commit-Position: refs/heads/master@{#420811}

---
+++ tools/win/IdleWakeups/stdafx.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TOOLS_WIN_IDLEWAKEUPS_STDAFX_H_
+#define TOOLS_WIN_IDLEWAKEUPS_STDAFX_H_
+
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+
+#endif  // TOOLS_WIN_IDLEWAKEUPS_STDAFX_H_
