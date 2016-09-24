a04a6fb58bbb05116602bd592b1524f9e53d8185
prashant.n <prashant.n@samsung.com>
2016-09-23 20:38:20 -0700

    cc: Detach spiral iterator implementation to separate file.
    Currently SpiralDifferenceIterator implements logic based on directions
    around the center rect. This logic does not take care of aspect ratios
    of tile size. To consider the aspect ratios during iteration new logic
    has to be implanted. Until the new logic gets tested thoroughly we
    need the current logic. This patch detaches the current spiral iterator
    to separate classes (SpiralIterator and ReverseSpiralIterator) and
    moves them to their own files making SpiralDifferenceIterator thinner.
    Now new logic can be added to SpiralDifferenceIterator based on some
    runtime switch.
    
    This patch also separates out unit tests related to SpiralIterator from
    tiling_data_unittest.cc to its own file.
    
    BUG=613695
    TESTS=TilingDataSpiralIteratorTest.*
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2352393002
    Cr-Commit-Position: refs/heads/master@{#420823}

--- cc/base/tiling_data.h
+++ cc/base/tiling_data.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "cc/base/cc_export.h"
 #include "cc/base/index_rect.h"
+#include "cc/base/reverse_spiral_iterator.h"
+#include "cc/base/spiral_iterator.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
 
@@ -53,6 +55,8 @@ class CC_EXPORT TilingData {
   // Return the highest tile index whose border texels include src_position.
   int LastBorderTileXIndexFromSrcCoord(int src_position) const;
   int LastBorderTileYIndexFromSrcCoord(int src_position) const;
+  // Return the tile indices around the given rect.
+  IndexRect TileAroundIndexRect(const gfx::Rect& center_rect) const;
 
   gfx::Rect ExpandRectIgnoringBordersToTileBounds(const gfx::Rect& rect) const;
   gfx::Rect ExpandRectToTileBounds(const gfx::Rect& rect) const;
@@ -140,22 +144,7 @@ class CC_EXPORT TilingData {
     SpiralDifferenceIterator& operator++();
 
    private:
-    int current_step_count() const {
-      return (direction_ == UP || direction_ == DOWN) ? vertical_step_count_
-                                                      : horizontal_step_count_;
-    }
-
-    bool needs_direction_switch() const;
-    void switch_direction();
-
-    enum Direction { UP, LEFT, DOWN, RIGHT };
-
-    Direction direction_;
-    int delta_x_;
-    int delta_y_;
-    int current_step_;
-    int horizontal_step_count_;
-    int vertical_step_count_;
+    SpiralIterator spiral_iterator_;
   };
 
   class CC_EXPORT ReverseSpiralDifferenceIterator
@@ -169,24 +158,7 @@ class CC_EXPORT TilingData {
     ReverseSpiralDifferenceIterator& operator++();
 
    private:
-    int current_step_count() const {
-      return (direction_ == UP || direction_ == DOWN) ? vertical_step_count_
-                                                      : horizontal_step_count_;
-    }
-
-    bool needs_direction_switch() const;
-    void switch_direction();
-
-    IndexRect around_index_rect_;
-
-    enum Direction { LEFT, UP, RIGHT, DOWN };
-
-    Direction direction_;
-    int delta_x_;
-    int delta_y_;
-    int current_step_;
-    int horizontal_step_count_;
-    int vertical_step_count_;
+    ReverseSpiralIterator reverse_spiral_iterator_;
   };
 
  private:
