11ef9d0d434f45521f17ee4dc006ef1a4c5d3673
fdoray <fdoray@chromium.org>
2016-09-26 09:18:58 -0700

    TaskScheduler: Add FlushForTesting().
    Many browser tests depend on being able to wait until there are no
    pending undelayed tasks in the blocking pool (via
    content::RunAllBlockingPoolTasksUntilIdle). This CL brings this
    functionality to TaskScheduler.
    
    BUG=622400, 553459
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_chromium_tsan_rel_ng
    
    Review-Url: https://codereview.chromium.org/2362253002
    Cr-Commit-Position: refs/heads/master@{#420901}

--- base/task_scheduler/scheduler_worker_unittest.cc
+++ base/task_scheduler/scheduler_worker_unittest.cc
@@ -353,11 +353,14 @@ namespace {
 
 class ControllableDetachDelegate : public SchedulerWorkerDefaultDelegate {
  public:
-  ControllableDetachDelegate()
-      : work_processed_(WaitableEvent::ResetPolicy::MANUAL,
+  ControllableDetachDelegate(TaskTracker* task_tracker)
+      : task_tracker_(task_tracker),
+        work_processed_(WaitableEvent::ResetPolicy::MANUAL,
                         WaitableEvent::InitialState::NOT_SIGNALED),
         detach_requested_(WaitableEvent::ResetPolicy::MANUAL,
-                          WaitableEvent::InitialState::NOT_SIGNALED) {}
+                          WaitableEvent::InitialState::NOT_SIGNALED) {
+    EXPECT_TRUE(task_tracker_);
+  }
 
   ~ControllableDetachDelegate() override = default;
 
@@ -377,6 +380,7 @@ class ControllableDetachDelegate : public SchedulerWorkerDefaultDelegate {
     std::unique_ptr<Task> task(new Task(
         FROM_HERE, Bind(&WaitableEvent::Signal, Unretained(&work_processed_)),
         TaskTraits(), TimeDelta()));
+    EXPECT_TRUE(task_tracker_->WillPostTask(task.get()));
     sequence->PushTask(std::move(task));
     return sequence;
   }
@@ -405,6 +409,7 @@ class ControllableDetachDelegate : public SchedulerWorkerDefaultDelegate {
   void set_can_detach(bool can_detach) { can_detach_ = can_detach; }
 
  private:
+  TaskTracker* const task_tracker_;
   bool work_requested_ = false;
   bool can_detach_ = false;
   WaitableEvent work_processed_;
@@ -419,7 +424,7 @@ TEST(TaskSchedulerWorkerTest, WorkerDetaches) {
   TaskTracker task_tracker;
   // Will be owned by SchedulerWorker.
   ControllableDetachDelegate* delegate =
-      new StrictMock<ControllableDetachDelegate>;
+      new StrictMock<ControllableDetachDelegate>(&task_tracker);
   delegate->set_can_detach(true);
   EXPECT_CALL(*delegate, OnMainEntry(_, TimeDelta::Max()));
   std::unique_ptr<SchedulerWorker> worker =
@@ -439,7 +444,7 @@ TEST(TaskSchedulerWorkerTest, WorkerDetachesAndWakes) {
   TaskTracker task_tracker;
   // Will be owned by SchedulerWorker.
   ControllableDetachDelegate* delegate =
-      new StrictMock<ControllableDetachDelegate>;
+      new StrictMock<ControllableDetachDelegate>(&task_tracker);
   delegate->set_can_detach(true);
   EXPECT_CALL(*delegate, OnMainEntry(_, TimeDelta::Max()));
   std::unique_ptr<SchedulerWorker> worker =
@@ -472,7 +477,7 @@ TEST(TaskSchedulerWorkerTest, CreateDetached) {
   TaskTracker task_tracker;
   // Will be owned by SchedulerWorker.
   ControllableDetachDelegate* delegate =
-      new StrictMock<ControllableDetachDelegate>;
+      new StrictMock<ControllableDetachDelegate>(&task_tracker);
   std::unique_ptr<SchedulerWorker> worker =
       SchedulerWorker::Create(
           ThreadPriority::NORMAL, WrapUnique(delegate), &task_tracker,
--- base/task_scheduler/task_tracker.h
+++ base/task_scheduler/task_tracker.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/atomicops.h"
 #include "base/base_export.h"
 #include "base/callback_forward.h"
 #include "base/macros.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
+class ConditionVariable;
 class SequenceToken;
 
 namespace internal {
@@ -40,6 +42,13 @@ class BASE_EXPORT TaskTracker {
   // CONTINUE_ON_SHUTDOWN tasks still may be running after Shutdown returns.
   // This can only be called once.
   void Shutdown();
+
+  // Waits until there are no pending undelayed tasks. May be called in tests
+  // to validate that a condition is met after all undelayed tasks have run.
+  //
+  // Does not wait for delayed tasks. Waits for undelayed tasks posted from
+  // other threads during the call. Returns immediately when shutdown completes.
+  void Flush();
 
   // Informs this TaskTracker that |task| is about to be posted. Returns true if
   // this operation is allowed (|task| should be posted if-and-only-if it is).
@@ -68,6 +77,8 @@ class BASE_EXPORT TaskTracker {
  private:
   class State;
 
+  void PerformShutdown();
+
   // Called before WillPostTask() informs the tracing system that a task has
   // been posted. Updates |num_tasks_blocking_shutdown_| if necessary and
   // returns true if the current shutdown state allows the task to be posted.
@@ -87,9 +98,29 @@ class BASE_EXPORT TaskTracker {
   // shutdown has started.
   void OnBlockingShutdownTasksComplete();
 
+  // Decrements the number of pending undelayed tasks and signals |flush_cv_| if
+  // it reaches zero.
+  void DecrementNumPendingUndelayedTasks();
+
   // Number of tasks blocking shutdown and boolean indicating whether shutdown
   // has started.
   const std::unique_ptr<State> state_;
+
+  // Number of undelayed tasks that haven't completed their execution. Is
+  // incremented and decremented without a barrier. When it reaches zero,
+  // |flush_lock_| is acquired (forcing memory synchronization) and |flush_cv_|
+  // is signaled.
+  subtle::Atomic32 num_pending_undelayed_tasks_ = 0;
+
+  // Lock associated with |flush_cv_|. Partially synchronizes access to
+  // |num_pending_undelayed_tasks_|. Full synchronization isn't needed because
+  // it's atomic, but synchronization is needed to coordinate waking and
+  // sleeping at the right time.
+  mutable SchedulerLock flush_lock_;
+
+  // Signaled when |num_pending_undelayed_tasks_| is zero or when shutdown
+  // completes.
+  const std::unique_ptr<ConditionVariable> flush_cv_;
 
   // Synchronizes access to shutdown related members below.
   mutable SchedulerLock shutdown_lock_;
