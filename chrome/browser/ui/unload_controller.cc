e2784ee20bb3febd8fa26e80d45d02ba0e1c7430
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-09-26 15:39:01 -0700

    [Extensions] Don't run unload listeners when an extension is removed
    When an extension is removed, we close all the tabs for that extension*.
    If any of these tabs had unload or beforeunload listeners, this close
    process is asynchronous. However, if the listener performs any network
    request, this would result in Chrome killing the tab for having an
    invalid origin (the extension has been removed).
    
    Instead, don't run these listeners at all. Since the extension has been
    removed, there's no reason the code in the tab should run - the
    extension should not be able to continue acting.
    
    *a tab for an extension refers to a tab pointing to a resource of that
    extension, like chrome-extension://<id>/page.html.
    
    Note: This does not address the case of terminated extensions, where we
    don't currently close the extension tabs, or the case of embedded
    iframes in non-extension pages.
    
    BUG=531378
    
    Review-Url: https://codereview.chromium.org/2355183002
    Cr-Commit-Position: refs/heads/master@{#421024}

--- chrome/browser/ui/unload_controller.cc
+++ chrome/browser/ui/unload_controller.cc
@@ -19,6 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 
+#if defined (ENABLE_EXTENSIONS)
+#include "extensions/browser/extension_registry.h"
+#include "extensions/common/constants.h"
+#endif  // (ENABLE_EXTENSIONS)
+
 namespace chrome {
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -52,6 +57,19 @@ bool UnloadController::ShouldRunUnloadEventsHelper(
 }
 
 bool UnloadController::RunUnloadEventsHelper(content::WebContents* contents) {
+#if defined (ENABLE_EXTENSIONS)
+  // Don't run for extensions that are disabled or uninstalled; the tabs will
+  // be killed if they make any network requests, and the extension shouldn't
+  // be doing any work if it's removed.
+  GURL url = contents->GetLastCommittedURL();
+  if (url.SchemeIs(extensions::kExtensionScheme) &&
+      !extensions::ExtensionRegistry::Get(browser_->profile())
+           ->enabled_extensions()
+           .GetExtensionOrAppByURL(url)) {
+    return false;
+  }
+#endif  // (ENABLE_EXTENSIONS)
+
   // Special case for when we quit an application. The devtools window can
   // close if it's beforeunload event has already fired which will happen due
   // to the interception of it's content's beforeunload.
