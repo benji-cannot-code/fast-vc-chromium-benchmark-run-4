0da0d74e8278958229b33e8b9fff294da12bb255
kundaji <kundaji@chromium.org>
2016-09-26 16:50:22 -0700

    Set up DataUseWebContentsObservers for extensions and apps WebContents.
    Set up WebContentsObservers for the purpose of ascribing all data use
    in Chrome to their respective page load. All possible WebContents
    created in Chrome should have a DataUseWebContentsObservers including
    popups, background pages, etc generated by Apps and Extensions.
    
    BUG=645188
    
    Review-Url: https://codereview.chromium.org/2354133002
    Cr-Commit-Position: refs/heads/master@{#421041}

--- chrome/browser/data_use_measurement/data_use_web_contents_observer.h
+++ chrome/browser/data_use_measurement/data_use_web_contents_observer.h
@@ -28,10 +28,8 @@ class DataUseWebContentsObserver
     : public content::WebContentsObserver,
       public content::WebContentsUserData<DataUseWebContentsObserver> {
  public:
-  // Creates a DataUseWebContentsObserver for the given WebContents and
-  // ChromeDataUseAscriberService.
-  static void CreateForWebContents(content::WebContents* web_contents,
-                                   ChromeDataUseAscriberService* service);
+  // Creates a DataUseWebContentsObserver for the given WebContents.
+  static void CreateForWebContents(content::WebContents* web_contents);
 
   ~DataUseWebContentsObserver() override;
 
--- chrome/browser/extensions/chrome_extension_host_delegate.cc
+++ chrome/browser/extensions/chrome_extension_host_delegate.cc
@@ -5,7 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/chrome_extension_host_delegate.h"
 
+#include <memory>
+#include <string>
+
 #include "base/lazy_instance.h"
+#include "chrome/browser/data_use_measurement/data_use_web_contents_observer.h"
 #include "chrome/browser/extensions/chrome_extension_web_contents_observer.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
@@ -40,6 +44,8 @@ ChromeExtensionHostDelegate::~ChromeExtensionHostDelegate() {}
 void ChromeExtensionHostDelegate::OnExtensionHostCreated(
     content::WebContents* web_contents) {
   ChromeExtensionWebContentsObserver::CreateForWebContents(web_contents);
+  data_use_measurement::DataUseWebContentsObserver::CreateForWebContents(
+      web_contents);
   PrefsTabHelper::CreateForWebContents(web_contents);
 }
 
