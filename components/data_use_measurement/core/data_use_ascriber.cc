ed11afc9eeb4251007c8c27c925454bc071dec1d
kundaji <kundaji@chromium.org>
2016-09-26 10:54:28 -0700

    Propagate network delegate events to ChromeDataUseAscriber.
    ChromeDataUseAscriber ascribes all data use to logical entities
    such as page loads. Propagating pertinent network events to
    the ascriber provides it with the hooks to track data usage.
    The actual tracking will be done in a future cl.
    
    BUG=645188
    
    Review-Url: https://codereview.chromium.org/2354323002
    Cr-Commit-Position: refs/heads/master@{#420926}

---
+++ components/data_use_measurement/core/data_use_ascriber.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/data_use_measurement/core/data_use_ascriber.h"
+
+#include <utility>
+
+#include "base/memory/ptr_util.h"
+#include "components/data_use_measurement/core/data_use_network_delegate.h"
+
+namespace data_use_measurement {
+
+std::unique_ptr<net::NetworkDelegate> DataUseAscriber::CreateNetworkDelegate(
+    std::unique_ptr<net::NetworkDelegate> wrapped_network_delegate) {
+  return base::MakeUnique<data_use_measurement::DataUseNetworkDelegate>(
+      std::move(wrapped_network_delegate), this);
+}
+
+void DataUseAscriber::OnBeforeUrlRequest(net::URLRequest* request) {}
+
+void DataUseAscriber::OnBeforeRedirect(net::URLRequest* request,
+                                       const GURL& new_location) {}
+
+void DataUseAscriber::OnNetworkBytesSent(net::URLRequest* request,
+                                         int64_t bytes_sent) {}
+
+void DataUseAscriber::OnNetworkBytesReceived(net::URLRequest* request,
+                                             int64_t bytes_received) {}
+
+void DataUseAscriber::OnUrlRequestCompleted(net::URLRequest* request,
+                                            bool started) {}
+
+}  // namespace data_use_measurement
