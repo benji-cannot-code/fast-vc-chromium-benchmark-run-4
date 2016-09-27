fde4d03b7a3ad1ac54c22993f934681aa1544ba6
dgozman <dgozman@chromium.org>
2016-09-27 11:06:31 -0700

    Notify ServiceWorkerContextWatcher about routing ids of ServiceWorkerVersion and their updates.
    We lacked these ids in OnNewLiveVersion and there were no updates when they changed.
    
    BUG=648775
    
    Review-Url: https://codereview.chromium.org/2365053002
    Cr-Commit-Position: refs/heads/master@{#421264}

--- content/browser/service_worker/embedded_worker_instance.h
+++ content/browser/service_worker/embedded_worker_instance.h
@@ -81,6 +81,7 @@ class CONTENT_EXPORT EmbeddedWorkerInstance {
 
     virtual void OnStarting() {}
     virtual void OnProcessAllocated() {}
+    virtual void OnRegisteredToDevToolsManager() {}
     virtual void OnStartWorkerMessageSent() {}
     virtual void OnThreadStarted() {}
     virtual void OnStarted() {}
