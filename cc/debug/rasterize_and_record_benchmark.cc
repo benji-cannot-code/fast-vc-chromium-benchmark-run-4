e46b05db0f09f69743d8c291088fd6f29f04d965
wkorman <wkorman@chromium.org>
2016-09-26 23:47:19 -0700

    Remove obsolete RECORD_WITH_SK_NULL_CANVAS recording mode.
    BUG=648336
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_precise_blink_rel
    
    Review-Url: https://codereview.chromium.org/2355723002
    Cr-Commit-Position: refs/heads/master@{#421131}

--- cc/debug/rasterize_and_record_benchmark.cc
+++ cc/debug/rasterize_and_record_benchmark.cc
@@ -40,7 +40,6 @@ const int kTimeCheckInterval = 1;
 
 const char* kModeSuffixes[RecordingSource::RECORDING_MODE_COUNT] = {
     "",
-    "_sk_null_canvas",
     "_painting_disabled",
     "_caching_disabled",
     "_construction_disabled",
@@ -127,9 +126,6 @@ void RasterizeAndRecordBenchmark::RunOnLayer(PictureLayer* layer) {
       case RecordingSource::RECORD_NORMALLY:
         // Already setup for normal recording.
         break;
-      case RecordingSource::RECORD_WITH_SK_NULL_CANVAS:
-        // Not supported for Display List recording.
-        continue;
       case RecordingSource::RECORD_WITH_PAINTING_DISABLED:
         painting_control = ContentLayerClient::DISPLAY_LIST_PAINTING_DISABLED;
         break;
