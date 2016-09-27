91cdc0b37a0b97f776d1879a27ba64a7b203a10f
manzagop <manzagop@chromium.org>
2016-09-27 04:39:59 -0700

    Wire in postmortem report collection
    Wires in postmortem report collection, which harvests unclean shutdown
    debug information to a protocol buffer, wraps this proto inside a
    barebone minidump and registers the minidump with CrashPad for upload.
    Reports are collected as part of the metrics service client
    initialization task chain. Stability file naming is revised so a unique
    file name is used every time.
    
    Notes:
    - This is experimental work that currently only targets the windows canary
    channel (using an experiment).
    - CrashPad handles consent checking/uploading/throttling.
    - Manufacturing the minidump is a temporary approach. Longer term, Crashpad
    should handle it.
    - This first version also collects reports for crashes. Longer term, the
    reports pertaining to crashes will be bundled with the crash reports.
    
    TEST
    - launch chrome with the stability instrumentation (--enable-features=StabilityDebugging)
    - crash chrome by visiting chrome://chrome://inducebrowsercrashforrealz
    - Validate the existence of a stability file in <user-data-dir>/Stability
    - start chrome again
    - validate "ActivityTracker.Collect.*" metrics chrome://histograms/ActivityTracker
    - validate the presence of a new crash at chrome://crashes
    
    BUG=620813
    
    Review-Url: https://codereview.chromium.org/2344343002
    Cr-Commit-Position: refs/heads/master@{#421172}

---
+++ components/browser_watcher/features.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/browser_watcher/features.h"
+
+namespace browser_watcher {
+
+// Additional stability instrumentation.
+const base::Feature kStabilityDebuggingFeature{
+    "StabilityDebugging", base::FEATURE_DISABLED_BY_DEFAULT};
+
+}  // namespace browser_watcher
