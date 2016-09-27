8967d0cebd63377fca51642abb9f837fd174329e
fdoray <fdoray@chromium.org>
2016-09-26 18:24:26 -0700

    Delete MessageLoop::Run()/RunUntilIdle().
    These methods are deprecated and no longer used in Chrome.
    base::RunLoop must be used instead.
    
    e.g.:
    
    Replace
      message_loop.Run();
    With
      base::RunLoop run_loop;
      // Pass |run_loop.QuitClosure()| to an object/task
      // that will quit the RunLoop when appropriate.
      run_loop.Run();
    
    Replace
      message_loop.RunUntilIdle();
    With
      base::RunLoop().RunUntilIdle();
    
    BUG=616447
    
    Review-Url: https://codereview.chromium.org/2356383003
    Cr-Commit-Position: refs/heads/master@{#421068}

--- base/message_loop/message_loop.h
+++ base/message_loop/message_loop.h
@@ -179,23 +179,6 @@ class BASE_EXPORT MessageLoop : public MessagePump::Delegate {
   void AddNestingObserver(NestingObserver* observer);
   void RemoveNestingObserver(NestingObserver* observer);
 
-#if defined(OS_MACOSX) && !defined(OS_IOS)
- protected:
-#endif  // defined(OS_MACOSX) && !defined(OS_IOS)
-
-  // Deprecated: use RunLoop instead.
-  // Run the message loop.
-  void Run();
-
-  // Deprecated: use RunLoop instead.
-  // Process all pending tasks, windows messages, etc., but don't wait/sleep.
-  // Return as soon as all items that can be run are taken care of.
-  void RunUntilIdle();
-
-#if defined(OS_MACOSX) && !defined(OS_IOS)
- public:
-#endif  // defined(OS_MACOSX) && !defined(OS_IOS)
-
   // Deprecated: use RunLoop instead.
   //
   // Signals the Run method to return when it becomes idle. It will continue to
@@ -507,9 +490,6 @@ class BASE_EXPORT MessageLoop : public MessagePump::Delegate {
 //
 class BASE_EXPORT MessageLoopForUI : public MessageLoop {
  public:
-  using MessageLoop::Run;
-  using MessageLoop::RunUntilIdle;
-
   MessageLoopForUI() : MessageLoop(TYPE_UI) {
   }
 
@@ -575,9 +555,6 @@ static_assert(sizeof(MessageLoop) == sizeof(MessageLoopForUI),
 //
 class BASE_EXPORT MessageLoopForIO : public MessageLoop {
  public:
-  using MessageLoop::Run;
-  using MessageLoop::RunUntilIdle;
-
   MessageLoopForIO() : MessageLoop(TYPE_IO) {
   }
 
