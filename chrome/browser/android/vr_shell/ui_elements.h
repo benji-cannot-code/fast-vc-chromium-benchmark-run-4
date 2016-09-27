e3af2d98ff5bfc53acce1c8e0d4822e9d890f96f
dtrainor <dtrainor@chromium.org>
2016-09-27 14:58:41 -0700

    Revert "Updated to GVR 1.0 SDK"
    This reverts commit 80ccce30a1ca124086bae9d46d4b9edbbd7059ba.
    
    BUG=389343
    TBR=bshe@
    
    Review-Url: https://codereview.chromium.org/2377763003
    Cr-Commit-Position: refs/heads/master@{#421352}

--- chrome/browser/android/vr_shell/ui_elements.h
+++ chrome/browser/android/vr_shell/ui_elements.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "chrome/browser/android/vr_shell/vr_math.h"
-#include "third_party/gvr-android-sdk/src/ndk/include/vr/gvr/capi/include/gvr_types.h"
+#include "third_party/gvr-android-sdk/src/ndk-beta/include/vr/gvr/capi/include/gvr_types.h"
 
 namespace vr_shell {
 
