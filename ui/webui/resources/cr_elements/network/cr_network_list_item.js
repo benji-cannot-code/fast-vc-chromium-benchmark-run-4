8cad1f1e3d5f8a3fb2a1738c054961dfa6abc408
stevenjb <stevenjb@chromium.org>
2016-09-27 09:43:26 -0700

    Settings/Oobe: Fix logic for network list item connecting text.
    BUG=648536
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2369833003
    Cr-Commit-Position: refs/heads/master@{#421234}

--- ui/webui/resources/cr_elements/network/cr_network_list_item.js
+++ ui/webui/resources/cr_elements/network/cr_network_list_item.js
@@ -114,8 +114,11 @@ Polymer({
     if (!this.isStateTextVisible_())
       return '';
     let network = this.networkState;
-    if (this.isListItem)
-      return this.i18n('networkListItemConnected');
+    if (this.isListItem) {
+      if (this.isConnected_())
+        return this.i18n('networkListItemConnected');
+      return '';
+    }
     if (network.Name && network.ConnectionState) {
       return this.getConnectionStateText_(
           network.ConnectionState, CrOnc.getNetworkName(network, this));
@@ -150,8 +153,7 @@ Polymer({
   /** @private */
   isConnected_: function() {
     return this.networkState &&
-        this.networkState.ConnectionState !=
-        CrOnc.ConnectionState.NOT_CONNECTED;
+        this.networkState.ConnectionState == CrOnc.ConnectionState.CONNECTED;
   },
 
   /**
