54ce9be2112e45d6cd4f840084ffcf10fe5750fc
stevenjb <stevenjb@chromium.org>
2016-09-27 09:33:52 -0700

    MD Settings: Use scrollable behavior for all iron-list containers
    This  adds a .scroll-container class which adds a 1px min-height
    to ensure that scrollable iron-list elements update.
    
    It also updates search_engines_page to use CrScrollableBehavior.
    
    Note: Even though we are now using scrollable-behavior for search
    engines, since we do not enforce a max height on the page or the
    container, the containers will never actually scroll (but if we
    do set a maximum page height they will, which is nice).
    
    BUG=639795
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2338133011
    Cr-Commit-Position: refs/heads/master@{#421232}

--- chrome/browser/resources/settings/search_engines_page/search_engines_list.js
+++ chrome/browser/resources/settings/search_engines_page/search_engines_list.js
@@ -10,19 +10,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'settings-search-engines-list',
 
+  behaviors: [CrScrollableBehavior],
+
   properties: {
     /** @type {!Array<!SearchEngine>} */
     engines: {
       type: Array,
-      value: function() { return []; },
+      value: function() {
+        return [];
+      },
       observer: 'enginesChanged_',
     }
   },
 
   /** @private */
   enginesChanged_: function() {
-    // Rather than adding an event to notify this element when it is shown,
-    // observe changes to |engines| and notify the iron-list.
-    this.$$('iron-list').fire('iron-resize');
+    this.updateScrollableContents();
   },
 });
