8868fce77c00b8c88ff6c08999746dc8551ebb7c
dcheng <dcheng@chromium.org>
2016-09-27 16:03:05 -0700

    Allow guest view console messages to appear in logging output.
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2372363002
    Cr-Commit-Position: refs/heads/master@{#421374}

--- extensions/browser/guest_view/web_view/web_view_guest.cc
+++ extensions/browser/guest_view/web_view/web_view_guest.cc
@@ -529,7 +529,7 @@ bool WebViewGuest::AddMessageToConsole(WebContents* source,
   args->SetString(webview::kSourceId, source_id);
   DispatchEventToView(base::MakeUnique<GuestViewEvent>(
       webview::kEventConsoleMessage, std::move(args)));
-  return true;
+  return false;
 }
 
 void WebViewGuest::CloseContents(WebContents* source) {
