553544c283b7eaa3229e8ab129808b7652213e43
treib <treib@chromium.org>
2016-09-27 10:14:22 -0700

    [NTP Snippets] Reschedule background fetching on app upgrade if it was scheduled before
    BUG=646842
    
    Review-Url: https://codereview.chromium.org/2362323002
    Cr-Commit-Position: refs/heads/master@{#421243}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ChromeBackgroundServiceTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ChromeBackgroundServiceTest.java
@@ -11,6 +11,7 @@ import android.test.suitebuilder.annotation.SmallTest;
 
 import com.google.android.gms.gcm.TaskParams;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.AdvancedMockContext;
@@ -32,6 +33,7 @@ public class ChromeBackgroundServiceTest extends InstrumentationTestCase {
     static class MockTaskService extends ChromeBackgroundService {
         private boolean mDidLaunchBrowser = false;
         private boolean mDidFetchSnippets = false;
+        private boolean mDidRescheduleFetching = false;
         private boolean mHasPrecacheInstance = true;
         private boolean mPrecachingStarted = false;
 
@@ -46,6 +48,11 @@ public class ChromeBackgroundServiceTest extends InstrumentationTestCase {
         }
 
         @Override
+        protected void rescheduleFetching() {
+            mDidRescheduleFetching = true;
+        }
+
+        @Override
         protected boolean hasPrecacheInstance() {
             return mHasPrecacheInstance;
         }
@@ -57,17 +64,26 @@ public class ChromeBackgroundServiceTest extends InstrumentationTestCase {
             }
         }
 
+        @Override
+        protected void rescheduleBackgroundSyncTasksOnUpgrade() {}
+
+        @Override
+        protected void reschedulePrecacheTasksOnUpgrade() {}
+
         // Posts an assertion task to the UI thread. Since this is only called after the call
         // to onRunTask, it will be enqueued after any possible call to launchBrowser, and we
         // can reliably check whether launchBrowser was called.
         protected void checkExpectations(final boolean expectedLaunchBrowser,
-                final boolean expectedPrecacheStarted, final boolean expectedFetchSnippets) {
+                final boolean expectedPrecacheStarted, final boolean expectedFetchSnippets,
+                final boolean expectedRescheduleFetching) {
             ThreadUtils.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
                     assertEquals("StartedService", expectedLaunchBrowser, mDidLaunchBrowser);
                     assertEquals("StartedPrecache", expectedPrecacheStarted, mPrecachingStarted);
                     assertEquals("FetchedSnippets", expectedFetchSnippets, mDidFetchSnippets);
+                    assertEquals("RescheduledFetching", expectedRescheduleFetching,
+                            mDidRescheduleFetching);
                 }
             });
         }
@@ -100,7 +116,7 @@ public class ChromeBackgroundServiceTest extends InstrumentationTestCase {
     private void startOnRunTaskAndVerify(String taskTag, boolean shouldStart,
             boolean shouldPrecache, boolean shouldFetchSnippets) {
         mTaskService.onRunTask(new TaskParams(taskTag));
-        mTaskService.checkExpectations(shouldStart, shouldPrecache, shouldFetchSnippets);
+        mTaskService.checkExpectations(shouldStart, shouldPrecache, shouldFetchSnippets, false);
     }
 
     @SmallTest
@@ -153,5 +169,48 @@ public class ChromeBackgroundServiceTest extends InstrumentationTestCase {
     public void testPrecacheLaunchBrowserWhenInstanceDoesNotExist() {
         mTaskService.deletePrecacheInstance();
         startOnRunTaskAndVerify(PrecacheController.PERIODIC_TASK_TAG, true, true, false);
+    }
+
+    private void startOnInitializeTasksAndVerify(boolean shouldStart, boolean shouldReschedule) {
+        mTaskService.onInitializeTasks();
+        mTaskService.checkExpectations(shouldStart, false, false, shouldReschedule);
+    }
+
+    @SmallTest
+    @Feature({"NTPSnippets"})
+    public void testNTPSnippetsNoRescheduleWithoutPrefWhenInstanceExists() {
+        startOnInitializeTasksAndVerify(/*shouldStart=*/false, /*shouldReschedule=*/false);
+    }
+
+    @SmallTest
+    @Feature({"NTPSnippets"})
+    public void testNTPSnippetsNoRescheduleWithoutPrefWhenInstanceDoesNotExist() {
+        deleteSnippetsLauncherInstance();
+        startOnInitializeTasksAndVerify(/*shouldStart=*/false, /*shouldReschedule=*/false);
+    }
+
+    @SmallTest
+    @Feature({"NTPSnippets"})
+    public void testNTPSnippetsRescheduleWithPrefWhenInstanceExists() {
+        // Set the pref indicating that fetching was scheduled before.
+        ContextUtils.getAppSharedPreferences()
+                .edit()
+                .putBoolean(SnippetsLauncher.PREF_IS_SCHEDULED, true)
+                .apply();
+
+        startOnInitializeTasksAndVerify(/*shouldStart=*/false, /*shouldReschedule=*/true);
+    }
+
+    @SmallTest
+    @Feature({"NTPSnippets"})
+    public void testNTPSnippetsRescheduleAndLaunchBrowserWithPrefWhenInstanceDoesNotExist() {
+        deleteSnippetsLauncherInstance();
+        // Set the pref indicating that fetching was scheduled before.
+        ContextUtils.getAppSharedPreferences()
+                .edit()
+                .putBoolean(SnippetsLauncher.PREF_IS_SCHEDULED, true)
+                .apply();
+
+        startOnInitializeTasksAndVerify(/*shouldStart=*/true, /*shouldReschedule=*/true);
     }
 }
