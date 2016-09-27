d33ecd97a777ce08e4cacf2a96e2c472c9e9f97a
alancutter <alancutter@chromium.org>
2016-09-26 22:53:27 -0700

    Clean up stale FIXMEs and TODOs in core/animation
    Review-Url: https://codereview.chromium.org/2370053003
    Cr-Commit-Position: refs/heads/master@{#421120}

--- third_party/WebKit/Source/core/animation/LengthPropertyFunctions.cpp
+++ third_party/WebKit/Source/core/animation/LengthPropertyFunctions.cpp
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// TODO(alancutter): Generate these functions.
-
 ValueRange LengthPropertyFunctions::getValueRange(CSSPropertyID property)
 {
     switch (property) {
