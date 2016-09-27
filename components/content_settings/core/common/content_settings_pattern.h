8228cf4fb0acd556c532c9dfbb82708f79584e5c
alexmos <alexmos@chromium.org>
2016-09-27 10:27:08 -0700

    Add metrics for paths in file scheme exceptions in content settings.
    These will tell how often file scheme exceptions have a non-empty
    path vs. a wildcard path, and which content settings types use them.
    
    BUG=628759, 621724
    
    Review-Url: https://codereview.chromium.org/2367683002
    Cr-Commit-Position: refs/heads/master@{#421248}

--- components/content_settings/core/common/content_settings_pattern.h
+++ components/content_settings/core/common/content_settings_pattern.h
@@ -195,6 +195,10 @@ class ContentSettingsPattern {
   // Returns scheme type of pattern.
   ContentSettingsPattern::SchemeType GetScheme() const;
 
+  // True if this pattern has a non-empty path.  Can only be used for patterns
+  // with file: schemes.
+  bool HasPath() const;
+
   // Compares the pattern with a given |other| pattern and returns the
   // |Relation| of the two patterns.
   Relation Compare(const ContentSettingsPattern& other) const;
--- components/content_settings/core/common/content_settings_pattern_unittest.cc
+++ components/content_settings/core/common/content_settings_pattern_unittest.cc
@@ -828,3 +828,10 @@ TEST(ContentSettingsPatternTest, Schemes) {
   EXPECT_EQ(ContentSettingsPattern::SCHEME_OTHER,
             Pattern("filesystem:http://www.google.com/temporary/").GetScheme());
 }
+
+TEST(ContentSettingsPatternTest, FileSchemeHasPath) {
+  EXPECT_FALSE(Pattern("file:///*").HasPath());
+  EXPECT_TRUE(Pattern("file:///foo").HasPath());
+  EXPECT_TRUE(Pattern("file:///foo/bar/").HasPath());
+  EXPECT_TRUE(Pattern("file:///foo/bar/test.html").HasPath());
+}
