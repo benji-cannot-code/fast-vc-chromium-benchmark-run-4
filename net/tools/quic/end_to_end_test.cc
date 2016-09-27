d88ed5cc74928e91d7fc296911c7e763ca8cffc8
rch <rch@chromium.org>
2016-09-27 12:58:36 -0700

    Move more members from QuicClient to QuicClientBase and change set_response_listener from taking a pointer to a unique_ptr.
    Merge internal change: 134417781
    
    Review-Url: https://codereview.chromium.org/2369863004
    Cr-Commit-Position: refs/heads/master@{#421310}

--- net/tools/quic/end_to_end_test.cc
+++ net/tools/quic/end_to_end_test.cc
@@ -2584,7 +2584,9 @@ TEST_P(EndToEndTestServerPush, ServerPush) {
   AddRequestAndResponseWithServerPush("example.com", "/push_example", kBody,
                                       push_urls, kNumResources, 0);
 
-  client_->client()->set_response_listener(new TestResponseListener);
+  client_->client()->set_response_listener(
+      std::unique_ptr<QuicClientBase::ResponseListener>(
+          new TestResponseListener));
 
   DVLOG(1) << "send request for /push_example";
   EXPECT_EQ(kBody, client_->SendSynchronousRequest(
@@ -2620,7 +2622,9 @@ TEST_P(EndToEndTestServerPush, ServerPushUnderLimit) {
   };
   AddRequestAndResponseWithServerPush("example.com", "/push_example", kBody,
                                       push_urls, kNumResources, 0);
-  client_->client()->set_response_listener(new TestResponseListener);
+  client_->client()->set_response_listener(
+      std::unique_ptr<QuicClientBase::ResponseListener>(
+          new TestResponseListener));
 
   // Send the first request: this will trigger the server to send all the push
   // resources associated with this request, and these will be cached by the
@@ -2666,7 +2670,9 @@ TEST_P(EndToEndTestServerPush, ServerPushOverLimitNonBlocking) {
   }
   AddRequestAndResponseWithServerPush("example.com", "/push_example", kBody,
                                       push_urls, kNumResources, 0);
-  client_->client()->set_response_listener(new TestResponseListener);
+  client_->client()->set_response_listener(
+      std::unique_ptr<QuicClientBase::ResponseListener>(
+          new TestResponseListener));
 
   // Send the first request: this will trigger the server to send all the push
   // resources associated with this request, and these will be cached by the
@@ -2722,7 +2728,9 @@ TEST_P(EndToEndTestServerPush, ServerPushOverLimitWithBlocking) {
   AddRequestAndResponseWithServerPush("example.com", "/push_example", kBody,
                                       push_urls, kNumResources, kBodySize);
 
-  client_->client()->set_response_listener(new TestResponseListener);
+  client_->client()->set_response_listener(
+      std::unique_ptr<QuicClientBase::ResponseListener>(
+          new TestResponseListener));
 
   client_->SendRequest("https://example.com/push_example");
 
@@ -2786,7 +2794,9 @@ TEST_P(EndToEndTestServerPush, DisabledWithoutConnectionOption) {
   };
   AddRequestAndResponseWithServerPush("example.com", "/push_example", kBody,
                                       push_urls, kNumResources, 0);
-  client_->client()->set_response_listener(new TestResponseListener);
+  client_->client()->set_response_listener(
+      std::unique_ptr<QuicClientBase::ResponseListener>(
+          new TestResponseListener));
   EXPECT_EQ(kBody, client_->SendSynchronousRequest(
                        "https://example.com/push_example"));
 
