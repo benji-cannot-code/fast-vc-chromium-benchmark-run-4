caeb231a7e1ca3c920dec5512df028d97f90981a
mdjones <mdjones@chromium.org>
2016-09-27 15:05:06 -0700

    Remove echo from distiller javascript
    BUG=
    
    Review-Url: https://codereview.chromium.org/2367253008
    Cr-Commit-Position: refs/heads/master@{#421354}

--- components/dom_distiller/content/browser/distiller_javascript_service_impl.cc
+++ components/dom_distiller/content/browser/distiller_javascript_service_impl.cc
@@ -23,9 +23,6 @@ DistillerJavaScriptServiceImpl::DistillerJavaScriptServiceImpl(
 
 DistillerJavaScriptServiceImpl::~DistillerJavaScriptServiceImpl() {}
 
-void DistillerJavaScriptServiceImpl::HandleDistillerEchoCall(
-    const mojo::String& message) {}
-
 void DistillerJavaScriptServiceImpl::HandleDistillerFeedbackCall(
     bool good) {
   FeedbackReporter::ReportQuality(good);
