80d8e9291248d252069aaa365331744d7e3b982f
joedow <joedow@chromium.org>
2016-09-27 14:01:10 -0700

    Removing It2MeConfirmationDialogFactory
    Since the It2MeConfirmationDialog is now supported on all platforms, we can
    remove the factory used to create dialog instances and instead use a static
    method on the class itself.
    
    BUG=649016
    
    Review-Url: https://codereview.chromium.org/2372833002
    Cr-Commit-Position: refs/heads/master@{#421333}

--- remoting/host/it2me/it2me_confirmation_dialog_chromeos.cc
+++ remoting/host/it2me/it2me_confirmation_dialog_chromeos.cc
@@ -69,8 +69,8 @@ void It2MeConfirmationDialogChromeOS::OnMessageBoxResult(
                                        Result::OK : Result::CANCEL);
 }
 
-std::unique_ptr<It2MeConfirmationDialog>
-It2MeConfirmationDialogFactory::Create() {
+// static
+std::unique_ptr<It2MeConfirmationDialog> It2MeConfirmationDialog::Create() {
   return base::MakeUnique<It2MeConfirmationDialogChromeOS>();
 }
 
