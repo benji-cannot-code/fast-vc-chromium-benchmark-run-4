ae316a48c4a04f3714fbb9c89bbf75b7344e6704
mvanouwerkerk <mvanouwerkerk@chromium.org>
2016-09-27 08:47:46 -0700

    Reland: Ntp: restore scroll position.
    * The scroll position is stored as extra data on the NavigationEntry.
    * The main use case handled is when the user clicks on a suggested
    article and then back to view more suggestions. Maintaining scroll position helps maintain context and flow here.
    * It is the RecyclerView Adapter position that is stored and restored, so if the device is rotated in the meantime, scroll restore will still present the same content as long as the underlying data has not changed.
    * Because the underlying data is subject to change a few times per day, the scroll position is not persisted. It would be confusing the restore to an old position that now shows different content.
    
    BUG=606356
    TBR=clamy,bauerb,tedchoc
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Committed: https://crrev.com/22b18a9a6d4f421ddb6d4f74280a571bf7dca4f6
    Cr-Commit-Position: refs/heads/master@{#420075}
    
    patch from issue 2327083002 at patchset 160001 (http://crrev.com/2327083002#ps160001)
    
    Committed: https://crrev.com/2c78ced6c8ce46eec8c9f7100b14b3414b7cbc07
    Review-Url: https://codereview.chromium.org/2365313002
    Cr-Original-Commit-Position: refs/heads/master@{#420896}
    Cr-Commit-Position: refs/heads/master@{#421223}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -302,9 +302,10 @@ public class NewTabPageView extends FrameLayout
      * @param manager NewTabPageManager used to perform various actions when the user interacts
      *                with the page.
      * @param searchProviderHasLogo Whether the search provider has a logo.
-     * @param snippetsBridge The optional bridge, that can be used to interact with the snippets.
+     * @param scrollPosition The adapter scroll position to initialize to.
      */
-    public void initialize(NewTabPageManager manager, boolean searchProviderHasLogo) {
+    public void initialize(
+            NewTabPageManager manager, boolean searchProviderHasLogo, int scrollPosition) {
         mManager = manager;
         mUiConfig = new UiConfig(this);
         ViewStub stub = (ViewStub) findViewById(R.id.new_tab_page_layout_stub);
@@ -360,6 +361,7 @@ public class NewTabPageView extends FrameLayout
             mNewTabPageAdapter =
                     new NewTabPageAdapter(mManager, mNewTabPageLayout, mUiConfig);
             mRecyclerView.setAdapter(mNewTabPageAdapter);
+            mRecyclerView.scrollToPosition(scrollPosition);
 
             // Set up swipe-to-dismiss
             ItemTouchHelper helper =
@@ -1185,5 +1187,12 @@ public class NewTabPageView extends FrameLayout
         } else {
             return mScrollView.getScrollY();
         }
+    }
+
+    /**
+     * @return The adapter position the user has scrolled to.
+     */
+    public int getScrollPosition() {
+        return mRecyclerView.getScrollPosition();
     }
 }
