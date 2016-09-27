a0ee7847fe7ebc8dc429e4c8476ba2c89ab9ee96
caseq <caseq@chromium.org>
2016-09-26 20:56:21 -0700

    Timeline: support new scheme of local/global ids in tracing
    Review-Url: https://codereview.chromium.org/2364283002
    Cr-Commit-Position: refs/heads/master@{#421096}

--- third_party/WebKit/Source/devtools/front_end/sdk/TracingManager.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/TracingManager.js
@@ -54,6 +54,8 @@ WebInspector.TracingManager = function(target)
         args: !Object,
         dur: number,
         id: string,
+        id2: (!{global: (string|undefined), local: (string|undefined)}|undefined),
+        scope: string,
         bind_id: string,
         s: string
     }}
