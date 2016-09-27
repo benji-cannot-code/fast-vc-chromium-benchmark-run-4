164d42c600437a3a6b376608ae91c0a60440dc26
kuznetsovs <kuznetsovs@yandex-team.ru>
2016-09-27 01:53:39 -0700

    Remove unused global variable with non-trivial constructor/destructor
    After https://codereview.chromium.org/2257363002
    I think it was added accidentally.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2352683002
    Cr-Commit-Position: refs/heads/master@{#421150}

--- chrome/browser/component_updater/chrome_component_updater_configurator_unittest.cc
+++ chrome/browser/component_updater/chrome_component_updater_configurator_unittest.cc
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
-std::unique_ptr<TestingPrefServiceSimple> pref(new TestingPrefServiceSimple());
-
 namespace component_updater {
 
 class ChromeComponentUpdaterConfiguratorTest : public testing::Test {
