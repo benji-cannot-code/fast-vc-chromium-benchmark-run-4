0b1487d4340e371e1316c60f41177e50a7318e54
maxbogue <maxbogue@chromium.org>
2016-09-27 16:43:08 -0700

    [Sync] Always upload stack traces for USS DeviceInfo.
    BUG=650861
    
    Review-Url: https://codereview.chromium.org/2369033005
    Cr-Commit-Position: refs/heads/master@{#421386}

--- components/browser_sync/profile_sync_components_factory_impl.cc
+++ components/browser_sync/profile_sync_components_factory_impl.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
@@ -147,9 +148,12 @@ void ProfileSyncComponentsFactoryImpl::RegisterCommonDataTypes(
 
   // TODO(stanisc): can DEVICE_INFO be one of disabled datatypes?
   if (base::FeatureList::IsEnabled(switches::kSyncUSSDeviceInfo)) {
+    // Use an error callback that always uploads a stacktrace if it can to help
+    // get USS as stable as possible.
     sync_service->RegisterDataTypeController(
-        base::MakeUnique<UIModelTypeController>(syncer::DEVICE_INFO,
-                                                error_callback, sync_client_));
+        base::MakeUnique<UIModelTypeController>(
+            syncer::DEVICE_INFO, base::Bind(&base::debug::DumpWithoutCrashing),
+            sync_client_));
   } else {
     sync_service->RegisterDataTypeController(
         base::MakeUnique<DeviceInfoDataTypeController>(
