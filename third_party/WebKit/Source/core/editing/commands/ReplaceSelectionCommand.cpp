7178920e296e25bb39d748117d22fdab005a6639
xiaochengh <xiaochengh@chromium.org>
2016-09-27 03:45:03 -0700

    Ensure valid VisiblePosition input for character{Before,After}
    This patch also drive-by eliminates a |createVisiblePositionDeprecated|
    in |CompositeEditCommand::prepareWhitespaceAtPositionForSplit|.
    
    BUG=648949
    
    Review-Url: https://codereview.chromium.org/2368903002
    Cr-Commit-Position: refs/heads/master@{#421164}

--- third_party/WebKit/Source/core/editing/commands/ReplaceSelectionCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/ReplaceSelectionCommand.cpp
@@ -1470,9 +1470,7 @@ static bool isCharacterSmartReplaceExemptConsideringNonBreakingSpace(UChar32 cha
 
 void ReplaceSelectionCommand::addSpacesForSmartReplace(EditingState* editingState)
 {
-    VisiblePosition startOfInsertedContent = positionAtStartOfInsertedContent();
     VisiblePosition endOfInsertedContent = positionAtEndOfInsertedContent();
-
     Position endUpstream = mostBackwardCaretPosition(endOfInsertedContent.deepEquivalent());
     Node* endNode = endUpstream.computeNodeBeforePosition();
     int endOffset = endNode && endNode->isTextNode() ? toText(endNode)->length() : 0;
@@ -1493,12 +1491,16 @@ void ReplaceSelectionCommand::addSpacesForSmartReplace(EditingState* editingStat
             insertNodeAfter(node, endNode, editingState);
             if (editingState->isAborted())
                 return;
+            // Make sure that |updateNodesInserted| does not change
+            // |m_startOfInsertedContent|.
+            DCHECK(m_startOfInsertedContent.isNotNull());
             updateNodesInserted(node);
         }
     }
 
     document().updateStyleAndLayout();
 
+    VisiblePosition startOfInsertedContent = positionAtStartOfInsertedContent();
     Position startDownstream = mostForwardCaretPosition(startOfInsertedContent.deepEquivalent());
     Node* startNode = startDownstream.computeNodeAfterPosition();
     unsigned startOffset = 0;
