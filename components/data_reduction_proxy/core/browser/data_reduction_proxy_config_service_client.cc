d14cb452ff0e3f2c1d6ab4effd7cf40620051e4f
rajendrant <rajendrant@chromium.org>
2016-09-26 23:38:19 -0700

    Add data usage tracking for data reduction proxy, precache services
    BUG=527304
    
    Review-Url: https://codereview.chromium.org/2274243002
    Cr-Commit-Position: refs/heads/master@{#421130}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_params.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_util.h"
 #include "components/data_reduction_proxy/proto/client_config.pb.h"
+#include "components/data_use_measurement/core/data_use_user_data.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/load_flags.h"
 #include "net/base/load_timing_info.h"
@@ -394,6 +395,9 @@ DataReductionProxyConfigServiceClient::GetURLFetcherForConfig(
   DCHECK(thread_checker_.CalledOnValidThread());
   std::unique_ptr<net::URLFetcher> fetcher(net::URLFetcher::Create(
       secure_proxy_check_url, net::URLFetcher::POST, this));
+  data_use_measurement::DataUseUserData::AttachToFetcher(
+      fetcher.get(),
+      data_use_measurement::DataUseUserData::DATA_REDUCTION_PROXY);
   fetcher->SetLoadFlags(net::LOAD_BYPASS_PROXY);
   fetcher->SetUploadData("application/x-protobuf", request_body);
   DCHECK(url_request_context_getter_);
