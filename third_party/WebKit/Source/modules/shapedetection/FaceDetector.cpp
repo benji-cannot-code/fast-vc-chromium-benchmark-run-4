6b7d9004da0bf5e54f23738a0b8afde0bbb01eec
xianglu <xianglu@chromium.org>
2016-09-27 12:10:00 -0700

    shapedetection module: Introduce basic structure for face detection
    This CL introduces the basic structure of Shape Detection API, including
    four classes:
    Detector, FaceDetector, DetectedObject, DetectedFace (dictionaries are
    not yet implemented)
    
    and adds simple layout tests in shapedetection-creation.html to verify
    corresponding constructors.
    
    R=mcasas@chromium.org
    BUG=646083
    
    Review-Url: https://codereview.chromium.org/2360783003
    Cr-Commit-Position: refs/heads/master@{#421290}

---
+++ third_party/WebKit/Source/modules/shapedetection/FaceDetector.cpp
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "modules/shapedetection/FaceDetector.h"
+
+#include "bindings/core/v8/ScriptPromiseResolver.h"
+#include "core/dom/DOMException.h"
+
+namespace blink {
+
+FaceDetector* FaceDetector::create()
+{
+    return new FaceDetector();
+}
+
+ScriptPromise FaceDetector::detect(ScriptState* scriptState, const HTMLImageElement* image)
+{
+    ScriptPromiseResolver* resolver = ScriptPromiseResolver::create(scriptState);
+    ScriptPromise promise = resolver->promise();
+    resolver->reject(DOMException::create(NotSupportedError, "Not implemented"));
+    return promise;
+}
+
+} // namespace blink
