1c25a4015db80584bcf87e9292d105a38ad005dd
tommycli <tommycli@chromium.org>
2016-09-27 14:53:08 -0700

    [HBD] Only use Plugin Content Settings for Flash.
    All other plugins (PDF, Widevine, Nacl), as well as custom plugins,
    should be treated as JavaScript.
    
    This is because starting in 55, all the Plugin content settings UI
    surfaces have been renamed the Flash content settings.
    
    This is related to the chrome://plugins deprecation.
    
    BUG=615738
    
    Review-Url: https://codereview.chromium.org/2356053002
    Cr-Commit-Position: refs/heads/master@{#421348}

--- chrome/browser/plugins/plugin_info_message_filter_unittest.cc
+++ chrome/browser/plugins/plugin_info_message_filter_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/plugin_service.h"
 #include "content/public/browser/plugin_service_filter.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/common/content_constants.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -145,9 +146,15 @@ class PluginInfoMessageFilterTest : public ::testing::Test {
         CONTENT_SETTING_BLOCK : CONTENT_SETTING_DEFAULT;
     bool is_default = !expected_is_default;
     bool is_managed = !expected_is_managed;
-    PluginUtils::GetPluginContentSetting(
-        host_content_settings_map_, content::WebPluginInfo(), url, url, plugin,
-        &setting, &is_default, &is_managed);
+
+    // Pass in a fake Flash plugin info.
+    content::WebPluginInfo plugin_info(
+        base::ASCIIToUTF16(content::kFlashPluginName), base::FilePath(),
+        base::ASCIIToUTF16("1"), base::ASCIIToUTF16("Fake Flash"));
+
+    PluginUtils::GetPluginContentSetting(host_content_settings_map_,
+                                         plugin_info, url, url, plugin,
+                                         &setting, &is_default, &is_managed);
     EXPECT_EQ(expected_setting, setting);
     EXPECT_EQ(expected_is_default, is_default);
     EXPECT_EQ(expected_is_managed, is_managed);
