0bdd040fecf33ec8b01e3fdc5d1114e18ee17aab
fdoray <fdoray@chromium.org>
2016-09-27 05:23:33 -0700

    Remove MessageLoop::current() from base::win::ObjectWatcher (reland).
    ---
    This is a reland of https://codereview.chromium.org/2125763003 which
    was reverted because it broke the Win10 builder. It should not break
    anything now that TestSimpleTaskRunner is thread-safe
    (https://codereview.chromium.org/2296923003)
    ---
    
    Why? The fact that there's a MessageLoop on the thread is an
    unnecessary implementation detail. When browser threads are migrated to
    base/task_scheduler, tasks will no longer have access to a MessageLoop
    but they will be able to get the current ThreadTaskRunnerHandle.
    
    Before this CL, ObjectWatcher implemented
    WillDestroyCurrentMessageLoop() to stop the watch when the
    MessageLoop responsible for running the callback was destroyed.
    This prevented the watch callback from being sent to a destroyed
    MessageLoop.
    
    Now that ObjectWatcher keeps a reference to a TaskRunner, this is
    no longer required. The TaskRunner can't be deleted while there are
    references to it. If the underlying MessageLoop has been deleted
    when the watch callback is posted, the callback will simply not run.
    
    Note that the watch will still be stopped when the ObjectWatcher is
    deleted.
    
    TBR=ben@chromium.org
    BUG=650318
    
    Review-Url: https://codereview.chromium.org/2365423003
    Cr-Commit-Position: refs/heads/master@{#421179}

--- chrome/common/service_process_util_win.cc
+++ chrome/common/service_process_util_win.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/service_process_util.h"
 
+#include <algorithm>
 #include <memory>
 
 #include "base/callback.h"
