9fa193033af2b1661528bd8bd4e3d32fb1815c8f
sky <sky@chromium.org>
2016-09-27 06:55:56 -0700

    Fixes possible crash in ~Display
    If the root of a WindowTree associated with the window manager was
    removed and the associated WindowTree destroyed, then Display
    would still have a reference to the WindowManagerState resulting in a
    possible crash.
    
    WindowServer::DestroyTree() had code to handle this case, but it
    looked up the Display using the roots of the WindowTree, so that once
    the roots were removed the Display couldn't be found. Fix is to move
    logic to ~WindowManagerState.
    
    BUG=none
    TEST=covered by test
    R=sadrul@chromium.org
    
    Review-Url: https://codereview.chromium.org/2365243003
    Cr-Commit-Position: refs/heads/master@{#421196}

--- services/ui/ws/window_manager_state.cc
+++ services/ui/ws/window_manager_state.cc
@@ -120,6 +120,9 @@ WindowManagerState::WindowManagerState(WindowTree* window_tree)
 }
 
 WindowManagerState::~WindowManagerState() {
+  for (auto& display_root : window_manager_display_roots_)
+    display_root->display()->OnWillDestroyTree(window_tree_);
+
   for (auto& display_root : orphaned_window_manager_display_roots_)
     display_root->root()->RemoveObserver(this);
 }
