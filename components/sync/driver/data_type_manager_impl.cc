dbf7dc92debfea6bb44eafe8266cb0b3ddc86c88
maxbogue <maxbogue@chromium.org>
2016-09-28 16:20:20 -0700

    [Sync] Add an error integration test for USS.
    Caught two bugs:
    
    - The NonBlockingDTC needs to report errors even if the type is running.
    - The DataTypeManagerImpl must do ProcessReconfigure asynchronously
      to give the ModelAssociationManager time to finish stopping the type.
    
    BUG=643269
    
    Review-Url: https://codereview.chromium.org/2369063003
    Cr-Commit-Position: refs/heads/master@{#421676}

--- components/sync/driver/data_type_manager_impl.cc
+++ components/sync/driver/data_type_manager_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/profiler/scoped_tracker.h"
 #include "base/strings/stringprintf.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/trace_event.h"
 #include "components/sync/core/data_type_debug_info_listener.h"
 #include "components/sync/driver/data_type_encryption_handler.h"
@@ -333,7 +334,10 @@ TypeSetPriorityList DataTypeManagerImpl::PrioritizeTypes(
 }
 
 void DataTypeManagerImpl::ProcessReconfigure() {
-  DCHECK(needs_reconfigure_);
+  // This may have been called asynchronously; no-op if it is no longer needed.
+  if (!needs_reconfigure_) {
+    return;
+  }
 
   // Wait for current download and association to finish.
   if (!download_types_queue_.empty() ||
@@ -500,7 +504,12 @@ void DataTypeManagerImpl::OnSingleDataTypeWillStop(
     if (error.error_type() != syncer::SyncError::UNRECOVERABLE_ERROR) {
       needs_reconfigure_ = true;
       last_configure_reason_ = syncer::CONFIGURE_REASON_PROGRAMMATIC;
-      ProcessReconfigure();
+      // Do this asynchronously so the ModelAssociationManager has a chance to
+      // finish stopping this type, otherwise DeactivateDataType() and Stop()
+      // end up getting called twice on the controller.
+      base::ThreadTaskRunnerHandle::Get()->PostTask(
+          FROM_HERE, base::Bind(&DataTypeManagerImpl::ProcessReconfigure,
+                                weak_ptr_factory_.GetWeakPtr()));
     }
   }
 }
