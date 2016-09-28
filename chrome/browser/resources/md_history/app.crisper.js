3bb5579ac92cb600b8b73d839ab14cc58e6ee0c9
tsergeant <tsergeant@chromium.org>
2016-09-27 22:11:36 -0700

    MD History: Focus the search field on load without 'showing' it
    This changes the behavior from the equivalent of clicking on the search
    bar to the equivalent of focusing it with the tab key. This is a subtle
    distinction, but the end result is that the search icon does not light
    up, and the search field will not animate open when resizing the window
    into narrow mode.
    
    This CL also changes the focus logic to trigger on the correct toolbar
    wide mode breakpoint, which fixes an issue where the search field
    wouldn't be focused at certain screen widths.
    
    BUG=642701
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2369823002
    Cr-Commit-Position: refs/heads/master@{#421453}

--- chrome/browser/resources/md_history/app.crisper.js
+++ chrome/browser/resources/md_history/app.crisper.js
@@ -5402,8 +5402,9 @@ Polymer({
     setTimeout(function() {
       chrome.send('metricsHandler:recordTime', [ 'History.ResultsRenderedTime', window.performance.now() ]);
     });
-    if (!this.hasDrawer_) {
-      this.focusToolbarSearchField();
+    var searchField = this.$.toolbar.searchField;
+    if (!searchField.narrow) {
+      searchField.getSearchInput().focus();
     }
     md_history.ensureLazyLoaded();
   },
--- chrome/browser/resources/md_history/app.js
+++ chrome/browser/resources/md_history/app.js
@@ -115,8 +115,10 @@ Polymer({
 
     // Focus the search field on load. Done here to ensure the history page
     // is rendered before we try to take focus.
-    if (!this.hasDrawer_) {
-      this.focusToolbarSearchField();
+    var searchField =
+        /** @type {HistoryToolbarElement} */ (this.$.toolbar).searchField;
+    if (!searchField.narrow) {
+      searchField.getSearchInput().focus();
     }
 
     // Lazily load the remainder of the UI.
@@ -177,7 +179,7 @@ Polymer({
   },
 
   /**
-   * Focuses the search bar in the toolbar.
+   * Shows and focuses the search bar in the toolbar.
    */
   focusToolbarSearchField: function() { this.$.toolbar.showSearchField(); },
 
