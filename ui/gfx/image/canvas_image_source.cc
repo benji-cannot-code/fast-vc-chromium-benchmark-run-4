a35c3354795fe18b0e084acacf9f79c0d2735f92
estade <estade@chromium.org>
2016-09-28 09:37:44 -0700

    Improve ergonomics of CanvasImageSource -> gfx::ImageSkia.
    So we don't have to write things like:
      auto source = new MySource;
      ... = gfx::ImageSkia(source, source->size());
    
    instead, we write
      ... = CanvasImageSource::MakeImageSkia<MySource>();
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2373633002
    Cr-Commit-Position: refs/heads/master@{#421538}

--- ui/gfx/image/canvas_image_source.cc
+++ ui/gfx/image/canvas_image_source.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "ui/gfx/canvas.h"
+#include "ui/gfx/image/image_skia.h"
 
 namespace gfx {
 
