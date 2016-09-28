125032f203790e4e894f22e63158717f2e13325f
tbansal <tbansal@chromium.org>
2016-09-28 15:30:58 -0700

    Improve logging of DRP.ConfigService.HTTPRequests
    Record DRP.ConfigService.HTTPRequests only if either Data
    Reduction Proxy (DRP) is going to be used for fetching a
    request, or if DRP can't be used because no valid DRP
    proxy is available.
    
    Also, renamed a histogram to match
    DataReductionProxy.ConfigService.* prefix. The new name is
    also the one present in histograms.xml.
    
    BUG=649794
    
    Review-Url: https://codereview.chromium.org/2365823002
    Cr-Commit-Position: refs/heads/master@{#421662}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client.cc
@@ -115,7 +115,7 @@ void RecordAuthExpiredSessionKey(bool matches) {
                                         : AUTH_EXPIRED_SESSION_KEY_MISMATCH;
 
   UMA_HISTOGRAM_ENUMERATION(
-      "DataReductionProxy.ClientConfig.AuthExpiredSessionKey", state,
+      "DataReductionProxy.ConfigService.AuthExpiredSessionKey", state,
       AUTH_EXPIRED_SESSION_KEY_BOUNDARY);
 }
 
--- components/data_reduction_proxy/core/browser/data_reduction_proxy_delegate_unittest.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_delegate_unittest.cc
@@ -727,6 +727,7 @@ TEST_F(DataReductionProxyDelegateTest, HTTPRequests) {
           "http=" + data_reduction_proxy + ",direct://;");
       data_reduction_proxy_config.set_id(1);
     }
+    EXPECT_NE(test.use_direct_proxy, data_reduction_proxy_config.is_valid());
     config()->SetStateForTest(test.enabled_by_user /* enabled */,
                               false /* at_startup */);
 
@@ -753,6 +754,7 @@ TEST_F(DataReductionProxyDelegateTest, HTTPRequests) {
 }
 
 TEST_F(DataReductionProxyDelegateTest, OnCompletedSizeFor200) {
+  base::HistogramTester histogram_tester;
   int64_t baseline_received_bytes = total_received_bytes();
   int64_t baseline_original_received_bytes = total_original_received_bytes();
 
@@ -779,6 +781,9 @@ TEST_F(DataReductionProxyDelegateTest, OnCompletedSizeFor200) {
   EXPECT_EQ(static_cast<int64_t>(raw_headers.size() +
                                  10000 /* original_response_body */),
             total_original_received_bytes() - baseline_original_received_bytes);
+
+  histogram_tester.ExpectUniqueSample(
+      "DataReductionProxy.ConfigService.HTTPRequests", 1, 1);
 }
 
 TEST_F(DataReductionProxyDelegateTest, OnCompletedSizeFor304) {
@@ -913,6 +918,7 @@ TEST_F(DataReductionProxyDelegateTest, PartialRangeSavings) {
   };
 
   for (const auto& test : test_cases) {
+    base::HistogramTester histogram_tester;
     int64_t baseline_received_bytes = total_received_bytes();
     int64_t baseline_original_received_bytes = total_original_received_bytes();
 
@@ -945,6 +951,8 @@ TEST_F(DataReductionProxyDelegateTest, PartialRangeSavings) {
     EXPECT_EQ(expected_original_size, total_original_received_bytes() -
                                           baseline_original_received_bytes)
         << (&test - test_cases);
+    histogram_tester.ExpectUniqueSample(
+        "DataReductionProxy.ConfigService.HTTPRequests", 1, 1);
   }
 }
 
