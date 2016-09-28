ed1425c6a17a2deade1c2871d9691833f3fec3ca
emircan <emircan@chromium.org>
2016-09-27 17:39:51 -0700

    Fix MediaFoundationVideoEncodeAccelerator timestamps
    This CL changes the timestamps sent from MediaFoundationVEA. Earlier I enabled
    its usage on the bots as a part of finch experiment. It caused some
    regressions as you can see from the bug below. Some of them were unexpected
    and had inconsistent output, such as goog_frame_rate_output more than
    goog_frame_rate_input for a couple seconds. Investigation showed that, these
    were related to timestamps. With this CL, we leave setting the timestamps to
    rtc_video_encoder by sending empty ones.
    
    BUG=649275
    TEST=Ran WebRTCPerfBrowserTest on Windows using MediaFoundationVEA.
    
    Review-Url: https://codereview.chromium.org/2375593003
    Cr-Commit-Position: refs/heads/master@{#421399}

--- media/gpu/media_foundation_video_encode_accelerator_win.cc
+++ media/gpu/media_foundation_video_encode_accelerator_win.cc
@@ -482,7 +482,7 @@ void MediaFoundationVideoEncodeAccelerator::ProcessOutput() {
     DVLOG(3) << "No bitstream buffers.";
     // We need to copy the output so that encoding can continue.
     std::unique_ptr<EncodeOutput> encode_output(
-        new EncodeOutput(size, keyframe, base::Time::Now() - base::Time()));
+        new EncodeOutput(size, keyframe, base::TimeDelta()));
     {
       MediaBufferScopedPointer scoped_buffer(output_buffer.get());
       memcpy(encode_output->memory(), scoped_buffer.get(), size);
@@ -501,9 +501,8 @@ void MediaFoundationVideoEncodeAccelerator::ProcessOutput() {
   }
 
   client_task_runner_->PostTask(
-      FROM_HERE,
-      base::Bind(&Client::BitstreamBufferReady, client_, buffer_ref->id, size,
-                 keyframe, base::Time::Now() - base::Time()));
+      FROM_HERE, base::Bind(&Client::BitstreamBufferReady, client_,
+                            buffer_ref->id, size, keyframe, base::TimeDelta()));
 
   // Keep calling ProcessOutput recursively until MF_E_TRANSFORM_NEED_MORE_INPUT
   // is returned to flush out all the output.
