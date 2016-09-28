b9bbd6455784fc651649076dc1b23aa9e6436a58
guidou <guidou@chromium.org>
2016-09-28 11:57:11 -0700

    Remove cancelMediaDevicesRequest dead code.
    BUG=650605
    
    Review-Url: https://codereview.chromium.org/2377613002
    Cr-Commit-Position: refs/heads/master@{#421593}

--- third_party/WebKit/Source/modules/mediastream/UserMediaController.h
+++ third_party/WebKit/Source/modules/mediastream/UserMediaController.h
@@ -46,12 +46,8 @@ public:
 
     void requestUserMedia(UserMediaRequest*);
     void cancelUserMediaRequest(UserMediaRequest*);
-
     void requestMediaDevices(MediaDevicesRequest*);
-    void cancelMediaDevicesRequest(MediaDevicesRequest*);
-
     void requestSources(MediaStreamTrackSourcesRequest*);
-
     void setMediaDeviceChangeObserver(MediaDevices*);
 
     static const char* supplementName();
@@ -76,11 +72,6 @@ inline void UserMediaController::cancelUserMediaRequest(UserMediaRequest* reques
 inline void UserMediaController::requestMediaDevices(MediaDevicesRequest* request)
 {
     m_client->requestMediaDevices(request);
-}
-
-inline void UserMediaController::cancelMediaDevicesRequest(MediaDevicesRequest* request)
-{
-    m_client->cancelMediaDevicesRequest(request);
 }
 
 inline void UserMediaController::requestSources(MediaStreamTrackSourcesRequest* request)
--- third_party/WebKit/Source/web/UserMediaClientImpl.cpp
+++ third_party/WebKit/Source/web/UserMediaClientImpl.cpp
@@ -65,12 +65,6 @@ void UserMediaClientImpl::requestMediaDevices(MediaDevicesRequest* request)
         m_client->requestMediaDevices(request);
 }
 
-void UserMediaClientImpl::cancelMediaDevicesRequest(MediaDevicesRequest* request)
-{
-    if (m_client)
-        m_client->cancelMediaDevicesRequest(WebMediaDevicesRequest(request));
-}
-
 void UserMediaClientImpl::requestSources(MediaStreamTrackSourcesRequest* request)
 {
     if (m_client)
