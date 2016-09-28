b557d90cc4a914113e70bf5161c9a2006763d18e
oshima <oshima@chromium.org>
2016-09-28 10:31:13 -0700

    Use WmLayoutManager::OnChildWindowVisibilityChanged to update the system modal state.
    The issue in WmWindowObserver::OnChildWindowVisibility is fixed in
    
    https://codereview.chromium.org/2369413002/
    
    but it's probably better to use WmLayoutManager::OnChildWindowVisibilityChanged because it's WmLayoutManager.
    
    BUG=crbug.com/650751
    TEST=covered by unit tests. tested manulally with the test app in the bug.
    
    Review-Url: https://codereview.chromium.org/2374613004
    Cr-Commit-Position: refs/heads/master@{#421558}

--- ash/common/wm/system_modal_container_layout_manager.cc
+++ ash/common/wm/system_modal_container_layout_manager.cc
@@ -53,6 +53,21 @@ SystemModalContainerLayoutManager::~SystemModalContainerLayoutManager() {
 ////////////////////////////////////////////////////////////////////////////////
 // SystemModalContainerLayoutManager, WmLayoutManager implementation:
 
+void SystemModalContainerLayoutManager::OnChildWindowVisibilityChanged(
+    WmWindow* window,
+    bool visible) {
+  if (GetModalType(window) != ui::MODAL_TYPE_SYSTEM)
+    return;
+
+  if (window->IsVisible()) {
+    DCHECK(!base::ContainsValue(modal_windows_, window));
+    AddModalWindow(window);
+  } else {
+    if (RemoveModalWindow(window))
+      WmShell::Get()->OnModalWindowRemoved(window);
+  }
+}
+
 void SystemModalContainerLayoutManager::OnWindowResized() {
   PositionDialogsAfterWorkAreaResize();
 }
@@ -108,20 +123,6 @@ void SystemModalContainerLayoutManager::OnWindowPropertyChanged(
   } else {
     if (RemoveModalWindow(window))
       WmShell::Get()->OnModalWindowRemoved(window);
-  }
-}
-
-void SystemModalContainerLayoutManager::OnWindowVisibilityChanged(
-    WmWindow* window,
-    bool visible) {
-  if (GetModalType(window) != ui::MODAL_TYPE_SYSTEM)
-    return;
-
-  if (window->IsVisible()) {
-    AddModalWindow(window);
-  } else {
-    RemoveModalWindow(window);
-    WmShell::Get()->OnModalWindowRemoved(window);
   }
 }
 
