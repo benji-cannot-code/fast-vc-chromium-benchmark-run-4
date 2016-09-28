55f0c901b11f5d03e53b1355400b4416d0cf73b4
yosin <yosin@chromium.org>
2016-09-27 22:48:28 -0700

    Avoid using deprecated version of createVisiblePosition() in DeleteSelectionCommand::mergeParagraphs()
    This patch changes to avoid using  deprecated version of
    |createVisiblePosition()| by ensuring clean layout before calling it for
    improving code health.
    
    This patch is inspired by http://crrev.com/2373483003, which introducing
    another |createVisiblePosition()| call here at this change.
    
    BUG=n/a
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2380483002
    Cr-Commit-Position: refs/heads/master@{#421457}

--- third_party/WebKit/Source/core/editing/commands/DeleteSelectionCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/DeleteSelectionCommand.cpp
@@ -663,7 +663,8 @@ void DeleteSelectionCommand::mergeParagraphs(EditingState* editingState)
         insertNodeAt(HTMLBRElement::create(document()), m_upstreamStart, editingState);
         if (editingState->isAborted())
             return;
-        mergeDestination = createVisiblePositionDeprecated(m_upstreamStart);
+        document().updateStyleAndLayoutIgnorePendingStylesheets();
+        mergeDestination = createVisiblePosition(m_upstreamStart);
         startOfParagraphToMove = createVisiblePosition(storedStartOfParagraphToMove);
     }
 
