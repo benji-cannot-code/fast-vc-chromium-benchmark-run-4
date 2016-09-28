17af01b591a9d4c2b5968118234b8131400e37e1
hbos <hbos@chromium.org>
2016-09-28 03:33:16 -0700

    Promise-based RTCPeerConnection::getStats implementation (behind flag).
    RTCStatsReport[1] added. It contains code for translating
    WebRTCStatsReport and stats types[2] into stats dictionaries[3] by
    looping over stats and stats members. JavaScript dictionaries are plain
    javascript objects created on the fly.
    
    RTCPeerConnection::getStats invokes the peer connection handler's
    getStats and creates the RTCStatsReport in a callback and resolves the
    promise.
    
    Test related changes:
    - peerconnection_getstats.js: This is a browser test that run the real
      code all the way down to the WebRTC layer (no mocking on any layer).
      It makes sure getStats resolves the promise and returns a non-empty
      set of stats, with simple sanity check on properties that all RTCStats
      dictionaries have in common.
    - mock_webrtc_peer_connection_handler.cc: Mock implementation of
      the handler's getStats, returning a stats object containing every type
      of stats member.
    - RTCPeerConnection-getStats-promise.html: A layout test that use the
      mock peer connection handler. Verifies the value of each stat member.
    
    [1] https://w3c.github.io/webrtc-pc/#rtcstatsreport-object
    [2] https://cs.chromium.org/chromium/src/third_party/WebKit/public/platform/WebRTCStats.h?q=WebRTCStats.h&sq=package:chromium&dr=CSs&l=37
    [3] https://w3c.github.io/webrtc-stats/
    
    BUG=chromium:627816
    
    Review-Url: https://codereview.chromium.org/2363673002
    Cr-Commit-Position: refs/heads/master@{#421491}

--- third_party/WebKit/Source/modules/peerconnection/RTCPeerConnection.cpp
+++ third_party/WebKit/Source/modules/peerconnection/RTCPeerConnection.cpp
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/peerconnection/RTCSessionDescriptionRequestImpl.h"
 #include "modules/peerconnection/RTCSessionDescriptionRequestPromiseImpl.h"
 #include "modules/peerconnection/RTCStatsCallback.h"
+#include "modules/peerconnection/RTCStatsReport.h"
 #include "modules/peerconnection/RTCStatsRequestImpl.h"
 #include "modules/peerconnection/RTCVoidRequestImpl.h"
 #include "modules/peerconnection/RTCVoidRequestPromiseImpl.h"
@@ -373,6 +374,29 @@ RTCOfferOptionsPlatform* parseOfferOptions(const Dictionary& options)
     RTCOfferOptionsPlatform* rtcOfferOptions = RTCOfferOptionsPlatform::create(offerToReceiveVideo, offerToReceiveAudio, voiceActivityDetection, iceRestart);
     return rtcOfferOptions;
 }
+
+// Helper class for |RTCPeerConnection::getStats(ScriptState*, MediaStreamTrack*)|
+class WebRTCStatsReportCallbackResolver : public WebRTCStatsReportCallback {
+public:
+    // Takes ownership of |resolver|.
+    static std::unique_ptr<WebRTCStatsReportCallback> create(ScriptPromiseResolver* resolver)
+    {
+        return std::unique_ptr<WebRTCStatsReportCallback>(new WebRTCStatsReportCallbackResolver(resolver));
+    }
+
+    ~WebRTCStatsReportCallbackResolver() override {}
+
+private:
+    WebRTCStatsReportCallbackResolver(ScriptPromiseResolver* resolver)
+        : m_resolver(resolver) {}
+
+    void OnStatsDelivered(std::unique_ptr<WebRTCStatsReport> report) override
+    {
+        m_resolver->resolve(new RTCStatsReport(std::move(report)));
+    }
+
+    Persistent<ScriptPromiseResolver> m_resolver;
+};
 
 } // namespace
 
@@ -974,8 +998,12 @@ ScriptPromise RTCPeerConnection::getStats(ScriptState* scriptState, MediaStreamT
 {
     ExecutionContext* context = scriptState->getExecutionContext();
     UseCounter::count(context, UseCounter::RTCPeerConnectionGetStats);
-    // TODO(hbos): Implement new |getStats| function. crbug.com/627816
-    return ScriptPromise::rejectWithDOMException(scriptState, DOMException::create(NotSupportedError, "getStats(optional MediaStreamTrack?) has not been implemented yet."));
+
+    ScriptPromiseResolver* resolver = ScriptPromiseResolver::create(scriptState);
+    ScriptPromise promise = resolver->promise();
+    m_peerHandler->getStats(WebRTCStatsReportCallbackResolver::create(resolver));
+
+    return promise;
 }
 
 RTCDataChannel* RTCPeerConnection::createDataChannel(String label, const Dictionary& options, ExceptionState& exceptionState)
