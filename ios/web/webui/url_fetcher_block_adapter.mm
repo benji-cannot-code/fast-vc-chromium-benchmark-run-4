5ea7631b684a66701c7d7c7b9ac18f8ca405e6e9
stkhapugin <stkhapugin@chromium.org>
2016-09-28 08:02:08 -0700

    [ARC] Converts part of ios/web/ui to ARC. Notable changes: none.
    BUG=639327
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2347133003
    Cr-Commit-Position: refs/heads/master@{#421521}

--- ios/web/webui/url_fetcher_block_adapter.mm
+++ ios/web/webui/url_fetcher_block_adapter.mm
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_context_getter.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace web {
 
 URLFetcherBlockAdapter::URLFetcherBlockAdapter(
--- ios/web/webui/web_ui_ios_impl.mm
+++ ios/web/webui/web_ui_ios_impl.mm
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/webui/web_ui_ios_message_handler.h"
 #include "ios/web/web_state/web_state_impl.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 using web::WebUIIOSController;
 
 namespace web {
