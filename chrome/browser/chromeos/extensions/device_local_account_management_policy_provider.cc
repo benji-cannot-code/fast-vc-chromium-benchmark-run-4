e8283ef28f562342df0dcdffc1cea6582b96f1fc
bartfab <bartfab@chromium.org>
2016-09-28 05:47:42 -0700

    Whitelist Ricoh print extension for user in Public Sessions
    This CL allows the RICOH Print for Chrome extension to run in
    Public Sessions.
    
    BUG=648993
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2373553002
    Cr-Commit-Position: refs/heads/master@{#421503}

--- chrome/browser/chromeos/extensions/device_local_account_management_policy_provider.cc
+++ chrome/browser/chromeos/extensions/device_local_account_management_policy_provider.cc
@@ -38,6 +38,7 @@ const char* const kPublicSessionWhitelist[] = {
     "ngjnkanfphagcaokhjecbgkboelgfcnf",  // Print button
     "gbchcmhmhahfdphkhkmpfmihenigjmpp",  // Chrome Remote Desktop
     "cjanmonomjogheabiocdamfpknlpdehm",  // HP printer driver
+    "ioofdkhojeeimmagbjbknkejkgbphdfl",  // RICOH Print for Chrome
     "pmnllmkmjilbojkpgplbdmckghmaocjh",  // Scan app by François Beaufort
     "khpfeaanjngmcnplbdlpegiifgpfgdco",  // Smart Card Connector App
     "haeblkpifdemlfnkogkipmghfcbonief",  // Charismathics Smart Card Middleware
