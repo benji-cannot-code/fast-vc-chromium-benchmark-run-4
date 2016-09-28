955a9d9f20c3426ca2e9328b061848671ab163ae
sdefresne <sdefresne@chromium.org>
2016-09-28 07:17:27 -0700

    Revert of Don't create child webviews for invalid URLs. (patchset #1 id:1 of https://codereview.chromium.org/2356683002/ )
    Reason for revert:
    This breaks downstream bots, see https://bugs.chromium.org/p/chromium/issues/detail?id=651075.
    
    Original issue's description:
    > Don't create child webviews for invalid URLs.
    >
    > This matches the behavior on Desktop Chrome, where window.open() calls
    > for invalid URLs are no-ops.
    >
    > BUG=627581
    >
    > Committed: https://crrev.com/0def4f438524a2b5aa3f55fab70b462b381b43c8
    > Cr-Commit-Position: refs/heads/master@{#421452}
    
    TBR=marq@chromium.org,eugenebut@chromium.org,kkhorimoto@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=627581
    
    Review-Url: https://codereview.chromium.org/2371373003
    Cr-Commit-Position: refs/heads/master@{#421515}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -4802,10 +4802,6 @@ const NSTimeInterval kSnapshotOverlayTransition = 0.5;
 
   GURL requestURL = net::GURLWithNSURL(action.request.URL);
 
-  // Don't create web views for invalid URLs.
-  if (!requestURL.is_valid())
-    return nil;
-
   if (![self userIsInteracting]) {
     NSString* referer = [self refererFromNavigationAction:action];
     GURL referrerURL =
