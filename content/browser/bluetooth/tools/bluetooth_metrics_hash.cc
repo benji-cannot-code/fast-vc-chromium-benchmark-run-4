86f3bd23f462734720b9747ea64050ba4526f3dc
beaufort.francois <beaufort.francois@gmail.com>
2016-09-28 14:45:48 -0700

    bluetooth: Add more UUID hashes to histograms.xml
    Review-Url: https://codereview.chromium.org/2365503005
    Cr-Commit-Position: refs/heads/master@{#421647}

--- content/browser/bluetooth/tools/bluetooth_metrics_hash.cc
+++ content/browser/bluetooth/tools/bluetooth_metrics_hash.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/bluetooth/bluetooth_uuid.h"
 
 int main(int argc, char** argv) {
-  if (argc <= 1) {
+  if (argc <= 2) {
     std::cout << "Generates hash values given UUIDs using the same method\n"
               << "as in bluetooth_metrics.cc.\n"
               << "\n"
@@ -18,21 +18,22 @@ int main(int argc, char** argv) {
               << "Note that tools/metrics/histograms/pretty_print.py will\n"
               << "sort enum entries for you.\n"
               << "\n"
-              << "Usage: " << argv[0] << " <uuid> [uuid2 ...]\n"
+              << "Usage: " << argv[0] << " <uuid> <label> [uuid2 label2...]\n"
               << "       The UUIDs may be short UUIDs, and will be made\n"
               << "       canonical before being hashed.\n"
               << "\n"
-              << "Example: " << argv[0] << " FEFF FEFE\n"
+              << "Example: " << argv[0] << " FEFF foo FEFE bar\n"
               << "  <int value=\"62669585\" "
-                 "label=\"0000feff-0000-1000-8000-00805f9b34fb\"/>\n"
+                 "label=\"foo; 0000feff-0000-1000-8000-00805f9b34fb\"/>\n"
               << "  <int value=\"643543662\" "
-                 "label=\"0000fefe-0000-1000-8000-00805f9b34fb\"/>\n";
+                 "label=\"bar; 0000fefe-0000-1000-8000-00805f9b34fb\"/>\n";
     return 1;
   }
 
-  for (int i = 1; i < argc; i++) {
-    std::string input_string(argv[i]);
-    device::BluetoothUUID uuid(input_string);
+  for (int i = 1; i < argc; i = i + 2) {
+    std::string uuid_string(argv[i]);
+    std::string label_string((i + 1 < argc) ? argv[i + 1] : "");
+    device::BluetoothUUID uuid(uuid_string);
     std::string uuid_canonical_string = uuid.canonical_value();
     uint32_t hash = base::SuperFastHash(uuid_canonical_string.data(),
                                         uuid_canonical_string.size());
@@ -41,8 +42,8 @@ int main(int argc, char** argv) {
     // but takes a signed int as input.
     hash &= 0x7fffffff;
 
-    std::cout << "  <int value=\"" << hash << "\" label=\""
-              << uuid_canonical_string << "\"/>\n";
+    std::cout << "  <int value=\"" << hash << "\" label=\"" << label_string
+              << "; " << uuid_canonical_string << "\"/>\n";
   }
   return 0;
 }
