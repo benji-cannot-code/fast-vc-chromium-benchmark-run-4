f0ca91c0163756891f820cf6c0b842ff21fcc6c3
jww <jww@chromium.org>
2016-09-28 08:23:49 -0700

    Convert remaining suborigin LayoutTests to testharness
    This moves the remaining suborigin tests to testharness instead of the
    js-test framework. Does some refactoring along the way.
    
    R=mkwst@chromium.org
    
    Review-Url: https://codereview.chromium.org/2380573002
    Cr-Commit-Position: refs/heads/master@{#421523}

--- third_party/WebKit/LayoutTests/http/tests/security/suborigins/crossorigin/suborigin-cross-origin-script-window-onerror-no-cors.php
+++ third_party/WebKit/LayoutTests/http/tests/security/suborigins/crossorigin/suborigin-cross-origin-script-window-onerror-no-cors.php
@@ -5,27 +5,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <!DOCTYPE html>
 <head>
 <meta charset="utf-8">
+<title>Verify that thrown exception is sanitized when crossorigin script loaded without crossorigin attribute</title>
+<script src="/resources/testharness.js"></script>
+<script src="/resources/testharnessreport.js"></script>
 </head>
 <body>
-<script src="/js-test-resources/js-test.js"></script>
 <script>
-window.jsTestIsAsync = true;
-description(
-  'The test passes if \'window.onerror\' gets sanitized information about ' +
-  'an exception thrown in a script loaded from a suborigin.');
+setup({ allow_uncaught_exception: true });
 
 window.onerror = function(msg, url, line, column, error) {
-  window.msg = msg;
-  window.url = url;
-  window.line = line;
-  window.column = column;
-  window.errorObject = error;
-  shouldBeFalse('/SomeError/.test(msg)');
-  shouldBeEqualToString('url', '');
-  shouldBe('line', '0');
-  shouldBe('column', '0');
-  shouldBe('window.errorObject', 'null');
-  finishJSTest();
+  assert_false(/SomeError/.test(msg));
+  assert_equals(url, '');
+  assert_equals(line, 0);
+  assert_equals(column, 0);
+  assert_equals(error, null);
+  done();
 }
 </script>
 <script src="/security/resources/cors-script.php?fail=true&cors=false"></script>
