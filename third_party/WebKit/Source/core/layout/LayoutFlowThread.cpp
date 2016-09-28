713fefb1cfe14727b16440de62e2e92dd804bd09
mstensho <mstensho@opera.com>
2016-09-28 14:28:06 -0700

    Support for multiple block fragments in getClientRects().
    Objects crossing column boundaries, for instance, should create one rectangle
    for each column they live in.
    
    Two old tests had to be updated, because they depended on the old and incorrect
    behavior (pick the bigger / center column and create one huge rectangle there).
    
    Add fragmentainerInFlowThread() to FragmentainerIterator. Removed the
    updateOutput() thing. Instead, have the getters compute what they need on the
    fly. This makes more sense now, since none of the (2) FragmentainerIterator
    users need to calculate everything. Also don't require a clip rectangle.
    
    Some extra attention is required when processing objects with a zero-height
    bounding box now. Previously, we didn't need to worry about those, since no
    bounding box means no painting, hit-testing, etc. But now, with
    getBoundingClientRect(), life is different.
    
    BUG=362232
    
    Review-Url: https://codereview.chromium.org/2360913004
    Cr-Commit-Position: refs/heads/master@{#421643}

--- third_party/WebKit/Source/core/layout/LayoutFlowThread.cpp
+++ third_party/WebKit/Source/core/layout/LayoutFlowThread.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/layout/LayoutFlowThread.h"
 
+#include "core/layout/FragmentainerIterator.h"
 #include "core/layout/LayoutMultiColumnSet.h"
 
 namespace blink {
@@ -119,6 +120,27 @@ void LayoutFlowThread::computeLogicalHeight(LayoutUnit, LayoutUnit logicalTop, L
     for (LayoutMultiColumnSetList::const_iterator iter = m_multiColumnSetList.begin(); iter != m_multiColumnSetList.end(); ++iter) {
         LayoutMultiColumnSet* columnSet = *iter;
         computedValues.m_extent += columnSet->logicalHeightInFlowThread();
+    }
+}
+
+void LayoutFlowThread::absoluteQuadsForDescendant(const LayoutBox& descendant, Vector<FloatQuad>& quads)
+{
+    LayoutPoint offsetFromFlowThread;
+    for (const LayoutObject* object = &descendant; object != this;) {
+        const LayoutObject* container = object->container();
+        offsetFromFlowThread += object->offsetFromContainer(container);
+        object = container;
+    }
+    LayoutRect boundingRectInFlowThread(offsetFromFlowThread, descendant.frameRect().size());
+    // Set up a fragments relative to the descendant, in the flow thread coordinate space, and
+    // convert each of them, individually, to absolute coordinates.
+    for (FragmentainerIterator iterator(*this, boundingRectInFlowThread); !iterator.atEnd(); iterator.advance()) {
+        LayoutRect fragment = boundingRectInFlowThread;
+        // We use inclusiveIntersect() because intersect() would reset the coordinates for
+        // zero-height objects.
+        fragment.inclusiveIntersect(iterator.fragmentainerInFlowThread());
+        fragment.moveBy(-offsetFromFlowThread);
+        quads.append(descendant.localToAbsoluteQuad(FloatRect(fragment)));
     }
 }
 
--- third_party/WebKit/Source/core/layout/MultiColumnFragmentainerGroup.h
+++ third_party/WebKit/Source/core/layout/MultiColumnFragmentainerGroup.h
@@ -68,6 +68,7 @@ public:
     LayoutPoint visualPointToFlowThreadPoint(const LayoutPoint& visualPoint) const;
     LayoutRect fragmentsBoundingBox(const LayoutRect& boundingBoxInFlowThread) const;
 
+    LayoutRect flowThreadPortionRectAt(unsigned columnIndex) const;
     LayoutRect flowThreadPortionOverflowRectAt(unsigned columnIndex) const;
 
     // Get the first and the last column intersecting the specified block range.
@@ -94,7 +95,6 @@ private:
 
     LayoutRect columnRectAt(unsigned columnIndex) const;
     LayoutUnit logicalTopInFlowThreadAt(unsigned columnIndex) const { return m_logicalTopInFlowThread + columnIndex * m_columnHeight; }
-    LayoutRect flowThreadPortionRectAt(unsigned columnIndex) const;
 
     // Return the column that the specified visual point belongs to. Only the coordinate on the
     // column progression axis is relevant. Every point belongs to a column, even if said point is
