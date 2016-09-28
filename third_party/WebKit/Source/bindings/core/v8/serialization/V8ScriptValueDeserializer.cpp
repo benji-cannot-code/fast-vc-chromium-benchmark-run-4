ff8339fdda443f8395e53f2c13f520a3911db6c3
jbroman <jbroman@chromium.org>
2016-09-28 13:34:46 -0700

    Support CompositorProxy on the V8-based structured clone path.
    BUG=148757
    
    Review-Url: https://codereview.chromium.org/2365163002
    Cr-Commit-Position: refs/heads/master@{#421629}

--- third_party/WebKit/Source/bindings/core/v8/serialization/V8ScriptValueDeserializer.cpp
+++ third_party/WebKit/Source/bindings/core/v8/serialization/V8ScriptValueDeserializer.cpp
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/core/v8/serialization/V8ScriptValueDeserializer.h"
 
 #include "bindings/core/v8/ToV8.h"
+#include "core/dom/CompositorProxy.h"
 #include "core/dom/DOMArrayBuffer.h"
 #include "core/dom/DOMSharedArrayBuffer.h"
 #include "core/dom/MessagePort.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/ImageData.h"
 #include "core/offscreencanvas/OffscreenCanvas.h"
 #include "platform/RuntimeEnabledFeatures.h"
+#include "platform/graphics/CompositorMutableProperties.h"
 #include "public/platform/WebBlobInfo.h"
 #include "wtf/CheckedNumeric.h"
 
@@ -124,6 +126,18 @@ ScriptWrappable* V8ScriptValueDeserializer::readDOMObject(SerializationTag tag)
             return nullptr;
         const WebBlobInfo& info = (*m_blobInfoArray)[index];
         return Blob::create(getOrCreateBlobDataHandle(info.uuid(), info.type(), info.size()));
+    }
+    case CompositorProxyTag: {
+        uint64_t element;
+        uint32_t properties;
+        const uint32_t validPropertiesMask = static_cast<uint32_t>(
+            (1u << CompositorMutableProperty::kNumProperties) - 1);
+        if (!RuntimeEnabledFeatures::compositorWorkerEnabled()
+            || !readUint64(&element)
+            || !readUint32(&properties)
+            || (properties & ~validPropertiesMask))
+            return nullptr;
+        return CompositorProxy::create(m_scriptState->getExecutionContext(), element, properties);
     }
     case ImageBitmapTag: {
         uint32_t originClean = 0, isPremultiplied = 0, width = 0, height = 0, pixelLength = 0;
