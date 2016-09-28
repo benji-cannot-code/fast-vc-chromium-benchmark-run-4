17062e41b3ade19f8b27cacd4c107dd39cfd62c6
avayvod <avayvod@chromium.org>
2016-09-27 20:21:59 -0700

    [Blink, RemotePlayback] Added the disableRemotePlayback check to prompt().
    BUG=647441
    TEST=avayvod.github.io/remote-playback/test.html and a layout test
    
    Review-Url: https://codereview.chromium.org/2348703002
    Cr-Commit-Position: refs/heads/master@{#421436}

--- third_party/WebKit/Source/modules/remoteplayback/RemotePlayback.cpp
+++ third_party/WebKit/Source/modules/remoteplayback/RemotePlayback.cpp
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "modules/remoteplayback/RemotePlayback.h"
 
 #include "bindings/core/v8/ScriptPromiseResolver.h"
+#include "core/HTMLNames.h"
 #include "core/dom/DOMException.h"
 #include "core/dom/Document.h"
 #include "core/events/Event.h"
@@ -83,10 +84,15 @@ ScriptPromise RemotePlayback::getAvailability(ScriptState* scriptState)
 
 ScriptPromise RemotePlayback::prompt(ScriptState* scriptState)
 {
-    // TODO(avayvod): implement steps 3, 4, 5, 8, 9 of the algorithm.
+    // TODO(avayvod): implement steps 4, 5, 8, 9 of the algorithm.
     // https://crbug.com/647441
     ScriptPromiseResolver* resolver = ScriptPromiseResolver::create(scriptState);
     ScriptPromise promise = resolver->promise();
+
+    if (m_mediaElement->fastHasAttribute(HTMLNames::disableremoteplaybackAttr)) {
+        resolver->reject(DOMException::create(InvalidStateError, "disableRemotePlayback attribute is present."));
+        return promise;
+    }
 
     // TODO(avayvod): should we have a separate flag to disable the user gesture
     // requirement (for tests) or reuse the one for the PresentationRequest::start()?
