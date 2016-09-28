b5e91778be0f9d55b534205a1cc217561bcbf760
pdr <pdr@chromium.org>
2016-09-28 08:25:53 -0700

    Run SVG image animations at 60fps
    Previously we ran SVG animations at 40fps for historical reasons, but
    this looks particularly bad when run alongside a compositor-driven
    animation running at or above 60fps. This patch switches SVG images to
    tick at 1/60ms which matches WebKit: https://trac.webkit.org/r200164.
    
    This patch is not the ideal solution because the main document timeline
    can differ (eg, 120fps) and drift from the image's timeline. Still, this
    dramatically improves https://pr.gg/svgExamples/smilAnimationImage.html.
    
    BUG=469196
    
    Review-Url: https://codereview.chromium.org/2374933002
    Cr-Commit-Position: refs/heads/master@{#421525}

--- third_party/WebKit/Source/core/svg/graphics/SVGImageChromeClient.cpp
+++ third_party/WebKit/Source/core/svg/graphics/SVGImageChromeClient.cpp
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-static const double animationFrameDelay = 0.025;
+static const double animationFrameDelay = 1.0 / 60;
 
 SVGImageChromeClient::SVGImageChromeClient(SVGImage* image)
     : m_image(image)
