3645f27090eb30fec3d6b3d545ba85b940e927a9
foolip <foolip@chromium.org>
2016-09-28 05:31:13 -0700

    Expose the MediaStream interface
    Intent to Ship:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/9WIVyDz1T34/5Nmj46UXAwAJ
    
    BUG=649331
    
    Review-Url: https://codereview.chromium.org/2365883002
    Cr-Commit-Position: refs/heads/master@{#421502}

--- ppapi/tests/test_video_destination.cc
+++ ppapi/tests/test_video_destination.cc
@@ -101,7 +101,7 @@ std::string TestVideoDestination::TestCreate() {
 
 std::string TestVideoDestination::TestPutFrame() {
   std::string js_code;
-  js_code += "var test_stream = new webkitMediaStream([]);"
+  js_code += "var test_stream = new MediaStream([]);"
              "var url = URL.createObjectURL(test_stream);"
              "var plugin = document.getElementById('plugin');"
              "plugin.postMessage(url);";
@@ -125,4 +125,3 @@ std::string TestVideoDestination::TestPutFrame() {
 
   PASS();
 }
-
