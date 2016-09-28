49cf777560e456432e7f4506ac211f42a197b928
bugsnash <bugsnash@chromium.org>
2016-09-28 01:17:36 -0700

    Revert of Made ElementRareData store ComputedStyle on LayoutObject if possible. (patchset #5 id:100001 of https://codereview.chromium.org/2293713002/ )
    Reason for revert:
    Storage of ComputedStyle on LayoutObject causes invalid LayoutObject state. Also no longer required for consistency as Squad project requires redesign and will no longer store ComputedStyle on Node's data union.
    
    Original issue's description:
    > Made ElementRareData store ComputedStyle on LayoutObject if possible.
    >
    > Made ElementRareData store ComputedStyle on LayoutObject of it's base
    > class NodeRareDataBase if it has a LayoutObject.
    > Otherwise it is stored on the ComputedStyle member of ElementRareData.
    >
    > The reason for this is to make it consistent with storage of
    > ComputedStyle on Node, which stores a union that can be one of:
    > 1) a ComputedStyle
    > 2) a LayoutObject (ComputedStyle can be stored on this)
    > 3) a NodeRareData (LayoutObject can be stored on this which stores
    > ComputedStyle or if no LayoutObject ComputedStyle can be stored
    > directly on ElementRareData)
    >
    > This patch encapsulates the logic of where to store the ComputedStyle
    > on the ElementRareData so that it doesn't need to be done at the call
    > site (e.g. in new method Node::setComputedStyle from
    > https://codereview.chromium.org/2001453002)
    >
    > The clearComputedStyle method is special and only clears the
    > ComputedStyle member as its current use is not intended to clear a
    > LayoutObject's ComputedStyle.
    >
    > BUG=595137
    >
    > Committed: https://crrev.com/c542d3744b905a2a00f5ac2dd2047a6a323ec181
    > Cr-Commit-Position: refs/heads/master@{#418453}
    
    TBR=meade@chromium.org,nainar@chromium.org,timloh@chromium.org,esprehn@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=595137
    
    Review-Url: https://codereview.chromium.org/2378573002
    Cr-Commit-Position: refs/heads/master@{#421478}

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -1563,7 +1563,7 @@ void Element::attachLayoutTree(const AttachContext& context)
     // need to clear any state that's been added since then.
     if (hasRareData() && getStyleChangeType() == NeedsReattachStyleChange) {
         ElementRareData* data = elementRareData();
-        data->clearComputedStyleIfNoLayoutObject();
+        data->clearComputedStyle();
     }
 
     if (!isSlotOrActiveInsertionPoint())
@@ -1609,7 +1609,7 @@ void Element::detachLayoutTree(const AttachContext& context)
 
         // attachLayoutTree() will clear the computed style for us when inside recalcStyle.
         if (!document().inStyleRecalc())
-            data->clearComputedStyleIfNoLayoutObject();
+            data->clearComputedStyle();
 
         if (ElementAnimations* elementAnimations = data->elementAnimations()) {
             if (context.performingReattach) {
@@ -1739,7 +1739,7 @@ void Element::recalcStyle(StyleRecalcChange change, Text* nextTextSibling)
         if (hasRareData()) {
             ElementRareData* data = elementRareData();
             if (change != IndependentInherit)
-                data->clearComputedStyleIfNoLayoutObject();
+                data->clearComputedStyle();
 
             if (change >= IndependentInherit) {
                 if (ElementAnimations* elementAnimations = data->elementAnimations())
