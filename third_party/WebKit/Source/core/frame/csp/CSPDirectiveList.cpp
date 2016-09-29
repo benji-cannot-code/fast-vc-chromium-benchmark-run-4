815fac2ec7a6ffe61a96a37b9290a7b50eb7de4c
dherkel <dherkel@google.com>
2016-09-29 08:00:46 -0700

    deleted UseCounter usages (InvalidFrameAncestorsDirectiveInMetaCSP, InvalidSandboxDirectiveInMetaCSP, CSPDirectiveList) from UseCounter header file and also removed associated calls to fix issue 645909
    BUG=645909
    
    Review-Url: https://codereview.chromium.org/2370273002
    Cr-Commit-Position: refs/heads/master@{#421818}

--- third_party/WebKit/Source/core/frame/csp/CSPDirectiveList.cpp
+++ third_party/WebKit/Source/core/frame/csp/CSPDirectiveList.cpp
@@ -671,7 +671,6 @@ void CSPDirectiveList::parseReportURI(const String& name, const String& value)
 
     // Remove report-uri in meta policies, per https://www.w3.org/TR/CSP2/#delivery-html-meta-element.
     if (m_headerSource == ContentSecurityPolicyHeaderSourceMeta) {
-        UseCounter::count(m_policy->document(), UseCounter::InvalidReportUriDirectiveInMetaCSP);
         m_policy->reportInvalidDirectiveInMeta(name);
         return;
     }
@@ -706,7 +705,6 @@ void CSPDirectiveList::setCSPDirective(const String& name, const String& value, 
 
     // Remove frame-ancestors directives in meta policies, per https://www.w3.org/TR/CSP2/#delivery-html-meta-element.
     if (m_headerSource == ContentSecurityPolicyHeaderSourceMeta && name == ContentSecurityPolicy::FrameAncestors) {
-        UseCounter::count(m_policy->document(), UseCounter::InvalidFrameAncestorsDirectiveInMetaCSP);
         m_policy->reportInvalidDirectiveInMeta(name);
         return;
     }
@@ -718,7 +716,6 @@ void CSPDirectiveList::applySandboxPolicy(const String& name, const String& sand
 {
     // Remove sandbox directives in meta policies, per https://www.w3.org/TR/CSP2/#delivery-html-meta-element.
     if (m_headerSource == ContentSecurityPolicyHeaderSourceMeta) {
-        UseCounter::count(m_policy->document(), UseCounter::InvalidSandboxDirectiveInMetaCSP);
         m_policy->reportInvalidDirectiveInMeta(name);
         return;
     }
@@ -839,8 +836,6 @@ void CSPDirectiveList::parseReflectedXSS(const String& name, const String& value
 void CSPDirectiveList::parseReferrer(const String& name, const String& value)
 {
     m_didSetReferrerPolicy = true;
-
-    UseCounter::count(m_policy->document(), UseCounter::CSPReferrerDirective);
 
     if (value.isEmpty()) {
         m_policy->reportInvalidReferrer(value);
