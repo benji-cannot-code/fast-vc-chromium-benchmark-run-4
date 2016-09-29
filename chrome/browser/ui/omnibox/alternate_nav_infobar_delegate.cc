085e5199d70f42fcdd67984bfaabaf17a54c823a
estade <estade@chromium.org>
2016-09-29 10:12:49 -0700

    Remove a couple desktop infobar png assets.
    BUG=648281
    TBR=oshima@chromium.org,rdevlin.cronin@chromium.org
    
    Review-Url: https://codereview.chromium.org/2372243003
    Cr-Commit-Position: refs/heads/master@{#421855}

--- chrome/browser/ui/omnibox/alternate_nav_infobar_delegate.cc
+++ chrome/browser/ui/omnibox/alternate_nav_infobar_delegate.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/grit/generated_resources.h"
-#include "chrome/grit/theme_resources.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/infobars/core/infobar.h"
 #include "components/omnibox/browser/shortcuts_backend.h"
@@ -110,14 +109,6 @@ AlternateNavInfoBarDelegate::GetIdentifier() const {
   return ALTERNATE_NAV_INFOBAR_DELEGATE;
 }
 
-int AlternateNavInfoBarDelegate::GetIconId() const {
-  return IDR_INFOBAR_ALT_NAV_URL;
-}
-
 gfx::VectorIconId AlternateNavInfoBarDelegate::GetVectorIconId() const {
-#if defined(OS_MACOSX)
-  return gfx::VectorIconId::VECTOR_ICON_NONE;
-#else
   return gfx::VectorIconId::GLOBE;
-#endif
 }
