fe4c29d4df5833ce31317e287d6335ae0c0f3e10
finnur <finnur@chromium.org>
2016-09-29 05:10:44 -0700

    Site Settings Desktop: Better support for chrome-extension scheme.
    BUG=650552
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2381583002
    Cr-Commit-Position: refs/heads/master@{#421803}

--- chrome/test/data/webui/settings/site_list_tests.js
+++ chrome/test/data/webui/settings/site_list_tests.js
@@ -283,6 +283,23 @@ cr.define('site_list', function() {
         }
       };
 
+      /**
+       * An example Javascript pref with a chrome-extension:// scheme.
+       * @type {SiteSettingsPref}
+       */
+      var prefsChromeExtension = {
+        exceptions: {
+          javascript: [
+            {
+              embeddingOrigin: '',
+              origin: 'chrome-extension://cfhgfbfpcbnnbibfphagcjmgjfjmojfa/',
+              setting: 'block',
+              source: 'preference',
+            },
+          ]
+        }
+      };
+
       // Import necessary html before running suite.
       suiteSetup(function() {
         CrSettingsPrefs.setInitialized();
@@ -500,11 +517,11 @@ cr.define('site_list', function() {
               MockInteractions.tap(menuItems[2]);
               return browserProxy.whenCalled(
                   'resetCategoryPermissionForOrigin');
-            }).then(function(arguments) {
-              assertEquals('http://foo.com', arguments[0]);
-              assertEquals('http://foo.com', arguments[1]);
-              assertEquals(settings.ContentSettingsTypes.COOKIES, arguments[2]);
-              assertFalse(arguments[3]);  // Incognito.
+            }).then(function(args) {
+              assertEquals('http://foo.com', args[0]);
+              assertEquals('http://foo.com', args[1]);
+              assertEquals(settings.ContentSettingsTypes.COOKIES, args[2]);
+              assertFalse(args[3]);  // Incognito.
             });
       });
 
@@ -536,11 +553,11 @@ cr.define('site_list', function() {
               MockInteractions.tap(menuItems[1]);
               return browserProxy.whenCalled(
                   'resetCategoryPermissionForOrigin');
-            }).then(function(arguments) {
-              assertEquals('http://foo.com', arguments[0]);
-              assertEquals('http://foo.com', arguments[1]);
-              assertEquals(settings.ContentSettingsTypes.COOKIES, arguments[2]);
-              assertTrue(arguments[3]);  // Incognito.
+            }).then(function(args) {
+              assertEquals('http://foo.com', args[0]);
+              assertEquals('http://foo.com', args[1]);
+              assertEquals(settings.ContentSettingsTypes.COOKIES, args[2]);
+              assertTrue(args[3]);  // Incognito.
             });
       });
 
@@ -767,6 +784,27 @@ cr.define('site_list', function() {
           assertTrue(!!menuItems);
           MockInteractions.tap(menuItems[0]);
           return browserProxy.whenCalled('setCategoryPermissionForOrigin');
+        });
+      });
+
+      test('Chrome Extension scheme', function() {
+        setupCategory(settings.ContentSettingsTypes.JAVASCRIPT,
+            settings.PermissionValues.BLOCK, prefsChromeExtension);
+        return browserProxy.whenCalled('getExceptionList').then(function(
+            contentType) {
+          Polymer.dom.flush();
+          assertMenu(['Allow', 'Remove'], testElement);
+
+          var menuItems = getMenuItems(testElement.$.listContainer, 0);
+          assertTrue(!!menuItems);
+          MockInteractions.tap(menuItems[0]);  // Action: Allow.
+          return browserProxy.whenCalled('setCategoryPermissionForOrigin');
+        }).then(function(args) {
+          assertEquals('chrome-extension://cfhgfbfpcbnnbibfphagcjmgjfjmojfa/',
+              args[0]);
+          assertEquals('', args[1]);
+          assertEquals(settings.ContentSettingsTypes.JAVASCRIPT, args[2]);
+          assertEquals('allow', args[3]);
         });
       });
     });
