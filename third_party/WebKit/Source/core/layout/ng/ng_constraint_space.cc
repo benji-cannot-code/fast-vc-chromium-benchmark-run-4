d8dc36c75286ff8cbe1e336ceb7f6db94e37fcfc
glebl <glebl@chromium.org>
2016-09-29 12:48:27 -0700

    Compute margin block start for 1st block in LayoutNG root constraint space
    It will correctly compute margin block start for the 1st block in
    LayoutNG's root constraint space. This is because margins of the root
    element's box do not collapse https://www.w3.org/TR/CSS2/box.html#collapsing-margins
    
    BUG=635619
    
    Review-Url: https://codereview.chromium.org/2368153003
    Cr-Commit-Position: refs/heads/master@{#421907}

--- third_party/WebKit/Source/core/layout/ng/ng_constraint_space.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_constraint_space.cc
@@ -19,7 +19,8 @@ NGConstraintSpace::NGConstraintSpace(NGWritingMode writing_mode,
           container_size.ConvertToPhysical(writing_mode))),
       size_(container_size),
       writing_mode_(writing_mode),
-      direction_(direction) {}
+      direction_(direction),
+      is_new_fc_(false) {}
 
 NGConstraintSpace::NGConstraintSpace(NGWritingMode writing_mode,
                                      NGDirection direction,
@@ -27,7 +28,8 @@ NGConstraintSpace::NGConstraintSpace(NGWritingMode writing_mode,
     : physical_space_(physical_space),
       size_(physical_space->ContainerSize().ConvertToLogical(writing_mode)),
       writing_mode_(writing_mode),
-      direction_(direction) {}
+      direction_(direction),
+      is_new_fc_(false) {}
 
 NGConstraintSpace::NGConstraintSpace(NGWritingMode writing_mode,
                                      NGDirection direction,
@@ -36,7 +38,8 @@ NGConstraintSpace::NGConstraintSpace(NGWritingMode writing_mode,
       offset_(constraint_space->Offset()),
       size_(constraint_space->Size()),
       writing_mode_(writing_mode),
-      direction_(direction) {}
+      direction_(direction),
+      is_new_fc_(false) {}
 
 NGConstraintSpace::NGConstraintSpace(const NGConstraintSpace& other,
                                      NGLogicalOffset offset,
@@ -45,13 +48,17 @@ NGConstraintSpace::NGConstraintSpace(const NGConstraintSpace& other,
       offset_(offset),
       size_(size),
       writing_mode_(other.WritingMode()),
-      direction_(other.Direction()) {}
+      direction_(other.Direction()),
+      is_new_fc_(false) {}
 
 NGConstraintSpace::NGConstraintSpace(NGWritingMode writing_mode,
                                      NGDirection direction,
                                      const NGConstraintSpace& other,
                                      NGLogicalSize size)
-    : size_(size), writing_mode_(writing_mode), direction_(direction) {
+    : size_(size),
+      writing_mode_(writing_mode),
+      direction_(direction),
+      is_new_fc_(false) {
   physical_space_ =
       new NGPhysicalConstraintSpace(size.ConvertToPhysical(writing_mode));
   for (const NGExclusion& exclusion : other.PhysicalSpace()->Exclusions()) {
