436ef9112d04e67a522da5af5e201ad43df67c3c
dbeam <dbeam@chromium.org>
2016-09-29 15:06:12 -0700

    MD Settings: hide signin-related UI when signin is disabled (e.g. by policy)
    R=tommycli@chromium.org
    BUG=629604
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2375013004
    Cr-Commit-Position: refs/heads/master@{#421948}

--- chrome/browser/resources/settings/people_page/people_page.js
+++ chrome/browser/resources/settings/people_page/people_page.js
@@ -359,4 +359,12 @@ Polymer({
   getIconImageset_: function(iconUrl) {
     return cr.icon.getImage(iconUrl);
   },
+
+  /**
+   * @return {boolean} Whether to show the "Sign in to Chrome" button.
+   * @private
+   */
+  showSignin_: function(syncStatus) {
+    return syncStatus.signinAllowed && !syncStatus.signedIn;
+  },
 });
