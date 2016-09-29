27a35e6148c54964bdce44c1690cfd42826d457d
mlamouri <mlamouri@chromium.org>
2016-09-29 07:21:03 -0700

    Fix media controls overflow button pseudo element and appearance names.
    Also fixes download button appearance name for more consistency.
    
    BUG=None
    R=foolip@chromium.org
    
    Review-Url: https://codereview.chromium.org/2377123002
    Cr-Commit-Position: refs/heads/master@{#421813}

--- third_party/WebKit/LayoutTests/media/overflow-menu.js
+++ third_party/WebKit/LayoutTests/media/overflow-menu.js
@@ -8,7 +8,7 @@ var overflowButtonsCSS = [
     "-internal-media-controls-cast-button",
     "-webkit-media-controls-toggle-closed-captions-button"];
 //  PseudoID for the overflow button
-var menuID = "-internal-overflow-menu-button";
+var menuID = "-internal-media-controls-overflow-button";
 //  PseudoID for the overflow list
 var listID = "-internal-media-controls-overflow-menu-list";
 
@@ -34,4 +34,3 @@ var OverflowMenuButtons = {
 
 // Default text within the overflow menu
 var overflowMenuText = ["Play", "Fullscreen", "Download", "Mute", "Cast", "Captions"];
-
--- third_party/WebKit/Source/core/html/shadow/MediaControlElements.cpp
+++ third_party/WebKit/Source/core/html/shadow/MediaControlElements.cpp
@@ -587,7 +587,7 @@ MediaControlOverflowMenuButtonElement* MediaControlOverflowMenuButtonElement::cr
     MediaControlOverflowMenuButtonElement* button = new MediaControlOverflowMenuButtonElement(mediaControls);
     button->ensureUserAgentShadowRoot();
     button->setType(InputTypeNames::button);
-    button->setShadowPseudoId(AtomicString("-internal-overflow-menu-button"));
+    button->setShadowPseudoId(AtomicString("-internal-media-controls-overflow-button"));
     button->setIsWanted(false);
     return button;
 }
